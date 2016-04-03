92229a7ee0adbf5319034a3bf57624a96fe25364
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-03 18:29:21 +0100

    hadolint: add 1.0 bottle.

--- Formula/hadolint.rb
+++ Formula/hadolint.rb
@@ -8,6 +8,12 @@ class Hadolint < Formula
   url "https://github.com/lukasmartinelli/hadolint/archive/v1.0.tar.gz"
   sha256 "9bdf9039877402f914f1f7127cc82bec43128508f199e31a5edd4b6f4555b840"
 
+  bottle do
+    sha256 "2e516c0655c320852eba081edafc0e9c48a2504460a0e55c0aaaa3e813db9a45" => :el_capitan
+    sha256 "62eea06fe495a030eec19f4e48dd4e56d9cc6e521a13eac77192c11d6edc7aaf" => :yosemite
+    sha256 "060d3f316e9237c8f5b64166da1dae5e844a41fcb5d1fd05ae577dfed0fd746f" => :mavericks
+  end
+
   depends_on "ghc" => :build
   depends_on "cabal-install" => :build
 
