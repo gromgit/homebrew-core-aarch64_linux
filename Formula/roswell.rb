9baa9e73ab5083523bac2a0966fff90d293d0e20
Alex Dunn <dunn.alex@gmail.com>
2016-04-03 20:21:54 -0700

    roswell 0.0.4.57 (new formula)
    Closes #46.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

---
+++ Formula/roswell.rb
@@ -0,0 +1,26 @@
+class Roswell < Formula
+  desc "Lisp installer and launcher for major environments"
+  homepage "https://github.com/roswell/roswell"
+  url "https://github.com/roswell/roswell/archive/v0.0.4.57.tar.gz"
+  sha256 "05985f10c8577bba664f6c5db3baa60d41704d76702609107599689504a93fd0"
+  head "https://github.com/roswell/roswell.git"
+
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
+
+  def install
+    system "./bootstrap"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--enable-manual-generation",
+                          "--enable-html-generation",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    ENV["ROSWELL_HOME"] = testpath
+    system bin/"ros", "init"
+    File.exist? testpath/".roswell/config"
+  end
+end
