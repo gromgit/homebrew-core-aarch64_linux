3af4d8124e1f07c5648575b06c3b23e8f18dc336
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-04 18:43:20 +0100

    roswell: add 0.0.4.57 bottle.

--- Formula/roswell.rb
+++ Formula/roswell.rb
@@ -5,6 +5,12 @@ class Roswell < Formula
   sha256 "05985f10c8577bba664f6c5db3baa60d41704d76702609107599689504a93fd0"
   head "https://github.com/roswell/roswell.git"
 
+  bottle do
+    sha256 "e788de3677cba691bd2197a067fff66d961b776590529081b4e554a1e5803a24" => :el_capitan
+    sha256 "b2bdbd13311e5363dfde0973d7db06a6580da4edc483dd7f8e313f524d0a178c" => :yosemite
+    sha256 "ae376fc09deabafcad4440ff0612e398817a259e19c0690741da0a81a41f035f" => :mavericks
+  end
+
   depends_on "automake" => :build
   depends_on "autoconf" => :build
 
