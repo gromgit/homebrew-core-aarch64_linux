155c75bdaf1aebc754978879b3363aede3ded3e4
Anton Petrov <givemeletter@gmail.com>
2016-04-05 02:29:28 +0400

    tee-clc 14.0.3
    Closes #41.

--- Formula/tee-clc.rb
+++ Formula/tee-clc.rb
@@ -1,8 +1,8 @@
 class TeeClc < Formula
-  desc "Eclipse client for Visual Studio 2010 Team Foundation Server"
-  homepage "https://www.microsoft.com/en-us/download/details.aspx?id=47727"
-  url "https://download.microsoft.com/download/8/F/6/8F68DDC8-4E75-4BEA-951E-C14BFF336E81/TEE-CLC-14.0.1.zip"
-  sha256 "2b7725901ae1e87427ba13df7c76e1492b37e4da7f6fbcd3b0edf2a723d0e556"
+  desc "Microsoft Team Explorer Everywhere 2015 Command Line Client"
+  homepage "https://www.visualstudio.com/en-us/products/team-explorer-everywhere-vs.aspx"
+  url "https://download.microsoft.com/download/8/F/6/8F68DDC8-4E75-4BEA-951E-C14BFF336E81/TEE-CLC-14.0.3.zip"
+  sha256 "615125b71305f2f8d03178d6850ea5088b52b1998bd99ff07eed5c22e29af5eb"
 
   bottle :unneeded
 
@@ -10,7 +10,6 @@ class TeeClc < Formula
 
   def install
     libexec.install "tf", "lib"
-    (libexec/"tf").chmod 0755
     (libexec/"native").install "native/macosx"
     bin.write_exec_script libexec/"tf"
     share.install "help"
