9c7bbbe18f058b7a2381c0cc4a4d1ac1736e4b7a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-05 14:56:27 +0100

    certstrap: add 1.0.0 bottle.

--- Formula/certstrap.rb
+++ Formula/certstrap.rb
@@ -5,6 +5,13 @@ class Certstrap < Formula
     :tag => "v1.0.0",
     :revision => "9671766fc7853ce55c64aebdada03dba918c9a95"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "113340ae23315f9a7e4edb3331eae2f05372e5cd33b79cdad3cd0d29eee42cce" => :el_capitan
+    sha256 "d227738fe010c1bd3a6a08b3237819fa4c9e4e456a814acda076d34371955330" => :yosemite
+    sha256 "fa77e8e9473beef9d382e1de68f8cea0e3b4adb879a0adbb8790fcf55e8bac2c" => :mavericks
+  end
+
   depends_on "go" => :build
   depends_on "godep" => :build
 
