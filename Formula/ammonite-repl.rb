ec671a5095faca53b811ad902f1df7070b9b2402
Srepfler Srdan <schrepfler@gmail.com>
2016-04-05 09:59:37 +0200

    ammonite-repl 0.5.7

--- Formula/ammonite-repl.rb
+++ Formula/ammonite-repl.rb
@@ -1,30 +1,20 @@
 class AmmoniteRepl < Formula
   desc "Ammonite is a cleanroom re-implementation of the Scala REPL"
   homepage "https://lihaoyi.github.io/Ammonite/#Ammonite-REPL"
-  url "https://github.com/lihaoyi/Ammonite/archive/0.5.6.tar.gz"
-  sha256 "c131a984aa101b927e34a84832b85f8e9f54c1d4a5a1f2e45d18d26a3c6122cb"
+  url "https://git.io/vVfy5", :using => :nounzip
+  sha256 "ef410dc843391c6b3199f2552a24f5fe3f3c79a7558d813089c8dce8b4ab97b6"
+  version "0.5.7"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "02fab559ad7837181378cf622476049808ba3eb02af2dacdf3ab651e6cb4d103" => :el_capitan
-    sha256 "95d030040a6fd69368b052f53c25cb8ec6ac0c84e422fe4a8f765d9d36459334" => :yosemite
-    sha256 "40f14e8805cb7b33fb685463bc77c1ab0e1c7e8e54eb15817c604d65b7dd984b" => :mavericks
-  end
+  bottle :unneeded
 
-  depends_on :java => "1.6+"
-  depends_on "sbt" => :build
+  depends_on :java => "1.7+"
 
   def install
-    ENV.java_cache
-
-    system "sbt", "repl/assembly"
-
-    bin.install "repl/target/scala-2.11/ammonite-repl-#{version}-2.11.7" => "amm"
+    bin.install "vVfy5" => "amm"
   end
 
   test do
     ENV.java_cache
-
     assert_equal "hello world!", shell_output("#{bin}/amm -c 'print(\"hello world!\")'")
   end
 end
