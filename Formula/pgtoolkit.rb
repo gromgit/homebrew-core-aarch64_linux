bf46c617805272a96fa4e590fa3eb1020f79df17
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-05 20:33:40 +0100

    pgtoolkit: add 1.0.2 bottle.

--- Formula/pgtoolkit.rb
+++ Formula/pgtoolkit.rb
@@ -4,6 +4,13 @@ class Pgtoolkit < Formula
   url "https://github.com/grayhemp/pgtoolkit/archive/v1.0.2.tar.gz"
   sha256 "d86f34c579a4c921b77f313d4c7efbf4b12695df89e6b68def92ffa0332a7351"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b194cb25016dff31295a0b782413e21f4cfdb97331573c6920f7a19c8a888f01" => :el_capitan
+    sha256 "3a4b78ebf326a158bc9eb6f1052eb1d3a95c6f4fcf7d7c5894da844cca324232" => :yosemite
+    sha256 "ca0c459a7ab2192786b3de034aa26016c5fdee75c58ec8bcb6d0e7d2a1fd784b" => :mavericks
+  end
+
   # depends on system Perl, no other dependencies
 
   def install
