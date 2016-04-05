9abc63d477d2a9e4bca453d81cb850728ce9aa0a
Tristan Bayfield <tristan.bayfield@sfu.ca>
2016-04-05 14:55:19 -0700

    neo4j 2.3.3, 3.0.0-M05 (devel)
    neo4j 2.3.3
    
    Closes #202.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/neo4j.rb
+++ Formula/neo4j.rb
@@ -1,21 +1,27 @@
 class Neo4j < Formula
   desc "Robust (fully ACID) transactional property graph database"
   homepage "http://neo4j.com"
-  url "http://dist.neo4j.org/neo4j-community-2.3.2-unix.tar.gz"
-  version "2.3.2"
-  sha256 "37e24d95c914c54d5cbbe99473d4beef89da78adb2db04eb87258a489225932a"
+  url "http://dist.neo4j.org/neo4j-community-2.3.3-unix.tar.gz"
+  version "2.3.3"
+  sha256 "01559c55055516a42ee2dd100137b6b55d63f02959a3c6c6db7a152e045828d9"
 
   devel do
-    url "http://dist.neo4j.org/neo4j-community-3.0.0-M02-unix.tar.gz"
-    sha256 "2a20f420e94fe4189363ce8ab327c0e5e054df3fc74a0249e9e2c7fe0455a0d6"
-    version "3.0.0-M02"
+    url "http://dist.neo4j.org/neo4j-community-3.0.0-M05-unix.tar.gz"
+    sha256 "2b7878f424859de6951e86f9abb71701d8d45ab22e1157410504fe6a6bb17a95"
+    version "3.0.0-M05"
   end
 
   bottle :unneeded
 
+  depends_on :java => "1.7+"
+
   def install
     # Remove windows files
     rm_f Dir["bin/*.bat"]
+
+    # Fix Java detection to work with 1.8
+    # https://github.com/neo4j/neo4j/issues/6895
+    inreplace "bin/utils", "java_home -v 1.7", "java_home -v 1.7+" if build.stable?
 
     # Install jars in libexec to avoid conflicts
     libexec.install Dir["*"]
@@ -29,5 +35,13 @@ class Neo4j < Formula
       wrapper.java.additional=-Djava.awt.headless=true
       wrapper.java.additional.4=-Dneo4j.ext.udc.source=homebrew
     EOS
+  end
+
+  test do
+    ENV.java_cache
+    ENV["NEO4J_LOG"] = testpath/"libexec/data/log/neo4j.log"
+    ENV["NEO4J_PIDFILE"] = testpath/"libexec/data/neo4j-service.pid"
+    mkpath testpath/"libexec/data/log"
+    assert_match /Neo4j .*is not running/i, shell_output("#{bin}/neo4j status", 3)
   end
 end
