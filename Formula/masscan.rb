dba0404796547f860fabe1c5f2bf8e173ebdd0ce
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-06 12:59:41 +0100

    masscan: add 1.0.3 bottle.

--- Formula/masscan.rb
+++ Formula/masscan.rb
@@ -5,6 +5,13 @@ class Masscan < Formula
   sha256 "331edd529df1904bcbcfb43029ced7e2dafe1744841e74cd9fc9f440b8301085"
   head "https://github.com/robertdavidgraham/masscan.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bb67f591ca801a8e070861f7888983aae0abde5ff74536013049d21b7659ad9a" => :el_capitan
+    sha256 "7ca9565d5ab497db0c1ce0963de6aa42259dcd7f87d54f52359cdba53deffd57" => :yosemite
+    sha256 "8c2afbee6a71b2e4280d8da97c1c30829477091d6fab7f43dcf8a6890bad520e" => :mavericks
+  end
+
   def install
     system "make"
     bin.install "bin/masscan"
