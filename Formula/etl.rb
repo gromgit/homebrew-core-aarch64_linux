4fbcdde5b85f60e3c157c0d1d11662ba29513842
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-06 11:59:14 +0100

    etl: add 0.04.19 bottle.

--- Formula/etl.rb
+++ Formula/etl.rb
@@ -4,6 +4,13 @@ class Etl < Formula
   url "https://downloads.sourceforge.net/project/synfig/releases/1.0.2/source/ETL-0.04.19.tar.gz"
   sha256 "ba944c1a07fd321488f9d034467931b8ba9e48454abef502a633ff4835380c1c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "10244415e0dbf71f94c7585595632a09773a49dbc5bf5ac8de7e062f29c7f2b4" => :el_capitan
+    sha256 "29198ad9d848f2ff79b224a5467da1fb22a474de5ffc3e287196fd3822a45178" => :yosemite
+    sha256 "024271929c1e3de9d4c4e256a932fa9525395f7421fc174e7010251ab9a4b37e" => :mavericks
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
