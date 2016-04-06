d859dc57063ff8e1ee192c429f37fafa08c0f867
Thibault Charbonnier <thibaultcha@me.com>
2016-04-06 12:47:53 -0700

    argon2 20160406
    Closes #108.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/argon2.rb
+++ Formula/argon2.rb
@@ -1,8 +1,8 @@
 class Argon2 < Formula
   desc "Password hashing library and CLI utility"
   homepage "https://github.com/P-H-C/phc-winner-argon2"
-  url "https://github.com/P-H-C/phc-winner-argon2/archive/20151206.tar.gz"
-  sha256 "29f9eb6d84bfe29604c48830432aa030e96158d66e85a2c83b115e29eb025e85"
+  url "https://github.com/P-H-C/phc-winner-argon2/archive/20160406.tar.gz"
+  sha256 "fbfbea50ebdaddd2b945ee877c8e47b7863c080ee2238603ed6918a84952dc3c"
 
   bottle do
     cellar :any
@@ -13,13 +13,13 @@ class Argon2 < Formula
 
   def install
     system "make"
-    include.install "src/argon2.h"
+    include.install "include/argon2.h"
     lib.install "libargon2.dylib"
     bin.install "argon2"
   end
 
   test do
-    assert_equal "$argon2i$m=65536,t=2,p=4$c29tZXNhbHQAAAAAAAAAAA$Q38e04oLanBHl/MHZra3ypCikyWHhVYRVbekTeGl03o\n",
+    assert_equal "$argon2i$v=19$m=65536,t=2,p=4$c29tZXNhbHQ$LY26Xe109v98jqoS57OD9ZBwh1CAQavxHyHx1IQ2g/A\n",
       pipe_output("echo -n password | #{bin}/argon2 somesalt -t 2 -m 16 -p 4 | grep Encoded | awk '{print $2}'")
   end
 end
