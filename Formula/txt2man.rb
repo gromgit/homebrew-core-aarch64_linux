ab295f78faf27474c7a1ba0bbb9de19d1beda83e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-07 21:11:59 +0100

    txt2man: add 1.5.6 bottle.

--- Formula/txt2man.rb
+++ Formula/txt2man.rb
@@ -5,6 +5,13 @@ class Txt2man < Formula
   sha256 "df9d972c6930576328b779e64aed6d3e0106118e5a4069172f06db290f32586a"
   head "https://github.com/mvertes/txt2man.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b21b275a37f0f8fd3623e3c5590326737f448f9cc2dd9f24556021d2326fef6c" => :el_capitan
+    sha256 "d3831605b2d3c5b40dcc9f9a24ab875015c399da283b09f4de61161752b7e590" => :yosemite
+    sha256 "b456aee5d116f56b8c0792f904f1c44b437d8ded3a3fa0409fa12ca897020233" => :mavericks
+  end
+
   depends_on "gawk"
 
   def install
