f77a63ab387988d03a004683bcda53c310db4ad6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-07 19:12:34 +0100

    vis: add 0.2 bottle.

--- Formula/vis.rb
+++ Formula/vis.rb
@@ -6,6 +6,13 @@ class Vis < Formula
 
   head "https://github.com/martanne/vis.git"
 
+  bottle do
+    cellar :any
+    sha256 "ddd993ff3896295ed2e224c10c4a21de7fdc19f50d62fdaca49fa0f8bccb1621" => :el_capitan
+    sha256 "2fbde4500fbf8a25c8e07ad4a5a62231778fe0d2f01a01c855889940629730cd" => :yosemite
+    sha256 "f11564785c0a4d4884ca69ad425af43b1f9ce629dcf92ce2e1e1f438998707d6" => :mavericks
+  end
+
   depends_on "lua" => :recommended
   depends_on "libtermkey"
 
