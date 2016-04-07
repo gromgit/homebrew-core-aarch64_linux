e9c286950301ee94c702583b8bcff272ad202098
Alex Dunn <dunn.alex@gmail.com>
2016-04-07 14:22:28 -0700

    moe 1.8
    Closes #141.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/moe.rb
+++ Formula/moe.rb
@@ -1,9 +1,9 @@
 class Moe < Formula
   desc "Console text editor for ISO-8859 and ASCII"
   homepage "https://www.gnu.org/software/moe/moe.html"
-  url "http://ftpmirror.gnu.org/moe/moe-1.7.tar.lz"
-  mirror "https://ftp.gnu.org/pub/gnu/moe/moe-1.7.tar.lz"
-  sha256 "33ba66f948353c105232e9c8b2da8dc7b6ddc8cdff4beb3e3f55d78bc3acf1bb"
+  url "http://ftpmirror.gnu.org/moe/moe-1.8.tar.lz"
+  mirror "https://ftp.gnu.org/pub/gnu/moe/moe-1.8.tar.lz"
+  sha256 "7eff4ef2197d8febec99ad4efd4e277b1f3b7d4660f75e3b42b8818bc6bb6457"
 
   bottle do
     sha256 "d47871f32ce6e75bfad7859cbfceb900b4bc5c2858336a798b346385b6fec9ae" => :yosemite
