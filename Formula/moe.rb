1869ce0720fa6e40cd9eb57b2eeb21a20c3ea530
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-07 22:35:51 +0100

    moe: update 1.8 bottle.

--- Formula/moe.rb
+++ Formula/moe.rb
@@ -6,9 +6,9 @@ class Moe < Formula
   sha256 "7eff4ef2197d8febec99ad4efd4e277b1f3b7d4660f75e3b42b8818bc6bb6457"
 
   bottle do
-    sha256 "d47871f32ce6e75bfad7859cbfceb900b4bc5c2858336a798b346385b6fec9ae" => :yosemite
-    sha256 "4f95aa8a7068d150be7ad7c9ad4a2d2c0dc7384e70100cae21e65f2ef67bf051" => :mavericks
-    sha256 "ee58b2b36a26dd18121f7edacf3d1bb645bc314a90b6a62963e8eded725970a9" => :mountain_lion
+    sha256 "feea70d7c37c302d0992a50c04e3c47b43f0404decc6bf651cf8fcaba46fa562" => :el_capitan
+    sha256 "4428475d4364b8dfd1a6fb34d4883eb6fce35b8a88bfab8675683f5f13127714" => :yosemite
+    sha256 "0ebf4f6c5ef42d83cdc2a2a129ebe9554c55ca807fa1be03f7d9b4f454719d55" => :mavericks
   end
 
   def install
