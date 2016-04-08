a0a1cc545b529cee85fbd83e52d67a7eb26b55b5
ilovezfs <ilovezfs@icloud.com>
2016-04-08 09:49:42 -0700

    mitmproxy: Pillow wants zlib
    Closes #77.
    
    Closes #104.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mitmproxy.rb
+++ Formula/mitmproxy.rb
@@ -58,8 +58,8 @@ class Mitmproxy < Formula
   end
 
   resource "click" do
-    url "https://pypi.python.org/packages/source/c/click/click-6.3.tar.gz"
-    sha256 "b720d9faabe193287b71e3c26082b0f249501288e153b7e7cfce3bb87ac8cc1c"
+    url "https://pypi.python.org/packages/source/c/click/click-6.6.tar.gz"
+    sha256 "cc6a19da8ebff6e7074f731447ef7e112bd23adf3de5c597cf9989f2fd8defe9"
   end
 
   resource "ConfigArgParse" do
@@ -83,13 +83,13 @@ class Mitmproxy < Formula
   end
 
   resource "h2" do
-    url "https://pypi.python.org/packages/source/h/h2/h2-2.1.2.tar.gz"
-    sha256 "e507c31e0647b262011470747e531b55aabcdb1522f87b92a01ee1dfa9569d1f"
+    url "https://pypi.python.org/packages/source/h/h2/h2-2.1.3.tar.gz"
+    sha256 "7d36132c42edcc19e771555e569c3f3a8610ff052e8e3d19c91b4c29a1c31e49"
   end
 
   resource "hpack" do
-    url "https://pypi.python.org/packages/source/h/hpack/hpack-2.1.0.tar.gz"
-    sha256 "535e0628e7b44f6eb76d2acf908b6537c6819f6aad400bcaebb8bde161de7cd9"
+    url "https://pypi.python.org/packages/source/h/hpack/hpack-2.1.1.tar.gz"
+    sha256 "17cf0750f0555447f546b4754f69c8a906a3c10a51d1884c83e41f4f3bd71f8a"
   end
 
   resource "html2text" do
@@ -103,8 +103,8 @@ class Mitmproxy < Formula
   end
 
   resource "idna" do
-    url "https://pypi.python.org/packages/source/i/idna/idna-2.0.tar.gz"
-    sha256 "16199aad938b290f5be1057c0e1efc6546229391c23cea61ca940c115f7d3d3b"
+    url "https://pypi.python.org/packages/source/i/idna/idna-2.1.tar.gz"
+    sha256 "ed36f281aebf3cd0797f163bb165d84c31507cedd15928b095b1675e2d04c676"
   end
 
   resource "ipaddress" do
@@ -133,8 +133,8 @@ class Mitmproxy < Formula
   end
 
   resource "Pillow" do
-    url "https://pypi.python.org/packages/source/P/Pillow/Pillow-3.1.1.tar.gz"
-    sha256 "486f4ccddee09429cb1c63ea56c02894aecf9d69acdcaf006c53835df2549fff"
+    url "https://pypi.python.org/packages/source/P/Pillow/Pillow-3.1.2.tar.gz"
+    sha256 "c593622445503ae1ee361d3a6bb40794e043b43d00c96fcb298ba43ecd375905"
   end
 
   resource "pyasn1" do
@@ -153,13 +153,13 @@ class Mitmproxy < Formula
   end
 
   resource "pyparsing" do
-    url "https://pypi.python.org/packages/source/p/pyparsing/pyparsing-2.1.0.tar.gz"
-    sha256 "f6cb2bc85a491347c3c699db47f7ecc02903959156b4f92669ebf82395982901"
+    url "https://pypi.python.org/packages/source/p/pyparsing/pyparsing-2.1.1.tar.gz"
+    sha256 "9bae5cd4cbee6da0d7d8d9a1647f5253a3b89652e707647eaf1961f4932ae6c6"
   end
 
   resource "pyperclip" do
-    url "https://pypi.python.org/packages/source/p/pyperclip/pyperclip-1.5.26.zip"
-    sha256 "15929bdb305ca54ff090f066de985aa7b21f3d5f0b695edab880f0246559a88b"
+    url "https://pypi.python.org/packages/source/p/pyperclip/pyperclip-1.5.27.zip"
+    sha256 "a3cb6df5d8f1557ca8fc514d94fabf50dc5a97042c90e5ba4f3611864fed3fc5"
   end
 
   resource "PyYAML" do
@@ -213,10 +213,15 @@ class Mitmproxy < Formula
 
     resource("Pillow").stage do
       inreplace "setup.py", "'brew', '--prefix'", "'#{HOMEBREW_PREFIX}/bin/brew', '--prefix'"
-      system "python", *Language::Python.setup_install_args(libexec/"vendor")
+      saved_sdkroot = ENV.delete "SDKROOT"
+      begin
+        system "python", *Language::Python.setup_install_args(libexec/"vendor")
+      ensure
+        ENV["SDKROOT"] = saved_sdkroot
+      end
     end
 
-    res = resources.map { |r| r.name }.to_set - ["Pillow"]
+    res = resources.map(&:name).to_set - ["Pillow"]
 
     res << "PyAMF" if build.with? "pyamf"
     res << "cssutils" if build.with? "cssutils"
