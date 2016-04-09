73a96a255880de7ba6a16d8eecfd79e8914bc09c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 13:34:41 +0100

    libiodbc: update 3.52.10 bottle.

--- Formula/libiodbc.rb
+++ Formula/libiodbc.rb
@@ -6,6 +6,7 @@ class Libiodbc < Formula
 
   bottle do
     cellar :any
+    sha256 "633bee4aad14f908a8b85fcbd8f5be2f0802f24b83fc5373f43111f0a04ef341" => :el_capitan
     sha256 "788c2b7746d436b6d90860f0aeaae96c34e94b890700a6f9e752604cffa1c030" => :yosemite
     sha256 "b47fd8f3e58259fecf6af5e6f7bab7343007b4fc34d04ed0d3885b8c6c4a48fd" => :mavericks
     sha256 "c70f91b15fd9efffb36f3849f8e1f66314fbf6d697ad6821aac8b160b7441b1a" => :mountain_lion
