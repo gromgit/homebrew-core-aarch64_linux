1ee3b5088b08cbd3f99c8c7a404d6d4436b663a8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 13:28:04 +0100

    cclive: add 0.7.16 bottle.

--- Formula/cclive.rb
+++ Formula/cclive.rb
@@ -4,6 +4,13 @@ class Cclive < Formula
   url "https://downloads.sourceforge.net/project/cclive/0.7/cclive-0.7.16.tar.xz"
   sha256 "586a120faddcfa16f5bb058b5c901f1659336c6fc85a0d3f1538882a44ee10e1"
 
+  bottle do
+    cellar :any
+    sha256 "ee97b1e9aed8607c31dc6e9689bb5230e0d114d993ff06cbfca55b3c5c3ea448" => :el_capitan
+    sha256 "118ddf82fb54c2dc14e664328499faea71d5b516166244a6a179037cce16b8bc" => :yosemite
+    sha256 "74168fac20d931d1b6e2c702ac5818188288271100c9e3f15245b63a1e27d9bc" => :mavericks
+  end
+
   conflicts_with "clozure-cl", :because => "both install a ccl binary"
 
   depends_on "pkg-config" => :build
