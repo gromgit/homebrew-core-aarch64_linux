dbee57077638a9b32573bb0194522d9a4aecb9ff
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 19:18:29 +0100

    lcdproc: add 0.5.6 bottle.

--- Formula/lcdproc.rb
+++ Formula/lcdproc.rb
@@ -4,6 +4,12 @@ class Lcdproc < Formula
   url "https://downloads.sourceforge.net/project/lcdproc/lcdproc/0.5.6/lcdproc-0.5.6.tar.gz"
   sha256 "bd2f43c30ff43b30f43110abe6b4a5bc8e0267cb9f57fa97cc5e5ef9488b984a"
 
+  bottle do
+    sha256 "2a7bd5effc794f54e2364b74caba344f949a288b4ae67e55e829e6256a585732" => :el_capitan
+    sha256 "7ed7e364837c7f0c3702d522d3dd8f73de0bd06fed5bd27c9010975042337ca7" => :yosemite
+    sha256 "2a1f1649eeab5698d37280c86e8d7ec935a99a48e3d844d9f366beea431e5175" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libusb"
   depends_on "libhid"
