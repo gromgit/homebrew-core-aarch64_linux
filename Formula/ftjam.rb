0df67671d2c63c7ae6b2fc22909b0786877172d2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 16:19:45 +0100

    ftjam: add 2.5.2 bottle.

--- Formula/ftjam.rb
+++ Formula/ftjam.rb
@@ -4,6 +4,13 @@ class Ftjam < Formula
   url "https://downloads.sourceforge.net/project/freetype/ftjam/2.5.2/ftjam-2.5.2.tar.bz2"
   sha256 "e89773500a92912de918e9febffabe4b6bce79d69af194435f4e032b8a6d66a3"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f94287203827dea6ac5031e695c217a48b1b69e939dcd68a489c8477b4100447" => :el_capitan
+    sha256 "95490ead99e537713d8c26d1c1bea72b31ea06153a405867ffe83c044593caa0" => :yosemite
+    sha256 "554e527a1e52be6ebd9f5f1fbae4e8f648f750a179936e329238dee32b32520a" => :mavericks
+  end
+
   conflicts_with "jam", :because => "both install a `jam` binary"
 
   def install
