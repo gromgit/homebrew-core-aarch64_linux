f4c320d7013d9508a97d3a26f980fe1faf75044b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 14:46:15 +0100

    cmu-sphinxbase: add 0.8 bottle.

--- Formula/cmu-sphinxbase.rb
+++ Formula/cmu-sphinxbase.rb
@@ -4,6 +4,13 @@ class CmuSphinxbase < Formula
   url "https://downloads.sourceforge.net/project/cmusphinx/sphinxbase/0.8/sphinxbase-0.8.tar.gz"
   sha256 "55708944872bab1015b8ae07b379bf463764f469163a8fd114cbb16c5e486ca8"
 
+  bottle do
+    cellar :any
+    sha256 "3f9d274b9d80b236ca2b567ce6fde2d98daf2ce61363c64b40b8d0f660835164" => :el_capitan
+    sha256 "488fe47cae524867c5a6fc49e4589ae480bb1c162a3bca1442e0a51035e2637b" => :yosemite
+    sha256 "b13d896f6888b96d6178980df2ef8eb0cfa4c994e38a9b7fb29cb69faca96f21" => :mavericks
+  end
+
   head do
     url "https://github.com/cmusphinx/sphinxbase.git"
 
