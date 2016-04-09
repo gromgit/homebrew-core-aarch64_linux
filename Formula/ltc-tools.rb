ae12671ff020c3185e63b50a74408829c8c86c0e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 19:40:07 +0100

    ltc-tools: add 0.6.4 bottle.

--- Formula/ltc-tools.rb
+++ Formula/ltc-tools.rb
@@ -5,6 +5,13 @@ class LtcTools < Formula
   sha256 "8fc9621df6f43ab24c65752a9fee67bee6625027c19c088e5498d2ea038a22ec"
   head "https://github.com/x42/ltc-tools.git"
 
+  bottle do
+    cellar :any
+    sha256 "b3b9a4d9f9969a81be3aa7a51b35a8c1f61a0c7b839fe3778a9665996faa4925" => :el_capitan
+    sha256 "e3bb3cca684ddf01e1d6e8f1830609a3de22a8cc654d29db959dccd211766489" => :yosemite
+    sha256 "12b8773c9045c5b3e8c5d57f085e02ee078f4807f8fc0b868c5d2eee99b5ea49" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "help2man" => :build
   depends_on "libltc"
