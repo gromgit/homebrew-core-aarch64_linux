d6f80ff19c99624bb0f4e8a021357e565c562946
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 17:20:26 +0100

    gtk-murrine-engine: add 0.98.2_1 bottle.

--- Formula/gtk-murrine-engine.rb
+++ Formula/gtk-murrine-engine.rb
@@ -5,6 +5,13 @@ class GtkMurrineEngine < Formula
   sha256 "e9c68ae001b9130d0f9d1b311e8121a94e5c134b82553ba03971088e57d12c89"
   revision 1
 
+  bottle do
+    cellar :any
+    sha256 "552525ea70d460775ffd1c73d34f6b7606b2ef6b34f58ef23b12d90659bbee8e" => :el_capitan
+    sha256 "8f4b630aac7727177cb36f323348dfe1a77c6cff64367ba881b94e05403b0bda" => :yosemite
+    sha256 "c04e691bfeca04d420f3f64990cd0b1d754bd87061757960bd6eda4e1b55c6c3" => :mavericks
+  end
+
   depends_on "intltool" => :build
   depends_on "pkg-config" => :build
   depends_on "gtk+"
