eba46bda272ac90f4814742c061d96ea52d86e8f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 19:40:07 +0100

    lwtools: add 4.9 bottle.

--- Formula/lwtools.rb
+++ Formula/lwtools.rb
@@ -4,6 +4,13 @@ class Lwtools < Formula
   url "http://lwtools.projects.l-w.ca/releases/lwtools/lwtools-4.9.tar.gz"
   sha256 "e0c96e4f1e10ef00a1c5b1f55ccba8f5924d826ac89837bef96382a474ccf975"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6f7c8a5b1ba86e231bc42ed6d65ea3cf5534161c73d403d0a750224695ba2c57" => :el_capitan
+    sha256 "4b4491727bf0ec368a2788332991855b343f13440326c6bc74b870a32d1c9def" => :yosemite
+    sha256 "50063049a2285723daa126a1ea3f10a60c9f729f7b0b82201a55c630076e356d" => :mavericks
+  end
+
   def install
     system "make"
     system "make", "install", "INSTALLDIR=#{bin}"
