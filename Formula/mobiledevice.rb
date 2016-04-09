c12b7d337758d8b99a2eef3e8ba5e65a90860a07
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 17:53:20 +0100

    mobiledevice: update 2.0.0 bottle.

--- Formula/mobiledevice.rb
+++ Formula/mobiledevice.rb
@@ -5,7 +5,8 @@ class Mobiledevice < Formula
   sha256 "07b167f6103175c5eba726fd590266bf6461b18244d34ef6d05a51fc4871e424"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "27f7b9fa9c4f1e34711c75bef34a06925ca50933987d1f925587a356492f7046" => :el_capitan
     sha256 "110dd69008feb20cbe6343169dfcc278d209e9430d59d44ab0bf6ce7eb920362" => :yosemite
     sha256 "18d5472c4b517413472be3b97ff66217d55690773ef952933e652dc8a57133bf" => :mavericks
     sha256 "19eb775bc12305341abe780c06308cf32f5fd6060227fefa4cd0f2ef28a3dae2" => :mountain_lion
