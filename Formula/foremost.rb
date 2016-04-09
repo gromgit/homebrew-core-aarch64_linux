100dc7391673648647a518ac4b5e5835aeea5528
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 15:48:18 +0100

    foremost: add 1.5.7 bottle.

--- Formula/foremost.rb
+++ Formula/foremost.rb
@@ -4,6 +4,13 @@ class Foremost < Formula
   url "http://foremost.sourceforge.net/pkg/foremost-1.5.7.tar.gz"
   sha256 "502054ef212e3d90b292e99c7f7ac91f89f024720cd5a7e7680c3d1901ef5f34"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "153967284382e4a8206a627ac415c7bf39732279c3e1dc1bf377edcf0f2939b2" => :el_capitan
+    sha256 "7b793ac9f697b1aa9da79a04b7394ce93909149dae5f2817c49998798e50f939" => :yosemite
+    sha256 "9ba1e4692226a7d1941c18b4317758393d806c95a4ec8b8500010f920ff13247" => :mavericks
+  end
+
   def install
     inreplace "Makefile" do |s|
       s.gsub! "/usr/", "#{prefix}/"
