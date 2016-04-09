060b8d1ec56e768e2d26ec94ddad80527f50d47a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 17:20:26 +0100

    grepcidr: add 2.0 bottle.

--- Formula/grepcidr.rb
+++ Formula/grepcidr.rb
@@ -4,6 +4,13 @@ class Grepcidr < Formula
   url "http://www.pc-tools.net/files/unix/grepcidr-2.0.tar.gz"
   sha256 "61886a377dabf98797145c31f6ba95e6837b6786e70c932324b7d6176d50f7fb"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "31ccf6792cab3c5022530ef4576ea53e6dedd4855b939d11212fea0d7fa294dc" => :el_capitan
+    sha256 "d0024b81610b4a698de415aef87958e2a61f74a9f1f2b7acf875f2f3d50ecc05" => :yosemite
+    sha256 "c4ed3ba91b4acd41f51850b143ea9826275b221fc8f041098dfe5f5a429a0289" => :mavericks
+  end
+
   def install
     system "make"
     bin.install "grepcidr"
