3874fb14252bb212f22b5b944a26915c01b1b789
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 14:07:32 +0100

    libnatpmp: update 20130911 bottle.

--- Formula/libnatpmp.rb
+++ Formula/libnatpmp.rb
@@ -7,6 +7,7 @@ class Libnatpmp < Formula
   bottle do
     cellar :any
     revision 1
+    sha256 "66c86d92e33fe778c8383066070983d44c5d6d8f479d57a6e1e391a52ae21632" => :el_capitan
     sha256 "56614258c625a9f98733b0fe0c451cf62725fd874b413f08e45ed93a8fdaa991" => :yosemite
     sha256 "66441cfb63476b8d74f5b90dccd434b081201cc7e900f7b2da4cba949cf40ef8" => :mavericks
     sha256 "e653484cb16a9732132635cf784bd7096d647277c017318316d8a442b559ca2a" => :mountain_lion
