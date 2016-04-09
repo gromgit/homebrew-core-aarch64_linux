c2340e7e43396e28fe63e9ba2b951da32adbe37e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 14:19:44 +0100

    chruby: add 0.3.9 bottle.

--- Formula/chruby.rb
+++ Formula/chruby.rb
@@ -6,6 +6,13 @@ class Chruby < Formula
 
   head "https://github.com/postmodern/chruby.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ff70dff83817f093d39384a40d3dfb2aaccc1cbe475d58383d4ef157085f2c64" => :el_capitan
+    sha256 "eb14810c552b693c5ae82a577be81398e7dfeadc5489666bb0ff89581f09bfe4" => :yosemite
+    sha256 "c7ede5a22e512d3c22406f222b539fe05b78dfb9721cfff8ce94ed0357883ba5" => :mavericks
+  end
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
