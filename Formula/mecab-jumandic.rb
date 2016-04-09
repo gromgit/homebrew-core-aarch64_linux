79443819eb56de799e6e46badcd65fb52ec03417
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 15:36:48 +0100

    mecab-jumandic: update 5.1-20070304 bottle.

--- Formula/mecab-jumandic.rb
+++ Formula/mecab-jumandic.rb
@@ -5,7 +5,8 @@ class MecabJumandic < Formula
   sha256 "042614dcc04afc68f1cfa2a32f353dc31b06f0674ebab3bfa8e67472709fe657"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "ab65000550be226e608e09e00672d4fc476300da138d68143af75389f8e58231" => :el_capitan
     sha256 "5a9dbc1f67ff36d1f79d6391de2c1d5ab78777322c6e0e829ba4b7a6ab3c88ad" => :yosemite
     sha256 "2d822346c3f44e341ae7b82574dedc1cca41e24e48c45d86fc5f382e26792661" => :mavericks
     sha256 "b61b60c146c415bde98046beecbbf1e7f64c12f072ad2610de6876b93d13a3b0" => :mountain_lion
