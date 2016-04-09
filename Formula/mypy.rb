cdbee742da03f221ace3279be35bc9366d0b48c3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 18:20:03 +0100

    mypy: update 0.2.0_1 bottle.

--- Formula/mypy.rb
+++ Formula/mypy.rb
@@ -8,6 +8,7 @@ class Mypy < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "596670f2387beba24f8b0a7d51dd7ad0f9d10599b1ac44b65dcca1785659b9b6" => :el_capitan
     sha256 "cc2780459f9cbb98805a0873ca7c5e6286bfad54545ba8d6898c663658867fea" => :yosemite
     sha256 "2c5872aed4f6c1fe965bb6962f448e47d412930bc270e4fbfc50427a094b3a23" => :mavericks
     sha256 "6a67aefdf446d5600f06aa0f7e9de8c79a7165cb73edfbd6e2dd73aba072ff73" => :mountain_lion
