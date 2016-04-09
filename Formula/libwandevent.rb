130a05640fd8243a28447d135349bca69c35a004
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 14:43:21 +0100

    libwandevent: update 3.0.1 bottle.

--- Formula/libwandevent.rb
+++ Formula/libwandevent.rb
@@ -6,6 +6,7 @@ class Libwandevent < Formula
 
   bottle do
     cellar :any
+    sha256 "78343d11a98d223c2e944971516c8c5213c2e88a5484c1a104be57b8e1b228e1" => :el_capitan
     sha256 "e6a07c41e838e86d5a1c39274d5d15e37d55dd7439f19a05a50818fd78f437e5" => :yosemite
     sha256 "5565d710cbaeb8110f2f50289965dc0cfc76d20ddb96138ddbbadd452b13eefc" => :mavericks
     sha256 "63c6840e4140921671922f960b63a48565fb0204ae2646d3355bf20f6e5c3958" => :mountain_lion
