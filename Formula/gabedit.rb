62fff610d6a7a47a118f81bf2a0d44c11e3c2b97
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 16:19:45 +0100

    gabedit: add 2.4.8_1 bottle.

--- Formula/gabedit.rb
+++ Formula/gabedit.rb
@@ -6,6 +6,13 @@ class Gabedit < Formula
   sha256 "38d6437a18280387b46fd136f2201a73b33e45abde13fa802c64806b6b64e4d3"
   revision 1
 
+  bottle do
+    cellar :any
+    sha256 "e8f13bf6bf5744f3439604b74c1313817dbf148b63e039e54089c10d65a818ee" => :el_capitan
+    sha256 "86ef29773a913efbce0b04aef8e6abfe8c4849b02840f411f418e939da479c45" => :yosemite
+    sha256 "507caa30aad09047605ca4df857b98e2c2172145d9382e8ca156fd63eeec334a" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "gtk+"
   depends_on "gtkglext"
