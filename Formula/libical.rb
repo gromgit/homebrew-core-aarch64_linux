1a99c4c37cc3367aea56ad633bb36071fb730d93
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 13:34:40 +0100

    libical: update 1.0.1 bottle.

--- Formula/libical.rb
+++ Formula/libical.rb
@@ -5,6 +5,7 @@ class Libical < Formula
   sha256 "089ce3c42d97fbd7a5d4b3c70adbdd82115dd306349c1f5c46a8fb3f8c949592"
 
   bottle do
+    sha256 "6ae0580e27fa630e88b8b460b82e3c995638e87d312c4e836425dbc321ae1bcb" => :el_capitan
     sha256 "eaef148f778ac575b2e1c3223cd26c8865d3ae86e274b1cab7bcf9b575086c38" => :yosemite
     sha256 "51288631d0f0656fdbe6d30eb333699e0f1c48a0b1defe72a9a7a3eeb0571a92" => :mavericks
     sha256 "014c0160a5bc9030409e6459799e4b9ae3474ea86d4ca1557b9a3d5d89e31232" => :mountain_lion
