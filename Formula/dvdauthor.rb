be93c09d8f2c4e97d82c99d052e5b6c9a8af0659
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 15:10:05 +0100

    dvdauthor: add 0.7.1_1 bottle.

--- Formula/dvdauthor.rb
+++ Formula/dvdauthor.rb
@@ -5,6 +5,12 @@ class Dvdauthor < Formula
   sha256 "501fb11b09c6eb9c5a229dcb400bd81e408cc78d34eab6749970685023c51fe9"
   revision 1
 
+  bottle do
+    cellar :any
+    sha256 "f295c54bd810948d3acf3223d7132c036acd9842a4c7fc449ac111f5e6971d9f" => :el_capitan
+    sha256 "e91dd3ffd68dd00a08d6c2f72e395dbd95c83a2057ebe19bf0310d1abba4fa04" => :mavericks
+  end
+
   # Dvdauthor will optionally detect ImageMagick or GraphicsMagick, too.
   # But we don't add either as deps because they are big.
 
