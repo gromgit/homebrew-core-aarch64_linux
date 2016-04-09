aa6ee90c07d5d4dc584bb928625d2f1cca553880
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 21:43:06 +0100

    jsmin: add 2013-03-29 bottle.

--- Formula/jsmin.rb
+++ Formula/jsmin.rb
@@ -5,6 +5,13 @@ class Jsmin < Formula
   version "2013-03-29"
   sha256 "aae127bf7291a7b2592f36599e5ed6c6423eac7abe0cd5992f82d6d46fe9ed2d"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7672c92faa52fbc0684808da9803ebfa8883df0e0243e63a9a0b7c6441218b85" => :el_capitan
+    sha256 "92ce35c390c8a2723e7b7cef8655e61ab9373f274c719ab4c04256cab1c42d1d" => :yosemite
+    sha256 "248da380666e6e08f25b75588c32d1dcad3952978e31d2a08c59c99756946bb4" => :mavericks
+  end
+
   def install
     system ENV.cc, "jsmin.c", "-o", "jsmin"
     bin.install "jsmin"
