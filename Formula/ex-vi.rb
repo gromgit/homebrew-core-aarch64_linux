e74cf81cc6056a7f02b31298206f014afc461e81
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 15:48:17 +0100

    ex-vi: add 050325 bottle.

--- Formula/ex-vi.rb
+++ Formula/ex-vi.rb
@@ -4,6 +4,12 @@ class ExVi < Formula
   url "https://downloads.sourceforge.net/project/ex-vi/ex-vi/050325/ex-050325.tar.bz2"
   sha256 "da4be7cf67e94572463b19e56850aa36dc4e39eb0d933d3688fe8574bb632409"
 
+  bottle do
+    sha256 "e3f68edff7a526463ae6a217b9292c2a6025489df848372abe777da141be14ef" => :el_capitan
+    sha256 "6e3195cd61b05a482e13162a5559ca90d65b1805b8559c006ffc960b56cbe935" => :yosemite
+    sha256 "e0ff9cadf9bc20d222ebc0fceada33db977a5b044346e7930e8349a46e8e6915" => :mavericks
+  end
+
   conflicts_with "vim",
     :because => "ex-vi and vim both install bin/ex and bin/view"
 
