973af9e7203d85100989384b35c39797f6970e3e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 11:28:51 +0100

    idcomments: update 0.18 bottle.

--- Formula/idcomments.rb
+++ Formula/idcomments.rb
@@ -5,7 +5,8 @@ class Idcomments < Formula
   sha256 "a75774495e1b9b799326f3c6875db927835546b82762d565efe17e3cbfa6c7b8"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "047ec15bd8811f6714ec8725ec9964abb3bb59489b7d2452d05fd4844c6ca4c2" => :el_capitan
     sha256 "d88751b05061809da43a70cb82c517dd82daae2d094d8d349e42f81a672c2667" => :yosemite
     sha256 "08f39fa9183cbae296859a932539f5b1d90a1e68ee0c2b59f1f22809c78fd78d" => :mavericks
     sha256 "f803cad790a7224fc20b2ba843683bb4687af58c21a08c96f23b0710c982af31" => :mountain_lion
