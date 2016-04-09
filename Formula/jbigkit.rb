1f954ae03110c53a7e073e67d623e8d424a672be
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 11:52:45 +0100

    jbigkit: update 2.1 bottle.

--- Formula/jbigkit.rb
+++ Formula/jbigkit.rb
@@ -6,7 +6,8 @@ class Jbigkit < Formula
   sha256 "de7106b6bfaf495d6865c7dd7ac6ca1381bd12e0d81405ea81e7f2167263d932"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "bdec08cd92dd59183b698c6bbd9072881fdfce64b4ecb6182e405e0f2ad26c00" => :el_capitan
     sha256 "764396342e87b84253aa06f5046f90c778cacca998ce970900cb2fdf1cfdc3fa" => :yosemite
     sha256 "0ce925915b984307d2e679622138143c5cc5baf832b0a16003fa1e6111a5df9f" => :mavericks
     sha256 "0afb6297101bc3269f0ebca1590cda66a62cbd90e3fdbec38dc011131711d32b" => :mountain_lion
