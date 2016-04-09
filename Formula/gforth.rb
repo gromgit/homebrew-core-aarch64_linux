36d1fab1d0bd2404dd00e8b2281f0187229c0538
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 16:19:46 +0100

    gforth: add 0.7.3 bottle.

--- Formula/gforth.rb
+++ Formula/gforth.rb
@@ -4,6 +4,12 @@ class Gforth < Formula
   url "https://www.complang.tuwien.ac.at/forth/gforth/gforth-0.7.3.tar.gz"
   sha256 "2f62f2233bf022c23d01c920b1556aa13eab168e3236b13352ac5e9f18542bb0"
 
+  bottle do
+    sha256 "942bca40ed6b1c85d4d5a3faf5c2742a48251e5cf36414965140f595ee758d04" => :el_capitan
+    sha256 "20f29be370717f9ce22e224dd4f509c69fc557ff921d622b2525ea7e25bf9f0c" => :yosemite
+    sha256 "d72074880ae4ab11e656645d0d9ab52630640fbb0df713c03fee1a6b8cd84ffa" => :mavericks
+  end
+
   depends_on "libtool" => :run
   depends_on "libffi"
   depends_on "pcre"
