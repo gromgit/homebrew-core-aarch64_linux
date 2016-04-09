627d6d1d95af09ad99dd5ce3cb898273a145771e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 18:20:04 +0100

    netcat: update 0.7.1 bottle.

--- Formula/netcat.rb
+++ Formula/netcat.rb
@@ -5,7 +5,8 @@ class Netcat < Formula
   sha256 "b55af0bbdf5acc02d1eb6ab18da2acd77a400bafd074489003f3df09676332bb"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "a600542d98c534b9790961b1f25c3e23276d1f7483c51efd06523129070a3f0e" => :el_capitan
     sha256 "7ae495a909a2a25f6d8314857009a475034994234186682cc8dace4ea3c40b0c" => :yosemite
     sha256 "fcdf724d792f286ff086a53f214b26eb2ebf61d6430e0f9305bf274b5faf85a5" => :mavericks
     sha256 "d30aa6a393e3b6b28542e1d2a79252cda875e9a0dc817ad6b4920385669a2aaa" => :mountain_lion
