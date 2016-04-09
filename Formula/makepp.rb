5c00d6e8ce6850a8b30f65649c6fdcbe6901bc83
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 20:08:12 +0100

    makepp: add 2.0 bottle.

--- Formula/makepp.rb
+++ Formula/makepp.rb
@@ -4,6 +4,13 @@ class Makepp < Formula
   url "https://downloads.sourceforge.net/project/makepp/2.0/makepp-2.0.tgz"
   sha256 "d1b64c6f259ed50dfe0c66abedeb059e5043fc02ca500b2702863d96cdc15a19"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e2d2e0cbb4999b69e9b1de09a75621ad6119f2978b0a86aefd0e63b2ee908203" => :el_capitan
+    sha256 "9ccedb5776a953719caa8cb8154a8dea1e633fca632eee9ff3ef286e4539f0e8" => :yosemite
+    sha256 "d54e884aac7589f363d2c67920c87861878777a59771cd2457ed86053cf6e6b8" => :mavericks
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
     system "make", "install"
