b0aa2c2a0f2cfd8421412be3aee3da9daf0176f8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 09:58:14 +0100

    aview: add 1.3.0rc1 bottle.

--- Formula/aview.rb
+++ Formula/aview.rb
@@ -4,6 +4,13 @@ class Aview < Formula
   url "https://downloads.sourceforge.net/aa-project/aview-1.3.0rc1.tar.gz"
   sha256 "42d61c4194e8b9b69a881fdde698c83cb27d7eda59e08b300e73aaa34474ec99"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "cb20b8513b3b7d2977943d7ba14f2627892697e9a6b69c4366563786810ca95c" => :el_capitan
+    sha256 "886a6800deefcf7a1e377db57c9df0579b6f1fcb4b491a6262171411bce3517b" => :yosemite
+    sha256 "142a0b64e457e900e395f35d5112bd968e605fa6182bdc9ca77b923a5e5263f6" => :mavericks
+  end
+
   depends_on "aalib"
 
   patch :DATA
