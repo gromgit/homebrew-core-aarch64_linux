f0a2e12312f979f73b0dcb6fbc0cc39913e5528a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 11:21:25 +0100

    id3ed: update 1.10.4 bottle.

--- Formula/id3ed.rb
+++ Formula/id3ed.rb
@@ -5,7 +5,8 @@ class Id3ed < Formula
   sha256 "56f26dfde7b6357c5ad22644c2a379f25fce82a200264b5d4ce62f2468d8431b"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "6448c8e19c8e0874ed5141193c7db06c443ac6c33ab2f6bbe8811098b063c0d1" => :el_capitan
     sha256 "8ca64da5c8c0cbbc7ec64af436fcf3a7ae457c8d8a8073887fc63ec4e89c98b9" => :yosemite
     sha256 "8dd4a14922e94245dd016a266aa23c7bcebb18a56e574c8179df83c2d68ff23c" => :mavericks
     sha256 "95311468f825fbf16c2a00151d01b641db11f6f8de905139845fff0f61e48e91" => :mountain_lion
