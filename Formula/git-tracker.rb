b2b6c16651d9e961825b529fcbd09784e31f2f09
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 16:56:42 +0100

    git-tracker: add 1.6.3 bottle.

--- Formula/git-tracker.rb
+++ Formula/git-tracker.rb
@@ -6,6 +6,13 @@ class GitTracker < Formula
 
   head "https://github.com/stevenharman/git_tracker.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9d04e96e283ae94ea0a789911aeddddd55d53af805313ad6bf8b1d84334daca0" => :el_capitan
+    sha256 "26afe6cdf02f1bda66d2d56518786970c8c1de878e00c09cb045cfbd3379487b" => :yosemite
+    sha256 "132caf19abd7bef677b3d74f38e7fc5d139e90f03b4cb96c3ce8e34ea8cf96ad" => :mavericks
+  end
+
   def install
     rake "standalone:install", "prefix=#{prefix}"
   end
