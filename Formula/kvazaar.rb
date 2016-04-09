d4faf8e3c04abbf44d1ee7a1be2a163bd496e44f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 12:08:37 +0100

    kvazaar: update 0.5.0 bottle.

--- Formula/kvazaar.rb
+++ Formula/kvazaar.rb
@@ -5,7 +5,8 @@ class Kvazaar < Formula
   sha256 "2facdbffcf739171127487cd7d1e48c925560f39755a16542c4a40e65e293070"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "a50a695243ea971c8ab2cb85358f3095128e52be588df7b609cbf22048fb58ef" => :el_capitan
     sha256 "1cf96786d4613d94b93a8091027ea9d14ae6c801d11996def7c4b408316032e6" => :yosemite
     sha256 "aa78c8fa2657ccac25a0c3ac30d7840f281ab8af8a59aafb8058a122090e1b97" => :mavericks
     sha256 "5fc3c49c10479539474539e1bee928f89a3586f1f44e2f346c3b6484ff6395d2" => :mountain_lion
