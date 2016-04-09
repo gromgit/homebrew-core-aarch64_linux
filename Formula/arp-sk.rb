67e75f573190e6f3e9b487bba8ba2b2dc485a265
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 09:29:57 +0100

    arp-sk: add 0.0.16 bottle.

--- Formula/arp-sk.rb
+++ Formula/arp-sk.rb
@@ -4,6 +4,13 @@ class ArpSk < Formula
   url "http://sid.rstack.org/arp-sk/files/arp-sk-0.0.16.tgz"
   sha256 "6e1c98ff5396dd2d1c95a0d8f08f85e51cf05b1ed85ea7b5bcf73c4ca5d301dd"
 
+  bottle do
+    cellar :any
+    sha256 "5d112e8d54329bff104270b7ca27cd4884e48f8c47904bb5838a2e107c035736" => :el_capitan
+    sha256 "11253608a659d16a179c9c7b25050989991a29a68c9c9b4647fe9614e191fcff" => :yosemite
+    sha256 "cea3047a876b12520e9614be8cfcf09348a49522bb5c8bbb7d2c185950e4c08d" => :mavericks
+  end
+
   depends_on "libnet"
 
   def install
