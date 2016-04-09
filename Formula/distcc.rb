35cafb20b3f80288465508ba3ddbeda061353bdd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 20:53:24 +0100

    distcc: add 3.2rc1 bottle.

--- Formula/distcc.rb
+++ Formula/distcc.rb
@@ -13,6 +13,12 @@ class Distcc < Formula
   url "https://distcc.googlecode.com/files/distcc-3.2rc1.tar.gz"
   sha256 "8cf474b9e20f5f3608888c6bff1b5f804a9dfc69ae9704e3d5bdc92f0487760a"
 
+  bottle do
+    sha256 "67cace8962a3046e66c71726e0800b93635e2b51d5ae95d1a4465ec6c93e3a93" => :el_capitan
+    sha256 "e2fb841415d554487d5cd7a9410862c08b99bde86d59e78d007b1eaf73173bdf" => :yosemite
+    sha256 "bf0e1b9468861414802356814229c8c1256abf22fb00bc2819644f4f6e336485" => :mavericks
+  end
+
   depends_on PythonWithoutPPCRequirement
 
   def install
