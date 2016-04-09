08a9b0799a887c15b080ef22f8de3e4226c4be8f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 15:36:48 +0100

    memo: update 1.6 bottle.

--- Formula/memo.rb
+++ Formula/memo.rb
@@ -6,7 +6,8 @@ class Memo < Formula
   head "https://github.com/nrosvall/memo.git"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "28394196d48084daec35822271fe6ca88f462815645022237b3e8b2b13a3ae84" => :el_capitan
     sha256 "841ec3aa9b83f218069c2035c4818fcf3da6ae2c137e94ac446874eeb9719e41" => :yosemite
     sha256 "e771214c2f839a58e329c986461810a89e259739ee24b209c6397191d2c2bb21" => :mavericks
     sha256 "967b7a7f27726310aefac3a109879281f0de0eac010e448deab6ef7670ee7c29" => :mountain_lion
