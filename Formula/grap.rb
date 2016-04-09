cfd87ea1ffdc6994cd2b6691155b0beb4c678d7d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 17:20:26 +0100

    grap: add 1.45 bottle.

--- Formula/grap.rb
+++ Formula/grap.rb
@@ -4,6 +4,12 @@ class Grap < Formula
   url "http://www.lunabase.org/~faber/Vault/software/grap/grap-1.45.tar.gz"
   sha256 "906743cdccd029eee88a4a81718f9d0777149a3dc548672b3ef0ceaaf36a4ae0"
 
+  bottle do
+    sha256 "b9491c4bf9baeb0e9edc3cb0256f0199256b26fcc2fb76ac15153fd6f74c48f9" => :el_capitan
+    sha256 "97c15c60c09da87b12c779fde541bff202f96327e76286a591d52a18b1c74d4e" => :yosemite
+    sha256 "036bb10da8e432b027a2a88714d91e60ba3e297c94b8fe27ab48040e3800760b" => :mavericks
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
