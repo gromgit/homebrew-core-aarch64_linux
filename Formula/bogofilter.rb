d0b62454ad7805f8d5b4b1e652e1fc95faa369f5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 12:57:50 +0100

    bogofilter: add 1.2.4 bottle.

--- Formula/bogofilter.rb
+++ Formula/bogofilter.rb
@@ -4,6 +4,11 @@ class Bogofilter < Formula
   url "https://downloads.sourceforge.net/project/bogofilter/bogofilter-1.2.4/bogofilter-1.2.4.tar.bz2"
   sha256 "e10287a58d135feaea26880ce7d4b9fa2841fb114a2154bf7da8da98aab0a6b4"
 
+  bottle do
+    sha256 "22cdf81e4f0f6bc9b45f5e46ddcca5262141f726f425724ef3b208138fd6d528" => :el_capitan
+    sha256 "b9fd5ae465526223f0ebf07db7fdb5fb25319525d79b087998b14ee7a3fbb1f6" => :yosemite
+  end
+
   depends_on "berkeley-db"
 
   def install
