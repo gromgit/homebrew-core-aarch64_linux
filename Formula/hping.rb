8263b3f87f3e4c049e7f4c0ce35765b584d18c49
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 17:39:02 +0100

    hping: add 3.20051105 bottle.

--- Formula/hping.rb
+++ Formula/hping.rb
@@ -6,6 +6,13 @@ class Hping < Formula
   version "3.20051105"
   sha256 "f5a671a62a11dc8114fa98eade19542ed1c3aa3c832b0e572ca0eb1a5a4faee8"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9644e041cb830ebd121c224cef3727e20c5bf4dcca918fd91135d74e10eb2123" => :el_capitan
+    sha256 "95f5fabc90cdd2e8ca9b10189f69a2057019b35ac1f6cb2e7e4afa7bea1221d5" => :yosemite
+    sha256 "3e8641cad09db531d4ee6b3686748627177d53234ad6b5df92abebf3c1a94ae7" => :mavericks
+  end
+
   patch :DATA
 
   patch :p0 do
