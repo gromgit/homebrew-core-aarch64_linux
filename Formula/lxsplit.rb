631885a7b4c9fbcedda6cf109382fff3d87452f5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 19:40:07 +0100

    lxsplit: add 0.2.4 bottle.

--- Formula/lxsplit.rb
+++ Formula/lxsplit.rb
@@ -4,6 +4,13 @@ class Lxsplit < Formula
   url "https://downloads.sourceforge.net/lxsplit/lxsplit-0.2.4.tar.gz"
   sha256 "858fa939803b2eba97ccc5ec57011c4f4b613ff299abbdc51e2f921016845056"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "25699d54183a01f446015fb02521a50b3967ef2d250e56bb1fe3fd0a5aaec2e1" => :el_capitan
+    sha256 "d7d8d9eb204599056a4e109c63114c90e3be797d2be700e114cc3675d8eba0bb" => :yosemite
+    sha256 "da0ee88012d21dc120c7247fd856305fe14a213e38babcd39ab652af06483b7e" => :mavericks
+  end
+
   def install
     bin.mkpath
     inreplace "Makefile", "/usr/local/bin", bin
