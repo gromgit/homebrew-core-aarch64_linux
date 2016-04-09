13e1aeb71d94112f730c47545d40e5f8a9a68f5d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 14:22:58 +0100

    libquantum: update 1.0.0 bottle.

--- Formula/libquantum.rb
+++ Formula/libquantum.rb
@@ -5,7 +5,8 @@ class Libquantum < Formula
   sha256 "b0f1a5ec9768457ac9835bd52c3017d279ac99cc0dffe6ce2adf8ac762997b2c"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "14c3e392521c20d45a993639de7f561bc0d2b2718158636074bac6f0a2d41581" => :el_capitan
     sha256 "ce31c2a7df81599bc4930ad4aef206f22e006db41d32d05ef1f2f3e72ff6d29d" => :yosemite
     sha256 "2347b6f64ac6a2463cded1679de4390f5bda4b07a74f304efd4ea3bc536af3df" => :mavericks
     sha256 "a7df989c22406155638d24ac755e851208d4cb7f72a8f1f17a985d172270006f" => :mountain_lion
