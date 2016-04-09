2d0609f9e3decd72a0f3505e7119f5a3155a7a70
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 21:43:06 +0100

    jam: add 2.6 bottle.

--- Formula/jam.rb
+++ Formula/jam.rb
@@ -4,6 +4,13 @@ class Jam < Formula
   url "https://swarm.workshop.perforce.com/projects/perforce_software-jam/download/main/jam-2.6.zip"
   sha256 "7c510be24dc9d0912886c4364dc17a013e042408386f6b937e30bd9928d5223c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d93effb9978322bc47d7d204e435ce7da90dd577a43b9086d6868a3118c2fb29" => :el_capitan
+    sha256 "9f8bffedce727f07a14ff7a9453bf66884dce87d463de464fe2c40e30f127c60" => :yosemite
+    sha256 "83ef7ba772948a5e06481cd0a32c54f09139d2693d880223b72ee27deb4d7e37" => :mavericks
+  end
+
   conflicts_with "ftjam", :because => "both install a `jam` binary"
 
   def install
