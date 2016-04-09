94d1e584bfc2c35855441b114b0f6624784a3931
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 12:08:36 +0100

    kanif: update 1.2.2 bottle.

--- Formula/kanif.rb
+++ Formula/kanif.rb
@@ -5,7 +5,8 @@ class Kanif < Formula
   sha256 "3f0c549428dfe88457c1db293cfac2a22b203f872904c3abf372651ac12e5879"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "ebe67d82aa8745a8ff29a82b9520e0fb4892a9645e17a12e8aeae4fda96cc8b4" => :el_capitan
     sha256 "7fc9517feb23867afe77730cdf28bb386570ccb5368d3b0c6b396214abf07e69" => :yosemite
     sha256 "3c003b7621fdef7d8451c3bab928e5b7bb0db0ff1fbe2c5d83b160a021ef93e6" => :mavericks
     sha256 "86f8813209c51d84889c8d5a3d024137a1bdb6d9e95a68042ab875ee00e396bf" => :mountain_lion
