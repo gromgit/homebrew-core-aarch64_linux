462e0e02e723168c4369668e210cfe324698cf57
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 11:52:45 +0100

    jed: update 0.99-19 bottle.

--- Formula/jed.rb
+++ Formula/jed.rb
@@ -6,6 +6,7 @@ class Jed < Formula
   sha256 "5eed5fede7a95f18b33b7b32cb71be9d509c6babc1483dd5c58b1a169f2bdf52"
 
   bottle do
+    sha256 "a437b8f62da67b6c5177ee34df0a0f0906e94336454ec81f915cbbe90536cb07" => :el_capitan
     sha256 "3b316c792feabf9622a70a8ccdf2d2e985e7f991dbcd49a104b2ee6b8ea078cb" => :yosemite
     sha256 "f0e02951e534d96baad147970e51b2f5c09155ad0e51114cfc72b3f49301dff3" => :mavericks
     sha256 "595aff2e43b8ec8c31626831dcab83ffe4ffc129ea85efa61bc2e35877137a29" => :mountain_lion
