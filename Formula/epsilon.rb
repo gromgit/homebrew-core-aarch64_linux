7e4ec9d83d9effdc44873dc198e1f92584eb53e6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 15:27:59 +0100

    epsilon: add 0.9.2 bottle.

--- Formula/epsilon.rb
+++ Formula/epsilon.rb
@@ -4,6 +4,13 @@ class Epsilon < Formula
   url "https://downloads.sourceforge.net/project/epsilon-project/epsilon/0.9.2/epsilon-0.9.2.tar.gz"
   sha256 "5421a15969d4d7af0ac0a11d519ba8d1d2147dc28d8c062bf0c52f3a0d4c54c4"
 
+  bottle do
+    cellar :any
+    sha256 "10da057d558d4d9df8c503135b53f7c23778258f7c66d3b39229bf70d9a887f1" => :el_capitan
+    sha256 "2811209670e68ab4316e6d177bc1376cb28462cad9b696b4a782deff4074a9ce" => :yosemite
+    sha256 "606cb80be8eebf2df61c726ac3e1d91abda3bc49d3c73ca5d07f85b59734069e" => :mavericks
+  end
+
   depends_on "popt"
 
   def install
