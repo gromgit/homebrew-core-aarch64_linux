41f3eed300885fc1f39551e4e089ccfe7c3a86e0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 17:58:33 +0100

    icon-naming-utils: add 0.8.90 bottle.

--- Formula/icon-naming-utils.rb
+++ Formula/icon-naming-utils.rb
@@ -4,6 +4,13 @@ class IconNamingUtils < Formula
   url "http://tango.freedesktop.org/releases/icon-naming-utils-0.8.90.tar.gz"
   sha256 "044ab2199ed8c6a55ce36fd4fcd8b8021a5e21f5bab028c0a7cdcf52a5902e1c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f8a29d74289a555ba7969b8d8f6984de7251393d7d0270e61abf69d36f270fc0" => :el_capitan
+    sha256 "22a80ba1d9042481251c8b85aa46dc163abfd9a54d6815ab43a3a5124592be5f" => :yosemite
+    sha256 "1e2f32f1e755df8a974482ad56c0cc85f7d537005419b0c0c42013f0f0ab717d" => :mavericks
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
