9815dd9b295906cd32a7fbfd58ca66214e8ff6ed
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 09:16:44 +0100

    ahcpd: add 0.53 bottle.

--- Formula/ahcpd.rb
+++ Formula/ahcpd.rb
@@ -4,6 +4,13 @@ class Ahcpd < Formula
   url "http://www.pps.univ-paris-diderot.fr/~jch/software/files/ahcpd-0.53.tar.gz"
   sha256 "a4622e817d2b2a9b878653f085585bd57f3838cc546cca6028d3b73ffcac0d52"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b37143ee365a4a3afd9623d5f49eab0bc4bdf9ac3662d22db9671cffa1078224" => :el_capitan
+    sha256 "36907bc1aadc9d9d874ebd74624d8c2c2e8b4057181df1e964720a41f72ccae8" => :yosemite
+    sha256 "8518f82187d2b8d2bc24648bd072f19073e159abb2bdaf5418ad31e3ab966d0b" => :mavericks
+  end
+
   patch :DATA
 
   def install
