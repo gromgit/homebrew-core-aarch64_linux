19b45a562ca2cab4652eb574dbafc796947f3fc2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 14:19:43 +0100

    cflow: add 1.4 bottle.

--- Formula/cflow.rb
+++ Formula/cflow.rb
@@ -5,6 +5,13 @@ class Cflow < Formula
   mirror "https://ftp.gnu.org/gnu/cflow/cflow-1.4.tar.bz2"
   sha256 "037e39d6048ea91c68a5f3a561e10f22fd085d1f7641643e19c831a94ec26bca"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b3540e10810d3ebde1e83ff1afef0921c185539dcff2d6f9826699c6c41db922" => :el_capitan
+    sha256 "7a6dd05ed5c0e5f976ff5654070b72ccbf60e55c2cd0a54b493576c5971bacaa" => :yosemite
+    sha256 "6eda420db0dc8040e9607efe65e6c508410759a2d04afa7f5420154b38a14058" => :mavericks
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}", "--infodir=#{info}"
