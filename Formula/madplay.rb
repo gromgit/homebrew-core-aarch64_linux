eb8bdb3b5347f9583a3ab0f22d8e35070966fd56
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 19:40:07 +0100

    madplay: add 0.15.2b bottle.

--- Formula/madplay.rb
+++ Formula/madplay.rb
@@ -4,6 +4,12 @@ class Madplay < Formula
   url "https://downloads.sourceforge.net/project/mad/madplay/0.15.2b/madplay-0.15.2b.tar.gz"
   sha256 "5a79c7516ff7560dffc6a14399a389432bc619c905b13d3b73da22fa65acede0"
 
+  bottle do
+    sha256 "81dbc8781c5da50f7188a4031ed5d500b07c51a7589da6799c6bf3477bb90bf6" => :el_capitan
+    sha256 "4ab0b6303cafe408494e85c38b80a3c44964953995c024d2b65a019bc5608c05" => :yosemite
+    sha256 "2b1967955d83ca172724b119e837457aec0eeaa7ded354c810f3635dafbec057" => :mavericks
+  end
+
   depends_on "mad"
   depends_on "libid3tag"
 
