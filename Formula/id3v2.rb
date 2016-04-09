a6ec89318428629a4116ee35dd59534bbe7fc27f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 17:58:33 +0100

    id3v2: add 0.1.12 bottle.

--- Formula/id3v2.rb
+++ Formula/id3v2.rb
@@ -4,6 +4,13 @@ class Id3v2 < Formula
   url "https://downloads.sourceforge.net/project/id3v2/id3v2/0.1.12/id3v2-0.1.12.tar.gz"
   sha256 "8105fad3189dbb0e4cb381862b4fa18744233c3bbe6def6f81ff64f5101722bf"
 
+  bottle do
+    cellar :any
+    sha256 "941e267b5a214013c8085c7918c0d8c1805c906cacf162191b764d2ae1df265f" => :el_capitan
+    sha256 "cd8dd2f943081a051214bf0eedb3c1431abf2bb060a528058e6b9d4c841995ce" => :yosemite
+    sha256 "129381e13539c589fce554a305f4c0a83763ae01865f8810c4145089969c51db" => :mavericks
+  end
+
   depends_on "id3lib"
 
   def install
