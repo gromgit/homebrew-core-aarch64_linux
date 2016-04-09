22e07ea2176c5d352a1978fab271f464c78c622e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 14:46:15 +0100

    cmuclmtk: add 0.7 bottle.

--- Formula/cmuclmtk.rb
+++ Formula/cmuclmtk.rb
@@ -4,6 +4,13 @@ class Cmuclmtk < Formula
   url "https://downloads.sourceforge.net/project/cmusphinx/cmuclmtk/0.7/cmuclmtk-0.7.tar.gz"
   sha256 "d23e47f00224667c059d69ac942f15dc3d4c3dd40e827318a6213699b7fa2915"
 
+  bottle do
+    cellar :any
+    sha256 "c647327d709c3b4a93d5541f8b340d2726540c9efdcbc53d1124043c8c4989bd" => :el_capitan
+    sha256 "320a3590af1e9a1bee6827eb71e4d91fb283952f178b7f0393406a120046d4ee" => :yosemite
+    sha256 "37703a65f22b994f724e54ebcf19ab8204b6d7a27e17d176af13440f611642a3" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
 
   def install
