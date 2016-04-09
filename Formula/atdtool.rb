d2558c50f9b189caf7374f3bd35e2c193239ad4d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 09:38:14 +0100

    atdtool: add 1.3 bottle.

--- Formula/atdtool.rb
+++ Formula/atdtool.rb
@@ -4,6 +4,13 @@ class Atdtool < Formula
   url "https://github.com/lpenz/atdtool/archive/upstream/1.3.tar.gz"
   sha256 "eb634fd9e8a57d5d5e4d8d2ca0dd9692610aa952e28fdf24909fd678a8f39155"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3c03f9deaae3f420231b6fa5331e3d213dc0d48c8817a7142403d35d54159283" => :el_capitan
+    sha256 "8120a3e4b9e2cbc74c533baa2fee5427cae2ebfbf634a39d75c3e2481159b03b" => :yosemite
+    sha256 "efda2ae7e414ac866210899a3f037e565631d07957c7ed44ffefc306d1944901" => :mavericks
+  end
+
   depends_on "txt2tags" => :build
 
   def install
