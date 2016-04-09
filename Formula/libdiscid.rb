90721cda315fbbf9d760d644dd1e828b863350dd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 12:56:17 +0100

    libdiscid: update 0.6.1 bottle.

--- Formula/libdiscid.rb
+++ Formula/libdiscid.rb
@@ -7,6 +7,7 @@ class Libdiscid < Formula
   bottle do
     cellar :any
     revision 1
+    sha256 "392984622c1f695d018cb6e79f3a6f4a7b66db8fc96c225631477b000a226ba2" => :el_capitan
     sha256 "8fb4093273d7b74311c16574a8fe8979dad11b81e25e4ae261cd429e1078d873" => :yosemite
     sha256 "1a812cec01d686ebceaabf3548715c767158a88e7ae245a9d44d14e2d632eab8" => :mavericks
     sha256 "c6a182d34a50257902bf122b7cd48f4e5bab32dbf03a1d59c7769330d9eec7e5" => :mountain_lion
