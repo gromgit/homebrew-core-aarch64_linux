bd96c71320f472af8fa5434e79eb1d24b71075e4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 20:53:25 +0100

    dmalloc: add 5.5.2 bottle.

--- Formula/dmalloc.rb
+++ Formula/dmalloc.rb
@@ -4,6 +4,13 @@ class Dmalloc < Formula
   url "http://dmalloc.com/releases/dmalloc-5.5.2.tgz"
   sha256 "d3be5c6eec24950cb3bd67dbfbcdf036f1278fae5fd78655ef8cdf9e911e428a"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9e1b5dad96d27fbd31e249de7965963fa2e8028286a5e41ce7ddf4555b344162" => :el_capitan
+    sha256 "9283914d34d43556b6a3779523834a056c78d565efacf4316846b1af90cedaf5" => :yosemite
+    sha256 "182c639d938b8b4fb237f7068bc295debba2539bf500adbb8761dabd99b3fcbf" => :mavericks
+  end
+
   def install
     system "./configure", "--enable-threads", "--prefix=#{prefix}"
     system "make", "install", "installth", "installcxx", "installthcxx"
