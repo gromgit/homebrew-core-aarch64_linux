677d8eae201ffc22374f4b4484b7c45c91d1c939
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 16:19:45 +0100

    gdal-grass: add 1.4.3 bottle.

--- Formula/gdal-grass.rb
+++ Formula/gdal-grass.rb
@@ -4,6 +4,12 @@ class GdalGrass < Formula
   url "http://download.osgeo.org/gdal/gdal-grass-1.4.3.tar.gz"
   sha256 "ea18d1e773e8875aaf3261a6ccd2a5fa22d998f064196399dfe73d991688f1dd"
 
+  bottle do
+    sha256 "54446071761bb8453325566649e56680351121dd7ee7b404d8d22034530188e7" => :el_capitan
+    sha256 "976fffb23b24d5c8da1c382156e855e5a0a13bb7ee49062faa79d3f70dba2915" => :yosemite
+    sha256 "5362ed467fedfaedecd815a1651323ac58dc11ea94f2586334ce4255b566a4ca" => :mavericks
+  end
+
   depends_on "gdal"
   depends_on "grass"
 
