c47c0d349eff4698366e1482a6cc70d9384c2c82
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 13:34:41 +0100

    libiscsi: update 1.15.0 bottle.

--- Formula/libiscsi.rb
+++ Formula/libiscsi.rb
@@ -7,6 +7,7 @@ class Libiscsi < Formula
 
   bottle do
     cellar :any
+    sha256 "c3dfb20ce7cc9e12616844d2f4aff31aef9dfdb831a20c1f0cf963d6b9453e05" => :el_capitan
     sha256 "a65aed3f44c8ed81917e339b4aa02f49ef4a64591885ac357d26fc1e85c42d86" => :yosemite
     sha256 "efa1b77699b63a29d1be9c834085d34a56be6c7fadab6587bb367d5223b71779" => :mavericks
     sha256 "a4ad4be9b3da7495606b18c92a38e5c99871672bbc4ddf651fc437ec00aacb0b" => :mountain_lion
