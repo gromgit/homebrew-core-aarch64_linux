e556ebd851949e2fd7c03676f72ba9e4106f7bd1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 12:00:13 +0100

    bchunk: add 1.2.0 bottle.

--- Formula/bchunk.rb
+++ Formula/bchunk.rb
@@ -4,6 +4,13 @@ class Bchunk < Formula
   url "http://he.fi/bchunk/bchunk-1.2.0.tar.gz"
   sha256 "afdc9d5e38bdd16f0b8b9d9d382b0faee0b1e0494446d686a08b256446f78b5d"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "150759123521a6c5aa18471a6474d248cc69b5f5b4c6284f8081988c95e26353" => :el_capitan
+    sha256 "196d12168c9e570676e5ae905e7a85226b7b37d867b5f850d3d82e6157627750" => :yosemite
+    sha256 "d823c661e0786dbde185a8f7de5f70c2ba2304ece128d4abfa35c0eb2c471477" => :mavericks
+  end
+
   def install
     system "make"
     bin.install "bchunk"
