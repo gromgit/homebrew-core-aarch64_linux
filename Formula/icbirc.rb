e19618824571b1c0600ddfeef249d4e6a5bea261
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 11:21:24 +0100

    icbirc: update 2.0 bottle.

--- Formula/icbirc.rb
+++ Formula/icbirc.rb
@@ -5,7 +5,8 @@ class Icbirc < Formula
   sha256 "7607c7d80fc3939ccb913c9fcc57a63d3552af3615454e406ff0e3737c0ce6bd"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "3d9cf7664d20818b648258da1c52cb397c0324d852eca445d62a8d3f2543fc3c" => :el_capitan
     sha256 "08fa7b7435d429c8c21ff2d7835d0b7341fa3dd7cc5677241d1a897ec47cc883" => :yosemite
     sha256 "d16b5155b2f117cdc442eef09114ecffe180ecaf3c67e3c501374ba6e03a144a" => :mavericks
     sha256 "fec513444c71ecef76d4b5650749f2a18cbfb16dc6cf35894ddd46b0096c248b" => :mountain_lion
