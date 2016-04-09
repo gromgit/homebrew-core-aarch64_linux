f8d3311289de840f223f9451ac0458a143f0cdd2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 15:48:17 +0100

    flake: add 0.11 bottle.

--- Formula/flake.rb
+++ Formula/flake.rb
@@ -4,6 +4,13 @@ class Flake < Formula
   url "https://downloads.sourceforge.net/project/flake-enc/flake/0.11/flake-0.11.tar.bz2"
   sha256 "8dd249888005c2949cb4564f02b6badb34b2a0f408a7ec7ab01e11ceca1b7f19"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e521dae57260b2a71f161f16609530c854ff8ebee4252b0addf3c16b5abc982c" => :el_capitan
+    sha256 "af8e2e83dd3c82e8cc26be91ca64e24571b34185d4f8f007725b54d37f38e2b1" => :yosemite
+    sha256 "caae2cab90e1e392a93d47b88c5c9a9242c3740ca95e767bca61d9d267f337f9" => :mavericks
+  end
+
   def install
     ENV.j1
     system "./configure", "--disable-debug", "--prefix=#{prefix}"
