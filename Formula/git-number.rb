858e5609d3110698382b1671c18329ebed7f9646
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 16:56:42 +0100

    git-number: add 1.0.0a bottle.

--- Formula/git-number.rb
+++ Formula/git-number.rb
@@ -5,6 +5,13 @@ class GitNumber < Formula
   sha256 "710804466860fdb6f985c0a1268887230c4c1ff708584b19e385bbaec872f4f5"
   version "1.0.0a"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e4a73d3be05fb32a693918d9ccd7c13d995abbc29df57a91af8bb572420bc82c" => :el_capitan
+    sha256 "aab690794b8908b84a02c8586831aa13bcdf4129d38fcbd9113e297b6b2c00d4" => :yosemite
+    sha256 "a8775c026b9f5ff48b938c61d382aa972d54ee681a3ae96aa523156ca4242af6" => :mavericks
+  end
+
   def install
     system "make", "test"
     system "make", "prefix=#{prefix}", "install"
