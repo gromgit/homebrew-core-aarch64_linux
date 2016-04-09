9f90c85e7aa334590666ab5d73b1228f23f41ffb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 15:36:48 +0100

    mecab-ipadic: update 2.7.0-20070801 bottle.

--- Formula/mecab-ipadic.rb
+++ Formula/mecab-ipadic.rb
@@ -5,7 +5,8 @@ class MecabIpadic < Formula
   sha256 "b62f527d881c504576baed9c6ef6561554658b175ce6ae0096a60307e49e3523"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "9f0ae0a62141e3b28807349cb7a9560e36770acb869f4a4e7a54ea1a28ef8ba5" => :el_capitan
     sha256 "55703c812de3e7cff503b9cd1eafa0656b3f17c4885165ce4d8e4d2b2356050e" => :yosemite
     sha256 "0a9ea36b7cc03f73ae34f72e078b7e84ebe814cf8e1cfbea2d5f876c1893b1c5" => :mavericks
     sha256 "c70d627c447086a66b6eaca37cd98e1da099f65bfb6b87f2c47d1901d5e4a090" => :mountain_lion
