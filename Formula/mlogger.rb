090b2fc54ac2f3974370ff7264979cb4fbd6ea8c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 17:53:20 +0100

    mlogger: update 1.1.2 bottle.

--- Formula/mlogger.rb
+++ Formula/mlogger.rb
@@ -5,7 +5,8 @@ class Mlogger < Formula
   sha256 "4163687c80b618f3e6c307eeba86cb95097661375cf76c977e8aad1bbc48ee99"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "a91ebd8db66c32179a41a6ba9fd595353c19773922ca5a82537926ca17502064" => :el_capitan
     sha256 "876a19cf8c72d8d14e08989d7ace12faac1cb9dad3b7406f37c9fe6370b98924" => :yosemite
     sha256 "3b0249322e3cd31e38b1e040c2fa4bc1aa6500461e7582f06d7260dcce77187a" => :mavericks
     sha256 "dd639c0cfb0e5715082cf744d50121b6e735065ea2216abf299b989646843f64" => :mountain_lion
