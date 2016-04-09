5178072f6d6f29f7d0aed8a099edfb45fb2c7f65
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 18:20:04 +0100

    netperf: update 2.7.0 bottle.

--- Formula/netperf.rb
+++ Formula/netperf.rb
@@ -5,7 +5,8 @@ class Netperf < Formula
   sha256 "842af17655835c8be7203808c3393e6cb327a8067f3ed1f1053eb78b4e40375a"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "9b3e6bfce1057cc7a3454288ae45684ef1c9805b23f2c4f52afa0fa6376e8057" => :el_capitan
     sha256 "12102c72722ff5a7a337414a41b6f3bc7fa67fd4dc32aadd84693c22907c99e8" => :yosemite
     sha256 "026fe8418ea6be50576850a9b3cde6a5a71a7d93924a1eb6b77cba88675b2a89" => :mavericks
     sha256 "4e0aa192670d1de341e77e0bd3af4b2cccc6a5b12a734e922e92c7b7d50f281d" => :mountain_lion
