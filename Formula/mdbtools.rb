046c3b5144f9fb39e5c9a7a6c90478d2b3fd8131
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 20:08:13 +0100

    mdbtools: add 0.7.1 bottle.

--- Formula/mdbtools.rb
+++ Formula/mdbtools.rb
@@ -4,6 +4,13 @@ class Mdbtools < Formula
   url "https://github.com/brianb/mdbtools/archive/0.7.1.tar.gz"
   sha256 "dcf310dc7b07e7ad2f9f6be16047dc81312cfe1ab1bd94d0fa739c8059af0b16"
 
+  bottle do
+    cellar :any
+    sha256 "ace9fa300968cb2360af0cf376498f4f4d175f02c4cf35e0f35d3d5f62a681fb" => :el_capitan
+    sha256 "6474bb245d7674e68588fc10dac8bf3325a0e3e262f417c8b5deb5151a8fd6e0" => :yosemite
+    sha256 "2df4eb2b58bb483016b86c35691587e168ea610c3381f7a4e7abe89b31507a9f" => :mavericks
+  end
+
   option "with-man-pages", "Build manual pages"
 
   depends_on "pkg-config" => :build
