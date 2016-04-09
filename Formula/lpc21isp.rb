78265c9bc1797dae1b7e89783bfb450e094ee101
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 19:40:07 +0100

    lpc21isp: add 1.97 bottle.

--- Formula/lpc21isp.rb
+++ Formula/lpc21isp.rb
@@ -5,6 +5,13 @@ class Lpc21isp < Formula
   sha256 "9f7d80382e4b70bfa4200233466f29f73a36fea7dc604e32f05b9aa69ef591dc"
   version "1.97"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c12b33d514be2490a3a5bb9d3c1f8468e7e24d13eee0a636a9d067f486af59fc" => :el_capitan
+    sha256 "10deab3f8de3cb88b27ea38344ba7c641b758faf45f9a247f3fca968f6db456b" => :yosemite
+    sha256 "5631ccbccd2bb128a1592399a558cbb837d63eb9b1ac2d8187c34fb42064b226" => :mavericks
+  end
+
   def install
     system "make"
     bin.install ["lpc21isp"]
