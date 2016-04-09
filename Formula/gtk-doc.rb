006c1d57474e5c21aa68e49aaae2fdab27bf25c5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 13:54:48 +0100

    gtk-doc: update 1.24 bottle.

--- Formula/gtk-doc.rb
+++ Formula/gtk-doc.rb
@@ -5,9 +5,11 @@ class GtkDoc < Formula
   sha256 "b420759ea05c760301bada14e428f1b321f5312f44e10a176d6804822dabb58b"
 
   bottle do
-    sha256 "7467edb24beda3fda8082721f96f3198c3165f1fc3c129a01d0f36b3e5518e8f" => :yosemite
-    sha256 "4b19d896364a304180cbf12a9df44fa00192774ab48f2020024eff962c2c2745" => :mavericks
-    sha256 "896f095dec0e72eba5c15ad962167febc56439c7152aa2951f0634acddaef0a7" => :mountain_lion
+    cellar :any_skip_relocation
+    revision 1
+    sha256 "cd47e91b2834ee4b6e6ba33c7ac76897fa1205507fab44728120f783e0ca3178" => :el_capitan
+    sha256 "9fdd9481987ef549e4b28e1bf7c8f85b593e092e39b4162eb260cc48c0b3abea" => :yosemite
+    sha256 "e7bce97e0b25bae469c86eec2d0f22d17987307077ad613bac9d9fc35917a407" => :mavericks
   end
 
   depends_on "pkg-config" => :build
