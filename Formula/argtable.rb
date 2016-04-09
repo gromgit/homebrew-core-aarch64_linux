0b7bf21ca94b13cbe1fb07225c41f1d46cfd013d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 09:25:43 +0100

    argtable: add 2.13 bottle.

--- Formula/argtable.rb
+++ Formula/argtable.rb
@@ -5,6 +5,13 @@ class Argtable < Formula
   version "2.13"
   sha256 "8f77e8a7ced5301af6e22f47302fdbc3b1ff41f2b83c43c77ae5ca041771ddbf"
 
+  bottle do
+    cellar :any
+    sha256 "0a720e738557215bf1b58fa642ec2fc51971da38e98b987862fcd05cc54756f7" => :el_capitan
+    sha256 "9e9d1451712580f090f0078ec7774a0daeb1057be3b1762e3d8465264d969432" => :yosemite
+    sha256 "7081198c76023e34380d35682b7a4274a9faf98d3e3e3fa2a9fa801e0a320a8c" => :mavericks
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
