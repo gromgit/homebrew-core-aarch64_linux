22f95972c07935aff64d52fac3753e391eebac22
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 17:39:01 +0100

    gts: add 0.7.6 bottle.

--- Formula/gts.rb
+++ Formula/gts.rb
@@ -4,6 +4,13 @@ class Gts < Formula
   url "https://downloads.sourceforge.net/project/gts/gts/0.7.6/gts-0.7.6.tar.gz"
   sha256 "059c3e13e3e3b796d775ec9f96abdce8f2b3b5144df8514eda0cc12e13e8b81e"
 
+  bottle do
+    cellar :any
+    sha256 "c958735937a398843e9a2b2cc1a1b9bc5305834de4257f1715a12dcc84b25f75" => :el_capitan
+    sha256 "1c6b31293f1db0384813fc935d0e9649e41fd46440363caf32da363d27328fd7" => :yosemite
+    sha256 "b6e2ce541c5b4b46076843076c6842723a896afa36619cfab8155194795c9817" => :mavericks
+  end
+
   option :universal
 
   depends_on "pkg-config" => :build
