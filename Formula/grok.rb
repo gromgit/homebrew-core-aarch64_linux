64590860a8689a77876a96de8e53e7104a324b4a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 21:17:04 +0100

    grok: add 0.9.2 bottle.

--- Formula/grok.rb
+++ Formula/grok.rb
@@ -5,6 +5,13 @@ class Grok < Formula
   sha256 "40edbdba488ff9145832c7adb04b27630ca2617384fbef2af014d0e5a76ef636"
   head "https://github.com/jordansissel/grok.git"
 
+  bottle do
+    cellar :any
+    sha256 "e78484b8a0633f90615103a6fe0a1f9e3f4b6c05531c5dab29ecab9cea05c0a7" => :el_capitan
+    sha256 "9b8d26342e9e9f7f172e32b25e97afbef5b360e541fbc4beacc6999f23b568f7" => :yosemite
+    sha256 "39852bfe279fdffd8ea8fa6b6f1377c3693de4c2e74e84fbd94f08db8e16bb33" => :mavericks
+  end
+
   depends_on "libevent"
   depends_on "pcre"
   depends_on "tokyo-cabinet"
