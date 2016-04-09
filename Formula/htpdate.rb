9625626a4f0a86334e019e7a67d31f5508256656
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 17:39:02 +0100

    htpdate: add 0.9.1 bottle.

--- Formula/htpdate.rb
+++ Formula/htpdate.rb
@@ -4,6 +4,13 @@ class Htpdate < Formula
   url "http://www.vervest.org/htp/archive/c/htpdate-0.9.1.tar.bz2"
   sha256 "2afd132b00d33cd45eea9445387441174fe9bedf3fdf72c5a19f0051cf5a2446"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "fbdf082a4cbde49e5a9e5ea28f8f9c76e634d1f4fc79397e70e55306947c37b8" => :el_capitan
+    sha256 "56073fc56009dac7f807d436f09732bf4e659d2374bb6a61646dc7f94740daa0" => :yosemite
+    sha256 "973fb72128a9fe5e0c4c1aaaf9671b4ea706bc98ba5f96ecd10a2dae46049e57" => :mavericks
+  end
+
   def install
     system "make", "prefix=#{prefix}",
                    "STRIP=/usr/bin/strip",
