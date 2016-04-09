528b6beabe0387dc4a958523fbb8e03894c269cb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 20:19:51 +0100

    cvsync: add 0.24.19 bottle.

--- Formula/cvsync.rb
+++ Formula/cvsync.rb
@@ -4,6 +4,13 @@ class Cvsync < Formula
   url "http://www.cvsync.org/dist/cvsync-0.24.19.tar.gz"
   sha256 "75d99fc387612cb47141de4d59cb3ba1d2965157230f10015fbaa3a1c3b27560"
 
+  bottle do
+    cellar :any
+    sha256 "065a2cac3278d73129fc7b27b7f35d873f5aa17fff52aa4fe70eb896c211b6a8" => :el_capitan
+    sha256 "778da584a283cdfd5b817b98b936ab3c6ba6609a0bad1caaf64a15f595994208" => :yosemite
+    sha256 "08048bcff7a8953bc5a49b1ffa72e18021b5196eb6443c2defc43e4a632ac882" => :mavericks
+  end
+
   depends_on "openssl"
 
   def install
