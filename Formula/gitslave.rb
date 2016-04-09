ed6696a84bf219aba31963793813ca9773c9d9a0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 16:56:43 +0100

    gitslave: add 2.0.2 bottle.

--- Formula/gitslave.rb
+++ Formula/gitslave.rb
@@ -4,6 +4,13 @@ class Gitslave < Formula
   url "https://downloads.sourceforge.net/project/gitslave/gitslave-2.0.2.tar.gz"
   sha256 "8aa3dcb1b50418cc9cee9bee86bb4b279c1c5a34b7adc846697205057d4826f0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e556bf6f7ddfa3e9f6a9b726d80a35404270c96e36ada122fd16d8946394aaa6" => :el_capitan
+    sha256 "395794a75f26acdf034f4ab1541cd9af327d13309517e2553bbcb1fdb4bb0f85" => :yosemite
+    sha256 "f960d16d68868685850464321b5e4f82be4b85b1e3baa9392f185773818e596f" => :mavericks
+  end
+
   def install
     system "make", "install", "prefix=#{prefix}"
   end
