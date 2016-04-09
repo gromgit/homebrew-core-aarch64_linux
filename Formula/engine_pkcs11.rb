c020e80f12ba4454a14d887387de90f92e23a286
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 15:27:59 +0100

    engine_pkcs11: add 0.1.8 bottle.

--- Formula/engine_pkcs11.rb
+++ Formula/engine_pkcs11.rb
@@ -4,6 +4,13 @@ class EnginePkcs11 < Formula
   url "https://downloads.sourceforge.net/project/opensc/engine_pkcs11/engine_pkcs11-0.1.8.tar.gz"
   sha256 "de7d7e41e7c42deef40c53e10ccc3f88d2c036d6656ecee7e82e8be07b06a2e5"
 
+  bottle do
+    cellar :any
+    sha256 "cd24061981fd0cd78e5c82d92c0ef686e2c8eea8a54e49339bf0606b51566b12" => :el_capitan
+    sha256 "af4873db45f9cec2e7dec281f646addeaac288a6ddf349fd731a8ea2e5809957" => :yosemite
+    sha256 "c8fda4c457231986703ffae42ef0033eb6afd05ed891d7b7ede2c38f14dd1105" => :mavericks
+  end
+
   head do
     url "https://github.com/OpenSC/engine_pkcs11.git"
 
