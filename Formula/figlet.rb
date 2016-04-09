d4e97aaf4aa598e857f7cf8bbfdeb106d2fa5572
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 15:48:17 +0100

    figlet: add 2.2.5 bottle.

--- Formula/figlet.rb
+++ Formula/figlet.rb
@@ -4,6 +4,12 @@ class Figlet < Formula
   url "ftp://ftp.figlet.org/pub/figlet/program/unix/figlet-2.2.5.tar.gz"
   sha256 "bf88c40fd0f077dab2712f54f8d39ac952e4e9f2e1882f1195be9e5e4257417d"
 
+  bottle do
+    sha256 "943067dae95de58518b20334aec401cf5fd24866ff77315c0d7bd8b5d4ab0011" => :el_capitan
+    sha256 "0a1b051fb0143dbfca1da36c83eca8580c215ff155e0dc755a924ce1f53a4b46" => :yosemite
+    sha256 "3d33cf3ee819346dc431c37f07e2051c9f92d222cb35d330a41ca88bd5153e2d" => :mavericks
+  end
+
   resource "contrib" do
     url "ftp://ftp.figlet.org/pub/figlet/fonts/contributed.tar.gz"
     sha256 "2c569e052e638b28e4205023ae717f7b07e05695b728e4c80f4ce700354b18c8"
