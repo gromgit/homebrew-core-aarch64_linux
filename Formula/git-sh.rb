1ef0dd32f909ca2f8109e8d58d269c5285aff7d5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 16:56:42 +0100

    git-sh: add 1.3 bottle.

--- Formula/git-sh.rb
+++ Formula/git-sh.rb
@@ -6,6 +6,13 @@ class GitSh < Formula
 
   head "https://github.com/rtomayko/git-sh.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e30e7836919a5d79712e3fd51a118279b412c44da909053b9b185eb48963323f" => :el_capitan
+    sha256 "cdbc6fc62300722f613f314e2859422edcf938c6807a3039bcf476e02fbe222c" => :yosemite
+    sha256 "1b9aa141f32145516db62304dda799611e1fc35ec57275ee75bf566325a6bfa5" => :mavericks
+  end
+
   def install
     system "make"
     system "make", "install", "PREFIX=#{prefix}"
