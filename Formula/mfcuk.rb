35d6699a2713e61a1d6829914a5791c79025b09f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 20:08:13 +0100

    mfcuk: add 0.3.8 bottle.

--- Formula/mfcuk.rb
+++ Formula/mfcuk.rb
@@ -4,6 +4,13 @@ class Mfcuk < Formula
   url "https://mfcuk.googlecode.com/files/mfcuk-0.3.8.tar.gz"
   sha256 "977595765b4b46e4f47817e9500703aaf5c1bcad39cb02661f862f9d83f13a55"
 
+  bottle do
+    cellar :any
+    sha256 "8b329dbd3feb25bc4f04f40451cf25e832395721a5184eb4ee287366aaa06334" => :el_capitan
+    sha256 "bdf696192e1a660b2fa1ad58498bdce941b1d45c4b51847b95427f41debd4c2d" => :yosemite
+    sha256 "1394e4115a4e65abacc23e81659fd77475d6039ac39979cea7fd335ee5cf09e6" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libnfc"
   depends_on "libusb"
