151d25bafefa79d2a97bc80ec34a529a933e2404
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 14:46:15 +0100

    cocot: add 1.1-20120313 bottle.

--- Formula/cocot.rb
+++ Formula/cocot.rb
@@ -6,6 +6,13 @@ class Cocot < Formula
 
   head "https://github.com/vmi/cocot.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "11efeaa69619030b19cbed51861d2af97d0e359bf3cd7d82b58af56e081911ae" => :el_capitan
+    sha256 "13cef35dc54d9715956e142ee7a4cc2dfae1a50276f111d595057654099db80b" => :yosemite
+    sha256 "307edb7057d0c5601ad9e1d9ad65cb15fd7416304b565deac6929e95f2295c74" => :mavericks
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
