3f85cb1bf826373377772fa3751988ba9b152ae1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 21:17:04 +0100

    esound: add 0.2.41 bottle.

--- Formula/esound.rb
+++ Formula/esound.rb
@@ -4,6 +4,12 @@ class Esound < Formula
   url "https://download.gnome.org/sources/esound/0.2/esound-0.2.41.tar.bz2"
   sha256 "5eb5dd29a64b3462a29a5b20652aba7aa926742cef43577bf0796b787ca34911"
 
+  bottle do
+    sha256 "3716e941f802ccd1abf832350b7afbd39aea8d9c4e6d57712e20ecb894473f77" => :el_capitan
+    sha256 "f280c15eb3bf09e99c99f450a778df21f993dc99a727eca1aeea0815aa065b72" => :yosemite
+    sha256 "95ffbc82437fb77ef235de636c3b8ab8baebbecf3223e39ca084670095fcaee2" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "audiofile"
 
