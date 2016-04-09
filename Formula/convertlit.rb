e219fbb75eefe20c3c753a05235c2a77affa5c8c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-09 14:46:16 +0100

    convertlit: add 1.8 bottle.

--- Formula/convertlit.rb
+++ Formula/convertlit.rb
@@ -5,6 +5,13 @@ class Convertlit < Formula
   version "1.8"
   sha256 "d70a85f5b945104340d56f48ec17bcf544e3bb3c35b1b3d58d230be699e557ba"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "66b05c2c6371f16620c82b31b507413556b511b859644322c65f4ceea4a83a64" => :el_capitan
+    sha256 "024a9fdb4b58a3e04c12ec300facbac636b3510f8726726c4be93c60cf272ab1" => :yosemite
+    sha256 "366ce6afb71223d3f14939c5d4d382a90cf56df7920cb41dca0eeae72e809702" => :mavericks
+  end
+
   depends_on "libtommath"
 
   def install
