9e19936d1782d45cf60e8f2439b4b6cd0a7ea7b7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 09:57:09 +0100

    remake: update 0.91 bottle.

--- Formula/remake.rb
+++ Formula/remake.rb
@@ -5,6 +5,7 @@ class Remake < Formula
   sha256 "02a1c62b47e99376701f8d99b45fffdf44e8512ecf92794fc6bf5d6779900dfb"
 
   bottle do
+    sha256 "6be98927b7043f275c945e57feec44a7ae7d6b7420e2925b2243a9f92673ee68" => :el_capitan
     sha256 "521c4370d42f1ceca81eb9b665a8886162f3c1ee7497ec0a66a9edc15c183502" => :yosemite
     sha256 "260d1ac2aac0f356a2548152d70a0cc4c68817fdc5f59fb505454aca7076a236" => :mavericks
     sha256 "685c98bcbdc8a9c4586802c9d73b895c0ff0a91df2ae3ee78dab57d7cfdba68e" => :mountain_lion
