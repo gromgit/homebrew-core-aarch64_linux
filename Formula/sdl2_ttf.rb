363ded47b5efc697824d22f1f38d0b9738946178
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 06:17:32 +0100

    sdl2_ttf: update 2.0.14 bottle.

--- Formula/sdl2_ttf.rb
+++ Formula/sdl2_ttf.rb
@@ -6,9 +6,9 @@ class Sdl2Ttf < Formula
 
   bottle do
     cellar :any
-    sha256 "cdc88dffd18a0875a1b2bb65082c8b4b457999c42a4f48352c2b40169e47d7a1" => :el_capitan
-    sha256 "d37c41ba21a809c7d60eb8779309fdde625a7e9b81b9fad34d17d2dbc90e6e28" => :yosemite
-    sha256 "bfddff4953dc3761a5e0379b6761bec7cdd6a7c90ea2b8e9e37a00009e586334" => :mavericks
+    sha256 "29e62db1a48f1cd9142c04d4a734298f30c8924b32eaa914a6aaef574d4a6f01" => :el_capitan
+    sha256 "557067e99848b4b8a61c805eeb545c6ec66184b7fc2718dc3dd50bd551b0b324" => :yosemite
+    sha256 "3b2dafa7edea6a2173c9ae17bb6a1cc5137a9004ffc44b6443bc885456adbb1b" => :mavericks
   end
 
   option :universal
