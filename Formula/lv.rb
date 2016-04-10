4952ceaa6275077df6bf951e15af96e65be18d52
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 17:55:16 +0100

    lv: update 4.51 bottle.

--- Formula/lv.rb
+++ Formula/lv.rb
@@ -6,9 +6,10 @@ class Lv < Formula
   sha256 "e1cd2e27109fbdbc6d435f2c3a99c8a6ef2898941f5d2f7bacf0c1ad70158bcf"
 
   bottle do
-    sha256 "f9d97339cfc34f5204cef5209e0a451181a05867e5230741bf09fcda93dfa370" => :mavericks
-    sha256 "e783717408f9810e18770270dc2b6ecd0ab4f5c9f18e84c4f9fa37ec9fe7c6b7" => :mountain_lion
-    sha256 "ffc51473182eb7a48a2e9efdc081a8e12b2ebf346f88239f42147cb5acfe7104" => :lion
+    revision 1
+    sha256 "49ad4ebf6830c1ef3f6899486e711f99bc293d422317f8851f174cf18de2a98f" => :el_capitan
+    sha256 "f31281558dc9da38402a86b2b3c03efb10ab471561bf72dd556c3cd8df23ba14" => :yosemite
+    sha256 "6e1894088a741aba921e77a4935d6ad2d11f06f03a4ff775c45e4256728511a4" => :mavericks
   end
 
   def install
