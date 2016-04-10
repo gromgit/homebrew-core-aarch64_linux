c5347092f4f248f22ea5340e0e8ae2cbbaa6ac8e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 13:28:20 +0100

    vttest: update 20140305 bottle.

--- Formula/vttest.rb
+++ Formula/vttest.rb
@@ -6,7 +6,8 @@ class Vttest < Formula
   sha256 "0168aa34061d4470a68b0dd0781a2a9e2bbfb1493e540c99f615b867a11cbf83"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "e758b4d59d1322b736f247c9ebbabe3c73ad06b324120997c6af784b8a3ab3f7" => :el_capitan
     sha256 "2584fb77678acad877286416b79db38673320ec028e6a6add37b987b150af648" => :yosemite
     sha256 "e2faf045a9a09d49e64f104bf65ca7a8fabf4552a50621bd22206e80ef579844" => :mavericks
     sha256 "acb979d3b90f828d2b504b9f0872ad4eb3c421d30872ad291df91123038433d4" => :mountain_lion
