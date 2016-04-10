e0989c3b2f7e0f881fd370b7f8c936d45a26f148
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 11:24:35 +0100

    scm-manager: update 1.46 bottle.

--- Formula/scm-manager.rb
+++ Formula/scm-manager.rb
@@ -8,7 +8,8 @@ class ScmManager < Formula
   depends_on :java => "1.6+"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "dd719aaa1992d8443e81f529e6c98a81da3187771033146b6c736a876e728335" => :el_capitan
     sha256 "cb4f58a1ec6c26ee09534d8b6d42c28a21fb461b1d7e4a8ac1a4793d0a0fecf9" => :yosemite
     sha256 "142969086cc96c7300bd92b4cc28063a57e9f014aafbcf45360fee5bae0fba1c" => :mavericks
     sha256 "979071c94926cda759b8270d4d51b0e54c9d70ce4b078864d041b9c926093919" => :mountain_lion
