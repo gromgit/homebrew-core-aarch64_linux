392ebd5ca623524e1af5b8b24f1abc8b09b50bed
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 09:24:56 +0100

    quazip: update 0.7.1 bottle.

--- Formula/quazip.rb
+++ Formula/quazip.rb
@@ -6,6 +6,7 @@ class Quazip < Formula
 
   bottle do
     cellar :any
+    sha256 "7e4cdb5d97b70dd67092965b00de2981adbcee229a7b00788cce11ebc7fb72d6" => :el_capitan
     sha256 "d15a12c624d377bd818458635ad078782659f313c223836f1689ed89cca32a63" => :yosemite
     sha256 "d6ea39c00ad991be78e2b6fdd1d69a5c4079fc85ef6dbdbedab7c8becf77d0c7" => :mavericks
     sha256 "a6a988cb89a12f6e7c2d5bd8ebe180f40b18f586f3bd1e09a6b881350daee637" => :mountain_lion
