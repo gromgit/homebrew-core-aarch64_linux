1e2db8de1dc3a6d40e6f4921206592c2d2250cae
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 12:07:45 +0100

    tnef: update 1.4.12 bottle.

--- Formula/tnef.rb
+++ Formula/tnef.rb
@@ -5,7 +5,8 @@ class Tnef < Formula
   sha256 "fefea5d9481555cc150ab799b9b1e957564e7fd2ead99fa19e87258f263f7c37"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "f890ad7f42e1abd4f2b4d50fa438642731c82fe01f237ed8926c28537029a7d3" => :el_capitan
     sha256 "a39a144c679b2ae1dfdde6c3062fd9aefefb9d20c44be0943439a17cd1a81a85" => :yosemite
     sha256 "f908fdb42132f3cc2904accf4cefa3623f461a5a683bc37f93c258b144d0c465" => :mavericks
     sha256 "fc3c6f4a0a89f97da20ffea1b99a153e16fc5d9359f73f3c45ca3776912a68e8" => :mountain_lion
