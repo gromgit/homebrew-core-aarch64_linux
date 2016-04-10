02eb3f0cd7d8e6f2251ab2709379290e16810017
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 11:24:39 +0100

    stklos: update 1.10 bottle.

--- Formula/stklos.rb
+++ Formula/stklos.rb
@@ -5,6 +5,7 @@ class Stklos < Formula
   sha256 "215e6e6ffcf7751be5f0c114f83286f99e556e089ca1b0ee66f54dd1be080de4"
 
   bottle do
+    sha256 "4cded14db644fa6b3efa95af862bcb10a928d16168afb85fc592209bfcd1ba9c" => :el_capitan
     sha256 "a47d7c08a0f93c492e836dbcb825b699d7e725bf98bb49c28f88a8a80b432b37" => :yosemite
     sha256 "487ea0c067d772458cb7fd51cf34335938337832151d401298fe0d7916fe1bd5" => :mavericks
     sha256 "7684bf3baa91152729fcd9909c6a0425f80aff212dd86f0393a98781b188afb4" => :mountain_lion
