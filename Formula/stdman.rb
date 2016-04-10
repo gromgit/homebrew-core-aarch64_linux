0573ab2478768a20cb00d625654f5d5b0ec0144a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 11:24:39 +0100

    stdman: update 0.2 bottle.

--- Formula/stdman.rb
+++ Formula/stdman.rb
@@ -5,7 +5,8 @@ class Stdman < Formula
   sha256 "9591835b0f57f88698d7c46ef645064a4af646644535cf2a052152656d73329a"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "c68756d067719ed712b672ade33586d24588e6806ce2aeaaafa4e6e7697b3032" => :el_capitan
     sha256 "bdafb5c1461c5166c682fa59617bc7d29c8c37eda3da862e5ce213a85fb08813" => :yosemite
     sha256 "a43224e47bc29cd6ddd3f18d53793bec65a44ac8107082bb492cfc49103ff22c" => :mavericks
     sha256 "b00742508c8b2dffe738114e606a887d189a8ed79653531b9f969e2e90a6d3d4" => :mountain_lion
