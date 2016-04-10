1217adb9541aa5c8158de93cace0a50846a447af
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 13:28:22 +0100

    xorriso: update 1.4.0 bottle.

--- Formula/xorriso.rb
+++ Formula/xorriso.rb
@@ -6,7 +6,8 @@ class Xorriso < Formula
   sha256 "0bd1e085015b28c24f57697d6def2fe84517967dc417554c0c3ccf1685ed0e56"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "9166d527cb3b26aadf5a7304e4110e38c30e34e94721c120ea1334205f1d275e" => :el_capitan
     sha256 "99291dcf6826ec15b82c9d32ddae8279244f304661f23e1086f69392ce14f34c" => :yosemite
     sha256 "611ac7bb03593af3216d772b0d4d0b3b5797257dd656b3eb04db3da0f7582f7d" => :mavericks
     sha256 "88442ed4676b021a09bafeef4cd3b40a7e822bb7b4239a1de0518360eefbd5a6" => :mountain_lion
