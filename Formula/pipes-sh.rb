c183fd84b3577baa5f69a8cf889ccd50e3333dbb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 08:35:44 +0100

    pipes-sh: update 1.1.0 bottle.

--- Formula/pipes-sh.rb
+++ Formula/pipes-sh.rb
@@ -6,7 +6,8 @@ class PipesSh < Formula
   head "https://github.com/pipeseroni/pipes.sh.git"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "14ea3381314db998d274e1bfe636ecd8cd3629ad708d28974bed1c144a286469" => :el_capitan
     sha256 "f6c2332220663da0ba0374a88b3a9409e69742c7f967e2fd536b4ce24a49f290" => :yosemite
     sha256 "8ad12a3cbe7eb4c688c596bc1ada47be817a023b4126e60396fff559177135bc" => :mavericks
     sha256 "b48d90407346007abae552a9b8466ec716f6b0ef0547e27ee19fc364005e83d9" => :mountain_lion
