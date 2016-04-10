05a41e99a8cb3b42b0f673ae852c93659dec4125
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 13:28:20 +0100

    w-calc: update 2.5 bottle.

--- Formula/w-calc.rb
+++ Formula/w-calc.rb
@@ -2,6 +2,7 @@ class WCalc < Formula
   url "https://downloads.sourceforge.net/w-calc/wcalc-2.5.tar.bz2"
   bottle do
     cellar :any
+    sha256 "67160a91e50ae33f723ead45c4150750b62b3bd45ec009eb4b493e138d2a908d" => :el_capitan
     sha256 "1737fad1cd9e5beac6f3a06057bd594b4de2c6b9f709544acd4825fae9160632" => :yosemite
     sha256 "14bcdc8bb396d6c3890a7a7719d6619911ffe92e8949278865b256eb5f74682e" => :mavericks
     sha256 "0eb8fb2e15ee8274ec673850eb5004654e3f7c3d3a835597b5809e87420db08f" => :mountain_lion
