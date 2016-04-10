344a6b81386cffd988fca9e3f0e69db7ce4f40e3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 13:28:19 +0100

    unyaffs: update 0.9.6 bottle.

--- Formula/unyaffs.rb
+++ Formula/unyaffs.rb
@@ -8,7 +8,8 @@ class Unyaffs < Formula
   head "https://github.com/ehlers/unyaffs.git"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "e33a63724b42277e86a91f37261892c4bca734f8362307c94dbf1a0f410a59c1" => :el_capitan
     sha256 "484cdbf34b9a81e2e127115ed78b80efc0355efcaba53d6797dcc2c431f4bcf1" => :yosemite
     sha256 "b0b9f241a1d576c3ad54fe16e4e20d2b545622ea8b7b30b668672214ecc03591" => :mavericks
     sha256 "f4bf95fc8f6d31a8ffb94dd01e1152a82abc0301078865372f140d347a4ee623" => :mountain_lion
