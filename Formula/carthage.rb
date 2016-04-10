5ede8ccc5f3e9b954f88c265d2bd6a4f3dada54e
Matt Diephouse <matt@diephouse.com>
2016-04-10 11:16:28 -0400

    carthage 0.16
    Closes homebrew/homebrew-core#189

--- Formula/carthage.rb
+++ Formula/carthage.rb
@@ -1,8 +1,8 @@
 class Carthage < Formula
   desc "Decentralized dependency manager for Cocoa"
   homepage "https://github.com/Carthage/Carthage"
-  url "https://github.com/Carthage/Carthage.git", :tag => "0.15.2",
-                                                  :revision => "f811a275aaa06df53119bbfad42af754fb7b6ff7",
+  url "https://github.com/Carthage/Carthage.git", :tag => "0.16",
+                                                  :revision => "39be4da2159e11dd5375385075139c7107835047",
                                                   :shallow => false
   head "https://github.com/Carthage/Carthage.git", :shallow => false
 
@@ -12,7 +12,7 @@ class Carthage < Formula
     sha256 "7411d6b369e1462f4c9b5da2aa17e9203e5e4168549328b2d8f8f7444ce5aeb4" => :yosemite
   end
 
-  depends_on :xcode => ["7.1", :build]
+  depends_on :xcode => ["7.3", :build]
 
   def install
     system "make", "prefix_install", "PREFIX=#{prefix}"
