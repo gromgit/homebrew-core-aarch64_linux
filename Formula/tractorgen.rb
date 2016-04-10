c02ed346e0d4f922bc766787de1c8f0ed8e8311c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 12:07:45 +0100

    tractorgen: update 0.31.7 bottle.

--- Formula/tractorgen.rb
+++ Formula/tractorgen.rb
@@ -5,7 +5,8 @@ class Tractorgen < Formula
   sha256 "469917e1462c8c3585a328d035ac9f00515725301a682ada1edb3d72a5995a8f"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "ccac503b4577fc81e69d3e778c27c31fad9a1c5fa8627e97f293d87ab1177f8d" => :el_capitan
     sha256 "e50de2fd2d9015873282a62fc7a21f3ef419d527d07eeab3830ace52ec25c3c9" => :yosemite
     sha256 "acbfbe90462924fbc6f2658ca0ee591a122639356ce6ff042b558199b477bf4a" => :mavericks
     sha256 "bcc0f270ae8414659db18f339044a3030beff37c8a4a305c1c544919b2fb0a7e" => :mountain_lion
