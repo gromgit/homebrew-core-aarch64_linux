ea7199e8736aace0dc46acdc97152de095322bf1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 19:48:49 +0100

    readosm: update 1.0.0e bottle.

--- Formula/readosm.rb
+++ Formula/readosm.rb
@@ -5,9 +5,11 @@ class Readosm < Formula
   sha256 "1fd839e05b411db6ba1ca6199bf3334ab9425550a58e129c07ad3c6d39299acf"
 
   bottle do
-    sha256 "4449dcac0910846c27c4c5b3fe68b7aa5505132835f1de07d27551eb7f9becd9" => :yosemite
-    sha256 "ab9b88f09f4debd36b0618700952344f38921937855f14937727cac8c2ff9af8" => :mavericks
-    sha256 "a73c751faf283779d13ae63350248e2207ca399a33ec87a753153415f3f52de3" => :mountain_lion
+    cellar :any
+    revision 1
+    sha256 "039504a7e57854871753465a0a0a99c0ed12a3001c63a8cb8b075df7c9646778" => :el_capitan
+    sha256 "36f2069bbc99e2cdd86749bfb2b6dee52ababbadd1d3b78f150787747e79578f" => :yosemite
+    sha256 "de44f9c6d2e6a8cf7f5e27bcd7e8215c70b219a5d1d289fcb06f79ef9f2db54f" => :mavericks
   end
 
   def install
