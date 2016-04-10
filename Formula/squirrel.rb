de3880c4dcbfe316473c88d96192142550b9e361
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 11:24:38 +0100

    squirrel: update 3.0.7 bottle.

--- Formula/squirrel.rb
+++ Formula/squirrel.rb
@@ -6,7 +6,8 @@ class Squirrel < Formula
   sha256 "c7c2548e2d2d74116303445118e197f585a3a5e6bde06fdfe668c05b1cb43fa2"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "8d8142b159c9452fa98bcd2df2ecc307b8f7c1d8e698866bbfbe10bff225b891" => :el_capitan
     sha256 "25e35b7af1a94e1c7495751dbb7be829774411cd05727c6745c668c5188ef8f2" => :yosemite
     sha256 "f20d4132e3913e8f62f459375ede7765f416f6b9756c935253f06f65b8d4a172" => :mavericks
     sha256 "1d077d71f76fe8b9bbb5ddc70654e5de4fed72b12ecbf35764bdd6e4396cd31e" => :mountain_lion
