cfd0bfa4c486529e76a714ad80ec883d41e7a18f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 12:07:45 +0100

    tsung: update 1.6.0 bottle.

--- Formula/tsung.rb
+++ Formula/tsung.rb
@@ -7,6 +7,7 @@ class Tsung < Formula
   head "https://github.com/processone/tsung.git"
 
   bottle do
+    sha256 "88d485cf8a667207acb6e06415a0f065b747cef4f11d8b047944e40da308cb22" => :el_capitan
     sha256 "a4d0f870f04ae0683f4647123c5c3e94e39fd0bcf97edaa98dd2a4a4aa4dca33" => :yosemite
     sha256 "bea0124e4ec1626cd6f49a1cee650dedcd11602ca820a1764f2862e7b7341288" => :mavericks
     sha256 "1fe31187fd7662216c1a879bb62302a638834220e84becf8a1ab182df45c3d14" => :mountain_lion
