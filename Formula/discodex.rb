c4648f8b84e005d5265f3a6579235ef344944beb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 14:37:02 +0100

    discodex: update 2012-01-10 bottle.

--- Formula/discodex.rb
+++ Formula/discodex.rb
@@ -4,8 +4,9 @@ class Discodex < Formula
   url "https://github.com/discoproject/discodex/archive/fa3fa57aa9fcd9c2bd3b4cd2233dc0d051dafc2b.tar.gz"
   bottle do
     cellar :any_skip_relocation
+    revision 1
     sha256 "db8480576b20c59fbc2d7ff4bd23cd3cad3ac70df746604992f28328ebb3eedf" => :yosemite
-    sha256 "e91346c476a0736873f90dc64b3a23661e1b6807c43cdf57a02b32d951cfeedc" => :mavericks
+    sha256 "0baf75b7d7087763907bbbfc7f23e3985393a4e941b66f0d3595cd22b7fbbb29" => :mavericks
   end
 
   version "2012-01-10" # No tags in the project; using date of last commit as a proxy
