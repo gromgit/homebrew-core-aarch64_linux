76d327c98a0fd1beefed8106c37fd3efae86f939
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 09:04:40 +0100

    pwntools: update 2.2.0_1 bottle.

--- Formula/pwntools.rb
+++ Formula/pwntools.rb
@@ -6,7 +6,8 @@ class Pwntools < Formula
   revision 1
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "f6ef1283746d8cff13876966cb1291a8418732ae185563db440bf518d3707deb" => :el_capitan
     sha256 "c4df287c8c1e517cc9fd5c4af69bf7e033e4caf6ee1d7f8d78cb851ad3be39ea" => :yosemite
     sha256 "635953d990da1bf6d9852a91896ece40985142327a697df39db4762e776773a5" => :mavericks
     sha256 "3f467927423abadcf73f516b31a51c3dfa33daaeecfddc9f02e19432de4d038a" => :mountain_lion
