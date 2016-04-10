b5bd7003cf474fc7974077cad43d76a3d69a04a0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 08:35:44 +0100

    pipebench: update 0.40 bottle.

--- Formula/pipebench.rb
+++ Formula/pipebench.rb
@@ -8,7 +8,8 @@ class Pipebench < Formula
   sha256 "ca764003446222ad9dbd33bbc7d94cdb96fa72608705299b6cc8734cd3562211"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "353cabdaf04a41e2169c1e489cd038f9fbe7f33cfd24a5a0b3068449ccc3446d" => :el_capitan
     sha256 "a999c7ba2978e60d38bdeec63d1f1a8b2667cb0d77d35c4da2e57212a37b85ac" => :yosemite
     sha256 "c5b0805588d8a8d9047d30b9330dd0be0215bfe694ed662cf11fa84595bba85c" => :mavericks
     sha256 "217b9b59e5592718c530e4843cb0dfdc830f9f8fc175766abfd3cade97c66c80" => :mountain_lion
