b48d614d49b80759b6a3a18eb64832ff588e6c63
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 15:17:51 +0100

    gcore: update 1.3 bottle.

--- Formula/gcore.rb
+++ Formula/gcore.rb
@@ -5,10 +5,11 @@ class Gcore < Formula
   sha256 "6b58095c80189bb5848a4178f282102024bbd7b985f9543021a3bf1c1a36aa2a"
 
   bottle do
-    cellar :any
-    sha256 "88cd9f3114b081d8f079e1ca5625c49ca1bedc67cc827a07ce70fb8db78deb81" => :mavericks
-    sha256 "48a90f4a6719d732ea99740c8423f810e615decfb10df124d47bc8322faebbc6" => :mountain_lion
-    sha256 "f7a90a39ed7f20ff96b7b313e9e8cebf6e60f5919ec6fc981cce139e90a7f32c" => :lion
+    cellar :any_skip_relocation
+    revision 1
+    sha256 "6ba5c2d5212f291b6410e9770e4bd8863f11462720c5f92898075d01acf0fb8e" => :el_capitan
+    sha256 "4b098515f445f46942b8b7deab79f03206441d1ba95aaa65ce5da3c9e081a17e" => :yosemite
+    sha256 "6479ee2516b07716c506155dee9e9d6be8484ae5f8ac044945644eb22db49a3b" => :mavericks
   end
 
   def install
