b94844aed0d9be2855fff52f27028d6d4e40ce9b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 07:56:45 +0100

    one-ml: update 0.1_1 bottle.

--- Formula/one-ml.rb
+++ Formula/one-ml.rb
@@ -6,7 +6,8 @@ class OneMl < Formula
   revision 1
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "f494747ba756eef47b6ed68b6886549b531f0e684ba09b43e71a3c1b2736de26" => :el_capitan
     sha256 "cc73f66afbba3987c55fcf9f1f285c8597c81390dcead47a65023e6d44aa6b02" => :yosemite
     sha256 "8697e17bf008608f3f81b8b561ec8ed82e3cdf9b06850a88c213b06f87b2abba" => :mavericks
     sha256 "d9cc9f66611ddf1d255a3090886d84f76ecd90c82fc1b6833a9c736f372dc484" => :mountain_lion
