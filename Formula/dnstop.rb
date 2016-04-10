8752dbc969fb7b53e3abc74da9ee2252140bb288
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 14:37:02 +0100

    dnstop: update 20140915 bottle.

--- Formula/dnstop.rb
+++ Formula/dnstop.rb
@@ -5,10 +5,11 @@ class Dnstop < Formula
   sha256 "b4b03d02005b16e98d923fa79957ea947e3aa6638bb267403102d12290d0c57a"
 
   bottle do
-    cellar :any
-    sha256 "5ce413357565d9ba4279ed10eb22fd83e49e5fcfd46fe350b8d1d21b4d2cce61" => :mavericks
-    sha256 "52cc4b54da7e8b49c9b57aea5db74a39b7b56d9b1ee5660620a061a639ecd861" => :mountain_lion
-    sha256 "9533d246369a9c792cdec3ca29680773b88857d1faa68d1fa1f5437301b01c4b" => :lion
+    cellar :any_skip_relocation
+    revision 1
+    sha256 "1d5b1ad056475ce9a27f40b48cbbf58421e4eb66fd134ac318413de2d025db66" => :el_capitan
+    sha256 "aa3b72d1432e7c13b9b7e0722cde3f7fafef17aff557489662029698929638dc" => :yosemite
+    sha256 "4a57a6144a94b3eb2cce64ec7f8f97447eafd1c0be2c5789593920d045e9a189" => :mavericks
   end
 
   def install
