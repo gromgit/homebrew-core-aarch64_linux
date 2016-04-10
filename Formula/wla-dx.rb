6554b327c7501b417020a41af568fd3c5e451d0d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 13:28:21 +0100

    wla-dx: update 9.6 bottle.

--- Formula/wla-dx.rb
+++ Formula/wla-dx.rb
@@ -6,7 +6,8 @@ class WlaDx < Formula
   head "https://github.com/vhelin/wla-dx.git"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "80c729c545582ba83294b3a4cadd23ef378f4e71daa49220812ab149aa66f49b" => :el_capitan
     sha256 "4c192ebb6538da0313496f6927a52f7a64782dbeedbf454b0004e203ddb2109e" => :yosemite
     sha256 "2b8cf1da40e1ce9fedbbf41277ee44955fa99712d46c2fb8d6140c8452b74445" => :mavericks
     sha256 "a78744385e31896de6b34ae3426c6081ada2f28b997f0c5c15555e10bd03d53e" => :mountain_lion
