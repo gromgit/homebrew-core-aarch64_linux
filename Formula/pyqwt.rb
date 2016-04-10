77316e403563feb6158088652e7828c86ec293c4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 09:04:40 +0100

    pyqwt: update 5.2.0 bottle.

--- Formula/pyqwt.rb
+++ Formula/pyqwt.rb
@@ -6,6 +6,7 @@ class Pyqwt < Formula
 
   bottle do
     cellar :any
+    sha256 "997a67617681ac3b3cbc3510da12ecc44ed8d3a8fbff4b7d6d1ba05a16998553" => :el_capitan
     sha256 "3ad94e2532ca76b2e88e1af10b08df8d6775bfd56d401be0590b1e6a39e3651b" => :yosemite
     sha256 "774dcf04f86a8b64672a9d6ec9580956f951a35d29d6f05ec2f5e4a5ee584b44" => :mavericks
     sha256 "736d1306c9929a54f7e4b9c27785bf0a8069bdb63e2fb03675cf00f4adb7475f" => :mountain_lion
