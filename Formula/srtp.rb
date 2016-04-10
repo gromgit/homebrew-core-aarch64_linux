e3d6d868156ccc9d7592db3206d165712710d8da
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 11:24:38 +0100

    srtp: update 1.5.2 bottle.

--- Formula/srtp.rb
+++ Formula/srtp.rb
@@ -8,6 +8,7 @@ class Srtp < Formula
   bottle do
     cellar :any
     revision 1
+    sha256 "d5f214f13d8c34c39241cd68cb201f72a4fbb493e6a8694d965b2f86b30b8015" => :el_capitan
     sha256 "7a9ac7216782ef2ff6e351705a627eaf535c412fb668086868ab86e5a8334413" => :yosemite
     sha256 "28561181099811365178f954b2938c7921dfcd5cdbf2e5129dbf4ae3fb15b910" => :mavericks
     sha256 "bc26d7076d2cb0c219ab498e2760153ed22b6703be69a5120c114be9d8e5ee2a" => :mountain_lion
