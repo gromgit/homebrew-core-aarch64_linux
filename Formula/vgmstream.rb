5e7d627590201b36f77082418aa85f54e263a18f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 13:28:20 +0100

    vgmstream: update r1040 bottle.

--- Formula/vgmstream.rb
+++ Formula/vgmstream.rb
@@ -6,6 +6,7 @@ class Vgmstream < Formula
 
   bottle do
     cellar :any
+    sha256 "8fedd0653375a376ec978739f24abc7a1dc24857a832be983ecd8978dd310043" => :el_capitan
     sha256 "314ab31528d85117117a4610a1f023b22686a565997357df92ceff52e4085013" => :yosemite
     sha256 "65522c757a6ce8392496e71279fa553074dc2b765d56a80b1709b58d1a56e704" => :mavericks
     sha256 "8e9771faf488616e96a159ee3d3681549f58ee240385c55f8a13be507e8a5a6a" => :mountain_lion
