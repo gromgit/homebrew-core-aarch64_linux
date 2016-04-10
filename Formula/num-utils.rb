6f6cafbdad8463fbbce041fa2bf202d14493c33d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 07:32:36 +0100

    num-utils: update 0.5 bottle.

--- Formula/num-utils.rb
+++ Formula/num-utils.rb
@@ -6,7 +6,8 @@ class NumUtils < Formula
   sha256 "03592760fc7844492163b14ddc9bb4e4d6526e17b468b5317b4a702ea7f6c64e"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "f2021913b67fe8ece09b204aa2715a4c8067121908695cee830b363a52a987eb" => :el_capitan
     sha256 "41a55ac6c46aca45473ca365443fb1fd2d77fdb6e4540edbe849d723d31ba0e0" => :yosemite
     sha256 "188ff1f94691f8bf5099ec1012d4732be8fa385bf738671f86780376dd2597b9" => :mavericks
     sha256 "ef3654fed50aff148b0264fdfc75f25310702e8931cc6dcecb4bb3ebce657f7a" => :mountain_lion
