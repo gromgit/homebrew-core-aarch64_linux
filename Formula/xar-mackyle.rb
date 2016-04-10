a28a3e836261292d053cfa08c99a007f33a9220c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 13:28:22 +0100

    xar-mackyle: update 1.6.1 bottle.

--- Formula/xar-mackyle.rb
+++ Formula/xar-mackyle.rb
@@ -5,6 +5,7 @@ class XarMackyle < Formula
   sha256 "ee46089968457cf710b8cf1bdeb98b7ef232eb8a4cdeb34502e1f16ef4d2153e"
 
   bottle do
+    sha256 "cd73fc6ff3a1cf6f1a19d7004c2b44274b91092a5f60af7c66ea972f18ba7946" => :el_capitan
     sha256 "36bea8e87e71c9ffb5e763771b6c23b0dd34a367bdb9d36d1ee945cc936e492d" => :yosemite
     sha256 "fe7a02bfdf51cbd9d931f4783e0fb2024604d7a4505bdf5ae1a5ed34e441dcbc" => :mavericks
     sha256 "2fdd4f84b6e755e7bab16b0f7f783569575a7d9d74b271940c2386981b1febfc" => :mountain_lion
