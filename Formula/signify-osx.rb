982d12fac50ca6790e26a51cf21070bcff8322b0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 11:24:36 +0100

    signify-osx: update 1.2 bottle.

--- Formula/signify-osx.rb
+++ Formula/signify-osx.rb
@@ -6,7 +6,8 @@ class SignifyOsx < Formula
   head "https://github.com/jpouellet/signify-osx.git"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "e7ab392d6bd66b7fc2f4d69226cabb867c73b98a496746b0cb32ba2b258defe4" => :el_capitan
     sha256 "5731f0ed4d6870d29fdcd0d040ba1c011a568b97618e6e752c09fe06ad712384" => :yosemite
     sha256 "6ff9987d91649052b39b06ccf3b8aa2c70c12e31d96477d0c1542775a8304e77" => :mavericks
     sha256 "d483a3522a976b1eb3152f549028afe0b57be262c07a354d60567d62aea657c8" => :mountain_lion
