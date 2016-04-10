63a28386fbf860287b1b2bba9efae82dbd013a12
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 08:14:50 +0100

    p0f: update 3.08b bottle.

--- Formula/p0f.rb
+++ Formula/p0f.rb
@@ -5,7 +5,8 @@ class P0f < Formula
   sha256 "da89a7fe4ba5db3704207f4bde231643bedd4fe8653ab8111c65766c8c768aec"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "0cb866d5486e0e8f3583f5acb2d2ce18ad346511caa8f3ecbace50d3ddb601f1" => :el_capitan
     sha256 "7405d2d0d6070223be3cc0f2ed11c8cd52d886b1480931c1a7e9436d297dbbe7" => :yosemite
     sha256 "113e04c8f1fba685b42620838cb7fa6907431ff8b4f88ed5ab620f67a7a3aca1" => :mavericks
     sha256 "cced7b6bab2cdd563d47c6734efb170eb23e3295f25eedaf3294d78ad8812999" => :mountain_lion
