0cd8b7e69980a8833612199e829cb5c893624322
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 15:38:20 +0100

    libdaemon: update 0.14 bottle.

--- Formula/libdaemon.rb
+++ Formula/libdaemon.rb
@@ -5,9 +5,10 @@ class Libdaemon < Formula
   sha256 "fd23eb5f6f986dcc7e708307355ba3289abe03cc381fc47a80bca4a50aa6b834"
 
   bottle do
-    sha256 "c4884ebab076d0407fde818785d4ad6279aeeada83baf0255048221f454d3000" => :mavericks
-    sha256 "79e977cafe04c9bc692e31896bfcf0fe01c6bc653da2bec6239a8b265f63f15d" => :mountain_lion
-    sha256 "a0a969626ac1c9d3f93a483024e72222609ca00d6ddaaa5f556de082c591772a" => :lion
+    revision 1
+    sha256 "d37febee18ba355a3d536c50dec03c51cb9cabb43cc76859ee2a772f6545e9f3" => :el_capitan
+    sha256 "48de2498d199a800418e557b7fa70c5834094a60b76936d2541e0e242ffc25ec" => :yosemite
+    sha256 "107d695ff51e515e6b8eb1c619a51efea5006d2df8d0dc5b0d32873994b3f936" => :mavericks
   end
 
   def install
