f4be0385d0e5982536715228a2043deac5fcc744
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 18:53:08 +0100

    mp3wrap: update 0.5 bottle.

--- Formula/mp3wrap.rb
+++ Formula/mp3wrap.rb
@@ -5,10 +5,10 @@ class Mp3wrap < Formula
   sha256 "1b4644f6b7099dcab88b08521d59d6f730fa211b5faf1f88bd03bf61fedc04e7"
 
   bottle do
-    cellar :any
-    sha256 "4e8f01653ab2067bcb7ed2716fdb864aac08b517c995839f6d3b942cb705504c" => :mavericks
-    sha256 "ea4a05df9dc44f6fc5f9658fcc6694c0ec0c649a882bc97b3b37354de08f7d0f" => :mountain_lion
-    sha256 "10128ee1f87efb4e7259c39c14633d16d1f6138f6bde064a50e66d3ba8427b1f" => :lion
+    cellar :any_skip_relocation
+    revision 1
+    sha256 "c65886799c1397eec33f48ef73774ad6a509fec44a18dec4a50c8755736f040a" => :el_capitan
+    sha256 "50e1b97fa8423acc0c3980c7171544cf248b049d31cb1c6d3ba1214c293bc2eb" => :mavericks
   end
 
   def install
