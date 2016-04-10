fe5cde414b847b1a4c3426812faa9a4f7e70fb98
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 07:32:36 +0100

    nutcracker: update 0.4.0 bottle.

--- Formula/nutcracker.rb
+++ Formula/nutcracker.rb
@@ -6,7 +6,8 @@ class Nutcracker < Formula
   head "https://github.com/twitter/twemproxy.git"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "276645111292739c724e136020622e688eb0c168989a917c7d1c10b1eb0040fd" => :el_capitan
     sha256 "369a87e5fc60849e8fb1b40f8df1f57406f7b86fbb7b2b5503b3b1a75fc1ecfd" => :yosemite
     sha256 "603e6a29b7f3c80680aa811e80e52ddbf5c5b35aa676d363ac79cdd254489d16" => :mavericks
     sha256 "58e30a2d345c686e6cecb492a8356d9696d9200481c55fabc7460a55ae24e162" => :mountain_lion
