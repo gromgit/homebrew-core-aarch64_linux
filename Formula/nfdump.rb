9a451c86eae2bbdd514c7b3b7ad0955bd57d942b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 07:32:35 +0100

    nfdump: update 1.6.13 bottle.

--- Formula/nfdump.rb
+++ Formula/nfdump.rb
@@ -5,7 +5,8 @@ class Nfdump < Formula
   sha256 "251533c316c9fe595312f477cdb051e9c667517f49fb7ac5b432495730e45693"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "4dabc32a43c7b2aef457e9f64f822a9a4f5ac1b816a97151824e7c961bd27975" => :el_capitan
     sha256 "2e0585426172efc2d436e3ab80574cbf048cfbcd7e9a5ca1fc072d90f763d748" => :yosemite
     sha256 "d0b6f686b1f95dda13cb282e818b288f29fd30bebfb1b8797093d707348d627f" => :mavericks
     sha256 "5bbd8ec9a51cde3c6ed0aaeb1b77a4f2ba59bea99d5304fa1415a7e2e0d5280b" => :mountain_lion
