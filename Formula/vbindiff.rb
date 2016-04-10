4a656a4790cdfc849645b46ebe6ffab5b10586f3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-10 13:28:19 +0100

    vbindiff: update 3.0_beta4 bottle.

--- Formula/vbindiff.rb
+++ Formula/vbindiff.rb
@@ -6,7 +6,8 @@ class Vbindiff < Formula
   sha256 "7d5d5a87fde953dc2089746f6f6ab811d60e127b01074c97611898fb1ef1983d"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "73f69ac6f7cea7a406c3bf755b9780d7cd87cc46f2916b8224a34e521af6f58c" => :el_capitan
     sha256 "e9eaffa8b280ec31a05a937f3bb6ec4921e96a8655172c5f282ead0a97f806ce" => :yosemite
     sha256 "fbdb102612ca9137b4c89b3dcfa3e624fd5e4bacdd0783f763a35cb050615f02" => :mavericks
     sha256 "e231beebaf188fa8188b0f5372643b7127acfbf3f105a7ed56f0b40d5f6b15d0" => :mountain_lion
