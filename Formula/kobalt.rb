7ac3cf6d43bb4b81b2d6e30e1467a93af335a5cb
Jerzerak <jerzerak@gmail.com>
2016-04-11 00:05:45 -0400

    kobalt 0.732
    Closes #205.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/kobalt.rb
+++ Formula/kobalt.rb
@@ -1,8 +1,8 @@
 class Kobalt < Formula
   desc "Build system"
   homepage "http://beust.com/kobalt"
-  url "https://github.com/cbeust/kobalt/releases/download/0.720/kobalt-0.720.zip"
-  sha256 "6c627c1df3a15981bc63f0eca456de37852fae58d3f6f6f79ffb880286238a60"
+  url "https://github.com/cbeust/kobalt/releases/download/0.732/kobalt-0.732.zip"
+  sha256 "4463c343a24b887ef8ae84cd06a495445d8820aaeef338f4f7d69e38adfaec65"
 
   bottle :unneeded
 
@@ -40,6 +40,5 @@ class Kobalt < Formula
     system "#{bin}/kobaltw", "assemble"
     output = "kobaltBuild/libs/test-1.0.jar"
     assert File.exist?(output), "Couldn't find #{output}"
-
   end
 end
