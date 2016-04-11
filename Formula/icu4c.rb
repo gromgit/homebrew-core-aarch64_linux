8b918c6aed26d2a5a5938224d258d67c757c1741
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-11 02:53:31 +0100

    icu4c: update 57.1 bottle.

--- Formula/icu4c.rb
+++ Formula/icu4c.rb
@@ -10,9 +10,9 @@ class Icu4c < Formula
 
   bottle do
     cellar :any
-    sha256 "a6ca9e033a1b0cc58620b5ab70dc32565a7700cc643773476ed8e1b7e5641d2c" => :el_capitan
-    sha256 "b1e4a9fcf06da4c0303d95d10fbc40b5553c22f97d5586d696807ac44a80e55b" => :yosemite
-    sha256 "64996840dbc39cb4a02cedc5d6bf2d266c01cab8ad46d956a12daf9b40c65763" => :mavericks
+    sha256 "c244860b11f70115cecc19a81566232de900dd2f9d049555c77db3744087ca2a" => :el_capitan
+    sha256 "1a18b7c039316fee2376565085c71cb141cae0e5a9680c69b2ca0e8e6d32ce58" => :yosemite
+    sha256 "cadba844e31de5cf9a1f9d05dc8671662cfb52610c7ce92f1fef6e3617dee2ea" => :mavericks
   end
 
   keg_only :provided_by_osx, "OS X provides libicucore.dylib (but nothing else)."
