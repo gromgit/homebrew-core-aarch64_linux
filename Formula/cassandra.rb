ff8a2e3954f767d8f0fbd5b24e8e9f49752f83eb
Ilia Abashin <iabashin@cisco.com>
2016-04-10 18:28:32 -0700

    cassandra: fix audit problem - added plist_options for formula-defined plist
    Closes #201.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/cassandra.rb
+++ Formula/cassandra.rb
@@ -120,6 +120,8 @@ class Cassandra < Formula
     (bin/"cqlsh").write_env_script libexec/"bin/cqlsh", :PYTHONPATH => pypath
   end
 
+  plist_options :manual => "cassandra start"
+
   def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
