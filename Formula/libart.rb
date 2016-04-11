91d10c6cd193420b192bb9e4228006c7a58e1668
matteocng <matteocng@users.noreply.github.com>
2016-04-11 23:45:12 +0200

    libart 2.3.21
    Closes #233.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/libart.rb
+++ Formula/libart.rb
@@ -1,8 +1,8 @@
 class Libart < Formula
   desc "Library for high-performance 2D graphics"
   homepage "http://freshmeat.net/projects/libart/"
-  url "https://download.gnome.org/sources/libart_lgpl/2.3/libart_lgpl-2.3.20.tar.bz2"
-  sha256 "d5531ae3a206a9b5cc74e9a20d89d61b2ba3ba03d342d6a2ed48d2130ad3d847"
+  url "https://download.gnome.org/sources/libart_lgpl/2.3/libart_lgpl-2.3.21.tar.bz2"
+  sha256 "fdc11e74c10fc9ffe4188537e2b370c0abacca7d89021d4d303afdf7fd7476fa"
 
   bottle do
     cellar :any
