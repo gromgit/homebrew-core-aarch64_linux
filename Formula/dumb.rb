d5cd0980ccf043242fc88cca7ee0b718deef3000
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-11 05:13:16 +0100

    dumb: add 0.9.3 bottle.

--- Formula/dumb.rb
+++ Formula/dumb.rb
@@ -4,6 +4,13 @@ class Dumb < Formula
   url "https://downloads.sourceforge.net/project/dumb/dumb/0.9.3/dumb-0.9.3.tar.gz"
   sha256 "8d44fbc9e57f3bac9f761c3b12ce102d47d717f0dd846657fb988e0bb5d1ea33"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d2352df11bee735e963b887609578ec1b3acf0e07748385f472a6add0e1cd2b6" => :el_capitan
+    sha256 "317ac8139d8efb03022bb4f9a76ad61f2358570680563924d13229c52b282dff" => :yosemite
+    sha256 "ee41051ed609807bfb8ce774fa614869db6bdd0fe9a307f6f2cb89f99e9db78e" => :mavericks
+  end
+
   def install
     (buildpath/"make/config.txt").write <<-EOS.undent
       include make/unix.inc
