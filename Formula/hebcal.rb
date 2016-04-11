f4d75515aad48f2380318b8015e3fa446fe52a13
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-11 06:51:41 +0100

    hebcal: add 4.4 bottle.

--- Formula/hebcal.rb
+++ Formula/hebcal.rb
@@ -4,6 +4,13 @@ class Hebcal < Formula
   url "https://github.com/hebcal/hebcal/archive/v4.4.tar.gz"
   sha256 "411f8c05f65c88a84d56ec1e28e13b2fe60a34fb17b9d06c52adbcb3af98ed74"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2e06bf24383b1ffb0057958a495ad63c56046e86abd8eea9a4efe0a90d762b45" => :el_capitan
+    sha256 "24e55910c7c1bbcb075196fca25fdc1a06abd5e16df9df536435e934b138dbb2" => :yosemite
+    sha256 "00c5f478340499a3aa2f5dc0855921ed91441d3c255dfd5d0975dc53f320b4db" => :mavericks
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
 
