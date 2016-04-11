18f6a55ae609ea4c8159e6fd093019972143d558
ilovezfs <ilovezfs@icloud.com>
2016-04-11 08:39:40 -0700

    disco 0.5.4
    Upstream PR discoproject/disco#637 gets it working with >= Erlang 18.1
    
    Closes Homebrew/legacy-homebrew#46780.
    Closes #181.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/disco.rb
+++ Formula/disco.rb
@@ -1,8 +1,10 @@
 class Disco < Formula
   desc "Distributed computing framework based on the MapReduce paradigm"
   homepage "http://discoproject.org/"
-  url "https://github.com/discoproject/disco/archive/0.5.4.tar.gz"
-  sha256 "a1872b91fd549cea6e709041deb0c174e18d0e1ea36a61395be37e50d9df1f8f"
+  url "https://github.com/discoproject/disco.git",
+    :tag => "0.5.4",
+    :revision => "87a755c5302625fceedbcd5a002a4757e8387996"
+  revision 1
 
   bottle do
     cellar :any
@@ -11,72 +13,83 @@ class Disco < Formula
     sha256 "003628e6ba324264409fb54c457089ead2d1c2490565e6a52fd01f19f4ffbaed" => :mountain_lion
   end
 
+  option "without-test", "Skip build-time tests (not recommended)"
+
   depends_on :python if MacOS.version <= :snow_leopard
   depends_on "erlang"
   depends_on "simplejson" => :python if MacOS.version <= :leopard
   depends_on "libcmph"
 
-  # Modifies config for single-node operation
-  patch :DATA
+  resource "nose" do
+    url "https://pypi.python.org/packages/source/n/nose/nose-1.3.7.tar.gz"
+    sha256 "f1bffef9cbc82628f6e7d7b40d7e255aefaa1adb6a1b1d26c69a8b79e6208a98"
+  end
+
+  resource "redis" do
+    url "https://pypi.python.org/packages/source/r/redis/redis-2.10.5.tar.gz"
+    sha256 "5dfbae6acfc54edf0a7a415b99e0b21c0a3c27a7f787b292eea727b1facc5533"
+  end
+
+  # Upstream PR for Erlang 18.1
+  patch do
+    url "https://github.com/discoproject/disco/pull/637.patch"
+    sha256 "97336a33ed115e476ec3aba0692236c4a9e22f17605fd9057ad415772a0f73ec"
+  end
 
   conflicts_with "mono", :because => "both install `disco` binaries"
 
   def install
-    ENV["PYTHONPATH"] = lib+"python2.7/site-packages"
+    ENV["PYTHONPATH"] = libexec/"vendor/lib/python2.7/site-packages"
+
+    # Modifies config for single-node operation.
+    inreplace "conf/gen.settings.sh", /(DDFS_.*_REPLICAS *= * )\d+/, "\\11"
 
     inreplace "Makefile" do |s|
       s.change_make_var! "prefix", prefix
       s.change_make_var! "sysconfdir", etc
       s.change_make_var! "localstatedir", var
+      if build.with? "test"
+        s.gsub! "(cd lib && python setup.py install --user)",
+                "(cd lib && python setup.py install --user --prefix=)"
+        s.gsub! "(cd lib/test && pip install nose --user && PATH=${PATH}:~/.local/bin nosetests)",
+                "(cd lib/test && PATH=${PATH}:#{libexec}/vendor/bin nosetests)"
+      end
     end
-
-    # Disco's "rebar" build tool refuses to build unless it's in a git repo, so
-    # make a dummy one
-    system "git init && git add master/rebar && git commit -a -m 'dummy commit'"
 
     system "make"
-    system "make", "install"
-    prefix.install %w[contrib doc examples]
 
-    # Fix the config file to point at the linked files, not in to cellar
-    # This isn't ideal - if there's a settings.py file left over from a previous disco
-    # installation, it'll issue a Warning
-    inreplace "#{etc}/disco/settings.py" do |s|
-      s.gsub!("Cellar/disco/"+version+"/", "")
+    if build.with? "test"
+      resources.each do |r|
+        r.stage do
+          system "python", *Language::Python.setup_install_args(libexec/"vendor")
+        end
+      end
+      system "make", "test"
     end
 
-    bin.env_script_all_files(libexec+"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    ENV.deparallelize { system "make", "install" }
+    prefix.install %w[contrib doc examples]
+
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+  end
+
+  def post_install
+    # Fix the config file to point at the linked files, not into the Cellar.
+    if File.read(etc/"disco/settings.py").include?(prefix)
+      inreplace etc/"disco/settings.py", prefix, HOMEBREW_PREFIX
+    end
   end
 
   def caveats
     <<-EOS.undent
-      Please copy #{etc}/disco/settings.py to ~/.disco and edit it if necessary.
-      The DDFS_*_REPLICA settings have been set to 1 assuming a single-machine install.
-      Please see http://discoproject.org/doc/disco/start/install.html for further instructions.
+      Please copy #{etc}/disco/settings.py to ~/.disco and edit it if necessary. The
+      DDFS_*_REPLICA settings have been set to 1 assuming a single-machine install.
+      Please see http://disco.readthedocs.org/en/latest/start/install.html for further
+      instructions.
     EOS
   end
 
   test do
-    system "#{bin}/disco"
+    assert_match version.to_s, shell_output("#{bin}/disco client_version")
   end
 end
-
-__END__
-diff -rupN disco-0.4.5/conf/gen.settings.sh my-edits/disco-0.4.5/conf/gen.settings.sh
---- disco-0.4.5/conf/gen.settings.sh  2013-03-28 12:21:30.000000000 -0400
-+++ my-edits/disco-0.4.5/conf/gen.settings.sh 2013-04-10 23:10:00.000000000 -0400
-@@ -23,8 +23,11 @@ DISCO_PORT = 8989
- # DISCO_PROXY_ENABLED = "on"
- # DISCO_HTTPD = "/usr/sbin/varnishd -a 0.0.0.0:\$DISCO_PROXY_PORT -f \$DISCO_PROXY_CONFIG -P \$DISCO_PROXY_PID -n/tmp -smalloc"
-
--DDFS_TAG_MIN_REPLICAS = 3
--DDFS_TAG_REPLICAS     = 3
--DDFS_BLOB_REPLICAS    = 3
-+# Settings appropriate for single-node operation
-+DDFS_TAG_MIN_REPLICAS = 1
-+DDFS_TAG_REPLICAS     = 1
-+DDFS_BLOB_REPLICAS    = 1
-+
-+DISCO_MASTER_HOST     = "localhost"
-
- EOF
