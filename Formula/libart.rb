c0ce9959af0bb631ba2eed8b8de5d0eda0c35ca6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-12 02:44:01 +0100

    libart: update 2.3.21 bottle.

--- Formula/libart.rb
+++ Formula/libart.rb
@@ -6,11 +6,9 @@ class Libart < Formula
 
   bottle do
     cellar :any
-    revision 1
-    sha256 "2509a0b0df6d212af078f2c45ca98b9334f1ec8714439b3071358325512015c2" => :el_capitan
-    sha256 "8244ab052847fae8d71599115dad7cfb2a0bb1820c61fef30b7ee7c23d0c7e37" => :yosemite
-    sha256 "ff0263c2dfa94de8196b3a46cce14e4c877286916d0ef6d06ac8b5ff4abc272e" => :mavericks
-    sha256 "3a84be9bec8759d406dfd1919325d8e415b1632217b77b6dc7052db76fda3986" => :mountain_lion
+    sha256 "18fb7a842650151fef102efadefa52aa12dc3f597ace95b8e25efe6518a65d2e" => :el_capitan
+    sha256 "006a9bf5e40ea99cdb4a10b7a2a2ac6a249f511254be1954a937dac0e50a6f0d" => :yosemite
+    sha256 "276eafd432499ab988a21d9e87104d744e82cf1adf4cc55d47639b1f72ab410a" => :mavericks
   end
 
   def install
