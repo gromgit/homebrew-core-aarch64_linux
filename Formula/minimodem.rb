45f088433b9c2dfd7c48f852317d17b33b85852e
Bart Leusink <bartleusink@gmail.com>
2016-04-12 08:18:58 +0200

    minimodem 0.23
    Closes #239.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/minimodem.rb
+++ Formula/minimodem.rb
@@ -1,10 +1,9 @@
 class Minimodem < Formula
   desc "General-purpose software audio FSK modem"
   homepage "http://www.whence.com/minimodem/"
-  url "http://www.whence.com/minimodem/minimodem-0.22.1.tar.gz"
-  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/m/minimodem/minimodem_0.22.1.orig.tar.gz"
-  sha256 "f41dd27367ffe1607c6b631bb7ab6e1c5c099490e295ce1b603cc54416845ce9"
-  revision 1
+  url "http://www.whence.com/minimodem/minimodem-0.23.tar.gz"
+  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/m/minimodem/minimodem_0.23.orig.tar.gz"
+  sha256 "aab1eacdb0567cf743afa20a54842f88ad3c5399878315abbb67692365204323"
 
   bottle do
     cellar :any
