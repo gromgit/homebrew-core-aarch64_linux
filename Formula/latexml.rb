4ce34ec98a07d69214778600fa1e3fdfa7d785d5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-13 16:54:44 +0100

    latexml: add 0.8.1 bottle.

--- Formula/latexml.rb
+++ Formula/latexml.rb
@@ -4,6 +4,13 @@ class Latexml < Formula
   url "https://github.com/brucemiller/LaTeXML/archive/v0.8.1.tar.gz"
   sha256 "2ba1a580258ff5c7e3d9c2b40fd15cb4c92e388e5cd6b6127e8fbf9b1b9c63ce"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ebef5edcdc7b8b992e547fcc57a0af84c9154391d21e9b63b1d8a4bfd90dcb18" => :el_capitan
+    sha256 "7b10c703cc3a89062210570d74ec27976d33a185b1a089e44ff438e7962d7d8d" => :yosemite
+    sha256 "916998851d7466e0b86d389b8385a9d4113f1da8105102c42e3f7fece8f31533" => :mavericks
+  end
+
   resource "Image::Size" do
     url "https://cpan.metacpan.org/authors/id/R/RJ/RJRAY/Image-Size-3.300.tar.gz"
     mirror "http://search.cpan.org/CPAN/authors/id/R/RJ/RJRAY/Image-Size-3.300.tar.gz"
