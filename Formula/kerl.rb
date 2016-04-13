01bb29f266c87d014f76c3b644476dd27d8d4523
Baptiste Fontaine <batifon@yahoo.fr>
2016-04-13 22:32:44 +0200

    kerl 0.9.1 (new formula, import from head-only)
    Closes #296.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

---
+++ Formula/kerl.rb
@@ -0,0 +1,19 @@
+class Kerl < Formula
+  desc "Easy building and installing of Erlang/OTP instances"
+  homepage "https://github.com/yrashk/kerl"
+  url "https://github.com/yrashk/kerl/archive/0.9.1.tar.gz"
+  sha256 "defb16ba081302135a45cf09eadfa05b64a4906c62edf432d2e9b8a49415a54e"
+  head "https://github.com/yrashk/kerl.git"
+
+  bottle :unneeded
+
+  def install
+    bin.install "kerl"
+    bash_completion.install "bash_completion/kerl"
+    zsh_completion.install "zsh_completion/_kerl"
+  end
+
+  test do
+    system "#{bin}/kerl", "list", "releases"
+  end
+end
