437f9fa69f9a832c8ecff70e9a64f1d0c94af068
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-13 19:44:43 +0100

    libfabric: update 1.3.0 bottle.

--- Formula/libfabric.rb
+++ Formula/libfabric.rb
@@ -5,9 +5,9 @@ class Libfabric < Formula
   sha256 "0a0d4f1a0d178d80ec336763a0fd371ade97199d6f1e884ef8f0e6bc99f258c9"
 
   bottle do
-    sha256 "fb5c566ee556d050f53f19568f752cb5bab989ef01198eddd1b64df26cbdbcdc" => :el_capitan
-    sha256 "34ad02c68e5d921bcf0e0d5086aeb0cbb39157353bbc7dba4cf03d3e4fcbb748" => :yosemite
-    sha256 "e3f95c8408b8b0ec925c65c812a4f1aec3206b1363dfe8fb85d886c099b7b6cd" => :mavericks
+    sha256 "53984f1dac904f22ac42ac6855a1ab61ff0433ac42b499048b47a2ddd66da9bc" => :el_capitan
+    sha256 "5828c0555c4d12ab148c26c91859bd389b9bba030114256fb9337118902283c9" => :yosemite
+    sha256 "2a3136934b1099fb3288e4283bab1ec2972eb002539e35620dc40d0a00c956bf" => :mavericks
   end
 
   def install
