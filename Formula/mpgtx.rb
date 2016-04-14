e0ed135f0c28dadc67849566fdec9b9310ffe96f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-14 17:53:03 +0100

    mpgtx: add 1.3.1 bottle.

--- Formula/mpgtx.rb
+++ Formula/mpgtx.rb
@@ -4,6 +4,13 @@ class Mpgtx < Formula
   url "https://downloads.sourceforge.net/project/mpgtx/mpgtx/1.3.1/mpgtx-1.3.1.tar.gz"
   sha256 "8815e73e98b862f12ba1ef5eaaf49407cf211c1f668c5ee325bf04af27f8e377"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "566ce06d938b4e3b7886a729d456bd3034325985acbdb5e21355b076d7acccf5" => :el_capitan
+    sha256 "dbe21236b1f2ae76dca4be4fa259c9dd902d2b109a6f0f0549cc7f6463945d06" => :yosemite
+    sha256 "a9b32ab7e68133b508d9f919a740ed279567e1b68d3d9a72e0a50013a1029b11" => :mavericks
+  end
+
   def install
     system "./configure", "--parachute",
                           "--prefix=#{prefix}",
