eab5859ec9b0fa23490ede664d310af0fdecc754
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-14 22:53:40 +0100

    plustache: add 0.3.0 bottle.

--- Formula/plustache.rb
+++ Formula/plustache.rb
@@ -4,6 +4,13 @@ class Plustache < Formula
   url "https://github.com/mrtazz/plustache/archive/v0.3.0.tar.gz"
   sha256 "ceb56d6cab81b8ed2d812e4a546036a46dd28685512255e3f52553ba70a15fc8"
 
+  bottle do
+    cellar :any
+    sha256 "45a5e0e42d835e175ff923fc3e2c9fe75a25d5e9c6cc24dd1449622bc0a82d01" => :el_capitan
+    sha256 "7535a016ea3e2e1ab59f452d11feeed4b287591c566a08d0c0c3680f8b2e1239" => :yosemite
+    sha256 "6d6fd22aa6da55eef0eb939aebf4a5ec17c5e3ad101bc5c3132213b573d265d7" => :mavericks
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
