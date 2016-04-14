446b6590482646a639bb62053acf03247cfb9326
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-14 22:53:39 +0100

    perceptualdiff: add 1.1.1 bottle.

--- Formula/perceptualdiff.rb
+++ Formula/perceptualdiff.rb
@@ -4,6 +4,13 @@ class Perceptualdiff < Formula
   url "https://downloads.sourceforge.net/project/pdiff/pdiff/perceptualdiff-1.1.1/perceptualdiff-1.1.1-src.tar.gz"
   sha256 "ab349279a63018663930133b04852bde2f6a373cc175184b615944a10c1c7c6a"
 
+  bottle do
+    cellar :any
+    sha256 "d47d680df91ee88897f95123e6b9f972351a603a5f4921726b2877cc2e67924f" => :el_capitan
+    sha256 "7a1956479cc1176b7340f4614db1b556318513b6359a025dca942142956b65d9" => :yosemite
+    sha256 "99baa893fa0ceaa71fd8c4443a315c2d8e51c567f65a6917eaa5e4ab3952a900" => :mavericks
+  end
+
   depends_on "cmake" => :build
   depends_on "freeimage"
 
