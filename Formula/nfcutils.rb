0bb0bfedb48688839c955f3efd7054bd0e5a3873
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-14 18:54:38 +0100

    nfcutils: add 0.3.2 bottle.

--- Formula/nfcutils.rb
+++ Formula/nfcutils.rb
@@ -4,6 +4,13 @@ class Nfcutils < Formula
   url "https://nfc-tools.googlecode.com/files/nfcutils-0.3.2.tar.gz"
   sha256 "dea258774bd08c8b7ff65e9bed2a449b24ed8736326b1bb83610248e697c7f1b"
 
+  bottle do
+    cellar :any
+    sha256 "ae0ac5663ac10557da9d42a12986268f5ab6149661c8e394df5a314b405d3b30" => :el_capitan
+    sha256 "24217bb0697bfde3272b966d664ed7e1d40c0c986207ea7f7edef84862302a9d" => :yosemite
+    sha256 "9c116d6d84f06a70e271b87268be451dc082a4e191e5cbeeb1a0cb6384f21e1d" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libnfc"
   depends_on "libusb"
