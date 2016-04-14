92f1bfc6b69ca71461632375ea12abf621b8ce17
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-14 18:54:39 +0100

    open-cobol: add 1.1 bottle.

--- Formula/open-cobol.rb
+++ Formula/open-cobol.rb
@@ -4,6 +4,12 @@ class OpenCobol < Formula
   url "https://downloads.sourceforge.net/project/open-cobol/open-cobol/1.1/open-cobol-1.1.tar.gz"
   sha256 "6ae7c02eb8622c4ad55097990e9b1688a151254407943f246631d02655aec320"
 
+  bottle do
+    sha256 "31b5e3af55285483f2e73463aa58cf35afae5a5ee675b7622703d62c34bbe529" => :el_capitan
+    sha256 "a36ae3bbf6eabe5b5119f489b41d26a0bb03ae0aa6935a597ebeb046eea32da7" => :yosemite
+    sha256 "64e3bb2cd22d0b95e4b143191d7dc2c3b245edef0f9a61e0cee366dc96723a77" => :mavericks
+  end
+
   depends_on "gmp"
   depends_on "berkeley-db"
 
