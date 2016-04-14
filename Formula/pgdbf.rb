1afd119b5522524f33cdf62f174b68795e0c9c13
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-14 22:53:39 +0100

    pgdbf: add 0.6.2 bottle.

--- Formula/pgdbf.rb
+++ Formula/pgdbf.rb
@@ -4,6 +4,13 @@ class Pgdbf < Formula
   url "https://downloads.sourceforge.net/project/pgdbf/pgdbf/0.6.2/pgdbf-0.6.2.tar.xz"
   sha256 "e46f75e9ac5f500bd12c4542b215ea09f4ebee638d41dcfd642be8e9769aa324"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "72ad6b801d25db2008d0ab4badd2bb280f55eb6f6956925ee5620d62d8f06bbb" => :el_capitan
+    sha256 "4042a284cac8abe88e7d1c9e6c003e00a9b8247905739829cd768a824df7993b" => :yosemite
+    sha256 "c53298c57bb2d31d82ddce41ed65057d7781de2118857f5f795aaaefe3c00110" => :mavericks
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
