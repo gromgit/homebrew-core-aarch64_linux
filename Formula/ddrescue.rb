954899beaa5e9b61670644bb70545a2556a08c73
matteocng <matteocng@users.noreply.github.com>
2016-04-14 07:08:37 +0200

    ddrescue 1.21
    Closes #307.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Formula/ddrescue.rb
+++ Formula/ddrescue.rb
@@ -1,9 +1,9 @@
 class Ddrescue < Formula
   desc "GNU data recovery tool"
   homepage "https://www.gnu.org/software/ddrescue/ddrescue.html"
-  url "http://ftpmirror.gnu.org/ddrescue/ddrescue-1.20.tar.lz"
-  mirror "https://ftp.gnu.org/gnu/ddrescue/ddrescue-1.20.tar.lz"
-  sha256 "83f993b1a167865d76e92e7c1406436331a8b3b337b9868fd6ca1ec2c45460bd"
+  url "http://ftpmirror.gnu.org/ddrescue/ddrescue-1.21.tar.lz"
+  mirror "https://ftp.gnu.org/gnu/ddrescue/ddrescue-1.21.tar.lz"
+  sha256 "f09e4eb6a209cbd0fe8ee6db2d558238cdc969afa1d94150f263402ac882e1ac"
 
   bottle do
     cellar :any_skip_relocation
