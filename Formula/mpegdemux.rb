6e975e317606a355fc93619f51ce31f6452295fb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-14 16:59:52 +0100

    mpegdemux: add 0.1.4 bottle.

--- Formula/mpegdemux.rb
+++ Formula/mpegdemux.rb
@@ -4,6 +4,13 @@ class Mpegdemux < Formula
   url "http://www.hampa.ch/mpegdemux/mpegdemux-0.1.4.tar.gz"
   sha256 "0067c31398ed08d3a4f62713bbcc6e4a83591290a599c66cdd8f5a3e4c410419"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4f00eabfededc549d4f419b6e6bf7896dbff0c95e83fda5f47067bf73294289b" => :el_capitan
+    sha256 "322f5cf622b25ea9e907e1eb8d1f0ce172672ed91a0592d2621019f3b4159947" => :yosemite
+    sha256 "baa0e3b7dffa4d3017c5639a0e07702b10e8913b87987e3c7235bbfae757fa0e" => :mavericks
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
