6077ee316702155a85e592c8e213955d9b8ad3c1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-14 17:53:02 +0100

    mpfi: add 1.5.1 bottle.

--- Formula/mpfi.rb
+++ Formula/mpfi.rb
@@ -4,6 +4,13 @@ class Mpfi < Formula
   url "https://gforge.inria.fr/frs/download.php/30130/mpfi-1.5.1.tar.gz"
   sha256 "ea2725c6f38ddd8f3677c9b0ce8da8f52fe69e34aa85c01fb98074dc4e3458bc"
 
+  bottle do
+    cellar :any
+    sha256 "9a36493cb162a66182b8792297db5b6cf32d75f9235252e215a2a6c196223f63" => :el_capitan
+    sha256 "6b202dd6c288c0a2f8025050a515a9f258e4adb7d244e98514e0b3adb5636a02" => :yosemite
+    sha256 "d9aaac81a9898a308063e9c2b3154a320f2ea2eb2c5c02bdf33b09a8227606c7" => :mavericks
+  end
+
   depends_on "gmp"
   depends_on "mpfr"
 
