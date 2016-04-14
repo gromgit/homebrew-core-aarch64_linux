9ce6f2451597c5c829d50b4afe541adb7958b768
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-14 16:59:51 +0100

    mimms: add 3.2.1 bottle.

--- Formula/mimms.rb
+++ Formula/mimms.rb
@@ -4,6 +4,13 @@ class Mimms < Formula
   url "https://launchpad.net/mimms/trunk/3.2.1/+download/mimms-3.2.1.tar.bz2"
   sha256 "92cd3e1800d8bd637268274196f6baec0d95aa8e709714093dd96ba8893c2354"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "24e4df14c313ddf76122bef62c9eb68d12ef5e7cc4cfe7f6c9effa2eb65710ba" => :el_capitan
+    sha256 "c260595a5f7382b7665b5a1e943a7ad72d781d0777b6b31a0c2f6a8a96ae95af" => :yosemite
+    sha256 "cd2bcde21ce30010034d006883fc72b02795e0e5f544294c0d0bf275264e231b" => :mavericks
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
   depends_on "libmms"
 
