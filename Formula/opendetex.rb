cbf404582814156983f3dd461d013a52c268adb2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-14 19:48:10 +0100

    opendetex: add 2.8.1 bottle.

--- Formula/opendetex.rb
+++ Formula/opendetex.rb
@@ -4,6 +4,13 @@ class Opendetex < Formula
   url "https://opendetex.googlecode.com/files/opendetex-2.8.1.tar.bz2"
   sha256 "8a47e4c7052672dfe5e0a4214dd5db42ac4322eb382efe6fd1fb271b409d051e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "279c7801888a46fdfea578b6a40874aa61b775b87f0ea6a92e738f57f0a21f27" => :el_capitan
+    sha256 "25c4ce538ac084436d393a29e9cd24a5d0da8ccab59e25bc76b65c0852563bd3" => :yosemite
+    sha256 "a640db332cfcb428d1994321aa0da127ec48eab01fc98b89509033eb6a189b89" => :mavericks
+  end
+
   patch :DATA
 
   def install
