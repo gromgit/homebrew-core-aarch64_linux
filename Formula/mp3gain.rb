d6e29d1a63435e783edea81495f93908ed3a8b77
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-14 16:59:52 +0100

    mp3gain: add 1.5.2 bottle.

--- Formula/mp3gain.rb
+++ Formula/mp3gain.rb
@@ -5,6 +5,13 @@ class Mp3gain < Formula
   version "1.5.2"
   sha256 "3378d32c8333c14f57622802f6a92b725f36ee45a6b181657b595b1b5d64260f"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "27506d45e11a7da5c9555cb17318cf99802d5a8749ad17ed66b7a4b1465790cd" => :el_capitan
+    sha256 "f14681110117c1762ef909dc0b282bb4c23f298c92eefe20d77fab0199cadbaa" => :yosemite
+    sha256 "17abc15792bc75fa186c6748f1cb666814334b2cfec11d708bb9431846746809" => :mavericks
+  end
+
   def install
     system "make"
     bin.install "mp3gain"
