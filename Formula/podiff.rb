6fba5946cdc24372f155e129e706f7d127d83481
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 00:09:50 +0100

    podiff: add 1.1 bottle.

--- Formula/podiff.rb
+++ Formula/podiff.rb
@@ -4,6 +4,13 @@ class Podiff < Formula
   url "http://download.gnu.org.ua/pub/release/podiff/podiff-1.1.tar.gz"
   sha256 "a97480109c26837ffa868ff629a32205622a44d8b89c83b398fb17352b5be6ff"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "24f385afff1c9074d995cd1374c005e8770a80ef11b06058f6024eee67a9de69" => :el_capitan
+    sha256 "21cc248bf36ce685061e234f57c6fc5cbe0d207b201e7a8e485ee7b71d3d21c6" => :yosemite
+    sha256 "c3a3b66344303ad473d8c6808eb9e2a2124bb0a801536870b739beba4de3d5ae" => :mavericks
+  end
+
   def install
     system "make"
     bin.install "podiff"
