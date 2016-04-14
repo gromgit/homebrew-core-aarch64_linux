43685b258f6879abaa15178b1b2b79a16cc7a05a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-14 19:48:11 +0100

    pakchois: add 0.4 bottle.

--- Formula/pakchois.rb
+++ Formula/pakchois.rb
@@ -4,6 +4,12 @@ class Pakchois < Formula
   url "http://www.manyfish.co.uk/pakchois/pakchois-0.4.tar.gz"
   sha256 "d73dc5f235fe98e4d1e8c904f40df1cf8af93204769b97dbb7ef7a4b5b958b9a"
 
+  bottle do
+    sha256 "b02057a2cc01daa05754c537820b58d7c77b632fc5fdb2a6f6dcec77341fe65b" => :el_capitan
+    sha256 "30a06a914f2025d7d23dff48fa8523be455bf925a3282a8c35f56779fd8bd27a" => :yosemite
+    sha256 "03d6ab9d51bdebf61b3c415908e222467fd31cefc4811200eee9e407c604f7f5" => :mavericks
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
