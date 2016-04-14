db87113c27a6fba858bbd95e58fcb45bc073189e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 00:09:51 +0100

    pssh: add 2.3.1 bottle.

--- Formula/pssh.rb
+++ Formula/pssh.rb
@@ -4,6 +4,13 @@ class Pssh < Formula
   url "https://parallel-ssh.googlecode.com/files/pssh-2.3.1.tar.gz"
   sha256 "539f8d8363b722712310f3296f189d1ae8c690898eca93627fc89a9cb311f6b4"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b13dcf5091ba493f21cd44c9ef43d028a4e23627b7b855ab4d299f0d543037a1" => :el_capitan
+    sha256 "16f3c0b42cd3bfabea6a22a39b62299de53e1fb894b72da0c12574f25a09963a" => :yosemite
+    sha256 "62595390d018a9a953928cf6adf8e9299b92f00c3846d74757a18437abbc5f27" => :mavericks
+  end
+
   conflicts_with "putty", :because => "both install `pscp` binaries"
 
   depends_on :python if MacOS.version <= :snow_leopard
