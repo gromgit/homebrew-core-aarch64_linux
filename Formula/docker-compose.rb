297f815a7d3691722a93813bf5368c15bd06922d
Mark Steve Samson <hello@marksteve.com>
2016-04-14 14:00:30 +0800

    docker-compose 1.7.0

--- Formula/docker-compose.rb
+++ Formula/docker-compose.rb
@@ -3,8 +3,8 @@ class DockerCompose < Formula
   homepage "https://docs.docker.com/compose/"
 
   stable do
-    url "https://github.com/docker/compose/archive/1.6.2.tar.gz"
-    sha256 "1c318cdf12bdc97e079224fb0cab45ed0f6ec2d1ce87599eff549b1aafa27837"
+    url "https://github.com/docker/compose/archive/1.7.0.tar.gz"
+    sha256 "a1e6cbe11b5612196dd8324d4cd5a33fda1a6ea99e1bca47cd7e0ab85020480e"
   end
 
   bottle do
@@ -27,8 +27,8 @@ class DockerCompose < Formula
   depends_on "docker-machine" => :recommended
 
   resource "docker-py" do
-    url "https://pypi.python.org/packages/source/d/docker-py/docker-py-1.7.2.tar.gz"
-    sha256 "95b1d14c4ae49dfbb724332cda9c63fb67628b8bdee79c321f2d405cf7a8d04c"
+    url "https://pypi.python.org/packages/source/d/docker-py/docker-py-1.8.0.tar.gz"
+    sha256 "09ccd3522d86ec95c0659887d1da7b2761529020694efb0eeac87074cb4536c2"
   end
 
   resource "requests" do
