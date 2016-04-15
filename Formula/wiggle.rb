470db603854fe9045cd35c9414ee20e1295f7489
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 12:52:53 +0100

    wiggle: add 1.0 bottle.

--- Formula/wiggle.rb
+++ Formula/wiggle.rb
@@ -4,6 +4,13 @@ class Wiggle < Formula
   url "http://neil.brown.name/wiggle/wiggle-1.0.tar.gz"
   sha256 "44c97b2d47a109c709cdd4181d9ba941fee50dbb64448018b91d4a2fffe69cf2"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7e832c2b0e4e4564f15c3a66b1d0b78c309a0f28ca548b5e39b6967bdf12a542" => :el_capitan
+    sha256 "e6799084357708eb5142ec5c1104f43c9fdcc0d953f0b80c458e931f53e210e8" => :yosemite
+    sha256 "09c23ec2dce1bc70a93671b85780f47f9bbb24c024a48cbfa9c3a13b79b048a5" => :mavericks
+  end
+
   # All three patches are upstream commits
   patch do
     url "https://github.com/neilbrown/wiggle/commit/16bb4be1c93be24917669d63ab68dd7d77597b63.diff"
