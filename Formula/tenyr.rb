c350993bc6f99b1f20f7560f3bdeaae5f066f60d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 09:07:39 +0100

    tenyr: add 0.6.0 bottle.

--- Formula/tenyr.rb
+++ Formula/tenyr.rb
@@ -6,6 +6,13 @@ class Tenyr < Formula
 
   head "https://github.com/kulp/tenyr.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "21f73763ac69ceca9e98ac6c8bc471772b6337ecc18acf13faabdaaabd01e25a" => :el_capitan
+    sha256 "180229f9b3855dcb64225c1e5b1ea22105f3931146d2b818d61a5f9058f5fbb7" => :yosemite
+    sha256 "c20b4f63feb6150edc2e46222eed8329d0e8e5bb8bdfa3f84a87059ed03d95a4" => :mavericks
+  end
+
   depends_on "bison" => :build # tenyr requires bison >= 2.5
   depends_on "sdl2"
   depends_on "sdl2_image"
