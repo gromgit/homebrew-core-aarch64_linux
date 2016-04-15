f4301869ac46b11fc2f42965433fa17934f72d4d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 07:03:48 +0100

    splint: add 3.1.2 bottle.

--- Formula/splint.rb
+++ Formula/splint.rb
@@ -4,6 +4,12 @@ class Splint < Formula
   url "http://www.splint.org/downloads/splint-3.1.2.src.tgz"
   sha256 "c78db643df663313e3fa9d565118391825dd937617819c6efc7966cdf444fb0a"
 
+  bottle do
+    sha256 "9eac9f8e530c1d9fc238b57f9d4e143fbf5727450657ba92e6d721660777753b" => :el_capitan
+    sha256 "4b385e4fcf9b82fa2ebd8dabaef7e712039b3f7c83d2f6d5e3263ebf51e7b6d7" => :yosemite
+    sha256 "ad8551b508f303c69499a60456de49d2b77d1f0f2383383d3c01c1b657a230b6" => :mavericks
+  end
+
   # fix compiling error of osd.c
   patch :DATA
 
