51e36ed93070834c71d2cb1a7b5accf1564a1871
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 07:03:48 +0100

    sqsh: add 2.5.16.1 bottle.

--- Formula/sqsh.rb
+++ Formula/sqsh.rb
@@ -4,6 +4,12 @@ class Sqsh < Formula
   url "https://downloads.sourceforge.net/project/sqsh/sqsh/sqsh-2.5/sqsh-2.5.16.1.tgz"
   sha256 "d6641f365ace60225fc0fa48f82b9dbed77a4e506a0e497eb6889e096b8320f2"
 
+  bottle do
+    sha256 "e66731b10a932cc72cb29e5fe4cf42b668a1e2736a4f588f867cd876e5cad11b" => :el_capitan
+    sha256 "39c8bc82d3e1c229d10590a03ba97004df93b325061a0633a03f6f5bdf051dc1" => :yosemite
+    sha256 "113379e4656eb5e0e3c45fffb1ea7fa568bf25e901a0e993bea18d21042bd24c" => :mavericks
+  end
+
   deprecated_option "enable-x" => "with-x11"
 
   depends_on :x11 => :optional
