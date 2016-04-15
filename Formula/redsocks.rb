9877de270427724ac23f8a347c5d41162661a157
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 02:28:25 +0100

    redsocks: add 0.4 bottle.

--- Formula/redsocks.rb
+++ Formula/redsocks.rb
@@ -4,6 +4,13 @@ class Redsocks < Formula
   url "https://github.com/darkk/redsocks/archive/release-0.4.tar.gz"
   sha256 "618cf9e8cd98082db31f4fde6450eace656fba8cd6b87aa4565512640d341045"
 
+  bottle do
+    cellar :any
+    sha256 "ad3df81c5bd45efce293e229c77ff0687141646024a41a0c7cae3b3da34dcdcc" => :el_capitan
+    sha256 "d59f7cd44ed44e76a1d03d83fdd7385330425d42913b4bab36e180c9b082d601" => :yosemite
+    sha256 "a6b82407933ada8111fe21b0491b051aa05f31cdb769840f50e6af5cc4f2c96e" => :mavericks
+  end
+
   depends_on "libevent"
 
   def install
