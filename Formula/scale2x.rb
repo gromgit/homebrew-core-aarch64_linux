83354232c74ad1e979335ee986d0a3cd0712c926
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 03:41:44 +0100

    scale2x: add 2.4_1 bottle.

--- Formula/scale2x.rb
+++ Formula/scale2x.rb
@@ -5,6 +5,13 @@ class Scale2x < Formula
   sha256 "83599b1627988c941ee9c7965b6f26a9fd2608cd1e0073f7262a858d0f4f7078"
   revision 1
 
+  bottle do
+    cellar :any
+    sha256 "c4d3727b99efa7e329259d5b8bc5e600f034db13e7ddfcbb792f8e512e2b5419" => :el_capitan
+    sha256 "ab48af00a09bbb84b1a99cb178d7e2943e51f8ec743b74321ef24a86cc646797" => :yosemite
+    sha256 "0ad21fade0671c6139a0d959e5952614fc9b796dee535db443aaa9e455aa766f" => :mavericks
+  end
+
   depends_on "libpng"
 
   def install
