17c274a00e890019044edff4982c5e44bd96bc0e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 01:28:01 +0100

    pygtkglext: add 1.1.0_1 bottle.

--- Formula/pygtkglext.rb
+++ Formula/pygtkglext.rb
@@ -5,6 +5,12 @@ class Pygtkglext < Formula
   sha256 "9712c04c60bf6ee7d05e0c6a6672040095c2ea803a1546af6dfde562dc0178a3"
   revision 1
 
+  bottle do
+    sha256 "f7300b52df0a015cc86269908b6c23887eb11a7700b40ebd6e2671c2565fb3a2" => :el_capitan
+    sha256 "abbda3f7a801b6b620e9ca77c009b3e3ff3dd2898d8969a8e8148161e38190be" => :yosemite
+    sha256 "afe62336baf3fb7ad52dfe7cd181419a30224dbeb126d77b186fa1600812e81a" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "pygtk"
   depends_on "gtkglext"
