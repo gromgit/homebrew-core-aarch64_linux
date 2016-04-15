cf50447e5924e4cc410e262fccd3da5c926a519b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 08:06:13 +0100

    stlviewer: add 0.1 bottle.

--- Formula/stlviewer.rb
+++ Formula/stlviewer.rb
@@ -4,6 +4,13 @@ class Stlviewer < Formula
   url "https://github.com/vishpat/stlviewer/archive/release-0.1.tar.gz"
   sha256 "55c1969537a7c663273d0d3ab242f0bf61b93d83a7a5ea0786436a2041ecdb8b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e2b6570f4f4fa6042528212a2c0c4798ee7375855e51eaa607e737d746aa2cd0" => :el_capitan
+    sha256 "7bf9c633d050993ecca2013098a3ea544bf25cdd5b5bfdff57607e887e58d0b6" => :yosemite
+    sha256 "cd58c5ba32e12ebb014a809ce8bdcd4a5b85c3caaedd7063710e409a33640707" => :mavericks
+  end
+
   def install
     system "./compile.py"
     bin.install "stlviewer"
