a0568e21299f152d07c07826b5b4f097e20ef867
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 06:02:28 +0100

    snobol4: add 1.5 bottle.

--- Formula/snobol4.rb
+++ Formula/snobol4.rb
@@ -4,6 +4,12 @@ class Snobol4 < Formula
   url "ftp://ftp.ultimate.com/snobol/snobol4-1.5.tar.gz"
   sha256 "9f7ec649f2d700a30091af3bbd68db90b916d728200f915b1ba522bcfd0d7abd"
 
+  bottle do
+    sha256 "f4ee5ba3a933998e7ea1493bab469f00f4ddd13a3e8458002ee43ba6f0cd0e74" => :el_capitan
+    sha256 "6903e1b05a795eae13f2f97fd2f1f4b883b03e1c94ba28e3747b3df98c6a955d" => :yosemite
+    sha256 "b76b9e5bbeccd4b879b2b3c450f3388b82f4641d4d414e0f2d83768eeb0c058b" => :mavericks
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
     system "make", "install"
