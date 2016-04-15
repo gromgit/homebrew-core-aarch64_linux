96e8b8115d2763d30c6016364990a5c9decb5b78
Nikolaus Wittenstein <nikolaus.wittenstein@gmail.com>
2016-04-15 16:23:27 -0400

    mspdebug: update caveat url
     mspdebug changed websites. The home url was changed but not the caveat.  (#349)

--- Formula/mspdebug.rb
+++ Formula/mspdebug.rb
@@ -19,7 +19,7 @@ class Mspdebug < Formula
   def caveats; <<-EOS.undent
       You may need to install a kernel extension if you're having trouble with
       RF2500-like devices such as the TI Launchpad:
-        http://mspdebug.sourceforge.net/faq.html#rf2500_osx
+        http://dlbeer.co.nz/mspdebug/faq.html#rf2500_osx
     EOS
   end
 
