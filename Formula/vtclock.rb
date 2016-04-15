f0156f7eddc3320a75cb8178b067070921230e72
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 12:52:53 +0100

    vtclock: add 2005-02-20 bottle.

--- Formula/vtclock.rb
+++ Formula/vtclock.rb
@@ -6,6 +6,13 @@ class Vtclock < Formula
 
   version "2005-02-20"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f87c685e59533a0085b439c4153c2734d4091447f5a81c627ccc0d2e589ac65d" => :el_capitan
+    sha256 "a72a8c176276c40a3e9b0c6083a61013efb55b5ea43cd786000dad3c4243dd96" => :yosemite
+    sha256 "9811bd8bb3e5cd2f94dc37346e09588bbab5cb1f0cb1ef5f094adf20046440da" => :mavericks
+  end
+
   def install
     system "make"
     bin.install "vtclock"
