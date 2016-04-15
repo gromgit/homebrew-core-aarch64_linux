33f49f7ed341b4c90fd400ec3fb60fc4ab631962
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 12:52:53 +0100

    wakeonlan: add 0.41 bottle.

--- Formula/wakeonlan.rb
+++ Formula/wakeonlan.rb
@@ -4,6 +4,13 @@ class Wakeonlan < Formula
   url "https://github.com/jpoliv/wakeonlan/archive/wakeonlan-0.41.tar.gz"
   sha256 "1d8c3cd106f515167e49134abb8209af891ca152a00ee94a8c5137f654e079bc"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7da7f512ae921016be21fa3899d4f01841e3da5bc96570fcd85b530e4e720c06" => :el_capitan
+    sha256 "78a5d905b250ddb09cc3fa4296f2ffc8c925788a7d8e5d05e96f5581e81a7e9d" => :yosemite
+    sha256 "0669ec3967a728dd2962727896711f418556491ad172483240e84110843998e0" => :mavericks
+  end
+
   def install
     system "perl", "Makefile.PL"
     system "make"
