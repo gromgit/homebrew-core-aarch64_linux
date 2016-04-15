39a4d1a05e1ddfcb391b9148937696659a458f0b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 11:28:38 +0100

    udptunnel: add 1.1 bottle.

--- Formula/udptunnel.rb
+++ Formula/udptunnel.rb
@@ -4,6 +4,13 @@ class Udptunnel < Formula
   url "http://www.cs.columbia.edu/~lennox/udptunnel/udptunnel-1.1.tar.gz"
   sha256 "45c0e12045735bc55734076ebbdc7622c746d1fe4e6f7267fa122e2421754670"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "acbc74c384071aa6be92bea754274c5025de0123afd389d1b174e444a7beec42" => :el_capitan
+    sha256 "8c4b8fb805fa94098925417a8fa61ced0d546b1b77330fd03cd5a72ee6e43b70" => :yosemite
+    sha256 "8f0051f653f62bef0e85f8ced4806a8bae244a911e05129e4fc2bf05912c9412" => :mavericks
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
 
