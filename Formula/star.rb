06988ef9386c07e68c0b07530a958f63a6b4014f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 07:03:48 +0100

    star: add 1.5.3 bottle.

--- Formula/star.rb
+++ Formula/star.rb
@@ -4,6 +4,13 @@ class Star < Formula
   url "https://downloads.sourceforge.net/project/s-tar/star-1.5.3.tar.bz2"
   sha256 "070342833ea83104169bf956aa880bcd088e7af7f5b1f8e3d29853b49b1a4f5b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "64288e33524b1d1afcc5ae7e6ff5dc1488f1793eba9452e54279054d55e93db3" => :el_capitan
+    sha256 "e3b77b33bc2c8ec917ddf41a29d937de1492253c7d039f5747e44e2361cfadd4" => :yosemite
+    sha256 "410f5637ccdf115373b5a08c5037cdb8c66cb113719ead191070d087eae43285" => :mavericks
+  end
+
   depends_on "smake" => :build
 
   def install
