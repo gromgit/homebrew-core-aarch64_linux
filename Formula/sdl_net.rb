351af62edbf14b22e1bc72554c1f70fd6527cfc1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 04:36:37 +0100

    sdl_net: add 1.2.8 bottle.

--- Formula/sdl_net.rb
+++ Formula/sdl_net.rb
@@ -4,6 +4,13 @@ class SdlNet < Formula
   url "https://www.libsdl.org/projects/SDL_net/release/SDL_net-1.2.8.tar.gz"
   sha256 "5f4a7a8bb884f793c278ac3f3713be41980c5eedccecff0260411347714facb4"
 
+  bottle do
+    cellar :any
+    sha256 "036938975b4060fdc944c2258a8d1d5d73f536860a9c807116e6c4fb2aa65dc8" => :el_capitan
+    sha256 "fe6b8eda1d640db450ed12f79feb731d49a62263c4b83601d69659498d697538" => :yosemite
+    sha256 "99f9035f95e548ea81eb11ac9c06ae5eab8d2797ea9ca03ac074fe30bb357748" => :mavericks
+  end
+
   option :universal
 
   depends_on "pkg-config" => :build
