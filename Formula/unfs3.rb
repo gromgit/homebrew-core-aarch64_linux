1ad747cb290bae230af09b5ab7bf7178a23cab9c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 11:28:39 +0100

    unfs3: add 0.9.22 bottle.

--- Formula/unfs3.rb
+++ Formula/unfs3.rb
@@ -4,6 +4,13 @@ class Unfs3 < Formula
   url "https://downloads.sourceforge.net/project/unfs3/unfs3/0.9.22/unfs3-0.9.22.tar.gz"
   sha256 "482222cae541172c155cd5dc9c2199763a6454b0c5c0619102d8143bb19fdf1c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b1387de21ce9672d5caea47ff223fdbca37e0ed08137a252ae14c7c80dea36e1" => :el_capitan
+    sha256 "8daaa9ce7c48d3a0efbf72dac2e7cdb429aa7aa8475b64837eaa4f5159b2a4d8" => :yosemite
+    sha256 "43e89011e0472b5a7aea6583f930d05d18a449474617f5d11b10b10b52badc66" => :mavericks
+  end
+
   head do
     url "https://svn.code.sf.net/p/unfs3/code/trunk/"
 
