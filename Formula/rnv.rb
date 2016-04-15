2dbd0858ab27e6245f88cc406920358974a142e5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 02:28:26 +0100

    rnv: add 1.7.11 bottle.

--- Formula/rnv.rb
+++ Formula/rnv.rb
@@ -4,6 +4,13 @@ class Rnv < Formula
   url "https://downloads.sourceforge.net/project/rnv/Sources/1.7.11/rnv-1.7.11.tar.bz2"
   sha256 "b2a1578773edd29ef7a828b3a392bbea61b4ca8013ce4efc3b5fbc18662c162e"
 
+  bottle do
+    cellar :any
+    sha256 "1c1aa519b786f842b39720e33900e92a2f2f8deef403755e79e2d3b518897ff1" => :el_capitan
+    sha256 "6d46cb2e6476e22b8bb04d00f599884aa8e44ba7e199ad860e4f15795b04e83b" => :yosemite
+    sha256 "f9c4575d7384100b1cc97d9f421b5906ea5068f612c346ffa0238db6c8e855be" => :mavericks
+  end
+
   depends_on "expat"
 
   def install
