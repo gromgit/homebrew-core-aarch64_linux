6961ca17770abd453f81a2927f5c4c61bc4c18a1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 10:18:59 +0100

    tofrodos: add 1.7.13 bottle.

--- Formula/tofrodos.rb
+++ Formula/tofrodos.rb
@@ -4,6 +4,13 @@ class Tofrodos < Formula
   url "http://tofrodos.sourceforge.net/download/tofrodos-1.7.13.tar.gz"
   sha256 "3457f6f3e47dd8c6704049cef81cb0c5a35cc32df9fe800b5fbb470804f0885f"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4a2b22ff08d0fb65c80be7359be2f04d12b70f4e6d490b96cb819ea69b3e3d88" => :el_capitan
+    sha256 "4a5427c6870c3d4822ef4da3ddd8d79c18b91e5b7f14edb4aa449a53da70114e" => :yosemite
+    sha256 "c9759ec570e6a284b250563b8d66076401641f40c1836e293f4eab82cc9fe4ff" => :mavericks
+  end
+
   def install
     cd "src" do
       system "make"
