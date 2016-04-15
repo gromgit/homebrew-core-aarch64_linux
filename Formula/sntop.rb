27109f54be6f38fc520d3adb39d0c893327db442
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 06:02:28 +0100

    sntop: add 1.4.3 bottle.

--- Formula/sntop.rb
+++ Formula/sntop.rb
@@ -4,6 +4,13 @@ class Sntop < Formula
   url "http://distcache.freebsd.org/ports-distfiles/sntop-1.4.3.tar.gz"
   sha256 "943a5af1905c3ae7ead064e531cde6e9b3dc82598bbda26ed4a43788d81d6d89"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5d878e67c779335a8eb02815ba54b77942e14f2ff3a8d21bf62cbec82ed82be1" => :el_capitan
+    sha256 "6249ada241e03cff9f860c18125016e646d88c8f97a417acad9c81dd95a8c708" => :yosemite
+    sha256 "3c7b79334ee07ec52ad014ccdcc42dca8b923f2486793b751eee3b239be07705" => :mavericks
+  end
+
   depends_on "fping"
 
   def install
