1e851a36a93ceefa63f8204895fbad5352973afd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 04:36:38 +0100

    sgrep: add 1.94a bottle.

--- Formula/sgrep.rb
+++ Formula/sgrep.rb
@@ -5,6 +5,12 @@ class Sgrep < Formula
   mirror "https://fossies.org/linux/misc/sgrep-1.94a.tar.gz"
   sha256 "d5b16478e3ab44735e24283d2d895d2c9c80139c95228df3bdb2ac446395faf9"
 
+  bottle do
+    sha256 "089890a739b047b429b88a583f71832fbfbd3c8f7abc067531424c14e8463df4" => :el_capitan
+    sha256 "a4228a3f40db355cbd6b3feedd4cbdaf8c9b582b24f188982f296b17ac14590f" => :yosemite
+    sha256 "f582b8ae918c1f279e1e3c362d016ad91aeb4accc229cfe9f6ccac8c991ce4a6" => :mavericks
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
