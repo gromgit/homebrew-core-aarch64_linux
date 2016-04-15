af19dfb0fc7fbc40504fabd374cc8de11dc27ac4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 07:03:48 +0100

    ssss: add 0.5 bottle.

--- Formula/ssss.rb
+++ Formula/ssss.rb
@@ -4,6 +4,13 @@ class Ssss < Formula
   url "http://point-at-infinity.org/ssss/ssss-0.5.tar.gz"
   sha256 "5d165555105606b8b08383e697fc48cf849f51d775f1d9a74817f5709db0f995"
 
+  bottle do
+    cellar :any
+    sha256 "ffc9b4c320b50f3d093000f9cde8fff3e4f2869ff4111a7da25b0cf17a2bc065" => :el_capitan
+    sha256 "8242a9583ca549f506c107ee1df51c19b04790a8f64605d67ffcd62de34c21ea" => :yosemite
+    sha256 "695899e6e9fac80f8502362c9bb11811113f33373cfc1d0ea99467ac26035776" => :mavericks
+  end
+
   depends_on "gmp"
   depends_on "xmltoman"
 
