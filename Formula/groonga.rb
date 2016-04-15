98b7e076058324fe11060a299012d933c3088845
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 15:04:08 +0100

    groonga: update 6.0.1 bottle.

--- Formula/groonga.rb
+++ Formula/groonga.rb
@@ -5,9 +5,9 @@ class Groonga < Formula
   sha256 "16297c1e4c7cb12c4fca515a28b5cb1e9ef0db0867ff4ad3d8d5436c0e175b6d"
 
   bottle do
-    sha256 "6d689d96def16b1519a5cf56cda44c3fa1faac896eb8e70c3995bf882bfd5c9b" => :el_capitan
-    sha256 "5d45d7d58c454dad31b7c218bbe1733dba140dc78af9bf633621658bc3872abb" => :yosemite
-    sha256 "9b914098f34d412e38148e127d2f1555f4b7bbf836f67ad4a550b1132d7edb07" => :mavericks
+    sha256 "dc92bb6507e72f77f020579f07aa2c767e8f3983bc2047f28732da02eacff3d2" => :el_capitan
+    sha256 "d9f036c5d9e22451c7f6f4dd8c377bb6f3efbe1efd49b3d19fb508240bf470d1" => :yosemite
+    sha256 "ff47589d938a2c4d8720cedf6291533daa1ca9d44bd2515f309d110f2b913352" => :mavericks
   end
 
   head do
