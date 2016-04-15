d3f649bdbd179dceb80abcf55635c8cf712cb49d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 03:41:44 +0100

    screenresolution: add 1.6 bottle.

--- Formula/screenresolution.rb
+++ Formula/screenresolution.rb
@@ -5,6 +5,13 @@ class Screenresolution < Formula
   sha256 "d3761663eaf585b014391a30a77c9494a6404e78e8a4863383e12c59b0f539eb"
   head "https://github.com/jhford/screenresolution.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "63cfb53fe13d5f5b2c72e8a644b312f8a144b12e2b3f284de5adfc5010e1570d" => :el_capitan
+    sha256 "3b3f5d4c414aa36ee1ce963d47a82a50e06f1ffc7a36759bf13ee12c43845c73" => :yosemite
+    sha256 "9e6944af938c0c9ec9e1e4a79a6849fabb222baa0d977a9425bee6a2827595d0" => :mavericks
+  end
+
   # Uses CGDisplayModeRef type, introduced in 10.6
   depends_on :macos => :snow_leopard
 
