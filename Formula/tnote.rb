5021484c88ab3744fd45eacb68a7bbed1bdae177
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 10:18:59 +0100

    tnote: add 0.2.1 bottle.

--- Formula/tnote.rb
+++ Formula/tnote.rb
@@ -4,6 +4,13 @@ class Tnote < Formula
   url "https://downloads.sourceforge.net/project/tnote/tnote/tnote-0.2.1/tnote-0.2.1.tar.gz"
   sha256 "451e0e352cb279725c5e12ad1c1377be63c7113f3fe568fb6213ede478ba6a87"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "63b2c1aea236fd24d2fa5c315bd0772b009d63018b9c18379a5e782f65debfea" => :el_capitan
+    sha256 "de7e2e72f85a8c42133329f77d242aa0b8a6c9cb2edbb305d843a6e7be1ea3b0" => :yosemite
+    sha256 "8e3a2baf4185b131e0223e6b0fe113890ace15b6a9c67b5f7a4068b3d767c4e6" => :mavericks
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
 
   def install
