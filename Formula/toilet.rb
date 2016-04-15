7ef329eca591c34c80ee387097165ca132494866
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 10:18:59 +0100

    toilet: add 0.3 bottle.

--- Formula/toilet.rb
+++ Formula/toilet.rb
@@ -4,6 +4,12 @@ class Toilet < Formula
   url "http://caca.zoy.org/raw-attachment/wiki/toilet/toilet-0.3.tar.gz"
   sha256 "89d4b530c394313cc3f3a4e07a7394fa82a6091f44df44dfcd0ebcb3300a81de"
 
+  bottle do
+    sha256 "93822fde3d2e69f46143dcb9d8551e7e4301c7a470ae53b3fda8ec6cb44584dd" => :el_capitan
+    sha256 "7362333eea743740a9cdb064c5a74829e37b3f15645797622bb283b9cf3f3b1a" => :yosemite
+    sha256 "ef2c34f742b366f84d2aeeb6d83fb94d6bd443f210e56968fb8b2b5700eab759" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libcaca"
 
