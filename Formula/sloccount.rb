1f15f3bc6badbe8c6710e65487918c34a35a9393
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 06:02:27 +0100

    sloccount: add 2.26 bottle.

--- Formula/sloccount.rb
+++ Formula/sloccount.rb
@@ -4,6 +4,13 @@ class Sloccount < Formula
   url "http://www.dwheeler.com/sloccount/sloccount-2.26.tar.gz"
   sha256 "fa7fa2bbf2f627dd2d0fdb958bd8ec4527231254c120a8b4322405d8a4e3d12b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9fe12539b280711faac5e0950b617ed91f35942311aab112607850645b5696bd" => :el_capitan
+    sha256 "8ee5aab0e8aba23cb6d949c84760fa775473b32b6ce3e245ccb5acfd715f8d68" => :yosemite
+    sha256 "16433612bab2bc3fd6d3b804210c1d71980756b02e5e034aa9402c8229e1c968" => :mavericks
+  end
+
   depends_on "md5sha1sum"
 
   patch :DATA
