d7a9b0dbca26d1ed9a14a06e74dd3a428ea113c3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 09:07:39 +0100

    tclap: add 1.2.1 bottle.

--- Formula/tclap.rb
+++ Formula/tclap.rb
@@ -4,6 +4,13 @@ class Tclap < Formula
   url "https://downloads.sourceforge.net/project/tclap/tclap-1.2.1.tar.gz"
   sha256 "9f9f0fe3719e8a89d79b6ca30cf2d16620fba3db5b9610f9b51dd2cd033deebb"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "71be8c4b552ec527decd02a459139353ca738b90d3971a560276a74694511caf" => :el_capitan
+    sha256 "af4d9a41dbfbccda4f583e9dff77a8f93978a07f624df7a1f9ed30662c006274" => :yosemite
+    sha256 "b93a8106cce72de8055b375519b93b00977e2368b758ccf3481497d075ec6738" => :mavericks
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
