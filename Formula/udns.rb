74def5f7cc7421a8853253bff8b25f7a8e400874
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 11:28:38 +0100

    udns: add 0.4 bottle.

--- Formula/udns.rb
+++ Formula/udns.rb
@@ -4,6 +4,13 @@ class Udns < Formula
   url "http://www.corpit.ru/mjt/udns/udns-0.4.tar.gz"
   sha256 "115108dc791a2f9e99e150012bcb459d9095da2dd7d80699b584ac0ac3768710"
 
+  bottle do
+    cellar :any
+    sha256 "59939957b47912ebb286426391a4e2c904ecc416e9de18dc8c0a74052ac82ffe" => :el_capitan
+    sha256 "342aff7270a4251655eb7cfc538b39db1805cfe965ada5cad1a2819b727d9107" => :yosemite
+    sha256 "d6be7acb570845e63c6ac69b8169c4ce1d5a31f5f76f60bad10168a5b13126ff" => :mavericks
+  end
+
   # Build target for dylib. See:
   # http://www.corpit.ru/pipermail/udns/2011q3/000154.html
   patch :DATA
