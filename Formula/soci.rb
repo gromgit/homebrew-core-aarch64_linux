340fd3ea4651424b665f7ce7e7e1da20b1c972c2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 06:02:28 +0100

    soci: add 3.2.2 bottle.

--- Formula/soci.rb
+++ Formula/soci.rb
@@ -4,6 +4,12 @@ class Soci < Formula
   url "https://download.sourceforge.net/project/soci/soci/soci-3.2.2/soci-3.2.2.zip"
   sha256 "e3ad3ea0ef37eab0ae9e6459d2cdd099b1fa1165b663c349e1200356cf4e9c91"
 
+  bottle do
+    sha256 "36f059c90784c6d4ebe3bcf9f83babddb2bbfef0b108eda04deb4c98e4e5b119" => :el_capitan
+    sha256 "a442c3d3c94c6132bd3e03a2f4b7dc34c1163b6befd807494dbc90049d7a1d82" => :yosemite
+    sha256 "7ea7c9074d708f276244e9b6a5251406a2b1fc7b6d57d0974b98e87df21f24cd" => :mavericks
+  end
+
   depends_on "cmake" => :build
   depends_on "boost" => [:build, :optional]
   depends_on "sqlite" if MacOS.version <= :snow_leopard
