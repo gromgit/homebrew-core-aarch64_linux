a67e210099413fb791817c5b2c724642a6410f69
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 12:52:54 +0100

    wv: add 1.2.9 bottle.

--- Formula/wv.rb
+++ Formula/wv.rb
@@ -4,6 +4,12 @@ class Wv < Formula
   url "http://abisource.com/downloads/wv/1.2.9/wv-1.2.9.tar.gz"
   sha256 "4c730d3b325c0785450dd3a043eeb53e1518598c4f41f155558385dd2635c19d"
 
+  bottle do
+    sha256 "9b9e70a5e3bb7e6d29c5af4eed803a7fcbe31df03deafaf35cba1cfc949f35fa" => :el_capitan
+    sha256 "9924b96a1c67c774976b40de2083717a833d4dcaf801fba100f98677af01051c" => :yosemite
+    sha256 "5076c41e62ea86c638b482904ca0e30cffb2cbd3eb3acfe7b105cb4b28766838" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "glib"
   depends_on "libgsf"
