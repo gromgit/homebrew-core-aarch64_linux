5b9051fa7f06f178bcc4a28c3e5d335d8a569b1f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 14:23:20 +0100

    xml2: add 0.5 bottle.

--- Formula/xml2.rb
+++ Formula/xml2.rb
@@ -4,6 +4,13 @@ class Xml2 < Formula
   url "http://download.ofb.net/gale/xml2-0.5.tar.gz"
   sha256 "e3203a5d3e5d4c634374e229acdbbe03fea41e8ccdef6a594a3ea50a50d29705"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5785498202dc54640b0d7ce48de39d675d5b2ecf2703bf60fa71a1f2b20f5d42" => :el_capitan
+    sha256 "7099efc43bc968b2916dcae199bfd3b32e08b0dc25a463236dc6cc23d00a85de" => :yosemite
+    sha256 "943d2f95f5ae95660f70bc0b3dca38eaf124d179ab3f2c9db204f81dfc44d3bb" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
 
   def install
