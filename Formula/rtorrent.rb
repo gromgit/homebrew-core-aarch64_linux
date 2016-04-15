26d35c899515e02bf16fd87e34580433a22e2998
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 03:41:43 +0100

    rtorrent: add 0.9.6 bottle.

--- Formula/rtorrent.rb
+++ Formula/rtorrent.rb
@@ -4,6 +4,13 @@ class Rtorrent < Formula
   url "http://rtorrent.net/downloads/rtorrent-0.9.6.tar.gz"
   sha256 "1e69c24f1f26f8f07d58d673480dc392bfc4317818c1115265b08a7813ff5b0e"
 
+  bottle do
+    cellar :any
+    sha256 "74df05e0d84fa0c8cb7f05ef17e7df98c30103f67f01bdc3935f37c6732832d0" => :el_capitan
+    sha256 "ac506ab6ba1e7913c44473929f0324d039bbee8e6a43a1619de8cb74ace7863c" => :yosemite
+    sha256 "7df1b4606e2e0e00fc11ade307f231b29c3d885a03b755bc3ea84feba7547b82" => :mavericks
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
