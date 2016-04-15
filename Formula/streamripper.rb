4d689ff37d4148660605276345a6681977cfbc53
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 08:06:13 +0100

    streamripper: add 1.64.6 bottle.

--- Formula/streamripper.rb
+++ Formula/streamripper.rb
@@ -4,6 +4,13 @@ class Streamripper < Formula
   url "https://downloads.sourceforge.net/sourceforge/streamripper/streamripper-1.64.6.tar.gz"
   sha256 "c1d75f2e9c7b38fd4695be66eff4533395248132f3cc61f375196403c4d8de42"
 
+  bottle do
+    cellar :any
+    sha256 "eff1bb37cd652e9b3194e2fda3c682bda9da12f413a11c4e5e337c9bc849b2ea" => :el_capitan
+    sha256 "3465e96b5f17000df88a85729674f911097ab9f1b0170a0c3c89f4892dba6fbb" => :yosemite
+    sha256 "a92b924639b9210e83cae1e63baa8f9b45ab4ec38816e19e32ad6fbae420a510" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "glib"
 
