9c00a564c611eca74a7abd2e274cfb8c52390fe1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 09:07:40 +0100

    tintin: add 2.01.0 bottle.

--- Formula/tintin.rb
+++ Formula/tintin.rb
@@ -4,6 +4,13 @@ class Tintin < Formula
   url "https://downloads.sourceforge.net/project/tintin/TinTin%2B%2B%20Source%20Code/2.01.0/tintin-2.01.0.tar.gz"
   sha256 "e0e35463a97ee5b33ef0b29b2c57fa8276c4e76328cb19c98a6ea92c603a9c76"
 
+  bottle do
+    cellar :any
+    sha256 "87db2f724f02d4904abd62cd1ca64761fc7c036581f002edc4165f304b39c0b1" => :el_capitan
+    sha256 "d5854bd2486230a6438a6de890ab23f812248298cdc4d4ab44c3b6b9866b9b1f" => :yosemite
+    sha256 "c8af90d6c19c9c8ccb3f09276fbe89f059e9762b36e35e981b2c856f952d12c7" => :mavericks
+  end
+
   depends_on "pcre"
 
   def install
