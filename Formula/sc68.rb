863447eb34473704e0044121f19c53c98e6c0ab5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 03:41:43 +0100

    sc68: add 2.2.1 bottle.

--- Formula/sc68.rb
+++ Formula/sc68.rb
@@ -4,6 +4,12 @@ class Sc68 < Formula
   url "https://downloads.sourceforge.net/project/sc68/sc68/2.2.1/sc68-2.2.1.tar.gz"
   sha256 "d7371f0f406dc925debf50f64df1f0700e1d29a8502bb170883fc41cc733265f"
 
+  bottle do
+    sha256 "d5ac5c810d4f3505230f2cdb9bc3f9f8c14394e1663f30f8d601fe4a559f99c8" => :el_capitan
+    sha256 "b6b3fb845e14cd2c35212911b261bb4a15f38c528522789fd5905e762b7d0bfc" => :yosemite
+    sha256 "8834ebed226bb20898b91be054fbd54491f524ea780c6559eba1c97166bf7e7d" => :mavericks
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
