a7849e6d4fc1e72f8193ffd9ed724961f9935dc3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 02:28:26 +0100

    ripmime: add 1.4.0.10 bottle.

--- Formula/ripmime.rb
+++ Formula/ripmime.rb
@@ -4,6 +4,13 @@ class Ripmime < Formula
   url "http://www.pldaniels.com/ripmime/ripmime-1.4.0.10.tar.gz"
   sha256 "896115488a7b7cad3b80f2718695b0c7b7c89fc0d456b09125c37f5a5734406a"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1151fa0bb8a10779979cec95c7039832eb81b7126f808ba9c89ccb73cf658814" => :el_capitan
+    sha256 "6ef2fdabe468bc42be725020ef23cc924d1572c7446648e38dbd6de3f1399a38" => :yosemite
+    sha256 "741b45ca155022fb6b540dd1cc0882f5f29330b6909e37fd5115e84705d9d6bb" => :mavericks
+  end
+
   def install
     system "make", "LIBS=-liconv", "CFLAGS=#{ENV.cflags}"
     bin.install "ripmime"
