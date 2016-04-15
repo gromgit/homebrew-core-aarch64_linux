1d6c8267491bb3dca5004e1294cd82b45d53f954
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 04:36:38 +0100

    serialosc: add 1.2 bottle.

--- Formula/serialosc.rb
+++ Formula/serialosc.rb
@@ -6,6 +6,13 @@ class Serialosc < Formula
 
   head "https://github.com/monome/serialosc.git"
 
+  bottle do
+    cellar :any
+    sha256 "b2b9ec1d9e3058d7752eed3773f7fa724981ddbae5a990db10ad3e98f02d1c1f" => :el_capitan
+    sha256 "a601c8dcadb93c4e4ee0f15e7db22dc0e124bcaf9a3e7b875091d612ea70b078" => :yosemite
+    sha256 "e08f42f4285870af8ba9f341cda5e4420c7535a55f54c9fb78582cfd53fdad3a" => :mavericks
+  end
+
   depends_on "liblo"
   depends_on "confuse"
   depends_on "libmonome"
