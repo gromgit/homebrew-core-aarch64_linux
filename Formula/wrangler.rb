0ff7f545ef2ddeb473dd466df0b05fc7af06730b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 12:52:53 +0100

    wrangler: add 1.1.01 bottle.

--- Formula/wrangler.rb
+++ Formula/wrangler.rb
@@ -4,6 +4,12 @@ class Wrangler < Formula
   url "https://github.com/RefactoringTools/wrangler/archive/wrangler1.1.01.tar.gz"
   sha256 "e61c13ec772e137efdcf5aa8f21210ef424eac3ee2b918efe5e456985bb19626"
 
+  bottle do
+    sha256 "9fb111bc3bf199f301205311fc2b419f4af55924d9741a43038ad01cd2d7284f" => :el_capitan
+    sha256 "eba5bb8ddd5db9b8789263b21faa1c9126a4699c67326353fd5a6a22ef5b482b" => :yosemite
+    sha256 "dfd60ec29bbd037decd94fd7369a5017182fb803608872b62a214381de29849c" => :mavericks
+  end
+
   depends_on "erlang"
 
   def install
