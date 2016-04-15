7e120dd72d7337c511102a6b8cd7de80bf8a8226
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 11:28:38 +0100

    tta: add 2.2 bottle.

--- Formula/tta.rb
+++ Formula/tta.rb
@@ -4,6 +4,13 @@ class Tta < Formula
   url "https://downloads.sourceforge.net/project/tta/tta/libtta/libtta-2.2.tar.gz"
   sha256 "1723424d75b3cda907ff68abf727bb9bc0c23982ea8f91ed1cc045804c1435c4"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0543d1561fe44fc6137f90076d247f16e6ac28e72413a7ba3bac08d422bb4e9c" => :el_capitan
+    sha256 "e25b0a3c395c62d2cb130f4817e405a9e09494c92c17fc71bf123d72b6da5f06" => :yosemite
+    sha256 "1b4bdda9786729fffe279cd17faea744108198064d2effcc42b078eb85862671" => :mavericks
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
