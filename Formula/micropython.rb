102fe18246f2764ca306fd4243821e23cea9d61a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-15 23:54:18 +0100

    micropython: add 1.7 bottle.

--- Formula/micropython.rb
+++ Formula/micropython.rb
@@ -4,6 +4,13 @@ class Micropython < Formula
   url "https://github.com/micropython/micropython/archive/v1.7.tar.gz"
   sha256 "ad44d28700d346ceb9a70ae92d36306d42e187fc1af19fa2c7a3ab7dc18742ef"
 
+  bottle do
+    cellar :any
+    sha256 "811bdee93e7d2c4dfcaed11437b540b3473e6a1e7ccec08b573c2601afb21176" => :el_capitan
+    sha256 "b2c3ccfd452ad15f6840dd3efd7706cd7bd19769920467534e23b0ea2107108a" => :yosemite
+    sha256 "78c2a5e5280c047e0f8aa1e20cb22f0ee31f41aa7287b718d36c10d80c76ae67" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libffi"  # Requires libffi v3 closure API; OS X version is too old
 
