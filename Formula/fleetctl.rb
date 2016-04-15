d4d97281c60808016684d895f6417080c0112bd3
Kurtis Kemple <kurtiskemple@gmail.com>
2016-04-15 18:57:06 -0400

    fleetctl 0.11.7
    Closes #354.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Formula/fleetctl.rb
+++ Formula/fleetctl.rb
@@ -1,9 +1,8 @@
 class Fleetctl < Formula
   desc "Distributed init system"
   homepage "https://github.com/coreos/fleet"
-  url "https://github.com/coreos/fleet/archive/v0.11.5.tar.gz"
-  sha256 "a6a785099df71645b5fe8755a36baa6c11138749bc02ae4990fd3f52663c0394"
-  revision 1
+  url "https://github.com/coreos/fleet/archive/v0.11.7.tar.gz"
+  sha256 "5c838059826d6cde1183554701d3af93619980c299fe9d1365588a30a4ca6cc8"
 
   head "https://github.com/coreos/fleet.git"
 
