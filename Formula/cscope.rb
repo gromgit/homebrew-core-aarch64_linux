ad21ca9b210d650ea3b851afc25b7fc7a267dcda
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-16 12:44:03 +0100

    cscope: update 15.8b bottle.

--- Formula/cscope.rb
+++ Formula/cscope.rb
@@ -6,10 +6,9 @@ class Cscope < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "10c4cd802d68e0c552a99e86b0609b882664583e39b9c3ff44591832e75277e2" => :el_capitan
-    sha256 "5625a04292cb85ee5ad70417db976aeb167b7b55c011f218d43febd2ee72b5c2" => :yosemite
-    sha256 "c456f77835232efe5e3f9ed52885175266a039fbbc250afd9b6e646292c4b7d7" => :mavericks
-    sha256 "80dbf0043c44a13d525b06096246e6ce493c1171d4b721cfa3828ac446e51831" => :mountain_lion
+    sha256 "97930be35cbcd08980651597acf931adbb50a80df10960d6564497f19d9c8032" => :el_capitan
+    sha256 "71d86771790165c777341e4457dd193008cfb2fb24628a138cb45fec61e6b42d" => :yosemite
+    sha256 "cb2f63522d072307cacf63e8eabf4c284f2e8c1b2ff8c6de3aeb6fb8759a1212" => :mavericks
   end
 
   def install
