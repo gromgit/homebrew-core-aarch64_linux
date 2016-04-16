87090310f4f529ccbbdf48b6fc7e49e34bbfc728
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-16 14:18:27 +0100

    cppformat: update 2.1.1 bottle.

--- Formula/cppformat.rb
+++ Formula/cppformat.rb
@@ -6,9 +6,9 @@ class Cppformat < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "384474bcf1de6c5e65781bbb89702edfbc26d1196ded1da685c0579d4f466607" => :el_capitan
-    sha256 "5d122ca83c6d62238bb4fb87f59a5dd0d7ab8c90d662c81abec154c8909e0de7" => :yosemite
-    sha256 "b6ef2a5513df531439f0f34b9687139a73f114c883ec57ba41fecf1abd46e60f" => :mavericks
+    sha256 "4b1ef0852ab31be3d350262d4a002a3a0de4d99191454968607b629d6728a034" => :el_capitan
+    sha256 "e8a3d6249d417f6a56e012b7327239ba5ab1993264260a612b5108e017cb5c66" => :yosemite
+    sha256 "eaf14af839288acc850ac113ccd9116cb51e1366344d4c2e17d8b1070bd29aa6" => :mavericks
   end
 
   depends_on "cmake" => :build
