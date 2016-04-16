dd0fe3cd7194a97fa9427d68793a05f5dd128b7d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-16 11:36:36 +0100

    cabal-install: update 1.22.9.0 bottle.

--- Formula/cabal-install.rb
+++ Formula/cabal-install.rb
@@ -5,10 +5,9 @@ class CabalInstall < Formula
   sha256 "874035e5730263653c7aa459f270efbffc06da92ea0c828e09ebc04400e94940"
 
   bottle do
-    revision 1
-    sha256 "75a8233c8825ca13afa976fadff8ddda532aa1deeeba1a1bb4d2b227e2dc4e2d" => :el_capitan
-    sha256 "5afe2d71b44c28af300384b6f957dbe65b87fb0d7276609016856733f7791bfa" => :yosemite
-    sha256 "a328d8c73a3913ef488db38a8ff29162ae863c9b66f1e76b1480c4f23560f9ee" => :mavericks
+    sha256 "e0aeba6df425d2e6d3c9f40059eeafffc4c7ab50fcf9e8a018a6bafed859c253" => :el_capitan
+    sha256 "07c790c32d85cc62230eb35bed0f1ba6c589000f2e7548f1bcd88d087d969e87" => :yosemite
+    sha256 "780faa5309f62c7e4cf222c9f7f82f4f2e6b8c3d87e18d45ca2e3ebbb9757347" => :mavericks
   end
 
   depends_on "ghc"
