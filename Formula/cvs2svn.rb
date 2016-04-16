dc0c17dc531b898dc1c89dd4665697e901195c28
Xu Cheng <xucheng@me.com>
2016-04-16 22:38:21 +0800

    cvs2svn: use Homebrew Python
    Closes #368.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Formula/cvs2svn.rb
+++ Formula/cvs2svn.rb
@@ -1,26 +1,11 @@
-class PythonWithGdbmRequirement < Requirement
-  fatal true
-
-  satisfy(:build_env => false) { quiet_system "python", "-c", "import gdbm" }
-
-  def message; <<-EOS.undent
-    The Python being used does not include gdbm support,
-    but it is required to build this formula:
-
-      #{which "python"}
-
-    Homebrew's Python includes gdbm support.
-    EOS
-  end
-end
-
 class Cvs2svn < Formula
   desc "Tool for converting from CVS to Subversion"
   homepage "http://cvs2svn.tigris.org/"
   url "http://cvs2svn.tigris.org/files/documents/1462/49237/cvs2svn-2.4.0.tar.gz"
   sha256 "a6677fc3e7b4374020185c61c998209d691de0c1b01b53e59341057459f6f116"
 
-  depends_on PythonWithGdbmRequirement
+  # cvs2svn requires python with gdbm support
+  depends_on "python"
 
   def install
     system "python", "setup.py", "install", "--prefix=#{prefix}"
@@ -42,5 +27,9 @@ class Cvs2svn < Formula
     Contrib scripts and example options files are installed in:
       #{opt_prefix}
     EOS
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/cvs2svn --version")
   end
 end
