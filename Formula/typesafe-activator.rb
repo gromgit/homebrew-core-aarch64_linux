5cc91efd6e68062c2939a50bd95fbe098779c2c0
Damir Vandic <info@damirvandic.com>
2016-04-16 13:24:16 +0200

    typesafe-activator 1.3.9
    Closes #364.
    
    Signed-off-by: Martin Afanasjew <martin@afanasjew.de>

--- Formula/typesafe-activator.rb
+++ Formula/typesafe-activator.rb
@@ -1,16 +1,16 @@
 class TypesafeActivator < Formula
   desc "Tools for working with Typesafe Reactive Platform"
   homepage "https://typesafe.com/activator"
-  url "https://downloads.typesafe.com/typesafe-activator/1.3.7/typesafe-activator-1.3.7-minimal.zip"
-  version "1.3.7"
-  sha256 "030cf23d3b68e588b44840a66aab82d2f927fe140eba46cce566cec5fc98c27c"
+  url "https://downloads.typesafe.com/typesafe-activator/1.3.9/typesafe-activator-1.3.9-minimal.zip"
+  version "1.3.9"
+  sha256 "a418cdc7f204aca9cc8777df6d3a18c1bae1157fd972d60d135fce43e217cd64"
 
   bottle :unneeded
 
   def install
-    rm Dir["*.bat"] # Remove Windows .bat files
-    libexec.install Dir["*"]
-    chmod 0755, libexec/"activator"
-    bin.write_exec_script libexec/"activator"
+    rm Dir["bin/*.bat"] # Remove Windows .bat files
+    libexec.install Dir["libexec/*"]
+    bin.install Dir["bin/*"]
+    chmod 0755, bin/"activator"
   end
 end
