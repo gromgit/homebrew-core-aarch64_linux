52abd62695672e87d0f98393fee62acf28161b97
Viktor Szakats <vszakats@users.noreply.github.com>
2016-04-18 01:13:12 +0200

    mmsrip: use secure urls

--- Formula/mmsrip.rb
+++ Formula/mmsrip.rb
@@ -1,7 +1,7 @@
 class Mmsrip < Formula
   desc "Client for the MMS:// protocol"
-  homepage "http://nbenoit.tuxfamily.org/index.php?page=MMSRIP"
-  url "http://nbenoit.tuxfamily.org/projects/mmsrip/mmsrip-0.7.0.tar.gz"
+  homepage "https://nbenoit.tuxfamily.org/index.php?page=MMSRIP"
+  url "https://nbenoit.tuxfamily.org/projects/mmsrip/mmsrip-0.7.0.tar.gz"
   sha256 "5aed3cf17bfe50e2628561b46e12aec3644cfbbb242d738078e8b8fce6c23ed6"
 
   bottle do
