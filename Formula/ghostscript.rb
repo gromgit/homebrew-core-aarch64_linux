ab3f0f6f0a4be5cfccb84480574f2b10217e6d85
Viktor Szakats <vszakats@users.noreply.github.com>
2016-04-17 22:26:23 +0200

    ghostscript: use secure url in comment
    Closes #391.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/ghostscript.rb
+++ Formula/ghostscript.rb
@@ -42,7 +42,7 @@ class Ghostscript < Formula
 
   conflicts_with "gambit-scheme", :because => "both install `gsc` binaries"
 
-  # http://sourceforge.net/projects/gs-fonts/
+  # https://sourceforge.net/projects/gs-fonts/
   resource "fonts" do
     url "https://downloads.sourceforge.net/project/gs-fonts/gs-fonts/8.11%20%28base%2035%2C%20GPL%29/ghostscript-fonts-std-8.11.tar.gz"
     sha256 "0eb6f356119f2e49b2563210852e17f57f9dcc5755f350a69a46a0d641a0c401"
