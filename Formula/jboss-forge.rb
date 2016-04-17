b9f07892269455b2ebe9085a962c73c942677ab9
Viktor Szakats <vszakats@users.noreply.github.com>
2016-04-17 23:54:01 +0200

    jboss-forge: use secure homepage

--- Formula/jboss-forge.rb
+++ Formula/jboss-forge.rb
@@ -1,6 +1,6 @@
 class JbossForge < Formula
   desc "Tools to help set up and configure a project"
-  homepage "http://forge.jboss.org/"
+  homepage "https://forge.jboss.org/"
   url "https://downloads.jboss.org/forge/releases/3.1.0.Final/forge-distribution-3.1.0.Final-offline.zip"
   version "3.1.0.Final"
   sha256 "710941f4db76b74e7ed9710060adcc416a056101294d9e758f3252f25c3f9569"
