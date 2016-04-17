a9e02321a4ffd779f72cd72011091a8034c79de0
Viktor Szakats <vszakats@users.noreply.github.com>
2016-04-17 23:49:22 +0200

    icon-naming-utils: fix homepage

--- Formula/icon-naming-utils.rb
+++ Formula/icon-naming-utils.rb
@@ -1,6 +1,6 @@
 class IconNamingUtils < Formula
   desc "Script to handle icon names in desktop icon themes"
-  homepage "http://tango.freedesktop.org/"
+  homepage "https://specifications.freedesktop.org/icon-naming-spec/icon-naming-spec-latest.html"
   url "http://tango.freedesktop.org/releases/icon-naming-utils-0.8.90.tar.gz"
   sha256 "044ab2199ed8c6a55ce36fd4fcd8b8021a5e21f5bab028c0a7cdcf52a5902e1c"
 
