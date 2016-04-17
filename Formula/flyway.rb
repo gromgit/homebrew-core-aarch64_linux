67fb868bb87566bc0b473e4f510d57a8cd719e1b
Viktor Szakats <vszakats@users.noreply.github.com>
2016-04-17 22:17:26 +0200

    flyway: use secure homepage

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,6 +1,6 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
-  homepage "http://flywaydb.org/"
+  homepage "https://flywaydb.org/"
   url "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/4.0/flyway-commandline-4.0.tar.gz"
   sha256 "218a7f6b3ac629f084e417bbaecd06ad9723704832aa72a81c6e096ac55aacd4"
 
