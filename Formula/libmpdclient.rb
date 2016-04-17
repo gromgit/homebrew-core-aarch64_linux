e4562d446d73b317ddef30c5ad4e286c2f7c35e3
Viktor Szakats <vszakats@users.noreply.github.com>
2016-04-18 00:35:04 +0200

    libmpdclient: use secure urls

--- Formula/libmpdclient.rb
+++ Formula/libmpdclient.rb
@@ -1,7 +1,7 @@
 class Libmpdclient < Formula
   desc "Library for MPD in the C, C++, and Objective-C languages"
-  homepage "http://www.musicpd.org/libs/libmpdclient/"
-  url "http://www.musicpd.org/download/libmpdclient/2/libmpdclient-2.10.tar.gz"
+  homepage "https://www.musicpd.org/libs/libmpdclient/"
+  url "https://www.musicpd.org/download/libmpdclient/2/libmpdclient-2.10.tar.gz"
   sha256 "bf88ddd9beceadef11144811adaabe45008005af02373595daa03446e6b1bf3d"
 
   bottle do
