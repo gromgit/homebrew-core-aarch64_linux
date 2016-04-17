d470bfa9a8545107f8329eaec98c80da8b00c550
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-17 17:51:39 +0100

    ossp-uuid: update 1.6.2_2 bottle.

--- Formula/ossp-uuid.rb
+++ Formula/ossp-uuid.rb
@@ -9,11 +9,9 @@ class OsspUuid < Formula
 
   bottle do
     cellar :any
-    sha256 "dc07aeeed6eba7f525745aebad48ea8fe9d2bd7d38b6ec61110783b670a2fd3c" => :el_capitan
-    sha256 "91c89715dcd8021801b453d99321a150cc814526ff86d9c71bd9166b884b515c" => :yosemite
-    sha256 "d42862adc71d3bdc481c7a4b49029aa44080be9d44b9b0700142dcb6ba182a55" => :mavericks
-    sha256 "37097a2a7341c05126e60fa9b1a32a21bb3791d770852503168ba633282071b0" => :mountain_lion
-    sha256 "d9ee9e1b764a9863834f6a8c2010038521f83a97b06ca3bb4d1c80ff9d1eae66" => :lion
+    sha256 "ac4456fc1c29db7e0d565ebdd392cf827be315b52c9eb3abcd113c4c7b981f25" => :el_capitan
+    sha256 "c6cfa39816d19fa8d4586d6a364cd17e3a089ea018242875dc371731578a4ac7" => :yosemite
+    sha256 "5253f4fab035aca3ca3b867ce0d081812eb17fe0dcaab6599087abaa385c478d" => :mavericks
   end
 
   option :universal
