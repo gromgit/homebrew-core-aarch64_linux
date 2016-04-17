bcc0432571448a2607595c54d5732cbb7c6994e6
Viktor Szakats <vszakats@users.noreply.github.com>
2016-04-18 01:17:38 +0200

    mysql++: use secure urls

--- Formula/mysql++.rb
+++ Formula/mysql++.rb
@@ -1,7 +1,7 @@
 class Mysqlxx < Formula
   desc "C++ wrapper for MySQL's C API"
-  homepage "http://tangentsoft.net/mysql++/"
-  url "http://tangentsoft.net/mysql++/releases/mysql++-3.2.1.tar.gz"
+  homepage "https://tangentsoft.net/mysql++/"
+  url "https://tangentsoft.net/mysql++/releases/mysql++-3.2.1.tar.gz"
   sha256 "aee521873d4dbb816d15f22ee93b6aced789ce4e3ca59f7c114a79cb72f75d20"
 
   bottle do
