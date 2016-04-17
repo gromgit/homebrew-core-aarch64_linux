f20bd36c502b7883e636e537e8a3d12d445f0952
Viktor Szakats <vszakats@users.noreply.github.com>
2016-04-18 01:14:25 +0200

    mpc: use secure urls

--- Formula/mpc.rb
+++ Formula/mpc.rb
@@ -1,7 +1,7 @@
 class Mpc < Formula
   desc "Command-line music player client for mpd"
-  homepage "http://www.musicpd.org/clients/mpc/"
-  url "http://www.musicpd.org/download/mpc/0/mpc-0.27.tar.gz"
+  homepage "https://www.musicpd.org/clients/mpc/"
+  url "https://www.musicpd.org/download/mpc/0/mpc-0.27.tar.gz"
   sha256 "07113c71a21cbd0ea028273baa8e35f23f2a76b94a5c37e16927fdc7c6934463"
 
   bottle do
