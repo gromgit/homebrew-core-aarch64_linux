31f74d62f7fbc5865a0e4788ff3580d188a798d7
Viktor Szakats <vszakats@users.noreply.github.com>
2016-04-18 01:42:39 +0200

    nylon: use secure url, fix homepage

--- Formula/nylon.rb
+++ Formula/nylon.rb
@@ -1,7 +1,7 @@
 class Nylon < Formula
   desc "Proxy server"
-  homepage "http://monkey.org/~marius/pages/?page=nylon"
-  url "http://monkey.org/~marius/nylon/nylon-1.21.tar.gz"
+  homepage "https://github.com/smeinecke/nylon"
+  url "https://monkey.org/~marius/nylon/nylon-1.21.tar.gz"
   sha256 "34c132b005c025c1a5079aae9210855c80f50dc51dde719298e1113ad73408a4"
 
   bottle do
