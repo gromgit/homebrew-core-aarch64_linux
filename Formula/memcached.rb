e2d611d7fcb01357b809b0822cae8ff15b0d5efd
Viktor Szakats <vszakats@users.noreply.github.com>
2016-04-18 01:07:57 +0200

    memcached: use secure urls, fix audit problem

--- Formula/memcached.rb
+++ Formula/memcached.rb
@@ -1,7 +1,7 @@
 class Memcached < Formula
   desc "High performance, distributed memory object caching system"
-  homepage "http://memcached.org/"
-  url "http://www.memcached.org/files/memcached-1.4.24.tar.gz"
+  homepage "https://memcached.org/"
+  url "https://www.memcached.org/files/memcached-1.4.24.tar.gz"
   sha256 "08a426c504ecf64633151eec1058584754d2f54e62e5ed2d6808559401617e55"
 
   bottle do
@@ -12,10 +12,10 @@ class Memcached < Formula
     sha256 "ad37c20bd1dfc1275c055ec33cb3fae594ed22463a264b80f08b01db3f7d0578" => :mountain_lion
   end
 
-  depends_on "libevent"
-
   option "with-sasl", "Enable SASL support -- disables ASCII protocol!"
   option "with-sasl-pwdb", "Enable SASL with memcached's own plain text password db support -- disables ASCII protocol!"
+
+  depends_on "libevent"
 
   deprecated_option "enable-sasl" => "with-sasl"
   deprecated_option "enable-sasl-pwdb" => "with-sasl-pwdb"
