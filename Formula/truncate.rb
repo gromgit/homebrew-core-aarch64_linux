d4ca73b3fb1a2666028c37cd2112020abd76004b
Viktor Szakats <vszakats@users.noreply.github.com>
2016-04-18 11:36:27 +0200

    truncate: use secure homepage

--- Formula/truncate.rb
+++ Formula/truncate.rb
@@ -1,6 +1,6 @@
 class Truncate < Formula
   desc "truncates a file to a given size"
-  homepage "http://www.vanheusden.com/truncate"
+  homepage "https://www.vanheusden.com/truncate/"
   url "https://github.com/flok99/truncate/archive/0.9.tar.gz"
   sha256 "a959d50cf01a67ed1038fc7814be3c9a74b26071315349bac65e02ca23891507"
   head "https://github.com/flok99/truncate.git"
