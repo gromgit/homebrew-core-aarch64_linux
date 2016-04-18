e77a03e02ea116baac7f0fc0cf109eb76de3af02
Viktor Szakats <vszakats@users.noreply.github.com>
2016-04-18 11:21:15 +0200

    spring-roo: use secure homepage, follow redirect

--- Formula/spring-roo.rb
+++ Formula/spring-roo.rb
@@ -1,6 +1,6 @@
 class SpringRoo < Formula
   desc "Rapid application development tool for Java developers"
-  homepage "http://www.springsource.org/spring-roo"
+  homepage "https://projects.spring.io/spring-roo/"
   url "https://s3.amazonaws.com/spring-roo-repository.springsource.org/release/ROO/spring-roo-1.3.2.RELEASE.zip"
   version "1.3.2"
   sha256 "535fb618fe2b9534f0a8bf7003750bb3835ec93ef8a48b05a3511e2adc8ffe9c"
