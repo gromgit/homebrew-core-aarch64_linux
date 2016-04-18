52ff8efb87a59977b5be8069ddd86d54a899d26f
Viktor Szakats <vszakats@users.noreply.github.com>
2016-04-18 10:59:59 +0200

    qt: use secure url in comment
    Closes #405.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/qt.rb
+++ Formula/qt.rb
@@ -18,7 +18,7 @@ class Qt < Formula
   end
 
   # Backport of Qt5 commit to fix the fatal build error with Xcode 7, SDK 10.11.
-  # http://code.qt.io/cgit/qt/qtbase.git/commit/?id=b06304e164ba47351fa292662c1e6383c081b5ca
+  # https://code.qt.io/cgit/qt/qtbase.git/commit/?id=b06304e164ba47351fa292662c1e6383c081b5ca
   patch do
     url "https://raw.githubusercontent.com/Homebrew/patches/480b7142c4e2ae07de6028f672695eb927a34875/qt/el-capitan.patch"
     sha256 "c8a0fa819c8012a7cb70e902abb7133fc05235881ce230235d93719c47650c4e"
