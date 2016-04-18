58843171d43074e67e4a721f433c7a56a9869663
Viktor Szakats <vszakats@users.noreply.github.com>
2016-04-18 11:23:36 +0200

    ssreflect: use secure homepage (redirect to http)

--- Formula/ssreflect.rb
+++ Formula/ssreflect.rb
@@ -1,6 +1,6 @@
 class Ssreflect < Formula
   desc "Virtual package provided by libssreflect-coq"
-  homepage "http://www.msr-inria.fr/projects/mathematical-components-2/"
+  homepage "https://www.msr-inria.fr/projects/mathematical-components-2/"
   url "http://ssr.msr-inria.inria.fr/FTP/ssreflect-1.5.tar.gz"
   sha256 "bad978693d1bfd0a89586a34678bcc244e3b7efba6431e0f83d8e1ae8f82a142"
 
