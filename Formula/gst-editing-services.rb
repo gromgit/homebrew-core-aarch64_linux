320a3698314b9b95d2c5c0561a5e77c47fcd6083
Alexandru Băluț <alex@triposo.com>
2016-04-19 13:34:47 +0200

    gst-editing-services 1.8.0 (new formula)
    Closes #456.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ Formula/gst-editing-services.rb
@@ -0,0 +1,21 @@
+class GstEditingServices < Formula
+  desc "GStreamer Editing Services"
+  homepage "https://gstreamer.freedesktop.org/modules/gst-editing-services.html"
+  url "https://gstreamer.freedesktop.org/src/gst-editing-services/gstreamer-editing-services-1.8.0.tar.xz"
+  sha256 "50f066d5b4090d5739fd6d38932bbe14c1aea83a64ed55f54a08cf90946b3abe"
+
+  depends_on "gstreamer"
+  depends_on "gst-plugins-base"
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-gtk-doc",
+                          "--disable-docbook"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/ges-launch-1.0", "--ges-version"
+  end
+end
