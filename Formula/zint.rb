fd8a8a08001ff1d47fb696dc2c78803cc7ac4feb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-20 14:30:34 +0100

    zint: update 2.4.3_2 bottle.

--- Formula/zint.rb
+++ Formula/zint.rb
@@ -9,9 +9,9 @@ class Zint < Formula
 
   bottle do
     cellar :any
-    sha256 "d3053fb59b35d60a284afbe672a5c70749b6d98181762a443934224aa87997f8" => :yosemite
-    sha256 "293bd454680c7533521d2d766bdbc8c271793cd2340635c5d1dfadace12a3f1c" => :mavericks
-    sha256 "1b3d656bea3766cbc955be1e348af63709b37e12acef35dac4943a712885546d" => :mountain_lion
+    sha256 "0d17d6cc0d330cb09dac162a8a56b69f97195761d26414b9898742af3ff35d9f" => :el_capitan
+    sha256 "8f7447588b730925be6f5e4d366a03394046933b1ae9ce6aebbcd102c8898d77" => :yosemite
+    sha256 "bfbd68636ae952c6b4bb27ac6eadfca425ea5658be708c1152baa0d336d6fce8" => :mavericks
   end
 
   option "with-qt", "Build the zint-qt GUI"
