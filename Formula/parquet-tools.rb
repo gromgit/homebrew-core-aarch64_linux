717098f23eb032ad55a6afc458d39918a7ae103b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-20 17:31:36 +0100

    parquet-tools: add 1.8.1 bottle.

--- Formula/parquet-tools.rb
+++ Formula/parquet-tools.rb
@@ -7,6 +7,13 @@ class ParquetTools < Formula
 
   head "https://github.com/apache/parquet-mr.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f1382310a6cfd278b0a545c944e32cc6a486b3885ef1f023f4290e0bde4be7bf" => :el_capitan
+    sha256 "1780f8fa81ef90becc15d5e6cbbe6994f639a51429347f9c223599518a9d049d" => :yosemite
+    sha256 "72c7af52e61a9f93ae80dd0c51f42ff09fc7024b985e785e3b79f866720e4d20" => :mavericks
+  end
+
   depends_on "maven" => :build
 
   def install
