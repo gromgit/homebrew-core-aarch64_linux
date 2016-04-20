e011e39315b510143dee2ed9fcc9ce249f72bc88
JP Simard <jp@jpsim.com>
2016-04-20 18:10:56 +0200

    sourcekitten 0.12.1
    Closes #487.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/sourcekitten.rb
+++ Formula/sourcekitten.rb
@@ -1,7 +1,7 @@
 class Sourcekitten < Formula
   desc "Framework and command-line tool for interacting with SourceKit"
   homepage "https://github.com/jpsim/SourceKitten"
-  url "https://github.com/jpsim/SourceKitten.git", :tag => "0.12.0", :revision => "c4da1e86f441ee9596c441ec3321e5d023beca19"
+  url "https://github.com/jpsim/SourceKitten.git", :tag => "0.12.1", :revision => "4749ef6230c43d19d153cea930fc5a6d0a195ef4"
   head "https://github.com/jpsim/SourceKitten.git"
 
   bottle do
@@ -10,7 +10,7 @@ class Sourcekitten < Formula
     sha256 "3e85b793448ef31bf4c9152db440f84c27e1a08f2ad05a73c94207251ccb43cd" => :yosemite
   end
 
-  depends_on :xcode => ["7.1", :build]
+  depends_on :xcode => ["7.3", :build]
 
   def install
     system "make", "prefix_install", "PREFIX=#{prefix}", "TEMPORARY_FOLDER=#{buildpath}/SourceKitten.dst"
