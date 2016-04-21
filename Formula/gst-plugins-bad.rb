043f1e5db2092b268d7666470b4b0396fb21fa38
Dominyk Tiller <dominyktiller@gmail.com>
2016-04-21 23:51:40 +0100

    gst-plugins-bad: add test

--- Formula/gst-plugins-bad.rb
+++ Formula/gst-plugins-bad.rb
@@ -61,4 +61,10 @@ class GstPluginsBad < Formula
     system "make"
     system "make", "install"
   end
+
+  test do
+    gst = Formula["gstreamer"].opt_bin/"gst-inspect-1.0"
+    output = shell_output("#{gst} --plugin dvbsuboverlay")
+    assert_match version.to_s, output
+  end
 end
