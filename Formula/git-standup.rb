d49ba8ee67c55af40c5923f6b157c01f465aae2b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-21 10:14:01 +0100

    git-standup: add 1.0.1 bottle.

--- Formula/git-standup.rb
+++ Formula/git-standup.rb
@@ -5,6 +5,13 @@ class GitStandup < Formula
   sha256 "0c43e7414b308bc29ef6f04e84b02aaf3e0ac4837522d34f84359a54c0a4d54b"
   head "https://github.com/kamranahmedse/git-standup.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9ec910ead4b99bdcaa2649a01a207257d56ba6e8d51233b18220637f968eb6d2" => :el_capitan
+    sha256 "a77681880f954d6e3337c582d589cb23b5b34e09a3d787737567278fbb33fc45" => :yosemite
+    sha256 "87050e61def08fe7c2c4b521c5ddc607348987241a94efeb91cf699e84fe63e0" => :mavericks
+  end
+
   def install
     system "make", "install", "prefix=#{prefix}"
   end
