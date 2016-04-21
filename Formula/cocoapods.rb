1a80423349986a2cec05e1c9948c02d044e13717
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-21 13:35:52 +0100

    cocoapods: add 0.39.0 bottle.

--- Formula/cocoapods.rb
+++ Formula/cocoapods.rb
@@ -4,6 +4,13 @@ class Cocoapods < Formula
   url "https://github.com/CocoaPods/CocoaPods/archive/0.39.0.tar.gz"
   sha256 "bb43540dad6d284db6175fc773e3b0f3d3a557ec3de012609b57c3ba7c280fd8"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "805986eb273cac67ab777443d102cc15deb73fda7bf49ede1bfbcf0a186d2911" => :el_capitan
+    sha256 "65ebef0ede3b9da11fdae5a1a5ac21424b441db463f8c0b02d92b22f860eb4ff" => :yosemite
+    sha256 "218ea92fcb19f52e37cd94c9568cac2495e7e354ced13e56f74fc71eb957113b" => :mavericks
+  end
+
   devel do
     url "https://github.com/CocoaPods/CocoaPods/archive/1.0.0.beta.8.tar.gz"
     sha256 "5dc0aa2a6ba68eff0f77f2c620de8a752def0c524934dfaaaf0e20d526908be9"
