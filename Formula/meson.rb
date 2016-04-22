3d207aa36df453129079f5fe8f92797a52ba66fb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-22 22:11:02 +0100

    meson: add 0.31.0 bottle.

--- Formula/meson.rb
+++ Formula/meson.rb
@@ -4,6 +4,13 @@ class Meson < Formula
   url "https://github.com/mesonbuild/meson/releases/download/0.31.0/meson-0.31.0.tar.gz"
   sha256 "803d7aa99329c5439ef971c1dcaa39de12d9208f82f0ed029e3e8f9c13ef93f0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9a807e6daf90cf6dab8768510e38d3f13a3ee99acd1f82f0f196b80dd9a62a89" => :el_capitan
+    sha256 "f2cab3cfa82e715f9d19ff996338028ba6240d11fa5254eae7d0da59d12c8969" => :yosemite
+    sha256 "f30d5c84f5dde05536a487108b7be4031a4f9b3dadb55e3bdb8a91dda9dc48d7" => :mavericks
+  end
+
   depends_on :python3
   depends_on "ninja"
 
