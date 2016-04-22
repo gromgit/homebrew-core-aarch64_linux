771e2eac5a6b3c5d9348ab5f54880728a2c611c1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-22 19:59:22 +0100

    pg_top: add 3.7.0 bottle.

--- Formula/pg_top.rb
+++ Formula/pg_top.rb
@@ -4,6 +4,13 @@ class PgTop < Formula
   url "http://pgfoundry.org/frs/download.php/3504/pg_top-3.7.0.tar.bz2"
   sha256 "c48d726e8cd778712e712373a428086d95e2b29932e545ff2a948d043de5a6a2"
 
+  bottle do
+    cellar :any
+    sha256 "f6942875416fa688254e82feac6f0991cab3181ca0a545bce53ca4af9dc856f3" => :el_capitan
+    sha256 "2e1a209d75f91eea0f22dc6e9c391c95a65289436d5060df7cc803bec8c3df35" => :yosemite
+    sha256 "6d9c0960d9594b6643126ef60ecdae71b21b8ab9ebdcd6d0a7efc9a1847dab87" => :mavericks
+  end
+
   depends_on :postgresql
 
   def install
