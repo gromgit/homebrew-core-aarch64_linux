d450562ff66fbf6beedaba614e840cb4338f8642
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-22 19:59:22 +0100

    pillar: add 2.0.1 bottle.

--- Formula/pillar.rb
+++ Formula/pillar.rb
@@ -4,6 +4,13 @@ class Pillar < Formula
   url "https://github.com/comeara/pillar/archive/v2.0.1.tar.gz"
   sha256 "d5db7fbf956dae05794ec7b26c1de70aaab1d21fed774d95d16a241e9dd3952e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "15701b3bcfcfbded994680a3b923ea430e1e5618f2b2cb96a45a65c50b012d0f" => :el_capitan
+    sha256 "6399b70e777a2067922a435c0bd71409d8c01dc3bb89618aa1859f1714a0dc2b" => :yosemite
+    sha256 "366bd1d56bd750f7886ff97c5108831213361d996fcf1b6085dde53cd1638c37" => :mavericks
+  end
+
   depends_on :java
   depends_on "sbt" => :build
 
