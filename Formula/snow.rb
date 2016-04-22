19229dcc4a83e4035f362802f7c30b359e1248af
Dominyk Tiller <dominyktiller@gmail.com>
2016-04-22 20:20:07 +0100

    snow: use brew mirror

--- Formula/snow.rb
+++ Formula/snow.rb
@@ -1,7 +1,9 @@
 class Snow < Formula
   desc "Whitespace steganography: coded messages using whitespace"
   homepage "http://www.darkside.com.au/snow/"
-  url "http://www.darkside.com.au/snow/snow-20130616.tar.gz"
+  # The upstream website seems to be rejecting curl connections.
+  # Consistently returns "HTTP/1.1 406 Not Acceptable".
+  url "https://dl.bintray.com/homebrew/mirror/snow-20130616.tar.gz"
   sha256 "c0b71aa74ed628d121f81b1cd4ae07c2842c41cfbdf639b50291fc527c213865"
 
   bottle do
