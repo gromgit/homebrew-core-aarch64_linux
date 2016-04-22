d52548fb37aa48751427f5b2aa3e338d1ab1d6fb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-22 15:35:26 +0100

    gst-editing-services: add 1.8.0 bottle.

--- Formula/gst-editing-services.rb
+++ Formula/gst-editing-services.rb
@@ -4,6 +4,12 @@ class GstEditingServices < Formula
   url "https://gstreamer.freedesktop.org/src/gst-editing-services/gstreamer-editing-services-1.8.0.tar.xz"
   sha256 "50f066d5b4090d5739fd6d38932bbe14c1aea83a64ed55f54a08cf90946b3abe"
 
+  bottle do
+    sha256 "28f3187a1bdeece0242dfa792dc35e7dce4398faa43535a2621e3a48e899504f" => :el_capitan
+    sha256 "0363cdae9d87834e3718ba72876f5bfe5cf4ba84b705d52330363cdc5cdf58c2" => :yosemite
+    sha256 "2d67360df8f0fd284955fd67bc9cdeeb64cde858ace20fb731ca124f03e9d240" => :mavericks
+  end
+
   depends_on "gstreamer"
   depends_on "gst-plugins-base"
 
