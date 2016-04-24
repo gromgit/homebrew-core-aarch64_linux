ef99332a75462f4033e0d9f85eea4123a8c3a369
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-24 20:12:11 +0100

    pjproject: update 2.4.5 bottle.

--- Formula/pjproject.rb
+++ Formula/pjproject.rb
@@ -6,10 +6,9 @@ class Pjproject < Formula
 
   bottle do
     cellar :any
-    sha256 "22be2e11f2538808f10aace043fb3096bfa7210bfe18eccd923e107b3c724311" => :el_capitan
-    sha256 "3ee6735dd996f8963d0d47cdd0019fb144485b3e10b604589f99164abd757c10" => :yosemite
-    sha256 "dae64c57018f78472c465c0f70883176d4edb5ceee27e93a8c1df448c8700fe8" => :mavericks
-    sha256 "ebe8c9879332a77a90523f4791c70c109f57ffcecc176e77e2acdcb47bacaa3d" => :mountain_lion
+    sha256 "38f3a930783d7236623b0d90d1be5ab8334c126a49cde9b49dd12cbe1e601b7c" => :el_capitan
+    sha256 "5c4ace5e1cf0aebf00f6a02967a09164659c4adbe24221f54bc4c2d8e6777db5" => :yosemite
+    sha256 "3fb7e329490962df23a0d89574d3469abb2a9dbc9cd279133f4eff79cc1744f6" => :mavericks
   end
 
   depends_on "openssl"
