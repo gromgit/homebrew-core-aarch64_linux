541c56626458376293b7a535448f3b89a2ef35d9
Raphael Randschau <nicolai86@users.noreply.github.com>
2016-04-25 05:58:09 +0200

    terraform 0.6.15
    terraform v0.6.15
    
    Closes #590.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Terraform < Formula
   desc "Tool to build, change, and version infrastructure"
   homepage "https://www.terraform.io/"
-  url "https://github.com/hashicorp/terraform/archive/v0.6.14.tar.gz"
-  sha256 "e19a35e4794ea551d3e42abf771069c658d121c5ebbb7bc1c48c2822412c1825"
+  url "https://github.com/hashicorp/terraform/archive/v0.6.15.tar.gz"
+  sha256 "5dc7cb1d29dee3de9ed9efacab7e72aa447052c96ae8269d932f6a979871a852"
   head "https://github.com/hashicorp/terraform.git"
 
   bottle do
@@ -15,17 +15,10 @@ class Terraform < Formula
   end
 
   depends_on "go" => :build
-  depends_on "godep" => :build
 
   terraform_deps = %w[
     github.com/mitchellh/gox 770c39f64e66797aa46b70ea953ff57d41658e40
     github.com/mitchellh/iochan 87b45ffd0e9581375c491fef3d32130bb15c5bd7
-    github.com/Azure/azure-sdk-for-go 1cb9dff8c37b2918ad1ebd7b294d01100a153d27
-    github.com/aws/aws-sdk-go 4da0bec8953a0a540f391930a946917b12a95671
-    github.com/cenkalti/backoff 4dc77674aceaabba2c7e3da25d4c823edfb73f99
-    github.com/davecgh/go-spew 5215b55f46b2b919f50a1df0eaa5886afe4e3b3d
-    gopkg.in/yaml.v2 f7716cbe52baa25d2e9b0d0da546fcf909fc16b4
-    github.com/golang/protobuf 45bba206dd5270d96bac4942dcfe515726613249
   ]
 
   terraform_deps.each_slice(2) do |x, y|
@@ -36,10 +29,6 @@ class Terraform < Formula
 
   go_resource "golang.org/x/tools" do
     url "https://go.googlesource.com/tools.git", :revision => "977844c7af2aa555048a19d28e9fe6c392e7b8e9"
-  end
-
-  go_resource "google.golang.org/grpc" do
-    url "https://github.com/grpc/grpc-go.git", :revision => "5d64098b94ee9dbbea8ddc130208696bcd199ba4"
   end
 
   def install
@@ -63,10 +52,11 @@ class Terraform < Formula
     end
 
     cd terrapath do
-      terraform_files = `go list ./...`.lines.map { |f| f.strip unless f.include? "/vendor/" }.compact
       # v0.6.12 - source contains tests which fail if these environment variables are set locally.
       ENV.delete "AWS_ACCESS_KEY"
       ENV.delete "AWS_SECRET_KEY"
+
+      terraform_files = `go list ./...`.lines.map { |f| f.strip unless f.include? "/vendor/" }.compact
       system "go", "test", *terraform_files
 
       mkdir "bin"
