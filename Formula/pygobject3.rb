a8681a695006880bbf5bb5c60d20f6c7b04f6015
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-26 07:47:04 +0100

    pygobject3: update 3.20.1 bottle.

--- Formula/pygobject3.rb
+++ Formula/pygobject3.rb
@@ -5,9 +5,9 @@ class Pygobject3 < Formula
   sha256 "3d261005d6fed6a92ac4c25f283792552f7dad865d1b7e0c03c2b84c04dbd745"
 
   bottle do
-    sha256 "28dd19d1d32f8890d246b0e98fbb788438ee753f85e880eb754044752bc3e322" => :el_capitan
-    sha256 "bf85d0cccf9311f91909886745a6ea330868f2dff1045e8f2d53c01ae0531bc7" => :yosemite
-    sha256 "133386638620543280a54cbfe5ed9982b1a55fdeb79e33ab3c3516fb3e6a8033" => :mavericks
+    sha256 "69948257796089035e7f0ca55471290493831ffe88fee85b6ab8621c4b6957fb" => :el_capitan
+    sha256 "11d9221d4dad4f2e7e01709892cb0b7f631087ecd37fae9d6d9cc80a0dbc417f" => :yosemite
+    sha256 "9765c0bbd5a32d844764592b455e506bc60ffec2a59b6c0f79d945c7c231990f" => :mavericks
   end
 
   option :universal
