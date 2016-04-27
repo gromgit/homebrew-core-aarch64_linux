e1a7145e900a67a53de5f6f65a66e366cd6c451b
Juan Ignacio Donoso <jidonoso@gmail.com>
2016-04-27 11:58:49 -0400

    cli53 0.7.4 (new formula)
    Closes #656.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ Formula/cli53.rb
@@ -0,0 +1,21 @@
+class Cli53 < Formula
+  desc "command-line tool for Amazon Route 53"
+  homepage "https://github.com/barnybug/cli53"
+  url "https://github.com/barnybug/cli53/archive/0.7.4.tar.gz"
+  sha256 "3cb89e6aa91676ffd0577798a4b06b056667d18ad836de4fa31c0564ee48474e"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    mkdir_p buildpath/"src/github.com/barnybug"
+    ln_s buildpath, buildpath/"src/github.com/barnybug/cli53"
+
+    system "make", "build"
+    bin.install "cli53"
+  end
+
+  test do
+    assert_match "list domains", shell_output("#{bin}/cli53 help list")
+  end
+end
