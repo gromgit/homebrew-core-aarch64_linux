bf4938c2cb852ddad132325cd4f91e8a765f9e99
Christopher Dolan <chris@codingstream.org>
2016-04-30 19:03:40 -0400

    google-benchmark 1.0.0
    Closes #730.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/google-benchmark.rb
+++ Formula/google-benchmark.rb
@@ -1,8 +1,8 @@
 class GoogleBenchmark < Formula
   desc "C++ microbenchmark support library"
   homepage "https://github.com/google/benchmark"
-  url "https://github.com/google/benchmark/archive/v0.1.0.tar.gz"
-  sha256 "41aa7dca7aa94911deee078e39f7602d760f2911bb33086b5a2c8204717ddad4"
+  url "https://github.com/google/benchmark/archive/v1.0.0.tar.gz"
+  sha256 "d2206c263fc1a7803d4b10e164e0c225f6bcf0d5e5f20b87929f137dee247b54"
   head "https://github.com/google/benchmark.git"
 
   bottle do
