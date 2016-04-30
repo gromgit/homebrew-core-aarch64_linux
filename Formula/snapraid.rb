693aacdba24ba7dbbf3f2885c2ccbdb125c1144f
Alex Dunn <dunn.alex@gmail.com>
2016-04-30 11:19:32 -0700

    snapraid 10.0
    Closes #725.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/snapraid.rb
+++ Formula/snapraid.rb
@@ -1,8 +1,8 @@
 class Snapraid < Formula
   desc "Backup program for disk arrays"
   homepage "http://snapraid.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/snapraid/snapraid-8.1.tar.gz"
-  sha256 "6bf89a1319ac3403958cd2c98a9c6102728c0070cfa1aedd90c4561d93c54e5d"
+  url "https://github.com/amadvance/snapraid/releases/download/v10.0/snapraid-10.0.tar.gz"
+  sha256 "f7dcf19480256fc2c1db9ab976aa12f786e76da6044cc397f0451524e8031ad6"
 
   head do
     url "https://github.com/amadvance/snapraid.git"
