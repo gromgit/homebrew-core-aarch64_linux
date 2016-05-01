46e5b79c530ce40f805bd7ea316c4cfc7a924e9a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-02 00:37:50 +0100

    gdrive: add 2.1.0 bottle.

--- Formula/gdrive.rb
+++ Formula/gdrive.rb
@@ -5,6 +5,13 @@ class Gdrive < Formula
   sha256 "a1ea624e913e258596ea6340c8818a90c21962b0a75cf005e49a0f72f2077b2e"
   head "https://github.com/prasmussen/gdrive.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4cb0399839f4d61561537a5e3e824fd4b158de9976fec00a3eaf5af2d75263a0" => :el_capitan
+    sha256 "a735d01325571d987ef32720a536928faad92a692b2f9008a769b07c6dac5931" => :yosemite
+    sha256 "cac347dada281fd7e1b31bd08b0fcf05f0cadfd2e16f603590505bd0f6592064" => :mavericks
+  end
+
   depends_on "go" => :build
 
   def install
