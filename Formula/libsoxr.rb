26c80bd0ee9d35fb20404cbf13e566bed6706ba4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-01 13:07:14 +0100

    libsoxr: update 0.1.2 bottle.

--- Formula/libsoxr.rb
+++ Formula/libsoxr.rb
@@ -7,10 +7,9 @@ class Libsoxr < Formula
 
   bottle do
     cellar :any
-    revision 1
-    sha256 "d90c59567b5507610bc93c740cde32d6d67707c4b038b7e93c261282be2c1a30" => :el_capitan
-    sha256 "1154e13a305ce25408d3f976d9459ca6d3a044493d87e1b607c68d79408b54c4" => :yosemite
-    sha256 "9e507fa80e4d4be8277e7e52245765a25cec712dde6e85aad646bb6ffad27009" => :mavericks
+    sha256 "077ef8de96bc1d6e91c102a1ef37a8abdfc5a5c58e630ddf4c71d588f4928514" => :el_capitan
+    sha256 "b4a93f140c6811066af0a9c4ed5018426cacd57708aeed413b81ff887823926e" => :yosemite
+    sha256 "ab97e9c831858081a06933a5394623b8e4dbafd660e507cfbe851dee07c73c09" => :mavericks
   end
 
   depends_on "cmake" => :build
