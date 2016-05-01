0171c1d07fcf9544294103ecdc6763aa307bc2db
Dan Poggi <dan.poggi@detroitlabs.com>
2016-05-01 11:17:59 -0400

    typesafe-activator 1.3.10
    Closes #747.
    
    Signed-off-by: Martin Afanasjew <martin@afanasjew.de>

--- Formula/typesafe-activator.rb
+++ Formula/typesafe-activator.rb
@@ -1,16 +1,23 @@
 class TypesafeActivator < Formula
-  desc "Tools for working with Typesafe Reactive Platform"
-  homepage "https://typesafe.com/activator"
-  url "https://downloads.typesafe.com/typesafe-activator/1.3.9/typesafe-activator-1.3.9-minimal.zip"
-  version "1.3.9"
-  sha256 "a418cdc7f204aca9cc8777df6d3a18c1bae1157fd972d60d135fce43e217cd64"
+  desc "Tools for working with Lightbend Reactive Platform"
+  homepage "https://www.lightbend.com/community/core-tools/activator-and-sbt"
+  url "https://downloads.typesafe.com/typesafe-activator/1.3.10/typesafe-activator-1.3.10-minimal.zip"
+  version "1.3.10"
+  sha256 "15352ce253aa804f707ef8be86390ee1ee91da4b78dbb2729ab1e9cae01d8937"
 
   bottle :unneeded
+
+  depends_on :java => "1.8+"
 
   def install
     rm Dir["bin/*.bat"] # Remove Windows .bat files
     libexec.install Dir["libexec/*"]
     bin.install Dir["bin/*"]
     chmod 0755, bin/"activator"
+  end
+
+  test do
+    out = shell_output("#{bin}/activator -help", 1).split("\n")[0]
+    assert_equal "Usage: activator [options]", out
   end
 end
