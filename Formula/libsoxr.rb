693f9859315603d2636b6f7bc70988bc788364ef
s172262 <s172262@users.noreply.github.com>
2016-05-01 20:57:15 +0900

    libsoxr 1.1.2
    Closes #743.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/libsoxr.rb
+++ Formula/libsoxr.rb
@@ -1,9 +1,9 @@
 class Libsoxr < Formula
   desc "High quality, one-dimensional sample-rate conversion library"
   homepage "https://sourceforge.net/projects/soxr/"
-  url "https://downloads.sourceforge.net/project/soxr/soxr-0.1.1-Source.tar.xz"
-  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/libs/libsoxr/libsoxr_0.1.1.orig.tar.xz"
-  sha256 "dcc16868d1a157079316f84233afcc2b52dd0bd541dd8439dc25bceb306faac2"
+  url "https://downloads.sourceforge.net/project/soxr/soxr-0.1.2-Source.tar.xz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/libs/libsoxr/libsoxr_0.1.2.orig.tar.xz"
+  sha256 "54e6f434f1c491388cd92f0e3c47f1ade082cc24327bdc43762f7d1eefe0c275"
 
   bottle do
     cellar :any
