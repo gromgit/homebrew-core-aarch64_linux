449c92c167d7f2bf65f1333594bca7b6046c9350
Martin Schumann <martinlschumann@gmail.com>
2016-05-02 14:56:32 +0200

    ocaml 4.03.0
    Added flambda support, as per avsm's suggestion

--- Formula/ocaml.rb
+++ Formula/ocaml.rb
@@ -17,8 +17,8 @@ class Ocaml < Formula
   head "http://caml.inria.fr/svn/ocaml/trunk", :using => :svn
 
   stable do
-    url "http://caml.inria.fr/pub/distrib/ocaml-4.02/ocaml-4.02.3.tar.gz"
-    sha256 "928fb5f64f4e141980ba567ff57b62d8dc7b951b58be9590ffb1be2172887a72"
+    url "http://caml.inria.fr/pub/distrib/ocaml-4.03/ocaml-4.03.0.tar.gz"
+    sha256 "7fdf280cc6c0a2de4fc9891d0bf4633ea417046ece619f011fd44540fcfc8da2"
   end
 
   bottle do
@@ -30,6 +30,7 @@ class Ocaml < Formula
   end
 
   option "with-x11", "Install with the Graphics module"
+  option "with-flambda", "Install with flambda support"
 
   depends_on :x11 => :optional
 
@@ -37,8 +38,9 @@ class Ocaml < Formula
     ENV.deparallelize # Builds are not parallel-safe, esp. with many cores
 
     # the ./configure in this package is NOT a GNU autoconf script!
-    args = ["-prefix", "#{HOMEBREW_PREFIX}", "-with-debug-runtime", "-mandir", man]
+    args = ["-prefix", HOMEBREW_PREFIX.to_s, "-with-debug-runtime", "-mandir", man]
     args << "-no-graph" if build.without? "x11"
+    args << "-flambda" if build.with? "flambda"
     system "./configure", *args
 
     system "make", "world.opt"
@@ -47,6 +49,6 @@ class Ocaml < Formula
 
   test do
     assert_match "val x : int = 1", shell_output("echo 'let x = 1 ;;' | ocaml 2>&1")
-    assert_match "#{HOMEBREW_PREFIX}", shell_output("ocamlc -where")
+    assert_match HOMEBREW_PREFIX.to_s, shell_output("ocamlc -where")
   end
 end
