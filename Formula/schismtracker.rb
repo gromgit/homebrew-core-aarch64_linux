470e7aabade01e56cc0ae4a36e19e258c4b035ca
Viktor Szakats <vszakats@users.noreply.github.com>
2016-05-02 22:39:47 +0200

    schismtracker: update urls, depend on automake
    Closes #783.
    
    Signed-off-by: Martin Afanasjew <martin@afanasjew.de>

--- Formula/schismtracker.rb
+++ Formula/schismtracker.rb
@@ -1,8 +1,9 @@
 class Schismtracker < Formula
   desc "Portable reimplementation of Impulse Tracker"
-  homepage "http://schismtracker.org"
-  url "http://schismtracker.org/dl/schismtracker-20120105.tar.bz2"
-  sha256 "31cef23e6806027618aef03675a5e4681043a99afa6e9e92d82ad4cec5e6c7db"
+  homepage "http://schismtracker.org/"
+  url "https://bitbucket.org/Storlek/schismtracker/get/20120105.tar.bz2"
+  sha256 "736163682c476c2f1afca9b3ebf2a1f5d1a0420dc2da12f2d8cfe6d1b3de379b"
+  head "https://bitbucket.org/Storlek/schismtracker", :using => :hg
 
   bottle do
     cellar :any
@@ -11,23 +12,19 @@ class Schismtracker < Formula
     sha256 "f892d60e3e74f352d7d9efbbf84b6470e4e751ffafe55c9b6ab9913bd611dba2" => :mavericks
   end
 
-  head do
-    url "http://schismtracker.org/hg/", :using => :hg
-
-    depends_on "autoconf" => :build
-  end
-
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "sdl"
 
   # CC BY-NC-ND licensed set of five mods by Keith Baylis/Vim! for testing purposes
-  # Mods from Mod Soul Brother: http://web.archive.org/web/20120215215707/http://www.mono211.com/modsoulbrother/vim.html
+  # Mods from Mod Soul Brother: https://web.archive.org/web/20120215215707/http://www.mono211.com/modsoulbrother/vim.html
   resource "demo_mods" do
     url "https://files.scene.org/get:us-http/mirrors/modsoulbrother/vim/vim-best-of.zip"
     sha256 "df8fca29ba116b10485ad4908cea518e0f688850b2117b75355ed1f1db31f580"
   end
 
   def install
-    system "autoreconf", "-ivf" if build.head?
+    system "autoreconf", "-ivf"
 
     mkdir "build" do
       # Makefile fails to create this directory before dropping files in it
