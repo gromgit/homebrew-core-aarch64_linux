3bf8825634b9dbdc49fce9c822eedaaf7feda1b4
Viktor Szakats <vszakats@users.noreply.github.com>
2016-05-02 10:55:05 +0200

    conserver: use secure homepage (redirected to insecure)

--- Formula/conserver.rb
+++ Formula/conserver.rb
@@ -1,7 +1,7 @@
 class Conserver < Formula
   desc "Allows multiple users to watch a serial console at the same time"
-  homepage "http://conserver.com"
-  url "http://conserver.com/conserver-8.2.1.tar.gz"
+  homepage "https://www.conserver.com/"
+  url "http://www.conserver.com/conserver-8.2.1.tar.gz"
   sha256 "251ae01997e8f3ee75106a5b84ec6f2a8eb5ff2f8092438eba34384a615153d0"
 
   bottle do
