60eb33f7456dd7c605a8ca56929c98ddd05d8545
Andrew Janke <andrew@apjanke.net>
2016-05-03 00:03:58 -0400

    no-more-secrets: tweak quote formatting

--- Formula/no-more-secrets.rb
+++ Formula/no-more-secrets.rb
@@ -1,5 +1,5 @@
 class NoMoreSecrets < Formula
-  desc 'Recreates the SETEC ASTRONOMY effect from "Sneakers"'
+  desc "Recreates the SETEC ASTRONOMY effect from 'Sneakers'"
   homepage "https://github.com/bartobri/no-more-secrets"
   url "https://github.com/bartobri/no-more-secrets/archive/v0.1.0.tar.gz"
   sha256 "9e42f359bda578716d176245d416b5924ed0b321390a28221467301bc10e537b"
