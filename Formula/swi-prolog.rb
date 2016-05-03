925c04ff7fdc4e189408343f2bac4410dd21b37d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-03 02:48:38 +0100

    swi-prolog: add 7.2.3 bottle.

--- Formula/swi-prolog.rb
+++ Formula/swi-prolog.rb
@@ -4,6 +4,12 @@ class SwiProlog < Formula
   url "http://www.swi-prolog.org/download/stable/src/swipl-7.2.3.tar.gz"
   sha256 "43657d51b7c5887bc2d2bced50a9822b86a08a6841399b8e76ee877f51d646b5"
 
+  bottle do
+    sha256 "9466d3335c7675046a2ef8e5886a2c3eb852a6d1e13b3bc064e76b9a9a775b29" => :el_capitan
+    sha256 "b6dbf92ebdc159642b9e4e8d5e7689d2fdb4bd51ee106f325c85d97885b7b40e" => :yosemite
+    sha256 "50ba51c36829d3a39c3bcda5e842175199d216d2a8e6ec2c11adc91a656dc5f0" => :mavericks
+  end
+
   devel do
     url "http://www.swi-prolog.org/download/devel/src/swipl-7.3.19.tar.gz"
     sha256 "693cfeac8f31d050ddcec27e550586359e98a64dc820398997d0ce6b8f564354"
