b70839c1ce89d18cf4eb1b9044f4e87fe29b0daa
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-03 21:17:29 +0100

    cfssl: update 1.2.0_2 bottle.

--- Formula/cfssl.rb
+++ Formula/cfssl.rb
@@ -9,9 +9,10 @@ class Cfssl < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b53506ab12658b30195f8bbd09b72c51e4d730a35cd303cb2b8c344e1aaa2830" => :el_capitan
-    sha256 "cbd93af7c030d90139ad751d417a14a2255dae413a92d9270fdd4c93782ce066" => :yosemite
-    sha256 "8e5346797e6b452e40e2e5434406c20d2a2dc8a411b8916268a6624c8cbe340d" => :mavericks
+    revision 1
+    sha256 "0d71b15ea2122abb7b50ca92ca0ef29d6e92faec00a7358cd4fc9212bcd8f917" => :el_capitan
+    sha256 "d53c26a753af41df8638eac07f7ae4c6bef90dd8304c2fbd03d4afed4a52b9a9" => :yosemite
+    sha256 "c351aac8d82c5484d60379be71a1a6e65f8cde09e3d28356087cdc2619aa2de7" => :mavericks
   end
 
   depends_on "go" => :build
