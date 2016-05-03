9bc1603c2f1fb99d3f3ee6e006a1dcaaf1acbaa4
Misty De Meo <mistydemeo@github.com>
2016-05-03 10:48:12 -0700

    imagemagick: disable vulnerable coders
    Closes #817.
    
    Signed-off-by: Misty De Meo <mistydemeo@github.com>

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -8,6 +8,8 @@ class Imagemagick < Formula
   mirror "https://www.imagemagick.org/download/ImageMagick-6.9.3-7.tar.xz"
   sha256 "6731c414b5b939713a73a088840ed68c22c91d1335514d228d6687d07ce2e1c8"
 
+  revision 1
+
   head "http://git.imagemagick.org/repos/ImageMagick.git"
 
   bottle do
@@ -55,6 +57,14 @@ class Imagemagick < Formula
   needs :openmp if build.with? "openmp"
 
   skip_clean :la
+
+  # Disables vulnerable coders: https://medium.com/@rhuber/imagemagick-is-on-fire-cve-2016-3714-379faf762247#.2tjfb3iks
+  # Next release will probably have a patch for the coders themselves,
+  # allowing us to remove this workaround.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/patches/ca3940923286cc1f763848eccbef6dcfd3e5fe1c/imagemagick/disable-coders.diff"
+    sha256 "b5950e047cdcc3a787c91bbdfbc0a76537f1c8febeb9c054768c5c7325ddf409"
+  end
 
   def install
     args = %W[
