eb8a4c5d3d8a7b929854a771d9b401f387a6598a
ilovezfs <ilovezfs@icloud.com>
2016-05-04 12:22:22 -0700

    hqx: revision for devil
    Also, add a test.
    
    Closes #846.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/hqx.rb
+++ Formula/hqx.rb
@@ -3,6 +3,7 @@ class Hqx < Formula
   homepage "https://code.google.com/p/hqx/"
   url "https://hqx.googlecode.com/files/hqx-1.1.tar.gz"
   sha256 "cc18f571fb4bc325317892e39ecd5711c4901831926bc93296de9ebb7b2f317b"
+  revision 1
 
   depends_on "devil"
 
@@ -11,5 +12,22 @@ class Hqx < Formula
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  test do
+    system bin/"hqx", test_fixtures("test.jpg"), "out.jpg"
+    output = pipe_output("php -r \"print_r(getimagesize(\'file://#{testpath}/out.jpg\'));\"")
+    assert_equal <<-EOS.undent, output
+    \tArray
+    \t(
+    \t    [0] => 4
+    \t    [1] => 4
+    \t    [2] => 2
+    \t    [3] => width="4" height="4"
+    \t    [bits] => 8
+    \t    [channels] => 3
+    \t    [mime] => image/jpeg
+    \t)
+    EOS
   end
 end
