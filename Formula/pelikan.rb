3c9d3623b15a48092580e2988433809d75e7305b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-04 02:02:13 +0100

    pelikan: add 0.1.1 bottle.

--- Formula/pelikan.rb
+++ Formula/pelikan.rb
@@ -5,6 +5,13 @@ class Pelikan < Formula
   sha256 "24a84bd9be7bb25da507a355b7ed3a35f7cb2a8c9447092f5acc91ff32f98775"
   head "https://github.com/twitter/pelikan.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "469116bf1661a4fc3fed1473bcf9619836226e14a6459d83ed67f5eed1c968ee" => :el_capitan
+    sha256 "b7370736251899a21560a4cd3d84d103c0f8ce11afa66a2663104e70f72837ad" => :yosemite
+    sha256 "654d1adc30a7585c7fc590006fd14124d8341eb9608ae88eeebb629e20003fd6" => :mavericks
+  end
+
   depends_on "cmake" => :build
 
   def install
