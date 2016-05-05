1fbd34c2aa5d91bc637a351cad4d89268f348796
Martin Schumann <martinlschumann@gmail.com>
2016-05-05 20:57:59 +0200

    opam 1.2.2
    Incremented revision to force rebuild
    
    Closes #776.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/opam.rb
+++ Formula/opam.rb
@@ -3,7 +3,7 @@ class Opam < Formula
   homepage "https://opam.ocaml.org"
   url "https://github.com/ocaml/opam/archive/1.2.2.tar.gz"
   sha256 "3e4a05df6ff8deecba019d885ebe902eb933acb6e2fc7784ffee1ee14871e36a"
-  revision 1
+  revision 2
   head "https://github.com/ocaml/opam.git"
 
   bottle do
