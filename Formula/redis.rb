fd6334888376145686efeac47166f2466f4d5aec
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-06 20:42:01 +0100

    redis: update 3.2.0 bottle.

--- Formula/redis.rb
+++ Formula/redis.rb
@@ -7,10 +7,9 @@ class Redis < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
-    sha256 "56aa3ed148606d6f4d0fa7a4f2efec2263f4d166949d5732836a133be589493b" => :el_capitan
-    sha256 "931dbc56104eced22a13c9f6ac314fd808132295984bcf9973c9f6dafda0f245" => :yosemite
-    sha256 "4be758b78e35b4a30b3d6e97255774f928de19332520447429afc64b7ec8cf41" => :mavericks
+    sha256 "ef4328987e9d881a4a1179bef440ee2b40f44bc31797517bd54e51169d542055" => :el_capitan
+    sha256 "3389d5eb619d82b85f628499baeb46da19d23cdb2d3469d855715008f0c743f8" => :yosemite
+    sha256 "aede9c4e91230c9b18953215d87a9be77076ad7d77866c865a5c1d8e239e96fc" => :mavericks
   end
 
   option "with-jemalloc", "Select jemalloc as memory allocator when building Redis"
