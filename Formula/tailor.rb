5eb41d7e40484ceec686d209321c22069f3a5344
Sleekbot <team@sleekbyte.com>
2016-05-06 11:15:03 -0400

    tailor 0.10.0

--- Formula/tailor.rb
+++ Formula/tailor.rb
@@ -1,8 +1,8 @@
 class Tailor < Formula
   desc "Cross-platform static analyzer and linter for Swift"
   homepage "https://tailor.sh"
-  url "https://github.com/sleekbyte/tailor/releases/download/v0.9.1/tailor-0.9.1.tar"
-  sha256 "6dff0e5ce1badaaa4b3440332a06e9a883a9e81535fca5e84f4e6e04eab1f1e7"
+  url "https://github.com/sleekbyte/tailor/releases/download/v0.10.0/tailor-0.10.0.tar"
+  sha256 "4a93db14c60d788e818de7af52c6ef492da5b20f02f23552c43ba1eee02d9b75"
 
   bottle :unneeded
 
