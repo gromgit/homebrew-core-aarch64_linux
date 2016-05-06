b845b90a2ba51b8f5cfc0f80b8dfce0701f61196
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-07 00:27:28 +0100

    platformio: update 2.9.1 bottle.

--- Formula/platformio.rb
+++ Formula/platformio.rb
@@ -6,9 +6,9 @@ class Platformio < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a424dd4ed6cab22405c38c19aa1abf2f58e43ace6da6f379606965489a06166a" => :el_capitan
-    sha256 "9077194af92cbd44a1abd1ec2117286549fb5a8aad441684bfe4351162239b0c" => :yosemite
-    sha256 "7d9de6261aa70184524957bdd896323f633c7c719be655beea4ae844593040ef" => :mavericks
+    sha256 "3688cbd0a83fadaaf51b5f304020acb1485839571305fe0c01ad3de2f65890e0" => :el_capitan
+    sha256 "9b2416040ec6508b695c2bb757a1d8d67bf3daface13f626dbd5165501a1a747" => :yosemite
+    sha256 "cf0e416e64dceb12b61bb97328d8865cfd3b4ab90a49cdc862013da895b266ef" => :mavericks
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
