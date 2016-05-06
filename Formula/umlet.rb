c3cf670e1c6d59b4cba1342ffa3a0742e1a4b6e1
Uwe Barthel <Uwe.Barthel@icongmbh.de>
2016-05-06 08:13:45 +0200

    umlet 14.2 (new formula)
    UMLet is a UML tool aimed at providing a fast way of creating UML
    diagrams.
    
    Closes #900.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Formula/umlet.rb
@@ -0,0 +1,33 @@
+class Umlet < Formula
+  desc "This UML tool aimed at providing a fast way of creating UML diagrams."
+  homepage "http://www.umlet.com/"
+  url "http://www.umlet.com/umlet_14_2/umlet-standalone-14.2.zip"
+  sha256 "dc09538d04cb899218f3bdfdb5205f60359cff68cb1428d1228c6d91743d8cb9"
+
+  bottle :unneeded
+
+  depends_on :java => "1.6+"
+
+  def install
+    rm Dir["*.{desktop,exe}"]
+
+    libexec.install Dir["*"]
+
+    inreplace "#{libexec}/umlet.sh", " java ", " ${JAVA_HOME}/bin/java "
+
+    inreplace "#{libexec}/umlet.sh", /^programDir=.*$/,
+      "programDir=#{libexec}"
+
+    chmod 0755, "#{libexec}/umlet.sh"
+
+    (bin/"umlet-#{version}").write_env_script "#{libexec}/umlet.sh",
+      Language::Java.java_home_env("1.6+")
+    bin.install_symlink "umlet-#{version}" => "umlet"
+  end
+
+  test do
+    system "#{bin}/umlet", "-action=convert", "-format=png",
+      "-output=#{testpath}/test-output.png",
+      "-filename=#{libexec}/palettes/Plots.uxf"
+  end
+end
