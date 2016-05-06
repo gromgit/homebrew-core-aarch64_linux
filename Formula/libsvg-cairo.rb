323e30b40a51c72f9a96e2a3107d5f03a9f2d1a7
Viktor Szakats <vszakats@users.noreply.github.com>
2016-05-06 23:54:28 +0200

    libsvg-cairo: use secure urls (#921)

--- Formula/libsvg-cairo.rb
+++ Formula/libsvg-cairo.rb
@@ -1,7 +1,7 @@
 class LibsvgCairo < Formula
   desc "SVG rendering library using Cairo"
-  homepage "http://cairographics.org/"
-  url "http://cairographics.org/snapshots/libsvg-cairo-0.1.6.tar.gz"
+  homepage "https://cairographics.org/"
+  url "https://cairographics.org/snapshots/libsvg-cairo-0.1.6.tar.gz"
   sha256 "a380be6a78ec2938100ce904363815a94068fca372c666b8cc82aa8711a0215c"
   revision 1
 
