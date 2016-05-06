a6b99a561cf09867c7613a6fdc937b53cc72ba65
Andrew Janke <andrew@apjanke.net>
2016-05-06 12:12:25 -0400

    civl: remove cvc4 dependency
    cvc4 is being boneyarded.
    civl can still run with just z3, though with reduced precsion.
    
    Closes #909.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/civl.rb
+++ Formula/civl.rb
@@ -4,12 +4,12 @@ class Civl < Formula
   url "http://vsl.cis.udel.edu/lib/sw/civl/1.7/r3157/release/CIVL-1.7_3157.tgz"
   version "1.7-3157"
   sha256 "33fa5b4877fbf494b0a4b426c15d6a165fa3db48a9f97cbf391818d965d2dfb9"
+  revision 1
 
   bottle :unneeded
 
   depends_on :java => "1.8+"
   depends_on "z3"
-  depends_on "cvc4"
 
   def install
     libexec.install "lib/civl-1.7_3157.jar"
