590ff003acd02eb6fd6d8fd3d214d4fad3eb8e19
Viktor Szakats <vszakats@users.noreply.github.com>
2016-05-06 23:54:13 +0200

    libsvg: use secure urls (#919)

--- Formula/libsvg.rb
+++ Formula/libsvg.rb
@@ -1,7 +1,7 @@
 class Libsvg < Formula
   desc "Library for SVG files"
-  homepage "http://cairographics.org/"
-  url "http://cairographics.org/snapshots/libsvg-0.1.4.tar.gz"
+  homepage "https://cairographics.org/"
+  url "https://cairographics.org/snapshots/libsvg-0.1.4.tar.gz"
   sha256 "4c3bf9292e676a72b12338691be64d0f38cd7f2ea5e8b67fbbf45f1ed404bc8f"
   revision 1
 
