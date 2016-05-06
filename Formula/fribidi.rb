8347cb4ecd5d62b8d73634c19dc27f87b974bb7b
Viktor Szakats <vszakats@users.noreply.github.com>
2016-05-06 21:22:50 +0200

    fribidi: use secure urls

--- Formula/fribidi.rb
+++ Formula/fribidi.rb
@@ -1,7 +1,7 @@
 class Fribidi < Formula
   desc "Implementation of the Unicode BiDi algorithm"
-  homepage "http://fribidi.org/"
-  url "http://fribidi.org/download/fribidi-0.19.7.tar.bz2"
+  homepage "https://fribidi.org/"
+  url "https://fribidi.org/download/fribidi-0.19.7.tar.bz2"
   sha256 "08222a6212bbc2276a2d55c3bf370109ae4a35b689acbc66571ad2a670595a8e"
 
   bottle do
