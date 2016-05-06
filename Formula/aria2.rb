58ced73112e11aec9766ce6e0cb89b5053d40c4f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-06 04:14:19 +0100

    aria2: update 1.22.0 bottle.

--- Formula/aria2.rb
+++ Formula/aria2.rb
@@ -6,9 +6,9 @@ class Aria2 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "724b7aa74bd3e7cd470162adf27ab5bd1d1369a16108ddd5def2dc1ab99fac95" => :el_capitan
-    sha256 "83b3f974bc53c3b59f8bf32fc84c855e24e794e0e0350f7477b4773e6e6a2fa0" => :yosemite
-    sha256 "8fc117fbdf3470cf3be39573b0553728538742a45776dc6a3dbfedef30b430e1" => :mavericks
+    sha256 "86774f3165e8d4faf05bf5741141f5f6187668f84d0b1a99491eb2e2ed743813" => :el_capitan
+    sha256 "97cbe9e0c08eec1318c3b25ea8b1ae0edd675c60d7c2b6d2cb1e011a4c399834" => :yosemite
+    sha256 "13efe8ff0822b98cccce73a84f84588bfe899d11d8272c9d02cc2d6228e7ea9b" => :mavericks
   end
 
   depends_on "pkg-config" => :build
