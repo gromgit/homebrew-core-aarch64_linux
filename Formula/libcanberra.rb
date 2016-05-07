232c7dd217cdb13728b35fe2f3d895621af8e7d5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-07 11:22:25 +0100

    libcanberra: add 0.30 bottle.

--- Formula/libcanberra.rb
+++ Formula/libcanberra.rb
@@ -8,6 +8,12 @@ class Libcanberra < Formula
 
     depends_on "gtk-doc" => :optional
   end
+  bottle do
+    sha256 "dc9f8e76c81bb21afedd4deb9b9d9bbf3382f012192e5796658845e2083ca018" => :el_capitan
+    sha256 "3d820f8c9747e8658c226483267536c76b3dfdf44a9e14ea1327cb5cf0385ba8" => :yosemite
+    sha256 "10c6f3c931a349848b6436d10893e9b1bc5051658f125f6b67ddc1f9319396a0" => :mavericks
+  end
+
 
   head do
     url "git://git.0pointer.de/libcanberra"
