d09b135b941578df836e5a3697230edd7f3dff17
Dominyk Tiller <dominyktiller@gmail.com>
2016-05-07 20:39:25 +0100

    vorbis-tools: bump revision for libnetwork linkage
    Also add a test.
    
    Should fix the issue raised in https://github.com/Homebrew/homebrew-core/pull/912#issuecomment-217649623, hopefully.
    
    Closes #964.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/vorbis-tools.rb
+++ Formula/vorbis-tools.rb
@@ -3,6 +3,7 @@ class VorbisTools < Formula
   homepage "http://vorbis.com"
   url "http://downloads.xiph.org/releases/vorbis/vorbis-tools-1.4.0.tar.gz"
   sha256 "a389395baa43f8e5a796c99daf62397e435a7e73531c9f44d9084055a05d22bc"
+  revision 1
 
   bottle do
     revision 1
@@ -18,16 +19,23 @@ class VorbisTools < Formula
   depends_on "flac" => :optional
 
   def install
-    args = [
-      "--disable-debug",
-      "--disable-dependency-tracking",
-      "--disable-nls",
-      "--prefix=#{prefix}"
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-nls
+      --prefix=#{prefix}
     ]
 
     args << "--without-flac" if build.without? "flac"
 
     system "./configure", *args
     system "make", "install"
+  end
+
+  test do
+    system bin/"oggenc", test_fixtures("test.wav"), "-o", "test.ogg"
+    assert File.exist?("test.ogg")
+    output = shell_output("#{bin}/ogginfo test.ogg")
+    assert_match "20.625000 kb/s", output
   end
 end
