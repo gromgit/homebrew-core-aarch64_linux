e3a3b65449d67bbd2f34db33c054686d87d528d4
Geoff Nixon <geoff@geoff.codes>
2016-05-07 02:01:46 -0700

    libarchive 3.2.0
    Closes #952.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/libarchive.rb
+++ Formula/libarchive.rb
@@ -1,9 +1,9 @@
 class Libarchive < Formula
   desc "Multi-format archive and compression library"
   homepage "http://www.libarchive.org"
-  url "http://www.libarchive.org/downloads/libarchive-3.1.2.tar.gz"
-  mirror "https://github.com/libarchive/libarchive/archive/v3.1.2.tar.gz"
-  sha256 "eb87eacd8fe49e8d90c8fdc189813023ccc319c5e752b01fb6ad0cc7b2c53d5e"
+  url "http://www.libarchive.org/downloads/libarchive-3.2.0.tar.gz"
+  mirror "https://github.com/libarchive/libarchive/archive/v3.2.0.tar.gz"
+  sha256 "7bce45fd71ff01dc20d19edd78322d4965583d81b8bed8e26cacb65d6f5baa87"
 
   bottle do
     cellar :any
@@ -16,14 +16,26 @@ class Libarchive < Formula
 
   keg_only :provided_by_osx
 
-  depends_on "xz" => :optional
+  depends_on "xz" => :recommended
+  depends_on "lz4" => :optional
+  depends_on "lzop" => :optional
 
   def install
-    system "./configure", "--prefix=#{prefix}",
-                          "--without-lzo2",
-                          "--without-nettle",
-                          "--without-xml2"
+    system "./configure",
+           "--prefix=#{prefix}",
+           "--without-lzo2",    # Use lzop binary instead of lzo2 due to GPL
+           "--without-nettle",  # xar hashing option but GPLv3
+           "--without-xml2",    # xar hashing option but tricky dependencies
+           "--without-openssl", # mtree hashing now possible without OpenSSL
+           "--with-expat"       # best xar hashing option
+
     system "make", "install"
+
+    # Just as apple does it.
+    ln_s bin/"bsdtar", bin/"tar"
+    ln_s bin/"bsdcpio", bin/"cpio"
+    ln_s man1/"bsdtar.1", man1/"tar.1"
+    ln_s man1/"bsdcpio.1", man1/"cpio.1"
   end
 
   test do
