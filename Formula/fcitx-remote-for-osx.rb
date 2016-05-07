4ba71b161e901fd0dc068a2fbae7b4b21f079a18
codefalling <code.falling@gmail.com>
2016-05-07 15:53:52 +0800

    fcitx-remote-for-osx 0.1.0
    update fcitx-remote-for-osx to 0.1.0
    
    Closes #948.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/fcitx-remote-for-osx.rb
+++ Formula/fcitx-remote-for-osx.rb
@@ -1,20 +1,13 @@
 class FcitxRemoteForOsx < Formula
   desc "handle input method in command-line"
   homepage "https://github.com/CodeFalling/fcitx-remote-for-osx"
-  url "https://github.com/CodeFalling/fcitx-remote-for-osx/archive/0.0.2.tar.gz"
-  sha256 "746505694090d98d5ad572758e7597c49d3bea1b00208a8272c3648aad15ad69"
+  url "https://github.com/CodeFalling/fcitx-remote-for-osx/archive/0.1.0.tar.gz"
+  sha256 "0a4233175e7dfbaaa0ba308b54552503e814c001c6366f28cce7f9bcae3abebb"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "1f2e9c699eacc33980431176f93b47650c840af3dec8e8c1ca20be038a2d9e03" => :el_capitan
-    sha256 "98bccbcc7fbab5f151549ca94f6167538733c5197d388bad340e38dd0a21fef3" => :yosemite
-    sha256 "c7f9b8e28c380c8b325ceb85511b0d51f340bc871cb5d779f2b51157eaafc57f" => :mavericks
-  end
-
-  option "with-input-method=", "Select input method: baidu-pinyin(default), baidu-wubi, sogou-pinyin, qq-wubi, squirrel-rime, osx-pinyin"
+  option "with-input-method=", "Select input method: general(default), baidu-pinyin, baidu-wubi, sogou-pinyin, qq-wubi, squirrel-rime, osx-pinyin"
 
   def install
-    input_method = ARGV.value("with-input-method") || "baidu-pinyin"
+    input_method = ARGV.value("with-input-method") || "general"
     system "./build.py", "build", input_method
     bin.install "fcitx-remote-#{input_method}"
     bin.install_symlink "fcitx-remote-#{input_method}" => "fcitx-remote"
