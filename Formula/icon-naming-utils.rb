2c96f339133529eb82baba6be0d0340715a16907
Dominyk Tiller <dominyktiller@gmail.com>
2016-05-07 04:35:30 +0100

    icon-naming-utils: use Debian mirror temporarily
    Closes #939.

--- Formula/icon-naming-utils.rb
+++ Formula/icon-naming-utils.rb
@@ -1,7 +1,11 @@
 class IconNamingUtils < Formula
   desc "Script to handle icon names in desktop icon themes"
   homepage "https://specifications.freedesktop.org/icon-naming-spec/icon-naming-spec-latest.html"
-  url "http://tango.freedesktop.org/releases/icon-naming-utils-0.8.90.tar.gz"
+  # Upstream seem to have enabled by default SSL/TLS across whole domain which
+  # is problematic when the cert is for www rather than a wildcard or similar.
+  # url "http://tango.freedesktop.org/releases/icon-naming-utils-0.8.90.tar.gz"
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/i/icon-naming-utils/icon-naming-utils_0.8.90.orig.tar.gz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/i/icon-naming-utils/icon-naming-utils_0.8.90.orig.tar.gz"
   sha256 "044ab2199ed8c6a55ce36fd4fcd8b8021a5e21f5bab028c0a7cdcf52a5902e1c"
 
   bottle do
