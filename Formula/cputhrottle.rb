86c24d3871799ad4066f83159f4a0b3ceacaba1a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-07 19:09:22 +0100

    cputhrottle: update 20100515_1 bottle.

--- Formula/cputhrottle.rb
+++ Formula/cputhrottle.rb
@@ -7,11 +7,9 @@ class Cputhrottle < Formula
   revision 1
 
   bottle do
-    cellar :any_skip_relocation
-    revision 1
-    sha256 "44be3076a501f8a5ff481e2433528c912496544c2932b1a75ee104b152370386" => :el_capitan
-    sha256 "820ba7f42568e30b05492a7a525f69ab916fb8f46dd6d8a367671892434b3f04" => :yosemite
-    sha256 "c1d43f7acfc4faa75fd02fa9e76224c542198f7455c394095452963c223d8172" => :mavericks
+    sha256 "4f80fe27e2a64468ab6613f526552003adb11125db9ac3aa7aee1c5d70d86653" => :el_capitan
+    sha256 "5196a098d92dbf7c9f1ed5c5a7350bbcdbdc07e0292565cf7729a3bb7de5f972" => :yosemite
+    sha256 "6df740581ec759d149a2f456da278361fcff522718799b5a24ca03762f8908b2" => :mavericks
   end
 
   depends_on "boost" => :build
