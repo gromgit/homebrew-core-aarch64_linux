b7f71db4686ad9af3a13eb921ebecf294e0cdaa4
uyjulian <uyjulian@gmail.com>
2016-05-07 20:17:35 -0500

    winetricks 20160425 (#965)

--- Formula/winetricks.rb
+++ Formula/winetricks.rb
@@ -1,8 +1,8 @@
 class Winetricks < Formula
   desc "Download and install various runtime libraries"
   homepage "https://github.com/Winetricks/winetricks"
-  url "https://github.com/Winetricks/winetricks/archive/20160219.tar.gz"
-  sha256 "ce399bb8c644ff276d97b2e9f97190ab4bc27a07016ad75483e3c750b806bc1c"
+  url "https://github.com/Winetricks/winetricks/archive/20160425.tar.gz"
+  sha256 "0ddd2d896ab3803057f0eeec3afd6fd2f1b8ebb21e4ec0647c66e3cad0da1d0e"
   head "https://github.com/Winetricks/winetricks.git"
 
   bottle :unneeded
