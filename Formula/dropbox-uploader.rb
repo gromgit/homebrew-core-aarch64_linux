1f587df9be166b1466d5e1fe6d17a78f0b621c75
Viktor Szakats <vszakats@users.noreply.github.com>
2016-05-08 12:59:52 +0200

    dropbox-uploader: use secure homepage

--- Formula/dropbox-uploader.rb
+++ Formula/dropbox-uploader.rb
@@ -1,6 +1,6 @@
 class DropboxUploader < Formula
   desc "Bash script for interacting with Dropbox"
-  homepage "http://www.andreafabrizi.it/?dropbox_uploader"
+  homepage "https://www.andreafabrizi.it/?dropbox_uploader"
   url "https://github.com/andreafabrizi/Dropbox-Uploader/archive/0.16.tar.gz"
   sha256 "738aebdb57f75e9033cb9bb319b14fe1c3b471f951180acb48631e9810ebf1c3"
 
