2b490a81e34b031fad88e2bb6b8bf6104d91fd86
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-11 15:38:14 +0100

    yafc: update 1.3.7 bottle.

--- Formula/yafc.rb
+++ Formula/yafc.rb
@@ -5,10 +5,9 @@ class Yafc < Formula
   sha256 "4b3ebf62423f21bdaa2449b66d15e8d0bb04215472cb63a31d473c3c3912c1e0"
 
   bottle do
-    revision 2
-    sha256 "85e490aee51701da0f7602b9d88700f18b6b2077dcae1f17c5c479132dd78d2e" => :el_capitan
-    sha256 "3f5fc5f56cce04404d49d82173d4aea09b3fdfd3dec7b3699ea106b947a6df2a" => :yosemite
-    sha256 "5f178c01235c5dd28bd359eb295a4a0aa848649c63a0a0386f43516d32b66bd2" => :mavericks
+    sha256 "7665d10fc6dd4cc6b7bced9145af75f6f9403b48b80b8579bbd0035cb13d6495" => :el_capitan
+    sha256 "d485826eef7aa06d918a3d9c928f22b417a8fe4dc252002dd2487f9899179abe" => :yosemite
+    sha256 "e382b1c32cea95f505242cda54555965772b00f37d8b251e86a0cb05be112b2e" => :mavericks
   end
 
   depends_on "pkg-config" => :build
