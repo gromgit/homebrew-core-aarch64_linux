62585dd92a2dfc5b9abb69f3c5e7c4aa1a3ed8f1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-12 13:01:31 +0100

    file-roller: update 3.20.2 bottle.

--- Formula/file-roller.rb
+++ Formula/file-roller.rb
@@ -5,10 +5,9 @@ class FileRoller < Formula
   sha256 "93188a7ac9285cb85551c327082aeaeb51ac39a9722cb96b0e29d5ec2ae353c6"
 
   bottle do
-    revision 1
-    sha256 "6ae889f74188bbc0063d4dec3aa433709af988c5056f42f1e03cb8272a12b8da" => :el_capitan
-    sha256 "660296eb74711e2c8a90ed8e2083585398fc166c452cc2f925fab6877cf7484f" => :yosemite
-    sha256 "05ca1ccdd5771e574bf4b1bad58fa0ddc6d2db65d2fda7999912ced5e0cb704e" => :mavericks
+    sha256 "c3a021b0f45d4a9f15ba04761fa9789bbec8b47ae2ee5501316003ce5a756451" => :el_capitan
+    sha256 "3c85ae1f3273a240c1ac4963b41f4807db644523e6df0a444c33e8a18d4b5549" => :yosemite
+    sha256 "7955d920d08916cb076e4e26d75f475d2e6b93f7d225ba435a377cba562e4046" => :mavericks
   end
 
   depends_on "pkg-config" => :build
