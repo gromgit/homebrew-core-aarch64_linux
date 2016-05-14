810d4469eae98e7917cfb3d40aff78338286c13f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-14 15:54:55 +0100

    wiredtiger: add 2.8.0 bottle.

--- Formula/wiredtiger.rb
+++ Formula/wiredtiger.rb
@@ -4,6 +4,13 @@ class Wiredtiger < Formula
   url "https://github.com/wiredtiger/wiredtiger/releases/download/2.8.0/wiredtiger-2.8.0.tar.bz2"
   sha256 "593e4858a21465db6f8360cc6281489f8a114fa301bd2753c0bde9a86ef107e2"
 
+  bottle do
+    cellar :any
+    sha256 "a5c3fd2444a26a657a4e8a1e84a9f8fbbe0388601b9b855b8b0b2c3075e1d1fd" => :el_capitan
+    sha256 "19ea6e84544c58a232b2a1231f038fd4a09e516e29fd761be815bdd0a3f06c30" => :yosemite
+    sha256 "e12d809b163ef92fbac6ec22f3083675716b3f83c55dc35d1b7ca902bb723fd7" => :mavericks
+  end
+
   depends_on "snappy"
 
   def install
