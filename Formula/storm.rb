f17ee01dd92984cc953e0021149b36d2e6ca6cc9
Jerzerak <jerzerak@gmail.com>
2016-05-14 00:41:55 -0400

    storm 1.0.1
    Closes #1148.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/storm.rb
+++ Formula/storm.rb
@@ -1,8 +1,8 @@
 class Storm < Formula
   desc "Distributed realtime computation system to process data streams"
   homepage "https://storm.apache.org"
-  url "https://www.apache.org/dyn/closer.cgi?path=storm/apache-storm-1.0.0/apache-storm-1.0.0.tar.gz"
-  sha256 "8233dab1a6898fc39bcf9fe68dfadbee38159a39c473ac2da9ce9da60db52066"
+  url "https://www.apache.org/dyn/closer.cgi?path=storm/apache-storm-1.0.1/apache-storm-1.0.1.tar.gz"
+  sha256 "1574c08d8cfb6bc7509c78871e98a535f1386d6ca50fa71c880c39e8800620bf"
 
   bottle :unneeded
 
