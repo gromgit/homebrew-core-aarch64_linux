949c91519d11781fb745d585d4dcaefbca618217
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-14 05:55:24 +0100

    trafficserver: update 6.1.1 bottle.

--- Formula/trafficserver.rb
+++ Formula/trafficserver.rb
@@ -6,10 +6,9 @@ class Trafficserver < Formula
   sha256 "67ddd7fc79e4435f353b2aa8937a7b205f217ca15beba3adb5213a92f9527d8b"
 
   bottle do
-    revision 3
-    sha256 "790009af9f33df827e87ba0f704852ddb72f994c7a981403c2f0793a36105214" => :el_capitan
-    sha256 "f54117573d34ce77b0a325db2af58ee83d8f7b86f22eb319b7b088c1a471c34b" => :yosemite
-    sha256 "1a9445b60e8aa891fa0666d9b5f4620d399d361558cc810307d8f2877a2dc05c" => :mavericks
+    sha256 "c8fa833d763a36c2f80112b062bae83c8a91873a5296cacd9894c6134f8170a6" => :el_capitan
+    sha256 "f3205d51a3cca216b838cd41e4e455d0c3efb082672651eaccc1a84149afeb67" => :yosemite
+    sha256 "80b1e7d13bae8b827599000a718ec659a559f494b379aa0d250a99eb1a60e6a4" => :mavericks
   end
 
   head do
