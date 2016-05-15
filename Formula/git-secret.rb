267855050325ca3e0ec11154bf4f3baf5360812a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-15 17:43:01 +0100

    git-secret: add 0.1.2 bottle.

--- Formula/git-secret.rb
+++ Formula/git-secret.rb
@@ -5,6 +5,13 @@ class GitSecret < Formula
   sha256 "96032c1c0b2b161c4df5b8627be8d98a97a92a5d32127c9a5e8686bd64b5a3e7"
   head "https://github.com/sobolevn/git-secret.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9844a5567d90039e43c24228d50cfe033aefd1196bfdc30b6a7986bc6cf4581d" => :el_capitan
+    sha256 "57f69c45ca7d665f9a6610d4212530f946ad8effbd1f806280b3f98f2e880ee8" => :yosemite
+    sha256 "620a145f092f79a5109f2f61f555f0d2319344b8c60d09639da37b1813ce592a" => :mavericks
+  end
+
   depends_on :gpg => :recommended
 
   def install
