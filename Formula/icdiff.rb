5ab9c3d22bb945e8b94d3ad2bdbfc732be3c568b
Chris Daish <chrisdaish@gmail.com>
2016-05-16 16:02:15 +0100

    icdiff 1.8.1
    Closes #1192.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/icdiff.rb
+++ Formula/icdiff.rb
@@ -1,8 +1,8 @@
 class Icdiff < Formula
   desc "Improved colored diff"
   homepage "https://github.com/jeffkaufman/icdiff"
-  url "https://github.com/jeffkaufman/icdiff/archive/release-1.7.3.tar.gz"
-  sha256 "5161265f72a7c9c1d2d7b0780a381743ef3d3127944a96786422802a6bc14ca5"
+  url "https://github.com/jeffkaufman/icdiff/archive/release-1.8.1.tar.gz"
+  sha256 "57a2f1164e9cce98e44cba35473203a19034e919a69762589779f54f4612d8f7"
 
   bottle do
     cellar :any_skip_relocation
