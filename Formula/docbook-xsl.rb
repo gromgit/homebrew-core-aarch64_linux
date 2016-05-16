4bc4d1576518edf92ba2e39c52ceedca5650f23c
Ivo Clarysse <ivo@bcdevices.com>
2016-05-16 13:21:59 -0700

    docbook-xsl 1.79.1
    Closes #1206.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/docbook-xsl.rb
+++ Formula/docbook-xsl.rb
@@ -1,9 +1,8 @@
 class DocbookXsl < Formula
   desc "XML vocabulary to create presentation-neutral documents"
   homepage "http://docbook.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/docbook/docbook-xsl/1.78.1/docbook-xsl-1.78.1.tar.bz2"
-  sha256 "c98f7296ab5c8ccd2e0bc07634976a37f50847df2d8a59bdb1e157664700b467"
-  revision 1
+  url "https://downloads.sourceforge.net/project/docbook/docbook-xsl/1.79.1/docbook-xsl-1.79.1.tar.bz2"
+  sha256 "725f452e12b296956e8bfb876ccece71eeecdd14b94f667f3ed9091761a4a968"
 
   bottle do
     cellar :any_skip_relocation
@@ -15,8 +14,8 @@ class DocbookXsl < Formula
   depends_on "docbook"
 
   resource "ns" do
-    url "https://downloads.sourceforge.net/project/docbook/docbook-xsl-ns/1.78.1/docbook-xsl-ns-1.78.1.tar.bz2"
-    sha256 "cf8ede7284d7f825c24b95ea273551439c55e9af9a4209ac89e3a7d915607af4"
+    url "https://downloads.sourceforge.net/project/docbook/docbook-xsl-ns/1.79.1/docbook-xsl-ns-1.79.1.tar.bz2"
+    sha256 "36ca9026e05b8985baebd61a23af8ded8e2cf71cc3163b673159c9d78a7b0f9c"
   end
 
   def install
@@ -28,7 +27,7 @@ class DocbookXsl < Formula
                    website xhtml xhtml-1_1 xhtml5]
     (prefix/"docbook-xsl").install xsl_files + doc_files
     resource("ns").stage do
-      (prefix/"docbook-xsl-ns").install xsl_files + doc_files + ["README.ns"]
+      (prefix/"docbook-xsl-ns").install xsl_files + doc_files
     end
 
     bin.write_exec_script "#{prefix}/docbook-xsl/epub/bin/dbtoepub"
@@ -40,5 +39,10 @@ class DocbookXsl < Formula
       system "xmlcatalog", "--noout", "--del", "file://#{catalog}", "#{etc}/xml/catalog"
       system "xmlcatalog", "--noout", "--add", "nextCatalog", "", "file://#{catalog}", "#{etc}/xml/catalog"
     end
+  end
+
+  test do
+    system "xmlcatalog", "#{etc}/xml/catalog", "http://docbook.sourceforge.net/release/xsl/snapshot_9899/"
+    system "xmlcatalog", "#{etc}/xml/catalog", "http://docbook.sourceforge.net/release/xsl-ns/1.79.1/"
   end
 end
