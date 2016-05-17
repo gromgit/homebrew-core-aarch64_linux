3c60fb06f42d1ca002bfced6df06c52287545c78
Nick Sieger <nicksieger@gmail.com>
2016-05-17 13:54:57 -0500

    brew-gem 0.7.0

--- Formula/brew-gem.rb
+++ Formula/brew-gem.rb
@@ -1,12 +1,19 @@
 class BrewGem < Formula
   desc "Install rubygems as homebrew formulae"
   homepage "https://github.com/sportngin/brew-gem"
-  url "https://github.com/sportngin/brew-gem/archive/v0.6.1.tar.gz"
-  sha256 "b3dd161d5fa6a643d97b08cad514f0c8fdba8b8f44ff80a70f6a5090cf02cdfd"
+  url "https://github.com/sportngin/brew-gem/archive/v0.7.0.tar.gz"
+  sha256 "34f5999fc2c0a99118e0419f89be35a6e42ad9abad99e1c13e34d9162121b541"
+
+  head "https://github.com/sportngin/brew-gem.git"
 
   bottle :unneeded
 
   def install
+    lib.install Dir["lib/*"]
     bin.install "bin/brew-gem"
+  end
+
+  test do
+    system "#{bin}/brew-gem", "help"
   end
 end
