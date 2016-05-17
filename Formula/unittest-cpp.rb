20db273744e0dc9ef400f8e4fdfb21aaf67cd5fc
Dominyk Tiller <dominyktiller@gmail.com>
2016-05-17 19:00:38 +0100

    unittest-cpp: tweak test
    Closes #1236.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/unittest-cpp.rb
+++ Formula/unittest-cpp.rb
@@ -3,7 +3,6 @@ class UnittestCpp < Formula
   homepage "https://github.com/unittest-cpp/unittest-cpp"
   url "https://github.com/unittest-cpp/unittest-cpp/releases/download/v1.6.1/unittest-cpp-1.6.1.tar.gz"
   sha256 "5b00a217f90fe262d91d09405ef5a8b5d28063da2f701cc58af48d560c4792af"
-  head "https://github.com/unittest-cpp/unittest-cpp.git"
 
   bottle do
     cellar :any
@@ -12,12 +11,20 @@ class UnittestCpp < Formula
     sha256 "67f3d01e1d608e9f27d230e8e2bb35851248a69d526f120e78b13d9fdc2874b4" => :mavericks
   end
 
+  head do
+    url "https://github.com/unittest-cpp/unittest-cpp.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   def install
+    system "autoreconf", "-fvi" if build.head?
     system "./configure", "--prefix=#{prefix}", "--disable-silent-rules"
     system "make", "install"
   end
 
   test do
-    assert_match version.to_s, shell_output("cat /usr/local/lib/pkgconfig/UnitTest++.pc | grep Version: | cut -d' ' -f2")
+    assert_match version.to_s, File.read(lib/"pkgconfig/UnitTest++.pc")
   end
 end
