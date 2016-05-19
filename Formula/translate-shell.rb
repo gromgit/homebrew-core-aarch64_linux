74a6aed4cb59220533c8f88825f010fa05060d7c
Zhiming Wang <zmwangx@gmail.com>
2016-05-19 13:18:54 -0700

    translate-shell 0.9.4
    https://github.com/soimort/translate-shell/releases/tag/v0.9.4
    
    Closes #1301.
    
    Signed-off-by: Martin Afanasjew <martin@afanasjew.de>

--- Formula/translate-shell.rb
+++ Formula/translate-shell.rb
@@ -1,18 +1,9 @@
 class TranslateShell < Formula
   desc "Command-line translator using Google Translate and more"
   homepage "https://www.soimort.org/translate-shell"
+  url "https://github.com/soimort/translate-shell/archive/v0.9.4.tar.gz"
+  sha256 "bfc04124d2fde7924e6b5c3a11fdce7fbd2fdb1819c0b78c3fd0a7d36e330164"
   head "https://github.com/soimort/translate-shell.git", :branch => "develop"
-  revision 1
-
-  stable do
-    url "https://github.com/soimort/translate-shell/archive/v0.9.3.2.tar.gz"
-    sha256 "4ddf7292802f6d81a8e9c736a3ff854ebcc193866e9774376dc0c2f8d893323a"
-
-    patch do
-      url "https://github.com/soimort/translate-shell/commit/c5f0744006e8dff78852cffae64837addff8c26b.diff"
-      sha256 "83aaa2b7182ac918bdc662a33cbf6faae12bd2a39e7567d5f4d32a0b3fcd527b"
-    end
-  end
 
   bottle do
     cellar :any_skip_relocation
