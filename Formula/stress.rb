27f024ef0f3978861b93c00572cc2a1bb64244df
Viktor Szakats <vszakats@users.noreply.github.com>
2016-05-19 03:23:20 +0200

    stress: use secure urls
    Closes #1279.
    
    Signed-off-by: Martin Afanasjew <martin@afanasjew.de>

--- Formula/stress.rb
+++ Formula/stress.rb
@@ -1,7 +1,7 @@
 class Stress < Formula
   desc "Tool to impose load on and stress test a computer system"
-  homepage "http://people.seas.harvard.edu/~apw/stress/"
-  url "http://people.seas.harvard.edu/~apw/stress/stress-1.0.4.tar.gz"
+  homepage "https://people.seas.harvard.edu/~apw/stress/"
+  url "https://people.seas.harvard.edu/~apw/stress/stress-1.0.4.tar.gz"
   mirror "https://mirrors.kernel.org/debian/pool/main/s/stress/stress_1.0.4.orig.tar.gz"
   sha256 "057e4fc2a7706411e1014bf172e4f94b63a12f18412378fca8684ca92408825b"
 
