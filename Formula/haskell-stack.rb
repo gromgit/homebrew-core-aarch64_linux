ee703a58c4c4c428c1a25c96e13f6ded2d7153fd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-21 00:06:28 +0100

    haskell-stack: update 1.1.2 bottle.

--- Formula/haskell-stack.rb
+++ Formula/haskell-stack.rb
@@ -11,9 +11,9 @@ class HaskellStack < Formula
   head "https://github.com/commercialhaskell/stack.git"
 
   bottle do
-    sha256 "74a78e41852fae739720fe03771837ad91fc547fd3f8a1138ff6f4d1ceacffbf" => :el_capitan
-    sha256 "435b9a5198e66396b66b9525956296caec01a62c40f236fb6e999d1b2ad83c69" => :yosemite
-    sha256 "f3f984dcf53a6a4084d62cf7c7e9d9735c960b39b6bcc106212b5292c1750071" => :mavericks
+    sha256 "648a91a315843c34ff42aac347e368b66df1689c3a7439ab36e5c369259e4a86" => :el_capitan
+    sha256 "5fa124ca2ca69a8ebf08312d663e1da9649ba9ab9d2810c844da809615a65e68" => :yosemite
+    sha256 "267a9daf5ffc8da11ff45484339f1fa286a516af55110e17be3e8c5a0b71bac9" => :mavericks
   end
 
   depends_on "ghc" => :build
