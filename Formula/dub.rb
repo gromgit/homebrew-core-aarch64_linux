0f3b5201f5955640a2f29f15e7d72ead9cf751b3
Cedric Meury <cedric@meury.com>
2016-05-24 11:05:50 +0200

    dub 0.9.25
    Closes #1378.
    
    Signed-off-by: Martin Afanasjew <martin@afanasjew.de>

--- Formula/dub.rb
+++ Formula/dub.rb
@@ -1,20 +1,13 @@
 class Dub < Formula
   desc "Build tool for D projects"
   homepage "https://code.dlang.org/about"
-  url "https://github.com/D-Programming-Language/dub/archive/v0.9.24.tar.gz"
-  sha256 "88fe9ff507d47cb74af685ad234158426219b7fdd7609de016fc6f5199def866"
-  head "https://github.com/D-Programming-Language/dub.git", :shallow => false
+  url "https://github.com/dlang/dub.git", :tag => "v0.9.25", :revision => "777e0033e9ca502c134a6cca6b5a06d0e3f78617"
+  head "https://github.com/dlang/dub.git", :shallow => false
 
   bottle do
     sha256 "bf14b900869d28bc8140731ee81d04d9ee5b456603dea51353863bd76358f49d" => :el_capitan
     sha256 "5cdd5f8c6729f3acf955afbd8d383daf196318bf1d2278085a28c28af00d33ce" => :yosemite
     sha256 "33db147c048a39cad51569940ff489e015a08f3d17d0c299efcce89c064a8513" => :mavericks
-  end
-
-  devel do
-    url "https://github.com/rejectedsoftware/dub/archive/v0.9.25-rc.1.tar.gz"
-    sha256 "577e127e5e361d1ed7fe6ef40100e91e361dd7063f304afeab7ae5a6274005de"
-    version "0.9.25-rc.1"
   end
 
   depends_on "pkg-config" => :build
