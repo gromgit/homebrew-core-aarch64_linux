951c08b2b24153f2d9b4bb59d35900642eb66aa8
Josh Tilles <MerelyAPseudonym@gmail.com>
2016-05-25 16:41:07 -0400

    ffind 1.0.0 (#1412)

--- Formula/ffind.rb
+++ Formula/ffind.rb
@@ -1,8 +1,8 @@
 class Ffind < Formula
   desc "Friendlier find"
   homepage "https://github.com/sjl/friendly-find"
-  url "https://github.com/sjl/friendly-find/archive/v0.3.2.tar.gz"
-  sha256 "4fe9b5fb4d64cc3a006b1496cf0a825a2494706c413778b77bbcf0fb9cfbbf80"
+  url "https://github.com/sjl/friendly-find/archive/v1.0.0.tar.gz"
+  sha256 "2eca8563bb77bd4bae84b3f0cbe104c0289698b29fbe22d67d007dc36af5f700"
 
   bottle :unneeded
 
