d86b8a819978366e9496e1148054e0c2827bfb29
Jordan Gwyn <j@ravellaw.com>
2016-05-25 13:54:01 -0700

    ammonite-repl 0.5.8 (#1393)

--- Formula/ammonite-repl.rb
+++ Formula/ammonite-repl.rb
@@ -1,16 +1,16 @@
 class AmmoniteRepl < Formula
   desc "Ammonite is a cleanroom re-implementation of the Scala REPL"
   homepage "https://lihaoyi.github.io/Ammonite/#Ammonite-REPL"
-  url "https://git.io/vVfy5", :using => :nounzip
-  version "0.5.7"
-  sha256 "ef410dc843391c6b3199f2552a24f5fe3f3c79a7558d813089c8dce8b4ab97b6"
+  url "https://github.com/lihaoyi/Ammonite/releases/download/0.5.8/ammonite-repl-0.5.8-2.11.8", :using => :nounzip
+  version "0.5.8"
+  sha256 "1bcaf9eee84d662f8c24396b9b8fa117afc7933c1c8c74d7650b3f358b77ee5b"
 
   bottle :unneeded
 
   depends_on :java => "1.7+"
 
   def install
-    bin.install "vVfy5" => "amm"
+    bin.install "ammonite-repl-0.5.8-2.11.8" => "amm"
   end
 
   test do
