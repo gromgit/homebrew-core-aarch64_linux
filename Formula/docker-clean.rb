232f64d359790e24e8359fc7c732a758bb9949eb
Killian Brackey <brack107@umn.edu>
2016-05-25 22:23:22 -0500

    docker-clean 2.0.4 (#1421)

--- Formula/docker-clean.rb
+++ Formula/docker-clean.rb
@@ -1,8 +1,8 @@
 class DockerClean < Formula
   desc "Clean Docker containers, images, networks, and volumes"
   homepage "https://github.com/ZZROTDesign/docker-clean"
-  url "https://github.com/ZZROTDesign/docker-clean/archive/v2.0.3.tar.gz"
-  sha256 "a2e1923fe2862ca2487e39000c68cba900711b768a8920dd1b1275a2fa0e6789"
+  url "https://github.com/ZZROTDesign/docker-clean/archive/v2.0.4.tar.gz"
+  sha256 "4b636fd7391358b60c05b65ba7e89d27eaf8dd56cc516f3c786b59cadac52740"
 
   bottle :unneeded
 
