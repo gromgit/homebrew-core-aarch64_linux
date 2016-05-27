55f867a9a5ab27b52b3cc2d1181555325f69248e
Jerzerak <jerzerak@gmail.com>
2016-05-27 17:38:25 -0400

    gradle 2.14-rc-2 (devel)
    Closes #1494.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/gradle.rb
+++ Formula/gradle.rb
@@ -5,9 +5,9 @@ class Gradle < Formula
   sha256 "0f665ec6a5a67865faf7ba0d825afb19c26705ea0597cec80dd191b0f2cbb664"
 
   devel do
-    url "https://downloads.gradle.org/distributions/gradle-2.14-rc-1-bin.zip"
-    sha256 "2ff85eb9eda60f13bc8debec69849e1e2a72b7bfc5af5d26e61a293b5dcbceba"
-    version "2.14-rc-1"
+    url "https://downloads.gradle.org/distributions/gradle-2.14-rc-2-bin.zip"
+    sha256 "eb824186223dec65fc3f9bb2755934b38c1cd005a0e6f0d80e282a2390214735"
+    version "2.14-rc-2"
   end
 
   bottle :unneeded
