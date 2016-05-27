528e50b6a712f64d91f4e1f79c5f3fd780d0627d
ilovezfs <ilovezfs@icloud.com>
2016-05-27 02:53:05 -0700

    purescript 0.9.0 (devel)
    Add devel and head specs.
    
    Closes #1474.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/purescript.rb
+++ Formula/purescript.rb
@@ -7,11 +7,17 @@ class Purescript < Formula
   homepage "http://www.purescript.org"
   url "https://github.com/purescript/purescript/archive/v0.8.5.tar.gz"
   sha256 "352c0c311710907d112e5d2745e7b152adc4d7b23aff3f069c463eceedddec17"
+  head "https://github.com/purescript/purescript.git"
 
   bottle do
     sha256 "9d9cf746058847a06cfbfdb7fb3a74c9a8c7eaf7dd1edafc604dfb720cdb8c83" => :el_capitan
     sha256 "4e526d5e657750476c3b85f5e2574ac353ecd1afe8b48baba598002b34f9e960" => :yosemite
     sha256 "9b0a0f6273917d86c9039025e92da4f88a74b678eac0810ae3cb27ae10746b77" => :mavericks
+  end
+
+  devel do
+    url "https://github.com/purescript/purescript/archive/v0.9.0.tar.gz"
+    sha256 "3c08582cacb909fbf2d2db7e7681afa14efd105d2e47ad93998377b273b853b8"
   end
 
   depends_on "ghc" => :build
