3752729656bbf5ca52f51d7c85ba1674bf6a6ec1
Jerzerak <jerzerak@gmail.com>
2016-05-27 15:50:22 -0400

    awscli 1.10.34
    - Amend pypi resource url's
    - `botocore` 1.4.24
    
    Closes #1490.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -1,8 +1,8 @@
 class Awscli < Formula
   desc "Official Amazon AWS command-line interface"
   homepage "https://aws.amazon.com/cli/"
-  url "https://pypi.python.org/packages/06/e8/d89e03662d513ff088f3c84ff2d1a05b4a1e145022c46022e8519a666c27/awscli-1.10.33.tar.gz"
-  sha256 "67538c4b61db6766b90cd3398cb7dcf26f3ab16b60d39b9e04dbadab1bb4b6ea"
+  url "https://pypi.python.org/packages/9a/c9/56c9c98c30bbe0115baea79cca303e7ee7d569987aaac34d822fbae95fe0/awscli-1.10.34.tar.gz"
+  sha256 "79b1cce1db0187b09fe8af2ace45155369d3d65f1de42e1cf73763e93a0bb812"
 
   bottle do
     cellar :any_skip_relocation
@@ -33,7 +33,7 @@ class Awscli < Formula
   depends_on :python if MacOS.version <= :lion
 
   resource "six" do
-    url "https://pypi.python.org/packages/source/s/six/six-1.10.0.tar.gz"
+    url "https://pypi.python.org/packages/b3/b2/238e2590826bfdd113244a40d9d3eb26918bd798fc187e2360a8367068db/six-1.10.0.tar.gz"
     sha256 "105f8d68616f8248e24bf0e9372ef04d3cc10104f1980f54d57b2ce73a5ad56a"
   end
 
@@ -43,37 +43,37 @@ class Awscli < Formula
   end
 
   resource "colorama" do
-    url "https://pypi.python.org/packages/source/c/colorama/colorama-0.3.3.tar.gz"
+    url "https://pypi.python.org/packages/24/84/29ce4167d1f5c4a320aaad91e1178e5a1baf9cfe1c63f9077c5dade0e3cc/colorama-0.3.3.tar.gz"
     sha256 "eb21f2ba718fbf357afdfdf6f641ab393901c7ca8d9f37edd0bee4806ffa269c"
   end
 
   resource "botocore" do
-    url "https://pypi.python.org/packages/c0/66/96b4d266787d6a28bad77650e69dc3f02945d71cba1e33dc698d7c3d7b74/botocore-1.4.23.tar.gz"
-    sha256 "88523db485decd00dd98b04ba01d956aee4806036fc8c82b5d244130f0b3150e"
+    url "https://pypi.python.org/packages/0d/b4/cf63a4ca94af097219d11147e9cbc9f5b4a23487e210cf526dbdb2eafc55/botocore-1.4.24.tar.gz"
+    sha256 "5261c804edbb4bb9422768fa0a46b7daeca3665f3ff664ab86fe0fe1d3413a46"
   end
 
   resource "jmespath" do
-    url "https://pypi.python.org/packages/source/j/jmespath/jmespath-0.9.0.tar.gz"
+    url "https://pypi.python.org/packages/8f/d8/6e3e602a3e90c5e3961d3d159540df6b2ff32f5ab2ee8ee1d28235a425c1/jmespath-0.9.0.tar.gz"
     sha256 "08dfaa06d4397f283a01e57089f3360e3b52b5b9da91a70e1fd91e9f0cdd3d3d"
   end
 
   resource "s3transfer" do
-    url "https://pypi.python.org/packages/source/s/s3transfer/s3transfer-0.0.1.tar.gz"
+    url "https://pypi.python.org/packages/6e/12/5d0ea478e6d261857a461af921b78f3bc6f92c479ffe57076f4fc9a362ab/s3transfer-0.0.1.tar.gz"
     sha256 "2bb9ed8db58af94dfa78f75f554d646dfe4b4741fc87f63a20c2bfb3f70f4355"
   end
 
   resource "futures" do
-    url "https://pypi.python.org/packages/source/f/futures/futures-3.0.5.tar.gz"
+    url "https://pypi.python.org/packages/55/db/97c1ca37edab586a1ae03d6892b6633d8eaa23b23ac40c7e5bbc55423c78/futures-3.0.5.tar.gz"
     sha256 "0542525145d5afc984c88f914a0c85c77527f65946617edb5274f72406f981df"
   end
 
   resource "docutils" do
-    url "https://pypi.python.org/packages/source/d/docutils/docutils-0.12.tar.gz"
+    url "https://pypi.python.org/packages/37/38/ceda70135b9144d84884ae2fc5886c6baac4edea39550f28bcd144c1234d/docutils-0.12.tar.gz"
     sha256 "c7db717810ab6965f66c8cf0398a98c9d8df982da39b4cd7f162911eb89596fa"
   end
 
   resource "pyasn1" do
-    url "https://pypi.python.org/packages/source/p/pyasn1/pyasn1-0.1.9.tar.gz"
+    url "https://pypi.python.org/packages/f7/83/377e3dd2e95f9020dbd0dfd3c47aaa7deebe3c68d3857a4e51917146ae8b/pyasn1-0.1.9.tar.gz"
     sha256 "853cacd96d1f701ddd67aa03ecc05f51890135b7262e922710112f12a2ed2a7f"
   end
 
@@ -83,7 +83,7 @@ class Awscli < Formula
   end
 
   resource "tox" do
-    url "https://pypi.python.org/packages/source/t/tox/tox-2.3.1.tar.gz"
+    url "https://pypi.python.org/packages/46/39/e15a857fda1852da1485bc88ac4268dbcef037ab526e1ac21accf2a5c24c/tox-2.3.1.tar.gz"
     sha256 "bf7fcc140863820700d3ccd65b33820ba747b61c5fe4e2b91bb8c64cb21a47ee"
   end
 
