973e9a487376aa30c9a289dc17da1f56228d4604
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-28 21:22:18 +0100

    asciiquarium: add 1.1 bottle.

--- Formula/asciiquarium.rb
+++ Formula/asciiquarium.rb
@@ -4,6 +4,13 @@ class Asciiquarium < Formula
   url "http://www.robobunny.com/projects/asciiquarium/asciiquarium_1.1.tar.gz"
   sha256 "1b08c6613525e75e87546f4e8984ab3b33f1e922080268c749f1777d56c9d361"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8303a8c1a413ba08d67b7f326663b6c2294fb537bfa18aa6f97a92c050fe0068" => :el_capitan
+    sha256 "15b9d47615685f3c32f8336b1c75274b7cb2e176f71c9bf2957328c529b58f6d" => :yosemite
+    sha256 "93e208857b2497ab81ff252f2186e5e6552930b82045f3b80671cf82471dce6c" => :mavericks
+  end
+
   resource "Curses" do
     url "https://cpan.metacpan.org/authors/id/G/GI/GIRAFFED/Curses-1.34.tar.gz"
     mirror "http://search.cpan.org/CPAN/authors/id/G/GI/GIRAFFED/Curses-1.34.tar.gz"
