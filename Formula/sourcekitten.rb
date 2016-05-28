d8c311385bef0519de759ecd76f58d3fc7954e3f
Dominyk Tiller <dominyktiller@gmail.com>
2016-05-28 21:11:08 +0100

    sourcekitten: small style nits

--- Formula/sourcekitten.rb
+++ Formula/sourcekitten.rb
@@ -1,7 +1,9 @@
 class Sourcekitten < Formula
   desc "Framework and command-line tool for interacting with SourceKit"
   homepage "https://github.com/jpsim/SourceKitten"
-  url "https://github.com/jpsim/SourceKitten.git", :tag => "0.12.2", :revision => "cb35dd8a3850b69bf612ef6fd7d5078123662bd6"
+  url "https://github.com/jpsim/SourceKitten.git",
+      :tag => "0.12.2",
+      :revision => "cb35dd8a3850b69bf612ef6fd7d5078123662bd6"
   head "https://github.com/jpsim/SourceKitten.git"
 
   bottle do
@@ -9,7 +11,7 @@ class Sourcekitten < Formula
     sha256 "7ae7965d52fd6fa215eb4dc7afa40f114d4dac6dbd2c21448e4026ba082da1f1" => :el_capitan
   end
 
-  depends_on :xcode => ["7.3"]
+  depends_on :xcode => "7.3"
 
   def install
     system "make", "prefix_install", "PREFIX=#{prefix}", "TEMPORARY_FOLDER=#{buildpath}/SourceKitten.dst"
