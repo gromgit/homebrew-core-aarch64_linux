2933b29f9fe0adea732a4b7f47c160685565a7dc
Justin Lecher <jlec@gentoo.org>
2016-05-30 19:34:00 +0100

    duply 1.11.3 (#1572)

--- Formula/duply.rb
+++ Formula/duply.rb
@@ -1,8 +1,8 @@
 class Duply < Formula
   desc "Frontend to the duplicity backup system"
   homepage "http://duply.net"
-  url "https://downloads.sourceforge.net/project/ftplicity/duply%20%28simple%20duplicity%29/1.11.x/duply_1.11.2.tgz"
-  sha256 "06843960ae5753395cb5be72a9c9f2fe78e6ea81a17c6d8d28f2ae49ab50d5aa"
+  url "https://downloads.sourceforge.net/project/ftplicity/duply%20%28simple%20duplicity%29/1.11.x/duply_1.11.3.tgz"
+  sha256 "585757a02abc91bcc0e37004eef735794d6581d1a30ec68303c6c04833b20940"
 
   bottle :unneeded
 
