8cd92ea9077b6f69fa4d53e28f6b9fe0730b7490
Zheng Ye <dreamhead.cn@gmail.com>
2016-06-02 03:58:09 +0800

    moco 0.11.0 (#1610)
    * Update moco to 0.11.0

--- Formula/moco.rb
+++ Formula/moco.rb
@@ -1,15 +1,15 @@
 class Moco < Formula
   desc "Stub server with Maven, Gradle, Scala, and shell integration"
   homepage "https://github.com/dreamhead/moco"
-  url "https://search.maven.org/remotecontent?filepath=com/github/dreamhead/moco-runner/0.10.2/moco-runner-0.10.2-standalone.jar"
-  version "0.10.2"
-  sha256 "ef946d090d3108843708c194809c57f192e3623dbbbcf86bdee54ce93c299a41"
+  url "https://search.maven.org/remotecontent?filepath=com/github/dreamhead/moco-runner/0.11.0/moco-runner-0.11.0-standalone.jar"
+  version "0.11.0"
+  sha256 "d3c772333f6a35fe4bc168d0541e97a0d36071afe343ced840f7afd1c037b661"
 
   bottle :unneeded
 
   def install
-    libexec.install "moco-runner-0.10.2-standalone.jar"
-    bin.write_jar_script libexec/"moco-runner-0.10.2-standalone.jar", "moco"
+    libexec.install "moco-runner-0.11.0-standalone.jar"
+    bin.write_jar_script libexec/"moco-runner-0.11.0-standalone.jar", "moco"
   end
 
   test do
@@ -28,7 +28,7 @@ class Moco < Formula
 
     port = 12306
     thread = Thread.new do
-      system bin/"moco", "start", "-p", port, "-c", testpath/"config.json"
+      system bin/"moco", "http", "-p", port, "-c", testpath/"config.json"
     end
 
     # Wait for Moco to start.
