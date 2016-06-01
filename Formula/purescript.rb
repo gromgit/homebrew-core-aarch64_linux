9905001dc7d67b8af0c097e70ff22ed04b8fa000
Srdan Srepfler <schrepfler@gmail.com>
2016-06-02 01:22:20 +0200

    purescript 0.9.1
    Closes #1642.
    
    Signed-off-by: Martin Afanasjew <martin@afanasjew.de>

--- Formula/purescript.rb
+++ Formula/purescript.rb
@@ -5,8 +5,8 @@ class Purescript < Formula
 
   desc "Strongly typed programming language that compiles to JavaScript"
   homepage "http://www.purescript.org"
-  url "https://github.com/purescript/purescript/archive/v0.8.5.tar.gz"
-  sha256 "352c0c311710907d112e5d2745e7b152adc4d7b23aff3f069c463eceedddec17"
+  url "https://github.com/purescript/purescript/archive/v0.9.1.tar.gz"
+  sha256 "ae5f565c1a5ce04cb3bb972be33855d2b932a1866fd8c21eb1f714692b951abc"
   head "https://github.com/purescript/purescript.git"
 
   bottle do
@@ -14,11 +14,6 @@ class Purescript < Formula
     sha256 "eb0c767c670a0c2667dce335f7c6779f217af76b74b9d2c7758bdbe5437be3e9" => :el_capitan
     sha256 "e310f8b86b303b509591f435cfe91b1e7edbd58cbdabe317eed748f817a630b8" => :yosemite
     sha256 "dccd44a886e5e8d8502fbc347399de854a1927607534c515a318e2e25a0e9b3a" => :mavericks
-  end
-
-  devel do
-    url "https://github.com/purescript/purescript/archive/v0.9.0.tar.gz"
-    sha256 "3c08582cacb909fbf2d2db7e7681afa14efd105d2e47ad93998377b273b853b8"
   end
 
   depends_on "ghc" => :build
