470d065faf8836146401e516924cffb7c51e0fbd
Martin Afanasjew <martin@afanasjew.de>
2016-06-02 02:08:04 +0200

    flyway: use canonical maven.org URL

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,7 +1,7 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/4.0.1/flyway-commandline-4.0.1.tar.gz"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/4.0.1/flyway-commandline-4.0.1.tar.gz"
   sha256 "d23d8e9a13d3f31b51385c8937adefc49b4b46bb61805b36026f8c26be5255d0"
 
   bottle :unneeded
