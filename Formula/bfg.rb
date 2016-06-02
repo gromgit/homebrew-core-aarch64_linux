b2db0fc380e7e22b8d4ffc80196bedc498add385
Martin Afanasjew <martin@afanasjew.de>
2016-06-02 02:07:36 +0200

    bfg: use canonical maven.org URL

--- Formula/bfg.rb
+++ Formula/bfg.rb
@@ -1,7 +1,7 @@
 class Bfg < Formula
   desc "Remove large files or passwords from Git history like git-filter-branch"
   homepage "https://rtyley.github.io/bfg-repo-cleaner/"
-  url "https://repo1.maven.org/maven2/com/madgag/bfg/1.12.12/bfg-1.12.12.jar"
+  url "https://search.maven.org/remotecontent?filepath=com/madgag/bfg/1.12.12/bfg-1.12.12.jar"
   sha256 "03291c9cd9cdc664104f49b22b76bd39edc73052936cbfd72fc8d8595971fd9a"
 
   bottle :unneeded
