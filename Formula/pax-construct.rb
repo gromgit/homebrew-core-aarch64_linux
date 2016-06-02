42205c9a3f41d0adebe9e8599fa2c9fc9aa1fbcf
Martin Afanasjew <martin@afanasjew.de>
2016-06-02 02:08:37 +0200

    pax-construct: use canonical maven.org URL

--- Formula/pax-construct.rb
+++ Formula/pax-construct.rb
@@ -1,7 +1,7 @@
 class PaxConstruct < Formula
   desc "Tools to setup and develop OSGi projects quickly"
   homepage "https://ops4j1.jira.com/wiki/display/paxconstruct/Pax+Construct"
-  url "https://repo1.maven.org/maven2/org/ops4j/pax/construct/scripts/1.5/scripts-1.5.zip"
+  url "https://search.maven.org/remotecontent?filepath=org/ops4j/pax/construct/scripts/1.5/scripts-1.5.zip"
   sha256 "d0325bbe571783097d4be782576569ea0a61529695c14e33a86bbebfe44859d1"
 
   bottle :unneeded
