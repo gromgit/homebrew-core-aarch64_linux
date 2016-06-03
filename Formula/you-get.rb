3566106d8aa651a7ac606a2c59dbe21e85c5af39
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-03 09:16:01 +0100

    you-get: add 0.4.424 bottle.

--- Formula/you-get.rb
+++ Formula/you-get.rb
@@ -5,6 +5,13 @@ class YouGet < Formula
   sha256 "d664fa7d1da6878c9f3f673e64506ee9593b7677a27b3fdd0b6e994b0f87072d"
   head "https://github.com/soimort/you-get.git", :branch => "develop"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "12d1f1a28cecb146535561a7ab06b30fa5b7afe817a1331df477c8ae8c674d9b" => :el_capitan
+    sha256 "5a39178566670a44f95ef400eac9ae5cd6320c00c68b39ba630a965776191e4c" => :yosemite
+    sha256 "6b3c2eaa88a62f0cb2d74c8952b4905888ecc97c0fcdeda8d51fd12a2af16025" => :mavericks
+  end
+
   depends_on :python3
 
   depends_on "rtmpdump" => :optional
