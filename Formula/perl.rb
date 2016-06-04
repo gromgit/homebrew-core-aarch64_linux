ad900729d1494f801b9d52a3848269c32976b5d7
Dominyk Tiller <dominyktiller@gmail.com>
2016-06-04 04:28:12 +0100

    perl: update caveats
    Improves the caveats, dropping the assumption everyone installing Perl is
    familiar enough with it to not be scared off by the somewhat brutal configuration
    output.

--- Formula/perl.rb
+++ Formula/perl.rb
@@ -52,8 +52,12 @@ class Perl < Formula
   end
 
   def caveats; <<-EOS.undent
-    By default Perl installs modules in your HOME dir. If this is an issue run:
-      `#{opt_bin}/cpan o conf init`
+    By default non-brewed cpan modules are installed to the Cellar. If you wish
+    for your modules to persist across updates we recommend using `local::lib`.
+
+    You can set that up like this:
+      PERL_MM_OPT="INSTALL_BASE=$HOME/perl5" cpan local::lib
+      echo 'eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)"' >> #{shell_profile}
     EOS
   end
 
