bfc1969506f39e782e4bcbb8d08315feda0dd81d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-05 23:05:49 +0100

    path-extractor: update 0.2.0 bottle.

--- Formula/path-extractor.rb
+++ Formula/path-extractor.rb
@@ -10,9 +10,9 @@ class PathExtractor < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "fc2d9e129562bdc7142c186d5e8c22b7b9bbf7bbd386d020e1804eec35b6faae" => :el_capitan
-    sha256 "cbb931f9afcb5178bb08ea6c09bd1a9ca9ea695e4279bf7b36461ff3eeb490f2" => :yosemite
-    sha256 "8afd140f08bdb53ac36ca4e221236cd58406ff16c136aa466c5a28c8c1829714" => :mavericks
+    sha256 "90521da4fd1834db41fbf19b7b6ce9f82a943ab2412acd41b6c5d749146770e7" => :el_capitan
+    sha256 "718512fe3585d82dee8d655c2ab534dac70d0b24a8164bcc012f0f2a65a55e5b" => :yosemite
+    sha256 "f883b0656efe0d31b35b98ab0c82d82f1fa827b39d3712136c49bae2363f539d" => :mavericks
   end
 
   depends_on "go" => :build
