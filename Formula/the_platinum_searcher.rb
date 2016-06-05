34b21daa5ea7de62c23d189e4707e335cbd693b1
ilovezfs <ilovezfs@icloud.com>
2016-06-05 14:44:19 -0700

    the_platinum_searcher 2.1.2
    Closes #1728.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/the_platinum_searcher.rb
+++ Formula/the_platinum_searcher.rb
@@ -3,8 +3,8 @@ require "language/go"
 class ThePlatinumSearcher < Formula
   desc "Multi-platform code-search similar to ack and ag"
   homepage "https://github.com/monochromegane/the_platinum_searcher"
-  url "https://github.com/monochromegane/the_platinum_searcher/archive/v2.1.1.tar.gz"
-  sha256 "627e01d6c80ea4c6e1406bb8a498640b0fd5ac9eef37c53e132c868c7f261c96"
+  url "https://github.com/monochromegane/the_platinum_searcher/archive/v2.1.2.tar.gz"
+  sha256 "db8cbe30381e7f0a6cdb4b8ab306194483df16197b268790b6d8cee3f8d32d05"
   head "https://github.com/monochromegane/the_platinum_searcher.git"
 
   bottle do
@@ -16,49 +16,49 @@ class ThePlatinumSearcher < Formula
 
   depends_on "go" => :build
 
-  go_resource "github.com/jessevdk/go-flags" do
-    url "https://github.com/jessevdk/go-flags.git",
-        :revision => "6b9493b3cb60367edd942144879646604089e3f7"
-  end
-
-  go_resource "github.com/monochromegane/terminal" do
-    url "https://github.com/monochromegane/terminal.git",
-        :revision => "2da212063ce19aed90ee5bbb00ad1ad7393d7f48"
-  end
-
-  go_resource "github.com/shiena/ansicolor" do
-    url "https://github.com/shiena/ansicolor.git",
-        :revision => "691ac7d4ac14053de3cbe16e07b79246300db97d"
-  end
-
-  go_resource "golang.org/x/text" do
-    url "https://github.com/golang/text.git",
-        :revision => "02704b6b714738b763ba478766eb55a4b4851cd4"
-  end
-
   go_resource "github.com/BurntSushi/toml" do
     url "https://github.com/BurntSushi/toml.git",
-        :revision => "bbd5bb678321a0d6e58f1099321dfa73391c1b6f"
+    :revision => "f0aeabca5a127c4078abb8c8d64298b147264b55"
+  end
+
+  go_resource "github.com/jessevdk/go-flags" do
+    url "https://github.com/jessevdk/go-flags.git",
+    :revision => "b9b882a3990882b05e02765f5df2cd3ad02874ee"
   end
 
   go_resource "github.com/monochromegane/conflag" do
     url "https://github.com/monochromegane/conflag.git",
-        :revision => "6d68c9aa4183844ddc1655481798fe4d90d483e9"
+    :revision => "6d68c9aa4183844ddc1655481798fe4d90d483e9"
   end
 
   go_resource "github.com/monochromegane/go-gitignore" do
     url "https://github.com/monochromegane/go-gitignore.git",
-        :revision => "38717d0a108ca0e5af632cd6845ca77d45b50729"
+    :revision => "38717d0a108ca0e5af632cd6845ca77d45b50729"
   end
 
   go_resource "github.com/monochromegane/go-home" do
     url "https://github.com/monochromegane/go-home.git",
-        :revision => "25d9dda593924a11ea52e4ffbc8abdb0dbe96401"
+    :revision => "25d9dda593924a11ea52e4ffbc8abdb0dbe96401"
+  end
+
+  go_resource "github.com/monochromegane/terminal" do
+    url "https://github.com/monochromegane/terminal.git",
+    :revision => "2da212063ce19aed90ee5bbb00ad1ad7393d7f48"
+  end
+
+  go_resource "github.com/shiena/ansicolor" do
+    url "https://github.com/shiena/ansicolor.git",
+    :revision => "a422bbe96644373c5753384a59d678f7d261ff10"
+  end
+
+  go_resource "golang.org/x/text" do
+    url "https://go.googlesource.com/text.git",
+    :revision => "da88cb54a2fe4ec041612bd04b321d6a79d3ea81"
   end
 
   go_resource "gopkg.in/yaml.v2" do
     url "https://gopkg.in/yaml.v2.git",
-        :revision => "a83829b6f1293c91addabc89d0571c246397bbf4"
+    :revision => "a83829b6f1293c91addabc89d0571c246397bbf4"
   end
 
   def install
