5093abb03f6243614d3d07be721fe8c8b8dfcd1a
Dominyk Tiller <dominyktiller@gmail.com>
2016-06-07 00:21:45 +0100

    pulledpork: follow move to GitHub
    New home as linked on the Snort download page.

--- Formula/pulledpork.rb
+++ Formula/pulledpork.rb
@@ -1,9 +1,9 @@
 class Pulledpork < Formula
   desc "Snort rule management"
-  homepage "https://code.google.com/p/pulledpork/"
+  homepage "https://github.com/shirkdog/pulledpork"
   url "https://pulledpork.googlecode.com/files/pulledpork-0.7.0.tar.gz"
   sha256 "f60c005043850bb65a72582b9d6d68a7e7d51107f30f2b3fc67e607c995aa1a8"
-  head "http://pulledpork.googlecode.com/svn/trunk/"
+  head "https://github.com/shirkdog/pulledpork.git"
 
   bottle :unneeded
 
