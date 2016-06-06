ee2bad43655ca97228e210df56a29b231b61d000
Olivier Halligon <olivier@halligon.net>
2016-06-06 13:10:14 +0200

    swiftgen 1.1.0
    Closes #1744.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/swiftgen.rb
+++ Formula/swiftgen.rb
@@ -1,8 +1,8 @@
 class Swiftgen < Formula
   desc "Collection of Swift tools to generate Swift code"
   homepage "https://github.com/AliSoftware/SwiftGen"
-  url "https://github.com/AliSoftware/SwiftGen/archive/1.0.0.tar.gz"
-  sha256 "336ff7ae873e15869ca9952a33ad0974f8cdd2a47042b28749ceb51362c22543"
+  url "https://github.com/AliSoftware/SwiftGen/archive/1.1.0.tar.gz"
+  sha256 "a0c2e86a3e1c127c50f9d94f25f429c9c9e2fb1eca1f918854f9fb58c7abc30c"
   head "https://github.com/AliSoftware/SwiftGen.git"
 
   bottle do
@@ -21,10 +21,12 @@ class Swiftgen < Formula
       UnitTests/fixtures/colors.txt
       UnitTests/fixtures/Localizable.strings
       UnitTests/fixtures/Message.storyboard
-      UnitTests/expected/Images-File-Defaults.swift.out
-      UnitTests/expected/Colors-Txt-File-Defaults.swift.out
-      UnitTests/expected/Strings-File-Defaults.swift.out
-      UnitTests/expected/Storyboards-Message-Defaults.swift.out
+      UnitTests/fixtures/Fonts
+      UnitTests/expected/Images-File-Default.swift.out
+      UnitTests/expected/Colors-Txt-File-Default.swift.out
+      UnitTests/expected/Strings-File-Default.swift.out
+      UnitTests/expected/Storyboards-Message-Default.swift.out
+      UnitTests/expected/Fonts-Dir-Default.swift.out
     ]
     (pkgshare/"fixtures").install fixtures
   end
@@ -35,15 +37,18 @@ class Swiftgen < Formula
     fixtures = pkgshare/"fixtures"
 
     output = shell_output("#{bin}/swiftgen images --templatePath #{pkgshare/"templates/images-default.stencil"} #{fixtures}/Images.xcassets").strip
-    assert_equal output, (fixtures/"Images-File-Defaults.swift.out").read.strip, "swiftgen images failed"
+    assert_equal output, (fixtures/"Images-File-Default.swift.out").read.strip, "swiftgen images failed"
 
     output = shell_output("#{bin}/swiftgen colors --templatePath #{pkgshare/"templates/colors-default.stencil"} #{fixtures}/colors.txt").strip
-    assert_equal output, (fixtures/"Colors-Txt-File-Defaults.swift.out").read.strip, "swiftgen colors failed"
+    assert_equal output, (fixtures/"Colors-Txt-File-Default.swift.out").read.strip, "swiftgen colors failed"
 
     output = shell_output("#{bin}/swiftgen strings --templatePath #{pkgshare/"templates/strings-default.stencil"} #{fixtures}/Localizable.strings").strip
-    assert_equal output, (fixtures/"Strings-File-Defaults.swift.out").read.strip, "swiftgen strings failed"
+    assert_equal output, (fixtures/"Strings-File-Default.swift.out").read.strip, "swiftgen strings failed"
 
     output = shell_output("#{bin}/swiftgen storyboards --templatePath #{pkgshare/"templates/storyboards-default.stencil"} #{fixtures}/Message.storyboard").strip
-    assert_equal output, (fixtures/"Storyboards-Message-Defaults.swift.out").read.strip, "swiftgen storyboards failed"
+    assert_equal output, (fixtures/"Storyboards-Message-Default.swift.out").read.strip, "swiftgen storyboards failed"
+
+    output = shell_output("#{bin}/swiftgen fonts --templatePath #{pkgshare/"templates/fonts-default.stencil"} #{fixtures}/Fonts").strip
+    assert_equal output, (fixtures/"Fonts-Dir-Default.swift.out").read.strip, "swiftgen fonts failed"
   end
 end
