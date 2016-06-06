a6eadc9e60880e7364c067712df356a4d62a60b3
John Colvin <john.loughran.colvin@gmail.com>
2016-06-06 09:56:46 +0100

    ldc 1.0.0
    Closes #1741.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/ldc.rb
+++ Formula/ldc.rb
@@ -1,24 +1,21 @@
 class Ldc < Formula
   desc "Portable D programming language compiler"
   homepage "https://wiki.dlang.org/LDC"
-  url "https://github.com/ldc-developers/ldc/releases/download/v0.17.1/ldc-0.17.1-src.tar.gz"
-  sha256 "8f5453e4e0878110ab03190ae9313ebbb323884090e6e7db87b02e5ed6a1c8b0"
 
-  bottle do
-    sha256 "2de136bd7d32035ab020e2baa8adc4f6bced37aa4a14f721ab7065dd4b572a03" => :el_capitan
-    sha256 "b4753490e35a5698f5f17ead17f12f58ca698a6c27418135692c781efae2b3dd" => :yosemite
-    sha256 "da22d9b923b89baab0ccec0801827d709ed0ddafbc514ac9b0bd87d28b21b1ff" => :mavericks
-  end
-
-  devel do
-    url "https://github.com/ldc-developers/ldc/releases/download/v1.0.0-beta2/ldc-1.0.0-beta2-src.tar.gz"
-    sha256 "0aa58dd3aba41623218af7bbdaaa3f5dc941c0b157ec612592aac0d52660f71e"
-    version "1.0.0-beta2"
+  stable do
+    url "https://github.com/ldc-developers/ldc/releases/download/v1.0.0/ldc-1.0.0-src.tar.gz"
+    sha256 "3740ee6d5871e953aeb03b11f9d8c951286a55884892b51981bfe579b8fe571d"
 
     resource "ldc-lts" do
       url "https://github.com/ldc-developers/ldc/releases/download/v0.17.1/ldc-0.17.1-src.tar.gz"
       sha256 "8f5453e4e0878110ab03190ae9313ebbb323884090e6e7db87b02e5ed6a1c8b0"
     end
+  end
+
+  bottle do
+    sha256 "2de136bd7d32035ab020e2baa8adc4f6bced37aa4a14f721ab7065dd4b572a03" => :el_capitan
+    sha256 "b4753490e35a5698f5f17ead17f12f58ca698a6c27418135692c781efae2b3dd" => :yosemite
+    sha256 "da22d9b923b89baab0ccec0801827d709ed0ddafbc514ac9b0bd87d28b21b1ff" => :mavericks
   end
 
   head do
@@ -37,25 +34,17 @@ class Ldc < Formula
 
   def install
     ENV.cxx11
-    if build.stable?
+    (buildpath/"ldc-lts").install resource("ldc-lts")
+    cd "ldc-lts" do
       mkdir "build" do
-        system "cmake", "..", "-DINCLUDE_INSTALL_DIR=#{include}/dlang/ldc", *std_cmake_args
+        system "cmake", "..", *std_cmake_args
         system "make"
-        system "make", "install"
       end
-    else
-      (buildpath/"ldc-lts").install resource("ldc-lts")
-      cd "ldc-lts" do
-        mkdir "build" do
-          system "cmake", "..", *std_cmake_args
-          system "make"
-        end
-      end
-      mkdir "build" do
-        system "cmake", "..", "-DINCLUDE_INSTALL_DIR=#{include}/dlang/ldc", "-DD_COMPILER=#{buildpath}/ldc-lts/build/bin/ldmd2", *std_cmake_args
-        system "make"
-        system "make", "install"
-      end
+    end
+    mkdir "build" do
+      system "cmake", "..", "-DINCLUDE_INSTALL_DIR=#{include}/dlang/ldc", "-DD_COMPILER=#{buildpath}/ldc-lts/build/bin/ldmd2", *std_cmake_args
+      system "make"
+      system "make", "install"
     end
   end
 
