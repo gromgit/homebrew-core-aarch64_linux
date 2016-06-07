eb565b0135b278aa5221f149b1a9d41b5ac072df
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-07 02:13:08 +0100

    pulledpork: add 0.7.0_1 bottle.

--- Formula/pulledpork.rb
+++ Formula/pulledpork.rb
@@ -7,6 +7,13 @@ class Pulledpork < Formula
 
   head "https://github.com/shirkdog/pulledpork.git"
 
+  bottle do
+    cellar :any
+    sha256 "1ed9d60f5b61b10dcbaee64a412880d18f4a8b383a970058ae8fcddabf70d97f" => :el_capitan
+    sha256 "df5100e17ef491d1cf9a01b472af6793b557c11e634aa03e070550a43e632ca6" => :yosemite
+    sha256 "df4b7616a783b56d557e86c65a376be78dbc3df8075f9589a5b255888dce3aad" => :mavericks
+  end
+
   depends_on "openssl"
 
   resource "Switch" do
