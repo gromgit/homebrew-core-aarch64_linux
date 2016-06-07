4e8122d9360f23f906598c8ffdbbe2bffc24571e
Jerzerak <jerzerak@gmail.com>
2016-06-07 13:31:05 -0400

    gradle 2.14-rc-5 (devel) (#1784)

--- Formula/gradle.rb
+++ Formula/gradle.rb
@@ -5,9 +5,9 @@ class Gradle < Formula
   sha256 "0f665ec6a5a67865faf7ba0d825afb19c26705ea0597cec80dd191b0f2cbb664"
 
   devel do
-    url "https://downloads.gradle.org/distributions/gradle-2.14-rc-4-bin.zip"
-    sha256 "471424c0998851572c8b0ac71807215fc717de1373dcb5b64889e0aa5d147d26"
-    version "2.14-rc-4"
+    url "https://downloads.gradle.org/distributions/gradle-2.14-rc-5-bin.zip"
+    sha256 "dcdd1021345cfabd2c06a345700afe537bde5478cfb8c3ac59d6348eeb0647e9"
+    version "2.14-rc-5"
   end
 
   bottle :unneeded
