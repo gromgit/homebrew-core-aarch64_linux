99eb1d4f110946d049c4156316d6d533e9751d20
Dominyk Tiller <dominyktiller@gmail.com>
2016-06-07 18:27:46 +0100

    gnutls 3.4.13
    Fixes GNUTLS-SA-2016-1.
    
    Closes #1787.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/gnutls.rb
+++ Formula/gnutls.rb
@@ -1,10 +1,10 @@
 class Gnutls < Formula
   desc "GNU Transport Layer Security (TLS) Library"
   homepage "https://gnutls.org/"
-  url "ftp://ftp.gnutls.org/gcrypt/gnutls/v3.4/gnutls-3.4.12.tar.xz"
-  mirror "https://gnupg.org/ftp/gcrypt/gnutls/v3.4/gnutls-3.4.12.tar.xz"
-  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gnutls/v3.4/gnutls-3.4.12.tar.xz"
-  sha256 "e3370a3bf60f2ca4a6204461ea99e7d7047ee46f96bc2fb7c63f103312d3c9c7"
+  url "ftp://ftp.gnutls.org/gcrypt/gnutls/v3.4/gnutls-3.4.13.tar.xz"
+  mirror "https://gnupg.org/ftp/gcrypt/gnutls/v3.4/gnutls-3.4.13.tar.xz"
+  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gnutls/v3.4/gnutls-3.4.13.tar.xz"
+  sha256 "fd3386e8e72725980bcd7f40949aa0121dcb7650b5147c6490e794555ed25859"
 
   bottle do
     cellar :any
