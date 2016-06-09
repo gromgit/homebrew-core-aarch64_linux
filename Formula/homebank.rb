4697d6164aa73f3593ca5b79401bc5b7c2922f64
aas <alexander.ashurkov@gmail.com>
2016-06-09 15:34:10 +0500

    homebank 5.0.8
    Homebank 5.0.8
    
    Closes #1846.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/homebank.rb
+++ Formula/homebank.rb
@@ -1,8 +1,8 @@
 class Homebank < Formula
   desc "Manage your personal accounts at home"
   homepage "http://homebank.free.fr"
-  url "http://homebank.free.fr/public/homebank-5.0.6.tar.gz"
-  sha256 "4a52ef7a20accd50f8cead0d0564042912573c7d60887ef5e4d462fb28b239e4"
+  url "http://homebank.free.fr/public/homebank-5.0.8.tar.gz"
+  sha256 "9bdf10b0d94c92d5216462fb110bba34cd2a1b01d38d2c6b629db81a0513a310"
 
   bottle do
     sha256 "07d147dcb3410ca05271f7adb41a5414f108c59e659c6a4f0255f79dcfa7487f" => :el_capitan
