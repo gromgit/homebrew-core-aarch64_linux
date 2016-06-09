df3f44d32e8bfbf8fb521c51c4c18cc5b70827e6
Rakesh <rakkesh@users.noreply.github.com>
2016-06-09 05:42:12 +0530

    hunspell 1.4.1
    * Update homepage to https://hunspell.github.io/
    * Update patch disscussed at https://sourceforge.net/p/hunspell/bugs/236/
    
    Closes #1823.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/hunspell.rb
+++ Formula/hunspell.rb
@@ -1,8 +1,8 @@
 class Hunspell < Formula
   desc "Spell checker and morphological analyzer"
-  homepage "http://hunspell.sourceforge.net/"
-  url "https://downloads.sourceforge.net/hunspell/hunspell-1.3.3.tar.gz"
-  sha256 "a7b2c0de0e2ce17426821dc1ac8eb115029959b3ada9d80a81739fa19373246c"
+  homepage "https://hunspell.github.io"
+  url "https://github.com/hunspell/hunspell/archive/v1.4.1.tar.gz"
+  sha256 "c4476aff0ced52eec334eae1e8d3fdaaebdd90f5ecd0b57cf2a92a6fd220d1bb"
 
   bottle do
     sha256 "cafeaa28d6684e743f52094975f46b962316c2263520d20e013606e2fac49f60" => :el_capitan
@@ -15,16 +15,22 @@ class Hunspell < Formula
   conflicts_with "freeling", :because => "both install 'analyze' binary"
 
   # hunspell does not prepend $HOME to all USEROODIRs
-  # https://sourceforge.net/p/hunspell/bugs/236/
-  patch :p0, :DATA
+  # https://github.com/hunspell/hunspell/issues/32
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/684440d/hunspell/prepend_user_home_to_useroodirs.diff"
+    sha256 "456186c9e569c51065e7df2a521e325d536ba4627d000ab824f7e97c1e4bc288"
+  end
 
   def install
+    ENV.deparallelize
+
+    # The following line can be removed on release of next stable version
+    inreplace "configure", "1.4.0", "1.4.1"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-ui",
                           "--with-readline"
     system "make"
-    ENV.deparallelize
     system "make", "install"
 
     pkgshare.install "tests"
@@ -44,45 +50,3 @@ class Hunspell < Formula
     system "./test.sh"
   end
 end
-
-__END__
---- src/tools/hunspell.cxx.old	2013-08-02 18:21:49.000000000 +0200
-+++ src/tools/hunspell.cxx	2013-08-02 18:20:27.000000000 +0200
-@@ -28,7 +28,7 @@
- #ifdef WIN32
-
- #define LIBDIR "C:\\Hunspell\\"
--#define USEROOODIR "Application Data\\OpenOffice.org 2\\user\\wordbook"
-+#define USEROOODIR { "Application Data\\OpenOffice.org 2\\user\\wordbook" }
- #define OOODIR \
-     "C:\\Program files\\OpenOffice.org 2.4\\share\\dict\\ooo\\;" \
-     "C:\\Program files\\OpenOffice.org 2.3\\share\\dict\\ooo\\;" \
-@@ -65,11 +65,11 @@
-     "/usr/share/myspell:" \
-     "/usr/share/myspell/dicts:" \
-     "/Library/Spelling"
--#define USEROOODIR \
--    ".openoffice.org/3/user/wordbook:" \
--    ".openoffice.org2/user/wordbook:" \
--    ".openoffice.org2.0/user/wordbook:" \
--    "Library/Spelling"
-+#define USEROOODIR { \
-+    ".openoffice.org/3/user/wordbook:", \
-+    ".openoffice.org2/user/wordbook:", \
-+    ".openoffice.org2.0/user/wordbook:", \
-+    "Library/Spelling" }
- #define OOODIR \
-     "/opt/openoffice.org/basis3.0/share/dict/ooo:" \
-     "/usr/lib/openoffice.org/basis3.0/share/dict/ooo:" \
-@@ -1664,7 +1664,10 @@
- 	path = add(path, PATHSEP);          // <- check path in root directory
- 	if (getenv("DICPATH")) path = add(add(path, getenv("DICPATH")), PATHSEP);
- 	path = add(add(path, LIBDIR), PATHSEP);
--	if (HOME) path = add(add(add(add(path, HOME), DIRSEP), USEROOODIR), PATHSEP);
-+  const char* userooodir[] = USEROOODIR;
-+  for (int i = 0; i < (sizeof(userooodir) / sizeof(userooodir[0])); i++) {
-+    if (HOME) path = add(add(add(add(path, HOME), DIRSEP), userooodir[i]), PATHSEP);
-+  }
- 	path = add(path, OOODIR);
-
- 	if (showpath) {
