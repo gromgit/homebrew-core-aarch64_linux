18c1b04acb9a2b6cca09f670ec6fbf66c4e7219a
Dominyk Tiller <dominyktiller@gmail.com>
2016-06-09 13:46:05 +0100

    wakatime-cli: tiny style nits

--- Formula/wakatime-cli.rb
+++ Formula/wakatime-cli.rb
@@ -14,11 +14,11 @@ class WakatimeCli < Formula
   depends_on :python if MacOS.version <= :snow_leopard
 
   def install
-    ENV["PYTHONPATH"] = libexec+"lib/python2.7/site-packages"
+    ENV["PYTHONPATH"] = libexec/"lib/python2.7/site-packages"
 
     system "python", *Language::Python.setup_install_args(libexec)
     bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec+"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
   test do
