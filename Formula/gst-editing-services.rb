59a2307a66c15810e634d6326b89aa3541084533
Thijs Vermeir <thijsvermeir@gmail.com>
2016-06-09 11:44:26 +0200

    gst-editing-services 1.8.2

--- Formula/gst-editing-services.rb
+++ Formula/gst-editing-services.rb
@@ -1,8 +1,8 @@
 class GstEditingServices < Formula
   desc "GStreamer Editing Services"
   homepage "https://gstreamer.freedesktop.org/modules/gst-editing-services.html"
-  url "https://gstreamer.freedesktop.org/src/gst-editing-services/gstreamer-editing-services-1.8.1.tar.xz"
-  sha256 "897babc2da242438992a2361a74cde16d3feb5eab56c653a3aeb553f45365020"
+  url "https://gstreamer.freedesktop.org/src/gst-editing-services/gstreamer-editing-services-1.8.2.tar.xz"
+  sha256 "a1d57ff9461407cca1f6e7a9f31a5bdb73f73f33c488a3e3318b27e10a4332ae"
 
   bottle do
     sha256 "f35cc94ab7b1efc5e01d6dcc3e35214d0b9566d0c0c53a6fa21d35d4dd7b741b" => :el_capitan
