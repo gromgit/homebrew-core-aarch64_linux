4b883cd530639f3216e9672c15c0717387fd7fbd
Dominyk Tiller <dominyktiller@gmail.com>
2016-06-09 14:17:52 +0100

    app-engine-go-32 1.9.38

--- Formula/app-engine-go-32.rb
+++ Formula/app-engine-go-32.rb
@@ -1,8 +1,8 @@
 class AppEngineGo32 < Formula
   desc "Google App Engine SDK for Go (i386)"
   homepage "https://cloud.google.com/appengine/docs/go/"
-  url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_386-1.9.37.zip"
-  sha256 "e3662a2dc55fb0ab21f02fc1b305483bba9c4324c437d6b92e6d9fdd2fd8abad"
+  url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_386-1.9.38.zip"
+  sha256 "7d389a44a43ec65d4688a8f1437575f67313180634ba2c5d047bcbfbba90413b"
 
   bottle :unneeded
 
