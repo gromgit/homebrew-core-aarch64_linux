ffe2bc48beceaaacd8af3952e5f527faf848aa3b
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-06-09 16:55:43 +0200

    zsh-navigation-tools 2.2.1
    Closes #1855.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/zsh-navigation-tools.rb
+++ Formula/zsh-navigation-tools.rb
@@ -1,8 +1,8 @@
 class ZshNavigationTools < Formula
   desc "Zsh curses-based tools, e.g. multi-word history searcher"
   homepage "https://github.com/psprint/zsh-navigation-tools"
-  url "https://github.com/psprint/zsh-navigation-tools/archive/v2.1.16.tar.gz"
-  sha256 "534f55155d288c7177dcfe10951c2cb8f6b9729043a550d8976da8eb2b3fb013"
+  url "https://github.com/psprint/zsh-navigation-tools/archive/v2.2.1.tar.gz"
+  sha256 "3937aa4cb4a77f8c7dd161e1703db63e6d6dc18a1c4b4562ed77bb6b8b0cd144"
 
   bottle do
     cellar :any_skip_relocation
