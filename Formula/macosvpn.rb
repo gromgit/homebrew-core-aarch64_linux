4f29a9d82684d36ea0c4cf7988a83dc8cef76f43
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-09 20:07:56 +0100

    macosvpn: update 0.2.1 bottle.

--- Formula/macosvpn.rb
+++ Formula/macosvpn.rb
@@ -6,7 +6,7 @@ class Macosvpn < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9951258df34bcc9de51e72deda6d3e0c1b8a8a0b5ec1b8a87fbae2e4ea7e6967" => :el_capitan
+    sha256 "c2d16daa3ef23c14f2db4320b4a9c1450ad7aa30563c330e7119a4584847edac" => :el_capitan
   end
 
   depends_on :xcode => ["7.3", :build]
