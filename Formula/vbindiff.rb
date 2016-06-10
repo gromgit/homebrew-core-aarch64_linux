2732d5f5662e664a146867a292492401441545c2
Viktor Szakats <vszakats@users.noreply.github.com>
2016-06-11 01:40:14 +0200

    vbindiff: use secure urls
    Closes #1881.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/vbindiff.rb
+++ Formula/vbindiff.rb
@@ -1,7 +1,7 @@
 class Vbindiff < Formula
   desc "Visual Binary Diff"
-  homepage "http://www.cjmweb.net/vbindiff/"
-  url "http://www.cjmweb.net/vbindiff/vbindiff-3.0_beta4.tar.gz"
+  homepage "https://www.cjmweb.net/vbindiff/"
+  url "https://www.cjmweb.net/vbindiff/vbindiff-3.0_beta4.tar.gz"
   version "3.0_beta4"
   sha256 "7d5d5a87fde953dc2089746f6f6ab811d60e127b01074c97611898fb1ef1983d"
 
