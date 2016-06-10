815526d1b1c3a699f0fff93102aa9258fa305d82
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-10 21:33:05 +0100

    gdbm: update 1.12 bottle.

--- Formula/gdbm.rb
+++ Formula/gdbm.rb
@@ -7,11 +7,9 @@ class Gdbm < Formula
 
   bottle do
     cellar :any
-    revision 2
-    sha256 "59868f18b7b88e9c47cfd802435618881ac8858408d3ebfb009803db0c415a32" => :el_capitan
-    sha256 "1ad0aacc00881d05f8f894d8d117e4e0b8b5f5afc15a37a3f7c735c040ef0c5a" => :yosemite
-    sha256 "440dc9069280d474af52847f18dbcdbb10813a237522f650fcbe6c63dae784dc" => :mavericks
-    sha256 "bf77827fb3e51ed8691ed357014343f72587cbd2dd7248922ac8f70117e4c5e8" => :mountain_lion
+    sha256 "80ee188768a6029012a576c29be718149378d058e1803c6149ee8a36ce879f58" => :el_capitan
+    sha256 "fa512dd57e18dc3db293cfcf305356d137a3fad0f85240a9788dc4057290ce9c" => :yosemite
+    sha256 "87bfecf948e8b6182519f627f95c244531b2a48c1941352bee0980275b515f43" => :mavericks
   end
 
   option :universal
