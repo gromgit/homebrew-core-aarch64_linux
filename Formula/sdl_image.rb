1ed223c6ad01ca76cca97b94ecce91bceb8e0459
Kyungdahm Yun <tomyun@gmail.com>
2016-06-09 21:18:01 -0700

    sdl_image: fix graphical glitching
    Closes #1864.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/sdl_image.rb
+++ Formula/sdl_image.rb
@@ -3,7 +3,7 @@ class SdlImage < Formula
   homepage "https://www.libsdl.org/projects/SDL_image"
   url "https://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12.tar.gz"
   sha256 "0b90722984561004de84847744d566809dbb9daf732a9e503b91a1b5a84e5699"
-  revision 2
+  revision 3
 
   bottle do
     cellar :any
@@ -12,6 +12,8 @@ class SdlImage < Formula
     sha256 "84272135304e7c475e8000a90af113127858273a4449ce6566805f5b4c2cb476" => :mavericks
   end
 
+  option :universal
+
   depends_on "pkg-config" => :build
   depends_on "sdl"
   depends_on "jpeg" => :recommended
@@ -19,7 +21,13 @@ class SdlImage < Formula
   depends_on "libtiff" => :recommended
   depends_on "webp" => :recommended
 
-  option :universal
+  # Fix graphical glitching
+  # https://github.com/Homebrew/homebrew-python/issues/281
+  # https://trac.macports.org/ticket/37453
+  patch :p0 do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/41996822/sdl_image/IMG_ImageIO.m.patch"
+    sha256 "c43c5defe63b6f459325798e41fe3fdf0a2d32a6f4a57e76a056e752372d7b09"
+  end
 
   def install
     ENV.universal_binary if build.universal?
