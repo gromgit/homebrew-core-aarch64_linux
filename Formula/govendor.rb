c14d2d100f6e12fa97b85f7fab58c5fdc812e203
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-10 17:07:01 +0100

    govendor: add 1.0.3 bottle.

--- Formula/govendor.rb
+++ Formula/govendor.rb
@@ -5,6 +5,13 @@ class Govendor < Formula
   sha256 "7d7a032355ee4117822150db72a9db745d9a147a2e32972e58a65fe7545444f2"
   head "https://github.com/kardianos/govendor.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d300d21f2c60d27b583cbd0ff60b3d4f31e9d1831cf09ba4ea07fd6fed1429d5" => :el_capitan
+    sha256 "882e1cf9816240820cf95a5954ee2c980a21d6c445b4b986329b375b715ef9cc" => :yosemite
+    sha256 "ade3e446c9270fcb24532d007f23f999e872caff9e33744b5e532506c683f1a7" => :mavericks
+  end
+
   depends_on "go"
 
   def install
