fe1a3dc00072aaf6e78d8d897f8f78b22a4475e3
Viktor Szakats <vszakats@users.noreply.github.com>
2016-06-10 13:22:20 +0200

    surfraw: use secure head url, fix audit nit

--- Formula/surfraw.rb
+++ Formula/surfraw.rb
@@ -4,13 +4,6 @@ class Surfraw < Formula
   url "https://surfraw.alioth.debian.org/dist/surfraw-2.2.9.tar.gz"
   sha256 "aa97d9ac24ca4299be39fcde562b98ed556b3bf5ee9a1ae497e0ce040bbcc4bb"
 
-  head do
-    url "git://git.debian.org/surfraw/surfraw.git", :shallow => false
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-  end
-
   bottle do
     cellar :any_skip_relocation
     revision 1
@@ -18,6 +11,13 @@ class Surfraw < Formula
     sha256 "cf0dd3b14c4e8034f0d0766fb09978eac0f19fa41730d72ed1422a67c0b0d0b3" => :yosemite
     sha256 "1291ba20882c2dc1fbb092782e943ab0b99ad3642fb73db207dadff1fc00c5fc" => :mavericks
     sha256 "30b885dc5908318868da2739f36834ce071bc7bff1a761fdc395afe82f75efa8" => :mountain_lion
+  end
+
+  head do
+    url "https://anonscm.debian.org/git/surfraw/surfraw.git", :shallow => false
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
   end
 
   def install
