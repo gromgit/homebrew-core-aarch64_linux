d2b90e146eb33ffb7547dc2300f8044b42e8b802
acareccia <alberto.careccia@gmail.com>
2016-06-11 12:55:13 +0200

    docker-machine: add launchd script
    Closes #1893.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/docker-machine.rb
+++ Formula/docker-machine.rb
@@ -36,6 +36,35 @@ class DockerMachine < Formula
     end
   end
 
+  plist_options :manual => "docker-machine start"
+
+  def plist; <<-EOS.undent
+     <?xml version="1.0" encoding="UTF-8"?>
+     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+     <plist version="1.0">
+       <dict>
+         <key>EnvironmentVariables</key>
+         <dict>
+             <key>PATH</key>
+             <string>/usr/bin:/bin:/usr/sbin:/sbin:#{HOMEBREW_PREFIX}/bin</string>
+         </dict>
+         <key>Label</key>
+         <string>#{plist_name}</string>
+         <key>ProgramArguments</key>
+         <array>
+             <string>#{opt_bin}/docker-machine</string>
+             <string>start</string>
+             <string>default</string>
+         </array>
+         <key>RunAtLoad</key>
+         <true/>
+         <key>WorkingDirectory</key>
+         <string>#{HOMEBREW_PREFIX}</string>
+       </dict>
+     </plist>
+     EOS
+  end
+
   test do
     assert_match /#{version}/, shell_output(bin/"docker-machine --version")
   end
