7ff0062bc4a5ce8607165f4102ff7c187c256aa5
Viktor Szakats <vszakats@users.noreply.github.com>
2016-06-11 18:06:38 +0200

    dante: remove ftp mirror (#1891)
    It's probably safe now, that it has an HTTP mirror.

--- Formula/dante.rb
+++ Formula/dante.rb
@@ -3,7 +3,6 @@ class Dante < Formula
   homepage "https://www.inet.no/dante/"
   url "https://www.inet.no/dante/files/dante-1.4.1.tar.gz"
   mirror "http://www.inet.no/dante/files/dante-1.4.1.tar.gz"
-  mirror "ftp://ftp.inet.no/pub/socks/dante-1.4.1.tar.gz"
   sha256 "b6d232bd6fefc87d14bf97e447e4fcdeef4b28b16b048d804b50b48f261c4f53"
 
   bottle do
