65dd9007ce10e186c046cc70a1b74507f7ab866d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-12 13:46:30 +0100

    evas-generic-loaders: update 1.17.0 bottle.

--- Formula/evas-generic-loaders.rb
+++ Formula/evas-generic-loaders.rb
@@ -6,10 +6,9 @@ class EvasGenericLoaders < Formula
 
   bottle do
     cellar :any
-    sha256 "495068062dfabab4fae10ac3873d3b3d26b90b14b3863d3d01c34faa6f2f467c" => :el_capitan
-    sha256 "e0b11cef8ea7877673be8374ff89ecbe1b106fa9afc30570087bb12d154eee24" => :yosemite
-    sha256 "fe36efe8f64c17ee3223b51273e85ba4e29aa8a540b75a0df371d9b4dc942939" => :mavericks
-    sha256 "db0f34eac5d976faaaef067df90a845e5c93277e6d1622acb77144984420a38a" => :mountain_lion
+    sha256 "8b840c0ff34d7e4d0ac146942d1d925637ac5ee990bdb59c7174c87f230620ed" => :el_capitan
+    sha256 "bc91362fca88ae3b17bea974eb22ba8d262c9456668e7c3dec6a8f8169f162d1" => :yosemite
+    sha256 "8c6f8dc36b315c1d1f9a2151b419e7786de781634fdc0cdd9c749d026c260394" => :mavericks
   end
 
   depends_on "pkg-config" => :build
