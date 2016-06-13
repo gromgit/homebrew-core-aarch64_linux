9b6108b277a37b899080b3a36134eea2fb638258
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-13 17:06:38 +0100

    dvm: add 0.6.2 bottle.

--- Formula/dvm.rb
+++ Formula/dvm.rb
@@ -4,6 +4,13 @@ class Dvm < Formula
   url "https://github.com/getcarina/dvm/archive/0.6.2.tar.gz"
   sha256 "57a14fc85c37d30c3c2265eb0f06d2fdc18273306052a5ccc748b54a5c38660c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "204406b846165f52bba80e3f7da648bce423ac872e8a4306285c6a693cf8b926" => :el_capitan
+    sha256 "06a2afc76691a0d9cfafbd8ea4328c7f54a5fdc4d02035b0f9bb5bf707c900ab" => :yosemite
+    sha256 "ffbd9b7cddbc6e31ddaec3203381b348ff100d383546d0a51ddee2620fb975dd" => :mavericks
+  end
+
   depends_on "go" => :build
 
   def install
