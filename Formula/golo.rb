40ba797044c33a9ade134977fce5af01b741249d
Romain Lespinasse <romain.lespinasse@gmail.com>
2016-06-13 14:55:54 +0200

    golo 3.2.0-M2 (#1935)

--- Formula/golo.rb
+++ Formula/golo.rb
@@ -6,9 +6,9 @@ class Golo < Formula
   head "https://github.com/eclipse/golo-lang.git"
 
   devel do
-    url "https://bintray.com/artifact/download/golo-lang/downloads/golo-3.2.0-M1.zip"
-    sha256 "d562adda31982a9ad18ead7b3b482b604b15545a74144e3f6e5a35b38d65688e"
-    version "3.2.0-M1"
+    url "https://bintray.com/artifact/download/golo-lang/downloads/golo-3.2.0-M2.zip"
+    sha256 "ed2138b9896a74a37efe9331de6bc15a3d607aa07c8335fe5de1c8765f17d856"
+    version "3.2.0-M2"
   end
   bottle :unneeded
 
