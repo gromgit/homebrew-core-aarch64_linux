60b97fa9684c1475277ff9c6f5d7f18b573e18eb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-14 03:05:18 +0100

    mpd: update 0.19.16 bottle.

--- Formula/mpd.rb
+++ Formula/mpd.rb
@@ -6,9 +6,9 @@ class Mpd < Formula
 
   bottle do
     cellar :any
-    sha256 "14b7d310c9d92c4632b4154754d9e60fcd0091fe2d1d32c17a51d90132e126e4" => :el_capitan
-    sha256 "13378d8796587be398336ab329d677a71971883dcc001fca61dc8c46f98200b2" => :yosemite
-    sha256 "74cee039cbf84d12baedbb6454987e362e18d82b625bbe97891dd2a10bdb7e82" => :mavericks
+    sha256 "c7f59dd212cc121bc18ae35218a1391577f73caab75756013f67399447e77f37" => :el_capitan
+    sha256 "1633dc1e733fe9d9c0028421e001d0f4b9e1794d361aefbb9485422f19ce3964" => :yosemite
+    sha256 "7282b0ef5a5f10d6f8d97ec83ffef8828c502524ec4b001e6ae9d0d1ec0c483c" => :mavericks
   end
 
   head do
