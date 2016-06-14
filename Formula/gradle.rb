991a0d8bc995dfdcfc56ac6d848976b787a87e83
Jared Burrows <jaredsburrows@gmail.com>
2016-06-14 14:32:26 -0700

    gradle 2.14, 3.0-milestone-1 (devel) (#1980)

--- Formula/gradle.rb
+++ Formula/gradle.rb
@@ -1,13 +1,13 @@
 class Gradle < Formula
   desc "Build system based on the Groovy language"
   homepage "https://www.gradle.org/"
-  url "https://downloads.gradle.org/distributions/gradle-2.13-bin.zip"
-  sha256 "0f665ec6a5a67865faf7ba0d825afb19c26705ea0597cec80dd191b0f2cbb664"
+  url "https://downloads.gradle.org/distributions/gradle-2.14-bin.zip"
+  sha256 "993b4f33b652c689e9721917d8e021cab6bbd3eae81b39ab2fd46fdb19a928d5"
 
   devel do
-    url "https://downloads.gradle.org/distributions/gradle-2.14-rc-5-bin.zip"
-    sha256 "dcdd1021345cfabd2c06a345700afe537bde5478cfb8c3ac59d6348eeb0647e9"
-    version "2.14-rc-5"
+    url "https://downloads.gradle.org/distributions/gradle-3.0-milestone-1-bin.zip"
+    sha256 "dfff9f7fbf75d025cf5b4f7b61ea9e24757f74a6934ba7249658e7e2de9d329f"
+    version "3.0-milestone-1"
   end
 
   bottle :unneeded
