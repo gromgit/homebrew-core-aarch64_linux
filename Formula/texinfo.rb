c5413b22c059ecafa151c4ff3b19ad66c5187c97
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-14 14:42:02 +0100

    texinfo: update 6.1 bottle.

--- Formula/texinfo.rb
+++ Formula/texinfo.rb
@@ -6,10 +6,9 @@ class Texinfo < Formula
   sha256 "ac68394ce21b2420ba7ed7cec65d84aacf308cc88e9bf4716fcfff88286883d2"
 
   bottle do
-    revision 1
-    sha256 "9157162006baab3069abb5d61241712f059a97c3752c1d1663e66d61062b18a5" => :el_capitan
-    sha256 "9f611165b36ae3aac1b5c7a965de48659c41e53209e00a80649560414c861667" => :yosemite
-    sha256 "d6031029c35ef99b7d3ef9d43daf993d6897bf73dd81cf2f53f5a6d1d38d7d73" => :mavericks
+    sha256 "f2494484ec7313c1d30156d6b4939daa4848e271030875de3d59def35db03e50" => :el_capitan
+    sha256 "b9f97a38fd4ce96c0e08bb7415c1d2c5f1e6af8e83f600a3aeff7457cbde9da6" => :yosemite
+    sha256 "9d95f0a35e4b245925f2b2a430a28b261c1b49659468d59a544ad28947fe34ad" => :mavericks
   end
 
   keg_only :provided_by_osx, <<-EOS.undent
