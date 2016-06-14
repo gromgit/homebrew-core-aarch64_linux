4fba1699874b173bb08c20d181be8635f2af1ff4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-14 21:55:54 +0100

    nsd: update 4.1.10 bottle.

--- Formula/nsd.rb
+++ Formula/nsd.rb
@@ -5,9 +5,9 @@ class Nsd < Formula
   sha256 "3a757014046752a0b0b11c1a2e22a36bb796f89f6939147ed3226556b4298727"
 
   bottle do
-    sha256 "ca1cc7f3b4558f4d9be7027a1c68d1c36b7e3b354202cf6de65143de659557e8" => :el_capitan
-    sha256 "dbf1cdbb15aa49181dacce1169b06bbce05b9d2b83baf676ca8892a76e13455f" => :yosemite
-    sha256 "09f97c4518e697a27191f38950716b17dca0a34ec00b502498aa28493bf716bc" => :mavericks
+    sha256 "27b27978fd94cf80e6d81f5fb4d79fd3c0200a49b701f46bd846ded6e04b4b0c" => :el_capitan
+    sha256 "f71f850f63ac67f88b8723fb339aa8c2c1e1f1a27d9344394bfd4860bbd160a9" => :yosemite
+    sha256 "dbea8ff1b2ad8c9ee9ae2c8b7bc2e6d199782bba4815b70bc55c6856ba310d06" => :mavericks
   end
 
   option "with-root-server", "Allow NSD to run as a root name server"
