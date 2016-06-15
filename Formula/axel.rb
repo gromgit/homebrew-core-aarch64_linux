82a98f7954aa045ba793d90ecd82e224ecbfa657
Denis Denisov <denji0k@gmail.com>
2016-06-15 10:53:06 +0300

    axel 2.11
    Closes #2009.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/axel.rb
+++ Formula/axel.rb
@@ -1,9 +1,9 @@
 class Axel < Formula
   desc "Light UNIX download accelerator"
-  homepage "https://packages.debian.org/sid/axel"
-  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/a/axel/axel_2.5.orig.tar.gz"
-  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/a/axel/axel_2.5.orig.tar.gz"
-  sha256 "02376767e7f9e6c4292333e69ad0f615f62be5df176a8daaee395f25b0ab1a83"
+  homepage "https://github.com/eribertomota/axel"
+  url "https://github.com/eribertomota/axel/archive/2.11.tar.gz"
+  sha256 "7e528df719bad8041346a371290efd8d67959e935f6763ac977ba278d39f5a15"
+  head "https://github.com/eribertomota/axel.git"
 
   bottle do
     cellar :any_skip_relocation
@@ -12,13 +12,16 @@ class Axel < Formula
     sha256 "b0a7bcbab57df1b9877ac3a6a8343a3a5b4256b4cfa5a6bfe777b9ba92930d31" => :mavericks
   end
 
-  def install
-    system "./configure", "--prefix=#{prefix}",
-                          "--etcdir=#{etc}",
-                          "--debug=0",
-                          "--i18n=0"
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "gettext"
+  depends_on "openssl"
 
-    system "make"
+  def install
+    system "./autogen.sh"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}"
     system "make", "install"
   end
 
