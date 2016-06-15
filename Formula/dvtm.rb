5a41a2267b7fc6ecb6eb41c818e47c7005ec18f4
ilovezfs <ilovezfs@icloud.com>
2016-06-14 18:04:35 -0700

    dvtm 0.15
    Closes #2005.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dvtm.rb
+++ Formula/dvtm.rb
@@ -1,8 +1,8 @@
 class Dvtm < Formula
   desc "Dynamic Virtual Terminal Manager"
   homepage "http://www.brain-dump.org/projects/dvtm/"
-  url "http://www.brain-dump.org/projects/dvtm/dvtm-0.14.tar.gz"
-  sha256 "8a9bb341f8a4c578b839e22d9a707f053a27ae6df15158e16f4fee787e43747a"
+  url "http://www.brain-dump.org/projects/dvtm/dvtm-0.15.tar.gz"
+  sha256 "8f2015c05e2ad82f12ae4cf12b363d34f527a4bbc8c369667f239e4542e1e510"
   head "git://repo.or.cz/dvtm.git"
 
   bottle do
