5ada2aad351d69d226a355a7675257933352f846
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-16 23:53:08 +0100

    pick: add 1.4.0 bottle.

--- Formula/pick.rb
+++ Formula/pick.rb
@@ -4,6 +4,13 @@ class Pick < Formula
   url "https://github.com/thoughtbot/pick/releases/download/v1.4.0/pick-1.4.0.tar.gz"
   sha256 "46f46b0df54cf27e8dd19ae291d5534cb55ef37d9cdb3cc774cd88c809f718fd"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "966359b2647a1268c4091bb76a7daed22cb4cf1fa96a40a95da8b2d7b54b5968" => :el_capitan
+    sha256 "429d8e11ae48b5e23a39039218f4e65f04be09bdd5916fdd2aaa81b467b3d711" => :yosemite
+    sha256 "ba74ec54344e9ab11638f6576e46128193b483b3e4bb1739a414f0a8df54ecc6" => :mavericks
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
