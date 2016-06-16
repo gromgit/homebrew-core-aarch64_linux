cc88ebd340b06ea752525b7a3ac3da58b8c70bb7
ilovezfs <ilovezfs@icloud.com>
2016-06-16 07:05:49 -0700

    libgetdata 0.9.2.1
    Also, fix sandbox violation when building with the perl option.
    
    Closes #2033.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libgetdata.rb
+++ Formula/libgetdata.rb
@@ -1,8 +1,8 @@
 class Libgetdata < Formula
   desc "Reference implementation of the Dirfile Standards"
   homepage "http://getdata.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/getdata/getdata/0.9.0/getdata-0.9.0.tar.xz"
-  sha256 "b38de059ff21df873e95978867eb82f716b89fc7e8e503e2cc7cef93d22685a2"
+  url "https://downloads.sourceforge.net/project/getdata/getdata/0.9.2.1/getdata-0.9.2.1.tar.xz"
+  sha256 "e41d82abdb86577ce41600f187a4271e17e544b67dc0e0518d42f285cd809499"
 
   bottle do
     cellar :any
@@ -20,6 +20,7 @@ class Libgetdata < Formula
   deprecated_option "zzip" => "with-libzzip"
 
   depends_on :fortran => :optional
+  depends_on :perl => ["5.3", :optional]
   depends_on "xz" => :optional
   depends_on "libzzip" => :optional
 
@@ -32,7 +33,11 @@ class Libgetdata < Formula
       --disable-php
     ]
 
-    args << "--disable-perl" if build.without? "perl"
+    if build.with? "perl"
+      args << "--with-perl-dir=#{lib}/perl5/site_perl"
+    else
+      args << "--disable-perl"
+    end
     args << "--without-liblzma" if build.without? "xz"
     args << "--without-libzzip" if build.without? "libzzip"
     args << "--disable-fortran" << "--disable-fortran95" if build.without? "fortran"
