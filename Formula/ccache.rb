59d8cb9ce746b5700a499e383fc57fcef251a955
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2016-06-16 16:01:51 +0100

    ccache: Add gcc-6 symbolic links
    Closes #2046.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/ccache.rb
+++ Formula/ccache.rb
@@ -3,6 +3,7 @@ class Ccache < Formula
   homepage "https://ccache.samba.org/"
   url "https://www.samba.org/ftp/ccache/ccache-3.2.5.tar.bz2"
   sha256 "7a553809e90faf9de3a23ee9c5b5f786cfd4836bf502744bedb824a24bee1097"
+  revision 1
 
   bottle do
     sha256 "f19a09b17c89e44176c006ba1747a9a524d855a0dea8d0c6501f70165713eabf" => :el_capitan
@@ -30,9 +31,9 @@ class Ccache < Formula
       clang
       clang++
       cc
-      gcc gcc2 gcc3 gcc-3.3 gcc-4.0 gcc-4.2 gcc-4.3 gcc-4.4 gcc-4.5 gcc-4.6 gcc-4.7 gcc-4.8 gcc-4.9 gcc-5
-      c++ c++3 c++-3.3 c++-4.0 c++-4.2 c++-4.3 c++-4.4 c++-4.5 c++-4.6 c++-4.7 c++-4.8 c++-4.9 c++-5
-      g++ g++2 g++3 g++-3.3 g++-4.0 g++-4.2 g++-4.3 g++-4.4 g++-4.5 g++-4.6 g++-4.7 g++-4.8 g++-4.9 g++-5
+      gcc gcc2 gcc3 gcc-3.3 gcc-4.0 gcc-4.2 gcc-4.3 gcc-4.4 gcc-4.5 gcc-4.6 gcc-4.7 gcc-4.8 gcc-4.9 gcc-5 gcc-6
+      c++ c++3 c++-3.3 c++-4.0 c++-4.2 c++-4.3 c++-4.4 c++-4.5 c++-4.6 c++-4.7 c++-4.8 c++-4.9 c++-5 c++-6
+      g++ g++2 g++3 g++-3.3 g++-4.0 g++-4.2 g++-4.3 g++-4.4 g++-4.5 g++-4.6 g++-4.7 g++-4.8 g++-4.9 g++-5 g++-6
     ].each do |prog|
       libexec.install_symlink bin/"ccache" => prog
     end
