847dbfae5bcd99dd89614aeacf61ea2a06d7fbec
Viktor Szakats <vszakats@users.noreply.github.com>
2016-06-18 12:12:14 +0200

    xorriso: use secure urls
    Closes #2106.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/xorriso.rb
+++ Formula/xorriso.rb
@@ -13,9 +13,9 @@ class Xorriso < Formula
   end
 
   # Upstream fix for "unknown type name 'ssize_t'"
-  # See http://lists.gnu.org/archive/html/bug-xorriso/2016-05/msg00001.html
+  # See https://lists.gnu.org/archive/html/bug-xorriso/2016-05/msg00001.html
   patch :p0 do
-    url "http://bazaar.launchpad.net/~libburnia-team/libisofs/scdbackup/diff/1319"
+    url "https://bazaar.launchpad.net/~libburnia-team/libisofs/scdbackup/diff/1319"
     sha256 "c548c59b143efb87ccb3dc08f80f527ae93918fab56894c9f8876f31dccce142"
   end
 
@@ -27,7 +27,7 @@ class Xorriso < Formula
     # `make install` has to be deparallelized due to the following error:
     #   mkdir: /usr/local/Cellar/xorriso/1.4.2/bin: File exists
     #   make[1]: *** [install-binPROGRAMS] Error 1
-    # Reported 14 Jun 2016: http://lists.gnu.org/archive/html/bug-xorriso/2016-06/msg00003.html
+    # Reported 14 Jun 2016: https://lists.gnu.org/archive/html/bug-xorriso/2016-06/msg00003.html
     ENV.deparallelize { system "make", "install" }
   end
 
