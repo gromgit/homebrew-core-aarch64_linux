ab7611e0770381fe2e2cb89cf77aa9c1674499c6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-18 15:30:23 +0100

    serialosc: update 1.4 bottle.

--- Formula/serialosc.rb
+++ Formula/serialosc.rb
@@ -8,9 +8,9 @@ class Serialosc < Formula
 
   bottle do
     cellar :any
-    sha256 "b2b9ec1d9e3058d7752eed3773f7fa724981ddbae5a990db10ad3e98f02d1c1f" => :el_capitan
-    sha256 "a601c8dcadb93c4e4ee0f15e7db22dc0e124bcaf9a3e7b875091d612ea70b078" => :yosemite
-    sha256 "e08f42f4285870af8ba9f341cda5e4420c7535a55f54c9fb78582cfd53fdad3a" => :mavericks
+    sha256 "6f02ff0f093354591e9690ed1b26b626c8cc2e334ab5906246720628e255f8f9" => :el_capitan
+    sha256 "54b6bc84567aa81ab8fba5dc0286413ed6dae4689c3903a32f05658abf135719" => :yosemite
+    sha256 "803b131b8d0aea12b0999506a8936f9467fe7b88b21fbfd2e19847fece6f6f7c" => :mavericks
   end
 
   depends_on "liblo"
