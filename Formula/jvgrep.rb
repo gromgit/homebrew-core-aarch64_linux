aa0abbb2d9982571a41b290e88b3bd15b6f31472
ilovezfs <ilovezfs@icloud.com>
2016-06-17 19:57:40 -0700

    jvgrep 4.9
    Closes #2094.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/jvgrep.rb
+++ Formula/jvgrep.rb
@@ -3,9 +3,8 @@ require "language/go"
 class Jvgrep < Formula
   desc "Grep for Japanese users of Vim"
   homepage "https://github.com/mattn/jvgrep"
-  url "https://github.com/mattn/jvgrep/archive/v4.7.tar.gz"
-  sha256 "2c273949f42d61c2791db1199e166ce50ad508b8ef80a401111fcd5bf2644ac8"
-
+  url "https://github.com/mattn/jvgrep/archive/v4.9.tar.gz"
+  sha256 "74fe0090eb131ca5b8962886122606c7a5a8540e7992c71d138403731b704b4f"
   head "https://github.com/mattn/jvgrep.git"
 
   bottle do
@@ -17,24 +16,24 @@ class Jvgrep < Formula
 
   depends_on "go" => :build
 
-  go_resource "github.com/daviddengcn/go-colortext" do
-    url "https://github.com/daviddengcn/go-colortext.git", :revision => "3b18c8575a432453d41fdafb340099fff5bba2f7"
-  end
-
   go_resource "github.com/mattn/go-isatty" do
-    url "https://github.com/mattn/go-isatty.git", :revision => "56b76bdf51f7708750eac80fa38b952bb9f32639"
+    url "https://github.com/mattn/go-isatty.git",
+    :revision => "56b76bdf51f7708750eac80fa38b952bb9f32639"
   end
 
   go_resource "github.com/mattn/go-colorable" do
-    url "https://github.com/mattn/go-colorable.git", :revision => "9cbef7c35391cca05f15f8181dc0b18bc9736dbb"
+    url "https://github.com/mattn/go-colorable.git",
+    :revision => "9056b7a9f2d1f2d96498d6d146acd1f9d5ed3d59"
   end
 
   go_resource "golang.org/x/net" do
-    url "https://go.googlesource.com/net.git", :revision => "4876518f9e71663000c348837735820161a42df7"
+    url "https://go.googlesource.com/net.git",
+    :revision => "1961d9def2b2d7a28d7958926d2457d05a178ecd"
   end
 
   go_resource "golang.org/x/text" do
-    url "https://go.googlesource.com/text.git", :revision => "1b466db55e0ba5d56ef5315c728216b42f796491"
+    url "https://go.googlesource.com/text.git",
+    :revision => "e3c902a8b2c4c420ce61514795e05b8e28a6364e"
   end
 
   def install
@@ -42,13 +41,11 @@ class Jvgrep < Formula
     mkdir_p buildpath/"src/github.com/mattn"
     ln_s buildpath, buildpath/"src/github.com/mattn/jvgrep"
     Language::Go.stage_deps resources, buildpath/"src"
-
-    system "go", "build", "jvgrep.go"
-    bin.install "jvgrep"
+    system "go", "build", "-o", bin/"jvgrep", "jvgrep.go"
   end
 
   test do
     (testpath/"Hello.txt").write("Hello World!")
-    system "#{bin}/jvgrep", "Hello World!", testpath
+    system bin/"jvgrep", "Hello World!", testpath
   end
 end
