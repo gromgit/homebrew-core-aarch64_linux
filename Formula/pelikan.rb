989cc626b87e20edd2305771e5b845adffed4938
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-19 22:27:14 +0100

    pelikan: update 0.1.2 bottle.

--- Formula/pelikan.rb
+++ Formula/pelikan.rb
@@ -7,9 +7,9 @@ class Pelikan < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "469116bf1661a4fc3fed1473bcf9619836226e14a6459d83ed67f5eed1c968ee" => :el_capitan
-    sha256 "b7370736251899a21560a4cd3d84d103c0f8ce11afa66a2663104e70f72837ad" => :yosemite
-    sha256 "654d1adc30a7585c7fc590006fd14124d8341eb9608ae88eeebb629e20003fd6" => :mavericks
+    sha256 "e314ce6288bf76e271bf69ce844e2e846b16cad68ce635faf1e5130c3c6911d0" => :el_capitan
+    sha256 "ab04b8488e6272d0000c8e67842c4b286eb23459a6de9e9a392f14aa87c9978e" => :yosemite
+    sha256 "80459134cbab7aa94ab55d38488b2058696f7408869306f75e80cfa0350ed40d" => :mavericks
   end
 
   depends_on "cmake" => :build
