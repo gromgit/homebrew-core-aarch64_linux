511c1b12e56b06700b23476b71688b61d2bb2c6b
Jerzerak <jerzerak@gmail.com>
2016-06-18 22:18:16 -0400

    kobalt 0.818
    Closes #2129.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/kobalt.rb
+++ Formula/kobalt.rb
@@ -1,8 +1,8 @@
 class Kobalt < Formula
   desc "Build system"
   homepage "http://beust.com/kobalt"
-  url "https://github.com/cbeust/kobalt/releases/download/0.808/kobalt-0.808.zip"
-  sha256 "46a80809be6a73f8e18051aabb0fe59fb11b47b3fd732d96e5c68e44a247448e"
+  url "https://github.com/cbeust/kobalt/releases/download/0.818/kobalt-0.818.zip"
+  sha256 "e8878383a6e5bc06a54ab01a92a564cdcc8de0018cf79dae43a20f3b2d1490d0"
 
   bottle :unneeded
 
