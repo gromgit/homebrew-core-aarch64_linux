856dbc27b2a585437b074670d5b2d7acc3142687
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-19 02:09:51 +0100

    mlt: update 6.2.0 bottle.

--- Formula/mlt.rb
+++ Formula/mlt.rb
@@ -5,9 +5,9 @@ class Mlt < Formula
   sha256 "dd2ee742e89620de78a259790f92a7cadad67f0e0a6c1ea7ed932f96fb739fff"
 
   bottle do
-    sha256 "b12a98b91f51fa92d9b170556a1e622bba20a65734ba12eaefb79ed52a0357bd" => :el_capitan
-    sha256 "c144f768f4f12f2b54771f6f108ade717a8aefd1af1ff03659a07a2e4f39f868" => :yosemite
-    sha256 "31d9b9d88ee5e76c8635c00af0951e06430d9ee20d721f420b0f539ecfbf4eac" => :mavericks
+    sha256 "37b56fd9ab3a237a6034c3cd5705881f0d09fe8eab0154b187f7de2f963463bf" => :el_capitan
+    sha256 "ac7e712edae8c1a510e8ffae58c5388b8390242090c78f820ff7aa0cf9331fea" => :yosemite
+    sha256 "492c149e06c91af7083d1a454dbf61b1f7f90129f254496db794683521ee7899" => :mavericks
   end
 
   depends_on "pkg-config" => :build
