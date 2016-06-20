e1d950f4f757ef91e9301a1312db94b4cb1da761
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-20 13:55:19 +0100

    ditaa 0.10
    ditaa: add 0.10 bottle.
    
    Closes #858.
    
    Signed-off-by: Andrew Janke <janke@eilonwy.local>

--- Formula/ditaa.rb
+++ Formula/ditaa.rb
@@ -4,6 +4,13 @@ class Ditaa < Formula
   url "https://github.com/stathissideris/ditaa/archive/v0.10.tar.gz"
   sha256 "82e49065d408cba8b323eea0b7f49899578336d566096c6eb6e2d0a28745d63b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b5b3b93c92efa5c50f7bfae9e625fbdd29098f768c695ff41e1cec32bdb28f3a" => :el_capitan
+    sha256 "e0495f37f6c612a5684c5ac6a64f91617da90775ff6b0f61dbffdb7ead374a93" => :yosemite
+    sha256 "2d4a0abeb8a7a5802555697c1cefddadf2e5c996b902a062dc6543d5968a8f3e" => :mavericks
+  end
+
   depends_on :ant => :build
   depends_on :java
 
