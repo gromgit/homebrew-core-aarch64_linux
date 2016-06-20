fcdf83a9076a75a75da6409a79a8fdd66b5dfa1c
Saúl Ibarra Corretgé <saghul@gmail.com>
2016-06-20 03:54:26 +0200

    ringojs 0.12.0 (#2138)

--- Formula/ringojs.rb
+++ Formula/ringojs.rb
@@ -1,8 +1,8 @@
 class Ringojs < Formula
   desc "CommonJS-based JavaScript runtime"
   homepage "http://ringojs.org"
-  url "https://github.com/ringo/ringojs/releases/download/v0.11.0/ringojs-0.11.tar.gz"
-  sha256 "aafccc75f41f1c6e78fbc270a4f1788c506f31a640b5fa23bacd77daadf11d27"
+  url "https://github.com/ringo/ringojs/releases/download/v0.12.0/ringojs-0.12.tar.gz"
+  sha256 "1ec1a325d94bcb8512cd9e8e972525c34dd5871bbad92689f00efdfc109ba668"
 
   bottle :unneeded
 
@@ -10,5 +10,13 @@ class Ringojs < Formula
     rm Dir["bin/*.cmd"]
     libexec.install Dir["*"]
     bin.write_exec_script Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"test.js").write <<-EOS.undent
+        var x = 40 + 2;
+        console.assert(x === 42);
+    EOS
+    system "#{bin}/ringo", "test.js"
   end
 end
