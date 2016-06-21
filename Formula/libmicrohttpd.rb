84190a1a9d2d85d77a303ba494770ca5fc5977f2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-21 11:44:54 +0100

    libmicrohttpd: update 0.9.50 bottle.

--- Formula/libmicrohttpd.rb
+++ Formula/libmicrohttpd.rb
@@ -7,10 +7,9 @@ class Libmicrohttpd < Formula
 
   bottle do
     cellar :any
-    revision 1
-    sha256 "658b30965728ec52454a70fc3bdd032fc3d995e77966e02821ffea0eba170896" => :el_capitan
-    sha256 "b94ebccf25a21c4bdeedd0d1a227601142231210d4182a38897a04c89356c8e7" => :yosemite
-    sha256 "50ead40afe7f5144004abfd5769de833313de841e3499495fe164190fc78cc26" => :mavericks
+    sha256 "e2672267f1431be3c924ec2f9a99c6c1396e0cd21a2553379faf1e6ba0b48ad1" => :el_capitan
+    sha256 "f7a4141e0f89a6934d93a8ed65cb4c59c0c9f9e9e8f408619668f005aa47f9c5" => :yosemite
+    sha256 "3a45b0257777cb5639b38ad88800307cd04e7faadedec3efb531a76c6254dfff" => :mavericks
   end
 
   option "with-ssl", "Enable SSL support"
