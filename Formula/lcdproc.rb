6a4d05819b7b79389066cffa67952b6cb7e683a2
ilovezfs <ilovezfs@icloud.com>
2016-06-21 06:15:06 -0700

    lcdproc 0.5.7
    also had to map uint -> u_int to fix the build
    
    Closes #2232.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/lcdproc.rb
+++ Formula/lcdproc.rb
@@ -1,8 +1,8 @@
 class Lcdproc < Formula
   desc "Display real-time system information on a LCD"
   homepage "http://www.lcdproc.org/"
-  url "https://downloads.sourceforge.net/project/lcdproc/lcdproc/0.5.6/lcdproc-0.5.6.tar.gz"
-  sha256 "bd2f43c30ff43b30f43110abe6b4a5bc8e0267cb9f57fa97cc5e5ef9488b984a"
+  url "https://downloads.sourceforge.net/project/lcdproc/lcdproc/0.5.7/lcdproc-0.5.7.tar.gz"
+  sha256 "843007d377adc856529ed0c7c42c9a7563043f06b1b73add0372bba3a3029804"
 
   bottle do
     sha256 "2a7bd5effc794f54e2364b74caba344f949a288b4ae67e55e829e6256a585732" => :el_capitan
@@ -14,6 +14,13 @@ class Lcdproc < Formula
   depends_on "libusb"
   depends_on "libhid"
   depends_on "libftdi0"
+
+  # Fixes build failure "error: unknown type name 'uint'"
+  # Reported 21 Jun 2016: https://sourceforge.net/p/lcdproc/patches/26/
+  patch do
+    url "https://sourceforge.net/p/lcdproc/patches/26/attachment/darwin-uint.diff"
+    sha256 "174a812f325bf60f801252ca1d680b1b781ae421ae54066b699276905423c7e1"
+  end
 
   def install
     system "./configure", "--disable-debug",
