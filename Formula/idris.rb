0e6c0debebf7b3302cb91e9078c77d0d1a9d0303
ilovezfs <ilovezfs@icloud.com>
2016-06-21 11:34:51 -0700

    idris: GHC 8 compatibility
    - restore the Cabal-based build
    - use upstream PR for GHC 8 compatibility
    
    Closes #2253.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/idris.rb
+++ Formula/idris.rb
@@ -7,6 +7,8 @@ class Idris < Formula
   homepage "http://www.idris-lang.org"
   url "https://github.com/idris-lang/Idris-dev/archive/v0.12.tar.gz"
   sha256 "dfc31dffd1bafd996a951cbcc551a69337f16a3fa5c2974ec872d62a38bd7c75"
+  revision 1
+
   head "https://github.com/idris-lang/Idris-dev.git"
 
   bottle do
@@ -15,31 +17,30 @@ class Idris < Formula
     sha256 "19ec0259b661319c5ec12fc71c64de050305946bcd145161defff9d64fca43e4" => :mavericks
   end
 
-  depends_on "haskell-stack" => :build
+  depends_on "ghc" => :build
+  depends_on "cabal-install" => :build
   depends_on "pkg-config" => :build
+
   depends_on "gmp"
-  depends_on "libffi"
+  depends_on "libffi" => :recommended
+
+  # GHC 8 compat
+  # Adapted from upstream PR: https://github.com/idris-lang/Idris-dev/pull/3226
+  # Fixes https://github.com/idris-lang/Idris-dev/issues/3193
+  # Underlying GHC 8 issue: https://ghc.haskell.org/trac/ghc/ticket/12201
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/49941f6249e5b05dfd180abb4776f9e3a3bb25d8/idris/idris-ghc8.diff"
+    sha256 "002e2b6d8392d451ecf0ddb37f181e3c87ed42ca0eee82cc4f8aba76e5e1cc11"
+  end
 
   def install
-    ENV["STACK_ROOT"] = "#{HOMEBREW_CACHE}/stack_root"
-    (libexec/"stack").install Dir["#{buildpath}/*"]
+    # GHC 8 compat for trifecta dependency; already fixed in trifecta HEAD
+    (buildpath/"cabal.config").write("allow-newer: comonad,transformers\n")
 
-    cd libexec/"stack" do
-      system "stack", "setup"
-      system "stack", "build"
-
-      bin.mkpath
-      system "stack", "install", "--local-bin-path=#{bin}"
-
-      install_dir = Utils.popen_read("stack", "path", "--local-install-root").chomp
-
-      share.mkpath
-      mv "#{install_dir}/share/man", share
-      mv "#{install_dir}/doc", share
-
-      libexec.install_symlink "#{install_dir}/lib"
-      libexec.install_symlink "#{install_dir}/share"
-    end
+    args = []
+    args << "-f FFI" if build.with? "libffi"
+    args << "-f release" if build.stable?
+    install_cabal_package *args
   end
 
   test do
@@ -49,18 +50,20 @@ class Idris < Formula
       main = putStrLn "Hello, Homebrew!"
     EOS
 
-    (testpath/"ffi.idr").write <<-EOS.undent
-      module Main
-      puts: String -> IO ()
-      puts x = foreign FFI_C "puts" (String -> IO ()) x
+    system bin/"idris", "hello.idr", "-o", "hello"
+    assert_equal "Hello, Homebrew!", shell_output("./hello").chomp
 
-      main : IO ()
-      main = puts "Hello, interpreter!"
-    EOS
-    system bin/"idris", testpath/"hello.idr", "-o", testpath/"hello"
-    assert_match /Hello, Homebrew!/, shell_output(testpath/"hello")
+    if build.with? "libffi"
+      (testpath/"ffi.idr").write <<-EOS.undent
+        module Main
+        puts: String -> IO ()
+        puts x = foreign FFI_C "puts" (String -> IO ()) x
+        main : IO ()
+        main = puts "Hello, interpreter!"
+      EOS
 
-    system bin/"idris", testpath/"ffi.idr", "-o", testpath/"ffi"
-    assert_match /Hello, interpreter!/, shell_output(testpath/"ffi")
+      system bin/"idris", "ffi.idr", "-o", "ffi"
+      assert_equal "Hello, interpreter!", shell_output("./ffi").chomp
+    end
   end
 end
