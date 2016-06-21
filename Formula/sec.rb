f6894f252f01d6f2a7296fcb496e3ecc8fa881b8
Tomasz Pajor <tomek@polishgeeks.com>
2016-06-21 10:12:36 +0200

    sec 2.7.10
    Closes #2227.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/sec.rb
+++ Formula/sec.rb
@@ -1,9 +1,9 @@
 class Sec < Formula
   desc "Event correlation tool for event processing of various kinds"
   homepage "http://simple-evcorr.sourceforge.net/"
-  url "https://github.com/simple-evcorr/sec/releases/download/2.7.8/sec-2.7.8.tar.gz"
-  mirror "https://downloads.sourceforge.net/project/simple-evcorr/sec/2.7.8/sec-2.7.8.tar.gz"
-  sha256 "4771d0c7b45937cce263ca9728cf243f82e44b2ef153da0e1a478c6b1e46537f"
+  url "https://github.com/simple-evcorr/sec/releases/download/2.7.10/sec-2.7.10.tar.gz"
+  mirror "https://downloads.sourceforge.net/project/simple-evcorr/sec/2.7.10/sec-2.7.10.tar.gz"
+  sha256 "8c1441db830d3689aa201c1b0a5e46a97a22e8187d3e0d1c2dbd8abbd47c3d21"
 
   bottle :unneeded
 
