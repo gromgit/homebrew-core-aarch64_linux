1b203bd4aeebf23a5666c508b32385f541fa6ba8
Tomasz Pajor <tomek@polishgeeks.com>
2016-06-21 10:57:31 +0200

    casperjs 1.1.2
    Closes #2231.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/casperjs.rb
+++ Formula/casperjs.rb
@@ -1,18 +1,9 @@
 class Casperjs < Formula
   desc "Navigation scripting and testing tool for PhantomJS"
   homepage "http://www.casperjs.org/"
-
-  stable do
-    url "https://github.com/n1k0/casperjs/archive/1.0.4.tar.gz"
-    sha256 "d71b9dd77ac202f3fbb958f8876f12b89aee2a1b09b2c2c55fd11aa928a1fb1f"
-
-    # https://github.com/Homebrew/homebrew/pull/38632
-    # Once 1.1.x is stable combine all the PhantomJS resource into one.
-    resource "phantomjs" do
-      url "https://phantomjs.googlecode.com/files/phantomjs-1.8.2-macosx.zip"
-      sha256 "7d19c1cce6c66bb3153d335522b4effe68ddd249f427776b82f2662fb5ed81cf"
-    end
-  end
+  url "https://github.com/casperjs/casperjs/archive/1.1.2.tar.gz"
+  sha256 "f4f4c3dd99c029dd6a52d0415e5a4dd6527df7ea6b7bb18468b0cda888a2a61a"
+  head "https://github.com/casperjs/casperjs.git"
 
   bottle do
     cellar :any_skip_relocation
@@ -23,38 +14,13 @@ class Casperjs < Formula
     sha256 "fc20c22ec10d175d84bec4dd05d83fa8ea7fc78723a4e8d0f4041d5c00878651" => :mountain_lion
   end
 
-  devel do
-    url "https://github.com/n1k0/casperjs/archive/1.1-beta4.tar.gz"
-    sha256 "144e9a32d8ca677419924f6183aad26e0061d2c08d79abdb998796c010553654"
-    version "1.1-beta4"
-
-    resource "phantomjs" do
-      url "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-macosx.zip"
-      sha256 "8f15043ae3031815dc5f884ea6ffa053d365491b1bc0dc3a0862d5ff1ac20a48"
-    end
-  end
-
-  head do
-    url "https://github.com/n1k0/casperjs.git"
-
-    resource "phantomjs" do
-      url "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-macosx.zip"
-      sha256 "8f15043ae3031815dc5f884ea6ffa053d365491b1bc0dc3a0862d5ff1ac20a48"
-    end
-  end
-
   # For embedded Phantomjs
   depends_on :macos => :snow_leopard
+  depends_on "phantomjs"
 
   def install
     libexec.install Dir["*"]
-    (libexec/"phantomjs").install resource("phantomjs")
-
-    (bin/"casperjs").write <<-EOS.undent
-      #!/bin/bash
-      export PATH=#{libexec}/phantomjs/bin:$PATH
-      exec "#{libexec}/bin/casperjs" "$@"
-    EOS
+    bin.install_symlink libexec/"bin/casperjs"
   end
 
   test do
