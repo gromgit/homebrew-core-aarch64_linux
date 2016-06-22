e4b5975d398c7b66ba1fb259829dd5f4b40a8aa8
ilovezfs <ilovezfs@icloud.com>
2016-06-22 07:06:49 -0700

    mysqltuner 1.6.0
    Closes #2307.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mysqltuner.rb
+++ Formula/mysqltuner.rb
@@ -1,8 +1,8 @@
 class Mysqltuner < Formula
   desc "Increase performance and stability of a MySQL installation"
   homepage "http://mysqltuner.com"
-  url "https://github.com/major/MySQLTuner-perl/archive/v1.4.0.tar.gz"
-  sha256 "8d5a03b64da2164c6bd93b79700c93db088a14155bcc8cb63c65d049909d793e"
+  url "https://github.com/major/MySQLTuner-perl/archive/1.6.0.tar.gz"
+  sha256 "dc3045b9ffae7837d187d2b1ef4c42648bf7ffc6bb9f69864a4bfeecd5205e37"
   head "https://github.com/major/MySQLTuner-perl.git"
 
   bottle :unneeded
