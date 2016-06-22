192ff2bca08c267987b49e211078b52d833aaaab
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-22 05:54:59 +0100

    b2sum: add 20160619 bottle.

--- Formula/b2sum.rb
+++ Formula/b2sum.rb
@@ -4,6 +4,13 @@ class B2sum < Formula
   url "https://github.com/BLAKE2/BLAKE2/archive/20160619.tar.gz"
   sha256 "cbac833ccae56b5c6173dbeaf871aa99b32745cf7a994c7451d4533ecda55633"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ff4a36110ddee427728c3c7027b6e64d4ecdcab4bbe73bf69cf200cbfd877657" => :el_capitan
+    sha256 "ac07e18f25467617297e60a4a46d2309241ecf16adc642d8ef0abfbd6dd2cd70" => :yosemite
+    sha256 "d2adbbbe2cce0efe380586ebbfd6a765fdc092c9d205134119613b0dbf9964ab" => :mavericks
+  end
+
   def install
     cd "b2sum" do
       system "make", "NO_OPENMP=1"
