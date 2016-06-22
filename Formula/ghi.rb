220f885e3f9458cfa4d876065558e4bbd7ec4bf9
ilovezfs <ilovezfs@icloud.com>
2016-06-22 06:13:30 -0700

    ghi 1.2.0
    Closes #2288.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/ghi.rb
+++ Formula/ghi.rb
@@ -1,8 +1,8 @@
 class Ghi < Formula
   desc "Work on GitHub issues on the command-line"
   homepage "https://github.com/stephencelis/ghi"
-  url "https://github.com/stephencelis/ghi/archive/1.1.1.tar.gz"
-  sha256 "a19fd947f1268d9f087d2a342964dfc1cb3aa96de970e82b8daf22461e07e49b"
+  url "https://github.com/stephencelis/ghi/archive/1.2.0.tar.gz"
+  sha256 "ffc17cfbdc8b88bf208f5f762e62c211bf8fc837f447354ad53cce39b1400671"
   head "https://github.com/stephencelis/ghi.git"
 
   bottle :unneeded
