2e692e103023f21fb9e0bb4709f6408ee073f6d7
ilovezfs <ilovezfs@icloud.com>
2016-06-22 09:26:03 -0700

    dbus 1.11.2 (devel)
    also restore the 1.10.8 bottles
    
    Closes #2320.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dbus.rb
+++ Formula/dbus.rb
@@ -2,15 +2,21 @@ class Dbus < Formula
   # releases: even (1.10.x) = stable, odd (1.11.x) = development
   desc "Message bus system, providing inter-application communication"
   homepage "https://wiki.freedesktop.org/www/Software/dbus"
-  url "https://dbus.freedesktop.org/releases/dbus/dbus-1.11.2.tar.gz"
-  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/d/dbus/dbus_1.11.2.orig.tar.gz"
-  sha256 "5abc4c57686fa82669ad0039830788f9b03fdc4fff487f0ccf6c9d56ba2645c9"
+  url "https://dbus.freedesktop.org/releases/dbus/dbus-1.10.8.tar.gz"
+  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/d/dbus/dbus_1.10.8.orig.tar.gz"
+  sha256 "baf3d22baa26d3bdd9edc587736cd5562196ce67996d65b82103bedbe1f0c014"
   head "https://anongit.freedesktop.org/git/dbus/dbus.git"
 
   bottle do
-    sha256 "54760666294128a120c17850b350d2af7200be7b71aa02829b6f7daa7cf928ee" => :el_capitan
-    sha256 "7ccdf677ca9a58320649671adbc32d6145ae307a60cb7e7b4b3c19c94b2da482" => :yosemite
-    sha256 "1e85ebe4cfc55de6971e60f19774821a56c907f403d845553d7a38eeb29c6494" => :mavericks
+    sha256 "df858961c007d3f1bf7d2bc03856b4f1981b9c260837516da523e9d8162a3e46" => :el_capitan
+    sha256 "e3bd3fc1b0a8eee96eda8dedbb4f610ac8c8a4b303f05c0f31be6bff55d573cd" => :yosemite
+    sha256 "397cd888dcf08bd86c08e7c667663b896c65099c80ebe2901aa81050a9480541" => :mavericks
+  end
+
+  devel do
+    url "https://dbus.freedesktop.org/releases/dbus/dbus-1.11.2.tar.gz"
+    mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/d/dbus/dbus_1.11.2.orig.tar.gz"
+    sha256 "5abc4c57686fa82669ad0039830788f9b03fdc4fff487f0ccf6c9d56ba2645c9"
   end
 
   # Patch applies the config templating fixed in https://bugs.freedesktop.org/show_bug.cgi?id=94494
