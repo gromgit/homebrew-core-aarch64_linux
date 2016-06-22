06fa321c8568d1f05d494adc832989a0ac3ceefe
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-22 14:29:43 +0100

    gti: update 1.3.0 bottle.

--- Formula/gti.rb
+++ Formula/gti.rb
@@ -8,10 +8,9 @@ class Gti < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a2dc006bbf70e86ba6a4535d20eb5c6d8a25685f2a5c7ce4f87bfbfef1097443" => :el_capitan
-    sha256 "12a2fcc7465f9fba5cdaa92ea0af3e5ef459f42b4c5c930017fef0fb3366f121" => :yosemite
-    sha256 "308b88323f417d45c627add34f7b74b9f63cbc3f3065b3b3909908a3b78769cf" => :mavericks
-    sha256 "2e19e781ff48dee619cadbda1937d7a7a295ddbaa29af4b8881d211ea27b0c57" => :mountain_lion
+    sha256 "b253c79ace49639af50fbe80ef39b584d2e4ebc26bf4e7069801790dee5fe000" => :el_capitan
+    sha256 "b5ec12785d74eca21caa9eaebb089f6d666ab4a4e5516f4c007bbb2f40d55e36" => :yosemite
+    sha256 "ec53667c16637cad636131f5b34885a517814531991160cf696e7713ebe2a36b" => :mavericks
   end
 
   def install
