dfb19f099bcd6fef1e42d32cc9d093d32eea7390
Dominyk Tiller <dominyktiller@gmail.com>
2016-06-22 21:56:18 +0100

    libsoup 2.54.1

--- Formula/libsoup.rb
+++ Formula/libsoup.rb
@@ -1,9 +1,8 @@
 class Libsoup < Formula
   desc "HTTP client/server library for GNOME"
   homepage "https://live.gnome.org/LibSoup"
-  url "https://download.gnome.org/sources/libsoup/2.52/libsoup-2.52.2.tar.xz"
-  sha256 "db55628b5c7d952945bb71b236469057c8dfb8dea0c271513579c6273c2093dc"
-  revision 1
+  url "https://download.gnome.org/sources/libsoup/2.54/libsoup-2.54.1.tar.xz"
+  sha256 "47b42c232034734d66e5f093025843a5d8cc4b2357c011085a2fd04ef02dd633"
 
   bottle do
     sha256 "de28079d444082ea4f6f2aca4388152aa8d303926917baa5f656bdd7d222ef72" => :el_capitan
@@ -20,17 +19,20 @@ class Libsoup < Formula
   depends_on "vala"
 
   def install
-    args = [
-      "--disable-debug",
-      "--disable-dependency-tracking",
-      "--disable-silent-rules",
-      "--prefix=#{prefix}",
-      "--without-gnome",
-      "--disable-tls-check",
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --disable-tls-check
+      --enable-introspection=yes
     ]
 
     # ensures that the vala files remain within the keg
-    inreplace "libsoup/Makefile.in", "VAPIDIR = @VAPIDIR@", "VAPIDIR = @datadir@/vala/vapi"
+    inreplace "libsoup/Makefile.in",
+              "VAPIDIR = @VAPIDIR@",
+              "VAPIDIR = @datadir@/vala/vapi"
+
     system "./configure", *args
     system "make", "install"
   end
