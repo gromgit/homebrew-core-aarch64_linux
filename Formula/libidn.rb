e97c3a845094f8e24e51ce3ff0da86a4ed65bfc8
Viktor Szakats <vszakats@users.noreply.github.com>
2016-06-23 15:40:45 +0200

    libidn: use secure url

--- Formula/libidn.rb
+++ Formula/libidn.rb
@@ -1,7 +1,7 @@
 class Libidn < Formula
   desc "International domain name library"
   homepage "https://www.gnu.org/software/libidn/"
-  url "http://ftpmirror.gnu.org/libidn/libidn-1.32.tar.gz"
+  url "https://ftpmirror.gnu.org/libidn/libidn-1.32.tar.gz"
   mirror "https://ftp.gnu.org/gnu/libidn/libidn-1.32.tar.gz"
   sha256 "ba5d5afee2beff703a34ee094668da5c6ea5afa38784cebba8924105e185c4f5"
 
