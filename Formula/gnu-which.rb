03650f0cff79515bee258a854395947a43f3c41f
Viktor Szakats <vszakats@users.noreply.github.com>
2016-06-23 11:48:29 +0200

    gnu-which: use secure url
    Closes #2339.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/gnu-which.rb
+++ Formula/gnu-which.rb
@@ -2,7 +2,7 @@ class GnuWhich < Formula
   desc "GNU implementation of which utility"
   # Previous homepage is dead. Have linked to the GNU Projects page for now.
   homepage "https://savannah.gnu.org/projects/which/"
-  url "http://ftpmirror.gnu.org/which/which-2.21.tar.gz"
+  url "https://ftpmirror.gnu.org/which/which-2.21.tar.gz"
   mirror "https://ftp.gnu.org/gnu/which/which-2.21.tar.gz"
   sha256 "f4a245b94124b377d8b49646bf421f9155d36aa7614b6ebf83705d3ffc76eaad"
 
