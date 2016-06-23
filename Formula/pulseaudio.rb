81d16889311cc07b6271774017b0f6b439a7da2b
ilovezfs <ilovezfs@icloud.com>
2016-06-23 04:05:55 -0700

    pulseaudio 9.0
    Upstream fixed CoreServices header location issue so remove patch :DATA
    
    Closes #2296.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pulseaudio.rb
+++ Formula/pulseaudio.rb
@@ -1,8 +1,8 @@
 class Pulseaudio < Formula
   desc "Sound system for POSIX OSes"
   homepage "https://wiki.freedesktop.org/www/Software/PulseAudio/"
-  url "https://www.freedesktop.org/software/pulseaudio/releases/pulseaudio-8.0.tar.xz"
-  sha256 "690eefe28633466cfd1ab9d85ebfa9376f6b622deec6bfee5091ac9737cd1989"
+  url "https://www.freedesktop.org/software/pulseaudio/releases/pulseaudio-9.0.tar.xz"
+  sha256 "c3d3d66b827f18fbe903fe3df647013f09fc1e2191c035be1ee2d82a9e404686"
 
   bottle do
     revision 1
@@ -48,10 +48,6 @@ class Pulseaudio < Formula
     sha256 "d3a2180600a4fbea538949b6c4e9e70fe7997495663334e50db96d18bfb1da5f"
   end
 
-  # Fix CoreServices header location check in configure for Xcode-only
-  # https://bugs.freedesktop.org/show_bug.cgi?id=55152
-  patch :DATA
-
   fails_with :clang do
     build 421
     cause "error: thread-local storage is unsupported for the current target"
@@ -89,41 +85,3 @@ class Pulseaudio < Formula
     system bin/"pulseaudio", "--dump-modules"
   end
 end
-
-__END__
-diff --git a/configure b/configure
-index a3c0fa4..39a7da8 100755
---- a/configure
-+++ b/configure
-@@ -22314,29 +22314,14 @@ fi
- if test "x$os_is_darwin" = "x1" ; then
-     { $as_echo "$as_me:${as_lineno-$LINENO}: checking looking for Apple CoreService Framework" >&5
- $as_echo_n "checking looking for Apple CoreService Framework... " >&6; }
--    # How do I check a framework "library" - AC_CHECK_LIB prob. won't work??, just assign LIBS & hope
--    ac_fn_c_check_header_mongrel "$LINENO" "/Developer/Headers/FlatCarbon/CoreServices.h" "ac_cv_header__Developer_Headers_FlatCarbon_CoreServices_h" "$ac_includes_default"
--if test "x$ac_cv_header__Developer_Headers_FlatCarbon_CoreServices_h" = xyes; then :
-+    ac_fn_c_check_header_mongrel "$LINENO" "CoreServices/CoreServices.h" "ac_cv_header_CoreServices_CoreServices_h" "$ac_includes_default"
-+if test "x$ac_cv_header_CoreServices_CoreServices_h" = xyes; then :
-   LIBS="$LIBS -framework CoreServices"
- else
--  for ac_header in /System/Library/Frameworks/CoreServices.framework/Headers/CoreServices.h
--do :
--  ac_fn_c_check_header_mongrel "$LINENO" "/System/Library/Frameworks/CoreServices.framework/Headers/CoreServices.h" "ac_cv_header__System_Library_Frameworks_CoreServices_framework_Headers_CoreServices_h" "$ac_includes_default"
--if test "x$ac_cv_header__System_Library_Frameworks_CoreServices_framework_Headers_CoreServices_h" = xyes; then :
--  cat >>confdefs.h <<_ACEOF
--#define HAVE__SYSTEM_LIBRARY_FRAMEWORKS_CORESERVICES_FRAMEWORK_HEADERS_CORESERVICES_H 1
--_ACEOF
-- LIBS="$LIBS -framework CoreServices"
--else
-   as_fn_error $? "CoreServices.h header file not found" "$LINENO" 5
-
- fi
-
--done
--
--
--fi
--
-
-
-     { $as_echo "$as_me:${as_lineno-$LINENO}: result: ok" >&5
