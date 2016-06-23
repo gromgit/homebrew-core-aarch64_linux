44570c432a2063cafe7cf8a4ce977db6e66b4dd7
Viktor Szakats <vszakats@users.noreply.github.com>
2016-06-23 11:41:41 +0200

    binutils: use secure url

--- Formula/binutils.rb
+++ Formula/binutils.rb
@@ -1,7 +1,7 @@
 class Binutils < Formula
   desc "FSF Binutils for native development"
   homepage "https://www.gnu.org/software/binutils/binutils.html"
-  url "http://ftpmirror.gnu.org/binutils/binutils-2.26.tar.gz"
+  url "https://ftpmirror.gnu.org/binutils/binutils-2.26.tar.gz"
   mirror "https://ftp.gnu.org/gnu/binutils/binutils-2.26.tar.gz"
   sha256 "9615feddaeedc214d1a1ecd77b6697449c952eab69d79ab2125ea050e944bcc1"
 
