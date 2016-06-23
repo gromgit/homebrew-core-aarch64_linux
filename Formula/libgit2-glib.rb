7628a100b0c7b768e041e7f9320b31d2ffd35df4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-23 07:06:19 +0100

    libgit2-glib: update 0.24.0 bottle.

--- Formula/libgit2-glib.rb
+++ Formula/libgit2-glib.rb
@@ -5,9 +5,9 @@ class Libgit2Glib < Formula
   sha256 "d616c268821c28ff8dc1a6419dbf8555fa48e31dc6509c10f5151be7690f4845"
 
   bottle do
-    sha256 "e1707622fa3434b1bec14221080022534fa830f6eaf9ecc97581e39ce4460dcf" => :el_capitan
-    sha256 "a7cfb651228710cfb4188580a0b13a9e3e09d2c7c0fd6be43175f121f7d3bae8" => :yosemite
-    sha256 "5eec43959952f6105784cae5afeefa93666c2d2c37b535e7d26e87403a35fcdb" => :mavericks
+    sha256 "aa8656e213e6eb56bbc095e15f56c20b6015fb89a82a960323473da59da592c0" => :el_capitan
+    sha256 "5b1b074b2862cda8d3afec6cf6f59662f2b33766168ead3206e5ea3d2b96e323" => :yosemite
+    sha256 "947fe370112c6a238d4cb1ccb6e8523832d49120eb2efad7a897e91b0ee9b601" => :mavericks
   end
 
   head do
