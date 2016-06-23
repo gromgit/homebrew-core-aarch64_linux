bcf8ab284048df70818145d125b6be89a80d9603
Viktor Szakats <vszakats@users.noreply.github.com>
2016-06-23 15:36:25 +0200

    autoconf: use secure url, fix audit nit (one remains)

--- Formula/autoconf.rb
+++ Formula/autoconf.rb
@@ -1,7 +1,7 @@
 class Autoconf < Formula
   desc "Automatic configure script builder"
   homepage "https://www.gnu.org/software/autoconf"
-  url "http://ftpmirror.gnu.org/autoconf/autoconf-2.69.tar.gz"
+  url "https://ftpmirror.gnu.org/autoconf/autoconf-2.69.tar.gz"
   mirror "https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz"
   sha256 "954bd69b391edc12d6a4a51a2dd1476543da5c6bbf05a95b59dc0dd6fd4c2969"
 
@@ -32,7 +32,7 @@ class Autoconf < Formula
   end
 
   test do
-    cp "#{share}/autoconf/autotest/autotest.m4", "autotest.m4"
+    cp "#{pkgshare}/autotest/autotest.m4", "autotest.m4"
     system "#{bin}/autoconf", "autotest.m4"
   end
 end
