10b85245dcef439b24dc76e8a9dac210d3027cdf
ilovezfs <ilovezfs@icloud.com>
2016-06-23 18:04:08 -0700

    shpotify 1.1.0
    Closes #2352.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/shpotify.rb
+++ Formula/shpotify.rb
@@ -1,8 +1,8 @@
 class Shpotify < Formula
   desc "Command-line interface for Spotify on a Mac"
   homepage "https://harishnarayanan.org/projects/shpotify/"
-  url "https://github.com/hnarayanan/shpotify/archive/1.0.0.tar.gz"
-  sha256 "4bdc2bd488132604c9b9e850816db1df77ab468144972f9649316593fb4a6ac0"
+  url "https://github.com/hnarayanan/shpotify/archive/1.1.0.tar.gz"
+  sha256 "825de0c1100b3f596e635765b02f65c73919a26990d5d4520e87afea45147987"
   head "https://github.com/hnarayanan/shpotify.git"
 
   bottle :unneeded
