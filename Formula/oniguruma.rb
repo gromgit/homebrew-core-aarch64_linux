b88bdd988b04d052e43bbbddcc8b747fca7929a4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-25 01:47:00 +0100

    oniguruma: update 6.0.0 bottle.

--- Formula/oniguruma.rb
+++ Formula/oniguruma.rb
@@ -6,11 +6,9 @@ class Oniguruma < Formula
 
   bottle do
     cellar :any
-    revision 1
-    sha256 "653e3734097028be0863cdab51a8d3c92c1e3206cfec50072c18ab9e4e36bfc3" => :el_capitan
-    sha256 "6a645e4c7bcf88d3d50aee632de816d884af7edf094fe793b451256e55bb8531" => :yosemite
-    sha256 "ed3f1ef356a9dcae1ffff0b9bd853314b07289dfacdbf6b403515bcd35233227" => :mavericks
-    sha256 "5a2ac152702eea6000bfa493607393f610fbec9684f1852cceb87834f20c9321" => :mountain_lion
+    sha256 "48b9a51758acc36c5217cb696910c5ffa344740955f822352346cc136e9d476e" => :el_capitan
+    sha256 "3d14f28ef7d925a56f582946d75333fbe8193843886c9dd2be198f0304ee0c8b" => :yosemite
+    sha256 "7dc18d75d30b020bcdca7105552277a52e78dbd90e9d963bb3812706a542179c" => :mavericks
   end
 
   def install
