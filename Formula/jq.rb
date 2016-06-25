13f3c8ad992d68d979ba054f8ec49f5c56ac6b67
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-25 01:47:00 +0100

    jq: update 1.5_1 bottle.

--- Formula/jq.rb
+++ Formula/jq.rb
@@ -7,10 +7,9 @@ class Jq < Formula
 
   bottle do
     cellar :any
-    revision 2
-    sha256 "4df3ac073164d99ecc20e6d0cdc363204ba992851fdd1f4a6d92eca3aae4671c" => :el_capitan
-    sha256 "c09bf921664a2d9fe9a9c1dc19b739a1b8e92ffc67e5a833c95db6687a02c200" => :yosemite
-    sha256 "6dc80e1c8032777ab107ec8ea650328cb3a9960d489877aa966924f533fee29f" => :mavericks
+    sha256 "468a0a375aa532141ca7bb0eed2efab41db2e3dae84355aedb8886512d24c704" => :el_capitan
+    sha256 "2b8562c46727d448b6d4eb6c034ac1ad6ee5e42430dfdff3f59596421bb32b72" => :yosemite
+    sha256 "66971fb13cdad99dd2b672025b3c9dff80b060602aacb5b73db67600df8ba2bf" => :mavericks
   end
 
   head do
