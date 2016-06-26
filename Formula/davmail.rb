18477d68671229d34b6be80b6ab869e00788ad34
Tomasz Pajor <tomek@polishgeeks.com>
2016-06-26 21:04:21 +0200

    davmail 4.7.2-2427
    Closes #2407.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/davmail.rb
+++ Formula/davmail.rb
@@ -1,8 +1,8 @@
 class Davmail < Formula
   desc "POP/IMAP/SMTP/Caldav/Carddav/LDAP exchange gateway"
   homepage "http://davmail.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/davmail/davmail/4.5.1/davmail-4.5.1-2303.zip"
-  sha256 "d69f0c8cf6cf5b76c1fb0ff82fe7cecd22d470c83206fa3fecbe486c758c080d"
+  url "https://downloads.sourceforge.net/project/davmail/davmail/4.7.2/davmail-4.7.2-2427.zip"
+  sha256 "1c08bb97e08e1d29bee9bab0a58c70ef975c2f98bb91354c4ab3da283462823c"
 
   bottle :unneeded
 
