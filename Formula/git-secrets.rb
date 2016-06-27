1d065f356992ba1e12c9ed2f8588f10e99c9a5db
ilovezfs <ilovezfs@icloud.com>
2016-06-27 16:34:03 -0700

    git-secrets 1.2.1
    Closes #2438.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/git-secrets.rb
+++ Formula/git-secrets.rb
@@ -1,9 +1,8 @@
 class GitSecrets < Formula
   desc "Prevents you from committing sensitive information to a git repo"
   homepage "https://github.com/awslabs/git-secrets"
-  url "https://github.com/awslabs/git-secrets/archive/1.2.0.tar.gz"
-  sha256 "bb6d9b5a0ffd74b0533e47eee51d5cdf6e0068cd2e066b8bb5df229821bce497"
-
+  url "https://github.com/awslabs/git-secrets/archive/1.2.1.tar.gz"
+  sha256 "9899907609b227e495725af7cf094cf9e09a8d732945db24ef5558fd0d6ad5ef"
   head "https://github.com/awslabs/git-secrets.git"
 
   bottle do
