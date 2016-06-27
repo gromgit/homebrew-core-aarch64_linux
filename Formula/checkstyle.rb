39290053502cc706dd3e7896513cbfd0d1e65c70
ilovezfs <ilovezfs@icloud.com>
2016-06-26 21:42:08 -0700

    checkstyle 7.0
    Closes #2418.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/checkstyle.rb
+++ Formula/checkstyle.rb
@@ -1,8 +1,8 @@
 class Checkstyle < Formula
   desc "Check Java source against a coding standard"
   homepage "http://checkstyle.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/checkstyle/checkstyle/6.19/checkstyle-6.19-all.jar"
-  sha256 "e0aca3445db90c481123f494b25c6add685fcad138c30c0a909052f2ad610360"
+  url "https://downloads.sourceforge.net/project/checkstyle/checkstyle/7.0/checkstyle-7.0-all.jar"
+  sha256 "a75780059d4580339d8b2e89edf731cbf4d935aa6413f940ccb1306ed50a8fd8"
 
   bottle :unneeded
 
