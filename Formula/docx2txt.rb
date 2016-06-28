b7ec946d9468170df8a0784340e8b39763bc0d97
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-28 02:24:06 +0100

    docx2txt: add 1.4 bottle.

--- Formula/docx2txt.rb
+++ Formula/docx2txt.rb
@@ -4,6 +4,13 @@ class Docx2txt < Formula
   url "https://downloads.sourceforge.net/docx2txt/docx2txt-1.4.tgz"
   sha256 "b297752910a404c1435e703d5aedb4571222bd759fa316c86ad8c8bbe58c6d1b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c3a67138c91e968e6c2a6ff1033bca0fe8527ebdcaaa208194c073b4f75dd453" => :el_capitan
+    sha256 "78154a4b95613538a9d508c521d74d0bc6b398b005de4468b4cb4e62c3208b8e" => :yosemite
+    sha256 "2d9f0f37b4c6c5a37f22a4b0e7cdc6d440e842d2d3e7df433ccebf1b03cf80cd" => :mavericks
+  end
+
   resource "sample_doc" do
     url "http://calibre-ebook.com/downloads/demos/demo.docx", :using => :nounzip
     sha256 "269329fc7ae54b3f289b3ac52efde387edc2e566ef9a48d637e841022c7e0eab"
