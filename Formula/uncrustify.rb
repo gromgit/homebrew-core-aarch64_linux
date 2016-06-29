51b63e2c6c40d74480278b67a037ebccfbca4f31
Gary Ash <Gary.Ash@icloud.com>
2016-06-28 22:38:55 -0400

    uncrustify: fix HEAD build (#2413)
    * fixed the build from source and HEAD options
    
    * fixed a url
    
    * following advice

--- Formula/uncrustify.rb
+++ Formula/uncrustify.rb
@@ -4,8 +4,6 @@ class Uncrustify < Formula
   url "https://downloads.sourceforge.net/project/uncrustify/uncrustify/uncrustify-0.63/uncrustify-0.63.tar.gz"
   sha256 "dffbb1341a8d208e0c76b65209750e34e75b29c5a0e9a5d5a943df58bfdc2ae3"
 
-  head "https://github.com/uncrustify/uncrustify.git"
-
   bottle do
     cellar :any_skip_relocation
     sha256 "387c422d75c6f1ca680cc4a0b8541dda9aeb40cd7b3fcf685efddbf1f29ea2d6" => :el_capitan
@@ -13,7 +11,15 @@ class Uncrustify < Formula
     sha256 "7bbc100632b13c81d2ec3ab897629f49effbb5dc76f991c4abde65566742b868" => :mavericks
   end
 
+  head do
+    url "https://github.com/uncrustify/uncrustify.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   def install
+    system "./autogen.sh" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
