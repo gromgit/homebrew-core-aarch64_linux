8c01ef687a67e1449cc4a5aede3f90b9c5769bde
Tomasz Pajor <tomek@polishgeeks.com>
2016-06-29 11:27:20 +0200

    plzip 1.5
    Closes #2489.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/plzip.rb
+++ Formula/plzip.rb
@@ -1,8 +1,8 @@
 class Plzip < Formula
   desc "Data compressor"
   homepage "http://www.nongnu.org/lzip/plzip.html"
-  url "http://download.savannah.gnu.org/releases/lzip/plzip/plzip-1.4.tar.gz"
-  sha256 "2a152ee429495cb96c22a51b618d1d19882db3e24aff79329d9c755a2a2f67bb"
+  url "http://download.savannah.gnu.org/releases/lzip/plzip/plzip-1.5.tar.lz"
+  sha256 "0e2e8644b0ec2c4319d0fab470eeb1bc41f36bdd612882beec332149a7aa564b"
 
   bottle do
     cellar :any_skip_relocation
@@ -10,11 +10,6 @@ class Plzip < Formula
     sha256 "d6034ff78f2a1e85f7794450bdd06bc95b1a07e30ee99c1227bf642d5be09ad4" => :yosemite
     sha256 "8cd3d1ac2c5b8c04a172188efdbc89dd1a7cfe936057d047d728484691e67adb" => :mavericks
     sha256 "7e8f6bbcb52876b808fc247a46e9f189b1e8a04f829717dc27979d1393f7a15a" => :mountain_lion
-  end
-
-  devel do
-    url "http://download.savannah.gnu.org/releases/lzip/plzip/plzip-1.5-rc2.tar.lz"
-    sha256 "3b4ba622875f33d124ddf79f13ad3ff0fb977fec078f1fd6e88f0432515472f5"
   end
 
   depends_on "lzlib"
