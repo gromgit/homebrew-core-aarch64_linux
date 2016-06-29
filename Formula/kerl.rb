6aa16bd573eb0b414c7f2715b7671bc4dd59e6b5
sanmiguel <michael.coles@gmail.com>
2016-06-29 04:01:49 +0200

    kerl 1.2.0 (#2472)

--- Formula/kerl.rb
+++ Formula/kerl.rb
@@ -1,8 +1,8 @@
 class Kerl < Formula
   desc "Easy building and installing of Erlang/OTP instances"
   homepage "https://github.com/kerl/kerl"
-  url "https://github.com/kerl/kerl/archive/1.1.1.tar.gz"
-  sha256 "7719c8daf24c12c676757cd4681367c1a6da86672b343b7b93f714f70ace6b58"
+  url "https://github.com/kerl/kerl/archive/1.2.0.tar.gz"
+  sha256 "b708e228a4e18b2f7ac6828ea192643a57647ba2b74acd5104920c95b8c7e892"
   head "https://github.com/kerl/kerl.git"
 
   bottle :unneeded
