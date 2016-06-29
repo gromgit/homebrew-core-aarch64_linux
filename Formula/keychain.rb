a5c6aef99333eec0481ea456d7f4835b333ccb45
Rajiv Aaron Manglani <rajiv@alum.mit.edu>
2016-06-28 22:17:08 -0400

    keychain 2.8.3 (#2462)

--- Formula/keychain.rb
+++ Formula/keychain.rb
@@ -1,9 +1,9 @@
 class Keychain < Formula
   desc "User-friendly front-end to ssh-agent(1)"
   homepage "http://www.funtoo.org/Keychain"
-  url "http://build.funtoo.org/distfiles/keychain/keychain-2.8.2.tar.bz2"
-  mirror "https://distfiles.macports.org/keychain/keychain-2.8.2.tar.bz2"
-  sha256 "74895832297616a1a951e81a56603f2fc6e5449576411f8b122a5cc933ae3301"
+  url "http://build.funtoo.org/distfiles/keychain/keychain-2.8.3.tar.bz2"
+  mirror "https://distfiles.macports.org/keychain/keychain-2.8.3.tar.bz2"
+  sha256 "d05eb924efcaef78eddff8e3190154a39778f0eee4f90362528c81ad8dadde56"
 
   bottle :unneeded
 
