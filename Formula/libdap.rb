32a2629c121452c16501bf522c30a56660369b38
Tomasz Pajor <tomek@polishgeeks.com>
2016-06-29 11:09:40 +0200

    libdap 3.18.1
    Closes #2486.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/libdap.rb
+++ Formula/libdap.rb
@@ -1,9 +1,8 @@
 class Libdap < Formula
   desc "Framework for scientific data networking"
   homepage "http://www.opendap.org"
-  url "https://github.com/OPENDAP/libdap4/archive/version-3.18.0.tar.gz"
-  sha256 "373c60cf2c5c9eaf598558167aedbc3ef9a0d9b652dfbd96b4725637cf03f628"
-  head "https://github.com/OPENDAP/libdap4.git"
+  url "http://www.opendap.org/pub/source/libdap-3.18.1.tar.gz"
+  sha256 "a755c472d7c9e54bc3aa6c92a847a69466fbc6cdc56ee912f411802a725d57a4"
 
   bottle do
     sha256 "4c7e4ac48248b98e01434e58b0f4bd49430728494a79a14606c7dc7b36a1214a" => :el_capitan
@@ -11,13 +10,18 @@ class Libdap < Formula
     sha256 "cf167f219d06fe935453d1af606d8d38711c865f541d1f61007cb27f2666bae6" => :mavericks
   end
 
+  head do
+    url "https://github.com/OPENDAP/libdap4.git"
+
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "libtool" => :build
+  end
+
   option "without-test", "Skip build-time tests (Not recommended)"
 
   depends_on "pkg-config" => :build
   depends_on "bison" => :build
-  depends_on "automake" => :build
-  depends_on "autoconf" => :build
-  depends_on "libtool" => :build
   depends_on "libxml2"
   depends_on "openssl"
 
@@ -47,7 +51,7 @@ class Libdap < Formula
     # over to `dap-config` and from there the contamination will spread.
     args << "--with-curl=/usr" if MacOS.version <= :snow_leopard
 
-    system "autoreconf", "-fvi"
+    system "autoreconf", "-fvi" if build.head?
     system "./configure", *args
     system "make"
     system "make", "check" if build.with? "test"
