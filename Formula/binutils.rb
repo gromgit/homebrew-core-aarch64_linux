bd66f9f5818d7175178b836fce2eb22efeff95dc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-29 14:49:15 +0100

    binutils: update 2.26.1 bottle.

--- Formula/binutils.rb
+++ Formula/binutils.rb
@@ -6,9 +6,9 @@ class Binutils < Formula
   sha256 "dd9c3e37c266e4fefba68e444e2a00538b3c902dd31bf4912d90dca6d830a2a1"
 
   bottle do
-    sha256 "6290d3d2b56f5b9450043d1f71d0597e7405e6d27c1c6eddd54f1f6bb5ff96f0" => :el_capitan
-    sha256 "12f87ef4be8a0ea5d92cf9ef2f6bb3db8b1836c61d7ab0a95ddcea3a6b8dc37f" => :yosemite
-    sha256 "a82e26a42ba2775534c7b3651feacfd1d3be7b4e83cfbe3635c7f728ac2cbb4d" => :mavericks
+    sha256 "61d1a4e3bdc1846267c984b478e3a5a822a8a14a1169153670a9a61402ab1d40" => :el_capitan
+    sha256 "daeccb1d2c5c4b324aa5ea38c6813ead25d9bfea8a84e093cc96687fa152b3e0" => :yosemite
+    sha256 "f2ae1527a595586195d773323a661135ce5a8eb96ec171ef5c387d1da5592d80" => :mavericks
   end
 
   # No --default-names option as it interferes with Homebrew builds.
