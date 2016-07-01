b3f694b502342120031e4624bcf8f3e9237ccd44
Dominyk Tiller <dominyktiller@gmail.com>
2016-07-02 00:27:22 +0100

    pgcli: squash pointless array

--- Formula/pgcli.rb
+++ Formula/pgcli.rb
@@ -72,8 +72,8 @@ class Pgcli < Formula
 
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
-    %w[click prompt_toolkit psycopg2 pgspecial sqlparse Pygments wcwidth six setproctitle configobj humanize].each do |r|
-      resource(r).stage do
+    resources.each do |r|
+      r.stage do
         system "python", *Language::Python.setup_install_args(libexec/"vendor")
       end
     end
