75996ed3c00691806e8c7a75e4be73107ef745b8
Viktor Szakats <vszakats@users.noreply.github.com>
2016-07-02 01:56:52 +0200

    vcdimager: use secure url

--- Formula/vcdimager.rb
+++ Formula/vcdimager.rb
@@ -1,7 +1,7 @@
 class Vcdimager < Formula
   desc "(Super) video CD authoring solution"
   homepage "https://www.gnu.org/software/vcdimager/"
-  url "http://ftpmirror.gnu.org/vcdimager/vcdimager-0.7.24.tar.gz"
+  url "https://ftpmirror.gnu.org/vcdimager/vcdimager-0.7.24.tar.gz"
   mirror "https://ftp.gnu.org/gnu/vcdimager/vcdimager-0.7.24.tar.gz"
   sha256 "075d7a67353ff3004745da781435698b6bc4a053838d0d4a3ce0516d7d974694"
 
