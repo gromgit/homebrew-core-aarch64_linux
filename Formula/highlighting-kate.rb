0e50927d5b82e95d89322cbfa7c88cc802cface8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-01 21:56:16 +0100

    highlighting-kate: update 0.6.2.1 bottle.

--- Formula/highlighting-kate.rb
+++ Formula/highlighting-kate.rb
@@ -11,9 +11,9 @@ class HighlightingKate < Formula
   head "https://github.com/jgm/highlighting-kate.git"
 
   bottle do
-    sha256 "cc5f019b34a5838ef52928bbf30e42191c96ed1e1e602193e6b71cc8ddf2d5f7" => :el_capitan
-    sha256 "99f8faf09ffb0852acc90e5e1f1bfbf6ec21e09fd2b1da748fd25dd960666f6d" => :yosemite
-    sha256 "be3f9ad7b797304a0403bf168f4a2519c44c8a790731004ab5c6862615f90fee" => :mavericks
+    sha256 "db900521259bce9e3c45dc26e2aadcd67e50fb296984feaf22183718118cd6e8" => :el_capitan
+    sha256 "8c5bc6ae824ed2a8ed8661417e1c893bb8500f20e0160f958ba5fd49e99ac0f5" => :yosemite
+    sha256 "affa1c557573dd839262a7b8dc1c7b895780115785038876a4fadb94a818e758" => :mavericks
   end
 
   depends_on "ghc" => :build
