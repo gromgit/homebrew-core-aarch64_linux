72953ea9e736daa8ab7a34c816dae4c75afb97b4
ilovezfs <ilovezfs@icloud.com>
2016-07-02 10:21:20 -0700

    rhino 1.7.7.1
    Closes #2619.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/rhino.rb
+++ Formula/rhino.rb
@@ -1,19 +1,21 @@
 class Rhino < Formula
   desc "JavaScript engine"
   homepage "https://www.mozilla.org/rhino/"
-  url "https://github.com/mozilla/rhino/releases/download/Rhino1_7_6_RELEASE/rhino1.7.6.zip"
-  sha256 "45f4431699887a72a7f383ef927ef1b2e79d1fe597260af8df11ea93255b10f5"
+  url "https://github.com/mozilla/rhino/releases/download/Rhino1_7_7_1_RELEASE/rhino-1.7.7.1.zip"
+  sha256 "f753d82002927208139f374e47febb35d7832a10890f57bb1a3c28cad0de6e84"
 
   bottle :unneeded
 
   conflicts_with "nut", :because => "both install `rhino` binaries"
 
   def install
-    libexec.install "js.jar"
-    bin.write_jar_script libexec/"js.jar", "rhino"
+    rhino_jar = "rhino-#{version}.jar"
+    libexec.install "lib/#{rhino_jar}"
+    bin.write_jar_script libexec/rhino_jar, "rhino"
+    doc.install (buildpath/"docs").children
   end
 
   test do
-    system "#{bin}/rhino", "-e", "print(42);"
+    assert_equal "42", shell_output("#{bin}/rhino -e \"print(6*7)\"").chomp
   end
 end
