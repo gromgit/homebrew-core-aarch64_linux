5bc977ad75611fedf2db189512004e5beaf56085
ilovezfs <ilovezfs@icloud.com>
2016-07-02 10:11:14 -0700

    pacapt 2.3.8
    Closes #2611.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pacapt.rb
+++ Formula/pacapt.rb
@@ -1,8 +1,8 @@
 class Pacapt < Formula
   desc "Package manager in the style or Arch's pacman"
   homepage "https://github.com/icy/pacapt"
-  url "https://github.com/icy/pacapt/archive/v2.2.7.tar.gz"
-  sha256 "9cc754c9005a50407412ac8520ba20a7c612553f31059ad47289bc3df2a6b254"
+  url "https://github.com/icy/pacapt/archive/v2.3.8.tar.gz"
+  sha256 "6ac7d16cf225a4fc625da40776577b62b44c6b83fe3a2e5b33fe3db467c1dd7e"
 
   bottle do
     cellar :any_skip_relocation
