41f00510bf806a0c267bafca03611bb01aba9b43
ilovezfs <ilovezfs@icloud.com>
2016-07-02 13:18:47 -0700

    bagit 4.12.1
    Closes #2648.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/bagit.rb
+++ Formula/bagit.rb
@@ -1,8 +1,8 @@
 class Bagit < Formula
   desc "Library for creation, manipulation, and validation of bags"
   homepage "https://github.com/LibraryOfCongress/bagit-java"
-  url "https://github.com/LibraryOfCongress/bagit-java/releases/download/v4.12.0/bagit-4.12.0.zip"
-  sha256 "045efaff7375af8cb47c9dc7890bf17d4d49d149e4d3ac388862288087505695"
+  url "https://github.com/LibraryOfCongress/bagit-java/releases/download/v4.12.1/bagit-4.12.1.zip"
+  sha256 "2abf838c090867e887cc46bb9f95ab697a6706ee297be8ff8ebc8dc4404ff64e"
 
   bottle :unneeded
 
