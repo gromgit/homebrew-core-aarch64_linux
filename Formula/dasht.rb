7b8cb29c6637b4b202656224af3ae815aa2a3865
ilovezfs <ilovezfs@icloud.com>
2016-07-02 09:39:07 -0700

    dasht 2.1.0
    Closes #2587.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dasht.rb
+++ Formula/dasht.rb
@@ -1,8 +1,8 @@
 class Dasht < Formula
   desc "Search API docs offline, in your terminal or browser"
   homepage "https://sunaku.github.io/dasht"
-  url "https://github.com/sunaku/dasht/archive/v2.0.0.tar.gz"
-  sha256 "bd058d57ecb13f95a7979e466572785fc08ede807f0558620aa4ec377f3afd72"
+  url "https://github.com/sunaku/dasht/archive/v2.1.0.tar.gz"
+  sha256 "98b4c130e64a33fdb9559d8c9d85ba1526236ccdb311b805f28f0128b167555e"
 
   bottle :unneeded
 
