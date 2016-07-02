757ceecfea9f19e7f6a607f120e713288e5b4275
ilovezfs <ilovezfs@icloud.com>
2016-07-02 10:25:58 -0700

    sfcgal 1.3.0
    Closes #2622.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/sfcgal.rb
+++ Formula/sfcgal.rb
@@ -1,8 +1,8 @@
 class Sfcgal < Formula
   desc "C++ wrapper library around CGAL"
   homepage "http://sfcgal.org/"
-  url "https://github.com/Oslandia/SFCGAL/archive/v1.2.2.tar.gz"
-  sha256 "dae7de4c7e1b4ef2a51c55f7d201a6d8049b518caac14f4033fd2d43f14eb031"
+  url "https://github.com/Oslandia/SFCGAL/archive/v1.3.0.tar.gz"
+  sha256 "7ed35439fc197e73790f4c3d1c1750acdc3044968769239b2185a7a845845df3"
 
   bottle do
     sha256 "cddb259f1fdc03ba213c56757485efee012acf10c96f39af7387ee40b4901f8e" => :el_capitan
@@ -10,13 +10,22 @@ class Sfcgal < Formula
     sha256 "6512802b7da2f59d1a449dda91d15537be24814bee913ca73c68ccd7082f1d9e" => :mavericks
   end
 
+  option :cxx11
+
   depends_on "cmake" => :build
-  depends_on "boost"
-  depends_on "cgal"
-  depends_on "gmp"
   depends_on "mpfr"
+  if build.cxx11?
+    depends_on "boost" => "c++11"
+    depends_on "cgal" => "c++11"
+    depends_on "gmp"   => "c++11"
+  else
+    depends_on "boost"
+    depends_on "cgal"
+    depends_on "gmp"
+  end
 
   def install
+    ENV.cxx11 if build.cxx11?
     system "cmake", ".", *std_cmake_args
     system "make", "install"
   end
