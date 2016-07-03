5af0b93738e2ba2c24835de8bd7f66739547cce9
ilovezfs <ilovezfs@icloud.com>
2016-07-03 07:22:43 -0700

    seexpr 2.11
    remove patch, which is already in this tag
    
    Closes #2621.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/seexpr.rb
+++ Formula/seexpr.rb
@@ -1,8 +1,8 @@
 class Seexpr < Formula
   desc "Embeddable expression evaluation engine"
   homepage "http://www.disneyanimation.com/technology/seexpr.html"
-  url "https://github.com/wdas/SeExpr/archive/rel-1.0.1.tar.gz"
-  sha256 "971ee8fff7eb195785031dedd7c06cb3fa8649fba8aa45c6ace746a23b8093a9"
+  url "https://github.com/wdas/SeExpr/archive/v2.11.tar.gz"
+  sha256 "bf4a498f86aa3fc19aad3d7384de11d5df76f7f71587c9bd789f5e50f8090e1a"
 
   bottle do
     cellar :any
@@ -14,10 +14,6 @@ class Seexpr < Formula
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
   depends_on "libpng"
-
-  # fix for macosx
-  # already present in HEAD so it can be removed after version 1.0.1
-  patch :DATA
 
   def install
     mkdir "build" do
@@ -31,33 +27,3 @@ class Seexpr < Formula
     system bin/"asciigraph"
   end
 end
-
-__END__
-diff --git a/src/SeExpr/SeExprFunc.cpp b/src/SeExpr/SeExprFunc.cpp
-index feb6b45..8269b39 100644
---- a/src/SeExpr/SeExprFunc.cpp
-+++ b/src/SeExpr/SeExprFunc.cpp
-@@ -208,7 +208,7 @@ SeExprFunc::getDocString(const char* functionName)
-
- #ifndef SEEXPR_WIN32
-
--#ifdef __APPLE__
-+#if defined(__APPLE__) && __MAC_OS_X_VERSION_MIN_REQUIRED <= __MAC_10_7
- static int MatchPluginName(struct dirent* dir)
- #else
- static int MatchPluginName(const struct dirent* dir)
-diff --git a/src/SeExpr/SePlatform.h b/src/SeExpr/SePlatform.h
-index 32a6b96..82b0f44 100644
---- a/src/SeExpr/SePlatform.h
-+++ b/src/SeExpr/SePlatform.h
-@@ -40,6 +40,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-     @brief Platform-specific classes, functions, and includes.
- */
-
-+#ifdef __APPLE__
-+#    include <Availability.h>
-+#endif
-+
- // platform-specific includes
- #if defined(_WIN32) || defined(_WINDOWS) || defined(_MSC_VER)
- #    ifndef WINDOWS
