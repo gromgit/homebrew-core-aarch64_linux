d98f2f7aebbb4e94b72ac4f88d3145c8cf601abb
Viktor Szakats <vszakats@users.noreply.github.com>
2016-07-03 14:41:13 +0200

    exiftags: use secure urls

--- Formula/exiftags.rb
+++ Formula/exiftags.rb
@@ -1,7 +1,7 @@
 class Exiftags < Formula
   desc "Utility to read EXIF tags from a digital camera JPEG file"
-  homepage "http://johnst.org/sw/exiftags"
-  url "http://johnst.org/sw/exiftags/exiftags-1.01.tar.gz"
+  homepage "https://johnst.org/sw/exiftags/"
+  url "https://johnst.org/sw/exiftags/exiftags-1.01.tar.gz"
   sha256 "d95744de5f609f1562045f1c2aae610e8f694a4c9042897a51a22f0f0d7591a4"
 
   bottle do
