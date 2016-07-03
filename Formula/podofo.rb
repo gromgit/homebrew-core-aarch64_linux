d54678131158e6e67b6fc37a398161c2a645a7f4
Tomasz Pajor <tomek@polishgeeks.com>
2016-07-04 00:07:34 +0200

    podofo 0.9.4
    Closes #2700.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/podofo.rb
+++ Formula/podofo.rb
@@ -1,9 +1,8 @@
 class Podofo < Formula
   desc "Library to work with the PDF file format"
   homepage "http://podofo.sourceforge.net"
-  url "https://downloads.sourceforge.net/podofo/podofo-0.9.3.tar.gz"
-  sha256 "ec261e31e89dce45b1a31be61e9c6bb250532e631a02d68ec5bb849ef0a222d8"
-  revision 1
+  url "https://downloads.sourceforge.net/podofo/podofo-0.9.4.tar.gz"
+  sha256 "ccdf505fcb4904617e728b15729da8700ff38442c1dd2f24fbd52934287ff859"
 
   bottle do
     cellar :any
