08c4d9e5b7144a25003fe66564a8919dfae30006
David Said <houmam@houmam.com>
2016-07-04 20:50:15 +1000

    apache-drill 1.7.0 (#2704)

--- Formula/apache-drill.rb
+++ Formula/apache-drill.rb
@@ -1,9 +1,9 @@
 class ApacheDrill < Formula
   desc "Schema-free SQL query engine for Hadoop and NoSQL"
   homepage "https://drill.apache.org/download/"
-  url "https://www.apache.org/dyn/closer.cgi?path=drill/drill-1.6.0/apache-drill-1.6.0.tar.gz"
-  mirror "https://archive.apache.org/dist/drill/drill-1.6.0/apache-drill-1.6.0.tar.gz"
-  sha256 "3ab36cf985272d89971b9a918a2484feccc2881bcf09da611edafdce686a9502"
+  url "https://www.apache.org/dyn/closer.cgi?path=drill/drill-1.7.0/apache-drill-1.7.0.tar.gz"
+  mirror "https://archive.apache.org/dist/drill/drill-1.7.0/apache-drill-1.7.0.tar.gz"
+  sha256 "b2216bd3c5a6047dc45134c22c0a0e9bac48daa9a4c5dd09b6eeed3cb83616d0"
 
   bottle :unneeded
 
