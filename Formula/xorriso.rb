5fbfbc2b99ebf8565b4e8a4b149ea0d61a9087ef
ilovezfs <ilovezfs@icloud.com>
2016-07-04 14:21:15 -0700

    xorriso 1.4.4
    remove ssize_t patch since it's included in this release
    
    Closes #2644.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/xorriso.rb
+++ Formula/xorriso.rb
@@ -1,22 +1,15 @@
 class Xorriso < Formula
   desc "ISO9660+RR manipulation tool"
   homepage "https://www.gnu.org/software/xorriso/"
-  url "https://ftpmirror.gnu.org/xorriso/xorriso-1.4.2.tar.gz"
-  mirror "https://www.gnu.org/software/xorriso/xorriso-1.4.2.tar.gz"
-  sha256 "2843beded1aa4c678706e96fdf9cc5e4b34430b559bdf5bc35df5202125004b3"
+  url "https://ftpmirror.gnu.org/xorriso/xorriso-1.4.4.tar.gz"
+  mirror "https://www.gnu.org/software/xorriso/xorriso-1.4.4.tar.gz"
+  sha256 "f9b954133324281e8dca6e2a3f0b101be02c3d353587bdc520d033c57743fbc7"
 
   bottle do
     cellar :any_skip_relocation
     sha256 "3e9c949005d71d7cef9779fca8934b129d0c740fccd432f629bf759cdfbc7ec7" => :el_capitan
     sha256 "86a2ef87dc3d1880050a155e2662f68838ab99d6996695e0259e1fdc04cd5e73" => :yosemite
     sha256 "f367a006365ecd19ac68c8af6d86f5a15cc872e2f9e3e7b0e0e002d671d395af" => :mavericks
-  end
-
-  # Upstream fix for "unknown type name 'ssize_t'"
-  # See https://lists.gnu.org/archive/html/bug-xorriso/2016-05/msg00001.html
-  patch :p0 do
-    url "https://bazaar.launchpad.net/~libburnia-team/libisofs/scdbackup/diff/1319"
-    sha256 "c548c59b143efb87ccb3dc08f80f527ae93918fab56894c9f8876f31dccce142"
   end
 
   def install
