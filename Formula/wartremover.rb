0a598ccce082f5e7629e2e2fbe33067f0df53e24
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-04 12:07:49 +0100

    wartremover: update 1.0.1 bottle.

--- Formula/wartremover.rb
+++ Formula/wartremover.rb
@@ -7,9 +7,9 @@ class Wartremover < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "11b231be52c5e33487d2a68c51dd2dec57e4f6d5ae0e5ea579813668b4ddc504" => :el_capitan
-    sha256 "153ba2e298749b87d75bdb04da31a63a0a453fcf1371525c6a3c9bae351e8fcb" => :yosemite
-    sha256 "dbb57e97b6672ef9fde16f61db87ed5c1568e629fa1c7672b17425ef267f23a5" => :mavericks
+    sha256 "606d8cae5fdc5bf67c1610898eb93abd4fea05d1a0a3b19b5844af0be1ab9d60" => :el_capitan
+    sha256 "5f60170aa9cc8fa18d3cc91c7ca0a3a32f0529842d2d27ce7cc347a1c9b3afe1" => :yosemite
+    sha256 "a1057ac0a84fcea8ab7b62b7dd1f523a45fa68f9de3fbe53ac095583330d1650" => :mavericks
   end
 
   depends_on "sbt" => :build
