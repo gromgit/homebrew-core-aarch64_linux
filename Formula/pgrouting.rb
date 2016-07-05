aa2a54348a1274d948598460ae1bf4fe442d41e1
ilovezfs <ilovezfs@icloud.com>
2016-07-05 09:51:19 -0700

    pgrouting 2.2.3
    - add head spec
    - apply upstream commits adding two missing includes
    - add linked-library dependency gmp
    
    Closes #2613.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pgrouting.rb
+++ Formula/pgrouting.rb
@@ -1,8 +1,26 @@
 class Pgrouting < Formula
   desc "Provides geospatial routing for PostGIS/PostgreSQL database"
   homepage "http://www.pgrouting.org"
-  url "https://github.com/pgRouting/pgrouting/archive/pgrouting-2.2.1.tar.gz"
-  sha256 "8d061c9ecd7b23e9931d342a41fbacce1870996291743bf6cbf51ca7aa6302b8"
+  head "https://github.com/pgRouting/pgrouting.git"
+
+  stable do
+    url "https://github.com/pgRouting/pgrouting/archive/pgrouting-2.2.3.tar.gz"
+    sha256 "ace0b2bfcfd468fa360867faf021d4447ebfa80d35f02cf5da549503b5dd4892"
+
+    # Fixes "use of undeclared identifier" for "ceil"
+    # Upstream commit that adds "#include <math.h>" to VRP_Solver.h
+    patch do
+      url "https://github.com/pgRouting/pgrouting/commit/3862e4cb.patch"
+      sha256 "936af1d25d3aae517de1d2cff021d8e6c5f7db98927ded5d699caf1bc535c1fb"
+    end
+
+    # Fixes "use of undeclared identifier" for "srand" and "rand"
+    # Upstream commit that adds "#include <stdlib.h>" to VRP_Solver.h
+    patch do
+      url "https://github.com/pgRouting/pgrouting/commit/ce811a03.patch"
+      sha256 "628c68f3d2348f60b3612a04868dc96797e2a357db18c41d62717fd70c3c5747"
+    end
+  end
 
   bottle do
     cellar :any
@@ -14,6 +32,7 @@ class Pgrouting < Formula
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "cgal"
+  depends_on "gmp"
   depends_on "postgis"
   depends_on "postgresql"
 
