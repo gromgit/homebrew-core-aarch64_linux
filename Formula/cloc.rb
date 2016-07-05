9bc98cd1ee2f5813f3166a3043681f6cab9bdb19
Dominyk Tiller <dominyktiller@gmail.com>
2016-07-05 03:50:11 +0100

    cloc: vendor perl modules
    The joys of `/usr/bin/env` hashbangs.
    
    Closes #2714.
    
    Closes #2723.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/cloc.rb
+++ Formula/cloc.rb
@@ -12,8 +12,34 @@ class Cloc < Formula
     sha256 "12bfa1e13c517e2c90ce0de1a5d5e468486f1191445b6ac19c7d9ad7be883ea9" => :mavericks
   end
 
+  resource "Regexp::Common" do
+    url "https://cpan.metacpan.org/authors/id/A/AB/ABIGAIL/Regexp-Common-2016060801.tar.gz"
+    sha256 "fc2fc178facf0292974d6511bad677dd038fe60d7ac118e3b83a1ca9e98a8403"
+  end
+
+  resource "Algorithm::Diff" do
+    url "https://cpan.metacpan.org/authors/id/T/TY/TYEMQ/Algorithm-Diff-1.1903.tar.gz"
+    sha256 "30e84ac4b31d40b66293f7b1221331c5a50561a39d580d85004d9c1fff991751"
+  end
+
   def install
-    system "make", "-C", "Unix", "prefix=#{prefix}", "install"
+    ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+
+    # These are shipped as standard with OS X/macOS' default Perl, but
+    # because upstream has chosen to use "#!/usr/bin/env perl" cloc will
+    # use whichever Perl is in the PATH, which isn't guaranteed to include
+    # these modules. Vendor them to be safe.
+    resources.each do |r|
+      r.stage do
+        system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+        system "make", "install"
+      end
+    end
+
+    system "make", "-C", "Unix", "prefix=#{libexec}", "install"
+    bin.install libexec/"bin/cloc"
+    bin.env_script_all_files(libexec/"bin", :PERL5LIB => ENV["PERL5LIB"])
+    man1.install libexec/"share/man/man1/cloc.1"
   end
 
   test do
