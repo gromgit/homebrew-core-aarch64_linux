ea46d3c6bfd5d8e3c16ced33e174ad71206632e7
Dominyk Tiller <dominyktiller@gmail.com>
2016-07-06 04:15:43 +0100

    kubernetes-cli: style nits

--- Formula/kubernetes-cli.rb
+++ Formula/kubernetes-cli.rb
@@ -1,12 +1,9 @@
 class KubernetesCli < Formula
   desc "Kubernetes command-line interface"
   homepage "http://kubernetes.io/"
+  url "https://github.com/kubernetes/kubernetes/archive/v1.3.0.tar.gz"
+  sha256 "77fbc5db607daa723e7b6576644d25e98924439954523808cf7ad2c992566398"
   head "https://github.com/kubernetes/kubernetes.git"
-
-  stable do
-    url "https://github.com/kubernetes/kubernetes/archive/v1.3.0.tar.gz"
-    sha256 "77fbc5db607daa723e7b6576644d25e98924439954523808cf7ad2c992566398"
-  end
 
   bottle do
     cellar :any_skip_relocation
@@ -30,10 +27,11 @@ class KubernetesCli < Formula
 
     dir = "_output/local/bin/darwin/#{arch}"
     bin.install "#{dir}/kubectl"
-    (bash_completion/"kubectl").write `#{bin}/kubectl completion bash`
+    (bash_completion/"kubectl").write Utils.popen_read("#{bin}/kubectl completion bash")
   end
 
   test do
-    assert_match /^kubectl controls the Kubernetes cluster manager./, shell_output("#{bin}/kubectl 2>&1", 0)
+    output = shell_output("#{bin}/kubectl 2>&1")
+    assert_match "kubectl controls the Kubernetes cluster manager.", output
   end
 end
