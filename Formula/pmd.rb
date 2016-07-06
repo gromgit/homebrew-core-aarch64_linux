eb56c065fa5dcfdfe7e9c832389d17c2d27acc95
ilovezfs <ilovezfs@icloud.com>
2016-07-06 11:02:19 -0700

    pmd 5.5.0
    - add Java requirement
    - build now requires a toolchains.xml file
    
    Closes #2615.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pmd.rb
+++ Formula/pmd.rb
@@ -1,8 +1,8 @@
 class Pmd < Formula
   desc "Source code analyzer for Java, JavaScript, and more"
   homepage "https://pmd.github.io"
-  url "https://github.com/pmd/pmd/releases/download/pmd_releases/5.3.3/pmd-src-5.3.3.zip"
-  sha256 "38004ea3274d2f71701f438606a4c4095b7a86110e8d7f2a8940170c9bd5ddbb"
+  url "https://github.com/pmd/pmd/releases/download/pmd_releases/5.5.0/pmd-src-5.5.0.zip"
+  sha256 "825d9061754cef30fc1b4584f509c7dd8a7b023a2c6ec264bf7b82bb0615c0b4"
 
   bottle do
     cellar :any_skip_relocation
@@ -11,10 +11,39 @@ class Pmd < Formula
     sha256 "e5ed61e35b5c980b0e1bcf616bc2615f1c5310ad66ce8f4901ca225764e7e78c" => :mavericks
   end
 
+  depends_on :java => "1.8+"
   depends_on "maven" => :build
 
   def install
-    ENV.java_cache
+    java_user_home = buildpath/"java_user_home"
+    ENV["_JAVA_OPTIONS"] = "-Duser.home=#{buildpath}/java_user_home"
+    java_cache_repo = HOMEBREW_CACHE/"java_cache/.m2/repository"
+    java_cache_repo.mkpath
+    (java_user_home/".m2").install_symlink java_cache_repo
+
+    (java_user_home/".m2/toolchains.xml").write <<-EOS.undent
+      <?xml version="1.0" encoding="UTF8"?>
+      <toolchains>
+        <toolchain>
+          <type>jdk</type>
+          <provides>
+            <version>#{ENV["JAVA_HOME"][/((\d\.?)+)\.\d/, 1]}</version>
+          </provides>
+          <configuration>
+            <jdkHome>#{ENV["JAVA_HOME"]}</jdkHome>
+          </configuration>
+        </toolchain>
+        <toolchain>
+          <type>jdk</type>
+          <provides>
+            <version>1.7</version>
+          </provides>
+          <configuration>
+            <jdkHome>#{ENV["JAVA_HOME"]}</jdkHome>
+          </configuration>
+        </toolchain>
+      </toolchains>
+    EOS
 
     system "mvn", "clean", "package"
 
