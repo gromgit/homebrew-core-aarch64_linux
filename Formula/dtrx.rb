999410aecf5a1c5dbf8a878157c734c56bde74e5
Viktor Szakats <vszakats@users.noreply.github.com>
2016-07-06 13:25:15 +0200

    dtrx: use secure urls
    Closes #2759.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/dtrx.rb
+++ Formula/dtrx.rb
@@ -1,7 +1,7 @@
 class Dtrx < Formula
   desc "Intelligent archive extraction"
-  homepage "http://brettcsmith.org/2007/dtrx/"
-  url "http://brettcsmith.org/2007/dtrx/dtrx-7.1.tar.gz"
+  homepage "https://brettcsmith.org/2007/dtrx/"
+  url "https://brettcsmith.org/2007/dtrx/dtrx-7.1.tar.gz"
   sha256 "1c9afe48e9d9d4a1caa4c9b0c50593c6fe427942716ce717d81bae7f8425ce97"
 
   bottle do
