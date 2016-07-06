efd1c63269ddf957f63c8cfd906d30c3b74075ac
Scott Bessler <scottbessler@gmail.com>
2016-07-06 03:09:24 -0700

    flyway 4.0.3 (#2749)
    latest version as per https://flywaydb.org/getstarted/download

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/4.0.1/flyway-commandline-4.0.1.tar.gz"
-  sha256 "d23d8e9a13d3f31b51385c8937adefc49b4b46bb61805b36026f8c26be5255d0"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/4.0.3/flyway-commandline-4.0.3.tar.gz"
+  sha256 "61bb9ae98e484209d924d46670059a2460a66768c619bd4802e565256b99dafb"
 
   bottle :unneeded
 
