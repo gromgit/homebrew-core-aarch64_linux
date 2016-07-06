4d1729e3e8126b9528334ef5516682105301a65b
Dominyk Tiller <dominyktiller@gmail.com>
2016-07-07 00:46:08 +0100

    haxe: style nits

--- Formula/haxe.rb
+++ Formula/haxe.rb
@@ -1,20 +1,17 @@
 class Haxe < Formula
   desc "Multi-platform programming language"
   homepage "https://haxe.org/"
+  url "https://github.com/HaxeFoundation/haxe.git",
+      :tag => "3.2.1",
+      :revision => "deab4424399b520750671e51e5f5c2684e942c17"
 
-  stable do
-    url "https://github.com/HaxeFoundation/haxe.git", :tag => "3.2.1", :revision => "deab4424399b520750671e51e5f5c2684e942c17"
-  end
+  head "https://github.com/HaxeFoundation/haxe.git", :branch => "development"
 
   bottle do
     cellar :any_skip_relocation
     sha256 "131893375697db4a6f747ebec788f9d0d7304b742a3c007f27ea877f561e1e9f" => :el_capitan
     sha256 "104c2a1e9cc5c0d3ca3ee794a4872e8b25107139106f904c2471162ce278474e" => :yosemite
     sha256 "1e4c88518c6747bf86345ac67b2e3e2da5d9f3cea3c3917e8837ac4db2083412" => :mavericks
-  end
-
-  head do
-    url "https://github.com/HaxeFoundation/haxe.git", :branch => "development"
   end
 
   depends_on "ocaml" => :build
