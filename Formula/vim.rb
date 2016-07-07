00244eb811e3abba9d16209dcfb5a1c64f7fcf84
jdelisle <jdelisle@users.noreply.github.com>
2016-07-07 09:48:11 -0400

    vim 7.4.1993
    Closes #2806.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi \"workalike\" with many additional features"
   homepage "http://www.vim.org/"
   # *** Vim should be updated no more than once every 7 days ***
-  url "https://github.com/vim/vim/archive/v7.4.1967.tar.gz"
-  sha256 "d52491f18f1740199ed957c8b7fc9e83d491b2df6460c5380c286681428bdb78"
+  url "https://github.com/vim/vim/archive/v7.4.1993.tar.gz"
+  sha256 "96a68ea57f7e326771a114fbc081fc716fbd9cf6f526fdf21c3ce9718c8c2425"
   head "https://github.com/vim/vim.git"
 
   bottle do
