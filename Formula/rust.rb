6832e34e4b2e86e0f545822034e4d39b7bddcfee
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-07 20:26:39 +0100

    rust: update 1.10.0 bottle.

--- Formula/rust.rb
+++ Formula/rust.rb
@@ -19,9 +19,9 @@ class Rust < Formula
   end
 
   bottle do
-    sha256 "b056dfa306e29933415f9d50c6b5a1d76fe84d346f1337754c44c1a961f18409" => :el_capitan
-    sha256 "47bf11c39eee712e1f8276e1a77ebf70d1d87a125ea34e4a7cf00431d9deea45" => :yosemite
-    sha256 "d813ee35ceca6c9e125a5f950d1f7dd60ad79682064b07fa2f48cf2aa16a8dbf" => :mavericks
+    sha256 "b6fc2ebfed237d15269118a52810a3660c7d50a8097c6c48044e95a2fb9ead24" => :el_capitan
+    sha256 "4200756cc985257f888a02a02032588108649e4aae84e7340239b5be77a7730c" => :yosemite
+    sha256 "816521dfa0f3f10cf50a2894dcf3aeceb6edb86ec29c7733a234d834e15287e7" => :mavericks
   end
 
   head do
