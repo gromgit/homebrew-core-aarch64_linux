0e84c6e9b89a32b3a0548e775c580776ac437649
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-08 19:38:56 +0100

    minio: add 20160603193205 bottle.

--- Formula/minio.rb
+++ Formula/minio.rb
@@ -8,6 +8,13 @@ class Minio < Formula
   sha256 "8ecf52cc344c99f84bef632b8dfecf8fdc6a8493a541b2fe42f5b22d8bbfa802"
   head "https://github.com/minio/minio.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "29d5d319618c235dc75b86b9849b1b8f9d3ccc8c5b313c28e73167119ae43c4e" => :el_capitan
+    sha256 "431a20fe6725e32f3a576937815b28e7af8fecfa1faab29e3c1d24d97edb0941" => :yosemite
+    sha256 "c79ee7ccfea2b00db2b8792a479bac3142ce9567c7c69eade8a435b99600a77b" => :mavericks
+  end
+
   depends_on "go" => :build
 
   def install
