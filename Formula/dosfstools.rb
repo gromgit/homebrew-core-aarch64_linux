888c3eb182f547861b56fa602b89a94cbad91b5b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-08 07:09:58 +0100

    dosfstools: add 4.0 bottle.

--- Formula/dosfstools.rb
+++ Formula/dosfstools.rb
@@ -14,6 +14,13 @@ class Dosfstools < Formula
       sha256 "824c800a589db8a779fddbbd4b8eef5a2941f838ef8f46ad390a680c8cabd2a4"
     end
   end
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "80ff9e3d9a13ee9b4af60f0ec099755bf9cc5f08927e8f5e5ed9e9deb15f8de5" => :el_capitan
+    sha256 "1272b2a6cd3264e5d69da30e6f74add66ca7f6f9eb2eb30af7ba738c41233fb5" => :yosemite
+    sha256 "fc128348728cfb7b634e5d286dc69aa954aee9dc79cb6dec0c27de31ebc1c264" => :mavericks
+  end
+
 
   option "with-compat-symlinks",
     "Symlink older names of the tools to the current ones on installation"
