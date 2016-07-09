3ef6b684c3ca91c5147b90a87269fd465576d425
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-10 00:37:05 +0100

    ubertooth: add 2015-10-R1 bottle.

--- Formula/ubertooth.rb
+++ Formula/ubertooth.rb
@@ -5,6 +5,13 @@ class Ubertooth < Formula
   version "2015-10-R1"
   sha256 "bc37e7978d137a64d918d7c8f1e7ca9cff093f9921d805e9809b12e5ab12ae35"
 
+  bottle do
+    cellar :any
+    sha256 "95ba25253b67e36e263d7d9aca987caec686b313c6dc3e3b2d4bbf4bd4a12056" => :el_capitan
+    sha256 "465f2ecfeacdd426609500d41bd0c3f5134e1885376a7aa2d99bafc3478b135e" => :yosemite
+    sha256 "947fd37511e25e5e824ae9001f8e725e04cadc4f462349beb3a8e266f1ee98db" => :mavericks
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "libbtbb"
