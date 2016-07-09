6623a3c7934371be6e6d604d1a3ad1e35fea8e40
ilovezfs <ilovezfs@icloud.com>
2016-07-09 16:46:53 -0700

    jsoncpp 1.7.4
    Closes #2875.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/jsoncpp.rb
+++ Formula/jsoncpp.rb
@@ -1,8 +1,8 @@
 class Jsoncpp < Formula
   desc "Library for interacting with JSON"
   homepage "https://github.com/open-source-parsers/jsoncpp"
-  url "https://github.com/open-source-parsers/jsoncpp/archive/1.7.3.tar.gz"
-  sha256 "1cfcad14054039ba97c22531888796cb9369e6353f257aacaad34fda956ada53"
+  url "https://github.com/open-source-parsers/jsoncpp/archive/1.7.4.tar.gz"
+  sha256 "10dcd0677e80727e572a1e462193e51a5fde3e023b99e144b2ee1a469835f769"
   head "https://github.com/open-source-parsers/jsoncpp.git"
 
   bottle do
