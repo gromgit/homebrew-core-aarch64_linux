6967be6e65d93a0e6fe3afa17ffd15d3482b2b53
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-10 17:51:01 +0100

    freetype: update 2.6.4 bottle.

--- Formula/freetype.rb
+++ Formula/freetype.rb
@@ -13,9 +13,9 @@ class Freetype < Formula
 
   bottle do
     cellar :any
-    sha256 "03d81469f42b34176ad9b7a382a2ab613445541a57e3db02e80121508cc43309" => :el_capitan
-    sha256 "ff8df778e420e76ac823fb5d31b3bfd1d89be87aa664bb1140bb44508f4d5e23" => :yosemite
-    sha256 "ecfb1b9f4dcce361d4c40367dc205842411cbcbb6628238da571aff84caf05c3" => :mavericks
+    sha256 "9ae6bdaddd64a845d7293990c656c6b5a2d55eb0a54d787d977d92bb3a34b462" => :el_capitan
+    sha256 "eb549e7f83c77a0419bfeb34e0cc965c05256b2dfa6d99b8a70df81e73b439e0" => :yosemite
+    sha256 "650cfebaa6852614b76a6451fc6d9d5538a1b90a5f80c0f6006d7cf1c08a083b" => :mavericks
   end
 
   keg_only :provided_pre_mountain_lion
