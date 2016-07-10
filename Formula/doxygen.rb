03040ab77566fcdbf672cea330fff8ec8ce7a162
Jake Petroules <jake.petroules@petroules.com>
2016-07-10 04:39:58 -0700

    doxygen: use Qt5 (#2861)
    Qt 5 works well and unlike Qt 4, properly supports new macOS versions.

--- Formula/doxygen.rb
+++ Formula/doxygen.rb
@@ -14,19 +14,20 @@ class Doxygen < Formula
   end
 
   option "with-graphviz", "Build with dot command support from Graphviz."
-  option "with-doxywizard", "Build GUI frontend with qt support."
+  option "with-qt5", "Build GUI frontend with Qt support."
   option "with-libclang", "Build with libclang support."
 
   deprecated_option "with-dot" => "with-graphviz"
+  deprecated_option "with-doxywizard" => "with-qt5"
 
   depends_on "cmake" => :build
   depends_on "graphviz" => :optional
-  depends_on "qt" if build.with? "doxywizard"
+  depends_on "qt5" => :optional
   depends_on "llvm" => "with-clang" if build.with? "libclang"
 
   def install
     args = std_cmake_args
-    args << "-Dbuild_wizard=ON" if build.with? "doxywizard"
+    args << "-Dbuild_wizard=ON" if build.with? "qt5"
     args << "-Duse_libclang=ON -DLLVM_CONFIG=#{Formula["llvm"].opt_bin}/llvm-config" if build.with? "libclang"
 
     mkdir "build" do
