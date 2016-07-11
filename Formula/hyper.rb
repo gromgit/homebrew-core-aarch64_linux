dc6ea6c48672fe819edd90f3c12c21ad2f169856
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-11 06:18:25 +0100

    hyper: add 1.10 bottle.

--- Formula/hyper.rb
+++ Formula/hyper.rb
@@ -7,6 +7,13 @@ class Hyper < Formula
 
   head "https://github.com/hyperhq/hypercli.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1b9e4fbf36466338f1a44a7b846d44f6cc50c74b82dca57e6e84307822df6d05" => :el_capitan
+    sha256 "021407a242ccdd446db396f4df93ecdb6fb86138e04143807d2a56227a965fd1" => :yosemite
+    sha256 "0b40e9481c209ec589910b149c3598504b1ea5d58e978c4d0dcbbe2a2e20f41c" => :mavericks
+  end
+
   depends_on "go" => :build
 
   def install
