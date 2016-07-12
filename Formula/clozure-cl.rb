b142384c51438c5bf8680c77b41b32ddf8b2e99a
Alex Dunn <dunn.alex@gmail.com>
2016-07-11 20:56:06 -0700

    clozure-cl: add HEAD
    Closes #2918.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/clozure-cl.rb
+++ Formula/clozure-cl.rb
@@ -4,12 +4,20 @@ class ClozureCl < Formula
   url "http://ccl.clozure.com/ftp/pub/release/1.11/ccl-1.11-darwinx86.tar.gz"
   version "1.11"
   sha256 "2fc4b519f26f7df3fbb62314b15bd5d098082b05d40c3539f8204aa10f546913"
+  head "http://svn.clozure.com/publicsvn/openmcl/trunk/darwinx86/ccl"
 
   bottle :unneeded
 
   conflicts_with "cclive", :because => "both install a ccl binary"
 
   def install
+    if build.head?
+      ENV["CCL_DEFAULT_DIRECTORY"] = buildpath
+      system "scripts/ccl64", "-n",
+             "-e", "(ccl:rebuild-ccl :full t)",
+             "-e", "(quit)"
+    end
+
     # Get rid of all the .svn directories
     rm_rf Dir["**/.svn"]
 
