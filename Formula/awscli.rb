e0ec7195db5417c2d0af7efab44ae2f276e25331
ilovezfs <ilovezfs@icloud.com>
2016-07-13 15:32:58 -0700

    awscli 1.10.47
    Closes #2978.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -2,8 +2,8 @@
 class Awscli < Formula
   desc "Official Amazon AWS command-line interface"
   homepage "https://aws.amazon.com/cli/"
-  url "https://files.pythonhosted.org/packages/e9/55/c5abdca0fd0be853bb7a5c23dd6abf153506c104b00638e8c5f7395f5412/awscli-1.10.46.tar.gz"
-  sha256 "a4945c075c463b4ad67daef0d19ec0beac78bd64672e4f54ff4959591b945db4"
+  url "https://files.pythonhosted.org/packages/49/63/ad95ceb876bd289ecf7df301d73f310dcd7972444045b0bbbb3a6aeb1509/awscli-1.10.47.tar.gz"
+  sha256 "176ec3e5783ed3e9291da5e9b6fc100cb4550b843bb0bb7201e780e4174cbf70"
 
   bottle do
     cellar :any_skip_relocation
@@ -33,13 +33,13 @@ class Awscli < Formula
   depends_on :python if MacOS.version <= :lion
 
   resource "botocore" do
-    url "https://files.pythonhosted.org/packages/7a/f5/2b277868ddca54465f248e2ce4a1788b5c0ebab4518ba826747c3ec42ac9/botocore-1.4.36.tar.gz"
-    sha256 "69c094d2171620b6b59a05dfef533cb278c13f322a62fe9d3da903bb966d7e56"
+    url "https://files.pythonhosted.org/packages/82/a2/15499d05a2c9a2e7b89904d2ff6906589eeaa4533f99a4aa478400a06fcd/botocore-1.4.37.tar.gz"
+    sha256 "20dc1a0bf37019cac33e0f76d04a2831f2786cbee17f54da0e32a93caf86ef49"
   end
 
   resource "colorama" do
-    url "https://files.pythonhosted.org/packages/24/84/29ce4167d1f5c4a320aaad91e1178e5a1baf9cfe1c63f9077c5dade0e3cc/colorama-0.3.3.tar.gz"
-    sha256 "eb21f2ba718fbf357afdfdf6f641ab393901c7ca8d9f37edd0bee4806ffa269c"
+    url "https://files.pythonhosted.org/packages/f0/d0/21c6449df0ca9da74859edc40208b3a57df9aca7323118c913e58d442030/colorama-0.3.7.tar.gz"
+    sha256 "e043c8d32527607223652021ff648fbb394d5e19cba9f1a698670b338c9d782b"
   end
 
   resource "docutils" do
