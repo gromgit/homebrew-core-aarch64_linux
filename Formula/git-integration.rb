c6840b1b95e50d0e4495302d6098bc719864710a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-14 05:14:41 +0100

    git-integration: update 0.4 bottle.

--- Formula/git-integration.rb
+++ Formula/git-integration.rb
@@ -20,10 +20,9 @@ class GitIntegration < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "390d49ad2dc16a2a1167094276cd225a76bd0422afd9ee3ccae3127270edfc1d" => :el_capitan
-    sha256 "0a41fd7d3c278edd15c2ba11a0e55dfec44f261a998a8b1392454a7c9a5445f9" => :yosemite
-    sha256 "6d98f63e393b56144ea6754593eb6c77a252c6876c72febef273a15440a8e655" => :mavericks
-    sha256 "c00c4058c45f75f2c455484c19fc9c761316334e53ae3eb928ddaa140ce1e937" => :mountain_lion
+    sha256 "f91de5bf8a16b8b54db99566b81862c7d1e898dd332fd2d4d4bd457694443d62" => :el_capitan
+    sha256 "149a1f6f3cc6a413795893c6d63d48e82264a383aa901bee796c4d6a217b0c9b" => :yosemite
+    sha256 "2bae67c0933f3e0e990a12f1f90dd319cd788736a0cb50ad9f57e01195639331" => :mavericks
   end
 
   depends_on "asciidoc" => [:build, :optional]
