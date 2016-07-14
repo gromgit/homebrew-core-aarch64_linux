1c7069419ddb3328fb4084062ee5968641b14185
ilovezfs <ilovezfs@icloud.com>
2016-07-14 05:16:20 -0700

    libdrawtext 0.4
    remove upstreamed patches
    
    Closes #2996.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libdrawtext.rb
+++ Formula/libdrawtext.rb
@@ -1,36 +1,9 @@
 class Libdrawtext < Formula
   desc "Library for anti-aliased text rendering in OpenGL"
   homepage "http://nuclear.mutantstargoat.com/sw/libdrawtext/"
+  url "https://github.com/jtsiomb/libdrawtext/archive/release_0.4.tar.gz"
+  sha256 "e9460eb489e0ef6d1496afed2dae2e41c94005c85737ff53a8c09d51b6f93074"
   head "https://github.com/jtsiomb/libdrawtext.git"
-
-  stable do
-    url "http://nuclear.mutantstargoat.com/sw/libdrawtext/libdrawtext-0.3.tar.gz"
-    sha256 "e9485a137ea898606b61445cc5b248b248b4f688b4d9d0a137f975193bfc9de4"
-
-    # upstream commit "fixed bug in dtx_printf, uninitialized argument pointer"
-    patch do
-      url "https://github.com/jtsiomb/libdrawtext/commit/9e60bc82.patch"
-      sha256 "22de3a8d66d8fa1991d0590ec7d77e14ac4aac7f5171026084f4c85d766a8009"
-    end
-
-    # upstream commit "removed dependency to glew"
-    patch do
-      url "https://github.com/jtsiomb/libdrawtext/commit/a8650760.patch"
-      sha256 "3c502a8de284773d5ee1ab31c99f8c26f09bf36241529816e2039fb56ace7bc6"
-    end
-
-    # upstream commit "updated readme file" (notes that glew dep was removed)
-    patch do
-      url "https://github.com/jtsiomb/libdrawtext/commit/b4d06601.patch"
-      sha256 "0cd16dbe636c6cf602730eb818ddad2ce12a60045c82116e73e891ce23db0099"
-    end
-
-    # upstream commit "Makefile.in: remove -lGLEW"
-    patch do
-      url "https://github.com/jtsiomb/libdrawtext/commit/c7acc36d.patch"
-      sha256 "9d0b81f2bd9e995fda8024e6ef70a12f5ce3aaf6e9147f5068f1993c2a3948de"
-    end
-  end
 
   bottle do
     cellar :any
