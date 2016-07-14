ec0bb6e34bd93eaab0c0c6bf3061f32b227a8d2b
ilovezfs <ilovezfs@icloud.com>
2016-07-14 09:28:28 -0700

    p7zip 16.02
    remove upstreamed patches
    
    Closes #3003.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/p7zip.rb
+++ Formula/p7zip.rb
@@ -1,26 +1,14 @@
 class P7zip < Formula
   desc "7-Zip (high compression file archiver) implementation"
   homepage "http://p7zip.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/p7zip/p7zip/15.14.1/p7zip_15.14.1_src_all.tar.bz2"
-  sha256 "699db4da3621904113e040703220abb1148dfef477b55305e2f14a4f1f8f25d4"
-  revision 1
+  url "https://downloads.sourceforge.net/project/p7zip/p7zip/16.02/p7zip_16.02_src_all.tar.bz2"
+  sha256 "5eb20ac0e2944f6cb9c2d51dd6c4518941c185347d4089ea89087ffdd6e2341f"
 
   bottle do
     cellar :any_skip_relocation
     sha256 "434d44f856b99cb91d764fa7d33381fa99b6dada32414cc389448c43d894d958" => :el_capitan
     sha256 "9600ee488e04666b807ecbb0df5bafbb36428ca31e649a0a5d754991cc8c41dd" => :yosemite
     sha256 "f6d30782b659590984cff65fd9bb8e53a7ea32ee3eb0b3c43d623e7eeacdbd12" => :mavericks
-  end
-
-  # CVE-2016-2334 and CVE-2016-2335
-  # https://www.talosintel.com/reports/TALOS-2016-0093/
-  # https://www.talosintel.com/reports/TALOS-2016-0094/
-  # https://packages.qa.debian.org/p/p7zip/news/20160515T102412Z.html
-  patch do
-    url "https://mirrors.ocf.berkeley.edu/debian/pool/main/p/p7zip/p7zip_15.14.1+dfsg-2.debian.tar.xz"
-    mirror "https://mirrors.kernel.org/debian/pool/main/p/p7zip/p7zip_15.14.1%2bdfsg-2.debian.tar.xz"
-    sha256 "f4db6803535fc30b6ae9db5aabfd9f57a851c6773d72073847ec5e3731b7af37"
-    apply "patches/CVE-2016-2334.patch", "patches/CVE-2016-2335.patch"
   end
 
   def install
