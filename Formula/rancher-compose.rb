a03993f8e60ffebad5398f211a42de745e50bc2d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-14 07:48:15 +0100

    rancher-compose: update 0.8.6 bottle.

--- Formula/rancher-compose.rb
+++ Formula/rancher-compose.rb
@@ -6,9 +6,9 @@ class RancherCompose < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d6c053ce3474479f1fa0f834627eb7b0d16825d42b056f162d14acdb5d4bd3aa" => :el_capitan
-    sha256 "e4dbd18eef5f87d48a9e38f6a105abc85ecbe20ae5db97e4269e225a4a82dcda" => :yosemite
-    sha256 "e2467027368d09e7b0ea1243db21ed210e73bdff956deb81a4cf5ab2ef751a41" => :mavericks
+    sha256 "662c401e194be2c8d088795e288d562cb88faa7327028c94a1fef6c79e53b48b" => :el_capitan
+    sha256 "288cf7c2fcaeacbadcc2fe44e1a0e91df9f92b6174696aa8286706859356a59c" => :yosemite
+    sha256 "83fa597f44fa6f5accb9bb8d3264b1c98dba8c923c93a3a1fc9057473526e715" => :mavericks
   end
 
   depends_on "go" => :build
