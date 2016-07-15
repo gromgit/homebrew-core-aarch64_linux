2a5151625cf10346b2ebcb7d62a9c1b3b6cbaf9d
Olivier "Oli" Dagenais <olivier.dagenais@gmail.com>
2016-07-14 22:04:28 -0400

    git-credential-manager 2.0.0 (#3008)

--- Formula/git-credential-manager.rb
+++ Formula/git-credential-manager.rb
@@ -1,16 +1,12 @@
 class GitCredentialManager < Formula
   desc "Stores Git credentials for Visual Studio Team Services"
   homepage "https://java.visualstudio.com/Docs/tools/gitcredentialmanager"
-  url "https://github.com/Microsoft/Git-Credential-Manager-for-Mac-and-Linux/releases/download/git-credential-manager-1.7.1/git-credential-manager-1.7.1.jar"
-  sha256 "99afa1079db6de3f3a2edd6efc008ad42b92f9934944491a35bc682c228a1b81"
+  url "https://github.com/Microsoft/Git-Credential-Manager-for-Mac-and-Linux/releases/download/git-credential-manager-2.0.0/git-credential-manager-2.0.0.jar"
+  sha256 "1216f16ce811647428b3f48f124e08d40defa397ee88141c7f505e4d0bc609bb"
 
   bottle :unneeded
 
-  if MacOS.version >= :el_capitan
-    depends_on :java => "1.8+"
-  else
-    depends_on :java => "1.7+"
-  end
+  depends_on :java => "1.6+"
 
   def install
     libexec.install "git-credential-manager-#{version}.jar"
