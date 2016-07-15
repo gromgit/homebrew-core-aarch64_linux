82e33489e8fe85e7411ec0e013439fb776ceb99c
Viktor Szakats <vszakats@users.noreply.github.com>
2016-07-15 19:24:01 +0200

    libcuefile: use secure url and fix an audit issue

--- Formula/libcuefile.rb
+++ Formula/libcuefile.rb
@@ -1,9 +1,9 @@
 class Libcuefile < Formula
   desc "Library to work with CUE files"
   homepage "https://www.musepack.net/"
-  url "http://files.musepack.net/source/libcuefile_r475.tar.gz"
-  sha256 "b681ca6772b3f64010d24de57361faecf426ee6182f5969fcf29b3f649133fe7"
+  url "https://files.musepack.net/source/libcuefile_r475.tar.gz"
   version "r475"
+  sha256 "b681ca6772b3f64010d24de57361faecf426ee6182f5969fcf29b3f649133fe7"
 
   bottle do
     cellar :any
