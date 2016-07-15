5539e81b17e784e07513df25c2af795a84ec53b6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-15 17:45:00 +0100

    es: update 0.9.1 bottle.

--- Formula/es.rb
+++ Formula/es.rb
@@ -5,10 +5,10 @@ class Es < Formula
   sha256 "b0b41fce99b122a173a06b899a4d92e5bd3cc48b227b2736159f596a58fff4ba"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "b8588bd88d1e96f8a7093934ebd004c0890334899892137014a6cdcd641c2720" => :el_capitan
-    sha256 "8d03d2211e286a2481f7f3876324c5db14a886c3b95585e873ecd99412928b41" => :yosemite
-    sha256 "83d459a0d8b05ffd742b950053ce1ae1b9f4f0cc2fd1a5375fcd690a8aa9d5e5" => :mavericks
+    cellar :any
+    sha256 "913ca2237f6dee8fda5024158cf74d6c531fa5f1d5543e48bd88a0b0c00043f0" => :el_capitan
+    sha256 "b030162c4cf054e3c5faaf4184634f438734ef5d44d0fa0b26e21da53cfe3b97" => :yosemite
+    sha256 "dc05ff3d6cda43cd0c8a0423eeefaf8a517d6a5412b561b7092451c4d49e1adf" => :mavericks
   end
 
   option "with-readline", "Use readline instead of libedit"
