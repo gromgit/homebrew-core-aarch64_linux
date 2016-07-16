e2f8c62557bc825daf84485c317032eea6a2e60b
ilovezfs <ilovezfs@icloud.com>
2016-07-16 04:42:53 -0700

    pyenv-virtualenv 20160716
    Closes #3055.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pyenv-virtualenv.rb
+++ Formula/pyenv-virtualenv.rb
@@ -1,8 +1,8 @@
 class PyenvVirtualenv < Formula
   desc "Pyenv plugin to manage virtualenv"
   homepage "https://github.com/yyuu/pyenv-virtualenv"
-  url "https://github.com/yyuu/pyenv-virtualenv/archive/v20160315.tar.gz"
-  sha256 "f2f5149b35894982a9aa17d05c1f0131b7b365c34b4555db0f6f08125f6e241b"
+  url "https://github.com/yyuu/pyenv-virtualenv/archive/v20160716.tar.gz"
+  sha256 "453c711e79a171eac69494e327705d3e26dcfaf4323da33f1c506d84e32ca6d5"
   head "https://github.com/yyuu/pyenv-virtualenv.git"
 
   bottle :unneeded
