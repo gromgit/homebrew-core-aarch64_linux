ede420140df7d184ee11b1bc0de352383738adca
Dominyk Tiller <dominyktiller@gmail.com>
2016-07-17 21:24:43 +0100

    espeak: nest documentation

--- Formula/espeak.rb
+++ Formula/espeak.rb
@@ -15,12 +15,12 @@ class Espeak < Formula
 
   def install
     share.install "espeak-data"
-    share.install "docs"
+    doc.install Dir["docs/*"]
     cd "src" do
       rm "portaudio.h"
       inreplace "Makefile", "SONAME_OPT=-Wl,-soname,", "SONAME_OPT=-Wl,-install_name,"
       # OS X does not use -soname so replacing with -install_name to compile for OS X.
-      # See http://stackoverflow.com/questions/4580789/ld-unknown-option-soname-on-os-x/32280483#32280483
+      # See https://stackoverflow.com/questions/4580789/ld-unknown-option-soname-on-os-x/32280483#32280483
       inreplace "speech.h", "#define USE_ASYNC", "//#define USE_ASYNC"
       # OS X does not provide sem_timedwait() so disabling #define USE_ASYNC to compile for OS X.
       # See https://sourceforge.net/p/espeak/discussion/538922/thread/0d957467/#407d
@@ -32,7 +32,7 @@ class Espeak < Formula
       lib.install "libespeak.so.1.1.48" => "libespeak.dylib"
       system "install_name_tool", "-id", "#{lib}/libespeak.dylib", "#{lib}/libespeak.dylib"
       # OS X does not use the convention libraryname.so.X.Y.Z. OS X uses the convention libraryname.X.dylib
-      # See http://stackoverflow.com/questions/4580789/ld-unknown-option-soname-on-os-x/32280483#32280483
+      # See https://stackoverflow.com/questions/4580789/ld-unknown-option-soname-on-os-x/32280483#32280483
     end
   end
 
