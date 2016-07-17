253e0010eb45af8dcf3e3a5181e3fa606811161f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-17 17:30:49 +0100

    vifm: update 0.8.2 bottle.

--- Formula/vifm.rb
+++ Formula/vifm.rb
@@ -5,9 +5,9 @@ class Vifm < Formula
   sha256 "8b466d766658a24d07fc2039a26fefc6a018f5653684a6035183ca79f02c211f"
 
   bottle do
-    sha256 "46ad2f98c56e9306c00540ead159cbc70f02e4a7947e9f3f80f2408a01752f01" => :el_capitan
-    sha256 "140c708112af6fc1c4f7c740abdca3af0e3fc12a8205cfa428800f7794541b6f" => :yosemite
-    sha256 "ba80ce8c6d4762404cfd8f1595823ad1d7e347447dbbe50c1d73ad75a10efdcf" => :mavericks
+    sha256 "cd87c86ced448263187664d65b06a627a311d0ea456a6eda4c68bddadca82de6" => :el_capitan
+    sha256 "0967f7e9dce7d0fa2572e897532dea2af274726a944a518c13119547a2b4f699" => :yosemite
+    sha256 "7cf24c1685bbf8b4cdc799f3f6ff767809207dd72b1f2f121d1a7b74c6c909c4" => :mavericks
   end
 
   def install
