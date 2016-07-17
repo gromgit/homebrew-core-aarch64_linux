41d801d275572625117ee336ecbf15bb6b5524e9
ilovezfs <ilovezfs@icloud.com>
2016-07-17 06:54:50 -0700

    slurm 0.4.3
    build is system is now cmake not scons
    
    Closes #2642.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/slurm.rb
+++ Formula/slurm.rb
@@ -1,8 +1,8 @@
 class Slurm < Formula
   desc "Yet another network load monitor"
   homepage "https://github.com/mattthias/slurm"
-  url "https://github.com/mattthias/slurm/archive/upstream/0.4.2.tar.gz"
-  sha256 "8a28e11650928d87a907f9b154f6efd1ad5854cdc56a528da2e02e756e0aa58e"
+  url "https://github.com/mattthias/slurm/archive/upstream/0.4.3.tar.gz"
+  sha256 "b960c0d215927be1d02c176e1b189321856030226c91f840284886b727d3a3ac"
 
   bottle do
     cellar :any_skip_relocation
@@ -12,14 +12,14 @@ class Slurm < Formula
     sha256 "d49d123d14395a089923427c6dfaad3048a7cd277c88a704584e9c3f22d3c783" => :mountain_lion
   end
 
-  depends_on "scons" => :build
+  depends_on "cmake" => :build
 
   def install
-    scons
-    bin.install "slurm"
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
   end
 
   test do
-    system "#{bin}/slurm", "-h"
+    system bin/"slurm", "-h"
   end
 end
