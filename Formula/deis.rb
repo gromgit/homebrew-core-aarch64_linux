cd4f9c62da17cbd02ec18b5aafd510190bfffe67
ilovezfs <ilovezfs@icloud.com>
2016-07-18 05:10:32 -0700

    deis 1.13.2
    use "godep restore" instead of go_resource blocks
    
    Closes #2588.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/deis.rb
+++ Formula/deis.rb
@@ -1,10 +1,8 @@
-require "language/go"
-
 class Deis < Formula
   desc "Deploy and manage applications on your own servers"
   homepage "http://deis.io"
-  url "https://github.com/deis/deis/archive/v1.13.1.tar.gz"
-  sha256 "6fd7d3f7947437ba513654cf893c06a761321dd6fbe53ab56a3d1ca2e30bd060"
+  url "https://github.com/deis/deis/archive/v1.13.2.tar.gz"
+  sha256 "3e583cc0af91fa617b1b732acd38beb8c094cfcd511af19ebac949533c981e2b"
 
   bottle do
     cellar :any_skip_relocation
@@ -16,35 +14,15 @@ class Deis < Formula
   depends_on "go" => :build
   depends_on "godep" => :build
 
-  go_resource "github.com/docopt/docopt-go" do
-    url "https://github.com/docopt/docopt-go.git",
-      :revision => "854c423c810880e30b9fecdabb12d54f4a92f9bb"
-  end
-
-  go_resource "golang.org/x/crypto" do
-    url "https://go.googlesource.com/crypto.git",
-      :revision => "f7445b17d61953e333441674c2d11e91ae4559d3"
-  end
-
-  go_resource "gopkg.in/yaml.v2" do
-    url "https://github.com/go-yaml/yaml.git",
-      :revision => "eca94c41d994ae2215d455ce578ae6e2dc6ee516"
-  end
-
   def install
     ENV["GOPATH"] = buildpath
-    mkdir_p "#{buildpath}/client/Godeps/_workspace/src/github.com/deis"
-    ln_s buildpath, "#{buildpath}/client/Godeps/_workspace/src/github.com/deis/deis"
-
-    Language::Go.stage_deps resources, buildpath/"src"
-
-    cd "client" do
-      system "godep", "go", "build", "-a", "-ldflags", "-s", "-o", "dist/deis"
-      bin.install "dist/deis"
-    end
+    (buildpath/"src/github.com/deis").mkpath
+    ln_s buildpath, "src/github.com/deis/deis"
+    system "godep", "restore"
+    system "go", "build", "-o", bin/"deis", "client/deis.go"
   end
 
   test do
-    system "#{bin}/deis", "logout"
+    system bin/"deis", "logout"
   end
 end
