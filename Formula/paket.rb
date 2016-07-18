408164a6869fc435106c1086ea79f0f256e65b06
Sergey Kostrukov <kostrse@gmail.com>
2016-07-19 01:57:52 +0800

    paket 3.7.3 (new formula) (#3074)
    * paket 3.7.3 (new formula)
    
    A new formula for **paket** package manager for .NET and Mono (http://fsprojects.github.io/Paket/).
    Includes primitive tests for install and pack commands.
    
    * paket 3.7.3 (new formula)
    
    A new formula for paket package manager for .NET and Mono (http://fsprojects.github.io/Paket/).
    Includes primitive tests for install and pack commands.
    
    FIXED: paket.rb location.
    
    * Fixed dependency to Mono as :recommended

---
+++ Formula/paket.rb
@@ -0,0 +1,46 @@
+class Paket < Formula
+  desc "Dependency manager for .NET with support for NuGet and Git repositories"
+  homepage "https://fsprojects.github.io/Paket/"
+  url "https://github.com/fsprojects/Paket/releases/download/3.7.3/paket.exe"
+  sha256 "fcb4efb71c78831b89b15c39a793165d34172677363d66e005e6789afb76fc2c"
+
+  bottle :unneeded
+
+  depends_on "mono" => :recommended
+
+  def install
+    libexec.install "paket.exe"
+    (bin/"paket").write <<-EOS.undent
+      #!/bin/bash
+      mono #{libexec}/paket.exe "$@"
+    EOS
+  end
+
+  test do
+    test_package_id = "Paket.Test"
+    test_package_version = "1.2.3"
+
+    touch testpath/"paket.dependencies"
+    touch testpath/"testfile.txt"
+
+    system bin/"paket", "install"
+    assert (testpath/"paket.lock").exist?
+
+    (testpath/"paket.template").write <<-EOS.undent
+      type file
+
+      id #{test_package_id}
+      version #{test_package_version}
+      authors Test package author
+
+      description
+          Description of this test package
+
+      files
+          testfile.txt ==> lib
+    EOS
+
+    system bin/"paket", "pack", "output", testpath
+    assert (testpath/"#{test_package_id}.#{test_package_version}.nupkg").exist?
+  end
+end
