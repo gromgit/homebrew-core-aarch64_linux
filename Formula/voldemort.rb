d6184574086edf725bd98306f5cbe8f765604b0a
ilovezfs <ilovezfs@icloud.com>
2016-07-18 01:58:51 -0700

    voldemort 1.10.18
    - build system now uses gradle instead of ant
    - use env scripts and remove the caveats
    
    Closes #2633.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/voldemort.rb
+++ Formula/voldemort.rb
@@ -1,8 +1,8 @@
 class Voldemort < Formula
   desc "Distributed key-value storage system"
   homepage "http://www.project-voldemort.com/"
-  url "https://github.com/voldemort/voldemort/archive/release-1.9.5-cutoff.tar.gz"
-  sha256 "d521943873665d863a207c93557dc322c091e22a4293df4e3b86eb298ebbda7c"
+  url "https://github.com/voldemort/voldemort/archive/release-1.10.18-cutoff.tar.gz"
+  sha256 "a51fd1a53adb36a3fc70617ae5cd05ed31892a64074dfb612bb4a78aae28ecf6"
 
   bottle do
     cellar :any_skip_relocation
@@ -11,34 +11,25 @@ class Voldemort < Formula
     sha256 "db6c54af16bfb881914e38ef90d2ce906a64c6f210a90ed03279c66c6e396a05" => :mavericks
   end
 
-  depends_on :ant => :build
+  depends_on "gradle" => :build
   depends_on :java => "1.7+"
 
   def install
-    args = []
-    # ant on ML and below is too old to support 1.8
-    args << "-Dbuild.compiler=javac1.7" if MacOS.version < :mavericks
-    system "ant", *args
-    libexec.install %w[bin lib dist contrib]
-    libexec.install "config" => "config-examples"
-    (libexec/"config").mkpath
-
-    # Write shim scripts for all utilities
-    bin.write_exec_script Dir["#{libexec}/bin/*.sh"]
-  end
-
-  def caveats; <<-EOS.undent
-    You will need to set VOLDEMORT_HOME to:
-      #{libexec}
-
-    Config files should be placed in:
-      #{libexec}/config
-    or you can set VOL_CONF_DIR to a more reasonable path.
-    EOS
+    ENV.java_cache
+    system "./gradlew", "build", "-x", "test"
+    libexec.install %w[lib dist contrib]
+    bin.install Dir["bin/*{.sh,.py}"]
+    libexec.install "bin"
+    pkgshare.install "config" => "config-examples"
+    (etc/"voldemort").mkpath
+    env = {
+      :VOLDEMORT_HOME => libexec,
+      :VOLDEMORT_CONFIG_DIR => etc/"voldemort",
+    }
+    bin.env_script_all_files(libexec/"bin", env)
   end
 
   test do
-    ENV["VOLDEMORT_HOME"] = libexec
-    system "#{bin}/vadmin.sh"
+    system bin/"vadmin.sh"
   end
 end
