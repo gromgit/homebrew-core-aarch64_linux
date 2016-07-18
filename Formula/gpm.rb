118fabc9cb3be351e39e79cccd11bba79d6a622a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-18 10:26:24 +0100

    gpm: update 1.4.0 bottle.

--- Formula/gpm.rb
+++ Formula/gpm.rb
@@ -6,10 +6,9 @@ class Gpm < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "baef6fd02013c452c8d74ac109de4191823ebc4fb957e184ffd620b4d71b87d4" => :el_capitan
-    sha256 "8694759b92b7a01d234ab3369391dcd65fb5558a15d678d5c73cb957eb7573d4" => :yosemite
-    sha256 "c4fb2b9cea8b116d1c6145619797bff68cd97c8be9f14dcd9c99cd6bf6e24deb" => :mavericks
-    sha256 "0efe4d9fbd5207cff83169a571c7ddb815b434e99b206fc9989e9a309436451f" => :mountain_lion
+    sha256 "a154116f76d46e8937df820ff8f36b2600ffa80f32bf0b37fca2204318306efe" => :el_capitan
+    sha256 "953434734f58fedf6224bc78310842ea7b49570274eee4d9f7365d2dc0876014" => :yosemite
+    sha256 "e3e4f39ffb4e13bdc4d3dcb96a44a9862f983213e21130969e8b4aafa7b0c9dd" => :mavericks
   end
 
   depends_on "go"
