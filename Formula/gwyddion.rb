d345e3259f843007349f290c0c94c2fc6f2553e6
Zhiming Wang <zmwangx@gmail.com>
2016-07-19 08:06:32 +0800

    gwyddion: remove compiler flags found in the environment

--- Formula/gwyddion.rb
+++ Formula/gwyddion.rb
@@ -54,8 +54,7 @@ class Gwyddion < Formula
     libpng = Formula["libpng"]
     pango = Formula["pango"]
     pixman = Formula["pixman"]
-    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
-    flags += %W[
+    flags = %W[
       -I#{atk.opt_include}/atk-1.0
       -I#{cairo.opt_include}/cairo
       -I#{fftw.opt_include}
