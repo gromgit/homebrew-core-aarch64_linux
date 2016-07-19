aac3ed08921e3ac338ef4eb30e72aba1894ee0f5
Zhiming Wang <zmwangx@gmail.com>
2016-07-19 08:06:38 +0800

    webkitgtk: remove compiler flags found in the environment
    Closes #3104.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/webkitgtk.rb
+++ Formula/webkitgtk.rb
@@ -72,8 +72,7 @@ class Webkitgtk < Formula
     libsoup = Formula["libsoup"]
     pango = Formula["pango"]
     pixman = Formula["pixman"]
-    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
-    flags += %W[
+    flags = %W[
       -I#{atk.opt_include}/atk-1.0
       -I#{cairo.opt_include}/cairo
       -I#{fontconfig.opt_include}
