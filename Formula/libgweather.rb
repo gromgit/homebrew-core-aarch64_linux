442ec135c3f6d31bb169fa1fa16eeaf5a8dd25bd
Zhiming Wang <zmwangx@gmail.com>
2016-07-19 08:06:34 +0800

    libgweather: remove compiler flags found in the environment

--- Formula/libgweather.rb
+++ Formula/libgweather.rb
@@ -58,8 +58,7 @@ class Libgweather < Formula
     libsoup = Formula["libsoup"]
     pango = Formula["pango"]
     pixman = Formula["pixman"]
-    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
-    flags += %W[
+    flags = %W[
       -I#{atk.opt_include}/atk-1.0
       -I#{cairo.opt_include}/cairo
       -I#{fontconfig.opt_include}
