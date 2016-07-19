00aeaea4affec335a944e3083d2668276951419c
Zhiming Wang <zmwangx@gmail.com>
2016-07-19 08:06:28 +0800

    gitg: remove compiler flags found in the environment

--- Formula/gitg.rb
+++ Formula/gitg.rb
@@ -74,8 +74,7 @@ class Gitg < Formula
     pango = Formula["pango"]
     pixman = Formula["pixman"]
     webkitgtk = Formula["webkitgtk"]
-    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
-    flags += %W[
+    flags = %W[
       -I#{atk.opt_include}/atk-1.0
       -I#{cairo.opt_include}/cairo
       -I#{fontconfig.opt_include}
