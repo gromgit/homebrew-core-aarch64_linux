fb16fe55c265e3d1a0d9cb3cb7d9c621a16fbcef
Zhiming Wang <zmwangx@gmail.com>
2016-07-19 08:06:26 +0800

    clutter-gst: remove compiler flags found in the environment

--- Formula/clutter-gst.rb
+++ Formula/clutter-gst.rb
@@ -58,8 +58,7 @@ class ClutterGst < Formula
     libpng = Formula["libpng"]
     pango = Formula["pango"]
     pixman = Formula["pixman"]
-    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
-    flags += %W[
+    flags = %W[
       -I#{atk.opt_include}/atk-1.0
       -I#{cairo.opt_include}/cairo
       -I#{clutter.opt_include}/clutter-1.0
