661ca1d20ffa8ae334ce97ea210c9d696cd2da06
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-19 08:55:21 +0100

    htmlcxx: update 0.86 bottle.

--- Formula/htmlcxx.rb
+++ Formula/htmlcxx.rb
@@ -5,9 +5,9 @@ class Htmlcxx < Formula
   sha256 "07542b5ea2442143b125ba213b6823ff4a23fff352ecdd84bbebe1d154f4f5c1"
 
   bottle do
-    sha256 "708b5eeb03eb5f2070f4fe34efb80f9198009a23106c98ec9aaf5472b49dbf1c" => :el_capitan
-    sha256 "a013c7a43b1587c458df1ebae586d4b15b2a3c994c7de12dc6c7c24a80886a09" => :yosemite
-    sha256 "fceb4660730162bdb44be089ab347afd7a24b3f06bc3579cffdf2bb5891466f5" => :mavericks
+    sha256 "2be957cd2a735529bc4e921733cdee1b7a7afdfc6614914c76e9fcd08a89c90e" => :el_capitan
+    sha256 "b456c9087eb6dc788ec52717cb7eef3bf6dac24dbdd0d46674ed30c8597398ef" => :yosemite
+    sha256 "86839a9ca4861d4409d7a71b6553d3cbff78d5f99bf2a72971c302891245ed64" => :mavericks
   end
 
   def install
