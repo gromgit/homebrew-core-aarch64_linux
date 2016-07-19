cad8a4530a66749c6f517ee25d1e3909034dd7fd
Zhiming Wang <zmwangx@gmail.com>
2016-07-19 08:06:28 +0800

    glibmm: remove compiler flags found in the environment

--- Formula/glibmm.rb
+++ Formula/glibmm.rb
@@ -36,8 +36,7 @@ class Glibmm < Formula
     gettext = Formula["gettext"]
     glib = Formula["glib"]
     libsigcxx = Formula["libsigc++"]
-    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
-    flags += %W[
+    flags = %W[
       -I#{gettext.opt_include}
       -I#{glib.opt_include}/glib-2.0
       -I#{glib.opt_lib}/glib-2.0/include
