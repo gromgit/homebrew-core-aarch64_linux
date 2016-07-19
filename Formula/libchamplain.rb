454552c6187ca61e835b30f859f293932b1769b7
Zhiming Wang <zmwangx@gmail.com>
2016-07-19 08:06:32 +0800

    libchamplain: remove compiler flags found in the environment

--- Formula/libchamplain.rb
+++ Formula/libchamplain.rb
@@ -53,8 +53,7 @@ class Libchamplain < Formula
     libsoup = Formula["libsoup"]
     pango = Formula["pango"]
     pixman = Formula["pixman"]
-    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
-    flags += %W[
+    flags = %W[
       -I#{atk.opt_include}/atk-1.0
       -I#{cairo.opt_include}/cairo
       -I#{clutter.opt_include}/clutter-1.0
