8fe03e354342d540ea4ad5af8b62a502159934fb
Zhiming Wang <zmwangx@gmail.com>
2016-07-19 08:06:28 +0800

    glade: remove compiler flags found in the environment

--- Formula/glade.rb
+++ Formula/glade.rb
@@ -61,8 +61,7 @@ class Glade < Formula
     libpng = Formula["libpng"]
     pango = Formula["pango"]
     pixman = Formula["pixman"]
-    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
-    flags += %W[
+    flags = %W[
       -I#{atk.opt_include}/atk-1.0
       -I#{cairo.opt_include}/cairo
       -I#{fontconfig.opt_include}
