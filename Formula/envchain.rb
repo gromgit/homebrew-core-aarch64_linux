fcbf5551b8e3437d8162f7edd1ced7c1746fccdd
ilovezfs <ilovezfs@icloud.com>
2016-07-18 23:07:04 -0700

    envchain 1.0.1
    Closes #3107.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/envchain.rb
+++ Formula/envchain.rb
@@ -1,8 +1,8 @@
 class Envchain < Formula
   desc "Secure your credentials in environment variables"
   homepage "https://github.com/sorah/envchain"
-  url "https://github.com/sorah/envchain/archive/v0.2.0.tar.gz"
-  sha256 "2a863688d1e0bdc47ba8339f57c8b5e22f5002fd3ab58928766e45f23c6ca267"
+  url "https://github.com/sorah/envchain/archive/v1.0.1.tar.gz"
+  sha256 "09af1fe1cfba3719418f90d59c29c081e1f22b38249f0110305b657bd306e9ae"
   head "https://github.com/sorah/envchain.git"
 
   bottle do
