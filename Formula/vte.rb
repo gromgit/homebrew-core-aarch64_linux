aea450ce2d5960821c82f4597d6941fdb1f0fcf5
Zhiming Wang <zmwangx@gmail.com>
2016-07-19 08:06:37 +0800

    vte: remove compiler flags found in the environment

--- Formula/vte.rb
+++ Formula/vte.rb
@@ -55,8 +55,7 @@ class Vte < Formula
     libpng = Formula["libpng"]
     pango = Formula["pango"]
     pixman = Formula["pixman"]
-    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
-    flags += %W[
+    flags = %W[
       -I#{atk.opt_include}/atk-1.0
       -I#{cairo.opt_include}/cairo
       -I#{fontconfig.opt_include}
