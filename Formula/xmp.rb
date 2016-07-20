415c075caf900e50843bd680f3b44455d86ee719
Misty De Meo <mistydemeo@gmail.com>
2016-07-20 21:09:37 +1000

    xmp 4.1.0
    Closes #3157.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/xmp.rb
+++ Formula/xmp.rb
@@ -1,8 +1,8 @@
 class Xmp < Formula
   desc "Command-line player for module music formats (MOD, S3M, IT, etc)"
   homepage "http://xmp.sourceforge.net"
-  url "https://downloads.sourceforge.net/project/xmp/xmp/4.0.11/xmp-4.0.11.tar.gz"
-  sha256 "ba09beefb9bc67cd01bba96e6c79c4896f9d99161ea41ddbfee0e25db09e7991"
+  url "https://downloads.sourceforge.net/project/xmp/xmp/4.1.0/xmp-4.1.0.tar.gz"
+  sha256 "1dbd61074783545ac7bef5b5daa772fd2110764cb70f937af8c3fad30f73289e"
 
   bottle do
     sha256 "0bc782b665dae4b64def3b78e59fa4ca4389f3afba0ab115d5ca707853d53b60" => :el_capitan
