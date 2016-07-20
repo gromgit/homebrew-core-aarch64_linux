73857e83a673732923fdd614239d77b153783c7a
ilovezfs <ilovezfs@icloud.com>
2016-07-20 02:43:14 -0700

    dockutil 2.0.4
    Closes #3155.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dockutil.rb
+++ Formula/dockutil.rb
@@ -1,8 +1,8 @@
 class Dockutil < Formula
   desc "Tool for managing dock items"
   homepage "https://github.com/kcrawford/dockutil"
-  url "https://github.com/kcrawford/dockutil/archive/2.0.3.tar.gz"
-  sha256 "6954330cc2b5280bbb345c208f9cb1c51a50c2b04a7ab5d278e987167ec01a4b"
+  url "https://github.com/kcrawford/dockutil/archive/2.0.4.tar.gz"
+  sha256 "630adf817dffb1eeb6484e7d5d997ea1018dd57e2729773dcb90a0958ed46a24"
 
   bottle :unneeded
 
