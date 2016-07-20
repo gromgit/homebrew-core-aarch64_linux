0d5d675c2e258363215098f6e448520952590ca3
Nikolay Siniakevich <croofti@gmail.com>
2016-07-20 23:38:32 +0300

    jetty 9.3.10.v20160621 (#3130)

--- Formula/jetty.rb
+++ Formula/jetty.rb
@@ -1,9 +1,9 @@
 class Jetty < Formula
   desc "Java servlet engine and webserver"
   homepage "https://www.eclipse.org/jetty/"
-  url "http://download.eclipse.org/jetty/9.3.9.v20160517/dist/jetty-distribution-9.3.9.v20160517.tar.gz"
-  version "9.3.9.v20160517"
-  sha256 "1adf7b0be04d8060d77baee34cfd83726803a9474e6b291042922759c9661f7b"
+  url "http://download.eclipse.org/jetty/9.3.10.v20160621/dist/jetty-distribution-9.3.10.v20160621.tar.gz"
+  version "9.3.10.v20160621"
+  sha256 "7f7de08bc395a8973c4de5fda57e91fa511b52f4131c55677c254581ee3a1b77"
 
   bottle :unneeded
 
