ba84d1bf97de0236492536357dfed4f0ac13026a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-21 12:09:25 +0100

    pacapt: update 2.3.13 bottle.

--- Formula/pacapt.rb
+++ Formula/pacapt.rb
@@ -6,9 +6,9 @@ class Pacapt < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6d2c7835da1f6a8757509d6cf39b4628b4f718874d51534b40c7610cf3170aaf" => :el_capitan
-    sha256 "cf581fde8d82808464244f7448f270c0b06a73c43248f834fc0e203df41914f6" => :yosemite
-    sha256 "a0f15f58f40a9270f449daf77cef89a58e2d9b04acbe3da94e467ee77e5311ec" => :mavericks
+    sha256 "7f6cffa5423937eadf3fa5ce19f1a579dd36394a30717514b245de18881940ab" => :el_capitan
+    sha256 "6a08957b772fcf7adc3e15e03bfd94c25eb5e204c33ec2a3812c3a3c7977b691" => :yosemite
+    sha256 "129d05a1bdbbc6f5b669bc3ae9d4750df51459b7689de480e59ea116b65705d4" => :mavericks
   end
 
   def install
