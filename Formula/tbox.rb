0e309c842af1885dba7d0964a320f3684d425bd8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-22 06:20:53 +0100

    tbox: add 1.5.2 bottle.

--- Formula/tbox.rb
+++ Formula/tbox.rb
@@ -5,6 +5,13 @@ class Tbox < Formula
   sha256 "c470a8a5b8f84d928d83af87c8f69c87ec9ecb6f89017bef93dc0d188e91a8c6"
   head "https://github.com/waruqi/tbox.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9e0491f0d90040d46c9ae24801d2eb330a47e27a8b53b5027044ce1785c141cf" => :el_capitan
+    sha256 "3b1fd6ca8831b887d924d375d5ce974810bd2d6ee02ce812d35ed28e8e5cee8f" => :yosemite
+    sha256 "4ba1ed7572832dd6e51be2bc6abf58e0a861e370b82007ed8d064f2eeca3ed91" => :mavericks
+  end
+
   depends_on "xmake" => :build
 
   def install
