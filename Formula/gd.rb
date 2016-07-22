1b631f88eff1b9aeaf7b06aa1716b6bf96e02afb
ilovezfs <ilovezfs@icloud.com>
2016-07-22 00:17:28 -0700

    gd 2.2.3
    remove upstreamed patch
    
    Closes #3213.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gd.rb
+++ Formula/gd.rb
@@ -1,21 +1,8 @@
 class Gd < Formula
   desc "Graphics library to dynamically manipulate images"
   homepage "https://libgd.github.io/"
-
-  stable do
-    url "https://github.com/libgd/libgd/releases/download/gd-2.2.2/libgd-2.2.2.tar.xz"
-    sha256 "489f756ce07f0c034b1a794f4d34fdb4d829256112cb3c36feb40bb56b79218c"
-
-    # OS X linker restricts the revision field to 8 bits: libgd/libgd#214.
-    # Same as https://github.com/libgd/libgd/commit/502e4cd8, but recommended by
-    # upstream for patching the release tarball; already fixed in HEAD.
-    patch do
-      url "https://gitweb.gentoo.org/repo/gentoo.git/plain/media-libs/gd/files/gd-2.2.2-osx-libtool.patch?id=ede657b970d1deee8305dbefaf5651c37aea115c"
-      sha256 "8af30e9f8da6ca7ed28ee766e87b66d8ccf034745851760fb4fc8e9bc4907f14"
-    end
-  end
-
-  revision 2
+  url "https://github.com/libgd/libgd/releases/download/gd-2.2.3/libgd-2.2.3.tar.xz"
+  sha256 "746b6cbd6769a22ff3ba6f5756f3512a769bd4cdf4695dff17f4867f25fa7d3c"
 
   bottle do
     cellar :any
