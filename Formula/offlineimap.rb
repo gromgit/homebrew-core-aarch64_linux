114eaae0e36df38ff972734773dd04ee20e31c4a
ilovezfs <ilovezfs@icloud.com>
2016-07-23 05:27:14 -0700

    offlineimap 7.0.0
    Closes #3255.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/offlineimap.rb
+++ Formula/offlineimap.rb
@@ -1,17 +1,25 @@
 class Offlineimap < Formula
   desc "Synchronizes emails between two repositories"
   homepage "http://offlineimap.org/"
-  url "https://github.com/OfflineIMAP/offlineimap/archive/v6.7.0.1.tar.gz"
-  sha256 "00e91ff782217506dd91f502d094ef0daa27732cfc68c914029aefc1c53642fb"
+  url "https://github.com/OfflineIMAP/offlineimap/archive/v7.0.0.tar.gz"
+  sha256 "2c664d85bd99057ec87cca2860074db60ceadb52179f47973ff1ac68fc72de42"
   head "https://github.com/OfflineIMAP/offlineimap.git"
 
-  bottle :unneeded
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/b3/b2/238e2590826bfdd113244a40d9d3eb26918bd798fc187e2360a8367068db/six-1.10.0.tar.gz"
+    sha256 "105f8d68616f8248e24bf0e9372ef04d3cc10104f1980f54d57b2ce73a5ad56a"
+  end
 
   def install
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+    resource("six").stage do
+      system "python", *Language::Python.setup_install_args(libexec/"vendor")
+    end
     etc.install "offlineimap.conf", "offlineimap.conf.minimal"
     libexec.install "bin/offlineimap" => "offlineimap.py"
     libexec.install "offlineimap"
-    bin.install_symlink libexec+"offlineimap.py" => "offlineimap"
+    (bin/"offlineimap").write_env_script(libexec/"offlineimap.py",
+      :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
   def caveats; <<-EOS.undent
