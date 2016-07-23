d652ebf60bab737d845b64de50cd61599c953062
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-23 20:24:42 +0100

    offlineimap: add 7.0.0 bottle.

--- Formula/offlineimap.rb
+++ Formula/offlineimap.rb
@@ -5,6 +5,13 @@ class Offlineimap < Formula
   sha256 "2c664d85bd99057ec87cca2860074db60ceadb52179f47973ff1ac68fc72de42"
   head "https://github.com/OfflineIMAP/offlineimap.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bdc9bcf613554253e0ab2deef59d69eec40bbb4e84acd4d3f6d3cd91292b9563" => :el_capitan
+    sha256 "ef9f16128e5c1b1cc2ebae438de3f9b757b2f46edb04cf4100096e59084684e4" => :yosemite
+    sha256 "bf3db1eea4b8dfb41fb5ff66f70c43a2191d65f3c8f123a6134123d816921365" => :mavericks
+  end
+
   resource "six" do
     url "https://files.pythonhosted.org/packages/b3/b2/238e2590826bfdd113244a40d9d3eb26918bd798fc187e2360a8367068db/six-1.10.0.tar.gz"
     sha256 "105f8d68616f8248e24bf0e9372ef04d3cc10104f1980f54d57b2ce73a5ad56a"
