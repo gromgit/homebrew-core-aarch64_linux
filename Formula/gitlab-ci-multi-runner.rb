22a346a30f5d11e542fb5d2a0c5e99d87356c39e
ilovezfs <ilovezfs@icloud.com>
2016-07-24 08:31:23 -0700

    gitlab-ci-multi-runner 1.4.0
    Closes #3231.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gitlab-ci-multi-runner.rb
+++ Formula/gitlab-ci-multi-runner.rb
@@ -4,8 +4,8 @@ class GitlabCiMultiRunner < Formula
   desc "The official GitLab CI runner written in Go"
   homepage "https://gitlab.com/gitlab-org/gitlab-ci-multi-runner"
   url "https://gitlab.com/gitlab-org/gitlab-ci-multi-runner.git",
-    :tag => "v1.3.3",
-    :revision => "6220bd5f0cdb5008ba9488fabcd556959e80dce5"
+    :tag => "v1.4.0",
+    :revision => "5dd9b2fdfe118bd146b06f905dfa0f3346e2a575"
   head "https://gitlab.com/gitlab-org/gitlab-ci-multi-runner.git"
 
   bottle do
@@ -16,7 +16,6 @@ class GitlabCiMultiRunner < Formula
   end
 
   depends_on "go" => :build
-  depends_on "godep" => :build
   depends_on "docker" => :recommended
 
   go_resource "github.com/jteeuwen/go-bindata" do
@@ -25,18 +24,17 @@ class GitlabCiMultiRunner < Formula
   end
 
   resource "prebuilt-x86_64.tar.gz" do
-    url "https://gitlab-ci-multi-runner-downloads.s3.amazonaws.com/v1.3.3/docker/prebuilt-x86_64.tar.gz",
+    url "https://gitlab-ci-multi-runner-downloads.s3.amazonaws.com/v1.4.0/docker/prebuilt-x86_64.tar.gz",
       :using => :nounzip
-    version "1.3.3"
-    sha256 "5ae536cb7598607f1a0d4c0cb7f31d83d6c19e386106ea50192933a726f33ca9"
+    version "1.4.0"
+    sha256 "01246d4b97ff91e3ce7de258bf0987bee265ed71d647e5dbf10b104b7073a645"
   end
 
   def install
-    mkdir_p buildpath/"src/gitlab.com/gitlab-org"
-    ln_sf buildpath, buildpath/"src/gitlab.com/gitlab-org/gitlab-ci-multi-runner"
-
     ENV["GOPATH"] = buildpath
-
+    proj = "gitlab.com/gitlab-org/gitlab-ci-multi-runner"
+    dir = buildpath/"src/#{proj}"
+    dir.install buildpath.children
     ENV.prepend_create_path "PATH", buildpath/"bin"
     Language::Go.stage_deps resources, buildpath/"src"
 
@@ -44,20 +42,27 @@ class GitlabCiMultiRunner < Formula
       system "go", "install"
     end
 
-    resource("prebuilt-x86_64.tar.gz").stage do
-      system "go-bindata", "-pkg", "docker", "-nocompress", "-nomemcopy", "-nometadata", "-o", buildpath/"executors/docker/bindata.go", "prebuilt-x86_64.tar.gz"
-    end
+    cd dir do
+      resource("prebuilt-x86_64.tar.gz").stage do
+        system "go-bindata", "-pkg", "docker",
+               "-nocompress", "-nomemcopy", "-nometadata",
+               "-o", dir/"executors/docker/bindata.go",
+               "prebuilt-x86_64.tar.gz"
+      end
 
-    cd "src/gitlab.com/gitlab-org/gitlab-ci-multi-runner" do
-      commit_sha = `git rev-parse --short HEAD`
-
-      # Disable vendor support for go 1.5 and above
-      ENV["GO15VENDOREXPERIMENT"] = "0"
-
-      # Copy from Makefile
-      system "godep", "go", "build", "-o", "gitlab-ci-multi-runner", "-ldflags", "-X main.NAME=gitlab-ci-multi-runner -X main.VERSION=#{version} -X main.REVISION=#{commit_sha}"
+      commit = Utils.popen_read("git", "rev-parse", "--short", "HEAD")
+      branch = Utils.popen_read("git", "name-rev", "--name-only", "HEAD")
+      ldflags = %W[
+        --ldflags=
+        -X #{proj}/common.NAME=gitlab-ci-multi-runner
+        -X #{proj}/common.VERSION=#{version}
+        -X #{proj}/common.REVISION=#{commit}
+        -X #{proj}/common.BRANCH=#{branch.strip_prefix("remotes/origin/").chomp}
+      ]
+      system "go", "build", ldflags.join(" ")
       bin.install "gitlab-ci-multi-runner"
-      bin.install_symlink "#{bin}/gitlab-ci-multi-runner" => "gitlab-runner"
+      bin.install_symlink bin/"gitlab-ci-multi-runner" => "gitlab-runner"
+      prefix.install_metafiles
     end
   end
 
