b61a5d9822e9075c0aed0ed3ad743c40b65fc8d0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-24 11:55:26 +0100

    wireguard-tools: add 0.0.20160722 bottle.

--- Formula/wireguard-tools.rb
+++ Formula/wireguard-tools.rb
@@ -1,6 +1,13 @@
 class WireguardTools < Formula
   desc "Tools for the WireGuard secure network tunnel"
   homepage "https://www.wireguard.io/"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "23d88bb47ff2aa10cdc9add6fc326b60f7f1a60b9a419249bca20b1843bbaf1f" => :el_capitan
+    sha256 "6cee2c9452178aba10e5b118ed5c8d705467c5e25437cc6d587822a5d199a9c0" => :yosemite
+    sha256 "81110c65982e178495f38d39f5d2f1f3e5a9ada37b22bc44260cdd92ffe806b6" => :mavericks
+  end
+
   # Despite the experimental tag the tools themselves are stable.
   # Please only update version when the tools have been modified/updated.
   url "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-experimental-0.0.20160722.tar.xz"
