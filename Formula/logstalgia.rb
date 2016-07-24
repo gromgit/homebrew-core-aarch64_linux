88afbd755db56929bf900f4d1aae9fdc99a1d719
ilovezfs <ilovezfs@icloud.com>
2016-07-24 00:45:51 -0700

    logstalgia: revision for glew

--- Formula/logstalgia.rb
+++ Formula/logstalgia.rb
@@ -3,6 +3,7 @@ class Logstalgia < Formula
   homepage "http://logstalgia.io/"
   url "https://github.com/acaudwell/Logstalgia/releases/download/logstalgia-1.0.7/logstalgia-1.0.7.tar.gz"
   sha256 "5553fd03fb7be564538fe56e871eac6e3caf56f40e8abc4602d2553964f8f0e1"
+  revision 1
 
   bottle do
     sha256 "248428cb04a28dd6cfac58d860417324e2d3349314d0fcbbf180470e618ca8a0" => :el_capitan
