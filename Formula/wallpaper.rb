208fb49e956568fcbb28880ffeee36b073a07d75
Sindre Sorhus <sindresorhus@gmail.com>
2016-07-24 19:29:57 +0200

    wallpaper 1.2.0
    Closes #3273.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/wallpaper.rb
+++ Formula/wallpaper.rb
@@ -1,9 +1,9 @@
 class Wallpaper < Formula
   desc "Get or set the desktop wallpaper"
-  homepage "https://github.com/sindresorhus/osx-wallpaper"
-  url "https://github.com/sindresorhus/osx-wallpaper/archive/1.1.1.tar.gz"
-  sha256 "a1797eac72da83f947a2a1f12b7b85484607d5a218c8254d1ed5573e5fab92bb"
-  head "https://github.com/sindresorhus/osx-wallpaper.git"
+  homepage "https://github.com/sindresorhus/macos-wallpaper"
+  url "https://github.com/sindresorhus/macos-wallpaper/archive/1.2.0.tar.gz"
+  sha256 "d57ab0cafb9c11cdee8dba9a3a0b9dc5bc0e27cc64bad65529cc1e979a510620"
+  head "https://github.com/sindresorhus/macos-wallpaper.git"
 
   bottle do
     cellar :any_skip_relocation
