1b623abf69def3ace56dc7524c5ca96681796e27
Romain Lespinasse <romain.lespinasse@gmail.com>
2016-07-25 10:07:22 +0200

    golo 3.2.0-M3 (devel)
    Closes #3287.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/golo.rb
+++ Formula/golo.rb
@@ -6,9 +6,9 @@ class Golo < Formula
   head "https://github.com/eclipse/golo-lang.git"
 
   devel do
-    url "https://bintray.com/artifact/download/golo-lang/downloads/golo-3.2.0-M2.zip"
-    sha256 "ed2138b9896a74a37efe9331de6bc15a3d607aa07c8335fe5de1c8765f17d856"
-    version "3.2.0-M2"
+    url "https://bintray.com/artifact/download/golo-lang/downloads/golo-3.2.0-M3.zip"
+    sha256 "e40cf12df28d9b5327bb679341e491fbc87ab0ce7f1bab051d5f7630a7e9f8a9"
+    version "3.2.0-M3"
   end
   bottle :unneeded
 
