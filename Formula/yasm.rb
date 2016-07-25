f77fd88d3efafb7e3ee1a1fb60c03b8bc79a6161
Maxim Belkin <mbelkin@illinois.edu>
2016-07-25 01:37:58 -0500

    yasm: fix audit errors
    Closes #3285.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/yasm.rb
+++ Formula/yasm.rb
@@ -21,12 +21,12 @@ class Yasm < Formula
     depends_on "gettext"
   end
 
-  resource "cython" do
-	url "https://files.pythonhosted.org/packages/c6/fe/97319581905de40f1be7015a0ea1bd336a756f6249914b148a17eefa75dc/Cython-0.24.1.tar.gz"
-	sha256 "84808fda00508757928e1feadcf41c9f78e9a9b7167b6649ab0933b76f75e7b9"
-  end
-
   depends_on :python => :optional
+
+  resource "cython" do
+    url "https://files.pythonhosted.org/packages/c6/fe/97319581905de40f1be7015a0ea1bd336a756f6249914b148a17eefa75dc/Cython-0.24.1.tar.gz"
+    sha256 "84808fda00508757928e1feadcf41c9f78e9a9b7167b6649ab0933b76f75e7b9"
+  end
 
   def install
     args = %W[
