734b8ebdbf17acbbf81686a82b6afc7d58da88fc
Arnaud Kervern <arnaud.k@gmail.com>
2016-07-25 18:07:30 +0200

    nuxeo: update sha256 for upstream retag (#3230)
    * Update nuxeo 8.3 SHA256
    
    SHA256 has been updated.
    
    * Update nuxeo.rb

--- Formula/nuxeo.rb
+++ Formula/nuxeo.rb
@@ -1,9 +1,9 @@
 class Nuxeo < Formula
   desc "Enterprise Content Management"
   homepage "https://nuxeo.github.io/"
-  url "https://cdn.nuxeo.com/nuxeo-8.3/nuxeo-cap-8.3-tomcat.zip"
+  url "https://cdn.nuxeo.com/nuxeo-8.3/nuxeo-server-8.3-tomcat.zip"
   version "8.3"
-  sha256 "6de18ffa1737d6ef3fb9561f6c35f3200cfeedc7e7693e424ce31396eb42206c"
+  sha256 "69a51ea0f5663723a58448be80b5caf4258f9d1f8df124e4de90676224afac3c"
 
   bottle :unneeded
 
