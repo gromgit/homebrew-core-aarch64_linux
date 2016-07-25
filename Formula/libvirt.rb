55faf289b26bf124f6a3a700a39b60b0a1ccf7a1
Justin Clift <justin@postgresql.org>
2016-07-25 18:11:06 +0100

    libvirt 2.0.0
    Closes #3301.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/libvirt.rb
+++ Formula/libvirt.rb
@@ -1,8 +1,8 @@
 class Libvirt < Formula
   desc "C virtualization API"
   homepage "https://www.libvirt.org"
-  url "https://libvirt.org/sources/libvirt-1.3.5.tar.gz"
-  sha256 "93a23c44eb431da46c9458f95a66e29c9b98e37515d44b6be09e75b35ec94ac8"
+  url "https://libvirt.org/sources/libvirt-2.0.0.tar.xz"
+  sha256 "10e90af55e613953c0ddc60b4ac3a10c73c0f3493d7014259e3f012b2ffc9acb"
 
   bottle do
     sha256 "b8eca973a86ff46830181f18d318295991f561fd3b672b8c874cff7e8e8ae2de" => :el_capitan
@@ -27,6 +27,10 @@ class Libvirt < Formula
     build 2326
     cause "Undefined symbols when linking"
   end
+
+  # Fixes compile failure.  Will be in next upstream release:
+  #  https://www.redhat.com/archives/libvir-list/2016-July/msg00815.html
+  patch :p1, :DATA
 
   def install
     args = ["--prefix=#{prefix}",
@@ -62,4 +66,26 @@ class Libvirt < Formula
       end
     end
   end
+
+  test do
+    output = shell_output("#{bin}/virsh -v")
+    assert_match version.to_s, output
+  end
 end
+
+__END__
+diff --git a/src/util/virsystemd.c b/src/util/virsystemd.c
+index 969cd68..7d6985b 100644
+--- a/src/util/virsystemd.c
++++ b/src/util/virsystemd.c
+@@ -41,6 +41,10 @@
+
+ VIR_LOG_INIT("util.systemd");
+
++#ifndef MSG_NOSIGNAL
++# define MSG_NOSIGNAL 0
++#endif
++
+ static void virSystemdEscapeName(virBufferPtr buf,
+                                  const char *name)
+ {
