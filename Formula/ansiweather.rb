a52548b14371d857c01b97033903aab8d874f01b
ilovezfs <ilovezfs@icloud.com>
2016-07-26 10:54:42 -0700

    ansiweather 1.08
    Closes #3327.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/ansiweather.rb
+++ Formula/ansiweather.rb
@@ -1,8 +1,8 @@
 class Ansiweather < Formula
   desc "Weather in your terminal, with ANSI colors and Unicode symbols"
   homepage "https://github.com/fcambus/ansiweather"
-  url "https://github.com/fcambus/ansiweather/archive/1.07.tar.gz"
-  sha256 "f9b377b23ecc9c2d3567424b300b8e370eb0959c9b1cd0828ba07ce38f2ef0a0"
+  url "https://github.com/fcambus/ansiweather/archive/1.08.tar.gz"
+  sha256 "22dd814ef158df13b3fdbe72ca39e820874e710a4d341d1a2367aa771609665d"
   head "https://github.com/fcambus/ansiweather.git"
 
   bottle :unneeded
