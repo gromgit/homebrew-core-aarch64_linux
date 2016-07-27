d1fb1ada8b364fae9f335f3439c705aa903b2a92
Gary Ewan Park <gep13@gep13.co.uk>
2016-07-27 04:37:02 +0100

    cake 0.15.0 (#3342)

--- Formula/cake.rb
+++ Formula/cake.rb
@@ -1,8 +1,8 @@
 class Cake < Formula
   desc "'C# Make' is a build automation system with a C# DSL."
   homepage "http://cakebuild.net/"
-  url "https://github.com/cake-build/cake/releases/download/v0.14.0/Cake-bin-v0.14.0.zip"
-  sha256 "20842244b81f68abd8f652c422e901bd62f6fe6565b4863e4eff6e44a3e80038"
+  url "https://github.com/cake-build/cake/releases/download/v0.15.0/Cake-bin-v0.15.0.zip"
+  sha256 "2960297f63c99c991c1359efda245565a3725a017419f7453efbc16aba76224d"
 
   bottle :unneeded
 
