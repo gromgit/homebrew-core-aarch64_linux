240d2223b4d48453f8385bfa17630385f809387e
Damien Martin-Guillerez <dmarting@google.com>
2016-07-29 12:17:50 +0200

    bazel 0.3.1
    - use BAZEL_WRKDIR to put generated files in buildpath/"work"
    - output root of second Bazel run set to buildpath/"output_user_root"
    - install the wrapper script along with Bazel
    
    Closes #3427.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/bazel.rb
+++ Formula/bazel.rb
@@ -1,8 +1,8 @@
 class Bazel < Formula
   desc "Google's own build tool"
   homepage "http://www.bazel.io/"
-  url "https://github.com/bazelbuild/bazel/archive/0.3.0.tar.gz"
-  sha256 "d2309c29781dc4ede79eb652776517ca3c1dff29b58849dc80bdb86031cce3ad"
+  url "https://github.com/bazelbuild/bazel/archive/0.3.1.tar.gz"
+  sha256 "52beafc9d78fc315115226f31425e21df1714d96c7dfcdeeb02306e2fe028dd8"
   head "https://github.com/bazelbuild/bazel.git"
 
   bottle do
@@ -16,11 +16,15 @@ class Bazel < Formula
 
   def install
     ENV["EMBED_LABEL"] = "#{version}-homebrew"
+    # Force Bazel ./compile.sh to put its temporary files in the buildpath
+    ENV["BAZEL_WRKDIR"] = buildpath/"work"
 
     system "./compile.sh"
-    system "./output/bazel", "build", "scripts:bash_completion"
+    system "./output/bazel", "--output_user_root", buildpath/"output_user_root",
+           "build", "scripts:bash_completion"
 
-    bin.install "output/bazel" => "bazel"
+    bin.install "scripts/packages/bazel.sh" => "bazel"
+    bin.install "output/bazel" => "bazel-real"
     bash_completion.install "bazel-bin/scripts/bazel-complete.bash"
     zsh_completion.install "scripts/zsh_completion/_bazel"
   end
@@ -44,7 +48,7 @@ class Bazel < Formula
       )
     EOS
 
-    system "#{bin}/bazel", "build", "//:bazel-test"
+    system bin/"bazel", "build", "//:bazel-test"
     system "bazel-bin/bazel-test"
   end
 end
