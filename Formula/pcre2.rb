32e1fcfb0205a6e7b142bdeb6f1cbf8419669d2a
Dominyk Tiller <dominyktiller@gmail.com>
2016-07-29 23:41:01 +0100

    pcre2: style tweaks

--- Formula/pcre2.rb
+++ Formula/pcre2.rb
@@ -1,14 +1,10 @@
 class Pcre2 < Formula
   desc "Perl compatible regular expressions library with a new API"
   homepage "http://www.pcre.org/"
-
+  url "https://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre2-10.22.tar.bz2"
+  mirror "https://www.mirrorservice.org/sites/downloads.sourceforge.net/p/pc/pcre/pcre2/10.21/pcre2-10.22.tar.bz2"
+  sha256 "b2b44619f4ac6c50ad74c2865fd56807571392496fae1c9ad7a70993d018f416"
   head "svn://vcs.exim.org/pcre2/code/trunk"
-
-  stable do
-    url "https://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre2-10.22.tar.bz2"
-    mirror "https://www.mirrorservice.org/sites/downloads.sourceforge.net/p/pc/pcre/pcre2/10.21/pcre2-10.22.tar.bz2"
-    sha256 "b2b44619f4ac6c50ad74c2865fd56807571392496fae1c9ad7a70993d018f416"
-  end
 
   bottle do
     cellar :any
@@ -35,6 +31,6 @@ class Pcre2 < Formula
   end
 
   test do
-    system "#{bin}/pcre2grep", "regular expression", "#{prefix}/README"
+    system bin/"pcre2grep", "regular expression", prefix/"README"
   end
 end
