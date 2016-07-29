2aab7bf22e11cc6bd6189ebb346b03fa5f8baa5b
Viktor Szakats <vszakats@users.noreply.github.com>
2016-07-30 00:12:21 +0200

    pcre2 10.22
    Closes #3437.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/pcre2.rb
+++ Formula/pcre2.rb
@@ -5,13 +5,9 @@ class Pcre2 < Formula
   head "svn://vcs.exim.org/pcre2/code/trunk"
 
   stable do
-    url "https://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre2-10.21.tar.bz2"
-    mirror "https://www.mirrorservice.org/sites/downloads.sourceforge.net/p/pc/pcre/pcre2/10.21/pcre2-10.21.tar.bz2"
-    sha256 "c66a17509328a7251782691093e75ede7484a203ebc6bed3c08122b092ccd4e0"
-    # Patch from http://vcs.pcre.org/pcre2/code/trunk/src/pcre2_compile.c?view=patch&r1=489&r2=488&pathrev=489
-    # Fixes CVE-2016-3191
-    # Can be dropped once 10.22 is released
-    patch :p2, :DATA
+    url "https://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre2-10.22.tar.bz2"
+    mirror "https://www.mirrorservice.org/sites/downloads.sourceforge.net/p/pc/pcre/pcre2/10.21/pcre2-10.22.tar.bz2"
+    sha256 "b2b44619f4ac6c50ad74c2865fd56807571392496fae1c9ad7a70993d018f416"
   end
 
   bottle do
@@ -42,31 +38,3 @@ class Pcre2 < Formula
     system "#{bin}/pcre2grep", "regular expression", "#{prefix}/README"
   end
 end
-__END__
---- code/trunk/src/pcre2_compile.c	2016/02/06 16:40:59	488
-+++ code/trunk/src/pcre2_compile.c	2016/02/10 18:24:02	489
-@@ -5901,10 +5901,22 @@
-               goto FAILED;
-               }
-             cb->had_accept = TRUE;
-+
-+            /* In the first pass, just accumulate the length required;
-+            otherwise hitting (*ACCEPT) inside many nested parentheses can
-+            cause workspace overflow. */
-+
-             for (oc = cb->open_caps; oc != NULL; oc = oc->next)
-               {
--              *code++ = OP_CLOSE;
--              PUT2INC(code, 0, oc->number);
-+              if (lengthptr != NULL)
-+                {
-+                *lengthptr += CU2BYTES(1) + IMM2_SIZE;
-+                }
-+              else
-+                {
-+                *code++ = OP_CLOSE;
-+                PUT2INC(code, 0, oc->number);
-+                }
-               }
-             setverb = *code++ =
-               (cb->assert_depth > 0)? OP_ASSERT_ACCEPT : OP_ACCEPT;
