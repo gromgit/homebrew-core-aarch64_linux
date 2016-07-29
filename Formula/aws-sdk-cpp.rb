ae04b12c053a6fef487a9aee89b8bf030ac99cdc
ilovezfs <ilovezfs@icloud.com>
2016-07-29 02:19:01 -0700

    aws-sdk-cpp 0.13.10
    Closes #3419.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/aws-sdk-cpp.rb
+++ Formula/aws-sdk-cpp.rb
@@ -1,8 +1,8 @@
 class AwsSdkCpp < Formula
   desc "AWS SDK for C++"
   homepage "https://github.com/aws/aws-sdk-cpp"
-  url "https://github.com/aws/aws-sdk-cpp/archive/0.13.9.tar.gz"
-  sha256 "9fdef6506ddf6d013ed978e626f694c6aee329d6874906bb20089666fdb1511a"
+  url "https://github.com/aws/aws-sdk-cpp/archive/0.13.10.tar.gz"
+  sha256 "4de6f67585afccb6c57bd0948766870375aed7b41d5541169f274a741b12540e"
   head "https://github.com/aws/aws-sdk-cpp.git"
 
   bottle do
