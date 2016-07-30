ececc21f2c39defa5d1b868563a16859fd040109
Weihang Lo <b01605002@gmail.com>
2016-07-30 23:29:39 +0800

    carthage: add bash/zsh/fish completions
    Closes #3459.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/carthage.rb
+++ Formula/carthage.rb
@@ -15,6 +15,9 @@ class Carthage < Formula
 
   def install
     system "make", "prefix_install", "PREFIX=#{prefix}"
+    bash_completion.install "Source/Scripts/carthage-bash-completion" => "carthage"
+    zsh_completion.install "Source/Scripts/carthage-zsh-completion" => "_carthage"
+    fish_completion.install "Source/Scripts/carthage-fish-completion" => "carthage.fish"
   end
 
   test do
