33301827c3d770bfd49f0e50d84e0b125b06b0b7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-30 04:07:22 +0100

    docker: update 1.12.0 bottle.

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -9,9 +9,9 @@ class Docker < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "85376c2591bb8e87981608764ee34d94b2341b6dd428fe75a415af0fd3be64d9" => :el_capitan
-    sha256 "44a84556a9d7ab7d631f847e3f761a09db9f0e1fffa9e7e9a341c27f0ead4be3" => :yosemite
-    sha256 "5d4657dfcebd6d2e1fe59fe6487f6fe54ed9f6971969ccc0b93088e70d3c9633" => :mavericks
+    sha256 "f67ee7204bb31e226cbdc3f8b3526f33eb2fc00eed03c27faeb5237ec9faf2b9" => :el_capitan
+    sha256 "c0aa18fc1c4726ea55b9e166bc61f490ecd520b5d0fcfd0a50cf540b3b5dd731" => :yosemite
+    sha256 "3dd5bb6508fea6356b9aa2ce96ceef9477a460c95ac72f9a759fc89ba1d82263" => :mavericks
   end
 
   option "with-experimental", "Enable experimental features"
