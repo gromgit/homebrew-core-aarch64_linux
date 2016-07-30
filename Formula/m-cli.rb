3538a380337be91e9c46771d8f8905a46b14ce81
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-30 20:40:43 +0100

    m-cli: add 0.0.9 bottle.

--- Formula/m-cli.rb
+++ Formula/m-cli.rb
@@ -4,6 +4,13 @@ class MCli < Formula
   url "https://github.com/rgcr/m-cli/archive/v0.0.9.tar.gz"
   sha256 "957e86c03a8196f60c852672aa0a7c86d9093347e37c16783e67e423fa56f341"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "980e5335b55609427d6100ed2a7d7cfe815fafc4922e2a302f3104524ff23fe1" => :el_capitan
+    sha256 "e24d33e100fba2466180bee916c395a74773722b2abe59fef13cde4cfdc1f8ef" => :yosemite
+    sha256 "2cc6472a01a3bf7a73ef4608c7005b1937e23cc51b0762fc49f5c237316acffb" => :mavericks
+  end
+
   def install
     prefix.install Dir["*"]
     # Use absolute rather than relative path to plugins.
