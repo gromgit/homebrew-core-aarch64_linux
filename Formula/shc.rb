b7f7f60914d29e08624e0b3b093274dcae91a7d3
ilovezfs <ilovezfs@icloud.com>
2016-07-30 10:11:26 -0700

    shc 3.9.3
    Closes #3460.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/shc.rb
+++ Formula/shc.rb
@@ -1,10 +1,8 @@
 class Shc < Formula
   desc "Shell Script Compiler"
   homepage "https://neurobin.github.io/shc"
-  url "https://github.com/neurobin/shc/archive/3.9.3a.tar.gz"
-  version "3.9.3a"
-  sha256 "76b3693cbf9db027e13c9f72d789d8197614ee872e421609a708ddb915bbc9d8"
-
+  url "https://github.com/neurobin/shc/archive/3.9.3.tar.gz"
+  sha256 "b7120f66177a35af7dc42763a55e7ade3a80043c0188739e57bcc648a5ac4bb3"
   head "https://github.com/neurobin/shc.git"
 
   bottle do
@@ -15,20 +13,18 @@ class Shc < Formula
   end
 
   def install
-    # Fix install scripts' permissions.
-    # submitted upstream: https://github.com/neurobin/shc/pull/8
-    chmod 0755, ["configure", "config/install-sh"]
-
     system "./configure"
     system "make", "install", "prefix=#{prefix}"
+    pkgshare.install "test"
   end
 
   test do
     (testpath/"test.sh").write <<-EOS.undent
       #!/bin/sh
+      echo hello
       exit 0
     EOS
-    system "#{bin}/shc", "-f", "test.sh", "-o", "test"
-    system "./test"
+    system bin/"shc", "-f", "test.sh", "-o", "test"
+    assert_equal "hello", shell_output("./test").chomp
   end
 end
