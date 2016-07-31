9f8f51db45df396d8b30ed37fb2c0409bc0a80cc
Dominyk Tiller <dominyktiller@gmail.com>
2016-07-31 02:25:49 +0100

    mytop: revision for CVE-2015-8949
    Force a rebuild to fix issue with bundled DBD::mysql module.

--- Formula/mytop.rb
+++ Formula/mytop.rb
@@ -1,12 +1,11 @@
 class Mytop < Formula
   desc "Top-like query monitor for MySQL"
   homepage "http://www.mysqlfanboy.com/mytop-3/"
-  # URL & homepage down since at least ~November 2015.
-  # url "http://www.mysqlfanboy.com/mytop-3/mytop-1.9.1.tar.gz"
-  url "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/m/mytop/mytop_1.9.1.orig.tar.gz"
+  url "http://www.mysqlfanboy.com/mytop-3/mytop-1.9.1.tar.gz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/m/mytop/mytop_1.9.1.orig.tar.gz"
   mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/mytop/mytop_1.9.1.orig.tar.gz"
   sha256 "179d79459d0013ab9cea2040a41c49a79822162d6e64a7a85f84cdc44828145e"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
@@ -20,14 +19,11 @@ class Mytop < Formula
   depends_on "openssl"
 
   conflicts_with "mariadb", :because => "both install `mytop` binaries"
-  conflicts_with "innotop", :because => "both install `perllocal.pod`"
-  conflicts_with "percona-toolkit", :because => "both install `perllocal.pod`"
-  conflicts_with "pgtap", :because => "both install `perllocal.pod`"
 
   resource "DBD::mysql" do
-    url "https://cpan.metacpan.org/authors/id/C/CA/CAPTTOFU/DBD-mysql-4.033.tar.gz"
-    mirror "http://search.cpan.org/CPAN/authors/id/C/CA/CAPTTOFU/DBD-mysql-4.033.tar.gz"
-    sha256 "cc98bbcc33581fbc55b42ae681c6946b70a26f549b3c64466740dfe9a7eac91c"
+    url "https://cpan.metacpan.org/authors/id/M/MI/MICHIELB/DBD-mysql-4.035.tar.gz"
+    mirror "http://search.cpan.org/CPAN/authors/id/M/MI/MICHIELB/DBD-mysql-4.035.tar.gz"
+    sha256 "b7eca365ea16bcf4c96c2fc0221304ff9c4995e7a551886837804a8f66b61937"
   end
 
   def install
