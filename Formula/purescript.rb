263894406179a26170774c74d69946b62e8dce89
ilovezfs <ilovezfs@icloud.com>
2016-08-01 03:41:06 -0700

    purescript 0.9.3
    Closes #3497.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/purescript.rb
+++ Formula/purescript.rb
@@ -5,20 +5,9 @@ class Purescript < Formula
 
   desc "Strongly typed programming language that compiles to JavaScript"
   homepage "http://www.purescript.org"
+  url "https://github.com/purescript/purescript/archive/v0.9.3.tar.gz"
+  sha256 "70ea9ea189300e8ce7ea89eec818a1c9e39be1e758638898b936028fc0155783"
   head "https://github.com/purescript/purescript.git"
-  revision 1
-
-  stable do
-    url "https://github.com/purescript/purescript/archive/v0.9.2.tar.gz"
-    sha256 "f02e5b39764346aa83103ef40cfd90e5aeea6958793ec64ab0eac37293b8df2f"
-
-    # "ambiguous occurrence" errors for fromStrict, decodeUtf8, and encodeUtf8
-    # upstream commit "protolude 0.1.6: fix ambiguous occurrences"
-    patch do
-      url "https://github.com/purescript/purescript/commit/15c466f1.patch"
-      sha256 "dbba6a25b0aea7c67423e58c1c44b87cd43884b1948c8ae1b9f0f4d28b7ddbb5"
-    end
-  end
 
   bottle do
     sha256 "60b40af395e3615c149387c3458e56938515cf25550e8f538393481b0971b6a2" => :el_capitan
