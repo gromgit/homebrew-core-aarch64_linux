8cbb050f55170ced9051bd0bd9c7da2c4bd2aeb8
Nathan Goldbaum <ngoldbau@illinois.edu>
2016-08-01 14:29:14 -0500

    mercurial 3.9
    Closes #3507.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/mercurial.rb
+++ Formula/mercurial.rb
@@ -3,8 +3,8 @@
 class Mercurial < Formula
   desc "Scalable distributed version control system"
   homepage "https://mercurial-scm.org/"
-  url "https://mercurial-scm.org/release/mercurial-3.8.4.tar.gz"
-  sha256 "4b2e3ef19d34fa1d781cb7425506a05d4b6b1172bab69d6ea78874175fdf3da6"
+  url "https://mercurial-scm.org/release/mercurial-3.9.tar.gz"
+  sha256 "834f25dcff44994198fb8a7ba161a6e24204dbd63c8e6270577e06e6cedbdabc"
 
   bottle do
     cellar :any_skip_relocation
