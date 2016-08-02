7eca5e7c3dd731d3c1aad3dc5575bb507fef726e
ilovezfs <ilovezfs@icloud.com>
2016-08-02 07:26:59 -0700

    iso-codes 3.69
    Closes #3536.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/iso-codes.rb
+++ Formula/iso-codes.rb
@@ -1,9 +1,8 @@
 class IsoCodes < Formula
   desc "Provides lists of various ISO standards"
   homepage "https://pkg-isocodes.alioth.debian.org/"
-  url "https://pkg-isocodes.alioth.debian.org/downloads/iso-codes-3.68.tar.xz"
-  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/i/iso-codes/iso-codes_3.68.orig.tar.xz"
-  sha256 "5881cf7caa5adfffb14ade99138949324c28a277babe8d07dafbff521acef9d1"
+  url "https://pkg-isocodes.alioth.debian.org/downloads/iso-codes-3.69.tar.xz"
+  sha256 "3f285d3c13f4dccfbdb9e432f172403ac1a54ab432616f10556eb18c23a1c0b2"
   head "https://anonscm.debian.org/git/pkg-isocodes/iso-codes.git"
 
   bottle do
