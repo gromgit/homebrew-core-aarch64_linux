a79d6b60b2cddb91202502b242e26d8a6b704595
ilovezfs <ilovezfs@icloud.com>
2016-08-02 07:26:17 -0700

    gssh 2.4.2
    Closes #3535.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gssh.rb
+++ Formula/gssh.rb
@@ -1,8 +1,8 @@
 class Gssh < Formula
   desc "SSH automation tool based on Groovy DSL"
   homepage "https://github.com/int128/groovy-ssh"
-  url "https://github.com/int128/groovy-ssh/releases/download/2.4.1/gssh.jar"
-  sha256 "7032029e1be28632b9dd59b9d837ccd403e729bafe793ddeff137625e7b09cba"
+  url "https://github.com/int128/groovy-ssh/releases/download/2.4.2/gssh.jar"
+  sha256 "20572612a012a12beb89edc37dc35b773dd387ef14e2cca5ca90ee709ad0e054"
 
   head "https://github.com/int128/groovy-ssh.git"
 
