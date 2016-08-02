44dea2fc6077227528851370941fc583835e4e26
Tim D. Smith <git@tim-smith.us>
2016-08-02 13:48:53 -0700

    mercurial: install against Homebrew python
    Hg uses the python ssl module, which is linked to OpenSSL 0.9.8 on OS X ≤ 10.11, which lacks TLS 1.1 support.
    
    Closes #3548. Closes #3541.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Formula/mercurial.rb
+++ Formula/mercurial.rb
@@ -5,12 +5,20 @@ class Mercurial < Formula
   homepage "https://mercurial-scm.org/"
   url "https://mercurial-scm.org/release/mercurial-3.9.tar.gz"
   sha256 "834f25dcff44994198fb8a7ba161a6e24204dbd63c8e6270577e06e6cedbdabc"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
     sha256 "db15d3613d2fd98424501aa3447b9f4c29c59fe08f8468ae072bec6bbabf3116" => :el_capitan
     sha256 "550f5dd794f7933892ff2e8da1891662aa3ef87d44e56a3389941c766fc16ea3" => :yosemite
     sha256 "8934a8fc26c36e56f017d99ac43a0ebe9d6922f8276c1dfccd9ccb980ae05e45" => :mavericks
+  end
+
+  option "with-custom-python", "Install against the python in PATH instead of Homebrew's python"
+  if build.with? "custom-python"
+    depends_on :python
+  else
+    depends_on "python"
   end
 
   def install
@@ -24,6 +32,20 @@ class Mercurial < Formula
     # install the completion scripts
     bash_completion.install "contrib/bash_completion" => "hg-completion.bash"
     zsh_completion.install "contrib/zsh_completion" => "_hg"
+  end
+
+  def caveats
+    return unless (opt_bin/"hg").exist?
+    cacerts_configured = `#{opt_bin}/hg config web.cacerts`.strip
+    return if cacerts_configured.empty?
+    <<-EOS.undent
+      Homebrew has detected that Mercurial is configured to use a certificate
+      bundle file as its trust store for TLS connections instead of using the
+      default OpenSSL store. If you have trouble connecting to remote
+      repositories, consider unsetting the `web.cacerts` property. You can
+      determine where the property is being set by running:
+        hg config --debug web.cacerts
+    EOS
   end
 
   test do
