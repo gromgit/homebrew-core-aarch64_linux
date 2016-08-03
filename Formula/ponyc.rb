c91fecf77a056011802d81582952d2e9623838d4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-03 04:45:11 +0100

    ponyc: update 0.2.2-alpha2_1 bottle.

--- Formula/ponyc.rb
+++ Formula/ponyc.rb
@@ -9,9 +9,9 @@ class Ponyc < Formula
 
   bottle do
     cellar :any
-    sha256 "0334cba03d7c3bd17b8c91429abbde308b821868287d747c34cf985477f4f1e4" => :el_capitan
-    sha256 "2dae1d035c593d1483420b8186bf767b0f3e11e9956c3cd55145f2dfceea262e" => :yosemite
-    sha256 "321006063e24e196c834cabc358b8872773cab743962ad0b75ecd559a6892148" => :mavericks
+    sha256 "59a03fc7a83f5c97262e9a70f383b3105f6d03062aac9ceac4729a6e3a19e7d3" => :el_capitan
+    sha256 "c3527fecac254cd94a2f6d42f9418fbd80ca9bf41f46a4d807141516e9f849f9" => :yosemite
+    sha256 "38b605078fd9cbc5010995b41327c9161d4b309b0c1696890abf572543f36a63" => :mavericks
   end
 
   depends_on "llvm"
