96297f8aeb1ae88b2a4bb483d667e90592dc1252
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-03 21:40:27 +0100

    git-remote-hg: add 0.3 bottle.

--- Formula/git-remote-hg.rb
+++ Formula/git-remote-hg.rb
@@ -4,6 +4,13 @@ class GitRemoteHg < Formula
   url "https://github.com/felipec/git-remote-hg/archive/v0.3.tar.gz"
   sha256 "2dc889b641d72f5a73c4c7d5df3b8ea788e75a7ce80f5884a7a8d2e099287dce"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0e85f91992caf6662783a2e53ffd4fbbb04c0600b702bb27f8ebe1a8b51bb683" => :el_capitan
+    sha256 "ffd2c6e7361d41e20209cf738b97292fda480ef78ab5264967be08ecb8103b7a" => :yosemite
+    sha256 "808c74fd0e1d5d5deb32e768f86393695075fa82359fd2b186604296606846da" => :mavericks
+  end
+
   depends_on :hg
   depends_on :python if MacOS.version <= :snow_leopard
 
