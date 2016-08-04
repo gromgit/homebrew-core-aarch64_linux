4d304ab9c3cb8d2d98244b4a1b5f5dc5c7d5bf2e
John Colvin <john.loughran.colvin@gmail.com>
2016-08-05 00:29:40 +0100

    dub 1.0.0
    Closes #3622.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/dub.rb
+++ Formula/dub.rb
@@ -1,7 +1,8 @@
 class Dub < Formula
   desc "Build tool for D projects"
   homepage "https://code.dlang.org/about"
-  url "https://github.com/dlang/dub.git", :tag => "v0.9.25", :revision => "777e0033e9ca502c134a6cca6b5a06d0e3f78617"
+  url "https://github.com/dlang/dub.git", :tag => "v1.0.0", :revision => "b59af2b8befb4fad4157d8c9cc86dba707b2fc87"
+
   head "https://github.com/dlang/dub.git", :shallow => false
 
   bottle do
@@ -10,17 +11,10 @@ class Dub < Formula
     sha256 "1c05ee4b3f609f81cd1507e22706befa2b78b945b8e2a7560d86e0a13746e845" => :mavericks
   end
 
-  devel do
-    url "https://github.com/dlang/dub/archive/v1.0.0-beta.1.tar.gz"
-    sha256 "47191b7299562e0f25bdad28ad8be1d4fe09e6f7c40f50acc78455e3dc28da0c"
-    version "1.0.0-beta.1"
-  end
-
   depends_on "pkg-config" => :build
   depends_on "dmd" => :build
 
   def install
-    ENV["GITVER"] = "1.0.0-beta.1" if build.devel?
     system "./build.sh"
     bin.install "bin/dub"
   end
