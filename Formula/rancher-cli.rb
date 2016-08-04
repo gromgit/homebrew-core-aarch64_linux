4b499a667f0aab5ea06cd055e5dd3c32ea946549
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-04 15:10:57 +0100

    rancher-cli: add 0.0.1 bottle.

--- Formula/rancher-cli.rb
+++ Formula/rancher-cli.rb
@@ -4,6 +4,13 @@ class RancherCli < Formula
   url "https://github.com/rancher/cli/archive/v0.0.1.tar.gz"
   sha256 "3789da0f54d984c37b7805ec4066a336a25ffbaa46800c7f5023973ddf8444cf"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bc054ef653fd409a27f7439b551f630c2fcc7aca92f3365bfad9df14d12003f8" => :el_capitan
+    sha256 "8b648da416cdecab158c82cccd2df4598444356faad7f86feabbec51f78fbf35" => :yosemite
+    sha256 "eb2e2bfebd5211e72432e4bb977ed0ab6fef50e9ff13cf157a92fba36bcdc17e" => :mavericks
+  end
+
   depends_on "go" => :build
 
   def install
