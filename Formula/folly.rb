c05d97e23b564a2625b3f9fcc88f1b11fc4be9f7
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-04 01:52:27 +0100

    folly: restore from boneyard.
    Ref: https://github.com/Homebrew/homebrew-core/pull/3321

---
+++ Formula/folly.rb
@@ -0,0 +1,64 @@
+class Folly < Formula
+  desc "Collection of reusable C++ library artifacts developed at Facebook"
+  homepage "https://github.com/facebook/folly"
+  url "https://github.com/facebook/folly/archive/v2016.08.01.00.tar.gz"
+  sha256 "e6aeda41f06c39b5be52011c6b8aa25739a5a5559bcb8b3c44d79db4f2a4204d"
+  head "https://github.com/facebook/folly.git"
+
+  depends_on :macos => :mavericks
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "double-conversion"
+  depends_on "glog"
+  depends_on "gflags"
+  depends_on "boost"
+  depends_on "libevent"
+  depends_on "xz"
+  depends_on "snappy"
+  depends_on "lz4"
+  depends_on "jemalloc"
+  depends_on "openssl"
+
+  needs :cxx11
+
+  # Known issue upstream. They're working on it:
+  # https://github.com/facebook/folly/pull/445
+  fails_with :gcc => "6"
+
+  def install
+    ENV.cxx11
+
+    cd "folly" do
+      # Build system relies on pkg-config but gflags removed
+      # the .pc files so now folly cannot find without flags.
+      ENV["GFLAGS_CFLAGS"] = Formula["gflags"].opt_include
+      ENV["GFLAGS_LIBS"] = Formula["gflags"].opt_lib
+
+      system "autoreconf", "-fvi"
+      system "./configure", "--prefix=#{prefix}", "--disable-silent-rules",
+                            "--disable-dependency-tracking"
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cc").write <<-EOS.undent
+      #include <folly/FBVector.h>
+      int main() {
+        folly::fbvector<int> numbers({0, 1, 2, 3});
+        numbers.reserve(10);
+        for (int i = 4; i < 10; i++) {
+          numbers.push_back(i * 2);
+        }
+        assert(numbers[6] == 12);
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "test.cc", "-I#{include}", "-L#{lib}",
+                    "-lfolly", "-o", "test"
+    system "./test"
+  end
+end
