3c4fb12c205d6562770f26400dc5f6af1e5d1ef3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-04 08:49:54 +0100

    fwup: update 0.8.1 bottle.

--- Formula/fwup.rb
+++ Formula/fwup.rb
@@ -6,9 +6,9 @@ class Fwup < Formula
 
   bottle do
     cellar :any
-    sha256 "590bfdf523902eec53518ca36a1913a32b42080b8533b588a1116d2e33197263" => :el_capitan
-    sha256 "aeb822c13562919113be2cf8b55214776a338021b6270b3f45505faf585c0fe1" => :yosemite
-    sha256 "5fe1615e422ccfe484aa81c8d7cbdd6a7cc59b86bb14d5965cac9c03a771a76c" => :mavericks
+    sha256 "5fff9c96643168ef512d0c07f27addc692e8d5dbc930d83b5b1ff7bd45a6097a" => :el_capitan
+    sha256 "8f1eaeed130c8efbe995ec76f45d3c9f2648e8ec4ff76de40fb8fec0d1eff576" => :yosemite
+    sha256 "fb01ce18d3fe1e62216dba4ffd141606a39cd7dcaf7829e2f17a80e2ec9386b6" => :mavericks
   end
 
   depends_on "pkg-config" => :build
