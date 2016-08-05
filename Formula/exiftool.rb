b2af868eca15064e682e07d34d1ee15cd0d852bb
Tommy Sparber <tsparber@users.noreply.github.com>
2016-08-05 16:10:23 +0200

    exiftool 10.25 (devel) (#3636)

--- Formula/exiftool.rb
+++ Formula/exiftool.rb
@@ -13,6 +13,11 @@ class Exiftool < Formula
     sha256 "627a623afdbd4fd5a9c4af02cb1228713f70ff68362733000a84890af89c4416" => :mavericks
   end
 
+  devel do
+    url "http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-10.25.tar.gz"
+    sha256 "edc2de5848375f7ccb88cd7d0260c98c4c581ffd509c4c249949f0cd1f522dd0"
+  end
+
   def install
     # replace the hard-coded path to the lib directory
     inreplace "exiftool", "$exeDir/lib", libexec/"lib"
