74092527624c9d1d56ff2c193123d6e54930435a
ilovezfs <ilovezfs@icloud.com>
2016-08-05 22:01:14 -0700

    apache-flink 1.1.0
    Closes #3661.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/apache-flink.rb
+++ Formula/apache-flink.rb
@@ -1,9 +1,9 @@
 class ApacheFlink < Formula
   desc "Scalable batch and stream data processing"
   homepage "https://flink.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.0.3/flink-1.0.3-bin-hadoop26-scala_2.10.tgz"
-  version "1.0.3"
-  sha256 "7ace5440681d81c19b4ff191998e35a60dc8b2ce2421a61084f2821befd3b4a2"
+  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.1.0/flink-1.1.0-bin-hadoop27-scala_2.11.tgz"
+  version "1.1.0"
+  sha256 "11eb4804dbbf024a189b1005348f13a608fb7419026eef102b4a174155cf4170"
   head "https://github.com/apache/flink.git"
 
   bottle :unneeded
@@ -16,6 +16,8 @@ class ApacheFlink < Formula
 
   test do
     ENV["_JAVA_OPTIONS"] = "-Djava.io.tmpdir=#{testpath} -Duser.home=#{testpath}"
-    assert_match /FINISHED/, pipe_output("#{libexec}/bin/start-scala-shell.sh local", "env.fromElements(1,2,3).print()\n", 1)
+    input = "benv.fromElements(1,2,3).print()\n"
+    output = pipe_output("#{libexec}/bin/start-scala-shell.sh local", input, 1)
+    assert_match "FINISHED", output
   end
 end
