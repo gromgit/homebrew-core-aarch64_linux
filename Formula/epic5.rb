98e6cdae2b223848e73b69733a15a1eeddfd935f
ilovezfs <ilovezfs@icloud.com>
2016-08-06 00:24:37 -0700

    epic5 2.0.1
    run "make test"
    
    Closes #3662.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/epic5.rb
+++ Formula/epic5.rb
@@ -1,9 +1,8 @@
 class Epic5 < Formula
   desc "Enhanced, programmable IRC client"
   homepage "http://www.epicsol.org/"
-  url "http://ftp.epicsol.org/pub/epic/EPIC5-PRODUCTION/epic5-1.4.tar.xz"
-  sha256 "3bc04e07768040db266667513158d2c640544abb49cbab29343f6ef05ebd621e"
-
+  url "http://ftp.epicsol.org/pub/epic/EPIC5-PRODUCTION/epic5-2.0.1.tar.xz"
+  sha256 "55260fc832c76f7a4975bde2bd0d0805fd8012fc8908ac94ec8c6de24a1be7aa"
   head "http://git.epicsol.org/epic5.git"
 
   bottle do
@@ -21,6 +20,8 @@ class Epic5 < Formula
                           "--mandir=#{man}",
                           "--with-ipv6",
                           "--with-ssl=#{Formula["openssl"].opt_prefix}"
+    system "make"
+    system "make", "test"
     system "make", "install"
   end
 
