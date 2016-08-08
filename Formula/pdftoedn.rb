47234458f8e8a1605fb2f4da23487cd1a35fbfe7
Ed Porras <ep@digressed.net>
2016-08-09 00:56:52 +0200

    pdftoedn 0.32.1 (new formula)
    Closes #3718.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Formula/pdftoedn.rb
@@ -0,0 +1,29 @@
+class Pdftoedn < Formula
+  desc "Extract PDF document data and save the output in EDN format"
+  homepage "https://github.com/edporras/pdftoedn"
+  url "https://github.com/edporras/pdftoedn/archive/v0.32.1.tar.gz"
+  sha256 "7b56e404710c0d835e8b693660a6e4e98e2d37ab7329a4f67010a10c34235e85"
+
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
+  depends_on "freetype"
+  depends_on "libpng"
+  depends_on "poppler"
+  depends_on "boost"
+  depends_on "leptonica"
+  depends_on "openssl"
+  depends_on "rapidjson"
+
+  def install
+    ENV.cxx11
+
+    system "autoreconf", "-i"
+    system "./configure", "--with-openssl=#{Formula["openssl"].opt_prefix}", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/pdftoedn", "-o", "test.edn", test_fixtures("test.pdf")
+  end
+end
