1244b2b02dc42065c766091bd31029492553b2d9
Krisztián Gödrei <godrekrisztian@gmil.com>
2016-08-09 14:09:30 +0200

    bitrise 1.3.7

--- Formula/bitrise.rb
+++ Formula/bitrise.rb
@@ -1,8 +1,8 @@
 class Bitrise < Formula
   desc "Command-line automation tool"
   homepage "https://github.com/bitrise-io/bitrise"
-  url "https://github.com/bitrise-io/bitrise/archive/1.3.6.tar.gz"
-  sha256 "b52d51fa0a8989ea5ecd3049868fefe3c77c6f845f063a8cd9863b8989275964"
+  url "https://github.com/bitrise-io/bitrise/archive/1.3.7.tar.gz"
+  sha256 "5695c81f795ec46ee1dfa7fd8a895a1bb8dfe087748a9da7b4b2c8d877323a45"
 
   bottle do
     cellar :any_skip_relocation
@@ -12,17 +12,6 @@ class Bitrise < Formula
   end
 
   depends_on "go" => :build
-  depends_on "godep" => :build
-
-  resource "envman" do
-    url "https://github.com/bitrise-io/envman/archive/1.1.0.tar.gz"
-    sha256 "5e8d92dc8d38050e9e4c037e6d02bf8ad454c74a9a55c1eaeec9df8c691c2a51"
-  end
-
-  resource "stepman" do
-    url "https://github.com/bitrise-io/stepman/archive/0.9.21.tar.gz"
-    sha256 "a9448ad6d139685287ebdf7f89707b5ec1d1e4206d981360050dd5675a73e987"
-  end
 
   def install
     ENV["GOPATH"] = buildpath
@@ -33,26 +22,6 @@ class Bitrise < Formula
 
     cd bitrise_go_path do
       system "go", "build", "-o", bin/"bitrise"
-    end
-
-    # Install envman
-    resource("envman").stage do
-      envman_go_pth = buildpath/"src/github.com/bitrise-io/envman"
-      envman_go_pth.install Dir["*"]
-
-      cd envman_go_pth do
-        system "godep", "go", "build", "-o", bin/"envman"
-      end
-    end
-
-    # Install stepman
-    resource("stepman").stage do
-      stepman_go_pth = buildpath/"src/github.com/bitrise-io/stepman"
-      stepman_go_pth.install Dir["*"]
-
-      cd stepman_go_pth do
-        system "godep", "go", "build", "-o", bin/"stepman"
-      end
     end
   end
 
@@ -68,8 +37,8 @@ class Bitrise < Formula
               - content: printf 'Test - OK' > brew.test.file
     EOS
 
-    # setup with --minimal flag, to skip the included `brew doctor` check
-    system "#{bin}/bitrise", "setup", "--minimal"
+    # setup
+    system "#{bin}/bitrise", "setup"
     # run the defined test_wf workflow
     system "#{bin}/bitrise", "run", "test_wf"
     assert_equal "Test - OK", (testpath/"brew.test.file").read.chomp
