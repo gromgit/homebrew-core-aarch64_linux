b19d7e5abaffb70dee05efe04db7584a081807a3
ilovezfs <ilovezfs@icloud.com>
2016-08-10 16:21:18 -0700

    launch4j 3.9
    Closes #3778.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/launch4j.rb
+++ Formula/launch4j.rb
@@ -1,9 +1,9 @@
 class Launch4j < Formula
   desc "Cross-platform Java executable wrapper"
   homepage "http://launch4j.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/launch4j/launch4j-3/3.5/launch4j-3.5-macosx-x86-10.8.tgz"
-  version "3.5"
-  sha256 "d994287342c0bbeaf3b967ccbf9bfb18d1a5b7123bca529c79760e67d6fd1e5f"
+  url "https://downloads.sourceforge.net/project/launch4j/launch4j-3/3.9/launch4j-3.9-macosx-x86.tgz"
+  version "3.9"
+  sha256 "5de7594b877827be169d88111f6d184db334871c88c2314cd70cc33c3c7344e1"
 
   bottle :unneeded
 
