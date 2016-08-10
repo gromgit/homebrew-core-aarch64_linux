01b1f5a92f5da05871be152a735cc8459f481956
Michael Fogleman <fogleman@gmail.com>
2016-08-10 16:36:35 -0400

    ispc 1.9.1
    Update ispc formula to v1.9.1
    
    Closes #3775.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/ispc.rb
+++ Formula/ispc.rb
@@ -1,8 +1,8 @@
 class Ispc < Formula
   desc "Compiler for SIMD programming on the CPU"
   homepage "https://ispc.github.io"
-  url "https://downloads.sourceforge.net/project/ispcmirror/v1.8.2/ispc-v1.8.2-osx.tar.gz"
-  sha256 "9e42d7cca73e598d26727e4e5ab2f8b8e24d009fcb64424e566fa0f45329c2ec"
+  url "https://downloads.sourceforge.net/project/ispcmirror/v1.9.1/ispc-v1.9.1-osx.tar.gz"
+  sha256 "ee949fcdf37ad3e3c0b946e6d6e32d7a7eafcec43e2cb908aea7e58e78bb0f55"
 
   bottle :unneeded
 
