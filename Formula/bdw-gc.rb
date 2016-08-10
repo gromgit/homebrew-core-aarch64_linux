5e422ce820f859472d4bbad9a36eb5bbc53ff1b5
ilovezfs <ilovezfs@icloud.com>
2016-08-10 01:31:06 -0700

    bdw-gc 7.6.0
    Closes #3759.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/bdw-gc.rb
+++ Formula/bdw-gc.rb
@@ -1,8 +1,8 @@
 class BdwGc < Formula
   desc "Garbage collector for C and C++"
   homepage "http://www.hboehm.info/gc/"
-  url "http://www.hboehm.info/gc/gc_source/gc-7.4.2.tar.gz"
-  sha256 "63320ad7c45460e4a40e03f5aa4c6893783f21a16416c3282b994f933312afa2"
+  url "http://www.hboehm.info/gc/gc_source/gc-7.6.0.tar.gz"
+  sha256 "a14a28b1129be90e55cd6f71127ffc5594e1091d5d54131528c24cd0c03b7d90"
 
   bottle do
     revision 2
