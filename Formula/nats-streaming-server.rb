886f894d6e63d5a850afc001450e1f6b788b1786
Colin Sullivan <colin.sullivan@apcera.com>
2016-08-10 14:02:02 -0600

    nats-streaming-server 0.2.2 (new formula)
    Closes #3773.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ Formula/nats-streaming-server.rb
@@ -0,0 +1,53 @@
+class NatsStreamingServer < Formula
+  desc "Lightweight cloud messaging system"
+  homepage "https://nats.io"
+  url "https://github.com/nats-io/nats-streaming-server/archive/v0.2.2.tar.gz"
+  sha256 "741d03db1f78c348856476b0b27f39c391a6695aa6c199997a517e6f8c0e58f1"
+  head "https://github.com/nats-io/nats-streaming-server.git"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    mkdir_p "src/github.com/nats-io"
+    ln_s buildpath, "src/github.com/nats-io/nats-streaming-server"
+    buildfile = buildpath/"src/github.com/nats-io/nats-streaming-server/nats-streaming-server.go"
+    system "go", "build", "-v", "-o", bin/"nats-streaming-server", buildfile
+  end
+
+  plist_options :manual => "nats-streaming-server"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/nats-streaming-server</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+      </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    pid = fork do
+      exec "#{bin}/nats-streaming-server --port=8085 --pid=#{testpath}/pid --log=#{testpath}/log"
+    end
+    sleep 3
+
+    begin
+      assert_match "INFO", shell_output("curl localhost:8085")
+      assert File.exist?(testpath/"log")
+      assert_match version.to_s, File.read(testpath/"log")
+    ensure
+      Process.kill "SIGINT", pid
+      Process.wait pid
+    end
+  end
+end
