d76a4bc5ea1f5fad5a2ea73340b7a267ef52b95a
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-10 03:20:45 +0100

    blackbox: transition to gnupg2

--- Formula/blackbox.rb
+++ Formula/blackbox.rb
@@ -3,10 +3,11 @@ class Blackbox < Formula
   homepage "https://github.com/StackExchange/blackbox"
   url "https://github.com/StackExchange/blackbox/archive/v1.20160122.tar.gz"
   sha256 "ac5de1d74fdbe88604b34949f3949e53cb72e55e148e46b8c2be98806c888a10"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :gpg
+  depends_on :gpg => :run
 
   def install
     libexec.install Dir["bin/*"]
@@ -14,7 +15,10 @@ class Blackbox < Formula
   end
 
   test do
+    Gpg.create_test_key(testpath)
     system "git", "init"
-    system "#{bin}/blackbox_initialize", "yes"
+    system bin/"blackbox_initialize", "yes"
+    add_created_key = shell_output("#{bin}/blackbox_addadmin Testing 2>&1")
+    assert_match "<testing@foo.bar>", add_created_key
   end
 end
