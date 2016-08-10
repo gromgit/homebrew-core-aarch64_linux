f6c1d0a49cba83a454848f61d433506105407d39
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-10 04:08:16 +0100

    pass: transition to gnupg2

--- Formula/pass.rb
+++ Formula/pass.rb
@@ -3,6 +3,8 @@ class Pass < Formula
   homepage "https://www.passwordstore.org/"
   url "https://git.zx2c4.com/password-store/snapshot/password-store-1.6.5.tar.xz"
   sha256 "337a39767e6a8e69b2bcc549f27ff3915efacea57e5334c6068fcb72331d7315"
+  revision 1
+
   head "https://git.zx2c4.com/password-store", :using => :git
 
   bottle do
@@ -16,17 +18,20 @@ class Pass < Formula
   depends_on "pwgen"
   depends_on "tree"
   depends_on "gnu-getopt"
-  depends_on :gpg
+  depends_on :gpg => :run
 
   def install
     system "make", "PREFIX=#{prefix}", "install"
-    share.install "contrib"
+    pkgshare.install "contrib"
     zsh_completion.install "src/completion/pass.zsh-completion" => "_pass"
     bash_completion.install "src/completion/pass.bash-completion" => "password-store"
     fish_completion.install "src/completion/pass.fish-completion" => "pass.fish"
   end
 
   test do
-    system "#{bin}/pass", "--version"
+    Gpg.create_test_key(testpath)
+    system bin/"pass", "init", "Testing"
+    system bin/"pass", "generate", "Email/testing@foo.bar", "15"
+    assert File.exist?(".password-store/Email/testing@foo.bar.gpg")
   end
 end
