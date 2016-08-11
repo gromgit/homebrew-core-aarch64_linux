623b25528d3759dd65aa2226d6e3406b04b95cde
Steve Mao <maochenyan@gmail.com>
2016-08-11 20:42:54 +1000

    diff-so-fancy 0.11.1
    Closes #3663.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/diff-so-fancy.rb
+++ Formula/diff-so-fancy.rb
@@ -1,17 +1,12 @@
 class DiffSoFancy < Formula
   desc "Good-lookin' diffs with diff-highlight and more"
   homepage "https://github.com/so-fancy/diff-so-fancy"
-  url "https://github.com/so-fancy/diff-so-fancy/archive/v0.11.0.tar.gz"
-  sha256 "659a25b5ca58c8d07de4023e0f40a50ee2d096e7109e00bd9cba36b6b701e383"
+  url "https://github.com/so-fancy/diff-so-fancy/archive/v0.11.1.tar.gz"
+  sha256 "6a9212d3d21af50987604c5930fd9ae899bf59bbcc873185525c81e860a455de"
 
   bottle :unneeded
 
   def install
-    # temporary fix until upstream uses a directory other
-    # than lib for the perl script.
-    inreplace "diff-so-fancy", "/lib/", "/libexec/"
-    prefix.install "lib" => "libexec"
-
     prefix.install Dir["third_party", "diff-so-fancy"]
     bin.install_symlink prefix/"diff-so-fancy"
   end
