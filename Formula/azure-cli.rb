a310a4f75b5bbcb6bd97a4d7a2e5b4e2b0aba4a7
Xy Ziemba <xy@buddy.com>
2016-08-10 17:40:47 -0700

    azure-cli 0.10.3
    Closes #3780.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/azure-cli.rb
+++ Formula/azure-cli.rb
@@ -3,9 +3,9 @@ require "language/node"
 class AzureCli < Formula
   desc "Official Azure CLI"
   homepage "https://github.com/azure/azure-xplat-cli"
-  url "https://github.com/Azure/azure-xplat-cli/archive/v0.10.2-July2016.tar.gz"
-  version "0.10.2"
-  sha256 "490290c628ca2ad36a006f4c689511d0950613909e80f3eb595471f84fd17c8f"
+  url "https://github.com/Azure/azure-xplat-cli/archive/v0.10.3-August2016.tar.gz"
+  version "0.10.3"
+  sha256 "90490475b04e516feae9a2d7793859b1d11bfbefbb0036436e166ea5696e4ec0"
 
   head "https://github.com/azure/azure-xplat-cli.git", :branch => "dev"
 
