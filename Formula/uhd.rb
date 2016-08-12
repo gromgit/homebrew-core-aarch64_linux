04b577f92621966ccb424e51b730f9c65aab88c5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-12 13:53:22 +0100

    uhd: update 003.010.000.000 bottle.

--- Formula/uhd.rb
+++ Formula/uhd.rb
@@ -6,9 +6,9 @@ class Uhd < Formula
   head "https://github.com/EttusResearch/uhd.git"
 
   bottle do
-    sha256 "e6091ab702424957d1209d1b036714bfe021ad5fc5229213da4d02a573b7af4e" => :el_capitan
-    sha256 "6bfebd9fc9da8558e0a617f55dbd7c74e8f75c7fbe995c7e1a77dadf16d3cd40" => :yosemite
-    sha256 "8fd41c74a36e3c831da569e04941c961e7c6a8d83a30bd394301315645519a83" => :mavericks
+    sha256 "73fd0a522d1e20635dcae8beaa565ff07f441ae1cb4ed7af9a1311ad224ece7c" => :el_capitan
+    sha256 "2a0a5b94a8f3c70cecfa7952855431eba304ed9509beeba5d9c892465d69d990" => :yosemite
+    sha256 "144dac9e3208b80681b6c40dc981bf28cc5938ebd4e52471f608b4a0fc1eb43b" => :mavericks
   end
 
   option :universal
