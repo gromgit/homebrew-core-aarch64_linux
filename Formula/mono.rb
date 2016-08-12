9eb7d5888d7b04b759e7daf14e953e1e8bf538e6
ilovezfs <ilovezfs@icloud.com>
2016-08-12 00:06:27 -0700

    mono: remove conflicts_with disco
    due to disco migration to boneyard
    
    Closes #3793.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mono.rb
+++ Formula/mono.rb
@@ -34,7 +34,6 @@ class Mono < Formula
   link_overwrite "lib/mono"
   link_overwrite "lib/cli"
 
-  conflicts_with "disco", :because => "both install `disco` binaries"
   conflicts_with "xsd", :because => "both install `xsd` binaries"
 
   def install
