4bdee4dc653ea03f295ce7f6a248cfdc4779a4df
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-12 21:51:37 +0100

    nats-streaming-server: add 0.2.2 bottle.

--- Formula/nats-streaming-server.rb
+++ Formula/nats-streaming-server.rb
@@ -5,6 +5,13 @@ class NatsStreamingServer < Formula
   sha256 "741d03db1f78c348856476b0b27f39c391a6695aa6c199997a517e6f8c0e58f1"
   head "https://github.com/nats-io/nats-streaming-server.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0b2428afbf8d52bbeedb6fb87277df0a6cca05887b85ed55d09ea361b85d3095" => :el_capitan
+    sha256 "fbe77a3a8c7ff7d2a04f27cb9fc048c26ff1188c3c6b038a8d92ccc65ca729a6" => :yosemite
+    sha256 "8b98ed9cf49e01e79c9ddd5fc1fdbdcacfda16a90a628a901c34aecbb4385004" => :mavericks
+  end
+
   depends_on "go" => :build
 
   def install
