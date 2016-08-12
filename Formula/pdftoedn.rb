bec3f0795e047d0ffdc9659b774e97988e7adb30
Ed Porras <ep@digressed.net>
2016-08-12 14:43:47 +0200

    pdftoedn 0.32.2
    Closes #3830.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/pdftoedn.rb
+++ Formula/pdftoedn.rb
@@ -1,8 +1,8 @@
 class Pdftoedn < Formula
   desc "Extract PDF document data and save the output in EDN format"
   homepage "https://github.com/edporras/pdftoedn"
-  url "https://github.com/edporras/pdftoedn/archive/v0.32.1.tar.gz"
-  sha256 "7b56e404710c0d835e8b693660a6e4e98e2d37ab7329a4f67010a10c34235e85"
+  url "https://github.com/edporras/pdftoedn/archive/v0.32.2.tar.gz"
+  sha256 "8a578b9022b90a9585471fbaad991148a336215008b42b497463bc91bde9fcf7"
 
   bottle do
     cellar :any
