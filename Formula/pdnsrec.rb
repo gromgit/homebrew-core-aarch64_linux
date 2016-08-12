42efab6e15021c395bdecdd61d47cec8eb57a24c
ilovezfs <ilovezfs@icloud.com>
2016-08-12 02:13:04 -0700

    pdnsrec: revision for boost
    boost 1.61.0 patch
    
    Closes #3526.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pdnsrec.rb
+++ Formula/pdnsrec.rb
@@ -3,6 +3,7 @@ class Pdnsrec < Formula
   homepage "https://www.powerdns.com/recursor.html"
   url "https://downloads.powerdns.com/releases/pdns-recursor-4.0.1.tar.bz2"
   sha256 "472db541307c8ca83a846d260ecfc854fd8e879c1bb2ce5683a8df5d21e860b0"
+  revision 1
 
   bottle do
     sha256 "31428673a49cfd7d9499e98154daefc493da5e4457adb3ff58ea5f60fdd08ba2" => :el_capitan
@@ -14,12 +15,20 @@ class Pdnsrec < Formula
   depends_on "boost"
   depends_on "openssl"
   depends_on "lua"
+  depends_on "gcc" if MacOS.version <= :mavericks
 
   needs :cxx11
 
   fails_with :clang do
     build 600
     cause "incomplete C++11 support"
+  end
+
+  # boost 1.61.0 compat
+  # upstream commit "fix type"; remove for > 4.0.1
+  patch :p2 do
+    url "https://github.com/PowerDNS/pdns/commit/33f13fde.patch"
+    sha256 "f7944b9fd573619bdeb20bc211b3eb5fd4f129cb0e691ece686d213ebf6e5393"
   end
 
   def install
