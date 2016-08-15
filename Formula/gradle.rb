e7ee228c7354e2071ec8a23ec7fe788d82d9a5be
Sean Reilly <seanjreilly@users.noreply.github.com>
2016-08-15 17:16:14 +0100

    gradle 3.0 (#3905)

--- Formula/gradle.rb
+++ Formula/gradle.rb
@@ -1,14 +1,8 @@
 class Gradle < Formula
   desc "Build system based on the Groovy language"
   homepage "https://www.gradle.org/"
-  url "https://downloads.gradle.org/distributions/gradle-2.14.1-bin.zip"
-  sha256 "cfc61eda71f2d12a572822644ce13d2919407595c2aec3e3566d2aab6f97ef39"
-
-  devel do
-    url "https://downloads.gradle.org/distributions/gradle-3.0-milestone-2-bin.zip"
-    sha256 "5c3e8e9a38c92ae85e05df609c6c1b6f51e5a08b39a26d61bd8a7044268135e7"
-    version "3.0-milestone-2"
-  end
+  url "https://downloads.gradle.org/distributions/gradle-3.0-bin.zip"
+  sha256 "39c906941a474444afbddc38144ed44166825acb0a57b0551dddb04bbf157f80"
 
   bottle :unneeded
 
