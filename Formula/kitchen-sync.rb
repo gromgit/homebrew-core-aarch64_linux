37992c20b259e00e355b92d531b43523f64c9672
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-16 17:35:24 +0100

    kitchen-sync: update 0.54 bottle.

--- Formula/kitchen-sync.rb
+++ Formula/kitchen-sync.rb
@@ -7,9 +7,9 @@ class KitchenSync < Formula
 
   bottle do
     cellar :any
-    sha256 "d33cfcc2f9929693f6b8f14a532538516967e880b65bc5e1f2a06b0ece322d1b" => :el_capitan
-    sha256 "5abe68abc5a10ab276dd860f793386ff55231433206430a5b0fe636441c98095" => :yosemite
-    sha256 "a16decbc9c92076204f4ac0759bef20766f530ea538c11686238a699fa93e0ac" => :mavericks
+    sha256 "dd9af539436441a17b9a11868ff599a24ae90c15313d52fff48398d06fd78cad" => :el_capitan
+    sha256 "b6a9636e00f2d3e79eae34f32a53751ddd205b1661dcf4bc3c61fd41f40ac0fa" => :yosemite
+    sha256 "d70bab51bb8f803546aba16c63097a3ffc334282caa8bbecc955c8cb21beff37" => :mavericks
   end
 
   depends_on "cmake" => :build
