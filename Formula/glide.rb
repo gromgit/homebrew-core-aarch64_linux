fbaa582072c74d730e23a1e1534080e323aa0406
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-16 03:28:53 +0100

    glide: bump revision for Go 1.7

--- Formula/glide.rb
+++ Formula/glide.rb
@@ -1,10 +1,10 @@
-require "language/go"
-
 class Glide < Formula
   desc "Simplified Go project management, dependency management, and vendoring"
   homepage "https://github.com/Masterminds/glide"
   url "https://github.com/Masterminds/glide/archive/v0.11.1.tar.gz"
   sha256 "3c4958d1ab9446e3d7b2dc280cd43b84c588d50eb692487bcda950d02b9acc4c"
+  revision 1
+
   head "https://github.com/Masterminds/glide.git"
 
   bottle do
@@ -14,40 +14,24 @@ class Glide < Formula
     sha256 "7e393a090ea6dc107fde98fd3d1e6da914269589efbaae3c26fbfb20ff2d50eb" => :mavericks
   end
 
-  depends_on "go" => :build
-
-  go_resource "gopkg.in/yaml.v2" do
-    url "https://gopkg.in/yaml.v2.git",
-      :revision => "a83829b6f1293c91addabc89d0571c246397bbf4"
-  end
-
-  go_resource "github.com/Masterminds/vcs" do
-    url "https://github.com/Masterminds/vcs.git",
-      :revision => "fbe9fb6ad5b5f35b3e82a7c21123cfc526cbf895"
-  end
-
-  go_resource "github.com/codegangsta/cli" do
-    url "https://github.com/codegangsta/cli.git",
-      :revision => "71f57d300dd6a780ac1856c005c4b518cfd498ec"
-  end
-
-  go_resource "github.com/Masterminds/semver" do
-    url "https://github.com/Masterminds/semver.git",
-      :revision => "8d0431362b544d1a3536cca26684828866a7de09"
-  end
+  depends_on "go"
 
   def install
     ENV["GOPATH"] = buildpath
-    mkdir_p buildpath/"src/github.com/Masterminds/"
-    ln_s buildpath, buildpath/"src/github.com/Masterminds/glide"
-    Language::Go.stage_deps resources, buildpath/"src"
+    glidepath = buildpath/"src/github.com/Masterminds/glide"
+    glidepath.install buildpath.children
 
-    system "go", "build", "-o", "glide", "-ldflags", "-X main.version #{version}"
-    bin.install "glide"
+    cd glidepath do
+      system "go", "build", "-o", "glide", "-ldflags", "-X main.version=#{version}"
+      bin.install "glide"
+      prefix.install_metafiles
+    end
   end
 
   test do
     version = pipe_output("#{bin}/glide --version")
-    assert_match /#{version}/, version
+    assert_match version.to_s, version
+    system bin/"glide", "create", "--non-interactive", "--skip-import"
+    assert File.exist?("glide.yaml")
   end
 end
