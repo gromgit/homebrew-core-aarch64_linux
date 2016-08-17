06422ce2a3e591470f8851f319dfba95577ab45f
ilovezfs <ilovezfs@icloud.com>
2016-08-17 01:28:12 -0700

    xonsh 0.4.5
    use new virtualenv feature
    
    Closes #3954.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/xonsh.rb
+++ Formula/xonsh.rb
@@ -1,8 +1,10 @@
 class Xonsh < Formula
+  include Language::Python::Virtualenv
+
   desc "Python-ish, BASHwards-compatible shell language and command prompt"
   homepage "http://xon.sh"
-  url "https://github.com/xonsh/xonsh/archive/0.4.4.tar.gz"
-  sha256 "c029b831615ea4f5f032172c10bb08c472ceda669fa119e190233e045ea434d3"
+  url "https://github.com/xonsh/xonsh/archive/0.4.5.tar.gz"
+  sha256 "1887206a62fab7ed24088f0d00cf549c4a69d6bfd638bde516a43e8102470a3b"
   head "https://github.com/scopatz/xonsh.git"
 
   bottle do
@@ -20,17 +22,8 @@ class Xonsh < Formula
   end
 
   def install
-    version = Language::Python.major_minor_version "python3"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{version}/site-packages"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{version}/site-packages"
-
-    resource("ply").stage do
-      system "python3", *Language::Python.setup_install_args(libexec/"vendor")
-    end
-
-    system "python3", *Language::Python.setup_install_args(libexec)
-    bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    virtualenv_create(libexec, "python3")
+    virtualenv_install_with_resources
   end
 
   test do
