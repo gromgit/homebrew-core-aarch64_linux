a62e39300ba57fbc05132664fabec640a3442992
Zhiming Wang <zmwangx@gmail.com>
2016-08-18 08:49:48 +0800

    hopenpgp-tools 0.19.2 (new formula)
    Closes #3997.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Formula/hopenpgp-tools.rb
@@ -0,0 +1,33 @@
+require "language/haskell"
+
+class HopenpgpTools < Formula
+  include Language::Haskell::Cabal
+
+  desc "Command-line tools for OpenPGP-related operations"
+  homepage "https://hackage.haskell.org/package/hopenpgp-tools"
+  url "https://hackage.haskell.org/package/hopenpgp-tools/hopenpgp-tools-0.19.2.tar.gz"
+  sha256 "6bcc067bfc2b54c0c47ea7169f3741ec8f64abee9bd8c398191a0b35520fa39c"
+  head "git://anonscm.debian.org/users/clint/hopenpgp-tools.git"
+
+  depends_on "ghc" => :build
+  depends_on "cabal-install" => :build
+  depends_on "nettle"
+  depends_on "pkg-config" => :build
+
+  resource "homebrew-key.gpg" do
+    url "https://gist.githubusercontent.com/zmwangx/be307671d11cd78985bd3a96182f15ea/raw/c7e803814efc4ca96cc9a56632aa542ea4ccf5b3/homebrew-key.gpg"
+    sha256 "994744ca074a3662cff1d414e4b8fb3985d82f10cafcaadf1f8342f71f36b233"
+  end
+
+  def install
+    install_cabal_package :using => ["alex", "happy"]
+  end
+
+  test do
+    ENV["TERM"] = "dumb"
+    resource("homebrew-key.gpg").stage do
+      assert_match "Homebrew <security@brew.sh>",
+                   shell_output("#{bin}/hokey lint <homebrew-key.gpg 2>/dev/null")
+    end
+  end
+end
