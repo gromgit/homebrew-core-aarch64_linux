5b581355c0a3d3f7522a0ac34510233ed6f10348
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-18 16:48:05 +0100

    l-smash: fix url placement

--- Formula/l-smash.rb
+++ Formula/l-smash.rb
@@ -1,9 +1,10 @@
 class LSmash < Formula
   desc "Tool for working with MP4 files"
   homepage "https://l-smash.github.io/l-smash/"
-  url "https://github.com/l-smash/l-smash.git", :shallow => false,
-                                                :tag => "v2.9.1",
-                                                :revision => "4cea08d264933634db5bc06da9d8d88fb5ddae07"
+  url "https://github.com/l-smash/l-smash.git",
+      :shallow => false,
+      :tag => "v2.9.1",
+      :revision => "4cea08d264933634db5bc06da9d8d88fb5ddae07"
   head "https://github.com/l-smash/l-smash.git"
 
   bottle do
@@ -20,9 +21,9 @@ class LSmash < Formula
   end
 
   test do
-    system "#{bin}/boxdumper", "-v"
-    system "#{bin}/muxer", "-v"
-    system "#{bin}/remuxer", "-v"
-    system "#{bin}/timelineeditor", "-v"
+    system bin/"boxdumper", "-v"
+    system bin/"muxer", "-v"
+    system bin/"remuxer", "-v"
+    system bin/"timelineeditor", "-v"
   end
 end
