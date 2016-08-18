335f55f184800833a4550ce2348ad4827ee9170e
Miguel Araújo <miguelarauj1o@gmail.com>
2016-08-17 22:28:08 -0300

    uberftp: fix indentation (#3980)

--- Formula/uberftp.rb
+++ Formula/uberftp.rb
@@ -16,10 +16,10 @@ class Uberftp < Formula
   def install
     globus = Formula["globus-toolkit"].opt_prefix
 
-   # patch needed since location changed with globus-toolkit versions>=6.0,
-   # patch to upstream is not yet merged
-   # (located at https://github.com/JasonAlt/UberFTP/pull/8)
-   # but solves not whole problem (needs aditional patch)
+    # patch needed since location changed with globus-toolkit versions>=6.0,
+    # patch to upstream is not yet merged
+    # (located at https://github.com/JasonAlt/UberFTP/pull/8)
+    # but solves not whole problem (needs aditional patch)
     inreplace "configure", "globus_location/include/globus/gcc64dbg", "globus_location/libexec/include"
     inreplace "configure", "globus_location/lib64", "globus_location/libexec/lib"
 
