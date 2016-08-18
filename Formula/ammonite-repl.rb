0f1e16f64adcc4b7fb0b0661108684429f5c541f
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-18 02:38:37 +0100

    ammonite-repl: add explicit version

--- Formula/ammonite-repl.rb
+++ Formula/ammonite-repl.rb
@@ -2,6 +2,7 @@ class AmmoniteRepl < Formula
   desc "Ammonite is a cleanroom re-implementation of the Scala REPL"
   homepage "https://lihaoyi.github.io/Ammonite/#Ammonite-REPL"
   url "https://github.com/lihaoyi/Ammonite/releases/download/0.7.4/0.7.4", :using => :nounzip
+  version "0.7.4"
   sha256 "d64021649484cf77bef58e1e21da755df932ccb1a0512c694a672a9acfcd2bd2"
 
   bottle :unneeded
