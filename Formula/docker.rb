6131cf81a7646876107fda7328105fbc5b32dd19
Rhys YM Lu <rhysym@googlemail.com>
2016-08-18 23:13:22 +0100

    docker 1.12.1
    Closes #4026.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -2,8 +2,8 @@ class Docker < Formula
   desc "Pack, ship and run any application as a lightweight container"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/docker.git",
-      :tag => "v1.12.0",
-      :revision => "8eab29edd820017901796eb60d4bea28d760f16f"
+      :tag => "v1.12.1",
+      :revision => "23cf638307f030cd8d48c9efc21feec18a6f88f8"
 
   head "https://github.com/docker/docker.git"
 
