4c31f8c5328339507f0a148ce46f326d78b8fa87
Miguel Araújo <miguelarauj1o@gmail.com>
2016-08-17 22:28:36 -0300

    sqtop: audit fixes (#3979)

--- Formula/sqtop.rb
+++ Formula/sqtop.rb
@@ -19,6 +19,6 @@ class Sqtop < Formula
   end
 
   test do
-    assert_match "#{version}", shell_output("#{bin}/sqtop --help")
+    assert_match version.to_s, shell_output("#{bin}/sqtop --help")
   end
 end
