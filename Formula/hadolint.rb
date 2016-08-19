7f29610249454113d6baa0a27f1056d21216edcf
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-19 20:12:59 +0100

    hadolint: update 1.2.1 bottle.

--- Formula/hadolint.rb
+++ Formula/hadolint.rb
@@ -9,9 +9,10 @@ class Hadolint < Formula
   sha256 "e0f06adf266f366d8ac847be979ca6db9f16b487f2b1d0d3a2d1db4d1a3e11ea"
 
   bottle do
-    sha256 "2e516c0655c320852eba081edafc0e9c48a2504460a0e55c0aaaa3e813db9a45" => :el_capitan
-    sha256 "62eea06fe495a030eec19f4e48dd4e56d9cc6e521a13eac77192c11d6edc7aaf" => :yosemite
-    sha256 "060d3f316e9237c8f5b64166da1dae5e844a41fcb5d1fd05ae577dfed0fd746f" => :mavericks
+    cellar :any_skip_relocation
+    sha256 "a100c0b33af09bfc6cebc79d4b77f1297d3af9fe9fd28c0a1b61317f921abdac" => :el_capitan
+    sha256 "dd73c444fe18969e280438855416bd2d03cab6f198f43fc8cc83fc03a9a6e7c3" => :yosemite
+    sha256 "68f377b99b1245afa10e940059fe22619702f7b9536f85f701afbaa6343a45db" => :mavericks
   end
 
   depends_on "ghc" => :build
