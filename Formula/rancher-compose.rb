36d6379d2f81ed93aba6d9dafa8d1e4f71179a3a
ilovezfs <ilovezfs@icloud.com>
2016-08-18 21:49:20 -0700

    rancher-compose 0.9.2
    Closes #4036.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/rancher-compose.rb
+++ Formula/rancher-compose.rb
@@ -1,8 +1,8 @@
 class RancherCompose < Formula
   desc "Docker Compose compatible client to deploy to Rancher"
   homepage "https://github.com/rancher/rancher-compose"
-  url "https://github.com/rancher/rancher-compose/archive/v0.9.1.tar.gz"
-  sha256 "8221c5dada2f35caa480314969d8a3919548b04ec54beba3608f9f2b31f1e4b9"
+  url "https://github.com/rancher/rancher-compose/archive/v0.9.2.tar.gz"
+  sha256 "7fd855a48c107e8f14bfa28b2f40ad2ccfac2fdfb1116d58268c68268e620ee6"
 
   bottle do
     cellar :any_skip_relocation
@@ -15,11 +15,12 @@ class RancherCompose < Formula
 
   def install
     ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/rancher/rancher-compose/").install Dir["*"]
+    (buildpath/"src/github.com/rancher/rancher-compose").install Dir["*"]
     system "go", "build", "-ldflags",
            "-w -X github.com/rancher/rancher-compose/version.VERSION=#{version}",
            "-o", "#{bin}/rancher-compose",
-           "-v", "github.com/rancher/rancher-compose/"
+           "-v", "github.com/rancher/rancher-compose"
+    prefix.install_metafiles "src/github.com/rancher/rancher-compose"
   end
 
   test do
