1281c4f351ac1383d0d4e7ada9f62481b1c9da86
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2016-08-19 16:16:11 -0600

    rfcmarkup 1.119
    Closes #4058.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/rfcmarkup.rb
+++ Formula/rfcmarkup.rb
@@ -1,8 +1,8 @@
 class Rfcmarkup < Formula
   desc "Add HTML markup and links to internet-drafts and RFCs"
   homepage "https://tools.ietf.org/tools/rfcmarkup/"
-  url "https://tools.ietf.org/tools/rfcmarkup/rfcmarkup-1.108.tgz"
-  sha256 "77a78a1df5e155acb1df24ced950b9c7af022a71f07ad7eec2d64b9cc8ef5466"
+  url "https://tools.ietf.org/tools/rfcmarkup/rfcmarkup-1.119.tgz"
+  sha256 "46c5522f3cba0d430019a60de0e995adbc12f055970b6b341f45181cf8deed8e"
 
   bottle do
     cellar :any_skip_relocation
