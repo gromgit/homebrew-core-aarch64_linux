358d24b212a48a8c089811299714b8eb7f9b9446
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-20 00:54:20 +0100

    hbase: update 1.2.2 bottle.

--- Formula/hbase.rb
+++ Formula/hbase.rb
@@ -5,9 +5,9 @@ class Hbase < Formula
   sha256 "8c9cc9a19e3f4a3137509513b5c1b9e1b5a2283ed261d44af9af1c02c5453c20"
 
   bottle do
-    sha256 "4d3cdeb9062d589359955117a2012c5ab1f9643cd9076bd2eaaa7ade8a1d94b2" => :el_capitan
-    sha256 "168366c440e13265393c3f918cfe3cf606de5571e9cb6bf56338ceb0fceb4d99" => :yosemite
-    sha256 "3cbb7b2809c152f66adf4450fb44e8a4c15934c364219773a786175be3e5c4b8" => :mavericks
+    sha256 "8f1972d8b77e7f5d83cdca0fff543205cecf6f5034bfc7a5aeb32da9ef0215ae" => :el_capitan
+    sha256 "2aba5a41ee729ad6a0897e317a5ddcbae9b80fbc22c9b997e1b98dbdd37aef8c" => :yosemite
+    sha256 "f6f10e6ccd23fc9f9586f570aca9c27fbb6500ae9156d63ad905ecda0eb9073e" => :mavericks
   end
 
   depends_on :java => "1.7+"
