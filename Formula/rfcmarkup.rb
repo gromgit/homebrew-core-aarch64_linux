530190f23c95eaddd9c7e7844dbfe39d6ba9d060
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-20 01:02:24 +0100

    rfcmarkup: update 1.119 bottle.

--- Formula/rfcmarkup.rb
+++ Formula/rfcmarkup.rb
@@ -6,10 +6,9 @@ class Rfcmarkup < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c0de96221a2fe2ea3a65bfec89258627302c873658f5a9c2a1e509b298b72ab6" => :el_capitan
-    sha256 "44a9b77ba4c5770813797b24e3bae75a9b5ad8dbd017ddd210c5c5919b788010" => :yosemite
-    sha256 "d97af1d0123131ba3bcd55cb1359d02ad402a4d5073fbf550e77bb1e37767409" => :mavericks
-    sha256 "71fa6ab7113c159ff80ae9eab0e2a7807552c51571f4bd645252bfdf670ed6fc" => :mountain_lion
+    sha256 "a15f3c6be0c5eb4b38c4801c6151d4a12b2f206ab6e9c7f11dd0cd94ba7f9e9d" => :el_capitan
+    sha256 "5eaeed274aca3e64cbc2407a6b9b531efed736fec325d15109b308bdbea971b4" => :yosemite
+    sha256 "5eaeed274aca3e64cbc2407a6b9b531efed736fec325d15109b308bdbea971b4" => :mavericks
   end
 
   def install
