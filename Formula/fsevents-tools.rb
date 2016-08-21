d3e6066d3705122f49111d306c3c9f2d0e78b2bb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-21 03:30:44 +0100

    fsevents-tools: add 1.0.0 bottle.

--- Formula/fsevents-tools.rb
+++ Formula/fsevents-tools.rb
@@ -4,6 +4,13 @@ class FseventsTools < Formula
   url "http://geoff.greer.fm/fsevents/releases/fsevents-tools-1.0.0.tar.gz"
   sha256 "498528e1794fa2b0cf920bd96abaf7ced15df31c104d1a3650e06fa3f95ec628"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a15d1b927a74d8b76b553d696343ac5fe82396f8b5b052e15adc6c3270f2ccd6" => :el_capitan
+    sha256 "9d4bda3175c503f8ff65f134495de43471854eacc3396a9cf1895d5c2b91cb63" => :yosemite
+    sha256 "045442f0279c20196e0941fbf160557ebdea04169267ba8105717d7d207cc346" => :mavericks
+  end
+
   head do
     url "https://github.com/ggreer/fsevents-tools.git"
 
