db521b721495901dae6023d2d50c0b7d8f869ec8
ilovezfs <ilovezfs@icloud.com>
2016-08-21 04:49:19 -0700

    osquery: revision for rocksdb
    Closes #4094.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/osquery.rb
+++ Formula/osquery.rb
@@ -5,7 +5,7 @@ class Osquery < Formula
   url "https://github.com/facebook/osquery.git",
     :tag => "1.7.3",
     :revision => "6901aa644a9bcc0667207008db71471abf756b82"
-  revision 2
+  revision 3
 
   bottle do
     cellar :any
