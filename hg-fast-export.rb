4c934591d963a9946fa3730ffe67528611d79ff4
Baptiste Fontaine <b@ptistefontaine.fr>
2016-08-21 21:23:41 +0200

    hg-fast-export 160415 (import from head-only)
    Closes #4112.
    
    Signed-off-by: Andrew Janke <janke@eilonwy.local>

---
+++ hg-fast-export.rb
@@ -0,0 +1,17 @@
+class HgFastExport < Formula
+  homepage "http://repo.or.cz/w/fast-export.git"
+  url "https://github.com/frej/fast-export/archive/v160415.tar.gz"
+  sha256 "0e8a8c27110f200b52be55d13858045ed069ae60a51b4d8878a310426d244316"
+
+  head "git://repo.or.cz/fast-export.git"
+
+  def install
+    bin.install "hg-fast-export.py", "hg-fast-export.sh",
+                "hg-reset.py", "hg-reset.sh",
+                "hg2git.py"
+  end
+
+  test do
+    system bin/"hg-reset.sh", "--help"
+  end
+end
