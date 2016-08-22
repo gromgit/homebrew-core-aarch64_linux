aa92696d92f70340ede6817e1bdf08443b8e8fe9
Andrew Janke <janke@eilonwy.local>
2016-08-22 17:12:56 -0500

    hg-fast-export: audit fixes

--- hg-fast-export.rb
+++ hg-fast-export.rb
@@ -1,9 +1,12 @@
 class HgFastExport < Formula
+  desc "Fast Mercurial to Git converter"
   homepage "http://repo.or.cz/w/fast-export.git"
   url "https://github.com/frej/fast-export/archive/v160415.tar.gz"
   sha256 "0e8a8c27110f200b52be55d13858045ed069ae60a51b4d8878a310426d244316"
 
   head "git://repo.or.cz/fast-export.git"
+
+  bottle :unneeded
 
   def install
     bin.install "hg-fast-export.py", "hg-fast-export.sh",
