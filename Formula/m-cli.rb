18bb9095ff6ea2dd12010276b80d96fec7344523
rgcr <rogelio.cedillo.rivera@gmail.com>
2016-08-22 12:27:32 -0500

    m-cli 0.1.8

--- Formula/m-cli.rb
+++ Formula/m-cli.rb
@@ -1,8 +1,8 @@
 class MCli < Formula
   desc "Swiss Army Knife for Mac OS X"
   homepage "https://github.com/rgcr/m-cli"
-  url "https://github.com/rgcr/m-cli/archive/v0.1.6.tar.gz"
-  sha256 "c77811380c250bbe4ca9333b1f8b26099ae697c40c9f80f85c9ec45c04cfbc34"
+  url "https://github.com/rgcr/m-cli/archive/v0.1.8.tar.gz"
+  sha256 "1982992625a14cefc2af480656a9e672ab071b7172b4859fd21cdcb07294cc8a"
   head "https://github.com/rgcr/m-cli.git"
 
   bottle :unneeded
