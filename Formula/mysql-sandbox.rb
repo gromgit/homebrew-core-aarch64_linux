878fe048711df0aa3053e21a9cadd75fdddc7195
ilovezfs <ilovezfs@icloud.com>
2016-08-21 23:06:08 -0700

    mysql-sandbox 3.1.13
    Closes #4121.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mysql-sandbox.rb
+++ Formula/mysql-sandbox.rb
@@ -1,8 +1,8 @@
 class MysqlSandbox < Formula
   desc "Install one or more MySQL servers"
   homepage "http://mysqlsandbox.net"
-  url "https://github.com/datacharmer/mysql-sandbox/releases/download/3.1.12/MySQL-Sandbox-3.1.12.tar.gz"
-  sha256 "1dd7eada1bf05d297c63a3c16c41183801d792393b850e4059a828841fbd5b38"
+  url "https://github.com/datacharmer/mysql-sandbox/releases/download/3.1.13/MySQL-Sandbox-3.1.13.tar.gz"
+  sha256 "504b4e7d1c134759aebff607f8e8350f47e68ff5833f8ba28c4f72bb494e4525"
   head "https://github.com/datacharmer/mysql-sandbox.git"
 
   bottle do
