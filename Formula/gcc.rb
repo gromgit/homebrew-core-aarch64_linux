270da3d865a541671b4e4b9e2b2c09af1157c3e6
Manuel Grandeit <m.grandeit@gmail.com>
2016-08-23 00:51:24 +0200

    gcc 6.2.0
    Closes #4149.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/gcc.rb
+++ Formula/gcc.rb
@@ -21,25 +21,13 @@ class Gcc < Formula
 
   desc "GNU compiler collection"
   homepage "https://gcc.gnu.org"
-  revision 1
 
   head "svn://gcc.gnu.org/svn/gcc/trunk"
 
   stable do
-    url "https://ftpmirror.gnu.org/gcc/gcc-6.1.0/gcc-6.1.0.tar.bz2"
-    mirror "https://ftp.gnu.org/gnu/gcc/gcc-6.1.0/gcc-6.1.0.tar.bz2"
-    sha256 "09c4c85cabebb971b1de732a0219609f93fc0af5f86f6e437fd8d7f832f1a351"
-
-    # 6.1.0 contains a bug that in certain circumstances causes GCC to consume
-    # all available memory very quickly & effectively crash systems.
-    # This should be safe to remove on the next stable release.
-    # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=70977
-    # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=70824
-    # https://github.com/gcc-mirror/gcc/commit/75e7d6607
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/c963247c6b/gcc/gcc-6.1.0_infinite_memory_snacking.patch"
-      sha256 "636394ab2024ab026ead265b13b4c2a24e44c20ddfeab43a9be6e78e824de4f2"
-    end
+    url "https://ftpmirror.gnu.org/gcc/gcc-6.2.0/gcc-6.2.0.tar.bz2"
+    mirror "https://ftp.gnu.org/gnu/gcc/gcc-6.2.0/gcc-6.2.0.tar.bz2"
+    sha256 "9944589fc722d3e66308c0ce5257788ebd7872982a718aa2516123940671b7c5"
   end
 
   bottle do
