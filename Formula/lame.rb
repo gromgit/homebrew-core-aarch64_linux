83567ffdf10b767a22c0a6dcb4e79ad7e0f35403
Miguel Araújo <miguelarauj1o@gmail.com>
2016-08-22 21:51:19 -0300

    lame: update description

--- Formula/lame.rb
+++ Formula/lame.rb
@@ -1,5 +1,5 @@
 class Lame < Formula
-  desc "Lame Aint an MP3 Encoder (LAME)"
+  desc "High quality MPEG Audio Layer III (MP3) encoder"
   homepage "http://lame.sourceforge.net/"
   url "https://downloads.sourceforge.net/sourceforge/lame/lame-3.99.5.tar.gz"
   sha256 "24346b4158e4af3bd9f2e194bb23eb473c75fb7377011523353196b19b9a23ff"
