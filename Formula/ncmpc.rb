52349285fb9cfc42088e6e5efa52f4c854a3d30e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-23 11:07:37 +0100

    ncmpc: update 0.25 bottle.

--- Formula/ncmpc.rb
+++ Formula/ncmpc.rb
@@ -5,9 +5,9 @@ class Ncmpc < Formula
   sha256 "5b00237be90367aff98b2b70df88b6d6d4b566291d870053be106b137dcc0fd9"
 
   bottle do
-    sha256 "83911b3c3d4bb8c91a3f2e436882dced0e7b8a7733449f2ac2cfc0b6bc3512d2" => :el_capitan
-    sha256 "5f97cdc0113766019222a32c3fd169d61d8fcb3bee1c613afa4a829604db13ff" => :yosemite
-    sha256 "26830f339ba2ff11f2f5c5a0c56e4e108849dd4522b9b66cb74629a20f37ef49" => :mavericks
+    sha256 "2e02f6c2a5adbb5bf27f3b1c9dd33d1e77fed67d8599f83f2c3b65921c4731dd" => :el_capitan
+    sha256 "a5ee00bd5006e9156199cd263cbd8ee6a5027b60f211017df0da14bb184b2cbb" => :yosemite
+    sha256 "570fe6aba12ceeea5aea099879915a00a6459cd2f7c58e15248f4cc88c374b5a" => :mavericks
   end
 
   depends_on "pkg-config" => :build
