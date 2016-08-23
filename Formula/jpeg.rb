d9b042788ff48f471caa2491fcfc1cce3bc4003b
Miguel Araújo <miguelarauj1o@gmail.com>
2016-08-22 21:49:28 -0300

    jpeg: fix audit warning
    * Description shouldn't include the formula name

--- Formula/jpeg.rb
+++ Formula/jpeg.rb
@@ -1,5 +1,5 @@
 class Jpeg < Formula
-  desc "JPEG image manipulation library"
+  desc "Image manipulation library"
   homepage "http://www.ijg.org"
   url "http://www.ijg.org/files/jpegsrc.v8d.tar.gz"
   sha256 "00029b1473f0f0ea72fbca3230e8cb25797fbb27e58ae2e46bb8bf5a806fe0b3"
