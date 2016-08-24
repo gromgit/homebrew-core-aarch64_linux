f1d447651da99b02ddb13fe69f6954af61091a21
Leon Koppel <leonkoppel@outlook.com>
2016-08-24 22:39:18 +0200

    libdivecomputer 0.4.2 (new formula)
    Add formula for libdivecomputer, a cross-platform
    library for communication with dive computers
    from various manufacturers.
    
    Closes #4215.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Formula/libdivecomputer.rb
@@ -0,0 +1,46 @@
+class Libdivecomputer < Formula
+  desc "Library for communication with various dive computers."
+  homepage "http://www.libdivecomputer.org"
+  url "http://www.libdivecomputer.org/releases/libdivecomputer-0.4.2.tar.gz"
+  sha256 "f3c5d0229de9dd82602e309273d0eb48fb0ee07fdcfc1ff8206edb6ba5154460"
+  head "git://git.code.sf.net/p/libdivecomputer/code"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "libusb" => :recommended
+
+  def install
+    system "autoreconf", "--install" if build.head?
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <libdivecomputer/context.h>
+      #include <libdivecomputer/descriptor.h>
+      #include <libdivecomputer/iterator.h>
+      int main(int argc, char *argv[]) {
+        dc_iterator_t *iterator;
+        dc_descriptor_t *descriptor;
+        dc_descriptor_iterator(&iterator);
+        while (dc_iterator_next(iterator, &descriptor) == DC_STATUS_SUCCESS)
+        {
+          dc_descriptor_free(descriptor);
+        }
+        dc_iterator_free(iterator);
+        return 0;
+      }
+    EOS
+    flags = %W[
+      -I#{include}
+      -L#{lib}
+      -ldivecomputer
+    ]
+    system ENV.cc, "-v", "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
