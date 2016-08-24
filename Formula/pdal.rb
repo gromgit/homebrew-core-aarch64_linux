bcb4a8370d0e4ea6d2c12113e622793a9f1d9cbb
Dominyk Tiller <DomT4@users.noreply.github.com>
2016-08-24 03:37:56 +0100

    pdal: add optional laszip dependency (#4195)
    Closes #3699.

--- Formula/pdal.rb
+++ Formula/pdal.rb
@@ -13,6 +13,7 @@ class Pdal < Formula
 
   depends_on "cmake" => :build
   depends_on "gdal"
+  depends_on "laszip" => :optional
 
   if MacOS.version < :mavericks
     depends_on "boost" => "c++11"
@@ -22,7 +23,15 @@ class Pdal < Formula
 
   def install
     ENV.cxx11 if MacOS.version < :mavericks
-    system "cmake", ".", *std_cmake_args
+
+    args = std_cmake_args
+    if build.with? "laszip"
+      args << "-DWITH_LASZIP=TRUE"
+    else
+      args << "-DWITH_LASZIP=FALSE"
+    end
+
+    system "cmake", ".", *args
     system "make", "install"
     doc.install "examples", "test"
   end
