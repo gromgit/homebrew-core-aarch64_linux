65463aac8c32fbb6e6dbfe5fa607220d5f995177
ilovezfs <ilovezfs@icloud.com>
2016-08-24 22:29:30 -0700

    repo 1.12.34
    bump `version_scheme` so that the tag and version can be the same again
    install manifest-format.txt in `doc`
    add a test
    
    Closes #4222.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/repo.rb
+++ Formula/repo.rb
@@ -2,13 +2,18 @@ class Repo < Formula
   desc "Repository tool for Android development"
   homepage "https://source.android.com/source/developing.html"
   url "https://gerrit.googlesource.com/git-repo.git",
-      :tag => "v1.12.32",
-      :revision => "745b4ad660f8050045b521c4e15b7d3ac0b3d70e"
-  version "1.22"
+      :tag => "v1.12.34",
+      :revision => "39252ba028012fd14e4e283217d842fb80206c52"
+  version_scheme 1
 
   bottle :unneeded
 
   def install
     bin.install "repo"
+    doc.install (buildpath/"docs").children
+  end
+
+  test do
+    assert_match "usage:", shell_output("#{bin}/repo help 2>&1", 1)
   end
 end
