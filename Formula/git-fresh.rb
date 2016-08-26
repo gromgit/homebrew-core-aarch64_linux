fdf9602cfb7bf78f8c03c5bb935c54795697ad33
Ivan Malopinsky <imsky@users.noreply.github.com>
2016-08-25 20:41:25 -0400

    git-fresh 1.8.0
    Closes #4240.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/git-fresh.rb
+++ Formula/git-fresh.rb
@@ -1,8 +1,8 @@
 class GitFresh < Formula
   desc "Utility to keep git repos fresh"
   homepage "https://github.com/imsky/git-fresh"
-  url "https://github.com/imsky/git-fresh/archive/v1.6.2.tar.gz"
-  sha256 "dccb33daa14939d53f43e1d02819ecaa960f10a5a479bee32ea47c6e13569623"
+  url "https://github.com/imsky/git-fresh/archive/v1.8.0.tar.gz"
+  sha256 "d05513f189b49573bd3d13015371a6ee06d84aae43443e37b31b940366ebe84b"
 
   bottle :unneeded
 
