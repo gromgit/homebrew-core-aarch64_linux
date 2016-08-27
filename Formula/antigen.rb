670685471309b6c9cb41c91b8214da4b774756c4
ilovezfs <ilovezfs@icloud.com>
2016-08-27 12:26:35 -0700

    antigen 1.0.4 (#4272)
    * antigen 1.0.4
    
    * Update antigen.rb

--- Formula/antigen.rb
+++ Formula/antigen.rb
@@ -1,26 +1,26 @@
 class Antigen < Formula
   desc "Plugin manager for zsh, inspired by oh-my-zsh and vundle."
   homepage "http://antigen.sharats.me/"
-  url "https://github.com/zsh-users/antigen/archive/v1.0.3.tar.gz"
-  sha256 "884092b4ed67f01e407f6354ad2a826797b6a3eebbf8b55cfa1833bdd766b3f4"
+  url "https://github.com/zsh-users/antigen/archive/v1.0.4.tar.gz"
+  sha256 "ab83436644bbcaf028b7cff60a6fcc8ba4904b3f6f376557848e07df4afe1555"
   head "https://github.com/zsh-users/antigen.git"
 
   bottle :unneeded
 
   def install
-    share.install "antigen.zsh"
+    pkgshare.install "antigen.zsh"
   end
 
   def caveats; <<-EOS.undent
     To activate antigen, add the following to your ~/.zshrc:
 
-      source $(brew --prefix)/share/antigen.zsh
+      source $(brew --prefix)/share/antigen/antigen.zsh
 
     EOS
   end
 
   test do
-    (testpath/".zshrc").write "source `brew --prefix`/share/antigen.zsh"
-    system "/bin/zsh", "--login", "-i", "-c", "antigen help"
+    (testpath/".zshrc").write "source `brew --prefix`/share/antigen/antigen.zsh\n"
+    system "zsh", "--login", "-i", "-c", "antigen help"
   end
 end
