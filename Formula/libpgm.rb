5341f30e47fb08ba70915ed74348b863ef4e2681
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 19:28:18 +0100

    libpgm: migrate to archive URL

--- Formula/libpgm.rb
+++ Formula/libpgm.rb
@@ -1,7 +1,7 @@
 class Libpgm < Formula
   desc "Implements the PGM reliable multicast protocol"
-  homepage "https://code.google.com/p/openpgm/"
-  url "https://openpgm.googlecode.com/files/libpgm-5.2.122%7Edfsg.tar.gz"
+  homepage "https://code.google.com/archive/p/openpgm/"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/openpgm/libpgm-5.2.122~dfsg.tar.gz"
   version "5.2.122"
   sha256 "e296f714d7057e3cdb87f4e29b1aecb3b201b9fcb60aa19ed4eec29524f08bd8"
 
