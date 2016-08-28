fe2dc8fc015cf992f9db039a525637d016751404
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 17:09:16 +0100

    csshx: migrate to archive URL

--- Formula/csshx.rb
+++ Formula/csshx.rb
@@ -1,7 +1,7 @@
 class Csshx < Formula
   desc "Cluster ssh tool for Terminal.app"
   homepage "https://github.com/brockgr/csshx"
-  url "https://csshx.googlecode.com/files/csshX-0.74.tgz"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/csshx/csshX-0.74.tgz"
   mirror "https://distfiles.macports.org/csshX/csshX-0.74.tgz"
   sha256 "eaa9e52727c8b28dedc87398ed33ffa2340d6d0f3ea9d261749c715cb7a0e9c8"
   head "https://github.com/brockgr/csshx.git"
@@ -10,5 +10,9 @@ class Csshx < Formula
 
   def install
     bin.install "csshX"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/csshX --version 2>&1", 2)
   end
 end
