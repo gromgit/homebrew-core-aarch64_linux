320dc88ec0bba727c9be421a9a78ced17ab9b35c
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 19:33:07 +0100

    ninja: update gtest URL

--- Formula/ninja.rb
+++ Formula/ninja.rb
@@ -17,7 +17,7 @@ class Ninja < Formula
   deprecated_option "without-tests" => "without-test"
 
   resource "gtest" do
-    url "https://googletest.googlecode.com/files/gtest-1.7.0.zip"
+    url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/googletest/gtest-1.7.0.zip"
     sha256 "247ca18dd83f53deb1328be17e4b1be31514cedfc1e3424f672bf11fd7e0d60d"
   end
 
