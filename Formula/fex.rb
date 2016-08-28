a6062e5daa2bbcda77f2a99f937fb3ca82c07ed9
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 17:52:44 +0100

    fex: migrate to archive URL

--- Formula/fex.rb
+++ Formula/fex.rb
@@ -1,7 +1,7 @@
 class Fex < Formula
   desc "Powerful field extraction tool"
   homepage "https://www.semicomplete.com/projects/fex/"
-  url "https://semicomplete.googlecode.com/files/fex-2.0.0.tar.gz"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/semicomplete/fex-2.0.0.tar.gz"
   sha256 "03043c8eac74f43173068a2e693b6f73d5b45f453a063e6da11f34455d0e374e"
 
   bottle do
