86810804d5e463f5801739511e391b801a09f04a
ilovezfs <ilovezfs@icloud.com>
2016-08-28 01:54:02 -0700

    gitbucket 4.4
    Closes #4283.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gitbucket.rb
+++ Formula/gitbucket.rb
@@ -1,8 +1,8 @@
 class Gitbucket < Formula
   desc "Git platform powered by Scala offering"
   homepage "https://github.com/gitbucket/gitbucket"
-  url "https://github.com/gitbucket/gitbucket/releases/download/4.3/gitbucket.war"
-  sha256 "b1ca4e4aa86c408018db69b003ff69b7a19a818c9c5474ad99af32d38d033bf8"
+  url "https://github.com/gitbucket/gitbucket/releases/download/4.4/gitbucket.war"
+  sha256 "1fb636a14cba6e6cc144e183315cd1af6c6a3f44fc899443f780dbc59798d765"
 
   head do
     url "https://github.com/gitbucket/gitbucket.git"
