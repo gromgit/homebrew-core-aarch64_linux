883cab34d84b6b519ea184de9c45d9c965595b89
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 19:24:34 +0100

    libmarisa: migrate to archive URL

--- Formula/libmarisa.rb
+++ Formula/libmarisa.rb
@@ -4,7 +4,7 @@ class Libmarisa < Formula
   revision 1
 
   stable do
-    url "https://marisa-trie.googlecode.com/files/marisa-0.2.4.tar.gz"
+    url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/marisa-trie/marisa-0.2.4.tar.gz"
     sha256 "67a7a4f70d3cc7b0a85eb08f10bc3eaf6763419f0c031f278c1f919121729fb3"
 
     # trie-test.cc:71: TestKey(): 115: Assertion `r_key.ptr() == NULL' failed.
