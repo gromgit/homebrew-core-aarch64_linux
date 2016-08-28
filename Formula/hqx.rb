0510d8b539177cbafd52554c1bbb162fa93e5dfd
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 18:26:26 +0100

    hqx: migrate to archive URL

--- Formula/hqx.rb
+++ Formula/hqx.rb
@@ -1,7 +1,7 @@
 class Hqx < Formula
   desc "Magnification filter designed for pixel art"
-  homepage "https://code.google.com/p/hqx/"
-  url "https://hqx.googlecode.com/files/hqx-1.1.tar.gz"
+  homepage "https://code.google.com/archive/p/hqx/"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/hqx/hqx-1.1.tar.gz"
   sha256 "cc18f571fb4bc325317892e39ecd5711c4901831926bc93296de9ebb7b2f317b"
   revision 1
 
