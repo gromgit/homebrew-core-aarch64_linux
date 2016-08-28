1ab6356885d3287d5826cf06bdc671c4715121de
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-28 19:14:32 +0100

    todolist: update 0.3.0 bottle.

--- Formula/todolist.rb
+++ Formula/todolist.rb
@@ -8,9 +8,9 @@ class Todolist < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6391f9b576bbbdcc136d92e16f69090589fbb6da73c23bb4ca160670f776afa1" => :el_capitan
-    sha256 "f90e4215f3eb542d81a1f22885e99a24fb2f9cb98febba8060b52fc0edbcdeac" => :yosemite
-    sha256 "6660423f7b92d5843a28341fd7eeae4ce965fd38bb8c8f38495d52e8d2f18f20" => :mavericks
+    sha256 "c0ef9f95fa1d682b51f71b75dc46e705c7d7f862b6ed24d55e17a9573b6a5f7d" => :el_capitan
+    sha256 "c79c6abc21121073283243176aabca2560558d654bd8caed2e6bd93666755a90" => :yosemite
+    sha256 "15d3e8b5304156097d843c2f4aabeb1b79f03cd67a57243a4040acaf426fb63d" => :mavericks
   end
 
   depends_on "go" => :build
