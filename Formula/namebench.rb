f093f6e786ab6ae6610b4fe852b7c27d2435fc05
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 19:53:20 +0100

    namebench: migrate to archive URL

--- Formula/namebench.rb
+++ Formula/namebench.rb
@@ -1,7 +1,7 @@
 class Namebench < Formula
   desc "DNS benchmark utility"
-  homepage "https://code.google.com/p/namebench/"
-  url "https://namebench.googlecode.com/files/namebench-1.3.1-source.tgz"
+  homepage "https://code.google.com/archive/p/namebench/"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/namebench/namebench-1.3.1-source.tgz"
   sha256 "30ccf9e870c1174c6bf02fca488f62bba280203a0b1e8e4d26f3756e1a5b9425"
 
   bottle do
@@ -27,6 +27,6 @@ class Namebench < Formula
   end
 
   test do
-    system "#{bin}/namebench", "--query_count", "1", "--only", "8.8.8.8"
+    system bin/"namebench", "--query_count", "1", "--only", "8.8.8.8"
   end
 end
