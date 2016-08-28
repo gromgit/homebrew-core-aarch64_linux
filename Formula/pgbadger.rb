cc96236c80e6cb818993c4198097568d9f931129
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-28 05:24:39 +0100

    pgbadger: update 8.3 bottle.

--- Formula/pgbadger.rb
+++ Formula/pgbadger.rb
@@ -8,9 +8,9 @@ class Pgbadger < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "80dddbcc393ef42d336fee644345de03a364c01e8901f8ee078e8d932509de7e" => :el_capitan
-    sha256 "39756e619f5671942cc279fffdcbac5ba43facb15c08da9e5afebfe5291890a1" => :yosemite
-    sha256 "d057b3c13e60b5abc2c78564800f638341a46608d5e43f9ae36d8b71e0eed6a5" => :mavericks
+    sha256 "ed123ca8ce80164885a2aca6399651b39bd558225022d5786124d546709eb7bf" => :el_capitan
+    sha256 "57d78cfaf000e4f70b05e9d76c357856e791e2b3df9200b46710653551f7cd14" => :yosemite
+    sha256 "f5d9e1e741afc4fad7b9f4d3138c02ec98cc12ef6c8cd26e785d231820c94f50" => :mavericks
   end
 
   def install
