996e3e2a4c5cae21e8d6598dc8e3d2413a264899
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 19:39:14 +0100

    memcache-top: migrate to archive URL

--- Formula/memcache-top.rb
+++ Formula/memcache-top.rb
@@ -1,7 +1,7 @@
 class MemcacheTop < Formula
   desc "Grab real-time stats from memcache"
-  homepage "https://code.google.com/p/memcache-top/"
-  url "https://memcache-top.googlecode.com/files/memcache-top-v0.6"
+  homepage "https://code.google.com/archive/p/memcache-top/"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/memcache-top/memcache-top-v0.6"
   sha256 "d5f896a9e46a92988b782e340416312cc480261ce8a5818db45ccd0da8a0f22a"
 
   bottle :unneeded
