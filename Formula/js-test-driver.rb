baf082b6350063f2e70c43401863dc589e8958e8
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 18:41:25 +0100

    js-test-driver: migrate to archive URL

--- Formula/js-test-driver.rb
+++ Formula/js-test-driver.rb
@@ -1,7 +1,7 @@
 class JsTestDriver < Formula
   desc "JavaScript test runner"
-  homepage "https://code.google.com/p/js-test-driver/"
-  url "https://js-test-driver.googlecode.com/files/JsTestDriver-1.3.5.jar"
+  homepage "https://code.google.com/archive/p/js-test-driver/"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/js-test-driver/JsTestDriver-1.3.5.jar"
   sha256 "78c0ff60a76bea38db0fa6f9c9f8e003d1bfd07517f44c3879f484abfbe87a68"
 
   bottle :unneeded
@@ -9,5 +9,9 @@ class JsTestDriver < Formula
   def install
     libexec.install "JsTestDriver-#{version}.jar"
     bin.write_jar_script libexec/"JsTestDriver-#{version}.jar", "js-test-driver"
+  end
+
+  test do
+    assert_match "QUIET", shell_output("#{bin}/js-test-driver --help", 1)
   end
 end
