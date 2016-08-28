6edce283a2bd21813bb985eb898e96dffd9b5456
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 17:03:39 +0100

    clamz: migrate to archive URL

--- Formula/clamz.rb
+++ Formula/clamz.rb
@@ -1,7 +1,7 @@
 class Clamz < Formula
   desc "Download MP3 files from Amazon's music store"
   homepage "https://code.google.com/archive/p/clamz/"
-  url "https://clamz.googlecode.com/files/clamz-0.5.tar.gz"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/clamz/clamz-0.5.tar.gz"
   sha256 "5a63f23f15dfa6c2af00ff9531ae9bfcca0facfe5b1aa82790964f050a09832b"
   revision 1
 
@@ -21,6 +21,6 @@ class Clamz < Formula
   end
 
   test do
-    system "#{bin}/clamz"
+    assert_match version.to_s, shell_output("#{bin}/clamz --version 2>&1")
   end
 end
