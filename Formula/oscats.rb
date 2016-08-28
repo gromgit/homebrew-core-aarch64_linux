6f7f5973ddadc9a66c10d1b0cac0a305fdb5f243
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 20:04:02 +0100

    oscats: migrate to archive URL/homepage

--- Formula/oscats.rb
+++ Formula/oscats.rb
@@ -1,7 +1,7 @@
 class Oscats < Formula
   desc "Computerized adaptive testing system"
-  homepage "https://code.google.com/p/oscats/"
-  url "https://oscats.googlecode.com/files/oscats-0.6.tar.gz"
+  homepage "https://code.google.com/archive/p/oscats/"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/oscats/oscats-0.6.tar.gz"
   sha256 "2f7c88cdab6a2106085f7a3e5b1073c74f7d633728c76bd73efba5dc5657a604"
 
   bottle do
@@ -12,13 +12,13 @@ class Oscats < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on :python => :optional
   depends_on "gsl"
   depends_on "glib"
+  depends_on :python => :optional
   depends_on "pygobject" if build.with? "python"
 
   def install
-    args = ["--disable-dependency-tracking", "--prefix=#{prefix}"]
+    args = %W[--disable-dependency-tracking --prefix=#{prefix}]
     args << "--enable-python-bindings" if build.with? "python"
     system "./configure", *args
     system "make", "install"
