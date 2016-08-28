bd70442f0d0a6dc0f8517a55503aa19226e69972
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 17:16:07 +0100

    cconv 0.6.3
    Closes #4297.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/cconv.rb
+++ Formula/cconv.rb
@@ -1,8 +1,8 @@
 class Cconv < Formula
   desc "Iconv based simplified-traditional Chinese conversion tool"
-  homepage "https://code.google.com/p/cconv/"
-  url "https://cconv.googlecode.com/files/cconv-0.6.2.tar.gz"
-  sha256 "f463da66c2ae18407441e12716f5f1c6cdea4e417ebfd475ec4c6dc6ad250c9d"
+  homepage "https://github.com/xiaoyjy/cconv"
+  url "https://github.com/xiaoyjy/cconv/archive/v0.6.3.tar.gz"
+  sha256 "82f46a94829f5a8157d6f686e302ff5710108931973e133d6e19593061b81d84"
 
   bottle do
     cellar :any
@@ -12,12 +12,15 @@ class Cconv < Formula
     sha256 "21adc67fe672719cbfc93e940d044a2b2ceb32653cc1a901b79e251c3fb6d090" => :mountain_lion
   end
 
-  def install
-    # fix link with iconv: https://code.google.com/p/cconv/issues/detail?id=18
-    inreplace "Makefile.in", "@ICONV_LIBS@", "@ICONV_LIBS@ -liconv"
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
+  depends_on "libtool" => :build
 
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+  def install
+    ENV.append "LDFLAGS", "-liconv"
+
+    system "autoreconf", "-fvi"
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install"
     rm_f include/"unicode.h"
   end
