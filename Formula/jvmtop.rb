dd92ba3ba024f01d1bccab9791165e70a6e04f3b
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 18:58:37 +0100

    jvmtop: upstream moved to GitHub

--- Formula/jvmtop.rb
+++ Formula/jvmtop.rb
@@ -1,10 +1,12 @@
 class Jvmtop < Formula
   desc "Console application for monitoring all running JVMs on a machine"
-  homepage "https://code.google.com/p/jvmtop/"
-  url "https://jvmtop.googlecode.com/files/jvmtop-0.8.0.tar.gz"
+  homepage "https://github.com/patric-r/jvmtop"
+  url "https://github.com/patric-r/jvmtop/releases/download/0.8.0/jvmtop-0.8.0.tar.gz"
   sha256 "f9de8159240b400a51b196520b4c4f0ddbcaa8e587fab1f0a59be8a00dc128c4"
 
   bottle :unneeded
+
+  depends_on :java
 
   def install
     rm Dir["*.bat"]
@@ -12,7 +14,6 @@ class Jvmtop < Formula
     chmod 0755, "jvmtop"
 
     libexec.install Dir["*"]
-
-    bin.write_exec_script libexec/"jvmtop"
+    (bin/"jvmtop").write_env_script(libexec/"jvmtop", Language::Java.java_home_env)
   end
 end
