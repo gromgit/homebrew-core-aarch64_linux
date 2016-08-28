3a306c63927aaea4790477f526e79e6478d497b2
ilovezfs <ilovezfs@icloud.com>
2016-08-28 09:03:55 -0700

    gnu-apl 1.6
    Closes #4296.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/gnu-apl.rb
+++ Formula/gnu-apl.rb
@@ -1,19 +1,9 @@
 class GnuApl < Formula
   desc "GNU implementation of the programming language APL"
   homepage "https://www.gnu.org/software/apl/"
-
-  stable do
-    url "https://ftpmirror.gnu.org/apl/apl-1.5.tar.gz"
-    mirror "https://ftp.gnu.org/gnu/apl/apl-1.5.tar.gz"
-    sha256 "013addd15cab829d3212fb787023a723e01f2e58e4836acbedbc2dc7078e8d40"
-
-    # Upstream fixes for clang build failures, the first of which is
-    # "non-ASCII characters are not allowed outside of literals and identifiers"
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/69c8268d/gnu-apl/clang-fixes.patch"
-      sha256 "1d2c3e6d97f26ea25bc1e0126a4968537e6d54044cf0a991e0477d462c0158bf"
-    end
-  end
+  url "https://ftpmirror.gnu.org/apl/apl-1.6.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/apl/apl-1.6.tar.gz"
+  sha256 "5e0da83048d81fd99330186f65309661f8070de2472851a8e639b3b7f7e7ff14"
 
   bottle do
     sha256 "fd46b7642d9bf9012dd2d8dc745131be3e1e49744892c17144bc2a1e90f81134" => :el_capitan
@@ -34,6 +24,9 @@ class GnuApl < Formula
   depends_on :macos => :mavericks
 
   def install
+    # Fix "LApack.cc:21:10: fatal error: 'malloc.h' file not found"
+    inreplace "src/LApack.cc", "malloc.h", "malloc/malloc.h"
+
     system "autoreconf", "-fiv" if build.head?
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
@@ -54,7 +47,7 @@ class GnuApl < Formula
     sleep 4
 
     begin
-      assert_match /Hello world/, shell_output("#{bin}/apl -s -f hello.apl")
+      assert_match "Hello world", shell_output("#{bin}/apl -s -f hello.apl")
     ensure
       Process.kill("SIGINT", pid)
       Process.wait(pid)
