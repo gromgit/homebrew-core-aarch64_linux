2a4dec4047a3b9020252d4a7ab715b724dacc866
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 19:06:14 +0100

    lib3ds: migrate to archive URL

--- Formula/lib3ds.rb
+++ Formula/lib3ds.rb
@@ -1,7 +1,7 @@
 class Lib3ds < Formula
   desc "Library for managing 3D-Studio Release 3 and 4 '.3DS' files"
-  homepage "https://code.google.com/p/lib3ds/"
-  url "https://lib3ds.googlecode.com/files/lib3ds-1.3.0.zip"
+  homepage "https://code.google.com/archive/p/lib3ds/"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/lib3ds/lib3ds-1.3.0.zip"
   sha256 "f5b00c302955a67fa5fb1f2d3f2583767cdc61fdbc6fd843c0c7c9d95c5629e3"
 
   bottle do
@@ -21,6 +21,6 @@ class Lib3ds < Formula
   test do
     # create a raw emtpy 3ds file.
     (testpath/"test.3ds").write("\x4d\x4d\x06\x00\x00\x00")
-    system "#{bin}/3dsdump", "test.3ds"
+    system bin/"3dsdump", "test.3ds"
   end
 end
