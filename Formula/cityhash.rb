2253a237af5445257a95a7d86022ea4e96634633
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 17:02:06 +0100

    cityhash: migrate to archive URL

--- Formula/cityhash.rb
+++ Formula/cityhash.rb
@@ -1,7 +1,7 @@
 class Cityhash < Formula
   desc "Hash functions for strings"
   homepage "https://github.com/google/cityhash"
-  url "https://cityhash.googlecode.com/files/cityhash-1.1.1.tar.gz"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/cityhash/cityhash-1.1.1.tar.gz"
   sha256 "76a41e149f6de87156b9a9790c595ef7ad081c321f60780886b520aecb7e3db4"
 
   bottle do
