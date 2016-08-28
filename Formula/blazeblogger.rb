0bd04b79f6717a762620c3b73bc9ad63c9a15f5e
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 16:55:42 +0100

    blazeblogger: migrate to archive URL

--- Formula/blazeblogger.rb
+++ Formula/blazeblogger.rb
@@ -1,7 +1,7 @@
 class Blazeblogger < Formula
   desc "CMS for the command-line"
   homepage "http://blaze.blackened.cz/"
-  url "https://blazeblogger.googlecode.com/files/blazeblogger-1.2.0.tar.gz"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/blazeblogger/blazeblogger-1.2.0.tar.gz"
   sha256 "39024b70708be6073e8aeb3943eb3b73d441fbb7b8113e145c0cf7540c4921aa"
 
   bottle do
