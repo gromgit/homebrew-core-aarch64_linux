28691d8ce91c0278039897eeb4c56b2c2ae585bf
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 19:49:19 +0100

    mp4v2: migrate to archive URL & add test

--- Formula/mp4v2.rb
+++ Formula/mp4v2.rb
@@ -1,7 +1,7 @@
 class Mp4v2 < Formula
   desc "Read, create, and modify MP4 files"
-  homepage "https://code.google.com/p/mp4v2/"
-  url "https://mp4v2.googlecode.com/files/mp4v2-2.0.0.tar.bz2"
+  homepage "https://code.google.com/archive/p/mp4v2/"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/mp4v2/mp4v2-2.0.0.tar.bz2"
   sha256 "0319b9a60b667cf10ee0ec7505eb7bdc0a2e21ca7a93db96ec5bd758e3428338"
 
   bottle do
@@ -17,6 +17,10 @@ class Mp4v2 < Formula
     system "./configure", "--disable-debug", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
-    system "make install-man"
+    system "make", "install-man"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/mp4art --version")
   end
 end
