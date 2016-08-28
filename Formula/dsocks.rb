f5a9024a96350e1d87a4e3a9a945218bb11b9493
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 17:51:02 +0100

    dsocks: migrate to archive URL

--- Formula/dsocks.rb
+++ Formula/dsocks.rb
@@ -1,8 +1,9 @@
 class Dsocks < Formula
   desc "SOCKS client wrapper for *BSD/OS X"
   homepage "https://monkey.org/~dugsong/dsocks/"
-  url "https://dsocks.googlecode.com/files/dsocks-1.8.tar.gz"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/dsocks/dsocks-1.8.tar.gz"
   sha256 "2b57fb487633f6d8b002f7fe1755480ae864c5e854e88b619329d9f51c980f1d"
+  head "https://github.com/dugsong/dsocks.git"
 
   bottle do
     cellar :any
@@ -12,7 +13,8 @@ class Dsocks < Formula
   end
 
   def install
-    system ENV.cc, ENV.cflags, "-shared", "-o", "libdsocks.dylib", "dsocks.c", "atomicio.c", "-lresolv"
+    system ENV.cc, ENV.cflags, "-shared", "-o", "libdsocks.dylib", "dsocks.c",
+                   "atomicio.c", "-lresolv"
     inreplace "dsocks.sh", "/usr/local", HOMEBREW_PREFIX
 
     lib.install "libdsocks.dylib"
