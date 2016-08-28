f4fb2207393aedf913f4e19b2b7cb48318e0d6b4
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 20:24:52 +0100

    shunit2: update homepage

--- Formula/shunit2.rb
+++ Formula/shunit2.rb
@@ -1,6 +1,6 @@
 class Shunit2 < Formula
   desc "xUnit unit testing framework for Bourne-based shell scripts"
-  homepage "https://code.google.com/archive/p/shunit2/"
+  homepage "https://github.com/kward/shunit2"
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/shunit2/shunit2-2.1.6.tgz"
   sha256 "65a313a76fd5cc1c58c9e19fbc80fc0e418a4cbfbd46d54b35ed5b6e0025d4ee"
 
@@ -11,6 +11,6 @@ class Shunit2 < Formula
   end
 
   test do
-    system "#{bin}/shunit2"
+    system bin/"shunit2"
   end
 end
