76776cafd4399e12a121701137f7123ab5a5a205
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 17:46:33 +0100

    dnsmap: migrate to archive URL

--- Formula/dnsmap.rb
+++ Formula/dnsmap.rb
@@ -1,7 +1,7 @@
 class Dnsmap < Formula
   desc "Passive DNS network mapper (a.k.a. subdomains bruteforcer)"
-  homepage "https://code.google.com/p/dnsmap/"
-  url "https://dnsmap.googlecode.com/files/dnsmap-0.30.tar.gz"
+  homepage "https://code.google.com/archive/p/dnsmap/"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/dnsmap/dnsmap-0.30.tar.gz"
   sha256 "fcf03a7b269b51121920ac49f7d450241306cfff23c76f3da94b03792f6becbc"
 
   bottle do
@@ -12,6 +12,11 @@ class Dnsmap < Formula
   end
 
   def install
-    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}", "BINDIR=#{bin}", "install"
+    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}",
+                   "BINDIR=#{bin}", "install"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/dnsmap", 1)
   end
 end
