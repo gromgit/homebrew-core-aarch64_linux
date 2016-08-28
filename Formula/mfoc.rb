eec9482b3bb468268bc727c3c14ca21e3f5b5269
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 19:42:14 +0100

    mfoc: migrate to archive URL/GitHub homepage

--- Formula/mfoc.rb
+++ Formula/mfoc.rb
@@ -1,7 +1,7 @@
 class Mfoc < Formula
   desc "Implementation of 'offline nested' attack by Nethemba"
-  homepage "https://code.google.com/p/mfoc/"
-  url "https://mfoc.googlecode.com/files/mfoc-0.10.7.tar.bz2"
+  homepage "https://github.com/nfc-tools/mfoc"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/mfoc/mfoc-0.10.7.tar.bz2"
   sha256 "93d8ac4cb0aa6ed94855ca9732a2ffd898a9095c087f12f9402205443c2eb98c"
 
   bottle do
