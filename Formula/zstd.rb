2f09b378dc9b7184aa6c846eaed6971221b05e62
Artem Zaytsev <arepo@nologin.ru>
2016-08-28 15:32:12 +0300

    zstd 0.8.1
    Closes #4293.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/zstd.rb
+++ Formula/zstd.rb
@@ -1,8 +1,8 @@
 class Zstd < Formula
   desc "Zstandard is a real-time compression algorithm"
   homepage "http://zstd.net/"
-  url "https://github.com/Cyan4973/zstd/archive/v0.8.0.tar.gz"
-  sha256 "297ef978fd956a503de6a303f7d58714de3300f602c7cf5e4b382a82f1483051"
+  url "https://github.com/Cyan4973/zstd/archive/v0.8.1.tar.gz"
+  sha256 "4632bee45988dd0fe3edf1e67bdf0a833895cbb1a7d1eb23ef0b7d753f8bffdd"
 
   bottle do
     cellar :any
