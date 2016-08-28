e236455177d07cbf331e9a145514e12edd7f3ca4
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-28 04:07:05 +0100

    plan9port: migrate to archive URL
    Closes https://github.com/Homebrew/homebrew-core/issues/4273.

--- Formula/plan9port.rb
+++ Formula/plan9port.rb
@@ -1,7 +1,7 @@
 class Plan9port < Formula
   desc "Many Plan 9 programs ported to UNIX-like operating systems"
   homepage "https://swtch.com/plan9port/"
-  url "https://plan9port.googlecode.com/files/plan9port-20140306.tgz"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/plan9port/plan9port-20140306.tgz"
   sha256 "cbb826cde693abdaa2051c49e7ebf75119bf2a4791fe3b3229f1ac36a408eaeb"
   head "https://github.com/9fans/plan9port.git"
 
@@ -22,13 +22,13 @@ class Plan9port < Formula
 
     system "./INSTALL"
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/9"
-    prefix.install Dir["#{libexec}/mac/*.app"]
+    bin.install_symlink libexec/"bin/9"
+    prefix.install Dir[libexec/"mac/*.app"]
   end
 
   def caveats; <<-EOS.undent
     In order not to collide with OSX system binaries, the Plan 9 binaries have
-    been installed to #{libexec}/bin.
+    been installed to #{opt_libexec}/bin.
     To run the Plan 9 version of a command simply call it through the command
     "9", which has been installed into the Homebrew prefix bin.  For example,
     to run Plan 9's ls run:
@@ -46,8 +46,8 @@ class Plan9port < Formula
         return printf("Hello World\\n");
       }
     EOS
-    system "#{bin}/9", "9c", "test.c"
-    system "#{bin}/9", "9l", "-o", "test", "test.o"
-    assert_equal "Hello World\n", `./test`
+    system bin/"9", "9c", "test.c"
+    system bin/"9", "9l", "-o", "test", "test.o"
+    assert_equal "Hello World\n", shell_output("./test", 1)
   end
 end
