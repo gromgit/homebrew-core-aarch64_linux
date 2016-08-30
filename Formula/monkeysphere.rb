72f5398e68de6f410cd8dc09e5edae04dbd3f0cd
ilovezfs <ilovezfs@icloud.com>
2016-08-30 00:50:26 -0700

    monkeysphere 0.39
    Closes #4346.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/monkeysphere.rb
+++ Formula/monkeysphere.rb
@@ -1,11 +1,9 @@
 class Monkeysphere < Formula
   desc "Use the OpenPGP web of trust to verify ssh connections"
   homepage "http://web.monkeysphere.info/"
-  url "http://archive.monkeysphere.info/debian/pool/monkeysphere/m/monkeysphere/monkeysphere_0.38.orig.tar.gz"
-  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/m/monkeysphere/monkeysphere_0.38.orig.tar.gz"
-  sha256 "6951821d11ad646e6e7651d677bfab8f800fbc52703a0ab9942d03cd13959073"
-  revision 1
-
+  url "http://archive.monkeysphere.info/debian/pool/monkeysphere/m/monkeysphere/monkeysphere_0.39.orig.tar.gz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/m/monkeysphere/monkeysphere_0.39.orig.tar.gz"
+  sha256 "73331e2df361b22e1dc6445a7d2b0b2c5a124daa4d850c2ecce721579592c29f"
   head "git://git.monkeysphere.info/monkeysphere"
 
   bottle do
