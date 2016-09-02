813df4bb723093b785777c0880a9548a8f9702c2
ilovezfs <ilovezfs@icloud.com>
2016-09-01 23:41:34 -0700

    gtk-gnutella 1.1.10
    Closes #4431.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gtk-gnutella.rb
+++ Formula/gtk-gnutella.rb
@@ -1,8 +1,8 @@
 class GtkGnutella < Formula
   desc "Share files in a peer-to-peer (P2P) network"
   homepage "http://gtk-gnutella.sourceforge.net/en/?page=news"
-  url "https://downloads.sourceforge.net/project/gtk-gnutella/gtk-gnutella/1.1.9/gtk-gnutella-1.1.9.tar.bz2"
-  sha256 "3b2e913969834a57edd93d22733646037b2a141172d5d4a240f5d6a9f56b6aff"
+  url "https://downloads.sourceforge.net/project/gtk-gnutella/gtk-gnutella/1.1.10/gtk-gnutella-1.1.10.tar.bz2"
+  sha256 "95e2a030124e7cc05e19e351eebc16f40f4520381b7bb306e8b940ed4e2e6409"
 
   bottle do
     sha256 "39fc56e5cc07dab27ce43cbb2a76b739fce8dc77cf6b1b1ad11e43c8440fd3ee" => :el_capitan
