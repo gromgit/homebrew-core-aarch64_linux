9a9f2c1750d1a313324a607173c83f3d96317ca1
ilovezfs <ilovezfs@icloud.com>
2016-09-02 09:39:46 -0700

    gputils 1.5.0
    Closes #4451.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gputils.rb
+++ Formula/gputils.rb
@@ -1,19 +1,13 @@
 class Gputils < Formula
   desc "GNU PIC Utilities"
   homepage "http://gputils.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/gputils/gputils/1.4.2/gputils-1.4.2-1.tar.gz"
-  sha256 "e27b5c5ef3802a9c6c4a859d9ac2c380c31b3a2d6d6880718198bd1139b71271"
+  url "https://downloads.sourceforge.net/project/gputils/gputils/1.5.0/gputils-1.5.0.tar.gz"
+  sha256 "f6a517c186b991f504be5e4585316871d5950568257885d37487bb368dc76227"
 
   bottle do
     sha256 "32a73229f86fc8a3bfbbd4582f435c9c92f0b7335e523f3b377d2528631b42b8" => :el_capitan
     sha256 "169a9bcc46bc5e57c7fe9c688ae7bd7f9afc157a222948179133a47d6d2038b4" => :yosemite
     sha256 "6d085798dce7be27385f28b97893d4bb3f44b0d02983e75865c7c6a54b3e5f5b" => :mavericks
-  end
-
-  devel do
-    url "https://downloads.sourceforge.net/project/gputils/gputils/1.5.0/gputils-1.5.0_RC6.tar.bz2"
-    version "1.5.0-rc6"
-    sha256 "b4cda94b4b256bd51a45229da034b80950209d375f50308131378444f8f223a9"
   end
 
   def install
