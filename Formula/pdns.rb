35828eba479066f29281a8e4e28cdd390184aa63
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-02 04:01:15 +0100

    pdns: update 4.0.1 bottle.

--- Formula/pdns.rb
+++ Formula/pdns.rb
@@ -5,9 +5,10 @@ class Pdns < Formula
   sha256 "d191eed4a6664430e85969f49835c59e810ecbb7b3eb506e64c6b2734091edd7"
 
   bottle do
-    sha256 "a4c88ba530128cff489e1267e6970c89ec1019248d33cc033e1a2672f7b9e7ec" => :el_capitan
-    sha256 "45dd89931b7b94e779ccdec204dac373fa9a3b5869944cfbf01b3a150c59f730" => :yosemite
-    sha256 "69ed11aa0eb0e223e498f8a2181322a706349ecd95d3a6a02cd670c6d2dbbe1b" => :mavericks
+    rebuild 1
+    sha256 "3ad6cd394a15d6aef7d68648103e934d1cd172733346ed2a72a1f0ddffd0a8df" => :el_capitan
+    sha256 "3f7651090d99aa1b1fe2d7dd317b936f7c8c357ff0e6c0a5587b98715b7b4658" => :yosemite
+    sha256 "e11c02d5f242205cd1190bcdd32a1cc96649e2ee7f0ec72039422c6993452179" => :mavericks
   end
 
   head do
