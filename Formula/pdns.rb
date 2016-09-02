c417c422bd9e881b424ca17d0a326c570657fa03
opteryxl <opteryxl@local>
2016-09-01 19:34:51 -0700

    pdns: add plist
    add launchd init script for running pdns as a daemon
    
    Closes #4428.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/pdns.rb
+++ Formula/pdns.rb
@@ -54,6 +54,31 @@ class Pdns < Formula
     system "make", "install"
   end
 
+  plist_options :manual => "pdns_server start"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <true/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/pdns_server</string>
+      </array>
+      <key>EnvironmentVariables</key>
+      <key>KeepAlive</key>
+      <true/>
+      <key>SHAuthorizationRight</key>
+      <string>system.preferences</string>
+    </dict>
+    </plist>
+    EOS
+  end
+
   test do
     output = shell_output("#{sbin}/pdns_server --version 2>&1", 99)
     assert_match "PowerDNS Authoritative Server #{version}", output
