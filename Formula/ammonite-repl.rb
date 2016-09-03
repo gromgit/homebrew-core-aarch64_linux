c4500fe5abd43e47fb7c5ef5d496a4d25f7c8cca
ilovezfs <ilovezfs@icloud.com>
2016-09-02 21:51:06 -0700

    ammonite-repl 0.7.6
    Closes #4467.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/ammonite-repl.rb
+++ Formula/ammonite-repl.rb
@@ -1,8 +1,8 @@
 class AmmoniteRepl < Formula
   desc "Ammonite is a cleanroom re-implementation of the Scala REPL"
   homepage "https://lihaoyi.github.io/Ammonite/#Ammonite-REPL"
-  url "https://github.com/lihaoyi/Ammonite/releases/download/0.7.5/0.7.5", :using => :nounzip
-  sha256 "a45cbc82aac4e903ad1ab9b6cfc991775a87314b52939c61ad7c7ced67e2254b"
+  url "https://github.com/lihaoyi/Ammonite/releases/download/0.7.6/0.7.6", :using => :nounzip
+  sha256 "2e048f5bceaa56ad0ce8c5af32b12761d6a64669b7aafb59cdc4bb4c18b02a16"
 
   bottle :unneeded
 
@@ -13,8 +13,8 @@ class AmmoniteRepl < Formula
   end
 
   test do
-    ENV.java_cache
+    ENV["_JAVA_OPTIONS"] = "-Duser.home=#{testpath}"
     output = shell_output("#{bin}/amm -c 'print(\"hello world!\")'")
-    assert_equal "hello world!", output
+    assert_equal "hello world!", output.lines.last
   end
 end
