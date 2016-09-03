c620c4b9405e8e41f7023a27c5ebd4fee157117a
ilovezfs <ilovezfs@icloud.com>
2016-09-02 21:36:51 -0700

    sonar-runner 2.7
    Closes #4463.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/sonar-runner.rb
+++ Formula/sonar-runner.rb
@@ -1,8 +1,8 @@
 class SonarRunner < Formula
   desc "Launcher to analyze a project with SonarQube"
   homepage "http://docs.sonarqube.org/display/SONAR/Analyzing+with+SonarQube+Scanner"
-  url "https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-2.6.1.zip"
-  sha256 "471dabb8b2c87f6080b61edfbba7c0a4103d18a71666dc44f03808c0e3714104"
+  url "https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-2.7.zip"
+  sha256 "cc13c5f68e8443e28fddd899becd1343c2a3880c3aadc94267cdb8280f1c9d63"
   head "https://github.com/SonarSource/sonar-runner.git"
 
   bottle :unneeded
