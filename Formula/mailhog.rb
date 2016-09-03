cef0b6cf21c56e763e98b3bb6b1c60a372e4caae
Dominyk Tiller <dominyktiller@gmail.com>
2016-09-03 21:09:54 +0100

    mailhog: fix audit nit

--- Formula/mailhog.rb
+++ Formula/mailhog.rb
@@ -40,6 +40,8 @@ class Mailhog < Formula
     end
   end
 
+  plist_options :manual => "MailHog"
+
   def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
