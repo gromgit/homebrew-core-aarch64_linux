f610cb7975c2e6707f3657a7f77507586a8c7237
ilovezfs <ilovezfs@icloud.com>
2016-09-03 22:24:57 -0700

    mbelib 1.3.0
    run "make test"
    add a test
    
    Closes #4499.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mbelib.rb
+++ Formula/mbelib.rb
@@ -1,8 +1,8 @@
 class Mbelib < Formula
   desc "P25 Phase 1 and ProVoice vocoder"
   homepage "https://github.com/szechyjs/mbelib"
-  url "https://github.com/szechyjs/mbelib/archive/v1.2.5.tar.gz"
-  sha256 "59d5e821b976a57f1eae84dd57ba84fd980d068369de0bc6a75c92f0b286c504"
+  url "https://github.com/szechyjs/mbelib/archive/v1.3.0.tar.gz"
+  sha256 "5a2d5ca37cef3b6deddd5ce8c73918f27936c50eb0e63b27e4b4fc493310518d"
   head "https://github.com/szechyjs/mbelib.git"
 
   bottle do
@@ -20,7 +20,23 @@ class Mbelib < Formula
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
       system "make"
+      system "make", "test"
       system "make", "install"
     end
+  end
+
+  test do
+    (testpath/"mb.cpp").write <<-EOS.undent
+      extern "C" {
+      #include "mbelib.h"
+      }
+      int main() {
+        float float_buf[160] = {1.23f, -1.12f, 4680.412f, 4800.12f, -4700.74f};
+        mbe_synthesizeSilencef(float_buf);
+        return (float_buf[0] != 0);
+      }
+    EOS
+    system ENV.cxx, "mb.cpp", "-o", "test", "-L#{lib}", "-lmbe"
+    system "./test"
   end
 end
