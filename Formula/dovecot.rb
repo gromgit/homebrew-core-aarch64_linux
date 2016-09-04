5f74759162a69d8fab6e859ee7a56e5048dc45fd
Ashish SHUKLA <ashish.is@lostca.se>
2016-09-04 15:04:01 +0530

    dovecot 2.2.25
    With dovecot-pigeonhole 0.4.15
    
    Closes #4506.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/dovecot.rb
+++ Formula/dovecot.rb
@@ -1,9 +1,9 @@
 class Dovecot < Formula
   desc "IMAP/POP3 server"
   homepage "http://dovecot.org/"
-  url "http://dovecot.org/releases/2.2/dovecot-2.2.24.tar.gz"
-  mirror "https://fossies.org/linux/misc/dovecot-2.2.24.tar.gz"
-  sha256 "71c86891ea8deb5703d3dbbc3ea31ce2cbf7638f1aa395d9e8794d3ff7aebeb7"
+  url "http://dovecot.org/releases/2.2/dovecot-2.2.25.tar.gz"
+  mirror "https://fossies.org/linux/misc/dovecot-2.2.25.tar.gz"
+  sha256 "d8d9f32c846397f7c22749a84c5cf6f59c55ff7ded3dc9f07749a255182f9667"
 
   bottle do
     sha256 "6ce8e803e9928bbaa63f5e1a5bc86e035138b02fd5b8c9337eab860ed1344fb8" => :el_capitan
@@ -20,8 +20,8 @@ class Dovecot < Formula
   depends_on "clucene" => :optional
 
   resource "pigeonhole" do
-    url "http://pigeonhole.dovecot.org/releases/2.2/dovecot-2.2-pigeonhole-0.4.13.tar.gz"
-    sha256 "7fd187b8393a5048b302f90ad84adc7bf6e73bf79fd8d22a1c1aaa71f836a910"
+    url "http://pigeonhole.dovecot.org/releases/2.2/dovecot-2.2-pigeonhole-0.4.15.tar.gz"
+    sha256 "c99ace6ead310c6c3b639922da618f90d846307da4fe252d994e5e51bf8a3de3"
   end
 
   resource "stemmer" do
