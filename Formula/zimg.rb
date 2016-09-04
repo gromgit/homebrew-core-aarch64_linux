e43c40ddd1c1f1498e0706e199e3a0c5de8d7df2
ilovezfs <ilovezfs@icloud.com>
2016-09-04 09:47:49 -0700

    zimg 2.2.1
    fix "error: use of undeclared identifier '_mm256_cvtph_ps'"
    add head spec
    specify cxx11 for < mavericks
    
    Closes #4109.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/zimg.rb
+++ Formula/zimg.rb
@@ -1,8 +1,9 @@
 class Zimg < Formula
   desc "Scaling, colorspace conversion, and dithering library"
   homepage "https://github.com/sekrit-twc/zimg"
-  url "https://github.com/sekrit-twc/zimg/archive/release-2.1.tar.gz"
-  sha256 "09093bbb4d73865362e1e346762a6efdc9acc3d2cab6a2ebf5f00ba5d90b17c3"
+  url "https://github.com/sekrit-twc/zimg/archive/release-2.2.1.tar.gz"
+  sha256 "0ac3004f7612f91e2eda1aaaf170c2ceef4f90f881647b8e248f36b0e6954f54"
+  head "https://github.com/sekrit-twc/zimg.git"
 
   bottle do
     cellar :any
@@ -15,7 +16,22 @@ class Zimg < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
 
+  needs :cxx11 if MacOS.version < :mavericks
+
   def install
+    ENV.cxx11 if MacOS.version < :mavericks
+
+    # Fixes "error: use of undeclared identifier '_mm256_cvtph_ps'"
+    # See https://reviews.llvm.org/rL254528 for the underlying cause
+    # Reported 4 Sep 2016 https://github.com/sekrit-twc/zimg/issues/52
+    if MacOS.version < :el_capitan
+      (buildpath/"brew_include/immintrin.h").write <<-EOS.undent
+        #include <x86intrin.h>
+        #include_next <immintrin.h>
+      EOS
+      ENV.prepend "CPPFLAGS", "-I#{buildpath}/brew_include"
+    end
+
     system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
