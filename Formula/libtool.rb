0e9aceeeb8c44fc579e880652e1a100c93d11edc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-05 21:12:24 +0100

    libtool: update 2.4.6_1 bottle.

--- Formula/libtool.rb
+++ Formula/libtool.rb
@@ -12,6 +12,7 @@ class Libtool < Formula
 
   bottle do
     cellar :any
+    sha256 "78a1f6c6644eae01eb5c204ef705f7e48721a0fe8ece492c10c84791061885db" => :sierra
     sha256 "b7651d0a082e2f103f03ca3a5ed831e2ff5655ccc1044ac0452e4d1825475a35" => :el_capitan
     sha256 "0eb206c0f51e8ce2e3e9340b5ce3c8ecef961ae6696f676073327a7ac04e5c0b" => :yosemite
     sha256 "2e51ef82ef2bd1ad9d921a9016b9e5d7fa82d131849e2c32a3c90daa119e2eda" => :mavericks
