541180dd6a14efea171f9b091c99d30bd059850b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-05 20:46:20 +0100

    pcre: update 8.39 bottle.

--- Formula/pcre.rb
+++ Formula/pcre.rb
@@ -7,6 +7,7 @@ class Pcre < Formula
 
   bottle do
     cellar :any
+    sha256 "c1be1b0c15e9e8f9cd568724618952d136612baccc66901317e877277ebd7230" => :sierra
     sha256 "a9333dda2e7b2f2a3f0de5e542d24f4cd0750edc53bfaa6bbf34f0d03db5fb3c" => :el_capitan
     sha256 "202f05c7d2dc78c30ef7115395ec0bf7bc3f40bfb2dd1704ca1a01ff1661142c" => :yosemite
     sha256 "d2bb8776d8dca524aa1cbbb4f18b156658d673fe5424daeb96100b01b55805dd" => :mavericks
