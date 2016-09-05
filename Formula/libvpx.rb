e5bd790d77c5c6e817accd5c16ba5dc5ae47493e
ilovezfs <ilovezfs@icloud.com>
2016-09-04 19:37:48 -0700

    libvpx 1.6.0
    Closes #4526.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libvpx.rb
+++ Formula/libvpx.rb
@@ -1,8 +1,8 @@
 class Libvpx < Formula
   desc "VP8 video codec"
   homepage "https://www.webmproject.org/code/"
-  url "https://github.com/webmproject/libvpx/archive/v1.5.0.tar.gz"
-  sha256 "f199b03b67042e8d94a3ae8bc841fb82b6a8430bdf3965aeeaafe8245bcfa699"
+  url "https://github.com/webmproject/libvpx/archive/v1.6.0.tar.gz"
+  sha256 "e2fc00c9f60c76f91a1cde16a2356e33a45b76a5a5a1370df65fd57052a4994a"
   head "https://chromium.googlesource.com/webm/libvpx", :using => :git
 
   bottle do
