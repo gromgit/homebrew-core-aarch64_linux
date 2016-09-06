01ea35836d3ad652178b235d46d24fe8a57b6bd6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-06 10:05:41 +0100

    gmp: update 6.1.1 bottle.

--- Formula/gmp.rb
+++ Formula/gmp.rb
@@ -7,6 +7,7 @@ class Gmp < Formula
 
   bottle do
     cellar :any
+    sha256 "7ff60fa13a22a22503566229b3e6b36244900a0ec6f5f897c18a7ef48f44b274" => :sierra
     sha256 "3d404810952ae59470709709f0b64e4122520afcbb65633d449bcca546f3ca41" => :el_capitan
     sha256 "8858a1e22d62875d5424b354302c818b194c8d21a669287d0e0b190816be32e9" => :yosemite
     sha256 "b604a20e2501a10773abc384d9750fa27112cf9ad9c532367ebd7caf241c8d4b" => :mavericks
