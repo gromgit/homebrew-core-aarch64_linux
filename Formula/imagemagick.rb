1dd6cba0186571c0b26e059e5fa52b06e65edd91
Zhiming Wang <zmwangx@gmail.com>
2016-09-06 19:29:35 -0400

    imagemagick: improve test
    - Actually check the output of identify;
    - Check support for recommended features and delegates (will fail if
      built --without certain recommends; salvaged from #3511).
    
    Closes #4570.
    
    Signed-off-by: Zhiming Wang <zmwangx@gmail.com>

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -139,6 +139,11 @@ class Imagemagick < Formula
   end
 
   test do
-    system "#{bin}/identify", test_fixtures("test.png")
+    assert_match "PNG", shell_output("#{bin}/identify #{test_fixtures("test.png")}")
+    # Check support for recommended features and delegates.
+    features = shell_output("#{bin}/convert -version")
+    %W[Modules freetype jpeg png tiff].each do |feature|
+      assert_match feature, features
+    end
   end
 end
