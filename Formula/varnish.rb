a732384ae65059a41f3fb95ee33e7c6d96bbacde
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-06 18:39:16 +0100

    varnish: update 4.1.0 bottle.

--- Formula/varnish.rb
+++ Formula/varnish.rb
@@ -5,9 +5,10 @@ class Varnish < Formula
   sha256 "4a6ea08e30b62fbf25f884a65f0d8af42e9cc9d25bf70f45ae4417c4f1c99017"
 
   bottle do
-    sha256 "0eda704c372f73437ee8b29d3d4183f69672e074ceceadae56b630d9f86499e8" => :el_capitan
-    sha256 "aaf399bcddbaec19f164834f6a514a573f9a6016048805729693ff199267ad06" => :yosemite
-    sha256 "b5fda8068aa76e6f580c334ada855796d911a6223150c782696c014968abed51" => :mavericks
+    rebuild 1
+    sha256 "ad41b596be38cc6fd5dcaef1b8108ad6ebb8cdded562f0fa59c81daa9cff0c93" => :el_capitan
+    sha256 "1c5b01ad96283950662fecb65254274a6c8481f4d130e1f1dbac1af821abec4a" => :yosemite
+    sha256 "270560b2d7bf43f660c9b126b68ce8df6f2226f93a472615d1c29e186d84da0a" => :mavericks
   end
 
   depends_on "pkg-config" => :build
