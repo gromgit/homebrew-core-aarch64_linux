fd431b65792b47d6f79a59bad725e0950de5c63f
Grant Ammons <gammons@gmail.com>
2016-09-07 11:08:18 -0400

    todolist 0.3.1
    Closes #4582.
    
    Signed-off-by: Zhiming Wang <zmwangx@gmail.com>

--- Formula/todolist.rb
+++ Formula/todolist.rb
@@ -1,10 +1,8 @@
-require "language/go"
-
 class Todolist < Formula
   desc "Very fast, simple task manager for the command-line, based upon GTD."
   homepage "http://todolist.site"
-  url "https://github.com/gammons/todolist/archive/0.3.0.tar.gz"
-  sha256 "3ec300a8f900c6c75d21a7178df73778b7572b8936fe0b2a73317941d6e8fa04"
+  url "https://github.com/gammons/todolist/archive/0.3.1.tar.gz"
+  sha256 "4d6587fba04e12793642df749f2c923d352c742c1409980820e6a51f6eec992f"
 
   bottle do
     cellar :any_skip_relocation
@@ -14,39 +12,12 @@ class Todolist < Formula
   end
 
   depends_on "go" => :build
-
-  go_resource "github.com/mattn/go-isatty" do
-    url "https://github.com/mattn/go-isatty.git",
-    :revision => "56b76bdf51f7708750eac80fa38b952bb9f32639"
-  end
-
-  go_resource "github.com/mattn/go-colorable" do
-    url "https://github.com/mattn/go-colorable.git",
-    :revision => "9056b7a9f2d1f2d96498d6d146acd1f9d5ed3d59"
-  end
-
-  go_resource "github.com/fatih/color" do
-    url "https://github.com/fatih/color.git",
-    :revision => "87d4004f2ab62d0d255e0a38f1680aa534549fe3"
-  end
-
-  go_resource "github.com/jinzhu/now" do
-    url "https://github.com/jinzhu/now.git",
-    :revision => "d9861969c7a7e84746d341c09020c2ef8a617f8f"
-  end
-
-  go_resource "github.com/julienschmidt/httprouter" do
-    url "https://github.com/julienschmidt/httprouter.git",
-    :revision => "77366a47451a56bb3ba682481eed85b64fea14e8"
-  end
+  depends_on "govendor" => :build
 
   def install
     ENV["GOPATH"] = buildpath
-
-    Language::Go.stage_deps resources, buildpath/"src"
     (buildpath/"src/github.com/gammons/").mkpath
     ln_s buildpath, buildpath/"src/github.com/gammons/todolist"
-
     system "go", "build", "-o", bin/"todolist", "./src/github.com/gammons/todolist"
   end
 
