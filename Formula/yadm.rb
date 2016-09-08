03d886ced70a593a76ee48e1d552e94572f0b21f
Tim Byrne <sultan@locehilios.com>
2016-09-08 10:00:11 -0500

    yadm 1.05
    Closes #4626.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/yadm.rb
+++ Formula/yadm.rb
@@ -1,8 +1,8 @@
 class Yadm < Formula
   desc "Yet Another Dotfiles Manager"
-  homepage "https://github.com/TheLocehiliosan/yadm"
-  url "https://github.com/TheLocehiliosan/yadm/archive/1.04.tar.gz"
-  sha256 "a73aa51245866ce67aeb4322a62995ebbb13f29dc35508f486819dceb534968a"
+  homepage "https://thelocehiliosan.github.io/yadm/"
+  url "https://github.com/TheLocehiliosan/yadm/archive/1.05.tar.gz"
+  sha256 "2989b458f62cd08a65ee4f43e7ecf18f539b52d4bbb20c52b0ecff619f5e76cd"
 
   bottle :unneeded
 
