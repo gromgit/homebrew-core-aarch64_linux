fdfe2445d01beb9e7ceb2c45df7f7bdf3da61380
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-08 12:06:36 +0100

    lean-cli: update 0.2.1 bottle.

--- Formula/lean-cli.rb
+++ Formula/lean-cli.rb
@@ -7,6 +7,7 @@ class LeanCli < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ee0342c354c884c1d44eaaa17a8ba887a5cdb4db094ffcb2a7671670bbb1be7b" => :sierra
     sha256 "c51421d7d5df89f771fe8497d0e64664654cdce2edbfa6bfab5c721d46039f09" => :el_capitan
     sha256 "093eb39ded8f6bda9182a5b53a25379e05f9ee3fe3b08a44095a6c1338d33db2" => :yosemite
     sha256 "8ad823f80addefadf372df10c2343af9181ec13af70b11634559eca45524dcc0" => :mavericks
