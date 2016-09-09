568a43796be5a53e1968fb0ab45e9e3be1a4210c
Andrew Janke <andrew@apjanke.net>
2016-09-09 17:40:01 -0400

    protobuf-c: audit fix

--- Formula/protobuf-c.rb
+++ Formula/protobuf-c.rb
@@ -3,13 +3,13 @@ class ProtobufC < Formula
   homepage "https://github.com/protobuf-c/protobuf-c"
   url "https://github.com/protobuf-c/protobuf-c/releases/download/v1.2.1/protobuf-c-1.2.1.tar.gz"
   sha256 "846eb4846f19598affdc349d817a8c4c0c68fd940303e6934725c889f16f00bd"
+  revision 1
 
   bottle do
     sha256 "bcd2adfa85afe9057c35b971d796396b3cc77f3698ffff167c166c54bbc56313" => :el_capitan
     sha256 "cdadcb2c34ea509130bd927d506acb7b4016de77f39419c07fbd629e3cfebb1f" => :yosemite
     sha256 "d17bdc061c6f65c82a9fc7a468a8748aed5e262fd7b81df0a69da80620cb2a01" => :mavericks
   end
-  revision 1
 
   option :universal
 
