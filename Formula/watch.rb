970c3ee5c7247930a8296bf14e6436ca17003157
David Selassie <selassid@gmail.com>
2016-09-09 15:58:37 -0700

    watch 3.3.12
    Closes #4670.
    
    Signed-off-by: Zhiming Wang <zmwangx@gmail.com>

--- Formula/watch.rb
+++ Formula/watch.rb
@@ -5,16 +5,14 @@ class Watch < Formula
 
   stable do
     url "https://gitlab.com/procps-ng/procps.git",
-      :tag => "v3.3.11",
-      :revision => "de985eced583f18df273146b110491b0f7404aab"
+      :tag => "v3.3.12",
+      :revision => "e0784ddaed30d095bb1d9a8ad6b5a23d10a212c4"
 
-    # Upstream commit, which (probably inadvertently) fixes the error
-    # "conflicting types for 'user_from_uid"
-    # Commit subject is "watch: Correctly process [m Remove lib dependency"
-    # Preexisting upstream report: https://gitlab.com/procps-ng/procps/issues/34
+    # Upstream commit, which fixes missing HOST_NAME_MAX on BSD-y systems.
+    # Commit subject is "watch: define HOST_NAME_MAX"
     patch do
-      url "https://gitlab.com/procps-ng/procps/commit/99fa7f9f.diff"
-      sha256 "7f907db30f4777746224506b120d5d402c01073fbd275e83d37259a8eb4f62b1"
+      url "https://gitlab.com/procps-ng/procps/commit/e564ddcb01c3c11537432faa9c7a7a6badb05930.diff"
+      sha256 "3a4664e154f324e93b2a8e453a12575b94aac9eb9d86831649731d0f1a7f869e"
     end
   end
 
