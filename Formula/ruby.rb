83b22eeb4744d367794b31fe7fe2ec42ceeace8a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-09 22:47:01 +0100

    ruby: update 2.3.1 bottle.

--- Formula/ruby.rb
+++ Formula/ruby.rb
@@ -18,6 +18,7 @@ class Ruby < Formula
   end
 
   bottle do
+    sha256 "b6c5bc13963494454101687611b4f35f2e7221addcf667af65a82aa3417d1b6c" => :sierra
     sha256 "e40f882e477f0e97c1650d952af368274f4df994ecca66db4b1146b56fbb4f24" => :el_capitan
     sha256 "f0dfef7b1f179d4a3f39a8bc910938f0c838dc9a7c5399d3a5dbb2367bd8ddbf" => :yosemite
     sha256 "6825cfee4ed3625d3b2c99d2de7ee22c59a70c0ea1efbf1883de966881ddf68b" => :mavericks
