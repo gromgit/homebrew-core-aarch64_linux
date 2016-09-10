92c7e72ff1adc70d6c5a18e578ca887ee3556988
ilovezfs <ilovezfs@icloud.com>
2016-09-09 23:14:41 -0700

    antigen 1.1.0
    Closes #4686.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/antigen.rb
+++ Formula/antigen.rb
@@ -1,14 +1,14 @@
 class Antigen < Formula
   desc "Plugin manager for zsh, inspired by oh-my-zsh and vundle."
   homepage "http://antigen.sharats.me/"
-  url "https://github.com/zsh-users/antigen/archive/v1.0.4.tar.gz"
-  sha256 "ab83436644bbcaf028b7cff60a6fcc8ba4904b3f6f376557848e07df4afe1555"
+  url "https://github.com/zsh-users/antigen/archive/v1.1.0.tar.gz"
+  sha256 "0e4dc76addb28bace5940ea762e7b699e458731d24e6ba32809845e6c771ced9"
   head "https://github.com/zsh-users/antigen.git"
 
   bottle :unneeded
 
   def install
-    pkgshare.install "antigen.zsh"
+    pkgshare.install "bin/antigen.zsh"
   end
 
   def caveats; <<-EOS.undent
