2a55058de467b4c46cbb8c7a1c4afdf738050774
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-10 17:38:39 +0100

    jemalloc: update 4.2.1 bottle.

--- Formula/jemalloc.rb
+++ Formula/jemalloc.rb
@@ -8,6 +8,7 @@ class Jemalloc < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "70c10932afc1f99132505300ab9b4b9bee66713b767da3ea7e78079cdf970de1" => :sierra
     sha256 "3a7d4d62fa79b02d9497ce12996f5f3184d40ef982c7d33c141c553e85570658" => :el_capitan
     sha256 "b9f4ef12ac5f78f0981f728d955a6d70b3a7fe952ea967433178addc30291669" => :yosemite
     sha256 "c6f28a796d10776f88c9909ece4cd3baa832d2f90bf654a49c8b7a0811597b35" => :mavericks
