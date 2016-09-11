c733bd521855cc1f847d0d273f1b82f8215c95aa
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-11 21:05:33 +0100

    nmap: update 7.12 bottle.

--- Formula/nmap.rb
+++ Formula/nmap.rb
@@ -6,6 +6,7 @@ class Nmap < Formula
   head "https://guest:@svn.nmap.org/nmap/", :using => :svn
 
   bottle do
+    sha256 "eb56ae006d639d6b524ec8fa9c47cb5166e3cd119f107a835d1a12b575f52e4d" => :sierra
     sha256 "1225505f7b77cafa55d300adffc2e1c5945d090a20cb18c417bead62a2eb0409" => :el_capitan
     sha256 "a57b65e0c14e232b332b332bbaa5e3ab9b07b4b07e9e3f36dc1cc6afbb4d42f9" => :yosemite
     sha256 "be7ba56789fa1aa900396955d2a99c1929907ca8df1a9ccd37081d5178c440de" => :mavericks
