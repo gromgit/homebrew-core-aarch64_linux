c4ee0699b7f1bb4c81bf1337cbc721afec4f0fa0
Dominyk Tiller <dominyktiller@gmail.com>
2016-09-11 18:53:18 +0100

    guile: add build hack for Sierra
    Also bump the revision to resolve some issues detailed in https://github.com/Homebrew/brew/pull/939.

--- Formula/guile.rb
+++ Formula/guile.rb
@@ -1,10 +1,10 @@
 class Guile < Formula
-  desc "GUILE: GNU Ubiquitous Intelligent Language for Extensions"
+  desc "GNU Ubiquitous Intelligent Language for Extensions"
   homepage "https://www.gnu.org/software/guile/"
   url "https://ftpmirror.gnu.org/guile/guile-2.0.11.tar.xz"
   mirror "https://ftp.gnu.org/pub/gnu/guile/guile-2.0.11.tar.xz"
   sha256 "aed0a4a6db4e310cbdfeb3613fa6f86fddc91ef624c1e3f8937a6304c69103e2"
-  revision 2
+  revision 3
 
   bottle do
     sha256 "d7e7ad8d491f84c1405b82ee8ef0da5b21f551b6a0f2795bae92e8bec2f19be2" => :el_capitan
@@ -17,6 +17,7 @@ class Guile < Formula
     url "http://git.savannah.gnu.org/r/guile.git",
         :tag => "v2.1.2",
         :revision => "d236022eb0d285af3d462de9e99a212eba459df2"
+
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "gettext" => :build
@@ -46,6 +47,15 @@ class Guile < Formula
   fails_with :clang do
     build 211
     cause "Segfaults during compilation"
+  end
+
+  # http://debbugs.gnu.org/cgi/bugreport.cgi?bug=23870
+  # https://github.com/Homebrew/homebrew-core/issues/1957#issuecomment-229347476
+  if MacOS.version >= :sierra
+    patch do
+      url "https://gist.githubusercontent.com/rahulg/baa500e84136f0965e9ade2fb36b90ba/raw/4f1081838972ac9621fc68bb571daaf99fc0c045/libguile-stime-sierra.patch"
+      sha256 "ff38aa01fe2447bc74ccb6297d2832d0a224ceeb8f00e3a1ca68446d6b1d0f6e"
+    end
   end
 
   def install
