76c27788f0e9b3e380a66f3a1534391d965c0d3c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-11 21:39:50 +0100

    cscope: update 15.8b bottle.

--- Formula/cscope.rb
+++ Formula/cscope.rb
@@ -6,6 +6,7 @@ class Cscope < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7065f35c7c69268a0c293c7108e936c92c83fa6c1aff78d9fb6eb4d6178cb3c9" => :sierra
     sha256 "97930be35cbcd08980651597acf931adbb50a80df10960d6564497f19d9c8032" => :el_capitan
     sha256 "71d86771790165c777341e4457dd193008cfb2fb24628a138cb45fec61e6b42d" => :yosemite
     sha256 "cb2f63522d072307cacf63e8eabf4c284f2e8c1b2ff8c6de3aeb6fb8759a1212" => :mavericks
