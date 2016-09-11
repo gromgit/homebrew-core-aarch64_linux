143c4c51bd2dddf13a88387c5fbdb6fc18768b8a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-11 21:39:51 +0100

    git-lfs: update 1.4.1 bottle.

--- Formula/git-lfs.rb
+++ Formula/git-lfs.rb
@@ -6,6 +6,7 @@ class GitLfs < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d293874293c57d6ed922f3193c1cf09a76c0867ae7153fa0eb85d22a497db299" => :sierra
     sha256 "aa45170d657b4cec16d9aa793f0958db3b47b11cf9bc07fa2aaf31fccdc5324e" => :el_capitan
     sha256 "519300efced3a82e219edc4a23aacf486212e2e0303ef29642e26bc7b656bfb9" => :yosemite
     sha256 "b70269206360e38015f46b7838270921da2266f4358cba98c0ac7949ffa43d36" => :mavericks
