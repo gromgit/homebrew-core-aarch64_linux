d248fe7a4b3d9c76128c9a295260a57f734dc0c1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-11 03:51:48 +0100

    faac: update 1.28 bottle.

--- Formula/faac.rb
+++ Formula/faac.rb
@@ -6,7 +6,8 @@ class Faac < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "1537586f1bb69e142a32886ba9ac6e8e244dc297d8be441573e46eacd57e7075" => :sierra
     sha256 "4607ea16f33aec6dabefe6ace66c41cb7ee487b03fff8c7b2d050cbc6340422f" => :el_capitan
     sha256 "c3fad36cecb8f5d4fc17ad5912d90ee9dffafeb2c5c60b21d27d9ea8bfeb351d" => :yosemite
     sha256 "1b97bdfb9334decc5d51c9d57788fa23ccf77c56d74a53fcfe6f21a4495ba463" => :mavericks
