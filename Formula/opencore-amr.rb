909781b2a733b8c1015fc695c00cdc391f97308f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-11 08:40:34 +0100

    opencore-amr: update 0.1.3 bottle.

--- Formula/opencore-amr.rb
+++ Formula/opencore-amr.rb
@@ -6,6 +6,7 @@ class OpencoreAmr < Formula
 
   bottle do
     cellar :any
+    sha256 "6412c1b3a927ed533d8dac323320c1d005eff643ede06070915ddcdd68975273" => :sierra
     sha256 "2434237ad9e130eab8f89b4b3f7fb7d299c216bb28b344a47460a93180025eea" => :el_capitan
     sha256 "b4373425e5d5ad32cfda4b98c17946a61a78d1d6001edd6bc31f17546d881036" => :yosemite
     sha256 "b7d22ef690bc40dee36e90d747cfb108fd69b6cabd3b6a4593767fb49c553aff" => :mavericks
