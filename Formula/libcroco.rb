fde49f9a9bc7fdacd88b96d7664eba7d3084b7aa
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-11 02:13:05 +0100

    libcroco: update 0.6.11 bottle.

--- Formula/libcroco.rb
+++ Formula/libcroco.rb
@@ -6,6 +6,7 @@ class Libcroco < Formula
 
   bottle do
     cellar :any
+    sha256 "6f2d7e943bf90574d0b6e11eb1d3919b06718bfd302d2a53366cf16b6abe8fe7" => :sierra
     sha256 "63df1fc35be64690e7a9e8557e6049c0086a9f284f3ae4964e016ff99ebbdf38" => :el_capitan
     sha256 "1e620e3c0b584702c9d4dde4cfeec359c8874f364d342d2e1ead68adb520ceed" => :yosemite
     sha256 "341aada2eec7e3b3db71a9eca1da5b8df6127e2d4cec90cdbb1d2ec24de27eed" => :mavericks
