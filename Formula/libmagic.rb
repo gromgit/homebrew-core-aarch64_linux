ccc4342537df986d19290d81fa54f9b7bbb547e1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-12 08:08:36 +0100

    libmagic: update 5.28 bottle.

--- Formula/libmagic.rb
+++ Formula/libmagic.rb
@@ -6,6 +6,7 @@ class Libmagic < Formula
   sha256 "0ecb5e146b8655d1fa84159a847ee619fc102575205a0ff9c6cc60fc5ee2e012"
 
   bottle do
+    sha256 "fdfa8607a14c9dcb0686731956507e75dea169ef505d20db91c4c9233335221c" => :sierra
     sha256 "13beb0a727a18c47d10c4569772fd2077b37ed363c008faa82bc184a104c380b" => :el_capitan
     sha256 "ab633517a9be283ce7518864086d58a49a1de0926da5f65e1bbdfb5ef1d20ffb" => :yosemite
     sha256 "dfbcd1f3bee44e44bbeaa8638167359966e810f77cc6f81516776dbba979c68b" => :mavericks
