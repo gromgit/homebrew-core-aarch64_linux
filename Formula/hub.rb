409806e528cd0b077e17a22be08aefe567be2933
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-12 05:44:51 +0100

    hub: update 2.2.8 bottle.

--- Formula/hub.rb
+++ Formula/hub.rb
@@ -8,6 +8,7 @@ class Hub < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "82228fb79156537b9649a653391640de922fb223c8147a3fa67bd27d386ffa56" => :sierra
     sha256 "7eb6188c498e61f7df86cf85bb8b5ae43e77dd0ceabdaf669149aca6db6530bb" => :el_capitan
     sha256 "96fd51e0996f9f04d25bce0c4e545989bb170b83f1a47b5725e97050772c6999" => :yosemite
     sha256 "336f670800e322c8bdb38d18c3e9566f63c58009233a4080ce6a25b39c17a372" => :mavericks
