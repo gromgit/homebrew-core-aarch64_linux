333be97e2adcaf9ba928319f87056a49c0a4a6d6
ilovezfs <ilovezfs@icloud.com>
2016-09-11 22:55:48 -0700

    cryptopp 5.6.4
    patch is no longer needed
    
    Closes #4738.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/cryptopp.rb
+++ Formula/cryptopp.rb
@@ -1,24 +1,14 @@
 class Cryptopp < Formula
   desc "Free C++ class library of cryptographic schemes"
   homepage "https://www.cryptopp.com/"
-  url "https://downloads.sourceforge.net/project/cryptopp/cryptopp/5.6.3/cryptopp563.zip"
-  mirror "https://www.cryptopp.com/cryptopp563.zip"
-  version "5.6.3"
-  sha256 "9390670a14170dd0f48a6b6b06f74269ef4b056d4718a1a329f6f6069dc957c9"
-  revision 1
+  url "https://downloads.sourceforge.net/project/cryptopp/cryptopp/5.6.4/cryptopp564.zip"
+  version "5.6.4"
+  sha256 "be430377b05c15971d5ccb6e44b4d95470f561024ed6d701fe3da3a188c84ad7"
 
   # https://cryptopp.com/wiki/Config.h#Options_and_Defines
   bottle :disable, "Library and clients must be built on the same microarchitecture"
 
   option :cxx11
-
-  patch do
-    # fix failing test
-    # https://github.com/Homebrew/homebrew-core/issues/2783
-    # http://comments.gmane.org/gmane.comp.encryption.cryptopp/8028
-    url "https://github.com/weidai11/cryptopp/commit/0059f48.diff"
-    sha256 "f75d6cf87f00dfa98839a89a3c0c57dc26c1739916d6d5c0abbd9228b0a01829"
-  end
 
   def install
     ENV.cxx11 if build.cxx11?
