972664c9ece7e1801bcf6f8253ddb5d236915d21
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-12 07:16:26 +0100

    pandoc: update 1.17.2 bottle.

--- Formula/pandoc.rb
+++ Formula/pandoc.rb
@@ -10,6 +10,7 @@ class Pandoc < Formula
   head "https://github.com/jgm/pandoc.git"
 
   bottle do
+    sha256 "c22facb7c3190b20c81a750bf54fa076aaaa0c3baf3837d199fb724c456e6f4a" => :sierra
     sha256 "57337c49f118e146afb7219a4b25928ea4600ce6baf1896bed740053d8d52865" => :el_capitan
     sha256 "13056c5ba9d30304588ac26b8a7f3db790a942c0026da47a4f248bb49bfd819b" => :yosemite
     sha256 "0cf6c4e41715083d283ae83af3e7d4f5618d82d65b9b395fcf7c43d3faf6e62c" => :mavericks
