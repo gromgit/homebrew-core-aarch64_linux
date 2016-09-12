097254e306f9a1eb913c6237c0a56b2a94ecebaa
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-12 11:18:10 +0100

    json-c: update 0.12 bottle.

--- Formula/json-c.rb
+++ Formula/json-c.rb
@@ -7,7 +7,8 @@ class JsonC < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "b61af7437b93495ba08b097e4da71e3c00f672402394620607977b45d2348f85" => :sierra
     sha256 "8ba8006e2eb97006a781ce8d93a95791ae1e26d094afce0aeb8483caa95febbd" => :el_capitan
     sha256 "f7a602faf71091f98eb7b8390c1bd36bbd14cfe7e20c2f418bcc5c797315a2be" => :yosemite
     sha256 "e755df0edf95cf76c20a551dd28bb1703e769371413feaa7f60660338a72ce6c" => :mavericks
