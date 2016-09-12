ef035ff515124d4f8bfd5b3b5f8f94ad34f90c84
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-12 08:49:35 +0100

    gpp: update 2.24 bottle.

--- Formula/gpp.rb
+++ Formula/gpp.rb
@@ -6,7 +6,8 @@ class Gpp < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "7b905243827e6b5877c8af1f13b6fb3b79c07341059fdb1b6a382fe1286d580b" => :sierra
     sha256 "6925eb92be766ed9fe61a9a98dc7bc3c22793079abf63f462cb7001017cac28c" => :el_capitan
     sha256 "e9bb30f85bd24890f97160649a3ed9ef8081d0e39154226487b29e4c58d154ab" => :yosemite
     sha256 "90463e69adac31b694bbcac3e90ad494bb8e4ef4927d1a04e3a7246b87c0d55d" => :mavericks
