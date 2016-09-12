92874320df960934b020a06d7f93d3ee2e8ce935
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-12 03:43:05 +0100

    lzlib: update 1.8 bottle.

--- Formula/lzlib.rb
+++ Formula/lzlib.rb
@@ -6,6 +6,7 @@ class Lzlib < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "026dbd89a759e89bb934b17e935bc75dc32516d1c8722ec52f2b8dbf3d3b4678" => :sierra
     sha256 "4c90f6b740eb6fea8fef693744a071c268ab1655a1b077ae6ddc582e8fe2fc6f" => :el_capitan
     sha256 "5964828d701a9f8f16d3d72f2725b08bfbf34816885f8dcede60a02f3bbc188c" => :yosemite
     sha256 "27ff9c5ce28a58317f5a77cff0b23ceef648774d984b89cf0b1f96066149036b" => :mavericks
