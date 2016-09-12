7bfc65e9c0867960b527dcbed95ae1d414e344f1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-12 11:28:01 +0100

    proj: update 4.9.2 bottle.

--- Formula/proj.rb
+++ Formula/proj.rb
@@ -6,6 +6,7 @@ class Proj < Formula
   head "http://svn.osgeo.org/metacrs/proj/trunk/proj"
 
   bottle do
+    sha256 "4f25681fb4385a8efb070caa753036c819e5e323706d3c417342cb7eee9c2b7d" => :sierra
     sha256 "b38c33321502ff9d808f06e1f8ff932c0c2c2c832c121c61cd30d5cd98fbaf8e" => :el_capitan
     sha256 "8146548590a89b1c82a115594b282f6d6708067490dd8c2f83d1e0d8a03845c3" => :yosemite
     sha256 "d76df7d958194e3e71993de9496480f0ee636170251b50013f79628ae27c9888" => :mavericks
