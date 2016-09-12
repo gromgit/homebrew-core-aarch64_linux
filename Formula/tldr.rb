e8d4a6ad7b29afd201361e994d6c7a14b51a3b27
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-12 14:26:57 +0100

    tldr: add 1.3.0 bottle.

--- Formula/tldr.rb
+++ Formula/tldr.rb
@@ -5,6 +5,13 @@ class Tldr < Formula
   sha256 "6210ece3f5d8f8e55b404e2f6c84be50bfdde9f0d194a271bce751a3ed6141be"
   head "https://github.com/tldr-pages/tldr-cpp-client.git"
 
+  bottle do
+    cellar :any
+    sha256 "3f98b7a5abcf565a4bce8c674b1f8d0ac5f79075a96f8695eab627a162b8154b" => :el_capitan
+    sha256 "9124b5e08e84aaf28cefb7a13ca37c969eb42edb3801ad73c6cb0e9a9092bc49" => :yosemite
+    sha256 "6087f487eb4de38affd318f610b95af55724624ea376ba0010e451f2bffc82bd" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libzip"
 
