1f8d627cc0500c46d980298c1c18623aff819794
Jack Maney <jackmaney@gmail.com>
2016-09-13 15:42:57 -0500

    pyenv-virtualenv 1.0.0 (#4781)

--- Formula/pyenv-virtualenv.rb
+++ Formula/pyenv-virtualenv.rb
@@ -1,8 +1,9 @@
 class PyenvVirtualenv < Formula
   desc "Pyenv plugin to manage virtualenv"
   homepage "https://github.com/yyuu/pyenv-virtualenv"
-  url "https://github.com/yyuu/pyenv-virtualenv/archive/v20160716.tar.gz"
-  sha256 "453c711e79a171eac69494e327705d3e26dcfaf4323da33f1c506d84e32ca6d5"
+  url "https://github.com/yyuu/pyenv-virtualenv/archive/v1.0.0.tar.gz"
+  sha256 "3523129c24ca3ad2cf130885ef9801cb066b04eabc354ee2c38c486b8b726f15"
+  version_scheme 1
   head "https://github.com/yyuu/pyenv-virtualenv.git"
 
   bottle :unneeded
