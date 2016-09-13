aefeffe53fc4b5c29c2b996d321912d75013d5b4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-13 15:31:35 +0100

    berkeley-db: update 6.1.26 bottle.

--- Formula/berkeley-db.rb
+++ Formula/berkeley-db.rb
@@ -6,6 +6,7 @@ class BerkeleyDb < Formula
 
   bottle do
     cellar :any
+    sha256 "9aeac35b89e11e5b1eec4e5f17534523b64e05117471cb2121a58ca0f3bda5db" => :sierra
     sha256 "70f4c1cea6a4c2f9454680988b73bb9fdc107307dafeec47e21a4539dce57b36" => :el_capitan
     sha256 "3e4324500931e32e3a187f9790b4465efd4d249dc9ec03ff46b4209ed061e935" => :yosemite
     sha256 "b2ba3aff83ee27a52115f47abc3c1767d124841ad342e63e433dfa36af064b36" => :mavericks
