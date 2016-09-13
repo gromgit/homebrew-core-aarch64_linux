adc8e85f7a17af50c4a367de464ce538d385768c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-13 20:39:33 +0100

    gringo: update 4.5.4 bottle.

--- Formula/gringo.rb
+++ Formula/gringo.rb
@@ -7,6 +7,7 @@ class Gringo < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "10fdd80c67761f30f49f95ec2e79f0f8cea9c8109053a07853491846af30fc6a" => :sierra
     sha256 "441c81748e551106a923ccd61298f98c8b5e46cba684fc2e0031a4fe11f1d0ae" => :el_capitan
     sha256 "e9b5ee97a0a0b8c14a66d6359936259b7edc96c2013531c250903a03df65bfb0" => :yosemite
     sha256 "7215d8a23fe6052bd67e4ce8b5537da6e16236f07fe387bd7b29fa5f9ee35ca9" => :mavericks
