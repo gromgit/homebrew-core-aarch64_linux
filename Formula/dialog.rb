88fcc2ae484293c484fc4862ffbf2e393e935c91
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-13 11:43:56 +0100

    dialog: update 1.3-20160209 bottle.

--- Formula/dialog.rb
+++ Formula/dialog.rb
@@ -7,6 +7,7 @@ class Dialog < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ee630bb7ae15cde86a4dd48a598b5eaf1151237bef9e0f1ffa9a120cc5fbd9ac" => :sierra
     sha256 "8c12527b91df19529a580ff081621d30514119bfd999f76a763d59c406b32531" => :el_capitan
     sha256 "89a8a52b64df7abe4aabfa9de1aa62523098154ae447da57fab3adce18b34a29" => :yosemite
     sha256 "74626b6e18ebd0b4b755e04457be5a385bc10745cf08130f1330ab2b108a0550" => :mavericks
