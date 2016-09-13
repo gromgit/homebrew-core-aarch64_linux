f05afc9d5e7e61d7edae8fb5cd026e70607880e6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-13 10:10:54 +0100

    popt: update 1.16 bottle.

--- Formula/popt.rb
+++ Formula/popt.rb
@@ -5,7 +5,8 @@ class Popt < Formula
   sha256 "e728ed296fe9f069a0e005003c3d6b2dde3d9cad453422a10d6558616d304cc8"
 
   bottle do
-    revision 1
+    rebuild 1
+    sha256 "ceae94fc8e588309670a7a045186eee8ff3a9966a68650f044a14d101267b7b2" => :sierra
     sha256 "60a7f19e8fecafd92a5beb7d6438efac915e8f3afe3d83575fb64bb4a6190aab" => :el_capitan
     sha256 "56d1104516e23bb314a248904b8ec85afe2fdbf71555417eb8f91edc1286e6da" => :yosemite
     sha256 "ba122e7f34b9b03ab5a32ab01124b61eb608c29e0c0d023462953ed03782dd2a" => :mavericks
