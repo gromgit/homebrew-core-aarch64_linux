2852ed05307fc4d4ea478fce3da9c116133ba0dc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-13 10:10:54 +0100

    ocamlbuild: update 0.9.2 bottle.

--- Formula/ocamlbuild.rb
+++ Formula/ocamlbuild.rb
@@ -6,6 +6,7 @@ class Ocamlbuild < Formula
   head "https://github.com/ocaml/ocamlbuild.git"
 
   bottle do
+    sha256 "fdd70a95ba781a983589909b5a082ee6e472cee937f5f5df6fed52caea67317b" => :sierra
     sha256 "04242e3fb56f3882605504c44ef850008c53839d4e0f742ff32ae306dd98c694" => :el_capitan
     sha256 "d5d4c80d69a1bf90984af137ca7c1facf6e8744d3a938e7e25b4a24bc0d3a824" => :yosemite
     sha256 "13b18cd85715e360b482fd011cb94e4132580e4ef7604b67057266a6331691b9" => :mavericks
