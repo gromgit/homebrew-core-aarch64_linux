a678e11bb229ee2408391cbbd74434880698fb6a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-13 09:06:39 +0100

    leveldb: update 1.19 bottle.

--- Formula/leveldb.rb
+++ Formula/leveldb.rb
@@ -6,6 +6,7 @@ class Leveldb < Formula
 
   bottle do
     cellar :any
+    sha256 "688768ac09d7b8facf8e76f12f42482ed1a58323a7067a087705b276ea9aabf0" => :sierra
     sha256 "a1cd94640590b34d997f4fc68fda303785c3087a592504500f5110b23cd91c85" => :el_capitan
     sha256 "a3c1d90348b005180c3c83c9d1180dea206b37b8f50f53725f63955552bd8485" => :yosemite
     sha256 "3de4411766fb99c683ab13e49dd6d788f7193c388b7e244b3b8868b9e23c4cdb" => :mavericks
