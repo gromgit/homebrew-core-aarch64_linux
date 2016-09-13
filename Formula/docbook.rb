5a645ef39be0038afa05e6068e52914af259f13b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-13 14:15:24 +0100

    docbook: update 5.0 bottle.

--- Formula/docbook.rb
+++ Formula/docbook.rb
@@ -6,7 +6,8 @@ class Docbook < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 3
+    rebuild 3
+    sha256 "40b2740609c1586d030d3b9a131761821425c211989bee297af29900981b3aba" => :sierra
     sha256 "3fb7e4070eaa9250fa947d38e3d7803d37c159d9765e3f71397702d5ad6bb578" => :el_capitan
     sha256 "dfdb315404c98dca2682f63260f2996de101cb6b41de69ac268dcded110e2a3f" => :yosemite
     sha256 "65925fda670fdb020fe9d52cd5891f8e3a2a44619e9129b30031127c7c2e998c" => :mavericks
