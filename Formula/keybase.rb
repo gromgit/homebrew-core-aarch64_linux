7cf1849e64bd92f803563b7888af9de8ecd270eb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-13 07:47:20 +0100

    keybase: update 1.0.17 bottle.

--- Formula/keybase.rb
+++ Formula/keybase.rb
@@ -8,6 +8,7 @@ class Keybase < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "aadf1887c4fb92be9b9ca0639f03670b056f4aea14b75db11197a4f29e5bf942" => :sierra
     sha256 "3b3a5451ba2cedf14adaaaa6928c300de9c88ff4ea7bb9e35bbd6c9090eb65a6" => :el_capitan
     sha256 "944e4effa6edbccb66649200782f6439295519390f2cbb47e476cfb4a1864c83" => :yosemite
     sha256 "300573098e20510b6593243628275c433f81f3cc8b08a74848e61b32ec6ef49a" => :mavericks
