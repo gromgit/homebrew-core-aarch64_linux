0f7fd45e7abd17558d0bbe29fc669f3cb903a8fd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-13 05:10:14 +0100

    gsl: update 2.2.1 bottle.

--- Formula/gsl.rb
+++ Formula/gsl.rb
@@ -7,6 +7,7 @@ class Gsl < Formula
 
   bottle do
     cellar :any
+    sha256 "3e762d44c1d72ae29e392b7215bdcd6a98c8e2ed17492d603dcdfa56e00edd94" => :sierra
     sha256 "89462bab1b0b7001ce6b71db851960f005c0436ffac840f3b9cc255ed7cb8d44" => :el_capitan
     sha256 "00e0d7aa1202bededfa2bdce311dd7ff08db0e412e45bd87b783eb29305ccde5" => :yosemite
     sha256 "00a05716a23a7bc333782dd77f547942563912650445d557aa5c8c941ac22c7e" => :mavericks
