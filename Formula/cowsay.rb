a5bde668d4d210e99510ec38763bf5189c1a3635
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-13 09:40:44 +0100

    cowsay: update 3.04 bottle.

--- Formula/cowsay.rb
+++ Formula/cowsay.rb
@@ -6,6 +6,7 @@ class Cowsay < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "17a1a4620885c9a4b7104b43072ed32348b37f6bb43a1120fe23a46d893e87ea" => :sierra
     sha256 "360390af15a3c4793e07eda95f55f4a5466ffafc766cb6b62f9790146080a62a" => :el_capitan
     sha256 "a622af361a6139bc930b371fbde7cfc54bbe8bebfbbe0782e59248fadb10b95f" => :yosemite
     sha256 "185bfafd379cd6f6d6202fdae0750fdd2998cc94f33ea349bd4872a0274cb1dc" => :mavericks
