44aa87e98c8fcc6bebfaa8a09d5cc52e8c91a799
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-13 22:12:29 +0100

    glide: update 0.12.2 bottle.

--- Formula/glide.rb
+++ Formula/glide.rb
@@ -8,6 +8,7 @@ class Glide < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "dedc72d451e5515a46d041695c1e002e65d1fc8ba9372c1390e228184239b3a0" => :sierra
     sha256 "4e4a8b3a959e8dff8c50c3a49483aeb7a6daa6de7b8b164c24348d626f6d4498" => :el_capitan
     sha256 "f62a55fbc7b3e5ce78d111e4843b9f958e3e31aa9e0decef80a725002e248e44" => :yosemite
     sha256 "3d4a9034e7d2c2eed872746f54c7f38ec8182258485b9fbe896e31b8599213af" => :mavericks
