4b09598a253e46be9a33b4dbaca93fad750945ec
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-13 11:12:10 +0100

    apr-util: update 1.5.4_3 bottle.

--- Formula/apr-util.rb
+++ Formula/apr-util.rb
@@ -6,6 +6,7 @@ class AprUtil < Formula
   revision 3
 
   bottle do
+    sha256 "befef73518b6503d4e6d24c8b7de4f9c30c985dedb85251cf580ccf53378c7ad" => :sierra
     sha256 "528b07e8938a0ad7794778f020773a099b569f14e301b90362f91c9944777408" => :el_capitan
     sha256 "5bb93452578f116f36abef059e3869f25d89315b11685d8a4c726e4133680311" => :yosemite
     sha256 "b615ab8f1080457550fd2fc4e68e5e1d9bd28fc8341aae1bf84fdbfa0420ea29" => :mavericks
