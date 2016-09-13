0ce728d3a31f1d29b3a7fd5f1407d3958e998355
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-13 14:15:24 +0100

    dpkg: update 1.18.10 bottle.

--- Formula/dpkg.rb
+++ Formula/dpkg.rb
@@ -6,6 +6,7 @@ class Dpkg < Formula
   sha256 "025524da41ba18b183ff11e388eb8686f7cc58ee835ed7d48bd159c46a8b6dc5"
 
   bottle do
+    sha256 "47929bc2b33b461788e0d1b74846cda3ad2454862486220345ec54eebac9b36a" => :sierra
     sha256 "1e13b24cd8b0ebcdc18974b2324d66e6b5c7e7984be8610ef098dacb8e592c3e" => :el_capitan
     sha256 "2d4703e267cc69a932dc5c7849111a8504bae13f363fc34d469d43f47699c900" => :yosemite
     sha256 "c859b1f92594ee0aa612bb3cd9a1a33fb9f7579ca0c23951d0bd0832a1080463" => :mavericks
