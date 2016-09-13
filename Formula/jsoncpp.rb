b2344741d8a744a0f981e96a871be882f73ab267
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-13 09:40:44 +0100

    jsoncpp: update 1.7.5 bottle.

--- Formula/jsoncpp.rb
+++ Formula/jsoncpp.rb
@@ -7,6 +7,7 @@ class Jsoncpp < Formula
 
   bottle do
     cellar :any
+    sha256 "3533ccc77f79d7a9da253004bdfa2ca381fdb8df0088888caa6ed89ead7f55e0" => :sierra
     sha256 "550124ad1dc43daa44425c9ab06f50a83cc6c9845ec1f2d9d06cbb680fa3aa73" => :el_capitan
     sha256 "6d021772ab9b0ff7590951ed26e83c6cbfe9839eb05b9753c476f07a6ff7ca8a" => :yosemite
     sha256 "cb6297d83e2898ae496f98450a05cb1c29b25b3ca7a1e35dfbb95837758acb81" => :mavericks
