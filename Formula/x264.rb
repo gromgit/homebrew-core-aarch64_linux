93210c4387dcd1418e74993aaef58ce4d38a77e9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-14 12:57:32 +0100

    x264: update r2668 bottle.

--- Formula/x264.rb
+++ Formula/x264.rb
@@ -9,6 +9,7 @@ class X264 < Formula
 
   bottle do
     cellar :any
+    sha256 "5120b2c5e51669a0eae052f7fdc0af4519eaef4575349fb10824ac2ba9526f0e" => :sierra
     sha256 "8dd89dfa62eb8837f1a49450e7f6069502f49426ec4ea79357e980f534ea3e7a" => :el_capitan
     sha256 "edc6732b61f996968cc07c591cb17a5b4358a0db400c69e881c0559f6cf41ef8" => :yosemite
     sha256 "a64a08088aef050f6fc27f97a5bb411c9d75d94c0b9c1f0c8dfd99cdb7f1f4c9" => :mavericks
