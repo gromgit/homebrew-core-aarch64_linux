94b489f924e63fd065795d9ce1e1933df98f8195
Mike McQuaid <mike@mikemcquaid.com>
2016-09-14 08:02:42 +0100

    mas 1.2.1
    Closes #4770.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/mas.rb
+++ Formula/mas.rb
@@ -1,8 +1,8 @@
 class Mas < Formula
   desc "Mac App Store command-line interface"
   homepage "https://github.com/argon/mas"
-  url "https://github.com/argon/mas/archive/v1.2.0.tar.gz"
-  sha256 "79a30d2b8c053a33b69a255da3ad35a96a337f0b9dded5112e6e48cdadddf73a"
+  url "https://github.com/argon/mas/archive/v1.2.1.tar.gz"
+  sha256 "ebf594b2b4ef348e2aea770960215feaf569d6cf5b3c5f104402c5c0eb9f46e7"
   head "https://github.com/argon/mas.git"
 
   bottle do
@@ -13,6 +13,10 @@ class Mas < Formula
   depends_on :xcode => ["7.3", :build]
 
   def install
+    # TODO: remove when >1.2.1 is released
+    # Reported upstream: https://github.com/argon/mas/commit/3a02d7
+    inreplace "mas-cli/mas-cli-Info.plist", "1.2.0", version
+
     ENV["GEM_HOME"] = buildpath/".gem"
     system "gem", "install", "bundler"
     ENV.prepend_path "PATH", "#{ENV["GEM_HOME"]}/bin"
