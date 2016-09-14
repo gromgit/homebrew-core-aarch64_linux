1d155efd9e91c7945ebcb8dc2c41fe1d111b9bb5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-14 13:11:45 +0100

    mas: update 1.2.2 bottle.

--- Formula/mas.rb
+++ Formula/mas.rb
@@ -7,6 +7,7 @@ class Mas < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d5cbfee80a78079b561c15c2f651febdcafee8030c1afdb43a19b9089cb85134" => :sierra
     sha256 "7c6d5cd11f167ae0a52a0970d68a342bc69924214575f81460849e28d034a412" => :el_capitan
   end
 
