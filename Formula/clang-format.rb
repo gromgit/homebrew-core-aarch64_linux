612fb173ec6490851691b8b1f20e34de81e0bccc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-14 01:40:54 +0100

    clang-format: update 2016-08-03 bottle.

--- Formula/clang-format.rb
+++ Formula/clang-format.rb
@@ -18,6 +18,7 @@ class ClangFormat < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "67a35cdff7ff1a4c361ce79f973ff7d5b718fd22202e5f81711d78882c3d337a" => :sierra
     sha256 "730d8a5f7b0608dce084df537eff1c45cd1b0c12c0e2bace2f27ffcb3a86dfb8" => :el_capitan
     sha256 "06a1aa4d1768e12d697888e829b772ced182431d20eb107817d069d0a1f91976" => :yosemite
     sha256 "f90425f4547f526e3637f622495afd89891e29dccaf7a9a58fb8dabdaaacb29d" => :mavericks
