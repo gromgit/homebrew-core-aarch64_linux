b0d4f11890a566204a04d7e02f4e7cea48e15cd4
Chris Donati <chris.donati@appscale.com>
2016-09-14 15:56:27 -0700

    appscale-tools 3.1.0
    Closes #4821.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Formula/appscale-tools.rb
+++ Formula/appscale-tools.rb
@@ -1,8 +1,8 @@
 class AppscaleTools < Formula
   desc "Command-line tools for working with AppScale"
   homepage "https://github.com/AppScale/appscale-tools"
-  url "https://github.com/AppScale/appscale-tools/archive/3.0.1.tar.gz"
-  sha256 "ccabc008dd84f60e446414f6151881baf9d061c7f8771e14bce9cf7e5311d6b5"
+  url "https://github.com/AppScale/appscale-tools/archive/3.1.0.tar.gz"
+  sha256 "8496e318fdebde9ff57be8b6789d9af3cc808aecbb509d19102d0d22d6e4fd09"
   head "https://github.com/AppScale/appscale-tools.git"
 
   bottle do
