646ba86d252b2ea833edd920539bb68f090a14b3
ilovezfs <ilovezfs@icloud.com>
2016-09-15 16:40:40 -0700

    darcs 2.12.4
    Closes #4866.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/darcs.rb
+++ Formula/darcs.rb
@@ -5,8 +5,8 @@ class Darcs < Formula
 
   desc "Distributed version control system that tracks changes, via Haskell"
   homepage "http://darcs.net/"
-  url "https://hackage.haskell.org/package/darcs-2.12.3/darcs-2.12.3.tar.gz"
-  sha256 "68ed535dce4bd2d8349ba04258bb56df7d47853dac9d3365fc0325a86db1cde5"
+  url "https://hackage.haskell.org/package/darcs-2.12.4/darcs-2.12.4.tar.gz"
+  sha256 "48e836a482bd2fcfe0be499fe4f255925ce50bdcf5ce8023bb9aa359288fdc49"
 
   bottle do
     cellar :any_skip_relocation
