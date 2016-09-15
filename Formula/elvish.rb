68e799d960dfc0ad533ec50858de8b708b48493b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-15 19:55:49 +0100

    elvish: update 0.4 bottle.

--- Formula/elvish.rb
+++ Formula/elvish.rb
@@ -7,6 +7,7 @@ class Elvish < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "55dc1e0f37ce67ce34ea22c145aff8056b3073a6aa3b8c3c39c7e56a1112812c" => :sierra
     sha256 "10fc6abb091014f7f7b77333303bcfc2b3d8b51033da9c608ea326957e1467a8" => :el_capitan
     sha256 "96db9dbc110145a8384838513f8db2258ed3f836a2ddbe34f5f8fc7d9092df71" => :yosemite
     sha256 "b3f5e6e46c0c94ad034a27c36da089e31b5960accc5136ed2f5f5c8ffa54e88d" => :mavericks
