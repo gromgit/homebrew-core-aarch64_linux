90c26b0d09c067c6c18f605b6ed1a9b601b4df31
Zhiming Wang <zmwangx@gmail.com>
2016-09-15 10:54:41 -0400

    elvish 0.4
    Upstream now using vendor directory.
    
    Closes #4847.
    
    Signed-off-by: Zhiming Wang <zmwangx@gmail.com>

--- Formula/elvish.rb
+++ Formula/elvish.rb
@@ -1,10 +1,8 @@
-require "language/go"
-
 class Elvish < Formula
   desc "Novel UNIX shell written in Go"
   homepage "https://github.com/elves/elvish"
-  url "https://github.com/elves/elvish/archive/0.3.tar.gz"
-  sha256 "83d463667055f38367302921e8d87c274ae71fc777f6ffb176dad96e875378e8"
+  url "https://github.com/elves/elvish/archive/0.4.tar.gz"
+  sha256 "05c32183462b0e63c832f1b0481ce79c4c6a289c14fe670b4400f47e349e2851"
   head "https://github.com/elves/elvish.git"
 
   bottle do
@@ -16,21 +14,10 @@ class Elvish < Formula
 
   depends_on "go" => :build
 
-  go_resource "github.com/elves/getopt" do
-    url "https://github.com/elves/getopt.git",
-        :revision => "f91a7bf920995832d55a1182f26657bc975b9c24"
-  end
-
-  go_resource "github.com/mattn/go-sqlite3" do
-    url "https://github.com/mattn/go-sqlite3.git",
-        :revision => "3fb7a0e792edd47bf0cf1e919dfc14e2be412e15"
-  end
-
   def install
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/elves").mkpath
     ln_sf buildpath, buildpath/"src/github.com/elves/elvish"
-    Language::Go.stage_deps resources, buildpath/"src"
     system "go", "build", "-o", bin/"elvish"
     system "make", "-C", "src/github.com/elves/elvish", "stub"
     bin.install "bin/elvish-stub"
