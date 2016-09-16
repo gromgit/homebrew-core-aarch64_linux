4d0e765b8b5dfc34e55d830dc2ff958f633e0e2a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-16 21:14:51 +0100

    asciidoc: update 8.6.9_1 bottle.

--- Formula/asciidoc.rb
+++ Formula/asciidoc.rb
@@ -7,6 +7,7 @@ class Asciidoc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0f6e983fd2e62ddc4c13e0028e76f2e206a83f9a7371f4158c0c7997ab14634c" => :sierra
     sha256 "2e2ab0cd2462155296a05f8982b4d0c69b48db53ab26e49483c3fc116d955b67" => :el_capitan
     sha256 "e460dd7a372d465bdb2e932c02fb1339e96b7e28f47c8f750ae0546e56e517f9" => :yosemite
     sha256 "fe63d108847b9b197ee4f853b52faa8eb6d5c3990cc9919567d799f1b3dbd674" => :mavericks
