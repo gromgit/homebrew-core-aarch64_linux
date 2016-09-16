7a499a29c2f70d1e331a207c1db9b7b4808ed5fd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-16 23:00:38 +0100

    taglib: update 1.11 bottle.

--- Formula/taglib.rb
+++ Formula/taglib.rb
@@ -7,6 +7,7 @@ class Taglib < Formula
 
   bottle do
     cellar :any
+    sha256 "894d6afda25dfae81d917cddb66de7e39e26b8ed2ca64168f6468dd175f2bfe1" => :sierra
     sha256 "bfcd6575c21fce26f3f49cfc43fb30b46906749d81d757e10597b6fdbaf8b512" => :el_capitan
     sha256 "e5a9e62fc16e32b8ad3239a712bf3eab630b245ed397bd957d1b89e2a807e310" => :yosemite
     sha256 "2e0df8adfa080dc22265478490aca953384e97d41e9421b110ef142e07d15ab2" => :mavericks
