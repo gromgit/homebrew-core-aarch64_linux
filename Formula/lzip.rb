1d49dd7d1a1e3aacfcf9f25a08156ad01064daa3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 00:23:58 +0100

    lzip: update 1.18 bottle.

--- Formula/lzip.rb
+++ Formula/lzip.rb
@@ -6,6 +6,7 @@ class Lzip < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3d44d4a51d26d920613b90d057200ad25c6d717d6d4b462b30b9125616d9819d" => :sierra
     sha256 "dcebc83e260a8bfdfe55a2cc991a4184d935b6fe53390160d7e2154371671845" => :el_capitan
     sha256 "a5e4b8f48d93f2d3ca8988df55b619ff779ff5331d3ee64dd31b57b7d88959db" => :yosemite
     sha256 "cfde4d3e188c4347a23811e2712a60bd646a3037e6f7b9cf6afecd6db933fda0" => :mavericks
