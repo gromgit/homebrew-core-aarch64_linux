9b04788f925149eeb36244f1ef86e29ac3d45b65
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-16 22:05:30 +0100

    spatialindex: update 1.8.5 bottle.

--- Formula/spatialindex.rb
+++ Formula/spatialindex.rb
@@ -6,6 +6,7 @@ class Spatialindex < Formula
 
   bottle do
     cellar :any
+    sha256 "6c60b7939a2220b10e04cc5e47a6672935697a13accc1284bb90b401b866044c" => :sierra
     sha256 "34d1e02dd4133ed67a8a4c299044e277e1e9cfc982962c50c44c751723eb85cb" => :el_capitan
     sha256 "907f40e614218622fd9fecc0a542adcdf768446a198ef4cc972b30a7eb5e6cd3" => :yosemite
     sha256 "33e053a03ea77bc87c3aab4f8319461baab56824e3c933cb09398e6df1b542ba" => :mavericks
