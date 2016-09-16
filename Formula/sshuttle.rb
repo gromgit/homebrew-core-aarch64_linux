16662febe55f40ec5cf989e011cbc0e2e0730f54
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-16 23:00:38 +0100

    sshuttle: update 0.78.1 bottle.

--- Formula/sshuttle.rb
+++ Formula/sshuttle.rb
@@ -9,6 +9,7 @@ class Sshuttle < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "11492c4d73a18bf5adcce436361fcd9ff5c37d416f110d6a3c1e3ce8dc745936" => :sierra
     sha256 "d07d306b5b13f9f6c9b9d9ff6b35b73cffc2c9fe9c429ac121b5d4b3fbfa1d33" => :el_capitan
     sha256 "d03433ae8b8530a36885ffc70d1c73acb71063940afb413a1bb7287e06fbbe5c" => :yosemite
     sha256 "d70d49fa20f1bd9b4504c1476e88f46ce03f0d1efeb2086d4de547aea9eeb6e1" => :mavericks
