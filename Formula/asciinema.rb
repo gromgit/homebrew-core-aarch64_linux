f169814b40417e3e5e03697bde2cf07c7e565cd6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-16 21:36:37 +0100

    asciinema: update 1.3.0 bottle.

--- Formula/asciinema.rb
+++ Formula/asciinema.rb
@@ -8,6 +8,7 @@ class Asciinema < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "aaba59c7528f909f665ae4150f9c21d87b818244aefa176ad5ccba1469c33a4c" => :sierra
     sha256 "6205d485d86ea24d0a0396cc56fd76b09490a83c5210e74ff505aa03cd4fcb1d" => :el_capitan
     sha256 "6578e8f11c7d2918f1f7a6c00350c485a937ae1a7037e4d715ecf66e80001cd7" => :yosemite
     sha256 "25455ff63f3f68f17671727ba02f09bb7b03bfa4fadab3fa0c9bd4a7d687cbf0" => :mavericks
