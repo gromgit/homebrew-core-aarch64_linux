acf60bf86a9bc00c6ccf903e9d3984c687955c2f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 00:23:58 +0100

    check: update 0.10.0 bottle.

--- Formula/check.rb
+++ Formula/check.rb
@@ -6,6 +6,7 @@ class Check < Formula
 
   bottle do
     cellar :any
+    sha256 "ab001aa687f2a1f18df5c8765086a3300d989d600a7895c8a2c8efd2242f62a1" => :sierra
     sha256 "2e37bca055ae0bf490b713d90c95a9d81518cae9b884a7e495f521c80b4f062f" => :el_capitan
     sha256 "bb824d8cb8a74ebbc1eb9d22b8ea15327dfe3ce91151b37c7f41c80939609f52" => :yosemite
     sha256 "022667ec28a8af0bf6e19f3afbaa144542f1754ebaff5710229484fa8071084e" => :mavericks
