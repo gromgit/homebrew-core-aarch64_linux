966013d9126d035ae7d33f3d54e31676a9e39739
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-16 09:09:54 +0100

    eventql: add 0.3.2 bottle.

--- Formula/eventql.rb
+++ Formula/eventql.rb
@@ -4,6 +4,13 @@ class Eventql < Formula
   url "https://github.com/eventql/eventql/releases/download/v0.3.2/eventql-0.3.2.tgz"
   sha256 "d235f3e78fa5f6569fc2db94161d3e3f9cb71dc0646e341acd91814cefd23640"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7d128462969fc4e431c6a486fca5f397fc15c46e657a622d042c0bf531efba82" => :el_capitan
+    sha256 "482a655e17d0b905d0101d2716f52266e1ec8bb6a4814f30a576431fa2d179e4" => :yosemite
+    sha256 "92adde0e08a4bc68c90f1e93f13ee7afc7bc1c7b69f2187610545bcbcb76abd8" => :mavericks
+  end
+
   head do
     url "https://github.com/eventql/eventql.git"
 
