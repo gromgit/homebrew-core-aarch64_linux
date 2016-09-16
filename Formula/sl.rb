4d2a1a218b8f13efac6a0dae0e9417127c93baba
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-16 21:14:52 +0100

    sl: update 5.02 bottle.

--- Formula/sl.rb
+++ Formula/sl.rb
@@ -8,6 +8,7 @@ class Sl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "afd30cb3a99d238a8ac52810834244d5f47fc2ff597db9ad61012bd2014395b9" => :sierra
     sha256 "f186cb86f4d48929aa671434dbd6be0a861069608098a30dc952697bcca85972" => :el_capitan
     sha256 "696104243a18e08279d461e66e6a696791e6c36b67df43e361ad6f6de1200440" => :yosemite
     sha256 "c7d4432bfc169f7338eeb0c8300a975495b229d6e85bfff4fdd6bbd11eb8da17" => :mavericks
