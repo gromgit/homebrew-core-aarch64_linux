acf0264a6b850418987dd2cf1c67d4f85ad010c4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-16 22:32:56 +0100

    mpage: add 2.5.6 bottle.

--- Formula/mpage.rb
+++ Formula/mpage.rb
@@ -4,6 +4,12 @@ class Mpage < Formula
   url "http://www.mesa.nl/pub/mpage/mpage-2.5.6.tgz"
   sha256 "4fe66dfc27f7c4bfbca60ef617f968aa2e6ee877e8921aa968c16a03aa4edc04"
 
+  bottle do
+    sha256 "48a2c82f44b9e241edc1ed4727e4b788da8f7dc48a28d0cc8f9344bec63ae757" => :el_capitan
+    sha256 "66815ec14edcec106911ae720b21bd5220b8791592a578c26fe4a23a74fd38ea" => :yosemite
+    sha256 "9016175184826209a098bc8a2e5c73949879bc68abd67497151f714e6922e0b6" => :mavericks
+  end
+
   def install
     args = %W[
       MANDIR=#{man1}
