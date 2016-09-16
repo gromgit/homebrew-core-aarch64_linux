44c3dec9dce368bc4f59aa508cab5f544d75ea3e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 00:52:54 +0100

    dfu-util: update 0.9 bottle.

--- Formula/dfu-util.rb
+++ Formula/dfu-util.rb
@@ -6,6 +6,7 @@ class DfuUtil < Formula
 
   bottle do
     cellar :any
+    sha256 "3b9f9f6cf990becc1f2979fbec2cf2833d6f0ee2ab1ba3db4979203f1d591ee2" => :sierra
     sha256 "49975a34b6bacad4549871097effce90e376d3cd26ed24b2d7dfd925a199f0f8" => :el_capitan
     sha256 "776c3ed14def87511f9280201a95490145a9d469e2c1e29873c335e2c39ed279" => :yosemite
     sha256 "75c7ffa4e2d5067618c41f999f3d0c8a9aac1080e9eb8cd1f9bec8dd154aa1c1" => :mavericks
