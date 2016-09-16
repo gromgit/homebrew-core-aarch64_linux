dc565ce648040b4ffcd3709bf4a3ee8cc22eacb4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-16 20:27:05 +0100

    direnv: update 2.9.0 bottle.

--- Formula/direnv.rb
+++ Formula/direnv.rb
@@ -8,6 +8,7 @@ class Direnv < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "92f2aacc387353d99649d02771bf115dc475cffae2d303b17e644656013237e2" => :sierra
     sha256 "a95bf5ca6f2d723883b0f7ed3811603943d801b975513928b0fab75681c0ae64" => :el_capitan
     sha256 "8cacdbe6ccdb43e5595824b781638479e8664b231344860c75f1fc27e55a00e0" => :yosemite
     sha256 "5df957312c326778b364a4b8e0319fe934bf0d466ef9a26ed8f6adf39f62d3c6" => :mavericks
