b60c89440f98440dba0af226428a00db3dd7283e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-16 20:53:24 +0100

    protobuf-c: update 1.2.1_1 bottle.

--- Formula/protobuf-c.rb
+++ Formula/protobuf-c.rb
@@ -6,6 +6,7 @@ class ProtobufC < Formula
   revision 1
 
   bottle do
+    sha256 "6a457b9bb03045e5a588358b0e0cc6413cafeb0871d2239c0e556da9d87cf0f3" => :sierra
     sha256 "bcd2adfa85afe9057c35b971d796396b3cc77f3698ffff167c166c54bbc56313" => :el_capitan
     sha256 "cdadcb2c34ea509130bd927d506acb7b4016de77f39419c07fbd629e3cfebb1f" => :yosemite
     sha256 "d17bdc061c6f65c82a9fc7a468a8748aed5e262fd7b81df0a69da80620cb2a01" => :mavericks
