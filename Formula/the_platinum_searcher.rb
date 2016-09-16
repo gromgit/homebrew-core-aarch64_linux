9a42e3495688eeb4c99d47b5559079a79944c3b7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 00:52:55 +0100

    the_platinum_searcher: update 2.1.4 bottle.

--- Formula/the_platinum_searcher.rb
+++ Formula/the_platinum_searcher.rb
@@ -7,6 +7,7 @@ class ThePlatinumSearcher < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "90de0e8a5dd79f0aee57195bc67e38bf31d463503b0998da7f3ffefc033d14a6" => :sierra
     sha256 "f6664670adb1962b1d98c36362c51aceff066681197cfcf13f40a7c120b4e43b" => :el_capitan
     sha256 "f4523e3861799fcb8ceb43db725e33c8b86f8dc93bd3cdac9573c2b624b3d1a9" => :yosemite
     sha256 "cac0c68ab54c5869bffe00217443afd02e10ac95b1cf0c5a9a03f83cd961e8c1" => :mavericks
