0fddd6b867d7fd71e0dae8e90de9a609dad6ef32
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 09:17:43 +0100

    fio: update 2.14 bottle.

--- Formula/fio.rb
+++ Formula/fio.rb
@@ -7,9 +7,9 @@ class Fio < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "22836835822072ba50423e9f4602a2307c1f6f3da1a495ff167f5186ab2f1d0d" => :el_capitan
-    sha256 "192afda69fa661b0a7c055b6b5fdcc000bac74933452f6a13bb38a3cc44879cf" => :yosemite
-    sha256 "06d87d4865923cd577eb8a34819abe07d00c8f68b4129ae8cbadec1570f447ea" => :mavericks
+    sha256 "f4caaad6102651a917bcf0faf2f9547b7dcf36f64bb9e5ec4ff676fbf6466853" => :el_capitan
+    sha256 "32c4f08379ac6fb031e54da94397dd4f74be1e937c8703f44602660be8e66bca" => :yosemite
+    sha256 "b6ff9b7c19f2cac95bff1f75a452578788ad8f0599b981dc3dd3fd2612f77b63" => :mavericks
   end
 
   def install
