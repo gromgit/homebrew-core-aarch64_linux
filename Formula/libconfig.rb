50607d76ced103fe696b26288af00919274677b6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 05:40:18 +0100

    libconfig: update 1.6 bottle.

--- Formula/libconfig.rb
+++ Formula/libconfig.rb
@@ -6,6 +6,7 @@ class Libconfig < Formula
 
   bottle do
     cellar :any
+    sha256 "76c392efe1620331e9840eb426d9551c15f0c1f5b3db6bd255f7a6f7d28a70ec" => :sierra
     sha256 "b761558d36680478ea69e888a35bb64df066a561f9534e9b893b26e07a4062e4" => :el_capitan
     sha256 "da3783f62333e9f65b235c7359de96264476e7bb7a0e472f7f81d288cbd059ec" => :yosemite
     sha256 "dfb06c8602d8cb3a81a0d63127fc45c112bbdd494772f5ce50715f06383d596d" => :mavericks
