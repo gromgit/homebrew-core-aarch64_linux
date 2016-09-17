9b5a2ad1975fcf3ad5453e7d4eecdfc81a1f83ff
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 02:20:02 +0100

    xhyve: update 0.2.0 bottle.

--- Formula/xhyve.rb
+++ Formula/xhyve.rb
@@ -7,6 +7,7 @@ class Xhyve < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0a3d1c11832ff072a9d438fbca8e4cf23e706a2ab876341472c305c697c013fb" => :sierra
     sha256 "5eaf3b257fc5fce821c5704b79f5671e6e8e9e73b25525da4f66b8c5c4e3c1fb" => :el_capitan
     sha256 "4b7fe0a81da6d1a6777a42c41d3465d7777047a1ec9581fcfdef949e13d68010" => :yosemite
   end
