3b54ed35ce14a02bb0676bbd390d0d0e7014fdd3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 10:18:32 +0100

    vapoursynth: update 33.1 bottle.

--- Formula/vapoursynth.rb
+++ Formula/vapoursynth.rb
@@ -6,6 +6,7 @@ class Vapoursynth < Formula
   head "https://github.com/vapoursynth/vapoursynth.git"
 
   bottle do
+    sha256 "39a5933621f0656e858ba8352088f819bd9b4f4a048936caae682f39a19b0da7" => :sierra
     sha256 "3c7f28c274b0ba202f0f2a455c689621fa847d04af3ca5f1ada3fa7480f4cf19" => :el_capitan
     sha256 "e452d48c3a5fae1d64ac1ef210bbf779c13df47126573f142fa2481ad2c5ac9c" => :yosemite
     sha256 "8a019dc33d5697b8a57248be2996ac255fc4d21e3ba7c90c956f7806bf0b570f" => :mavericks
