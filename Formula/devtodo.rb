c79a94e6542399b1341092245b8508f3bf65f687
ilovezfs <ilovezfs@icloud.com>
2016-09-17 06:00:04 -0700

    devtodo: revision for readline

--- Formula/devtodo.rb
+++ Formula/devtodo.rb
@@ -3,6 +3,7 @@ class Devtodo < Formula
   homepage "http://swapoff.org/DevTodo"
   url "http://swapoff.org/files/devtodo/devtodo-0.1.20.tar.gz"
   sha256 "379c6ac4499fc97e9676075188f7217e324e7ece3fb6daeda7bf7969c7093e09"
+  revision 1
 
   bottle do
     sha256 "5b7ec857a599dd407518e922a093a46d5a9af55ce8469915dcc94793c3700c5c" => :el_capitan
