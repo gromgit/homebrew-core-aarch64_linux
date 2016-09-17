4d5c8fdf202babb5613fe3bab551c6bc8dd81999
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 04:08:24 +0100

    libftdi0: update 0.20 bottle.

--- Formula/libftdi0.rb
+++ Formula/libftdi0.rb
@@ -6,7 +6,8 @@ class Libftdi0 < Formula
 
   bottle do
     cellar :any
-    revision 2
+    rebuild 2
+    sha256 "f89e79f5eb94d55e130dcc55deca87f1241b386bf45070dda52c2c22707ead15" => :sierra
     sha256 "8fbc5ef456600b919430d86dd4e7090ae1f1064e4a51d631ab9bd7b39887ead2" => :el_capitan
     sha256 "92ecdb3a110e1abcba05561f0def8e573d1f8174d4e04247375dd5cd47d3bc24" => :yosemite
     sha256 "29d786dcb87e4251c4cbd25c0d5e215eb248b74304f175054b31cbd6a464c071" => :mavericks
