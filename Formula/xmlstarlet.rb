997786abfaca083c6daee1802f1c45588ed0fbbc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 10:18:32 +0100

    xmlstarlet: update 1.6.1 bottle.

--- Formula/xmlstarlet.rb
+++ Formula/xmlstarlet.rb
@@ -6,6 +6,7 @@ class Xmlstarlet < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4958bf868beefb9a2b391c0fe05f5289b67a4cded708d71c4cc5fced130bac55" => :sierra
     sha256 "2d9a9b5f875b91c78378e7f3df12595528d8e4b9ec9e321131b7f9f78f30acd8" => :el_capitan
     sha256 "525eafe6cab96cc6e04fef756e25316119b3c96cb61e5f7f51770cd062ad1bec" => :yosemite
     sha256 "7004b98b4dd9195a35f736ba3f6282369a2c63397a710056c5d1ae71d149fa3a" => :mavericks
