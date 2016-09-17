1e964acc3857470e2a2ec8f21c15c57df97ccbcb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 10:18:32 +0100

    opusfile: update 0.8 bottle.

--- Formula/opusfile.rb
+++ Formula/opusfile.rb
@@ -6,6 +6,7 @@ class Opusfile < Formula
 
   bottle do
     cellar :any
+    sha256 "60977347332d650de34fead40a69312d3eac3b00d8d7756726bef751bc9b4ca9" => :sierra
     sha256 "dc95a08834e93a459f47c0dc4114542b038a79d266c4722da27365879901b71a" => :el_capitan
     sha256 "f04e162a07dff00edb780fb3eed26084b7416debb33c13061d8d84d84d47fefc" => :yosemite
     sha256 "a44a9b6a34eae4f1a0c6c3b3c0fd1bae2e2c7893376e3b345d5498eee30a8622" => :mavericks
