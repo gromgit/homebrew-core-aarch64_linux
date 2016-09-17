eac6f65d1628fb22fb2f0c6e443a52f3d4e478a5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 05:40:17 +0100

    gource: update 0.44 bottle.

--- Formula/gource.rb
+++ Formula/gource.rb
@@ -5,6 +5,7 @@ class Gource < Formula
   sha256 "2604ca4442305ffdc5bb1a7bac07e223d59c846f93567be067e8dfe2f42f097c"
 
   bottle do
+    sha256 "b5cd5eb88c4f72c95672a82f28c540ce52e80ea491aee023640acc3fc5585139" => :sierra
     sha256 "5870be68c62621907fb0f3f04dfc0f93cf9c99f12d715e36a85cbd8fcd6abd0e" => :el_capitan
     sha256 "35ce40b076a186bceadd909edc82ce007d83372ed65832c473ea91174dd077b7" => :yosemite
     sha256 "1f33484c86e75e1be2ba4447f19c98645f5483119c4dba101e76213b425746ab" => :mavericks
