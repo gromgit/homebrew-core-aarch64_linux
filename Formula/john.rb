f040bb566f2edbf0090e05070a801259bfdc0297
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 09:43:08 +0100

    john: update 1.8.0 bottle.

--- Formula/john.rb
+++ Formula/john.rb
@@ -6,6 +6,7 @@ class John < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2574e6b0ae4e5906b1cb546b23dc74b06c0c3494d477b8ce0c1d743d1515bfee" => :sierra
     sha256 "729c644b587941668f0412de6a1d7aafc078b375f96421b278daecba51469ed8" => :el_capitan
     sha256 "1576ba09d791c923265c5668aa0a0d5e7d806848d45e06210c0e3a5449bf1403" => :yosemite
     sha256 "394909ad355846b561077f89a216fc87902d116dfcb970b92ff7f563a3d5ce3e" => :mavericks
