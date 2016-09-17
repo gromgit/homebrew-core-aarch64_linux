efebee0d4b0c0278d6e025b533ea439f3e3ea44b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 13:08:10 +0100

    httrack: update 3.48.21 bottle.

--- Formula/httrack.rb
+++ Formula/httrack.rb
@@ -7,7 +7,8 @@ class Httrack < Formula
   sha256 "871b60a1e22d7ac217e4e14ad4d562fbad5df7c370e845f1ecf5c0e4917be482"
 
   bottle do
-    revision 1
+    rebuild 1
+    sha256 "d01beef135e94fd249da1bee887da70a73694a08820512b146b4340b9530da3a" => :sierra
     sha256 "032f5fde39f04bdf772c223cdcd4974a7bb72bfcff0f71f34a8716870a80e37c" => :el_capitan
     sha256 "5dafcc2d3cf49fcbf3b804aacd42955d32cfff7643fdbb157ffbecbdaddfae68" => :yosemite
     sha256 "d6c9e71315a64a0fb1ac42e3515742f6d7b8dfac6ec1049a0f81e5aedd4f4364" => :mavericks
