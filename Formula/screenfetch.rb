fd21bf96b7e9248f87579be7887fe567dfc62cb4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 08:08:37 +0100

    screenfetch: update 3.7.0 bottle.

--- Formula/screenfetch.rb
+++ Formula/screenfetch.rb
@@ -7,6 +7,7 @@ class Screenfetch < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5a351a099b84c85e6923e0aecc1504dd8e6b58e4b7a17fcfe6c7ad06b1803dc0" => :sierra
     sha256 "a432e872bbe8d87199d49a9806350e52247bb591c6732e736f082cf12329050a" => :el_capitan
     sha256 "f06711aa9632682d30fc81ecec15eb9822ed1946fa39880ba18a9d57a27c66d9" => :yosemite
     sha256 "ec75963396cf12dfd7277d6149a2aa87dd63079ddaccb2a03fd04e55f163e36b" => :mavericks
