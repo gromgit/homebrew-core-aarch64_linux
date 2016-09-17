d6e637f75039757f6697b9887bbd443efa729369
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 01:57:47 +0100

    cmatrix: update 1.2a bottle.

--- Formula/cmatrix.rb
+++ Formula/cmatrix.rb
@@ -7,6 +7,7 @@ class Cmatrix < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ae46840a9d0e08909665694d161a3a8e0962a5936c523812057dc39d61eda8fd" => :sierra
     sha256 "da919a1964d6ef0633eac14bd7138ab91f6676d4dfc36fd5e27f956943714d22" => :el_capitan
     sha256 "14ae5c06eac81783ee61e3547d9de174f6742c688a254e172d7c2e566f14b426" => :yosemite
     sha256 "8479d25ddc608462c974bbc1a9fb229f6ffa99d19368fcd43f667bc6a6d8493f" => :mavericks
