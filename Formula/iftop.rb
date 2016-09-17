09c98752adf3de5e238f9894447fb728f7bcf35f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 01:21:46 +0100

    iftop: update 1.0pre4 bottle.

--- Formula/iftop.rb
+++ Formula/iftop.rb
@@ -9,6 +9,7 @@ class Iftop < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3a75c5edf29f6cbac19641910661423ed2f0b83f3e0de28c8417be76f4ce5c38" => :sierra
     sha256 "e7b9ed6bef435d7de8e986cedfc76779f2655bfac7ed780afb33a42e92b8d01d" => :el_capitan
     sha256 "5ac910598f86716e0dc676f0fd44af51da141ad84dfbaf5535a31f11ab91ffd7" => :yosemite
     sha256 "9563857745dca2db9f91388d3f0d0fae4b6cdc2fc6ff84ffce14325c0cafee0f" => :mavericks
