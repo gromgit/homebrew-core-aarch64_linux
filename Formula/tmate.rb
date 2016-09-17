384025dbe27b71f6fd4e5577ea41e93156a39cd2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 08:08:37 +0100

    tmate: update 2.2.1_2 bottle.

--- Formula/tmate.rb
+++ Formula/tmate.rb
@@ -9,6 +9,7 @@ class Tmate < Formula
 
   bottle do
     cellar :any
+    sha256 "b22bae010aea479918d31c42d1a243b2e70ca19af0a8bbb61b377dee79a6cc4b" => :sierra
     sha256 "cf21009a5a1d3d41e111d607d157510934d1c6db5fa1c72698af98a2d38ca5aa" => :el_capitan
     sha256 "f3f6a40c9393292a94862cd4f36f9a27e4d3c4077699b5196d417cc3d97a7daf" => :yosemite
     sha256 "2aa8a5a8934bbe0cc9271937500e26b97bedb7659a50560152bbd5f060b704ad" => :mavericks
