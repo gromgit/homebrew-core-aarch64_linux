336b510f2bfcf87b82a0b4de79c1c210a249fde0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 04:08:24 +0100

    physfs: update 2.0.3 bottle.

--- Formula/physfs.rb
+++ Formula/physfs.rb
@@ -7,7 +7,8 @@ class Physfs < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "c06c09374e9691665cd54947ba44b4d1f69be852c03f55148310db882912a3ff" => :sierra
     sha256 "d501cbfdfea7df8ae807f158ad428c02354366a546925a4d042ccb2f9eb30267" => :el_capitan
     sha256 "c53001feb6316238029050dd5f07cdb6a1f17a3c96df9a09b43f709a99b7504e" => :yosemite
     sha256 "c4d372b4db8a7b0ed8019562cebce7ac59b1778c8a88d27a0d6cd508607826b9" => :mavericks
