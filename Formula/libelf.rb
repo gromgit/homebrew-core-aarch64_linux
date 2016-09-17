24270769d211e5e969925c324c7cb27c0d1e693c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 05:40:18 +0100

    libelf: update 0.8.13_1 bottle.

--- Formula/libelf.rb
+++ Formula/libelf.rb
@@ -7,6 +7,7 @@ class Libelf < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a771e35555810a4910304e3ca5967ea3e4f8cbe45576e5b2dc6b80cd9c1f0f13" => :sierra
     sha256 "a06b058c7e401942f442f573b63aa2cdd548b45d38b02b7af92393c67093f56e" => :el_capitan
     sha256 "3b4ea9ab20228d9e912f80a330b6d6d093f9bb65a712208c83cd49bdcc4fc9ea" => :yosemite
     sha256 "eded3b774d412e533f37bc6d5dc133859141653ce953a0d4cbf4a950dda633f6" => :mavericks
