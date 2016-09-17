e598af55e6fd660ddbda117e6cbf40a5b497d46d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 09:43:07 +0100

    autogen: update 5.18.7 bottle.

--- Formula/autogen.rb
+++ Formula/autogen.rb
@@ -6,6 +6,7 @@ class Autogen < Formula
   sha256 "a7a580a5e18931cb341b255cec2fee2dfd81bea5ddbf0d8ad722703e19aaa405"
 
   bottle do
+    sha256 "34f6959cbd858533dd3d86fa88663247f619fe2650df135616f983479da6cb68" => :sierra
     sha256 "319d4d6c54c3025e590d15553c86ffbe5c2db1c2d7946d5cd8fb4a02786adc27" => :el_capitan
     sha256 "5805b867cab218e2a1a933f646344cec285d883c5c6c9f04dfb90795e21a7dcc" => :yosemite
     sha256 "7a00b94115673c045497246de4f1e5b90cb636ae10fa3aea1409f7e4be52c2dc" => :mavericks
