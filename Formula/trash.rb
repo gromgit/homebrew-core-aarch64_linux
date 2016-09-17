173f64c4d12f4887670e94c6e3cb9f927097ca0f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 01:21:47 +0100

    trash: update 0.8.5 bottle.

--- Formula/trash.rb
+++ Formula/trash.rb
@@ -6,6 +6,7 @@ class Trash < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a00374dc159ab870e2c1267280d63e3ac5f7e7256e300fcb282ad6638abd02f0" => :sierra
     sha256 "4a890c2c7e5fe136eff1df5552fc94e1f56c46f41ccfa43bc5892de08329a3f4" => :el_capitan
     sha256 "05ae485401cd5becced038755656b120b74e9cffd15358724ea5f7eea2411972" => :yosemite
     sha256 "f74b3d47c9208cb0ccee9d017b146052df8748f3bc4a2bb4525a1d6c5e55909a" => :mavericks
