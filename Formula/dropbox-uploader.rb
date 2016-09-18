ef560dd1994c79d8248cc3a6313eb5fdf4b2315e
ilovezfs <ilovezfs@icloud.com>
2016-09-18 06:22:06 -0700

    dropbox-uploader 1.0
    Closes #4944.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dropbox-uploader.rb
+++ Formula/dropbox-uploader.rb
@@ -1,8 +1,8 @@
 class DropboxUploader < Formula
   desc "Bash script for interacting with Dropbox"
   homepage "https://www.andreafabrizi.it/?dropbox_uploader"
-  url "https://github.com/andreafabrizi/Dropbox-Uploader/archive/0.16.tar.gz"
-  sha256 "738aebdb57f75e9033cb9bb319b14fe1c3b471f951180acb48631e9810ebf1c3"
+  url "https://github.com/andreafabrizi/Dropbox-Uploader/archive/1.0.tar.gz"
+  sha256 "8c9be8bd38fb3b0f0b4d1a863132ad38c8299ac62ecfbd1e818addf32b48d84c"
 
   bottle :unneeded
 
