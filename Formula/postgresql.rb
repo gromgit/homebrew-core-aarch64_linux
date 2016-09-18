6e30897ec41f684cba9d755ba80f9047c7bbc84d
ilovezfs <ilovezfs@icloud.com>
2016-09-18 09:41:11 -0700

    postgresql: revision for readline

--- Formula/postgresql.rb
+++ Formula/postgresql.rb
@@ -3,6 +3,7 @@ class Postgresql < Formula
   homepage "https://www.postgresql.org/"
   url "https://ftp.postgresql.org/pub/source/v9.5.4/postgresql-9.5.4.tar.bz2"
   sha256 "cf5e571164ad66028ecd7dd8819e3765470d45bcd440d258b686be7e69c76ed0"
+  revision 1
 
   bottle do
     rebuild 1
