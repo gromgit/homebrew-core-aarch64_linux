b3659e0b92e791cd01a23707ee1714b6beb90235
ilovezfs <ilovezfs@icloud.com>
2016-09-18 03:08:26 -0700

    gjs: revision for readline

--- Formula/gjs.rb
+++ Formula/gjs.rb
@@ -3,6 +3,7 @@ class Gjs < Formula
   homepage "https://wiki.gnome.org/Projects/Gjs"
   url "https://download.gnome.org/sources/gjs/1.44/gjs-1.44.0.tar.xz"
   sha256 "88c960f6ad47a6931d123f5d6317d13704f58572f68a4391913a254ff27dce80"
+  revision 1
 
   bottle do
     rebuild 1
