f957402171a229b6f990d5629ec065eb52bce101
DT <dmofot@gmail.com>
2016-09-19 14:53:47 -0400

    shpotify 1.2.0 (#4983)

--- Formula/shpotify.rb
+++ Formula/shpotify.rb
@@ -1,8 +1,8 @@
 class Shpotify < Formula
   desc "Command-line interface for Spotify on a Mac"
   homepage "https://harishnarayanan.org/projects/shpotify/"
-  url "https://github.com/hnarayanan/shpotify/archive/1.1.0.tar.gz"
-  sha256 "825de0c1100b3f596e635765b02f65c73919a26990d5d4520e87afea45147987"
+  url "https://github.com/hnarayanan/shpotify/archive/1.2.0.tar.gz"
+  sha256 "5de136b3f01d63de2677ed443f8b398c8add3e2e7343798dac3bec0dd6607955"
   head "https://github.com/hnarayanan/shpotify.git"
 
   bottle :unneeded
