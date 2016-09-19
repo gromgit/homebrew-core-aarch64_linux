128e33259423e7f9c3ef042533798c3dadd7a0a5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-19 09:12:43 +0100

    pkg-config: update 0.29.1_2 bottle.

--- Formula/pkg-config.rb
+++ Formula/pkg-config.rb
@@ -7,10 +7,9 @@ class PkgConfig < Formula
   revision 2
 
   bottle do
-    sha256 "236a2aa2b7f414ac36279cf5b11b9f64429c5224d8e765224a6315061f39bbaa" => :sierra
-    sha256 "7354d2e9f337af437a22433926dd38a68413a8d8bef3aac52f203ae581d7669d" => :el_capitan
-    sha256 "d84a72010d87c3f517a53b72a266bd5a4674efcf398eb94382621d2ee939a6b4" => :yosemite
-    sha256 "7fe94019f0b3d0417bc9fffdaed1b066290cb0c051282bf6799cc589b80bb9ec" => :mavericks
+    sha256 "aa5f169cffe4576a6a6b0e3d528ab2ee0ac331ff3fe51bfbb182fb4ef7cdb794" => :sierra
+    sha256 "6c89838a29bea6a641b611e631de3765b7755d987029de34419e04d85191d8a5" => :el_capitan
+    sha256 "fc68806653ba68fe742885cc050990433125588099d2ea30139d1663d3d4311a" => :yosemite
   end
 
   def install
