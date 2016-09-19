fc419d811ac6e2daa260dd943dc8abbaeab7cf0e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-19 06:14:43 +0100

    par: update 1.52 bottle.

--- Formula/par.rb
+++ Formula/par.rb
@@ -7,10 +7,10 @@ class Par < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
-    sha256 "2dd2b1808f820354b03927fc30b11eeb33bd5704877756938cb8c5daac18a393" => :el_capitan
-    sha256 "195a82f58ad34fe5d4e3c061a6d83909017861ede1e8af5668ef2062e0c0bdcb" => :yosemite
-    sha256 "3d666b815997eda86ee0f96e71ba6fca3193432c5a01affb59918f22408c8dc6" => :mavericks
+    rebuild 2
+    sha256 "efa3ba3bdd3b34ad8e5089b8cd5562d8b8cf4a5e5488e54e43dfb45760a1b4fa" => :sierra
+    sha256 "3683d5918dc91fcd073fc8e35af2fca416b3756aff8479ff549598bcd2500e8b" => :el_capitan
+    sha256 "cb1042ef12ead6645653775571ebe84798b707194922030563ff4056687954e3" => :yosemite
   end
 
   conflicts_with "rancid", because: "both install `par` binaries"
