68390ca2c7a5d052c4a35c15f8f1a54be7d779cd
Jean-Philippe Ouellet <jpo@vt.edu>
2016-09-20 13:08:01 -0400

    signify-osx 1.3
    Closes #5038.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/signify-osx.rb
+++ Formula/signify-osx.rb
@@ -1,8 +1,8 @@
 class SignifyOsx < Formula
   desc "Cryptographically sign and verify files"
   homepage "http://man.openbsd.org/OpenBSD-current/man1/signify.1"
-  url "https://github.com/jpouellet/signify-osx/archive/1.2.tar.gz"
-  sha256 "ac6c0cffc098abe5ad6ec444ff2d6eaf5cc61c84cbedf4f5d65766454b5f34fd"
+  url "https://github.com/jpouellet/signify-osx/archive/1.3.tar.gz"
+  sha256 "c67090135a55478a6a6c11d507d9c3865a11de665c010a8a5f2457737f277f89"
   head "https://github.com/jpouellet/signify-osx.git"
 
   bottle do
