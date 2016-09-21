32809d24f4a3d6b019b92326e29adc4d81540d5d
Matt Robenolt <matt@ydekproductions.com>
2016-09-21 00:22:39 -0700

    carina: update 1.5.0 url revision
    Confirmed upstream retag.
    
    Closes #5047.
    
    Signed-off-by: Zhiming Wang <zmwangx@gmail.com>

--- Formula/carina.rb
+++ Formula/carina.rb
@@ -3,7 +3,7 @@ class Carina < Formula
   homepage "https://github.com/getcarina/carina"
   url "https://github.com/getcarina/carina.git",
         tag: "v1.5.0",
-        revision: "621b6ecc9d34bad00b016a9829210b33fd4f2355"
+        revision: "2d7f5c0afe28a3e9e794c99ce5024be283979e71"
   head "https://github.com/getcarina/carina.git"
 
   bottle do
