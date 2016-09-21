47662e3a41c8a28377f48086ae85523561ff6d05
ilovezfs <ilovezfs@icloud.com>
2016-09-21 03:06:16 -0700

    proguard 5.3
    Closes #5063.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/proguard.rb
+++ Formula/proguard.rb
@@ -1,8 +1,8 @@
 class Proguard < Formula
   desc "Java class file shrinker, optimizer, and obfuscator"
   homepage "http://proguard.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/proguard/proguard/5.2/proguard5.2.1.tar.gz"
-  sha256 "162fb2816212c6a7a195884a01ff826920919e97f57914a5b00bdf7641fc00f6"
+  url "https://downloads.sourceforge.net/project/proguard/proguard/5.3/proguard5.3.tar.gz"
+  sha256 "8f185c343dcc4504b3c496bdbf870feba3523abe7cec060b44bbacd1fc2da955"
 
   bottle :unneeded
 
