3bc2d98f61bf96e7950198f3b9b0510758dcf597
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-22 18:34:33 +0100

    lnav: update 0.8.1 bottle.

--- Formula/lnav.rb
+++ Formula/lnav.rb
@@ -5,9 +5,10 @@ class Lnav < Formula
   sha256 "db942abccdb5327d7594ca9e32e0b44802790fad8577bdbed44f81220fd62153"
 
   bottle do
-    sha256 "5eb0e048b35a7ee79b36691ca0678163961ee8f3e8c5841e88c90cf69a07b41c" => :el_capitan
-    sha256 "35bf1e6722eed4c8a1c5bdffbce5fed4257ff5aee1eb1e3c54208e9392a32b21" => :yosemite
-    sha256 "fdcbbcf3db3cffd0aa7ae5d4c9c1320beb8a35a0f3f66f18f32ff675ea3d9418" => :mavericks
+    rebuild 1
+    sha256 "c8a5b93c1995c7dd6bc65bd93ff87b766d7e51af31e13cd1dadef0650e2c42f6" => :sierra
+    sha256 "382301cd4f6bbf7f1161cbe602f89933c622890a1e992b103f2f5de6f98b353a" => :el_capitan
+    sha256 "46e86d46607c0a8dc876354eecb08cb3b6216f33f7fc13f83a330288c521ba04" => :yosemite
   end
 
   head do
