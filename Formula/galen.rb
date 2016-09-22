3854f58efb24ce8b55f91cc8fdb34d5712146e00
ilovezfs <ilovezfs@icloud.com>
2016-09-22 02:33:14 -0700

    galen 2.3.1
    Closes #5098.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/galen.rb
+++ Formula/galen.rb
@@ -1,8 +1,8 @@
 class Galen < Formula
   desc "Automated testing of look and feel for responsive websites"
   homepage "http://galenframework.com/"
-  url "https://github.com/galenframework/galen/releases/download/galen-2.3.0/galen-bin-2.3.0.zip"
-  sha256 "2a3333c3313eaed6566125d688d3ef9de4c9352903c871ff5ff3e02807dce783"
+  url "https://github.com/galenframework/galen/releases/download/galen-2.3.1/galen-bin-2.3.1.zip"
+  sha256 "5a11b8471fd919635655ad25443e7cfac27c76766ee61022b5c58f39894e09c2"
 
   bottle :unneeded
 
