1b7f69f34b6b7790241d8546f49473886f277cc2
Martin Schürrer <martin@schuerrer.org>
2016-09-22 15:11:07 +0200

    erlang 19.1
    Closes #5114.
    
    Signed-off-by: Zhiming Wang <zmwangx@gmail.com>

--- Formula/erlang.rb
+++ Formula/erlang.rb
@@ -4,15 +4,10 @@
 class Erlang < Formula
   desc "Programming language for highly scalable real-time systems"
   homepage "https://www.erlang.org/"
-  revision 1
-
+  # Download tarball from GitHub; it is served faster than the official tarball.
+  url "https://github.com/erlang/otp/archive/OTP-19.1.tar.gz"
+  sha256 "caf320c07bdd4c6e11831a0b0d25645a29112007077dbf11eec22437f8b041ed"
   head "https://github.com/erlang/otp.git"
-
-  stable do
-    # Download tarball from GitHub; it is served faster than the official tarball.
-    url "https://github.com/erlang/otp/archive/OTP-19.0.2.tar.gz"
-    sha256 "d0ec363d460994e63ef984c2367598990978ffe1d41bb0e0c25520a0ee1dab21"
-  end
 
   bottle do
     cellar :any
@@ -41,13 +36,13 @@ class Erlang < Formula
   fails_with :llvm
 
   resource "man" do
-    url "https://www.erlang.org/download/otp_doc_man_19.0.tar.gz"
-    sha256 "c7a3d6d85a5a2b96d844297a3fa1bee448c3dd86237734688466249fd5a1401e"
+    url "https://www.erlang.org/download/otp_doc_man_19.1.tar.gz"
+    sha256 "7200e9e5b3a229a6b3838046e1b3e64afc869265539d49d0e4853212f19c0c79"
   end
 
   resource "html" do
-    url "https://www.erlang.org/download/otp_doc_html_19.0.tar.gz"
-    sha256 "b6f7c4e964673333f6c3eea8530dd988b41339b8912ae293f6f1b429489159ff"
+    url "https://www.erlang.org/download/otp_doc_html_19.1.tar.gz"
+    sha256 "76c89aee1ac69b5107114b0065189101e3f42d53e929d8ef8f08b5c586f15930"
   end
 
   def install
