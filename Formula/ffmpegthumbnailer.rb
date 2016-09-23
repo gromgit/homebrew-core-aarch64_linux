26b3ef8f08122ca5c1f4ce8346bfdd0930186ad6
Mike McQuaid <mike@mikemcquaid.com>
2016-09-23 19:57:07 +0100

    ffmpegthumbnailer: fix rubocop warnings.

--- Formula/ffmpegthumbnailer.rb
+++ Formula/ffmpegthumbnailer.rb
@@ -33,9 +33,10 @@ class Ffmpegthumbnailer < Formula
   test do
     f = Formula["ffmpeg"].opt_bin/"ffmpeg"
     png = test_fixtures("test.png")
-    system *%W[#{f} -loop 1 -i #{png} -c:v libx264 -t 30 -pix_fmt yuv420p v.mp4]
+    system f.to_s, "-loop", "1", "-i", png.to_s, "-c:v", "libx264", "-t", "30",
+                   "-pix_fmt", "yuv420p", "v.mp4"
     assert File.exist?("v.mp4"), "Failed to generate source video!"
-    system *%W[#{bin}/ffmpegthumbnailer -i v.mp4 -o out.jpg]
+    system "#{bin}/ffmpegthumbnailer", "-i", "v.mp4", "-o", "out.jpg"
     assert File.exist?("out.jpg"), "Failed to create thumbnail!"
   end
 end
