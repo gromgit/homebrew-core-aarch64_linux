29d7f71205b20b217404a904f533d0fbc6829051
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-23 12:19:56 +0100

    tbb: update 4.4-20160526 bottle.

--- Formula/tbb.rb
+++ Formula/tbb.rb
@@ -7,11 +7,10 @@ class Tbb < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "db0eab0393252553a2d0d409008281c1c71bd5e3b84d7d11f61c360f51704988" => :sierra
-    sha256 "e005a2ed49deb9f4594cf99e0294f964da1cc4af14e4035d04b499c55a8ad628" => :el_capitan
-    sha256 "9b956c24106f29433ff5b3e8ea3f94d2e1b5b800a1eb32d697fdf60e8978768b" => :yosemite
-    sha256 "0dd424959052ce80bf4a5f6a37254f15009ca9d450d8c799145fff55efc5268d" => :mavericks
+    rebuild 2
+    sha256 "36b58a15f159a9e80b3d681ddb0861db319cc7f64644db3102d54778ffe2878b" => :sierra
+    sha256 "fb0e2ad65a78c7402bd45c932d9c05a2fdebc62ad19d09166bec235909a1f0a6" => :el_capitan
+    sha256 "68c2752f7bdb5ea84bf205a3ed73fd7ccb3fc4473bb56a6366084c9ff6291075" => :yosemite
   end
 
   option :cxx11
