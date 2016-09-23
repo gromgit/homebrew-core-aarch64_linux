182f6e183579a9efa5ea9c7b9d84fbf4a239b0bb
Dominyk Tiller <dominyktiller@gmail.com>
2016-09-23 13:13:09 +0100

    openssl@1.1: bump revision for bottle issue
    Closes #5149.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/openssl@1.1.rb
+++ Formula/openssl@1.1.rb
@@ -4,6 +4,7 @@ class OpensslAT11 < Formula
   url "https://www.openssl.org/source/openssl-1.1.0a.tar.gz"
   mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.1.0a.tar.gz"
   sha256 "c2e696e34296cde2c9ec5dcdad9e4f042cd703932591d395c389de488302442b"
+  revision 1
   version_scheme 1
 
   bottle do
