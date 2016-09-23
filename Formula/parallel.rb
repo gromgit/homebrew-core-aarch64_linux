d6d350ccf453a02446ffcbe6d7034ed87024ee30
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-23 11:11:02 +0100

    parallel: update 20160922 bottle.

--- Formula/parallel.rb
+++ Formula/parallel.rb
@@ -8,10 +8,9 @@ class Parallel < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "dfbd47a069df6fec320b56ce0246280a1338af106dfa7fe407801e5e07ad6af2" => :sierra
-    sha256 "9b28e16fc1227835d55209cce6a21d4cd6e681316571463a5554481c43367b49" => :el_capitan
-    sha256 "dfbd47a069df6fec320b56ce0246280a1338af106dfa7fe407801e5e07ad6af2" => :yosemite
-    sha256 "dfbd47a069df6fec320b56ce0246280a1338af106dfa7fe407801e5e07ad6af2" => :mavericks
+    sha256 "6192d6eb299ced5a0a5ef854f3d889b6819e507319c8a6c32e1057f971ab3532" => :sierra
+    sha256 "6192d6eb299ced5a0a5ef854f3d889b6819e507319c8a6c32e1057f971ab3532" => :el_capitan
+    sha256 "6192d6eb299ced5a0a5ef854f3d889b6819e507319c8a6c32e1057f971ab3532" => :yosemite
   end
 
   conflicts_with "moreutils", because: "both install a 'parallel' executable."
