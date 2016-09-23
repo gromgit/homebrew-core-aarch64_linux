9e3a66b6b7ca978bfea86897dcc3391c37f9f0ef
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-23 22:27:36 +0100

    gpgme: update 1.7.0 bottle.

--- Formula/gpgme.rb
+++ Formula/gpgme.rb
@@ -7,10 +7,9 @@ class Gpgme < Formula
 
   bottle do
     cellar :any
-    sha256 "dd747d6498ee88f0ac6822ec1e2a0ac93d1b38343658c2c9d448110793134b39" => :sierra
-    sha256 "d909ec33dd574c20f2d5b24bb90588ee811a9c3c9cdc6bb11827b17af73458d2" => :el_capitan
-    sha256 "f9976c4883fca4da0343dded00f144c4a2bfe5d814981e2bd42e000c31bbd181" => :yosemite
-    sha256 "c304e956d73cca79685225902ec6989de8d9feb54d63b60e8999e290ea23c55a" => :mavericks
+    sha256 "0b4a59398bd222620a4d837d7e33ca3141dddd14886c68bccca15a177bda93c7" => :sierra
+    sha256 "9764f539b4b51161127d95687c5611f9571ae2abde1dce66ba014a0f33d2bcc1" => :el_capitan
+    sha256 "4a4c3e90175b02ded974a8a385b6d9799bcc2873c5a1087c558f350eb103934d" => :yosemite
   end
 
   depends_on "gnupg2"
