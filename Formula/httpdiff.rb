7cb192968dbb6d646c642769a0476f68ac6facf2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-24 19:39:26 +0100

    httpdiff: add 1.0.0 bottle.

--- Formula/httpdiff.rb
+++ Formula/httpdiff.rb
@@ -6,6 +6,13 @@ class Httpdiff < Formula
 
   head "https://github.com/jgrahamc/httpdiff.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "39a0d685904aba4c3e55ff22b4d231b8890c022a1eb0366dc264bbabc410a117" => :sierra
+    sha256 "59b46605118f8789c10facd53e9d4ce4c9f54c8de85611d423984c4316a169eb" => :el_capitan
+    sha256 "d5919069e31192cfd6f7d33dd4ff80d2142a8c36d23b50291e914c158d91ffac" => :yosemite
+  end
+
   depends_on "go" => :build
 
   def install
