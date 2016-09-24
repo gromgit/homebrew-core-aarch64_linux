403ca3f429fe542342cc0aa3023018af2e1e4ce0
ilovezfs <ilovezfs@icloud.com>
2016-09-24 00:06:13 -0700

    tailor 0.11.0
    Closes #5164.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/tailor.rb
+++ Formula/tailor.rb
@@ -1,8 +1,8 @@
 class Tailor < Formula
   desc "Cross-platform static analyzer and linter for Swift"
   homepage "https://tailor.sh"
-  url "https://github.com/sleekbyte/tailor/releases/download/v0.10.1/tailor-0.10.1.tar"
-  sha256 "3960aca4218a773170fb9ac51c7b17b816b2a967c01d6277d45998ebffc961b4"
+  url "https://github.com/sleekbyte/tailor/releases/download/v0.11.0/tailor-0.11.0.tar"
+  sha256 "1e6ceda8bc36c2a4386ec1bcd363e15aaf30be1d36c67b356dc2f711e5a47d6c"
 
   bottle :unneeded
 
