dcad992a6dab919e1c2186fd2c94f50d30f616eb
ilovezfs <ilovezfs@icloud.com>
2016-09-24 04:23:15 -0700

    upscaledb: revision for protobuf

--- Formula/upscaledb.rb
+++ Formula/upscaledb.rb
@@ -3,7 +3,7 @@ class Upscaledb < Formula
   homepage "https://upscaledb.com/"
   url "http://files.upscaledb.com/dl/upscaledb-2.2.0.tar.gz"
   sha256 "7d0d1ace47847a0f95a9138637fcaaf78b897ef682053e405e2c0865ecfd253e"
-  revision 2
+  revision 3
 
   bottle do
     cellar :any
