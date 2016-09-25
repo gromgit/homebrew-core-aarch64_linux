02db6997a66cbda29416b89735cbf161107f6a3d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 07:58:12 +0100

    duff: update 0.5.2 bottle.

--- Formula/duff.rb
+++ Formula/duff.rb
@@ -6,6 +6,7 @@ class Duff < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2af1262a9b02e687c0efc14eed3d837920ab746fe8fca9b12b9361c4729f06ef" => :sierra
     sha256 "8a469e92a6303d80752ebc80ade382261d263b9c7226ca6652eddc8954e5ff2f" => :el_capitan
     sha256 "927ba61ce39cf9be33f796197063b1a6865bbc2db2f4b1340ad6786acf0494df" => :yosemite
     sha256 "a66cbddeb95dd67055ef6db7acf729a925427778d6cc88ed2ef52a2dd40b7856" => :mavericks
