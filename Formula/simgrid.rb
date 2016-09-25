17a21c34ea3aba9c42e21e6ab440863dc83cace0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 20:40:01 +0100

    simgrid: update 3.11.1 bottle.

--- Formula/simgrid.rb
+++ Formula/simgrid.rb
@@ -6,7 +6,8 @@ class Simgrid < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "805c0f23b83c5f0808fe810e91d98e8c88a55acc1292481dc4aaa003b43aceb5" => :sierra
     sha256 "37282c46131c5cfed6c3a12be3cb05487b800904a38c46cf3133a5b9a335d2fa" => :el_capitan
     sha256 "ade94652c0b0157914e4a6e1e637cae0fedf57e036245680595d0913e7df815a" => :yosemite
     sha256 "e36538092a178bed8956c4537095a17feb594846ee0dfe72c9baec8e9a52eb80" => :mavericks
