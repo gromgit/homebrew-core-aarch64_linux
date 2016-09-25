49e40547bbb88dee65a5a033c9a771cf7ee12ce6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 22:16:48 +0100

    aften: update 0.0.8 bottle.

--- Formula/aften.rb
+++ Formula/aften.rb
@@ -6,6 +6,7 @@ class Aften < Formula
 
   bottle do
     cellar :any
+    sha256 "535ef47b08163c8d1d7a66ffda7d3f280c0569a74d9feedbcfc93cd3c55194ca" => :sierra
     sha256 "68b4983cc843e2d57854a263038a965a2dd6c473c98111f482ec1c69d09ace83" => :el_capitan
     sha256 "4f785f04a3bbde677452f2c5d1c04f77605e156b4020294c5799c85d0b8586d3" => :yosemite
     sha256 "b7acaf77ece8e6b51493ce69e713990a4ce13bc5b9d5ad6914cc86c0f745c9d0" => :mavericks
