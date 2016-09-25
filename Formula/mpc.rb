b929f8a112f3a77df2d2ec81c68004b365edc91f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 08:43:32 +0100

    mpc: update 0.28 bottle.

--- Formula/mpc.rb
+++ Formula/mpc.rb
@@ -6,6 +6,7 @@ class Mpc < Formula
 
   bottle do
     cellar :any
+    sha256 "26f8fd128483fa5ce7b3543f4152c1f1ee49ea6e2ec8f5e59874991d84627e96" => :sierra
     sha256 "40b5cd34fd7b212a4d83476f0511bdd50bdc7533450665fc1ca5f0f008d510b7" => :el_capitan
     sha256 "b521e1e20acbe6b1cfb0bc52cd810e8fc8c5b8ae69441964122606c81bfc0dea" => :yosemite
     sha256 "8e96cbd74506a8d2b86e3fab070bdb36a30fe2fba987d928053d160e5b036f6f" => :mavericks
