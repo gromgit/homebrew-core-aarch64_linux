cf207100df46f3a17d41ba5db99669dd7aefbf04
ilovezfs <ilovezfs@icloud.com>
2016-09-25 04:33:48 -0700

    plowshare: depend on libcaca instead of aview
    Closes #5199.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/plowshare.rb
+++ Formula/plowshare.rb
@@ -3,6 +3,7 @@ class Plowshare < Formula
   homepage "https://github.com/mcrapet/plowshare"
   url "https://github.com/mcrapet/plowshare/archive/v2.1.5.tar.gz"
   sha256 "31a1d379b738b007ff000107b03562bf73ed5f05d7fa1ebef50082f0799a59ce"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -11,11 +12,11 @@ class Plowshare < Formula
     sha256 "dd48bc1601b784affe7911585f1ff9ee02dc16c2202fd94c20d26e157beb4d98" => :mavericks
   end
 
-  depends_on "aview"
   depends_on "bash"
   depends_on "coreutils"
   depends_on "gnu-sed"
   depends_on "imagemagick" => "with-x11"
+  depends_on "libcaca"
   depends_on "recode"
   depends_on "spidermonkey"
 
