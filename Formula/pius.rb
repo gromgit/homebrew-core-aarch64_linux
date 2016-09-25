42f8a7bd312875f3fe1a8dd89211869c8d4e0258
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 05:24:14 +0100

    pius: update 2.2.2_1 bottle.

--- Formula/pius.rb
+++ Formula/pius.rb
@@ -11,6 +11,7 @@ class Pius < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "19db3d8573539c391c13e4f168c61ccc07a8d275e397207e51d04fee4a2f695f" => :sierra
     sha256 "47a73a40992522c5834b81bc8f6888db64b2ae381e6e2287fa1e1fac366347b7" => :el_capitan
     sha256 "8d9e44165fa23aa04484118977188186ff0becd0fbf6cbd498ea88121322ac15" => :yosemite
     sha256 "8879e7d0fc7970e838091dfaed8b1d36676528cc185cdda4908c63858b8b124b" => :mavericks
