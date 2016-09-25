129fb63ef537b60f591c6aa815145d635e67fe45
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 14:27:04 +0100

    chicken: update 4.11.0 bottle.

--- Formula/chicken.rb
+++ Formula/chicken.rb
@@ -6,6 +6,7 @@ class Chicken < Formula
   head "https://code.call-cc.org/git/chicken-core.git"
 
   bottle do
+    sha256 "ebe35d692b413a147a60666e8242f41c278ef62d435c546a7d7b63d40dc1e43f" => :sierra
     sha256 "46cf38e5f44a23d9fbbe9a833fba73297c54721387e74da4349e82997a5c657b" => :el_capitan
     sha256 "972a4c6c87fc3ea8d23312a76d650da696ebb66a2905ecf792f1e762dc21ea1e" => :yosemite
     sha256 "1521fde608c55c8fcf182a341f90c96274c7a6f5eb07ad75f437a6a3fde44ad5" => :mavericks
