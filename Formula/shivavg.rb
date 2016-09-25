2a2572c76f19a8a0dcad02d6b2c214488bfa7f6d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 11:31:11 +0100

    shivavg: update 0.2.1 bottle.

--- Formula/shivavg.rb
+++ Formula/shivavg.rb
@@ -6,6 +6,7 @@ class Shivavg < Formula
 
   bottle do
     cellar :any
+    sha256 "f92bdb7b86632d7bf59d25259e26eece00e502759dd52adaac7495424290da4a" => :sierra
     sha256 "3e9de2887110c90051ad5b89080f62cd5990ae39f8fdef02a4c50ba11e413ca8" => :el_capitan
     sha256 "f3de3b35fcfeff41c1836bc7722579d1d4b461e10d4152802bae1ab48b5a3bbb" => :yosemite
     sha256 "e9bdb03d76c994311a7e38c95eb40561bcb8f7fefdc12ac80137724f39a6bb4a" => :mavericks
