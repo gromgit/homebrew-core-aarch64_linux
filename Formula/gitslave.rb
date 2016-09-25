ab3a03bb5b2ee2fe4aa2485fe7b407d243af5dc1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 08:27:38 +0100

    gitslave: update 2.0.2 bottle.

--- Formula/gitslave.rb
+++ Formula/gitslave.rb
@@ -6,6 +6,7 @@ class Gitslave < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3ccd021a4393d137eed5c0dfdfe94b325b6142258a7090ad04f9166039efa52d" => :sierra
     sha256 "e556bf6f7ddfa3e9f6a9b726d80a35404270c96e36ada122fd16d8946394aaa6" => :el_capitan
     sha256 "395794a75f26acdf034f4ab1541cd9af327d13309517e2553bbcb1fdb4bb0f85" => :yosemite
     sha256 "f960d16d68868685850464321b5e4f82be4b85b1e3baa9392f185773818e596f" => :mavericks
