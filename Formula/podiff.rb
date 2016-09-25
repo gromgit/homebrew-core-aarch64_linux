4b72275db267b5c3b3bdf2006917127f5b91bd87
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 00:21:28 +0100

    podiff: update 1.1 bottle.

--- Formula/podiff.rb
+++ Formula/podiff.rb
@@ -6,6 +6,7 @@ class Podiff < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "be964cc5259d37f0f4676d5e3a0fac77b42e4f3e7641c7579d47182e4f036e83" => :sierra
     sha256 "24f385afff1c9074d995cd1374c005e8770a80ef11b06058f6024eee67a9de69" => :el_capitan
     sha256 "21cc248bf36ce685061e234f57c6fc5cbe0d207b201e7a8e485ee7b71d3d21c6" => :yosemite
     sha256 "c3a3b66344303ad473d8c6808eb9e2a2124bb0a801536870b739beba4de3d5ae" => :mavericks
