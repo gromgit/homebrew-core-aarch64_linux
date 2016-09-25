b34c52d8ad942593cd936f65441edfc5552d5515
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 11:31:11 +0100

    dvdauthor: update 0.7.1_1 bottle.

--- Formula/dvdauthor.rb
+++ Formula/dvdauthor.rb
@@ -7,6 +7,7 @@ class Dvdauthor < Formula
 
   bottle do
     cellar :any
+    sha256 "bd87b4f4e8c33a7a8275ea3b34c8e995f58b61d71a8a7b726d35639868b447c9" => :sierra
     sha256 "f295c54bd810948d3acf3223d7132c036acd9842a4c7fc449ac111f5e6971d9f" => :el_capitan
     sha256 "e91dd3ffd68dd00a08d6c2f72e395dbd95c83a2057ebe19bf0310d1abba4fa04" => :mavericks
   end
