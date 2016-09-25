75e7db79729decf7038af2b15b2d2d8a3bc63d67
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 04:26:38 +0100

    ttf2eot: update 0.0.2-2 bottle.

--- Formula/ttf2eot.rb
+++ Formula/ttf2eot.rb
@@ -6,6 +6,7 @@ class Ttf2eot < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "85a5617fe0207bf48cb27cac94d9c9b5c2b3ea812a83c71b9d4017b55969302a" => :sierra
     sha256 "e6d90a726548a3321d33135538390ff4bcfda18faf01f97fdea6e3dbd2dee165" => :el_capitan
     sha256 "e0c767aefbe0c95c28c07efdd63a86fb397e0bcb6b42173ff6792ec216aa063f" => :yosemite
     sha256 "8091c9f2a8b3c75c28d4f646ef15e42d6a205e5beeea19d72dc2883623dc5cd6" => :mavericks
