741646a4662fc7ae40a644bfd29c49cb79c19af8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 04:46:22 +0100

    ttf2pt1: update 3.4.4 bottle.

--- Formula/ttf2pt1.rb
+++ Formula/ttf2pt1.rb
@@ -6,6 +6,7 @@ class Ttf2pt1 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e70efa3a1b28b212ea2366ac50b33fbf48e9b7922d03f1a6b86965af87244bee" => :sierra
     sha256 "0ef606dfb439ad46c5442b35458f009e864ee3270145c7be940581a5d272bc54" => :el_capitan
     sha256 "65c1456cab73a91161e4dddbc4f04842029a810a8e4e4c396e90fbf039e61f60" => :yosemite
     sha256 "c81f56318e2311c422c1a53647650478660ec16fc935380c01cf10c1f53edd73" => :mavericks
