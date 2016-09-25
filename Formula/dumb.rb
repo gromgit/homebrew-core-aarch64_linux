c4fe2df42565318d0bdc8f86d22a6818b6564c66
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 08:27:38 +0100

    dumb: update 0.9.3 bottle.

--- Formula/dumb.rb
+++ Formula/dumb.rb
@@ -6,6 +6,7 @@ class Dumb < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "04219fcc6bf6cd174cb5c2ddde4bfdbff266ed665e543c9948911e731d682dc9" => :sierra
     sha256 "d2352df11bee735e963b887609578ec1b3acf0e07748385f472a6add0e1cd2b6" => :el_capitan
     sha256 "317ac8139d8efb03022bb4f9a76ad61f2358570680563924d13229c52b282dff" => :yosemite
     sha256 "ee41051ed609807bfb8ce774fa614869db6bdd0fe9a307f6f2cb89f99e9db78e" => :mavericks
