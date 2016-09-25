1be3e03221a74e8833ec5381f0c93e3892190cd8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 21:40:20 +0100

    udptunnel: update 1.1 bottle.

--- Formula/udptunnel.rb
+++ Formula/udptunnel.rb
@@ -6,6 +6,7 @@ class Udptunnel < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c129b87c78c3297365d7e78b77985b69deb4def8d030b029d812bb25610b8bb0" => :sierra
     sha256 "acbc74c384071aa6be92bea754274c5025de0123afd389d1b174e444a7beec42" => :el_capitan
     sha256 "8c4b8fb805fa94098925417a8fa61ced0d546b1b77330fd03cd5a72ee6e43b70" => :yosemite
     sha256 "8f0051f653f62bef0e85f8ced4806a8bae244a911e05129e4fc2bf05912c9412" => :mavericks
