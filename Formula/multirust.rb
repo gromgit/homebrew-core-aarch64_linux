7ea43165db626a15b513f589569e64868d1bba41
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 23:55:17 +0100

    multirust: update 0.8.0_1 bottle.

--- Formula/multirust.rb
+++ Formula/multirust.rb
@@ -11,6 +11,7 @@ class Multirust < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4e637caa68fca005e21846abb579a7487df02e3413f32efb01885a2c5e1746e6" => :sierra
     sha256 "18f1e661ce701e74f18d525a75e6c4ceb6e82f4096e77354452acec76593d43c" => :el_capitan
     sha256 "b7eb9f2d07493cda17eb49e9b79654c1f0ce4c60cd22b0e53e6b60469e3d32de" => :yosemite
     sha256 "26ad5ec92fbf20e556bffd4a73fdd7ce968db17c9078f626c497a91897fad4c7" => :mavericks
