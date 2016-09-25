ad2fbd15175a4e3e8e7b7d2d9a43d3df837cbb95
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 17:29:52 +0100

    dwatch: update 0.1.1 bottle.

--- Formula/dwatch.rb
+++ Formula/dwatch.rb
@@ -6,6 +6,7 @@ class Dwatch < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ba53e818fa75c684a88f3ce402266d44199ac4e3a67274b4bc788c1cdaca9c09" => :sierra
     sha256 "333be69806f30f6d169e79a7b59ff773d7c0b2e0d285e901a3fdb7c5c5e97ae7" => :el_capitan
     sha256 "fc0e151b4bf12ed2c1cfe714a09522965aa8f78e7a45d2f773e97b47d8c653f6" => :yosemite
     sha256 "f7fff9f814307bda8b0c6995c82a1114f0c62de23da90d754ef23edd5233d7f8" => :mavericks
