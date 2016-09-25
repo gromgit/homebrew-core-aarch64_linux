c8696d0e2ddf6ac2f87690887dd18535d4550cf7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 09:57:40 +0100

    juise: update 0.7.2 bottle.

--- Formula/juise.rb
+++ Formula/juise.rb
@@ -5,6 +5,7 @@ class Juise < Formula
   sha256 "869f18cb6095c2340872bc02235530616fcfc2e88c523c6a05238a521d0afe82"
 
   bottle do
+    sha256 "e6471f194bb00840083cbf692fb4e726edd88982b937e3a41dbbb3c95d5fe665" => :sierra
     sha256 "fc1e16d6035e93e4a13aa2eddb37d5588ee51095705a0f13470bd8a3c68e1ada" => :el_capitan
     sha256 "c0efb90b6a493eb7ca92844378f560da1d11043e2a9c20f58309892613e2baeb" => :yosemite
     sha256 "9fee9246ca23f5928cc5e8fd1d4feb77ce45ca31c8ce93385eb611976c63481b" => :mavericks
