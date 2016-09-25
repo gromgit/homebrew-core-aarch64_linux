84f8899c990251c2b5c1f3d04e852ddbe36995c2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 23:55:16 +0100

    ecm: update 1.0 bottle.

--- Formula/ecm.rb
+++ Formula/ecm.rb
@@ -7,6 +7,7 @@ class Ecm < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "888612dee7486ca5413e2b1e0090a4e1bd5ea7f2fe5cc53fe02bb326ed4f3d4c" => :sierra
     sha256 "3ecb325a368ef42737e77003e9ecc13a8d402a34da3a25c039b36565fef0b55d" => :el_capitan
     sha256 "9eef5eb54af2ad50ab05ee9382efe8d0ca831a6d058fe1fa2679cff87aa0a064" => :yosemite
     sha256 "c80a78299a5042d8588806066af2b03e9a3a679e8284fb863d9ee15edc690933" => :mavericks
