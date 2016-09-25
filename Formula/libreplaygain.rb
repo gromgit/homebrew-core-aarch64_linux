f60b73754ba8d435f72b0e582605e49170089b52
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 09:11:07 +0100

    libreplaygain: update r475 bottle.

--- Formula/libreplaygain.rb
+++ Formula/libreplaygain.rb
@@ -7,7 +7,8 @@ class Libreplaygain < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "d8f7cfc1bfad75b97271300a16f5c927849b03ff488141423ecf48b25c6ed8c3" => :sierra
     sha256 "58b52d360c2f37f3ab3a50c4a2fe72b9a370bd951d52939f8853a5ef49fcc322" => :el_capitan
     sha256 "d47338c5b86daabf3e2e05ab9dd2443c04c1233f3319307e8e5d545b24dcf722" => :yosemite
     sha256 "dc3f2c3823c5552bddad7b1727b9086dc2fe79e8fa13987b420d1621c97e2bce" => :mavericks
