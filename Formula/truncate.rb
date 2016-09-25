4d3193de7c19180ca35f93fb5f3ac557a36cfea9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 03:38:48 +0100

    truncate: update 0.9 bottle.

--- Formula/truncate.rb
+++ Formula/truncate.rb
@@ -7,6 +7,7 @@ class Truncate < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c4c892f0afbdf3a401ccb0af2a7cf8c65b37ccfdfe2412dda5284faa94f562ff" => :sierra
     sha256 "299b80454c20134c5d0916da25fb3d5f0b6843e620dac6babebe01a899253a69" => :el_capitan
     sha256 "a9d1c87d6cfec42674f0e7db25b786ba100a04c8c0da318fd5f6299a7418843f" => :yosemite
     sha256 "d8751674842b772bd3a5318c1234f262518d05d66a7fe3b06ce5f59b2176bba8" => :mavericks
