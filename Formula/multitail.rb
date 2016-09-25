ff1995b7af66891c68a53358ad29f1f92ccc61f2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 00:21:27 +0100

    multitail: update 6.4.2 bottle.

--- Formula/multitail.rb
+++ Formula/multitail.rb
@@ -6,6 +6,7 @@ class Multitail < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "967b77c466bb79a341ecbca261d4f263859d008e25746405f3444cb7de482ad4" => :sierra
     sha256 "01ac7f53386a8099b4dd9e80bcc14dcb8097676199819ed8e2dc2a0893aba930" => :el_capitan
     sha256 "60c748bbcac5188c00b1f0033bb46491623061cf08dfc5e6f5514d9b6042b5f4" => :yosemite
     sha256 "5d2219191236e2209bb4642ecb865716390e9984b27ce145f391fb2280e9f906" => :mavericks
