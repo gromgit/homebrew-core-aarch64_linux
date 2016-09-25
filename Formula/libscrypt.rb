63bb72e8a462b9bed7c060da9f7bc0296f0e876e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 13:12:37 +0100

    libscrypt: update 1.21 bottle.

--- Formula/libscrypt.rb
+++ Formula/libscrypt.rb
@@ -6,6 +6,7 @@ class Libscrypt < Formula
 
   bottle do
     cellar :any
+    sha256 "3adc43863f9b966dcecd89f507a4706891f94129dd88ba810ed0269278e931cf" => :sierra
     sha256 "bc2c8318384a72f82802937f7e6dd8017ec44fb6fc94583e5f0c38056e1a660c" => :el_capitan
     sha256 "0e870b01dbbfc49432cc8ea81c90ee6d8732b6d8adc4665368844536d5c6e092" => :yosemite
     sha256 "fe3bc1ca8b19e7c86e103f1345cb9294da01cc15b950302ad5486ef49b2b212d" => :mavericks
