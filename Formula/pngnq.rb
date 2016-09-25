c46d8145c4beb2fbfdea7aa6b4b1e278b6d49562
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 21:46:24 +0100

    pngnq: update 1.1_1 bottle.

--- Formula/pngnq.rb
+++ Formula/pngnq.rb
@@ -7,7 +7,8 @@ class Pngnq < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "0914104edfd7c6089ae4b053e5a57cf1b5a0d9bb476424ce654a923cafef651c" => :sierra
     sha256 "dd6970fb9055fb1a6702c820e75a3d7b826e165e61c23c17b0845cca780c3da9" => :el_capitan
     sha256 "cba40b130f3d16666580be2b572721d0d8d312f60f62e4fdef656ffa825bc65e" => :yosemite
     sha256 "4d9e35ec7c627bd2dc8c1ca26982e9c93e0a74687600830d5d491587ec04f967" => :mavericks
