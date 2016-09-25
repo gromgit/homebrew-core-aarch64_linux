87305ced8c64f8d16c691417a5416b77242a5814
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 14:27:04 +0100

    mplayershell: update 0.9.3 bottle.

--- Formula/mplayershell.rb
+++ Formula/mplayershell.rb
@@ -15,6 +15,7 @@ class Mplayershell < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e9377eaebb65903037105bf3ed6ee301a182452791e9daeaadd08ccb732d9d1b" => :sierra
     sha256 "ae4c1c9d069053afa7e71867256b577e23bd0dec87a90ccab2ebeab089a3634b" => :el_capitan
     sha256 "1637360e180d7b48367cb7c4f01d03856b9d13247000e4cc33f0af5f6ed92101" => :yosemite
     sha256 "a95437813704c56c3e52bd1b17974bec24c209e26df8e9dfe07af45d51ecaf49" => :mavericks
