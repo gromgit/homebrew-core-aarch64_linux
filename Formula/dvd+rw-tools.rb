892183627f1f433cf4e0701c6eeb517848ea89c7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 10:40:06 +0100

    dvd+rw-tools: update 7.1 bottle.

--- Formula/dvd+rw-tools.rb
+++ Formula/dvd+rw-tools.rb
@@ -6,6 +6,7 @@ class DvdxrwTools < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "932e3879247dd1587f35d99c7132c302ddeaf3b5efad9effb05f5b086a55541a" => :sierra
     sha256 "01bae78a5187a47ea770a9cb9c0cabdbafb60485e333a563240a6ea74d6718b0" => :el_capitan
     sha256 "13fa5b14889c82bd2ff44d4da2ba8049603bdfc6026196440fe33102939faa06" => :yosemite
     sha256 "834a3e5e1276e77a9dd5182d60b55484599bd5d705e6bb0d89a8db5720b7e197" => :mavericks
