400fea45deffa720ec471c20675981d34e3a2e86
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 17:29:53 +0100

    plzip: update 1.5 bottle.

--- Formula/plzip.rb
+++ Formula/plzip.rb
@@ -6,6 +6,7 @@ class Plzip < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8bb211a7a090509a64c331dcdca1bbcd201bc7e1b6a8bcf59cde64637be5d053" => :sierra
     sha256 "0da26f741281bcf54cd0ae9d443b867bdde9dde340bf83fcabc6c0f22c887016" => :el_capitan
     sha256 "fe05c1a91ccf2cd262041facaed4fade1ea45b957eabb127070333d79efed3eb" => :yosemite
     sha256 "3e7f98cc5a785d008c7fa9a9dac0b641240ca268089964730705601744e7ff38" => :mavericks
