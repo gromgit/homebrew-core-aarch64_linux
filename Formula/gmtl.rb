3ceb15dfa12fe662d9b72a6e791fb101bb92ae35
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 17:50:39 +0100

    gmtl: update 0.6.1 bottle.

--- Formula/gmtl.rb
+++ Formula/gmtl.rb
@@ -14,6 +14,7 @@ class Gmtl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ee8d0c9f5f52453421a189c040459b5126a5b739231493a3e39d331c934c6478" => :sierra
     sha256 "8aa9f0f1fb77376dd333bb03e9c5a07f6457b76008a74018a932dca930148606" => :el_capitan
     sha256 "5e6d70f957f11e58d8b3cd24d5474a8bedc73e0aec6df13f85322f4fda8a1164" => :mavericks
     sha256 "ffeb26dd58a9b05a4427ca02392f93f9d5b352af790e536e4d2989baa81e4faf" => :mountain_lion
