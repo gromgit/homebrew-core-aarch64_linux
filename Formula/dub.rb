fcb575ed6eb18b4ff4b589287f5e299214548c54
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 05:24:13 +0100

    dub: update 1.0.0 bottle.

--- Formula/dub.rb
+++ Formula/dub.rb
@@ -8,6 +8,7 @@ class Dub < Formula
   head "https://github.com/dlang/dub.git", :shallow => false
 
   bottle do
+    sha256 "68fcb18d5b548c8e676809ab318354ebea47950b5a4a5ff8ff20cb05d9ae2a0e" => :sierra
     sha256 "51a032793dc4671d6c12172ef29c38c98998dcd4b6aa501be2a5d35248b27244" => :el_capitan
     sha256 "d438a6fde4094513dd9f5ea30a7aa672e7dc6b764033fd4405adfb37861e49ad" => :yosemite
     sha256 "5bab5095000421e561fbf41268c615e079023b86a0d6d7ed00328ab52a7db906" => :mavericks
