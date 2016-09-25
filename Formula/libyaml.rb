983dc315eb905b5f23d87f282a3872a4b631252a
Chulki Lee <chulki.lee@gmail.com>
2016-09-24 21:35:12 -0700

    libyaml 0.1.7
    Closes #5192.
    
    Signed-off-by: Zhiming Wang <zmwangx@gmail.com>

--- Formula/libyaml.rb
+++ Formula/libyaml.rb
@@ -1,10 +1,9 @@
 class Libyaml < Formula
   desc "YAML Parser"
   homepage "http://pyyaml.org/wiki/LibYAML"
-  url "http://pyyaml.org/download/libyaml/yaml-0.1.6.tar.gz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/liby/libyaml/libyaml_0.1.6.orig.tar.gz"
-  sha256 "7da6971b4bd08a986dd2a61353bc422362bd0edcc67d7ebaac68c95f74182749"
-  revision 1
+  url "http://pyyaml.org/download/libyaml/yaml-0.1.7.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/liby/libyaml/libyaml_0.1.7.orig.tar.gz"
+  sha256 "8088e457264a98ba451a90b8661fcb4f9d6f478f7265d48322a196cec2480729"
 
   bottle do
     cellar :any
@@ -16,13 +15,6 @@ class Libyaml < Formula
   end
 
   option :universal
-
-  # address CVE-2014-9130
-  # https://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2014-9130
-  patch do
-    url "https://bitbucket.org/xi/libyaml/commits/2b9156756423e967cfd09a61d125d883fca6f4f2/raw/"
-    sha256 "30546a280c4f9764a93ff5f4f88671a02222e9886e7f63ee19aebf1b2086a7fe"
-  end
 
   def install
     ENV.universal_binary if build.universal?
