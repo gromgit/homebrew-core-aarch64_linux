bf895e55e7550874d56e2d0ebd2b2ab02d0d8db7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 23:55:17 +0100

    since: update 1.1 bottle.

--- Formula/since.rb
+++ Formula/since.rb
@@ -6,6 +6,7 @@ class Since < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ff4ba4b7cad5fa4211bff04d5868521bc21b60995cf40f15bd507abb7c4cbaab" => :sierra
     sha256 "ec4898462899cb632329f71dc0b4dd9a13a051aafd6da7dfd22e940e9d1ce01a" => :el_capitan
     sha256 "e92218f17ac1926f4651b3e70d3fe42d43b7024e1f10d0ab6f1c7c9dd6bad606" => :yosemite
     sha256 "bfd7889688facdf732cf0bf2bb8c7a917df71e80615a5f367468708437c0519e" => :mavericks
