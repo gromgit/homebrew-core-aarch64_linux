53ce73e5e8543890cd9c8e48b508420409be2ebf
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 15:17:32 +0100

    mpop: update 1.2.5 bottle.

--- Formula/mpop.rb
+++ Formula/mpop.rb
@@ -6,6 +6,7 @@ class Mpop < Formula
 
   bottle do
     cellar :any
+    sha256 "2d03238722efe5532d69ef80bf86e700664f1d6cadbc3ff4c2a1b4ac203fef3c" => :sierra
     sha256 "6aaf347d5917c4366b7572ff6ac48e5cf7be653a1dc34347503f64c6d5b38d4d" => :el_capitan
     sha256 "55de4c36ac34e22563d5adf327a50e9859e7ba6a848254d3f9f226791086bf72" => :yosemite
     sha256 "6152acd7293ead40fce85a67e9e451d15ddfa71775e688e08aef11d9565005ae" => :mavericks
