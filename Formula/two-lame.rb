54d2b9f494e9debca7124c9b8d75e667f2159421
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 11:31:11 +0100

    two-lame: update 0.3.13 bottle.

--- Formula/two-lame.rb
+++ Formula/two-lame.rb
@@ -8,6 +8,7 @@ class TwoLame < Formula
 
   bottle do
     cellar :any
+    sha256 "aaa23ea68a8e9ce852c99bc4edbaa4eb008f596d64c1b694a4dd8ed53888d4ad" => :sierra
     sha256 "f42b6a20cbe1d020ed818145180be176361ceda2c203a09bddc0bc1f4c903530" => :el_capitan
     sha256 "7d5a39e1189f77321a2603b77984c57970f897a69aee9bccc008e84a807dbf59" => :yosemite
     sha256 "b03ee3602cad74094fc1141c3fd9aac92f2e298bf0f98a3281a6ba3f547511cd" => :mavericks
