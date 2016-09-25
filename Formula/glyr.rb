5ed408e54400e0fd6d564841270895710d79c65c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 15:17:31 +0100

    glyr: update 1.0.9 bottle.

--- Formula/glyr.rb
+++ Formula/glyr.rb
@@ -6,6 +6,7 @@ class Glyr < Formula
 
   bottle do
     cellar :any
+    sha256 "1ad972684bd9c9a30cec00852d1091ad0c7c07d6affc5e7b38f762cdd4b91c7c" => :sierra
     sha256 "d734d6df100a1fce794cf3a4369f209d8ef3035bde0396a857f116b58945b229" => :el_capitan
     sha256 "f7f98aaa44e4132985579133546abb0f7ef4a7fc2e12f1f438950835b10efd86" => :yosemite
     sha256 "57679677a24b330752690f30172d8fc187363cb7109b2368ea26996840a3bf21" => :mavericks
