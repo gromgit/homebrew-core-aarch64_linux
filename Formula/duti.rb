55f917a86f7d53b5a22242f5359ea3346d6c73a8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 09:57:40 +0100

    duti: update 1.5.3 bottle.

--- Formula/duti.rb
+++ Formula/duti.rb
@@ -7,7 +7,8 @@ class Duti < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 2
+    rebuild 2
+    sha256 "8f34c5664b0a9c05274fd67102fca6c969f7dc966279b2ca0f11906df3d2d03a" => :sierra
     sha256 "53748f3ad97a48b468326e66d869e20c05fc1f67219ac3ea8a147b558717ee45" => :el_capitan
     sha256 "c2661fc4e59d5cc941a416bf7abad035af3d15f75e7bca73d2bc29706d89f560" => :yosemite
     sha256 "e301c36a6809acc2a0dd62fba59eac4820b90cf2cf9df30f46480f7ce736dad3" => :mavericks
