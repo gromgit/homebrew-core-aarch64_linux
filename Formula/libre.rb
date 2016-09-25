3a662914b789e9538790af79277e9d1755b8d76a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 07:58:12 +0100

    libre: update 0.4.16 bottle.

--- Formula/libre.rb
+++ Formula/libre.rb
@@ -6,6 +6,7 @@ class Libre < Formula
 
   bottle do
     cellar :any
+    sha256 "b83e08f9bda6bdaf135cd3ea44f9f0e8ee49efc25d7a1a7d33a05a75a15e0b1b" => :sierra
     sha256 "b27f86fb350b5ad67b06a9725227a16f8a74f84353ba056fb7e747888107dee3" => :el_capitan
     sha256 "1d2b0f0ccdb818095d4ccd6b0ad8a9896e7e7d9102add4cba35121020f79ed05" => :yosemite
     sha256 "bd919c8fdfd4719987f352cd76b8ca5e624255f0e89d8e7095609e122eee122b" => :mavericks
