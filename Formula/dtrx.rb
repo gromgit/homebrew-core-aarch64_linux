50b57f0cf186f55ed8d486c1c518426b71f5597b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 04:46:21 +0100

    dtrx: update 7.1 bottle.

--- Formula/dtrx.rb
+++ Formula/dtrx.rb
@@ -6,6 +6,7 @@ class Dtrx < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "812e92d3899f795921dced2d1085dec1258a659595e1a0545e67a03be653b9ca" => :sierra
     sha256 "5419cb8b5327a5770d1d8a541a11c47f9195885964af3977110a4bd752a63ff7" => :el_capitan
     sha256 "15359cfa40144872520eba6cdb132a1bfc0534b66cfd87c6efab3d57221ba66f" => :yosemite
     sha256 "c023c31dfc27c2a198ff310bfa0438fac034209f39e42800ee5d462bb2be3e6b" => :mavericks
