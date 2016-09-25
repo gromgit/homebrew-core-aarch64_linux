98af947b2a179498ed3035ebc0a3ca49c869f1db
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 22:16:49 +0100

    libstxxl: update 1.4.1 bottle.

--- Formula/libstxxl.rb
+++ Formula/libstxxl.rb
@@ -6,6 +6,7 @@ class Libstxxl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0f9cb534bcdc8d344dd5da5b8381cca527e515d1a3df2bf5b3785783f7f482d7" => :sierra
     sha256 "8753b0c9ec11f1f6195fa52c346b4d6015652931908156dcdb4468cc94103d1c" => :el_capitan
     sha256 "fbefd1ceb6f328ef77b9a005f5bc3c93fe23fa68d60b4a5e126a68a6a9d8e17d" => :yosemite
     sha256 "5a64626281265368e41f51089271c031ae8782651d65fe012f8a86949e37573b" => :mavericks
