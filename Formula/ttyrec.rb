28cb807ae9ec350076e77071101619220cf30c90
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 08:43:32 +0100

    ttyrec: update 1.0.8 bottle.

--- Formula/ttyrec.rb
+++ Formula/ttyrec.rb
@@ -6,7 +6,8 @@ class Ttyrec < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "941620132dea9468c3d548d8f9c63ffcade5dd45eff322cab699ae4db1e61079" => :sierra
     sha256 "f24cda5af4db432d89cf3d3f5fb68a6f4afd1950c2c7e32456c8a8a779dfefd6" => :el_capitan
     sha256 "86ff5251f02aa7becad5f95ff8a6bdb62572d2cac4f99a72166bda8cfffc7981" => :yosemite
     sha256 "0324a2412722841b8f58bf41ab18d50e3dd15bed3d956f7d3738a6b1911f1130" => :mavericks
