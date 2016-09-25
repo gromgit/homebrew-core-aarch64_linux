baf9a7f6616a486ba2eb5496c7164ca4afef4168
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 02:08:10 +0100

    picoc: update 2.1 bottle.

--- Formula/picoc.rb
+++ Formula/picoc.rb
@@ -6,6 +6,7 @@ class Picoc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8677b5fe46a67991ed8bbec9d9727472a866d77e23f159124bb833dfc98f4f28" => :sierra
     sha256 "8c9bff2043ec4140a347e048741caed8c9f286f3958af1bd0e4fdb5c8817ae43" => :el_capitan
     sha256 "05b84e43b6dc919361a4dc3763350bd471ff793c2aaddcd8696fe708be1dad10" => :yosemite
     sha256 "19a25b578aaf48405e46341158dc62379a66fca36f7363d01bfcab4c0cea5209" => :mavericks
