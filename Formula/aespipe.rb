932521daacc35693a0d462e978d3310b3d3aa1b9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 20:06:32 +0100

    aespipe: update 2.4d bottle.

--- Formula/aespipe.rb
+++ Formula/aespipe.rb
@@ -6,6 +6,7 @@ class Aespipe < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "cc85335c772454b99eaa884e335876caa019fab2f31835df181137e554b491fc" => :sierra
     sha256 "64ee9d5b5abca294191d2b9cfb923c5213cd4c1967fc53383edb651c217a461c" => :el_capitan
     sha256 "9b6d98e9fad116799c96d5f423d7e12504d75b7e8c2d23ade617267cde47c89b" => :yosemite
     sha256 "68cab23444ced7d4087ed8a0892127956328cebaebc66009344e4bddf0ae3857" => :mavericks
