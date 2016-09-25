afa6528054af051d9291f5042f92ac50566f1c4c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 00:53:42 +0100

    cimg: update 1.6.9 bottle.

--- Formula/cimg.rb
+++ Formula/cimg.rb
@@ -6,6 +6,7 @@ class Cimg < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1fd54143bdc49bb378ec3576a65ecb33c278b8031653bdc67de5aa7c0cd5d145" => :sierra
     sha256 "4279078a6e13098070cc62632596a0f2187115eab197fc093eca49f8c57d175a" => :el_capitan
     sha256 "df6f99dab3ac2e1046e891d2594d9e9845d4b2164421bd4fccebf50d4a74e0cf" => :yosemite
     sha256 "066d8e2062bf9b0cfb7b0ff4a4edf24c55e5fdaecb8cf262e549081c281f2fe5" => :mavericks
