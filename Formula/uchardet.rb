2f44cbe97b4def401b4a74b8a05114cdbba9239f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 17:50:40 +0100

    uchardet: update 0.0.6 bottle.

--- Formula/uchardet.rb
+++ Formula/uchardet.rb
@@ -6,6 +6,7 @@ class Uchardet < Formula
 
   bottle do
     cellar :any
+    sha256 "dce2d199e163858a10f27f9d94d232b8df5d38507098b629356ee5154d4f182c" => :sierra
     sha256 "998232b6d034c090680202ca6d48a9af4924f091f3b597e4aa318f87fdf29bb8" => :el_capitan
     sha256 "ab930a4e2c217362dc7e05940cc6449d024f18c5014847ff9428facef02316c7" => :yosemite
     sha256 "c02f20920ac97596ab425b057275372a77c80c7d523191f2e5ab78c636d6827f" => :mavericks
