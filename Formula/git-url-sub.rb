580f212b3290d9fbfc642dd94395e43ebb28f350
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 04:46:21 +0100

    git-url-sub: update 1.0.1 bottle.

--- Formula/git-url-sub.rb
+++ Formula/git-url-sub.rb
@@ -8,6 +8,7 @@ class GitUrlSub < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2fcf47332e070caed126fef2be0a1108a23e18a9d1ba80b6059b45a417af1b31" => :sierra
     sha256 "cf954ff293abbcaf8816c8142b5762ebe7601107f76530f6bab0edea71e2d609" => :el_capitan
     sha256 "2edfbc5f15001b1c4c08b26251a845533473a79bc2f387d3fd1d74751080cd1b" => :yosemite
     sha256 "ce9c28238d1904b9d2c97da10fd7a6be0b1ceafde423311078dfac0bbe8a82dc" => :mavericks
