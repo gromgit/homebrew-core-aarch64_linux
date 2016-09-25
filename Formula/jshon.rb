7322ae52181719261aa038fd02447d4bf3f979aa
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 04:46:21 +0100

    jshon: update 8 bottle.

--- Formula/jshon.rb
+++ Formula/jshon.rb
@@ -7,6 +7,7 @@ class Jshon < Formula
 
   bottle do
     cellar :any
+    sha256 "eaf121512303610e873d508312acc46547c3399f4171e08a7a391ed5f0604786" => :sierra
     sha256 "96c3b3298a7975eacf2135f3b75a21a4a2d4df94f3b0b54a83a58da6e83148db" => :el_capitan
     sha256 "6b58d61da14d8f458290888b7576a44ec12daa26077f94d8bbeb406fb165d117" => :yosemite
     sha256 "66e48123e2b04de8f829cf861590d336b64215623aafc846ee9e6d6eda4aed57" => :mavericks
