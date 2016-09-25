b81dab8095d8139ad9beba4739b2dc5564ba540c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 11:31:11 +0100

    juju-quickstart: update 2.2.4 bottle.

--- Formula/juju-quickstart.rb
+++ Formula/juju-quickstart.rb
@@ -6,6 +6,7 @@ class JujuQuickstart < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "32b70041e0433294ac291b531dcb7340efc20b2d1609a96496d654a03c24322c" => :sierra
     sha256 "a6dd9cb553b399995f36a076ffdb949f429a46a312bd9d8580ba28d3fe172b2a" => :el_capitan
     sha256 "55994b277c93c19b9534f56ac23c183ec0f27e7441609107e510f892e2e98c86" => :yosemite
     sha256 "b4c3a7b5d42d808bd96ec8b1a398e4d2950e1f5e3c0d1e00cfd259069e1191f5" => :mavericks
