109939afe4af2dd8742cbc31af1bc2b0e04a6556
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 21:46:24 +0100

    udpxy: update 1.0.23-9 bottle.

--- Formula/udpxy.rb
+++ Formula/udpxy.rb
@@ -7,7 +7,8 @@ class Udpxy < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "856dd5abfc350e8ce06e361664c21732e0b11dfd84aca7f9bd1e0e40704ccb67" => :sierra
     sha256 "6f2fb0a9baf932d599fca41b8ec80cd35491332ab89464bdda6d7ac8e5b5e01d" => :el_capitan
     sha256 "7624631dffaa797191689b05fcb5d7c87c0ad233e49c308b10462c08c8a955e4" => :yosemite
     sha256 "45dcc2c1a7d1f0170ae44edf600fee1f6112fd1e11530548a7e3b1870d71a7d8" => :mavericks
