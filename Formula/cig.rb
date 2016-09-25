4dd9ce75709ab263369dc496fc00a3cafa55b61b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 00:21:27 +0100

    cig: update 0.1.5 bottle.

--- Formula/cig.rb
+++ Formula/cig.rb
@@ -9,7 +9,8 @@ class Cig < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "8d6219bd7d8f795608a58bf95fed5489cf8aa8266b3d06e6ff506037fa449ead" => :sierra
     sha256 "c64c5aaab66ee0853e0b3437c002dcf233fd543a019e4b411f9cf3f9555de702" => :el_capitan
     sha256 "d349ccf020a30a7db72333a4aa1a8f73bcb2b3c6f0984c7a0e88de38bc07ed4c" => :yosemite
     sha256 "b61176af3e53d2505f36ee3deafbb92a5f02f6c0841fefdcdfdd084821a95837" => :mavericks
