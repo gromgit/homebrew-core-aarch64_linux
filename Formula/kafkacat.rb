aaca59c43cd1b590326cba67d1430aa7003ec07b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 16:16:44 +0100

    kafkacat: update 1.3.0 bottle.

--- Formula/kafkacat.rb
+++ Formula/kafkacat.rb
@@ -6,6 +6,7 @@ class Kafkacat < Formula
 
   bottle do
     cellar :any
+    sha256 "25a5f4adac93dba85de1681b79ff87d923eb5a1a95582853e92cd70a22c83c4d" => :sierra
     sha256 "632efceabc31814305eb5331bc25a361414afd9ef1562cb9bb6463526997bb5e" => :el_capitan
     sha256 "32bcf9d514f4e566029b3427098c6d73f78dad911fd6b762e38395c1ef41edb8" => :yosemite
     sha256 "c1dcf8abd6c4917950403a872bca4d0502a116dfdfb0eb29c950f03a120ada6f" => :mavericks
