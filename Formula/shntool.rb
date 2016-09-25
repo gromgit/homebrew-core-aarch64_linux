766e9efc86aa5cab5e42730d56fab382f69afadc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 13:12:37 +0100

    shntool: update 3.0.10 bottle.

--- Formula/shntool.rb
+++ Formula/shntool.rb
@@ -6,6 +6,7 @@ class Shntool < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5548c0401df42faa7ade30e9c98656828864c3677836f72100aa80f4a92ff249" => :sierra
     sha256 "910da256436f55ebf40c487ebd0e74da2979f01bf0adac83272b353c66ba99a8" => :el_capitan
     sha256 "46d99776bb02d9721f30a6cb9b2c44293d3fbc6b4e3f522df869a7106b09a448" => :yosemite
     sha256 "fa745e31b3c2aadb20d5c87f7175f00b3c5a39b6664ea0a9aa9d88d5767781c5" => :mavericks
