e228b96cc5c9ad9df0993af62560760c3ed07718
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 11:31:11 +0100

    librevenge: update 0.0.4 bottle.

--- Formula/librevenge.rb
+++ Formula/librevenge.rb
@@ -7,6 +7,7 @@ class Librevenge < Formula
 
   bottle do
     cellar :any
+    sha256 "2f8a2a371c35b578d181d1ce8d45084a2f699bbed95cabd10f5cd75977249542" => :sierra
     sha256 "827a37488cc92f16ba8f4d7343e7944c7faed4b8cf9d930f49d93e4104784c94" => :el_capitan
     sha256 "a95c4fc2b7832e226d21a209811a2f149b8fde4962d07d354e3a6cb80b7f0a01" => :yosemite
     sha256 "45c4df842b9cf38554efeb4d04f2c2abf2ed8341e0fb4bc0d80830e02e1fbfeb" => :mavericks
