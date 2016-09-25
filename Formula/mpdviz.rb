0286f72f8b30881750cfeb14c7efc4bc2b22d4d3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 11:31:11 +0100

    mpdviz: update 0.4.6 bottle.

--- Formula/mpdviz.rb
+++ Formula/mpdviz.rb
@@ -7,6 +7,7 @@ class Mpdviz < Formula
   sha256 "c34243ec3f3d91adbc36d608d5ba7082ff78870f2fd76a6650d5fb3218cc2ba3"
 
   bottle do
+    sha256 "f76523028928595c731469cfaecbf2ab9201a90290d7c21e176b2cfc4560956d" => :sierra
     sha256 "9ba7f88de1dec1c4c6d4bdbb2f45541d43ae56c5039b2193c92186202a74322c" => :el_capitan
     sha256 "a3ccae36bb636ebe51ef1c3f70bf582d1ca55053639c58aaefb7e48672191dcf" => :yosemite
     sha256 "063b1d5a369b91869c21ae8aa5dbfa96cd1e9b3c4d5091fc573c965e1b253493" => :mavericks
