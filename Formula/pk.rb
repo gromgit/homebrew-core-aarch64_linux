1b6eaa4d45658c8aaa2045c763d5c333ee2a2f89
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 08:27:39 +0100

    pk: update 1.0.2 bottle.

--- Formula/pk.rb
+++ Formula/pk.rb
@@ -6,6 +6,7 @@ class Pk < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "790f7e9670dcda15b7472264eea54666e7e34e8adb4343b3699ab87a60c9f3b1" => :sierra
     sha256 "74c7822b2e3a74bc657d5e8490f184af120eddf9230695fe26dbb075391e10e6" => :el_capitan
     sha256 "2e86bd1b33521a5856308b58ab35f7384988e9cb6506a4f3d9191ea38361235d" => :yosemite
     sha256 "29897298823bb20019ab5f608e1e6f2c8189076a5c0097d5f09eb504e6e814bf" => :mavericks
