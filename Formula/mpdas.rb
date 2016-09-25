b47a9921903091a50d13d2c2b1f52789d1363e82
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 09:57:40 +0100

    mpdas: update 0.3.1 bottle.

--- Formula/mpdas.rb
+++ Formula/mpdas.rb
@@ -8,6 +8,7 @@ class Mpdas < Formula
 
   bottle do
     cellar :any
+    sha256 "90e1ba54b35ed714b5558336793455c2db11e3dab95951cae5b846b0763b1e04" => :sierra
     sha256 "e82e85475795c700ad560280d551c94eac4a7e376eac2848d214e46747023644" => :el_capitan
     sha256 "ce6d4b85c76698e1dd16325d3e4fc0b560a53e5e7a9608aa9060833f7059ff5d" => :yosemite
     sha256 "7844d826d96940c932d89af353e456b17df5cd6ee67627aa51f264d1b713456e" => :mavericks
