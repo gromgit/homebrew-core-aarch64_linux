0a7c4cf07d456ab82561be6888d3d23d9abf83ff
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 14:03:28 +0100

    glslang: update 3.0 bottle.

--- Formula/glslang.rb
+++ Formula/glslang.rb
@@ -6,6 +6,7 @@ class Glslang < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d5de78fc374f3c92cdf8f06f1518405346c63b42ffd7ad53bb153c27bd00a935" => :sierra
     sha256 "770943fa3e43b765e303cc88da1aa0bf2455f91cc0e84a636bfadd517cc87776" => :el_capitan
     sha256 "111206ad8b23ca9f78fa5657d371056e238f3eabf747d48001115d85f4ea88bf" => :yosemite
     sha256 "4d22c058983e127f3dbb02d86ef6d6cb94fcc5b87c5f3e46802b8b157d56e1c9" => :mavericks
