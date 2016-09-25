eecc46688ebffc734026a20dc779f6eaaa844b22
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 00:21:28 +0100

    sipcalc: update 1.1.6 bottle.

--- Formula/sipcalc.rb
+++ Formula/sipcalc.rb
@@ -6,6 +6,7 @@ class Sipcalc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1ccdaec0a816dde9f7caa0f7a77cd984ece78a61a5886032c4c8821915753482" => :sierra
     sha256 "56aa686252ac703ed3dbe91f5737ec4d4b95d52516f4ab52947df15b77d1c58f" => :el_capitan
     sha256 "6b2fc300755693d382fd5ea971c272a7c8c7bff49614dd88d8db4270aa496012" => :yosemite
     sha256 "7ddf7b200984de97143828faf6385314a2ff3f4436432d810e5aaf7dfe44e78c" => :mavericks
