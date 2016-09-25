7cd9d4edd431c5a2430a88ee2fceda11152711bb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 04:26:37 +0100

    git-tracker: update 1.6.3 bottle.

--- Formula/git-tracker.rb
+++ Formula/git-tracker.rb
@@ -8,6 +8,7 @@ class GitTracker < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "46aa6bb9caddfe0bb589e73132b67e5f9cf5558203ca276f24d994fdafa1327d" => :sierra
     sha256 "9d04e96e283ae94ea0a789911aeddddd55d53af805313ad6bf8b1d84334daca0" => :el_capitan
     sha256 "26afe6cdf02f1bda66d2d56518786970c8c1de878e00c09cb045cfbd3379487b" => :yosemite
     sha256 "132caf19abd7bef677b3d74f38e7fc5d139e90f03b4cb96c3ce8e34ea8cf96ad" => :mavericks
