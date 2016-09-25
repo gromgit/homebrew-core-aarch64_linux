cb886e6d5b8255ad8139fce2a25b1afb9ea15589
Zhiming Wang <zmwangx@gmail.com>
2016-09-24 23:57:10 -0400

    aardvark_shell_utils: fix test (#5191)
    Need to compare realpath(1) output to the resolved path, or you may run
    into a /tmp vs /private/tmp discrepancy.

--- Formula/aardvark_shell_utils.rb
+++ Formula/aardvark_shell_utils.rb
@@ -26,6 +26,6 @@ class AardvarkShellUtils < Formula
   test do
     assert_equal "movcpm", shell_output("#{bin}/filebase movcpm.com").strip
     assert_equal "com", shell_output("#{bin}/fileext movcpm.com").strip
-    assert_equal testpath.to_s, shell_output("#{bin}/realpath .").strip
+    assert_equal testpath.realpath.to_s, shell_output("#{bin}/realpath .").strip
   end
 end
