4131880f0fae00f01291cd9b9bbadda1f72c1334
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 15:17:31 +0100

    jython: update 2.7.0 bottle.

--- Formula/jython.rb
+++ Formula/jython.rb
@@ -6,6 +6,7 @@ class Jython < Formula
 
   # This isn't accidental; there is actually a compile process here.
   bottle do
+    sha256 "355056444220bf1bb7bd5aeeb761c81a6632426ec2bd193270600615e5eaec44" => :sierra
     sha256 "12a261c1f35421587b21cb1e9de24dbe109716746c5393d8d2046e15aab0b821" => :el_capitan
     sha256 "2cfee4b728f3eb12528b66f53efee4fd2963c8da4103a47a1789afe75f1d5192" => :yosemite
     sha256 "a20a699bcbec43953cec0f8de72dc98ceef2264fc62744cc62d0a96c5e2b175e" => :mavericks
