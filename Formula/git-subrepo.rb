42e29739cd227d1642c9a97384534b006a59696e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 03:59:00 +0100

    git-subrepo: update 0.3.0 bottle.

--- Formula/git-subrepo.rb
+++ Formula/git-subrepo.rb
@@ -7,6 +7,7 @@ class GitSubrepo < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1c21b30078673bd581298b904ea627c84eb39c611c5558d9e920184d63698f69" => :sierra
     sha256 "e42d73137d06bc594ac917e7e8fc35810083461c26d541eed49e0a459dc31e21" => :el_capitan
     sha256 "f1415cb1ec64de2a854511c5a5aed5b4574222e4c2b795f3c1603011c661a24b" => :yosemite
     sha256 "a818994ecab1ca578f715dff0d7bb11018e1e724d98d4bb21234e2575210fca8" => :mavericks
