11c48c644899b07faf1d11c8bdc553dd684620b0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 03:55:50 +0100

    ahcpd: update 0.53 bottle.

--- Formula/ahcpd.rb
+++ Formula/ahcpd.rb
@@ -6,6 +6,7 @@ class Ahcpd < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d3a8a4efb712e2c6a8a055276e5d93d3275a638df4231a4dfe8d428a2606d776" => :sierra
     sha256 "b37143ee365a4a3afd9623d5f49eab0bc4bdf9ac3662d22db9671cffa1078224" => :el_capitan
     sha256 "36907bc1aadc9d9d874ebd74624d8c2c2e8b4057181df1e964720a41f72ccae8" => :yosemite
     sha256 "8518f82187d2b8d2bc24648bd072f19073e159abb2bdaf5418ad31e3ab966d0b" => :mavericks
