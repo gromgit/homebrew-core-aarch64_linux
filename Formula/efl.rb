38277509d5ff1fe6b092c0a09ad99a582573468b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 03:37:32 +0100

    efl: update 1.14.2_2 bottle.

--- Formula/efl.rb
+++ Formula/efl.rb
@@ -6,6 +6,7 @@ class Efl < Formula
   revision 2
 
   bottle do
+    sha256 "edf518f51d6ee54d5d4c8dbaa8ea4a3f1c775c2df6abbdc1636a41f8360af8ea" => :sierra
     sha256 "a8e51818234a137bc021469bf37a52c84608d4e399420b71ff360682c8a7a8ae" => :el_capitan
     sha256 "a8fe89f450fc0328f84a442b138a93ebbdd516d03ccfe333e3c21fa080befd7f" => :yosemite
     sha256 "4d1e000d79e426ffaf82e1b4603239b2c629c8eab5549b29d403a2b88eca24f5" => :mavericks
