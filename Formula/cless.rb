fbf239b5bcbd6097281b80cccb202d9118dca9ec
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 09:04:17 +0100

    cless: update 0.3.0.0_1 bottle.

--- Formula/cless.rb
+++ Formula/cless.rb
@@ -10,7 +10,8 @@ class Cless < Formula
   revision 1
 
   bottle do
-    revision 1
+    rebuild 1
+    sha256 "13a58ff321351e824e1972af009610772fba593cebcb49449eef10ad43413ed5" => :sierra
     sha256 "4d6c49c06cf947e75aaaef79f45b13d25c343641312639ad16b9745615169dff" => :el_capitan
     sha256 "ef83bdacb690d95de845c03b12b8c18819ac7a1733cc97e46755be79ac17fdf3" => :yosemite
     sha256 "7e5f360fccaee62ea35aaba208d88a20e64fabf4a512da71323191cd582fb35d" => :mavericks
