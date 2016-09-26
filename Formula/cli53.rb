111b9af713a7d34f4bfa3a78cffa213c8c30ed14
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 10:13:11 +0100

    cli53: update 0.8.5 bottle.

--- Formula/cli53.rb
+++ Formula/cli53.rb
@@ -6,6 +6,7 @@ class Cli53 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "13ff90b5de6883f7713b5285ee43fc9c24bd3a101dccbd6b16f8b7a676b5a3dd" => :sierra
     sha256 "971739862970057906cf8f9dcec332d7ef2ac8afa5cea508a3f6d8c339f9fe7f" => :el_capitan
     sha256 "f1d457a3fbd3a9b9a8935726165ea04b5ce5c72f28fb0a12b34343396ef00e14" => :yosemite
     sha256 "763bb87f2a16121b0b1bb0b6fd9068f0efc63172807a6cd774ecef9f7a084088" => :mavericks
