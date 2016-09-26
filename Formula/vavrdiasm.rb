729b0ef53c9fcc88ec18daeca97fe9a60cbf1e7b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 21:50:17 +0100

    vavrdiasm: update 3.1 bottle.

--- Formula/vavrdiasm.rb
+++ Formula/vavrdiasm.rb
@@ -6,6 +6,7 @@ class Vavrdiasm < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c04a9755b9f2e15fa512fdb08d28b95b8cf0304287f3a7930975b4ad75417fcf" => :sierra
     sha256 "0671b1062a86e8d596a9f404fd843cb37d6d2d1bb28ebb2b8a8f6cbdd763c97c" => :el_capitan
     sha256 "ce57062586ca9cb91290141376f1da1f5de3c6efb6fe4687585a3e64cc29c014" => :yosemite
     sha256 "f881c5a6d94581c4fc9efb13118c84c40700f13d130302f6ee4cb16968d1f6b0" => :mavericks
