eb38471f1858ccab0f1230f20a36c875c5a7d053
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 18:45:47 +0100

    epic5: update 2.0.1 bottle.

--- Formula/epic5.rb
+++ Formula/epic5.rb
@@ -6,6 +6,7 @@ class Epic5 < Formula
   head "http://git.epicsol.org/epic5.git"
 
   bottle do
+    sha256 "9aeed705e50bf7f3459d0a8d0dd086c8c2c86d8b6028c22a002d60e3a5183183" => :sierra
     sha256 "0040885911e6bbab3b2b7b1c29e99e31363e6f24ff83d982afb54a1d11623715" => :el_capitan
     sha256 "8617a71ad3aead7226e7a71f90f5a834341031b6195c862d64757a662cb7e8a1" => :yosemite
     sha256 "0f17c628454c763deb0d5e20d488b0ef427ec6d5041a7f7223a0eaa9fa601e7c" => :mavericks
