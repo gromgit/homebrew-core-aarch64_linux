9153749a578e7549030d48664c0ec1da18c5097b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 07:04:10 +0100

    clens: update 0.7.0 bottle.

--- Formula/clens.rb
+++ Formula/clens.rb
@@ -6,6 +6,7 @@ class Clens < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f034c79bf5a16265db249c673b2d2a3e6850676dba739adeb6e90394d8f77475" => :sierra
     sha256 "f6c68d7dce9d824274e16e3867926528cc79d161418fac0a0052e37dc6604668" => :el_capitan
     sha256 "b08580c90a8ed647005c437158972a1dbd770792c2db74391eec8ed5f4ec1b6e" => :yosemite
     sha256 "e3642b0982f846103eace85598f242e982ead98bbc8963b58e4192b2d1b1ae84" => :mavericks
