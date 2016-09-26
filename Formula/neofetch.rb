e2009b9b3e34d25eb3405619ce41a810e9bf3b0f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 23:59:45 +0100

    neofetch: update 1.7 bottle.

--- Formula/neofetch.rb
+++ Formula/neofetch.rb
@@ -7,6 +7,7 @@ class Neofetch < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "be4cc07f6463ae123b3702fb67e5f2c2c9ffa1f2ddc1a73383c5c7d0696de4bb" => :sierra
     sha256 "2ef8e1115c21b1932362e829c50e06d7a493bee18f254042d224560f4e7d5052" => :el_capitan
     sha256 "73d764934acf748f11b6540425ad08c1454dc98bd4571bb6d503d3ff793824eb" => :yosemite
     sha256 "15f36b6e099dba428645a9c125f9e5f70537949a66a10c280f48a693df0516c8" => :mavericks
