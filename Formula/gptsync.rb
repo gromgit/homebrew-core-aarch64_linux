1e73db73350752d832cc54e4d318ef09b4d8c2ed
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 20:36:51 +0100

    gptsync: update 0.14 bottle.

--- Formula/gptsync.rb
+++ Formula/gptsync.rb
@@ -6,6 +6,7 @@ class Gptsync < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e822ef6c99aeaf6eee5812cd83ede2bc9a045dd556105150293bcf486898a59d" => :sierra
     sha256 "d355de7bea36e310d22ed1109a34574ab93859bfe9e44b9493ebe75cfe429c33" => :el_capitan
     sha256 "34756250a7bbd8470dd98401c86c65d9886cfac802adb2371bf0a23fa9351f7f" => :yosemite
     sha256 "77233898efcd0dee5ec73bf8a11294bd5f6c64f5f7d34136c792d1e96ef13d61" => :mavericks
