d1351d7662c2bd148f2011c87bef896154a15bf0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 03:37:31 +0100

    cityhash: update 1.1.1 bottle.

--- Formula/cityhash.rb
+++ Formula/cityhash.rb
@@ -6,6 +6,7 @@ class Cityhash < Formula
 
   bottle do
     cellar :any
+    sha256 "62d8d1409dfe744d4de7a1727824b06c5a80b248433c2d8bd8a4efcd444346cb" => :sierra
     sha256 "b09962ca43b3bb3321e1e57bf74a0936142ec5c94e198113ac3aa14e669e4d28" => :el_capitan
     sha256 "2b155183e2422811593d91b415ac2e90a00b7d6972f284e54b3214940250935e" => :yosemite
     sha256 "6c361a421b5f59c32c1098d4c29dd0c8f3048cf288c8880e954448926ed26184" => :mavericks
