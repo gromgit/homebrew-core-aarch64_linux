f2227b36b020c6af4628f33e92cfd8f37d28fc75
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 11:29:33 +0100

    enca: update 1.19 bottle.

--- Formula/enca.rb
+++ Formula/enca.rb
@@ -6,6 +6,7 @@ class Enca < Formula
   head "https://github.com/nijel/enca.git"
 
   bottle do
+    sha256 "0920a4dd92de3f4d7725e6753a37d1cb5f2468063f4020def9167648ff21e046" => :sierra
     sha256 "889b9d13ff462aee05bb0afdbe012f6a388a2b5e30e13b55954f94a18db69a13" => :el_capitan
     sha256 "c7e41db5725d169800674add5dfc3ab82d888f55f6703e44cda109348ed509e7" => :yosemite
     sha256 "9dcf7437c2ccedd4a7e10b167ba37c184a0e3aed08714af382f85771196cb0bb" => :mavericks
