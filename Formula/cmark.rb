2879063f35472f5aed5d12042786a432fea7c98e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 19:09:27 +0100

    cmark: update 0.26.1 bottle.

--- Formula/cmark.rb
+++ Formula/cmark.rb
@@ -6,6 +6,7 @@ class Cmark < Formula
 
   bottle do
     cellar :any
+    sha256 "98a7a59ab704bd4f73ea13aab5e5e728c536657c115fd6fa5074f081f739fb55" => :sierra
     sha256 "5df622e01450936300ed1d69eded3bd6ecd3b5bab4b57c7c8ee87822b41fa28d" => :el_capitan
     sha256 "ed3b5eb6d3a5c55bb3c1c75848a8439db6a30de7322db0c12075e218aaef25cb" => :yosemite
     sha256 "4797178f1c1360fc53a115d6760b5d677707204ae588fa848ce869d58ae35bf4" => :mavericks
