2bd2296d445510527d8f4ff35fa460d3f649f451
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 02:59:54 +0100

    unbound: update 1.5.9 bottle.

--- Formula/unbound.rb
+++ Formula/unbound.rb
@@ -6,6 +6,7 @@ class Unbound < Formula
 
   bottle do
     cellar :any
+    sha256 "e76d88065a95409c1e0bb322334daf3dffc0a1d2dbe86f5b6c0349b5d26ab8d0" => :sierra
     sha256 "0be2dedc49de0ba2afa1ac12542e20155625eb6bbad784065dd7c01da0550863" => :el_capitan
     sha256 "0b641ab092cbd5aa02b33054f6f3fa27c76787bd17d7c4f1cd9dc7d798725a60" => :yosemite
     sha256 "bdf1c759a2767507889a253069dc2cb9ef9c3bb558e08113665b59a6bcbbc204" => :mavericks
