1852b5499c42e7e6edebbde0f98dd311de2978f3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 05:00:28 +0100

    eiffelstudio: update 16.05 bottle.

--- Formula/eiffelstudio.rb
+++ Formula/eiffelstudio.rb
@@ -6,6 +6,7 @@ class Eiffelstudio < Formula
 
   bottle do
     cellar :any
+    sha256 "d19b199c5e25f0dd6589107a879a9ec3fb06ebf02a44491cf7430ac8798a8e85" => :sierra
     sha256 "4231e0735d8a2db64e084e84bc9fe9eab31fd6cdc144f6519f07e5db87812dc8" => :el_capitan
     sha256 "ee15c05e2f4410e479e338b7fb263fdccb03e55cf3dbceb3724140ea872cd058" => :yosemite
     sha256 "2eda67f3856bb63209d0ca8292d47feea8fd3077fbfc345e7fc5bd03599d833e" => :mavericks
