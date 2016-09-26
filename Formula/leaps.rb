e3e49f80c756a9e99f5cfb928029381ca4ba15ad
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 00:58:55 +0100

    leaps: update 0.5.0 bottle.

--- Formula/leaps.rb
+++ Formula/leaps.rb
@@ -10,7 +10,8 @@ class Leaps < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "0258e0b038825e9c49aad7c629b5b648536347928615f118e1ea0a022b523e51" => :sierra
     sha256 "fa920e95afa37d1a2690e3d5ac2cd6010f0b8c67f56c989250c542867cfc8825" => :el_capitan
     sha256 "dcaf8dd1314c3b3d1b3e904e7c37f3b251cd5d3e7fc5e58fc49bec09418a9c29" => :yosemite
     sha256 "b7d162e71b1daa2ff79218c0ea9caf51f8bd0f6d003c7c605c713f76a559daf1" => :mavericks
