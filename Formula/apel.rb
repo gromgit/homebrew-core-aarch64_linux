773410ca0761ad68820093e6f48d6773d095605c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 20:36:51 +0100

    apel: update 10.8 bottle.

--- Formula/apel.rb
+++ Formula/apel.rb
@@ -6,6 +6,7 @@ class Apel < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f39010912a4a48b6dfce1b64e420bf3e3979a373a15a16dd5d5440c97c2d3a76" => :sierra
     sha256 "f47d90fd2aea06a0e52a75b84af03c7a97f479f00f621168eb5afb6f911e999f" => :el_capitan
     sha256 "90038f974eb80c5d670990f349a13d629e2139098720ca13b5a26c7c9a8c9360" => :yosemite
     sha256 "00acef6949043235fc8a613c1d5dc9f58d8e365bde486d42461fc89449ff834b" => :mavericks
