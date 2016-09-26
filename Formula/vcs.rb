4ceb3fbcf8ca44e6c36003a979284d710de79e78
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 00:29:36 +0100

    vcs: update 1.13.2 bottle.

--- Formula/vcs.rb
+++ Formula/vcs.rb
@@ -6,6 +6,7 @@ class Vcs < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0dca28fe119b535229501593178b46f326e1124880458f56499d0281ce87ccba" => :sierra
     sha256 "dc49a592164ea2e3051b344731baf565819f1e63ec71e0a505bddfd0f06bb61d" => :el_capitan
     sha256 "9722755748810bbb7f75ee2a47161c382fba614845d1407a9825a3b4fb8c0ee6" => :yosemite
     sha256 "d8c55b780119fa7ab6b2bdc7c9b71a8257a612bea52a2a359fdf289fa937509f" => :mavericks
