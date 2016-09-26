91317d5d38c102fb758b0f71a934a3f0f3420ff2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 12:08:05 +0100

    powerman: update 2.3.24 bottle.

--- Formula/powerman.rb
+++ Formula/powerman.rb
@@ -5,6 +5,7 @@ class Powerman < Formula
   sha256 "85d5d0e0aef05a1637a8efe58f436f1548d2411c98c90c1616d22ee79c19d275"
 
   bottle do
+    sha256 "c31cb738ebc06c20c07cd2c6c10ff69bd21df62657cbf7f5d08a8a54317f0fc5" => :sierra
     sha256 "26b893065e1f5e2f345d8b75fe2770bb4616fb62d7aec73022c4472df8158b2a" => :el_capitan
     sha256 "e90be29b1ab6ab310f39775973edbaa647a0ac12d81bbde374bbc5ed262c317c" => :yosemite
     sha256 "412042f83e03f1cbd9e285b1566bb785471dd79f93049df8bbfdde3544122a24" => :mavericks
