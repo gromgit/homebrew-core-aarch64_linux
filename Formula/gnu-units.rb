fe70360f7675013a66cbaa73bba8dbf1e8945ecc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 02:59:53 +0100

    gnu-units: update 2.13 bottle.

--- Formula/gnu-units.rb
+++ Formula/gnu-units.rb
@@ -6,6 +6,7 @@ class GnuUnits < Formula
   sha256 "0ba5403111f8e5ea22be7d51ab74c8ccb576dc30ddfbf18a46cb51f9139790ab"
 
   bottle do
+    sha256 "66934e9cc51bc341484bd1f8a6380292ddcd1a0a776b92517e726bbf06a860f1" => :sierra
     sha256 "76982a809c64af4f45e97cc450cd717b4a12fef538c78ba362bf45b0e49877cb" => :el_capitan
     sha256 "ad75ca96d61f7f09dd95c831b6b468e04c4912e9834bebea17a02162758e0f9b" => :yosemite
     sha256 "89fe5cd2960513e836909fae964b0e787d8b9747711cc1cee79fc30be2332fcd" => :mavericks
