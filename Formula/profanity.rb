9cf0e0908b8402faffcb92cd4248e7a61d27026e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 18:14:31 +0100

    profanity: update 0.4.7_2 bottle.

--- Formula/profanity.rb
+++ Formula/profanity.rb
@@ -8,6 +8,7 @@ class Profanity < Formula
   head "https://github.com/boothj5/profanity.git"
 
   bottle do
+    sha256 "ba20918546e3fa26fd2f22338d5647a96bec401d1d61631516080fc5f1176ed6" => :sierra
     sha256 "e42d99f8e15de670c0d5d0d3bd90cc946759c6d55697ecf0f20928f8ac63c062" => :el_capitan
     sha256 "01807fbbe872b9921ca8c9af55fc0843ae57bbdeeed565ed929f254523d6f758" => :yosemite
     sha256 "f1e8e071263d364cf86b0adcaf475ef3dabb2cd6eededa8aa1f7e7d4e6fe0c2b" => :mavericks
