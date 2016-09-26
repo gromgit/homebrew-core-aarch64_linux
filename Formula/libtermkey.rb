11bf66817f6f7f1dd0ea5662d416342ef882ff82
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 03:55:51 +0100

    libtermkey: update 0.18 bottle.

--- Formula/libtermkey.rb
+++ Formula/libtermkey.rb
@@ -6,6 +6,7 @@ class Libtermkey < Formula
 
   bottle do
     cellar :any
+    sha256 "3ae613ddcb184ff95fce6763779284e2e03d6c39e441d3663965d216b57eb5a8" => :sierra
     sha256 "6f5a5f6120c9c9a61df4d706b6ec3fb8825ea07e013367e5b92158e361decb60" => :el_capitan
     sha256 "fe1507d296341a28e7f4ec68dabbbcdcd7f2f07371b74a594ee99dd8f2bd3b36" => :yosemite
     sha256 "29fb6a44d6ea50f8bc7cedf6dbdf6de9ad85c9568797ff40b4cb25c2ac28b07a" => :mavericks
