035ec287ca5a1e18492c314498f106c232882891
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 13:13:39 +0100

    gnome-builder: update 3.20.4 bottle.

--- Formula/gnome-builder.rb
+++ Formula/gnome-builder.rb
@@ -5,6 +5,7 @@ class GnomeBuilder < Formula
   sha256 "b3e69495cd0fcfd3e3a7590f52aadaae7f45393eefd47ab5581a851cdd489041"
 
   bottle do
+    sha256 "721c67af0e9bd89b0afdd1fcd61aa7039b63707655243c09b8ff67cd175dc3ad" => :sierra
     sha256 "f998975b2542a7b8a28a0c3c59a5a01efc025f6452bafa742021be3ad07115d6" => :el_capitan
     sha256 "485b20e976415463b143b67c4f778e7da373a68d6053d1c1b1f4807fb8c8115e" => :yosemite
     sha256 "bbcab43c4a10dd4bca406eae433cdbd6c459c97decdd67fa8fa8870aa5da5bd0" => :mavericks
