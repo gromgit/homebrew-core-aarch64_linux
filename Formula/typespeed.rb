6cd756a1655914a792e8be521aac7995f484c70a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 02:25:46 +0100

    typespeed: update 0.6.5 bottle.

--- Formula/typespeed.rb
+++ Formula/typespeed.rb
@@ -5,6 +5,7 @@ class Typespeed < Formula
   sha256 "5c860385ceed8a60f13217cc0192c4c2b4705c3e80f9866f7d72ff306eb72961"
 
   bottle do
+    sha256 "8c4af1a3e4e8c32eab5da01fc3b30604eaad86bf84f4a96af7878599c92a4a36" => :sierra
     sha256 "82223614505b9fac677ba4ac4ad9f3b646597cddde604f8c981cc000b8ed7bf6" => :el_capitan
     sha256 "23d3acaedb26f5bedccc2186dec138679fdea40f036edd57ce84ff363c082206" => :yosemite
     sha256 "2dca5154739dc08e5a3ac88acfed5377f7547124cfceb6e4e86e9a1bf2fa531a" => :mavericks
