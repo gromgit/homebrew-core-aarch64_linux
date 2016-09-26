ed822546bdfdb3dbbd0551ba059f8c3b0389ce46
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 20:01:58 +0100

    ape: update 1.1.2 bottle.

--- Formula/ape.rb
+++ Formula/ape.rb
@@ -6,6 +6,7 @@ class Ape < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "dd095fa1465e0a20613481720e26a5917c08882b340a18ab4d351a95e5eb3a3e" => :sierra
     sha256 "259b19e211ff6d6ffc376db0b3696a912a6ac48dca83cbcbe525c78e56755c82" => :el_capitan
     sha256 "3859216e566e6faaccc7183d737527dd4785260a698c8344520e7951baebca76" => :yosemite
     sha256 "83c7ef23309dec2e7bd4bec3ae75b6f0e04fcfecbda489c90810b6948eb3bb28" => :mavericks
