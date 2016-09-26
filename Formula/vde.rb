0b70368c8ba9becb267076550e3ff512235decfb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 00:58:55 +0100

    vde: update 2.3.2 bottle.

--- Formula/vde.rb
+++ Formula/vde.rb
@@ -5,6 +5,7 @@ class Vde < Formula
   sha256 "22df546a63dac88320d35d61b7833bbbcbef13529ad009c7ce3c5cb32250af93"
 
   bottle do
+    sha256 "3e9215e631197e75b179f9f33bc27483ead1d2ce8b3875b7846cc2568cfe67c2" => :sierra
     sha256 "97989b0577f7a1fbd13c916aff1e61391cf3d7b886c4ef965f0b765e034c8bbc" => :el_capitan
     sha256 "5ca4100e3dae3df4704e2fdf9ae07a1fb0637d2cb2e916e7db931a4cb84a0c55" => :yosemite
     sha256 "ab336b6d84a03dd981d70ab8b377ec3a61dcb9abfffd233c84a0e74c8fadc8b8" => :mavericks
