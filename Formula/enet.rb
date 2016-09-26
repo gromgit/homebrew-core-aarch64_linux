44842292785c8eea261a57dd20971c92f97dc17c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 12:08:05 +0100

    enet: update 1.3.13 bottle.

--- Formula/enet.rb
+++ Formula/enet.rb
@@ -6,6 +6,7 @@ class Enet < Formula
 
   bottle do
     cellar :any
+    sha256 "0f17698e0206e19f1ab693131f91f8f3267366a9ca9646463323880d459cf7a1" => :sierra
     sha256 "4efff251b59d56ebc5368cf8cbdc6e59f48a52d97e8fd73e1900869cb3a634bb" => :el_capitan
     sha256 "14a3e5aebe4adb9a76c643a85a91e15c4815fec76697709a3d56f68c3921666e" => :yosemite
     sha256 "927255e6afb287eb95c6c7a53b275084229d3c11ad58066b824628e30a89dcea" => :mavericks
