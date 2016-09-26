f9e83c33510b289c21c1ed2524457451142c799a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 03:37:32 +0100

    kore: update 2.0.0 bottle.

--- Formula/kore.rb
+++ Formula/kore.rb
@@ -6,6 +6,7 @@ class Kore < Formula
   head "https://github.com/jorisvink/kore.git"
 
   bottle do
+    sha256 "533aba9652749af143e213d66217d4330d7e5829334cd5258c22437266e78468" => :sierra
     sha256 "226f73e82833adaecc36f648c742619b4ae8795f2fb30fc77c6e37cd5c9e73f1" => :el_capitan
     sha256 "d6f89fc8e1527340fe295bc916327b1d41baf97dd5f0c8cd1f3f4b92c39c3da3" => :yosemite
   end
