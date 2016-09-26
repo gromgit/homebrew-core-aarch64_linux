ac8ee5823f2653b3245257072a33084e98823454
Bairen Yi <byronyi@users.noreply.github.com>
2016-09-26 22:37:54 +0800

    storm 1.0.2 (#5254)

--- Formula/storm.rb
+++ Formula/storm.rb
@@ -1,8 +1,8 @@
 class Storm < Formula
   desc "Distributed realtime computation system to process data streams"
   homepage "https://storm.apache.org"
-  url "https://www.apache.org/dyn/closer.cgi?path=storm/apache-storm-1.0.1/apache-storm-1.0.1.tar.gz"
-  sha256 "1574c08d8cfb6bc7509c78871e98a535f1386d6ca50fa71c880c39e8800620bf"
+  url "https://www.apache.org/dyn/closer.cgi?path=storm/apache-storm-1.0.2/apache-storm-1.0.2.tar.gz"
+  sha256 "b3fd2475e88e4b5e30f0e47eab70363419d67c15ddc7fe1f3c423ac0499eb76e"
 
   bottle :unneeded
 
