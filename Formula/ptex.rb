5b07077de1374e37a5f2f2f392ad1513cc36b99c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 23:20:44 +0100

    ptex: update 2.1.28 bottle.

--- Formula/ptex.rb
+++ Formula/ptex.rb
@@ -6,6 +6,7 @@ class Ptex < Formula
 
   bottle do
     cellar :any
+    sha256 "38572ce52fb672e652c3ac27a9704eebabef60b673e6e7a21d11ae14b565a75e" => :sierra
     sha256 "fd5994b3ab6f116c242a997a186a39a149a4ad2318d43cc587456f8e12797d87" => :el_capitan
     sha256 "71d3eee613d7825583ee7eb673928e6ad6a8876310f6628cb70f8ecb82ff53fb" => :yosemite
     sha256 "ef6b1d4b5969dd1512299cdc9e00e520464792ca37081a16ea4d77abcb253dcb" => :mavericks
