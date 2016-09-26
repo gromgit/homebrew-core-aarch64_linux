5749468846fa59f8f9139ae6f40efec353261cc6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 23:59:44 +0100

    grap: update 1.45 bottle.

--- Formula/grap.rb
+++ Formula/grap.rb
@@ -5,6 +5,7 @@ class Grap < Formula
   sha256 "906743cdccd029eee88a4a81718f9d0777149a3dc548672b3ef0ceaaf36a4ae0"
 
   bottle do
+    sha256 "4e4b22198d42beea1e531a6903f4085b67692e7da7bc1b7a3e51f42c235169d1" => :sierra
     sha256 "b9491c4bf9baeb0e9edc3cb0256f0199256b26fcc2fb76ac15153fd6f74c48f9" => :el_capitan
     sha256 "97c15c60c09da87b12c779fde541bff202f96327e76286a591d52a18b1c74d4e" => :yosemite
     sha256 "036bb10da8e432b027a2a88714d91e60ba3e297c94b8fe27ab48040e3800760b" => :mavericks
