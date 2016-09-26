e41ccc2293181b6733c409b973a4c64f6c94ea23
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 12:08:05 +0100

    nacl: update 20110221 bottle.

--- Formula/nacl.rb
+++ Formula/nacl.rb
@@ -7,7 +7,8 @@ class Nacl < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 2
+    rebuild 2
+    sha256 "86e5ef1c0a14b029d1ed3f63df48fde9c302adbbc3e1dcacd1bb7617bf547615" => :sierra
     sha256 "e08c93b814989405fa3b7db9e3a9c4f149e36aaab32aba44e9a2f1659d2d3efd" => :el_capitan
     sha256 "1a1a7fffc6d41f2f7bcc393375f2907f63b5a13f9414fe0827daef96246301e7" => :yosemite
     sha256 "44bbb2d7cb0daa6eb06c79e5881ae827786a04dece9b4a34cb0a6ea06cddb1e1" => :mavericks
