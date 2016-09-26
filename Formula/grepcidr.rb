497908ba5032cc3c1d2e90abbc85193084784b61
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 00:58:55 +0100

    grepcidr: update 2.0 bottle.

--- Formula/grepcidr.rb
+++ Formula/grepcidr.rb
@@ -6,6 +6,7 @@ class Grepcidr < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "12dfa49026bffb77ed1c4a08e9b60b56859eb183bbf791754d0b1d476ba6d795" => :sierra
     sha256 "31ccf6792cab3c5022530ef4576ea53e6dedd4855b939d11212fea0d7fa294dc" => :el_capitan
     sha256 "d0024b81610b4a698de415aef87958e2a61f74a9f1f2b7acf875f2f3d50ecc05" => :yosemite
     sha256 "c4ed3ba91b4acd41f51850b143ea9826275b221fc8f041098dfe5f5a429a0289" => :mavericks
