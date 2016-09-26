b4dface0a61b07389e168d1db0531e07b3090f20
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 15:42:52 +0100

    clog: update 1.3.0 bottle.

--- Formula/clog.rb
+++ Formula/clog.rb
@@ -7,6 +7,7 @@ class Clog < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "97e07b94ea058c766f4d036cc503fc6ec08ca64cddced33d63723e4611534595" => :sierra
     sha256 "8f42168b8e165c4c1f1265b410ef62087b370075cc27269f1908eb0f373645c5" => :el_capitan
     sha256 "a6c42c7d0795252434a3e1fc0307fc40490a4f29a9186408fa3ed7d82ba5f02e" => :yosemite
     sha256 "61ce3b9c332f9487f9981d8bb93d62fd4b6dfd0bbf0aa8f680b3fd625b2d8576" => :mavericks
