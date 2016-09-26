317e6decc4cbb7b80ea8bae66205d4136ede35d6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 12:39:43 +0100

    amap: update 5.4_2 bottle.

--- Formula/amap.rb
+++ Formula/amap.rb
@@ -7,6 +7,7 @@ class Amap < Formula
 
   bottle do
     cellar :any
+    sha256 "81ff7d4c48a2bb6e0ba15ef0dbcbf31f98f1a1410d65bd53cd60ffc0e8d8c9d1" => :sierra
     sha256 "c90c8fb7641960727299f576812dd38d88f836f9d3b99a21fdf652d2032acb52" => :el_capitan
     sha256 "0ab42765f948fe016bc38c8d6005e66a86e93b4e93b073615baaf0fa0f0e95dc" => :yosemite
     sha256 "18d4464b634e7aec9fefc45079dd97d0867b956ee71f189dc7f0393e77f7dba7" => :mavericks
