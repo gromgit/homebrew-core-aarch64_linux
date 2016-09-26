ab6c1e745693ccb2b3531276db983f4c8c862b76
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 17:29:05 +0100

    lbdb: update 0.41 bottle.

--- Formula/lbdb.rb
+++ Formula/lbdb.rb
@@ -7,7 +7,8 @@ class Lbdb < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 2
+    rebuild 2
+    sha256 "ea5e8fc0989f5f829e886781212abd6a4bc2d812a61a35e87c17709095f5ba59" => :sierra
     sha256 "d336634147b6e8466b09c0e6918733381cb3f0e7d933660969c9d7ffea53d724" => :el_capitan
     sha256 "1f7e155f77361a674442f376da2119486509f2f2802cca6ef6ea2b81b95f5339" => :yosemite
     sha256 "d9a370f2aa20e318db463903e5e6f1aa967a718ef97346c7b3eeafd7eb699a0e" => :mavericks
