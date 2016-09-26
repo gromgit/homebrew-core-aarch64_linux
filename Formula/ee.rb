30990048985756888af79dfabef683beaa0d16b0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 02:59:53 +0100

    ee: update 1.4.6 bottle.

--- Formula/ee.rb
+++ Formula/ee.rb
@@ -6,6 +6,7 @@ class Ee < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6ee6cda46ce7949176149637326332eedcf53e03d5e7fcd58759e5b173ef8fe4" => :sierra
     sha256 "3da2d595dec856251eb734ce0f12b5d04fd2c7354d48198c1014a923c63769ab" => :el_capitan
     sha256 "7cff46a098f916a37f45fa09799b79a204ba9866871413e596ab29295ece7b40" => :yosemite
     sha256 "b30c4ef2a49b136eb0912cad5ed4d36a954746c42aca3fb7d8197d827d8b3ff6" => :mavericks
