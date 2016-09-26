08724e0d49cf553b64c43e139f60eba92ed74ec1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 12:08:05 +0100

    lasi: update 1.1.2 bottle.

--- Formula/lasi.rb
+++ Formula/lasi.rb
@@ -8,6 +8,7 @@ class Lasi < Formula
 
   bottle do
     cellar :any
+    sha256 "777226a80b3fad497241ea6810d16d39846a4ff2d51acea4d4924cb44751419b" => :sierra
     sha256 "35a95d3bf2ad71999df616c06417883d25d22f343a40ce3a27b6a3fb11c1e689" => :el_capitan
     sha256 "4e1c3dc744e265709bc196acc936d8bf4307196972a772130a812c8c7bb1a80b" => :yosemite
     sha256 "31c9f3a86877476a4c1c6fbdd8c6064512cf3aebc50b25bf0be53a1b5ed2ba1e" => :mavericks
