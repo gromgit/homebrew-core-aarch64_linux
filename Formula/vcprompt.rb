77d7cdd27472bd7ace4d1f829e66e6edd247f4b1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 23:59:45 +0100

    vcprompt: update 1.2.1 bottle.

--- Formula/vcprompt.rb
+++ Formula/vcprompt.rb
@@ -6,7 +6,8 @@ class Vcprompt < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "9416ab35f637cc751b667f1a8481d17936faa58f39749d87e4e32b07b647f229" => :sierra
     sha256 "11e4de5f008aec3510274ef2265d6d30d214249e127cc1d3045b01b47232c96b" => :el_capitan
     sha256 "a6b02f96a018c993568e4ee43bfa23343c88aa4c6aa58b4727b69c01340f59ff" => :yosemite
     sha256 "ee133ff8277ce6d7792acd261ba3f27259e677badfe73b80ffd6fd08c6cd3665" => :mavericks
