2d32777885e75b6f84345cea08fae9f41457cf31
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 14:07:43 +0100

    lastpass-cli: update 1.0.0 bottle.

--- Formula/lastpass-cli.rb
+++ Formula/lastpass-cli.rb
@@ -7,6 +7,7 @@ class LastpassCli < Formula
 
   bottle do
     cellar :any
+    sha256 "bc5e958d0b2529ab05e78dfbecd332ddb7f2b061ffa89e8337519a8bfe58861e" => :sierra
     sha256 "42710f8cf1bef7628e61251e10bd0bf2a36f219e950fd40fb9d93561f87f4335" => :el_capitan
     sha256 "6d8a04d5bab5db00b80f5d6a35c639040ca0494e81ee85e7ae21bb52d8e617b8" => :yosemite
     sha256 "dc2eb72ebe79a0963dc9cae50ec6a38633740866b356e7521f37bf1c586a37f0" => :mavericks
