2c5e0d83aba676d3073e12cab5c6b011a4ba69a0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 05:31:38 +0100

    gnustep-make: update 2.6.8 bottle.

--- Formula/gnustep-make.rb
+++ Formula/gnustep-make.rb
@@ -6,6 +6,7 @@ class GnustepMake < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "72bdbf93d49c36d17dea050197fbe857aa44d56339993bc23b063e4a52f7880b" => :sierra
     sha256 "1598a85b3b9bbf721fb75d57c36d5dd6bc5810ec0f5281b35d2e8a13d24edf12" => :el_capitan
     sha256 "a944ce4d2cd6131d53837f1e674806a073f534e15870ffd01c484aaab8702f8c" => :yosemite
     sha256 "e1aed229b3582202cc41d91a208a0e92b36cdb05007450ddd1e6c824e9cde952" => :mavericks
