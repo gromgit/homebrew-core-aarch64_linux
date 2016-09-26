6560868717fa34b2e1cd3242d42b3670dc3e129a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 12:39:44 +0100

    ppl: update 1.2 bottle.

--- Formula/ppl.rb
+++ Formula/ppl.rb
@@ -5,6 +5,7 @@ class Ppl < Formula
   sha256 "691f0d5a4fb0e206f4e132fc9132c71d6e33cdda168470d40ac3cf62340e9a60"
 
   bottle do
+    sha256 "925201fe3772b8dc994c8edc742fe6925426a534a62f8d706c94ba8a859eacf7" => :sierra
     sha256 "34a1396fefc946754958e92d7ecffde0175b33f16f4545f46a00d4f63408cfd8" => :el_capitan
     sha256 "83c3f03aa0766155e98e322409f6d4389bfee3ec510848c7eab1c91329d5fa82" => :yosemite
     sha256 "d174ac172a3c986dfa92fe650354ce6b614d080f5ab497c90fc9b6395faf5caf" => :mavericks
