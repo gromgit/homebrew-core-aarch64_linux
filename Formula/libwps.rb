11523c89173bb53c744f34151790fa4ad087dd05
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 21:28:03 +0100

    libwps: update 0.4.2 bottle.

--- Formula/libwps.rb
+++ Formula/libwps.rb
@@ -6,6 +6,7 @@ class Libwps < Formula
 
   bottle do
     cellar :any
+    sha256 "bc2f7b7cd3b38e38c6f6ed0423406340f33c4719e4c4833a0f2af8e0df0d58a2" => :sierra
     sha256 "955cec0a064444c0c6db350b7c3a55b72b9382a98dae5060e25d78b928267fbb" => :el_capitan
     sha256 "b747ff7cf50572bfe44fa363f8e9a987d8a5c27f07315a6ca15c0e6903e75d82" => :yosemite
     sha256 "f41d5d0f77a02ed7f903a1761a5aabe7847f8dd4e6bf19c9aacd54924d7abcc6" => :mavericks
