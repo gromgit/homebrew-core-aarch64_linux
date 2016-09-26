3448875ee5ea863e46f6943d7d36e9424823f9ac
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 03:37:32 +0100

    musepack: update r475 bottle.

--- Formula/musepack.rb
+++ Formula/musepack.rb
@@ -7,6 +7,7 @@ class Musepack < Formula
 
   bottle do
     cellar :any
+    sha256 "26b774c3ca9b6c43cfdc868d71e635d292bcf218d0577fbc271be5138e8ef3c0" => :sierra
     sha256 "33e4734a8714484a2c506241d91ab45145c19228abe51d355e0cbd60020ee11a" => :el_capitan
     sha256 "2e6bad894063f1178ff43b5cd10a899b82baa757228c3a51488793a4a22acdf2" => :yosemite
     sha256 "4ff9d23d8631300ff2619d9f1a7d99e07f8636c6e8d8db8dbf1335a21c1f7e27" => :mavericks
