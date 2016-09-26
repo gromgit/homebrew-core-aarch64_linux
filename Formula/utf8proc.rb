ad702051a4588421e533bd2e15f54e056f0defe6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 16:52:19 +0100

    utf8proc: update 2.0.2 bottle.

--- Formula/utf8proc.rb
+++ Formula/utf8proc.rb
@@ -6,6 +6,7 @@ class Utf8proc < Formula
 
   bottle do
     cellar :any
+    sha256 "e0dd57a0dc6fd530a9519bf54fd3823879438b57b5a599adeae196f66984b541" => :sierra
     sha256 "1ceb4dfa21540ec620c0e6b04d2242aa5fc39ab8da1a231182317ebd08eff9e2" => :el_capitan
     sha256 "eb5434e4817951b5d57e1f7d3e926924f0b38036e79410d76c6a652f67701cfd" => :yosemite
     sha256 "2acce240bd4abeb3e931177dfaaa93637d43faff7f01305df1be66d5cd01dff5" => :mavericks
