38789a55a8e769abd49b735161e83180d20870af
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 09:04:18 +0100

    kyoto-tycoon: update 0.9.56_1 bottle.

--- Formula/kyoto-tycoon.rb
+++ Formula/kyoto-tycoon.rb
@@ -6,6 +6,7 @@ class KyotoTycoon < Formula
   revision 1
 
   bottle do
+    sha256 "0d10f657af62307608b9059e52339bf4961831ac8e713a68ea0522ad04428854" => :sierra
     sha256 "f018a810f3925d61fcce6a3ef558ac3d72fbbaf3f4e4ef10b7b5b1b994081a38" => :el_capitan
     sha256 "82548c79c4b3830b0991eb60656887d2b52f4b2d7be9467d2a446be34b550991" => :yosemite
     sha256 "b3fd82a6dd9e2949037da1a8c634632bab67c9628169c2f81bdbfc81515e7f50" => :mavericks
