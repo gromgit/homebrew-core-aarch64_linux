709118429531cfc7bb2ef22f58c8f986157d321c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 18:14:31 +0100

    libvterm: update 681 bottle.

--- Formula/libvterm.rb
+++ Formula/libvterm.rb
@@ -6,6 +6,7 @@ class Libvterm < Formula
 
   bottle do
     cellar :any
+    sha256 "1b0e1cd45ec1aa67280fa555c47139d2d0b36d7c28313148bd7d23d85a31178c" => :sierra
     sha256 "fdab6481377220ea48474d7af256df3b82ee202d28ba010d644aa5ff200c2fbd" => :el_capitan
     sha256 "e74dceb0e58c42be4c8e1ab96867ad71931b9412b7c692cfc40dc5bc924d0daa" => :yosemite
     sha256 "a3b42686b7e365b71c766cfb44bb564e5d0e157ac85cf9b9ffba6b6d570f3ef8" => :mavericks
