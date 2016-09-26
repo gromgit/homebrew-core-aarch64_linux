4d2c22d6caab7e40c82b49ba4996830ba6e3f0d7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 09:47:24 +0100

    align: update 1.7.5 bottle.

--- Formula/align.rb
+++ Formula/align.rb
@@ -6,6 +6,7 @@ class Align < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4d07f4f2ae948de293afdc80a5a736cf81da7c335cec1778f5b7304debda6599" => :sierra
     sha256 "c2c177c8be3b5a58e60f3a1f39d9fdd3cc3d39247d92be45142cd06ae80273bf" => :el_capitan
     sha256 "caa9e8c3b3a9d946b95d5222b1518c5307499d57fe17f593ec3911f9cc6eace7" => :yosemite
     sha256 "f903cb30e079f56c5743e2ca22a168c61d7a7c57b2cf6bc3c6492ed214a296a3" => :mavericks
