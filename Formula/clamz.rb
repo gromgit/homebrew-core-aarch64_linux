2cc331a8ed6b33068146777e9ea2150172dbaac5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 05:31:38 +0100

    clamz: update 0.5_1 bottle.

--- Formula/clamz.rb
+++ Formula/clamz.rb
@@ -7,6 +7,7 @@ class Clamz < Formula
 
   bottle do
     cellar :any
+    sha256 "fd35e22d601781e32cf9c5264f351c989d732d0a516617e3431522fef55bde61" => :sierra
     sha256 "b960106e00e01e4dd8ff259feab6e0a1e399d373aa79d2b5d622f2ccf6f1e41b" => :el_capitan
     sha256 "e0ba09e61f28b4d224f20b0922277b849bff48ce8c7738e8d22fe1a514d56fe2" => :yosemite
     sha256 "70f9f355c7f53a6201b5e175dbc6db9b1f8b275327250a1e70e06d5c139c2a53" => :mavericks
