fcde8659257a2e33b111671179de1878e1ee045b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 17:29:05 +0100

    epeg: update 0.9.1.042 bottle.

--- Formula/epeg.rb
+++ Formula/epeg.rb
@@ -8,6 +8,7 @@ class Epeg < Formula
 
   bottle do
     cellar :any
+    sha256 "17be20c46c7c9253312469a158a2546685e5a1eceaa889179fc01b57f125fab7" => :sierra
     sha256 "b2d3f25d28280107b503d31ebfdb59bf452fc117d027fa136934b621a922b0a4" => :el_capitan
     sha256 "f7abbd3547174648e37bd74d73e5ec0a6e4f8391c97a1153d0ed63adc7b02193" => :yosemite
     sha256 "617efb3a645d90f883921752c45d298178e26f535270c38fb46c44a46e3fcc9d" => :mavericks
