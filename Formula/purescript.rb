d3a7871a7367b18a4423a60daea4ea4e5c888890
ilovezfs <ilovezfs@icloud.com>
2016-09-26 02:55:20 -0700

    purescript: fix optparse-applicative issue
    Use upstream fix for building with optparse-applicative-0.13.0.0
    
    Closes #5248.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/purescript.rb
+++ Formula/purescript.rb
@@ -5,9 +5,23 @@ class Purescript < Formula
 
   desc "Strongly typed programming language that compiles to JavaScript"
   homepage "http://www.purescript.org"
-  url "https://github.com/purescript/purescript/archive/v0.9.3.tar.gz"
-  sha256 "70ea9ea189300e8ce7ea89eec818a1c9e39be1e758638898b936028fc0155783"
+  revision 1
+
   head "https://github.com/purescript/purescript.git"
+
+  stable do
+    url "https://github.com/purescript/purescript/archive/v0.9.3.tar.gz"
+    sha256 "70ea9ea189300e8ce7ea89eec818a1c9e39be1e758638898b936028fc0155783"
+
+    # Remove when 0.10.0 is released; fix "Variable not in scope" errors
+    # Adds compatibility with optparse-applicative 0.13.0.0
+    # Upstream PR was https://github.com/purescript/purescript/pull/2278
+    # Same as upstream commit https://github.com/purescript/purescript/commit/bdbf4e2
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/7a5ebb6/purescript/purescript-optparse-applicative.patch"
+      sha256 "22967efbda6cca1dcae6898d848160bff57112e229fc2a87642f5180089b5feb"
+    end
+  end
 
   bottle do
     sha256 "7493c36c9072080361e7c4e894fbc3b6207a41e28f916bbf832c45af17a9acd5" => :el_capitan
