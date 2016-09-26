fac89e1cd71bc478f62b139028917f0ef2400d48
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 23:20:44 +0100

    etl: update 0.04.19 bottle.

--- Formula/etl.rb
+++ Formula/etl.rb
@@ -6,6 +6,7 @@ class Etl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7275d40af2ee9e99feec8a04a9296b1167b24ca8f7125a875d08c13b4913e81b" => :sierra
     sha256 "10244415e0dbf71f94c7585595632a09773a49dbc5bf5ac8de7e062f29c7f2b4" => :el_capitan
     sha256 "29198ad9d848f2ff79b224a5467da1fb22a474de5ffc3e287196fd3822a45178" => :yosemite
     sha256 "024271929c1e3de9d4c4e256a932fa9525395f7421fc174e7010251ab9a4b37e" => :mavericks
