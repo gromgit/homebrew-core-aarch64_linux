bdf300d5fe8e3d01c629f694beb100f643d5177a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 11:29:33 +0100

    goolabs: update 0.3.0 bottle.

--- Formula/goolabs.rb
+++ Formula/goolabs.rb
@@ -6,6 +6,7 @@ class Goolabs < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9f542e33d1641a50c0faa25c193982a7498c1511a0b89e30ef72c021a31bdb71" => :sierra
     sha256 "e6dbce36a389b7f1197634034f5c547060e5c6491f799aa78a68719234a36507" => :el_capitan
     sha256 "dc27e5bf44ae61cdfca02fe3529f3a7d1572a76441571794ad698c54096e1419" => :yosemite
     sha256 "ce5182935462c479de2e574d3c10a512cdb134854538e442e2794bc7d6271355" => :mavericks
