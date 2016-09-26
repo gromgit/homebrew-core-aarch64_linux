8d66c297e0d4fc711bacd9d2d0cb627209148185
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 06:27:45 +0100

    alac: update 0.2.0 bottle.

--- Formula/alac.rb
+++ Formula/alac.rb
@@ -6,6 +6,7 @@ class Alac < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3c833c71834ea65498c761d4fe444a26e97e107433de526ab55ad1fb0d36a2ba" => :sierra
     sha256 "4cb85c125553c6c2a49576790c5be5e0b89096569131df3b8576f3499e65ef5a" => :el_capitan
     sha256 "a3a54a254a147f3a1173870bdd2e9399043b3e506d8c04383f99cf3ce67a4fca" => :yosemite
     sha256 "20cca431ce69d7eb2e5d894ebbfffdbc633eef2b3447be6d0afdb7c25cac8c0e" => :mavericks
