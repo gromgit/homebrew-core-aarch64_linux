ff90d1a5ea8f0a7ca9fef059870eb23fe63c55c6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 09:47:24 +0100

    elixirscript: update 0.21.0 bottle.

--- Formula/elixirscript.rb
+++ Formula/elixirscript.rb
@@ -8,6 +8,7 @@ class Elixirscript < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4243ede37038b43ff91c5c669cb4ef89aa61784d28f4b0e2290bb285a28917e3" => :sierra
     sha256 "bc42d5a2dba3a02c35436741bc9a3e5aaf79e75d9be04ddd20e115a092a6da72" => :el_capitan
     sha256 "3a0e04f52b69a4e9c852906439956b324cb11fe78534b5a72d1e2a9c6a00de4d" => :yosemite
     sha256 "349cb1b7acf6075a9ee5d3dddc42ed5aea179fd7162416c19e48a45f4436dc91" => :mavericks
