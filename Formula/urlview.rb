74378a18b2167d5e13305ffc57d0542bb0cdaf39
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 14:36:04 +0100

    urlview: update 0.9 bottle.

--- Formula/urlview.rb
+++ Formula/urlview.rb
@@ -7,7 +7,8 @@ class Urlview < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "0a65eaf4a1889d615e6abb61c0dd289df2054e1c2b224d842fd9170111f89bd1" => :sierra
     sha256 "9dbd6e280b74de72eecfab8d51728bfe46fc6a02f22a5c2e6b1639b904680ebb" => :el_capitan
     sha256 "dfbc0ddbb5705383fda6175930cb57bbc5b124c31e9b4ec879d86b6211efe3d0" => :yosemite
     sha256 "5ece2167d692912f75c743425a9b0dc50d63863335b38c6ecb72175ab1bc7eb8" => :mavericks
