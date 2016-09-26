43c6c6f8e14b04c83aff6646c40af55f28fa3631
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 09:47:25 +0100

    postmark: update 1.53 bottle.

--- Formula/postmark.rb
+++ Formula/postmark.rb
@@ -7,6 +7,7 @@ class Postmark < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9d702b7bc49f646fd1623b316fd62c84ee6413e035a81e95866b0ca7240f100e" => :sierra
     sha256 "784b46fe9883d27d347a44da73413ccf5c589088c0b57da577ebc1c79e64e1e6" => :el_capitan
     sha256 "7fb38c3960e124a836cdc48650fd5f4d1fc446897b590e0dd6b6b6b5cbdec522" => :yosemite
     sha256 "2ccb3812b371bc02e66d84ff853cb9684f8941485af3287424b4c183205bc649" => :mavericks
