c75361745cf4d1aa847ce4873233d39d954f8f6b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 18:45:47 +0100

    clutter-gtk: update 1.8.0 bottle.

--- Formula/clutter-gtk.rb
+++ Formula/clutter-gtk.rb
@@ -5,6 +5,7 @@ class ClutterGtk < Formula
   sha256 "742ef9d68ece36cbb1b2e1a4a6fbdad932f6645360be7e6de75abbb140dfbf1d"
 
   bottle do
+    sha256 "511f2583872ea32f10a9a91b51a287a95448e697da755317a10867259abadbb9" => :sierra
     sha256 "1491c366ff7686c09ffd8d23491e1689c716e6df820ef81bd20a5ca9f685e788" => :el_capitan
     sha256 "d12a6bdf9a4f12aadb2603031b23a35e94da32bdfe1c612868f60d6edd14d377" => :yosemite
     sha256 "e0a49758f14433e41493183cc8a0f4106ec2f42435a11a8f64ef1c6f576255cf" => :mavericks
