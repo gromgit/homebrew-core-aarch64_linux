17adbeb73406c68db096c38c1affe78539b3b0bc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 14:07:43 +0100

    uriparser: update 0.8.4 bottle.

--- Formula/uriparser.rb
+++ Formula/uriparser.rb
@@ -7,6 +7,7 @@ class Uriparser < Formula
 
   bottle do
     cellar :any
+    sha256 "d1bea100c4dfd9c34ed7c39b7e5c27f2a24453da48fc6b45f37a2e23e7c13eb5" => :sierra
     sha256 "9e4e1ad413aa5ffb4f0e52666df1c6f60551f085969a4e48583d4745d8a85fe6" => :el_capitan
     sha256 "22c5ea8ff69d218577f29684e1b73d81c6d37850639e14e2da497f40ceb29cee" => :yosemite
     sha256 "a09c4d7a93bf0e6f7e67b9c0a30d881f4eda1471acaf1e80c74e4b8f145332bc" => :mavericks
