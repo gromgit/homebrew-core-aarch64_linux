bcc2c1522b6f8ffdeabd8aa6307b2f8db7f6e809
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 16:52:19 +0100

    snobol4: update 1.5 bottle.

--- Formula/snobol4.rb
+++ Formula/snobol4.rb
@@ -5,6 +5,7 @@ class Snobol4 < Formula
   sha256 "9f7ec649f2d700a30091af3bbd68db90b916d728200f915b1ba522bcfd0d7abd"
 
   bottle do
+    sha256 "2c8d1b2a54a3a3f0d810c88bc0a2545dbea08f73b57dda6052c4de27bdde62ee" => :sierra
     sha256 "f4ee5ba3a933998e7ea1493bab469f00f4ddd13a3e8458002ee43ba6f0cd0e74" => :el_capitan
     sha256 "6903e1b05a795eae13f2f97fd2f1f4b883b03e1c94ba28e3747b3df98c6a955d" => :yosemite
     sha256 "b76b9e5bbeccd4b879b2b3c450f3388b82f4641d4d414e0f2d83768eeb0c058b" => :mavericks
