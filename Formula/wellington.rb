c7cb3a76b3d82afc235f40b32ec533dadc5b49bb
Drew Wells <drew.wells00@gmail.com>
2016-09-25 21:41:36 -0500

    wellington 1.0.3
    updated to new golang project patterns
    switch to ssl for homepage
    this version works with Go 1.7.1 Sierra
    
    Signed-off-by: Drew Wells <drew.wells00@gmail.com>
    
    Closes #5226.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/wellington.rb
+++ Formula/wellington.rb
@@ -1,13 +1,11 @@
 require "language/go"
 
 class Wellington < Formula
-  desc "Adds file awareness to SASS"
-  homepage "https://github.com/wellington/wellington"
-
-  stable do
-    url "https://github.com/wellington/wellington/archive/v0.9.3.tar.gz"
-    sha256 "108e5626dad9494a1de7d6241a2f96c6fa5bd774133a00c301d42abd1089f3e2"
-  end
+  desc "Project-focused tool to manage Sass and spriting"
+  homepage "https://getwt.io/"
+  url "https://github.com/wellington/wellington/archive/v1.0.3.tar.gz"
+  sha256 "d3d49a53bc6d206a751585d4cf14fc895d02ea21cb1ef3c508e032db192d3001"
+  head "https://github.com/wellington/wellington.git"
 
   bottle do
     cellar :any_skip_relocation
@@ -17,72 +15,40 @@ class Wellington < Formula
     sha256 "2ef26afb326f22102249daee8c795fb467ce9716a2aae306efd23efc65020df3" => :mavericks
   end
 
-  devel do
-    url "https://github.com/wellington/wellington/archive/v1.0.0-beta1.tar.gz"
-    sha256 "6ea2a260ba7146a6bd87f42ab22082dfd84eb5aa52adae0629cbe71395cf56de"
-    version "1.0.0-beta1"
-  end
-
   needs :cxx11
 
-  head do
-    url "https://github.com/wellington/wellington.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
-
   depends_on "go" => :build
-  depends_on "godep" => :build
   depends_on "pkg-config" => :build
 
-  go_resource "github.com/kr/fs" do
-    url "https://github.com/kr/fs.git",
-        :revision => "2788f0dbd16903de03cb8186e5c7d97b69ad387b"
-  end
-
-  go_resource "golang.org/x/tools" do
-    url "https://github.com/golang/tools.git",
-        :revision => "ea5101579e09ace53571c8a5bae6ebb896f8d5e4"
+  go_resource "golang.org/x/net" do
+    url "https://github.com/golang/net.git",
+        :revision => "f09c4662a0bd6bd8943ac7b4931e185df9471da4"
   end
 
   def install
     ENV.cxx11 if MacOS.version < :mavericks
 
-    version = File.read("version.txt").chomp
     ENV["GOPATH"] = buildpath
+
+    dir = buildpath/"src/github.com/wellington/wellington"
+    dir.install buildpath.children
     Language::Go.stage_deps resources, buildpath/"src"
 
-    system "godep", "restore"
-
-    # Build libsass from source for head build
-    if build.head?
-      ENV.cxx11
-      ENV["PKG_CONFIG_PATH"] = buildpath/"src/github.com/wellington/go-libsass/lib/pkgconfig"
-
-      ENV.append "CGO_LDFLAGS", "-stdlib=libc++" if ENV.compiler == :clang
-      cd "src/github.com/wellington/go-libsass/" do
-        system "make", "deps"
-      end
+    cd dir do
+      system "go", "build", "-ldflags",
+             "-X github.com/wellington/wellington/version.Version=#{version}",
+             "-o", bin/"wt", "wt/main.go"
+      prefix.install_metafiles
     end
-
-    # symlink into $GOPATH so builds work
-    mkdir_p buildpath/"src/github.com/wellington"
-    ln_s buildpath, buildpath/"src/github.com/wellington/wellington"
-
-    system "go", "build", "-ldflags", "-X github.com/wellington/wellington/version.Version #{version}", "-o", "dist/wt", "wt/main.go"
-    bin.install "dist/wt"
   end
 
   test do
     s = "div { p { color: red; } }"
     expected = <<-EOS.undent
-      Reading from stdin, -h for help
       /* line 1, stdin */
       div p {
         color: red; }
     EOS
-    assert_equal expected, pipe_output("#{bin}/wt", s, 0)
+    assert_equal expected, pipe_output("#{bin}/wt --comment", s, 0)
   end
 end
