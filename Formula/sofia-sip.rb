a9dc0a2b8453453aa8d18d89f1db665e655a31f1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 20:36:52 +0100

    sofia-sip: update 1.12.11_1 bottle.

--- Formula/sofia-sip.rb
+++ Formula/sofia-sip.rb
@@ -7,6 +7,7 @@ class SofiaSip < Formula
 
   bottle do
     cellar :any
+    sha256 "ddbaf0fc570bd632c520057effc396a1f5e5cc48930644559ca9cafd9cd25360" => :sierra
     sha256 "2e78cc40330c53363fb1dddc0568464001b46944628283e26811e4e6ccae28fe" => :el_capitan
     sha256 "e2f9ede8ce51b2074659a1fcf576d83031c9037aafd3c39f75330e8e7cb236a5" => :yosemite
     sha256 "1e68620530a8dda00d795bdf92f7c564174eefde5d7e703839de2e080bd89ea4" => :mavericks
