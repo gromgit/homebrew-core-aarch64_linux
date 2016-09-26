12e0fd7616167b042ff07ff773b82a8078db4718
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-26 19:09:28 +0100

    progress: update 0.13 bottle.

--- Formula/progress.rb
+++ Formula/progress.rb
@@ -7,6 +7,7 @@ class Progress < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2f7422b950b3dfb9d2fe7be3380cec810321bf5fe490b5ddb225b29580be891d" => :sierra
     sha256 "92fdddbc22594566e79458fa3b4a63bee3f4bcdd569b88827837e8a18d6eaf3a" => :el_capitan
     sha256 "35741d1986bd5df66ecec5603af82aefa0f1c5728fd6481780a1b77875edc4ad" => :yosemite
     sha256 "965f987b35a069855eb9d43068c05f284272211ebfaaf1e3197386974d6da3a6" => :mavericks
