6231dc35e15e09c23ed8850db040de28ede41229
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 10:57:41 +0100

    pxz: update 4.999.9_1 bottle.

--- Formula/pxz.rb
+++ Formula/pxz.rb
@@ -8,6 +8,7 @@ class Pxz < Formula
 
   bottle do
     cellar :any
+    sha256 "9e1f3354927dc20d1c1bdfea578f2f608ec038852f62fae18c05b70a92642a15" => :sierra
     sha256 "fb94fe085e695c7a097701bdac07cb406ff1ea59a9d220f82f2f458a0a860325" => :el_capitan
     sha256 "443b2e618e2977c3abc54eb50b43e4ab8ac727878914425ba24d9789be737c16" => :yosemite
     sha256 "164217b1098d7a3231eb6bee4c80d5e224a7f2afcd9d2a42773d513481f476eb" => :mavericks
