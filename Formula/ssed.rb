0cd16f69c030bba0da9ad3d309a780b70c4de317
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 23:54:19 +0100

    ssed: update 3.62 bottle.

--- Formula/ssed.rb
+++ Formula/ssed.rb
@@ -5,6 +5,7 @@ class Ssed < Formula
   sha256 "af7ff67e052efabf3fd07d967161c39db0480adc7c01f5100a1996fec60b8ec4"
 
   bottle do
+    sha256 "0834fb44e8acc946a9a7030b4313295777c3c9d8808f67c24daaaa2ce6c6a2ed" => :sierra
     sha256 "6c8c5a5547c7c97e59f1f7284083f46d5e61e7ab80ac08ebdfeec4a4b4e95fbb" => :el_capitan
     sha256 "7e313ca41f3a8e37bc91ab4a9d8c7acbf508cd7a89ac605df7cee3adcf108510" => :yosemite
     sha256 "448b2fdfee6f84c3d72fdf29d5ccd042027a9850ea16d75b5f4ee576d8cbadcc" => :mavericks
