e27a7a5c70caa250b8d3d7b7509d43bc872480fa
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 23:58:09 +0100

    convertlit: update 1.8 bottle.

--- Formula/convertlit.rb
+++ Formula/convertlit.rb
@@ -7,6 +7,7 @@ class Convertlit < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "43e28e7711f27843223b29d351ba0ce03a4deee76bbc99c4bdac50969b8eaeb7" => :sierra
     sha256 "66b05c2c6371f16620c82b31b507413556b511b859644322c65f4ceea4a83a64" => :el_capitan
     sha256 "024a9fdb4b58a3e04c12ec300facbac636b3510f8726726c4be93c60cf272ab1" => :yosemite
     sha256 "366ce6afb71223d3f14939c5d4d382a90cf56df7920cb41dca0eeae72e809702" => :mavericks
