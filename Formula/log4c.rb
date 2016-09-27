e5481b011bdff9c030ac1274dd822cf0417a7a85
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 23:57:12 +0100

    log4c: update 1.2.4 bottle.

--- Formula/log4c.rb
+++ Formula/log4c.rb
@@ -7,6 +7,7 @@ class Log4c < Formula
   head ":pserver:anonymous:@log4c.cvs.sourceforge.net:/cvsroot/log4c", :using => :cvs
 
   bottle do
+    sha256 "171a6c3f12f957d5442998f0f02df959aa4376ef543338765930ed4e062ef0ea" => :sierra
     sha256 "2334e58e3ae201b28362707d2b64701e2e1378695e915baad886956e4edea50a" => :el_capitan
     sha256 "d345d0ab182855859fb21148c708893a395ecd416ba3f05d5e2a5a3111f2bc61" => :yosemite
     sha256 "676eeaaf8bb2b112ff1e5c1586cc0302e6e5dd0253939b7533dd519095497171" => :mavericks
