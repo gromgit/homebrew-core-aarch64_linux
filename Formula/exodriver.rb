4aea11e67adc9ff8b4243ee093f7638d0be48ed4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 11:35:10 +0100

    exodriver: update 2.5.3 bottle.

--- Formula/exodriver.rb
+++ Formula/exodriver.rb
@@ -8,6 +8,7 @@ class Exodriver < Formula
 
   bottle do
     cellar :any
+    sha256 "911a3d571d0ccc6f2ae8df67caec275470d179a2b435fec1cd521fe86f271bc6" => :sierra
     sha256 "5897f540e38aded535f6f3aa11d1df93c90305fe5196c106057ebbdda8620806" => :el_capitan
     sha256 "dc685c1d58f01fbe304d36fc33f1c2f2993599c83636054755c0f0b7cc887969" => :yosemite
     sha256 "bcc5eea01c69b14b1a2f3256105523016294ae54f9e026f03fbd4f65f7ce5c66" => :mavericks
