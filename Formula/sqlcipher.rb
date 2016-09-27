697930b1d16fca46699be4b8856e5befb2e19706
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 11:35:11 +0100

    sqlcipher: update 3.4.0 bottle.

--- Formula/sqlcipher.rb
+++ Formula/sqlcipher.rb
@@ -8,6 +8,7 @@ class Sqlcipher < Formula
 
   bottle do
     cellar :any
+    sha256 "7a34ffc2a9ce6284f34d178986bffcfb90c0670620b01a6e44dbd1bd4a7857f6" => :sierra
     sha256 "4bf0216834686577051b6d58aa2b6b2a1c0a91b45831a0c8f07aea9877e2df70" => :el_capitan
     sha256 "0354c675587c3727bf360c1562d87743361045fabca11567ad8e1fff7dd586f4" => :yosemite
     sha256 "ed88417a177ddd5340dd7f04523aa1d444863648a7cd82a3bfa642bc4f5b1992" => :mavericks
