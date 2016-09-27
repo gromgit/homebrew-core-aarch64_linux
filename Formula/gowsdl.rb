18ec8b639d5c9bae2a348d3ed71136931aa942ea
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 23:43:46 +0100

    gowsdl: update 0.2.1 bottle.

--- Formula/gowsdl.rb
+++ Formula/gowsdl.rb
@@ -7,6 +7,7 @@ class Gowsdl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9423cc4a3a0ff5a786f38fe49f8cd01873d0fd29208028b3857bf791f78970df" => :sierra
     sha256 "c8ca657c1c298726fed20e337c289721f8f357bc68fda4e225b73b8197da7a28" => :el_capitan
     sha256 "b0af34a9c397fda92fc535830388724bb3371e10fe7ed91e8cc72849bf9356b2" => :yosemite
     sha256 "00f1906326025dffe89200c430314cff2e0f9c1b8ca42357d7c2ec5469ce23cd" => :mavericks
