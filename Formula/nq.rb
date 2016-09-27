0b154bdb4a262890851b05ed6731123739fda578
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 21:08:05 +0100

    nq: update 0.1 bottle.

--- Formula/nq.rb
+++ Formula/nq.rb
@@ -8,6 +8,7 @@ class Nq < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f321a091c031ca3870cfd10844835ddbef55bd82c8c6ecf5275ae3323b9b4897" => :sierra
     sha256 "5ce475e389f345a2f1a0376a4253cf59672393f58b88e5a03b25f4c3c7d14ba6" => :el_capitan
     sha256 "1d6d53c0c53a230a600ee3be346c07625d7deffd2c134c46eb61d429d4392722" => :yosemite
   end
