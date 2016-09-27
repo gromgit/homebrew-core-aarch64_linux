ee310cd9fc8c84bb29316de3950dbb85102e3318
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 12:39:05 +0100

    lensfun: update 0.3.2 bottle.

--- Formula/lensfun.rb
+++ Formula/lensfun.rb
@@ -6,6 +6,7 @@ class Lensfun < Formula
   head "http://git.code.sf.net/p/lensfun/code.git"
 
   bottle do
+    sha256 "0a843c2663d8f6bca2f524fc6b8e3933587126481b4549667ae2cd9f3e96da00" => :sierra
     sha256 "4f75d6f3a79a1af741caf67401edf7436b7c1ad0d100ac1978043f9fadd14b96" => :el_capitan
     sha256 "2f86f02f6ffa890f23da494f4ea84fad99809793b472260e968846b3f1599ef1" => :yosemite
     sha256 "16bafe93312a00428a38e2488c825363152d7b11706596031649164780a41017" => :mavericks
