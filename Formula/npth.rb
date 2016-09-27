c4b575cd6535622c5c2127ec976f27980a041180
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 19:52:55 +0100

    npth: update 1.2 bottle.

--- Formula/npth.rb
+++ Formula/npth.rb
@@ -7,6 +7,7 @@ class Npth < Formula
 
   bottle do
     cellar :any
+    sha256 "e7ea90bcf31139dbdbbb496039d19f733a27296a99a968bd2decd9e5500c8eb4" => :sierra
     sha256 "e0383072b47031a5ca5b129447fe7b0d90a161c78c0dcd91f3d398067262d469" => :el_capitan
     sha256 "ed46e1fed9a33a4961b32fe2de844d8bbeff8f44d4863a3f2da364bb292ffa59" => :yosemite
     sha256 "cc6148b47d88580a4d18efc4140ea423189333c564e11c61b8a4764a182ac766" => :mavericks
