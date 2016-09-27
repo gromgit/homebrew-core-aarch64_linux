a63fb454233dea7a437355c84c5e2925471c62a1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 19:27:04 +0100

    logcheck: update 1.3.17 bottle.

--- Formula/logcheck.rb
+++ Formula/logcheck.rb
@@ -7,7 +7,8 @@ class Logcheck < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 3
+    rebuild 3
+    sha256 "7fe24461210fc2d5f4809e08566e85e010a2715b9b7a6426f686327f40f25cdf" => :sierra
     sha256 "aab0ab066fe378c88c74b9783a90fb0a4896dd3a6258d00b08cd1d0d2987b108" => :el_capitan
     sha256 "c75e01fb14bdd0adfc04e110a3c8a65d036b9bd71ac03a6ac58d69006a892fe9" => :yosemite
     sha256 "25f2dfec7bb30fded535bdb354767a2680108dcd93d0627f8384a115c008cf89" => :mavericks
