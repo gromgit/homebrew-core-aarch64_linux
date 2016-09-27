a24b6905b24618f08ad466a0b5d3b71bd88a88a8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 23:43:46 +0100

    snapraid: update 10.0 bottle.

--- Formula/snapraid.rb
+++ Formula/snapraid.rb
@@ -12,6 +12,7 @@ class Snapraid < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3b4ecf8fe89ab329411170c6825b6a7339f0762d097ccd9398dd66a17ab9cc0d" => :sierra
     sha256 "ffa69724c9970b59d658a9c5dafd59b2cce7180219da4960de215dd106a20001" => :el_capitan
     sha256 "4cc74d39c187216544ebb319b1ed6537198456551725b8f15e5397be687854af" => :yosemite
     sha256 "bb60bddade2e2b5cfa7df974c7d8aabc1819c00ab2a98694c87d86ed9abbf804" => :mavericks
