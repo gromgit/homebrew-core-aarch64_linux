578278b8feb475accc36be70ff316b66e816d140
ilovezfs <ilovezfs@icloud.com>
2016-09-27 09:40:44 -0700

    mpfr 3.1.5
    Closes #5298.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mpfr.rb
+++ Formula/mpfr.rb
@@ -2,9 +2,9 @@ class Mpfr < Formula
   desc "C library for multiple-precision floating-point computations"
   homepage "http://www.mpfr.org/"
   # Upstream is down a lot, so use mirrors
-  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/m/mpfr4/mpfr4_3.1.4.orig.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/mpfr/mpfr-3.1.4.tar.xz"
-  sha256 "761413b16d749c53e2bfd2b1dfaa3b027b0e793e404b90b5fbaeef60af6517f5"
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/m/mpfr4/mpfr4_3.1.5.orig.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/mpfr/mpfr-3.1.5.tar.xz"
+  sha256 "015fde82b3979fbe5f83501986d328331ba8ddf008c1ff3da3c238f49ca062bc"
 
   bottle do
     cellar :any
@@ -12,12 +12,6 @@ class Mpfr < Formula
     sha256 "06b1521d06eeec85bf2052fd416f19f7624901641596617e39b8c433540d990f" => :el_capitan
     sha256 "2695bf9b6090c817144b0650a9eb332fdc22efa37265fefd7ea2c55d4f62732b" => :yosemite
     sha256 "47dc6d25a9ba43dd2e710849d7d2dbfffc6de3ded02554ea973cd26797a2b31d" => :mavericks
-  end
-
-  # http://www.mpfr.org/mpfr-current/allpatches
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/30141937f9d16/mpfr/3.1.4-patches.diff"
-    sha256 "9a03c3f304feaff747d1832f4a0f3653bbd24764df403305add0b76ca6cd6541"
   end
 
   option "32-bit"
