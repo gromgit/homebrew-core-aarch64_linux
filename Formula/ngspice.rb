ec96394c74d88e55475d9e6c9c77a796af3d414e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 23:32:10 +0100

    ngspice: update 26 bottle.

--- Formula/ngspice.rb
+++ Formula/ngspice.rb
@@ -14,6 +14,7 @@ class Ngspice < Formula
   end
 
   bottle do
+    sha256 "267664560fe26b818457e00ba124ad1951b452ad978bd34ee7d7b9f4567bd3b9" => :sierra
     sha256 "b508b0ca4f88db18b48796715a9de16ee5e7c119b13809ce01a87685f3a9393e" => :el_capitan
     sha256 "ea1f0b65fcb3bb926124694d210e14800be6e47e73dcbf47a8e2baaac5314f44" => :yosemite
     sha256 "4bc737b0801c82999a290f602f416de0ed38777df599afc688d9695c1f16d704" => :mavericks
