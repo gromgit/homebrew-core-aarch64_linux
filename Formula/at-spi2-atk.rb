8f96dca14b4551dc8aeef5a9eac0c0e702b34b50
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 21:08:04 +0100

    at-spi2-atk: update 2.20.1 bottle.

--- Formula/at-spi2-atk.rb
+++ Formula/at-spi2-atk.rb
@@ -6,6 +6,7 @@ class AtSpi2Atk < Formula
 
   bottle do
     cellar :any
+    sha256 "d011be23d3fd7b8283c6d8272215f46bc96cdccf104393feaba4534cd7d0c4f2" => :sierra
     sha256 "b4882d54f60b72e358085993472ec1ddd7a970a85cb7bf7eea28614eb1a6f4a7" => :el_capitan
     sha256 "ff9994b927e7b0cd7dc4ea4cffec772770f5b67422ce37ce9264d726af72c75c" => :yosemite
     sha256 "c775a1002d8adfaec9ca439b910243702bd50b519e8fec648a776472179dd28e" => :mavericks
