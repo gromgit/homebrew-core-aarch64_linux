527efc1d84e697e53e49ea9821ac0a0e36046af9
blackstar257 <blackstar257@gmail.com>
2016-09-26 22:35:03 -0700

    kompose 0.2.0 (new formula)
    Closes #5276.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Formula/kompose.rb
@@ -0,0 +1,20 @@
+class Kompose < Formula
+  desc "Tool to move from `docker-compose` to Kubernetes"
+  homepage "http://kompose.io"
+  url "https://github.com/kubernetes-incubator/kompose/archive/v0.2.0.tar.gz"
+  sha256 "a6be3935ff666b75ad06f389a5c23184c3c83659ae25135ab7c2c3d37b22d92d"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    mkdir_p buildpath/"src/github.com/kubernetes-incubator"
+    ln_s buildpath, buildpath/"src/github.com/kubernetes-incubator/kompose"
+    system "make", "bin"
+    bin.install "kompose"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/kompose version")
+  end
+end
