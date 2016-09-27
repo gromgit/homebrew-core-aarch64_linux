b1a693e5eac37ba6a007735612b88156a2dce302
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 00:25:26 +0100

    vpcs: update 0.6 bottle.

--- Formula/vpcs.rb
+++ Formula/vpcs.rb
@@ -6,6 +6,7 @@ class Vpcs < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c98123da9a4158f1b9c2cb7757f553e172adf64f106028af07aee50e9d4e9111" => :sierra
     sha256 "b0f62e8959cb98038b817174eec35dd657a8107241f58fc1421a57adcb1eedea" => :el_capitan
     sha256 "d905fc7dd6ca0dd07a0bafb6d8e71bebd0d2c3c516c0c00f6adb87aebf6d7057" => :yosemite
     sha256 "a6a5285281f81b0f035b024f0f8e81211ac71f69390c5c6d03820ce7eccdc116" => :mavericks
