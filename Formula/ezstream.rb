e53adb11f881c9df2efd7118dd3423374f3701ca
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 17:26:40 +0100

    ezstream: update 0.6.0 bottle.

--- Formula/ezstream.rb
+++ Formula/ezstream.rb
@@ -6,7 +6,8 @@ class Ezstream < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "eee3cc2ed988d5c0e131d9ba8f0aef0e7bb520311096a9719b914c0a1ca6ffe4" => :sierra
     sha256 "365c26a87addf50359e65ccd98ce4244b61f7e9015a335ff47bf55a90b35ad19" => :el_capitan
     sha256 "dfa4b2537b1ce6b0da0c4214ccedca664bdd2e69962fa6579d9c437b1ff94e92" => :yosemite
     sha256 "04e3a89b8b1e91ce160ec94c981b71d8fb08d7be8fef3da3f1c33b29dc9e3f8c" => :mavericks
