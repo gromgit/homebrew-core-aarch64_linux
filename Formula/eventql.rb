3749c711bd8b599b21edf5d02d86a6f50891c37d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 04:00:00 +0100

    eventql: update 0.3.2 bottle.

--- Formula/eventql.rb
+++ Formula/eventql.rb
@@ -6,6 +6,7 @@ class Eventql < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "bb5a697a9e91c2523e24d8d81e842d26001992aa5d4d9867dac7ec4ebe6b81a0" => :sierra
     sha256 "7d128462969fc4e431c6a486fca5f397fc15c46e657a622d042c0bf531efba82" => :el_capitan
     sha256 "482a655e17d0b905d0101d2716f52266e1ec8bb6a4814f30a576431fa2d179e4" => :yosemite
     sha256 "92adde0e08a4bc68c90f1e93f13ee7afc7bc1c7b69f2187610545bcbcb76abd8" => :mavericks
