588c3d971df95cf41e7a4f56f8c12cf9af2a9af9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 10:57:41 +0100

    litmus: update 0.13 bottle.

--- Formula/litmus.rb
+++ Formula/litmus.rb
@@ -6,6 +6,7 @@ class Litmus < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "fd55b54482009ae82c55532f45fa5ef054688fb2df5685eadf4b7a3dbcb8b1e3" => :sierra
     sha256 "10a49ffd5dfde15e35effdcf51ba8097086294e33fb7cb7fab65628c03cdef3e" => :el_capitan
     sha256 "07ce12b82b1a1bb63c9c0c596a7fb3f577afb369b87b4d6bce23e97fae9b610f" => :yosemite
     sha256 "c9fd308d0d348619c8af1a5f95c4f129734fcc07e2b3eea1151fbadd89225b94" => :mavericks
