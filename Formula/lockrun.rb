be63455f1c753cfeb6794a908f595b4ff79664ff
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 23:53:37 +0100

    lockrun: update 1.1.3 bottle.

--- Formula/lockrun.rb
+++ Formula/lockrun.rb
@@ -7,6 +7,7 @@ class Lockrun < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5c37b3e9c3f55cfa50379c72fc00259bffa8d3d48688bcaaa44122805ffa4c3a" => :sierra
     sha256 "10782346442c28b235f80579c1b0dac3ec784fb151f7bef475757c1bde944b16" => :el_capitan
     sha256 "8f2914ed87c42a369b3870b5688720cf0cc7382ae6428452ba32fdf0e422ab57" => :yosemite
     sha256 "c319dba85122ea12d120a7ea3acbdc1c50ee35f2eadb274aa5ec59622b026ca0" => :mavericks
