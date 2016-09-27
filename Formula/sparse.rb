fc1e3ff846d9c074d2488feaafc37d44e3585b32
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 02:47:44 +0100

    sparse: update 0.5.0 bottle.

--- Formula/sparse.rb
+++ Formula/sparse.rb
@@ -7,6 +7,7 @@ class Sparse < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "bf8ccfa445389a6f69dbb6a58660b2228b58560e8ecc7a8045a07538c3702a88" => :sierra
     sha256 "78bc3435fd4818f38848fb1b6c57bfb70f540adf527f71390274d0d2a31efbac" => :el_capitan
     sha256 "be1693a0ec2050625898d960ffd99468d4ce7471785fe1ae6d6f373da2416b11" => :yosemite
     sha256 "4c33d0589d81abda44fef8904892dc7f6361e96caa82012a71101e9fefe4425c" => :mavericks
