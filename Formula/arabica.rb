33cf976701db49ee94de6bc3e9701dd1a6656c0c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 03:59:59 +0100

    arabica: update 20160214 bottle.

--- Formula/arabica.rb
+++ Formula/arabica.rb
@@ -8,6 +8,7 @@ class Arabica < Formula
 
   bottle do
     cellar :any
+    sha256 "6a08cb5b8af8d2034569451ad499260acebc610ab390383b20398fc10a6fb115" => :sierra
     sha256 "185edd120b5759f25d1e69b9f24840eef6a404b1001c90e684546e359cf75928" => :el_capitan
     sha256 "1882f30edf8da8d98df603a6006c3dce96e21941e4266103366930b3e5a922c2" => :yosemite
     sha256 "5d247d4d5819106404bc7091e3b6141b4d298c77636bee39bfc524a3c5481e7f" => :mavericks
