f9f351bcb15c06076325932f051d9051f2979df5
Shuanglei Tao <tsl0922@gmail.com>
2016-09-28 07:39:30 +0800

    ttyd 1.1.0 (new formula)
    Closes #5310.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Formula/ttyd.rb
@@ -0,0 +1,23 @@
+class Ttyd < Formula
+  desc "Command-line tool for sharing terminal over the web"
+  homepage "https://github.com/tsl0922/ttyd"
+  url "https://github.com/tsl0922/ttyd/archive/1.1.0.tar.gz"
+  sha256 "12e38c16a34d36384813cbf4b459343185d2fde17cc696aab9b87f2150fd766e"
+  head "https://github.com/tsl0922/ttyd.git"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "openssl"
+  depends_on "json-c"
+  depends_on "libwebsockets"
+
+  def install
+    cmake_args = std_cmake_args + ["-DOPENSSL_ROOT_DIR=#{Formula["openssl"].opt_prefix}"]
+    system "cmake", ".", *cmake_args
+    system "make", "install"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ttyd --version")
+  end
+end
