6811e3fbadf7237d0cec69ea1231b38d545be4d0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 10:03:41 +0100

    splint: update 3.1.2 bottle.

--- Formula/splint.rb
+++ Formula/splint.rb
@@ -5,6 +5,7 @@ class Splint < Formula
   sha256 "c78db643df663313e3fa9d565118391825dd937617819c6efc7966cdf444fb0a"
 
   bottle do
+    sha256 "e5847a77e137e1f2339b55ae1fff93a94de33c6ad1a3a34c8a45b3d06a6bf0f9" => :sierra
     sha256 "9eac9f8e530c1d9fc238b57f9d4e143fbf5727450657ba92e6d721660777753b" => :el_capitan
     sha256 "4b385e4fcf9b82fa2ebd8dabaef7e712039b3f7c83d2f6d5e3263ebf51e7b6d7" => :yosemite
     sha256 "ad8551b508f303c69499a60456de49d2b77d1f0f2383383d3c01c1b657a230b6" => :mavericks
