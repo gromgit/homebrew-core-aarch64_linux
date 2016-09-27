dbdd469f5ea472dbcd1f72a03fb9bec4bf4ae732
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 02:47:44 +0100

    nethogs: update 0.8.5 bottle.

--- Formula/nethogs.rb
+++ Formula/nethogs.rb
@@ -6,6 +6,7 @@ class Nethogs < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "25731b08cad9b8f5a420172bab21d6986d49ee7044cbdd3a7717c6b10ee8da8d" => :sierra
     sha256 "f133cdfd28bd88778241a607fda39d17419d8aaf24fd6de49d8a086e112fc7bf" => :el_capitan
     sha256 "aa00775f5f8add09c031b13d3ec2575b082da47aa573e869288b28cad879f431" => :yosemite
     sha256 "3652cadd0558c01c522e586a483b03b95badf98028ab3b7f9a227f8bae275def" => :mavericks
