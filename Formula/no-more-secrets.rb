178c2f3610852e3a53ebdb26781641b2f1ad87fb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 23:55:23 +0100

    no-more-secrets: update 0.2.1 bottle.

--- Formula/no-more-secrets.rb
+++ Formula/no-more-secrets.rb
@@ -6,6 +6,7 @@ class NoMoreSecrets < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5fba9c507bf8b91747a85e849d5954b3292c57371184553670a986d81ee1abd2" => :sierra
     sha256 "731a98492d79ba911f862f6e0749cec40983e23ccb22ef55fe020d07760cdee9" => :el_capitan
     sha256 "15baf1b90d42f6215780f6b51b2dd51a0b41423c6fa68725124c66b94bf0d9cf" => :yosemite
     sha256 "f3eac856adca1ff5a5573e32902052e3a43629cb2bd09bec1945a663b5392a63" => :mavericks
