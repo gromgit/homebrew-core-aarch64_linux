39c0bb26565204090b0d80adb0d10d70a79a8f85
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 12:39:05 +0100

    colorsvn: update 0.3.3_1 bottle.

--- Formula/colorsvn.rb
+++ Formula/colorsvn.rb
@@ -7,6 +7,7 @@ class Colorsvn < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5c56662f331161022c31f665d980e077d6a01328864c6c59c137de3b0b57e4f2" => :sierra
     sha256 "bf4048c281332c5cfcae4fc74c0fa233ad84c3fe2c111e633101d593284fe601" => :el_capitan
     sha256 "88c79f8a9bc43d118449ce9d97061af4633f15f942a0a48caef5e1b327aea0e5" => :yosemite
     sha256 "2711d058fa4c892f350b6309a82f7eeb85455bc1b336afc75587c467121a553d" => :mavericks
