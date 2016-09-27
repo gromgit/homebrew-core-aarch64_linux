8df9cf4ba4ae1f7489e87c62608e88976d461bc9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 01:43:55 +0100

    sparkey: update 0.2.0 bottle.

--- Formula/sparkey.rb
+++ Formula/sparkey.rb
@@ -6,7 +6,8 @@ class Sparkey < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "035ae7c18eb13ee8ae2167aafecade136cec43816aeff76df790f7a5e3350408" => :sierra
     sha256 "fb1e14e774cf28b43cda024e73aa1201967dfa3c8d192500e56b3d42bdf41850" => :el_capitan
     sha256 "40bb3d9f1937bcfc3ddb1608228affa3676bafea61665f9aa1525936d2887260" => :yosemite
     sha256 "b7a5df3013850fbb10e2e46ffde968972bed654dfab6964784b7b2b00b054e2d" => :mavericks
