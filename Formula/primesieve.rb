e3f8d7bd8d306652763f3add67c640be034c9264
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 23:43:46 +0100

    primesieve: update 5.7.0 bottle.

--- Formula/primesieve.rb
+++ Formula/primesieve.rb
@@ -6,6 +6,7 @@ class Primesieve < Formula
 
   bottle do
     cellar :any
+    sha256 "5e50eb398e186c61cf06d17d4c4cc3a42f5424ed40400f4392e9bcd890ae5951" => :sierra
     sha256 "bbf14a1041b404069102a0eb853fdfe8d099ccb3aa67a9b453e42f560919c4d9" => :el_capitan
     sha256 "159886a4250641220e2ef2c41f58caf3c595485df32340f68e9007214a860e6a" => :yosemite
     sha256 "b15504dd7f1bfb4cd9dc5d4474b7180bd5b345d49e10619f5383516efa061964" => :mavericks
