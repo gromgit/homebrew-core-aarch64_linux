537254881be87ed31ba2202df4bfe64ee0ebeb9f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 08:27:31 +0100

    exempi: update 2.3.0 bottle.

--- Formula/exempi.rb
+++ Formula/exempi.rb
@@ -6,6 +6,7 @@ class Exempi < Formula
 
   bottle do
     cellar :any
+    sha256 "9f5de7fcc210405533becdd6a25595df115674aa5828596242562cdac0681e71" => :sierra
     sha256 "8445c3fe80c44a8b7589415b797704340ec865e1c572a728f753c33ebeeec141" => :el_capitan
     sha256 "02f7d164840640fef806abf0b22e53ab4e479189fe077ecf2bcc4f7fac812164" => :yosemite
     sha256 "204ac5dd569a2f54fe13a586a1fbc011b96de1a58531dc4d9694d452148bd236" => :mavericks
