61cc350ef8f29922e098cf31366e6cdc8d35f59b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 21:39:20 +0100

    vorbis-tools: update 1.4.0_1 bottle.

--- Formula/vorbis-tools.rb
+++ Formula/vorbis-tools.rb
@@ -6,6 +6,7 @@ class VorbisTools < Formula
   revision 1
 
   bottle do
+    sha256 "d98cd4b862786d666f031829fa72a367734ac0ec63c6477e80e16b575ead3b51" => :sierra
     sha256 "b95e6fb92f692cd321bc09952fc9d369533d847c320eac9f0172f0fce3f4beff" => :el_capitan
     sha256 "0619896b3b6b268bc1f8ac81b52b77011196a019134473f2b680e9c941214590" => :yosemite
     sha256 "4f4355cd1413f3aec51d51287fe4bedebd4571e1222f611421f5557edc411cab" => :mavericks
