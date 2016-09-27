7846933ffb6bda0fd3341b318ce2e13eb6f0ba44
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 18:58:50 +0100

    asn1c: update 0.9.27 bottle.

--- Formula/asn1c.rb
+++ Formula/asn1c.rb
@@ -6,6 +6,7 @@ class Asn1c < Formula
   sha256 "025f64e1c27211b36e181350b52fde34ad23f4330fff96b2563ed3fda7b0db9e"
 
   bottle do
+    sha256 "3f959cce0a231c1aff0f4dd5c5f4824314e3546aeb914b549843fe93b66f8816" => :sierra
     sha256 "c5fe1a7eafbd35d91566118f3e0f29ac27e5f8dd769f038bc34fa2eb4873723a" => :el_capitan
     sha256 "d3ea138d06691c9aa0589efb8d55bb908d79b65d3b6775e02f21cbabddee284c" => :yosemite
     sha256 "8be35afe4531688fb5bfbe4212e989151c75523882404cb0f4362c0c558ba033" => :mavericks
