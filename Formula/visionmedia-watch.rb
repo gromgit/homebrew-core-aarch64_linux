b7a34ed568962d2f6306c87b8fe7f911ad4cee4c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 23:53:38 +0100

    visionmedia-watch: update 0.3.1 bottle.

--- Formula/visionmedia-watch.rb
+++ Formula/visionmedia-watch.rb
@@ -8,7 +8,8 @@ class VisionmediaWatch < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "2d0df99a4e8b377f064c393a4e349cf12374df139a3cf04f76dd8b69f2558d39" => :sierra
     sha256 "b43dbb305fcb6c681d2208456a1f39dd0aa5b97790b629ac907a666869119f20" => :el_capitan
     sha256 "4d31b501672801394c687aa45a44741f3461fb4730e96fe94197a1e7952fe2e0" => :yosemite
     sha256 "941cd639bf71a7d0e7397b77a3eebce4bd06fbc4ef30aac69147b04b3f6569f8" => :mavericks
