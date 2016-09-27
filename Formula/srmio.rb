e57270c127c5106a92d402ee917c78b9ea565858
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 19:52:55 +0100

    srmio: update 0.1.1~git1 bottle.

--- Formula/srmio.rb
+++ Formula/srmio.rb
@@ -7,6 +7,7 @@ class Srmio < Formula
 
   bottle do
     cellar :any
+    sha256 "9ca9c4a2d17c7f431b1ad9899ae97ea22ec44e24a9c0c60220638c0f31f9b2c4" => :sierra
     sha256 "9e45cba0daaa89683552f1feb19cd49c42d27a311113ecb204ae8c2e48231f3f" => :el_capitan
     sha256 "e71a6c2fac5115cae2fe1a8b7eea9fb5800b96f908adf357a667b5df70bd7089" => :yosemite
     sha256 "d0c35e531e9defc37adc487e00a18ce46b59181bbdf74d46cbc9f5618153d5e4" => :mavericks
