473f9a376e60082a861dbba9e398121000148022
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 19:27:03 +0100

    assh: update 2.4.1 bottle.

--- Formula/assh.rb
+++ Formula/assh.rb
@@ -7,6 +7,7 @@ class Assh < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "28e49700c458e4c8df4e9342a927bc1cbc09877cf762a07d4ee7893b1c47393d" => :sierra
     sha256 "f2a3fa0bc38c3ddc7cf442fbd4114bbc49a4753bbe9963502b46be0825212f19" => :el_capitan
     sha256 "75876951c5bc5706898141f4591fdb1f807ddd6849ae1bd2e75dc3dcdc0baa13" => :yosemite
     sha256 "88a5f8d738264b099333ec4e4fdbcbdf5aada82a3b788a5086b825769036fa23" => :mavericks
