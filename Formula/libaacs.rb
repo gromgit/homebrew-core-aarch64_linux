3f578775be6942ac96c2324e908696309aac971a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 23:53:37 +0100

    libaacs: update 0.8.1 bottle.

--- Formula/libaacs.rb
+++ Formula/libaacs.rb
@@ -7,7 +7,8 @@ class Libaacs < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "02b04ab4f2f48ab65fec5a79756df19ed7dccc9b58006fb861d2ff66c266aff1" => :sierra
     sha256 "9c7aed37c3991fd326c976c498423a1df4801f3ef65c8bc7a8b68a8a87f1bc31" => :el_capitan
     sha256 "5b7526780e9ad562555a03d2d3d66c6aabdc9b0502aad0537b5588ab568fca6f" => :yosemite
     sha256 "d440c657e0cfd21cc6e8b86bed857a731f8cd80fa574a5366a5c70fb6192bbd7" => :mavericks
