932c5daaa3261de807a414f119f308d8718a92f9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 00:25:25 +0100

    cpmtools: update 2.20 bottle.

--- Formula/cpmtools.rb
+++ Formula/cpmtools.rb
@@ -5,7 +5,8 @@ class Cpmtools < Formula
   sha256 "d8c7e78a9750994124f3aab6e461da8fa0021acc7dbad76eafbac8b0ed8279d3"
 
   bottle do
-    revision 1
+    rebuild 1
+    sha256 "b9074a4680a3fedd140d31d6597f286a8b49fd47cdedae36b45356ec2f60b18f" => :sierra
     sha256 "a659a233dc338bb7ec669185e22f0c19d03d9697bbe3521473cb7ff791fd010a" => :el_capitan
     sha256 "a127034e7197c21acbcc172b86d476988754ed56bb44b0d9b447f362017fc8bd" => :yosemite
     sha256 "b810122c220af6b36ab9316deec811adca68313d4371f8a0121239c40b94a015" => :mavericks
