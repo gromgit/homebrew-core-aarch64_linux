d40f51cde77337e629763d19802673dfd60fe4d1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 17:26:41 +0100

    libantlr3c: update 3.4_1 bottle.

--- Formula/libantlr3c.rb
+++ Formula/libantlr3c.rb
@@ -9,6 +9,7 @@ class Libantlr3c < Formula
 
   bottle do
     cellar :any
+    sha256 "a5e779c431e16bdaab829c774468ce11f8e7ea359412800e294433b011704541" => :sierra
     sha256 "fea1cde8ae732cdbbffa6a6d329239b1da067d2b69424d53178e60309748c403" => :el_capitan
     sha256 "8026d876b20980138c076cb4008f358deb858204b6399c436cf45e93594274e7" => :yosemite
     sha256 "010c3866a3c183b10dec76f845ce826a26a5a384074f8c56f41abeb9e4aa6e77" => :mavericks
