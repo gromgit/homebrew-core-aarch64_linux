2e5be9b678cac9cea5e16f483157180eb7ebca15
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 23:43:46 +0100

    libuvc: update 0.0.5 bottle.

--- Formula/libuvc.rb
+++ Formula/libuvc.rb
@@ -8,7 +8,8 @@ class Libuvc < Formula
 
   bottle do
     cellar :any
-    revision 2
+    rebuild 2
+    sha256 "87fe5bb38f766397c7dfd1a22529f781679dbf32f0bc2c1d2e1f5c53c1b124dd" => :sierra
     sha256 "6893748683e4eef0e0bd595acc9d7dce1e70d06778e264514ffd0fd26f3cf22d" => :el_capitan
     sha256 "5a20089a01f4c7f1d85bc91f6f2369ce5b1f1c4faf8c9beed2a764f80609ab41" => :yosemite
     sha256 "c2b007f5dad961d8767cc129d69ba352e0c8d6dff9746ee45697ceff6208ed8a" => :mavericks
