e870b8b6dc2c4f8bef714597782102079174af53
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 19:52:56 +0100

    voltdb: update 5.6 bottle.

--- Formula/voltdb.rb
+++ Formula/voltdb.rb
@@ -7,6 +7,7 @@ class Voltdb < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6e1e07f1c931d65d5b78d0257e96105e07c7c933044a277f164be733d459ea1f" => :sierra
     sha256 "abc39a5926dc83b8516c8697beeef5d722049bf487d28db60f4a095cc5299329" => :el_capitan
     sha256 "300a47847d49c7e2d2dfb37c860cd4e60b1074d43dab450433a8c49879c9c46b" => :yosemite
     sha256 "de7b5adf80d177bde762b71d1044a901b2b68b0b22ec328f73137b99fc885248" => :mavericks
