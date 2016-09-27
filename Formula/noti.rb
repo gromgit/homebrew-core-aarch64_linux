fea7f2fb6b6acf111587b71683d6e2c4fc69e622
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 18:58:51 +0100

    noti: update 2.3.0 bottle.

--- Formula/noti.rb
+++ Formula/noti.rb
@@ -6,6 +6,7 @@ class Noti < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8df2d0230ae15425b49c14d3f84772114f7392e355ea2e0af7ed80564a8ebfc7" => :sierra
     sha256 "a1a6a536e0bed405afa38ed991a384022dc78145b0592207afc9b81d98f851e9" => :el_capitan
     sha256 "39e250fd963f55f25a6433226ac956e6d59dd070b4ac112f94e38b3937a877ec" => :yosemite
     sha256 "cae07c7c8a9e506974879cd37c1e146924bfdf527bb29ed51205f28fd33a22f9" => :mavericks
