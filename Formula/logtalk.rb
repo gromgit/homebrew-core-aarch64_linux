dd1c6fdd56da4ca1b0caaa92653af9b5c251a6f8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 22:56:19 +0100

    logtalk: update 3.07.0 bottle.

--- Formula/logtalk.rb
+++ Formula/logtalk.rb
@@ -7,6 +7,7 @@ class Logtalk < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ee5251fe0985273e8b0d30a6456eee3a0f74f277b0fe2647249a196a0c622075" => :sierra
     sha256 "60c1a91c0780e01bbe4506c58645254ca3d7ef69646245834e3891946cc9b582" => :el_capitan
     sha256 "8cf2388939520f59ce94b73f3b4bb1d0b2cd4b5b8de51c5b7588b246fbcb40b1" => :yosemite
     sha256 "15a0821a53dd2dcd33d4b87ab666809752c0ae623cdd52aec1a2f16ecfbb6bb2" => :mavericks
