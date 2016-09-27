18459c3462db3cec20e9266b5cb98acf66a145dd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 04:34:37 +0100

    compose2kube: add 0.0.2 bottle.

--- Formula/compose2kube.rb
+++ Formula/compose2kube.rb
@@ -5,6 +5,12 @@ class Compose2kube < Formula
   sha256 "d09b86994949f883c5aa4d041a12f6c5a8989f3755a2fb49a2abac2ad5380c30"
   head "https://github.com/kelseyhightower/compose2kube.git"
 
+  bottle do
+    sha256 "e77b7695ecee39b715f8e04a11a4b0d1299e6c3f02350414c27cf68cd25eb7cb" => :sierra
+    sha256 "c2b21ee0e5d1afbb2bf411cab7c8080f391967a67ccd0ebb798bcd3aa9158588" => :el_capitan
+    sha256 "95150f85aed2956b82385d0d73cf45cd9bc2445100f69eaae19c0388f00b37ec" => :yosemite
+  end
+
   depends_on "go" => :build
 
   def install
