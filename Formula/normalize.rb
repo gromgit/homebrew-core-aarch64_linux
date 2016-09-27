78353d7b69fd7a1e873f34faab0341f77d215624
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 17:55:06 +0100

    normalize: update 0.7.7 bottle.

--- Formula/normalize.rb
+++ Formula/normalize.rb
@@ -6,7 +6,8 @@ class Normalize < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "1165de2721e8b4d7f373b9ad10f52c2cd49c44a24cd8fddab5ba51983164cefe" => :sierra
     sha256 "052ab2e8b1f6a2aa1e634a30749612d927b5cee5cc9302e057bd02c599a1c256" => :el_capitan
     sha256 "dcb42f107b9674e50d8994215f6d125e0fb9523b1d99b393fd00ee2b827be5e0" => :yosemite
     sha256 "9c12615d384a706feb8ddb693dadacfc5bfc48827e5722dd6476325bbe5e90b9" => :mavericks
