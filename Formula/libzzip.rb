03a07462cf83e40f2689fc0f6da5303566834610
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-27 05:43:07 +0100

    libzzip: update 0.13.62 bottle.

--- Formula/libzzip.rb
+++ Formula/libzzip.rb
@@ -6,7 +6,8 @@ class Libzzip < Formula
 
   bottle do
     cellar :any
-    revision 3
+    rebuild 3
+    sha256 "301d6dd1b0d24f8aaccfbd3737bbf51d6c477af59c2d06838acfe6faa1921189" => :sierra
     sha256 "661b7f130316bfd82f6781652820198afecc0b92b5f9d92d6028ea866252e761" => :el_capitan
     sha256 "648e60acdbbe15d1abfccbdb8e34656cea044036eddbcc61e081eee9ccac245b" => :yosemite
     sha256 "6356e30f6be759bdb0234b811ef83069d36fdef29f5f3cf618a9547773672918" => :mavericks
