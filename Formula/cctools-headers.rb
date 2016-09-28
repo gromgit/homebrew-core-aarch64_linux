374286eefb202a803216dc1e3c14cc35fccfee33
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 21:06:50 +0100

    cctools-headers: update 855 bottle.

--- Formula/cctools-headers.rb
+++ Formula/cctools-headers.rb
@@ -7,6 +7,7 @@ class CctoolsHeaders < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b30ece09a0ea68969de159ea1004f8bf7b764e0d1930acb199331c915494c8a7" => :sierra
     sha256 "ca835a4d93f50715875bc1ab323630f788e64a0573ac994a7e5d60c9b064268d" => :el_capitan
     sha256 "a8ada963317cade3c7ed5df84fdcd3251a8b31a0bb4835a78ae4375ee0624b4e" => :yosemite
     sha256 "ac7f685067262f3b1c4f843cab7ed4c83fc58a63bb57a8c5416428db0d6c7ddd" => :mavericks
