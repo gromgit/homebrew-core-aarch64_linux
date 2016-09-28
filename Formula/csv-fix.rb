640966e8690a85a1f6dd7acc3bb41f0446292028
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 23:32:49 +0100

    csv-fix: update 1.6 bottle.

--- Formula/csv-fix.rb
+++ Formula/csv-fix.rb
@@ -6,6 +6,7 @@ class CsvFix < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b52224f7cd1dd228ffe751c67993f854a8a59f17d6099d40fca02374f1949d02" => :sierra
     sha256 "ba19053a978b57b6b962f8fa24d099d964ceb90cd28304e3a6c2a7fe0d3abc32" => :el_capitan
     sha256 "b8dbaf2e14e35cc4c1d7b5d04a5615377f7eeb4d9b1f25fe554b8711511c28f6" => :yosemite
     sha256 "0b86933c8e32830d5abd0f26ef83b1a60e0254da67542b695fd50ab1e3ba2e68" => :mavericks
