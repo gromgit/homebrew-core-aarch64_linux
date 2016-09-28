30d5677622df21eb9560fb93105ac7b25f83127d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 19:07:31 +0100

    bro: update 2.4.1 bottle.

--- Formula/bro.rb
+++ Formula/bro.rb
@@ -6,7 +6,8 @@ class Bro < Formula
   head "https://github.com/bro/bro.git"
 
   bottle do
-    revision 1
+    rebuild 1
+    sha256 "8a397f3731a1c7abf4126235705b82381cb55b4a70b2e354f96f00b214a5ef83" => :sierra
     sha256 "a48cb079b41fe45aad9e4acf3f9d6ef774569cfa14b970a9e205c40882147848" => :el_capitan
     sha256 "fb0a8b536d58745f837a3e5731e6c34c09dd4542ca33c523860c3c9aea6dea84" => :yosemite
     sha256 "e0aab7ebf5af8aea92fadc1df19f2ad6d65a2a1a91f62ecd4a2c2146466b989c" => :mavericks
