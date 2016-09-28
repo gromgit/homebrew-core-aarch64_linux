0f11bb75e0ec0f5177dd57a9b24c565e8379aece
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 16:18:53 +0100

    avro-c: update 1.8.1 bottle.

--- Formula/avro-c.rb
+++ Formula/avro-c.rb
@@ -6,6 +6,7 @@ class AvroC < Formula
 
   bottle do
     cellar :any
+    sha256 "baef97c134a8b14fa6cc162ad1321561ca3f1122ab95f42149e7058f2dbe722e" => :sierra
     sha256 "e572f37156e85db6e008fc394c7cb09fc5ff87c6a8a3f9f55d38f38f2253cdd9" => :el_capitan
     sha256 "c84beca0ab8defc026fccb3eecc77b26523014f64f7d377e195f8906622beba4" => :yosemite
     sha256 "ba3f6ab12df1ad13ec5aa04a038f4e4d4823c671bfb0f65b50d7015488db346e" => :mavericks
