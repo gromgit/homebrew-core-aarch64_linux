261f634cc8e870ee92cb6347d06db4a4ad845c57
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 08:46:18 +0100

    libmms: update 0.6.4 bottle.

--- Formula/libmms.rb
+++ Formula/libmms.rb
@@ -6,7 +6,8 @@ class Libmms < Formula
 
   bottle do
     cellar :any
-    revision 2
+    rebuild 2
+    sha256 "5319927f799dd20effbfc9f8bb90ebc844b39852c433bf434ab6b36c11c36417" => :sierra
     sha256 "61c4dd24598198386342dd9c700e218b6b83c82627efc781daa89acfaca96066" => :el_capitan
     sha256 "f915d916dd81ad9f767b6905e166dae07df72e70dc0c844c8011abed9f144252" => :yosemite
     sha256 "b55ae55a0d684ba1e3654eee96769d206ce0c22a4ab7bad5241eb1c51bda7778" => :mavericks
