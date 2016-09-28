ea7c9a71b53cc71a351f13c9685ee8fbd29052cd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 22:31:50 +0100

    cpputest: update 3.8 bottle.

--- Formula/cpputest.rb
+++ Formula/cpputest.rb
@@ -7,6 +7,7 @@ class Cpputest < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "47343762cc3b1b76538115940177a710897254ddf3e968c6a7161c0dd4dc505c" => :sierra
     sha256 "8c0b631188acc7fc0e6e5a8a2502871e4856990786fcb1268b32da7058304297" => :el_capitan
     sha256 "415557e97a8e876764595006b205bd9c61fce448558d22863306ca6b22a30e12" => :yosemite
     sha256 "12d974e118f21b0858c74db544604b7c984d72610a74883ffed12639965d7b99" => :mavericks
