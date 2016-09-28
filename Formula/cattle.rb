0e5383f05e47546a63ba9320de1c16b694ce217a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 20:45:05 +0100

    cattle: update 1.2.1 bottle.

--- Formula/cattle.rb
+++ Formula/cattle.rb
@@ -9,6 +9,7 @@ class Cattle < Formula
   head "https://github.com/andreabolognani/cattle.git"
 
   bottle do
+    sha256 "0c7554bcd19bbe7b416741b27f9447cebcd1ffff9081b3a3d4877a6ab1395e87" => :sierra
     sha256 "724e4f89fb7b7ea2f2f31594d036e5a8a4932d78374e1f08ab91b7d0ed2e37dc" => :el_capitan
     sha256 "7c507fd0f0e3c175ad6c3ac6656e61195512cb88d25412ca7332748ac6872d25" => :yosemite
     sha256 "93bea5bb1f99a9ec84a3ddc2bbe752cc9fd7627b6f099ac54fadc3150405749b" => :mavericks
