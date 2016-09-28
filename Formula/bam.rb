4f1e04491af2df35254192225cfdb0610d7eeefd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 17:09:26 +0100

    bam: update 0.5.0 bottle.

--- Formula/bam.rb
+++ Formula/bam.rb
@@ -7,6 +7,7 @@ class Bam < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1ed38b80dc9aaad3e9a4655cd28ad95d8f2d7392f5b5bbdb1dbd210665aa4281" => :sierra
     sha256 "bdf84408b791d7e5dca2e0f96cec0aea62f549611a8f6e1c0ea4b8dd8b03d7c0" => :el_capitan
     sha256 "1d76b73f0b46d0bb9f0ba998128abd1271f79ca5d2016703ac783a55369892e2" => :yosemite
     sha256 "505d0773336c1820057945fc016af04a1ea8b159d042725f435dd314e0f07f67" => :mavericks
