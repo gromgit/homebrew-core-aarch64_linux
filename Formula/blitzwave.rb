42461efdfe46a7db10c6399a8d31b9c66df9aec1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 19:21:49 +0100

    blitzwave: update 0.8.0 bottle.

--- Formula/blitzwave.rb
+++ Formula/blitzwave.rb
@@ -6,6 +6,7 @@ class Blitzwave < Formula
 
   bottle do
     cellar :any
+    sha256 "1722c7dfacc458ca54d05dcc06a5281bbe48935f66eaaf7374c2551ad50298a8" => :sierra
     sha256 "be9ba4deb07a468b23f430fe2f0896206b120f70e07f94d48267448c0524d3bc" => :el_capitan
     sha256 "609c85eec329a8aa988a2b026522642f41b392039936661ce428d13887dfa84d" => :yosemite
     sha256 "af7d02c7520db927c0d835992719922753e89d26588a00a2a53601a1e5aabd8b" => :mavericks
