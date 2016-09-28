a8ac400b3274535c74377b7e20d4137781abb078
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 16:18:52 +0100

    autocode: update 1.3.1 bottle.

--- Formula/autocode.rb
+++ Formula/autocode.rb
@@ -8,7 +8,8 @@ class Autocode < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "f369819b2f33327071a68455a14f66855286c7614977f06704f21c38e2df5f89" => :sierra
     sha256 "c321c73e1662332392c5949467c544e18db30849019555086ad14eeb097656d2" => :el_capitan
     sha256 "a0b7c969db9e2870e818587c7d832bbe0bb187cbc01346b85bb81a6097a9e015" => :yosemite
     sha256 "04effb5aecdd48e2a3c38435079424fd83f08dff206096f9807ff7c4ccd68b93" => :mavericks
