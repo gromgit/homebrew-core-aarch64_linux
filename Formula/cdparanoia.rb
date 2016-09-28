15dcf492eb74ee93085cdeb619c091091e218e8a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 21:56:38 +0100

    cdparanoia: update 10.2 bottle.

--- Formula/cdparanoia.rb
+++ Formula/cdparanoia.rb
@@ -6,6 +6,7 @@ class Cdparanoia < Formula
 
   bottle do
     cellar :any
+    sha256 "709190d769f7b8c61d19867ae2faf902a2f84dec6f0d5506bd71c56a99e4a67a" => :sierra
     sha256 "135250473fe692dc976ecbf7324676fa8cef3cdb48a091287bb183c31548fed9" => :el_capitan
     sha256 "3cd7bbd1a4a0a7992287b255cf0d6409bdb5f4a3fed245b0fd2296e535e9f2de" => :yosemite
     sha256 "14ec797a041edffe73fef897853a833e5588278c03511f27499e55efb68c848d" => :mavericks
