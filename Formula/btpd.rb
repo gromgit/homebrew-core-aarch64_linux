01b473191343977e18af6a797ba32c6f07657b32
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 19:07:32 +0100

    btpd: update 0.16_1 bottle.

--- Formula/btpd.rb
+++ Formula/btpd.rb
@@ -7,6 +7,7 @@ class Btpd < Formula
 
   bottle do
     cellar :any
+    sha256 "c951cbaadc8a4a04dbe95fe28fdb6f2ad760352c58fba5a36309ed3fdff69e16" => :sierra
     sha256 "4dce615bca726cf8ea3adfd0ab5e18f4f1e3d95d1a4d98ce38d6f894b1206a25" => :el_capitan
     sha256 "382f8e3ec6e514f5a5116b562c82e0f3b2ae786b625ba08ab222fe7db9a4bcff" => :yosemite
     sha256 "62a5bcf9db33b7b543053ce0a7d6ce4ed1fdfc43c9fca2500adc289c8bf34dc8" => :mavericks
