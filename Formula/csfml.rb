ad65b5ecaa082df08d4d579272a3df7a68739892
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 23:07:55 +0100

    csfml: update 2.3_1 bottle.

--- Formula/csfml.rb
+++ Formula/csfml.rb
@@ -10,6 +10,7 @@ class Csfml < Formula
 
   bottle do
     cellar :any
+    sha256 "89a50b1ce1bd73ed5039f7739a6872decadead365a2cc44289a1e0a2a223284a" => :sierra
     sha256 "5cbe3b961dac61fc0e7956edbee6ba799c3f8e5511ff1817f38494ff4acccf9d" => :el_capitan
     sha256 "076fff54696922762c7256979fc2c9859348709c155ff110bf41c08b14e87b1b" => :yosemite
     sha256 "b46151808b6ec8439fb8a69dfe35109edd30f6ec5cb0bd89337deddc89c5064d" => :mavericks
