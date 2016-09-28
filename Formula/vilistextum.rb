8236a8db217107342839a7940ad28645de948c29
Zhiming Wang <zmwangx@gmail.com>
2016-09-27 21:33:46 -0400

    vilistextum: downgrade to http (#5287)
    Cert expired months ago.

--- Formula/vilistextum.rb
+++ Formula/vilistextum.rb
@@ -1,7 +1,7 @@
 class Vilistextum < Formula
   desc "HTML to text converter"
-  homepage "https://bhaak.net/vilistextum/"
-  url "https://bhaak.net/vilistextum/vilistextum-2.6.9.tar.gz"
+  homepage "http://bhaak.net/vilistextum/"
+  url "http://bhaak.net/vilistextum/vilistextum-2.6.9.tar.gz"
   sha256 "3a16b4d70bfb144e044a8d584f091b0f9204d86a716997540190100c20aaf88d"
 
   bottle do
