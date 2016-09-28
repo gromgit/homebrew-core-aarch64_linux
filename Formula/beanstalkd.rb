32d2e1b07c1237c4d338a31d765a5111542ab3a9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 17:31:42 +0100

    beanstalkd: update 1.10 bottle.

--- Formula/beanstalkd.rb
+++ Formula/beanstalkd.rb
@@ -6,6 +6,7 @@ class Beanstalkd < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "95a75ad2e7f06dfff9881762bddeb1fb06319a411165cba0114e4b7c9b1a4103" => :sierra
     sha256 "6665ec5a9a493341134eca920517547340b672513f96317620c5095db3db9499" => :el_capitan
     sha256 "1772e0af60fe42a471437285f29a9c03b52cd8ddd805eaf3339a9d644c4d1bb5" => :yosemite
     sha256 "3d0c54d751784dd7ecfe1b482f30067ade6e7f99ec21d1e87e8f850fe2582f37" => :mavericks
