89991015f2ca8ed63d61cea0a4c73f2bf800f765
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 19:42:25 +0100

    bwctl: update 1.5.4 bottle.

--- Formula/bwctl.rb
+++ Formula/bwctl.rb
@@ -6,6 +6,7 @@ class Bwctl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "23f32c2e2b5203c4b4fecfb33e2152e5d732305e60ef5b02e4bbf1a9f7355c73" => :sierra
     sha256 "ac9e615919ebd84515022f9650f42194d9ad4b1c1f5f97509e1293962a96e943" => :el_capitan
     sha256 "c8890647536e60b3ed8599eb3239ee59fde0382e9df8b7585ee7eeb20275fc39" => :yosemite
     sha256 "f10efbf8f41f526130340cc6087ce3dfad83b71b69d21e0b01c11b3169d88bdd" => :mavericks
