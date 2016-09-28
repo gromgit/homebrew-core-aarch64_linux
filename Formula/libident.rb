69dd54271155ce50e2699b79860ac389eb8a3237
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 09:12:37 +0100

    libident: update 0.32 bottle.

--- Formula/libident.rb
+++ Formula/libident.rb
@@ -6,7 +6,8 @@ class Libident < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "4fb8a991f9f83d499b32a814b2d68465327b7b77c0108764e58e4296a968100f" => :sierra
     sha256 "6236d3b4ee424795859cc64da30997ff67f7ac5bd42702e8eabe10f99339ca48" => :el_capitan
     sha256 "53db8e889d8efa34b4a3b6a145bcec2bcb53595e7db0cfdd55c8d857dff3a442" => :yosemite
     sha256 "d8e20cc9d2015b83785d4eb05110afc082a6389267df82546cc89e693dd2db6a" => :mavericks
