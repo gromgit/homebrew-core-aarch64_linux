23a8afc2c66912b6bf81be568f5f14b88ec7a8ff
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 07:24:21 +0100

    picocom: update 2.1 bottle.

--- Formula/picocom.rb
+++ Formula/picocom.rb
@@ -6,6 +6,7 @@ class Picocom < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f1f3456a8175fbe77696b34511c225c02bb426d80d2c63d065072b3eb16f972c" => :sierra
     sha256 "f45b55ceb211413c32d769a45db1f0b5588143a183b570509502f7c3c8fc6770" => :el_capitan
     sha256 "b4a7c54a0333c78b186cbcdccecc44676967f5284d11695010c182074deed443" => :yosemite
     sha256 "c7a9665b8ea06236a251be26a945f47d8720e302dc004306ab95c7e19abf9079" => :mavericks
