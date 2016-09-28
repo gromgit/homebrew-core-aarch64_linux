0d99ed80c6df5d27b323d6514a1b3b3c1448459c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 16:18:52 +0100

    avian: update 1.2.0 bottle.

--- Formula/avian.rb
+++ Formula/avian.rb
@@ -7,7 +7,8 @@ class Avian < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "ef092ec60093190857b558fd9a663ca0c6d0356bb9af7798be21cf466678f27e" => :sierra
     sha256 "d2719509725f4c1fad3a53c32de18aff5d45685fb35ae352f1d51fc61e566f4a" => :el_capitan
     sha256 "d002876c03742fc7ec4157fff598e7c11ed1e62f97ce1b217f8b089db87e43ed" => :yosemite
     sha256 "20dd7125d138e05021b473d026190d8f4652e807afcfe057614e5c2e66ce0ed1" => :mavericks
