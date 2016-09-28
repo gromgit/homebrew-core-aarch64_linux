3892358b60ea65107d2a8ab705cbbf2fcd3db584
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 17:31:43 +0100

    befunge93: update 2.23-2015.0101 bottle.

--- Formula/befunge93.rb
+++ Formula/befunge93.rb
@@ -8,6 +8,7 @@ class Befunge93 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0b2b344570f71b3fa702675b2305b96632fde0a8da44085a15b15fe72863f66a" => :sierra
     sha256 "fcebeb62391bd6e3eef571123af573766666ce9c40f139c889cc350bf6410d8b" => :el_capitan
     sha256 "825c5d86e93d7cf0ecc2f3f16f626c27e658f1d4792bd6e74092b11f815097d7" => :yosemite
     sha256 "a4f6102ac80c19ef969e7b2bbe70bdfd4f192df08d455b2b6162ce16e3616564" => :mavericks
