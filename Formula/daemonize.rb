e5bf984ebc3df775e9f0ca82d2404104610ae5cd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 00:27:39 +0100

    daemonize: update 1.7.7 bottle.

--- Formula/daemonize.rb
+++ Formula/daemonize.rb
@@ -6,6 +6,7 @@ class Daemonize < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "50c47ba9d070c10807b1861688082c2a242f0f2df6f6fd021da9e061fabc8806" => :sierra
     sha256 "cc17945de7476011059bcb7541776a02db3db45566af2ef86780b4d468fe1d14" => :el_capitan
     sha256 "badaed3738790f69926bbc9cc377b9d4a12c1972e004261cf492a9460cd9d3ca" => :yosemite
     sha256 "a05ef16ac15480ea28e235288ba025a75a7017e03ab0df48b08cceb255416032" => :mavericks
