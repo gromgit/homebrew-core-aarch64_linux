218c8971f2bee50fd88c2c30c40330372647e77c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 17:09:27 +0100

    bbcolors: update 1.0.1 bottle.

--- Formula/bbcolors.rb
+++ Formula/bbcolors.rb
@@ -6,6 +6,7 @@ class Bbcolors < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "cdc81c86b829ba9e051d693bccdb821ed78a8dc3a5df644fc156bfcd700d5686" => :sierra
     sha256 "2a713dae009e44685d1ef02b01d5202a24087129dab70366d2e30800b7dfb9cb" => :el_capitan
     sha256 "506d7f82fa38e1f694550be30a29554b8ecc8b303d47e9bb4fcadfc534ac55c7" => :yosemite
     sha256 "68b63b5913be9e20b8ebc726c5272e030f7572aeb6baab709a70725f632c69b1" => :mavericks
