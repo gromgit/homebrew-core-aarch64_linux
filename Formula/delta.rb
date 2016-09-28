4e68a68dd2ab81915b82437688f86a7bf400d006
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 14:03:04 +0100

    delta: update 2006.08.03 bottle.

--- Formula/delta.rb
+++ Formula/delta.rb
@@ -7,7 +7,8 @@ class Delta < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 2
+    rebuild 2
+    sha256 "07e775a1054966ad2924512386643bc8cb4ef3ad7e12ce9a140015c82fba3072" => :sierra
     sha256 "202409012500969cfd034c9d44c441a809445a3b367d514357346438aa850f14" => :el_capitan
     sha256 "d3374cc3e84c93bb84615b1669503ea8b708ab65baf629ee0be9a728b12b10bc" => :yosemite
     sha256 "04102ae55ffc2cc4351816b010544b854c21f1c5e2a462a6af0e57ec2f57b501" => :mavericks
