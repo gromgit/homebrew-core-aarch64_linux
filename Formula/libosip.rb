cfcad013b4731bf65504c634cce7419a5c436b6b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 08:20:56 +0100

    libosip: update 4.1.0 bottle.

--- Formula/libosip.rb
+++ Formula/libosip.rb
@@ -7,7 +7,8 @@ class Libosip < Formula
 
   bottle do
     cellar :any
-    revision 2
+    rebuild 2
+    sha256 "62a51e2ea7a623ab89b628d0a5655afe390dbeab86f5d86dd799c046e6767e77" => :sierra
     sha256 "f55f17a9fcefb3780ec0ef62dcf65e46606e6a76f86e37bbbb1a70b9de12e1b1" => :el_capitan
     sha256 "87803e02c0c3b65c8f028864200425f90b5a708bb6204a410f6c76a9e35545ee" => :yosemite
     sha256 "531e7d5fb51ec0ccdc05b3e3346710770f756fa8b3eb7eb2cbbbe5b2cb1c8d59" => :mavericks
