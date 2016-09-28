6c19c99760965a40b3f78198d84dfaa4fe70ad0a
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2016-09-28 06:33:25 +0100

    librasterlite: revision for proj
    Closes #5289.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/librasterlite.rb
+++ Formula/librasterlite.rb
@@ -3,7 +3,7 @@ class Librasterlite < Formula
   homepage "https://www.gaia-gis.it/fossil/librasterlite/index"
   url "https://www.gaia-gis.it/gaia-sins/librasterlite-sources/librasterlite-1.1g.tar.gz"
   sha256 "0a8dceb75f8dec2b7bd678266e0ffd5210d7c33e3d01b247e9e92fa730eebcb3"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
