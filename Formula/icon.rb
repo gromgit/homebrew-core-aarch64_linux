a007d8bc37622c737c51ee12966112128bd0519f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 10:45:34 +0100

    icon: update 9.5.1 bottle.

--- Formula/icon.rb
+++ Formula/icon.rb
@@ -7,6 +7,7 @@ class Icon < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "13d3963ef90d3f94f13a97e922185ea640233aee356e3bf8c2a0336de278482c" => :sierra
     sha256 "5218afb915b7892d4c242c659218735293136c3b100f54aa7199bcc716915939" => :el_capitan
     sha256 "44450b176b56db833a91ca6ae681e3876b2864a094b254340bcb5cd136957f17" => :yosemite
     sha256 "ca5ba233b4713e54680525ffd3ee7554988aa48f6a959f78b53c24e58d8c1c59" => :mavericks
