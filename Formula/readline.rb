153594aca5734cbcc9aebe2fec1408a5ff061c9c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 23:34:10 +0100

    readline: update 7.0 bottle.

--- Formula/readline.rb
+++ Formula/readline.rb
@@ -7,12 +7,9 @@ class Readline < Formula
 
   bottle do
     cellar :any
-    sha256 "11f2ad09f5eb9e055b2567c4308f35cc8e006b9ffdfe49599e4c4d92a0a3a066" => :sierra
-    sha256 "9d38481c935cef21ead25c294285b78a9e8fa556fd15ede9126926c055c40d37" => :el_capitan
-    sha256 "d0cdb3e162c05e21c7184e86629f04685d4be98e38cbf024373142bf8764500e" => :yosemite
-    sha256 "adcc6352d3fb271a1e9ce034d80996405a5c46afbd1bab1507d3ff5e89e02bc1" => :mavericks
-    sha256 "6b1d7e806e169e2b778371d74e7cc62106c348ea9f5c80fd658b763b94dc748e" => :mountain_lion
-    sha256 "c129333634dd00ab2267ae9c531fca1f5cc50dd519ed3399918289fdfdf2663b" => :lion
+    sha256 "9bc3671fa05a042e8de11d5fd7769e45f5bfa674d4cb48c52720c7a3e7a521c6" => :sierra
+    sha256 "35451200004c16ac49b5a990f9c0ed88a29eea8627c26db4ddf48020c9a73ec2" => :el_capitan
+    sha256 "492648ce1c152c73c32c625926cc7ea6c9c56333a2dfa1f2e235d83ca3d9ee10" => :yosemite
   end
 
   keg_only :shadowed_by_osx, <<-EOS.undent
