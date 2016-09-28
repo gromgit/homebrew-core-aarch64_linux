4d70d7a6ca50ed1d3d73ae626ebd16c27c0bdcc5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 16:18:52 +0100

    autopano-sift-c: update 2.5.1 bottle.

--- Formula/autopano-sift-c.rb
+++ Formula/autopano-sift-c.rb
@@ -6,6 +6,7 @@ class AutopanoSiftC < Formula
 
   bottle do
     cellar :any
+    sha256 "252749876209b696ff1dfecc2d6eddef263ea06491c3cb640f26cd0a63a4abab" => :sierra
     sha256 "325d74775797dfe0cdc0c6b6e0255166fe7573b4648c20758adf1386c2c85991" => :el_capitan
     sha256 "929174dc5dd8b519136154249df83e026ac9c234988a979a592d03c47477988b" => :yosemite
     sha256 "5a777245c630a0d91178d347f3e452b7d9784c7d7ff15f9fa4ed12d9bdc2830c" => :mavericks
