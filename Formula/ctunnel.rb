e9df2e835f73cceb182787575ae1dd7c0060bff6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 23:32:50 +0100

    ctunnel: update 0.7_1 bottle.

--- Formula/ctunnel.rb
+++ Formula/ctunnel.rb
@@ -7,6 +7,7 @@ class Ctunnel < Formula
 
   bottle do
     cellar :any
+    sha256 "dfcf3349529b7b2ab47298a9090d916be9eb3b9c0d72cd644f0e4b1de09796be" => :sierra
     sha256 "6a5de1059ec2e138e68530fd62f51b102e29d6ecfb83eaa22e3f79bc0d9acf4c" => :el_capitan
     sha256 "70736b739ab67933916bff1ce0d4ea9105c0faef9576646a5f7abf5e80f2b9b3" => :yosemite
     sha256 "3a442229dba723b14300c711e84b3dd0740b1b0488c6d4b59121193cba7195c7" => :mavericks
