230b86c122122a833bfecb771efc75c300e75ad6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 06:49:27 +0100

    taktuk: update 3.7.5 bottle.

--- Formula/taktuk.rb
+++ Formula/taktuk.rb
@@ -6,7 +6,8 @@ class Taktuk < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "236b1b7277a6ff6e33bab7818cb779f32f1415e3e51e4edda6f243499328d1e5" => :sierra
     sha256 "b5f260c944e09210f94a3b215112ed2975bf9c4a93ad1fdd30a627700e48a364" => :el_capitan
     sha256 "4f703e2c8fb0f1b5c4c8b19b6a42e3a14023b40d6c511a10e0d460b8810d629e" => :yosemite
     sha256 "b0ca7976fb797a3d74c4e97d26214f6b7fdd6cf6764cd9fc3d0f2b3931479bd5" => :mavericks
