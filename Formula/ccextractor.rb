cc680930476a8033c2ef5cab42742e9c9f5bf9b9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 20:45:05 +0100

    ccextractor: update 0.82 bottle.

--- Formula/ccextractor.rb
+++ Formula/ccextractor.rb
@@ -7,6 +7,7 @@ class Ccextractor < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e9b4e38e0b81d257841d64eb8aeec6e3015d1ad7ac9afe3d8b797f969109960d" => :sierra
     sha256 "358c85300067eb0ae9a7b09d2325c24103ecbdf73f2d07c8e76baaeac5743931" => :el_capitan
     sha256 "e5dfe6c34730149839a16860fb012776633dc0021562d26a6f06a5825116ee3c" => :yosemite
     sha256 "b513081333efed3bf23e133bd062c18507b587a627379693a14372c45b966d6b" => :mavericks
