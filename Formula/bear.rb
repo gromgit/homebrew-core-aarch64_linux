fbd2643195a29a7ed5847a0ea709d2e99e6e392c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 17:31:43 +0100

    bear: update 2.2.0 bottle.

--- Formula/bear.rb
+++ Formula/bear.rb
@@ -8,6 +8,7 @@ class Bear < Formula
 
   bottle do
     cellar :any
+    sha256 "8469bf0b4ae18208304134787c9ff6938b191f1f4ecf5e88c516c228ad0eeff2" => :sierra
     sha256 "316e61b4a0198e4627e18491e52422df67d7fb23cd72ad01b6d8b2e7d6682e34" => :el_capitan
     sha256 "5e71d4bfae579a1eac79692f0558d2541d1d9b46ad8bafdbaa303ef94f716509" => :yosemite
     sha256 "570c03e31526cfbfc2b7d120858cffeb5f04f00a8d96730aa79732714b0c92f4" => :mavericks
