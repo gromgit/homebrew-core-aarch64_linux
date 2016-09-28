a3b3d3793d32161f6031459474fc189f84ece796
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 09:12:37 +0100

    libestr: update 0.1.10 bottle.

--- Formula/libestr.rb
+++ Formula/libestr.rb
@@ -6,6 +6,7 @@ class Libestr < Formula
 
   bottle do
     cellar :any
+    sha256 "180b2620f9ba546d062835b15753236a616ecd53fa01bacb07bf6368d4825b64" => :sierra
     sha256 "a95f0ed48b7a7879128ea8b7d1dfac5d6c736ee504d88a65155c040eff6ea134" => :el_capitan
     sha256 "b6378b804c98be7a85e665f7f98035446941720f2ae9de94cd1cfedd607a5e10" => :yosemite
     sha256 "5215ffe64cf57a7c95561588e8e117983419fece70fbc3c61d26099a249cf098" => :mavericks
