8164a8be33722257974bc9f69628a8b3a3119a11
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 10:45:35 +0100

    itpp: update 4.3.1 bottle.

--- Formula/itpp.rb
+++ Formula/itpp.rb
@@ -7,6 +7,7 @@ class Itpp < Formula
 
   bottle do
     cellar :any
+    sha256 "168b61ba02bd54e625ccbcac998595dc2e96a4210a0709a7b7296a8ed9b985b5" => :sierra
     sha256 "c9f2f041dbdbea87029180a253fa0cd470aee6a2144a848b10f9fb5c40f01b8e" => :el_capitan
     sha256 "6e772c61104760fcf2c9500fa06fb2d032d4db1f53ef44d090e18aca54ee75f0" => :yosemite
     sha256 "c9e5ed1ab5febb67c61139b451ac70c501e6dd8fe656f7658d9e6aaa3f025e2a" => :mavericks
