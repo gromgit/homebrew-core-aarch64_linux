7da4142ce235ef0fcf9263d907c199f89862c3c4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 07:55:35 +0100

    mktorrent: update 1.0_1 bottle.

--- Formula/mktorrent.rb
+++ Formula/mktorrent.rb
@@ -7,6 +7,7 @@ class Mktorrent < Formula
 
   bottle do
     cellar :any
+    sha256 "d465e987b90707240ee59118fb719fef082a16261924449309a009abb6b6dae4" => :sierra
     sha256 "4e1fbfdfd5bcd1f4ae28e07f2955eb34e785a72b5e93ef23ac5ae733b4f36a22" => :el_capitan
     sha256 "afd5e147727bc83fcde127e06f244513708045a295976d6571c23ee704f618f0" => :yosemite
     sha256 "79d448b9d2272a350d423668675a6b1504302505ae94af99f2a780efd0b82958" => :mavericks
