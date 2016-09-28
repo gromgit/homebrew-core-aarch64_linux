911859d859ceb8c7d70f0bcd7b9b7c6817b59658
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 06:49:27 +0100

    scrollkeeper: update 0.3.14_1 bottle.

--- Formula/scrollkeeper.rb
+++ Formula/scrollkeeper.rb
@@ -6,6 +6,7 @@ class Scrollkeeper < Formula
   revision 1
 
   bottle do
+    sha256 "d1722082c68c3a2910fafd049b8f469a91e29586798b1b7ffc6cdb5b4e1b8f2d" => :sierra
     sha256 "fe0171c12bd61b59032a0914fd2279ca91132a714993d2dddd0f2641d8cb8142" => :el_capitan
     sha256 "ca1090d4daf705fc9bddc18b303ed1bf511ebd1173bcb48b17f5e47daab74351" => :yosemite
     sha256 "7e302cb0c50b34bf47cceefa2c79a8b565b06e7d850ea07f10992598c2058773" => :mavericks
