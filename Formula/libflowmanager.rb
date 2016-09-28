a7fffe29cf968d698a3a9ca59e1f869fd98d0398
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 09:12:37 +0100

    libflowmanager: update 2.0.5 bottle.

--- Formula/libflowmanager.rb
+++ Formula/libflowmanager.rb
@@ -6,6 +6,7 @@ class Libflowmanager < Formula
 
   bottle do
     cellar :any
+    sha256 "b71b99087cdddbd75b4cc932c52f3f5ed826ae019863a8d512570a847bc28e0f" => :sierra
     sha256 "aa68f7f79671aca71837840606be729da65803d56904a2712034a1ddf44516c5" => :el_capitan
     sha256 "cc62e753f86b63f2f0d547eb23b364d8f7df3d5b67d9826f53b7233ed3a64538" => :yosemite
     sha256 "f28480a77bb774599e0eee240195b1b6543c04f23912c666c593da6e51aa4316" => :mavericks
