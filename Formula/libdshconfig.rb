7e495ca37a52838e0e77a8abbe1f1d870dd98377
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 09:12:36 +0100

    libdshconfig: update 0.20.13 bottle.

--- Formula/libdshconfig.rb
+++ Formula/libdshconfig.rb
@@ -6,7 +6,8 @@ class Libdshconfig < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "82fc9db7c3ad20bdcd5681be1075ae4853b6f19caeb41624dac33d53470b2523" => :sierra
     sha256 "a26ea1d1cefed24fd890bbc65f9a11d171cdbcb1c00936562255e2adfe29205f" => :el_capitan
     sha256 "7f4e5e77fc14d6920bd212e9c812c7dad51ad1dbe1542f1c0e4999928db9ad3f" => :yosemite
     sha256 "c1c35337d7b2bebb59d07a8f792493f1da89f2701eae58d382bb24edfd2c73d3" => :mavericks
