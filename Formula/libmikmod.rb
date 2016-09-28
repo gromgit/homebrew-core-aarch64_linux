503fdfdd54e5ac820ed031df89a292b39fd87189
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 08:46:18 +0100

    libmikmod: update 3.3.8 bottle.

--- Formula/libmikmod.rb
+++ Formula/libmikmod.rb
@@ -6,7 +6,8 @@ class Libmikmod < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "ec5f612dffeab778e8aaa78478a2157291b9e4a9f0601344123d5d421692023e" => :sierra
     sha256 "d4ab73d546746bec04cd6b46458457d5c8fb35244acc8289deaba2d27ea4d1aa" => :el_capitan
     sha256 "aa439fc42772f79091390244f084476def518280ba53699b8d540cfefa311778" => :yosemite
     sha256 "3bc07cf9d3295a6888ada8b508834246d26981e8f2935ac2d9847f7038c3f8c3" => :mavericks
