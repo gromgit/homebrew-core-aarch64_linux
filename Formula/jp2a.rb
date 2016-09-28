a9087da0aca4d7ee7b80b289749cc5da5c1802c6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 09:52:35 +0100

    jp2a: update 1.0.6 bottle.

--- Formula/jp2a.rb
+++ Formula/jp2a.rb
@@ -6,6 +6,7 @@ class Jp2a < Formula
 
   bottle do
     cellar :any
+    sha256 "34f358dbc4335ba01acb74b65a2c12965af1317b2d46d5815bf6aadc299c98bc" => :sierra
     sha256 "d1c7db27a0b2704c0f5f3584fe7891e40ac1d26b175a85cbf8899c139330d4bc" => :el_capitan
     sha256 "1a16fd055a4c68ef949df2f37f690259860d0c3d97962fc1b0bc5b8654f1b442" => :yosemite
     sha256 "b31b7564584f3e91dc73c4f511d74c26341d621a5b6e0533541237400acfca84" => :mavericks
