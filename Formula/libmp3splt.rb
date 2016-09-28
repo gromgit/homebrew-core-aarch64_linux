dbafda8fcae2116bd91af1b82b09722785f7c88f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 08:46:19 +0100

    libmp3splt: update 0.9.2 bottle.

--- Formula/libmp3splt.rb
+++ Formula/libmp3splt.rb
@@ -5,6 +5,7 @@ class Libmp3splt < Formula
   sha256 "30eed64fce58cb379b7cc6a0d8e545579cb99d0f0f31eb00b9acc8aaa1b035dc"
 
   bottle do
+    sha256 "e513672ee4682c1f722e710d4446fc2721325875acb65ccaaee0e5de113cb82b" => :sierra
     sha256 "587226a840b162aeef70cc8022bdbcd61218e1be6dd1b98418774f3f48405072" => :el_capitan
     sha256 "47d3aaeee6d237273e8457666cd2717e1264742ae776d541c40a203e1b82003f" => :yosemite
     sha256 "0bac13f95cb16925fe28cd1d662bec10a66c93bf9b27c2c9533ab38b7a1f38a2" => :mavericks
