8a748dcb26fbf3f54c5e97ff0d10d3f1a61de5ba
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 18:42:34 +0100

    bmon: update 3.9_1 bottle.

--- Formula/bmon.rb
+++ Formula/bmon.rb
@@ -6,6 +6,7 @@ class Bmon < Formula
   revision 1
 
   bottle do
+    sha256 "5764df478a5fd162c212b989aa22060d94100ec8becda51493adeb2215479fed" => :sierra
     sha256 "6673c478d4f7558bab1ce6f1939d24bc8b88f81f3f31a92cf447317f8143cdf0" => :el_capitan
     sha256 "e70015fe4b38f8c75783ac2d3a6f828d4a6d20ef7fbbb95378483fa2a22af7d4" => :yosemite
     sha256 "dba21c9ad3060c78df2d11b71b6a23dd1a7dc609648fd0562cf59e08f45a7884" => :mavericks
