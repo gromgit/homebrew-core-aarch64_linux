35cfcc9531eb0960ef646cdac61dae64e8cada58
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 05:41:38 +0100

    ykpers: update 1.17.3 bottle.

--- Formula/ykpers.rb
+++ Formula/ykpers.rb
@@ -6,6 +6,7 @@ class Ykpers < Formula
 
   bottle do
     cellar :any
+    sha256 "caa9b3e94726845cb1d1d972a822e105bf7092d9ddf5bcda90e33f14039eaf4b" => :sierra
     sha256 "61d466fe3b295a6b4a6a1642dc8f1a393e28a6e9705260cf4107e8ad62a8bcab" => :el_capitan
     sha256 "b3b65497b0b754c40a4b72ba607f9cc8149400dc2b4a89debc9a752f8add51a9" => :yosemite
     sha256 "708fcea4a38578c8dabe046c07d8705bb8d1874c8b389c46e2edd6bc4f8b1d5c" => :mavericks
