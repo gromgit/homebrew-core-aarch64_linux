0db1b29e940c1d5660e886fd050bfd087180488d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-28 19:21:48 +0100

    bitrise: update 1.4.0 bottle.

--- Formula/bitrise.rb
+++ Formula/bitrise.rb
@@ -6,6 +6,7 @@ class Bitrise < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "184a953b39384b566a700b9de385b46ea59c5135f313a28d765a66d0e74fd832" => :sierra
     sha256 "81aa3e7dd7e7b59a2d3f738988b5b542215cf8c2d3c3f957ec3817e83c4713e5" => :el_capitan
     sha256 "4348bfc455b065af672e29c542bde6769f3938f7ff2426e0521a34b4d02622e7" => :yosemite
     sha256 "610f56df3975d3c0c1408c90992cfc42247ddd968ca0bd19592fd848a5fd06bf" => :mavericks
