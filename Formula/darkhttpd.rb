02949149f45a0a4a4977fd4bcdc6870605f71cf1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 01:01:15 +0100

    darkhttpd: update 1.12 bottle.

--- Formula/darkhttpd.rb
+++ Formula/darkhttpd.rb
@@ -6,6 +6,7 @@ class Darkhttpd < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1271ecbcd73b02bc8977235ea0bfcfdceb1819cb476213e74bd7d352df8a464f" => :sierra
     sha256 "cf8e5885072baed885238dc1a6b23466f80d96a32eb48d5f61f3b9d519df88b5" => :el_capitan
     sha256 "2fc16040a837b47ac947b8462f93530a387f9db0e0d6a594e4b7dba3437e6e11" => :yosemite
     sha256 "0ac0b5be3f8e944981806ed255740a6feaee64cd14d78d817e8c5a75391d9837" => :mavericks
