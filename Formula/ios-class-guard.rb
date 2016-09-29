2604e8ed784ce81489cb4fa48fa7d46bc1a70a22
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 18:06:25 +0100

    ios-class-guard: update 0.8 bottle.

--- Formula/ios-class-guard.rb
+++ Formula/ios-class-guard.rb
@@ -7,6 +7,7 @@ class IosClassGuard < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a7843a0767e916aa6be1509a984eb698bb54d125d06ad762fd25f4a3d6a55db1" => :sierra
     sha256 "0bb9abaac82cbc4e66a12493548659197559a01a779db6ceda4cf6c4439ea0bb" => :el_capitan
     sha256 "4cada6d32bb82fbd8ad2afa58b7041bd5da12dc5d9fceab6277eec97459a2d33" => :yosemite
     sha256 "4eddde784c843628cb8bcb8c971142683c5a17373058f5bda62356b432dec00a" => :mavericks
