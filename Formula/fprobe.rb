87b8aefafb6ddde54796f2f3657e643bd8be6602
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 08:24:00 +0100

    fprobe: update 1.1 bottle.

--- Formula/fprobe.rb
+++ Formula/fprobe.rb
@@ -6,6 +6,7 @@ class Fprobe < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "fe38758d956c43b2a223734d426c990c63ee44ac643dc769c3d6a0cd4f07ef6b" => :sierra
     sha256 "9b06507a358024842b59c9f4d637b94b3681e720dbd3a1a8bc93d4d34f9a4442" => :el_capitan
     sha256 "18043cf3fcc930ee1690ee4bc74d92eed3c56a2424f85d58720c56a4b5bcad1d" => :yosemite
     sha256 "71b149edc078b237aecde23a53d76dc9809e3c9efafb32b52c03f3cc4af91c36" => :mavericks
