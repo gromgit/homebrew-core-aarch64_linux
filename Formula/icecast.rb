5932d19e804d279ea3b60fe063f2d2b3a61945b0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 17:12:47 +0100

    icecast: update 2.4.2 bottle.

--- Formula/icecast.rb
+++ Formula/icecast.rb
@@ -6,7 +6,8 @@ class Icecast < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "27da9615fa078987b9b52d254b2d2edd43f37fb64f7f3ba958740e7890078494" => :sierra
     sha256 "5b80c269400e3f3acd3c46cfc9e771760f9e51128f515f4a96a7a1917d48656e" => :el_capitan
     sha256 "037a9845bedfc65b44bae252a9529771944d3de47f291ca8b1fca01eab9043f2" => :yosemite
     sha256 "0e00235cc643715c86a710ab12c31470e23b6304740ef942406db04b41cdf671" => :mavericks
