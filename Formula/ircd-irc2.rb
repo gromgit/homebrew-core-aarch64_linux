1bcf85afe861899140fb033a38238f339d0fa89a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 18:55:58 +0100

    ircd-irc2: update 2.11.2p3 bottle.

--- Formula/ircd-irc2.rb
+++ Formula/ircd-irc2.rb
@@ -6,6 +6,7 @@ class IrcdIrc2 < Formula
   version "2.11.2p3"
 
   bottle do
+    sha256 "72b85345931772dc3ac1fe96201906db0c70c24e129e9ee7006253080926bd2f" => :sierra
     sha256 "259ddceb29a5d5e0705c3b0a130368053de98282ecec2036c17d30062bd6f9f4" => :el_capitan
     sha256 "af6c845d852e4a525d64f1cfbd551377c90da201c2ef3e521d48fc1513a58064" => :yosemite
     sha256 "9fd885d98218c6e570f16b238cb72546130f5ca1bbe2e06f260b7a672dba02e2" => :mavericks
