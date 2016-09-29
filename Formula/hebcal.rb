5ad942d114e310f4af6c57700098e18ee8319c2c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 14:25:45 +0100

    hebcal: update 4.4 bottle.

--- Formula/hebcal.rb
+++ Formula/hebcal.rb
@@ -6,6 +6,7 @@ class Hebcal < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "24249ae917adfd74ec1c1e67ef870329ebfa1b77c7ad3c3c2e58358b0990074b" => :sierra
     sha256 "2e06bf24383b1ffb0057958a495ad63c56046e86abd8eea9a4efe0a90d762b45" => :el_capitan
     sha256 "24e55910c7c1bbcb075196fca25fdc1a06abd5e16df9df536435e934b138dbb2" => :yosemite
     sha256 "00c5f478340499a3aa2f5dc0855921ed91441d3c255dfd5d0975dc53f320b4db" => :mavericks
