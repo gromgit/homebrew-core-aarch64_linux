21c1f3a2cc31fed6a1e0d3feb84910b72cdc21fa
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 18:06:25 +0100

    ioping: update 0.9 bottle.

--- Formula/ioping.rb
+++ Formula/ioping.rb
@@ -8,6 +8,7 @@ class Ioping < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4b686bc05884339642374568ca3cdab6cdb0bb10513c81e91ce44595150fb24b" => :sierra
     sha256 "20ced9a086650e90c0bf5d1fe475833e9f6e42036997381c4f41156d09b7f300" => :el_capitan
     sha256 "3e41312ac455f0901b365b8c60de6c55869cea417ce57960ed9affb9721690d0" => :yosemite
     sha256 "e0c9fd5902a4147e866f38c4d4123f9133ad7cfbe10ab1f9d2e78a887ee0603a" => :mavericks
