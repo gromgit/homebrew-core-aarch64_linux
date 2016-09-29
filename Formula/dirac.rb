6840288fb29bbd2fcaea02542d2fcf77b9dc6b17
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 03:09:20 +0100

    dirac: update 1.0.2 bottle.

--- Formula/dirac.rb
+++ Formula/dirac.rb
@@ -8,7 +8,8 @@ class Dirac < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "09b846fe4069e971ec6d10668d97ac599cb555e5799f3ba3076d0d088e1f78cf" => :sierra
     sha256 "8f4414614755f863d3ba0f43d6415684fbc00976ae24c7e45c88fe736be918d2" => :el_capitan
     sha256 "1d3049d9dcdbd0116c65c54582601b20cdd17c8b89cf80e74efc79f71b641ca4" => :yosemite
     sha256 "e7c407545085631c27c77f2d15abe84b3cc0a3645cf5e538aa15f0aacfe6de50" => :mavericks
