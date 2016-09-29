58408625346515f288e59186f1ecf380831737bc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 17:12:48 +0100

    id3tool: update 1.2a bottle.

--- Formula/id3tool.rb
+++ Formula/id3tool.rb
@@ -6,6 +6,7 @@ class Id3tool < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2b7bcf184092881638221c606ae50879d02478bf9d433dc946f63dbe2e68368a" => :sierra
     sha256 "b5b4d49b4710a67df5200149873dd8c17131ef4a4eaac722a2095b1445d053ff" => :el_capitan
     sha256 "348a229d26dd699013d8e3372c5382da10b12d02d286304e61d9f2c71ed77101" => :yosemite
     sha256 "dd989abacf0c1113879b6864a5ca08a59ac766a320897b9c323e5f7eaeeb2e70" => :mavericks
