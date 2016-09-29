c2abf32afdaa7da4df81455742e30fd8c2fe7ad8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 16:17:59 +0100

    hyperestraier: update 1.4.13 bottle.

--- Formula/hyperestraier.rb
+++ Formula/hyperestraier.rb
@@ -61,6 +61,7 @@ class Hyperestraier < Formula
 
   bottle do
     cellar :any
+    sha256 "c6018d888e9a4f03546f1727d9ec7b6d7eb6a87fc4f6755667bdafa71929aca7" => :sierra
     sha256 "c90ef2d3ccac1af3247726697be33748ec53df85a98af4611b6dbfc9a8dca0c7" => :el_capitan
     sha256 "d18c19a9d691e2bd209cc05006b608776066352d297865238cc7262a527a82bd" => :yosemite
     sha256 "b52c716897730a939ba7763492b7b1080a70c918b07571f4a4e296aea42f42ee" => :mavericks
