c4af24596578145e89826867752de5700ed61430
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 14:07:37 +0100

    gist: update 4.5.0 bottle.

--- Formula/gist.rb
+++ Formula/gist.rb
@@ -7,6 +7,7 @@ class Gist < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "25005c688506066c69fc2ca12cf9b351fbfa407a258807f84b55dbf2ec35f14c" => :sierra
     sha256 "2412d7a6ab8631910becf70fc6749b9746da002145582b0ad9fbbf225ba31369" => :el_capitan
     sha256 "365a758d97ee79f1601d36848f8efb2b5466eb70256ef2ad78129169cc363c0b" => :yosemite
     sha256 "83e5d999746477c29d8d42f9e16554d248a97528abc311ab3bd880f819ccc94c" => :mavericks
