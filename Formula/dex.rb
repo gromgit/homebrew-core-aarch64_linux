f8c480efca2f4d1e2fbf8d4c1c4fb834b8456470
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 04:16:01 +0100

    dex: update 1.0 bottle.

--- Formula/dex.rb
+++ Formula/dex.rb
@@ -7,7 +7,8 @@ class Dex < Formula
   head "https://github.com/tihirvon/dex.git"
 
   bottle do
-    revision 1
+    rebuild 1
+    sha256 "1e413a64cd9e2c594ec47c7e5e9ff36ab199126f6708265f5ad87363e66f033e" => :sierra
     sha256 "70c249809920acc2d10405c0487427d154ee55cf201507d910d8178693c7fd61" => :el_capitan
     sha256 "a4cffc5c0b61be9452988d4435ccff1d1c72d2b9cdec595e55ea5f37ca2541a6" => :yosemite
     sha256 "ce004b66bad4f8ad7d363f45a0b6af15fc96f719a591f3383cd2a84dc424d9e3" => :mavericks
