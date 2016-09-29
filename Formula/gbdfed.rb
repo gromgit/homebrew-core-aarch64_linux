3aa9120beef5d8f7264b36ae8b95004d640c5a44
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 10:12:57 +0100

    gbdfed: update 1.6_1 bottle.

--- Formula/gbdfed.rb
+++ Formula/gbdfed.rb
@@ -7,6 +7,7 @@ class Gbdfed < Formula
 
   bottle do
     cellar :any
+    sha256 "59f48f1bb6eca29530d74775ae09f845409f0b39416a866c8b9adcd7832819ea" => :sierra
     sha256 "9362dbaf834be1657f7984de06acfcc00ec29779b4c03738ad16731c5d91f41d" => :el_capitan
     sha256 "b511b3b460e70022e9dcb4750a042a9e488114173641859f04bb589363bb4fd0" => :yosemite
     sha256 "86762c5a866cf55625e323266661af05e66c37e18c1320d644a7627eee8ca416" => :mavericks
