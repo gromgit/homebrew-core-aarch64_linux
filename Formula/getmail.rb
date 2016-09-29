f0b4e23227080ac697b4d8d3f51358b4160982e4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 11:50:22 +0100

    getmail: update 4.49.0 bottle.

--- Formula/getmail.rb
+++ Formula/getmail.rb
@@ -6,6 +6,7 @@ class Getmail < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "160d1c3504443e51a1ec9c85e7d1aac59b9fa2fded204fa023324618d213833e" => :sierra
     sha256 "37f222a5229a89983b0238748ea2828df9ac08eb87b0260feda54603fba572a0" => :el_capitan
     sha256 "8123f6f1a331902fab3ab427fad3e2c8f00277e54a8bfdcc081ce1053ae9ad1e" => :yosemite
     sha256 "db0a01b3ed8ecd7c8cd30fd929713d9ac3d2b24e18369dd526703b79e5c6492e" => :mavericks
