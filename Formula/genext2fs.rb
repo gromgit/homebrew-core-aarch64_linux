2c03f7e86e9c764a046054f906bd8c81547d2f8d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 00:59:49 +0100

    genext2fs: update 1.4.1 bottle.

--- Formula/genext2fs.rb
+++ Formula/genext2fs.rb
@@ -6,6 +6,7 @@ class Genext2fs < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "82ac8092d73d2f81fd0770b15bad060f4f3b010c089a0cda5131f9bcec3318ea" => :sierra
     sha256 "3842e46ce4c24b75364337fbe4a10243cd01a8aaf4b51feca6631c7cf0649aa6" => :el_capitan
     sha256 "acdca2f9efcacafc7f105a43837a2f36e42dca1fd1325d62f9e5327797c69164" => :yosemite
     sha256 "f8f37e86e32de96736daac7b2b24594647e28d2b1610ccd68237d028d9b4dd43" => :mavericks
