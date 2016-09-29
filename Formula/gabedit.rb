26515c31a7b8bb9f124a92b23b0994a56d28dddb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 09:21:48 +0100

    gabedit: update 2.4.8_1 bottle.

--- Formula/gabedit.rb
+++ Formula/gabedit.rb
@@ -8,6 +8,7 @@ class Gabedit < Formula
 
   bottle do
     cellar :any
+    sha256 "76f03dc778e3379b2824c875c9eb344e8042c27fdf57f39576dcce78aa96dbb7" => :sierra
     sha256 "e8f13bf6bf5744f3439604b74c1313817dbf148b63e039e54089c10d65a818ee" => :el_capitan
     sha256 "86ef29773a913efbce0b04aef8e6abfe8c4849b02840f411f418e939da479c45" => :yosemite
     sha256 "507caa30aad09047605ca4df857b98e2c2172145d9382e8ca156fd63eeec334a" => :mavericks
