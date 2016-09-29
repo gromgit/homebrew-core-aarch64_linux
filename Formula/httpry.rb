360ce3f391cc2278ffaa5c18cae61b176381c995
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 16:17:58 +0100

    httpry: update 0.1.8 bottle.

--- Formula/httpry.rb
+++ Formula/httpry.rb
@@ -7,7 +7,8 @@ class Httpry < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "71014794d2a136fea229dd19d6fe7dc136037c074a817d70bd7b13713653f19f" => :sierra
     sha256 "56d6a77e429bf9dde3d5e5edb9959fc7ed913430236cf628e0aec6445c07c85a" => :el_capitan
     sha256 "af0deb9d79e72df6369f57ed1050abeb70c62f77ab481232b556ba6da5ace66c" => :yosemite
     sha256 "ec016612be65aa5761213134d211f9bee121d8904dae9b9d73ebfc37d4de3cea" => :mavericks
