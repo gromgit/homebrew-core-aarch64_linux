9988b923b6662876b4a07d86a97575499c4ebab0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 12:25:58 +0100

    git-number: update 1.0.0a bottle.

--- Formula/git-number.rb
+++ Formula/git-number.rb
@@ -7,6 +7,7 @@ class GitNumber < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4e961bbed534ae3bec5661574f333aa0217da960920ccf956421487a27d5e6e5" => :sierra
     sha256 "e4a73d3be05fb32a693918d9ccd7c13d995abbc29df57a91af8bb572420bc82c" => :el_capitan
     sha256 "aab690794b8908b84a02c8586831aa13bcdf4129d38fcbd9113e297b6b2c00d4" => :yosemite
     sha256 "a8775c026b9f5ff48b938c61d382aa972d54ee681a3ae96aa523156ca4242af6" => :mavericks
