515aaf9c074cfaea2424c57347760de50edacbb5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 11:50:22 +0100

    gexiv2: update 0.10.3_2 bottle.

--- Formula/gexiv2.rb
+++ Formula/gexiv2.rb
@@ -6,6 +6,7 @@ class Gexiv2 < Formula
   revision 2
 
   bottle do
+    sha256 "9bcb50feeaedd2aa01a3c9dca279301f8e366c370d15f03fbc1dae014a77fc5c" => :sierra
     sha256 "52056f0cc9405210101df91d2611652448f2c0e330c448a6e56bd0379a2065cc" => :el_capitan
     sha256 "63ed0d6a7ccaf0bab215a21ccd54eee9e7ad36cd7e8c31b75c382def37bd53c0" => :yosemite
     sha256 "04ba9507fd3e8c0aa84bcedad02db05387f12d965170d6dc594ab6a3d205106c" => :mavericks
