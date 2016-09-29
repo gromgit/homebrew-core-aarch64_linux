06b4c1d7feb62fa973a51b49cd13860e487d471a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 18:33:48 +0100

    ipv6calc: update 0.94.1 bottle.

--- Formula/ipv6calc.rb
+++ Formula/ipv6calc.rb
@@ -6,6 +6,7 @@ class Ipv6calc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1ccbbf74bb739f03f508b3b9de284a976deb6d812b162ee8993950537503ca18" => :sierra
     sha256 "437b1ccd14d570eed3f5aefc6136921fecf64a6e5271342b56f265436562f610" => :el_capitan
     sha256 "07aaa189403bd9d01a12e5aa79e8447e6f182cc6ef1ddcd68d5a9588cd6dd435" => :yosemite
     sha256 "7b3518dc5a1a0e9d8804b2d3e231b271f112569f61ae816f3649ca2ba55b9168" => :mavericks
