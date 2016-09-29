a48b6d3e80dc5594db4977db53b5d0919177d845
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 13:16:45 +0100

    gzrt: update 0.8 bottle.

--- Formula/gzrt.rb
+++ Formula/gzrt.rb
@@ -6,6 +6,7 @@ class Gzrt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "da5c89596737f514900f32986dd9eb32f010c6c1b9f1643dd03a07eae7e383a7" => :sierra
     sha256 "01df00fd35c6eaee9d32da4644d694ce33deda79a9c3da0284b52694f94a9515" => :el_capitan
     sha256 "af8ffc53bcf606b0634537adfeb67733c27ec079fa0347de41c668dbb5cce037" => :yosemite
     sha256 "0df681add87a86ffad0954b1699e3d92613faad902184b24ed595bccb7d3897d" => :mavericks
