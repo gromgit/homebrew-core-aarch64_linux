d33073ff057c1bf6e0c97e8f1428658eac0762c6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 02:44:48 +0100

    dhcping: update 1.2 bottle.

--- Formula/dhcping.rb
+++ Formula/dhcping.rb
@@ -7,6 +7,7 @@ class Dhcping < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5c41d596cb2a9835fc5f170ccd602294c98f163ba3f2a8d5c83bae252189817e" => :sierra
     sha256 "d3b03b1004d3a2d97b80fbbe9714bd29d006d9099a8f6baec343feb2833f3996" => :el_capitan
     sha256 "7741adb9bc166ee2450e521f7468e2b023632e737eb4da065848c5e87b6bd35a" => :yosemite
     sha256 "49206410d2fc5259798c2a76ee871df08c54772d1501d7ce1d29be652d600905" => :mavericks
