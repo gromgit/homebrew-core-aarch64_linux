5fd1602014d9b87591dd671a551e669ca2c0ea78
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 05:09:49 +0100

    fcrackzip: update 1.0 bottle.

--- Formula/fcrackzip.rb
+++ Formula/fcrackzip.rb
@@ -6,6 +6,7 @@ class Fcrackzip < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ce2d79b833f5805cbc475711a38db0a7a791b793b24b094e68f3ed54dfe5fd82" => :sierra
     sha256 "169a5e7ea0e7ee9d04dc7ecce5288ef3a096fc9875d9af134b342878ce8c55fd" => :el_capitan
     sha256 "1e9a5e3d9d37ce1bf7338d3f12f84bf67b31de4e2a6eb1511f90458c45b1b810" => :yosemite
     sha256 "305533df364c7b91ae837dc38b3632bc9e2f0d167e10ad94901b5f2c06ab4924" => :mavericks
