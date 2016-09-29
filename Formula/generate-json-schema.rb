50bd78c3be8dbfba1618f55fc71ed856cefac9a9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 00:59:49 +0100

    generate-json-schema: update 2.1.1 bottle.

--- Formula/generate-json-schema.rb
+++ Formula/generate-json-schema.rb
@@ -10,7 +10,8 @@ class GenerateJsonSchema < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "0915c902a8de34bfe2298d4f91040eea18762c0e3e68747ce6e32a07e135810c" => :sierra
     sha256 "d573136e4025aa6ecdad3ca73bfc53307ef6ab212bd8307387a7ff9763db14ed" => :el_capitan
     sha256 "ac72c27b5ae36a0c40ba48b21d590519175395bd903ca7a3c46e4af840daa449" => :yosemite
     sha256 "8fc1b18f001ea586ce7f3e5d5c021ad76be4cb8010688a783a1c80b22e3e3167" => :mavericks
