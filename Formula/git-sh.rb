f74655087983b3625c168e45b2566094a44ca1a3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 13:16:45 +0100

    git-sh: update 1.3 bottle.

--- Formula/git-sh.rb
+++ Formula/git-sh.rb
@@ -8,6 +8,7 @@ class GitSh < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d371fba61367507f5e88818eb1f0630e388d198c37faa957ce410d97675a7f5d" => :sierra
     sha256 "e30e7836919a5d79712e3fd51a118279b412c44da909053b9b185eb48963323f" => :el_capitan
     sha256 "cdbc6fc62300722f613f314e2859422edcf938c6807a3039bcf476e02fbe222c" => :yosemite
     sha256 "1b9aa141f32145516db62304dda799611e1fc35ec57275ee75bf566325a6bfa5" => :mavericks
