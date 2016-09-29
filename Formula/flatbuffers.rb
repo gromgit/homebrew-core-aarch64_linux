5bb87a01166c1e81356a6ba7b492066c96b17bde
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 06:15:33 +0100

    flatbuffers: update 1.4.0 bottle.

--- Formula/flatbuffers.rb
+++ Formula/flatbuffers.rb
@@ -7,6 +7,7 @@ class Flatbuffers < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4e7a7048a3eddd16f080c8a1267c255bf5e7c08ab9d087d0f64b1f3e4f229cba" => :sierra
     sha256 "11e6a7e4c4b2c7dca2e5582dae037b15f2547aeb3f49ab1fb716f10e5ec65977" => :el_capitan
     sha256 "4b0330850e90721864c2c5a4f73e4efd6fbee006b52cd2f77cdbcd0ac98dce64" => :yosemite
     sha256 "e60373d7db349515b22e81b3b2fe8cdfb3438688b310b90d3e227b9d5d106e6d" => :mavericks
