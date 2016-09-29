57b187a7bf43d32e415c0b783514f6483702a15f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 00:59:49 +0100

    geckodriver: update 0.10.0 bottle.

--- Formula/geckodriver.rb
+++ Formula/geckodriver.rb
@@ -6,6 +6,7 @@ class Geckodriver < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e0219242056eac613772f1ad44c255bec61fe6a6c9bc88b05ba725c72787efec" => :sierra
     sha256 "fccfa382aa88aed9ad57afc4cd80a66a89217842dcd7f5f4274b4fc41719f15a" => :el_capitan
     sha256 "a4f6d2e76307ee1dced483223b0a3d8242aa5877ae8f9f828f6dbf474749de5e" => :yosemite
     sha256 "cdfacfa93eb1a418b2fa58e575294658b7384eb148e638c2b9a0bb4c172855cd" => :mavericks
