7ae0ae9f606f2b11fa73f6487c821e3bfd2a28bf
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 08:49:21 +0100

    fsevents-tools: update 1.0.0 bottle.

--- Formula/fsevents-tools.rb
+++ Formula/fsevents-tools.rb
@@ -6,6 +6,7 @@ class FseventsTools < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8190bdd8d9d88f9ee6ebdc56434551a44a39502225160ad2604562d9c7ed0822" => :sierra
     sha256 "a15d1b927a74d8b76b553d696343ac5fe82396f8b5b052e15adc6c3270f2ccd6" => :el_capitan
     sha256 "9d4bda3175c503f8ff65f134495de43471854eacc3396a9cf1895d5c2b91cb63" => :yosemite
     sha256 "045442f0279c20196e0941fbf160557ebdea04169267ba8105717d7d207cc346" => :mavericks
