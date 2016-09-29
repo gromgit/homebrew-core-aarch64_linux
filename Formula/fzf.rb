9c98a2ea0f73ceeed44f19db8923f1487098748b
ilovezfs <ilovezfs@icloud.com>
2016-09-28 21:42:10 -0700

    fzf 0.15.3
    Closes #5355.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/fzf.rb
+++ Formula/fzf.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Fzf < Formula
   desc "Command-line fuzzy finder written in Go"
   homepage "https://github.com/junegunn/fzf"
-  url "https://github.com/junegunn/fzf/archive/0.15.2.tar.gz"
-  sha256 "32bbe680f9830845b73f043212d6dbf57b7335702c791fce3efd86b252d8d125"
+  url "https://github.com/junegunn/fzf/archive/0.15.3.tar.gz"
+  sha256 "aac1675bd4665b10addf2782fea7043bb6849e04d985360fb13db17264d03035"
   head "https://github.com/junegunn/fzf.git"
 
   bottle do
