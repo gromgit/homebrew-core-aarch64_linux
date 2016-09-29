fb9573df4e2469f9707e1c129113ae9973197424
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 04:16:01 +0100

    depqbf: update 5.0 bottle.

--- Formula/depqbf.rb
+++ Formula/depqbf.rb
@@ -7,6 +7,7 @@ class Depqbf < Formula
 
   bottle do
     cellar :any
+    sha256 "8d500845a553dafeffbc2ac3a34d15d05f84e39707141ad6eddf30dd68b80c8a" => :sierra
     sha256 "7c0b8ef336f9d2bac14e11f0ca838620428376ba4b1f29b6ac3614d3a5f61774" => :el_capitan
     sha256 "d10617714d882cce0a4a8754c03fe7f9df7adf01de8b0016cceafe092e98c163" => :yosemite
     sha256 "92ef32e3fff775db370d3c83ee1b09c0d3c7debab448be37f30465094b17f028" => :mavericks
