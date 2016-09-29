9d90d39983c6854322a7e5d3e284c2ab0cacfb0d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 15:50:47 +0100

    hexedit: update 1.2.13 bottle.

--- Formula/hexedit.rb
+++ Formula/hexedit.rb
@@ -8,7 +8,8 @@ class Hexedit < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "a876b73ca63a0da195ddee709716ca973d579b85062a93f85261500f8ffeb612" => :sierra
     sha256 "078d2bd1b7fd56db28b0b4d972826aedd117e810d9a885b5b2545cd8e5e5ccd5" => :el_capitan
     sha256 "4f06836e7a2f4a280084fe8f9f5ff3903272a6e9995f24bf93156afc56d7b996" => :yosemite
     sha256 "1931661462fffa57fb8b0b6b7cb3c4439ed72b93f5a0a6db94e9bb2f5fa1cd4d" => :mavericks
