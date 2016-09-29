d2430e51a6b4f33e126795ddff230c509472fb6b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 14:25:44 +0100

    haxe: update 3.2.1 bottle.

--- Formula/haxe.rb
+++ Formula/haxe.rb
@@ -9,6 +9,7 @@ class Haxe < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4865e7dbd816f5fc433390d55c6493fd0b82cab3c1e84038b21ea77fce88c1c2" => :sierra
     sha256 "131893375697db4a6f747ebec788f9d0d7304b742a3c007f27ea877f561e1e9f" => :el_capitan
     sha256 "104c2a1e9cc5c0d3ca3ee794a4872e8b25107139106f904c2471162ce278474e" => :yosemite
     sha256 "1e4c88518c6747bf86345ac67b2e3e2da5d9f3cea3c3917e8837ac4db2083412" => :mavericks
