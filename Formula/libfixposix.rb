986831e2207213855a9d92c4b359c612a8886113
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 23:45:30 +0100

    libfixposix: update 0.4.1 bottle.

--- Formula/libfixposix.rb
+++ Formula/libfixposix.rb
@@ -6,6 +6,7 @@ class Libfixposix < Formula
 
   bottle do
     cellar :any
+    sha256 "f9511f4ca3c17903a2aea6e6ed089286cc0d8b8c1f21349a2381bd31acb4fd02" => :sierra
     sha256 "e876af0f5a95391c41f9bf7e7e1ca7c69ba0c5afe4ce570daff34474f3ece1dd" => :el_capitan
     sha256 "defc55272fbda383c8598e216a65b79ba8c5c4fc5c0d0a1752168919f0eee1c7" => :yosemite
     sha256 "f1f7f7248fd249cbb4ac366ad45e313c39055e51ffb0c15679f1b880d5b7a566" => :mavericks
