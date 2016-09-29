ef2a16fcd08e4618ec146a0e4012ba9a5a345326
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 04:16:01 +0100

    deja-gnu: update 1.6 bottle.

--- Formula/deja-gnu.rb
+++ Formula/deja-gnu.rb
@@ -7,6 +7,7 @@ class DejaGnu < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3f6f667f06ed0a74f993668847f437e60f90397b0b1615ebbf0f930f7d2836d2" => :sierra
     sha256 "099657338971c6b84bf135f2935559a4753b2137edd807d777c999edaf0fb8d6" => :el_capitan
     sha256 "0d589e4ec11e3d8de7f00abcbb4a62b50e578e15e6d8fafd42371ca9550b04c8" => :yosemite
     sha256 "575e2565feb8d2d60c3163a1a8d38c4ba719fd5a523fc147a7812aed7f26fb88" => :mavericks
