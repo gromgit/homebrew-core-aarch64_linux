4e0af4964ec76395ec572e145e1ff6a31ede016e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 17:34:51 +0100

    ike-scan: update 1.9_1 bottle.

--- Formula/ike-scan.rb
+++ Formula/ike-scan.rb
@@ -6,6 +6,7 @@ class IkeScan < Formula
   revision 1
 
   bottle do
+    sha256 "36d4250aba9099fefd630e3a585f4dc035ccb43d53d3270fc41bb08e92242a49" => :sierra
     sha256 "65df0ddc049fadf6c9deb605442092026ec1c4ef5a6358a63ed96928085ac155" => :el_capitan
     sha256 "70276baa029ee6c1a451175e3254cc74754fd5de83fe748d17ba4fcf0586349a" => :yosemite
     sha256 "a0967f6ce6e9903068149c9b379926a25b315c3bdb9aca451f28b0d9cc744abd" => :mavericks
