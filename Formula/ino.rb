f80025dbfcf72dc3e675406cc089d019c7f6b3ed
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 18:06:25 +0100

    ino: update 0.3.6 bottle.

--- Formula/ino.rb
+++ Formula/ino.rb
@@ -6,6 +6,7 @@ class Ino < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ba6d8a28197816999132d07732bf522e8b50f5c8030aba43b2fa48556c9c8115" => :sierra
     sha256 "fe8bd44bff433014afad389f2afd63253ac747eba19ecbd055f2f37bef236430" => :el_capitan
     sha256 "326863d7187f8c280a39c0d20a5ef568ad58a6c2f9d5f20d9fba75f4d2c2f03d" => :yosemite
     sha256 "a2a0aeb71d02e6d67578f9c3265fa2b11ef716bc47519f3b31d001f9712e8b6e" => :mavericks
