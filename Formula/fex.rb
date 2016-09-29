61536ff5b7fd45459f83990ddcf00abb12c5740d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 05:09:49 +0100

    fex: update 2.0.0 bottle.

--- Formula/fex.rb
+++ Formula/fex.rb
@@ -6,6 +6,7 @@ class Fex < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e42328824017f1432ace562fdd70061e504c5524d2702f3d4b470c40b1bf105e" => :sierra
     sha256 "952c166ae7efc5c7955bba3c54fefc1e5c18fe2296804b15554a8703285034e1" => :el_capitan
     sha256 "2c20c6bf653b60b290d6e08aee9aaa46475754cfd61680e312d46678ea9a1f4c" => :yosemite
     sha256 "1fc2bf94e26daa8dd17b1e1f614b8fdffabac6665b7eb38a659e4e3dee5772ae" => :mavericks
