fd1d2a69c40a0b6be7612ad0b21878fe991f4111
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 01:01:15 +0100

    dark-mode: update 1.0.1 bottle.

--- Formula/dark-mode.rb
+++ Formula/dark-mode.rb
@@ -7,6 +7,7 @@ class DarkMode < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "50d354de8d7fbfe726895dc44547779a70d43cd252fe16580e35925812061a5e" => :sierra
     sha256 "15ffc6e8d672a7601462f3597b5ca26b7ef5d2ce739ea7afa78e0cad23bee49e" => :el_capitan
     sha256 "f58b190cb89027daaea97e8cdc31bc4f097c51fef33e805751a1aef797871e5f" => :yosemite
   end
