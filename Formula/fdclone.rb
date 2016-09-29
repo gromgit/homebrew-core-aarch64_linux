bcbc10abbbdcaec977083287d9d970b89b26bc66
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 05:09:49 +0100

    fdclone: update 3.01b bottle.

--- Formula/fdclone.rb
+++ Formula/fdclone.rb
@@ -5,6 +5,7 @@ class Fdclone < Formula
   sha256 "d66d902cac9d4f64a91d42ceb487a138d544c9fd9cb2961730889cc8830303d4"
 
   bottle do
+    sha256 "ce0319a6a58dbf44dbfdd8d8716c86cf34fbb856bf660034eae9b18dc1362dc6" => :sierra
     sha256 "f9992eacec6b447a8bef9946445e371f560483799c0912e5ecd6b095bbd0b542" => :el_capitan
     sha256 "e5983768aec9c3d61a72a06ea25b121cf54178461b22f3848c446249a99b26a9" => :yosemite
     sha256 "1add0151a1961ea9b9b167b220dc0248a0fec6263efa351c2558f7eded98c361" => :mavericks
