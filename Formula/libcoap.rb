a5b8b672167d97c8d16c85e0e0bb49ef008d4fc0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 22:48:23 +0100

    libcoap: update 4.1.1 bottle.

--- Formula/libcoap.rb
+++ Formula/libcoap.rb
@@ -6,7 +6,8 @@ class Libcoap < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "18f79fcd64996479f193b4872e3c5a641bf9550c4fd3447c1b663b0f38a53edf" => :sierra
     sha256 "daa7dc3537fba0f8403a8a03af895b6a7f6914c2ec75fd36c7f4e9e67e5295a3" => :el_capitan
     sha256 "dbb1193f7e0d36d2980653715e205a54d41ea4d0bffa53b39b27e637636de4b9" => :yosemite
     sha256 "17c6b22921cb4ea64009a8d076f95b6da70c42e3f65e0d49bfc36bcf3e1372e9" => :mavericks
