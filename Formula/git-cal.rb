beca39e7417e8a358002bfdab2813e6a560c9eef
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 14:07:38 +0100

    git-cal: update 0.9.1 bottle.

--- Formula/git-cal.rb
+++ Formula/git-cal.rb
@@ -8,7 +8,8 @@ class GitCal < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "82847887556bd0334e65c1c7a3a063c2d62e5f71e81e89c53aa8e0df1cc41e31" => :sierra
     sha256 "8f928d65dc3414900ace5751d9e93bc712f03ffcabfefeb0d659e18db3998622" => :el_capitan
     sha256 "d61d27644f236dd221d2ea15aea7ee088afaf08c305217084b63fb85cb0d7dba" => :yosemite
     sha256 "2f09af33dd202d56be91b6bb7ead320cbc322332e411a7b712879a9198364e64" => :mavericks
