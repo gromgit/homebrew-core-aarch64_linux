6273a7078e4f0c604abe362fd04d75e25c108f47
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 07:03:32 +0100

    fossil: update 1.35 bottle.

--- Formula/fossil.rb
+++ Formula/fossil.rb
@@ -8,6 +8,7 @@ class Fossil < Formula
 
   bottle do
     cellar :any
+    sha256 "2040cf4c0d0fe1f671ce869c08597f856e475701111524e131c876d4f6f09a3d" => :sierra
     sha256 "4ff3c58d50dea6f0324d48f821f1e3371f94e14cb8d0d6e4b0dfbaab0f07db69" => :el_capitan
     sha256 "42495830280a61b3122388aefb108a9c20547c5cd0745f47020999d73bcc03aa" => :yosemite
     sha256 "f0967b37baa7fc8ad43f20a12c61dbbbaf78b2f711acd004180e5d0374617b5d" => :mavericks
