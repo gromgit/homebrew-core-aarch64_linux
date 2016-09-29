09a2b489f8fdb71cb0cffef3724565afd05ffdd9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 14:07:38 +0100

    git-fixup: update 1.1.1 bottle.

--- Formula/git-fixup.rb
+++ Formula/git-fixup.rb
@@ -8,6 +8,7 @@ class GitFixup < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "478f6c76e15aa1fe183711e609fdc492bb9be5970a555b2b6a1ccfbbd3b96be0" => :sierra
     sha256 "0145c568d255c20a0f728d14152aad8a83a0ceb25e9f833d016efc57d19191c8" => :el_capitan
     sha256 "077bcf80be63e6da3bdd7699f6b82549d99014210ff8e015e5319234d8a9e75a" => :yosemite
     sha256 "f0c6934dbd1317abae4ef8c0e1440d624dbc7025f06eb1006d654c0c2925cc5b" => :mavericks
