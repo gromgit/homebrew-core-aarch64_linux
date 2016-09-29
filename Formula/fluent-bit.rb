b382235138ae60ab4ad744ae6453b2b3181ba00c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 06:53:28 +0100

    fluent-bit: update 0.8.5 bottle.

--- Formula/fluent-bit.rb
+++ Formula/fluent-bit.rb
@@ -7,6 +7,7 @@ class FluentBit < Formula
 
   bottle do
     cellar :any
+    sha256 "8147ff5c949478a670c0520a317325c09f626f07acacf8f3652ef948baa8ca33" => :sierra
     sha256 "686a68b6aac9d14aea6d39f96ae8fdb4cf37a099e948acc55634193e836e0930" => :el_capitan
     sha256 "ed1a751e17d5e90d6af02382cbe05b736afcdeebd9d120d7fbbb16b3ced33679" => :yosemite
     sha256 "e2cfd67ad579251f39e883356e06a2b56e316497d51478473d94771ee49bcc33" => :mavericks
