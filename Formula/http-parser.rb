0d23048dc8fc9c226a244b2bf58cf45194f26920
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 16:17:58 +0100

    http-parser: update 2.7.1 bottle.

--- Formula/http-parser.rb
+++ Formula/http-parser.rb
@@ -6,6 +6,7 @@ class HttpParser < Formula
 
   bottle do
     cellar :any
+    sha256 "a19519cd90917dccb31171febe7e25baaa583ec9bdd7c4e4d06dcb2a7605447f" => :sierra
     sha256 "afc0af78e3a4789b18b96ec83a09a38c5e6fc0f6ba72523a1f5c0f4b4d1441e6" => :el_capitan
     sha256 "0d62dc8723a72e60d13fbd61f870a8e3cc3a7f29fc5814c645322c284be7514c" => :yosemite
     sha256 "5d3aaf70d3ca4ee5ff688b9f834acd68891d4fb637721b54597deeba6fca41d0" => :mavericks
