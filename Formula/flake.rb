d2c2dad69faa5de602211618d01089c3d2522203
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 06:15:33 +0100

    flake: update 0.11 bottle.

--- Formula/flake.rb
+++ Formula/flake.rb
@@ -6,6 +6,7 @@ class Flake < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b2eca0967c020b082f7d7f8c4d15765204ea63aa9332012074c852967a0fee07" => :sierra
     sha256 "e521dae57260b2a71f161f16609530c854ff8ebee4252b0addf3c16b5abc982c" => :el_capitan
     sha256 "af8e2e83dd3c82e8cc26be91ca64e24571b34185d4f8f007725b54d37f38e2b1" => :yosemite
     sha256 "caae2cab90e1e392a93d47b88c5c9a9242c3740ca95e767bca61d9d267f337f9" => :mavericks
