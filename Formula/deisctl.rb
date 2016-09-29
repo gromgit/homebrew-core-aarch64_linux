8346b0b3498615ce788d05b731a01e738bfa7171
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 04:16:01 +0100

    deisctl: update 1.13.3 bottle.

--- Formula/deisctl.rb
+++ Formula/deisctl.rb
@@ -6,6 +6,7 @@ class Deisctl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "dc7a728554046e2f6191ca4d0d2b61ecbd7c3c6b1e5f929b2f6f71a53c94a458" => :sierra
     sha256 "1c3ce561d7b81441c162084775d5d295919550534520ec475216ca6c9af96b56" => :el_capitan
     sha256 "3450f08fb4a5e46768c81ac57742b5b234a1171c93c195004627445c03f8ddbf" => :yosemite
     sha256 "7ce46bd2b45d6772625ff82f4076ec42cb0232e4f896b07bda108c5f2db59f2e" => :mavericks
