68031c20a94d0b474b7ff53ee729a0127cf91afb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 08:49:21 +0100

    ftgl: update 2.1.3-rc5 bottle.

--- Formula/ftgl.rb
+++ Formula/ftgl.rb
@@ -6,6 +6,7 @@ class Ftgl < Formula
 
   bottle do
     cellar :any
+    sha256 "946a9530f7eae5c8f2bc71dfc91b3a8138ae2228cd441fd7cf39f047b957ce47" => :sierra
     sha256 "6462eb0b97ab120639f1a191f6e3a39419bbb813abd71f5c741303dbf0aed7fb" => :el_capitan
     sha256 "26db05485600adfb7ead23d04fae9b1ee1d1a4b7ac304e1453ad83b4b2c39f64" => :yosemite
     sha256 "50a41f3c95a363b52bc367abf4b5b9dc272d71c8b35fe8e63f058c7cf7162225" => :mavericks
