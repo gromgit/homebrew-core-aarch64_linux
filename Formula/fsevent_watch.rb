7fd1387c95e14cda08f83054a0db35dbd3a8f9d7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 08:49:21 +0100

    fsevent_watch: update 0.1 bottle.

--- Formula/fsevent_watch.rb
+++ Formula/fsevent_watch.rb
@@ -8,6 +8,7 @@ class FseventWatch < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "63d964a1a42e46191b76fb86a955a56e989c7df86fb4787f6341fc1b8c99a91a" => :sierra
     sha256 "d9ff549a7f9f5b31ffe923beddc1a8ab123c11e76bb833fb882785342d119768" => :el_capitan
     sha256 "085b1a0cdc155ec6833d782ebd86e8109f6a4529ff3719f3605fce5779925456" => :yosemite
     sha256 "900dff7d67ce9b31c9e1a3884315d8ed407cbd89358aed68fda283f7782ff2c6" => :mavericks
