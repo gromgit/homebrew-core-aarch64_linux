a47efb24f908ec0f35fd2a7e290c68c8eb6fd30a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 03:30:06 +0100

    dnsmap: update 0.30 bottle.

--- Formula/dnsmap.rb
+++ Formula/dnsmap.rb
@@ -6,6 +6,7 @@ class Dnsmap < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2015e3fdee32fcdbc0501f522ba720280e8cb07a75f8166fd62a9f876ea12124" => :sierra
     sha256 "57d5f7ea4a8a73b454bbc60e38fdb9a55e07c2d97f0b5acf38db0408e83b197b" => :el_capitan
     sha256 "6ede00304c268f3a0013f8aa708f5bfc314acbac1db7a3547e8381dc620b18eb" => :yosemite
     sha256 "8ed334077b5c440a7e7015e0b178f0e9500d816d6ee99d00e81a532b8e028e26" => :mavericks
