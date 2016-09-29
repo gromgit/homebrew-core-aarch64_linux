e5ff1c75d821bbee46776deace7e54e8a11d1dca
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 02:44:48 +0100

    dhex: update 0.68 bottle.

--- Formula/dhex.rb
+++ Formula/dhex.rb
@@ -6,6 +6,7 @@ class Dhex < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "133636389b472224c6d12f8082be088464e256d7d108ef33f94f3e4e034086cf" => :sierra
     sha256 "78d10f5fc83e4c46f4c6fbe46df834498118b80f190b2b40d1093630be3e039b" => :el_capitan
     sha256 "c6d92d8f4175ecd84be55b071887a97c7924977fbd24509162c956d17d85c84e" => :yosemite
     sha256 "de8a9b04e49e85b4cca75050375724076c9e496a124b0af49006d60fe44dc81f" => :mavericks
