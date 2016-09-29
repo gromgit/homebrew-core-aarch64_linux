80846d23943eaf679f8ef6fa6cf83383f89fef2f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 03:30:06 +0100

    dnsperf: update 2.0.0.0-1 bottle.

--- Formula/dnsperf.rb
+++ Formula/dnsperf.rb
@@ -6,6 +6,7 @@ class Dnsperf < Formula
 
   bottle do
     cellar :any
+    sha256 "5fef021470831807af35bd6b449027f7d75c2031120725541ae19c37a613020b" => :sierra
     sha256 "e169ef157bfb88e0161f7c14f2ab7752138175f93c2f05c7e0d00d9afd696f06" => :el_capitan
     sha256 "54cdcb0c2c6e150031b2ce436494a5a218828058ad37a087203165eb6a24ce4e" => :yosemite
     sha256 "dc94a7815272721dc7a9d89fbdd133122a5002bb6b9dc84417acd6ec19f1a636" => :mavericks
