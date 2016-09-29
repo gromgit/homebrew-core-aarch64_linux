9d7aa69166c9c2c735842f00ff82d0ef3529a264
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 17:12:47 +0100

    icecream: update 1.0.1 bottle.

--- Formula/icecream.rb
+++ Formula/icecream.rb
@@ -25,6 +25,7 @@ class Icecream < Formula
   end
 
   bottle do
+    sha256 "85ad1eca8866acbc1666a8054f9e30ccf8acb42c08cd05e6304a34186788dcd4" => :sierra
     sha256 "b7735e14d19d4b8fb4e861fe2b40b19f524ab904c53b954062be9d9a26a2f99c" => :el_capitan
     sha256 "9767c0d31cce91446873f3f4baa8068f73909e575f6a8f81f1ccfa33d17ec2b4" => :yosemite
     sha256 "7dbedeb6418bd830e33818c95bb6339e193f37e664452a1ffc5af514b2778921" => :mavericks
