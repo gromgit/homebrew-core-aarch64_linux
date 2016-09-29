90a2744e5935b44eba8832e9dc64c05494661738
Gary <gary@gary-lai.com>
2016-09-29 03:14:09 -0400

    jena 3.1.0 (#5222)

--- Formula/jena.rb
+++ Formula/jena.rb
@@ -1,8 +1,8 @@
 class Jena < Formula
   desc "Framework for building semantic web and linked data apps"
   homepage "https://jena.apache.org/"
-  url "https://archive.apache.org/dist/jena/binaries/apache-jena-3.0.1.tar.gz"
-  sha256 "8175f1624dbe33ff33054ee2a8e8ef556af19f1d3a1065b8bbfcbf84ad3c3562"
+  url "https://archive.apache.org/dist/jena/binaries/apache-jena-3.1.0.tar.gz"
+  sha256 "532ad87eab7792ff1ffae34375d4c27956aada7c659743c39027e8b48f29cbd9"
 
   bottle :unneeded
 
@@ -18,11 +18,14 @@ class Jena < Formula
     rm_rf "bat" # Remove Windows scripts
 
     prefix.install %w[LICENSE NOTICE README]
-    doc.install ["javadoc-arq", "javadoc-core", "javadoc-tdb", "src-examples"]
     libexec.install Dir["*"]
     Dir.glob("#{libexec}/bin/*") do |path|
       bin_name = File.basename(path)
       (bin/bin_name).write shim_script(bin_name)
     end
+  end
+
+  test do
+    system "#{bin}/sparql", "--version"
   end
 end
