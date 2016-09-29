3058a0483ee1c4ffb3b0449a0044a9aed9694082
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 23:07:02 +0100

    libdvbpsi: update 1.3.0 bottle.

--- Formula/libdvbpsi.rb
+++ Formula/libdvbpsi.rb
@@ -6,6 +6,7 @@ class Libdvbpsi < Formula
 
   bottle do
     cellar :any
+    sha256 "11c95243fafbca574d4270a3c5ae60997f77f4b5871fac75f17006014b32afdb" => :sierra
     sha256 "4a2441b1d7ef602c05e46744a394373de1de33284955c455937b4bbdecddfc89" => :el_capitan
     sha256 "db2d97fb4e3460aa1b44e75af5398c0b9d2d8b860f18cb8a197c18e9e1ec8229" => :yosemite
     sha256 "45af2a1bd8b769216fcff8b5c8c37dd136ab4db99167e46a8371db4880afc2c9" => :mavericks
