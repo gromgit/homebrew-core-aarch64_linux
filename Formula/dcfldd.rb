12ab25e3345c4d915b55fd2d96d50b27aa99a015
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 01:25:38 +0100

    dcfldd: update 1.3.4-1 bottle.

--- Formula/dcfldd.rb
+++ Formula/dcfldd.rb
@@ -6,6 +6,7 @@ class Dcfldd < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0958d948042f047d4249a7400f8c4f7adfe41f11c20aa04a0dbaac09c718ea2a" => :sierra
     sha256 "0d5ff357d74fa90a97d80e202ddb5b5554bfec35efa2c4cb6e0f7e6dc9cf8ece" => :el_capitan
     sha256 "4731a1409199c0eb8d83f9f2f23106c1f71ccb1f8d8faf71a3fd691ba394791f" => :yosemite
     sha256 "5aeedb1165a426057dce57cf8b9b5b33db33f502d8cf2611276f2526bde1dda4" => :mavericks
