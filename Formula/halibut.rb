2149569c2922fd7bee7c12b4b9bc53211290fb23
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 14:01:03 +0100

    halibut: update 1.1 bottle.

--- Formula/halibut.rb
+++ Formula/halibut.rb
@@ -8,6 +8,7 @@ class Halibut < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "52d1b558f693989ad6a5a0f78edb984a4d4a67a559a6013b29c3a6aee31a4937" => :sierra
     sha256 "3499646cc8fb4fc71c87743b0e27ab5fc37b7b72e03b2ff3cdf86b3809ebda7e" => :el_capitan
     sha256 "db13c78d65619b0b602a83d9641afdbe4d603492bc33bab263ae2530630578f4" => :yosemite
     sha256 "eedcff72763e75094aadb2a05115614484a2e46561bb11a0466a98153d5dbcab" => :mavericks
