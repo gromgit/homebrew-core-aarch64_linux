0e4567a35cb9f4e016b3b89014f13b2956e23ef4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 03:30:06 +0100

    dmtx-utils: update 0.7.4_1 bottle.

--- Formula/dmtx-utils.rb
+++ Formula/dmtx-utils.rb
@@ -7,6 +7,7 @@ class DmtxUtils < Formula
 
   bottle do
     cellar :any
+    sha256 "16d48cbf2475c843a9d42dfa248e16fccf1a88ad07ff33e05bebe55430fb1d2f" => :sierra
     sha256 "652da1ff2d9d49c415b4acd6e0127aad2716c6aafcb47f9f1276b0d2faa4b3b8" => :el_capitan
     sha256 "e214477f057b308ad54ddad51d2592c47d472164f73b03762ca0f6fb519de5c9" => :yosemite
     sha256 "f97053b367d71a7d2ffed6ad5216888aa19d7a615264ffb5c63d72bb5dbe440e" => :mavericks
