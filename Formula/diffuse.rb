dd4a71a852c91e60e882f018a7de67fd72f0fb85
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 03:09:20 +0100

    diffuse: update 0.4.8 bottle.

--- Formula/diffuse.rb
+++ Formula/diffuse.rb
@@ -6,6 +6,7 @@ class Diffuse < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ca2764034fc37d643f2efadfc4fc307479263988945d8d65019fad240f4a5ea6" => :sierra
     sha256 "d2f0ed47838f888753d7fe4a7c93a9557d8b8826548c53b6098789ab22211562" => :el_capitan
     sha256 "5e7703ec672b8f5636463a5cb0e3a79bb635a485d6657e1921d3cae63a140125" => :yosemite
     sha256 "e2b2b9bcb2c60f014c56e8c924b020a9568b9ebbb9b97c81e6be02be89933c69" => :mavericks
