e66dcc578aa917ba117053b6fc34fe722b31e028
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 18:55:58 +0100

    ired: update 0.5.0 bottle.

--- Formula/ired.rb
+++ Formula/ired.rb
@@ -6,6 +6,7 @@ class Ired < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "07b729be0f1b5218111d26df84352e1985a0a558a44f32724055953a7fe8e4bd" => :sierra
     sha256 "7d02292ccb5d3f0cf283c732428259be1d1a12c934cae9e20b1a5d64f7ca473a" => :el_capitan
     sha256 "8f3ea6c227b0a939a3c9cd65528439d59bbd8cb493c5d5f87de64f54173fa926" => :yosemite
     sha256 "17661990a22e84dce84fa684d096f0082925bb5e05d04807704bc6403904a558" => :mavericks
