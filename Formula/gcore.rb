d54725efcf84f85bf0a97f387854a60938af4021
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 10:12:57 +0100

    gcore: update 1.3 bottle.

--- Formula/gcore.rb
+++ Formula/gcore.rb
@@ -6,7 +6,8 @@ class Gcore < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "624d85ab5533dec9806d4b89c2abf60ef193b0fea0bcf4fb135d2e84ced3631c" => :sierra
     sha256 "6ba5c2d5212f291b6410e9770e4bd8863f11462720c5f92898075d01acf0fb8e" => :el_capitan
     sha256 "4b098515f445f46942b8b7deab79f03206441d1ba95aaa65ce5da3c9e081a17e" => :yosemite
     sha256 "6479ee2516b07716c506155dee9e9d6be8484ae5f8ac044945644eb22db49a3b" => :mavericks
