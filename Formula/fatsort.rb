dd4188e03705879c4a24dec570a2d23b4f4e7aa9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 04:47:25 +0100

    fatsort: update 1.3.365 bottle.

--- Formula/fatsort.rb
+++ Formula/fatsort.rb
@@ -6,6 +6,7 @@ class Fatsort < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "53f12d55b4101fb0b3d5e30dacd0a8dfce6dc7ae1c8bd7bda8f49396d8c789e5" => :sierra
     sha256 "104cd675fc257344c5c96209a8cc924f50cf1bc4696f966e10e61ebeb4e2f62c" => :el_capitan
     sha256 "56540697be3d92f196343199911fc2a780fb4f554bd6542818659158081aaa43" => :yosemite
     sha256 "24362ba75c2e644c1480ba2e73536fdf3010e1f76b6d0b3dbde54e396d95095f" => :mavericks
