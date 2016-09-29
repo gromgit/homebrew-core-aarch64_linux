b2ee94e9575f515c0508f9dbbc5cab697433f542
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 06:53:29 +0100

    fmt: update 3.0.0 bottle.

--- Formula/fmt.rb
+++ Formula/fmt.rb
@@ -6,6 +6,7 @@ class Fmt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "64594784bf98259bc0e08499b03d65395b849f2db5e132b7d885b4dcae1893cd" => :sierra
     sha256 "89e5da4b7ccda59c840406b830b547b50e9621ec73524ec9d90bdec95087c115" => :el_capitan
     sha256 "713dd1a92c1e3509c9bd67b0f72cbf4141923288188ffc0630648981c3239c3c" => :yosemite
     sha256 "0f563f0c3bb0e8425fc4d4dd48d5e493eb2a2e6301fb7eb2e0e646d1dd2f12ca" => :mavericks
