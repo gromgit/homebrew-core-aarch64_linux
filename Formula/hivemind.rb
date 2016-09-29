2d6df7e72197d18e792f3dde1a8050620515e2fc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 15:50:47 +0100

    hivemind: update 1.0 bottle.

--- Formula/hivemind.rb
+++ Formula/hivemind.rb
@@ -8,6 +8,7 @@ class Hivemind < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "18fa6c0f6d7df12745a8524fcdc5661cceca8b3a3f76c68d4670747848e30638" => :sierra
     sha256 "0ed5b8e1b27d73d069e12b4d4fb8099c8a3b3c87eb61c871187805d3c2f2ee34" => :el_capitan
     sha256 "8a671c1dc313cd009d2444308d4d97ed70055f399e9a06cf84a01498c498f345" => :yosemite
     sha256 "2eda3f4062198d18fd134ce092a02bed9837246d262af9ead0475986c9870cd4" => :mavericks
