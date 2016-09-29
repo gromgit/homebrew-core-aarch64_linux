b46cfa2acb6ce20da842eccc61dde3acdaffeb7b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 16:17:59 +0100

    hyperscan: update 4.3.1 bottle.

--- Formula/hyperscan.rb
+++ Formula/hyperscan.rb
@@ -6,6 +6,7 @@ class Hyperscan < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c2dc0b11011fc0cea95abf9c23e587d81cead5da9caad417b3d18129bea9affe" => :sierra
     sha256 "98c09016eae2cd8ac7b757efd97dc13b25577d993b5c2a33892ba07060d38834" => :el_capitan
     sha256 "51a0f38350e24d8c45e39504c604e2af3b4cbfa8d3db339864cc84cf19366edb" => :yosemite
     sha256 "10d6c0f07c56d706c8c568963e937fa96bdcd7d2117cadb64046210c8b1235ab" => :mavericks
