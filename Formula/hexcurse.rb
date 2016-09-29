77e98c963f0edfc13f1428e544b45daa8638b0f4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 15:50:47 +0100

    hexcurse: update 1.60.0 bottle.

--- Formula/hexcurse.rb
+++ Formula/hexcurse.rb
@@ -6,6 +6,7 @@ class Hexcurse < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "580efaffc5d8dccb0f4f6532ad5be35e372c6b8d91dfb6d3930aa773c9bf7ea1" => :sierra
     sha256 "ffe690a87522627dc0088c391f7237fc6a3f2aa12fc5a3487c0aa6694905dc4d" => :el_capitan
     sha256 "ef5644e4e96604f6f3bbe802e7824a7fd82e01163d532d0e2be6a93cc6595eab" => :yosemite
     sha256 "fe4f9ddeaa505e9b8554f1118a0298fecf8008c2e4d4569675b349e51feba00b" => :mavericks
