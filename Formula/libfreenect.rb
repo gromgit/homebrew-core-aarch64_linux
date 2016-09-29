a77ebfbd11d67cfab7b9bb0416e84d5780baf717
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 23:45:31 +0100

    libfreenect: update 0.5.5 bottle.

--- Formula/libfreenect.rb
+++ Formula/libfreenect.rb
@@ -7,6 +7,7 @@ class Libfreenect < Formula
 
   bottle do
     cellar :any
+    sha256 "4fd9e1385c35157288875419a1893df94cc08a3a8e888f1fe3a7a8d3d213b737" => :sierra
     sha256 "cb74a0b8ef6efafe3dabb008958e3c7a8b1704a6e1930b50bef61f164c6d2207" => :el_capitan
     sha256 "bb3ee8b4cd9ef4c57432f89a070b0b85e32ff0d8971715eb1de07041ecbd0f10" => :yosemite
     sha256 "f714532e1b21365063746846544a340dac70cf0c5cc877a207dd17284ee100b7" => :mavericks
