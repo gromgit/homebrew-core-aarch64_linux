b5ce5ebf12d3e71152ce34635d9f0113d7b069ea
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 03:54:34 +0100

    docutils: update 0.12 bottle.

--- Formula/docutils.rb
+++ Formula/docutils.rb
@@ -6,6 +6,7 @@ class Docutils < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6254928e32d119dbce4fc37f0a84306942f760696ecae02f8db4654934a238e6" => :sierra
     sha256 "ee05c025462c3a5cb0ffd524e8fa146c1b3918eb4f4c6fd1006ea4bf4ae5db96" => :el_capitan
     sha256 "a0cb7ec024d31a9f6f3bbcde7f07249e966bf9000008f7aee46a0978dcda9cb9" => :yosemite
     sha256 "61fe1f390fdef3c130f46d66123a483ab0d15994420c89884fb0e99143ff8c19" => :mavericks
