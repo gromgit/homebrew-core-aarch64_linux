550cc6328b97e3441d8d816029fbf5279f6f801a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 15:45:41 +0100

    htpdate: update 0.9.1 bottle.

--- Formula/htpdate.rb
+++ Formula/htpdate.rb
@@ -6,6 +6,7 @@ class Htpdate < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c5490cb23845604c332f2e5536639d7cb87ecc75fc3b6f8e0f8e799fc959a320" => :sierra
     sha256 "fbdf082a4cbde49e5a9e5ea28f8f9c76e634d1f4fc79397e70e55306947c37b8" => :el_capitan
     sha256 "56073fc56009dac7f807d436f09732bf4e659d2374bb6a61646dc7f94740daa0" => :yosemite
     sha256 "973fb72128a9fe5e0c4c1aaaf9671b4ea706bc98ba5f96ecd10a2dae46049e57" => :mavericks
