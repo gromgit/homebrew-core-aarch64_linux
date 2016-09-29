4bb5afce88be0d1900c68b04f8641fe27dd13706
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 19:42:28 +0100

    jmxtrans: update 20160706-259 bottle.

--- Formula/jmxtrans.rb
+++ Formula/jmxtrans.rb
@@ -7,6 +7,7 @@ class Jmxtrans < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d6246d3fd3593dd46adca7f66346c9931f55b52f342fba58f243a0d080b59722" => :sierra
     sha256 "a41b37df947adff1a076e1066ca0b8847105f8f8d5ca13c1bed1dbc44aca636f" => :el_capitan
     sha256 "51624098720247a7a54a2a1023578cba4adbb7673c804bfbfbfe83124e37d51e" => :yosemite
     sha256 "c57be793f849944d5fdb0a98ca089dacc61afb9a99ab3e353ef67aa1ff605d9f" => :mavericks
