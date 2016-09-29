35a2c88074f1024646a842779fa946b385b0bb7b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 02:44:48 +0100

    dieharder: update 3.31.1_1 bottle.

--- Formula/dieharder.rb
+++ Formula/dieharder.rb
@@ -7,6 +7,7 @@ class Dieharder < Formula
 
   bottle do
     cellar :any
+    sha256 "6970071bd722a3bbca3892ec4101d06b59461fb9ab3d47aebf71e8ce9d750cd4" => :sierra
     sha256 "ee6c8f5dd87948423016574258755ea42a01f55c38e81bbf6dc3d35d5cf0d733" => :el_capitan
     sha256 "6bf47d1e141f260abcf8926d75c82ff7ae45e582cb1af79fc984a849d9a08f71" => :yosemite
     sha256 "3a1fafb4843d5a16798b9a9375446b52bffb7f9a0516884b444d7e8a4d4adc24" => :mavericks
