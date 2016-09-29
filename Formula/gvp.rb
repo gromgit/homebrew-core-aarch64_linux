464f85b404d7ea8103c6621d1e40dd165dcd0bfe
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 13:16:45 +0100

    gvp: update 0.3.0 bottle.

--- Formula/gvp.rb
+++ Formula/gvp.rb
@@ -6,6 +6,7 @@ class Gvp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2405a1e481ebfafcd4fbfdc2874feacc402b851fafdc69596d1afa120924c157" => :sierra
     sha256 "ddd00ded9d21c3ecfe23e807619d3ab1b3011bc586db0d7d4aa8d5d87e3689c6" => :el_capitan
     sha256 "5e63da6d9c8d065277491db1658fee5c53089f7dd1bf1180e5d9e7172b376cde" => :yosemite
     sha256 "afbe7d649883c750c182976a7c09035fe30f4d56b1b5859c8d214c01334874f7" => :mavericks
