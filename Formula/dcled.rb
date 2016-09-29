3db78637528060c5764423ac6fd71669342fb359
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 01:25:38 +0100

    dcled: update 2.2 bottle.

--- Formula/dcled.rb
+++ Formula/dcled.rb
@@ -6,6 +6,7 @@ class Dcled < Formula
 
   bottle do
     cellar :any
+    sha256 "bfc1532d76b4d37c706d065bc98feb5a3aeff20751a713d7b7efb08c0976fe9e" => :sierra
     sha256 "53d07c9548eaeba12645e944ce92c27a02667758176815220dc4ee2a8945c661" => :el_capitan
     sha256 "2ead7c4eb3c170690890c294936a2d3fc39def2fc332ce4c1da6d17cc8f91b50" => :yosemite
     sha256 "47a0b2e1eba58932936c25726d631d19f0f2a0a7b8872aff9e1d3a83b4e3cfc9" => :mavericks
