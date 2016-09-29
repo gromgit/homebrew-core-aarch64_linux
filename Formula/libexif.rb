918c53bfb65cf5ae0a383c3a03156491cdcd7ef0
Zhiming Wang <zmwangx@gmail.com>
2016-09-29 10:45:53 -0400

    libexif: add test
    Closes #5387.
    
    Signed-off-by: Zhiming Wang <zmwangx@gmail.com>

--- Formula/libexif.rb
+++ Formula/libexif.rb
@@ -23,4 +23,29 @@ class Libexif < Formula
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make", "install"
   end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <stdio.h>
+      #include <libexif/exif-loader.h>
+
+      int main(int argc, char **argv) {
+        ExifLoader *loader = exif_loader_new();
+        ExifData *data;
+        if (loader) {
+          exif_loader_write_file(loader, argv[1]);
+          data = exif_loader_get_data(loader);
+          printf(data ? "Exif data loaded" : "No Exif data");
+        }
+      }
+    EOS
+    flags = %W[
+      -I#{include}
+      -L#{lib}
+      -lexif
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    test_image = test_fixtures("test.jpg")
+    assert_equal "No Exif data", shell_output("./test #{test_image}")
+  end
 end
