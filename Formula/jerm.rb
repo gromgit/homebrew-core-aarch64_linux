397c5cbe204b7bc7556de2268743371b4980becf
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 19:42:27 +0100

    jerm: update 0.8096 bottle.

--- Formula/jerm.rb
+++ Formula/jerm.rb
@@ -7,6 +7,7 @@ class Jerm < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ee9a8a2e559bf9ab82ba413e8741759fed6d59cfe82a063c82b72b81a56cfe5e" => :sierra
     sha256 "5c8409bfdeba7b55199659f4b82b8df9ec2ca8685435703bf1ddff29f9e027e5" => :el_capitan
     sha256 "bce73bc0790565d58c129116833c2bf6dab677c95287036f4b3717a02792da12" => :yosemite
     sha256 "e7a2ed29af497e459175ac4b7bf9d4e0b9a367c653ee3d7798b316a95d8e5cbe" => :mavericks
