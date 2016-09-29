e12b581a41dcc2ce75b80564b9b9a951b3eae6e3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 04:16:01 +0100

    dfix: update 0.3.1 bottle.

--- Formula/dfix.rb
+++ Formula/dfix.rb
@@ -7,6 +7,7 @@ class Dfix < Formula
   head "https://github.com/Hackerpilot/dfix.git", :shallow => false
 
   bottle do
+    sha256 "58c2e5ee2f3fe572a0b07797af26de4d44e9194bf65a2eef89cf46d8d2dc7da4" => :sierra
     sha256 "6b19da6a3db617b0dcce69a9c19b6b212e6f64adaacfe0e7296328c74aa8f398" => :el_capitan
     sha256 "28db097fe0facaa42f4b1a441f89237e1ad2480ecb7d81f56dd6937d586e1974" => :yosemite
     sha256 "186aaedeb0b223858a5cb16e133ef03886fe81ece596e376c111342251842c5e" => :mavericks
