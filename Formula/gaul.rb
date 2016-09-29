c268995f93a737aa06a7d5d3bc164b7517dbb61d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 10:12:56 +0100

    gaul: update 0.1850-0 bottle.

--- Formula/gaul.rb
+++ Formula/gaul.rb
@@ -6,6 +6,7 @@ class Gaul < Formula
 
   bottle do
     cellar :any
+    sha256 "5dcd424881f8395070bf534b8bd480279a17cbf8a5784ba2be7dffdbfbc85f51" => :sierra
     sha256 "0a6fb9c8ae17bb0785cc9c9da0fa0b3bf5fd6ca69b1ef8516b800d0d28d77360" => :el_capitan
     sha256 "8b0cb8b79f456faf4b7a8f9af2c788290b3e2eb1785f120875f2b72b4159fbf5" => :yosemite
     sha256 "2ce7947353b3ea8e9be3925b1e516c92cbcca5602039d91ebe729c6fb96f5a37" => :mavericks
