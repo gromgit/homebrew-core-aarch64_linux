d1494894555f35f32efa94e85ba4413b8b997ccb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 04:47:25 +0100

    fastjar: update 0.94 bottle.

--- Formula/fastjar.rb
+++ Formula/fastjar.rb
@@ -6,6 +6,7 @@ class Fastjar < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5d03ecf7d89b4c9cd3ca25735692b77f55ae7df83bdb4073e013f5361256c689" => :sierra
     sha256 "996937a030b443cee74e1de1945e3199022fc27514cf9925c332ed5d5804c80a" => :el_capitan
     sha256 "07dd91fef374251b87b1a2987089234a3da225b79313afa4d8a7f502d1a51aae" => :yosemite
     sha256 "4e1d46c61723a1babbba0841b9dbde5c33388e107202f6ca292adc23ab7149a3" => :mavericks
