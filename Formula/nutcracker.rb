8c268a7c0ab9f6131e02b3ebd5832950d579ef05
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 16:11:50 +0100

    nutcracker: update 0.4.1 bottle.

--- Formula/nutcracker.rb
+++ Formula/nutcracker.rb
@@ -7,6 +7,7 @@ class Nutcracker < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4987e6e2ef6fffb2fe05a65795cb513d628edda38f0e0ee69cea05159d16a4b5" => :sierra
     sha256 "1f3714c4459185e9002ea87f4fc4a4de429f028df957615ee9876c61a7c89a6d" => :el_capitan
     sha256 "73698710fa026b8585665a9b730626df444dabe6acf118cc4f0c2f57c27e214c" => :yosemite
     sha256 "8e66691c40fe71934bb5eab848c61ef07f8f427774e253c0065eb64cc5410f9b" => :mavericks
