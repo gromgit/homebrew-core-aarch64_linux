d1d4cc915ac73574c5f9d6edf94d636076e28060
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 16:11:49 +0100

    mp3cat: update 0.4 bottle.

--- Formula/mp3cat.rb
+++ Formula/mp3cat.rb
@@ -6,6 +6,7 @@ class Mp3cat < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4ae83c8ee5219135c9ca52b16b8e103b8c9f67c12cdf60e6584353f39276ed7c" => :sierra
     sha256 "c582f29c5c3c05697e4805b75209dbab5797d6a3c62de5b4d21972250bc8d2e9" => :el_capitan
     sha256 "a70d1a1a379c4813f57c32284f13348e36302a61d0c933c2a4fb19342c43be1f" => :yosemite
     sha256 "43053bea923c17bd7a31532cdcaedfcb3681a04aa1b0b66e33defc41c7e00629" => :mavericks
