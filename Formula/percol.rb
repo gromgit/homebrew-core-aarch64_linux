936ea62b6df37217535c5b885957df55d8b4f9d1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 23:52:11 +0100

    percol: update 0.2.1 bottle.

--- Formula/percol.rb
+++ Formula/percol.rb
@@ -7,6 +7,7 @@ class Percol < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "dac0631f61d1fad12ffed033d16c163a237e6d863bf5350971a8305fbd69c171" => :sierra
     sha256 "e0acd43c0270f0277dc69492da9c31e0e819c2b4bd1ca8f23db012ba2e4e3aab" => :el_capitan
     sha256 "8a46e774ad1128721b2b425f11083d6494101834b887a575f9071c006abab887" => :yosemite
     sha256 "a5c8be8d7e307651de4951384ac2603e7ca932bfffbf9434170a597f801b799e" => :mavericks
