657937fab476238dadfd62a28ce2b2b5f6ddd54e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 00:49:13 +0100

    pdftoedn: update 0.34.1 bottle.

--- Formula/pdftoedn.rb
+++ Formula/pdftoedn.rb
@@ -6,6 +6,7 @@ class Pdftoedn < Formula
 
   bottle do
     cellar :any
+    sha256 "04741c63be592ede3c058ede03b215491524f4b8610c201ccae25fe4ffa18367" => :sierra
     sha256 "1db4ccab956227cf5db09e9a925b0373427e3d237e4486661e13d2b1b4bcde1f" => :el_capitan
     sha256 "3e0307c2943aba4c8dc1d849c579f75bcca989b24f1470b8da25817c03b27d50" => :yosemite
     sha256 "08959d8bb3f153e9ab5dd34827387fcea19c167f3ac1dbc5566eb35b638d5a5e" => :mavericks
