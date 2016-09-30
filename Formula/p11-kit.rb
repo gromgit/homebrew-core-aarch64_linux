d338c44bb1963af09be895fab20dd05a49f72dfa
Bryce Glover <RandomDSdevel@gmail.com>
2016-09-30 16:24:17 -0400

    p11-kit: Versioning fixes.
    Moves v0.23.2 of p11-kit to `devel` status and (correctly) gives v0.22.1 of the software `stable` status.
    
    Closes #5434.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/p11-kit.rb
+++ Formula/p11-kit.rb
@@ -1,13 +1,21 @@
 class P11Kit < Formula
   desc "Library to load and enumerate PKCS#11 modules"
   homepage "https://p11-glue.freedesktop.org"
-  url "https://p11-glue.freedesktop.org/releases/p11-kit-0.23.2.tar.gz"
-  sha256 "ba726ea8303c97467a33fca50ee79b7b35212964be808ecf9b145e9042fdfaf0"
+
+  stable do
+    url "https://p11-glue.freedesktop.org/releases/p11-kit-0.22.1.tar.gz"
+    sha256 "ef3a339fcf6aa0e32c8c23f79ba7191e57312be2bda8b24e6d121c2670539a5c"
+  end
 
   bottle do
     sha256 "f8a576f0e0c58aeb43d0973c689d2de5d959febb86082d5f9505661402217946" => :sierra
     sha256 "056f262ed1ed5fa665885f577e5b2463429c255ff2a987c4f2af67f4f23e0a54" => :el_capitan
     sha256 "f82755ab85440b64ec4db85ecaee5c0185ba751a08d693fffd98f8f80c92afb5" => :yosemite
+  end
+
+  devel do
+    url "https://p11-glue.freedesktop.org/releases/p11-kit-0.23.2.tar.gz"
+    sha256 "ba726ea8303c97467a33fca50ee79b7b35212964be808ecf9b145e9042fdfaf0"
   end
 
   depends_on "pkg-config" => :build
