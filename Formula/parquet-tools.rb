3303786c0702cd0fd61edc000816124da36113e2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 00:38:58 +0100

    parquet-tools: update 1.8.1 bottle.

--- Formula/parquet-tools.rb
+++ Formula/parquet-tools.rb
@@ -9,6 +9,7 @@ class ParquetTools < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "38775c26308bfa9cde712927d60d5f89daf54fbef837ba8639db13c4abe0be07" => :sierra
     sha256 "f1382310a6cfd278b0a545c944e32cc6a486b3885ef1f023f4290e0bde4be7bf" => :el_capitan
     sha256 "1780f8fa81ef90becc15d5e6cbbe6994f639a51429347f9c223599518a9d049d" => :yosemite
     sha256 "72c7af52e61a9f93ae80dd0c51f42ff09fc7024b985e785e3b79f866720e4d20" => :mavericks
