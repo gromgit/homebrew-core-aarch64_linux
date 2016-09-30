fbf02fca34467c6b4b3f14f06b7ace222720dcae
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 20:16:57 +0100

    opus-tools: update 0.1.9 bottle.

--- Formula/opus-tools.rb
+++ Formula/opus-tools.rb
@@ -6,6 +6,7 @@ class OpusTools < Formula
 
   bottle do
     cellar :any
+    sha256 "2b0c4de73db22557218c03803b7c27cfb23527f35059aa36240cc12076c95e5a" => :sierra
     sha256 "7bfccafbee68fc61d1a5d8e3ea3d2f99f95876c45ada9570fac5ba08f3916b1a" => :el_capitan
     sha256 "ae51146c28dced90e2a4f0f869bb19a6b442e96727cf655240d3d01f17b51de3" => :yosemite
     sha256 "ea7c44249700b7a2e0d6809f55b9e52b87ece5290aaa9bd4fc73ffd1a30cc38f" => :mavericks
