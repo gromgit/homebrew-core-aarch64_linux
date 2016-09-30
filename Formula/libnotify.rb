92a2d53d2739c0010d221f1cd5321be023c2c429
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 02:50:02 +0100

    libnotify: update 0.7.6 bottle.

--- Formula/libnotify.rb
+++ Formula/libnotify.rb
@@ -5,6 +5,7 @@ class Libnotify < Formula
   sha256 "0ef61ca400d30e28217979bfa0e73a7406b19c32dd76150654ec5b2bdf47d837"
 
   bottle do
+    sha256 "7b9bbf7af44434aca82465bf7f234d4affb50daeae54c65a7758b9814e113ff5" => :sierra
     sha256 "6c0c8e5b8b9e38ff2820b7df7aefb1510d95a5f9dcb31b37ed800da0c737f4e1" => :el_capitan
     sha256 "c0646c9d0c3b53290875df575a3b97fb30c852a70825e048b70475ed1e9e72d5" => :yosemite
     sha256 "e4f657ff6ecaafa2e448c86521330ba3fbc0ef3181b0608808fe32f03f4fb80c" => :mavericks
