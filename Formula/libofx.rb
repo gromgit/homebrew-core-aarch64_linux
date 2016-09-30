8c9d8f2da3f329c154cd1c86d929237d4e3a6a4f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 02:50:03 +0100

    libofx: update 0.9.10 bottle.

--- Formula/libofx.rb
+++ Formula/libofx.rb
@@ -5,6 +5,7 @@ class Libofx < Formula
   sha256 "54e26a4944ef2785087cfd8ed8f187ab9d397d9b92b5acc199dd7d5d095cf695"
 
   bottle do
+    sha256 "f43a3d30d5490038591245675c739613720c50502630cf21574e2de401b5ad8f" => :sierra
     sha256 "51e0d70b279d22394058c04cc4509788047d9d6cdb0ac7ed7a121f2863bddb7b" => :el_capitan
     sha256 "f569247d3c0849ed8ebe0d639fb281f64ce175c6db38241f1c60e5d46dc0cff6" => :yosemite
     sha256 "d2aedd050d47d0ca1274a34cf6ae14f3a2a4c8db65309ab3d46554c9d64ccd2b" => :mavericks
