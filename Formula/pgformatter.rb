b6608d6183d61f433e31f338aee66c844bef7991
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 00:53:32 +0100

    pgformatter: update 1.5 bottle.

--- Formula/pgformatter.rb
+++ Formula/pgformatter.rb
@@ -6,6 +6,7 @@ class Pgformatter < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5c1ae9b379d82e5270978d9bae77674136701ae78eb007a9439aea78aaafdf6c" => :sierra
     sha256 "221f601d5cd69d05c733c9a95654707e231f726ac08f9c4b3faabd38531df3c0" => :el_capitan
     sha256 "2747addee67d2055fee10de760e7c6af4debf119978c63651b3d718b34b9d144" => :yosemite
     sha256 "160b6d71ed83367689e069145d255bbcf9a2f136da48be1278c2969e4b56b5a9" => :mavericks
