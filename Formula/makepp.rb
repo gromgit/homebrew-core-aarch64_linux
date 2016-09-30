6923d67dfd4cc6e9bf91683e990c18e628236626
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 05:22:37 +0100

    makepp: update 2.0 bottle.

--- Formula/makepp.rb
+++ Formula/makepp.rb
@@ -6,6 +6,7 @@ class Makepp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d9244cdf9ca16edf5972aa60783ecfd675c581ba3a9b53339593f1fdc355a0ab" => :sierra
     sha256 "e2d2e0cbb4999b69e9b1de09a75621ad6119f2978b0a86aefd0e63b2ee908203" => :el_capitan
     sha256 "9ccedb5776a953719caa8cb8154a8dea1e633fca632eee9ff3ef286e4539f0e8" => :yosemite
     sha256 "d54e884aac7589f363d2c67920c87861878777a59771cd2457ed86053cf6e6b8" => :mavericks
