c7555358159a129dbf166cf4eae3cb732d89ef3f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 09:11:26 +0100

    micropython: update 1.8.4 bottle.

--- Formula/micropython.rb
+++ Formula/micropython.rb
@@ -7,6 +7,7 @@ class Micropython < Formula
 
   bottle do
     cellar :any
+    sha256 "15d484420ea9077d47646c65f4dbca8aa02f892a60365136ce1a2a98c5040062" => :sierra
     sha256 "94d7600c5c434d700005dbfd7db94482fe5f1adf577c45d54e0968e2125d0d43" => :el_capitan
     sha256 "67e265ff6255c73294802e113b3cf51bc8d9859033297a62728a1637776be3cc" => :yosemite
     sha256 "d4387ba44d795547b8253b9e5131c1cb700a69b50b2b9609452ac93c475b3c4a" => :mavericks
