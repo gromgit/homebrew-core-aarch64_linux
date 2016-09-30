eba874a54c7697c6c2a0c5719af591c0c18606db
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 00:55:14 +0100

    qpdf: update 6.0.0 bottle.

--- Formula/qpdf.rb
+++ Formula/qpdf.rb
@@ -6,6 +6,7 @@ class Qpdf < Formula
 
   bottle do
     cellar :any
+    sha256 "e13fed06dbbb7aae8b6c4c7a32e1975603d18a61203ebb6b15094cd992cf9e16" => :sierra
     sha256 "de523886e15f79209dbe270043dc252ebd2856649ac94f98141f37c3436ed20e" => :el_capitan
     sha256 "d352cf417a9ee038157343f138ff3f341e5aa5d9e91757c3ce88950b4509aba3" => :yosemite
     sha256 "f7059fb9d944230b06ad8ddb34528e4090161ee0a3ddee7068a86046c61d4b04" => :mavericks
