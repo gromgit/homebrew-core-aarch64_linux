d747a6050c7c1b1c4bcf8406edbad1145fa8195c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 16:11:49 +0100

    mp3gain: update 1.5.2 bottle.

--- Formula/mp3gain.rb
+++ Formula/mp3gain.rb
@@ -7,6 +7,7 @@ class Mp3gain < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a9548ef1c648c0aaa4d203726431a5425518a7a2f54df0b6d5e8c92715404e3d" => :sierra
     sha256 "27506d45e11a7da5c9555cb17318cf99802d5a8749ad17ed66b7a4b1465790cd" => :el_capitan
     sha256 "f14681110117c1762ef909dc0b282bb4c23f298c92eefe20d77fab0199cadbaa" => :yosemite
     sha256 "17abc15792bc75fa186c6748f1cb666814334b2cfec11d708bb9431846746809" => :mavericks
