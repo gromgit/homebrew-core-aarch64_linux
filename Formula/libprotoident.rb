4f0599f1ad5dd2008cf0b6d3af6392ab26bb555e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 11:54:10 +0100

    libprotoident: update 2.0.9 bottle.

--- Formula/libprotoident.rb
+++ Formula/libprotoident.rb
@@ -6,6 +6,7 @@ class Libprotoident < Formula
 
   bottle do
     cellar :any
+    sha256 "0e8341bccc9208a52db3037063367190c43fb626e98915e3bec9130c69697806" => :sierra
     sha256 "df7d25c77ac8d178cd46163a1812bbbc69a7b1ca4eac37c399d5b39b584176a5" => :el_capitan
     sha256 "dc01273bfa37c0ca9cd05d2510fe597cea4e90ab038792bb6e2f51885a79e15c" => :yosemite
     sha256 "e314edb9521e0493b3d870fa27f3e2cf1e387a88a23ae48ddea45affc7181ef3" => :mavericks
