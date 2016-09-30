bcbe9f3be703e8166e5a616c26d704d7dc9f3871
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 17:40:22 +0100

    open-cobol: update 1.1 bottle.

--- Formula/open-cobol.rb
+++ Formula/open-cobol.rb
@@ -5,6 +5,7 @@ class OpenCobol < Formula
   sha256 "6ae7c02eb8622c4ad55097990e9b1688a151254407943f246631d02655aec320"
 
   bottle do
+    sha256 "5803c69bb9b04d1fdebd4aa8a3e2b211a97ec3e8bc8e50f3918635d367d9d1bf" => :sierra
     sha256 "31b5e3af55285483f2e73463aa58cf35afae5a5ee675b7622703d62c34bbe529" => :el_capitan
     sha256 "a36ae3bbf6eabe5b5119f489b41d26a0bb03ae0aa6935a597ebeb046eea32da7" => :yosemite
     sha256 "64e3bb2cd22d0b95e4b143191d7dc2c3b245edef0f9a61e0cee366dc96723a77" => :mavericks
