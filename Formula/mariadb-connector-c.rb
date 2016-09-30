ea15d5a1243a8a090da396ea67928aaf65096354
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 05:22:38 +0100

    mariadb-connector-c: update 2.2.2 bottle.

--- Formula/mariadb-connector-c.rb
+++ Formula/mariadb-connector-c.rb
@@ -5,6 +5,7 @@ class MariadbConnectorC < Formula
   sha256 "93f56ad9f08bbaf0da8ef03bc96f7093c426ae40dede60575d485e1b99e6406b"
 
   bottle do
+    sha256 "2efc882d3518dc6932d711b62d5d65a6b2151f7e44d2e6a15917ca4f1d3ce897" => :sierra
     sha256 "fe5606d7c8ac822398f01f2b2b9045c8d6b3a46df8b862e1fe151aa17619d3b2" => :el_capitan
     sha256 "6b5b743616a46585a9bfe1505a94addccf24802090094a9dcd791852bf8d566c" => :yosemite
     sha256 "14f9440a48d0035a8e77ff8e7b260b50c89326453041ec77d0cc38febbf4f5cc" => :mavericks
