a9c9234b1101c00dde994af4165e055a0c2952b2
Alex Alonso <alex4551@gmail.com>
2016-09-30 11:31:40 +0800

    vim 8.0.0019
    update vim version 8.0.0019
    
    Closes #5361.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi \"workalike\" with many additional features"
   homepage "http://www.vim.org/"
   # *** Vim should be updated no more than once every 7 days ***
-  url "https://github.com/vim/vim/archive/v8.0.0013.tar.gz"
-  sha256 "46b509cd0c4d085cfec60b2e8d30daca568908c4d7dbcaa2af586ed897cc8d4b"
+  url "https://github.com/vim/vim/archive/v8.0.0019.tar.gz"
+  sha256 "f13a76504b2f976228edd7169d592870a6af45614d82ae2a9c312fc17ab3d4ad"
   head "https://github.com/vim/vim.git"
 
   bottle do
