d327394e452fdc91092fb8f2320765c42d17dbc1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 11:54:10 +0100

    libpuzzle: update 0.11 bottle.

--- Formula/libpuzzle.rb
+++ Formula/libpuzzle.rb
@@ -6,7 +6,8 @@ class Libpuzzle < Formula
 
   bottle do
     cellar :any
-    revision 2
+    rebuild 2
+    sha256 "62452be0513886b00ad766fc6c444f69af8a70d89948a65b3fe201c12383f536" => :sierra
     sha256 "0768fc24347a5e5e061722175cae535b6e295c28302d98ad3e03dc9f79a32bf0" => :el_capitan
     sha256 "d8f7de77378d0fa29e34876ccc8def7f8e60e6564a1c17dae77f4c32ebd8ae5a" => :yosemite
     sha256 "ed3d860aa40203a73921fc7f6919828599a28fb39e2d95f0c963ae4eb5c7811b" => :mavericks
