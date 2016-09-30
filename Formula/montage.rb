49296daf372ebe7c0d2cfd60985666eac09dd693
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 15:37:56 +0100

    montage: update 4.0 bottle.

--- Formula/montage.rb
+++ Formula/montage.rb
@@ -6,6 +6,7 @@ class Montage < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "70b1769202095b84da05fe00a1934d8e8da3fd08b7ddb7135937f4cdc0107f07" => :sierra
     sha256 "503c3e946aa0d8f277b5e4a5aab75086d5c895551fa679a3129183b95f89b236" => :el_capitan
     sha256 "7f9bb66eff925f20099f11ee247e4ba4c8b4821b74c7f2a3efd93d474e9a1b3f" => :yosemite
     sha256 "30e68dcecc111af10a65b1edd33a0142457b2f2064e1bce45e33a6d3d11539d4" => :mavericks
