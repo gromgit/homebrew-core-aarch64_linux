6016e36992a77bf8be386cccafd613d02f5771a4
ilovezfs <ilovezfs@icloud.com>
2016-09-30 13:44:40 -0700

    calabash 1.1.12-96
    Closes #5439.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/calabash.rb
+++ Formula/calabash.rb
@@ -1,8 +1,8 @@
 class Calabash < Formula
   desc "XProc (XML Pipeline Language) implementation"
   homepage "http://xmlcalabash.com"
-  url "https://github.com/ndw/xmlcalabash1/releases/download/1.1.11-96/xmlcalabash-1.1.11-96.zip"
-  sha256 "6eba2a820dfbad5ddfd8b3b34ee60201c5871f99e5257e6cda8f0afdccbdc591"
+  url "https://github.com/ndw/xmlcalabash1/releases/download/1.1.12-96/xmlcalabash-1.1.12-96.zip"
+  sha256 "ad52d7f7c7f75c0adc41ff73fd78ef52e853deeeb2ca4363b1f29c5c4546a291"
 
   bottle :unneeded
 
