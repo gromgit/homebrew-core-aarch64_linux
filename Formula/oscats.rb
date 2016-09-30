48a19ea209129359817c00267c256cf26775b600
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 20:16:58 +0100

    oscats: update 0.6_1 bottle.

--- Formula/oscats.rb
+++ Formula/oscats.rb
@@ -7,6 +7,7 @@ class Oscats < Formula
 
   bottle do
     cellar :any
+    sha256 "d08faef8fc25411bfda3c5727a3393a492052cafb684038cf80e951f5949b44c" => :sierra
     sha256 "0c5dc5fdae72d08c43efe3d1053e7409d998054fa1a14431fbe192cf2b77d1f6" => :el_capitan
     sha256 "859a82a1f1211bae6887cdc53c09592a11996964f892048e05c1775cecfad0ce" => :yosemite
     sha256 "4c6ebccd2065ad9f72c9611521cd51b9e529a54c199e9937c86bc77875f5637d" => :mavericks
