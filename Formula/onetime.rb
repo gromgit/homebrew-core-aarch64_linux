7139d9599d7fe85f084511e3ca58cc1adfa4204c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 17:40:21 +0100

    onetime: update 1.81 bottle.

--- Formula/onetime.rb
+++ Formula/onetime.rb
@@ -22,7 +22,8 @@ class Onetime < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 2
+    rebuild 2
+    sha256 "37cf417291ce11a6313bde209490f3cce0ca5f47b17579e28720568088de89fa" => :sierra
     sha256 "4d27502d9a4b8d257182dcaf99b05121033352928c3716a1ebe932a24276e73a" => :el_capitan
     sha256 "561f129baa60ba8aa08f47130a35f531fdd7ddda80c3e0636bd39c96c3d06930" => :yosemite
     sha256 "31698cc41c95bdb23f340f2641124826f8b5324a69ce338146e7c01800646fa5" => :mavericks
