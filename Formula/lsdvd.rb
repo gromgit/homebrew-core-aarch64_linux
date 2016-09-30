8718f15cebdebea6cf8d71043056efc7a9622591
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 11:54:11 +0100

    lsdvd: update 0.16 bottle.

--- Formula/lsdvd.rb
+++ Formula/lsdvd.rb
@@ -6,6 +6,7 @@ class Lsdvd < Formula
 
   bottle do
     cellar :any
+    sha256 "24d7f2b86648de4b7477d41b0a2adfefa870ac2a973f4a31e16bb88a88fc3904" => :sierra
     sha256 "29aa32a4b1b1c327aaea8b568f625c0c8e49723a3397d722df927e0b1b4493d7" => :el_capitan
     sha256 "9af38820e4747c002f38be75d31577533980ca731f12cffc2b9f41c6a37e1a3d" => :yosemite
     sha256 "b00f07a2636d1d73ab1b3456843d35de78e01f98f9ac818c4f0d70a88893253b" => :mavericks
