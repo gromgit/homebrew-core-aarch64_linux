7bc27b9d27bffbe8fbf589ec80a4812617c16000
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 11:57:17 +0100

    md5deep: update 4.4 bottle.

--- Formula/md5deep.rb
+++ Formula/md5deep.rb
@@ -7,6 +7,7 @@ class Md5deep < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4ee90230c25f9872541d3f895fbe010765dd2e5449e56a0987e3652f89014916" => :sierra
     sha256 "986dad46d2945aac775eb625e41b0236f2413b3924244d5e9aba445994c38687" => :el_capitan
     sha256 "227b8b8e4f4dd71972cd02062faefef90515b44ef5c3ce55f5c665cf679a26d1" => :yosemite
     sha256 "1bacd45d420975ff8b90d633e361b54c7f6a14776a41f175313360d31fb03ba4" => :mavericks
