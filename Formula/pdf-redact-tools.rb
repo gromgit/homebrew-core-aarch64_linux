1bb9876e42ca95a28cec6e7add6bd783458272bb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 00:47:06 +0100

    pdf-redact-tools: update 0.1.1 bottle.

--- Formula/pdf-redact-tools.rb
+++ Formula/pdf-redact-tools.rb
@@ -7,6 +7,7 @@ class PdfRedactTools < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "307a77850976866c2e12a782bff0b42983a75b722411420709d7f624c2cfba4f" => :sierra
     sha256 "660fd61a0bb83272ec2d5e818435484ff53d5a88f7bc619fcccafd5c5e855203" => :el_capitan
     sha256 "1bfb6823d1c7e94f37cf86a3d30bcd777873b69f1a58556dcb7f3d2d25cd4123" => :yosemite
     sha256 "13ef89b6f2f20f22513412f4a4b7f815fe297276a451097228df21bb8835e610" => :mavericks
