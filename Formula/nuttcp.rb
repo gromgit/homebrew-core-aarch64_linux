5569f3c01090bdf0eeeb165039a918afbfbd0682
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 16:51:33 +0100

    nuttcp: update 6.1.2 bottle.

--- Formula/nuttcp.rb
+++ Formula/nuttcp.rb
@@ -6,6 +6,7 @@ class Nuttcp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "478522cb3e50128eb9b7b09d519bfaa553ae05025d1708a3d5d0c7b76dcbac8a" => :sierra
     sha256 "7fe34e31cd50393b055dde913785931696bb332bc1b5948720d7817e1247151c" => :el_capitan
     sha256 "18cfa8910325006d83e53c94a845d611704ce882f3f48a5edd2ebc6e75def200" => :yosemite
     sha256 "57c31d24a43302c4fb1b08f6a17a05da60c3486a52481b0580c3e1f5d75c7eaa" => :mavericks
