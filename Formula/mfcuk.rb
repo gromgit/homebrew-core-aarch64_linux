8b7ddf1847da34331112676a1376251d09e0ddb5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 09:11:26 +0100

    mfcuk: update 0.3.8 bottle.

--- Formula/mfcuk.rb
+++ Formula/mfcuk.rb
@@ -6,6 +6,7 @@ class Mfcuk < Formula
 
   bottle do
     cellar :any
+    sha256 "a4ae4d6f6cdec9dd28c52ff04da99b9de86c79a19c6e182ef3a557f48dde0741" => :sierra
     sha256 "8b329dbd3feb25bc4f04f40451cf25e832395721a5184eb4ee287366aaa06334" => :el_capitan
     sha256 "bdf696192e1a660b2fa1ad58498bdce941b1d45c4b51847b95427f41debd4c2d" => :yosemite
     sha256 "1394e4115a4e65abacc23e81659fd77475d6039ac39979cea7fd335ee5cf09e6" => :mavericks
