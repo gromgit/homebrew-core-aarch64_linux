f860246adadf8796b4d28ee214bd8884542d0f87
Jeremy Mailen <jeremy.mailen@gmail.com>
2016-09-30 00:20:54 -0700

    kotlin 1.0.4 (#5405)

--- Formula/kotlin.rb
+++ Formula/kotlin.rb
@@ -1,8 +1,8 @@
 class Kotlin < Formula
   desc "Statically typed programming language for the JVM"
   homepage "https://kotlinlang.org/"
-  url "https://github.com/JetBrains/kotlin/releases/download/v1.0.3/kotlin-compiler-1.0.3.zip"
-  sha256 "37615f1d63e8500cd33c7f3e60b715263f65189d6d8f25defba78968c896dc97"
+  url "https://github.com/JetBrains/kotlin/releases/download/v1.0.4/kotlin-compiler-1.0.4.zip"
+  sha256 "cf665121a7075da6ffca3e0572c23faa9bd64a368c8c9fc6ba3f4242deb44572"
 
   bottle :unneeded
 
