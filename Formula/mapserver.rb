ac0a55ec9df3ca0b4d22a60b6327003c91bd4fd8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 05:22:38 +0100

    mapserver: update 6.4.3 bottle.

--- Formula/mapserver.rb
+++ Formula/mapserver.rb
@@ -6,6 +6,7 @@ class Mapserver < Formula
 
   bottle do
     cellar :any
+    sha256 "01fffabbf7d8f94f450275e513c66d64323d40a07ae3e52012017c1408de31d5" => :sierra
     sha256 "76b78fec6ddef503c68bc791259dba5e19afec1a0bca7bb0e9c70f06e7e01e33" => :el_capitan
     sha256 "fa018ad2b14492f50e0b6a4ed50212201c016bb0f35baf81031062fbe5a337e2" => :yosemite
     sha256 "a2c6ecfd38837a5a9287fdb0fb729ee4acbfb1d2df5a5e47dff8905cb982e237" => :mavericks
