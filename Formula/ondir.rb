d0dbddf00d5d46dd04c9454ea3e9fcacdafeb120
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 17:40:21 +0100

    ondir: update 0.2.3 bottle.

--- Formula/ondir.rb
+++ Formula/ondir.rb
@@ -7,6 +7,7 @@ class Ondir < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "90e85060a76337368083c889379b71cda5994ab163b73337050819472f41800c" => :sierra
     sha256 "8d841a2a8b98a512265dc05deb3ea74e7458a4d5412da786f595c31420b7fadd" => :el_capitan
     sha256 "3d7b419d963bcd2be6d04cb3f666c8c58866f9556251f6efcb2f0b6abcad5902" => :yosemite
     sha256 "a994efec34c5c9edd1b014959b3fe1a1a95c1d9ece14c5d7d7c51c2b421c7a11" => :mavericks
