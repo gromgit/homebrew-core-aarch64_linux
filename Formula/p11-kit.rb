46be01e503df1e30dba395bffd87d2619e117748
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 03:44:52 +0100

    p11-kit: add 0.23.2 bottle.

--- Formula/p11-kit.rb
+++ Formula/p11-kit.rb
@@ -4,6 +4,12 @@ class P11Kit < Formula
   url "https://p11-glue.freedesktop.org/releases/p11-kit-0.23.2.tar.gz"
   sha256 "ba726ea8303c97467a33fca50ee79b7b35212964be808ecf9b145e9042fdfaf0"
 
+  bottle do
+    sha256 "f8a576f0e0c58aeb43d0973c689d2de5d959febb86082d5f9505661402217946" => :sierra
+    sha256 "056f262ed1ed5fa665885f577e5b2463429c255ff2a987c4f2af67f4f23e0a54" => :el_capitan
+    sha256 "f82755ab85440b64ec4db85ecaee5c0185ba751a08d693fffd98f8f80c92afb5" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libffi"
 
