8a72a67e4123eb8bc1e51b713a6d61c3336f810b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 00:49:13 +0100

    pdftohtml: update 0.40a bottle.

--- Formula/pdftohtml.rb
+++ Formula/pdftohtml.rb
@@ -6,6 +6,7 @@ class Pdftohtml < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e896f26ac73f3a272aa17ee1815f3b173b94fa59680a8db3c0a814865da86afc" => :sierra
     sha256 "622c66d77a35af1bbf94ee35ecba358d8868e187f664c68da729da0d3bead266" => :el_capitan
     sha256 "9d222cce9a02a311248083aead8e4b55e9ac98ce98035a13d021b48d05eb5119" => :yosemite
     sha256 "9dc35272d91c790627d21a93ca058c038ab660b9a6e619f35758a10c02e7dea9" => :mavericks
