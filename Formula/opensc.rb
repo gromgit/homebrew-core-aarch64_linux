ce5968c99743b2e9b093294ae142b5536c08fde7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 00:31:56 +0100

    opensc: update 0.16.0 bottle.

--- Formula/opensc.rb
+++ Formula/opensc.rb
@@ -6,6 +6,7 @@ class Opensc < Formula
   head "https://github.com/OpenSC/OpenSC.git"
 
   bottle do
+    sha256 "a0885f1ba63438d600212f1adf6c1d03bbc7355fc72778d8a21f0c877eaf97be" => :sierra
     sha256 "bf69be51f29b45bd0c5f862560748d30f7d8661002b171a03e78823c73c5eeae" => :el_capitan
     sha256 "1c76d44ec875c6d622a0f6d4ae1f873adb75a789afbe0f2ce3a96231dd3dafd6" => :yosemite
     sha256 "8ef4c4c62042d5db9c6454a1fb050e79ce8ff0081be44483ce2b8b909d701af3" => :mavericks
