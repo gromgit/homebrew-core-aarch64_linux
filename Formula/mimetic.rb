18f1b39d1ef423ea8570ad3308666cdc8fecc79e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 10:05:06 +0100

    mimetic: update 0.9.8 bottle.

--- Formula/mimetic.rb
+++ Formula/mimetic.rb
@@ -6,6 +6,7 @@ class Mimetic < Formula
 
   bottle do
     cellar :any
+    sha256 "8ed48d215ceee3cfd34faef45e460ca022e690b7bc787fdb91d9e84e1e71863b" => :sierra
     sha256 "d836b7b260b8705617f0a7e49ad674c0e2428810fa6e8228913253e4e4e2e873" => :el_capitan
     sha256 "0d3f1d86b2c73efdfad1e3c65e1cb9e0257c75d24cc46133730c2deb0b710319" => :yosemite
     sha256 "bc076f799788cbb59f7c822d77a5693f7fbd1c11eb5cc3a6388bf11921b2e749" => :mavericks
