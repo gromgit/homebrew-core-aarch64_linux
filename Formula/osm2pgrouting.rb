bb6bf42296f7bf5eb364c8c83dec64d9a9a32a35
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 20:16:58 +0100

    osm2pgrouting: update 2.0.0 bottle.

--- Formula/osm2pgrouting.rb
+++ Formula/osm2pgrouting.rb
@@ -7,6 +7,7 @@ class Osm2pgrouting < Formula
 
   bottle do
     cellar :any
+    sha256 "cc4bad23a3c966466e516e1c2818145d6702c71f57d957cc17befe7bf2a983b0" => :sierra
     sha256 "e2eeea2392b2febb64ad5beb1e974ba10f9b38012b6d41edb9111b6cc3e19911" => :el_capitan
     sha256 "24a7fbe3a7079145f76b8e9f70c3f1fd02bd82e4324ef782950d3f98678530c8" => :yosemite
     sha256 "6fced08cc54a523a0923a7091b735c45becc63d51ca16775b381fd455150a124" => :mavericks
