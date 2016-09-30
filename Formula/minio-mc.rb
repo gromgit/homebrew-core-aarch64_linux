873ea4d02ab90e02ba6d763556cb75fc393924b9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 11:20:54 +0100

    minio-mc: update 20160821030249 bottle.

--- Formula/minio-mc.rb
+++ Formula/minio-mc.rb
@@ -8,6 +8,7 @@ class MinioMc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8e95a7efe7ac147d3abc7dd97b0c16d815b9736bda35c9d0bf37b5a291172eed" => :sierra
     sha256 "3630fecfa0ee90f393a70ddb7a3af8e38450d548e61ba9f6193283726cf7f27a" => :el_capitan
     sha256 "8b8edb248da6713fd05459d053121459ee95ca3b7ccc3b43bdee95a4c4113c02" => :yosemite
     sha256 "5e1d16de0d9854d1f49a732d4f29ae69bba7ebd5af5f74c39879bf17a7597484" => :mavericks
