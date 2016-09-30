49ab5c7e9941640ce046efc96210026a45c3eb56
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 17:08:25 +0100

    oggz: update 1.1.1 bottle.

--- Formula/oggz.rb
+++ Formula/oggz.rb
@@ -6,6 +6,7 @@ class Oggz < Formula
 
   bottle do
     cellar :any
+    sha256 "a0fad22ba18930be45c7226f2db0fe8b39c988c84c392807ddc75e2d40b3a9ad" => :sierra
     sha256 "4c1819dbc134981faf5e2e03dc69d210deb8dabd59b71969c1f479fa32322635" => :el_capitan
     sha256 "c6076111f111c5d77dc608bcb4892f10dffb84e5b4f5ebdfba311ec332fa6623" => :yosemite
     sha256 "a3aa5e741dd3e7a9aebb65748f80f45947549a79915b68161a79f12cb37b4b12" => :mavericks
