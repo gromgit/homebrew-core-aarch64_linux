4655a0e69dce42406492ad88312b54ea7e5655ec
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 11:54:11 +0100

    lpc21isp: update 1.97 bottle.

--- Formula/lpc21isp.rb
+++ Formula/lpc21isp.rb
@@ -7,6 +7,7 @@ class Lpc21isp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "68c3756fd99268814cfdc861e971d1201bac42bf5b922ab37119fcb082c86a1c" => :sierra
     sha256 "c12b33d514be2490a3a5bb9d3c1f8468e7e24d13eee0a636a9d067f486af59fc" => :el_capitan
     sha256 "10deab3f8de3cb88b27ea38344ba7c641b758faf45f9a247f3fca968f6db456b" => :yosemite
     sha256 "5631ccbccd2bb128a1592399a558cbb837d63eb9b1ac2d8187c34fb42064b226" => :mavericks
