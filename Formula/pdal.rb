9e89edcc8bd7d5530e0dd14f1d2d956c234685c4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 00:47:06 +0100

    pdal: update 1.1.0 bottle.

--- Formula/pdal.rb
+++ Formula/pdal.rb
@@ -6,6 +6,7 @@ class Pdal < Formula
   head "https://github.com/PDAL/PDAL.git"
 
   bottle do
+    sha256 "4f0596b4cfeea17cf857777cdf60772708c54a6669f85a89c5548ba6b3309a76" => :sierra
     sha256 "21dc2f8487f310e9ccafbb625639c2fb795ff915b5ace70272c1f101760d05da" => :el_capitan
     sha256 "7b7d7a29d450d3e6f4a8abce3b3ac24900419d753e3b64726d4991fe6e0219b7" => :yosemite
     sha256 "cd3a166ed19422b0cc9eba5de4e37031cc8c1cfe29973373c54d48e72e2f68b2" => :mavericks
