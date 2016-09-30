3a671b61de535fd7e3ed1e251fe837ab9c878c7d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 00:53:32 +0100

    pgpdump: update 0.31 bottle.

--- Formula/pgpdump.rb
+++ Formula/pgpdump.rb
@@ -7,6 +7,7 @@ class Pgpdump < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8925b858ba5c77df3c982d318a047936c211578b05c9c6140f11f869f4ef1c3f" => :sierra
     sha256 "78e39ebbde35347ccdf9f552cba605593b4a76511ef25dc147fdf63f57ff96b6" => :el_capitan
     sha256 "e526cce3b8ac5cc687f5a87feb2f3d8828255f4e63cf8f225e56439df549a25e" => :yosemite
     sha256 "27a463ac9a015d1484508f2d625d1e5f4349e79b8331f8e9f78c647f4964da9e" => :mavericks
