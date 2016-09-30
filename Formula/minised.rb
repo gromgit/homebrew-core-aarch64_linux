907dfffb1a7b3f4cf7458b16c9be0350a22d2850
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 11:20:54 +0100

    minised: update 1.15 bottle.

--- Formula/minised.rb
+++ Formula/minised.rb
@@ -6,7 +6,8 @@ class Minised < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "c0a44653ebb7cf8f795fbb96d126abf1f80d5b2bb38a2d8d998dee1b7997e019" => :sierra
     sha256 "4f33f6d39c9190899cf04857f70481ffd57996daf5001cad661ae0ea7f002a88" => :el_capitan
     sha256 "d169d87a77fe06c1190065e502e84fc3f3b3714cdc98a1235c78033a41e6a292" => :yosemite
     sha256 "505d4a7dcb7deeef34344f72b7c7801f90e2c38393add6e2bc41a6434c3fd899" => :mavericks
