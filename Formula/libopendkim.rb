5c6c0ae6f4f5db27062fd5b42a96ec484fd00d4f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 03:21:04 +0100

    libopendkim: update 2.10.3 bottle.

--- Formula/libopendkim.rb
+++ Formula/libopendkim.rb
@@ -5,7 +5,8 @@ class Libopendkim < Formula
   sha256 "43a0ba57bf942095fe159d0748d8933c6b1dd1117caf0273fa9a0003215e681b"
 
   bottle do
-    revision 1
+    rebuild 1
+    sha256 "1c97cec7533c8974c4699854f8463ee309277e246b8bf8e9b66e490f54013007" => :sierra
     sha256 "6b5052b456871186bc251a2e9551c87ca53c501bf162f104c4841fa07b4f2458" => :el_capitan
     sha256 "cf601de122898a144b1c42c72980f1a19688c718638df2ab49735c65a9beb1ce" => :yosemite
     sha256 "d1399f0f0bf8f13aee75557d516b7e09cc31155c3b72da6159e13f25ff94cbd8" => :mavericks
