cda525d64e2b940b659bb29044932e83d09ae512
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 11:57:17 +0100

    mboxgrep: update 0.7.9 bottle.

--- Formula/mboxgrep.rb
+++ Formula/mboxgrep.rb
@@ -6,6 +6,7 @@ class Mboxgrep < Formula
 
   bottle do
     cellar :any
+    sha256 "44a294d075cb08e577c4d1c1e45c222bea93f3c8488f9bed54ded5b36797f536" => :sierra
     sha256 "ecc1d1a83a7ffbc3414feb24c970b89509737f7d1de5c0d8dbd71ba55e008220" => :el_capitan
     sha256 "08cbb574005db6e34cc191ae4f46670ca35af252e4e6bbc2041a92e32397bc49" => :yosemite
     sha256 "bb5cefa83e2fb8b37dac858f3119aedb338b29e0eb2715cbff08b4644689ad86" => :mavericks
