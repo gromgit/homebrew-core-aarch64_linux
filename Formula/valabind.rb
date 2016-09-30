36443d1b0c959774e024dc11710c0a085426b43f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 17:14:06 +0100

    valabind: update 0.10.0_2 bottle.

--- Formula/valabind.rb
+++ Formula/valabind.rb
@@ -9,10 +9,9 @@ class Valabind < Formula
 
   bottle do
     cellar :any
-    sha256 "f5ab08ee268c72de90001029bd89e95c1f15dcdb4336e5b4e3629e952158b908" => :sierra
-    sha256 "41b55e5323f21bf0c8482d1a38bf71c06e1cf27728f157fa4a918a1895d47749" => :el_capitan
-    sha256 "758b949e314db82f2a1d66d22ff05628733a816913ad7baeaed44158e18372a0" => :yosemite
-    sha256 "92a9c255be4f890f40db24b26e261c1df320cd74dbada405d2cd2fc2ac177078" => :mavericks
+    sha256 "c0e12dc929404794cc14061bb410ed8f156607ec72722d0d1dc564b48d6e49db" => :sierra
+    sha256 "76dd86a5634171b12cb63802cc7d6d944cd5307982975744dfa96d7babe0fc32" => :el_capitan
+    sha256 "57c5f65f392082c54cfa7f3fee715a4bf767892d102ea2bf931d14eb11dc1dcf" => :yosemite
   end
 
   depends_on "pkg-config" => :build
