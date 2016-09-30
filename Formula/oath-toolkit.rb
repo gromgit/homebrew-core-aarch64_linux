4e00b4e8242f5c4e4aeecc7892788b10962b6077
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 16:51:33 +0100

    oath-toolkit: update 2.6.1 bottle.

--- Formula/oath-toolkit.rb
+++ Formula/oath-toolkit.rb
@@ -6,6 +6,7 @@ class OathToolkit < Formula
   sha256 "9c57831907bc26eadcdf90ba1827d0bd962dd1f737362e817a1dd6d6ec036f79"
 
   bottle do
+    sha256 "b9e174a86ba5d8801e8c13d5e6629e3ee1133236e4839f1a150c18d7142f7db6" => :sierra
     sha256 "ec5da5bee84826207290f09bd74d913e2e99c937cb70104e3ab818251c21f794" => :el_capitan
     sha256 "e333157196ce196c998cc46e2dec0a73fd3b49969dda9262effb8a565b591f1b" => :yosemite
     sha256 "e082a0eb2ffa11d107574d72197a22208e71bdfa9e8993187933bffb89b8b603" => :mavericks
