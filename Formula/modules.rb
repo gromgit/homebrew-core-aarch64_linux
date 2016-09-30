c46b8a1fa99950a0049a1dce27f2023689b7f5a0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 23:58:47 +0100

    modules: update 3.2.10 bottle.

--- Formula/modules.rb
+++ Formula/modules.rb
@@ -5,6 +5,7 @@ class Modules < Formula
   sha256 "e8403492a8d57ace6485813ad6cdaafe0a735b7d93b9435553a8d11d3fdd29a2"
 
   bottle do
+    sha256 "828fb83b2dd8da143e64c87e7c93455c93a8cdc5ee8357b442cb20ab64ac6951" => :sierra
     sha256 "a33a62060774cb6bf433653771cd5069a308ba6fe86daceeee044f4e5dd6a745" => :el_capitan
     sha256 "62203b2abd9c20f31dd3e29da16bc0c27bc404b68beaa8f0bdce491c6f261e89" => :yosemite
     sha256 "d46c87889814f42b07e22b634061a899baac9a49191cb049c8eb3c91ad3650bc" => :mavericks
