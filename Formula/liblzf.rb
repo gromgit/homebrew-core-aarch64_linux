399698df9843290fffe4e45e5788a9fe5123e0f1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 01:52:19 +0100

    liblzf: update 3.6 bottle.

--- Formula/liblzf.rb
+++ Formula/liblzf.rb
@@ -6,6 +6,7 @@ class Liblzf < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b5871cec84c0cb00b6a44fdce75ee519b243d3ea1f048b5634082279bf9234ed" => :sierra
     sha256 "1319038c7de754dd38b22ec45b616419b60a0a8c19072374da01f5ee48f9e8c8" => :el_capitan
     sha256 "1d8f96a8336407a1dd20adb305d6ecd7b1d534c0c2e9015596cb4259ea368eb1" => :yosemite
     sha256 "025ca90ede89fa17407e1aec34f3a7cf3d91e414c2d629401c3b877e91c56661" => :mavericks
