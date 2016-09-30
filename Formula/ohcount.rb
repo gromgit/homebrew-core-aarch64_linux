eff5bfb2a2f3839bae926ca40b4c94e1f26649df
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 17:08:25 +0100

    ohcount: update 3.0.0 bottle.

--- Formula/ohcount.rb
+++ Formula/ohcount.rb
@@ -6,6 +6,7 @@ class Ohcount < Formula
 
   bottle do
     cellar :any
+    sha256 "b3d4be11858ec755a35d97259c145a76d64ee2e3c0e776cc81578a323b549399" => :sierra
     sha256 "4c6ca73681e204052237392b9c931e46fcdd599c6605bfc7861e3e91dce9ceac" => :el_capitan
     sha256 "08d9df78da1afb3654e96fa142db9eb1981dbbc14861db522dae6e8cc08791e4" => :yosemite
     sha256 "055b2eb9460b1723bcb8a0f215ddda35750ce6d9b9c3cd0bce75d4e9584f0b62" => :mavericks
