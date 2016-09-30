b5ff653d009f768c13c41dc9a64ef54922550ab4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 03:21:04 +0100

    libpoker-eval: update 138.0 bottle.

--- Formula/libpoker-eval.rb
+++ Formula/libpoker-eval.rb
@@ -6,7 +6,8 @@ class LibpokerEval < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "5216cd33d433fd9212ed14d6fffec593c7106226547c1555344604186e7aafc6" => :sierra
     sha256 "67b105600a8e29ed2d38421bc27340ff6e9092806f6458f0ddd6a27de0bcfb9c" => :el_capitan
     sha256 "b15086546ac1ac0310e3113231bfcc2c9de0d23474be8a1a1b4663e6bc8f713f" => :yosemite
     sha256 "9bbfb3886a4e530455dbf53581aecd0df8c86a2f80a444692441449c30f76d92" => :mavericks
