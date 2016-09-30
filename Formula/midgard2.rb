ed0d28abe7591fa2d738c730066baf7292dda27b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 09:11:27 +0100

    midgard2: update 12.09_1 bottle.

--- Formula/midgard2.rb
+++ Formula/midgard2.rb
@@ -6,6 +6,7 @@ class Midgard2 < Formula
   revision 1
 
   bottle do
+    sha256 "5c522e6151422dcc32f28f276f8315b66aab03c8147ea85f8f4b5f1700a664d2" => :sierra
     sha256 "e9c17c300e476ee15ca5d3ccc34c70715da56a8e7be41a7e0638d3dfbbf49113" => :el_capitan
     sha256 "b1d949df0056f3855adfdd4542825fbffa13043e4666516662b3e69f81c24e2b" => :yosemite
     sha256 "8cb700e06f34ca0a08745d42f53dc2b451eb7a739105dda3aade4db2b03a9a3f" => :mavericks
