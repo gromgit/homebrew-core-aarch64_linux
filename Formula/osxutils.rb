30ac38ea877fcabaf58762a77217791f2042306e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 20:38:26 +0100

    osxutils: update 1.8.2 bottle.

--- Formula/osxutils.rb
+++ Formula/osxutils.rb
@@ -7,6 +7,7 @@ class Osxutils < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "576127145840dc6ffa7af17adeef36f61de6a9bafcb17b80286109103729cf41" => :sierra
     sha256 "8a3039916a2cc607a98f5d4576b534e341f7823b7b16bdcfa66fca487379f366" => :el_capitan
     sha256 "86cee8409262fdda5d6634a86a29fe2d0fcee537baa8c9696de3a8abd27c2aa8" => :yosemite
     sha256 "91808d79c75537c563ee9a36b45e21703fcc4377d6c6ea7e7215f5ad9b0aa605" => :mavericks
