f48ab7b32129ac6ec9f97be09309990f09bb7070
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 16:51:33 +0100

    ocp: update 0.1.21 bottle.

--- Formula/ocp.rb
+++ Formula/ocp.rb
@@ -5,6 +5,7 @@ class Ocp < Formula
   sha256 "d88eeaed42902813869911e888971ab5acd86a56d03df0821b376f2ce11230bf"
 
   bottle do
+    sha256 "5566054299b2a05716a3234c7c3d0acee15b4077360c89ec815b6162bee89319" => :sierra
     sha256 "4bd576f3d75594928348d30b3b3436cdeebba844be8a8ba65251eb1731de437e" => :el_capitan
     sha256 "e6b941f5aa2508a9628487cf40a186188f1dbf986a9a5ab2a824c57a03d45055" => :yosemite
     sha256 "d2a095ce47bdea35fad3f6f7ffac500ccc4dc8dd149a9c1dbbae2bbf92809886" => :mavericks
