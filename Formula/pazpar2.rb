03ead06e5443acc442936f9048297fbdca169f85
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 21:45:01 +0100

    pazpar2: update 1.12.5_1 bottle.

--- Formula/pazpar2.rb
+++ Formula/pazpar2.rb
@@ -7,6 +7,7 @@ class Pazpar2 < Formula
 
   bottle do
     cellar :any
+    sha256 "eb97500d2e2732215a178644afa9d1304f81ac0de2815cf4e55c98eccd74c79a" => :sierra
     sha256 "d5af2ca75125a99354bfde6fd4102645230bbe85a4b92831291d4555759e04d6" => :el_capitan
     sha256 "e72a4126cb415562bae2dc8c07b41fec585d521fe674910d172fb1546311fcdc" => :yosemite
     sha256 "acb104dd12781c2d6b7e8eedcaaa7b2576a890510269b561a321fc3667e1d863" => :mavericks
