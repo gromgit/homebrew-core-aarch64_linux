c1b36ef25728b427d22caa0f676c01b173a8e9b0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 11:54:10 +0100

    lout: update 3.40 bottle.

--- Formula/lout.rb
+++ Formula/lout.rb
@@ -5,6 +5,7 @@ class Lout < Formula
   sha256 "3d16f1ce3373ed96419ba57399c2e4d94f88613c2cb4968cb0331ecac3da68bd"
 
   bottle do
+    sha256 "2cfc68ddba21e6f485a4a57df9e810b6996d5364374c66e77b06d41ce230f060" => :sierra
     sha256 "2fbc90ffc3f12312dc11e31996ba94da3b8a4ba1c55f33ca60a5d81aef4e137f" => :el_capitan
     sha256 "366023d41536d0220a3d226a9f7a5e65b89fcf8ec212bfd6e53f8c2b4110abce" => :yosemite
     sha256 "7cbcdcbf720e5e93c7e8d41861fedbcb0f1b46233414c7897e94671e4e42a9fa" => :mavericks
