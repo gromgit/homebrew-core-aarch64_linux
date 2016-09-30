25f02fcfae76e25a402c0eb032781cf126387867
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 10:05:06 +0100

    miller: update 4.5.0 bottle.

--- Formula/miller.rb
+++ Formula/miller.rb
@@ -6,6 +6,7 @@ class Miller < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3526727329df7caa90bd79af1d0a507e25f8d37f497aefbbc195e3e95f966f40" => :sierra
     sha256 "21ad852a4e442347a56f9544e6bc37a19e5ec6da408546965eabcfae4731c6d4" => :el_capitan
     sha256 "b1c960a061390e0c2dd3126529cd03d79858358279fa3014f2515755abd6f1ba" => :yosemite
     sha256 "3a1d0ec591312b790a6cd669b9b724adef11ceb9264cc68d66fda9eed08ebb9a" => :mavericks
