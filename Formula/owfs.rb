9032aa8ea6402607233e012e79446f9479abd9a4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 20:38:26 +0100

    owfs: update 3.1p4 bottle.

--- Formula/owfs.rb
+++ Formula/owfs.rb
@@ -7,6 +7,7 @@ class Owfs < Formula
 
   bottle do
     cellar :any
+    sha256 "d2596e89c6f0a388ba62f66ca152a1c155c3992497c354eebc0701290fc82b25" => :sierra
     sha256 "63142415875279e8c5a4cdb6c54fa898fe8648570558c0f1a8d3626ad6d868af" => :el_capitan
     sha256 "7ea77ba55ee52f8bad219d904e78a708262b21ec7746bbfd02e9f70f10aa7f7b" => :yosemite
     sha256 "dd21b7a305e3469d5542c45b82b9a678eb6b5ddfb853cb835e10fc91c63dd5eb" => :mavericks
