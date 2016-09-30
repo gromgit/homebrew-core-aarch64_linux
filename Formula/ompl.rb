040bbbc080704500f6988527b866484bdbf1bc80
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 17:40:21 +0100

    ompl: update 1.1.0 bottle.

--- Formula/ompl.rb
+++ Formula/ompl.rb
@@ -5,6 +5,7 @@ class Ompl < Formula
   sha256 "4d141ad3aa322c65ee7ecfa90017a44a8114955316e159b635fae5b5e7db74f8"
 
   bottle do
+    sha256 "4a90bb34ebda949327b8873faf73cebc22b2de9e603b0f0b8fbc023935cb46f2" => :sierra
     sha256 "5f29d3dc453e5f1d294333d250fed884b4a38fa91f0bb1048a14eaa58774b709" => :el_capitan
     sha256 "ee99c05b5f1084ded43e0cacf7bd3ca0a1d3046bf99c1d5faafd225db2fb3a61" => :yosemite
     sha256 "ab89e5350fdf56e044503f363c0e36ab689e45ef4f38806cf66f214908720838" => :mavericks
