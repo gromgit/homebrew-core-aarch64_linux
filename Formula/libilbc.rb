e8ed1d820937814abea5e9c330059e60c852ad1a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-30 01:26:45 +0100

    libilbc: update 2.0.2 bottle.

--- Formula/libilbc.rb
+++ Formula/libilbc.rb
@@ -6,6 +6,7 @@ class Libilbc < Formula
 
   bottle do
     cellar :any
+    sha256 "421757bd970d1ba1d0c1fe2eff208cc0b44f8a75bbf9a09344ba504ad377750f" => :sierra
     sha256 "fff34390e949e037bb4b16937b62ab4993f55d2fb805656116ceab6a7c9b6e83" => :el_capitan
     sha256 "7f16b3e0f254f35be8b6275339dc813a6443f65d1c27e1748e08835a49733f6f" => :yosemite
     sha256 "7aa8495e4050ea38152ec218452d6fac97387ad385a6d63806238e838664471b" => :mavericks
