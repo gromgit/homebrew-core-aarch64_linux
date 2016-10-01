d3b50208e59272dbc2d3edaa7591dea347c8d0b7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 14:43:54 +0100

    wbox: update 5 bottle.

--- Formula/wbox.rb
+++ Formula/wbox.rb
@@ -6,6 +6,7 @@ class Wbox < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "241edb51af197d72022a48cb8444506188269b335b057ceaa7bf952db86777d8" => :sierra
     sha256 "0e813a0982d6b9228217f14352812d9e6880cce44757f8af9a0447bf5e4a1e63" => :el_capitan
     sha256 "ee2bd7599a73c3a9f3fe9f8c2d441d753914981b2420e591050780d436bbf011" => :yosemite
     sha256 "35b640ce39cd36f75ec595215099f121feb517672fb11abf30b36a1e567fc117" => :mavericks
