c3087a557fc0bdf957c41ac2f16fd02524641200
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 09:24:11 +0100

    sylpheed: update 3.5.0 bottle.

--- Formula/sylpheed.rb
+++ Formula/sylpheed.rb
@@ -5,6 +5,7 @@ class Sylpheed < Formula
   sha256 "4a0b62d17bca6f1a96ab951ef55a9a67813d87bc1dc3ee55d8ec2c045366a05c"
 
   bottle do
+    sha256 "a9d6c8f2f1da15af3ab8048fedc3d5996be9de40c781c6c120cc79821529c817" => :sierra
     sha256 "fced00597f19a4f6d5c1eea145582ba35e6a7885d4de94c234a47021b0ce2939" => :el_capitan
     sha256 "0743231958dd2029e227fda080acbe73d3fe038b3a59ff1826f3ea8290743bb4" => :yosemite
     sha256 "e547604d5b9059de979bcb412820c6ae137975f49b2a247491588b988e73c308" => :mavericks
