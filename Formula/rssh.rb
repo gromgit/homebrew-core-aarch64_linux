120ac2a7c01c400ec8637b3d1f0122f3890f719e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 03:52:02 +0100

    rssh: update 2.3.4 bottle.

--- Formula/rssh.rb
+++ Formula/rssh.rb
@@ -5,6 +5,7 @@ class Rssh < Formula
   sha256 "f30c6a760918a0ed39cf9e49a49a76cb309d7ef1c25a66e77a41e2b1d0b40cd9"
 
   bottle do
+    sha256 "aebe589ee047200b1fd0486d3a1fb08c4a601366391e80bcd1e7dcb87ca456e0" => :sierra
     sha256 "23891b0317ef29a36b0b8ee9f140193d17ae616983c13f2179d5b317b33e1eee" => :el_capitan
     sha256 "a63ead463b79c5375e0b919976173db13a236fcea06c4fa038a719375a550ca9" => :yosemite
     sha256 "8b36773d40fb20e51fc0f277fe7930540dfc6e1cec0f6c5d19d20242ce1de3b9" => :mavericks
