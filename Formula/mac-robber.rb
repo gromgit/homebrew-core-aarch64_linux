284194a17aff2ddf0396a741d723e484efac9237
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 20:49:05 +0100

    mac-robber: update 1.02 bottle.

--- Formula/mac-robber.rb
+++ Formula/mac-robber.rb
@@ -6,6 +6,7 @@ class MacRobber < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "160983c4988cb22bd68a0beeb48de91a8af3461722a42e65e523c4a6af08f444" => :sierra
     sha256 "0647670a38eb3ae5d8085ad1126f8d70b6e9ac99b086c0ec2f3301ac51ecdb3f" => :el_capitan
     sha256 "5e8b7656cafbab151ed82702cbd7e712ee30af62b6a6c031f9f440e95c174ed0" => :yosemite
     sha256 "87b8de3e43626713461398aac48d12a4b494c36b8da6cd4e6587d352fcb251fe" => :mavericks
