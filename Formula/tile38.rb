705c2f01e0422775c045c7a3fb13e5db6c6f4dc9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 11:41:01 +0100

    tile38: update 1.4.2 bottle.

--- Formula/tile38.rb
+++ Formula/tile38.rb
@@ -7,6 +7,7 @@ class Tile38 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "37b69850f2a2b051514b7fe2b7f5cea129a6e380df0d32d8ddeaf974c8f52af3" => :sierra
     sha256 "4589b978e8c3fe2fba7584543bf911a7d3b34ae077c882633375a3070f743918" => :el_capitan
     sha256 "dde5c61b297fdf69834b92b64105450d5e56500bffd5d75fca2ccc21a8754d72" => :yosemite
     sha256 "c3c28dacfdc6813e44cf39ab93435e4352dbf11517ddf80ad1184eb1b0be35c3" => :mavericks
