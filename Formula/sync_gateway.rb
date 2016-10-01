3b4a1330b7ddf122f732e7ab31ca9d593af068f8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 09:24:11 +0100

    sync_gateway: update 1.2.1 bottle.

--- Formula/sync_gateway.rb
+++ Formula/sync_gateway.rb
@@ -9,6 +9,7 @@ class SyncGateway < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ec6f97bbc7d3afeef7bc7274d93a2a89e4d51fd04eb64882295537aa46f407c6" => :sierra
     sha256 "d2885b854b63c1acf88918a29122bbd26383f80001e9f5f6cace389d189df24e" => :el_capitan
     sha256 "ee84b69ab0eeedc05fce8b24879414c539d31418aba1ad32a4e59bf8ccd73e9d" => :yosemite
     sha256 "a49e6035c48b7117c3b3f672cc50b928878f9c0b04a6529d02dabd0397c5c0ff" => :mavericks
