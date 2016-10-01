be8710f93e09d3cb9e1e33af3302ddf91fd433c2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 08:08:35 +0100

    subnetcalc: update 2.4.4 bottle.

--- Formula/subnetcalc.rb
+++ Formula/subnetcalc.rb
@@ -6,6 +6,7 @@ class Subnetcalc < Formula
 
   bottle do
     cellar :any
+    sha256 "2d2eb47734e58bb15d737336d74e21829e6b73506c38060e77070b93a407a934" => :sierra
     sha256 "e5c7229e2be576443d373b274d8b24e8a3d4820e520469d4f891dceff18e795d" => :el_capitan
     sha256 "57248f88bf4f9031fd162878a556115e150d65c9b52d7d3871b0eba44da2750e" => :yosemite
     sha256 "95cc39715a9bcd2cb46f7daf974f2e4b31810052b61b38be196a51f136d6aa28" => :mavericks
