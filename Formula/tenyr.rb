319641662d728005d2910a0365455f1a174e8e63
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 12:07:00 +0100

    tenyr: update 0.6.0 bottle.

--- Formula/tenyr.rb
+++ Formula/tenyr.rb
@@ -8,6 +8,7 @@ class Tenyr < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8ffa29217b05ea8f74719b43369b483f0ad20ba1534b80b430e22f585b1dbc44" => :sierra
     sha256 "21f73763ac69ceca9e98ac6c8bc471772b6337ecc18acf13faabdaaabd01e25a" => :el_capitan
     sha256 "180229f9b3855dcb64225c1e5b1ea22105f3931146d2b818d61a5f9058f5fbb7" => :yosemite
     sha256 "c20b4f63feb6150edc2e46222eed8329d0e8e5bb8bdfa3f84a87059ed03d95a4" => :mavericks
