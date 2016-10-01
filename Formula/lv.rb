7cc900d275bc0931d724da84059c53605faea267
Mike McQuaid <mike@mikemcquaid.com>
2016-10-01 13:20:22 +0100

    lv: bottle revision to rebuild.

--- Formula/lv.rb
+++ Formula/lv.rb
@@ -6,7 +6,7 @@ class Lv < Formula
   sha256 "e1cd2e27109fbdbc6d435f2c3a99c8a6ef2898941f5d2f7bacf0c1ad70158bcf"
 
   bottle do
-    revision 1
+    rebuild 1
     sha256 "49ad4ebf6830c1ef3f6899486e711f99bc293d422317f8851f174cf18de2a98f" => :el_capitan
     sha256 "f31281558dc9da38402a86b2b3c03efb10ab471561bf72dd556c3cd8df23ba14" => :yosemite
     sha256 "6e1894088a741aba921e77a4935d6ad2d11f06f03a4ff775c45e4256728511a4" => :mavericks
