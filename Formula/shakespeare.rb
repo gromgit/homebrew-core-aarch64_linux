dd8f9d8e1b3c0e0f807dcddb5eff63eda409b24d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 17:31:13 +0100

    shakespeare: update 1.2.1 bottle.

--- Formula/shakespeare.rb
+++ Formula/shakespeare.rb
@@ -5,10 +5,11 @@ class Shakespeare < Formula
   sha256 "1206ef0a2c853b8b40ca0c682bc9d9e0a157cc91a7bf4e28f19ccd003674b7d3"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "03be6d4dcc0f3c6718f3c99eff2a469d78349d5842015823a57d1212ac675cd6" => :el_capitan
-    sha256 "075eadd260c621c05ba7ffd8fc85d05f12355d4b90050b6285ee52c6c4fba43e" => :yosemite
-    sha256 "307444fb92a5b99b57b4231ca3e6fc8892faf170b1aae15385971359492bcdab" => :mavericks
+    cellar :any
+    rebuild 1
+    sha256 "6a8e746959adcbd5629bd6ec74fcc3854fa7355d098c14a640a6df23358ce335" => :sierra
+    sha256 "86547f1b0967f8399f00b7120a251a126e66dfe9c52a4fb9b3d17331e2381895" => :el_capitan
+    sha256 "1e35a35e7ca7eef401a76360320389fe23e2cea6db8bf9f2d266732c742ad8d5" => :yosemite
   end
 
   depends_on "flex" if MacOS.version >= :mountain_lion
