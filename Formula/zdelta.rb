dc51822fbace188f8a9dbcfe1ed9c9b500ce8a21
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 18:52:59 +0100

    zdelta: update 2.1 bottle.

--- Formula/zdelta.rb
+++ Formula/zdelta.rb
@@ -8,6 +8,7 @@ class Zdelta < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "49b891fbf6b6f93796cb3080dcbefc1d873000e84d74a4bf2dd851b625e6a698" => :sierra
     sha256 "3a186612374b9b2aca2e56f5dd68049c0b1ef952e4cb0b07263faf2ea62f136a" => :el_capitan
     sha256 "2ade2838217be1b9f6bc55be6bf05fa5046ae09a42d17b714b9f2a73f934c993" => :yosemite
     sha256 "86f93c2e260d321d3bf30b34c2313d2cec5bc6d23bfb5a86cf99ab6b5f64f157" => :mavericks
