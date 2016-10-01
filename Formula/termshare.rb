2765df9a575f49a222badc339c970ed523c25bf1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 12:07:00 +0100

    termshare: update 0.2.0_1 bottle.

--- Formula/termshare.rb
+++ Formula/termshare.rb
@@ -12,6 +12,7 @@ class Termshare < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "5d883c6747f478ab161ca648923a7397a782f437bb59d660df6a252b21f62e99" => :sierra
     sha256 "c540732aab70ec29b60459c19bb4ee55c0584b3a63476473219a115d2ec380af" => :el_capitan
     sha256 "c3b9c2784b02536ce97a2a3b3a205314e7ada8e727ac60b54577d933a04aa808" => :yosemite
     sha256 "aa9131a7eae6efe7e7d3bac1e73711f7bfe52f1dd246389bdbb137c70c815310" => :mavericks
