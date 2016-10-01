c06716a3c3f9787b11e07720b4785ac35abb2dd5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 01:28:25 +0100

    qtfaststart: update 11.6 bottle.

--- Formula/qtfaststart.rb
+++ Formula/qtfaststart.rb
@@ -6,6 +6,7 @@ class Qtfaststart < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "27b695d00f82590b223f82bcd88b50a45664168c75cfa88a11f75a5de68a8e43" => :sierra
     sha256 "da201fbe1dd453fea35599206496bdca5ece22ee80a482a327c0d51d1a264d80" => :el_capitan
     sha256 "4e85bed5cb743e34dcc74b67ff2b3f5fb4628313ee1ba82b02ee97d81d616e2e" => :yosemite
     sha256 "e18f2b26647d07d313f2163d16cfacdc0d4d4cdfcfd2e6558d2756cbcdc56b20" => :mavericks
