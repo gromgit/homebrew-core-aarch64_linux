332f12188388c3d7d13dca0fa1327af704790fef
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 12:07:00 +0100

    termrec: update 0.17 bottle.

--- Formula/termrec.rb
+++ Formula/termrec.rb
@@ -7,6 +7,7 @@ class Termrec < Formula
 
   bottle do
     cellar :any
+    sha256 "945043d319c728bfb239514c13407816dce87c1ad2f6b2b4cd8590d9d5c7dc86" => :sierra
     sha256 "787ed19e10d093b52b4aab2e6962480ea26b02ebda78bffb54258ce585c31ce1" => :el_capitan
     sha256 "53f6c1350027212566b1bcd5bb632a5cc5a9fbd56954b619a9bc0a96dd587bb4" => :yosemite
     sha256 "ffcb4996ef7e88fe41fef79289a65aa9d797e8ad10b7cc382fabc479d504bc31" => :mavericks
