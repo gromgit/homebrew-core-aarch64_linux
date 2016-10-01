598ca3d2a015e26b6248db56b6f625351bfb70dc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 03:37:17 +0100

    rnv: update 1.7.11 bottle.

--- Formula/rnv.rb
+++ Formula/rnv.rb
@@ -6,6 +6,7 @@ class Rnv < Formula
 
   bottle do
     cellar :any
+    sha256 "8dd3263bb656dcca22605b12faf4c6f54d65e5040e58a7a464c85b69ca19dc99" => :sierra
     sha256 "1c1aa519b786f842b39720e33900e92a2f2f8deef403755e79e2d3b518897ff1" => :el_capitan
     sha256 "6d46cb2e6476e22b8bb04d00f599884aa8e44ba7e199ad860e4f15795b04e83b" => :yosemite
     sha256 "f9c4575d7384100b1cc97d9f421b5906ea5068f612c346ffa0238db6c8e855be" => :mavericks
