ecc773d8e773702cbdab9bca93451da7e76da2d8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 10:27:21 +0100

    tcsh: update 6.19.00 bottle.

--- Formula/tcsh.rb
+++ Formula/tcsh.rb
@@ -6,6 +6,7 @@ class Tcsh < Formula
   sha256 "12e271e0b89e4259d9d6e8d525322e77340e7244cfbd199a591e5f8146285c49"
 
   bottle do
+    sha256 "0ef20f94a3d60fdd22a9cbeebb5474d04d5a95fbfeb60e5de51e3fa102d94913" => :sierra
     sha256 "dd05f0d9c6c12520c5b20fd0505288877a79a2b380091261490f659f6a81aadd" => :el_capitan
     sha256 "2d3fed239e9d5ac71b47c3e5bf96b93c1e9ba77c3799ca482a5b6809621d5f1e" => :yosemite
     sha256 "ff5b980d71cb0f151264f0a7f6c6b34120e2420aac284ecfe8fcf908f4e90c07" => :mavericks
