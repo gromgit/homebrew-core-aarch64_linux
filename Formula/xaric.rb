c76a186c975601f40cfb8d0bfb5c7e877a2626e6
Mike McQuaid <mike@mikemcquaid.com>
2016-10-01 13:20:23 +0100

    xaric: bottle revision to rebuild.

--- Formula/xaric.rb
+++ Formula/xaric.rb
@@ -5,7 +5,7 @@ class Xaric < Formula
   sha256 "fd8cd677e2403e44ff525eac7c239cd8d64b7448aaf56a1272d1b0c53df1140c"
 
   bottle do
-    revision 1
+    rebuild 1
     sha256 "9ddfb8878904f92a7281f5611a11b72b81ebed0ef6ac7af9c10588cb717b9317" => :el_capitan
     sha256 "f29d234ec8065f976ce8f14e21374871e5b8b2d092a26ad163d9cac32988bb9b" => :yosemite
     sha256 "3b8f2a6b837e43ff57ef626b4d46142562c1eda120ac5889124eab11d8b46b86" => :mavericks
