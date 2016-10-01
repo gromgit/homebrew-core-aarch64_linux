95bc212b944906d005e6e279ae955fd26a2246a1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 11:41:00 +0100

    theharvester: update 2.7 bottle.

--- Formula/theharvester.rb
+++ Formula/theharvester.rb
@@ -7,6 +7,7 @@ class Theharvester < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "926a441d788bc21e4cfdebb9b98c5a69f06f2bc9f1d0c4763ebb0cd6301f3597" => :sierra
     sha256 "eb8efb01299ff3a4581e733ec1b7d7d27c42d88d10e2b31cf78f109cb61c4031" => :el_capitan
     sha256 "a3712887bea4ea3586d39672cb0b194588694de050aa7f1bd762df5cba463fba" => :yosemite
     sha256 "56c3d5b41a821be12fc6f27ac8beb266984f0c245495c9970614ed776107633f" => :mavericks
