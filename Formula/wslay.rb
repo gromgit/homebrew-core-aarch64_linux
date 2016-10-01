5f9bec8d32bd5da5ba0099e31d483c88946d8a04
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 19:11:56 +0100

    wslay: update 1.0.0 bottle.

--- Formula/wslay.rb
+++ Formula/wslay.rb
@@ -6,7 +6,8 @@ class Wslay < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "9a442f80a67b037402618b0a76874c3db4d93213081f96f3c3f0b22d79068556" => :sierra
     sha256 "30939fd620cff4702d15d61a75774b71e1b226fb7b2b3fab8a3acdf96bdd9b7d" => :el_capitan
     sha256 "294d4646dcf7d352368de8b422a5354e72b3027a8678e993d68ea8d55646388e" => :yosemite
     sha256 "410634e15d5ce6f680ccd5d96b97fc2226aad9530aebe42690d3ceb4d7011e69" => :mavericks
