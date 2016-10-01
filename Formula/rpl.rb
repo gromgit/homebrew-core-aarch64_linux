55627771a5d3e9d1dbf29c3dfa543baff15832e0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 03:52:01 +0100

    rpl: update 1.4.1 bottle.

--- Formula/rpl.rb
+++ Formula/rpl.rb
@@ -6,6 +6,7 @@ class Rpl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2c9e55b51762d835db949c20f9eba36e83213082db82c69602658e2f28003b80" => :sierra
     sha256 "175e1f127c8c707b0d90c3c7e4399cc5c1e18410bf8b7f6ec9340dbca4c16e4b" => :el_capitan
     sha256 "d718355e56dd13c690f1d5a0541b5f051518f65b953aade9c525853a19266a61" => :yosemite
     sha256 "b0c4dbf06500053703ee4a8c8e751d43c435f0e3b0e25d2d328d310dcf490c23" => :mavericks
