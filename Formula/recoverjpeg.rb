475cf7044830569afbfdc319a9731c9b22876314
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 02:49:13 +0100

    recoverjpeg: update 2.3 bottle.

--- Formula/recoverjpeg.rb
+++ Formula/recoverjpeg.rb
@@ -6,6 +6,7 @@ class Recoverjpeg < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "764fa949a336bd1a7f2b96b9bbd96450382d12fde6aef19bc7528cde3c95a936" => :sierra
     sha256 "f36a76936a645367cd0d508709a9658a827188a7ceb818fc2af56ecf37946b56" => :el_capitan
     sha256 "0fc2ef04e6e9bb3f8bc2f2ce738a11970f426bab8c586c722cf1d8ee07d35766" => :yosemite
     sha256 "7c5d73b1e71d41dcae6c8d7fd25b40fbc4bcccc665b89eca5dab43185ad17e94" => :mavericks
