591b44b68bf39a5d70961b2103718bf8eb44c7c9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 20:49:05 +0100

    mailhog: update 0.2.0_1 bottle.

--- Formula/mailhog.rb
+++ Formula/mailhog.rb
@@ -24,6 +24,7 @@ class Mailhog < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ae2cc4e74e86634e24dba328424475c238b87886bcf75eb35e42fb53e7f402c6" => :sierra
     sha256 "30feb97181d3d4075ebd417ba7aeb3e5041e1c544b1ecec962b50f32d02760ae" => :el_capitan
     sha256 "a4ada6cbc833f8040a1f0c37c4ed8be58eefc7e6858c049ae1df2f9034ee4944" => :yosemite
     sha256 "215cf3c14a47cd83ff3e8201c822486c2ffb043bf1f5f1a24b3917c71af5367e" => :mavericks
