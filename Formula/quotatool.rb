4eb9a5fb9ae58e8f2eb75aed59de828e78dcb26a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 01:28:25 +0100

    quotatool: update 1.6.2 bottle.

--- Formula/quotatool.rb
+++ Formula/quotatool.rb
@@ -6,6 +6,7 @@ class Quotatool < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4d04c382c8cf8b0376b34ce12813be06e879fdf6b60711cf90643d08887304fb" => :sierra
     sha256 "da5c90f85204fa90a38da073765ec5c0f0a20333bcdcd131d79b682afa74233f" => :el_capitan
     sha256 "8af3549d42247f0b79458c96978f8f5e5fbe04cc1c0dd86f84accdf03e8e510f" => :yosemite
     sha256 "724a3fc561188de5e0e050f7459480cc8c613d399faee5290ec7a68b9715960d" => :mavericks
