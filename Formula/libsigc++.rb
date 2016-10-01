33edee1547cd436698c98d543b905ee23ef1a744
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-10-01 08:33:17 +0100

    libsigc++ 2.10.0

--- Formula/libsigc++.rb
+++ Formula/libsigc++.rb
@@ -1,8 +1,8 @@
 class Libsigcxx < Formula
   desc "Callback framework for C++"
   homepage "http://libsigc.sourceforge.net"
-  url "https://download.gnome.org/sources/libsigc++/2.8/libsigc++-2.8.0.tar.xz"
-  sha256 "774980d027c52947cb9ee4fac6ffe2ca60cc2f753068a89dfd281c83dbff9651"
+  url "https://download.gnome.org/sources/libsigc++/2.10/libsigc++-2.10.0.tar.xz"
+  sha256 "f843d6346260bfcb4426259e314512b99e296e8ca241d771d21ac64f28298d81"
 
   bottle do
     cellar :any
