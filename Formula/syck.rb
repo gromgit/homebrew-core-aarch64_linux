493b5322bd44bcdf0b6df504b719935e788839d9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 09:24:11 +0100

    syck: update 0.70 bottle.

--- Formula/syck.rb
+++ Formula/syck.rb
@@ -6,6 +6,7 @@ class Syck < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5a5f8c0f9a9def7ab59a052d85fbb128bc9a1c9c9b7ea2f9639109147eeba252" => :sierra
     sha256 "a2270f693ce6e8f0542f7d57dc6c8fcb731a268aee3c75a314523446c68602d6" => :el_capitan
     sha256 "cacc98eb6fab2440d3f69708688cb80b54bd14e5c472b7097ab9e235d5a670d6" => :yosemite
     sha256 "17606e97952cbea53c5649814d0d86c0cc2cdc5893b8063dcb4be8b4606c4f40" => :mavericks
