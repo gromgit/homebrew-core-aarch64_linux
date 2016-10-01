24349085f7412ab91268d2dd1089c48441f91956
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 07:39:25 +0100

    stlviewer: update 0.1 bottle.

--- Formula/stlviewer.rb
+++ Formula/stlviewer.rb
@@ -6,6 +6,7 @@ class Stlviewer < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9490714ae75aaa68e9c13f9b8d3a9cd87965d09167388d768f6299067774cc69" => :sierra
     sha256 "e2b6570f4f4fa6042528212a2c0c4798ee7375855e51eaa607e737d746aa2cd0" => :el_capitan
     sha256 "7bf9c633d050993ecca2013098a3ea544bf25cdd5b5bfdff57607e887e58d0b6" => :yosemite
     sha256 "cd58c5ba32e12ebb014a809ce8bdcd4a5b85c3caaedd7063710e409a33640707" => :mavericks
