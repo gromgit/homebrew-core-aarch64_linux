152faa5f143fda729145d5b1b61aa79fedc84e3f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 07:39:25 +0100

    stlink: update 1.2.0 bottle.

--- Formula/stlink.rb
+++ Formula/stlink.rb
@@ -8,6 +8,7 @@ class Stlink < Formula
 
   bottle do
     cellar :any
+    sha256 "994705db9f6774bd689fc28e84ecab12314bff57f1b6f256139da4c2b8027964" => :sierra
     sha256 "66488952113480623d60375bbf01eca978fa9d090e6a9b359ecfdc49611753c3" => :el_capitan
     sha256 "8baf694edef81adcb72dcde439c376aa20e632b856278c7e490d1521fb7b52b2" => :yosemite
     sha256 "78d3598eb1de58654014bd48935f1c3ca1bc760f38d7d8165223054b9273f109" => :mavericks
