36a2bd54ca3fbd3fb31835945aad3791a0100223
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 02:49:13 +0100

    reclass: update 1.4.1 bottle.

--- Formula/reclass.rb
+++ Formula/reclass.rb
@@ -7,7 +7,8 @@ class Reclass < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "0c381ac12c60fa23c9069f670cf262ce1068d346fe1e577ecb28ee566aa3fe51" => :sierra
     sha256 "92c143420d36d71d49ecde69eca47da2a46f1ae6d37e0f7522cb95a9d19735e7" => :el_capitan
     sha256 "7be55ff36c5e4b02e3bf2c6ea297e8a3248452dc6b15c1e6fd77633373ff3bb3" => :yosemite
     sha256 "7c63989f940dc69a54c8e1eebb1d14a1ea04141a4a806354ed1ad338c8d4ea6b" => :mavericks
