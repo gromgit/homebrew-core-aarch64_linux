2b773c20187f4c0ee6a47449b2f152ccade99a1e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 03:52:01 +0100

    rolldice: update 1.15 bottle.

--- Formula/rolldice.rb
+++ Formula/rolldice.rb
@@ -6,6 +6,7 @@ class Rolldice < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "590b82ca6bb16b5acd3bff29985138c9d3383eb274f27615d5843e176b08464b" => :sierra
     sha256 "b15c8dd417d710a734f2b445083752dd072b6d1cb39260901dcbd9634820fe8d" => :el_capitan
     sha256 "4e818eaba72271765a7c50c97a60764c37e0a1c6d4308e4490dbe811b32ff8ba" => :yosemite
     sha256 "4974aa7720ba08112cfd0eecc2611a82eb8fc9ac379fe779ba98f5e1509255e6" => :mavericks
