a40a19ede5281e08b6fa69fe03a5bae606260314
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 04:49:02 +0100

    scamper: update 20141211e bottle.

--- Formula/scamper.rb
+++ Formula/scamper.rb
@@ -7,6 +7,7 @@ class Scamper < Formula
 
   bottle do
     cellar :any
+    sha256 "047a768da4329c58e177cd9a15e2811019438e14357746ac96c60fa499d3175b" => :sierra
     sha256 "5ce1e8a4ae2c76df62e60987c0f3a41f5ce7ae268d79ce5fb608a63837f36242" => :el_capitan
     sha256 "87ff82dc8a7a2c058726f07fe73c61f2acd0b3c4f124c4072f3159c995317cf5" => :yosemite
     sha256 "81991c6883e2c9767cdca29feb0e1cb7cab07e6b7c1af6d0832c706ad161340b" => :mavericks
