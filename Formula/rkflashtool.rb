e17708d14c9fff3cd1cd9911054dd27dfe206937
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 03:37:16 +0100

    rkflashtool: update 5.1 bottle.

--- Formula/rkflashtool.rb
+++ Formula/rkflashtool.rb
@@ -8,7 +8,8 @@ class Rkflashtool < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "b049c39dfe71266669ed37b0af50f5d1fa981963eee08abab29a8cfb32ccc23f" => :sierra
     sha256 "6e129daf2945875ed2f3d162cc4705e1643d826e910574d61d7947c96de007e5" => :el_capitan
     sha256 "6c42ca5e0a23c0e246b6e58baff3a32215ba94e60115c8ef4f38306dadfabbeb" => :yosemite
     sha256 "1f3260720ba6ca946000f10f3675140d8b5e737e6fd18abe7b5c0cb4f7b2d972" => :mavericks
