a4a10b3adcbdb5f6f4efe351a2db30d029b8f58f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 09:24:11 +0100

    synscan: update 5.02 bottle.

--- Formula/synscan.rb
+++ Formula/synscan.rb
@@ -6,6 +6,7 @@ class Synscan < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "98648d8ae9cce116cd800d7773ca84e44af92d37059bc62b02b548f3e5b8cbe8" => :sierra
     sha256 "d9d3cfbe1016ff7d314cf9db8b7fa796e3d3a43e78b2552e84b224e53f73f541" => :el_capitan
     sha256 "3298295fda8028da39ddbc6c3f2d26b42de9dd4f6e3a46a4e19bb871fa545035" => :yosemite
     sha256 "4cacc06fdeda9a24bb681cb90c52c4692d5bf3993f18db496c5de19ab9d46dac" => :mavericks
