55b5c9485ce43f51c65275a6d8c311246f7254a5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 03:37:17 +0100

    rmcast: update 2.0.0 bottle.

--- Formula/rmcast.rb
+++ Formula/rmcast.rb
@@ -6,6 +6,7 @@ class Rmcast < Formula
 
   bottle do
     cellar :any
+    sha256 "9ef73c5d52886029cd89d829cdceccca0d03bce0dc72647d8cce6704d492f080" => :sierra
     sha256 "4fe0a1745659bb99748972c2fa0640e6b864e92739ba192a89ed12c0614b1372" => :el_capitan
     sha256 "502e09994a9b455d9040f8e346419a2a3ef0156a73b0249bc161446448729292" => :yosemite
     sha256 "1afd20258226dc16873b567b1b5ab920b9e414f4733bfa99fa917074685f2b8a" => :mavericks
