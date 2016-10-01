8c09c5a6968d5e76309314f5c4e4bdfb364d94d2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 01:28:25 +0100

    radamsa: update 0.3 bottle.

--- Formula/radamsa.rb
+++ Formula/radamsa.rb
@@ -6,6 +6,7 @@ class Radamsa < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6fdb159c26f40239fbc9cb382ac42770ffae948d043e6972ba6c23c922b88587" => :sierra
     sha256 "44569c384623b1419a5431a8ba9c48743854bfb3ea79e3736bb1ad63d1fe04a1" => :el_capitan
     sha256 "184f9a9d1c030ab7ee83b84236f3749e1d3e0de11214166b264c4f52af902fc6" => :yosemite
     sha256 "fd10a5879e5d80d0decfc10b96a9a2bdbdbb3ce667b030a51ff152566020353b" => :mavericks
