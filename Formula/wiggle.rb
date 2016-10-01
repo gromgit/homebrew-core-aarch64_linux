f6ee1a0b76e01542fc7fd6e337b07338e343f7f6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 15:23:39 +0100

    wiggle: update 1.0 bottle.

--- Formula/wiggle.rb
+++ Formula/wiggle.rb
@@ -6,6 +6,7 @@ class Wiggle < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0f002fb282dfaa1df66f28cd3f2135b571095b4f5db5528e288dcf439d480a2f" => :sierra
     sha256 "7e832c2b0e4e4564f15c3a66b1d0b78c309a0f28ca548b5e39b6967bdf12a542" => :el_capitan
     sha256 "e6799084357708eb5142ec5c1104f43c9fdcc0d953f0b80c458e931f53e210e8" => :yosemite
     sha256 "09c23ec2dce1bc70a93671b85780f47f9bbb24c024a48cbfa9c3a13b79b048a5" => :mavericks
