836a670f47017586978ebab7b2aeb5b6ac95ca3a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 06:16:36 +0100

    sersniff: update 0.0.5 bottle.

--- Formula/sersniff.rb
+++ Formula/sersniff.rb
@@ -8,7 +8,8 @@ class Sersniff < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "077112b0300e14f956fbe45ff650cf973e04c355707a3add63b8efc7fc047737" => :sierra
     sha256 "abde8af644fecfa883abf597486fd269b379001ae29161fbd21777d0506edc86" => :el_capitan
     sha256 "c0c00897dd19dc6f8dff05b57e961079c8f783ba9afc345cac9f064dd2ae6630" => :yosemite
     sha256 "cd8d98e4360a068975fa25f1816e1a79533b46e5da65e455a0498d334daa1761" => :mavericks
