04b3884c3d5c0a39d8b332a90ffffdb18b781b13
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 11:49:16 +0100

    swfmill: update 0.3.3_1 bottle.

--- Formula/swfmill.rb
+++ Formula/swfmill.rb
@@ -7,6 +7,7 @@ class Swfmill < Formula
 
   bottle do
     cellar :any
+    sha256 "5e141351c1aadf19e71d254ced8200698d0c07e8bbac21db938fc4734a494c0f" => :sierra
     sha256 "9c9674f4a986e2af30ffaa69c67276a43f0e0fcb4803a9463e66c63eb2398627" => :el_capitan
     sha256 "eee9695134c883a9348a7c5c1e66b32b7b01b8ef1f24069688c727eaf146ad50" => :yosemite
     sha256 "90cc5c0dcf27189d3f9cf0440c6a9e8ac4312d57c7afd60243f4496f1dd79541" => :mavericks
