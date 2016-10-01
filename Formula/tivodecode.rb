4fd00963195f326e60faaf33e0c6d78817c5a844
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 12:02:27 +0100

    tivodecode: update 0.2pre4 bottle.

--- Formula/tivodecode.rb
+++ Formula/tivodecode.rb
@@ -7,6 +7,7 @@ class Tivodecode < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5682668b2e721933054656cebc49ccb46c382428b77409d94251c6f1dfd3092d" => :sierra
     sha256 "d50450e62c6fcf71643ceaf5f33dcf4e904e389c89597ccbe148de3053839ccd" => :el_capitan
     sha256 "cb74afdc87eca67025849836bee29e04f6c8e7755753a9daa52c84cfc9201cd8" => :yosemite
     sha256 "7622e7c60ecd22127b454e303d7c15b14c6cc79bcddf00c0de0de58e9350b5bd" => :mavericks
