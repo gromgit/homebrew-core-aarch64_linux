44520e0fd46493ad3478daa31bf32d849f60e7dc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 14:21:59 +0100

    wallpaper: update 1.2.0 bottle.

--- Formula/wallpaper.rb
+++ Formula/wallpaper.rb
@@ -7,6 +7,7 @@ class Wallpaper < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9a4578853631de822d6d09008ac849447c10a22f09a3a88939cdb808f6384744" => :sierra
     sha256 "371927997832b14cfc197aa5be3c5bcc2e3bca84523c8e43340080bf8e81e340" => :el_capitan
     sha256 "5010ad61cc504d0268a7da142d1fea366498675dda2e92a08b93a5f24b28613d" => :yosemite
     sha256 "6f60873e8ac2f4f8a41770496e43e1284534a954134198cee9837cdd8cad7e0f" => :mavericks
