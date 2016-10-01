d857e554a2244630de9337e4efbd564262b2e553
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 14:21:59 +0100

    wait_on: update 1.1 bottle.

--- Formula/wait_on.rb
+++ Formula/wait_on.rb
@@ -6,6 +6,7 @@ class WaitOn < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "31a21b915c4f8d901ed5d51d14f3f2f81993ed07891832fdbfb314b7cc8599e5" => :sierra
     sha256 "f362694f30021c32557e23b5add36ac4bdfbcabc6056996c5d23fc125527d85d" => :el_capitan
     sha256 "1719c6f8843af24cc8a985ee3e246fd10c6fe7b08297a1086e0636ed437199fd" => :yosemite
     sha256 "91acf6c588980ddfa44f17778a82eac20e0b1cc8401f21d2c145060a83fae97b" => :mavericks
