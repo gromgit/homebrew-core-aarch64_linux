3374a4af5c6e4dd3a7182f6f40c4597f4caa4d44
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 10:10:46 +0100

    tcpkali: update 0.9 bottle.

--- Formula/tcpkali.rb
+++ Formula/tcpkali.rb
@@ -6,6 +6,7 @@ class Tcpkali < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b18665fbfe27ebb7d9613753c63ca7aedc0a51ed9e6409111ffd332ed11bc25d" => :sierra
     sha256 "2ebc6fd13a75a2b95bde049906d1cd3af1b406f256ad7490a113538d5228892d" => :el_capitan
     sha256 "d37c5e39ec28c5f9a73e3ae023418f0009e9a4c59a82dffc4fd936259db75853" => :yosemite
     sha256 "7dbf2ecc4c3b259693ff338cb6f8db6f4025afde3cc93bcf36ad8419c5ade959" => :mavericks
