10a65ee6bc065999c7aa6f7e59d6449b0effb326
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 20:16:04 +0100

    luvit: update 2.11.5 bottle.

--- Formula/luvit.rb
+++ Formula/luvit.rb
@@ -7,6 +7,7 @@ class Luvit < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4e105e74d17f2e7f0322116f7336a3df4a68685104981f456d01879079f68ece" => :sierra
     sha256 "728882f240f666b5b04458206af37e4e84d9d656c8918c657f61b4fcb1d29188" => :el_capitan
     sha256 "7dd6c06c1bec92c22d957663a27ece45eb9ce10edb11a4c1f87ba8a78f245b56" => :yosemite
     sha256 "66ad68f41d253bab1197f1b8445e254ae587ded4a5d0b9af554b55d4a7179c34" => :mavericks
