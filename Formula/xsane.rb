e09dd288aa70687930501cb5a13f041874ad6c1e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 18:05:07 +0100

    xsane: update 0.999_1 bottle.

--- Formula/xsane.rb
+++ Formula/xsane.rb
@@ -6,6 +6,7 @@ class Xsane < Formula
   revision 1
 
   bottle do
+    sha256 "d8986e38bfaee594922515ca9406213ddabb52c39d8567b997fe3008acc504e6" => :sierra
     sha256 "7608ab59dd7e21a4c6bff5ccb29e682fd9dbcff9be876516e85fcacff10719f6" => :el_capitan
     sha256 "df7b1492a1b526a7883fc810cffc2a974e297f0f287be44047f6795a7ae5e8f0" => :yosemite
     sha256 "64b437736bc0f0c16d0c5bec06facb010939709c4a6923835337e4a5ee63b096" => :mavericks
