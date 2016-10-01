50b375de7d88042ac57c9ecdbdd88b5382ad2e05
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 19:41:24 +0100

    zshdb: update 0.92 bottle.

--- Formula/zshdb.rb
+++ Formula/zshdb.rb
@@ -6,6 +6,7 @@ class Zshdb < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3b1a4a420b1e74938b673bf38e20b471417f035407bf019fa6c89e41da108359" => :sierra
     sha256 "3be3a69734982ccbed4285dc8821483c8ababe00bba9f0bd1cb54252ba772107" => :el_capitan
     sha256 "aacaceab10fb77097c1c25ae87c68f422ead071b333242b085126aaa6560638f" => :yosemite
     sha256 "0b41e171d32a9cfe1d3293fe18263f30af64cfaabfe551c09fa46d1c55cd1c0c" => :mavericks
