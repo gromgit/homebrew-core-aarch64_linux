30b02692be3fe6b6bf5dcba56e3f09a8737c717f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 13:55:10 +0100

    vtclock: update 2005-02-20 bottle.

--- Formula/vtclock.rb
+++ Formula/vtclock.rb
@@ -7,6 +7,7 @@ class Vtclock < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "766e69763326b8a8c5cfdc636cbba9f6fcffde0739be56612c54a2904d95d456" => :sierra
     sha256 "f87c685e59533a0085b439c4153c2734d4091447f5a81c627ccc0d2e589ac65d" => :el_capitan
     sha256 "a72a8c176276c40a3e9b0c6083a61013efb55b5ea43cd786000dad3c4243dd96" => :yosemite
     sha256 "9811bd8bb3e5cd2f94dc37346e09588bbab5cb1f0cb1ef5f094adf20046440da" => :mavericks
