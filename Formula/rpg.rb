807286efa5d1457db6bd555aebf2c3f323cb6268
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 03:52:01 +0100

    rpg: update 0.3.0 bottle.

--- Formula/rpg.rb
+++ Formula/rpg.rb
@@ -8,6 +8,7 @@ class Rpg < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ddb4f887b798101d95300f0b0c22a0760d06158c09796e9bfb65869f28b69434" => :sierra
     sha256 "be6550c915e361dcc0e020048f9a5f4ea37f1c5890a526282a3d70c610594981" => :el_capitan
     sha256 "5c1af29955697dcd46ff58fd70f9aca986b977f3cc17f638822c81289f180df2" => :yosemite
     sha256 "2ebf1a744c3c072c107883f565c04154b3e530c93868bb438bb90a1be35a4cb7" => :mavericks
