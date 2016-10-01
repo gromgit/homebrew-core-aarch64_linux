7657b30d9af43a5aeaf8277996d7f982356a29ad
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-10-01 10:29:37 +0100

    gsettings-desktop-schemas 3.22.0
    Closes #5461.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/gsettings-desktop-schemas.rb
+++ Formula/gsettings-desktop-schemas.rb
@@ -1,8 +1,8 @@
 class GsettingsDesktopSchemas < Formula
   desc "GSettings schemas for desktop components"
   homepage "https://download.gnome.org/sources/gsettings-desktop-schemas/"
-  url "https://download.gnome.org/sources/gsettings-desktop-schemas/3.20/gsettings-desktop-schemas-3.20.0.tar.xz"
-  sha256 "55a41b533c0ab955e0a36a84d73829451c88b027d8d719955d8f695c35c6d9c1"
+  url "https://download.gnome.org/sources/gsettings-desktop-schemas/3.22/gsettings-desktop-schemas-3.22.0.tar.xz"
+  sha256 "0f06c7ba34c3a99e4d58b10889496133c9aaad6698ea2d8405d481c7f1a7eae1"
 
   bottle do
     cellar :any_skip_relocation
