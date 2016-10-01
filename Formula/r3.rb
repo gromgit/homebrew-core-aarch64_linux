ed30a7a9dc8005ed9a0e649477f799f07ffc9b72
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 01:28:25 +0100

    r3: update 1.3.4 bottle.

--- Formula/r3.rb
+++ Formula/r3.rb
@@ -7,6 +7,7 @@ class R3 < Formula
 
   bottle do
     cellar :any
+    sha256 "d39c22ae9e69454cc7c205ff0cecc3dd6084a38a1e1742091f55df389e5a8f4a" => :sierra
     sha256 "6122bbc3566581f130e54cd563ed69f169598f5ce62d6319e7b5a95b10b802ef" => :el_capitan
     sha256 "56a37f8cab8af3833eb52c6fc739027ffd755fb0e60530bd96dc643bdb8e18ed" => :yosemite
     sha256 "26bd4bc4114b54d57d9f39bd00f15914f03eea7407fbcc50df4c1925b412a879" => :mavericks
