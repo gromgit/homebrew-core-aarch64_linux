d86a8c8432ec1b1ec42d89aec555bd3a67baa63e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 08:08:35 +0100

    suil: update 0.8.2 bottle.

--- Formula/suil.rb
+++ Formula/suil.rb
@@ -5,7 +5,8 @@ class Suil < Formula
   sha256 "787608c1e5b1f5051137dbf77c671266088583515af152b77b45e9c3a36f6ae8"
 
   bottle do
-    revision 1
+    rebuild 1
+    sha256 "3b7b1944db83edb792e26ebbd610cb5310357e2f24983ab7dd8be0c0c7dd4aeb" => :sierra
     sha256 "30069bffb5c8f3903f4888135c3d1939f1329a19fda93b3bc8bf244827413995" => :el_capitan
     sha256 "30c20891f724dfe103704e1b3c4e17b068a152fb3098560462a156fba29474c7" => :yosemite
     sha256 "0f82a7cf59da98e7dfbdf437ee22c412eb5e52a821acba2f92b65de4ea7a5fe6" => :mavericks
