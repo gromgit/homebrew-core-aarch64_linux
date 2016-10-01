e8f77131352a802e0c289b849502512f0bec2cf6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 07:18:17 +0100

    sslscan: update 1.11.6 bottle.

--- Formula/sslscan.rb
+++ Formula/sslscan.rb
@@ -8,6 +8,7 @@ class Sslscan < Formula
 
   bottle do
     cellar :any
+    sha256 "5a045b679fc13fb41ec5d6db720ca0275afdf0e7ad4e8247ad69cf781c5b563f" => :sierra
     sha256 "7fb494cd35dd02742c5bc303fa1c08e814d79c2c1ab94a7259473fa356ae0166" => :el_capitan
     sha256 "854ff0b05b00e8a53f34c1c8e802f13c3fd296460716fc78d5d818ba56d5730d" => :yosemite
     sha256 "ba2d28eb56bb0ac8563d0763e3c01ba00866ec842a8af0af688367589a462350" => :mavericks
