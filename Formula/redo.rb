71f178e8a2cf6fac797b466f997c22169e7540d7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 16:26:35 +0100

    redo: update 0.11 bottle.

--- Formula/redo.rb
+++ Formula/redo.rb
@@ -5,9 +5,11 @@ class Redo < Formula
   sha256 "2d7743e1389b538e2bd06117779204058fc0fcc0e05fd5ae14791d7f3fc3bcfa"
 
   bottle do
-    sha256 "1100591f6491ecede9896fbe1608277faaaaa002a170f4e0f7372e1f1a98eca1" => :el_capitan
-    sha256 "e83a1a2519401dcc187310f1b12c3063f32cee2ca76a003b7cf8a5d873779c90" => :yosemite
-    sha256 "c0c79234b90f83ca99b7dfea11834ee123bc581ba3822201950be7fe3c82afe2" => :mavericks
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 "81834b6558862943db1789472cc50393dfece52dcc4b4d1720fd85c11a615217" => :sierra
+    sha256 "ffb1132c1c7327971b89c6a6bfd7abe267f08c0790dc66816c09052c93a28ebb" => :el_capitan
+    sha256 "70523de82822fa0cb289197dbbd228a6105781e80fd89dc1ea594637eccad6a1" => :yosemite
   end
 
   resource "docs" do
