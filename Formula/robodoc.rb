2cddaa23deee7b0ef941b17ff8b0d41871d5469e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 03:37:17 +0100

    robodoc: update 4.99.43 bottle.

--- Formula/robodoc.rb
+++ Formula/robodoc.rb
@@ -5,7 +5,8 @@ class Robodoc < Formula
   sha256 "3d826424a3957502caacf39951f7805f1d72bb374c0533de7ca1036f306afdc7"
 
   bottle do
-    revision 1
+    rebuild 1
+    sha256 "230f593f9fadd10d98868593e273ad40dfee6ad877b396073042a187f19c60b8" => :sierra
     sha256 "54532e8e7629f4eeedbdef400f40997c25558b692755b816f6facc37a4975d4d" => :el_capitan
     sha256 "a5c2794eb7e02c27707aad1bbea593ddc0d21fbf197f6b4313f8d0ba84fb34a5" => :yosemite
     sha256 "cea580dd90f87075879dc722262668deac629673ed22dc1e2e5a6cb52e2439ca" => :mavericks
