f7f6eb82664b59a1cda60a94629b350d11c2877b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 06:16:36 +0100

    setweblocthumb: update 1.0.0 bottle.

--- Formula/setweblocthumb.rb
+++ Formula/setweblocthumb.rb
@@ -6,6 +6,7 @@ class Setweblocthumb < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "563620905a209f198f30bbffc9177294b224cee3098719af6da8cfca74092157" => :sierra
     sha256 "2a9c327d5d594d00d7d283d6627a5eeef160731616aec9d62bab017b52d71f1a" => :el_capitan
     sha256 "f55cbbabd19c245e42249b8d75c51b4fcec05d6d08674a448bf6e7a3da70aae5" => :yosemite
     sha256 "fa6ca731192b7645165ab28c148122688e5e584e2de1c8b500371d1c36cf9dda" => :mavericks
