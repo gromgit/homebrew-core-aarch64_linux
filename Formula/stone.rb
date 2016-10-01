470a8325f388420569e396d08ec6a994a5e77851
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 07:39:25 +0100

    stone: update 2.3e bottle.

--- Formula/stone.rb
+++ Formula/stone.rb
@@ -6,6 +6,7 @@ class Stone < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "540bd64b2264bfe03d88ad620a7138a1d96742d6810eec301fd7e5ea63970ee9" => :sierra
     sha256 "9d4038c7882d2fe256d77340b5e0ec52a551697b9869fb61f3a22b0be917e92b" => :el_capitan
     sha256 "e01e391d43da23b477186a54f96b0d20bb67c24e1503e20408dc12d025f04def" => :yosemite
     sha256 "ab43aca5038bdf014c1a5aaadb9e526626c9c4369dcaeac045b9dce6514b30bc" => :mavericks
