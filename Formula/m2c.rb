dff022de62578d320c51b15011072e5972191683
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 20:49:05 +0100

    m2c: update 0.7 bottle.

--- Formula/m2c.rb
+++ Formula/m2c.rb
@@ -6,6 +6,7 @@ class M2c < Formula
   head "http://git.savannah.gnu.org/cgit/m2c.git"
 
   bottle do
+    sha256 "e98a99fb06c6b72bd0cf11d8369df21e1e9e57253e0a9d63ae8641444079df93" => :sierra
     sha256 "4aa6ac4f5fd855f4f84d5577ff6f79495fda9edc2ce5335c64bd96a881975eb0" => :el_capitan
     sha256 "67659bd6a5fe922c1b34d5068a5cecbfee1f804e5ff432e32c8682a04029ccac" => :yosemite
     sha256 "7bf62153eeb0976851785db04e1319f745709294aa9d0bc99e47ffee3eba1315" => :mavericks
