8ea5c2dd291c84f547575110f93e6621104f9666
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 19:17:19 +0100

    zopfli: update 1.0.1 bottle.

--- Formula/zopfli.rb
+++ Formula/zopfli.rb
@@ -7,6 +7,7 @@ class Zopfli < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5f99599ab4ec444d4eab8fda1dc408dbf66b7b2755500e2f77367690d25566db" => :sierra
     sha256 "c79a06778c3f97ea3480bba4f16ce15748d258674be2170423205163a56efed3" => :el_capitan
     sha256 "a1eee001d7ade7d9a6920b05e8a9f1ae834c32ef8b9ccd5b1e02e7b9e09fd5e5" => :yosemite
     sha256 "ae4f89b431c3f641385a66f61cab2e460b7217d0120b50da187b07dafb3a559c" => :mavericks
