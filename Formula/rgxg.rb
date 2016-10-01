826345175deca4ffec14e67066da11066a4d218e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 11:22:12 +0100

    rgxg: update 0.1.1 bottle.

--- Formula/rgxg.rb
+++ Formula/rgxg.rb
@@ -6,6 +6,7 @@ class Rgxg < Formula
 
   bottle do
     cellar :any
+    sha256 "3b15445df62f8f57c4447a85cd719251f8820596548913e76d9738124d08f763" => :sierra
     sha256 "e84c6dfcb4195ef84bd4c5373bfa6029ea4d8600f94b58bc97fbee7334bf3f74" => :el_capitan
     sha256 "68a4566c40db42aa298862e8b2cc02dc16dfcb9f373454e48be31e0899dc6a64" => :yosemite
     sha256 "6fc96e2fbf981e67c6374928430dbfd20691d59f6b5dd7b1e255284edeeb6fc5" => :mavericks
