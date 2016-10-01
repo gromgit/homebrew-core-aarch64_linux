fee0a41b28247b68d2a17eeb20a313ca01594f19
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 12:19:39 +0100

    tmux-mem-cpu-load: update 3.4.0 bottle.

--- Formula/tmux-mem-cpu-load.rb
+++ Formula/tmux-mem-cpu-load.rb
@@ -8,6 +8,7 @@ class TmuxMemCpuLoad < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8743cb844ff2a55657f2f1eb7bfae300c02a3fdf255fdd5e8242d1a60103838d" => :sierra
     sha256 "9e2c7e5fd03feb98cead3f366a9cc35375cee80c30fd570c742440d69319c296" => :el_capitan
     sha256 "abd6293238671268ea1f0362518cd82c4b3133cb42b0327d579c93768ea81110" => :yosemite
     sha256 "24e52a177d0201edf30621a648c7cbbf1f2cc7e4bd9f9145a7f8c258d9219725" => :mavericks
