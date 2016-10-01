2508ae1d6eb29fff3e39b0518fe9ded06120243d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 10:27:21 +0100

    tcptrack: update 1.4.2 bottle.

--- Formula/tcptrack.rb
+++ Formula/tcptrack.rb
@@ -7,6 +7,7 @@ class Tcptrack < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f84deb6bc918678e365db7810b3dd3ff2b3dbc0af25bce58f81fe22256951459" => :sierra
     sha256 "93022ecf618c41d508175e15bb6cdbc6d24206cebe9e6f77524b8ab066134f2f" => :el_capitan
     sha256 "23ee40e3789155774146fee0e1b99cba1792bef8ddfe0aa9d98864a32545445e" => :yosemite
     sha256 "f1d33c99082cbcff0030a40c164f680a6e6e995b9b92c6b414c1e2d4d25d67f6" => :mavericks
