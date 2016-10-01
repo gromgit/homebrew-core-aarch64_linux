ada6f04af3e769fc50608dc0c87c90761f831eab
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 12:07:00 +0100

    teensy_loader_cli: update 2.1 bottle.

--- Formula/teensy_loader_cli.rb
+++ Formula/teensy_loader_cli.rb
@@ -6,6 +6,7 @@ class TeensyLoaderCli < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "bdbf31e0917a9dba051d14b5e06deb95ac95857e9a1b2a21118c051b8e402c4a" => :sierra
     sha256 "6328eeb1ed51edb8527874bd7c0f1a45cdf4052f0c35863dd1a67b5e0644c57e" => :el_capitan
     sha256 "90d5b5bf9adbece0001da72b1881617406bb9eeb76ff97ad5989e779179f5590" => :yosemite
     sha256 "dcd10140babb4d2937ce376c89e9c24a2e8046d2cabdad2cfdbc2542afa14471" => :mavericks
