04be0eb4e6000ed3f7966ba81c68aaf38bbb9167
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 09:49:12 +0100

    tag: update 0.8.1 bottle.

--- Formula/tag.rb
+++ Formula/tag.rb
@@ -7,6 +7,7 @@ class Tag < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e96f388c073710ee630ce6437758995ab4f76df047b31a70cdfc45d050e1916a" => :sierra
     sha256 "7406b17715a627677917d75800850bfcf52b5796584dfc90f5e17d67a01cf4a8" => :el_capitan
     sha256 "ae93b14ff99a6e0827359effbed9ca31f148affbf28f6d212bed39232023e1eb" => :yosemite
     sha256 "8aa970a198d4c14b310d0f2ba5dcb238699d44585205137061a21d0e7690d629" => :mavericks
