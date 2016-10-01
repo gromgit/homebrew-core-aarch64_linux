d5cf75b82bd4f0ca8b8bb800f331f67c74373264
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 17:20:10 +0100

    xdot: update 0.6 bottle.

--- Formula/xdot.rb
+++ Formula/xdot.rb
@@ -8,7 +8,8 @@ class Xdot < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "688f1d7c05d6999c1d097b6c2125680a6e808eb0bda06224b845ec7293be0f36" => :sierra
     sha256 "18110705b33c93b878b20c73b897cde121b4e4e2c054c1af2e0d5c863f16017f" => :el_capitan
     sha256 "d074dd582b0ca17aa565f75a2b53048f97360ac03d53e6fd0edb3f520c8efc06" => :yosemite
     sha256 "6c9a134c44cb444421e29cc398c9c00ca6f8849def89012dce1d69581b41227e" => :mavericks
