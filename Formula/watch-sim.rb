3c2cd4949126629eb47e0ed3ca53e3a0570e373b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 14:22:00 +0100

    watch-sim: update 1.0.0 bottle.

--- Formula/watch-sim.rb
+++ Formula/watch-sim.rb
@@ -7,6 +7,7 @@ class WatchSim < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4c0b4b9cf453585d826f3950eba375d8dea80194c50c8d5ab3a014dec9a49c0d" => :sierra
     sha256 "1a7666cc09ecf2228350ea766d0f8e3bd32ab545ac44d9b17b7dc42107c6f15d" => :el_capitan
     sha256 "06d95de04920d991ee1ee5a9e8035fb12ef103aa072382cef82cab683797e8d9" => :yosemite
     sha256 "b04846befdb378679cdd93bdf182784a8ed0da3957cb0b4a124576b3cd06e54c" => :mavericks
