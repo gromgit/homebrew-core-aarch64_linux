7512981c5b36351a7df5026f2f2323e4edc301db
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 20:49:06 +0100

    mairix: update 0.23 bottle.

--- Formula/mairix.rb
+++ Formula/mairix.rb
@@ -8,6 +8,7 @@ class Mairix < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0a7ee5ac029afa61f328a1c316e9dfc8be3d5f2ae66951599305b432e3b80bc8" => :sierra
     sha256 "453f15461f87619409983ab90620d5bb57d151a3f3485d08d3e5a9e26c59713d" => :el_capitan
     sha256 "f41193f4f7ca899f5ad2ecd8ef9e1fab3b6810f008b2ea8e55d310dba8c7a795" => :yosemite
     sha256 "4fadb513b3478d776d3a23e8352015b18f45a4347a50b2fed988f1729a7336e7" => :mavericks
