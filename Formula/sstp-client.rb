d9acbfcc491c36946038d019d8a1a905343bca0d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 07:18:18 +0100

    sstp-client: update 1.0.10 bottle.

--- Formula/sstp-client.rb
+++ Formula/sstp-client.rb
@@ -6,6 +6,7 @@ class SstpClient < Formula
 
   bottle do
     cellar :any
+    sha256 "fd3e7126998bbb07c0727b550b63322fe1eed186f92d4ae446133b427b2e911a" => :sierra
     sha256 "d70a56f837f32f2ceab2050fc32855428f6c1df368da447491107dd3c80c030b" => :el_capitan
     sha256 "8c4a3b20b2b92a4dfca222ecb3ccf46a1c20c8cdbcdab60fe6907e81f039927b" => :yosemite
     sha256 "5a07e517c449759938b41c886203fed7294667ba3bc185924e26b7d94267a92f" => :mavericks
