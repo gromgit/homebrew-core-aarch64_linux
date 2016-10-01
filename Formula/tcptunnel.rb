e3a0811f26cd319c30b7c37d7d84b47ca2e9924e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 10:27:21 +0100

    tcptunnel: update 0.8 bottle.

--- Formula/tcptunnel.rb
+++ Formula/tcptunnel.rb
@@ -6,6 +6,7 @@ class Tcptunnel < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8243b6410ae3d61df3d9c400be33c24b8da0fd0807161a02f38440c18d984661" => :sierra
     sha256 "e387a861c4a9ceb3014883c851cdc43a56eddba635e1d313d976095ff78bb686" => :el_capitan
     sha256 "513995a3f0a331a06ac6531ddad6e1812a9c32add2252852c81d8abe6714c5aa" => :yosemite
     sha256 "d8b4b1fc5969c71bdf24c0793df0d0bcf77475ffd6eeb12a74304d2c6e1c3b1c" => :mavericks
