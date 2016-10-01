0493c532e1061720f47281675f7b662f04da573b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 07:39:25 +0100

    stout: update 1.2.2 bottle.

--- Formula/stout.rb
+++ Formula/stout.rb
@@ -6,6 +6,7 @@ class Stout < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a1d9dfdf662db57af9a177ebffebb0aed586d7fb6e3c17df2eb9e7e70c7e4d7b" => :sierra
     sha256 "9020d9cfd6ca410fec81df29a31732c6c9025958e1247fb95f93e6c5942c389e" => :el_capitan
     sha256 "30cbc8417a496b515f41d743a883ad6821122b4ead8fe96b69d11d3cbd4a980b" => :yosemite
     sha256 "29c3f260aa2c7ac82a26304002fda6afe78ac9e7529b4b09f3fdaed031a8e764" => :mavericks
