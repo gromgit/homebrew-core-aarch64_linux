0f3d99a3d8dbe54c53a69cb6e8a06a867444ae06
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 12:38:17 +0100

    torrentcheck: update 1.00 bottle.

--- Formula/torrentcheck.rb
+++ Formula/torrentcheck.rb
@@ -6,6 +6,7 @@ class Torrentcheck < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c1169f112306b1f235297cba2c8920894e063b9b1f774e36be75f3f2c194bda5" => :sierra
     sha256 "ea6fbaa86be1c799c3baa4405aa1a750c2b3e1deb4bea0a412027d427f0922da" => :el_capitan
     sha256 "46426cdf1c627f448d54895b7f08379b90948030be346753104f5f6a5fabca3b" => :yosemite
     sha256 "ed300dfc8d1f7f7fe3c9c161b8f86cc6a379c7a4cca3914bb0c665d66ec6596a" => :mavericks
