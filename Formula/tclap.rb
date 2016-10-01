e2acd976c3f86375084f1be8a4ef8088532b9515
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 10:10:46 +0100

    tclap: update 1.2.1 bottle.

--- Formula/tclap.rb
+++ Formula/tclap.rb
@@ -6,6 +6,7 @@ class Tclap < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5f1ea1598586b9dfaa54d0362eb21cd2c9210006f46a7568eca09ded1c507dfa" => :sierra
     sha256 "71be8c4b552ec527decd02a459139353ca738b90d3971a560276a74694511caf" => :el_capitan
     sha256 "af4d9a41dbfbccda4f583e9dff77a8f93978a07f624df7a1f9ed30662c006274" => :yosemite
     sha256 "b93a8106cce72de8055b375519b93b00977e2368b758ccf3481497d075ec6738" => :mavericks
