69e108f635f4da4e2b1d2c8f0cd122dde0ede516
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 18:05:07 +0100

    xplanetfx: update 2.6.12 bottle.

--- Formula/xplanetfx.rb
+++ Formula/xplanetfx.rb
@@ -7,6 +7,7 @@ class Xplanetfx < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "37eb283551634da81516b7c0cd33beef8e1f44277b37305cedac53fd8233e1f8" => :sierra
     sha256 "a705f96d9dbb1d179a8df93c60fac8a134d8270403a20b730501692d59ca737e" => :el_capitan
     sha256 "da2d1f07a54aa76a853221024ad64ea599b1b7f57ed46cc0fb88538af44b75b8" => :yosemite
     sha256 "58d9b7fafb5ea11f87275502ec817784ba3c93e0ee1c9aceb646f7a349ca5ae4" => :mavericks
