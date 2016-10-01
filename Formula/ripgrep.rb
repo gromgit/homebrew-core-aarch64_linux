affe0b96533dfec80c12a31b04d1ab57a5f270d5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 06:40:03 +0100

    ripgrep: add 0.2.1 bottle.

--- Formula/ripgrep.rb
+++ Formula/ripgrep.rb
@@ -5,6 +5,13 @@ class Ripgrep < Formula
   sha256 "372ccd0a93c98e9f3cc51644a9c52d1d8437ecb8b0e2908b33df9a46ca7b9ee2"
   head "https://github.com/BurntSushi/ripgrep.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f01bd007f11a52584a13311f8c5d07273165041b55d2b29092eb00cc3b776fe9" => :sierra
+    sha256 "55fff501f56c25413cef2cdd9df32bb3e2339256728f6b3d6dc59e2a49159cd4" => :el_capitan
+    sha256 "1cfd3d638d0520b4f2bd370c8671a9d29f99316ebee963d86e27ad89ca694fbf" => :yosemite
+  end
+
   depends_on "rust" => :build
 
   def install
