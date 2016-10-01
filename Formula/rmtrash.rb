20e2757ede649fb59fb084a1d41d65ca4a7c5562
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 03:37:17 +0100

    rmtrash: update 0.3.3 bottle.

--- Formula/rmtrash.rb
+++ Formula/rmtrash.rb
@@ -7,7 +7,8 @@ class Rmtrash < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
+    sha256 "e9770d2d1fef053ba9743548537fb70ca491755c10c0a6b6d106e919d86c4b54" => :sierra
     sha256 "a8b6815f555377a91e800085db9680cc9234b27a4b114ea13e6664a2b1b663d3" => :el_capitan
     sha256 "adab3849ec805b7c2679e9b278e4d101249193dbd7210f078224e69fc3bb2f1f" => :yosemite
     sha256 "102f54f713574d7009e3b4fd33738b6b631608a0c38f6ad0945ca1af5a8d6ef5" => :mavericks
