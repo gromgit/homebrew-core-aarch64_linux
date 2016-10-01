e71b63bb46909976aae6e59c5aeb6520edc78d84
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 10:27:22 +0100

    td: update 1.3.0 bottle.

--- Formula/td.rb
+++ Formula/td.rb
@@ -8,6 +8,7 @@ class Td < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c4a2ee497032f6ca01acbbd66ef0fbc27709fd463d0d33a583b1c6de57a18edf" => :sierra
     sha256 "867b8f9f687f76225eebbb3b183206178fb1321f87e14e6a101bdf40d2f9fbce" => :el_capitan
     sha256 "38428b68601f84a7cf0360bea09b17d891bd5bb2a5d6fbc6bce420011de698d2" => :yosemite
     sha256 "f73a1a4e82115217791c73e95938f474f906f5215164abd71d71c9304674895e" => :mavericks
