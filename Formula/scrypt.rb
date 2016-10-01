6fc626aa903dc8c64f60ececbe4b583176c54fac
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 05:09:51 +0100

    scrypt: update 1.2.0 bottle.

--- Formula/scrypt.rb
+++ Formula/scrypt.rb
@@ -6,7 +6,8 @@ class Scrypt < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "52fa80754404f2b6a6afba727c02e339c4066d5235a0648b5e07492771ce937a" => :sierra
     sha256 "a81147625da927a61035f18d320a56d4b7e0055cc3ff0640dde0e6f2c0cada9d" => :el_capitan
     sha256 "ad6fee3523c53b2f323b896d60de404c1358459ceffdf8a9014967b045863051" => :yosemite
     sha256 "fc7272e275fd43e3c5a63bf00294564823a944639ab60ed10aaae40cf2a67f8a" => :mavericks
