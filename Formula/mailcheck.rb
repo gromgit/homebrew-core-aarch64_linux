fc251fd36fee5384ddb4c47cda90890f34cd9a0e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 20:49:05 +0100

    mailcheck: update 1.91.2 bottle.

--- Formula/mailcheck.rb
+++ Formula/mailcheck.rb
@@ -6,6 +6,7 @@ class Mailcheck < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8d33e3b08eef4dfaa7fa3d2c4e5f4a697cd2e5eb950c963f1f0845c0651da5ea" => :sierra
     sha256 "b7c134dc23431dfaa3f402b859b7154cab5e176711363bd884dc82ce896d7c7a" => :el_capitan
     sha256 "242b05a6e9b8ccc1ac70e22cbf89bc33a885e726d32509fad6b34a3bee123945" => :yosemite
     sha256 "32b40cf41ec15bcd0efbfb90858534e4b84056915ceacd6914d71d8acdffeb6f" => :mavericks
