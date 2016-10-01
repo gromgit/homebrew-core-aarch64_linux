214f7a034ac82ac62d1c54f13493a015840eebdc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 17:20:10 +0100

    xml-coreutils: update 0.8.1 bottle.

--- Formula/xml-coreutils.rb
+++ Formula/xml-coreutils.rb
@@ -6,7 +6,8 @@ class XmlCoreutils < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
+    sha256 "5f7519c9be40f731b0dca6238b3bedf4070f0663fc47ab8e4b0eff02d187718c" => :sierra
     sha256 "19bdcacd49657e78f82fd7743a50266ff4945e644b069ac2c39a8787a57911a5" => :el_capitan
     sha256 "1342c807e5ddc23a72e750f07258864fdf2fc1a8ce9072cb7797955fdd0e3656" => :yosemite
     sha256 "6bada2b9690d698b77f9293cf7f1066e77d36c8c07ba2892b2869ddb0516bc6d" => :mavericks
