4e2b3c3f3036336576c308b2553c0f2cc2e55f74
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 06:16:36 +0100

    seexpr: update 2.11 bottle.

--- Formula/seexpr.rb
+++ Formula/seexpr.rb
@@ -6,6 +6,7 @@ class Seexpr < Formula
 
   bottle do
     cellar :any
+    sha256 "762881f740d763d3c2477f5ead9b2fc7c549f38721b39e999f9b6634f5215b68" => :sierra
     sha256 "0a374c2f84169d0b4ab064dfdd2dee856d5f80c9b8b27eee57e3d1614abffaa9" => :el_capitan
     sha256 "c023332d27c1977db807f890011688ed756eb130fa1df40aa562776192733ffd" => :yosemite
     sha256 "2e55d79f1519adbf0f1fdcf566aa67bd5515a60132516e2908250831d8e40355" => :mavericks
