04f2b8c9dfc1a3b6b06be39e217f19523b0722be
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 13:33:36 +0100

    treecc: update 0.3.10 bottle.

--- Formula/treecc.rb
+++ Formula/treecc.rb
@@ -6,6 +6,7 @@ class Treecc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0b3e61d5a910222d170fcee80d094be0dcd2707b7bebc6d40667a8f25b4b2e5c" => :sierra
     sha256 "e74d23594113e594ad8021fe55b0f0f863fcd4b01140c3fd8b1a5f2bb6c8ad74" => :el_capitan
     sha256 "595dada9ecb2cef6d3e225e99a98997968d15f8009038511c464b6499cbcd872" => :yosemite
     sha256 "9f9a9e6a66c9e0a60888ad2af502070683637b5cd19dec6e080211a45c3313e6" => :mavericks
