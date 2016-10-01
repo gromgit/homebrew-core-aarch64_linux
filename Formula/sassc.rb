6abb70235294295148daed4004c14898631a2a6a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 04:49:01 +0100

    sassc: update 3.3.2 bottle.

--- Formula/sassc.rb
+++ Formula/sassc.rb
@@ -6,6 +6,7 @@ class Sassc < Formula
 
   bottle do
     cellar :any
+    sha256 "5fbfde57c9d7bd1c1702f3a5979055adc1bf742e6529f0c1e2a12a32228f35c3" => :sierra
     sha256 "afcce4ded5d004f16ac4ae183299db22ff63ca08a3dc0739f1de2aa88a49fe56" => :el_capitan
     sha256 "dcc04e5f93e260a94d60449e815dfb6ca1fbd84964b7d40d038a3d4fa1e362b8" => :yosemite
     sha256 "b194d61b61895ba2082c4ece22ae51abd1a2ab9c16449acc7f5b6f7a77ee17a6" => :mavericks
