dfa6c350cd736450d8483044c33b2a1fcda4c05e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 03:06:05 +0100

    redsocks: update 0.4 bottle.

--- Formula/redsocks.rb
+++ Formula/redsocks.rb
@@ -6,6 +6,7 @@ class Redsocks < Formula
 
   bottle do
     cellar :any
+    sha256 "20fdde6819d44b7034a1af7ed8e30ba4ae42abfe5c70e8c067908de3c9bdcfed" => :sierra
     sha256 "ad3df81c5bd45efce293e229c77ff0687141646024a41a0c7cae3b3da34dcdcc" => :el_capitan
     sha256 "d59f7cd44ed44e76a1d03d83fdd7385330425d42913b4bab36e180c9b082d601" => :yosemite
     sha256 "a6b82407933ada8111fe21b0491b051aa05f31cdb769840f50e6af5cc4f2c96e" => :mavericks
