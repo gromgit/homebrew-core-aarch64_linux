977606d2a51e4e18ed491ad93aa943f0ee9f819f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 12:19:39 +0100

    tnote: update 0.2.1 bottle.

--- Formula/tnote.rb
+++ Formula/tnote.rb
@@ -6,6 +6,7 @@ class Tnote < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6665cd2351aff6cf025483f711b9620667a2d32e275230cb80d5d790477c5e3f" => :sierra
     sha256 "63b2c1aea236fd24d2fa5c315bd0772b009d63018b9c18379a5e782f65debfea" => :el_capitan
     sha256 "de7e2e72f85a8c42133329f77d242aa0b8a6c9cb2edbb305d843a6e7be1ea3b0" => :yosemite
     sha256 "8e3a2baf4185b131e0223e6b0fe113890ace15b6a9c67b5f7a4068b3d767c4e6" => :mavericks
