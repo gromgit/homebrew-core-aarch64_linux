fe73ce3b7f511f7a4778eb5a080593ab47c959c9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 20:20:56 +0100

    yamdi: update 1.9 bottle.

--- Formula/yamdi.rb
+++ Formula/yamdi.rb
@@ -6,6 +6,7 @@ class Yamdi < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "546a4c5400ef75431ecd3a39dbabda5e5599d82ac3f65f6dafc5d3745a90d8e2" => :sierra
     sha256 "cfaf451a985b0a8cba24a0131c8e0e9a6102eb4b6c315e045ce258999cb19494" => :el_capitan
     sha256 "7041c6dcf877e8e003e2acae68a75ae6a461e92df63fde374157884b52cf2d82" => :yosemite
     sha256 "f0a2a40d6667893a51fb5082a3c09685b688a06df2df8d8b42c350c1dd0f6e16" => :mavericks
