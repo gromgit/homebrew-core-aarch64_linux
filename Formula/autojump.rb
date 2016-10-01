ad4a82f9099367ddd04904fc28e19cfd979ae7d4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 07:55:43 +0100

    autojump: update 22.4.1 bottle.

--- Formula/autojump.rb
+++ Formula/autojump.rb
@@ -7,10 +7,9 @@ class Autojump < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "fe815d90822007242fd254529774660c5c85becdfff5353f1935f9d2e30b0dd3" => :sierra
-    sha256 "3094fb0c2ad201dfe6718568dff87ad141bcfade1bb7e088d66101f94f252dd3" => :el_capitan
-    sha256 "fe815d90822007242fd254529774660c5c85becdfff5353f1935f9d2e30b0dd3" => :yosemite
-    sha256 "fe815d90822007242fd254529774660c5c85becdfff5353f1935f9d2e30b0dd3" => :mavericks
+    sha256 "274b5bb680afeb031e1abfabc7d79060f4779e85014f715708f9e273ebab0813" => :sierra
+    sha256 "274b5bb680afeb031e1abfabc7d79060f4779e85014f715708f9e273ebab0813" => :el_capitan
+    sha256 "274b5bb680afeb031e1abfabc7d79060f4779e85014f715708f9e273ebab0813" => :yosemite
   end
 
   def install
