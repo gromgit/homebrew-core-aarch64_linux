a743072b98567a479a3212bd53aef4cc4c702731
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 09:49:12 +0100

    tal: update 1.9 bottle.

--- Formula/tal.rb
+++ Formula/tal.rb
@@ -6,6 +6,7 @@ class Tal < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9c2c4e5f9d6922f9a9d434485dea4ddf321744728c83adcda822c3c314f6a86e" => :sierra
     sha256 "bbdef6b2c92650352b7199cc2a9e3bc4698bf2a14fce46397eebcee72c1de419" => :el_capitan
     sha256 "e6b6f315bc14f5e001893371d18fb0ba88bea4c4d76dd657820eecf509103f9e" => :yosemite
     sha256 "26945b8471e2731ce34604a19f8bdb046770d961422969999a789168aa7ee25b" => :mavericks
