b0c7f78568619a08ee2ed14e3f7d6bba624c1aa3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-02 10:06:28 +0100

    ltc-tools: update 0.6.4 bottle.

--- Formula/ltc-tools.rb
+++ Formula/ltc-tools.rb
@@ -7,6 +7,7 @@ class LtcTools < Formula
 
   bottle do
     cellar :any
+    sha256 "659226a125668939abb2c8e2b89e18ae2805d225e9b7295af83adfbb4b2b00a5" => :sierra
     sha256 "b3b9a4d9f9969a81be3aa7a51b35a8c1f61a0c7b839fe3778a9665996faa4925" => :el_capitan
     sha256 "e3bb3cca684ddf01e1d6e8f1830609a3de22a8cc654d29db959dccd211766489" => :yosemite
     sha256 "12b8773c9045c5b3e8c5d57f085e02ee078f4807f8fc0b868c5d2eee99b5ea49" => :mavericks
