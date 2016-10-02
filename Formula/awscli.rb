b15b095c326879b9f8dc9396d9821ffe93bb3e43
ilovezfs <ilovezfs@icloud.com>
2016-10-01 21:45:36 -0700

    awscli: remove resources
    this will allow the formula to be maintained with bump-formula-pr
    
    Closes #5500.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -1,11 +1,11 @@
-# Make sure that you also bump botocore
 class Awscli < Formula
   include Language::Python::Virtualenv
 
   desc "Official Amazon AWS command-line interface"
   homepage "https://aws.amazon.com/cli/"
-  url "https://files.pythonhosted.org/packages/32/ec/ca73928d42d01468eff9b6e6541375d0b5bb9494a554cc7be7564b309a19/awscli-1.11.0.tar.gz"
-  sha256 "48f26765be66689ef073e086873b016885f443471e542156d46376b527aa2d63"
+  url "https://github.com/aws/aws-cli/archive/1.11.0.tar.gz"
+  sha256 "a50d005447fa8db2a2d90e5b84c27396134206ec14f6606ea0d8f1c1694540bf"
+  head "https://github.com/aws/aws-cli.git", :branch => "develop"
 
   bottle do
     cellar :any_skip_relocation
@@ -14,78 +14,18 @@ class Awscli < Formula
     sha256 "c9806f56bb3628e02f614638d9c7b0c1bff9e22b83d00e73331ef170683c8f75" => :yosemite
   end
 
-  head do
-    url "https://github.com/aws/aws-cli.git", :branch => "develop"
-
-    resource "botocore" do
-      url "https://github.com/boto/botocore.git", :branch => "develop"
-    end
-
-    resource "jmespath" do
-      url "https://github.com/boto/jmespath.git", :branch => "develop"
-    end
-
-    resource "s3transfer" do
-      url "https://github.com/boto/s3transfer.git", :branch => "develop"
-    end
-  end
-
   # Use :python on Lion to avoid urllib3 warning
   # https://github.com/Homebrew/homebrew/pull/37240
   depends_on :python if MacOS.version <= :lion
 
-  resource "botocore" do
-    url "https://files.pythonhosted.org/packages/48/46/9f178ccd2b6c95cf90be659811c787b92f0168a041c7539711479c67ecb2/botocore-1.4.58.tar.gz"
-    sha256 "a464bb803faffdd689c732768e472da8732b87795bc03ed4342a2ab99338e9f7"
-  end
-
-  resource "colorama" do
-    url "https://files.pythonhosted.org/packages/f0/d0/21c6449df0ca9da74859edc40208b3a57df9aca7323118c913e58d442030/colorama-0.3.7.tar.gz"
-    sha256 "e043c8d32527607223652021ff648fbb394d5e19cba9f1a698670b338c9d782b"
-  end
-
-  resource "docutils" do
-    url "https://files.pythonhosted.org/packages/37/38/ceda70135b9144d84884ae2fc5886c6baac4edea39550f28bcd144c1234d/docutils-0.12.tar.gz"
-    sha256 "c7db717810ab6965f66c8cf0398a98c9d8df982da39b4cd7f162911eb89596fa"
-  end
-
-  resource "futures" do
-    url "https://files.pythonhosted.org/packages/55/db/97c1ca37edab586a1ae03d6892b6633d8eaa23b23ac40c7e5bbc55423c78/futures-3.0.5.tar.gz"
-    sha256 "0542525145d5afc984c88f914a0c85c77527f65946617edb5274f72406f981df"
-  end
-
-  resource "jmespath" do
-    url "https://files.pythonhosted.org/packages/8f/d8/6e3e602a3e90c5e3961d3d159540df6b2ff32f5ab2ee8ee1d28235a425c1/jmespath-0.9.0.tar.gz"
-    sha256 "08dfaa06d4397f283a01e57089f3360e3b52b5b9da91a70e1fd91e9f0cdd3d3d"
-  end
-
-  resource "pyasn1" do
-    url "https://files.pythonhosted.org/packages/f7/83/377e3dd2e95f9020dbd0dfd3c47aaa7deebe3c68d3857a4e51917146ae8b/pyasn1-0.1.9.tar.gz"
-    sha256 "853cacd96d1f701ddd67aa03ecc05f51890135b7262e922710112f12a2ed2a7f"
-  end
-
-  resource "python-dateutil" do
-    url "https://files.pythonhosted.org/packages/3e/f5/aad82824b369332a676a90a8c0d1e608b17e740bbb6aeeebca726f17b902/python-dateutil-2.5.3.tar.gz"
-    sha256 "1408fdb07c6a1fa9997567ce3fcee6a337b39a503d80699e0f213de4aa4b32ed"
-  end
-
-  resource "rsa" do
-    url "https://files.pythonhosted.org/packages/14/89/adf8b72371e37f3ca69c6cb8ab6319d009c4a24b04a31399e5bd77d9bb57/rsa-3.4.2.tar.gz"
-    sha256 "25df4e10c263fb88b5ace923dd84bf9aa7f5019687b5e55382ffcdb8bede9db5"
-  end
-
-  resource "s3transfer" do
-    url "https://files.pythonhosted.org/packages/04/92/7976f89bc162d62a6e3014497694ad2caed0f6f920bdc1b679fefedb9440/s3transfer-0.1.5.tar.gz"
-    sha256 "c500b27b5a8693507e4768730c4c423d4717477c8676139fc764ac4e8e88708e"
-  end
-
-  resource "six" do
-    url "https://files.pythonhosted.org/packages/b3/b2/238e2590826bfdd113244a40d9d3eb26918bd798fc187e2360a8367068db/six-1.10.0.tar.gz"
-    sha256 "105f8d68616f8248e24bf0e9372ef04d3cc10104f1980f54d57b2ce73a5ad56a"
-  end
-
   def install
-    virtualenv_install_with_resources
+    virtualenv_create(libexec)
+    bin_before = Dir[libexec/"bin/*"].to_set
+    system libexec/"bin/pip", "install", "-v", "--no-binary", ":all:",
+                              "--ignore-installed", buildpath
+    bin_after = Dir[libexec/"bin/*"].to_set
+    bin_to_link = (bin_after - bin_before).to_a
+    bin.install_symlink(bin_to_link)
 
     pkgshare.install "awscli/examples"
 
@@ -108,6 +48,6 @@ class Awscli < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/aws --version 2>&1")
+    assert_match "topics", shell_output("#{bin}/aws help")
   end
 end
