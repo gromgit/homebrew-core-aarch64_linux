cf5b89a454664308d7e71e94719843cdfa1f9760
ilovezfs <ilovezfs@icloud.com>
2016-10-01 18:36:55 -0700

    rush 1.8
    Closes #5494.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/rush.rb
+++ Formula/rush.rb
@@ -1,9 +1,9 @@
 class Rush < Formula
   desc "GNU's Restricted User SHell"
   homepage "https://www.gnu.org/software/rush/"
-  url "https://ftpmirror.gnu.org/rush/rush-1.7.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/rush/rush-1.7.tar.gz"
-  sha256 "35077fa36902fd451db52b49bf059992a20cc8ea031437171f384670d77a003a"
+  url "https://ftpmirror.gnu.org/rush/rush-1.8.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/rush/rush-1.8.tar.gz"
+  sha256 "7f321e5556435b12638982c8d94c587982122f28c220f8bc679d7817035aadef"
 
   bottle do
     sha256 "62c2ec419695bcfee37b7af7e934220e3ded257be452f67ad632a67c8580e056" => :sierra
