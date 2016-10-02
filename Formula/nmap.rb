6a267de118a7085195c0f51fca72949318da7923
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-02 14:44:02 +0100

    nmap: update 7.30 bottle.

--- Formula/nmap.rb
+++ Formula/nmap.rb
@@ -6,10 +6,9 @@ class Nmap < Formula
   head "https://guest:@svn.nmap.org/nmap/", :using => :svn
 
   bottle do
-    sha256 "eb56ae006d639d6b524ec8fa9c47cb5166e3cd119f107a835d1a12b575f52e4d" => :sierra
-    sha256 "1225505f7b77cafa55d300adffc2e1c5945d090a20cb18c417bead62a2eb0409" => :el_capitan
-    sha256 "a57b65e0c14e232b332b332bbaa5e3ab9b07b4b07e9e3f36dc1cc6afbb4d42f9" => :yosemite
-    sha256 "be7ba56789fa1aa900396955d2a99c1929907ca8df1a9ccd37081d5178c440de" => :mavericks
+    sha256 "2db273dbacd4041ad9bd5aefe457aad9a35efaf3817e3e94caa8f4819465529a" => :sierra
+    sha256 "b4abf3d589eb43e5bbd8ae831d75a813eeba2579382528d43ed2b6656625016b" => :el_capitan
+    sha256 "f564efc3da6773f39e491e23280c72b1e6e91251db6841377af7450cb64edaaa" => :yosemite
   end
 
   option "with-pygtk", "Build Zenmap GUI"
