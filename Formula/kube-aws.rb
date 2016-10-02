3af9f6ea89d0dbf1e12785fe3091f3d97042eca2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-02 04:37:14 +0100

    kube-aws: add 0.8.2 bottle.

--- Formula/kube-aws.rb
+++ Formula/kube-aws.rb
@@ -7,6 +7,13 @@ class KubeAws < Formula
   sha256 "85125641fca7431d8844192a22fb981dbd3de9c568aff2a7638812bf30f26d22"
   head "https://github.com/coreos/coreos-kubernetes.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b9423ac76c7163cf9b6810740ba27ac12f43a2281b946fb84f8ac51aea5bff72" => :sierra
+    sha256 "05a8a4f3a52cf8be2ddb703fc6538528cbcc8c545666347f1f2cb54672db1d1e" => :el_capitan
+    sha256 "5f66d7addd9e2c71a625e90083c88c9070cc76e16b2864f19c61c7b0ea3227eb" => :yosemite
+  end
+
   depends_on "go" => :build
 
   def install
