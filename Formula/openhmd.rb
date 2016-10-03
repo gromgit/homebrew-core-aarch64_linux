35ce640744b41c9ca39a6d4b77dfcff965151826
ilovezfs <ilovezfs@icloud.com>
2016-10-03 05:16:54 -0700

    openhmd 0.2.0
    Closes #5531.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/openhmd.rb
+++ Formula/openhmd.rb
@@ -1,8 +1,9 @@
 class Openhmd < Formula
   desc "Free and open source API and drivers for immersive technology"
   homepage "http://openhmd.net"
-  url "http://openhmd.net/releases/openhmd-0.1.0.tar.gz"
-  sha256 "14e54b4cd33d7cd961a2c9724efb77a5504fd4dc6091d488e7737a0e2f2df51d"
+  url "https://github.com/OpenHMD/OpenHMD/archive/0.2.0.tar.gz"
+  sha256 "b5be787229d7c7c5cb763cec6207f9814b0bb993c68842ef0a390184ca25380d"
+  head "https://github.com/OpenHMD/OpenHMD.git"
 
   bottle do
     cellar :any
@@ -13,34 +14,26 @@ class Openhmd < Formula
     sha256 "d3b4293c6c9f9979cf36e259da2775065c8732107f54f12a7bf55d08d9a2ac79" => :mavericks
   end
 
-  head do
-    url "https://github.com/OpenHMD/OpenHMD.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "hidapi"
 
   conflicts_with "cspice", :because => "both install `simple` binaries"
   conflicts_with "libftdi0", :because => "both install `simple` binaries"
 
-  depends_on "cmake" => :build
-  depends_on "pkg-config" => :build
-  depends_on "hidapi"
-
   def install
-    args = ["--prefix", prefix,
-            "--disable-debug",
-            "--disable-silent-rules",
-            "--disable-dependency-tracking",
-            "--bindir=#{bin}"]
-
-    system "./autogen.sh" if build.head?
-    system "./configure", *args
+    system "./autogen.sh"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
     system "make", "install"
-    (share+"tests").install "#{bin}/unittests"
+    (pkgshare/"tests").install bin/"unittests"
   end
 
   test do
-    system "#{share}/tests/unittests"
+    system pkgshare/"tests/unittests"
   end
 end
