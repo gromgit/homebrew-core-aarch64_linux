034a033280ccd7d734175a1bce1737061ec83afc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-03 09:26:55 +0100

    midnight-commander: update 4.8.18 bottle.

--- Formula/midnight-commander.rb
+++ Formula/midnight-commander.rb
@@ -7,11 +7,9 @@ class MidnightCommander < Formula
   head "https://github.com/MidnightCommander/mc.git"
 
   bottle do
-    rebuild 1
-    sha256 "e43fedc65c05ea7c79de7e861370f3f69ede6b721db29dbc77bd50b5a8e64f57" => :sierra
-    sha256 "645437aeeac3ee18c74bdc580550c9a6ef93411adebcd068f1c4061ef2a5a643" => :el_capitan
-    sha256 "b7efc5f7d40b2d76a701add5f5dff35b8e6718f09fe749eb72d6258e5616ed0c" => :yosemite
-    sha256 "2b6b3fee4eb893e62900a048b080dfafa2a1f029a04b4cb251d9f35487d52bb9" => :mavericks
+    sha256 "17a0c9039bd8f1c862ea282a3305d91b75759a3c6200056e5e1c4ff38ba1e9ff" => :sierra
+    sha256 "7f0bc00fdfa069c8139f3aebff3621824dfa4b5f6c6b4688336162615a2bb4fa" => :el_capitan
+    sha256 "033b658071cefd0af127e1a9a88cf5bddd9bafc50b583df9f6f850f2a457d34d" => :yosemite
   end
 
   option "without-nls", "Build without Native Language Support"
