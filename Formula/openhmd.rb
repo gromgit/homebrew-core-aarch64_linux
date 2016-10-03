757477cd4bdde0d10f34969dd520490f4c235ba5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-03 13:52:06 +0100

    openhmd: update 0.2.0 bottle.

--- Formula/openhmd.rb
+++ Formula/openhmd.rb
@@ -7,11 +7,9 @@ class Openhmd < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "cee3cb751d0bc2312f23a0a8747abab8b7b85e199deb8a36de6aee97a61c85e8" => :sierra
-    sha256 "50917839a2a060a9a350dad6117bc3bc946a37c129774a4c8e2be89aa9ecec16" => :el_capitan
-    sha256 "7d50acf26200c9ef86554a79ec610d745ce6c5b4188cd882b00222ec3faa5fea" => :yosemite
-    sha256 "d3b4293c6c9f9979cf36e259da2775065c8732107f54f12a7bf55d08d9a2ac79" => :mavericks
+    sha256 "999945d3e8003410777939f746f8f460c767ab2824dc2d1de3ab24af532daf0d" => :sierra
+    sha256 "0e94a1356e5f6f0fb4079e16f431aae23003fa00c3ccaa8add9d5dd382b89b34" => :el_capitan
+    sha256 "71e356cdd47df73193ec52341d4bc252237e48b8bad182d695fbeb74a3ed41ed" => :yosemite
   end
 
   depends_on "autoconf" => :build
