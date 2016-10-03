c2814a0a4a5b9eaff317be3a21a5379e2b5066f3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-03 20:44:42 +0100

    sqldiff: add 3.14.2 bottle.

--- Formula/sqldiff.rb
+++ Formula/sqldiff.rb
@@ -5,6 +5,13 @@ class Sqldiff < Formula
   version "3.14.2"
   sha256 "52507e20c2757b24b703b43ede77ce464c8106c1658a5b357974c435aa0677a6"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "de0d0f9add4b98bd62f94f37d6545a2a64a38ea018dd80839e29a39ca3f64a9e" => :sierra
+    sha256 "c464cbc13adb7a8fc4824810aee5ca8e7d0b10e9857a2e85f60e66871900f299" => :el_capitan
+    sha256 "4c2da93e7410f42a2c72634df29347b58141622fc3f4c863ae109015a812dfe6" => :yosemite
+  end
+
   def install
     system "./configure", "--disable-debug", "--prefix=#{prefix}"
     system "make", "sqldiff"
