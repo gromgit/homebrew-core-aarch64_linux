80cdac9e38a543fdb01aec83a03e9a44e974f69b
ilovezfs <ilovezfs@icloud.com>
2016-10-03 05:44:22 -0700

    libjson-rpc-cpp: revision for jsoncpp

--- Formula/libjson-rpc-cpp.rb
+++ Formula/libjson-rpc-cpp.rb
@@ -3,6 +3,8 @@ class LibjsonRpcCpp < Formula
   homepage "https://github.com/cinemast/libjson-rpc-cpp"
   url "https://github.com/cinemast/libjson-rpc-cpp/archive/v0.7.0.tar.gz"
   sha256 "669c2259909f11a8c196923a910f9a16a8225ecc14e6c30e2bcb712bab9097eb"
+  revision 1
+
   head "https://github.com/cinemast/libjson-rpc-cpp.git"
 
   bottle do
