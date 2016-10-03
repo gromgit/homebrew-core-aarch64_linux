540ef4a1ae7c4b2ec8eca214dc930d01fb4397e3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-03 12:13:27 +0100

    amqp-cpp: add 2.6.2 bottle.

--- Formula/amqp-cpp.rb
+++ Formula/amqp-cpp.rb
@@ -5,6 +5,13 @@ class AmqpCpp < Formula
   sha256 "1a60d900a8e32b55b39229f2ea00070156b99c0a1336450d531038e6241a4d8b"
   head "https://github.com/CopernicaMarketingSoftware/AMQP-CPP.git"
 
+  bottle do
+    cellar :any
+    sha256 "f6a140f35b267b9cdd933da245bc822b41eb39ce9cf7728fdaf2e2b7b1cd85a5" => :sierra
+    sha256 "d55ffcc984e7c28383677382523e50fd706fb7a3e61daf3f4a531fd609f80491" => :el_capitan
+    sha256 "e2df5e3dd1ed00d3400addc5b79479b91e0fc34dead3b6c45b8a2aacf3f043d6" => :yosemite
+  end
+
   needs :cxx11
 
   depends_on "cmake" => :build
