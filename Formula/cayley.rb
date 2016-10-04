77a7252cc87c9a87a473e40266e7c87fc28610c9
ilovezfs <ilovezfs@icloud.com>
2016-10-04 11:50:59 -0700

    cayley 0.6.0
    glide vendoring
    
    Closes #5315.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/cayley.rb
+++ Formula/cayley.rb
@@ -1,8 +1,8 @@
 class Cayley < Formula
   desc "Graph database inspired by Freebase and Knowledge Graph"
-  homepage "https://github.com/google/cayley"
-  url "https://github.com/google/cayley/archive/v0.5.0.tar.gz"
-  sha256 "2f48445377bc73b125a7f65c01307fa301e4996e536fa83b42a2fcbaa2141e82"
+  homepage "https://github.com/cayleygraph/cayley"
+  url "https://github.com/cayleygraph/cayley/archive/v0.6.0.tar.gz"
+  sha256 "b55d6b02567dd0a1c51001cb25d5bde602358f621cdf78ba40bdf8a8c51422b0"
   head "https://github.com/google/cayley.git"
 
   bottle do
@@ -17,16 +17,18 @@ class Cayley < Formula
 
   depends_on "bazaar" => :build
   depends_on :hg => :build
+  depends_on "glide" => :build
   depends_on "go" => :build
-  depends_on "godep" => :build
 
   def install
     ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/google/cayley").install buildpath.children
-    cd "src/github.com/google/cayley" do
-      system "godep", "restore"
-      system "go", "build", "-ldflags=-X main.Version=#{version}", "cmd/cayley/cayley.go"
-      bin.install "cayley"
+    ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
+
+    (buildpath/"src/github.com/cayleygraph/cayley").install buildpath.children
+    cd "src/github.com/cayleygraph/cayley" do
+      system "glide", "install"
+      system "go", "build", "-o", bin/"cayley", "-ldflags",
+             "-X main.Version=#{version}", ".../cmd/cayley"
 
       inreplace "cayley.cfg.example", "/tmp/cayley_test", var/"cayley"
       etc.install "cayley.cfg.example" => "cayley.conf"
