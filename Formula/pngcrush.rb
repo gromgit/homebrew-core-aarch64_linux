78dd33dedc7f13d272092553ff5cf431bdb4415f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-04 10:09:15 +0100

    pngcrush: update 1.8.7 bottle.

--- Formula/pngcrush.rb
+++ Formula/pngcrush.rb
@@ -6,10 +6,9 @@ class Pngcrush < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b6c671d45d99d6977faefca33d4346d37db5b644cf74fbde82ba353b1337d096" => :sierra
-    sha256 "c4ed270d22feeb48689542d44dd3225cca13d27657e147a0aa6220027c55be07" => :el_capitan
-    sha256 "bf46beae36a0ad019b9d6c5a676612b45aab99dc4d28273c74b980dcd00fe10b" => :yosemite
-    sha256 "ed228209fe5e22d1c298843852987649818dbc9e0f00b93165f608a2c650a7e1" => :mavericks
+    sha256 "c693bce9590eae0b7b8abda1c234eebc3a81ecd07cebb253d564bb7afb2fed08" => :sierra
+    sha256 "ca2b8ef288519b402029386616a3c0e591b9dd157df84e8c219106465d320642" => :el_capitan
+    sha256 "884fd72417efb852d049114ba06f276a750fa03ffef5bbd956784a119f1947c2" => :yosemite
   end
 
   def install
