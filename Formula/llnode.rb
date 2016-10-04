1c933449266ea1fca23853339a29287f446464f0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-04 11:12:46 +0100

    llnode: update 1.2.2 bottle.

--- Formula/llnode.rb
+++ Formula/llnode.rb
@@ -6,9 +6,9 @@ class Llnode < Formula
 
   bottle do
     cellar :any
-    sha256 "303944ab0881818f0c24e1e71431b14927c2621adab4beff640fc69659de2cfe" => :sierra
-    sha256 "0d7c7fab104e495e5f392d900d8f187bb10a99088d63cca6cce5ced042b1a6aa" => :el_capitan
-    sha256 "e761fdb6dc583cead747bcce0907d023ac800c18f0d754397187e3018f513874" => :yosemite
+    sha256 "fd5f6a10d91bbd4ea48f0a91745d9a14ad7f8e25c939d6955dcc79d5f6355540" => :sierra
+    sha256 "9dad366ed90f4f97c266f2df7875460e5a7b2525ae427cd48f276d0e5a671156" => :el_capitan
+    sha256 "75be2616873cff178cf6cc5dc14809922ab5089aca3d334a7845e4064dd15a98" => :yosemite
   end
 
   depends_on :macos => :yosemite
