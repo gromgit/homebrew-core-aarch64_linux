48002a7d3f66dcd9ca2fdbf008761f744836b6a1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-05 00:35:02 +0100

    nmh: add 1.6 bottle.

--- Formula/nmh.rb
+++ Formula/nmh.rb
@@ -4,6 +4,12 @@ class Nmh < Formula
   url "https://download.savannah.gnu.org/releases/nmh/nmh-1.6.tar.gz"
   sha256 "29338ae2bc8722fe8a5904b7b601a63943b72b07b6fcda53f3a354edb6a64bc3"
 
+  bottle do
+    sha256 "62b6f1754f82dcf84674eeddfe2bf7637262324b384e6d90a52bd2a694061da0" => :sierra
+    sha256 "8c508d92233154eb888357f62d84440526fd2acea65566ef5d3325fd0b57caa8" => :el_capitan
+    sha256 "bdd5c9d0e4bd341df41972b4e651761798660eadbac1332409e184e5e8e2b877" => :yosemite
+  end
+
   depends_on "openssl"
 
   def install
