160020005b9526ae2a12534363fd7f997db608c6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-05 00:05:12 +0100

    archi-steam-farm: add 2.1.5 bottle.

--- Formula/archi-steam-farm.rb
+++ Formula/archi-steam-farm.rb
@@ -4,6 +4,13 @@ class ArchiSteamFarm < Formula
   url "https://github.com/JustArchi/ArchiSteamFarm/releases/download/2.1.5.4/ASF.zip"
   sha256 "244f2e213927ad4512036a30bff201d94db1129a9f8650e39bdbf22a0f4951f8"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3688dab74bde7418f71352ed30ccef036336351b660daf306567173f71997d55" => :sierra
+    sha256 "3688dab74bde7418f71352ed30ccef036336351b660daf306567173f71997d55" => :el_capitan
+    sha256 "3688dab74bde7418f71352ed30ccef036336351b660daf306567173f71997d55" => :yosemite
+  end
+
   depends_on "mono"
 
   def install
