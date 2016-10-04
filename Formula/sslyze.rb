d5068f0f854fc4a912750cb5c6b2fe39fb456ec7
ilovezfs <ilovezfs@icloud.com>
2016-10-04 09:50:13 -0700

    sslyze 0.14.0
    use new virtualenv feature
    build nassl from source instead of using upstream prebuilt binaries
    add head spec
    run test suite
    
    Closes #2625.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/sslyze.rb
+++ Formula/sslyze.rb
@@ -1,9 +1,23 @@
 class Sslyze < Formula
+  include Language::Python::Virtualenv
+
   desc "SSL scanner"
   homepage "https://github.com/nabla-c0d3/sslyze"
-  url "https://github.com/nabla-c0d3/sslyze/archive/release-0.12.tar.gz"
-  version "0.12.0"
-  sha256 "5b3220d42cb66067b18d9055a2234252d849090e9fba660af52a3da18fa8a899"
+
+  stable do
+    url "https://github.com/nabla-c0d3/sslyze/archive/0.14.0.tar.gz"
+    sha256 "a4450cec121dfde9e52869f430197e83082752f61c02af3010ab96a8957773aa"
+
+    resource "nassl" do
+      url "https://github.com/nabla-c0d3/nassl/archive/0.14.0.tar.gz"
+      sha256 "b268b20eb6e1c32990d85933120ea459251e7fb70838ebd677ab9003e1b0fa0c"
+    end
+
+    resource "openssl" do
+      url "https://github.com/PeterMosmans/openssl.git",
+          :revision => "b5caa78f0c54e7f7746b2c3bbfd1d786c8fd21f9"
+    end
+  end
 
   bottle do
     cellar :any_skip_relocation
@@ -13,53 +27,60 @@ class Sslyze < Formula
     sha256 "0fab1f496b3f52e7637e0ed8ef23d8b435b925150ca15b85899c95cf61084e8b" => :mavericks
   end
 
+  head do
+    url "https://github.com/nabla-c0d3/sslyze.git"
+
+    resource "nassl" do
+      url "https://github.com/nabla-c0d3/nassl.git"
+    end
+
+    resource "openssl" do
+      url "https://github.com/PeterMosmans/openssl.git",
+          :branch => "1.0.2-chacha"
+    end
+  end
+
   depends_on :arch => :x86_64
   depends_on :python if MacOS.version <= :snow_leopard
 
-  resource "nassl" do
-    url "https://github.com/nabla-c0d3/nassl/archive/v0.12.tar.gz"
-    sha256 "40b3766fe98144e912ea7a8f1c34bf974e76f99ac40f128c3ce50b46b1fe315e"
-  end
-
-  resource "openssl" do
-    url "https://www.openssl.org/source/old/1.0.2/openssl-1.0.2d.tar.gz"
-    sha256 "671c36487785628a703374c652ad2cebea45fa920ae5681515df25d9f2c9a8c8"
-  end
-
   resource "zlib" do
-    url "http://zlib.net/zlib-1.2.8.tar.gz"
-    sha256 "36658cb768a54c1d4dec43c3116c27ed893e88b02ecfcb44f2166f9c0b7f2a0d"
+    url "https://mirrors.ocf.berkeley.edu/debian/pool/main/z/zlib/zlib_1.2.8.dfsg.orig.tar.gz"
+    mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/z/zlib/zlib_1.2.8.dfsg.orig.tar.gz"
+    version "1.2.8"
+    sha256 "2caecc2c3f1ef8b87b8f72b128a03e61c307e8c14f5ec9b422ef7914ba75cf9f"
   end
 
   def install
-    # openssl fails on parallel build. Related issues:
-    # - https://rt.openssl.org/Ticket/Display.html?id=3736&user=guest&pass=guest
-    # - https://rt.openssl.org/Ticket/Display.html?id=3737&user=guest&pass=guest
-    ENV.deparallelize
-
-    resource("openssl").stage do
-      (buildpath/"nassl/openssl-1.0.2d").install Dir["*"]
-    end
-
-    resource("zlib").stage do
-      (buildpath/"nassl/zlib-1.2.8").install Dir["*"]
-    end
-
+    venv = virtualenv_create(libexec)
     resource("nassl").stage do
-      (buildpath/"nassl").install Dir["*"]
+      nassl_path = Pathname.pwd
+      # openssl fails on parallel build. Related issues:
+      # - https://rt.openssl.org/Ticket/Display.html?id=3736&user=guest&pass=guest
+      # - https://rt.openssl.org/Ticket/Display.html?id=3737&user=guest&pass=guest
+      ENV.deparallelize do
+        mv "bin/openssl/include", "nassl_openssl_include"
+        rm_rf "bin" # make sure we don't use the prebuilt binaries
+        (nassl_path/"bin/openssl").install "nassl_openssl_include" => "include"
+        (nassl_path/"zlib-#{resource("zlib").version}").install resource("zlib")
+        (nassl_path/"openssl").install resource("openssl")
+        system "python", "build_from_scratch.py"
+      end
+      system "python", "run_tests.py"
+      venv.pip_install nassl_path
     end
+    venv.pip_install_and_link buildpath
+    mv bin/"sslyze_cli.py", bin/"sslyze"
 
-    cd "nassl" do
-      system "python", "buildAll_unix.py"
-      libexec.install "test/nassl"
-    end
-
-    libexec.install %w[plugins utils sslyze.py xml_out.xsd]
-    bin.install_symlink libexec/"sslyze.py" => "sslyze"
+    # Fix test_tlsv1_2_enable assertion error
+    # Reported 29 Aug 2016 https://github.com/nabla-c0d3/sslyze/issues/170
+    inreplace "tests/test_openssl_cipher_suites_plugin.py",
+      "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256",
+      "ECDHE-RSA-CHACHA20-POLY1305-OLD"
+    ENV.prepend "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    system "python", "run_tests.py"
   end
 
   test do
-    assert_equal "0.12.0", shell_output("#{bin}/sslyze --version").strip
     assert_match "SCAN COMPLETED", shell_output("#{bin}/sslyze --regular google.com")
   end
 end
