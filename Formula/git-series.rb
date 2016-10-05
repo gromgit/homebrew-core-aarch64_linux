b3662cbab65690a7d641ea7992215d33d419e252
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-05 09:16:04 +0100

    git-series: add 0.8.10 bottle.

--- Formula/git-series.rb
+++ Formula/git-series.rb
@@ -4,6 +4,13 @@ class GitSeries < Formula
   url "https://github.com/git-series/git-series/archive/0.8.10.tar.gz"
   sha256 "d70c9a2105c69fdd6059c04285ddb74b3b10852d9729dd8a14681b9bf36790be"
 
+  bottle do
+    cellar :any
+    sha256 "2daebeea29b8dab29cec91fc882c2b2d800b893e995d68ebfa76c00f45e25590" => :sierra
+    sha256 "9e763fcdeca9dbe7e8033e19c8e58288a85ab01e9dbf887f53a87dd90f1746a3" => :el_capitan
+    sha256 "4b9788b40b76dc4811b251ef8c08b654cefeae4fde5a8ef34e64bad52b4e0010" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "rust" => :build
   depends_on "libssh2"
