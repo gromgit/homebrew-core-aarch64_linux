24bbf981b014dfcca4cfa92905cb2a1980dce223
Teo Ljungberg <teo@teoljungberg.com>
2016-10-05 10:52:26 +0200

    pick 1.5.2
    Specify `TERM=xterm` to get the test suite to pass on CI.
    
    Closes #5608.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/pick.rb
+++ Formula/pick.rb
@@ -1,8 +1,8 @@
 class Pick < Formula
   desc "Utility to choose one option from a set of choices"
-  homepage "https://github.com/thoughtbot/pick"
-  url "https://github.com/thoughtbot/pick/releases/download/v1.4.0/pick-1.4.0.tar.gz"
-  sha256 "46f46b0df54cf27e8dd19ae291d5534cb55ef37d9cdb3cc774cd88c809f718fd"
+  homepage "https://github.com/calleerlandsson/pick"
+  url "https://github.com/calleerlandsson/pick/releases/download/v1.5.2/pick-1.5.2.tar.gz"
+  sha256 "183b278981233c70ac7f4d9af0728bf99d9d237e3f88d979f93fcc5968c2f789"
 
   bottle do
     cellar :any_skip_relocation
@@ -13,6 +13,7 @@ class Pick < Formula
   end
 
   def install
+    ENV["TERM"] = "xterm"
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
