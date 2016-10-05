9ed47b528c4b8427cbba21f5c3bc730f9efdca72
ilovezfs <ilovezfs@icloud.com>
2016-10-05 06:24:54 -0700

    bind 9.11.0
    Closes #5612.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/bind.rb
+++ Formula/bind.rb
@@ -1,10 +1,9 @@
 class Bind < Formula
   desc "Implementation of the DNS protocols"
   homepage "https://www.isc.org/downloads/bind/"
-  url "https://ftp.isc.org/isc/bind9/9.10.4-P3/bind-9.10.4-P3.tar.gz"
-  mirror "https://fossies.org/linux/misc/dns/bind9/9.10.4-P3/bind-9.10.4-P3.tar.gz"
-  version "9.10.4-P3"
-  sha256 "a075e5ce89fddccb0e64d1777d59161387dd5151cf4e7d1a93875a487812baef"
+  url "https://ftp.isc.org/isc/bind9/9.11.0/bind-9.11.0.tar.gz"
+  mirror "https://fossies.org/linux/misc/dns/bind9/9.11.0/bind-9.11.0.tar.gz"
+  sha256 "6f0b403036e0281b272a0fbdd0dc3417f3050b625cb059c5409432611418058b"
   head "https://source.isc.org/git/bind9.git"
 
   bottle do
