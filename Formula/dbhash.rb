80100bf8b644fc020e8c53a4394b00383248d052
Alexander Hamann <neutric@users.noreply.github.com>
2016-10-06 21:52:04 +0200

    dbhash 3.14.2 (new formula)
    Closes #5632.
    
    Signed-off-by: Misty De Meo <mistydemeo@github.com>

---
+++ Formula/dbhash.rb
@@ -0,0 +1,22 @@
+class Dbhash < Formula
+  desc "Computes the SHA1 hash of schema and content of a SQLite database"
+  homepage "https://www.sqlite.org/dbhash.html"
+  url "https://www.sqlite.org/2016/sqlite-src-3140200.zip"
+  version "3.14.2"
+  sha256 "52507e20c2757b24b703b43ede77ce464c8106c1658a5b357974c435aa0677a6"
+
+  def install
+    system "./configure", "--disable-debug", "--prefix=#{prefix}"
+    system "make", "dbhash"
+    bin.install "dbhash"
+  end
+
+  test do
+    dbpath = testpath/"test.sqlite"
+    sqlpath = testpath/"test.sql"
+    sqlpath.write "create table test (name text);"
+    system "/usr/bin/sqlite3 #{dbpath} < #{sqlpath}"
+    assert_equal "b6113e0ce62c5f5ca5c9f229393345ce812b7309",
+                 shell_output("#{bin}/dbhash #{dbpath}").strip.split.first
+  end
+end
