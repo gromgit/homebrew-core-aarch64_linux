dc77c4a82b6bd9a9f60ad3e3eb384f7ef93798ba
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-06 21:04:47 +0100

    dbhash: add 3.14.2 bottle.

--- Formula/dbhash.rb
+++ Formula/dbhash.rb
@@ -5,6 +5,13 @@ class Dbhash < Formula
   version "3.14.2"
   sha256 "52507e20c2757b24b703b43ede77ce464c8106c1658a5b357974c435aa0677a6"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e30691050b398f92047e0e0e874244a71c64c59935c8699dd5cfdde355c06b89" => :sierra
+    sha256 "5490da75d257a767b564a407dafe1d2fb71d2a3087054414fb735dcfb5ac292c" => :el_capitan
+    sha256 "15bac28a21ffa1469abe10ea2d539536281cb9a19d54a186f769d1fcac0a8f17" => :yosemite
+  end
+
   def install
     system "./configure", "--disable-debug", "--prefix=#{prefix}"
     system "make", "dbhash"
