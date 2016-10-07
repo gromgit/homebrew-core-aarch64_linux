3772a86660b6fd18a05165c24907455217fa243e
Joshua Rubin <jrubin@zvelo.com>
2016-10-07 14:10:03 -0600

    tmux: enable utf8proc
    Closes #5665.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/tmux.rb
+++ Formula/tmux.rb
@@ -3,6 +3,7 @@ class Tmux < Formula
   homepage "https://tmux.github.io/"
   url "https://github.com/tmux/tmux/releases/download/2.3/tmux-2.3.tar.gz"
   sha256 "55313e132f0f42de7e020bf6323a1939ee02ab79c48634aa07475db41573852b"
+  revision 1
 
   bottle do
     cellar :any
@@ -21,6 +22,7 @@ class Tmux < Formula
 
   depends_on "pkg-config" => :build
   depends_on "libevent"
+  depends_on "utf8proc"
 
   resource "completion" do
     url "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/homebrew_1.0.0/completions/tmux"
@@ -33,7 +35,8 @@ class Tmux < Formula
     ENV.append "LDFLAGS", "-lresolv"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
-                          "--sysconfdir=#{etc}"
+                          "--sysconfdir=#{etc}",
+                          "--enable-utf8proc"
 
     system "make", "install"
 
