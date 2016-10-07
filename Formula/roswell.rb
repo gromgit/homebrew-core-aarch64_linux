0b293ab479e4b0d6d14bfd1b00d7511190f9b6ea
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-07 09:03:03 +0100

    roswell: update 0.0.6.67 bottle.

--- Formula/roswell.rb
+++ Formula/roswell.rb
@@ -6,10 +6,9 @@ class Roswell < Formula
   head "https://github.com/roswell/roswell.git"
 
   bottle do
-    sha256 "c86ef56af7f92ee4c088503e8c3683045cb71653af4705dae7ff957dad7d617b" => :sierra
-    sha256 "ebf4dc986cc82737b3f8c953f4f5ce20d372be5f2b42b2cb1f91d79d26cbe33e" => :el_capitan
-    sha256 "004394c3b7f5199dab8e68ec4107ccfa8e35de6935ae0320c91d2efdb9ef84c5" => :yosemite
-    sha256 "639fa7bb1f504a3f7bc1f71b9527f4b29c2d6d38acffd7fa5768957710091951" => :mavericks
+    sha256 "b19d3188685acda7c1211a5156ca04f2c69b5cef4ad55a71203e07b4094de038" => :sierra
+    sha256 "9a26eaec144b811c4de52f92bb4d0b77793215c1289769a16f537ceb2955f189" => :el_capitan
+    sha256 "d1c8e8e30d1803b5874fcf125ae4de08e8e3d3a5826656bf9ec8f092e6d81489" => :yosemite
   end
 
   depends_on "automake" => :build
