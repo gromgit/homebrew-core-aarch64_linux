56d4eee8ed5c5bb5e36186eb5ed12191c6d13ac4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-07 14:22:56 +0100

    pdftoedn: update 0.34.1_1 bottle.

--- Formula/pdftoedn.rb
+++ Formula/pdftoedn.rb
@@ -7,10 +7,9 @@ class Pdftoedn < Formula
 
   bottle do
     cellar :any
-    sha256 "04741c63be592ede3c058ede03b215491524f4b8610c201ccae25fe4ffa18367" => :sierra
-    sha256 "1db4ccab956227cf5db09e9a925b0373427e3d237e4486661e13d2b1b4bcde1f" => :el_capitan
-    sha256 "3e0307c2943aba4c8dc1d849c579f75bcca989b24f1470b8da25817c03b27d50" => :yosemite
-    sha256 "08959d8bb3f153e9ab5dd34827387fcea19c167f3ac1dbc5566eb35b638d5a5e" => :mavericks
+    sha256 "3867cf77c2226770eab4014aea7ae39ae89a3cdb02d81b918022557aca37b8a5" => :sierra
+    sha256 "cd0199695e2cd739c86b3d03eeaefb1564b37b671b1d53733ba6963dc5082bad" => :el_capitan
+    sha256 "7e62b90c53555db9913a33f29fe58875b83e56e7c5b5009cfe88dcc618834230" => :yosemite
   end
 
   depends_on "automake" => :build
