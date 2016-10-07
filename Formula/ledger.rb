f8059618f8e3c541c72e29aef6a6e0d0d28936e6
ilovezfs <ilovezfs@icloud.com>
2016-10-07 03:17:31 -0700

    ledger: revision for boost

--- Formula/ledger.rb
+++ Formula/ledger.rb
@@ -3,7 +3,7 @@ class Ledger < Formula
   homepage "http://ledger-cli.org"
   url "https://github.com/ledger/ledger/archive/v3.1.1.tar.gz"
   sha256 "90f06561ab692b192d46d67bc106158da9c6c6813cc3848b503243a9dfd8548a"
-  revision 2
+  revision 3
 
   head "https://github.com/ledger/ledger.git"
 
