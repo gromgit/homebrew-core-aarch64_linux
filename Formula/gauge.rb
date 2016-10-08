5963636d3bcae9d6b3b89f31241789dc01311eb3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-08 20:09:31 +0100

    gauge: update 0.6.2 bottle.

--- Formula/gauge.rb
+++ Formula/gauge.rb
@@ -8,11 +8,9 @@ class Gauge < Formula
   head "https://github.com/getgauge/gauge.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "c897f59e5bfb1fac00b8aa0bafcb17362ec9ff592775c487fb006b6ec2645092" => :sierra
-    sha256 "8a89b6d685890418d81b176b6ab5993ebbf299282e598f328d0f960ef5bf8b5d" => :el_capitan
-    sha256 "3bc49428a78b72b602b839dcec1de5b6e8c493406d162128c25190f8f5020d55" => :yosemite
-    sha256 "5af574faba350f2725c25c2fdcb992918ee88f67d0ad437e02f3b8ebb48c9302" => :mavericks
+    sha256 "3aa0d0a2621ed6742cff4f1575432f511f734a6d7f93080ea6c6d5fa9f2c5412" => :sierra
+    sha256 "0b5fa1edfbf86c2263df3a4b716fc914e261d077cbc99add429a7e582aecb788" => :el_capitan
+    sha256 "3ad5bb700139d027aa05e3a766b44b9f15cc868fb6958b39eda3d3902fc48bd8" => :yosemite
   end
 
   depends_on "go" => :build
