c333d4fdd9b349e17c3f5448dc21c797d3dd7f39
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-08 18:52:24 +0100

    csvtomd: add 0.1.1 bottle.

--- Formula/csvtomd.rb
+++ Formula/csvtomd.rb
@@ -6,6 +6,12 @@ class Csvtomd < Formula
   url "https://files.pythonhosted.org/packages/5a/d8/9dbce820243bb6db670cd1ddea80ea1890c6bfd5d122910fdd157d51d71f/csvtomd-0.1.1.tar.gz"
   sha256 "f2cd1da71ca8ed823d1f02167039e07e58d15a73d672069dfddcda8639576490"
 
+  bottle do
+    sha256 "4364ae91ea62c6b25d87245d0f9046bb3156191b1826ff337f717a43fe4c480d" => :sierra
+    sha256 "ff9ed1382f01e5351c6377052e9e62618feeb04377294a5b7dc4c5f7b5a915c6" => :el_capitan
+    sha256 "ae4316cbe060984c617344c9a581996b1456f3c7c4ad951097c7aa09bb331d97" => :yosemite
+  end
+
   depends_on :python3
 
   def install
