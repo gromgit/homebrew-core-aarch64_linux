cf043acf7895b77f402499bcd10b7b82e3440a02
Tomi Pakarinen <tomi.pakarinen@resys.io>
2016-10-09 13:03:11 +0300

    voltdb 6.6
    Closes #5713.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/voltdb.rb
+++ Formula/voltdb.rb
@@ -1,8 +1,8 @@
 class Voltdb < Formula
   desc "Horizontally-scalable, in-memory SQL RDBMS"
   homepage "https://github.com/VoltDB/voltdb"
-  url "https://github.com/VoltDB/voltdb/archive/voltdb-5.6.tar.gz"
-  sha256 "9ea24d8cacdf2e19ba60487f3e9dfefa83c18cb3987571abc44b858ce0db7c3e"
+  url "https://github.com/VoltDB/voltdb/archive/voltdb-6.6.tar.gz"
+  sha256 "300d06c8f5a1cc4a3537f216ea105d5ccc8987b10bad2c676a40543c8903d72e"
   head "https://github.com/VoltDB/voltdb.git"
 
   bottle do
@@ -14,12 +14,24 @@ class Voltdb < Formula
   end
 
   depends_on :ant => :build
+  depends_on "cmake" => :build
 
   def install
     system "ant"
 
-    inreplace Dir["bin/*"] - ["bin/voltadmin", "bin/voltdb", "bin/rabbitmqloader"],
+    # Edit VOLTDB_LIB variable in bash scripts to match homebrew's folder structure. Python scripts work without
+    # changes and are excluded here.
+    inreplace Dir["bin/*"] - ["bin/voltadmin", "bin/voltdb", "bin/rabbitmqloader", "bin/voltdeploy", "bin/voltenv"],
       %r{VOLTDB_LIB=\$VOLTDB_HOME\/lib}, "VOLTDB_LIB=$VOLTDB_HOME/lib/voltdb"
+
+    inreplace "bin/voltenv" do |s|
+      # voltenv location detection is different than in other bash scripts.
+      s.gsub! /VOLTDB_VOLTDB="\$VOLTDB_LIB"/, "VOLTDB_VOLTDB=\"$VOLTDB_BASE/voltdb\""
+
+      # Remove voltenv installed as link check
+      # Upstream PR https://github.com/VoltDB/voltdb/pull/3973
+      s.gsub! /if \[ "\$\{0\}" = "\$SOURCE" \]; then/, "if [ \"${0}\" = \"${BASH_SOURCE[0]}\" ]; then"
+    end
 
     (lib/"voltdb").install Dir["lib/*"]
     lib.install_symlink lib/"voltdb/python"
