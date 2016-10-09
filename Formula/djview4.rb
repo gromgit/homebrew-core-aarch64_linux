dc74bc6f3ffa50758227fa7ecbb779955f3eb2c1
ilovezfs <ilovezfs@icloud.com>
2016-10-09 08:05:25 -0700

    djview4 4.10.6
    use qt5
    
    Closes #5736.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/djview4.rb
+++ Formula/djview4.rb
@@ -1,8 +1,8 @@
 class Djview4 < Formula
   desc "Viewer for the DjVu image format"
   homepage "http://djvu.sourceforge.net/djview4.html"
-  url "https://downloads.sourceforge.net/project/djvu/DjView/4.10/djview-4.10.5.tar.gz"
-  sha256 "a0b302807106053d1983659ebb0139c7b1c393b3e6407f1f47990d7e3c5d7b3d"
+  url "https://downloads.sourceforge.net/project/djvu/DjView/4.10/djview-4.10.6.tar.gz"
+  sha256 "8446f3cd692238421a342f12baa365528445637bffb96899f319fe762fda7c21"
 
   bottle do
     sha256 "0b5835bf2e00677dbe5ac47fc17f93fc7e36575c7f2b8820e80f98a83639c224" => :el_capitan
@@ -10,11 +10,17 @@ class Djview4 < Formula
     sha256 "864c1b3ae86440205b6f045b2fbc6db5b8e3af241ccf3f99b7f3ce9683ef66c4" => :mavericks
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "djvulibre"
-  depends_on "qt"
+  depends_on "qt5"
 
   def install
+    inreplace "src/djview.pro", "10.6", MacOS.version
+    system "autoreconf", "-fiv"
+
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}",
                           "--with-x=no",
