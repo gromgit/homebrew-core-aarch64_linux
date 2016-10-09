80f18defefc814d60d3799e58835cbeffc8e93c8
Alex Dunn <dunn.alex@gmail.com>
2016-10-09 11:29:16 -0700

    wgetpaste 2.28
    Closes #5744.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/wgetpaste.rb
+++ Formula/wgetpaste.rb
@@ -1,8 +1,8 @@
 class Wgetpaste < Formula
   desc "Automate pasting to a number of pastebin services"
   homepage "http://wgetpaste.zlin.dk/"
-  url "http://wgetpaste.zlin.dk/wgetpaste-2.26.tar.bz2"
-  sha256 "9265cd0718c815cce19a12c4745f74b288cafce404a26d64daf85ddcbadc8f86"
+  url "http://wgetpaste.zlin.dk/wgetpaste-2.28.tar.bz2"
+  sha256 "538d38bab491544bdf6f05f7a38f83d4c3dfee77de7759cb6b9be1ebfdd609c2"
 
   bottle :unneeded
 
@@ -10,6 +10,7 @@ class Wgetpaste < Formula
 
   def install
     bin.install "wgetpaste"
+    zsh_completion.install "_wgetpaste"
   end
 
   test do
