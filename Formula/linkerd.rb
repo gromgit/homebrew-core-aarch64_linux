4fd0062a0ce8a988dd625c66d2ebb327727489af
Viktor Szakats <vszakats@users.noreply.github.com>
2016-10-10 23:59:22 +0200

    linkerd: use secure homepage

--- Formula/linkerd.rb
+++ Formula/linkerd.rb
@@ -1,6 +1,6 @@
 class Linkerd < Formula
   desc "Drop-in RPC proxy designed for microservices"
-  homepage "http://linkerd.io/"
+  homepage "https://linkerd.io/"
   url "https://github.com/BuoyantIO/linkerd/releases/download/0.8.1/linkerd-0.8.1.tgz"
   sha256 "d8fe77daaf0a7d261a96dd4dc360a1045ae6d302134ab5c7628754e4f4dc781c"
 
