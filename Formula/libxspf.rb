0e10ffaf059dfee716d14cd750525f1df01ce96c
Viktor Szakats <vszakats@users.noreply.github.com>
2016-10-10 23:59:22 +0200

    libxspf: use secure url in comment

--- Formula/libxspf.rb
+++ Formula/libxspf.rb
@@ -19,7 +19,7 @@ class Libxspf < Formula
   depends_on "uriparser"
 
   # Fix build against clang and GCC 4.7+
-  # http://git.xiph.org/?p=libxspf.git;a=commit;h=7f1f68d433f03484b572657ff5df47bba1b03ba6
+  # https://git.xiph.org/?p=libxspf.git;a=commit;h=7f1f68d433f03484b572657ff5df47bba1b03ba6
   patch :DATA
 
   def install
