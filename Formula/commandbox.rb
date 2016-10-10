f3fac78b0c5950adf219f0f9843a6e6c5bb12823
Jon Clausen <jon_clausen@silowebworks.com>
2016-10-10 12:56:41 -0400

    commandbox 3.3.0
    Closes #5764.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/commandbox.rb
+++ Formula/commandbox.rb
@@ -1,16 +1,16 @@
 class Commandbox < Formula
   desc "CFML embedded server, package manager, and app scaffolding tools"
   homepage "https://www.ortussolutions.com/products/commandbox"
-  url "http://downloads.ortussolutions.com/ortussolutions/commandbox/3.2.0/commandbox-bin-3.2.0.zip"
-  sha256 "42b0be0f5bcfdae161e24a587c98f8a21550ad76f23303cfb7f06eed03cbd8c5"
+  url "http://downloads.ortussolutions.com/ortussolutions/commandbox/3.3.0/commandbox-bin-3.3.0.zip"
+  sha256 "72091365a6babbf0410db22affb1c03879195b8a18f8af4bf97e988fe68b9c02"
 
   bottle :unneeded
 
   depends_on :java => "1.7+"
 
   resource "apidocs" do
-    url "http://downloads.ortussolutions.com/ortussolutions/commandbox/3.2.0/commandbox-apidocs-3.2.0.zip"
-    sha256 "7444852e09d10f04cd087ade4e4962e4abb0b0a8a1e0b63beba971bf2c563a3c"
+    url "http://downloads.ortussolutions.com/ortussolutions/commandbox/3.3.0/commandbox-apidocs-3.3.0.zip"
+    sha256 "17c073bc2741503d7105366a81675bf9a77b56697cc75387fdd6bd76545f0b9c"
   end
 
   def install
@@ -19,7 +19,7 @@ class Commandbox < Formula
   end
 
   test do
-    system bin/"box", "--commandbox_home=~/", "version"
-    system bin/"box", "--commandbox_home=~/", "help"
+    system "box", "--commandbox_home=~/", "version"
+    system "box", "--commandbox_home=~/", "help"
   end
 end
