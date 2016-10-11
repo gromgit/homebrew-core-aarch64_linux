0654a291905cca2d0de41bbbd004ffca448026c7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-12 00:39:07 +0100

    open-babel: update 2.4.1 bottle.

--- Formula/open-babel.rb
+++ Formula/open-babel.rb
@@ -7,9 +7,9 @@ class OpenBabel < Formula
   head "https://github.com/openbabel/openbabel.git"
 
   bottle do
-    sha256 "3852d0fe38c854ac1f88f7e8dadaa227efcbbdbf8ac8b8dc60432ddb7f1cf201" => :sierra
-    sha256 "ced3ef914169409f24c79c27721d8f1bbafe0cfa95bc645fb2acdeae2e2b27a3" => :el_capitan
-    sha256 "0078ae5007d3d0bab51b2a032d627417bf12521fe7a52bffdb57222116e992a4" => :yosemite
+    sha256 "28bb84f75639741efbbf3a19ebffc1fc122d15fa74584440b84e265cdfd18db0" => :sierra
+    sha256 "d2ca98556d58c6268b6be3f93cfc9a00a79559d081d7713ed14bc7882212b2ef" => :el_capitan
+    sha256 "48724ff8b63ea446ea0f2095361ea93de0647eec2e220c8369b9910a11450213" => :yosemite
   end
 
   option "with-cairo", "Support PNG depiction"
