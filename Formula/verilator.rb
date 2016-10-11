193f627854da78eebd24d7b829d9de400502551b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-11 15:45:11 +0100

    verilator: update 3.886 bottle.

--- Formula/verilator.rb
+++ Formula/verilator.rb
@@ -5,10 +5,9 @@ class Verilator < Formula
   sha256 "d21c882e0c079ade4b155cb4a763f7d246decead8b38d2d0aad8d1dd03e0f77d"
 
   bottle do
-    sha256 "92bfd3a1c0254181993088ce7b139aea42df28ddebc71c2ad2a358a2c14e0b44" => :sierra
-    sha256 "24fac1b570220c9616f00bfc6c52ce75df4bfc6b25f58c8770da8e6690114cbb" => :el_capitan
-    sha256 "cea32dc698d89578e0e08a4e947b2117d12e0aac25561313ee052ab1c4a19a59" => :yosemite
-    sha256 "133e859467db7fe6bd8888467f2be0ce165025f32581236f018642d88f0c1d06" => :mavericks
+    sha256 "15e76edd90d337938bc7ab4aa65262f84bea0e829c518f0130277e7c7c83efff" => :sierra
+    sha256 "4c588bfd844073b06b638c543cd7b77a994702afde965a2943bcee7b46dbead2" => :el_capitan
+    sha256 "29b48507f10e9a315d93b6acc55cb4eb0b5f7e67ba74c67be5bb74c45a33e7b2" => :yosemite
   end
 
   head do
