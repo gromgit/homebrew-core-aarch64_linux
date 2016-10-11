02a13ae6b3df56a1c31b56648e7e9f0a2c5c012f
LivHere <LivHere@users.noreply.github.com>
2016-10-11 04:32:18 -0700

    hicolor-icon-theme: Fix audit --strict expression order. (#5668)

--- Formula/hicolor-icon-theme.rb
+++ Formula/hicolor-icon-theme.rb
@@ -4,12 +4,6 @@ class HicolorIconTheme < Formula
   url "https://icon-theme.freedesktop.org/releases/hicolor-icon-theme-0.15.tar.xz"
   sha256 "9cc45ac3318c31212ea2d8cb99e64020732393ee7630fa6c1810af5f987033cc"
 
-  head do
-    url "https://anongit.freedesktop.org/git/xdg/default-icon-theme.git"
-    depends_on "automake" => :build
-    depends_on "autoconf" => :build
-  end
-
   bottle do
     cellar :any_skip_relocation
     sha256 "96769af537321b17416273e465e01408b41a2881ca5abab6bdfdba902d11e452" => :sierra
@@ -17,6 +11,12 @@ class HicolorIconTheme < Formula
     sha256 "e1e09d7dee2b5560d45d99a310d8e2903d30413eb53408a4079261e8ef5f3b55" => :yosemite
     sha256 "e3e7a63d5af66fe6721839c12e00288e061ef092a046ff6db2dcc6f62f75b9c2" => :mavericks
     sha256 "5647ecc1f44a15ee6cef8d37ae62d606251a0ae94f2659c9fac497270876367e" => :mountain_lion
+  end
+
+  head do
+    url "https://anongit.freedesktop.org/git/xdg/default-icon-theme.git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
   end
 
   def install
