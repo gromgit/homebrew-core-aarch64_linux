87cf4870d74c44395a21513755ffc93841bdbafa
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-12 08:10:34 +0100

    dspdfviewer: update 1.15 bottle.

--- Formula/dspdfviewer.rb
+++ Formula/dspdfviewer.rb
@@ -7,6 +7,7 @@ class Dspdfviewer < Formula
 
   bottle do
     cellar :any
+    sha256 "f689fd84944b153babb3cf1b22b6c735abb897f8d9edb6abf4a0fd7ec9f0340f" => :sierra
     sha256 "5f23135aa30bf6fae45d453d892361daef51e19ffe510f5a9e79b5ed7393d0e3" => :el_capitan
     sha256 "69bf9e53d7b8fd5414f770baea119c7f22d1a05f752c7915cbe1f0cdc201a004" => :yosemite
     sha256 "d2f2ad9306d3417ab848ed98ca47481242a4a4064f8a8a1e1e6cc41aa286d4aa" => :mavericks
