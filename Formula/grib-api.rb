93fe71428b8d5fd392c2609a0e94e83435dc53ce
Jean-Francois Simoneau <jfsimoneau@skymotion.com>
2016-10-12 11:01:36 -0400

    grib-api 1.18.0
    Closes #5849.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/grib-api.rb
+++ Formula/grib-api.rb
@@ -1,8 +1,8 @@
 class GribApi < Formula
   desc "Encode and decode grib messages (editions 1 and 2)"
   homepage "https://software.ecmwf.int/wiki/display/GRIB/Home"
-  url "https://software.ecmwf.int/wiki/download/attachments/3473437/grib_api-1.17.0-Source.tar.gz"
-  sha256 "55cbb4fdcb4ee1be6a27cece9ae7e26070beb8ab6cb7e77773db3fb0d4272462"
+  url "https://software.ecmwf.int/wiki/download/attachments/3473437/grib_api-1.18.0-Source.tar.gz"
+  sha256 "dfffeeb4df715b234907cb12d6729617bed0df0ff023337c2dd3cd20ab58199e"
 
   bottle do
     sha256 "0426ca81bf84c562373f5a3359276724d9b06a3c021412b3f3cd1a7df7a08496" => :sierra
@@ -17,10 +17,6 @@ class GribApi < Formula
   depends_on "jasper" => :recommended
   depends_on "libpng" => :optional
   depends_on :fortran
-
-  # Fixes build errors in Lion
-  # https://software.ecmwf.int/wiki/plugins/viewsource/viewpagesrc.action?pageId=12648475
-  patch :DATA
 
   def install
     mkdir "build" do
@@ -43,36 +39,3 @@ class GribApi < Formula
     system bin/"grib_ls", "#{grib_samples_path}/GRIB2.tmpl"
   end
 end
-
-__END__
-diff --git a/configure b/configure
-index 0a88b28..9dafe46 100755
---- a/configure
-+++ b/configure
-@@ -7006,7 +7006,7 @@ $as_echo_n "checking for $compiler option to produce PIC... " >&6; }
-     darwin* | rhapsody*)
-       # PIC is the default on this platform
-       # Common symbols not allowed in MH_DYLIB files
--      lt_prog_compiler_pic='-fno-common'
-+      #lt_prog_compiler_pic='-fno-common'
-       ;;
- 
-     hpux*)
-@@ -12186,7 +12186,7 @@ $as_echo_n "checking for $compiler option to produce PIC... " >&6; }
-     darwin* | rhapsody*)
-       # PIC is the default on this platform
-       # Common symbols not allowed in MH_DYLIB files
--      lt_prog_compiler_pic_F77='-fno-common'
-+      #lt_prog_compiler_pic_F77='-fno-common'
-       ;;
- 
-     hpux*)
-@@ -15214,7 +15214,7 @@ $as_echo_n "checking for $compiler option to produce PIC... " >&6; }
-     darwin* | rhapsody*)
-       # PIC is the default on this platform
-       # Common symbols not allowed in MH_DYLIB files
--      lt_prog_compiler_pic_FC='-fno-common'
-+      #lt_prog_compiler_pic_FC='-fno-common'
-       ;;
- 
-     hpux*)
