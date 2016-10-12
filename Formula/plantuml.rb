52a1b31c09ee1e7d981942ce998d663231cce6bb
Ryan Ovrevik <rovrevik@users.noreply.github.com>
2016-10-12 15:10:20 -0400

    plantuml 2048 (#5834)

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "http://plantuml.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/plantuml/plantuml.8046.jar"
-  sha256 "136997cd0d5c40b71b5cf9f45b5f1e93d3dbfa6c67b57a3679a628183c6f236e"
+  url "https://downloads.sourceforge.net/project/plantuml/plantuml.8048.jar"
+  sha256 "e500ea94600ecade4af7262b32ea20a1c884de0013f4a3f07aeae02d594424ab"
 
   bottle :unneeded
 
