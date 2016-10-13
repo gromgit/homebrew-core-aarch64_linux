3c4eff9a60c51514d1896f0ea4ebe00069b2fa9a
ilovezfs <ilovezfs@icloud.com>
2016-10-13 01:54:18 -0700

    qt5 5.6.2
    Closes #5843.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/qt5.rb
+++ Formula/qt5.rb
@@ -14,9 +14,8 @@ end
 class Qt5 < Formula
   desc "Version 5 of the Qt framework"
   homepage "https://www.qt.io/"
-  url "https://download.qt.io/official_releases/qt/5.6/5.6.1-1/single/qt-everywhere-opensource-src-5.6.1-1.tar.xz"
-  mirror "https://www.mirrorservice.org/sites/download.qt-project.org/official_releases/qt/5.6/5.6.1-1/single/qt-everywhere-opensource-src-5.6.1-1.tar.xz"
-  sha256 "ce08a7eb54661705f55fb283d895a089b267c688fabe017062bd71b9231736db"
+  url "https://download.qt.io/official_releases/qt/5.6/5.6.2/single/qt-everywhere-opensource-src-5.6.2.tar.xz"
+  sha256 "83e61bfc78bba230770704e828fa4d23fe3bbfdcfa4a8f5db37ce149731d89b3"
 
   head "https://code.qt.io/qt/qt5.git", :branch => "5.6", :shallow => false
 
@@ -25,28 +24,6 @@ class Qt5 < Formula
     sha256 "2aaa410f2ab2fbbddbc8c3438e43bc9f4271774c794bcae8f935fb6b1b5a82ed" => :el_capitan
     sha256 "eefa531c6ebc757982b31f17935fa2220aad52caf3112e389a878dce04f40490" => :yosemite
     sha256 "73d33dd2563c39542844c276a7bd43463f2974fde141e7afeb3057168adbe606" => :mavericks
-  end
-
-  # Restore `.pc` files for framework-based build of Qt 5 on OS X. This
-  # partially reverts <https://codereview.qt-project.org/#/c/140954/> merged
-  # between the 5.5.1 and 5.6.0 releases. (Remove this as soon as feasible!)
-  #
-  # Core formulae known to fail without this patch (as of 2016-03-17):
-  #   * mkvtoolnix (with `--with-qt5` option, silent build failure)
-  #   * poppler    (with `--with-qt5` option)
-  #   * wireshark  (with `--with-qt5` option)
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/e8fe6567/qt5/restore-pc-files.patch"
-    sha256 "48ff18be2f4050de7288bddbae7f47e949512ac4bcd126c2f504be2ac701158b"
-  end
-
-  # Fix build error due to missing Mac QtBase widget example targets, detected
-  # by logic introduced in <https://codereview.qt-project.org/#/c/156610/> and
-  # corrected in <https://codereview.qt-project.org/#/c/161001/>.
-  # Should land in either 5.6.2 and/or 5.7.1.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/6ffd0e250d374193613a51beda8830dda9b67e56/qt5/QTBUG-54110.patch"
-    sha256 "2cf77b820f46f0c404284882b4a4a97bf005b680062842cdc53e107a821deeda"
   end
 
   keg_only "Qt 5 conflicts Qt 4"
@@ -74,8 +51,25 @@ class Qt5 < Formula
 
   resource "qt-webkit" do
     # http://lists.qt-project.org/pipermail/development/2016-March/025358.html
-    url "https://download.qt.io/community_releases/5.6/5.6.1/qtwebkit-opensource-src-5.6.1.tar.gz"
-    sha256 "f5ba5afc5846fc755575dd04081a90a9536f920e312f18f6fb1f5a0c33f477b0"
+    url "https://download.qt.io/community_releases/5.6/5.6.2/qtwebkit-opensource-src-5.6.2.tar.xz"
+    sha256 "528a6b8b1c5095367b26e8ce4f3a46bb739e2e9913ff4dfc6ef58a04fcd73966"
+  end
+
+  # Upstream commit from 3 Oct 2016 "Fixed build with MaxOSX10.12 SDK"
+  # http://code.qt.io/cgit/qt/qtconnectivity.git/commit/?h=5.6&id=462323dba4f963844e8c9911da27a0d21e4abf43
+  # Should be removed for > 5.6.2 or > 5.7.0
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/04c2de3/qt5/qtconnectivity-bluetooth-fix.diff"
+    sha256 "41fd73cba0018180015c2be191d63b3c33289f19132c136f482f5c7477620931"
+  end
+
+  # Upstream commit from 1 Aug 2016 "BASELINE: Update Chromium to 53.0.2785.41"
+  # http://code.qt.io/cgit/qt/qtwebengine-chromium.git/commit/chromium/base/mac/sdk_forward_declarations.h?h=upstream-master&id=28b1110370900897ab652cb420c371fab8857ad4
+  # Remove when qtwebengine's src/3rdparty/chromium submodule points to a commit
+  # of qtwebengine-chromium with Chromium >= 53.0.2785.41
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/04c2de3/qt5/qtwebengine-bluetooth-fix.diff"
+    sha256 "218b8682d7e8a3f74618d0bd87e4797e13fc2cbfe49c21a2845e64da3fe8868a"
   end
 
   def install
@@ -157,13 +151,6 @@ class Qt5 < Formula
     Pathname.glob("#{bin}/*.app") do |app|
       mv app, libexec/"#{app.basename(".app")}-qt5.app"
     end
-  end
-
-  def post_install
-    # Upstream "configure and mkspecs: Don't try to find xcrun with xcrun"
-    # https://code.qt.io/cgit/qt/qtbase.git/patch/?id=77a71c32c9d19b87f79b208929e71282e8d8b5d9
-    inreplace prefix/"mkspecs/features/mac/default_pre.prf",
-      "xcrun -find xcrun", "xcrun -find xcodebuild"
   end
 
   def caveats; <<-EOS.undent
