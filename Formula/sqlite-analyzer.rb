9f84296e9e3d2ad3afc752dacaa9519a5f4dc764
Alexander Hamann <neutric@users.noreply.github.com>
2016-10-14 20:28:18 +0200

    sqlite-analyzer 3.15.0
    Closes #5918.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/sqlite-analyzer.rb
+++ Formula/sqlite-analyzer.rb
@@ -1,9 +1,9 @@
 class SqliteAnalyzer < Formula
   desc "Analyze how space is allocated inside an SQLite file"
   homepage "https://www.sqlite.org/"
-  url "https://sqlite.org/2016/sqlite-src-3140200.zip"
-  version "3.14.2"
-  sha256 "52507e20c2757b24b703b43ede77ce464c8106c1658a5b357974c435aa0677a6"
+  url "https://www.sqlite.org/2016/sqlite-src-3150000.zip"
+  version "3.15.0"
+  sha256 "c68a7064b1932aa8c40f9b1e644ab899771530e16d514ba4a065434666d4a3a2"
 
   bottle do
     cellar :any_skip_relocation
