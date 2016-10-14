c29e13158b80b63bad0c7ea39d315bb81c121be9
ilovezfs <ilovezfs@icloud.com>
2016-10-13 21:05:28 -0700

    apache-flink 1.1.3
    Closes #5897.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/apache-flink.rb
+++ Formula/apache-flink.rb
@@ -1,9 +1,9 @@
 class ApacheFlink < Formula
   desc "Scalable batch and stream data processing"
   homepage "https://flink.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.1.2/flink-1.1.2-bin-hadoop27-scala_2.11.tgz"
-  version "1.1.2"
-  sha256 "3a5def68f0c9ff7c944b80c26d6fb09d29fa20d6f7c3a8b48579dbf91bfcec46"
+  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.1.3/flink-1.1.3-bin-hadoop27-scala_2.11.tgz"
+  version "1.1.3"
+  sha256 "2d26c0cdb5cd8294721b98bd8caec85e3c7f4269e9517e82ade79b86478b6035"
   head "https://github.com/apache/flink.git"
 
   bottle :unneeded
