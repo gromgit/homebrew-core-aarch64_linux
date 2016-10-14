2f587bf1eb0b133dc0e024a9474d00c17d676a3c
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-10-14 09:16:57 +0100

    libnotify 0.7.7
    Removed redundant gtk+3 dependency
    Added support for gobject-introspection
    
    Closes #5901.
    
    Signed-off-by: Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>

--- Formula/libnotify.rb
+++ Formula/libnotify.rb
@@ -1,8 +1,8 @@
 class Libnotify < Formula
   desc "Library that sends desktop notifications to a notification daemon"
   homepage "https://developer.gnome.org/libnotify"
-  url "https://download.gnome.org/sources/libnotify/0.7/libnotify-0.7.6.tar.xz"
-  sha256 "0ef61ca400d30e28217979bfa0e73a7406b19c32dd76150654ec5b2bdf47d837"
+  url "https://download.gnome.org/sources/libnotify/0.7/libnotify-0.7.7.tar.xz"
+  sha256 "9cb4ce315b2655860c524d46b56010874214ec27e854086c1a1d0260137efc04"
 
   bottle do
     sha256 "7b9bbf7af44434aca82465bf7f234d4affb50daeae54c65a7758b9814e113ff5" => :sierra
@@ -12,12 +12,15 @@ class Libnotify < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on "gtk+3"
+  depends_on "gdk-pixbuf"
+  depends_on "gobject-introspection"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+                          "--prefix=#{prefix}",
+                          "--disable-tests",
+                          "--enable-introspection"
     system "make", "install"
   end
 end
