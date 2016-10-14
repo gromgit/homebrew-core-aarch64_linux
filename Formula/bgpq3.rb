0614d20965f8d1d0f2afe7a0757f4d7a89bc30c7
Job Snijders <job@instituut.net>
2016-10-14 18:21:00 +0200

    bgpq3 0.1.33
    - OpenBGPD support
    - bugfixes
    - minor features
    
    Closes #5913.
    
    Signed-off-by: Zhiming Wang <zmwangx@gmail.com>

--- Formula/bgpq3.rb
+++ Formula/bgpq3.rb
@@ -1,8 +1,8 @@
 class Bgpq3 < Formula
-  desc "bgp filtering automation for Cisco and Juniper routers"
+  desc "bgp filtering automation for Cisco, Juniper, BIRD and OpenBGPD routers"
   homepage "http://snar.spb.ru/prog/bgpq3/"
-  url "https://github.com/snar/bgpq3/archive/v0.1.31.tar.gz"
-  sha256 "839da59b153b2c748ca021d742cb9da248681e323f380e614ac546b796fbe71b"
+  url "https://github.com/snar/bgpq3/archive/v0.1.33.tar.gz"
+  sha256 "f3a58d547368c4396e5bc2b6189fe968567f1afde47245220b1ba9486b26fb39"
   head "https://github.com/snar/bgpq3.git"
 
   bottle do
@@ -12,12 +12,6 @@ class Bgpq3 < Formula
     sha256 "f0be355cc0ce42364f31d1a0190d4239d0a05ee3545d166ec4630c305af23dd0" => :yosemite
     sha256 "66251ef2771eef19ea08c52961d142f787a4b0184ad73905a5f80d3191700306" => :mavericks
     sha256 "e4c36bd2ac02efee5c2f74bbf4c207e552d95069773713e3b25c27493a23ebd3" => :mountain_lion
-  end
-
-  devel do
-    url "https://github.com/snar/bgpq3/archive/0.1.32-rc5.tar.gz"
-    sha256 "80da4ff47cc527f6c2d33eb39e41a11a75e71f85d94ce5e8206d9c6c87107c19"
-    version "0.1.32-rc5"
   end
 
   # Makefile: upstream has been informed of the patch through email (multiple
