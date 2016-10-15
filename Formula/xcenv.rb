40ac15fb2941b6e76d0a5afbac5676f5e9875ac5
Mark Schall <schall.mark@gmail.com>
2016-10-14 21:11:00 -0400

    xcenv 1.1.0
    Closes #5925.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/xcenv.rb
+++ Formula/xcenv.rb
@@ -1,8 +1,8 @@
 class Xcenv < Formula
   desc "Xcode version manager"
-  homepage "https://github.com/xcenv/xcenv#readme"
-  url "https://github.com/xcenv/xcenv/archive/v1.0.4.tar.gz"
-  sha256 "cac6e5475b0dda35fe1c3771bc60b703e0df7b87b1bf607988c447768ca02122"
+  homepage "http://xcenv.org"
+  url "https://github.com/xcenv/xcenv/archive/v1.1.0.tar.gz"
+  sha256 "3a08afad39bf8243769b7aa49597688a8418ded9c229f672a3af8b007db4d331"
   head "https://github.com/xcenv/xcenv.git"
 
   bottle do
@@ -15,13 +15,6 @@ class Xcenv < Formula
 
   def install
     prefix.install ["bin", "libexec"]
-  end
-
-  def caveats; <<-EOS.undent
-    Xcenv stores data under ~/.xcenv by default. If you absolutely need to
-    store everything under Homebrew's prefix, include this in your profile:
-      export XCENV_ROOT=#{var}/xcenv
-    EOS
   end
 
   test do
