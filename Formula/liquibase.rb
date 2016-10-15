1a29060809b7d1da3ae5112ae1fe8641c51a39d7
ilovezfs <ilovezfs@icloud.com>
2016-10-14 23:04:03 -0700

    liquibase 3.5.3
    Closes #5932.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/liquibase.rb
+++ Formula/liquibase.rb
@@ -1,8 +1,8 @@
 class Liquibase < Formula
   desc "Library for database change tracking"
   homepage "http://liquibase.org"
-  url "https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.5.1/liquibase-3.5.1-bin.tar.gz"
-  sha256 "b99870efec8c779b4e279a8b0c730273f837d4b6dffd037176737ecc0641ab6b"
+  url "https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.5.3/liquibase-3.5.3-bin.tar.gz"
+  sha256 "fb85d27f1ef8aef0539a74eca3b71692292f2e961171701a38ba82cdbcb39711"
 
   bottle :unneeded
 
