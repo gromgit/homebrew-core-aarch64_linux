3eedfa816fa74023f27865e39a592518563e1bea
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-10-16 08:33:57 +0100

    gupnp-av 0.12.10
    Closes #5963.
    
    Signed-off-by: Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>

--- Formula/gupnp-av.rb
+++ Formula/gupnp-av.rb
@@ -1,8 +1,8 @@
 class GupnpAv < Formula
   desc "Library to help implement UPnP A/V profiles"
   homepage "https://wiki.gnome.org/GUPnP/"
-  url "https://download.gnome.org/sources/gupnp-av/0.12/gupnp-av-0.12.9.tar.xz"
-  sha256 "62c56449256a1a97b66c8ee59aa6455b90a7921285745ef3b79566218e85d447"
+  url "https://download.gnome.org/sources/gupnp-av/0.12/gupnp-av-0.12.10.tar.xz"
+  sha256 "8038ef84dddbe7ad91c205bf91dddf684f072df8623f39b6555a6bb72837b85a"
 
   bottle do
     sha256 "c450fea99ba2f8bca3dda25c50741a3cd6dc91b5ac888279ac2705c5b12a85a9" => :sierra
