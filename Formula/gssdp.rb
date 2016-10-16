65a25b44c7776a2e6c3f5c17852348e25e4ae823
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-10-16 08:29:43 +0100

    gssdp 1.0.1
    Closes #5961.
    
    Signed-off-by: Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>

--- Formula/gssdp.rb
+++ Formula/gssdp.rb
@@ -1,8 +1,8 @@
 class Gssdp < Formula
   desc "GUPnP library for resource discovery and announcement over SSDP"
   homepage "https://wiki.gnome.org/GUPnP/"
-  url "https://download.gnome.org/sources/gssdp/1.0/gssdp-1.0.0.tar.xz"
-  sha256 "1b1df551df5ff4d856d064f785753a2eabccb89c358bae9f23f4b88c5c984173"
+  url "https://download.gnome.org/sources/gssdp/1.0/gssdp-1.0.1.tar.xz"
+  sha256 "601d67f40bc28c067742b7c46f3018da96b364fd3e83f30ddac6e190e323d2e1"
 
   bottle do
     sha256 "597b60408b18902393e99b75f1118c9af9eed8d451a840a2d790da61ff993233" => :sierra
