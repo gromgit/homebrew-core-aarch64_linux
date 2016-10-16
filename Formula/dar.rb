73ca7d1e780f93355235f322016cb0057b76a517
ilovezfs <ilovezfs@icloud.com>
2016-10-16 11:55:48 -0700

    dar 2.5.6
    drop support for Yosemite and below due to thread_local requirement
    
    Closes #3888.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dar.rb
+++ Formula/dar.rb
@@ -1,8 +1,8 @@
 class Dar < Formula
   desc "Backup directory tree and files"
   homepage "http://dar.linux.free.fr/doc/index.html"
-  url "https://downloads.sourceforge.net/project/dar/dar/2.4.22/dar-2.4.22.tar.gz"
-  sha256 "19a977e3ca7ed15e4406bb3e08ff2b8225d291a5c4189a6d5b7b3d75eea50f2b"
+  url "https://downloads.sourceforge.net/project/dar/dar/2.5.6/dar-2.5.6.tar.gz"
+  sha256 "47db04275d9f10f3a074e226d6fe760184e9694f1811a98f0b41e4f1dee30815"
 
   bottle do
     sha256 "7ce41e06f2f8a0d4dd65a52904250c6cd7f44223b3c438931646c4ad2ff6f97c" => :sierra
@@ -11,38 +11,37 @@ class Dar < Formula
     sha256 "a74696553c86f94dfd1ebdad29ad9c5d22c79d78fc023e43bb86f272fbfc4d87" => :mavericks
   end
 
-  option "with-docs", "build programming documentation (in particular libdar API documentation) and html man page"
+  option "with-doxygen", "build libdar API documentation and html man page"
   option "with-libgcrypt", "enable strong encryption support"
   option "with-lzo", "enable lzo compression support"
   option "with-upx", "make executables compressed at installation time"
 
-  if build.with? "docs"
-    depends_on "coreutils" => :build
-    depends_on "doxygen" => :build
-  end
+  deprecated_option "with-docs" => "with-doxygen"
 
-  depends_on "gettext" => :optional
-  depends_on "gnu-sed" => :build
+  depends_on :macos => :el_capitan # needs thread-local storage
+  depends_on "doxygen" => [:build, :optional]
+  depends_on "upx" => [:build, :optional]
   depends_on "libgcrypt" => :optional
   depends_on "lzo" => :optional
-  depends_on "upx" => [:build, :optional]
+  depends_on "xz" => :optional
+
+  needs :cxx11
 
   def install
-    ENV.prepend_path "PATH", "#{Formula["gnu-sed"].opt_libexec}/gnubin"
-    ENV.prepend_path "PATH", "#{Formula["coreutils"].opt_libexec}/gnubin" if build.with? "docs"
-    ENV.libstdcxx if ENV.compiler == :clang && MacOS.version >= :mavericks
+    ENV.cxx11
 
     args = %W[
+      --enable-mode=64
       --disable-debug
       --disable-dependency-tracking
       --disable-dar-static
       --prefix=#{prefix}
     ]
-
-    args << "--disable-build-html" if build.without? "docs"
+    args << "--disable-build-html" if build.without? "doxygen"
+    args << "--disable-upx" if build.without? "upx"
     args << "--disable-libgcrypt-linking" if build.without? "libgcrypt"
     args << "--disable-liblzo2-linking" if build.without? "lzo"
-    args << "--disable-upx" if build.without? "upx"
+    args << "--disable-libxz-linking" if build.without? "xz"
 
     system "./configure", *args
     system "make", "install"
