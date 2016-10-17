98f7e3f6c2f2b3112c9e7bbd979c36c2f83c97cb
ilovezfs <ilovezfs@icloud.com>
2016-10-16 22:48:04 -0700

    pgrouting 2.3.0
    Closes #5998.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pgrouting.rb
+++ Formula/pgrouting.rb
@@ -1,10 +1,8 @@
 class Pgrouting < Formula
   desc "Provides geospatial routing for PostGIS/PostgreSQL database"
   homepage "http://www.pgrouting.org"
-  url "https://github.com/pgRouting/pgrouting/archive/pgrouting-2.2.4.tar.gz"
-  sha256 "34ccf2b1acd076ad7da92c0692a114d0b607b84771fdfd4e131246ef2c66bf84"
-  revision 1
-
+  url "https://github.com/pgRouting/pgrouting/archive/pgrouting-2.3.0.tar.gz"
+  sha256 "cadeed30bfc57f6e77bc48a10301b26e5d71d8e86f366ed60ab5b0eaa0c45662"
   head "https://github.com/pgRouting/pgrouting.git"
 
   bottle do
@@ -12,11 +10,6 @@ class Pgrouting < Formula
     sha256 "8a511d98a83830b7e8b2c86831eac254bcbde507726c001a369f943fd5f6b778" => :sierra
     sha256 "c71db8f54805e0278d1be93d1e7a638968d392b877be093cbc08d8db2556233c" => :el_capitan
     sha256 "64ba007f1967c02f0383198053a02196e30b1ae9993895d5828bddfd01596bd8" => :yosemite
-  end
-
-  devel do
-    url "https://github.com/pgRouting/pgrouting/archive/pgrouting-2.3.0-rc1.tar.gz"
-    sha256 "c903d6dab0e48b987a9906eb8331e1bc18f78143d41db8888dee06d2d8c5ce4b"
   end
 
   depends_on "cmake" => :build
