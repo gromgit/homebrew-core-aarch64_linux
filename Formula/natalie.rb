996c7468d502828a4b81950d3ba79cfcacda9e9a
ilovezfs <ilovezfs@icloud.com>
2016-10-17 02:46:14 -0700

    natalie 0.5.0
    Closes #4921.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/natalie.rb
+++ Formula/natalie.rb
@@ -1,8 +1,8 @@
 class Natalie < Formula
   desc "Storyboard Code Generator (for Swift)"
   homepage "https://github.com/krzyzanowskim/Natalie"
-  url "https://github.com/krzyzanowskim/Natalie/archive/0.4.2.tar.gz"
-  sha256 "2b3e3bd90fca4d4b2406a794abb4f4371c11e673606c3233b9b8e55b2b6b7245"
+  url "https://github.com/krzyzanowskim/Natalie/archive/0.5.0.tar.gz"
+  sha256 "66e00a4095121255a9740a16a8a59daa289f878f2e1e77ba6a9f98d6a671a33c"
   head "https://github.com/krzyzanowskim/Natalie.git"
 
   bottle do
@@ -11,21 +11,16 @@ class Natalie < Formula
     sha256 "49c77d43fca5c3e76dfc00d5d8c4465cb2e98772415a3372b747b9f73b3eeff6" => :yosemite
   end
 
-  depends_on :xcode => "7.0"
+  depends_on :xcode => ["8.0", :build]
 
   def install
-    mv "natalie.swift", "natalie-script.swift"
-    system "xcrun", "-sdk", "macosx", "swiftc", "-O", "natalie-script.swift", "-o", "natalie.swift"
-    bin.install "natalie.swift"
+    system "swift", "build", "-c", "release", "-Xswiftc", "-static-stdlib"
+    bin.install ".build/release/natalie"
     share.install "NatalieExample"
   end
 
   test do
-    example_path = "#{share}/NatalieExample"
-    output_path = testpath/"Storyboards.swift"
-    generated_code = `#{bin}/natalie.swift #{example_path}`
-    output_path.write(generated_code)
-    line_count = `wc -l #{output_path}`
-    assert line_count.to_i > 1
+    generated_code = Utils.popen_read("#{bin}/natalie #{share}/NatalieExample")
+    assert generated_code.lines.count > 1, "Natalie failed to generate code!"
   end
 end
