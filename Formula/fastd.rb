8aaec10ad744a73fd44bc4b2a18b7847caa14295
Zhiming Wang <zmwangx@gmail.com>
2016-10-17 08:34:42 -0400

    fastd: patch for Xcode 8 clock_gettime issue
    Applying the upstream patch.
    
    Closes #6012.
    
    Signed-off-by: Zhiming Wang <zmwangx@gmail.com>

--- Formula/fastd.rb
+++ Formula/fastd.rb
@@ -1,11 +1,21 @@
 class Fastd < Formula
   desc "Fast and Secure Tunnelling Daemon"
   homepage "https://projects.universe-factory.net/projects/fastd"
-  url "https://projects.universe-factory.net/attachments/download/86/fastd-18.tar.xz"
-  sha256 "714ff09d7bd75f79783f744f6f8c5af2fe456c8cf876feaa704c205a73e043c9"
-  revision 1
+  revision 2
 
   head "https://git.universe-factory.net/fastd/", :using => :git
+
+  stable do
+    url "https://projects.universe-factory.net/attachments/download/86/fastd-18.tar.xz"
+    sha256 "714ff09d7bd75f79783f744f6f8c5af2fe456c8cf876feaa704c205a73e043c9"
+
+    # https://projects.universe-factory.net/issues/239
+    # https://projects.universe-factory.net/projects/fastd/repository/revisions/2fa2187
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/c2c6049/fastd/patch-xcode8-clock_gettime.diff"
+      sha256 "dfa07eccf01a84a6e5eacc82c47c2cd5ba216e1f5032b41d2cef32e0b205ba9c"
+    end
+  end
 
   bottle do
     cellar :any
