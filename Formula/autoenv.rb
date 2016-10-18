964b083f840e765cbf5d55c62f02506aeea0d4b7
Janne Heß <jannehess@gmail.com>
2016-10-18 15:57:06 +0200

    autoenv 0.2.1
    A new autoenv release! (minor)
    
    Closes #6054.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/autoenv.rb
+++ Formula/autoenv.rb
@@ -1,8 +1,8 @@
 class Autoenv < Formula
   desc "Per-project, per-directory shell environments"
   homepage "https://github.com/kennethreitz/autoenv"
-  url "https://github.com/kennethreitz/autoenv/archive/v0.2.0.tar.gz"
-  sha256 "8c360fde059a3b25d55ecab0c2ae90caf011a851aa3552bb42177eef650b46dc"
+  url "https://github.com/kennethreitz/autoenv/archive/v0.2.1.tar.gz"
+  sha256 "d10ee4d916a11a664453e60864294fec221c353f8ad798aa0aa6a2d2c5d5b318"
   head "https://github.com/kennethreitz/autoenv.git"
 
   bottle :unneeded
