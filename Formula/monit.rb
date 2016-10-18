f0375e7c458c7bc53eebd71d8d60d9ac5d7db50b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-18 19:30:11 +0100

    monit: update 5.19.0 bottle.

--- Formula/monit.rb
+++ Formula/monit.rb
@@ -7,10 +7,9 @@ class Monit < Formula
 
   bottle do
     cellar :any
-    sha256 "cd4622ecbb475cc9ee10613714fc59d7b200ccabbf7d73c978783f3b7b0145aa" => :sierra
-    sha256 "5a146e27154d2434cfb44dbd61ae55b7ed8fa01bd105cf0bc5828bf90a7252b7" => :el_capitan
-    sha256 "924ec6e8d44fa5168442431f5ba61724243196929e7a18d1a936f4b9a99035c1" => :yosemite
-    sha256 "25be7ac4a24e829b081d562c0249e2c4a48d77a49e9f5c3f49a609023d702fc5" => :mavericks
+    sha256 "fd7408fc598000d81c294125645de093951747a27383e7d78ea49e60a70b9864" => :sierra
+    sha256 "1a477a903f166904ff7105062f5ec35494d65c235e056e4caffe5b486e833869" => :el_capitan
+    sha256 "ec3d67f372b43df39477358de470c652ff29a0c8b76c55536083518c0c684382" => :yosemite
   end
 
   depends_on "openssl"
