9b0650edfa2fd71065c340d5c05e428983e512d6
Chiedo <chiedo@chie.do>
2016-10-18 14:16:46 -0400

    yarn 0.16.0
    Closes #6060.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/yarn.rb
+++ Formula/yarn.rb
@@ -3,13 +3,8 @@ require "language/node"
 class Yarn < Formula
   desc "Javascript package manager"
   homepage "https://yarnpkg.com/"
-  # Note: If updating this to a newer version, please change the URL to the official
-  # yarnpkg.com download URL:
-  # url "https://yarnpkg.com/downloads/0.15.0/yarn-v0.15.0.tar.gz"
-  # This was not updated yet as Yarn 0.15.1 was an npm-specific release and is not available
-  # as a standalone tarball.
-  url "https://registry.npmjs.org/yarn/-/yarn-0.15.1.tgz"
-  sha256 "f99fd587e84987909d5f9e918b8fe524349fdc548e5bc5c380c8f8c0a70c6b87"
+  url "https://yarnpkg.com/downloads/0.16.0/yarn-v0.16.0.tar.gz"
+  sha256 "cd1d7eeb8eb2518441d99c914e5fd18b68e2759743d212dfd8f00574a1de6da8"
   head "https://github.com/yarnpkg/yarn.git"
 
   bottle do
