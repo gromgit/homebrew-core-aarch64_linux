c615b9cb4014d2b353f8580e1fd1fde70d7ca031
Francisco Souza <f@souza.cc>
2016-10-19 16:07:54 -0400

    go 1.7.3
    Closes #6096.
    
    Signed-off-by: Zhiming Wang <zmwangx@gmail.com>

--- Formula/go.rb
+++ Formula/go.rb
@@ -3,10 +3,10 @@ class Go < Formula
   homepage "https://golang.org"
 
   stable do
-    url "https://storage.googleapis.com/golang/go1.7.1.src.tar.gz"
-    mirror "https://fossies.org/linux/misc/go1.7.1.src.tar.gz"
-    version "1.7.1"
-    sha256 "2b843f133b81b7995f26d0cb64bbdbb9d0704b90c44df45f844d28881ad442d3"
+    url "https://storage.googleapis.com/golang/go1.7.3.src.tar.gz"
+    mirror "https://fossies.org/linux/misc/go1.7.3.src.tar.gz"
+    version "1.7.3"
+    sha256 "79430a0027a09b0b3ad57e214c4c1acfdd7af290961dd08d322818895af1ef44"
 
     go_version = version.to_s.split(".")[0..1].join(".")
     resource "gotools" do
