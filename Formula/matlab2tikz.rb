680491b0117e69c197be1c6756954280a08bc92c
ilovezfs <ilovezfs@icloud.com>
2016-10-21 06:16:05 -0700

    matlab2tikz 1.1.0
    Closes #6141.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/matlab2tikz.rb
+++ Formula/matlab2tikz.rb
@@ -1,8 +1,8 @@
 class Matlab2tikz < Formula
   desc "Convert MATLAB(R) figures into TikZ/Pgfplots figures"
   homepage "https://github.com/matlab2tikz/matlab2tikz"
-  url "https://github.com/matlab2tikz/matlab2tikz/archive/1.0.0.tar.gz"
-  sha256 "16d212f333afa100f2ec307c85957532ceb8163aebc7021770dfc68f6f2f8f1a"
+  url "https://github.com/matlab2tikz/matlab2tikz/archive/v1.1.0.tar.gz"
+  sha256 "4e6fe80ebe4c8729650eb00679f97398c2696fd9399c17f9c5b60a1a6cf23a19"
   head "https://github.com/matlab2tikz/matlab2tikz.git"
 
   bottle :unneeded
