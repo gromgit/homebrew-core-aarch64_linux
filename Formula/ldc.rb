be8ff19762a6070c30f992216cad3d082dcf229f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-22 16:30:20 +0100

    ldc: update 1.0.1-alpha1 bottle.

--- Formula/ldc.rb
+++ Formula/ldc.rb
@@ -16,9 +16,9 @@ class Ldc < Formula
   end
 
   bottle do
-    sha256 "fa31a94f88612af29fbb76ba9f7daca79a61d67ca4f9b1ab1eff2fc4481acc20" => :sierra
-    sha256 "a74ba2d0bb5847a9789d009218a132b91732b7b63ca9e1a3711e648aed595f0c" => :el_capitan
-    sha256 "09ed1998e344071843bfb5a9b4abe74ad7cf29068094b410a72aefdebc7f16f2" => :yosemite
+    sha256 "5d4e2c20dd74909113448f9d80032b9eea9776061c2aabde9b58d33b36503588" => :sierra
+    sha256 "4837cf9fdb9b9c030fb26674254a844610459203a79847b1ff3cf0d85770fc33" => :el_capitan
+    sha256 "8d827625dea278303befcbb997d3e1713b7d85e257705a30da72ce1519eab147" => :yosemite
   end
 
   devel do
