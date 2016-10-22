bc2ade0be9e4e673c6182367996b08c4d20f441c
ilovezfs <ilovezfs@icloud.com>
2016-10-22 08:08:33 -0700

    hh 1.21
    Closes #6169.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/hh.rb
+++ Formula/hh.rb
@@ -1,9 +1,9 @@
 class Hh < Formula
   desc "Bash and zsh history suggest box"
   homepage "https://github.com/dvorka/hstr"
-  url "https://github.com/dvorka/hstr/releases/download/1.19/hh-1.19-src.tgz"
-  sha256 "b67cb5e2515948fd0fb402b732630a51885be5dfe58cbf914c22ea444129a647"
-  revision 1
+  url "https://github.com/dvorka/hstr/archive/1.21.tar.gz"
+  sha256 "f0e9762f2a9587f0995bbd51cb64526ae852c2425ceb8ceee0747efba80ac6b3"
+  head "https://github.com/dvorka/hstr.git"
 
   bottle do
     cellar :any
@@ -12,26 +12,22 @@ class Hh < Formula
     sha256 "ee3ece1e2ce9794d6e68f5f2a5749753e1fe4f4512de50b0397e4865ccbe81d1" => :yosemite
   end
 
-  head do
-    url "https://github.com/dvorka/hstr.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
-
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
   depends_on "readline"
 
   def install
-    system "autoreconf", "-fvi" if build.head?
+    system "autoreconf", "-fvi"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
   end
 
   test do
-    path = testpath/".hh_test"
-    path.write "test\n"
-    ENV["HISTFILE"] = path
-    assert_equal "test\n", `#{bin}/hh -n`
+    ENV["HISTFILE"] = testpath/".hh_test"
+    (testpath/".hh_test").write("test\n")
+    assert_equal "test", shell_output("#{bin}/hh -n").chomp
   end
 end
