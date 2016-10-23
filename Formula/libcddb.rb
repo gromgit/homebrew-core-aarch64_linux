d1bc314e19c7b54b60a2313bf3c924e5148ea758
ilovezfs <ilovezfs@icloud.com>
2016-10-22 23:05:40 -0700

    libcddb: revision for libcdio

--- Formula/libcddb.rb
+++ Formula/libcddb.rb
@@ -3,6 +3,7 @@ class Libcddb < Formula
   homepage "http://libcddb.sourceforge.net/"
   url "https://downloads.sourceforge.net/libcddb/libcddb-1.3.2.tar.bz2"
   sha256 "35ce0ee1741ea38def304ddfe84a958901413aa829698357f0bee5bb8f0a223b"
+  revision 1
 
   bottle do
     cellar :any
@@ -18,6 +19,10 @@ class Libcddb < Formula
   depends_on "libcdio"
 
   def install
+    if MacOS.version == :yosemite && MacOS::Xcode.installed? && MacOS::Xcode.version >= "7.0"
+      ENV.delete("SDKROOT")
+    end
+
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
