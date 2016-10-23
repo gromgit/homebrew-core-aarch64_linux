a3ca1912b4f52aed79f1dc031d592a2b83a66fe2
Wilhelm Schuster <ws@wilhelm.re>
2016-10-23 21:56:35 +0200

    tin 2.4.0
    Closes #6201.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/tin.rb
+++ Formula/tin.rb
@@ -1,8 +1,8 @@
 class Tin < Formula
   desc "Threaded, NNTP-, and spool-based UseNet newsreader"
   homepage "http://www.tin.org"
-  url "http://ftp.cuhk.edu.hk/pub/packages/news/tin/v2.2/tin-2.2.1.tar.gz"
-  sha256 "cf588884bbe4711498b807311e53d82a1b6ca343f4c85e53001c90e8c0e456ac"
+  url "http://ftp.cuhk.edu.hk/pub/packages/news/tin/v2.4/tin-2.4.0.tar.gz"
+  sha256 "26ee20980c88308f225c0bae55b6db12365ced3398dbea0621992dff0e74cbb6"
 
   bottle do
     sha256 "299d98d164fd4a1aba5e66ff4f1bc75d415acfad49555183a793ec9e44323ecf" => :sierra
@@ -10,11 +10,6 @@ class Tin < Formula
     sha256 "1444c2fa1a3f63f4f8f8330b69d6c255c70c4732d13673a4beab2d621d753d3e" => :yosemite
     sha256 "11505b2e7b9953370a54fab8dc3460e28d3aabc1e02d2d713d514e4a78eb68d7" => :mavericks
     sha256 "231470502b1e124011bd79c44d84740d068ccc32aad86a5599e7006755e7a200" => :mountain_lion
-  end
-
-  devel do
-    url "http://ftp.cuhk.edu.hk/pub/packages/news/tin/v2.3/tin-2.3.1.tar.gz"
-    sha256 "d53ee03850988c96162f2a30a24f63a6976612f04fc049fd1e0c17d0d4567083"
   end
 
   conflicts_with "mutt", :because => "both install mmdf.5 and mbox.5 man pages"
