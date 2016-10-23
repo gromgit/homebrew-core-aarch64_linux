06e9168deab3809363a53ada1617f61e137e3a18
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-23 02:16:33 +0100

    minio: update 20161022005041 bottle.

--- Formula/minio.rb
+++ Formula/minio.rb
@@ -8,10 +8,9 @@ class Minio < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0f77765b5ed9d83666fdb6c15de6e96d6d397a174388debd5c70244201cce741" => :sierra
-    sha256 "d236098663cc6b4a4016855e1f5ce21608b42e1a4d33aa24e4c9963de7bd0d14" => :el_capitan
-    sha256 "a1f3a72ebd68b63b6ef658b218f7d67031d3d9ed8c95c142764bf777b3cd0ddc" => :yosemite
-    sha256 "51ae709a683eb435d915377745f66d28c0ae1ca3b1d8e4f28f7b014908baf323" => :mavericks
+    sha256 "d3cc6b58140185e010be0a0b8eebd71fe0b0d223a5d54565ff0c1703b57c4f12" => :sierra
+    sha256 "bb55278468be86048e7181ee9d19060ebda62f7098673836f0b0042c69af4545" => :el_capitan
+    sha256 "f78226ab1797733c9cec4ffe72050c3ddc9faf6f8d3743eb47f8b0cdfec7ade2" => :yosemite
   end
 
   depends_on "go" => :build
