a9a9ef34799343608a71a0cc285311fd0b819dcb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-24 05:59:24 +0100

    tin: update 2.4.0 bottle.

--- Formula/tin.rb
+++ Formula/tin.rb
@@ -5,11 +5,9 @@ class Tin < Formula
   sha256 "26ee20980c88308f225c0bae55b6db12365ced3398dbea0621992dff0e74cbb6"
 
   bottle do
-    sha256 "299d98d164fd4a1aba5e66ff4f1bc75d415acfad49555183a793ec9e44323ecf" => :sierra
-    sha256 "24b40c6319c0673d13ad30121de18c7797a97ef9b4ad14b92f7aa56fb07a1ae4" => :el_capitan
-    sha256 "1444c2fa1a3f63f4f8f8330b69d6c255c70c4732d13673a4beab2d621d753d3e" => :yosemite
-    sha256 "11505b2e7b9953370a54fab8dc3460e28d3aabc1e02d2d713d514e4a78eb68d7" => :mavericks
-    sha256 "231470502b1e124011bd79c44d84740d068ccc32aad86a5599e7006755e7a200" => :mountain_lion
+    sha256 "8449ea4864b1fb5daa8f28f94b3561dbc0f165dbfb0c1c4fc7173f2552ad040d" => :sierra
+    sha256 "6cff69c3cbafa44246b88cc5721cf497933b80f12b703de54afe4b00d8fa54a9" => :el_capitan
+    sha256 "ca307ff45977125664a3e8add2f28bc9f37570a5150db8ad573ba7aac67a8bad" => :yosemite
   end
 
   conflicts_with "mutt", :because => "both install mmdf.5 and mbox.5 man pages"
