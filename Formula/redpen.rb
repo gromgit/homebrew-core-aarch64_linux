bf2aa2b764dfbc2c00443e178098804202ae29cb
takahi-i <takahikoito@gmail.com>
2016-10-25 16:01:16 +0900

    redpen 1.7.6
    Closes #6243.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/redpen.rb
+++ Formula/redpen.rb
@@ -1,8 +1,8 @@
 class Redpen < Formula
   desc "Proofreading tool to help writers of technical documentation"
   homepage "http://redpen.cc/"
-  url "https://github.com/redpen-cc/redpen/releases/download/redpen-1.7.4/redpen-1.7.4.tar.gz"
-  sha256 "d7b7ed2ebebecf1219187b562a648cba8cd23bb6a4eaef7f1add25ddeb8b985d"
+  url "https://github.com/redpen-cc/redpen/releases/download/redpen-1.7.6/redpen-1.7.6.tar.gz"
+  sha256 "7543a50e9de7465b2f9e81ec5b5f4dd707310c3534b4100e62fdede42fa9d045"
 
   bottle :unneeded
 
