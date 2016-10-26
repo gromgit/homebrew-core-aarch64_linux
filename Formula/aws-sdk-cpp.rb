85ac27d4b60cef43ebf53ab1cc1602962680b59a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-26 08:51:16 +0100

    aws-sdk-cpp: update 1.0.22 bottle.

--- Formula/aws-sdk-cpp.rb
+++ Formula/aws-sdk-cpp.rb
@@ -7,10 +7,9 @@ class AwsSdkCpp < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "a7945528abdd6ceb3795d152bf344064fd9b235586812f77c5bc5bc34325b175" => :sierra
-    sha256 "9ccebc0124906322d1e5d459e8602742de4dbaba6ae8acd566b087f30ce744a6" => :el_capitan
-    sha256 "0e244ea3e846440fbaf76755758e7c86cc22be3266152e04005e22d2cdd8909f" => :yosemite
+    sha256 "743d7658f7270e5be19c3cf5d1ddf3089ad50b93ec96517edfed2abacc0ccd4f" => :sierra
+    sha256 "073ff5342f6a58ff1c3e2e4904a63334988ed7f4df5cdae2ec72b75f8e66a0b8" => :el_capitan
+    sha256 "b369b7658fc1cd9de090ef5fa8f87d4049953c69d2b8348ac3239565f0e0802d" => :yosemite
   end
 
   option "with-static", "Build with static linking"
