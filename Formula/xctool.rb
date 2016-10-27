edad7729e62902ff0454a059a721e84de0fa7832
Aleksey Kononov <nekto@whatsapp.com>
2016-10-27 13:59:59 -0700

    xctool 0.3
    New xctool release.
    
    Closes #6337.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/xctool.rb
+++ Formula/xctool.rb
@@ -1,8 +1,8 @@
 class Xctool < Formula
   desc "Drop-in replacement for xcodebuild with a few extra features"
   homepage "https://github.com/facebook/xctool"
-  url "https://github.com/facebook/xctool/archive/0.2.9.tar.gz"
-  sha256 "4ac19d19742bb844bdf3f6e73c7458cde835efd9d91accba090d3f37a357bf39"
+  url "https://github.com/facebook/xctool/archive/0.3.tar.gz"
+  sha256 "d1eb62840ed0b7488f9d432c3d3bd198f357ee7bd268fea7e9d17166dfd90f25"
   head "https://github.com/facebook/xctool.git"
 
   bottle do
@@ -12,7 +12,7 @@ class Xctool < Formula
     sha256 "d035eebe4a7203b1ed2db3d59c3f4a6f418cad5e3cfd872355d0fa561c568b02" => :mavericks
   end
 
-  depends_on :xcode => "6.0"
+  depends_on :xcode => "7.0"
 
   def install
     system "./scripts/build.sh", "XT_INSTALL_ROOT=#{libexec}", "-IDECustomDerivedDataLocation=#{buildpath}"
