e626233048473ca6a8985a0276efe23ab69d5d93
ilovezfs <ilovezfs@icloud.com>
2016-10-29 04:47:34 -0700

    hg-fast-export 160914
    Closes #6380.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/hg-fast-export.rb
+++ Formula/hg-fast-export.rb
@@ -1,8 +1,8 @@
 class HgFastExport < Formula
   desc "Fast Mercurial to Git converter"
   homepage "http://repo.or.cz/w/fast-export.git"
-  url "https://github.com/frej/fast-export/archive/v160415.tar.gz"
-  sha256 "0e8a8c27110f200b52be55d13858045ed069ae60a51b4d8878a310426d244316"
+  url "https://github.com/frej/fast-export/archive/v160914.tar.gz"
+  sha256 "1eb2c520f9fa93413d17e4a4551e3dde0dad31564498f1204b191741bd5a4763"
 
   head "git://repo.or.cz/fast-export.git"
 
