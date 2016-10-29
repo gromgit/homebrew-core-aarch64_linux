6fb4a68c2050431754dd1f4afa72ad833dc12db5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-29 20:27:31 +0100

    kafka: update 0.10.1.0 bottle.

--- Formula/kafka.rb
+++ Formula/kafka.rb
@@ -7,11 +7,9 @@ class Kafka < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "16b2d2ad87154ee65f7b7e90bf2344aa495e518fc41a000642bfbb37e0bc329b" => :sierra
-    sha256 "2874959d241121c73d914ea4d7b00055274d1bbeb4680d899e80243095c5d523" => :el_capitan
-    sha256 "16b2d2ad87154ee65f7b7e90bf2344aa495e518fc41a000642bfbb37e0bc329b" => :yosemite
-    sha256 "16b2d2ad87154ee65f7b7e90bf2344aa495e518fc41a000642bfbb37e0bc329b" => :mavericks
+    sha256 "4fcea199b4a3a9e4eb3aecd280cb82adee79bf222964e615c64c9b2d02e1fdb3" => :sierra
+    sha256 "4fcea199b4a3a9e4eb3aecd280cb82adee79bf222964e615c64c9b2d02e1fdb3" => :el_capitan
+    sha256 "4fcea199b4a3a9e4eb3aecd280cb82adee79bf222964e615c64c9b2d02e1fdb3" => :yosemite
   end
 
   depends_on "zookeeper"
