77f404cf353815ef9b005fa5b123037f908f4a64
Dominyk Tiller <dominyktiller@gmail.com>
2016-10-30 21:23:54 +0000

    mbedtls 2.4.0
    Closes #6426.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/mbedtls.rb
+++ Formula/mbedtls.rb
@@ -1,32 +1,9 @@
 class Mbedtls < Formula
   desc "Cryptographic & SSL/TLS library"
   homepage "https://tls.mbed.org/"
+  url "https://tls.mbed.org/download/mbedtls-2.4.0-apache.tgz"
+  sha256 "c1c3559ed39f7a1b1550c4cf4ccb918bf239301a3311d98dda92bed8a25b7f0d"
   head "https://github.com/ARMmbed/mbedtls.git", :branch => "development"
-  revision 1
-
-  stable do
-    url "https://tls.mbed.org/download/mbedtls-2.3.0-apache.tgz"
-    sha256 "590734c8bc8b3ac48e9123d44bf03562e91f8dce0d1ac2615c318c077f3215b2"
-
-    # https://github.com/ARMmbed/mbedtls/issues/522
-    # They are commits already applied to the upstream.
-    patch do
-      url "https://github.com/ARMmbed/mbedtls/commit/7247f99b3e068a2b90b7776a2cdd438fddb7a38b.patch"
-      sha256 "071830f9b1870ed319fcc65e34ce6d6f9b3476e81f0a204d474635e59ac08687"
-    end
-    patch do
-      url "https://github.com/ARMmbed/mbedtls/commit/b5b6af2663fdb7f57c30494607bade90810f6844.patch"
-      sha256 "859ffed5d6e4dfb386c0ee3b9f00efa904d0ee8977272def4bd787e1c9efda82"
-    end
-    patch do
-      url "https://github.com/ARMmbed/mbedtls/commit/b92834324f29768a5bf39c58c674c5f3c09b6763.patch"
-      sha256 "f48b42e10ed0462945391ac2c7eb737ed39d377d91960baeaf91fe1325b38c96"
-    end
-    patch do
-      url "https://github.com/ARMmbed/mbedtls/commit/23e9778684ba734dbfba1445e145b04dd6b59e76.patch"
-      sha256 "5f642020d7706660778a14547425c5cb5a8c99b56b92ed46514b7a43823487b7"
-    end
-  end
 
   bottle do
     cellar :any
@@ -54,12 +31,12 @@ class Mbedtls < Formula
     system "make", "install"
 
     # Why does Mbedtls ship with a "Hello World" executable. Let's remove that.
-    rm_f "#{bin}/hello"
+    rm_f bin/"hello"
     # Rename benchmark & selftest, which are awfully generic names.
     mv bin/"benchmark", bin/"mbedtls-benchmark"
     mv bin/"selftest", bin/"mbedtls-selftest"
     # Demonstration files shouldn't be in the main bin
-    libexec.install "#{bin}/mpi_demo"
+    libexec.install bin/"mpi_demo"
   end
 
   test do
