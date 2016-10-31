22a8a46a39569b5630a5988a279d2fd594b7a4e9
ilovezfs <ilovezfs@icloud.com>
2016-10-30 21:35:37 -0700

    esniper 2.32
    Closes #6436.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/esniper.rb
+++ Formula/esniper.rb
@@ -1,9 +1,9 @@
 class Esniper < Formula
   desc "Snipe eBay auctions from the command-line"
   homepage "https://sourceforge.net/projects/esniper/"
-  url "https://downloads.sourceforge.net/project/esniper/esniper/2.31.0/esniper-2-31-0.tgz"
-  version "2.31"
-  sha256 "30d2378c700b72b5363c8af59e7566564d9ec8cd4b44cd389c2830907d7bc676"
+  url "https://downloads.sourceforge.net/project/esniper/esniper/2.32.0/esniper-2-32-0.tgz"
+  version "2.32"
+  sha256 "5ee3e5237c6a12059ae9d38380505d38448c2c12787f9fcc603cdb0a1a519312"
 
   bottle do
     cellar :any_skip_relocation
