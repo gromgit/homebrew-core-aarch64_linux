3630740b7fc5eb8208c6e7b7c2ac847ce4468e2a
ilovezfs <ilovezfs@icloud.com>
2016-10-31 02:33:36 -0700

    dc3dd 7.2.646
    Closes #6439.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dc3dd.rb
+++ Formula/dc3dd.rb
@@ -1,8 +1,9 @@
 class Dc3dd < Formula
   desc "Patched GNU dd that is intended for forensic acquisition of data"
   homepage "https://sourceforge.net/projects/dc3dd/"
-  url "https://downloads.sourceforge.net/project/dc3dd/dc3dd/7.1.0/dc3dd-7.1.614.tar.gz"
-  sha256 "f6fb4d921928e6354e9f8ff2a8415ebc42b129959996e1ab0d10899c11aac198"
+  url "https://downloads.sourceforge.net/project/dc3dd/dc3dd/7.2.646/dc3dd%207.2.646/dc3dd-7.2.646.zip",
+      :using => :nounzip
+  sha256 "c4e325e5cbdae49e3855b0849ea62fed17d553428724745cea53fe6d91fd2b7f"
 
   bottle do
     cellar :any_skip_relocation
@@ -11,7 +12,33 @@ class Dc3dd < Formula
     sha256 "dc413cbaaf429d0304d2c678b154a411fd631ee6556c7784587c2547f14c6a1b" => :mavericks
   end
 
+  depends_on "gettext"
+
+  resource "gettext-pm" do
+    url "https://cpan.metacpan.org/authors/id/P/PV/PVANDRY/gettext-1.07.tar.gz"
+    mirror "http://search.cpan.org/CPAN/authors/id/P/PV/PVANDRY/gettext-1.07.tar.gz"
+    sha256 "909d47954697e7c04218f972915b787bd1244d75e3bd01620bc167d5bbc49c15"
+  end
+
   def install
+    # Regular zip files created by 7-zip can upset /usr/bin/unzip by reporting a
+    # non-zero size for dirs; the workaround below avoids a p7zip dependency
+    # Reported 32 Oct 2016 https://sourceforge.net/p/dc3dd/bugs/14/
+    zip_file = cached_download.basename(".zip")
+    Utils.popen_read("unzip #{zip_file}.zip")
+    buildpath.install (buildpath/zip_file).children
+
+    ENV.prepend_create_path "PERL5LIB", buildpath/"gettext-pm/lib/perl5"
+    resource("gettext-pm").stage do
+      inreplace "Makefile.PL", "$libs = \"-lintl\"",
+                               "$libs = \"-L/usr/local/opt/gettext/lib -lintl\""
+      system "perl", "Makefile.PL", "INSTALL_BASE=#{buildpath}/gettext-pm"
+      system "make"
+      system "make", "install"
+    end
+
+    chmod 0555, ["build-aux/install-sh", "configure"]
+
     args = %W[--disable-debug
               --disable-dependency-tracking
               --prefix=#{prefix}
@@ -24,5 +51,9 @@ class Dc3dd < Formula
     system "make"
     system "make", "install"
     prefix.install %w[Options_Reference.txt Sample_Commands.txt]
+  end
+
+  test do
+    system bin/"dc3dd", "--help"
   end
 end
