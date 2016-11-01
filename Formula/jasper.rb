00830df51e6e8ae709184d90755d8144d1412676
Dominyk Tiller <dominyktiller@gmail.com>
2016-11-01 00:52:09 +0000

    jasper 1.900.16

--- Formula/jasper.rb
+++ Formula/jasper.rb
@@ -1,9 +1,8 @@
 class Jasper < Formula
   desc "Library for manipulating JPEG-2000 images"
   homepage "https://www.ece.uvic.ca/~frodo/jasper/"
-  url "http://download.osgeo.org/gdal/jasper-1.900.1.uuid.tar.gz"
-  version "1.900.1"
-  sha256 "0021684d909de1eb2f7f5a4d608af69000ce37773d51d1fb898e03b8d488087d"
+  url "https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.16.tar.gz"
+  sha256 "349f2261767c51a9e59e37025a027686f45f55cfbb8c46fd55c8697092f9f971"
 
   bottle do
     cellar :any
@@ -24,12 +23,6 @@ class Jasper < Formula
     cause "Undefined symbols when linking"
   end
 
-  # The following patch fixes a bug (still in upstream as of jasper 1.900.1)
-  # where an assertion fails when Jasper is fed certain JPEG-2000 files with
-  # an alpha channel. See:
-  # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=469786
-  patch :DATA
-
   def install
     ENV.universal_binary if build.universal?
     system "./configure", "--disable-debug",
@@ -40,30 +33,3 @@ class Jasper < Formula
     system "make", "install"
   end
 end
-
-__END__
-diff --git a/src/libjasper/jpc/jpc_dec.c b/src/libjasper/jpc/jpc_dec.c
-index fa72a0e..1f4845f 100644
---- a/src/libjasper/jpc/jpc_dec.c
-+++ b/src/libjasper/jpc/jpc_dec.c
-@@ -1069,12 +1069,18 @@ static int jpc_dec_tiledecode(jpc_dec_t *dec, jpc_dec_tile_t *tile)
-	/* Apply an inverse intercomponent transform if necessary. */
-	switch (tile->cp->mctid) {
-	case JPC_MCT_RCT:
--		assert(dec->numcomps == 3);
-+		if (dec->numcomps != 3 && dec->numcomps != 4) {
-+			jas_eprintf("bad number of components (%d)\n", dec->numcomps);
-+			return -1;
-+		}
-		jpc_irct(tile->tcomps[0].data, tile->tcomps[1].data,
-		  tile->tcomps[2].data);
-		break;
-	case JPC_MCT_ICT:
--		assert(dec->numcomps == 3);
-+		if (dec->numcomps != 3 && dec->numcomps != 4) {
-+			jas_eprintf("bad number of components (%d)\n", dec->numcomps);
-+			return -1;
-+		}
-		jpc_iict(tile->tcomps[0].data, tile->tcomps[1].data,
-		  tile->tcomps[2].data);
-		break;
