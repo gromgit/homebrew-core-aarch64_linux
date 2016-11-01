2a0e5ae94439aba67d17de54306366376a9bc82e
ilovezfs <ilovezfs@icloud.com>
2016-11-01 14:41:45 -0700

    libsamplerate 0.1.9
    Closes #6489.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libsamplerate.rb
+++ Formula/libsamplerate.rb
@@ -1,8 +1,8 @@
 class Libsamplerate < Formula
   desc "Library for sample rate conversion of audio data"
   homepage "http://www.mega-nerd.com/SRC"
-  url "http://www.mega-nerd.com/SRC/libsamplerate-0.1.8.tar.gz"
-  sha256 "93b54bdf46d5e6d2354b7034395fe329c222a966790de34520702bb9642f1c06"
+  url "http://www.mega-nerd.com/SRC/libsamplerate-0.1.9.tar.gz"
+  sha256 "0a7eb168e2f21353fb6d84da152e4512126f7dc48ccb0be80578c565413444c1"
 
   bottle do
     cellar :any
