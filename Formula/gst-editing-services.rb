a71767dbff9e21b1531685c23f7220f3b482c679
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-02 10:57:19 +0000

    gst-editing-services: update 1.10.0 bottle.

--- Formula/gst-editing-services.rb
+++ Formula/gst-editing-services.rb
@@ -5,10 +5,9 @@ class GstEditingServices < Formula
   sha256 "1f2c8b8ba33c1cc746f129bb9d6b85e5fc4eff5fbb1c85bbc2c5b6683362560c"
 
   bottle do
-    sha256 "904dab5c27793e042b8011c865dd54cc646f54d4919351bf4cdd22f6e4f29190" => :sierra
-    sha256 "0700828b35f87a30550a67407b37efe2c4f3a95e0862b47a341c83c20583722b" => :el_capitan
-    sha256 "ed4d5a32de582c6909316e096dbf89ce3c11165b734e613114aa2b1c61ec692a" => :yosemite
-    sha256 "301359de1f3de795464c84d36f9685caf4f2acf54f8d722c997812a2da8b480e" => :mavericks
+    sha256 "0ffcda1ae32f30fb11019fa191a8b3d4ccd5c6b717bc787d9265f1a02906b3e5" => :sierra
+    sha256 "cc2c75248812f2385cc4ef9520f2f50b900180df8f24d06eaa2c3faed553a678" => :el_capitan
+    sha256 "d3c44ee64b7b467f4dc7500361b1cc607c49ab1258e266c82e2f74d520d84e33" => :yosemite
   end
 
   depends_on "gstreamer"
