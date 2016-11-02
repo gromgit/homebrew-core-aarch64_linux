03a7013ed2d3879ce6326f7617df2d83bf4c487a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-02 09:55:28 +0000

    curl: update 7.51.0 bottle.

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -6,10 +6,9 @@ class Curl < Formula
 
   bottle do
     cellar :any
-    sha256 "638108732f8c4dacea7953c81b070d8ab8bde837e0608f0b4ca36124b7ff1055" => :sierra
-    sha256 "b425bee3c2602e9b470db43b00418805e97ab675bfdd292be194e61fca8d9f52" => :el_capitan
-    sha256 "acd850690b6578bf1f7682804734cadf5b922aa17d696ef5154b15d048712319" => :yosemite
-    sha256 "cfbb0a2c28b150d13a239b9a6acd95cdd530cf57f7e26831f966d672e88dfcc0" => :mavericks
+    sha256 "fbd406e5b08a68fa79355c0a7748a7f0ec0cf85dfc95004126a726e539809dfc" => :sierra
+    sha256 "d91f9c9cef38c17a8a7f605a993ec0d7e318359700aafcc6267274e6234cde38" => :el_capitan
+    sha256 "81ed544005d737772c44103a84aabaf911962b460e5494688e78b5e666d63212" => :yosemite
   end
 
   keg_only :provided_by_osx
