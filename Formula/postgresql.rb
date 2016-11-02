7de63e23b0402cd99626ff750fb11f8c7eec571b
Alan Orth <alan.orth@gmail.com>
2016-11-02 09:05:50 +0200

    postgresql 9.5.5
    Closes #6501.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/postgresql.rb
+++ Formula/postgresql.rb
@@ -1,9 +1,8 @@
 class Postgresql < Formula
   desc "Object-relational database system"
   homepage "https://www.postgresql.org/"
-  url "https://ftp.postgresql.org/pub/source/v9.5.4/postgresql-9.5.4.tar.bz2"
-  sha256 "cf5e571164ad66028ecd7dd8819e3765470d45bcd440d258b686be7e69c76ed0"
-  revision 1
+  url "https://ftp.postgresql.org/pub/source/v9.5.5/postgresql-9.5.5.tar.bz2"
+  sha256 "02c65290be74de6604c3fed87c9fd3e6b32e949f0ab8105a75bd7ed5aa71f394"
 
   head "https://github.com/postgres/postgres.git"
 
