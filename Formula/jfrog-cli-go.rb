c2593ff932c453a80d20af7b31ef30f2e84f981f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-03 08:56:41 +0000

    jfrog-cli-go: update 1.5.1 bottle.

--- Formula/jfrog-cli-go.rb
+++ Formula/jfrog-cli-go.rb
@@ -6,9 +6,9 @@ class JfrogCliGo < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "aa9649be99921715d09179083eefe55bdc8d1f12772cc2a30e5953326832c5ef" => :sierra
-    sha256 "3ad2ea145f3949675e4e79ffc9accff0ee90ba883fc7e8ac53741232461adc5c" => :el_capitan
-    sha256 "ea2bce23173b9b5dc5453ce2261c5bd50b6d454a50573ce3913792802bc7187a" => :yosemite
+    sha256 "623d6192f2a54e3e0ebb71f8fb56e54a873b3de0bd999c4fd75112884e099eaf" => :sierra
+    sha256 "6febd1355d658f72a87438b005f0699ba5ac30db089dd596d1700cf2ec502676" => :el_capitan
+    sha256 "d5262211a5243ba4c415a152b7318654effb71150543704ca2592ff08c97cfa0" => :yosemite
   end
 
   depends_on "go" => :build
