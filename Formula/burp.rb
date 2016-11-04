59b262175056d4f5f017887daea765e5a259e0f4
ilovezfs <ilovezfs@icloud.com>
2016-11-04 02:36:13 -0700

    burp 2.0.50 (devel) (#6575)

--- Formula/burp.rb
+++ Formula/burp.rb
@@ -19,8 +19,8 @@ class Burp < Formula
   end
 
   devel do
-    url "https://github.com/grke/burp/archive/2.0.48.tar.gz"
-    sha256 "62c8304afc30f764eb16c6f0794e593d3ad24850261d1a3b705156bded16116a"
+    url "https://github.com/grke/burp/archive/2.0.50.tar.gz"
+    sha256 "c7bfe015c0ab276ab7a4133e769bacfc41e0196137174124916590922138f349"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
