1d71fa0b76ece07bf5684f21272e758808139a88
Alex Dunn <dunn.alex@gmail.com>
2016-11-03 19:17:57 -0700

    xapian 1.4.1

--- Formula/xapian.rb
+++ Formula/xapian.rb
@@ -1,16 +1,9 @@
 class Xapian < Formula
   desc "C++ search engine library with many bindings"
   homepage "https://xapian.org/"
-
-  stable do
-    url "https://oligarchy.co.uk/xapian/1.2.23/xapian-core-1.2.23.tar.xz"
-    sha256 "9783aeae4e1a6d06e5636b270db4b458a7d0804a31da158269f57fa5dc86347d"
-
-    resource "bindings" do
-      url "https://oligarchy.co.uk/xapian/1.2.23/xapian-bindings-1.2.23.tar.xz"
-      sha256 "19b4b56c74863c51733d8c2567272ef7f004b898cf44016711ae25bc524b2215"
-    end
-  end
+  url "https://oligarchy.co.uk/xapian/1.4.1/xapian-core-1.4.1.tar.xz"
+  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/x/xapian-core/xapian-core_1.4.1.orig.tar.xz"
+  sha256 "c5f2534de73c067ac19eed6d6bec65b7b2c1be00131c8867da9e1dfa8bce70eb"
 
   bottle do
     cellar :any
@@ -18,17 +11,6 @@ class Xapian < Formula
     sha256 "08232fd96069f4d80cf947a22d40a729bb9a65169e0c60eb59091041da987722" => :el_capitan
     sha256 "a076ee6fbab9f6eb3171171d9d54d68f2b6ffcabf88016d028b16bc68cfeafdf" => :yosemite
     sha256 "f881021eee674478fbd70179414d27e502a1de2751e8d4454f933551052b06fb" => :mavericks
-  end
-
-  devel do
-    url "https://oligarchy.co.uk/xapian/1.3.5/xapian-core-1.3.5.tar.xz"
-    mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/x/xapian-core/xapian-core_1.3.5.orig.tar.xz"
-    sha256 "3ad99ff4e91a4ff997fd576377e7c8f0134ceb3695c49e8f7d78ebf3c19b70ad"
-
-    resource "bindings" do
-      url "https://oligarchy.co.uk/xapian/1.3.5/xapian-bindings-1.3.5.tar.xz"
-      sha256 "4b5b9089d39b2a725651349127f64d24fe66db46572bdd92f39b8483bca400c3"
-    end
   end
 
   option "with-java", "Java bindings"
@@ -42,6 +24,11 @@ class Xapian < Formula
   depends_on :python => :optional
 
   skip_clean :la
+
+  resource "bindings" do
+    url "https://oligarchy.co.uk/xapian/1.4.1/xapian-bindings-1.4.1.tar.xz"
+    sha256 "6ca9731eed0fdfd84c6f8d788389bc7e7a7dc62fa46e0383eb0bb502576c2331"
+  end
 
   def install
     build_binds = build.with?("ruby") || build.with?("python") || build.with?("java") || build.with?("php")
@@ -112,7 +99,6 @@ class Xapian < Formula
   end
 
   test do
-    suffix = devel? ? "-1.3" : ""
-    system bin/"xapian-config#{suffix}", "--libs"
+    system bin/"xapian-config", "--libs"
   end
 end
