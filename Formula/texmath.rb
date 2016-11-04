e4bf0c6707f9bd00f1e9068c8d06bccafde0f062
ilovezfs <ilovezfs@icloud.com>
2016-11-04 06:44:40 -0700

    texmath 0.8.6.7
    Closes #6596.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/texmath.rb
+++ Formula/texmath.rb
@@ -5,8 +5,8 @@ class Texmath < Formula
 
   desc "Haskell library for converting LaTeX math to MathML"
   homepage "http://johnmacfarlane.net/texmath.html"
-  url "https://hackage.haskell.org/package/texmath-0.8.6.6/texmath-0.8.6.6.tar.gz"
-  sha256 "9c78e53e685b4537a39a4d2bc785df1d0d0ee775085bd532d8ae88d10d4c58b4"
+  url "https://hackage.haskell.org/package/texmath-0.8.6.7/texmath-0.8.6.7.tar.gz"
+  sha256 "9e5fd9571a7257bdc8cfa6e0da077b16e867011a9f813065d68dd046bd358c88"
 
   bottle do
     sha256 "eb4644a054fe9e0a0edd63941defb462d9ff6bbdd5b4a5c6c2253150c9e357dc" => :sierra
