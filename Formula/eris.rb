ea572085cac36767af303cfc905f1c2111e87724
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-04 06:17:25 +0000

    eris: update 0.12.0 bottle.

--- Formula/eris.rb
+++ Formula/eris.rb
@@ -7,10 +7,9 @@ class Eris < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "dd142c8a5911ed924f9f84790299ed41c01e912f34fab3a5b1edfd062c6983d4" => :sierra
-    sha256 "a2320c9b6b108ac65a7377b91243470b5513fa91622655036a652cdb8616c66a" => :el_capitan
-    sha256 "3d62226e28cc5f9293c8a4a3dae5c3d3a865dec7e85381d36fa443455c522c30" => :yosemite
+    sha256 "9542a6efef2e45633ca7607f4d4b70362a98ddb8ef7038a830a45c1da623caf6" => :sierra
+    sha256 "ae31e13e58224e20c669a49bb9939aa00f5fd3dbf245449e077cefdff16fd00e" => :el_capitan
+    sha256 "da8d00b5d246007abcb16715da89ed5cb46eb6742cb553dc35e23f8ee053c949" => :yosemite
   end
 
   depends_on "go" => :build
