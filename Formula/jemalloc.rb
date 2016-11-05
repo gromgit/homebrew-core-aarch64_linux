1b6714cc6768d836dae8638824bf33a7b4d7d0fd
ilovezfs <ilovezfs@icloud.com>
2016-11-05 03:46:09 -0700

    jemalloc 4.3.0
    Closes #6625.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/jemalloc.rb
+++ Formula/jemalloc.rb
@@ -1,9 +1,8 @@
 class Jemalloc < Formula
   desc "malloc implementation emphasizing fragmentation avoidance"
   homepage "http://www.canonware.com/jemalloc/"
-  url "https://github.com/jemalloc/jemalloc/releases/download/4.2.1/jemalloc-4.2.1.tar.bz2"
-  sha256 "5630650d5c1caab95d2f0898de4fe5ab8519dc680b04963b38bb425ef6a42d57"
-  revision 1
+  url "https://github.com/jemalloc/jemalloc/releases/download/4.3.0/jemalloc-4.3.0.tar.bz2"
+  sha256 "2142d4093708b2f988f60ed5fd8d869447cd9f5354933e596400c0a69bfef5e0"
   head "https://github.com/jemalloc/jemalloc.git"
 
   bottle do
@@ -13,19 +12,13 @@ class Jemalloc < Formula
     sha256 "90886fb8e88457061e03693c2955163a790f223a8365584f836c924c3b92ae1f" => :yosemite
   end
 
-  # https://github.com/jemalloc/jemalloc/issues/420
-  # Should be in the next release, but please check.
-  patch do
-    url "https://github.com/jemalloc/jemalloc/commit/4abaee5d13.patch"
-    sha256 "05c754089098c4275b460b90d1f4b94e32a2c819496187e5378e460c9398a65f"
-  end
-
-  patch do
-    url "https://github.com/jemalloc/jemalloc/commit/19c9a3e828.patch"
-    sha256 "b736dab20d2688d4b21b4ba4755fd19b68145b2d9ae299a1ae154e8553d9261d"
-  end
-
   def install
+    # dyld: lazy symbol binding failed: Symbol not found: _os_unfair_lock_lock
+    # Reported 6 Nov 2016 https://github.com/jemalloc/jemalloc/issues/494
+    if MacOS.version == :el_capitan && MacOS::Xcode.installed? && MacOS::Xcode.version >= "8.0"
+      ENV["je_cv_os_unfair_lock"] = "no"
+    end
+
     system "./configure", "--disable-debug", "--prefix=#{prefix}", "--with-jemalloc-prefix="
     system "make"
     system "make", "check"
