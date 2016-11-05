19cf80b60193ef579292499bc073c141b5092e4c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-05 20:36:23 +0000

    ipbt: update 20160908 bottle.

--- Formula/ipbt.rb
+++ Formula/ipbt.rb
@@ -7,10 +7,9 @@ class Ipbt < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "2a0909001f6dd70a27e1f2a595c86043f03f1c7064a3a7bbffc339a4c2a4f327" => :el_capitan
-    sha256 "31cb431e2612d8eb4c1fc1653d675c2d6789c74acf3fbe09367df8e0b5350a8e" => :yosemite
-    sha256 "d5ae41af0fa56f3cfb1e5fef2e17fb20fc2d18b2b1c11ecb0f0d57fd97edfc58" => :mavericks
-    sha256 "2f6782c8fa4d72545b367c6a210d58bffa87db3e36c8d66ac28934183a6d4547" => :mountain_lion
+    sha256 "a9de6013e11b074ac7ecb8036e75b6954690add7213245bf3eaffd16182f9669" => :sierra
+    sha256 "5d0a67f1c51c0e4f15b498f0877b491240b36207099a98d19ca7b0acf8313722" => :el_capitan
+    sha256 "6b60f3a8177c298cec4bce8a53361fbea60e8f71be24b35504b2a699c2bd3f26" => :yosemite
   end
 
   def install
