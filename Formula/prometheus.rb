6d64def8e48fe4e9408bcafe9cab3ad83f47673b
ilovezfs <ilovezfs@icloud.com>
2016-11-05 03:48:59 -0700

    prometheus 1.3.1
    Closes #6627.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/prometheus.rb
+++ Formula/prometheus.rb
@@ -1,8 +1,8 @@
 class Prometheus < Formula
   desc "Service monitoring system and time series database"
   homepage "https://prometheus.io/"
-  url "https://github.com/prometheus/prometheus/archive/v1.3.0.tar.gz"
-  sha256 "df8845515085b5babf308ac871d6e939a51a8f2a865eb4dd5a058af99a7d0f5c"
+  url "https://github.com/prometheus/prometheus/archive/v1.3.1.tar.gz"
+  sha256 "6463369891cc9e748e1025a600bc948e95f276830c58b44689165d5d8de7f5f6"
 
   bottle do
     cellar :any_skip_relocation
