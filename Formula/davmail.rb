98d48cf759c952f5ef7d6e61ee142506727825c6
Patrick O'Brien <obrien.patrickl@gmail.com>
2016-11-06 07:46:01 -0800

    davmail: fix formula to add plist for brew services
    Closes #6493.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/davmail.rb
+++ Formula/davmail.rb
@@ -10,4 +10,37 @@ class Davmail < Formula
     libexec.install Dir["*"]
     bin.write_jar_script libexec/"davmail.jar", "davmail"
   end
+
+  plist_options :manual => "davmail"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>EnvironmentVariables</key>
+        <dict>
+          <key>PATH</key>
+          <string>/usr/bin:/bin:/usr/sbin:/sbin:#{HOMEBREW_PREFIX}/bin</string>
+        </dict>
+        <key>KeepAlive</key>
+        <false/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/davmail</string>
+        </array>
+        <key>StartInterval</key>
+        <integer>300</integer>
+        <key>RunAtLoad</key>
+        <true />
+        <key>StandardErrorPath</key>
+        <string>/dev/null</string>
+        <key>StandardOutPath</key>
+        <string>/dev/null</string>
+      </dict>
+    </plist>
+    EOS
+  end
 end
