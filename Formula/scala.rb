7a76277f98a5798cf5da5c11965276b4e22f6711
Mike McQuaid <mike@mikemcquaid.com>
2016-11-08 09:45:16 +0000

    Revert "scala 2.12.0"
    This reverts commit 2a1675c23fa256b7cf9aaf77093e2b99c903ed62.

--- Formula/scala.rb
+++ Formula/scala.rb
@@ -1,9 +1,23 @@
 class Scala < Formula
   desc "JVM-based programming language"
   homepage "https://www.scala-lang.org/"
-  url "https://downloads.lightbend.com/scala/2.12.0/scala-2.12.0.tgz"
-  mirror "http://www.scala-lang.org/files/archive/scala-2.12.0.tgz"
-  sha256 "d4f485b3f05bbf1224ed5db34d5a5322ce8d1676236757a6cfbf4a11ac59ca22"
+
+  stable do
+    url "https://downloads.lightbend.com/scala/2.11.8/scala-2.11.8.tgz"
+    sha256 "87fc86a19d9725edb5fd9866c5ee9424cdb2cd86b767f1bb7d47313e8e391ace"
+
+    depends_on :java => "1.6+"
+
+    resource "docs" do
+      url "https://downloads.lightbend.com/scala/2.11.8/scala-docs-2.11.8.zip"
+      sha256 "73bd44375ebffd5f401950a11d78addc52f8164c30d8528d26c82c1f819cfc16"
+    end
+
+    resource "src" do
+      url "https://github.com/scala/scala/archive/v2.11.8.tar.gz"
+      sha256 "4f11273b4b3c771019253b2c09102245d063a7abeb65c7b1c4519bd57605edcf"
+    end
+  end
 
   bottle do
     cellar :any_skip_relocation
@@ -13,24 +27,31 @@ class Scala < Formula
     sha256 "ddd6e527a6e93c326d761c61d9811648c1eba82044ef24ded32837fa37581c16" => :mavericks
   end
 
+  devel do
+    url "http://www.scala-lang.org/files/archive/scala-2.12.0-RC1.tgz"
+    sha256 "ac4f05a75306b241a60a57dfabd8c5adec15435f0f380688ee5eb1caaa600192"
+    version "2.12.0-RC1"
+
+    depends_on :java => "1.8+"
+
+    resource "docs" do
+      url "http://www.scala-lang.org/files/archive/scala-docs-2.12.0-RC1.zip"
+      sha256 "4c1deb463b5d9e4ec0915b90557cc9e856609262f2cbeaeb6f248ae2054ea41d"
+      version "2.12.0-RC1"
+    end
+
+    resource "src" do
+      url "https://github.com/scala/scala/archive/v2.12.0-RC1.tar.gz"
+      sha256 "55219e9d25c28a7c3966c6333b06c7d718be89bc5f8310b2428d47c9f38be851"
+      version "2.12.0-RC1"
+    end
+  end
+
   option "with-docs", "Also install library documentation"
   option "with-src", "Also install sources for IDE support"
 
-  depends_on :java => "1.8+"
-
-  resource "docs" do
-    url "https://downloads.lightbend.com/scala/2.12.0/scala-docs-2.12.0.txz"
-    mirror "http://www.scala-lang.org/files/archive/scala-docs-2.12.0.txz"
-    sha256 "52e57918e7090c47354fbc11067c7a50a5c075dbfba6b297fd25f3ac606c4728"
-  end
-
-  resource "src" do
-    url "https://github.com/scala/scala/archive/v2.12.0.tar.gz"
-    sha256 "692c66a898e7a658303664a6cb08c2ab3626ca26324197ac7df1b0db82275564"
-  end
-
   resource "completion" do
-    url "https://raw.githubusercontent.com/scala/scala-tool-support/0a217bc/bash-completion/src/main/resources/completion.d/2.9.1/scala"
+    url "https://raw.githubusercontent.com/scala/scala-dist/v2.11.4/bash-completion/src/main/resources/completion.d/2.9.1/scala"
     sha256 "95aeba51165ce2c0e36e9bf006f2904a90031470ab8d10b456e7611413d7d3fd"
   end
 
