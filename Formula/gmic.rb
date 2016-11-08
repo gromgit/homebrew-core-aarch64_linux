33e88acc78022bc79817f484996d96ae2b2a5131
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-08 17:42:41 +0000

    gmic: add 1.7.8 bottle.

--- Formula/gmic.rb
+++ Formula/gmic.rb
@@ -5,6 +5,13 @@ class Gmic < Formula
   sha256 "3a2f32d79714239cfa56ecb10799b7d73362b4e2a852444bc24866272cd041a4"
   head "https://github.com/dtschump/gmic.git"
 
+  bottle do
+    cellar :any
+    sha256 "ba328c321a3512d50e3c629cd90f07708d2c7f419864256b11f45c0598e88eb6" => :sierra
+    sha256 "8fbc070d68220caff4f6163ba5047041cf468ca1cb56cd994e160cec2541e6d8" => :el_capitan
+    sha256 "59505d916d46bb661986fa3291ecc781c06f94aa3fb1ce84fd3f2a3f07084929" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "jpeg" => :recommended
   depends_on "libpng" => :recommended
