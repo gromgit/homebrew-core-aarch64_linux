4508a42176b775444589a9846494688ab5bc8ee5
ilovezfs <ilovezfs@icloud.com>
2016-11-08 03:51:36 -0800

    libosip 5.0.0

--- Formula/libosip.rb
+++ Formula/libosip.rb
@@ -1,9 +1,9 @@
 class Libosip < Formula
   desc "Implementation of the eXosip2 stack"
   homepage "https://www.gnu.org/software/osip/"
-  url "https://ftpmirror.gnu.org/osip/libosip2-4.1.0.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/osip/libosip2-4.1.0.tar.gz"
-  sha256 "996aa0363316a871915b6f12562af53853a9962bb93f6abe1ae69f8de7008504"
+  url "https://ftpmirror.gnu.org/osip/libosip2-5.0.0.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/osip/libosip2-5.0.0.tar.gz"
+  sha256 "18a13c954f7297978e7bf1a0cdadde7c531e519d61a045dae304e054f3b2df03"
 
   bottle do
     cellar :any
