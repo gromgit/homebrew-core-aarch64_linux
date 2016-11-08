7aa270a2618c777a41530a46c974d2b01601f48f
Tommy Sparber <tommy@sparber.net>
2016-11-08 12:55:04 +0100

    sdl_image: disable ImageIO
    Closes #6723.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/sdl_image.rb
+++ Formula/sdl_image.rb
@@ -3,7 +3,7 @@ class SdlImage < Formula
   homepage "https://www.libsdl.org/projects/SDL_image"
   url "https://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12.tar.gz"
   sha256 "0b90722984561004de84847744d566809dbb9daf732a9e503b91a1b5a84e5699"
-  revision 3
+  revision 4
 
   bottle do
     cellar :any
@@ -36,6 +36,7 @@ class SdlImage < Formula
 
     system "./configure", "--prefix=#{prefix}",
                           "--disable-dependency-tracking",
+                          "--disable-imageio",
                           "--disable-sdltest"
     system "make", "install"
   end
