29bb78d8eee3c6d98f65cf93ddb29ab88fbb01d3
ilovezfs <ilovezfs@icloud.com>
2016-11-08 00:06:25 -0800

    rhash 1.3.4
    Closes #6711.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/rhash.rb
+++ Formula/rhash.rb
@@ -1,10 +1,9 @@
 class Rhash < Formula
   desc "Utility for computing and verifying hash sums of files"
   homepage "http://rhash.anz.ru/"
-  url "https://downloads.sourceforge.net/project/rhash/rhash/1.3.3/rhash-1.3.3-src.tar.gz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/r/rhash/rhash_1.3.3.orig.tar.gz"
-  sha256 "5b520b597bd83f933d316fce1382bb90e0b0b87b559b8c9c9a197551c935315a"
-
+  url "https://downloads.sourceforge.net/project/rhash/rhash/1.3.4/rhash-1.3.4-src.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/r/rhash/rhash_1.3.4.orig.tar.gz"
+  sha256 "406662c4703bd4cb1caae26f32700951a5e12adf39f141d3f40e0b461b1e9e49"
   head "https://github.com/rhash/RHash.git"
 
   bottle do
