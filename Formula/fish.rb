29df28e8dc99bf778efcb673369d6356afb834b8
David Adam <zanchey@ucc.gu.uwa.edu.au>
2016-11-08 11:58:02 +0800

    fish 2.4.0
    Closes #6699.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/fish.rb
+++ Formula/fish.rb
@@ -3,14 +3,9 @@ class Fish < Formula
   homepage "https://fishshell.com"
 
   stable do
-    url "https://fishshell.com/files/2.3.1/fish-2.3.1.tar.gz"
-    mirror "https://github.com/fish-shell/fish-shell/releases/download/2.3.1/fish-2.3.1.tar.gz"
-    sha256 "328acad35d131c94118c1e187ff3689300ba757c4469c8cc1eaa994789b98664"
-
-    # Skip extra dirs creation during install phase patch for current stable. To be removed on the next fish release.
-    # As discussed in https://github.com/Homebrew/homebrew-core/pull/2813
-    # Use part of https://github.com/fish-shell/fish-shell/commit/9abbc5f upstream commit and patch the makefile.
-    patch :DATA
+    url "https://fishshell.com/files/2.4.0/fish-2.4.0.tar.gz"
+    mirror "https://github.com/fish-shell/fish-shell/releases/download/2.4.0/fish-2.4.0.tar.gz"
+    sha256 "06bbb2323360439c4044da762d114ec1aa1aba265cec71c0543e6a0095c9efc5"
   end
 
   bottle do
@@ -19,14 +14,6 @@ class Fish < Formula
     sha256 "9b14c4a2cc0ff774d4b7d1dfbec92ff96c3e4655e7b6b3f06122c7fea1c46a8e" => :el_capitan
     sha256 "82262790b325b614b6c550a1e07b7c0f5da3ac21aba78d28dfb0e61f6812ecc5" => :yosemite
     sha256 "19fe34be078746b38b4b8bcf0f384c80963a6437587844116645ee1679e444bd" => :mavericks
-  end
-
-  devel do
-    url "https://github.com/fish-shell/fish-shell/releases/download/2.4b1/fish-2.4b1.tar.gz"
-    sha256 "36139e09064f7904d8b7de4603789c41c1a9bf425280494468948de61572cbbd"
-
-    depends_on "autoconf" => :build
-    depends_on "doxygen" => :build
   end
 
   head do
@@ -39,7 +26,7 @@ class Fish < Formula
   depends_on "pcre2"
 
   def install
-    system "autoconf" if build.head? || build.devel?
+    system "autoconf" if build.head?
 
     # In Homebrew's 'superenv' sed's path will be incompatible, so
     # the correct path is passed into configure here.
@@ -75,18 +62,3 @@ class Fish < Formula
     system "#{bin}/fish", "-c", "echo"
   end
 end
-
-__END__
-As discussed in https://github.com/Homebrew/homebrew-core/pull/2813
-Grab part of https://github.com/fish-shell/fish-shell/commit/9abbc5f upstream commit and patch the makefile.
---- a/Makefile.in
-+++ b/Makefile.in@@ -664,5 +664,5 @@ install-force: all install-translations
-@@ -664,5 +664,5 @@ install-force: all install-translations
-	$(INSTALL) -m 755 -d $(DESTDIR)$(datadir)/fish/completions
--	$(INSTALL) -m 755 -d $(DESTDIR)$(extra_completionsdir)
--	$(INSTALL) -m 755 -d $(DESTDIR)$(extra_functionsdir)
--	$(INSTALL) -m 755 -d $(DESTDIR)$(extra_confdir)
-+	$(INSTALL) -m 755 -d $(DESTDIR)$(extra_completionsdir); true
-+	$(INSTALL) -m 755 -d $(DESTDIR)$(extra_functionsdir); true
-+	$(INSTALL) -m 755 -d $(DESTDIR)$(extra_confdir); true
-	$(INSTALL) -m 755 -d $(DESTDIR)$(datadir)/fish/functions
