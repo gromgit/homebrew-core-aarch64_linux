59160d3a59d59a43db9279d4aef1c4bdf5f47caa
Ewen Cheslack-Postava <me@ewencp.org>
2016-11-08 14:25:27 -0800

    librdkafka 0.9.2
    Closes #6741.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/librdkafka.rb
+++ Formula/librdkafka.rb
@@ -1,8 +1,8 @@
 class Librdkafka < Formula
   desc "The Apache Kafka C/C++ library"
   homepage "https://github.com/edenhill/librdkafka"
-  url "https://github.com/edenhill/librdkafka/archive/0.9.1.tar.gz"
-  sha256 "5ad57e0c9a4ec8121e19f13f05bacc41556489dfe8f46ff509af567fdee98d82"
+  url "https://github.com/edenhill/librdkafka/archive/v0.9.2.tar.gz"
+  sha256 "e4fcde677b8cbb6076070a3fbf54ef5f5b495879a2d2e78af8601f2544ee84ae"
 
   head "https://github.com/edenhill/librdkafka.git"
 
