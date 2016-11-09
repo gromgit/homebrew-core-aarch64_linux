dfe217318ee050b33bb928b23ed756da22b2dd7f
_homebrew <_homebrew@alex-macbookpro.home>
2016-11-09 00:20:29 +0000

    rocksdb: added option to build tools
    Closes #6743.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/rocksdb.rb
+++ Formula/rocksdb.rb
@@ -1,5 +1,5 @@
 class Rocksdb < Formula
-  desc "Persistent key-value store for fast storage environments"
+  desc "Embeddable, persistent key-value store for fast storage"
   homepage "http://rocksdb.org"
   url "https://github.com/facebook/rocksdb/archive/v4.11.2.tar.gz"
   sha256 "9374be06fdfccbbdbc60de90b72b5db7040e1bc4e12532e4c67aaec8181b45be"
@@ -12,10 +12,12 @@ class Rocksdb < Formula
   end
 
   option "with-lite", "Build mobile/non-flash optimized lite version"
+  option "with-tools", "Build tools"
 
   needs :cxx11
   depends_on "snappy"
   depends_on "lz4"
+  depends_on "gflags" if build.with? "lite"
 
   def install
     ENV.cxx11
@@ -24,7 +26,18 @@ class Rocksdb < Formula
     system "make", "clean"
     system "make", "static_lib"
     system "make", "shared_lib"
+    system "make", "tools" if build.with? "tools"
     system "make", "install", "INSTALL_PATH=#{prefix}"
+    if build.with? "tools"
+      bin.install "sst_dump" => "rocksdb_sst_dump"
+      bin.install "db_sanity_test" => "rocksdb_sanity_test"
+      bin.install "db_stress" => "rocksdb_stress"
+      bin.install "write_stress" => "rocksdb_write_stress"
+      bin.install "ldb" => "rocksdb_ldb"
+      bin.install "db_repl_stress" => "rocksdb_repl_stress"
+      bin.install "rocksdb_dump"
+      bin.install "rocksdb_undump"
+    end
   end
 
   test do
@@ -35,8 +48,7 @@ class Rocksdb < Formula
       using namespace rocksdb;
       int main() {
         Options options;
-        options.memtable_factory.reset(
-                    NewHashSkipListRepFactory(16));
+        options.memtable_factory.reset(NewHashSkipListRepFactory(16));
         return 0;
       }
     EOS
@@ -48,5 +60,16 @@ class Rocksdb < Formula
                                 "-L#{Formula["snappy"].opt_lib}", "-lsnappy",
                                 "-L#{Formula["lz4"].opt_lib}", "-llz4"
     system "./db_test"
+
+    if build.with? "tools"
+      system "#{bin}/rocksdb_sst_dump", "--help"
+      system "#{bin}/rocksdb_sanity_test", "--help"
+      system "#{bin}/rocksdb_stress", "--help"
+      system "#{bin}/rocksdb_write_stress", "--help"
+      system "#{bin}/rocksdb_ldb", "--help"
+      system "#{bin}/rocksdb_repl_stress", "--help"
+      system "#{bin}/rocksdb_dump", "--help"
+      system "#{bin}/rocksdb_undump", "--help"
+    end
   end
 end
