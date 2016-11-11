ff1130c1295e55bdca894fe5b8054cb16bd4ec03
ilovezfs <ilovezfs@icloud.com>
2016-11-11 02:31:30 -0800

    libexosip: revision for libosip
    Closes #6721.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libexosip.rb
+++ Formula/libexosip.rb
@@ -3,6 +3,7 @@ class Libexosip < Formula
   homepage "https://www.antisip.com/category/osip-and-exosip-toolkit"
   url "https://download.savannah.gnu.org/releases/exosip/libeXosip2-4.1.0.tar.gz"
   sha256 "3c77713b783f239e3bdda0cc96816a544c41b2c96fa740a20ed322762752969d"
+  revision 1
 
   bottle do
     cellar :any
