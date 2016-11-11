8526285842239480ae662356a8513c427fb468f8
Mike McQuaid <mike@mikemcquaid.com>
2016-11-11 14:08:32 +0000

    quazip: use Qt5.
    Qt4 is EOL.

--- Formula/quazip.rb
+++ Formula/quazip.rb
@@ -3,6 +3,7 @@ class Quazip < Formula
   homepage "http://quazip.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/quazip/quazip/0.7.2/quazip-0.7.2.tar.gz"
   sha256 "91d827fbcafd099ae814cc18a8dd3bb709da6b8a27c918ee1c6c03b3f29440f4"
+  revision 1
 
   bottle do
     cellar :any
@@ -11,35 +12,24 @@ class Quazip < Formula
     sha256 "09deaa6657f2465b631b2f19697806f284c6ce328cdcd26bc23cdc3f21261c2f" => :mavericks
   end
 
-  depends_on "qt"
+  depends_on "qt5"
 
   def install
-    # On Mavericks we want to target libc++, this requires a unsupported/macx-clang-libc++ flag
-    if ENV.compiler == :clang && MacOS.version >= :mavericks
-      spec = "unsupported/macx-clang-libc++"
-    else
-      spec = "macx-g++"
-    end
-
     args = %W[
       -config release
-      -spec #{spec}
-      PREFIX=#{prefix}
-      LIBS+=-lz
     ]
 
-    system "qmake", "quazip.pro", *args
+    system "qmake", "quazip.pro", *args, "PREFIX=#{prefix}", "LIBS+=-lz"
     system "make", "install"
 
     cd "qztest" do
-      args = %W[-config release -spec #{spec}]
       system "qmake", *args
       system "make"
-      bin.install "qztest"
+      (pkgshare/"test").install "qztest"
     end
   end
 
   test do
-    system "#{bin}/qztest"
+    system "#{pkgshare}/test/qztest"
   end
 end
