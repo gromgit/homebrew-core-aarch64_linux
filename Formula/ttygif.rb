53170c359856abc096096e3e32526f92e5bb5ae1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-12 12:14:53 +0000

    ttygif: add 1.4.0 bottle.

--- Formula/ttygif.rb
+++ Formula/ttygif.rb
@@ -4,6 +4,13 @@ class Ttygif < Formula
   url "https://github.com/icholy/ttygif/archive/1.4.0.tar.gz"
   sha256 "6ca3dc5dcade2bdcf8000068ae991eac518204960c157634d92f87248c3cee2a"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f64ad6118dc421166dca05ac1ef2d146d545caddc49dcf1a17e06d3c9deee6fd" => :sierra
+    sha256 "c32e7ce8c456e02c9b945e9cca6252a071b68b9cda84651877481b54a76f6c1c" => :el_capitan
+    sha256 "6459139f3d5eb5a52aacf11d1774c0404a7947d0e87e674c2bba54724bc06bbf" => :yosemite
+  end
+
   depends_on "imagemagick"
   depends_on "ttyrec"
 
