672265a716d8581832cf12cc6099bf6ad4327bd2
JCount <JCount42@gmail.com>
2016-11-12 12:27:24 -0500

    bash 4.4.5
    Closes #6866.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/bash.rb
+++ Formula/bash.rb
@@ -1,16 +1,37 @@
 class Bash < Formula
   desc "Bourne-Again SHell, a UNIX command interpreter"
   homepage "https://www.gnu.org/software/bash/"
-  url "https://ftpmirror.gnu.org/bash/bash-4.4.tar.gz"
-  mirror "https://mirrors.ocf.berkeley.edu/gnu/bash/bash-4.4.tar.gz"
-  mirror "https://mirrors.kernel.org/gnu/bash/bash-4.4.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/bash/bash-4.4.tar.gz"
-  mirror "https://gnu.cu.be/bash/bash-4.4.tar.gz"
-  mirror "https://mirror.unicorncloud.org/gnu/bash/bash-4.4.tar.gz"
-  sha256 "d86b3392c1202e8ff5a423b302e6284db7f8f435ea9f39b5b1b20fd3ac36dfcb"
-  revision 1
 
   head "http://git.savannah.gnu.org/r/bash.git"
+
+  stable do
+    url "https://ftpmirror.gnu.org/bash/bash-4.4.tar.gz"
+    mirror "https://mirrors.ocf.berkeley.edu/gnu/bash/bash-4.4.tar.gz"
+    mirror "https://mirrors.kernel.org/gnu/bash/bash-4.4.tar.gz"
+    mirror "https://ftp.gnu.org/gnu/bash/bash-4.4.tar.gz"
+    mirror "https://gnu.cu.be/bash/bash-4.4.tar.gz"
+    mirror "https://mirror.unicorncloud.org/gnu/bash/bash-4.4.tar.gz"
+    sha256 "d86b3392c1202e8ff5a423b302e6284db7f8f435ea9f39b5b1b20fd3ac36dfcb"
+    version "4.4.5"
+
+    %w[
+      001 3e28d91531752df9a8cb167ad07cc542abaf944de9353fe8c6a535c9f1f17f0f
+      002 7020a0183e17a7233e665b979c78c184ea369cfaf3e8b4b11f5547ecb7c13c53
+      003 51df5a9192fdefe0ddca4bdf290932f74be03ffd0503a3d112e4199905e718b2
+      004 ad080a30a4ac6c1273373617f29628cc320a35c8cd06913894794293dc52c8b3
+      005 221e4b725b770ad0bb6924df3f8d04f89eeca4558f6e4c777dfa93e967090529
+    ].each_slice(2) do |p, checksum|
+      patch :p0 do
+        url "https://ftpmirror.gnu.org/bash/bash-4.4-patches/bash44-#{p}"
+        mirror "https://mirrors.ocf.berkeley.edu/gnu/bash/bash-4.4-patches/bash44-#{p}"
+        mirror "https://mirrors.kernel.org/gnu/bash/bash-4.4-patches/bash44-#{p}"
+        mirror "https://ftp.gnu.org/gnu/bash/bash-4.4-patches/bash44-#{p}"
+        mirror "https://gnu.cu.be/bash/bash-4.4-patches/bash44-#{p}"
+        mirror "https://mirror.unicorncloud.org/gnu/bash/bash-4.4-patches/bash44-#{p}"
+        sha256 checksum
+      end
+    end
+  end
 
   bottle do
     sha256 "a2f73335f36d0a35970b1ee2b41e4a6b939ffd9df2a3ee9e9ebb942765d10002" => :sierra
