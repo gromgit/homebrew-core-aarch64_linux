9a14fc325ac9f98cc9861591e3dbbedfe7d44c5d
ilovezfs <ilovezfs@icloud.com>
2016-11-12 04:49:15 -0800

    lbdb 0.42
    Closes #6856.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/lbdb.rb
+++ Formula/lbdb.rb
@@ -1,9 +1,9 @@
 class Lbdb < Formula
   desc "Little brother's database for the mutt mail reader"
   homepage "https://www.spinnaker.de/lbdb/"
-  url "https://www.spinnaker.de/debian/lbdb_0.41.tar.xz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/l/lbdb/lbdb_0.41.tar.xz"
-  sha256 "fc9261cdc361d95e33da08762cafe57f8b73ab2598f9073986f0f9e8ad64a813"
+  url "https://www.spinnaker.de/debian/lbdb_0.42.tar.xz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/l/lbdb/lbdb_0.42.tar.xz"
+  sha256 "f7b2ec9f145c77207085af35d1ff87dae3c721848d8de1e97aa328b2f85492fe"
 
   bottle do
     cellar :any_skip_relocation
