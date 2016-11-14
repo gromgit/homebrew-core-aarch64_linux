6f4f7b9b8dfdfe918554090dcdfe890d5db641c0
Yinghai Zhao <zyinghai@gmail.com>
2016-11-14 16:49:16 -0500

    cassandra 3.9
    Closes #6916.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/cassandra.rb
+++ Formula/cassandra.rb
@@ -1,9 +1,9 @@
 class Cassandra < Formula
   desc "Eventually consistent, distributed key-value store"
   homepage "https://cassandra.apache.org"
-  url "https://www.apache.org/dyn/closer.lua/cassandra/3.7/apache-cassandra-3.7-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/cassandra/3.7/apache-cassandra-3.7-bin.tar.gz"
-  sha256 "335f5344c4e6b98ec51324d821fa06e99101145ac6e83b5f6ede8c0ca5d15748"
+  url "https://www.apache.org/dyn/closer.lua/cassandra/3.9/apache-cassandra-3.9-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/cassandra/3.9/apache-cassandra-3.9-bin.tar.gz"
+  sha256 "27cf88a6bce1ee2fb1a1c936094b9200ad844414c2b5b1491ba4991fcc0fd693"
 
   bottle do
     cellar :any_skip_relocation
