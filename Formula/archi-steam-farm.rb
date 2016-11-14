e8896e49ab3c0f38cb14def391fdd272fb58a7c8
Oleg Kostyaev <okostyaev@gmail.com>
2016-11-14 09:55:14 +0300

    archi-steam-farm 2.1.6.7
    Closes #6901.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/archi-steam-farm.rb
+++ Formula/archi-steam-farm.rb
@@ -1,9 +1,9 @@
 class ArchiSteamFarm < Formula
   desc "ASF is a C# application that allows you to farm steam cards"
   homepage "https://github.com/JustArchi/ArchiSteamFarm"
-  url "https://github.com/JustArchi/ArchiSteamFarm/releases/download/2.1.6.3/ASF.zip"
-  version "2.1.6.3"
-  sha256 "62671db765e06f959b007a97f9bd9ad2cb5521c849aef81d8a48f5d2cee52b5a"
+  url "https://github.com/JustArchi/ArchiSteamFarm/releases/download/2.1.6.7/ASF.zip"
+  version "2.1.6.7"
+  sha256 "b09ccbe5df66bf7b771476f84ff760e2fb8f614cc3bb5b29c4bee6cdd3d7c6c4"
 
   bottle do
     cellar :any_skip_relocation
