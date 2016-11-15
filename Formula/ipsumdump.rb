b0a79683692047572eb177923a161ccdbaaa554d
Miguel Araújo <miguelarauj1o@gmail.com>
2016-11-15 05:48:10 -0300

    ipsumdump: audit fix (#6919)
    Description is too long. "name: desc" should be less than 80 characters.
    Length is calculated as ipsumdump + desc. (currently 91)

--- Formula/ipsumdump.rb
+++ Formula/ipsumdump.rb
@@ -1,5 +1,5 @@
 class Ipsumdump < Formula
-  desc "Summarizes TCP/IP dump files into a self-describing ASCII format easily readable"
+  desc "Summarizes TCP/IP dump files into a self-describing ASCII format"
   homepage "http://www.read.seas.harvard.edu/~kohler/ipsumdump/"
   url "http://www.read.seas.harvard.edu/~kohler/ipsumdump/ipsumdump-1.85.tar.gz"
   sha256 "98feca0f323605a022ba0cabcd765a8fcad1b308461360a5ae6c4c293740dc32"
