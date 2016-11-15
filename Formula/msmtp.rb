dafae5c6fecc8a0885eea834e7567658444854f7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-15 07:47:17 +0000

    msmtp: update 1.6.6 bottle.

--- Formula/msmtp.rb
+++ Formula/msmtp.rb
@@ -5,10 +5,9 @@ class Msmtp < Formula
   sha256 "da15db1f62bd0201fce5310adb89c86188be91cd745b7cb3b62b81a501e7fb5e"
 
   bottle do
-    sha256 "df5712948f700ef5a393c92cac1f1d23377054b1cea9bd80bff271c285d569c9" => :sierra
-    sha256 "587b8765d86f2b497bbb081ac4532d274e89d9e4ebd0a3917a798c67e921d712" => :el_capitan
-    sha256 "5605ee05b8922d12d228c271cb3e8df87c2294cccc083501f19ea5316e19e539" => :yosemite
-    sha256 "fc48e443dfe5c98f3ca5d40e66e05b6def31b06f17927d5743ea9906fc228775" => :mavericks
+    sha256 "d0b8a2a76d7ee8ed6beda0c383acd28d7a85d9d677c8d89a8a2e6b717055fe70" => :sierra
+    sha256 "115ce90fcc11a1fbda6bf4496200b50e89d4cccdb32f999cf6b3b749635f8e3e" => :el_capitan
+    sha256 "6f5227576bf8ac42fed7190c22f2e62b0fb2a3af59fa085e783426661c606758" => :yosemite
   end
 
   option "with-gsasl", "Use GNU SASL authentication library"
