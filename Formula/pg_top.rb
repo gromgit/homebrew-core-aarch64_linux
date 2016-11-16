046fed84f281c8a825f5b53e7d3d2c66c2b919c4
Mike McQuaid <mike@mikemcquaid.com>
2016-11-16 14:04:02 +0000

    pg_top: revision for postgres 9.6

--- Formula/pg_top.rb
+++ Formula/pg_top.rb
@@ -3,6 +3,7 @@ class PgTop < Formula
   homepage "http://ptop.projects.postgresql.org/"
   url "http://pgfoundry.org/frs/download.php/3504/pg_top-3.7.0.tar.bz2"
   sha256 "c48d726e8cd778712e712373a428086d95e2b29932e545ff2a948d043de5a6a2"
+  revision 1
 
   bottle do
     cellar :any
