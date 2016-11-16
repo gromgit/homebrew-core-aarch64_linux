2cdf464c1582f6b0427733a5f50eb6ff84c629db
Mike McQuaid <mike@mikemcquaid.com>
2016-11-16 14:04:02 +0000

    ephemeralpg: revision for postgres 9.6

--- Formula/ephemeralpg.rb
+++ Formula/ephemeralpg.rb
@@ -4,6 +4,7 @@ class Ephemeralpg < Formula
   url "http://ephemeralpg.org/code/ephemeralpg-1.9.tar.gz"
   mirror "https://bitbucket.org/eradman/ephemeralpg/get/ephemeralpg-1.9.tar.gz"
   sha256 "3caf06f2be5d9f206f3c1174cc0c44cc359357fc7d41da026f858e01ef192792"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
