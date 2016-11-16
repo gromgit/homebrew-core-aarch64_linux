4fbc28e6929c1bc34dbabd3b5fb0c201a9df0684
ilovezfs <ilovezfs@icloud.com>
2016-11-16 02:24:42 -0800

    cabal-install 1.24.0.1
    Closes #6971.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/cabal-install.rb
+++ Formula/cabal-install.rb
@@ -1,26 +1,9 @@
 class CabalInstall < Formula
   desc "Command-line interface for Cabal and Hackage"
   homepage "https://www.haskell.org/cabal/"
-  revision 2
+  url "https://hackage.haskell.org/package/cabal-install-1.24.0.1/cabal-install-1.24.0.1.tar.gz"
+  sha256 "09f5fd8a2aa7f9565800a711a133f8142d36d59b38f59da09c25045b83ee9ecc"
   head "https://github.com/haskell/cabal.git", :branch => "1.24"
-
-  stable do
-    if MacOS.version >= :sierra
-      url "https://github.com/haskell/cabal.git",
-          :branch => "1.24",
-          :revision => "51ff8b66468977dcccb81d19ac2d42ee27c9ccd1"
-      version "1.24.0.0"
-    else
-      url "https://hackage.haskell.org/package/cabal-install-1.24.0.0/cabal-install-1.24.0.0.tar.gz"
-      sha256 "d840ecfd0a95a96e956b57fb2f3e9c81d9fc160e1fd0ea350b0d37d169d9e87e"
-
-      # disables haddock for hackage-security
-      patch :p2 do
-        url "https://github.com/haskell/cabal/commit/9441fe.patch"
-        sha256 "5506d46507f38c72270efc4bb301a85799a7710804e033eaef7434668a012c5e"
-      end
-    end
-  end
 
   bottle do
     cellar :any_skip_relocation
@@ -34,7 +17,12 @@ class CabalInstall < Formula
   fails_with :clang if MacOS.version <= :lion # Same as ghc.rb
 
   def install
-    cd "cabal-install" if MacOS.version >= :sierra || build.head?
+    cd "cabal-install" if build.head?
+
+    # Fix "'Distribution.Simple.Configure' does not export 'computeEffectiveProfiling'"
+    # Equivalent to upstream PR from 16 Nov 2016 https://github.com/haskell/cabal/pull/4117
+    inreplace "bootstrap.sh", 'CABAL_VER_REGEXP="1\.24\.[0-9]"',
+                              'CABAL_VER_REGEXP="1\.24\.[1-9]"'
     system "sh", "bootstrap.sh", "--sandbox"
     bin.install ".cabal-sandbox/bin/cabal"
     bash_completion.install "bash-completion/cabal"
