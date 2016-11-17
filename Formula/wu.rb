0710c02fe7ae6cc3c175cf8aa49f88b8ec2e7b5d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-17 20:01:49 +0000

    wu: add 3.10.0 bottle.

--- Formula/wu.rb
+++ Formula/wu.rb
@@ -4,6 +4,13 @@ class Wu < Formula
   url "https://github.com/sramsay/wu/archive/3.10.0.tar.gz"
   sha256 "0934771cdd04cb4c38b45190dbfa97fa8b3aafaf53ea75152f070c97de850223"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5efc0cdf39ff0d7ce0cb70be665118459cea9f8523f9a01bf67a341e1330fb56" => :sierra
+    sha256 "bd297452f33d081c7720190bc9ef17f1c8b247de5a27c64e78fede64bd871050" => :el_capitan
+    sha256 "61b96421517b20e4d6090e37ffe3b52a93ee0934ecc06e8282fd06d0bb457883" => :yosemite
+  end
+
   depends_on "go" => :build
 
   def install
