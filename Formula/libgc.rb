b6832928ac1f053e334f49a46bf17e2698ac33f9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-17 15:08:02 +0000

    libgc: add 7.6.0 bottle.

--- Formula/libgc.rb
+++ Formula/libgc.rb
@@ -4,6 +4,13 @@ class Libgc < Formula
   url "http://www.hboehm.info/gc/gc_source/gc-7.6.0.tar.gz"
   sha256 "a14a28b1129be90e55cd6f71127ffc5594e1091d5d54131528c24cd0c03b7d90"
 
+  bottle do
+    cellar :any
+    sha256 "99877426181b0c152be5ff36c8dd499baf311dcdad3e9077213aba815937dec6" => :sierra
+    sha256 "2876d79f771da642949673c98a876f25effbefaf1432d91c9643b6934cedccaa" => :el_capitan
+    sha256 "7f1086d2ec668f06298f56a0a56c05c9258fd4e3a7d907846606d91c6a19aaea" => :yosemite
+  end
+
   resource "libatomic_ops" do
     url "http://www.hboehm.info/gc/gc_source/libatomic_ops-7.4.4.tar.gz"
     sha256 "bf210a600dd1becbf7936dd2914cf5f5d3356046904848dcfd27d0c8b12b6f8f"
