f71dd908683c7f89618861a772a171ec1f06b748
Kit Pearson <dev_kit@icloud.com>
2016-11-16 22:03:49 -0800

    n: Fix line order. (#6991)

--- Formula/n.rb
+++ Formula/n.rb
@@ -1,9 +1,9 @@
 class N < Formula
   desc "Node version management"
   homepage "https://github.com/tj/n"
-  head "https://github.com/tj/n.git"
   url "https://github.com/tj/n/archive/v2.1.0.tar.gz"
   sha256 "6fb70b39065a6d6ba1d12915906c06907a3e1afbb25c7653ad23a21217f51c76"
+  head "https://github.com/tj/n.git"
 
   bottle do
     cellar :any_skip_relocation
