2fada494a83320593e6f913f4c7a7896e50546af
Junliang Huang <JLHwung@users.noreply.github.com>
2016-11-17 16:59:05 +0800

    nginx 1.11.6 (devel) (#6984)

--- Formula/nginx.rb
+++ Formula/nginx.rb
@@ -14,8 +14,8 @@ class Nginx < Formula
   end
 
   devel do
-    url "https://nginx.org/download/nginx-1.11.5.tar.gz"
-    sha256 "223f8a2345a75f891098cf26ccdf208b293350388f51ce69083674c9432db6f6"
+    url "https://nginx.org/download/nginx-1.11.6.tar.gz"
+    sha256 "3153abbb518e2d9c032e1b127da3dc0028ad36cd4679e5f3be0b8afa33bc85bd"
   end
 
   # Before submitting more options to this formula please check they aren't
