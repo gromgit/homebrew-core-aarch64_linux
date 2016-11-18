e4143ac408bba0ed74b98a0a50a1f3c0dc6ddec5
Oliver Newman <oliver@whmsi.com>
2016-11-18 13:45:31 -0500

    shellcheck: fully scope test shell_output call

--- Formula/shellcheck.rb
+++ Formula/shellcheck.rb
@@ -33,6 +33,6 @@ class Shellcheck < Formula
         echo "$f"
       done
     EOS
-    assert_match "[SC2045]", shell_output("shellcheck -f gcc #{sh}", 1)
+    assert_match "[SC2045]", shell_output("#{bin}/shellcheck -f gcc #{sh}", 1)
   end
 end
