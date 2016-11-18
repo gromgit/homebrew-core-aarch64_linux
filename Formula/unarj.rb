78680f8b954bef36e810ba229c4e58e257c8f689
Oliver Newman <oliver@whmsi.com>
2016-11-18 13:49:14 -0500

    unarj: fully scope test system call

--- Formula/unarj.rb
+++ Formula/unarj.rb
@@ -27,7 +27,7 @@ class Unarj < Formula
   test do
     # Ensure that you can extract ARJ.EXE from a sample self-extracting file
     resource("testfile").stage do
-      system "unarj", "e", "ARJ286.EXE"
+      system "#{bin}/unarj", "e", "ARJ286.EXE"
       assert File.exist? "ARJ.EXE"
     end
   end
