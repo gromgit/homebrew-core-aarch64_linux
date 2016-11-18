4ae884bd04c623b01305072d65e1fca2cbe4a270
Alex Dunn <dunn.alex@gmail.com>
2016-11-17 20:37:05 -0800

    cpanminus 1.7042
    Closes #7031.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/cpanminus.rb
+++ Formula/cpanminus.rb
@@ -1,8 +1,8 @@
 class Cpanminus < Formula
   desc "Get, unpack, build, and install modules from CPAN"
   homepage "https://github.com/miyagawa/cpanminus"
-  url "https://github.com/miyagawa/cpanminus/archive/1.7040.tar.gz"
-  sha256 "48a747c040689445f7db0edd169da0abd709a37cfece3ceecff0816c09beab0e"
+  url "https://github.com/miyagawa/cpanminus/archive/1.7042.tar.gz"
+  sha256 "ec814986cfbb6dc5db9dd7b20de2b3e9b8da46c170854b110cd5d404b4e46974"
   head "https://github.com/miyagawa/cpanminus.git"
 
   bottle :unneeded
