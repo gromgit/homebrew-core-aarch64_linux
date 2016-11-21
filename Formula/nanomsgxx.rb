d8cd6b834671fcce6cd5863effeed27546947262
Giuseppe Roberti <gr@silenti.net>
2016-11-21 21:33:19 +0100

    nanomsgxx 0.2 (new formula)
    Closes #7133.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Formula/nanomsgxx.rb
@@ -0,0 +1,36 @@
+class Nanomsgxx < Formula
+  desc "Nanomsg binding for C++11"
+  homepage "https://achille-roussel.github.io/nanomsgxx/doc/nanomsgxx.7.html"
+  url "https://github.com/achille-roussel/nanomsgxx/archive/0.2.tar.gz"
+  sha256 "116ad531b512d60ea75ef21f55fd9d31c00b172775548958e5e7d4edaeeedbaa"
+
+  depends_on "pkg-config" => :build
+  depends_on :python => :build if MacOS.version <= :snow_leopard
+
+  depends_on "nanomsg"
+
+  def install
+    args = %W[
+      --static
+      --shared
+      --prefix=#{prefix}
+    ]
+
+    system "python", "./waf", "configure", *args
+    system "python", "./waf", "build"
+    system "python", "./waf", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <iostream>
+      int main(int argc, char **argv) {
+        std::cout << "Hello Nanomsgxx!" << std::endl;
+      }
+    EOS
+
+    system ENV.cxx, "-std=c++11", "-L#{lib}", "-lnnxx", "test.cpp"
+
+    assert_equal "Hello Nanomsgxx!\n", shell_output("#{testpath}/a.out")
+  end
+end
