4028918b69a1a9a82fff75b45ec4ec70eb90dc94
Oleg Kostyaev <okostyaev@gmail.com>
2016-11-21 13:29:14 +0300

    archi-steam-farm: audit passed
    Closes #7117.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/archi-steam-farm.rb
+++ Formula/archi-steam-farm.rb
@@ -1,8 +1,8 @@
 class ArchiSteamFarm < Formula
   desc "ASF is a C# application that allows you to farm steam cards"
   homepage "https://github.com/JustArchi/ArchiSteamFarm"
+  url "https://github.com/JustArchi/ArchiSteamFarm/releases/download/2.1.6.8/ASF.zip"
   version "2.1.6.8"
-  url "https://github.com/JustArchi/ArchiSteamFarm/releases/download/#{version}/ASF.zip"
   sha256 "c0e7f4efdbaa51acb3b64ad0c41383de752dda26db5ad990761ff088d80544c7"
 
   bottle do
