11f2841c065e32d7519818418d37604f40cf5d92
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-21 20:39:01 +0000

    nanomsgxx: add 0.2 bottle.

--- Formula/nanomsgxx.rb
+++ Formula/nanomsgxx.rb
@@ -4,6 +4,13 @@ class Nanomsgxx < Formula
   url "https://github.com/achille-roussel/nanomsgxx/archive/0.2.tar.gz"
   sha256 "116ad531b512d60ea75ef21f55fd9d31c00b172775548958e5e7d4edaeeedbaa"
 
+  bottle do
+    cellar :any
+    sha256 "d61b7a3a91b7568325a5b94382ac978a60fb4c190ef2627db9d3a563cc3cbc15" => :sierra
+    sha256 "13abe2463ffcf219b5bcc3ae0ef48e0d99e2d9810ebb9e28c2344e9f4ef4cd6a" => :el_capitan
+    sha256 "5d1ef8eed698ca5e10d28fa5609033f546f8d831130b58d17c3369098482230c" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on :python => :build if MacOS.version <= :snow_leopard
 
