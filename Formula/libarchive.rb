05cb5e903f8547d33ded9b08e38ec6e2a69de13d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-22 16:27:05 +0000

    libarchive: update 3.2.2 bottle.

--- Formula/libarchive.rb
+++ Formula/libarchive.rb
@@ -6,10 +6,9 @@ class Libarchive < Formula
 
   bottle do
     cellar :any
-    sha256 "1196858d90db42d9171175d616ed3c58ec2d4e4895d684f1c163a7ae9eb19a19" => :sierra
-    sha256 "6ef9838a7fda2a6ffacf250a29a1ce5fb9a71072f6c02b134c464096d6514491" => :el_capitan
-    sha256 "f79d996c549f190b3225dbef988824afdec92cdcd76e39faae3ac2e67731d34f" => :yosemite
-    sha256 "a702d412a72902bfec4c7093673d9df7e7a6df819a73aa1ccec5f074685d30eb" => :mavericks
+    sha256 "3903b25c80d2654fcaa6133064622a911dcd87aa8c14414fb87e32d412bbfd36" => :sierra
+    sha256 "9f9daba158d0249cc6208421fde53f9c66c6c4c18992f8bf5eb5a932feb146b1" => :el_capitan
+    sha256 "3f9b96060ed43436e38a07cf09772deed133da1ce886e3e71043dd41daa171a5" => :yosemite
   end
 
   keg_only :provided_by_osx
