dddf7339e7a7690fd0bf07880ebb99b3be0e42ba
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-22 01:01:44 +0000

    tippecanoe: update 1.15.0 bottle.

--- Formula/tippecanoe.rb
+++ Formula/tippecanoe.rb
@@ -6,9 +6,9 @@ class Tippecanoe < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a38904c410c5c5ca4ac40f8ab9a45d2eb2ef75f189324dd2debc08d07d25d98d" => :sierra
-    sha256 "480c581f08212a7305a3295c1152cf088de46ea9bebebfdfee0533d82053fe06" => :el_capitan
-    sha256 "60b4a22639cac708c29b7ff3beb71c7b502f9adc32e2718481ba912e245efab7" => :yosemite
+    sha256 "08a814657c211e468bf4382f772c6039b7f0831aad2e9a05b20cc6c8f8a1c6c6" => :sierra
+    sha256 "87be8ca5c514fc8e74e7fae040b058f8e2d45fdf1a3289eadb90a178db23a3c3" => :el_capitan
+    sha256 "710199ea49fd9eacfd59b19dd4799a8b837e36da3eb007810a721c0a5937ec17" => :yosemite
   end
 
   def install
