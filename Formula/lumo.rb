d88fc7b4ed8e7e57da09aadcfff5794ad1897d26
Greg Leppert <greg@leppert.me>
2016-11-22 18:09:03 -0500

    lumo 1.0.0 (new formula)
    Closes #6885.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

---
+++ Formula/lumo.rb
@@ -0,0 +1,22 @@
+class Lumo < Formula
+  desc "Fast, cross-platform, standalone ClojureScript REPL"
+  homepage "https://github.com/anmonteiro/lumo"
+  url "https://github.com/anmonteiro/lumo/archive/1.0.0.tar.gz"
+  sha256 "347ec7a57f2b85d7d23489f5d1f7e0ef9705d652d14fbbf83e0058a73cd44387"
+  head "https://github.com/anmonteiro/lumo.git"
+
+  depends_on "boot-clj" => :build
+  depends_on "node" => :build
+  depends_on "yarn" => :build
+
+  def install
+    ENV["BOOT_HOME"] = "#{buildpath}/.boot"
+    ENV["BOOT_LOCAL_REPO"] = "#{buildpath}/.m2/repository"
+    system "boot", "release"
+    bin.install "build/lumo"
+  end
+
+  test do
+    assert_equal "0", shell_output("#{bin}/lumo -e '(- 1 1)'").chomp
+  end
+end
