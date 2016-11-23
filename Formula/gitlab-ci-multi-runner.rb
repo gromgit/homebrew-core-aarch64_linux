9da01f1dfe9d59058ff7028387046053e901d729
ilovezfs <ilovezfs@icloud.com>
2016-11-22 18:57:58 -0800

    gitlab-ci-multi-runner 1.8.0
    Closes #7173.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gitlab-ci-multi-runner.rb
+++ Formula/gitlab-ci-multi-runner.rb
@@ -4,8 +4,8 @@ class GitlabCiMultiRunner < Formula
   desc "The official GitLab CI runner written in Go"
   homepage "https://gitlab.com/gitlab-org/gitlab-ci-multi-runner"
   url "https://gitlab.com/gitlab-org/gitlab-ci-multi-runner.git",
-      :tag => "v1.7.1",
-      :revision => "f896af7df9cf8a70b6413c566a4ab03055735891"
+      :tag => "v1.8.0",
+      :revision => "734bc5d95643a55f9ca298312af1bcd363731a68"
   head "https://gitlab.com/gitlab-org/gitlab-ci-multi-runner.git"
 
   bottle do
@@ -24,10 +24,10 @@ class GitlabCiMultiRunner < Formula
   end
 
   resource "prebuilt-x86_64.tar.xz" do
-    url "https://gitlab-ci-multi-runner-downloads.s3.amazonaws.com/v1.7.1/docker/prebuilt-x86_64.tar.xz",
+    url "https://gitlab-ci-multi-runner-downloads.s3.amazonaws.com/v1.8.0/docker/prebuilt-x86_64.tar.xz",
         :using => :nounzip
-    version "1.7.1"
-    sha256 "28ea4c9bf49f1b50659541c8c09fbcccf814211230be235e52babcee61468dbd"
+    version "1.8.0"
+    sha256 "9588eb55ade4e323bd5bfeffd3bfc119659d0c2075e18c5bb003a4d79b834839"
   end
 
   def install
