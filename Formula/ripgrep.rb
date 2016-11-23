f08dee559c762d8af073b7a498ec59ceb5aa48a2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-23 19:03:15 +0000

    ripgrep: update 0.3.1 bottle.

--- Formula/ripgrep.rb
+++ Formula/ripgrep.rb
@@ -7,9 +7,10 @@ class Ripgrep < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c01b699c06411cfaf5abf506950b9307adc1479e7943980694b8ff02c5e67b63" => :sierra
-    sha256 "1c8a0025a68e8879751fdaca2b2f70ce1e538b62c4ed2f5c0fb0edad295ee01f" => :el_capitan
-    sha256 "70a123b6254a752f8ae87420bd07384a0cde8340d866a354e83af4c1b4dcd127" => :yosemite
+    rebuild 1
+    sha256 "80dcd354b2a5c1a905c1b5b2bd6894b195d535d94c3cbe08d1ec8a4c3f77e1e5" => :sierra
+    sha256 "d53f7547fcfd68d434133ee533143e9da96ad34c274e5f14f3489f013b10f75a" => :el_capitan
+    sha256 "3a88c43c13576a561d21c8257b8a7b696620bc844f2e10ba0aaf1ae7e0092ee5" => :yosemite
   end
 
   depends_on "rust" => :build
