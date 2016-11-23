ad3a1c17551d7e20da33a30ebedf6e47eb407bcb
ilovezfs <ilovezfs@icloud.com>
2016-11-22 19:05:29 -0800

    testssl 2.8rc3
    Closes #7174.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/testssl.rb
+++ Formula/testssl.rb
@@ -1,8 +1,9 @@
 class Testssl < Formula
   desc "Tool which checks for the support of TLS/SSL ciphers and flaws"
   homepage "https://testssl.sh/"
-  url "https://github.com/drwetter/testssl.sh/archive/v2.6.tar.gz"
-  sha256 "286b3285f096a5d249de1507eee88b14848514696bc5bbc4faceffa46b563ebd"
+  url "https://github.com/drwetter/testssl.sh/archive/v2.8rc3.tar.gz"
+  version "2.8rc3"
+  sha256 "e36a60acc0aa09191ff2f86762cd07925630df82498d48d81d86e3b7402a3312"
 
   head "https://github.com/drwetter/testssl.sh.git"
 
