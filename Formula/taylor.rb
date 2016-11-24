ef9e02a59a8ed92cedee643d5cd9e63c25c88c0f
Andrei Raifura <andrei.raifura@yopeso.com>
2016-11-24 17:06:46 +0200

    taylor 0.2.1
    Closes #7216.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/taylor.rb
+++ Formula/taylor.rb
@@ -1,8 +1,8 @@
 class Taylor < Formula
   desc "Measure Swift code metrics and get reports in Xcode and Jenkins."
   homepage "https://github.com/yopeso/Taylor/"
-  url "https://github.com/yopeso/Taylor/archive/0.1.3.tar.gz"
-  sha256 "eb763ad683535df32270cf96e3489c1d9a593516ab9e0224ca41e10946f33382"
+  url "https://github.com/yopeso/Taylor/archive/0.2.1.tar.gz"
+  sha256 "ecbc7701120910c18384e1a255b65e5cb0d495f0f02188d76279e3b70850c362"
   head "https://github.com/yopeso/Taylor.git"
 
   bottle do
@@ -10,7 +10,7 @@ class Taylor < Formula
     sha256 "892f7e3145820f4ac482bef208b7a96435dfae38efe7bb2e1723b27a244c19cb" => :el_capitan_or_later
   end
 
-  depends_on :xcode => ["7.3"]
+  depends_on :xcode => ["8.1"]
 
   def install
     system "make", "install", "PREFIX=#{prefix}", "MAKE_SYMLINKS=no"
