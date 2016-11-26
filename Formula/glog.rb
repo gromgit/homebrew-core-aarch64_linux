62dc00b4cf4cf3c5eca8f9ff02ba8d0d53c27b79
ilovezfs <ilovezfs@icloud.com>
2016-11-26 01:50:06 -0800

    glog: revision for gflags

--- Formula/glog.rb
+++ Formula/glog.rb
@@ -3,6 +3,7 @@ class Glog < Formula
   homepage "https://github.com/google/glog"
   url "https://github.com/google/glog/archive/v0.3.4.tar.gz"
   sha256 "ce99d58dce74458f7656a68935d7a0c048fa7b4626566a71b7f4e545920ceb10"
+  revision 1
 
   bottle do
     cellar :any
