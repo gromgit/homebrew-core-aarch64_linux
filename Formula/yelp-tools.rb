f28f8e54cd7f2bf956804e7ad6a71cd651ba612f
A. Jesse Jiryu Davis <jesse@mongodb.com>
2016-11-26 11:02:54 -0500

    yelp-tools: convert yelp-xsl to a resource.
    yelp-tools is the only package that depends on yelp-xsl, and both
    packages must have the same version. Better to make yelp-xsl a resource
    of yelp-tools instead of a distinct package that can get out of sync.
    
    Closes #7243
    Closes #7260.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/yelp-tools.rb
+++ Formula/yelp-tools.rb
@@ -12,20 +12,51 @@ class YelpTools < Formula
     sha256 "66721baa2d6c6978cceac29f35e8112e96804b6ae0197d89175867f17caca2cd" => :mavericks
   end
 
+  depends_on "gettext" => :build
+  depends_on "gtk+3"
+  depends_on "intltool" => :build
   depends_on "itstool" => :build
-  depends_on "pkg-config" => :build
-  depends_on "libxslt" => :build
   depends_on "libxml2" => :build
-  depends_on "yelp-xsl"
+  depends_on "libxslt" => :build
+  depends_on "pkg-config" => :build
+
+  yelp_tools_version = version
+
+  resource "yelp-xsl" do
+    url "https://download.gnome.org/sources/yelp-xsl/3.18/yelp-xsl-#{yelp_tools_version}.tar.xz"
+    sha256 "893620857b72b3b43ee3b462281240b7ca4d80292f469552827f0597bf60d2b2"
+  end
 
   def install
+    resource("yelp-xsl").stage do
+      system "./configure", "--disable-debug",
+                            "--disable-dependency-tracking",
+                            "--disable-silent-rules",
+                            "--prefix=#{prefix}"
+      system "make", "install"
+      ENV.append_path "PKG_CONFIG_PATH", "#{share}/pkgconfig"
+    end
+
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  def post_install
+    system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache",
+           "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
   end
 
   test do
     system "#{bin}/yelp-new", "task", "ducksinarow"
     system "#{bin}/yelp-build", "html", "ducksinarow.page"
     system "#{bin}/yelp-check", "validate", "ducksinarow.page"
+    [
+      prefix/"share/yelp-xsl/icons/hicolor/24x24/status/yelp-note-warning.png",
+      prefix/"share/yelp-xsl/js/jquery.syntax.brush.smalltalk.js",
+      prefix/"share/yelp-xsl/xslt/mallard/html/mal2html-links.xsl",
+      share/"pkgconfig/yelp-xsl.pc",
+    ].each do |filename|
+      assert filename.exist?, "#{filename} doesn't exist"
+    end
   end
 end
