3a440d926667c18ab0d96991e44c8eb2ee4f111d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-26 15:44:20 +0000

    sfk: update 1.8.0 bottle.

--- Formula/sfk.rb
+++ Formula/sfk.rb
@@ -6,9 +6,9 @@ class Sfk < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a6297dac82e1ed14dd9b2d37a90384113bcd526b93053dd03db0f5154063245f" => :el_capitan
-    sha256 "d5be7f90c20799b81c6de1ec6ab2ebdbf8cdad63a551603061a21afde43aab56" => :yosemite
-    sha256 "3c8d89606a20039aa02a45614c877dc6d0d6d2365e13cb4e143f7bc99187ba16" => :mavericks
+    sha256 "485b91b860e40819398fd6cc8058bcc0b13f0280b365935c804bdcbaaa3b1791" => :sierra
+    sha256 "9c9f7a323e1406b2ea7190d4f9dba91563e491f86dc4097c444cee2d6e822f7f" => :el_capitan
+    sha256 "5876fa19b37c2bd4062837ee17e66eb516a6fc75b2fba476d5bd334932859c04" => :yosemite
   end
 
   def install
