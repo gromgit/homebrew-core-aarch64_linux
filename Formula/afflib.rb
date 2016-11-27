833a19f71b1480782696b1b1a68deeb4bb3fc524
ilovezfs <ilovezfs@icloud.com>
2016-11-27 00:06:37 -0800

    afflib 3.7.13
    always pass --enable-python and remove the with-python option
    no longer need to add -lcurl and -lexpat to LDFLAGS
    
    Closes #7267.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/afflib.rb
+++ Formula/afflib.rb
@@ -1,8 +1,8 @@
 class Afflib < Formula
   desc "Advanced Forensic Format"
   homepage "https://github.com/sshock/AFFLIBv3"
-  url "https://github.com/sshock/AFFLIBv3/archive/v3.7.11.tar.gz"
-  sha256 "931a6f3399c6397a4ac2d84664d80d7ae3e81de55f98e781ac43319cabfedeb7"
+  url "https://github.com/sshock/AFFLIBv3/archive/v3.7.13.tar.gz"
+  sha256 "4356bb576eaa0d51651ec9754e8c3948f56e439c6c8b98ec6c23d5bebaae86bc"
 
   bottle do
     cellar :any
@@ -11,27 +11,20 @@ class Afflib < Formula
     sha256 "fcde02aa5b1176477451d688bbac7fd3a0aa99e09657681a16ac37e9b2fa664e" => :yosemite
   end
 
-  option "with-python", "Build with python support"
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "openssl"
+  depends_on :python if MacOS.version <= :snow_leopard
   depends_on :osxfuse => :optional
-  depends_on :python if build.with?("python") && MacOS.version <= :snow_leopard
 
   def install
-    ENV.prepend "LDFLAGS", "-L/usr/lib -lcurl -lexpat"
+    inreplace "m4/acinclude.m4",
+      "PYTHON_LDFLAGS=\"-L$ac_python_libdir -lpython$ac_python_version\"",
+      "PYTHON_LDFLAGS=\"-undefined dynamic_lookup\""
 
-    args = ["--enable-s3"]
-
-    if build.with? "python"
-      inreplace "m4/acinclude.m4",
-        "PYTHON_LDFLAGS=\"-L$ac_python_libdir -lpython$ac_python_version\"",
-        "PYTHON_LDFLAGS=\"-undefined dynamic_lookup\""
-      args << "--enable-python"
-    end
+    args = ["--enable-s3", "--enable-python"]
 
     if build.with? "osxfuse"
       ENV.append "CPPFLAGS", "-I/usr/local/include/osxfuse"
