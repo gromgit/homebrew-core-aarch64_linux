0de89a673078d1e3b04191c92ac7f73872798276
ilovezfs <ilovezfs@icloud.com>
2016-11-27 12:33:20 -0800

    ufraw: revision for jasper
    Closes #7274.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/ufraw.rb
+++ Formula/ufraw.rb
@@ -3,6 +3,7 @@ class Ufraw < Formula
   homepage "http://ufraw.sourceforge.net"
   url "https://downloads.sourceforge.net/project/ufraw/ufraw/ufraw-0.22/ufraw-0.22.tar.gz"
   sha256 "f7abd28ce587db2a74b4c54149bd8a2523a7ddc09bedf4f923246ff0ae09a25e"
+  revision 1
 
   bottle do
     rebuild 1
