a3f37a49cba59e85d3d887fc2e8d9fbba67d3456
ilovezfs <ilovezfs@icloud.com>
2016-11-27 14:19:27 -0800

    hunspell 1.5.2
    Closes #7295.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/hunspell.rb
+++ Formula/hunspell.rb
@@ -1,9 +1,8 @@
 class Hunspell < Formula
   desc "Spell checker and morphological analyzer"
   homepage "https://hunspell.github.io"
-  url "https://github.com/hunspell/hunspell/archive/v1.4.1.tar.gz"
-  sha256 "c4476aff0ced52eec334eae1e8d3fdaaebdd90f5ecd0b57cf2a92a6fd220d1bb"
-  revision 1
+  url "https://github.com/hunspell/hunspell/archive/v1.5.2.tar.gz"
+  sha256 "6d649981f61ab0db81027e165a98902201a68f12f9abe0d42acd57948d67f3d7"
 
   bottle do
     sha256 "d4cc7d577e89e8e67312821828978a5bfa812188f674a67b29cfbc7dc7695dac" => :sierra
@@ -15,18 +14,7 @@ class Hunspell < Formula
 
   conflicts_with "freeling", :because => "both install 'analyze' binary"
 
-  # hunspell does not prepend $HOME to all USEROODIRs
-  # https://github.com/hunspell/hunspell/issues/32
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/684440d/hunspell/prepend_user_home_to_useroodirs.diff"
-    sha256 "456186c9e569c51065e7df2a521e325d536ba4627d000ab824f7e97c1e4bc288"
-  end
-
   def install
-    ENV.deparallelize
-
-    # The following line can be removed on release of next stable version
-    inreplace "configure", "1.4.0", "1.4.1"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-ui",
