c1356cc82fa4ef9524100ea5a9164c4af546bccd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-27 03:33:32 +0000

    google-java-format: add 1.1 bottle.

--- Formula/google-java-format.rb
+++ Formula/google-java-format.rb
@@ -4,6 +4,13 @@ class GoogleJavaFormat < Formula
   url "https://github.com/google/google-java-format/archive/google-java-format-1.1.tar.gz"
   sha256 "14907ce82cd405f6bc11dee9e5b609af17b4d3a062d728fde2cd1a841611b809"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "cd02ff7d091b8a70fd5a70b708f0a2782f023ce61fc2b9f971c2f1af75647e19" => :sierra
+    sha256 "112ab893b27010c93d31a6d2328fd3b0179c962bf1d84420634d878dd6453dff" => :el_capitan
+    sha256 "7fbf50feec06fea5760971ce8202f9704effdb500ee6728ae3eb65a18523cf4e" => :yosemite
+  end
+
   depends_on "maven" => :build
 
   def install
