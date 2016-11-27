66a233cd7ab7331cfbf477454d631f98d2bbb4af
ilovezfs <ilovezfs@icloud.com>
2016-11-27 03:46:25 -0800

    gibo 1.0.5
    Closes #7275.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/gibo.rb
+++ Formula/gibo.rb
@@ -1,8 +1,8 @@
 class Gibo < Formula
   desc "Access GitHub's .gitignore boilerplates"
   homepage "https://github.com/simonwhitaker/gibo"
-  url "https://github.com/simonwhitaker/gibo/archive/1.0.4.tar.gz"
-  sha256 "243e4c2710b92ae722a74470305ea5d2c326c396330dcb2ccbce33bf690e4dfc"
+  url "https://github.com/simonwhitaker/gibo/archive/1.0.5.tar.gz"
+  sha256 "772860ce11142354e48292691d0fc392618df7bb8336a6ea3d926eaba4615189"
 
   bottle :unneeded
 
