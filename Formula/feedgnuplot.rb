a097a2bc85f43f1445f22e22e83aae638038b793
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-28 16:49:00 +0000

    feedgnuplot: update 1.40 bottle.

--- Formula/feedgnuplot.rb
+++ Formula/feedgnuplot.rb
@@ -6,9 +6,9 @@ class Feedgnuplot < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3ca77c9ff696f75b5c4837c2116e61f73f2e0d5b5426b88c67e8763e1fce4a10" => :sierra
-    sha256 "6966556a04cba4ed5839675595faeceb6265034d544b7cb5123fdd676635d4ac" => :el_capitan
-    sha256 "05b365537624871e498c61c8117ab0954dba3437892e593bbde85e1b4aecf9e5" => :yosemite
+    sha256 "4437f2afc8a15746f351149db1877e0a0c46aa3b056a220c255d4bee5ca5ba62" => :sierra
+    sha256 "a4b976a5929cdae3763061a2bacd4c36f1db2aabc10ff07eaa2032e042a9f8fa" => :el_capitan
+    sha256 "dff635e056ebb7e50dedc9aebd5bc0240f64c7ed5daeab3afe4edced3aa717ec" => :yosemite
   end
 
   depends_on "gnuplot"
