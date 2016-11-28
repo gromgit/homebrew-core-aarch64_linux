4bb3c3b99ba13da32599a803a104caa791ba3943
ilovezfs <ilovezfs@icloud.com>
2016-11-28 05:49:08 -0800

    beansdb 0.7.1.4
    Closes #7316.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/beansdb.rb
+++ Formula/beansdb.rb
@@ -1,8 +1,9 @@
 class Beansdb < Formula
   desc "Yet another distributed key-value storage system"
   homepage "https://github.com/douban/beansdb"
-  url "https://github.com/douban/beansdb/archive/v0.6.tar.gz"
-  sha256 "b24512862f948d5191f5c43316a41f632bc386f43dcbb69b03ffffe95122a33e"
+  url "https://github.com/douban/beansdb/archive/v0.7.1.4.tar.gz"
+  sha256 "c89f267484dd47bab272b985ba0a9b9196ca63a9201fdf86963b8ed04f52ccdb"
+  head "https://github.com/douban/beansdb.git"
 
   bottle do
     cellar :any_skip_relocation
@@ -12,15 +13,11 @@ class Beansdb < Formula
     sha256 "a6f26d8e3aafc48c6be59c466e4a868ccaa1694b0a8b94220f145d2cf4359fee" => :mavericks
   end
 
-  head do
-    url "https://github.com/douban/beansdb.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-  end
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
 
   def install
-    ENV.append "CFLAGS", "-std=gnu89"
-    system "./autogen.sh" if build.head?
+    system "./autogen.sh"
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
