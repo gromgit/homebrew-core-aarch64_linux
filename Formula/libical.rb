6631401905ec69efd1711c5988da97d1f1b1d933
ilovezfs <ilovezfs@icloud.com>
2016-11-28 06:04:11 -0800

    libical 2.0.0

--- Formula/libical.rb
+++ Formula/libical.rb
@@ -1,8 +1,8 @@
 class Libical < Formula
   desc "Implementation of iCalendar protocols and data formats"
   homepage "https://libical.github.io/libical/"
-  url "https://github.com/libical/libical/releases/download/v1.0.1/libical-1.0.1.tar.gz"
-  sha256 "089ce3c42d97fbd7a5d4b3c70adbdd82115dd306349c1f5c46a8fb3f8c949592"
+  url "https://github.com/libical/libical/releases/download/v2.0.0/libical-2.0.0.tar.gz"
+  sha256 "654c11f759c19237be39f6ad401d917e5a05f36f1736385ed958e60cf21456da"
 
   bottle do
     sha256 "89a2b365a23f3d99a24f62c45d060daabc0aa06c07f613bf47997db2a5caeca5" => :sierra
@@ -12,11 +12,15 @@ class Libical < Formula
     sha256 "014c0160a5bc9030409e6459799e4b9ae3474ea86d4ca1557b9a3d5d89e31232" => :mountain_lion
   end
 
-  depends_on "cmake" => :build
-
   option :universal
 
+  depends_on "cmake" => :build
+
   def install
+    # Fix libical-glib build failure due to undefined symbol
+    # Upstream issue https://github.com/libical/libical/issues/225
+    inreplace "src/libical/icallangbind.h", "*callangbind_quote_as_ical_r(",
+                                            "*icallangbind_quote_as_ical_r("
     args = std_cmake_args
     if build.universal?
       ENV.universal_binary
