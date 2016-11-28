e78a08f40a2973b7fa89c4e4c41d77e908bd575b
ilovezfs <ilovezfs@icloud.com>
2016-11-28 02:08:32 +0000

    javarepl 347 (#7298)

--- Formula/javarepl.rb
+++ Formula/javarepl.rb
@@ -1,8 +1,8 @@
 class Javarepl < Formula
   desc "Read Eval Print Loop (REPL) for Java"
   homepage "https://github.com/albertlatacz/java-repl"
-  url "https://s3.amazonaws.com/albertlatacz.published/repo/javarepl/javarepl/339/javarepl-339.jar"
-  sha256 "c858c1a714990807b0c0595eb1658225fecee63bb19b697459c657ce49a7b59f"
+  url "https://s3.amazonaws.com/albertlatacz.published/repo/javarepl/javarepl/347/javarepl-347.jar"
+  sha256 "16c219613e95bd728e4b7433888bf506f8f20cbcae6b73ae8becb54841252cf2"
 
   bottle :unneeded
 
