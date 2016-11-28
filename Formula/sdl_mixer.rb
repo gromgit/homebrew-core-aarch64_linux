9f00cc37b6304189e26673b39973acdf3987c917
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-28 10:07:01 +0000

    sdl_mixer: update 1.2.12_1 bottle.

--- Formula/sdl_mixer.rb
+++ Formula/sdl_mixer.rb
@@ -7,12 +7,9 @@ class SdlMixer < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "b038d52d3b439e8a72ff8edfb769a48bd65549e849eb536e2f62794dc14a43c9" => :sierra
-    sha256 "51556b8d0036664453d71bd52cce26bf25a246b15f88f887d08e658f9b62d2e5" => :el_capitan
-    sha256 "80e6127e15c1613bad625841cad39c1df3935d332b6c38a8ded1f022e38b3850" => :yosemite
-    sha256 "5fc9bb3ce927e706e413b9171a7a312f0d22e663f8cd362012fc8cdb66c20c25" => :mavericks
-    sha256 "bb94a051a4426e1014c6a5005a225ad87722fc57f3f44effa8bc602b67c9cf8e" => :mountain_lion
+    sha256 "568e373cf4e876779e63e36258be20c0e6acb6df81a81653ff09cb9b09deca99" => :sierra
+    sha256 "e2398010d90664403b7f9c306b46876deec07e6d970371a47c7f7484ffa201f8" => :el_capitan
+    sha256 "559377bb70595dc716d1f0c703e986ea4bc30666085812756230b00194b97d87" => :yosemite
   end
 
   option :universal
