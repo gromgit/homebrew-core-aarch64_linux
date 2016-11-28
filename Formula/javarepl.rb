1942a46f421f0b0f7f4f519ab9ee4175b53ee5d1
ilovezfs <ilovezfs@icloud.com>
2016-11-28 05:44:49 -0800

    javarepl 348
    Closes #7312.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/javarepl.rb
+++ Formula/javarepl.rb
@@ -1,8 +1,8 @@
 class Javarepl < Formula
   desc "Read Eval Print Loop (REPL) for Java"
   homepage "https://github.com/albertlatacz/java-repl"
-  url "https://s3.amazonaws.com/albertlatacz.published/repo/javarepl/javarepl/347/javarepl-347.jar"
-  sha256 "16c219613e95bd728e4b7433888bf506f8f20cbcae6b73ae8becb54841252cf2"
+  url "https://s3.amazonaws.com/albertlatacz.published/repo/javarepl/javarepl/348/javarepl-348.jar"
+  sha256 "23bebc905ca1ae0d83183595f0fb400934b6256f90b5563ad742dd74c5ec8ae9"
 
   bottle :unneeded
 
