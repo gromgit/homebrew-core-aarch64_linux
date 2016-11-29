b4fe5218095b4b3bf5ac450d099b057cfbbedc21
ilovezfs <ilovezfs@icloud.com>
2016-11-29 06:22:08 -0800

    terragrunt 0.3.0
    use glide vendoring
    
    Closes #7349.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -1,10 +1,8 @@
-require "language/go"
-
 class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state."
   homepage "https://github.com/gruntwork-io/terragrunt"
-  url "https://github.com/gruntwork-io/terragrunt/archive/v0.1.3.tar.gz"
-  sha256 "e7a10b50e8b7fadfc16e0951acb23e9f77b5e7365a8b7a65480fb86997489e49"
+  url "https://github.com/gruntwork-io/terragrunt/archive/v0.3.0.tar.gz"
+  sha256 "9369b133a3ce1d834a06d9aac45a611fb1290b2e1d893ff3d6c752332b5f5835"
   head "https://github.com/gruntwork-io/terragrunt.git"
 
   bottle do
@@ -14,39 +12,16 @@ class Terragrunt < Formula
     sha256 "562d85fbb52151ad7be4a804efd0f48f64d2b305fa74768f7c22190e21ca9346" => :yosemite
   end
 
+  depends_on "glide" => :build
   depends_on "go" => :build
   depends_on "terraform"
 
-  go_resource "github.com/aws/aws-sdk-go" do
-    url "https://github.com/aws/aws-sdk-go.git",
-      :revision => "1b2abe886743dc2bcc78472bfd30a15dc0a61fb8"
-  end
-
-  go_resource "github.com/go-errors/errors" do
-    url "https://github.com/go-errors/errors.git",
-      :revision => "a41850380601eeb43f4350f7d17c6bbd8944aaf8"
-  end
-
-  go_resource "github.com/hashicorp/hcl" do
-    url "https://github.com/hashicorp/hcl.git",
-      :revision => "6f5bfed9a0a22222fbe4e731ae3481730ba41e93"
-  end
-
-  go_resource "github.com/stretchr/testify" do
-    url "https://github.com/stretchr/testify.git",
-      :revision => "976c720a22c8eb4eb6a0b4348ad85ad12491a506"
-  end
-
-  go_resource "github.com/urfave/cli" do
-    url "https://github.com/urfave/cli.git",
-      :revision => "55f715e28c46073d0e217e2ce8eb46b0b45e3db6"
-  end
-
   def install
+    ENV["GOPATH"] = buildpath
+    ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
     mkdir_p buildpath/"src/github.com/gruntwork-io/"
     ln_s buildpath, buildpath/"src/github.com/gruntwork-io/terragrunt"
-    ENV["GOPATH"] = buildpath
-    Language::Go.stage_deps resources, buildpath/"src"
+    system "glide", "install"
     system "go", "build", "-o", bin/"terragrunt", "-ldflags", "-X main.VERSION=" + version.to_s
   end
 
