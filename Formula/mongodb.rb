6decedbd3496d7323999f24fab89721371f1fb1f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-30 15:25:53 +0000

    mongodb: update 3.4.0 bottle.

--- Formula/mongodb.rb
+++ Formula/mongodb.rb
@@ -7,9 +7,9 @@ class Mongodb < Formula
   sha256 "5a904b66d09e4d15f3ed35028a004640afcba5d8ecb5644165bd08cd7eb79df6"
 
   bottle do
-    sha256 "1d10bf3afdc6aee7c4f5635cc5c4a42e85f4176d8041787d596917795e42e8f4" => :sierra
-    sha256 "46d7ffb599836679637d5d8844e5db5ebb7d4554f853ed1711127880a9c8e425" => :el_capitan
-    sha256 "4a0fba4796db81d07a4ec4a67679c366bde079e9516359f91f5ce95d0486c947" => :yosemite
+    sha256 "552c50d973ee466cf3bb6d41f43d92fdfd27dda2f94dc990e4a6cdd1efad61c5" => :sierra
+    sha256 "8ff4bbc28e50109374f2eb136da624cd15074f7419f099ad57d0931da33e6bc3" => :el_capitan
+    sha256 "8a63a2c0e6c76e86edf5a9321bc033830754fa2cdf7577d31781918c27405791" => :yosemite
   end
 
   option "with-boost", "Compile using installed boost, not the version shipped with mongodb"
