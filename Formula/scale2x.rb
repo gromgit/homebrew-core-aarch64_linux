9cf18c580a72f981b74504ebf44fca8fc3d0d8ea
Uladzislau Shablinski <vladshablinsky@gmail.com>
2016-12-02 01:57:42 +0300

    scale2x: add CPPFLAGS
    Closes #7341.
    
    Signed-off-by: Uladzislau Shablinski <vladshablinsky@gmail.com>

--- Formula/scale2x.rb
+++ Formula/scale2x.rb
@@ -6,18 +6,13 @@ class Scale2x < Formula
 
   bottle do
     cellar :any
-    sha256 "d6c9c08d16c16c6da5b5acc5e7b2de861c8325d67dd0e9dc0e1a5032ad6cc637" => :sierra
-    sha256 "c4d3727b99efa7e329259d5b8bc5e600f034db13e7ddfcbb792f8e512e2b5419" => :el_capitan
-    sha256 "ab48af00a09bbb84b1a99cb178d7e2943e51f8ec743b74321ef24a86cc646797" => :yosemite
-    sha256 "0ad21fade0671c6139a0d959e5952614fc9b796dee535db443aaa9e455aa766f" => :mavericks
+    sha256 "682fee40f32660f8123e06a0c918895a14515517e7fb1dcc6eb362fe1a870c4a" => :sierra
   end
 
   depends_on "libpng"
 
   def install
-    # This function was renamed in current versions of libpng.
-    inreplace "file.c", "png_set_gray_1_2_4_to_8", "png_set_expand_gray_1_2_4_to_8"
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "CPPFLAGS=-I/usr/include/malloc/"
     system "make", "install"
   end
 end
