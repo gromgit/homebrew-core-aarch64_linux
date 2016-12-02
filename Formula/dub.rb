f8bc2e6e4b958464457c3cc69c1b685f04e5bb52
John Colvin <john.loughran.colvin@gmail.com>
2016-12-02 12:04:03 +0000

    dub 1.1.1
    dub v1.1.1
    
    Closes #7470.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/dub.rb
+++ Formula/dub.rb
@@ -1,11 +1,10 @@
 class Dub < Formula
   desc "Build tool for D projects"
   homepage "https://code.dlang.org/about"
-  url "https://github.com/dlang/dub.git",
-      :tag => "v1.0.0",
-      :revision => "b59af2b8befb4fad4157d8c9cc86dba707b2fc87"
+  url "https://github.com/dlang/dub/archive/v1.1.1.tar.gz"
+  sha256 "9931c03fa908e83632553572bdcfb3570b8175a57b00b9ff7c9b69b991662b89"
 
-  head "https://github.com/dlang/dub.git", :shallow => false
+  head "https://github.com/dlang/dub.git"
 
   bottle do
     sha256 "68fcb18d5b548c8e676809ab318354ebea47950b5a4a5ff8ff20cb05d9ae2a0e" => :sierra
@@ -14,13 +13,7 @@ class Dub < Formula
     sha256 "5bab5095000421e561fbf41268c615e079023b86a0d6d7ed00328ab52a7db906" => :mavericks
   end
 
-  devel do
-    url "https://github.com/dlang/dub.git",
-        :tag => "v1.1.0-beta.1",
-        :revision => "1824d16056a680fcca52d3ce87fb43d3679ff3ee"
-  end
-
-  depends_on "pkg-config" => :build
+  depends_on "pkg-config" => [:recommended, :run]
   depends_on "dmd" => :build
 
   def install
