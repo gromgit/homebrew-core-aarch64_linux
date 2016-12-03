2748902229d2516ffa29d6995ef79f228c9b46ed
Kenzie Togami <ket1999@gmail.com>
2016-12-02 22:24:24 -0800

    gradle: add option to install gradle-all

--- Formula/gradle.rb
+++ Formula/gradle.rb
@@ -1,8 +1,10 @@
 class Gradle < Formula
   desc "Build system based on the Groovy language"
   homepage "https://www.gradle.org/"
-  url "https://downloads.gradle.org/distributions/gradle-3.2.1-bin.zip"
-  sha256 "9843a3654d3e57dce54db06d05f18b664b95c22bf90c6becccb61fc63ce60689"
+  url "https://downloads.gradle.org/distributions/gradle-3.2.1-all.zip"
+  sha256 "0209696f1723f607c475109cf3ed8b51c8a91bb0cda05af0d4bd980bdefe75cd"
+
+  option "with-all", "Installs Javadoc, examples, and source in addition to the binaries"
 
   bottle :unneeded
 
@@ -10,6 +12,9 @@ class Gradle < Formula
 
   def install
     libexec.install %w[bin lib]
+    if build.with? "all"
+      libexec.install %w[docs media samples src]
+    end
     bin.install_symlink libexec/"bin/gradle"
   end
 
