2f22db7209b2e12475bbc35dbf2d9d35859c7548
Tomasz Pajor <tomek@polishgeeks.com>
2016-12-03 08:44:40 +0100

    percona-toolkit: bump resources
    Closes #7509.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/percona-toolkit.rb
+++ Formula/percona-toolkit.rb
@@ -4,6 +4,7 @@ class PerconaToolkit < Formula
   url "https://www.percona.com/downloads/percona-toolkit/2.2.19/tarball/percona-toolkit-2.2.19.tar.gz"
   mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/p/percona-toolkit/percona-toolkit_2.2.19.orig.tar.gz"
   sha256 "e9f4d4730265813fa7a39ed8799d12ca5775c8e5d6fa27ff48bae11db0f7e671"
+  revision 1
 
   head "lp:percona-toolkit", :using => :bzr
 
@@ -13,9 +14,9 @@ class PerconaToolkit < Formula
   depends_on "openssl"
 
   resource "DBD::mysql" do
-    url "https://cpan.metacpan.org/authors/id/M/MI/MICHIELB/DBD-mysql-4.035.tar.gz"
-    mirror "http://search.cpan.org/CPAN/authors/id/M/MI/MICHIELB/DBD-mysql-4.035.tar.gz"
-    sha256 "b7eca365ea16bcf4c96c2fc0221304ff9c4995e7a551886837804a8f66b61937"
+    url "https://cpan.metacpan.org/authors/id/M/MI/MICHIELB/DBD-mysql-4.041.tar.gz"
+    mirror "http://search.cpan.org/CPAN/authors/id/M/MI/MICHIELB/DBD-mysql-4.041.tar.gz"
+    sha256 "4777de11c464b515db9da95c08c225900d0594b65ba3256982dc21f9f9379040"
   end
 
   resource "JSON" do
