8545b617522de7e6c8488a82ecce0cf024d210d2
ilovezfs <ilovezfs@icloud.com>
2016-12-03 01:56:55 -0800

    ipmitool 1.8.18
    Closes #5706.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/ipmitool.rb
+++ Formula/ipmitool.rb
@@ -1,9 +1,9 @@
 class Ipmitool < Formula
   desc "Utility for IPMI control with kernel driver or LAN interface"
   homepage "http://ipmitool.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/ipmitool/ipmitool/1.8.17/ipmitool-1.8.17.tar.bz2"
-  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/i/ipmitool/ipmitool_1.8.17.orig.tar.bz2"
-  sha256 "97fa20efd9c87111455b174858544becae7fcc03a3cb7bf5c19b09065c842d02"
+  url "https://downloads.sourceforge.net/project/ipmitool/ipmitool/1.8.18/ipmitool-1.8.18.tar.bz2"
+  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/i/ipmitool/ipmitool_1.8.18.orig.tar.bz2"
+  sha256 "0c1ba3b1555edefb7c32ae8cd6a3e04322056bc087918f07189eeedfc8b81e01"
 
   bottle do
     cellar :any
@@ -19,6 +19,10 @@ class Ipmitool < Formula
     # Required to get NI_MAXHOST and NI_MAXSERV defined in /usr/include/netdb.h, see
     # https://sourceforge.net/p/ipmitool/bugs/418
     inreplace "src/plugins/ipmi_intf.c", "#define _GNU_SOURCE 1", "#define _GNU_SOURCE 1\n#define _DARWIN_C_SOURCE 1"
+
+    # Fix ipmi_cfgp.c:33:10: fatal error: 'malloc.h' file not found
+    # Upstream issue from 8 Nov 2016 https://sourceforge.net/p/ipmitool/bugs/474/
+    inreplace "lib/ipmi_cfgp.c", "#include <malloc.h>", ""
 
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
