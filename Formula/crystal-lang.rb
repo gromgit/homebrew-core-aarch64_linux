a997839f4c1eaec05482249e7e4df2c9faa688cc
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-12-05 12:56:48 -0300

    crystal-lang 0.20.1
    Closes #7562.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/crystal-lang.rb
+++ Formula/crystal-lang.rb
@@ -1,8 +1,8 @@
 class CrystalLang < Formula
   desc "Fast and statically typed, compiled language with Ruby-like syntax"
   homepage "https://crystal-lang.org/"
-  url "https://github.com/crystal-lang/crystal/archive/0.20.0.tar.gz"
-  sha256 "97118becc450cdfbafd881f8e98e93d9373d71b4994f49f41dec1eaf081e8894"
+  url "https://github.com/crystal-lang/crystal/archive/0.20.1.tar.gz"
+  sha256 "3792856bfe768421f4f0488dd8717deb0db4c94fee7587d39174f4911ab2c01d"
   head "https://github.com/crystal-lang/crystal.git"
 
   bottle do
@@ -23,14 +23,14 @@ class CrystalLang < Formula
   depends_on "libyaml" if build.with? "shards"
 
   resource "boot" do
-    url "https://github.com/crystal-lang/crystal/releases/download/0.19.4/crystal-0.19.4-1-darwin-x86_64.tar.gz"
-    version "0.19.4"
-    sha256 "ec9f21f29035b76b2bc32bd3d975aa057f68007191cc6aab1535effe4c1c60d5"
+    url "https://github.com/crystal-lang/crystal/releases/download/0.20.0/crystal-0.20.0-1-darwin-x86_64.tar.gz"
+    version "0.20.0"
+    sha256 "4865c464d37aa4328f1f401e3a66136d8149bad51ef5ecd6fef0d032067e7fee"
   end
 
   resource "shards" do
-    url "https://github.com/crystal-lang/shards/archive/v0.7.0.tar.gz"
-    sha256 "2e8e20cb0dcaed7f756146954b8946bd01d98d9462740233b204647e5aa628a2"
+    url "https://github.com/crystal-lang/shards/archive/v0.7.1.tar.gz"
+    sha256 "31de819c66518479682ec781a39ef42c157a1a8e6e865544194534e2567cb110"
   end
 
   def install
@@ -42,7 +42,7 @@ class CrystalLang < Formula
       ENV["CRYSTAL_CONFIG_VERSION"] = version
     end
 
-    ENV["CRYSTAL_CONFIG_PATH"] = prefix/"src:libs:lib"
+    ENV["CRYSTAL_CONFIG_PATH"] = prefix/"src:lib:libs"
     ENV.append_path "PATH", "boot/bin"
 
     if build.with? "release"
