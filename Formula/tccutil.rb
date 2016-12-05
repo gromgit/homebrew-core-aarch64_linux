451b6a5e95e8c62cc38123bdc86879066da36468
ilovezfs <ilovezfs@icloud.com>
2016-12-04 23:28:33 -0800

    tccutil 1.2.3
    Closes #7553.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/tccutil.rb
+++ Formula/tccutil.rb
@@ -1,8 +1,8 @@
 class Tccutil < Formula
   desc "Utility to modify the macOS Accessibility Database (TCC.db)"
   homepage "https://github.com/jacobsalmela/tccutil"
-  url "https://github.com/jacobsalmela/tccutil/archive/v1.2.2.tar.gz"
-  sha256 "7d25cee6d08b2c27d9578e6ec8c51cfa172e34188cb725dddee1620b31e71086"
+  url "https://github.com/jacobsalmela/tccutil/archive/v1.2.3.tar.gz"
+  sha256 "8146f44e6b44add60ce29469ce04000baa0066d632373f7c8ad21cf906d06cc3"
   head "https://github.com/jacobsalmela/tccutil.git"
 
   bottle :unneeded
