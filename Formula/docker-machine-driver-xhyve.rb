c44c5a8c850ab8675c47040f293a8c65eb21a5d0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-06 05:24:45 +0000

    docker-machine-driver-xhyve: update 0.3.1 bottle.

--- Formula/docker-machine-driver-xhyve.rb
+++ Formula/docker-machine-driver-xhyve.rb
@@ -8,9 +8,9 @@ class DockerMachineDriverXhyve < Formula
   head "https://github.com/zchee/docker-machine-driver-xhyve.git"
 
   bottle do
-    sha256 "f650f9a530c52b62d6aeaeca51453f2488155f9c3b9e7ee6fec8f1006f4bad3b" => :sierra
-    sha256 "4425bd727de66f57cc15f4c14d10cf764a1e968ac19022ead3f5144b3d859934" => :el_capitan
-    sha256 "6042fe21e3e6e6feb46273796d5cbe9ba307ea73e3e6c40135f23e5e820f411d" => :yosemite
+    sha256 "b1d626f1d466e87f483cda698ccfa5e8d4e40bf25144e52d2387aaa0a1f1c1f7" => :sierra
+    sha256 "af5f4d61072d524a27ecea05bac0e2ab75e8600d28c2ce442b993a84f20ffa1d" => :el_capitan
+    sha256 "d90a5a24b067ef66ba5f81b6050e7111d8527ef189560a0cdc740acc8ba12e44" => :yosemite
   end
 
   option "without-qcow2", "Do not support qcow2 disk image format"
