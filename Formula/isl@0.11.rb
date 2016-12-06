5056be35f750f3766984e9327c1293d020d0a488
Mike McQuaid <mike@mikemcquaid.com>
2016-12-06 09:25:59 +0000

    isl@0.11: import from homebrew/versions.

---
+++ Formula/isl@0.11.rb
@@ -0,0 +1,38 @@
+class IslAT011 < Formula
+  desc "Integer Set Library for the polyhedral model"
+  homepage "http://freecode.com/projects/isl"
+  # Track gcc infrastructure releases.
+  url "http://isl.gforge.inria.fr/isl-0.11.1.tar.bz2"
+  mirror "ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.11.1.tar.bz2"
+  sha256 "095f4b54c88ca13a80d2b025d9c551f89ea7ba6f6201d701960bfe5c1466a98d"
+
+  keg_only "Older version of isl"
+
+  depends_on "gmp@4"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--with-gmp=system",
+                          "--with-gmp-prefix=#{Formula["gmp@4"].opt_prefix}"
+    system "make", "install"
+    (share/"gdb/auto-load").install Dir["#{lib}/*-gdb.py"]
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <isl/ctx.h>
+
+      int main()
+      {
+        isl_ctx* ctx = isl_ctx_alloc();
+        isl_ctx_free(ctx);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lisl",
+      "-I#{include}", "-I#{Formula["gmp@4"].include}", "-o", "test"
+    system "./test"
+  end
+end
