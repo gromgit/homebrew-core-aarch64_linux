0ee9cf028d2a759e131ed0337586844cb205f4d6
ilovezfs <ilovezfs@icloud.com>
2016-12-05 22:23:48 -0800

    awscli 1.11.25
    Closes #7588.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -3,8 +3,8 @@ class Awscli < Formula
 
   desc "Official Amazon AWS command-line interface"
   homepage "https://aws.amazon.com/cli/"
-  url "https://github.com/aws/aws-cli/archive/1.11.24.tar.gz"
-  sha256 "1a60754c40fd5ff0e97ebe97d6fe7dde6a57a34a7bdb3484d9abd5d4fc5c5fdb"
+  url "https://github.com/aws/aws-cli/archive/1.11.25.tar.gz"
+  sha256 "91a60dc0196fd67d74368488297b94c546407a615c62d386b9b0012c3d8b5499"
   head "https://github.com/aws/aws-cli.git", :branch => "develop"
 
   bottle do
