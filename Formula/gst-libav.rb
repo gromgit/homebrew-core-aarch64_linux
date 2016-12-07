7d81f651d753049472f836133c096289bde80321
ilovezfs <ilovezfs@icloud.com>
2016-12-07 02:05:14 -0800

    gst-libav 1.10.2
    Closes #7625.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/gst-libav.rb
+++ Formula/gst-libav.rb
@@ -1,8 +1,8 @@
 class GstLibav < Formula
   desc "GStreamer plugins for Libav (a fork of FFmpeg)"
   homepage "https://gstreamer.freedesktop.org/"
-  url "https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-1.10.1.tar.xz"
-  sha256 "27b28b8de0e6dff1e3952428e8ed8ba4a12f452f789ac0ae9bbe50f00a5c72c7"
+  url "https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-1.10.2.tar.xz"
+  sha256 "a3dd7036211f061e1eda04f985b7a9dd1a91b754f767ff2587eb6ec28f44e73c"
 
   bottle do
     sha256 "0bfe4c427fd490bdb11171b6affa11d9ad99b29e997aeb8120a5793aabc32f07" => :sierra
