36be212caf83b3de57180279c6420e3d38d623e0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-10 08:24:18 +0000

    docutils: update 0.13.1 bottle.

--- Formula/docutils.rb
+++ Formula/docutils.rb
@@ -6,10 +6,9 @@ class Docutils < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6254928e32d119dbce4fc37f0a84306942f760696ecae02f8db4654934a238e6" => :sierra
-    sha256 "ee05c025462c3a5cb0ffd524e8fa146c1b3918eb4f4c6fd1006ea4bf4ae5db96" => :el_capitan
-    sha256 "a0cb7ec024d31a9f6f3bbcde7f07249e966bf9000008f7aee46a0978dcda9cb9" => :yosemite
-    sha256 "61fe1f390fdef3c130f46d66123a483ab0d15994420c89884fb0e99143ff8c19" => :mavericks
+    sha256 "cae7b82e3555eacdd6156c67af59a71d03274b006857ddbc84fe639136b21902" => :sierra
+    sha256 "1a7d2c671cbd6f88c81de7c2abf2cc099794313b082c81ae8079b86d40ad3cd2" => :el_capitan
+    sha256 "1a7d2c671cbd6f88c81de7c2abf2cc099794313b082c81ae8079b86d40ad3cd2" => :yosemite
   end
 
   def install
