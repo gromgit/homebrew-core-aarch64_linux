0f3d07d5828577e49cf9fea082321ec8d81ca954
Tim D. Smith <git@tim-smith.us>
2016-12-11 10:03:24 -0500

    pyexiv2: fix build

--- Formula/pyexiv2.rb
+++ Formula/pyexiv2.rb
@@ -18,14 +18,12 @@ class Pyexiv2 < Formula
   depends_on "boost"
   depends_on "boost-python"
 
-  # Patch to use Framework Python
-  patch :DATA
-
   def install
     # this build script ignores CPPFLAGS, but it honors CXXFLAGS
     ENV.append "CXXFLAGS", ENV.cppflags
     ENV.append "CXXFLAGS", "-I#{Formula["boost"].include}"
     ENV.append "CXXFLAGS", "-I#{Formula["exiv2"].include}"
+    ENV.append "LDFLAGS", "-undefined dynamic_lookup"
 
     scons "BOOSTLIB=boost_python-mt"
 
@@ -35,19 +33,3 @@ class Pyexiv2 < Formula
   end
 end
 
-__END__
-diff --git a/src/SConscript b/src/SConscript
-index f4b3e8c..748cad0 100644
---- a/src/SConscript
-+++ b/src/SConscript
-@@ -26,6 +26,10 @@ env.Append(CPPPATH=[get_python_inc(plat_specific=True)])
- libs = [ARGUMENTS.get('BOOSTLIB', 'boost_python'), 'exiv2']
- env.Append(LIBS=libs)
-
-+# Link against Python framework on macOS
-+if env['PLATFORM'] == 'darwin':
-+	env['FRAMEWORKS'] += ['Python']
-+
- # Build shared library libpyexiv2
- cpp_sources = ['exiv2wrapper.cpp', 'exiv2wrapper_python.cpp']
- libpyexiv2 = env.SharedLibrary('exiv2python', cpp_sources)
