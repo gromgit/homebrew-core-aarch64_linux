293b4ca9f88db047b4f2e359312568e7c8f51d08
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-12 16:28:26 +0000

    gmp@4: add 4.3.2 bottle.

--- Formula/gmp@4.rb
+++ Formula/gmp@4.rb
@@ -1,6 +1,13 @@
 class GmpAT4 < Formula
   desc "GNU multiple precision arithmetic library"
   homepage "http://gmplib.org/"
+  bottle do
+    cellar :any
+    sha256 "ac2e99801fe553b8501baa3c90c4644cef9ec63e9ba444ff269edf202d62bed0" => :sierra
+    sha256 "cf2f856bb513adb136f8a81ec9fd6fc46a0b52f08495226f21ec1bc4d81c18e0" => :el_capitan
+    sha256 "c259062b2b338e24503e0c0abd2d3467afd8b3ed48210aca7dc1206ea9ab4cc0" => :yosemite
+  end
+
   # Track gcc infrastructure releases.
   url "https://ftpmirror.gnu.org/gmp/gmp-4.3.2.tar.bz2"
   mirror "https://ftp.gnu.org/gnu/gmp/gmp-4.3.2.tar.bz2"
