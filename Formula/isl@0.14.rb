39f7fcc2730b59684849b431192aaeeaa4cd5c70
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-12 16:28:26 +0000

    isl@0.14: add 0.14 bottle.

--- Formula/isl@0.14.rb
+++ Formula/isl@0.14.rb
@@ -6,6 +6,13 @@ class IslAT014 < Formula
   mirror "ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.14.tar.bz2"
   sha256 "7e3c02ff52f8540f6a85534f54158968417fd676001651c8289c705bd0228f36"
 
+  bottle do
+    cellar :any
+    sha256 "33114e084463d3b4f81bc82e37af5feec655840b43c143df1452b2a68f132fae" => :sierra
+    sha256 "2149a627c0264bce672cd1db53f62ce1bb3d77d6013344559cc316d08cd09abc" => :el_capitan
+    sha256 "a0f74e8b8c350176d4b90eaf1d5ea569e2ebf6b715a7c1403040b13a5c05b7a5" => :yosemite
+  end
+
   keg_only "Older version of isl"
 
   depends_on "gmp"
