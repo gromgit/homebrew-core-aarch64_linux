5fb70c657bd39a007529510efaec7714508040aa
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-12-12 07:45:39 +0000

    gmime 2.6.21
    added gobject-introspection support
    
    Closes #7788.
    
    Signed-off-by: Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>

--- Formula/gmime.rb
+++ Formula/gmime.rb
@@ -1,8 +1,8 @@
 class Gmime < Formula
   desc "MIME mail utilities"
   homepage "http://spruce.sourceforge.net/gmime/"
-  url "https://download.gnome.org/sources/gmime/2.6/gmime-2.6.20.tar.xz"
-  sha256 "e0a170fb264c2ae4cecd852f4e7aaddb8d58e8f3f0b569ce2d2a4704f55bdf65"
+  url "https://download.gnome.org/sources/gmime/2.6/gmime-2.6.21.tar.xz"
+  sha256 "e6f40bb3f11b71f8004e7a91d9e20b2abe3898d211d0d815c061121bbcddb54f"
 
   bottle do
     cellar :any
@@ -16,12 +16,13 @@ class Gmime < Formula
   depends_on "pkg-config" => :build
   depends_on "libgpg-error" => :build
   depends_on "glib"
+  depends_on "gobject-introspection"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--enable-largefile",
-                          "--disable-introspection",
+                          "--enable-introspection",
                           "--disable-vala",
                           "--disable-mono",
                           "--disable-glibtest"
