97a6cba357bedb71c4e8174cc2b9db4b8f69ab65
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-12 16:28:25 +0000

    cloog@0.15: add 0.15.11 bottle.

--- Formula/cloog@0.15.rb
+++ Formula/cloog@0.15.rb
@@ -5,6 +5,13 @@ class CloogAT015 < Formula
   mirror "http://gcc.cybermirror.org/infrastructure/cloog-ppl-0.15.11.tar.gz"
   sha256 "7cd634d0b2b401b04096b545915ac67f883556e9a524e8e803a6bf6217a84d5f"
 
+  bottle do
+    cellar :any
+    sha256 "927f1240d896e0844c13b327fdf8f1a446566fb1609104dcc407b4422923ffd1" => :sierra
+    sha256 "797fdfe117784d713dcd56e48f1403f57a75fea2b6ae777e85c001c6b2fe9298" => :el_capitan
+    sha256 "b6945a3a781be4c9d4ad3e866812f859055c8b8c53895bedf0b071e2741b7ab4" => :yosemite
+  end
+
   keg_only "Older version of cloog"
 
   depends_on "gmp@4"
