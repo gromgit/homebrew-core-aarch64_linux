e60be212e931492b27a93e5ea4b12ceb16dd8535
CristianCantoro <kikkocristian@gmail.com>
2016-12-13 16:37:31 +0100

    cabal-install 1.24.0.2
    Fixes https://github.com/Linuxbrew/homebrew-core/issues/1365.
    
    Closes #7827.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/cabal-install.rb
+++ Formula/cabal-install.rb
@@ -1,8 +1,8 @@
 class CabalInstall < Formula
   desc "Command-line interface for Cabal and Hackage"
   homepage "https://www.haskell.org/cabal/"
-  url "https://hackage.haskell.org/package/cabal-install-1.24.0.1/cabal-install-1.24.0.1.tar.gz"
-  sha256 "09f5fd8a2aa7f9565800a711a133f8142d36d59b38f59da09c25045b83ee9ecc"
+  url "https://hackage.haskell.org/package/cabal-install-1.24.0.2/cabal-install-1.24.0.2.tar.gz"
+  sha256 "2ac8819238a0e57fff9c3c857e97b8705b1b5fef2e46cd2829e85d96e2a00fe0"
   head "https://github.com/haskell/cabal.git", :branch => "1.24"
 
   bottle do
@@ -19,10 +19,6 @@ class CabalInstall < Formula
   def install
     cd "cabal-install" if build.head?
 
-    # Fix "'Distribution.Simple.Configure' does not export 'computeEffectiveProfiling'"
-    # Equivalent to upstream PR from 16 Nov 2016 https://github.com/haskell/cabal/pull/4117
-    inreplace "bootstrap.sh", 'CABAL_VER_REGEXP="1\.24\.[0-9]"',
-                              'CABAL_VER_REGEXP="1\.24\.[1-9]"'
     system "sh", "bootstrap.sh", "--sandbox"
     bin.install ".cabal-sandbox/bin/cabal"
     bash_completion.install "bash-completion/cabal"
