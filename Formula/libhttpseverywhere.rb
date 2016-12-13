b5a5cf0d4d197b8cf810d56570dff52afed982e3
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-12-13 20:41:51 +0000

    libhttpseverywhere 0.2.9 (new formula)
    Closes #7831.
    
    Signed-off-by: Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>

---
+++ Formula/libhttpseverywhere.rb
@@ -0,0 +1,76 @@
+class Libhttpseverywhere < Formula
+  desc "Bring HTTPSEverywhere to desktop apps"
+  homepage "https://github.com/gnome/libhttpseverywhere"
+  url "https://download.gnome.org/sources/libhttpseverywhere/0.2/libhttpseverywhere-0.2.9.tar.xz"
+  sha256 "1226b2f7cc30db28bd1ccb0a84902e999488f1a99847940cbdab3d2e510be3e1"
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "vala" => :build
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "json-glib"
+  depends_on "libsoup"
+  depends_on "libgee"
+  depends_on "libarchive"
+
+  def install
+    mkdir "build" do
+      system "meson", "--prefix=#{prefix}", ".."
+      system "ninja"
+      system "ninja", "test"
+      system "ninja", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <httpseverywhere.h>
+
+      int main(int argc, char *argv[]) {
+        GType type = https_everywhere_context_get_type();
+        return 0;
+      }
+    EOS
+    ENV.libxml2
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    json_glib = Formula["json-glib"]
+    libarchive = Formula["libarchive"]
+    libgee = Formula["libgee"]
+    libsoup = Formula["libsoup"]
+    pcre = Formula["pcre"]
+    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
+    flags += %W[
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{include}/httpseverywhere-0.2
+      -I#{json_glib.opt_include}/json-glib-1.0
+      -I#{libarchive.opt_include}
+      -I#{libgee.opt_include}/gee-0.8
+      -I#{libsoup.opt_include}/libsoup-2.4
+      -I#{pcre.opt_include}
+      -D_REENTRANT
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{json_glib.opt_lib}
+      -L#{libarchive.opt_lib}
+      -L#{libgee.opt_lib}
+      -L#{libsoup.opt_lib}
+      -L#{lib}
+      -larchive
+      -lgee-0.8
+      -lgio-2.0
+      -lglib-2.0
+      -lgobject-2.0
+      -lhttpseverywhere-0.2
+      -lintl
+      -ljson-glib-1.0
+      -lsoup-2.4
+      -lxml2
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
