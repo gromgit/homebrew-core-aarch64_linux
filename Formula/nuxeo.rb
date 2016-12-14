3aaae69b16f4f27959dab6829424f68c0d4ce5aa
Arnaud Kervern <arnaud.k@gmail.com>
2016-12-14 06:50:19 +0100

    nuxeo 8.10 (#7794)
    nuxeo 8.10

--- Formula/nuxeo.rb
+++ Formula/nuxeo.rb
@@ -1,9 +1,9 @@
 class Nuxeo < Formula
   desc "Enterprise Content Management"
   homepage "https://nuxeo.github.io/"
-  url "https://cdn.nuxeo.com/nuxeo-8.3/nuxeo-server-8.3-tomcat.zip"
-  version "8.3"
-  sha256 "69a51ea0f5663723a58448be80b5caf4258f9d1f8df124e4de90676224afac3c"
+  url "https://cdn.nuxeo.com/nuxeo-8.10/nuxeo-server-8.10-tomcat.zip"
+  version "8.10"
+  sha256 "b41e515498b61e9cb394981ca3b2e662fd98b4c598d3d7e83465bc7a268e1faf"
 
   bottle :unneeded
 
