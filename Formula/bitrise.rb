636b9d1ddb775921f7698bc33f7d6f414b456ffa
Krisztian Godrei <godreikrisztian@gmail.com>
2016-12-14 13:05:22 +0100

    bitrise 1.5.1
    Closes #7851.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/bitrise.rb
+++ Formula/bitrise.rb
@@ -1,8 +1,8 @@
 class Bitrise < Formula
   desc "Command-line automation tool"
   homepage "https://github.com/bitrise-io/bitrise"
-  url "https://github.com/bitrise-io/bitrise/archive/1.4.5.tar.gz"
-  sha256 "80aab3dfe8e34eb7996f98edb7102c8566af87616acf5f7a76a5012bb421d415"
+  url "https://github.com/bitrise-io/bitrise/archive/1.5.1.tar.gz"
+  sha256 "329ba79d117fae7a638ec163731c09ad593c647ddab4320bb415e1144e4c7bf7"
 
   bottle do
     cellar :any_skip_relocation
