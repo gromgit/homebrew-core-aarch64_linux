93272510f09b7da88831b0be1b5473fd4a8f1b81
ilovezfs <ilovezfs@icloud.com>
2016-12-15 08:09:07 -0800

    mailutils 3.1
    depend on libtool
    
    Closes #7871.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mailutils.rb
+++ Formula/mailutils.rb
@@ -1,9 +1,9 @@
 class Mailutils < Formula
   desc "Swiss Army knife of email handling"
   homepage "http://mailutils.org/"
-  url "https://ftpmirror.gnu.org/mailutils/mailutils-3.0.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/mailutils/mailutils-3.0.tar.gz"
-  sha256 "41e5a1e9b1da1efd184b4cb3ed8e88bb3013ff09f9774b15a65253ff31db2f9f"
+  url "https://ftpmirror.gnu.org/mailutils/mailutils-3.1.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/mailutils/mailutils-3.1.tar.gz"
+  sha256 "65c8a4f9a59904ba1035c0f891cba03a6ccdd8ac841c22dac0905ed994fd8f1a"
 
   bottle do
     sha256 "3a2d771c6ff402a0345e263f1ca24775cd8a3c153f69ee00006ac1eb3ee14cbc" => :sierra
@@ -11,10 +11,14 @@ class Mailutils < Formula
     sha256 "43af4b9eab94c5af57aa35e4d44312834ca66b14d456030233f6820a96d53621" => :yosemite
   end
 
+  depends_on "libtool" => :build
   depends_on "gnutls"
   depends_on "gsasl"
   depends_on "readline"
 
+  # Remove second patch (the restoration of argcv.h) for > 3.1
+  # See upstream commit "Restore prematurely deleted header"
+  # http://git.savannah.gnu.org/cgit/mailutils.git/commit/?id=723ade1ac72fa2635d7aa04f6a118cefce44f15a
   patch :DATA
 
   def install
@@ -22,6 +26,9 @@ class Mailutils < Formula
                           "--prefix=#{prefix}",
                           "--without-tokyocabinet"
     system "make", "PYTHON_LIBS=-undefined dynamic_lookup", "install"
+
+    # Remove for > 3.1
+    (include/"mailutils").install "include/mailutils/argcv.h"
   end
 
   test do
@@ -42,3 +49,63 @@ index e5bd5a1..6de2647 100644
 
  #include <mailutils/sockaddr.h>
  #include <mailutils/errno.h>
+diff --git a/include/mailutils/argcv.h b/include/mailutils/argcv.h
+new file mode 100644
+index 0000000..4744fb5
+--- /dev/null
++++ b/include/mailutils/argcv.h
+@@ -0,0 +1,54 @@
++/* GNU Mailutils -- a suite of utilities for electronic mail
++   Copyright (C) 1999-2001, 2005, 2007, 2010-2012, 2014-2016 Free
++   Software Foundation, Inc.
++
++   This library is free software; you can redistribute it and/or
++   modify it under the terms of the GNU Lesser General Public
++   License as published by the Free Software Foundation; either
++   version 3 of the License, or (at your option) any later version.
++
++   This library is distributed in the hope that it will be useful,
++   but WITHOUT ANY WARRANTY; without even the implied warranty of
++   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
++   Lesser General Public License for more details.
++
++   You should have received a copy of the GNU Lesser General
++   Public License along with this library.  If not, see
++   <http://www.gnu.org/licenses/>. */
++
++#ifndef _ARGCV_H
++#define _ARGCV_H 1
++
++#include <stdio.h>
++#include <stdlib.h>
++#include <unistd.h>
++#include <string.h>
++
++#include <mailutils/types.h>
++
++#ifdef __cplusplus
++extern "C" {
++#endif
++
++void mu_argcv_free (size_t argc, char **argv);
++void mu_argv_free (char **argv);
++
++enum mu_argcv_escape
++  {
++    mu_argcv_escape_no,
++    mu_argcv_escape_c
++    /*    mu_argcv_escape_sh */
++  };
++
++int mu_argcv_join (int argc, char **argv, char *delim,
++		   enum mu_argcv_escape esc,
++		   char **pstring);
++void mu_argcv_remove (int *pargc, char ***pargv,
++		      int (*sel) (const char *, void *), void *);
++int mu_argcv_string (int argc, char **argv, char **string);
++
++#ifdef __cplusplus
++}
++#endif
++
++#endif /* _ARGCV_H */
