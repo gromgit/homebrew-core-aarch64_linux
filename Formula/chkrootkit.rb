7652cf7c1885b135e02ba401f534cacf2325fd5c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-15 19:29:14 +0000

    chkrootkit: update 0.51 bottle.

--- Formula/chkrootkit.rb
+++ Formula/chkrootkit.rb
@@ -7,11 +7,9 @@ class Chkrootkit < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "687f4b80b7c7a82af8cb3c3079e848b9538600b990e4b57acee2bab5efc1fdeb" => :sierra
-    sha256 "d631f10284719bd507c6e08bfadc68507adda611d1488dfe6f59f2cdf3b7d1e9" => :el_capitan
-    sha256 "e55c78bfac4b081cef6b51e46cf3ff7fed13b4150f8772535fa401e83bcfe213" => :yosemite
-    sha256 "db4476a3cfc7d10f9991628c95068784c702662bc0be79b124fd775c56c65ecf" => :mavericks
-    sha256 "8aa44d8d76a8d6ddbc27adac04e64e3120c4599b2d617ff0fa82d15f8229957b" => :mountain_lion
+    sha256 "fe08ca7c95b7153659a55de068d320103719eca8d7672bae9af1542c88af5046" => :sierra
+    sha256 "d2092429968f96cff683f76ca3c302e1d55a01c0d63d44fcfefd31bd077f939d" => :el_capitan
+    sha256 "bb5b75999c36b2323e0348b161bfe0d4891d9f09782367aa8b8eaf345f04de26" => :yosemite
   end
 
   def install
