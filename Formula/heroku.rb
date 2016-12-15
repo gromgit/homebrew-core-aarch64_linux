26cbb099952fa792ef4e4c1dcf0444d97f8982c8
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2016-12-15 14:54:33 +0000

    heroku 5.6.1
    Closes #7890.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -1,24 +1,17 @@
 class Heroku < Formula
   desc "Everything you need to get started with Heroku"
-  homepage "https://toolbelt.heroku.com/standalone"
-  url "https://s3.amazonaws.com/assets.heroku.com/heroku-client/heroku-client-3.43.16.tgz"
-  sha256 "c7c2901c7a16a56300a90c7280d25265771cb2dfb332e921d4fc4825d988ea22"
-  head "https://github.com/heroku/heroku.git"
+  homepage "https://cli.heroku.com"
+  url "http://cli-assets.heroku.com/branches/stable/5.6.1-0976cf3/heroku-v5.6.1-0976cf3-darwin-amd64.tar.xz"
+  version "5.6.1-0976cf3"
+  sha256 "27994f23258ef21c0e40b9fa21fc24b5fcabc44c697c07bf2883fb9b6fc65869"
 
   bottle :unneeded
 
   depends_on :arch => :x86_64
-  depends_on :ruby => "1.9"
 
   def install
     libexec.install Dir["*"]
-    # turn off autoupdates (off by default in HEAD)
-    if build.stable?
-      inreplace libexec/"bin/heroku",
-                "Heroku::Updater.inject_libpath",
-                "Heroku::Updater.disable(\"Use `brew upgrade heroku` to update\")"
-    end
-    bin.write_exec_script libexec/"bin/heroku"
+    bin.install_symlink libexec/"bin/heroku"
   end
 
   test do
