5d012507c915ef16f2d0d441d18d54f823eaae4e
Marc Savy <marc@rhymewithgravy.com>
2016-12-15 14:56:43 +0000

    byteman 4.0.0-BETA1 (devel)
    Closes #7891.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/byteman.rb
+++ Formula/byteman.rb
@@ -5,9 +5,9 @@ class Byteman < Formula
   sha256 "c7b90b63346d4a0f648c55e12c21110fb173fb9ae8471727fd44906591fa8c26"
 
   devel do
-    url "https://downloads.jboss.org/byteman/4.0.0-BETA0/byteman-download-4.0.0-BETA0-bin.zip"
-    sha256 "345b10b207e7213e96a124b05b52b71fb932d1668733cbd7e0b4ec4f497bb5a8"
-    version "4.0.0-BETA0"
+    url "https://downloads.jboss.org/byteman/4.0.0-BETA1/byteman-download-4.0.0-BETA1-bin.zip"
+    sha256 "1cdc26781a6837242c9c2f96ef8dec55ea25022012047b3c1ad70c04cb181da8"
+    version "4.0.0-BETA1"
   end
 
   bottle :unneeded
