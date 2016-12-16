b01c896ee1db5eb45cc9da6361e7d627d9d76ad3
Wez Furlong <wez@wezfurlong.org>
2016-12-15 16:02:14 -0800

    folly 2016.12.12.00
    Update folly to a more recent tag.
    
    The principal change that impacts homebrew is that we fixed
    a problem with jemalloc linkage that affected wangle when
    linking against the brew built folly.
    
    Closes #7913.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/folly.rb
+++ Formula/folly.rb
@@ -1,8 +1,8 @@
 class Folly < Formula
   desc "Collection of reusable C++ library artifacts developed at Facebook"
   homepage "https://github.com/facebook/folly"
-  url "https://github.com/facebook/folly/archive/v2016.11.28.00.tar.gz"
-  sha256 "5a50f75dcf421bf723fbe8d69dc9e3d1bb9935d3b4d5982fc52f29c029ed8c04"
+  url "https://github.com/facebook/folly/archive/v2016.12.12.00.tar.gz"
+  sha256 "ea96682d14423506136edb51a0a761573f50814c1634eb06f1b5d1ea6038cb44"
   head "https://github.com/facebook/folly.git"
 
   bottle do
@@ -62,7 +62,8 @@ class Folly < Formula
 
       system "autoreconf", "-fvi"
       system "./configure", "--prefix=#{prefix}", "--disable-silent-rules",
-                            "--disable-dependency-tracking"
+                            "--disable-dependency-tracking",
+                            "--with-jemalloc"
       system "make"
       system "make", "install"
     end
