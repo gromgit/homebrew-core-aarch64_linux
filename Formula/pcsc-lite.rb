66f1a8fcea413f853043cc3156790a091eaa5441
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-16 22:14:24 +0000

    pcsc-lite: update 1.8.19 bottle.

--- Formula/pcsc-lite.rb
+++ Formula/pcsc-lite.rb
@@ -5,10 +5,9 @@ class PcscLite < Formula
   sha256 "b65e25ec6dd1328983b424ce1a649e2993b1c4c59fc87252689b5fa7037c4340"
 
   bottle do
-    sha256 "d144b95c0db7082ad21938f4c2e909bfb44f26e90bac8278d809c99e3840b876" => :sierra
-    sha256 "54683ac6133d00972c5a88067aa7417bded62ceed95c0763fb3ac89518269055" => :el_capitan
-    sha256 "55c0449cd3b8dc7ef64de836127f9e599896d50ae5cf774b416f49789665a01b" => :yosemite
-    sha256 "1c8b7ffabc889c164fac3fc6ff6f1c6f49e1c510ff46359bca544c8978075480" => :mavericks
+    sha256 "c1d5af50c8b6ff831f3b20deb79d597e2c73f7af268760c845ddb23a8a932155" => :sierra
+    sha256 "86a89000db018b25e696b73c18fd9986f8419fff86297f2ab4d462d168e28ae2" => :el_capitan
+    sha256 "83faab4d9869a01bec70f89b4d0c93842b6320741d3ea08a96cf414bdb4bcaee" => :yosemite
   end
 
   keg_only :provided_by_osx,
