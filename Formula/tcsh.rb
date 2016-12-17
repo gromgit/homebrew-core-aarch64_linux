98df518ed1dfcb27852d6293c802d563221d4952
Gregory Nisbet <gregory.nisbet@gmail.com>
2016-12-17 10:44:04 -0800

    tcsh 6.20.00
    Closes #7971.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/tcsh.rb
+++ Formula/tcsh.rb
@@ -1,9 +1,9 @@
 class Tcsh < Formula
   desc "Enhanced, fully compatible version of the Berkeley C shell"
   homepage "http://www.tcsh.org/"
-  url "ftp://ftp.astron.com/pub/tcsh/tcsh-6.19.00.tar.gz"
-  mirror "http://ftp.funet.fi/pub/mirrors/ftp.astron.com/pub/tcsh/tcsh-6.19.00.tar.gz"
-  sha256 "12e271e0b89e4259d9d6e8d525322e77340e7244cfbd199a591e5f8146285c49"
+  url "ftp://ftp.astron.com/pub/tcsh/tcsh-6.20.00.tar.gz"
+  mirror "http://ftp.funet.fi/pub/mirrors/ftp.astron.com/pub/tcsh/tcsh-6.20.00.tar.gz"
+  sha256 "b89de7064ab54dac454a266cfe5d8bf66940cb5ed048d0c30674ea62e7ecef9d"
 
   bottle do
     sha256 "0ef20f94a3d60fdd22a9cbeebb5474d04d5a95fbfeb60e5de51e3fa102d94913" => :sierra
@@ -20,7 +20,7 @@ class Tcsh < Formula
 
   test do
     (testpath/"test.csh").write <<-EOS.undent
-      #!#{bin}/tcsh
+      #!#{bin}/tcsh -f
       set ARRAY=( "t" "e" "s" "t" )
       foreach i ( `seq $#ARRAY` )
         echo -n $ARRAY[$i]
