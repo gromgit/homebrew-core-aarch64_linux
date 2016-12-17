8f32e780b3f5208418f68904b621408fa3f69a41
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-17 12:29:32 +0000

    diff-pdf: update 0.2_16 bottle.

--- Formula/diff-pdf.rb
+++ Formula/diff-pdf.rb
@@ -7,9 +7,9 @@ class DiffPdf < Formula
 
   bottle do
     cellar :any
-    sha256 "7940e298466394c32c5f945515e939dbdd1b2238f8ab98d879eaa4f57145d453" => :sierra
-    sha256 "ca165f7eef57e0590975475ad6b709076593d340086bbc8ad3ec5cfc8f92fecf" => :el_capitan
-    sha256 "20b0a594c7a20427f5dd6545ee936092314aaccc450e3569990f746e08c20fa4" => :yosemite
+    sha256 "2b213ae3da945069b8392cbc60e0250e5c34f0ba2bd33073062682483f876c2c" => :sierra
+    sha256 "738d3e9dd1d6f066c2ae52c9a5bc77f50ff68b93f9b254b4df00523459df717f" => :el_capitan
+    sha256 "df95f62b368e52c54aaa7d2557dee32743b08c6b83bb2afd4a20f12b8e77ff03" => :yosemite
   end
 
   depends_on "pkg-config" => :build
