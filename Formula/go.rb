cb624fdebe9d4b3a38d2606ede015d29ec136aa1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-17 10:58:01 +0000

    go: update 1.7.4_1 bottle.

--- Formula/go.rb
+++ Formula/go.rb
@@ -18,9 +18,9 @@ class Go < Formula
   end
 
   bottle do
-    sha256 "bde94987de94c3b872760961782ff44e7cfa904b1df84507e90ed39d5cb63b05" => :sierra
-    sha256 "c684fbf681412a1aa06c0e64159d2dede4fd49dd3987e2c9679794c203e307a7" => :el_capitan
-    sha256 "5d7d8e26f7c96470be2cdf23fd10d83bd97a2ae3c4095aff0c6c253cbee84e68" => :yosemite
+    sha256 "f30470e4cef40ed0afcf111274eac6e921241e6ceaa6950bf771572356bd567e" => :sierra
+    sha256 "e04994e1c0885cda9b3ca313829405979ad53213a87571eb6e462e73fe76fe7c" => :el_capitan
+    sha256 "064ceb562c5ad8a99029a7bd92be27161d96760e36939c9287c2f4176d82dc48" => :yosemite
   end
 
   devel do
