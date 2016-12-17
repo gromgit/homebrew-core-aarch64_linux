0a1772a078729f9e33f458dcb9e46dade5ed17ee
Artem Medvedev <amedvedev@outlook.com>
2016-12-18 01:58:03 +0200

    antigen 1.3.1
    Closes #7975.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/antigen.rb
+++ Formula/antigen.rb
@@ -1,8 +1,8 @@
 class Antigen < Formula
   desc "Plugin manager for zsh, inspired by oh-my-zsh and vundle."
   homepage "http://antigen.sharats.me/"
-  url "https://github.com/zsh-users/antigen/archive/v1.3.0.tar.gz"
-  sha256 "e27bcc785c89fba0886fd03996dfc4bfb301ae6c07ed6a3305c4a0b45dcef004"
+  url "https://github.com/zsh-users/antigen/releases/download/v1.3.1/v1.3.1.tar.gz"
+  sha256 "58bc36b898ce02c8e06f8e69f65edae7c340a9870a2fd2724df3dd50bd338d8e"
   head "https://github.com/zsh-users/antigen.git"
 
   bottle :unneeded
