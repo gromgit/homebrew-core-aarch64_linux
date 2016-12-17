63901c0d1b208f57f1c182d0e7e26969f6e52a26
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-17 18:51:05 +0000

    tcsh: update 6.20.00 bottle.

--- Formula/tcsh.rb
+++ Formula/tcsh.rb
@@ -6,11 +6,9 @@ class Tcsh < Formula
   sha256 "b89de7064ab54dac454a266cfe5d8bf66940cb5ed048d0c30674ea62e7ecef9d"
 
   bottle do
-    sha256 "0ef20f94a3d60fdd22a9cbeebb5474d04d5a95fbfeb60e5de51e3fa102d94913" => :sierra
-    sha256 "dd05f0d9c6c12520c5b20fd0505288877a79a2b380091261490f659f6a81aadd" => :el_capitan
-    sha256 "2d3fed239e9d5ac71b47c3e5bf96b93c1e9ba77c3799ca482a5b6809621d5f1e" => :yosemite
-    sha256 "ff5b980d71cb0f151264f0a7f6c6b34120e2420aac284ecfe8fcf908f4e90c07" => :mavericks
-    sha256 "beab7a774551d3b446526359c31900fbbc7300403b545395c1949ba302a65e24" => :mountain_lion
+    sha256 "3a59ccfdab60133b8854d528465882a3a8aaaa874f70ef1e4a0deee2f06802c6" => :sierra
+    sha256 "d43bbcefe883ba5bd0dc998e5c4e6e9afcd35bacc780864fdcfe5a560002d7d1" => :el_capitan
+    sha256 "ecbd811718e22c579434568185a8ea87d78d420c251913f84da8093f61d1b408" => :yosemite
   end
 
   def install
