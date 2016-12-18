abef600ab68caaba6753e554c662b77e72d91d19
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-18 13:16:45 +0000

    gmime: update 2.6.22 bottle.

--- Formula/gmime.rb
+++ Formula/gmime.rb
@@ -5,9 +5,9 @@ class Gmime < Formula
   sha256 "c25f9097d5842a4808f1d62faf5eace24af2c51d6113da58d559a3bfe1d5553a"
 
   bottle do
-    sha256 "b5d17f3cfd4d52cec54202009ffb2bd9bcdde83a907832be6edea59af0202a6a" => :sierra
-    sha256 "a80e1965b633a6a5971f3ce343ad65be3df49837542f943e7e346e17f64a9a63" => :el_capitan
-    sha256 "92865a7e99cb816c66a02befbec4f463ed4a749cbd8fe99aeaffa509292c2320" => :yosemite
+    sha256 "86225b6bfb8839d6641d3a7e158fb71ddcb84d9613acea8238b3e9e31cd33888" => :sierra
+    sha256 "0cb3dbc9dede5046e04ae5024afed3934a52ffb208d1ebc21e5d36a71aed7385" => :el_capitan
+    sha256 "bbc9ad0689d9c35835a8ca9ee1d162807ee107bb10bf447f23c4858f4d7f42ca" => :yosemite
   end
 
   depends_on "pkg-config" => :build
