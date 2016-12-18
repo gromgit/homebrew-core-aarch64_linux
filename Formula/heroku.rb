9397de719bfc67e3d7b1d8d812f8ef146ed65262
Kevin Burke <kev@inburke.com>
2016-12-18 09:40:16 -0800

    heroku: install Heroku CLI over HTTPS
    cli-assets.heroku.com is listening on port 443 and presents a valid
    certificate. We should use HTTPS for downloads to prevent man in the middle
    attacks.
    
    Closes #7993.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -1,7 +1,7 @@
 class Heroku < Formula
   desc "Everything you need to get started with Heroku"
   homepage "https://cli.heroku.com"
-  url "http://cli-assets.heroku.com/branches/stable/5.6.1-0976cf3/heroku-v5.6.1-0976cf3-darwin-amd64.tar.xz"
+  url "https://cli-assets.heroku.com/branches/stable/5.6.1-0976cf3/heroku-v5.6.1-0976cf3-darwin-amd64.tar.xz"
   version "5.6.1-0976cf3"
   sha256 "27994f23258ef21c0e40b9fa21fc24b5fcabc44c697c07bf2883fb9b6fc65869"
 
