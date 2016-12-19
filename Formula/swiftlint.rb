cc103631075f83c4033f782c7b86d828cddbb484
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-19 04:41:37 +0000

    swiftlint: update 0.14.0 bottle.

--- Formula/swiftlint.rb
+++ Formula/swiftlint.rb
@@ -8,8 +8,8 @@ class Swiftlint < Formula
 
   bottle do
     cellar :any
-    sha256 "035ce56cb74d7affc699acb46e442bfa8444d2822b6ed5e646ebfd3148185a15" => :sierra
-    sha256 "0352beb89305e2b9b71e68579bed8fb317271d7673a8ab6504d906e12d3d3071" => :el_capitan
+    sha256 "cf9d794e66aff772cc2a61cbb5bfe7cf4b851cb1109f7714c572bf4a9c760f8f" => :sierra
+    sha256 "78d41e8e7bf1c662c0b8546ae171b358b0693344ce912f7837a5711d106e584f" => :el_capitan
   end
 
   depends_on :xcode => "8.0"
