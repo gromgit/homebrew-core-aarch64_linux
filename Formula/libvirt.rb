bbaac756dedfbbd7b4497cfa01e91f96c319569a
Justin Clift <justin@postgresql.org>
2016-12-19 18:08:59 +0000

    libvirt: add HEAD
    Closes #8015.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/libvirt.rb
+++ Formula/libvirt.rb
@@ -3,6 +3,7 @@ class Libvirt < Formula
   homepage "https://www.libvirt.org"
   url "https://libvirt.org/sources/libvirt-2.5.0.tar.xz"
   sha256 "819dffefbfd6ae95dfe1b28cfdee15ebcf9f91bbd732157488a57705bf81cb1e"
+  head "https://github.com/libvirt/libvirt.git"
 
   bottle do
     sha256 "9c5eb8632e525f9520f9ea4fcd708b04b561b27df95de84c4ecc218a02597f8b" => :sierra
@@ -16,6 +17,14 @@ class Libvirt < Formula
   depends_on "gnutls"
   depends_on "libgcrypt"
   depends_on "yajl"
+
+  if build.head?
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "gettext" => :build
+    depends_on "libtool" => :build
+    depends_on "rpcgen" => :build
+  end
 
   if MacOS.version <= :leopard
     # Definitely needed on Leopard, but not on Snow Leopard.
@@ -39,8 +48,10 @@ class Libvirt < Formula
       --without-qemu
     ]
 
+    args << "ac_cv_path_RPCGEN=#{Formula["rpcgen"].opt_prefix}/bin/rpcgen" if build.head?
     args << "--without-libvirtd" if build.without? "libvirtd"
 
+    system "./autogen.sh" if build.head?
     system "./configure", *args
 
     # Compilation of docs doesn't get done if we jump straight to "make install"
@@ -61,7 +72,12 @@ class Libvirt < Formula
   end
 
   test do
-    output = shell_output("#{bin}/virsh -v")
-    assert_match version.to_s, output
+    if build.head?
+      output = shell_output("#{bin}/virsh -V")
+      assert_match "Compiled with support for:", output
+    else
+      output = shell_output("#{bin}/virsh -v")
+      assert_match version.to_s, output
+    end
   end
 end
