12c9f413a5e3c75fe28ca48b229622ca2f441b7a
Mike McQuaid <mike@mikemcquaid.com>
2016-12-19 17:39:26 +0000

    mysql@5.5: remove ENV.minimal_optimization no-op.

--- Formula/mysql@5.5.rb
+++ Formula/mysql@5.5.rb
@@ -49,10 +49,6 @@ class MysqlAT55 < Formula
       "COMMAND /usr/bin/libtool -static -o ${TARGET_LOCATION}",
       "COMMAND libtool -static -o ${TARGET_LOCATION}"
 
-    # Build without compiler or CPU specific optimization flags to facilitate
-    # compilation of gems and other software that queries `mysql-config`.
-    ENV.minimal_optimization
-
     # -DINSTALL_* are relative to prefix
     args = %W[
       -DMYSQL_DATADIR=#{datadir}
