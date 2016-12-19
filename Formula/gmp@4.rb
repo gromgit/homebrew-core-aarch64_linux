1c2819bc12ae965f2ed36c35717c4fb10b32f635
Mike McQuaid <mike@mikemcquaid.com>
2016-12-19 17:24:52 +0000

    gmp@4: use ENV.deparallelize.

--- Formula/gmp@4.rb
+++ Formula/gmp@4.rb
@@ -40,7 +40,7 @@ class GmpAT4 < Formula
 
     system "./configure", *args
     system "make"
-    ENV.j1 # Doesn't install in parallel on 8-core Mac Pro
+    ENV.deparallelize # Doesn't install in parallel on 8-core Mac Pro
     system "make", "install"
 
     # Different compilers and options can cause tests to fail even
