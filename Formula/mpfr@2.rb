16c3bf906148735350eca616e2dc5e1b9b669031
Mike McQuaid <mike@mikemcquaid.com>
2016-12-19 06:07:08 -0800

    mpfr@2: remove 32-bit option.
    Closes #8024.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/mpfr@2.rb
+++ Formula/mpfr@2.rb
@@ -34,7 +34,7 @@ class MpfrAT2 < Formula
 
     # Build 32-bit where appropriate, and help configure find 64-bit CPUs
     # Note: This logic should match what the GMP formula does.
-    if MacOS.prefer_64_bit? && !build.build_32_bit?
+    if MacOS.prefer_64_bit?
       ENV.m64
       args << "--build=x86_64-apple-darwin"
     else
