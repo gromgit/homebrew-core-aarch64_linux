4a40b2a24065e0beab839fc4e40b6b97110c0a56
ilovezfs <ilovezfs@icloud.com>
2016-12-20 09:48:52 -0800

    lcov 1.13
    Closes #8061.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/lcov.rb
+++ Formula/lcov.rb
@@ -1,9 +1,8 @@
 class Lcov < Formula
   desc "Graphical front-end for GCC's coverage testing tool (gcov)"
   homepage "http://ltp.sourceforge.net/coverage/lcov.php"
-  url "https://downloads.sourceforge.net/ltp/lcov-1.12.tar.gz"
-  sha256 "b474e49c6c962754063b3be97a757a2ba9e7a455f0aea612863bf67e9b8b8ea7"
-
+  url "https://downloads.sourceforge.net/ltp/lcov-1.13.tar.gz"
+  sha256 "44972c878482cc06a05fe78eaa3645cbfcbad6634615c3309858b207965d8a23"
   head "https://github.com/linux-test-project/lcov.git"
 
   bottle do
@@ -14,23 +13,9 @@ class Lcov < Formula
     sha256 "6e2ffa607ebeec0b4c58cabeb8831ed1fa2f410bdb9032030209ab50afb8082a" => :mavericks
   end
 
-  patch :DATA
-
   def install
     inreplace %w[bin/genhtml bin/geninfo bin/lcov],
       "/etc/lcovrc", "#{prefix}/etc/lcovrc"
     system "make", "PREFIX=#{prefix}", "BIN_DIR=#{bin}", "MAN_DIR=#{man}", "install"
   end
 end
-
-__END__
---- lcov-1.12/bin/install.sh
-+++ lcov-1.12/bin/install.sh
-@@ -34,7 +34,8 @@ do_install()
-   local TARGET=$2
-   local PARAMS=$3
- 
--  install -p -D $PARAMS $SOURCE $TARGET
-+  mkdir -p `dirname $TARGET`
-+  install -p $PARAMS $SOURCE $TARGET
- }
