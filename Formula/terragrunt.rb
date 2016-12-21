3b7be50f335bea22f86acbb3780347dd4e75abff
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-21 15:55:15 +0000

    terragrunt: update 0.7.1 bottle.

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -6,10 +6,9 @@ class Terragrunt < Formula
   head "https://github.com/gruntwork-io/terragrunt.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "f8962d897f0dcc3e4795d4977c053cb3c8d114e4b190ce3f1c7ea2471e26d31e" => :sierra
-    sha256 "9a7c3567331d4c2bbd959122e707faa5510c550d389f5ac173e06cc45f834103" => :el_capitan
-    sha256 "7b847413a8df3f094c1617d6af6a1fcf579240dcaf65b9921a343ac5dc6b5489" => :yosemite
+    sha256 "a891e1913460e41d3140b59fb721cb10ddaf01c4ef7c26773a9a6cc4cb2072db" => :sierra
+    sha256 "fc743ab58bdd77dff6e37d8ce820046ff57406c258e2cb781590ea4076f86b6a" => :el_capitan
+    sha256 "67e655d46ed326236b816bccb8d5f105600b8fc92efb4c3e08677dc2525e2b1a" => :yosemite
   end
 
   depends_on "glide" => :build
