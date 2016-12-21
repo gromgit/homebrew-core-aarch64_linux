2b511fb411c64e7991bdf3667b9915ecbb0c9fbd
Alex Dunn <dunn.alex@gmail.com>
2016-12-20 19:57:11 -0800

    epeg 0.9.2
    Closes #8079.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/epeg.rb
+++ Formula/epeg.rb
@@ -1,8 +1,8 @@
 class Epeg < Formula
   desc "JPEG/JPG thumbnail scaling"
   homepage "https://github.com/mattes/epeg"
-  url "https://github.com/mattes/epeg/archive/v0.9.1.042.tar.gz"
-  sha256 "644362f87605e92f1e1cc0c421867252e4402939aeb4b36ad7cb385cc57a137c"
+  url "https://github.com/mattes/epeg/archive/v0.9.2.tar.gz"
+  sha256 "f8285b94dd87fdc67aca119da9fc7322ed6902961086142f345a39eb6e0c4e29"
 
   head "https://github.com/mattes/epeg.git"
 
@@ -16,21 +16,21 @@ class Epeg < Formula
   end
 
   depends_on "automake" => :build
+  depends_on "autoconf" => :build
+  depends_on "libtool" => :build
   depends_on "jpeg"
   depends_on "libexif"
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    system "./autogen.sh", "--disable-debug",
+                           "--disable-dependency-tracking",
+                           "--disable-silent-rules",
+                           "--prefix=#{prefix}"
     system "make", "install"
   end
 
   test do
-    system "#{bin}/epeg", "--width=1",
-                   "--height=1",
-                   test_fixtures("test.jpg")
-    "out.jpg"
+    system "#{bin}/epeg", "--width=1", "--height=1", test_fixtures("test.jpg"), "out.jpg"
+    assert File.exist? testpath/"out.jpg"
   end
 end
