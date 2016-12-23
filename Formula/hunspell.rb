5b1a068ea63bc8ea11a2f0adbc1bda78126f410c
ilovezfs <ilovezfs@icloud.com>
2016-12-23 01:31:05 -0800

    hunspell 1.6.0
    Closes #8150.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/hunspell.rb
+++ Formula/hunspell.rb
@@ -1,8 +1,8 @@
 class Hunspell < Formula
   desc "Spell checker and morphological analyzer"
   homepage "https://hunspell.github.io"
-  url "https://github.com/hunspell/hunspell/archive/v1.5.4.tar.gz"
-  sha256 "852c01a2e3b87eb1f2adb5261466608ba79f1f950202e18a2a1735de5098fc59"
+  url "https://github.com/hunspell/hunspell/archive/v1.6.0.tar.gz"
+  sha256 "512e7d2ee69dad0b35ca011076405e56e0f10963a02d4859dbcc4faf53ca68e2"
 
   bottle do
     sha256 "048a1986ce8f9b45674bcd2e47fe63fb1906f16935631082c7a77ce2fabbf0dd" => :sierra
@@ -10,16 +10,22 @@ class Hunspell < Formula
     sha256 "c056793bd40e4718fdafa022d005cecb73a498234dd98043f5441d345d54748c" => :yosemite
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "gettext" => :build
+  depends_on "libtool" => :build
   depends_on "readline"
 
   conflicts_with "freeling", :because => "both install 'analyze' binary"
 
   def install
+    system "autoreconf", "-fiv"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-ui",
                           "--with-readline"
     system "make"
+    system "make", "check"
     system "make", "install"
 
     pkgshare.install "tests"
