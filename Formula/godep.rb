ced50a747738d814562c46fc3497194051134ff9
ilovezfs <ilovezfs@icloud.com>
2016-12-23 08:01:43 -0800

    godep: revision for go
    Closes #8169.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/godep.rb
+++ Formula/godep.rb
@@ -3,8 +3,7 @@ class Godep < Formula
   homepage "https://godoc.org/github.com/tools/godep"
   url "https://github.com/tools/godep/archive/v75.tar.gz"
   sha256 "a9508db6a792170f9815864b70a70a8e0e66ca0bf57f1a9cc087d811ec105494"
-  revision 1
-
+  revision 2
   head "https://github.com/tools/godep.git"
 
   bottle do
