6b0e65a45771eafe4c6c2ba6524dde384775b9ef
ilovezfs <ilovezfs@icloud.com>
2016-12-23 01:55:01 -0800

    speedtest_cli 1.0.1
    Closes #8158.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/speedtest_cli.rb
+++ Formula/speedtest_cli.rb
@@ -1,8 +1,8 @@
 class SpeedtestCli < Formula
   desc "Command-line interface for https://speedtest.net bandwidth tests"
   homepage "https://github.com/sivel/speedtest-cli"
-  url "https://github.com/sivel/speedtest-cli/archive/v1.0.0.tar.gz"
-  sha256 "c9692ae3ec2005f39830ef8e645f63ccf45d6797624b5a56a53bde4d2174edca"
+  url "https://github.com/sivel/speedtest-cli/archive/v1.0.1.tar.gz"
+  sha256 "3ec2e6444e309f03d5f56526e6a66e4d556bd95e470abab605ac9c71d8231bc8"
   head "https://github.com/sivel/speedtest-cli.git"
 
   bottle :unneeded
