50eab1cfdbaccf06424011a66e4b5c64d493bde6
equal-l2 <equal-l2@users.noreply.github.com>
2016-12-24 12:40:07 +0900

    libtermkey 0.19
    Closes #8181.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/libtermkey.rb
+++ Formula/libtermkey.rb
@@ -1,8 +1,8 @@
 class Libtermkey < Formula
   desc "Library for processing keyboard entry from the terminal"
   homepage "http://www.leonerd.org.uk/code/libtermkey/"
-  url "http://www.leonerd.org.uk/code/libtermkey/libtermkey-0.18.tar.gz"
-  sha256 "239746de41c845af52bb3c14055558f743292dd6c24ac26c2d6567a5a6093926"
+  url "http://www.leonerd.org.uk/code/libtermkey/libtermkey-0.19.tar.gz"
+  sha256 "c505aa4cb48c8fa59c526265576b97a19e6ebe7b7da20f4ecaae898b727b48b7"
 
   bottle do
     cellar :any
