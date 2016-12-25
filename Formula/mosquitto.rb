151586c69c7a11194111727208fddba23d8340b4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-25 03:42:14 +0000

    mosquitto: update 1.4.10_1 bottle.

--- Formula/mosquitto.rb
+++ Formula/mosquitto.rb
@@ -6,10 +6,9 @@ class Mosquitto < Formula
   revision 1
 
   bottle do
-    sha256 "7785ee99e6ecdeea449194b75d9a3e0b53ee1e7cb63547ab09569483f12bc903" => :sierra
-    sha256 "f8280108975c87e1500129e747f6bffbae0b2801d0361b298d2cd2da6b23966f" => :el_capitan
-    sha256 "9ee968068aa301fad671fb753aeebd16402b246af42c1615a8fb0fde9c29194c" => :yosemite
-    sha256 "03ed2ccbe913b4df166683c8285770a8ef6dbb7e1445f1ab09263f882e1e0ac1" => :mavericks
+    sha256 "a046aab3261d705d49201f28c7475d232fac3911abfe74f798bb5ebfb2fb726c" => :sierra
+    sha256 "789d92c8c59f88597e1bc8360fd4baa73e21e517b84e5a6d54ea245ae9dcf480" => :el_capitan
+    sha256 "a216efeefe34baf53593bc9ae940a5898d6636586f7ba43f1eeaa8524ce4378d" => :yosemite
   end
 
   depends_on "pkg-config" => :build
