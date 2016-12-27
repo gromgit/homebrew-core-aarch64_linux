e14b8b4cbc62bfc8a2176d2dc44f70a09283a693
ilovezfs <ilovezfs@icloud.com>
2016-12-26 23:05:56 -0800

    cadubi 1.3.3
    Closes #8242.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/cadubi.rb
+++ Formula/cadubi.rb
@@ -1,8 +1,8 @@
 class Cadubi < Formula
   desc "Creative ASCII drawing utility"
   homepage "https://github.com/statico/cadubi/"
-  url "https://github.com/statico/cadubi/archive/v1.3.1.tar.gz"
-  sha256 "162c3ba748bbd2ab1699c95d4ad0e257ffe183959e6ce084ab91efbd3eb73f8a"
+  url "https://github.com/statico/cadubi/archive/v1.3.3.tar.gz"
+  sha256 "79af56d5d659e28306ea07741e3ad97d5f0e6e9db7a5b0b632a9c21b87f6324a"
 
   bottle :unneeded
 
@@ -11,12 +11,6 @@ class Cadubi < Formula
     bin.install "cadubi"
     doc.install "help.txt"
     man1.install "cadubi.1"
-  end
-
-  # Fix incompatibilities with perl 5.22
-  patch do
-    url "https://github.com/statico/cadubi/commit/f079b6eb666d9930abad825ee5c0720415a93b9e.patch"
-    sha256 "95c54ddc6f35a37cb09fa661da2de29c4346aebf6944b15a8e8a27ecf7a4ceb5"
   end
 
   test do
