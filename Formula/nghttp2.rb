fb0bd97f71dc4d008e2948b71758be07c0a3a344
Tomasz Pajor <tomek@polishgeeks.com>
2016-12-27 20:33:51 +0100

    nghttp2 1.18.0
    Closes #8253.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/nghttp2.rb
+++ Formula/nghttp2.rb
@@ -1,8 +1,8 @@
 class Nghttp2 < Formula
   desc "HTTP/2 C Library"
   homepage "https://nghttp2.org/"
-  url "https://github.com/nghttp2/nghttp2/releases/download/v1.17.0/nghttp2-1.17.0.tar.xz"
-  sha256 "6bb446e6977f1daac96b94cd983bab15172728342f7e2b9b0f855e1532dd2ed2"
+  url "https://github.com/nghttp2/nghttp2/releases/download/v1.18.0/nghttp2-1.18.0.tar.xz"
+  sha256 "5e5620e103f9239c0758e0fbfcf9bc04744794c1ce7a415583fbd4c2671a4499"
 
   bottle do
     sha256 "f783e7baebda5734a8ad3be7b9428ec1744cb58dcaef2e44be726c8c47faa746" => :sierra
@@ -27,6 +27,7 @@ class Nghttp2 < Formula
   depends_on "libxml2" if MacOS.version <= :lion
   depends_on "pkg-config" => :build
   depends_on "cunit" => :build
+  depends_on "c-ares"
   depends_on "libev"
   depends_on "openssl"
   depends_on "libevent"
@@ -36,8 +37,8 @@ class Nghttp2 < Formula
   depends_on "jemalloc" => :recommended
 
   resource "Cython" do
-    url "https://pypi.python.org/packages/c6/fe/97319581905de40f1be7015a0ea1bd336a756f6249914b148a17eefa75dc/Cython-0.24.1.tar.gz"
-    sha256 "84808fda00508757928e1feadcf41c9f78e9a9b7167b6649ab0933b76f75e7b9"
+    url "https://files.pythonhosted.org/packages/b7/67/7e2a817f9e9c773ee3995c1e15204f5d01c8da71882016cac10342ef031b/Cython-0.25.2.tar.gz"
+    sha256 "f141d1f9c27a07b5a93f7dc5339472067e2d7140d1c5a9e20112a5665ca60306"
   end
 
   # https://github.com/tatsuhiro-t/nghttp2/issues/125
