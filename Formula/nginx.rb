9558e47dd85a5e54ca6f83564f386dc87c9a5b76
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2016-12-27 20:12:56 +0200

    nginx 1.11.8 (devel)
    Closes #8252.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/nginx.rb
+++ Formula/nginx.rb
@@ -14,8 +14,8 @@ class Nginx < Formula
   end
 
   devel do
-    url "https://nginx.org/download/nginx-1.11.7.tar.gz"
-    sha256 "0d55beb52b2126a3e6c7ae40f092986afb89d77b8062ca0974512b8c76d9300e"
+    url "https://nginx.org/download/nginx-1.11.8.tar.gz"
+    sha256 "53aef3715d79015314c2dcb18f2b185a0c64368cc01b30bdf0737a215f666b34"
   end
 
   # Before submitting more options to this formula please check they aren't
