778f49a595775389d079726b3541d963f4db84de
Tomasz Pajor <tomek@polishgeeks.com>
2016-12-27 15:49:26 +0100

    baresip 0.5.0
    Closes #8248.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/baresip.rb
+++ Formula/baresip.rb
@@ -1,8 +1,8 @@
 class Baresip < Formula
   desc "Modular SIP useragent"
   homepage "http://www.creytiv.com/baresip.html"
-  url "http://www.creytiv.com/pub/baresip-0.4.20.tar.gz"
-  sha256 "8bcf874a0207711489998f823e77aeae59b1a579c75b923c3b4525d6dc720a55"
+  url "http://www.creytiv.com/pub/baresip-0.5.0.tar.gz"
+  sha256 "4c364f4bb2cf17f83b6ff2b35962fd94258158202dfa566f68531e8ba77c1436"
 
   bottle do
     sha256 "3afae7bb354ef15e3b277e3a5621672e3cbf0fe3ba4ba5cd4af72dc3427c376f" => :sierra
