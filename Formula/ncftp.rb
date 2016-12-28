7ab47adaf3e485f0a4e3ee4c85b3bbd738e94ad1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-28 13:18:43 +0000

    ncftp: update 3.2.6 bottle.

--- Formula/ncftp.rb
+++ Formula/ncftp.rb
@@ -5,11 +5,9 @@ class Ncftp < Formula
   sha256 "129e5954850290da98af012559e6743de193de0012e972ff939df9b604f81c23"
 
   bottle do
-    sha256 "70199e4db05dd77686e0256f0c19ac8082b8bf59ec9285df8688fb5d22640d42" => :sierra
-    sha256 "8e1bdefb84fcd91f461441d5c45d33536807b0c4fe661c626645cd9ffe04b03a" => :el_capitan
-    sha256 "50fb345b28bc8a20d2877d67108f87c9544568de9e27ae7a3545da3af3cb0b35" => :yosemite
-    sha256 "07a73c0ac7005566895f7e42f4a1d1b8295a9cdc03ec7986028ae783313e428f" => :mavericks
-    sha256 "e59965dc867f70420046475b30829cad0b334bc687e18362918fdfbab7521b59" => :mountain_lion
+    sha256 "25caf7d9c7ac3c1642d3d205fcedfbea05878798033c0ff82cc3b5fbab4674d5" => :sierra
+    sha256 "821f66bcd8991168314bb77f1404b3af6f93fda5c4fdcb3c651d3b7fbdd7f4fe" => :el_capitan
+    sha256 "ac12f87bb648eb4dde883ad5ccca0c8cf80c60e1437ce03e5fc8d768fcec1bde" => :yosemite
   end
 
   def install
