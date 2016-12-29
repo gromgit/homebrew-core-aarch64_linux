d519e05dc3352df945974f23d7c9b7a52943c901
Tom <sirtomo@gmail.com>
2016-12-29 07:02:28 -0500

    gst-plugins-bad: apple media fixed on 10.9 (#8142)
    * gst-plugins-bad.rb fixed for Mavericks
    
    apple media now working for Mavericks
    
    * gst-plugins-bad apple media now works in mavericks
    
    * updating based on feedback, thanks

--- Formula/gst-plugins-bad.rb
+++ Formula/gst-plugins-bad.rb
@@ -49,13 +49,6 @@ class GstPluginsBad < Formula
       --disable-dependency-tracking
     ]
 
-    # upstream does not support Apple video for older SDKs
-    # error: use of undeclared identifier 'AVQueuedSampleBufferRenderingStatusFailed'
-    # https://github.com/Homebrew/legacy-homebrew/pull/35284
-    if MacOS.version <= :mavericks
-      args << "--disable-apple_media"
-    end
-
     args << "--with-gtk=3.0" if build.with? "gtk+3"
 
     if build.head?
