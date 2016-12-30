e66fae3722a913802f4b259d27fb890e31a28aef
Chongyu Zhu <i@lembacon.com>
2016-12-31 06:39:57 +0800

    freetype 2.7.1
    Closes #8341.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/freetype.rb
+++ Formula/freetype.rb
@@ -1,9 +1,9 @@
 class Freetype < Formula
   desc "Software library to render fonts"
   homepage "https://www.freetype.org/"
-  url "https://downloads.sf.net/project/freetype/freetype2/2.7/freetype-2.7.tar.bz2"
-  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.7.tar.bz2"
-  sha256 "d6a451f5b754857d2aa3964fd4473f8bc5c64e879b24516d780fb26bec7f7d48"
+  url "https://downloads.sf.net/project/freetype/freetype2/2.7.1/freetype-2.7.1.tar.bz2"
+  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.7.1.tar.bz2"
+  sha256 "3a3bb2c4e15ffb433f2032f50a5b5a92558206822e22bfe8cbe339af4aa82f88"
 
   bottle do
     cellar :any
