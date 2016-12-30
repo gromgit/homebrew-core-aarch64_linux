c97f4f69158fa693c33316a34f5ca40241521a0e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-30 04:41:47 +0000

    libswiftnav: add 0.21 bottle.

--- Formula/libswiftnav.rb
+++ Formula/libswiftnav.rb
@@ -4,6 +4,13 @@ class Libswiftnav < Formula
   url "https://github.com/swift-nav/libswiftnav/archive/v0.21.tar.gz"
   sha256 "99e7ac3f190d76b2c1ceb17dae76d24b86d1a71501db4f6a49759539bb393756"
 
+  bottle do
+    cellar :any
+    sha256 "3f0e52c5a7be06dbcf140870af9ee3f74520d78a51104973d8279793f7919d1d" => :sierra
+    sha256 "c5730d05abf427a9213faf36dd13517771bf1905f8df01619932c173c581e550" => :el_capitan
+    sha256 "96e3a1e2e74ddce115e6ea88c389aa970c9acabaf6e75b3322ac0f23858fec85" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
 
