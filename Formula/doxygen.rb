685e5c9ac1237d56aa877d8f0eca3f0132775ff5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-30 17:22:41 +0000

    doxygen: update 1.8.13 bottle.

--- Formula/doxygen.rb
+++ Formula/doxygen.rb
@@ -7,10 +7,9 @@ class Doxygen < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ed81e27c7b251dda01c9d8f44657ed6508c12bb2f1703250fb40bbe9ff1392b1" => :sierra
-    sha256 "60656bd148d8a143f6feda2ed4e61a910a62788ab5177864f9e19766546f32c0" => :el_capitan
-    sha256 "9459c7ee8153939bd8ac152548d6c206d67a0aa16be2dcc61dcac616d652d019" => :yosemite
-    sha256 "7bb03f81dc587296272560b6768f6088a5ef7846b9c4975ca6a1a05742393f20" => :mavericks
+    sha256 "1a5065ab4f03154643aa0b3988ef460833898c459c1af3ba2a6e9206891e51d7" => :sierra
+    sha256 "5614c88dc28e4c3f54645e02366646a19b3dad6ff0a3fc9f9525f5495dca467d" => :el_capitan
+    sha256 "d1bd29b70a813314c2e7308d2d8e2593d22913c9a59b3a289885dfe08eba9096" => :yosemite
   end
 
   option "with-graphviz", "Build with dot command support from Graphviz."
