bc1e6059e25a6b3077b96f8fd5404bcfb9144f52
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-12-30 20:30:21 +0100

    gnome-autoar: add test, fix audit.

--- Formula/gnome-autoar.rb
+++ Formula/gnome-autoar.rb
@@ -1,12 +1,11 @@
 class GnomeAutoar < Formula
-  desc "A convenient library for archive handling"
+  desc "GNOME library for archive handling"
   homepage "https://github.com/GNOME/gnome-autoar"
   url "https://download.gnome.org/sources/gnome-autoar/0.1/gnome-autoar-0.1.1.tar.xz"
   sha256 "f65cb810b562dc038ced739fbf59739fd5df1a8e848636e21f363ded9f349ac9"
 
-  depends_on "pkg-config"
+  depends_on "pkg-config" => :build
   depends_on "libarchive"
-  depends_on "glib"
   depends_on "gtk+3"
 
   def install
@@ -14,7 +13,7 @@ class GnomeAutoar < Formula
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}",
-			  "--disable-glibtest",
+	                        "--disable-glibtest",
                           "--disable-schemas-compile"
     system "make", "install"
   end
@@ -24,6 +23,39 @@ class GnomeAutoar < Formula
   end
 
   test do
-    system "false"
+    (testpath/"test.c").write <<-EOS.undent
+      #include <gnome-autoar/gnome-autoar.h>
+
+      int main(int argc, char *argv[]) {
+        GType type = autoar_extractor_get_type();
+        return 0;
+      }
+    EOS
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    libarchive = Formula["libarchive"]
+    pcre = Formula["pcre"]
+    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
+    flags += %W[
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{include}/gnome-autoar-0
+      -I#{libarchive.opt_include}
+      -I#{pcre.opt_include}
+      -D_REENTRANT
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{libarchive.opt_lib}
+      -L#{lib}
+      -larchive
+      -lgio-2.0
+      -lglib-2.0
+      -lgnome-autoar-0
+      -lgobject-2.0
+      -lintl
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
   end
 end
