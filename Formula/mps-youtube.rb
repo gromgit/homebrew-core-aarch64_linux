3ec6ae7d85638f5fdd82dcad76ee4a94a1f82e28
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-30 22:58:07 +0000

    mps-youtube: add 0.2.7.1 bottle.

--- Formula/mps-youtube.rb
+++ Formula/mps-youtube.rb
@@ -6,6 +6,12 @@ class MpsYoutube < Formula
   url "https://github.com/mps-youtube/mps-youtube/archive/v0.2.7.1.tar.gz"
   sha256 "917958ab02f8dace9c84974f510bd8838f905814c1a05a91fb1a38d37d19f0e8"
 
+  bottle do
+    sha256 "b1fc3938f57ae10e0ca3142a609ecdd90c28cbd17b1f440e451781a548c885ef" => :sierra
+    sha256 "7b4c82c52b109ba3ede80db038296148021667e2383b6ec09734f495a52beb86" => :el_capitan
+    sha256 "952920e383831ccccdc884d681a31f8eb1d435edeaf4d713ab199a50cc91e3b5" => :yosemite
+  end
+
   depends_on "python3"
   depends_on "mpv" => :recommended
   depends_on "youtube-dl" => :recommended
