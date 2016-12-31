3668959e5e7a3730975758a1066b1e5e634cc6bd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-31 11:49:54 +0000

    flex: update 2.6.3 bottle.

--- Formula/flex.rb
+++ Formula/flex.rb
@@ -5,9 +5,9 @@ class Flex < Formula
   sha256 "68b2742233e747c462f781462a2a1e299dc6207401dac8f0bbb316f48565c2aa"
 
   bottle do
-    sha256 "c7bcd12da4584e7d59e3801f92711820f2b9223d686326693a42da7733cd408d" => :sierra
-    sha256 "b9f443e7292fe613dca088f7c4d26bf636086bee799c0dda06d8371b6702b410" => :el_capitan
-    sha256 "637020dcd2cb5895b9da6c248e6035a3cbb91e3a310b7e71cb5f9c4ae959f149" => :yosemite
+    sha256 "ab9447f77fbef802c703ad7e8ac606e217205880b55b20b90c2f58674f848162" => :sierra
+    sha256 "3f8c8003a5ae1f88cc397590c85787f1710f8798fcfd9ed8691d81f3df20e926" => :el_capitan
+    sha256 "915dad088301f2bb607c3af7a7eb13946500e1a0566767e91ecbbcaf7c813725" => :yosemite
   end
 
   keg_only :provided_by_osx, "Some formulae require a newer version of flex."
