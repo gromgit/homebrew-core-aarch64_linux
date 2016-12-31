59a31646562aa93fbb4d01ad4eeef034cf6eb5b9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-31 10:22:28 +0000

    x265: update 2.2 bottle.

--- Formula/x265.rb
+++ Formula/x265.rb
@@ -8,9 +8,9 @@ class X265 < Formula
 
   bottle do
     cellar :any
-    sha256 "a8baa8e1290de5c92396b14da4128a427ac03b2f46c5b3b2daf3786590c86405" => :sierra
-    sha256 "2149bd6737507fc48a8eb8cc9b664324f616a97a59f69884870c605452521881" => :el_capitan
-    sha256 "1caa3cdda0a9b3ff71d776358195e975d5f368894f83887e135f6b89bebd57f0" => :yosemite
+    sha256 "9ff35598401ce827ea176b7f311bb5cf2260f51007f02915fb78e86c6f9e2176" => :sierra
+    sha256 "ffaef9f463769872cbdaf57fd0323d416fa344c5e4f54f36d08bb4f5885a064e" => :el_capitan
+    sha256 "ac714ad740db69cff490c81efe5b7f46f293f0f9339280cec1b5a77bbfa255cd" => :yosemite
   end
 
   option "with-16-bit", "Build a 16-bit x265 (default: 8-bit)"
