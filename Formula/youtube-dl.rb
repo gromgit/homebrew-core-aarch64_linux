f38df92782b68a132fcb1c374f98157815291b6d
Zhiming Wang <zmwangx@gmail.com>
2016-12-31 13:10:31 -0500

    youtube-dl 2016.12.31
    Closes #8361.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -4,8 +4,9 @@
 class YoutubeDl < Formula
   desc "Download YouTube videos from the command-line"
   homepage "https://rg3.github.io/youtube-dl/"
-  url "https://github.com/rg3/youtube-dl/releases/download/2016.12.22/youtube-dl-2016.12.22.tar.gz"
-  sha256 "f5bc5eb1af17391b7a1da795f4181c3702fb2c1da2668b25f1260dbdba882524"
+  url "https://github.com/rg3/youtube-dl/releases/download/2016.12.31/youtube-dl-2016.12.31.tar.gz"
+  sha256 "94d9f6cb99a1f5c27a8885f1bffe1f36c7e89feef961a83f78d8093284cf1ec9"
+  head "https://github.com/rg3/youtube-dl.git"
 
   bottle do
     cellar :any_skip_relocation
@@ -14,11 +15,7 @@ class YoutubeDl < Formula
     sha256 "f35411edcc00d23b4a488b49af6fe688d5cc2286a7951ecf9e6bc96af87ce741" => :yosemite
   end
 
-  head do
-    url "https://github.com/rg3/youtube-dl.git"
-    depends_on "pandoc" => :build
-  end
-
+  depends_on "pandoc" => :build
   depends_on "rtmpdump" => :optional
 
   def install
