f3ec7987398861c9b300174218f53e1c251fbf13
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-31 07:29:27 +0000

    ucg: add 0.3.3 bottle.

--- Formula/ucg.rb
+++ Formula/ucg.rb
@@ -5,6 +5,13 @@ class Ucg < Formula
   sha256 "116d832bbc743c7dd469e5e7f1b20addb3b7a08df4b4441d59da3acf221caf2d"
   head "https://github.com/gvansickle/ucg.git"
 
+  bottle do
+    cellar :any
+    sha256 "cf699c7e60e56c4d1880813071fec45f66d001f36debd1e72682bb19da2d7f67" => :sierra
+    sha256 "f98c5f7d7c4501f2628caeef25adbd7caa0c0185af42fed9a7c9e7676557d747" => :el_capitan
+    sha256 "b5eb0d7540c6a4e64f6fedb293eb6476bcd33c4eab64fc5c3d0496a5e35b7b6b" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "autoconf" => :build
   depends_on "libtool" => :build
