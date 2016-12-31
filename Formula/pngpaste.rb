c065521066f737eee433274c6035d831dd2d34ab
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-31 12:53:07 +0000

    pngpaste: update 0.2.2 bottle.

--- Formula/pngpaste.rb
+++ Formula/pngpaste.rb
@@ -6,11 +6,8 @@ class Pngpaste < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "263e84687d3da619b7349b1295638675719531822dece2b5e1e55a82930900dd" => :sierra
-    sha256 "bfb5fd80a48aa24949ef9551f12144afeef8cbc54a04db47476754681dbcac2d" => :el_capitan
-    sha256 "e134f7bca93ef8e51d1729c145b060f688999f6d3eeda051f836831c6774c2ef" => :yosemite
-    sha256 "eeea5b97cd20b83f7d01fa73d9b8bc98f776dc122769cc682312981dde54b07e" => :mavericks
-    sha256 "c8577460546d1c8f7f4cf9e833a420989b1fb94969dc1fcffd1c8f6d07a78e93" => :mountain_lion
+    sha256 "53dcb74c9ae6f97470adda82a565e70b821879dd02733aa404a8c59db49eff79" => :sierra
+    sha256 "8dec6973a1c579264b4832dd6b766c5e1ce344b486bf2c302b47a299a14e6952" => :el_capitan
   end
 
   depends_on :macos => :el_capitan # needs NSBitmapImageFileTypePNG, etc.
