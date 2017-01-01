3e4cbc068d1dc6dfdd71d08b1a1bc0227eac9dcd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-01 10:24:06 +0000

    gnome-autoar: add 0.1.1 bottle.

--- Formula/gnome-autoar.rb
+++ Formula/gnome-autoar.rb
@@ -4,6 +4,12 @@ class GnomeAutoar < Formula
   url "https://download.gnome.org/sources/gnome-autoar/0.1/gnome-autoar-0.1.1.tar.xz"
   sha256 "f65cb810b562dc038ced739fbf59739fd5df1a8e848636e21f363ded9f349ac9"
 
+  bottle do
+    sha256 "45812c9e3e758dd5d6e5fb4357a4e6b35b1de7aeb7a46b821dbc326a6506f440" => :sierra
+    sha256 "4df813bf0adedd8b079991de35e2efa8def78640c959b24840c12c06f9463298" => :el_capitan
+    sha256 "ce143626e83d030de0fae3d6257301c3ec0c7ff8230bce78422178fe8b97ff54" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libarchive"
   depends_on "gtk+3"
