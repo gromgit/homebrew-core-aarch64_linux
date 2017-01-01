d8e8a182531eafa738de21f63b472822e7f436dd
miccal <miccal.matthews@gmail.com>
2017-01-01 22:43:09 +0800

    pdftoipe: migrate from homebrew/homebrew-head-only
    Closes #8373.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Formula/pdftoipe.rb
@@ -0,0 +1,23 @@
+class Pdftoipe < Formula
+  desc "Reads arbitrary PDF files and generates an XML file readable by Ipe"
+  homepage "https://github.com/otfried/ipe-tools"
+  url "https://github.com/otfried/ipe-tools/archive/v7.2.7.tar.gz"
+  sha256 "889cb31bd8769ba111f541ba795cf53fad474aeeafbc87b7cd37c8a24b2dc6f6"
+
+  depends_on "pkg-config" => :build
+  depends_on "poppler"
+
+  def install
+    cd "pdftoipe" do
+      system "make"
+      bin.install "pdftoipe"
+      man1.install "pdftoipe.1"
+    end
+  end
+
+  test do
+    cp test_fixtures("test.pdf"), testpath
+    system bin/"pdftoipe", "test.pdf"
+    assert_match "Homebrew test.</text>", File.read("test.ipe")
+  end
+end
