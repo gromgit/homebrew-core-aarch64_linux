667247049cda62202018524b3980b9955189472a
Tomasz Pajor <tomek@polishgeeks.com>
2017-01-02 12:56:32 +0100

    astyle 2.06
    Closes #8394.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/astyle.rb
+++ Formula/astyle.rb
@@ -1,8 +1,8 @@
 class Astyle < Formula
   desc "Source code beautifier for C, C++, C#, and Java"
   homepage "http://astyle.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/astyle/astyle/astyle%202.05.1/astyle_2.05.1_macosx.tar.gz"
-  sha256 "de66da286dee2b9de1dc1c05092cbf5368c0889f25d1e2ee8b51766aff8e4baf"
+  url "https://downloads.sourceforge.net/project/astyle/astyle/astyle%202.06/astyle_2.06_macos.tar.gz"
+  sha256 "ad26b437365add1ec718b0f5f7c03ef0297616528619c2d1de19e940cd18d88a"
   head "svn://svn.code.sf.net/p/astyle/code/trunk/AStyle"
 
   bottle do
