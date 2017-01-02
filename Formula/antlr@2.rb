6cf719f4eb2048b43b697f73dfa52b5dd7678dd4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-02 20:00:35 +0000

    antlr@2: add 2.7.7 bottle.

--- Formula/antlr@2.rb
+++ Formula/antlr@2.rb
@@ -4,6 +4,13 @@ class AntlrAT2 < Formula
   url "http://www.antlr2.org/download/antlr-2.7.7.tar.gz"
   sha256 "853aeb021aef7586bda29e74a6b03006bcb565a755c86b66032d8ec31b67dbb9"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ec2e5dacbcbc0463cec0876b164de6f40b75443f51070b5c31755acf2ad6ffd4" => :sierra
+    sha256 "3c340537a171cdf7c87788cd6e507a403decaf864dc81249a2da01e4bac5b3f7" => :el_capitan
+    sha256 "90b75cee100dd1f98e50d3c858b5a54c5c676dca7fd22c81863be76504777180" => :yosemite
+  end
+
   depends_on :java
 
   def install
