d117ccf19975dcc65b8af2033397be8751a42f30
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-02 20:00:35 +0000

    autoconf@2.13: add 2.13 bottle.

--- Formula/autoconf@2.13.rb
+++ Formula/autoconf@2.13.rb
@@ -5,6 +5,12 @@ class AutoconfAT213 < Formula
   mirror "https://ftp.gnu.org/gnu/autoconf/autoconf-2.13.tar.gz"
   sha256 "f0611136bee505811e9ca11ca7ac188ef5323a8e2ef19cffd3edb3cf08fd791e"
 
+  bottle do
+    sha256 "1631e2f720355ef11b150ebddde824c02eab44a336203bcdb5282e4784ecbcd9" => :sierra
+    sha256 "f6b148c2bcf08f0e143a4757fa6784bf9e0d780d5ca8ec8cd97c042316c50d84" => :el_capitan
+    sha256 "f6b148c2bcf08f0e143a4757fa6784bf9e0d780d5ca8ec8cd97c042316c50d84" => :yosemite
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
