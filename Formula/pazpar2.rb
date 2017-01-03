d062d21a365cb5b3a6d06c5ae446981824f98422
Sven-S. Porst <ssp-web@earthlingsoft.net>
2017-01-04 00:37:41 +0100

    pazpar2: restore proper test
    This was removed in dacab6311c as running pazpar2
    with the -t option caused a crash at the time.
    
    This issue has been fixed in pazpar2 by now, so we
    can restore the test we previously had.
    
    Closes #8459.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/pazpar2.rb
+++ Formula/pazpar2.rb
@@ -30,6 +30,16 @@ class Pazpar2 < Formula
   end
 
   test do
-    system "#{sbin}/pazpar2", "-V"
+    (testpath/"test-config.xml").write <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <pazpar2 xmlns="http://www.indexdata.com/pazpar2/1.0">
+      <threads number="2"/>
+      <server>
+        <listen port="8004"/>
+      </server>
+    </pazpar2>
+    EOS
+
+    system "#{sbin}/pazpar2", "-t", "-f", "#{testpath}/test-config.xml"
   end
 end
