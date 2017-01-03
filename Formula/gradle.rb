74edf375423cbdf042bcab464e65f09426c0190f
Kenzie Togami <ket1999@gmail.com>
2017-01-03 15:47:26 -0800

    gradle 3.3
    Closes #8458.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/gradle.rb
+++ Formula/gradle.rb
@@ -1,12 +1,12 @@
 class Gradle < Formula
   desc "Build system based on the Groovy language"
   homepage "https://www.gradle.org/"
-  url "https://downloads.gradle.org/distributions/gradle-3.2.1-all.zip"
-  sha256 "0209696f1723f607c475109cf3ed8b51c8a91bb0cda05af0d4bd980bdefe75cd"
-
-  option "with-all", "Installs Javadoc, examples, and source in addition to the binaries"
+  url "https://downloads.gradle.org/distributions/gradle-3.3-all.zip"
+  sha256 "71a787faed83c4ef21e8464cc8452b941b5fcd575043aa29d39d15d879be89f7"
 
   bottle :unneeded
+
+  option "with-all", "Installs Javadoc, examples, and source in addition to the binaries"
 
   depends_on :java => "1.7+"
 
