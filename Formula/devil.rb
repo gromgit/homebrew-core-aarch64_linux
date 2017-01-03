756c8ecd2943c10654d15336beb386e58a6bcaf2
ilovezfs <ilovezfs@icloud.com>
2017-01-03 06:21:26 -0800

    devil 1.8.0

--- Formula/devil.rb
+++ Formula/devil.rb
@@ -1,18 +1,10 @@
 class Devil < Formula
   desc "Cross-platform image library"
   homepage "https://sourceforge.net/projects/openil/"
-  revision 3
+  url "https://downloads.sourceforge.net/project/openil/DevIL/1.8.0/DevIL-1.8.0.tar.gz"
+  sha256 "0075973ee7dd89f0507873e2580ac78336452d29d34a07134b208f44e2feb709"
+  head "https://github.com/DentonW/DevIL.git"
 
-  stable do
-    url "https://downloads.sourceforge.net/project/openil/DevIL/1.7.8/DevIL-1.7.8.tar.gz"
-    sha256 "682ffa3fc894686156337b8ce473c954bf3f4fb0f3ecac159c73db632d28a8fd"
-
-    # fix compilation issue for ilur.c
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/3db2f9727cea4a51fbcfae742518c614020fb8f2/devil/patch-src-ILU-ilur-ilur.c.diff"
-      sha256 "ce96bc4aad940b80bc918180d6948595ee72624ae925886b1b770f2a7be8a2f9"
-    end
-  end
   bottle do
     cellar :any
     sha256 "4d26759aad301828515cd26de8a58e712fa23bd746949306f1c88b1eb4b7af27" => :sierra
@@ -21,85 +13,33 @@ class Devil < Formula
     sha256 "5d77a3dcfed8123738dbfa1d77ea6dd2459688b0acbc35a0fd5dfe4b2cf0fead" => :mavericks
   end
 
-  head do
-    url "https://github.com/DentonW/DevIL.git"
-
-    option "with-ilut", "Also build the ILUT library"
-
-    depends_on "libtool" => :build
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "homebrew/x11/freeglut" if build.with? "ilut"
-
-    # fix compilation issue for ilur.c
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/3db2f9727cea4a51fbcfae742518c614020fb8f2/devil/patch-DevIL-src-ILU-ilur-ilur.c.diff"
-      sha256 "8021ffcd5c9ea151b991c7cd29b49ecea14afdfe07cb04fa9d25ab07d836f7d0"
-    end
-  end
-
   option :universal
 
-  depends_on "gcc"
+  depends_on "cmake" => :build
+  depends_on "jasper"
   depends_on "jpeg"
   depends_on "libpng"
-  depends_on "libtiff" => :recommended
-
-  # most compilation issues with clang are fixed in the following pull request
-  # see https://github.com/DentonW/DevIL/pull/30
-  # see https://sourceforge.net/p/openil/bugs/204/
-  # also, even with -std=gnu99 removed from the configure script,
-  # devil fails to build with clang++ while compiling il_exr.cpp
-  fails_with :clang do
-    cause "invalid -std=gnu99 flag while building C++"
-  end
+  depends_on "libtiff"
+  depends_on "little-cms2"
 
   def install
     ENV.universal_binary if build.universal?
-
-    if build.head?
-      cd "DevIL"
-      system "./autogen.sh"
+    cd "DevIL" do
+      system "cmake", ".", *std_cmake_args
+      system "make", "install"
     end
-
-    # GCC 5 build failure: https://github.com/Homebrew/legacy-homebrew/issues/40442
-    # Reported 4th May 2016: https://github.com/DentonW/DevIL/issues/31
-    # Fix is from NetBSD: http://cvsweb.netbsd.org/bsdweb.cgi/pkgsrc/devel/devIL/patches/patch-include_IL_il.h
-    inreplace "include/IL/il.h",
-      "#ifdef RESTRICT_KEYWORD",
-      "#if defined(RESTRICT_KEYWORD) && !defined(__cplusplus)"
-
-    args = %W[
-      --disable-debug
-      --disable-dependency-tracking
-      --prefix=#{prefix}
-      --enable-ILU
-    ]
-    args << "--enable-ILUT" if build.stable? || build.with?("ilut")
-    args << "--disable-tiff" if build.without? "libtiff"
-
-    # "fatal error: 'IL/ilut.h' file not found"
-    # Reported 4th May 2016: https://github.com/DentonW/DevIL/issues/32
-    # Fixes the test for HEAD builds that have ILUT disabled
-    if build.without? "ilut"
-      inreplace "include/IL/devil_cpp_wrapper.hpp",
-        "<IL/ilut.h>", "\"ilu.h\""
-    end
-
-    system "./configure", *args
-    system "make", "install"
   end
 
   test do
     (testpath/"test.cpp").write <<-EOS.undent
-      #include <IL/devil_cpp_wrapper.hpp>
+      #include <IL/il.h>
       int main() {
-        ilImage image;
+        ilInit();
         return 0;
       }
     EOS
-
-    system ENV.cxx, "test.cpp", "-L#{lib}", "-lIL", "-lILU", "-o", "test"
+    system ENV.cxx, "test.cpp", "-o", "test", "-I#{include}",
+                    "-L#{lib}", "-lIL", "-lILU"
     system "./test"
   end
 end
