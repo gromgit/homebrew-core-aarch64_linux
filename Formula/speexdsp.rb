88b4075a040a1a6d2b0237aa0f894a04ee554b33
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-03 20:14:28 +0000

    speexdsp: update 1.2rc3 bottle.

--- Formula/speexdsp.rb
+++ Formula/speexdsp.rb
@@ -5,6 +5,10 @@ class Speexdsp < Formula
   sha256 "e8be7482df7c95735e5466efb371bd7f21115f39eb45c20ab7264d39c57b6413"
 
   bottle do
+    cellar :any
+    sha256 "367e34d4ba6e7087762193a18910da38905496ec41c0f329604d310a09e4f5ed" => :sierra
+    sha256 "4891bd2a89fc9369d9c573afd80a1e7593e9414a0d6a956921af1b65f61ab264" => :el_capitan
+    sha256 "34a342effdc414829a7063d4b8dc7f2bbfa2d37231904f4fa6784c38d90bdb3d" => :yosemite
   end
 
   depends_on "autoconf" => :build
