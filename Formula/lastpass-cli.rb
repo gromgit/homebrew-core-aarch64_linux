1a5176800d1799bb447d3816f2ffc3e53bd421b8
Bob Copeland <copeland@lastpass.com>
2017-01-03 12:45:39 -0500

    lastpass-cli: head depends on cmake
    lastpass-cli (HEAD) build now uses cmake.  There is a top-level
    Makefile which emulates most of previous targets, so no other
    changes are needed to the actual build steps.
    
    Closes #8445.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/lastpass-cli.rb
+++ Formula/lastpass-cli.rb
@@ -3,7 +3,6 @@ class LastpassCli < Formula
   homepage "https://github.com/lastpass/lastpass-cli"
   url "https://github.com/lastpass/lastpass-cli/archive/v1.0.0.tar.gz"
   sha256 "42096c0bd3972b0e9cc9cef32fbf141e47b04b9e2387fb3abe8b105e135fb41e"
-  head "https://github.com/lastpass/lastpass-cli.git"
 
   bottle do
     cellar :any
@@ -11,6 +10,12 @@ class LastpassCli < Formula
     sha256 "42710f8cf1bef7628e61251e10bd0bf2a36f219e950fd40fb9d93561f87f4335" => :el_capitan
     sha256 "6d8a04d5bab5db00b80f5d6a35c639040ca0494e81ee85e7ae21bb52d8e617b8" => :yosemite
     sha256 "dc2eb72ebe79a0963dc9cae50ec6a38633740866b356e7521f37bf1c586a37f0" => :mavericks
+  end
+
+  head do
+    url "https://github.com/lastpass/lastpass-cli.git"
+
+    depends_on "cmake" => :build
   end
 
   option "with-doc", "Install man pages"
