307341b2a1d860dab87b23858278b0a0d8f79775
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-03 19:11:10 +0000

    swiftgen: update 4.1.0 bottle.

--- Formula/swiftgen.rb
+++ Formula/swiftgen.rb
@@ -7,8 +7,8 @@ class Swiftgen < Formula
 
   bottle do
     cellar :any
-    sha256 "9e61be0768fe063354824382aba8759e3635f3b916df667729abf560776fb4a5" => :sierra
-    sha256 "90c013628201d4a6a3bd9bbd139012978b4e4520b5d11b84aedde10d3395223f" => :el_capitan
+    sha256 "bcbcb8459251dd3e9813489279bf85d73b16fbcdf976d674578445c5aa3cc87d" => :sierra
+    sha256 "44b2e03994449b9326a38333a9e622bf15a2ce36c59b3c769a482f4ca7236f8c" => :el_capitan
   end
 
   depends_on :xcode => ["8.0", :build]
