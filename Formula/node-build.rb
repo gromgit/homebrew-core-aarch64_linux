49c69c7409d48e788e7d50a002f57578a51f4047
Jason Karns <jason@karns.name>
2017-01-04 18:09:15 -0500

    node-build 2.5.1
    Closes #8500.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/node-build.rb
+++ Formula/node-build.rb
@@ -1,8 +1,8 @@
 class NodeBuild < Formula
   desc "Install NodeJS versions"
   homepage "https://github.com/nodenv/node-build"
-  url "https://github.com/nodenv/node-build/archive/v2.5.0.tar.gz"
-  sha256 "d55ada37c9fc06231927e899168bfab45062544f303f3c69958787e3df842bf7"
+  url "https://github.com/nodenv/node-build/archive/v2.5.1.tar.gz"
+  sha256 "2fbbcecd8b52706c154f5f21798e9a2197b80748759f4dcc1fe4c50e1283a198"
   head "https://github.com/nodenv/node-build.git"
 
   bottle :unneeded
