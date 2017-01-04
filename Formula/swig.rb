3aa86aaa3f3547fe3acb6c1fbf43731ff0c52ce4
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-01-04 09:29:08 +0000

    swig 3.0.11
    Closes #8466.
    
    Signed-off-by: Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>

--- Formula/swig.rb
+++ Formula/swig.rb
@@ -1,9 +1,8 @@
 class Swig < Formula
   desc "Generate scripting interfaces to C/C++ code"
   homepage "http://www.swig.org/"
-  url "https://downloads.sourceforge.net/project/swig/swig/swig-3.0.10/swig-3.0.10.tar.gz"
-  sha256 "2939aae39dec06095462f1b95ce1c958ac80d07b926e48871046d17c0094f44c"
-  revision 1
+  url "https://downloads.sourceforge.net/project/swig/swig/swig-3.0.11/swig-3.0.11.tar.gz"
+  sha256 "d9031d531d7418829a54d0d51c4ed9007016b213657ec70be44031951810566e"
 
   bottle do
     sha256 "0b5ade8c002e0f28c6e6f2d7643119138a8cc75d1363809f59e042c4fb54c6b7" => :sierra
@@ -14,13 +13,6 @@ class Swig < Formula
   option :universal
 
   depends_on "pcre"
-
-  # Remove for > 3.0.10
-  # Upstream fix for build failures caused by generated SWIG code for R
-  patch do
-    url "https://github.com/swig/swig/commit/1fcbf07.patch"
-    sha256 "ceaa4e6c59f6dce2036c35612adfb752dd7957c14d5ecfd91e6b508905944833"
-  end
 
   def install
     ENV.universal_binary if build.universal?
@@ -50,7 +42,7 @@ class Swig < Formula
     system "#{bin}/swig", "-ruby", "test.i"
     system ENV.cc, "-c", "test.c"
     system ENV.cc, "-c", "test_wrap.c", "-I/System/Library/Frameworks/Ruby.framework/Headers/"
-    system ENV.cc, "-bundle", "-flat_namespace", "-undefined", "suppress", "test.o", "test_wrap.o", "-o", "test.bundle"
+    system ENV.cc, "-bundle", "-undefined", "dynamic_lookup", "test.o", "test_wrap.o", "-o", "test.bundle"
     assert_equal "2", shell_output("ruby run.rb").strip
   end
 end
