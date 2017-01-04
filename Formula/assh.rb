4a828ef0b53b93f5f2c82570d6338b8fd863c7ba
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-04 19:53:25 +0000

    assh: update 2.5.0 bottle.

--- Formula/assh.rb
+++ Formula/assh.rb
@@ -6,11 +6,9 @@ class Assh < Formula
   head "https://github.com/moul/advanced-ssh-config.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "28e49700c458e4c8df4e9342a927bc1cbc09877cf762a07d4ee7893b1c47393d" => :sierra
-    sha256 "f2a3fa0bc38c3ddc7cf442fbd4114bbc49a4753bbe9963502b46be0825212f19" => :el_capitan
-    sha256 "75876951c5bc5706898141f4591fdb1f807ddd6849ae1bd2e75dc3dcdc0baa13" => :yosemite
-    sha256 "88a5f8d738264b099333ec4e4fdbcbdf5aada82a3b788a5086b825769036fa23" => :mavericks
+    sha256 "53a8c1dcf2feacd3ee1c2c0c876b236605e2475b3d110430c3fe79e0011d488e" => :sierra
+    sha256 "6c476bf520513ccdf0a48956430c602d98bc7490eb2e40d5f6b8a2edbc583061" => :el_capitan
+    sha256 "608d04ba760964a018aeb8386feddf3bc18e78708aa1f862f9dd10b41e69f757" => :yosemite
   end
 
   depends_on "go" => :build
