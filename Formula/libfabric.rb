562550f83e84a34cb3b763f99ac0a94c11983ee8
ilovezfs <ilovezfs@icloud.com>
2017-01-04 21:48:33 -0800

    libfabric 1.4.0
    Closes #6551.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libfabric.rb
+++ Formula/libfabric.rb
@@ -1,8 +1,18 @@
 class Libfabric < Formula
   desc "OpenFabrics libfabric"
   homepage "https://ofiwg.github.io/libfabric/"
-  url "https://github.com/ofiwg/libfabric/releases/download/v1.3.0/libfabric-1.3.0.tar.bz2"
-  sha256 "0a0d4f1a0d178d80ec336763a0fd371ade97199d6f1e884ef8f0e6bc99f258c9"
+  head "https://github.com/ofiwg/libfabric.git"
+
+  stable do
+    url "https://github.com/ofiwg/libfabric/releases/download/v1.4.0/libfabric-1.4.0.tar.bz2"
+    sha256 "e9f449a137d2f1713ffc970f80a9a8c1fd2970f74f1d118941eafd4b2021bc94"
+
+    # Upstream commit from 19 Nov 2016 "core: remove use of clock_gettime(3)"
+    patch do
+      url "https://github.com/ofiwg/libfabric/commit/0b0c889.patch"
+      sha256 "9ed89d80a2edccc84d157e7fa41159e461e8ebefd717db99123b2323df9ae0aa"
+    end
+  end
 
   bottle do
     sha256 "53984f1dac904f22ac42ac6855a1ab61ff0433ac42b499048b47a2ddd66da9bc" => :el_capitan
@@ -10,16 +20,12 @@ class Libfabric < Formula
     sha256 "2a3136934b1099fb3288e4283bab1ec2972eb002539e35620dc40d0a00c956bf" => :mavericks
   end
 
-  head do
-    url "https://github.com/ofiwg/libfabric.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool"  => :build
-  end
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool"  => :build
 
   def install
-    system "./autogen.sh" if build.head?
+    system "autoreconf", "-fiv"
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
