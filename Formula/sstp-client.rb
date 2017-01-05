8b6461f54c7f7ac189ee99df84665b9788fdbb99
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-05 04:29:49 +0000

    sstp-client: update 1.0.11 bottle.

--- Formula/sstp-client.rb
+++ Formula/sstp-client.rb
@@ -5,11 +5,9 @@ class SstpClient < Formula
   sha256 "1b851b504030ed5522ced431217a5c700b35e8bb72d6f5b40b006c7becb8fb20"
 
   bottle do
-    cellar :any
-    sha256 "fd3e7126998bbb07c0727b550b63322fe1eed186f92d4ae446133b427b2e911a" => :sierra
-    sha256 "d70a56f837f32f2ceab2050fc32855428f6c1df368da447491107dd3c80c030b" => :el_capitan
-    sha256 "8c4a3b20b2b92a4dfca222ecb3ccf46a1c20c8cdbcdab60fe6907e81f039927b" => :yosemite
-    sha256 "5a07e517c449759938b41c886203fed7294667ba3bc185924e26b7d94267a92f" => :mavericks
+    sha256 "1fc1e56346f4cb8f35effd7986e22a13f9f2236448c44f479a8118f5600e6bc1" => :sierra
+    sha256 "f3c353fbd733524d34ef2448c30e3064391ed4b13b4e0181fee825912a9c3977" => :el_capitan
+    sha256 "faeff7f1d82cafa3e463451e38320fcc9461d24b1122a0cbcc4f2e2b93b3876f" => :yosemite
   end
 
   depends_on "pkg-config" => :build
