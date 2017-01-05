adf1a90795a656bf98d18a04d904b78da723aa1f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-05 11:09:13 +0000

    gtk+3: update 3.22.6 bottle.

--- Formula/gtk+3.rb
+++ Formula/gtk+3.rb
@@ -5,9 +5,9 @@ class Gtkx3 < Formula
   sha256 "eba75a216a117f4391beb2971ba20ff8a1823f109893f0ab6c2eac2210ea172f"
 
   bottle do
-    sha256 "3e379ca1f54f09352a4404d5b2dbf2052f074fe329c7cedd5f860350e2d9b6b2" => :sierra
-    sha256 "8c948037023601dcab74b53a16be91b66171f619d097b843418c74b967b285b4" => :el_capitan
-    sha256 "9edcd14868f2b6948df6a2b2009b6c87d734c0a528c1c1860bd33706e79a90f4" => :yosemite
+    sha256 "7f0655b39fc517d4971053d95e12b897188e3360e63c03061e4e4b422dfe5529" => :sierra
+    sha256 "b6f9787c76347d15bbe00d6a09dbadf82807b880bef8b5b22212bc72949a6efc" => :el_capitan
+    sha256 "8f0a001d19fe9b49dc1bbfabdc6827fc49421a2cc475c9941948455721b1dfaf" => :yosemite
   end
 
   option :universal
