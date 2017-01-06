95a67f18bad41af4fba7ed84670a7bc55226d02a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-06 01:29:28 +0000

    berkeley-db: update 6.2.23 bottle.

--- Formula/berkeley-db.rb
+++ Formula/berkeley-db.rb
@@ -6,11 +6,9 @@ class BerkeleyDb < Formula
 
   bottle do
     cellar :any
-    sha256 "9aeac35b89e11e5b1eec4e5f17534523b64e05117471cb2121a58ca0f3bda5db" => :sierra
-    sha256 "70f4c1cea6a4c2f9454680988b73bb9fdc107307dafeec47e21a4539dce57b36" => :el_capitan
-    sha256 "3e4324500931e32e3a187f9790b4465efd4d249dc9ec03ff46b4209ed061e935" => :yosemite
-    sha256 "b2ba3aff83ee27a52115f47abc3c1767d124841ad342e63e433dfa36af064b36" => :mavericks
-    sha256 "8f5a87bc3336e01f8528ae8aaae24c83e8fa10e8f01ee65e8cac4af7d0786bf1" => :mountain_lion
+    sha256 "3c2ad512ec0ecb113c966f9a7cfb06e100cb36e9a9b1698808f31b6c43f37ab6" => :sierra
+    sha256 "bb75788493c5a0c8bdb5225b571864f82601d3d4974ae38e5ce7e239f9fb24e3" => :el_capitan
+    sha256 "f194651ba24b94d97ff43629b05d601892f1d7ab87a32184c0e848f1dffaacde" => :yosemite
   end
 
   depends_on :java => [:optional, :build]
