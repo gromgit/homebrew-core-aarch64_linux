24e856427ddd5c99c1c2c298b2d17dd73b4e3b35
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-06 01:29:29 +0000

    moc: update 2.5.2_1 bottle.

--- Formula/moc.rb
+++ Formula/moc.rb
@@ -6,9 +6,9 @@ class Moc < Formula
   revision 1
 
   bottle do
-    sha256 "c6441a6396a213644e39dae1bf920a7b773bb7c4730bfd1e751dd480d0bb80a0" => :sierra
-    sha256 "db6e67d930ebfb2c414c834f67a8a228bf0300d8025df34fd57dccc5ffd84bd3" => :el_capitan
-    sha256 "2ad36030c2731d46aa672e927a957843cbbbefcb1bc51fdb91c973e1e3a18a66" => :yosemite
+    sha256 "310b5d45eba637fd748a1b83688d68c5fc280ac6e8457dbb278d9d5ed9e6487b" => :sierra
+    sha256 "68d17bf31ee1e3632d8b33ec87ab1604c3bb28c7cefa61811328b98ae41d6247" => :el_capitan
+    sha256 "a2aa1968a00d16c9225d7b6eb0ae8fa4dba3826cc710e05da06321d72b99cd1b" => :yosemite
   end
 
   devel do
