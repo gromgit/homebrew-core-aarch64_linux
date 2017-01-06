174a7d20d60ee094f016facdccefb34961808323
Tomasz Pajor <tomek@polishgeeks.com>
2017-01-06 10:16:59 +0100

    thrift 0.10.0
    Closes #8548.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/thrift.rb
+++ Formula/thrift.rb
@@ -1,24 +1,8 @@
 class Thrift < Formula
   desc "Framework for scalable cross-language services development"
   homepage "https://thrift.apache.org/"
-
-  stable do
-    url "https://www.apache.org/dyn/closer.cgi?path=/thrift/0.9.3/thrift-0.9.3.tar.gz"
-    sha256 "b0740a070ac09adde04d43e852ce4c320564a292f26521c46b78e0641564969e"
-
-    # Apply any necessary patches (none currently required)
-    [
-      # Example patch:
-      #
-      # Apply THRIFT-2201 fix from master to 0.9.1 branch (required for clang to compile with C++11 support)
-      # %w{836d95f9f00be73c6936d407977796181d1a506c f8e14cbae1810ade4eab49d91e351459b055c81dba144c1ca3c5d6f4fe440925},
-    ].each do |name, sha|
-      patch do
-        url "https://git-wip-us.apache.org/repos/asf?p=thrift.git;a=commitdiff_plain;h=#{name}"
-        sha256 sha
-      end
-    end
-  end
+  url "https://www.apache.org/dyn/closer.cgi?path=/thrift/0.10.0/thrift-0.10.0.tar.gz"
+  sha256 "2289d02de6e8db04cbbabb921aeb62bfe3098c4c83f36eec6c31194301efa10b"
 
   bottle do
     cellar :any
