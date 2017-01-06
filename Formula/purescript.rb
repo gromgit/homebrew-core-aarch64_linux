0ce234fbebd847aedf0af15931c9fa7dedff52ae
ilovezfs <ilovezfs@icloud.com>
2017-01-06 02:46:20 -0800

    purescript 0.10.5
    Closes #8555.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/purescript.rb
+++ Formula/purescript.rb
@@ -5,21 +5,9 @@ class Purescript < Formula
 
   desc "Strongly typed programming language that compiles to JavaScript"
   homepage "http://www.purescript.org"
-  revision 1
+  url "https://github.com/purescript/purescript/archive/v0.10.5.tar.gz"
+  sha256 "cb38d14e353bf200471385dd08dc5108f4f8690a76806e79c8f2fb3f8facca9c"
   head "https://github.com/purescript/purescript.git"
-
-  stable do
-    url "https://github.com/purescript/purescript/archive/v0.10.4.tar.gz"
-    sha256 "4224d5595352ad000e3b0b39c3f9e4d21ddddad337b651a5bc7480eecfe731e3"
-
-    # Fix "Couldn't match type '[Char]' with 'Text'"
-    # Upstream PR from 2 Jan 2017 "Update bower-json to 1.0.0.1"
-    # https://github.com/purescript/purescript/pull/2531
-    patch do
-      url "https://github.com/purescript/purescript/commit/b84ef77.patch"
-      sha256 "1fd272dff1a09b1bc49e9fea54d829a5fdee04487a436b97b3e573513b96f532"
-    end
-  end
 
   bottle do
     sha256 "b1963ea8c20e0deaa6eabbeee2de8295c80b253265b858db30747b4e152c89ee" => :sierra
