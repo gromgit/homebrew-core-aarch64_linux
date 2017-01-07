5a002b9b3c474b4beafd7c1120737b9458ef45a7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-07 11:28:33 +0000

    doitlive: update 2.6.0 bottle.

--- Formula/doitlive.rb
+++ Formula/doitlive.rb
@@ -6,10 +6,9 @@ class Doitlive < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "668a95dc3b3b49b6f9f7838684083faf6f12569bcc6614aa9a2c4375411e2148" => :sierra
-    sha256 "7511278488a6526d67ae4a2aed6f30f82b376fb37481fac9e95739675cc54091" => :el_capitan
-    sha256 "1f57f0702e75092cf0b96156181cff026ee0e3a72c492fc0ed24899d20ad9b3d" => :yosemite
-    sha256 "8be17aac97bb36b09759bee0eee85c264274c642033f895d4828997541df7f17" => :mavericks
+    sha256 "002fdcd45d96e3c6974230f59efdf5194f5e3dda41cac5add23b7376b4f2c575" => :sierra
+    sha256 "93ef969beb5380908c77239573b622e82ee0863d8222c22ae3189fd3d54c3ee2" => :el_capitan
+    sha256 "93ef969beb5380908c77239573b622e82ee0863d8222c22ae3189fd3d54c3ee2" => :yosemite
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
