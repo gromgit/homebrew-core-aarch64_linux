9c44a9b6a1c00aabf772b93d118988baaf485dc7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-07 11:02:06 +0000

    aiccu: update 20070115 bottle.

--- Formula/aiccu.rb
+++ Formula/aiccu.rb
@@ -8,10 +8,10 @@ class Aiccu < Formula
   sha256 "d23cf50a16fa842242c97683c3c1c1089a7a4964e3eaba97ad1f17110fdfe3cc"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "c49bc224f4ab5bc469a55c9236a74efb585b176f5addff1853d17d753cb3551c" => :el_capitan
-    sha256 "00d05929df3a1951fa3c28108071e0dcc88236a37dfb89af37af66142b1746bf" => :yosemite
-    sha256 "31117537770464542e1186d32032aba90507a11bd4e41c9f46aa717515e8e3cf" => :mavericks
+    rebuild 1
+    sha256 "ee19bef55805a8562bddb41a3af66e5bce9589b1e4d96b05348a37b5ada2c091" => :sierra
+    sha256 "572e103e9de9c872eb202e18d5c4f352f0b9dc26d284d5979b83ff6fa3daa5b2" => :el_capitan
+    sha256 "e4db05626f082c10398f46ac40aa25ec271be6e4372330d6d7c27b2349d0e789" => :yosemite
   end
 
   # Patches per MacPorts
