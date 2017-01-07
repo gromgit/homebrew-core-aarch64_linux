797d3c09d8475919b9ea4db8aa689a71577169c2
Tom Robinson <tlrobinson@gmail.com>
2017-01-07 05:42:02 -0800

    camlistore: add service plist (#8537)

--- Formula/camlistore.rb
+++ Formula/camlistore.rb
@@ -26,6 +26,29 @@ class Camlistore < Formula
     prefix.install "bin"
   end
 
+  plist_options :manual => "camlistored"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <true/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/camlistored</string>
+        <string>-openbrowser=false</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+    </dict>
+    </plist>
+    EOS
+  end
+
   test do
     system bin/"camget", "-version"
   end
