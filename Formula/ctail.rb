7e6d7ddf8e36b5f8dc7c63adc9411069dc03282d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-08 08:17:25 +0000

    ctail: update 0.1.0 bottle.

--- Formula/ctail.rb
+++ Formula/ctail.rb
@@ -5,11 +5,11 @@ class Ctail < Formula
   sha256 "864efb235a5d076167277c9f7812ad5678b477ff9a2e927549ffc19ed95fa911"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "e871b6482648744654b88cace6590eca1e6dd9e6a3d89a57904437f950d0b8e9" => :el_capitan
-    sha256 "c054af9816f833035b8922abf3e935bbe563cc841b098569239f7f9e7b5ff499" => :yosemite
-    sha256 "dd29c6d94e2ded11aef266f5d50bdb840e8b0b3b23841d41e68aad777754d23b" => :mavericks
-    sha256 "dfab40d65950327c679bde97a335779526c58e99f5679f32f95e517a7249e332" => :mountain_lion
+    cellar :any
+    rebuild 1
+    sha256 "829ed2ea1ac94bf32fd1817f714b87301abf2c488cf151675239d5d9bf6f6ef8" => :sierra
+    sha256 "80a2ae43fba99e6eb5eb4b50b52ee0e32213d521f59e147a109444439b86365d" => :el_capitan
+    sha256 "e23b4f67bc165d9d4549963b41b9ee89a5fa2e18040277750c4800da016a919e" => :yosemite
   end
 
   depends_on "autoconf" => :build
