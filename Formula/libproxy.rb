94176c9826fe6f5995135dd21380fc13fee4dc6a
ilovezfs <ilovezfs@icloud.com>
2017-01-09 12:36:57 -0800

    libproxy 0.4.14
    Closes #8663.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libproxy.rb
+++ Formula/libproxy.rb
@@ -1,8 +1,8 @@
 class Libproxy < Formula
   desc "Library that provides automatic proxy configuration management"
   homepage "https://libproxy.github.io/libproxy/"
-  url "https://github.com/libproxy/libproxy/archive/0.4.13.tar.gz"
-  sha256 "d610bc0ef81a18ba418d759c5f4f87bf7102229a9153fb397d7d490987330ffd"
+  url "https://github.com/libproxy/libproxy/archive/0.4.14.tar.gz"
+  sha256 "6220a6cab837a8996116a0568324cadfd09a07ec16b930d2a330e16d5c2e1eb6"
   head "https://github.com/libproxy/libproxy.git"
 
   bottle do
@@ -17,7 +17,7 @@ class Libproxy < Formula
   def install
     mkdir "build" do
       # build tries to install to non-standard locations for Python bindings
-      system "cmake", "..", "-DWITH_PYTHON=no", *std_cmake_args
+      system "cmake", "..", "-DWITH_PYTHON2=OFF", "-DWITH_PYTHON3=OFF", *std_cmake_args
       system "make", "install"
     end
   end
