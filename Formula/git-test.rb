2b6558b3a35e22f5d45ea2a79d5522241c8f5729
ilovezfs <ilovezfs@icloud.com>
2017-01-09 06:53:02 -0800

    git-test 1.0.4 (#8651)

--- Formula/git-test.rb
+++ Formula/git-test.rb
@@ -1,22 +1,15 @@
 class GitTest < Formula
   desc "Run tests on each distinct tree in a revision list"
   homepage "https://github.com/spotify/git-test"
-  url "https://github.com/spotify/git-test/archive/v1.0.2.tar.gz"
-  sha256 "d07ead646d12218b857fe756cc04e15a9bd200726ff5d3692ef17b80cdc6ccc6"
+  url "https://github.com/spotify/git-test/archive/v1.0.4.tar.gz"
+  sha256 "7c2331c8dc3c815e440ffa1a4dc7a9ff8a28a0a8cbfd195282f53c3e4cb2ee00"
 
   bottle :unneeded
 
-  # Fixes two tests in the test.sh in the test block
-  # Reported 17 Jul 2016: https://github.com/spotify/git-test/issues/10
-  patch do
-    url "https://github.com/spotify/git-test/pull/11.patch"
-    sha256 "b69e23e66b61d9333876fdb6756949d19b24ac03ed5052a576b84b74a88616b6"
-  end
-
   def install
-    pkgshare.install "test.sh"
     bin.install "git-test"
     man1.install "git-test.1"
+    pkgshare.install "test.sh"
   end
 
   test do
