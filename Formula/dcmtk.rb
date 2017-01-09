4925a6cbb9685271c4758c74f3e2890f209e0e1e
ilovezfs <ilovezfs@icloud.com>
2017-01-08 22:47:36 -0800

    dcmtk 3.6.1-20161102
    Closes #8640.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dcmtk.rb
+++ Formula/dcmtk.rb
@@ -3,10 +3,9 @@ class Dcmtk < Formula
   homepage "http://dicom.offis.de/dcmtk.php.en"
 
   # Current snapshot used for stable now.
-  url "http://dicom.offis.de/download/dcmtk/snapshot/dcmtk-3.6.1_20160216.tar.gz"
-  url "http://dicom.offis.de/download/dcmtk/snapshot/dcmtk-3.6.1_20160630.tar.gz"
-  version "3.6.1-20160630"
-  sha256 "7dacf0e704f9b5a3a92bd40f1391be3e4473c06c0adb4dbf5855b5c5ddc5f12a"
+  url "http://dicom.offis.de/download/dcmtk/snapshot/dcmtk-3.6.1_20161102.tar.gz"
+  version "3.6.1-20161102"
+  sha256 "657adb3811e0c5c08d8f143a6d878afcd92fac7dcf0d3c89860eecffd5a1a873"
 
   head "git://git.dcmtk.org/dcmtk.git"
 
