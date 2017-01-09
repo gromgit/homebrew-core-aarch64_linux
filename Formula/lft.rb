672fd92232f3f8361816ca69d8dad9e9ec37da27
Tomasz Pajor <tomek@polishgeeks.com>
2017-01-09 13:52:38 +0100

    lft 3.77
    Closes #8649.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/lft.rb
+++ Formula/lft.rb
@@ -1,8 +1,8 @@
 class Lft < Formula
   desc "Layer Four Traceroute (LFT), an advanced traceroute tool"
   homepage "http://pwhois.org/lft/"
-  url "http://pwhois.org/dl/index.who?file=lft-3.73.tar.gz"
-  sha256 "3ecd5371a827288a5f5a4abbd8a5ea8229e116fc2f548cee9afeb589bf206114"
+  url "http://pwhois.org/dl/index.who?file=lft-3.77.tar.gz"
+  sha256 "d182d6774e64dcdeb19ed1a598fa588e57ddb6cbb6847d9277c405d3a4597d89"
 
   bottle do
     cellar :any_skip_relocation
