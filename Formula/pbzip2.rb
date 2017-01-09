806044ae25d0dc6766bed0865fb847f8ec31c03f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-09 13:14:52 +0000

    pbzip2: update 1.1.13 bottle.

--- Formula/pbzip2.rb
+++ Formula/pbzip2.rb
@@ -6,11 +6,9 @@ class Pbzip2 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "85c8604a5fbe8387a183ff0f0afbfe16cc57612c882a003bc1736a31f727318f" => :sierra
-    sha256 "baaa2981ded1c6a0731bed32fbb22b3e143612fadd8452b79c78732f9ac1a903" => :el_capitan
-    sha256 "0fb0998fb35b62add5348bbf1c50372220052d52347be7b11e949e27b3997e1c" => :yosemite
-    sha256 "ac0e6128b16cb551a926fb713d8a4154e29ada2b71f231ecee2ebdab4d4bea96" => :mavericks
-    sha256 "7604662202fbb60acdb016da163b552b1c2cee6a4bdd21ca2be85afb1ce4c987" => :mountain_lion
+    sha256 "c15b9c38b5302286033e54ff4be006c3b31ccb179f96641e44f1126958527d7e" => :sierra
+    sha256 "be653d724b6f061cb9939dbdbf457aebc275e16dbf599f598b9ff3999fdd5db3" => :el_capitan
+    sha256 "ad103aef3e2d72293cfed3fcc42999afee9b4fc332f8319e3c079758215411c9" => :yosemite
   end
 
   def install
