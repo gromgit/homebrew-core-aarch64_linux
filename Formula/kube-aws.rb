8a7eca22b22fd8a24e1d9bdf5302565f8c087256
ArchiFleKs <lefevre.kevin@gmail.com>
2017-01-09 13:34:48 +0100

    kube-aws 0.9.1, 0.9.3 (devel)
    Closes #8646.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/kube-aws.rb
+++ Formula/kube-aws.rb
@@ -3,9 +3,9 @@ require "yaml"
 class KubeAws < Formula
   desc "CoreOS Kubernetes on AWS"
   homepage "https://coreos.com/kubernetes/docs/latest/kubernetes-on-aws.html"
-  url "https://github.com/coreos/coreos-kubernetes/archive/v0.8.3.tar.gz"
-  sha256 "549e5c572fb8842b777d839a8aebadb4165aa5e3d8f4887e87a64347cb6ece09"
-  head "https://github.com/coreos/coreos-kubernetes.git"
+  url "https://github.com/coreos/kube-aws/archive/v0.9.1.tar.gz"
+  sha256 "45f1ac64d6e1132811cd777e2f25ce2dd131cc38d8d7c6c0257ad5c5ff8f5e26"
+  head "https://github.com/coreos/kube-aws.git"
 
   bottle do
     cellar :any_skip_relocation
@@ -14,36 +14,49 @@ class KubeAws < Formula
     sha256 "6c5d8d12c01d94918ef68bf4d487aa1b6f577bed3aa64145cd58192fdb8aa948" => :yosemite
   end
 
+  devel do
+    url "https://github.com/coreos/kube-aws/archive/v0.9.3.tar.gz"
+    sha256 "cd33c88ee607fdcbca34af4a01b3aa386014f0394bd47c651d309b431bc51c30"
+  end
+
   depends_on "go" => :build
 
   def install
     gopath_vendor = buildpath/"_gopath-vendor"
     gopath_kube_aws = buildpath/"_gopath-kube-aws"
-    kube_aws_dir = "#{gopath_kube_aws}/src/github.com/coreos/coreos-kubernetes/multi-node/aws"
+    kube_aws_dir = "#{gopath_kube_aws}/src/github.com/coreos/kube-aws"
 
     mkdir_p gopath_vendor
     mkdir_p File.dirname(kube_aws_dir)
 
-    ln_s buildpath/"multi-node/aws/vendor", "#{gopath_vendor}/src"
-    ln_s buildpath/"multi-node/aws", kube_aws_dir
+    ln_s buildpath/"vendor", "#{gopath_vendor}/src"
+    ln_s buildpath, kube_aws_dir
 
     ENV["GOPATH"] = "#{gopath_vendor}:#{gopath_kube_aws}"
 
     cd kube_aws_dir do
-      system "go", "generate", "./pkg/config"
-      system "go", "build", "-ldflags",
-             "-X github.com/coreos/coreos-kubernetes/multi-node/aws/pkg/cluster.VERSION=#{version}",
-             "-a", "-tags", "netgo", "-installsuffix", "netgo",
-             "-o", bin/"kube-aws", "./cmd/kube-aws"
+      system "go", "generate", "./config"
+      if build.stable?
+        system "go", "build", "-ldflags",
+               "-X github.com/coreos/kube-aws/cluster.VERSION=#{version}",
+               "-a", "-tags", "netgo", "-installsuffix", "netgo",
+               "-o", bin/"kube-aws", "./cmd/kube-aws"
+      else
+        system "go", "generate", "./nodepool/config"
+        system "go", "build", "-ldflags",
+               "-X github.com/coreos/kube-aws/cluster.VERSION=#{version}",
+               "-a", "-tags", "netgo", "-installsuffix", "netgo",
+               "-o", bin/"kube-aws", "./"
+      end
     end
   end
 
   test do
     system "#{bin}/kube-aws"
-
     cluster = { "clusterName" => "test-cluster", "externalDNSName" => "dns",
                 "keyName" => "key", "region" => "west",
                 "availabilityZone" => "zone", "kmsKeyArn" => "arn" }
+    cluster["controller"] = nil unless build.stable?
     system "#{bin}/kube-aws", "init", "--cluster-name", "test-cluster",
            "--external-dns-name", "dns", "--region", "west",
            "--availability-zone", "zone", "--key-name", "key",
