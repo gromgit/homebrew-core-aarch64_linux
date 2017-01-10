c0f28652e58aa4f0efc013270cc4158d52b28209
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-10 02:16:52 +0000

    pev: update 0.80 bottle.

--- Formula/pev.rb
+++ Formula/pev.rb
@@ -6,12 +6,9 @@ class Pev < Formula
   head "https://github.com/merces/pev.git"
 
   bottle do
-    cellar :any
-    sha256 "ae2da367a475feabca973d61db14a8f7b9bb14d0508453059bb09c66ad8c7ee4" => :sierra
-    sha256 "81e831cd776a7ecea986012a4a8aae05c316b7c628314c679bbc89daf89190e8" => :el_capitan
-    sha256 "e01f09a92f3af2fb454e17529bac7900978960382bc52ae62aa4fa6b751f41c0" => :yosemite
-    sha256 "f2ffcbda87b08fdd2a4621468f711c7f5542b2413833f350f93105d0825a97d9" => :mavericks
-    sha256 "08ba1dbebcb68b2c6a67a2052840764803ad3efc975563e72960da0f8b6e463d" => :mountain_lion
+    sha256 "c995e2ccdc4fde4e2e1beaa3039421ef8e77cdbe0ec00f1af50d7ba551d2c695" => :sierra
+    sha256 "ec81bb70fec6dd27f249a575194c62db766b2e31349224d503ecb1cbf2603c37" => :el_capitan
+    sha256 "1013086fe324f7b6147181762541d89dcf4837bd29bd89756d7df23c26fad1f2" => :yosemite
   end
 
   depends_on "pcre"
