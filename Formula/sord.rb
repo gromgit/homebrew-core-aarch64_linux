c70a7d72cdaef7e2da0a64b5a21cf89678e32ce9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-11 08:31:08 +0000

    sord: update 0.16.0 bottle.

--- Formula/sord.rb
+++ Formula/sord.rb
@@ -6,11 +6,9 @@ class Sord < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "450cf1c1bd1440776e63d288ab9feedfdaa572e717258355e8fcbd523e944067" => :sierra
-    sha256 "1f03c341d2ff39082e12ca7ef48b7c599cbfa57c1064f3117997dc3641bf29eb" => :el_capitan
-    sha256 "dfa53d9dae13b29a164710351e772e3518e3821c1b83b52bcd5c1e76e7471786" => :yosemite
-    sha256 "cb2ca5b304181e590334ad9cfec8b2dbce606b385a39297f7dd31aebbb9d7c39" => :mavericks
+    sha256 "79ac2ab52531da8fbf650a4415907e6098e5038fea13c8a00db4efb0b6e343cc" => :sierra
+    sha256 "1d2d04b071394a4b7381f89812ecc05f6967b87936935131d077c95f13b9a2f2" => :el_capitan
+    sha256 "b4c0d5559e0c1aae7e8de803e75cdf84858f67bad70ba663979efc29c07b081f" => :yosemite
   end
 
   depends_on "pkg-config" => :build
