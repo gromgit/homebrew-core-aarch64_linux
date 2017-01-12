ea4c805e72daf5df4a07c18ea0dd8b1b58b8f077
Tomasz Pajor <tomek@polishgeeks.com>
2017-01-12 11:16:29 +0100

    mktorrent 1.1
    Closes #8768.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/mktorrent.rb
+++ Formula/mktorrent.rb
@@ -1,9 +1,8 @@
 class Mktorrent < Formula
   desc "Create BitTorrent metainfo files"
   homepage "http://mktorrent.sourceforge.net/"
-  url "https://downloads.sourceforge.net/mktorrent/mktorrent-1.0.tar.gz"
-  sha256 "6f8e562af6366e0d9bde76e434f740b55722c6c3c555860dbe80083f9d1d119f"
-  revision 1
+  url "https://github.com/Rudde/mktorrent/archive/v1.1.tar.gz"
+  sha256 "d0f47500192605d01b5a2569c605e51ed319f557d24cfcbcb23a26d51d6138c9"
 
   bottle do
     cellar :any
