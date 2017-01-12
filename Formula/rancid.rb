e1a1b94b2523d6907ed73858c754f603e8db7ee0
Tomasz Pajor <tomek@polishgeeks.com>
2017-01-12 11:27:54 +0100

    rancid 3.6.2
    Closes #8772.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/rancid.rb
+++ Formula/rancid.rb
@@ -1,9 +1,9 @@
 class Rancid < Formula
   desc "Really Awesome New Cisco confIg Differ"
   homepage "http://www.shrubbery.net/rancid/"
-  url "ftp://ftp.shrubbery.net/pub/rancid/rancid-3.6.1.tar.gz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/r/rancid/rancid_3.6.1.orig.tar.gz"
-  sha256 "b7506b5e75eb324c41474b254e3b91ecd59bbeebd4b4155cd690ba1a6870a65a"
+  url "ftp://ftp.shrubbery.net/pub/rancid/rancid-3.6.2.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/r/rancid/rancid_3.6.2.orig.tar.gz"
+  sha256 "23751829c9bacdd07f90512271265c0cac279ca2f36ac86be815dd21831c253d"
 
   bottle do
     cellar :any_skip_relocation
