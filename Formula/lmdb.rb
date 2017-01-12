4e79d7f11db08840e3f237b8704853e205b0b127
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-12 07:58:27 +0000

    lmdb: update 0.9.20 bottle.

--- Formula/lmdb.rb
+++ Formula/lmdb.rb
@@ -7,9 +7,9 @@ class Lmdb < Formula
 
   bottle do
     cellar :any
-    sha256 "fdcde37d7b72ef7d2034a72c663cfb218d20a3800fdc2e38ea8bf0ae9580a6ae" => :sierra
-    sha256 "0c636ad2fae846054705f014e89b0ad7061afaecd15ba1ef318dd7d4a8fb95a7" => :el_capitan
-    sha256 "f4d580f33d301f7c61acae9f0ee5034c4ec6d1a86c8a912fa90205926624a8ce" => :yosemite
+    sha256 "01ba0974a48a7718969b76976c756b6e2fec4e335dc1d2a8fde852d3e12ef335" => :sierra
+    sha256 "b6ac68239ea9d6f68f94b2ddfdd7be38a449f78e00a8ebb35019fa948e255328" => :el_capitan
+    sha256 "9414ebf7fc8668e2f7336598775cbe01d5a5c787061461744615879d6166c9be" => :yosemite
   end
 
   def install
