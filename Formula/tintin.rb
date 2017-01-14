e9326d1c68dc8b3f8653d9586a188177cc7b8654
ilovezfs <ilovezfs@icloud.com>
2017-01-14 07:32:47 -0800

    tintin 2.01.1
    Closes #8897.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/tintin.rb
+++ Formula/tintin.rb
@@ -1,8 +1,8 @@
 class Tintin < Formula
   desc "MUD client"
   homepage "http://tintin.sf.net"
-  url "https://downloads.sourceforge.net/project/tintin/TinTin%2B%2B%20Source%20Code/2.01.0/tintin-2.01.0.tar.gz"
-  sha256 "e0e35463a97ee5b33ef0b29b2c57fa8276c4e76328cb19c98a6ea92c603a9c76"
+  url "https://downloads.sourceforge.net/project/tintin/TinTin%2B%2B%20Source%20Code/2.01.1/tintin-2.01.1.tar.gz"
+  sha256 "cc0ba550880bd5efdba8fde11d1e4ddd4ae8737f81e6f64649de23ed99cbbca4"
 
   bottle do
     cellar :any
