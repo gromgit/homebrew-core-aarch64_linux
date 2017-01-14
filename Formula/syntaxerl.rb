66d18922232efcc4f5a6a7afeab742bc130800d1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-14 09:28:48 +0000

    syntaxerl: update 0.13.0 bottle.

--- Formula/syntaxerl.rb
+++ Formula/syntaxerl.rb
@@ -6,10 +6,9 @@ class Syntaxerl < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a5b022d728babd0fd5f492b45f63e99b4ff9086821ceb250a085a051f33e5b68" => :sierra
-    sha256 "e41ec5d0fe9d78f9574c8ddeb08943e4e4d09cc826e7012d089ca7ed0d719523" => :el_capitan
-    sha256 "9402057ae3c091f56a08c2746ac0d56da0bb337250c9765ccc01a0e1f2331656" => :yosemite
-    sha256 "ccf1aecf79fbf0da7f8857a88ce536068095148d47a9791503b256cde0eed2d3" => :mavericks
+    sha256 "83d2862830f4a5d72b22ef20b11e211a661a00bbc27b9369aed56c2bcc868d93" => :sierra
+    sha256 "7327d1cde23cfaecec5ea018423e3eea0801ada045460a910e26e6cdb871c5e7" => :el_capitan
+    sha256 "41a04901b1cef9d2e9f69a929a92f07f3f76039247c1645fa6eb44c78c3ea276" => :yosemite
   end
 
   depends_on "erlang"
