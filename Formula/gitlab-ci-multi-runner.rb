84721640ec18ecc6aa12bc852545c5e2f16535e7
JCount <JCount42@gmail.com>
2017-01-13 20:43:00 -0500

    gitlab-ci-multi-runner 1.9.2
    also add ARM prebuilt image,
    static build time
    
    Closes #8830.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/gitlab-ci-multi-runner.rb
+++ Formula/gitlab-ci-multi-runner.rb
@@ -4,8 +4,8 @@ class GitlabCiMultiRunner < Formula
   desc "The official GitLab CI runner written in Go"
   homepage "https://gitlab.com/gitlab-org/gitlab-ci-multi-runner"
   url "https://gitlab.com/gitlab-org/gitlab-ci-multi-runner.git",
-      :tag => "v1.9.0",
-      :revision => "82714ae882225d77636f8780197309e59cd20ecf"
+      :tag => "v1.9.2",
+      :revision => "ade6572ddb2d1e75243c31333fb392ac2a745685"
   head "https://gitlab.com/gitlab-org/gitlab-ci-multi-runner.git"
 
   bottle do
@@ -23,10 +23,17 @@ class GitlabCiMultiRunner < Formula
   end
 
   resource "prebuilt-x86_64.tar.xz" do
-    url "https://gitlab-ci-multi-runner-downloads.s3.amazonaws.com/v1.9.0/docker/prebuilt-x86_64.tar.xz",
+    url "https://gitlab-ci-multi-runner-downloads.s3.amazonaws.com/v1.9.2/docker/prebuilt-x86_64.tar.xz",
         :using => :nounzip
-    version "1.9.0"
-    sha256 "ba08b5e0095b09b80bc833661735e9783d4bb5764f840f3100e21909d8be0c8a"
+    version "1.9.2"
+    sha256 "7d0759dd04f930a231c1a713d8e44a149666112ff1d841c5173847a373496278"
+  end
+
+  resource "prebuilt-arm.tar.xz" do
+    url "https://gitlab-ci-multi-runner-downloads.s3.amazonaws.com/v1.9.2/docker/prebuilt-arm.tar.xz",
+        :using => :nounzip
+    version "1.9.2"
+    sha256 "c4f7297865fafa4e7841e513a26b334cf9f29ad6d7ec4b12381d6031fa6f9648"
   end
 
   def install
@@ -41,21 +48,24 @@ class GitlabCiMultiRunner < Formula
     end
 
     cd dir do
-      resource("prebuilt-x86_64.tar.xz").stage do
-        system "go-bindata", "-pkg", "docker", "-nocompress", "-nomemcopy",
-                             "-nometadata", "-o",
-                             "#{dir}/executors/docker/bindata.go",
-                             "prebuilt-x86_64.tar.xz"
-      end
+      Pathname.pwd.install resource("prebuilt-x86_64.tar.xz"),
+                           resource("prebuilt-arm.tar.xz")
+      system "go-bindata", "-pkg", "docker", "-nocompress", "-nomemcopy",
+                           "-nometadata", "-o",
+                           "#{dir}/executors/docker/bindata.go",
+                           "prebuilt-x86_64.tar.xz",
+                           "prebuilt-arm.tar.xz"
 
       proj = "gitlab.com/gitlab-org/gitlab-ci-multi-runner"
       commit = Utils.popen_read("git", "rev-parse", "--short", "HEAD").chomp
       branch = version.to_s.split(".")[0..1].join("-") + "-stable"
+      built = Time.new.strftime("%Y-%m-%dT%H:%M:%S%:z")
       system "go", "build", "-ldflags", <<-EOS.undent
              -X #{proj}/common.NAME=gitlab-ci-multi-runner
              -X #{proj}/common.VERSION=#{version}
              -X #{proj}/common.REVISION=#{commit}
              -X #{proj}/common.BRANCH=#{branch}
+             -X #{proj}/common.BUILT=#{built}
       EOS
 
       bin.install "gitlab-ci-multi-runner"
