a73df912687f1836edae77af4e49f2ff9ec7304a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-15 01:13:15 +0000

    git-cola: update 2.10 bottle.

--- Formula/git-cola.rb
+++ Formula/git-cola.rb
@@ -7,9 +7,9 @@ class GitCola < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b2cd63a33cbea24cd5c17efe72c54e538443113aae3d3f7fc204bbd30bf9360e" => :sierra
-    sha256 "b8d0b83f12bb03056fba491b78ca632b73377e9a444f026585b7ac5a75e8553f" => :el_capitan
-    sha256 "b8d0b83f12bb03056fba491b78ca632b73377e9a444f026585b7ac5a75e8553f" => :yosemite
+    sha256 "c742348a63a6e34d0c52bcd489b1e843bf801a9d2ed942f85d4081ad4aceeb96" => :sierra
+    sha256 "f0be7e1fbae9d40b3a4134f5c5774b1598731e32832b4b753bcf74ebb7b625c1" => :el_capitan
+    sha256 "f0be7e1fbae9d40b3a4134f5c5774b1598731e32832b4b753bcf74ebb7b625c1" => :yosemite
   end
 
   option "with-docs", "Build manpages and HTML docs"
