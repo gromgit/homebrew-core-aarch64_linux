6515ef801a798fb36371119464a4d4615b485716
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-15 11:58:50 +0000

    cnats: add 1.4.4 bottle.

--- Formula/cnats.rb
+++ Formula/cnats.rb
@@ -4,6 +4,13 @@ class Cnats < Formula
   url "https://github.com/nats-io/cnats/archive/v1.4.4.tar.gz"
   sha256 "fea8c510d6b2ec3df1a9b92b78863780dd0e0fb536070000ed384dc2e05d13a5"
 
+  bottle do
+    cellar :any
+    sha256 "5519a5385a4459cd68a72eae9732e48ca1bcac95fe6e067f5719c984d749744e" => :sierra
+    sha256 "b2b3f93ddb948782066b2504e4f07e3cebe6d95819354394a3113518d87e4ee8" => :el_capitan
+    sha256 "77f3d37ac8fffce4a8853f2faadb6cdd7b7e753dd8d038986f45a7695b641a89" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "libevent" => :optional
   depends_on "libuv" => :optional
