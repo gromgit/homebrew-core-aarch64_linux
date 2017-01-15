bbf20ec7fe0942c10e448d5d64088f512374419d
ilovezfs <ilovezfs@icloud.com>
2017-01-14 16:44:56 -0800

    bib-tool 2.67
    Closes #8919.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/bib-tool.rb
+++ Formula/bib-tool.rb
@@ -1,8 +1,8 @@
 class BibTool < Formula
   desc "Manipulates BibTeX databases"
   homepage "http://www.gerd-neugebauer.de/software/TeX/BibTool/en/"
-  url "https://github.com/ge-ne/bibtool/archive/BibTool_2_66.tar.gz"
-  sha256 "6db5dcecdf0d068276fc4e28e8f23975c3f53361bbe713013ca339117e34aced"
+  url "https://github.com/ge-ne/bibtool/releases/download/BibTool_2_67/BibTool-2.67.tar.gz"
+  sha256 "5b6c4160975a926356e8e59d0e5c01ac2a7be337ecace2494918fc2a46d9d784"
 
   bottle do
     sha256 "693f5ab5a84e614123e1e28fab4ca7e9d6b3a4785bc26b21b2a1a45d98ce4a02" => :sierra
