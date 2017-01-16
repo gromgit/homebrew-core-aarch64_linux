a5777a3482f3f48f0ee6c4ddbfeddb3311f17041
Vishal Rana <vr@labstack.com>
2017-01-15 17:18:14 -0800

    armor 0.2.5
    Closes #8952.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/armor.rb
+++ Formula/armor.rb
@@ -1,8 +1,8 @@
 class Armor < Formula
   desc "Uncomplicated HTTP server, supports HTTP/2 and auto TLS"
   homepage "https://github.com/labstack/armor"
-  url "https://github.com/labstack/armor/archive/v0.2.4.tar.gz"
-  sha256 "58098c2e3dce1f652555f269e1544ad4b9bc9bf0aac926c0bef8bf0febaa6fb2"
+  url "https://github.com/labstack/armor/archive/v0.2.5.tar.gz"
+  sha256 "0283492ab8bc92305418e90a5704fda89c6f83fc75a8ce30659d35ac55696d2a"
   head "https://github.com/labstack/armor.git"
 
   bottle do
