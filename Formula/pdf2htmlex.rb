a7e93d392992359d0710e1e39780d2e223fbf017
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-16 13:54:54 +0000

    pdf2htmlex: update 0.14.6_10 bottle.

--- Formula/pdf2htmlex.rb
+++ Formula/pdf2htmlex.rb
@@ -8,9 +8,9 @@ class Pdf2htmlex < Formula
   head "https://github.com/coolwanglu/pdf2htmlEX.git"
 
   bottle do
-    sha256 "13de76e9061f6e36caaf57c95269eb290677a98307345d6f17f55a62264a908a" => :sierra
-    sha256 "d55de889401417d4089138b04e6bfc6f834c0db723672795ce95610532c1f547" => :el_capitan
-    sha256 "48064256fc3c27f4a821ec1631a246d0128320324ccd0528909130ed70d47a79" => :yosemite
+    sha256 "92017c148cb879d5ce360a1c85f188361d8c52fad2d8d3dece2b2c88ba57441d" => :sierra
+    sha256 "70ffaa20785537e43ecd9a8989536b38644a71a831271aa0a7605e0210c9d709" => :el_capitan
+    sha256 "2bb953eb7e4b8c29c0b4af2f276571b904b42cb5fcab3e1aa36e9c08c3755e5b" => :yosemite
   end
 
   depends_on :macos => :lion
