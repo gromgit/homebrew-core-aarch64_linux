00004549dc682cc53b9dfbcbd969dd6fa0a83d7d
Mike McQuaid <mike@mikemcquaid.com>
2017-01-17 10:53:14 +0000

    juju: rename from juju@2.0.
    Closes #9001.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -1,8 +1,9 @@
-class JujuAT20 < Formula
+class Juju < Formula
   desc "DevOps management tool"
   homepage "https://jujucharms.com/"
   url "https://launchpad.net/juju/2.0/2.0.1/+download/juju-core_2.0.1.tar.gz"
   sha256 "af5d59f4b4508c3f81b15fe052fe377876f5de845885d6d41d054f4ac605b9e9"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -12,7 +13,6 @@ class JujuAT20 < Formula
   end
 
   depends_on "go" => :build
-  conflicts_with "juju@1.25", :because => "juju 1 and 2 cannot be installed simultaneously."
 
   def install
     ENV["GOPATH"] = buildpath
