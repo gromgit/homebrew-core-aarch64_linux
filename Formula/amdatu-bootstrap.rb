3cbacb089b84d1de1db3ee6106752791616462ae
JCount <JCount42@gmail.com>
2017-01-17 16:30:06 -0500

    amdatu-bootstrap: update homepage (#9005)

--- Formula/amdatu-bootstrap.rb
+++ Formula/amdatu-bootstrap.rb
@@ -1,6 +1,6 @@
 class AmdatuBootstrap < Formula
   desc "Bootstrapping OSGi development"
-  homepage "http://amdatu.org/bootstrap/intro.html"
+  homepage "http://www.amdatu.com/bootstrap/intro.html"
   url "https://bitbucket.org/amdatu/amdatu-bootstrap/downloads/bootstrap-bin-r9.zip"
   sha256 "937ef932a740665439ea0118ed417ff7bdc9680b816b8b3c81ecfd6d0fc4773b"
 
