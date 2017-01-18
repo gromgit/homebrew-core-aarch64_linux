76cf5dfb04e41b123b4141d31f6b9c0d37e7dbb8
Tomasz Pajor <tomek@polishgeeks.com>
2017-01-18 08:01:14 +0100

    libev 4.24
    Closes #9019.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/libev.rb
+++ Formula/libev.rb
@@ -1,8 +1,8 @@
 class Libev < Formula
   desc "Asynchronous event library"
   homepage "http://software.schmorp.de/pkg/libev.html"
-  url "http://dist.schmorp.de/libev/Attic/libev-4.22.tar.gz"
-  sha256 "736079e8ac543c74d59af73f9c52737b3bfec9601f020bf25a87a4f4d0f01bd6"
+  url "http://dist.schmorp.de/libev/Attic/libev-4.24.tar.gz"
+  sha256 "973593d3479abdf657674a55afe5f78624b0e440614e2b8cb3a07f16d4d7f821"
 
   bottle do
     cellar :any
