ad6f4301928c9c507333f5409c2657cee7aef1e9
Tomasz Pajor <tomek@polishgeeks.com>
2017-01-18 07:53:03 +0100

    swaks 20170101.0
    Closes #9018.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/swaks.rb
+++ Formula/swaks.rb
@@ -1,12 +1,16 @@
 class Swaks < Formula
   desc "SMTP command-line test tool"
   homepage "http://www.jetmore.org/john/code/swaks/"
-  url "http://www.jetmore.org/john/code/swaks/files/swaks-20130209.0.tar.gz"
-  sha256 "0b0967256dca82776f610f1db862bc47644b236f325fa48cbdb2651babd41f7c"
+  url "http://www.jetmore.org/john/code/swaks/files/swaks-20170101.0.tar.gz"
+  sha256 "84e62dd0de4a56d5daebe25afd16835bd8d3c7f39caa5e6bc7d86a056925915e"
 
   bottle :unneeded
 
   def install
     bin.install "swaks"
+  end
+
+  test do
+    system "#{bin}/swaks", "--version"
   end
 end
