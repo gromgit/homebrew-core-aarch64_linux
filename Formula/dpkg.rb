b6daf14ec972f2d3d859849dab20e948fe1e8072
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-19 15:42:05 +0000

    dpkg: update 1.18.18 bottle.

--- Formula/dpkg.rb
+++ Formula/dpkg.rb
@@ -6,9 +6,9 @@ class Dpkg < Formula
   sha256 "c88b61e3d4660500753142689e8ddbeff1c731f29549f3338e6975f655936ff5"
 
   bottle do
-    sha256 "d4948d2da0cfd747dc4926fb0b9ed8febe067b925bc65082dbad62fdc03128a8" => :sierra
-    sha256 "904a2b6d2461b41bd87b92c66758c084416f85cf71bce3b6413c5cd99c238bf2" => :el_capitan
-    sha256 "f65544444a68dfd1100485ac081584d9771b4baabe8f4099708be5fd92cb38ba" => :yosemite
+    sha256 "0cb724b24e6259a1b792d081fca589925809f841b271c57b3ca5bb9100299412" => :sierra
+    sha256 "a612146da7dffef7b2324726b07237e7af9ab67215f9b05ca4825adb4c5b6cb5" => :el_capitan
+    sha256 "990103f2fada3040e5056d492a331bb0ba4defb921d1f66abcd452851e3940ed" => :yosemite
   end
 
   depends_on "pkg-config" => :build
