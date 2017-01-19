8cf41415a6f711479018816902d00f6642d148f7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-19 09:09:54 +0000

    apt-dater: update 0.9.0 bottle.

--- Formula/apt-dater.rb
+++ Formula/apt-dater.rb
@@ -6,10 +6,10 @@ class AptDater < Formula
   version_scheme 1
 
   bottle do
-    rebuild 1
-    sha256 "49bb3d854aaf984fcc9da2aed057d98edd56b9383425f4a800e22e2513765132" => :sierra
-    sha256 "b80cb29f55c78f6a3089001f35f282d539249f80cb2ffacb332f5194c9bbb112" => :el_capitan
-    sha256 "5ab44ab078c14e21529949901f62a170157f97b50e702191299453763ff717f3" => :yosemite
+    rebuild 2
+    sha256 "026b29a9428c2c1d77e70001c8651f8e8ac20b20dee1ba62a89e0d69e2da570e" => :sierra
+    sha256 "a2f37094132e6f5cd8ad9b287bf299eea8acbc99b1d468002dfe875a8a14985d" => :el_capitan
+    sha256 "2ac3ba56f32d018a9af477484d8ad561871f855aca78726dbe8f43f5552f6acc" => :yosemite
   end
 
   depends_on "pkg-config" => :build
