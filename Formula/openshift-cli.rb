4f32b86166bd543b6ec9e7c97c9ec77eba094a8b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-19 09:44:33 +0000

    openshift-cli: update 1.4.0 bottle.

--- Formula/openshift-cli.rb
+++ Formula/openshift-cli.rb
@@ -8,10 +8,9 @@ class OpenshiftCli < Formula
   head "https://github.com/openshift/origin.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "aa16bc29805dd80a448cc6af5c9cc9b140bdd37c945dd8efab0e0170c4a17975" => :sierra
-    sha256 "54c1a7542b02ceeff3fc226ac99a50164f12d9da41f8ccefdd948376b6a892a5" => :el_capitan
-    sha256 "bd528eb29cfd0010060fa8c5c5f2945df9d53e53cd758a0d73a83887aeb7b8a9" => :yosemite
+    sha256 "88d5f098c0c13dabb5ca0929b33490c383de133905645e12b6e33869ffa60ab5" => :sierra
+    sha256 "4931aeffeb58ac57166a2ea5c72a60aeb7615b0dd8e7c2b5160db41c95a3672b" => :el_capitan
+    sha256 "b24f889d037d250a2f69c4cd055f3d8eb657d31dc0578821b2fecc8620359c67" => :yosemite
   end
 
   devel do
