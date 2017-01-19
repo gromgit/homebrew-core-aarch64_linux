21d6164832d0b29eb5532a171a3d3483ebdc8ad6
ilovezfs <ilovezfs@icloud.com>
2017-01-19 03:29:58 -0800

    gnupg-pkcs11-scd 0.7.4
    Closes #9061.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/gnupg-pkcs11-scd.rb
+++ Formula/gnupg-pkcs11-scd.rb
@@ -1,11 +1,8 @@
 class GnupgPkcs11Scd < Formula
   desc "Enable the use of PKCS#11 tokens with GnuPG"
   homepage "http://gnupg-pkcs11.sourceforge.net"
-  url "https://github.com/alonbl/gnupg-pkcs11-scd/archive/gnupg-pkcs11-scd-0.7.3.tar.gz"
-  sha256 "69412cf0a71778026dd9a8adc5276b43e54dc698d12ca36f7f6969d1a76330b8"
-  revision 2
-
-  head "https://github.com/alonbl/gnupg-pkcs11-scd.git"
+  url "https://github.com/alonbl/gnupg-pkcs11-scd/releases/download/gnupg-pkcs11-scd-0.7.4/gnupg-pkcs11-scd-0.7.4.tar.bz2"
+  sha256 "790dfd55f1651fe86f6186558cc8f1631897db81feb1a91c0e2f012049c6a922"
 
   bottle do
     cellar :any
@@ -15,9 +12,6 @@ class GnupgPkcs11Scd < Formula
     sha256 "534831eb894e729bf231892c974305d0e70763179a105ba5b9babb43756ee5ba" => :mavericks
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "libgpg-error"
   depends_on "libassuan"
@@ -25,7 +19,6 @@ class GnupgPkcs11Scd < Formula
   depends_on "pkcs11-helper"
 
   def install
-    system "autoreconf", "-fvi"
     system "./configure", "--disable-dependency-tracking",
                           "--with-libgpg-error-prefix=#{Formula["libgpg-error"].opt_prefix}",
                           "--with-libassuan-prefix=#{Formula["libassuan"].opt_prefix}",
