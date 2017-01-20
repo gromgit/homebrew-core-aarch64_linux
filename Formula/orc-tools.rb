0a26133a0098d89b66c7fbfb214a3336c1bf2b29
Michael Häusler <michael.haeusler@researchgate.net>
2017-01-20 18:44:22 +0100

    orc-tools 1.2.3 (new formula)
    orc-tools 1.2.3
    
    Closes #9108.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

---
+++ Formula/orc-tools.rb
@@ -0,0 +1,18 @@
+class OrcTools < Formula
+  desc "ORC java command-line tools and utilities"
+  homepage "https://orc.apache.org/"
+  url "https://search.maven.org/remotecontent?filepath=org/apache/orc/orc-tools/1.2.3/orc-tools-1.2.3-uber.jar"
+  version "1.2.3"
+  sha256 "6969df89f24eab45ed4f1471b0296b7eaef9853e4e76c273e820170b6f18506d"
+
+  bottle :unneeded
+
+  def install
+    libexec.install "orc-tools-#{version}-uber.jar"
+    bin.write_jar_script libexec/"orc-tools-#{version}-uber.jar", "orc-tools"
+  end
+
+  test do
+    system "#{bin}/orc-tools", "meta", "-h"
+  end
+end
