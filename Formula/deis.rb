beedd5176b921d58298377b026e581f791715367
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-20 09:11:40 +0000

    deis: update 1.13.4 bottle.

--- Formula/deis.rb
+++ Formula/deis.rb
@@ -5,11 +5,9 @@ class Deis < Formula
   sha256 "c59b244b2d4e87e04491898a401238ea6f302326957ffe1aeb6154485fe1d8ec"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "18e0780dabcd28d1f579574f8bb9aa09bddc53bdf1f6c3804b9d9a37463fa121" => :sierra
-    sha256 "310cfc77e1edddab84270547c87c567958cb44d25437dfd4793f0a461b5b7de5" => :el_capitan
-    sha256 "be5c1307443b5fbf5a9cbeb027af3c1960f09b0318bc589850299afd5694b52e" => :yosemite
-    sha256 "e7550c86cdbf3618cb89445067aff24733c0786817760ed87b1c38268fc1b808" => :mavericks
+    sha256 "274ce1639c8b85fff3fd4a315e260ac37baa5d66b52357d4d08b45cefa29cb15" => :sierra
+    sha256 "2ac131bc5838ab6970cb6fb8269491c6eaa1ba4520c73ff4f0544d4b818c673b" => :el_capitan
+    sha256 "a0b0c61cd39b47af6262a7fa13ad6b6d02118cc233b8bd78303172f3d0d02e83" => :yosemite
   end
 
   depends_on "go" => :build
