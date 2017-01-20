c5db3947d489f6534db4eba105532c4fe71210cd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-20 20:02:43 +0000

    source-to-image: add 1.1.4 bottle.

--- Formula/source-to-image.rb
+++ Formula/source-to-image.rb
@@ -6,6 +6,12 @@ class SourceToImage < Formula
     :revision => "870b2730357b2664598b47672a4840e3ebd31338"
   head "https://github.com/openshift/source-to-image.git"
 
+  bottle do
+    sha256 "32f2456dba8758ffc4041903b129f38446ef5dc2916c70f1001a94849af77c43" => :sierra
+    sha256 "e88c854a0dad4612e469940f0a09566aef5317b0c416eb0dcb09b73349332024" => :el_capitan
+    sha256 "785d3a1d1301ef2d59af28cf32648c1de2ee2f8241bc9d65501693222badf8b2" => :yosemite
+  end
+
   depends_on "go" => :build
 
   def install
