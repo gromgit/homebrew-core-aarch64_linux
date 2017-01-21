d52fa0ed6d9d0fc95304367df4fed343e9bfebdb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-21 23:25:23 +0000

    swimat: add 1.2.1 bottle.

--- Formula/swimat.rb
+++ Formula/swimat.rb
@@ -5,6 +5,12 @@ class Swimat < Formula
   sha256 "ef2f0fe83ca779698679a1a110ebc6ae9c5a457805b6647af79db32cfed0a018"
   head "https://github.com/Jintin/Swimat.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "afb03ff97abd634eb8d5650fdba20cbdc874b68e40542c2a6c0082afd8a97de1" => :sierra
+    sha256 "b21a2dd90ed13653dd53ddf07c1cb1ee50ac8efa61a42603bf47fc5266614732" => :el_capitan
+  end
+
   depends_on :xcode => "8.0"
 
   def install
