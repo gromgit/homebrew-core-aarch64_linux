ea90ae8e299a946282933524a5b1ab0a79f9fdb9
ilovezfs <ilovezfs@icloud.com>
2017-01-21 00:17:49 -0800

    tcpkali 1.1.1
    Closes #9132.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/tcpkali.rb
+++ Formula/tcpkali.rb
@@ -1,8 +1,8 @@
 class Tcpkali < Formula
   desc "High performance TCP and WebSocket load generator and sink"
   homepage "https://github.com/machinezone/tcpkali"
-  url "https://github.com/machinezone/tcpkali/releases/download/v1.1/tcpkali-1.1.tar.gz"
-  sha256 "e477b03f3e0aa45ddd5c058ea5e6107486983c2aa658a076b061af84c1ed61e7"
+  url "https://github.com/machinezone/tcpkali/releases/download/v1.1.1/tcpkali-1.1.1.tar.gz"
+  sha256 "a9a15a1703fc4960360a414ee282d821a7b42d4bbba89f9e72a796164ff69598"
 
   bottle do
     cellar :any_skip_relocation
@@ -11,16 +11,7 @@ class Tcpkali < Formula
     sha256 "358ce45dd065b2f92e4de1430343d9339d09e21a052d7593d9c308963f36d7d1" => :yosemite
   end
 
-  # Upstream issue "1.1 release tarball is missing pcg_basic.h"
-  # Reported 20 Jan 2016 https://github.com/machinezone/tcpkali/issues/39
-  resource "pcg_basic_header" do
-    url "https://raw.githubusercontent.com/machinezone/tcpkali/1be382b/deps/pcg-c-basic/pcg_basic.h"
-    sha256 "cd823ddc225da9be520a54f13ef2c491506c353800cae04e8b673b2de58f2cc4"
-  end
-
   def install
-    raise "remove the resource" if File.exist? "deps/pcg-c-basic/pcg_basic.h"
-    resource("pcg_basic_header").stage buildpath/"deps/pcg-c-basic"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
