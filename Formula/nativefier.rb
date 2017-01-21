2fab18999ea7924d8c5e4deb894141f0db20a93a
Weihang Lo <weihanglotw@gmail.com>
2017-01-22 02:42:24 +0800

    nativefier 7.0.1 (new formula)
    Closes #9142.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

---
+++ Formula/nativefier.rb
@@ -0,0 +1,19 @@
+require "language/node"
+
+class Nativefier < Formula
+  desc "Wrap web apps natively"
+  homepage "https://github.com/jiahaog/nativefier"
+  url "https://registry.npmjs.org/nativefier/-/nativefier-7.0.1.tgz"
+  sha256 "09a22a0d4b5fe85b5075d2a5896bb3648928d0da73c44c61fdbded2bda608d5e"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system bin/"nativefier", "--version"
+  end
+end
