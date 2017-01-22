f71fdafbc0013fef09cc8450f88ae1f22b456131
Mike McQuaid <mike@mikemcquaid.com>
2017-01-22 19:29:27 +0000

    gtk-chtheme: import from homebrew/gui.

---
+++ Formula/gtk-chtheme.rb
@@ -0,0 +1,25 @@
+class GtkChtheme < Formula
+  desc "GTK+ 2.0 theme changer GUI"
+  homepage "http://plasmasturm.org/code/gtk-chtheme/"
+  url "http://plasmasturm.org/code/gtk-chtheme/gtk-chtheme-0.3.1.tar.bz2"
+  sha256 "26f4b6dd60c220d20d612ca840b6beb18b59d139078be72c7b1efefc447df844"
+  revision 1
+
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
+  depends_on "gtk+"
+
+  def install
+    # Unfortunately chtheme relies on some deprecated functionality
+    # we need to disable errors for it to compile properly
+    inreplace "Makefile", "-DGTK_DISABLE_DEPRECATED", ""
+
+    system "make", "PREFIX=#{prefix}", "install"
+  end
+
+  test do
+    # package contains just an executable and a man file
+    # executable accepts no options and just spawns a GUI
+    assert (bin/"gtk-chtheme").exist?
+  end
+end
