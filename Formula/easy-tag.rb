16fc357a680c769e884acdad3db09a18272cf1b9
Mike McQuaid <mike@mikemcquaid.com>
2017-01-22 19:27:47 +0000

    easy-tag: import from homebrew/gui.

---
+++ Formula/easy-tag.rb
@@ -0,0 +1,59 @@
+class EasyTag < Formula
+  desc "Application for viewing and editing audio file tags"
+  homepage "https://projects.gnome.org/easytag"
+  url "https://download.gnome.org//sources/easytag/2.4/easytag-2.4.3.tar.xz"
+  sha256 "fc51ee92a705e3c5979dff1655f7496effb68b98f1ada0547e8cbbc033b67dd5"
+
+  depends_on "pkg-config" => :build
+  depends_on "intltool" => :build
+  depends_on "itstool" => :build
+  depends_on "gtk+3"
+  depends_on "hicolor-icon-theme"
+  depends_on "gnome-icon-theme"
+  depends_on "id3lib"
+  depends_on "libid3tag"
+  depends_on "taglib"
+
+  depends_on "libvorbis"
+  depends_on "flac"
+  depends_on "libogg"
+  depends_on "speex"
+  depends_on "wavpack"
+
+  # disable gtk-update-icon-cache
+  patch :DATA
+
+  def install
+    ENV.append_path "PYTHONPATH", "#{Formula["libxml2"].opt_lib}/python2.7/site-packages"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--disable-schemas-compile"
+    system "make"
+    ENV.deparallelize # make install fails in parallel
+    system "make", "install"
+  end
+
+  def post_install
+    system "#{Formula["glib"].opt_bin}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
+    system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
+  end
+
+  test do
+    system "#{bin}/easytag", "--version"
+  end
+end
+
+__END__
+diff --git a/Makefile.in b/Makefile.in
+index 9dbde5f..4ffe52e 100644
+--- a/Makefile.in
++++ b/Makefile.in
+@@ -3960,8 +3960,6 @@ data/org.gnome.EasyTAG.gschema.valid: data/.dstamp
+ @ENABLE_MAN_TRUE@		--path $(builddir)/doc --output $(builddir)/doc/ \
+ @ENABLE_MAN_TRUE@		http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl $<
+
+-install-data-hook: install-update-icon-cache
+-uninstall-hook: uninstall-update-icon-cache
+
+ install-update-icon-cache:
+	$(AM_V_at)$(POST_INSTALL)
