c2860331b6cfdf4f2b9e5cf5cb7b9f67fbf450fd
colezlaw <will@cltnc.us>
2017-01-22 17:36:47 -0500

    dependency-check 1.4.5
    Closes #9169.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/dependency-check.rb
+++ Formula/dependency-check.rb
@@ -1,9 +1,9 @@
 class DependencyCheck < Formula
   desc "OWASP Dependency Check"
   homepage "https://www.owasp.org/index.php/OWASP_Dependency_Check"
-  url "https://dl.bintray.com/jeremy-long/owasp/dependency-check-1.4.4-release.zip"
-  version "1.4.4"
-  sha256 "48f224b178d2f8ae9a7e0207e0a4a86f1291a36f8b396ad29b1e023d17a38086"
+  url "https://dl.bintray.com/jeremy-long/owasp/dependency-check-1.4.5-release.zip"
+  version "1.4.5"
+  sha256 "129323b238c372b156c0c315d0b365518025765f3c0855f0ec6ac385f91b91be"
 
   bottle :unneeded
 
@@ -32,7 +32,7 @@ class DependencyCheck < Formula
     output = shell_output("#{libexec}/bin/dependency-check --version").strip
     assert_match "Dependency-Check Core version #{version}", output
 
-    (testpath/"temp-props.properties").write "cve.startyear=2016"
+    (testpath/"temp-props.properties").write "cve.startyear=2017"
     system bin/"dependency-check", "-P", "temp-props.properties", "-f", "XML",
                "--project", "dc", "-s", libexec, "-d", testpath, "-o", testpath
     assert File.exist?(testpath/"dependency-check-report.xml")
