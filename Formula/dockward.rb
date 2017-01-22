88f55d4d5a4cd760a2a5e7a5931ca86d23b7303b
ilovezfs <ilovezfs@icloud.com>
2017-01-22 08:45:06 -0800

    dockward 0.0.4
    Closes #9159.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dockward.rb
+++ Formula/dockward.rb
@@ -3,9 +3,8 @@ require "language/go"
 class Dockward < Formula
   desc "Port forwarding tool for Docker containers."
   homepage "https://github.com/abiosoft/dockward"
-  url "https://github.com/abiosoft/dockward/archive/0.0.3.tar.gz"
-  sha256 "afe9e7d8e8c6e2f60fb79516e90e7fb95b088eb444517aa5f0811f325a967a49"
-
+  url "https://github.com/abiosoft/dockward/archive/0.0.4.tar.gz"
+  sha256 "b96244386ae58aefb16177837d7d6adf3a9e6d93b75eea3308a45eb8eb9f4116"
   head "https://github.com/abiosoft/dockward.git"
 
   bottle do
@@ -20,39 +19,41 @@ class Dockward < Formula
 
   go_resource "github.com/Sirupsen/logrus" do
     url "https://github.com/Sirupsen/logrus.git",
-        :revision => "a26f43589d737684363ff856c5a0f9f24b946510"
+        :revision => "61e43dc76f7ee59a82bdf3d71033dc12bea4c77d"
+  end
+
+  go_resource "github.com/docker/distribution" do
+    url "https://github.com/docker/distribution.git",
+        :revision => "7a0972304e201e2a5336a69d00e112c27823f554"
   end
 
   go_resource "github.com/docker/engine-api" do
     url "https://github.com/docker/engine-api.git",
-        :revision => "fba5dc8922bbc5098a0da24704c04ae3c4bf8b4a"
+        :revision => "4290f40c056686fcaa5c9caf02eac1dde9315adf"
   end
 
   go_resource "github.com/docker/go-connections" do
     url "https://github.com/docker/go-connections.git",
-        :revision => "f549a9393d05688dff0992ef3efd8bbe6c628aeb"
+        :revision => "eb315e36415380e7c2fdee175262560ff42359da"
   end
 
   go_resource "github.com/docker/go-units" do
     url "https://github.com/docker/go-units.git",
-        :revision => "5d2041e26a699eaca682e2ea41c8f891e1060444"
+        :revision => "e30f1e79f3cd72542f2026ceec18d3bd67ab859c"
   end
 
   go_resource "golang.org/x/net" do
     url "https://go.googlesource.com/net.git",
-        :revision => "042ba42fa6633b34205efc66ba5719cd3afd8d38"
+        :revision => "f2499483f923065a842d38eb4c7f1927e6fc6e6d"
   end
 
   def install
     ENV["GOBIN"] = bin
     ENV["GOPATH"] = buildpath
-    ENV["GOHOME"] = buildpath
-
-    path = buildpath/"src/github.com/abiosoft/dockward"
-    path.install Dir["*"]
+    (buildpath/"src/github.com/abiosoft").mkpath
+    ln_s buildpath, buildpath/"src/github.com/abiosoft/dockward"
     Language::Go.stage_deps resources, buildpath/"src"
-
-    system "go", "build", "-o", "#{bin}/dockward", "-v", "github.com/abiosoft/dockward"
+    system "go", "install", "github.com/abiosoft/dockward"
   end
 
   test do
