0eca2b486d0f7d70641d3de906262ea740915432
Alex Gaynor <alex.gaynor@gmail.com>
2017-01-22 15:03:21 -0500

    afl-fuzz 2.37b
    Closes #9166.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/afl-fuzz.rb
+++ Formula/afl-fuzz.rb
@@ -1,8 +1,8 @@
 class AflFuzz < Formula
   desc "American fuzzy lop: Security-oriented fuzzer"
   homepage "http://lcamtuf.coredump.cx/afl/"
-  url "http://lcamtuf.coredump.cx/afl/releases/afl-2.36b.tgz"
-  sha256 "d36e2643eaddbd6e79a0dd835fd6e23428198a7a7cd640e96ccbf21d5bd8bcc8"
+  url "http://lcamtuf.coredump.cx/afl/releases/afl-2.37b.tgz"
+  sha256 "9cf072c9a1a92e5ed8d3cc69addd2ca7c5e562db88505e09dc060167038c299b"
 
   bottle do
     sha256 "cd205cd0f6246e86f0a154deffea52503e4229f4b2b52b4820d2732b310882a3" => :sierra
