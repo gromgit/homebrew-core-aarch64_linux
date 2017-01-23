1167cd24248dabdd149cafd13ca0d43c76cc619c
Mike McQuaid <mike@mikemcquaid.com>
2017-01-23 15:26:31 +0000

    rails-completion: import from homebrew/completions.

---
+++ Formula/rails-completion.rb
@@ -0,0 +1,20 @@
+class RailsCompletion < Formula
+  desc "Bash completion for Rails"
+  homepage "https://github.com/mernen/completion-ruby"
+  url "https://github.com/mernen/completion-ruby.git",
+    :revision => "f3e4345042b0cc48317e45b673dfd3d23904b9a7"
+  version "2"
+
+  head "https://github.com/mernen/completion-ruby.git"
+
+  bottle :unneeded
+
+  def install
+    bash_completion.install "completion-rails" => "rails"
+  end
+
+  test do
+    assert_match "-F __rails",
+      shell_output("source #{bash_completion}/rails && complete -p rails")
+  end
+end
