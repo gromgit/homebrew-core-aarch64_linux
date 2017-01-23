de00a3dea4b36e2645220b00d79c00f35bda1ae5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-23 11:17:35 +0000

    gsmartcontrol: add 0.8.7_2 bottle.

--- Formula/gsmartcontrol.rb
+++ Formula/gsmartcontrol.rb
@@ -5,6 +5,12 @@ class Gsmartcontrol < Formula
   sha256 "708fa803243abb852ed52050fc82cd3592a798c02743342441996e77f19ffec6"
   revision 2
 
+  bottle do
+    sha256 "74418f9a4fd88bb957d1c10725ffc509b9955075646b7ac0b18d9ef3c65730b4" => :sierra
+    sha256 "1d5d9c1007c083f8806a6243eca3907c16986d667bdca947379d95de409a4b2c" => :el_capitan
+    sha256 "adf4259a04f10319300f83ffe55f309271490763af48dc8a02ab05cc1009b00f" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "smartmontools"
   depends_on "gtkmm"
