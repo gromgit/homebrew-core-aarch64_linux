b65be7c5525fd78c06f8366d01edd089ae3eae57
Mike McQuaid <mike@mikemcquaid.com>
2017-01-23 15:26:29 +0000

    docker-completion: import from homebrew/completions.

---
+++ Formula/docker-completion.rb
@@ -0,0 +1,23 @@
+class DockerCompletion < Formula
+  desc "Bash, Zsh and Fish completion for Docker"
+  homepage "https://github.com/docker/docker"
+  url "https://github.com/docker/docker/archive/v1.13.0.tar.gz"
+  sha256 "845cc9a7a48ed1cc60f062882285358a1c2a845b74c5d1d859606a28e4a47d3e"
+  head "https://github.com/docker/docker"
+
+  bottle :unneeded
+
+  conflicts_with "docker",
+    :because => "docker already includes these completion scripts"
+
+  def install
+    bash_completion.install "contrib/completion/bash/docker"
+    fish_completion.install "contrib/completion/fish/docker.fish"
+    zsh_completion.install "contrib/completion/zsh/_docker"
+  end
+
+  test do
+    assert_match "-F _docker",
+      shell_output("bash -c 'source #{bash_completion}/docker && complete -p docker'")
+  end
+end
