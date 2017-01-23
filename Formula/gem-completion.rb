06719657602ee98cb019b8ae9a73be64fe4c46c2
Mike McQuaid <mike@mikemcquaid.com>
2017-01-23 15:26:30 +0000

    gem-completion: import from homebrew/completions.

---
+++ Formula/gem-completion.rb
@@ -0,0 +1,20 @@
+class GemCompletion < Formula
+  desc "Bash completion for gem"
+  homepage "https://github.com/mernen/completion-ruby"
+  url "https://github.com/mernen/completion-ruby.git",
+    :revision => "f3e4345042b0cc48317e45b673dfd3d23904b9a7"
+  version "2"
+
+  head "https://github.com/mernen/completion-ruby.git"
+
+  bottle :unneeded
+
+  def install
+    bash_completion.install "completion-gem" => "gem"
+  end
+
+  test do
+    assert_match "-F __gem",
+      shell_output("source #{bash_completion}/gem && complete -p gem")
+  end
+end
