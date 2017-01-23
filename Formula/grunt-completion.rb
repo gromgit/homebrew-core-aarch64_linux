7e1f8fc17ce4807fee87abb74a74f5efdc90073c
Mike McQuaid <mike@mikemcquaid.com>
2017-01-23 15:26:30 +0000

    grunt-completion: import from homebrew/completions.

---
+++ Formula/grunt-completion.rb
@@ -0,0 +1,19 @@
+class GruntCompletion < Formula
+  desc "Bash and Zsh completion for Grunt"
+  homepage "http://gruntjs.com/"
+  url "https://github.com/gruntjs/grunt-cli/archive/v0.1.13.tar.gz"
+  sha256 "bb291c97f5ac5dc3f549343436f64ff066a0138565e15c794b1636d37fdc4992"
+  head "https://github.com/gruntjs/grunt-cli.git"
+
+  bottle :unneeded
+
+  def install
+    bash_completion.install "completion/bash" => "grunt"
+    zsh_completion.install "completion/zsh" => "_grunt"
+  end
+
+  test do
+    assert_match "-F _grunt_completions",
+      shell_output("source #{bash_completion}/grunt && complete -p grunt")
+  end
+end
