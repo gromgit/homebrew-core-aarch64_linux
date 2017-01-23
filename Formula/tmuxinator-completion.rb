d89d0d3b583902d000225b2ade443e9391befd20
Mike McQuaid <mike@mikemcquaid.com>
2017-01-23 15:26:32 +0000

    tmuxinator-completion: import from homebrew/completions.

---
+++ Formula/tmuxinator-completion.rb
@@ -0,0 +1,21 @@
+class TmuxinatorCompletion < Formula
+  desc "Shell completion for Tmuxinator"
+  homepage "https://github.com/tmuxinator/tmuxinator"
+  url "https://github.com/tmuxinator/tmuxinator/archive/v0.9.0.tar.gz"
+  sha256 "dd4924d13a2b4b26bf4823c342c6bd773a7c393e5da1e858e6dfad216e59e5fb"
+
+  head "https://github.com/tmuxinator/tmuxinator.git"
+
+  bottle :unneeded
+
+  def install
+    bash_completion.install "completion/tmuxinator.bash" => "tmuxinator"
+    zsh_completion.install "completion/tmuxinator.zsh" => "_tmuxinator"
+    fish_completion.install Dir["completion/*.fish"]
+  end
+
+  test do
+    assert_match "-F _tmuxinator",
+      shell_output("source #{bash_completion}/tmuxinator && complete -p tmuxinator")
+  end
+end
