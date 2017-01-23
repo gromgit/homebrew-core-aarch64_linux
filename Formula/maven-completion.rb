b478d52dd6b10fdc83a91f170282af47f2a59eb0
Mike McQuaid <mike@mikemcquaid.com>
2017-01-23 15:26:30 +0000

    maven-completion: import from homebrew/completions.

---
+++ Formula/maven-completion.rb
@@ -0,0 +1,20 @@
+class MavenCompletion < Formula
+  desc "Bash completion for Maven"
+  homepage "https://github.com/juven/maven-bash-completion"
+  url "https://github.com/juven/maven-bash-completion.git",
+    :revision => "106b6ca03badc9474cf7b6b1b7039ad950a17f89"
+  version "20160501"
+
+  head "https://github.com/juven/maven-bash-completion.git"
+
+  bottle :unneeded
+
+  def install
+    bash_completion.install "bash_completion.bash" => "maven"
+  end
+
+  test do
+    assert_match "-F _mvn",
+      shell_output("source #{bash_completion}/maven && complete -p mvn")
+  end
+end
