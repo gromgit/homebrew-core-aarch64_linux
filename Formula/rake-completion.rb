7916adfa2a8c29fb55b880f3f0c139f14a619e64
Mike McQuaid <mike@mikemcquaid.com>
2017-01-23 15:26:31 +0000

    rake-completion: import from homebrew/completions.

---
+++ Formula/rake-completion.rb
@@ -0,0 +1,16 @@
+class RakeCompletion < Formula
+  desc "Bash completion for Rake"
+  homepage "https://github.com/MasterRoot24/rake-completion"
+  url "https://github.com/MasterRoot24/rake-completion/archive/v1.0.0.tar.gz"
+  sha256 "2d619d0d1d8052994011209c62f926b9e41d45e9268da4b9858fa45911b04cd1"
+  bottle :unneeded
+
+  def install
+    bash_completion.install "rake.sh" => "rake"
+  end
+
+  test do
+    assert_match "-F _rakecomplete",
+      shell_output("source #{bash_completion}/rake && complete -p rake")
+  end
+end
