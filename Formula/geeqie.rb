69b95cf99ee7a59600834fbfbab80d3e8bde838d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-23 11:17:34 +0000

    geeqie: add 1.3 bottle.

--- Formula/geeqie.rb
+++ Formula/geeqie.rb
@@ -4,6 +4,12 @@ class Geeqie < Formula
   url "http://www.geeqie.org/geeqie-1.3.tar.xz"
   sha256 "4b6f566dd1a8badac68c4353c7dd0f4de17f8627b85a7a70d5eb1ae3b540ec3f"
 
+  bottle do
+    sha256 "81d37ee2270c7c7f0b0c38042ee06bdbf3ec1ca4465b75e9687b033a77a8aa94" => :sierra
+    sha256 "1eaf69673392f31b14190f6c8430e75a6621e0cad74ad79d3df130da1586e27b" => :el_capitan
+    sha256 "b0855a3824ee1d7606c7c7a79b879360536ade7b355b76da57ff3d2a81271d6a" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
