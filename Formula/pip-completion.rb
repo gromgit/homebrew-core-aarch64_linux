964e895de52c7276a21f4c75dfaf6f0c1e7cff85
Mike McQuaid <mike@mikemcquaid.com>
2017-01-23 15:26:31 +0000

    pip-completion: import from homebrew/completions.

---
+++ Formula/pip-completion.rb
@@ -0,0 +1,20 @@
+class PipCompletion < Formula
+  desc "Bash completion for Pip"
+  homepage "https://github.com/ekalinin/pip-bash-completion"
+  url "https://github.com/ekalinin/pip-bash-completion.git",
+    :revision => "92faa3e4504428ea658a1e83e54caf08cdbb555a"
+  version "20150819"
+
+  head "https://github.com/ekalinin/pip-bash-completion.git"
+
+  bottle :unneeded
+
+  def install
+    bash_completion.install "pip"
+  end
+
+  test do
+    assert_match "-F _pip",
+      shell_output("source #{bash_completion}/pip && complete -p pip")
+  end
+end
