ba30d05dd24ecd25a222022e69f1f1f07c42c8e0
Mike McQuaid <mike@mikemcquaid.com>
2017-01-23 15:26:31 +0000

    t-completion: import from homebrew/completions.

---
+++ Formula/t-completion.rb
@@ -0,0 +1,19 @@
+class TCompletion < Formula
+  desc "Completion for CLI power tool for Twitter"
+  homepage "http://sferik.github.com/t"
+  url "https://github.com/sferik/t/archive/v3.1.0.tar.gz"
+  sha256 "900ef6e3d6180b70bf2434503774ea5e1bf985b9110d4f051c44a191b08f6062"
+  head "https://github.com/sferik/t.git"
+
+  bottle :unneeded
+
+  def install
+    bash_completion.install "etc/t-completion.sh" => "t"
+    zsh_completion.install "etc/t-completion.zsh" => "_t"
+  end
+
+  test do
+    assert_match "-F _t",
+      shell_output("source #{bash_completion}/t && complete -p t")
+  end
+end
