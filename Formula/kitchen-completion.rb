d4047f65fb64f75e0f3002a9928f5811fefbe7b9
Mike McQuaid <mike@mikemcquaid.com>
2017-01-23 15:26:30 +0000

    kitchen-completion: import from homebrew/completions.

---
+++ Formula/kitchen-completion.rb
@@ -0,0 +1,19 @@
+class KitchenCompletion < Formula
+  desc "bash completion for Kitchen"
+  homepage "https://github.com/MarkBorcherding/test-kitchen-bash-completion"
+  url "https://github.com/MarkBorcherding/test-kitchen-bash-completion/archive/v1.0.0.tar.gz"
+  sha256 "6a9789359dab220df0afad25385dd3959012cfa6433c8c96e4970010b8cfc483"
+
+  head "https://github.com/MarkBorcherding/test-kitchen-bash-completion.git"
+
+  bottle :unneeded
+
+  def install
+    bash_completion.install "kitchen-completion.bash" => "kitchen"
+  end
+
+  test do
+    assert_match "-F __kitchen_options",
+      shell_output("source #{bash_completion}/kitchen && complete -p kitchen")
+  end
+end
