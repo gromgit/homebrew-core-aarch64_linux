70a9650981d05eeea37329fc5ac2512ced590ce5
Mike McQuaid <mike@mikemcquaid.com>
2017-01-23 15:26:29 +0000

    django-completion: import from homebrew/completions.

---
+++ Formula/django-completion.rb
@@ -0,0 +1,18 @@
+class DjangoCompletion < Formula
+  desc "Bash completion for Django"
+  homepage "https://github.com/django/django"
+  url "https://github.com/django/django/archive/1.10.5.tar.gz"
+  sha256 "65e2e8bcfb630cc26ac40599d3b0745efd66c5da138843e933e13fafeccb4742"
+  head "https://github.com/django/django.git"
+
+  bottle :unneeded
+
+  def install
+    bash_completion.install "extras/django_bash_completion" => "django"
+  end
+
+  test do
+    assert_match "-F _django_completion",
+      shell_output("source #{bash_completion}/django && complete -p django-admin.py")
+  end
+end
