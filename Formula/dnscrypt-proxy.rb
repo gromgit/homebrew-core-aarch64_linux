6b0fb0894777f17426c8f4e9cd422306d68ed2a3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-24 14:42:36 +0000

    dnscrypt-proxy: update 1.9.4 bottle.

--- Formula/dnscrypt-proxy.rb
+++ Formula/dnscrypt-proxy.rb
@@ -6,9 +6,9 @@ class DnscryptProxy < Formula
   head "https://github.com/jedisct1/dnscrypt-proxy.git"
 
   bottle do
-    sha256 "8ba7499cf515996462c3826bc475e3834f3f2bb032fa385c667a4ef844e2eb81" => :sierra
-    sha256 "14d57e687899746ede2dad8370484d8ed19e338a65a171c5c0008d74df92bb8a" => :el_capitan
-    sha256 "6a88a97fe3b416865c8e0d17042f12247a0dc25f4650760a077efb0735198f78" => :yosemite
+    sha256 "181a68875712338b03f0b900974cba5e458b51bea8889a50c6d19519d4bd3c75" => :sierra
+    sha256 "f90876292441fb13b976ebb329bc7e7d26f71c57674945a7dd538abc9a71e768" => :el_capitan
+    sha256 "8bf5ff9dd9ff91c03fb839869a1354d0fc95cb494a7526b5c1980fdff120fcb7" => :yosemite
   end
 
   option "with-plugins", "Support plugins and install example plugins."
