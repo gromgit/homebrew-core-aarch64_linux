e7534b285ac8285c84984d6a6540490f2fd0d1a4
ilovezfs <ilovezfs@icloud.com>
2017-01-24 02:14:50 -0800

    ammonite-repl 0.8.2
    Closes #9218.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/ammonite-repl.rb
+++ Formula/ammonite-repl.rb
@@ -1,8 +1,8 @@
 class AmmoniteRepl < Formula
   desc "Ammonite is a cleanroom re-implementation of the Scala REPL"
   homepage "https://lihaoyi.github.io/Ammonite/#Ammonite-REPL"
-  url "https://github.com/lihaoyi/Ammonite/releases/download/0.8.1/0.8.1", :using => :nounzip
-  sha256 "a1e8561c492baa747ab9aeffc9ea040683ca81a9962735af772d64ec7c019e77"
+  url "https://github.com/lihaoyi/Ammonite/releases/download/0.8.2/2.12-0.8.2", :using => :nounzip
+  sha256 "4084101271944253d0528587d437e9f15c2661c3289b50bb69925a3077c3fc39"
 
   bottle :unneeded
 
