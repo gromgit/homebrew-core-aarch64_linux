6290cce050c4a60ed10cf266f02577b7bcf9526c
Mike McQuaid <mike@mikemcquaid.com>
2017-01-25 10:24:16 +0000

    feh: import from homebrew/x11.

---
+++ Formula/feh.rb
@@ -0,0 +1,21 @@
+class Feh < Formula
+  desc "X11 image viewer"
+  homepage "https://feh.finalrewind.org/"
+  url "https://feh.finalrewind.org/feh-2.16.1.tar.bz2"
+  sha256 "6e55289a3be4495a437a0b037c7b5e86edf64ec74ab63d2d26fa50df1b62b6b3"
+
+  depends_on :x11
+  depends_on "imlib2"
+  depends_on "libexif" => :recommended
+
+  def install
+    args = []
+    args << "exif=1" if build.with? "libexif"
+    system "make", "PREFIX=#{prefix}", *args
+    system "make", "PREFIX=#{prefix}", "install"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/feh -v")
+  end
+end
