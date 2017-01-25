0d8ba9ac74abfe3b771a17669d48af7bf9d498b3
Mike McQuaid <mike@mikemcquaid.com>
2017-01-25 10:24:18 +0000

    startup-notification: import from homebrew/x11.

---
+++ Formula/startup-notification.rb
@@ -0,0 +1,21 @@
+class StartupNotification < Formula
+  desc "Reference implementation of startup notification protocol"
+  homepage "https://www.freedesktop.org/wiki/Software/startup-notification/"
+  url "https://www.freedesktop.org/software/startup-notification/releases/startup-notification-0.12.tar.gz"
+  sha256 "3c391f7e930c583095045cd2d10eb73a64f085c7fde9d260f2652c7cb3cfbe4a"
+
+  depends_on "pkg-config" => :build
+  depends_on :x11
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/startup-notification", "--version"
+  end
+end
