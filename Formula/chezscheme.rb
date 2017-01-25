c5b6f0e1adf458257861688a3360f5f8faa56ad9
Mike McQuaid <mike@mikemcquaid.com>
2017-01-25 10:24:16 +0000

    chezscheme: import from homebrew/x11.

---
+++ Formula/chezscheme.rb
@@ -0,0 +1,28 @@
+class Chezscheme < Formula
+  desc "Chez Scheme"
+  homepage "https://cisco.github.io/ChezScheme/"
+  url "https://github.com/cisco/ChezScheme/archive/v9.4.tar.gz"
+  sha256 "9f4e6fe737300878c3c9ca6ed09ed97fc2edbf40e4cf37bd61f48a27f5adf952"
+
+  depends_on :x11 => :build
+
+  def install
+    system "./configure",
+              "--installprefix=#{prefix}",
+              "--threads",
+              "--installschemename=chez"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"hello.ss").write <<-EOS.undent
+      (display "Hello, World!") (newline)
+    EOS
+
+    expected = <<-EOS.undent
+      Hello, World!
+    EOS
+
+    assert_equal expected, shell_output("#{bin}/chez --script hello.ss")
+  end
+end
