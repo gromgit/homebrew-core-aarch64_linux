5e9deac6fd4c50201395003e4e3b91e533560539
Mike McQuaid <mike@mikemcquaid.com>
2017-01-25 10:24:18 +0000

    x3270: import from homebrew/x11.

---
+++ Formula/x3270.rb
@@ -0,0 +1,31 @@
+class X3270 < Formula
+  desc "IBM 3270 terminal emulator for the X Window System and Windows"
+  homepage "http://x3270.bgp.nu/"
+  url "https://downloads.sourceforge.net/project/x3270/x3270/3.5ga9/suite3270-3.5ga9-src.tgz"
+  sha256 "654756cc1204fd69a861d416d350a0ab3c9cea317173a80b06aca0402a517d3e"
+
+  option "with-c3270", "Include c3270 (curses-based version)"
+  option "with-s3270", "Include s3270 (displayless version)"
+  option "with-tcl3270", "Include tcl3270 (integrated with Tcl)"
+  option "with-pr3287", "Include pr3287 (printer emulation)"
+
+  depends_on :x11
+  depends_on "openssl"
+
+  def install
+    args = ["--prefix=#{prefix}"]
+    args << "--enable-x3270"
+    args << "--enable-c3270" if build.with? "c3270"
+    args << "--enable-s3270" if build.with? "s3270"
+    args << "--enable-tcl3270" if build.with? "tcl3270"
+    args << "--enable-pr3287" if build.with? "pr3287"
+
+    system "./configure", *args
+    system "make", "install"
+    system "make", "install.man"
+  end
+
+  test do
+    system bin/"x3270", "--version"
+  end
+end
