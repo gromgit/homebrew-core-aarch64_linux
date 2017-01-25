0176d9da7f9d2c1aeec8dc9f193e22fe7a7212d9
Mike McQuaid <mike@mikemcquaid.com>
2017-01-25 10:24:16 +0000

    dwm: import from homebrew/x11.

---
+++ Formula/dwm.rb
@@ -0,0 +1,36 @@
+class Dwm < Formula
+  desc "Dynamic window manager"
+  homepage "http://dwm.suckless.org/"
+  url "http://dl.suckless.org/dwm/dwm-6.1.tar.gz"
+  sha256 "c2f6c56167f0acdbe3dc37cca9c1a19260c040f2d4800e3529a21ad7cce275fe"
+  head "http://git.suckless.org/dwm", :using => :git
+
+  depends_on :x11
+  depends_on "dmenu" => :optional
+
+  def install
+    # The dwm default quit keybinding Mod1-Shift-q collides with
+    # the Mac OS X Log Out shortcut in the Apple menu.
+    inreplace "config.def.h",
+    "{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },",
+    "{ MODKEY|ControlMask,           XK_q,      quit,           {0} },"
+    inreplace "dwm.1", '.B Mod1\-Shift\-q', '.B Mod1\-Control\-q'
+    system "make", "PREFIX=#{prefix}", "install"
+  end
+
+  def caveats; <<-EOS.undent
+    In order to use the Mac OS X command key for dwm commands,
+    change the X11 keyboard modifier map using xmodmap (1).
+
+    e.g. by running the following command from $HOME/.xinitrc
+    xmodmap -e 'remove Mod2 = Meta_L' -e 'add Mod1 = Meta_L'&
+
+    See also https://gist.github.com/311377 for a handful of tips and tricks
+    for running dwm on Mac OS X.
+    EOS
+  end
+
+  test do
+    assert_match /#{version}/, shell_output("#{bin}/dwm -v 2>&1", 1)
+  end
+end
