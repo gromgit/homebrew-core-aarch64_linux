90d610e56015eaf858e804611576a7f1a18e4f47
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-25 09:43:37 +0000

    pick: update 1.5.4 bottle.

--- Formula/pick.rb
+++ Formula/pick.rb
@@ -6,9 +6,9 @@ class Pick < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "77eacf48b9f317239e5d9fcfa62dbaf90fb11fbbf603d7b4652b456fe563bdde" => :sierra
-    sha256 "af14394e7e4374fbdb960ce07e5128f669a09dd6ef325ea229d26d2e7e627715" => :el_capitan
-    sha256 "75e6b3b7b4e85f81b8c5528c2cd78a36e4601b5932908b1ebdd522a934c3d6fc" => :yosemite
+    sha256 "320de1fd40ca46c11a239b3f86e0cdd4a9fb82791670dc30f8cad59bf6d5a4c6" => :sierra
+    sha256 "73c0f2f2702e6380161ebf0a7d503d549e287c8e38f84bd9676e70c18f625d91" => :el_capitan
+    sha256 "fdc4572e9871a980a7a4aae10f2799fc94d0db5617ce32acebdd4636631e6770" => :yosemite
   end
 
   def install
