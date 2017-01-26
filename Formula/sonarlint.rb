9be4625d7e3aa53655bb161bb5b2f1e18b2ffc9f
Adam Moss <adam.moss@bcs.org.uk>
2017-01-26 20:28:28 +0000

    sonarlint 2.1.0.566
    Closes #9301.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/sonarlint.rb
+++ Formula/sonarlint.rb
@@ -1,8 +1,8 @@
 class Sonarlint < Formula
   desc "provides pre-commit feedback on quality issues injected into code."
   homepage "http://www.sonarlint.org/commandline/index.html"
-  url "https://bintray.com/sonarsource/Distribution/download_file?file_path=sonarlint-cli%2Fsonarlint-cli-2.0.zip"
-  sha256 "dafbaba032330f898e0f750f80ac212260cf825070dced1c2376c07c9d34c007"
+  url "https://bintray.com/sonarsource/Distribution/download_file?file_path=sonarlint-cli%2Fsonarlint-cli-2.1.0.566.zip"
+  sha256 "75320a0629f3f7b83ccaa4098ccbbb8decedc518a1b8fc2503f753266dc71d70"
 
   bottle :unneeded
 
