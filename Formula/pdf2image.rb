5da09d3002ef650db2d87f8bebb6c74ad96dcc05
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-27 02:33:31 +0000

    pdf2image: add 0.53 bottle.

--- Formula/pdf2image.rb
+++ Formula/pdf2image.rb
@@ -4,6 +4,12 @@ class Pdf2image < Formula
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/pdf2image/pdf2image-0.53-source.tar.gz"
   sha256 "e8672c3bdba118c83033c655d90311db003557869c92903e5012cdb368a68982"
 
+  bottle do
+    sha256 "46df7ae58a1bfc73cd1a1c1075de032724514d29995e965169d69f51409cad7e" => :sierra
+    sha256 "c12d781ab5136a717cb88cadb50b2dfcd1f67cf263b5b668b1e171f562bcb072" => :el_capitan
+    sha256 "a0bb792123e4754d5cf80cf248e8932dd1885616af2c4c9c7f00e35cda962725" => :yosemite
+  end
+
   depends_on :x11
   depends_on "freetype"
   depends_on "ghostscript"
