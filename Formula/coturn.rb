4abb3b2708dcf03639e086d55104fea81e35a182
JCount <JCount42@gmail.com>
2017-01-27 13:41:57 -0500

    coturn: revision for libevent

--- Formula/coturn.rb
+++ Formula/coturn.rb
@@ -3,6 +3,7 @@ class Coturn < Formula
   homepage "https://github.com/coturn/coturn"
   url "http://turnserver.open-sys.org/downloads/v4.5.0.5/turnserver-4.5.0.5.tar.gz"
   sha256 "8484fa6c8d4aab43e1161c02eb8914154a21178b05f8a285e04094ddbb64acf4"
+  revision 1
 
   bottle do
     sha256 "d22be2c9222767571e4e403a0ba8c266ea652e2bf1c2918c5ed6ed898f294487" => :sierra
