053a787a59e07c249bf207d7fe423c7e0588956c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-27 02:33:28 +0000

    chezscheme: add 9.4 bottle.

--- Formula/chezscheme.rb
+++ Formula/chezscheme.rb
@@ -4,6 +4,11 @@ class Chezscheme < Formula
   url "https://github.com/cisco/ChezScheme/archive/v9.4.tar.gz"
   sha256 "9f4e6fe737300878c3c9ca6ed09ed97fc2edbf40e4cf37bd61f48a27f5adf952"
 
+  bottle do
+    sha256 "b781813aa808743355d6f5287ea614d648c2394023f61179c2747fe9e367347b" => :sierra
+    sha256 "0ba364536190033f5758ef73f673f42054b636c843430ee7a7cdccba312fbc79" => :yosemite
+  end
+
   depends_on :x11 => :build
 
   def install
