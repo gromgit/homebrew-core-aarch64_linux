48693dc79bb4d52f68881e2e218e537c28f5d218
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-27 02:33:30 +0000

    morse: add 2.5 bottle.

--- Formula/morse.rb
+++ Formula/morse.rb
@@ -4,6 +4,13 @@ class Morse < Formula
   url "http://www.catb.org/~esr/morse/morse-2.5.tar.gz"
   sha256 "476d1e8e95bb173b1aadc755db18f7e7a73eda35426944e1abd57c20307d4987"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d779902b961e9ebbfa41b0906d8d41357232fd4da83a393e112cde87f5bcdcaa" => :sierra
+    sha256 "491a1ea5455d058af9adb607e0e49d95b94e52f0068cd5fb197c1ea71666b524" => :el_capitan
+    sha256 "c89c45cdc2ff59d6ac327188c484659c769fe94a07e5e1f38f4d568f0b1a943d" => :yosemite
+  end
+
   depends_on :x11
 
   def install
