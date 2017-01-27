8b377b1a3db8515491cd6a3eea86efff4457b3c0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-27 02:33:32 +0000

    x3270: add 3.5ga9 bottle.

--- Formula/x3270.rb
+++ Formula/x3270.rb
@@ -4,6 +4,12 @@ class X3270 < Formula
   url "https://downloads.sourceforge.net/project/x3270/x3270/3.5ga9/suite3270-3.5ga9-src.tgz"
   sha256 "654756cc1204fd69a861d416d350a0ab3c9cea317173a80b06aca0402a517d3e"
 
+  bottle do
+    sha256 "1cd09052ab45a091596bf1be4cc8bab7acbbd8adf0b28f7667590c8b46871230" => :sierra
+    sha256 "2bed418e88841d641907b730a260dfcac9394c1c69b24eadcdf32e00570f2c0f" => :el_capitan
+    sha256 "897d18de2b85172e751719687710f20b1717293b10dac669d6eb052e08d4b02a" => :yosemite
+  end
+
   option "with-c3270", "Include c3270 (curses-based version)"
   option "with-s3270", "Include s3270 (displayless version)"
   option "with-tcl3270", "Include tcl3270 (integrated with Tcl)"
