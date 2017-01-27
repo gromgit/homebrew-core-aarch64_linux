915eef8e8444e05b4dd38948a809addf5d9c7883
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-27 02:33:29 +0000

    feh: add 2.16.1 bottle.

--- Formula/feh.rb
+++ Formula/feh.rb
@@ -4,6 +4,12 @@ class Feh < Formula
   url "https://feh.finalrewind.org/feh-2.16.1.tar.bz2"
   sha256 "6e55289a3be4495a437a0b037c7b5e86edf64ec74ab63d2d26fa50df1b62b6b3"
 
+  bottle do
+    sha256 "5f3fef786e5fcd5f4425ae63dc4dcb1091a98388d38755702f9d2a354a8afff5" => :sierra
+    sha256 "507e05ec459723730987908f2f4bd89c2f6fd5a63bf3d5ae7f09314c58edebfc" => :el_capitan
+    sha256 "51c5e457dda2a02afc900ef46c6cb8d1d2cf88e2ff5b471dd584de3d4e3630d1" => :yosemite
+  end
+
   depends_on :x11
   depends_on "imlib2"
   depends_on "libexif" => :recommended
