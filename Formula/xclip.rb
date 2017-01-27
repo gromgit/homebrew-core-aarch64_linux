8b0b3615ae4196888ac7b90818eae510119a0d35
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-27 02:33:32 +0000

    xclip: add 0.12 bottle.

--- Formula/xclip.rb
+++ Formula/xclip.rb
@@ -4,6 +4,13 @@ class Xclip < Formula
   url "https://downloads.sourceforge.net/project/xclip/xclip/0.12/xclip-0.12.tar.gz"
   sha256 "b7c7fad059ba446df5692d175c2a1d3816e542549661224806db369a0d716c45"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d2b9571202947dbcb572e6a30c7083a67f95d1a2df67cbf4e416a386ac3b8f5b" => :sierra
+    sha256 "4e5bfbd56dbb84b22542d930e261c0a45e23e287064e19b3932926af6c214466" => :el_capitan
+    sha256 "6870e6606bfbbb10f29d212beae6aa9cdb1fed938a2fe9e54b0acf15ae09de0e" => :yosemite
+  end
+
   depends_on :x11
 
   def install
