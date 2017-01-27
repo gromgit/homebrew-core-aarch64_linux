3f55fba1057e509820411414a5c49c162c5ccb33
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-27 02:33:29 +0000

    fox: add 1.6.51 bottle.

--- Formula/fox.rb
+++ Formula/fox.rb
@@ -4,6 +4,13 @@ class Fox < Formula
   url "http://ftp.fox-toolkit.org/pub/fox-1.6.51.tar.gz"
   sha256 "15a99792965d933a4936e48b671c039657546bdec6a318c223ab1131624403d1"
 
+  bottle do
+    cellar :any
+    sha256 "84e26e4ae534c17cf53eb27f611990d565cf5f942001e131ed9204570cf3c371" => :sierra
+    sha256 "3ae937491777d69408c0062cb7e21e2231037d7e8b3036c6bf0f0e6c83f070ad" => :el_capitan
+    sha256 "06f31b3d710dde932f37b8d24b45791a04cfa3767bee288a03ee31587a156980" => :yosemite
+  end
+
   depends_on :x11
   depends_on "freetype"
   depends_on "libpng"
