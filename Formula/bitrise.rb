7482493101fece0f62a0e655c3d3c7b39541b0a7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-27 10:58:10 +0000

    bitrise: update 1.5.3 bottle.

--- Formula/bitrise.rb
+++ Formula/bitrise.rb
@@ -5,9 +5,9 @@ class Bitrise < Formula
   sha256 "60e1932ff549f3762cee9f87395a8ab5bbfae44342902a38eef33363b8fcfee8"
 
   bottle do
-    sha256 "df413a76ce0de41ebdb4d81b50bf3e3ba5668c4aa7852ed69407e5b11684005b" => :sierra
-    sha256 "c8c2101c0b8c07d93217a7578a19c776b487ecc43194cc0bbad4d8bf8e616eea" => :el_capitan
-    sha256 "c7908d173855ed9ebefc4a4ed302087390a673c35d3c4eeb073354b6946e08de" => :yosemite
+    sha256 "5b465311a9f37945ec97d32c67b0eb3e3723ef9338e2609f9c974e83e5d7b3cb" => :sierra
+    sha256 "1e58391448424f53c34b6b53b58863f7808e5982b3bda99f14c799356d7a3652" => :el_capitan
+    sha256 "0670321b4c9d0df0c3bb237d24b27a8e83f36f2f9450ad776b443f4a513be94b" => :yosemite
   end
 
   depends_on "go" => :build
