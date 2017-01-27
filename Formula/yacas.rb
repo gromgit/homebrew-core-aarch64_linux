2715061e8d0a2a089e9d2490030c58fc89ae9e52
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-27 07:49:44 +0000

    yacas: update 1.6.1 bottle.

--- Formula/yacas.rb
+++ Formula/yacas.rb
@@ -5,12 +5,10 @@ class Yacas < Formula
   sha256 "6b94394f705bed70a9d104967073efd6c23e9eb1a832805c4d805ef875555ae5"
 
   bottle do
-    rebuild 1
-    sha256 "95808542ccd26e5286a42411c0f848bebd63ece8315d4ac92742639bee5eb049" => :sierra
-    sha256 "700fa65e0279625861a0f7166c3e213c807ed7ea46b5f10d5ba6be920aa76a64" => :el_capitan
-    sha256 "c143a1764ec3dd64563e78e75c09b11d519124a10c97614bd5c6fbc99bab058b" => :yosemite
-    sha256 "52c5071ec1f10c3181d11be1cd54d345fef5cdcbd4dcd6ef1d2cd71a8cff1d85" => :mavericks
-    sha256 "c3fb8303de3f7e455047994b26b0cb7edbdee3c466eb77e22a66c42746ea766e" => :mountain_lion
+    cellar :any
+    sha256 "6aef11d42795146a35f9f3b374dfdfbe79b89b5a57c15a7fb3510d27b522c251" => :sierra
+    sha256 "807fafec64ac4c3589f5b760299d3b8fd42b29a66a914a5e6c119337a63bb6ab" => :el_capitan
+    sha256 "c7fb1eb19bdb219645ed2bc94cfed491de1c2eb1f770dfd7a7b4cc63871f0dab" => :yosemite
   end
 
   depends_on "cmake" => :build
