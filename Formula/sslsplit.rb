a6e63d5185415b0022b0a53c1136378a2b765ce8
JCount <JCount42@gmail.com>
2017-01-27 14:41:58 -0500

    sslsplit: revision for libevent

--- Formula/sslsplit.rb
+++ Formula/sslsplit.rb
@@ -3,6 +3,8 @@ class Sslsplit < Formula
   homepage "https://www.roe.ch/SSLsplit"
   url "https://mirror.roe.ch/rel/sslsplit/sslsplit-0.5.0.tar.bz2"
   sha256 "3eb13c1d0164bf04e7602d9fc45ef7460444b953efaee3ee7d52c357adb3a89a"
+  revision 1
+
   head "https://github.com/droe/sslsplit.git", :branch => "develop"
 
   bottle do
