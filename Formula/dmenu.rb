ceaf721a94128f824a90ccf550f368be724f6433
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-27 02:33:29 +0000

    dmenu: add 4.6 bottle.

--- Formula/dmenu.rb
+++ Formula/dmenu.rb
@@ -6,6 +6,13 @@ class Dmenu < Formula
 
   head "http://git.suckless.org/dmenu/", :using => :git
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a9b0c177c36f006f820ac7a673f21b6d4e63fcb71d9e2a685346ac98464cebcb" => :sierra
+    sha256 "dbc4f0dead6c3fae5245b6771f5196130dd5768a862db89932ea258c849f338f" => :el_capitan
+    sha256 "5ab9424eb602d93e02afd71e2206cadbdcc6be985251881cc214468741544826" => :yosemite
+  end
+
   depends_on :x11
 
   def install
