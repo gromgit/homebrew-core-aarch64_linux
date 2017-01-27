275408f704f1a9ec188697557362bf0143b459b2
JCount <JCount42@gmail.com>
2017-01-27 14:32:45 -0500

    nsd: revision for libevent

--- Formula/nsd.rb
+++ Formula/nsd.rb
@@ -3,6 +3,7 @@ class Nsd < Formula
   homepage "https://www.nlnetlabs.nl/projects/nsd/"
   url "https://www.nlnetlabs.nl/downloads/nsd/nsd-4.1.14.tar.gz"
   sha256 "bdfc61c5f3bf11febd8f4776eef1d4f2d95ed70f12f11d4eeee943c186ffd802"
+  revision 1
 
   bottle do
     sha256 "2c32b662005bd31ed1b30aa49d41e8c26a380458f3c78aae228d3563523ff4fb" => :sierra
