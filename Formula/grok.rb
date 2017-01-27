42227280d532a8ed72eb5febbd7b009f989bdfb2
JCount <JCount42@gmail.com>
2017-01-27 14:16:37 -0500

    grok: revision for libevent

--- Formula/grok.rb
+++ Formula/grok.rb
@@ -4,6 +4,7 @@ class Grok < Formula
   url "https://github.com/jordansissel/grok/archive/v0.9.2.tar.gz"
   sha256 "40edbdba488ff9145832c7adb04b27630ca2617384fbef2af014d0e5a76ef636"
   head "https://github.com/jordansissel/grok.git"
+  revision 1
 
   bottle do
     cellar :any
