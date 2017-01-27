53478f52cc3edc4033dbbcb3d6a7832c028b76eb
JCount <JCount42@gmail.com>
2017-01-27 12:54:41 -0500

    bash 4.4.11
    Closes #9342.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/bash.rb
+++ Formula/bash.rb
@@ -12,7 +12,7 @@ class Bash < Formula
     mirror "https://gnu.cu.be/bash/bash-4.4.tar.gz"
     mirror "https://mirror.unicorncloud.org/gnu/bash/bash-4.4.tar.gz"
     sha256 "d86b3392c1202e8ff5a423b302e6284db7f8f435ea9f39b5b1b20fd3ac36dfcb"
-    version "4.4.5"
+    version "4.4.11"
 
     %w[
       001 3e28d91531752df9a8cb167ad07cc542abaf944de9353fe8c6a535c9f1f17f0f
@@ -20,6 +20,12 @@ class Bash < Formula
       003 51df5a9192fdefe0ddca4bdf290932f74be03ffd0503a3d112e4199905e718b2
       004 ad080a30a4ac6c1273373617f29628cc320a35c8cd06913894794293dc52c8b3
       005 221e4b725b770ad0bb6924df3f8d04f89eeca4558f6e4c777dfa93e967090529
+      006 6a8e2e2a6180d0f1ce39dcd651622fb6d2fd05db7c459f64ae42d667f1e344b8
+      007 de1ccc07b7bfc9e25243ad854f3bbb5d3ebf9155b0477df16aaf00a7b0d5edaf
+      008 86144700465933636d7b945e89b77df95d3620034725be161ca0ca5a42e239ba
+      009 0b6bdd1a18a0d20e330cc3bc71e048864e4a13652e29dc0ebf3918bea729343c
+      010 8465c6f2c56afe559402265b39d9e94368954930f9aa7f3dfa6d36dd66868e06
+      011 dd56426ef7d7295e1107c0b3d06c192eb9298f4023c202ca2ba6266c613d170d
     ].each_slice(2) do |p, checksum|
       patch :p0 do
         url "https://ftpmirror.gnu.org/bash/bash-4.4-patches/bash44-#{p}"
