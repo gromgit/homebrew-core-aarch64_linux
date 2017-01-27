82d63382b8e9b9c3f71e40f57d83ae5b3de8512a
Tomasz Pajor <tomek@polishgeeks.com>
2017-01-27 21:01:45 +0100

    x264 r2748
    Closes #9349.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/x264.rb
+++ Formula/x264.rb
@@ -2,8 +2,8 @@ class X264 < Formula
   desc "H.264/AVC encoder"
   homepage "https://www.videolan.org/developers/x264.html"
   # the latest commit on the stable branch
-  url "https://git.videolan.org/git/x264.git", :revision => "4d5c8b01a48f72f9c40651e92c39294326a0863f"
-  version "r2728"
+  url "https://git.videolan.org/git/x264.git", :revision => "97eaef2ab82a46d13ea5e00270712d6475fbe42b"
+  version "r2748"
   head "https://git.videolan.org/git/x264.git"
 
   bottle do
@@ -11,12 +11,6 @@ class X264 < Formula
     sha256 "454351400844abc9ef9dc75eda60613e3522089f395aa7d7239abb6d801455dc" => :sierra
     sha256 "00ddf0e14555830c2be93db080e51a18915ecd7afdf43de676b57945f199f9ae" => :el_capitan
     sha256 "02985537eae4685adc60829b04f180b5a6e23192bb1864d2086f3cd6a7895781" => :yosemite
-  end
-
-  devel do
-    # the latest commit on the master branch
-    url "https://git.videolan.org/git/x264.git", :revision => "b97ae0644f16bad2e2c9c9181264a946769a0aa0"
-    version "r2744"
   end
 
   option "with-10-bit", "Build a 10-bit x264 (default: 8-bit)"
