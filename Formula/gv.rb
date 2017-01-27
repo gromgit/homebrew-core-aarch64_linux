8a10e9153865c1e662e89f6d4da41130aaf9ae79
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-27 02:33:30 +0000

    gv: add 3.7.4 bottle.

--- Formula/gv.rb
+++ Formula/gv.rb
@@ -5,6 +5,12 @@ class Gv < Formula
   mirror "https://ftp.gnu.org/gnu/gv/gv-3.7.4.tar.gz"
   sha256 "2162b3b3a95481d3855b3c4e28f974617eef67824523e56e20b56f12fe201a61"
 
+  bottle do
+    sha256 "ae08a48cf7464fbf8af390ac017646cfa986fd95447ed8af5b91687350e10c93" => :sierra
+    sha256 "80956fd9b222cb9185517f3bb2809d93fd23a8acff70e0bbb9e295cc3b54725b" => :el_capitan
+    sha256 "aabf19a4073bae6b5b337c595169f18a3c4af9157bfcbd2b9b15744c3d0e4f2c" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "ghostscript" => "with-x11"
   depends_on :x11 => "2.7.2"
