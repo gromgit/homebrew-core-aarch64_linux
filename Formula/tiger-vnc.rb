d4b50ca385a27a2252f9e321fc7d730687cbd0fe
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-27 02:33:31 +0000

    tiger-vnc: add 1.7.1 bottle.

--- Formula/tiger-vnc.rb
+++ Formula/tiger-vnc.rb
@@ -4,6 +4,12 @@ class TigerVnc < Formula
   url "https://github.com/TigerVNC/tigervnc/archive/v1.7.1.tar.gz"
   sha256 "3c021ec0bee4611020c0bcbab995b0ef2f6f1a46127a52b368827f3275527ccc"
 
+  bottle do
+    sha256 "da4b58cfb0117ac7f2c4a81cba70bb653f8404d6bc8ff02e5320e05f02904ebb" => :sierra
+    sha256 "ffeabdeebdeadb9837915070a9730a93fb5bfc8bd69dd8719e2c15d6eb103e64" => :el_capitan
+    sha256 "a719884736ffa0fa5ecf99bda2df2afde53897cb08115399a729fbe73a38513a" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "gnutls" => :recommended
   depends_on "jpeg-turbo"
