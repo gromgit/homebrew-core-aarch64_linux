e891fa15ed4e54c5369bde567509e289ad6b2b25
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-27 02:33:29 +0000

    freeglut: add 3.0.0 bottle.

--- Formula/freeglut.rb
+++ Formula/freeglut.rb
@@ -4,6 +4,13 @@ class Freeglut < Formula
   url "https://downloads.sourceforge.net/project/freeglut/freeglut/3.0.0/freeglut-3.0.0.tar.gz"
   sha256 "2a43be8515b01ea82bcfa17d29ae0d40bd128342f0930cd1f375f1ff999f76a2"
 
+  bottle do
+    cellar :any
+    sha256 "ac029acfd40b4c4381dc241e1e0ad94f950cd05ed7c76348e5550a3f848d0152" => :sierra
+    sha256 "e3f9e0796917b54055f737677324560db1ffd849c415a1a40019598383671139" => :el_capitan
+    sha256 "c2816abf98614e8bf4fc1e6fb65a10d637bf1b85659cf7e9e524bee4d46b3ebe" => :yosemite
+  end
+
   depends_on :x11
   depends_on "cmake" => :build
 
