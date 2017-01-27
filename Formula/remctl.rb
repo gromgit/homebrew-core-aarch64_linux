f6049a3d809dde79598362a676039c8f2078c68c
JCount <JCount42@gmail.com>
2017-01-27 14:38:48 -0500

    remctl: revision for libevent

--- Formula/remctl.rb
+++ Formula/remctl.rb
@@ -3,6 +3,7 @@ class Remctl < Formula
   homepage "https://www.eyrie.org/~eagle/software/remctl/"
   url "https://archives.eyrie.org/software/kerberos/remctl-3.13.tar.xz"
   sha256 "e8f249c5ef54d5cff95ae503278d262615b3e7ebe13dfb368a1576ef36ee9109"
+  revision 1
 
   bottle do
     sha256 "4b9aef69010313dc6429cc6a47ef51e40cd61d31eb98d2bb6e32c22a95f2bff3" => :sierra
