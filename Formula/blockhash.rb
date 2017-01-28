6be25daad078eeadedacab2a4b6fec8b3c427c91
Tomasz Pajor <tomek@polishgeeks.com>
2017-01-28 18:04:20 +0100

    blockhash: bump revision for imagemagick

--- Formula/blockhash.rb
+++ Formula/blockhash.rb
@@ -4,6 +4,7 @@ class Blockhash < Formula
   url "https://github.com/commonsmachinery/blockhash/archive/0.2.1.tar.gz"
   sha256 "549e0f52e7a7545bae7eca801c64c79b95cbe9417975718e262fddaf78b00cca"
   head "https://github.com/commonsmachinery/blockhash.git"
+  revision 1
 
   bottle do
     cellar :any
