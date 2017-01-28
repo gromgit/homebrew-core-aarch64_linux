64645fb1935d5fdac3346d7045774f65c413e2b6
ilovezfs <ilovezfs@icloud.com>
2017-01-28 06:52:21 -0800

    protobuf-c: revision for protobuf

--- Formula/protobuf-c.rb
+++ Formula/protobuf-c.rb
@@ -3,7 +3,7 @@ class ProtobufC < Formula
   homepage "https://github.com/protobuf-c/protobuf-c"
   url "https://github.com/protobuf-c/protobuf-c/releases/download/v1.2.1/protobuf-c-1.2.1.tar.gz"
   sha256 "846eb4846f19598affdc349d817a8c4c0c68fd940303e6934725c889f16f00bd"
-  revision 2
+  revision 3
 
   bottle do
     sha256 "70cf3b1d4853eddeb9cbd3b54e77ebd2ad47465f68581e8ca3a000ad63322551" => :sierra
@@ -11,14 +11,10 @@ class ProtobufC < Formula
     sha256 "253b1bb42eb8e21cae3802ef429df21f9a07ebc2b41393cc3eb966c68ec640f9" => :yosemite
   end
 
-  option :universal
-
   depends_on "pkg-config" => :build
   depends_on "protobuf"
 
   def install
-    ENV.universal_binary if build.universal?
-
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install"
   end
