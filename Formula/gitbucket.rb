f457ef4614c4bab9b3746d8f3a4af25dbce086cc
ilovezfs <ilovezfs@icloud.com>
2017-01-28 14:06:15 -0800

    gitbucket 4.9
    Closes #9395.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/gitbucket.rb
+++ Formula/gitbucket.rb
@@ -1,8 +1,8 @@
 class Gitbucket < Formula
   desc "Git platform powered by Scala offering"
   homepage "https://github.com/gitbucket/gitbucket"
-  url "https://github.com/gitbucket/gitbucket/releases/download/4.8/gitbucket.war"
-  sha256 "6379bdbe6180c32de47553e70f4d627ff222018fa7064ea5373ca832ded7fa60"
+  url "https://github.com/gitbucket/gitbucket/releases/download/4.9/gitbucket.war"
+  sha256 "386f7e1436ef5f2736c8b9a70c668f4552046175f367e1b51693c66f51230445"
 
   head do
     url "https://github.com/gitbucket/gitbucket.git"
