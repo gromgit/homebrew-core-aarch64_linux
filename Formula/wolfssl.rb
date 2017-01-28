f6b146dc1b1cd1618d476316c88cc624ac382743
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-28 21:11:27 +0000

    wolfssl: update 3.10.0 bottle.

--- Formula/wolfssl.rb
+++ Formula/wolfssl.rb
@@ -8,9 +8,9 @@ class Wolfssl < Formula
 
   bottle do
     cellar :any
-    sha256 "828a00ce40e429a5558324b5a857ed125035a55fcac5f9113ff659a86cb90a36" => :sierra
-    sha256 "6609efa8db1447213575eee116b894d700618a9a24c61dd784eea1d5e709f13c" => :el_capitan
-    sha256 "2c56d086cf739379bc9e3640b2ccf2a30bfca6a86c595dd790dcaa4ffa6d9301" => :yosemite
+    sha256 "c757e73e6399dd391824eedbcfe995d8a5274dc150f9ae9b3bd227dbda47b72f" => :sierra
+    sha256 "3241c3fa890572cf99cc268a4846328605a4749833f77597ebb7c327a270d0c6" => :el_capitan
+    sha256 "0f90124a769af2037b0bfea48eb56945c31d3dfdb5ce144e41f00d1e0aa7c8f8" => :yosemite
   end
 
   option "without-test", "Skip compile-time tests"
