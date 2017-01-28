32a27872b8fca9bd9dbaffb2bf881a400616ddb5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-28 13:56:26 +0000

    libtrace: update 4.0.0 bottle.

--- Formula/libtrace.rb
+++ Formula/libtrace.rb
@@ -5,11 +5,9 @@ class Libtrace < Formula
   sha256 "e89ac39808e2bb1e17e031191af8ab7bdbe3d2b0aeca4c6040e6fc8761ec0240"
 
   bottle do
-    sha256 "6447919d8e8463b070e05b6b28c6ff18fb0378dd89cc01b61eba1a6ffd604a27" => :sierra
-    sha256 "410e2bc8e025bed1cc3011ad7bbf0b0fbf1e585147fcbb64c237f3cd205fb558" => :el_capitan
-    sha256 "3b4b9e487c60283fa6de2def172493fbc5e78a856174ccfd8a2850adcf734dcc" => :yosemite
-    sha256 "1ad2c47d6b52a7b40760242f40e40f28730ef3baa26868a1f9300fece0532e0f" => :mavericks
-    sha256 "1e1115e10b3c226a49a3c29187327ba39cc5a83941736ed48a5cf866fae3da69" => :mountain_lion
+    sha256 "19f87788d761a5bc9a8ebd8c5efbd043dbb21f7c3eaaaf38b0bc015b51b5c284" => :sierra
+    sha256 "a83f537c9410ff1253b770e75bd3ffef3131238d3d4c541dbdc77092726ceee4" => :el_capitan
+    sha256 "96ff4f275637ccee0fb9949d0d1e29345b2105c5e26e12a5ae85d31c13051241" => :yosemite
   end
 
   depends_on "openssl"
