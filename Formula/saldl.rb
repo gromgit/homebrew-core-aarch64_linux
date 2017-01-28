a8cb72c0df36e60f999a53b1cdd51758b6122eba
JCount <JCount42@gmail.com>
2017-01-28 14:11:30 -0500

    saldl: fix build on 10.11 and 10.12
    Closes #9390.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/saldl.rb
+++ Formula/saldl.rb
@@ -25,8 +25,17 @@ class Saldl < Formula
   end
 
   def install
+    ENV.refurbish_args
+
     # a2x/asciidoc needs this to build the man page successfully
     ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+
+    # Fixes clock_gettime build error on macOS 10.11
+    # Reported 28 January 2017 https://github.com/saldl/saldl/issues/8
+    if MacOS.version == "10.11" && MacOS::Xcode.installed? && MacOS::Xcode.version >= "8.0"
+      inreplace "wscript", "clock_gettime(CLOCK_MONOTONIC_RAW, &tp);",
+                           "not_a_function(NOT_A_SYMBOL, &tp);"
+    end
 
     args = ["--prefix=#{prefix}"]
 
