a2e6c6de6b813fcbb5bb0d0095ad60b84fc3441a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-29 20:06:00 +0000

    remctl: update 3.13_1 bottle.

--- Formula/remctl.rb
+++ Formula/remctl.rb
@@ -6,9 +6,9 @@ class Remctl < Formula
   revision 1
 
   bottle do
-    sha256 "4b9aef69010313dc6429cc6a47ef51e40cd61d31eb98d2bb6e32c22a95f2bff3" => :sierra
-    sha256 "23f441a3c0a54354aaa4b73ecd1c6b6f041d8232d38198db47ff11e788dd2b9e" => :el_capitan
-    sha256 "78587e49de505753c9f842a6bb1705d4652f1f9f7c875e1e0a329a62413db839" => :yosemite
+    sha256 "edfe97b717e944a87d29abe653d9ffafee8c1b70f5809d94434bf9d5b9430725" => :sierra
+    sha256 "ca265d9856564ddc2b72f671760be7d06a16718ec7f16cf8c6a548eaa83fcb8d" => :el_capitan
+    sha256 "85aca4fa0248cdf724a304ba559144c69b487d25e1870d9b89bb52558bd34b6b" => :yosemite
   end
 
   depends_on "pcre"
