4af73bc5da0aacfe72c23d75ba24ba1f69187b18
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-29 20:05:59 +0000

    libcouchbase: update 2.7.1_1 bottle.

--- Formula/libcouchbase.rb
+++ Formula/libcouchbase.rb
@@ -8,9 +8,9 @@ class Libcouchbase < Formula
   head "https://github.com/couchbase/libcouchbase.git"
 
   bottle do
-    sha256 "8d55f50e81932a583636e7012b1f7c9d16957e949e86757a061f10fea4db674e" => :sierra
-    sha256 "e435bc360f3e5e6b3eb59ce2db92d1b887c867a6027e832e49aea614dfd92d3f" => :el_capitan
-    sha256 "68ed79b82d348b012bd4c06374f81b8066ce65175d90dc7245f969505bc4758c" => :yosemite
+    sha256 "cba376ad2874f6bc503fa8b88da030dff1a1797d086f63c3f5a735e28a002a6b" => :sierra
+    sha256 "0f22ba69c7f3e39626ff39d7577be600f5785564a54ff66ac7bb1b264bcf0c81" => :el_capitan
+    sha256 "ccf3208241fdc5a101f70668687800ed7b9982138ba2e56affc5bf5cbd2218d5" => :yosemite
   end
 
   option "with-libev", "Build libev plugin"
