b162dd98f487988b37ce51720c331c51a4b5a853
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-29 20:06:00 +0000

    redsocks: update 0.4_1 bottle.

--- Formula/redsocks.rb
+++ Formula/redsocks.rb
@@ -7,10 +7,9 @@ class Redsocks < Formula
 
   bottle do
     cellar :any
-    sha256 "20fdde6819d44b7034a1af7ed8e30ba4ae42abfe5c70e8c067908de3c9bdcfed" => :sierra
-    sha256 "ad3df81c5bd45efce293e229c77ff0687141646024a41a0c7cae3b3da34dcdcc" => :el_capitan
-    sha256 "d59f7cd44ed44e76a1d03d83fdd7385330425d42913b4bab36e180c9b082d601" => :yosemite
-    sha256 "a6b82407933ada8111fe21b0491b051aa05f31cdb769840f50e6af5cc4f2c96e" => :mavericks
+    sha256 "ccfba64129d3e3a01b1bba7d18564e7b1ac174ce4b7a1c41424e2985d7a97523" => :sierra
+    sha256 "5d9e3f0dcc812146c27c7916052605be3f08b4be45682ccda27a9ab0dc97f574" => :el_capitan
+    sha256 "5d017a684dd4810da302823e4122ac8f43808a87c618fd24667b5b89d6b812b3" => :yosemite
   end
 
   depends_on "libevent"
