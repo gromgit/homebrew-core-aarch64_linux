b6880b3a884cef9dc2f722feaaa29c4b4376e8d1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-29 20:06:00 +0000

    pgbouncer: update 1.7.2_1 bottle.

--- Formula/pgbouncer.rb
+++ Formula/pgbouncer.rb
@@ -7,10 +7,9 @@ class Pgbouncer < Formula
 
   bottle do
     cellar :any
-    sha256 "f1b22cffe62914e51387e902f7ed8dd7ff2d43c65723abf5f22c97f6c32fa25f" => :sierra
-    sha256 "f0a7e86e73f82b123b993491ed593e3a3683e4de80180c6b412f40e1c7426b1b" => :el_capitan
-    sha256 "e92de7aa798c662214aa866f1160226a941df217a90081080daccfd6b91d0ade" => :yosemite
-    sha256 "5934355f560dbbddd7e0caf8b1e7c861debb1e9a96d8575461971689ca90a875" => :mavericks
+    sha256 "856e2b0f818374b88290b5d794f9e41d691da1a164d14aedf6ef6192d726d67a" => :sierra
+    sha256 "90de8ccdfe3cd6f84de801a5c003c01b95dd6f95eea77620d9be859e78a4d288" => :el_capitan
+    sha256 "14b72883c284c2ca5602f29cf393b4c289c77033c6c47596fe8eb9b26245c8ab" => :yosemite
   end
 
   depends_on "asciidoc" => :build
