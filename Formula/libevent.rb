6c94ad706a91732bac894720062c0e0c0ce58594
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-29 20:05:59 +0000

    libevent: update 2.1.8 bottle.

--- Formula/libevent.rb
+++ Formula/libevent.rb
@@ -6,11 +6,9 @@ class Libevent < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "de2aeabfb41fb7e601fb33948b81da4cd56b88bd5d1bfe325370924e75a81fae" => :sierra
-    sha256 "1f18bad6b4b19c55ed11b63256241558923eea26814a7d551cf7384ffe0dd098" => :el_capitan
-    sha256 "8f02227ee85ed96fd3499befc12cf00aad34dcdf436f0b58f29945432a69d4b4" => :yosemite
-    sha256 "e32a8b4b74b3a41fb7ccf7933f0cc883c16d9fbd8ed55ff3d204556afa9d1a41" => :mavericks
+    sha256 "cdd11d67b5f49b94cf3fbfd24753f84082957e3d55680e2b5979eec19091e694" => :sierra
+    sha256 "136a93a91c3724d0403b0d43d0b9a4bf6b857278c4ebb7c7585ef70a19b0964c" => :el_capitan
+    sha256 "ef703db1b4cbdab35b89aabe80c225dd9b7a2c3ea14b1eae681478c5b9df15fe" => :yosemite
   end
 
   depends_on "autoconf" => :build
