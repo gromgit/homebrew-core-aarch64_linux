00ea2dbff59654ed9c8017b75a5d93a9b284cad0
kolen <incredible.angst@gmail.com>
2017-01-29 17:50:21 +0300

    z80dasm 1.1.4
    Closes #9416.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/z80dasm.rb
+++ Formula/z80dasm.rb
@@ -1,8 +1,8 @@
 class Z80dasm < Formula
   desc "Disassembler for the Zilog Z80 microprocessor and compatibles"
   homepage "https://www.tablix.org/~avian/blog/articles/z80dasm/"
-  url "https://www.tablix.org/~avian/z80dasm/z80dasm-1.1.3.tar.gz"
-  sha256 "1d6966bf7bddd0965421455e666872607019cfa43352188f5580304dd1039539"
+  url "https://www.tablix.org/~avian/z80dasm/z80dasm-1.1.4.tar.gz"
+  sha256 "ca52fdf3bcb65d928baf3e852e0961944f713f1d7ea33903aa8db4d1615bac0a"
 
   bottle do
     cellar :any_skip_relocation
