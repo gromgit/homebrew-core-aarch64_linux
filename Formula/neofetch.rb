7406803172a1b8106d01c74ef9540c04db886d6f
ilovezfs <ilovezfs@icloud.com>
2017-01-30 09:06:12 -0800

    neofetch 3.0.1
    Closes #9453.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/neofetch.rb
+++ Formula/neofetch.rb
@@ -1,8 +1,8 @@
 class Neofetch < Formula
   desc "fast, highly customisable system info script"
   homepage "https://github.com/dylanaraps/neofetch"
-  url "https://github.com/dylanaraps/neofetch/archive/3.0.tar.gz"
-  sha256 "b5d3319474ff432c874dd39d38800ac8eb31da0b8e481b871ff4792f07fb67c4"
+  url "https://github.com/dylanaraps/neofetch/archive/3.0.1.tar.gz"
+  sha256 "31447da6507c13c44eb2006901c00ed4ca08f0423d9439aaddea64edcaca2c38"
   head "https://github.com/dylanaraps/neofetch.git"
 
   bottle do
@@ -11,13 +11,6 @@ class Neofetch < Formula
     sha256 "d76acad31a6c94dc33602133fda65a1968b8403b5a42cb2cff96927137008b9d" => :sierra
     sha256 "d76acad31a6c94dc33602133fda65a1968b8403b5a42cb2cff96927137008b9d" => :el_capitan
     sha256 "d76acad31a6c94dc33602133fda65a1968b8403b5a42cb2cff96927137008b9d" => :yosemite
-  end
-
-  # Fixes config file detection now that neofetch stores the config in /usr/local/etc
-  # Can be removed with the next release
-  patch do
-    url "https://github.com/dylanaraps/neofetch/commit/0fed289d58bb5f0675ebed37f5bb71b11ad54b9e.patch"
-    sha256 "39dd55b268b515f22521e0dd2f1a31edd0b869cabf4c515142399906d9fa2d6d"
   end
 
   depends_on "screenresolution" => :recommended
