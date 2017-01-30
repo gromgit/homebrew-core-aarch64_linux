772040d66019a23cfce782faced410981640076d
ilovezfs <ilovezfs@icloud.com>
2017-01-30 01:21:29 -0800

    grc 1.10
    Closes #9437.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/grc.rb
+++ Formula/grc.rb
@@ -1,14 +1,15 @@
 class Grc < Formula
   desc "Colorize logfiles and command output"
   homepage "http://korpus.juls.savba.sk/~garabik/software/grc.html"
-  url "http://korpus.juls.savba.sk/~garabik/software/grc/grc_1.9.orig.tar.gz"
-  sha256 "41626e571ca255e1a9fe0816f3c0dfd1a30d9564d0decaf4b7365e28e3c54f5b"
-
+  url "https://github.com/garabik/grc/archive/v1.10.tar.gz"
+  sha256 "8d65a141c659c5f992b417c15fe8e1283698bb9f39f8b201fd811deb0841d1f0"
   head "https://github.com/garabik/grc.git"
 
   bottle :unneeded
 
   conflicts_with "cc65", :because => "both install `grc` binaries"
+
+  depends_on :python3
 
   def install
     inreplace ["grc", "grc.1"], "/etc", etc
@@ -16,7 +17,7 @@ class Grc < Formula
 
     etc.install "grc.conf"
     bin.install %w[grc grcat]
-    (share+"grc").install Dir["conf.*"]
+    pkgshare.install Dir["conf.*"]
     man1.install %w[grc.1 grcat.1]
 
     etc.install "grc.bashrc"
@@ -30,7 +31,7 @@ class Grc < Formula
   end
 
   test do
-    actual = pipe_output("#{bin}/grcat #{share}/grc/conf.ls", "root")
-    assert_equal "\e[0m\e[1m\e[31mroot\e[0m\n", actual
+    actual = pipe_output("#{bin}/grcat #{share}/grc/conf.ls", "hello root")
+    assert_equal "\e[0mhello \e[0m\e[1m\e[37m\e[41mroot\e[0m", actual.chomp
   end
 end
