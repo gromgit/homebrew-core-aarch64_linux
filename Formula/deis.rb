4816bf5b62371fd2f65c02f3bcbe04ab35d081d8
Blake <blake.barnett@postmates.com>
2017-01-30 13:08:44 -0800

    deis 2.10.0
    Closes #9457.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/deis.rb
+++ Formula/deis.rb
@@ -1,8 +1,8 @@
 class Deis < Formula
   desc "Deploy and manage applications on your own servers"
   homepage "https://deis.io/"
-  url "https://github.com/deis/deis/archive/v1.13.4.tar.gz"
-  sha256 "c59b244b2d4e87e04491898a401238ea6f302326957ffe1aeb6154485fe1d8ec"
+  url "https://github.com/deis/workflow-cli/archive/v2.10.0.tar.gz"
+  sha256 "de5a9b1dd8746059c77b63472ae8d9a788e3f3cccdb190f8f307e3fdbb309700"
 
   bottle do
     sha256 "274ce1639c8b85fff3fd4a315e260ac37baa5d66b52357d4d08b45cefa29cb15" => :sierra
@@ -11,14 +11,18 @@ class Deis < Formula
   end
 
   depends_on "go" => :build
-  depends_on "godep" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
+    ENV["GOPATH"] = `dirname #{buildpath}`.chomp
+    ENV["GOBIN"] = ENV["GOPATH"] + "/bin"
     (buildpath/"src/github.com/deis").mkpath
-    ln_s buildpath, "src/github.com/deis/deis"
-    system "godep", "restore"
-    system "go", "build", "-o", bin/"deis", "client/deis.go"
+    (buildpath/"bin").mkpath
+    ln_s buildpath, "src/github.com/deis/workflow-cli"
+    system "go", "get", "github.com/mitchellh/gox"
+    system "go", "get"
+    system "../bin/gox", "-verbose", "-ldflags",
+      "'-X=github.com/deis/workflow-cli/version.Version=v2.10.0'",
+      "-os=darwin", "-arch=amd64", "-output=#{bin}/deis"
   end
 
   test do
