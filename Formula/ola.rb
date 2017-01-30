e4f9b88fcafd07eb0da69d9efaf16c8d7117e778
ilovezfs <ilovezfs@icloud.com>
2017-01-30 08:35:18 -0800

    ola: depend on protobuf@3.1
    also during the build vendor protobuf-c built against protobuf@3.1
    
    Closes #9450.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/ola.rb
+++ Formula/ola.rb
@@ -3,6 +3,7 @@ class Ola < Formula
   homepage "https://www.openlighting.org/ola/"
   url "https://github.com/OpenLightingProject/ola/archive/0.10.3.tar.gz"
   sha256 "474db6752940cea6cd9493dcbeeb13429b5d29f4777973d08738cb5ef04c9dcd"
+  revision 1
   head "https://github.com/OpenLightingProject/ola.git"
 
   bottle do
@@ -11,7 +12,6 @@ class Ola < Formula
     sha256 "fd4409d7fa44587e429d240c38f546ccf5a1986d9d1a0a323182148a97547c1e" => :yosemite
   end
 
-  option :universal
   option "with-libftdi", "Install FTDI USB plugin for OLA."
   option "with-rdm-tests", "Install RDM Tests for OLA."
   deprecated_option "with-ftdi" => "with-libftdi"
@@ -23,7 +23,7 @@ class Ola < Formula
   depends_on "cppunit"
   depends_on "libmicrohttpd"
   depends_on "ossp-uuid"
-  depends_on "protobuf-c"
+  depends_on "protobuf@3.1"
   depends_on :python if MacOS.version <= :snow_leopard
   depends_on "liblo" => :recommended
   depends_on "libusb" => :recommended
@@ -31,8 +31,21 @@ class Ola < Formula
   depends_on "libftdi" => :optional
   depends_on "libftdi0" if build.with? "libftdi"
 
+  resource "protobuf-c" do
+    url "https://github.com/protobuf-c/protobuf-c/releases/download/v1.2.1/protobuf-c-1.2.1.tar.gz"
+    sha256 "846eb4846f19598affdc349d817a8c4c0c68fd940303e6934725c889f16f00bd"
+  end
+
   def install
-    ENV.universal_binary if build.universal?
+    resource("protobuf-c").stage do
+      system "./configure", "--disable-dependency-tracking",
+                            "--prefix=#{buildpath}/vendor/protobuf-c"
+      system "make", "install"
+    end
+    ENV.prepend_path "PKG_CONFIG_PATH", buildpath/"vendor/protobuf-c/lib/pkgconfig"
+
+    protobuf_pth = Formula["protobuf@3.1"].opt_lib/"python2.7/site-packages/homebrew-protobuf.pth"
+    (buildpath/".brew_home/Library/Python/2.7/lib/python/site-packages").install_symlink protobuf_pth
 
     args = %W[
       --disable-fatal-warnings
