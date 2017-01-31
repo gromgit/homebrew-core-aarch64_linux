b57dce2e53fd4d1d72554c8bdcb9f8112141c56c
Stephen <stephengroat@users.noreply.github.com>
2017-01-30 16:55:29 -0800

    ipmitool: add std=gnu99 to CFLAGS + cleanup
    Closes #9423.
    Closes #9460.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/ipmitool.rb
+++ Formula/ipmitool.rb
@@ -4,6 +4,7 @@ class Ipmitool < Formula
   url "https://downloads.sourceforge.net/project/ipmitool/ipmitool/1.8.18/ipmitool-1.8.18.tar.bz2"
   mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/i/ipmitool/ipmitool_1.8.18.orig.tar.bz2"
   sha256 "0c1ba3b1555edefb7c32ae8cd6a3e04322056bc087918f07189eeedfc8b81e01"
+  revision 1
 
   bottle do
     cellar :any
@@ -15,19 +16,21 @@ class Ipmitool < Formula
   depends_on "openssl"
 
   def install
-    # Required to get NI_MAXHOST and NI_MAXSERV defined in /usr/include/netdb.h, see
-    # https://sourceforge.net/p/ipmitool/bugs/418
-    inreplace "src/plugins/ipmi_intf.c", "#define _GNU_SOURCE 1", "#define _GNU_SOURCE 1\n#define _DARWIN_C_SOURCE 1"
-
     # Fix ipmi_cfgp.c:33:10: fatal error: 'malloc.h' file not found
     # Upstream issue from 8 Nov 2016 https://sourceforge.net/p/ipmitool/bugs/474/
     inreplace "lib/ipmi_cfgp.c", "#include <malloc.h>", ""
 
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--mandir=#{man}",
-                          "--disable-intf-usb"
+    # https://sourceforge.net/p/ipmitool/bugs/436/
+    ENV.append "CFLAGS", "--std=gnu99"
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --mandir=#{man}
+      --disable-intf-usb
+    ]
+    system "./configure", *args
+    system "make", "check"
     system "make", "install"
   end
 
