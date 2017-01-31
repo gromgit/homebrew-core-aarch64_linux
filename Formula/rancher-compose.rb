aa23b38ea7bb83d13defd19a7d04c7dd6078a645
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-31 09:13:49 +0000

    rancher-compose: update 0.12.2 bottle.

--- Formula/rancher-compose.rb
+++ Formula/rancher-compose.rb
@@ -5,9 +5,10 @@ class RancherCompose < Formula
   sha256 "5b9311479eac85f4a0665258b4e1859e7b5eba3cbae66eb7c6864c59ff78d806"
 
   bottle do
-    sha256 "6a7b98ec52f3ddb0987eefd85f3c994295cc805b41e493bf6233cd705005999a" => :sierra
-    sha256 "f1a53710d2f225f79ada6ed41c2d16ae7a4fe856e5c181b77fd733f4881a9763" => :el_capitan
-    sha256 "692a4b3dc5478398bb283c02d5b7ecd350b5488797c6a0300e5b922a23564c56" => :yosemite
+    cellar :any_skip_relocation
+    sha256 "78344dea5f79d93b8d1a3156544d68865316ba5fa30a623f8abaaa8a03612bca" => :sierra
+    sha256 "71323be798d099e44e3dde836a050f6389d532edf4dedb22daef769c39324448" => :el_capitan
+    sha256 "91b9780007f50baecb622a78190a78adae7bde12f68ae2e9d2f7a3e87c4aa505" => :yosemite
   end
 
   depends_on "go" => :build
