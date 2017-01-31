1964fa8a03519127fbc03edeffdd0eb542e38a8c
Ollie Ford <dev@ojford.com>
2017-01-31 11:50:17 +0000

    git-recent 1.0.3 (new formula)
    This commit adds git-recent [0] - a git utility for listing the users'
    most recently active branches.
    
    [0] - https://github.com/paulirish/git-recent
    
    Closes #8979.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Formula/git-recent.rb
@@ -0,0 +1,20 @@
+class GitRecent < Formula
+  desc "See your latest local git branches, formatted real fancy"
+  homepage "https://github.com/paulirish/git-recent"
+  url "https://github.com/paulirish/git-recent/archive/v1.0.3.tar.gz"
+  sha256 "2ea954f3c1cc3917ad1a0ff5cd361dff7c3f82410bd464a9f5decb0a539155ff"
+
+  def install
+    bin.install "git-recent"
+  end
+
+  test do
+    system "git", "init"
+    system "git", "recent"
+    # User will be 'BrewTestBot' on CI, needs to be set here to work locally
+    system "git", "config", "user.name", "BrewTestBot"
+    system "git", "config", "user.email", "brew@test.bot"
+    system "git", "commit", "--allow-empty", "-m", "test_commit"
+    assert_match(/.*master.*seconds? ago.*BrewTestBot.*\n.*test_commit/, shell_output("git recent"))
+  end
+end
