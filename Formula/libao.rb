7db8fa2f0a709688aa1cafa170c85b22f94f3b94
equal-l2 <equal-l2@users.noreply.github.com>
2017-01-31 14:11:26 +0900

    libao 1.2.2
    Closes #9462.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/libao.rb
+++ Formula/libao.rb
@@ -1,8 +1,9 @@
 class Libao < Formula
   desc "Cross-platform Audio Library"
   homepage "https://www.xiph.org/ao/"
-  url "http://downloads.xiph.org/releases/ao/libao-1.2.0.tar.gz"
-  sha256 "03ad231ad1f9d64b52474392d63c31197b0bc7bd416e58b1c10a329a5ed89caf"
+  url "https://github.com/xiph/libao/archive/1.2.2.tar.gz"
+  sha256 "df8a6d0e238feeccb26a783e778716fb41a801536fe7b6fce068e313c0e2bf4d"
+  head "https://git.xiph.org/libao.git"
 
   bottle do
     rebuild 1
@@ -13,23 +14,25 @@ class Libao < Formula
     sha256 "21aa15e92c5577a4a610de8fbb3f5a72638a0c37a40c4ebebc14826359932efa" => :mountain_lion
   end
 
-  head do
-    url "https://git.xiph.org/libao.git"
-    depends_on "automake" => :build
-    depends_on "autoconf" => :build
-    depends_on "libtool" => :build
-  end
-
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
+  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
+  depends_on "pulseaudio" => :optional
 
   def install
-    if build.head?
-      ENV["AUTOMAKE_FLAGS"] = "--include-deps"
-      system "./autogen.sh"
-    end
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--enable-static"
+    ENV["AUTOMAKE_FLAGS"] = "--include-deps"
+    system "./autogen.sh"
+
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --enable-static
+    ]
+
+    args << "--enable-pulse" if build.with? "pulseaudio"
+
+    system "./configure", *args
     system "make", "install"
   end
 
@@ -41,7 +44,7 @@ class Libao < Formula
         return 0;
       }
     EOS
-    system ENV.cc, "test.cpp", "-L#{lib}", "-lao", "-o", "test"
+    system ENV.cc, "test.cpp", "-I#{include}", "-L#{lib}", "-lao", "-o", "test"
     system "./test"
   end
 end
