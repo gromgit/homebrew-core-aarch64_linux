d7d9668ac95f38b6f862329650537f976435fbc4
JCount <JCount42@gmail.com>
2017-01-31 18:06:04 -0500

    mldonkey: revision for webp
    Closes #9495.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/mldonkey.rb
+++ Formula/mldonkey.rb
@@ -1,7 +1,7 @@
 class Mldonkey < Formula
   desc "OCaml/GTK client for the eDonkey P2P network"
   homepage "http://mldonkey.sourceforge.net/Main_Page"
-  revision 1
+  revision 2
 
   head "https://github.com/ygrek/mldonkey.git", :branch => "next"
 
