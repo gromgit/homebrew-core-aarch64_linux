e9cd9ed806627dcd0f650710f687f87bea248ac8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-01 09:39:46 +0000

    bit: add 0.1.12 bottle.

--- Formula/bit.rb
+++ Formula/bit.rb
@@ -5,6 +5,12 @@ class Bit < Formula
   url "https://bitsrc.jfrog.io/bitsrc/bit-brew/stable/bit/0.1.12/bit-0.1.12-brew.tar.gz"
   sha256 "122ec737cca207bba1fa9ae6e3ea22acf53cecd56b542a2a5bacd380a88bdef9"
 
+  bottle do
+    sha256 "a3c8f1d2888e2320928dfc76ea6bc7b5b5b699929afc98fc542d0d9ac8184d40" => :sierra
+    sha256 "1c8a8923bf16a13ac6a978c183d0b0a84671216942d8e2a72c19ae2b39b88cfd" => :el_capitan
+    sha256 "622587fc49651195cd61471939a2c4d6aa109f3673b622c0e1ddd4d490f9adfc" => :yosemite
+  end
+
   depends_on "node"
 
   def install
