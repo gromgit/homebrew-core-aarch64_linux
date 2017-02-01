3491836b5f98550f03e91758d186bf67baa97175
ilovezfs <ilovezfs@icloud.com>
2017-01-31 17:40:03 -0800

    babl 0.1.24
    Closes #9500.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/babl.rb
+++ Formula/babl.rb
@@ -1,9 +1,9 @@
 class Babl < Formula
   desc "Dynamic, any-to-any, pixel format translation library"
   homepage "http://www.gegl.org/babl/"
-  url "https://download.gimp.org/pub/babl/0.1/babl-0.1.22.tar.bz2"
-  mirror "https://mirrors.kernel.org/debian/pool/main/b/babl/babl_0.1.22.orig.tar.bz2"
-  sha256 "f38a02e76fb96f4119ca82b4088c23f9183b9601cae19c1d8147e3ee8eaf2cca"
+  url "https://download.gimp.org/pub/babl/0.1/babl-0.1.24.tar.bz2"
+  mirror "https://mirrors.kernel.org/debian/pool/main/b/babl/babl_0.1.24.orig.tar.bz2"
+  sha256 "472bf1acdde5bf076e6d86f3004eea4e9b007b1377ab305ebddec99994f29d0b"
 
   bottle do
     sha256 "641f5989d5ccdf216fa8eccec71097812009b3b348c349d000870753690b1b57" => :sierra
@@ -20,20 +20,9 @@ class Babl < Formula
     depends_on "libtool" => :build
   end
 
-  option :universal
-
   depends_on "pkg-config" => :build
 
-  if build.universal?
-    fails_with :gcc_4_0
-    fails_with :gcc
-    ("4.3".."5.1").each do |n|
-      fails_with :gcc => n
-    end
-  end
-
   def install
-    ENV.universal_binary if build.universal?
     system "./autogen.sh" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
