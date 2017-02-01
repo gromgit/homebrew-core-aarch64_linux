9139a03d4420f210a2117328ce1453c4e0f202c0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-01 08:48:22 +0000

    oath-toolkit: update 2.6.2 bottle.

--- Formula/oath-toolkit.rb
+++ Formula/oath-toolkit.rb
@@ -6,11 +6,10 @@ class OathToolkit < Formula
   sha256 "b03446fa4b549af5ebe4d35d7aba51163442d255660558cd861ebce536824aa0"
 
   bottle do
-    sha256 "b9e174a86ba5d8801e8c13d5e6629e3ee1133236e4839f1a150c18d7142f7db6" => :sierra
-    sha256 "ec5da5bee84826207290f09bd74d913e2e99c937cb70104e3ab818251c21f794" => :el_capitan
-    sha256 "e333157196ce196c998cc46e2dec0a73fd3b49969dda9262effb8a565b591f1b" => :yosemite
-    sha256 "e082a0eb2ffa11d107574d72197a22208e71bdfa9e8993187933bffb89b8b603" => :mavericks
-    sha256 "c22dd6aeadd6465d796957705595f4333072f1c6c6087de5131931c70467e9e7" => :mountain_lion
+    cellar :any
+    sha256 "24304b5b3ec6fd838ab3fd42013e3934e247d3f7bdb6cb7fe992b89fe7dbff29" => :sierra
+    sha256 "de07bd83252dce6769862320365f0bf340807a67ab27a0bb7fb8db8f9aafc04a" => :el_capitan
+    sha256 "c30dabd27de1b1cf4a83f2b24d9929075662e7d18ab69fb7241bfbeaddae9723" => :yosemite
   end
 
   depends_on "pkg-config" => :build
