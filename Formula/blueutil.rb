1034002dca29b902c86dc8f1a116cb2d83f040ab
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-02 14:47:30 +0000

    blueutil: update 1.1.0 bottle.

--- Formula/blueutil.rb
+++ Formula/blueutil.rb
@@ -8,12 +8,9 @@ class Blueutil < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "6c45551ce81e67e02cfb165ad37d344170d1f7fc964a0ed576800d568d888d97" => :sierra
-    sha256 "c6599527da984b998b03321634b6facf9ed4bb2f3de35b8c8d4462fb2727bfea" => :el_capitan
-    sha256 "287bb35433ac8745719d4f920e1afd0ef75785cd8cb15a17a49214eb82d9e439" => :yosemite
-    sha256 "74d6c635e6199da479e819b8b4de8ace42a7cb75deb9d52c37a4373faa7b9690" => :mavericks
-    sha256 "fd1e25a79407536f93b4ae5552d30826eef3573aeb44fdd6cb31989bf31ec0d8" => :mountain_lion
+    sha256 "321ae52b2c801a5baff84f40e0a8dabeb47186b3cfcf1997c2417127d9b362d8" => :sierra
+    sha256 "a1f609b801cedbd69d60288c4cb07c03a55fbf4dbd753b32243200fc2ede6e87" => :el_capitan
+    sha256 "763607470c2121df23851cd4a6b624f6d3fc403df686f4f43bcc65222dc08455" => :yosemite
   end
 
   depends_on :xcode => :build
