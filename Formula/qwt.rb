fbe5781243a7481327e6ff2c8f2bef455c8e63f7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-02 16:47:22 +0000

    qwt: update 6.1.3_3 bottle.

--- Formula/qwt.rb
+++ Formula/qwt.rb
@@ -6,9 +6,9 @@ class Qwt < Formula
   revision 3
 
   bottle do
-    sha256 "e760d252093b926ecabe83dbe830f59dbcba2e69d746a3d616de46f1f51dd2f8" => :sierra
-    sha256 "8b78ba16555a4663022e03ac47ed4fbce3aff709fa3f5a9728f6ea4cf37d3e49" => :el_capitan
-    sha256 "ea06b99697a283b2186f46c3be4b55a1a7d871d99b51f11e8e49d550a1c726c5" => :yosemite
+    sha256 "c27465d6fa732f966ab1f8c6acde2fa331028e7b2c4c50124970c5ffedad8bb6" => :sierra
+    sha256 "287b3aa35bd3925a61d867ea9a122c04deee767cd587719fd11282f5f1cd171c" => :el_capitan
+    sha256 "b5d6c8c36a6090a8dd4aa0172b64389c27e973d3bfc6ae20c030d353fbd8ab34" => :yosemite
   end
 
   option "with-qwtmathml", "Build the qwtmathml library"
