a390466b970408530e8adb068d43fd4197aaf88e
JCount <JCount42@gmail.com>
2017-02-01 21:27:41 -0500

    saldl 35
    Closes #9547.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/saldl.rb
+++ Formula/saldl.rb
@@ -1,9 +1,8 @@
 class Saldl < Formula
   desc "CLI downloader optimized for speed and early preview."
   homepage "https://saldl.github.io"
-  url "https://github.com/saldl/saldl/archive/v34.tar.gz"
-  sha256 "12053f306306023e5bbdc6bb8594cc83f8793da0ce99dab1981179cdeccea4da"
-  revision 1
+  url "https://github.com/saldl/saldl/archive/v35.tar.gz"
+  sha256 "6b5f45a8f7782bcad124df4a24876c8b3c47d45aa25d0b09b2030837c6ece82c"
 
   head "https://github.com/saldl/saldl.git", :shallow => false
 
@@ -31,13 +30,6 @@ class Saldl < Formula
 
     # a2x/asciidoc needs this to build the man page successfully
     ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
-
-    # Fixes clock_gettime build error on macOS 10.11
-    # Reported 28 January 2017 https://github.com/saldl/saldl/issues/8
-    if MacOS.version == "10.11" && MacOS::Xcode.installed? && MacOS::Xcode.version >= "8.0"
-      inreplace "wscript", "clock_gettime(CLOCK_MONOTONIC_RAW, &tp);",
-                           "not_a_function(NOT_A_SYMBOL, &tp);"
-    end
 
     args = ["--prefix=#{prefix}"]
 
