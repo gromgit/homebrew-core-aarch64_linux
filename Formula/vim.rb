d8596a325465cc00839c721a9dc5ad5841604132
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-02 02:19:34 +0000

    vim: update 8.0.0295 bottle.

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -6,9 +6,9 @@ class Vim < Formula
   head "https://github.com/vim/vim.git"
 
   bottle do
-    sha256 "0a52b540cdb359686ea36e372862f716e13e2301471493f8f829198f54e4798c" => :sierra
-    sha256 "7075a7daf2d4dbb18278bbc2a09b0d78ccbebb85e1074e1d1609d912f2a29e77" => :el_capitan
-    sha256 "05d5c0dec4a68b4ed2aa350c9b499a434127f80bf86aae44ef1c640011b01334" => :yosemite
+    sha256 "1baf8966058143cc99f448f9baf51cff573a9a3bb1ffa49f5d89add74873ded3" => :sierra
+    sha256 "6f7436e8a0223ae23d383c10017a2738c93e3912fdcd98e25990b3108e19e7f1" => :el_capitan
+    sha256 "45baac4badfe2ac3ea77e51cd8d4f9fb9ec18220c4ee233df6d4ab3d83b58933" => :yosemite
   end
 
   deprecated_option "disable-nls" => "without-nls"
