11e5d6f7d5415e75078883e2d16f61d6b2d2764f
Omar Bahareth <omarb.work@gmail.com>
2017-02-02 11:47:15 +0300

    tika 1.14
    Update Tika to v1.14
    
    Closes #9550.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/tika.rb
+++ Formula/tika.rb
@@ -1,16 +1,16 @@
 class Tika < Formula
   desc "Content analysis toolkit"
   homepage "https://tika.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=tika/tika-app-1.13.jar"
-  sha256 "e340c3fee155b93eb4033feb2302264fff3772c80a5843a047876c44eff23df7"
+  url "https://www.apache.org/dyn/closer.cgi?path=tika/tika-app-1.14.jar"
+  sha256 "403847bf7ac6f55412949e32c5bc91faca57b1d683d191ee9ccb8d06623a2ef6"
 
   bottle :unneeded
 
   depends_on :java => "1.7+"
 
   resource "server" do
-    url "https://search.maven.org/remotecontent?filepath=org/apache/tika/tika-server/1.13/tika-server-1.13.jar"
-    sha256 "97bcecd72271c75ecd715619e4f91bcaae84f0e06a1e9c4f3ba48d90be9912df"
+    url "https://search.maven.org/remotecontent?filepath=org/apache/tika/tika-server/1.14/tika-server-1.14.jar"
+    sha256 "d3915c30521206ec1f206e2ae1279cbeabcad19a56f9ddb58b1a47eba934f6d1"
   end
 
   def install
