b2b3dfa7f90ce98860efb492ed4b834baed63064
Harshavardhana <harsha@minio.io>
2017-02-02 18:16:37 -0800

    minio-mc 20170202223848
    Closes #9585.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/minio-mc.rb
+++ Formula/minio-mc.rb
@@ -2,9 +2,9 @@ class MinioMc < Formula
   desc "ls, cp, mkdir, diff and rsync for filesystems and object storage"
   homepage "https://github.com/minio/mc"
   url "https://github.com/minio/mc.git",
-    :tag => "RELEASE.2016-12-09T18-23-19Z",
-    :revision => "64166319eeaed70cdb0e1a24ea97416a9600052e"
-  version "20161209182319"
+    :tag => "RELEASE.2017-02-02T22-38-48Z",
+    :revision => "0098fbcc5b95b916614f199c5d3e0b1078c6cb7e"
+  version "20170202223848"
 
   bottle do
     cellar :any_skip_relocation
