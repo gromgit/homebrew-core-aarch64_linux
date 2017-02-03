bac280bec5d291bf225c9da970146cbf181cc68a
Roberto Tyley <roberto.tyley@gmail.com>
2017-02-03 09:24:13 +0000

    bfg 1.12.15
    Closes #9593.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/bfg.rb
+++ Formula/bfg.rb
@@ -1,16 +1,16 @@
 class Bfg < Formula
   desc "Remove large files or passwords from Git history like git-filter-branch"
   homepage "https://rtyley.github.io/bfg-repo-cleaner/"
-  url "https://search.maven.org/remotecontent?filepath=com/madgag/bfg/1.12.14/bfg-1.12.14.jar"
-  sha256 "7a44d53ef626a4282c34cc363166e678c5d9e91e4c6815e8e069036edd9fab64"
+  url "https://search.maven.org/remotecontent?filepath=com/madgag/bfg/1.12.15/bfg-1.12.15.jar"
+  sha256 "330af214a0fed320c591afc1046b0f31e8a438f290da09672973aeaa6411b09d"
 
   bottle :unneeded
 
   depends_on :java => "1.7+"
 
   def install
-    libexec.install "bfg-1.12.14.jar"
-    bin.write_jar_script libexec/"bfg-1.12.14.jar", "bfg"
+    libexec.install "bfg-1.12.15.jar"
+    bin.write_jar_script libexec/"bfg-1.12.15.jar", "bfg"
   end
 
   test do
