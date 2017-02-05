467b16c5dce3525699ab69013b9c6644391351be
Mike McQuaid <mike@mikemcquaid.com>
2017-02-05 12:40:55 +0100

    jenv: remove non-Homebrew-specific caveats.

--- Formula/jenv.rb
+++ Formula/jenv.rb
@@ -12,15 +12,6 @@ class Jenv < Formula
     bin.write_exec_script libexec/"bin/jenv"
   end
 
-  def caveats; <<-EOS.undent
-     To use Homebrew's directories rather than ~/.jenv add to your profile:
-       export JENV_ROOT=#{var}/jenv
-
-     To enable shims and autocompletion add to your profile:
-       if which jenv > /dev/null; then eval "$(jenv init -)"; fi
-     EOS
-  end
-
   test do
     shell_output("eval \"$(#{bin}/jenv init -)\" && jenv versions")
   end
