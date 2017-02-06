9623a46831e7b67cb6ec196ebfb08d5360c250ec
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-02-06 19:11:46 +0000

    libepoxy 1.4.0
    Closes #9696.
    
    Signed-off-by: Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>

--- Formula/libepoxy.rb
+++ Formula/libepoxy.rb
@@ -1,8 +1,8 @@
 class Libepoxy < Formula
   desc "Library for handling OpenGL function pointer management"
   homepage "https://github.com/anholt/libepoxy"
-  url "https://github.com/anholt/libepoxy/archive/v1.3.1.tar.gz"
-  sha256 "6700ddedffb827b42c72cce1e0be6fba67b678b19bf256e1b5efd3ea38cc2bb4"
+  url "https://download.gnome.org/sources/libepoxy/1.4/libepoxy-1.4.0.tar.xz"
+  sha256 "25a906b14a921bc2b488cfeaa21a00486fe92630e4a9dd346e4ecabeae52ab41"
 
   bottle do
     cellar :any
@@ -16,29 +16,13 @@ class Libepoxy < Formula
   option :universal
 
   depends_on "pkg-config" => :build
-  depends_on "automake" => :build
-  depends_on "autoconf" => :build
-  depends_on "libtool" => :build
   depends_on :python => :build if MacOS.version <= :snow_leopard
-
-  resource "xorg-macros" do
-    url "https://xorg.freedesktop.org/releases/individual/util/util-macros-1.19.0.tar.bz2"
-    sha256 "2835b11829ee634e19fa56517b4cfc52ef39acea0cd82e15f68096e27cbed0ba"
-  end
 
   def install
     ENV.universal_binary if build.universal?
 
-    resource("xorg-macros").stage do
-      system "./configure", "--prefix=#{buildpath}/xorg-macros"
-      system "make", "install"
-    end
-
-    ENV.append_path "PKG_CONFIG_PATH", "#{buildpath}/xorg-macros/share/pkgconfig"
-    ENV.append_path "ACLOCAL_PATH", "#{buildpath}/xorg-macros/share/aclocal"
-
-    system "./autogen.sh", "--disable-dependency-tracking",
-                           "--prefix=#{prefix}"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make"
     system "make", "install"
   end
