15293ea3fe880ea4d083f5e7883967951595be93
ilovezfs <ilovezfs@icloud.com>
2017-02-06 15:55:48 -0800

    grc: work around incompatibility with BSD ls (#9688)

--- Formula/grc.rb
+++ Formula/grc.rb
@@ -3,13 +3,15 @@ class Grc < Formula
   homepage "http://korpus.juls.savba.sk/~garabik/software/grc.html"
   url "https://github.com/garabik/grc/archive/v1.10.tar.gz"
   sha256 "8d65a141c659c5f992b417c15fe8e1283698bb9f39f8b201fd811deb0841d1f0"
+  revision 1
   head "https://github.com/garabik/grc.git"
 
   bottle :unneeded
 
-  conflicts_with "cc65", :because => "both install `grc` binaries"
-
+  depends_on "coreutils"
   depends_on :python3
+
+  conflicts_with "cc65", :because => "both install `grc` binaries"
 
   def install
     inreplace ["grc", "grc.1"], "/etc", etc
@@ -22,6 +24,12 @@ class Grc < Formula
 
     etc.install "grc.bashrc"
     etc.install "grc.zsh" if build.head?
+
+    # Probably can be removed for > 1.10
+    # Upstream issue "Error in ls command alias"
+    # Reported 5 Feb 2017 https://github.com/garabik/grc/issues/50
+    gnubin = Formula["coreutils"].opt_libexec/"gnubin"
+    bin.env_script_all_files(libexec/"bin", :PATH => "#{gnubin}:$PATH")
   end
 
   def caveats; <<-EOS.undent
@@ -31,7 +39,7 @@ class Grc < Formula
   end
 
   test do
-    actual = pipe_output("#{bin}/grcat #{share}/grc/conf.ls", "hello root")
+    actual = pipe_output("#{bin}/grcat #{pkgshare}/conf.ls", "hello root")
     assert_equal "\e[0mhello \e[0m\e[1m\e[37m\e[41mroot\e[0m", actual.chomp
   end
 end
