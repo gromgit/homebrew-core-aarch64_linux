59eafbecf74e81b18a01ef29a8c8074fef89a079
Zhiming Wang <zmwangx@gmail.com>
2017-02-06 13:40:02 -0500

    ffmpeg 3.2.3

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -1,8 +1,8 @@
 class Ffmpeg < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-3.2.2.tar.bz2"
-  sha256 "0b129a56d1b8d06101b1fcbfaa9f4f5eee3182d1ad6e44f511a84c12113a366b"
+  url "https://ffmpeg.org/releases/ffmpeg-3.2.3.tar.bz2"
+  sha256 "e9a41be04e8a7f62f83d0ff5ecaa9c82a857f9200f61b02ef5f304a16fb9b072"
   head "https://github.com/FFmpeg/FFmpeg.git"
 
   bottle do
