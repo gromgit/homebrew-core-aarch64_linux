624c47a42babea473f1ad15629e0860d09911b97
ilovezfs <ilovezfs@icloud.com>
2017-02-06 00:00:17 -0800

    sysbench 1.0.1
    Closes #9679.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/sysbench.rb
+++ Formula/sysbench.rb
@@ -1,8 +1,8 @@
 class Sysbench < Formula
   desc "System performance benchmark tool"
   homepage "https://github.com/akopytov/sysbench"
-  url "https://github.com/akopytov/sysbench/archive/1.0.0.tar.gz"
-  sha256 "c73817799ed646dced6f13899cd01145c775cdcf6d431d1689c3c084ed45eb29"
+  url "https://github.com/akopytov/sysbench/archive/1.0.1.tar.gz"
+  sha256 "27a1426923d83105448180d7ca9680aad9b00c460a13887bce72f877e13de6a7"
 
   bottle do
     sha256 "57227064ec68bf24c1c2a7fe0feab46999dfba810f3154a780591b6ece5299cb" => :sierra
@@ -19,12 +19,6 @@ class Sysbench < Formula
   depends_on :mysql => :recommended
 
   def install
-    # Fixes "dyld: lazy symbol binding failed: Symbol not found: _clock_gettime"
-    # Reported 4 Feb 2017 https://github.com/akopytov/sysbench/issues/105
-    if MacOS.version == :el_capitan && MacOS::Xcode.installed? && MacOS::Xcode.version >= "8.0"
-      ENV["ac_cv_have_decl_clock_gettime"] = "no"
-    end
-
     system "./autogen.sh"
 
     args = ["--prefix=#{prefix}"]
