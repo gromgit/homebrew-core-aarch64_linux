dd54d142f09415bcce4ba652e4e8ead229a266a5
Mike McQuaid <mike@mikemcquaid.com>
2017-02-06 10:26:53 +0000

    git-game: import from homebrew/games.

---
+++ Formula/git-game.rb
@@ -0,0 +1,16 @@
+class GitGame < Formula
+  desc "Game for git to guess who made which commit"
+  homepage "https://github.com/jsomers/git-game"
+  url "https://github.com/jsomers/git-game/archive/1.2.tar.gz"
+  sha256 "d893b2c813388754c16d867cf37726cd7e73c9ccd316735aac43bf1cd3ab1412"
+
+  bottle :unneeded
+
+  def install
+    bin.install "git-game"
+  end
+
+  test do
+    system "git", "game", "help"
+  end
+end
