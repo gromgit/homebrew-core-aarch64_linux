e3330ffffdaa85647c10f2759676ffd58df0f2d3
Sebastian Van Sande <sebastian@vansande.org>
2017-02-07 10:17:47 +0100

    feh 2.18.1
    Closes #9717.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/feh.rb
+++ Formula/feh.rb
@@ -1,8 +1,8 @@
 class Feh < Formula
   desc "X11 image viewer"
   homepage "https://feh.finalrewind.org/"
-  url "https://feh.finalrewind.org/feh-2.16.1.tar.bz2"
-  sha256 "6e55289a3be4495a437a0b037c7b5e86edf64ec74ab63d2d26fa50df1b62b6b3"
+  url "https://feh.finalrewind.org/feh-2.18.1.tar.bz2"
+  sha256 "4c3840285eaf7ad474547d12870430d0392efdce4cf897666aa1ab02612e65b2"
 
   bottle do
     sha256 "5f3fef786e5fcd5f4425ae63dc4dcb1091a98388d38755702f9d2a354a8afff5" => :sierra
