f09f0f4e5b93e7c72bb0630759fe851fcc8d789c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-08 08:28:12 +0000

    lcs: add 4.07.4b bottle.

--- Formula/lcs.rb
+++ Formula/lcs.rb
@@ -6,6 +6,12 @@ class Lcs < Formula
 
   head "svn://svn.code.sf.net/p/lcsgame/code/trunk"
 
+  bottle do
+    sha256 "a8fa614ec5adc3ee2d7417a024bf5e9c78e9f8d4e043e0b916dc5a99f1bb1d9c" => :sierra
+    sha256 "621487b12c93a9b37e1330041f979a28d3d310c1d8c9efecf274808d081d510e" => :el_capitan
+    sha256 "9ca23650e17e177c4c9fa5352dc81a9f415bc3778b2fd8a55330936eb4d7d28c" => :yosemite
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
 
