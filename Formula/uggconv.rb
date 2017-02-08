e1b165964feea44da5119b2f8346c2887a82f889
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-08 08:28:19 +0000

    uggconv: add 1.0 bottle.

--- Formula/uggconv.rb
+++ Formula/uggconv.rb
@@ -4,6 +4,13 @@ class Uggconv < Formula
   url "http://wyrmcorp.com/software/uggconv/uggconv-1.0.tar.gz"
   sha256 "9a215429bc692b38d88d11f38ec40f43713576193558cd8ca6c239541b1dd7b8"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a22594f94bf7baa1908bd1225f52f1db3dd01daa17f99038ecfbd60e22d12b5d" => :sierra
+    sha256 "5ab8b271f2ccc17e5229921f01b92ff7b0c297908902c83d24612bb47592af3c" => :el_capitan
+    sha256 "a40a8a1adee9286acedba6e8eedf20bc53e4bf291fc73478bd3ba0314792c6ce" => :yosemite
+  end
+
   def install
     system "make"
     bin.install "uggconv"
