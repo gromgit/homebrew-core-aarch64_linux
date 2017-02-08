b7992e631390442bcfe9c52bb9678a3713f13d81
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-08 08:28:14 +0000

    ninvaders: add 0.1.1 bottle.

--- Formula/ninvaders.rb
+++ Formula/ninvaders.rb
@@ -4,6 +4,13 @@ class Ninvaders < Formula
   url "https://downloads.sourceforge.net/project/ninvaders/ninvaders/0.1.1/ninvaders-0.1.1.tar.gz"
   sha256 "bfbc5c378704d9cf5e7fed288dac88859149bee5ed0850175759d310b61fd30b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3de94522f9f6f5560e1e6f354470aef0c46de68792fd93bd2b044d45db8328c6" => :sierra
+    sha256 "b2d4f23349e2214d5a0c8b51218974b0f8b2704d333f1bca19ca4b4539e2b9f1" => :el_capitan
+    sha256 "fc7369a82b14fa5879d2e072ac7ceaa1b6b7bd6cabd163e6bea8adb3a5670b80" => :yosemite
+  end
+
   def install
     ENV.j1 # this formula's build system can't parallelize
     inreplace "Makefile" do |s|
