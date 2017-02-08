e489d826903af77f7a456478945815d279eb726f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-08 08:28:16 +0000

    puzzles: update 10274 bottle.

--- Formula/puzzles.rb
+++ Formula/puzzles.rb
@@ -9,10 +9,10 @@ class Puzzles < Formula
   head "git://git.tartarus.org/simon/puzzles.git"
 
   bottle do
-    cellar :any
-    sha256 "c81998464a7c05a2b0c8388d2719b02f22874bf14b83b61c947fbac1d9dfd23d" => :yosemite
-    sha256 "d0ef8a9ea7b3a395557d20c64ef0e7301c8fd62caca5f9e5084f4fb9542dd439" => :mavericks
-    sha256 "91aceb93ef4d2369588f7de865654e7617eb4f6e4f99fd97ee633220c932ede6" => :mountain_lion
+    cellar :any_skip_relocation
+    sha256 "9803aa0125ef2ab48668305752edaea7b37ba80f122c6d8a721b801819a82027" => :sierra
+    sha256 "9803aa0125ef2ab48668305752edaea7b37ba80f122c6d8a721b801819a82027" => :el_capitan
+    sha256 "5bc665ab99098febf65f0c5765ae94cc43c07a1b930bda8b950001d8e1676237" => :yosemite
   end
 
   depends_on "halibut"
