b97a7a2167b9f9ca22b8409a609f78193a0e531c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-08 08:28:08 +0000

    cgoban: add 1.9.12 bottle.

--- Formula/cgoban.rb
+++ Formula/cgoban.rb
@@ -4,6 +4,13 @@ class Cgoban < Formula
   url "http://www.igoweb.org/~wms/comp/cgoban/cgoban-1.9.12.tar.gz"
   sha256 "b9e8b0d2f793fecbc26803d673de11d8cdc88af9d286a6d49b7523f8b4fa20e1"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4f88f760ce464806c607e9a29da5f701cc6d395f27110cd866579fdf8737931a" => :sierra
+    sha256 "f6f9b32dddefb3474dca91d55c0aecdacec7e3b7ccbb2cb8c9b151b41e16f4d0" => :el_capitan
+    sha256 "65b5a814e1b4e3c115c1fb873d8a740ee4c0b39eff33331336d9bfac073e1a27" => :yosemite
+  end
+
   depends_on :x11
 
   def install
