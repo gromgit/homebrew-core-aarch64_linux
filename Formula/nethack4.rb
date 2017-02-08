853342d7b54e564611fe08c225c0f981cb43be19
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-08 14:55:11 +0000

    nethack4: add 4.3.0-beta2 bottle.

--- Formula/nethack4.rb
+++ Formula/nethack4.rb
@@ -6,6 +6,12 @@ class Nethack4 < Formula
   sha256 "b143a86b5e1baf55c663ae09c2663b169d265e95ac43154982296a1887d05f15"
   head "http://nethack4.org/media/nethack4.git"
 
+  bottle do
+    sha256 "e839c203fb39fe659b358ca339563f7c8f00095a9830262cd2295e851d242841" => :sierra
+    sha256 "42a56f8970103a2be4214437596155fc4f103c7b93fb47580f2eb00b9b4c6e31" => :el_capitan
+    sha256 "2dcacbd514524bcac4c52df5c334f5b23e065f01371f6777b001ce8a1826df17" => :yosemite
+  end
+
   # Assumes C11 _Noreturn is available for clang:
   # http://trac.nethack4.org/ticket/568
   fails_with :clang do
