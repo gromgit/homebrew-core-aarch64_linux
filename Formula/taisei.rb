35306c8760df190491d2939abeb94f9581c455be
ilovezfs <ilovezfs@icloud.com>
2017-02-08 05:42:13 -0800

    taisei: import from homebrew/games
    Closes #9762.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/taisei.rb
@@ -0,0 +1,46 @@
+class Taisei < Formula
+  desc "Clone of Touhou Project shoot-em-up games"
+  homepage "http://taisei-project.org/"
+  url "https://github.com/laochailan/taisei/archive/v1.0a.tar.gz"
+  version "1.0a"
+  sha256 "1561c84c9fd8b9c7a91b864bdfc07fb811bb6da5c54cf32a2b6bd63de5f8f3ff"
+
+  depends_on "cmake" => :build
+  depends_on "freealut"
+  depends_on "freetype"
+  depends_on "libpng"
+  depends_on "openal-soft" # OpenAL.framework gave ALUT state error
+  depends_on "sdl"
+  depends_on "sdl_ttf"
+
+  # Fix newline at end of file to match master
+  patch do
+    url "https://github.com/laochailan/taisei/commit/779ff58684b1f229aedfcc03bfc6ac7aac17bf6a.diff"
+    sha256 "eec218752bb025024112442ed9a254e352f71be966de98c3d9d4f1ed482059a0"
+  end
+
+  # Fix missing inline symbols
+  patch do
+    url "https://github.com/laochailan/taisei/commit/0f78b1a7eb05aa741541ca56559d7a3f381b57e2.diff"
+    sha256 "a68859106a5426a4675b2072eb659fd4fb30c46a7c94f3af20a1a2e434685e1b"
+  end
+
+  # Support Mac OS X build
+  patch do
+    url "https://github.com/laochailan/taisei/commit/be8be15.patch"
+    sha256 "29225ba39ce1aa093897ad4276da35a972b320e3ad01ffa14ab7b32e3acb4626"
+  end
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+    (share/"applications").rmtree
+    (share/"icons").rmtree
+  end
+
+  def caveats
+    "Sound may not work."
+  end
+end
