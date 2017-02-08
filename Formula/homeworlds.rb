bd4636e526993c6110d2805f75f0bb4f3f578bae
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-08 08:28:12 +0000

    homeworlds: add 20141022 bottle.

--- Formula/homeworlds.rb
+++ Formula/homeworlds.rb
@@ -5,6 +5,13 @@ class Homeworlds < Formula
     :revision => "917cd7e7e6d0a5cdfcc56cd69b41e3e80b671cde"
   version "20141022"
 
+  bottle do
+    cellar :any
+    sha256 "499e9a94e24c8965b9a31902ab2a14a021c780756451b82ac2313c7c86ac5756" => :sierra
+    sha256 "2665c0ed4da2eb399314d044699385250ca5db54e6f8c22287222b7877881d22" => :el_capitan
+    sha256 "47251f13fa79c98b3c41d45cafade044bded134256f56e6ee1a118f67eb325d8" => :yosemite
+  end
+
   depends_on "wxmac"
 
   def install
