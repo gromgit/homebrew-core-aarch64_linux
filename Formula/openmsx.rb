e8101ff1a1c6606ff9212fe4c7bbf70563579fda
Mike McQuaid <mike@mikemcquaid.com>
2017-02-08 10:33:12 +0000

    openmsx: import from homebrew/games.

---
+++ Formula/openmsx.rb
@@ -0,0 +1,36 @@
+class Openmsx < Formula
+  desc "MSX emulator"
+  homepage "http://openmsx.org/"
+  url "https://github.com/openMSX/openMSX/releases/download/RELEASE_0_13_0/openmsx-0.13.0.tar.gz"
+  sha256 "41e37c938be6fc9f90659f8808418133601a85475058725d3e0dccf2902e62cb"
+  head "https://github.com/openMSX/openMSX.git"
+
+  option "without-opengl", "Disable OpenGL post-processing renderer"
+  option "with-laserdisc", "Enable Laserdisc support"
+
+  depends_on "sdl"
+  depends_on "sdl_ttf"
+  depends_on "freetype"
+  depends_on "libpng"
+  depends_on "glew" if build.with? "opengl"
+
+  if build.with? "laserdisc"
+    depends_on "libogg"
+    depends_on "libvorbis"
+    depends_on "theora"
+  end
+
+  def install
+    inreplace "build/custom.mk", "/opt/openMSX", prefix
+    # Help finding Tcl
+    inreplace "build/libraries.py", /\((distroRoot), \)/, "(\\1, '/usr', '#{MacOS.sdk_path}/usr')"
+    system "./configure"
+    system "make"
+    prefix.install Dir["derived/**/openMSX.app"]
+    bin.write_exec_script "#{prefix}/openMSX.app/Contents/MacOS/openmsx"
+  end
+
+  test do
+    system "#{bin}/openmsx", "-testconfig"
+  end
+end
