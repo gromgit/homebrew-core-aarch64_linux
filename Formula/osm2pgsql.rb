0c71d0ed144b7146951734f649d6f05f1c9252ed
Alexander Hamann <neutric@users.noreply.github.com>
2017-02-09 18:43:59 +0100

    osm2pgsql: revision for postgresql

--- Formula/osm2pgsql.rb
+++ Formula/osm2pgsql.rb
@@ -3,7 +3,7 @@ class Osm2pgsql < Formula
   homepage "https://wiki.openstreetmap.org/wiki/Osm2pgsql"
   url "https://github.com/openstreetmap/osm2pgsql/archive/0.92.0.tar.gz"
   sha256 "b741cfdf6489fd5def721f75a9558b8cda53165dda7ca9548fcc5b43e163ee77"
-  revision 2
+  revision 3
   head "https://github.com/openstreetmap/osm2pgsql.git"
 
   bottle do
