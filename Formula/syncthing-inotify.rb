dda74cec05cb9b28fe8f7fdd01e8a8764e09a709
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-09 07:36:32 +0000

    syncthing-inotify: update 0.8.5 bottle.

--- Formula/syncthing-inotify.rb
+++ Formula/syncthing-inotify.rb
@@ -6,9 +6,9 @@ class SyncthingInotify < Formula
   head "https://github.com/syncthing/syncthing-inotify.git"
 
   bottle do
-    sha256 "9abaaa025ea4448c7f0a98a2fe4dd083640cf808a98192b878bdbd32e927dd43" => :sierra
-    sha256 "dca3eabfc41811d174fedf04301c3279418342fc9cbc39d1b33765e7e185df8a" => :el_capitan
-    sha256 "b53bf4dc4265aebdb7d536562224a34ed1dfa1637a110208c344e80b0587028d" => :yosemite
+    sha256 "dec93b9b38dbb1eb1ac03ee0ba536f48b1cf87f224fd193ffe64f4f824de4646" => :sierra
+    sha256 "f9fee9216c207e1c2ba0b004d4a5dc83a204a5845c7a6445999ba4baebb0d8bb" => :el_capitan
+    sha256 "c6ad5afd61f7c91fb4ed60c6bfccee2a1d9ae532509a7edd2ae24bbaa74dbd4d" => :yosemite
   end
 
   depends_on "go" => :build
