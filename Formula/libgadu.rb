8158efb596135cc96eda40ae5c3750889da7da25
ilovezfs <ilovezfs@icloud.com>
2017-02-09 02:12:49 -0800

    libgadu 1.12.2
    Closes #9795.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libgadu.rb
+++ Formula/libgadu.rb
@@ -1,8 +1,8 @@
 class Libgadu < Formula
   desc "Library for ICQ instant messenger protocol"
   homepage "http://libgadu.net/"
-  url "https://github.com/wojtekka/libgadu/releases/download/1.12.1/libgadu-1.12.1.tar.gz"
-  sha256 "a2244074a89b587ba545b5d87512d6eeda941fec4a839b373712de93308d5386"
+  url "https://github.com/wojtekka/libgadu/releases/download/1.12.2/libgadu-1.12.2.tar.gz"
+  sha256 "28e70fb3d56ed01c01eb3a4c099cc84315d2255869ecf08e9af32c41d4cbbf5d"
 
   bottle do
     cellar :any
@@ -15,7 +15,8 @@ class Libgadu < Formula
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug",
-                          "--disable-dependency-tracking"
+                          "--disable-dependency-tracking",
+                          "--without-pthread"
     system "make", "install"
   end
 end
