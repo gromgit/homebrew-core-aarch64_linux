62e08ff9d3a5c518442a1a1f87974c2022ee12b6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-09 00:36:17 +0000

    bind: update 9.11.0-P3 bottle.

--- Formula/bind.rb
+++ Formula/bind.rb
@@ -8,9 +8,9 @@ class Bind < Formula
   head "https://source.isc.org/git/bind9.git"
 
   bottle do
-    sha256 "ebd3457d697a5abf409b77e415cde3c53e72417060aa48862f0b07a539f56484" => :sierra
-    sha256 "cf3ae06e6dff27f4ce1ec0580430790f3b6173ddf4bdebb464756384a63b63fd" => :el_capitan
-    sha256 "b5183985190a5d853e1189ea36a779fd93d4e5aa55df09fa69cc667003547082" => :yosemite
+    sha256 "d7b9eb300b88812b9aa179df409409d653b29ac7c9a9e1e8c000082ddd63e38c" => :sierra
+    sha256 "7ba650a4f473df454a989d1c97a10c60ffdf702170ec274bb98bf8c27e8ebed5" => :el_capitan
+    sha256 "091ba8dd7e95dad7c35a351f3d97655c3ef1be4507b4bad72fd427c765cb5732" => :yosemite
   end
 
   depends_on "openssl"
