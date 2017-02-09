39f09b09c874c8cfe61248849cc6d9e803168a0d
Curtis Hovey <curtis.hovey@canonical.com>
2017-02-09 15:17:21 -0500

    juju@1.25 1.25.10
    Closes #9811.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/juju@1.25.rb
+++ Formula/juju@1.25.rb
@@ -1,9 +1,8 @@
 class JujuAT125 < Formula
   desc "DevOps management tool"
   homepage "https://jujucharms.com/"
-  url "https://launchpad.net/juju-core/1.25/1.25.8/+download/juju-core_1.25.8.tar.gz"
-  sha256 "7866cf4195d7fe87463bc7501cece12b4d0c3d08b8983f66cecf54f6f8b28267"
-  revision 1
+  url "https://launchpad.net/juju-core/1.25/1.25.10/+download/juju-core_1.25.10.tar.gz"
+  sha256 "8d5e6eee6a3adc9d6c240ef6a5806653644e30dc5b0b9825046a242b07944927"
 
   bottle do
     cellar :any_skip_relocation
@@ -13,6 +12,8 @@ class JujuAT125 < Formula
   end
 
   keg_only :versioned_formula
+
+  depends_on "go" => :build
 
   depends_on "go" => :build
 
