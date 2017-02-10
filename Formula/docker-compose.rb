b0af9c618931bd488b793b1b1ba7dcab1bb5d148
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-10 06:53:50 +0000

    docker-compose: update 1.11.1 bottle.

--- Formula/docker-compose.rb
+++ Formula/docker-compose.rb
@@ -9,9 +9,9 @@ class DockerCompose < Formula
 
   bottle do
     cellar :any
-    sha256 "a94bedf78bc111b54a07cde990a03961bc400fb62e26422a499fc45a07064f6d" => :sierra
-    sha256 "c5bc82ed3a2cbfe99fa2d1f6895e2901dc3cd9892e12fb704b6ff5774d6109e7" => :el_capitan
-    sha256 "873e04032312891c9c4b2de56593a45579636bc7218964f75e1db9010af41ed9" => :yosemite
+    sha256 "2f0c95d3cc9cac92265aae462e3994b49dce147acae90b878210764975fd2705" => :sierra
+    sha256 "6de9307328dcd59134ef41a1592627afc3392987bcbeac2589dd36303f25997c" => :el_capitan
+    sha256 "6d178745ce546d503a1d29237d7f10ba57bd227c4a498377fc59d40d66ade93e" => :yosemite
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
