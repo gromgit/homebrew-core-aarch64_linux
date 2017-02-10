f777e0defd4b0cdc8ac9f2ffeb04fe65cb8fcc99
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-10 15:31:17 +0000

    teleconsole: add 0.3.1 bottle.

--- Formula/teleconsole.rb
+++ Formula/teleconsole.rb
@@ -6,6 +6,13 @@ class Teleconsole < Formula
   url "https://github.com/gravitational/teleconsole/archive/0.3.1.tar.gz"
   sha256 "663307a1dfe4baadf7e1ed9f5b66b1d203bf9696068e9bcd86e535f286e64d59"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "77f2ea18e72c3662cdc3a9b204752b4d18ee65df2a4d9b47d53b31a7440a5109" => :sierra
+    sha256 "d7b7e6dd7c164f7145532f14fae09909651c4f997e75c8b8b2f5b98b9512a117" => :el_capitan
+    sha256 "7e2997cfe314a0b08332bfc6c4611707f8c97aa80ba92bc74a6ce3d2abc9c159" => :yosemite
+  end
+
   depends_on "go" => :build
 
   go_resource "github.com/Sirupsen/logrus" do
