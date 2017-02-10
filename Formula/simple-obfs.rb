e7e932be542699d0fc3cc6e0c6db8958f833b35c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-10 15:26:59 +0000

    simple-obfs: add 0.0.2 bottle.

--- Formula/simple-obfs.rb
+++ Formula/simple-obfs.rb
@@ -4,6 +4,13 @@ class SimpleObfs < Formula
   url "https://github.com/shadowsocks/simple-obfs/archive/v0.0.2.tar.gz"
   sha256 "cfd7b847be57401e1c664662781d41bc5b089b341232d94375ca4dd612b3852f"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "48259596cebc653218edff860e18a1f0daf53d6c8180969e62adb466d2c3e97d" => :sierra
+    sha256 "ce861a5056909c62375f163be15a4fd061473d555c60e6295d0656e513f2df7a" => :el_capitan
+    sha256 "62eb73c58140f4cbcc36715dd84fd6e95385f96eddb4f48ffc11a655b91c842d" => :yosemite
+  end
+
   depends_on "asciidoc" => :build
   depends_on "xmlto" => :build
   depends_on "pcre"
