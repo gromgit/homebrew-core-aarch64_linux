e8710084582a879ebd1d34ddc15f7052fa77208b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-10 08:46:43 +0000

    ephemeralpg: update 2.2_1 bottle.

--- Formula/ephemeralpg.rb
+++ Formula/ephemeralpg.rb
@@ -8,9 +8,9 @@ class Ephemeralpg < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "af7e9dbfb79e73d85fae802c67081631d4cd91eb0589acd86b862d118de1674e" => :sierra
-    sha256 "f8a48478b1fb03439265e149292e8c3db60b9c4b71a4cfa8e940422efef510c2" => :el_capitan
-    sha256 "10d81ab2b2734f216f8afb639eb0f6e111fbad9d37281d219687835260671853" => :yosemite
+    sha256 "b74184bcf5e26faf500d6381bfc2d933d2a0ebfa5f4a13fb2c109e257d591b4d" => :sierra
+    sha256 "2dcbf5709d0242399f79ac4ccf765cbbff0288ab906ff2ad67af3dde482815cd" => :el_capitan
+    sha256 "c3061720932617170d09d7812d722607a71c165106bddc9315d8981a05d9ad17" => :yosemite
   end
 
   depends_on :postgresql
