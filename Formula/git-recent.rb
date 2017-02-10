7a5698b45c796db15b194dcb4d91af15758f28b9
Mike McQuaid <mike@mikemcquaid.com>
2017-02-10 15:08:49 +0000

    git-recent: bottle unneeded.

--- Formula/git-recent.rb
+++ Formula/git-recent.rb
@@ -4,12 +4,7 @@ class GitRecent < Formula
   url "https://github.com/paulirish/git-recent/archive/v1.0.3.tar.gz"
   sha256 "2ea954f3c1cc3917ad1a0ff5cd361dff7c3f82410bd464a9f5decb0a539155ff"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "5e6979e23694dc1ebed5dceb5d9c8f2e3692cbdf1110960df4e333a7cb49d81a" => :sierra
-    sha256 "ad6f9b78d7c5c3769218122c9858238727eca0ef98ea7a958092e0d93cd2df53" => :el_capitan
-    sha256 "5e6979e23694dc1ebed5dceb5d9c8f2e3692cbdf1110960df4e333a7cb49d81a" => :yosemite
-  end
+  bottle :unneeded
 
   def install
     bin.install "git-recent"
