01b4d5f999baa756524d15635fbb0432a749ca5b
Shuanglei Tao <tsl0922@gmail.com>
2017-02-11 20:34:43 +0800

    ttyd 1.3.0
    Closes #9867.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/ttyd.rb
+++ Formula/ttyd.rb
@@ -1,8 +1,8 @@
 class Ttyd < Formula
   desc "Command-line tool for sharing terminal over the web"
   homepage "https://github.com/tsl0922/ttyd"
-  url "https://github.com/tsl0922/ttyd/archive/1.2.2.tar.gz"
-  sha256 "6e85335c11df68e6401178269920a586bce7e66045bd0b225f6d2bc58356d105"
+  url "https://github.com/tsl0922/ttyd/archive/1.3.0.tar.gz"
+  sha256 "1b756ff3782f31ec4677cde4642bc3ef44d52d3b85625eed402405c689d877c0"
   head "https://github.com/tsl0922/ttyd.git"
 
   bottle do
