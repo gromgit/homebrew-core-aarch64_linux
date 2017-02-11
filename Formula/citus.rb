17ded41a0e8412153cf2d6be4828caea95f1b58c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-11 11:35:53 +0000

    citus: update 6.1.0 bottle.

--- Formula/citus.rb
+++ Formula/citus.rb
@@ -7,9 +7,9 @@ class Citus < Formula
 
   bottle do
     cellar :any
-    sha256 "dc99c3fd8339686e7e0e0fa331ae644b74ffbfb413a6625a93729b79b9d7bcff" => :sierra
-    sha256 "1fd647fc24ef0cffef192bc0c36a987c81e1c46d26b368120f2a618f41babdc0" => :el_capitan
-    sha256 "9a54d8afdbcb0353430f3486c7101fe23727bcea33f7e4929c077f5702287f10" => :yosemite
+    sha256 "0a5df9927e6975c03abee7ca49d4853a0977f77d117ad1fc5e97c804891f4625" => :sierra
+    sha256 "6f6345a929607605c5dea7b2725b69d5f68a124d96291ec94c2f470efd35cb7c" => :el_capitan
+    sha256 "17eca2f1b0bb377193b89a6ac6df621c9a25281b027f7c3500dfe157a9ada460" => :yosemite
   end
 
   depends_on "postgresql"
