792bb6de6d6e131fb334887b8375943555a6ccb9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-11 11:17:40 +0000

    httest: add 2.4.12 bottle.

--- Formula/httest.rb
+++ Formula/httest.rb
@@ -4,6 +4,13 @@ class Httest < Formula
   url "https://downloads.sourceforge.net/project/htt/htt2.4/httest-2.4.12/httest-2.4.12.tar.gz"
   sha256 "fd18cdc996c199d56d77e9355c07e1e1701d5550c03fbecb06a255ce72d79bd1"
 
+  bottle do
+    cellar :any
+    sha256 "397795cc9e8bddc8f8a3164530eb1437735285dc3d0710868249a4e1faf28d89" => :sierra
+    sha256 "02ccd19470885531820f61c1734d4f024eac78b548247a68fa5f1be9f20a5501" => :el_capitan
+    sha256 "60b78ecb5540a2bc3d7eb7af04280943ea16c64e97f6e5d048219fb1789260aa" => :yosemite
+  end
+
   depends_on "apr"
   depends_on "apr-util"
   depends_on "openssl"
