a31eda939560347e279e4005dd9e6aed08784af4
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-12 08:48:06 -0300

    smake: fix audit
    smake:
      * `env :std` in formulae is deprecated
      * http://s-make.sourceforge.net/ should be `https://s-make.sourceforge.io/`

--- Formula/smake.rb
+++ Formula/smake.rb
@@ -1,6 +1,6 @@
 class Smake < Formula
   desc "Portable make program with automake features"
-  homepage "http://s-make.sourceforge.net/"
+  homepage "https://s-make.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/s-make/smake-1.2.5.tar.bz2"
   sha256 "27566aa731a400c791cd95361cc755288b44ff659fa879933d4ea35d052259d4"
 
@@ -11,11 +11,6 @@ class Smake < Formula
     sha256 "4e8157c27f8ab0d5ad2c9673a86357f38acfabea1ac4eef80c54e8141dfdb336" => :mavericks
     sha256 "ce1edbcc0ec3f7db2208e39a09183d7dcfa21d50250393f5ad5c83204ab7b3ed" => :mountain_lion
   end
-
-  # A sed operation silently fails on Lion or older, due
-  # to some locale settings in smake's build files. The sed
-  # wrapper on 10.8+ overrides them.
-  env :std if MacOS.version <= :lion
 
   def install
     ENV.deparallelize # the bootstrap smake does not like -j
