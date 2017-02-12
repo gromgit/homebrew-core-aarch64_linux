4b4a19d2512250847f9e6ddc7bdb3f6fa81ef08e
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-11 21:17:04 -0300

    git-number: fix audit
    git-number:
      * `version` (line 6) should be put before `checksum` (line 5)

--- Formula/git-number.rb
+++ Formula/git-number.rb
@@ -2,8 +2,8 @@ class GitNumber < Formula
   desc "Use numbers for dealing with files in git"
   homepage "https://github.com/holygeek/git-number"
   url "https://github.com/holygeek/git-number/archive/1.0.0a.tar.gz"
-  sha256 "710804466860fdb6f985c0a1268887230c4c1ff708584b19e385bbaec872f4f5"
   version "1.0.0a"
+  sha256 "710804466860fdb6f985c0a1268887230c4c1ff708584b19e385bbaec872f4f5"
 
   bottle do
     cellar :any_skip_relocation
