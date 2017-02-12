cea35c82c848d17daa9c6b48073643b0b8e5353c
Misty De Meo <mistydemeo@gmail.com>
2017-02-12 21:31:10 +1100

    arx-libertatis: add comment to time commit

--- Formula/arx-libertatis.rb
+++ Formula/arx-libertatis.rb
@@ -29,6 +29,7 @@ class ArxLibertatis < Formula
     # other changes; these minimal inreplaces get it building.
     # HEAD is fine, and the next stable release will contain these changes.
     if build.stable?
+      # https://github.com/arx/ArxLibertatis/commit/39fb9a0e3a6888a6a5f040e39896e88750c89065
       inreplace "src/platform/Time.cpp", "clock_t", "clockid_t"
 
       # Version parsing is broken in the current stable; fixed upstream.
