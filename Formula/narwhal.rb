17be1473c923f8b4f77c4b2f784c4e23cbb0b3e7
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-12 08:21:15 -0300

    narwhal: fix audit
    narwhal:
      * `checksum` (line 6) should be put before `head` (line 4)

--- Formula/narwhal.rb
+++ Formula/narwhal.rb
@@ -1,9 +1,9 @@
 class Narwhal < Formula
   desc "General purpose JavaScript platform for building applications"
   homepage "https://github.com/280north/narwhal"
-  head "https://github.com/280north/narwhal.git"
   url "https://github.com/280north/narwhal/archive/v0.3.2.tar.gz"
   sha256 "a26ac20097839a5c7b5de665678fb76699371eea433d6e3b820d4d8de2ad4937"
+  head "https://github.com/280north/narwhal.git"
 
   bottle :unneeded
 
