3fb6940cb15612f8e32513090ca3a7962745deb6
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-11 21:10:57 -0300

    cclive: fix audit
    cclive:
      * `depends_on` (line 17) should be put before `conflicts_with` (line 15)

--- Formula/cclive.rb
+++ Formula/cclive.rb
@@ -12,12 +12,12 @@ class Cclive < Formula
     sha256 "74168fac20d931d1b6e2c702ac5818188288271100c9e3f15245b63a1e27d9bc" => :mavericks
   end
 
-  conflicts_with "clozure-cl", :because => "both install a ccl binary"
-
   depends_on "pkg-config" => :build
   depends_on "quvi"
   depends_on "boost"
   depends_on "pcre"
+
+  conflicts_with "clozure-cl", :because => "both install a ccl binary"
 
   def install
     system "./configure", "--disable-dependency-tracking",
