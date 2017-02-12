78180f7fe2cb2b52e41cc8c8241dbe2903c064e5
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-12 08:15:37 -0300

    mpssh: fix audit
    mpssh:
      * `stable block` (line 17) should be put before `bottle block` (line 8)
      * `url` should be put inside `stable block`
      * `checksum` should be put inside `stable block`

--- Formula/mpssh.rb
+++ Formula/mpssh.rb
@@ -1,9 +1,17 @@
 class Mpssh < Formula
   desc "Mass parallel ssh"
   homepage "https://github.com/ndenev/mpssh"
-  url "https://github.com/ndenev/mpssh/archive/1.3.3.tar.gz"
-  sha256 "510e11c3e177a31c1052c8b4ec06357c147648c86411ac3ed4ac814d0d927f2f"
   head "https://github.com/ndenev/mpssh.git"
+
+  stable do
+    url "https://github.com/ndenev/mpssh/archive/1.3.3.tar.gz"
+    sha256 "510e11c3e177a31c1052c8b4ec06357c147648c86411ac3ed4ac814d0d927f2f"
+    patch do
+      # don't install binaries as root (upstream commit)
+      url "https://github.com/ndenev/mpssh/commit/3cbb868b6fdf8dff9ab86868510c0455ad1ec1b3.diff"
+      sha256 "f5df424a91df1f427f96cd482d0bc22cfd90ac25c9e6beb8ca029f3a1038c3de"
+    end
+  end
 
   bottle do
     cellar :any_skip_relocation
@@ -12,14 +20,6 @@ class Mpssh < Formula
     sha256 "2b91c9a9dbae19e99b8b8735bb3292cc056dcf8e06472c0b2d354f64896a4186" => :yosemite
     sha256 "60d489a872cb7ed8855c0f95913af4fffe3082b6bee8669b0080c3d30d73249d" => :mavericks
     sha256 "a528ce8c03771e3366f5380b18e2a43aba6283e09bb8945234c42dc25116cf41" => :mountain_lion
-  end
-
-  stable do
-    patch do
-      # don't install binaries as root (upstream commit)
-      url "https://github.com/ndenev/mpssh/commit/3cbb868b6fdf8dff9ab86868510c0455ad1ec1b3.diff"
-      sha256 "f5df424a91df1f427f96cd482d0bc22cfd90ac25c9e6beb8ca029f3a1038c3de"
-    end
   end
 
   def install
