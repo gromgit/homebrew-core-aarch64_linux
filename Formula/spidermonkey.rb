b80095621a3fb759bb3da83b66644d153ef92499
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-12 08:51:59 -0300

    spidermonkey: fix audit
    spidermonkey:
      * `depends_on` (line 20) should be put before `conflicts_with` (line 18)

--- Formula/spidermonkey.rb
+++ Formula/spidermonkey.rb
@@ -15,10 +15,10 @@ class Spidermonkey < Formula
     sha256 "38d1b7f54b5dbdd4a0e28e3a1077aed2ada42a9266cfaddeda6a08d761a2d8b2" => :yosemite
   end
 
-  conflicts_with "narwhal", :because => "both install a js binary"
-
   depends_on "readline"
   depends_on "nspr"
+
+  conflicts_with "narwhal", :because => "both install a js binary"
 
   def install
     cd "js/src" do
