957b120c63fb32db53c8b961dcd42f42a075745c
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-12 08:13:46 -0300

    mpgtx: fix audit
    mpgtx:
      * Use `system "make", "install", "cpflags=RP"` instead of `system "make install cpflags=RP"`
      * http://mpgtx.sourceforge.net should be `https://mpgtx.sourceforge.io/`

--- Formula/mpgtx.rb
+++ Formula/mpgtx.rb
@@ -1,6 +1,6 @@
 class Mpgtx < Formula
   desc "Toolbox to manipulate MPEG files"
-  homepage "http://mpgtx.sourceforge.net"
+  homepage "https://mpgtx.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/mpgtx/mpgtx/1.3.1/mpgtx-1.3.1.tar.gz"
   sha256 "8815e73e98b862f12ba1ef5eaaf49407cf211c1f668c5ee325bf04af27f8e377"
 
@@ -19,6 +19,6 @@ class Mpgtx < Formula
     # Unset LFLAGS, "-s" causes the linker to crash
     system "make", "LFLAGS="
     # Overide BSD incompatible cp flags set in makefile
-    system "make install cpflags=RP"
+    system "make", "install", "cpflags=RP"
   end
 end
