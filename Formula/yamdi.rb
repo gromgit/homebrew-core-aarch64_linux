2892ef0172457fd067e931c9dcd93976a9aff56b
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-12 09:11:01 -0300

    yamdi: fix audit
    yamdi:
      * Use `system "#{ENV.cc}", "#{ENV.cflags}", "yamdi.c", "-o", "yamdi"` instead of `system "#{ENV.cc} #{ENV.cflags} yamdi.c -o yamdi"`
      * http://yamdi.sourceforge.net/ should be `https://yamdi.sourceforge.io/`
    
    Closes #9886.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/yamdi.rb
+++ Formula/yamdi.rb
@@ -1,6 +1,6 @@
 class Yamdi < Formula
   desc "Add metadata to Flash video"
-  homepage "http://yamdi.sourceforge.net/"
+  homepage "https://yamdi.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/yamdi/yamdi/1.9/yamdi-1.9.tar.gz"
   sha256 "4a6630f27f6c22bcd95982bf3357747d19f40bd98297a569e9c77468b756f715"
 
@@ -13,7 +13,7 @@ class Yamdi < Formula
   end
 
   def install
-    system "#{ENV.cc} #{ENV.cflags} yamdi.c -o yamdi"
+    system ENV.cc, ENV.cflags, "yamdi.c", "-o", "yamdi"
     bin.install "yamdi"
     man1.install "man1/yamdi.1"
   end
