325fb09e6eaeae2985bbfcbce10cba75e8fe3965
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-12 07:45:07 -0300

    lhasa: fix audit
    lhasa:
      * `depends_on` (line 18) should be put before `conflicts_with` (line 16)

--- Formula/lhasa.rb
+++ Formula/lhasa.rb
@@ -13,12 +13,12 @@ class Lhasa < Formula
     sha256 "eecde92149160d0ec03d5b2f88408d67a4bc03b415ed4a78ff12474f6aa965f4" => :mavericks
   end
 
-  conflicts_with "lha", :because => "both install a `lha` binary"
-
   depends_on "pkg-config" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+
+  conflicts_with "lha", :because => "both install a `lha` binary"
 
   def install
     system "./autogen.sh", "--prefix=#{prefix}"
