3ea9c43460afbbfbb43af436da9bc64e627dd1f7
Eduardo Sebastian <esebastian@altiora.es>
2017-02-12 19:21:45 +0100

    docker-machine-completion 0.9.0
    Closes #9897.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/docker-machine-completion.rb
+++ Formula/docker-machine-completion.rb
@@ -1,9 +1,9 @@
 class DockerMachineCompletion < Formula
   desc "docker-machine completion script"
-  homepage "https://github.com/Ketouem/docker-machine-completions"
-  url "https://github.com/Ketouem/docker-machine-completions/archive/0.1.tar.gz"
-  sha256 "0b76cace0f71043c768e6ebc84ad424c62e70f2557c70389d7440ca71b1e3482"
-  head "https://github.com/Ketouem/docker-machine-completions.git"
+  homepage "https://docs.docker.com/machine/completion/"
+  url "https://github.com/docker/machine/archive/v0.9.0.tar.gz"
+  sha256 "8e445e70a92c98a5e73594d8aea07b31dda6fa4ed2d1e7643663f0267e05f25f"
+  head "https://github.com/docker/machine.git"
 
   bottle :unneeded
 
@@ -11,11 +11,12 @@ class DockerMachineCompletion < Formula
     :because => "docker-machine already includes completion scripts"
 
   def install
-    bash_completion.install "docker-machine_completions.sh" => "docker-machine"
+    bash_completion.install "contrib/completion/bash/docker-machine.bash"
+    zsh_completion.install "contrib/completion/zsh/_docker-machine"
   end
 
   test do
-    assert_match "-F _docker-machine",
-      shell_output("bash -c 'source #{bash_completion}/docker-machine && complete -p docker-machine'")
+    assert_match "-F _docker_machine",
+      shell_output("bash -c 'source #{bash_completion}/docker-machine.bash && complete -p docker-machine'")
   end
 end
