8aaa63b55f10fbbc7cdb6e9ffe7638f85da11d7d
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-12 08:42:38 -0300

    sdl_rtf: fix audit
    sdl_rtf:
      * `head` (line 15) should be put before `bottle block` (line 7)

--- Formula/sdl_rtf.rb
+++ Formula/sdl_rtf.rb
@@ -3,6 +3,7 @@ class SdlRtf < Formula
   homepage "https://www.libsdl.org/projects/SDL_rtf/"
   url "https://www.libsdl.org/projects/SDL_rtf/release/SDL_rtf-0.1.0.tar.gz"
   sha256 "3dc0274b666e28010908ced24844ca7d279e07b66f673c990d530d4ea94b757e"
+  head "https://hg.libsdl.org/SDL_rtf", :using => :hg
 
   bottle do
     cellar :any
@@ -11,8 +12,6 @@ class SdlRtf < Formula
     sha256 "8dd89df32c9ea02bcab36932c2f22bcb6de58d6002bd6fb9e95f9bbfe5ccf41e" => :yosemite
     sha256 "9d077d10fc0102738e3c7d445cf2c8290150f98b4fb92e1b72bb3e5857dc3b3e" => :mavericks
   end
-
-  head "https://hg.libsdl.org/SDL_rtf", :using => :hg
 
   depends_on "sdl"
 
