3c72adfb8422c7c9971b23d5173b2c3d8e831346
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-12 13:40:56 +0000

    purescript: update 0.10.7 bottle.

--- Formula/purescript.rb
+++ Formula/purescript.rb
@@ -10,9 +10,9 @@ class Purescript < Formula
   head "https://github.com/purescript/purescript.git"
 
   bottle do
-    sha256 "eaf59ca3ed0a22aa3b0a98f870181f9f8616e3b075f2004f5544f4676fb8fd10" => :sierra
-    sha256 "ddca04b5e79433357b6719119c1e9b59cb92c2f4a5b8433fc1eefcc0256d1c57" => :el_capitan
-    sha256 "49bb89ad6e013d54bb8881f822ebdeedfd9c738d91759a2a3f4d05ba981feccf" => :yosemite
+    sha256 "1cfbe96996e3e66500df1ae97f5701d8e10eec7fb3c95f4706ac947dbedd1863" => :sierra
+    sha256 "dbe6d2bbc294b0177736f537547b22886491814c2eeb7f911ce8b3d883e7fad8" => :el_capitan
+    sha256 "10a9d1cb56b8db272d770c5def376c50fc3ddca69be52a9d31da5cacba390f8e" => :yosemite
   end
 
   depends_on "ghc" => :build
