3450a59b567da1a1d6bdc4dc61e88dc06f740c45
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-12 08:46:25 -0300

    simple-amqp-client: fix audit
    simple-amqp-client:
      * `revision` (line 7) should be put before `head` (line 6)

--- Formula/simple-amqp-client.rb
+++ Formula/simple-amqp-client.rb
@@ -3,8 +3,8 @@ class SimpleAmqpClient < Formula
   homepage "https://github.com/alanxz/SimpleAmqpClient"
   url "https://github.com/alanxz/SimpleAmqpClient/archive/v2.4.0.tar.gz"
   sha256 "5735ccccd638b2e2c275ca254f2f947bdfe34511247a32822985c3c25239e06e"
-  head "https://github.com/alanxz/SimpleAmqpClient.git"
   revision 1
+  head "https://github.com/alanxz/SimpleAmqpClient.git"
 
   bottle do
     cellar :any
