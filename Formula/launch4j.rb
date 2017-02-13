7ab1dfdb3623e10f0e07c61c0634dfe91de9e60f
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-13 20:55:29 -0300

    launch4j: fix audit --warning

--- Formula/launch4j.rb
+++ Formula/launch4j.rb
@@ -1,6 +1,6 @@
 class Launch4j < Formula
   desc "Cross-platform Java executable wrapper"
-  homepage "http://launch4j.sourceforge.net/"
+  homepage "https://launch4j.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/launch4j/launch4j-3/3.9/launch4j-3.9-macosx-x86.tgz"
   version "3.9"
   sha256 "5de7594b877827be169d88111f6d184db334871c88c2314cd70cc33c3c7344e1"
