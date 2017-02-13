ef1f3f50810a0e64bcb9bdf41045551629cb84b4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-13 23:37:29 +0000

    telegram-cli: add 1.3.1 bottle.

--- Formula/telegram-cli.rb
+++ Formula/telegram-cli.rb
@@ -6,6 +6,12 @@ class TelegramCli < Formula
       :revision => "5935c97ed05b90015418b5208b7beeca15a6043c"
   head "https://github.com/vysheng/tg.git"
 
+  bottle do
+    sha256 "e3d11d044d22c704c4cce49b3e02c1016361c0ca0416cb9c97648c7b6972c6da" => :sierra
+    sha256 "899040eda68f601bcdfbf317159f885d6f6adc59b4c58ce4e1e9d72f7943dc7e" => :el_capitan
+    sha256 "55487d971dec4cbfc5944ba94c691ade8a814de805ea451dea1c5884f2e28925" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "readline"
   depends_on "libevent"
