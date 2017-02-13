fcc0b9194b4e1e620bb3d2e70d03dbafed3bf21d
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-13 20:55:24 -0300

    genext2fs: fix audit --warning

--- Formula/genext2fs.rb
+++ Formula/genext2fs.rb
@@ -1,6 +1,6 @@
 class Genext2fs < Formula
   desc "Generates an ext2 filesystem as a normal (non-root) user"
-  homepage "http://genext2fs.sourceforge.net/"
+  homepage "https://genext2fs.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/genext2fs/genext2fs/1.4.1/genext2fs-1.4.1.tar.gz"
   sha256 "404dbbfa7a86a6c3de8225c8da254d026b17fd288e05cec4df2cc7e1f4feecfc"
 
