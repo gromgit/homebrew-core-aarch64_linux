f79701dc3c00b27a3875571c2117b64542e2e4ee
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-13 20:55:27 -0300

    htmlcxx: fix audit --warning

--- Formula/htmlcxx.rb
+++ Formula/htmlcxx.rb
@@ -1,6 +1,6 @@
 class Htmlcxx < Formula
   desc "Non-validating CSS1 and HTML parser for C++"
-  homepage "http://htmlcxx.sourceforge.net/"
+  homepage "https://htmlcxx.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/htmlcxx/htmlcxx/0.86/htmlcxx-0.86.tar.gz"
   sha256 "07542b5ea2442143b125ba213b6823ff4a23fff352ecdd84bbebe1d154f4f5c1"
 
