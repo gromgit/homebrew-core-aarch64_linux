4d4f5c6ff372be9dec83533522629d1de617103d
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-13 20:55:22 -0300

    fatsort: fix audit --warning

--- Formula/fatsort.rb
+++ Formula/fatsort.rb
@@ -1,6 +1,6 @@
 class Fatsort < Formula
   desc "Sorts FAT16 and FAT32 partitions"
-  homepage "http://fatsort.sourceforge.net/"
+  homepage "https://fatsort.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/fatsort/fatsort-1.3.365.tar.gz"
   sha256 "77acc374b189e80e3d75d3508f3c0ca559f8030f1c220f7cfde719a4adb03f3d"
 
