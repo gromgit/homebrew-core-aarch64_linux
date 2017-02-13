b490ca1404d34b91ac7186b3b94a47b4bbe60a8c
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-13 20:55:41 -0300

    sec: fix audit --warning

--- Formula/sec.rb
+++ Formula/sec.rb
@@ -1,6 +1,6 @@
 class Sec < Formula
   desc "Event correlation tool for event processing of various kinds"
-  homepage "http://simple-evcorr.sourceforge.net/"
+  homepage "https://simple-evcorr.sourceforge.io/"
   url "https://github.com/simple-evcorr/sec/releases/download/2.7.11/sec-2.7.11.tar.gz"
   sha256 "59cd744c36be43c0cb69f1570d2aa6911ebb3492ff01fc292347ec8876dfe991"
 
