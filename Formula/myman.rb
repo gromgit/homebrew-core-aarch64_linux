09c03a5d51e4f7b0b512b98b870ef25b46031f77
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-13 20:55:35 -0300

    myman: fix audit --warning

--- Formula/myman.rb
+++ Formula/myman.rb
@@ -1,6 +1,6 @@
 class Myman < Formula
   desc "Text-mode videogame inspired by Namco's Pac-Man"
-  homepage "http://myman.sourceforge.net/"
+  homepage "https://myman.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/myman/myman-cvs/myman-cvs-2009-10-30/myman-wip-2009-10-30.tar.gz"
   sha256 "bf69607eabe4c373862c81bf56756f2a96eecb8eaa8c911bb2abda78b40c6d73"
   head ":pserver:anonymous:@myman.cvs.sourceforge.net:/cvsroot/myman", :using => :cvs
