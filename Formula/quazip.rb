a025c0a491dbecc5f289667a92c8d979c9dc918b
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-13 20:55:40 -0300

    quazip: fix audit --warning

--- Formula/quazip.rb
+++ Formula/quazip.rb
@@ -1,6 +1,6 @@
 class Quazip < Formula
   desc "C++ wrapper over Gilles Vollant's ZIP/UNZIP package"
-  homepage "http://quazip.sourceforge.net/"
+  homepage "https://quazip.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/quazip/quazip/0.7.3/quazip-0.7.3.tar.gz"
   sha256 "2ad4f354746e8260d46036cde1496c223ec79765041ea28eb920ced015e269b5"
 
