0cfc37f66904a6731eb537c6145f53ecd4043960
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-14 22:15:17 +0000

    dehydrated: add 0.4.0 bottle.

--- Formula/dehydrated.rb
+++ Formula/dehydrated.rb
@@ -4,6 +4,13 @@ class Dehydrated < Formula
   url "https://github.com/lukas2511/dehydrated/archive/v0.4.0.tar.gz"
   sha256 "611da321330ffd43d1dc497990b486b2dec12c59149803ad7d156980c8527f48"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1e10f18db5678d8e22b0b08766d9f4f94ee86ad56111555fe8e715f8de4c00c4" => :sierra
+    sha256 "1e10f18db5678d8e22b0b08766d9f4f94ee86ad56111555fe8e715f8de4c00c4" => :el_capitan
+    sha256 "1e10f18db5678d8e22b0b08766d9f4f94ee86ad56111555fe8e715f8de4c00c4" => :yosemite
+  end
+
   def install
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/lukas2511/dehydrated").install buildpath.children
