f70baa1bf15e666b18ed0eb33bd5ff3495479ad6
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-16 19:51:12 -0300

    xmp: fix audit about sourceforge link

--- Formula/xmp.rb
+++ Formula/xmp.rb
@@ -1,6 +1,6 @@
 class Xmp < Formula
   desc "Command-line player for module music formats (MOD, S3M, IT, etc)"
-  homepage "http://xmp.sourceforge.net"
+  homepage "https://xmp.sourceforge.io"
   url "https://downloads.sourceforge.net/project/xmp/xmp/4.1.0/xmp-4.1.0.tar.gz"
   sha256 "1dbd61074783545ac7bef5b5daa772fd2110764cb70f937af8c3fad30f73289e"
 
