7ac6ae5ef3ea5058f47420f0e6d814aa0848ba2b
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-16 20:24:04 -0300

    autogen: fix audit about sourceforge link

--- Formula/autogen.rb
+++ Formula/autogen.rb
@@ -1,6 +1,6 @@
 class Autogen < Formula
   desc "Automated text file generator"
-  homepage "http://autogen.sourceforge.net"
+  homepage "https://autogen.sourceforge.io"
   url "https://ftpmirror.gnu.org/autogen/autogen-5.18.7.tar.xz"
   mirror "https://ftp.gnu.org/gnu/autogen/autogen-5.18.7.tar.xz"
   sha256 "a7a580a5e18931cb341b255cec2fee2dfd81bea5ddbf0d8ad722703e19aaa405"
