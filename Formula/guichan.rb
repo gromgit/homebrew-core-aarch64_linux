a21102cb36c0f1165d7ccf91220c7e7fcf0320fa
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-16 20:32:14 -0300

    guichan: fix audit about sourceforge link

--- Formula/guichan.rb
+++ Formula/guichan.rb
@@ -1,6 +1,6 @@
 class Guichan < Formula
   desc "small, efficient C++ GUI library designed for games"
-  homepage "http://guichan.sourceforge.net/oldsite/about.shtml"
+  homepage "https://guichan.sourceforge.io"
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/guichan/guichan-0.8.2.tar.gz"
   sha256 "eedf206eae5201eaae027b133226d0793ab9a287bfd74c5f82c7681e3684eeab"
 
