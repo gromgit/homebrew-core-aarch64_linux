265fcdf96a71defc3ebacbb73eaedf930912aa06
ilovezfs <ilovezfs@icloud.com>
2017-02-17 02:11:36 -0800

    ansiweather 1.10 (#10055)

--- Formula/ansiweather.rb
+++ Formula/ansiweather.rb
@@ -1,8 +1,8 @@
 class Ansiweather < Formula
   desc "Weather in your terminal, with ANSI colors and Unicode symbols"
   homepage "https://github.com/fcambus/ansiweather"
-  url "https://github.com/fcambus/ansiweather/archive/1.09.tar.gz"
-  sha256 "75f705263c8ca1eea74039c478b6c760632f26e3b503ac9d04cd1ab188d1ca77"
+  url "https://github.com/fcambus/ansiweather/archive/1.10.tar.gz"
+  sha256 "9656ac85b67ff10c5959f31345bb08bfef0b038f6f2fbe09c78363b28f42cc21"
   head "https://github.com/fcambus/ansiweather.git"
 
   bottle :unneeded
@@ -14,6 +14,6 @@ class Ansiweather < Formula
   end
 
   test do
-    system bin/"ansiweather", "-h"
+    assert_match "Wind", shell_output("#{bin}/ansiweather")
   end
 end
