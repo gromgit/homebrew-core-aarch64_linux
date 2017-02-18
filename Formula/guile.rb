d3868dcfac40724b5775285a38a60bd9b3fc74ab
ilovezfs <ilovezfs@icloud.com>
2017-02-18 09:29:50 -0800

    guile 2.0.14, 2.1.7 (devel)
    Closes #6379.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/guile.rb
+++ Formula/guile.rb
@@ -1,10 +1,31 @@
 class Guile < Formula
   desc "GNU Ubiquitous Intelligent Language for Extensions"
   homepage "https://www.gnu.org/software/guile/"
-  url "https://ftpmirror.gnu.org/guile/guile-2.0.12.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/guile/guile-2.0.12.tar.xz"
-  sha256 "de8187736f9b260f2fa776ed39b52cb74dd389ccf7039c042f0606270196b7e9"
-  revision 1
+
+  stable do
+    url "https://ftpmirror.gnu.org/guile/guile-2.0.14.tar.xz"
+    mirror "https://ftp.gnu.org/gnu/guile/guile-2.0.14.tar.xz"
+    sha256 "e8442566256e1be14e51fc18839cd799b966bc5b16c6a1d7a7c35155a8619d82"
+
+    if MacOS.version >= :sierra
+      # https://debbugs.gnu.org/cgi/bugreport.cgi?bug=23870
+      # http://osdir.com/ml/bug-guile-gnu/2016-06/msg00180.html
+      # https://github.com/Homebrew/homebrew-core/issues/1957#issuecomment-229347476
+      # https://gist.githubusercontent.com/rahulg/baa500e84136f0965e9ade2fb36b90ba/raw/4f1081838972ac9621fc68bb571daaf99fc0c045/libguile-stime-sierra.patch
+      patch :p0 do
+        url "https://raw.githubusercontent.com/macports/macports-ports/5a3bba7/lang/guile/files/sierra.patch"
+        sha256 "6947f15e1aa6129f12eb692253bcc1ff969862f804de1f4d6360ad4786ae53f0"
+      end
+
+      # Filter incompat. mkostemp(3) flags on macOS 10.12
+      # https://trac.macports.org/ticket/52613
+      # https://debbugs.gnu.org/cgi/bugreport.cgi?bug=24862
+      patch :p0 do
+        url "https://raw.githubusercontent.com/macports/macports-ports/8b7f401/lang/guile/files/sierra-filter-incompatible-mkostemp-flags.patch"
+        sha256 "90750429d92a2ea97c828435645a2fd3b399e1b571ced41ff1988894155b4934"
+      end
+    end
+  end
 
   bottle do
     sha256 "fd19aadcaad4476771fd642b39e9e86e420bdcd999bbc16d27b0247dee956513" => :sierra
@@ -14,19 +35,16 @@ class Guile < Formula
 
   devel do
     url "http://git.savannah.gnu.org/r/guile.git",
-        :tag => "v2.1.4",
-        :revision => "f9620e01c3d01abc2fd306ba5dc062a2f252eb97"
+        :tag => "v2.1.7",
+        :revision => "c58c143f31fe4c1717fc8846a8681de2bb4b3869"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "gettext" => :build
 
-    # Fix "error: address argument to atomic operation must be a pointer to
-    # _Atomic type ('gl_uint32_t *' (aka 'unsigned int *') invalid)"
-    patch do
-      url "https://raw.githubusercontent.com/ilovezfs/formula-patches/d2798a4/guile/guile-atomic-type.patch"
-      sha256 "6cec784aa446e4485c79d75ed71c59d04d622293c858cd3d5d5edfe4b5e001ac"
-    end
+    # Avoid undeclared identifier errors for SOCK_CLOEXEC and SOCK_NONBLOCK
+    # Reported 19 Feb 2017 https://debbugs.gnu.org/cgi/bugreport.cgi?bug=25790
+    patch :DATA
   end
 
   head do
@@ -50,17 +68,15 @@ class Guile < Formula
     cause "Segfaults during compilation"
   end
 
-  # https://debbugs.gnu.org/cgi/bugreport.cgi?bug=23870
-  # https://github.com/Homebrew/homebrew-core/issues/1957#issuecomment-229347476
-  if MacOS.version >= :sierra
-    patch do
-      url "https://gist.githubusercontent.com/rahulg/baa500e84136f0965e9ade2fb36b90ba/raw/4f1081838972ac9621fc68bb571daaf99fc0c045/libguile-stime-sierra.patch"
-      sha256 "ff38aa01fe2447bc74ccb6297d2832d0a224ceeb8f00e3a1ca68446d6b1d0f6e"
-    end
-  end
-
   def install
-    system "./autogen.sh" unless build.stable?
+    unless build.stable?
+      # Avoid "address argument to atomic operation must be a pointer to _Atomic type"
+      # Reported 19 Feb 2017 http://debbugs.gnu.org/cgi/bugreport.cgi?bug=25791
+      ENV["ac_cv_header_stdatomic_h"] = "no"
+
+      system "./autogen.sh"
+    end
+
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-libreadline-prefix=#{Formula["readline"].opt_prefix}",
@@ -87,3 +103,21 @@ class Guile < Formula
     system bin/"guile", hello
   end
 end
+
+__END__
+diff --git a/libguile/socket.c b/libguile/socket.c
+index 64df64f..446243c 100644
+--- a/libguile/socket.c
++++ b/libguile/socket.c
+@@ -1655,8 +1655,12 @@ scm_init_socket ()
+ 
+   /* accept4 flags.  No ifdef as accept4 has a gnulib
+      implementation.  */
++#ifdef SOCK_CLOEXEC
+   scm_c_define ("SOCK_CLOEXEC", scm_from_int (SOCK_CLOEXEC));
++#endif
++#ifdef SOCK_NONBLOCK
+   scm_c_define ("SOCK_NONBLOCK", scm_from_int (SOCK_NONBLOCK));
++#endif
+ 
+   /* setsockopt level.
