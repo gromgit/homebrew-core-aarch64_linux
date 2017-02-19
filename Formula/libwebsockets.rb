5592bd62bb4fd4ab56a52f2df28db49ed4753246
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-19 02:08:53 +0000

    libwebsockets: update 2.1.1 bottle.

--- Formula/libwebsockets.rb
+++ Formula/libwebsockets.rb
@@ -6,9 +6,9 @@ class Libwebsockets < Formula
   head "https://github.com/warmcat/libwebsockets.git"
 
   bottle do
-    sha256 "b09efe9e43cf67e2efe96906df774609296eb2e2d2f56701b5bf6ff3548647ca" => :sierra
-    sha256 "702a65dbc2965cf2f34bf25f53d3ac3d69330e85136d2da31772261b00158e19" => :el_capitan
-    sha256 "3b5763bf2588ed7d7e3b985f8d15b47ea1624523c5ec87b3fe980b1397b85f0d" => :yosemite
+    sha256 "ee61a613972a636e61dc68df72e78d66b33f656a9996c9965ed3e03edac01334" => :sierra
+    sha256 "bdf869501cfcbff68c705f9007ea9c47308e4f544798b68fe55c65ec1c5f566a" => :el_capitan
+    sha256 "55fea7bd9bbdc3de96119541fc5057fe74386b0c6bec17271ce6b4c8c40601ef" => :yosemite
   end
 
   depends_on "cmake" => :build
