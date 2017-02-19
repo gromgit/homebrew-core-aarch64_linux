e0ae628471185ddbf26e7bba06b744c624bd3cc6
Alex Dunn <dunn.alex@gmail.com>
2017-02-19 00:32:57 +0000

    aalib: fix homepage

--- Formula/aalib.rb
+++ Formula/aalib.rb
@@ -1,6 +1,6 @@
 class Aalib < Formula
   desc "Portable ASCII art graphics library"
-  homepage "https://aa-project.sourceforge.io/"
+  homepage "https://aa-project.sourceforge.io/aalib/"
   url "https://downloads.sourceforge.net/aa-project/aalib-1.4rc5.tar.gz"
   sha256 "fbddda9230cf6ee2a4f5706b4b11e2190ae45f5eda1f0409dc4f99b35e0a70ee"
 
