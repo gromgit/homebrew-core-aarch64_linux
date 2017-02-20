60bef9a57a7df25c9150201c5d76ce2d3e3a08de
Sven Nierlein <sven@nierlein.de>
2017-02-20 10:25:41 -0500

    monitoring-plugins 2.2 (new formula)

---
+++ Formula/monitoring-plugins.rb
@@ -0,0 +1,39 @@
+class MonitoringPlugins < Formula
+  desc "Plugins for nagios compatible monitoring systems"
+  homepage "https://www.monitoring-plugins.org"
+  url "https://www.monitoring-plugins.org/download/monitoring-plugins-2.2.tar.gz"
+  sha256 "296a538f00a9cbef7f528ff2d43af357a44b384dc98a32389a675b62a6dd3665"
+
+  depends_on "openssl"
+  depends_on "postgresql" => :optional
+  depends_on :mysql => :optional
+
+  conflicts_with "nagios-plugins", :because => "nagios-plugins ships their plugins to the same folder."
+
+  def install
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{libexec}
+      --libexecdir=#{libexec}/sbin
+      --with-openssl=#{Formula["openssl"].opt_prefix}
+    ]
+
+    args << "--with-pgsql=#{Formula["postgresql"].opt_prefix}" if build.with? "postgresql"
+
+    system "./configure", *args
+    system "make", "install"
+    sbin.write_exec_script Dir["#{libexec}/sbin/*"]
+  end
+
+  def caveats
+    <<-EOS.undent
+    All plugins have been installed in:
+      #{HOMEBREW_PREFIX}/sbin
+    EOS
+  end
+
+  test do
+    output = shell_output("#{sbin}/check_dns -H 8.8.8.8 -t 3")
+    assert_match "google-public-dns", output
+  end
+end
