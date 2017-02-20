c53576dc384be1a62364b5d84816967efbcf92d6
FX Coudert <fxcoudert@gmail.com>
2017-02-20 13:18:29 +0100

    poppler: add missing conflicts (#10103)

--- Formula/poppler.rb
+++ Formula/poppler.rb
@@ -31,7 +31,8 @@ class Poppler < Formula
   depends_on "qt5" => :optional
   depends_on "little-cms2" => :optional
 
-  conflicts_with "pdftohtml", :because => "both install `pdftohtml` binaries"
+  conflicts_with "pdftohtml", "pdf2image", "xpdf",
+    :because => "poppler, pdftohtml, pdf2image, and xpdf install conflicting executables"
 
   resource "font-data" do
     url "https://poppler.freedesktop.org/poppler-data-0.4.7.tar.gz"
