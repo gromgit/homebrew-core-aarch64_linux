520306fbba3ff232d40dd9080ff446de074f097a
Mike McQuaid <mike@mikemcquaid.com>
2017-02-21 17:36:03 +0000

    chruby: remove non-Homebrew-specific caveats.

--- Formula/chruby.rb
+++ Formula/chruby.rb
@@ -20,27 +20,10 @@ class Chruby < Formula
 
   def caveats; <<-EOS.undent
     Add the following to the ~/.bashrc or ~/.zshrc file:
-
       source #{opt_share}/chruby/chruby.sh
-
-    By default chruby will search for Rubies installed into /opt/rubies/ or
-    ~/.rubies/. For non-standard installation locations, simply set the RUBIES
-    variable after loading chruby.sh:
-
-      RUBIES+=(
-        /opt/jruby-1.7.0
-        $HOME/src/rubinius
-      )
-
-    If you are migrating from another Ruby manager, set `RUBIES` accordingly:
-
-      RVM:   RUBIES+=(~/.rvm/rubies/*)
-      rbenv: RUBIES+=(~/.rbenv/versions/*)
-      rbfu:  RUBIES+=(~/.rbfu/rubies/*)
 
     To enable auto-switching of Rubies specified by .ruby-version files,
     add the following to ~/.bashrc or ~/.zshrc:
-
       source #{opt_share}/chruby/auto.sh
     EOS
   end
