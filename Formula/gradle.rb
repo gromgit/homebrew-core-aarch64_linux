3d5e7faadca286f05760c40b5fdf70048fe3c0b2
Matty Southall <matty@users.noreply.github.com>
2017-02-21 12:27:11 +0000

    gradle 3.4
    Closes #10181.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/gradle.rb
+++ Formula/gradle.rb
@@ -1,8 +1,8 @@
 class Gradle < Formula
   desc "Build system based on the Groovy language"
   homepage "https://www.gradle.org/"
-  url "https://downloads.gradle.org/distributions/gradle-3.3-all.zip"
-  sha256 "71a787faed83c4ef21e8464cc8452b941b5fcd575043aa29d39d15d879be89f7"
+  url "https://downloads.gradle.org/distributions/gradle-3.4-all.zip"
+  sha256 "37c2fdce55411e4c89b896c292cae1f8f437862c8433c8a74cfc3805d7670c0a"
 
   bottle :unneeded
 
