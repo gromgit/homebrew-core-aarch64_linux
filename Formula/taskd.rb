c879ea5ae6dca56e63a4311bd7744520ae9c6639
ilovezfs <ilovezfs@icloud.com>
2017-02-21 13:43:58 -0800

    taskd: update homepage (#10205)

--- Formula/taskd.rb
+++ Formula/taskd.rb
@@ -1,6 +1,6 @@
 class Taskd < Formula
   desc "Client-server synchronization for todo lists"
-  homepage "https://taskwarrior.org/projects/show/taskwarrior"
+  homepage "https://taskwarrior.org/docs/taskserver/setup.html"
   url "https://taskwarrior.org/download/taskd-1.1.0.tar.gz"
   sha256 "7b8488e687971ae56729ff4e2e5209ff8806cf8cd57718bfd7e521be130621b4"
   revision 1
