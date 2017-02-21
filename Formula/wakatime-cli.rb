c4287d7886d4c76096404bb395fdc5855da15fc5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-21 01:07:01 +0000

    wakatime-cli: update 7.0.2 bottle.

--- Formula/wakatime-cli.rb
+++ Formula/wakatime-cli.rb
@@ -6,9 +6,9 @@ class WakatimeCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a078f8989c6d709ccd01c4523812aabf8f85a408f4796159bcb2cfab20098559" => :sierra
-    sha256 "a078f8989c6d709ccd01c4523812aabf8f85a408f4796159bcb2cfab20098559" => :el_capitan
-    sha256 "a078f8989c6d709ccd01c4523812aabf8f85a408f4796159bcb2cfab20098559" => :yosemite
+    sha256 "972154f7c4c27e50cb7c5a09bf10c6e8a79ac9ffe0d43e777bd4bb88f201f353" => :sierra
+    sha256 "972154f7c4c27e50cb7c5a09bf10c6e8a79ac9ffe0d43e777bd4bb88f201f353" => :el_capitan
+    sha256 "972154f7c4c27e50cb7c5a09bf10c6e8a79ac9ffe0d43e777bd4bb88f201f353" => :yosemite
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
