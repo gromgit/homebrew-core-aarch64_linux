be309a7d6b99875f5b01a1838980c4f4c8740f30
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-22 02:28:07 +0000

    protobuf@2.6: update 2.6.0 bottle.

--- Formula/protobuf@2.6.rb
+++ Formula/protobuf@2.6.rb
@@ -4,6 +4,12 @@ class ProtobufAT26 < Formula
   url "https://github.com/google/protobuf/releases/download/v2.6.0/protobuf-2.6.0.tar.bz2"
   sha256 "0a2f8533b2e0587a2b4efce0c4c8aea21bbfae1c41c466634d958dedf580f6aa"
 
+  bottle do
+    sha256 "86267a38f98ad22da8d222ff0cbaff8ffb65e8b7f56c7bad788f1a2c451ff668" => :sierra
+    sha256 "10be7a7a45c9ea4ec06f70f62522f3296f3d3b49ec6fb9e3fe071268f262ef88" => :el_capitan
+    sha256 "6337d6ac951577be7ec1bac4587b6c91325bacb53f6e3523ced7bd4b78fd6f7d" => :yosemite
+  end
+
   keg_only :versioned_formula
 
   # this will double the build time approximately if enabled
