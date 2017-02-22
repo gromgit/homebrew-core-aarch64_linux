5de7356c45c025f94d87ac06fda618d8614a227c
Curtis Hovey <curtis.hovey@canonical.com>
2017-02-22 17:34:57 -0500

    juju 2.1.0
    Closes #10255.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -1,19 +1,14 @@
 class Juju < Formula
   desc "DevOps management tool"
   homepage "https://jujucharms.com/"
-  url "https://launchpad.net/juju/2.0/2.0.3/+download/juju-core_2.0.3.tar.gz"
-  sha256 "b37e78b91c5d96ddd28044b645f4fe7144f7c44e55ddc2dfea2ad134101a4ed0"
+  url "https://launchpad.net/juju/2.1/2.1.0/+download/juju-core_2.1.0.tar.gz"
+  sha256 "babfd21373f5866473dbd126aad460d17a71f972edaaa17eab0e6c239e5d2bd5"
 
   bottle do
     cellar :any_skip_relocation
     sha256 "079d70b168b3269648459958271ffcdf75823d81d0ea5c6f68c143a2f54df36d" => :sierra
     sha256 "5cb9110b133c45925bc86e2d962a760af67f065439c0ab32fc2efc18c3f1ab35" => :el_capitan
     sha256 "9beb96271da0a17929a7af8dba29af789c31ab022aae07eb6444192c4255b6df" => :yosemite
-  end
-
-  devel do
-    url "https://launchpad.net/juju/2.1/2.1-rc1/+download/juju-core_2.1-rc1.tar.gz"
-    sha256 "dde7058b904d167c671e68a6915f6448bdcb04f2102923e136e28dac15c2e229"
   end
 
   depends_on "go" => :build
@@ -23,11 +18,7 @@ class Juju < Formula
     system "go", "build", "github.com/juju/juju/cmd/juju"
     system "go", "build", "github.com/juju/juju/cmd/plugins/juju-metadata"
     bin.install "juju", "juju-metadata"
-    if build.stable?
-      bash_completion.install "src/github.com/juju/juju/etc/bash_completion.d/juju-2.0"
-    else
-      bash_completion.install "src/github.com/juju/juju/etc/bash_completion.d/juju"
-    end
+    bash_completion.install "src/github.com/juju/juju/etc/bash_completion.d/juju"
   end
 
   test do
