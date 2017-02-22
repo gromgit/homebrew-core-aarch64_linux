7dbc5afcafc60c389ed47354f7c7a6d035b81c1d
ilovezfs <ilovezfs@icloud.com>
2017-02-22 03:20:25 -0800

    geoserver 2.10.2 (#10229)

--- Formula/geoserver.rb
+++ Formula/geoserver.rb
@@ -1,8 +1,8 @@
 class Geoserver < Formula
   desc "Java server to share and edit geospatial data"
   homepage "http://geoserver.org/"
-  url "https://downloads.sourceforge.net/project/geoserver/GeoServer/2.10.1/geoserver-2.10.1-bin.zip"
-  sha256 "9bbb65041c4293ea3b3956a0dae631cb11b1fefe8a9f866746b766ef244ec8c8"
+  url "https://downloads.sourceforge.net/project/geoserver/GeoServer/2.10.2/geoserver-2.10.2-bin.zip"
+  sha256 "c6856bb51a138c0dcd759ab584751d44d9287e7aaaf4d20aa6b845417ee2f19c"
 
   bottle :unneeded
 
