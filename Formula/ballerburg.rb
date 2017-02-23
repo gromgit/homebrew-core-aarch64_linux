282e61d97c667af03df90552a1234a3a8411aa52
ilovezfs <ilovezfs@icloud.com>
2017-02-23 00:30:59 -0800

    ballerburg: use https for homepage, stable url, and head url

--- Formula/ballerburg.rb
+++ Formula/ballerburg.rb
@@ -1,9 +1,9 @@
 class Ballerburg < Formula
   desc "Castle combat game"
-  homepage "http://baller.tuxfamily.org/"
-  url "http://download.tuxfamily.org/baller/ballerburg-1.2.0.tar.gz"
+  homepage "https://baller.tuxfamily.org/"
+  url "https://download.tuxfamily.org/baller/ballerburg-1.2.0.tar.gz"
   sha256 "0625f4b213c1180f2cb2179ef2bc6ce35c7e99db2b27306a8690c389ceac6300"
-  head "http://git.tuxfamily.org/baller/baller.git"
+  head "https://git.tuxfamily.org/baller/baller.git"
 
   bottle do
     cellar :any
