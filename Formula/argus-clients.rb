5794a70b0ddc74b1b8f5d3cf2b1067b98163b252
ilovezfs <ilovezfs@icloud.com>
2017-02-23 00:11:31 -0800

    argus-clients: use https for homepage and stable url

--- Formula/argus-clients.rb
+++ Formula/argus-clients.rb
@@ -1,7 +1,7 @@
 class ArgusClients < Formula
   desc "Audit Record Generation and Utilization System clients"
-  homepage "http://qosient.com/argus/"
-  url "http://qosient.com/argus/src/argus-clients-3.0.8.2.tar.gz"
+  homepage "https://qosient.com/argus/"
+  url "https://qosient.com/argus/src/argus-clients-3.0.8.2.tar.gz"
   sha256 "32073a60ddd56ea8407a4d1b134448ff4bcdba0ee7399160c2f801a0aa913bb1"
   revision 1
 
