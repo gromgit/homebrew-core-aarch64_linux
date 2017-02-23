89b661414817d01f5fbdae9de591d28bcff2164b
ilovezfs <ilovezfs@icloud.com>
2017-02-23 07:40:02 -0800

    owamp: use https for stable url

--- Formula/owamp.rb
+++ Formula/owamp.rb
@@ -1,7 +1,7 @@
 class Owamp < Formula
   desc "Implementation of the One-Way Active Measurement Protocol"
   homepage "https://www.internet2.edu/products-services/performance-analytics/performance-tools/"
-  url "http://software.internet2.edu/sources/owamp/owamp-3.4-10.tar.gz"
+  url "https://software.internet2.edu/sources/owamp/owamp-3.4-10.tar.gz"
   sha256 "059f0ab99b2b3d4addde91a68e6e3641c85ce3ae43b85fe9435841d950ee2fb3"
 
   bottle do
