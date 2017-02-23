c7d5eafdf5889259e106b84f6174df0081e10ce7
ilovezfs <ilovezfs@icloud.com>
2017-02-23 00:26:04 -0800

    argus: use https for homepage and stable url

--- Formula/argus.rb
+++ Formula/argus.rb
@@ -1,7 +1,7 @@
 class Argus < Formula
   desc "Audit Record Generation and Utilization System server"
-  homepage "http://qosient.com/argus/"
-  url "http://qosient.com/argus/src/argus-3.0.8.1.tar.gz"
+  homepage "https://qosient.com/argus/"
+  url "https://qosient.com/argus/src/argus-3.0.8.1.tar.gz"
   sha256 "1fb921104c8bd843fb9f5a1c32b57b20bfe8cd8a103b3f1d9bb686b9e6c490a4"
 
   bottle do
