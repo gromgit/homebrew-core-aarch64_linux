3b8a83d6f00fdd0cd23d39b252762302997687a1
ilovezfs <ilovezfs@icloud.com>
2017-02-23 00:27:08 -0800

    argyll-cms: use https for homepage and stable url

--- Formula/argyll-cms.rb
+++ Formula/argyll-cms.rb
@@ -1,7 +1,7 @@
 class ArgyllCms < Formula
   desc "ICC compatible color management system"
-  homepage "http://www.argyllcms.com/"
-  url "http://www.argyllcms.com/Argyll_V1.9.2_src.zip"
+  homepage "https://www.argyllcms.com/"
+  url "https://www.argyllcms.com/Argyll_V1.9.2_src.zip"
   version "1.9.2"
   sha256 "4d61ae0b91686dea721d34df2e44eaf36c88da87086fd50ccc4e999a58e9ce90"
 
