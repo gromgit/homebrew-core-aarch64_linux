bd7a79e9c127e0240da3c2a5927a4977ca30af83
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-23 05:53:23 +0000

    charm: update 2.2.1 bottle.

--- Formula/charm.rb
+++ Formula/charm.rb
@@ -8,10 +8,9 @@ class Charm < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0d17b9463753cc790e7624fadaab788bed3b998b64f3e47df6867d2766ce28c5" => :sierra
-    sha256 "8aa5731bad2c9b2a915a709fc6ebeff388ed12f84a7c1cd1c4a28ec6d8949d3b" => :el_capitan
-    sha256 "0421d7e7e5addcba0f62298d664c19d14ae4ea1d4469a18e76a6ffe97b4236e3" => :yosemite
-    sha256 "8525b63c3eb14f480c65109f644fcfb278f0b29667e4644c69e11f80b5e334e4" => :mavericks
+    sha256 "81f53ef3cf04f02a9fd4cfba8e4d62a7617dc3f3d666e67788a6ff9242699ef1" => :sierra
+    sha256 "337e5ec536c1f840aa4165b4a0167fad52bfa88ce273aabbd34b164a450853b5" => :el_capitan
+    sha256 "0bb88c2873be6c396c20ff9fcf2214cc7314a12edb76e57e72a21eb02c758003" => :yosemite
   end
 
   depends_on "go" => :build
