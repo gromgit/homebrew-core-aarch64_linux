8c0f39fce624d566402588d690004328ee75bfef
Brett Koonce <koonce@hello.com>
2017-02-23 19:23:53 -0800

    bagit 4.12.2
    Closes #10287.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/bagit.rb
+++ Formula/bagit.rb
@@ -1,10 +1,12 @@
 class Bagit < Formula
   desc "Library for creation, manipulation, and validation of bags"
   homepage "https://github.com/LibraryOfCongress/bagit-java"
-  url "https://github.com/LibraryOfCongress/bagit-java/releases/download/v4.12.1/bagit-4.12.1.zip"
-  sha256 "2abf838c090867e887cc46bb9f95ab697a6706ee297be8ff8ebc8dc4404ff64e"
+  url "https://github.com/LibraryOfCongress/bagit-java/releases/download/v4.12.2/bagit-4.12.2.zip"
+  sha256 "6fa04f2d5f679894bd0dfcf0b2023dabdf6b2c254da6c28c9a226e8b5bf8303c"
 
   bottle :unneeded
+
+  depends_on :java => "1.8+"
 
   def install
     # put logs in var, not in the Cellar
