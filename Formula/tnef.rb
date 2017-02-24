981326957f4e054d52c3c8b9531519de5f0fad4f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-24 10:32:41 +0000

    tnef: update 1.4.13 bottle.

--- Formula/tnef.rb
+++ Formula/tnef.rb
@@ -6,11 +6,9 @@ class Tnef < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "8971b75ed212864d6fe77ec60a9e9edd24ca43cb5ecacd96f7f99dd271be6387" => :sierra
-    sha256 "f890ad7f42e1abd4f2b4d50fa438642731c82fe01f237ed8926c28537029a7d3" => :el_capitan
-    sha256 "a39a144c679b2ae1dfdde6c3062fd9aefefb9d20c44be0943439a17cd1a81a85" => :yosemite
-    sha256 "f908fdb42132f3cc2904accf4cefa3623f461a5a683bc37f93c258b144d0c465" => :mavericks
-    sha256 "fc3c6f4a0a89f97da20ffea1b99a153e16fc5d9359f73f3c45ca3776912a68e8" => :mountain_lion
+    sha256 "f16d41879d9eb670fd67c74511f2aa6fd6f1e9cfd3d09f4cacd743c781414db5" => :sierra
+    sha256 "8ad0c07995a560fa616d06dddb7c9441bf8801e3975e0930e848d2c6188d850d" => :el_capitan
+    sha256 "e30a805ba061861a3ac3afcfe731c1b314402c93dd6a935f674d0459eaf225cc" => :yosemite
   end
 
   depends_on "autoconf" => :build
