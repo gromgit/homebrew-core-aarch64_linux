bb5ed4d8aa6db392f75df78d94b1b5ece3a5e3a4
FX Coudert <fxcoudert@gmail.com>
2017-02-24 14:34:00 +0100

    lftp 4.7.6
    Closes #10299.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/lftp.rb
+++ Formula/lftp.rb
@@ -1,9 +1,9 @@
 class Lftp < Formula
   desc "Sophisticated file transfer program"
   homepage "https://lftp.tech"
-  url "https://lftp.yar.ru/ftp/lftp-4.7.5.tar.bz2"
-  mirror "ftp://ftp.st.ryukoku.ac.jp/pub/network/ftp/lftp/lftp-4.7.5.tar.bz2"
-  sha256 "90f3cbc827534c3b3a391a2dd8b39cc981ac4991fa24b6f90e2008ccc0a5207d"
+  url "https://lftp.yar.ru/ftp/lftp-4.7.6.tar.bz2"
+  mirror "ftp://ftp.st.ryukoku.ac.jp/pub/network/ftp/lftp/lftp-4.7.6.tar.bz2"
+  sha256 "6b46389e9c2e67af9029a783806facea4c8f0b4d6adca5c1088e948d2fd68ae7"
 
   bottle do
     rebuild 1
@@ -16,20 +16,7 @@ class Lftp < Formula
   depends_on "openssl"
   depends_on "libidn"
 
-  # Fix a cast issue, patch was merged upstream: https://github.com/lavv17/lftp/pull/307
-  # Remove when lftp-4.7.6 is released
-  patch do
-    url "https://github.com/lavv17/lftp/commit/259d642e1fea2ddf38763d49e8e7701f0a947d4c.diff"
-    sha256 "729e9d8e2759e79d2f2a07564dc740dada37870cd8bd7065b322bf827138d2c5"
-  end
-
   def install
-    # Fix "error: use of undeclared identifier 'SSL_OP_NO_TLSv1_1'"
-    # Reported 6 Feb 2017 https://github.com/lavv17/lftp/issues/317
-    # Equivalent to upstream PR from 14 Feb 2017 https://github.com/lavv17/lftp/pull/321
-    inreplace "src/Makefile.in", "$(ZLIB_CPPFLAGS) $(OPENSSL_CPPFLAGS)",
-                                 "$(OPENSSL_CPPFLAGS) $(ZLIB_CPPFLAGS)"
-
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-openssl=#{Formula["openssl"].opt_prefix}",
