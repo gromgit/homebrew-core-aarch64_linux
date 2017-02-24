cc72e11204457a1f7b6ce29ec3fcdbd9be0cf78e
ilovezfs <ilovezfs@icloud.com>
2017-02-24 10:33:05 -0800

    gmime: correct homepage (#10306)
    the conversion to sourceforge.io went awry

--- Formula/gmime.rb
+++ Formula/gmime.rb
@@ -1,6 +1,6 @@
 class Gmime < Formula
   desc "MIME mail utilities"
-  homepage "https://gmime.sourceforge.io"
+  homepage "https://spruce.sourceforge.io/gmime/"
   url "https://download.gnome.org/sources/gmime/2.6/gmime-2.6.23.tar.xz"
   sha256 "7149686a71ca42a1390869b6074815106b061aaeaaa8f2ef8c12c191d9a79f6a"
 
