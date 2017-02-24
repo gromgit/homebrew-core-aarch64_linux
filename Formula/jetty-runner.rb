144931c91c35bf712a02ceb83d41f6055d06468d
ilovezfs <ilovezfs@icloud.com>
2017-02-24 10:33:35 -0800

    jetty-runner: update homepage (#10308)

--- Formula/jetty-runner.rb
+++ Formula/jetty-runner.rb
@@ -1,6 +1,6 @@
 class JettyRunner < Formula
   desc "Use Jetty without an installed distribution"
-  homepage "https://www.eclipse.org/jetty/documentation/9.3.10.v20160621/runner.html"
+  homepage "https://www.eclipse.org/jetty/documentation/9.3.x/index.html"
   url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-runner/9.3.10.v20160621/jetty-runner-9.3.10.v20160621.jar"
   version "9.3.10.v20160621"
   sha256 "1dd8773ec7d4cedc15d5f99f13ff68ff04c3917b801942e02d07336a0d011bab"
