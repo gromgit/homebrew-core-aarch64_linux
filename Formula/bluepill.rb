c487c9481a5a052e2d61b1f6cb23d0daf37c0beb
Khoi Lai <k@khoi.io>
2017-02-24 23:06:42 +0700

    bluepill 0.2.2 (new formula)
    Closes #9779.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/bluepill.rb
@@ -0,0 +1,23 @@
+class Bluepill < Formula
+  desc "iOS testing tool that runs UI tests using multiple simulators"
+  homepage "https://github.com/linkedin/bluepill"
+  url "https://github.com/linkedin/bluepill/archive/v0.2.2.tar.gz"
+  sha256 "51498393b0a59619a3adc624439abf116225d5d2666f7aae9390c2e4f8e88475"
+  head "https://github.com/linkedin/bluepill.git"
+
+  depends_on :macos => :el_capitan # needed for xcode 8
+  depends_on :xcode => :build
+
+  def install
+    xcodebuild "-workspace", "Bluepill.xcworkspace",
+               "-scheme", "bluepill",
+               "-configuration", "Release",
+               "SYMROOT=../",
+               "DSTROOT=../dstroot"
+    bin.install "dstroot/usr/local/bin/bluepill"
+  end
+
+  test do
+    assert_match "Usage:", shell_output("#{bin}/bluepill -h")
+  end
+end
