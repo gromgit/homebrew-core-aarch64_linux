3648c7806938fdcc0026afb6c6b521b39c6f490a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-25 03:23:20 +0000

    buildifier: add 0.4.3 bottle.

--- Formula/buildifier.rb
+++ Formula/buildifier.rb
@@ -4,6 +4,13 @@ class Buildifier < Formula
   url "https://github.com/bazelbuild/buildifier/archive/0.4.3.tar.gz"
   sha256 "f6c092b44639fbb7ba11570c2f38a199d112e278ffba10df3dea5cefee3fe553"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "dbb4f613dcc9f616b5b382b14bb433f332c7d1136adabda5ba13b6a77a6f227e" => :sierra
+    sha256 "8802021f54c7df51e4a9fcac474da3478cd445fcedb9ca0b3c9c297c7cf39a7e" => :el_capitan
+    sha256 "f9124c3d186c89e8f27482e3d3e7195f13df7b5400ccda4117dd36c617f9e711" => :yosemite
+  end
+
   depends_on "bazel" => :build
 
   def install
