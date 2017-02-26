09bbd451b924245f9f6d92332276d3942d5220a2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-26 19:32:53 +0000

    git-secret: update 0.2.2 bottle.

--- Formula/git-secret.rb
+++ Formula/git-secret.rb
@@ -7,10 +7,9 @@ class GitSecret < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ea7e2b5f4421aa058b8ee3f37ea9dd6bbaa51e04a4f509fb700d94dfec89028b" => :sierra
-    sha256 "11f99b9a00368b61b702d8721dd5e3912313193049a774122d0d47821b084e74" => :el_capitan
-    sha256 "d5031333baeed0aeb462697201830c5b322f32bdedcda34fd0bf79956806b572" => :yosemite
-    sha256 "f6a410b125f8bed9125e6fe2c6fb741353725860c023665e272e8d57e28be245" => :mavericks
+    sha256 "560e8aaaa04b0bfa45596ddbbd6adacf0ce709481643b407eaeed9971edb30a0" => :sierra
+    sha256 "560e8aaaa04b0bfa45596ddbbd6adacf0ce709481643b407eaeed9971edb30a0" => :el_capitan
+    sha256 "560e8aaaa04b0bfa45596ddbbd6adacf0ce709481643b407eaeed9971edb30a0" => :yosemite
   end
 
   depends_on :gpg => :recommended
