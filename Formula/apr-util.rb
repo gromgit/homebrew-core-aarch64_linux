724763b73fd45c99757872929f71614f14325fe8
FX Coudert <fxcoudert@gmail.com>
2017-02-26 01:18:14 +0100

    apr-util: drop universal

--- Formula/apr-util.rb
+++ Formula/apr-util.rb
@@ -14,8 +14,6 @@ class AprUtil < Formula
 
   keg_only :provided_by_osx, "Apple's CLT package contains apr."
 
-  option :universal
-
   depends_on "apr"
   depends_on "openssl"
   depends_on "postgresql" => :optional
@@ -26,8 +24,6 @@ class AprUtil < Formula
   depends_on "homebrew/dupes/openldap" => :optional
 
   def install
-    ENV.universal_binary if build.universal?
-
     # Stick it in libexec otherwise it pollutes lib with a .exp file.
     args = %W[
       --prefix=#{libexec}
