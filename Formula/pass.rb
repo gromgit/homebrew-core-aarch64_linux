9d71106d6a6657810d1baae4982b75342eae636b
Jason A. Donenfeld <Jason@zx2c4.com>
2017-02-26 20:00:58 +0100

    pass 1.7
    Closes #10374.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/pass.rb
+++ Formula/pass.rb
@@ -1,34 +1,27 @@
 class Pass < Formula
   desc "Password manager"
   homepage "https://www.passwordstore.org/"
-  url "https://git.zx2c4.com/password-store/snapshot/password-store-1.6.5.tar.xz"
-  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/p/password-store/password-store_1.6.5.orig.tar.xz"
-  sha256 "337a39767e6a8e69b2bcc549f27ff3915efacea57e5334c6068fcb72331d7315"
-  revision 1
+  url "https://git.zx2c4.com/password-store/snapshot/password-store-1.7.tar.xz"
+  sha256 "161ac3bd3c452a97f134aa7aa4668fe3f2401c839fd23c10e16b8c0ae4e15500"
 
   head "https://git.zx2c4.com/password-store", :using => :git
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
     sha256 "c297d2352590d65c7edffe5667a11c4c89d2efa17a4b207e303883cf19c36770" => :sierra
     sha256 "c297d2352590d65c7edffe5667a11c4c89d2efa17a4b207e303883cf19c36770" => :el_capitan
     sha256 "c297d2352590d65c7edffe5667a11c4c89d2efa17a4b207e303883cf19c36770" => :yosemite
   end
 
-  depends_on "pwgen"
+  depends_on "qrencode"
   depends_on "tree"
   depends_on "gnu-getopt"
   depends_on :gpg => :run
 
   def install
-    system "make", "PREFIX=#{prefix}", "install"
-
+    system "make", "PREFIX=#{prefix}", "WITH_ALLCOMP=yes", "BASHCOMPDIR=#{bash_completion}", "ZSHCOMPDIR=#{zsh_completion}", "FISHCOMPDIR=#{fish_completion}", "install"
     elisp.install "contrib/emacs/password-store.el"
     pkgshare.install "contrib"
-    zsh_completion.install "src/completion/pass.zsh-completion" => "_pass"
-    bash_completion.install "src/completion/pass.bash-completion" => "password-store"
-    fish_completion.install "src/completion/pass.fish-completion" => "pass.fish"
   end
 
   test do
