a4f111786a79f2f10641bcc3eecb6008dd3246bc
FX Coudert <fxcoudert@gmail.com>
2017-02-26 01:27:30 +0100

    libghthash: drop universal

--- Formula/libghthash.rb
+++ Formula/libghthash.rb
@@ -13,14 +13,11 @@ class Libghthash < Formula
     sha256 "67fa9f1cda39b827ecd318a9f08980e322be034f00f198c0b7c83c2cf9a3d6a8" => :mountain_lion
   end
 
-  option :universal
-
   depends_on "libtool" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
 
   def install
-    ENV.universal_binary if build.universal?
     system "autoreconf", "-ivf"
     system "./configure", "--disable-dependency-tracking",
            "--prefix=#{prefix}"
