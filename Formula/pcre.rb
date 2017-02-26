384004d6cd83f5fce5fd321a0e19c0763b999543
FX Coudert <fxcoudert@gmail.com>
2017-02-26 01:33:15 +0100

    pcre: drop universal

--- Formula/pcre.rb
+++ Formula/pcre.rb
@@ -21,11 +21,7 @@ class Pcre < Formula
     depends_on "libtool" => :build
   end
 
-  option :universal
-
   def install
-    ENV.universal_binary if build.universal?
-
     system "./autogen.sh" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
