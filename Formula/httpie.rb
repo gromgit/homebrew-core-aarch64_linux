ec9fdfcf75f4a60f121c3a1ffffbd1bb858140a0
Rakesh <rakkesh@users.noreply.github.com>
2017-02-27 05:17:29 +0000

    httpie: add SOCKS support
    Closes #10381.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/httpie.rb
+++ Formula/httpie.rb
@@ -5,7 +5,7 @@ class Httpie < Formula
   homepage "https://httpie.org/"
   url "https://github.com/jkbrzt/httpie/archive/0.9.8.tar.gz"
   sha256 "5ccc65dd8e60a9310f575c1a9600f3cc7daf8704cc88bf6c40118b3659b98dc7"
-  revision 1
+  revision 2
   head "https://github.com/jkbrzt/httpie.git"
 
   bottle do
@@ -24,6 +24,11 @@ class Httpie < Formula
   resource "requests" do
     url "https://files.pythonhosted.org/packages/d9/03/155b3e67fe35fe5b6f4227a8d9e96a14fda828b18199800d161bcefc1359/requests-2.12.3.tar.gz"
     sha256 "de5d266953875e9647e37ef7bfe6ef1a46ff8ddfe61b5b3652edf7ea717ee2b2"
+  end
+
+  resource "PySocks" do
+    url "https://files.pythonhosted.org/packages/fd/70/ba9982cedc9b3ed3c06934f1f46a609e0f23c7bfdf567c52a09f1296b8cb/PySocks-1.6.6.tar.gz"
+    sha256 "02419a225ff5dcfc3c9695ef8fc9b4d8cc99658e650c6d4718d4c8f451e63f41"
   end
 
   def install
