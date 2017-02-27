afb9052172d9d630dcf06675968bb1a994078383
Christian Guggenmos <christian.guggenmos@consol.de>
2017-02-27 15:55:34 +0100

    flyway 4.1.1
    Closes #10411.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/4.0.3/flyway-commandline-4.0.3.tar.gz"
-  sha256 "61bb9ae98e484209d924d46670059a2460a66768c619bd4802e565256b99dafb"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/4.1.1/flyway-commandline-4.1.1.tar.gz"
+  sha256 "7df1f469e2efb5619a461e287dfc123b7a02389a21cbae944fd5d06bf028c287"
 
   bottle :unneeded
 
