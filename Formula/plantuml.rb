e304ef18b1c8785b6f10d0e7749340d512472720
ito <ito_ma@ap-com.co.jp>
2017-02-28 15:51:13 +0900

    plantuml 8057
    Closes #10430.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,12 +1,13 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/plantuml/plantuml.8053.jar"
-  sha256 "9551a8d4a7cce5c7a2fb1124910ff7347aee9a3a90ff2e451d6629fe82f7a380"
+  url "https://downloads.sourceforge.net/project/plantuml/plantuml.8057.jar"
+  sha256 "5519dea83f6a3890cee67655a51932aa0fe6c50000890e5723255cd05f8e0af7"
 
   bottle :unneeded
 
   depends_on "graphviz"
+  depends_on :java
 
   def install
     jar = "plantuml.jar"
