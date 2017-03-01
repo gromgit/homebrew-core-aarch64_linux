14054fa966f9393b6e6bfaaf19d630d6fcb5887a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-01 17:47:09 +0000

    libtensorflow: add 1.0.0 bottle.

--- Formula/libtensorflow.rb
+++ Formula/libtensorflow.rb
@@ -4,6 +4,13 @@ class Libtensorflow < Formula
   url "https://github.com/tensorflow/tensorflow/archive/v1.0.0.tar.gz"
   sha256 "db8b3b8f4134b7c9c1b4165492ad5d5bb78889fcd99ffdffc325e97da3e8c677"
 
+  bottle do
+    cellar :any
+    sha256 "019c831a1ce4136cf5868bdb62496469ade683245c08419a8cc045bb88f192bb" => :sierra
+    sha256 "ef57183754f02645533cab1433b06a1d1fea4f330c61fd595c8a55ace2352982" => :el_capitan
+    sha256 "0053d5d1e1be0cfad55508e11dc73b6b06f5c6a9cb37b58bb0526847c43d5b8a" => :yosemite
+  end
+
   depends_on "bazel" => :build
 
   def install
