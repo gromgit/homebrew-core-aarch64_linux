e4d3511a1088eed76b9545d9a9c4caf4b46e06c3
ilovezfs <ilovezfs@icloud.com>
2017-03-01 02:46:01 -0800

    re2 20170301
    Closes #10475.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/re2.rb
+++ Formula/re2.rb
@@ -1,9 +1,9 @@
 class Re2 < Formula
   desc "Alternative to backtracking PCRE-style regular expression engines"
   homepage "https://github.com/google/re2"
-  url "https://github.com/google/re2/archive/2017-01-01.tar.gz"
-  version "20170101"
-  sha256 "e46019b4428942464bf65ba92f2fcd88739d1b05fe7c3787bc031a03a50a327a"
+  url "https://github.com/google/re2/archive/2017-03-01.tar.gz"
+  version "20170301"
+  sha256 "19db0b87bdc22e7e4c66af17f3170167a1b9cb9e32fd6b26189157f1336b73e8"
   head "https://github.com/google/re2.git"
 
   bottle do
