83b6ba234399fc575c1d0955a1fb36b817667d75
ilovezfs <ilovezfs@icloud.com>
2017-03-01 22:42:16 -0800

    csfml 2.4
    Closes #10505.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/csfml.rb
+++ Formula/csfml.rb
@@ -1,11 +1,9 @@
 class Csfml < Formula
   # Don't update CSFML until there's a corresponding SFML release
   desc "SMFL bindings for C"
-  homepage "http://www.sfml-dev.org/"
-  url "https://github.com/SFML/CSFML/archive/2.3.tar.gz"
-  sha256 "ba8f5529fd264c2778844a8b1bb71ede7e902bbd6841275c344dc488ce7054cd"
-  revision 1
-
+  homepage "https://www.sfml-dev.org/"
+  url "https://github.com/SFML/CSFML/archive/2.4.tar.gz"
+  sha256 "4e3d9a03afafbd3a507c39457a7619b68616ec79e870b975e09665e924f9c4c6"
   head "https://github.com/SFML/CSFML.git"
 
   bottle do
