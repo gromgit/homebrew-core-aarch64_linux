090fe15d4079b4e233c7785824ecded94a3e893a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-02 07:01:06 +0000

    xonsh: update 0.5.7 bottle.

--- Formula/xonsh.rb
+++ Formula/xonsh.rb
@@ -8,9 +8,9 @@ class Xonsh < Formula
   head "https://github.com/scopatz/xonsh.git"
 
   bottle do
-    sha256 "fd9f7cbafb6b0759c3e6c7f1f419906e1eae379eeffa599113ed056e7dc353a2" => :sierra
-    sha256 "1b42ea2ad940257c5b6e771ed0aaa9ccdeb3b3fe263b47b581a8fc6c295b33fc" => :el_capitan
-    sha256 "c97ba9bbbbfedb32788a7c62543cf1da9cf85de6b161995ce234c073c0fbcc0a" => :yosemite
+    sha256 "2a13124f9dbda8620024dcaa484a8770f2de556de78736e38cb550dfa0b611f0" => :sierra
+    sha256 "c3c667a7bc5f15a0fc67b9f664939b1632bff314808cf0021b6e4f824ceaafbf" => :el_capitan
+    sha256 "e10642eb1e1dec6bb73ce142a4b767c1fc4fa08424347c926d3e258d7de0e706" => :yosemite
   end
 
   depends_on :python3
