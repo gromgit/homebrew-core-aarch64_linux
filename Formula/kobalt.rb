720584233daeb7a88aaea1046e949f990a55f5bd
ilovezfs <ilovezfs@icloud.com>
2017-03-01 23:53:57 -0800

    kobalt 1.0.2 (#10506)

--- Formula/kobalt.rb
+++ Formula/kobalt.rb
@@ -1,8 +1,8 @@
 class Kobalt < Formula
   desc "Build system"
   homepage "http://beust.com/kobalt"
-  url "https://github.com/cbeust/kobalt/releases/download/0.939/kobalt-0.939.zip"
-  sha256 "59fee164873f74323accdb69f8b2d7e23db6266130f0d9e3fa0579c7f3cc6229"
+  url "https://github.com/cbeust/kobalt/releases/download/1.0.2/kobalt-1.0.2.zip"
+  sha256 "dce08aef6aaa0cf754c7d889f784082a00f303bb66ea51480138854c15edd81a"
 
   bottle :unneeded
 
