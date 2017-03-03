f4e06cfcaa1966915a1235cb3331253cbf85aa43
Tako Schotanus <tako@codejive.org>
2017-03-03 13:22:34 +0100

    ceylon 1.3.2

--- Formula/ceylon.rb
+++ Formula/ceylon.rb
@@ -1,9 +1,8 @@
 class Ceylon < Formula
   desc "Programming language for writing large programs in teams"
   homepage "https://ceylon-lang.org/"
-  url "https://ceylon-lang.org/download/dist/1_3_1"
-  sha256 "3bbc58fd00ead7cc43bb49d54d11a95db9d6ead4799aecab108e290c3b890159"
-  revision 1
+  url "https://ceylon-lang.org/download/dist/1_3_2"
+  sha256 "ec9a3f05a9a361eb1bb5817c5c3930417e8dcf0c46114dca841a5459af2f9c46"
 
   bottle :unneeded
 
