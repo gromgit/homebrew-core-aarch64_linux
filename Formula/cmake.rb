8653bdb8b0bd2f03eeb9fe5db25da3e17b6d7533
ilovezfs <ilovezfs@icloud.com>
2017-03-04 06:26:54 -0800

    cmake 3.8.0-rc2 (devel) (#10582)

--- Formula/cmake.rb
+++ Formula/cmake.rb
@@ -13,8 +13,8 @@ class Cmake < Formula
   end
 
   devel do
-    url "https://cmake.org/files/v3.8/cmake-3.8.0-rc1.tar.gz"
-    sha256 "320a4820d34e1733f14f9ddf0fdd0de90354516260a056246afb0f670afa4d94"
+    url "https://cmake.org/files/v3.8/cmake-3.8.0-rc2.tar.gz"
+    sha256 "f6302ec906eeddd9d5bc1bb4cfc06ffeb49fe9f6cf7d25d64a3acfa05a577b9a"
   end
 
   option "without-docs", "Don't build man pages"
