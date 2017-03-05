2d4df3368117fda94cbeb79702e27297c8ef7a43
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-05 18:14:35 +0000

    docker-credential-helper: add 0.4.2 bottle.

--- Formula/docker-credential-helper.rb
+++ Formula/docker-credential-helper.rb
@@ -5,6 +5,13 @@ class DockerCredentialHelper < Formula
   sha256 "6fb414e71d5352165f43d87e1f711a6638b4ae3b8d4e20a5856a1eaddbd68e36"
   head "https://github.com/docker/docker-credential-helpers.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ba8ed3e3ef6b633aa2980cd37376e13aeb732550d0e53b618bd03e5d68894d70" => :sierra
+    sha256 "c1162fb0846ada1307f1b53160aa8ee9c3223578f1e248829001ce0c033c7753" => :el_capitan
+    sha256 "38e33081722df0b7d0c220dfed7c490ae047dcb193fc87c88c08fac09e724866" => :yosemite
+  end
+
   depends_on "go" => :build
 
   def install
