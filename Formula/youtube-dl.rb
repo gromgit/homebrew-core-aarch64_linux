3e2626608d7fa37ae6d3607db0c08fca86fd31f8
Zhiming Wang <zmwangx@gmail.com>
2017-03-05 16:06:46 -0500

    youtube-dl 2017.03.06
    Closes #10619.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -4,8 +4,8 @@
 class YoutubeDl < Formula
   desc "Download YouTube videos from the command-line"
   homepage "https://rg3.github.io/youtube-dl/"
-  url "https://github.com/rg3/youtube-dl/releases/download/2017.03.05/youtube-dl-2017.03.05.tar.gz"
-  sha256 "da0f4dd74479767d6791643b556fd856d48a1fa99f8c01d3c2ca6e957db9283d"
+  url "https://github.com/rg3/youtube-dl/releases/download/2017.03.06/youtube-dl-2017.03.06.tar.gz"
+  sha256 "7b9a2b785828eab266b80b01ada07ac40e34253797f31b980fdc62174ae00b0a"
 
   bottle do
     cellar :any_skip_relocation
