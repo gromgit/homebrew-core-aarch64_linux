b432310b7829d08bb2705e0d564c1a8d3d5cd5df
JCount <JCount42@gmail.com>
2017-03-05 14:47:08 -0500

    bash-completion: add conflicts_with bash-completion@2
    Closes #10652.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/bash-completion.rb
+++ Formula/bash-completion.rb
@@ -1,12 +1,12 @@
-# NOTE: version 2.0 is out, but it requires Bash 4, and macOS ships
-# with 3.2.48. See homebrew-versions for a 2.0 formula.
+# NOTE: version 2 is out, but it requires Bash 4, and macOS ships
+# with 3.2.57. If you've upgraded bash, use bash-completion@2 instead.
 class BashCompletion < Formula
   desc "Programmable completion for Bash 3.2"
   homepage "https://bash-completion.alioth.debian.org/"
   url "https://bash-completion.alioth.debian.org/files/bash-completion-1.3.tar.bz2"
   mirror "http://pkgs.fedoraproject.org/repo/pkgs/bash-completion/bash-completion-1.3.tar.bz2/a1262659b4bbf44dc9e59d034de505ec/bash-completion-1.3.tar.bz2"
   sha256 "8ebe30579f0f3e1a521013bcdd183193605dab353d7a244ff2582fb3a36f7bec"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any_skip_relocation
@@ -15,6 +15,8 @@ class BashCompletion < Formula
     sha256 "9389068fbb802b321e2b782eac34b2597e6cf031c2ce4f7a6d7436cd5b0699ce" => :el_capitan
     sha256 "9389068fbb802b321e2b782eac34b2597e6cf031c2ce4f7a6d7436cd5b0699ce" => :yosemite
   end
+
+  conflicts_with "bash-completion@2", :because => "Differing version of same formula"
 
   # Backports the following upstream patch from 2.x:
   # https://anonscm.debian.org/gitweb/?p=bash-completion/bash-completion.git;a=commitdiff_plain;h=50ae57927365a16c830899cc1714be73237bdcb2
