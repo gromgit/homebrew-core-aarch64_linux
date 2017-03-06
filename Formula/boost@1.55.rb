4e614d6f62398e2ef4531f1ab2677a4fbb423688
FX Coudert <fxcoudert@gmail.com>
2017-03-06 14:22:17 +0100

    boost@1.55: remove `env :userpaths` (#10637)

--- Formula/boost@1.55.rb
+++ Formula/boost@1.55.rb
@@ -1,6 +1,6 @@
 class BoostAT155 < Formula
   desc "Collection of portable C++ source libraries"
-  homepage "http://www.boost.org"
+  homepage "https://www.boost.org"
   revision 1
 
   stable do
@@ -38,8 +38,6 @@ class BoostAT155 < Formula
   end
 
   keg_only :versioned_formula
-
-  env :userpaths
 
   option "with-icu", "Build regexp engine with icu support"
   option "without-single", "Disable building single-threading variant"
