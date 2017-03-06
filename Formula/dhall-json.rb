e76205606cb0f22fd467a9cc092dd1f379b05a5e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-06 23:57:45 +0000

    dhall-json: add 1.0.0 bottle.

--- Formula/dhall-json.rb
+++ Formula/dhall-json.rb
@@ -9,6 +9,13 @@ class DhallJson < Formula
   sha256 "514e14a765b0fd360dad7aec62980ca02424d6670be9bf5b9a5a171835a7758d"
   head "https://github.com/Gabriel439/Haskell-Dhall-JSON-Library"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2efc13fffa393645a208a495713d66f797b2ff3095eb98d48b8a79413039fd9f" => :sierra
+    sha256 "f38d9d7467133d090eb12db0282620ec571479148f3a95eee290efacfc6b7e72" => :el_capitan
+    sha256 "5a4265e9917ecf5faf3f9fdeef5f24932f36bd008ef286f3f49bdb84785a05d5" => :yosemite
+  end
+
   depends_on "ghc" => :build
   depends_on "cabal-install" => :build
 
