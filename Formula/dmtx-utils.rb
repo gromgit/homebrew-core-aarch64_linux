afd4f40cc1a9bdcd6ec0827781b49cd1d656ae0c
ilovezfs <ilovezfs@icloud.com>
2017-03-07 09:27:28 -0800

    dmtx-utils 0.7.5
    restore from boneyard
    new upstream on GitHub
    now works with ImageMagick 7
    improved test block
    
    Closes #10693.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/dmtx-utils.rb
@@ -0,0 +1,35 @@
+class DmtxUtils < Formula
+  desc "Read and write data matrix barcodes"
+  homepage "https://github.com/dmtx/dmtx-utils"
+  url "https://github.com/dmtx/dmtx-utils/archive/v0.7.5.tar.gz"
+  sha256 "6fa365cd321609db40800f6b34a2387c0ac56ef4643f6101ac83cb762f5ce9ef"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "imagemagick"
+  depends_on "libdmtx"
+  depends_on "libtool" => :run
+
+  resource "test_image12" do
+    url "https://raw.githubusercontent.com/dmtx/libdmtx/ca9313f/test/rotate_test/images/test_image12.png"
+    sha256 "683777f43ce2747c8a6c7a3d294f64bdbfee600d719aac60a18fcb36f7fc7242"
+  end
+
+  def install
+    system "autoreconf", "-fiv"
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    testpath.install resource("test_image12")
+    image = File.read("test_image12.png")
+    assert_equal "9411300724000003", pipe_output("#{bin}/dmtxread", image, 0)
+    system "/bin/dd", "if=/dev/random", "of=in.bin", "bs=512", "count=3"
+    dmtxwrite_output = pipe_output("#{bin}/dmtxwrite", File.read("in.bin"), 0)
+    dmtxread_output = pipe_output("#{bin}/dmtxread", dmtxwrite_output, 0)
+    (testpath/"out.bin").atomic_write dmtxread_output
+    assert_equal (testpath/"in.bin").sha256, (testpath/"out.bin").sha256
+  end
+end
