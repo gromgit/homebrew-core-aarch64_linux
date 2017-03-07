b3d68caf2665253c9bc87d82f9b5f20cbdc66624
ilovezfs <ilovezfs@icloud.com>
2017-03-07 12:20:43 -0800

    git-quick-stats 1.0.3 (#10709)

--- Formula/git-quick-stats.rb
+++ Formula/git-quick-stats.rb
@@ -1,8 +1,8 @@
 class GitQuickStats < Formula
   desc "Simple and efficient way to access statistics in git."
   homepage "https://github.com/arzzen/git-quick-stats"
-  url "https://github.com/arzzen/git-quick-stats/archive/1.0.1.tar.gz"
-  sha256 "397fabda3e894bf058767e32114ce8496ecf23f088e6de6203cae4486e1f755b"
+  url "https://github.com/arzzen/git-quick-stats/archive/1.0.3.tar.gz"
+  sha256 "00d827ca2d51aa752149342bbd294e5bbba329a67679b9c915cc79e6fd8184a4"
 
   bottle :unneeded
 
