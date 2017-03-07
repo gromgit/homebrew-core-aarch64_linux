cc20687f814a1baab353ad9d97aed661249822d3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-07 22:04:05 +0000

    stout: update 1.3.1 bottle.

--- Formula/stout.rb
+++ Formula/stout.rb
@@ -6,9 +6,9 @@ class Stout < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9e1e825f22facc7e04295268f3bd36417f2fffd596e5d1c2d5b71c861172c035" => :sierra
-    sha256 "aac5e3bac19e22e13d626a8d5b241659a9efd66267f970bd62ed0e394250c288" => :el_capitan
-    sha256 "48cae1a0395e12fe8919269033a0bb7b7d2aa314aa2284f2390b7a01bc2fb4a0" => :yosemite
+    sha256 "144aac3cb78b98bf773b19e63e7eb3598261ab264e30b6d39ee3c8fdb9442cf9" => :sierra
+    sha256 "74dac56156c250fef9de8ebae64a1d6ae7b93c068a43f602a4debdc1b23a3945" => :el_capitan
+    sha256 "bc065cf4232169432ce91ea22c456c6891f00a386055c795131dc82572f5a3ae" => :yosemite
   end
 
   depends_on "go" => :build
