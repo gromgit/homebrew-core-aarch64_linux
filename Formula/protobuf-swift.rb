088111791112ed7833c6f14cdf2af78850f577ef
ilovezfs <ilovezfs@icloud.com>
2017-03-07 02:58:16 -0800

    protobuf-swift 3.0.13
    Closes #10678.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/protobuf-swift.rb
+++ Formula/protobuf-swift.rb
@@ -1,8 +1,8 @@
 class ProtobufSwift < Formula
   desc "Implementation of Protocol Buffers in Apple Swift."
   homepage "https://github.com/alexeyxo/protobuf-swift"
-  url "https://github.com/alexeyxo/protobuf-swift/archive/3.0.11.tar.gz"
-  sha256 "0eb6bdcdfe2ef4ff0f1771336a26c70be68272e585efa4c2aab37ca298a7508e"
+  url "https://github.com/alexeyxo/protobuf-swift/archive/3.0.13.tar.gz"
+  sha256 "79bd0604bf8a41120334db94c84c49f9f03c0796a0f985774bf361a659e474d1"
 
   bottle do
     cellar :any
