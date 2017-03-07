f091f834eef22f74535bab5c3a768e2f6f49408e
ilovezfs <ilovezfs@icloud.com>
2017-03-07 15:27:29 -0800

    excel-compare 0.6.1 (#10705)

--- Formula/excel-compare.rb
+++ Formula/excel-compare.rb
@@ -1,13 +1,13 @@
 class ExcelCompare < Formula
   desc "Command-line tool (and API) for diffing Excel Workbooks"
   homepage "https://github.com/na-ka-na/ExcelCompare"
-  url "https://github.com/na-ka-na/ExcelCompare/releases/download/0.6.0/ExcelCompare-0.6.0.zip"
-  sha256 "63bda982644ec8633b60eed5bc199892c428b54addb2eb63dda0b894d98a56c4"
+  url "https://github.com/na-ka-na/ExcelCompare/releases/download/0.6.1/ExcelCompare-0.6.1.zip"
+  sha256 "9da80954db03f23ebcb6571fdd24520a020a0ea05f46fbe4aba8e2af54c6048c"
 
   bottle :unneeded
 
   resource "sample_workbook" do
-    url "https://github.com/na-ka-na/ExcelCompare/raw/0.6.0/test/resources/ss1.xlsx", :using => :nounzip
+    url "https://github.com/na-ka-na/ExcelCompare/raw/0.6.1/test/resources/ss1.xlsx", :using => :nounzip
     sha256 "f362153aea24092e45a3d306a16a49e4faa19939f83cdcb703a215fe48cc196a"
   end
 
