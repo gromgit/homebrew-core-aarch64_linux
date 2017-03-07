7d81abd0d583f65ee1cde548325676a7d0e6ab47
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-07 23:24:50 +0000

    mighttpd2: update 3.4.1 bottle.

--- Formula/mighttpd2.rb
+++ Formula/mighttpd2.rb
@@ -9,9 +9,9 @@ class Mighttpd2 < Formula
   sha256 "0f24c72662be4a0e3d75956fff53899216e29ac7eb29dae771c7e4eb77bdd8d5"
 
   bottle do
-    sha256 "a6e59a654b245351c7e1c22e4bb41c9d597f9dca58c56c7866802940c0a06584" => :sierra
-    sha256 "49ae8aa503d8eb1e5fedc830b0b6a0ceea817cb9cf1c74725156505f5eac9a84" => :el_capitan
-    sha256 "6380d985fae9cf067c134f30f7fcadb2fb6d1807e17f54ba01a3b1941638bba0" => :yosemite
+    sha256 "7f26272fd123a12d54b5cdbf52ede0131885da7e1f9778eb36b6481c6b9e4186" => :sierra
+    sha256 "98d87017e4b7dc074f4da31fab05816338d01421de0e22ad037371f1dc060efd" => :el_capitan
+    sha256 "b21f19f4262c05883e11b37388ff46126c1130bf84c568105e9e47b6266290d6" => :yosemite
   end
 
   depends_on "ghc" => :build
