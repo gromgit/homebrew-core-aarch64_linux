b45334891ab91c60a60675f8266f99421bcf2abe
ilovezfs <ilovezfs@icloud.com>
2017-03-07 12:39:23 -0800

    minimesos 0.12.0 (#10724)

--- Formula/minimesos.rb
+++ Formula/minimesos.rb
@@ -1,8 +1,8 @@
 class Minimesos < Formula
   desc "Testing infrastructure for Mesos frameworks"
   homepage "https://minimesos.org/"
-  url "https://github.com/ContainerSolutions/minimesos/archive/0.11.1.tar.gz"
-  sha256 "75b3416ec1fb56ba809e9766efcdb75ae35fa753a016ae9912ab6fb2f29075e8"
+  url "https://github.com/ContainerSolutions/minimesos/archive/0.12.0.tar.gz"
+  sha256 "0c79f31e37c1d9418125d4c8aa795bfdc2d29d01f516b91041f7212f97b7febd"
 
   bottle :unneeded
 
