09923cbc003952fc65fb5885de4a02d4019b1bfa
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-07 15:33:32 +0000

    gobuster: add 1.3 bottle.

--- Formula/gobuster.rb
+++ Formula/gobuster.rb
@@ -7,6 +7,13 @@ class Gobuster < Formula
   sha256 "606a8b760bc498f9605423b4ef3b093fbb07fead5877a0da81b1c92d8af3ebbb"
   head "https://github.com/OJ/gobuster.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7da053cb4f0a73f55dc0cfaeea6628422e214e29c76a3f82c6ebe58faa2fe659" => :sierra
+    sha256 "2499eb9e86e59b87640cc93fe8270be64cb1b5ce2a7e5a8d5a0bdcf69e68f054" => :el_capitan
+    sha256 "4d781dd4d6bf9b3217d5d197b987583392ba8b6a4abc539720360041779889aa" => :yosemite
+  end
+
   depends_on "go" => :build
 
   go_resource "github.com/satori/go.uuid" do
