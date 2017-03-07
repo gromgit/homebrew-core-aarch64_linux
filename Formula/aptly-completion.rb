d8f9ee258e178a8646efa4e9d475b746716eb0d6
ilovezfs <ilovezfs@icloud.com>
2017-03-07 12:22:37 -0800

    aptly-completion 0.9.7 (#10695)

--- Formula/aptly-completion.rb
+++ Formula/aptly-completion.rb
@@ -1,8 +1,8 @@
 class AptlyCompletion < Formula
   desc "Bash completion for Aptly"
   homepage "https://github.com/aptly-dev/aptly-bash-completion"
-  url "https://github.com/aptly-dev/aptly-bash-completion/archive/0.9.5.tar.gz"
-  sha256 "3b912787808e86ddd7c30555d2b09951e564956dddfe8b521f104651cfe7da96"
+  url "https://github.com/aptly-dev/aptly-bash-completion/archive/0.9.7.tar.gz"
+  sha256 "08504ffd9d8fd00a0cafe5758f92e5dbda2675e77e3d6c258110980c6e7527a6"
   head "https://github.com/aptly-dev/aptly-bash-completion.git"
 
   bottle :unneeded
