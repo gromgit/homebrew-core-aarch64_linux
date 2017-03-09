f6bb5e12ce2396a7a1ec34713564e7a8347c30ac
George Gastaldi <gegastaldi@gmail.com>
2017-03-09 15:02:09 -0300

    jboss-forge 3.6.0.Final (#10782)

--- Formula/jboss-forge.rb
+++ Formula/jboss-forge.rb
@@ -1,11 +1,12 @@
 class JbossForge < Formula
   desc "Tools to help set up and configure a project"
   homepage "https://forge.jboss.org/"
-  url "https://downloads.jboss.org/forge/releases/3.5.1.Final/forge-distribution-3.5.1.Final-offline.zip"
-  version "3.5.1.Final"
-  sha256 "13121caf9c103f87d9344dff717cab52c383614acebfbe117e40ef5c094c36ba"
+  url "https://downloads.jboss.org/forge/releases/3.6.0.Final/forge-distribution-3.6.0.Final-offline.zip"
+  version "3.6.0.Final"
+  sha256 "e7093b7beef94031d00c771306aeccf69a15d45029de92bef51d8b3c3ab3af9d"
 
   bottle :unneeded
+  depends_on :java=>"1.8+"
 
   def install
     rm_f Dir["bin/*.bat"]
