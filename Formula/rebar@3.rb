91cd9f5dd47fc50baae49b634a990fcb01244426
Rui Coelho <rui.coelho@miniclip.com>
2017-03-09 17:06:36 +0000

    rebar@3: import from homebrew/versions
    Closes #10815.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Formula/rebar@3.rb
@@ -0,0 +1,22 @@
+class RebarAT3 < Formula
+  desc "Erlang build tool"
+  homepage "https://github.com/erlang/rebar3"
+  url "https://github.com/erlang/rebar3/archive/3.3.5.tar.gz"
+  sha256 "b17661bedaf2060179e0b7b2c4a64350b7a303d809397325ee4be38cc1dc9058"
+  head "https://github.com/rebar/rebar3.git"
+
+  depends_on "erlang"
+
+  def install
+    system "./bootstrap"
+    bin.install "rebar3"
+
+    bash_completion.install "priv/shell-completion/bash/rebar3"
+    zsh_completion.install "priv/shell-completion/zsh/_rebar3"
+    fish_completion.install "priv/shell-completion/fish/rebar3.fish"
+  end
+
+  test do
+    system bin/"rebar3", "--version"
+  end
+end
