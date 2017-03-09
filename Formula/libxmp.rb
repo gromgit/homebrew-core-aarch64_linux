a9c0086b2cb9113153c19b862a7383ebe6ca2b62
Viktor Szakats <vszakats@users.noreply.github.com>
2017-03-09 22:33:46 +0000

    libxmp: use secure head url

--- Formula/libxmp.rb
+++ Formula/libxmp.rb
@@ -1,6 +1,6 @@
 class Libxmp < Formula
   desc "C library for playback of module music (MOD, S3M, IT, etc)"
-  homepage "https://xmp.sourceforge.io"
+  homepage "https://xmp.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/xmp/libxmp/4.4.1/libxmp-4.4.1.tar.gz"
   sha256 "353535cc84c8cddae8decec4e65fa4c51fc64f22eb0891bc3dae6eaf25f9cccf"
 
@@ -12,12 +12,12 @@ class Libxmp < Formula
   end
 
   head do
-    url "git://git.code.sf.net/p/xmp/libxmp"
+    url "https://git.code.sf.net/p/xmp/libxmp.git"
     depends_on "autoconf" => :build
   end
 
   # CC BY-NC-ND licensed set of five mods by Keith Baylis/Vim! for testing purposes
-  # Mods from Mod Soul Brother: https://web.archive.org/web/20120215215707/http://www.mono211.com/modsoulbrother/vim.html
+  # Mods from Mod Soul Brother: https://web.archive.org/web/20120215215707/www.mono211.com/modsoulbrother/vim.html
   resource "demo_mods" do
     url "https://files.scene.org/get:us-http/mirrors/modsoulbrother/vim/vim-best-of.zip"
     sha256 "df8fca29ba116b10485ad4908cea518e0f688850b2117b75355ed1f1db31f580"
