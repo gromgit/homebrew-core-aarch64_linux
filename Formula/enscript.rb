3673e77edceed986d4573edbf65745e27185f595
Viktor Szakats <vszakats@users.noreply.github.com>
2017-03-09 21:50:54 +0000

    enscript: use secure head url

--- Formula/enscript.rb
+++ Formula/enscript.rb
@@ -5,7 +5,7 @@ class Enscript < Formula
   mirror "https://ftp.gnu.org/gnu/enscript/enscript-1.6.6.tar.gz"
   sha256 "6d56bada6934d055b34b6c90399aa85975e66457ac5bf513427ae7fc77f5c0bb"
 
-  head "git://git.savannah.gnu.org/enscript.git"
+  head "https://git.savannah.gnu.org/git/enscript.git"
 
   bottle do
     rebuild 1
