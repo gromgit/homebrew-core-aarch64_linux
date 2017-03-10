87ba2f38f1131dcf5c02b1be315627fac58110d9
Mike McQuaid <mike@mikemcquaid.com>
2017-03-10 18:50:23 +0000

    docker@1.11: import from homebrew/versions.

---
+++ Formula/docker@1.11.rb
@@ -0,0 +1,42 @@
+class DockerAT111 < Formula
+  desc "Pack, ship and run any application as a lightweight container"
+  homepage "https://www.docker.com/"
+  url "https://github.com/docker/docker.git",
+      :tag => "v1.11.2",
+      :revision => "b9f10c951893f9a00865890a5232e85d770c1087"
+
+  head "https://github.com/docker/docker.git"
+
+  keg_only :versioned_formula
+
+  option "with-experimental", "Enable experimental features"
+  option "without-completions", "Disable bash/zsh completions"
+
+  depends_on "go" => :build
+
+  if build.with? "experimental"
+    depends_on "libtool" => :run
+    depends_on "yubico-piv-tool" => :recommended
+  end
+
+  def install
+    ENV["AUTO_GOPATH"] = "1"
+    ENV["DOCKER_CLIENTONLY"] = "1"
+    ENV["DOCKER_EXPERIMENTAL"] = "1" if build.with? "experimental"
+
+    system "hack/make.sh", "dynbinary"
+
+    build_version = build.head? ? File.read("VERSION").chomp : version
+    bin.install "bundles/#{build_version}/dynbinary/docker-#{build_version}" => "docker"
+
+    if build.with? "completions"
+      bash_completion.install "contrib/completion/bash/docker"
+      fish_completion.install "contrib/completion/fish/docker.fish"
+      zsh_completion.install "contrib/completion/zsh/_docker"
+    end
+  end
+
+  test do
+    system "#{bin}/docker", "--version"
+  end
+end
