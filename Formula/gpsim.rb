9a30ff9f3bca06bf030e1ac0729a605e04c97931
Viktor Szakats <vszakats@users.noreply.github.com>
2017-03-10 08:56:38 +0000

    gpsim: use secure head url

--- Formula/gpsim.rb
+++ Formula/gpsim.rb
@@ -4,7 +4,7 @@ class Gpsim < Formula
   url "https://downloads.sourceforge.net/project/gpsim/gpsim/0.28.0/gpsim-0.28.1.tar.gz"
   sha256 "d8d41fb530630e6df31db89a0ca630038395aed4d07c48859655468ed25658ed"
 
-  head "svn://svn.code.sf.net/p/gpsim/code/trunk"
+  head "https://svn.code.sf.net/p/gpsim/code/trunk"
 
   bottle do
     cellar :any
