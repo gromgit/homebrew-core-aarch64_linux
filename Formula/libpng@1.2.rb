e9c5ef8cb7764968ce093c827bf305ca16753d82
Mike McQuaid <mike@mikemcquaid.com>
2017-03-10 18:50:42 +0000

    libpng@1.2: import from homebrew/versions.

---
+++ Formula/libpng@1.2.rb
@@ -0,0 +1,34 @@
+class LibpngAT12 < Formula
+  desc "Library for manipulating PNG images"
+  homepage "http://www.libpng.org/pub/png/libpng.html"
+  url "ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng12/libpng-1.2.54.tar.xz"
+  mirror "https://dl.bintray.com/homebrew/mirror/libpng-1.2.54.tar.xz"
+  sha256 "cf85516482780f2bc2c5b5073902f12b1519019d47bf473326c2018bdff1d272"
+
+  keg_only :versioned_formula
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "test"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <png.h>
+
+      int main()
+      {
+        png_structp png_ptr;
+        png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+        png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lpng", "-o", "test"
+    system "./test"
+  end
+end
