87b53a31aad9e1617cf03aa3a2b6846ca9a7d8a6
Mike McQuaid <mike@mikemcquaid.com>
2017-03-10 18:51:03 +0000

    varnish@4: import from homebrew/versions.

---
+++ Formula/varnish@4.rb
@@ -0,0 +1,61 @@
+class VarnishAT4 < Formula
+  desc "High-performance HTTP accelerator"
+  homepage "https://www.varnish-cache.org/"
+  url "https://repo.varnish-cache.org/source/varnish-4.1.3.tar.gz"
+  sha256 "9f9469b9fda2a578da2a9d282c71c34eeb5c42eda7f8d8728284d92282108429"
+
+  depends_on "pkg-config" => :build
+  depends_on "docutils" => :build
+  depends_on "pcre"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--localstatedir=#{var}"
+    system "make", "install"
+    (etc+"varnish").install "etc/example.vcl" => "default.vcl"
+    (var+"varnish").mkpath
+  end
+
+  plist_options :manual => "#{HOMEBREW_PREFIX}/sbin/varnishd -n #{HOMEBREW_PREFIX}/var/varnish -f #{HOMEBREW_PREFIX}/etc/varnish/default.vcl -s malloc,1G -T 127.0.0.1:2000 -a 0.0.0.0:8080"
+
+  def plist; <<-EOS.undent
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_sbin}/varnishd</string>
+          <string>-n</string>
+          <string>#{var}/varnish</string>
+          <string>-f</string>
+          <string>#{etc}/varnish/default.vcl</string>
+          <string>-s</string>
+          <string>malloc,1G</string>
+          <string>-T</string>
+          <string>127.0.0.1:2000</string>
+          <string>-a</string>
+          <string>0.0.0.0:8080</string>
+        </array>
+        <key>KeepAlive</key>
+        <true/>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/varnish/varnish.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/varnish/varnish.log</string>
+      </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{sbin}/varnishd -V 2>&1")
+  end
+end
