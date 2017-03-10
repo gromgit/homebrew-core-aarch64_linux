1e4dcb795571ae3a4a94d1420f3b4480f7aa3ab3
Viktor Szakats <vszakats@users.noreply.github.com>
2017-03-10 09:05:25 +0000

    sz81: use secure head url

--- Formula/sz81.rb
+++ Formula/sz81.rb
@@ -3,7 +3,7 @@ class Sz81 < Formula
   homepage "https://sz81.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/sz81/sz81/2.1.7/sz81-2.1.7-source.tar.gz"
   sha256 "4ad530435e37c2cf7261155ec43f1fc9922e00d481cc901b4273f970754144e1"
-  head "svn://svn.code.sf.net/p/sz81/code/sz81"
+  head "https://svn.code.sf.net/p/sz81/code/sz81"
 
   bottle do
     sha256 "853475dfc7991beea12b01669e81fc35ce10e6a9b067716eb026e0ff693d5c4c" => :sierra
