fb438cccedefadacd1b541203e9858c912063c11
Mike McQuaid <mike@mikemcquaid.com>
2017-03-10 18:51:03 +0000

    swig@3.04: import from homebrew/versions.

---
+++ Formula/swig@3.04.rb
@@ -0,0 +1,41 @@
+class SwigAT304 < Formula
+  desc "Generate scripting interfaces to C/C++ code"
+  homepage "http://www.swig.org/"
+  url "https://downloads.sourceforge.net/project/swig/swig/swig-3.0.4/swig-3.0.4.tar.gz"
+  sha256 "410ffa80ef5535244b500933d70c1b65206333b546ca5a6c89373afb65413795"
+
+  keg_only :versioned_formula
+
+  depends_on "pcre"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      int add(int x, int y)
+      {
+        return x + y;
+      }
+    EOS
+    (testpath/"test.i").write <<-EOS.undent
+      %module test
+      %inline %{
+      extern int add(int x, int y);
+      %}
+    EOS
+    (testpath/"run.rb").write <<-EOS.undent
+      require "./test"
+      puts Test.add(1, 1)
+    EOS
+    system "#{bin}/swig", "-ruby", "test.i"
+    system ENV.cc, "-c", "test.c"
+    system ENV.cc, "-c", "test_wrap.c", "-I/System/Library/Frameworks/Ruby.framework/Headers/"
+    system ENV.cc, "-bundle", "-flat_namespace", "-undefined", "suppress", "test.o", "test_wrap.o", "-o", "test.bundle"
+    assert_equal "2", shell_output("ruby run.rb").strip
+  end
+end
