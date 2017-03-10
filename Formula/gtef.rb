2256cc1645ccc7be1b88a12823c81007a294a3c8
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-03-10 20:31:42 +0000

    gtef 1.0.0 (new formula)
    Closes #10864.
    
    Signed-off-by: Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>

---
+++ Formula/gtef.rb
@@ -0,0 +1,95 @@
+class Gtef < Formula
+  desc "GNOME Text Editor Framework"
+  homepage "https://wiki.gnome.org/Projects/Gtef"
+  url "https://download.gnome.org/sources/gtef/1.0/gtef-1.0.0.tar.xz"
+  sha256 "8d17c33adf6d7edd8c55f0bb8f4701c8d8822a67d28c389df04c775eb95fc1b4"
+
+  depends_on "pkg-config" => :build
+  depends_on "vala" => :recommended
+  depends_on "gtksourceview3"
+  depends_on "uchardet"
+
+  def install
+    ENV.delete "SDKROOT"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <gtef/gtef.h>
+
+      int main(int argc, char *argv[]) {
+        GType type = gtef_file_get_type();
+        return 0;
+      }
+    EOS
+    ENV.libxml2
+    atk = Formula["atk"]
+    cairo = Formula["cairo"]
+    fontconfig = Formula["fontconfig"]
+    freetype = Formula["freetype"]
+    gdk_pixbuf = Formula["gdk-pixbuf"]
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    gtkx3 = Formula["gtk+3"]
+    gtksourceview3 = Formula["gtksourceview3"]
+    harfbuzz = Formula["harfbuzz"]
+    libepoxy = Formula["libepoxy"]
+    libpng = Formula["libpng"]
+    pango = Formula["pango"]
+    pcre = Formula["pcre"]
+    pixman = Formula["pixman"]
+    uchardet = Formula["uchardet"]
+    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
+    flags += %W[
+      -I#{atk.opt_include}/atk-1.0
+      -I#{cairo.opt_include}/cairo
+      -I#{fontconfig.opt_include}
+      -I#{freetype.opt_include}/freetype2
+      -I#{gdk_pixbuf.opt_include}/gdk-pixbuf-2.0
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/gio-unix-2.0/
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{gtksourceview3.opt_include}/gtksourceview-3.0
+      -I#{gtkx3.opt_include}/gtk-3.0
+      -I#{harfbuzz.opt_include}/harfbuzz
+      -I#{include}/gtef-1
+      -I#{libepoxy.opt_include}
+      -I#{libpng.opt_include}/libpng16
+      -I#{pango.opt_include}/pango-1.0
+      -I#{pcre.opt_include}
+      -I#{pixman.opt_include}/pixman-1
+      -I#{uchardet.opt_include}/uchardet
+      -D_REENTRANT
+      -L#{atk.opt_lib}
+      -L#{cairo.opt_lib}
+      -L#{gdk_pixbuf.opt_lib}
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{gtksourceview3.opt_lib}
+      -L#{gtkx3.opt_lib}
+      -L#{lib}
+      -L#{pango.opt_lib}
+      -latk-1.0
+      -lcairo
+      -lcairo-gobject
+      -lgdk-3
+      -lgdk_pixbuf-2.0
+      -lgio-2.0
+      -lglib-2.0
+      -lgobject-2.0
+      -lgtef-1
+      -lgtk-3
+      -lgtksourceview-3.0
+      -lintl
+      -lpango-1.0
+      -lpangocairo-1.0
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
