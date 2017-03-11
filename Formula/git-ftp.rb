bd01844deeff063952ce8388dadf74a577ab725b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-11 09:46:29 +0000

    git-ftp: update 1.3.3 bottle.

--- Formula/git-ftp.rb
+++ Formula/git-ftp.rb
@@ -8,9 +8,9 @@ class GitFtp < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "574d76cf9a3aef558d3f938e1441a772b28ac752fa8ffb92253bfb5012ddb389" => :sierra
-    sha256 "f7f6c0fb4176147dd9d6473411ee4a6a3583d6d5fff4bc7009f026e9f7e10ef4" => :el_capitan
-    sha256 "f7f6c0fb4176147dd9d6473411ee4a6a3583d6d5fff4bc7009f026e9f7e10ef4" => :yosemite
+    sha256 "7c15fc59bcde7f29f12b918f2abf62ff5a20675010ba384ba9a257f474c3c714" => :sierra
+    sha256 "7c15fc59bcde7f29f12b918f2abf62ff5a20675010ba384ba9a257f474c3c714" => :el_capitan
+    sha256 "7c15fc59bcde7f29f12b918f2abf62ff5a20675010ba384ba9a257f474c3c714" => :yosemite
   end
 
   option "with-manpage", "build and install the manpage (depends on pandoc)"
