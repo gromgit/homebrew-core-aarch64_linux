8178b83798c65dc21d6c3f5a070d19392b58c63e
ilovezfs <ilovezfs@icloud.com>
2017-03-11 09:00:13 -0800

    glfw@2: add description

--- Formula/glfw@2.rb
+++ Formula/glfw@2.rb
@@ -1,4 +1,5 @@
 class GlfwAT2 < Formula
+  desc "Multi-platform library for OpenGL applications"
   homepage "http://www.glfw.org/"
   url "https://downloads.sourceforge.net/project/glfw/glfw/2.7.9/glfw-2.7.9.tar.bz2"
   sha256 "d1f47e99e4962319f27f30d96571abcb04c1022c000de4d01df69ec59aae829d"
