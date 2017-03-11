b440d0815858589834a949b23c1214b36cf4b9eb
FX Coudert <fxcoudert@gmail.com>
2017-03-11 19:36:48 +0100

    fontconfig: drop universal

--- Formula/fontconfig.rb
+++ Formula/fontconfig.rb
@@ -46,13 +46,10 @@ class Fontconfig < Formula
 
   keg_only :provided_pre_mountain_lion
 
-  option :universal
-
   depends_on "pkg-config" => :build
   depends_on "freetype"
 
   def install
-    ENV.universal_binary if build.universal?
     system "autoreconf", "-iv" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
