ff83760ec969f8175e0e056491949a24fc1656bc
ilovezfs <ilovezfs@icloud.com>
2017-03-11 09:24:21 -0800

    ffmpeg@2.8: use Hardware::CPU.is_32_bit? instead of deprecated Hardware.is_32_bit?

--- Formula/ffmpeg@2.8.rb
+++ Formula/ffmpeg@2.8.rb
@@ -149,7 +149,7 @@ class FfmpegAT28 < Formula
 
     # For 32-bit compilation under gcc 4.2, see:
     # https://trac.macports.org/ticket/20938#comment:22
-    ENV.append_to_cflags "-mdynamic-no-pic" if Hardware.is_32_bit? && Hardware::CPU.intel? && ENV.compiler == :clang
+    ENV.append_to_cflags "-mdynamic-no-pic" if Hardware::CPU.is_32_bit? && Hardware::CPU.intel? && ENV.compiler == :clang
 
     system "./configure", *args
 
