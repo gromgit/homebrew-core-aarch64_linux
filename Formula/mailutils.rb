9d0e83a18dae78e068bfb31e7b99b0ed34af3b60
ilovezfs <ilovezfs@icloud.com>
2017-03-11 09:05:26 -0800

    mailutils 3.2
    use https for homepage
    
    Closes #10901.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/mailutils.rb
+++ Formula/mailutils.rb
@@ -1,10 +1,9 @@
 class Mailutils < Formula
   desc "Swiss Army knife of email handling"
-  homepage "http://mailutils.org/"
-  url "https://ftpmirror.gnu.org/mailutils/mailutils-3.1.1.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/mailutils/mailutils-3.1.1.tar.gz"
-  sha256 "d237622ee1957e8e14cd15713bd8bad710bdb0e408be3de2db12db0b8437049b"
-  revision 1
+  homepage "https://mailutils.org/"
+  url "https://ftpmirror.gnu.org/mailutils/mailutils-3.2.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/mailutils/mailutils-3.2.tar.gz"
+  sha256 "0b977195fed004eaf1cb680883d5adc099c31bbac5d666c58cde82ac8b9b4e01"
 
   bottle do
     sha256 "5fffeefa72bf0af1f6b7d216e363a6723e7651eb0d409450c94f7baed9f882b0" => :sierra
@@ -17,8 +16,6 @@ class Mailutils < Formula
   depends_on "gsasl"
   depends_on "readline"
 
-  patch :DATA
-
   def install
     system "./configure", "--disable-mh",
                           "--prefix=#{prefix}",
@@ -30,17 +27,3 @@ class Mailutils < Formula
     system "#{bin}/movemail", "--version"
   end
 end
-
-__END__
-diff --git a/libmailutils/sockaddr/str.c b/libmailutils/sockaddr/str.c
-index e5bd5a1..6de2647 100644
---- a/libmailutils/sockaddr/str.c
-+++ b/libmailutils/sockaddr/str.c
-@@ -25,6 +25,7 @@
- #include <netinet/in.h>
- #include <arpa/inet.h>
- #include <netdb.h>
-+#include <stdlib.h>
-
- #include <mailutils/sockaddr.h>
- #include <mailutils/errno.h>
