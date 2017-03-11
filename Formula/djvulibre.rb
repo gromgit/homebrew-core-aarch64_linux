2d6a6cbade77a540477f100ab4cbdbb382fca3a6
Viktor Szakats <vszakats@users.noreply.github.com>
2017-03-11 19:44:23 +0100

    djvulibre: use secure head, remove 404 mirror (#10905)

--- Formula/djvulibre.rb
+++ Formula/djvulibre.rb
@@ -2,7 +2,6 @@ class Djvulibre < Formula
   desc "DjVu viewer"
   homepage "https://djvu.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/djvu/DjVuLibre/3.5.27/djvulibre-3.5.27.tar.gz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/d/djvulibre/djvulibre_3.5.27.orig.tar.gz"
   sha256 "e69668252565603875fb88500cde02bf93d12d48a3884e472696c896e81f505f"
 
   bottle do
@@ -14,7 +13,7 @@ class Djvulibre < Formula
   end
 
   head do
-    url "git://git.code.sf.net/p/djvu/djvulibre-git"
+    url "https://git.code.sf.net/p/djvu/djvulibre-git.git"
     depends_on "automake" => :build
     depends_on "autoconf" => :build
     depends_on "libtool" => :build
