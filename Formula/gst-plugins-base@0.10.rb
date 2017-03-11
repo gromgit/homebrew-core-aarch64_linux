f2ff7bae2d7f4694e1cc186ce778dab85e981aa3
ilovezfs <ilovezfs@icloud.com>
2017-03-11 08:48:04 -0800

    gst-plugins-base@0.10: add a test

--- Formula/gst-plugins-base@0.10.rb
+++ Formula/gst-plugins-base@0.10.rb
@@ -40,4 +40,10 @@ class GstPluginsBaseAT010 < Formula
     system "make"
     system "make", "install"
   end
+
+  test do
+    gst = Formula["gstreamer@0.10"].opt_bin/"gst-inspect-0.10"
+    output = shell_output("#{gst} --plugin volume")
+    assert_match version.to_s, output
+  end
 end
