c43558bd86e8254541102d686d285f7adff625c3
ilovezfs <ilovezfs@icloud.com>
2017-03-11 02:35:57 -0800

    pcap_dnsproxy 0.4.8.4
    Closes #10885.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pcap_dnsproxy.rb
+++ Formula/pcap_dnsproxy.rb
@@ -1,8 +1,8 @@
 class PcapDnsproxy < Formula
   desc "Powerful DNS proxy designed to anti DNS spoofing"
   homepage "https://github.com/chengr28/Pcap_DNSProxy"
-  url "https://github.com/chengr28/Pcap_DNSProxy/archive/v0.4.7.8.tar.gz"
-  sha256 "57af22d8688cd23e74e5eee0b716ea0c8e68671d3be34ef993df7b8303aa73d0"
+  url "https://github.com/chengr28/Pcap_DNSProxy/archive/v0.4.8.4.tar.gz"
+  sha256 "a15ee4c00fe4863aaf54bdeec71de2739a5ea1ab4f8e14ca0c6712ba3ed5695c"
   head "https://github.com/chengr28/Pcap_DNSProxy.git"
 
   bottle do
@@ -16,13 +16,16 @@ class PcapDnsproxy < Formula
   depends_on :macos => :yosemite
   depends_on :xcode => :build
   depends_on "libsodium"
+  depends_on "openssl@1.1"
 
   def install
-    (buildpath/"Source/LibSodium").install_symlink Formula["libsodium"].opt_lib/"libsodium.a" => "LibSodium_Mac.a"
+    (buildpath/"Source/Dependency/LibSodium").install_symlink Formula["libsodium"].opt_lib/"libsodium.a" => "LibSodium_macOS.a"
+    (buildpath/"Source/Dependency/OpenSSL").install_symlink Formula["openssl@1.1"].opt_lib/"libssl.a" => "LibSSL_macOS.a"
+    (buildpath/"Source/Dependency/OpenSSL").install_symlink Formula["openssl@1.1"].opt_lib/"libcrypto.a" => "LibCrypto_macOS.a"
     xcodebuild "-project", "./Source/Pcap_DNSProxy.xcodeproj", "-target", "Pcap_DNSProxy", "-configuration", "Release", "SYMROOT=build"
     bin.install "Source/build/Release/Pcap_DNSProxy"
-    (etc/"pcap_DNSproxy").install Dir["Source/ExampleConfig/*.{ini,txt}"]
-    prefix.install "Source/ExampleConfig/pcap_dnsproxy.service.plist"
+    (etc/"pcap_DNSproxy").install Dir["Source/Auxiliary/ExampleConfig/*.{ini,txt}"]
+    prefix.install "Source/Auxiliary/ExampleConfig/pcap_dnsproxy.service.plist"
   end
 
   plist_options :startup => true, :manual => "sudo #{HOMEBREW_PREFIX}/opt/pcap_dnsproxy/bin/Pcap_DNSProxy -c #{HOMEBREW_PREFIX}/etc/pcap_DNSproxy/"
