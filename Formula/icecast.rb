750d8cc427c03783126722ea1cf7d3f8ed6acfc9
Tomasz Pajor <tomek@polishgeeks.com>
2017-03-11 12:18:12 +0100

    icecast 2.4.3
    Closes #10894.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/icecast.rb
+++ Formula/icecast.rb
@@ -1,8 +1,8 @@
 class Icecast < Formula
   desc "Streaming MP3 audio server"
   homepage "http://www.icecast.org/"
-  url "http://downloads.xiph.org/releases/icecast/icecast-2.4.2.tar.gz"
-  sha256 "aa1ae2fa364454ccec61a9247949d19959cb0ce1b044a79151bf8657fd673f4f"
+  url "http://downloads.xiph.org/releases/icecast/icecast-2.4.3.tar.gz"
+  sha256 "c85ca48c765d61007573ee1406a797ae6cb31fb5961a42e7f1c87adb45ddc592"
 
   bottle do
     cellar :any
