8705ddf3ded4899f671ab184756f6f50d60ad390
ilovezfs <ilovezfs@icloud.com>
2017-03-12 08:17:10 -0700

    fbida: use https for homepage and stable url

--- Formula/fbida.rb
+++ Formula/fbida.rb
@@ -1,7 +1,7 @@
 class Fbida < Formula
   desc "View and edit photo images"
-  homepage "http://linux.bytesex.org/fbida/"
-  url "http://dl.bytesex.org/releases/fbida/fbida-2.10.tar.gz"
+  homepage "https://linux.bytesex.org/fbida/"
+  url "https://dl.bytesex.org/releases/fbida/fbida-2.10.tar.gz"
   sha256 "7a5a3aac61b40a6a2bbf716d270a46e2f8e8d5c97e314e927d41398a4d0b6cb6"
 
   bottle do
