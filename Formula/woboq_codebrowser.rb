8d3a64d2fbdf00dd147632a0882b10ba42a85259
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-12 22:06:54 +0000

    woboq_codebrowser: add 2.0.1 bottle.

--- Formula/woboq_codebrowser.rb
+++ Formula/woboq_codebrowser.rb
@@ -4,6 +4,12 @@ class WoboqCodebrowser < Formula
   url "https://github.com/woboq/woboq_codebrowser/archive/2.0.1.tar.gz"
   sha256 "f2ec67bb49415986a4bbef515024009433756c83e14fa84f5acc40c83eec19e6"
 
+  bottle do
+    sha256 "61b8336b7118c845ff09ea2d8733a741fe5bf1e08469d474c62ddea3245519e8" => :sierra
+    sha256 "503bfd97e11ae244521a501487f4a4d0b48c512550ced9cb29fc5e4fcb355eb7" => :el_capitan
+    sha256 "ceec3aed2f4ccf00b27d2676113dde02cc131251745abbfc039e37a8391c52eb" => :yosemite
+  end
+
   depends_on "llvm"
   depends_on "cmake" => :build
 
