f9dd468e224713aba5f95c66de8575ef9ba10292
Waldir Pimenta <waldyrious@gmail.com>
2017-03-12 23:46:13 +0000

    dcmtk: add dicomdict and openssl options (#10805)

--- Formula/dcmtk.rb
+++ Formula/dcmtk.rb
@@ -4,6 +4,7 @@ class Dcmtk < Formula
 
   # Current snapshot used for stable now (using - instead of _ in the version field).
   url "http://dicom.offis.de/download/dcmtk/snapshot/dcmtk-3.6.1_20170228.tar.gz"
+  mirror "http://dicom.offis.de/download/dcmtk/snapshot/old/dcmtk-3.6.1_20170228.tar.gz"
   version "3.6.1-20170228"
   sha256 "8de2f2ae70f71455288ec85c96a2579391300c7462f69a4a6398e9ec51779c11"
 
@@ -17,18 +18,20 @@ class Dcmtk < Formula
 
   option "with-docs", "Install development libraries/headers and HTML docs"
   option "with-libiconv", "Build with brewed libiconv. Dcmtk and system libiconv can have problems with utf8."
+  option "with-dicomdict", "Build with baked-in DICOM data dictionary."
 
   depends_on "cmake" => :build
   depends_on "doxygen" => :build if build.with? "docs"
   depends_on "libpng"
   depends_on "libtiff"
-  depends_on "openssl"
+  depends_on "openssl" => :recommended
   depends_on "homebrew/dupes/libiconv" => :optional
 
   def install
     args = std_cmake_args
-    args << "-DDCMTK_WITH_OPENSSL=YES"
+    args << "-DDCMTK_WITH_OPENSSL=NO" if build.without? "openssl"
     args << "-DDCMTK_WITH_DOXYGEN=YES" if build.with? "docs"
+    args << "-DDCMTK_ENABLE_BUILTIN_DICTIONARY=YES -DDCMTK_ENABLE_EXTERNAL_DICTIONARY=NO" if build.with? "dicomdict"
     args << "-DDCMTK_WITH_ICONV=YES -DLIBICONV_DIR=#{Formula["libiconv"].opt_prefix}" if build.with? "libiconv"
     args << ".."
 
