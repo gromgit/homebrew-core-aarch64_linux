0c72d794bfe7c3c79ce8de1eb2f28618c25eeaa1
Viktor Szakats <vszakats@users.noreply.github.com>
2017-03-12 17:01:46 +0000

    xu4: use secure head and stable urls

--- Formula/xu4.rb
+++ Formula/xu4.rb
@@ -1,9 +1,13 @@
 class Xu4 < Formula
   desc "Remake of Ultima IV"
   homepage "https://xu4.sourceforge.io/"
-  url "http://xu4.svn.sourceforge.net/svnroot/xu4/trunk/u4", :revision => "3088"
+  if MacOS.version >= :sierra
+    url "https://svn.code.sf.net/p/xu4/code/trunk/u4", :revision => "3088"
+  else
+    url "http://svn.code.sf.net/p/xu4/code/trunk/u4", :revision => "3088"
+  end
   version "1.0beta4+r3088"
-  head "http://xu4.svn.sourceforge.net/svnroot/xu4/trunk/u4"
+  head "https://svn.code.sf.net/p/xu4/code/trunk/u4"
 
   bottle do
     cellar :any
