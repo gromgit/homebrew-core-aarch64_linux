8c0bc0bb7ca37c30cf533b7fae12eb6ce757ad2f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-12 14:52:34 +0000

    docker@1.71: add 1.7.1 bottle.

--- Formula/docker@1.71.rb
+++ Formula/docker@1.71.rb
@@ -5,6 +5,13 @@ class DockerAT171 < Formula
       :tag => "v1.7.1",
       :revision => "786b29d4db80a6175e72b47a794ee044918ba734"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1b0c6d26c4242e328f82dfb00d3cd788fbcf58986d71ff62647f1459e9136dbd" => :sierra
+    sha256 "c826383171425a87bb42fa0a83f590465a79f76fb8992b970efafb04cc0565a2" => :el_capitan
+    sha256 "e84c670e21f03563bbff047bff3a6925933f094015cb51ad2c461183c32f19d3" => :yosemite
+  end
+
   keg_only :versioned_formula
 
   option "without-completions", "Disable bash/zsh completions"
