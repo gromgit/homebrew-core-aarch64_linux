7bc12772fc1efe30710791975c1954957a185280
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-12 14:52:33 +0000

    bazel@0.2: add 0.2.3 bottle.

--- Formula/bazel@0.2.rb
+++ Formula/bazel@0.2.rb
@@ -5,6 +5,13 @@ class BazelAT02 < Formula
   sha256 "37fd2d49c57df171b704bf82c94e7bf954d94748e2a8621c5456c5c9d5f2c845"
   head "https://github.com/bazelbuild/bazel.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3d24665d4e81d9b647bc68a55c8692a2a74db2269c602306aec4c371534180ae" => :sierra
+    sha256 "9fedb09a8bf76ff6d447f60ff0c6f3e4e904c38b501b42030ec1c9eb68f7851b" => :el_capitan
+    sha256 "06fda3195453ef6a2d98adcfbafb28f186dc690697e7855fdb029fab2fadae0f" => :yosemite
+  end
+
   keg_only :versioned_formula
 
   depends_on :java => "1.8+"
