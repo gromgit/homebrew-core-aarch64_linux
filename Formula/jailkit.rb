19d09ab9b2c227a66d14e1d03d78f554279a723d
ilovezfs <ilovezfs@icloud.com>
2017-03-12 23:29:58 -0700

    jailkit: use https for homepage and stable url

--- Formula/jailkit.rb
+++ Formula/jailkit.rb
@@ -1,7 +1,7 @@
 class Jailkit < Formula
   desc "Utilities to create limited user accounts in a chroot jail"
-  homepage "http://olivier.sessink.nl/jailkit/"
-  url "http://olivier.sessink.nl/jailkit/jailkit-2.19.tar.bz2"
+  homepage "https://olivier.sessink.nl/jailkit/"
+  url "https://olivier.sessink.nl/jailkit/jailkit-2.19.tar.bz2"
   sha256 "bebbf6317a5a15057194dd2cf6201821c48c022dbc64c12756eb13b61eff9bf9"
 
   bottle do
