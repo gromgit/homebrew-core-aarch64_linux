f24ae49a2dc1316ce08b97037fea63c2c7658928
ilovezfs <ilovezfs@icloud.com>
2017-03-13 08:07:41 -0700

    gst-plugins-base@0.10: make not keg_only

--- Formula/gst-plugins-base@0.10.rb
+++ Formula/gst-plugins-base@0.10.rb
@@ -4,8 +4,6 @@ class GstPluginsBaseAT010 < Formula
   url "https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-0.10.36.tar.bz2"
   sha256 "2cd3b0fa8e9b595db8f514ef7c2bdbcd639a0d63d154c00f8c9b609321f49976"
 
-  keg_only :versioned_formula
-
   depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "gstreamer@0.10"
