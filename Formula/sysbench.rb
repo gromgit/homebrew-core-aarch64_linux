fe2a0a7f0e70004558d7c2c25f380caf31eee2df
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-13 14:08:08 +0000

    sysbench: update 1.0.4 bottle.

--- Formula/sysbench.rb
+++ Formula/sysbench.rb
@@ -5,9 +5,9 @@ class Sysbench < Formula
   sha256 "fa8dbc56ddfd095b09b977c321f7ebbb12818cfc872542a0bffa195cab0716a1"
 
   bottle do
-    sha256 "984c166d3089dcd46fde18a2d0058d157619cae4de20bfcd1ae9645e675f441a" => :sierra
-    sha256 "01e4a62c33dd7fa015dbe825c8416eb24c6aae988eb5459a68fc3675a9b52be1" => :el_capitan
-    sha256 "ce857222f35a6f76b34ea5db987816ae68b317daa112d9bb2fd958ac44b9304d" => :yosemite
+    sha256 "404b5a31bff80d60da02b8c3aee5a08f2968c553f72342095d64e1632c993174" => :sierra
+    sha256 "fcd4b5ac02067a797613119e19eb1d50f5295d3597607169caa6a6f142c417bd" => :el_capitan
+    sha256 "a253720912f37bb8354f4202f1d8a2979dedb43d70adf377d1f270d31dd05ad9" => :yosemite
   end
 
   depends_on "autoconf" => :build
