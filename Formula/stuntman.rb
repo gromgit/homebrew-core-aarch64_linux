a0b7f165d6b76ce1a37cc510ff4e89f283c18c0b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-13 14:07:29 +0000

    stuntman: update 1.2.13 bottle.

--- Formula/stuntman.rb
+++ Formula/stuntman.rb
@@ -6,10 +6,10 @@ class Stuntman < Formula
   head "https://github.com/jselbie/stunserver.git"
 
   bottle do
-    cellar :any
-    sha256 "f3a17b16bfd32c4a888500edf6dd5db05e9ddbb72d8767ae86dfd11d5916c8c5" => :sierra
-    sha256 "f25bb4553534d83eeefeffcd0af509f8d16dbd12480102154d9b7da8bb5b3828" => :el_capitan
-    sha256 "51ccb9f6ef78dad5125b5d10c1d6943a5d04761e693ebe2a06e2c4f7da080d57" => :yosemite
+    cellar :any_skip_relocation
+    sha256 "1d16a7d994f57dbac8b7d2496ac3346f9c753f3d09ec0760b213cabaf92ab403" => :sierra
+    sha256 "2d3187c4be196200c5007849e56abbd2ea10f30567977feda55455897a5b6061" => :el_capitan
+    sha256 "4f1011c0cdd34060150d50889a0ed100ee449c5ea5621c4fee8262741b1816af" => :yosemite
   end
 
   depends_on "boost" => :build
