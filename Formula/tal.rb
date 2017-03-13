c328867c0d8017dccbb40d39a3a5ed68ab4330e0
Alexander Hamann <neutric@users.noreply.github.com>
2017-03-13 09:51:54 +0100

    tal: use https for homepage and url

--- Formula/tal.rb
+++ Formula/tal.rb
@@ -1,7 +1,7 @@
 class Tal < Formula
   desc "Align line endings if they match"
-  homepage "http://thomasjensen.com/software/tal/"
-  url "http://thomasjensen.com/software/tal/tal-1.9.tar.gz"
+  homepage "https://thomasjensen.com/software/tal/"
+  url "https://thomasjensen.com/software/tal/tal-1.9.tar.gz"
   sha256 "5d450cee7162c6939811bca945eb475e771efe5bd6a08b520661d91a6165bb4c"
 
   bottle do
