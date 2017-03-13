7abc34afdcfa62d1a211b95b1458c222d60d1e0e
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-03-13 09:34:35 +0000

    gxml 0.14.0
    Closes #11022.
    
    Signed-off-by: Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>

--- Formula/gxml.rb
+++ Formula/gxml.rb
@@ -1,8 +1,8 @@
 class Gxml < Formula
   desc "GObject-based XML DOM API"
   homepage "https://wiki.gnome.org/GXml"
-  url "https://download.gnome.org/sources/gxml/0.12/gxml-0.12.0.tar.xz"
-  sha256 "f9310d81078df031c8a5defc90be814a4666e641fb242cd3f4b6f3b6706fb44d"
+  url "https://download.gnome.org/sources/gxml/0.14/gxml-0.14.0.tar.xz"
+  sha256 "3e1f28ba6fc06b5c96a57c1f099ad3bc21683c54bb3b5e5bc4d7ceaff7c74066"
 
   bottle do
     sha256 "bfa5e9b306172cfebff6f082211097e4dd58d2ff38e539325b5bb2eb4f64e902" => :sierra
@@ -13,12 +13,26 @@ class Gxml < Formula
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
   depends_on "vala" => :build
+  # remove next three lines when next release is out
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "libxml2"
   depends_on "glib"
   depends_on "libgee"
   depends_on "gobject-introspection"
 
+  # see https://bugzilla.gnome.org/show_bug.cgi?id=779836
+  # should be fixed in next version
+  patch do
+    url "https://git.gnome.org/browse/gxml/patch/?id=0829fd0b83941efe85ec45e6c4900d76ca0c5b29"
+    sha256 "984b9a3e5a7cfa125b3b0b5e479d1f185005c1c90612f809e320a4d0617bb8a9"
+  end
+
   def install
+    # remove when next release is out
+    system "autoreconf", "-i"
+
     # ensures that the gobject-introspection files remain within the keg
     inreplace "gxml/Makefile.in" do |s|
       s.gsub! "@HAVE_INTROSPECTION_TRUE@girdir = $(INTROSPECTION_GIRDIR)",
@@ -57,7 +71,7 @@ class Gxml < Formula
       -I#{libxml2.opt_include}/libxml2
       -I#{glib.opt_include}/glib-2.0
       -I#{glib.opt_lib}/glib-2.0/include
-      -I#{include}/gxml-0.12
+      -I#{include}/gxml-0.14
       -I#{libgee.opt_include}/gee-0.8
       -D_REENTRANT
       -L#{gettext.opt_lib}
@@ -69,7 +83,7 @@ class Gxml < Formula
       -lgio-2.0
       -lglib-2.0
       -lgobject-2.0
-      -lgxml-0.12
+      -lgxml-0.14
       -lintl
       -lxml2
     ]
