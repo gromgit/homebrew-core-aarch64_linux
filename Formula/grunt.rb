ade1b3a7555c25183aea9ba88054f50d04f266d0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-13 06:19:18 +0000

    grunt: add 1.2.0 bottle.

--- Formula/grunt.rb
+++ Formula/grunt.rb
@@ -6,6 +6,13 @@ class Grunt < Formula
   url "https://registry.npmjs.org/grunt-cli/-/grunt-cli-1.2.0.tgz"
   sha256 "fdb1d4bd83435b3f70614b608e0027a0d75ebfda151396bb99c46405334a01d8"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "dc90514c838978579131c9fdfd783a4f1efe0abde9f9b86ca6052cbb2b904267" => :sierra
+    sha256 "b6fc675be3657f7f4ba8027ae685937bffdcdfe2bdcece551ae0397cfc9dfc95" => :el_capitan
+    sha256 "2a20ca00cb57a0254d1c728e4ead10a132a8ede8dab0338aa042546a89380842" => :yosemite
+  end
+
   depends_on "node"
 
   def install
