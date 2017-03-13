9d4e43408c50769e8663b9e77a8b25b85dea6aa1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-13 19:07:52 +0000

    gst-plugins-base@0.10: add 0.10.36 bottle.

--- Formula/gst-plugins-base@0.10.rb
+++ Formula/gst-plugins-base@0.10.rb
@@ -4,6 +4,12 @@ class GstPluginsBaseAT010 < Formula
   url "https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-0.10.36.tar.bz2"
   sha256 "2cd3b0fa8e9b595db8f514ef7c2bdbcd639a0d63d154c00f8c9b609321f49976"
 
+  bottle do
+    sha256 "5ad2f68ff53c006d92db4543f9ed90dd7cd95ddb48bf99bae2a7718db76bcd91" => :sierra
+    sha256 "1c52815b6407f35e967dc7c2c9885f43bd290342f0e16463596a9e57e10f36b6" => :el_capitan
+    sha256 "795d24de05759adaf3b54ced0d8aa81f77a8eb8b3652a7c156fa2c5c9b2de7c9" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "gstreamer@0.10"
