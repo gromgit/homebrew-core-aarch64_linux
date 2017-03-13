885b22da8404a71f35fde95bbe623a056787e16a
Miguel Araújo <miguelarauj1o@gmail.com>
2017-03-13 03:38:18 -0300

    python3: `revision` should be put before `head` (#10979)

--- Formula/python3.rb
+++ Formula/python3.rb
@@ -3,8 +3,8 @@ class Python3 < Formula
   homepage "https://www.python.org/"
   url "https://www.python.org/ftp/python/3.6.0/Python-3.6.0.tar.xz"
   sha256 "b0c5f904f685e32d9232f7bdcbece9819a892929063b6e385414ad2dd6a23622"
-  head "https://github.com/python/cpython", :using => :git
   revision 1
+  head "https://github.com/python/cpython", :using => :git
 
   bottle do
     sha256 "e8b177926ce5e110d7cf9f03f9d11c60b08ad74ccee07bc72fe38ac981bf0cef" => :sierra
