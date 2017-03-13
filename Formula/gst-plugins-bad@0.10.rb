1e13050b15e62ce763faba27b5cd98add57a1d2a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-13 19:07:51 +0000

    gst-plugins-bad@0.10: add 0.10.23_1 bottle.

--- Formula/gst-plugins-bad@0.10.rb
+++ Formula/gst-plugins-bad@0.10.rb
@@ -5,6 +5,12 @@ class GstPluginsBadAT010 < Formula
   sha256 "0eae7d1a1357ae8377fded6a1b42e663887beabe0e6cc336e2ef9ada42e11491"
   revision 1
 
+  bottle do
+    sha256 "5b2ee005df419f9b9397fd5b618f4a97f0d2b1af60404b5cc7d15f3c7d02c243" => :sierra
+    sha256 "51c6fccf38e4825c1615207c7290c5a7712cbc68282914a3966589e663f1e6fc" => :el_capitan
+    sha256 "5d8685e6aea37bad4a76b40dfff1772daf53289e3b985e43251022d0c70b2ca8" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "gst-plugins-base@0.10"
