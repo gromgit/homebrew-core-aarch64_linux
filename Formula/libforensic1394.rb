6e62d4d2ce558fbe31e2ad0244a84e684b4bac21
ilovezfs <ilovezfs@icloud.com>
2017-03-13 01:52:21 -0700

    libforensic1394: update head url

--- Formula/libforensic1394.rb
+++ Formula/libforensic1394.rb
@@ -3,7 +3,7 @@ class Libforensic1394 < Formula
   homepage "https://freddie.witherden.org/tools/libforensic1394/"
   url "https://freddie.witherden.org/tools/libforensic1394/releases/libforensic1394-0.2.tar.gz"
   sha256 "50a82fe2899aa901104055da2ac00b4c438cf1d0d991f5ec1215d4658414652e"
-  head "git://git.freddie.witherden.org/forensic1394.git"
+  head "https://github.com/FreddieWitherden/libforensic1394.git"
 
   bottle do
     cellar :any
