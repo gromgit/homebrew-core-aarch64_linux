5f39bd6b76b25c86a2f4f242b68c1afc29ac6db5
ilovezfs <ilovezfs@icloud.com>
2017-03-14 10:30:41 -0700

    guile 2.1.8 (devel) (#11075)

--- Formula/guile.rb
+++ Formula/guile.rb
@@ -35,16 +35,12 @@ class Guile < Formula
 
   devel do
     url "https://git.savannah.gnu.org/git/guile.git",
-        :tag => "v2.1.7",
-        :revision => "c58c143f31fe4c1717fc8846a8681de2bb4b3869"
+        :tag => "v2.1.8",
+        :revision => "e3374320415df973a6d8b0e1065b5b74e9e3e5e0"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "gettext" => :build
-
-    # Avoid undeclared identifier errors for SOCK_CLOEXEC and SOCK_NONBLOCK
-    # Reported 19 Feb 2017 https://debbugs.gnu.org/cgi/bugreport.cgi?bug=25790
-    patch :DATA
   end
 
   head do
@@ -69,14 +65,7 @@ class Guile < Formula
   end
 
   def install
-    unless build.stable?
-      # Avoid "address argument to atomic operation must be a pointer to _Atomic type"
-      # Reported 19 Feb 2017 https://debbugs.gnu.org/cgi/bugreport.cgi?bug=25791
-      ENV["ac_cv_header_stdatomic_h"] = "no"
-
-      system "./autogen.sh"
-    end
-
+    system "./autogen.sh" unless build.stable?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-libreadline-prefix=#{Formula["readline"].opt_prefix}",
@@ -103,21 +92,3 @@ class Guile < Formula
     system bin/"guile", hello
   end
 end
-
-__END__
-diff --git a/libguile/socket.c b/libguile/socket.c
-index 64df64f..446243c 100644
---- a/libguile/socket.c
-+++ b/libguile/socket.c
-@@ -1655,8 +1655,12 @@ scm_init_socket ()
- 
-   /* accept4 flags.  No ifdef as accept4 has a gnulib
-      implementation.  */
-+#ifdef SOCK_CLOEXEC
-   scm_c_define ("SOCK_CLOEXEC", scm_from_int (SOCK_CLOEXEC));
-+#endif
-+#ifdef SOCK_NONBLOCK
-   scm_c_define ("SOCK_NONBLOCK", scm_from_int (SOCK_NONBLOCK));
-+#endif
- 
-   /* setsockopt level.
