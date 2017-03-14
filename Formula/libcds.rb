c8ee6b8689146ad00c30b97c15858749ef2604e7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-14 12:25:51 +0000

    libcds: add 2.2.0 bottle.

--- Formula/libcds.rb
+++ Formula/libcds.rb
@@ -4,6 +4,13 @@ class Libcds < Formula
   url "https://github.com/khizmax/libcds/archive/v2.2.0.tar.gz"
   sha256 "e582fbd6492c91ff24b11468f094b111d5f62eca69e6445c88bf16cad50e40ed"
 
+  bottle do
+    cellar :any
+    sha256 "17e4584663eaf891444b4feef6932917eac10c4d0f3cee58cc48cd6dc2af8415" => :sierra
+    sha256 "7c444cf3570fb54e1647ba9235066f9d7f6a35b97b9a2cec6397faf2d8e9555b" => :el_capitan
+    sha256 "35498f70f6f1f52df20dd1c1759168410af73724184c16ba3c4a2de9fd6dc78a" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "boost"
   if DevelopmentTools.clang_build_version < 800
