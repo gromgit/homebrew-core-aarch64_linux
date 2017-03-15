043b9a3737e630f2b661b8dd0cca2d78409f7de7
Tomasz Pajor <tomek@polishgeeks.com>
2017-03-15 10:18:14 +0100

    lftp 4.7.7
    Closes #11105.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/lftp.rb
+++ Formula/lftp.rb
@@ -1,9 +1,9 @@
 class Lftp < Formula
   desc "Sophisticated file transfer program"
   homepage "https://lftp.tech"
-  url "https://lftp.yar.ru/ftp/lftp-4.7.6.tar.bz2"
-  mirror "ftp://ftp.st.ryukoku.ac.jp/pub/network/ftp/lftp/lftp-4.7.6.tar.bz2"
-  sha256 "6b46389e9c2e67af9029a783806facea4c8f0b4d6adca5c1088e948d2fd68ae7"
+  url "https://lftp.yar.ru/ftp/lftp-4.7.7.tar.bz2"
+  mirror "ftp://ftp.st.ryukoku.ac.jp/pub/network/ftp/lftp/lftp-4.7.7.tar.bz2"
+  sha256 "fe441f20a9a317cfb99a8b8e628ba0457df472b6d93964d17374d5b5ebdf9280"
 
   bottle do
     sha256 "ce6de0c8cad4501507b47457b9fcaa08048f82db0ed4ea6ad42a5901f6aecffe" => :sierra
