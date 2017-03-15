a659c1a714071070dc16a431cb5aa7c3ef1e4a08
ilovezfs <ilovezfs@icloud.com>
2017-03-15 01:27:00 -0700

    dirmngr: make keg_only

--- Formula/dirmngr.rb
+++ Formula/dirmngr.rb
@@ -4,7 +4,7 @@ class Dirmngr < Formula
   url "https://gnupg.org/ftp/gcrypt/dirmngr/dirmngr-1.1.1.tar.bz2"
   mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/dirmngr/dirmngr-1.1.1.tar.bz2"
   sha256 "d2280b8c314db80cdaf101211a47826734443436f5c3545cc1b614c50eaae6ff"
-  revision 2
+  revision 3
 
   bottle do
     sha256 "13f12a1797e6f7fe61ee049fdec8043b09f56c77dbc0537f436d0fa19cefe77a" => :sierra
@@ -12,6 +12,8 @@ class Dirmngr < Formula
     sha256 "ddc0de1dff6015fd5072c2da6e0173be8fedf29db27edd03c33a3275b842e402" => :yosemite
     sha256 "47fe29be8ca19eeb4d4a3e3434cd35ef7b13e1c1a9e8696f5ebd4434dc8cc062" => :mavericks
   end
+
+  keg_only "GPG 2.1.x ships an internal dirmngr which it must use."
 
   depends_on "libassuan"
   depends_on "libgpg-error"
