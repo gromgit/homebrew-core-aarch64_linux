85cdd581684decb04b75cdf6b9335fc5f8b1757f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-15 10:13:26 +0000

    lftp: update 4.7.7 bottle.

--- Formula/lftp.rb
+++ Formula/lftp.rb
@@ -6,9 +6,9 @@ class Lftp < Formula
   sha256 "fe441f20a9a317cfb99a8b8e628ba0457df472b6d93964d17374d5b5ebdf9280"
 
   bottle do
-    sha256 "ce6de0c8cad4501507b47457b9fcaa08048f82db0ed4ea6ad42a5901f6aecffe" => :sierra
-    sha256 "ea3c52aa05979ca8e68ca44dd94d61c6f91a040df90fb64ddc52e5f92c2c7a95" => :el_capitan
-    sha256 "049f0d2fb6a75d4ae7d4a9b02242cc286307d11b23793793879a2644263d7268" => :yosemite
+    sha256 "d27973d0a87c6cb2362be22562ae8082f521c68895a394294219aa2812409d32" => :sierra
+    sha256 "cba2c456d098e8d7db989207ba27d383e602b88de78fe25c186932e5be18f6e6" => :el_capitan
+    sha256 "5ba5ec09928d5314ec91ce77825b3415f8e323e8812889cb6839d30304fb608e" => :yosemite
   end
 
   depends_on "readline"
