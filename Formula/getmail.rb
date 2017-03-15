ce190a3bb5b4730ed94c737b89f8d3c9a80c08cf
Tomasz Pajor <tomek@polishgeeks.com>
2017-03-15 10:35:33 +0100

    getmail 4.54.0
    Closes #11109.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/getmail.rb
+++ Formula/getmail.rb
@@ -1,8 +1,8 @@
 class Getmail < Formula
   desc "Extensible mail retrieval system with POP3, IMAP4, SSL support"
   homepage "http://pyropus.ca/software/getmail/"
-  url "http://pyropus.ca/software/getmail/old-versions/getmail-4.49.0.tar.gz"
-  sha256 "1c6c4a3ebd17c1b15dc940a5888b0ceca4d135c1c20629ebe274ba025aff1ed4"
+  url "http://pyropus.ca/software/getmail/old-versions/getmail-4.54.0.tar.gz"
+  sha256 "d45657945353c68785b106dd9c5fae6bc2cec8f99fbb202d9dddd4967f483a65"
 
   bottle do
     cellar :any_skip_relocation
@@ -10,12 +10,6 @@ class Getmail < Formula
     sha256 "37f222a5229a89983b0238748ea2828df9ac08eb87b0260feda54603fba572a0" => :el_capitan
     sha256 "8123f6f1a331902fab3ab427fad3e2c8f00277e54a8bfdcc081ce1053ae9ad1e" => :yosemite
     sha256 "db0a01b3ed8ecd7c8cd30fd929713d9ac3d2b24e18369dd526703b79e5c6492e" => :mavericks
-  end
-
-  # See: https://github.com/Homebrew/homebrew/pull/28739
-  patch do
-    url "https://gist.githubusercontent.com/sigma/11295734/raw/5a7f39d600fc20d7605d3c9e438257285700b32b/ssl_timeout.patch"
-    sha256 "cd5efe16c848c14b8db91780bf4e08a5920f6576cc68628b0941aa81857f4e2f"
   end
 
   def install
