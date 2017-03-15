fba78f1e1949507bb7404e8d54be93887f3e5b09
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-15 10:35:52 +0000

    calcurse: update 4.2.2 bottle.

--- Formula/calcurse.rb
+++ Formula/calcurse.rb
@@ -5,10 +5,9 @@ class Calcurse < Formula
   sha256 "c6881ddbd1cc7fbd02898187ac0fb4c6d8ac4c2715909b1cf00fb7a90cf08046"
 
   bottle do
-    sha256 "efc4322d5103b6a2f8f93f5d7556459854245330bd3beaca923757502ff9733e" => :sierra
-    sha256 "98598389661cd000b63a44a95bffd813883c52cc75564f4420ce37548f5340b8" => :el_capitan
-    sha256 "037998bbb6d0c6393fe91fec579ca6d951021c4706e001cc3d79eb7287153ec9" => :yosemite
-    sha256 "d8d136a8d35d2c41bebf2180ca6a8a45712b40e17109ba58a197601548ad3d1c" => :mavericks
+    sha256 "375463064c0e4aefd6f63225fb75f218595ee075213f105b3ee823751aa9fe75" => :sierra
+    sha256 "83948ec7e431450ac3141b4a4ab7fa9c1699763e532146696d79d9a9419e3d2d" => :el_capitan
+    sha256 "bb97afde65f1066d39f8471e0f53f2cefc694eda3693ecb8c1e1b65f5197aec5" => :yosemite
   end
 
   depends_on "gettext"
