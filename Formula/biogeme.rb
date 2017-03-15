633c3733edee0280e29b6d745d7ff2e77723bf86
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-15 09:00:58 +0000

    biogeme: add 2.5 bottle.

--- Formula/biogeme.rb
+++ Formula/biogeme.rb
@@ -4,6 +4,12 @@ class Biogeme < Formula
   url "http://biogeme.epfl.ch/distrib/biogeme-2.5.tar.gz"
   sha256 "88548e99f4f83c24bf7ddb8e0de07588adc2bec515569c56e816ed5b20a624b3"
 
+  bottle do
+    sha256 "57b8dc37a3f62f8158019ba7ed16955d84603e9d5544bf8ec43ca1b7f2c8ff5b" => :sierra
+    sha256 "747a09308ea2475fc2d1e4823a352c1f5c10fadab1d88bf7250cd78c59a326ee" => :el_capitan
+    sha256 "2f15fe2bc8ccf24889fdb13eba6f8e9299e55a277fa06417dc556802bb40e385" => :yosemite
+  end
+
   depends_on :python3
   depends_on "gtkmm3"
 
