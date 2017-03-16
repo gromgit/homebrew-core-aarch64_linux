ee18e522ff4b50ec4fc315af1b5bbc3a0e28f50b
Miguel Araújo <miguelarauj1o@gmail.com>
2017-03-16 12:51:20 -0300

    sloccount: update link to HTTPS

--- Formula/sloccount.rb
+++ Formula/sloccount.rb
@@ -1,7 +1,7 @@
 class Sloccount < Formula
   desc "Count lines of code in many languages"
-  homepage "http://www.dwheeler.com/sloccount/"
-  url "http://www.dwheeler.com/sloccount/sloccount-2.26.tar.gz"
+  homepage "https://www.dwheeler.com/sloccount/"
+  url "https://www.dwheeler.com/sloccount/sloccount-2.26.tar.gz"
   sha256 "fa7fa2bbf2f627dd2d0fdb958bd8ec4527231254c120a8b4322405d8a4e3d12b"
 
   bottle do
