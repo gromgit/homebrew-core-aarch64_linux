fa054d9b044c645b8a09e11e5b8054db4ae9fb04
Viktor Szakats <vszakats@users.noreply.github.com>
2017-03-16 22:58:08 +0100

    fondu: https url

--- Formula/fondu.rb
+++ Formula/fondu.rb
@@ -1,7 +1,7 @@
 class Fondu < Formula
   desc "Tools to convert between different font formats"
   homepage "https://fondu.sourceforge.io/"
-  url "http://fondu.sourceforge.net/fondu_src-060102.tgz"
+  url "https://fondu.sourceforge.io/fondu_src-060102.tgz"
   sha256 "22bb535d670ebc1766b602d804bebe7e84f907c219734e6a955fcbd414ce5794"
 
   bottle do
