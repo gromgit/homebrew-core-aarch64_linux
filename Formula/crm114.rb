ddfc71e24cc5ea6631573d4b57853f6132788125
Viktor Szakats <vszakats@users.noreply.github.com>
2017-03-16 22:58:08 +0100

    crm114: https url

--- Formula/crm114.rb
+++ Formula/crm114.rb
@@ -1,7 +1,7 @@
 class Crm114 < Formula
   desc "Examine, sort, filter or alter logs or data streams"
   homepage "https://crm114.sourceforge.io/"
-  url "http://crm114.sourceforge.net/tarballs/crm114-20100106-BlameMichelson.src.tar.gz"
+  url "https://crm114.sourceforge.io/tarballs/crm114-20100106-BlameMichelson.src.tar.gz"
   sha256 "fb626472eca43ac2bc03526d49151c5f76b46b92327ab9ee9c9455210b938c2b"
 
   bottle do
