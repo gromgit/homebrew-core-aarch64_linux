2d58b7f35740afc132e3c9c668ed2d6b1161c6db
Miguel Araújo <miguelarauj1o@gmail.com>
2017-03-15 21:23:20 -0300

    liblas: update homepage to HTTPS

--- Formula/liblas.rb
+++ Formula/liblas.rb
@@ -1,6 +1,6 @@
 class Liblas < Formula
   desc "C/C++ library for reading and writing the LAS LiDAR format"
-  homepage "http://liblas.org"
+  homepage "https://liblas.org"
   url "http://download.osgeo.org/liblas/libLAS-1.8.1.tar.bz2"
   sha256 "9adb4a98c63b461ed2bc82e214ae522cbd809cff578f28511122efe6c7ea4e76"
   head "https://github.com/libLAS/libLAS.git"
