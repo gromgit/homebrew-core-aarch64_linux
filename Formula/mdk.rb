9f3fa758561d61822088b348c0bca75a7485cdd6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-17 19:54:00 +0000

    mdk: update 1.2.9_2 bottle.

--- Formula/mdk.rb
+++ Formula/mdk.rb
@@ -7,9 +7,9 @@ class Mdk < Formula
   revision 2
 
   bottle do
-    sha256 "9ba8df4910f272cdce34416cffb4d9b5ea27ab17a18c15a7a03b78e2c82bdde5" => :sierra
-    sha256 "ec7f02653ee96c042c3699337c480cd986d6aef25c158cf4f5051dba53a4c586" => :el_capitan
-    sha256 "9c51160fede51db605a8cf475dfc74b1733304ab2e8179c79a6973f73ec27669" => :yosemite
+    sha256 "f294500b0cce468da3b3420eed199ce6f7e50a6adfd582089a1d1f36080c4fbb" => :sierra
+    sha256 "2f7d0012a3a4ccb69e4ca500e16f5059642ddebc79bbac2df08dd73385d8e9e9" => :el_capitan
+    sha256 "264586d3518f0a1a5641c979471acf23860119d407c8efef608ae69e559a42cd" => :yosemite
   end
 
   depends_on "intltool" => :build
