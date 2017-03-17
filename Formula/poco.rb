2a9624f13b9a4e3dc73003a85fb7af55d013fb73
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-17 13:20:11 +0000

    poco: update 1.7.8_1 bottle.

--- Formula/poco.rb
+++ Formula/poco.rb
@@ -9,9 +9,9 @@ class Poco < Formula
 
   bottle do
     cellar :any
-    sha256 "43f5f683dc6368bd5565f49088e6853e7a5902f3526ef38d8fd25eca6d348c8b" => :sierra
-    sha256 "82b3b4488afe1d58452cb4451eb9e3f91128635687708ec8ab23158e43a5a6a2" => :el_capitan
-    sha256 "8d62a92a35cef00ac6eb7fdd6fded8f4ec045cae5b002c542b34b0aab14e025c" => :yosemite
+    sha256 "1dee35ba8ef2da7de4be8a158af03f39734cb605878e6cd32c20297ddb682b4e" => :sierra
+    sha256 "8e28312666a16f39dd68f612fb1f9e357be17bab70424e0f7e5cd35995de099e" => :el_capitan
+    sha256 "2863537edc4f16a96a031f9b9742a7c2d6d2a3eb605523d859b0d8be52b55c78" => :yosemite
   end
 
   option "with-static", "Build static libraries (instead of shared)"
