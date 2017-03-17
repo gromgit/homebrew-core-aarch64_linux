f3ae558837e4d7cb28eabf606a5351a255584dc6
ilovezfs <ilovezfs@icloud.com>
2017-03-16 23:27:07 -0700

    boxes 1.1.3
    Closes #11194.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/boxes.rb
+++ Formula/boxes.rb
@@ -1,9 +1,8 @@
 class Boxes < Formula
   desc "Draw boxes around text"
   homepage "http://boxes.thomasjensen.com/"
-  url "https://github.com/ascii-boxes/boxes/archive/v1.1.2.tar.gz"
-  sha256 "4d5e536be91b476ee48640bef9122f3114b16fe2da9b9906947308b94682c5fe"
-  revision 1
+  url "https://github.com/ascii-boxes/boxes/archive/v1.1.3.tar.gz"
+  sha256 "4087ca01413c0aa79d4b9dcaf917e0a91721e5de76e8f6a38d1a16234e8290bf"
   head "https://github.com/ascii-boxes/boxes.git"
 
   bottle do
@@ -11,10 +10,6 @@ class Boxes < Formula
     sha256 "bb0d5b9f24c531aaa9b45e09868073547ac0f1bc94ed39021c03f31cf8ed284b" => :el_capitan
     sha256 "8fe10850d2df635ca8800e826fa542be10e47a8c1a9da8ed2111245c61129ff7" => :yosemite
   end
-
-  # Patch to fix 64-bit compilation
-  # https://github.com/ascii-boxes/boxes/issues/38
-  patch :DATA
 
   def install
     # distro uses /usr/share/boxes change to prefix
@@ -26,35 +21,6 @@ class Boxes < Formula
   end
 
   test do
-    assert_match "/* test brew */", pipe_output("#{bin}/boxes", "test brew")
+    assert_match "test brew", pipe_output("#{bin}/boxes", "test brew")
   end
 end
-
-__END__
-diff --git a/src/Makefile b/src/Makefile
-index c2656df..db19d29 100644
---- a/src/Makefile
-+++ b/src/Makefile
-@@ -62,7 +62,7 @@ boxes.exe: $(ALL_OBJ)
- 
- 
- flags_unix:
--	$(eval CFLAGS := -ansi -I. -Iregexp -Wall -W $(CFLAGS_ADDTL))
-+	$(eval CFLAGS := -I. -Iregexp -Wall -W $(CFLAGS_ADDTL))
- 	$(eval LDFLAGS := -Lregexp $(LDFLAGS_ADDTL))
- 	$(eval BOXES_EXECUTABLE_NAME := boxes)
- 	$(eval ALL_OBJ := $(GEN_SRC:.c=.o) $(ORIG_NORM:.c=.o))
-
-
-diff --git a/src/parser.y b/src/parser.y
-index c9acfbd..a32ef36 100644
---- a/src/parser.y
-+++ b/src/parser.y
-@@ -27,6 +27,7 @@
- #include <stdio.h>
- #include <stdlib.h>
- #include <string.h>
-+#include <strings.h>
- #include "shape.h"
- #include "boxes.h"
- #include "tools.h"
