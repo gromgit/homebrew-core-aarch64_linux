328b29590fa64c286053e5eecea538404443baed
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-03-18 13:15:25 +0000

    valabind: revision bump + patch

--- Formula/valabind.rb
+++ Formula/valabind.rb
@@ -3,7 +3,7 @@ class Valabind < Formula
   homepage "https://radare.org/"
   url "https://www.radare.org/get/valabind-0.10.0.tar.gz"
   sha256 "35517455b4869138328513aa24518b46debca67cf969f227336af264b8811c19"
-  revision 2
+  revision 3
 
   head "https://github.com/radare/valabind.git"
 
@@ -17,6 +17,13 @@ class Valabind < Formula
   depends_on "pkg-config" => :build
   depends_on "swig" => :run
   depends_on "vala"
+
+  # patch necessary to support vala 0.36.0
+  # remove upon next release
+  patch do
+    url "https://github.com/radare/valabind/commit/f23ff9421c1875d18b1e558596557009b45faa19.patch"
+    sha256 "9fe8f9485e1a4f52c68831670b880efcbbae33c6bc70e67297a00cc1d2fe0d4f"
+  end
 
   def install
     system "make"
