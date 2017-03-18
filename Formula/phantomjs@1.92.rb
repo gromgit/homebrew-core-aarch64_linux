0482a086ded56d791aca783aad0aa9d0471f4193
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-18 13:12:34 +0000

    phantomjs@1.92: add 1.9.2 bottle.

--- Formula/phantomjs@1.92.rb
+++ Formula/phantomjs@1.92.rb
@@ -4,6 +4,13 @@ class PhantomjsAT192 < Formula
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/phantomjs/phantomjs-1.9.2-macosx.zip"
   sha256 "85a1ddc5c5acb630abbfdc10617b5b248856d400218a9ec14872c7e1afef6698"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ac9b23318fd500de6bee2447a79e1fe57c8f5d6c0aab6df6a003e27435ce8cf6" => :sierra
+    sha256 "ac9b23318fd500de6bee2447a79e1fe57c8f5d6c0aab6df6a003e27435ce8cf6" => :el_capitan
+    sha256 "ac9b23318fd500de6bee2447a79e1fe57c8f5d6c0aab6df6a003e27435ce8cf6" => :yosemite
+  end
+
   depends_on :macos => :snow_leopard
 
   def install
