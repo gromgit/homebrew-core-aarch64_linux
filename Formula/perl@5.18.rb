78ebb760e9b96bf1e09d621481df595973f1f613
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-18 13:12:34 +0000

    perl@5.18: add 5.18.2 bottle.

--- Formula/perl@5.18.rb
+++ Formula/perl@5.18.rb
@@ -4,6 +4,12 @@ class PerlAT518 < Formula
   url "https://www.cpan.org/src/5.0/perl-5.18.2.tar.gz"
   sha256 "7cbed5ef11900e8f68041215eea0de5e443d53393f84c41d5c9c69c150a4982f"
 
+  bottle do
+    sha256 "bd7cec940fcec85651d37e00c6f3b073c4f0e7c6dc82d8836276947a504dd169" => :sierra
+    sha256 "5ea15144192fc50417125abb4735272d95a0ae7602b99deaca7e316b3cce3dd8" => :el_capitan
+    sha256 "dd567491e3d46487ec5657ec381a991164205c043798973c949498ead69e5519" => :yosemite
+  end
+
   keg_only :versioned_formula
 
   option "with-dtrace", "Build with DTrace probes"
