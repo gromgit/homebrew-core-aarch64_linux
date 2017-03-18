90b306a2c5844905aa33451f1bb95ec11c2e1dc8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-18 10:08:43 +0000

    boxes: update 1.2 bottle.

--- Formula/boxes.rb
+++ Formula/boxes.rb
@@ -6,9 +6,9 @@ class Boxes < Formula
   head "https://github.com/ascii-boxes/boxes.git"
 
   bottle do
-    sha256 "5715225d1b80a729ce073906aad9a785c03cb4df064dfde0683595dc2fceb51b" => :sierra
-    sha256 "b34022a6136281c73eec0d470cf6cf1c520e681ca50055b0a026ba2430542ab8" => :el_capitan
-    sha256 "6c7f30eb447ce1c54d073aa3b8aa16611671522d709a460a8a53d08063a68d6b" => :yosemite
+    sha256 "ebb5d353fb518ceb79ad3f52c0e53e867c5fc7bef1577cdca712ce91e522af56" => :sierra
+    sha256 "6f0a27e8bad4e4294bf4b4f82d1774eaf987cc7e8e6486f3204619ab84f01b13" => :el_capitan
+    sha256 "ae79b8cff43636d551a7f3e34dd74c5d6c15c526c79b6a9ad6b0fb2d3b60c0e0" => :yosemite
   end
 
   def install
