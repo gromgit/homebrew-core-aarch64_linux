e6615ac06288d90d65d44dd52141a525359f947a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-18 13:12:33 +0000

    perl@5.14: add 5.14.4 bottle.

--- Formula/perl@5.14.rb
+++ Formula/perl@5.14.rb
@@ -5,6 +5,12 @@ class PerlAT514 < Formula
   mirror "https://mirrors.kernel.org/debian/pool/main/p/perl/perl_5.14.2.orig.tar.bz2"
   sha256 "803fd44c492fcef79fda456a6f50455766e00ddec2e568a543630f65ff3f44cb"
 
+  bottle do
+    sha256 "e4f4b46b3505bff84553c33ffb0422132726b2c71e3daa16869632aba15410b1" => :sierra
+    sha256 "4d3d3954696d31de35a1152a1777eaef3bc847f2129ba8b33d80c3fa0c95ea55" => :el_capitan
+    sha256 "1f9dafd45c73c84ecaf87c0f0c7528b4070e2d7f394f0d6ae56b771259695d67" => :yosemite
+  end
+
   keg_only :versioned_formula
 
   option "with-dtrace", "Build with DTrace probes"
