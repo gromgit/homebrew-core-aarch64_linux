17d2d1db1262bb3c99a359f888fbbdd28d3f168c
Lukáš Mešťan <arzzen@users.noreply.github.com>
2017-03-19 11:46:28 +0100

    git-quick-stats 1.0.5 (#11259)

--- Formula/git-quick-stats.rb
+++ Formula/git-quick-stats.rb
@@ -1,8 +1,8 @@
 class GitQuickStats < Formula
   desc "Simple and efficient way to access statistics in git."
   homepage "https://github.com/arzzen/git-quick-stats"
-  url "https://github.com/arzzen/git-quick-stats/archive/1.0.3.tar.gz"
-  sha256 "00d827ca2d51aa752149342bbd294e5bbba329a67679b9c915cc79e6fd8184a4"
+  url "https://github.com/arzzen/git-quick-stats/archive/1.0.5.tar.gz"
+  sha256 "256633aee79896183ac783462707dbd8ade3d910a16ec2c53bc5be2870aace80"
 
   bottle :unneeded
 
