a437531cf422b2ea36106c7e8874cce755a5cf91
Mike McQuaid <mike@mikemcquaid.com>
2017-03-19 20:34:19 +0200

    rofs-filtered: import from homebrew/fuse.

---
+++ Formula/rofs-filtered.rb
@@ -0,0 +1,17 @@
+class RofsFiltered < Formula
+  desc "Filtered read-only filesystem for FUSE"
+  homepage "https://github.com/gburca/rofs-filtered/"
+  url "https://github.com/gburca/rofs-filtered/archive/rel-1.7.tar.gz"
+  sha256 "d66066dfd0274a2fb7b71dd929445377dd23100b9fa43e3888dbe3fc7e8228e8"
+
+  depends_on "cmake" => :build
+  depends_on :osxfuse
+  depends_on :macos => :yosemite
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", "-DCMAKE_INSTALL_SYSCONFDIR=#{etc}", *std_cmake_args
+      system "make", "install"
+    end
+  end
+end
