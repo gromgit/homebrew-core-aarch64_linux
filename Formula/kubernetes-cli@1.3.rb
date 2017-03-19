6c434ff5de80858dfd02d77c4980f70a91affcf8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-19 11:59:52 +0000

    kubernetes-cli@1.3: add 1.3.10 bottle.

--- Formula/kubernetes-cli@1.3.rb
+++ Formula/kubernetes-cli@1.3.rb
@@ -4,6 +4,13 @@ class KubernetesCliAT13 < Formula
   url "https://github.com/kubernetes/kubernetes/archive/v1.3.10.tar.gz"
   sha256 "7167e8c8e68d34596018103b55724f01d9b5e1f715c816c347a967cb438d6d18"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "90f6531017970c3adc25a33412ff5e6189b7e5c4fd7ed755ff254357984afee5" => :sierra
+    sha256 "e12f80dd4ec73d313f474fc4f79a9619209e5d40656a7808c50846c1d169f564" => :el_capitan
+    sha256 "490566a4266fb26ca84175197db2ec55fe3aac0e70e7334fccc9343d3d1556c5" => :yosemite
+  end
+
   keg_only :versioned_formula
 
   depends_on "go" => :build
