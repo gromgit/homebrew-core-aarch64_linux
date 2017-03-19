6d8a8c9c5bf0c2dd2507c1f1565c9c3c4b455e1b
Zhiming Wang <zmwangx@gmail.com>
2017-03-19 15:28:53 -0400

    youtube-dl: skip make step for stable and stop bottling
    - Make artifacts are already included in release tarballs (at least for now);
    - Stop bottling (#11281);
    - Remove useless comments (all GitHub releases are published on PyPI).

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -1,18 +1,10 @@
-# Please only update to versions that are published on PyPi as there are too
-# many releases for us to update to every single one:
-# https://pypi.python.org/pypi/youtube_dl
 class YoutubeDl < Formula
   desc "Download YouTube videos from the command-line"
   homepage "https://rg3.github.io/youtube-dl/"
   url "https://github.com/rg3/youtube-dl/releases/download/2017.03.20/youtube-dl-2017.03.20.tar.gz"
   sha256 "6e4201d7b45da75e9ed04c0393e9b1ce86fad27375337d5e1700549a26597215"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "dcede4ff2da08acf2ee876a9f72127188d1890be66140170f48d97f9418b6ac3" => :sierra
-    sha256 "dcede4ff2da08acf2ee876a9f72127188d1890be66140170f48d97f9418b6ac3" => :el_capitan
-    sha256 "dcede4ff2da08acf2ee876a9f72127188d1890be66140170f48d97f9418b6ac3" => :yosemite
-  end
+  bottle :unneeded
 
   head do
     url "https://github.com/rg3/youtube-dl.git"
@@ -22,7 +14,7 @@ class YoutubeDl < Formula
   depends_on "rtmpdump" => :optional
 
   def install
-    system "make", "PREFIX=#{prefix}"
+    system "make", "PREFIX=#{prefix}" if build.head?
     bin.install "youtube-dl"
     man1.install "youtube-dl.1"
     bash_completion.install "youtube-dl.bash-completion"
