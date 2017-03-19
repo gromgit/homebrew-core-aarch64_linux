7b2a554cee255dfe933dc8718e124eb15002c7fe
ilovezfs <ilovezfs@icloud.com>
2017-03-19 06:49:48 -0700

    gammaray 2.7.0
    Closes #11269.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gammaray.rb
+++ Formula/gammaray.rb
@@ -1,19 +1,9 @@
 class Gammaray < Formula
   desc "Examine and manipulate Qt application internals at runtime"
   homepage "https://www.kdab.com/kdab-products/gammaray/"
-  revision 2
+  url "https://github.com/KDAB/GammaRay/releases/download/v2.7.0/gammaray-2.7.0.tar.gz"
+  sha256 "09b814a33a53ae76f897ca8a100af9b57b08807f6fc2a1a8c7889212ee10c83b"
   head "https://github.com/KDAB/GammaRay.git"
-
-  stable do
-    url "https://github.com/KDAB/GammaRay/releases/download/v2.6.0/gammaray-2.6.0.tar.gz"
-    sha256 "6fe8e0bf9f9a479b7edf7d15e6ed48ad3cca666e149bc26e8fea54c12ded9039"
-
-    # Upstream commit from 23 Jan 2017 "Fix macOS startup crash with Qt 5.8"
-    patch do
-      url "https://github.com/KDAB/GammaRay/commit/01c5154.patch"
-      sha256 "c4d44f2625fa3823e0c6788437446247473cc460877be87a5e33d2b1b1a9d6ff"
-    end
-  end
 
   bottle do
     sha256 "40abbbf3706bcd8671fae35f7e53397b090895d9022e7f1dc1370d572342a87a" => :sierra
@@ -22,7 +12,6 @@ class Gammaray < Formula
   end
 
   option "with-vtk", "Build with VTK-with-Qt support, for object 3D visualizer"
-  option "with-test", "Verify the build with `make test`"
 
   needs :cxx11
 
@@ -37,17 +26,11 @@ class Gammaray < Formula
     # For Mountain Lion
     ENV.libcxx
 
-    # attachtest-lldb causes "make check" to fail
-    # Reported 31 Jul 2016: https://github.com/KDAB/GammaRay/issues/241
-    inreplace "tests/CMakeLists.txt", "/gammaray lldb", "/gammaray nosuchfile"
-
     args = std_cmake_args
     args << "-DCMAKE_DISABLE_FIND_PACKAGE_VTK=" + ((build.without? "vtk") ? "ON" : "OFF")
     args << "-DCMAKE_DISABLE_FIND_PACKAGE_Graphviz=" + ((build.without? "graphviz") ? "ON" : "OFF")
 
     system "cmake", *args
-    system "make"
-    system "make", "test" if build.bottle? || build.with?("test")
     system "make", "install"
   end
 
