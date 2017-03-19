ca3afc289efae4372d6cfa717a85945d99e8e7e2
Alex Dunn <dunn.alex@gmail.com>
2017-03-19 09:52:47 -0700

    omega 1.4.3
    Closes #11280.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/omega.rb
+++ Formula/omega.rb
@@ -1,8 +1,9 @@
 class Omega < Formula
   desc "Packaged search engine for websites, built on top of Xapian"
   homepage "https://xapian.org/"
-  url "https://oligarchy.co.uk/xapian/1.4.1/xapian-omega-1.4.1.tar.xz"
-  sha256 "d114bb16431adad50f505edadfb4c97aca9657132b239509ff8cf335aff738af"
+  url "https://oligarchy.co.uk/xapian/1.4.3/xapian-omega-1.4.3.tar.xz"
+  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/x/xapian-omega/xapian-omega_1.4.3.orig.tar.xz"
+  sha256 "2eea0344a0703ba379d845b86d08a9c2e9faf0deb21834d9ea6939b712c6216e"
 
   bottle do
     sha256 "513cf3679b92be4aa65c3a098323960d53691d0b69f13b846c4e6d0ee762195d" => :sierra
@@ -10,6 +11,7 @@ class Omega < Formula
     sha256 "f4830534a3a4803142c6ce5b037955c98b0756bd9c483f435314d0a1dcd6d2ec" => :yosemite
   end
 
+  depends_on "libmagic"
   depends_on "pcre"
   depends_on "xapian"
 
