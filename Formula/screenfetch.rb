c559e4935d98970e0c552172401e0173442aef9a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-19 14:19:02 +0000

    screenfetch: update 3.8.0 bottle.

--- Formula/screenfetch.rb
+++ Formula/screenfetch.rb
@@ -7,11 +7,9 @@ class Screenfetch < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5a351a099b84c85e6923e0aecc1504dd8e6b58e4b7a17fcfe6c7ad06b1803dc0" => :sierra
-    sha256 "a432e872bbe8d87199d49a9806350e52247bb591c6732e736f082cf12329050a" => :el_capitan
-    sha256 "f06711aa9632682d30fc81ecec15eb9822ed1946fa39880ba18a9d57a27c66d9" => :yosemite
-    sha256 "ec75963396cf12dfd7277d6149a2aa87dd63079ddaccb2a03fd04e55f163e36b" => :mavericks
-    sha256 "16810381df9a010c1c5c84dd1eba262e4e7b276a3e12ca87d5d0e2e3302545bd" => :mountain_lion
+    sha256 "62441999c591325de600f4cbd5d46e4157c443cfb514a3b1dfce8764c911785d" => :sierra
+    sha256 "62441999c591325de600f4cbd5d46e4157c443cfb514a3b1dfce8764c911785d" => :el_capitan
+    sha256 "62441999c591325de600f4cbd5d46e4157c443cfb514a3b1dfce8764c911785d" => :yosemite
   end
 
   def install
