c8ff28bfeef27b8fd4cbd3db47fd277e449b715a
Mike McQuaid <mike@mikemcquaid.com>
2017-03-19 20:34:20 +0200

    wdfs: import from homebrew/fuse.

---
+++ Formula/wdfs.rb
@@ -0,0 +1,21 @@
+class Wdfs < Formula
+  desc "Webdav file system"
+  homepage "http://noedler.de/projekte/wdfs/"
+  url "http://noedler.de/projekte/wdfs/wdfs-1.4.2.tar.gz"
+  sha256 "fcf2e1584568b07c7f3683a983a9be26fae6534b8109e09167e5dff9114ba2e5"
+
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "neon"
+  depends_on :osxfuse
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/wdfs", "-v"
+  end
+end
