ec83d4e6accc046395a7989387934bcf8b2dd4cc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-19 11:59:52 +0000

    jpeg@9: add 9.1 bottle.

--- Formula/jpeg@9.rb
+++ Formula/jpeg@9.rb
@@ -5,6 +5,13 @@ class JpegAT9 < Formula
   version "9.1"
   sha256 "240fd398da741669bf3c90366f58452ea59041cacc741a489b99f2f6a0bad052"
 
+  bottle do
+    cellar :any
+    sha256 "0fcac25009100dd809a12c537b9563da2e1244f63a55a33bc2df1376fb80eb82" => :sierra
+    sha256 "9bb92308131d09dd0a30601fd12d26538b2006f4c477eac225af51460813958b" => :el_capitan
+    sha256 "51c5f9c675dc4a37ebaeeff0689c87d75f960dea1e3e4e7d92bad06795694093" => :yosemite
+  end
+
   keg_only :versioned_formula
 
   def install
