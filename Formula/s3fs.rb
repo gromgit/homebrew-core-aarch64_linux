c89e5df1e63550ee873ebaa7945065e5be35357b
Mike McQuaid <mike@mikemcquaid.com>
2017-03-19 20:34:20 +0200

    s3fs: import from homebrew/fuse.

---
+++ Formula/s3fs.rb
@@ -0,0 +1,36 @@
+class S3fs < Formula
+  desc "FUSE-based file system backed by Amazon S3"
+  homepage "https://github.com/s3fs-fuse/s3fs-fuse/wiki"
+  url "https://github.com/s3fs-fuse/s3fs-fuse/archive/v1.80.tar.gz"
+  sha256 "de8381c9bd73e491904e73561882dc91e5fad9c28fe88834a93d7b4fe12cbd35"
+
+  head "https://github.com/s3fs-fuse/s3fs-fuse.git"
+
+  depends_on "pkg-config" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "gnutls"
+  depends_on "nettle"
+  depends_on "libgcrypt"
+
+  depends_on :osxfuse
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--disable-dependency-tracking", "--with-gnutls", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats; <<-EOS.undent
+    Be aware that s3fs has some caveats concerning S3 "directories"
+    that have been created by other tools. See the following issue for
+    details:
+
+      https://code.google.com/p/s3fs/issues/detail?id=73
+    EOS
+  end
+
+  test do
+    system "#{bin}/s3fs", "--version"
+  end
+end
