d4a36abea290eba72bc3cbedf633b7462fa66593
Mike McQuaid <mike@mikemcquaid.com>
2017-03-19 20:34:17 +0200

    afuse: import from homebrew/fuse.

---
+++ Formula/afuse.rb
@@ -0,0 +1,18 @@
+class Afuse < Formula
+  desc "Automounting file system implemented in userspace with FUSE"
+  homepage "https://github.com/pcarrier/afuse/"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/afuse/afuse-0.4.1.tar.gz"
+  sha256 "c6e0555a65d42d3782e0734198bbebd22486386e29cb00047bc43c3eb726dca8"
+
+  depends_on "pkg-config" => :build
+  depends_on :osxfuse
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    assert_match /OSXFUSE/, pipe_output("#{bin}/afuse --version 2>&1")
+  end
+end
