2ec3fef1322c4327264e97c075a8f487795f51f1
ilovezfs <ilovezfs@icloud.com>
2017-03-19 06:53:38 -0700

    jsonschema2pojo 0.4.31 (#11262)

--- Formula/jsonschema2pojo.rb
+++ Formula/jsonschema2pojo.rb
@@ -1,8 +1,8 @@
 class Jsonschema2pojo < Formula
   desc "Generates Java types from JSON Schema (or example JSON)"
   homepage "http://www.jsonschema2pojo.org/"
-  url "https://github.com/joelittlejohn/jsonschema2pojo/releases/download/jsonschema2pojo-0.4.30/jsonschema2pojo-0.4.30.tar.gz"
-  sha256 "12f6d7b79aa6f41b9d027510e52481308503cb4587daada46b7e035ae228f988"
+  url "https://github.com/joelittlejohn/jsonschema2pojo/releases/download/jsonschema2pojo-0.4.31/jsonschema2pojo-0.4.31.tar.gz"
+  sha256 "53f8ba01cbde07705efb032c033aa241842436975e85a0e9ca1c47a428a8270d"
 
   bottle :unneeded
 
