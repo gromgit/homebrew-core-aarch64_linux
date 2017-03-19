6f1d4270b68f828b0d45a236a672d9fa27a27c7f
Mike McQuaid <mike@mikemcquaid.com>
2017-03-19 20:34:18 +0200

    btfs: import from homebrew/fuse.

---
+++ Formula/btfs.rb
@@ -0,0 +1,27 @@
+class Btfs < Formula
+  desc "BitTorrent filesystem based on FUSE"
+  homepage "https://github.com/johang/btfs"
+  url "https://github.com/johang/btfs/archive/v2.13.tar.gz"
+  sha256 "494c3e16261e3c7535efa86493e09270dde29cd7121e97c29d226c239cbeabd6"
+  head "https://github.com/johang/btfs.git"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on :osxfuse
+  depends_on "libtorrent-rasterbar"
+
+  def install
+    inreplace "configure.ac", "fuse >= 2.8.0", "fuse >= 2.7.3"
+    system "autoreconf", "--force", "--install"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/btfs", "--help"
+  end
+end
