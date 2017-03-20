4762c6639bf66cf0a34f4752ac3194f86f095e8f
Viktor Szakats <vszakats@users.noreply.github.com>
2017-03-20 17:43:43 +0100

    libtrace: url updates

--- Formula/libtrace.rb
+++ Formula/libtrace.rb
@@ -1,7 +1,7 @@
 class Libtrace < Formula
   desc "Library for trace processing supporting multiple inputs"
-  homepage "http://research.wand.net.nz/software/libtrace.php"
-  url "http://research.wand.net.nz/software/libtrace/libtrace-4.0.0.tar.bz2"
+  homepage "https://research.wand.net.nz/software/libtrace.php"
+  url "https://research.wand.net.nz/software/libtrace/libtrace-4.0.0.tar.bz2"
   sha256 "e89ac39808e2bb1e17e031191af8ab7bdbe3d2b0aeca4c6040e6fc8761ec0240"
 
   bottle do
