353320c9c558eb53820487f666befc40f75f33a5
Viktor Szakats <vszakats@users.noreply.github.com>
2017-03-20 17:34:20 +0100

    schismtracker: remove protocol from web.archive.org param

--- Formula/schismtracker.rb
+++ Formula/schismtracker.rb
@@ -17,7 +17,7 @@ class Schismtracker < Formula
   depends_on "sdl"
 
   # CC BY-NC-ND licensed set of five mods by Keith Baylis/Vim! for testing purposes
-  # Mods from Mod Soul Brother: https://web.archive.org/web/20120215215707/http://www.mono211.com/modsoulbrother/vim.html
+  # Mods from Mod Soul Brother: https://web.archive.org/web/20120215215707/www.mono211.com/modsoulbrother/vim.html
   resource "demo_mods" do
     url "https://files.scene.org/get:us-http/mirrors/modsoulbrother/vim/vim-best-of.zip"
     sha256 "df8fca29ba116b10485ad4908cea518e0f688850b2117b75355ed1f1db31f580"
