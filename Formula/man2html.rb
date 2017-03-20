c3444a1c98b24d9df40eed65c5ea7c608f1573de
FX Coudert <fxcoudert@gmail.com>
2017-03-20 14:14:31 +0100

    man2html: fix stable URL

--- Formula/man2html.rb
+++ Formula/man2html.rb
@@ -1,7 +1,7 @@
 class Man2html < Formula
   desc "Convert nroff man pages to HTML"
   homepage "https://savannah.nongnu.org/projects/man2html/"
-  url "http://dcssrv1.oit.uci.edu/indiv/ehood/tar/man2html3.0.1.tar.gz"
+  url "https://www.mhonarc.org/release/misc/man2html3.0.1.tar.gz"
   mirror "https://distfiles.macports.org/man2html/man2html3.0.1.tar.gz"
   sha256 "a3dd7fdd80785c14c2f5fa54a59bf93ca5f86f026612f68770a0507a3d4e5a29"
 
