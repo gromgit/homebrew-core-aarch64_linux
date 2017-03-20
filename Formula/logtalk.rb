ec53559387ca35bd8f6f6596d28e4b6e947cf20e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-20 21:07:49 +0000

    logtalk: update 3.10.3 bottle.

--- Formula/logtalk.rb
+++ Formula/logtalk.rb
@@ -7,9 +7,9 @@ class Logtalk < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "2fcba4cbc830f048a16c9cee829b4f9f026723b54473a316dc32afaef20bf712" => :sierra
-    sha256 "5c2a5f6cc605fd884b6a5c68b8dd8cf00522897ffeebb1c24aa7c1cda9383a7b" => :el_capitan
-    sha256 "1f73f94e4e3babb461773f55b4e9f3269246cb902fb4b53370203073fbb30aa9" => :yosemite
+    sha256 "b15966f40b59a92ea22498e510c5ea76b3493c9cbff4ba7865f35080fa4be255" => :sierra
+    sha256 "a778a2a3c4aebbdbe57e882e03098066936f4df9e5968edf664d4d8de668a42d" => :el_capitan
+    sha256 "b3edce01af4190fd41d09dafbac751da4526004d8707ba09b37ec36a207913f7" => :yosemite
   end
 
   option "with-swi-prolog", "Build using SWI Prolog as backend"
