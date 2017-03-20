40a1d8cb66258583990dbefcbc30aae6e32a272b
Viktor Szakats <vszakats@users.noreply.github.com>
2017-03-20 17:31:00 +0100

    gpcslots2: https in homepage

--- Formula/gpcslots2.rb
+++ Formula/gpcslots2.rb
@@ -1,6 +1,6 @@
 class Gpcslots2 < Formula
   desc "Casino text-console game"
-  homepage "http://sourceforge.net/projects/gpcslots2/"
+  homepage "https://sourceforge.net/projects/gpcslots2/"
   url "https://downloads.sourceforge.net/project/gpcslots2/gpcslots2_0-4-5b"
   version "0.4.5b"
   sha256 "4daf5b6e5a23fe6cd121fe1250f10ad9f3b936bd536d475ec585f57998736f55"
