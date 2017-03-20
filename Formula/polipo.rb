2bb5060e89598660c604efd05a2384941f6f341f
FX Coudert <fxcoudert@gmail.com>
2017-03-20 14:14:11 +0100

    polipo: fix HEAD URL

--- Formula/polipo.rb
+++ Formula/polipo.rb
@@ -4,7 +4,7 @@ class Polipo < Formula
   url "https://www.irif.univ-paris-diderot.fr/~jch/software/files/polipo/polipo-1.1.1.tar.gz"
   sha256 "a259750793ab79c491d05fcee5a917faf7d9030fb5d15e05b3704e9c9e4ee015"
 
-  head "git://git.wifi.pps.jussieu.fr/polipo"
+  head "https://github.com/jech/polipo.git"
 
   bottle do
     rebuild 1
