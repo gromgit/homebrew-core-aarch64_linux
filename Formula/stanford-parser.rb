cb84ffd5f471a33d137b0d05982b74c239d4345e
Viktor Szakats <vszakats@users.noreply.github.com>
2017-03-20 17:43:43 +0100

    stanford-parser: url updates

--- Formula/stanford-parser.rb
+++ Formula/stanford-parser.rb
@@ -1,7 +1,7 @@
 class StanfordParser < Formula
   desc "Statistical NLP parser"
-  homepage "http://nlp.stanford.edu/software/lex-parser.shtml"
-  url "http://nlp.stanford.edu/software/stanford-parser-full-2015-12-09.zip"
+  homepage "https://nlp.stanford.edu/software/lex-parser.shtml"
+  url "https://nlp.stanford.edu/software/stanford-parser-full-2015-12-09.zip"
   version "3.6.0"
   sha256 "b09ca9711d92004184c9212156f09d21f9af0d3e745e95d4c438b6ca1e4f950a"
 
