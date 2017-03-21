88df0d71369f6262d104f5deec25989497f71ac0
ilovezfs <ilovezfs@icloud.com>
2017-03-21 11:18:41 -0700

    sonar-scanner 2.9.0.670 (#11405)

--- Formula/sonar-scanner.rb
+++ Formula/sonar-scanner.rb
@@ -1,8 +1,8 @@
 class SonarScanner < Formula
   desc "Launcher to analyze a project with SonarQube"
   homepage "https://docs.sonarqube.org/display/SCAN/Analyzing+with+SonarQube+Scanner"
-  url "https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-2.8.zip"
-  sha256 "0295365a7e5d4499ec6b46cb6c70f3fa127159b58b73930f675acd0897a6b350"
+  url "https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-2.9.0.670.zip"
+  sha256 "d10ca9009fde6ae6cc01b203e9995ce1f6efc90a02de46997ce27572eb4fb855"
   head "https://github.com/SonarSource/sonar-scanner-cli.git"
 
   bottle :unneeded
