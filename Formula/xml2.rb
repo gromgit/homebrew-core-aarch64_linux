36387cce5baa181fb99528ecf346ded3ef5a4967
Viktor Szakats <vszakats@users.noreply.github.com>
2017-03-21 17:17:41 +0100

    xml2: remove protocol from web.archive.org param

--- Formula/xml2.rb
+++ Formula/xml2.rb
@@ -1,7 +1,7 @@
 class Xml2 < Formula
   desc "Makes XML and HTML more amenable to classic UNIX text tools"
-  homepage "https://web.archive.org/web/20160730094113/http://www.ofb.net/~egnor/xml2/"
-  url "https://web.archive.org/web/20160427221603/http://download.ofb.net/gale/xml2-0.5.tar.gz"
+  homepage "https://web.archive.org/web/20160730094113/www.ofb.net/~egnor/xml2/"
+  url "https://web.archive.org/web/20160427221603/download.ofb.net/gale/xml2-0.5.tar.gz"
   sha256 "e3203a5d3e5d4c634374e229acdbbe03fea41e8ccdef6a594a3ea50a50d29705"
 
   bottle do
