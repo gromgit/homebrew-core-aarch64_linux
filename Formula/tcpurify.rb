11dd78b32fd3cac1d8395ba030f1386339721e63
Viktor Szakats <vszakats@users.noreply.github.com>
2017-03-21 17:17:41 +0100

    tcpurify: remove protocol from web.archive.org param

--- Formula/tcpurify.rb
+++ Formula/tcpurify.rb
@@ -1,7 +1,7 @@
 class Tcpurify < Formula
   desc "Packet sniffer/capture program"
-  homepage "https://web.archive.org/web/20140203210616/http://irg.cs.ohiou.edu/~eblanton/tcpurify/"
-  url "https://web.archive.org/web/20140203210616/http://irg.cs.ohiou.edu/~eblanton/tcpurify/tcpurify-0.11.2.tar.gz"
+  homepage "https://web.archive.org/web/20140203210616/irg.cs.ohiou.edu/~eblanton/tcpurify/"
+  url "https://web.archive.org/web/20140203210616/irg.cs.ohiou.edu/~eblanton/tcpurify/tcpurify-0.11.2.tar.gz"
   sha256 "9822f88125e912c568de23b04cee7c84452eefa27a80dcaeaeb001f87cb60e99"
 
   bottle do
