c210fcb68a3a402fb441a7e76dd60094f97ef543
Viktor Szakats <vszakats@users.noreply.github.com>
2017-03-21 17:17:40 +0100

    alac: remove protocol from web.archive.org param

--- Formula/alac.rb
+++ Formula/alac.rb
@@ -1,7 +1,7 @@
 class Alac < Formula
   desc "Basic decoder for Apple Lossless Audio Codec files (ALAC)"
-  homepage "https://web.archive.org/web/20150319040222/http://craz.net/programs/itunes/alac.html"
-  url "https://web.archive.org/web/20150510210401/http://craz.net/programs/itunes/files/alac_decoder-0.2.0.tgz"
+  homepage "https://web.archive.org/web/20150319040222/craz.net/programs/itunes/alac.html"
+  url "https://web.archive.org/web/20150510210401/craz.net/programs/itunes/files/alac_decoder-0.2.0.tgz"
   sha256 "7f8f978a5619e6dfa03dc140994fd7255008d788af848ba6acf9cfbaa3e4122f"
 
   bottle do
