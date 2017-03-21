8e12f658d47eb227a7c84d43f11e334372009e88
ilovezfs <ilovezfs@icloud.com>
2017-03-21 08:03:36 -0700

    borg 0.0.3
    Closes #11383.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/borg.rb
+++ Formula/borg.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Borg < Formula
   desc "Terminal based search engine for bash commands"
   homepage "https://ok-b.org/"
-  url "https://github.com/crufter/borg/archive/v0.0.2.tar.gz"
-  sha256 "e1b24f34a5b391e910af5aa903a376106c6328389b0accadbca231822ca1ff32"
+  url "https://github.com/ok-borg/borg/archive/v0.0.3.tar.gz"
+  sha256 "d90a55b9c25c2b1fa0c662f1f22fa79f19e77479ad10368756ddf2fa9bee21cc"
 
   bottle do
     cellar :any_skip_relocation
@@ -15,6 +15,11 @@ class Borg < Formula
 
   depends_on "go" => :build
 
+  go_resource "github.com/fatih/color" do
+    url "https://github.com/fatih/color.git",
+        :revision => "9131ab34cf20d2f6d83fdc67168a5430d1c7dc23"
+  end
+
   go_resource "github.com/juju/gnuflag" do
     url "https://github.com/juju/gnuflag.git",
         :revision => "4e76c56581859c14d9d87e1ddbe29e1c0f10195f"
@@ -22,16 +27,16 @@ class Borg < Formula
 
   go_resource "gopkg.in/yaml.v2" do
     url "https://gopkg.in/yaml.v2.git",
-        :revision => "a5b47d31c556af34a302ce5d659e6fea44d90de0"
+        :revision => "a3f3340b5840cee44f372bddb5880fcbc419b46a"
   end
 
   def install
     ENV["GOPATH"] = buildpath
 
     Language::Go.stage_deps resources, buildpath/"src"
-    (buildpath/"src/github.com/crufter").mkpath
-    ln_s buildpath, buildpath/"src/github.com/crufter/borg"
-    system "go", "build", "-o", bin/"borg", "github.com/crufter/borg"
+    (buildpath/"src/github.com/ok-borg").mkpath
+    ln_s buildpath, buildpath/"src/github.com/ok-borg/borg"
+    system "go", "build", "-o", bin/"borg", "github.com/ok-borg/borg"
   end
 
   test do
