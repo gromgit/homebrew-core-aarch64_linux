06d5b559145bfb5a4cd9e568ba96c1813a417c62
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-03-21 12:53:12 +0000

    gjs 1.48.0
    Closes #11376.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/gjs.rb
+++ Formula/gjs.rb
@@ -1,8 +1,8 @@
 class Gjs < Formula
   desc "Javascript Bindings for GNOME"
   homepage "https://wiki.gnome.org/Projects/Gjs"
-  url "https://download.gnome.org/sources/gjs/1.46/gjs-1.46.0.tar.xz"
-  sha256 "2283591fa70785443793e1d7db66071b36052d707075f229baeb468d8dd25ad4"
+  url "https://download.gnome.org/sources/gjs/1.48/gjs-1.48.0.tar.xz"
+  sha256 "ed7db93e12709fe43b2fbe1e157331a0b1dcf3e0d19b81ccebd2d96bfcbe9525"
 
   bottle do
     sha256 "9a90cec9628aa4c648a3071eb43e0291c30bee3ad1f53b3907e8358395ef9e80" => :sierra
@@ -16,32 +16,51 @@ class Gjs < Formula
   depends_on "readline"
   depends_on "gtk+3" => :recommended
 
-  resource "mozjs24" do
-    url "https://ftp.mozilla.org/pub/mozilla.org/js/mozjs-24.2.0.tar.bz2"
-    sha256 "e62f3f331ddd90df1e238c09d61a505c516fe9fd8c5c95336611d191d18437d8"
+  needs :cxx11
+
+  # patch submitted upstream: https://bugzilla.gnome.org/show_bug.cgi?id=780350
+  patch :DATA
+
+  resource "mozjs38" do
+    url "https://archive.mozilla.org/pub/firefox/releases/38.8.0esr/source/firefox-38.8.0esr.source.tar.bz2"
+    sha256 "9475adcee29d590383c4885bc5f958093791d1db4302d694a5d2766698f59982"
   end
 
   def install
-    resource("mozjs24").stage do
+    resource("mozjs38").stage do
+      inreplace "config/rules.mk", "-install_name @executable_path/$(SHARED_LIBRARY) ", "-install_name #{lib}/$(SHARED_LIBRARY) "
       cd("js/src") do
-        # patches taken from MacPorts
-        # fixes a problem with Perl 5.22
-        inreplace "config/milestone.pl", "if (defined(@TEMPLATE_FILE)) {", "if (@TEMPLATE_FILE) {"
-        # use absolute path for install_name, don't assume will be put into an app bundle
-        inreplace "config/rules.mk", "@executable_path", "${prefix}/lib"
+        ENV["PYTHON"] = "python"
+        inreplace "configure", "'-Wl,-executable_path,$(LIBXUL_DIST)/bin'", ""
         system "./configure", "--disable-debug",
                               "--disable-dependency-tracking",
                               "--disable-silent-rules",
                               "--prefix=#{prefix}",
                               "--with-system-nspr",
+                              "--with-system-zlib",
+                              "--with-system-icu",
+                              "--enable-system-ffi",
                               "--enable-readline",
+                              "--enable-shared-js",
                               "--enable-threadsafe"
         system "make"
         system "make", "install"
         rm Dir["#{bin}/*"]
       end
+      mv "#{lib}/pkgconfig/js.pc", "#{lib}/pkgconfig/mozjs-38.pc"
+      # headers were installed as softlinks, which is not acceptable
+      cd(include.to_s) do
+        `find . -type l`.chomp.split.each do |link|
+          header = File.readlink(link)
+          rm link
+          cp header, link
+        end
+      end
       ENV.append_path "PKG_CONFIG_PATH", "#{lib}/pkgconfig"
+      # remove mozjs static lib
+      rm "#{lib}/libjs_static.ajs"
     end
+    ENV.cxx11
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
@@ -58,3 +77,18 @@ class Gjs < Formula
     system "#{bin}/gjs", "test.js"
   end
 end
+
+__END__
+diff --git a/gi/arg.cpp b/gi/arg.cpp
+index 5502b00..aacc07d 100644
+--- a/gi/arg.cpp
++++ b/gi/arg.cpp
+@@ -37,6 +37,8 @@
+ #include "gjs/byteArray.h"
+ #include "gjs/jsapi-wrapper.h"
+ #include <util/log.h>
++#include <cstdlib>
++#include <cmath>
+
+ bool
+ _gjs_flags_value_is_valid(JSContext   *context,
