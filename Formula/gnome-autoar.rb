23c334bbd3b4aa5b3412c0ba7ed9aa3d3a0b532d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-21 13:40:52 +0000

    gnome-autoar: update 0.2.2 bottle.

--- Formula/gnome-autoar.rb
+++ Formula/gnome-autoar.rb
@@ -5,9 +5,9 @@ class GnomeAutoar < Formula
   sha256 "e1fe2c06eed30305c38bf0939c72b0e51b4716658e2663a0cf4a4bf57874ca62"
 
   bottle do
-    sha256 "94421e49840cb95db58c73a2055c6108f26b5e696354eaff3e788a8251a16ccb" => :sierra
-    sha256 "ac9ea02b0ecc34eb1f044434180cf7e4937a840fce91bd76c91b7e52b9534cd1" => :el_capitan
-    sha256 "03d98d38c9e11e71d9a59aeeecc284025afb9533cb697443c9a2ead67c30b766" => :yosemite
+    sha256 "ea737743690c568c3aedba7ca5807c319abcb9549d6fd41bb708ebd2ee4b17c1" => :sierra
+    sha256 "6e9d51dec3fa8330f6584a735403da1f98ac9c0e917e89f4a2aec42174f5acd6" => :el_capitan
+    sha256 "925174b30b4a5577720f30d5b1d04450e5f0c515505f589f2df3188ab36ae227" => :yosemite
   end
 
   depends_on "pkg-config" => :build
