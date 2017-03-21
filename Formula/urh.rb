c8805750e525414ad2030f425318027a73f11588
ilovezfs <ilovezfs@icloud.com>
2017-03-21 09:47:33 -0700

    urh 1.6.1.8
    Closes #11413.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/urh.rb
+++ Formula/urh.rb
@@ -1,8 +1,8 @@
 class Urh < Formula
   desc "Universal Radio Hacker"
   homepage "https://github.com/jopohl/urh"
-  url "https://pypi.python.org/packages/c2/fd/4b1475d36054083f71bd6d0e42d21e2eceafa5cc4e6dde8192867d32562a/urh-1.6.1.1.tar.gz"
-  sha256 "a787ee8d742cb1f785604d0b8a6734147db51ffec5ad161c0f5e07029529331b"
+  url "https://files.pythonhosted.org/packages/29/47/f00588ff9d6807c7f8b54e1c7cf6fe8f66fd1f4ddaa85f03d7f2eff6719c/urh-1.6.1.8.tar.gz"
+  sha256 "7baca3e51484946644584509966d827943b3abd42fd82dcaf0d3e3c2acabaf8f"
   head "https://github.com/jopohl/urh.git"
 
   bottle do
@@ -23,12 +23,12 @@ class Urh < Formula
   depends_on "hackrf" => :optional
 
   resource "psutil" do
-    url "https://pypi.python.org/packages/3c/2f/f3ab91349c666f009077157b12057e613a3152a46a6c3be883777546b6de/psutil-5.2.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/3c/2f/f3ab91349c666f009077157b12057e613a3152a46a6c3be883777546b6de/psutil-5.2.0.tar.gz"
     sha256 "2fc91d068faa5613c093335f0e758673ef8c722ad4bfa4aded64c13ae69089eb"
   end
 
   resource "pyzmq" do
-    url "https://pypi.python.org/packages/af/37/8e0bf3800823bc247c36715a52e924e8f8fd5d1432f04b44b8cd7a5d7e55/pyzmq-16.0.2.tar.gz#md5=9a8768b00a566a400d70318f8c359cfe"
+    url "https://files.pythonhosted.org/packages/af/37/8e0bf3800823bc247c36715a52e924e8f8fd5d1432f04b44b8cd7a5d7e55/pyzmq-16.0.2.tar.gz"
     sha256 "0322543fff5ab6f87d11a8a099c4c07dd8a1719040084b6ce9162bcdf5c45c9d"
   end
 
