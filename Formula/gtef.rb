5eee8616a413c0816841b8641b0bceb5a6b127e4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-22 15:56:56 +0000

    gtef: update 2.0.0 bottle.

--- Formula/gtef.rb
+++ Formula/gtef.rb
@@ -5,9 +5,9 @@ class Gtef < Formula
   sha256 "12fb28b101bc14a7ed266a25e8ff41c0535e1201ce38905e443c8fca83e5ac71"
 
   bottle do
-    sha256 "027cd5f9151a8d73dc61d9708815d02e551010db4a1ff50c41daa484efc617d9" => :sierra
-    sha256 "0dc2178d62adbb9ea38515e4e9136f4294e18ee24b832c16fa15275acddec90d" => :el_capitan
-    sha256 "7c558cee5794e809d0e5c333e97f3031b7cff702c1e8ed6909687431fb240f88" => :yosemite
+    sha256 "d6b45a7bb9234ae2aa59bafe8a836f04643fa631d92b98e4b53257b52790051b" => :sierra
+    sha256 "18d083c4faba1b9065c978c004887d76d99f31d1f257164e9154a8cfe24eeba7" => :el_capitan
+    sha256 "1abfe88582878bcfd8ebcc16a59d0bcdfd7576ba42f4aa0f39b671a6f49bd6f4" => :yosemite
   end
 
   depends_on "pkg-config" => :build
