eaf9bfa7bc87fd968f3d47e0c9933da536754ef4
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-03-22 12:32:14 +0000

    latexila: import from homebrew-tex

---
+++ Formula/latexila.rb
@@ -0,0 +1,42 @@
+class Latexila < Formula
+  desc "LaTeX editor for the GNOME desktop"
+  homepage "https://wiki.gnome.org/Apps/LaTeXila"
+  url "https://download.gnome.org/sources/latexila/3.22/latexila-3.22.1.tar.xz"
+  sha256 "41692d850a48521d2a700ae296680464078ef0bef1c1383383b433c5ecdf2719"
+
+  depends_on "pkg-config" => :build
+  depends_on "intltool" => :build
+  depends_on "itstool" => :build
+  depends_on "vala" => :build
+  depends_on "gtksourceview3"
+  depends_on "gspell"
+  depends_on "libgee"
+  depends_on "gobject-introspection"
+  depends_on "gnome-icon-theme"
+  depends_on "gnome-themes-standard" => :optional
+  depends_on "libxml2" => "with-python"
+  depends_on :python if MacOS.version <= :snow_leopard
+  depends_on :tex => :recommended
+
+  def install
+    ENV.append_path "PYTHONPATH", "#{Formula["libxml2"].opt_lib}/python2.7/site-packages"
+    system "./configure", "--disable-schemas-compile",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def post_install
+    system "#{Formula["glib"].opt_bin}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
+    system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
+    # HighContrast is provided by gnome-themes-standard
+    if File.file?("#{HOMEBREW_PREFIX}/share/icons/HighContrast/.icon-theme.cache")
+      system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/HighContrast"
+    end
+  end
+
+  test do
+    system "#{bin}/latexila", "--version"
+  end
+end
