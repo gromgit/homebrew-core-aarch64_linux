80ea9a8296fd8b2e6470fd772277c9c8612ef25d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-22 15:56:56 +0000

    gst-plugins-bad@0.10: update 0.10.23_2 bottle.

--- Formula/gst-plugins-bad@0.10.rb
+++ Formula/gst-plugins-bad@0.10.rb
@@ -6,9 +6,9 @@ class GstPluginsBadAT010 < Formula
   revision 2
 
   bottle do
-    sha256 "5b2ee005df419f9b9397fd5b618f4a97f0d2b1af60404b5cc7d15f3c7d02c243" => :sierra
-    sha256 "51c6fccf38e4825c1615207c7290c5a7712cbc68282914a3966589e663f1e6fc" => :el_capitan
-    sha256 "5d8685e6aea37bad4a76b40dfff1772daf53289e3b985e43251022d0c70b2ca8" => :yosemite
+    sha256 "cb457e5de9fcacfb5ddc33cfe7abd4880a60f8a9a94435840ebb1c1c911702c0" => :sierra
+    sha256 "f12917530993bac1876a6b07764b3c784b0ccc50ef44b0d3694ee4bc30c9bed2" => :el_capitan
+    sha256 "d1db9d5e75ddbe8475050fdccf0f82147fecef42fa22289fd166936baaf41a6d" => :yosemite
   end
 
   depends_on "pkg-config" => :build
