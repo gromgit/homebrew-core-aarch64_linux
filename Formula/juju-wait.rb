c29d9333de17df44dfb9ec8757bec95aa2cd17d2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-22 20:00:40 +0000

    juju-wait: add 2.5.0 bottle.

--- Formula/juju-wait.rb
+++ Formula/juju-wait.rb
@@ -6,6 +6,12 @@ class JujuWait < Formula
   url "https://pypi.python.org/packages/96/82/6b1b566b75f668605469d9af220bed0104bd4dc12c66160771b32f3aab58/juju-wait-2.5.0.tar.gz"
   sha256 "05354b87e65b19a67176e470b4edf2588ae3ec301576b4a5214bc698c420671e"
 
+  bottle do
+    sha256 "61c0d5abc9f494f01b6ed108b9c39424d9c5f8e189bdaf2802c18652419f1cc4" => :sierra
+    sha256 "a51b32c830f32bdd9124255d7309b3c0d0d28eb5c601ef60aba1d6903ccc5dd9" => :el_capitan
+    sha256 "41dd695a166212b33abb723568c6a149ddf62be91c56545dbe4ee658382c5c70" => :yosemite
+  end
+
   depends_on :python3
   depends_on "libyaml"
   depends_on "juju"
