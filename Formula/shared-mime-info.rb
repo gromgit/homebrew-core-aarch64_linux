97785bf61f7e247eaa354742f6a9016cfd8ee5ce
ilovezfs <ilovezfs@icloud.com>
2017-03-23 16:23:36 +0000

    shared-mime-info: ensure HOMEBREW_PREFIX/share/mime gets used
    Closes #11311.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/shared-mime-info.rb
+++ Formula/shared-mime-info.rb
@@ -3,6 +3,7 @@ class SharedMimeInfo < Formula
   homepage "https://wiki.freedesktop.org/www/Software/shared-mime-info"
   url "https://freedesktop.org/~hadess/shared-mime-info-1.8.tar.xz"
   sha256 "2af55ef1a0319805b74ab40d331a3962c905477d76c086f49e34dc96363589e9"
+  revision 1
 
   bottle do
     cellar :any
@@ -36,9 +37,14 @@ class SharedMimeInfo < Formula
       system "./configure", *args
     end
     system "make", "install"
+    pkgshare.install share/"mime/packages"
+    rmdir share/"mime"
   end
 
   def post_install
+    ln_sf HOMEBREW_PREFIX/"share/mime", share/"mime"
+    (HOMEBREW_PREFIX/"share/mime/packages").mkpath
+    cp (pkgshare/"packages").children, HOMEBREW_PREFIX/"share/mime/packages"
     system bin/"update-mime-database", HOMEBREW_PREFIX/"share/mime"
   end
 
