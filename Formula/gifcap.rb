4729c7ab318f7c7af026ed4659ae02f674831462
Ben Bader <benjamin-bader@users.noreply.github.com>
2017-03-24 20:13:11 -0700

    gifcap 1.0.4 (#11561)

--- Formula/gifcap.rb
+++ Formula/gifcap.rb
@@ -1,8 +1,8 @@
 class Gifcap < Formula
   desc "Capture video from an Android device and make a gif"
   homepage "https://github.com/outlook/gifcap"
-  url "https://github.com/outlook/gifcap/archive/1.0.3.tar.gz"
-  sha256 "43b5f477b11f479ce2f4621e0e2c90ed5bc4563fc5f1dcbabfa6fe72e02454c9"
+  url "https://github.com/outlook/gifcap/archive/1.0.4.tar.gz"
+  sha256 "32747a6cf77f7ea99380752ba35ecd929bb185167e5908cf910d2a92f05029ad"
   head "https://github.com/outlook/gifcap.git"
 
   bottle :unneeded
