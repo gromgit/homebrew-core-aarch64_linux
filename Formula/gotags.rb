556db7afb804c68ec97078f3f6fcc5e6e778eb1a
ilovezfs <ilovezfs@icloud.com>
2017-03-25 09:15:04 -0700

    gotags 1.4.0
    Closes #11568.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/gotags.rb
+++ Formula/gotags.rb
@@ -1,8 +1,8 @@
 class Gotags < Formula
   desc "ctags-compatible tag generator for Go"
   homepage "https://github.com/jstemmer/gotags"
-  url "https://github.com/jstemmer/gotags/archive/v1.3.0.tar.gz"
-  sha256 "414e1f96b560b089f11f814cd9000974a8ee376bb2cd9119cce60368e89ba226"
+  url "https://github.com/jstemmer/gotags/archive/v1.4.0.tar.gz"
+  sha256 "7d186af067fbd998a92fd04ac5909b7e9ff15e693fbda666a160e280e47fdb7b"
 
   head "https://github.com/jstemmer/gotags.git"
 
