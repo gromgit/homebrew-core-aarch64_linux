f70557a74db2555acc545cc796c214070d8ba2dd
ilovezfs <ilovezfs@icloud.com>
2017-03-27 16:25:09 -0700

    libsmi: new mirror (#11701)

--- Formula/libsmi.rb
+++ Formula/libsmi.rb
@@ -2,7 +2,7 @@ class Libsmi < Formula
   desc "Library to Access SMI MIB Information"
   homepage "https://www.ibr.cs.tu-bs.de/projects/libsmi/"
   url "https://www.ibr.cs.tu-bs.de/projects/libsmi/download/libsmi-0.4.8.tar.gz"
-  mirror "https://distfiles.macports.org/libsmi/libsmi-0.4.8.tar.gz"
+  mirror "https://ftp.mirrorservice.org/sites/download.salixos.org/i486/extra-14.2/source/libraries/libsmi/libsmi-0.4.8.tar.gz"
   sha256 "f048a5270f41bc88b0c3b0a8fe70ca4d716a46b531a0ecaaa87c462f49d74849"
 
   bottle do
