5e4f4e4da20ac1cfd2b6bf14c3f86a3fbb38a543
ilovezfs <ilovezfs@icloud.com>
2017-03-27 02:21:08 -0700

    coturn 4.5.0.6
    Closes #11640.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/coturn.rb
+++ Formula/coturn.rb
@@ -1,9 +1,8 @@
 class Coturn < Formula
   desc "Free open source implementation of TURN and STUN Server"
   homepage "https://github.com/coturn/coturn"
-  url "http://turnserver.open-sys.org/downloads/v4.5.0.5/turnserver-4.5.0.5.tar.gz"
-  sha256 "8484fa6c8d4aab43e1161c02eb8914154a21178b05f8a285e04094ddbb64acf4"
-  revision 1
+  url "http://turnserver.open-sys.org/downloads/v4.5.0.6/turnserver-4.5.0.6.tar.gz"
+  sha256 "c0ff3224084ff9a9504147a7b87431ee815ebeea0de7c7cb67126859da7e25a6"
 
   bottle do
     sha256 "7703a16ab5ef734b03f8142c819438d9b6b05aa642cd0ff442a933848ff7af41" => :sierra
