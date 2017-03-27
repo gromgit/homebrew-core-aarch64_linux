19ff039903cd15796870fab828505c3d5a54ab41
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-27 22:03:19 +0100

    dialog: update 1.3-20170131 bottle.

--- Formula/dialog.rb
+++ Formula/dialog.rb
@@ -7,10 +7,9 @@ class Dialog < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ee630bb7ae15cde86a4dd48a598b5eaf1151237bef9e0f1ffa9a120cc5fbd9ac" => :sierra
-    sha256 "8c12527b91df19529a580ff081621d30514119bfd999f76a763d59c406b32531" => :el_capitan
-    sha256 "89a8a52b64df7abe4aabfa9de1aa62523098154ae447da57fab3adce18b34a29" => :yosemite
-    sha256 "74626b6e18ebd0b4b755e04457be5a385bc10745cf08130f1330ab2b108a0550" => :mavericks
+    sha256 "15f85afce2306e17031370299bb97cdf2a6f8ede915fd2c19d8bb06847dbacdd" => :sierra
+    sha256 "6042c7e4b069367cb607c97708eb097207f37230d7bc22fbc4e0924e20d134b3" => :el_capitan
+    sha256 "3a5f37ec2481464ee96e383d619f1f28c14ed0b30645df91c1768dc6825592cf" => :yosemite
   end
 
   def install
