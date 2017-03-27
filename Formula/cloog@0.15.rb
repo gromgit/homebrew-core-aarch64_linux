04438434e0318331588433fedb09073bec0dd3b1
ilovezfs <ilovezfs@icloud.com>
2017-03-27 12:47:01 -0700

    cloog@0.15: new mirror (#11663)

--- Formula/cloog@0.15.rb
+++ Formula/cloog@0.15.rb
@@ -2,7 +2,7 @@ class CloogAT015 < Formula
   desc "Generate code for scanning Z-polyhedra"
   homepage "http://repo.or.cz/w/cloog-ppl.git"
   url "ftp://gcc.gnu.org/pub/gcc/infrastructure/cloog-ppl-0.15.11.tar.gz"
-  mirror "http://gcc.cybermirror.org/infrastructure/cloog-ppl-0.15.11.tar.gz"
+  mirror "https://ftp.mirrorservice.org/sites/sourceware.org/pub/gcc/infrastructure/cloog-ppl-0.15.11.tar.gz"
   sha256 "7cd634d0b2b401b04096b545915ac67f883556e9a524e8e803a6bf6217a84d5f"
 
   bottle do
