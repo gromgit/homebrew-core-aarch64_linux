71aa841ee67c6a08f33d8daf63a508f31d98cc33
ilovezfs <ilovezfs@icloud.com>
2017-03-27 13:03:08 -0700

    arm: use libexec (#11664)

--- Formula/arm.rb
+++ Formula/arm.rb
@@ -3,12 +3,13 @@ class Arm < Formula
   homepage "https://www.atagar.com/arm/"
   url "https://www.atagar.com/arm/resources/static/arm-1.4.5.0.tar.bz2"
   sha256 "fc0e771585dde3803873b4807578060f0556cf1cac6c38840a714ffada3b28fa"
+  revision 1
 
   bottle :unneeded
 
   def install
-    pkgshare.install Dir["*"]
-    bin.write_exec_script pkgshare/"arm"
+    libexec.install Dir["*"]
+    bin.write_exec_script libexec/"arm"
   end
 
   def caveats; <<-EOS.undent
@@ -16,7 +17,7 @@ class Arm < Formula
     See here for details: https://www.torproject.org/tor-manual.html.en
 
     To configure Arm, copy the sample configuration from
-    #{opt_pkgshare}/armrc.sample
+    #{opt_libexec}/armrc.sample
     to ~/.arm/armrc, adjusting as needed.
     EOS
   end
