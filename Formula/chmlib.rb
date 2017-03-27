c604e984b94ff5247e3de670b38a3824b053251d
ilovezfs <ilovezfs@icloud.com>
2017-03-27 13:33:12 -0700

    chmlib: new mirror (#11670)

--- Formula/chmlib.rb
+++ Formula/chmlib.rb
@@ -2,7 +2,7 @@ class Chmlib < Formula
   desc "Library for dealing with Microsoft ITSS/CHM files"
   homepage "http://www.jedrea.com/chmlib"
   url "http://www.jedrea.com/chmlib/chmlib-0.40.tar.gz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/c/chmlib/chmlib_0.40.orig.tar.gz"
+  mirror "https://download.tuxfamily.org/slitaz/sources/packages/c/chmlib-0.40.tar.gz"
   sha256 "512148ed1ca86dea051ebcf62e6debbb00edfdd9720cde28f6ed98071d3a9617"
 
   bottle do
