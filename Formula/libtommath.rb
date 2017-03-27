73f045173645dd699cec13a6a8972f3b16b2bc6b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-27 10:29:25 +0100

    libtommath: update 1.0 bottle.

--- Formula/libtommath.rb
+++ Formula/libtommath.rb
@@ -7,12 +7,9 @@ class Libtommath < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 2
-    sha256 "ff8b8d215c445537602f1ce68ded380035a8445c407272dc713e480605f57a9a" => :sierra
-    sha256 "b0f4e06eaf98729a5a8657538f7b9b8cd6f11926f09c34489b3e0b5cfb9bb376" => :el_capitan
-    sha256 "e6ed2cbd074bb7b8c2287baac219bf5a928dd84c8fc4c69d178358f3810164fe" => :yosemite
-    sha256 "fbc9c911c91862cad93470cac7e03aba2183faeadb6d67d2a15a0e730efc8103" => :mavericks
-    sha256 "537f66d9668409f36ef2c2b99bbd02e1a103e86a2603a66dc66c280763ab9c4a" => :mountain_lion
+    sha256 "4c5da0ecce4ba1a208b0556aefe34599e6846876f6a6fc5d5a6acde06519101c" => :sierra
+    sha256 "d4397cca9659c99660ef0a2c5e3b580cf60cc2748171592f0b61d596cdbb75a1" => :el_capitan
+    sha256 "9475003c6454215fbf018835460a2a6b681abc719be6176132473128237921f7" => :yosemite
   end
 
   def install
