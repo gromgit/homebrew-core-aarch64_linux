09413842b8c11d2d7116c252254d7ee65aaad5ad
Kaz Sera <seratch@gmail.com>
2017-03-28 16:41:29 +0900

    skinny 2.3.6 (#11660)

--- Formula/skinny.rb
+++ Formula/skinny.rb
@@ -1,10 +1,11 @@
 class Skinny < Formula
   desc "Full-stack web app framework in Scala"
   homepage "http://skinny-framework.org/"
-  url "https://github.com/skinny-framework/skinny-framework/releases/download/2.3.5/skinny-2.3.5.tar.gz"
-  sha256 "aba2495eba4c172726135b23d118b386cc5ddaaa6839b041c13369bc3e34da20"
+  url "https://github.com/skinny-framework/skinny-framework/releases/download/2.3.6/skinny-2.3.6.tar.gz"
+  sha256 "f58402433b5ede9a6a94e95a9bf651b59f67c3b3ae87c04771b371533b0ad4cd"
 
   bottle :unneeded
+  depends_on :java => "1.7+"
 
   def install
     libexec.install Dir["*"]
