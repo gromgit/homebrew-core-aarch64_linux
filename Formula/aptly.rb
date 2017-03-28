3df9448a0172e632f4e7d82f92de4ffc86175d31
ilovezfs <ilovezfs@icloud.com>
2017-03-27 22:12:30 -0700

    aptly 1.0.0
    Closes #11714.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/aptly.rb
+++ Formula/aptly.rb
@@ -1,10 +1,8 @@
-require "language/go"
-
 class Aptly < Formula
   desc "Swiss army knife for Debian repository management"
   homepage "https://www.aptly.info/"
-  url "https://github.com/smira/aptly/archive/v0.9.7.tar.gz"
-  sha256 "364d7b294747512c7e7a27f1b4c1d3f1e3f8b502944be142f9ab48a58ebe9a69"
+  url "https://github.com/smira/aptly/archive/v1.0.0.tar.gz"
+  sha256 "d919ae4be762b689a03aee9eff9209091c40b7d37327fc718c981f4e1a77e8ea"
   head "https://github.com/smira/aptly.git"
 
   bottle do
@@ -17,39 +15,14 @@ class Aptly < Formula
 
   depends_on "go" => :build
 
-  go_resource "github.com/daviddengcn/go-colortext" do
-    url "https://github.com/daviddengcn/go-colortext.git",
-        :revision => "511bcaf42ccd42c38aba7427b6673277bf19e2a1"
-  end
-
-  go_resource "github.com/hashicorp/go-version" do
-    url "https://github.com/hashicorp/go-version.git",
-        :revision => "0181db47023708a38c2d20d2fe25a5fa034d5743"
-  end
-
-  go_resource "github.com/mattn/gover" do
-    url "https://github.com/mattn/gover.git",
-        :revision => "715629d6b57a2104c5221dc72514cfddc992e1de"
-  end
-
-  go_resource "github.com/mattn/gom" do
-    url "https://github.com/mattn/gom.git",
-        :revision => "393e714d663c35e121a47fec32964c44a630219b"
-  end
-
   def install
     ENV["GOPATH"] = buildpath
-    ENV.prepend_create_path "PATH", buildpath/"bin"
-
-    (buildpath/"src/github.com/smira").mkpath
-    ln_s buildpath, buildpath/"src/github.com/smira/aptly"
-
-    Language::Go.stage_deps resources, buildpath/"src"
-
-    cd("src/github.com/mattn/gom") { system "go", "install" }
-
-    system "gom", "-production", "install"
-    system "gom", "build", "-o", bin/"aptly"
+    ENV["GOBIN"] = bin
+    (buildpath/"src/github.com/smira/aptly").install buildpath.children
+    cd "src/github.com/smira/aptly" do
+      system "make", "VERSION=#{version}", "install"
+      prefix.install_metafiles
+    end
   end
 
   test do
