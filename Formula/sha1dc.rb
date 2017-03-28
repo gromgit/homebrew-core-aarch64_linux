12e998d3321196b45eb46af10f3d95acd8fafabc
ilovezfs <ilovezfs@icloud.com>
2017-03-27 22:28:09 -0700

    sha1dc 1.0.3
    Closes #11724.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/sha1dc.rb
+++ Formula/sha1dc.rb
@@ -1,8 +1,8 @@
 class Sha1dc < Formula
   desc "Tool to detect SHA-1 collisions in files, including SHAttered"
   homepage "https://github.com/cr-marcstevens/sha1collisiondetection"
-  url "https://github.com/cr-marcstevens/sha1collisiondetection/archive/stable-v1.0.2.tar.gz"
-  sha256 "ab1a5624d12417b5af9ffe6662546b6932c39c0e4c4bef822851a2d0e53df790"
+  url "https://github.com/cr-marcstevens/sha1collisiondetection/archive/stable-v1.0.3.tar.gz"
+  sha256 "77a1c2b2a4fbe4f78de288fa4831ca63938c3cb84a73a92c79f436238bd9ac07"
 
   # The "master" branch is unusably broken and behind the
   # "simplified_c90" branch that's the basis for release.
