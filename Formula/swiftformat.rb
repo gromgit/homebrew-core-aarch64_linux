588c9dd9fe16e266171288976cce5a191bc55610
Nick Lockwood <nick@charcoaldesign.co.uk>
2017-03-28 18:35:45 +0100

    swiftformat 0.28.1
    Closes #11750.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/swiftformat.rb
+++ Formula/swiftformat.rb
@@ -1,8 +1,8 @@
 class Swiftformat < Formula
   desc "Formatting tool for reformatting Swift code"
   homepage "https://github.com/nicklockwood/SwiftFormat"
-  url "https://github.com/nicklockwood/SwiftFormat/archive/0.28.0.tar.gz"
-  sha256 "a7f7d348e0703a3822aae8b1da83250c8b9d4ccb83e347e891f4b555725dec0a"
+  url "https://github.com/nicklockwood/SwiftFormat/archive/0.28.1.tar.gz"
+  sha256 "433d4f4352defb0c15efc89dcfe9fc0836f7358a2d8ded9dc5f0e1d82e126c08"
 
   head "https://github.com/nicklockwood/SwiftFormat.git", :shallow => false
 
