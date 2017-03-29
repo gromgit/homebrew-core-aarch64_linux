f01cbc70657a9676c1dbd5141977717626bec6b0
ilovezfs <ilovezfs@icloud.com>
2017-03-29 01:53:06 -0700

    riemann: new mirror (#11786)

--- Formula/riemann.rb
+++ Formula/riemann.rb
@@ -2,7 +2,7 @@ class Riemann < Formula
   desc "Event stream processor"
   homepage "http://riemann.io"
   url "https://github.com/riemann/riemann/releases/download/0.2.12/riemann-0.2.12.tar.bz2"
-  mirror "https://wintermute.skunkwerks.at/riemann/releases/0.2.12/riemann-0.2.12.tar.bz2"
+  mirror "https://ftp.openbsd.org/pub/OpenBSD/distfiles/riemann-0.2.12.tar.bz2"
   sha256 "79f21fb2ac90a3d55e7cfa4a1eb14d1bdcdb1266ba6052bb72e6e500467ca7f4"
 
   bottle :unneeded
