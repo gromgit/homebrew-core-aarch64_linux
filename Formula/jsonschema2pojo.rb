9383f0832b18fdf238db25c39fe895ce8a82a66a
ilovezfs <ilovezfs@icloud.com>
2017-03-28 19:09:56 -0700

    jsonschema2pojo 0.4.33
    Closes #11761.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/jsonschema2pojo.rb
+++ Formula/jsonschema2pojo.rb
@@ -1,8 +1,8 @@
 class Jsonschema2pojo < Formula
   desc "Generates Java types from JSON Schema (or example JSON)"
   homepage "http://www.jsonschema2pojo.org/"
-  url "https://github.com/joelittlejohn/jsonschema2pojo/releases/download/jsonschema2pojo-0.4.31/jsonschema2pojo-0.4.31.tar.gz"
-  sha256 "53f8ba01cbde07705efb032c033aa241842436975e85a0e9ca1c47a428a8270d"
+  url "https://github.com/joelittlejohn/jsonschema2pojo/releases/download/jsonschema2pojo-0.4.33/jsonschema2pojo-0.4.33.tar.gz"
+  sha256 "4716531fc86d8e8ff59032c22c542854c0052e9b4956c30a92208c1dad9cc243"
 
   bottle :unneeded
 
