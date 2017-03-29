860fc236a21c011c559d70b69c756deb7505d72c
Gianluca <gianluca@teamdigitale.governo.it>
2017-03-29 18:43:26 +0200

    xmlsectool 2.0.0 (new formula)
    Closes #11812.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Formula/xmlsectool.rb
@@ -0,0 +1,20 @@
+class Xmlsectool < Formula
+  desc "Check schema validity and signature of an XML document."
+  homepage "https://wiki.shibboleth.net/confluence/display/XSTJ2/xmlsectool+V2+Home"
+  url "https://shibboleth.net/downloads/tools/xmlsectool/latest/xmlsectool-2.0.0-bin.zip"
+  sha256 "83ea1e977d947dd5f2f5600a3280bc5d372a4913f0dbce1ddf2c0ca1c869ca57"
+
+  depends_on :java => "1.7+"
+
+  def install
+    prefix.install "doc/LICENSE.TXT"
+    rm_rf "doc"
+    prefix.install_metafiles
+    libexec.install Dir["*"]
+    (bin/"xmlsectool").write_env_script "#{libexec}/xmlsectool.sh", Language::Java.java_home_env
+  end
+
+  test do
+    system "#{bin}/xmlsectool", "--listBlacklist"
+  end
+end
