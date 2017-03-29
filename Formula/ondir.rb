3b9643efd59f2500091d9fb6627124c83ad21afb
ilovezfs <ilovezfs@icloud.com>
2017-03-29 00:10:51 -0700

    ondir: use https for homepage and stable url (#11773)

--- Formula/ondir.rb
+++ Formula/ondir.rb
@@ -1,7 +1,7 @@
 class Ondir < Formula
   desc "Automatically execute scripts as you traverse directories"
-  homepage "http://swapoff.org/ondir.html"
-  url "http://swapoff.org/files/ondir/ondir-0.2.3.tar.gz"
+  homepage "https://swapoff.org/ondir.html"
+  url "https://swapoff.org/files/ondir/ondir-0.2.3.tar.gz"
   sha256 "504a677e5b7c47c907f478d00f52c8ea629f2bf0d9134ac2a3bf0bbe64157ba3"
   head "https://github.com/alecthomas/ondir.git"
 
