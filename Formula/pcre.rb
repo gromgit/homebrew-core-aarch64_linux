ad0350766c4f7502ebd5ccaeef6e24ea4661ac12
ilovezfs <ilovezfs@icloud.com>
2017-03-29 00:13:43 -0700

    pcre: new mirror (#11775)

--- Formula/pcre.rb
+++ Formula/pcre.rb
@@ -2,7 +2,7 @@ class Pcre < Formula
   desc "Perl compatible regular expressions library"
   homepage "http://www.pcre.org/"
   url "https://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.40.tar.bz2"
-  mirror "https://www.mirrorservice.org/sites/downloads.sourceforge.net/p/pc/pcre/pcre/8.40/pcre-8.40.tar.bz2"
+  mirror "https://www.mirrorservice.org/sites/ftp.exim.org/pub/pcre/pcre-8.40.tar.bz2"
   sha256 "00e27a29ead4267e3de8111fcaa59b132d0533cdfdbdddf4b0604279acbcf4f4"
 
   bottle do
