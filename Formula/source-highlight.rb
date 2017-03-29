dcdf0f540f0a1cb3676c304d351797ec4ad20995
ilovezfs <ilovezfs@icloud.com>
2017-03-29 02:24:46 -0700

    source-highlight: new mirror (#11792)

--- Formula/source-highlight.rb
+++ Formula/source-highlight.rb
@@ -3,7 +3,7 @@ class SourceHighlight < Formula
   homepage "https://www.gnu.org/software/src-highlite/"
   url "https://ftpmirror.gnu.org/src-highlite/source-highlight-3.1.8.tar.gz"
   mirror "https://ftp.gnu.org/gnu/src-highlite/source-highlight-3.1.8.tar.gz"
-  mirror "http://mirror.anl.gov/pub/gnu/src-highlite/source-highlight-3.1.8.tar.gz"
+  mirror "https://fossies.org/linux/www/source-highlight-3.1.8.tar.gz"
   sha256 "01336a7ea1d1ccc374201f7b81ffa94d0aecb33afc7d6903ebf9fbf33a55ada3"
   revision 4
 
