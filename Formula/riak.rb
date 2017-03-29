1547cb2da30077e9057b0bafbae17575854d5eb2
ilovezfs <ilovezfs@icloud.com>
2017-03-29 01:10:55 -0700

    riak 2.2.2 (#11783)

--- Formula/riak.rb
+++ Formula/riak.rb
@@ -1,18 +1,11 @@
 class Riak < Formula
   desc "Distributed database"
   homepage "http://basho.com/products/riak-kv/"
-  url "https://s3.amazonaws.com/downloads.basho.com/riak/2.2/2.2.0/osx/10.8/riak-2.2.0-OSX-x86_64.tar.gz"
-  version "2.2.0"
-  sha256 "51ea63d6efaa3bba4efb0ca13de81da2e2662b6691b4132cf552ca7635c8a857"
+  url "https://s3.amazonaws.com/downloads.basho.com/riak/2.2/2.2.2/osx/10.8/riak-2.2.2-OSX-x86_64.tar.gz"
+  version "2.2.2"
+  sha256 "73fef949afe3864559fd4ead4cb368749259fac2162a9c931bed20d131f5b8cb"
 
   bottle :unneeded
-
-  # Broken dylib links (should be fixed in 2.2.1)
-  # https://github.com/basho/eleveldb/issues/236
-
-  # Currently refuses to use non-system OpenSSL
-  # https://github.com/basho/riak/issues/888
-  # depends_on "openssl"
 
   depends_on :macos => :mountain_lion
   depends_on :arch => :x86_64
