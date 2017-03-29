7ae461ed93084a8774ce4b5547e60ce8f97ec2aa
ilovezfs <ilovezfs@icloud.com>
2017-03-29 00:10:26 -0700

    nyancat: remove formula name from description (#11772)

--- Formula/nyancat.rb
+++ Formula/nyancat.rb
@@ -1,5 +1,5 @@
 class Nyancat < Formula
-  desc "Nyancat in your terminal, rendered through ANSI escape sequences."
+  desc "Renders an animated, color, ANSI-text loop of the Poptart Cat"
   homepage "https://github.com/klange/nyancat"
   url "https://github.com/klange/nyancat/archive/1.5.1.tar.gz"
   sha256 "c948c769d230b4e41385173540ae8ab1f36176de689b6e2d6ed3500e9179b50a"
