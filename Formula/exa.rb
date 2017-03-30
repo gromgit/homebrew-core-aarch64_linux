853974f9ab2645fbb5a9a0ec93d3ed5aa168b7d4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-30 10:12:13 +0100

    exa: add 0.4.1 bottle.

--- Formula/exa.rb
+++ Formula/exa.rb
@@ -5,6 +5,13 @@ class Exa < Formula
   sha256 "1005f320aabc776971ac2adb422eaae001d7adafac47f67c9000f94e70c3b56a"
   head "https://github.com/ogham/exa.git"
 
+  bottle do
+    cellar :any
+    sha256 "a56feb823f55ead6683087e9aca48d86b9610896a0a7b738e3359e8f4cb8ce31" => :sierra
+    sha256 "dec140f9b80302b23c231a373daa9993f56d7070b01248b0583eadafe05726aa" => :el_capitan
+    sha256 "36379d3dc4cbfce03e5ebd7f4009240ba78a6388912f43ff57d241411b5d3d96" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "rust" => :build
   depends_on "libgit2" => :recommended
