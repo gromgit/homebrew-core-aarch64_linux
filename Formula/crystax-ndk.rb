e9481cc34407de5818e0ef1c8eead96793ec8cc0
Mike McQuaid <mike@mikemcquaid.com>
2017-03-30 10:06:36 +0100

    crystax-ndk: remove recommend android-sdk dependency.

--- Formula/crystax-ndk.rb
+++ Formula/crystax-ndk.rb
@@ -3,6 +3,7 @@ class CrystaxNdk < Formula
   homepage "https://www.crystax.net/android/ndk"
 
   version "10.3.1"
+  revision 1
 
   if MacOS.prefer_64_bit?
     url "https://www.crystax.net/download/crystax-ndk-#{version}-darwin-x86_64.tar.xz"
@@ -13,11 +14,6 @@ class CrystaxNdk < Formula
   end
 
   bottle :unneeded
-
-  depends_on "android-sdk" => :recommended
-
-  conflicts_with "android-ndk",
-    :because => "both install `ndk-build`, `ndk-gdb`, `ndk-stack`, `ndk-depends` and `ndk-which` binaries"
 
   def install
     bin.mkpath
