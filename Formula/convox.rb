bff2311df9a85f07a6f77b3dba6434d7efdc98d5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-31 07:08:03 +0100

    convox: update 20170330004259 bottle.

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -6,9 +6,9 @@ class Convox < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6cf6e1e73a1c9c293aeda676fa42ff9f2873c4f67cdee1e7d7849ed81807c350" => :sierra
-    sha256 "dea3a7e1036683a9a5ddeb18421d441fcf438485e8b58d744d3edebeb84cf5d4" => :el_capitan
-    sha256 "9eda9ec23034e561d7e9bbc647d4fc0a736ccf72a89b70cbf9868f39aeeb10f4" => :yosemite
+    sha256 "022bc6a576896191b5ceb4b86600157a21e979312b82ada207a3a006ee6e7581" => :sierra
+    sha256 "b1bb469115ed318329fcf027ded6427411897dcf9d474ff53d4b6cf358c789ce" => :el_capitan
+    sha256 "d31f3002eaf3336bdbee2260b7fa20fe9daafbe1de472495e7bf180b5c7ae1dc" => :yosemite
   end
 
   depends_on "go" => :build
