3bac15055f1d816fadb344c8946863827cf6846f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-01 07:40:27 +0100

    nomad: update 0.5.6 bottle.

--- Formula/nomad.rb
+++ Formula/nomad.rb
@@ -7,9 +7,9 @@ class Nomad < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "1fde97603ae3768354da259bacc34e88e7e049feb99b55e733c0b2548c2cc4f0" => :sierra
-    sha256 "fe947e847507b62c3b43941e1737713bc0800fc4c84677772f0e76eab3e20a23" => :el_capitan
-    sha256 "c7c00907f74f9330771bf3f87604f77bf390cb4651e84d1b3eb936e411269d38" => :yosemite
+    sha256 "747763a159d6832e7c0b64d324d8192f555dfab8269d83768955b51751891eef" => :sierra
+    sha256 "a01321e051368e4cc4d214460b5c1d5f4df1d18d6a2bb0653106e378ba2b039d" => :el_capitan
+    sha256 "40f24a34a885622b7a95d99ee32b39a145b5e8ee8452e5d886e8ca4b204715d6" => :yosemite
   end
 
   option "with-dynamic", "Build dynamic binary with CGO_ENABLED=1"
