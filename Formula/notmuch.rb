f95f281fcedbe7f8f0ea003ad64563fb8c6a7a3f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-02 01:43:17 +0100

    notmuch: update 0.24.1 bottle.

--- Formula/notmuch.rb
+++ Formula/notmuch.rb
@@ -7,9 +7,9 @@ class Notmuch < Formula
 
   bottle do
     cellar :any
-    sha256 "2cd03f79d1ed66c8ee03cc30374acb00085f45c91c65be354071502452d95a3c" => :sierra
-    sha256 "762440b2bf20ca96e80d4eafbb7ce1872245fdab148647b75734c8722b8d7db0" => :el_capitan
-    sha256 "62b96187822a1d79389997cbd21552bcd69398e30a0b28c07cd59a3630e0f82a" => :yosemite
+    sha256 "0fb3360c41dfc3ed91420875b3ec74f78c124743e79036c30248f49a4ce9bf45" => :sierra
+    sha256 "087c52f7178efd621eab51187406332380134cc08f2759af4a488294db39bd95" => :el_capitan
+    sha256 "58440c1e81a60042efc6cada19db917d9af7c9094ee7a7dc7cc7be5e1a7f24f4" => :yosemite
   end
 
   option "without-python", "Build without python support"
