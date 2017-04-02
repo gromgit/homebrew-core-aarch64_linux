545f3db38b3cea91cdd295888c3eb64ea31139c3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-02 15:36:28 +0100

    codequery: update 0.19.0 bottle.

--- Formula/codequery.rb
+++ Formula/codequery.rb
@@ -6,9 +6,9 @@ class Codequery < Formula
 
   bottle do
     cellar :any
-    sha256 "0a69d079240d98269069d0c38ca1579685084d975cf6c3e49e92554e2562e0a7" => :sierra
-    sha256 "7b305f7a624567e92d16637863d6d8c16946e8df22ece7502d5bd187a5e8517e" => :el_capitan
-    sha256 "d21a56640656f08060dd3c9e8b1800808f314e90f003a7d51158deb6be5f2a6e" => :yosemite
+    sha256 "7891ec24972adf76df868c0a67d1407e005683530b56ea52cea0a7acc3ddd0c8" => :sierra
+    sha256 "c06e0d6e41de521001c647a20012683c6d91d1ed5eb2d6abe8fef312203236bb" => :el_capitan
+    sha256 "56bddde5976a54be551b3295b82eab02aa16e9497c392e8c5f69b4816e5705fd" => :yosemite
   end
 
   depends_on "cmake" => :build
