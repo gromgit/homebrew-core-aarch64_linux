237dadfcbf4ddcf820beb44c987477ad5fa0dfbd
ilovezfs <ilovezfs@icloud.com>
2017-04-02 00:54:14 -0700

    plustache 0.4.0
    Closes #11958.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/plustache.rb
+++ Formula/plustache.rb
@@ -1,8 +1,8 @@
 class Plustache < Formula
   desc "C++ port of Mustache templating system"
   homepage "https://github.com/mrtazz/plustache"
-  url "https://github.com/mrtazz/plustache/archive/v0.3.0.tar.gz"
-  sha256 "ceb56d6cab81b8ed2d812e4a546036a46dd28685512255e3f52553ba70a15fc8"
+  url "https://github.com/mrtazz/plustache/archive/0.4.0.tar.gz"
+  sha256 "83960c412a7c176664c48ba4d718e72b5d39935b24dc13d7b0f0840b98b06824"
 
   bottle do
     cellar :any
