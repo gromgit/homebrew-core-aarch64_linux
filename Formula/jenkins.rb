0510566ca1bcfad697f1a634b1372a3c59a09eea
ilovezfs <ilovezfs@icloud.com>
2017-04-03 02:58:00 -0700

    jenkins 2.53 (#11992)

--- Formula/jenkins.rb
+++ Formula/jenkins.rb
@@ -1,8 +1,8 @@
 class Jenkins < Formula
   desc "Extendable open source continuous integration server"
   homepage "https://jenkins-ci.org"
-  url "http://mirrors.jenkins-ci.org/war/2.52/jenkins.war"
-  sha256 "7a78fc8b3f1eacd45891d052f7fa03471a60b9df52213b4ee948e05834cffc38"
+  url "http://mirrors.jenkins-ci.org/war/2.53/jenkins.war"
+  sha256 "39c9cbcddd9eaa6f5675ace29c85d9db34b1c96fff4f2e3791c1044beee38f9a"
 
   head do
     url "https://github.com/jenkinsci/jenkins.git"
