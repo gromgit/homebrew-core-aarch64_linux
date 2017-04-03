1049d7aa21a669f9ec8f6c45c0e44eb02d815ca3
Viktor Szakats <vszakats@users.noreply.github.com>
2017-04-03 20:57:11 +0000

    ncurses: secure url in comment, update homepage

--- Formula/ncurses.rb
+++ Formula/ncurses.rb
@@ -1,6 +1,6 @@
 class Ncurses < Formula
   desc "Text-based UI library"
-  homepage "https://www.gnu.org/s/ncurses/"
+  homepage "https://www.gnu.org/software/ncurses/"
   url "https://ftpmirror.gnu.org/ncurses/ncurses-6.0.tar.gz"
   mirror "https://ftp.gnu.org/gnu/ncurses/ncurses-6.0.tar.gz"
   sha256 "f551c24b30ce8bfb6e96d9f59b42fbea30fa3a6123384172f9e7284bcf647260"
@@ -28,7 +28,7 @@ class Ncurses < Formula
     # Fix the build for GCC 5.1
     # error: expected ')' before 'int' in definition of macro 'mouse_trafo'
     # See https://lists.gnu.org/archive/html/bug-ncurses/2014-07/msg00022.html
-    # and http://trac.sagemath.org/ticket/18301
+    # and https://trac.sagemath.org/ticket/18301
     # Disable linemarker output of cpp
     ENV.append "CPPFLAGS", "-P"
 
