adab381e59a4938a96c0ffdfd1a1dbc7d9c9d15a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-03 23:37:37 +0100

    afl-fuzz: update 2.40b bottle.

--- Formula/afl-fuzz.rb
+++ Formula/afl-fuzz.rb
@@ -5,9 +5,9 @@ class AflFuzz < Formula
   sha256 "4d35934a77ceee1f219d70ec6bac927c334540b4d9050f359728e2490dcba46e"
 
   bottle do
-    sha256 "fb5d5790f22074a91d8eb1da73a0752c4ee551fe043d9ce987d80fc993c7efd2" => :sierra
-    sha256 "08ae52c1817603f0e61ccf1036fc09ecdcde1ab3ebec97b6b9578330971799e1" => :el_capitan
-    sha256 "6acbad9ee52f58399e453d06cbfd6c2fe27ee7aa7741f9eeac041222e0fcaba1" => :yosemite
+    sha256 "6ad0e561a360d9776feab1da1d89ddbabc7fd8dfd29db3014ce8df2bc7543450" => :sierra
+    sha256 "dc6f3d74333d311663a112850fa663d7cff2d7a98831cb73af2de3028601beba" => :el_capitan
+    sha256 "01bc90a7ed53e5f40dd791197db38cf3030dee7285815384b1fabd67e8c4b9bb" => :yosemite
   end
 
   def install
