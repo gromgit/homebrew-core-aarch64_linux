96499ad3ed73974825ed2501f88c999cfd1d2f11
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-03 16:36:07 +0100

    awk: add 20121220 bottle.

--- Formula/awk.rb
+++ Formula/awk.rb
@@ -5,6 +5,13 @@ class Awk < Formula
   version "20121220"
   sha256 "8dc092165c5a4e1449f964286483d06d0dbfba4b0bd003cb5dab30de8f6d9b83"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "40dbcdf8549b662e7775074fe8c172f364862705b0c0ba00495c2da6e1440a70" => :sierra
+    sha256 "593cdb489ae25ec556af3a02e26b957efc3a67731f8edf48335a994fa58d533e" => :el_capitan
+    sha256 "d74b6dc04cd9dac3791216744a56813b8396c419f325244526c79c433ca95b3d" => :yosemite
+  end
+
   conflicts_with "gawk",
     :because => "both install awk executables."
 
