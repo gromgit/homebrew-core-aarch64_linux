4245a072f672825eb551929ca03f8c457f2c9567
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-03 16:36:08 +0100

    less: update 481 bottle.

--- Formula/less.rb
+++ Formula/less.rb
@@ -6,10 +6,9 @@ class Less < Formula
   sha256 "3fa38f2cf5e9e040bb44fffaa6c76a84506e379e47f5a04686ab78102090dda5"
 
   bottle do
-    rebuild 2
-    sha256 "5894668335c7ba7b3bce8d7e3db1fd899e05db8a251e9ac39d33dd0be94b6d88" => :sierra
-    sha256 "b64e1e151c141f2d9bd67529e1c877542e337447c9aae4ab42409a38ee06e80d" => :el_capitan
-    sha256 "1a3f2691a70564b5e4935d01fc6760a97a42b7a4cf372b6feb55f197925bf0d9" => :yosemite
+    sha256 "a88b14210f0436da45740bf5dbf5d98559c73a74312c8b77755aa90c819fe974" => :sierra
+    sha256 "1baf4050cc03e2f488fe3ce96fc41985b1e5911b15cd7b964dbdf09aebd3dceb" => :el_capitan
+    sha256 "e0606d693adfae4c2bc3c4050d0f92ed4d588896ceb16e88c05f967e3e0fe90d" => :yosemite
   end
 
   devel do
