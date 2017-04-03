3eec524371b820427529469107af2cdda575b247
Mike McQuaid <mike@mikemcquaid.com>
2017-04-03 08:02:20 +0100

    xmlsectool: bottle unneeded.

--- Formula/xmlsectool.rb
+++ Formula/xmlsectool.rb
@@ -4,6 +4,8 @@ class Xmlsectool < Formula
   url "https://shibboleth.net/downloads/tools/xmlsectool/latest/xmlsectool-2.0.0-bin.zip"
   sha256 "83ea1e977d947dd5f2f5600a3280bc5d372a4913f0dbce1ddf2c0ca1c869ca57"
 
+  bottle :unneeded
+
   depends_on :java => "1.7+"
 
   def install
