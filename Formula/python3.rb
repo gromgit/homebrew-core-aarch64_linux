0c07502e530bae1ca8811a3a27ab0583fbb396b0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-04 12:51:52 +0100

    python3: update 3.6.1 bottle.

--- Formula/python3.rb
+++ Formula/python3.rb
@@ -6,9 +6,10 @@ class Python3 < Formula
   head "https://github.com/python/cpython", :using => :git
 
   bottle do
-    sha256 "23497e9d8b42213b97fd6d4a3fc4a081dc4ae95ff1fe036f6f0b7777b4ff3903" => :sierra
-    sha256 "417acd394c737b79fabd87767d6fcd7fad67b08191e5ee41c10beb2b24d52461" => :el_capitan
-    sha256 "ed333f6d95a0eead4f69a226cb5a0886e0837e56dfe11d8d05042fc2730ad5e5" => :yosemite
+    rebuild 1
+    sha256 "77fddf274eed11c29f7d9dc0b7b1183860fa70ffff00bcf046150a0f65cd675c" => :sierra
+    sha256 "e9060ef5d5e291e55533e2b8e7ce742906e53c2eb1302ed54036301016eefe19" => :el_capitan
+    sha256 "16753928434530a84ec309c0a083ba3da9535b8e7750fd5b2440e77c71c8f7b0" => :yosemite
   end
 
   option "with-tcl-tk", "Use Homebrew's Tk instead of macOS Tk (has optional Cocoa and threads support)"
