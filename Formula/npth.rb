b9d87ffca790fc54ec082d2d4fea8ef02c7e128c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-04 11:30:20 +0100

    npth: update 1.3 bottle.

--- Formula/npth.rb
+++ Formula/npth.rb
@@ -7,11 +7,9 @@ class Npth < Formula
 
   bottle do
     cellar :any
-    sha256 "e7ea90bcf31139dbdbbb496039d19f733a27296a99a968bd2decd9e5500c8eb4" => :sierra
-    sha256 "e0383072b47031a5ca5b129447fe7b0d90a161c78c0dcd91f3d398067262d469" => :el_capitan
-    sha256 "ed46e1fed9a33a4961b32fe2de844d8bbeff8f44d4863a3f2da364bb292ffa59" => :yosemite
-    sha256 "cc6148b47d88580a4d18efc4140ea423189333c564e11c61b8a4764a182ac766" => :mavericks
-    sha256 "277c3d694bda25fa805241d6c6799aaede32bf56393e7f0912a0e1e05940a4e5" => :mountain_lion
+    sha256 "1dd86e0fb9379ec101c85acb6689c43f2df49117ee9494060acb503d24516d09" => :sierra
+    sha256 "c29fe0821faba0cc0b22738a7588d536c263ed0fd8b10ccd151bc0a69f05519f" => :el_capitan
+    sha256 "2c067eec2b441b5edc2119432f02e9e8dde51cc7ed33515357b6c70e1bf7722b" => :yosemite
   end
 
   def install
