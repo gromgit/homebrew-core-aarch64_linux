45b2bfb0281c09d8aab7ce0137125bff05c1883f
FX Coudert <fxcoudert@gmail.com>
2017-04-05 19:38:54 +0200

    xplanet: revision for giflib

--- Formula/xplanet.rb
+++ Formula/xplanet.rb
@@ -3,6 +3,7 @@ class Xplanet < Formula
   homepage "https://xplanet.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/xplanet/xplanet/1.3.1/xplanet-1.3.1.tar.gz"
   sha256 "4380d570a8bf27b81fb629c97a636c1673407f4ac4989ce931720078a90aece7"
+  revision 1
 
   bottle do
     rebuild 1
@@ -42,6 +43,13 @@ class Xplanet < Formula
   patch :p2 do
     url "https://raw.githubusercontent.com/Homebrew/formula-patches/f952f1d/xplanet/xplanet-1.3.1-ntimes.patch"
     sha256 "3f95ba8d5886703afffdd61ac2a0cd147f8d659650e291979f26130d81b18433"
+  end
+
+  # Fix compilation with giflib 5
+  # https://xplanet.sourceforge.io/FUDforum2/index.php?t=msg&th=592
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/master/xplanet/xplanet-1.3.1-giflib5.patch"
+    sha256 "0a88a9c984462659da37db58d003da18a4c21c0f4cd8c5c52f5da2b118576d6e"
   end
 
   def install
