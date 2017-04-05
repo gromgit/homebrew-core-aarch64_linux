08ff0f6b033f4836a542faabd418882041fe214a
FX Coudert <fxcoudert@gmail.com>
2017-04-05 19:38:45 +0200

    sxiv: revision for giflib

--- Formula/sxiv.rb
+++ Formula/sxiv.rb
@@ -4,6 +4,7 @@ class Sxiv < Formula
   url "https://github.com/muennich/sxiv/archive/v1.3.2.tar.gz"
   mirror "https://mirrors.kernel.org/debian/pool/main/s/sxiv/sxiv_1.3.2.orig.tar.gz"
   sha256 "9f5368de8f0f57e78ebe02cb531a31107a993f2769cec51bcc8d70f5c668b653"
+  revision 1
 
   head "https://github.com/muennich/sxiv.git"
 
