29d365c6c20b536f984ee66d4711e191d844131e
FX Coudert <fxcoudert@gmail.com>
2017-04-05 19:38:14 +0200

    gdal: revision for giflib

--- Formula/gdal.rb
+++ Formula/gdal.rb
@@ -3,7 +3,7 @@ class Gdal < Formula
   homepage "http://www.gdal.org/"
   url "http://download.osgeo.org/gdal/1.11.5/gdal-1.11.5.tar.gz"
   sha256 "49f99971182864abed9ac42de10545a92392d88f7dbcfdb11afe449a7eb754fe"
-  revision 1
+  revision 2
 
   bottle do
     rebuild 1
