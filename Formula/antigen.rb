de175f6f9d287780cf96070a0451f6abc35ef05d
John Sockwell <johnsockwell@users.noreply.github.com>
2017-04-05 16:20:06 -0400

    antigen: adjust HEAD (#12104)

--- Formula/antigen.rb
+++ Formula/antigen.rb
@@ -3,7 +3,7 @@ class Antigen < Formula
   homepage "http://antigen.sharats.me/"
   url "https://github.com/zsh-users/antigen/releases/download/v1.4.1/v1.4.1.tar.gz"
   sha256 "a2f30020ff8414341d855e33b6dfdc4827cd2079e1f7297ef65cd9a380e66cd8"
-  head "https://github.com/zsh-users/antigen.git"
+  head "https://github.com/zsh-users/antigen.git", :branch => "develop"
 
   bottle :unneeded
 
