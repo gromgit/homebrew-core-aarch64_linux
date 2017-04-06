7c2820fbce59c4b59f881cd369bb8d7b0a996884
Dominyk Tiller <dominyktiller@gmail.com>
2017-04-06 15:26:00 +0100

    mktorrent: add test

--- Formula/mktorrent.rb
+++ Formula/mktorrent.rb
@@ -17,4 +17,17 @@ class Mktorrent < Formula
     system "make", "USE_PTHREADS=1", "USE_OPENSSL=1", "USE_LONG_OPTIONS=1"
     bin.install "mktorrent"
   end
+
+  test do
+    (testpath/"test.txt").write <<-EOS.undent
+      Injustice anywhere is a threat to justice everywhere.
+    EOS
+
+    system bin/"mktorrent", "-d", "-c", "Martin Luther King Jr", "test.txt"
+    assert_predicate testpath/"test.txt.torrent", :exist?, "Torrent was not created"
+
+    file = File.read(testpath/"test.txt.torrent")
+    output = file.force_encoding("ASCII-8BIT") if file.respond_to?(:force_encoding)
+    assert_match "Martin Luther King Jr", output
+  end
 end
