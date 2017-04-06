3b207c8d69d244d6b3f556605b7175edb2acf85d
ilovezfs <ilovezfs@icloud.com>
2017-04-06 02:06:22 -0700

    flow 0.43.1
    Closes #12134.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/flow.rb
+++ Formula/flow.rb
@@ -1,8 +1,8 @@
 class Flow < Formula
   desc "Static type checker for JavaScript"
   homepage "https://flowtype.org/"
-  url "https://github.com/facebook/flow/archive/v0.43.0.tar.gz"
-  sha256 "b8cbc5e8c8b4334b649608752ca17814612ba8c79fcbb9365cacf869a43b7f74"
+  url "https://github.com/facebook/flow/archive/v0.43.1.tar.gz"
+  sha256 "235492eea867e3d2ea92733a369b4eea196d2404280415b72d8e6cdc1f41b879"
   head "https://github.com/facebook/flow.git"
 
   bottle do
