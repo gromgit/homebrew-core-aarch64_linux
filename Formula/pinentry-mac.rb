e4b279ed793ce911755ffafc4422df97dc5d8780
Dominyk Tiller <dominyktiller@gmail.com>
2017-04-06 14:42:03 +0100

    pinentry-mac: add test

--- Formula/pinentry-mac.rb
+++ Formula/pinentry-mac.rb
@@ -1,12 +1,9 @@
 class PinentryMac < Formula
   desc "Pinentry for GPG on Mac"
   homepage "https://github.com/GPGTools/pinentry-mac"
+  url "https://github.com/GPGTools/pinentry-mac/archive/v0.9.4.tar.gz"
+  sha256 "037ebb010377d3a3879ae2a832cefc4513f5c397d7d887d7b86b4e5d9a628271"
   head "https://github.com/GPGTools/pinentry-mac.git"
-
-  stable do
-    url "https://github.com/GPGTools/pinentry-mac/archive/v0.9.4.tar.gz"
-    sha256 "037ebb010377d3a3879ae2a832cefc4513f5c397d7d887d7b86b4e5d9a628271"
-  end
 
   bottle do
     cellar :any_skip_relocation
@@ -31,5 +28,9 @@ class PinentryMac < Formula
     ~/.gnupg/gpg-agent.conf
         pinentry-program #{HOMEBREW_PREFIX}/bin/pinentry-mac
     EOS
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/pinentry-mac --version")
   end
 end
