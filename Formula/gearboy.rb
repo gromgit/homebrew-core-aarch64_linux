1202aa412d3960bfb66848201080058b881cccd8
Mike McQuaid <mike@mikemcquaid.com>
2017-04-06 09:03:08 +0100

    gearboy: rename qt5 to qt.

--- Formula/gearboy.rb
+++ Formula/gearboy.rb
@@ -3,6 +3,7 @@ class Gearboy < Formula
   homepage "https://github.com/drhelius/Gearboy"
   url "https://github.com/drhelius/Gearboy/archive/gearboy-2.3.1.tar.gz"
   sha256 "5182f74b7d4a76ee3a995968149e8f2b07841225b30193537ffda17dfd6feb01"
+  revision 1
   head "https://github.com/drhelius/Gearboy.git"
 
   bottle do
@@ -12,7 +13,7 @@ class Gearboy < Formula
     sha256 "1e0697e7131d6a8b273055153c60fa4d596e23fc916a6a7bc85c47bc2ac02a5f" => :yosemite
   end
 
-  depends_on "qt5"
+  depends_on "qt"
   depends_on "sdl2"
 
   def install
@@ -21,7 +22,7 @@ class Gearboy < Formula
         s.gsub! "/usr/local/include", Formula["sdl2"].include
         s.gsub! "/usr/local/lib", Formula["sdl2"].lib
       end
-      system "#{Formula["qt5"].bin}/qmake", "PREFIX=#{prefix}", "CONFIG+=c++11"
+      system "#{Formula["qt"].bin}/qmake", "PREFIX=#{prefix}", "CONFIG+=c++11"
       system "make"
       prefix.install "Gearboy.app"
       bin.write_exec_script "#{prefix}/Gearboy.app/Contents/MacOS/Gearboy"
