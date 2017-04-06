d7b7b5b92864842ba24ed693c1c23451c744c179
Alexander Hamann <neutric@users.noreply.github.com>
2017-04-06 16:25:17 +0200

    dbus 1.10.18
    Closes #12149.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/dbus.rb
+++ Formula/dbus.rb
@@ -2,9 +2,9 @@ class Dbus < Formula
   # releases: even (1.10.x) = stable, odd (1.11.x) = development
   desc "Message bus system, providing inter-application communication"
   homepage "https://wiki.freedesktop.org/www/Software/dbus"
-  url "https://dbus.freedesktop.org/releases/dbus/dbus-1.10.16.tar.gz"
-  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/d/dbus/dbus_1.10.16.orig.tar.gz"
-  sha256 "a7b0ba6ea3e8d0e08afec5e3030d0245614268276620c536726f8fa6e5c43388"
+  url "https://dbus.freedesktop.org/releases/dbus/dbus-1.10.18.tar.gz"
+  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/d/dbus/dbus_1.10.18.orig.tar.gz"
+  sha256 "6049ddd5f3f3e2618f615f1faeda0a115104423a7996b7aa73e2f36e38cc514a"
 
   bottle do
     sha256 "63e7eeeb00d00b75294e45932d9a9718672497d0a94061ad992565804f8e8230" => :sierra
