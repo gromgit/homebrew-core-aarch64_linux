78df434eee2b1614603a5b096ce82fd5aefcb25b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-06 14:52:56 +0100

    qjson: update 0.9.0_1 bottle.

--- Formula/qjson.rb
+++ Formula/qjson.rb
@@ -7,9 +7,9 @@ class Qjson < Formula
 
   bottle do
     cellar :any
-    sha256 "cd843f07c21db43f1d38a1651b371a53ccd4c44975c08d56db0d6a4284c9d587" => :sierra
-    sha256 "89d3c3cc1ce2d45b37a6c8001c047b46135111419c27e27377a0158fd0685ebf" => :el_capitan
-    sha256 "4cabfd9f2086b49b21bac869d061b0bd467f8cbef378f16f23c4b3019f655c05" => :yosemite
+    sha256 "bd50e784f99285df8e70448f041c67fe1f8c79f5d6b17f130a2e3a11bc19227d" => :sierra
+    sha256 "befe6eeb2426c2f698dd54999398fa569d91246d239aef3e877680902a20f945" => :el_capitan
+    sha256 "f17d608977669101c13d3f57136d8d8121a0f87e26a0d7a55ee5a21659294355" => :yosemite
   end
 
   depends_on "cmake" => :build
