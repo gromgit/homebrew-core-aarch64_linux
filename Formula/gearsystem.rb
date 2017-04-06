c1a8d7e54a77fa88b18a3edfef23fdaa8011ed3e
Mike McQuaid <mike@mikemcquaid.com>
2017-04-06 09:03:08 +0100

    gearsystem: rename qt5 to qt.

--- Formula/gearsystem.rb
+++ Formula/gearsystem.rb
@@ -3,6 +3,7 @@ class Gearsystem < Formula
   homepage "https://github.com/drhelius/Gearsystem"
   url "https://github.com/drhelius/Gearsystem/archive/gearsystem-2.2.tar.gz"
   sha256 "58004ae6cc7497d466213d2b7f00f2f1abbbd2ea900de228e2f392ceb505984a"
+  revision 1
   head "https://github.com/drhelius/Gearsystem.git"
 
   bottle do
@@ -12,7 +13,7 @@ class Gearsystem < Formula
     sha256 "65b352779cf002a0bdffaed489887e24d3b397d736aeeaa4f09ac2fc653c7832" => :yosemite
   end
 
-  depends_on "qt5"
+  depends_on "qt"
   depends_on "sdl2"
 
   def install
@@ -21,7 +22,7 @@ class Gearsystem < Formula
         s.gsub! "/usr/local/include", Formula["sdl2"].include.to_s
         s.gsub! "/usr/local/lib", Formula["sdl2"].lib.to_s
       end
-      system "#{Formula["qt5"].bin}/qmake", "PREFIX=#{prefix}", "CONFIG+=c++11"
+      system "#{Formula["qt"].bin}/qmake", "PREFIX=#{prefix}", "CONFIG+=c++11"
       system "make"
       prefix.install "Gearsystem.app"
       bin.write_exec_script "#{prefix}/Gearsystem.app/Contents/MacOS/Gearsystem"
