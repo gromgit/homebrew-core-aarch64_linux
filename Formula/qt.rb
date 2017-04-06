b92c519bbd3cb869b4861017210e07fc42a0cc5f
Mike McQuaid <mike@mikemcquaid.com>
2017-04-06 08:58:11 +0100

    qt: rename from qt5.

--- Formula/qt.rb
+++ Formula/qt.rb
@@ -1,9 +1,9 @@
-# Patches for Qt5 must be at the very least submitted to Qt's Gerrit codereview
+# Patches for Qt must be at the very least submitted to Qt's Gerrit codereview
 # rather than their bug-report Jira. The latter is rarely reviewed by Qt.
-class Qt5 < Formula
+class Qt < Formula
   desc "Cross-platform application and UI framework"
   homepage "https://www.qt.io/"
-  revision 1
+  revision 2
   head "https://code.qt.io/qt/qt5.git", :branch => "5.8", :shallow => false
 
   stable do
@@ -54,9 +54,9 @@ class Qt5 < Formula
   # between the 5.5.1 and 5.6.0 releases. (Remove this as soon as feasible!)
   #
   # Core formulae known to fail without this patch (as of 2016-10-15):
-  #   * gnuplot  (with `--with-qt5` option)
-  #   * mkvtoolnix (with `--with-qt5` option, silent build failure)
-  #   * poppler    (with `--with-qt5` option)
+  #   * gnuplot  (with `--with-qt` option)
+  #   * mkvtoolnix (with `--with-qt` option, silent build failure)
+  #   * poppler    (with `--with-qt` option)
   patch do
     url "https://raw.githubusercontent.com/Homebrew/formula-patches/e8fe6567/qt5/restore-pc-files.patch"
     sha256 "48ff18be2f4050de7288bddbae7f47e949512ac4bcd126c2f504be2ac701158b"
@@ -123,14 +123,11 @@ class Qt5 < Formula
     end
 
     # Move `*.app` bundles into `libexec` to expose them to `brew linkapps` and
-    # because we don't like having them in `bin`. Also add a `-qt5` suffix to
-    # avoid conflict with the `*.app` bundles provided by the `qt` formula.
-    # (Note: This move/rename breaks invocation of Assistant via the Help menu
+    # because we don't like having them in `bin`.
+    # (Note: This move breaks invocation of Assistant via the Help menu
     # of both Designer and Linguist as that relies on Assistant being in `bin`.)
     libexec.mkpath
-    Pathname.glob("#{bin}/*.app") do |app|
-      mv app, libexec/"#{app.basename(".app")}-qt5.app"
-    end
+    Pathname.glob("#{bin}/*.app") {|app| mv app, libexec }
   end
 
   def caveats; <<-EOS.undent
