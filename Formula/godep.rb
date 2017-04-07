6cb97d6087f32bcda1cd55ec58656482c1310859
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-07 03:12:08 +0100

    godep: update 79_2 bottle.

--- Formula/godep.rb
+++ Formula/godep.rb
@@ -8,10 +8,9 @@ class Godep < Formula
   head "https://github.com/tools/godep.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "b95f7d571487a25827e078c0540a50769b113ba38c9036f31dc03fc38095b301" => :sierra
-    sha256 "8ecef19b29c614746ee6a53e016b913e1eb690ec979c77ccc8df716fcaa45919" => :el_capitan
-    sha256 "46f1d914fb950c44bcddde06f34f1b556105f2109243169765560c6b099f9bde" => :yosemite
+    sha256 "2787471d1831ac8980437b0818180291bf5dbdb552cc55a0887c9cec19ae77ff" => :sierra
+    sha256 "f24fd0974edb179c3e6ae5c87c006d36b9b329866d41f130447d129e9994d1fe" => :el_capitan
+    sha256 "6f5d428791e587fb4d936404b159ef77f1e97d8537f76c55dd41d095e3160f26" => :yosemite
   end
 
   depends_on "go"
