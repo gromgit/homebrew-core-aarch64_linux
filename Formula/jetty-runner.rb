19c31ab422bd1f91bf574dcb47345d1a06eb8b71
Igor Bolic <igor.bolic@symbolic.rs>
2017-04-07 14:18:04 +0200

    jettyrunner 9.4.3.v20170317 (#12026)

--- Formula/jetty-runner.rb
+++ Formula/jetty-runner.rb
@@ -1,16 +1,36 @@
 class JettyRunner < Formula
   desc "Use Jetty without an installed distribution"
-  homepage "https://www.eclipse.org/jetty/documentation/9.3.x/index.html"
-  url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-runner/9.3.10.v20160621/jetty-runner-9.3.10.v20160621.jar"
-  version "9.3.10.v20160621"
-  sha256 "1dd8773ec7d4cedc15d5f99f13ff68ff04c3917b801942e02d07336a0d011bab"
+  homepage "https://www.eclipse.org/jetty/"
+  url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-runner/9.4.3.v20170317/jetty-runner-9.4.3.v20170317.jar"
+  version "9.4.3.v20170317"
+  sha256 "e4dddfbb313ec115d10faf6b93971ae929f319d366ab3548e5d21be29cd41fbf"
 
   bottle :unneeded
+
+  depends_on :java => "1.8+"
 
   def install
     libexec.install Dir["*"]
 
     bin.mkpath
     bin.write_jar_script libexec/"jetty-runner-#{version}.jar", "jetty-runner"
+  end
+
+  test do
+    ENV.append "_JAVA_OPTIONS", "-Djava.io.tmpdir=#{testpath}"
+    touch "#{testpath}/test.war"
+
+    pid = fork do
+      exec "#{bin}/jetty-runner test.war"
+    end
+    sleep 5
+
+    begin
+      output = shell_output("curl -I http://localhost:8080")
+      assert_match %r{HTTP\/1\.1 200 OK}, output
+    ensure
+      Process.kill 9, pid
+      Process.wait pid
+    end
   end
 end
