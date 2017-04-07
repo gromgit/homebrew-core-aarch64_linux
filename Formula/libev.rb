2de73942712d6bb7cc75bdccd43fa0a2061c0768
Zhiming Wang <zmwangx@gmail.com>
2017-04-07 00:10:42 -0400

    libev: add test (#12170)

--- Formula/libev.rb
+++ Formula/libev.rb
@@ -20,4 +20,37 @@ class Libev < Formula
     # Remove compatibility header to prevent conflict with libevent
     (include/"event.h").unlink
   end
+
+  test do
+    (testpath/"test.c").write <<-'EOS'.undent
+      /* Wait for stdin to become readable, then read and echo the first line. */
+
+      #include <stdio.h>
+      #include <stdlib.h>
+      #include <unistd.h>
+      #include <ev.h>
+
+      ev_io stdin_watcher;
+
+      static void stdin_cb (EV_P_ ev_io *watcher, int revents) {
+        char *buf;
+        size_t nbytes = 255;
+        buf = (char *)malloc(nbytes + 1);
+        getline(&buf, &nbytes, stdin);
+        printf("%s", buf);
+        ev_io_stop(EV_A_ watcher);
+        ev_break(EV_A_ EVBREAK_ALL);
+      }
+
+      int main() {
+        ev_io_init(&stdin_watcher, stdin_cb, STDIN_FILENO, EV_READ);
+        ev_io_start(EV_DEFAULT, &stdin_watcher);
+        ev_run(EV_DEFAULT, 0);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "-I#{include}", "-L#{lib}", "-lev", "-o", "test", "test.c"
+    input = "hello, world\n"
+    assert_equal input, pipe_output("./test", input, 0)
+  end
 end
