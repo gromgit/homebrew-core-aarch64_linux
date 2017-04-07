11ae011899005d0c6064f248443132cb8d4f1a02
ilovezfs <ilovezfs@icloud.com>
2017-04-06 22:05:05 -0700

    pybind11 2.1.1
    Closes #12181.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pybind11.rb
+++ Formula/pybind11.rb
@@ -1,8 +1,8 @@
 class Pybind11 < Formula
   desc "Seamless operability between C++11 and Python"
   homepage "https://github.com/pybind/pybind11"
-  url "https://github.com/pybind/pybind11/archive/v2.1.0.tar.gz"
-  sha256 "2860f2b8d0c9f65f0698289a161385f59d099b7ead1bf64e8993c486f2b93ee0"
+  url "https://github.com/pybind/pybind11/archive/v2.1.1.tar.gz"
+  sha256 "f2c6874f1ea5b4ad4ffffe352413f7d2cd1a49f9050940805c2a082348621540"
 
   bottle do
     cellar :any_skip_relocation
