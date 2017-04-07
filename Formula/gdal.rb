56f96a9e5f36457a3e3ec55e14bef5d08dcdf579
neutric <neutric@users.noreply.github.com>
2017-04-07 09:41:12 +0200

    gdal: fix description (#12186)

--- Formula/gdal.rb
+++ Formula/gdal.rb
@@ -1,5 +1,5 @@
 class Gdal < Formula
-  desc "GDAL: Geospatial Data Abstraction Library"
+  desc "Geospatial Data Abstraction Library"
   homepage "http://www.gdal.org/"
   url "http://download.osgeo.org/gdal/1.11.5/gdal-1.11.5.tar.gz"
   sha256 "49f99971182864abed9ac42de10545a92392d88f7dbcfdb11afe449a7eb754fe"
