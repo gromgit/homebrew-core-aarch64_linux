c57980958c9c81bbe7a5313cb8150d8748c2d375
Thibaud Lepretre <thibaud.lepretre@gmail.com>
2017-04-08 14:36:37 +0200

    maven 3.5.0 (#12228)

--- Formula/maven.rb
+++ Formula/maven.rb
@@ -3,16 +3,9 @@ class Maven < Formula
   homepage "https://maven.apache.org/"
 
   stable do
-    url "https://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
-    mirror "https://archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
-    sha256 "6e3e9c949ab4695a204f74038717aa7b2689b1be94875899ac1b3fe42800ff82"
-  end
-
-  devel do
-    url "https://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.5.0-beta-1/binaries/apache-maven-3.5.0-beta-1-bin.tar.gz"
-    mirror "https://archive.apache.org/dist/maven/maven-3/3.5.0-beta-1/binaries/apache-maven-3.5.0-beta-1-bin.tar.gz"
-    sha256 "26870ab318d8226540fe8fa6f876bb14a85776f15acf516a9375686bb551bcdf"
-    version "3.5.0-beta-1"
+    url "https://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz"
+    mirror "https://archive.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz"
+    sha256 "beb91419245395bd69a4a6edad5ca3ec1a8b64e41457672dc687c173a495f034"
   end
 
   bottle :unneeded
