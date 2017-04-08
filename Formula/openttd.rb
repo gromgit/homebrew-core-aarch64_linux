1ab0405544fb6814917aea87c788b6fd8978dc49
Weilu Jia <weilu.jia@gusto.com>
2017-04-08 00:09:36 -0700

    openttd 1.7.0
    Closes #12222.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/openttd.rb
+++ Formula/openttd.rb
@@ -1,8 +1,8 @@
 class Openttd < Formula
   desc "Simulation game based upon Transport Tycoon Deluxe"
   homepage "https://www.openttd.org/"
-  url "https://binaries.openttd.org/releases/1.6.1/openttd-1.6.1-source.tar.xz"
-  sha256 "9b08996e31c3485ef8dedfa1ab65147091593f3f11bd51eb7662ce5ea41363aa"
+  url "https://binaries.openttd.org/releases/1.7.0/openttd-1.7.0-source.tar.xz"
+  sha256 "df9307f42a45ac57dff23fe5cfb9bdb2a3d676456e7c771de173de060c2a99e0"
 
   head "https://git.openttd.org/openttd/trunk.git"
 
@@ -29,24 +29,6 @@ class Openttd < Formula
   resource "openmsx" do
     url "https://bundles.openttdcoop.org/openmsx/releases/0.3.1/openmsx-0.3.1.zip"
     sha256 "92e293ae89f13ad679f43185e83fb81fb8cad47fe63f4af3d3d9f955130460f5"
-  end
-
-  # Ensures a deployment target is not set on 10.9:
-  # https://bugs.openttd.org/task/6295
-  patch :p0 do
-    url "https://trac.macports.org/export/117147/trunk/dports/games/openttd/files/patch-config.lib-remove-deployment-target.diff"
-    sha256 "95c3d54a109c93dc88a693ab3bcc031ced5d936993f3447b875baa50d4e87dac"
-  end
-
-  # Fixes for 10.11
-  # https://bugs.openttd.org/task/6380
-  patch :p0 do
-    url "https://bugs.openttd.org/task/6380/getfile/10390/patch-src__video__cocoa__wnd_quartz.mm-avoid-removed-cmgetsystemprofile.diff"
-    sha256 "2cf010eb69df588134aceda0eba62cc21e221b6f2dfb7d836869b6edf4bdc093"
-  end
-  patch :p1 do
-    url "https://bugs.openttd.org/task/6380/getfile/10422/cocoa_m.patch"
-    sha256 "cbd559318f653a2e7aaadad2fd7eb1097b24a68ad42cf417c4ca530b34d2a776"
   end
 
   def install
