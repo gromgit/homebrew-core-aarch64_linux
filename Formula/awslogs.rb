5e0329dde764f2d1ea7f304ce263e5f56c9303f7
ilovezfs <ilovezfs@icloud.com>
2017-04-10 05:18:56 -0700

    awslogs 0.8
    Closes #12283.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/awslogs.rb
+++ Formula/awslogs.rb
@@ -3,8 +3,8 @@ class Awslogs < Formula
 
   desc "Simple command-line tool to read AWS CloudWatch logs"
   homepage "https://github.com/jorgebastida/awslogs"
-  url "https://github.com/jorgebastida/awslogs/archive/0.7.tar.gz"
-  sha256 "614b8f6856a51cbe9677d2dfcc5b5c635916783a0dc75f972ba5a35b96359d7b"
+  url "https://github.com/jorgebastida/awslogs/archive/0.8.tar.gz"
+  sha256 "74163ff3dad377de71014d11fd5ffeeb84bf1d8f6a4acbb45f9fae762551b037"
   head "https://github.com/jorgebastida/awslogs.git"
 
   bottle do
@@ -15,27 +15,27 @@ class Awslogs < Formula
   end
 
   resource "boto3" do
-    url "https://pypi.python.org/packages/58/61/50d2e459049c5dbc963473a71fae928ac0e58ffe3fe7afd24c817ee210b9/boto3-1.4.4.tar.gz"
+    url "https://files.pythonhosted.org/packages/58/61/50d2e459049c5dbc963473a71fae928ac0e58ffe3fe7afd24c817ee210b9/boto3-1.4.4.tar.gz"
     sha256 "518f724c4758e5a5bed114fbcbd1cf470a15306d416ff421a025b76f1d390939"
   end
 
   resource "botocore" do
-    url "https://pypi.python.org/packages/ab/cb/c609c845fa4639afde0c8cf02de8f93381dd6d73878bf9f9e5223daa8cfb/botocore-1.5.27.tar.gz"
-    sha256 "1a239a4690a4bab0e0c1c85d02bbd911be3a51dc4fd583e415f79810e504da65"
+    url "https://files.pythonhosted.org/packages/e4/f9/b7026f410edf33b6c10143482b42d0168c7ffdb14dcc89fe0eb4c7f2d7c9/botocore-1.5.38.tar.gz"
+    sha256 "dd6314cee663ccbb96aa115ec188075567df685956e43df22e795cf450d49b11"
   end
 
   resource "docutils" do
-    url "https://pypi.python.org/packages/05/25/7b5484aca5d46915493f1fd4ecb63c38c333bd32aa9ad6e19da8d08895ae/docutils-0.13.1.tar.gz"
+    url "https://files.pythonhosted.org/packages/05/25/7b5484aca5d46915493f1fd4ecb63c38c333bd32aa9ad6e19da8d08895ae/docutils-0.13.1.tar.gz"
     sha256 "718c0f5fb677be0f34b781e04241c4067cbd9327b66bdd8e763201130f5175be"
   end
 
   resource "futures" do
-    url "https://pypi.python.org/packages/55/db/97c1ca37edab586a1ae03d6892b6633d8eaa23b23ac40c7e5bbc55423c78/futures-3.0.5.tar.gz"
+    url "https://files.pythonhosted.org/packages/55/db/97c1ca37edab586a1ae03d6892b6633d8eaa23b23ac40c7e5bbc55423c78/futures-3.0.5.tar.gz"
     sha256 "0542525145d5afc984c88f914a0c85c77527f65946617edb5274f72406f981df"
   end
 
   resource "jmespath" do
-    url "https://pypi.python.org/packages/96/6e/0723cccec195a37de6a428ad8879fe063b6debe5c855444e9285b27d253e/jmespath-0.9.2.tar.gz"
+    url "https://files.pythonhosted.org/packages/96/6e/0723cccec195a37de6a428ad8879fe063b6debe5c855444e9285b27d253e/jmespath-0.9.2.tar.gz"
     sha256 "54c441e2e08b23f12d7fa7d8e6761768c47c969e6aed10eead57505ba760aee9"
   end
 
@@ -45,8 +45,13 @@ class Awslogs < Formula
   end
 
   resource "s3transfer" do
-    url "https://pypi.python.org/packages/8b/13/517e8ec7c13f0bb002be33fbf53c4e3198c55bb03148827d72064426fe6e/s3transfer-0.1.10.tar.gz"
+    url "https://files.pythonhosted.org/packages/8b/13/517e8ec7c13f0bb002be33fbf53c4e3198c55bb03148827d72064426fe6e/s3transfer-0.1.10.tar.gz"
     sha256 "ba1a9104939b7c0331dc4dd234d79afeed8b66edce77bbeeecd4f56de74a0fc1"
+  end
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/b3/b2/238e2590826bfdd113244a40d9d3eb26918bd798fc187e2360a8367068db/six-1.10.0.tar.gz"
+    sha256 "105f8d68616f8248e24bf0e9372ef04d3cc10104f1980f54d57b2ce73a5ad56a"
   end
 
   resource "termcolor" do
@@ -59,6 +64,6 @@ class Awslogs < Formula
   end
 
   test do
-    assert_match "awslogs 0.7.0", shell_output("#{bin}/awslogs --version 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/awslogs --version 2>&1")
   end
 end
