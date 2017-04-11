f321bb7422713843342aab6b59ef14ae70a48d61
Abraham Williams <4braham@gmail.com>
2017-04-11 12:39:19 -0500

    heroku: specify version explicitly
    Fixes #12328.
    
    * Update heroku.rb

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -2,6 +2,7 @@ class Heroku < Formula
   desc "Everything you need to get started with Heroku"
   homepage "https://cli.heroku.com"
   url "https://cli-assets.heroku.com/branches/stable/5.8.5-614a805/heroku-v5.8.5-614a805-darwin-amd64.tar.xz"
+  version "5.8.5"
   sha256 "9f108c351f887866d9887169069740648006459575e4064e7c810c46da27cb0b"
 
   bottle :unneeded
