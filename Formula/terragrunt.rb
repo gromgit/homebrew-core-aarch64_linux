9d268ccac10229757a79bd8a3ab56fe55497fd92
Alexander Hamann <neutric@users.noreply.github.com>
2017-04-11 19:38:17 +0200

    terragrunt 0.11.1
    Closes #12331.
    
    Signed-off-by: Alexander Hamann <neutric@users.noreply.github.com>

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -1,8 +1,8 @@
 class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state."
   homepage "https://github.com/gruntwork-io/terragrunt"
-  url "https://github.com/gruntwork-io/terragrunt/archive/v0.11.0.tar.gz"
-  sha256 "847fb1c8522bc3eb25248f3f626946a7e5d3d1ec79c809952ccbe5441c9ce593"
+  url "https://github.com/gruntwork-io/terragrunt/archive/v0.11.1.tar.gz"
+  sha256 "997276ea4c42d541b570f72a3e69bf2e889382b54f9c1cf38b337ec9ec0553f5"
   head "https://github.com/gruntwork-io/terragrunt.git"
 
   bottle do
