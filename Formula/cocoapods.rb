fae87b0d767b6057f185ad4ad360735fa8911a11
ilovezfs <ilovezfs@icloud.com>
2017-04-11 21:40:59 -0700

    cocoapods 1.2.1
    Closes #12343.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/cocoapods.rb
+++ Formula/cocoapods.rb
@@ -1,20 +1,14 @@
 class Cocoapods < Formula
   desc "The Cocoa Dependency Manager."
   homepage "https://cocoapods.org/"
-  url "https://github.com/CocoaPods/CocoaPods/archive/1.2.0.tar.gz"
-  sha256 "715caded3e7c614b5c80d132f79b005ea4a83136a69077452623698d66ce8b1b"
+  url "https://github.com/CocoaPods/CocoaPods/archive/1.2.1.tar.gz"
+  sha256 "e5a31921f624649ff780d2c146c77fe194be8740de2c18991bb274d56a35ea69"
 
   bottle do
     cellar :any_skip_relocation
     sha256 "bd22a54023e078b33ec533c2dd24eff762337122cd2c06a0cf8a30e7bc5ad38d" => :sierra
     sha256 "9d317193d69c036226df8bf8268238ee4370106a2eb78e1a6d9bd872aa15b65f" => :el_capitan
     sha256 "d31d6d9de2bc4aa582d5804ea9d1f5485d54ea6981f85db55ad69200dab06a25" => :yosemite
-  end
-
-  devel do
-    url "https://github.com/CocoaPods/CocoaPods/archive/1.2.1.rc.1.tar.gz"
-    version "1.2.1.rc.1"
-    sha256 "0081087959a164e44795bcae95dab86868a9cd777ea646b1d0275cabad4862de"
   end
 
   def install
