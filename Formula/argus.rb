074857990a3a3335f7c1204714a69a0efa01d11d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-13 13:36:59 +0100

    argus: update 3.0.8.2 bottle.

--- Formula/argus.rb
+++ Formula/argus.rb
@@ -6,11 +6,9 @@ class Argus < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ed5c7ac2bdbc0b8b8fd3f0dccc878b2618f91c736e9db428bc35c800f06a1519" => :sierra
-    sha256 "422d8a3a75e0c6d4095200c38e33722721acd8826f2833e6fe2269a5dc307c92" => :el_capitan
-    sha256 "7e369883e4ef8eda6efd19eb31ba83f619fc13b758e40ade9f199c696e9e37b5" => :yosemite
-    sha256 "6f6b81839f5f6a23b3a0be14bea615dab821bbe8be7dbfae886871ad41590191" => :mavericks
-    sha256 "10f68e1435d342133a0d0fbcfc878b9f01fd95fa2d83f0e3adff7fdfaa1f3185" => :mountain_lion
+    sha256 "42487c51fa731752e10da402b5fac0f973ee090eaad19f8f4fd52fc5317c9cfb" => :sierra
+    sha256 "ea46f2010610e46c120e2df100d61e01c21ee58627e105273c0e0a76437150e1" => :el_capitan
+    sha256 "b4d359943e8404d7c6a340c36bbc4d42e14a56cd80e17a997114fdc6f76552d8" => :yosemite
   end
 
   def install
