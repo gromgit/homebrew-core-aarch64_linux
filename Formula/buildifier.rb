19e98a443bd45d45265ad27245c0f0eaad2910f3
ilovezfs <ilovezfs@icloud.com>
2017-04-13 00:54:51 -0700

    buildifier 0.4.5
    Closes #12371.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/buildifier.rb
+++ Formula/buildifier.rb
@@ -1,8 +1,8 @@
 class Buildifier < Formula
   desc "Format bazel BUILD files with a standard convention"
   homepage "https://github.com/bazelbuild/buildifier"
-  url "https://github.com/bazelbuild/buildifier/archive/0.4.3.tar.gz"
-  sha256 "f6c092b44639fbb7ba11570c2f38a199d112e278ffba10df3dea5cefee3fe553"
+  url "https://github.com/bazelbuild/buildifier/archive/0.4.5.tar.gz"
+  sha256 "7a732ea12d88ddbf9adc99ff5b5c39bfda53b6286ecc79c3bc082d5f53f46f44"
 
   bottle do
     cellar :any_skip_relocation
