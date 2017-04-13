4a74760c9014b9783204e3dfd769c8c0b4c674e7
ilovezfs <ilovezfs@icloud.com>
2017-04-13 01:02:31 -0700

    vc4asm 0.2.3
    Closes #12379.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/vc4asm.rb
+++ Formula/vc4asm.rb
@@ -1,8 +1,8 @@
 class Vc4asm < Formula
   desc "Macro assembler for Broadcom VideoCore IV aka Raspberry Pi GPU"
   homepage "http://maazl.de/project/vc4asm/doc/index.html"
-  url "https://github.com/maazl/vc4asm/archive/V0.2.2.tar.gz"
-  sha256 "6f3da580aacecfd68219771d28ceddab987d26ca38eb4e38d8a93423e30eacb2"
+  url "https://github.com/maazl/vc4asm/archive/V0.2.3.tar.gz"
+  sha256 "8d5f49f7573d1cc6a7baf7cee5e1833af2a87427ad8176989083c6ba7d034c8c"
 
   bottle do
     cellar :any_skip_relocation
@@ -13,17 +13,24 @@ class Vc4asm < Formula
 
   needs :cxx11
 
+  # Fixes "ar: illegal option combination for -r"
+  # Reported 13 Apr 2017 https://github.com/maazl/vc4asm/issues/18
+  resource "old_makefile" do
+    url "https://raw.githubusercontent.com/maazl/vc4asm/c6991f0/src/Makefile"
+    sha256 "2ea9a9e660e85dace2e9b1c9be17a57c8a91e89259d477f9f63820aee102a2d3"
+  end
+
   def install
     ENV.cxx11
 
-    # Fixes "use of undeclared identifier 'fabs'" and similar errors
-    inreplace "src/AssembleInst.cpp", "#include <cstdlib>",
-                                      "#include <cstdlib>\n#include <cmath>"
+    # Fixes "error: use of undeclared identifier 'errno'"
+    # Reported 13 Apr 2017 https://github.com/maazl/vc4asm/issues/19
+    inreplace "src/utils.cpp", "#include <unistd.h>",
+                               "#include <unistd.h>\n#include <errno.h>"
 
-    cd "src" do
-      system "make"
-    end
-    bin.install %w[bin/vc4asm bin/vc4dis]
+    (buildpath/"src").install resource("old_makefile")
+    system "make", "-C", "src"
+    bin.install "bin/vc4asm", "bin/vc4dis"
     share.install "share/vc4.qinc"
   end
 
