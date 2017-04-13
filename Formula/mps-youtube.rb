e9a47b2b7185aff1a1e64d0e1a0186c31c826fda
Zhiming Wang <zmwangx@gmail.com>
2017-04-13 10:35:06 -0400

    mps-youtube: update resource youtube_dl
    In response to a test failure
    https://bot.brew.sh/job/Homebrew/job/homebrew-core/job/PR-12389/4/testReport/junit/brew-test-bot/sierra/test_mps_youtube/.
    
    Closes #12390.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mps-youtube.rb
+++ Formula/mps-youtube.rb
@@ -5,7 +5,7 @@ class MpsYoutube < Formula
   homepage "https://github.com/mps-youtube/mps-youtube"
   url "https://github.com/mps-youtube/mps-youtube/archive/v0.2.7.1.tar.gz"
   sha256 "917958ab02f8dace9c84974f510bd8838f905814c1a05a91fb1a38d37d19f0e8"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "0fdbafcf9d54293d91247cb2b933e5fe6c3a48264a7c1c8ca69aff1564f93213" => :sierra
@@ -18,13 +18,13 @@ class MpsYoutube < Formula
   depends_on "mplayer" => :optional
 
   resource "pafy" do
-    url "https://pypi.python.org/packages/23/74/0e32a671b445107f34fa785ea2ac3658b0e80aef5446538a6181eba7c2e7/pafy-0.5.3.1.tar.gz"
+    url "https://files.pythonhosted.org/packages/23/74/0e32a671b445107f34fa785ea2ac3658b0e80aef5446538a6181eba7c2e7/pafy-0.5.3.1.tar.gz"
     sha256 "35e64ff495b5d62f31f65a31ac0ca6dc1ab39e1dbde4d07b1e04845a52eceda8"
   end
 
   resource "youtube_dl" do
-    url "https://pypi.python.org/packages/5e/28/f2aeaaf0e2b5a97c8072ebea282f2318c46327a6c07e2854c16744a0e548/youtube_dl-2017.1.31.tar.gz"
-    sha256 "9edabc7881d13849f8a88aca47da7612ed8675b8706b99adca3aa46cadf71c95"
+    url "https://files.pythonhosted.org/packages/19/21/0ff2fe2aabfea1a6ba2fd13275df2d137a703c98f1485ef5fa94cd9f82fa/youtube_dl-2017.4.11.tar.gz"
+    sha256 "940d8541f9b1b6a1a099c60ec0e48cb552fc1f12c7ce69b39495b42c16ee7845"
   end
 
   def install
