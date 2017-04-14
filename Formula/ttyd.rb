a5d985353c752c3a44593310ac539b40656b8fe8
ilovezfs <ilovezfs@icloud.com>
2017-04-14 08:36:13 -0700

    ttyd 1.3.2
    Closes #12441.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/ttyd.rb
+++ Formula/ttyd.rb
@@ -1,9 +1,8 @@
 class Ttyd < Formula
   desc "Command-line tool for sharing terminal over the web"
   homepage "https://github.com/tsl0922/ttyd"
-  url "https://github.com/tsl0922/ttyd/archive/1.3.1.tar.gz"
-  sha256 "7133704cab2a5fbc187d96511fad87c00e220ae8ed6cb83220d39205cb928070"
-  revision 2
+  url "https://github.com/tsl0922/ttyd/archive/1.3.2.tar.gz"
+  sha256 "6fcbdf4e4e630bb72597bcbebd529d57a4de2478040f465e412a6827b73ca46c"
   head "https://github.com/tsl0922/ttyd.git"
 
   bottle do
