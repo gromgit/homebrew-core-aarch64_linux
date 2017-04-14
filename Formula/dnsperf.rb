63961af68f44c37ca07be6b390c5ce51e1842d27
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-14 16:37:05 +0100

    dnsperf: update 2.1.0.0 bottle.

--- Formula/dnsperf.rb
+++ Formula/dnsperf.rb
@@ -7,10 +7,9 @@ class Dnsperf < Formula
 
   bottle do
     cellar :any
-    sha256 "5fef021470831807af35bd6b449027f7d75c2031120725541ae19c37a613020b" => :sierra
-    sha256 "e169ef157bfb88e0161f7c14f2ab7752138175f93c2f05c7e0d00d9afd696f06" => :el_capitan
-    sha256 "54cdcb0c2c6e150031b2ce436494a5a218828058ad37a087203165eb6a24ce4e" => :yosemite
-    sha256 "dc94a7815272721dc7a9d89fbdd133122a5002bb6b9dc84417acd6ec19f1a636" => :mavericks
+    sha256 "022ff860326489a63461893dc00c22854fea6fd63949b48ae44abf37e539c20b" => :sierra
+    sha256 "5e605b056442f7c84a1f7b826ae3fc0e113d12b8ab2174371e004c79acc4336a" => :el_capitan
+    sha256 "8d18fb864b8cb9fd3624e10d178fc11d9bd569863ca9e94ff9fd7afc0ee91afc" => :yosemite
   end
 
   depends_on "bind"
