fe4ca4cbd47f6b5df124eeb832f98bf3fb46a439
darklore <zodiac.brave.story@gmail.com>
2017-04-14 23:19:45 +0900

    hadoop 2.8.0 (#12366)

--- Formula/hadoop.rb
+++ Formula/hadoop.rb
@@ -1,9 +1,9 @@
 class Hadoop < Formula
   desc "Framework for distributed processing of large data sets"
   homepage "https://hadoop.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz"
-  mirror "https://archive.apache.org/dist/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz"
-  sha256 "d489df3808244b906eb38f4d081ba49e50c4603db03efd5e594a1e98b09259c2"
+  url "https://www.apache.org/dyn/closer.cgi?path=hadoop/common/hadoop-2.8.0/hadoop-2.8.0.tar.gz"
+  mirror "https://archive.apache.org/dist/hadoop/common/hadoop-2.8.0/hadoop-2.8.0.tar.gz"
+  sha256 "3c0c6053651970c3ce283c100ee3e4e257c7f2dd7d7f92c98c8b0a3a440c04a0"
 
   bottle :unneeded
 
