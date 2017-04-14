f7b763fa2f4ab3fac1234dc4a8031ab4a3d1b750
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-14 10:10:36 +0100

    gpac: update 0.7.0 bottle.

--- Formula/gpac.rb
+++ Formula/gpac.rb
@@ -14,11 +14,9 @@ class Gpac < Formula
   head "https://github.com/gpac/gpac.git"
 
   bottle do
-    rebuild 1
-    sha256 "30c49c6bfe3dc199eeef5929b43cb5e59eda7139a41ebb7ed62f7755e75ceebc" => :sierra
-    sha256 "3c1ebe6435a0ca2daba610c89db1156e7296413942833184f6303bc72d38c98f" => :el_capitan
-    sha256 "89488b011f9080924e2bc2e4c02c8aed633ab3bb7b1d7418b01e94c9e3136fc6" => :yosemite
-    sha256 "831e4b7c778f7adb373d94b82a01f2aec35d02e1363f3389d67148481a7ecca4" => :mavericks
+    sha256 "597da09d46c30397e67ca45d64c5783972db18e4f50a7a58c75c624d43e8e0d5" => :sierra
+    sha256 "01308c943c8804c2ad077d860e9b58e47ad86beb85a019a85bd4de1fb659c710" => :el_capitan
+    sha256 "2ddd05953278ade434e2bc7ba6e13c3a53767586d45816c7f6a1c7c2fa05df7b" => :yosemite
   end
 
   depends_on "openssl"
