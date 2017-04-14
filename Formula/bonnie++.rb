50648866b15680a6733f5d54101a7d1c9c08c717
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-14 14:40:15 +0100

    bonnie++: update 1.97.3 bottle.

--- Formula/bonnie++.rb
+++ Formula/bonnie++.rb
@@ -6,11 +6,9 @@ class Bonniexx < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7b8b5896322c3510224ae84f4eee50d2e8012cbbc713d7e2dba59c859ec1f183" => :sierra
-    sha256 "4d3c98c842106b28a9ce7de5612600c6ba53d2aea1b4950af942ee56c89cd5af" => :el_capitan
-    sha256 "0e262e7f22ad5b31e7aa3eb197c26e3b0b20bda3e4f5d7650b3c6a5771adf75b" => :yosemite
-    sha256 "9edb4a622920f753243c3ec9dcf8cfa4e9cea31e56656f6c55004a6d74e8821a" => :mavericks
-    sha256 "84cc882f89feb0df9ac06b2f7b3ace5bc817da9bf77b73da2dbb8ca3e1583504" => :mountain_lion
+    sha256 "a907767a514063b80faccd7760b299aac95ee79fc736c210f3215fbfcb106a4c" => :sierra
+    sha256 "d3a7233d84654028c12bd9b177ffa09a83d6cf4a9bbbe76656332472a7cd3e7d" => :el_capitan
+    sha256 "b4aa055bc2828f007a6337319ca18499b8cd6520cc405907f21df3b264a448c7" => :yosemite
   end
 
   def install
