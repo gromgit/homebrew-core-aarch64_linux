d9bcb581efa7de44900e9cc18ceb2cc234dc5023
Dominyk Tiller <dominyktiller@gmail.com>
2017-04-15 18:26:53 +0100

    mysql-connector-c 6.1.9
    Closes #12472.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mysql-connector-c.rb
+++ Formula/mysql-connector-c.rb
@@ -1,8 +1,8 @@
 class MysqlConnectorC < Formula
   desc "MySQL database connector for C applications"
   homepage "https://dev.mysql.com/downloads/connector/c/"
-  url "https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.6-src.tar.gz"
-  sha256 "2222433012c415871958b61bc4f3683e1ebe77e3389f698b267058c12533ea78"
+  url "https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.9-src.tar.gz"
+  sha256 "4e808704443365ad5b649673d03eca8fbebc0a4da9f1f96616c6bd1b1901ab2b"
 
   bottle do
     sha256 "14f816bf3aeea9c0cf7b680112f3ca30b2e6f13b45da176a0f1cf888b3e7bc8b" => :sierra
@@ -13,18 +13,17 @@ class MysqlConnectorC < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on "openssl"
 
   conflicts_with "mysql", "mariadb", "percona-server",
     :because => "both install MySQL client libraries"
 
   def install
-    system "cmake", ".", *std_cmake_args
+    system "cmake", ".", "-DWITH_SSL=system", *std_cmake_args
     system "make", "install"
   end
 
   test do
-    system "#{bin}/mysql_config", "--cflags"
-    system "#{bin}/mysql_config", "--include"
-    system "#{bin}/mysql_config", "--libs"
+    assert_match include.to_s, shell_output("#{bin}/mysql_config --cflags")
   end
 end
