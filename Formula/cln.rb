e324439fdcd0aad9aa6ce63205abcb014bf74d9c
Viktor Szakats <vszakats@users.noreply.github.com>
2017-04-16 02:05:21 +0200

    cln: secure url(s)

--- Formula/cln.rb
+++ Formula/cln.rb
@@ -1,7 +1,7 @@
 class Cln < Formula
   desc "Class Library for Numbers"
-  homepage "http://www.ginac.de/CLN/"
-  url "http://www.ginac.de/CLN/cln-1.3.4.tar.bz2"
+  homepage "https://www.ginac.de/CLN/"
+  url "https://www.ginac.de/CLN/cln-1.3.4.tar.bz2"
   sha256 "2d99d7c433fb60db1e28299298a98354339bdc120d31bb9a862cafc5210ab748"
 
   bottle do
