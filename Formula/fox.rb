02f063cc4c058df522c219b58b0abcefc379da18
Baptiste Fontaine <b@ptistefontaine.fr>
2017-04-17 17:47:51 +0200

    fox 1.6.53
    Closes #12552.
    
    Signed-off-by: Baptiste Fontaine <b@ptistefontaine.fr>

--- Formula/fox.rb
+++ Formula/fox.rb
@@ -1,8 +1,8 @@
 class Fox < Formula
   desc "Toolkit for developing Graphical User Interfaces easily."
   homepage "http://www.fox-toolkit.org/"
-  url "http://ftp.fox-toolkit.org/pub/fox-1.6.51.tar.gz"
-  sha256 "15a99792965d933a4936e48b671c039657546bdec6a318c223ab1131624403d1"
+  url "http://ftp.fox-toolkit.org/pub/fox-1.6.53.tar.gz"
+  sha256 "1fe3d53691dad766f91e288fc7ec0f10ae735127766ed17298c3519591d83806"
 
   bottle do
     cellar :any
