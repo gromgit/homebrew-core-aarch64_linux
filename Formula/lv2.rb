522598190b6aae9d91130cd23000d2122eb40fbd
Baptiste Fontaine <b@ptistefontaine.fr>
2017-04-17 17:24:32 +0200

    lv2 1.14.0

--- Formula/lv2.rb
+++ Formula/lv2.rb
@@ -1,8 +1,8 @@
 class Lv2 < Formula
   desc "Portable plugin standard for audio systems"
   homepage "http://lv2plug.in"
-  url "http://lv2plug.in/spec/lv2-1.12.0.tar.bz2"
-  sha256 "7a4a53138f10ed997174c8bc5a8573d5f5a5d8441aaac2de6cf2178ff90658e9"
+  url "http://lv2plug.in/spec/lv2-1.14.0.tar.bz2"
+  sha256 "b8052683894c04efd748c81b95dd065d274d4e856c8b9e58b7c3da3db4e71d32"
 
   bottle do
     cellar :any_skip_relocation
