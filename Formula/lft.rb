3dfc12c4bb0e692659425bccaf83ab83c04685fc
Baptiste Fontaine <b@ptistefontaine.fr>
2017-04-17 17:30:17 +0200

    lft 3.79
    Closes #12545.
    
    Signed-off-by: Baptiste Fontaine <b@ptistefontaine.fr>

--- Formula/lft.rb
+++ Formula/lft.rb
@@ -1,8 +1,8 @@
 class Lft < Formula
   desc "Layer Four Traceroute (LFT), an advanced traceroute tool"
   homepage "https://pwhois.org/lft/"
-  url "https://pwhois.org/dl/index.who?file=lft-3.78.tar.gz"
-  sha256 "000575891b4094319a56d395c0ec576651fe76b251596ab19d3eb2e5f8d4bf78"
+  url "https://pwhois.org/dl/index.who?file=lft-3.79.tar.gz"
+  sha256 "08e5c7973551b529c850bffbc7152c4e5f0bcb1f07ebbb39151a7dc9a3bf9de0"
 
   bottle do
     cellar :any_skip_relocation
