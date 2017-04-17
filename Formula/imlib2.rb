93263211253bf5665269f9377a27f91d37f5428d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-17 14:26:59 +0100

    imlib2: update 1.4.10 bottle.

--- Formula/imlib2.rb
+++ Formula/imlib2.rb
@@ -5,9 +5,9 @@ class Imlib2 < Formula
   sha256 "3f698cd285cbbfc251c1d6405f249b99fafffafa5e0a5ecf0ca7ae49bbc0a272"
 
   bottle do
-    sha256 "5dc22a4152e58bfe2904fdaaffac11969533ef53ed82f5cd73e426cecfb8a226" => :sierra
-    sha256 "9ab5d77db490a64419a743ec4925f4e7ef3efe0c5d8faa93a86c3c24fe61be1d" => :el_capitan
-    sha256 "90a72c95bc99270c3ae082aca367ecd74b55a60c6d6fe101a4205316ae7c0d70" => :yosemite
+    sha256 "7dca8d4fdbbcf9abc2d6280bf086f5f9d20485897d75194cbb80c8a3dc9012be" => :sierra
+    sha256 "869d92d080f4a99fbcebf35caa675a2b3a92cb3431531589d41f5ecd12b92b01" => :el_capitan
+    sha256 "337b2306f391929e58e876e60151a0f1d0068b154b4b4f6b316f3fce3e660cab" => :yosemite
   end
 
   deprecated_option "without-x" => "without-x11"
