c51a7523f905bc4409c2736588801e6c5700da99
Baptiste Fontaine <b@ptistefontaine.fr>
2017-04-17 17:31:28 +0200

    less 487
    Closes #12546.
    
    Signed-off-by: Baptiste Fontaine <b@ptistefontaine.fr>

--- Formula/less.rb
+++ Formula/less.rb
@@ -1,19 +1,14 @@
 class Less < Formula
   desc "Pager program similar to more"
   homepage "http://www.greenwoodsoftware.com/less/index.html"
-  url "https://ftpmirror.gnu.org/less/less-481.tar.gz"
-  mirror "http://www.greenwoodsoftware.com/less/less-481.tar.gz"
-  sha256 "3fa38f2cf5e9e040bb44fffaa6c76a84506e379e47f5a04686ab78102090dda5"
+  url "https://ftpmirror.gnu.org/less/less-487.tar.gz"
+  mirror "http://www.greenwoodsoftware.com/less/less-487.tar.gz"
+  sha256 "f3dc8455cb0b2b66e0c6b816c00197a71bf6d1787078adeee0bcf2aea4b12706"
 
   bottle do
     sha256 "a88b14210f0436da45740bf5dbf5d98559c73a74312c8b77755aa90c819fe974" => :sierra
     sha256 "1baf4050cc03e2f488fe3ce96fc41985b1e5911b15cd7b964dbdf09aebd3dceb" => :el_capitan
     sha256 "e0606d693adfae4c2bc3c4050d0f92ed4d588896ceb16e88c05f967e3e0fe90d" => :yosemite
-  end
-
-  devel do
-    url "http://www.greenwoodsoftware.com/less/less-487.tar.gz"
-    sha256 "f3dc8455cb0b2b66e0c6b816c00197a71bf6d1787078adeee0bcf2aea4b12706"
   end
 
   depends_on "pcre" => :optional
