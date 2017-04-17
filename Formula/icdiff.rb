04135a4a4e74ffaac34fbcb0d2156e4e73af4080
ilovezfs <ilovezfs@icloud.com>
2017-04-17 00:28:01 -0700

    icdiff 1.9.0 (#12516)

--- Formula/icdiff.rb
+++ Formula/icdiff.rb
@@ -1,8 +1,8 @@
 class Icdiff < Formula
   desc "Improved colored diff"
   homepage "https://github.com/jeffkaufman/icdiff"
-  url "https://github.com/jeffkaufman/icdiff/archive/release-1.8.1.tar.gz"
-  sha256 "57a2f1164e9cce98e44cba35473203a19034e919a69762589779f54f4612d8f7"
+  url "https://github.com/jeffkaufman/icdiff/archive/release-1.9.0.tar.gz"
+  sha256 "ef64fda913c21be229e1ed967c577edcfc917543293c3bbd6d1a5775a84471cb"
 
   bottle :unneeded
 
