5db707cd37f17c78d16af4adb811573ffc664aee
ilovezfs <ilovezfs@icloud.com>
2017-04-17 08:10:05 -0700

    nodebrew 0.9.7 (#12531)

--- Formula/nodebrew.rb
+++ Formula/nodebrew.rb
@@ -1,8 +1,8 @@
 class Nodebrew < Formula
   desc "Node.js version manager"
   homepage "https://github.com/hokaccha/nodebrew"
-  url "https://github.com/hokaccha/nodebrew/archive/v0.9.6.tar.gz"
-  sha256 "e51c811061cca880ef3c87c37902a23367ad5ff84e7d6c248f9f181c15c3bf3c"
+  url "https://github.com/hokaccha/nodebrew/archive/v0.9.7.tar.gz"
+  sha256 "3aa8b0cf30024d105f1ac6921aadf0440bc95bcae43df9d6ec58fc9de8cd352e"
   head "https://github.com/hokaccha/nodebrew.git"
 
   bottle :unneeded
