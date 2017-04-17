e6f72387e0592bc30e71632e78d6290e7f330659
Dave Rice <dave@dericed.com>
2017-04-17 10:08:23 -0400

    qcli 0.8 (new formula)
    Closes #12527.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/qcli.rb
@@ -0,0 +1,36 @@
+class Qcli < Formula
+  desc "Report audiovisual metrics via libavfilter"
+  homepage "https://bavc.org/preserve-media/preservation-tools"
+  url "https://github.com/bavc/qctools/archive/v0.8.tar.gz"
+  sha256 "5362dc8325aeb37e0742a5e5df7b831e7fe82a7b06c72c50463a43a7ad0b56bc"
+  head "https://github.com/bavc/qctools.git"
+
+  depends_on "pkg-config" => :build
+  depends_on "qwt"
+  depends_on "qt"
+  depends_on "ffmpeg"
+
+  def install
+    ENV["QCTOOLS_USE_BREW"]="true"
+
+    cd "Project/QtCreator/qctools-lib" do
+      system "qmake", "qctools-lib.pro"
+      system "make"
+    end
+    cd "Project/QtCreator/qctools-cli" do
+      system "qmake", "qctools-cli.pro"
+      system "make"
+      bin.install "qcli"
+    end
+  end
+
+  test do
+    # Create an example mp4 file
+    mp4out = testpath/"video.mp4"
+    system "ffmpeg", "-filter_complex", "testsrc=rate=1:duration=1", mp4out
+    # Create a qcli report from the mp4
+    qcliout = testpath/"video.mp4.qctools.xml.gz"
+    system bin/"qcli", "-i", mp4out, "-o", qcliout
+    assert qcliout.exist?
+  end
+end
