6ae4e17b336249d93561d10f1e0532a8554b07f3
ilovezfs <ilovezfs@icloud.com>
2017-04-17 17:01:01 -0700

    par2 0.7.0
    Closes #12579.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/par2.rb
+++ Formula/par2.rb
@@ -1,8 +1,8 @@
 class Par2 < Formula
   desc "Parchive: Parity Archive Volume Set for data recovery"
   homepage "https://github.com/Parchive/par2cmdline"
-  url "https://github.com/Parchive/par2cmdline/archive/v0.6.14.tar.gz"
-  sha256 "2fd831ba924d9f0ecd9242ca45551b6995ede1ed281af79aa30e7490d5596e7a"
+  url "https://github.com/Parchive/par2cmdline/archive/v0.7.0.tar.gz"
+  sha256 "d877b728d6d3af422904644310980e4da9eba6685960950fd1cd8277c7b63bd5"
 
   bottle do
     cellar :any_skip_relocation
@@ -14,26 +14,16 @@ class Par2 < Formula
   depends_on "automake" => :build
   depends_on "autoconf" => :build
 
-  # The tests for this version of par2cmdline use the `iflag` option to dd,
-  # which doesn't exist in OS X. `iflag` has been removed as of commit
-  # 855f3096c9f52cd0fbdb9e237c6c9624f7b90ea6, but no release has been cut since
-  # then.
-  patch :p1, :DATA
-
   def install
-    system "aclocal"
-    system "automake", "--add-missing"
-    system "autoconf"
+    system "autoreconf", "-fiv"
     system "./configure", "--prefix=#{prefix}"
-    system "make"
-    system "make", "check"
     system "make", "install"
   end
 
   test do
     # Protect a file with par2.
-    test_file = (testpath/"some-file")
-    File.write(test_file, "file contents")
+    test_file = "some-file"
+    (testpath/test_file).write "file contents"
     system "#{bin}/par2", "create", test_file
 
     # "Corrupt" the file by overwriting, then ask par2 to repair it.
@@ -48,18 +38,3 @@ class Par2 < Formula
     assert File.read(test_file) == "file contents"
   end
 end
-
-__END__
-diff --git a/tests/test20 b/tests/test20
-index cbedaf3..0a5bf50 100755
---- a/tests/test20
-+++ b/tests/test20
-@@ -16,7 +16,7 @@ echo $dashes
- echo $banner
- echo $dashes
-
--dd bs=1000 count=2 iflag=fullblock if=/dev/random of=myfile.dat
-+dd bs=1000 count=2 if=/dev/random of=myfile.dat
-
- banner="Creating PAR 2.0 recovery data"
- dashes=`echo "$banner" | sed s/./-/g`
