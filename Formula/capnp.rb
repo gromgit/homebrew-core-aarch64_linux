eb31e9b9e0093bed561bb4246669672885e7323b
Dominyk Tiller <dominyktiller@gmail.com>
2017-04-18 17:02:58 +0100

    capnp 0.5.3.1
    Closes #12597.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/capnp.rb
+++ Formula/capnp.rb
@@ -1,8 +1,8 @@
 class Capnp < Formula
   desc "Data interchange format and capability-based RPC system"
   homepage "https://capnproto.org/"
-  url "https://capnproto.org/capnproto-c++-0.5.3.tar.gz"
-  sha256 "cdb17c792493bdcd4a24bcd196eb09f70ee64c83a3eccb0bc6534ff560536afb"
+  url "https://capnproto.org/capnproto-c++-0.5.3.1.tar.gz"
+  sha256 "04b5c56805895fd15751167ab04c99fa86602110ea7591ef611695de9923911b"
 
   bottle do
     cellar :any_skip_relocation
@@ -26,9 +26,9 @@ class Capnp < Formula
     gtest = resource("gtest")
     gtest.verify_download_integrity(gtest.fetch)
     inreplace "src/CMakeLists.txt" do |s|
-      s.gsub! "http://googletest.googlecode.com/files/gtest-1.7.0.zip",
+      s.gsub! "https://github.com/google/googletest/archive/release-1.7.0.zip",
               gtest.cached_download
-      s.gsub! "URL_MD5 2d6ec8ccdf5c46b05ba54a9fd1d130d7",
+      s.gsub! "URL_MD5 ef5e700c8a0f3ee123e2e0209b8b4961",
               "URL_HASH SHA256=#{gtest.checksum}"
     end
 
@@ -39,6 +39,11 @@ class Capnp < Formula
   end
 
   test do
-    system "#{bin}/capnp", "--version"
+    file = testpath/"test.capnp"
+    text = "\"Is a happy little duck\""
+
+    file.write Utils.popen_read("#{bin}/capnp id").chomp + ";\n"
+    file.append_lines "const dave :Text = #{text};"
+    assert_match text, shell_output("#{bin}/capnp eval #{file} dave")
   end
 end
