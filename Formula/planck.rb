4b32d3c1879d40d5a1359233fcf21b89fcae08a8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-18 21:31:38 +0100

    planck: update 2.3.0 bottle.

--- Formula/planck.rb
+++ Formula/planck.rb
@@ -7,9 +7,9 @@ class Planck < Formula
 
   bottle do
     cellar :any
-    sha256 "b4318ff7cbcd7fd34087636db868c75ebcb3bdab1fd0788c4db4426963ff994e" => :sierra
-    sha256 "16350f5db666611250cd98507314567af706f1f031f5e8e24320233865184414" => :el_capitan
-    sha256 "5e726041cd50428c818b69d4ad03c5fa78c37ac515a0ea07aefa9899c0b7d84c" => :yosemite
+    sha256 "46fe0421ff1b8f935452a1635b3021c6d345a1a06ca16343faf9450c715415d1" => :sierra
+    sha256 "b611b552f4945adcb45ee0641ab66064e96073ac12b583205fe7fcc0c5fcd9c1" => :el_capitan
+    sha256 "9e192fc4c4d04387568beffb4728fbf3805e8755eeab47e16f09bbc49b8b6865" => :yosemite
   end
 
   depends_on "libzip"
