c1b829f4e31955bc8c9e19227ab7a41e92ab9b77
Dominyk Tiller <dominyktiller@gmail.com>
2017-04-18 16:16:59 +0100

    batik 1.9
    Closes #12592.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/batik.rb
+++ Formula/batik.rb
@@ -1,8 +1,8 @@
 class Batik < Formula
   desc "Java-based toolkit for SVG images"
   homepage "https://xmlgraphics.apache.org/batik/"
-  url "https://www.apache.org/dist/xmlgraphics/batik/binaries/batik-bin-1.8.zip"
-  sha256 "d1e5d6c08ef769bb53289250f17ba2d8b18a803d2a82aa082c8bbfae07c648f0"
+  url "https://www.apache.org/dist/xmlgraphics/batik/binaries/batik-bin-1.9.tar.gz"
+  sha256 "349bf6a4f02ce9f631248b224994e20ffc263fe65c1673c9180daa3584418b75"
 
   bottle :unneeded
 
@@ -11,5 +11,11 @@ class Batik < Formula
     bin.write_jar_script libexec/"batik-rasterizer-#{version}.jar", "batik-rasterizer"
     bin.write_jar_script libexec/"batik-#{version}.jar", "batik"
     bin.write_jar_script libexec/"batik-ttf2svg-#{version}.jar", "batik-ttf2svg"
+  end
+
+  test do
+    system bin/"batik-ttf2svg", "/Library/Fonts/Webdings.ttf", "-autorange",
+           "-o", "Webdings.svg", "-testcard"
+    assert_match "abcdefghijklmnopqrstuvwxyz", File.read("Webdings.svg")
   end
 end
