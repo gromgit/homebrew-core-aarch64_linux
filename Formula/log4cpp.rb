3c89efc8686886e143022d85fc686e9bd37e5d8f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-18 16:34:17 +0100

    log4cpp: update 1.1.2 bottle.

--- Formula/log4cpp.rb
+++ Formula/log4cpp.rb
@@ -6,10 +6,9 @@ class Log4cpp < Formula
 
   bottle do
     cellar :any
-    sha256 "d375586cef8f94bf153f26b32a7db92142619eb390fb6b7e015c2b62c6ea7ed7" => :sierra
-    sha256 "2a54503862126d4d4ac872983286eb77356691bb1df4bc2002b6b9000f12a059" => :el_capitan
-    sha256 "c5792136bff0c40ad7baa89974c5335aa3ff0f414d5db86b46d90c2f8cf28210" => :yosemite
-    sha256 "03fde62de11b82029b872296e675d123dba1895b7a7b0e5431664edcb9815ab5" => :mavericks
+    sha256 "0ec821d25392727392d3d180a2c879f661ffdb93e03855fdba5aef323b78143d" => :sierra
+    sha256 "8fcd11872030aea987ada00df36c30bfc8602e2a1607657a4c84b37a5672f17c" => :el_capitan
+    sha256 "01471407b5381cc03e2911aae4876693295960ca9df12aa37d551890dc859fb0" => :yosemite
   end
 
   def install
