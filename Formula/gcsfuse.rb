c65e6106379ec0f9bc6c63afde38372932dcf2bd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-19 14:11:58 +0100

    gcsfuse: add 0.20.1 bottle.

--- Formula/gcsfuse.rb
+++ Formula/gcsfuse.rb
@@ -5,6 +5,13 @@ class Gcsfuse < Formula
   sha256 "cd79b22e9c00ddb2d747a2c0efb3dc73fd9fd19030767106154a3dab0009ff3f"
   head "https://github.com/GoogleCloudPlatform/gcsfuse.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d30b6dc67105bccc275b1e76f366f14ecdffa9925ad8e0666e8ec12e1aa5c567" => :sierra
+    sha256 "60c67924171752200d7a82119069e0ee7b33f770fb5d020cb3629b0524e3c479" => :el_capitan
+    sha256 "d1b059510b0a0be2ef91e72c21fa65b7ffcde25def7d1bc7693c53bae58c1fe2" => :yosemite
+  end
+
   depends_on :osxfuse
 
   depends_on "go" => :build
