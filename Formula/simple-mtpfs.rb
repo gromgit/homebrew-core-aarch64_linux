7d837bac84b447f8da9af93e205d974e749cd95e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-19 14:11:59 +0100

    simple-mtpfs: add 0.3.0 bottle.

--- Formula/simple-mtpfs.rb
+++ Formula/simple-mtpfs.rb
@@ -4,6 +4,13 @@ class SimpleMtpfs < Formula
   url "https://github.com/phatina/simple-mtpfs/archive/simple-mtpfs-0.3.0.tar.gz"
   sha256 "5556cae4414254b071d79ce656cce866b42fd7ba40ce480abfc3ba4e357cd491"
 
+  bottle do
+    cellar :any
+    sha256 "e73ec4a78592b0fc76d86d3027615e2e8addc8b9a30da9caad433b2d1fced262" => :sierra
+    sha256 "947d0e96fd262e1d493662955b3eb27e247d3fc52ed1e8dc07e58a4fb167892f" => :el_capitan
+    sha256 "64c1df0ab967904c00f8b61d41bd4de70ed75f01902d552594ccac990cba5b24" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
