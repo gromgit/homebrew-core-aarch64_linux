eb9ac748baabb2fd47fe349ca0ba058ae1902cfb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-19 14:11:57 +0100

    bindfs: add 1.13.6 bottle.

--- Formula/bindfs.rb
+++ Formula/bindfs.rb
@@ -4,6 +4,13 @@ class Bindfs < Formula
   url "http://bindfs.org/downloads/bindfs-1.13.6.tar.gz"
   sha256 "3c654c2d5d9780189b87b19002ecdeb32d97914481f5f1f841f7585700f8df97"
 
+  bottle do
+    cellar :any
+    sha256 "8777dbb90778636bdcc29da83ac45799ad14df497db3f6784eead9dfade0f59c" => :sierra
+    sha256 "ea5db902b0ec2ffd77e7e9cdbc291a00b56f50148c76ee264da67f4e57b4b7cc" => :el_capitan
+    sha256 "22a997934238dee06239fcad12a7c888868c3d80465ede781b314f1ee2a21b34" => :yosemite
+  end
+
   head do
     url "https://github.com/mpartel/bindfs.git"
     depends_on "autoconf" => :build
