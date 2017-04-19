b4766a7eb2818c27366a34ccddaae89032ff1d0b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-19 14:12:00 +0100

    xmount: add 0.7.4 bottle.

--- Formula/xmount.rb
+++ Formula/xmount.rb
@@ -5,6 +5,12 @@ class Xmount < Formula
       :tag => "v0.7.4",
       :revision => "ca84080b2c857de6951f154886bb7ed5fa100149"
 
+  bottle do
+    sha256 "4162068d6ede326a5419c795bfb9fb017d084bf561134e753d0bc5ea39c12a08" => :sierra
+    sha256 "6da7e85a81d46691f96b425302a4dbc15b5bbfd41d2e7a12751de2937e3bf8b9" => :el_capitan
+    sha256 "b53e4ac2e5bf222d6f64092d44f2dbdd318b7362db3bb10ab3c114dcbae6837a" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "afflib"
