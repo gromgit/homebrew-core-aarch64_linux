c69823e3e4f713f31d203e8289a049072b0c4a1b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-19 14:11:59 +0100

    s3-backer: add 1.4.3 bottle.

--- Formula/s3-backer.rb
+++ Formula/s3-backer.rb
@@ -4,6 +4,13 @@ class S3Backer < Formula
   url "https://github.com/archiecobbs/s3backer/archive/1.4.3.tar.gz"
   sha256 "bf095e41b368067c766c0831e088e1e93411e29f4482efadaaf44e699ada16f6"
 
+  bottle do
+    cellar :any
+    sha256 "64df94342e386fa4163eba7f44fb3fa8104ae702ef4649e72530ec0a01a67221" => :sierra
+    sha256 "96f82aa40f55286359f3a5c4866eb84a15491b979fc78788eb16cf82bd89947d" => :el_capitan
+    sha256 "33fbbf30c5ce36a3d522e55df99b3fa15c2494b1f3d835ae267e44b77c033827" => :yosemite
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
