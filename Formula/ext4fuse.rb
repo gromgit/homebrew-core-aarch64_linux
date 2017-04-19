2f3eae2532b677179d524f551948779701f3509f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-19 14:11:58 +0100

    ext4fuse: add 0.1.3 bottle.

--- Formula/ext4fuse.rb
+++ Formula/ext4fuse.rb
@@ -6,6 +6,13 @@ class Ext4fuse < Formula
 
   head "https://github.com/gerard/ext4fuse.git"
 
+  bottle do
+    cellar :any
+    sha256 "fe8bbe7cd5362f00ff06ef750926bf349d60563c20b0ecf212778631c8912ba2" => :sierra
+    sha256 "291047c821b7b205d85be853fb005510c6ab01bd4c2a2193c192299b6f049d35" => :el_capitan
+    sha256 "b11f564b7e7c08af0b0a3e9854973d39809bf2d8a56014f4882772b2f7307ac1" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on :osxfuse
 
