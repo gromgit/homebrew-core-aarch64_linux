435211476dc6b1ebde32504a534968e28e739eca
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-19 14:11:57 +0100

    avfs: add 1.0.2 bottle.

--- Formula/avfs.rb
+++ Formula/avfs.rb
@@ -4,6 +4,12 @@ class Avfs < Formula
   url "https://downloads.sourceforge.net/project/avf/avfs/1.0.2/avfs-1.0.2.tar.bz2"
   sha256 "52693953e8290281d441a898040c5ac2989f11bc1fcb83ca5063c37a125a1c9a"
 
+  bottle do
+    sha256 "c5c6efd00433246719f6c48e54500e9fe47fd826be53b01298c5dd1519b682a7" => :sierra
+    sha256 "8ba2cb10205b2c948bdc58f26f39ea2f897059f7523a24cba29a3ddefcb08ae3" => :el_capitan
+    sha256 "1188ba5773929183fc38fb187d8e14c2c50b59e7b5ba652192ac449d359ea516" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "xz" => :recommended # Upstream recommends building with lzma support.
   depends_on "openssl" => :optional
