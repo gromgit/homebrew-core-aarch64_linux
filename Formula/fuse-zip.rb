5ad95eb0591cb5f6900ab5b1c011a12ed90557d8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-19 14:11:58 +0100

    fuse-zip: add 0.4.0_1 bottle.

--- Formula/fuse-zip.rb
+++ Formula/fuse-zip.rb
@@ -7,6 +7,13 @@ class FuseZip < Formula
 
   head "https://code.google.com/p/fuse-zip/", :using => :hg
 
+  bottle do
+    cellar :any
+    sha256 "42b7f614872bb6d904a270cdda17b6cf28cd2b343e83278418b39d885607913e" => :sierra
+    sha256 "698ece5594c8279baedbb205bca7be8508b36e48ae10a8a7ab3aaf13a806712b" => :el_capitan
+    sha256 "6be1b2a0977ee4367ec6d84eee84268226e3a914a9509a9de12aff080642f813" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libzip"
   depends_on :osxfuse
