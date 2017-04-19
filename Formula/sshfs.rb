a643071886190e682d20f7972846cbe352c170d7
ilovezfs <ilovezfs@icloud.com>
2017-04-19 12:42:27 -0700

    sshfs 2.9
    Closes #12641.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/sshfs.rb
+++ Formula/sshfs.rb
@@ -1,8 +1,8 @@
 class Sshfs < Formula
   desc "File system client based on SSH File Transfer Protocol"
   homepage "https://osxfuse.github.io/"
-  url "https://github.com/libfuse/sshfs/releases/download/sshfs_2.8/sshfs-2.8.tar.gz"
-  sha256 "7f689174d02e6b7e2631306fda4fb8e6b4483102d1bce82b3cdafba33369ad22"
+  url "https://github.com/libfuse/sshfs/releases/download/sshfs-2.9/sshfs-2.9.tar.gz"
+  sha256 "46d1e1287ce97255fcb50010355184d8c5585329f73ab1e755217419a8e6e5de"
 
   bottle do
     cellar :any
@@ -19,12 +19,6 @@ class Sshfs < Formula
   depends_on "libtool" => :build
   depends_on :osxfuse
   depends_on "glib"
-
-  # Fixes issue https://github.com/libfuse/sshfs/issues/27
-  patch do
-    url "https://github.com/libfuse/sshfs/commit/e5acfce8eda218d.patch"
-    sha256 "53b165353c944303d0839bbe1bf16c04eaaee2deca89ccff729b1974d14aa8cb"
-  end
 
   def install
     args = %W[
