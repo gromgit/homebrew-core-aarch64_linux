3551b36332638e400a82b46eef41341fa284930b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-19 14:11:57 +0100

    btfs: add 2.13 bottle.

--- Formula/btfs.rb
+++ Formula/btfs.rb
@@ -5,6 +5,13 @@ class Btfs < Formula
   sha256 "494c3e16261e3c7535efa86493e09270dde29cd7121e97c29d226c239cbeabd6"
   head "https://github.com/johang/btfs.git"
 
+  bottle do
+    cellar :any
+    sha256 "dcf77a38114495f29e64983a60cb3d0e62b0fa645cad60c32fb2357d1a4bf60e" => :sierra
+    sha256 "58e90041b8552a2cfc490066330e17839fd848a8fb7b011df4fc4b49fcab927c" => :el_capitan
+    sha256 "44eb8e7c5145aec2088c4edf88718b87886f04497dba22160d033e7d680385bf" => :yosemite
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
