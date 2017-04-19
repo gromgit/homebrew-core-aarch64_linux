8202525426d681d9a4340aa0eb88c3dbb672e949
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-19 14:11:59 +0100

    s3fs: add 1.80 bottle.

--- Formula/s3fs.rb
+++ Formula/s3fs.rb
@@ -6,6 +6,13 @@ class S3fs < Formula
 
   head "https://github.com/s3fs-fuse/s3fs-fuse.git"
 
+  bottle do
+    cellar :any
+    sha256 "ee4073bc8c15f2bef27189c02202e830b71aa13d1570aeb6f0462d01731ab17f" => :sierra
+    sha256 "6f56e9ab765e3531ce375704281f304dd823aee9397abd5258bf4fc4225d3fd0" => :el_capitan
+    sha256 "894aed9c105da125630d90a0e3017390eb60d7929fb942e86ba0dcfa55b38403" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
