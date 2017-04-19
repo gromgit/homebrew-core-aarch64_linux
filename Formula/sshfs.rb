3a657931682db38d9f3ff635178189425b6a6886
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-19 14:11:59 +0100

    sshfs: add 2.8 bottle.

--- Formula/sshfs.rb
+++ Formula/sshfs.rb
@@ -4,6 +4,13 @@ class Sshfs < Formula
   url "https://github.com/libfuse/sshfs/releases/download/sshfs_2.8/sshfs-2.8.tar.gz"
   sha256 "7f689174d02e6b7e2631306fda4fb8e6b4483102d1bce82b3cdafba33369ad22"
 
+  bottle do
+    cellar :any
+    sha256 "e1f2ea35bc0a40e43a803f9b4469d77094ff4abfdbb52db4e836d45e13adf924" => :sierra
+    sha256 "1ca7bbe7e75f714ead1f2e23b6c3c4c2ede8315a99623837b31110aa855186f7" => :el_capitan
+    sha256 "90a1d7b12563a40f21dfd452143da17bff82562b17b8881d7c2d02acc0680149" => :yosemite
+  end
+
   option "without-sshnodelay", "Don't compile NODELAY workaround for ssh"
 
   depends_on "pkg-config" => :build
