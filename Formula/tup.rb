3fe7217d1c3f6e595505669e4490302210b44e53
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-19 14:11:59 +0100

    tup: add 0.7.5 bottle.

--- Formula/tup.rb
+++ Formula/tup.rb
@@ -5,6 +5,13 @@ class Tup < Formula
   sha256 "361b3e069308ce1d9505d1cb927999ac448811a3425c724123e0c48602a9d1e4"
   head "https://github.com/gittup/tup.git"
 
+  bottle do
+    cellar :any
+    sha256 "7cac0f32fced84617e15c3684502089d972be91ed5bcebe3fb382ae1ac4d8762" => :sierra
+    sha256 "fdd04fc05f339b5d87ecb77838538f80bf7f0343be020037feb7101a1966c953" => :el_capitan
+    sha256 "6c898724dee0ed50fda5900ff95957feae31673f8b59c7d58e1a988fbd132a8d" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on :osxfuse
 
