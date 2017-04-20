bd12d2b3624785ac84a755ee46506cfa9c0d7b25
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-20 05:54:39 +0100

    mono: update 4.8.1.0 bottle.

--- Formula/mono.rb
+++ Formula/mono.rb
@@ -5,9 +5,9 @@ class Mono < Formula
   sha256 "18cb38a670e51609c36c687ed90ad42cfedabeffd0a2dc5f7f0c46249eb8dbef"
 
   bottle do
-    sha256 "46c5725563889e50b7d596c68c8b4f64db03bfde04b2e08e0279b8a1471b894d" => :sierra
-    sha256 "79194690365bb49745c0036d0efdfe619de2c6578ddd83539216ff61f6ceac22" => :el_capitan
-    sha256 "91ac33de9793f1724afc4d61f1bb8a073d3cbf682f7996a5a5049c22efef2f99" => :yosemite
+    sha256 "ecd5d2b9f7b291dbaec3d05c29680c8f70bde4808d82320bebe6155cb45c6ab1" => :sierra
+    sha256 "a36188b252d8742d5c9d440d60d5a416328fb3a596b8679a0ec9dd986409e95b" => :el_capitan
+    sha256 "b55ff56c58542d7f771117d511982be934c7d355de97b4e1d61ca55abf05568c" => :yosemite
   end
 
   # xbuild requires the .exe files inside the runtime directories to
