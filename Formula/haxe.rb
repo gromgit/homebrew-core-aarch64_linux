ea1636a8079a74724ca5d68545fee5a42e21b2a2
Andy Li <andy@onthewings.net>
2017-04-20 17:00:27 +0800

    haxe: fix head spec (#12156)

--- Formula/haxe.rb
+++ Formula/haxe.rb
@@ -4,13 +4,17 @@ class Haxe < Formula
   url "https://github.com/HaxeFoundation/haxe.git",
       :tag => "3.4.2",
       :revision => "890f8c70cf23ce6f9fe0fdd0ee514a9699433ca7"
-  head "https://github.com/HaxeFoundation/haxe.git", :branch => "development"
 
   bottle do
     cellar :any
     sha256 "698bb9fe8d6fe5d546949469d082f6877658ad74feff0c7149d22675ce01109e" => :sierra
     sha256 "2b72ad031bc21749162ae4cb2e8cb8488519b1b9998b6fe051a3b4de25cbda8a" => :el_capitan
     sha256 "07da6182e4e2482c941a0b7e5d3ec0bda852ab6702c0067153c02e0c3a3a1925" => :yosemite
+  end
+
+  head do
+    url "https://github.com/HaxeFoundation/haxe.git", :branch => "development"
+    depends_on "opam" => :build
   end
 
   depends_on "ocaml" => :build
@@ -22,9 +26,16 @@ class Haxe < Formula
   def install
     # Build requires targets to be built in specific order
     ENV.deparallelize
-    args = ["OCAMLOPT=ocamlopt.opt"]
-    args << "ADD_REVISION=1" if build.head?
-    system "make", *args
+
+    if build.head?
+      ENV["OPAMROOT"] = buildpath/"opamroot"
+      ENV["OPAMYES"] = "1"
+      system "opam", "init", "--no-setup"
+      system "opam", "install", "ocamlfind"
+      system "opam", "config", "exec", "--", "make", "ADD_REVISION=1"
+    else
+      system "make", "OCAMLOPT=ocamlopt.opt"
+    end
 
     # Rebuild haxelib as a valid binary
     cd "extra/haxelib_src" do
