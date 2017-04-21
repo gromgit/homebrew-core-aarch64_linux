f2e06e5bdce30a6fca433967f468c868d6e76c57
ilovezfs <ilovezfs@icloud.com>
2017-04-21 08:55:54 -0700

    gitlab-ci-multi-runner 9.1.0
    Closes #12732.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gitlab-ci-multi-runner.rb
+++ Formula/gitlab-ci-multi-runner.rb
@@ -4,8 +4,8 @@ class GitlabCiMultiRunner < Formula
   desc "The official GitLab CI runner written in Go"
   homepage "https://gitlab.com/gitlab-org/gitlab-ci-multi-runner"
   url "https://gitlab.com/gitlab-org/gitlab-ci-multi-runner.git",
-      :tag => "v9.0.2",
-      :revision => "fa8b86d3a3299434e8985e0c915321ec56508085"
+      :tag => "v9.1.0",
+      :revision => "0118d891c7ec1a0ff9e92fae57b02780fac75a7b"
   head "https://gitlab.com/gitlab-org/gitlab-ci-multi-runner.git"
 
   bottle do
@@ -23,17 +23,17 @@ class GitlabCiMultiRunner < Formula
   end
 
   resource "prebuilt-x86_64.tar.xz" do
-    url "https://gitlab-ci-multi-runner-downloads.s3.amazonaws.com/v9.0.2/docker/prebuilt-x86_64.tar.xz",
+    url "https://gitlab-ci-multi-runner-downloads.s3.amazonaws.com/v9.1.0/docker/prebuilt-x86_64.tar.xz",
         :using => :nounzip
-    version "9.0.2"
-    sha256 "de46893da40a74b01c584d98dee4fc271b7b62ec68ed6757d0e64956d0ba7205"
+    version "9.1.0"
+    sha256 "b9fa9dfd1b50b327c51c720a5e5eaf587e207a1d213fb0c170f67957cae1acd5"
   end
 
   resource "prebuilt-arm.tar.xz" do
-    url "https://gitlab-ci-multi-runner-downloads.s3.amazonaws.com/v9.0.2/docker/prebuilt-arm.tar.xz",
+    url "https://gitlab-ci-multi-runner-downloads.s3.amazonaws.com/v9.1.0/docker/prebuilt-arm.tar.xz",
         :using => :nounzip
-    version "9.0.2"
-    sha256 "516559f4ef5e1efc0b9e6035655957acd22789c761ecdfd9d5fe82755f100bc6"
+    version "9.1.0"
+    sha256 "e6c97f7748f104a30cf439221dcc12635f22ae6fd751992103ae0f0f6935b7d4"
   end
 
   def install
