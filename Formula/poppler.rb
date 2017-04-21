40efe1af2c9c17e1c3007754add823b7d0a7380b
Alexander Hamann <neutric@users.noreply.github.com>
2017-04-21 19:46:44 +0200

    poppler 0.54.0
    Closes #12738.
    
    Signed-off-by: Alexander Hamann <neutric@users.noreply.github.com>

--- Formula/poppler.rb
+++ Formula/poppler.rb
@@ -1,8 +1,8 @@
 class Poppler < Formula
   desc "PDF rendering library (based on the xpdf-3.0 code base)"
   homepage "https://poppler.freedesktop.org/"
-  url "https://poppler.freedesktop.org/poppler-0.53.0.tar.xz"
-  sha256 "592bf72960c6b5948b67657594b05e72d9a278daf7613c9f3cdff9a5b73096a8"
+  url "https://poppler.freedesktop.org/poppler-0.54.0.tar.xz"
+  sha256 "10cca9a67cc2e6f4f9024192b1067c444218bf94430891f43dc819d78536ca31"
 
   bottle do
     rebuild 1
