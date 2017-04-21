2bf1526842e302de13f069ae70cfc61e0b7edb71
ilovezfs <ilovezfs@icloud.com>
2017-04-21 03:06:53 -0700

    paket 4.5.1 (#12719)

--- Formula/paket.rb
+++ Formula/paket.rb
@@ -1,8 +1,8 @@
 class Paket < Formula
   desc "Dependency manager for .NET with support for NuGet and Git repositories"
   homepage "https://fsprojects.github.io/Paket/"
-  url "https://github.com/fsprojects/Paket/releases/download/4.5.0/paket.exe"
-  sha256 "0ac484faafbb5e41014201f9dff9538f3d209dc77ce440b63e8e51561f8bbfdf"
+  url "https://github.com/fsprojects/Paket/releases/download/4.5.1/paket.exe"
+  sha256 "20a4a1fbb3359682b46cc542e0d35aeda915bfdafc3ccf6270c6eb8c0e98a6a1"
 
   bottle :unneeded
 
