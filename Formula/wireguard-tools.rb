f68c0c01a481497692419b966e2c436bc04657fe
Jason A. Donenfeld <Jason@zx2c4.com>
2017-04-21 06:20:21 +0200

    wireguard 0.0.20170421
    Simple version bump.
    
    Closes #12706.
    
    Signed-off-by: Alexander Hamann <neutric@users.noreply.github.com>

--- Formula/wireguard-tools.rb
+++ Formula/wireguard-tools.rb
@@ -3,8 +3,8 @@ class WireguardTools < Formula
   homepage "https://www.wireguard.io/"
   # Please only update version when the tools have been modified/updated,
   # since the Linux module aspect isn't of utility for us.
-  url "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-0.0.20170324.tar.xz"
-  sha256 "2ec08a5d74cb3a63576f06d3cae695b6b8995acd9665e2fa4da91927b467ca51"
+  url "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-0.0.20170421.tar.xz"
+  sha256 "03c82af774224cd171d000ee4a519b5e474cc6842ac04967773cf77b26750000"
   head "https://git.zx2c4.com/WireGuard", :using => :git
 
   bottle do
