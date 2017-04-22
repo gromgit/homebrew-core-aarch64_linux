4c264bc64e7a95d90ff0106eeadf9f19bef205bf
Tim Brust <ratchet.player@gmx.de>
2017-04-22 16:18:43 +0200

    selenium-server-standalone 3.4.0 (#12766)

--- Formula/selenium-server-standalone.rb
+++ Formula/selenium-server-standalone.rb
@@ -1,8 +1,8 @@
 class SeleniumServerStandalone < Formula
   desc "Browser automation for testing purposes"
   homepage "http://seleniumhq.org/"
-  url "https://selenium-release.storage.googleapis.com/3.3/selenium-server-standalone-3.3.1.jar"
-  sha256 "94a0bd034636a2430d9d52b73b8e29e819af103ab84000de241ca83eb4e142f6"
+  url "https://selenium-release.storage.googleapis.com/3.4/selenium-server-standalone-3.4.0.jar"
+  sha256 "21cbbd775678821b6b72c208b8d59664a4c7381b3c50b008b331914d2834ec8d"
 
   bottle :unneeded
 
