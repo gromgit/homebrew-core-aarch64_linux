43fdf6d4e08b92033a3d6b7a2428baeaacaa0c13
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:25 -0700

    rush: swap GNU url and mirror.

--- Formula/rush.rb
+++ Formula/rush.rb
@@ -1,8 +1,8 @@
 class Rush < Formula
   desc "GNU's Restricted User SHell"
   homepage "https://www.gnu.org/software/rush/"
-  url "https://ftpmirror.gnu.org/rush/rush-1.8.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/rush/rush-1.8.tar.gz"
+  url "https://ftp.gnu.org/gnu/rush/rush-1.8.tar.gz"
+  mirror "https://ftpmirror.gnu.org/rush/rush-1.8.tar.gz"
   sha256 "7f321e5556435b12638982c8d94c587982122f28c220f8bc679d7817035aadef"
 
   bottle do
