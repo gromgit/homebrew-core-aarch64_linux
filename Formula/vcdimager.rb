bef57f8af3877c60f7ddfdd3703aa327a16b4686
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:26 -0700

    vcdimager: swap GNU url and mirror.

--- Formula/vcdimager.rb
+++ Formula/vcdimager.rb
@@ -1,8 +1,8 @@
 class Vcdimager < Formula
   desc "(Super) video CD authoring solution"
   homepage "https://www.gnu.org/software/vcdimager/"
-  url "https://ftpmirror.gnu.org/vcdimager/vcdimager-0.7.24.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/vcdimager/vcdimager-0.7.24.tar.gz"
+  url "https://ftp.gnu.org/gnu/vcdimager/vcdimager-0.7.24.tar.gz"
+  mirror "https://ftpmirror.gnu.org/vcdimager/vcdimager-0.7.24.tar.gz"
   sha256 "075d7a67353ff3004745da781435698b6bc4a053838d0d4a3ce0516d7d974694"
   revision 1
 
