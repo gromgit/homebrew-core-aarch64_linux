93caa6ff83c01bc77ac5e8a984908e09fa064d37
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-23 21:42:58 +0100

    vim: update 8.0.0586 bottle.

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -6,9 +6,9 @@ class Vim < Formula
   head "https://github.com/vim/vim.git"
 
   bottle do
-    sha256 "2c6530c5130d306b10e95767073ee9264df89543ae21d7fd35511309cc91314c" => :sierra
-    sha256 "06488acab16e6f9e7cfe42a00d3b5b4e06cef5da52e3673f59eeccf0d257e0ea" => :el_capitan
-    sha256 "5a973fa28c8d194880e95712b087b46051459bbb56e4720b9ac85ddf966983fc" => :yosemite
+    sha256 "0067bb3a58049056b900b3ef436abab8dc3730417c0ed3898855be256343cf29" => :sierra
+    sha256 "ad1a08476c92af0859275bf7d8725cb5c9b6e0b98da72d65065220d150b93e07" => :el_capitan
+    sha256 "4aa42fa04914d4c7dde342ee2eddc9d3da8c2c9b167d151433ffc4c3aad3ff94" => :yosemite
   end
 
   deprecated_option "override-system-vi" => "with-override-system-vi"
