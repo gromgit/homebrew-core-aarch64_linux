1d03fee7f110509b972b256606e4c00090590531
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:26 -0700

    vice: swap GNU url and mirror.

--- Formula/vice.rb
+++ Formula/vice.rb
@@ -27,8 +27,8 @@ class Vice < Formula
 
   # needed to avoid Makefile errors with the vendored ffmpeg 2.4.2
   resource "make" do
-    url "https://ftpmirror.gnu.org/make/make-4.2.1.tar.bz2"
-    mirror "https://ftp.gnu.org/gnu/make/make-4.2.1.tar.bz2"
+    url "https://ftp.gnu.org/gnu/make/make-4.2.1.tar.bz2"
+    mirror "https://ftpmirror.gnu.org/make/make-4.2.1.tar.bz2"
     sha256 "d6e262bf3601b42d2b1e4ef8310029e1dcf20083c5446b4b7aa67081fdffc589"
   end
 
