7946e3cf6d3f25bcc3de9c5653a11dbb97741226
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:14 -0700

    automake@1.12: swap GNU url and mirror.

--- Formula/automake@1.12.rb
+++ Formula/automake@1.12.rb
@@ -1,8 +1,8 @@
 class AutomakeAT112 < Formula
   desc "Tool for generating GNU Standards-compliant Makefiles"
   homepage "https://www.gnu.org/software/automake/"
-  url "https://ftpmirror.gnu.org/automake/automake-1.12.6.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/automake/automake-1.12.6.tar.gz"
+  url "https://ftp.gnu.org/gnu/automake/automake-1.12.6.tar.gz"
+  mirror "https://ftpmirror.gnu.org/automake/automake-1.12.6.tar.gz"
   sha256 "0cbe570db487908e70af7119da85ba04f7e28656b26f717df0265ae08defd9ef"
   revision 1
 
