73592dd95b9f3b104c764231e68feb21bbeeb60d
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:19 -0700

    gnu-which: swap GNU url and mirror.

--- Formula/gnu-which.rb
+++ Formula/gnu-which.rb
@@ -2,8 +2,8 @@ class GnuWhich < Formula
   desc "GNU implementation of which utility"
   # Previous homepage is dead. Have linked to the GNU Projects page for now.
   homepage "https://savannah.gnu.org/projects/which/"
-  url "https://ftpmirror.gnu.org/which/which-2.21.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/which/which-2.21.tar.gz"
+  url "https://ftp.gnu.org/gnu/which/which-2.21.tar.gz"
+  mirror "https://ftpmirror.gnu.org/which/which-2.21.tar.gz"
   sha256 "f4a245b94124b377d8b49646bf421f9155d36aa7614b6ebf83705d3ffc76eaad"
 
   bottle do
