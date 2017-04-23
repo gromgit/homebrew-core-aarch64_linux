979f14a802a77f2585a722bd5eda265f7e859cb7
Dan <dan@wadoku.de>
2017-04-23 16:12:23 +0200

    valabind: patch to support BSD sed
    Closes #12799.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/valabind.rb
+++ Formula/valabind.rb
@@ -1,11 +1,27 @@
 class Valabind < Formula
   desc "Vala bindings for radare, reverse engineering framework"
   homepage "https://radare.org/"
-  url "https://www.radare.org/get/valabind-0.10.0.tar.gz"
-  sha256 "35517455b4869138328513aa24518b46debca67cf969f227336af264b8811c19"
-  revision 3
+  revision 4
 
   head "https://github.com/radare/valabind.git"
+
+  stable do
+    url "https://www.radare.org/get/valabind-0.10.0.tar.gz"
+    sha256 "35517455b4869138328513aa24518b46debca67cf969f227336af264b8811c19"
+    # patch necessary to support vala 0.36.0
+    # remove upon next release
+    patch do
+      url "https://github.com/radare/valabind/commit/f23ff9421c1875d18b1e558596557009b45faa19.patch"
+      sha256 "9fe8f9485e1a4f52c68831670b880efcbbae33c6bc70e67297a00cc1d2fe0d4f"
+    end
+
+    # patch to support BSD sed
+    # remove upon next release
+    patch do
+      url "https://github.com/radare/valabind/commit/03762a0fca7ff4bbfe3e668f70bb75422e05ac07.patch"
+      sha256 "13c5712ed5b081135d1e2df0dd04e1a05856b76de1e0acfef838c2fbc76964c5"
+    end
+  end
 
   bottle do
     cellar :any
@@ -17,13 +33,6 @@ class Valabind < Formula
   depends_on "pkg-config" => :build
   depends_on "swig" => :run
   depends_on "vala"
-
-  # patch necessary to support vala 0.36.0
-  # remove upon next release
-  patch do
-    url "https://github.com/radare/valabind/commit/f23ff9421c1875d18b1e558596557009b45faa19.patch"
-    sha256 "9fe8f9485e1a4f52c68831670b880efcbbae33c6bc70e67297a00cc1d2fe0d4f"
-  end
 
   def install
     system "make"
