46ea62b2cc513a3d1ca73bcb409842efe1e77275
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:18 -0700

    gnu-shogi: swap GNU url and mirror.

--- Formula/gnu-shogi.rb
+++ Formula/gnu-shogi.rb
@@ -1,8 +1,8 @@
 class GnuShogi < Formula
   desc "GNU Shogi (Japanese Chess)"
   homepage "https://www.gnu.org/software/gnushogi/"
-  url "https://ftpmirror.gnu.org/gnushogi/gnushogi-1.4.2.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/gnushogi/gnushogi-1.4.2.tar.gz"
+  url "https://ftp.gnu.org/gnu/gnushogi/gnushogi-1.4.2.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gnushogi/gnushogi-1.4.2.tar.gz"
   sha256 "1ecc48a866303c63652552b325d685e7ef5e9893244080291a61d96505d52b29"
 
   bottle do
