9b54827d2a08e59cfd2f681d5a66499cfae8b97a
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:22 -0700

    libosip: swap GNU url and mirror.

--- Formula/libosip.rb
+++ Formula/libosip.rb
@@ -1,8 +1,8 @@
 class Libosip < Formula
   desc "Implementation of the eXosip2 stack"
   homepage "https://www.gnu.org/software/osip/"
-  url "https://ftpmirror.gnu.org/osip/libosip2-5.0.0.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/osip/libosip2-5.0.0.tar.gz"
+  url "https://ftp.gnu.org/gnu/osip/libosip2-5.0.0.tar.gz"
+  mirror "https://ftpmirror.gnu.org/osip/libosip2-5.0.0.tar.gz"
   sha256 "18a13c954f7297978e7bf1a0cdadde7c531e519d61a045dae304e054f3b2df03"
 
   bottle do
