dcfaaa80781840212d21acff2f2bab7e274c839f
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:14 -0700

    bison@2.7: swap GNU url and mirror.

--- Formula/bison@2.7.rb
+++ Formula/bison@2.7.rb
@@ -1,8 +1,8 @@
 class BisonAT27 < Formula
   desc "Parser generator"
   homepage "https://www.gnu.org/software/bison/"
-  url "https://ftpmirror.gnu.org/bison/bison-2.7.1.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/bison/bison-2.7.1.tar.gz"
+  url "https://ftp.gnu.org/gnu/bison/bison-2.7.1.tar.gz"
+  mirror "https://ftpmirror.gnu.org/bison/bison-2.7.1.tar.gz"
   sha256 "08e2296b024bab8ea36f3bb3b91d071165b22afda39a17ffc8ff53ade2883431"
 
   bottle do
