bd5eab59e50e6935de50364f7bb4389c79df6a5a
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:26 -0700

    wget: swap GNU url and mirror.

--- Formula/wget.rb
+++ Formula/wget.rb
@@ -3,8 +3,8 @@
 class Wget < Formula
   desc "Internet file retriever"
   homepage "https://www.gnu.org/software/wget/"
-  url "https://ftpmirror.gnu.org/wget/wget-1.19.1.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/wget/wget-1.19.1.tar.gz"
+  url "https://ftp.gnu.org/gnu/wget/wget-1.19.1.tar.gz"
+  mirror "https://ftpmirror.gnu.org/wget/wget-1.19.1.tar.gz"
   sha256 "9e4f12da38cc6167d0752d934abe27c7b1599a9af294e73829be7ac7b5b4da40"
 
   bottle do
