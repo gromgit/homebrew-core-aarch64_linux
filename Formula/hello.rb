45a0ff3c516aa3c4d173faf83773d6abe738f90a
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:20 -0700

    hello: swap GNU url and mirror.

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -1,7 +1,7 @@
 class Hello < Formula
   desc "Program providing model for GNU coding standards and practices"
   homepage "https://www.gnu.org/software/hello/"
-  url "https://ftpmirror.gnu.org/hello/hello-2.10.tar.gz"
+  url "https://ftp.gnu.org/gnu/hello/hello-2.10.tar.gz"
   sha256 "31e066137a962676e89f69d1b65382de95a7ef7d914b8cb956f41ea72e0f516b"
 
   bottle do
