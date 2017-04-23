51fde72c428d44332aeb733dae8f992a391906e9
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:15 -0700

    direvent: swap GNU url and mirror.

--- Formula/direvent.rb
+++ Formula/direvent.rb
@@ -1,8 +1,8 @@
 class Direvent < Formula
   desc "Monitors events in the file system directories"
   homepage "http://www.gnu.org.ua/software/direvent/direvent.html"
-  url "https://ftpmirror.gnu.org/direvent/direvent-5.1.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/direvent/direvent-5.1.tar.gz"
+  url "https://ftp.gnu.org/gnu/direvent/direvent-5.1.tar.gz"
+  mirror "https://ftpmirror.gnu.org/direvent/direvent-5.1.tar.gz"
   sha256 "c461600d24183563a4ea47c2fd806037a43354ea68014646b424ac797a959bdb"
 
   bottle do
