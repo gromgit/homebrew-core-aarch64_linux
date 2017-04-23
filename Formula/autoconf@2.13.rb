0bf0762333ae3308ec503dec89b275cf87392db9
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:13 -0700

    autoconf@2.13: swap GNU url and mirror.

--- Formula/autoconf@2.13.rb
+++ Formula/autoconf@2.13.rb
@@ -1,8 +1,8 @@
 class AutoconfAT213 < Formula
   desc "Automatic configure script builder"
   homepage "https://www.gnu.org/software/autoconf/"
-  url "https://ftpmirror.gnu.org/autoconf/autoconf-2.13.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/autoconf/autoconf-2.13.tar.gz"
+  url "https://ftp.gnu.org/gnu/autoconf/autoconf-2.13.tar.gz"
+  mirror "https://ftpmirror.gnu.org/autoconf/autoconf-2.13.tar.gz"
   sha256 "f0611136bee505811e9ca11ca7ac188ef5323a8e2ef19cffd3edb3cf08fd791e"
 
   bottle do
