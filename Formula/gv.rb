95601e5d519a4696a69e8ac53386e5e8fadcc94b
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:20 -0700

    gv: swap GNU url and mirror.

--- Formula/gv.rb
+++ Formula/gv.rb
@@ -1,8 +1,8 @@
 class Gv < Formula
   desc "View and navigate through PostScript and PDF documents"
   homepage "https://www.gnu.org/s/gv/"
-  url "https://ftpmirror.gnu.org/gv/gv-3.7.4.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/gv/gv-3.7.4.tar.gz"
+  url "https://ftp.gnu.org/gnu/gv/gv-3.7.4.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gv/gv-3.7.4.tar.gz"
   sha256 "2162b3b3a95481d3855b3c4e28f974617eef67824523e56e20b56f12fe201a61"
 
   bottle do
