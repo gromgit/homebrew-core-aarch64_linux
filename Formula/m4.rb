661fa374d2739ac537a47e8ed8823e751434dd5a
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:23 -0700

    m4: swap GNU url and mirror.

--- Formula/m4.rb
+++ Formula/m4.rb
@@ -1,8 +1,8 @@
 class M4 < Formula
   desc "Macro processing language"
   homepage "https://www.gnu.org/software/m4"
-  url "https://ftpmirror.gnu.org/m4/m4-1.4.18.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.xz"
+  url "https://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.xz"
+  mirror "https://ftpmirror.gnu.org/m4/m4-1.4.18.tar.xz"
   sha256 "f2c1e86ca0a404ff281631bdc8377638992744b175afb806e25871a24a934e07"
 
   bottle do
