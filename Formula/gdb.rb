17e1bc8f9bdd9c50677b179eeb089898b2581e6b
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:17 -0700

    gdb: swap GNU url and mirror.

--- Formula/gdb.rb
+++ Formula/gdb.rb
@@ -14,8 +14,8 @@ end
 class Gdb < Formula
   desc "GNU debugger"
   homepage "https://www.gnu.org/software/gdb/"
-  url "https://ftpmirror.gnu.org/gdb/gdb-7.12.1.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/gdb/gdb-7.12.1.tar.xz"
+  url "https://ftp.gnu.org/gnu/gdb/gdb-7.12.1.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gdb/gdb-7.12.1.tar.xz"
   sha256 "4607680b973d3ec92c30ad029f1b7dbde3876869e6b3a117d8a7e90081113186"
 
   bottle do
