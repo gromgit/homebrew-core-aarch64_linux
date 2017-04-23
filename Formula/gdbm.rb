790c20d28f1fb041af3ea39301d10d043d957dd0
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:17 -0700

    gdbm: swap GNU url and mirror.

--- Formula/gdbm.rb
+++ Formula/gdbm.rb
@@ -1,8 +1,8 @@
 class Gdbm < Formula
   desc "GNU database manager"
   homepage "https://www.gnu.org/software/gdbm/"
-  url "https://ftpmirror.gnu.org/gdbm/gdbm-1.13.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/gdbm/gdbm-1.13.tar.gz"
+  url "https://ftp.gnu.org/gnu/gdbm/gdbm-1.13.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gdbm/gdbm-1.13.tar.gz"
   sha256 "9d252cbd7d793f7b12bcceaddda98d257c14f4d1890d851c386c37207000a253"
 
   bottle do
