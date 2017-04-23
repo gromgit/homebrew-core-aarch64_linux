33d8d270e552d0a1a09f726366f7602cdde11536
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:16 -0700

    enscript: swap GNU url and mirror.

--- Formula/enscript.rb
+++ Formula/enscript.rb
@@ -1,8 +1,8 @@
 class Enscript < Formula
   desc "Convert text to Postscript, HTML, or RTF, with syntax highlighting"
   homepage "https://www.gnu.org/software/enscript/"
-  url "https://ftpmirror.gnu.org/enscript/enscript-1.6.6.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/enscript/enscript-1.6.6.tar.gz"
+  url "https://ftp.gnu.org/gnu/enscript/enscript-1.6.6.tar.gz"
+  mirror "https://ftpmirror.gnu.org/enscript/enscript-1.6.6.tar.gz"
   sha256 "6d56bada6934d055b34b6c90399aa85975e66457ac5bf513427ae7fc77f5c0bb"
 
   head "https://git.savannah.gnu.org/git/enscript.git"
