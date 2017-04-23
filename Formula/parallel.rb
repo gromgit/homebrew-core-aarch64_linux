8a53962620f27de3b0b3613930bbee19cef0dc59
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:24 -0700

    parallel: swap GNU url and mirror.

--- Formula/parallel.rb
+++ Formula/parallel.rb
@@ -1,8 +1,8 @@
 class Parallel < Formula
   desc "GNU parallel shell command"
   homepage "https://savannah.gnu.org/projects/parallel/"
-  url "https://ftpmirror.gnu.org/parallel/parallel-20170322.tar.bz2"
-  mirror "https://ftp.gnu.org/gnu/parallel/parallel-20170322.tar.bz2"
+  url "https://ftp.gnu.org/gnu/parallel/parallel-20170322.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/parallel/parallel-20170322.tar.bz2"
   sha256 "f8f810040088bf3c52897a2ee0c0c71bd8d097e755312364b946f107ae3553f6"
   head "https://git.savannah.gnu.org/git/parallel.git"
 
