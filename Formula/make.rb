3d280be3ad16efa3338c1536b18330b26577573e
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:23 -0700

    make: swap GNU url and mirror.

--- Formula/make.rb
+++ Formula/make.rb
@@ -1,8 +1,8 @@
 class Make < Formula
   desc "Utility for directing compilation"
   homepage "https://www.gnu.org/software/make/"
-  url "https://ftpmirror.gnu.org/make/make-4.2.1.tar.bz2"
-  mirror "https://ftp.gnu.org/gnu/make/make-4.2.1.tar.bz2"
+  url "https://ftp.gnu.org/gnu/make/make-4.2.1.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/make/make-4.2.1.tar.bz2"
   sha256 "d6e262bf3601b42d2b1e4ef8310029e1dcf20083c5446b4b7aa67081fdffc589"
 
   bottle do
