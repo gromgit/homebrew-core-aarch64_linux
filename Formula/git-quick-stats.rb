9ffc269e61e44c5e17b38b78dea973e4342ffac7
Lukáš Mešťan <arzzen@users.noreply.github.com>
2017-04-23 16:01:12 +0200

    git-quick-stats 1.0.6 (#12796)

--- Formula/git-quick-stats.rb
+++ Formula/git-quick-stats.rb
@@ -1,8 +1,8 @@
 class GitQuickStats < Formula
   desc "Simple and efficient way to access statistics in git."
   homepage "https://github.com/arzzen/git-quick-stats"
-  url "https://github.com/arzzen/git-quick-stats/archive/1.0.5.tar.gz"
-  sha256 "256633aee79896183ac783462707dbd8ade3d910a16ec2c53bc5be2870aace80"
+  url "https://github.com/arzzen/git-quick-stats/archive/1.0.6.tar.gz"
+  sha256 "70926f1a9f933ad4474c7f80a0b60498f80adf51bce5386edb2c3fba359fa8b5"
 
   bottle :unneeded
 
@@ -14,6 +14,6 @@ class GitQuickStats < Formula
     system "git", "init"
     assert_match "All branches (sorted by most recent commit)",
       shell_output("#{bin}/git-quick-stats branchesByDate")
-    assert_match /^Invalid argument/, shell_output("#{bin}/git-quick-stats command")
+    assert_match /^Invalid argument/, shell_output("#{bin}/git-quick-stats command", 1)
   end
 end
