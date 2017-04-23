4514878b9a2334df35a5a63ba83158f6df589f1c
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:19 -0700

    gnu-typist: swap GNU url and mirror.

--- Formula/gnu-typist.rb
+++ Formula/gnu-typist.rb
@@ -1,8 +1,8 @@
 class GnuTypist < Formula
   desc "GNU typing tutor"
   homepage "https://www.gnu.org/software/gtypist/"
-  url "https://ftpmirror.gnu.org/gtypist/gtypist-2.9.5.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/gtypist/gtypist-2.9.5.tar.xz"
+  url "https://ftp.gnu.org/gnu/gtypist/gtypist-2.9.5.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gtypist/gtypist-2.9.5.tar.xz"
   sha256 "c13af40b12479f8219ffa6c66020618c0ce305ad305590fde02d2c20eb9cf977"
 
   bottle do
