3c9fb336aac532dc372293fc380332ffe9713dbb
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:13 -0700

    autoconf-archive: swap GNU url and mirror.

--- Formula/autoconf-archive.rb
+++ Formula/autoconf-archive.rb
@@ -1,8 +1,8 @@
 class AutoconfArchive < Formula
   desc "Collection of over 500 reusable autoconf macros"
   homepage "https://savannah.gnu.org/projects/autoconf-archive/"
-  url "https://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-2017.03.21.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/autoconf-archive/autoconf-archive-2017.03.21.tar.xz"
+  url "https://ftp.gnu.org/gnu/autoconf-archive/autoconf-archive-2017.03.21.tar.xz"
+  mirror "https://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-2017.03.21.tar.xz"
   sha256 "386ad455f12bdeb3a7d19280441a5ab77355142349200ff11040a8d9d455d765"
 
   bottle do
