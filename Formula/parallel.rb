dd65bb6757e2b6a479481cd67d6753f1019b0478
ilovezfs <ilovezfs@icloud.com>
2017-04-22 21:46:35 -0700

    parallel 20170422
    Closes #12783.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/parallel.rb
+++ Formula/parallel.rb
@@ -1,9 +1,9 @@
 class Parallel < Formula
   desc "GNU parallel shell command"
   homepage "https://savannah.gnu.org/projects/parallel/"
-  url "https://ftp.gnu.org/gnu/parallel/parallel-20170322.tar.bz2"
-  mirror "https://ftpmirror.gnu.org/parallel/parallel-20170322.tar.bz2"
-  sha256 "f8f810040088bf3c52897a2ee0c0c71bd8d097e755312364b946f107ae3553f6"
+  url "https://ftp.gnu.org/gnu/parallel/parallel-20170422.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/parallel/parallel-20170422.tar.bz2"
+  sha256 "7a2438a92692c662dae3d4e80f1190af4cfe527cd3fb1a0d14e07f5c110ed329"
   head "https://git.savannah.gnu.org/git/parallel.git"
 
   bottle do
