c0e5af5aa9b0c8c86d61387865ed3e1fdf7889d2
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:25 -0700

    stow: swap GNU url and mirror.

--- Formula/stow.rb
+++ Formula/stow.rb
@@ -1,8 +1,8 @@
 class Stow < Formula
   desc "Organize software neatly under a single directory tree (e.g. /usr/local)"
   homepage "https://www.gnu.org/software/stow/"
-  url "https://ftpmirror.gnu.org/stow/stow-2.2.2.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/stow/stow-2.2.2.tar.gz"
+  url "https://ftp.gnu.org/gnu/stow/stow-2.2.2.tar.gz"
+  mirror "https://ftpmirror.gnu.org/stow/stow-2.2.2.tar.gz"
   sha256 "e2f77649301b215b9adbc2f074523bedebad366812690b9dc94457af5cf273df"
 
   bottle do
