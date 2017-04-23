558cd6629398bcab6f64a9c3bec0ae653b06aa15
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:22 -0700

    libtool: swap GNU url and mirror.

--- Formula/libtool.rb
+++ Formula/libtool.rb
@@ -4,8 +4,8 @@
 class Libtool < Formula
   desc "Generic library support script"
   homepage "https://www.gnu.org/software/libtool/"
-  url "https://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/libtool/libtool-2.4.6.tar.xz"
+  url "https://ftp.gnu.org/gnu/libtool/libtool-2.4.6.tar.xz"
+  mirror "https://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.xz"
   sha256 "7c87a8c2c8c0fc9cd5019e402bed4292462d00a718a7cd5f11218153bf28b26f"
 
   revision 1
