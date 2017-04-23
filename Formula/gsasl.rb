c86d30f0741e486cd5e9dae7cbdb5a7a37cb1a10
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:20 -0700

    gsasl: swap GNU url and mirror.

--- Formula/gsasl.rb
+++ Formula/gsasl.rb
@@ -1,8 +1,8 @@
 class Gsasl < Formula
   desc "SASL library command-line interface"
   homepage "https://www.gnu.org/software/gsasl/"
-  url "https://ftpmirror.gnu.org/gsasl/gsasl-1.8.0.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/gsasl/gsasl-1.8.0.tar.gz"
+  url "https://ftp.gnu.org/gnu/gsasl/gsasl-1.8.0.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gsasl/gsasl-1.8.0.tar.gz"
   sha256 "310262d1ded082d1ceefc52d6dad265c1decae8d84e12b5947d9b1dd193191e5"
 
   bottle do
