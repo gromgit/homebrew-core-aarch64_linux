87fa011ff0754a484ccd2a533d4a9a73f4bf8f7a
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:22 -0700

    libunistring: swap GNU url and mirror.

--- Formula/libunistring.rb
+++ Formula/libunistring.rb
@@ -1,8 +1,8 @@
 class Libunistring < Formula
   desc "C string library for manipulating Unicode strings"
   homepage "https://www.gnu.org/software/libunistring/"
-  url "https://ftpmirror.gnu.org/libunistring/libunistring-0.9.7.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/libunistring/libunistring-0.9.7.tar.xz"
+  url "https://ftp.gnu.org/gnu/libunistring/libunistring-0.9.7.tar.xz"
+  mirror "https://ftpmirror.gnu.org/libunistring/libunistring-0.9.7.tar.xz"
   sha256 "2e3764512aaf2ce598af5a38818c0ea23dedf1ff5460070d1b6cee5c3336e797"
 
   bottle do
