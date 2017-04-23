7658e8bdd5b5f9d30ce0e6cc6e304d11ada1453f
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:21 -0700

    libidn: swap GNU url and mirror.

--- Formula/libidn.rb
+++ Formula/libidn.rb
@@ -1,8 +1,8 @@
 class Libidn < Formula
   desc "International domain name library"
   homepage "https://www.gnu.org/software/libidn/"
-  url "https://ftpmirror.gnu.org/libidn/libidn-1.33.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/libidn/libidn-1.33.tar.gz"
+  url "https://ftp.gnu.org/gnu/libidn/libidn-1.33.tar.gz"
+  mirror "https://ftpmirror.gnu.org/libidn/libidn-1.33.tar.gz"
   sha256 "44a7aab635bb721ceef6beecc4d49dfd19478325e1b47f3196f7d2acc4930e19"
 
   bottle do
