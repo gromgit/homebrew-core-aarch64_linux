c765a8eb233a99f6c6ccf8d9a442656c730686fe
ilovezfs <ilovezfs@icloud.com>
2017-04-22 22:13:17 -0700

    gcc@4.9: swap GNU url and mirror.

--- Formula/gcc@4.9.rb
+++ Formula/gcc@4.9.rb
@@ -21,8 +21,8 @@ class GccAT49 < Formula
 
   desc "The GNU Compiler Collection"
   homepage "https://gcc.gnu.org/"
-  url "https://ftpmirror.gnu.org/gcc/gcc-4.9.4/gcc-4.9.4.tar.bz2"
-  mirror "https://ftp.gnu.org/gnu/gcc/gcc-4.9.4/gcc-4.9.4.tar.bz2"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-4.9.4/gcc-4.9.4.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/gcc/gcc-4.9.4/gcc-4.9.4.tar.bz2"
   sha256 "6c11d292cd01b294f9f84c9a59c230d80e9e4a47e5c6355f046bb36d4f358092"
   head "svn://gcc.gnu.org/svn/gcc/branches/gcc-4_9-branch"
 
