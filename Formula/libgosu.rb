3c88e4c2d7e95fb9ade3d139309cf7dd4c025cc2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-24 08:10:55 +0100

    libgosu: update 0.12.0 bottle.

--- Formula/libgosu.rb
+++ Formula/libgosu.rb
@@ -8,9 +8,9 @@ class Libgosu < Formula
 
   bottle do
     cellar :any
-    sha256 "6c17c6b07b7c34178d9f11311a1d4545c40d32d70111cae9d5f441c140f8f338" => :sierra
-    sha256 "f67880ff41a81d63c87dca466441d695552bad6026249eef361a301e9badae91" => :el_capitan
-    sha256 "9014132e29491f801da5f3fe223096d36326c63af3ad6801cd7376dc516bf5e1" => :yosemite
+    sha256 "6b71fbca136a0677314cd40a274b552849ec3d6a584c09e8d60dbaddb28e9786" => :sierra
+    sha256 "86f44821f648eed20c1f686ba5b35f9f307be225615bfbd563a37fbb95327d43" => :el_capitan
+    sha256 "aa127db04dcbd378c9bd4367f37398b1d6f252c8d503e923074632c9b72e97d7" => :yosemite
   end
 
   depends_on "cmake" => :build
