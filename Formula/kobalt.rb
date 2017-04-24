0b669484c85ab945cd0d49b920fdb3f0e40d7cbd
neutric <neutric@users.noreply.github.com>
2017-04-25 01:18:46 +0200

    kobalt 1.0.76 (#12842)

--- Formula/kobalt.rb
+++ Formula/kobalt.rb
@@ -1,8 +1,8 @@
 class Kobalt < Formula
   desc "Build system"
   homepage "http://beust.com/kobalt"
-  url "https://github.com/cbeust/kobalt/releases/download/1.0.75/kobalt-1.0.75.zip"
-  sha256 "3f4e684ebaff76035dfda7132e490eed69c548468075199b818ab8595013ca0b"
+  url "https://github.com/cbeust/kobalt/releases/download/1.0.76/kobalt-1.0.76.zip"
+  sha256 "5f896ef623fb524d58b279fa9c8929ce57d53b98e446768f6f3c43e8ac1dd6c1"
 
   bottle :unneeded
 
