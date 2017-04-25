119adfef4d972069c2bb2612f8d99f2ee5945cab
Chris Long <christopherglong@mac.com>
2017-04-24 21:30:35 -0700

    firebase-cli 3.6.1 (new formula)
    Closes #12851.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/firebase-cli.rb
@@ -0,0 +1,24 @@
+require "language/node"
+
+class FirebaseCli < Formula
+  desc "Firebase command-line tools"
+  homepage "https://firebase.google.com/docs/cli/"
+  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-3.6.1.tgz"
+  sha256 "0235f8752d8ab7db647c07a255cfbe0a3db62c75e770595170fb9126ce8456b8"
+  head "https://github.com/firebase/firebase-tools.git"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"test.exp").write <<-EOS.undent
+      spawn #{bin}/firebase login:ci --no-localhost
+      expect "Paste"
+    EOS
+    assert_match "authorization code", shell_output("expect -f test.exp")
+  end
+end
