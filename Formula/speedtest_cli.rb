11bbcb6d8f36d0d3d2646eadcfcfac91b8982c66
ilovezfs <ilovezfs@icloud.com>
2017-04-25 11:56:49 -0700

    speedtest_cli 1.0.6 (#12879)

--- Formula/speedtest_cli.rb
+++ Formula/speedtest_cli.rb
@@ -1,8 +1,8 @@
 class SpeedtestCli < Formula
   desc "Command-line interface for https://speedtest.net bandwidth tests"
   homepage "https://github.com/sivel/speedtest-cli"
-  url "https://github.com/sivel/speedtest-cli/archive/v1.0.5.tar.gz"
-  sha256 "0833f09920f072b11a5712a4830755aab0848211a5ed647e0b824e9108422878"
+  url "https://github.com/sivel/speedtest-cli/archive/v1.0.6.tar.gz"
+  sha256 "9ed312e552929241ed090e0c9370801c348e252af89e498034cf4a1ae2aa8aaa"
   head "https://github.com/sivel/speedtest-cli.git"
 
   bottle :unneeded
