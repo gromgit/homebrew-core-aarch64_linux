e6d3babceb8924c9ca79576531d33d2c4e384f60
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-25 17:50:04 +0000

    passwdqc: add 1.3.1 bottle.

--- Formula/passwdqc.rb
+++ Formula/passwdqc.rb
@@ -4,6 +4,13 @@ class Passwdqc < Formula
   url "http://www.openwall.com/passwdqc/passwdqc-1.3.1.tar.gz"
   sha256 "d1fedeaf759e8a0f32d28b5811ef11b5a5365154849190f4b7fab670a70ffb14"
 
+  bottle do
+    cellar :any
+    sha256 "e63d866e12db3c5b031b33681a8a6b5163908cbbedde6d33e72e2543a4a75ef2" => :sierra
+    sha256 "607a5adfb33eca79f847569357c77d643b9be4b17ba73c915575990ad676bddd" => :el_capitan
+    sha256 "6aac1b96be6144cdb889af5cbcccc3c6779593f3544abdc186d17c61cc4acf34" => :yosemite
+  end
+
   def install
     args = %W[
       BINDIR=#{bin}
