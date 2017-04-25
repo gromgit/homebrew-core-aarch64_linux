632ebc118cfdfb86ea9da1a5a262a32d9f771881
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-25 15:06:54 +0000

    carthage: update 0.21.0 bottle.

--- Formula/carthage.rb
+++ Formula/carthage.rb
@@ -9,8 +9,8 @@ class Carthage < Formula
 
   bottle do
     cellar :any
-    sha256 "e9d72b290971cb7ec20c2e4d72008f8e85e1a1e2f4965d459abcd42cfae81bdd" => :sierra
-    sha256 "1c5275e2a68e343642e3556c130a0e7103807c8495ebea945cce4e7291309c4f" => :el_capitan
+    sha256 "7f1383633ff45349dd968120dba62686b7d346079c90cf72776d75084928b8cf" => :sierra
+    sha256 "51452cb13a20402aa17ab6638825060d54ccd8084b9a1c33127ebee29e97d60c" => :el_capitan
   end
 
   depends_on :xcode => ["8.2", :build]
