1479fe9adc7eb7dedf2fac23471fa9ebf23b5524
Jake Petroules <jake.petroules@petroules.com>
2017-04-26 08:57:52 -0700

    qbs 1.7.2
    Closes #12906.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/qbs.rb
+++ Formula/qbs.rb
@@ -1,9 +1,8 @@
 class Qbs < Formula
   desc "Build tool for developing projects across multiple platforms."
   homepage "https://wiki.qt.io/Qbs"
-  url "https://download.qt.io/official_releases/qbs/1.7.1/qbs-src-1.7.1.tar.gz"
-  sha256 "e4f5627ffcdba4d74a432f89215e7df1c1657f5416b61612467a7a9267cd4851"
-  revision 1
+  url "https://download.qt.io/official_releases/qbs/1.7.2/qbs-src-1.7.2.tar.gz"
+  sha256 "ab82fb9f9fd72617b175b73ebadd3a3ac8a089af741edb777ead6dc9937394b0"
   head "https://code.qt.io/qt-labs/qbs.git"
 
   bottle do
