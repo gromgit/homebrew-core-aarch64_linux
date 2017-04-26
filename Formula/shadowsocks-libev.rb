6f52604b25358b7a7eef70c2f93950b2194cf362
ilovezfs <ilovezfs@icloud.com>
2017-04-26 02:59:41 -0700

    shadowsocks-libev 3.0.6
    Closes #12898.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/shadowsocks-libev.rb
+++ Formula/shadowsocks-libev.rb
@@ -1,8 +1,8 @@
 class ShadowsocksLibev < Formula
   desc "Libev port of shadowsocks"
   homepage "https://github.com/shadowsocks/shadowsocks-libev"
-  url "https://github.com/shadowsocks/shadowsocks-libev/releases/download/v3.0.5/shadowsocks-libev-3.0.5.tar.gz"
-  sha256 "961ddbe06c2fd6299d0f181a371af16463d59e146228c53769f24e08a1d95f2e"
+  url "https://github.com/shadowsocks/shadowsocks-libev/releases/download/v3.0.6/shadowsocks-libev-3.0.6.tar.gz"
+  sha256 "7d9b43b0235a57c115bfe160efd54abef96bffcbfff61c5496e7c2800f0734ca"
 
   bottle do
     cellar :any
