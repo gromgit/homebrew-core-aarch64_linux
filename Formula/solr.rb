557862d85cfb81685ab5ba399d7fda9492f6dc4b
jeremyweber-np <jeremy@northpage.com>
2017-04-26 17:31:47 -0400

    solr 6.5.0 (#12908)

--- Formula/solr.rb
+++ Formula/solr.rb
@@ -1,9 +1,9 @@
 class Solr < Formula
   desc "Enterprise search platform from the Apache Lucene project"
   homepage "https://lucene.apache.org/solr/"
-  url "https://www.apache.org/dyn/closer.cgi?path=lucene/solr/6.4.1/solr-6.4.1.tgz"
-  mirror "https://archive.apache.org/dist/lucene/solr/6.4.1/solr-6.4.1.tgz"
-  sha256 "ebce206dd4a66600384ccdd9a604536135594ce4c24814bbd5a3e8a8ec1efbb9"
+  url "https://www.apache.org/dyn/closer.cgi?path=lucene/solr/6.5.0/solr-6.5.0.tgz"
+  mirror "https://archive.apache.org/dist/lucene/solr/6.5.0/solr-6.5.0.tgz"
+  sha256 "893835a1d724bda80bc0b9d87893a321f442460937d61d26746cefb52286543c"
 
   bottle :unneeded
 
