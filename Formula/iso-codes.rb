ca304b6ff0d2b01433b3e686146013e81b78bb12
ilovezfs <ilovezfs@icloud.com>
2017-04-25 23:52:26 -0700

    iso-codes 3.75
    Closes #12894.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/iso-codes.rb
+++ Formula/iso-codes.rb
@@ -1,8 +1,8 @@
 class IsoCodes < Formula
   desc "Provides lists of various ISO standards"
   homepage "https://pkg-isocodes.alioth.debian.org/"
-  url "https://pkg-isocodes.alioth.debian.org/downloads/iso-codes-3.74.tar.xz"
-  sha256 "21f4f3cea8fe09f5b53784522303a0e1e7d083964ecaf1c75b1441d4d9ec6aee"
+  url "https://pkg-isocodes.alioth.debian.org/downloads/iso-codes-3.75.tar.xz"
+  sha256 "7335e0301cd77cd4ee019bf5d3709aa79309d49dd66e85ba350caf67e00b00cd"
   head "https://anonscm.debian.org/git/pkg-isocodes/iso-codes.git"
 
   bottle do
