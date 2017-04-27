0627fedddd29d3f064c29945e997b9ccf97f3e11
Alexander Hamann <neutric@users.noreply.github.com>
2017-04-27 11:58:39 +0200

    tor 0.3.0.6
    Closes #12935.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/tor.rb
+++ Formula/tor.rb
@@ -1,20 +1,14 @@
 class Tor < Formula
   desc "Anonymizing overlay network for TCP"
   homepage "https://www.torproject.org/"
-  url "https://www.torproject.org/dist/tor-0.2.9.10.tar.gz"
-  mirror "https://tor.eff.org/dist/tor-0.2.9.10.tar.gz"
-  sha256 "d611283e1fb284b5f884f8c07e7d3151016851848304f56cfdf3be2a88bd1341"
+  url "https://www.torproject.org/dist/tor-0.3.0.6.tar.gz"
+  mirror "https://tor.eff.org/dist/tor-0.3.0.6.tar.gz"
+  sha256 "a3e512e93cb555601fd207d914d7c5fe981d66d6ebb5821ecdf5dea738c2fb14"
 
   bottle do
     sha256 "16237392bef04e8f4857f6a76d10842676732b80e2d3d9e184f14f572e7b457c" => :sierra
     sha256 "d553869d5a12c5d8cdb8d7db30d8c0b3f11be9a6efdcf51a7cca7088b9f34d49" => :el_capitan
     sha256 "9bc64bf56797221ebce23972c908565399995058f2db6ba05c4a3ea26b61e3d0" => :yosemite
-  end
-
-  devel do
-    url "https://www.torproject.org/dist/tor-0.3.0.5-rc.tar.gz"
-    mirror "https://tor.eff.org/dist/tor-0.3.0.5-rc.tar.gz"
-    sha256 "622c9366d8b753da9192e037d0a13d2d87ffec25b5c606a3d73a2452976b43e0"
   end
 
   depends_on "pkg-config" => :build
