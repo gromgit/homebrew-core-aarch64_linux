d9dda3f77c7c89590afe8c3cfaae9d9cd3d866fe
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-27 08:04:06 +0000

    immortal: add 0.13.2 bottle.

--- Formula/immortal.rb
+++ Formula/immortal.rb
@@ -5,6 +5,13 @@ class Immortal < Formula
   sha256 "f34674720828c984ef34df33b75b614fabe81e4fd50eb152746a33a273daa4f9"
   head "https://github.com/immortal/immortal.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "33f246592883862a31fead5edd112a6b599df2bf1ddcf4025bc1c57e0163cac8" => :sierra
+    sha256 "339e8a2d41554c8fa0df6d6c6c9dc05f4e1635fc492415e529865b271d29297e" => :el_capitan
+    sha256 "62bf96158973768f1e2ebc4ce476b370f9c421ab65ded7346a7203b901133223" => :yosemite
+  end
+
   depends_on "go" => :build
 
   def install
