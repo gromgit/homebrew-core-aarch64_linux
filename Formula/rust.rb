6bcd79ee34c15e8d1d98f094c5ab2da7a1c16438
Alex Gaynor <alex.gaynor@gmail.com>
2017-04-27 15:03:01 -0400

    rust 1.17.0
    Closes #12964.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/rust.rb
+++ Formula/rust.rb
@@ -3,13 +3,13 @@ class Rust < Formula
   homepage "https://www.rust-lang.org/"
 
   stable do
-    url "https://static.rust-lang.org/dist/rustc-1.16.0-src.tar.gz"
-    sha256 "f966b31eb1cd9bd2df817c391a338eeb5b9253ae0a19bf8a11960c560f96e8b4"
+    url "https://static.rust-lang.org/dist/rustc-1.17.0-src.tar.gz"
+    sha256 "4baba3895b75f2492df6ce5a28a916307ecd1c088dc1fd02dbfa8a8e86174f87"
 
     resource "cargo" do
       url "https://github.com/rust-lang/cargo.git",
-          :tag => "0.17.0",
-          :revision => "f9e54817e53c7b9845cc7c1ede4c11e4d3e42e36"
+          :tag => "0.18.0",
+          :revision => "fe7b0cdcf5ca7aab81630706ce40b70f6aa2e666"
     end
 
     resource "racer" do
@@ -50,10 +50,10 @@ class Rust < Formula
 
   resource "cargobootstrap" do
     # From https://github.com/rust-lang/rust/blob/#{version}/src/stage0.txt
-    url "https://s3.amazonaws.com/rust-lang-ci/cargo-builds/6e0c18cccc8b0c06fba8a8d76486f81a792fb420/cargo-nightly-x86_64-apple-darwin.tar.gz"
+    url "https://s3.amazonaws.com/rust-lang-ci/cargo-builds/6b05583d71f982bcad049b9fa094c637c062e751/cargo-nightly-x86_64-apple-darwin.tar.gz"
     # From name=cargo-nightly-x86_64-apple-darwin; tar -xf $name.tar.gz $name/version; cat $name/version
-    version "2017-01-27"
-    sha256 "0a6b78b8c6344e7a14f1aa57ebfa0154d4ea560332833846dbeaa3a6772a010a"
+    version "2017-03-13"
+    sha256 "0ed926ec3e5299f27fcc65f565572d2ee62fb79fc1707acfe81ced84981a30c6"
   end
 
   def install
