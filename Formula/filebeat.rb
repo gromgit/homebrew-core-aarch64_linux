59896e35863f96b1b5d1acfa7359d96a8266b23b
ilovezfs <ilovezfs@icloud.com>
2017-04-27 09:38:57 -0700

    filebeat 5.3.2
    Closes #12955.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/filebeat.rb
+++ Formula/filebeat.rb
@@ -1,8 +1,8 @@
 class Filebeat < Formula
   desc "File harvester to ship log files to Elasticsearch or Logstash"
   homepage "https://www.elastic.co/products/beats/filebeat"
-  url "https://github.com/elastic/beats/archive/v5.3.1.tar.gz"
-  sha256 "392c647fd3a4fd40b70d911950f38c0fd65a3ad4bb8548fcd0afd77c01d31d9e"
+  url "https://github.com/elastic/beats/archive/v5.3.2.tar.gz"
+  sha256 "e8c765531276e4731c5148bd99fad1efc799f3ec68e1d32a2fe116995f2b9e2f"
 
   head "https://github.com/elastic/beats.git"
 
