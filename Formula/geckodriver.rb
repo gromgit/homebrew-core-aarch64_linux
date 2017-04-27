4bc17dc84872d6b92314e1232082150e973ba6d1
ilovezfs <ilovezfs@icloud.com>
2017-04-26 22:39:56 -0700

    geckodriver 0.16.1
    Closes #12918.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/geckodriver.rb
+++ Formula/geckodriver.rb
@@ -1,8 +1,8 @@
 class Geckodriver < Formula
   desc "WebDriver <-> Marionette proxy"
   homepage "https://github.com/mozilla/geckodriver"
-  url "https://github.com/mozilla/geckodriver/archive/v0.16.0.tar.gz"
-  sha256 "5a3258d8314f586f1224703ef8281b3ff0dd3682e28ee094e88e5ddae4da7a0e"
+  url "https://github.com/mozilla/geckodriver/archive/v0.16.1.tar.gz"
+  sha256 "7345696ada459312d28c9f2892f0cb3023cde352cf77482cc6c8912a2f02ac06"
 
   bottle do
     sha256 "e01a8ec26cdce60bf32a7035e6e640649bd152f95a44c9373ccfcd1323a60dbf" => :sierra
