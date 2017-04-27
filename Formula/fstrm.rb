9c10b1ae4e40e1255d6810f4a6ce70eedba863ab
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-27 19:18:02 +0000

    fstrm: add 0.3.2 bottle.

--- Formula/fstrm.rb
+++ Formula/fstrm.rb
@@ -5,6 +5,13 @@ class Fstrm < Formula
   url "https://dl.farsightsecurity.com/dist/fstrm/fstrm-0.3.2.tar.gz"
   sha256 "2d509999ac904e48c038f88820f47859da85ceb86c06552e4052897082423ec5"
 
+  bottle do
+    cellar :any
+    sha256 "fb7031a0e06d718a7d345ab21796b0f9b6da6ebf411d24b1f019ba3e526dbfdc" => :sierra
+    sha256 "45d2c965a757c50c5b65d929082baeedd045a7794eb933618e10a4371cfc0d90" => :el_capitan
+    sha256 "36ec0bf5368fd0bbcfe7b14302b003f543ef4365fe458d2975998993b2b1f0d8" => :yosemite
+  end
+
   head do
     url "https://github.com/farsightsec/fstrm.git"
 
