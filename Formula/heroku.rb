1f9377ef4d45131339547745bac2b1d2135c178a
ilovezfs <ilovezfs@icloud.com>
2017-04-27 06:21:46 -0700

    heroku 5.8.11 (#12945)

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -1,9 +1,9 @@
 class Heroku < Formula
   desc "Everything you need to get started with Heroku"
   homepage "https://cli.heroku.com"
-  url "https://cli-assets.heroku.com/branches/stable/5.8.5-614a805/heroku-v5.8.5-614a805-darwin-amd64.tar.xz"
-  version "5.8.5"
-  sha256 "9f108c351f887866d9887169069740648006459575e4064e7c810c46da27cb0b"
+  url "https://cli-assets.heroku.com/branches/stable/5.8.11-0e9b5ce/heroku-v5.8.11-0e9b5ce-darwin-amd64.tar.xz"
+  version "5.8.11"
+  sha256 "613544bf6b9585ee5f453471543984f847325575d5c95e91a46a4b8b2a10cf70"
 
   bottle :unneeded
 
