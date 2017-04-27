b6ede0dc25775d72e8b908e9a1b43edbed8105e5
Gabe Levi <glevi@fb.com>
2017-04-27 17:48:05 -0400

    flow 0.45.0
    Closes #12970.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/flow.rb
+++ Formula/flow.rb
@@ -1,9 +1,26 @@
 class Flow < Formula
   desc "Static type checker for JavaScript"
   homepage "https://flowtype.org/"
-  url "https://github.com/facebook/flow/archive/v0.44.1.tar.gz"
-  sha256 "2dbe1c863ea8c594dfde8e2924b32033336f31f039d61ab07e276467195c2028"
   head "https://github.com/facebook/flow.git"
+
+  stable do
+    url "https://github.com/facebook/flow/archive/v0.45.0.tar.gz"
+    sha256 "9a76cb1669d5d1f07a55b3163edb0329c46565033eaf7ed9320058b6e3a9cbbf"
+
+    # Remove for > 0.45.0
+    # Upstream commit from 28 Apr 2017 "Add `make all-homebrew`"
+    patch do
+      url "https://github.com/facebook/flow/commit/8a811a4b443ec545d89aa201d5ebcc254ae220e4.patch"
+      sha256 "f92aca59e2014b91974c02672e0d55b3b278d3a267150cfb534d266befda4ad4"
+    end
+
+    # Remove for > 0.45.0
+    # Upstream commit from 28 Apr 2017 "mktemp's interface changed in OSX 10.11"
+    patch do
+      url "https://github.com/facebook/flow/commit/7cc0f0b3f34e6932e7a713de2a651a1848eef395.patch"
+      sha256 "d7eac833d7ee1dd7ee6ec47dfdc4f68e9b31555d3d39670d675ec81ec8a5127d"
+    end
+  end
 
   bottle do
     cellar :any_skip_relocation
@@ -13,10 +30,11 @@ class Flow < Formula
   end
 
   depends_on "ocaml" => :build
-  depends_on "ocamlbuild" => :build
+  depends_on "opam" => :build
 
   def install
-    system "make"
+    system "make", "all-homebrew"
+
     bin.install "bin/flow"
 
     bash_completion.install "resources/shell/bash-completion" => "flow-completion.bash"
