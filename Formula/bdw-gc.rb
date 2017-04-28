5c488dc5fb00147da75fa1d0e4b13167f2fb2dba
FX Coudert <fxcoudert@gmail.com>
2017-04-28 13:55:19 +0200

    bdw-gc: add test

--- Formula/bdw-gc.rb
+++ Formula/bdw-gc.rb
@@ -1,7 +1,7 @@
 class BdwGc < Formula
   desc "Garbage collector for C and C++"
-  homepage "https://www.hboehm.info/gc/"
-  url "https://www.hboehm.info/gc/gc_source/gc-7.6.0.tar.gz"
+  homepage "http://www.hboehm.info/gc/"
+  url "http://www.hboehm.info/gc/gc_source/gc-7.6.0.tar.gz"
   sha256 "a14a28b1129be90e55cd6f71127ffc5594e1091d5d54131528c24cd0c03b7d90"
 
   bottle do
@@ -30,5 +30,31 @@ class BdwGc < Formula
     system "make"
     system "make", "check"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <assert.h>
+      #include <stdio.h>
+      #include "gc.h"
+
+      int main(void)
+      {
+        int i;
+
+        GC_INIT();
+        for (i = 0; i < 10000000; ++i)
+        {
+          int **p = (int **) GC_MALLOC(sizeof(int *));
+          int *q = (int *) GC_MALLOC_ATOMIC(sizeof(int));
+          assert(*p == 0);
+          *p = (int *) GC_REALLOC(q, 2 * sizeof(int));
+        }
+        return 0;
+      }
+    EOS
+
+    system ENV.cc, "-I#{include}", "-L#{lib}", "-lgc", "-o", "test", "test.c"
+    system "./test"
   end
 end
