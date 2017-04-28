4b2e10e5c464f8749c06d9dcc2b549336584075f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-28 11:05:33 +0000

    ck: add 0.6.0 bottle.

--- Formula/ck.rb
+++ Formula/ck.rb
@@ -4,6 +4,13 @@ class Ck < Formula
   url "http://concurrencykit.org/releases/ck-0.6.0.tar.gz"
   sha256 "d7e27dd0a679e45632951e672f8288228f32310dfed2d5855e9573a9cf0d62df"
 
+  bottle do
+    cellar :any
+    sha256 "29ded4898ebd1f8dfa7fc5adb377a7b3404b8a9c1d11db5ab2e85c3cb6f62ad7" => :sierra
+    sha256 "4e74455ae32382c6eb1b7077d1166e72c2d1f8f66c5c74fc0fc975323c6e1e40" => :el_capitan
+    sha256 "c01c837e8999093e50cc565681fe22436289c0cbd5bed288d758fca95f414622" => :yosemite
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
