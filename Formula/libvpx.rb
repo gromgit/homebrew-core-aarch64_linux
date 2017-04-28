4cb8deeeeeb954ad3d05da6112736969e57653f8
Werner Robitza <slhck@users.noreply.github.com>
2017-04-28 11:30:46 +0200

    libvpx: add VP9 in description
    add VP9 in the description of the libvpx formula

--- Formula/libvpx.rb
+++ Formula/libvpx.rb
@@ -1,5 +1,5 @@
 class Libvpx < Formula
-  desc "VP8 video codec"
+  desc "VP8/VP9 video codec"
   homepage "https://www.webmproject.org/code/"
   url "https://github.com/webmproject/libvpx/archive/v1.6.1.tar.gz"
   sha256 "cda8bb6f0e4848c018177d3a576fa83ed96d762554d7010fe4cfb9d70c22e588"
