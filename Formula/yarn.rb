4f22ca7ca20662a570ca0972bfa5298b9ee20fb1
Artem Medvedev <amedvedev@outlook.com>
2017-04-28 21:34:22 +0300

    yarn 0.23.3 (#12943)

--- Formula/yarn.rb
+++ Formula/yarn.rb
@@ -1,8 +1,8 @@
 class Yarn < Formula
   desc "JavaScript package manager"
   homepage "https://yarnpkg.com/"
-  url "https://yarnpkg.com/downloads/0.23.2/yarn-v0.23.2.tar.gz"
-  sha256 "2e4f3c5eb0bddad10fdc08a300ab43fe0f626544893deb9e07a4497e998cb82f"
+  url "https://yarnpkg.com/downloads/0.23.3/yarn-v0.23.3.tar.gz"
+  sha256 "9f7569b9b89bbe4c3c0bbd8917f551ec26935802668b6e6139ea45db67e3a314"
   head "https://github.com/yarnpkg/yarn.git"
 
   bottle :unneeded
