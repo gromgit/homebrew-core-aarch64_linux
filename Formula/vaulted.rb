8731c570156fa069621477bd171dc300c929e419
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-29 00:08:57 +0000

    vaulted: update 2.2.0 bottle.

--- Formula/vaulted.rb
+++ Formula/vaulted.rb
@@ -10,9 +10,9 @@ class Vaulted < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c3e5997ac5d059daa21e70a20a81ae1ac921fb1a5d52686884e1dee0cd8f0061" => :sierra
-    sha256 "4e4afb85b441e944f731280761ded36de02cf27cff2a13d7e461483a453559ef" => :el_capitan
-    sha256 "d7c2bc58bdabf6ada0730ac1b90d3f08c9aaa3754b7070e17873b11445e63f7d" => :yosemite
+    sha256 "f26213f97d628b3fd09da3499599ce809836d35a8d9066c4ca0956337e8004e2" => :sierra
+    sha256 "d6cb9dee20339fe668adcf97447a6451f59e970ccef032cb97665582fa064ce4" => :el_capitan
+    sha256 "643939ca33c34a15ac264bf4cb66aaf1b14fb175528b40ab2fbaa206baa62624" => :yosemite
   end
 
   depends_on "go" => :build
