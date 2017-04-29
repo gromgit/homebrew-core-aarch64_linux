e2633fa7dbbaa7c4741580dadd660f91ae9c374c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-29 14:04:44 +0000

    mysql-connector-c: update 6.1.10 bottle.

--- Formula/mysql-connector-c.rb
+++ Formula/mysql-connector-c.rb
@@ -5,9 +5,9 @@ class MysqlConnectorC < Formula
   sha256 "73d2c3d633dcec63b9e985e524cc386ce52ddedcd06bdc40e094aaae35b14a69"
 
   bottle do
-    sha256 "a5719207674a94a3e2303b246f48e71f3726a95388e733e19abbb732eea95a95" => :sierra
-    sha256 "06082e549fec918fea7d09ea80f6a9f1bc1ed645b0938879c57a4cb16620efa4" => :el_capitan
-    sha256 "539f992aae3609351505d4e1214f706905234c95774fa678090ec15b466144f0" => :yosemite
+    sha256 "6f0b845224739ff3e1405bb94e96a24c99ce1007eff212168c86b3f6ba37c4ac" => :sierra
+    sha256 "252b03549c645c40155c281e02ffb130298bca5efdf40d2179051dd854e1b0ed" => :el_capitan
+    sha256 "2e5182e2fd30babb7aba13049c0f36e05b0f3ebf0e9bb2dc64370f28047ba955" => :yosemite
   end
 
   depends_on "cmake" => :build
