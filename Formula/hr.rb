08e58e9816bea4df8aa97ea55128380d99c082b4
neutric <neutric@users.noreply.github.com>
2017-04-30 22:43:35 +0200

    hr: fix head url (#13077)

--- Formula/hr.rb
+++ Formula/hr.rb
@@ -3,7 +3,7 @@ class Hr < Formula
   homepage "https://github.com/LuRsT/hr"
   url "https://github.com/LuRsT/hr/archive/1.2.tar.gz"
   sha256 "8f611b3f25e10fac1e67cf8b30fea4c1c02db7ab8c55d39402fe08caecb68a1a"
-  head "https://github.com/LuRsT/hr"
+  head "https://github.com/LuRsT/hr.git"
 
   bottle :unneeded
 
