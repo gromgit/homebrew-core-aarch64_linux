23b8b0e50bb478c7c3fbe329341a7af6f2b79932
ilovezfs <ilovezfs@icloud.com>
2017-04-30 23:24:31 -0700

    kvazaar 1.1.0
    Closes #13094.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/kvazaar.rb
+++ Formula/kvazaar.rb
@@ -1,28 +1,8 @@
 class Kvazaar < Formula
   desc "Ultravideo HEVC encoder"
   homepage "https://github.com/ultravideo/kvazaar"
-  head "https://github.com/ultravideo/kvazaar.git"
-
-  stable do
-    url "https://github.com/ultravideo/kvazaar/archive/v1.0.0.tar.gz"
-    sha256 "40eb7b4b23897299e99050f0c011e9380cf898b25615dd143f018b278b972a46"
-
-    # Remove for > 1.0.0
-    # Upstream commit from 2 Feb 2017 "Fix encoder getting stuck on OS-X"
-    # See https://github.com/ultravideo/kvazaar/issues/153
-    patch do
-      url "https://github.com/ultravideo/kvazaar/commit/d893474.patch"
-      sha256 "0d2087dcf535ce01b2cc8afdb138f207e7e2389976fda6167bbb7c22c78c4797"
-    end
-
-    # Remove for > 1.0.0
-    # Upstream commit from 8 Feb 2017 "Fix crash with sub-LCU frame sizes and WPP"
-    # See https://github.com/ultravideo/kvazaar/issues/153
-    patch do
-      url "https://github.com/ultravideo/kvazaar/commit/b8e3513.patch"
-      sha256 "de52ecb665f65be4d364f0070632e660f4bf5a16a47e63064846595b64afe14a"
-    end
-  end
+  url "https://github.com/ultravideo/kvazaar/releases/download/v1.1.0/kvazaar-v1.1.0.tar.gz"
+  sha256 "81386b25651ad328b2b94c269eb5601096b966724e6c308ecc3096eb37ef33bb"
 
   bottle do
     cellar :any
@@ -31,10 +11,14 @@ class Kvazaar < Formula
     sha256 "ec981006d5607147b9af8a1a1d6e9dca1642b1dd85ac8a528f1c801f82053573" => :yosemite
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
-  depends_on "pkg-config" => :build
+  head do
+    url "https://github.com/ultravideo/kvazaar.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   depends_on "yasm" => :build
 
   resource "videosample" do
@@ -43,7 +27,7 @@ class Kvazaar < Formula
   end
 
   def install
-    system "./autogen.sh"
+    system "./autogen.sh" if build.head?
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
