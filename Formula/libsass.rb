0daca460e8565cde91787354de181546af7c7ffc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-01 06:33:05 +0000

    libsass: update 3.4.4 bottle.

--- Formula/libsass.rb
+++ Formula/libsass.rb
@@ -6,9 +6,9 @@ class Libsass < Formula
 
   bottle do
     cellar :any
-    sha256 "1bf8fdd91e81a6ae71c0b285bf9bda4892d2d5fa152ed9cd9eb99c8a0588bcf6" => :sierra
-    sha256 "d6818e9a5872d39f02be581e6d31cc8942c158abfc428d8e662454248ae97c87" => :el_capitan
-    sha256 "030c6b49661b482562fb1ce2d2e2ca383dada5124a78439a4b7afa2eb7e6312d" => :yosemite
+    sha256 "2e7eed733ad88eadfc7f1a4d514b014bbfbc2771243ea2e1d6bebf38d95d0061" => :sierra
+    sha256 "71fe112942b4e8c6825d51fc4363aeb5d74ce18bbb317cb6f3897fd1d33a6d7c" => :el_capitan
+    sha256 "a48a3af046a54c2a7b72d3ad72fbc4bed706f6bb3b4d3f5a6869d0b7a4512fd1" => :yosemite
   end
 
   depends_on "autoconf" => :build
