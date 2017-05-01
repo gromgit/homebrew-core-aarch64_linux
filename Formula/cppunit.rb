6484b329118c5ac803efa2876b95a20873a88d94
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-01 08:42:31 +0000

    cppunit: update 1.14.0 bottle.

--- Formula/cppunit.rb
+++ Formula/cppunit.rb
@@ -6,12 +6,9 @@ class Cppunit < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "1f0d4bfcd8fb19cd29e1c1cf74effe73e2950ab9252b85d07bf712b5926cb041" => :sierra
-    sha256 "27730fdd237f61dd3698e422edab55246d657f15fcbb73999d8b35087e3cb3c8" => :el_capitan
-    sha256 "235b62030002ef0c7c84c4989ac0ca3401c96929053e0095a605660b30aa9eba" => :yosemite
-    sha256 "f66998bbde3f7c3ca32f3a99c35177cd80ff8f6583ec63627d7526455a503214" => :mavericks
-    sha256 "0494ee4b157acec4c86c4d26a2c1155e71e11e5dc3a897ae1888a3da4edbb21f" => :mountain_lion
+    sha256 "3c620068fba4bf15b6138ffc4042ab2111a67201310523104c07c314115520bb" => :sierra
+    sha256 "a2d2bf8be8ffb614f0490801e38558681b8b01a9fc7ff4be5f785d3db7f71157" => :el_capitan
+    sha256 "8078fbe4b7fd092a197c452af4caf3f5eaeb27dce16afd46e1da2a1ec1ae7f6d" => :yosemite
   end
 
   def install
