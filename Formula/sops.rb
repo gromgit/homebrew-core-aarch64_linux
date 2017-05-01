922c8116f41622cbd4994c7cf8236a7254922d6e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-01 23:47:40 +0000

    sops: update 2.0.9 bottle.

--- Formula/sops.rb
+++ Formula/sops.rb
@@ -9,9 +9,9 @@ class Sops < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7a7a73cfb3fd56e2b8b9f1e57e89a65013f6d84b04f3ba57e53ce050409ce971" => :sierra
-    sha256 "0594d0c02f8dfcc0717cbc59c2c4962bfa7b2cee43e108dff937d9948c7f0e08" => :el_capitan
-    sha256 "3ec75075f0de2c93f67950e226c4f1ca63199abb4d0d3871ca4b4dad0bcfa443" => :yosemite
+    sha256 "37febebf9fc1fde4f24447193b813b3d10591a5ca6726ef56c1c7f484d366abc" => :sierra
+    sha256 "7d73a691780a7f2ea4988d92ee1699b15834f5fa85ab6ed9130dd9e481aa9239" => :el_capitan
+    sha256 "823208d974408d4a1c0838f6c6f35e8aa5c0c4ccdb22732e675bcb611c582004" => :yosemite
   end
 
   depends_on "go" => :build
