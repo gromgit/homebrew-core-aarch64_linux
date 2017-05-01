56d0aeb63c26ff61c2d893bc105ed992dd1873d0
Zhiming Wang <zmwangx@gmail.com>
2017-04-30 23:28:13 -0400

    cmark-gfm 0.27.1.gfm.0 (new formula)
    Closes #13089.
    
    Signed-off-by: JCount <JCount42@gmail.com>

---
+++ Formula/cmark-gfm.rb
@@ -0,0 +1,26 @@
+class CmarkGfm < Formula
+  desc "C implementation of GitHub Flavored Markdown"
+  homepage "https://github.com/github/cmark"
+  url "https://github.com/github/cmark/archive/0.27.1.gfm.0.tar.gz"
+  version "0.27.1.gfm.0"
+  sha256 "71644ba7503816e4ba695d7c88b3ddeeefd934ab7bb8707982922a402631730b"
+
+  depends_on "cmake" => :build
+  depends_on :python3 => :build
+
+  conflicts_with "cmark", :because => "both install a `cmark.h` header"
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      system "make", "test"
+      system "make", "install"
+    end
+  end
+
+  test do
+    output = pipe_output("#{bin}/cmark-gfm --extension autolink", "https://brew.sh")
+    assert_equal '<p><a href="https://brew.sh">https://brew.sh</a></p>', output.chomp
+  end
+end
