b81d7499f454173e7c9785d8120494d519385a98
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-02 16:53:18 +0000

    logcheck: update 1.3.18 bottle.

--- Formula/logcheck.rb
+++ Formula/logcheck.rb
@@ -7,11 +7,9 @@ class Logcheck < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 3
-    sha256 "7fe24461210fc2d5f4809e08566e85e010a2715b9b7a6426f686327f40f25cdf" => :sierra
-    sha256 "aab0ab066fe378c88c74b9783a90fb0a4896dd3a6258d00b08cd1d0d2987b108" => :el_capitan
-    sha256 "c75e01fb14bdd0adfc04e110a3c8a65d036b9bd71ac03a6ac58d69006a892fe9" => :yosemite
-    sha256 "25f2dfec7bb30fded535bdb354767a2680108dcd93d0627f8384a115c008cf89" => :mavericks
+    sha256 "ce71670cdeba739650cd716867e12cda182a948189a68ca565d318299f847a61" => :sierra
+    sha256 "94fde1124326dc8ed8428878b84db52b22b4ce1c19949ee0dd170402164cbadd" => :el_capitan
+    sha256 "94fde1124326dc8ed8428878b84db52b22b4ce1c19949ee0dd170402164cbadd" => :yosemite
   end
 
   def install
