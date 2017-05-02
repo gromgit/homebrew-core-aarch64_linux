75081ef61ae80e00c6dcdce9e91e4433c7bb6e0c
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-05-02 08:35:25 +0100

    corebird 1.4.2 (new formula)
    Closes #13150.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/corebird.rb
@@ -0,0 +1,36 @@
+class Corebird < Formula
+  desc "Native Gtk+ Twitter Client"
+  homepage "https://corebird.baedert.org"
+  url "https://github.com/baedert/corebird/releases/download/1.4.2/corebird-1.4.2.tar.xz"
+  sha256 "1c07a65382e78308f7de406be8464789c1ec42d531c519b69510a685234b4074"
+
+  depends_on "pkg-config" => :build
+  depends_on "vala" => :build
+  depends_on "gtk+3"
+  depends_on "librest"
+  depends_on "libsoup"
+  depends_on "json-glib"
+  depends_on "gspell"
+  depends_on "gst-plugins-good"
+  depends_on "gst-plugins-bad"
+  depends_on "gst-plugins-base"
+  depends_on "gst-libav"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--disable-schemas-compile",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def post_install
+    system "#{Formula["glib"].opt_bin}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
+    system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
+  end
+
+  test do
+    system "#{bin}/corebird", "--help"
+  end
+end
