b15752bc1fc6f28216d96e6b7b9387e98f6be899
Dominyk Tiller <dominyktiller@gmail.com>
2017-05-02 17:03:31 +0100

    apr-util: remove duped period from keg_only

--- Formula/apr-util.rb
+++ Formula/apr-util.rb
@@ -12,7 +12,7 @@ class AprUtil < Formula
     sha256 "29aefc556d7712da0a957d9b34e4e6e320f3997922a1b1b18705c486acfb6890" => :yosemite
   end
 
-  keg_only :provided_by_osx, "Apple's CLT package contains apr."
+  keg_only :provided_by_osx, "Apple's CLT package contains apr"
 
   depends_on "apr"
   depends_on "openssl"
