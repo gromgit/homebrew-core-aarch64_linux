64e2e53685b6a589af954a2f471c65a43cdbce3c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-02 17:47:45 +0000

    cheapglk: update 1.0.6 bottle.

--- Formula/cheapglk.rb
+++ Formula/cheapglk.rb
@@ -7,9 +7,9 @@ class Cheapglk < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "def8cf0f954279c503b3b67c456721af6ccd9657f87df37fecd4459a0a44918d" => :sierra
-    sha256 "e58d40a1c283dbb45ef9d672361849e807d104d627a93626cdb06e66105f1d4a" => :el_capitan
-    sha256 "93d9f95fbdef5fc3c51b3ecbd69fedea47bbaaf9a89e3a3f8275b9c801bba2e3" => :yosemite
+    sha256 "d76d29db8ea0201fcef949e02cbddb1c06311dece796a263192ffef487a3aa2c" => :sierra
+    sha256 "497a5399738c026d318d3213b764f20fb80ccea94181919fad2e80eb75086055" => :el_capitan
+    sha256 "8351c9dec39fd8e860e50a8e693e1c648def81c3ddcbdd3a856f656d585b0082" => :yosemite
   end
 
   keg_only "it conflicts with other Glk libraries"
