5ab2b935e8e983014c4e433c7f90a725f77fe862
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-02 13:41:46 +0000

    corebird: add 1.4.2 bottle.

--- Formula/corebird.rb
+++ Formula/corebird.rb
@@ -4,6 +4,12 @@ class Corebird < Formula
   url "https://github.com/baedert/corebird/releases/download/1.4.2/corebird-1.4.2.tar.xz"
   sha256 "1c07a65382e78308f7de406be8464789c1ec42d531c519b69510a685234b4074"
 
+  bottle do
+    sha256 "a7401a2c8a4b172978e5a4eaa7f895c18ab97292188018f30ad7c6da93fabe94" => :sierra
+    sha256 "bd5c64459452e1890f6dce1faf0bbf3bc86bd95419d7f3042d37a68e28514f6b" => :el_capitan
+    sha256 "b40a0f4f3779ea105d2ace59490de15a18f3cee8be4dca61c29eb85d58e2c905" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "vala" => :build
   depends_on "gtk+3"
