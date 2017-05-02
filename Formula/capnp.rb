8375974250fd20b2ac8ec6084c8ea6960eae6479
Zhiming Wang <zmwangx@gmail.com>
2017-05-02 16:06:11 -0400

    capnp 0.6.0
    Closes #13177.
    
    Signed-off-by: Alexander Hamann <neutric@users.noreply.github.com>

--- Formula/capnp.rb
+++ Formula/capnp.rb
@@ -1,8 +1,8 @@
 class Capnp < Formula
   desc "Data interchange format and capability-based RPC system"
   homepage "https://capnproto.org/"
-  url "https://capnproto.org/capnproto-c++-0.5.3.1.tar.gz"
-  sha256 "04b5c56805895fd15751167ab04c99fa86602110ea7591ef611695de9923911b"
+  url "https://capnproto.org/capnproto-c++-0.6.0.tar.gz"
+  sha256 "e50911191afc44d6ab03b8e0452cf8c00fd0edfcd34b39f169cea6a53b0bf73e"
 
   bottle do
     cellar :any_skip_relocation
@@ -14,23 +14,8 @@ class Capnp < Formula
   needs :cxx11
   depends_on "cmake" => :build
 
-  resource "gtest" do
-    url "https://github.com/google/googletest/archive/release-1.7.0.tar.gz"
-    sha256 "f73a6546fdf9fce9ff93a5015e0333a8af3062a152a9ad6bcb772c96687016cc"
-  end
-
   def install
     ENV.cxx11
-
-    gtest = resource("gtest")
-    gtest.verify_download_integrity(gtest.fetch)
-    inreplace "src/CMakeLists.txt" do |s|
-      s.gsub! "https://github.com/google/googletest/archive/release-1.7.0.zip",
-              gtest.cached_download
-      s.gsub! "URL_MD5 ef5e700c8a0f3ee123e2e0209b8b4961",
-              "URL_HASH SHA256=#{gtest.checksum}"
-    end
-
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
       system "make", "install"
