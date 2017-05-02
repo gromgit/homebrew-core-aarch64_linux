3096a07ac7ca121ff7eb35f4201caa2d4a94d87e
Dominyk Tiller <dominyktiller@gmail.com>
2017-05-02 17:59:17 +0100

    pcsc-lite: remove duped period from keg_only

--- Formula/pcsc-lite.rb
+++ Formula/pcsc-lite.rb
@@ -11,7 +11,7 @@ class PcscLite < Formula
   end
 
   keg_only :provided_by_osx,
-    "pcsc-lite interferes with detection of macOS's PCSC.framework."
+    "pcsc-lite interferes with detection of macOS's PCSC.framework"
 
   def install
     system "./configure", "--disable-dependency-tracking",
