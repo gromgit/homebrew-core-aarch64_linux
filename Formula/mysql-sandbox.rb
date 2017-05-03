bcac550f4350ab3f4764025276d55ac3e3604d6d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-03 10:29:37 +0000

    mysql-sandbox: update 3.2.10 bottle.

--- Formula/mysql-sandbox.rb
+++ Formula/mysql-sandbox.rb
@@ -7,9 +7,9 @@ class MysqlSandbox < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "100c05966289927442c13344de62418fa2d32f8c7be18f6fb110a3af7dcedc54" => :sierra
-    sha256 "9108192928d844115393b269e5d47c09532c8a87c718682b87dd7fa74d2410fa" => :el_capitan
-    sha256 "3f0554af07d732939b2dfdfa3a9ab13afee4f85c2abb5cbf8e12a44ec4d65fad" => :yosemite
+    sha256 "e41e49bb52dee038f4b4f7106ca60610ea1569ff1fb383cf0201463bda2e0202" => :sierra
+    sha256 "2ac770766ecdc5eb8d373d5ca9db7f370ef03a95d0f00063332e58d1b30d12c7" => :el_capitan
+    sha256 "b26f7b2f7bddafa61e81d54bd650e4363f8118448448a3ad114ee22e10274dd2" => :yosemite
   end
 
   def install
