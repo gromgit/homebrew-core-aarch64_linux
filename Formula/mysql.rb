63813d45dfcb5e6c69b5a431942409fade9a3801
neutric <neutric@users.noreply.github.com>
2017-05-03 13:00:21 +0200

    mysql: remove superfluous cmake argument

--- Formula/mysql.rb
+++ Formula/mysql.rb
@@ -58,7 +58,6 @@ class Mysql < Formula
       -DINSTALL_DOCDIR=share/doc/#{name}
       -DINSTALL_INFODIR=share/info
       -DINSTALL_MYSQLSHAREDIR=share/mysql
-      -DWITH_SSL=yes
       -DWITH_SSL=system
       -DDEFAULT_CHARSET=utf8
       -DDEFAULT_COLLATION=utf8_general_ci
