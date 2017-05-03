dfbab4c147324d0e53f41a9712f24b6bc1108636
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-03 19:54:40 +0000

    git-remote-hg: update 0.3 bottle.

--- Formula/git-remote-hg.rb
+++ Formula/git-remote-hg.rb
@@ -9,10 +9,10 @@ class GitRemoteHg < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d9e8ae77b5ce8908031ddf5f4a1e166226ad2cc6664a98ab9edcb8c44b82c270" => :sierra
-    sha256 "0e85f91992caf6662783a2e53ffd4fbbb04c0600b702bb27f8ebe1a8b51bb683" => :el_capitan
-    sha256 "ffd2c6e7361d41e20209cf738b97292fda480ef78ab5264967be08ecb8103b7a" => :yosemite
-    sha256 "808c74fd0e1d5d5deb32e768f86393695075fa82359fd2b186604296606846da" => :mavericks
+    rebuild 1
+    sha256 "f0b8d090aaf5e8aa4b63418efcc8a0e6fc1fe7cafc39bee6a8bc85abd8c106db" => :sierra
+    sha256 "6af3e5642dbe91d832b035baf74d199cbda4af7bdb39b0a0d09d336098fa4693" => :el_capitan
+    sha256 "45529e66698b9505e61c718d43f46c99dc31ef2b37802939e17d391ede5ae912" => :yosemite
   end
 
   depends_on :hg
