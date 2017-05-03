0b91039a7dcad02018af8d60b77e1cc70504b1b9
ilovezfs <ilovezfs@icloud.com>
2017-05-03 08:04:11 -0700

    zinc 0.3.15 (#13195)

--- Formula/zinc.rb
+++ Formula/zinc.rb
@@ -1,8 +1,8 @@
 class Zinc < Formula
   desc "Stand-alone version of sbt's Scala incremental compiler"
   homepage "https://github.com/typesafehub/zinc"
-  url "https://downloads.typesafe.com/zinc/0.3.13/zinc-0.3.13.tgz"
-  sha256 "6ae329abb526afde4ee78480be1f2675310b067e3e143fbb02f429f6f816f996"
+  url "https://downloads.typesafe.com/zinc/0.3.15/zinc-0.3.15.tgz"
+  sha256 "5ec4df3fa2cbb271d65a5b478c940a9da6ef4902aa8c9d41a76dd253e3334ca7"
 
   bottle :unneeded
 
