804282db6d3c87146cae9627a3ecaacd1e3eb13d
ilovezfs <ilovezfs@icloud.com>
2017-05-04 10:12:51 -0700

    metricbeat 5.4.0
    Closes #13243.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/metricbeat.rb
+++ Formula/metricbeat.rb
@@ -1,8 +1,8 @@
 class Metricbeat < Formula
   desc "Collect metrics from your systems and services."
   homepage "https://www.elastic.co/products/beats/metricbeat"
-  url "https://github.com/elastic/beats/archive/v5.3.2.tar.gz"
-  sha256 "e8c765531276e4731c5148bd99fad1efc799f3ec68e1d32a2fe116995f2b9e2f"
+  url "https://github.com/elastic/beats/archive/v5.4.0.tar.gz"
+  sha256 "d236f9433d48b69cc2f0bf3617f17d5f2eec261acee96cc28d8b72be84afc0a7"
 
   head "https://github.com/elastic/beats.git"
 
