e7485374550bb6f331fdbba17da315cd16490314
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2017-05-04 13:46:53 -0300

    make: add gnubin and gnuman files
    Closes #13236.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/make.rb
+++ Formula/make.rb
@@ -4,6 +4,7 @@ class Make < Formula
   url "https://ftp.gnu.org/gnu/make/make-4.2.1.tar.bz2"
   mirror "https://ftpmirror.gnu.org/make/make-4.2.1.tar.bz2"
   sha256 "d6e262bf3601b42d2b1e4ef8310029e1dcf20083c5446b4b7aa67081fdffc589"
+  revision 1
 
   bottle do
     sha256 "d6a0f2bfd83a8299b6a7dfa4aa936ffcd03603da10c26cccad06137c668df894" => :sierra
@@ -28,6 +29,30 @@ class Make < Formula
 
     system "./configure", *args
     system "make", "install"
+
+    if build.without? "default-names"
+      (libexec/"gnubin").install_symlink bin/"gmake" =>"make"
+      (libexec/"gnuman/man1").install_symlink man1/"gmake.1" => "make.1"
+    end
+  end
+
+  def caveats
+    if build.without? "default-names"
+      <<-EOS.undent
+        All commands have been installed with the prefix 'g'.
+        If you do not want the prefix, install using the "with-default-names" option.
+
+        If you need to use these commands with their normal names, you
+        can add a "gnubin" directory to your PATH from your bashrc like:
+
+            PATH="#{opt_libexec}/gnubin:$PATH"
+
+        Additionally, you can access their man pages with normal names if you add
+        the "gnuman" directory to your MANPATH from your bashrc as well:
+
+            MANPATH="#{opt_libexec}/gnuman:$MANPATH"
+      EOS
+    end
   end
 
   test do
