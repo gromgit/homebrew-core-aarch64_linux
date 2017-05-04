98c76691d56de835f51329052942a9264b77a6bc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-04 22:52:50 +0000

    ne: update 3.1.0 bottle.

--- Formula/ne.rb
+++ Formula/ne.rb
@@ -6,10 +6,9 @@ class Ne < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "40cdbd64e9afa63534e2c4c9fcf768351eaf8aea1b1d9d301ec5b9188cac2ebd" => :sierra
-    sha256 "56d04a63498270ddfed02081ab5970bcc5751a45468ff08b205d639d2e8878ff" => :el_capitan
-    sha256 "78875d4f901208279d8d13da35d40d0fbe2278b549a0c33754280f77dcf864e1" => :yosemite
-    sha256 "8b7072ee6e35427d7343af22811fb9af854788a14725b4fc6516f6872b273157" => :mavericks
+    sha256 "6cb47aff1c91d22fd98578df10d6c17c1afaa6e493085e453954e540d0dacfa0" => :sierra
+    sha256 "b9f8988279a64a070cbadd618d3561b4ef8ed3b4fe3f474b4ea5a1dcbb0f3d2a" => :el_capitan
+    sha256 "b5c3a4e39341af06d04d2d233cea0ae56f81d2f1f52ece6ecb9982651794f225" => :yosemite
   end
 
   def install
