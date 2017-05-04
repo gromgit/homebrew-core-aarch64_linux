0ab90d39e3ca786c9f0b2fb44c2fd29880336cd2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-04 02:36:35 +0000

    gcc: update 7.1.0 bottle.

--- Formula/gcc.rb
+++ Formula/gcc.rb
@@ -11,9 +11,9 @@ class Gcc < Formula
   end
 
   bottle do
-    sha256 "9a05a75102206bfb3e37ff60b822421dd73c4a14b67f7acb258054317fd0f5ca" => :sierra
-    sha256 "c76a4b7294e41e410d37cd45d9c2f820c55001a7cfa82b71ac81d6a0051de5f4" => :el_capitan
-    sha256 "807d107ada2e8774a5bf1461d3f8dc636d1135bdfde5a64f03ccabc063a11328" => :yosemite
+    sha256 "a9e9e939786a0f13c75dfbfe47c12b1e0b0c577f2aae942dfc16d2f8d0fd487c" => :sierra
+    sha256 "e237e79704d0738745ecff5286d9466624ecf942761c2735467b04f1de6fbd68" => :el_capitan
+    sha256 "551031101225e76d9268f2ac5bdddc48a24c8dd7810aeccca8e95f481ae5b1e0" => :yosemite
   end
 
   option "with-jit", "Build just-in-time compiler"
