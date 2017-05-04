0963c879f5cc5811398201b36dc01941f71bb666
ilovezfs <ilovezfs@icloud.com>
2017-05-03 21:40:33 -0700

    apache-spark 2.1.1
    Closes #13217.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/apache-spark.rb
+++ Formula/apache-spark.rb
@@ -1,9 +1,9 @@
 class ApacheSpark < Formula
   desc "Engine for large-scale data processing"
   homepage "https://spark.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=spark/spark-2.1.0/spark-2.1.0-bin-hadoop2.7.tgz"
-  version "2.1.0"
-  sha256 "0834c775f38473f67cb122e0ec21074f800ced50c1ff1b9e37e222a0069dc5c7"
+  url "https://www.apache.org/dyn/closer.lua?path=spark/spark-2.1.1/spark-2.1.1-bin-hadoop2.7.tgz"
+  version "2.1.1"
+  sha256 "372ac4f73221c07696793101007a4f19e31566d1f0d9bd0e5205b6fb5b45bfc2"
   head "https://github.com/apache/spark.git"
 
   bottle :unneeded
