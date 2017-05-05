d097895c888772a20e90adc6871d617740924cdc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-05 03:56:19 +0000

    docker-machine-driver-vultr: add 1.2.2 bottle.

--- Formula/docker-machine-driver-vultr.rb
+++ Formula/docker-machine-driver-vultr.rb
@@ -6,6 +6,13 @@ class DockerMachineDriverVultr < Formula
 
   head "https://github.com/janeczku/docker-machine-vultr.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ec610463d4f1920712c203e11fed4a761d6962315f0ac08d06d0b1c34ab8a5bd" => :sierra
+    sha256 "d6880593f426c0bedd0d8bed2bf45077e1f1edb903c47516581a80c789d1d2a2" => :el_capitan
+    sha256 "64a72eb914d5f56a700ebed4e2d4ae9e3aeaa7a5ae9deb28fb033b00b3cae63b" => :yosemite
+  end
+
   depends_on "go" => :build
   depends_on "godep" => :build
   depends_on "docker-machine" => :recommended
