64e1c5ed6b83e0997b1c585a3d48fb3627e4fad5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-05 09:08:54 +0000

    glbinding: update 2.1.3 bottle.

--- Formula/glbinding.rb
+++ Formula/glbinding.rb
@@ -6,9 +6,9 @@ class Glbinding < Formula
 
   bottle do
     cellar :any
-    sha256 "0bfa8cc86b4780171fa6d3f0291b7a42df4ac0dad2515e3ebfc48a4251053a84" => :sierra
-    sha256 "e9f164a73222dfb97042204c213f2fba91362cfe60d736a1837124018447b807" => :el_capitan
-    sha256 "c0601d21497363fd8c855c0ae8dc8dd799fa9eabd2253aa03dacf8bc0fd18dc2" => :yosemite
+    sha256 "792fd850648cdeea9e5e8a699ba1554e3936b62419c3a0912c81ce22b58d096e" => :sierra
+    sha256 "c26c8b3e87d1721dc224a5b8c3438c451fc2661fb184f3d450cff051f61a64cd" => :el_capitan
+    sha256 "f8143d1a2fcf8a3d08d85b964d6325068c19c19565c463559dc9f264b65766ed" => :yosemite
   end
 
   option "with-glfw", "Enable tools that display OpenGL information for your system"
