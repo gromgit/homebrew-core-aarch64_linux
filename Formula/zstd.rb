c627f025134ab0b0d2880017d60364d1f3235d7e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-05 08:44:37 +0000

    zstd: update 1.2.0 bottle.

--- Formula/zstd.rb
+++ Formula/zstd.rb
@@ -6,9 +6,9 @@ class Zstd < Formula
 
   bottle do
     cellar :any
-    sha256 "22b77f70740763574e611dadf154c8ca3ba9810544b9121857b69c4d3faec696" => :sierra
-    sha256 "2d72e2f0f7a56200bf555a8b9c272b02ed66bf166a2e34040d03c7ecd82f7098" => :el_capitan
-    sha256 "52a1193dfb3d93c71c92bd66fe974efef6252d1e4367a158a701c74ea79aafe9" => :yosemite
+    sha256 "bc2ff6cb7373b79d53896ded9b54fbaf225dc24621dee26dbe9d482ff592274a" => :sierra
+    sha256 "70cd43a12ae9a638221725bb3ca770d8080ed437c3f9f34a8789e44642e40e65" => :el_capitan
+    sha256 "2d0e998725164b7cee9197f5cf5d254370250528adfb1b0328b65683fd2a1ce5" => :yosemite
   end
 
   option "without-pzstd", "Build without parallel (de-)compression tool"
