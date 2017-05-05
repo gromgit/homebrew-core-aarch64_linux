2277e290b5bb19c9f9c94fdac68bae395f593b71
Igor Bolic <igor.bolic@symbolic.rs>
2017-05-05 10:49:38 +0200

    jetty 9.4.5.v20170502

--- Formula/jetty.rb
+++ Formula/jetty.rb
@@ -1,9 +1,9 @@
 class Jetty < Formula
   desc "Java servlet engine and webserver"
   homepage "https://www.eclipse.org/jetty/"
-  url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-distribution/9.4.4.v20170414/jetty-distribution-9.4.4.v20170414.tar.gz"
-  version "9.4.4.v20170414"
-  sha256 "ca93c7f88e842fcb1e7bd551c071b3302b7be1faf9cad3ce415af19c77d6cb74"
+  url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-distribution/9.4.5.v20170502/jetty-distribution-9.4.5.v20170502.tar.gz"
+  version "9.4.5.v20170502"
+  sha256 "7fc0dbc7faf17994d349f38ff8e7ee5384e631d3e1d36dd6a09ab8f426977fd9"
 
   bottle :unneeded
 
