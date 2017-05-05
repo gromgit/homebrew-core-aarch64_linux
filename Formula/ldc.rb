9a0ad48b3d69bf61c5e9d320ce627143f7f76fc8
John Colvin <john.loughran.colvin@gmail.com>
2017-05-05 14:43:33 +0200

    ldc 1.3.0-beta1 (devel) (#13273)

--- Formula/ldc.rb
+++ Formula/ldc.rb
@@ -18,6 +18,17 @@ class Ldc < Formula
     sha256 "f29f90210d4e53f147fe67c30cee4527d5dfb326765c134dcdd0d7d41292ed70" => :yosemite
   end
 
+  devel do
+    url "https://github.com/ldc-developers/ldc/releases/download/v1.3.0-beta1/ldc-1.3.0-beta1-src.tar.gz"
+    version "1.3.0-beta1"
+    sha256 "35d5c37e967ccfa762572d0c5cb73e42617615d5eb16e43595a4bf239dd14e49"
+
+    resource "ldc-lts" do
+      url "https://github.com/ldc-developers/ldc/releases/download/v0.17.4/ldc-0.17.4-src.tar.gz"
+      sha256 "48428afde380415640f3db4e38529345f3c8485b1913717995547f907534c1c3"
+    end
+  end
+
   head do
     url "https://github.com/ldc-developers/ldc.git", :shallow => false
 
