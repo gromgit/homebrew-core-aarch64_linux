a2e2dc15a5bc5b2cded597f3eee989d1b519c12c
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-05-05 08:25:38 +0100

    libgnomecanvasmm: revision to rebuild bottles
    Closes #13261.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libgnomecanvasmm.rb
+++ Formula/libgnomecanvasmm.rb
@@ -3,7 +3,7 @@ class Libgnomecanvasmm < Formula
   homepage "https://launchpad.net/libgnomecanvasmm"
   url "https://download.gnome.org/sources/libgnomecanvasmm/2.26/libgnomecanvasmm-2.26.0.tar.bz2"
   sha256 "996577f97f459a574919e15ba7fee6af8cda38a87a98289e9a4f54752d83e918"
-  revision 2
+  revision 3
 
   bottle do
     cellar :any
