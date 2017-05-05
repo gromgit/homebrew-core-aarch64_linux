232144b8435737fef95c7c314d323d101cdec8f2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-05 12:45:33 +0000

    goad: add 1.4.1 bottle.

--- Formula/goad.rb
+++ Formula/goad.rb
@@ -5,6 +5,13 @@ class Goad < Formula
   homepage "https://goad.io/"
   url "https://github.com/goadapp/goad.git", :tag => "v1.4.1"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d1fb82dbc708716ae26f8d5b80bd56d727852c2045089c8830fd479ea2e66df1" => :sierra
+    sha256 "a7bca02de108e120aa1ab0e2dd83c1d2d1f665a4181cc577fc9c694442af1398" => :el_capitan
+    sha256 "dacd6c9ea183a71031cdad3506d76b836d77dc3eae5470388a1cf2bd7b662883" => :yosemite
+  end
+
   depends_on "go" => :build
 
   go_resource "github.com/jteeuwen/go-bindata" do
