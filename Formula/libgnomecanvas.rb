be18c6ba60e77b5d19c8314e76131cd51ea9564e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-05 07:57:35 +0000

    libgnomecanvas: update 2.30.3_2 bottle.

--- Formula/libgnomecanvas.rb
+++ Formula/libgnomecanvas.rb
@@ -7,11 +7,9 @@ class Libgnomecanvas < Formula
 
   bottle do
     cellar :any
-    sha256 "bece26f3628d89996c516bcc219f0d2ef8a52c13e562fe42f0a976d0d1cd63c0" => :sierra
-    sha256 "f16e2324c0d9a2f75f4133833230b9e51d9d5ac09132de0565b9556f81c6e301" => :el_capitan
-    sha256 "d92afd4e619d083844ab2eb3f36e8775512fb896c5caa677456c5d527e9d81e6" => :yosemite
-    sha256 "3ba01178cdc04412a402b043cc89fc59269573347c704f9c37281a84bf111988" => :mavericks
-    sha256 "c3aac3d57c825f6634d07130fb868e5325fd405a0e7583ab28756fca4515508d" => :mountain_lion
+    sha256 "df0745b46ef2872c5460a2a0893309d149d9365299d980fc66c395d410fc0b81" => :sierra
+    sha256 "53b98bae1958e60b6f47653aee1a75976a9b169b44bc670a34b54159ab333fe9" => :el_capitan
+    sha256 "0526ea2163ce27104da600bb45c847584f5843e6bfffe5ddc8ef24ebd9a2acd1" => :yosemite
   end
 
   depends_on "pkg-config" => :build
