16b69dc690c7cafa20d8812f5d2cd6f4346f55e8
ilovezfs <ilovezfs@icloud.com>
2017-05-05 21:06:41 -0700

    graphite2 1.3.10
    Closes #13295.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/graphite2.rb
+++ Formula/graphite2.rb
@@ -1,23 +1,9 @@
 class Graphite2 < Formula
   desc "Smart font renderer for non-Roman scripts"
   homepage "http://graphite.sil.org"
-  revision 1
-
+  url "https://github.com/silnrsi/graphite/releases/download/1.3.10/graphite2-1.3.10.tgz"
+  sha256 "90fde3b2f9ea95d68ffb19278d07d9b8a7efa5ba0e413bebcea802ce05cda1ae"
   head "https://github.com/silnrsi/graphite.git"
-
-  stable do
-    url "https://github.com/silnrsi/graphite/releases/download/1.3.9/graphite2-1.3.9.tgz"
-    # Debian mirror the release tarball, not the GitHub archive tarball.
-    mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/g/graphite2/graphite2_1.3.9.orig.tar.gz"
-    sha256 "ec0185b663059553fd46e8c4a4f0dede60a02f13a7a1fefc2ce70332ea814567"
-
-    # Patch for CVE-2017-5436.
-    # https://www.vuxml.org/freebsd/cf133acc-82e7-4755-a66a-5ddf90dacbe6.html
-    patch do
-      url "https://github.com/silnrsi/graphite/commit/1ce331d5548b.patch"
-      sha256 "39613db98f959b48adc2387d37a5f384566172b906d949edad452fcd48c3874c"
-    end
-  end
 
   bottle do
     cellar :any
