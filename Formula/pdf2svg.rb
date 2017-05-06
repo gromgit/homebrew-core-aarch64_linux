ebf1a51e70978e6e133972a09d353cbfcfebd78e
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-05-06 12:37:40 +0100

    pdf2svg: bottle rebuild
    Closes #13314.
    
    Signed-off-by: Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>

--- Formula/pdf2svg.rb
+++ Formula/pdf2svg.rb
@@ -3,6 +3,7 @@ class Pdf2svg < Formula
   homepage "http://www.cityinthesky.co.uk/opensource/pdf2svg"
   url "https://github.com/db9052/pdf2svg/archive/v0.2.3.tar.gz"
   sha256 "4fb186070b3e7d33a51821e3307dce57300a062570d028feccd4e628d50dea8a"
+  revision 1
 
   bottle do
     cellar :any
