436460c729d438f2fa3f3e5045589f02e945ef03
FX Coudert <fxcoudert@gmail.com>
2017-05-06 10:25:16 +0200

    libmms: remove Leopard patch

--- Formula/libmms.rb
+++ Formula/libmms.rb
@@ -16,14 +16,6 @@ class Libmms < Formula
   depends_on "pkg-config" => :build
   depends_on "glib"
 
-  # https://trac.macports.org/ticket/27988
-  if MacOS.version <= :leopard
-    patch :p0 do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/1fac7062/libmms/src_mms-common.h.patch"
-      sha256 "773193b878b7c061f05fe76f0ea5d331b8ab3e7b348608fae8cb144139e94798"
-    end
-  end
-
   def install
     ENV.append "LDFLAGS", "-liconv"
     system "./configure", "--disable-dependency-tracking",
