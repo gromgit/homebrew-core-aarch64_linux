7f85fc0a6367af1b9a94ea83da7774b1670a3417
FX Coudert <fxcoudert@gmail.com>
2017-05-06 10:47:35 +0200

    erlang@1.8: remove Leopard-specific build option

--- Formula/erlang@18.rb
+++ Formula/erlang@18.rb
@@ -72,10 +72,7 @@ class ErlangAT18 < Formula
     args << "--enable-native-libs" if build.with? "native-libs"
     args << "--enable-dirty-schedulers" if build.with? "dirty-schedulers"
     args << "--enable-wx" if build.with? "wxmac"
-
-    if MacOS.version >= :snow_leopard && MacOS::CLT.installed?
-      args << "--with-dynamic-trace=dtrace"
-    end
+    args << "--with-dynamic-trace=dtrace" if MacOS::CLT.installed?
 
     if build.without? "hipe"
       # HIPE doesn't strike me as that reliable on macOS
