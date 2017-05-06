f0c5f68123440a5f18b3279a663b31fe940d47e9
ilovezfs <ilovezfs@icloud.com>
2017-05-06 10:34:14 -0700

    docker-completion 17.05.0 (#13321)

--- Formula/docker-completion.rb
+++ Formula/docker-completion.rb
@@ -1,9 +1,9 @@
 class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://github.com/docker/docker"
-  url "https://github.com/docker/docker/archive/v17.04.0-ce.tar.gz"
-  version "17.04.0"
-  sha256 "76e84fbb7b5f1077f424ff40ddd14c98247f5aa3f49ad6412eede5a8cb11a29e"
+  url "https://github.com/moby/moby/archive/v17.05.0-ce.tar.gz"
+  version "17.05.0"
+  sha256 "4716df117d867b82ddab2e82395cd40aa3d0925a689eedcec8919729e4c9f121"
   head "https://github.com/docker/docker.git"
 
   bottle :unneeded
