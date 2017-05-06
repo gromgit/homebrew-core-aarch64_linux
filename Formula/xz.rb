6b3eeb9483cde8d90e663fc7572f012b674557e0
chdiza <chdiza@gmail.com>
2017-05-06 16:39:15 -0400

    xz: Use https for homepage and mirror (#13327)

--- Formula/xz.rb
+++ Formula/xz.rb
@@ -2,9 +2,9 @@
 # https://github.com/Homebrew/homebrew/pull/21419
 class Xz < Formula
   desc "General-purpose data compression with high compression ratio"
-  homepage "http://tukaani.org/xz/"
+  homepage "https://tukaani.org/xz/"
   url "https://fossies.org/linux/misc/xz-5.2.3.tar.gz"
-  mirror "http://tukaani.org/xz/xz-5.2.3.tar.gz"
+  mirror "https://tukaani.org/xz/xz-5.2.3.tar.gz"
   sha256 "71928b357d0a09a12a4b4c5fafca8c31c19b0e7d3b8ebb19622e96f26dbf28cb"
 
   bottle do
