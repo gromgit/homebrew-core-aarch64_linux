6af3a4b4d4100082cc44664903d9a65541ef8d40
ilovezfs <ilovezfs@icloud.com>
2017-05-05 21:08:53 -0700

    gws 0.1.10

--- Formula/gws.rb
+++ Formula/gws.rb
@@ -1,8 +1,8 @@
 class Gws < Formula
   desc "Manage workspaces composed of git repositories"
   homepage "https://streakycobra.github.io/gws/"
-  url "https://github.com/StreakyCobra/gws/archive/0.1.8.tar.gz"
-  sha256 "c240601b0adcc5ae402199217fc0d5fd6775ada69919a860ad3c1c4b16805e63"
+  url "https://github.com/StreakyCobra/gws/archive/0.1.10.tar.gz"
+  sha256 "816592f3fec8c437ba96d17dbbfa911d88d1fa70071d3d127efc64f73551f747"
 
   bottle :unneeded
 
