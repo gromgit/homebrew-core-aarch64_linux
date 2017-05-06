aebe58625837dec4e7eb4ea4af5a7d11a22c310e
FX Coudert <fxcoudert@gmail.com>
2017-05-06 10:50:48 +0200

    postgresql: remove Leopard-specific dependency

--- Formula/postgresql.rb
+++ Formula/postgresql.rb
@@ -22,7 +22,6 @@ class Postgresql < Formula
 
   depends_on "openssl"
   depends_on "readline"
-  depends_on "libxml2" if MacOS.version <= :leopard # Leopard libxml is too old
 
   option "with-python", "Enable PL/Python2"
   depends_on :python => :optional
