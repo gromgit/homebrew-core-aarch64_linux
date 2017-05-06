006ef704d1662bda229ad678dbcd1a6b39122af6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-06 11:52:57 +0000

    pdf2svg: update 0.2.3_1 bottle.

--- Formula/pdf2svg.rb
+++ Formula/pdf2svg.rb
@@ -7,11 +7,9 @@ class Pdf2svg < Formula
 
   bottle do
     cellar :any
-    sha256 "5dc857315e0833882c51d0814dd38c6cdede0979937e3838b31cdbadd4dd279e" => :sierra
-    sha256 "0c3e08bcf229edd83952486a7a8d7398c25fce0bb5c2ec6c72e50c33ce31fcbd" => :el_capitan
-    sha256 "786c55feabe3accf358cd8749e5579cabab96c8c33098ab8f4164227c864bd01" => :yosemite
-    sha256 "7d4235fa396d76c58637be177306ab52ac8b97e2d8fd4010349f8f447117426e" => :mavericks
-    sha256 "1a85e25f7769ec7a37357fb628953959e8a8216f875df894bc64bb400ed808a7" => :mountain_lion
+    sha256 "72720d163c7b21b7aedcd00ef9730b6173f3928082eb404bc5445f13a7284ca1" => :sierra
+    sha256 "37a695cd77dbd082006a9d4581f1da885aeb6b3e557cbc679fe551b150c317df" => :el_capitan
+    sha256 "cff3cfd6fa07b5af5074540e4f7329160b1ecd71819310018a36ebf0a0b8b34f" => :yosemite
   end
 
   depends_on "pkg-config" => :build
