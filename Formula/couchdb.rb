6c749b1a4039f13ecfe0fc8bd3ae378d0bad504b
FX Coudert <fxcoudert@gmail.com>
2017-05-06 10:50:47 +0200

    couchdb: remove Leopard-specific dependency

--- Formula/couchdb.rb
+++ Formula/couchdb.rb
@@ -35,7 +35,6 @@ class Couchdb < Formula
   depends_on "spidermonkey"
   depends_on "icu4c"
   depends_on "erlang"
-  depends_on "curl" if MacOS.version <= :leopard
 
   resource "geocouch" do
     url "https://github.com/couchbase/geocouch/archive/couchdb1.3.x.tar.gz"
