d952551f1b75cfccf4ef92dd2667cb5ef01ce31e
FX Coudert <fxcoudert@gmail.com>
2017-05-07 11:20:53 +0200

    ruby@2.0: remove universal

--- Formula/ruby@2.0.rb
+++ Formula/ruby@2.0.rb
@@ -33,11 +33,6 @@ class RubyAT20 < Formula
       --with-vendordir=#{HOMEBREW_PREFIX}/lib/ruby/vendor_ruby
     ]
 
-    if build.universal?
-      ENV.universal_binary
-      args << "--with-arch=#{Hardware::CPU.universal_archs.join(",")}"
-    end
-
     args << "--program-suffix=#{program_suffix}" if build.with? "suffix"
     args << "--with-out-ext=tk" if build.without? "tcltk"
     args << "--disable-install-doc" if build.without? "doc"
