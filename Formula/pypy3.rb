60be5ed139ba4fc30b18b9e3198088b4872c2198
Jeethu Rao <jeethu@jeethurao.com>
2017-05-08 00:34:38 +0100

    pypy3 5.7.1
    Support for installing without bootstrap
    
    Closes #13354.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Formula/pypy3.rb
+++ Formula/pypy3.rb
@@ -1,8 +1,8 @@
 class Pypy3 < Formula
   desc "Implementation of Python 3 in Python"
   homepage "https://pypy.org/"
-  url "https://dl.bintray.com/homebrew/mirror/pypy3-5.5.0.tar.bz2"
-  sha256 "d5591c34d77253e9ed57d182b6f49585b95f7c09c3e121f0e8630e5a7e75ab5f"
+  url "https://bitbucket.org/pypy/pypy/downloads/pypy3-v5.7.1-src.tar.bz2"
+  sha256 "40ece0145282980ac121390f13709404c0532896507d5767496381180b631bd0"
 
   bottle do
     cellar :any
@@ -24,13 +24,37 @@ class Pypy3 < Formula
   depends_on "xz" => :recommended
 
   resource "bootstrap" do
-    url "https://bitbucket.org/pypy/pypy/downloads/pypy-2.5.0-osx64.tar.bz2"
-    sha256 "30b392b969b54cde281b07f5c10865a7f2e11a229c46b8af384ca1d3fe8d4e6e"
+    url "https://bitbucket.org/pypy/pypy/downloads/pypy2-v5.7.1-osx64.tar.bz2"
+    sha256 "4e99ba356432861534917a9477ace0ccee617bd631512759a530f8383e153a3d"
+  end
+
+  # packaging depends on pyparsing
+  resource "pyparsing" do
+    url "https://pypi.python.org/packages/3c/ec/a94f8cf7274ea60b5413df054f82a8980523efd712ec55a59e7c3357cf7c/pyparsing-2.2.0.tar.gz"
+    sha256 "0832bcf47acd283788593e7a0f542407bd9550a55a8a8435214a1960e04bcb04"
+  end
+
+  # packaging and setuptools depend on six
+  resource "six" do
+    url "https://pypi.python.org/packages/b3/b2/238e2590826bfdd113244a40d9d3eb26918bd798fc187e2360a8367068db/six-1.10.0.tar.gz"
+    sha256 "105f8d68616f8248e24bf0e9372ef04d3cc10104f1980f54d57b2ce73a5ad56a"
+  end
+
+  # setuptools depends on packaging
+  resource "packaging" do
+    url "https://pypi.python.org/packages/c6/70/bb32913de251017e266c5114d0a645f262fb10ebc9bf6de894966d124e35/packaging-16.8.tar.gz"
+    sha256 "5d50835fdf0a7edf0b55e311b7c887786504efea1177abd7e69329a8e5ea619e"
+  end
+
+  # setuptools depends on appdirs
+  resource "appdirs" do
+    url "https://pypi.python.org/packages/48/69/d87c60746b393309ca30761f8e2b49473d43450b150cb08f3c6df5c11be5/appdirs-1.4.3.tar.gz"
+    sha256 "9e5896d1372858f8dd3344faf4e5014d21849c756c8d5701f78f8a103b372d92"
   end
 
   resource "setuptools" do
-    url "https://pypi.python.org/packages/25/4e/1b16cfe90856235a13872a6641278c862e4143887d11a12ac4905081197f/setuptools-28.8.0.tar.gz"
-    sha256 "432a1ad4044338c34c2d09b0ff75d509b9849df8cf329f4c1c7706d9c2ba3c61"
+    url "https://pypi.python.org/packages/88/13/7d560b75334a8e4b4903f537b7e5a1ad9f1a2f1216e2587aaaf91b38c991/setuptools-35.0.2.zip"
+    sha256 "1e55496ca8058db68ae12ac29a985d1ee2c2483a5901f7692fb68fa2f9a250fd"
   end
 
   resource "pip" do
@@ -38,15 +62,19 @@ class Pypy3 < Formula
     sha256 "09f243e1a7b461f654c26a725fa373211bb7ff17a9300058b205c61658ca940d"
   end
 
+  resource "pycparser" do
+    url "https://pypi.python.org/packages/be/64/1bb257ffb17d01f4a38d7ce686809a736837ad4371bcc5c42ba7a715c3ac/pycparser-2.17.tar.gz"
+    sha256 "0aac31e917c24cb3357f5a4d5566f2cc91a19ca41862f6c3c22dc60a629673b6"
+  end
+
   # https://bugs.launchpad.net/ubuntu/+source/gcc-4.2/+bug/187391
   fails_with :gcc
 
-  # Disable clock_gettime() use on Darwin; applied upstream.
-  # This fixes 10.11 when built using the Xcode 8 SDK.
-  # See: https://github.com/Homebrew/homebrew-core/issues/6949
+  # Change _cffi_src/openssl/callbacks.py to stop relying on the CPython C API.
+  # See: https://bitbucket.org/pypy/pypy/issues/2538/
   patch do
-    url "https://bitbucket.org/pypy/pypy/commits/91e202bbd0b983c88fa9c33b9215b0f910d1f405/raw"
-    sha256 "7a5f5d1c3c0e7bd1652c4d17018d8c1328338b73858712c02c41ef563a04314c"
+    url "https://bitbucket.org/pypy/pypy/commits/198dc138680f96c391802fa1e77b8b6d2e0134e6/raw"
+    sha256 "547bfc444ae4e8f9ae22ebad66d20ab1fe0c5e174a160419fb97860d1595b612"
   end
 
   def install
@@ -60,6 +88,14 @@ class Pypy3 < Formula
     if build.with?("bootstrap") && MacOS.prefer_64_bit?
       resource("bootstrap").stage buildpath/"bootstrap"
       python = buildpath/"bootstrap/bin/pypy"
+    end
+
+    # PyPy 5.7.1 needs either cffi or pycparser to build
+    if build.without?("bootstrap")
+      %w[pycparser].each do |pkg|
+        resource(pkg).stage buildpath/"pycparser"
+        ENV.append "PYTHONPATH", buildpath/"pycparser"
+      end
     end
 
     cd "pypy/goal" do
@@ -77,10 +113,10 @@ class Pypy3 < Formula
       system "tar", "-C", libexec.to_s, "--strip-components", "1", "-xzf", "pypy3.tar.bz2"
     end
 
-    (libexec/"lib").install libexec/"bin/libpypy-c.dylib" => "libpypy3-c.dylib"
+    (libexec/"lib").install libexec/"bin/libpypy3-c.dylib" => "libpypy3-c.dylib"
 
-    MachO::Tools.change_install_name("#{libexec}/bin/pypy3.3",
-                                     "@rpath/libpypy-c.dylib",
+    MachO::Tools.change_install_name("#{libexec}/bin/pypy3",
+                                     "@rpath/libpypy3-c.dylib",
                                      "#{libexec}/lib/libpypy3-c.dylib")
     MachO::Tools.change_dylib_id("#{libexec}/lib/libpypy3-c.dylib",
                                  "#{opt_libexec}/lib/libpypy3-c.dylib")
@@ -93,7 +129,7 @@ class Pypy3 < Formula
     # we want to avoid putting PyPy's Python.h somewhere that configure
     # scripts will find it.
     bin.install_symlink libexec/"bin/pypy3"
-    bin.install_symlink libexec/"bin/pypy3.3"
+    bin.install_symlink libexec/"bin/pypy3.5"
     lib.install_symlink libexec/"lib/libpypy3-c.dylib"
   end
 
@@ -121,7 +157,7 @@ class Pypy3 < Formula
       install-scripts=#{scripts_folder}
     EOF
 
-    %w[setuptools pip].each do |pkg|
+    %w[appdirs pyparsing six packaging setuptools pip].each do |pkg|
       resource(pkg).stage do
         system bin/"pypy3", "-s", "setup.py", "install", "--force", "--verbose"
       end
