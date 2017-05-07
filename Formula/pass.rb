9c4f788f512c3fdb9f83dace59b2194a778c13bd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-07 00:57:30 +0000

    pass: update 1.7.1 bottle.

--- Formula/pass.rb
+++ Formula/pass.rb
@@ -9,10 +9,9 @@ class Pass < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "4f0795fd255e6881acf8151ce6ae2813767f85b4a821a0879e8e4672aa4367a9" => :sierra
-    sha256 "4041972f360ddebf962fe51d4240e90072701bc4235ef170c5ea97a10308a3e8" => :el_capitan
-    sha256 "4041972f360ddebf962fe51d4240e90072701bc4235ef170c5ea97a10308a3e8" => :yosemite
+    sha256 "cde7a0225dbcd62b9cc9d8e5e8d9ed8e0534bbe590cb3cc974cc31a8177defdd" => :sierra
+    sha256 "692b37f152b5bad4d841323c7938b8d1f94c858026d7ad941b7a270ee9503705" => :el_capitan
+    sha256 "692b37f152b5bad4d841323c7938b8d1f94c858026d7ad941b7a270ee9503705" => :yosemite
   end
 
   depends_on "qrencode"
