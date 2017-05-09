1337323297ddd3f2abecf934d48529da11eb22f0
Zhiming Wang <zmwangx@gmail.com>
2017-05-09 15:30:41 -0400

    mypy 0.511
    Closes #13403.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mypy.rb
+++ Formula/mypy.rb
@@ -2,8 +2,8 @@ class Mypy < Formula
   desc "Experimental optional static type checker for Python"
   homepage "http://www.mypy-lang.org/"
   url "https://github.com/python/mypy.git",
-      :tag => "v0.510",
-      :revision => "12e9107bd0493fb7cdf2d7c07e29c6ef8ca37942"
+      :tag => "v0.511",
+      :revision => "88e8ef28be2465d1693ace4d39cd422987a97220"
   head "https://github.com/python/mypy.git"
 
   bottle do
@@ -21,13 +21,13 @@ class Mypy < Formula
   depends_on "sphinx-doc" => [:build, :recommended]
 
   resource "sphinx_rtd_theme" do
-    url "https://files.pythonhosted.org/packages/99/b5/249a803a428b4fd438dd4580a37f79c0d552025fb65619d25f960369d76b/sphinx_rtd_theme-0.1.9.tar.gz"
-    sha256 "273846f8aacac32bf9542365a593b495b68d8035c2e382c9ccedcac387c9a0a1"
+    url "https://files.pythonhosted.org/packages/8b/e5/b1933472424b30affb0a8cea8f0ef052a31ada96e5d1823911d7f4bfdf8e/sphinx_rtd_theme-0.2.4.tar.gz"
+    sha256 "2df74b8ff6fae6965c527e97cca6c6c944886aae474b490e17f92adfbe843417"
   end
 
   resource "typed-ast" do
-    url "https://files.pythonhosted.org/packages/1e/5e/ca6cef7a04c6c5df26b827e6cdca71af047fcf4d439b28a0f7bbf3b9a720/typed-ast-1.0.1.zip"
-    sha256 "b5f578a05498922300b8150716f9689ec4c3e7071f99f6568eed73e68bfa5983"
+    url "https://files.pythonhosted.org/packages/89/3d/9684616ba2b69ed73cc51396d777544e8379806fce1d60731b2237c3063c/typed-ast-1.0.3.tar.gz"
+    sha256 "67184179697ea9128fa8fec1d3b4e26b41d6a2eceab4674c6e3da4b024309862"
   end
 
   def install
