da80330d2cc0b91e3b9ea129a8827bd29e1023c4
Gary Bernhardt <gary.bernhardt@gmail.com>
2017-05-09 00:29:49 -0700

    serveit 0.0.2 (#13379)

--- Formula/serveit.rb
+++ Formula/serveit.rb
@@ -1,8 +1,8 @@
 class Serveit < Formula
-  desc "synchronous server and rebuilder of static content"
+  desc "Synchronous server and rebuilder of static content"
   homepage "https://github.com/garybernhardt/serveit"
-  url "https://github.com/garybernhardt/serveit/archive/v0.0.1.tar.gz"
-  sha256 "8cf25c80ea9b9fe0383545d083e305f7723b6dec7e70d29a4e15016264861f49"
+  url "https://github.com/garybernhardt/serveit/archive/v0.0.2.tar.gz"
+  sha256 "d7a04f2fbd97f90c31e1838da952cbf5fd8844abeabce0a88569e3d279488ad4"
   head "https://github.com/garybernhardt/serveit.git"
 
   bottle :unneeded
