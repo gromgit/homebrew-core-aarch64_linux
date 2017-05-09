469251c8f7f2e841aa7eaad31bdbbb2338102b47
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-09 22:05:53 +0000

    consul-template: update 0.18.3 bottle.

--- Formula/consul-template.rb
+++ Formula/consul-template.rb
@@ -8,9 +8,9 @@ class ConsulTemplate < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "4810a80f495db526959e6f830cf95c6d219b99ddb4915e934b98797bd95552e0" => :sierra
-    sha256 "4a131cec77f31b38e8821372adc7f2196cfe7723613c53db41f6cd1c4dfc68bd" => :el_capitan
-    sha256 "2e56bcf8fff635631457330a43ee4e5f24901102674020958b5698fbd5c3598e" => :yosemite
+    sha256 "aa4acaaff0f85ee7b7860e3b18746d0404657a1c9244999817e06f52c145bf6e" => :sierra
+    sha256 "451ae2b901a7d1f28398dbe2be704257869b8e37f89f173bf072adae6a8418e9" => :el_capitan
+    sha256 "cf358ac82b4c6ca739ac62b5d1eb092902a5d3d6d38f22b600c9491c3abfd114" => :yosemite
   end
 
   depends_on "go" => :build
