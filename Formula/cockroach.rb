19f09ab963cb588eac1743a75cdcdbd797253cca
Nikhil Benesch <nikhil.benesch@gmail.com>
2017-05-10 11:29:28 -0400

    cockroach 1.0 (new formula)
    Closes #13431.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/cockroach.rb
@@ -0,0 +1,81 @@
+class Cockroach < Formula
+  desc "Distributed SQL database"
+  homepage "https://www.cockroachlabs.com"
+  url "https://binaries.cockroachdb.com/cockroach-v1.0.src.tgz"
+  version "1.0"
+  sha256 "ca87b10eec688195e0df4f85431b019f2980ae4b511ee321f91f945315efeb76"
+  head "https://github.com/cockroachdb/cockroach.git"
+
+  depends_on "cmake" => :build
+  depends_on "go" => :build
+  depends_on "xz" => :build
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  def caveats; <<-EOS.undent
+    For local development only, this formula ships a launchd configuration to
+    start a single-node cluster that stores its data under:
+      #{var}/cockroach/
+    Instead of the default port of 8080, the node serves its admin UI at:
+      #{Formatter.url("http://localhost:26256")}
+
+    Do NOT use this cluster to store data you care about; it runs in insecure
+    mode and may expose data publicly in e.g. a DNS rebinding attack. To run
+    CockroachDB securely, please see:
+      #{Formatter.url("https://www.cockroachlabs.com/docs/secure-a-cluster.html")}
+    EOS
+  end
+
+  plist_options :manual => "cockroach start --insecure"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/cockroach</string>
+        <string>start</string>
+        <string>--store=#{var}/cockroach/</string>
+        <string>--http-port=26256</string>
+        <string>--insecure</string>
+        <string>--host=localhost</string>
+      </array>
+      <key>WorkingDirectory</key>
+      <string>#{var}</string>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <true/>
+    </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    begin
+      # Redirect stdout and stderr to a file, or else  `brew test --verbose`
+      # will hang forever as it waits for stdout and stderr to close.
+      system "#{bin}/cockroach start --insecure --background &> start.out"
+      pipe_output("#{bin}/cockroach sql --insecure", <<-EOS.undent)
+        CREATE DATABASE bank;
+        CREATE TABLE bank.accounts (id INT PRIMARY KEY, balance DECIMAL);
+        INSERT INTO bank.accounts VALUES (1, 1000.50);
+      EOS
+      output = pipe_output("#{bin}/cockroach sql --insecure --format=csv",
+        "SELECT * FROM bank.accounts;")
+      assert_equal <<-EOS.undent, output
+        1 row
+        id,balance
+        1,1000.50
+      EOS
+    ensure
+      system "#{bin}/cockroach", "quit", "--insecure"
+    end
+  end
+end
