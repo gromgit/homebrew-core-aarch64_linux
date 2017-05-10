b50a8bf0a42a26f87dafdbaebcc62b882f49703d
Gabe Levi <glevi@fb.com>
2017-05-10 11:44:52 -0400

    flow 0.46.0
    Closes #13432.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/flow.rb
+++ Formula/flow.rb
@@ -1,26 +1,9 @@
 class Flow < Formula
   desc "Static type checker for JavaScript"
   homepage "https://flowtype.org/"
+  url "https://github.com/facebook/flow/archive/v0.46.0.tar.gz"
+  sha256 "f6991604d95285c0944cab4b1b075facae53a4dd59bd836ee24cacd7f85b42a7"
   head "https://github.com/facebook/flow.git"
-
-  stable do
-    url "https://github.com/facebook/flow/archive/v0.45.0.tar.gz"
-    sha256 "9a76cb1669d5d1f07a55b3163edb0329c46565033eaf7ed9320058b6e3a9cbbf"
-
-    # Remove for > 0.45.0
-    # Upstream commit from 28 Apr 2017 "Add `make all-homebrew`"
-    patch do
-      url "https://github.com/facebook/flow/commit/8a811a4b443ec545d89aa201d5ebcc254ae220e4.patch"
-      sha256 "f92aca59e2014b91974c02672e0d55b3b278d3a267150cfb534d266befda4ad4"
-    end
-
-    # Remove for > 0.45.0
-    # Upstream commit from 28 Apr 2017 "mktemp's interface changed in OSX 10.11"
-    patch do
-      url "https://github.com/facebook/flow/commit/7cc0f0b3f34e6932e7a713de2a651a1848eef395.patch"
-      sha256 "d7eac833d7ee1dd7ee6ec47dfdc4f68e9b31555d3d39670d675ec81ec8a5127d"
-    end
-  end
 
   bottle do
     cellar :any_skip_relocation
