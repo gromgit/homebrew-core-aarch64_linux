2246e0e33525111beea8a694fa4d5f70eda15af2
Viktor Szakats <vszakats@users.noreply.github.com>
2017-05-10 19:21:27 +0200

    unifdef: secure url(s)

--- Formula/unifdef.rb
+++ Formula/unifdef.rb
@@ -1,7 +1,7 @@
 class Unifdef < Formula
   desc "Selectively process conditional C preprocessor directives"
-  homepage "http://dotat.at/prog/unifdef/"
-  url "http://dotat.at/prog/unifdef/unifdef-2.11.tar.gz"
+  homepage "https://dotat.at/prog/unifdef/"
+  url "https://dotat.at/prog/unifdef/unifdef-2.11.tar.gz"
   sha256 "e8483c05857a10cf2d5e45b9e8af867d95991fab0f9d3d8984840b810e132d98"
   head "https://github.com/fanf2/unifdef.git"
 
