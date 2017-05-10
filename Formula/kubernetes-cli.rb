a9b0aeb1dcdb44e6da3a02f14b0e7767b598cb37
Christoph Blecker <admin@toph.ca>
2017-05-10 16:24:41 -0700

    kubernetes-cli 1.6.3
    Closes #13446.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/kubernetes-cli.rb
+++ Formula/kubernetes-cli.rb
@@ -2,8 +2,8 @@ class KubernetesCli < Formula
   desc "Kubernetes command-line interface"
   homepage "https://kubernetes.io/"
   url "https://github.com/kubernetes/kubernetes.git",
-      :tag => "v1.6.2",
-      :revision => "477efc3cbe6a7effca06bd1452fa356e2201e1ee"
+      :tag => "v1.6.3",
+      :revision => "0480917b552be33e2dba47386e51decb1a211df6"
   head "https://github.com/kubernetes/kubernetes.git"
 
   bottle do
@@ -11,13 +11,6 @@ class KubernetesCli < Formula
     sha256 "d279c2ccfd4c7b292d17f986b3343daa6fc1f623c5ca6efdb47be596ec5ea783" => :sierra
     sha256 "abe4db7246df1d5df3007bfebedaeb6114ea19fb393940f44599c081deee47bd" => :el_capitan
     sha256 "154a2032c3b757b60efeb13e827c1ae63381a25f870783f07fd13611967bdd71" => :yosemite
-  end
-
-  devel do
-    url "https://github.com/kubernetes/kubernetes.git",
-        :tag => "v1.6.3-beta.0",
-        :revision => "a5dec22fe9062288a6c79ef77cc5cfdd0f9c7a89"
-    version "1.6.3-beta.0"
   end
 
   depends_on "go" => :build
