a2454041a5dcaeca2606b0572d07d46917a4fa00
JCount <JCount42@gmail.com>
2017-05-10 13:13:06 -0400

    sslscan: use static vendor of insecure openssl 1.0.2f
    Closes #13439.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/sslscan.rb
+++ Formula/sslscan.rb
@@ -4,6 +4,7 @@ class Sslscan < Formula
   url "https://github.com/rbsec/sslscan/archive/1.11.10-rbsec.tar.gz"
   version "1.11.10"
   sha256 "fbb26fdbf2cf5b2f3f8c88782721b7875f206552cf83201981411e0af9521204"
+  revision 1
   head "https://github.com/rbsec/sslscan.git"
 
   bottle do
@@ -13,14 +14,27 @@ class Sslscan < Formula
     sha256 "0537cb08b5d621601741a9b92d9a0bcec978242b3ca5830711a49a3ebf99036b" => :yosemite
   end
 
-  depends_on "openssl"
+  resource "insecure-openssl" do
+    url "https://github.com/openssl/openssl/archive/OpenSSL_1_0_2f.tar.gz"
+    sha256 "4c9492adcb800ec855f11121bd64ddff390160714d93f95f279a9bd7241c23a6"
+  end
 
   def install
-    system "make"
+    (buildpath/"openssl").install resource("insecure-openssl")
+
+    # prevent sslscan from fetching the tip of the openssl fork
+    # at https://github.com/PeterMosmans/openssl
+    inreplace "Makefile", "openssl/Makefile: .openssl.is.fresh",
+                          "openssl/Makefile:"
+
+    ENV.deparallelize do
+      system "make", "static"
+    end
     system "make", "install", "PREFIX=#{prefix}"
   end
 
   test do
+    assert_match "static", shell_output("#{bin}/sslscan --version")
     system "#{bin}/sslscan", "google.com"
   end
 end
