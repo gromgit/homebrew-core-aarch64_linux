6130283587dc2e2f3a8067cc1c46ad42d62cb717
Viktor Szakats <vszakats@users.noreply.github.com>
2017-05-10 19:24:19 +0200

    icbirc: secure url(s)

--- Formula/icbirc.rb
+++ Formula/icbirc.rb
@@ -1,7 +1,7 @@
 class Icbirc < Formula
   desc "Proxy IRC client and ICB server"
-  homepage "http://www.benzedrine.ch/icbirc.html"
-  url "http://www.benzedrine.ch/icbirc-2.0.tar.gz"
+  homepage "https://www.benzedrine.ch/icbirc.html"
+  url "https://www.benzedrine.ch/icbirc-2.0.tar.gz"
   sha256 "7607c7d80fc3939ccb913c9fcc57a63d3552af3615454e406ff0e3737c0ce6bd"
 
   bottle do
