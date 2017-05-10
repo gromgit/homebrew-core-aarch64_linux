6a09d4ca1766e0224b952b754b72c518b143dcbd
Viktor Szakats <vszakats@users.noreply.github.com>
2017-05-10 19:16:54 +0200

    adns: secure url(s)

--- Formula/adns.rb
+++ Formula/adns.rb
@@ -1,7 +1,7 @@
 class Adns < Formula
   desc "C/C++ resolver library and DNS resolver utilities"
-  homepage "http://www.chiark.greenend.org.uk/~ian/adns/"
-  url "http://www.chiark.greenend.org.uk/~ian/adns/ftp/adns-1.5.1.tar.gz"
+  homepage "https://www.chiark.greenend.org.uk/~ian/adns/"
+  url "https://www.chiark.greenend.org.uk/~ian/adns/ftp/adns-1.5.1.tar.gz"
   sha256 "5b1026f18b8274be869245ed63427bf8ddac0739c67be12c4a769ac948824eeb"
   head "git://git.chiark.greenend.org.uk/~ianmdlvl/adns.git"
 
