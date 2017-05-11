dabb610cc4d0aee493d0e10cb878c65b21ed883a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-11 18:23:53 +0000

    urbit: update 0.4.5 bottle.

--- Formula/urbit.rb
+++ Formula/urbit.rb
@@ -6,9 +6,9 @@ class Urbit < Formula
 
   bottle do
     cellar :any
-    sha256 "cd025199fe0e5400355a7f18e9b49dc1a6726ec0479a02bf9d55b57087796f77" => :sierra
-    sha256 "bba9db5b2cca6916fade1d258b98f349532eeb85b39c4b3879125a0a96bb0ecd" => :el_capitan
-    sha256 "200efba13f31b9632ff2d6b8622e9a80965d94d02eb5178850c52105ff0fe517" => :yosemite
+    sha256 "6aa8484fbfaa20cd2b2b53b8de1cea7b342fe2a34185a91a680d6d544ac93d1f" => :sierra
+    sha256 "fa9109dff4cde264e6581f81e9bd30574081fd94ebff4436888d77460db4b8ad" => :el_capitan
+    sha256 "5544b9553137481df6e2035a4e0a0b022f362fab12f2b3047cc206a93f79cc5c" => :yosemite
   end
 
   depends_on "gmp"
