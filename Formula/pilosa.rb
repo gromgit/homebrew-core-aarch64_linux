f87dcb64e064ce6ff6483afa67003e77246f4041
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-11 21:22:20 +0000

    pilosa: add 0.3.2 bottle.

--- Formula/pilosa.rb
+++ Formula/pilosa.rb
@@ -4,6 +4,13 @@ class Pilosa < Formula
   url "https://github.com/pilosa/pilosa/archive/v0.3.2.tar.gz"
   sha256 "12c5c5c2d511c655f02836e09d09cfafcb9eaa656a43850ddc6f6f9a2151b896"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c9f0bb531a7c89f388792be61349d623e5a1e4101349a12cbf34904c6bfea3e6" => :sierra
+    sha256 "2c6f9b38ef3881c07769d7ceb166b94f9af67ec8c8e2c8271b4ee61ed3e15489" => :el_capitan
+    sha256 "91238f37a2d26db10541cc5ee2343c5c0ff114ac254cde88c0bca54f44d09c83" => :yosemite
+  end
+
   depends_on "go" => :build
   depends_on "glide" => :build
 
