eb047aba917bc5ce4a1f3a000d6526e9fecf810d
Aleksey Cheusov <vle@gmx.net>
2017-05-12 02:09:41 +0300

    mk-configure 0.30.0 (new formula)
    Co-authored-by: Misty De Meo <mistydemeo@gmail.com>

---
+++ Formula/mk-configure.rb
@@ -0,0 +1,22 @@
+class MkConfigure < Formula
+  desc "Lightweight replacement for GNU autotools"
+  homepage "https://github.com/cheusov/mk-configure"
+  url "https://downloads.sourceforge.net/project/mk-configure/mk-configure/mk-configure-0.30.0/mk-configure-0.30.0.tar.gz"
+  sha256 "5c8f953d82a2d36f9492cafcad61d34e81b892d1d91b401af406b086046ef227"
+
+  depends_on "bmake"
+  depends_on "makedepend"
+
+  def install
+    ENV["PREFIX"] = prefix
+    ENV["MANDIR"] = man
+
+    system "bmake", "all"
+    system "bmake", "install"
+    doc.install "presentation/presentation.pdf"
+  end
+
+  test do
+    system "#{bin}/mkcmake", "-V", "MAKE_VERSION", "-f", "/dev/null"
+  end
+end
