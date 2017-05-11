f372172c4e3b5b922a8625b85df785c9b85fe889
Dominyk Tiller <dominyktiller@gmail.com>
2017-05-11 17:49:01 +0100

    pcre2: add upstream CVE-2017-8786 patch
    Closes #13474.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/pcre2.rb
+++ Formula/pcre2.rb
@@ -1,10 +1,21 @@
 class Pcre2 < Formula
   desc "Perl compatible regular expressions library with a new API"
   homepage "http://www.pcre.org/"
-  url "https://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre2-10.23.tar.bz2"
-  mirror "https://www.mirrorservice.org/sites/ftp.exim.org/pub/pcre/pcre2-10.23.tar.bz2"
-  sha256 "dfc79b918771f02d33968bd34a749ad7487fa1014aeb787fad29dd392b78c56e"
+  revision 1
+
   head "svn://vcs.exim.org/pcre2/code/trunk"
+
+  stable do
+    url "https://ftp.pcre.org/pub/pcre/pcre2-10.23.tar.bz2"
+    mirror "https://www.mirrorservice.org/sites/ftp.exim.org/pub/pcre/pcre2-10.23.tar.bz2"
+    sha256 "dfc79b918771f02d33968bd34a749ad7487fa1014aeb787fad29dd392b78c56e"
+
+    # Fix for CVE-2017-8786.
+    patch :p2 do
+      url "https://vcs.pcre.org/pcre2/code/trunk/src/pcre2test.c?r1=692&r2=697&view=patch"
+      sha256 "15041ad515e9daa513cb1b2c75c355000dc220864c1367fda3c1c47b9194ed43"
+    end
+  end
 
   bottle do
     cellar :any
