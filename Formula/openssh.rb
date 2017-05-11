ef4e9ee679e718f00a15089a8da4139281596b12
Dominyk Tiller <dominyktiller@gmail.com>
2017-05-11 19:42:54 +0100

    openssh: drop ldns dependency temporarily
    We need to decide what to do here in future. Worst-case scenario
    is that this waits to return until the migration to OpenSSL@1.1
    has been completed.
    
    Closes https://github.com/Homebrew/homebrew-core/issues/13132.

--- Formula/openssh.rb
+++ Formula/openssh.rb
@@ -17,8 +17,6 @@ class Openssh < Formula
   # https://github.com/Homebrew/homebrew-dupes/pull/482#issuecomment-118994372
 
   depends_on "openssl"
-  depends_on "ldns" => :optional
-  depends_on "pkg-config" => :build if build.with? "ldns"
 
   # Both these patches are applied by Apple.
   patch do
@@ -43,18 +41,12 @@ class Openssh < Formula
     # We introduce this issue with patching, it's not an upstream bug.
     inreplace "sandbox-darwin.c", "@PREFIX@/share/openssh", etc/"ssh"
 
-    args = %W[
-      --with-libedit
-      --with-kerberos5
-      --prefix=#{prefix}
-      --sysconfdir=#{etc}/ssh
-      --with-pam
-      --with-ssl-dir=#{Formula["openssl"].opt_prefix}
-    ]
-
-    args << "--with-ldns" if build.with? "ldns"
-
-    system "./configure", *args
+    system "./configure", "--with-libedit",
+                          "--with-kerberos5",
+                          "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}/ssh",
+                          "--with-pam",
+                          "--with-ssl-dir=#{Formula["openssl"].opt_prefix}"
     system "make"
     system "make", "install"
 
