96301de542add0d73decb8078e4d14e5b5e9fa25
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-11 18:56:15 +0000

    psqlodbc: update 09.06.0310 bottle.

--- Formula/psqlodbc.rb
+++ Formula/psqlodbc.rb
@@ -6,9 +6,9 @@ class Psqlodbc < Formula
 
   bottle do
     cellar :any
-    sha256 "62ce11d45d9cedf7272a60480575dd79aa3faabd77ed0ae1bb5761bf5fecc098" => :sierra
-    sha256 "f26864912d79fbc6c0678af382f9d85b5848845aef66d5ab4445c7c8fc084e05" => :el_capitan
-    sha256 "d47422be2dcf193328ccf496d61c9b2c280378a4f9f465bdc4f9d90f008235b0" => :yosemite
+    sha256 "8ddf0a7f5d06fe38c4a7429ecb3a22cf4b96b58d847e493410358cc0886b6fdd" => :sierra
+    sha256 "5aeb648c9a73629c46551143f01230bef9781e8c337c06b879b187141bac5fd0" => :el_capitan
+    sha256 "b8ba49f0d167189398492ad2961f4289bc964ffb5095c9469876daafb9235d5c" => :yosemite
   end
 
   head do
