84c0b616495b69c6af86553e39f23444be6ba853
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-12 18:30:49 +0000

    pyenv: add 1.0.10_1 bottle.

--- Formula/pyenv.rb
+++ Formula/pyenv.rb
@@ -4,6 +4,13 @@ class Pyenv < Formula
   url "https://github.com/pyenv/pyenv/archive/v1.0.10.tar.gz"
   sha256 "cc071d6a63445dc1f7cefa5961c74768bc3dadf8edab5c5a7e1d63bf536a99b5"
   revision 1
+  bottle do
+    cellar :any
+    sha256 "bb53d2af447f7b4e54511a89f8a5b18e2d9d0fc44887241c0bea7532aca93e0b" => :sierra
+    sha256 "c5ac14c00ed44d999ab972026b2a0fe3639b4144abb9668c5096de0f1f6e0cee" => :el_capitan
+    sha256 "31db3cb79bb7f8213787faea877bb6bc688acd12e3905210b3d73f7deaf8bdaf" => :yosemite
+  end
+
   version_scheme 1
   head "https://github.com/pyenv/pyenv.git"
 
