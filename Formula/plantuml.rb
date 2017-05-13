431240b8638200b1d4922e33db6c3d1ada257b69
Karthik Muralidharan <karthikmuralidharan@users.noreply.github.com>
2017-05-13 23:17:38 +0530

    plantuml 1.2017.13 (#13533)

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://downloads.sourceforge.net/project/plantuml/1.2017.12/plantuml.1.2017.12.jar"
-  sha256 "3eb511e45c4b31666b365020ed046b4b670d877e248b6fa8d76ca2c9cbefc5ab"
+  url "https://downloads.sourceforge.net/project/plantuml/1.2017.13/plantuml.1.2017.13.jar"
+  sha256 "068842ef3035eaaaf2642cf7fb8f5325c98de02f6ea189acda8448614258a0b2"
   version_scheme 1
 
   bottle :unneeded
