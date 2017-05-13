ccf8e79b3a5474554d3939862f6f3e8459988ad2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-13 09:09:11 +0000

    gexiv2: update 0.10.6 bottle.

--- Formula/gexiv2.rb
+++ Formula/gexiv2.rb
@@ -5,9 +5,9 @@ class Gexiv2 < Formula
   sha256 "132788919667254b42c026ab39ab3c3a5be59be8575c05fa4b371ca8aed55825"
 
   bottle do
-    sha256 "32a037cadf438566743c2e2bf9ce5eb964afeba84e95ae9397db10c1f59d6c6e" => :sierra
-    sha256 "1d957ba2a091887550e1cc7f72fe61489fb6a9085626486cb1a071bfc9ec169b" => :el_capitan
-    sha256 "57c5a3eec78d1091357be4ac89b93847837c592ceb8908d6dd0ba39dfa624d6a" => :yosemite
+    sha256 "8ba4cb6474e1cdc8016a98b882560ce76ace4eba0327b5332d58f879ac3b56e2" => :sierra
+    sha256 "675253f04420c2b74c65838d139847253d6e36aaf45f1f1a613c02ca95cbeddf" => :el_capitan
+    sha256 "d4c9828f17ea1ea09e124d0e49e96a5483a0fe8226e68f920ea1858f51722ad9" => :yosemite
   end
 
   depends_on "pkg-config" => :build
