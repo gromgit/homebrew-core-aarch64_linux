1d903197f3a4adf5840633330d7d02cc6a832d29
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-15 12:56:11 +0000

    bit: update 0.6.0 bottle.

--- Formula/bit.rb
+++ Formula/bit.rb
@@ -7,9 +7,9 @@ class Bit < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "cf66e35183ac949b85363c2082da9b59076cd19b347e610c4ec6108577c68e32" => :sierra
-    sha256 "a73d115fe024446462b6d9ca7a04bf15a9e4ebd951966c61527aeb526e8a8cc8" => :el_capitan
-    sha256 "a73d115fe024446462b6d9ca7a04bf15a9e4ebd951966c61527aeb526e8a8cc8" => :yosemite
+    sha256 "1d7402ed5fdc6c857f14f865bdee62b100930f4187d31ad84a649e88fb62f45a" => :sierra
+    sha256 "f4cf14bb7eb79800cde0cdf752130b8de850f17431e43f145b04c5d1437896d8" => :el_capitan
+    sha256 "f4cf14bb7eb79800cde0cdf752130b8de850f17431e43f145b04c5d1437896d8" => :yosemite
   end
 
   def install
