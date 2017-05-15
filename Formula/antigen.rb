1bd1552b159314b5467c032643f97af03ed4faa8
ilovezfs <ilovezfs@icloud.com>
2017-05-14 18:11:48 -0700

    antigen 2.1.0
    Closes #13573.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/antigen.rb
+++ Formula/antigen.rb
@@ -1,8 +1,8 @@
 class Antigen < Formula
   desc "Plugin manager for zsh, inspired by oh-my-zsh and vundle."
   homepage "http://antigen.sharats.me/"
-  url "https://github.com/zsh-users/antigen/releases/download/v2.0.2/v2.0.2.tar.gz"
-  sha256 "f47ec933b32c578abe8cb39b24e0ddd114ef5cc01b3c05bcb634859ead31493f"
+  url "https://github.com/zsh-users/antigen/releases/download/v2.1.0/v2.1.0.tar.gz"
+  sha256 "044ac01f91f71f2bea81cc617035167613527f5160c148a0c636184358ae7115"
   head "https://github.com/zsh-users/antigen.git", :branch => "develop"
 
   bottle :unneeded
