b2b4b2f493078fb74c2772dc0aa6f4ca0e4e774e
Dominyk Tiller <dominyktiller@gmail.com>
2017-05-15 14:19:37 +0100

    bitlbee 3.5.1
    Closes #13586.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/bitlbee.rb
+++ Formula/bitlbee.rb
@@ -1,10 +1,23 @@
 class Bitlbee < Formula
   desc "IRC to other chat networks gateway"
   homepage "https://www.bitlbee.org/"
-  url "https://get.bitlbee.org/src/bitlbee-3.5.tar.gz"
-  sha256 "549d02181ab303dfe8a219faafd7a1aea7ee642eb071b767f668782a57388319"
-
   head "https://github.com/bitlbee/bitlbee.git"
+
+  stable do
+    url "https://get.bitlbee.org/src/bitlbee-3.5.1.tar.gz"
+    sha256 "9636d7fd89ebb3756c13a9a3387736ca6d56ccf66ec0580d512f07b21db0fa69"
+
+    # Fixes a couple of bugs/potential crashes.
+    patch do
+      url "https://github.com/bitlbee/bitlbee/commit/17a58dfa.patch"
+      sha256 "2eab8a84eac25d3e5630e7da8cbd45f6e6a2e0d70d0dac111c8b0053b73cab68"
+    end
+
+    patch do
+      url "https://github.com/bitlbee/bitlbee/commit/eb73d05e.patch"
+      sha256 "0bdf1478f21b666cf1df194d05be7bcaf9b7d6d1ce3c99ac90563288e3c30935"
+    end
+  end
 
   bottle do
     sha256 "25e0fccb21f49326295ef5b24de1b6001ebb60e82f7dfad58ea1b14753aa7006" => :sierra
@@ -33,6 +46,7 @@ class Bitlbee < Formula
       --plugindir=#{HOMEBREW_PREFIX}/lib/bitlbee/
       --debug=0
       --ssl=gnutls
+      --etcdir=#{etc}/bitlbee
       --pidfile=#{var}/bitlbee/run/bitlbee.pid
       --config=#{var}/bitlbee/lib/
       --ipsocket=#{var}/bitlbee/run/bitlbee.sock
@@ -51,7 +65,9 @@ class Bitlbee < Formula
     system "make", "install-dev"
     # This build has an extra step.
     system "make", "install-etc"
+  end
 
+  def post_install
     (var/"bitlbee/run").mkpath
     (var/"bitlbee/lib").mkpath
   end
@@ -100,6 +116,6 @@ class Bitlbee < Formula
   end
 
   test do
-    shell_output("#{sbin}/bitlbee -V", 1)
+    assert_match version.to_s, shell_output("#{sbin}/bitlbee -V", 1)
   end
 end
