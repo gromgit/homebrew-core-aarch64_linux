a52b75fef4878223da9c555ebae919c82c1e5c9a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-15 19:06:43 +0000

    ipfs: update 0.4.9 bottle.

--- Formula/ipfs.rb
+++ Formula/ipfs.rb
@@ -8,9 +8,9 @@ class Ipfs < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "224c3c9f0d172d734410800cd42d73ab95087323dce6d7827169c875a4a71204" => :sierra
-    sha256 "49c6258ae0403a4be33daef5f24b1d678a2eb5d3d3e201fd833c7eb2e2993678" => :el_capitan
-    sha256 "490912c4efcee5870cdcf6bc3e56473851142e9bc722801aaa4e3e926d723aa0" => :yosemite
+    sha256 "c84486e1b6b73cd71555cbb7b068c00ae1c6a269b5724917e6515106247af7b8" => :sierra
+    sha256 "f1c0aa48b9140d408a89a48375255c85af150e457ed99c797229fe2802c2747e" => :el_capitan
+    sha256 "17a9a3ab32e70311f13e74d9f737aad86ccaba0f700ee290535d5298e30dc946" => :yosemite
   end
 
   depends_on "go" => :build
