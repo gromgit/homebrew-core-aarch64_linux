0097da627bb19986f07496c1ab101c2933901b90
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-16 20:11:30 +0000

    mono-libgdiplus: add 5.4 bottle.

--- Formula/mono-libgdiplus.rb
+++ Formula/mono-libgdiplus.rb
@@ -4,6 +4,13 @@ class MonoLibgdiplus < Formula
   url "https://github.com/mono/libgdiplus/archive/5.4.tar.gz"
   sha256 "ce31da0c6952c8fd160813dfa9bf4a9a871bfe7284e9e3abff9a8ee689acfe58"
 
+  bottle do
+    cellar :any
+    sha256 "6e46cbc48b7de8501b9a926840f8a2cefb6f571eecd797a5fc6a63e6edf6a6a6" => :sierra
+    sha256 "340ccb6b7480d14d7b4c86567d8eee88c7756e3981848b6c6381f2bc68fe7443" => :el_capitan
+    sha256 "b3f99bf768ece5efd9f265969f2554083e3ceff6323e7ef8a390b7e6261abb0a" => :yosemite
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
