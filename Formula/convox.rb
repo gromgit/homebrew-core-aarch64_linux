bdc9f520452f304683dfb36204338efa590177fc
ilovezfs <ilovezfs@icloud.com>
2017-05-16 08:15:44 -0700

    convox 20170515223736
    Closes #13622.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -1,8 +1,8 @@
 class Convox < Formula
   desc "The convox AWS PaaS CLI tool"
   homepage "https://convox.com/"
-  url "https://github.com/convox/rack/archive/20170508213441.tar.gz"
-  sha256 "74860e3aff62449c0398c3c01cfd448857be35fd8bea4286ce657494331047d8"
+  url "https://github.com/convox/rack/archive/20170515223736.tar.gz"
+  sha256 "ad5c562a7da478cb6977a9069cd886e9b2da3a0a9d4518af67e78d82f4ca5301"
 
   bottle do
     cellar :any_skip_relocation
