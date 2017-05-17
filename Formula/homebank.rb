2b26ef572c2464b0856381c919a04bf37037d966
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-17 16:17:33 +0000

    homebank: update 5.1.5 bottle.

--- Formula/homebank.rb
+++ Formula/homebank.rb
@@ -5,9 +5,9 @@ class Homebank < Formula
   sha256 "5c3fd3478577e91a5d8e6e28fced58b5ad611eecb06e8f335a9585b817a1330f"
 
   bottle do
-    sha256 "66fac13d713d56d4d506797b85fc2f82f67da5007182d96a9d2d8483499c53e0" => :sierra
-    sha256 "da0180fd6835b3f921cde03d80946184a4b77bc81f6d21b13806f59d56997a86" => :el_capitan
-    sha256 "6df11deccc339311eb4f3623b2469edc6b2aba78312f2b633e51c61bc6affdcd" => :yosemite
+    sha256 "d31666b5b8a1bcfd960fefe3b5703a07ac343705d2b21a86ace3a1c7bb7b574e" => :sierra
+    sha256 "9d4844e119ff8697ee8be60708930a4e52465f8bac4201c3741c15db34b9dd47" => :el_capitan
+    sha256 "c93d921f4d547201239ee9b81fcf082b7d101d23b35072c95c42f653fae71739" => :yosemite
   end
 
   depends_on "pkg-config" => :build
