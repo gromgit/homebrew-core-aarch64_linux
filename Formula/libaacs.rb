a8be1d8a9c9cc82be2f7950a89301ca7bb9534e5
JCount <JCount42@gmail.com>
2017-05-17 12:09:30 -0400

    libaacs 0.9.0
    also add test
    
    Closes #13665.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libaacs.rb
+++ Formula/libaacs.rb
@@ -1,9 +1,8 @@
 class Libaacs < Formula
   desc "Implements the Advanced Access Content System specification"
   homepage "https://www.videolan.org/developers/libaacs.html"
-  url "https://download.videolan.org/pub/videolan/libaacs/0.8.1/libaacs-0.8.1.tar.bz2"
-  mirror "http://videolan-nyc.defaultroute.com/libaacs/0.8.1/libaacs-0.8.1.tar.bz2"
-  sha256 "95c344a02c47c9753c50a5386fdfb8313f9e4e95949a5c523a452f0bcb01bbe8"
+  url "https://download.videolan.org/pub/videolan/libaacs/0.9.0/libaacs-0.9.0.tar.bz2"
+  sha256 "47e0bdc9c9f0f6146ed7b4cc78ed1527a04a537012cf540cf5211e06a248bace"
 
   bottle do
     cellar :any
@@ -30,5 +29,24 @@ class Libaacs < Formula
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include "libaacs/aacs.h"
+      #include <stdio.h>
+
+      int main() {
+        int major_v = 0, minor_v = 0, micro_v = 0;
+
+        aacs_get_version(&major_v, &minor_v, &micro_v);
+
+        printf("%d.%d.%d", major_v, minor_v, micro_v);
+        return(0);
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-laacs",
+                   "-o", "test"
+    system "./test"
   end
 end
