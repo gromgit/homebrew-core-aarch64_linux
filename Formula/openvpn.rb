7a63d24b32b66ab964229060b82b54ebb536e645
ilovezfs <ilovezfs@icloud.com>
2017-05-18 06:56:30 -0700

    openvpn: enable pkcs11
    Closes #13691.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/openvpn.rb
+++ Formula/openvpn.rb
@@ -4,6 +4,7 @@ class Openvpn < Formula
   url "https://swupdate.openvpn.org/community/releases/openvpn-2.4.2.tar.xz"
   mirror "https://build.openvpn.net/downloads/releases/openvpn-2.4.2.tar.xz"
   sha256 "df5c4f384b7df6b08a2f6fa8a84b9fd382baf59c2cef1836f82e2a7f62f1bff9"
+  revision 1
 
   bottle do
     sha256 "a5a6702461c9cfeed907c9ef9dcc6e52a8d7518a4c51c15ba8eea5a1340d1ba9" => :sierra
@@ -18,11 +19,31 @@ class Openvpn < Formula
   depends_on "lzo"
   depends_on "openssl"
 
+  resource "pkcs11-helper" do
+    url "https://github.com/OpenSC/pkcs11-helper/releases/download/pkcs11-helper-1.22/pkcs11-helper-1.22.tar.bz2"
+    sha256 "fbc15f5ffd5af0200ff2f756cb4388494e0fb00b4f2b186712dce6c48484a942"
+  end
+
   def install
+    vendor = buildpath/"brew_vendor"
+
+    resource("pkcs11-helper").stage do
+      system "./configure", "--disable-debug",
+                            "--disable-dependency-tracking",
+                            "--prefix=#{vendor}/pkcs11-helper",
+                            "--disable-threading",
+                            "--disable-slotevent",
+                            "--disable-shared"
+      system "make", "install"
+    end
+
+    ENV.prepend_path "PKG_CONFIG_PATH", vendor/"pkcs11-helper/lib/pkgconfig"
+
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--with-crypto-library=openssl",
+                          "--enable-pkcs11",
                           "--prefix=#{prefix}"
     system "make", "install"
 
