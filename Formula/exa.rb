179ccaa2477f945532571c15daa89dbe90e019e3
ilovezfs <ilovezfs@icloud.com>
2017-05-17 20:58:57 -0700

    exa 0.6.0
    Closes #13676.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/exa.rb
+++ Formula/exa.rb
@@ -1,8 +1,8 @@
 class Exa < Formula
   desc "Modern replacement for 'ls'"
   homepage "https://the.exa.website"
-  url "https://github.com/ogham/exa/archive/v0.5.0.tar.gz"
-  sha256 "273573df4234829c48102b5afa050d8e1adfc235e426e5ab9faa136bea46c20c"
+  url "https://github.com/ogham/exa/archive/v0.6.0.tar.gz"
+  sha256 "84cd6b3c389d5ec0483f8e438557d971897b5e1015d22a8cb3ae7558f87f4bf0"
   head "https://github.com/ogham/exa.git"
 
   bottle do
@@ -14,15 +14,12 @@ class Exa < Formula
 
   depends_on "cmake" => :build
   depends_on "rust" => :build
-  depends_on "libgit2" => :recommended
 
   def install
-    args = ["--release"]
-    args << "--no-default-features" if build.without? "libgit2"
-
-    system "cargo", "build", *args
-    bin.install "target/release/exa"
-    man1.install "contrib/man/exa.1"
+    system "make", "install", "PREFIX=#{prefix}"
+    bash_completion.install "contrib/completions.bash" => "exa"
+    zsh_completion.install  "contrib/completions.zsh"  => "_exa"
+    fish_completion.install "contrib/completions.fish" => "exa.fish"
   end
 
   test do
