a730197ad59c25e011939732c9c46f7988e491c1
Viktor Szakats <vszakats@users.noreply.github.com>
2017-05-19 12:27:47 +0200

    minizip: secure url(s)

--- Formula/minizip.rb
+++ Formula/minizip.rb
@@ -1,7 +1,7 @@
 class Minizip < Formula
   desc "C library for zip/unzip via zLib"
   homepage "http://www.winimage.com/zLibDll/minizip.html"
-  url "http://zlib.net/zlib-1.2.11.tar.gz"
+  url "https://zlib.net/zlib-1.2.11.tar.gz"
   sha256 "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1"
 
   bottle do
