8b1037f2b9865d19b6d2f61a9afdc9984b46ed11
Viktor Szakats <vszakats@users.noreply.github.com>
2017-05-19 12:27:31 +0200

    sslyze: secure url(s)

--- Formula/sslyze.rb
+++ Formula/sslyze.rb
@@ -103,7 +103,7 @@ class Sslyze < Formula
   end
 
   resource "zlib" do
-    url "http://zlib.net/zlib-1.2.11.tar.gz"
+    url "https://zlib.net/zlib-1.2.11.tar.gz"
     mirror "https://downloads.sourceforge.net/project/libpng/zlib/1.2.11/zlib-1.2.11.tar.gz"
     sha256 "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1"
   end
