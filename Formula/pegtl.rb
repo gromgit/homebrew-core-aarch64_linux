2058c1668b51cab64e2755612b2ba7f61910dda3
ilovezfs <ilovezfs@icloud.com>
2017-05-19 06:34:36 -0700

    pegtl 2.0.0 (#13713)

--- Formula/pegtl.rb
+++ Formula/pegtl.rb
@@ -1,18 +1,19 @@
 class Pegtl < Formula
   desc "Parsing Expression Grammar Template Library"
-  homepage "https://github.com/ColinH/PEGTL"
-  url "https://github.com/ColinH/PEGTL/archive/1.3.1.tar.gz"
-  sha256 "34201d56284a449c72798a0536020c6b46684c371a0a886f4c3c586c9372e9bc"
+  homepage "https://github.com/taocpp/PEGTL"
+  url "https://github.com/taocpp/PEGTL/archive/2.0.0.tar.gz"
+  sha256 "50d66d715d47a5eadc7e5fd74a229752b42dde5d0c8215e6caa8011837fd1e17"
 
   bottle :unneeded
 
   def install
-    include.install "pegtl.hh", "pegtl"
-    pkgshare.install "examples"
+    prefix.install "include"
+    rm "src/example/pegtl/CMakeLists.txt"
+    (pkgshare/"examples").install (buildpath/"src/example/pegtl").children
   end
 
   test do
-    system ENV.cxx, pkgshare/"examples/hello_world.cc", "-std=c++11", "-stdlib=libc++", "-lc++", "-o", "helloworld"
+    system ENV.cxx, pkgshare/"examples/hello_world.cpp", "-std=c++11", "-o", "helloworld"
     assert_equal "Good bye, homebrew!\n", shell_output("./helloworld 'Hello, homebrew!'")
   end
 end
