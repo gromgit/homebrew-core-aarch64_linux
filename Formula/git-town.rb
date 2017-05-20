05b92b4fd120fe254a2d5bd992f55b3c2ac7661a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-20 06:15:15 +0000

    git-town: add 4.0.0 bottle.

--- Formula/git-town.rb
+++ Formula/git-town.rb
@@ -4,6 +4,12 @@ class GitTown < Formula
   url "https://github.com/Originate/git-town/archive/v4.0.0.tar.gz"
   sha256 "4145a77ee7fe1f4579d04dfcc0a4658a21db4e07e80d7dd79328162f12797193"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6d78506ef2ff7718979baf019039dc6a76d1f2d589f22f583a329312b7796584" => :sierra
+    sha256 "3ceff065b52006cba4380dc589437f9a1c182d684082f2c71ff5ead55208b3d4" => :el_capitan
+  end
+
   depends_on "go" => :build
   depends_on :macos => :el_capitan
 
