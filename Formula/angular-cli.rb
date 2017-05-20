e89444e19b31fd0668ca323eb23795dd5ffd7e1c
Chris Long <christopherglong@mac.com>
2017-05-19 20:27:43 -0700

    angular-cli 1.0.4 (new formula)
    Closes #13754.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

---
+++ Formula/angular-cli.rb
@@ -0,0 +1,20 @@
+require "language/node"
+
+class AngularCli < Formula
+  desc "CLI tool for Angular"
+  homepage "https://cli.angular.io/"
+  url "https://registry.npmjs.org/@angular/cli/-/cli-1.0.4.tgz"
+  sha256 "263b5031d18afbc86670ddc1dd08a05d930e407506dcc343c2178dc69f60ccb3"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system bin/"ng", "new", "--skip-install", "angular-homebrew-test"
+    assert File.exist?("angular-homebrew-test/package.json"), "Project was not created"
+  end
+end
