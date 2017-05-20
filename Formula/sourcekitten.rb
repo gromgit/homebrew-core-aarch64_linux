c02fbfc303fb0a2bd982a0d58a2a34762ab98309
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-20 16:26:12 +0000

    sourcekitten: update 0.17.5 bottle.

--- Formula/sourcekitten.rb
+++ Formula/sourcekitten.rb
@@ -8,8 +8,8 @@ class Sourcekitten < Formula
 
   bottle do
     cellar :any
-    sha256 "53e121192b4bab4f9211f4c4ce66a5f2e1ff1040c1ab0623d1a3b150990ba41e" => :sierra
-    sha256 "173a733f3edc50101365964d602936c41f6f8318af536d089760ae3d3d50a757" => :el_capitan
+    sha256 "cf660b627e3aa57b7b9f481d7dfc1243effd93cb2ba7eddc52342ea64cc38a4c" => :sierra
+    sha256 "6a79527ad7b83aa2b29b7b732935949c162102055b401c03108afe52c00045ed" => :el_capitan
   end
 
   depends_on :xcode => ["6.0", :run]
