8738c803c72d6922477b12ae599e116b273fded5
ilovezfs <ilovezfs@icloud.com>
2017-05-20 21:41:30 -0700

    antigen 2.1.1 (#13770)

--- Formula/antigen.rb
+++ Formula/antigen.rb
@@ -1,8 +1,8 @@
 class Antigen < Formula
   desc "Plugin manager for zsh, inspired by oh-my-zsh and vundle."
   homepage "http://antigen.sharats.me/"
-  url "https://github.com/zsh-users/antigen/releases/download/v2.1.0/v2.1.0.tar.gz"
-  sha256 "044ac01f91f71f2bea81cc617035167613527f5160c148a0c636184358ae7115"
+  url "https://github.com/zsh-users/antigen/releases/download/v2.1.1/v2.1.1.tar.gz"
+  sha256 "394c8892398977ce2945460b81a6b747bc73daf0c7999b464e1dde1eaed29cdf"
   head "https://github.com/zsh-users/antigen.git", :branch => "develop"
 
   bottle :unneeded
