e89d261fa7c55cba8fe220c31874816ada821547
ilovezfs <ilovezfs@icloud.com>
2017-05-22 00:39:13 -0700

    osm2pgsql: fix patch checksum(s)

--- Formula/osm2pgsql.rb
+++ Formula/osm2pgsql.rb
@@ -22,7 +22,7 @@ class Osm2pgsql < Formula
   # Upstream PR from 27 Oct 2016 "Geos36"
   patch do
     url "https://github.com/openstreetmap/osm2pgsql/pull/636.patch"
-    sha256 "54aa12fe5a3ebbc9ecc02b7e5771939b99f6437f5a55b67d8835df6d8d58619a"
+    sha256 "89d1edb197d79a5567636ca0574d68877a31b82157ae8ad549614fd8327a4c79"
   end
 
   def install
