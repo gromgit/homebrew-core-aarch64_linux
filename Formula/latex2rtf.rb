5b7285d1fe1cbe2d362458f5c38ce597875199b8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-23 12:31:35 +0000

    latex2rtf: update 2.3.16 bottle.

--- Formula/latex2rtf.rb
+++ Formula/latex2rtf.rb
@@ -5,9 +5,9 @@ class Latex2rtf < Formula
   sha256 "e1538fe0dcadec28afae087cf8a6ee073e6368ca7a75728360068c6f3914b35b"
 
   bottle do
-    sha256 "bfae90986f27ac48103f07be94fac8b11298a9418bd497e3c582a83244d49377" => :sierra
-    sha256 "ce6bfc6ad26382ecdc5694f79efcc44be0b8c39b6cfc299f1c3e0a4153961e18" => :el_capitan
-    sha256 "5d2225892b7ab8f89f11cfa91cb434879d2726d88ed13553fa244761cd2e15d5" => :yosemite
+    sha256 "f93ad353b24f12c312f2a325dfc1fc20d948a1a02e3fa928b7ec19c7fc216d22" => :sierra
+    sha256 "33d85c22d7076259782b67188a33feb4b1daa11dd0fcc15d06c934745115e32e" => :el_capitan
+    sha256 "801e93dac4cc038e07226c949ab72daa661211303d7a33b7b8c6a86d52501ca6" => :yosemite
   end
 
   def install
