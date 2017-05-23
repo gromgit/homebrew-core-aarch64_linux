9299c5f14f8df66c57756c00b58af905426e516c
ilovezfs <ilovezfs@icloud.com>
2017-05-23 06:25:22 -0700

    jfrog-cli-go 1.9.0
    Closes #13851.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/jfrog-cli-go.rb
+++ Formula/jfrog-cli-go.rb
@@ -1,8 +1,8 @@
 class JfrogCliGo < Formula
   desc "command-line interface for Jfrog Artifactory and Bintray"
   homepage "https://github.com/JFrogDev/jfrog-cli-go"
-  url "https://github.com/JFrogDev/jfrog-cli-go/archive/1.8.0.tar.gz"
-  sha256 "f529559c900632ece646044fb25fb48ccc6f20d780148273585c667535b6fa88"
+  url "https://github.com/JFrogDev/jfrog-cli-go/archive/1.9.0.tar.gz"
+  sha256 "d422a32a249be781576f44f1adf2fc35396fb0be172557affa6e51bc6f2aec9d"
 
   bottle do
     cellar :any_skip_relocation
