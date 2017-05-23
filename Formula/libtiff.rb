0b9cd825aa3f0d65103d9bd9ce2b84cfea1f0707
ilovezfs <ilovezfs@icloud.com>
2017-05-23 04:37:50 -0700

    libtiff 4.0.8
    Closes #13845.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libtiff.rb
+++ Formula/libtiff.rb
@@ -1,10 +1,9 @@
 class Libtiff < Formula
   desc "TIFF library and utilities"
   homepage "http://libtiff.maptools.org/"
-  url "http://download.osgeo.org/libtiff/tiff-4.0.7.tar.gz"
-  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/t/tiff/tiff_4.0.7.orig.tar.gz"
-  sha256 "9f43a2cfb9589e5cecaa66e16bf87f814c945f22df7ba600d63aac4632c4f019"
-  revision 3
+  url "http://download.osgeo.org/libtiff/tiff-4.0.8.tar.gz"
+  mirror "https://fossies.org/linux/misc/tiff-4.0.8.tar.gz"
+  sha256 "59d7a5a8ccd92059913f246877db95a2918e6c04fb9d43fd74e5c3390dac2910"
 
   bottle do
     cellar :any
@@ -18,40 +17,6 @@ class Libtiff < Formula
 
   depends_on "jpeg"
   depends_on "xz" => :optional
-
-  # Patches from Debian for CVE-2016-10094, and various other issues.
-  # All reported upstream, so should be safe to remove this block on next stable.
-  patch do
-    url "https://mirrors.ocf.berkeley.edu/debian/pool/main/t/tiff/tiff_4.0.7-6.debian.tar.xz"
-    mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/t/tiff/tiff_4.0.7-6.debian.tar.xz"
-    sha256 "9c9048c28205bdbeb5ba36c7a194d0cd604bd137c70961607bfc8a079be5fa31"
-    apply "patches/01-CVE.patch",
-          "patches/02-CVE.patch",
-          "patches/03-CVE.patch",
-          "patches/04-CVE.patch",
-          "patches/05-CVE.patch",
-          "patches/06-CVE.patch",
-          "patches/07-CVE.patch",
-          "patches/08-CVE.patch",
-          "patches/09-CVE.patch",
-          "patches/10-CVE.patch",
-          "patches/11-CVE.patch",
-          "patches/12-CVE.patch",
-          "patches/13-CVE.patch",
-          "patches/14-CVE.patch",
-          "patches/15-TIFFFaxTabEnt_bugfix.patch",
-          "patches/16-CVE-2016-10094.patch",
-          "patches/17-CVE-2017-5225.patch",
-          "patches/18-CVE-2017-7595.patch",
-          "patches/19-CVE-2017-7598.patch",
-          "patches/20-CVE-2017-7596_CVE-2017-7597_CVE-2017-7599_CVE-2017-7600.patch",
-          "patches/21-CVE-2017-7601.patch",
-          "patches/22-CVE-2017-7602.patch",
-          "patches/23-CVE-2017-7592.patch",
-          "patches/24-CVE-2017-7593.patch",
-          "patches/25-CVE-2017-7594_part1.patch",
-          "patches/26-CVE-2017-7594_part2.patch"
-  end
 
   def install
     ENV.cxx11 if build.cxx11?
