e3a5828f3e24d20d980564b107e5883e12175096
Dominyk Tiller <dominyktiller@gmail.com>
2017-05-24 16:15:50 +0100

    imageworsener 1.3.2
    make jpeg recommended
    pass without-png when building without libpng
    
    Closes #13896.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/imageworsener.rb
+++ Formula/imageworsener.rb
@@ -1,8 +1,8 @@
 class Imageworsener < Formula
   desc "Utility and library for image scaling and processing"
   homepage "http://entropymine.com/imageworsener/"
-  url "http://entropymine.com/imageworsener/imageworsener-1.3.1.tar.gz"
-  sha256 "beb0c988c02b1d461dccdb3d6c4fc229316a692ea38689874013ba349dff66d1"
+  url "http://entropymine.com/imageworsener/imageworsener-1.3.2.tar.gz"
+  sha256 "0946f8e82eaf4c51b7f3f2624eef89bfdf73b7c5b04d23aae8d3fbe01cca3ea2"
 
   bottle do
     cellar :any
@@ -19,7 +19,7 @@ class Imageworsener < Formula
   end
 
   depends_on "libpng" => :recommended
-  depends_on "jpeg" => :optional
+  depends_on "jpeg" => :recommended
   depends_on "webp" => :optional
 
   def install
@@ -27,20 +27,22 @@ class Imageworsener < Formula
       inreplace "./scripts/autogen.sh", "libtoolize", "glibtoolize"
       system "./scripts/autogen.sh"
     end
+
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
     ]
+    args << "--without-png" if build.without? "libpng"
     args << "--without-jpeg" if build.without? "jpeg"
     args << "--without-webp" if build.without? "webp"
 
     system "./configure", *args
     system "make", "install"
-    share.install "tests"
+    pkgshare.install "tests"
   end
 
   test do
-    cp_r Dir["#{share}/tests/*"], testpath
+    cp_r Dir["#{pkgshare}/tests/*"], testpath
     system "./runtest", bin/"imagew"
   end
 end
