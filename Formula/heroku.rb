faede0ddbfd860fdacaa37e6fe04a1354ef8ef96
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-05-24 03:30:06 -0700

    heroku 6.6.17 (#13857)

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -1,9 +1,9 @@
 class Heroku < Formula
   desc "Everything you need to get started with Heroku"
   homepage "https://cli.heroku.com"
-  url "https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-v6.6.7-ecf0119-darwin-x64.tar.xz"
-  version "6.6.7"
-  sha256 "52687425b0044e0b086e8c577f081f0d027f73c1b9bb0ac59740864d051dba2f"
+  url "https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-v6.6.17-f15d070-darwin-x64.tar.xz"
+  version "6.6.17"
+  sha256 "a6e6cd7ca302dbaa8f9388008076c5e8807d3a59120c69f7fd5411564f773d3f"
 
   bottle :unneeded
 
