e56b646f45dd5b9d1c36322a4651276b5d7fc726
ilovezfs <ilovezfs@icloud.com>
2017-05-24 03:35:54 -0700

    kerl 1.5.0 (#13879)

--- Formula/kerl.rb
+++ Formula/kerl.rb
@@ -1,8 +1,8 @@
 class Kerl < Formula
   desc "Easy building and installing of Erlang/OTP instances"
   homepage "https://github.com/kerl/kerl"
-  url "https://github.com/kerl/kerl/archive/1.4.2.tar.gz"
-  sha256 "989596f66e813a69d0db3847900476cf6c0bed38cae73dac6eae1cd3f8610c54"
+  url "https://github.com/kerl/kerl/archive/1.5.0.tar.gz"
+  sha256 "1700998235fb250099516b8839558f12a7686162d23ac2842e34746818892816"
   head "https://github.com/kerl/kerl.git"
 
   bottle :unneeded
