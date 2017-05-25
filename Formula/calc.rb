31503f6b7174240f475dd3eda2b697da780efdd4
ilovezfs <ilovezfs@icloud.com>
2017-05-24 22:57:28 -0700

    calc 2.12.5.6
    Closes #13914.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/calc.rb
+++ Formula/calc.rb
@@ -1,9 +1,8 @@
 class Calc < Formula
   desc "Arbitrary precision calculator"
   homepage "http://www.isthe.com/chongo/tech/comp/calc/"
-  url "http://www.isthe.com/chongo/src/calc/calc-2.12.5.0.tar.bz2"
-  sha256 "a0e7b47af38330f188970998c8e5039134dadf6f2e3f2c00d7efdae272a4338d"
-  revision 1
+  url "http://www.isthe.com/chongo/src/calc/calc-2.12.5.6.tar.bz2"
+  sha256 "76090203a96d35dd10103112a7ad612f4d9b0526b758f2f49ac5bef02ba7cb39"
 
   bottle do
     sha256 "dd0ed50f3e98e105a46b378352bfecda1562c9bb79524d77a63fc7ec46e8ed64" => :sierra
@@ -20,20 +19,16 @@ class Calc < Formula
     ENV["EXTRA_LDFLAGS"] = ENV.ldflags
 
     readline = Formula["readline"]
-    inreplace "Makefile" do |s|
-      s.change_make_var! "INCDIR", "#{MacOS.sdk_path}/usr/include"
-      s.change_make_var! "BINDIR", bin
-      s.change_make_var! "LIBDIR", lib
-      s.change_make_var! "MANDIR", man1
-      s.change_make_var! "CALC_INCDIR", "#{include}/calc"
-      s.change_make_var! "CALC_SHAREDIR", pkgshare
-      s.change_make_var! "USE_READLINE", "-DUSE_READLINE"
-      s.change_make_var! "READLINE_LIB", "-L#{readline.lib} -lreadline"
-      s.change_make_var! "READLINE_EXTRAS", "-lhistory -lncurses"
-    end
 
-    system "make"
-    system "make", "install"
+    system "make", "install", "INCDIR=#{MacOS.sdk_path}/usr/include",
+                              "BINDIR=#{bin}",
+                              "LIBDIR=#{lib}",
+                              "MANDIR=#{man1}",
+                              "CALC_INCDIR=#{include}/calc",
+                              "CALC_SHAREDIR=#{pkgshare}",
+                              "USE_READLINE=-DUSE_READLINE",
+                              "READLINE_LIB=-L#{readline.opt_lib} -lreadline",
+                              "READLINE_EXTRAS=-lhistory -lncurses"
     libexec.install "#{bin}/cscript"
   end
 
