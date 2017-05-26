ea53d5e15a786b1beefc8ab12bb5b0ddd2ea266a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-26 19:15:04 +0000

    chakra: update 1.5.0 bottle.

--- Formula/chakra.rb
+++ Formula/chakra.rb
@@ -5,10 +5,10 @@ class Chakra < Formula
   sha256 "acaa6e788f8e3defba7350c7ef7bac93ba6c31cca7cd821420725d5ab0151539"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "fdd4e4dd04846af48290e70ab3cc12ec5d268e2443e2e2008c88f388fba14c22" => :sierra
-    sha256 "05da27ee145e09cbd81c7c24e15fba44fd469c14139778402aa5ba7fe4752c9e" => :el_capitan
-    sha256 "e1446136769d178a0be96743024ae6d8b22f49b7caa8499a9f7a1be6350b45fb" => :yosemite
+    cellar :any
+    sha256 "c99d26f04dbac1fce7f4ec6cae0db0d8e68f82666cfc2c6f32e91ea8fbe10deb" => :sierra
+    sha256 "ecac360f3374d9a0cc57d30a9fba61f497c633a46bab6025479ee18c87ebb9bd" => :el_capitan
+    sha256 "9595742613dec59fd0531b1033e769e2c237ce22ff83f5f3c44bd0601da11f95" => :yosemite
   end
 
   depends_on "cmake" => :build
