e58c905dfb3da3b2bc7c911d395dfe7f8fd2a358
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-28 09:51:49 +0000

    fsql: add 0.1.1 bottle.

--- Formula/fsql.rb
+++ Formula/fsql.rb
@@ -4,6 +4,13 @@ class Fsql < Formula
   url "https://github.com/kshvmdn/fsql/archive/v0.1.1.tar.gz"
   sha256 "d3ff000b8374d2eb9a44aa20220c758a82a24c82ee8a369fae146e84fc422f2c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bf46899434596282a5a7b93bf362d0ecb6cf59ae7442cd7e3c0fc4bd1a46b902" => :sierra
+    sha256 "4bd39ff926ef47576e84de9423b7d04aedee8ae62c683641607a44700aa173f1" => :el_capitan
+    sha256 "58bb2a0a759bf6ab236e52d3dae6ce4b7ac583446203d7c4d21eee20d3df77b7" => :yosemite
+  end
+
   depends_on "go" => :build
 
   def install
