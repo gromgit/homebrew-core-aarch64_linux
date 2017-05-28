7029a4c2af72b51b75154e1a15e1b955fe4b3618
Qi Xiao <xiaqqaix@gmail.com>
2017-05-28 17:09:44 +0100

    elvish 0.8
    elvish: Update to 0.8 and change build procedure.
    
    Closes #14018.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/elvish.rb
+++ Formula/elvish.rb
@@ -1,8 +1,8 @@
 class Elvish < Formula
   desc "Novel UNIX shell written in Go"
   homepage "https://github.com/elves/elvish"
-  url "https://github.com/elves/elvish/archive/0.4.tar.gz"
-  sha256 "05c32183462b0e63c832f1b0481ce79c4c6a289c14fe670b4400f47e349e2851"
+  url "https://github.com/elves/elvish/archive/0.8.tar.gz"
+  sha256 "74ce8baf5dc76cf1b9aede6781331c30177c6856c1d6a55f3eb62d6af53b78bb"
   head "https://github.com/elves/elvish.git"
 
   bottle do
@@ -20,8 +20,6 @@ class Elvish < Formula
     (buildpath/"src/github.com/elves").mkpath
     ln_sf buildpath, buildpath/"src/github.com/elves/elvish"
     system "go", "build", "-o", bin/"elvish"
-    system "make", "-C", "src/github.com/elves/elvish", "stub"
-    bin.install "bin/elvish-stub"
   end
 
   test do
