286867c991feca895489a3b2ff4c57889f880910
Dominyk Tiller <DomT4@users.noreply.github.com>
2017-05-28 07:57:37 +0100

    hive 2.1.1 (#13989)

--- Formula/hive.rb
+++ Formula/hive.rb
@@ -1,8 +1,8 @@
 class Hive < Formula
   desc "Hadoop-based data summarization, query, and analysis"
   homepage "https://hive.apache.org"
-  url "https://www.apache.org/dyn/closer.cgi?path=hive/hive-2.1.0/apache-hive-2.1.0-bin.tar.gz"
-  sha256 "ef5a2c2c4942adc3e0c5cd63510a05628fde9f14b57b8c3b509447e88c735a7a"
+  url "https://www.apache.org/dyn/closer.cgi?path=hive/hive-2.1.1/apache-hive-2.1.1-bin.tar.gz"
+  sha256 "c945dfc39f6489a098507fffa8fe78d8bd41de64887439b024c163ba1d958edc"
 
   bottle :unneeded
 
@@ -24,7 +24,7 @@ class Hive < Formula
     Hadoop must be in your path for hive executable to work.
 
     If you want to use HCatalog with Pig, set $HCAT_HOME in your profile:
-      export HCAT_HOME=#{libexec}/hcatalog
+      export HCAT_HOME=#{opt_libexec}/hcatalog
     EOS
   end
 
