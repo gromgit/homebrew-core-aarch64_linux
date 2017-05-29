bc5d3eb18d081f2726f7a74ab03369996639afda
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-29 14:31:41 +0000

    qbs: update 1.8.0 bottle.

--- Formula/qbs.rb
+++ Formula/qbs.rb
@@ -7,9 +7,9 @@ class Qbs < Formula
 
   bottle do
     cellar :any
-    sha256 "eee3ed2c17eff42e8218f0ca371b3d0a36f5675a5718bbf48b6a50df9294a8a0" => :sierra
-    sha256 "5fcb3537a14ca873f040253e27955708cdd3499be5534eb1f14e29e1414320de" => :el_capitan
-    sha256 "8abb924b0e7aeba5b3432575f298caddf42cdc552cf337899e71bde8f71c524a" => :yosemite
+    sha256 "5377d93bbc7ff878fac7c0fe9a55e4eeae70a1552dcb28eb5c9c4b89094413b7" => :sierra
+    sha256 "e105afd8faa9d756b29b09530671cb952f3c85da18c1f8a01339d39a76b38297" => :el_capitan
+    sha256 "c724abfa963bb0468df2f794348657fa54130b388982d4cdaac217ccae0045a7" => :yosemite
   end
 
   depends_on "qt"
