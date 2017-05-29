96ad919d07dc1240c7aba43d0edaa8c343f6a6a3
Dominyk Tiller <dominyktiller@gmail.com>
2017-05-29 02:45:21 +0100

    file-roller: update libxml2 dependency

--- Formula/file-roller.rb
+++ Formula/file-roller.rb
@@ -13,7 +13,7 @@ class FileRoller < Formula
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
   depends_on "itstool" => :build
-  depends_on "libxml2" => ["with-python", :build]
+  depends_on "libxml2" => :build
   depends_on "gtk+3"
   depends_on "json-glib"
   depends_on "libmagic"
@@ -34,6 +34,7 @@ class FileRoller < Formula
     # be filed for this since it only occurs because Homebrew renames gtk+3's gtk-update-icon-cache
     # to gtk3-update-icon-cache in order to avoid a collision between gtk+ and gtk+3.
     inreplace "data/Makefile.in", "gtk-update-icon-cache", "gtk3-update-icon-cache"
+
     ENV.append "CFLAGS", "-I#{Formula["libmagic"].opt_include}"
     ENV.append "LIBS", "-L#{Formula["libmagic"].opt_lib}"
 
