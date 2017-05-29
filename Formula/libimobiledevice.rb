23e37da733b31eb0065d28c939cd043be77c286d
ilovezfs <ilovezfs@icloud.com>
2017-05-29 01:54:03 -0700

    libimobiledevice: revision for libtasn1

--- Formula/libimobiledevice.rb
+++ Formula/libimobiledevice.rb
@@ -3,7 +3,7 @@ class Libimobiledevice < Formula
   homepage "http://www.libimobiledevice.org/"
   url "http://www.libimobiledevice.org/downloads/libimobiledevice-1.2.0.tar.bz2"
   sha256 "786b0de0875053bf61b5531a86ae8119e320edab724fc62fe2150cc931f11037"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
