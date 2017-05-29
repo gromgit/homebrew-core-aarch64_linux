57bf4ef6deb3e1c74d727beaa9c92973e8980c7e
Olivier Mehani <olivier.mehani@learnosity.com>
2017-05-29 10:14:34 +1000

    offlineimap: Use Basic UI in LaunchAgent
    This allows OfflineIMAP not to hang on a password prompt, which would
    otherwise leave the instance hanging with a lock.
    
    Closes #14027.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/offlineimap.rb
+++ Formula/offlineimap.rb
@@ -3,6 +3,7 @@ class Offlineimap < Formula
   homepage "http://offlineimap.org/"
   url "https://github.com/OfflineIMAP/offlineimap/archive/v7.1.1.tar.gz"
   sha256 "a624f8a77eae664dd458be47c5306c28911d4a1f788ff5641d7bb37e01ecb703"
+  revision 1
   head "https://github.com/OfflineIMAP/offlineimap.git"
 
   bottle do
@@ -58,6 +59,8 @@ class Offlineimap < Formula
         <key>ProgramArguments</key>
         <array>
           <string>#{opt_bin}/offlineimap</string>
+          <string>-u</string>
+          <string>basic</string>
         </array>
         <key>StartInterval</key>
         <integer>300</integer>
