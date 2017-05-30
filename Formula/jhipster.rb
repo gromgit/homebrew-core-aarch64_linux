ffae31b62047e89458509e6d8adcd5536c0280fa
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-30 09:56:35 +0000

    jhipster: add 4.5.2 bottle.

--- Formula/jhipster.rb
+++ Formula/jhipster.rb
@@ -6,6 +6,13 @@ class Jhipster < Formula
   url "https://registry.npmjs.org/generator-jhipster/-/generator-jhipster-4.5.2.tgz"
   sha256 "5df0edbbdb685df5ae598b53fb2def43b6c23bb75d44763a63cda69567395954"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0fb61e7d24bf386d415a4b608d5f4b180cf752dc87043f0a84506bada85281c1" => :sierra
+    sha256 "dfd93ee2481f2a88eb26bbe9e4f17b50c4b870210453da616b8b20f520f01c6f" => :el_capitan
+    sha256 "2722ebb74b9321b0fd258f59f084d8394f20977f1bcc2872e55ae455fa37e361" => :yosemite
+  end
+
   depends_on "node"
 
   def install
