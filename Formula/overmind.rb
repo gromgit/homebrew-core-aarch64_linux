2222b253e8b75255218ba3289265940518c263fe
Alexey Plutalov <demiazz.py@gmail.com>
2017-05-30 12:06:26 +0300

    overmind 1.0.4 (new formula)
    Closes #14508.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/overmind.rb
@@ -0,0 +1,24 @@
+class Overmind < Formula
+  desc "Process manager for Procfile-based applications and tmux"
+  homepage "https://github.com/DarthSim/overmind"
+  url "https://github.com/DarthSim/overmind/archive/v1.0.4.tar.gz"
+  sha256 "6a2219044fbd68757f1d1551f6750dc639b10cf9d0eab036dda25f1a0879bfe1"
+  head "https://github.com/DarthSim/overmind.git"
+
+  depends_on "go" => :build
+  depends_on "tmux"
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/DarthSim/overmind").install buildpath.children
+    system "go", "build", "-o", "#{bin}/overmind", "-v", "github.com/DarthSim/overmind"
+  end
+
+  test do
+    expected_message = "overmind: open ./Procfile: no such file or directory"
+    assert_match expected_message, shell_output("#{bin}/overmind start 2>&1", 1)
+    (testpath/"Procfile").write("test: echo 'test message'")
+    expected_message = "inappropriate ioctl for device"
+    assert_match expected_message, shell_output("#{bin}/overmind start")
+  end
+end
