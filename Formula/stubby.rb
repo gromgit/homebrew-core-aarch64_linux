076af5f8d0ab2241b621b2ac2fdf04d492a784a4
Sara Dickinson <sara@sinodun.com>
2017-06-01 17:53:54 +0100

    stubby 0.1.2 (new formula)
    Closes #14584.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/stubby.rb
@@ -0,0 +1,78 @@
+class Stubby < Formula
+  desc "DNS privacy enabled stub resolver service based on getdns"
+  homepage "https://getdnsapi.net/blog/dns-privacy-daemon-stubby/"
+  url "https://github.com/getdnsapi/stubby/archive/v0.1.2.tar.gz"
+  sha256 "bdac5dac5873f34a34db8da2538ab7d4f2bd7ff0315eb53db3d5cca5adedabfc"
+  head "https://github.com/getdnsapi/stubby.git", :branch => "develop"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "getdns"
+
+  def install
+    system "autoreconf", "-fiv"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}"
+    system "make", "install"
+  end
+
+  plist_options :startup => true, :manual => "sudo stubby -C #{HOMEBREW_PREFIX}/etc/stubby/stubby.conf"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-/Apple/DTD PLIST 1.0/EN" "http:/www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>KeepAlive</key>
+        <true/>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/stubby</string>
+          <string>-C</string>
+          <string>#{etc}/stubby/stubby.conf</string>
+          <string>-l</string>
+        </array>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/stubby/stubby.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/stubby/stubby.log</string>
+      </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    (testpath/"stubby_test.conf").write <<-EOS.undent
+      { resolution_type: GETDNS_RESOLUTION_STUB
+      , dns_transport_list: [ GETDNS_TRANSPORT_TLS, GETDNS_TRANSPORT_UDP, GETDNS_TRANSPORT_TCP ]
+      , listen_addresses: [ 127.0.0.1@5553]
+      , idle_timeout: 0
+      , upstream_recursive_servers:
+        [ { address_data: 145.100.185.15},
+          { address_data: 145.100.185.16},
+          { address_data: 185.49.141.37}
+        ]
+      }
+    EOS
+    output = shell_output("#{bin}/stubby -i -C stubby_test.conf")
+    assert_match "bindata for 145.100.185.15", output
+    pid = fork do
+      exec "#{bin}/stubby", "-C", testpath/"stubby_test.conf"
+    end
+    begin
+      sleep 2
+      output = shell_output("dig @127.0.0.1 -p 5553 getdnsapi.net")
+      assert_match "status: NOERROR", output
+    ensure
+      Process.kill 9, pid
+      Process.wait pid
+    end
+  end
+end
