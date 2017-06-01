4dd69ac5cab60791e08336075f22ca054383c0e5
Christian Moritz <chrmoritz@gmail.com>
2017-06-01 21:32:50 +0200

    azure-cli: fix fs permissions in easy-table dep
    The easy-table@0.0.1 package published at npm has incorrect file system
    permissions set (missing permission for lib folder). Until npm@4 npm
    didn't handle file system permissions inside .tgz at all, but npm@5
    started to handle them correctly. This breaks the azure-cli build with
    npm@5.
    
    Closes #14152.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/azure-cli.rb
+++ Formula/azure-cli.rb
@@ -20,6 +20,13 @@ class AzureCli < Formula
 
   def install
     rm_rf "bin/windows"
+
+    # Workaround for incorrect file system permissios inside the npm published
+    # easy_table@0.0.1 package, which would break build with npm@5.
+    # See: https://github.com/Azure/azure-xplat-cli/issues/3605
+    inreplace "package.json", '"easy-table": "0.0.1",',
+              '"easy-table": "https://github.com/eldargab/easy-table/archive/v0.0.1.tar.gz",'
+
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
     (bash_completion/"azure").write Utils.popen_read("#{bin}/azure --completion")
