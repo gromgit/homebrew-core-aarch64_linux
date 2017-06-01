f6f07421543edc9f555b2b9ff41b1be46ee50c49
Mattias Appelgren <mattias@ppelgren.se>
2017-06-01 22:10:34 +0200

    forego 20170327195458
    adapt formula to new build system, update stable
    
    Closes #14154.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/forego.rb
+++ Formula/forego.rb
@@ -1,9 +1,8 @@
 class Forego < Formula
-  desc "Foreman in Go"
+  desc "Foreman in Go for Procfile-based application management"
   homepage "https://github.com/ddollar/forego"
-  url "https://github.com/ddollar/forego/archive/v0.16.1.tar.gz"
-  sha256 "d4c8305262ac18c7e51d9d8028827f83b37fb3f9373d304686d084d68033ac6d"
-
+  url "https://github.com/ddollar/forego/archive/20170327195458.tar.gz"
+  sha256 "8a9bdff7f3d62ba64e9a8bb7fe619223e2fb20af6aff18f618ff2703a5bf1860"
   head "https://github.com/ddollar/forego.git"
 
   bottle do
@@ -16,15 +15,15 @@ class Forego < Formula
   end
 
   depends_on "go" => :build
-  depends_on "godep" => :build
 
   def install
     ENV["GOPATH"] = buildpath
-    mkdir_p buildpath/"src/github.com/ddollar/"
-    ln_sf buildpath, buildpath/"src/github.com/ddollar/forego"
-
-    ldflags = "-X main.Version=#{version} -X main.allowUpdate=false"
-    system "godep", "go", "build", "-ldflags", ldflags, "-o", bin/"forego"
+    (buildpath/"src/github.com/ddollar/forego").install buildpath.children
+    cd "src/github.com/ddollar/forego" do
+      system "go", "build", "-o", bin/"forego", "-ldflags",
+             "-X main.Version=#{version} -X main.allowUpdate=false"
+      prefix.install_metafiles
+    end
   end
 
   test do
