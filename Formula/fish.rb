22e8ff80330832fe192e27025c27661ebc0bf15e
David Adam <zanchey@ucc.gu.uwa.edu.au>
2017-06-03 21:31:32 +0800

    fish 2.6.0
    Closes #14210.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/fish.rb
+++ Formula/fish.rb
@@ -3,25 +3,15 @@ class Fish < Formula
   homepage "https://fishshell.com"
 
   stable do
-    url "https://github.com/fish-shell/fish-shell/releases/download/2.5.0/fish-2.5.0.tar.gz"
-    mirror "https://fishshell.com/files/2.5.0/fish-2.5.0.tar.gz"
-    sha256 "f8c0edadca2de379ccf305aeace660a9255fa2180c72e85e97705a24c256b2a5"
+    url "https://github.com/fish-shell/fish-shell/releases/download/2.6.0/fish-2.6.0.tar.gz"
+    mirror "https://fishshell.com/files/2.6.0/fish-2.6.0.tar.gz"
+    sha256 "7ee5bbd671c73e5323778982109241685d58a836e52013e18ee5d9f2e638fdfb"
   end
 
   bottle do
     sha256 "d012c7b4d1dce5e766fe9990f19ec7ac0ae5c4a69fc15e01cf123e8f88c1910f" => :sierra
     sha256 "2b28972cc66472fa11782b344c64b51569a43d0fd8b098ad4b787a1c79aca2b1" => :el_capitan
     sha256 "385def41fe9c29237b7319218ae0184f64b6b750e525f92ee6913cf20c478a69" => :yosemite
-  end
-
-  devel do
-    url "https://github.com/fish-shell/fish-shell/releases/download/2.6b1/fish-2.6b1.tar.gz"
-    sha256 "be36568dfebc0cb144b769e9ac09a570d09c528859e426480c9b4f96b0ccf001"
-
-    patch do
-      url "https://github.com/fish-shell/fish-shell/commit/40501667389c2ae1ad7e7cc1c39200466ad12232.diff"
-      sha256 "af47bcac6789aee745429fc6bb1c6498003a1ba8f05d27ec06f84e71441a0eda"
-    end
   end
 
   head do
