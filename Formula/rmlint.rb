cb123d840fc96655cf3b4111d75ce0ab61850705
ilovezfs <ilovezfs@icloud.com>
2017-06-04 01:00:17 -0700

    rmlint 2.6.0
    Closes #14224.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/rmlint.rb
+++ Formula/rmlint.rb
@@ -1,8 +1,8 @@
 class Rmlint < Formula
   desc "Extremely fast tool to remove dupes and other lint from your filesystem"
   homepage "https://github.com/sahib/rmlint"
-  url "https://github.com/sahib/rmlint/archive/v2.4.6.tar.gz"
-  sha256 "1ec614c38dbeb0a5d181286111ed4e37707fcd10e2c3569db0a41ef1765c5415"
+  url "https://github.com/sahib/rmlint/archive/v2.6.0.tar.gz"
+  sha256 "f6810bdca1eb09fabf51dc8d9c4ba9cad70b1072b9965c0343a1747bedfc9415"
 
   bottle do
     cellar :any
@@ -21,6 +21,14 @@ class Rmlint < Formula
   depends_on "sphinx-doc" => :build
   depends_on "json-glib" => :optional
   depends_on "libelf" => :optional
+
+  # Remove for > 2.6.0
+  # Fix "fatal error: 'sys/sysmacros.h' file not found"
+  # Upstream commit from 4 Jun 2017 "Use #if not #ifdef with HAVE_SYSMACROS_H"
+  patch do
+    url "https://github.com/sahib/rmlint/commit/cf7d50d6.patch"
+    sha256 "b85b042c4b7e51869fb8695fd1ceb4363434a19598249084b393e87b269ee19e"
+  end
 
   def install
     scons "config"
