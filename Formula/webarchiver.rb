95e601ad7c47ebe228ae5109ed16db699cdfabe7
Isabell Long <isabell@issyl0.co.uk>
2017-06-04 14:07:50 +0100

    webarchiver: fix audit 'description must start with capital letter'

--- Formula/webarchiver.rb
+++ Formula/webarchiver.rb
@@ -1,5 +1,5 @@
 class Webarchiver < Formula
-  desc "allows you to create Safari .webarchive files"
+  desc "Allows you to create Safari .webarchive files"
   homepage "https://github.com/newzealandpaul/webarchiver"
   url "https://github.com/newzealandpaul/webarchiver/archive/0.9.tar.gz"
   sha256 "8ea826038e923c72e75a4bbb1416910368140a675421f6aaa51fd0dea703f75c"
