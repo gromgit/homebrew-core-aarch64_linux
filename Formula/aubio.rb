65c2617cb138adb3df703fbee9b8023dd7b42a41
ilovezfs <ilovezfs@icloud.com>
2017-06-04 02:10:43 -0700

    aubio 0.4.5
    Closes #14226.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/aubio.rb
+++ Formula/aubio.rb
@@ -1,10 +1,8 @@
 class Aubio < Formula
   desc "Extract annotations from audio signals"
   homepage "https://aubio.org/"
-  url "https://aubio.org/pub/aubio-0.4.4.tar.bz2"
-  sha256 "2acdb92623b9d4ba641c387760ffe3ec1e4c6ab498e64e5e2286c99e36ffbff8"
-
-  head "https://github.com/piem/aubio.git", :branch => "develop"
+  url "https://aubio.org/pub/aubio-0.4.5.tar.bz2"
+  sha256 "70c2804e6f4fbf0ebc0fb9ac8cc9d465ef4a4d438311c074c9a7364e98827af6"
 
   bottle do
     cellar :any
@@ -13,21 +11,17 @@ class Aubio < Formula
     sha256 "c826af70ae0ac208299282b6935747736ef58d5505b9c7235c699590ac26020d" => :yosemite
   end
 
-  depends_on :macos => :lion
+  option "with-python", "Build with python support"
 
-  depends_on :python => :optional
+  depends_on :macos => :lion
   depends_on "pkg-config" => :build
   depends_on "libtool" => :build
-
   depends_on "libav" => :optional
   depends_on "libsndfile" => :optional
   depends_on "libsamplerate" => :optional
   depends_on "fftw" => :optional
   depends_on "jack" => :optional
-
-  if build.with? "python"
-    depends_on "numpy" => :python
-  end
+  depends_on "numpy" if build.with? "python"
 
   def install
     # Needed due to issue with recent cland (-fno-fused-madd))
@@ -38,10 +32,8 @@ class Aubio < Formula
     system "./waf", "install"
 
     if build.with? "python"
-      cd "python" do
-        system "python", *Language::Python.setup_install_args(prefix)
-        bin.env_script_all_files(libexec+"bin", :PYTHONPATH => ENV["PYTHONPATH"])
-      end
+      system "python", *Language::Python.setup_install_args(prefix)
+      bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
     end
   end
 
