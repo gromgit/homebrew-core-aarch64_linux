b86539c95ba8f02a6c0c231a4d1b8fe00fb94b0c
Isabell Long <isabell@issyl0.co.uk>
2017-06-04 14:05:43 +0100

    truncate: fix audit 'description must start with capital letter'

--- Formula/truncate.rb
+++ Formula/truncate.rb
@@ -1,5 +1,5 @@
 class Truncate < Formula
-  desc "truncates a file to a given size"
+  desc "Truncates a file to a given size"
   homepage "https://www.vanheusden.com/truncate/"
   url "https://github.com/flok99/truncate/archive/0.9.tar.gz"
   sha256 "a959d50cf01a67ed1038fc7814be3c9a74b26071315349bac65e02ca23891507"
