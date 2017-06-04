78f966943074a809b96aa8f2126b650c392955a4
ilovezfs <ilovezfs@icloud.com>
2017-06-04 00:53:32 -0700

    zanata-client 4.2.1 (#14206)
    set bottle unneeded

--- Formula/zanata-client.rb
+++ Formula/zanata-client.rb
@@ -1,15 +1,10 @@
 class ZanataClient < Formula
   desc "Zanata translation system command-line client"
   homepage "http://zanata.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/zanata/zanata-cli/4.1.0/zanata-cli-4.1.0-dist.tar.gz"
-  sha256 "ac08f3da73bfa439b17389537d4ccc383fa4932f1b90fe456f7c671bf99ce7a1"
+  url "https://search.maven.org/remotecontent?filepath=org/zanata/zanata-cli/4.2.1/zanata-cli-4.2.1-dist.tar.gz"
+  sha256 "8052b11d0dcda8262221bd573cae70933e00a5e84336235e7149aabcf8cb61f7"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "028df1b1361d2b0f7296047ac0723bdce1a916dd10e1e98f33329ca5b8ff0466" => :sierra
-    sha256 "028df1b1361d2b0f7296047ac0723bdce1a916dd10e1e98f33329ca5b8ff0466" => :el_capitan
-    sha256 "028df1b1361d2b0f7296047ac0723bdce1a916dd10e1e98f33329ca5b8ff0466" => :yosemite
-  end
+  bottle :unneeded
 
   depends_on :java => "1.8+"
 
