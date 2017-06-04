96d06f956ddeb374d26b419e3907abed5cf15b76
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-04 09:35:12 +0000

    aubio: update 0.4.5 bottle.

--- Formula/aubio.rb
+++ Formula/aubio.rb
@@ -6,9 +6,9 @@ class Aubio < Formula
 
   bottle do
     cellar :any
-    sha256 "e72e6f6ca2562e43dd3b79755bac1741fbda2cc12c1adf41e19712da9eb292c7" => :sierra
-    sha256 "d754ca2e173318c33fd6af21184fcbf7e9d739ece3c35b329f616483dc8696d3" => :el_capitan
-    sha256 "c826af70ae0ac208299282b6935747736ef58d5505b9c7235c699590ac26020d" => :yosemite
+    sha256 "a118cd7a1446f2f8dc57558e2e3e8ec151030976a04467cd2b3a32bc84903dd3" => :sierra
+    sha256 "bef9b466b761bccebf7cb440ffec7d35d64bd1b579a64018924ebd80fab37a6f" => :el_capitan
+    sha256 "d5ade46b9f42376b59cdbd7a44c00ab41e28218b5568f9e9caf6ab2fd4688c76" => :yosemite
   end
 
   option "with-python", "Build with python support"
