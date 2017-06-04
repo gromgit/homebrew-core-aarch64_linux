07e850a13f4c5022e8560465d3abdd76d3f07710
Isabell Long <isabell@issyl0.co.uk>
2017-06-04 13:09:49 +0100

    dehydrated: fix audit 'description must start with capital letter'

--- Formula/dehydrated.rb
+++ Formula/dehydrated.rb
@@ -1,5 +1,5 @@
 class Dehydrated < Formula
-  desc "letsencrypt/acme client implemented as a shell-script"
+  desc "LetsEncrypt/acme client implemented as a shell-script"
   homepage "https://dehydrated.de"
   url "https://github.com/lukas2511/dehydrated/archive/v0.4.0.tar.gz"
   sha256 "611da321330ffd43d1dc497990b486b2dec12c59149803ad7d156980c8527f48"
