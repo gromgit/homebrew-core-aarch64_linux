ad292264fea737243ded25c52802c31eb97643b9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-05 11:03:31 +0000

    shc: update 3.9.6 bottle.

--- Formula/shc.rb
+++ Formula/shc.rb
@@ -7,9 +7,9 @@ class Shc < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "af67fd885a25116979b7633c359895c87b3e9fa651ecf776303a17659bce1d9d" => :sierra
-    sha256 "1223921ffcf26c06d89d36be43f3ebf1404fa7e3b23a2bb561845667a61c64ef" => :el_capitan
-    sha256 "fd57edf8ab1d2929a1007dcf9672c0263cddde3d2a9b2245364ed344d8aee650" => :yosemite
+    sha256 "f7c53fd5fa9c86057260ad66961406b4579738b30091d7ca6899eb5349013b0c" => :sierra
+    sha256 "c54ddaec872f8c3613c53f7b1653250f3a11ad6db789418f8921be96fff6e8a3" => :el_capitan
+    sha256 "0b40ee06c9a5db74e302be41adea362975bf86c9c5e6290da13a623341e4468d" => :yosemite
   end
 
   def install
