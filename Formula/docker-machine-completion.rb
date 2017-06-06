950648d83f5f669b62afd5f10af70d9295f131d3
ilovezfs <ilovezfs@icloud.com>
2017-06-05 22:36:31 -0700

    docker-machine-completion 0.12.0

--- Formula/docker-machine-completion.rb
+++ Formula/docker-machine-completion.rb
@@ -1,8 +1,8 @@
 class DockerMachineCompletion < Formula
-  desc "docker-machine completion script"
+  desc "Docker-machine completion script"
   homepage "https://docs.docker.com/machine/completion/"
-  url "https://github.com/docker/machine/archive/v0.11.0.tar.gz"
-  sha256 "a29f8f028cad2525c016f5ca6b14a4fd5bf5feb15eb9ad902696991385819e91"
+  url "https://github.com/docker/machine/archive/v0.12.0.tar.gz"
+  sha256 "2238dbb8580ca2399c64fa6cda0089a9dde6142d4dcbb8d750aedbeee042fbfe"
   head "https://github.com/docker/machine.git"
 
   bottle :unneeded
