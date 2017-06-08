efdd22451b3d67e01be78474f2a1bd98dcbc1821
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-08 06:49:47 +0000

    liboping: update 1.10.0 bottle.

--- Formula/liboping.rb
+++ Formula/liboping.rb
@@ -5,11 +5,9 @@ class Liboping < Formula
   sha256 "eb38aa93f93e8ab282d97e2582fbaea88b3f889a08cbc9dbf20059c3779d5cd8"
 
   bottle do
-    sha256 "d1f47bef9f08fa16e0dff46fc42ab617aa2a4124456a84db918c363cde62432d" => :sierra
-    sha256 "2a5ed61ca95f5714209c1098a17eeac0f693d06d6882c3af9fabbbadf0e0d0c3" => :el_capitan
-    sha256 "20a7a47dc6cdc3ffc058b04f36c0022d594f518cce1e389da1518f8649f5fdf1" => :yosemite
-    sha256 "fa861b822a560e8dfde07a6e2fc1549100dbc9b02b9a5e13d0c8639479adc1c4" => :mavericks
-    sha256 "ed0aaa7a4f7625d4ee4a7d50a9e9f2b96d8da45ecf184666988fb719dba1d735" => :mountain_lion
+    sha256 "42b80e23afe4fb4f296d039b0bdd4ccd0da21937514fdd04a90bc01d39da7aec" => :sierra
+    sha256 "de0bb72a0752469b262db3a24a41c84746930858462cd08993c057caadd46264" => :el_capitan
+    sha256 "c4f46d01bdace450a49e2c4fc4ba4056070bf1b869ed07f1b0a1d6a4f7646bc9" => :yosemite
   end
 
   def install
