db4a6c260dc55d4ae7e8415e033d63a956444bf1
tvatter <thibault.vatter@gmail.com>
2017-06-09 12:15:59 -0400

    cereal: always build with cmake
    so that cereal-config.cmake gets installed.
    
    Also,
    - remove option for "make test"
    - remove bottle :unneeded
    
    Closes #14427.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/cereal.rb
+++ Formula/cereal.rb
@@ -5,33 +5,15 @@ class Cereal < Formula
   sha256 "1921f26d2e1daf9132da3c432e2fd02093ecaedf846e65d7679ddf868c7289c4"
   head "https://github.com/USCiLab/cereal.git", :branch => "develop"
 
-  bottle :unneeded
-
-  option "with-test", "Build and run the test suite"
-
-  deprecated_option "with-tests" => "with-test"
-
-  if build.with? "test"
-    depends_on "cmake" => :build
-    depends_on "boost" => :build
-  end
+  depends_on "cmake" => :build
 
   # error: chosen constructor is explicit in copy-initialization
   # Reported 3 Sep 2016: https://github.com/USCiLab/cereal/issues/339
   depends_on :macos => :yosemite
 
-  needs :cxx11
-
   def install
-    if build.with? "test"
-      ENV.cxx11
-      mkdir "build" do
-        system "cmake", "..", *std_cmake_args
-        system "make"
-        system "make", "test"
-      end
-    end
-    include.install "include/cereal"
+    system "cmake", ".", "-DJUST_INSTALL_CEREAL=ON", *std_cmake_args
+    system "make", "install"
   end
 
   test do
