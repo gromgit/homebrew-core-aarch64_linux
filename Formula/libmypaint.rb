d920b9361d16561c91a7c214189f97ee5ee0dc6f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-10 19:08:02 +0000

    libmypaint: add 1.3.0 bottle.

--- Formula/libmypaint.rb
+++ Formula/libmypaint.rb
@@ -4,6 +4,13 @@ class Libmypaint < Formula
   url "https://github.com/mypaint/libmypaint/releases/download/v1.3.0/libmypaint-1.3.0.tar.xz"
   sha256 "6a07d9d57fea60f68d218a953ce91b168975a003db24de6ac01ad69dcc94a671"
 
+  bottle do
+    cellar :any
+    sha256 "713c83d8bffaf3aefbfe692bee7e568427eee277b2168de172761633643f1df9" => :sierra
+    sha256 "7511afd642619eccd9a0fdb2ff25ff2ff0e451ed6cb2e544ed8cdefaacb2a11a" => :el_capitan
+    sha256 "4587871608abf371b5f691d33a5f2fb081d38b5de3794a67729ac9f09f7cc880" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
   depends_on "gettext"
