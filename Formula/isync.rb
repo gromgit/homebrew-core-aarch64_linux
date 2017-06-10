4c182aaca49dc83dfa34111371bac3d5e8cf6da8
James P. Ascher <jpa4q@virginia.edu>
2017-06-10 16:01:58 -0400

    isync: add plist
    Closes #14462.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/isync.rb
+++ Formula/isync.rb
@@ -4,6 +4,8 @@ class Isync < Formula
   url "https://downloads.sourceforge.net/project/isync/isync/1.2.1/isync-1.2.1.tar.gz"
   sha256 "e716de28c9a08e624a035caae3902fcf3b511553be5d61517a133e03aa3532ae"
 
+  revision 1
+
   bottle do
     cellar :any
     rebuild 1
@@ -35,6 +37,40 @@ class Isync < Formula
 
     system "./configure", *args
     system "make", "install"
+  end
+
+  plist_options :manual => "isync"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>EnvironmentVariables</key>
+        <dict>
+          <key>PATH</key>
+          <string>/usr/bin:/bin:/usr/sbin:/sbin:#{HOMEBREW_PREFIX}/bin</string>
+        </dict>
+        <key>KeepAlive</key>
+        <false/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/mbsync</string>
+          <string>Periodic</string>
+        </array>
+        <key>StartInterval</key>
+        <integer>300</integer>
+        <key>RunAtLoad</key>
+        <true />
+        <key>StandardErrorPath</key>
+        <string>/dev/null</string>
+        <key>StandardOutPath</key>
+        <string>/dev/null</string>
+      </dict>
+    </plist>
+    EOS
   end
 
   test do
