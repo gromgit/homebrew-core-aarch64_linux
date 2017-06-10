134273d9aec10c1cdf5bb09c94b0f457659f9db1
ilovezfs <ilovezfs@icloud.com>
2017-06-09 18:39:43 -0700

    riemann-client 1.10.0
    Closes #14437.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/riemann-client.rb
+++ Formula/riemann-client.rb
@@ -1,8 +1,8 @@
 class RiemannClient < Formula
   desc "C client library for the Riemann monitoring system"
   homepage "https://github.com/algernon/riemann-c-client"
-  url "https://github.com/algernon/riemann-c-client/archive/riemann-c-client-1.9.1.tar.gz"
-  sha256 "6c8279362384e0ee01cb84a12f645bf7229c7d61f565158fe4ecc82c36ce8dc0"
+  url "https://github.com/algernon/riemann-c-client/archive/riemann-c-client-1.10.0.tar.gz"
+  sha256 "74e6c8f79a230598278f44533b42b48a2f9531523a7e3d458e5618efc8f96b22"
   head "https://github.com/algernon/riemann-c-client.git"
 
   bottle do
@@ -19,6 +19,13 @@ class RiemannClient < Formula
 
   depends_on "json-c"
   depends_on "protobuf-c"
+
+  # Fix build failure "error: only weak aliases are supported on darwin"
+  # Reported 11 Jun 2017 https://github.com/algernon/riemann-c-client/issues/19
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/1edeff1/riemann-client/alias-fix.diff"
+    sha256 "4d4c33ab109688364ebbbcbf2487b1f80dd9b7aece0dfed8c4f1d804d0b56f3e"
+  end
 
   def install
     system "autoreconf", "-i"
