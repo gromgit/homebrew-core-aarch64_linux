c6246a347b22517e78abb2a7feae6c500aada4f8
ilovezfs <ilovezfs@icloud.com>
2017-06-10 01:08:38 -0700

    fontconfig 2.12.3
    Closes #14444.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/fontconfig.rb
+++ Formula/fontconfig.rb
@@ -1,23 +1,8 @@
 class Fontconfig < Formula
   desc "XML-based font configuration API for X Windows"
   homepage "https://wiki.freedesktop.org/www/Software/fontconfig/"
-  revision 2
-
-  stable do
-    url "https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.12.1.tar.bz2"
-    sha256 "b449a3e10c47e1d1c7a6ec6e2016cca73d3bd68fbbd4f0ae5cc6b573f7d6c7f3"
-
-    patch do
-      # Fixes https://bugs.freedesktop.org/show_bug.cgi?id=97546, "fc-cache
-      # failure with /System/Library/Fonts", and #4172.
-      #
-      # Patch from upstream maintainer Akira TAGOH. See
-      #   https://bugs.freedesktop.org/show_bug.cgi?id=97546#c7
-      #   https://bugs.freedesktop.org/attachment.cgi?id=126464
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/3790bcd/fontconfig/patch-2.12.1-fccache.diff"
-      sha256 "e7c074109a367bf3966578034b20d11f7e0b4a611785a040aef1fd11359af04d"
-    end
-  end
+  url "https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.12.3.tar.bz2"
+  sha256 "bd24bf6602731a11295c025909d918180e98385625182d3b999fd6f1ab34f8bd"
 
   # The bottle tooling is too lenient and thinks fontconfig
   # is relocatable, but it has hardcoded paths in the executables.
