b97df7aac1ce690fb90a5cf7a7d7c7829a84d24d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-11 15:27:20 +0000

    ncmpc: update 0.27 bottle.

--- Formula/ncmpc.rb
+++ Formula/ncmpc.rb
@@ -5,9 +5,9 @@ class Ncmpc < Formula
   sha256 "f9a26a3fc869cfdf0a16b0ea3e6512c2fe28a031bbc71b1d24a2bf0bbd3e15d9"
 
   bottle do
-    sha256 "c4d24c040b851b642342d196a2e5c5a8146a8d6bed68d79d8cfcb7ec2e4699b9" => :sierra
-    sha256 "fbc08c275e60e7267c5984995ab80725ffcf46c21ab0725950bd4ebe6ec2f61e" => :el_capitan
-    sha256 "ad1fdbc26705129584d628f0c32d8ff527622b7ba19272e107e61d4521818e22" => :yosemite
+    sha256 "d6dea0bd60e7d938df31a851f7c2f8f21476114e375b90d54aa3cd1eaa07c0dc" => :sierra
+    sha256 "99b512e3c6e36e22c48e04eba8cfdb751b3cf603cb241e5290f2961eeed68aef" => :el_capitan
+    sha256 "4de982b7fbbb34b72d38ee56312bcc706ad9857dfe7d2fdd4c0a0c2c34280319" => :yosemite
   end
 
   depends_on "pkg-config" => :build
