f6cf205e27fdc727c86868148a0e65939cc16675
Hidenori MATSUKI <MATSUKI.Hidenori@gmail.com>
2017-06-12 06:50:20 +0900

    sbtenv: fix url from my namespace to `sbtenv` org.
    Closes #14491.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/sbtenv.rb
+++ Formula/sbtenv.rb
@@ -1,9 +1,9 @@
 class Sbtenv < Formula
   desc "Command-line tool for managing sbt environments"
-  homepage "https://github.com/mazgi/sbtenv"
-  url "https://github.com/mazgi/sbtenv/archive/version/0.0.12.tar.gz"
+  homepage "https://github.com/sbtenv/sbtenv"
+  url "https://github.com/sbtenv/sbtenv/archive/version/0.0.12.tar.gz"
   sha256 "12b0fe7d66717ccd849ae51cb05a3b12e0080b6c1f9f5b7455a732fa2e5e28a9"
-  head "https://github.com/mazgi/sbtenv.git"
+  head "https://github.com/sbtenv/sbtenv.git"
 
   bottle :unneeded
 
