541a255d7b0e34fe183654f778a1ca91a899d7cd
ilovezfs <ilovezfs@icloud.com>
2017-06-12 00:29:15 -0700

    archi-steam-farm 2.3.1.8 (#14494)
    set bottle unneeded

--- Formula/archi-steam-farm.rb
+++ Formula/archi-steam-farm.rb
@@ -1,16 +1,11 @@
 class ArchiSteamFarm < Formula
   desc "ASF is a C# application that allows you to farm steam cards"
   homepage "https://github.com/JustArchi/ArchiSteamFarm"
-  url "https://github.com/JustArchi/ArchiSteamFarm/releases/download/2.3.1.4/ASF.zip"
-  version "2.3.1.4"
-  sha256 "73941584b97a8b4820a5c4587e033092873f520ff88dbe9b8da085e72b7525bf"
+  url "https://github.com/JustArchi/ArchiSteamFarm/releases/download/2.3.1.8/ASF.zip"
+  version "2.3.1.8"
+  sha256 "169a2f6deaf582646781089d301a03208cbbc127a9f924a5815ab9ba8707cb19"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "7a2e792ce756de3ca77ae63d00073da56b7ba0b599f2583b8e286ba60be63c97" => :sierra
-    sha256 "0054489a26c12f9379ad9f79ac959c07e9b4a6d0a387e08400ad8cb978a602d8" => :el_capitan
-    sha256 "0054489a26c12f9379ad9f79ac959c07e9b4a6d0a387e08400ad8cb978a602d8" => :yosemite
-  end
+  bottle :unneeded
 
   depends_on "mono"
 
