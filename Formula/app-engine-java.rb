6b66e4ca9a26b496f999ced50110db55a08c09c1
Mike McQuaid <mike@mikemcquaid.com>
2017-06-12 16:44:47 +0100

    app-engine-java: fix RuboCop warnings.

--- Formula/app-engine-java.rb
+++ Formula/app-engine-java.rb
@@ -19,10 +19,10 @@ class AppEngineJava < Formula
     (testpath/"WEB-INF/appengine-web.xml").write "<appengine-web-app><threadsafe>true</threadsafe></appengine-web-app>"
     Process.setsid
     IO.popen("#{bin}/dev_appserver.sh . 2>&1") do |io|
-      assert_not_nil(io.gets, "Dev App Server terminated prematurely") until $_ == "INFO: Dev App Server is now running\n"
+      assert_not_nil(io.gets, "Dev App Server terminated prematurely") until $LAST_READ_LINE == "INFO: Dev App Server is now running\n"
       Signal.trap "INT", "IGNORE"
       Process.kill "INT", 0
     end
-    assert_equal(130, $?.exitstatus, "Dev App Server exited with unexpected status code")
+    assert_equal(130, $CHILD_STATUS.exitstatus, "Dev App Server exited with unexpected status code")
   end
 end
