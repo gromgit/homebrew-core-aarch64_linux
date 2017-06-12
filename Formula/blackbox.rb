83583dfa82f46868e4afdcd601b19be08855b62c
ilovezfs <ilovezfs@icloud.com>
2017-06-12 00:28:41 -0700

    blackbox 1.20170611 (#14487)

--- Formula/blackbox.rb
+++ Formula/blackbox.rb
@@ -1,8 +1,8 @@
 class Blackbox < Formula
   desc "Safely store secrets in Git/Mercurial/Subversion"
   homepage "https://github.com/StackExchange/blackbox"
-  url "https://github.com/StackExchange/blackbox/archive/v1.20170309.tar.gz"
-  sha256 "c5f75252ab298b6b7b12d9407228a29112df85a228d7ee56a7bc6b0c807da284"
+  url "https://github.com/StackExchange/blackbox/archive/v1.20170611.tar.gz"
+  sha256 "dcdc7a9b7c9fd7144c90baa5f48041af434ed4c8f77c8e41f1017789a2f9c017"
 
   bottle :unneeded
 
@@ -14,10 +14,27 @@ class Blackbox < Formula
   end
 
   test do
-    Gpg.create_test_key(testpath)
-    system "git", "init"
-    system bin/"blackbox_initialize", "yes"
-    add_created_key = shell_output("#{bin}/blackbox_addadmin Testing 2>&1")
-    assert_match "<testing@foo.bar>", add_created_key
+    (testpath/"batch.gpg").write <<-EOS.undent
+      Key-Type: RSA
+      Key-Length: 2048
+      Subkey-Type: RSA
+      Subkey-Length: 2048
+      Name-Real: Testing
+      Name-Email: testing@foo.bar
+      Expire-Date: 1d
+      %no-protection
+      %commit
+    EOS
+    begin
+      system Formula["gnupg"].opt_bin/"gpg", "--batch", "--gen-key", "batch.gpg"
+      system "git", "init"
+      system bin/"blackbox_initialize", "yes"
+      add_created_key = shell_output("#{bin}/blackbox_addadmin Testing 2>&1")
+      assert_match "<testing@foo.bar>", add_created_key
+    ensure
+      system Formula["gnupg"].opt_bin/"gpgconf", "--kill", "gpg-agent"
+      system Formula["gnupg"].opt_bin/"gpgconf", "--homedir", "keyrings/live",
+                                                 "--kill", "gpg-agent"
+    end
   end
 end
