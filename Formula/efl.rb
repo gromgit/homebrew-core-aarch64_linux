d4821f1318a7b4ca5fea187cc06817960917580c
ilovezfs <ilovezfs@icloud.com>
2017-06-12 05:28:34 -0700

    efl: revision for libraw
    Closes #14518.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/efl.rb
+++ Formula/efl.rb
@@ -3,7 +3,7 @@ class Efl < Formula
   homepage "https://www.enlightenment.org"
   url "https://download.enlightenment.org/rel/libs/efl/efl-1.18.4.tar.xz"
   sha256 "39ebc07e37437d6ecdeb0f645783484e28a882b38f7e619ad12c2bf9b5548025"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "cdf482a2830d0e67776cc7f45b2a445eb8be6f72df25e9de9d0a68f984f62bf5" => :sierra
