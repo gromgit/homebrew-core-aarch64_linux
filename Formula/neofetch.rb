e2e79d241300b0a3306c11425846b634fe651d72
Geoff MacIntosh <geoff@mac.into.sh>
2017-06-13 15:57:38 -0600

    neofetch: capitalize description
    Closes #14569.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/neofetch.rb
+++ Formula/neofetch.rb
@@ -1,5 +1,5 @@
 class Neofetch < Formula
-  desc "fast, highly customisable system info script"
+  desc "Fast, highly customisable system info script"
   homepage "https://github.com/dylanaraps/neofetch"
   url "https://github.com/dylanaraps/neofetch/archive/3.1.0.tar.gz"
   sha256 "db7afe24d859b9c8230c3491640d996701816ddc9cf66f98a5071775e8b4ffe5"
