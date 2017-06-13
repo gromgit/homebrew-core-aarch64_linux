a3e52b2d1a658f660021ae0b687f62851e7ad107
Viktor Szakats <vszakats@users.noreply.github.com>
2017-06-13 13:53:59 +0200

    signify-osx: secure url(s)

--- Formula/signify-osx.rb
+++ Formula/signify-osx.rb
@@ -1,6 +1,6 @@
 class SignifyOsx < Formula
   desc "Cryptographically sign and verify files"
-  homepage "http://man.openbsd.org/OpenBSD-current/man1/signify.1"
+  homepage "https://man.openbsd.org/OpenBSD-current/man1/signify.1"
   url "https://github.com/jpouellet/signify-osx/archive/1.3.tar.gz"
   sha256 "c67090135a55478a6a6c11d507d9c3865a11de665c010a8a5f2457737f277f89"
   head "https://github.com/jpouellet/signify-osx.git"
