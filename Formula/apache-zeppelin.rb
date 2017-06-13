c88f72186513b4d183b79def32f52ea13212de43
ilovezfs <ilovezfs@icloud.com>
2017-06-12 22:58:58 -0700

    apache-zeppelin 0.7.2 (#14547)

--- Formula/apache-zeppelin.rb
+++ Formula/apache-zeppelin.rb
@@ -1,8 +1,8 @@
 class ApacheZeppelin < Formula
   desc "Web-based notebook that enables interactive data analytics"
   homepage "https://zeppelin.apache.org"
-  url "https://www.apache.org/dyn/closer.lua?path=zeppelin/zeppelin-0.7.1/zeppelin-0.7.1-bin-all.tgz"
-  sha256 "eaa7c34bb7e4dd0e722f330fe524e361cd8f05980dad214f92f5d6701861f3d8"
+  url "https://www.apache.org/dyn/closer.lua?path=zeppelin/zeppelin-0.7.2/zeppelin-0.7.2-bin-all.tgz"
+  sha256 "67652f0771890fa4e195ee4c37ef4e445be9b709b5856c8d7a5617a2afc4cdbf"
   head "https://github.com/apache/zeppelin.git"
 
   bottle :unneeded
