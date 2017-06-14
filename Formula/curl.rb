5dcbb1c295692c432cd4cc0bbc4b23226f1c1ca8
ilovezfs <ilovezfs@icloud.com>
2017-06-14 01:05:23 -0700

    curl 7.54.1
    Closes #14581.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -1,9 +1,9 @@
 class Curl < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.haxx.se/"
-  url "https://curl.haxx.se/download/curl-7.54.0.tar.bz2"
-  mirror "http://curl.askapache.com/download/curl-7.54.0.tar.bz2"
-  sha256 "f50ebaf43c507fa7cc32be4b8108fa8bbd0f5022e90794388f3c7694a302ff06"
+  url "https://curl.haxx.se/download/curl-7.54.1.tar.bz2"
+  mirror "http://curl.askapache.com/download/curl-7.54.1.tar.bz2"
+  sha256 "fdfc4df2d001ee0c44ec071186e770046249263c491fcae48df0e1a3ca8f25a0"
 
   bottle do
     cellar :any
