38a674d7d306ea94f6958071c5461f19043ee7f0
ilovezfs <ilovezfs@icloud.com>
2017-06-15 07:57:11 -0700

    docutils: use new virtualenv feature
    Closes #14474.
    
    Closes #14624.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/docutils.rb
+++ Formula/docutils.rb
@@ -1,8 +1,11 @@
 class Docutils < Formula
+  include Language::Python::Virtualenv
+
   desc "Text processing system for reStructuredText"
   homepage "https://docutils.sourceforge.io"
   url "https://downloads.sourceforge.net/project/docutils/docutils/0.13.1/docutils-0.13.1.tar.gz"
   sha256 "718c0f5fb677be0f34b781e04241c4067cbd9327b66bdd8e763201130f5175be"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -12,7 +15,7 @@ class Docutils < Formula
   end
 
   def install
-    system "python", *Language::Python.setup_install_args(prefix)
+    virtualenv_install_with_resources
   end
 
   test do
