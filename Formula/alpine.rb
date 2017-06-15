6de4acf0d944534405bacd780a795ce91cf717d9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-15 05:18:07 +0000

    alpine: update 2.21 bottle.

--- Formula/alpine.rb
+++ Formula/alpine.rb
@@ -5,9 +5,10 @@ class Alpine < Formula
   sha256 "6030b6881b8168546756ab3a5e43628d8d564539b0476578e287775573a77438"
 
   bottle do
-    sha256 "54f70a9e4c99187db5c75e0aa0dad06726de997824e306ba625e95466bad8b27" => :sierra
-    sha256 "ac340d52fc2055c7e78622809750274fa9957bb1f3d7a635a29f46416a312d85" => :el_capitan
-    sha256 "551758443501811d21f90d3fc4f29a0787cca2409e9413138c9b8fac96395da0" => :yosemite
+    rebuild 1
+    sha256 "8d0c2b6cd5b91cb904f1ddebe8b5ba27f1c2db50fe26db9a40a8131943abe2b5" => :sierra
+    sha256 "a3385e12f96372323504cf50f32b7a045a24e90d0b767ed9be98fdf705d4d65b" => :el_capitan
+    sha256 "5b57214d7c4603dea4081f4aa8edee42c148a7daad1ed1fd881d4fb01a28d778" => :yosemite
   end
 
   depends_on "openssl"
