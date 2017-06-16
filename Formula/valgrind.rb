35244dfdc9ec618ebadf40bb2ebfd5bdf61bc947
Zhiming Wang <zmwangx@gmail.com>
2017-06-16 05:39:24 -0400

    valgrind 3.13.0
    Closes #14653.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/valgrind.rb
+++ Formula/valgrind.rb
@@ -1,22 +1,8 @@
 class Valgrind < Formula
   desc "Dynamic analysis tools (memory, debug, profiling)"
   homepage "http://www.valgrind.org/"
-
-  stable do
-    url "http://valgrind.org/downloads/valgrind-3.12.0.tar.bz2"
-    sha256 "67ca4395b2527247780f36148b084f5743a68ab0c850cb43e4a5b4b012cf76a1"
-
-    # SVN r16103:
-    # "bzero is non-POSIX (deprecated), accordingly __bzero template required
-    # for all macOS versions. n-i-bz."
-    #
-    # Fixes build on macOS 10.12 (at least). Otherwise we would get undefined
-    # symbols error for __bzero.
-    patch :p0 do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/54d59bf/valgrind/bzero.diff"
-      sha256 "48de4054dba20c27ef6089d3ea7832e48dcbbb5368ac4316394b8be55ffe93a2"
-    end
-  end
+  url "ftp://sourceware.org/pub/valgrind/valgrind-3.13.0.tar.bz2"
+  sha256 "d76680ef03f00cd5e970bbdcd4e57fb1f6df7d2e2c071635ef2be74790190c3b"
 
   bottle do
     sha256 "ab3ef2be621bc6b0619d2a87a263d8d7a99056101305b3f4116d4c98049bd782" => :el_capitan
@@ -30,10 +16,6 @@ class Valgrind < Formula
     depends_on "automake" => :build
     depends_on "libtool" => :build
   end
-
-  # https://bugs.kde.org/show_bug.cgi?id=365327#c2
-  # https://github.com/Homebrew/homebrew-core/pull/6231#issuecomment-255779374
-  depends_on MaximumMacOSRequirement => :el_capitan
 
   # Valgrind needs vcpreload_core-*-darwin.so to have execute permissions.
   # See #2150 for more information.
