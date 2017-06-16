709293640fa6cc08d34a3505c24ca5afdfedb388
ilovezfs <ilovezfs@icloud.com>
2017-06-16 06:12:54 -0700

    chakra: remove formula name from description

--- Formula/chakra.rb
+++ Formula/chakra.rb
@@ -1,5 +1,5 @@
 class Chakra < Formula
-  desc "The core part of the Chakra JavaScript engine that powers Microsoft Edge"
+  desc "The core part of the JavaScript engine that powers Microsoft Edge"
   homepage "https://github.com/Microsoft/ChakraCore"
   url "https://github.com/Microsoft/ChakraCore/archive/v1.5.2.tar.gz"
   sha256 "4bb5c49539a7f6e41632288625cfaff86afe9319f18d88a5caa2c30c21e2bd23"
