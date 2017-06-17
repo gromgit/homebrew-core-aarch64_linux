64dac7a7b84a225c5f9feac2247b46e921040119
Jared Ledvina <jared@techsmix.net>
2017-06-17 12:01:21 -0400

    zork 1.0.2 (new formula)
    Closes #14681.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/zork.rb
@@ -0,0 +1,25 @@
+class Zork < Formula
+  desc "The Dungeon modified from FORTRAN to C"
+  homepage "https://github.com/devshane/zork"
+  url "https://github.com/devshane/zork/archive/v1.0.2.tar.gz"
+  sha256 "169e1848b1e3c503591c23ad4e66ce45e1d5ae617831634e1da9c8fca659e283"
+  head "https://github.com/devshane/zork.git"
+
+  def install
+    system "make", "DATADIR=#{share}", "BINDIR=#{bin}"
+    system "make", "install", "DATADIR=#{share}", "BINDIR=#{bin}", "MANDIR=#{man}"
+  end
+
+  test do
+    test_phrase = <<-EOS.undent.chomp
+      Welcome to Dungeon.\t\t\tThis version created 11-MAR-91.
+      You are in an open field west of a big white house with a boarded
+      front door.
+      There is a small mailbox here.
+      >Opening the mailbox reveals:
+        A leaflet.
+      >
+    EOS
+    assert_equal test_phrase, pipe_output("#{bin}/zork", "open mailbox", 0)
+  end
+end
