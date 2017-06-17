c1e98da9946d8598f74726a68e253e8da8a37e1c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-17 07:27:27 +0000

    jose: add 9 bottle.

--- Formula/jose.rb
+++ Formula/jose.rb
@@ -4,6 +4,13 @@ class Jose < Formula
   url "https://github.com/latchset/jose/releases/download/v9/jose-9.tar.bz2"
   sha256 "64262b1344d92fc183f70ca93db6100cd97b3dfa7cddea1e08e8588e6cd681eb"
 
+  bottle do
+    cellar :any
+    sha256 "8daa4c108b2bf94d1ebec9079d4375a5ca214c09d45f3f29cd541953d6262653" => :sierra
+    sha256 "7729b474946230922fc6d6c9f6799d0a486bd2d6a8cf283f18f2e1e9257ee928" => :el_capitan
+    sha256 "1ef50f01624ba2d71de37f775ed054734fab893b0b4c6b12e9ca8cab6ad8f581" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "jansson"
   depends_on "openssl"
