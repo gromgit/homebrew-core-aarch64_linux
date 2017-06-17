471709d4a0ff741c1b6adeba2276225fec8cfd3d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-17 11:01:45 +0000

    iamy: add 2.0.0 bottle.

--- Formula/iamy.rb
+++ Formula/iamy.rb
@@ -5,6 +5,13 @@ class Iamy < Formula
   sha256 "44906985ac2a7a0f4a703c4eb872e626770ffa1ea01a589279e75867a4348630"
   head "https://github.com/99designs/iamy.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "70502c9a9e56883b3fbb2d4b7b3c6eef670c797624bbe7de50812ec2adf6f263" => :sierra
+    sha256 "eab9f48a18661e6c016c01c822da39f9edf84c06b1a46fcd5ba5b07f138a309f" => :el_capitan
+    sha256 "2691c8fcf937d4fe3887204cdec9836cb9123e747223eb5a324b21cb19bdd303" => :yosemite
+  end
+
   depends_on "glide" => :build
   depends_on "go" => :build
   depends_on "awscli"
