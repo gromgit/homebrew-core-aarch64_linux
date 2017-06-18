431fec3f9bfde190289b61c6f26db07c80d4c187
ilovezfs <ilovezfs@icloud.com>
2017-06-18 04:16:53 -0700

    git-ftp 1.4.0
    vendor curl with libssh2
    always install the man page
    
    Closes #14692.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/git-ftp.rb
+++ Formula/git-ftp.rb
@@ -1,9 +1,8 @@
 class GitFtp < Formula
   desc "Git-powered FTP client"
   homepage "https://git-ftp.github.io/git-ftp"
-  url "https://github.com/git-ftp/git-ftp/archive/1.3.3.tar.gz"
-  sha256 "4e36ef178b498f273dc2e9b7685d28dea69b939dcf3602fdda69d1f525e24f31"
-
+  url "https://github.com/git-ftp/git-ftp/archive/1.4.0.tar.gz"
+  sha256 "080e9385a9470d70a5a2a569c6e7db814902ffed873a77bec9d0084bcbc3e054"
   head "https://github.com/git-ftp/git-ftp.git", :branch => "develop"
 
   bottle do
@@ -13,23 +12,37 @@ class GitFtp < Formula
     sha256 "7c15fc59bcde7f29f12b918f2abf62ff5a20675010ba384ba9a257f474c3c714" => :yosemite
   end
 
-  option "with-manpage", "build and install the manpage (depends on pandoc)"
+  depends_on "pandoc" => :build
+  depends_on "libssh2"
 
-  depends_on "curl" => "with-libssh2"
-  depends_on "pandoc" => :build if build.with? "manpage"
+  resource "curl" do
+    url "https://curl.haxx.se/download/curl-7.54.1.tar.bz2"
+    mirror "http://curl.askapache.com/download/curl-7.54.1.tar.bz2"
+    sha256 "fdfc4df2d001ee0c44ec071186e770046249263c491fcae48df0e1a3ca8f25a0"
+  end
 
   def install
-    system "make", "prefix=#{prefix}", "install"
-    if build.with? "manpage"
-      system "make", "-C", "man", "man"
-      man1.install "man/git-ftp.1"
+    resource("curl").stage do
+      system "./configure", "--disable-debug",
+                            "--disable-dependency-tracking",
+                            "--disable-silent-rules",
+                            "--prefix=#{libexec}",
+                            "--with-darwinssl",
+                            "--without-ca-bundle",
+                            "--without-ca-path",
+                            "--with-libssh2",
+                            "--without-libmetalink",
+                            "--without-gssapi",
+                            "--without-librtmp",
+                            "--disable-ares"
+      system "make", "install"
     end
-    libexec.install bin/"git-ftp"
-    (bin/"git-ftp").write <<-EOS.undent
-      #!/bin/sh
-      PATH=#{Formula["curl"].opt_bin}:$PATH
-      #{libexec}/git-ftp "$@"
-    EOS
+
+    system "make", "prefix=#{prefix}", "install"
+    system "make", "-C", "man", "man"
+    man1.install "man/git-ftp.1"
+    (libexec/"bin").install bin/"git-ftp"
+    (bin/"git-ftp").write_env_script(libexec/"bin/git-ftp", :PATH => "#{libexec}/bin:$PATH")
   end
 
   test do
