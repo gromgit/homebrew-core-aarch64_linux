da0234baba23dca169f9383b6dd4011e8dbd8fd8
ilovezfs <ilovezfs@icloud.com>
2017-06-19 07:13:34 -0700

    gsl 2.4

--- Formula/gsl.rb
+++ Formula/gsl.rb
@@ -1,9 +1,9 @@
 class Gsl < Formula
   desc "Numerical library for C and C++"
   homepage "https://www.gnu.org/software/gsl/"
-  url "https://ftp.gnu.org/gnu/gsl/gsl-2.3.tar.gz"
-  mirror "https://ftpmirror.gnu.org/gsl/gsl-2.3.tar.gz"
-  sha256 "562500b789cd599b3a4f88547a7a3280538ab2ff4939504c8b4ac4ca25feadfb"
+  url "https://ftp.gnu.org/gnu/gsl/gsl-2.4.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gsl/gsl-2.4.tar.gz"
+  sha256 "4d46d07b946e7b31c19bbf33dda6204d7bedc2f5462a1bae1d4013426cd1ce9b"
 
   bottle do
     cellar :any
