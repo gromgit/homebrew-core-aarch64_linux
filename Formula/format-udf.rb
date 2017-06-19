6754f3f1f3e50f2a26caa271676832584e6fddb4
ilovezfs <ilovezfs@icloud.com>
2017-06-19 02:59:02 -0700

    format-udf 1.5.0 (#14722)

--- Formula/format-udf.rb
+++ Formula/format-udf.rb
@@ -1,8 +1,8 @@
 class FormatUdf < Formula
   desc "Bash script to format a block device to UDF"
   homepage "https://github.com/JElchison/format-udf"
-  url "https://github.com/JElchison/format-udf/archive/1.4.4.tar.gz"
-  sha256 "65eacd8835209d5e2f0320cc54436347deffea22e0ffdca312c8bdcebda3af91"
+  url "https://github.com/JElchison/format-udf/archive/1.5.0.tar.gz"
+  sha256 "b6dc304582fa85698d3528beb11ce2633cfe4fe2d43ce3fb3ff7e331934e8d6a"
 
   bottle :unneeded
 
