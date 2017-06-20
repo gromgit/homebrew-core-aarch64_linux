1ef980fea3e123760c4a3e9781548dafd4f0beca
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-20 00:07:33 +0000

    automake: update 1.15.1 bottle.

--- Formula/automake.rb
+++ Formula/automake.rb
@@ -7,12 +7,9 @@ class Automake < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 2
-    sha256 "d5af56f4ded965d47d571de0b07f3b2da35b2a8cfeebc8b7dcc9f93e1ec90dd2" => :sierra
-    sha256 "70a763221d2bb9baaf630f2170224c915ca96a9966fbb1d86781c8743740bb7b" => :el_capitan
-    sha256 "d8e4773130e25ff576a0c7d18b4d010b1e03eba90b0074e1ac749fdf3bc13e26" => :yosemite
-    sha256 "69c1635672fa682a40949572e64fe3495055e97ad2c105dd46fb2e447d0d65a8" => :mavericks
-    sha256 "6e6fdaa7fb7ddaaeb103341d1ca351e0669874f86eb21eb6623cb345dd1f5b6f" => :mountain_lion
+    sha256 "d2b27ce7782a966eb7c8570f7855c8c659bbefc97491a1fb54b66e7b58d26c63" => :sierra
+    sha256 "0b25b38f9df72b7869783514811b7ccd17284a240a7ee5950da501811b9942ff" => :el_capitan
+    sha256 "0b25b38f9df72b7869783514811b7ccd17284a240a7ee5950da501811b9942ff" => :yosemite
   end
 
   keg_only :provided_until_xcode43
