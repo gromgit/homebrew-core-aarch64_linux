4b7a98c2b4752ca3405a4fb2b5e514726c71f4c6
ilovezfs <ilovezfs@icloud.com>
2017-06-21 00:36:47 -0700

    gsmartcontrol 1.0.1
    Closes #14800.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gsmartcontrol.rb
+++ Formula/gsmartcontrol.rb
@@ -1,9 +1,8 @@
 class Gsmartcontrol < Formula
   desc "Graphical user interface for smartctl"
   homepage "https://gsmartcontrol.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/gsmartcontrol/0.8.7/gsmartcontrol-0.8.7.tar.bz2"
-  sha256 "708fa803243abb852ed52050fc82cd3592a798c02743342441996e77f19ffec6"
-  revision 2
+  url "https://downloads.sourceforge.net/project/gsmartcontrol/1.0.1/gsmartcontrol-1.0.1.tar.bz2"
+  sha256 "4ec0320c4c40a129cacabbfa7f302146abb69e927cfe9ded0e55b5b9cbe0d949"
 
   bottle do
     sha256 "74418f9a4fd88bb957d1c10725ffc509b9955075646b7ac0b18d9ef3c65730b4" => :sierra
@@ -13,14 +12,10 @@ class Gsmartcontrol < Formula
 
   depends_on "pkg-config" => :build
   depends_on "smartmontools"
-  depends_on "gtkmm"
+  depends_on "gtkmm3"
   depends_on "pcre"
 
   needs :cxx11
-
-  # Fix bad includes with gtkmm-2.24.3
-  # Check if this is still needed with new versions of gsmartcontrol and gtkmm
-  patch :DATA
 
   def install
     ENV.cxx11
@@ -34,29 +29,3 @@ class Gsmartcontrol < Formula
     system "#{bin}/gsmartcontrol", "--version"
   end
 end
-
-__END__
-diff --git a/src/applib/cmdex_sync_gui.cpp b/src/applib/cmdex_sync_gui.cpp
-index d253a17..83b2e11 100644
---- a/src/applib/cmdex_sync_gui.cpp
-+++ b/src/applib/cmdex_sync_gui.cpp
-@@ -9,6 +9,7 @@
- /// \weakgroup applib
- /// @{
-
-+#include <glibmm.h>
- #include <gtkmm/main.h>  // Gtk::Main
-
- #include "hz/fs_path.h"
-diff --git a/src/gsc_init.cpp b/src/gsc_init.cpp
-index 0ded7bc..6fb1bb7 100644
---- a/src/gsc_init.cpp
-+++ b/src/gsc_init.cpp
-@@ -15,6 +15,7 @@
- #include <cstdio>  // std::printf
- #include <vector>
- #include <sstream>
-+#include <glibmm.h>
- #include <gtkmm/main.h>
- #include <gtkmm/messagedialog.h>
- #include <gtk/gtk.h>  // gtk_window_set_default_icon_name, gtk_icon_theme_*
