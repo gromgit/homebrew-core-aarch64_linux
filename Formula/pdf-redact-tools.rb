a2886ba4282ef8700d3385088f28c0d34540f317
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-21 22:37:58 +0000

    pdf-redact-tools: update 0.1.2 bottle.

--- Formula/pdf-redact-tools.rb
+++ Formula/pdf-redact-tools.rb
@@ -7,10 +7,9 @@ class PdfRedactTools < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "307a77850976866c2e12a782bff0b42983a75b722411420709d7f624c2cfba4f" => :sierra
-    sha256 "660fd61a0bb83272ec2d5e818435484ff53d5a88f7bc619fcccafd5c5e855203" => :el_capitan
-    sha256 "1bfb6823d1c7e94f37cf86a3d30bcd777873b69f1a58556dcb7f3d2d25cd4123" => :yosemite
-    sha256 "13ef89b6f2f20f22513412f4a4b7f815fe297276a451097228df21bb8835e610" => :mavericks
+    sha256 "80b4e04bd761bad0cfdea11abfcd19cf96b785cf4e2877998f17bb8a8136afb0" => :sierra
+    sha256 "b462fadc539b2c646c9ff8a59b98df344d0a6cb2fb010c2d4a82785d2f0007e6" => :el_capitan
+    sha256 "b462fadc539b2c646c9ff8a59b98df344d0a6cb2fb010c2d4a82785d2f0007e6" => :yosemite
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
