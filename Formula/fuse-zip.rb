8197302b29003820cc12c252e59d8e6df7b79ded
JCount <JCount42@gmail.com>
2017-06-22 10:29:53 -0400

    fuse-zip: revision for libzip

--- Formula/fuse-zip.rb
+++ Formula/fuse-zip.rb
@@ -3,7 +3,7 @@ class FuseZip < Formula
   homepage "https://code.google.com/p/fuse-zip/"
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/fuse-zip/fuse-zip-0.4.0.tar.gz"
   sha256 "db9eb13aa250061eaa7df6b1ee5022dbea54089094af94cc3d1767b63bdc9ca7"
-  revision 1
+  revision 2
 
   head "https://code.google.com/p/fuse-zip/", :using => :hg
 
