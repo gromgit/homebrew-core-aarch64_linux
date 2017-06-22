b6491ed7ddc45462f8bf06113372d5ce5ef5d545
ilovezfs <ilovezfs@icloud.com>
2017-06-21 23:26:01 -0700

    ansiweather 1.11 (#14829)

--- Formula/ansiweather.rb
+++ Formula/ansiweather.rb
@@ -1,8 +1,8 @@
 class Ansiweather < Formula
   desc "Weather in your terminal, with ANSI colors and Unicode symbols"
   homepage "https://github.com/fcambus/ansiweather"
-  url "https://github.com/fcambus/ansiweather/archive/1.10.tar.gz"
-  sha256 "9656ac85b67ff10c5959f31345bb08bfef0b038f6f2fbe09c78363b28f42cc21"
+  url "https://github.com/fcambus/ansiweather/archive/1.11.tar.gz"
+  sha256 "14e3e55022925b7f54c7f15507c5e6612eb7826f3d1b19b7d3cc9cde68501463"
   head "https://github.com/fcambus/ansiweather.git"
 
   bottle :unneeded
