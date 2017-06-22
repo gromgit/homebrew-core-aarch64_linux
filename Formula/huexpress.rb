17fd9216d2a763241968fc871a125b90cd090512
JCount <JCount42@gmail.com>
2017-06-22 10:31:17 -0400

    huexpress: revision for libzip

--- Formula/huexpress.rb
+++ Formula/huexpress.rb
@@ -3,7 +3,7 @@ class Huexpress < Formula
   homepage "https://github.com/kallisti5/huexpress"
   url "https://github.com/kallisti5/huexpress/archive/3.0.3.tar.gz"
   sha256 "159a13cd469d0645377377604c0fc4b3d3d1980d4d0e71c634c293f99db2c497"
-  revision 1
+  revision 2
   head "https://github.com/kallisti5/huexpress.git"
 
   bottle do
