0547f99969b7df842424aa9e654cc738599ba823
ilovezfs <ilovezfs@icloud.com>
2017-06-23 02:00:54 -0700

    prest 0.2.0
    Closes #14867.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/prest.rb
+++ Formula/prest.rb
@@ -1,8 +1,8 @@
 class Prest < Formula
   desc "Serve a RESTful API from any PostgreSQL database"
   homepage "https://github.com/nuveo/prest"
-  url "https://github.com/nuveo/prest/archive/v0.1.7.tar.gz"
-  sha256 "48ed93539238be8589e7b95937e7d4157acea8209d0badb8a04fec7675088102"
+  url "https://github.com/nuveo/prest/archive/v0.2.0.tar.gz"
+  sha256 "8fb0416105895424fc4ae6b13b583a3de33c15875b73435766b99b65781123ab"
 
   bottle do
     cellar :any_skip_relocation
