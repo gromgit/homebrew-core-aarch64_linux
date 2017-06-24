5f6536e48e44b83e98b54d8b422b99166897317f
ilovezfs <ilovezfs@icloud.com>
2017-06-24 08:30:37 -0700

    hg-fast-export 170624 (#14910)

--- Formula/hg-fast-export.rb
+++ Formula/hg-fast-export.rb
@@ -1,8 +1,8 @@
 class HgFastExport < Formula
   desc "Fast Mercurial to Git converter"
   homepage "http://repo.or.cz/w/fast-export.git"
-  url "https://github.com/frej/fast-export/archive/v170617.tar.gz"
-  sha256 "e0c415a36faa41a8b34e7637817b4ea19258cb86141d7791ee26d1be2523a7f9"
+  url "https://github.com/frej/fast-export/archive/v170624.tar.gz"
+  sha256 "f46963ba643c509f32733384b40cdb98f058f25211cd7c107056285efa83148f"
 
   bottle :unneeded
 
