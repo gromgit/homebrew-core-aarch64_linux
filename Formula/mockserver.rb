06840cb29cf828af8ed1270f81dbe26bdb9cbbba
James D Bloom <jamesdbloom@gmail.com>
2017-06-24 08:33:51 +0100

    mockserver 3.10.8 (#14863)

--- Formula/mockserver.rb
+++ Formula/mockserver.rb
@@ -1,9 +1,9 @@
 class Mockserver < Formula
   desc "Mock HTTP server and proxy"
   homepage "https://www.mock-server.com/"
-  url "https://oss.sonatype.org/content/repositories/releases/org/mock-server/mockserver-netty/3.10.4/mockserver-netty-3.10.4-brew-tar.tar"
-  version "3.10.4"
-  sha256 "0abd89cb0f894cf53d678360452962f6102477a45b41691eaaea47dfce8d8467"
+  url "https://oss.sonatype.org/content/repositories/releases/org/mock-server/mockserver-netty/3.10.8/mockserver-netty-3.10.8-brew-tar.tar"
+  version "3.10.8"
+  sha256 "f381d0318c8cb8be04e7d18c117ebcf91b48228cb0bf8783311a38f268311fe5"
 
   bottle :unneeded
 
