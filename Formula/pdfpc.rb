2ad36e60e513e003e628a86b26484e7469b93938
Simon Wörner <git@simon-woerner.de>
2017-06-24 12:26:31 +0200

    pdfpc 4.0.7 (new formula)
    Closes #14906.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/pdfpc.rb
@@ -0,0 +1,23 @@
+class Pdfpc < Formula
+  desc "Presenter console with multi-monitor support for PDF files"
+  homepage "https://pdfpc.github.io/"
+  url "https://github.com/pdfpc/pdfpc/archive/v4.0.7.tar.gz"
+  sha256 "25d5aa2cce344f1554f1f45a7c1f636576587cfd553d75d704e0c95ae3b2a503"
+  head "https://github.com/pdfpc/pdfpc.git"
+
+  depends_on "cmake" => :build
+  depends_on "vala" => :build
+  depends_on "gtk+3"
+  depends_on "libgee"
+  depends_on "librsvg"
+  depends_on "poppler"
+
+  def install
+    system "cmake", ".", "-DMOVIES=off", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/pdfpc", "--version"
+  end
+end
