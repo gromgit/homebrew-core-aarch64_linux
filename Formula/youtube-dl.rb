78255b540d0444fd4ad5fb6ab6ad66e7877544a0
Zhiming Wang <zmwangx@gmail.com>
2017-06-25 01:49:21 -0400

    youtube-dl 2017.06.25 (#14918)

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -1,8 +1,8 @@
 class YoutubeDl < Formula
   desc "Download YouTube videos from the command-line"
   homepage "https://rg3.github.io/youtube-dl/"
-  url "https://github.com/rg3/youtube-dl/releases/download/2017.06.23/youtube-dl-2017.06.23.tar.gz"
-  sha256 "632109900e84619471c1c578688ec0a93834275be80ed98434ddd598cc0ca127"
+  url "https://github.com/rg3/youtube-dl/releases/download/2017.06.25/youtube-dl-2017.06.25.tar.gz"
+  sha256 "0531958d4dc10f0fae7960a4c8d08d17fe57acf79719a76387d765c382809f59"
 
   head do
     url "https://github.com/rg3/youtube-dl.git"
