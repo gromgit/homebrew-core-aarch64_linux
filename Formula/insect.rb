b836663d37eeded4ac1deebd5db1392cf0edc8ad
Zhiming Wang <zmwangx@gmail.com>
2017-06-25 15:25:31 -0400

    insect 4.7.0
    Closes #14937.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/insect.rb
+++ Formula/insect.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Insect < Formula
   desc "High precision scientific calculator with support for physical units"
   homepage "https://insect.sh/"
-  url "https://registry.npmjs.org/insect/-/insect-4.6.0.tgz"
-  sha256 "f043cc0da5b0ca77192902af76c2e1b7ba62a003d1a62e97c6296de9db2a1938"
+  url "https://registry.npmjs.org/insect/-/insect-4.7.0.tgz"
+  sha256 "a5f5fbd4f4a50362e611a7be7585fb24b973219857f8a56290ae4b95b7f1f00f"
 
   bottle do
     cellar :any_skip_relocation
