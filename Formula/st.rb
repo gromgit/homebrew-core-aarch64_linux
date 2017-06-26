48cc1aeca9482470cea42fd3711e3d2f7466c573
ilovezfs <ilovezfs@icloud.com>
2017-06-26 09:05:13 -0700

    st 1.1.4
    Closes #14965.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/st.rb
+++ Formula/st.rb
@@ -1,8 +1,8 @@
 class St < Formula
   desc "Statistics from the command-line"
   homepage "https://github.com/nferraz/st"
-  url "https://github.com/nferraz/st/archive/v1.1.2.tar.gz"
-  sha256 "46a3d10995a910870d07550ed86c2979a46523059bed4067e9a49a403be331c8"
+  url "https://github.com/nferraz/st/archive/v1.1.4.tar.gz"
+  sha256 "c02a16f67e4c357690a5438319843149fd700c223128f9ffebecab2849c58bb8"
 
   bottle do
     cellar :any_skip_relocation
@@ -18,7 +18,6 @@ class St < Formula
 
     system "perl", "Makefile.PL", "PREFIX=#{libexec}"
     system "make", "install"
-    inreplace libexec/"bin/st", "perl -T", "perl"
 
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files libexec/"bin", :PERL5LIB => ENV["PERL5LIB"]
