e43494174c52caef1158e92635abcd37217a9f8b
ilovezfs <ilovezfs@icloud.com>
2017-06-26 00:44:54 -0700

    ipv6calc 1.0.0
    Closes #14947.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/ipv6calc.rb
+++ Formula/ipv6calc.rb
@@ -1,19 +1,14 @@
 class Ipv6calc < Formula
   desc "Small utility for manipulating IPv6 addresses"
   homepage "https://www.deepspace6.net/projects/ipv6calc.html"
-  url "https://github.com/pbiering/ipv6calc/archive/0.99.2.tar.gz"
-  sha256 "f2eeec1b8d8626756f2cb9c461e9d1db20affccf582d43ded439bdb2d12646ef"
+  url "https://github.com/pbiering/ipv6calc/archive/1.0.0.tar.gz"
+  sha256 "74b0455e61834843bf8a5e7e0e0f39dd2b148114ff896d590eb2d826714594bd"
 
   bottle do
     cellar :any_skip_relocation
     sha256 "a67a510d9788019d98836c391811b08af79f0bcbdfa0fc0b7c2ea119b7bc9afd" => :sierra
     sha256 "ef399281d676e0f9d65038fa7d43f7abca807e3aaf437366126a8a1a4f6a8bf6" => :el_capitan
     sha256 "ad4d9073a25962b4321648ee79658e04517d340025cc6ae346342d301cab3a40" => :yosemite
-  end
-
-  patch do
-    url "https://github.com/pbiering/ipv6calc/commit/128cb3b178dde1b9bcadc1b7a334c5eebcc529be.patch"
-    sha256 "3148380d4aba3b50150597ec209a1cf7aca7091d3be5f57a2e517445cb55430c"
   end
 
   def install
