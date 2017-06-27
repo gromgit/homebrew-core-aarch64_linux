6b0eef1148fc1a0a66a68749af12aa42e7c2a8ea
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-27 11:47:00 +0000

    poppler: update 0.56.0 bottle.

--- Formula/poppler.rb
+++ Formula/poppler.rb
@@ -5,9 +5,9 @@ class Poppler < Formula
   sha256 "869dbadf99ed882e776acbdbc06689d8a81872a2963440b1e8516cd7a2577173"
 
   bottle do
-    sha256 "55bc6d2cac4f69d0f019583d19859c0d91ca0802bcc0d836b5d324c3a8d171d1" => :sierra
-    sha256 "b16c0388cebccac0aac1bc897030404fb8629b27b5281d18528e16d303a885d9" => :el_capitan
-    sha256 "e9f126d38017b290f881c6970163494f91d267034f063f3afe3cdce5417e7be3" => :yosemite
+    sha256 "183cfb95cb3f30ee45db2e6b6bef3a9b98bd001b6b03c209a6e3fdd62b15313d" => :sierra
+    sha256 "9aa64b99728fbf5340da9333648492ff3cd868aa6c9a1bdb0f3b367dfa657926" => :el_capitan
+    sha256 "2d64600d12dbe6e687c2a0499d5828aeb27d9963cec8bcaba62c5ea19381d598" => :yosemite
   end
 
   option "with-qt", "Build Qt5 backend"
