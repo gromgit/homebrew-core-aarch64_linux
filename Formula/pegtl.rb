d21fe19054bfa7850fd399dc1858aa41624ea3f0
ilovezfs <ilovezfs@icloud.com>
2017-06-27 13:08:14 -0700

    pegtl 2.1.3
    Closes #15017.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/pegtl.rb
+++ Formula/pegtl.rb
@@ -1,8 +1,8 @@
 class Pegtl < Formula
   desc "Parsing Expression Grammar Template Library"
   homepage "https://github.com/taocpp/PEGTL"
-  url "https://github.com/taocpp/PEGTL/archive/2.1.2.tar.gz"
-  sha256 "5a6df6460d84decf93dc40cbf7e8f977f45b2871e144ad400da13a13c9723d41"
+  url "https://github.com/taocpp/PEGTL/archive/2.1.3.tar.gz"
+  sha256 "1c979a9903f9ce9f644f80e3a5e882c6cf7a68e4152572ac6f25d56a7e119bf3"
 
   bottle :unneeded
 
