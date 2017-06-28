a95e4094a3125ecb165896770e841d2b936b5a6f
ilovezfs <ilovezfs@icloud.com>
2017-06-28 01:16:32 -0700

    apache-flink 1.3.1 (#15023)

--- Formula/apache-flink.rb
+++ Formula/apache-flink.rb
@@ -1,9 +1,9 @@
 class ApacheFlink < Formula
   desc "Scalable batch and stream data processing"
   homepage "https://flink.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.3.0/flink-1.3.0-bin-hadoop27-scala_2.11.tgz"
-  version "1.3.0"
-  sha256 "a4e2f98605233999d018b5950ba7172e38fc2e3be30196e1fe13af5ea8856a45"
+  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.3.1/flink-1.3.1-bin-hadoop27-scala_2.11.tgz"
+  version "1.3.1"
+  sha256 "6522d0d8a10af596e40ed710306027758a0535c1e078f587eee420032a75eed9"
   head "https://github.com/apache/flink.git"
 
   bottle :unneeded
