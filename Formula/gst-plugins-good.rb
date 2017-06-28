04d35246c04bddb17dd092a58d75c2a1cdec51d4
Philippe Normand <phil@base-art.net>
2017-06-28 12:47:39 +0100

    gst-plugins-good 1.12.1

--- Formula/gst-plugins-good.rb
+++ Formula/gst-plugins-good.rb
@@ -3,8 +3,8 @@ class GstPluginsGood < Formula
   homepage "https://gstreamer.freedesktop.org/"
 
   stable do
-    url "https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-1.10.4.tar.xz"
-    sha256 "8a86c61434a8c44665365bd0b3557a040937d1f44bf69caee4e9ea816ce74d7e"
+    url "https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-1.12.1.tar.xz"
+    sha256 "121e8e46a7f0e622f09ec9be012607b89d737dd72d48b0f2f0680821ae2cf54b"
 
     depends_on "check" => :optional
   end
