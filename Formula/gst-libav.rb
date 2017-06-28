c6b4b9353d90427816689c8b401103be75263429
Philippe Normand <phil@base-art.net>
2017-06-28 15:13:34 +0100

    gst-libav 1.12.1

--- Formula/gst-libav.rb
+++ Formula/gst-libav.rb
@@ -1,8 +1,8 @@
 class GstLibav < Formula
   desc "GStreamer plugins for Libav (a fork of FFmpeg)"
   homepage "https://gstreamer.freedesktop.org/"
-  url "https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-1.10.4.tar.xz"
-  sha256 "6ca0feca75e3d48315e07f20ec37cf6260ed1e9dde58df355febd5016246268b"
+  url "https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-1.12.1.tar.xz"
+  sha256 "55d7823fdf63195de991adfef32db4b8a8bef3cafa46115a951e590dc18a5077"
 
   bottle do
     sha256 "8b87c592e99c7f553f1e78fb257ecc34e2d022d5cdec86e230c2d9ac1e77a1dc" => :sierra
