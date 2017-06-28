36ae1c65ef04353b1cc485a6699c4002e3912251
Tomasz Pajor <tomek@polishgeeks.com>
2017-06-28 10:35:14 +0200

    scamper 20161204a
    Closes #15042.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/scamper.rb
+++ Formula/scamper.rb
@@ -1,8 +1,8 @@
 class Scamper < Formula
   desc "Advanced traceroute and network measurement utility"
   homepage "https://www.caida.org/tools/measurement/scamper/"
-  url "https://www.caida.org/tools/measurement/scamper/code/scamper-cvs-20161204.tar.gz"
-  sha256 "0c9d8a593765a6d1074690de987c6ffd5753406c9d1e527e20c0289bde0db764"
+  url "https://www.caida.org/tools/measurement/scamper/code/scamper-cvs-20161204a.tar.gz"
+  sha256 "78eeabed8a4161a2238d02dac4c4361c9fb78f53d52fd8dd4bdb27434b512038"
 
   bottle do
     cellar :any
