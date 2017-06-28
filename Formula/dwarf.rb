e6f554d2009a5a22cebffc556ef8731a324cf18c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-28 15:13:52 +0000

    dwarf: add 0.3.2 bottle.

--- Formula/dwarf.rb
+++ Formula/dwarf.rb
@@ -4,6 +4,12 @@ class Dwarf < Formula
   url "https://github.com/elboza/dwarf-ng/archive/dwarf-0.3.2.tar.gz"
   sha256 "dc3db5273c02f0b05beedada0a3935af0c60c011d7213f37c39f43ce088f9436"
 
+  bottle do
+    sha256 "84c3d641587619c55073a819edf62b23a6437aeed72075e257272df685e226aa" => :sierra
+    sha256 "053fba2171b46fe1d9fd22f52ca5eee61e462682e2b9340c671505e5351fd5d6" => :el_capitan
+    sha256 "1a798403cb54f055465e16fe67e7db63dd693ee993d0a871c32c6f143621d7f3" => :yosemite
+  end
+
   depends_on "flex"
   depends_on "readline"
 
