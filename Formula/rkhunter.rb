e9c133e633b3492aafa97e29f184987c7d5cf0a9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-29 11:33:52 +0000

    rkhunter: update 1.4.4 bottle.

--- Formula/rkhunter.rb
+++ Formula/rkhunter.rb
@@ -6,10 +6,9 @@ class Rkhunter < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d314b78c20ac638e0e8db2488cde0d249fffcaf4fff983202019f7918fbd69e3" => :sierra
-    sha256 "7c13ec707da81dfb0cdc9ad8b8ffd9d3549454db698379c6159f3d69c4ed617d" => :el_capitan
-    sha256 "5269398b0bc05b26f47033f790c96c508ba8a072d904d6e06a4abbe4c3778e95" => :yosemite
-    sha256 "1eeeab51dfa8dafeb1b4f9b283f89bf18180c4c55ef65b9b716f892ef53ee85f" => :mavericks
+    sha256 "d0f86dcf70bb5a827317f2c487d4238b61b03edc87cd7a0d1d0dcba1ea87d774" => :sierra
+    sha256 "dddb86855d2ab0bb0a82c5861eea40b5efddaa8feff85744bebcd213aeea2bdd" => :el_capitan
+    sha256 "5019567a795b47aa07bb27c897dd1971795d220747c38968e6834ff5edf430cc" => :yosemite
   end
 
   def install
