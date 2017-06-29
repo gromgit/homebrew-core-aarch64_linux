13e5f2cc7f7e3ac77068e4aaa207e8f50885e037
JCount <JCount42@gmail.com>
2017-06-29 16:19:26 -0400

    m4: %n crash fix for 10.13
    Fix crash from the usage of %n in
    dynamic format strings on High Sierra

--- Formula/m4.rb
+++ Formula/m4.rb
@@ -14,6 +14,15 @@ class M4 < Formula
 
   keg_only :provided_by_osx
 
+  # Fix crash from usage of %n in dynamic format strings on High Sierra
+  # Patch credit to Jeremy Huddleston Sequoia <jeremyhu@apple.com>
+  if MacOS.version >= :high_sierra
+    patch :p0 do
+      url "https://raw.githubusercontent.com/macports/macports-ports/edf0ee1e2cf/devel/m4/files/secure_snprintf.patch"
+      sha256 "57f972940a10d448efbd3d5ba46e65979ae4eea93681a85e1d998060b356e0d2"
+    end
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make"
