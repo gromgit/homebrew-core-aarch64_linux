3651399772084041ff7bfd45cb7cf11e9f9aeaf2
Misty De Meo <mistydemeo@gmail.com>
2017-06-28 22:09:40 -0700

    partio: use full_index patch

--- Formula/partio.rb
+++ Formula/partio.rb
@@ -18,18 +18,18 @@ class Partio < Formula
 
   # These fixes are upstream and can be removed in the next released version.
   patch do
-    url "https://github.com/wdas/partio/commit/5b80b00ddedaef9ffed19ea4e6773ed1dc27394e.diff"
-    sha256 "f3808c2b8032f35fee84d90ebaaa8f740376129cd5af363a32ea1e0f92d9282a"
+    url "https://github.com/wdas/partio/commit/5b80b00ddedaef9ffed19ea4e6773ed1dc27394e.diff?full_index=1"
+    sha256 "b14b5526d5b61a3dfec7ddd07b54d5a678170b15b3f83687ab1b4151ae0cd5f3"
   end
 
   patch do
-    url "https://github.com/wdas/partio/commit/bdce60e316b699fb4fd813c6cad9d369205657c8.diff"
-    sha256 "144bdd9c8f490a26e1f39cd1f15be06f8fcbe3cdc99d43abf307bfd25dc5402e"
+    url "https://github.com/wdas/partio/commit/bdce60e316b699fb4fd813c6cad9d369205657c8.diff?full_index=1"
+    sha256 "58dc0b77155b80301595c0b6a439e852f41779a31348f1716f2c9714273c638b"
   end
 
   patch do
-    url "https://github.com/wdas/partio/commit/e557c212b0e8e0c4830e7991541686d568853afd.diff"
-    sha256 "f73a6db9ab41eb796f929264a47eba7b0c8826e4590f0caa7679c493aa21b382"
+    url "https://github.com/wdas/partio/commit/e557c212b0e8e0c4830e7991541686d568853afd.diff?full_index=1"
+    sha256 "b20a25142316cf93c0cc1188508c39a01275cf784d628e9768d5ee1471adbee2"
   end
 
   def install
