1d13102b6c2cd74967c7d7c6a6e364996910b90e
ilovezfs <ilovezfs@icloud.com>
2017-06-29 09:37:52 -0700

    nodeenv 1.1.4 (#15108)
    set bottle unneeded

--- Formula/nodeenv.rb
+++ Formula/nodeenv.rb
@@ -1,20 +1,13 @@
 class Nodeenv < Formula
-  include Language::Python::Virtualenv
-
   desc "Node.js virtual environment builder"
   homepage "https://github.com/ekalinin/nodeenv"
-  url "https://files.pythonhosted.org/packages/d5/33/0ec380a95c527b1c6077bcedfc7c4d05b275f56b15a0e7f9a3da9558cd02/nodeenv-1.1.3.tar.gz"
-  sha256 "0d7bdb2f5583839cabdbab2ebc091f28cb1d947cc077431b77002e8396b089f9"
+  url "https://github.com/ekalinin/nodeenv/archive/1.1.4.tar.gz"
+  sha256 "8f844c64bc0d8e14cb104a83d0f5b66b7cfeb413b4bae590318103f6b8126327"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "89d883152e213d8a38d933272d717ee0aaf229aa5958ad69285091dab09c2138" => :sierra
-    sha256 "c2f11f9d4a5a97ee9867ed6d3bd516c228b81308e5b12eca6ac18dc52231cb82" => :el_capitan
-    sha256 "4896da3820fe3fa49916753223379063137dc49cead42a4f652d5be9f7454ecc" => :yosemite
-  end
+  bottle :unneeded
 
   def install
-    virtualenv_install_with_resources
+    bin.install "nodeenv.py" => "nodeenv"
   end
 
   test do
