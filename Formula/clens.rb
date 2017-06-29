f37c943c254fd9e63175f11822393056fa1cbe05
Misty De Meo <mistydemeo@gmail.com>
2017-06-28 22:09:38 -0700

    clens: use full_index patch

--- Formula/clens.rb
+++ Formula/clens.rb
@@ -13,8 +13,8 @@ class Clens < Formula
   end
 
   patch do
-    url "https://github.com/conformal/clens/commit/83648cc9027d9f76a1bc79ddddcbed1349b9d5cd.diff"
-    sha256 "a685d970c9bc785dcc892f39803dad2610ce979eb58738da5d45365fd81a14be"
+    url "https://github.com/conformal/clens/commit/83648cc9027d9f76a1bc79ddddcbed1349b9d5cd.diff?full_index=1"
+    sha256 "efd43aa84f7ff1582c3716282ac1b3b650c16af9a0f55039d20bfd4b085188e2"
   end
 
   def install
