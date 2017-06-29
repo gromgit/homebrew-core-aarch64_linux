ece91f0b2a487d4ce999523da06b9709e1d05835
Misty De Meo <mistydemeo@gmail.com>
2017-06-28 22:09:40 -0700

    mpssh: use full_index patch

--- Formula/mpssh.rb
+++ Formula/mpssh.rb
@@ -8,8 +8,8 @@ class Mpssh < Formula
     sha256 "510e11c3e177a31c1052c8b4ec06357c147648c86411ac3ed4ac814d0d927f2f"
     patch do
       # don't install binaries as root (upstream commit)
-      url "https://github.com/ndenev/mpssh/commit/3cbb868b6fdf8dff9ab86868510c0455ad1ec1b3.diff"
-      sha256 "f5df424a91df1f427f96cd482d0bc22cfd90ac25c9e6beb8ca029f3a1038c3de"
+      url "https://github.com/ndenev/mpssh/commit/3cbb868b6fdf8dff9ab86868510c0455ad1ec1b3.diff?full_index=1"
+      sha256 "c91c3478f05de4b3c200a384ca28449d0c43ad569e8035b3b5247c787f0a984b"
     end
   end
 
