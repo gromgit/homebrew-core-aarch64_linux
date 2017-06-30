0bfa9b627a9a41b8540acba9b8980ee21e31e9d4
ilovezfs <ilovezfs@icloud.com>
2017-06-30 08:58:27 -0700

    archi-steam-farm: remove redundant version specification

--- Formula/archi-steam-farm.rb
+++ Formula/archi-steam-farm.rb
@@ -2,7 +2,6 @@ class ArchiSteamFarm < Formula
   desc "ASF is a C# application that allows you to farm steam cards"
   homepage "https://github.com/JustArchi/ArchiSteamFarm"
   url "https://github.com/JustArchi/ArchiSteamFarm/releases/download/2.3.2.0/ASF.zip"
-  version "2.3.2.0"
   sha256 "1a9f50c3cf2eb00e5148bc21a209b0c7c275b6c36c8cae8b4d9b2469bee7ff33"
 
   bottle :unneeded
