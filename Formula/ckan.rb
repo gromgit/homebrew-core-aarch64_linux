0b97150b38128dc85a03ee5a4986065ced7d4676
ilovezfs <ilovezfs@icloud.com>
2017-06-30 08:58:28 -0700

    ckan: remove redundant version specification

--- Formula/ckan.rb
+++ Formula/ckan.rb
@@ -2,7 +2,6 @@ class Ckan < Formula
   desc "The Comprehensive Kerbal Archive Network"
   homepage "https://github.com/KSP-CKAN/CKAN/"
   url "https://github.com/KSP-CKAN/CKAN/releases/download/v1.22.3/ckan.exe", :using => :nounzip
-  version "1.22.3"
   sha256 "8b02bb62f9786a9f3a764594d4a3297c73bc5d476099afcf48a4fa14b56a1204"
 
   bottle :unneeded
