a3b1e123dc712cb0450ac5de446602d0e62b2c8d
ilovezfs <ilovezfs@icloud.com>
2017-06-30 02:26:41 -0700

    groovy 2.4.12 (#15119)

--- Formula/groovy.rb
+++ Formula/groovy.rb
@@ -1,8 +1,8 @@
 class Groovy < Formula
   desc "Java-based scripting language"
   homepage "http://www.groovy-lang.org"
-  url "https://dl.bintray.com/groovy/maven/apache-groovy-binary-2.4.11.zip"
-  sha256 "4479a3653c565b173fc0d0e5e514a06f1c7d6f93926cbe0c5d8d29e5e4a0347c"
+  url "https://dl.bintray.com/groovy/maven/apache-groovy-binary-2.4.12.zip"
+  sha256 "93b9a19c760c2af846afa0e9c78692d70186cdde36e070e9806fe11b84a8a7b6"
 
   bottle :unneeded
 
