7f70b2c9ca790356387053ee9ebddb5df104afe9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-30 09:53:36 +0000

    kubernetes-cli: update 1.7.0 bottle.

--- Formula/kubernetes-cli.rb
+++ Formula/kubernetes-cli.rb
@@ -8,9 +8,9 @@ class KubernetesCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "fdcdc3f168288a0a0289c518a811cfe544e19d3ceba1f61fd5cc86ebd259a31b" => :sierra
-    sha256 "8dc6e6eb12fa3ec5d4cd6148c2f425ad453d98ce05be79e3853b49fca61584ce" => :el_capitan
-    sha256 "0f6d79b7e5cac303959f38dc786388e78aa7a3cfbd9c5f8d79c03705b3f19ad0" => :yosemite
+    sha256 "9bd5963b6c30333ff4df6dfef3e1ed716e6a8ee469314192ae1458d747f9d068" => :sierra
+    sha256 "fdd681be3ffa3283bd26c0548c097d10a119d974d631176633dc91e7a76455bc" => :el_capitan
+    sha256 "5a00f0a6f8f0e9c9891728e50097719a89c4c38dcc0e754b0134512ee00f1b8f" => :yosemite
   end
 
   depends_on "go" => :build
