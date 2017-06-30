8865a85b2bdbb0e61dee3f6beee1b505cc88a879
ilovezfs <ilovezfs@icloud.com>
2017-06-30 08:58:28 -0700

    nuget: remove redundant version specification

--- Formula/nuget.rb
+++ Formula/nuget.rb
@@ -2,7 +2,6 @@ class Nuget < Formula
   desc "Package manager for Microsoft development platform including .NET"
   homepage "https://www.nuget.org/"
   url "https://dist.nuget.org/win-x86-commandline/v4.0.0/NuGet.exe"
-  version "4.0.0"
   sha256 "cc52f94b2f1ba7cd485e546f8059cada2e9daee2ae27abde54507e9b1661e6d1"
 
   bottle :unneeded
