0258c7982137b9dbe0ca5f2e1610e56e0578334d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-30 01:22:06 +0000

    apache-arrow: add 0.4.1 bottle.

--- Formula/apache-arrow.rb
+++ Formula/apache-arrow.rb
@@ -6,6 +6,13 @@ class ApacheArrow < Formula
 
   head "https://github.com/apache/arrow.git"
 
+  bottle do
+    cellar :any
+    sha256 "4baf8d8e346d82f8babed57ec2097843297d62222dc61d8c9c7359b58906f6cc" => :sierra
+    sha256 "cc4fcc810cf56d16285bf7084a704b0ccf51906149c416423b92c24889c460d4" => :el_capitan
+    sha256 "85ae6ec3bec5d9c9796a8d1dd87c7af8e6301d282c8327a58385519eda0c4214" => :yosemite
+  end
+
   # NOTE: remove ccache with Apache Arrow 0.5 and higher version
   depends_on "cmake" => :build
   depends_on "boost"
