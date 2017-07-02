dfbe64dd3972ae1b31da55699ffefb7e8406b109
ilovezfs <ilovezfs@icloud.com>
2017-07-02 02:13:51 -0700

    xvid: don't use fossies.org for primary url

--- Formula/xvid.rb
+++ Formula/xvid.rb
@@ -1,9 +1,8 @@
 class Xvid < Formula
   desc "High-performance, high-quality MPEG-4 video library"
   homepage "https://www.xvid.com/"
-  url "https://fossies.org/linux/misc/xvidcore-1.3.4.tar.gz"
-  # Official download takes a long time to fail, so set it as the mirror for now
-  mirror "http://downloads.xvid.org/downloads/xvidcore-1.3.4.tar.gz"
+  url "http://downloads.xvid.org/downloads/xvidcore-1.3.4.tar.gz"
+  mirror "https://fossies.org/linux/misc/xvidcore-1.3.4.tar.gz"
   sha256 "4e9fd62728885855bc5007fe1be58df42e5e274497591fec37249e1052ae316f"
 
   bottle do
