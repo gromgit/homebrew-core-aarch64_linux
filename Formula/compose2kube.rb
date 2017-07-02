42ad2b7156c0f8883c9d1800e410afcf8a4d4608
ilovezfs <ilovezfs@icloud.com>
2017-07-02 02:58:52 -0700

    compose2kube: remove unnecessary `go get`
    Closes #15212.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/compose2kube.rb
+++ Formula/compose2kube.rb
@@ -3,6 +3,7 @@ class Compose2kube < Formula
   homepage "https://github.com/kelseyhightower/compose2kube"
   url "https://github.com/kelseyhightower/compose2kube/archive/0.0.2.tar.gz"
   sha256 "d09b86994949f883c5aa4d041a12f6c5a8989f3755a2fb49a2abac2ad5380c30"
+  revision 1
   head "https://github.com/kelseyhightower/compose2kube.git"
 
   bottle do
@@ -17,7 +18,6 @@ class Compose2kube < Formula
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/kelseyhightower/compose2kube").install buildpath.children
     cd "src/github.com/kelseyhightower/compose2kube" do
-      system "go", "get"
       system "go", "build", "-o", bin/"compose2kube"
       prefix.install_metafiles
     end
