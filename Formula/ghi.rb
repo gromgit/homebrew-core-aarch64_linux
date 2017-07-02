68ed7d3642780bec664ece199e0db7ff9de5b1cd
ilovezfs <ilovezfs@icloud.com>
2017-07-02 03:48:42 -0700

    ghi: vendor ruby gems
    remove bottle unneeded
    
    Closes #15213.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/ghi.rb
+++ Formula/ghi.rb
@@ -3,15 +3,30 @@ class Ghi < Formula
   homepage "https://github.com/stephencelis/ghi"
   url "https://github.com/stephencelis/ghi/archive/1.2.0.tar.gz"
   sha256 "ffc17cfbdc8b88bf208f5f762e62c211bf8fc837f447354ad53cce39b1400671"
+  revision 1
   head "https://github.com/stephencelis/ghi.git"
 
-  bottle :unneeded
-
   depends_on :ruby => "1.8"
-  depends_on "pygments.rb" => :ruby
+
+  resource "multi_json" do
+    url "https://rubygems.org/gems/multi_json-1.12.1.gem"
+    sha256 "b387722b0a31fff619a2682c7011affb5a13fed2cce240c75c5d6ca3e910ecf2"
+  end
+
+  resource "pygments.rb" do
+    url "https://rubygems.org/gems/pygments.rb-1.1.2.gem"
+    sha256 "55a5deed9ecba6037ac22bf27191e0073bd460f87291b2f384922e4b0d59511c"
+  end
 
   def install
+    ENV["GEM_HOME"] = libexec
+    resources.each do |r|
+      r.verify_download_integrity(r.fetch)
+      system "gem", "install", r.cached_download, "--no-document",
+                    "--install-dir", libexec
+    end
     bin.install "ghi"
+    bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"])
     man1.install "man/ghi.1"
   end
 
