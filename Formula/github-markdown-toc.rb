8c066510beceaea8fe637cec65ce1a207452cb60
ilovezfs <ilovezfs@icloud.com>
2017-07-04 03:31:28 -0700

    github-markdown-toc 0.7.1
    use go vendor directory
    
    Closes #15273.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/github-markdown-toc.rb
+++ Formula/github-markdown-toc.rb
@@ -1,10 +1,8 @@
-require "language/go"
-
 class GithubMarkdownToc < Formula
   desc "Easy TOC creation for GitHub README.md (in go)"
   homepage "https://github.com/ekalinin/github-markdown-toc.go"
-  url "https://github.com/ekalinin/github-markdown-toc.go/archive/0.6.0.tar.gz"
-  sha256 "fe6995e9f06febca0f3a68d0df5f124726737bcfbcc027dce4aa9d5dfa1ee5ae"
+  url "https://github.com/ekalinin/github-markdown-toc.go/archive/0.7.1.tar.gz"
+  sha256 "458032ba4e74a0c5284ae476e874986e0c6c96876faf414dac181e5b9d0217bb"
 
   bottle do
     cellar :any_skip_relocation
@@ -16,29 +14,14 @@ class GithubMarkdownToc < Formula
 
   depends_on "go" => :build
 
-  go_resource "github.com/alecthomas/template" do
-    url "https://github.com/alecthomas/template.git",
-        :revision => "14fd436dd20c3cc65242a9f396b61bfc8a3926fc"
-  end
-
-  go_resource "github.com/alecthomas/units" do
-    url "https://github.com/alecthomas/units.git",
-        :revision => "2efee857e7cfd4f3d0138cc3cbb1b4966962b93a"
-  end
-
-  go_resource "gopkg.in/alecthomas/kingpin.v2" do
-    url "https://github.com/alecthomas/kingpin.git",
-        :revision => "v2.1.11"
-  end
-
   def install
     ENV["GOPATH"] = buildpath
-    mkdir_p buildpath/"src/github.com/ekalinin/"
-    ln_sf buildpath, buildpath/"src/github.com/ekalinin/github-markdown-toc.go"
-    Language::Go.stage_deps resources, buildpath/"src"
-
-    system "go", "build", "-o", "gh-md-toc", "main.go"
-    bin.install "gh-md-toc"
+    dir = buildpath/"src/github.com/ekalinin/github-markdown-toc.go"
+    dir.install buildpath.children
+    cd dir do
+      system "go", "build", "-o", bin/"gh-md-toc", "main.go"
+      prefix.install_metafiles
+    end
   end
 
   test do
