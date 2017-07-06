bd82f95e4b54275ffc17703905ed7f4b1cc5f7bd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-06 08:50:47 +0000

    heroku: add 6.12.4 bottle.

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -7,6 +7,13 @@ class Heroku < Formula
   sha256 "0320f25bf786c851805472d352cc8fa6f318ddd11ad3d65e0b1ef9110208c6cd"
   head "https://github.com/heroku/cli.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f72601084952494a4cb9ad989093f8faac15c2c4d421db4db683c8fb713d82dd" => :sierra
+    sha256 "d197cb57a817fa932bb5746de1407b09fbeab607265301794ca1af9dd1eb6ae8" => :el_capitan
+    sha256 "f2a023de07e9370aa489fc3b569d0001e3d2b1604e96f3e243050e36da68fa8c" => :yosemite
+  end
+
   depends_on "node"
 
   def install
