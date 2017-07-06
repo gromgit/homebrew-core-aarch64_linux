03caedbafa58ad9aae3656b73700fcea973bb469
ilovezfs <ilovezfs@icloud.com>
2017-07-05 21:16:44 -0700

    rbenv-bundler-ruby-version 1.0.0
    Closes #15328.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/rbenv-bundler-ruby-version.rb
+++ Formula/rbenv-bundler-ruby-version.rb
@@ -1,8 +1,8 @@
 class RbenvBundlerRubyVersion < Formula
   desc "Pick a ruby version from bundler's Gemfile"
   homepage "https://github.com/aripollak/rbenv-bundler-ruby-version"
-  url "https://github.com/aripollak/rbenv-bundler-ruby-version/archive/v0.3.1.tar.gz"
-  sha256 "dfe9d2b79591a022d0185e20a5847e62999dcb51d4b1604f4a5527b6f9b88a8e"
+  url "https://github.com/aripollak/rbenv-bundler-ruby-version/archive/v1.0.0.tar.gz"
+  sha256 "96c6b7eb191d436142fef0bb8c28071d54aca3e1a10ca01a525d1066699b03f2"
   head "https://github.com/aripollak/rbenv-bundler-ruby-version.git"
 
   bottle :unneeded
