a4039003665e4817dcd38da9e63f6bcc5c4f5a67
JCount <JCount42@gmail.com>
2017-07-05 22:21:18 -0400

    lftp: %n crash fix for 10.13 (#15306)
    Fix crash from the usage of %n in
    dynamic format strings on High Sierra

--- Formula/lftp.rb
+++ Formula/lftp.rb
@@ -15,6 +15,15 @@ class Lftp < Formula
   depends_on "openssl"
   depends_on "libidn"
 
+  # Fix crash from usage of %n in dynamic format strings on High Sierra
+  # Repurposed patch, credit to Jeremy Huddleston Sequoia <jeremyhu@apple.com>
+  if MacOS.version >= :high_sierra
+    patch :p0 do
+      url "https://raw.githubusercontent.com/macports/macports-ports/edf0ee1e2cf/devel/m4/files/secure_snprintf.patch"
+      sha256 "57f972940a10d448efbd3d5ba46e65979ae4eea93681a85e1d998060b356e0d2"
+    end
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
