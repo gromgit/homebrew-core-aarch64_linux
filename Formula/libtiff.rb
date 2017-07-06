42ab8af9049b436f011dec92ef5cb615d1f22d9f
Dominyk Tiller <dominyktiller@gmail.com>
2017-07-06 04:02:57 +0100

    libtiff: apply security fixes
    I return to Homebrew PRs, with what else but security fixes.
    
    Fixes:
    * CVE-2017-9936
    * CVE-2017-10688
    * CVE-2017-9147
    * CVE-2016-10095
    
    Closes #15320.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libtiff.rb
+++ Formula/libtiff.rb
@@ -4,6 +4,7 @@ class Libtiff < Formula
   url "http://download.osgeo.org/libtiff/tiff-4.0.8.tar.gz"
   mirror "https://fossies.org/linux/misc/tiff-4.0.8.tar.gz"
   sha256 "59d7a5a8ccd92059913f246877db95a2918e6c04fb9d43fd74e5c3390dac2910"
+  revision 1
 
   bottle do
     cellar :any
@@ -17,6 +18,21 @@ class Libtiff < Formula
 
   depends_on "jpeg"
   depends_on "xz" => :optional
+
+  # All of these have been reported upstream & should
+  # be fixed in the next release, but please check.
+  patch do
+    url "https://mirrors.ocf.berkeley.edu/debian/pool/main/t/tiff/tiff_4.0.8-3.debian.tar.xz"
+    mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/t/tiff/tiff_4.0.8-3.debian.tar.xz"
+    sha256 "8803ef2917ceb80c472e97d85e86f71a20d04cf7de94ebffcc1b3100f51058ce"
+    apply "patches/01-CVE-2015-7554.patch",
+          "patches/02-CVE.patch",
+          "patches/03-CVE.patch",
+          "patches/04-CVE-2016-10095_CVE-2017-9147.patch",
+          "patches/05-CVE-2017-9936.patch",
+          "patches/06-OOM_in_gtTileContig.patch",
+          "patches/07-CVE-2017-10688.patch"
+  end
 
   def install
     ENV.cxx11 if build.cxx11?
