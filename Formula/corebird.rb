d7c93923d14b7b106af4ae12f1cfc674e14f96a1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-08 20:24:01 +0000

    corebird: update 1.5.1 bottle.

--- Formula/corebird.rb
+++ Formula/corebird.rb
@@ -5,9 +5,9 @@ class Corebird < Formula
   sha256 "e7b641a25e279d8251ca0a9fa2ef11a5dd364e369e1fa54f5545824dc96deda0"
 
   bottle do
-    sha256 "1d1a8de6fdf0bb1eed7bfa24398e43247ee1f06b007c33fe79815fb2082ff7f7" => :sierra
-    sha256 "2e1d1a7cd09f385805d0672e9a1b7e211c4778b0b711e5b3095418442c36ac13" => :el_capitan
-    sha256 "b20316807a544dee5cce001624260d3a0faa14dcba857704c0aa8842d203f5a3" => :yosemite
+    sha256 "787c77b73b7c34bc0b59ba74699483e65cf9b9a2f62f865fcf06406f23218821" => :sierra
+    sha256 "17fff95b88b23ea7f2dec025b53692e344335ece07ab11982806e458d23c669d" => :el_capitan
+    sha256 "1cfffdf6dba702e9d40933bb5058def94e9f2d0e6886e93488c6e4a47db6e093" => :yosemite
   end
 
   depends_on "pkg-config" => :build
