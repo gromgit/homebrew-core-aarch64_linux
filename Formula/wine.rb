041b7960f19be4748b4a8f43146c56bca6960303
uyjulian <uyjulian@gmail.com>
2017-07-08 00:28:50 -0500

    wine 2.12 (devel) (#15398)

--- Formula/wine.rb
+++ Formula/wine.rb
@@ -35,9 +35,9 @@ class Wine < Formula
   end
 
   devel do
-    url "https://dl.winehq.org/wine/source/2.x/wine-2.11.tar.xz"
-    mirror "https://downloads.sourceforge.net/project/wine/Source/wine-2.11.tar.xz"
-    sha256 "efa96dade206aa21accf0da195335a805d38209d6a14320607c6308ebde4cc3c"
+    url "https://dl.winehq.org/wine/source/2.x/wine-2.12.tar.xz"
+    mirror "https://downloads.sourceforge.net/project/wine/Source/wine-2.12.tar.xz"
+    sha256 "496c11af841487e6ec4e1953ac19b05a7f48e397da502653ffbd9fe9004213a7"
 
     # Patch to fix screen-flickering issues. Still relevant on 2.3.
     # https://bugs.winehq.org/show_bug.cgi?id=34166
