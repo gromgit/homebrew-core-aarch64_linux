2d1914e78e47697caf1aa0b2cf2127e7ac5c7d43
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-08 18:36:44 +0000

    tectonic: add 0.1.6 bottle.

--- Formula/tectonic.rb
+++ Formula/tectonic.rb
@@ -4,6 +4,12 @@ class Tectonic < Formula
   url "https://github.com/tectonic-typesetting/tectonic/archive/v0.1.6.tar.gz"
   sha256 "5c201c979069dfb780040b42d8c516847668bd6d429eabb20f5fb15a3873832c"
 
+  bottle do
+    sha256 "bfb4b5cc790c8aa94be88b05776ca8944725d0bb0efaffb6b908ac2a57c546bf" => :sierra
+    sha256 "911f1157cd3a5a858f4338f0145be38da0025dfb4a008d138f9307498356decb" => :el_capitan
+    sha256 "e53f3203f455be568b7d8cdb51b2008d87d2b5c8bcee55c4784157288242b479" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
   depends_on "freetype"
