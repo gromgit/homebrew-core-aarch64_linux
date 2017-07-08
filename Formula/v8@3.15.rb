ddfe5c7487fa070d0702d5c254bdacbb65ec0c79
Jeroen <jeroenooms@gmail.com>
2017-07-08 09:24:05 +0200

    v8@3.15: build with c++11
    Fixes #14230.
    
    Closes #14271.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/v8@3.15.rb
+++ Formula/v8@3.15.rb
@@ -3,6 +3,7 @@ class V8AT315 < Formula
   homepage "https://github.com/v8/v8/wiki"
   url "https://github.com/v8/v8-git-mirror/archive/3.15.11.18.tar.gz"
   sha256 "93a4945a550e5718d474113d9769a3c010ba21e3764df8f22932903cd106314d"
+  revision 1
 
   bottle do
     cellar :any
@@ -19,6 +20,9 @@ class V8AT315 < Formula
   end
 
   def install
+    # Bully GYP into correctly linking with c++11
+    ENV.cxx11
+    ENV["GYP_DEFINES"] = "clang=1 mac_deployment_target=#{MacOS.version}"
     (buildpath/"build/gyp").install resource("gyp")
 
     # fix up libv8.dylib install_name
