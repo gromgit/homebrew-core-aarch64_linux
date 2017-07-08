283b093e314eaccffa15e6464a1e571255d8476c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-08 10:24:57 +0000

    git-cinnabar: add 0.4.0 bottle.

--- Formula/git-cinnabar.rb
+++ Formula/git-cinnabar.rb
@@ -6,6 +6,13 @@ class GitCinnabar < Formula
       :revision => "6d374888ff0287517084c0ec7573963961f6acec"
   head "https://github.com/glandium/git-cinnabar.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "03f64128f2ec4fb5f0597157c90db69e273cfa1ed521aa928c700b9de1f176d4" => :sierra
+    sha256 "5942ec7cbd204c8e6e691b48c38333f0e7cea11cd2a80714fe3952a1f14507e9" => :el_capitan
+    sha256 "3f5f4a461ab56361b323f2a934704652011c8a4ada7da719c957005ced268f67" => :yosemite
+  end
+
   devel do
     url "https://github.com/glandium/git-cinnabar.git",
         :tag => "0.5.0b1",
