42cf449383bfefd87e220bfb9d7b92e103e80e30
ilovezfs <ilovezfs@icloud.com>
2017-07-09 00:55:48 -0700

    ansible-cmdb 1.22 (#15417)
    set bottle unneeded

--- Formula/ansible-cmdb.rb
+++ Formula/ansible-cmdb.rb
@@ -1,28 +1,19 @@
 class AnsibleCmdb < Formula
   desc "Generates static HTML overview page from Ansible facts"
   homepage "https://github.com/fboender/ansible-cmdb"
-  url "https://github.com/fboender/ansible-cmdb/releases/download/1.21/ansible-cmdb-1.21.zip"
-  sha256 "ee7b35803a25c69c8a3d1dd644b701da40567f3d3f2fbc5a782b407308325f1d"
+  url "https://github.com/fboender/ansible-cmdb/releases/download/1.22/ansible-cmdb-1.22.zip"
+  sha256 "c012d46a0f782d7443c6b230194d5afde1e3f72915869a43b119304800c916fc"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "20c2907fa46c4e89ae89eb1d63a4cb29a9d4f1cbfc40d7fdd8bc8c147a6ff1c0" => :sierra
-    sha256 "20c2907fa46c4e89ae89eb1d63a4cb29a9d4f1cbfc40d7fdd8bc8c147a6ff1c0" => :el_capitan
-    sha256 "20c2907fa46c4e89ae89eb1d63a4cb29a9d4f1cbfc40d7fdd8bc8c147a6ff1c0" => :yosemite
-  end
+  bottle :unneeded
 
   depends_on :python if MacOS.version <= :snow_leopard
   depends_on "libyaml"
 
   def install
-    bin.mkpath
-    man1.mkpath
-    inreplace "Makefile" do |s|
-      s.gsub! "/usr/local/lib/${PROG}", prefix
-      s.gsub! "/usr/local/bin", bin
-      s.gsub! "/usr/local/share/man/man1", man1
-    end
-    system "make", "install"
+    prefix.install_metafiles
+    man1.install "ansible-cmdb.man.1" => "ansible-cmdb.1"
+    libexec.install Dir["*"] - ["Makefile"]
+    bin.write_exec_script libexec/"ansible-cmdb"
   end
 
   test do
