1bc3ba5034f85d664d48e06a6c6b7315450dc86c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-09 12:26:04 +0000

    packetq: add 1.3.1 bottle.

--- Formula/packetq.rb
+++ Formula/packetq.rb
@@ -4,6 +4,13 @@ class Packetq < Formula
   url "https://www.dns-oarc.net/files/packetq/packetq-1.3.1.tar.gz"
   sha256 "b787ad2d2446f43fd494ef82a587cb79e6e69a85bbbf131c9c64258f0359acff"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "cc4d3d447c8d64fe3ae02bbe1307ac5fc14080c197d35510c65970647f72a558" => :sierra
+    sha256 "3f2501e1534913dea6d0c5928ffd0ca6d0a60771fca4ab104729e7523882566c" => :el_capitan
+    sha256 "319784b955102e90e100a62aa2d2891e1ee9fc4c3fc1112596840182f6b4a3f1" => :yosemite
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
