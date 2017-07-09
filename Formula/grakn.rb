c6a6d76cd5acdeb77458f6b7f1eb5d7e72282e2b
ilovezfs <ilovezfs@icloud.com>
2017-07-09 01:01:21 -0700

    grakn 0.14.0 (#15426)

--- Formula/grakn.rb
+++ Formula/grakn.rb
@@ -1,8 +1,8 @@
 class Grakn < Formula
   desc "The Database for AI"
   homepage "https://grakn.ai"
-  url "https://github.com/graknlabs/grakn/releases/download/v0.13.0/grakn-dist-0.13.0.tar.gz"
-  sha256 "bcc6b92effb154665dab8f01748fafe099a8f580db783f26c960a5396e901e8e"
+  url "https://github.com/graknlabs/grakn/releases/download/v0.14.0/grakn-dist-0.14.0.tar.gz"
+  sha256 "97b1325d9aa994013b7678e414d0dfee5f0ea165d7ab33867994422e03d1f516"
 
   bottle :unneeded
 
