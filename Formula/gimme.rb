775a2c347553284d6d35a5db6b56105d4b195e0a
ilovezfs <ilovezfs@icloud.com>
2017-07-09 11:28:40 -0700

    gimme 1.2.0
    Closes #15446.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/gimme.rb
+++ Formula/gimme.rb
@@ -1,8 +1,8 @@
 class Gimme < Formula
   desc "Shell script to install any Go version"
   homepage "https://github.com/travis-ci/gimme"
-  url "https://github.com/travis-ci/gimme/archive/v1.1.0.tar.gz"
-  sha256 "9f6016a00198b66fc9ea529d0cb768acef2fe9db4b18675c613aa440161612a1"
+  url "https://github.com/travis-ci/gimme/archive/v1.2.0.tar.gz"
+  sha256 "ae4fd0b5fb7c53e08c3e803b0e81e03d6b1aa3bd982625df8f1e3ef79c5fe760"
 
   bottle :unneeded
 
