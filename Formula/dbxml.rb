ec69b977d2c582f8ff739853f8f23fc7fd0d4410
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-10 13:48:22 +0000

    dbxml: update 6.1.4 bottle.

--- Formula/dbxml.rb
+++ Formula/dbxml.rb
@@ -5,9 +5,10 @@ class Dbxml < Formula
   sha256 "a8fc8f5e0c3b6e42741fa4dfc3b878c982ff8f5e5f14843f6a7e20d22e64251a"
 
   bottle do
-    sha256 "a471d0f7df2e846a3eee974b29d33e48185e45ec49791552f034d0b4c7f28fdd" => :sierra
-    sha256 "bf89aad9e0c5341a69f45041044f4a87f73c469973c4c039f105c5af2d30c9fd" => :el_capitan
-    sha256 "8fe99043112262ad2575e3624b2755147a474d7abce7369624b2746932a212af" => :yosemite
+    rebuild 1
+    sha256 "669e6aadc43f46b060eb2acadd214dae28b5b6727d46e18c4e302d81665f2170" => :sierra
+    sha256 "402542ead71ef0b095bca06052270709a1302dca5ae4d2202c23e6457e83fd77" => :el_capitan
+    sha256 "a055bb860d3059aebe23029e14a4fe58fb6ad1a586864f1c74dc51970cfdc432" => :yosemite
   end
 
   depends_on "xerces-c"
