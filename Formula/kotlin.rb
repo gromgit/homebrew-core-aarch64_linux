f62fb5bbcd500c6bb1f3fd9f12464d4f572cbfbb
equal-l2 <eng.equall2@gmail.com>
2017-07-10 16:35:37 +0900

    kotlin 1.1.3-2
    Closes #15464.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/kotlin.rb
+++ Formula/kotlin.rb
@@ -1,8 +1,8 @@
 class Kotlin < Formula
   desc "Statically typed programming language for the JVM"
   homepage "https://kotlinlang.org/"
-  url "https://github.com/JetBrains/kotlin/releases/download/v1.1.3/kotlin-compiler-1.1.3.zip"
-  sha256 "bf5e6d8f1fa93a1fc27c56de3256cc94f26de483e8dd7fefc1b07758e2171dc9"
+  url "https://github.com/JetBrains/kotlin/releases/download/v1.1.3-2/kotlin-compiler-1.1.3-2.zip"
+  sha256 "42abf39538e0ea5960b98e314191b40570090789431e2eac17dd54ebd8b74406"
 
   bottle :unneeded
 
