8e9a82e41fbfbf6586131fa3088da5b66276b174
ilovezfs <ilovezfs@icloud.com>
2017-07-09 21:05:11 -0700

    format-udf 1.5.1
    Closes #15452.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- Formula/format-udf.rb
+++ Formula/format-udf.rb
@@ -1,8 +1,8 @@
 class FormatUdf < Formula
   desc "Bash script to format a block device to UDF"
   homepage "https://github.com/JElchison/format-udf"
-  url "https://github.com/JElchison/format-udf/archive/1.5.0.tar.gz"
-  sha256 "b6dc304582fa85698d3528beb11ce2633cfe4fe2d43ce3fb3ff7e331934e8d6a"
+  url "https://github.com/JElchison/format-udf/archive/1.5.1.tar.gz"
+  sha256 "58644fc128e52c6f886c8852193aef1ee58bfef3a59a1646433c4bfcec0400d8"
 
   bottle :unneeded
 
