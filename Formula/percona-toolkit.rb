2588ac13fae641800620aff66e8306794289b404
ilovezfs <ilovezfs@icloud.com>
2017-07-10 04:48:24 -0700

    percona-toolkit 3.0.3
    re-enable bottle
    
    Closes #15478.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/percona-toolkit.rb
+++ Formula/percona-toolkit.rb
@@ -1,25 +1,23 @@
 class PerconaToolkit < Formula
   desc "Percona Toolkit for MySQL"
   homepage "https://www.percona.com/software/percona-toolkit/"
-  url "https://www.percona.com/downloads/percona-toolkit/3.0.2/source/tarball/percona-toolkit-3.0.2.tar.gz"
-  sha256 "ace5325424af6f115b4b31c5a62b0ff48b24aed43b3991f479ab18dfc1c1572d"
+  url "https://www.percona.com/downloads/percona-toolkit/3.0.3/source/tarball/percona-toolkit-3.0.3.tar.gz"
+  sha256 "f1da1d6c3ad04875806f55ad2aa0706a10bc7f22fb55307ee97c169296276e0e"
   head "lp:percona-toolkit", :using => :bzr
-
-  bottle :disable, "To use the user's database of choice."
 
   depends_on :mysql
   depends_on "openssl"
 
   resource "DBD::mysql" do
-    url "https://cpan.metacpan.org/authors/id/M/MI/MICHIELB/DBD-mysql-4.041.tar.gz"
-    mirror "http://search.cpan.org/CPAN/authors/id/M/MI/MICHIELB/DBD-mysql-4.041.tar.gz"
-    sha256 "4777de11c464b515db9da95c08c225900d0594b65ba3256982dc21f9f9379040"
+    url "https://cpan.metacpan.org/authors/id/M/MI/MICHIELB/DBD-mysql-4.043.tar.gz"
+    mirror "http://search.cpan.org/CPAN/authors/id/M/MI/MICHIELB/DBD-mysql-4.043.tar.gz"
+    sha256 "629f865e8317f52602b2f2efd2b688002903d2e4bbcba5427cb6188b043d6f99"
   end
 
   resource "JSON" do
-    url "https://cpan.metacpan.org/authors/id/M/MA/MAKAMAKA/JSON-2.90.tar.gz"
-    mirror "http://search.cpan.org/CPAN/authors/id/M/MA/MAKAMAKA/JSON-2.90.tar.gz"
-    sha256 "4ddbb3cb985a79f69a34e7c26cde1c81120d03487e87366f9a119f90f7bdfe88"
+    url "https://cpan.metacpan.org/authors/id/I/IS/ISHIGAKI/JSON-2.94.tar.gz"
+    mirror "http://search.cpan.org/CPAN/authors/id/I/IS/ISHIGAKI/JSON-2.94.tar.gz"
+    sha256 "12271b5cee49943bbdde430eef58f1fe64ba6561980b22c69585e08fc977dc6d"
   end
 
   def install
