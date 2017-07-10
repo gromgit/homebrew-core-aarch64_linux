873278fcbc2d9bd35050f8c2546a4e5a6819d482
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-10 05:24:00 +0000

    faac: update 1.29 bottle.

--- Formula/faac.rb
+++ Formula/faac.rb
@@ -6,12 +6,9 @@ class Faac < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "1537586f1bb69e142a32886ba9ac6e8e244dc297d8be441573e46eacd57e7075" => :sierra
-    sha256 "4607ea16f33aec6dabefe6ace66c41cb7ee487b03fff8c7b2d050cbc6340422f" => :el_capitan
-    sha256 "c3fad36cecb8f5d4fc17ad5912d90ee9dffafeb2c5c60b21d27d9ea8bfeb351d" => :yosemite
-    sha256 "1b97bdfb9334decc5d51c9d57788fa23ccf77c56d74a53fcfe6f21a4495ba463" => :mavericks
-    sha256 "e2cf2e63defd76653bc96443956f28bb9e0388a76cda5d0c8c463528d68a191a" => :mountain_lion
+    sha256 "ec05352ba412fb64cda82e4a964b9a0e6e625481a3d5b13a66db8be9d39d29f6" => :sierra
+    sha256 "eb799e6822088371803fb833a44099cb1ab54ab1b775da3c858b85db963be411" => :el_capitan
+    sha256 "1d3c4c9b4848d88a29d16702ab1d3e9cca5d2e801cf99886d9b13b38510f09ef" => :yosemite
   end
 
   depends_on "autoconf" => :build
