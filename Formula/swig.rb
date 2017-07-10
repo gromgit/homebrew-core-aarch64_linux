a0eabe7b1f9add972bc7122949e51778ad8efeca
ilovezfs <ilovezfs@icloud.com>
2017-07-10 01:21:28 -0700

    swig: use full path to ruby in the test
    The test would fail if Ruby 2.4 happened to be installed, since the test
    hard codes -I/System/Library/Frameworks/Ruby.framework/Headers.

--- Formula/swig.rb
+++ Formula/swig.rb
@@ -40,6 +40,6 @@ class Swig < Formula
     system ENV.cc, "-c", "test.c"
     system ENV.cc, "-c", "test_wrap.c", "-I/System/Library/Frameworks/Ruby.framework/Headers/"
     system ENV.cc, "-bundle", "-undefined", "dynamic_lookup", "test.o", "test_wrap.o", "-o", "test.bundle"
-    assert_equal "2", shell_output("ruby run.rb").strip
+    assert_equal "2", shell_output("/usr/bin/ruby run.rb").strip
   end
 end
