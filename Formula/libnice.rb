957d30f794e87a0be7d46779454bac34fd3c8966
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-10 20:40:25 +0000

    libnice: add 0.1.14 bottle.

--- Formula/libnice.rb
+++ Formula/libnice.rb
@@ -4,6 +4,13 @@ class Libnice < Formula
   url "https://nice.freedesktop.org/releases/libnice-0.1.14.tar.gz"
   sha256 "be120ba95d4490436f0da077ffa8f767bf727b82decf2bf499e39becc027809c"
 
+  bottle do
+    cellar :any
+    sha256 "3782f1868a247063e772f0ac5b9f59524ed6c0ad5a72e1d96af7078e5a36f526" => :sierra
+    sha256 "f9247e1697faac654fa25fc461f080486731d8fbcffc7855c46ab9c716fa62fc" => :el_capitan
+    sha256 "2cbf1077ed2e87caf285188031d47710bedc3bfb801f1f0a87ca0fbb081c8e30" => :yosemite
+  end
+
   depends_on "pkg-config" => :build
   depends_on "glib"
   depends_on "gnutls"
