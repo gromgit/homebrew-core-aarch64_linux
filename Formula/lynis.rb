85861236826c342e85491dd933898ed97bfe84f0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-11 03:26:52 +0000

    lynis: update 2.5.2 bottle.

--- Formula/lynis.rb
+++ Formula/lynis.rb
@@ -6,9 +6,9 @@ class Lynis < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5d5d30e7f31bdc107d9f550789dc1bcd4b666fe680138c20abfbae7c432eab9f" => :sierra
-    sha256 "bcb0f8aadd3d6bd0abf0b95a66212ed00207bed64e01674158f95555099b651d" => :el_capitan
-    sha256 "bcb0f8aadd3d6bd0abf0b95a66212ed00207bed64e01674158f95555099b651d" => :yosemite
+    sha256 "191aaa47304a649d973607526ed626d5018ce23a3ff40a57a7cd752f58788b3f" => :sierra
+    sha256 "2090c33d07ed3a1c86e66420f3a1cd6ef9fce5173d9809bf8144080fabc802f6" => :el_capitan
+    sha256 "2090c33d07ed3a1c86e66420f3a1cd6ef9fce5173d9809bf8144080fabc802f6" => :yosemite
   end
 
   def install
