7306738df2385cd584a7fd9bc48167086fcd6be0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-11 11:36:24 +0000

    mpg123: update 1.25.2 bottle.

--- Formula/mpg123.rb
+++ Formula/mpg123.rb
@@ -6,9 +6,9 @@ class Mpg123 < Formula
   sha256 "5314b0fb8ad291bfc79ff4c5c321b971916819a65233ec065434358fcf8aee38"
 
   bottle do
-    sha256 "3bd78d5a840d679e8685a9ddbf85364e193b92b62d6f98b8cec31bc8baab686b" => :sierra
-    sha256 "23ff6e524f6f1f5f401d5c6a407c811b93d69be5d8ccdc1188bdd1a404be6e02" => :el_capitan
-    sha256 "cabb97e3808e12c440e3ab5c399338b7fce1e20fd84063270e6e2322273e72de" => :yosemite
+    sha256 "47d2df36aab1833709ecc013d4a70adcb13a8a26727cb2a12f634e1256ec4d53" => :sierra
+    sha256 "6ba0a4eb846bb874aade35a862e7c2191d0ddd5b7e973f4fca0c752d7bc8db35" => :el_capitan
+    sha256 "4eb741a9bcde436c0741b982a26a46f4ad142ad868f4e9d7b2b61a2d4a8f2145" => :yosemite
   end
 
   def install
