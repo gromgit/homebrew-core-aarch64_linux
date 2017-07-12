bdf68bd79ebd16a70b7a747e027afbe5831f9cc3
ilovezfs <ilovezfs@icloud.com>
2017-07-11 22:19:12 -0700

    apache-spark 2.2.0 (#15507)

--- Formula/apache-spark.rb
+++ Formula/apache-spark.rb
@@ -1,9 +1,9 @@
 class ApacheSpark < Formula
   desc "Engine for large-scale data processing"
   homepage "https://spark.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=spark/spark-2.1.1/spark-2.1.1-bin-hadoop2.7.tgz"
-  version "2.1.1"
-  sha256 "372ac4f73221c07696793101007a4f19e31566d1f0d9bd0e5205b6fb5b45bfc2"
+  url "https://www.apache.org/dyn/closer.lua?path=spark/spark-2.2.0/spark-2.2.0-bin-hadoop2.7.tgz"
+  version "2.2.0"
+  sha256 "97fd2cc58e08975d9c4e4ffa8d7f8012c0ac2792bcd9945ce2a561cf937aebcc"
   head "https://github.com/apache/spark.git"
 
   bottle :unneeded
