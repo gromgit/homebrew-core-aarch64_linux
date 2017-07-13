31e602fea943eaaa842728dc181bda90ba6c9db6
ilovezfs <ilovezfs@icloud.com>
2017-07-12 21:07:51 -0700

    docker-machine 0.12.2
    Closes #15572.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/docker-machine.rb
+++ Formula/docker-machine.rb
@@ -2,8 +2,8 @@ class DockerMachine < Formula
   desc "Create Docker hosts locally and on cloud providers"
   homepage "https://docs.docker.com/machine"
   url "https://github.com/docker/machine.git",
-      :tag => "v0.12.1",
-      :revision => "c8b17e86de70f02fb9e7a9743089395000625b50"
+      :tag => "v0.12.2",
+      :revision => "937160559e02cc430fe176991beb9a6ffbfe3de0"
   head "https://github.com/docker/machine.git"
 
   bottle do
