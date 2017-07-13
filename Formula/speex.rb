22f0236fc4ef3cbffa6248d50da8d54e4ad83cf1
Viktor Szakats <vszakats@users.noreply.github.com>
2017-07-13 16:37:41 +0200

    speex: secure url(s)

--- Formula/speex.rb
+++ Formula/speex.rb
@@ -1,7 +1,7 @@
 class Speex < Formula
   desc "Audio codec designed for speech"
-  homepage "https://speex.org"
-  url "http://downloads.xiph.org/releases/speex/speex-1.2.0.tar.gz"
+  homepage "https://speex.org/"
+  url "https://downloads.xiph.org/releases/speex/speex-1.2.0.tar.gz"
   sha256 "eaae8af0ac742dc7d542c9439ac72f1f385ce838392dc849cae4536af9210094"
 
   bottle do
