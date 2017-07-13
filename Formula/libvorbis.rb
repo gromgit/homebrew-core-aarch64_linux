bf0423ded58e15218a0b77039e7a10ef42afe8b4
Viktor Szakats <vszakats@users.noreply.github.com>
2017-07-13 16:32:40 +0200

    libvorbis: secure url(s)

--- Formula/libvorbis.rb
+++ Formula/libvorbis.rb
@@ -1,7 +1,7 @@
 class Libvorbis < Formula
   desc "Vorbis General Audio Compression Codec"
   homepage "http://vorbis.com/"
-  url "http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.xz"
+  url "https://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.xz"
   sha256 "54f94a9527ff0a88477be0a71c0bab09a4c3febe0ed878b24824906cd4b0e1d1"
   revision 1
 
