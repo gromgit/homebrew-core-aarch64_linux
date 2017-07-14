3fe7d47fe9dfc62291e1560c1a14f7c7063cd480
ilovezfs <ilovezfs@icloud.com>
2017-07-14 11:10:04 -0700

    diff-so-fancy 1.1.0 (#15629)
    set bottle unneeded
    remove node build-time dependency

--- Formula/diff-so-fancy.rb
+++ Formula/diff-so-fancy.rb
@@ -1,23 +1,14 @@
-require "language/node"
-
 class DiffSoFancy < Formula
   desc "Good-lookin' diffs with diff-highlight and more"
   homepage "https://github.com/so-fancy/diff-so-fancy"
-  url "https://registry.npmjs.org/diff-so-fancy/-/diff-so-fancy-1.0.0.tgz"
-  sha256 "13d8d6f8dd3b3100ab48de04df593daf137e21c28d1e10082b75f723f9b182b9"
+  url "https://github.com/so-fancy/diff-so-fancy/archive/v1.1.0.tar.gz"
+  sha256 "4313659fc5bd3d076f3725e9f412d8f84312661c772c7429321896e7d09c61ac"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "538a3a546b2fc6091a455cca78f6349c1efffef85646d336bda25dd857abc625" => :sierra
-    sha256 "6d6abeec030b9d3ddf9bb9d65a731ad4a6495707fb8be5859a8f3f788227d536" => :el_capitan
-    sha256 "e4466cd5508fddcda748ceed7e01f0be45e10499ce16f92c0d4928c8028d5708" => :yosemite
-  end
-
-  depends_on "node" => :build
+  bottle :unneeded
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir[libexec/"bin/*"]
+    libexec.install "diff-so-fancy", "lib"
+    bin.install_symlink libexec/"diff-so-fancy"
   end
 
   test do
