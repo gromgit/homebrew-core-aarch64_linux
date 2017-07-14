86c7fb254fe0ebc200556e02445189dc6a541e5f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-14 05:04:15 +0000

    pdf2json: update 0.69 bottle.

--- Formula/pdf2json.rb
+++ Formula/pdf2json.rb
@@ -5,12 +5,9 @@ class Pdf2json < Formula
   sha256 "69394ef5d5d5504f7106e8b55e15bf491c48d906d611e6bc2e5952005a85b593"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "9a692a971b1e57a0948a1dc0664043d982df1b70cef8a8f39a70f84359bedffd" => :sierra
-    sha256 "01a63a2a56b0209884b0edf4d4857cbea4316f2a475dc17a8e390b36536b5d0a" => :el_capitan
-    sha256 "c82fefb4779c474d6a6b5eaa0e6fd6a9ccf7bd7e3962b5943367e15e6cbbdea1" => :yosemite
-    sha256 "79fb8c9376d0dcc0ec6d9100912f533d9b7301ab9c6a2b90d2564701eaacaa4a" => :mavericks
-    sha256 "6796505fd330e4806d40d07804532eb97b9d4cdb8c0a1d270727b9181b4b7ab9" => :mountain_lion
+    sha256 "586c26331ad9becac719c803c71be519ea3684c28ab80db457e8f61df9485234" => :sierra
+    sha256 "92852d5246f34ed87340f347f8645c60e39f7cbd924c2e94cf199f4f9d42ddc6" => :el_capitan
+    sha256 "3c10495304bdf5d1c99127219e79d3693bfd8141c861596e791935472c59246c" => :yosemite
   end
 
   def install
