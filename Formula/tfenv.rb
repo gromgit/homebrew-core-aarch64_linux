0c830b9e24d485afdbc5b3494bcc8e04179713b1
ilovezfs <ilovezfs@icloud.com>
2017-07-15 02:30:04 -0700

    tfenv 0.4.4 (#15642)

--- Formula/tfenv.rb
+++ Formula/tfenv.rb
@@ -1,8 +1,8 @@
 class Tfenv < Formula
   desc "Terraform version manager inspired by rbenv"
   homepage "https://github.com/kamatama41/tfenv"
-  url "https://github.com/kamatama41/tfenv/archive/v0.4.3.tar.gz"
-  sha256 "c02e53a46400f54b62e2fcb46eb08510fea82f4079772912a310cec6a80de6ed"
+  url "https://github.com/kamatama41/tfenv/archive/v0.4.4.tar.gz"
+  sha256 "decbd1e871d3da5e7b1299ab3d36852dbb94e3932bc3b5fad047c0488e4eac1b"
   head "https://github.com/kamatama41/tfenv.git"
 
   bottle :unneeded
