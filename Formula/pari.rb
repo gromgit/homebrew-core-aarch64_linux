05bcb9567bb3d49f3297e028777e0912d080e552
ilovezfs <ilovezfs@icloud.com>
2017-07-15 01:42:41 -0700

    pari 2.9.3
    Closes #15650.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pari.rb
+++ Formula/pari.rb
@@ -1,9 +1,9 @@
 class Pari < Formula
   desc "Computer algebra system designed for fast computations in number theory"
   homepage "https://pari.math.u-bordeaux.fr/"
-  url "https://pari.math.u-bordeaux.fr/pub/pari/unix/pari-2.9.2.tar.gz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/p/pari/pari_2.9.2.orig.tar.gz"
-  sha256 "9aa24cbbcf4e0b09dcc21cf9b09f2eb08e38ee16ab13651be7274c9b3e46207e"
+  url "https://pari.math.u-bordeaux.fr/pub/pari/unix/pari-2.9.3.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/p/pari/pari_2.9.3.orig.tar.gz"
+  sha256 "e76a27779d2b1210ce1aba48363b98dd201a1bf876eb14f46ea6bd7769a00a63"
 
   bottle do
     sha256 "706967276bb148178350324b1e9f625cb23fdb8e2a7fd75a152e6732227fd0e9" => :sierra
