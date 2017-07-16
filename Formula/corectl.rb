19f9b59c64aaf291eb21980d2869df51fa5c19ca
ilovezfs <ilovezfs@icloud.com>
2017-07-16 11:59:09 -0700

    corectl: build with OCaml 4.04.2 from OPAM
    and pin mirage-block-unix>2.3.0 and lwt<3.1.0
    
    Closes #15681.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/corectl.rb
+++ Formula/corectl.rb
@@ -3,6 +3,7 @@ class Corectl < Formula
   homepage "https://github.com/TheNewNormal/corectl"
   url "https://github.com/TheNewNormal/corectl/archive/v0.7.18.tar.gz"
   sha256 "9bdf7bc8c6a7bd861e2b723c0566d0a093ed5d5caf370a065a1708132b4ab98a"
+  revision 1
   head "https://github.com/TheNewNormal/corectl.git", :branch => "golang"
 
   bottle do
@@ -36,9 +37,18 @@ class Corectl < Formula
 
     cd path do
       system "opam", "init", "--no-setup"
-      system "opam", "install", "uri", "ocamlfind", "qcow-format", "conf-libev", "io-page<2"
 
-      system "make", "tarball", *args
+      # Upstream issue "OCaml 4.05.0 support - cannot build in Homebrew"
+      # Reported 23 Jul 2017 https://github.com/TheNewNormal/corectl/issues/119
+      inreplace "opamroot/compilers/4.04.2/4.04.2/4.04.2.comp",
+        '["./configure"', '["./configure" "-no-graph"'
+      system "opam", "switch", "4.04.2"
+
+      system "opam", "config", "exec", "--", "opam", "install", "uri",
+             "ocamlfind", "qcow-format", "conf-libev", "io-page<2",
+             "mirage-block-unix>2.3.0", "lwt<3.1.0"
+      (opamroot/"system/bin").install_symlink opamroot/"4.04.2/bin/qcow-tool"
+      system "opam", "config", "exec", "--", "make", "tarball", *args
 
       bin.install Dir["bin/*"]
 
