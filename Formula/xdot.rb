ad435f3a104a491ca910b8c7e94620b38ee55fce
Mike McQuaid <mike@mikemcquaid.com>
2017-07-16 17:44:57 +0100

    xdot: revision for new python location.

--- Formula/xdot.rb
+++ Formula/xdot.rb
@@ -3,6 +3,7 @@ class Xdot < Formula
   homepage "https://github.com/jrfonseca/xdot.py"
   url "https://files.pythonhosted.org/packages/f5/52/7cec1decf2b07c7749eb997fa5f365781a512722f48e6ad4294e31c94629/xdot-0.7.tar.gz"
   sha256 "d2100c3201d974915d1b89220ce52f380334eb365ab48903573a8135f51d0ee0"
+  revision 1
 
   head "https://github.com/jrfonseca/xdot.py.git"
 
