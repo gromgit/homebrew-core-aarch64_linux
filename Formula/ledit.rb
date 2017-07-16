2be8f592aa57c4d665367973dbe1ba425e79b72f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-16 18:35:39 +0000

    ledit: update 2.03_2 bottle.

--- Formula/ledit.rb
+++ Formula/ledit.rb
@@ -7,9 +7,9 @@ class Ledit < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "19a3c01773ed5cfa28d3b3cf144d3c21f26f5e716bfa0b446115c11fad4ab3ba" => :sierra
-    sha256 "d863f55875afb7efd6ccce5ace4c5722c1c62fe50c474231a3f406630aeb5cad" => :el_capitan
-    sha256 "5ba23ccff679120de626f8287bae7bfd6d5d0ffc2b97b1e0f559d62f1cf05c8e" => :yosemite
+    sha256 "4a26786028c08c41b493f9e05dcd8e1cad1be78607a1dc243f23162632bbaa2a" => :sierra
+    sha256 "4a3c0a8da1f765c3490c2fceac34ca093927c94d5ae03e5701654b83fa16d4fc" => :el_capitan
+    sha256 "9d16c9b4d75e5d0d1caa9823f282899a0034bada1a46d7809a9dd37d5759563a" => :yosemite
   end
 
   depends_on "ocaml"
