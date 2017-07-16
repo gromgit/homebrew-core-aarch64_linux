e78d9e8512bdc5b42c9ab043c05879cbf2280f3d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-16 18:35:41 +0000

    unison@2.40: update 2.40.128_1 bottle.

--- Formula/unison@2.40.rb
+++ Formula/unison@2.40.rb
@@ -7,9 +7,8 @@ class UnisonAT240 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "76fa8dc81fbf5ab4fe19dbd96bc221095c0eb67550d68a6f74c9670c3abeaf29" => :sierra
-    sha256 "6046281f4e119843646b8d2312f0b25824b4ec7c6c7857e35beb5a90b03dfcac" => :el_capitan
-    sha256 "20893d19f048adccfd232a9bec3ccd96127cc6949f6cc0b4734397160641be81" => :yosemite
+    sha256 "d576e0581b5117b374127c0f6af6e402a4f3ebc7dcdf413c4352ed8695b78a11" => :sierra
+    sha256 "a94cfdeb1b501a8b85a72fe1790edd339d3f65c86898a5d8729f596af87a66b7" => :el_capitan
   end
 
   keg_only :versioned_formula
