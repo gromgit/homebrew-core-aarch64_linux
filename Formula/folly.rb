ac792a042c3ee084288290306ac7d6ad8de89426
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-20 04:21:08 +0000

    folly: update 2017.07.17.01 bottle.

--- Formula/folly.rb
+++ Formula/folly.rb
@@ -7,8 +7,8 @@ class Folly < Formula
 
   bottle do
     cellar :any
-    sha256 "b7bdbc79ad10085e9810ba63f3fe32ffe6c304ba3f810a85117a159c1292b75d" => :sierra
-    sha256 "1769d310c5f8f83414acef8a7e914d6d1fbe83acb6947faf84b1e30adcbe579d" => :el_capitan
+    sha256 "d8fdf9cd0de011a53b802b4f74e0cfd535ddbfd8737cebec3ae759ecce989e9e" => :sierra
+    sha256 "576c22ed7ea2534000a753ab88fe4ac3cc3250389e16bc76d7d94bfbd28d083d" => :el_capitan
   end
 
   depends_on "autoconf" => :build
