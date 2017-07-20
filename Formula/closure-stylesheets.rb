b05884d604b46f36be7262d2641cf7110d501764
ilovezfs <ilovezfs@icloud.com>
2017-07-19 17:35:09 -0700

    closure-stylesheets 1.5.0 (#15754)

--- Formula/closure-stylesheets.rb
+++ Formula/closure-stylesheets.rb
@@ -1,9 +1,9 @@
 class ClosureStylesheets < Formula
   desc "Extended CSS preprocessor, linter, and internationalizer"
   homepage "https://github.com/google/closure-stylesheets"
-  url "https://github.com/google/closure-stylesheets/releases/download/v1.4.0/closure-stylesheets.jar"
-  version "1.4.0"
-  sha256 "08c7f4c9694f9cf07e2d401af831f9208fac937e906ac65288d308fc09d2a8e3"
+  url "https://github.com/google/closure-stylesheets/releases/download/v1.5.0/closure-stylesheets.jar"
+  version "1.5.0"
+  sha256 "aa4e9b23093187a507a4560d13e59411fc92e285bc911b908a6bcf39479df03c"
 
   bottle :unneeded
 
