37352cf9de116aa3550db177ecd9ffe7040ef94d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-21 13:43:20 +0000

    qhull: add 2015.2 bottle.

--- Formula/qhull.rb
+++ Formula/qhull.rb
@@ -5,6 +5,13 @@ class Qhull < Formula
   version "2015.2"
   sha256 "78b010925c3b577adc3d58278787d7df08f7c8fb02c3490e375eab91bb58a436"
 
+  bottle do
+    cellar :any
+    sha256 "2b0192507bef8e077cae894ee59a2a25b490170cc1be64413caa1ac6e48da86d" => :sierra
+    sha256 "98d3967e46833ff3278b6815b638b5c4b049bb621543b9015124fef83276ab01" => :el_capitan
+    sha256 "00fb087ce5f3d674d22959ea820b4e3605b02ea31694d4f00d2044bc54ca3c02" => :yosemite
+  end
+
   depends_on "cmake" => :build
 
   def install
