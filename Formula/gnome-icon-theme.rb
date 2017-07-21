fa5b7f409d16ea1b91b5f4700f100cc7efd8e47f
cherenkov <claucece@users.noreply.github.com>
2017-07-21 04:23:56 -0500

    gnome-icon-theme: update dependencies (#15765)

--- Formula/gnome-icon-theme.rb
+++ Formula/gnome-icon-theme.rb
@@ -12,11 +12,10 @@ class GnomeIconTheme < Formula
   end
 
   depends_on "pkg-config" => :build
+  depends_on "intltool" => :build
   depends_on "gettext" => :build
   depends_on "gtk+3" => :build # for gtk3-update-icon-cache
-  depends_on "icon-naming-utils" => :build
-  depends_on "intltool" => :build
-  depends_on "librsvg" => :build
+  depends_on "librsvg"
 
   def install
     system "./configure", "--disable-dependency-tracking",
