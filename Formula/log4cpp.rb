a4712b4a12be47286b198d98bc29458bb3396e07
ilovezfs <ilovezfs@icloud.com>
2017-07-22 00:39:59 -0700

    log4cpp 1.1.3
    Closes #15887.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/log4cpp.rb
+++ Formula/log4cpp.rb
@@ -1,8 +1,8 @@
 class Log4cpp < Formula
   desc "Configurable logging for C++"
   homepage "https://log4cpp.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/log4cpp/log4cpp-1.1.x%20%28new%29/log4cpp-1.1/log4cpp-1.1.2.tar.gz"
-  sha256 "a92bb210cddca7a1d6e7ea89f52b6eecbee7c3e7c1bc22a6e2593ef46fe8798b"
+  url "https://downloads.sourceforge.net/project/log4cpp/log4cpp-1.1.x%20%28new%29/log4cpp-1.1/log4cpp-1.1.3.tar.gz"
+  sha256 "2cbbea55a5d6895c9f0116a9a9ce3afb86df383cd05c9d6c1a4238e5e5c8f51d"
 
   bottle do
     cellar :any
