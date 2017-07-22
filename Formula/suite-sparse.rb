77398d40d51bc9d3cca461fd5381b2ba8767cdb7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-22 16:13:35 +0000

    suite-sparse: add 4.5.5 bottle.

--- Formula/suite-sparse.rb
+++ Formula/suite-sparse.rb
@@ -4,6 +4,13 @@ class SuiteSparse < Formula
   url "http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-4.5.5.tar.gz"
   sha256 "b9a98de0ddafe7659adffad8a58ca3911c1afa8b509355e7aa58b02feb35d9b6"
 
+  bottle do
+    cellar :any
+    sha256 "9344c95830eebd2e6a418beb5a3671cb3955a8b798486994d2c2163c7a4a86c2" => :sierra
+    sha256 "925358aebf66ab22eb8f577cb3adcfc1f16045645266c4bab226a520261f78c0" => :el_capitan
+    sha256 "3a527e97525683038594795e95794cb763aabad83c6df44fcecc0c7e57a8ef45" => :yosemite
+  end
+
   depends_on "metis"
 
   def install
