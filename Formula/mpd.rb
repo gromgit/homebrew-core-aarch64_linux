adff10acc0544fd8de78c2c9ff39b64bb9ef8062
ilovezfs <ilovezfs@icloud.com>
2017-07-21 23:37:56 -0700

    mpd: revision for libmpdclient

--- Formula/mpd.rb
+++ Formula/mpd.rb
@@ -3,6 +3,7 @@ class Mpd < Formula
   homepage "https://www.musicpd.org/"
   url "https://www.musicpd.org/download/mpd/0.20/mpd-0.20.9.tar.xz"
   sha256 "cd77a2869e32354b004cc6b34fcb0bee56114caa2d9ed862aaa8071441e34eb7"
+  revision 1
 
   bottle do
     sha256 "6aa7348e26c4fb985f5405f536067d6748cbddae11c56dca364fdcef9a5f4003" => :sierra
