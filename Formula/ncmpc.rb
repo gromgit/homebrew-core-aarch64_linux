7595a4fc81142281ba04ee934dcd40c5c28ea8ee
ilovezfs <ilovezfs@icloud.com>
2017-07-23 00:08:16 -0700

    ncmpc: revision for libmpdclient
    add a test
    
    Closes #15924.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/ncmpc.rb
+++ Formula/ncmpc.rb
@@ -3,6 +3,7 @@ class Ncmpc < Formula
   homepage "https://www.musicpd.org/clients/ncmpc/"
   url "https://www.musicpd.org/download/ncmpc/0/ncmpc-0.27.tar.xz"
   sha256 "f9a26a3fc869cfdf0a16b0ea3e6512c2fe28a031bbc71b1d24a2bf0bbd3e15d9"
+  revision 1
 
   bottle do
     sha256 "d6dea0bd60e7d938df31a851f7c2f8f21476114e375b90d54aa3cd1eaa07c0dc" => :sierra
@@ -18,5 +19,9 @@ class Ncmpc < Formula
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make", "install"
+  end
+
+  test do
+    system bin/"ncmpc", "--help"
   end
 end
