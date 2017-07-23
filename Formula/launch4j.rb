7ddfd39fcdb92b188049c5ea34d84a326f91f222
Florian Kistner <fkistner@users.noreply.github.com>
2017-07-23 09:39:33 +0200

    launch4j 3.11 (#15787)

--- Formula/launch4j.rb
+++ Formula/launch4j.rb
@@ -1,9 +1,9 @@
 class Launch4j < Formula
   desc "Cross-platform Java executable wrapper"
   homepage "https://launch4j.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/launch4j/launch4j-3/3.9/launch4j-3.9-macosx-x86.tgz"
-  version "3.9"
-  sha256 "5de7594b877827be169d88111f6d184db334871c88c2314cd70cc33c3c7344e1"
+  url "https://downloads.sourceforge.net/project/launch4j/launch4j-3/3.11/launch4j-3.11-macosx-x86.tgz"
+  version "3.11"
+  sha256 "add9e680f82f45b79f9c7e878fbde43aa333cefe3ae86bcdc85937c480b378fa"
 
   bottle :unneeded
 
