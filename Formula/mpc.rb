1e16de87c793aca163b28f6e2464ecf468581c25
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-23 18:30:20 +0000

    mpc: update 0.28_1 bottle.

--- Formula/mpc.rb
+++ Formula/mpc.rb
@@ -7,10 +7,9 @@ class Mpc < Formula
 
   bottle do
     cellar :any
-    sha256 "26f8fd128483fa5ce7b3543f4152c1f1ee49ea6e2ec8f5e59874991d84627e96" => :sierra
-    sha256 "40b5cd34fd7b212a4d83476f0511bdd50bdc7533450665fc1ca5f0f008d510b7" => :el_capitan
-    sha256 "b521e1e20acbe6b1cfb0bc52cd810e8fc8c5b8ae69441964122606c81bfc0dea" => :yosemite
-    sha256 "8e96cbd74506a8d2b86e3fab070bdb36a30fe2fba987d928053d160e5b036f6f" => :mavericks
+    sha256 "aba27710d5d28f11e4df66e31baa307064c3ac6760083b0ee6411a57ff4920c1" => :sierra
+    sha256 "c963c018ed6d13b3bd46fd292fa42d29298499b96fc8cb60ae62accb292d6b7a" => :el_capitan
+    sha256 "10dbe56eb9a55d841001a9b7b553b80cd7287404b97f8526e2343b66cf6510e6" => :yosemite
   end
 
   depends_on "pkg-config" => :build
