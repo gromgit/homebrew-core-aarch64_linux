f542cd1e2d577264f898ef5f446dded45ef13e5f
ilovezfs <ilovezfs@icloud.com>
2017-07-23 06:24:25 -0700

    tfenv 0.5.2
    Closes #15937.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/tfenv.rb
+++ Formula/tfenv.rb
@@ -1,8 +1,8 @@
 class Tfenv < Formula
   desc "Terraform version manager inspired by rbenv"
   homepage "https://github.com/kamatama41/tfenv"
-  url "https://github.com/kamatama41/tfenv/archive/v0.5.1.tar.gz"
-  sha256 "dda9d6bf205c47fac1cb8fae6750a4fe26bbf8fff7765f1f7b03ce45862ef404"
+  url "https://github.com/kamatama41/tfenv/archive/v0.5.2.tar.gz"
+  sha256 "771679a32b221d3258e31d839bceffe56ff65567628a47e5831da9ca9f62d73b"
   head "https://github.com/kamatama41/tfenv.git"
 
   bottle :unneeded
