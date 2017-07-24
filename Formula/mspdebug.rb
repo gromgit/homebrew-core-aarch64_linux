9ca609f722cc2b94023fdecb6bf86bb61f3a4afb
ilovezfs <ilovezfs@icloud.com>
2017-07-24 04:30:39 -0700

    mspdebug 0.25
    depend on hidapi
    
    Closes #15978.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mspdebug.rb
+++ Formula/mspdebug.rb
@@ -1,8 +1,8 @@
 class Mspdebug < Formula
   desc "Debugger for use with MSP430 MCUs"
   homepage "http://dlbeer.co.nz/mspdebug/"
-  url "https://github.com/dlbeer/mspdebug/archive/v0.24.tar.gz"
-  sha256 "ace77951dc36227bbc4d5df1c33c1e5de833cccded33aa2a322c831bd8f8c146"
+  url "https://github.com/dlbeer/mspdebug/archive/v0.25.tar.gz"
+  sha256 "347b5ae5d0ab0cddb54363b72abe482f9f5d6aedb8f230048de0ded28b7d1503"
 
   bottle do
     sha256 "66539d0d14a5aea20add7ca1c9ef3ddd52a3c55f297ef363ac87d5e8637b5fb9" => :sierra
@@ -11,9 +11,11 @@ class Mspdebug < Formula
     sha256 "0c2ec6054005d777a0da7dfff74d401811fe5d0543b14cd6b02e6e03dc5bd173" => :mavericks
   end
 
+  depends_on "hidapi"
   depends_on "libusb-compat"
 
   def install
+    ENV.append_to_cflags "-I#{Formula["hidapi"].opt_include}/hidapi"
     system "make", "PREFIX=#{prefix}", "install"
   end
 
