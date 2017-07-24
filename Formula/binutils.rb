5d0f027a03511a9973782f55371fea733c5b8181
ilovezfs <ilovezfs@icloud.com>
2017-07-24 04:26:03 -0700

    binutils 2.29
    Closes #15975.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/binutils.rb
+++ Formula/binutils.rb
@@ -1,9 +1,9 @@
 class Binutils < Formula
   desc "FSF/GNU ld, ar, readelf, etc. for native development"
   homepage "https://www.gnu.org/software/binutils/binutils.html"
-  url "https://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz"
-  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.28.tar.gz"
-  sha256 "cd717966fc761d840d451dbd58d44e1e5b92949d2073d75b73fccb476d772fcf"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.29.tar.gz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.29.tar.gz"
+  sha256 "172e8c89472cf52712fd23a9f14e9bca6182727fb45b0f8f482652a83d5a11b4"
 
   bottle do
     rebuild 1
