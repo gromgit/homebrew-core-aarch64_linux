51bf9402b5c17ec6da98bfd2f71620bff878e7e3
ilovezfs <ilovezfs@icloud.com>
2017-07-25 13:21:30 -0700

    tomee-jax-rs: remove redundant version specification

--- Formula/tomee-jax-rs.rb
+++ Formula/tomee-jax-rs.rb
@@ -2,7 +2,6 @@ class TomeeJaxRs < Formula
   desc "TomeEE Web Profile plus JAX-RS"
   homepage "https://tomee.apache.org/"
   url "https://www.apache.org/dyn/closer.cgi?path=tomee/tomee-1.7.4/apache-tomee-1.7.4-jaxrs.tar.gz"
-  version "1.7.4"
   sha256 "35f56e5f79dfa3ebfe220c2c45b280b204dcd265c4e905b994669391f4672e71"
 
   bottle :unneeded
