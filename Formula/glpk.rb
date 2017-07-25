3dee41b70912816abb21b3e5031240e8d8d9fde4
ilovezfs <ilovezfs@icloud.com>
2017-07-25 02:03:03 -0700

    glpk 4.63
    Closes #16008.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/glpk.rb
+++ Formula/glpk.rb
@@ -1,9 +1,9 @@
 class Glpk < Formula
   desc "Library for Linear and Mixed-Integer Programming"
   homepage "https://www.gnu.org/software/glpk/"
-  url "https://ftp.gnu.org/gnu/glpk/glpk-4.62.tar.gz"
-  mirror "https://ftpmirror.gnu.org/glpk/glpk-4.62.tar.gz"
-  sha256 "096e4be3f83878ccf70e1fdb62ad1c178715ef8c0d244254c29e2f9f0c1afa70"
+  url "https://ftp.gnu.org/gnu/glpk/glpk-4.63.tar.gz"
+  mirror "https://ftpmirror.gnu.org/glpk/glpk-4.63.tar.gz"
+  sha256 "914d27f1a51c2bf4a51f1bd4a507f875fcca99db7b219380b836a25b29b3e7f6"
 
   bottle do
     cellar :any
