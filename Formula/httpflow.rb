9a69ba28d7dfbe68ec9ee592e7dd79ad765ce240
ilovezfs <ilovezfs@icloud.com>
2017-07-25 02:01:04 -0700

    httpflow 0.0.7
    depend on pcre
    
    Closes #16007.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/httpflow.rb
+++ Formula/httpflow.rb
@@ -1,8 +1,8 @@
 class Httpflow < Formula
   desc "Packet capture and analysis utility similar to tcpdump for HTTP"
   homepage "https://github.com/six-ddc/httpflow"
-  url "https://github.com/six-ddc/httpflow/archive/0.0.6.tar.gz"
-  sha256 "8a462e7c56a00c039cf35aae8f983350b4a88a8e2a9a40614e1584ec8fe7aa1d"
+  url "https://github.com/six-ddc/httpflow/archive/0.0.7.tar.gz"
+  sha256 "9bcc7bc00427a4b01c5a5178113780f2321fbe2f81857c915a420df3a6b41de2"
   head "https://github.com/six-ddc/httpflow.git"
 
   bottle do
@@ -11,6 +11,8 @@ class Httpflow < Formula
     sha256 "3f64470510030ce9fa0241ae229619fb6c1ca99bffb72826ab63b6d343f7ef8f" => :el_capitan
     sha256 "159bb97cf4d34ae30506fb5ce854231e95423ecd904d985ae1c4ee43e81dca6c" => :yosemite
   end
+
+  depends_on "pcre"
 
   def install
     system "make"
