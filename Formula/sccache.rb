3b832cd3bb1f3292bb818e1daf69e240e1941eeb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-26 13:38:20 +0000

    sccache: add 0.2.0 bottle.

--- Formula/sccache.rb
+++ Formula/sccache.rb
@@ -5,6 +5,12 @@ class Sccache < Formula
   sha256 "441d9ffb4ae76cc8219ad4d372c1c3335dc81a144d795663e58161f25e1dc651"
   head "https://github.com/mozilla/sccache.git"
 
+  bottle do
+    sha256 "09c5615fbbc77ede76b3152f89263c956968fc16623d3a49df1ddb5c1884e575" => :sierra
+    sha256 "1f54c022f6a760b18d1e05bf8b75a6981a9c4039b7efada98d47a0a0e83ec767" => :el_capitan
+    sha256 "cdb270d1802117dd470792ee04d0c722a58cf899c2428432efa4ed9b3fe88efb" => :yosemite
+  end
+
   depends_on "rust" => :build
 
   def install
