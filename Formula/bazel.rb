61eb2051c3e75372e5e3bf681136bde03acd8cb0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-27 19:41:41 +0000

    bazel: update 0.5.3 bottle.

--- Formula/bazel.rb
+++ Formula/bazel.rb
@@ -6,10 +6,9 @@ class Bazel < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "c102f7c97f48397e2cd128b111f4d1d84698299554c20e5228b0f5526f9d095f" => :sierra
-    sha256 "c9d8c15c0105d78b35ce4d405ca7649562aef7bc108ae62d03282ada358ec655" => :el_capitan
-    sha256 "a22b2da3456830f057b94ad043f28a511c2d66fd215010745b4fb55d9644b68b" => :yosemite
+    sha256 "7437094497afb5b9d842690cf9f01c254265c344603fff43f7560f064914ebbc" => :sierra
+    sha256 "6155a251b0b571fc2e3a9d5293fc5696eaa0cd2df4ef751a334f04c4ce378d0f" => :el_capitan
+    sha256 "ccdfccb0373c11b81dd67c04fe85e0b1b78c828f36108b83565df26c059b5aed" => :yosemite
   end
 
   depends_on :java => "1.8+"
