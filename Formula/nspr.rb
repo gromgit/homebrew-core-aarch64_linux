0687df6c0f550a33b08a905cce9d8dc2101710e2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-27 19:55:07 +0000

    nspr: update 4.16 bottle.

--- Formula/nspr.rb
+++ Formula/nspr.rb
@@ -6,9 +6,9 @@ class Nspr < Formula
 
   bottle do
     cellar :any
-    sha256 "b1f33b12d253a2b626b9927cec0f6656feab4f2e15498a94eab476f81a140b23" => :sierra
-    sha256 "b3dfc884c4a3c56c7d2a2020d245994ada0df487846d13984632b938fac5019b" => :el_capitan
-    sha256 "9e00336a77113aca800c43e60a23fc56797ea589e2a22abcca906ce7dfb21a4e" => :yosemite
+    sha256 "1c651efa1235d579cae80dc8c0f626c16037c87183e831987fb4fc63a3a0ed3c" => :sierra
+    sha256 "0ad1e250f6770a2d322f4abcbf9b7068aaa1bb9ad66da92d97b68d3f35d32132" => :el_capitan
+    sha256 "43b35c3f35ee4705d91ce375759765e10e4d4400ad7d9dc58fced096d594972d" => :yosemite
   end
 
   def install
