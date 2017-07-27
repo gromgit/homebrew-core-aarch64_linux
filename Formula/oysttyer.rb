9aeced9d77154301d69cc6fbac23c561eda54686
ilovezfs <ilovezfs@icloud.com>
2017-07-26 22:47:31 -0700

    oysttyer 2.9.1 (#16095)

--- Formula/oysttyer.rb
+++ Formula/oysttyer.rb
@@ -1,8 +1,8 @@
 class Oysttyer < Formula
   desc "Command-line Twitter client"
   homepage "https://github.com/oysttyer/oysttyer"
-  url "https://github.com/oysttyer/oysttyer/archive/2.9.0.tar.gz"
-  sha256 "0d8ec6bd10674f805552c8d77fa26bf491613f318fa8481e3dc714660887e6e0"
+  url "https://github.com/oysttyer/oysttyer/archive/2.9.1.tar.gz"
+  sha256 "2539e993f72bc4c4547dc8c93ae385d06f285a136bc3e9192f4ce0182f6619e3"
   head "https://github.com/oysttyer/oysttyer.git"
 
   bottle :unneeded
