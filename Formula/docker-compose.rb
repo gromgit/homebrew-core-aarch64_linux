d6bbe8eb23f0dbc4af926084baa0611526f57dbe
ilovezfs <ilovezfs@icloud.com>
2017-07-26 21:33:45 -0700

    docker-compose 1.15.0
    Closes #16090.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/docker-compose.rb
+++ Formula/docker-compose.rb
@@ -3,8 +3,8 @@ class DockerCompose < Formula
 
   desc "Isolated development environments using Docker"
   homepage "https://docs.docker.com/compose/"
-  url "https://github.com/docker/compose/archive/1.14.0.tar.gz"
-  sha256 "61d9e9669e1402f77320fbda4e9d3584c201781a9a6061b54a6786f5dcde888f"
+  url "https://github.com/docker/compose/archive/1.15.0.tar.gz"
+  sha256 "a2f3fb12a7ac9ecdefbbf9c3c33a7e1e4c033a7cc9eccf74477c393d73191172"
   head "https://github.com/docker/compose.git"
 
   bottle do
