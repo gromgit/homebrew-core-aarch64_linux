fdb3baad435b5fb919531e832c79da2634e50afb
Alex Epstein <epsteina@wit.edu>
2017-07-27 22:21:32 -0400

    bash-snippets 1.16.0

--- Formula/bash-snippets.rb
+++ Formula/bash-snippets.rb
@@ -1,8 +1,8 @@
 class BashSnippets < Formula
   desc "Collection of small bash scripts for heavy terminal users"
   homepage "https://github.com/alexanderepstein/Bash-Snippets"
-  url "https://github.com/alexanderepstein/Bash-Snippets/archive/v1.15.1.tar.gz"
-  sha256 "ecc771d8d8486229309bdcaef330492ce37fb827e6ce276f251380db4a63930b"
+  url "https://github.com/alexanderepstein/Bash-Snippets/archive/v1.16.0.tar.gz"
+  sha256 "19a029a204f16bd4db30cf7fa32aa6953dd10dcb996138b9bc65c01612afaac7"
 
   bottle :unneeded
 
