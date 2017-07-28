ba84b046665bbf9d8d21c7698f7336420a2129ef
Sean Reilly <seanjreilly@users.noreply.github.com>
2017-07-28 13:31:09 +0100

    gradle 4.0.2 (#16107)

--- Formula/gradle.rb
+++ Formula/gradle.rb
@@ -1,14 +1,8 @@
 class Gradle < Formula
   desc "Build system based on the Groovy language"
   homepage "https://www.gradle.org/"
-  url "https://services.gradle.org/distributions/gradle-4.0.1-all.zip"
-  sha256 "8a8005633be0ca38a206f2590445685683452a0b98a5d8ffd5b8413be09bf998"
-
-  devel do
-    url "https://services.gradle.org/distributions/gradle-4.1-rc-1-all.zip"
-    version "4.1-rc-1"
-    sha256 "eb504c25abae8ee43e01a56f6873137d74ce68c43b8b1fe89dc0562e6b81ca51"
-  end
+  url "https://services.gradle.org/distributions/gradle-4.0.2-all.zip"
+  sha256 "b683cb9f8510f3ae9533871e6b244c24eb9398e8c5b432471f91f48dbc91e7a9"
 
   bottle :unneeded
 
