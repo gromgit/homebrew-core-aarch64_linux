07d2abc0a90ebb03ddd4648a5e5c85bf833e56c6
Timothy Sutton <tim@synthist.net>
2017-07-31 10:04:12 -0400

    wimlib 1.12.0
    Closes #16244.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/wimlib.rb
+++ Formula/wimlib.rb
@@ -1,8 +1,8 @@
 class Wimlib < Formula
   desc "Library to create, extract, and modify Windows Imaging files"
   homepage "https://wimlib.net/"
-  url "https://wimlib.net/downloads/wimlib-1.11.0.tar.gz"
-  sha256 "16d19c3fbbda1c3ac8034b0884f42962e70ec0608ac54defc60f84328ae447a0"
+  url "https://wimlib.net/downloads/wimlib-1.12.0.tar.gz"
+  sha256 "852cf59d682a91974f715f09fa98cab621b740226adcfea7a42360be0f86464f"
 
   bottle do
     cellar :any
