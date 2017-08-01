f4c625e46153b2e35f2e4367712b8da8eab74317
Patrick Tescher <pat2man@gmail.com>
2017-08-01 13:06:54 -0700

    openshift-cli 3.6.0
    Closes #16293.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/openshift-cli.rb
+++ Formula/openshift-cli.rb
@@ -2,8 +2,8 @@ class OpenshiftCli < Formula
   desc "OpenShift command-line interface tools"
   homepage "https://www.openshift.com/"
   url "https://github.com/openshift/origin.git",
-    :tag => "v1.5.1",
-    :revision => "7b451fc9cade386042723a2c03c2deee626b579c"
+    :tag => "v3.6.0",
+    :revision => "c4dd4cf368b6204571faacde702e624b930a5214"
 
   head "https://github.com/openshift/origin.git"
 
@@ -12,13 +12,6 @@ class OpenshiftCli < Formula
     sha256 "349a5809fc1e681f722147bfa4044d3f2d3d6ff39a99b1264fe313be71c183c4" => :sierra
     sha256 "9f30956118d9a9a009d737818a70498befa62432f222865b33bca23f943a9910" => :el_capitan
     sha256 "b76d411adf0a5beabd7ea380f2013ecfb91ee6dc30f35b9cccd67265967817fc" => :yosemite
-  end
-
-  devel do
-    url "https://github.com/openshift/origin.git",
-      :tag => "v3.6.0-alpha.1",
-      :revision => "46942adea0aed0ff58965f846aa5d5021ce30e50"
-    version "3.6.0-alpha.1"
   end
 
   depends_on "go" => :build
@@ -30,8 +23,7 @@ class OpenshiftCli < Formula
 
     system "make", "all", "WHAT=cmd/oc", "GOFLAGS=-v", "OS_OUTPUT_GOPATH=1"
 
-    arch = MacOS.prefer_64_bit? ? "amd64" : "x86"
-    bin.install "_output/local/bin/darwin/#{arch}/oc"
+    bin.install "_output/local/bin/darwin/amd64/oc"
     bin.install_symlink "oc" => "oadm"
 
     bash_completion.install Dir["contrib/completions/bash/*"]
@@ -39,10 +31,6 @@ class OpenshiftCli < Formula
 
   test do
     assert_match /^oc v#{version}/, shell_output("#{bin}/oc version")
-    if version >= "3.6.0-alpha.0"
-      assert_match /^oc v#{version}/, shell_output("#{bin}/oadm version")
-    else
-      assert_match /^oadm v#{version}/, shell_output("#{bin}/oadm version")
-    end
+    assert_match /^oc v#{version}/, shell_output("#{bin}/oadm version")
   end
 end
