c0cfe29b130f3e760a3ff07afa16985dafc0d788
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-01 18:20:39 +0000

    sqldiff: update 3.20.0 bottle.

--- Formula/sqldiff.rb
+++ Formula/sqldiff.rb
@@ -7,9 +7,9 @@ class Sqldiff < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "79c9cdff6fe2ea2f6597ffd0e9002b2c0191f1605911a420772dda2dcc550abc" => :sierra
-    sha256 "19908bc00b0656cf462f00913f74bf91bee1f37745863333a71f2b74c40ae102" => :el_capitan
-    sha256 "d4f98a722fe0b7e0738f82d289c3225b4347c8c05ac2f14bcc6b9f026d85e94f" => :yosemite
+    sha256 "e7144d517abf89893e9cd9a17111e28807a8ff11be1dbbe0826890ed2a267dcc" => :sierra
+    sha256 "1b03a3a116344ff1a496655aaf6ab7bc7ad60b38396d4257e5358d5dff7f1f2b" => :el_capitan
+    sha256 "d9a18d50688253cf9914fc3c8779a6184de8af7852c8d90921b61f96e14dd627" => :yosemite
   end
 
   def install
