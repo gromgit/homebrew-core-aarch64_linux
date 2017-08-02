dfa744157a0edb56a5528e42f67f6ba48cdb7a7b
ilovezfs <ilovezfs@icloud.com>
2017-08-01 23:07:25 -0700

    django-completion 1.11.4 (#16286)

--- Formula/django-completion.rb
+++ Formula/django-completion.rb
@@ -1,8 +1,8 @@
 class DjangoCompletion < Formula
   desc "Bash completion for Django"
   homepage "https://github.com/django/django"
-  url "https://github.com/django/django/archive/1.11.3.tar.gz"
-  sha256 "d6f7ea3d6dfd1369d62bff7b4ae64fbd9389214df5aea155fe29c21dad58e69d"
+  url "https://github.com/django/django/archive/1.11.4.tar.gz"
+  sha256 "e7e3831b79f232a81aefd93ce3ad0ac2ead4c60732068acccda96a0b3cd91d79"
   head "https://github.com/django/django.git"
 
   bottle :unneeded
