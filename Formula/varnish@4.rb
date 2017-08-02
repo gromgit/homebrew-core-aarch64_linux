aaecfb974f23dd43b5b2d190743d935c25839db7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-02 19:16:09 +0000

    varnish@4: update 4.1.8 bottle.

--- Formula/varnish@4.rb
+++ Formula/varnish@4.rb
@@ -5,9 +5,9 @@ class VarnishAT4 < Formula
   sha256 "908e7fbfa0325498717686b2050181134aa0a69d1495c02b2625cd34d35a4ff1"
 
   bottle do
-    sha256 "e34cb934931f91ae0691e69455dbf0710ad4b455ed3d4713470b5cbae1a78efb" => :sierra
-    sha256 "cf20c9bb932bfacf42c92e07fa6f822f5db5bc89e4057bb08c2c60801953baa4" => :el_capitan
-    sha256 "da5777b7b53efa6bcad9bb4054ed84565d56f977fde127441734ee6f3dfc7cb4" => :yosemite
+    sha256 "feee2d796d10d27aab921d531d74be2e0a7ec789d5e5387d55d1628f0eb1d25a" => :sierra
+    sha256 "8e8e30c902fbce8dc6ed1533bf604a9d4a197645240ca770fe51928a29ec3aaf" => :el_capitan
+    sha256 "7ff168bb774232159547fdc251f4548e4e68cdcc7d01261b612872ef90c6030f" => :yosemite
   end
 
   keg_only :versioned_formula
