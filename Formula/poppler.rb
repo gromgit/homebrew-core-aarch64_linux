aa9b046b0176f075cfede2b95dfe08fea05bda3b
Ed Porras <ep@digressed.net>
2017-08-02 09:38:07 +0200

    poppler 0.57.0

--- Formula/poppler.rb
+++ Formula/poppler.rb
@@ -1,9 +1,8 @@
 class Poppler < Formula
   desc "PDF rendering library (based on the xpdf-3.0 code base)"
   homepage "https://poppler.freedesktop.org/"
-  url "https://poppler.freedesktop.org/poppler-0.56.0.tar.xz"
-  sha256 "869dbadf99ed882e776acbdbc06689d8a81872a2963440b1e8516cd7a2577173"
-  revision 1
+  url "https://poppler.freedesktop.org/poppler-0.57.0.tar.xz"
+  sha256 "0ea37de71b7db78212ebc79df59f99b66409a29c2eac4d882dae9f2397fe44d8"
 
   bottle do
     sha256 "15dab34a883c6dbff79f1ea4c51133266505bce11d9b2698cc1080bc71cfec6b" => :sierra
