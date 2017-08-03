35424e41128ad0f7ad6b8ee97b3dea29c376618c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-03 16:02:01 +0000

    imagemagick@6: update 6.9.9-5 bottle.

--- Formula/imagemagick@6.rb
+++ Formula/imagemagick@6.rb
@@ -9,9 +9,9 @@ class ImagemagickAT6 < Formula
   sha256 "aa6444ece8d29b87fb4f25e997cd0b4fb48774206cea9490c6028babdee7d056"
 
   bottle do
-    sha256 "af680169dca9deffcf82ae491201fa0a68a69be359b3d9ba9d4f3a34bc956a46" => :sierra
-    sha256 "5abd2a1ccc960d92fa2c7b8dff75e168cbca1ea1b349d54fe2b0f81a3b67c188" => :el_capitan
-    sha256 "8541ec59898860d5ecc29fbec48dcfaecfe5851af78ca9119a1c7371d42159cc" => :yosemite
+    sha256 "26a16ed29d78f9a7cdb534e856f38bef3302ee50da14dde004c48312b39c2ef2" => :sierra
+    sha256 "c5894a7269b6fee2bb86d7acb6402904c23fdab948e7e1dcaad970efb140b703" => :el_capitan
+    sha256 "b3905f9ecb4baccd2f7dfd8d8b365aca40f3b284291731860b28c294045bf141" => :yosemite
   end
 
   keg_only :versioned_formula
