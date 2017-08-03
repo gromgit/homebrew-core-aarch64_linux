6631889a724817877e89a1811c80f8869d89ff3c
Nick Sieger <nicksieger@gmail.com>
2017-08-03 13:03:41 -0500

    brew-gem 0.8.2 (#16369)

--- Formula/brew-gem.rb
+++ Formula/brew-gem.rb
@@ -1,8 +1,8 @@
 class BrewGem < Formula
-  desc "Install rubygems as homebrew formulae"
+  desc "Install RubyGems as Homebrew formulae"
   homepage "https://github.com/sportngin/brew-gem"
-  url "https://github.com/sportngin/brew-gem/archive/v0.8.1.tar.gz"
-  sha256 "9054ceb834b12a45c72bf4a2c082de25a7b3380ea1b36083b65a5dd992f215ed"
+  url "https://github.com/sportngin/brew-gem/archive/v0.8.2.tar.gz"
+  sha256 "55380a940ef7b93d4456918f746418942b277d8db0a7e68b0f1e6f884dc581b3"
 
   head "https://github.com/sportngin/brew-gem.git"
 
