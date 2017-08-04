9c1cdbce3031153376d1d69d6579886e252f8f0c
ilovezfs <ilovezfs@icloud.com>
2017-08-04 03:05:59 -0700

    angular-cli 1.2.7
    Closes #16398.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/angular-cli.rb
+++ Formula/angular-cli.rb
@@ -3,18 +3,13 @@ require "language/node"
 class AngularCli < Formula
   desc "CLI tool for Angular"
   homepage "https://cli.angular.io/"
-  url "https://registry.npmjs.org/@angular/cli/-/cli-1.2.6.tgz"
-  sha256 "3ef8d33edf42799decda6247f1bb565b6f486e7ddcd6798bd8b3b54df9cc592c"
+  url "https://registry.npmjs.org/@angular/cli/-/cli-1.2.7.tgz"
+  sha256 "26a6c39d953aa8b99ef6aebbeae935d0582c296bfa04cf85722c598386d3a611"
 
   bottle do
     sha256 "e442b23383b736c398ef06d41931faa7359699fa36b9f4e7887242fbf1d3937c" => :sierra
     sha256 "1b148c04fad9483f856940aa8ed4efe74b93470e4276c7b6191fe37ddecbccd2" => :el_capitan
     sha256 "82336b6afa4a90bddbb44b5bb336af9e65d2ad90d5cd60d79093023e687fee45" => :yosemite
-  end
-
-  devel do
-    url "https://registry.npmjs.org/@angular/cli/-/cli-1.3.0-rc.3.tgz"
-    sha256 "7452bb854dc9b522f132ae28ee8357a241dc78dcc8283267797bd1702039d258"
   end
 
   depends_on "node"
