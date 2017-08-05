4c9cc3843ad8bc7decc20be860eba9c7d3adb5de
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-05 20:23:46 +0000

    double-conversion: update 3.0.0 bottle.

--- Formula/double-conversion.rb
+++ Formula/double-conversion.rb
@@ -8,11 +8,9 @@ class DoubleConversion < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 2
-    sha256 "a314b7d76922e889a1dc0414b24d399a275459d2c3916e16921c74713593000b" => :sierra
-    sha256 "727930169ce2bde673b86c5ae242c6641cf1a593902f5e7821981009dd4a2306" => :el_capitan
-    sha256 "ffcf11a69c596f04cfb936cbc33a489f9f994b8fdaf66901f23e88a5638a4477" => :yosemite
-    sha256 "648a9a7dcebe2abca8848a11c4647c0d3c85c6c49181100859277d24b8b71f62" => :mavericks
+    sha256 "bb2a51610649c9f86c06adc2ae66764a42b66e4d95166d306b05dff35e91e422" => :sierra
+    sha256 "30a844eb4530cbe35e05fefed118fe096bf34de61c536283e6a1845f2f087639" => :el_capitan
+    sha256 "dae47f87d852b0df160efaa57b1d2b81f96f31a8a2526e478f073f7fd0fc0beb" => :yosemite
   end
 
   depends_on "cmake" => :build
