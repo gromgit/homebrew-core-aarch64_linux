92ce1629b598f0b1a0f86d2b6e395c74250e7444
ilovezfs <ilovezfs@icloud.com>
2017-08-06 04:19:50 -0700

    zbar: revision for jpeg

--- Formula/zbar.rb
+++ Formula/zbar.rb
@@ -1,7 +1,7 @@
 class Zbar < Formula
   desc "Suite of barcodes-reading tools"
   homepage "https://zbar.sourceforge.io"
-  revision 5
+  revision 6
 
   stable do
     url "https://downloads.sourceforge.net/project/zbar/zbar/0.10/zbar-0.10.tar.bz2"
