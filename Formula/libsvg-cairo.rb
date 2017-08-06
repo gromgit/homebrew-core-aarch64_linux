605aa0a8b22afd7366911950c25b40d3ff7c7a31
ilovezfs <ilovezfs@icloud.com>
2017-08-06 06:24:55 -0700

    libsvg-cairo: revision for jpeg

--- Formula/libsvg-cairo.rb
+++ Formula/libsvg-cairo.rb
@@ -3,7 +3,7 @@ class LibsvgCairo < Formula
   homepage "https://cairographics.org/"
   url "https://cairographics.org/snapshots/libsvg-cairo-0.1.6.tar.gz"
   sha256 "a380be6a78ec2938100ce904363815a94068fca372c666b8cc82aa8711a0215c"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
