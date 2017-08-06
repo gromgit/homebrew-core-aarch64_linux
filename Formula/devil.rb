28e73fd0f8f5b2330270a4a1bad15cfda7f21874
ilovezfs <ilovezfs@icloud.com>
2017-08-06 04:19:39 -0700

    devil: revision for jpeg
    patch for jpeg 9 compatibility

--- Formula/devil.rb
+++ Formula/devil.rb
@@ -1,9 +1,29 @@
 class Devil < Formula
   desc "Cross-platform image library"
   homepage "https://sourceforge.net/projects/openil/"
-  url "https://downloads.sourceforge.net/project/openil/DevIL/1.8.0/DevIL-1.8.0.tar.gz"
-  sha256 "0075973ee7dd89f0507873e2580ac78336452d29d34a07134b208f44e2feb709"
+  revision 1
   head "https://github.com/DentonW/DevIL.git"
+
+  stable do
+    url "https://downloads.sourceforge.net/project/openil/DevIL/1.8.0/DevIL-1.8.0.tar.gz"
+    sha256 "0075973ee7dd89f0507873e2580ac78336452d29d34a07134b208f44e2feb709"
+
+    # jpeg 9 compatibility
+    # Upstream commit from 3 Jan 2017 "Fixed int to boolean conversion error
+    # under Linux"
+    patch do
+      url "https://github.com/DentonW/DevIL/commit/41fcabbe.patch?full_index=1"
+      sha256 "324dc09896164bef75bb82b37981cc30dfecf4f1c2386c695bb7e92a2bb8154d"
+    end
+
+    # jpeg 9 compatibility
+    # Upstream commit from 7 Jan 2017 "Fixing boolean compilation errors under
+    # Linux / MacOS, issue #48 at https://github.com/DentonW/DevIL/issues/48"
+    patch do
+      url "https://github.com/DentonW/DevIL/commit/4a2d7822.patch?full_index=1"
+      sha256 "7e74a4366ef485beea1c4285f2b371b6bfa0e2513b83d4d45e4e120690c93f1d"
+    end
+  end
 
   bottle do
     cellar :any
