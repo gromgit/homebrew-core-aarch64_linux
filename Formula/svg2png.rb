50de9aab187246a67ea15c05b06f8ce13222c9f9
ilovezfs <ilovezfs@icloud.com>
2017-08-06 06:31:39 -0700

    svg2png: revision for jpeg

--- Formula/svg2png.rb
+++ Formula/svg2png.rb
@@ -3,6 +3,7 @@ class Svg2png < Formula
   homepage "https://cairographics.org/"
   url "https://cairographics.org/snapshots/svg2png-0.1.3.tar.gz"
   sha256 "e658fde141eb7ce981ad63d319339be5fa6d15e495d1315ee310079cbacae52b"
+  revision 1
 
   bottle do
     cellar :any
