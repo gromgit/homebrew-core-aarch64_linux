ae6a382844103d05b1141556dfeca7e420276af9
FX Coudert <fxcoudert@gmail.com>
2017-08-06 09:06:22 +0200

    libmpc: expand test

--- Formula/libmpc.rb
+++ Formula/libmpc.rb
@@ -33,11 +33,15 @@ class Libmpc < Formula
   test do
     (testpath/"test.c").write <<-EOS.undent
       #include <mpc.h>
+      #include <assert.h>
+      #include <math.h>
 
-      int main()
-      {
+      int main() {
         mpc_t x;
         mpc_init2 (x, 256);
+        mpc_set_d_d (x, 1., INFINITY, MPC_RNDNN);
+        mpc_tanh (x, x, MPC_RNDNN);
+        assert (mpfr_nan_p (mpc_realref (x)) && mpfr_nan_p (mpc_imagref (x)));
         mpc_clear (x);
         return 0;
       }
