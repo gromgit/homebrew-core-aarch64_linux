a1453d76612619f7040272e06bb673999b528eae
ilovezfs <ilovezfs@icloud.com>
2017-08-06 04:19:46 -0700

    mjpegtools: revision for jpeg

--- Formula/mjpegtools.rb
+++ Formula/mjpegtools.rb
@@ -3,7 +3,7 @@ class Mjpegtools < Formula
   homepage "https://mjpeg.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/mjpeg/mjpegtools/2.1.0/mjpegtools-2.1.0.tar.gz"
   sha256 "864f143d7686377f8ab94d91283c696ebd906bf256b2eacc7e9fb4dddcedc407"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
