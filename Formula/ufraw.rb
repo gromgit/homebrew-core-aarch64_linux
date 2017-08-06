c990e5fcfa43dcf11542e25ede61e82bb31963ce
ilovezfs <ilovezfs@icloud.com>
2017-08-06 04:19:49 -0700

    ufraw: revision for jpeg
    patch for jpeg 9 compatibility

--- Formula/ufraw.rb
+++ Formula/ufraw.rb
@@ -3,7 +3,7 @@ class Ufraw < Formula
   homepage "https://ufraw.sourceforge.io"
   url "https://downloads.sourceforge.net/project/ufraw/ufraw/ufraw-0.22/ufraw-0.22.tar.gz"
   sha256 "f7abd28ce587db2a74b4c54149bd8a2523a7ddc09bedf4f923246ff0ae09a25e"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "d77f0f432aa697d644d3a1458a9deeeb7802676a8ee95caa35e32f7bb8919906" => :sierra
@@ -21,6 +21,12 @@ class Ufraw < Formula
   depends_on "libtiff"
   depends_on "little-cms2"
   depends_on "exiv2" => :optional
+
+  # jpeg 9 compatibility
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/b8ed064/ufraw/jpeg9.patch"
+    sha256 "45de293a9b132eb675302ba8870f5b6216c51da8247cd096b24a5ab60ffbd7f9"
+  end
 
   def install
     system "./configure", "--disable-dependency-tracking",
