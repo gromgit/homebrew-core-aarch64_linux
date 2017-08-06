8da8808858eddcd41d4c28c8db76203f1b9fa9db
ilovezfs <ilovezfs@icloud.com>
2017-08-06 04:19:41 -0700

    libgeotiff: revision for jpeg

--- Formula/libgeotiff.rb
+++ Formula/libgeotiff.rb
@@ -3,6 +3,7 @@ class Libgeotiff < Formula
   homepage "https://geotiff.osgeo.org/"
   url "http://download.osgeo.org/geotiff/libgeotiff/libgeotiff-1.4.2.tar.gz"
   sha256 "ad87048adb91167b07f34974a8e53e4ec356494c29f1748de95252e8f81a5e6e"
+  revision 1
 
   bottle do
     rebuild 1
