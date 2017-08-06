f17ce8cb25b625dc4e88f3ab0736081f26c61b82
ilovezfs <ilovezfs@icloud.com>
2017-08-06 04:19:48 -0700

    sane-backends: revision for jpeg

--- Formula/sane-backends.rb
+++ Formula/sane-backends.rb
@@ -5,7 +5,7 @@ class SaneBackends < Formula
   mirror "https://mirrors.kernel.org/debian/pool/main/s/sane-backends/sane-backends_1.0.27.orig.tar.gz"
   mirror "https://fossies.org/linux/misc/sane-backends-1.0.27.tar.gz"
   sha256 "293747bf37275c424ebb2c833f8588601a60b2f9653945d5a3194875355e36c9"
-  revision 1
+  revision 2
   head "https://anonscm.debian.org/cgit/sane/sane-backends.git"
 
   bottle do
