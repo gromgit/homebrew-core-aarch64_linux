ad79d4b9c7b219ccfa14aea87debee5562c8aa05
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-08-07 09:41:40 -0700

    heroku 6.13.8
    Closes #16496.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Heroku < Formula
   desc "Command-line client for the cloud PaaS"
   homepage "https://cli.heroku.com"
-  url "https://registry.npmjs.org/heroku-cli/-/heroku-cli-6.13.7.tgz"
-  sha256 "822a3473d7a4bb9518f8aa02d00bce6f1b1e7d190b97f31272dd7e4879fee684"
+  url "https://registry.npmjs.org/heroku-cli/-/heroku-cli-6.13.8.tgz"
+  sha256 "467c436f146198ad344d82572e3d3b75b148b832a0a8396811a86c9f019d37c3"
   head "https://github.com/heroku/cli.git"
 
   bottle do
