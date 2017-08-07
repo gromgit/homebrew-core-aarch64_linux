394d468f3bebbc32bc463e506ffd35608c9e2eed
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-07 06:33:21 +0000

    webp: update 0.6.0_1 bottle.

--- Formula/webp.rb
+++ Formula/webp.rb
@@ -7,9 +7,9 @@ class Webp < Formula
 
   bottle do
     cellar :any
-    sha256 "4ccfbbb42e4b718ab6eec838b50be3ae36da1ecc6f00d3a438d80c9220936247" => :sierra
-    sha256 "cd0d20f0410b96f7564a0ccae776fcb2246030a8052a709ae3e5a3dd200fe6f3" => :el_capitan
-    sha256 "a3db82a59e4726452ff8b5c9352e016571e6fb1b0713180d7b6ccc7c5b64541d" => :yosemite
+    sha256 "f2867ef67916dfa80ce3d991218b8ac62c87f78b7ae3827a8df2d7d5c4a479dc" => :sierra
+    sha256 "ce30fa5f6f417955bfc1c2b0d3846c9a8504153a4ff22b73b4eec4b563885cf5" => :el_capitan
+    sha256 "cc3cc93c84295330637b0ab25c35f2344f9a6c4dbe460be4e6134066866185f2" => :yosemite
   end
 
   head do
