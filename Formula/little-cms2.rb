c18eed6aaf38520f93a39c64b0fbeba3444b0ae0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-07 06:33:16 +0000

    little-cms2: update 2.8_1 bottle.

--- Formula/little-cms2.rb
+++ Formula/little-cms2.rb
@@ -7,10 +7,9 @@ class LittleCms2 < Formula
 
   bottle do
     cellar :any
-    sha256 "919b1bbdb351c10d60136e83af36f7d2637ae09bf710cc49393127d77a460005" => :sierra
-    sha256 "cf9ce2f00b795f4b8e245a9a5b1650c526503e30b1eb332496bcd1c41568594f" => :el_capitan
-    sha256 "57e2eaf3df51fbc1642eebc2a3c9655409948c85565dbbf91168498d21ad7d57" => :yosemite
-    sha256 "966dd0d04898592c268ddd31a6cd2657a63e520d504acc7c4c6046c31fed81eb" => :mavericks
+    sha256 "5175bace6103d7ca8fb014db619bc11cb0d0fb29bcd2430d8e3dac3bda745a69" => :sierra
+    sha256 "23b02130356986222e3f6c16aa85cadd674e2fca33ebe044adc95a16c12bb093" => :el_capitan
+    sha256 "3cbcf21c725ccb1a260433b99136762cbbe8938a33ecdc8cdc82a48c7eb5acb7" => :yosemite
   end
 
   depends_on "jpeg" => :recommended
