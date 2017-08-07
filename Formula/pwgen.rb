e07879a8759bda8af7fff8b78332bdb5722ae774
ilovezfs <ilovezfs@icloud.com>
2017-08-07 11:45:48 -0700

    pwgen 2.08
    Closes #16502.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pwgen.rb
+++ Formula/pwgen.rb
@@ -1,8 +1,8 @@
 class Pwgen < Formula
   desc "Password generator"
   homepage "https://pwgen.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/pwgen/pwgen/2.07/pwgen-2.07.tar.gz"
-  sha256 "eb74593f58296c21c71cd07933e070492e9222b79cedf81d1a02ce09c0e11556"
+  url "https://downloads.sourceforge.net/project/pwgen/pwgen/2.08/pwgen-2.08.tar.gz"
+  sha256 "dab03dd30ad5a58e578c5581241a6e87e184a18eb2c3b2e0fffa8a9cf105c97b"
 
   bottle do
     cellar :any_skip_relocation
