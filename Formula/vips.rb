3977a56dac3e9dc6eaf89fb2f0d7f60839a8db42
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-07 06:33:19 +0000

    vips: update 8.5.7_1 bottle.

--- Formula/vips.rb
+++ Formula/vips.rb
@@ -6,9 +6,9 @@ class Vips < Formula
   revision 1
 
   bottle do
-    sha256 "a22674edfa0c053e7f462bb0f06cfc181529f524c901c3f5c107e94e64cbc863" => :sierra
-    sha256 "960290216f6a5f0ea4db41df47266369d28443a2c545612566ef0ad9709b257e" => :el_capitan
-    sha256 "e9f637f2b977b98141baf45ef4585a9baa74dd44869eb958fd65c024baff96a3" => :yosemite
+    sha256 "3f466e57cecb3e710ff3a5b60746b454f7b9c37b2a4d4c70f5ee2874328a61ef" => :sierra
+    sha256 "c260bf9a715b5dc1f9ca03239062adbebb0ff539bc60dc8e06454ff8a5dd3140" => :el_capitan
+    sha256 "68ad59cd3cc951a7900e309fe656874fae74bf036d9b8e9dd99ced90b37c4ecf" => :yosemite
   end
 
   depends_on "pkg-config" => :build
