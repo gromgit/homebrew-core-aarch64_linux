782ab08566e9254f909b58017e909f2a2b93d2ca
Christian Barcenas <christian@cbarcenas.com>
2017-08-07 03:22:14 -0700

    mimic 1.2.0.2 (new formula)
    Closes #16480.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/mimic.rb
@@ -0,0 +1,29 @@
+class Mimic < Formula
+  desc "Lightweight text-to-speech engine based on CMU Flite"
+  homepage "https://mimic.mycroft.ai"
+  url "https://github.com/MycroftAI/mimic/archive/1.2.0.2.tar.gz"
+  sha256 "6adcc9911b09d6e9513add41ad9dfc0893ece277f556419869520a0f0708c102"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "icu4c"
+  depends_on "portaudio"
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--enable-shared",
+                          "--enable-static",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"mimic", "-t", "Hello, Homebrew!", "test.wav"
+    assert (testpath/"test.wav").exist?
+  end
+end
