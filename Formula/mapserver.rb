42d1dcdbf538c43f3759cd76298e01a4ee8b27ce
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-07 06:33:23 +0000

    mapserver: update 7.0.6_1 bottle.

--- Formula/mapserver.rb
+++ Formula/mapserver.rb
@@ -7,9 +7,9 @@ class Mapserver < Formula
 
   bottle do
     cellar :any
-    sha256 "1ab6c6b519e7ba102308e125a84ad3a28a1c058c93dc9a14db33b646ca85315e" => :sierra
-    sha256 "7a89c7d190dad3b7686139102cabeb24e8fea79d739982020a95d5a7b5958138" => :el_capitan
-    sha256 "82f0e7a8b6d0f90831f74aea71e70b1463bdce213b763b559da3f23db6864c0c" => :yosemite
+    sha256 "0c7aaed0575b6136d627ad3431254d97e2e5bb81cc2027a1819c7a073b218384" => :sierra
+    sha256 "6d34027e2b3642d789d25e7bac23ea31c84ebd355c57146e4e32bc601cd8194f" => :el_capitan
+    sha256 "8fdacab080389dee4453f7c4872cce9eab67e4a8f0884b82eb30746f884ce269" => :yosemite
   end
 
   option "with-fastcgi", "Build with fastcgi support"
