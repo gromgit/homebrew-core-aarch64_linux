106d192fdd71557f4b2db01c2ecb6eaff2f91253
ilovezfs <ilovezfs@icloud.com>
2017-08-07 01:48:24 -0700

    grib-api 1.23.0
    also serves as revision for jpeg
    depend on numpy instead of vendoring it
    
    Closes #16476.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/grib-api.rb
+++ Formula/grib-api.rb
@@ -1,10 +1,9 @@
 class GribApi < Formula
   desc "Encode and decode grib messages (editions 1 and 2)"
   homepage "https://software.ecmwf.int/wiki/display/GRIB/Home"
-  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/g/grib-api/grib-api_1.19.0.orig.tar.xz"
-  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/g/grib-api/grib-api_1.19.0.orig.tar.xz"
-  sha256 "c234a0a6d551a79ac77eae86b5effaa82c96dfc16ba6a8e7570067d83f1f6326"
-  revision 1
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/g/grib-api/grib-api_1.23.0.orig.tar.xz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/g/grib-api/grib-api_1.23.0.orig.tar.xz"
+  sha256 "eeb908248f974daa8cc6224325c9938aca63f99b4e3c9366f4c34e3d179e2772"
 
   bottle do
     sha256 "6a405004526ab9d97fe7d26bd3ab9b08badfb8a77e09ce87a7a10872a606014c" => :sierra
@@ -15,21 +14,12 @@ class GribApi < Formula
   option "with-static", "Build static instead of shared library."
 
   depends_on "cmake" => :build
+  depends_on "numpy"
   depends_on "jasper" => :recommended
   depends_on "libpng" => :optional
   depends_on :fortran
 
-  resource "numpy" do
-    url "https://files.pythonhosted.org/packages/16/f5/b432f028134dd30cfbf6f21b8264a9938e5e0f75204e72453af08d67eb0b/numpy-1.11.2.tar.gz"
-    sha256 "04db2fbd64e2e7c68e740b14402b25af51418fc43a59d9e54172b38b906b0f69"
-  end
-
   def install
-    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
-    resource("numpy").stage do
-      system "python", *Language::Python.setup_install_args(libexec/"vendor")
-    end
-
     # Fix "no member named 'inmem_' in 'jas_image_t'"
     inreplace "src/grib_jasper_encoding.c", "image.inmem_    = 1;", ""
 
@@ -44,7 +34,7 @@ class GribApi < Formula
         args << "-DENABLE_PNG=ON"
       end
 
-      system "cmake", "..", *args
+      system "cmake", "..", "-DENABLE_NETCDF=OFF", *args
       system "make", "install"
     end
   end
