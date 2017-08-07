3bf0755ccf8f33592afb86cc82d16a69b3deea2a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-07 06:33:13 +0000

    scummvm: update 1.9.0_1 bottle.

--- Formula/scummvm.rb
+++ Formula/scummvm.rb
@@ -7,9 +7,9 @@ class Scummvm < Formula
   head "https://github.com/scummvm/scummvm.git"
 
   bottle do
-    sha256 "34e01d9f579230ff887a801722d9bd2d4dd4f7245ae5c96ef3d6a0c3a13003bf" => :sierra
-    sha256 "300612434290d59de56fde9715ca32ee8b8671d76625c3b79197cf44c81b2201" => :el_capitan
-    sha256 "213a1905e6d46cfe685e0cf25f0d7bb164bace5abbaff9497f4c1e40f794240d" => :yosemite
+    sha256 "eb9cad5628c6a13be6dadeedfe15d05c8ec18e5ecc5d6e725916c103a45f3200" => :sierra
+    sha256 "3e691aba43ac7f9bb56eff09d0b8c1d6a9e5843d30d3c7d209135ca1965d5169" => :el_capitan
+    sha256 "03a4c813c8a6ab9dd1f08f8efaf5d6972c27fffad4312070a7f22f04845de257" => :yosemite
   end
 
   depends_on "faad2"
