d32f93ebc4f3a7e026c43425f1d3f1dc82d938bc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-07 06:33:11 +0000

    htmldoc: update 1.9_1 bottle.

--- Formula/htmldoc.rb
+++ Formula/htmldoc.rb
@@ -7,9 +7,9 @@ class Htmldoc < Formula
   head "https://github.com/michaelrsweet/htmldoc.git"
 
   bottle do
-    sha256 "2d7de95746e4ac5e8a220a5207f31592e30900993c91394795028b8dfbdaac52" => :sierra
-    sha256 "6570a8d07e4cf9b4f2bf5503814ba1153d32a819128db5a679c0b75a0c8aaf60" => :el_capitan
-    sha256 "0c364daf5e5ad045c35f38e4024e80037f03193494c1d237016a8b24bec2d3a5" => :yosemite
+    sha256 "c34dd52990fcf2454678e81787709fd41a1d0cc7024eed82438676d7aed1c62d" => :sierra
+    sha256 "0ef48c9bdff878f3293fc187700a926854d565a3060e444e29386c44b3e5ec6b" => :el_capitan
+    sha256 "d0b5adc798dbcdfdc9a5ae242904fae7c85f65af5b696b9b49f3d7018fcaf703" => :yosemite
   end
 
   depends_on "libpng"
