73d1339498afdfe9db2a672b6a083737148f7d31
FX Coudert <fxcoudert@gmail.com>
2017-08-08 17:38:07 +0200

    glassfish: clean up install

--- Formula/glassfish.rb
+++ Formula/glassfish.rb
@@ -3,24 +3,23 @@ class Glassfish < Formula
   homepage "https://glassfish.java.net"
   url "http://download.java.net/glassfish/4.1.2/release/glassfish-4.1.2.zip"
   sha256 "68d5c0d95152a07e68e9b00535b11e7b8727646eb8bca05f918abdadebac7266"
+  revision 1
 
   bottle :unneeded
+
+  depends_on :java => "1.7+"
+
+  conflicts_with "payara", :because => "both install the same scripts"
 
   def install
     rm_rf Dir["bin/*.bat"]
     libexec.install Dir["*", ".org.opensolaris,pkg"]
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   def caveats; <<-EOS.undent
-    The home of GlassFish Application Server 4 is:
-      #{opt_libexec}
-
     You may want to add the following to your .bash_profile:
       export GLASSFISH_HOME=#{opt_libexec}
-      export PATH=${PATH}:${GLASSFISH_HOME}/bin
-
-    Note: The support scripts used by GlassFish Application Server 4
-    are *NOT* linked to bin.
   EOS
   end
 end
