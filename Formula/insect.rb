3b015e0bb2ce83a176e03144bca5fb59195f80dc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-08 22:06:45 +0000

    insect: update 4.9.0 bottle.

--- Formula/insect.rb
+++ Formula/insect.rb
@@ -8,9 +8,9 @@ class Insect < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "dc3d95026270fa0cea8cca524f92405a3d25f181e665c8852f373f43415073dd" => :sierra
-    sha256 "4f6c779f753ab5d31845bc1b22ea7baad3d98667d99a4feaf900017f41e3b423" => :el_capitan
-    sha256 "e905fb7cf8dce1ff269631c63ed46cf7d6c781f60e2cc4662ad2ea81f49cbf10" => :yosemite
+    sha256 "0937ee3986157830d4236f2c8fe7fe5c899987a31655a509ef7c4377c3320f37" => :sierra
+    sha256 "2ed00f6cc4f79444514ed856d728070593d2e8155d9fbb6bca1a22d24bc0cbf3" => :el_capitan
+    sha256 "7fbaa2aa1c505cf26f5a5b991f539a736474ac5aff1b9fdd22c7481ee44656b2" => :yosemite
   end
 
   depends_on "node"
