b5ea67d1630821c46852c0d97403bd5453fd58a2
ilovezfs <ilovezfs@icloud.com>
2017-08-08 21:22:26 -0700

    vim 8.0.0893
    Closes #16562.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -1,8 +1,8 @@
 class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://vim.sourceforge.io/"
-  url "https://github.com/vim/vim/archive/v8.0.0844.tar.gz"
-  sha256 "3f4edbb2f781775269acd3c19cc6ec0493b59e5cfedf1bbdc51ae3fa6c3fffcb"
+  url "https://github.com/vim/vim/archive/v8.0.0893.tar.gz"
+  sha256 "aadfa73607e7595ea355f73fa6671f58782c6c12c31e510c26e192174347afdc"
   head "https://github.com/vim/vim.git"
 
   bottle do
