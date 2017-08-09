8dba5ce2cbfff4d5581389b8c40264068e678b45
FX Coudert <fxcoudert@gmail.com>
2017-08-09 19:19:35 +0200

    armadillo: migrate from homebrew/science

---
+++ Formula/armadillo.rb
@@ -0,0 +1,30 @@
+class Armadillo < Formula
+  desc "C++ linear algebra library"
+  homepage "https://arma.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/arma/armadillo-7.960.1.tar.xz"
+  sha256 "57de6b9eb547f168e5dd2d2736c75b609bb2d1761120f608ff6530d7247082ff"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "arpack"
+  depends_on "hdf5"
+  depends_on "superlu"
+
+  def install
+    system "cmake", ".", "-DDETECT_HDF5=ON", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <iostream>
+      #include <armadillo>
+
+      int main(int argc, char** argv) {
+        std::cout << arma::arma_version::as_string() << std::endl;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-I#{include}", "-L#{lib}", "-larmadillo", "-o", "test"
+    assert_equal `./test`.to_i, version.to_s.to_i
+  end
+end
