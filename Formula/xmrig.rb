aab8718e787e07d845c2964e6db6f227e8981d4b
Raymond Tau <raymondtau@gmail.com>
2017-08-09 20:31:57 +0800

    xmrig 2.3.1 (new formula)
    Closes #17206.
    
    Signed-off-by: JCount <JCount42@gmail.com>

---
+++ Formula/xmrig.rb
@@ -0,0 +1,33 @@
+class Xmrig < Formula
+  desc "Monero (XMR) CPU miner"
+  homepage "https://github.com/xmrig/xmrig"
+  url "https://github.com/xmrig/xmrig/archive/v2.3.1.tar.gz"
+  sha256 "99be5e69732bf2720f3f34a1beb14ba06d440431bc55d308307fdbd1f877f6e5"
+
+  depends_on "cmake" => :build
+  depends_on "libuv"
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", "-DUV_LIBRARY=#{Formula["libuv"].opt_lib}/libuv.a", *std_cmake_args
+      system "make"
+      bin.install "xmrig"
+    end
+  end
+
+  test do
+    require "open3"
+    test_server="donotexist.localhost:65535"
+    timeout=10
+    Open3.popen2e("#{bin}/xmrig", "--no-color", "--max-cpu-usage=1", "--print-time=1",
+                  "--threads=1", "--retries=1", "--url=#{test_server}") do |stdin, stdouts, _wait_thr|
+      start_time=Time.now
+      stdin.close_write
+
+      stdouts.each do |line|
+        assert (Time.now - start_time <= timeout), "No server connect after timeout"
+        break if line.include? "\] \[#{test_server}\] DNS error: \"unknown node or service\""
+      end
+    end
+  end
+end
