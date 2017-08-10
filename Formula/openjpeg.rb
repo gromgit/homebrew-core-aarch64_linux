54409e3f29a5282630e6cbb38c5390dc0c858bb6
ilovezfs <ilovezfs@icloud.com>
2017-08-09 21:24:14 -0700

    openjpeg 2.2.0

--- Formula/openjpeg.rb
+++ Formula/openjpeg.rb
@@ -1,10 +1,8 @@
 class Openjpeg < Formula
   desc "Library for JPEG-2000 image manipulation"
   homepage "http://www.openjpeg.org/"
-  url "https://github.com/uclouvain/openjpeg/archive/v2.1.2.tar.gz"
-  sha256 "4ce77b6ef538ef090d9bde1d5eeff8b3069ab56c4906f083475517c2c023dfa7"
-  revision 1
-
+  url "https://github.com/uclouvain/openjpeg/archive/v2.2.0.tar.gz"
+  sha256 "6fddbce5a618e910e03ad00d66e7fcd09cc6ee307ce69932666d54c73b7c6e7b"
   head "https://github.com/uclouvain/openjpeg.git"
 
   bottle do
@@ -22,15 +20,6 @@ class Openjpeg < Formula
   depends_on "little-cms2"
   depends_on "libtiff"
   depends_on "libpng"
-
-  # https://github.com/uclouvain/openjpeg/issues/862
-  # https://github.com/uclouvain/openjpeg/issues/863
-  patch do
-    url "https://mirrors.ocf.berkeley.edu/debian/pool/main/o/openjpeg2/openjpeg2_2.1.2-1.1.debian.tar.xz"
-    mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/o/openjpeg2/openjpeg2_2.1.2-1.1.debian.tar.xz"
-    sha256 "b19b15ac6306c19734f0626f974c8863e4dc21a1df849a8ae81008479b5b0daf"
-    apply "patches/CVE-2016-9572_CVE-2016-9573.patch"
-  end
 
   def install
     args = std_cmake_args
@@ -55,7 +44,7 @@ class Openjpeg < Formula
         return 0;
       }
     EOS
-    system ENV.cc, "-I#{include}/openjpeg-2.1", "-L#{lib}", "-lopenjp2",
+    system ENV.cc, "-I#{include}/openjpeg-2.2", "-L#{lib}", "-lopenjp2",
            testpath/"test.c", "-o", "test"
     system "./test"
   end
