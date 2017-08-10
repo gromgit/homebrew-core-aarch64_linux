d37099c01a0414555ed168e6809d7aba7377055a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-10 00:17:36 +0000

    hwloc: add 1.11.7 bottle.

--- Formula/hwloc.rb
+++ Formula/hwloc.rb
@@ -4,6 +4,13 @@ class Hwloc < Formula
   url "https://www.open-mpi.org/software/hwloc/v1.11/downloads/hwloc-1.11.7.tar.bz2"
   sha256 "ab6910e248eed8c85d08b529917a6aae706b32b346e886ba830895e36a809729"
 
+  bottle do
+    cellar :any
+    sha256 "3d87ff082922581fecb14af62b50e65b69c314935dd24e7a4750b8108a86e014" => :sierra
+    sha256 "091d23471f53c3e6b5bbbb5f880734fb78a725e1dbb6d12541a0c6630141f68d" => :el_capitan
+    sha256 "45891878bbe669bf29e2248cf73ed8487d35950d382278aba42b10d5a4bf9370" => :yosemite
+  end
+
   head do
     url "https://github.com/open-mpi/hwloc.git"
     depends_on "autoconf" => :build
