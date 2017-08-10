8fda2823a838d226537ae186b996949c89ebf38f
ipopyk <igor.popyk.te@gmail.com>
2017-08-10 15:03:25 +0300

    crowdin 2.0.18

--- Formula/crowdin.rb
+++ Formula/crowdin.rb
@@ -1,8 +1,8 @@
 class Crowdin < Formula
   desc "Command-line tool that allows to manage your resources with crowdin.com"
   homepage "https://support.crowdin.com/cli-tool/"
-  url "https://crowdin.com/downloads/crowdin-cli-2.0.17.zip"
-  sha256 "9f777b83831e40d1d6a587b8379fbd60b5d9f7377dd055913c1cd8e20aa9fec2"
+  url "https://crowdin.com/downloads/crowdin-cli-2.0.18.zip"
+  sha256 "4af37ef20a65157096698477af9c803f625f461611ac4fcd1fdc9c0bd1f939fc"
 
   bottle :unneeded
 
