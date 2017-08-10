b6205be5ec551dbd4dd29fed75b137fd070a4f6f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-10 20:54:35 +0000

    libpq: add 9.6.3 bottle.

--- Formula/libpq.rb
+++ Formula/libpq.rb
@@ -4,6 +4,12 @@ class Libpq < Formula
   url "https://ftp.postgresql.org/pub/source/v9.6.3/postgresql-9.6.3.tar.bz2"
   sha256 "1645b3736901f6d854e695a937389e68ff2066ce0cde9d73919d6ab7c995b9c6"
 
+  bottle do
+    sha256 "fc7ee090e7864661f5f0d77a4d148ee2727ed48bb68ea1f6543ec2cbe60c7f71" => :sierra
+    sha256 "43b9913b52fb7798ccece25a406e771f6dc544c72de173a33b8abf7e26f9b65a" => :el_capitan
+    sha256 "4083597f56940a57df6a28ec1f27d428d8acca30dd61b284a6e53752acd82ca5" => :yosemite
+  end
+
   keg_only "conflicts with postgres formula"
 
   depends_on "openssl"
