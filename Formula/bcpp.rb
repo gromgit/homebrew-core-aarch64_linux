aaf8ec2b1ab41e191517ebfa1d037d940af1d980
Viktor Szakats <vszakats@users.noreply.github.com>
2017-08-10 11:10:21 +0200

    bcpp: secure url(s)

--- Formula/bcpp.rb
+++ Formula/bcpp.rb
@@ -1,7 +1,7 @@
 class Bcpp < Formula
   desc "C(++) beautifier"
-  homepage "http://invisible-island.net/bcpp/"
-  url "ftp://invisible-island.net/bcpp/bcpp-20131209.tgz"
+  homepage "https://invisible-island.net/bcpp/"
+  url "ftp://ftp.invisible-island.net/bcpp/bcpp-20131209.tgz"
   sha256 "4732d606c9b5857d0ea2cee2f2b54eb3b8928f547f8a0c6b89096b674c6bd7f7"
 
   bottle do
