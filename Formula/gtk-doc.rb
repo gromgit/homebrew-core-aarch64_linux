7434387c56ed680f68eb31df8280c8399a0cd1a9
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-08-11 16:58:59 +0100

    gtk-doc 1.26
    Closes #16687.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gtk-doc.rb
+++ Formula/gtk-doc.rb
@@ -1,8 +1,8 @@
 class GtkDoc < Formula
   desc "GTK+ documentation tool"
   homepage "https://www.gtk.org/gtk-doc/"
-  url "https://download.gnome.org/sources/gtk-doc/1.25/gtk-doc-1.25.tar.xz"
-  sha256 "1ea46ed400e6501f975acaafea31479cea8f32f911dca4dff036f59e6464fd42"
+  url "https://download.gnome.org/sources/gtk-doc/1.26/gtk-doc-1.26.tar.xz"
+  sha256 "bff3f44467b1d39775e94fad545f050faa7e8d68dc6a31aef5024ba3c2d7f2b7"
 
   bottle do
     cellar :any_skip_relocation
@@ -13,17 +13,24 @@ class GtkDoc < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on "gnome-doc-utils" => :build
   depends_on "itstool" => :build
   depends_on "gettext"
-  depends_on "glib"
   depends_on "docbook"
   depends_on "docbook-xsl"
   depends_on "libxml2"
-  depends_on :perl => "5.18" if MacOS.version <= :mavericks
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/b3/b2/238e2590826bfdd113244a40d9d3eb26918bd798fc187e2360a8367068db/six-1.10.0.tar.gz"
+    sha256 "105f8d68616f8248e24bf0e9372ef04d3cc10104f1980f54d57b2ce73a5ad56a"
+  end
 
   def install
     ENV.append_path "PYTHONPATH", "#{Formula["libxml2"].opt_lib}/python2.7/site-packages"
+
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+    resource("six").stage do
+      system "python", *Language::Python.setup_install_args(libexec/"vendor")
+    end
 
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
@@ -31,6 +38,8 @@ class GtkDoc < Formula
                           "--with-xml-catalog=#{etc}/xml/catalog"
     system "make"
     system "make", "install"
+
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
   test do
