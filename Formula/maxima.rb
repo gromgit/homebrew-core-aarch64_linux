f9dab58ea2c6a92817ea03dd78392863eb779e9a
FX Coudert <fxcoudert@gmail.com>
2017-08-11 23:14:35 +0200

    maxima: migrate from homebrew/science
    Closes #16744.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/maxima.rb
@@ -0,0 +1,28 @@
+class Maxima < Formula
+  desc "Computer algebra system"
+  homepage "https://maxima.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/maxima/Maxima-source/5.38.1-source/maxima-5.38.1.tar.gz"
+  sha256 "0e866536ab5847ec045ba013570f80f36206ca6ce07a5d13987010bcb321c6dc"
+
+  depends_on "sbcl" => :build
+  depends_on "gettext"
+  depends_on "gnuplot"
+  depends_on "rlwrap"
+
+  def install
+    ENV["LANG"] = "C" # per build instructions
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--enable-gettext",
+                          "--enable-sbcl",
+                          "--enable-sbcl-exec",
+                          "--with-sbcl=#{Formula["sbcl"].opt_bin}/sbcl"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/maxima", "--batch-string=run_testsuite(); quit();"
+  end
+end
