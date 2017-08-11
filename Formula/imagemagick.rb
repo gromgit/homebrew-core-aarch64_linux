6da60589db846d6be41b19ffa15f3a2ae8d25ef3
Zhiming Wang <zmwangx@gmail.com>
2017-08-11 08:15:58 -0400

    imagemagick 7.0.6-6
    Closes #16682.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -4,11 +4,10 @@ class Imagemagick < Formula
   # Please always keep the Homebrew mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://dl.bintray.com/homebrew/mirror/imagemagick-7.0.6-5.tar.xz"
-  mirror "https://www.imagemagick.org/download/ImageMagick-7.0.6-5.tar.xz"
-  sha256 "5dbcd45b67514fe1981703759d008f9aa2c6b6201917679dfd35776c904e51b2"
-  revision 2
-  head "http://git.imagemagick.org/repos/ImageMagick.git"
+  url "https://dl.bintray.com/homebrew/mirror/imagemagick-7.0.6-6.tar.xz"
+  mirror "https://www.imagemagick.org/download/ImageMagick-7.0.6-6.tar.xz"
+  sha256 "2c20d4d2d822fdafc6312af95a8ae0ec2256f35316efdde217522e9668c397f0"
+  head "https://github.com/ImageMagick/ImageMagick.git"
 
   bottle do
     sha256 "3ef42cac59d3e7aaadd2ddd5e109526d0f2588a1feca35ac91d98480080a89fe" => :sierra
