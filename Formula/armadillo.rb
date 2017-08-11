eb8e8c5ef6d4a16eadd07a5d403095e64402990d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-11 12:51:29 +0000

    armadillo: add 7.960.1 bottle.

--- Formula/armadillo.rb
+++ Formula/armadillo.rb
@@ -4,6 +4,13 @@ class Armadillo < Formula
   url "https://downloads.sourceforge.net/project/arma/armadillo-7.960.1.tar.xz"
   sha256 "57de6b9eb547f168e5dd2d2736c75b609bb2d1761120f608ff6530d7247082ff"
 
+  bottle do
+    cellar :any
+    sha256 "ce59f8055dec6ac44e427ee8ae2a62dd4069e7800edf6241238e36c6be770386" => :sierra
+    sha256 "6a1344f69abef546eedb9e5d4c8417514cd4f5ed8233747fe63bd12361b1bfcb" => :el_capitan
+    sha256 "6b78c0bcb68bde9943018f2864277f9ef6c40381c2a4ed2fa690871e8277991e" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "arpack"
