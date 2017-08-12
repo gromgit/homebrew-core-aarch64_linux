d7e19f6fb8ca8610af118f641fe476263ec1edbb
ilovezfs <ilovezfs@icloud.com>
2017-08-12 06:33:50 -0700

    couchdb: revision for icu4c

--- Formula/couchdb.rb
+++ Formula/couchdb.rb
@@ -1,7 +1,7 @@
 class Couchdb < Formula
   desc "Document database server"
   homepage "https://couchdb.apache.org/"
-  revision 11
+  revision 12
 
   stable do
     url "https://www.apache.org/dyn/closer.cgi?path=/couchdb/source/1.6.1/apache-couchdb-1.6.1.tar.gz"
