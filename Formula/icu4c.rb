1c6a390c39f7b7cf837e31c6e07f0fca972953eb
ilovezfs <ilovezfs@icloud.com>
2017-08-12 04:50:28 -0700

    icu4c 59.1
    always builds with C++11 so remove the option

--- Formula/icu4c.rb
+++ Formula/icu4c.rb
@@ -1,11 +1,11 @@
 class Icu4c < Formula
   desc "C/C++ and Java libraries for Unicode and globalization"
   homepage "http://site.icu-project.org/"
-  url "https://ssl.icu-project.org/files/icu4c/58.2/icu4c-58_2-src.tgz"
-  mirror "https://fossies.org/linux/misc/icu4c-58_2-src.tgz"
-  mirror "https://downloads.sourceforge.net/project/icu/ICU4C/58.2/icu4c-58_2-src.tgz"
-  version "58.2"
-  sha256 "2b0a4410153a9b20de0e20c7d8b66049a72aef244b53683d0d7521371683da0c"
+  url "https://ssl.icu-project.org/files/icu4c/59.1/icu4c-59_1-src.tgz"
+  mirror "https://fossies.org/linux/misc/icu4c-59_1-src.tgz"
+  mirror "https://downloads.sourceforge.net/project/icu/ICU4C/59.1/icu4c-59_1-src.tgz"
+  version "59.1"
+  sha256 "7132fdaf9379429d004005217f10e00b7d2319d0fea22bdfddef8991c45b75fe"
   head "https://ssl.icu-project.org/repos/icu/trunk/icu4c/", :using => :svn
 
   bottle do
@@ -17,11 +17,7 @@ class Icu4c < Formula
 
   keg_only :provided_by_osx, "macOS provides libicucore.dylib (but nothing else)"
 
-  option :cxx11
-
   def install
-    ENV.cxx11 if build.cxx11?
-
     args = %W[--prefix=#{prefix} --disable-samples --disable-tests --enable-static]
     args << "--with-library-bits=64" if MacOS.prefer_64_bit?
 
