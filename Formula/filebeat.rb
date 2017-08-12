543d8e9b27b9bb25ce4491773684c30c8cb66dcc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-12 14:33:35 +0000

    filebeat: update 5.5.1_1 bottle.

--- Formula/filebeat.rb
+++ Formula/filebeat.rb
@@ -8,9 +8,9 @@ class Filebeat < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "1f93032a00185167df3d27c363fd7fd11558a56c922b1bd76e174044984af3e4" => :sierra
-    sha256 "84b48aef5745312f307bf620429fc27277c0336ca50b3006b01372ac541cce5c" => :el_capitan
-    sha256 "646d1a08e85fe724f82fc7bcf5d536dd08c8d66fa71a954ed71519effedda144" => :yosemite
+    sha256 "2121385fe5c8a70582ed98e5083675ecb7fb5ccc8a2453565a73c3750e37c60d" => :sierra
+    sha256 "384f49b2b9569f51a73861c6e5a89bb17293c62b5bf2282eb935d64013a6cd38" => :el_capitan
+    sha256 "8a3fde162bac47ec335d4f10c3bbb20d3d6f02993f2ceee5a85a8b81c19152e9" => :yosemite
   end
 
   depends_on "go" => :build
