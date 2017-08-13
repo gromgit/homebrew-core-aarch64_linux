fa175fcebac93003b117a63afcae39144aaa55bd
FX Coudert <fxcoudert@gmail.com>
2017-08-13 22:48:05 +0200

    gcc@4.9: remove --without-multilib

--- Formula/gcc@4.9.rb
+++ Formula/gcc@4.9.rb
@@ -37,14 +37,11 @@ class GccAT49 < Formula
   option "with-all-languages", "Enable all compilers and languages, except Ada"
   option "with-nls", "Build with native language support (localization)"
   option "with-profiled-build", "Make use of profile guided optimization when bootstrapping GCC"
-  # enabling multilib on a host that can't run 64-bit results in build failures
-  option "without-multilib", "Build without multilib support" if MacOS.prefer_64_bit?
 
   deprecated_option "enable-java" => "with-java"
   deprecated_option "enable-all-languages" => "with-all-languages"
   deprecated_option "enable-nls" => "with-nls"
   deprecated_option "enable-profiled-build" => "with-profiled-build"
-  deprecated_option "disable-multilib" => "without-multilib"
 
   depends_on "gmp@4"
   depends_on "libmpc@0.8"
@@ -118,10 +115,10 @@ class GccAT49 < Formula
       args << "--with-ecj-jar=#{Formula["ecj"].opt_prefix}/share/java/ecj.jar"
     end
 
-    if !MacOS.prefer_64_bit? || build.without?("multilib")
-      args << "--disable-multilib"
-    else
+    if MacOS.prefer_64_bit?
       args << "--enable-multilib"
+    else
+      args << "--disable-multilib"
     end
 
     # Ensure correct install names when linking against libgcc_s;
