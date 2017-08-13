6b658de401f0f0d546b332c84f030797c9883a62
Juan Cruz Viotti <jv@jviotti.com>
2017-08-12 20:35:04 -0400

    duplicity: downgrade dropbox to 6.9.0
    The current duplicity version is incompatible with the latest dropbox
    python packages, resulting in the following error when performing a
    Dropbox backup:
    
    ```
    error "expected request_binary as binary type, got <type 'file'>"
    ```
    
    This commit also updates `cffi`, since it seems the formula is not
    building correctly at the moment, independently on the Dropbox issue
    mentioned before.
    
    See: http://nongnu.13855.n7.nabble.com/Dropbox-backend-error-on-backup-td217454.html
    
    Closes #16735.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/duplicity.rb
+++ Formula/duplicity.rb
@@ -5,6 +5,7 @@ class Duplicity < Formula
   homepage "http://www.nongnu.org/duplicity/"
   url "https://launchpad.net/duplicity/0.7-series/0.7.12/+download/duplicity-0.7.12.tar.gz"
   sha256 "11cbad44a90891af1bf9e294260ba7c21a1660ccd3ab2c6e736ba74ac5cf0fe6"
+  revision 1
 
   bottle do
     cellar :any
@@ -59,8 +60,8 @@ class Duplicity < Formula
   end
 
   resource "cffi" do
-    url "https://files.pythonhosted.org/packages/a1/32/e3d6c3a8b5461b903651dd6ce958ed03c093d2e00128e3f33ea69f1d7965/cffi-1.9.1.tar.gz"
-    sha256 "563e0bd53fda03c151573217b3a49b3abad8813de9dd0632e10090f6190fdaf8"
+    url "https://files.pythonhosted.org/packages/5b/b9/790f8eafcdab455bcd3bd908161f802c9ce5adbf702a83aa7712fcc345b7/cffi-1.10.0.tar.gz"
+    sha256 "b3b02911eb1f6ada203b0763ba924234629b51586f72a21faacc638269f4ced5"
   end
 
   resource "cryptography" do
@@ -74,8 +75,8 @@ class Duplicity < Formula
   end
 
   resource "dropbox" do
-    url "https://files.pythonhosted.org/packages/19/a2/690e468e815cb80175677d2d1990e81367c641f75da4bf35461315a27936/dropbox-7.1.1.tar.gz"
-    sha256 "8f362c443b50da516bb42b291e655390331b2005da1fae70898697c3d0127fdc"
+    url "https://files.pythonhosted.org/packages/1f/7d/6e90b169fe4142b3b2332fd22d2aecfdc971e42eef19ea1c50b2384067f2/dropbox-6.9.0.tar.gz"
+    sha256 "db1be6dab980f6819508c9f70d2c82b32aad4b7f5f0c52354fdb48ca4abdee49"
   end
 
   resource "enum34" do
