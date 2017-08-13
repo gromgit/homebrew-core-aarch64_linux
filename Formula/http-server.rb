c1a68c3d844f94c76b12a3040dce16056583f609
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-13 20:26:41 +0000

    http-server: add 0.10.0 bottle.

--- Formula/http-server.rb
+++ Formula/http-server.rb
@@ -7,6 +7,13 @@ class HttpServer < Formula
   sha256 "cf7bde2def672698f78463ce8bec54dfe6392c24552f2b1c5e2e5ce94428de23"
   head "https://github.com/indexzero/http-server.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "232e9b864df77fb42296b9c0a065ae998d47c227700b6f969344228df2ceea5d" => :sierra
+    sha256 "19539364bfc94381abc86b2920bd2400bd6ea25e25f782a3491f51cfd0b2f632" => :el_capitan
+    sha256 "63dc26f7b77915ed23cb96bdbbf23ecc9d3dbd6ac5fc72c168de8d1fa9217e83" => :yosemite
+  end
+
   depends_on "node"
 
   def install
