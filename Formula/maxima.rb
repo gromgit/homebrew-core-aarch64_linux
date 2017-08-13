96403bc0f7609b9850e71f84b9107ede59c1f70b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-13 10:16:34 +0000

    maxima: add 5.38.1 bottle.

--- Formula/maxima.rb
+++ Formula/maxima.rb
@@ -4,6 +4,12 @@ class Maxima < Formula
   url "https://downloads.sourceforge.net/project/maxima/Maxima-source/5.38.1-source/maxima-5.38.1.tar.gz"
   sha256 "0e866536ab5847ec045ba013570f80f36206ca6ce07a5d13987010bcb321c6dc"
 
+  bottle do
+    sha256 "0a1c186375b35420a55590187655ac4a0160c8faf76750f4b156febf25665c4e" => :sierra
+    sha256 "5c9f091e0d6993bffb750378076d5d05fe525fe2dafd7af85652c421e840f814" => :el_capitan
+    sha256 "96e042b2ff652ffdbc1b10236a119240bdf6ae5c29389c7d3e9f01ee3721aa7e" => :yosemite
+  end
+
   depends_on "sbcl" => :build
   depends_on "gettext"
   depends_on "gnuplot"
