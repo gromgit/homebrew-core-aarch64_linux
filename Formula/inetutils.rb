63598892d56c62b5acd04e1bebb5100eac8f44e8
Chongyu Zhu <i@lembacon.com>
2017-08-15 03:23:42 +0800

    inetutils: ftp is also missing from macOS High Sierra
    Closes #16793.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/inetutils.rb
+++ Formula/inetutils.rb
@@ -30,7 +30,7 @@ class Inetutils < Formula
     if build.without? "default-names"
       # Binaries not shadowing macOS utils symlinked without 'g' prefix
       noshadow = %w[dnsdomainname rcp rexec rlogin rsh]
-      noshadow << "telnet" if MacOS.version >= :high_sierra
+      noshadow += %w[ftp telnet] if MacOS.version >= :high_sierra
       noshadow.each do |cmd|
         bin.install_symlink "g#{cmd}" => cmd
         man1.install_symlink "g#{cmd}.1" => "#{cmd}.1"
@@ -39,7 +39,7 @@ class Inetutils < Formula
       # Symlink commands without 'g' prefix into libexec/gnubin and
       # man pages into libexec/gnuman
       bin.find.each do |path|
-        next unless File.executable?(path)
+        next unless File.executable?(path) && !File.directory?(path)
         cmd = path.basename.to_s.sub(/^g/, "")
         (libexec/"gnubin").install_symlink bin/"g#{cmd}" => cmd
         (libexec/"gnuman"/"man1").install_symlink man1/"g#{cmd}" => cmd
