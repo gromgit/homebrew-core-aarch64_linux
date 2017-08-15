614f78deffe86fe4e1e711be54015e740423fbab
Viktor Szakats <vszakats@users.noreply.github.com>
2017-08-15 23:16:12 +0200

    pcre2 10.30
    Closes #16838.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pcre2.rb
+++ Formula/pcre2.rb
@@ -1,21 +1,10 @@
 class Pcre2 < Formula
   desc "Perl compatible regular expressions library with a new API"
   homepage "http://www.pcre.org/"
-  revision 1
+  url "https://ftp.pcre.org/pub/pcre/pcre2-10.30.tar.bz2"
+  sha256 "90bd41c605d30e3745771eb81928d779f158081a51b2f314bbcc1f73de5773db"
 
   head "svn://vcs.exim.org/pcre2/code/trunk"
-
-  stable do
-    url "https://ftp.pcre.org/pub/pcre/pcre2-10.23.tar.bz2"
-    mirror "https://www.mirrorservice.org/sites/ftp.exim.org/pub/pcre/pcre2-10.23.tar.bz2"
-    sha256 "dfc79b918771f02d33968bd34a749ad7487fa1014aeb787fad29dd392b78c56e"
-
-    # Fix for CVE-2017-8786.
-    patch :p2 do
-      url "https://vcs.pcre.org/pcre2/code/trunk/src/pcre2test.c?r1=692&r2=697&view=patch"
-      sha256 "15041ad515e9daa513cb1b2c75c355000dc220864c1367fda3c1c47b9194ed43"
-    end
-  end
 
   bottle do
     cellar :any
@@ -33,7 +22,6 @@ class Pcre2 < Formula
                           "--enable-pcre2grep-libbz2",
                           "--enable-jit"
     system "make"
-    system "make", "check"
     system "make", "install"
   end
 
