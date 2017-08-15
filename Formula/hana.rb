cae9cff641f7f84a21ba351d728350c2a99db38a
JCount <JCount42@gmail.com>
2017-08-15 15:16:23 -0400

    hana 1.2.0
    Closes #16830.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/hana.rb
+++ Formula/hana.rb
@@ -1,8 +1,8 @@
 class Hana < Formula
   desc "The Boost.Hana C++14 metaprogramming library"
   homepage "https://github.com/boostorg/hana"
-  url "https://github.com/boostorg/hana/archive/v1.1.0.tar.gz"
-  sha256 "bb33358842300524fbac61710850a349771ae125fba16f3c39a047a15c9bcfb9"
+  url "https://github.com/boostorg/hana/archive/v1.2.0.tar.gz"
+  sha256 "ebee14728839ef71803eeefecd8ee514833a784a38cf2e0ab3c214f618cd1e26"
   head "https://github.com/boostorg/hana.git", :branch => "develop"
 
   bottle do
