5661b86b44e745c666eefc2890193cc5178adabc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-15 14:19:29 +0000

    libosinfo: update 1.1.0 bottle.

--- Formula/libosinfo.rb
+++ Formula/libosinfo.rb
@@ -5,10 +5,9 @@ class Libosinfo < Formula
   sha256 "600f43a4a8dae5086a01a3d44bcac2092b5fa1695121289806d544fb287d3136"
 
   bottle do
-    rebuild 1
-    sha256 "a47562567ab29bd73b7f8412c66e744092102fcfa9919fff6659adfb5149b2f6" => :sierra
-    sha256 "657ba115e5432c8ab9e1262abd8b759fa847d8cded4249ee7681c0a73cddb5fd" => :el_capitan
-    sha256 "f15d2fccb9db83969e14a854ec1e3c8c1a4a78ba5e47e638622abcb0fe38edf8" => :yosemite
+    sha256 "764ecf89e19d39bf7e6d15d41d22d466893afa462f475582a445ac622aa024f1" => :sierra
+    sha256 "d42abc1c79c3bf147bddcada54f1720bca29b18fa573f1d0dbf250ee894bcd97" => :el_capitan
+    sha256 "6f2ce2d0cb0725f846644a95bc4701330c1f920d4e1eb3c0683829c2baf5a937" => :yosemite
   end
 
   depends_on "intltool" => :build
