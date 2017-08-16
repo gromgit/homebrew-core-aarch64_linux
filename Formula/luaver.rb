d0191369d99a97d4d6d683781f7976869fa5e2a2
Jared Sartin <jaredsartin@gmail.com>
2017-08-16 14:34:08 -0700

    luaver 1.1.0 (#16839)

--- Formula/luaver.rb
+++ Formula/luaver.rb
@@ -1,17 +1,9 @@
 class Luaver < Formula
   desc "Manage and switch between versions of Lua, LuaJIT, and Luarocks"
   homepage "https://github.com/DhavalKapil/luaver"
+  url "https://github.com/DhavalKapil/luaver/archive/v1.1.0.tar.gz"
+  sha256 "441b1b72818889593d15a035807c95321118ac34270da49cf8d5d64f5f2e486d"
   head "https://github.com/DhavalKapil/luaver.git"
-
-  stable do
-    url "https://github.com/DhavalKapil/luaver/archive/v1.0.0.tar.gz"
-    sha256 "5c7eb65ea9b3cb4f10987c1d564e6e86bd6ad4f9d829a799121c83f0d85bd390"
-
-    patch do
-      url "https://github.com/DhavalKapil/luaver/pull/9.patch?full_index=1"
-      sha256 "0fa92083eabc2e9cb369c3aefbcf17cee15442ad8c22b18cbe6e2a39e226ca8e"
-    end
-  end
 
   bottle :unneeded
 
@@ -30,13 +22,13 @@ class Luaver < Formula
   test do
     lua_versions = %w[5.3.3 5.2.4 5.1.5]
     lua_versions.each do |v|
-      ENV.deparallelize { system ". #{bin}/luaver && luaver install #{v} < /dev/null" }
-      system ". #{bin}/luaver && luaver use #{v} && lua -v"
+      ENV.deparallelize { system ". #{bin}/luaver install #{v} < /dev/null" }
+      system ". #{bin}/luaver use #{v} && lua -v"
     end
     luajit_versions = %w[2.0.4]
     luajit_versions.each do |v|
-      system ". #{bin}/luaver && luaver install-luajit #{v} < /dev/null"
-      system ". #{bin}/luaver && luaver use-luajit #{v} && luajit -v"
+      system ". #{bin}/luaver install-luajit #{v} < /dev/null"
+      system ". #{bin}/luaver use-luajit #{v} && luajit -v"
     end
   end
 end
