42cdaa672cf07fd86e75fa932743b09e47099dbf
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-16 10:29:51 +0000

    osmium-tool: update 1.7.0 bottle.

--- Formula/osmium-tool.rb
+++ Formula/osmium-tool.rb
@@ -6,9 +6,9 @@ class OsmiumTool < Formula
 
   bottle do
     cellar :any
-    sha256 "842c6ca8f0756d1e445f525c6e0cc87f7b4610071e4f4eaba657fde765612fc1" => :sierra
-    sha256 "292194bdcc7b14c4757e082f9b6078da2900001da4d6ba190734fed529e97981" => :el_capitan
-    sha256 "a579e794efd4350c9ec9a53009854da32334f291918560dc2fcac809ff23dafa" => :yosemite
+    sha256 "49b71d3333ab369aaebeff89105c79559e7bd3c743c5213ab7a0e04bc82b1787" => :sierra
+    sha256 "1994ded450727df891726f316421d6f8956e0f757678446fea16ca76d7d0a9bb" => :el_capitan
+    sha256 "b7949426174e8f4b18a15412bb588b9a698af0065e0e1d0e4e78752d2d02f171" => :yosemite
   end
 
   depends_on "cmake" => :build
