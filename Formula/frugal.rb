555dfa417959555c56a1f65451a79301bf41b6cc
Charlie Strawn <charlie.strawn@workiva.com>
2017-08-17 16:54:34 -0500

    frugal 2.8.1 (new formula)
    Closes #16922.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/frugal.rb
@@ -0,0 +1,25 @@
+class Frugal < Formula
+  desc "Cross language code generator for creating scalable microservices"
+  homepage "https://github.com/Workiva/frugal"
+  url "https://github.com/Workiva/frugal/archive/2.8.1.tar.gz"
+  sha256 "7bcbecee6cc908ed58f3dc7963b101eda43cbe9ab139d655d8c4e0dd670f06ce"
+
+  depends_on "go" => :build
+  depends_on "godep" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/Workiva/frugal").install buildpath.children
+    cd buildpath/"src/github.com/Workiva/frugal" do
+      system "godep", "restore"
+      system "go", "build", "-o", bin/"frugal"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    (testpath/"test.frugal").write("typedef double Test")
+    system "#{bin}/frugal", "--gen", "go", "test.frugal"
+    assert_match "type Test float64", (testpath/"gen-go/test/f_types.go").read
+  end
+end
