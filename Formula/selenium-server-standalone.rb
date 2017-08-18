6b333d68c8424628ec97701daa3cca7a87a76d45
François JACQUES <hypnoce@donarproject.org>
2017-08-18 18:05:46 +0200

    selenium-server-standalone 3.5.1 (#16938)

--- Formula/selenium-server-standalone.rb
+++ Formula/selenium-server-standalone.rb
@@ -1,8 +1,8 @@
 class SeleniumServerStandalone < Formula
   desc "Browser automation for testing purposes"
   homepage "http://seleniumhq.org/"
-  url "https://selenium-release.storage.googleapis.com/3.4/selenium-server-standalone-3.4.0.jar"
-  sha256 "21cbbd775678821b6b72c208b8d59664a4c7381b3c50b008b331914d2834ec8d"
+  url "https://selenium-release.storage.googleapis.com/3.5/selenium-server-standalone-3.5.1.jar"
+  sha256 "d63bbb22feb0e67dbb2ea751cbb85ddc4061c542e3da8346ccb479725eb22d8d"
 
   bottle :unneeded
 
