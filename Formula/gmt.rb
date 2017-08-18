a2db296387a53390159a2ce0db985da433b38678
FX Coudert <fxcoudert@gmail.com>
2017-08-19 01:00:36 +0200

    gmt: migrate from homebrew/science
    Closes #16969.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/gmt.rb
@@ -0,0 +1,64 @@
+class Gmt < Formula
+  desc "Tools for processing and displaying xy and xyz datasets"
+  homepage "https://gmt.soest.hawaii.edu/"
+  url "ftp://ftp.soest.hawaii.edu/gmt/gmt-5.4.2-src.tar.xz"
+  mirror "ftp://ftp.star.nesdis.noaa.gov/pub/sod/lsa/gmt/gmt-5.4.2-src.tar.xz"
+  mirror "ftp://gd.tuwien.ac.at/pub/gmt/gmt-5.4.2-src.tar.xz"
+  mirror "ftp://ftp.iris.washington.edu/pub/gmt/gmt-5.4.2-src.tar.xz"
+  sha256 "ddcd63094aeda5a60f541626ed7ab4a78538d52dea24ba915f168e4606e587f5"
+
+  depends_on "cmake" => :build
+  depends_on "fftw"
+  depends_on "gdal"
+  depends_on "netcdf"
+  depends_on "pcre"
+
+  resource "gshhg" do
+    url "ftp://ftp.soest.hawaii.edu/gmt/gshhg-gmt-2.3.7.tar.gz"
+    mirror "ftp://ftp.star.nesdis.noaa.gov/pub/sod/lsa/gmt/gshhg-gmt-2.3.7.tar.gz"
+    mirror "ftp://gd.tuwien.ac.at/pub/gmt/gshhg-gmt-2.3.7.tar.gz"
+    mirror "ftp://ftp.iris.washington.edu/pub/gmt/gshhg-gmt-2.3.7.tar.gz"
+    sha256 "9bb1a956fca0718c083bef842e625797535a00ce81f175df08b042c2a92cfe7f"
+  end
+
+  resource "dcw" do
+    url "ftp://ftp.soest.hawaii.edu/gmt/dcw-gmt-1.1.2.tar.gz"
+    mirror "ftp://ftp.star.nesdis.noaa.gov/pub/sod/lsa/gmt/dcw-gmt-1.1.2.tar.gz"
+    mirror "ftp://gd.tuwien.ac.at/pub/gmt/dcw-gmt-1.1.2.tar.gz"
+    mirror "ftp://ftp.iris.washington.edu/pub/gmt/dcw-gmt-1.1.2.tar.gz"
+    sha256 "f719054f8d657e7b10b5182d4c15bc7f38ef7483ed05cdaa9f94ab1a0008bfb6"
+  end
+
+  def install
+    (buildpath/"gshhg").install resource("gshhg")
+    (buildpath/"dcw").install resource("dcw")
+
+    args = std_cmake_args.concat %W[
+      -DCMAKE_INSTALL_PREFIX=#{prefix}
+      -DGMT_DOCDIR=#{share}/doc/gmt
+      -DGMT_MANDIR=#{man}
+      -DGSHHG_ROOT=#{buildpath}/gshhg
+      -DCOPY_GSHHG:BOOL=TRUE
+      -DDCW_ROOT=#{buildpath}/dcw
+      -DCOPY_DCW:BOOL=TRUE
+      -DFFTW3_ROOT=#{Formula["fftw"].opt_prefix}
+      -DGDAL_ROOT=#{Formula["gdal"].opt_prefix}
+      -DNETCDF_ROOT=#{Formula["netcdf"].opt_prefix}
+      -DPCRE_ROOT=#{Formula["pcre"].opt_prefix}
+      -DFLOCK:BOOL=TRUE
+      -DGMT_INSTALL_MODULE_LINKS:BOOL=TRUE
+      -DGMT_INSTALL_TRADITIONAL_FOLDERNAMES:BOOL=FALSE
+      -DLICENSE_RESTRICTED:BOOL=FALSE
+    ]
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "#{bin}/pscoast -R0/360/-70/70 -Jm1.2e-2i -Ba60f30/a30f15 -Dc -G240 -W1/0 -P > test.ps"
+    assert File.exist? "test.ps"
+  end
+end
