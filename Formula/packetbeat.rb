410d500aa48a19557c5973fa22201c23ec59fbbc
Jason Tedor <jason@tedor.me>
2017-08-18 08:05:27 -0400

    packetbeat 5.5.2
    Closes #16949.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/packetbeat.rb
+++ Formula/packetbeat.rb
@@ -1,8 +1,8 @@
 class Packetbeat < Formula
   desc "Lightweight Shipper for Network Data"
   homepage "https://www.elastic.co/products/beats/packetbeat"
-  url "https://github.com/elastic/beats/archive/v5.5.1.tar.gz"
-  sha256 "b6c85901b1feb0e184dd56d9012ccda10bf62566ddfbe3d9790c771b73db3a46"
+  url "https://github.com/elastic/beats/archive/v5.5.2.tar.gz"
+  sha256 "39e792324a35fe84ef9a63cd5324252bc71d1c665188e8d597e12ca170cfde7a"
 
   head "https://github.com/elastic/beats.git"
 
