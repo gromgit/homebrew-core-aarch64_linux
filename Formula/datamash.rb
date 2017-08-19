f21462972ce37193044ce1a721a71aefa3704f20
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-19 21:28:25 +0000

    datamash: add 1.1.1 bottle.

--- Formula/datamash.rb
+++ Formula/datamash.rb
@@ -5,6 +5,13 @@ class Datamash < Formula
   mirror "https://ftp.gnu.org/gnu/datamash/datamash-1.1.1.tar.gz"
   sha256 "420819b3d7372ee3ce704add847cff7d08c4f8176c1d48735d4a632410bb801b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6b0e4f5e26b38b4db687a3e9eaeb103ef580461a40ddbc31af1eaf0758d54efa" => :sierra
+    sha256 "296cf7d1f291f1ffb0f6f877d1d1a7724877195f0a53e7abdeb7b91389742757" => :el_capitan
+    sha256 "734d31b032f5dc7ed2968c20d04938f9b028ed3e3b9587efc548bdfba2d589ce" => :yosemite
+  end
+
   head do
     url "git://git.savannah.gnu.org/datamash.git"
     depends_on "autoconf" => :build
