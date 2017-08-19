ebf2dcc69398d1873387186a53233b056543c109
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-19 21:58:29 +0000

    pcb: add 4.0.1 bottle.

--- Formula/pcb.rb
+++ Formula/pcb.rb
@@ -3,6 +3,12 @@ class Pcb < Formula
   homepage "http://pcb.geda-project.org/"
   url "https://downloads.sourceforge.net/project/pcb/pcb/pcb-4.0.1/pcb-4.0.1.tar.gz"
   sha256 "5d1bd189d8f9b362ac0890ea7ce826e77e61f724439d83c9f854f9c782ca7ef5"
+  bottle do
+    sha256 "2662ca0773a1a10c2e1e203c9186ddb066659b94d854e079e45853aaf3e21aea" => :sierra
+    sha256 "7662578205878b0c46db601619a5370db18f1c9abbb5111e28c5c52e2aa21c44" => :el_capitan
+    sha256 "b5c1c8ce425cfc22d328254e03bf40b463521577f055c182f3807f5bcf9db033" => :yosemite
+  end
+
   version_scheme 1
 
   head do
