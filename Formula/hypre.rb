c437f3560ee781acb0539a32e0dded2a66ebb987
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-19 11:15:00 +0000

    hypre: add 2.11.2 bottle.

--- Formula/hypre.rb
+++ Formula/hypre.rb
@@ -5,6 +5,13 @@ class Hypre < Formula
   sha256 "25b6c1226411593f71bb5cf3891431afaa8c3fd487bdfe4faeeb55c6fdfb269e"
   head "https://github.com/LLNL/hypre.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "541fcb1adc778e587e3f8993bdf7d6c7fabf026ed2256af0e8a4fc64d831f9ec" => :sierra
+    sha256 "a610e8ee47f8962ff959e8223d44f87320303144690458550264bceb016464cc" => :el_capitan
+    sha256 "e228abcdbeeed01a30b13f1471cb848d309fc306a861d745338b71451374e032" => :yosemite
+  end
+
   depends_on "veclibfort"
   depends_on :fortran
   depends_on :mpi => [:cc, :cxx, :f90, :f77]
