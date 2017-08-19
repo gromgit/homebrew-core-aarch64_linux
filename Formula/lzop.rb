21c093791672be265f1ea8a4edccbd28bd3c1245
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-19 16:51:55 +0000

    lzop: update 1.04 bottle.

--- Formula/lzop.rb
+++ Formula/lzop.rb
@@ -7,11 +7,9 @@ class Lzop < Formula
 
   bottle do
     cellar :any
-    sha256 "6d0637dd2d12db57881ffdd672ee6fd0ae743fe3f1f14a8175d2047e9d191c60" => :sierra
-    sha256 "441a5396549a826b805a181f3b7c6115ee81714acd06978ebf18396ab2fda717" => :el_capitan
-    sha256 "8948b68bd5f6ca91371dfbd3522c60015f1010e389ff138e12460bec3ade52b6" => :yosemite
-    sha256 "dd86655bbf09a2d39c2b357fd8d19daec04516ed6d9d471e5d7031aed1a64d0d" => :mavericks
-    sha256 "0fe2af0dba0474808f9f4979e4db3cd2c1be4df1f2237c75a9a323283c894280" => :mountain_lion
+    sha256 "73c2ce334be9317ca79509aec3acef2fa1eff0ffb69fdc10b3850b7f51101f72" => :sierra
+    sha256 "26e49bf0d06fb60d7cd5c431634966f28993edc250c4d06b0db26b28aae3cd0d" => :el_capitan
+    sha256 "d9e12c4bb51c43dd306d5283fde5c3350e3e1f7f1d48c05c831a57b058db1354" => :yosemite
   end
 
   depends_on "lzo"
