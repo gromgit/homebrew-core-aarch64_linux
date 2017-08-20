86d21e034cc17fa7efac479078a873b4057c5bec
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2017-08-20 02:51:19 -0400

    xtensor 0.10.10 (new formula)
    Closes #17010.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/xtensor.rb
@@ -0,0 +1,39 @@
+class Xtensor < Formula
+  desc "Multi-dimensional arrays with broadcasting and lazy computing"
+  homepage "http://quantstack.net/xtensor"
+  url "https://github.com/QuantStack/xtensor/archive/0.10.10.tar.gz"
+  sha256 "ebc9b18793ee692ecb45965119d4fcd9e65908be4d068487a1b7892e31c508c7"
+
+  needs :cxx14
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cc").write <<-EOS.undent
+      #include <iostream>
+      #include "xtensor/xarray.hpp"
+      #include "xtensor/xio.hpp"
+
+      int main() {
+        xt::xarray<double> arr1
+          {{11.0, 12.0, 13.0},
+           {21.0, 22.0, 23.0},
+           {31.0, 32.0, 33.0}};
+
+        xt::xarray<double> arr2
+          {100.0, 200.0, 300.0};
+
+        xt::xarray<double> res = xt::view(arr1, 1) + arr2;
+
+        std::cout << res(2) << std::endl;
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-std=c++14", "test.cc", "-o", "test", "-I#{include}"
+    assert_equal "323", shell_output("./test").chomp
+  end
+end
