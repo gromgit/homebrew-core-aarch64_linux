c01d028034a5afc22fc403e0eb59520b4ac241b8
FX Coudert <fxcoudert@gmail.com>
2017-08-20 22:56:09 +0200

    ncview: migrate from homebrew/science
    Closes #17036.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/ncview.rb
@@ -0,0 +1,29 @@
+class Ncview < Formula
+  desc "Visual browser for netCDF format files"
+  homepage "http://meteora.ucsd.edu/~pierce/ncview_home_page.html"
+  url "ftp://cirrus.ucsd.edu/pub/ncview/ncview-2.1.7.tar.gz"
+  sha256 "a14c2dddac0fc78dad9e4e7e35e2119562589738f4ded55ff6e0eca04d682c82"
+  revision 4
+
+  depends_on "netcdf"
+  depends_on "udunits"
+  depends_on :x11
+
+  def install
+    # Bypass compiler check (which fails due to netcdf's nc-config being
+    # confused by our clang shim)
+    inreplace "configure",
+      "if test x$CC_TEST_SAME != x$NETCDF_CC_TEST_SAME; then",
+      "if false; then"
+
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+    man1.install "data/ncview.1"
+  end
+
+  test do
+    assert_match "Ncview #{version}",
+                 shell_output("#{bin}/ncview -c 2>&1", 1)
+  end
+end
