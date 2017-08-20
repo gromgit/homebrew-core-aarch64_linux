4a87bbef487d5251536a70ee34dfc917a4ed70a9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-20 17:29:12 +0000

    latexila: update 3.24.3 bottle.

--- Formula/latexila.rb
+++ Formula/latexila.rb
@@ -5,9 +5,9 @@ class Latexila < Formula
   sha256 "a1df72539bbe3f84ba69463f7b6bceb9d964f08c4ef75909c1853c872d035870"
 
   bottle do
-    sha256 "1f7325b4e75c112185cf49b86f5e05ea013f40c4db3cddd8fefaa05fc965bff6" => :sierra
-    sha256 "74b9e1021f438624aa47e96c01b1cd2a59cd0184e4075542c59aa17bf7e6fb41" => :el_capitan
-    sha256 "441ecb13f217b9a689a6107a9418e758bdae973eadd28f40c988db37c826838d" => :yosemite
+    sha256 "774ca73e811ecd4dd1bcd57f9a6a482be42939f4b0bb0431c4921414323ee196" => :sierra
+    sha256 "55832e46ee032876b33c2db847b9d75b9089a5945d4b1f88d2bd4e3ea1674341" => :el_capitan
+    sha256 "aa8f9c27221af8f7dede4a0af9f1c38dd5538f2bfa660fe3bdc89830fd7a14f2" => :yosemite
   end
 
   depends_on "pkg-config" => :build
