56781df9d8d8f7fb2c438e4d5bb34123e4ae1765
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-20 21:05:13 +0000

    ncview: add 2.1.7_4 bottle.

--- Formula/ncview.rb
+++ Formula/ncview.rb
@@ -5,6 +5,12 @@ class Ncview < Formula
   sha256 "a14c2dddac0fc78dad9e4e7e35e2119562589738f4ded55ff6e0eca04d682c82"
   revision 4
 
+  bottle do
+    sha256 "415185d4e1dba216877d4bc55020a18f62a59008b3499ffe7fa51a334274b93c" => :sierra
+    sha256 "2ab04bd88cc2d265215f48f272a50a16ef3c32cdef624e935f7ee97ec94957fb" => :el_capitan
+    sha256 "d9c28e5974e1babe1b69b121827e0392fb43e44ec2caadec2db891eb5f18c78b" => :yosemite
+  end
+
   depends_on "netcdf"
   depends_on "udunits"
   depends_on :x11
