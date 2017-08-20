0eb6a51c5d6e71be5f20229f04ae024b450d3c6e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-20 14:59:29 +0000

    clblas: add 2.12 bottle.

--- Formula/clblas.rb
+++ Formula/clblas.rb
@@ -4,6 +4,13 @@ class Clblas < Formula
   url "https://github.com/clMathLibraries/clBLAS/archive/v2.12.tar.gz"
   sha256 "7269c7cb06a43c5e96772010eba032e6d54e72a3abff41f16d765a5e524297a9"
 
+  bottle do
+    cellar :any
+    sha256 "22a6cc8252ed5d431ccea7d51631f57bcee3876be7f65a0ac0fbaabfe09a9484" => :sierra
+    sha256 "e18aa93ecbd78f5f70607653a1e1c48f73952aeef1a568e2205362368c40ba4c" => :el_capitan
+    sha256 "ac0d50729480e60afd56862a49f92408cb0ed61967ba91fcdc9e024e06f39917" => :yosemite
+  end
+
   depends_on "boost" => :build
   depends_on "cmake" => :build
 
