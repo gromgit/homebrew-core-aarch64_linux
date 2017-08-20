fb4061c175144330edc2e9c17b456ae9830567eb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-20 18:33:18 +0000

    libxlsxwriter: add 0.7.4 bottle.

--- Formula/libxlsxwriter.rb
+++ Formula/libxlsxwriter.rb
@@ -4,6 +4,13 @@ class Libxlsxwriter < Formula
   url "https://github.com/jmcnamara/libxlsxwriter/archive/RELEASE_0.7.4.tar.gz"
   sha256 "4a1143e4d9532468305fe7553792ab01274febdf715175a89c7ab8296378fa14"
 
+  bottle do
+    cellar :any
+    sha256 "a0ad27b0aaaf650c15eca6115e7559ceaf3d75b4d16cbc29ed58672608200859" => :sierra
+    sha256 "8e3a337398602bf37e5100e95613a5ab7f300c341c5ee72271402e3eb22b9481" => :el_capitan
+    sha256 "378013e1ca6dabf13e069c61ed997a5ca07264f31ef5ad65dae9e267ff98737e" => :yosemite
+  end
+
   def install
     system "make", "install", "INSTALL_DIR=#{prefix}", "V=1"
   end
