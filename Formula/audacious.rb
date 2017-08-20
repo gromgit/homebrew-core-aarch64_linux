a71ab9b1fb2184d4fe6375406f48564295ab124b
ilovezfs <ilovezfs@icloud.com>
2017-08-20 00:58:27 -0700

    audacious 3.9
    Closes #17017.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/audacious.rb
+++ Formula/audacious.rb
@@ -3,12 +3,12 @@ class Audacious < Formula
   homepage "http://audacious-media-player.org"
 
   stable do
-    url "http://distfiles.audacious-media-player.org/audacious-3.8.2.tar.bz2"
-    sha256 "bdf1471cce9becf9599c742c03bdf67a2b26d9101f7d865f900a74d57addbe93"
+    url "http://distfiles.audacious-media-player.org/audacious-3.9.tar.bz2"
+    sha256 "2d8044673ac786d71b08004f190bbca368258bf60e6602ffc0d9622835ccb05e"
 
     resource "plugins" do
-      url "http://distfiles.audacious-media-player.org/audacious-plugins-3.8.2.tar.bz2"
-      sha256 "d7cefca7a0e32bf4e58bb6e84df157268b5e9a6771a0e8c2da98b03f92a5fdd4"
+      url "http://distfiles.audacious-media-player.org/audacious-plugins-3.9.tar.bz2"
+      sha256 "8bf7f21089cb3406968cc9c71307774aee7100ec4607f28f63cf5690d5c927b8"
     end
   end
 
