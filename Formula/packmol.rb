b837b66288b0ee422fc35569ebc3905926d21041
FX Coudert <fxcoudert@gmail.com>
2017-08-21 10:47:32 +0200

    packmol: migrate from homebrew/science
    Closes #17051.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/packmol.rb
@@ -0,0 +1,27 @@
+class Packmol < Formula
+  desc "Packing optimization for molecular dynamics simulations"
+  homepage "https://www.ime.unicamp.br/~martinez/packmol/"
+  url "https://github.com/leandromartinez98/packmol/archive/17.221.tar.gz"
+  sha256 "b5dcdeb694ffc17f620a4517e4eba67018145774d6fa06c4076bfbfe79400407"
+  head "https://github.com/leandromartinez98/packmol.git"
+
+  depends_on :fortran
+
+  resource "examples" do
+    url "https://www.ime.unicamp.br/~martinez/packmol/examples/examples.tar.gz"
+    sha256 "97ae64bf5833827320a8ab4ac39ce56138889f320c7782a64cd00cdfea1cf422"
+  end
+
+  def install
+    system "./configure"
+    system "make"
+    bin.install("packmol")
+    pkgshare.install "solvate.tcl"
+    (pkgshare/"examples").install resource("examples")
+  end
+
+  test do
+    cp Dir["#{pkgshare}/examples/*"], testpath
+    system bin/"packmol < interface.inp"
+  end
+end
