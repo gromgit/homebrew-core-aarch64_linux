bcab916fa8bf1be433232ad98f6425e4e8f8f0a5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-21 15:05:27 +0000

    ffmpeg: update 3.3.3 bottle.

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -17,8 +17,9 @@ class Ffmpeg < Formula
   end
 
   bottle do
-    sha256 "1b419e42dd75c6d1faf17bd9bc0598bc6e2f6720f9fec1247ece6b3e2c820f31" => :sierra
-    sha256 "dffc8990189b26ea1848b0c798c7abb0bc2348b3e60d01fdb40e51d079345347" => :el_capitan
+    rebuild 1
+    sha256 "6e3ad60d2c3ad7430cc5edf147b1c0e50a7e695e8c74451ab0a3f9d9723ecf46" => :sierra
+    sha256 "d49156d6f3d5717add11f697361b13c5bf321161ff3d84d7062e111f0dfdb513" => :el_capitan
   end
 
   head do
