d27168bb8b4e2746920e7ad68e012237c7c1231a
JCount <JCount42@gmail.com>
2017-08-21 17:59:44 -0400

    cppad 20170821
    Closes #17072.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/cppad.rb
+++ Formula/cppad.rb
@@ -1,9 +1,9 @@
 class Cppad < Formula
   desc "Differentiation of C++ Algorithms"
   homepage "https://www.coin-or.org/CppAD"
-  url "https://www.coin-or.org/download/source/CppAD/cppad-20170820.epl.tgz"
-  version "20170820"
-  sha256 "f491115fe0be06cc29cac938e90f6e3cfbddb9cb7f0a0ae1f68f17123e32d0c1"
+  url "https://www.coin-or.org/download/source/CppAD/cppad-20170821.epl.tgz"
+  version "20170821"
+  sha256 "05b1cd63cab494c788b340310cd8bb28163c98edc6ed79117da55f0e15544cb5"
   head "https://github.com/coin-or/CppAD.git"
 
   bottle do
