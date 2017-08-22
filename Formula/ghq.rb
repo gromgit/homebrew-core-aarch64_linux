025a67704a6c9cb299e1029d766cff91574139c9
ilovezfs <ilovezfs@icloud.com>
2017-08-22 02:57:04 -0700

    ghq 0.8.0
    Closes #17095.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/ghq.rb
+++ Formula/ghq.rb
@@ -3,16 +3,8 @@ require "language/go"
 class Ghq < Formula
   desc "Remote repository management made easy"
   homepage "https://github.com/motemen/ghq"
-
-  stable do
-    url "https://github.com/motemen/ghq/archive/v0.7.4.tar.gz"
-    sha256 "f6e79a7efec2cc11dd8489ae31619de85f15b588158d663256bc9fd45aca6a5d"
-
-    go_resource "github.com/codegangsta/cli" do
-      url "https://github.com/codegangsta/cli.git",
-          :revision => "aca5b047ed14d17224157c3434ea93bf6cdaadee"
-    end
-  end
+  url "https://github.com/motemen/ghq/archive/v0.8.0.tar.gz"
+  sha256 "e3df98794a423000676987812ded7dab30367dfbc82a2b7a8d2ebbea12fa81b2"
 
   bottle do
     cellar :any_skip_relocation
@@ -23,22 +15,18 @@ class Ghq < Formula
     sha256 "45bb6555ca33acfad9a9b0d8abe702dc1a2c3b07c4fd28e889acc8c952383022" => :mavericks
   end
 
-  head do
-    url "https://github.com/motemen/ghq.git"
-
-    go_resource "github.com/codegangsta/cli" do
-      url "https://github.com/codegangsta/cli.git",
-          :revision => "1efa31f08b9333f1bd4882d61f9d668a70cd902e"
-    end
-  end
-
   option "without-completions", "Disable zsh completions"
 
   depends_on "go" => :build
 
+  go_resource "github.com/daviddengcn/go-colortext" do
+    url "https://github.com/daviddengcn/go-colortext.git",
+        :revision => "805cee6e0d43c72ba1d4e3275965ff41e0da068a"
+  end
+
   go_resource "github.com/mitchellh/go-homedir" do
     url "https://github.com/mitchellh/go-homedir.git",
-        :revision => "981ab348d865cf048eb7d17e78ac7192632d8415"
+        :revision => "b8bc1bf767474819792c23f32d8286a45736f1c6"
   end
 
   go_resource "github.com/motemen/go-colorine" do
@@ -46,9 +34,9 @@ class Ghq < Formula
         :revision => "49ff36b8fa42db28092361cd20dcefd0b03b1472"
   end
 
-  go_resource "github.com/daviddengcn/go-colortext" do
-    url "https://github.com/daviddengcn/go-colortext.git",
-        :revision => "3b18c8575a432453d41fdafb340099fff5bba2f7"
+  go_resource "github.com/urfave/cli" do
+    url "https://github.com/urfave/cli.git",
+        :revision => "f017f86fccc5a039a98f23311f34fdf78b014f78"
   end
 
   def install
