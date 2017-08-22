46fc3a33403f941c07ad07d1d6f070db4f558d0c
Caesar Schinas <caesar@caesarschinas.com>
2017-08-22 17:13:57 -0300

    timedog: add head spec (#17103)
    and add a test

--- Formula/timedog.rb
+++ Formula/timedog.rb
@@ -3,10 +3,15 @@ class Timedog < Formula
   homepage "https://github.com/nlfiedler/timedog"
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/timedog/timedog-1.3.zip"
   sha256 "4683f37a28407dabc5c56dc45e6480dd2db460289321edce8980a236cc2787ec"
+  head "https://github.com/nlfiedler/timedog.git"
 
   bottle :unneeded
 
   def install
     bin.install "timedog"
+  end
+
+  test do
+    assert_match "Cannot locate timemachine directory", shell_output("#{bin}/timedog 2>&1", 2)
   end
 end
