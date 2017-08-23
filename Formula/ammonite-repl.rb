e32f6f66f2c535bc1189990f3e9dd4fda5299468
ilovezfs <ilovezfs@icloud.com>
2017-08-22 23:44:20 -0700

    ammonite-repl 1.0.2 (#17135)

--- Formula/ammonite-repl.rb
+++ Formula/ammonite-repl.rb
@@ -1,8 +1,8 @@
 class AmmoniteRepl < Formula
   desc "Ammonite is a cleanroom re-implementation of the Scala REPL"
   homepage "https://lihaoyi.github.io/Ammonite/#Ammonite-REPL"
-  url "https://github.com/lihaoyi/Ammonite/releases/download/1.0.1/2.12-1.0.1", :using => :nounzip
-  sha256 "4dfc506dac08b4656bf42534bfac8a7bc23573fd47b1fa21473f3c6e54e4dabd"
+  url "https://github.com/lihaoyi/Ammonite/releases/download/1.0.2/2.12-1.0.2", :using => :nounzip
+  sha256 "48b78793b824b6ca641d56b525052986c26b53f7f78c2b2e70191f923041b687"
 
   bottle :unneeded
 
