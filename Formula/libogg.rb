23d09e4f84c65452295dcc545bba30281ca9f0fb
FX Coudert <fxcoudert@gmail.com>
2017-08-23 15:05:02 +0200

    libogg: add test

--- Formula/libogg.rb
+++ Formula/libogg.rb
@@ -22,6 +22,11 @@ class Libogg < Formula
     depends_on "libtool" => :build
   end
 
+  resource("oggfile") do
+    url "https://upload.wikimedia.org/wikipedia/commons/c/c8/Example.ogg"
+    sha256 "379071af4fa77bc7dacf892ad81d3f92040a628367d34a451a2cdcc997ef27b0"
+  end
+
   def install
     system "./autogen.sh" if build.head?
     system "./configure", "--disable-dependency-tracking",
@@ -29,5 +34,42 @@ class Libogg < Formula
     system "make"
     ENV.deparallelize
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <ogg/ogg.h>
+      #include <stdio.h>
+
+      int main (void) {
+        ogg_sync_state oy;
+        ogg_stream_state os;
+        ogg_page og;
+        ogg_packet op;
+        char *buffer;
+        int bytes;
+
+        ogg_sync_init (&oy);
+        buffer = ogg_sync_buffer (&oy, 4096);
+        bytes = fread(buffer, 1, 4096, stdin);
+        ogg_sync_wrote (&oy, bytes);
+        if (ogg_sync_pageout (&oy, &og) != 1)
+          return 1;
+        ogg_stream_init (&os, ogg_page_serialno (&og));
+        if (ogg_stream_pagein (&os, &og) < 0)
+          return 1;
+        if (ogg_stream_packetout (&os, &op) != 1)
+         return 1;
+
+        return 0;
+      }
+    EOS
+    testpath.install resource("oggfile")
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-logg",
+                   "-o", "test"
+    # Should work on an OGG file
+    shell_output("./test < Example.ogg")
+    # Expected to fail on a non-OGG file
+    shell_output("./test < #{test_fixtures("test.wav")}", 1)
   end
 end
