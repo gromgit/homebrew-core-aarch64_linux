539a7ec1c616f109d383f0c859c7a79c731feca0
ilovezfs <ilovezfs@icloud.com>
2017-08-23 00:49:21 -0700

    innoextract: revision for boost

--- Formula/innoextract.rb
+++ Formula/innoextract.rb
@@ -3,6 +3,7 @@ class Innoextract < Formula
   homepage "https://constexpr.org/innoextract/"
   url "https://constexpr.org/innoextract/files/innoextract-1.6.tar.gz"
   sha256 "66463f145fffd4d5883c6b3e2f7b2c2837d6f07095810114e514233a88c1033e"
+  revision 1
   head "https://github.com/dscharrer/innoextract.git"
 
   bottle do
