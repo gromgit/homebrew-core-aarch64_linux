21ef9eced8942adca608c85a6b1e256809c3dc92
ilovezfs <ilovezfs@icloud.com>
2017-08-23 00:49:24 -0700

    widelands: revision for boost
    Closes #17122.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/widelands.rb
+++ Formula/widelands.rb
@@ -3,7 +3,7 @@ class Widelands < Formula
   homepage "https://wl.widelands.org/"
   url "https://launchpad.net/widelands/build19/build19/+download/widelands-build19-src.tar.bz2"
   sha256 "e511f9d26828a2b71b64cdfc6674e6e847543b2da73961ab882acca36c7c01a6"
-  revision 3
+  revision 4
 
   bottle do
     sha256 "d0bb034f2bda22a724a588ec315a0e7576e5d106beddb67d14b8eb4fec8fe850" => :sierra
