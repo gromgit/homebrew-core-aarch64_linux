209b153f199d485d921c9a1ebcd11ce920b3510e
FX Coudert <fxcoudert@gmail.com>
2017-08-23 11:23:39 +0200

    phantomjs: patch for macOS 10.13

--- Formula/phantomjs.rb
+++ Formula/phantomjs.rb
@@ -16,6 +16,13 @@ class Phantomjs < Formula
     end
   end
 
+  # Fix a variant of QTBUG-62266 in included Qt source
+  # https://github.com/ariya/phantomjs/issues/15116
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/33dbb45f82/phantomjs/QTBUG-62266.diff"
+    sha256 "d47b52c6a932139a448340244f66ea126412d210ab94dc19da7c468afaf5f45a"
+  end
+
   bottle do
     cellar :any
     rebuild 1
