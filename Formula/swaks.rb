c9b424e9b78ed28688486c43931a9a3bd91011aa
Viktor Szakats <vszakats@users.noreply.github.com>
2017-08-24 12:10:24 +0200

    swaks: secure url(s)

--- Formula/swaks.rb
+++ Formula/swaks.rb
@@ -1,7 +1,7 @@
 class Swaks < Formula
   desc "SMTP command-line test tool"
-  homepage "http://www.jetmore.org/john/code/swaks/"
-  url "http://www.jetmore.org/john/code/swaks/files/swaks-20170101.0.tar.gz"
+  homepage "https://www.jetmore.org/john/code/swaks/"
+  url "https://www.jetmore.org/john/code/swaks/files/swaks-20170101.0.tar.gz"
   sha256 "84e62dd0de4a56d5daebe25afd16835bd8d3c7f39caa5e6bc7d86a056925915e"
 
   bottle :unneeded
