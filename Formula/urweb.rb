4762766b1f8ab05070b7edde2bf7532d52e0658c
Tej Chajed <tchajed@mit.edu>
2017-08-24 16:32:12 +0100

    urweb 20170720
    add a test
    
    Closes #17197.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/urweb.rb
+++ Formula/urweb.rb
@@ -1,8 +1,8 @@
 class Urweb < Formula
   desc "Ur/Web programming language"
   homepage "http://www.impredicative.com/ur/"
-  url "http://www.impredicative.com/ur/urweb-20170105.tgz"
-  sha256 "2ad3aea2c4851c9b18f752d38c7127af8293fbbbbdb3dd06b73a4603fe399b67"
+  url "http://www.impredicative.com/ur/urweb-20170720.tgz"
+  sha256 "8b978b9c26b02d6bff358e26c04e37fdc83363a248462f4a367b415a594d109f"
 
   bottle do
     sha256 "5f406928ad3e00bc835b7b04e29a9a3edad0e727ebc5e16c650a72bed19d8766" => :sierra
@@ -34,6 +34,19 @@ class Urweb < Formula
   end
 
   test do
-    system "#{bin}/urweb"
+    (testpath/"hello.ur").write <<-EOS.undent
+      fun target () = return <xml><body>
+        Welcome!
+      </body></xml>
+      fun main () = return <xml><body>
+        <a link={target ()}>Go there</a>
+      </body></xml>
+    EOS
+    (testpath/"hello.urs").write <<-EOS.undent
+      val main : unit -> transaction page
+    EOS
+    (testpath/"hello.urp").write "hello"
+    system "#{bin}/urweb", "hello"
+    system "./hello.exe", "-h"
   end
 end
