8b769c26ae32b576e4ffdabf688c3975d8f0fc99
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-25 16:30:42 +0000

    percona-server@5.5: update 5.5.57-38.9 bottle.

--- Formula/percona-server@5.5.rb
+++ Formula/percona-server@5.5.rb
@@ -6,9 +6,10 @@ class PerconaServerAT55 < Formula
   sha256 "253f5c254b038c0622055dc8f0259a517be58736cfdb2eefebcba028a8c58da4"
 
   bottle do
-    sha256 "1c24e2604a53f6635f44863c7f783ddc5e86da02cf2e8acd0153a15f9819360a" => :sierra
-    sha256 "705307ce9dafb9714effad8b19414b3548580ce1138f8072d83868a4808eedc2" => :el_capitan
-    sha256 "6efa77dc61308d0b717e6cde6f5c42d154226d4db0e7d86c0f508e2e95e7c9e4" => :yosemite
+    rebuild 1
+    sha256 "6e983264db4df69a954d7232e908bbc7bd283d058a8831b9c9fe4f907f55e417" => :sierra
+    sha256 "0f6b7b056117903982a979fd68c0a1a74279dac6fe941d27d1d217ad2ebfdc8a" => :el_capitan
+    sha256 "5a4743c33b2e5fc3546b82cb267f60ca35b16eb20b5635df468a470c2fa90999" => :yosemite
   end
 
   keg_only :versioned_formula
