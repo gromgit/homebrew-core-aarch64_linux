15eb59d9054e1d37d561d7ac1b80008685112860
Leon Klingele <git@leonklingele.de>
2017-08-25 08:52:25 +0200

    the_silver_searcher 2.1.0
    Closes #17221.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/the_silver_searcher.rb
+++ Formula/the_silver_searcher.rb
@@ -1,8 +1,8 @@
 class TheSilverSearcher < Formula
   desc "Code-search similar to ack"
   homepage "https://github.com/ggreer/the_silver_searcher"
-  url "https://github.com/ggreer/the_silver_searcher/archive/2.0.0.tar.gz"
-  sha256 "ff7243863f22ed73eeab6f7a6d17cfff585a7eaa41d5ab3ae4f5d6db97701d5f"
+  url "https://github.com/ggreer/the_silver_searcher/archive/2.1.0.tar.gz"
+  sha256 "cb416a0da7fe354a009c482ae709692ed567f8e7d2dad4d242e726dd7ca202f0"
   head "https://github.com/ggreer/the_silver_searcher.git"
 
   bottle do
