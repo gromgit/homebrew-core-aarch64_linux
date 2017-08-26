3a922fc83592edbd953cba594306fb0026d7fb5f
Orta <orta.therox+github@gmail.com>
2017-08-26 11:05:57 -0400

    marathon-swift 1.0.1 (new formula)
    Closes #17266.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/marathon-swift.rb
@@ -0,0 +1,29 @@
+class MarathonSwift < Formula
+  desc "Makes it easy to write, run and manage your Swift scripts"
+  homepage "https://github.com/JohnSundell/Marathon"
+  url "https://github.com/JohnSundell/Marathon/archive/1.0.1.tar.gz"
+  sha256 "ebf455159497ae7747784dd9a95b9678dab27db49e8e10c7c6fc2878edcdbce9"
+
+  depends_on :xcode => ["8.3", :build]
+
+  def install
+    if MacOS::Xcode.version >= "9.0"
+      system "swift", "package", "--disable-sandbox", "update"
+      system "swift", "build", "-c", "release", "-Xswiftc", "-static-stdlib",
+             "--disable-sandbox"
+    else
+      ENV.delete("CC") # https://bugs.swift.org/browse/SR-3151
+      system "swift", "package", "update"
+      system "swift", "build", "-c", "release", "-Xswiftc", "-static-stdlib"
+    end
+
+    system "make", "install_bin", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/marathon", "create", "helloWorld",
+           "import Foundation; print(\"Hello World\")", "--no-xcode",
+           "--no-open"
+    system "#{bin}/marathon", "run", "helloWorld"
+  end
+end
