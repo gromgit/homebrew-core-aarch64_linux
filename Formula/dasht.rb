a9d060e678e1df5afd8ba2c410e477b46f09dea9
JCount <JCount42@gmail.com>
2017-08-27 05:30:18 -0400

    dasht 2.2.0 (#17255)

--- Formula/dasht.rb
+++ Formula/dasht.rb
@@ -1,8 +1,8 @@
 class Dasht < Formula
   desc "Search API docs offline, in your terminal or browser"
   homepage "https://sunaku.github.io/dasht"
-  url "https://github.com/sunaku/dasht/archive/v2.1.0.tar.gz"
-  sha256 "98b4c130e64a33fdb9559d8c9d85ba1526236ccdb311b805f28f0128b167555e"
+  url "https://github.com/sunaku/dasht/archive/v2.2.0.tar.gz"
+  sha256 "ba3e20b351c0c0b4bd526d306c577e104414656a9ac011d672cb46892d394030"
 
   bottle :unneeded
 
