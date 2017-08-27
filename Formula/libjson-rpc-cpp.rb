ae4520952f8b1e055049ab66b6ffd19f7b864da1
Peter Spiess-Knafl <dev@spiessknafl.at>
2017-08-27 19:29:18 +0200

    libjson-rpc-cpp 1.0.0
    Closes #17305.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libjson-rpc-cpp.rb
+++ Formula/libjson-rpc-cpp.rb
@@ -1,9 +1,8 @@
 class LibjsonRpcCpp < Formula
   desc "C++ framework for json-rpc"
   homepage "https://github.com/cinemast/libjson-rpc-cpp"
-  url "https://github.com/cinemast/libjson-rpc-cpp/archive/v0.7.0.tar.gz"
-  sha256 "669c2259909f11a8c196923a910f9a16a8225ecc14e6c30e2bcb712bab9097eb"
-  revision 1
+  url "https://github.com/cinemast/libjson-rpc-cpp/archive/v1.0.0.tar.gz"
+  sha256 "888c10f4be145dfe99e007d5298c90764fb73b58effb2c6a3fc522a5b60a18c6"
 
   head "https://github.com/cinemast/libjson-rpc-cpp.git"
 
@@ -18,6 +17,7 @@ class LibjsonRpcCpp < Formula
   depends_on "argtable"
   depends_on "jsoncpp"
   depends_on "libmicrohttpd"
+  depends_on "hiredis"
 
   def install
     system "cmake", ".", *std_cmake_args
