121d1eda94edbda8e083dc998db503312f6108f8
Zhiming Wang <zmwangx@gmail.com>
2017-08-28 07:28:00 +0800

    zsh 5.4.2
    Closes #17299.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/zsh.rb
+++ Formula/zsh.rb
@@ -1,10 +1,9 @@
 class Zsh < Formula
   desc "UNIX shell (command interpreter)"
   homepage "https://www.zsh.org/"
-  url "https://downloads.sourceforge.net/project/zsh/zsh/5.4.1/zsh-5.4.1.tar.xz"
-  mirror "https://www.zsh.org/pub/zsh-5.4.1.tar.xz"
-  sha256 "94cbd57508287e8faa081424509738d496f5f41e32ed890e3a5498ce05d3633b"
-  revision 1
+  url "https://downloads.sourceforge.net/project/zsh/zsh/5.4.2/zsh-5.4.2.tar.gz"
+  mirror "https://www.zsh.org/pub/zsh-5.4.2.tar.gz"
+  sha256 "957bcdb2c57f64c02f673693ea5a7518ef24b6557aeb3a4ce222cefa6d74acc9"
 
   bottle do
     sha256 "825bbbfdddd3908544bd9bbca9054c7316869cd3fa4c31f9c0d2f4d732bb1d84" => :high_sierra
@@ -26,9 +25,9 @@ class Zsh < Formula
   depends_on "pcre"
 
   resource "htmldoc" do
-    url "https://downloads.sourceforge.net/project/zsh/zsh-doc/5.4.1/zsh-5.4.1-doc.tar.xz"
-    mirror "https://www.zsh.org/pub/zsh-5.4.1-doc.tar.xz"
-    sha256 "b8b1a40aeec852806ad2b74b0a0c534320bf517e2fe2a087c0c9d39e75dc29f1"
+    url "https://downloads.sourceforge.net/project/zsh/zsh-doc/5.4.2/zsh-5.4.2-doc.tar.xz"
+    mirror "https://www.zsh.org/pub/zsh-5.4.2-doc.tar.xz"
+    sha256 "5229cc93ebe637a07deb5b386b705c37a50f4adfef788b3c0f6647741df4f6bd"
   end
 
   def install
