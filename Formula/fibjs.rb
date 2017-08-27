ed78de7627eb32bec40409bcbd64e17d8821de64
JCount <JCount42@gmail.com>
2017-08-27 14:12:18 -0400

    fibjs 0.12.0
    Closes #17291.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/fibjs.rb
+++ Formula/fibjs.rb
@@ -1,8 +1,8 @@
 class Fibjs < Formula
   desc "JavaScript on Fiber"
   homepage "http://fibjs.org/"
-  url "https://github.com/fibjs/fibjs/releases/download/v0.11.0/fullsrc.zip"
-  sha256 "aef7f21f5cf2e25b653b28a8ada245f9e7b43e0f9da5c71ea9bf4da599fb726b"
+  url "https://github.com/fibjs/fibjs/releases/download/v0.12.0/fullsrc.zip"
+  sha256 "1dd8a2fb39961e95f7bd4e3615da8bd071f5a483b2775f9d792a8b1f657c7659"
 
   head "https://github.com/fibjs/fibjs.git"
 
