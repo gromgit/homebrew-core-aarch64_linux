7b93c9234ccac4cba1d4a9c3769eb6b65076a253
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-28 12:18:45 +0000

    remake: update 4.1-1.2 bottle.

--- Formula/remake.rb
+++ Formula/remake.rb
@@ -6,10 +6,9 @@ class Remake < Formula
   sha256 "cc6bae282c064b66a3d058133d682fc445b8cf8916f5a875150c379dcf7b4a9e"
 
   bottle do
-    sha256 "442fd8ead728131cf4b6844ea05e6d285e93f5ddd56afb3ff2d419f4ac467275" => :sierra
-    sha256 "acf1304abfe8aafc9795519d7bced2e48a30e756d59e354402f0476ce497ced6" => :el_capitan
-    sha256 "08379f9f4deb5700416c7a65dd2f46fe00e4a9d91e0c036dc877fdabe86a8001" => :yosemite
-    sha256 "a6a8e14b9abad883c20c76e26226018864245e21ac934984f5131a0846784fda" => :mavericks
+    sha256 "50aca3e89fb405911a8cba1cef1cab204a5a93fd9b79df871a53119c7f04a785" => :sierra
+    sha256 "c7238548e920287581ca97dcd0464fc84e1456e8bf097e37cb37dde6b290eb52" => :el_capitan
+    sha256 "4dac4fdd06520c8426c227ed4bc1f7db5421e7cca8003190cbce0f6e32046fda" => :yosemite
   end
 
   def install
