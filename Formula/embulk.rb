38c9500637ac65e07b73cf6f3ffdc91b9e54e2cf
Haokang Den <haokang.den@gmail.com>
2017-08-28 14:48:05 +0900

    embulk 0.8.30 (#17306)

--- Formula/embulk.rb
+++ Formula/embulk.rb
@@ -1,8 +1,8 @@
 class Embulk < Formula
   desc "Data transfer between various databases, file formats and services"
   homepage "http://www.embulk.org/"
-  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.8.29.jar"
-  sha256 "243bfa84a5ed014154737721cd2c5e97e2ea4b4a06705069e98dfeb48cadac62"
+  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.8.30.jar"
+  sha256 "dc45a288e4bbbf98c93daca56470facd515b1d464b8ab524588ff5bc8f2e9c86"
 
   bottle :unneeded
 
