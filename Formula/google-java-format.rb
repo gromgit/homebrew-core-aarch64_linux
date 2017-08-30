72b587fff00618ec767eacb1e53c4781e96fc8b5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-30 03:37:40 +0000

    google-java-format: update 1.4 bottle.

--- Formula/google-java-format.rb
+++ Formula/google-java-format.rb
@@ -6,9 +6,9 @@ class GoogleJavaFormat < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e393dae7521416a76d31067874a8633cf80a79531a78085b8ff836a9049c7886" => :sierra
-    sha256 "7f4cd5af0fe23370d5f494dad918ff1454a699a073d9a63496249cc614dbe6a7" => :el_capitan
-    sha256 "02374c403254edb8e69e164030f634850ae4bea8907622dd4d78731922c1e1fb" => :yosemite
+    sha256 "72c94986490c68f259b20bd78dcb5c041ef0f6d567ddc81ed737473652c5531b" => :sierra
+    sha256 "7d4bd1cee1962c452582a880bfa480f7f1e42be79b2ad9ac37fdae64648b5066" => :el_capitan
+    sha256 "54c538e78f8e0ff9131cd59088859c45446b366c7074b4fadeb01188e63d102e" => :yosemite
   end
 
   depends_on "maven" => :build
