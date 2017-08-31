64e3c77e717e2e89bf697ab40ae6cba83de64a69
Viktor Szakats <vszakats@users.noreply.github.com>
2017-08-31 14:50:55 +0000

    sic: secure url(s)

--- Formula/sic.rb
+++ Formula/sic.rb
@@ -1,10 +1,10 @@
 class Sic < Formula
   desc "Minimal multiplexing IRC client"
-  homepage "http://tools.suckless.org/sic"
-  url "http://dl.suckless.org/tools/sic-1.2.tar.gz"
+  homepage "https://tools.suckless.org/sic/"
+  url "https://dl.suckless.org/tools/sic-1.2.tar.gz"
   sha256 "ac07f905995e13ba2c43912d7a035fbbe78a628d7ba1c256f4ca1372fb565185"
 
-  head "http://git.suckless.org/sic", :using => :git
+  head "https://git.suckless.org/sic", :using => :git
 
   bottle do
     cellar :any_skip_relocation
