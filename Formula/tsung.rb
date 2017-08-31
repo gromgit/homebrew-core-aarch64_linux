6f3ee8546d71ff8629ce0f5de9ea6886ddf93883
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-31 05:40:50 +0000

    tsung: update 1.7.0 bottle.

--- Formula/tsung.rb
+++ Formula/tsung.rb
@@ -7,11 +7,9 @@ class Tsung < Formula
   head "https://github.com/processone/tsung.git"
 
   bottle do
-    sha256 "70b33befe9ef6fba1981a0297728e04582ade4f651569f2168d2b5f6fa82db49" => :sierra
-    sha256 "88d485cf8a667207acb6e06415a0f065b747cef4f11d8b047944e40da308cb22" => :el_capitan
-    sha256 "a4d0f870f04ae0683f4647123c5c3e94e39fd0bcf97edaa98dd2a4a4aa4dca33" => :yosemite
-    sha256 "bea0124e4ec1626cd6f49a1cee650dedcd11602ca820a1764f2862e7b7341288" => :mavericks
-    sha256 "1fe31187fd7662216c1a879bb62302a638834220e84becf8a1ab182df45c3d14" => :mountain_lion
+    sha256 "aa42236bbee2669782a403e2ae494b48fb97e2155cdd6d3bd60728719c1f1265" => :sierra
+    sha256 "9febf50b525e5c631f5bc5219d105f781668963644b4765bd0ef4098ed939b47" => :el_capitan
+    sha256 "0b12c238f39d105daf49018f31e2b5fda548890a93b5f5c5c840dd140c18b23b" => :yosemite
   end
 
   depends_on "erlang"
