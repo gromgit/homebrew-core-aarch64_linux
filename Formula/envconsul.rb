51b94cb3dc9ab3c93e6f31c6e4f7dcb78f0c571c
Marek Kubica <marek@xivilization.net>
2017-08-31 15:24:34 +0200

    envconsul 0.7.2 (new formula)
    Closes #17442.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/envconsul.rb
@@ -0,0 +1,56 @@
+class Envconsul < Formula
+  desc "Launch process with environment variables from Consul and Vault"
+  homepage "https://github.com/hashicorp/envconsul"
+  url "https://github.com/hashicorp/envconsul/archive/v0.7.2.tar.gz"
+  sha256 "c2b2723089f82f7b1623676fda8d378795bf87b4dbc6d4b297e5fc27aeab0aca"
+
+  depends_on "go" => :build
+  depends_on "consul" => :recommended # only used in test
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/hashicorp/envconsul").install buildpath.children
+    cd "src/github.com/hashicorp/envconsul" do
+      system "go", "build", "-o", bin/"envconsul"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    require "socket"
+    require "timeout"
+
+    CONSUL_DEFAULT_PORT = 8500
+    LOCALHOST_IP = "127.0.0.1".freeze
+
+    def port_open?(ip, port, seconds = 1)
+      Timeout.timeout(seconds) do
+        begin
+          TCPSocket.new(ip, port).close
+          true
+        rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
+          false
+        end
+      end
+    rescue Timeout::Error
+      false
+    end
+
+    begin
+      if !port_open?(LOCALHOST_IP, CONSUL_DEFAULT_PORT)
+        fork do
+          exec "consul agent -dev -bind 127.0.0.1"
+          puts "consul started"
+        end
+        sleep 5
+      else
+        puts "Consul already running"
+      end
+      system "consul", "kv", "put", "homebrew-recipe-test/working", "1"
+      output = shell_output("#{bin}/envconsul -consul-addr=127.0.0.1:8500 -upcase -prefix homebrew-recipe-test env")
+      assert_match "WORKING=1", output
+    ensure
+      system "consul", "leave"
+    end
+  end
+end
