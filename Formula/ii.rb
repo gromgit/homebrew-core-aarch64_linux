cb70190a5bda1a72be2f5aa21be502ae9a0a594d
Viktor Szakats <vszakats@users.noreply.github.com>
2017-08-31 14:50:52 +0000

    ii: secure url(s)

--- Formula/ii.rb
+++ Formula/ii.rb
@@ -1,9 +1,9 @@
 class Ii < Formula
   desc "Minimalist IRC client"
-  homepage "http://tools.suckless.org/ii"
-  url "http://dl.suckless.org/tools/ii-1.7.tar.gz"
+  homepage "https://tools.suckless.org/ii/"
+  url "https://dl.suckless.org/tools/ii-1.7.tar.gz"
   sha256 "3a72ac6606d5560b625c062c71f135820e2214fed098e6d624fc40632dc7cc9c"
-  head "http://git.suckless.org/ii", :using => :git
+  head "https://git.suckless.org/ii", :using => :git
 
   bottle do
     cellar :any_skip_relocation
