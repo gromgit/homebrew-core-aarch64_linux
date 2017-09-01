af60a1a0bdba54401ee87b4c5d76d4967957f098
ilovezfs <ilovezfs@icloud.com>
2017-09-01 11:16:49 -0700

    dub 1.5.0
    Closes #17507.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dub.rb
+++ Formula/dub.rb
@@ -1,8 +1,8 @@
 class Dub < Formula
   desc "Build tool for D projects"
   homepage "https://code.dlang.org/getting_started"
-  url "https://github.com/dlang/dub/archive/v1.4.1.tar.gz"
-  sha256 "56f99f06fb1fde0c0f5d92261032fca1eeba1e23d224b614da9fffbcb22ef442"
+  url "https://github.com/dlang/dub/archive/v1.5.0.tar.gz"
+  sha256 "3509f959cc5b34e44adaae586b62ded387ac10031f6c1aaf1cfbb4aae5af37dd"
   version_scheme 1
 
   head "https://github.com/dlang/dub.git"
@@ -11,18 +11,6 @@ class Dub < Formula
     sha256 "2cde0af323c7ecb5376f3b0615c44451f2f97d4c5856b2e1252599f4f3f81902" => :sierra
     sha256 "64f01c4b8fdfc75d298af9b65e256b3600c87370ec8b6d05a3f28b2220f84be1" => :el_capitan
     sha256 "a6fd6a055aaa21fe63b5fb7203d825922d12304e35ba11fea8794987b8688a62" => :yosemite
-  end
-
-  devel do
-    url "https://github.com/dlang/dub/archive/v1.5.0-beta.1.tar.gz"
-    sha256 "fd95787065f1059e8c29801e3f8bef3661fa37ed8bf39e2f379280e52433e37e"
-
-    # Minor problem with a missing public import, fixed in master,
-    # fix should be in next beta
-    patch do
-      url "https://github.com/dlang/dub/pull/1221.patch?full_index=1"
-      sha256 "40b38363c91f6aa1440d81f2b3cb29f0bf8aab3b8e945503437f271d290f4344"
-    end
   end
 
   depends_on "pkg-config" => [:recommended, :run]
