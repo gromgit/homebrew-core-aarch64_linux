c49825adb5a1d3458198aadf44af2b308bfb2299
ilovezfs <ilovezfs@icloud.com>
2017-09-01 04:21:59 -0700

    rhino 1.7.7.2 (#17481)

--- Formula/rhino.rb
+++ Formula/rhino.rb
@@ -1,18 +1,24 @@
 class Rhino < Formula
   desc "JavaScript engine"
   homepage "https://www.mozilla.org/rhino/"
-  url "https://github.com/mozilla/rhino/releases/download/Rhino1_7_7_1_RELEASE/rhino-1.7.7.1.zip"
-  sha256 "f753d82002927208139f374e47febb35d7832a10890f57bb1a3c28cad0de6e84"
+  url "https://github.com/mozilla/rhino/releases/download/Rhino1_7_7_2_Release/rhino-1.7.7.2.zip",
+      :using => :nounzip
+  sha256 "20b5736a2e596cf9367266dc749791f590838e5236b6bd2d21d582a560b33c4d"
 
   bottle :unneeded
 
   conflicts_with "nut", :because => "both install `rhino` binaries"
 
   def install
+    # zip file contains duplicates
+    # Reported 1 Sep 2017 https://github.com/mozilla/rhino/issues/321
+    system "unzip", "-qqo", "rhino-#{version}.zip"
+    cd "rhino#{version}"
+
     rhino_jar = "rhino-#{version}.jar"
     libexec.install "lib/#{rhino_jar}"
     bin.write_jar_script libexec/rhino_jar, "rhino"
-    doc.install (buildpath/"docs").children
+    doc.install Dir["docs/*"]
   end
 
   test do
