bd6b3e7e4111814a3f015a5245eb098a23c1b4e4
ilovezfs <ilovezfs@icloud.com>
2017-09-01 04:12:38 -0700

    uhd 003.010.002.000
    Closes #17484.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/uhd.rb
+++ Formula/uhd.rb
@@ -1,9 +1,8 @@
 class Uhd < Formula
   desc "Hardware driver for all USRP devices."
   homepage "https://files.ettus.com/manual/"
-  url "https://github.com/EttusResearch/uhd/archive/release_003_010_000_000.tar.gz"
-  sha256 "9e018c069851fd68ba63908a9f9944763832ce657f5b357d4e6c64293ad0d2cd"
-  revision 4
+  url "https://github.com/EttusResearch/uhd/archive/release_003_010_002_000.tar.gz"
+  sha256 "7f96d00ed8a1458b31add31291fae66afc1fed47e1dffd886dffa71a8281fabe"
   head "https://github.com/EttusResearch/uhd.git"
 
   bottle do
@@ -20,8 +19,8 @@ class Uhd < Formula
   depends_on "gpsd" => :optional
 
   resource "Mako" do
-    url "https://files.pythonhosted.org/packages/7a/ae/925434246ee90b42e8ef57d3b30a0ab7caf9a2de3e449b876c56dcb48155/Mako-1.0.4.tar.gz"
-    sha256 "fed99dbe4d0ddb27a33ee4910d8708aca9ef1fe854e668387a9ab9a90cbf9059"
+    url "https://files.pythonhosted.org/packages/eb/f3/67579bb486517c0d49547f9697e36582cd19dafb5df9e687ed8e22de57fa/Mako-1.0.7.tar.gz"
+    sha256 "4e02fde57bd4abb5ec400181e4c314f56ac3e49ba4fb8b0d50bba18cb27d25ae"
   end
 
   def install
