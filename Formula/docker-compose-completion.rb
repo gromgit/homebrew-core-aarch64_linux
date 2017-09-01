03b12e608913454d3dc2bb3197d5265fa9347762
ilovezfs <ilovezfs@icloud.com>
2017-08-31 22:11:42 -0700

    docker-compose-completion 1.16.0 (#17466)

--- Formula/docker-compose-completion.rb
+++ Formula/docker-compose-completion.rb
@@ -1,8 +1,8 @@
 class DockerComposeCompletion < Formula
   desc "Docker-compose completion script"
   homepage "https://docs.docker.com/compose/completion/"
-  url "https://github.com/docker/compose/archive/1.15.0.tar.gz"
-  sha256 "a2f3fb12a7ac9ecdefbbf9c3c33a7e1e4c033a7cc9eccf74477c393d73191172"
+  url "https://github.com/docker/compose/archive/1.16.0.tar.gz"
+  sha256 "6848394490465baaa3d8bafec5fa055aabab25c422de90b5772a91f73ff6d9e6"
   head "https://github.com/docker/compose.git"
 
   bottle :unneeded
