180163ee12b783a4d90dfc1a23929e9f7de6aec9
Alex Gaynor <alex.gaynor@gmail.com>
2017-09-01 08:35:37 -0400

    postgresql 9.6.5
    Closes #17487.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/postgresql.rb
+++ Formula/postgresql.rb
@@ -1,8 +1,8 @@
 class Postgresql < Formula
   desc "Object-relational database system"
   homepage "https://www.postgresql.org/"
-  url "https://ftp.postgresql.org/pub/source/v9.6.4/postgresql-9.6.4.tar.bz2"
-  sha256 "2b3ab16d82e21cead54c08b95ce3ac480696944a68603b6c11b3205b7376ce13"
+  url "https://ftp.postgresql.org/pub/source/v9.6.5/postgresql-9.6.5.tar.bz2"
+  sha256 "06da12a7e3dddeb803962af8309fa06da9d6989f49e22865335f0a14bad0744c"
   head "https://github.com/postgres/postgres.git"
 
   bottle do
