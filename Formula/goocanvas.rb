a6181cf77ab7f3285b82b95f19cf9554f78ad311
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-01 19:13:13 +0000

    goocanvas: update 2.0.3 bottle.

--- Formula/goocanvas.rb
+++ Formula/goocanvas.rb
@@ -5,9 +5,9 @@ class Goocanvas < Formula
   sha256 "6b5b9c25d32c05b9bafc42f5fcc28d55f1426e733e78e9fe4d191cfcd666c800"
 
   bottle do
-    sha256 "d4bd80b46bc20db029d985ef65dc5e2792fd6d717333e74d2ac64bf9f4def920" => :sierra
-    sha256 "a71314b56e99000a8dcce9df319c128abbe8d0b2dc88dc2d6c799aada5b106cf" => :el_capitan
-    sha256 "d1c6ab2ec01306647c060409cac001856ecea0b83c3111eef3440f3055ae42f1" => :yosemite
+    sha256 "ef387c378d0deff93c53b45c6910c537fa658ef0fd2bfb0b273fd5a7494d1725" => :sierra
+    sha256 "d9e77f5e105c5aead29dd61f447dc2d805638ec1f3881923c3a8949c8c3de789" => :el_capitan
+    sha256 "31ea24c91d6af157bdb47ab5d6fc7db1a3a2ce8c5fbfbb9fba21ce6fb9a1a13d" => :yosemite
   end
 
   depends_on "pkg-config" => :build
