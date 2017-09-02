64c898e200b17d62bf0ec3fa0a9e9597b8fda3cb
ilovezfs <ilovezfs@icloud.com>
2017-09-02 11:56:51 -0700

    file-formula 5.32
    Closes #17538.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/file-formula.rb
+++ Formula/file-formula.rb
@@ -2,9 +2,9 @@
 class FileFormula < Formula
   desc "Utility to determine file types"
   homepage "https://www.darwinsys.com/file/"
-  url "ftp://ftp.astron.com/pub/file/file-5.31.tar.gz"
-  mirror "https://fossies.org/linux/misc/file-5.31.tar.gz"
-  sha256 "09c588dac9cff4baa054f51a36141793bcf64926edc909594111ceae60fce4ee"
+  url "ftp://ftp.astron.com/pub/file/file-5.32.tar.gz"
+  mirror "https://fossies.org/linux/misc/file-5.32.tar.gz"
+  sha256 "8639dc4d1b21e232285cd483604afc4a6ee810710e00e579dbe9591681722b50"
   head "https://github.com/file/file.git"
 
   bottle do
