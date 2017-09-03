88842af54afeabd7cd336686c202f03994d590be
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-03 05:16:46 +0000

    file-formula: update 5.32 bottle.

--- Formula/file-formula.rb
+++ Formula/file-formula.rb
@@ -9,9 +9,9 @@ class FileFormula < Formula
 
   bottle do
     cellar :any
-    sha256 "94defeca333534a5c8029c96f82a8a51f6b6d8d2e38d7a14b55146ecb84bc7f1" => :sierra
-    sha256 "461106ebaea7f18af0bf9aafdac131f901038f8b04d595e32bc050eafaa43731" => :el_capitan
-    sha256 "a9600f214a411f73615c432268c4aa76ab318e39835f8c3e20288cd746307888" => :yosemite
+    sha256 "97c729a73319a8121272b2b350acd1272dac4c056a33837b4e18fed35ee66378" => :sierra
+    sha256 "915f60a63b3cd5881a385f177becdb1f2c89f8c875a3361f7dd4c55db6fb4305" => :el_capitan
+    sha256 "aeee887f5d75f5d1d452adf2d9024bbc354bd28c5c20e4da0f8d6d7bb5f6f2f0" => :yosemite
   end
 
   keg_only :provided_by_osx
