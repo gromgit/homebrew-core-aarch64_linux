635a789cad1fe78fb29267f1776479862bb423fe
ilovezfs <ilovezfs@icloud.com>
2017-09-03 09:10:24 -0700

    mdp 1.0.10
    Closes #17560.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mdp.rb
+++ Formula/mdp.rb
@@ -1,8 +1,8 @@
 class Mdp < Formula
   desc "Command-line based markdown presentation tool"
   homepage "https://github.com/visit1985/mdp"
-  url "https://github.com/visit1985/mdp/archive/1.0.9.tar.gz"
-  sha256 "893e13a9a61a89bacf29ee141bd9f6e8935710323701e3d36584a4bb90e1372d"
+  url "https://github.com/visit1985/mdp/archive/1.0.10.tar.gz"
+  sha256 "7384c1ba32bd8e4b11342570d2144165a60682499b4cb54e50c8eb3164cfabc5"
   head "https://github.com/visit1985/mdp.git"
 
   bottle do
