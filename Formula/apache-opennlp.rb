bd183da98ef5b50be2dad791ccde11194ea4f3dd
ilovezfs <ilovezfs@icloud.com>
2017-09-04 15:01:12 -0700

    apache-opennlp 1.8.1 (#17606)

--- Formula/apache-opennlp.rb
+++ Formula/apache-opennlp.rb
@@ -1,9 +1,9 @@
 class ApacheOpennlp < Formula
   desc "Machine learning toolkit for processing natural language text"
   homepage "https://opennlp.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=opennlp/opennlp-1.6.0/apache-opennlp-1.6.0-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/opennlp/opennlp-1.6.0/apache-opennlp-1.6.0-bin.tar.gz"
-  sha256 "417ca3d4e535fa69238ab0eb657a0b471da821218d078de959967b31748d99e6"
+  url "https://www.apache.org/dyn/closer.cgi?path=opennlp/opennlp-1.8.1/apache-opennlp-1.8.1-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/opennlp/opennlp-1.8.1/apache-opennlp-1.8.1-bin.tar.gz"
+  sha256 "86098147a75bf590e93ccbcb95d2b9f83c1ca198350b93dc989898700cc63c18"
 
   bottle :unneeded
 
@@ -13,6 +13,6 @@ class ApacheOpennlp < Formula
   end
 
   test do
-    assert_equal "Hello , friends", pipe_output("#{bin}/opennlp SimpleTokenizer", "Hello, friends").chomp
+    assert_equal "Hello , friends", pipe_output("#{bin}/opennlp SimpleTokenizer", "Hello, friends").lines.first.chomp
   end
 end
