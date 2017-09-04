c403d34c393d2077aab4deca613f0434c16e3a0f
ilovezfs <ilovezfs@icloud.com>
2017-09-04 03:24:40 -0700

    poppler@0.57 0.57.0 (new formula)
    Closes #17586.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/poppler@0.57.rb
@@ -0,0 +1,65 @@
+class PopplerAT057 < Formula
+  desc "PDF rendering library (based on the xpdf-3.0 code base)"
+  homepage "https://poppler.freedesktop.org/"
+  url "https://poppler.freedesktop.org/poppler-0.57.0.tar.xz"
+  sha256 "0ea37de71b7db78212ebc79df59f99b66409a29c2eac4d882dae9f2397fe44d8"
+
+  keg_only :versioned_formula
+
+  option "with-qt", "Build Qt5 backend"
+  option "with-little-cms2", "Use color management system"
+
+  depends_on "pkg-config" => :build
+  depends_on "cairo"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "gettext"
+  depends_on "glib"
+  depends_on "gobject-introspection"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "openjpeg"
+  depends_on "qt" => :optional
+  depends_on "little-cms2" => :optional
+
+  resource "font-data" do
+    url "https://poppler.freedesktop.org/poppler-data-0.4.7.tar.gz"
+    sha256 "e752b0d88a7aba54574152143e7bf76436a7ef51977c55d6bd9a48dccde3a7de"
+  end
+
+  needs :cxx11 if build.with?("qt") || MacOS.version < :mavericks
+
+  def install
+    ENV.cxx11 if build.with?("qt") || MacOS.version < :mavericks
+    ENV["LIBOPENJPEG_CFLAGS"] = "-I#{Formula["openjpeg"].opt_include}/openjpeg-2.2"
+
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --enable-xpdf-headers
+      --enable-poppler-glib
+      --disable-gtk-test
+      --enable-introspection=yes
+      --disable-poppler-qt4
+    ]
+
+    if build.with? "qt"
+      args << "--enable-poppler-qt5"
+    else
+      args << "--disable-poppler-qt5"
+    end
+
+    args << "--enable-cms=lcms2" if build.with? "little-cms2"
+
+    system "./configure", *args
+    system "make", "install"
+    resource("font-data").stage do
+      system "make", "install", "prefix=#{prefix}"
+    end
+  end
+
+  test do
+    system "#{bin}/pdfinfo", test_fixtures("test.pdf")
+  end
+end
