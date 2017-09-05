bb2a1743ed986871e6d4cb63eca44ff336b71ee2
ilovezfs <ilovezfs@icloud.com>
2017-09-05 13:45:47 -0700

    eye-d3 0.8.1
    depend on libmagic
    
    Closes #17679.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/eye-d3.rb
+++ Formula/eye-d3.rb
@@ -1,8 +1,8 @@
 class EyeD3 < Formula
   desc "Work with ID3 metadata in .mp3 files"
   homepage "http://eyed3.nicfit.net/"
-  url "http://eyed3.nicfit.net/releases/eyeD3-0.7.8.tar.gz"
-  sha256 "06b956572b8d63c52db8f62447277a5647fc185b7afef9f2a918b4601db467db"
+  url "http://eyed3.nicfit.net/releases/eyeD3-0.8.1.tar.gz"
+  sha256 "a982eccb220c54e9ca5eb2467a3eb11082b6fb962f0fec58fd9b555397208998"
 
   bottle do
     cellar :any_skip_relocation
@@ -14,16 +14,34 @@ class EyeD3 < Formula
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
+  depends_on "libmagic"
 
   # Looking for documentation? Please submit a PR to build some!
   # See https://github.com/Homebrew/homebrew/issues/32770 for previous attempt.
 
+  resource "pathlib" do
+    url "https://files.pythonhosted.org/packages/ac/aa/9b065a76b9af472437a0059f77e8f962fe350438b927cb80184c32f075eb/pathlib-1.0.1.tar.gz"
+    sha256 "6940718dfc3eff4258203ad5021090933e5c04707d5ca8cc9e73c94a7894ea9f"
+  end
+
+  resource "python-magic" do
+    url "https://files.pythonhosted.org/packages/65/0b/c6b31f686420420b5a16b24a722fe980724b28d76f65601c9bc324f08d02/python-magic-0.4.13.tar.gz"
+    sha256 "604eace6f665809bebbb07070508dfa8cabb2d7cb05be9a56706c60f864f1289"
+  end
+
   def install
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+    resources.each do |r|
+      r.stage do
+        system "python", *Language::Python.setup_install_args(libexec/"vendor")
+      end
+    end
+
     # Install in our prefix, not the first-in-the-path python site-packages dir.
     ENV.prepend_create_path "PYTHONPATH", libexec+"lib/python2.7/site-packages"
 
     system "python", "setup.py", "install", "--prefix=#{libexec}"
-    share.install "docs/plugins", "docs/api", "docs/cli.rst"
+    share.install "docs/plugins", "docs/cli.rst"
 
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec+"bin", :PYTHONPATH => ENV["PYTHONPATH"])
