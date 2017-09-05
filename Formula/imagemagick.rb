f7be90fd062adb5d02b8f69c5db8dacadb812b5a
ilovezfs <ilovezfs@icloud.com>
2017-09-05 00:18:05 -0700

    imagemagick 7.0.7-0

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -4,9 +4,9 @@ class Imagemagick < Formula
   # Please always keep the Homebrew mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://dl.bintray.com/homebrew/mirror/imagemagick-7.0.6-10.tar.xz"
-  mirror "https://www.imagemagick.org/download/ImageMagick-7.0.6-10.tar.xz"
-  sha256 "5a9889c87dc351cf4b57ca3ae418c9cdcefc77aaa9a3b16044ae8a4eecf3aeed"
+  url "https://dl.bintray.com/homebrew/mirror/imagemagick-7.0.7-0.tar.xz"
+  mirror "https://www.imagemagick.org/download/ImageMagick-7.0.7-0.tar.xz"
+  sha256 "77e2a309edebd1389e5a16e8f3c3cd919b43f0b0df04a614ec6f8697032674d8"
   head "https://github.com/ImageMagick/ImageMagick.git"
 
   bottle do
