ac46241f105a233e0f9c9c912acb65d851e67f9c
ilovezfs <ilovezfs@icloud.com>
2017-09-05 07:07:04 -0700

    dsh 0.25.10
    Closes #17648.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dsh.rb
+++ Formula/dsh.rb
@@ -1,8 +1,8 @@
 class Dsh < Formula
   desc "Dancer's shell, or distributed shell"
   homepage "https://www.netfort.gr.jp/~dancer/software/dsh.html.en"
-  url "https://www.netfort.gr.jp/~dancer/software/downloads/dsh-0.25.9.tar.gz"
-  sha256 "147c59c902dbd7a3290e20f41b5cc594192d6c93957d34dd061eb8d27bd9e466"
+  url "https://www.netfort.gr.jp/~dancer/software/downloads/dsh-0.25.10.tar.gz"
+  sha256 "520031a5474c25c6b3f9a0840e06a4fea4750734043ab06342522f533fa5b4d0"
 
   bottle do
     sha256 "b9f958c1595b577fbe386d96a8dec6fbbb518792ba78b1d689598b6bd3cd53bb" => :sierra
