e82d33ec11c21b9d3360f7c26e735c615c0a3114
ilovezfs <ilovezfs@icloud.com>
2017-09-05 11:56:57 -0700

    duply 2.0.3 (#17653)

--- Formula/duply.rb
+++ Formula/duply.rb
@@ -1,8 +1,8 @@
 class Duply < Formula
   desc "Frontend to the duplicity backup system"
   homepage "http://duply.net"
-  url "https://downloads.sourceforge.net/project/ftplicity/duply%20%28simple%20duplicity%29/2.0.x/duply_2.0.1.tgz"
-  sha256 "9d2baf55ada5ab36a6da3fa909c8bdc4ce9a0116eac259ebba0efe9b93180bb0"
+  url "https://downloads.sourceforge.net/project/ftplicity/duply%20%28simple%20duplicity%29/2.0.x/duply_2.0.3.tgz"
+  sha256 "34b4c544a92faf190c29cbc9eda5f1420ae1550fc7e0a33126a1775d3187b9e1"
 
   bottle :unneeded
 
