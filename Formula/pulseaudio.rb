4d428eb89c66c8f674aad1eb87efac6610d50034
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-05 17:33:35 +0000

    pulseaudio: update 11.0 bottle.

--- Formula/pulseaudio.rb
+++ Formula/pulseaudio.rb
@@ -5,9 +5,9 @@ class Pulseaudio < Formula
   sha256 "072305d4018fc5e75bb1b45ee6b938fa52fc9fd27493bf327415ef89ed14c969"
 
   bottle do
-    sha256 "5f1dbafa53bd5cc117e08d8c1c6207fcbb42865e3e8d202742cb9dc03971c6cb" => :sierra
-    sha256 "4a84b0cfa38aa689c2e542e235a64d15cd8d5dd447eb4f7b03a6a11985e0d3dc" => :el_capitan
-    sha256 "fc358d4cfc3b4d3a0e7822d09909cbe133842aced9e8eb10c64133f52d7f9770" => :yosemite
+    sha256 "feee09c09d98204cf95a388793a862029a235380f16205e57923455264120ffe" => :sierra
+    sha256 "317e8b66bf3e1d7b75eeb2cddcbc97ae4b2c62642ead619beafaa7c804caa27b" => :el_capitan
+    sha256 "4e944423928d9745e8487d249c3f6b84d7d4cfdd5e804c77bf6ae216d5a6060a" => :yosemite
   end
 
   head do
