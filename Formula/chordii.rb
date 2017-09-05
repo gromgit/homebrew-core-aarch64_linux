133eb5cd446fc95eef4e4bf84322e1253e995610
ilovezfs <ilovezfs@icloud.com>
2017-09-05 06:21:44 -0700

    chordii 4.5.3
    Closes #17646.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/chordii.rb
+++ Formula/chordii.rb
@@ -1,8 +1,8 @@
 class Chordii < Formula
   desc "Text file to music sheet converter"
   homepage "https://www.vromans.org/johan/projects/Chordii/"
-  url "https://downloads.sourceforge.net/project/chordii/chordii/4.5/chordii-4.5.1.tar.gz"
-  sha256 "16a3fe92a82ca734cb6d08a573e513510acd41d4020a6306ac3769f6af5aa08d"
+  url "https://downloads.sourceforge.net/project/chordii/chordii/4.5/chordii-4.5.3.tar.gz"
+  sha256 "140d24a8bc8c298e8db1b9ca04cf02890951aa048a656adb7ee7212c42ce8d06"
 
   bottle do
     cellar :any_skip_relocation
