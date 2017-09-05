cedeaa24c9d91080796d5ea3c0d732b1e3246378
ilovezfs <ilovezfs@icloud.com>
2017-09-05 02:50:47 -0700

    avfs 1.0.5
    set minimum macOS requirement to 10.12
    
    Closes #17634.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/avfs.rb
+++ Formula/avfs.rb
@@ -1,8 +1,8 @@
 class Avfs < Formula
   desc "Virtual file system that facilitates looking inside archives"
   homepage "https://avf.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/avf/avfs/1.0.2/avfs-1.0.2.tar.bz2"
-  sha256 "52693953e8290281d441a898040c5ac2989f11bc1fcb83ca5063c37a125a1c9a"
+  url "https://downloads.sourceforge.net/project/avf/avfs/1.0.5/avfs-1.0.5.tar.bz2"
+  sha256 "e5ce6b1f4193c37148b1b8a021f4f3d05e88f725cf11b16b95a58e8fdae50176"
 
   bottle do
     sha256 "c5c6efd00433246719f6c48e54500e9fe47fd826be53b01298c5dd1519b682a7" => :sierra
@@ -11,6 +11,7 @@ class Avfs < Formula
   end
 
   depends_on "pkg-config" => :build
+  depends_on :macos => :sierra # needs clock_gettime
   depends_on "xz" => :recommended # Upstream recommends building with lzma support.
   depends_on "openssl" => :optional
   depends_on :osxfuse
