5947956eed122cc21204064be2cee41074a89b28
ilovezfs <ilovezfs@icloud.com>
2017-09-06 07:07:29 -0700

    freexl 1.0.3
    Closes #17714.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/freexl.rb
+++ Formula/freexl.rb
@@ -1,8 +1,8 @@
 class Freexl < Formula
   desc "Library to extract data from Excel .xls files"
   homepage "https://www.gaia-gis.it/fossil/freexl/index"
-  url "https://www.gaia-gis.it/gaia-sins/freexl-sources/freexl-1.0.2.tar.gz"
-  sha256 "b39a4814a0f53f5e09a9192c41e3e51bd658843f770399023a963eb064f6409d"
+  url "https://www.gaia-gis.it/gaia-sins/freexl-sources/freexl-1.0.3.tar.gz"
+  sha256 "f8ed29e03a6155454e538fce621e53991a270fcee31120ded339cff2523650a8"
 
   bottle do
     cellar :any
