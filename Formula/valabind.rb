74fed9288fed0778e7197a076235b228221ee241
ilovezfs <ilovezfs@icloud.com>
2017-09-06 02:26:16 -0700

    valabind: vendor vala 0.34.9
    Closes #17693.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/valabind.rb
+++ Formula/valabind.rb
@@ -1,19 +1,13 @@
 class Valabind < Formula
   desc "Vala bindings for radare, reverse engineering framework"
   homepage "https://radare.org/"
-  revision 4
+  revision 5
 
   head "https://github.com/radare/valabind.git"
 
   stable do
     url "https://www.radare.org/get/valabind-0.10.0.tar.gz"
     sha256 "35517455b4869138328513aa24518b46debca67cf969f227336af264b8811c19"
-    # patch necessary to support vala 0.36.0
-    # remove upon next release
-    patch do
-      url "https://github.com/radare/valabind/commit/f23ff9421c1875d18b1e558596557009b45faa19.patch?full_index=1"
-      sha256 "d76e5fffaa209468619d5db36f3f8dad89480c1b74d3d2b14b14fa029d2c4234"
-    end
 
     # patch to support BSD sed
     # remove upon next release
@@ -30,11 +24,32 @@ class Valabind < Formula
     sha256 "b916fc236518c29a64f7f86e7b0be611564532ab21855822be966107e52d8103" => :yosemite
   end
 
-  depends_on "pkg-config" => :build
+  depends_on "pkg-config" => :run # :run, not :build, for vala
   depends_on "swig" => :run
-  depends_on "vala"
+
+  # vala dependencies
+  depends_on "gettext"
+  depends_on "glib"
+
+  # Upstream issue "Build failure with vala 0.38.0"
+  # Reported 6 Sep 2017 https://github.com/radare/valabind/issues/43
+  resource "vala" do
+    url "https://download.gnome.org/sources/vala/0.34/vala-0.34.9.tar.xz"
+    sha256 "36662f77e36abf9ce6f46e3015c4512276e6361553bdcc2d75566ed83a1da19d"
+  end
 
   def install
+    resource("vala").stage do
+      system "./configure", "--disable-dependency-tracking",
+                            "--disable-silent-rules",
+                            "--prefix=#{libexec}"
+      system "make"
+      system "make", "install"
+    end
+
+    ENV.prepend_path "PATH", libexec/"bin"
+    ENV.prepend_path "PKG_CONFIG_PATH", libexec/"lib/pkgconfig"
+
     system "make"
     system "make", "install", "PREFIX=#{prefix}"
   end
