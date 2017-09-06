8d04bc937a1f1b04d73c197e96e16a3e70639107
ilovezfs <ilovezfs@icloud.com>
2017-09-06 07:33:20 -0700

    fuse-zip 0.4.2
    switch to new repository and homepage on bitbucket
    
    Closes #17718.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/fuse-zip.rb
+++ Formula/fuse-zip.rb
@@ -1,11 +1,9 @@
 class FuseZip < Formula
   desc "FUSE file system to create & manipulate ZIP archives"
-  homepage "https://code.google.com/p/fuse-zip/"
-  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/fuse-zip/fuse-zip-0.4.0.tar.gz"
-  sha256 "db9eb13aa250061eaa7df6b1ee5022dbea54089094af94cc3d1767b63bdc9ca7"
-  revision 2
-
-  head "https://code.google.com/p/fuse-zip/", :using => :hg
+  homepage "https://bitbucket.org/agalanin/fuse-zip"
+  url "https://bitbucket.org/agalanin/fuse-zip/downloads/fuse-zip-0.4.2.tar.gz"
+  sha256 "3d4ee113d4c7918ad3c660f8088473d5fabf67b3476fef16ec7f5bd8a4182fdc"
+  head "https://bitbucket.org/agalanin/fuse-zip", :using => :hg
 
   bottle do
     cellar :any
@@ -19,7 +17,7 @@ class FuseZip < Formula
   depends_on :osxfuse
 
   def install
-    system "make", "INSTALLPREFIX=#{prefix}", "install"
+    system "make", "prefix=#{prefix}", "install"
   end
 
   test do
