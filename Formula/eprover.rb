badf4b4f7872fa8f688f50bedcba74206cd9aab8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-06 04:56:55 +0000

    eprover: update 2.0 bottle.

--- Formula/eprover.rb
+++ Formula/eprover.rb
@@ -6,10 +6,9 @@ class Eprover < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "28c7ee291eeb3860eb5dd1ec6cad9a33317b94588d4a90add58df12ea5f00dae" => :sierra
-    sha256 "396ff4a7c412a5aa773f4df990611e27bd7d7ad5e4b297f9da3afa29cc2271ba" => :el_capitan
-    sha256 "f698ea516f874623f22a6a87335b0881b0014e8bd1811f213b81a2bdd32f6f14" => :yosemite
-    sha256 "0a5a1d15a59ec5921c3416de25df8e9fd518604e761396df3612bd587cdc00cf" => :mavericks
+    sha256 "fffcc75e400937600fd2c059ecb5427476b216af5c56249c756400de4906885b" => :sierra
+    sha256 "8d3dd0289c6f77d68dc2e1c9f06c039e106f2b6e324788315815a8c37b433e9c" => :el_capitan
+    sha256 "6d13db7618b7a1d3e567165000a81d34384941c0f3de51177cf7b2a9b8f39ce7" => :yosemite
   end
 
   def install
