f019ef8b40fbf1bfe21b9332474289a1d7943c6d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-07 17:08:54 +0000

    icbirc: update 2.1 bottle.

--- Formula/icbirc.rb
+++ Formula/icbirc.rb
@@ -6,11 +6,9 @@ class Icbirc < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "85d7afcdfe8009b68a697a0fdea24dfda0cda6cc65dfae9f99fae7a1d1fb0ea8" => :sierra
-    sha256 "3d9cf7664d20818b648258da1c52cb397c0324d852eca445d62a8d3f2543fc3c" => :el_capitan
-    sha256 "08fa7b7435d429c8c21ff2d7835d0b7341fa3dd7cc5677241d1a897ec47cc883" => :yosemite
-    sha256 "d16b5155b2f117cdc442eef09114ecffe180ecaf3c67e3c501374ba6e03a144a" => :mavericks
-    sha256 "fec513444c71ecef76d4b5650749f2a18cbfb16dc6cf35894ddd46b0096c248b" => :mountain_lion
+    sha256 "cbec4e472c640a63081f12723fc9d144f00aa00c9229ce5bfc2edd99199aee74" => :sierra
+    sha256 "2f943e4af7a9c1e2524d9583b0ef5539988f68f56a8f8c483b2c2d1990fff21d" => :el_capitan
+    sha256 "c4fdfe55febee01d5fc84bdc7bb49ad6bb2366f6f5a54dfbb851ee891a67c5a3" => :yosemite
   end
 
   depends_on "bsdmake" => :build
