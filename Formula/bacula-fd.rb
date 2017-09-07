d638e8687202c279cb0c5efa289f5fc1699ba02c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-07 04:37:58 +0000

    bacula-fd: update 9.0.4 bottle.

--- Formula/bacula-fd.rb
+++ Formula/bacula-fd.rb
@@ -5,9 +5,9 @@ class BaculaFd < Formula
   sha256 "cafa2d9a9e89a77d3b79984972487e72fb90d949269c77a74a68e97a9d28996f"
 
   bottle do
-    sha256 "a558d0384f4e2f1607d84104fe52fbad9c0fdaa3f335e101203b545a74d01264" => :sierra
-    sha256 "0b55c3e0b949d5e29d0bb4b0530e8b27734f4c43233a5afea354a97b09678ce9" => :el_capitan
-    sha256 "2c72c95ac1e6d53458c0e8d5105011176c0ef901c9c4ea5f63950fac86d4042a" => :yosemite
+    sha256 "725d7cfea18a08afae8bb22fbe4da5cc0496b712ff212e351b63bd58b91c1fd0" => :sierra
+    sha256 "c48c7d7e31183ff2a035c8f0558cb373a1a89b4c41ae7a1a4188c4adcf75c2c8" => :el_capitan
+    sha256 "1b7a60b6813c519491d693e4fa44a8f6f26ad06683a314dd0748c0d5197fc57c" => :yosemite
   end
 
   depends_on "readline"
