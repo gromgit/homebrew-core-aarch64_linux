0659a541fbf1ae95fe835299a8685fb7ffdcdb99
Travis Reeder <treeder@gmail.com>
2017-09-07 08:49:56 -0700

    fn 0.3.84 (new formula)
    new formula for fn
    
    Updated version and added more tests.
    
    Changed test per comments.
    
    Closes #17192.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

---
+++ Formula/fn.rb
@@ -0,0 +1,53 @@
+class Fn < Formula
+  desc "Command-line tool for the fn project"
+  homepage "https://fnproject.github.io"
+  url "https://github.com/fnproject/cli/archive/0.3.84.tar.gz"
+  sha256 "495c93a955b2a4cfb0a2ea4a32cd150fc2a98192051d99a51a1f63bddfe178fc"
+
+  depends_on "go" => :build
+  depends_on "glide" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
+    dir = buildpath/"src/github.com/fnproject/cli"
+    dir.install Dir["*"]
+    cd dir do
+      system "glide", "install", "-v", "--force", "--skip-test"
+      system "go", "build", "-o", "#{bin}/fn"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    require "socket"
+    assert_match version.to_s, shell_output("#{bin}/fn --version")
+    system "#{bin}/fn", "init", "--runtime", "go", "--name", "myfunc"
+    assert_predicate testpath/"func.go", :exist?, "expected file func.go doesn't exist"
+    assert_predicate testpath/"func.yaml", :exist?, "expected file func.yaml doesn't exist"
+    server = TCPServer.new("localhost", 0)
+    port = server.addr[1]
+    pid = fork do
+      loop do
+        socket = server.accept
+        response = '{"route": {"path": "/myfunc", "image": "fnproject/myfunc"} }'
+        socket.print "HTTP/1.1 200 OK\r\n" \
+                    "Content-Length: #{response.bytesize}\r\n" \
+                    "Connection: close\r\n"
+        socket.print "\r\n"
+        socket.print response
+        socket.close
+      end
+    end
+    begin
+      ENV["API_URL"] = "http://localhost:#{port}"
+      ENV["FN_REGISTRY"] = "fnproject"
+      expected = "/myfunc created with fnproject/myfunc"
+      output = shell_output("#{bin}/fn routes create myapp myfunc")
+      assert_match expected, output.chomp
+    ensure
+      Process.kill("TERM", pid)
+      Process.wait(pid)
+    end
+  end
+end
