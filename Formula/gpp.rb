768fc2055381729a433898c5d0cb7b376db4b324
ilovezfs <ilovezfs@icloud.com>
2017-09-07 03:24:16 -0700

    gpp 2.25
    Closes #17765.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gpp.rb
+++ Formula/gpp.rb
@@ -1,8 +1,8 @@
 class Gpp < Formula
   desc "General-purpose preprocessor with customizable syntax"
   homepage "http://en.nothingisreal.com/wiki/GPP"
-  url "https://files.nothingisreal.com/software/gpp/gpp-2.24.tar.bz2"
-  sha256 "9bc2db874ab315ddd1c03daba6687f5046c70fb2207abdcbd55d0e9ad7d0f6bc"
+  url "https://files.nothingisreal.com/software/gpp/gpp-2.25.tar.bz2"
+  sha256 "16ba9329208f587f96172f951ad3d24a81afea6a5b7836fe87955726eacdd19f"
 
   bottle do
     cellar :any_skip_relocation
