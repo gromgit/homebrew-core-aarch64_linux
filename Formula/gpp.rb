c56f27c58dd367c4a5d9c2341962d3d653cea586
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-07 10:51:43 +0000

    gpp: update 2.25 bottle.

--- Formula/gpp.rb
+++ Formula/gpp.rb
@@ -6,12 +6,9 @@ class Gpp < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "7b905243827e6b5877c8af1f13b6fb3b79c07341059fdb1b6a382fe1286d580b" => :sierra
-    sha256 "6925eb92be766ed9fe61a9a98dc7bc3c22793079abf63f462cb7001017cac28c" => :el_capitan
-    sha256 "e9bb30f85bd24890f97160649a3ed9ef8081d0e39154226487b29e4c58d154ab" => :yosemite
-    sha256 "90463e69adac31b694bbcac3e90ad494bb8e4ef4927d1a04e3a7246b87c0d55d" => :mavericks
-    sha256 "bfcf6ef95b33a600dc6471b4f80e3dbb8e4f4e3cf13aa21b67990576ade35414" => :mountain_lion
+    sha256 "ceec5eeca75c3ad3e2192f9fcef09857533de93e92413cc501e23e9b494da0f7" => :sierra
+    sha256 "2a7f2e1371f6bcbff03db5b9b260c47cd41cb25ec1e5726dfb5ef838f3c417e0" => :el_capitan
+    sha256 "2ea663aa6b1be3c2f6f11b99a31663aeab30c9e7de3712886b23b4c7cc114a5b" => :yosemite
   end
 
   def install
