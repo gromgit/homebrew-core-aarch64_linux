cfbe1b0ef55dbf67372d84c46721022d207f0a06
ilovezfs <ilovezfs@icloud.com>
2017-09-07 23:26:15 -0700

    castxml 0.1+git20170823
    Closes #17810.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/castxml.rb
+++ Formula/castxml.rb
@@ -1,11 +1,10 @@
 class Castxml < Formula
   desc "C-family Abstract Syntax Tree XML Output"
   homepage "https://github.com/CastXML/CastXML"
-  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/c/castxml/castxml_0.1+git20161215.orig.tar.xz"
-  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/c/castxml/castxml_0.1+git20161215.orig.tar.xz"
-  version "0.1+git20161215"
-  sha256 "6710486f72ea32020d30e04ff9d6e629a94b79d4fb10b834f93d3f87ebd9c091"
-  revision 1
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/c/castxml/castxml_0.1+git20170823.orig.tar.xz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/c/castxml/castxml_0.1+git20170823.orig.tar.xz"
+  version "0.1+git20170823"
+  sha256 "aa10c17f703ef46a88f9772205d8f51285fd3567aa91931ee1a7a5abfff95b11"
   head "https://github.com/CastXML/castxml.git"
 
   bottle do
