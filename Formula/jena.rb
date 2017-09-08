75ff1b86d21d15c801c1a2488dc5a3a5b3bc4b02
ilovezfs <ilovezfs@icloud.com>
2017-09-08 13:25:53 -0700

    jena 3.4.0
    Closes #17837.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/jena.rb
+++ Formula/jena.rb
@@ -1,8 +1,8 @@
 class Jena < Formula
   desc "Framework for building semantic web and linked data apps"
   homepage "https://jena.apache.org/"
-  url "https://archive.apache.org/dist/jena/binaries/apache-jena-3.1.0.tar.gz"
-  sha256 "532ad87eab7792ff1ffae34375d4c27956aada7c659743c39027e8b48f29cbd9"
+  url "https://archive.apache.org/dist/jena/binaries/apache-jena-3.4.0.tar.gz"
+  sha256 "35b1eb88d20ea7908e3971825340bed3856e2aa94f3edd1643f811c019d0bf8e"
 
   bottle :unneeded
 
