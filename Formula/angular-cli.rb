8a5c99b47533ae16f06f19249055cf29e68de7f6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-08 01:33:05 +0000

    angular-cli: update 1.4.0 bottle.

--- Formula/angular-cli.rb
+++ Formula/angular-cli.rb
@@ -7,9 +7,9 @@ class AngularCli < Formula
   sha256 "daca32fa048704f9eb16bf410cc78c4d95fdddb9026859cfc172cd365e7feb94"
 
   bottle do
-    sha256 "f21fcdaf492e0077d2249864cd3430dcac8e1541372e4948c765a46f9a16f10a" => :sierra
-    sha256 "cedaf42d3f0442cf2a3d86ff4721d809a6bd016b7b939e00d55da8d6e0b21294" => :el_capitan
-    sha256 "9549de0cb6f23363422497482279a3af2df2fd254760c76c40c301b3d78224e1" => :yosemite
+    sha256 "e9d90a0ac96bb98fa259bb79a64febd193aa624e9ad66a94df040d3d9a124ac6" => :sierra
+    sha256 "e06f19e366648b888f949ba0ecabe3d54fbafb39df89461c5297572d643743ed" => :el_capitan
+    sha256 "ec2c8c269735f1b9293c378619c38b0a71ee18417bb50399f5f2f8b2ce134a4c" => :yosemite
   end
 
   depends_on "node"
