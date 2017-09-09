27e351c6520bcc6aa7aeb04c22217c36152b0f25
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-09 10:01:58 +0000

    json-c: update 0.12.1 bottle.

--- Formula/json-c.rb
+++ Formula/json-c.rb
@@ -7,12 +7,9 @@ class JsonC < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "b61af7437b93495ba08b097e4da71e3c00f672402394620607977b45d2348f85" => :sierra
-    sha256 "8ba8006e2eb97006a781ce8d93a95791ae1e26d094afce0aeb8483caa95febbd" => :el_capitan
-    sha256 "f7a602faf71091f98eb7b8390c1bd36bbd14cfe7e20c2f418bcc5c797315a2be" => :yosemite
-    sha256 "e755df0edf95cf76c20a551dd28bb1703e769371413feaa7f60660338a72ce6c" => :mavericks
-    sha256 "df94de81086ff76a48531df981ea32390dbae338e93fc0e157efc97193cd1f74" => :mountain_lion
+    sha256 "2ce9b90873fa07bcb526c7b9aea55e58af52d88402e891227a5927a41d525ef5" => :sierra
+    sha256 "6055bdc414153c7928fa30f215e8354510d9610cb3b75c75def3e6850b48e11f" => :el_capitan
+    sha256 "4e6850e0b1627f622b64ae270883df999ab986046b1d4f9f0ca446fbd24a729b" => :yosemite
   end
 
   head do
