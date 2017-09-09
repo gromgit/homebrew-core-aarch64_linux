7af8803f26b04afa9bc64a1f76021910ac164add
ilovezfs <ilovezfs@icloud.com>
2017-09-09 05:30:14 -0700

    lcdproc 0.5.9
    upstream is now on GitHub
    
    Closes #17867.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/lcdproc.rb
+++ Formula/lcdproc.rb
@@ -1,8 +1,8 @@
 class Lcdproc < Formula
   desc "Display real-time system information on a LCD"
   homepage "http://www.lcdproc.org/"
-  url "https://downloads.sourceforge.net/project/lcdproc/lcdproc/0.5.7/lcdproc-0.5.7.tar.gz"
-  sha256 "843007d377adc856529ed0c7c42c9a7563043f06b1b73add0372bba3a3029804"
+  url "https://github.com/lcdproc/lcdproc/releases/download/v0.5.9/lcdproc-0.5.9.tar.gz"
+  sha256 "d48a915496c96ff775b377d2222de3150ae5172bfb84a6ec9f9ceab962f97b83"
 
   bottle do
     sha256 "c46b64b6eb8f3c183ac2bbcde765762132a19547b9670c268256b419a9202468" => :sierra
@@ -16,20 +16,13 @@ class Lcdproc < Formula
   depends_on "libhid"
   depends_on "libftdi0"
 
-  # Fixes build failure "error: unknown type name 'uint'"
-  # Reported 21 Jun 2016: https://sourceforge.net/p/lcdproc/patches/26/
-  patch do
-    url "https://sourceforge.net/p/lcdproc/patches/26/attachment/darwin-uint.diff"
-    sha256 "174a812f325bf60f801252ca1d680b1b781ae421ae54066b699276905423c7e1"
-  end
-
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
+                          "--disable-silent-rules",
                           "--prefix=#{prefix}",
                           "--enable-drivers=all",
                           "--enable-libftdi=yes"
-    system "make"
     system "make", "install"
   end
 
