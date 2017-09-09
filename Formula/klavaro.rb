c3377075f6193674de81a6866e63bcc0e62e2a7a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-09 11:51:56 +0000

    klavaro: update 3.03 bottle.

--- Formula/klavaro.rb
+++ Formula/klavaro.rb
@@ -5,9 +5,9 @@ class Klavaro < Formula
   sha256 "e0959f21e54e7f4700042a3a14987a7f8fc898701eab4f721ebcf4559a2c87b5"
 
   bottle do
-    sha256 "d2b2b39f035d969207cf6fb20577247771862c2c8f6a35ed280a5e95cd159e64" => :sierra
-    sha256 "43835be86f96388d899af872ebc780441edb6b416c0d6b4e768c6a7e4221410e" => :el_capitan
-    sha256 "d9971fbbd61aaf76298a9ec1046e88ca71064dfbb4f1c5be9372be9899ed2331" => :yosemite
+    sha256 "4024187379c8dced802206da91fe6d771e618f6eeaaecf327dfd114908ece438" => :sierra
+    sha256 "1fc81369acf6063bbd0905976443a3dff4bc053c4ae975675b9ff6ddfb00f10c" => :el_capitan
+    sha256 "ecbf95acf3edaa13be06446c8e00a3236b4cd48417e6feeabe0f67363b721d89" => :yosemite
   end
 
   depends_on "pkg-config" => :build
