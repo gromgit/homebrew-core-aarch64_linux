7c818b4f9ce240f8d6b6e6544f00507bbfb745d5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-10 18:30:25 +0000

    trash-cli: add 0.17.1.14 bottle.

--- Formula/trash-cli.rb
+++ Formula/trash-cli.rb
@@ -7,6 +7,12 @@ class TrashCli < Formula
   sha256 "8fdd20e5e9c55ea4e24677e602a06a94a93f1155f9970c55b25dede5e037b974"
   head "https://github.com/andreafrancia/trash-cli.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6c2e56df6aeed7ed8bcbdea702b9d66058ea17be38bc39a26feba5928dbe053a" => :sierra
+    sha256 "92c6ab8dc868bad029103c897ccdc5a04e6e0f6809dfd2759c58aa390a7d5e8f" => :el_capitan
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
 
   conflicts_with "osxutils", :because => "both install a `trash` binary"
