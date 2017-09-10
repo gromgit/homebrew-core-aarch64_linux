df0e8836e0165acc826ff558eb09f273018da2fc
ilovezfs <ilovezfs@icloud.com>
2017-09-10 05:30:11 -0700

    libflowmanager 3.0.0

--- Formula/libflowmanager.rb
+++ Formula/libflowmanager.rb
@@ -1,9 +1,8 @@
 class Libflowmanager < Formula
   desc "Flow-based measurement tasks with packet-based inputs"
   homepage "https://research.wand.net.nz/software/libflowmanager.php"
-  url "https://research.wand.net.nz/software/libflowmanager/libflowmanager-2.0.5.tar.gz"
-  sha256 "00cae0a13ac0a486a6b8db2c98a909099fd22bd8e688571e2833cf3ee7ad457e"
-  revision 1
+  url "https://research.wand.net.nz/software/libflowmanager/libflowmanager-3.0.0.tar.gz"
+  sha256 "0866adfcdc223426ba17d6133a657d94928b4f8e12392533a27387b982178373"
 
   bottle do
     cellar :any
