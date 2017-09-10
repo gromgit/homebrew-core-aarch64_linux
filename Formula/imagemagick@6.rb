0268fa9dcaad6712eb92f8075916381c8d665cbf
ilovezfs <ilovezfs@icloud.com>
2017-09-10 05:07:13 -0700

    imagemagick@6 6.9.9-12
    Closes #17891.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/imagemagick@6.rb
+++ Formula/imagemagick@6.rb
@@ -4,9 +4,9 @@ class ImagemagickAT6 < Formula
   # Please always keep the Homebrew mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://dl.bintray.com/homebrew/mirror/imagemagick%406-6.9.9-11.tar.xz"
-  mirror "https://www.imagemagick.org/download/ImageMagick-6.9.9-11.tar.xz"
-  sha256 "b78415bf49eb34c06247010ca2d564a055ae8c2384f7b3166c8a8f7910eee732"
+  url "https://dl.bintray.com/homebrew/mirror/imagemagick%406-6.9.9-12.tar.xz"
+  mirror "https://www.imagemagick.org/download/ImageMagick-6.9.9-12.tar.xz"
+  sha256 "856529bf01f90b69270af7359b91d7de10780f7772f2f07ac0ae441e961d6682"
 
   bottle do
     sha256 "aa250ec953496118a2c4060056bc52482f0d72f583da4834224a66d429c065fb" => :sierra
