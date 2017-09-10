6fb5ff035693281517baba5ed91844dc9428b39c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-10 16:34:17 +0000

    lean: add 3.2.0 bottle.

--- Formula/lean.rb
+++ Formula/lean.rb
@@ -5,6 +5,12 @@ class Lean < Formula
   sha256 "2668e075a9bdf4270d75fd60181078b33c078c7288b5bb8ad795819d8d5272cf"
   head "https://github.com/leanprover/lean.git"
 
+  bottle do
+    cellar :any
+    sha256 "f960928337fdac1fda7d3ee4e20167d73fbb87a05c09bdfc0a626bbae6031233" => :sierra
+    sha256 "ec63932b9dc5d768c6bb2a647d4f7f491a774e38af6b9019cab68220b46fd657" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "gmp"
   depends_on "jemalloc"
