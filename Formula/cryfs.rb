d162f0ed02e55fbbf2bcbf1595163dda760313a4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-10 15:17:54 +0000

    cryfs: add 0.9.7 bottle.

--- Formula/cryfs.rb
+++ Formula/cryfs.rb
@@ -5,6 +5,12 @@ class Cryfs < Formula
   sha256 "c998069217c29c026a944da47eea0a9e73eda914ef0f891f436701bcbdbbe4d7"
   head "https://github.com/cryfs/cryfs.git", :branch => "develop"
 
+  bottle do
+    cellar :any
+    sha256 "ed69c7628756a2db65cdd0720c97f329de632dc8fb295615b22b351fe774623a" => :sierra
+    sha256 "8e3be5b7d701349d3d4e2ebb072320abd3d09f4b7a42a68e292878279a83c20b" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "cryptopp"
