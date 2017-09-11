7ba069dad431e9c3684d160f02de3be030a3f3c4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-11 17:23:46 +0000

    packetbeat: update 5.6.0 bottle.

--- Formula/packetbeat.rb
+++ Formula/packetbeat.rb
@@ -8,9 +8,8 @@ class Packetbeat < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ed3c13ef781cbd2831aedc6947081c5a475d03446ced31d9c50ad62291fae33a" => :sierra
-    sha256 "3701b387b4c09d74352a558842f49a353d19e4554ea2f5155e1f6339d6834bf4" => :el_capitan
-    sha256 "d4abd6198945ab0f03f565e3b34f7180fe2433b7c6f212b7e735d322b177b605" => :yosemite
+    sha256 "52b1cabcff735ca1dfce28435e1c09ac3f3e01ffc9f1ee001270f243ebddb4e8" => :sierra
+    sha256 "255c90ebaf55394f12b3e3aec1cb8b29dbe42766f740fc21aff0af55c3327f32" => :el_capitan
   end
 
   depends_on "go" => :build
