05b906399f02791f637867b0f899edae0d78e8af
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-11 08:09:22 +0000

    fizmo: update 0.8.5 bottle.

--- Formula/fizmo.rb
+++ Formula/fizmo.rb
@@ -5,9 +5,8 @@ class Fizmo < Formula
   sha256 "1c259a29b21c9f401c12fc24d555aca4f4ff171873be56fb44c0c9402c61beaa"
 
   bottle do
-    sha256 "1d7086e1db185650e7fa5a7bd77719e21e7acf71d99d05e3e91d1ab0e8485d6a" => :sierra
-    sha256 "496cb25c39256ad1cf54453cea1f66fcbbd3f1f4f1f8a569adb8d1e2e1849a53" => :el_capitan
-    sha256 "0280ceb9ab03a7ffbe150b5fa398f0c649fde1c4af2183938cb6d6420005c3de" => :yosemite
+    sha256 "fcde25bc1e4eb7f481d132e8da671c7eb445fe3139a1f1784aff830e353332a6" => :sierra
+    sha256 "49943781111aafa7f1a1094bd79f6faea13e168d51119ae2d84a9f74ad9e4008" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
