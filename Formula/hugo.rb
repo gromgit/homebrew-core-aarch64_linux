c6b301143037f2cc0b336a8213cb867f4c833608
ilovezfs <ilovezfs@icloud.com>
2017-09-11 01:05:33 -0700

    hugo 0.27
    Closes #17917.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/hugo.rb
+++ Formula/hugo.rb
@@ -1,8 +1,8 @@
 class Hugo < Formula
   desc "Configurable static site generator"
   homepage "https://gohugo.io/"
-  url "https://github.com/gohugoio/hugo/archive/v0.26.tar.gz"
-  sha256 "1e76b6f5f0281d845c2189f7f3ae065a3ec38e32c560f4b573188878998f9c68"
+  url "https://github.com/gohugoio/hugo/archive/v0.27.tar.gz"
+  sha256 "72e2e31c753f102a4a7633cf18d713bfa2788618472675d7d1d956b62ec59f9e"
   head "https://github.com/gohugoio/hugo.git"
 
   bottle do
