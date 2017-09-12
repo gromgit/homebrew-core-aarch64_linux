4259d70093b7e6d7bebfae38c53abd87aa94cea8
Jesse Tan <jessetan@users.noreply.github.com>
2017-09-12 12:16:52 +0200

    plantuml 1.2017.16 (#17981)

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://downloads.sourceforge.net/project/plantuml/1.2017.14/plantuml.1.2017.14.jar"
-  sha256 "08d83041814057d8d99c72ecada7e0f7e52e46265ee4af3ebc64525b63ef46ed"
+  url "https://downloads.sourceforge.net/project/plantuml/1.2017.16/plantuml.1.2017.16.jar"
+  sha256 "a3d319ed54ed78ce3cf7bfcfe76ffc5aa6ed28a72b5aa575fe9125e0c6bd985c"
   version_scheme 1
 
   bottle :unneeded
