c7d848cba74b3d5d53bb455f64fb3e9fc692dc26
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-12 06:05:26 +0000

    pony-stable: add 0.0.1 bottle.

--- Formula/pony-stable.rb
+++ Formula/pony-stable.rb
@@ -4,6 +4,12 @@ class PonyStable < Formula
   url "https://github.com/ponylang/pony-stable/archive/0.0.1.tar.gz"
   sha256 "b175d5a235676eaf48b62cf28d5c44f6c13b9986d5dc97f75b477285fe9ec8fb"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4ba9e20a3773db35d9ca8e8c56abe72a62714285196195d700ff5aef517de8d0" => :sierra
+    sha256 "d294894e3b412a47654afafa2a573b83f57aa7cc72938914c99b416fc249355c" => :el_capitan
+  end
+
   depends_on "ponyc"
 
   def install
