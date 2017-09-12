1445dea50f66e17d0367e4363a674744da3d7bee
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-12 12:40:29 +0000

    pam_yubico: update 2.24 bottle.

--- Formula/pam_yubico.rb
+++ Formula/pam_yubico.rb
@@ -6,10 +6,8 @@ class PamYubico < Formula
 
   bottle do
     cellar :any
-    sha256 "96f7147fccca005c066d53b1e6ba2bee6013c847b6bc0ec188d8af22e337870a" => :sierra
-    sha256 "5d9cb094345cede6f701f686f6660ae59c47b03192e85fa8701e6730ce859027" => :el_capitan
-    sha256 "93769c01776befd585bfcbd1f07992c695e5e3d65f33b8b656a04a4d1ddfb7c9" => :yosemite
-    sha256 "8b68985a95c26661f9cad4467449a60e826cbbf95e67c20a2edbbcf474cf13f6" => :mavericks
+    sha256 "68098087eb87639f10607c030e58b7cf7ae86a8a95ff863a17ca1e7cfa2ca5b0" => :sierra
+    sha256 "ce01a964d539a569478c9dd882a9652354d9d7ab35d935df9aa543eb31716b58" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
