c0e103b9cf76c3b71fef772a83183100cf565099
ilovezfs <ilovezfs@icloud.com>
2017-09-12 04:51:04 -0700

    valabind 1.4.0
    Closes #17984.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/valabind.rb
+++ Formula/valabind.rb
@@ -1,21 +1,9 @@
 class Valabind < Formula
   desc "Vala bindings for radare, reverse engineering framework"
   homepage "https://radare.org/"
-  revision 5
-
+  url "https://github.com/radare/valabind/archive/1.4.0.tar.gz"
+  sha256 "b2e4939912feada6138b8269d228ea82fb0f1391fd2e2e7003f404677b0cdbc9"
   head "https://github.com/radare/valabind.git"
-
-  stable do
-    url "https://www.radare.org/get/valabind-0.10.0.tar.gz"
-    sha256 "35517455b4869138328513aa24518b46debca67cf969f227336af264b8811c19"
-
-    # patch to support BSD sed
-    # remove upon next release
-    patch do
-      url "https://github.com/radare/valabind/commit/03762a0fca7ff4bbfe3e668f70bb75422e05ac07.patch?full_index=1"
-      sha256 "2d9eb2c9c1b64327bc444fc3fc94f7ef284535d9cf28d9ecf887859b253426b3"
-    end
-  end
 
   bottle do
     sha256 "6383f4c303ff8540f628bafe454a8ca30c7441542bed6bb2714126adb36605b5" => :sierra
@@ -33,8 +21,8 @@ class Valabind < Formula
   # Upstream issue "Build failure with vala 0.38.0"
   # Reported 6 Sep 2017 https://github.com/radare/valabind/issues/43
   resource "vala" do
-    url "https://download.gnome.org/sources/vala/0.34/vala-0.34.9.tar.xz"
-    sha256 "36662f77e36abf9ce6f46e3015c4512276e6361553bdcc2d75566ed83a1da19d"
+    url "https://download.gnome.org/sources/vala/0.36/vala-0.36.5.tar.xz"
+    sha256 "7ae7eb8a976005afecf4f647b9043f2bb11e8b263c7fe9e905ab740b3d8a9f40"
   end
 
   def install
