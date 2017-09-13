75bed3721110929183a0ab2f674ef707d5c0bc61
DanBuild <build@dan.cx>
2017-09-13 10:29:25 -0700

    yarn 1.0.2

--- Formula/yarn.rb
+++ Formula/yarn.rb
@@ -1,8 +1,8 @@
 class Yarn < Formula
   desc "JavaScript package manager"
   homepage "https://yarnpkg.com/"
-  url "https://yarnpkg.com/downloads/1.0.1/yarn-v1.0.1.tar.gz"
-  sha256 "6b00b5e0a7074a512d39d2d91ba6262dde911d452617939ca4be4a700dd77cf1"
+  url "https://yarnpkg.com/downloads/1.0.2/yarn-v1.0.2.tar.gz"
+  sha256 "8a31f8fa50ab6d5f8852025fb0ea4a50f2f8b82792f060fa99de0acc370b0698"
 
   bottle :unneeded
 
