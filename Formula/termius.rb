f396b00927dead2423ad936fc5ed66400e32d0bb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-13 05:11:04 +0000

    termius: update 1.2.6 bottle.

--- Formula/termius.rb
+++ Formula/termius.rb
@@ -9,9 +9,8 @@ class Termius < Formula
 
   bottle do
     cellar :any
-    sha256 "c8f96ab65d9e41bf2045bad4b54b16114c53b35e9c276db81d765bdbff5d617c" => :sierra
-    sha256 "8d46dcfd111018b959edb27bff7cfafe0b065a5d79899b06ee942d90cb06446d" => :el_capitan
-    sha256 "4a5bc67e7f631ecddefecc81f0d141925253a9d53b330d57312822586fd0f1b5" => :yosemite
+    sha256 "7a83a18c139f04696adfd0f4cf127060cef6b17ca005a0aacdfa543b47047e2f" => :sierra
+    sha256 "3476f0d2bc9cac2de557e5d0a85ef09ecccee16b6defe18230f67e7125f46ab9" => :el_capitan
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
