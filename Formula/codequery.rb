a4883cbaffd68260793f879fe79698db82d3130e
ilovezfs <ilovezfs@icloud.com>
2017-09-13 14:18:12 -0700

    codequery: fix checksum

--- Formula/codequery.rb
+++ Formula/codequery.rb
@@ -2,7 +2,7 @@ class Codequery < Formula
   desc "Code-understanding, code-browsing or code-search tool."
   homepage "https://github.com/ruben2020/codequery"
   url "https://github.com/ruben2020/codequery/archive/v0.21.0.tar.gz"
-  sha256 "9af232e0d3bb10e5defbc222a9e422f4580dd5766cfc7f921d38baa18675f98d"
+  sha256 "32c4fd9e7d1c05246b0b3866bb1ccb6eab5cf246b589ffcb9f1b32b07b5e6ff7"
 
   bottle do
     cellar :any
