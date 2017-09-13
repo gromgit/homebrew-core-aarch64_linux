119951603f92211609a36d0822b265b8d01600d9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-13 15:49:49 +0000

    ssdeep: add 2.14 bottle.

--- Formula/ssdeep.rb
+++ Formula/ssdeep.rb
@@ -4,6 +4,12 @@ class Ssdeep < Formula
   url "https://github.com/ssdeep-project/ssdeep/releases/download/release-2.14/ssdeep-2.14.tar.gz"
   sha256 "3aad00b51adf8f0086b37198e50dc779d2313b7d9df09a96bce73c5376dcdd36"
 
+  bottle do
+    cellar :any
+    sha256 "e1e51327a493f62a0ee23970e052e05037207b733d32f42280084c2d46b9d03b" => :sierra
+    sha256 "6e6d611d9391ce64ae983e2b1622c69ded52600f6e1791034e1d796d868e1123" => :el_capitan
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install"
