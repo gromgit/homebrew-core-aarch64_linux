a7a1a0683e3145721e6ac755fa9144221e7b43d8
ilovezfs <ilovezfs@icloud.com>
2017-09-13 14:18:19 -0700

    protobuf: fix checksum

--- Formula/protobuf.rb
+++ Formula/protobuf.rb
@@ -2,7 +2,7 @@ class Protobuf < Formula
   desc "Protocol buffers (Google's data interchange format)"
   homepage "https://github.com/google/protobuf/"
   url "https://github.com/google/protobuf/archive/v3.4.0.tar.gz"
-  sha256 "cd55ee08e64a86cf12aaadd4672961813f592c194ed0c9ad94da0ec75acf219f"
+  sha256 "f6600abeee3babfa18591961a0ff21e7db6a6d9ef82418a261ec4fee44ee6d44"
   head "https://github.com/google/protobuf.git"
 
   bottle do
