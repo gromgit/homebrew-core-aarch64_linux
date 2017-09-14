7894fd97320d33a15e46c3856fff41baa0dce957
Mike McQuaid <mike@mikemcquaid.com>
2017-09-14 20:25:34 +0100

    makedepend: add HTTP mirror.

--- Formula/makedepend.rb
+++ Formula/makedepend.rb
@@ -2,6 +2,7 @@ class Makedepend < Formula
   desc "Creates dependencies in makefiles"
   homepage "https://x.org/"
   url "https://xorg.freedesktop.org/releases/individual/util/makedepend-1.0.5.tar.bz2"
+  mirror "http://xorg.mirrors.pair.com/individual/util/makedepend-1.0.5.tar.bz2"
   sha256 "f7a80575f3724ac3d9b19eaeab802892ece7e4b0061dd6425b4b789353e25425"
 
   bottle do
@@ -18,11 +19,13 @@ class Makedepend < Formula
 
   resource "xproto" do
     url "https://xorg.freedesktop.org/releases/individual/proto/xproto-7.0.28.tar.gz"
+    mirror "http://xorg.mirrors.pair.com/individual/proto/xproto-7.0.28.tar.gz"
     sha256 "6cabc8ce3fa2b1a2427871167b62c24d5b08a58bd3e81ed7aaf08f2bf6dbcfed"
   end
 
   resource "xorg-macros" do
     url "https://xorg.freedesktop.org/releases/individual/util/util-macros-1.19.0.tar.bz2"
+    mirror "http://xorg.mirrors.pair.com/individual/util/util-macros-1.19.0.tar.bz2"
     sha256 "2835b11829ee634e19fa56517b4cfc52ef39acea0cd82e15f68096e27cbed0ba"
   end
 
