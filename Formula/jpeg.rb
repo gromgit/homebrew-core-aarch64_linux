5205c9486b3a7c769c5fc4b4659667e3caede324
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-14 22:08:10 +0000

    jpeg: update 9b bottle.

--- Formula/jpeg.rb
+++ Formula/jpeg.rb
@@ -6,6 +6,7 @@ class Jpeg < Formula
 
   bottle do
     cellar :any
+    sha256 "06fdebbd4693b6a25850fb885f8e8c9fd2389a5d0a1b78008d569cc04c441272" => :high_sierra
     sha256 "658fd93026eafbaf3d3a8c2db53aa8ffd0097837ac8f500477b2352433d81723" => :sierra
     sha256 "e22ae898671f312ed0a2d0eab8bf6a280e3f709642789c7932d30fc0e93cfb22" => :el_capitan
     sha256 "306863ccbb13b68787378b55a717784c214209496ac70d761f92ab03e52c03d0" => :yosemite
