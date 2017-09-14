085861a46c40a82cb504063668aa5c08b4847b38
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-14 23:22:53 +0000

    libgpg-error: update 1.27 bottle.

--- Formula/libgpg-error.rb
+++ Formula/libgpg-error.rb
@@ -6,6 +6,7 @@ class LibgpgError < Formula
   sha256 "4f93aac6fecb7da2b92871bb9ee33032be6a87b174f54abf8ddf0911a22d29d2"
 
   bottle do
+    sha256 "2b6f050bcd7b8c08f774aa491c2731aec585b0f1f14f972e9e485654314508f6" => :high_sierra
     sha256 "7ffa1c12c9a452f2292cb6a171b4e88136c631cee4da67d71143529bf3352a93" => :sierra
     sha256 "a81e55fecb35b921df543e8cf83729a8695d745fa778f716d4c96f989aceb6b4" => :el_capitan
     sha256 "06669414f7aba892558875994c411dc2a0723aa19f5d854b435987b3c8b9f19a" => :yosemite
