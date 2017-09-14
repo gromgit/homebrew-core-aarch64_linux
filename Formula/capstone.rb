c3805d6d522b00f6403b2c269c73822ffdd50eac
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-14 23:22:54 +0000

    capstone: update 3.0.4 bottle.

--- Formula/capstone.rb
+++ Formula/capstone.rb
@@ -6,6 +6,7 @@ class Capstone < Formula
 
   bottle do
     cellar :any
+    sha256 "82e40e06f3a41633326d3ceb7f268945441dcb7a0ae1caa88e06fc0504c73cc0" => :high_sierra
     sha256 "7d0f04a49d42bd9c953a5ea6cb85159f72f8e948d6aea4d7c64b3e82a12459f1" => :sierra
     sha256 "3aa8d8b679cc5261a3fbf44b191c61480cdb34576f71b769e63d68c5e27c19b1" => :el_capitan
     sha256 "5bbd8f7d9e0ae0d3b23c7d478fdb02476e8cee847577576d543bf98649985975" => :yosemite
