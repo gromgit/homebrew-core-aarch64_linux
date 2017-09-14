44a2e55109503fea00270f96ecabc8b6fcb0fed0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-14 23:22:54 +0000

    perl: update 5.26.0 bottle.

--- Formula/perl.rb
+++ Formula/perl.rb
@@ -6,6 +6,7 @@ class Perl < Formula
   head "https://perl5.git.perl.org/perl.git", :branch => "blead"
 
   bottle do
+    sha256 "bb431f3b89827667a5217cca4dcc6a4162b2aed5ff97c57444d0533881a2aa30" => :high_sierra
     sha256 "5a802e10df0c3845811f58c3f44f1c88d1a693d718997e3c45264055ec9393a7" => :sierra
     sha256 "238ee28350a29c19b8f154660ce58870fc0bf7033a54667a8e2c744a246dea47" => :el_capitan
     sha256 "ebb2181b33d2f08807d7a5d32e2c5332f861d9c3b3ca5fe46d3b7c14166f45f3" => :yosemite
