a753a4ee0794103ba1bbcca4d23ab2d284fc10d3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-14 22:08:10 +0000

    libpng: update 1.6.32 bottle.

--- Formula/libpng.rb
+++ Formula/libpng.rb
@@ -7,6 +7,7 @@ class Libpng < Formula
 
   bottle do
     cellar :any
+    sha256 "ffa7741d9147b0aacf2ea6427284d256e2096bc33aae1ab70b54fe5bd24dae7c" => :high_sierra
     sha256 "53c8fa0a036f36a0a01aaee8ab7138ee8bb1f436c0fec9c372f138b9840d5729" => :sierra
     sha256 "7ecb31ca907099430fbc4820b10f2fcb61b1a5f34ac7ceb7667b0295742485f2" => :el_capitan
     sha256 "01681368a1f310130415d4488772864bb22e0565c31ee4f6a9b491baec94210d" => :yosemite
