2bdd55ddbdf52801fe790851911b60d727414a16
dersvenhesse <dersvenhesse@users.noreply.github.com>
2017-09-14 14:28:17 +0200

    pgrouting 2.5.0
    Closes #18074.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pgrouting.rb
+++ Formula/pgrouting.rb
@@ -1,20 +1,14 @@
 class Pgrouting < Formula
   desc "Provides geospatial routing for PostGIS/PostgreSQL database"
   homepage "http://www.pgrouting.org"
-  url "https://github.com/pgRouting/pgrouting/archive/v2.4.2.tar.gz"
-  sha256 "f6d0df00279944f91ac672bdb6507a6c63755ba7cdd7e16022c4cb8ddaaf0034"
-  revision 1
+  url "https://github.com/pgRouting/pgrouting/archive/v2.5.0.tar.gz"
+  sha256 "2c97df865484bf4e4e6f059b535b63bbe64534076b9d0c4bdd494f916fa24e9d"
   head "https://github.com/pgRouting/pgrouting.git"
 
   bottle do
     cellar :any
     sha256 "5b65993f1e0d0ae7df63643809b87e58d3f1386d39afb06fd2548e7ea0600738" => :sierra
     sha256 "edad9c5855f3cc860bdcd07e16d0a3cbea9115cd3f0b411dd42157dfdb5c5776" => :el_capitan
-  end
-
-  devel do
-    url "https://github.com/pgRouting/pgrouting/archive/v2.5.0-rc.tar.gz"
-    sha256 "c0b091382b8df8ca7d3f7a4b168cfc13386276b6b7e47a2c6f85fc907081c3c0"
   end
 
   depends_on "cmake" => :build
