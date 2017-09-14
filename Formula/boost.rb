0f446d97ba00a3e9d8e2192c030164f067d3944c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-14 23:50:54 +0000

    boost: update 1.65.1 bottle.

--- Formula/boost.rb
+++ Formula/boost.rb
@@ -7,6 +7,7 @@ class Boost < Formula
 
   bottle do
     cellar :any
+    sha256 "b7b199b8c4e40a8a32a9c8852989645c1e1e30e9f11b49927f400299ab60b1cf" => :high_sierra
     sha256 "3d896801bccfc3f3ee1934e5764a5a613eaae8fda5567deb8e9f8b3fa0c2727f" => :sierra
     sha256 "97f9486b46396c659741b069697e9acc5e146c8df12631a4981d7ad168f806e2" => :el_capitan
     sha256 "0970b4eef313fe6188a4318b949a727d658ef31522aebc3ddaa44a877b67444a" => :yosemite
