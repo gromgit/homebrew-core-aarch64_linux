f6f4cf954225cd9ef7a3b4d13bfab34aa4a51e1e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-14 23:42:28 +0000

    intltool: update 0.51.0 bottle.

--- Formula/intltool.rb
+++ Formula/intltool.rb
@@ -6,6 +6,7 @@ class Intltool < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7924c9c7dc7b3eee0056171df8c6b66c2e0e8888e4638232e967a5ea31ca5b86" => :high_sierra
     sha256 "e587e46b6ebdebb7864eb4f9cb17c221024d9167ae0148899adedb6127b2bdfb" => :sierra
     sha256 "14bb0680842b8b392cb1a5f5baf142e99a54a538d1a587f6d1158785b276ffc6" => :el_capitan
     sha256 "da6c24f1cc40fdf6ae286ec003ecd779d0f866fe850e36f5e5953786fa45a561" => :yosemite
