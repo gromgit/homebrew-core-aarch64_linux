cf2dd80659fbf6fa12eb8da2c4b59c4cb25d3c3e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-14 23:22:54 +0000

    unixodbc: update 2.3.4 bottle.

--- Formula/unixodbc.rb
+++ Formula/unixodbc.rb
@@ -7,6 +7,7 @@ class Unixodbc < Formula
 
   bottle do
     rebuild 1
+    sha256 "67d8a07e5abf3f964101d94911321271ebd755076f4a39f73657b826619b88df" => :high_sierra
     sha256 "b3117348129272e966e9fd3e37f29ccdffbf9a0dde9d8e2691a3185feaae92ff" => :sierra
     sha256 "78bb1bb4468cb34d0acf6566ecaad6535a9b77ff7a740bafd2ed91e1d7812c70" => :el_capitan
     sha256 "82f217f67f78a6dab125b77558e99f9ee18227f991ab8d842e881e83d0c7d917" => :yosemite
