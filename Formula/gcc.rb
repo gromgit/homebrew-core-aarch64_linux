6f46cf93b288cff919e2a8ee41af4eac5e51ba81
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 00:36:24 +0000

    gcc: update 7.2.0 bottle.

--- Formula/gcc.rb
+++ Formula/gcc.rb
@@ -11,6 +11,7 @@ class Gcc < Formula
   end
 
   bottle do
+    sha256 "3b7606d2b98cf9ca5c25d2620d2c9d6c146a910f6063c071ac4bf5abdeb73faa" => :high_sierra
     sha256 "bc96bddd0e9f7c074eab7c4036973bc60d5d5ef4489e65db64018363d63d248d" => :sierra
     sha256 "755ed27d3aa9b60523aead68f36d17f6396b9f4b622a0972c05eae3302922d5c" => :el_capitan
     sha256 "eecedf7c9233bd1553d3e22027f415f15a9d1a7ad11e486855bf3a8f7d36ed23" => :yosemite
