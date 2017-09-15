039d56a20d8178b9a501c4a77b6042af802e7e80
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 23:44:56 +0000

    veclibfort: update 0.4.2_4 bottle.

--- Formula/veclibfort.rb
+++ Formula/veclibfort.rb
@@ -8,6 +8,7 @@ class Veclibfort < Formula
 
   bottle do
     cellar :any
+    sha256 "25835893af5a10d804e69da1bc6ea998e2de9be5585d39cf8869b42f4d34d1d0" => :high_sierra
     sha256 "62d957a0a6448a180babab06bbc02ec1cc87eec18a19f4f5cdaac7b86e0c3d69" => :sierra
     sha256 "d713b2879ad5a435e3adcc7ed50ad8c3e99e7e17abd57bef3940f1dcbd5dbfae" => :el_capitan
     sha256 "6dcd136a00a22f25301da83534a6faf0171b391e00baf45b790d04d551c7202e" => :yosemite
