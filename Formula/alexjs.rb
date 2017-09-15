452490d88f45370cbba50e175e01076deb10becd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 10:20:03 +0000

    alexjs: update 5.0.0 bottle.

--- Formula/alexjs.rb
+++ Formula/alexjs.rb
@@ -8,6 +8,7 @@ class Alexjs < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5e3301e477d01680b172594ef06d384c37d5c90309034ede42e1ef8f09c3191e" => :high_sierra
     sha256 "abac2c52f7cd7567f2777c0e5b262064437863f4f75313d11eb76e9e7878fdbc" => :sierra
     sha256 "db81db3e1657c63523b7d1564085909854e8ed61c4fcf31d3868ee0e43ca4c62" => :el_capitan
     sha256 "6be3f84ce78a39997be986bb1d6c3e679fabd4d83fb3389f5b09375aac913802" => :yosemite
