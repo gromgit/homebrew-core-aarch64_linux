020c6d3d62ff4bd39d5f3d5042ec4df6661bf83a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 16:07:41 +0000

    pidof: update 0.1.4 bottle.

--- Formula/pidof.rb
+++ Formula/pidof.rb
@@ -8,6 +8,7 @@ class Pidof < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "fd5f89cf3a9685142e08a23980d9438e961096d74ee508a96ccbaecb55da6e1a" => :high_sierra
     sha256 "6991d110a73724959f84edc398647e3cac5a029645daedef5f263ae51218130d" => :sierra
     sha256 "d02c826db5564d7750c0e309a771b164f7764250507955d0b87d09837c3c2ba6" => :el_capitan
     sha256 "54ffe0be6ef278aa45cacb856687df925bab1c117d1ab4c1a8f81ae835fb293e" => :mavericks
