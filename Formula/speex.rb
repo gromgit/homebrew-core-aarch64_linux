4ed39b1008042b64e57b1f91e432d8d214a48b50
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 07:50:16 +0000

    speex: update 1.2.0 bottle.

--- Formula/speex.rb
+++ Formula/speex.rb
@@ -6,6 +6,7 @@ class Speex < Formula
 
   bottle do
     cellar :any
+    sha256 "525970161e7c1629b242c91d889201ca368814945695efd5b441d58b5b5dcc75" => :high_sierra
     sha256 "5aa61761fb5426de78297fdc83579515dda1a880f47c925cb3405b7175079b92" => :sierra
     sha256 "056781a4d7c5fe9a05f30160c059352bda0a4f8a759820df7dde7233aa08cba5" => :el_capitan
     sha256 "a0b3c91782b8242508adac3ebc0cd86688e75b043ea0d84f4ef7ac9940f8a21b" => :yosemite
