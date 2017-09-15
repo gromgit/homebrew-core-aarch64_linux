59c136223c2cc607b9a890fb8b3000c612956f07
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 18:44:03 +0000

    orc: update 0.4.27 bottle.

--- Formula/orc.rb
+++ Formula/orc.rb
@@ -6,6 +6,7 @@ class Orc < Formula
 
   bottle do
     cellar :any
+    sha256 "0d14acdd8539f1aaf104c8e72a0cde4e7d163afb9d5afb58df3a04babd0094aa" => :high_sierra
     sha256 "6e9283bf3a50c68724965b8abbf7ee1084d3e086bb50d96af7ee5fca420a078c" => :sierra
     sha256 "6ce6a66ae7ff4321144f66e02f6d71be139336ea5256fd28d0d78be2188eee29" => :el_capitan
     sha256 "39aec42200bf5957c7b8d6c1c4357c9397eddd82f0df7b7acb36f146734c3b3f" => :yosemite
