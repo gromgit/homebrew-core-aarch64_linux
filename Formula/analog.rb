cc1ea247cd6f15de2d6a6cb90c29dd9fdeccd3d1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 23:43:23 +0000

    analog: update 6.0_1 bottle.

--- Formula/analog.rb
+++ Formula/analog.rb
@@ -10,6 +10,7 @@ class Analog < Formula
 
   bottle do
     rebuild 2
+    sha256 "d6cf3bcc19b376b693cc27ccd0ebeafb80a05c783405a13ed0d24abd07368cb2" => :high_sierra
     sha256 "cb8cb25d3050dc3a08445987739c43b5fd7dad7a798342fb7538c016930a9978" => :sierra
     sha256 "097f11e7f53078e6b248e38fc326cded49b08cdbe75ab61e20ab7b2a6e770256" => :el_capitan
     sha256 "f2f29ea2dcbb9e0576c72f009d8814b0c7f84efd49d6f005085c876c85fd29b9" => :yosemite
