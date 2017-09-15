31f8e5673ff8f45ed54a68fe2686d38baad1d578
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 12:21:25 +0000

    portaudio: update 19.6.0 bottle.

--- Formula/portaudio.rb
+++ Formula/portaudio.rb
@@ -9,6 +9,7 @@ class Portaudio < Formula
 
   bottle do
     cellar :any
+    sha256 "c8c55723ecdb61b8e4f4431062814d6ce8a267a19fe3e34c70b2bd677a0e20f9" => :high_sierra
     sha256 "4fb62387583b02607e013f376c02b4a1f6c2a2fa9b68ee43e79c9c04d12f9a45" => :sierra
     sha256 "96afa37e0de1723e4fa206360f189ed0486ecd74a5554dcab75eb47395be78db" => :el_capitan
     sha256 "64b21e55c28066264ee09918c045b77c0b1049a19f8df4636283ce17b1d84944" => :yosemite
