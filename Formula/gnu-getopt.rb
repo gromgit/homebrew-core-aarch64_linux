4b11c2be868fcf51305c7470b6a878dc50bdbecf
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 07:28:47 +0000

    gnu-getopt: update 1.1.6 bottle.

--- Formula/gnu-getopt.rb
+++ Formula/gnu-getopt.rb
@@ -5,6 +5,7 @@ class GnuGetopt < Formula
   sha256 "d0bf1dc642a993e7388a1cddfb9409bed375c21d5278056ccca3a0acd09dc5fe"
 
   bottle do
+    sha256 "5dc8b07eb3425e5b57d7deb4dea187fc992ef358c9c053d3a2dc230d748b4252" => :high_sierra
     sha256 "05391b0dd0876ead74b18a4c5bb9c7db996586bf6918bd014db534027fd9ae2a" => :sierra
     sha256 "5e9e87fe18c5681e80f1cf940fed275ed895831304326bc5e7be6fb6e53e8594" => :el_capitan
     sha256 "f8dbbec03aaaeb1bc774d9bf606701901cc9a8ad15cecc5473567e51845057e6" => :yosemite
