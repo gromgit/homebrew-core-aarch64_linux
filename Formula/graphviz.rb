66661ca9ddb072a94c5830f0461f48a84a22bfdc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 12:41:46 +0000

    graphviz: update 2.40.1 bottle.

--- Formula/graphviz.rb
+++ Formula/graphviz.rb
@@ -7,6 +7,7 @@ class Graphviz < Formula
 
   bottle do
     rebuild 1
+    sha256 "b592ce51c2a929c3da82e96ec856571ebfc54cf4dac90c2924cd3845078d7082" => :high_sierra
     sha256 "41b5811054f03978db12525919540fe41e073fb2c20e899247ed9c2a191f7a66" => :sierra
     sha256 "cab27f92a59d543e2f2c1494c28c7563a4c2d7e0dce4c4fbc22587db91cafc5b" => :el_capitan
     sha256 "6bd4c01e724cfc965871e1aad9a4fb2a6afef90a1e254d81e2fe33a997f50aaa" => :yosemite
