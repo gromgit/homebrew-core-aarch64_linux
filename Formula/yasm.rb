7b9292b183fef24d6f7ec0c1409e6b874ccc2ec1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 07:38:28 +0000

    yasm: update 1.3.0_1 bottle.

--- Formula/yasm.rb
+++ Formula/yasm.rb
@@ -7,6 +7,7 @@ class Yasm < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ddb536410f898cba342c9c2d01696a690a28a4f1e9e30c67a3e352a41791fc85" => :high_sierra
     sha256 "2ca19bb3f563569aa4eac4fd3398a7eb14a7fb1268b4ffe86ad7980f9701d1b7" => :sierra
     sha256 "0dd9ef773dfbf9c59ab13c1fb7ec616d1ffba1c240357497d75482e0743c4119" => :el_capitan
     sha256 "fc205e75319ba9e63a2e5fa6beccc66a163325a3a4a51807a2cf1844512f2c24" => :yosemite
