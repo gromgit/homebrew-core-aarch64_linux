44c220f9d37255126ce156147d4360e243b63581
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 11:39:20 +0000

    libgsf: update 1.14.41 bottle.

--- Formula/libgsf.rb
+++ Formula/libgsf.rb
@@ -5,6 +5,7 @@ class Libgsf < Formula
   sha256 "150b98586a1021d5c49b3d4d065d0aa3e3674ae31db131af5372499d2d3f08d3"
 
   bottle do
+    sha256 "76b39b701b7d152744861ccc137107277589f65d7b731a24fff6404fa146fe48" => :high_sierra
     sha256 "c7540109a9d799969ccf04f4cebd1c55c20e2f0b93313b3d0d0502ea6be28ade" => :sierra
     sha256 "434efb359e8f149061659e9173826fa9e2ade089ad4cff88da0efce0070c813e" => :el_capitan
     sha256 "f0f105ef96ede22df075cc256385c9df5f9c61832a9eb60a86aa1087696b4c85" => :yosemite
