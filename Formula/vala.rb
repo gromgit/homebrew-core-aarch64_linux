9dd10601d07772c7fa183b52669693cdec98cb71
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 12:41:46 +0000

    vala: update 0.38.1 bottle.

--- Formula/vala.rb
+++ Formula/vala.rb
@@ -5,6 +5,7 @@ class Vala < Formula
   sha256 "1035344f3ba50a572894c0aed3f794dd0efb9f08fce1c1d0cc537936e9a05084"
 
   bottle do
+    sha256 "47cf2afd0ae28dd89669a75f7e6ec6654a860914e70811f660ea211934021cd1" => :high_sierra
     sha256 "0ce8315e19c9f57bbea817849f420894c73195207866d2aa6075b5cbb5b5678e" => :sierra
     sha256 "abf8b5e9f4ab9df366f18d1c76c5a9244ca09ac845bce85e9a3dac35c1bc9696" => :el_capitan
   end
