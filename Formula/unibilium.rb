7705326b28617372c9e964514f76c54d87625e86
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 22:54:25 +0000

    unibilium: update 1.2.1 bottle.

--- Formula/unibilium.rb
+++ Formula/unibilium.rb
@@ -6,6 +6,7 @@ class Unibilium < Formula
 
   bottle do
     cellar :any
+    sha256 "0eabdc993e3e9af53e634963ef18f3735216eaf6da7cb12802643f0b9fb3e7df" => :high_sierra
     sha256 "53b708223a2e95298390b6313ff4917f10efefc3cf349eb57688938f982ee487" => :sierra
     sha256 "5c4b97baf010a4d3482f7270e313a274a281df71beb55a8ce69b473611c955ae" => :el_capitan
     sha256 "a43da6e759273b81f501077deb01633b54591a907e2cef40ba81b1611af5692e" => :yosemite
