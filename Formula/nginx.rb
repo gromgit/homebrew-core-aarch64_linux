82de8202af91989e7d42fad9f5e9b8426aa7248d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 23:43:24 +0000

    nginx: update 1.12.1 bottle.

--- Formula/nginx.rb
+++ Formula/nginx.rb
@@ -7,6 +7,7 @@ class Nginx < Formula
   head "https://hg.nginx.org/nginx/", :using => :hg
 
   bottle do
+    sha256 "97cc615490e05b2c642d9c317e6bc64c00503f4ffbd763a1e1a13f26d553409d" => :high_sierra
     sha256 "93bcf8e3aec465c219b6c0b4f4d5437c61bf00f2a930ef5702e0521edc51f20e" => :sierra
     sha256 "8a7c3580534aa0854927f750d4f044a2a85f90d4c1936338a4a09fef7db0824e" => :el_capitan
     sha256 "0caae754f402abbe1eca413a7f0291fe2499d5779bb1e537d7f80a4d7d3156d3" => :yosemite
