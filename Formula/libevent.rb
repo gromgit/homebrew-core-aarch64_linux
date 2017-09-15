c8d47cf94f339b87b2522361533b4bd35bc63a2a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 06:53:08 +0000

    libevent: update 2.1.8 bottle.

--- Formula/libevent.rb
+++ Formula/libevent.rb
@@ -6,6 +6,7 @@ class Libevent < Formula
 
   bottle do
     cellar :any
+    sha256 "61a8cf2df6d58f79678fad0f798b6a9d368245097f908e2f911f25cb3f7916cf" => :high_sierra
     sha256 "cdd11d67b5f49b94cf3fbfd24753f84082957e3d55680e2b5979eec19091e694" => :sierra
     sha256 "136a93a91c3724d0403b0d43d0b9a4bf6b857278c4ebb7c7585ef70a19b0964c" => :el_capitan
     sha256 "ef703db1b4cbdab35b89aabe80c225dd9b7a2c3ea14b1eae681478c5b9df15fe" => :yosemite
