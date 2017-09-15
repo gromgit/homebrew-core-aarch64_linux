36df25e2ca596fff165970fd4314454e2eb7d08f
Damian Janowski <jano@dimaion.com>
2017-09-15 14:16:39 -0300

    clac 0.3.0 (new formula)
    Closes #18125.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/clac.rb
@@ -0,0 +1,14 @@
+class Clac < Formula
+  desc "Command-line, stack-based calculator with postfix notation"
+  homepage "https://github.com/soveran/clac"
+  url "https://github.com/soveran/clac/archive/0.3.0.tar.gz"
+  sha256 "a7a2397fdbac93924ca83c72e2df2027029f54c8921b813230208dd167ed0c20"
+
+  def install
+    system "make", "PREFIX=#{prefix}", "install"
+  end
+
+  test do
+    assert_equal "7", shell_output("#{bin}/clac '3 4 +'").strip
+  end
+end
