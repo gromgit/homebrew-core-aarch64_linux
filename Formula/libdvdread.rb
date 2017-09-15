97b96608a72c440bf4da2a1f4d297dd7f1c15498
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 12:06:19 +0000

    libdvdread: update 5.0.3 bottle.

--- Formula/libdvdread.rb
+++ Formula/libdvdread.rb
@@ -7,6 +7,7 @@ class Libdvdread < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "df09ae7b2e9919423316037c551e7ba4505be5315b6c22137361889c4bde6cea" => :high_sierra
     sha256 "9a6674198c5624c82f7abe163c606f0f372a02a42a49a3f259f1d74d0c1e2f46" => :sierra
     sha256 "9de98e88e99fbcc899a299786575472c93d442b06838f16bb757e09d4ba92593" => :el_capitan
     sha256 "75006f367876e6ccce744d782f1204ea99d73b55a856ce0afaae2c194eac336c" => :yosemite
