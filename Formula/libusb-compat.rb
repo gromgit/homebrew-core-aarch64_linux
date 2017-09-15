f2ac324398b1788d370ca1eccd1ce98db712b6b1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 07:50:16 +0000

    libusb-compat: update 0.1.5_1 bottle.

--- Formula/libusb-compat.rb
+++ Formula/libusb-compat.rb
@@ -7,6 +7,7 @@ class LibusbCompat < Formula
 
   bottle do
     cellar :any
+    sha256 "fccc08c6c3ff2bf93d2aa8e7cc18f30c1fb95fbca044ecaa42d45f7c73a8facf" => :high_sierra
     sha256 "e24ad80ee860f6f6c7e6c8dbb100aaa2de3294e2ecf7f591f2f51c52e11f09ea" => :sierra
     sha256 "7b62449f8a9c02834b74adeb0827ca2ae32b47cb82923de0a8e88f16c36ca8b8" => :el_capitan
     sha256 "0e4f131b8fd8210db3ff353a92c35ed12643a717b8780618680e3b4a16d7f347" => :yosemite
