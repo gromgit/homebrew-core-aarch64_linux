bbb5ad12b5417bf99678ba0e9b1deb32c9530406
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 11:39:19 +0000

    wget: update 1.19.1_1 bottle.

--- Formula/wget.rb
+++ Formula/wget.rb
@@ -9,6 +9,7 @@ class Wget < Formula
   revision 1
 
   bottle do
+    sha256 "f0b01475fa8fb7f99b7e5e75f7c06e99a609cd9a3f11b62c10f4c76a9f46dbff" => :high_sierra
     sha256 "fe0679b932dd43a87fd415b609a7fbac7a069d117642ae8ebaac46ae1fb9f0b3" => :sierra
     sha256 "a4d259460edf940de5c780e8461c23e3bced288e2ef2532bd1707a086f9842b9" => :el_capitan
     sha256 "61b3eab1439b8dfaed1f18dbd6e8d0ad87d15b9864677e62f3618cc02600064a" => :yosemite
