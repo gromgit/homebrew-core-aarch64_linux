4e73997ecc1189b3f34074237aaf50efc0da190e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-15 13:49:48 +0000

    openjpeg: update 2.2.0 bottle.

--- Formula/openjpeg.rb
+++ Formula/openjpeg.rb
@@ -7,6 +7,7 @@ class Openjpeg < Formula
 
   bottle do
     cellar :any
+    sha256 "f460c31ee19c3dd02e9e5e336de098a98afbd71e0a50eb53db691c6321cedb72" => :high_sierra
     sha256 "437b7f58d8f2e8944adea7481a233bdf7f5c06609bfcf209169e677c93ab621c" => :sierra
     sha256 "ceebb6f74ce06b2a9ea716cd6f72bdbe4590b23819e4d0a980a320ff150760bd" => :el_capitan
     sha256 "bd0c66eb1f759d447a35203a0861698283ee148c97b96ed13922d83adaab4ab7" => :yosemite
