9d15a4b21b03cb0e9452affcd423ccc1e50a0f05
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 05:45:41 +0000

    isl@0.12: update 0.12.2 bottle.

--- Formula/isl@0.12.rb
+++ Formula/isl@0.12.rb
@@ -9,6 +9,7 @@ class IslAT012 < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "8d222adaa566a6f8b8b50c391c5f95dce795472e2cf3926d3aa6d1408d4cfab4" => :high_sierra
     sha256 "9ca9d2bc65afcdce0d1816a98273701a2381c5ae8b58e47b2237d0dd89d6181c" => :sierra
     sha256 "57e01a30d3847bfa805c21e0cf8b45bfe958e10a0d3c2e8d225e780dba6f93f5" => :el_capitan
     sha256 "4da54a5f24e4d7dd0cd315096ee4a3c3a26388d8368f80b6a9bc0735c7b259ac" => :yosemite
