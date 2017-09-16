e1883b3e4970299990ca6be1a4c629034b2292da
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 11:38:28 +0000

    clutter: update 1.26.2 bottle.

--- Formula/clutter.rb
+++ Formula/clutter.rb
@@ -5,6 +5,7 @@ class Clutter < Formula
   sha256 "e7233314983055e9018f94f56882e29e7fc34d8d35de030789fdcd9b2d0e2e56"
 
   bottle do
+    sha256 "082e8d9fa6a2fc3e69a572734847373a47884bef8ddebba763219e5252b6e334" => :high_sierra
     sha256 "16b3ecbbf37e62ce9e491ca525a948571f51c349fa25ba381db566fd0f825457" => :sierra
     sha256 "3bfa2df55eed3598e510999b02f8d226eca4a4fc58ba7adcace0e443ca5cc64e" => :el_capitan
     sha256 "c224d96814d9ef9c1929d2e4dbe4251e09395bbf9d61fc3a9f20886daf8581c8" => :yosemite
