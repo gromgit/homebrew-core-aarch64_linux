cea9518344a6bccbcde3e43fcfed183f7a9b80b2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 13:34:45 +0000

    wdfs: update 1.4.2 bottle.

--- Formula/wdfs.rb
+++ Formula/wdfs.rb
@@ -6,6 +6,7 @@ class Wdfs < Formula
 
   bottle do
     cellar :any
+    sha256 "204b9c0227c3ccf14444259fea4e26cedc42eb2e461ce02952bf4d7f2802fb66" => :high_sierra
     sha256 "d88bd5f277e42a04048c6641e764c919b71b4cec554440ddb590b608c7d02955" => :sierra
     sha256 "7133df159d9ca995fd982a7ffc2e3213112113e740c6ed5a5c48458d1f1bd1c3" => :el_capitan
     sha256 "f427e5e1cde130dc890db1f27e7bbc8600ae9a22dfbb3fcdc8ada9eb73686f51" => :yosemite
