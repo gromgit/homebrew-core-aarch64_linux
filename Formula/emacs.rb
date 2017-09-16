bdd87c6bf222ef401db694cd92c8a2c9b9e53657
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 12:05:59 +0000

    emacs: update 25.3 bottle.

--- Formula/emacs.rb
+++ Formula/emacs.rb
@@ -5,6 +5,7 @@ class Emacs < Formula
   sha256 "253ac5e7075e594549b83fd9ec116a9dc37294d415e2f21f8ee109829307c00b"
 
   bottle do
+    sha256 "d5ce62eb55d64830264873a363a99f3de58c35c0bd1602cb7fd0bc37137b0c9d" => :high_sierra
     sha256 "4d7ff7f96c9812a9f58cd45796aef789a1b5d26c58e3e68ecf520fab34af524d" => :sierra
     sha256 "7bf6dfba77259ef5454696834c14fcab3643197ba70eef1e608476167c3d387b" => :el_capitan
   end
