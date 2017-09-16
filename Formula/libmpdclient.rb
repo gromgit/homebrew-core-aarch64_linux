1be238321b24169623a61dd2321ecf254b73bf07
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 01:49:53 +0000

    libmpdclient: update 2.13 bottle.

--- Formula/libmpdclient.rb
+++ Formula/libmpdclient.rb
@@ -7,6 +7,7 @@ class Libmpdclient < Formula
 
   bottle do
     cellar :any
+    sha256 "af530e739905c173c9327f560c7a04ba39f8c5a3476d1c2d0516be62dc967789" => :high_sierra
     sha256 "30f5b095ddc086dbb8d15b82f0184f83308c7bd5d020facacee2b27c54757add" => :sierra
     sha256 "56530667e499edce59960bf7e7ef3fe3cda08b96c9a8af1f6e056a98ae86c0fc" => :el_capitan
     sha256 "a4eab9eb2e1e33ef07412d5edb202983245f2a5b285771d352eb87c5fa162f4b" => :yosemite
