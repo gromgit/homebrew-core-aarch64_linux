2e4c2bb04c08074723274e618343301038777663
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 11:10:27 +0000

    opencv: update 3.3.0_3 bottle.

--- Formula/opencv.rb
+++ Formula/opencv.rb
@@ -6,6 +6,7 @@ class Opencv < Formula
   revision 3
 
   bottle do
+    sha256 "386ac2caf18b558237cf8a8a510fce0c56213259ce18e8bcf418513829e790fe" => :high_sierra
     sha256 "cf9f5f528cb2217a4fde2c635ce90d07b51e01564d7c66fc4413c41e22b17572" => :sierra
     sha256 "bd88772952a81606b7ff43e7d3874213680d4d190c319360f3da603b76f1cfb3" => :el_capitan
     sha256 "6073b44f5b601de364147a9b6b3ee784c6070303582cf3db9a513ce8a1796a0c" => :yosemite
