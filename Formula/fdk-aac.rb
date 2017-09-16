56b084c9bbc5a441667b980675302d2569efc771
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 09:01:58 +0000

    fdk-aac: update 0.1.5 bottle.

--- Formula/fdk-aac.rb
+++ Formula/fdk-aac.rb
@@ -6,6 +6,7 @@ class FdkAac < Formula
 
   bottle do
     cellar :any
+    sha256 "2c64ad6b69b8c0aa6787d001eb2f9abfcb4bd420ad3dc13e476ef2e02202a7c2" => :high_sierra
     sha256 "dcedf1b0e8d29c6edefcef515845828bf743cd6520498ddb648b3a1a3ecc6599" => :sierra
     sha256 "d36cd5e64d8c77c7658cc221fd5cef8cf110add87f0aebc5875c6c5059c48cc5" => :el_capitan
     sha256 "77b887abb2bf1249334aad8a26c6a66af562d923a23f742e042bcbcbf2dd1f38" => :yosemite
