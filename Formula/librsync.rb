bd6a7f055af9d2a13fb926d877ab37a83f9ca44b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 09:30:41 +0000

    librsync: update 2.0.0_1 bottle.

--- Formula/librsync.rb
+++ Formula/librsync.rb
@@ -6,6 +6,7 @@ class Librsync < Formula
   revision 1
 
   bottle do
+    sha256 "de4c71e13829b6344a859c01145f2cd599b4e456e9c1dac6049f657b3b42dcfe" => :high_sierra
     sha256 "f17fa395aecc8198c3a09d6eb483a45d0a2b091280f18b5c6576cf3c7b54557e" => :sierra
     sha256 "f9b52eb45dd0cc166e5d31e3a90cd02bd99c619a5893dd3c8129757528e410cb" => :el_capitan
     sha256 "31709e2fe6f0480a825209e7aa13602f768db43be57477738e2f7f7150213869" => :yosemite
