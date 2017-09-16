6fbe540a9bbaa01fdb0d9ad6cd5b4c7e136be724
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 12:55:14 +0000

    pwgen: update 2.08 bottle.

--- Formula/pwgen.rb
+++ Formula/pwgen.rb
@@ -6,6 +6,7 @@ class Pwgen < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "185f2f56eb03da60277520734452204ec2e0059cbc1f0af5d0fec1e7fa837658" => :high_sierra
     sha256 "01a0709f74923e7b86d680f03d3ec056d3175cb7e54be176a26d5bfae890fd21" => :sierra
     sha256 "7dade70b172cb91635afffe8bb1eadb251f9dbd3368ab9e4a37f98a7c0a14b01" => :el_capitan
     sha256 "1799bdbb42974d10e2ff3a4e382351b1f03f0a3be31c15ff718d8935d1226101" => :yosemite
