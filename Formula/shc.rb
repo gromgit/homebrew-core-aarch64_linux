e5c86197e80bc2f616b770c6bdc42d2a0a5e0bba
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 08:11:04 +0000

    shc: update 3.9.6 bottle.

--- Formula/shc.rb
+++ Formula/shc.rb
@@ -7,6 +7,7 @@ class Shc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "733638c58c4638ae63dedbcd35ebef1bb20365c74978f7bdbb3059a7a1039e34" => :high_sierra
     sha256 "f7c53fd5fa9c86057260ad66961406b4579738b30091d7ca6899eb5349013b0c" => :sierra
     sha256 "c54ddaec872f8c3613c53f7b1653250f3a11ad6db789418f8921be96fff6e8a3" => :el_capitan
     sha256 "0b40ee06c9a5db74e302be41adea362975bf86c9c5e6290da13a623341e4468d" => :yosemite
