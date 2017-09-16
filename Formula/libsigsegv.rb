5cf724d614da44f889aaec1bb03ec52f49f22dcc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 09:30:40 +0000

    libsigsegv: update 2.11 bottle.

--- Formula/libsigsegv.rb
+++ Formula/libsigsegv.rb
@@ -7,6 +7,7 @@ class Libsigsegv < Formula
 
   bottle do
     cellar :any
+    sha256 "8309550c503ae621b9d105a0cf8916afe71dc75c173c90751656dc6ea93319c0" => :high_sierra
     sha256 "ab8908ac5dd0796d8f3f4452d057ac18cc335f909f13ad866f8c721f043d3ce3" => :sierra
     sha256 "ded2b14d1110f67dcc52f79525e3b37ba85943cc487b14e4e330f28963c8ebf2" => :el_capitan
     sha256 "71448886166937e22df81d47a2836470fed0ddc4349cac1dec7f668c9048e398" => :yosemite
