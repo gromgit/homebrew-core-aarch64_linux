07fe587609091e5e5ddcce9992e11e541dfaa433
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 12:13:24 +0000

    glpk: update 4.63 bottle.

--- Formula/glpk.rb
+++ Formula/glpk.rb
@@ -7,6 +7,7 @@ class Glpk < Formula
 
   bottle do
     cellar :any
+    sha256 "cf81b5c532d726bb08ae3ebb5c22e7ad763334ecc586b4ec7eafeba49b9a8dc4" => :high_sierra
     sha256 "c5491836b25e7fe78f37d56b72406f9dabf9caeec8ea97e1b4b0356ab228fd6c" => :sierra
     sha256 "af604b5830e07dafadc57b26b84c352d6de62904dfb2587dcdc6a201601d8cda" => :el_capitan
     sha256 "ea544cd7cc6b81e513909ac35825cd4b67ae5d7500d2553dad3eb679939d7f71" => :yosemite
