6c4ecf43ab1a5794344dd3ca8a3d1423ddf50a31
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 09:28:57 +0000

    docker: update 17.07.0 bottle.

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -7,6 +7,7 @@ class Docker < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9059211aff9fac56d70b2b3159db5b5857fa2d9524e84fa7a88029642d353fe4" => :high_sierra
     sha256 "11b32ee5016223a9f4f2d1268bf0aa4b0bd23836ed3dc2cb4284b5771cc96080" => :sierra
     sha256 "d671ad1994cbb5452459b7fd6aae3d9365cce5593974c09f48c6de44330ee6b7" => :el_capitan
     sha256 "115c22e0f10c80376f04514295fdd3ef41c95c10bb7c658b2670fb921b556501" => :yosemite
