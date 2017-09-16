c66b0b5aeea4960ad76d323242afb97342a3875c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 00:25:37 +0000

    ossp-uuid: update 1.6.2_2 bottle.

--- Formula/ossp-uuid.rb
+++ Formula/ossp-uuid.rb
@@ -9,6 +9,7 @@ class OsspUuid < Formula
 
   bottle do
     cellar :any
+    sha256 "a04214b22c58bd5167778925cb9e55b98f28330bcc6c6a37929e6085ea3a0162" => :high_sierra
     sha256 "3c15cd0e25e3039e0d05b94d14b714745cec3033863d5dc7a6d9ddd7cacc1c71" => :sierra
     sha256 "ac4456fc1c29db7e0d565ebdd392cf827be315b52c9eb3abcd113c4c7b981f25" => :el_capitan
     sha256 "c6cfa39816d19fa8d4586d6a364cd17e3a089ea018242875dc371731578a4ac7" => :yosemite
