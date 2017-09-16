469f3051e8a6ee53cd9cf75336b4e485b62b4e5c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 08:03:07 +0000

    rpm2cpio: update 1.3 bottle.

--- Formula/rpm2cpio.rb
+++ Formula/rpm2cpio.rb
@@ -7,6 +7,7 @@ class Rpm2cpio < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "82c3021a2e882829d635ff45fc9780e72d0199777aa5dbbeb6f07b1a17944369" => :high_sierra
     sha256 "864c4e9533aaf62e67a119006017df772a961bb0c407a0184fb0c9d8e1202509" => :sierra
     sha256 "d26a07db8b6c1293171dc4937d81b2dc9936da0d52718d5f94ddd83524d5974e" => :el_capitan
     sha256 "623651ab4e150e1f05ba3a42aa6a18a1e7cb2c023cc3327cd3388f178e65d80a" => :yosemite
