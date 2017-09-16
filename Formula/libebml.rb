60bb6ae890f7421c0a71457a14c61774e37ec47f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 07:43:26 +0000

    libebml: update 1.3.5 bottle.

--- Formula/libebml.rb
+++ Formula/libebml.rb
@@ -7,6 +7,7 @@ class Libebml < Formula
 
   bottle do
     cellar :any
+    sha256 "7d69a43858af582acc49663e2e464ce6e361b99e33f58ea8fcd605df02f60128" => :high_sierra
     sha256 "72ea79b1b694d3ba054af56ed8aef46957e9ca2cb71642a0c9576121d1333e48" => :sierra
     sha256 "07ee02f2d332d60c9377117e47471f44ef7843de0f162acd067fe732f4d23c1d" => :el_capitan
     sha256 "c2d4a00cc8e80a2969fa14cb71a84f0e3e26342402c77fdbc641b2e48fec851e" => :yosemite
