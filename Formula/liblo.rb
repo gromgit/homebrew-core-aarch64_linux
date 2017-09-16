df938e6940ad47685e87c4857ec4b1646171de9e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 09:19:20 +0000

    liblo: update 0.29 bottle.

--- Formula/liblo.rb
+++ Formula/liblo.rb
@@ -6,6 +6,7 @@ class Liblo < Formula
 
   bottle do
     cellar :any
+    sha256 "961d1559e59bdcf92134a61c799456db776434d4309c2b1dc0ba17581fca7084" => :high_sierra
     sha256 "a618d8405b4e13bc1871a5a24da2489a2b64f3d08bff3b7920bf8a6b2b3d6302" => :sierra
     sha256 "4d5fb18d769550dd5415441b78e167533813c36391819f3c67ea7ea1e69ce7a2" => :el_capitan
     sha256 "2e2a5f7bc6e95657cec57fc1b969c32dac1405a9731dffec1983c69cb941dde2" => :yosemite
