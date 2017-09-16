ba8e74f1362a47aacbd249ea91a50dc8a4c26560
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 09:01:59 +0000

    ragel: update 6.10 bottle.

--- Formula/ragel.rb
+++ Formula/ragel.rb
@@ -6,6 +6,7 @@ class Ragel < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8dc6d7e1a3617cd31d9738c5ae595fd57ddb157266c1970646a7d5fbba85a6ae" => :high_sierra
     sha256 "69d6d65c2ef3da7b829e3391fd17b1ef088b92c2baf64979707033e2a7dd8c01" => :sierra
     sha256 "f4ea3a8c0476fd82000223fae69170ac9f266cd36334bd60d9d6cf4fab3273c1" => :el_capitan
     sha256 "dd8469ac3e08d5d8a257ce7fc7de05de398e8521abff83eceea0741099685b38" => :yosemite
