fd71cc36a6b4108258b2b57f24782534752352f4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 06:04:47 +0000

    guile: update 2.2.2 bottle.

--- Formula/guile.rb
+++ Formula/guile.rb
@@ -6,6 +6,7 @@ class Guile < Formula
   sha256 "1c91a46197fb1adeba4fd62a25efcf3621c6450be166d7a7062ef6ca7e11f5ab"
 
   bottle do
+    sha256 "6c3f5a6df5527562e75c1c9e95dd4e1b0b55ea2d07ab18008ac18cf59492285c" => :high_sierra
     sha256 "f99bf6e5381bcb8c0a77d1eeee51f5a3a10094771cba1e06c6036c66dfcc0181" => :sierra
     sha256 "894296e0f264fb2e9b093dfd2798363ef8ece41e5d92ba9544f65f5690c9c662" => :el_capitan
     sha256 "dd1dbd7d3c0f9c5be7ae5177e7024739026886e9422704b2e561c5ed25f11052" => :yosemite
