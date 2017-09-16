b0cf899505981ebbf7e3d1c8b0e9da30853bd1bb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 05:06:35 +0000

    gtkglext: update 1.2.0_2 bottle.

--- Formula/gtkglext.rb
+++ Formula/gtkglext.rb
@@ -7,6 +7,7 @@ class Gtkglext < Formula
 
   bottle do
     cellar :any
+    sha256 "273c2ef65c3757736c30a0179c051fe293da12e11d1c26e735d9d0968c8a6296" => :high_sierra
     sha256 "06c59b81c678523054566cb101bac3170fed61c74e1a8beadd25e32ea62de4e2" => :sierra
     sha256 "1bfd5206621c55215c249f86971be17f9de6385ff08141a53797931e621e507c" => :el_capitan
     sha256 "bbc371cd59e3955e3bf31f37df5dfe56e977c3552d01596f0e93f252d80430e0" => :yosemite
