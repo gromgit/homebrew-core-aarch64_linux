707ec45e88f77d4c2e3c9b5a6e6d3ddb3f54155e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 13:34:46 +0000

    sshfs: update 2.10 bottle.

--- Formula/sshfs.rb
+++ Formula/sshfs.rb
@@ -6,6 +6,7 @@ class Sshfs < Formula
 
   bottle do
     cellar :any
+    sha256 "283859a4868df7d91fc4a6fcef6ddc590957a661f833fbf81da25f6f2c72a585" => :high_sierra
     sha256 "534fc64f25ee6757e4fd802988deee9d2df8f1c38604d36c525a12a67b1d23fa" => :sierra
     sha256 "27c5e0d7f9bed4219c12267bd459fbd25a143abf0a9e3f5fa12fd0b587f10007" => :el_capitan
     sha256 "61c578fbd666a6c2b5b452a7429e3fd5a64da153652d428386c41b4ebb6e30fa" => :yosemite
