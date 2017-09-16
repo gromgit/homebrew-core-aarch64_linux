e6d3ca2ec6595036f9b08fcae3471ed0ec8dbf2e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 05:45:40 +0000

    cloog@0.15: update 0.15.11 bottle.

--- Formula/cloog@0.15.rb
+++ Formula/cloog@0.15.rb
@@ -8,6 +8,7 @@ class CloogAT015 < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "8c202cbeee724aa7127ac7338f50caae847c3d36741102adf68a77cd0f073e9a" => :high_sierra
     sha256 "44c06713c26b42ddc9cf13ec6a3d29181210dcc8d60c80e10e31415c022e1bf6" => :sierra
     sha256 "a81e2487e340bec8b5fcc1624f10f1786cdf046900cb433c51a728971e820342" => :el_capitan
     sha256 "6d542434511b3a90639672f3cd58e5495b5304516c7cf46c2fbf7483ad9494d8" => :yosemite
