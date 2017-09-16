f3a396e9b7691b0cead00b672e0a88b9105b802b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 04:28:06 +0000

    texi2html: update 5.0 bottle.

--- Formula/texi2html.rb
+++ Formula/texi2html.rb
@@ -5,11 +5,9 @@ class Texi2html < Formula
   sha256 "e60edd2a9b8399ca615c6e81e06fa61946ba2f2406c76cd63eb829c91d3a3d7d"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "f9d8f83a09a8e9fb53002a90dff79f5328c1e2de243687ca3376cee70606728b" => :sierra
-    sha256 "75d7f657b619df77f6242cf24f07819730bd3907b1a74f683d54a4d2fb351fb2" => :el_capitan
-    sha256 "3c3922e7c49aa3a0e1c02c845fd186d6feb1832067801d07168ef311960bdbf3" => :yosemite
-    sha256 "b38ff58a22f0b79bc719bd16c987af8cd6673230f150eaac5d11c886e5a3883a" => :mavericks
+    rebuild 1
+    sha256 "5259ab2074f122f4725058d2477233add1a32c30d385680276b06c11e06bc67f" => :sierra
+    sha256 "5259ab2074f122f4725058d2477233add1a32c30d385680276b06c11e06bc67f" => :el_capitan
   end
 
   keg_only :provided_pre_mountain_lion
