ecdac8711274ee1daab7a449b9cf3880208b34c7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 13:27:32 +0000

    portmidi: update 217_1 bottle.

--- Formula/portmidi.rb
+++ Formula/portmidi.rb
@@ -7,6 +7,7 @@ class Portmidi < Formula
 
   bottle do
     cellar :any
+    sha256 "c8f2755fd775064c282da84d666336d9125c6e70082975ffdc0867dee60b5802" => :high_sierra
     sha256 "3ab40020a258be907f829205952a3336f424c0de4588fe41c5859e8c16ebaf72" => :sierra
     sha256 "0d699de535a558e1bc72811f0b0ac7ccc158ee224564ff8e4d0b959c5872a9dc" => :el_capitan
     sha256 "c36b7219ff6d838884d8fbe13a1d159b5375e5868b9a9c0d84de332952549e36" => :yosemite
