545abc55dd907d61650b9dc23113e03e1249409d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 11:38:28 +0000

    glfw: update 3.2.1 bottle.

--- Formula/glfw.rb
+++ Formula/glfw.rb
@@ -8,6 +8,7 @@ class Glfw < Formula
 
   bottle do
     cellar :any
+    sha256 "dd4e0a7ec81510315f7f3a443c09f682ff95b6edd59f1f1e507d656aabe86b41" => :high_sierra
     sha256 "c19bbe78ab9d7d376b2cd265389348e4ad4572b9881bb1048b05d3eb4bc67762" => :sierra
     sha256 "874e364604c386252a1d639f24c8d2333bc4715c67acd77109c291d724509538" => :el_capitan
     sha256 "ecfc037c61cedd936d230880dd052691e8c07c4f10c3c95ccde4d8bc4e3f5e35" => :yosemite
