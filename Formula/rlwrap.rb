101ab1864014ed98e408f745c632ea2454d5ab23
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 01:49:54 +0000

    rlwrap: update 0.43 bottle.

--- Formula/rlwrap.rb
+++ Formula/rlwrap.rb
@@ -6,6 +6,7 @@ class Rlwrap < Formula
   head "https://github.com/hanslub42/rlwrap.git"
 
   bottle do
+    sha256 "a756fa3e95c418932e6791d3d6f4feaa88b4ac3e0da66c38285ce2bc54882063" => :high_sierra
     sha256 "689b716c824586bdd0399f0acce9271417222436b387862567dc9d48aba2ece7" => :sierra
     sha256 "b0548979bc62fadccf1d3f6f13f0df1c851e30e4d282d7ac20f9936f3e4b975f" => :el_capitan
     sha256 "7b8a163bb614b481b88a627ad9a579e5b0acb24deb736ccea02da2606b6dc8a0" => :yosemite
