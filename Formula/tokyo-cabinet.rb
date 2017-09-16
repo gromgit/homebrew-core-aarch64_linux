aab5f71ce5f31c08d28a60c88da6b379e4baf85f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 06:23:46 +0000

    tokyo-cabinet: update 1.4.48 bottle.

--- Formula/tokyo-cabinet.rb
+++ Formula/tokyo-cabinet.rb
@@ -7,6 +7,7 @@ class TokyoCabinet < Formula
 
   bottle do
     rebuild 1
+    sha256 "6470326d4c4d4d9a459407ec73a6ea6a2d6d2d459fb547467584dcf4e777aea8" => :high_sierra
     sha256 "9ace00b3ee94dbd63c427910c5aff77935f04bb884047061c792d6e90836a380" => :sierra
     sha256 "a209fa62fdb84a86784de5eb9699a9a6811c962afab2ebf418b2a712f51852d8" => :el_capitan
     sha256 "3267823914e250aff7c8d3a5a686a010f0fc96242a417dbf47bb1502aa020ad6" => :yosemite
