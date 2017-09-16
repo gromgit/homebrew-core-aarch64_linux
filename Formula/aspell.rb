704402d5d22c4623b0c2e7cb0b7aedc5749d89d2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 06:04:46 +0000

    aspell: update 0.60.6.1_1 bottle.

--- Formula/aspell.rb
+++ Formula/aspell.rb
@@ -7,6 +7,7 @@ class Aspell < Formula
   revision 1
 
   bottle do
+    sha256 "d5927b312520252d7634902269336fe28148dac0d986e7bb0883327233462f01" => :high_sierra
     sha256 "69c2de621e5d6c0a18ca7bf43b153deefef24eaa65d8ae3f5939a106d52948ce" => :sierra
     sha256 "d107e964156e9932303a3569cbfc489d96593d67c5a724e70c45dbe2ed961516" => :el_capitan
     sha256 "4a50a1b26bcbaa10dc4968332d03ce3e24772a60ec7b1561ab23102d0f46052a" => :yosemite
