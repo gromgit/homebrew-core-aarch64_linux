80af0571e82b5d91db186e633f5e7e2cc7a180e0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 06:54:37 +0000

    rbenv: update 1.1.1 bottle.

--- Formula/rbenv.rb
+++ Formula/rbenv.rb
@@ -7,6 +7,7 @@ class Rbenv < Formula
 
   bottle do
     cellar :any
+    sha256 "ffafe9cbf0f10545e0c7db8a5ea0ed3056226441391911d2992b2373de94afe3" => :high_sierra
     sha256 "a99c9b4ba77938ce03b8e06e0e4d7670c611214b07b78d2b5e1bc9a7571f9186" => :sierra
     sha256 "9d1a7da30fb133b43b243e562167ffdde6c125f054c3fde7a866a0b15173f269" => :el_capitan
     sha256 "e3e0e0b32a1bb337178d915a91ac7f552153cbf351973f9ef1692d9644824f61" => :yosemite
