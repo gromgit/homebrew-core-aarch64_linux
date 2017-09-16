362c6fe2e703e7ac0759b8e0b5dddc8bafa63c6f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 09:19:21 +0000

    libass: update 0.13.7 bottle.

--- Formula/libass.rb
+++ Formula/libass.rb
@@ -6,6 +6,7 @@ class Libass < Formula
 
   bottle do
     cellar :any
+    sha256 "72377d1cf3f27c3fd8aa3c5c09f90d95381100e839640fbc634110de2f0cd0c4" => :high_sierra
     sha256 "cc7d1e9db4ecabd76d0fbae0e4fdec4b8d30b5ff65064c3308e00265fe26b060" => :sierra
     sha256 "a6875e1e74496f2498204fa7e3c86fcbc8695b5f3ba91e3eaabe23986e4edb10" => :el_capitan
     sha256 "829f16aa3e2e7004b024b5ddf0a246efcd56aad297d1bb453a712a91cc4ddf1c" => :yosemite
