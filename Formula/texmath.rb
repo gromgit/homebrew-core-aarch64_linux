f38f853a5beb0878ba955e195eeed4e55b3c43d4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 06:40:05 +0000

    texmath: update 0.9.4.2 bottle.

--- Formula/texmath.rb
+++ Formula/texmath.rb
@@ -10,6 +10,7 @@ class Texmath < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "94e103354f40dee24c7a715ce4bbf19af635c35f317668e58b826e17bb7b1721" => :high_sierra
     sha256 "b7974f892e6967175bf00f7d50ab64b9f8a5664e27953054170e555d0c67cf2d" => :sierra
     sha256 "bf6762b6590315997c4dcb8eebdeef965bd7129f20e3d11deb97a047c4dff5f2" => :el_capitan
   end
