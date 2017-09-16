6bec19808fcb0b3800af03674c72a35a6c7012f3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-16 08:11:05 +0000

    bzip2: update 1.0.6_1 bottle.

--- Formula/bzip2.rb
+++ Formula/bzip2.rb
@@ -7,6 +7,7 @@ class Bzip2 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "197655415959a8e856f5926ebbae88715b8aef68fa8612aaf0bfe7e1c96723bf" => :high_sierra
     sha256 "8911d7904862dc4930d024d0459390c510566241015bc06ec97f9e3fbb869101" => :sierra
     sha256 "a22f768ce625a56cc2f4b3c7f08f4b1ba30f79865b786dc4c57a97f672badff4" => :el_capitan
     sha256 "1468f967e8a35954509a8beb40bd29b60b730db158054aeddadc7586890737e8" => :yosemite
