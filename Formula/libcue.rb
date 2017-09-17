1e10ae5f86c6eb8dbf3f0fc8d3adc5326b687673
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-17 21:52:04 +0000

    libcue: update 2.1.0 bottle.

--- Formula/libcue.rb
+++ Formula/libcue.rb
@@ -6,6 +6,7 @@ class Libcue < Formula
 
   bottle do
     cellar :any
+    sha256 "db3a35dc3f96e846daa16dc52c74b70ff36484d95c7dd28ab1db77db9d19901b" => :high_sierra
     sha256 "36aa09e84bcd5c561ebe75d4357e1f5c4702bdec20b706ad97ed09b5aabf15f5" => :sierra
     sha256 "82b0f3b0ae9f85046e495fc4405e93128a4d4118fcbb46dff8941d76d8c35335" => :el_capitan
     sha256 "73393d9546784ef457fc2659918c5b46e2f3cdbe61707d2a8cf2a997ec2e5246" => :yosemite
