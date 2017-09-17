3e59096a04daaa3d8be21d5e5d7bc3c98c204f5a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-17 22:22:02 +0000

    libpcap: update 1.8.1 bottle.

--- Formula/libpcap.rb
+++ Formula/libpcap.rb
@@ -7,6 +7,7 @@ class Libpcap < Formula
 
   bottle do
     cellar :any
+    sha256 "68a738a006b2226b2de1af999b2914b018aff21d1822a8be27d1cd83cba08eb3" => :high_sierra
     sha256 "59aa812cdec684990f5b6a4a6243116f728155a52de29ad6b553012b5b4bb75f" => :sierra
     sha256 "d65185154e1d7d34070c0b61855fb2cec6fcf0b47f679135ad3f7c4105acd52a" => :el_capitan
     sha256 "cca6e084403fedada146bd162dade2638a6380733c136bb2e65b6657d5c3fe92" => :yosemite
