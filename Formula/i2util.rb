5bde8b0b29089d7b0c971eede254b5fc9682a957
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-17 21:27:40 +0000

    i2util: update 1.2 bottle.

--- Formula/i2util.rb
+++ Formula/i2util.rb
@@ -6,6 +6,7 @@ class I2util < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "47c66cf5e0bfec05a5c254dc4088fe2ec3dd45772d729bd0b38146afdfbd0f0a" => :high_sierra
     sha256 "562e2d9021ff8044ca05a63c31d6560e5071ffc62f34ff1046cf195118b3471a" => :sierra
     sha256 "44f87d48502ae3e34ebfc0882aa689a70e8c92d398247c5a53e2f4b7d7652b39" => :el_capitan
     sha256 "ad1821b2637c75638de2ecd2bd3127a0c8300fe4fbd72c18ae648a131b97b6f7" => :yosemite
