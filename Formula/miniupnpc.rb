6a70b80d727168632f12d5fce930bdd1e90c8d4f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-17 21:23:00 +0000

    miniupnpc: update 2.0.20170509 bottle.

--- Formula/miniupnpc.rb
+++ Formula/miniupnpc.rb
@@ -6,6 +6,7 @@ class Miniupnpc < Formula
 
   bottle do
     cellar :any
+    sha256 "f451ae30090c9a8c019450ecebbad8bef497bdc21cbe1fc2b999c1f320b93d3d" => :high_sierra
     sha256 "491ca26ae7439d252143c5b08724a6497fc5cc8152b2b5d846509648040b5c81" => :sierra
     sha256 "f014f42bd47faa463bcb8ef6a2d28bc906523453405591aef062ed89e21844d5" => :el_capitan
     sha256 "374df29065f15c57469954527b5a7a185157a99a20b5d795659e83af4a20560d" => :yosemite
