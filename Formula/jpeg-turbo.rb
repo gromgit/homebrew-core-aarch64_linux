4b920ff07957c29b696d2979a2e2a0190a52081f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-17 21:27:40 +0000

    jpeg-turbo: update 1.5.2 bottle.

--- Formula/jpeg-turbo.rb
+++ Formula/jpeg-turbo.rb
@@ -6,6 +6,7 @@ class JpegTurbo < Formula
 
   bottle do
     cellar :any
+    sha256 "8c95c24c19a4f7c80be4a7d226b69235011b04ba638f7b16d1f8908f17e41962" => :high_sierra
     sha256 "21f8159b00dadb55d54ac116fad05609c037232247de5a4a68cbecaf6370efa8" => :sierra
     sha256 "467f1bf4bdc4b6cda9a9dde47eafe13270183c6c1d8dfa13d6149d1f1ae02ca8" => :el_capitan
   end
