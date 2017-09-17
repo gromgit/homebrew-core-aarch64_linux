cad443fe1dc40ada980c646567b82a0abd1c4b00
ilovezfs <ilovezfs@icloud.com>
2017-09-17 04:12:26 -0700

    apache-opennlp 1.8.2

--- Formula/apache-opennlp.rb
+++ Formula/apache-opennlp.rb
@@ -1,9 +1,9 @@
 class ApacheOpennlp < Formula
   desc "Machine learning toolkit for processing natural language text"
   homepage "https://opennlp.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=opennlp/opennlp-1.8.1/apache-opennlp-1.8.1-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/opennlp/opennlp-1.8.1/apache-opennlp-1.8.1-bin.tar.gz"
-  sha256 "86098147a75bf590e93ccbcb95d2b9f83c1ca198350b93dc989898700cc63c18"
+  url "https://www.apache.org/dyn/closer.cgi?path=opennlp/opennlp-1.8.2/apache-opennlp-1.8.2-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/opennlp/opennlp-1.8.2/apache-opennlp-1.8.2-bin.tar.gz"
+  sha256 "cbc9640fb8f86bca398a562aa0f5d93c87e3966d7e6574f32628d8ea080ad5ab"
 
   bottle :unneeded
 
