e083c75125cfc0cd9069b17dce320908756c9d9e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-17 20:27:12 +0000

    frei0r: update 1.6.1 bottle.

--- Formula/frei0r.rb
+++ Formula/frei0r.rb
@@ -6,6 +6,7 @@ class Frei0r < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "28ca48192fd55c2cf008a7a82fadc7336b35c27baae7388f52c6a5b7d2663605" => :high_sierra
     sha256 "c5c4efcf43d6c2cd495c82bfcf3839574b1d7f507b963151284ed186f53974a5" => :sierra
     sha256 "4f4e3e6ed474ba0e667f161e7e05c8cd8a3b67ad67201818ebc0cdac8e737220" => :el_capitan
     sha256 "a8ee4a509fa1d10137a3be9f2791d088606dcd1727079fa681a237ebc65e8fe7" => :yosemite
