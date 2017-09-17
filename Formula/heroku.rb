8b2665ab78d95a6d63246ecccc8659b8d8cacec8
ilovezfs <ilovezfs@icloud.com>
2017-09-17 04:21:20 -0700

    heroku 6.14.24
    Closes #18174.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Heroku < Formula
   desc "Command-line client for the cloud PaaS"
   homepage "https://cli.heroku.com"
-  url "https://registry.npmjs.org/heroku-cli/-/heroku-cli-6.14.22.tgz"
-  sha256 "29a5c88a0f8ece242c2ac6403596b69511773a8f36469c4f7e8de191020601ff"
+  url "https://registry.npmjs.org/heroku-cli/-/heroku-cli-6.14.24.tgz"
+  sha256 "2b327b63479f24bd21a3beb56134dcb9218021f31b3fbc45ad694b35b05f186e"
   head "https://github.com/heroku/cli.git"
 
   bottle do
