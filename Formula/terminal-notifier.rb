19325d2f5152de356c0d81d68f8f3a79eb6b6937
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-17 20:31:59 +0000

    terminal-notifier: update 1.8.0 bottle.

--- Formula/terminal-notifier.rb
+++ Formula/terminal-notifier.rb
@@ -7,6 +7,7 @@ class TerminalNotifier < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3271d76a334b9ba6311b131b155402610c9c6afc710a83d1f09db4097b668307" => :high_sierra
     sha256 "ff749a3e3bf23b1c0830b5a9529ffab0b480d1d442ad2babe821e91d0af66eb2" => :sierra
     sha256 "17d17d143099cacf047751eb56a3f7397cf808f56311ccce6be03b784e60501b" => :el_capitan
     sha256 "16aefa24a26c812094ab13e66a9b87d38f5c26e23d6049e0c6d584dde9927afd" => :yosemite
