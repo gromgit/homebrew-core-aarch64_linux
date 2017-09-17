b1a843a647840e258f06af84ba5dd107ef318e6c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-17 21:52:05 +0000

    libfixbuf: update 1.7.1 bottle.

--- Formula/libfixbuf.rb
+++ Formula/libfixbuf.rb
@@ -6,6 +6,7 @@ class Libfixbuf < Formula
 
   bottle do
     cellar :any
+    sha256 "69feffdd0ed493ae68a568d51f1fae6644d2c89a4b8bd766dfb130092afd8765" => :high_sierra
     sha256 "3d6014e37bac0a264e32a8210decbe6dcfa8e8c8512591d401b041e6483833c6" => :sierra
     sha256 "25f95e58cbb615835cf32e623319a1d6688f64af9547520f76008eb13f500a71" => :el_capitan
   end
