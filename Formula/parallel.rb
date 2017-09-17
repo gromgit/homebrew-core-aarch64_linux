4b4740f3290d50ced5aae71f73a7e8c3998e9101
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-17 22:53:31 +0000

    parallel: update 20170822 bottle.

--- Formula/parallel.rb
+++ Formula/parallel.rb
@@ -8,6 +8,7 @@ class Parallel < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7acff57fc77b2144efaadcdb1abc7e2ba8e208fc110ba4df840ac0fb499c0ef2" => :high_sierra
     sha256 "e6baf61b18591c4b8fea6f15784759563ea9a20f5915fce1f59845d853bccbbb" => :sierra
     sha256 "e6baf61b18591c4b8fea6f15784759563ea9a20f5915fce1f59845d853bccbbb" => :el_capitan
     sha256 "e6baf61b18591c4b8fea6f15784759563ea9a20f5915fce1f59845d853bccbbb" => :yosemite
