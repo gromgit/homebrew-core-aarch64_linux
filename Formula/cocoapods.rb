3b8779fa7d2631f194dfb10c0f45cb5c5bc47549
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-17 22:53:31 +0000

    cocoapods: update 1.3.1 bottle.

--- Formula/cocoapods.rb
+++ Formula/cocoapods.rb
@@ -6,6 +6,7 @@ class Cocoapods < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "34e2ab58868f7b2bcbc45d3453c0d0c4053ce73c145835762a4899ba6b549f5b" => :high_sierra
     sha256 "0541d42091e0a98568bef37e8aa687fa8eef35357d9f1f23f57a60950107de88" => :sierra
     sha256 "cfe2d6c713b728d0433b6e1c1553bbe3a04f8e1397fbae885fbc89ca6c85ed56" => :el_capitan
     sha256 "f309be4487af3f81dab480f6a2051d9e1c489d99388a4f891533b6a74bafd946" => :yosemite
