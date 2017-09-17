af5056218acc2f42398b854ffcf57eec81714d71
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-17 21:27:40 +0000

    lmdb: update 0.9.21 bottle.

--- Formula/lmdb.rb
+++ Formula/lmdb.rb
@@ -8,6 +8,7 @@ class Lmdb < Formula
 
   bottle do
     cellar :any
+    sha256 "dec11100f72b3dacd2c8da3679d9078b3badaeac42c6259e1f79894e4482d9db" => :high_sierra
     sha256 "f7d8acade2be32886edf1d039e57070e35db0bdeca6f37ef7f42530ad404cc91" => :sierra
     sha256 "b4dd9a1387599e22744bbb769ce71fd1588e5ea6f1ef1d83a2d32f71190cb6ff" => :el_capitan
     sha256 "66e48198e7bf2a466a614727a71b10e3b865cf1f65451fe10c7d5d4068e46a5c" => :yosemite
