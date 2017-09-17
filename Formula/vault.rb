3fc1d7ef626cd4fa781cf26cd381d752dab02143
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-17 23:00:54 +0000

    vault: update 0.8.2 bottle.

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -13,6 +13,7 @@ class Vault < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d9ce49df056db584a728a6f833d187580ba8c76f3db78b570e641312b108402b" => :high_sierra
     sha256 "28d1c91ea3bbbef25d9bb3be3a0e0a2fc595b764fbeb8ec42c264937fda007c8" => :sierra
     sha256 "356dbb43dbb18fae7e474805c9fde5d14e648d090fcab8458a17551fc01835dd" => :el_capitan
     sha256 "37bee050939947808686e062096d40903da168ff9feaf8229e246ad99ee8b696" => :yosemite
