8c89335d0a31f9ea40c38180a451568f111fc42a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-17 20:27:12 +0000

    libcdio: update 0.94 bottle.

--- Formula/libcdio.rb
+++ Formula/libcdio.rb
@@ -7,6 +7,7 @@ class Libcdio < Formula
 
   bottle do
     cellar :any
+    sha256 "a6ad3bc9df9cbe1d8d94adb9c5e3613210558ec547fb93dd59eb7727c12d948f" => :high_sierra
     sha256 "a33a83ce904f907197bea29caab598b3fb505c0057a720e4b0ac1d7f8b19e15c" => :sierra
     sha256 "b875027374fb8585468bc1c75161ff893598ec0f5490b6d542fd5a80b7e77052" => :el_capitan
     sha256 "45f6e37c831b5cbadd68721efc070196b0946b24f4ac05d532b4fcd65d30289b" => :yosemite
