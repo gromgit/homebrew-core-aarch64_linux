ffe8a8f23d54790eee06d08a958ffcb97f44e143
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-17 22:22:02 +0000

    libsecret: update 0.18.5_1 bottle.

--- Formula/libsecret.rb
+++ Formula/libsecret.rb
@@ -6,6 +6,7 @@ class Libsecret < Formula
   revision 1
 
   bottle do
+    sha256 "fe0fb636bf96c93bade82da91245ef92a5734dee84a8f2ed6294f0bd9e822806" => :high_sierra
     sha256 "70dc8c53fe5e90878623f3d78dc31e23894429e5ffac816efaa17ac683d6e80d" => :sierra
     sha256 "bc8741bca918709da77c0144e985918b720afcd27b6228963f117348994d680c" => :el_capitan
     sha256 "9abd02a3cc9049c185321f691df43da2b150a2f55ca0463e346632b93a7a7900" => :yosemite
