5e728f2759a054674c353ab3bbfe0248cd78354b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-17 17:22:02 +0000

    re2: update 20170801 bottle.

--- Formula/re2.rb
+++ Formula/re2.rb
@@ -8,6 +8,7 @@ class Re2 < Formula
 
   bottle do
     cellar :any
+    sha256 "6a9bceee39fdd8c25a0e08cc430c14fedfeb61691f737d337c8307de0cfbb712" => :high_sierra
     sha256 "a5e367b48b0e39f779af46261614851a5d64e799fbd7e98a23ea6a3a1ee1c1eb" => :sierra
     sha256 "acfd83f1fde8c71d9eea5b5b3fe04faa00541c58f3331aa8841680749485c9c3" => :el_capitan
     sha256 "7658df57f4ee5ab781b017ff3df629c3fad1dda84527376042299923c0b481b6" => :yosemite
