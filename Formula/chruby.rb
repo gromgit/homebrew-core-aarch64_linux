f19e9b34cb744ed46f2cb34e17c3c5a170277bc2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 20:19:16 +0000

    chruby: update 0.3.9 bottle.

--- Formula/chruby.rb
+++ Formula/chruby.rb
@@ -8,6 +8,7 @@ class Chruby < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d59074fe39429eb9979acd0e81e6b9a142aa73595971cee42ab91bbe850c6105" => :high_sierra
     sha256 "17dc507695fed71749b5a58152d652bb7b92a4574f200b631a39f5f004e86cca" => :sierra
     sha256 "ff70dff83817f093d39384a40d3dfb2aaccc1cbe475d58383d4ef157085f2c64" => :el_capitan
     sha256 "eb14810c552b693c5ae82a577be81398e7dfeadc5489666bb0ff89581f09bfe4" => :yosemite
