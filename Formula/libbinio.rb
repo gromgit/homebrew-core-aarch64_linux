d68da51dbe9a8490a1ea69d96cec48e7a3b5519b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 23:54:06 +0000

    libbinio: update 1.4 bottle.

--- Formula/libbinio.rb
+++ Formula/libbinio.rb
@@ -6,6 +6,7 @@ class Libbinio < Formula
 
   bottle do
     cellar :any
+    sha256 "176f0a11a333240770e59ada053e1656081a324debb64b96afa942a86e18f28a" => :high_sierra
     sha256 "beba76f7c3b6c54228c8972cfa01d1cb06d309d870f23ce6aad457c23e11742f" => :sierra
     sha256 "a0e373df44eee915d0f9259fb8627df92bfe3db8547bf66a9918f5c398342709" => :el_capitan
     sha256 "99e8bdd47cde67290e0854c8854c0eef32a995ff10cbf1f991ca37834d60e0a4" => :yosemite
