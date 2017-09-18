4bc6707920e8e88b7af30f65b294afe21d088200
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 23:54:06 +0000

    libbtbb: update 2017-03-R2 bottle.

--- Formula/libbtbb.rb
+++ Formula/libbtbb.rb
@@ -9,6 +9,7 @@ class Libbtbb < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "3b83b0f80659a8e8e5f774f0c75e8de9e867397e6420cfc09d7640f6d816f65f" => :high_sierra
     sha256 "b7dc719910c8c6fe4a14bf016dd98a6e9aeff4163bcc2763ba79c1030cf60432" => :sierra
     sha256 "904bceae63d8ad367c07e90ffc095d0e3163bb45116b90bf1986791b030beb32" => :el_capitan
     sha256 "72fa7e942ce1fe4d7a2cc983bc79893b56924ebdd9076917db47c5b73408bb79" => :yosemite
