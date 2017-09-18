ad9cb4d455a80079b72c77a13fdafc625de2f6ac
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 11:04:55 +0000

    freetds@0.91: update 0.91.112 bottle.

--- Formula/freetds@0.91.rb
+++ Formula/freetds@0.91.rb
@@ -5,6 +5,7 @@ class FreetdsAT091 < Formula
   sha256 "be4f04ee57328c32e7e7cd7e2e1483e535071cec6101e46b9dd15b857c5078ed"
 
   bottle do
+    sha256 "00870a22aefb2a8f0253b6a4657a02675eae56f37271c7142737e8b78cea7b46" => :high_sierra
     sha256 "ab0419c2290e4204524a74de3e652e8fae23b9d29dce793554c38c79a28be947" => :sierra
     sha256 "3e9bfe7bf16e71cc6a68e1296c4af381f40ccb68363edaf94f276393e05d8ac6" => :el_capitan
     sha256 "55b8960ca59ecc738e5973451195da1b4133eb0e31559454e87c5b1f103337bb" => :yosemite
