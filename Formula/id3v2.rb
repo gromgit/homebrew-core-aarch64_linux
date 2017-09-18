5ba69915479f340365a5e0ecd59d475939b9dc6c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 10:43:34 +0000

    id3v2: update 0.1.12 bottle.

--- Formula/id3v2.rb
+++ Formula/id3v2.rb
@@ -6,6 +6,7 @@ class Id3v2 < Formula
 
   bottle do
     cellar :any
+    sha256 "ca2c1296318425931c5eec52c70adf98665edeb19d5b681271c3b6353ddf171a" => :high_sierra
     sha256 "3b1d75af49217a58f5ecb6f0e9e34564b299903898c76145218a6496de3a7778" => :sierra
     sha256 "941e267b5a214013c8085c7918c0d8c1805c906cacf162191b764d2ae1df265f" => :el_capitan
     sha256 "cd8dd2f943081a051214bf0eedb3c1431abf2bb060a528058e6b9d4c841995ce" => :yosemite
