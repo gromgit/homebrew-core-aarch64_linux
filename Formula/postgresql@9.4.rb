55a1e753222c92d6f2f43b2eec918c4cfa6359ab
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 11:02:07 +0000

    postgresql@9.4: update 9.4.14 bottle.

--- Formula/postgresql@9.4.rb
+++ Formula/postgresql@9.4.rb
@@ -5,6 +5,7 @@ class PostgresqlAT94 < Formula
   sha256 "8e7df23a104b057b360d03180ebcb67f645e198a4a0bee94bf56b2bc9505ec6b"
 
   bottle do
+    sha256 "a66cb718bcef0bf531b3c600587686d2f94d52d9ed43380a89d9d8feb5025ee2" => :high_sierra
     sha256 "195d2d4472ed45cf1c377bedda84c0bd867f7d414ca099193d1f3945d807cdcd" => :sierra
     sha256 "e06a5ce1ca401d146a8b6c6c2ca2b172010ccfa160dcfb2bd273f36b2caf9004" => :el_capitan
     sha256 "4ce5801b9f3330ab7036c0711f1b57fcd9fefb8b63fce1f1e053ab2fa602be0e" => :yosemite
