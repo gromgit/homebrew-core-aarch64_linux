f9703a12dfadc9cb956f3d7ccb2764a54bfd7878
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 01:32:40 +0000

    llvm@4: update 4.0.1 bottle.

--- Formula/llvm@4.rb
+++ Formula/llvm@4.rb
@@ -75,6 +75,7 @@ class LlvmAT4 < Formula
 
   bottle do
     cellar :any
+    sha256 "d0bfdb67e52a97994cef179c3b8659f5c9fd6783f792524452f19ec234474ecd" => :high_sierra
     sha256 "0c97a3cd61602de11f49bdff478a3eb43fd2b72c47b58bcd607a7a4b8652fdb2" => :sierra
     sha256 "cfe3899f563c1dd1f5f6db15d5aeee6163a990344d29892453fe6f0bc6b3299c" => :el_capitan
     sha256 "1bbffa119d25d27b4b2596c0277882d0a4de3c327bfecffcce98529cd4275486" => :yosemite
