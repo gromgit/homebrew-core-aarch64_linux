445870830a45ff5dcd94d963051aa57d33724b21
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 21:50:13 +0000

    proxychains-ng: update 4.12_1 bottle.

--- Formula/proxychains-ng.rb
+++ Formula/proxychains-ng.rb
@@ -8,6 +8,7 @@ class ProxychainsNg < Formula
   head "https://github.com/rofl0r/proxychains-ng.git"
 
   bottle do
+    sha256 "0ffdd8d99f0b9e6f3d90829dfb8237cefee718b9095c9432bd39d929b31c7240" => :high_sierra
     sha256 "5e6fec4ad1150223a3c97d353b22085632dd634181ff85f7c125f49b74975710" => :sierra
     sha256 "ac61e782f334b00180f7b0008e4c5da35ee9f5632821a806c99af7b144938916" => :el_capitan
     sha256 "2298ad9f27411a1da59bd925bf9cf49ae5579d368acbaebf1c29103e84684a09" => :yosemite
