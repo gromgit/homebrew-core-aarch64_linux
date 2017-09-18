4e99a83f8037afba5cee5c6a1242404b28d7ab42
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 17:01:08 +0000

    argtable: update 2.13 bottle.

--- Formula/argtable.rb
+++ Formula/argtable.rb
@@ -7,6 +7,7 @@ class Argtable < Formula
 
   bottle do
     cellar :any
+    sha256 "e68b3df66d638a024c3b57b069bcdebfbdabb230a9c851de886321c2b3df7099" => :high_sierra
     sha256 "9485d1e045ed40c0145eb867f9d24425ccedd53b4f0cb0ec949139b0c99507c7" => :sierra
     sha256 "0a720e738557215bf1b58fa642ec2fc51971da38e98b987862fcd05cc54756f7" => :el_capitan
     sha256 "9e9d1451712580f090f0078ec7774a0daeb1057be3b1762e3d8465264d969432" => :yosemite
