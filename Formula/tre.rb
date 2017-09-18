d995c3a9bf60c6b8460c6a34914ddd2108d62458
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 15:15:52 +0000

    tre: update 0.8.0 bottle.

--- Formula/tre.rb
+++ Formula/tre.rb
@@ -6,6 +6,7 @@ class Tre < Formula
 
   bottle do
     cellar :any
+    sha256 "eaab931989b5bf5fc18949eaa234a1840531ef3aeb9deda65e4d66be40cae149" => :high_sierra
     sha256 "e28b7ac6153b06c067538f555f9ac5973df49c14ac2693aa4239ae407982e2c9" => :sierra
     sha256 "8a1762dbd40b98869e01a19c29cdb1cfa5a127543b3e132fb0fdff996e46f566" => :el_capitan
     sha256 "6fada15a2fd1c5905f8ed45d3c966da5e14efeb10522f82c26d2a23a918abaad" => :yosemite
