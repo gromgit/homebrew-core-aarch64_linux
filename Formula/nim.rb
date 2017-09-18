5770126973b6fc5d5bf2ed5cdcdf8621e86dc1c4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 20:19:17 +0000

    nim: update 0.17.2 bottle.

--- Formula/nim.rb
+++ Formula/nim.rb
@@ -6,6 +6,7 @@ class Nim < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f976e6aecf9c6211eab818ab86b3f7ee42d9ff2c02cd272017ba12657fd9353b" => :high_sierra
     sha256 "2bc6629eb88e0c023f62e486ef45cfb31756e6c5fae5726e1cc243f6bb091f22" => :sierra
     sha256 "b18b6e44b530db76c429f6be3ef367dc993cf1f78956f006d1dcf7cd5c465cfb" => :el_capitan
     sha256 "99e962cbea6146efceba1e7eed10bea6e1ce84d8715571f57078e7e736e67751" => :yosemite
