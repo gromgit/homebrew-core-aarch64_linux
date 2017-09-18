cfd6e3adb008073f6670a04f7f08d79b6a12162c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 13:06:44 +0000

    libjson-rpc-cpp: update 1.0.0_1 bottle.

--- Formula/libjson-rpc-cpp.rb
+++ Formula/libjson-rpc-cpp.rb
@@ -8,6 +8,7 @@ class LibjsonRpcCpp < Formula
 
   bottle do
     cellar :any
+    sha256 "e5d4cc49afc50d844b8b6cc74a33606ba3edd514fc31ac3bc24bd0321975f984" => :high_sierra
     sha256 "2e0fba342ff4b2912ec0df0293a4fa9f95b3cc33a424eb5bbc57c3819d337470" => :sierra
     sha256 "08cbd358998f26882aec42e81c4f24a7fbb244c1769597ac62e3e4dc738bc479" => :el_capitan
     sha256 "9711596b93811aed07a7308097c39d39c7ac4d5b7b7be57be59f57567def7c81" => :yosemite
