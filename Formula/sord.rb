09bd3ce08c1997827f372674846a890934b7045b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 11:53:10 +0000

    sord: update 0.16.0 bottle.

--- Formula/sord.rb
+++ Formula/sord.rb
@@ -6,6 +6,7 @@ class Sord < Formula
 
   bottle do
     cellar :any
+    sha256 "82abe13de14ae4d3a03e4c52653a08682b568f535e8ad321ac227f1b19f416e4" => :high_sierra
     sha256 "79ac2ab52531da8fbf650a4415907e6098e5038fea13c8a00db4efb0b6e343cc" => :sierra
     sha256 "1d2d04b071394a4b7381f89812ecc05f6967b87936935131d077c95f13b9a2f2" => :el_capitan
     sha256 "b4c0d5559e0c1aae7e8de803e75cdf84858f67bad70ba663979efc29c07b081f" => :yosemite
