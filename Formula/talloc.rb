7cb90fd895e18271f7b7a93b22ec9c2931a8b6fe
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 11:53:10 +0000

    talloc: update 2.1.10 bottle.

--- Formula/talloc.rb
+++ Formula/talloc.rb
@@ -7,6 +7,7 @@ class Talloc < Formula
 
   bottle do
     cellar :any
+    sha256 "f067641d705b891ecd10a13af13edde6670a24d4745ae1d867390fd97d373595" => :high_sierra
     sha256 "a9c830bacedca452b99f9ff33181fbe170a1a8ee4534b859704ba62ed0de974d" => :sierra
     sha256 "d6ec63f5bf8d85c274aab1bb635efc85284e3c71c908e5c1e7061c939ed814fc" => :el_capitan
     sha256 "4a9e0e1720e4294e4aa31d5c9ac5bc77e2d225a28f6ca8a3bd14fc645e85a7a7" => :yosemite
