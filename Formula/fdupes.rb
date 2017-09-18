6385add43a0bf714fdb70a941d370032645b9d63
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 19:36:42 +0000

    fdupes: update 1.6.1 bottle.

--- Formula/fdupes.rb
+++ Formula/fdupes.rb
@@ -8,6 +8,7 @@ class Fdupes < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "2ca42f56f5b4e48a4a51cf9687108eb2ebbbf43ce610596d4420be1a68f1ec1b" => :high_sierra
     sha256 "4838e3104ea06e61d7acce5f482ff80bae1d634f29a1edd44e388b9f8c63f19b" => :sierra
     sha256 "b0b7afcd64459cfc3c2bb95ac92e1aa7f6531fbf05603e472c97c5d4e72c94b7" => :el_capitan
     sha256 "ce706b289e019a30c4d07a307ae2c5c10ef1b886e4ee8e5e62f7275a9213a370" => :yosemite
