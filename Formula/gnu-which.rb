3544ef7794bd570a2fa3b35f400881101e3f1706
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 07:30:52 +0000

    gnu-which: update 2.21 bottle.

--- Formula/gnu-which.rb
+++ Formula/gnu-which.rb
@@ -8,6 +8,7 @@ class GnuWhich < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3932d82472cb95be1af5692a3cdaf55b841f2799f80271ffb2ff22b940210dbc" => :high_sierra
     sha256 "ad546f7de7825dfbadcfbadd27a493d8acdb44c4a7a6855d02473ef96fb2f716" => :sierra
     sha256 "c7ed10cf7ef215ef098e2e340d1f491037080acac59a38ade51088ebc09e71c4" => :el_capitan
     sha256 "469d61be66afe0da2758838d1bde62544a661691568eaa30bc4f5abc16402efc" => :yosemite
