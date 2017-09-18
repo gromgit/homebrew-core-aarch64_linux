058cee39af6a31fcfac0bd77525b1efac81d750a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 10:43:35 +0000

    laszip: update 2.2.0 bottle.

--- Formula/laszip.rb
+++ Formula/laszip.rb
@@ -7,6 +7,7 @@ class Laszip < Formula
 
   bottle do
     cellar :any
+    sha256 "26635adc73522f43a5e6f27565e6b9cf9e75320d75155aad816aae5b583b6bc7" => :high_sierra
     sha256 "9b3799014035a92909269d87f3756d98c83922c708c2ae0b2ac8f2c0ccc6f20e" => :sierra
     sha256 "3544d8dd1e7db052d685ecc588803dd8723172df160619d7451afe6ede7b884e" => :el_capitan
     sha256 "e757a001cce1bacae92297fedb006cd40a91e20035e84e071664d89a55862af5" => :yosemite
