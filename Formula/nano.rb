ad9cff1a08f936b0ad231decc466c2f03c159dcc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 10:31:30 +0000

    nano: update 2.8.7 bottle.

--- Formula/nano.rb
+++ Formula/nano.rb
@@ -5,6 +5,7 @@ class Nano < Formula
   sha256 "a49a888c20f09a17c7f1049d1fda619472c92eedcb25fd4be5f088a51cc392a5"
 
   bottle do
+    sha256 "831b7c3e751dff9bce0e874e2f650e0e02cf556349913e94379dc9aaed8b3c3e" => :high_sierra
     sha256 "5b84f256231301bddf7e079cbd2845ce12911ef0c9dc05870e5c50d86babd5c8" => :sierra
     sha256 "2ab25ffe79af65eedc41445497600c0a5723c2e196886155f9f5beb0f5eada92" => :el_capitan
     sha256 "8ea801434cc2dd837f5a0f147030d850106d26ddf9740d97f23e6275ab57222b" => :yosemite
