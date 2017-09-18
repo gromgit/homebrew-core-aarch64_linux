e077318d5876b71be1d5d4254cf22091d056e36a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 19:55:03 +0000

    sphinx: update 2.2.11 bottle.

--- Formula/sphinx.rb
+++ Formula/sphinx.rb
@@ -6,6 +6,7 @@ class Sphinx < Formula
   head "https://github.com/sphinxsearch/sphinx.git"
 
   bottle do
+    sha256 "b890cf523db9777c7d125842fd6b0a53fe9a7a5a4cb816389ba6f5ee6483c78d" => :high_sierra
     sha256 "55ce34bdedf13946fa614bde50839d93135eae720f1021e2c87807d04515ab18" => :sierra
     sha256 "c75e018d69afb7d3cb662ebd129af67607d47f7b7f71ce8ea95be75d66dc502d" => :el_capitan
     sha256 "f89b43df8735d295a55c74f18d6af4a1a10b9f3ae81df69713c27f9240f78d14" => :yosemite
