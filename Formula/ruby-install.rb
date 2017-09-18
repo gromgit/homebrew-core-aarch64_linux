62612c09595f08644e6cf5826c1e1ad1206bb657
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 20:52:34 +0000

    ruby-install: update 0.6.1 bottle.

--- Formula/ruby-install.rb
+++ Formula/ruby-install.rb
@@ -8,6 +8,7 @@ class RubyInstall < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "90078fb4aefc71c0790721c0b4e62c6cbc933f702f42b723c2ef5869cd5f9227" => :high_sierra
     sha256 "a9e4545b46128f0d35e25c7e558330a93a8ceaf025410610c795b9904bf241a1" => :sierra
     sha256 "3b0b594e01f951b7161b36b33ae59fd0063ac05212708e6eb107bf70fd508258" => :el_capitan
     sha256 "3b0b594e01f951b7161b36b33ae59fd0063ac05212708e6eb107bf70fd508258" => :yosemite
