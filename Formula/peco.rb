41a411af78994ffb6031a7598054994cfbc20587
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 21:47:51 +0000

    peco: update 0.5.1 bottle.

--- Formula/peco.rb
+++ Formula/peco.rb
@@ -7,6 +7,7 @@ class Peco < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e5adad1de9144d6ef9b5c53dd713272e47dbf1ba13fecdc92059f54cd342e7fa" => :high_sierra
     sha256 "78e54d92559b862a2620392cd0274e59693718a69c1a133d2b0d932889bad2c6" => :sierra
     sha256 "d91fdde06f21ab5d27ff2fba0118facfbda8f77d9d3cdfb8368bb1a4678e2470" => :el_capitan
     sha256 "0740fec8c778247f3bbeac76376fff7097cc8ed756d0c029f106c21fa105705e" => :yosemite
