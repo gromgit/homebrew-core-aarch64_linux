b0ba28416b32fa9fa2e36e81a2761b43ff949166
ilovezfs <ilovezfs@icloud.com>
2017-09-18 01:47:36 -0700

    pkcrack: remove patch (#18207)
    in favor of a CPPFLAGS hack.

--- Formula/pkcrack.rb
+++ Formula/pkcrack.rb
@@ -12,18 +12,13 @@ class Pkcrack < Formula
     sha256 "47f2ffa2e27f0dc5e6df45de7335e316a8ea83288153b274ae5d8e11c7157055" => :yosemite
   end
 
-  # This patch is to build correctly in OSX. I've changed #include<malloc.h> to
-  # include<stdlib.h> because OSX doesn't have malloc.h.
-  # I have sent to the author [conrad@unix-ag.uni-kl.de] for this patch at 2015/03/31.
-  # Detail: https://gist.github.com/jtwp470/e998c720451f8ec849b0
-  patch do
-    url "https://gist.githubusercontent.com/jtwp470/e998c720451f8ec849b0/raw/012657af1dffd38db4e072a8b793661808a58d69/pkcrack_for_osx_brew.diff"
-    sha256 "e0303d9adeffb2fb2a61a82ad040a3fec4edc23cae044ac1517b826c27fce412"
-  end
-
   conflicts_with "libextractor", :because => "both install `extract` binaries"
 
   def install
+    # Fix "fatal error: 'malloc.h' file not found"
+    # Reported 18 Sep 2017 to conrad AT unix-ag DOT uni-kl DOT de
+    ENV.prepend "CPPFLAGS", "-I#{MacOS.sdk_path}/usr/include/malloc"
+
     system "make", "-C", "src/"
     bin.install Dir["src/*"].select { |f| File.executable? f }
   end
