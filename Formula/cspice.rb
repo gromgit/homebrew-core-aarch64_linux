53503fc065c5ed0aba8ea4c2c7519340abab38e7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 15:15:52 +0000

    cspice: update 66 bottle.

--- Formula/cspice.rb
+++ Formula/cspice.rb
@@ -8,6 +8,7 @@ class Cspice < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "dac29486067ad080407dfd76641a8902103ce333750d5e2c9723409806f2ab61" => :high_sierra
     sha256 "5ffb3eec6da9aa84ff58330734d024df9ea1378b1cc93365736b66d4315c47b9" => :sierra
     sha256 "ceec1738779c07c06bd21b5c81816fb66854b728a1a098fe5ac1f37a176ee32f" => :el_capitan
     sha256 "ff72f9d54707e03e86016b286117528720134acd4f23bd6e6b4402c8cd4def73" => :yosemite
