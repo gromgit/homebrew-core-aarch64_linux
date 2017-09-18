bef6b2babea5fe9b96ebfab6b9414577e64279e7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 07:30:51 +0000

    figlet: update 2.2.5 bottle.

--- Formula/figlet.rb
+++ Formula/figlet.rb
@@ -5,6 +5,7 @@ class Figlet < Formula
   sha256 "bf88c40fd0f077dab2712f54f8d39ac952e4e9f2e1882f1195be9e5e4257417d"
 
   bottle do
+    sha256 "3047847adef9cb5bd5588cf65f64bfcc0549ed44d4370a862071aba2f9d98ba6" => :high_sierra
     sha256 "c53966c742bf88b8481f6ed0bde1a951ea11185af2c631fb02b84fa7120f2e17" => :sierra
     sha256 "943067dae95de58518b20334aec401cf5fd24866ff77315c0d7bd8b5d4ab0011" => :el_capitan
     sha256 "0a1b051fb0143dbfca1da36c83eca8580c215ff155e0dc755a924ce1f53a4b46" => :yosemite
