18445ff1e0f2e19315c88ccea9f2dde2f6c4206b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 10:31:29 +0000

    colordiff: update 1.0.18 bottle.

--- Formula/colordiff.rb
+++ Formula/colordiff.rb
@@ -7,6 +7,7 @@ class Colordiff < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1b381f5726976016f7996abd414eb24b19bfd63dcc7a8a628e51ca4e3507b4d1" => :high_sierra
     sha256 "c4d57d0ae142725bd320600864843ce040457c7652a3e403e1c65a6e53b42afd" => :sierra
     sha256 "19c797b186c5b2ec4e4692a21d25f3c8e48addea3cc8aaa2809cf37dcc0f1100" => :el_capitan
     sha256 "19c797b186c5b2ec4e4692a21d25f3c8e48addea3cc8aaa2809cf37dcc0f1100" => :yosemite
