d01c24833168bda59ae456db942f30aea1d273cc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 07:30:50 +0000

    telnet: update 54.50.1 bottle.

--- Formula/telnet.rb
+++ Formula/telnet.rb
@@ -6,6 +6,7 @@ class Telnet < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7197673f6d77b6ca1e6c493cdef5f8625cc63f8fd831c0b2a633fcde4ee5a2c6" => :high_sierra
     sha256 "64fa2138be873d53d055bcb1d31ec2583cab0956d98ed32b365414e4f08ecb1c" => :sierra
     sha256 "915b9a554df5714d85ff831b0721fc471533970b6d7fe6ab2d0ccc7e441d99d0" => :el_capitan
     sha256 "69ff324ea8f0bcc1478c31a3e5b99e183a947e312cf180027dfecc260c478500" => :yosemite
