ea68f474cdd3799465d121ca505feb4143c4d8d6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 19:29:31 +0000

    gnu-go: update 3.8_1 bottle.

--- Formula/gnu-go.rb
+++ Formula/gnu-go.rb
@@ -9,6 +9,7 @@ class GnuGo < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5e6ee72c1ccd877c08591680117bf73d809f6422ea9855596b286970d165c14a" => :high_sierra
     sha256 "25fa92bd5c129cb655ec06c441523ada5cbc90a560111c32f5b1246c8f7d124c" => :sierra
     sha256 "f845be5a48a89cf0e46322b4d3a64a86b9fd4793f6b98fee0c45de5e8e5eda69" => :el_capitan
     sha256 "57731f2cb8dcb2959e85baebdb779989390688a70447b923b9d5c1ce8575da44" => :yosemite
