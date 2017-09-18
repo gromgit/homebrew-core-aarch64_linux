cf7c8a793d182869c68b7c0f0644ec0d545487df
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 11:53:09 +0000

    sleuthkit: update 4.4.2 bottle.

--- Formula/sleuthkit.rb
+++ Formula/sleuthkit.rb
@@ -6,6 +6,7 @@ class Sleuthkit < Formula
 
   bottle do
     cellar :any
+    sha256 "7ea055e4657be61ef7e37fd9103c8d7d12feb241d2eda0b1a75fa804a3a321cc" => :high_sierra
     sha256 "58e1527d98ad4284fb5a4f78e315d4c8626a618c5f47035761ca3b0e147b152d" => :sierra
     sha256 "6afe4510ae6e1c707f3f5e3e7ad40e9c539bfea3f5ac45f25949a8c4ab5a536d" => :el_capitan
     sha256 "eda246e21dbf2974eb352c0ff57c313a4d8778526c16cc977c4fa807d4cbf12a" => :yosemite
