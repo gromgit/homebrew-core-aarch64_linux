c15e4180cb360c75519bb6030c45a8f265aca5c1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 08:51:00 +0000

    nagios-plugins: update 2.2.1 bottle.

--- Formula/nagios-plugins.rb
+++ Formula/nagios-plugins.rb
@@ -5,6 +5,7 @@ class NagiosPlugins < Formula
   sha256 "647c0ba4583d891c965fc29b77c4ccfeccc21f409fdf259cb8af52cb39c21e18"
 
   bottle do
+    sha256 "d8f55e381e65df1be3113923dc351fac227ae17b6334a9d2c939cf346434eca9" => :high_sierra
     sha256 "f88a0ce6fd30f875cc9654f3989b0728d2bd230e09bec994cdb8c2461a7f2166" => :sierra
     sha256 "d9920741a2e4322d7c9fd55a87f3d7bf56f4abfa3a49f0fc9adcf408f891775a" => :el_capitan
     sha256 "1fcf4d4934fe7f7793fb78d13f17f948d46a19600e02881b22695f736f327e65" => :yosemite
