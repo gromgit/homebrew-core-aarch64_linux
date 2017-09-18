d3880087512d46fc8e8f3491240570af581ade14
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 12:06:46 +0000

    py2cairo: update 1.15.3 bottle.

--- Formula/py2cairo.rb
+++ Formula/py2cairo.rb
@@ -6,6 +6,7 @@ class Py2cairo < Formula
 
   bottle do
     cellar :any
+    sha256 "85182360aa4658f903d2f9934945c02bd27b9450a4e64e49d86efd8a67056e2d" => :high_sierra
     sha256 "6cc07da4276294a5cf82efda7e4d3c5c25de137e76b3928d8c831631512ac8d0" => :sierra
     sha256 "a9fce1abe4f466d2a8be3a0477fd8e1851c913615898dd8f80e2143da7f09f47" => :el_capitan
   end
