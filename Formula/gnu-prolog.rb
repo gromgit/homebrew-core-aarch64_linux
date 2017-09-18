def5022710356a9f552495559856bcc8a3fe3baa
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 19:10:13 +0000

    gnu-prolog: update 1.4.4 bottle.

--- Formula/gnu-prolog.rb
+++ Formula/gnu-prolog.rb
@@ -7,6 +7,7 @@ class GnuProlog < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "f8d446ec6be0b8db9d4a2b6602877386a050f678d7900e5b8b00f345a4562f5c" => :high_sierra
     sha256 "9a449562401a31c551224e9703ec45dcf4b013b1a77d9b5de65a0b162c744d0a" => :sierra
     sha256 "9d8e49f034bd641578443d1c422937a14f4819742ef6844dee4df1998a6388c9" => :el_capitan
     sha256 "b835e9887909affa110d4a57abf7bfecee8d9aee0fb29f38432112c1347441d2" => :yosemite
