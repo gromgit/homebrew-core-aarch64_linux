98c2372b0304c6f06957d0ffac8c4afcc9f86180
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 07:30:52 +0000

    imap-uw: update 2007f bottle.

--- Formula/imap-uw.rb
+++ Formula/imap-uw.rb
@@ -9,6 +9,7 @@ class ImapUw < Formula
 
   bottle do
     cellar :any
+    sha256 "b051874130e8c65f442ff4fdb9875ec981da9f73e24ba0e1577bb23f4c379d8d" => :high_sierra
     sha256 "714de61f534fb8f9264183a96b527868ffee4e568f10b2ad5c283276896ffa2f" => :sierra
     sha256 "2b9f5cbd682299610f20f1fda87c5d5c63b378ec071be2ed4889276bed53f0b4" => :el_capitan
     sha256 "001a10201375e639d4dbb4c8cba174ef02b19b89ba8e71ce2e2182610b6f120d" => :yosemite
