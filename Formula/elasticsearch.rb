a9a224f373353b95cfa3b1575314f29815cc5bd0
Jason Tedor <jason@tedor.me>
2017-09-18 13:50:17 -0400

    elasticsearch 5.6.1

--- Formula/elasticsearch.rb
+++ Formula/elasticsearch.rb
@@ -1,8 +1,8 @@
 class Elasticsearch < Formula
   desc "Distributed search & analytics engine"
   homepage "https://www.elastic.co/products/elasticsearch"
-  url "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.0.tar.gz"
-  sha256 "a38f6e9b4282e1e39d2e7f237d3e0226b46001af38164ed40f764bb6768af057"
+  url "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.1.tar.gz"
+  sha256 "006f9cb3886877df845e3c3dea8a688777fb739a862d3afe1a113c16a732715f"
 
   head do
     url "https://github.com/elasticsearch/elasticsearch.git"
