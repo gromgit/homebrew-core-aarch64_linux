6d0f6b88414895b9fa71baddb6c85f74a14b45b1
ilovezfs <ilovezfs@icloud.com>
2017-09-17 23:19:48 -0700

    libngspice 27
    Closes #18197.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libngspice.rb
+++ Formula/libngspice.rb
@@ -1,8 +1,8 @@
 class Libngspice < Formula
   desc "Spice circuit simulator as shared library"
   homepage "https://ngspice.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/ngspice/ng-spice-rework/26/ngspice-26.tar.gz"
-  sha256 "51e230c8b720802d93747bc580c0a29d1fb530f3dd06f213b6a700ca9a4d0108"
+  url "https://downloads.sourceforge.net/project/ngspice/ng-spice-rework/27/ngspice-27.tar.gz"
+  sha256 "0c08c7d57a2e21cf164496f3237f66f139e0c78e38345fbe295217afaf150695"
 
   bottle do
     sha256 "08f1356662aa3107b7deb6022242988f20ea14c6784523e2121c799586da6f25" => :sierra
