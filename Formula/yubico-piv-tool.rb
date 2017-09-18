9c0b70d8135fd4c8fc814461220407ad8ba97955
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 02:45:45 +0000

    yubico-piv-tool: update 1.4.3 bottle.

--- Formula/yubico-piv-tool.rb
+++ Formula/yubico-piv-tool.rb
@@ -6,6 +6,7 @@ class YubicoPivTool < Formula
 
   bottle do
     cellar :any
+    sha256 "db317d0ba65e375ff13918fdc8b0fd2ff4dabcc69f71e090c4c1bad4d5722b25" => :high_sierra
     sha256 "2b82a5831449f91ef8a7f7ec2900b867107353ee3e5f3b00fbcb69130341292a" => :sierra
     sha256 "42a20303f0d5a0cf81aea803427138893411e5111e629bc94427332a87241fcc" => :el_capitan
     sha256 "2b3fa02ae52f8868e111144c7fd8d581c92fba89c8dd0de84de3b7e9e620e54e" => :yosemite
