7781dd2d57ec38032a84d520e1b5761b5a3547e6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 10:31:30 +0000

    fortune: update 9708 bottle.

--- Formula/fortune.rb
+++ Formula/fortune.rb
@@ -7,6 +7,7 @@ class Fortune < Formula
 
   bottle do
     rebuild 2
+    sha256 "6f8a87247d72ed1d9197370b1df307bdb9d949d868ae210b7dc32dc43eaba8ab" => :high_sierra
     sha256 "ac140349dc3a0ce55d2299e90651492df8ccf7839f57d8b1fa6ca221a665efc7" => :sierra
     sha256 "fe681ea371ce058faeebbd459ac9b5f492b7b523652da937ed8cb7d9bbf0eaf8" => :el_capitan
     sha256 "97c35357e5becf525ddaede462e40283872d0b5d2cebfeeb7d509cb0ef06fc7c" => :yosemite
