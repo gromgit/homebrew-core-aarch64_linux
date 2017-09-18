5b8db0351daf3d91994d8ce433e304948c93d33b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 19:55:03 +0000

    hyper: update 1.10.14 bottle.

--- Formula/hyper.rb
+++ Formula/hyper.rb
@@ -9,6 +9,7 @@ class Hyper < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "622a203e0eca9e0119db6873db9ef3639c668abbdaf9fc9cae2d04965b4565df" => :high_sierra
     sha256 "eb1589822317964a0d767f408bb6ad8352e5a95ff247e63b5f68c5feee144444" => :sierra
     sha256 "29d4616bce077339982b742013a470f69e63da316cd845c90cd9114d295360f5" => :el_capitan
     sha256 "45bfd893e2bfbbd07b97d000802e500d0fbe90df77d60332fdf301a23eab77b4" => :yosemite
