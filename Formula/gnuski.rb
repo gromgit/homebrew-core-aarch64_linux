c7e3c7851d293a39061caaf0b984e9a059fa23a9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 19:20:41 +0000

    gnuski: update 0.3 bottle.

--- Formula/gnuski.rb
+++ Formula/gnuski.rb
@@ -6,6 +6,7 @@ class Gnuski < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6f15bd497951ea784e84b2ec888be83343ad1ad96eb6bab9ba343bff31246700" => :high_sierra
     sha256 "3874907a4ad715492c026d969ec3265dcd5f71424dde07a83aa1c21a1e36fa38" => :sierra
     sha256 "ce14d8ee8b8d58c710b93adb2f4cedfb9d78fb64746f38daee4ea38aa977ae43" => :el_capitan
     sha256 "3163ed8b9f1487e0f5f5a42006e0edfbfdb3a4dbea9b917c0aa692db282ec13b" => :yosemite
