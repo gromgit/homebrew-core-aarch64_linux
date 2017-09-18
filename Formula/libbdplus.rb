5b36e587d81be845a39165a9d5f621c618afb5a3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 23:26:01 +0000

    libbdplus: update 0.1.2 bottle.

--- Formula/libbdplus.rb
+++ Formula/libbdplus.rb
@@ -7,6 +7,7 @@ class Libbdplus < Formula
 
   bottle do
     cellar :any
+    sha256 "478e405b0f9687edcea3f651f4ec922a1bd12c12476c3aa14d1a35d0bb0362bb" => :high_sierra
     sha256 "8205ed5218393f7aa7f2035f089e91a417f13d73f4b7e3d46f3afc5073ce7e37" => :sierra
     sha256 "7136cdf433318efb9691d43d078eb20e9647f2ae4999b42cf791736d95047a81" => :el_capitan
     sha256 "13f271c0bb73d496cda7314d6665478c19193f0eb3b9b7a9fbc1eb4a957894c9" => :yosemite
