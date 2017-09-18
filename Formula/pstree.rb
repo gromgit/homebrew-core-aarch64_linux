1b277d62744603da4899df79ff88c9e9c412e5e5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 10:31:31 +0000

    pstree: update 2.39 bottle.

--- Formula/pstree.rb
+++ Formula/pstree.rb
@@ -11,6 +11,7 @@ class Pstree < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "426d5701e835bc1f9313c3b7cd630aa0f2b279ad5f95406bd73f50d174e8eaf1" => :high_sierra
     sha256 "063d2498a346002265c44bf9ad237ae47fd9923a10dd529575640d7d63bef2fa" => :sierra
     sha256 "624458274db8e826c170121061ad25547c5a245788c8108bd2bf0af4a3678dea" => :el_capitan
     sha256 "127b605bf4b20cbddf63f875bd15f78ad5fc31eaebb57d9ce2051a3b856a8bd5" => :yosemite
