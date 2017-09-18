e0f07f67ff7363f17d0ff722b99cf6da40c95b02
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 18:37:15 +0000

    scalapack: update 2.0.2_8 bottle.

--- Formula/scalapack.rb
+++ Formula/scalapack.rb
@@ -7,6 +7,7 @@ class Scalapack < Formula
 
   bottle do
     cellar :any
+    sha256 "49f16a2a841545b3fd46ba7c76e467748db5810391c2813f0bf3a40e0062018c" => :high_sierra
     sha256 "2bbbb6168843fd0c4d625eae362d5998b5b1d51f2b2cdb3c2bac1c3389a6f8ae" => :sierra
     sha256 "1273e195bb17b4d178f06ad336f96d8a42038c4a98be8d13d25401e8a44bd193" => :el_capitan
     sha256 "d91bd993babb651afeffc48932d6343fbb6f73c698708f583dde18e9a69666f9" => :yosemite
