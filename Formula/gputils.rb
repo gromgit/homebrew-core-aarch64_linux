e063b108b7bedf99d89338767f5e5b0af84bba98
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 10:43:34 +0000

    gputils: update 1.5.0-1 bottle.

--- Formula/gputils.rb
+++ Formula/gputils.rb
@@ -5,6 +5,7 @@ class Gputils < Formula
   sha256 "6f88a018e85717b57a22f27a0ca41b2157633a82351f7755be92e2d7dc40bb14"
 
   bottle do
+    sha256 "c5ed95fc323471f635edbc08e81394f66486c4c81953a0881c5b26791d1176bb" => :high_sierra
     sha256 "aba5cb544582e26bdb212f9782f911b0e9d36c5049d0aed3928c48ae9b74a6e9" => :sierra
     sha256 "ad3a6688ca0dac1da0a10db36d9119d9bedcd8d0f389920a45832cc0676c67a3" => :el_capitan
     sha256 "dd69717c349e405ed04eebc07fc86dcc09d3763f892488514528b328a725a3f9" => :yosemite
