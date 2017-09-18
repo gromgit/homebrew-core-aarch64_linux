6860fbb624c2a0df084ca606280ae0311971cf8d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 20:13:41 +0000

    libcouchbase: update 2.8.0 bottle.

--- Formula/libcouchbase.rb
+++ Formula/libcouchbase.rb
@@ -6,6 +6,7 @@ class Libcouchbase < Formula
   head "https://github.com/couchbase/libcouchbase.git"
 
   bottle do
+    sha256 "5ec8d3eb6d3363bccbc4e314fc26fdfa6bbb134d7935aa70a1fecb7f3f119c59" => :high_sierra
     sha256 "9371839c4f7756e6ee7bea6f4bffbdbb246be66262cb7ed5a8854fcf91b39020" => :sierra
     sha256 "ad3b2e9a39ba095edff470175371092fc3d990a1d5bafc8f376c5604cd80af16" => :el_capitan
     sha256 "7d29ae128f2b10a187efc54b0db42f921c8d0ed63999e375618d7626a529af75" => :yosemite
