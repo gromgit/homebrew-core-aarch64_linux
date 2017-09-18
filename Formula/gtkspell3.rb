3bef4f8b7e57740834565e09111f2f05e5a61475
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 10:43:35 +0000

    gtkspell3: update 3.0.9 bottle.

--- Formula/gtkspell3.rb
+++ Formula/gtkspell3.rb
@@ -5,6 +5,7 @@ class Gtkspell3 < Formula
   sha256 "a4f4a4a2789f7499563e26d96b22d8085222ebe278da47d026b2de782b8b4d26"
 
   bottle do
+    sha256 "fb78bacfa701beafc1269d4d11ca80566549051cd6863606764bfc596b701f2f" => :high_sierra
     sha256 "e4f7b8513bac8614cb9a56f340f798eea9d34f94214231b9bbfb498c4171b302" => :sierra
     sha256 "d6efa8211ed46be3141f7d0c4db63044f10a42ae5ed2926f49a533c455aa08d2" => :el_capitan
     sha256 "134aeb97483576ea4d2df0260de35071f1c5622d7c23c20f83c51d0edcfa4fb9" => :yosemite
