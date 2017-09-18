c94932b4ade12e0ba36e362ce2d28a7388c0a40e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 02:45:46 +0000

    v8: update 5.1.281.47 bottle.

--- Formula/v8.rb
+++ Formula/v8.rb
@@ -8,6 +8,7 @@ class V8 < Formula
 
   bottle do
     cellar :any
+    sha256 "179a8442510eb0a022ea6823cd6a76044c14c4fe18415710cac3d746d432020e" => :high_sierra
     sha256 "8106efc14371982af11a66d8db533dc0589bc240950e0e445467cf6ce8871393" => :sierra
     sha256 "487f2ca72096ee27d13533a6dad2d472a92ba40ef518a45226f19e94d4a79242" => :el_capitan
     sha256 "dc9af3e08eda8a4acd1ff3c6b47a4c5170a92dbab7d2d79958a14d8aa42eefac" => :yosemite
