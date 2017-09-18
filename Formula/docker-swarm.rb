5c6c7f50ba3cf2248d3d4d6ad7fc5a91f6a34480
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 19:56:16 +0000

    docker-swarm: update 1.2.8 bottle.

--- Formula/docker-swarm.rb
+++ Formula/docker-swarm.rb
@@ -7,6 +7,7 @@ class DockerSwarm < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f3940875a0cbb1cf562376e61ffb6206aa75e266931e54f52fe08c2ceaaf6329" => :high_sierra
     sha256 "17efa0a36074c19516377ed860540ed5e8672794607d8a1a78b7f18b12b0f403" => :sierra
     sha256 "de4c2cb59c9a198bec4bb6442cbb6fd0465964b27842d77d6d277429cfe51b27" => :el_capitan
     sha256 "01b32b2a21df9ffb5f3e0721cbb33f65aa5ef8e0207ec9648feb769fcb4ae932" => :yosemite
