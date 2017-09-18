9f0fb38ef8a7b4d285551ec6c9caf29d608849db
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 07:30:52 +0000

    lolcat: update 90.8.8 bottle.

--- Formula/lolcat.rb
+++ Formula/lolcat.rb
@@ -7,6 +7,7 @@ class Lolcat < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "79a876424a8942e9bd196c73778c61479d1092d96ce1833ccdecc5814a484cfc" => :high_sierra
     sha256 "492e7fe98802675c99f981a57ad9526b248351a9c62ff4c65c92cc91a7ce7d0a" => :sierra
     sha256 "ede0105710e8d64af8998252e978dc4983efa10a234ecdab25b6bd61a8d11a08" => :el_capitan
     sha256 "b338cd33256e246e29f7e3c79b2442d8abb895e10e97e5fb988c7d8dced2ac36" => :yosemite
