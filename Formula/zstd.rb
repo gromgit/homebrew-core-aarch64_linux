71c0f36b1a21ab55186196c1a6741edd29e75a0d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 02:45:45 +0000

    zstd: update 1.3.1 bottle.

--- Formula/zstd.rb
+++ Formula/zstd.rb
@@ -6,6 +6,7 @@ class Zstd < Formula
 
   bottle do
     cellar :any
+    sha256 "b2021d43b1a39c9405a7976099817ceb7ce7b2f23ede232fbeed840ee104ac3f" => :high_sierra
     sha256 "8ed5d6634d35dabef86531e168f7b42aee4e75e3eb373e4cc1ac68f160f26a2d" => :sierra
     sha256 "ebb20d76a07b95d0771ae66b338fdd7e47f6bb4e7a49e8fd1ec882b91d10c762" => :el_capitan
     sha256 "68dcd8dd4fa5f5a88c19d1a1cf8339c17723baef2554895063ef2afaf80774ef" => :yosemite
