7be7d510fad07973dca74341ac808977b5910ae6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 18:37:14 +0000

    libxc: update 3.0.0_1 bottle.

--- Formula/libxc.rb
+++ Formula/libxc.rb
@@ -7,6 +7,7 @@ class Libxc < Formula
 
   bottle do
     cellar :any
+    sha256 "d3322d32b265367ac0c798073dd6b68394faa006c88b9aedbe88138b92930afe" => :high_sierra
     sha256 "984695d0b1ffadebe99d03c5c9061740bb45eaa6024595bb675213d388a3cebf" => :sierra
     sha256 "a5f71cb519b4573600046acfa3203febeb9bc56e79c89978289421dc8e07be01" => :el_capitan
     sha256 "729c2a9d858dae15cdc960c6a816ae6f153307c078cc2b21cad51263f0b27879" => :yosemite
