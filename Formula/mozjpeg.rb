a856d04463a3384620c437ad92d236c0c8818dbb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 08:50:59 +0000

    mozjpeg: update 3.2 bottle.

--- Formula/mozjpeg.rb
+++ Formula/mozjpeg.rb
@@ -6,6 +6,7 @@ class Mozjpeg < Formula
 
   bottle do
     cellar :any
+    sha256 "da5e5714bb881b7c4fba0f91b0649caff19424a63d397211506e28deff57947b" => :high_sierra
     sha256 "064155896464277965bd3abc8581ade6a6c42938045df5418af2a17cb8892b11" => :sierra
     sha256 "cd32c9a3b91acf653f34bfe82c92b44dfa7820ac15fd4c497a1e68a218cdebc0" => :el_capitan
     sha256 "ba216c159f6ef8573699a11dd423c53f9b34432000cef18a30c8e3c3e27bda2f" => :yosemite
