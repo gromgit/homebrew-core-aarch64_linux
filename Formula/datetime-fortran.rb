a2604fe930789bcd1b1e07e046d617fef931f8d2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 17:59:39 +0000

    datetime-fortran: update 1.6.0_1 bottle.

--- Formula/datetime-fortran.rb
+++ Formula/datetime-fortran.rb
@@ -6,6 +6,7 @@ class DatetimeFortran < Formula
   revision 1
 
   bottle do
+    sha256 "4c9c8a4f70f1eabeab5a0ffee1e2a20455c08bd4fa88bc69bc4d6b9176b44dfb" => :high_sierra
     sha256 "a9fa586679b05377aeeeea71e2ae1932c630fb43a89eb7314276bc6fbbdd3383" => :sierra
     sha256 "2fe1c9faee819e539afa30b8bbceb203bea388de7609418d80ee5fed4e716e7a" => :el_capitan
     sha256 "ba3ad1df170d2027251c9c322e4250e8c49e4af1786a3f42d66f08562eb25a16" => :yosemite
