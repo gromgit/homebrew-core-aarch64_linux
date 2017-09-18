cdd6efdf5f4adaa18114bf53f5f21df4be0fb67a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 07:30:50 +0000

    smartmontools: update 6.5 bottle.

--- Formula/smartmontools.rb
+++ Formula/smartmontools.rb
@@ -5,6 +5,7 @@ class Smartmontools < Formula
   sha256 "89e8bb080130bc6ce148573ba5bb91bfe30236b64b1b5bbca26515d4b5c945bc"
 
   bottle do
+    sha256 "c4ad7926a0da73101e7ef83eda23cfd21c808af7f6d19b8d8b8ea4714432895b" => :high_sierra
     sha256 "36d3ef7c8959108c52d0d3e8539c09f40547f73f7b30c295a66e741ff5ec110f" => :sierra
     sha256 "026783b59f7fbea367d6fe845db61b84ad8ecbcea7b39277503bd5548ffc3e4b" => :el_capitan
     sha256 "1f44588d95c27cf0d0a5efc4e1aa892d00bbd3b5d55515db026c0715a6254e70" => :yosemite
