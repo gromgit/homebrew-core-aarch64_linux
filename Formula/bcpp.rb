1dde41ba49d177ace900abe034d0cda9ad659d47
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 06:53:34 +0000

    bcpp: update 20150811 bottle.

--- Formula/bcpp.rb
+++ Formula/bcpp.rb
@@ -6,6 +6,7 @@ class Bcpp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ca36fc5ff5890cebc995c184c9cd80d441fe5dd407d9a9ce11d662ce88df7551" => :high_sierra
     sha256 "c25d34a2e112bcdd0321d97629aa52cbfd594c1216876e759269df6ea3289e2f" => :sierra
     sha256 "7494b0aa2c2e24050eea15913c7c6a443f5bfed2640a3e53ee5fb2a3260b6495" => :el_capitan
     sha256 "d4f058c5d33b3cdffa2e844777bbd5aaa4f9b977f4cfe7ac9a31e4c0547161b0" => :yosemite
