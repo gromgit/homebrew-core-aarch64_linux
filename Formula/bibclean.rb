4829e0f5dcbe1692f078a9da116958a2f3313b70
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 07:07:16 +0000

    bibclean: update 2.17 bottle.

--- Formula/bibclean.rb
+++ Formula/bibclean.rb
@@ -7,6 +7,7 @@ class Bibclean < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0716be98bca14351bf4f0ca3da343e6a79bb945a34929eadf7742824342f48ff" => :high_sierra
     sha256 "addcead999d4c3ec3e5112c227512e200681e4775b163bfc24f2bff71ac15f77" => :sierra
     sha256 "1e09e98ec9528bbe6386fc12274bf56fcdc69ddccdef119b0dd3381cebefc8c0" => :el_capitan
     sha256 "6cba0c4e4d59324ed70a64718c6541dddba994c8cc509db1d7914a1e7f584dcb" => :yosemite
