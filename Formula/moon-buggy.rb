70c8ee2a3ab184af8bb56a7afbf15d3cca899644
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 22:11:17 +0000

    moon-buggy: update 1.0 bottle.

--- Formula/moon-buggy.rb
+++ Formula/moon-buggy.rb
@@ -7,6 +7,7 @@ class MoonBuggy < Formula
     sha256 "f8296f3fabd93aa0f83c247fbad7759effc49eba6ab5fdd7992f603d2d78e51a"
   end
   bottle do
+    sha256 "54948d0646240382661b765ab2253258946fb10b2974587d719b24a771172d91" => :high_sierra
     sha256 "fb2abda84d3e2b20f286caa036fadb9bfd6c4df151352a171385a54ca43acda9" => :sierra
     sha256 "b71bfe4abfb1d2c3d35db544850cb56f1b2ba50df18d27d3fef3ed5845b30e76" => :el_capitan
     sha256 "08b485a97197d8a0a2733e74622a232a8a1407ebd2564caccdffb9438176c1ee" => :yosemite
