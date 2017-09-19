da2ced7eedbd4cd1045b52ff49bbbaf47df88a55
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 00:28:03 +0000

    libfabric: update 1.5.0 bottle.

--- Formula/libfabric.rb
+++ Formula/libfabric.rb
@@ -6,6 +6,7 @@ class Libfabric < Formula
   head "https://github.com/ofiwg/libfabric.git"
 
   bottle do
+    sha256 "6f3d88a5b1897005827e14510daf10dcc2be59bc8c702a4192d59a612b3ad1bd" => :high_sierra
     sha256 "636003a9f03bd507340c0583f5798a8382d37357d4725b494df1e1d37010570f" => :sierra
     sha256 "0268dd158761aa12c821c3553e3d90469e6102d410e00c94fb52063af4c1af1b" => :el_capitan
     sha256 "aa9e0ca7778056e27fc156a2c427ea9c3284d853486fc767974e6fd449bf8d01" => :yosemite
