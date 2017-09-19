4041f0c9f66c935148dfffaa42499caece3d68ea
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 19:41:50 +0000

    mesalib-glw: update 7.2 bottle.

--- Formula/mesalib-glw.rb
+++ Formula/mesalib-glw.rb
@@ -6,6 +6,7 @@ class MesalibGlw < Formula
 
   bottle do
     cellar :any
+    sha256 "eed2ba469d6eb3188243850389500a436a5ecce39e946ff2e84c3fb85348f069" => :high_sierra
     sha256 "270fab4437a38d2d78d7789b2dd34b04e9c8a392975af41440aa14138bc31104" => :sierra
     sha256 "40caa5087e3b8b31b02c9ee7ec00f4eca3cdd4b35eec1fea0ce0ecd64a32689a" => :el_capitan
     sha256 "d700939e346f00e21a71e273895ff61d6984924d85cf41de461fb6674e326f8e" => :yosemite
