627915aec8523f0e838143323ae33ed0b4b76254
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 01:55:45 +0000

    libmpd: update 11.8.17 bottle.

--- Formula/libmpd.rb
+++ Formula/libmpd.rb
@@ -7,6 +7,7 @@ class Libmpd < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "366b75cc5d921e946f5d987cb2627a9d66d04db36032ff98f5dd881ff0df754e" => :high_sierra
     sha256 "e5affb45da15f4b7df327b993216d44f76f88da1e8c2f1051a8045c63a5a9d04" => :sierra
     sha256 "d4b932dc975f7fe87d8e26ebe9080d3633c33a66438c29d0403160adb6c7ada5" => :el_capitan
     sha256 "36471b19608eea97bc9916fdb65937fbb385ade1bf43aac4c01031d3c3c1192f" => :yosemite
