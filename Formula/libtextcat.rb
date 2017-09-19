780467aee4b388d10aa086a2380e279d5207ab54
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 03:39:01 +0000

    libtextcat: update 2.2 bottle.

--- Formula/libtextcat.rb
+++ Formula/libtextcat.rb
@@ -7,6 +7,7 @@ class Libtextcat < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "7997ea512b672f165e1e53e941147e9a520a9ab5d71b8b22e4a71622690e7cdb" => :high_sierra
     sha256 "24fe8791549204d8ef6e596fc327fbd3a645c729b440ba31ef47cf545f6f5b30" => :sierra
     sha256 "afa51f83d0a3c96ffc6f6c35011c864347f31d2c3aea987102c59f0257177072" => :el_capitan
     sha256 "1a63f24b16949843f6a3f6c17d9467208a471cfa6bf1b193738fa94c2d320f02" => :yosemite
