208c4ba412741ab8016d670b79266ad5a270d533
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 03:58:02 +0000

    libxdiff: update 0.23 bottle.

--- Formula/libxdiff.rb
+++ Formula/libxdiff.rb
@@ -7,6 +7,7 @@ class Libxdiff < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "46a8499130fcdafc69e79436a77338398139b7ac54b3ae5f0ca9ba75b9f7efc9" => :high_sierra
     sha256 "4a29b90dc48e4ce505bb50e575cc91107df8d93a90fe49ab4ec02df6118158ec" => :sierra
     sha256 "55d89877bd5457b7a5b77cf68187f544ff413228ec17a701e7879644ae528f35" => :el_capitan
     sha256 "6269c8d291cea44aceda9bd8e1e93a061e64d76852e47b781ae49aee28f0c31c" => :yosemite
