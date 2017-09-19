5b364f5aebcd1ef8734c00cdfcf3adb3f722d161
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 14:38:09 +0000

    gsoap: update 2.8.54 bottle.

--- Formula/gsoap.rb
+++ Formula/gsoap.rb
@@ -5,6 +5,7 @@ class Gsoap < Formula
   sha256 "158ed9c674941c382850e8e96ac9b0174f4e7aacb25794349bd07f52261921c8"
 
   bottle do
+    sha256 "ecb620b1ce7dca9aac94f83ca6f5ad8a1e304118dbe47ebdba4283a81060108c" => :high_sierra
     sha256 "0b2d2e9bbc46470fb53f832c6745037b5843f2d48d762795ea5f51451163a959" => :sierra
     sha256 "d12f03a42dc55ec64d99ce3680d8883f88ffc620d38265364d873b62ba0dc3e1" => :el_capitan
   end
