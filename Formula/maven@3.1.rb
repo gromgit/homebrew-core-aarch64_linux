c399ee48484f70ff9442f1181956b6d77da56eaf
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 17:14:22 +0000

    maven@3.1: update 3.1.1 bottle.

--- Formula/maven@3.1.rb
+++ Formula/maven@3.1.rb
@@ -6,6 +6,7 @@ class MavenAT31 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "816abdeb1351192b568e21da788f39fb53d80bacb97c25b453efffe77456320a" => :high_sierra
     sha256 "eebf2748f352b0bc30d358f47e0ecd33ddb1d4286a801c2971fa54ba10639ae8" => :sierra
     sha256 "ea999fb27f3944de65bc42f824b6f16a6b8f4d31c04624ac233e997908284310" => :el_capitan
     sha256 "ea999fb27f3944de65bc42f824b6f16a6b8f4d31c04624ac233e997908284310" => :yosemite
