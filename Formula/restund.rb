1af6d313d520bed818f1c469eaf78024914eaa1e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 10:33:04 +0000

    restund: update 0.4.12 bottle.

--- Formula/restund.rb
+++ Formula/restund.rb
@@ -6,11 +6,8 @@ class Restund < Formula
 
   bottle do
     cellar :any
-    sha256 "17efaad7de4d9fedca7f71b2381d27d0520aaa4bb06f232ac1f42cc815d70110" => :sierra
-    sha256 "215521ec7fea46b6060e35b674d905ebafe0bf2aa5135793e1e1698db5680c07" => :el_capitan
-    sha256 "39260c5e38d471692edc0b140ea1d684bb1b86549a4299d1606256d9165df706" => :yosemite
-    sha256 "6ac6767f9ce4917b089be8545532768fc5318e40feed05c34ca0ea0580c4c749" => :mavericks
-    sha256 "08ec540956dd4491876f7edd6c00bcab38b019579c92de565390efedeac50822" => :mountain_lion
+    sha256 "b54c91bb6457a8af37e55064d9f5093212ae7dc7c53863f4344f0862a0e6706e" => :sierra
+    sha256 "520b20cfdfb1cb5da1ee3a147a707802f6cc77a289c80e2b04a4299446e67408" => :el_capitan
   end
 
   depends_on "libre"
