fa422499834b3d02455baa185208725c1aa5ac00
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 03:07:36 +0000

    libsmf: update 1.3 bottle.

--- Formula/libsmf.rb
+++ Formula/libsmf.rb
@@ -7,6 +7,7 @@ class Libsmf < Formula
   bottle do
     cellar :any
     rebuild 2
+    sha256 "fe3d74c8f7ef3e623e00c7ae3e9bfbba1746047e6334fcc2f46464179f6594e3" => :high_sierra
     sha256 "69c8d53cfacadd06dc4669cb3713028d67333b306e82f36ba1b2d2eae0b9ddb1" => :sierra
     sha256 "c2cd00a63de6490b025d343c349eebda8e4d4545e2669acf116c7d8f84e48dca" => :el_capitan
     sha256 "9b099b33da929eac5c88deadb46136c8e728783793d8a07439de72eb838a61f7" => :yosemite
