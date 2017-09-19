5fe855f23fdbfcb9b12a735fec0bc29084962d98
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 19:43:45 +0000

    ori: update 0.8.1_1 bottle.

--- Formula/ori.rb
+++ Formula/ori.rb
@@ -7,9 +7,9 @@ class Ori < Formula
 
   bottle do
     cellar :any
-    sha256 "0a956101868e56913489c8d89dc8fdb741a2bc407d432e5d86985e87f850fadb" => :sierra
-    sha256 "163e473c5d766ee758cb1140c8ac3428f9a5a2cd9490983ffd1bf475b07cd8b2" => :el_capitan
-    sha256 "c33275fafd72cbd3139c7e9dcbaa16f48e2f450d08e06df717700b3ca2664d34" => :yosemite
+    rebuild 1
+    sha256 "3851019914d6ea9efc9115c100b73d24775b6d5d6c517399aea4259e74097670" => :sierra
+    sha256 "c19677cd17c419810b6389aa7c2815f0f8da16d7035dbdad5e209a96913e1f45" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
