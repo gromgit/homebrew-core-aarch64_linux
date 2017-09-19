a025f39c487e667acb30fa9a3adfbcff1426b664
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 16:53:17 +0000

    makefile2graph: update 1.5.0 bottle.

--- Formula/makefile2graph.rb
+++ Formula/makefile2graph.rb
@@ -7,6 +7,7 @@ class Makefile2graph < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "51231ed0ef44fd31a10f4ea0a7500570181332786ddd5a8a9a886958ad1b1408" => :high_sierra
     sha256 "274ee025c45df9757d608249d64105b9314c8e59fc52a81ad6906f807498b67c" => :sierra
     sha256 "ed1939b1b0fd106f3e328e310a887cf454b81481f78fdf57ce75c0480a922d7d" => :el_capitan
     sha256 "37aebae489e0f341f80417ec711e5c2817f5b8097c3493dcc11bc754bdd1b1cf" => :yosemite
