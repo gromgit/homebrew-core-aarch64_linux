6e4e6cd1280f758d74ba0bb1d58f7fef9c845507
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 01:16:14 +0000

    libidl: update 0.8.14 bottle.

--- Formula/libidl.rb
+++ Formula/libidl.rb
@@ -7,6 +7,7 @@ class Libidl < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "3d80aba8c6f6cc762bf20abbcdc5098d39d9cc5ecc9a31f1e08b96c35deb1004" => :high_sierra
     sha256 "d8c0e559183f9cfb1ac579acb95db74c73c4f276ac791eb4ffcb65db8e47c0ca" => :sierra
     sha256 "0c81487e7c93733097a62e6c693943fda8b13ce03ff6a6972a320c711d561e39" => :el_capitan
     sha256 "356ad466ab477116e8f030ef75b70bc46321f68ec1f077f9b06dca3bb076e499" => :yosemite
