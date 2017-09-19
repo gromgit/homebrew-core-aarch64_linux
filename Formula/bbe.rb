91dddc42459a9669e78c8e1dba4eea4b5820c7fe
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 06:37:32 +0000

    bbe: update 0.2.2 bottle.

--- Formula/bbe.rb
+++ Formula/bbe.rb
@@ -6,6 +6,7 @@ class Bbe < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "95cef154264d814bcdb543da64b8947ed8219411c3da20d854f30bd0aeb1332a" => :high_sierra
     sha256 "4f533ae33e0c46a01bc11f1c8b99ef6baba62a376ddee1000de1fa199f18545a" => :sierra
     sha256 "d9c63d7b9657e6f1c0e53048564f275283177e3513e202a7a9cfc69571bb5008" => :el_capitan
     sha256 "ae0aa826fcd9f11e93428e9eaeedb14a56c193c861f09123999a8ba0f3d7f9cd" => :yosemite
