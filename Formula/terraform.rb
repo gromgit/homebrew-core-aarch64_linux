e9544cad4604d796635a82c568de7fcaafc7c4d6
Christoph Blecker <admin@toph.ca>
2017-09-19 10:26:15 -0700

    terraform 0.10.6
    Closes #18287.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Terraform < Formula
   desc "Tool to build, change, and version infrastructure"
   homepage "https://www.terraform.io/"
-  url "https://github.com/hashicorp/terraform/archive/v0.10.5.tar.gz"
-  sha256 "9030db9346ee7fa19a49ebb4207812dcda44721c36d1250ca8afa28e9b14c7cc"
+  url "https://github.com/hashicorp/terraform/archive/v0.10.6.tar.gz"
+  sha256 "3170223c601263a7be9af9d14194e36170900cecfec784464abca1679e03fb11"
   head "https://github.com/hashicorp/terraform.git"
 
   bottle do
