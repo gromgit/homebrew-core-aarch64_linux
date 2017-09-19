d67da95981c9307a7d37b1f9674bd13b4ffe750d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 14:21:48 +0000

    grsync: update 1.2.6 bottle.

--- Formula/grsync.rb
+++ Formula/grsync.rb
@@ -5,6 +5,7 @@ class Grsync < Formula
   sha256 "66d5acea5e6767d6ed2082e1c6e250fe809cb1e797cbbee5c8e8a2d28a895619"
 
   bottle do
+    sha256 "1ebac759b0f920f9bbd810694bce106976d7bdfbda2d2922e73aabb05c5b19bc" => :high_sierra
     sha256 "c35defcfcef7d9f1af5c8fd82c95733d1d0191e7c9a338f9223744e55adf32de" => :sierra
     sha256 "d0afd0d818a8c4a72368c85d1123f76997470c16c2ab38ed78b6e1b977c7b691" => :el_capitan
     sha256 "7046b8ffefa83d5a153ecd75d9255813b95e325595c712bec1d0a11727f70017" => :yosemite
