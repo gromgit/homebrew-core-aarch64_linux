e9febefc3fbfdfa98942ea09bfe06e50627f5575
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 03:26:12 +0000

    libtecla: update 1.6.3 bottle.

--- Formula/libtecla.rb
+++ Formula/libtecla.rb
@@ -6,6 +6,7 @@ class Libtecla < Formula
 
   bottle do
     cellar :any
+    sha256 "dffae78362e21bf324ed651a2b80ff924b1bbec60916159863e66c7171072a9c" => :high_sierra
     sha256 "21cd696f6e79ae6401dd19f832ac24263f016a62c2d15ec31e25d515bbea5983" => :sierra
     sha256 "3ceb3942ea4ae1434dcc0aea00fa58b6f16787bc1a0067e9497ad4cb050f771a" => :el_capitan
     sha256 "836d6100343197540f079ea7f6b9e5641fd8efc4e331d3492f8be4cd41ced6e9" => :yosemite
