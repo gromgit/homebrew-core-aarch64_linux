eb5929b59c2a70e6fce28ebb1a82cff3fd9a4e06
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 16:22:03 +0000

    kompose: update 1.2.0 bottle.

--- Formula/kompose.rb
+++ Formula/kompose.rb
@@ -6,6 +6,7 @@ class Kompose < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "04e2cca9ffd595e926ed3a386e348ba58bc606e9a54d4bf013992ddecfacd4d0" => :high_sierra
     sha256 "6c1f0fc6d0b821719d8ebb3b2447c969512c176b5878f9a7df32333547ea8d7a" => :sierra
     sha256 "ae9f5cdbdc636d6f0f9e1afda5fccab2d0ff0054b07654657581f6c6d71e8597" => :el_capitan
   end
