a57fc8022437eb29b24951a2f54eb4b8fc3bc9ff
ilovezfs <ilovezfs@icloud.com>
2017-09-19 04:38:30 -0700

    gcore: use http for homepage and url
    as the SSL certificate expired 9 Aug 2017.
    
    Also, mirror to Bintray.

--- Formula/gcore.rb
+++ Formula/gcore.rb
@@ -1,7 +1,8 @@
 class Gcore < Formula
   desc "Produce a snapshot (core dump) of a running process"
-  homepage "https://osxbook.com/book/bonus/chapter8/core/"
-  url "https://osxbook.com/book/bonus/chapter8/core/download/gcore-1.3.tar.gz"
+  homepage "http://osxbook.com/book/bonus/chapter8/core/"
+  url "http://osxbook.com/book/bonus/chapter8/core/download/gcore-1.3.tar.gz"
+  mirror "https://dl.bintray.com/homebrew/mirror/gcore-1.3.tar.gz"
   sha256 "6b58095c80189bb5848a4178f282102024bbd7b985f9543021a3bf1c1a36aa2a"
   revision 1
 
