65041d559752d20ef2c6aa4b7fff87c4394c56e7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 13:43:45 +0000

    gosu: update 1.14.6 bottle.

--- Formula/gosu.rb
+++ Formula/gosu.rb
@@ -7,6 +7,7 @@ class Gosu < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b8aa5ec8782193ef8abc8dd1d461b08aadad62c498ab5837a37d57702d45afbc" => :high_sierra
     sha256 "9655ec17694b5410a1deb620c8d6c03c5ddc0532814d11ce0bcf9487c9b5f5ac" => :sierra
     sha256 "fd311848681a64d3727889d47c061a52a55d64dc5236df1b06c0628a0bf71837" => :el_capitan
     sha256 "dee97ff03b12443a4fcaab053ca8860377279e9b8676f4b487f57e7095422d1e" => :yosemite
