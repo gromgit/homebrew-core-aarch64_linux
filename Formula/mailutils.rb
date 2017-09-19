e687f9f02f7cf856b8288cf8258a838b28ec2f22
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 16:53:17 +0000

    mailutils: update 3.2 bottle.

--- Formula/mailutils.rb
+++ Formula/mailutils.rb
@@ -6,6 +6,7 @@ class Mailutils < Formula
   sha256 "0b977195fed004eaf1cb680883d5adc099c31bbac5d666c58cde82ac8b9b4e01"
 
   bottle do
+    sha256 "e33264c4196d8401b5dc91df196392e8bbebccafcfa718e2b38209533dfb67a6" => :high_sierra
     sha256 "a024ce8993a0cb91d634f85d20e7febe6fcffefb1bf25d41a06965999bcca402" => :sierra
     sha256 "7a8fbf59e72392e54e921551d1467b6374e23f7acb9171764dd1537117430f52" => :el_capitan
     sha256 "51a5d3453cd8b45d49cf776bb6ab2c4f88f34334ce55ff949cfd1ac08aa47306" => :yosemite
