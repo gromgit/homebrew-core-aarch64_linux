54d7a15e90e5e1e91e57989bd151408ce98d1335
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 13:11:53 +0000

    gobby: update 0.5.0_3 bottle.

--- Formula/gobby.rb
+++ Formula/gobby.rb
@@ -8,6 +8,7 @@ class Gobby < Formula
   head "git://git.0x539.de/git/gobby.git"
 
   bottle do
+    sha256 "2101fa7eda3329eb4c59645a536819a2b63ad1179db2a09a5cad315a7c14e66d" => :high_sierra
     sha256 "78045e763ef4add614e757f3c22b4bdd6266930550b80850ac3967955eb0a24e" => :sierra
     sha256 "21e2f4df3800d200209ab46be26e30a29579a93395cb2e8b385c23d41906f12e" => :el_capitan
     sha256 "dfb1e48d8d013fa4f969d2617b816d02bc019fe396d3d789634a146374faf10f" => :yosemite
