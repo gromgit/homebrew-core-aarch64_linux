625f60785999df6c42e391e58f02b251831dc759
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 13:43:45 +0000

    gpa: update 0.9.10 bottle.

--- Formula/gpa.rb
+++ Formula/gpa.rb
@@ -6,6 +6,7 @@ class Gpa < Formula
   sha256 "c3b9cc36fd9916e83524930f99df13b1d5f601f4c0168cb9f5d81422e282b727"
 
   bottle do
+    sha256 "0f3c6b1ab260f768ca9337ae2c0f0a38daa896c1a1b76e6f19a0186911211ee8" => :high_sierra
     sha256 "858ba357b188c140d61ba5570b0237a827b0a3bea3d4a87ac7e9aaa95e397230" => :sierra
     sha256 "91271433b3ff2c4de071f2dc9fc1dacdc9957b6d4075211d913d61f7d2c02ed4" => :el_capitan
     sha256 "fb0e954550a5a90a492afc6f31c13a4d96a4a5b462228a46ccbf133e62e7497b" => :yosemite
