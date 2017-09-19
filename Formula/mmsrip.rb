4c7b3f77c7bfd12cd68d9c0a189a99a231ccc479
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 21:40:26 +0000

    mmsrip: update 0.7.0 bottle.

--- Formula/mmsrip.rb
+++ Formula/mmsrip.rb
@@ -7,6 +7,7 @@ class Mmsrip < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "e7c12a6c8e6ef612d1c789fad3e06c0b21acfe6e4dbac1643ae7797faeafcb35" => :high_sierra
     sha256 "b4578327661828737b3aa71615806ba6e2781d7c0815a12815023242ac80e598" => :sierra
     sha256 "cf0bc6b407f4861b174eddf55ae5da45330d37abc428013ca19f173d36a96d2a" => :el_capitan
     sha256 "07f4c9d5a84bb52e3c799ebe7c395a4939c0c7ac5dff0fb46e2ce84abd9b5417" => :yosemite
