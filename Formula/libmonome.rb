7bf68072c675c8df50dc1831cd6f1427bab177af
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 01:33:40 +0000

    libmonome: update 1.4.0 bottle.

--- Formula/libmonome.rb
+++ Formula/libmonome.rb
@@ -7,6 +7,7 @@ class Libmonome < Formula
   head "https://github.com/monome/libmonome.git"
 
   bottle do
+    sha256 "0862688b7c3a848086aad05bc368144028a3cddca711f4c1b7bf37a10566a161" => :high_sierra
     sha256 "6473c190c553546e9f648b7dbcc5c43b1b4cfeff54898d7f373c309092c5ad86" => :sierra
     sha256 "b6553b4ce4d56cca44493acd9615cc399d5e20b6acb403a36914a0df5151926e" => :el_capitan
     sha256 "0c730849c05d8899a6e4bd0f1c3bfdeb791de8fd5d8b10d5c29800b68a2a0906" => :yosemite
