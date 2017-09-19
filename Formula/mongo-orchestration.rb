92d4c79ef10211d54057b89209f4b6a3f86533bb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 22:04:14 +0000

    mongo-orchestration: update 0.6.10 bottle.

--- Formula/mongo-orchestration.rb
+++ Formula/mongo-orchestration.rb
@@ -9,6 +9,7 @@ class MongoOrchestration < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6d949946d2adcfb643c44af429874b237956bd3d991c732862fa41722db043e3" => :high_sierra
     sha256 "da51a365363fc735053559a1d2597a27bb64dac8134107a1f2e07eb50125d1af" => :sierra
     sha256 "05ea25edb9666d4f8899babbce349694a96c10d7abff4cadbdb9cf286269fac6" => :el_capitan
     sha256 "69e5b710ef81077007e50ca8e8817343f133229d76f12162ccf63220bfa0afb1" => :yosemite
