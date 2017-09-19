fc191e845b7d2c552cc059a0eb7def7e485d2f86
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 10:27:45 +0000

    gauche: update 0.9.5 bottle.

--- Formula/gauche.rb
+++ Formula/gauche.rb
@@ -5,6 +5,7 @@ class Gauche < Formula
   sha256 "4c8a53213de112708bbda5fa9648c21497d43ebf809ed5b32b15f21266b4e73c"
 
   bottle do
+    sha256 "7833855dcc680ac0617171d68bb063a193d74e3133f12bd06a01dd221201fa51" => :high_sierra
     sha256 "0bfbcbb0f5066b939712e3eed33f0ed221f1e6ba096aca95bc6ad1d69b92c74d" => :sierra
     sha256 "2959fc3962fe83ac5d66f522d0be8b5abb7d86f43594c47b0ea34bf9c4db7e5d" => :el_capitan
     sha256 "31baa1ce9e676f98f708b9d2ec84fdeab7ca8a61a7153c90fefc120fd298b66c" => :yosemite
