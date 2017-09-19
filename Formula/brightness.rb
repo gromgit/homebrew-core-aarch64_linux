84afea9054c7c99811bcbec7c9069e6dfe9cbe82
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 15:35:58 +0000

    brightness: update 1.2 bottle.

--- Formula/brightness.rb
+++ Formula/brightness.rb
@@ -6,6 +6,7 @@ class Brightness < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "08b29843308b1cd3603aba3f2e5e3d2e7dec34dbe62bdb5e506b7bacdcff8df5" => :high_sierra
     sha256 "edd4123953a961e94ce78b076b116c987f668ca73e0a67339e908ead6ded8441" => :sierra
     sha256 "675d9a1b7e39b75d2b569fa4f148fbc2342dbcd4a1b23045763c0103058ecc26" => :el_capitan
     sha256 "360b009d1a2ffed665c9d9168b3f91edba44c8da8f08d2f307b09ee63f399e0d" => :yosemite
