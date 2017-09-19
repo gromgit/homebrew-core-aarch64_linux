c62ae55a316d1af9db2a468137bdc324e9d8e38b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 14:21:48 +0000

    grt: update 0.0.1 bottle.

--- Formula/grt.rb
+++ Formula/grt.rb
@@ -6,6 +6,7 @@ class Grt < Formula
 
   bottle do
     cellar :any
+    sha256 "150d3e7148af480198401bf4e7763fc296a81f81748efe8247f3b603087d7bca" => :high_sierra
     sha256 "8c31e85370522a4db436657f0f5501f0fd20befc31b969a0c0db6c33ed12aed7" => :sierra
     sha256 "f48b42fd6f856239fb1f004a700aec5a85c129dc0a4a2b5955ce6a9a9721b231" => :el_capitan
     sha256 "aa26978c7029c36ab7d20a0e092968132e96255087f4155a1739db6dfcb9c170" => :yosemite
