979aee3d12ad6b344c4a70db1d21cbbd9ac141f2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 07:05:13 +0000

    balance: update 3.57 bottle.

--- Formula/balance.rb
+++ Formula/balance.rb
@@ -6,6 +6,7 @@ class Balance < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "77589c441e2c89d6fb3df19b51487fb4684327fe63c5fe768224d10f81868d3c" => :high_sierra
     sha256 "02b241cd5085873f6f2e78c99c01b1be6c89a3a2ff9fc12e17600035096dc44e" => :sierra
     sha256 "c6af3ec64f795a6ba24400e83b3ab3753564a57f8546f0137368860bd2605421" => :el_capitan
     sha256 "07f517fc19b99e5d52f6a90576ccd718650bd6a291d7c808f0d8b8193bce7779" => :yosemite
