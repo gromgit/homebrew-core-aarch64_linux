d116a0e6c1fd087482b453d6dbb0b1b1151e0c8f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 00:29:58 +0000

    libedit: update 20170329-3.1 bottle.

--- Formula/libedit.rb
+++ Formula/libedit.rb
@@ -7,6 +7,7 @@ class Libedit < Formula
 
   bottle do
     cellar :any
+    sha256 "537d74532e5778f106e87e682169c7ebfc3acd6624286d3ded9e79262bafc4d4" => :high_sierra
     sha256 "1bac371537a7a38d1193bcbe80170b5a2d592f568c5d7f6f8e01fd2fada68a3f" => :sierra
     sha256 "3015b4190af4a5ddf26739884dde6ebb289cc16187958213d904551a51777013" => :el_capitan
     sha256 "45a9c2abf6fb9cd0c8cdcbad8a708e00879aef224b573069cc21cdb6e42e109b" => :yosemite
