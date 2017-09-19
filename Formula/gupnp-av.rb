96ae819f9a3f8871382ba1845083e4dfc1e63718
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 15:19:16 +0000

    gupnp-av: update 0.12.10 bottle.

--- Formula/gupnp-av.rb
+++ Formula/gupnp-av.rb
@@ -5,6 +5,7 @@ class GupnpAv < Formula
   sha256 "8038ef84dddbe7ad91c205bf91dddf684f072df8623f39b6555a6bb72837b85a"
 
   bottle do
+    sha256 "0765d8ba4e39bbbb16726d472cdff252fcaac5c5ddbfb62527e91e80967f39e0" => :high_sierra
     sha256 "87410e9f5a9aa1fb9b7d706d55762d540922f35fade1394ce8f65614e9d3a16a" => :sierra
     sha256 "a92492fb1109fd453415e73bcf2b7ac5e99aefa3ea85f863d94b741277394a6a" => :el_capitan
     sha256 "368db5c08311273ff6bd51c2f30afe9acfd06481c93191e76d73cce6050c128e" => :yosemite
