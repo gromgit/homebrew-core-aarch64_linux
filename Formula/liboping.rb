1169762fe2655b748852cefdaba08364856c95ca
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 02:17:20 +0000

    liboping: update 1.10.0 bottle.

--- Formula/liboping.rb
+++ Formula/liboping.rb
@@ -5,6 +5,7 @@ class Liboping < Formula
   sha256 "eb38aa93f93e8ab282d97e2582fbaea88b3f889a08cbc9dbf20059c3779d5cd8"
 
   bottle do
+    sha256 "accffc91ab24ccba1214727abadb59c497f403e3bcad1dfe8ff0377d32e05ebc" => :high_sierra
     sha256 "42b80e23afe4fb4f296d039b0bdd4ccd0da21937514fdd04a90bc01d39da7aec" => :sierra
     sha256 "de0bb72a0752469b262db3a24a41c84746930858462cd08993c057caadd46264" => :el_capitan
     sha256 "c4f46d01bdace450a49e2c4fc4ba4056070bf1b869ed07f1b0a1d6a4f7646bc9" => :yosemite
