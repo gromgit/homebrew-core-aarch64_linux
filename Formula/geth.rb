6432337413ccabe22c94793c6bb994993bef2eff
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 11:24:34 +0000

    geth: update 1.6.7 bottle.

--- Formula/geth.rb
+++ Formula/geth.rb
@@ -7,6 +7,7 @@ class Geth < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "482c28166ec0f0a3c30bf0e4a46c35229f6a5db4332b6f52ae00699756a8c110" => :high_sierra
     sha256 "31dd16120f0f1bac73c78290269dd368c407f832bfbc72f297fd951647793f39" => :sierra
     sha256 "4c7422b68122e556784543239933e9562a0a465aaac2b32ab3da63495aea6a85" => :el_capitan
     sha256 "cd1e48eda14f089b9360d391e24f0224f289f212150ae4e9bc5698bfe01e6a8b" => :yosemite
