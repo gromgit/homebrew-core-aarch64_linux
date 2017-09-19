bd5869de7d92c4ac999718822dfe6de1dec633ee
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 07:05:13 +0000

    baobab: update 3.26.0 bottle.

--- Formula/baobab.rb
+++ Formula/baobab.rb
@@ -5,6 +5,7 @@ class Baobab < Formula
   sha256 "ca07c18c8582f6c2fcb54b414f0b9057ebe53e54296c3ce9ccee44b78e86b2db"
 
   bottle do
+    sha256 "036e138c507f190b7fc243911d8e2a9426235fba7932097a79f2361fb1a7437c" => :high_sierra
     sha256 "24d64fd6acefcf26f71bf121bddae4a3749515e47de2ea233642d99444804f06" => :sierra
     sha256 "749b46d3a4e2d3ca1fb1969ccd85a5f0ec844e1a5dbb824173627af172b862e6" => :el_capitan
   end
