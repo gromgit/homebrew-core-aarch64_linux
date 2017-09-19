017880f8eab10d670ae224febfb87511144e1707
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 01:55:45 +0000

    libmxml: update 2.10 bottle.

--- Formula/libmxml.rb
+++ Formula/libmxml.rb
@@ -8,6 +8,7 @@ class Libmxml < Formula
 
   bottle do
     cellar :any
+    sha256 "ce2ace2a91e3d948911a6c44c378b092d8dc807aff0a328a705822ca5c391b41" => :high_sierra
     sha256 "4287dd3c4346f82d9a410bf4bdce69329ef1f931e7639e6c27fa8f79e1e31cb4" => :sierra
     sha256 "01d01bf5766e398837a416949f74eb405e57903dbc27a55fc589e0559e60c518" => :el_capitan
     sha256 "bb2a6d25c76e7e65eb5a14728763a03f41cb7c5f68bb43cd73da4f92df0226cc" => :yosemite
