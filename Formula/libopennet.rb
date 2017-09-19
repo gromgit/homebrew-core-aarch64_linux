7be6e40b9ce47043a0ceeee0fa3f3e26e95a0724
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 02:17:20 +0000

    libopennet: update 0.9.9 bottle.

--- Formula/libopennet.rb
+++ Formula/libopennet.rb
@@ -6,6 +6,7 @@ class Libopennet < Formula
 
   bottle do
     cellar :any
+    sha256 "82232ab7a71481570e3119e119bc93ef29a9d553c11e16fb93f157c66a7dfe8a" => :high_sierra
     sha256 "c3447365aeb1a478b14b5a71b6ffd29a1f95754fb460ed6f618b55e2f958c227" => :sierra
     sha256 "de1cb9622ec3b6501236af7e66367bc5bbaa20dfb8e3ae328a339bea6d708bab" => :el_capitan
     sha256 "abcf105b630a05b8a7d26f1a0ba8defafcf31c0dc23e79c3a5f8a9a14878e6de" => :yosemite
