ed87c33dd009ad5e2db34b294958683b51b09288
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 07:07:15 +0000

    bibutils: update 6.2 bottle.

--- Formula/bibutils.rb
+++ Formula/bibutils.rb
@@ -6,6 +6,7 @@ class Bibutils < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3a3bbfc99cc8fa1745ee68f974ab2de3b95259dc17dc3bba12f533ebccfb6156" => :high_sierra
     sha256 "8cf6d98679a31d08464c3d5351cb647d36984c31006822806f97c34420167837" => :sierra
     sha256 "0de88bf525c48769c2d8729787aac0571efbdcbd035f6f1e540350b31301fd7b" => :el_capitan
     sha256 "575f83b6e50e14095ea3add04428260a875812aa063fb335f12946d340820187" => :yosemite
