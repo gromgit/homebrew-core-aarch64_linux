48f6c4e4c7dde514a29c180a2eb045105abf2da8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 17:42:30 +0000

    mawk: update 1.3.4-20161120 bottle.

--- Formula/mawk.rb
+++ Formula/mawk.rb
@@ -6,6 +6,7 @@ class Mawk < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "94139be328284f4cc67be40cf7d07045b0a26bd173aa58fa448437077cc877e5" => :high_sierra
     sha256 "52d61963ba6494674cdea5d3fd95e9551b8277b9bc5e189f93b1126d5ae3dd27" => :sierra
     sha256 "d39e71dfe41cdf2e480a5858938918509e2a00e6b5dcb181d14fc5cdedd91b24" => :el_capitan
     sha256 "8ef45e294dbab1b4c4615861b6eee4fdd179649072fe41eb554e6bc93f2f5a42" => :yosemite
