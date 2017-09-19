78ef2b1a0f36301d6b41f95bc37ba295857ea2cb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 09:08:50 +0000

    bzr-fastimport: update 0.13.0_1 bottle.

--- Formula/bzr-fastimport.rb
+++ Formula/bzr-fastimport.rb
@@ -7,6 +7,7 @@ class BzrFastimport < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "af2955ee5a026c4573dbeb4be897babf2f8d3805636765b982b48793b8143c60" => :high_sierra
     sha256 "420a665897be0a5e807a396720e51cb722b8ead02effee281d5843d56e3881be" => :sierra
     sha256 "119240e135fcf0d170a009bd414b07fe13f65734afd5929de2527a62c66b6c79" => :el_capitan
     sha256 "1155531ccdff247dcf8ab9cae133263199cbd708a1ae6ddc4d6e68133d1ab712" => :yosemite
