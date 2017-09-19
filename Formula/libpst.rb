f91a4b193634d2c0039d30524469a265e85f0195
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 02:49:22 +0000

    libpst: update 0.6.71 bottle.

--- Formula/libpst.rb
+++ Formula/libpst.rb
@@ -6,6 +6,7 @@ class Libpst < Formula
 
   bottle do
     cellar :any
+    sha256 "23a35d1d7f679083d680723ca988801d9d215f7397fc5c87bf7f69e9982ee325" => :high_sierra
     sha256 "e869aa195bf9ad6d4d07d62cb48d9a4a52a289ecd79d1e61a44f84384d278878" => :sierra
     sha256 "85834aa1da7888a054391c7fe036d9d72f2a889adb6df3ec911193a3919a7db3" => :el_capitan
     sha256 "4b61b07a89496925c88f0f47d361465cf646f7b3caa01e470ba7eba7945442aa" => :yosemite
