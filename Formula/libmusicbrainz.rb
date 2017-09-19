3b2206f1abfbe90ba2838f22b91e8f2d2344ff42
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 01:55:45 +0000

    libmusicbrainz: update 5.1.0 bottle.

--- Formula/libmusicbrainz.rb
+++ Formula/libmusicbrainz.rb
@@ -6,6 +6,7 @@ class Libmusicbrainz < Formula
 
   bottle do
     cellar :any
+    sha256 "99b598b941ac0ce3747d8821943a1e730f3673b721421d9c0428b70259e789c0" => :high_sierra
     sha256 "8fe055e1f987e23a569f915082031e172a5c3d0aef6f86de78ce9c8258f53cd2" => :sierra
     sha256 "6a63410ca9eae84b263d7165d05701801f4e05de26a9e95a7396f95a602cedd7" => :el_capitan
     sha256 "0851c7889df9dc2971b60fe9fd8ad891afd8d5dae08877393e2f69e3cc33f589" => :yosemite
