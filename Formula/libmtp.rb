4b860d226cd54cb278988e2f248cc47f9580cb1c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 01:55:45 +0000

    libmtp: update 1.1.13 bottle.

--- Formula/libmtp.rb
+++ Formula/libmtp.rb
@@ -6,6 +6,7 @@ class Libmtp < Formula
 
   bottle do
     cellar :any
+    sha256 "16766b79eebef88f9bfa18d9a456b2ba2e83f0b3f34fe7b01315b598444438a2" => :high_sierra
     sha256 "4af77826ce6f18d800a15903896f63b72b0d8dcf96e006d515c73410befbe7f8" => :sierra
     sha256 "995f8eed26c66e201b16859754ff2d182b6846c5a04e547dc9c130e4cd335b56" => :el_capitan
     sha256 "ff44d7f06315ac5dd4332495bb726b6dd431e915b7a5d5f37299ceba94a96b85" => :yosemite
