e7e4f80d40d5e7954ea562e24153b4339ba027af
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 10:55:28 +0000

    gdnsd: update 2.2.4 bottle.

--- Formula/gdnsd.rb
+++ Formula/gdnsd.rb
@@ -5,6 +5,7 @@ class Gdnsd < Formula
   sha256 "ee26ddd11087f4dd617c9aa6bfa3b963c5d883578b49c7d5dcae5d0edf38094f"
 
   bottle do
+    sha256 "bef99d76ac59572c2f15d516c35815f62348c1287e578c6c02a37abefb4af6b6" => :high_sierra
     sha256 "609e16a1a067d94f0b492465b3bc23c6c77f350bd3d5f11c6425fcd950855f90" => :sierra
     sha256 "849263f34b81cfebc8c0f681a7c4454fbaf2c0e70f44dd2e9ae9f2ffda8e0aef" => :el_capitan
     sha256 "438cd01e0e0e06b9f8bb1a08c43403fbeb66d36a95040f38e67342f9e516ca48" => :yosemite
