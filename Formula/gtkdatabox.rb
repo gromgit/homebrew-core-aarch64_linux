1d4b49abc737d4e2a16afcbfc2f5b3c86c1c0d13
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 15:15:09 +0000

    gtkdatabox: update 0.9.3.0 bottle.

--- Formula/gtkdatabox.rb
+++ Formula/gtkdatabox.rb
@@ -6,6 +6,7 @@ class Gtkdatabox < Formula
 
   bottle do
     cellar :any
+    sha256 "b75b6701d5d94ce047235f1957a7be2a5f3c08ec1ed1eae8c6b6981c9d13add0" => :high_sierra
     sha256 "f58cf9bd17de4573596172b6ea10c4b2892f75c8dd597f5c98d74e30f21aacc6" => :sierra
     sha256 "eed0f8e816ec34ae1995e14f6596aeee02cb51f2b8f12a90715431de8847ca8f" => :el_capitan
     sha256 "f18139540018b72756a5ed3c89cd67e4235d75aa61bd15a7b86825441f4d33cd" => :yosemite
