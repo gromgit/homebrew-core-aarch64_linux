a96b12632bcb6c9bd60f1fb4d6930c16a99077ab
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 03:56:43 +0000

    libzdb: update 3.1_3 bottle.

--- Formula/libzdb.rb
+++ Formula/libzdb.rb
@@ -7,6 +7,7 @@ class Libzdb < Formula
 
   bottle do
     cellar :any
+    sha256 "4cbb32921ac9edc6308ac6ac0d26553661d6ea3f154c62af9c3aaf63d2afdf58" => :high_sierra
     sha256 "9cec69cc93f6b975d95c506f3c7b04a4d2934ac30adc13afedd1d32fe2e31d8c" => :sierra
     sha256 "0a7bc557e3e91db185787147df1a87ae9c7aef33a1b57875bc74daa52a1338c8" => :el_capitan
     sha256 "3a5394289ceffbd2bc7bde8dd4dfad4f9b27a4d0180fab199a839c44739f0344" => :yosemite
