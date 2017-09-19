47a0461d5de9b43494d798a11244ffc2c991eabf
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 03:26:11 +0000

    libsvm: update 3.22 bottle.

--- Formula/libsvm.rb
+++ Formula/libsvm.rb
@@ -7,6 +7,7 @@ class Libsvm < Formula
 
   bottle do
     cellar :any
+    sha256 "6fae589e624777638a8a5bc1c8b03e3bb48f88a61380b608273b44bd2d25aec4" => :high_sierra
     sha256 "39f3552e425be4bcb6e42d917b508bf94904520526c49ec52712c017680583fd" => :sierra
     sha256 "67867d2ddde33efd85da4c1a03757af0e3dcf591186552140876ddd11916d5df" => :el_capitan
     sha256 "3ee2001f87f2a58e698aeb0bfc413baa820184dba1caa2b3ec0a5a593a80d651" => :yosemite
