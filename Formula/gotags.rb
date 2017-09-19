bdf6f45ff0d4dd63667ea5ac37bd2731d2f60624
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 13:43:46 +0000

    gotags: update 1.4.1 bottle.

--- Formula/gotags.rb
+++ Formula/gotags.rb
@@ -8,6 +8,7 @@ class Gotags < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "fd44bf7cd73b2fddbe2184c79e3227a1ffd72896958482785c592c22b1186c25" => :high_sierra
     sha256 "1a942b988d0362a034c030a6d5609cdae6edc24a6d8ca21c61eb29e9fe37e194" => :sierra
     sha256 "8f59b1c7639c0d4a8f2ec7a4ec037ed48d365cb1f64a51e7d6704264bc4e840d" => :el_capitan
     sha256 "ecab701e7806da9a5d81977a68460df32a4f15c5d424c396ce52e175d8dbee15" => :yosemite
