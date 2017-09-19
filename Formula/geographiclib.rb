b0f9a570a78d7cdfccd20ced1ea8e39e40d07c7e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 11:21:20 +0000

    geographiclib: update 1.48 bottle.

--- Formula/geographiclib.rb
+++ Formula/geographiclib.rb
@@ -6,6 +6,7 @@ class Geographiclib < Formula
 
   bottle do
     cellar :any
+    sha256 "c35847e689804879bf9a6cd0177ecf69af577759e003cc42991a9ae3e9104641" => :high_sierra
     sha256 "f330164f390a5a55cb8b14e8e5ec3d140c0a5563d3365608e7068dd09542cb2d" => :sierra
     sha256 "bd02a5b4a1ed096d7ae1ae7df893fa8e17fecdf537173aeb34682a014309cad8" => :el_capitan
     sha256 "a5fc291555d18dcd2c80a6ea3f7ff77337b8c60cc016c07c2cda52599bf1c963" => :yosemite
