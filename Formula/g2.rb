d809b9f38d0b6aa9454eba18ad0d16c578c530f0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 09:43:39 +0000

    g2: update 1.1 bottle.

--- Formula/g2.rb
+++ Formula/g2.rb
@@ -8,6 +8,7 @@ class G2 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "172bb101cd40ab61ea7a6ce85798556da5bbb980ae050023e8e8ff9f4d2e2c52" => :high_sierra
     sha256 "6bd5de5c1e1335c1be168bf5eec800c8ac5d0b4d16534a7e686d9c4e8d396417" => :sierra
     sha256 "45c2029c3fc914866ba32966a78cba39b8415ba7f191cd1eaaf604db798b6d3f" => :el_capitan
     sha256 "5645b9c9401aa9f047082612de0e7bbd119ff7fd9fd49d94d45ce2adfbbfb69a" => :yosemite
