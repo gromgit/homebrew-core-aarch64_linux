88e9948d4443ed1da82795f2aab074f04a9cad7a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 11:37:04 +0000

    gistit: update 0.1.3 bottle.

--- Formula/gistit.rb
+++ Formula/gistit.rb
@@ -8,6 +8,7 @@ class Gistit < Formula
 
   bottle do
     cellar :any
+    sha256 "decb56c455eb39e379b94b6832281ea06ae8a42c745eb6c108c15883a0ef2fad" => :high_sierra
     sha256 "269b7429070e11980d6764f2f6bd1d870d2e391cfd919f948159cb35cfab1184" => :sierra
     sha256 "b968a2885e3ac3e0c717fdabb9986149a39d58d031476d4ef3ada7a9e1ad07e9" => :el_capitan
     sha256 "052536cb990d1c5ea4b48612026dfb13addd03cbc06ed8c6b42f3636eb6771a7" => :yosemite
