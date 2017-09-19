daa9db86ff4ab8791b9593c8d1f1730e13b00a4c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 01:58:05 +0000

    bvi: update 1.4.0 bottle.

--- Formula/bvi.rb
+++ Formula/bvi.rb
@@ -5,6 +5,7 @@ class Bvi < Formula
   sha256 "015a3c2832c7c097d98a5527deef882119546287ba8f2a70c736227d764ef802"
 
   bottle do
+    sha256 "832a212daacc98df05797371ff7fb6ff0bbe5b7ac824a430c04b2b7f04d0e1e7" => :high_sierra
     sha256 "f918f3425865c4b8df97c32cf700f631d2c19e285adf01cc6ceadd429f7d0bfb" => :sierra
     sha256 "47c35b91db7052ee938daa6ddd95e96d49982e8767a48a0821f054b543fedb7f" => :el_capitan
     sha256 "4b5a3bdfa1bf083bde13338fc8fc5a8027880b3e79611ad064e44fd2f4d7a4a0" => :yosemite
