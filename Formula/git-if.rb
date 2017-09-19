d6904cf495b0f67872cff0c5f7c0a5e316071de7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 11:56:41 +0000

    git-if: update 1.3.5 bottle.

--- Formula/git-if.rb
+++ Formula/git-if.rb
@@ -8,6 +8,7 @@ class GitIf < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "bedbf580c8b073c7dfcd6bbb470aee7c14fb31d2c3ec54b4be8fd2cf8545e577" => :high_sierra
     sha256 "7c09116244a4c04a46337a0453d519523204233fc33d2d60c89c4b9469498380" => :sierra
     sha256 "c38c41e66ca16a1ef9627112980fa49411b870c2438086db35f6dd9053f99850" => :el_capitan
     sha256 "e727f112e350e8a12b87094715800e9c2abc03f2d45ad521c0d78e4c6bfff3ad" => :yosemite
