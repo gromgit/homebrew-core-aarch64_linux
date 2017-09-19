b425303942bad33590e17de2187b893f69c7ee0a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 09:43:39 +0000

    game-music-emu: update 0.6.1 bottle.

--- Formula/game-music-emu.rb
+++ Formula/game-music-emu.rb
@@ -7,6 +7,7 @@ class GameMusicEmu < Formula
 
   bottle do
     cellar :any
+    sha256 "5a5d6931476c23ab2542af2206e13486ada649b4e5a02ea9e07325bf9bfd64e1" => :high_sierra
     sha256 "2f6a1e59526c952ae8c231adcf71f928bec5c11d35760d4d7def6a3c2477339b" => :sierra
     sha256 "706653420e17c230ae1763da921c8ab08ace7f2db42dfbfd983746074e26b12d" => :el_capitan
     sha256 "b9bba2cd313e782b126cd934acfab5ee801b55d887d6360c6f36c9843dec1c41" => :yosemite
