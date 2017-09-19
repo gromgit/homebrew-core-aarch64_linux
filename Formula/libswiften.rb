b5b310df34ee7c9d712ab26ad174ad4a3b8814cc
ilovezfs <ilovezfs@icloud.com>
2017-09-19 15:03:16 -0700

    libswiften: fix build with SCons 3.0 and with Boost 1.65.1 (#18298)

--- Formula/libswiften.rb
+++ Formula/libswiften.rb
@@ -26,6 +26,17 @@ class Libswiften < Formula
   depends_on "lua" => :recommended
 
   def install
+    inreplace "Sluift/main.cpp", "#include <string>",
+                                 "#include <iostream>\n#include <string>"
+
+    inreplace "BuildTools/SCons/SConstruct",
+              /(\["BOOST_SIGNALS_NO_DEPRECATION_WARNING")\]/,
+              "\\1, \"__ASSERT_MACROS_DEFINE_VERSIONS_WITHOUT_UNDERSCORES=0\"]"
+
+    system "2to3", "--write", "--fix=print", "SConstruct",
+           "BuildTools/SCons/SConstruct", "3rdParty/LibIDN/SConscript",
+           "Slimber/SConscript", "Sluift/SConscript", "Swift/SConscript"
+
     boost = Formula["boost"]
     libidn = Formula["libidn"]
 
