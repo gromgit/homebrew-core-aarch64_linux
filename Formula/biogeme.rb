6db502b33054793e30ca3b1b4d46446c356af9b4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 07:36:33 +0000

    biogeme: update 2.6a bottle.

--- Formula/biogeme.rb
+++ Formula/biogeme.rb
@@ -5,6 +5,7 @@ class Biogeme < Formula
   sha256 "f6de0ea12f83ed183f31a41b9a56d1ec7226d2305549fb89ea7b1de8273ede49"
 
   bottle do
+    sha256 "235199118108d8ab8a023c6ec0abc276b2fcb840af5cb913511f9974c2342df5" => :high_sierra
     sha256 "eed003531786bc7e459b29b328ccf65b374a2e9aa23e190b694d2c13b8e44c43" => :sierra
     sha256 "9aa0816147a77c99af05717c7360b1a738f6b51d532a6e0908960ac49dc7940b" => :el_capitan
     sha256 "b6822b9c753fa818d79ba18857820e0a4c77944f47fd56b9dfcde444c73af6fe" => :yosemite
