3a8f31a59d88c684536c54769ec7792342614f73
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 19:27:11 +0000

    mgba: update 0.5.2_4 bottle.

--- Formula/mgba.rb
+++ Formula/mgba.rb
@@ -17,6 +17,7 @@ class Mgba < Formula
 
   bottle do
     cellar :any
+    sha256 "b1b8e1cc12513830ebba82ce391fb44984dcccd503608b8b0d214ab7c7e93c95" => :high_sierra
     sha256 "913b8ea3f4268206dc3a633189c61301d5aa69677e616d7ab331a22f2abfdb87" => :sierra
     sha256 "bc9f25939e113ff965b5ac747dd04ef9145679ddba246814215662bbcac7a50c" => :el_capitan
     sha256 "d8feb2b07ced1d12548512c02bb9b53b765a04557819b390c6e9578527460ae9" => :yosemite
