c65fa5379dd51f29afcc5cac74f081f2202b2b4c
Chris Long <christopherglong@mac.com>
2017-09-18 22:45:22 -0700

    coffeescript 2.0.0
    Closes #18248.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/coffeescript.rb
+++ Formula/coffeescript.rb
@@ -3,9 +3,9 @@ require "language/node"
 class Coffeescript < Formula
   desc "Unfancy JavaScript"
   homepage "http://coffeescript.org"
-  url "https://registry.npmjs.org/coffeescript/-/coffeescript-1.12.7.tgz"
-  sha256 "307640c6bf0d7ac51f6ba41fc329a88487d7d4be4cdadd33200ceaf4e6977992"
-  head "https://github.com/jashkenas/coffeescript.git", :branch => "2"
+  url "https://registry.npmjs.org/coffeescript/-/coffeescript-2.0.0.tgz"
+  sha256 "73a7dcf24ff8207f74182287b68a70ff3010fe8259819a6bd88626962036c14c"
+  head "https://github.com/jashkenas/coffeescript.git"
 
   bottle do
     cellar :any_skip_relocation
@@ -14,11 +14,6 @@ class Coffeescript < Formula
     sha256 "36501a7dc6e14ffca4009848e9aeee3958c1ed2f66cb2a8384a960c5cadeb0ef" => :sierra
     sha256 "7310cdbb6f272f1eb084a82540f09bf9eff97ebfa4a291b9bd5634b2045492cd" => :el_capitan
     sha256 "8516cacd1700b329b9610697926199f5200554b720d9f1c6a7b3419fb41ac7e8" => :yosemite
-  end
-
-  devel do
-    url "https://registry.npmjs.org/coffeescript/-/coffeescript-2.0.0-beta5.tgz"
-    sha256 "e8fbb09df9a70b39110393ab4013a67cb13176e1986bedef51b4f961ad61f66d"
   end
 
   depends_on "node"
