dc7596e1af5fc4fac80a5ea2ead52f77c707e439
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 09:43:39 +0000

    gammu: update 1.38.4 bottle.

--- Formula/gammu.rb
+++ Formula/gammu.rb
@@ -6,6 +6,7 @@ class Gammu < Formula
   head "https://github.com/gammu/gammu.git"
 
   bottle do
+    sha256 "0657669eda3e7bb1febb124ce89a7362b60d43afd5d88a5aa4143ae05b02667b" => :high_sierra
     sha256 "a25dd0c1236d5b26a214b9caa16e9963045b29a2e59391ef7993c2636656466f" => :sierra
     sha256 "e254b9cf36865dcdb99b5dc8701d052259bc8024b49f462cb805f58186b93fc9" => :el_capitan
     sha256 "e37cb111f1fe16aa168ac07d6515d2a59f1b02928ba19a2e123cc5b65c7b4fef" => :yosemite
