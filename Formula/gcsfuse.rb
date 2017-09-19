38c30f632586d50cad65ab5e253e7640212445f6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 09:58:18 +0000

    gcsfuse: update 0.23.0 bottle.

--- Formula/gcsfuse.rb
+++ Formula/gcsfuse.rb
@@ -7,6 +7,7 @@ class Gcsfuse < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4b0e7e4e145c199f18d21293e4002b8a6d3e626e5b5f330daee08ccfc3f1d7d9" => :high_sierra
     sha256 "9f42846472bbe6bc06b1016f6494415b16e148de6262fdcbb5bf9677fbee0a36" => :sierra
     sha256 "f4a837589708aed85da5762394004fa2398a2e907b37cb1c9554282578e635e7" => :el_capitan
     sha256 "4f1e5e0db02f9f148a61d9062626f754ba3f55dcf8df9ff69fc0f84cf5363577" => :yosemite
