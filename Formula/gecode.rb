303983bbc730115198ea20cfe983cda352bd666e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 10:55:28 +0000

    gecode: update 5.1.0 bottle.

--- Formula/gecode.rb
+++ Formula/gecode.rb
@@ -6,6 +6,7 @@ class Gecode < Formula
 
   bottle do
     cellar :any
+    sha256 "ee3fc9460300730c47ad2c966215aac05dfe0a9ee40e0d4f9f5dcb9b97cd31d1" => :high_sierra
     sha256 "a9a444e09a5afa31e6ec872b37603b1bb5463fd989c85d2971cf373363a9fc7b" => :sierra
     sha256 "a8b6584f71e730587cd3c4b4dac38afd9e5c3d25a26d1cb531a6c760344592ac" => :el_capitan
     sha256 "5288ebe7c86747c9b884a3139ecaf6bda4b9226ba930d7582ff6528ed1e63968" => :yosemite
