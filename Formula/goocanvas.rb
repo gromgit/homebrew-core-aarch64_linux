ca90cccf21dca730a65012852139e7609ebda583
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 13:33:25 +0000

    goocanvas: update 2.0.3 bottle.

--- Formula/goocanvas.rb
+++ Formula/goocanvas.rb
@@ -5,6 +5,7 @@ class Goocanvas < Formula
   sha256 "6b5b9c25d32c05b9bafc42f5fcc28d55f1426e733e78e9fe4d191cfcd666c800"
 
   bottle do
+    sha256 "7b687fd0c76c647ddde2cdab1977c86e6a5beb7881824f6474b2abdf9c35580e" => :high_sierra
     sha256 "ef387c378d0deff93c53b45c6910c537fa658ef0fd2bfb0b273fd5a7494d1725" => :sierra
     sha256 "d9e77f5e105c5aead29dd61f447dc2d805638ec1f3881923c3a8949c8c3de789" => :el_capitan
     sha256 "31ea24c91d6af157bdb47ab5d6fc7db1a3a2ce8c5fbfbb9fba21ce6fb9a1a13d" => :yosemite
