26804346e360fd24d6f1a9dfd9d26524c3a2947d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 10:27:45 +0000

    gauge: update 0.9.1 bottle.

--- Formula/gauge.rb
+++ Formula/gauge.rb
@@ -8,6 +8,7 @@ class Gauge < Formula
   head "https://github.com/getgauge/gauge.git"
 
   bottle do
+    sha256 "549961642f91a13ddeecb54e42fc29082a2caded0899db94322b94cf65044b56" => :high_sierra
     sha256 "4ca860842b71c958face93d07452cae6ccc220ec50914123e7775b77e6b286f6" => :sierra
     sha256 "0e7d6e4108eab026d9fcd9905fce65c159146bdf903f516c5499a2b52ede18d2" => :el_capitan
     sha256 "15cfbf4804217e796de1d7508bdcd28b54130b69e5fa791ddef9eb04f243c770" => :yosemite
