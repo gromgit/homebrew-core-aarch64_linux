84363304e13c98cc90d5ad185a880754f7c59900
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 15:41:31 +0000

    gwenhywfar: update 4.18.0 bottle.

--- Formula/gwenhywfar.rb
+++ Formula/gwenhywfar.rb
@@ -5,6 +5,7 @@ class Gwenhywfar < Formula
   sha256 "6915bba42d8b7f0213cee186a944296e5e5e97cdbde5b539a924261af03086ca"
 
   bottle do
+    sha256 "3a20a0d8d35bcc1fd39a1def1da4caa00a6ab57724b796cd7a45ddb5b6c9bc33" => :high_sierra
     sha256 "7eaedffb5b5dcee09131803d95d461f6cc87220ae3aa7294bc861da92d6870bd" => :sierra
     sha256 "e5510dc740fe9f2cf1f191c23d09ce413c7163d5ff9486b34b49d28d14f60bf0" => :el_capitan
     sha256 "534810125470167a96b204c418c170b4313fe653902a92908e5f1b619565e47e" => :yosemite
