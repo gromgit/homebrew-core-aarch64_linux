16dc51a2dcfa34e37799f15d37f1f87792d71ac4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 14:21:48 +0000

    gromacs: update 2016.3 bottle.

--- Formula/gromacs.rb
+++ Formula/gromacs.rb
@@ -5,6 +5,7 @@ class Gromacs < Formula
   sha256 "7bf00e74a9d38b7cef9356141d20e4ba9387289cbbfd4d11be479ef932d77d27"
 
   bottle do
+    sha256 "d3cb6d19f2f26d259bb717b3575cf514f7bade230531b11ec0d1d21b4d9dd676" => :high_sierra
     sha256 "90faa8b279f78823dae87bea50c3f1e88f9ede89678fb3f1711f643f576359a1" => :sierra
     sha256 "10f94061644f511939db8cc4d4c06ee767901c7aebd54c95872cc547b85826f7" => :el_capitan
     sha256 "3b93cbd26c93a4abb5401b7d87bfc8cb27d4b9641acecf47c26675f0fbd0f8e6" => :yosemite
