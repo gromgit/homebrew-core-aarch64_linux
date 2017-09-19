671afa9557f2cde5a8c376edd6b3cc34269315b9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 02:36:28 +0000

    libowfat: update 0.29 bottle.

--- Formula/libowfat.rb
+++ Formula/libowfat.rb
@@ -8,6 +8,7 @@ class Libowfat < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "4be1305b60bb8f5d3e283d58f3d8649b98289946c5abf55c42fb174c00ed5e8e" => :high_sierra
     sha256 "07d058c654a42d34821399ff800dfa22e928a64bcba4f465443197fe4168e97a" => :sierra
     sha256 "b8f9b6f14ef4ca9e703ef458659604738f2f6b11899fb9546d8764dde47de4c0" => :el_capitan
     sha256 "f84024088302c6452f32f2398d0c39a59ee7f2a2eda117e516608e0299763dda" => :yosemite
