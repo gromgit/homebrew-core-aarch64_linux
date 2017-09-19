7a83e6142c42896e717100dcbe1b2df7efa6f6b6
Alex Dunn <dunn.alex@gmail.com>
2017-09-19 09:12:03 -0700

    activemq-cpp 3.9.4
    Closes #18285.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/activemq-cpp.rb
+++ Formula/activemq-cpp.rb
@@ -1,8 +1,8 @@
 class ActivemqCpp < Formula
   desc "C++ API for message brokers such as Apache ActiveMQ"
   homepage "https://activemq.apache.org/cms/index.html"
-  url "https://www.apache.org/dyn/closer.cgi?path=activemq/activemq-cpp/3.9.3/activemq-cpp-library-3.9.3-src.tar.bz2"
-  sha256 "d7554c6245f7a5f96e8b9751a562f841ee285777487ccfbca1bfd74db024b50e"
+  url "https://www.apache.org/dyn/closer.cgi?path=activemq/activemq-cpp/3.9.4/activemq-cpp-library-3.9.4-src.tar.bz2"
+  sha256 "6505137fd4835a388b5ddecf6a96a62abd01b6d80f124e95dc2076127f4a84d3"
 
   bottle do
     cellar :any
