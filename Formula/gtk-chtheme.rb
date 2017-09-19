9d3ce4f1024f807bfeec24f037f4665fd1979e9f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 14:48:59 +0000

    gtk-chtheme: update 0.3.1_1 bottle.

--- Formula/gtk-chtheme.rb
+++ Formula/gtk-chtheme.rb
@@ -7,6 +7,7 @@ class GtkChtheme < Formula
 
   bottle do
     cellar :any
+    sha256 "0c228f7f5b8cfcc95556443db4acea3bd763b99a30a9108bd0eb68ad228838ca" => :high_sierra
     sha256 "913c9417ea21ecebdaeefb0329178d0304530310e4cfe64bf8831da5510bad4b" => :sierra
     sha256 "dc5fb21e189707e3bbc2bea4ac6e8d2091961bae4ea5c593ad2ff7272c5709e6" => :el_capitan
     sha256 "bdda9f20a50734e3ed0802fd12062160dfa378c47a09affb7a4716b892e70afe" => :yosemite
