6b971340a4ec2ae357779f4aa338a878a26875ac
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 06:30:17 +0000

    babeld: update 1.8.0 bottle.

--- Formula/babeld.rb
+++ Formula/babeld.rb
@@ -7,6 +7,7 @@ class Babeld < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "dab41722a62b7983230dfb88928bb438ff87080fe2ceed17c9ddb459b726052e" => :high_sierra
     sha256 "d384ced16e50c28eabac680e1b7f95b7863d6d6a8cced97e96c783b3a6860a23" => :sierra
     sha256 "52fe6dc33f81bce43054f00386b19a56f3bb9c153920dfba2236a239df84e5ab" => :el_capitan
     sha256 "be20af1a2505f188d4a5e32a3793542db187d9ec8ed85a33909616e59d775f4e" => :yosemite
