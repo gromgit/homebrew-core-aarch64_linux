49f9893f902e867e60f36a680e23e88ba5d15024
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 11:21:19 +0000

    get-flash-videos: update 1.25.94 bottle.

--- Formula/get-flash-videos.rb
+++ Formula/get-flash-videos.rb
@@ -6,6 +6,7 @@ class GetFlashVideos < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ef67bfb8dbf0a78c4e0df2976ceae3b85d4debafb191ffb4aa4677dee15db056" => :high_sierra
     sha256 "1bc1ddeba3234670bbbd03bdaae15e5014db0306edbf075914466db280c01904" => :sierra
     sha256 "311d82564584099f493d97597e1a2a7d7e611a939f47b9c7d40c78592be79d2c" => :el_capitan
     sha256 "f5493203c42ebcb77fed042135de07639c07b786bf344f336a24e428def4b4ad" => :yosemite
