f8bca79babe700e7d16853451eb0d2b52f64800f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 03:00:27 +0000

    librealsense: update 1.12.1 bottle.

--- Formula/librealsense.rb
+++ Formula/librealsense.rb
@@ -8,6 +8,7 @@ class Librealsense < Formula
 
   bottle do
     cellar :any
+    sha256 "6d7b2b1f63606818bdbc0f8e1db5b36b3add1e2171fada3c8abced7b5a606f37" => :high_sierra
     sha256 "3076d299219ddee4b4b11e68238f339fc6f1b85f2acc360e11d815b85ed6a577" => :sierra
     sha256 "ee7fe9916adcdca8fbea9a2bfc9f90662db0fbe8ec94c61ef6342eb2ec27775d" => :el_capitan
     sha256 "36faefdb0a151dfbd090ce80c4e0692bde78b9ee4a1cde7bdd8ab82f15f96dae" => :yosemite
