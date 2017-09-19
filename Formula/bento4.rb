3657ccfcd90d45332f48cf95f26e8b03e95ade33
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 06:49:18 +0000

    bento4: update 1.5.0-618 bottle.

--- Formula/bento4.rb
+++ Formula/bento4.rb
@@ -7,6 +7,7 @@ class Bento4 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "508efcd868dbca5fb8c0c097d13f20b468e2e85936e6148e66247b8ae283d01b" => :high_sierra
     sha256 "8bb6a41d88fa6c21e5d6ac86482aa232986cf7bd8298ec525e4640b5aab81cc7" => :sierra
     sha256 "c5e4cdb5623d116bb302f179040286337ba34a8a0a1906f6e26ee95aa57ee9ee" => :el_capitan
   end
