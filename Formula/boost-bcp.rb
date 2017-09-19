e1953fba50bec13c7f8db14a0130bcc9f7dc2adb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 08:06:00 +0000

    boost-bcp: update 1.65.1 bottle.

--- Formula/boost-bcp.rb
+++ Formula/boost-bcp.rb
@@ -7,6 +7,7 @@ class BoostBcp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "dd37ef056378e4628242b98e1b8ac8c0d867c9b99db357f463910009166c4ba3" => :high_sierra
     sha256 "2f9715e15a784c42c02d3e96bbebe5cab5bff01191e63db1649bb4cd529e7fdc" => :sierra
     sha256 "0479bb9b6002bf3c8f0e4063392f67850f34602826d2b3ffccf580509d48d37f" => :el_capitan
     sha256 "eb23497221bd792d1474b2c86e270a492266ea416e33c7bcfe678b4af0581eea" => :yosemite
