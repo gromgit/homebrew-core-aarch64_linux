b901ac0cc87658912de712626ef086a2169507f1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 16:34:51 +0000

    pre-commit: update 1.1.2 bottle.

--- Formula/pre-commit.rb
+++ Formula/pre-commit.rb
@@ -8,6 +8,7 @@ class PreCommit < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8372aa2238821aaac4af4d96fe98a09793837cf863b234c101a79c64da2f1908" => :high_sierra
     sha256 "e0dc2f37f3d969c171d64da46ceb30eb8868c215e2a74e780037c19592336dee" => :sierra
     sha256 "fce58b216c9329a96323c98567414cbffe4f3118e4e4a15a2614eb7c2192a9b1" => :el_capitan
   end
