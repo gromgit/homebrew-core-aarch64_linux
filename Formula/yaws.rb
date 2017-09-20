3243308743d0fb54f0518abf2ee67904caccf57a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 18:10:59 +0000

    yaws: update 2.0.4_1 bottle.

--- Formula/yaws.rb
+++ Formula/yaws.rb
@@ -7,6 +7,7 @@ class Yaws < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0c21fbdde094c31460c5046c1d0e0a0633c8ddb49d934b1eb8edae396a5e765e" => :high_sierra
     sha256 "9e3633fab3d158e738391c020fb018f5991d340c7cf02ec585a81dbdfe4b9a6e" => :sierra
     sha256 "80bddcf13c0dd84bbec08f407fe2093c3989d12764aa8ddc6ffd29e41dc1cb09" => :el_capitan
     sha256 "0c3befb6a035e66f74536cef3db652d653233670c57476220c2314af6cbcd484" => :yosemite
