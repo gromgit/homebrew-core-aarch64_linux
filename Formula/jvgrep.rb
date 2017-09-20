ce750a80943c2c10f97808c1c25c6e44c1f505b9
ilovezfs <ilovezfs@icloud.com>
2017-09-19 22:39:14 -0700

    jvgrep 5.4
    Closes #18328.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/jvgrep.rb
+++ Formula/jvgrep.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Jvgrep < Formula
   desc "Grep for Japanese users of Vim"
   homepage "https://github.com/mattn/jvgrep"
-  url "https://github.com/mattn/jvgrep/archive/v5.3.tar.gz"
-  sha256 "c4724296d35c13d950a7632ccd8d6c7583a38d98cdf2da6203a413175c044712"
+  url "https://github.com/mattn/jvgrep/archive/v5.4.tar.gz"
+  sha256 "42094336b769db7dbc731f0c146c606ee3f6435c038db53c9b67793edb3c5be0"
   head "https://github.com/mattn/jvgrep.git"
 
   bottle do
@@ -18,7 +18,7 @@ class Jvgrep < Formula
 
   go_resource "github.com/mattn/go-colorable" do
     url "https://github.com/mattn/go-colorable.git",
-        :revision => "ded68f7a9561c023e790de24279db7ebf473ea80"
+        :revision => "ad5389df28cdac544c99bd7b9161a0b5b6ca9d1b"
   end
 
   go_resource "github.com/mattn/go-isatty" do
@@ -28,12 +28,12 @@ class Jvgrep < Formula
 
   go_resource "golang.org/x/net" do
     url "https://go.googlesource.com/net.git",
-        :revision => "59a0b19b5533c7977ddeb86b017bf507ed407b12"
+        :revision => "8351a756f30f1297fe94bbf4b767ec589c6ea6d0"
   end
 
   go_resource "golang.org/x/text" do
     url "https://go.googlesource.com/text.git",
-        :revision => "ccbd3f7822129ff389f8ca4858a9b9d4d910531c"
+        :revision => "1cbadb444a806fd9430d14ad08967ed91da4fa0a"
   end
 
   def install
