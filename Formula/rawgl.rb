2e3071b1319cb61f7d0761a84a6c5fd57b4bdcf9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 15:01:51 +0000

    rawgl: update 0.2.1 bottle.

--- Formula/rawgl.rb
+++ Formula/rawgl.rb
@@ -7,6 +7,7 @@ class Rawgl < Formula
 
   bottle do
     cellar :any
+    sha256 "97ce691b12b91ba6f948168e993fa04ec6ea18aad89d60dd40754d2554649ef0" => :high_sierra
     sha256 "73791cd66afb8da8c36bfcec4ffda39fbd4db8bf94e5112dff8bda46462c3365" => :sierra
     sha256 "83711f3b4e919fbe21d02221356ad3e0108ee00739c699eb1acf79f2be8b6b18" => :el_capitan
     sha256 "b453fd2cf86a0299e372222942500f0b816165218ce0dec1726e0be493cf77c9" => :yosemite
