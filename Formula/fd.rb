9a9c46d96858432bbdefb38161e089f217ccb51f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 18:52:38 +0000

    fd: update 3.1.0 bottle.

--- Formula/fd.rb
+++ Formula/fd.rb
@@ -6,6 +6,7 @@ class Fd < Formula
   head "https://github.com/sharkdp/fd.git"
 
   bottle do
+    sha256 "26783ca7907c72f893fa988ec5b78d21c270a4a11b309e16357cbe21299e9b3a" => :high_sierra
     sha256 "fb3731f970bc97458c723700f8f7da1f47b259ef96ae85e2160256c463391742" => :sierra
     sha256 "94827a4f6413385ae1fe88bb9858c8fefaf050d161b95e37f765578784582133" => :el_capitan
   end
