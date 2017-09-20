16ca414276ae90f2d2d417cf54db0d6c4faebae7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 09:07:01 +0000

    dfmt: update 0.5.0 bottle.

--- Formula/dfmt.rb
+++ Formula/dfmt.rb
@@ -8,6 +8,7 @@ class Dfmt < Formula
   head "https://github.com/dlang-community/dfmt.git", :shallow => false
 
   bottle do
+    sha256 "ccb9816bf837e999563e470b020b9d8b1305b2dcf2c9c8400276bf0ca766d94f" => :high_sierra
     sha256 "08ce677f6ae697ea33e29d4ecff0e419cbe71b065b8a24cb13bc65a4c5834b40" => :sierra
     sha256 "745cc85d47967fd74ad25a08dd763f028440a2c5811730c2b2c6643d7f5236b6" => :el_capitan
     sha256 "66fe1b25802b529a08f7a46e0b043fdf06e64c9fe1a48dbc04293954187b65a1" => :yosemite
