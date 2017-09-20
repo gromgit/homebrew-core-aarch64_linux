3b970420d913b184a9a6137ef09e48ade7bcf669
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 15:22:17 +0000

    rem: update 20150618 bottle.

--- Formula/rem.rb
+++ Formula/rem.rb
@@ -6,6 +6,7 @@ class Rem < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0a3365c8653023f2b4de8c5b6243aec2de7c180d1be982adcdbe58afc159800e" => :high_sierra
     sha256 "326f7a21f696b7614a55a5edeb57e08482ff7b4c72506bcecff5deaa0552828e" => :sierra
     sha256 "c9892df4f6aa5d58097e4cc4d62388ccbb1e0c02604b1139cfe829d47d992442" => :el_capitan
     sha256 "d9a6303ff3935923ba53d093e95387caaf24460a4cd7fb7d330fa5c3988b551c" => :yosemite
