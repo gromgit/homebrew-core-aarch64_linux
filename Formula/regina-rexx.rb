ca4380823a4ac69230c28f99401a1e42a31dd0dd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 15:22:17 +0000

    regina-rexx: update 3.9.1 bottle.

--- Formula/regina-rexx.rb
+++ Formula/regina-rexx.rb
@@ -5,6 +5,7 @@ class ReginaRexx < Formula
   sha256 "5d13df26987e27f25e7779a2efa87a5775213beeda449a9efac59b57a5d5f3ee"
 
   bottle do
+    sha256 "26829912ce6476406e696694d27d1f758bcf3c43841969bc4f4ba4587e1fc2f4" => :high_sierra
     sha256 "9380b7c431fc2bc4cee9f3dc4997c9f7e568bc7884f3dc9aade5c2c73d805dbe" => :sierra
     sha256 "99ea4d0288bda8704830d1c1730d63e27d68a5d4e5c0c2fe2b078ba4c8e39b63" => :el_capitan
     sha256 "4be2121b50a9d988ad30b592c3123ea913a26082a2b1be7ca3ee83ae75b944d3" => :yosemite
