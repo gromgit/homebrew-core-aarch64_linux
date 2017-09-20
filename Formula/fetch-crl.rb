61d2d94c6a7cfe7b4a948bccfa0a8de7099a9f86
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 19:04:06 +0000

    fetch-crl: update 3.0.19 bottle.

--- Formula/fetch-crl.rb
+++ Formula/fetch-crl.rb
@@ -7,6 +7,7 @@ class FetchCrl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0e83737b69bacb9920d6c0543c54b87999372722c45426a5003746eb9c8f3434" => :high_sierra
     sha256 "d8907b1d805f5bf782f4b9fbccb225b66104631269f0d4b69488518b12c919f2" => :sierra
     sha256 "66c8bbf8a0971d925e3b92b4f3dee740ebee385aa9f7b76974b62d1eaa9e7672" => :el_capitan
     sha256 "66c8bbf8a0971d925e3b92b4f3dee740ebee385aa9f7b76974b62d1eaa9e7672" => :yosemite
