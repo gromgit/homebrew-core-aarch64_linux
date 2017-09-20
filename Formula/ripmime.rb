1e09c1d3076c286793455cd43adeabd207584a38
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 16:03:35 +0000

    ripmime: update 1.4.0.10 bottle.

--- Formula/ripmime.rb
+++ Formula/ripmime.rb
@@ -6,6 +6,7 @@ class Ripmime < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "915cd6326fe857e0608d25c9b6e2f4fab06734df23d0ad938184c1b791981345" => :high_sierra
     sha256 "09a2b60d927bbc236998e29ea50969ce95ab4470d74cd7a40a54f9f4ec24252b" => :sierra
     sha256 "1151fa0bb8a10779979cec95c7039832eb81b7126f808ba9c89ccb73cf658814" => :el_capitan
     sha256 "6ef2fdabe468bc42be725020ef23cc924d1572c7446648e38dbd6de3f1399a38" => :yosemite
