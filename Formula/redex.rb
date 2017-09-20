b9b4fe654f8bd7b9f8b69c2372c105817974cb51
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 14:46:16 +0000

    redex: update 1.1.0_1 bottle.

--- Formula/redex.rb
+++ Formula/redex.rb
@@ -8,6 +8,7 @@ class Redex < Formula
 
   bottle do
     cellar :any
+    sha256 "c66e4160ebdcad99f1854a99fa6fefc8a8da2f1bc62d808cc1aa79cd9e2c2bed" => :high_sierra
     sha256 "0486e6105e25f310e802b4adc0f6e50ad2fcb02c4b1d4edc1f85d16963892b86" => :sierra
     sha256 "af389493d114dac27c04ec780d16c4865591c1fa5e611e9b84f94f0562b22078" => :el_capitan
     sha256 "3c8b98773de86c4025b3f2be18a3666987b5e06b958f1bf03c64ab2e3193c8e7" => :yosemite
