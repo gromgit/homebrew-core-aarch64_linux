d5c722007a3188e1a027e1a75961c242fc60d6dd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 19:55:08 +0000

    flake8: update 3.4.1 bottle.

--- Formula/flake8.rb
+++ Formula/flake8.rb
@@ -10,6 +10,7 @@ class Flake8 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5183704dc1be1eb7b69a799332701b3537dcd344cc1da5efb1a8c6d10c4f1d23" => :high_sierra
     sha256 "52247f453420402adf92473646822df170eba8988869408536758a9dca3cf02a" => :sierra
     sha256 "7cedbefbd6364386eaed992f81cfeb1dfca08997bda82e59d726a247d6fd6dea" => :el_capitan
     sha256 "867901db2671873cfdbaec243d6a7e13f8fcc9e61b3b7f6353b7d9142c52c874" => :yosemite
