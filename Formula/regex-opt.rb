4c4500fbdafff67e44ca9ee17628bd62de034f50
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 15:22:18 +0000

    regex-opt: update 1.2.3 bottle.

--- Formula/regex-opt.rb
+++ Formula/regex-opt.rb
@@ -6,6 +6,7 @@ class RegexOpt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "be7eb7a1ab846967ae0185c1d998c87d1699b95407c34e6d7d5222912fb8faad" => :high_sierra
     sha256 "6450d5a7ab5cc7a19e14753aa797e3044b7f0f29dba2cc22c4db32c2706d439d" => :sierra
     sha256 "3f9bdf79820441d3a0941a8cb17b9d2c4f512b9e0fbdc36560681c3534e3615a" => :el_capitan
     sha256 "01d8af104a70bc0bcf9c9fb1b93b0e4f3a7d08aaadd367e34ec223c38a00d58c" => :yosemite
