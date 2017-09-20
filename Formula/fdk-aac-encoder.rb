7f1d77d041b11b17334a129aaaa1885428eb7741
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 18:52:38 +0000

    fdk-aac-encoder: update 0.6.3 bottle.

--- Formula/fdk-aac-encoder.rb
+++ Formula/fdk-aac-encoder.rb
@@ -6,6 +6,7 @@ class FdkAacEncoder < Formula
 
   bottle do
     cellar :any
+    sha256 "40b98a23002f12df182d0bc814aa11ce6b9ef3fea9bf092e02f5dd5a437f873d" => :high_sierra
     sha256 "786e3ce4a555ea473a8915df451f51ad81f5c27749c29b1e87e64d80c799588f" => :sierra
     sha256 "4a157e1d1f0f69db4c8070dd0f9c48e5099710aa516c41a3db83c25a0c84adb6" => :el_capitan
     sha256 "03fbc5fed2792b358a9bf8e777678bbfca219cb8c9e969b6e3a6c5292de7b65f" => :yosemite
