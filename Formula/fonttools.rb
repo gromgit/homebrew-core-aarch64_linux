356f17ee3fdb0528a30ded4eb644391c97e808ca
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 20:38:50 +0000

    fonttools: update 3.15.1 bottle.

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -9,6 +9,7 @@ class Fonttools < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "88894d8e0b3cd90e0af41c278b72f4064112849229bb1f1b3bdef479eb83e5b2" => :high_sierra
     sha256 "21823f14b416bbdb52733f62b8e6365a70222704c01deb041563d4e9e25199fa" => :sierra
     sha256 "5adcf8fd9bb7f3927c864df2f8046199d8d8b93f041882635a7e50275db7bdb1" => :el_capitan
     sha256 "d0e924db392775237fcfcc80f943a2e6d792876f981dd3907ef76c9f00862b36" => :yosemite
