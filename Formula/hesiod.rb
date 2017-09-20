252bf6ef4fb747fe312c825140efdfadd9e1c762
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 01:09:27 +0000

    hesiod: update 3.2.1 bottle.

--- Formula/hesiod.rb
+++ Formula/hesiod.rb
@@ -5,6 +5,7 @@ class Hesiod < Formula
   sha256 "813ccb091ad15d516a323bb8c7693597eec2ef616f36b73a8db78ff0b856ad63"
 
   bottle do
+    sha256 "efab462c4b62f2da9d370d5d457a56082ae15a5b1e54bc0a91b2f5e822f33a3c" => :high_sierra
     sha256 "6b12ee90c4f50d02503a44b68b6b4c95097b299aefc7fbfa68872925a5ca48d5" => :sierra
     sha256 "1fd5997be54205d6b7825e156eba9ffae51bdd9e651fd3bb269f9f696d95071b" => :el_capitan
     sha256 "d8db2bc394437272bfcf0225f435075f71269ee9e41b6e453514463e57501348" => :yosemite
