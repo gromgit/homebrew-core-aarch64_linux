680cc4bf9470624cbd4b6f898e11e23a0241521d
Kaz Sera <seratch@gmail.com>
2017-09-20 15:42:46 +0900

    skinny 2.5.0 (#18324)

--- Formula/skinny.rb
+++ Formula/skinny.rb
@@ -1,8 +1,8 @@
 class Skinny < Formula
   desc "Full-stack web app framework in Scala"
   homepage "http://skinny-framework.org/"
-  url "https://github.com/skinny-framework/skinny-framework/releases/download/2.4.0/skinny-2.4.0.tar.gz"
-  sha256 "8ee02680f876d603cde2c87057d83e13647eb118ae1c19498ff6e95ce2526051"
+  url "https://github.com/skinny-framework/skinny-framework/releases/download/2.5.0/skinny-2.5.0.tar.gz"
+  sha256 "1319d98e6ef02d58e63fdf14f37985ba672b4bcb9a672dee379bc6b51e1d1b37"
 
   bottle :unneeded
   depends_on :java => "1.8+"
