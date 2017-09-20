9d55741f0739de9cfbee347441ead40c189b88d0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 23:54:53 +0000

    scw: update 1.14 bottle.

--- Formula/scw.rb
+++ Formula/scw.rb
@@ -7,6 +7,7 @@ class Scw < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7bab9dbbedcaf00fad74ee52d3ac7dcdd825d28018a9356a6dfe94392cd06ecf" => :high_sierra
     sha256 "c2bc2d7b6576719ebed7cd961795e65bea23fc5375b8232643556e9d12b67377" => :sierra
     sha256 "f72df0768951c24d5f8b53d7e92715205ac3aa207c9ef7731bd3979ac2525be1" => :el_capitan
     sha256 "460c30f2ea93e5ffe5759ed1e20711f1914e31d3d0e25e741434bdf8de1df904" => :yosemite
