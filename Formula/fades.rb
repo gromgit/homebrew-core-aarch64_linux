6ce37dec15f50e9ce3a37e82ef2333b042672516
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 18:27:47 +0000

    fades: update 6.0.1 bottle.

--- Formula/fades.rb
+++ Formula/fades.rb
@@ -7,6 +7,7 @@ class Fades < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a253b997f7e16ad61f602449567b1883a9ccd742665b1ff67dce545f6cae52d1" => :high_sierra
     sha256 "c24a39184e8fb1c487e6c579b3fd66aba3e6c13be4f86c5583ce59beaab99655" => :sierra
     sha256 "8a6869cbb401853eff4aa5c176feebdcb0b493ec09052f9f7a516d87320d96f3" => :el_capitan
     sha256 "8a6869cbb401853eff4aa5c176feebdcb0b493ec09052f9f7a516d87320d96f3" => :yosemite
