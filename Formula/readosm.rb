ea053cd821565d921abbf27e3a6ea638e849d4e1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 14:46:17 +0000

    readosm: update 1.0.0e bottle.

--- Formula/readosm.rb
+++ Formula/readosm.rb
@@ -7,6 +7,7 @@ class Readosm < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "598852e6e9ce971d4f9280d91da37e69e416616a78a5322fa12f8e1455904891" => :high_sierra
     sha256 "1c05e54fa7f490244443566a5d705152575511de1dfb7b8acf437b8483bedb79" => :sierra
     sha256 "039504a7e57854871753465a0a0a99c0ed12a3001c63a8cb8b075df7c9646778" => :el_capitan
     sha256 "36f2069bbc99e2cdd86749bfb2b6dee52ababbadd1d3b78f150787747e79578f" => :yosemite
