1dc56ab2af509d5dbb7b37afae6a0bb9d46371ac
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 20:14:22 +0000

    flvstreamer: update 2.1c1 bottle.

--- Formula/flvstreamer.rb
+++ Formula/flvstreamer.rb
@@ -7,6 +7,7 @@ class Flvstreamer < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "52d09a95883b401b1d77d0e85354099cc351285a2243d00c257778033f36dbf6" => :high_sierra
     sha256 "e257779383d236611212078f2e6db28d457d51a282d6163806e0232134a046b0" => :sierra
     sha256 "5a4b649ce0f2c32bca4091f4867a37cca0e8ae2a292d4ef29aa2949530bdd651" => :el_capitan
     sha256 "243e6ce44b77212ff84e3a739bf2b203c687bdcdd36b17ba24daa5335bf0a151" => :yosemite
