e8ded687dd00f870f4e8a1625d96e878c7e9401d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 18:08:47 +0000

    yetris: update 2.3.0 bottle.

--- Formula/yetris.rb
+++ Formula/yetris.rb
@@ -6,6 +6,7 @@ class Yetris < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "21537f5957c5ce90281195e6d962363920bda756a6c965ca107c329ec712f126" => :high_sierra
     sha256 "cf350d8daaf62f863b7466477aebea02145abf1f14e50ee56ad324c99dcee018" => :sierra
     sha256 "fd08bc62fc0c4687ed7e76fe604c345a647fb52a348c55cf446fcbf52c7af8dd" => :el_capitan
     sha256 "a14c5327ab931d7394b3f617422916eafbc76a936ac77e81a959b38aa223dd5e" => :yosemite
