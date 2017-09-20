09e9455f30a8b73349df391f1dfce354dafe2482
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 18:10:58 +0000

    yaml-cpp: update 0.5.3 bottle.

--- Formula/yaml-cpp.rb
+++ Formula/yaml-cpp.rb
@@ -6,6 +6,7 @@ class YamlCpp < Formula
 
   bottle do
     cellar :any
+    sha256 "f4e9ed4651906c28464baf45d3a460502a24b42add49b35e969141818348680c" => :high_sierra
     sha256 "fe1fca40afa3817fd44b1f3c4810dce9f4d20390522526e72d775b9336fc3c3a" => :sierra
     sha256 "20b38c2f7c47550b458cb5b6f054795d90f4955d2525656e6d713cdbe7d451b1" => :el_capitan
     sha256 "e0a51ff0b33568695412fe43cdc51b26642ae46dfb6dac56b813295057c91bb6" => :yosemite
