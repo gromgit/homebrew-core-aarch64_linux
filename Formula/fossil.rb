db1bf856df58f14db2818bca8751240eddc91889
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 20:38:49 +0000

    fossil: update 2.3 bottle.

--- Formula/fossil.rb
+++ Formula/fossil.rb
@@ -8,6 +8,7 @@ class Fossil < Formula
 
   bottle do
     cellar :any
+    sha256 "bc988651d4ed64e8fa55fdafb9c1c9a8fb442bc14559e8ff3c2b42ac0e816fef" => :high_sierra
     sha256 "523704982ce10097cdf1a43d77a920c0776369a6e33757ec9ac94aa708e1b784" => :sierra
     sha256 "28b64773ea5f1cd4ebaa51ce65961e205a5b919d521d1fe473c620fa96ce757b" => :el_capitan
     sha256 "6d422e94e2ec8bf0ae41c387cf37377ca22d8523638b4613938403e912e7ea3e" => :yosemite
