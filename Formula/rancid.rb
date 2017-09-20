424e952270092fb3018fc8fb7987006fcb6fcd05
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 15:01:51 +0000

    rancid: update 3.6.2 bottle.

--- Formula/rancid.rb
+++ Formula/rancid.rb
@@ -7,6 +7,7 @@ class Rancid < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b016dd160445f121a4404cc7f9556b173b580d88db53799f720ae27088ac35c2" => :high_sierra
     sha256 "a60d7c30da581df22d0950a56a040ff4b88385bdf411d0d5a215791229e830aa" => :sierra
     sha256 "f889063d5ec93094647aad6ef6f0958dc730ce23ec72be6826ac121471bb75d7" => :el_capitan
     sha256 "07fb9fed57cbb0c6ee07d9bf12796ceb4588a6990d68648c854047f815693b22" => :yosemite
