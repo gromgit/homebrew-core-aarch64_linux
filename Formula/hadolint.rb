b751df4e26463b9942dd8fae1abd3bc144e63ad2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 01:09:28 +0000

    hadolint: update 1.2.2 bottle.

--- Formula/hadolint.rb
+++ Formula/hadolint.rb
@@ -10,6 +10,7 @@ class Hadolint < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d4c3729595b2346d96a1a02c19a90a47eb86f9dc4a0dceb7f7dce9abf1f266cb" => :high_sierra
     sha256 "e50b8e3ecbaa931e47a6eef649c041af70569f3812433707e22502cfe281c186" => :sierra
     sha256 "8e2cf9aa35ef51c0ffe475af366a97149b15a64558c97445e34574f6a66ce43d" => :el_capitan
     sha256 "c079436775b7811e6e3b566fa040d9c39580c31e9362fad7386f2527212fde10" => :yosemite
