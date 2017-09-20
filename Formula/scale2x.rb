6c1c6b3086a348a3321d311640382c64dad02122
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 22:53:43 +0000

    scale2x: update 3.1 bottle.

--- Formula/scale2x.rb
+++ Formula/scale2x.rb
@@ -6,6 +6,7 @@ class Scale2x < Formula
 
   bottle do
     cellar :any
+    sha256 "fd683dea067c68253aa4fe198438fc19f023bca3a368931bfebbbabb5032fd99" => :high_sierra
     sha256 "8699587cf81a5d38985d81e32bb7b7815cda1d731a5f134d045692b5276e0a12" => :sierra
     sha256 "ad49eb9e6167e9ad5d5ea4f08a870b06ee9118465668762ac05a8b50cdbd7ebf" => :el_capitan
     sha256 "c431a4d6ccc671f7d010fc1d010d6f31372c2075942ffc402f7969f7b0c7b43a" => :yosemite
