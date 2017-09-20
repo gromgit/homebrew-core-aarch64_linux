dbbe8513a23ed64cbbc6cda02958480d4c785fed
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 17:37:58 +0000

    rtmidi: update 3.0.0 bottle.

--- Formula/rtmidi.rb
+++ Formula/rtmidi.rb
@@ -6,6 +6,7 @@ class Rtmidi < Formula
 
   bottle do
     cellar :any
+    sha256 "f4a74942bb3c18351c0ff2d77a27703d70aa3311f3a39228a7615c424e9fac79" => :high_sierra
     sha256 "27e811c4d1ad96adbc31f2f432543b85907a065a91cc0a4e515e980a2f45b253" => :sierra
     sha256 "387b587f77423ee2ecfd716fa5235aa235ba5d33632b2765292ec318e0d67af3" => :el_capitan
     sha256 "6e2f97819b113f8c95618377030170ddba66e79085f4f1553306b77e99683c6c" => :yosemite
