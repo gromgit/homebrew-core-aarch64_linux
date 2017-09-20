4037d045834815b9292d9ca3e86bd25460030688
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 20:46:57 +0000

    foremost: update 1.5.7 bottle.

--- Formula/foremost.rb
+++ Formula/foremost.rb
@@ -5,11 +5,9 @@ class Foremost < Formula
   sha256 "502054ef212e3d90b292e99c7f7ac91f89f024720cd5a7e7680c3d1901ef5f34"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "35b0f207c144db1493182ea448b4458b179971b024b00fd1d7611173b5656cad" => :sierra
-    sha256 "153967284382e4a8206a627ac415c7bf39732279c3e1dc1bf377edcf0f2939b2" => :el_capitan
-    sha256 "7b793ac9f697b1aa9da79a04b7394ce93909149dae5f2817c49998798e50f939" => :yosemite
-    sha256 "9ba1e4692226a7d1941c18b4317758393d806c95a4ec8b8500010f920ff13247" => :mavericks
+    rebuild 1
+    sha256 "1fec9cd62de192dcbd5217cfa2c6c1c1ca056afa85a434114748171bf740ed96" => :sierra
+    sha256 "73f4f781edd9621d498238d77536dda5fbfa60dbb613bd7f15bb31d72699c986" => :el_capitan
   end
 
   def install
