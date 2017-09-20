cf19e190b69bcf9e4cc45ec83aa3b1d1cc697e09
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 18:10:59 +0000

    yank: update 0.8.3 bottle.

--- Formula/yank.rb
+++ Formula/yank.rb
@@ -6,6 +6,7 @@ class Yank < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "aa67d326b2059610675288a2ff20edb1f156327f8c4399e0d725fed6bb3962f1" => :high_sierra
     sha256 "07b4356f309e74541d37da6c1a619e836e7743ad206e38d4e1ce66204ca03ce1" => :sierra
     sha256 "3c5ebdc4717d374aa9775c137129463fea9d255080e6a90d1380443c50cf192e" => :el_capitan
     sha256 "eafe51016b3b0b08f5af4db3f9a143ec291cdeb1f8dca59285ea90d5b1fb1101" => :yosemite
