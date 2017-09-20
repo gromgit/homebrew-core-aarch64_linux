d94b88715b6c6cbe084bfb56c2e453f6763e66a6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 19:04:06 +0000

    ffe: update 0.3.7-1 bottle.

--- Formula/ffe.rb
+++ Formula/ffe.rb
@@ -6,6 +6,7 @@ class Ffe < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ccecbf9c6595344a6988ed708a38bb18d8d754d5e948e9e9a0d8fd2b933e1eec" => :high_sierra
     sha256 "9471c2e19149beb552d0f7b724b6f6f140b2f2dd26d180f8293d6e95a704907f" => :sierra
     sha256 "a85474d61e83b870dd30b1df63b16eb28cc87f18c74d4484e49f5719b2ebad43" => :el_capitan
     sha256 "df84e69d719b76fd4c9dd6d7462c2dabcad331454dd20f64ba55ab0a0a7ba03d" => :yosemite
