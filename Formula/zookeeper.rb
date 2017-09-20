c23168c608cdcaa7f48b9b61fb61fe9491e5bbac
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 01:59:27 +0000

    zookeeper: update 3.4.10 bottle.

--- Formula/zookeeper.rb
+++ Formula/zookeeper.rb
@@ -8,6 +8,7 @@ class Zookeeper < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "08431d9c2f04f5a735149f374975df4f2e0d8140b1cc901f505d379ef7e20fe0" => :high_sierra
     sha256 "8c7304be183d4c28c0f5d88e22626188c76794bee004e1b330c3e79bf5ae9d53" => :sierra
     sha256 "2d792cb3963a7caf57922635888ae4e3cf363ef5a81d23f13a11d0ee42a780cf" => :el_capitan
   end
