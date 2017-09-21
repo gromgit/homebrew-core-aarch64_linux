5317e05317c3c3a3884a67e37cc6054b0f5af797
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 03:45:46 +0000

    somagic-tools: update 1.1 bottle.

--- Formula/somagic-tools.rb
+++ Formula/somagic-tools.rb
@@ -6,6 +6,7 @@ class SomagicTools < Formula
 
   bottle do
     cellar :any
+    sha256 "121e3b6667ee8dcd81cf2331342d27b6221b1ebf955f83e00311176fa5fe11ca" => :high_sierra
     sha256 "b0fa394d0211f43fe5c9da6e7f36b8e3b6ed5086b8a447b06df42e21bf0e30cd" => :sierra
     sha256 "b73262d08d3ec9e10645290555b5fb0c5fd95492c9d5db2ab451285ccb69eac6" => :el_capitan
     sha256 "0b0b6840133039a9f7c33579d45fbd93e68dc00e6eabe0bd4d36d7d4da56fc06" => :yosemite
