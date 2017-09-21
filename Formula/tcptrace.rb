e560a64dff126d32b48de9a6a3279c2853ef160a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 16:57:26 +0000

    tcptrace: update 6.6.7 bottle.

--- Formula/tcptrace.rb
+++ Formula/tcptrace.rb
@@ -7,6 +7,7 @@ class Tcptrace < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "39916506fcd6385aee6375813128a126a84f947623594011f6c2c9df1b6dc8b2" => :high_sierra
     sha256 "7ccc5e6859be970a5a8a064630704111d37b03a7e3cf3a9874e16a60e4abe02b" => :sierra
     sha256 "e46775d7cc808b5b52a0a36a33142b824a9b2d8bce5b0557bc1041c2e55c5ffb" => :el_capitan
     sha256 "f9de7ef41a2b9dc8daee1fddef1035ddf6a08cf473b6edafcf4bb069ab5f0052" => :yosemite
