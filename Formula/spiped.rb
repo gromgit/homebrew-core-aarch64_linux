22850e114be9ed40854f433a8f70a698ebfdeeb7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 01:18:14 +0000

    spiped: update 1.6.0 bottle.

--- Formula/spiped.rb
+++ Formula/spiped.rb
@@ -6,6 +6,7 @@ class Spiped < Formula
 
   bottle do
     cellar :any
+    sha256 "f2008c86b377357db0888b384029b54ee9573d73e891ec322018b80dc499d165" => :high_sierra
     sha256 "53e5ec39c188fed1d8a762904988490dd6a73f34f6bfcef986063a34a804cd6a" => :sierra
     sha256 "3597aef864440d96505a2445348060926cbab9b2ea44af4ee61094e4bc419d4e" => :el_capitan
     sha256 "d61db0a6cf96273e9564fcd3208ff5836840b0369e0a9c269c79904d9f1d3ab6" => :yosemite
