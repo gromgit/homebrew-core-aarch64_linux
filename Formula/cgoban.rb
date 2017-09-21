cfa2efd2e6360af23c547065d440cc9056395a28
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 14:53:47 +0000

    cgoban: update 1.9.12 bottle.

--- Formula/cgoban.rb
+++ Formula/cgoban.rb
@@ -6,6 +6,7 @@ class Cgoban < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "14efcd85d7d9f5a15fe0693eb8ebd4b1ee8b49fb7604681be91c14964af0cee3" => :high_sierra
     sha256 "4f88f760ce464806c607e9a29da5f701cc6d395f27110cd866579fdf8737931a" => :sierra
     sha256 "f6f9b32dddefb3474dca91d55c0aecdacec7e3b7ccbb2cb8c9b151b41e16f4d0" => :el_capitan
     sha256 "65b5a814e1b4e3c115c1fb873d8a740ee4c0b39eff33331336d9bfac073e1a27" => :yosemite
