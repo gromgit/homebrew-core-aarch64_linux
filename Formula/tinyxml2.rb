0b3ee2f67043af1b4270096c13350420e3290399
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 18:17:20 +0000

    tinyxml2: update 5.0.1 bottle.

--- Formula/tinyxml2.rb
+++ Formula/tinyxml2.rb
@@ -7,6 +7,7 @@ class Tinyxml2 < Formula
 
   bottle do
     cellar :any
+    sha256 "53192ab7ffcdcbb79b4d2ce4278ebf062395392b0397a857fe5ba212e41a7317" => :high_sierra
     sha256 "b570b43054e9a1e7a0bf9ada534d8d823b04fb944011a94e31d1e984fe615999" => :sierra
     sha256 "36a3d12afbb44d6b9c9d5c2fb36499e9a5d56b46c7c72141d78e2aa9041d282a" => :el_capitan
     sha256 "fe1653e341e976a0d226cab0842035703b369caa7504b069ffe26b7717f85d98" => :yosemite
