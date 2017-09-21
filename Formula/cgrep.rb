1c649bec90549de69220b69229676ead2ccd33ff
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 14:53:46 +0000

    cgrep: update 6.6.18 bottle.

--- Formula/cgrep.rb
+++ Formula/cgrep.rb
@@ -11,6 +11,7 @@ class Cgrep < Formula
 
   bottle do
     cellar :any
+    sha256 "ad692cea91721d3a6790393d4abf7d5b1baf7d5836619df9ad306a9aaa95ca82" => :high_sierra
     sha256 "ba15c5f7e86bbb71aac18461e2865413cfd90d3794f7f3e53ab241eacb2ddcbb" => :sierra
     sha256 "3427acebbfe2e88072d227cc565b4d577c6900b2694bf39b01a985dbd15f41db" => :el_capitan
   end
