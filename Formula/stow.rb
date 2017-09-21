8c36ccd486800503c03a425ed95157fbe56c783a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 04:13:18 +0000

    stow: update 2.2.2 bottle.

--- Formula/stow.rb
+++ Formula/stow.rb
@@ -7,6 +7,7 @@ class Stow < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e530cfa129ccce32a3a593c9ab09ea5c2976bef981d8ea3dc5af9f65e301e78a" => :high_sierra
     sha256 "93dc759da363883c6cbf581f957ed0c23abac5563de1680cb11721adc425dedc" => :sierra
     sha256 "0bf0a55711d83b08953a2de183d20cec481029dc95e9ea2ebd5049bf13ea8e1b" => :el_capitan
     sha256 "4062d438086c6f9e407db818d318ef9d857f7c909bd4730edfc174573f5958d4" => :yosemite
