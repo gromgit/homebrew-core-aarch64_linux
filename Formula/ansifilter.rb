c114611afe6ebed9ce0966d40c95a4cb3f36bc1e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 23:44:43 +0000

    ansifilter: update 2.8.1 bottle.

--- Formula/ansifilter.rb
+++ Formula/ansifilter.rb
@@ -6,6 +6,7 @@ class Ansifilter < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "292128701440bad066fdb8b2fde122676f4df839ef38f2b4b4c8c33fb5214614" => :high_sierra
     sha256 "a05109993c3f9385e7fe021317ef2c9300a2e9f827f346162a0425e3ec478516" => :sierra
     sha256 "cde309f398ea0e0e3e0c0f3457f7c399716b025e7e8f3c3ff32304f925295282" => :el_capitan
     sha256 "6218b58e6e15bf1eda8a5e2d2141721b7f899e2478f9a51d5a19c6a79b25176d" => :yosemite
