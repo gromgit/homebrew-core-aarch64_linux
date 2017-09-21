b9060805bee08ba9190e9ac978ef19477d95c90d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 09:54:37 +0000

    packetq: update 1.4.0 bottle.

--- Formula/packetq.rb
+++ Formula/packetq.rb
@@ -6,6 +6,7 @@ class Packetq < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a5dad7c640b3c17658e23862e03f4cc9f1d0b4b4839e3e68d62a27699d307610" => :high_sierra
     sha256 "0272a62174eb6c0097c5942c9aa63774158234d3221b1905767425a7d1994a1a" => :sierra
     sha256 "e2515e9d55249cdc82e3ef8854acc7c509c207ac755ee5ea4dfc170c638744ea" => :el_capitan
     sha256 "8dac4f6f08064639c090863705a044f73432206e189e00d56ee3e15d7cc8e4f3" => :yosemite
