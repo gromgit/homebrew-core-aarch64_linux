23b5ab69f91cf38735f98cb0a82fedac3050d907
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 16:31:22 +0000

    tarsnap-gui: update 1.0 bottle.

--- Formula/tarsnap-gui.rb
+++ Formula/tarsnap-gui.rb
@@ -6,6 +6,7 @@ class TarsnapGui < Formula
   head "https://github.com/Tarsnap/tarsnap-gui.git"
 
   bottle do
+    sha256 "95ce5a2a28ff6981d569ad264cd7ef97bfbb20a808309f199b4044d50ea59e9f" => :high_sierra
     sha256 "65333d2097628d889e78ecb717a11de647f0d11058dd8ce7f263c573e828b54e" => :sierra
     sha256 "2963c8328a97832660467d27207bdb92340b7e1d10c4f70b552a56b433db5c5d" => :el_capitan
     sha256 "6c33861a277da0171c26ee389ab5e3f1d2277f3e577f18446e816909680cf9fb" => :yosemite
