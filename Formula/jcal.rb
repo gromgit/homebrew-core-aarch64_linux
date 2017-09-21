db0c76aa83bd8ea8a1dcac6ca39198973950b834
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 07:22:38 +0000

    jcal: update 0.4.1 bottle.

--- Formula/jcal.rb
+++ Formula/jcal.rb
@@ -6,6 +6,7 @@ class Jcal < Formula
 
   bottle do
     cellar :any
+    sha256 "348fdd02ce58859bf75ebe00feaf5c90e1f4f052d531e7667343f4c220d8e7bb" => :high_sierra
     sha256 "d6f50844723751f0de8181f751ffc0912013b518b5ac60777a3ade7e1aaa3179" => :sierra
     sha256 "4d876e18cb50c7aa31211b60b66e42637ca3c9eeed9c688c1945dc4755977597" => :el_capitan
     sha256 "3640b058b034b519a5aa3bb1dde36b4efb2ec7bb8124bdbd106617202bf87b22" => :yosemite
