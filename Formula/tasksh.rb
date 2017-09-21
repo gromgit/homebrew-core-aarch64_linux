d5c9b875b1a57f0660304cf69f86a00002046dc0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 16:43:42 +0000

    tasksh: update 1.1.0 bottle.

--- Formula/tasksh.rb
+++ Formula/tasksh.rb
@@ -15,6 +15,7 @@ class Tasksh < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "cc9cf043fcf84c05be38708d6217fe90836380075269b3ecebee12dc5d0096db" => :high_sierra
     sha256 "f4fbdfe7e82a50bce8b9bb9b1455ed4928ab32ebfbb185d5bb6bba1e6dacd1c0" => :sierra
     sha256 "0daecc2a8478e84ee4e84dc7bbca8888341ab021fa5f9379416862c28c2d1eb4" => :el_capitan
     sha256 "09895c7c1a3da30eb510fe971854af62d31dcd57c08dd8f60a4556b9273ad237" => :yosemite
