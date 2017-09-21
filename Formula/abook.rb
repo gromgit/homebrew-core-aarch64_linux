9fda327232b84e06e1c71fd5cfc2733ce6d05d8d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 22:19:52 +0000

    abook: update 0.5.6_1 bottle.

--- Formula/abook.rb
+++ Formula/abook.rb
@@ -8,6 +8,7 @@ class Abook < Formula
   head "https://git.code.sf.net/p/abook/git.git"
 
   bottle do
+    sha256 "3eb9e2e7003ef3501c63d87a5a632791769c84ac4d81a2c5fe1d40c04986e19f" => :high_sierra
     sha256 "e32cff277928e0b5cd24f201b1b5f94faf5469f263856b48c78f85b539018c86" => :sierra
     sha256 "fc5e09a73519a20dbe90258d6779bfddb1a02b2fc277fd54b4cd8c80c378539d" => :el_capitan
     sha256 "2f3a8d37fd17ecdda801f8de53e4048f19d824748e11a34c6f9abca0aae06c3b" => :yosemite
