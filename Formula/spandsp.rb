453d7654dcd414dd678bedcce2332f3cfd796809
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 03:45:46 +0000

    spandsp: update 0.0.6_1 bottle.

--- Formula/spandsp.rb
+++ Formula/spandsp.rb
@@ -7,6 +7,7 @@ class Spandsp < Formula
 
   bottle do
     cellar :any
+    sha256 "0b4bb6795c931452181252754da3db80189e6557c344e743296de823cb711efa" => :high_sierra
     sha256 "c0b8349525680304f99e464d328df804279be85398969974f2b708e5167c89fe" => :sierra
     sha256 "428120be3841ca77961f2d19fe8f98f1615972db4f568e3cfa2c0a44cae44e77" => :el_capitan
     sha256 "65fd095ea758180f18ca9c39864dbd3432115610b0db2439dbc923d3c0002f7b" => :yosemite
