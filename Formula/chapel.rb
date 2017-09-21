ee82d895f2514c1cbe65256c9dc913bc6ac2dd4a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 14:53:46 +0000

    chapel: update 1.15.0 bottle.

--- Formula/chapel.rb
+++ Formula/chapel.rb
@@ -6,6 +6,7 @@ class Chapel < Formula
   head "https://github.com/chapel-lang/chapel.git"
 
   bottle do
+    sha256 "47694f1d634eed07c52be8f22ca58a34ad03f2bf560bff214c2712d2d2a59463" => :high_sierra
     sha256 "df7074e302bb157e9eb54044a1bbf07639cca888badc6e57c5eed78412de4d15" => :sierra
     sha256 "b98d3a5b6bbdc0e117ce6b4706c6b4f14de294280c7d54494f6655234fb5734e" => :el_capitan
     sha256 "fb8d33a9926c574e025d74955164e9e2aadfe2a67099814e7089049d39b01bc6" => :yosemite
