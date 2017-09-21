fa34cca395fcac1a7cbcd93ff86e6ef5e42868b0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 07:03:29 +0000

    jsmin: update 2013-03-29 bottle.

--- Formula/jsmin.rb
+++ Formula/jsmin.rb
@@ -7,6 +7,7 @@ class Jsmin < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "333b2cb7e9b9b575580cf9a100760641117e2c178b766eee49dcd18854f40d8f" => :high_sierra
     sha256 "21ce8792fb1bb8b004f884953b2ab97ebd0d00568f5507c3b168f594ebbbd084" => :sierra
     sha256 "7672c92faa52fbc0684808da9803ebfa8883df0e0243e63a9a0b7c6441218b85" => :el_capitan
     sha256 "92ce35c390c8a2723e7b7cef8655e61ab9373f274c719ab4c04256cab1c42d1d" => :yosemite
