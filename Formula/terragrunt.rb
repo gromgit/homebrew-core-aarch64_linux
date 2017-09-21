625b75eb11710dd77e67d11de728dcc110eb260c
ilovezfs <ilovezfs@icloud.com>
2017-09-21 10:58:36 -0700

    terragrunt: remove case-insensitivity fix for aws-sdk-go (#18395)

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -16,18 +16,11 @@ class Terragrunt < Formula
   depends_on "go" => :build
   depends_on "terraform"
 
-  resource "aws-sdk-go" do
-    url "https://github.com/aws/aws-sdk-go",
-        :using => :git,
-        :revision => "00fb2125993965df739fa3398b03bef3eb2e198f"
-  end
-
   def install
     ENV["GOPATH"] = buildpath
     ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
     mkdir_p buildpath/"src/github.com/gruntwork-io/"
     ln_s buildpath, buildpath/"src/github.com/gruntwork-io/terragrunt"
-    (HOMEBREW_CACHE/"glide_home/#{name}/cache/src/https-github.com-aws-aws-sdk-go").install resource("aws-sdk-go")
     system "glide", "install"
     system "go", "build", "-o", bin/"terragrunt", "-ldflags", "-X main.VERSION=v#{version}"
   end
