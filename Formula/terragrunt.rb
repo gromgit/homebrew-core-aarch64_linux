b7b5a84a2291329b358322b28d81f21bd9d6c7a5
ilovezfs <ilovezfs@icloud.com>
2017-09-20 21:30:29 -0700

    terragrunt 0.13.3
    Closes #18363.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -1,8 +1,8 @@
 class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state"
   homepage "https://github.com/gruntwork-io/terragrunt"
-  url "https://github.com/gruntwork-io/terragrunt/archive/v0.13.2.tar.gz"
-  sha256 "408e235c4f8ea24ed227d5f254ba107be60c80ac6a2d54aad46053174b1cf0dc"
+  url "https://github.com/gruntwork-io/terragrunt/archive/v0.13.3.tar.gz"
+  sha256 "6e5f7c26054b91e0cce0de1deaa204d194bb78a73507d68b9a08b43dc217fe6a"
   head "https://github.com/gruntwork-io/terragrunt.git"
 
   bottle do
@@ -17,11 +17,18 @@ class Terragrunt < Formula
   depends_on "go" => :build
   depends_on "terraform"
 
+  resource "aws-sdk-go" do
+    url "https://github.com/aws/aws-sdk-go",
+        :using => :git,
+        :revision => "00fb2125993965df739fa3398b03bef3eb2e198f"
+  end
+
   def install
     ENV["GOPATH"] = buildpath
     ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
     mkdir_p buildpath/"src/github.com/gruntwork-io/"
     ln_s buildpath, buildpath/"src/github.com/gruntwork-io/terragrunt"
+    (HOMEBREW_CACHE/"glide_home/#{name}/cache/src/https-github.com-aws-aws-sdk-go").install resource("aws-sdk-go")
     system "glide", "install"
     system "go", "build", "-o", bin/"terragrunt", "-ldflags", "-X main.VERSION=v#{version}"
   end
