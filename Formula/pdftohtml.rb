c0b26e9556202baf2cac8f3233aafaeb4107993a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 12:39:30 +0000

    pdftohtml: update 0.40a bottle.

--- Formula/pdftohtml.rb
+++ Formula/pdftohtml.rb
@@ -5,11 +5,9 @@ class Pdftohtml < Formula
   sha256 "277ec1c75231b0073a458b1bfa2f98b7a115f5565e53494822ec7f0bcd8d4655"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "e896f26ac73f3a272aa17ee1815f3b173b94fa59680a8db3c0a814865da86afc" => :sierra
-    sha256 "622c66d77a35af1bbf94ee35ecba358d8868e187f664c68da729da0d3bead266" => :el_capitan
-    sha256 "9d222cce9a02a311248083aead8e4b55e9ac98ce98035a13d021b48d05eb5119" => :yosemite
-    sha256 "9dc35272d91c790627d21a93ca058c038ab660b9a6e619f35758a10c02e7dea9" => :mavericks
+    rebuild 1
+    sha256 "22264623cdbc31cb0d24f1a7fa5afa22a7fbea71f420877ab921611ea9b5c983" => :sierra
+    sha256 "8ecdcb1b267387509e8efc6637f119c782a70c6d4326201e0e76f06eb29dbcc1" => :el_capitan
   end
 
   conflicts_with "poppler", :because => "both install `pdftohtml` binaries"
