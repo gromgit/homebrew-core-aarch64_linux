949939d2e9e81e70f3a52d328374d4b5337e2117
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 13:19:22 +0000

    c-kermit: update 9.0.302 bottle.

--- Formula/c-kermit.rb
+++ Formula/c-kermit.rb
@@ -8,6 +8,7 @@ class CKermit < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "b6eae07c8d3365501f4e13af80b54ded073a2b1fc09fa885a445c7f52d96d589" => :high_sierra
     sha256 "b19ecd36ee298cba626b1276c228cdb4ee57726cf5af64166d8ff2800067e926" => :sierra
     sha256 "446776aff790c8f3b6f30be915dc18f4beffa973b92201384682beb7dc714562" => :el_capitan
     sha256 "fe01b123ec7cddfbf46908bbf2071542a92f195d75733230896b5de78d92cdef" => :yosemite
