ea51962c20b9e4e36123daa5e300806ed4cca7c1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 14:09:58 +0000

    cd-discid: update 1.4_2 bottle.

--- Formula/cd-discid.rb
+++ Formula/cd-discid.rb
@@ -16,6 +16,7 @@ class CdDiscid < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "26b88be0312f960484625161d94adf9a44aa88ef5817ba28b61af520a6e17e03" => :high_sierra
     sha256 "6b0d9c55a1adfce8a2c6e9eabd00c37118a05b60678564e7a9695d876bca117b" => :sierra
     sha256 "f0c17cfc3c345c661104a6f29562b766cac2a80747feea0c26cda04ece3c8326" => :el_capitan
     sha256 "3331be095997a1e5e6acb9f82f5e5473ed51c0f35976229371dc1d0c703c2e3b" => :yosemite
