45ac254189b3024758d181dbf196aaa6f8ac2a34
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 15:45:04 +0000

    clib: update 1.8.1 bottle.

--- Formula/clib.rb
+++ Formula/clib.rb
@@ -8,6 +8,7 @@ class Clib < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0dee597cc5870323ea38804337d3c3bd7b681a53303031399d31c12e1c1f1f55" => :high_sierra
     sha256 "054b42c0cb78315e454759303b7f25945a9ed18ee76f32a14d58a6911861f37d" => :sierra
     sha256 "64a97a9de695bc96f596d5a626428b8758ae0365b67c161bcd9519ccdf7dcfc4" => :el_capitan
     sha256 "ea221a1093f4bdb63209c30fc29a888ae5312baa9f50f1bc8c5b56dac75cbb46" => :yosemite
