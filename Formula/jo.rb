b3be0e82ae8057155d5b53c67dfdc1c024b185ef
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 07:00:12 +0000

    jo: update 1.1 bottle.

--- Formula/jo.rb
+++ Formula/jo.rb
@@ -6,6 +6,7 @@ class Jo < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "60c2de40a59a6caab00a3d23467e4b87e7388f7daca7307e84fb318a9f4a1352" => :high_sierra
     sha256 "8d0f2a55eb728d06f4640675d6aeec5de0a967fa2aa2614af211822c77548154" => :sierra
     sha256 "e1661162b8b18fbffa42a488a56748c2b368329fc2f0cc5fb5947fc98f049e8d" => :el_capitan
     sha256 "e5dc735e45b7eff98a43c8b9279b6d3a9b1992d8e8eab5a6e999af4cc31afbf3" => :yosemite
