05333909288e7fb919d3cffa35939f8a571a48d8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 13:47:18 +0000

    ccze: update 0.2.1_1 bottle.

--- Formula/ccze.rb
+++ Formula/ccze.rb
@@ -7,6 +7,7 @@ class Ccze < Formula
   revision 1
 
   bottle do
+    sha256 "6aa074341d1442c6795ece5581a5b404277254d4fdf3a42004d23e32e73824dd" => :high_sierra
     sha256 "7f1d8fb98c7ca95eb938ff2bae748ad081772542234bbd25151cc37e0f097461" => :sierra
     sha256 "795fc9b842f53197ec45774d909fb14efd463a64215fbec799ad870bb78a6834" => :el_capitan
     sha256 "11c34c8ad4df9993b7f465c7c7a7fdb2588e3c75d678f3b22f36166ca1c04520" => :yosemite
