b1aba56098a0ef5796e2d5862cdc3a53e22254e1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 13:01:50 +0000

    terragrunt: update 0.13.3 bottle.

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -7,6 +7,7 @@ class Terragrunt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f074b3b8c0e90622cba53c844abbd5f4cb12f06eefb8ac6cc1b3fad43562b2a6" => :high_sierra
     sha256 "9438610b09e24bc29575f71448530cd4629b0aeb474f04a018774e04ccaa5d80" => :sierra
     sha256 "77afdd7905c9286381531981904fe1fe89967cf1de290837051a05c73c5e3223" => :el_capitan
   end
