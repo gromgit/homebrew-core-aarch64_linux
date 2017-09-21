1ed6edd1487d1dc9d4b71c9d29794c6fded25e34
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 13:47:19 +0000

    cc65: update 2.16 bottle.

--- Formula/cc65.rb
+++ Formula/cc65.rb
@@ -7,6 +7,7 @@ class Cc65 < Formula
   head "https://github.com/cc65/cc65.git"
 
   bottle do
+    sha256 "d0e95f0073a40ad6a7cf252c0a9ac3097fecdd7d1b8e132f3cb0314554c8e673" => :high_sierra
     sha256 "c3e1ba0c47aa7cff0841b4aba4688d8598fbc39733cc4da9cfebbb626330e222" => :sierra
     sha256 "f0c5fee3dab48fc030953717cdea847a1bd7fc040d50f34407330f190de54420" => :el_capitan
     sha256 "c7b91bec2fa66f33f2c1f87a12db927ca613652e9a0e2b52848f6cc06d00acbc" => :yosemite
