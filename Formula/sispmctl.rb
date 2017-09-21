aacc8d32623324c85e4292368c9f98cb0fa63974
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 02:22:54 +0000

    sispmctl: update 3.1 bottle.

--- Formula/sispmctl.rb
+++ Formula/sispmctl.rb
@@ -6,6 +6,7 @@ class Sispmctl < Formula
 
   bottle do
     cellar :any
+    sha256 "721d04838452da87c60ff09d21e3bc948df16f626adbae7ea8108fb0fc10ebe4" => :high_sierra
     sha256 "4aae3c4b94d58d9b0cee1e8ad3eeed2583e7c247711a764dde0485b89687d9b2" => :sierra
     sha256 "a83e61cd8748b55173148cd71ee71852c257502a633674c01d78c473188ae4c3" => :el_capitan
     sha256 "a3bf8d25e2c4fddb14edf0c77ad4ecbf7773445ee833fed4106efcae03cbe529" => :yosemite
