e53758e30afd9fb30a80ad33479f9372f6722b0e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 22:23:20 +0000

    advancecomp: update 2.0 bottle.

--- Formula/advancecomp.rb
+++ Formula/advancecomp.rb
@@ -6,6 +6,7 @@ class Advancecomp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0fe41182dbed3eba095f1262b22ab9e032020c9b1c4c6d603f7d0ff4b1c84f59" => :high_sierra
     sha256 "91ccc9deafc27709e679cde5ce72a1d5086d765710495958bbdfc695a074511f" => :sierra
     sha256 "0b376386378fd8bea0864f6b5bb06abe38a7e4f1dfb6e22632a69c299c8238b1" => :el_capitan
     sha256 "1ee325f6dc47b3c17963372e31591442d6f977b8e20776a09f71cf7503e3bc5d" => :yosemite
