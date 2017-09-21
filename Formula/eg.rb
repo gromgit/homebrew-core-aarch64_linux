e249c5d18b0c4902012ff85132dd3516b8e0a312
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 08:47:52 +0000

    eg: update 1.02 bottle.

--- Formula/eg.rb
+++ Formula/eg.rb
@@ -6,6 +6,7 @@ class Eg < Formula
   head "https://github.com/davep/eg.git"
 
   bottle do
+    sha256 "4955ef20bd0d41b433f077784ca1a9d96a40eb2e6f7840c70f308b60d1fc553d" => :high_sierra
     sha256 "307a0ce0f1514288179dbbc56fdac3de02100c80e8c57b1abedcab5cd0cff458" => :sierra
     sha256 "500a97f229b78ab83b97591d9276f7d9e1e4ce4d392f2530f5c8a9f10543b469" => :el_capitan
     sha256 "65d2bab2a43912dfd487f817724dc7feee3ba3e226c07d0bd78ad22edcea970c" => :yosemite
