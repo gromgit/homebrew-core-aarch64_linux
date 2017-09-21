e34de0b556d4e783ccbf9512a490d474823ccd6e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 15:59:47 +0000

    qrencode: update 4.0.0 bottle.

--- Formula/qrencode.rb
+++ Formula/qrencode.rb
@@ -6,12 +6,8 @@ class Qrencode < Formula
 
   bottle do
     cellar :any
-    sha256 "5d6a5bde4d2fb094014783d53eacfaebcf5508c3c18fe59aecd030ae06d73f82" => :high_sierra
-    sha256 "1dbb2ba3b05519a9857fb26c7751c8dc4c312929650354a458f6931f260de7cc" => :sierra
-    sha256 "199fe87d536ffab8075f49d0fc95ddb1a3c45db8cdc26cfc6c2aa3a4b1379c0a" => :el_capitan
-    sha256 "2866e5c3b66b55d8bfb98c674e8467f4ca60de3994d82216277d9a8c88633672" => :yosemite
-    sha256 "1365ca76177b060b8f83d69f28711cd92d21fc9c9596ce32f4b4fae5a195854a" => :mavericks
-    sha256 "559fb8304c7da49cc214de238c57c6322ae8b389f102acb20f8b537e7f587814" => :mountain_lion
+    sha256 "df4727cb66c9ca501675f7b169ac9c211e7b766a7908c69ee6248536f810bd81" => :sierra
+    sha256 "f9bfb2e870776c67d8cfb735ac64162de374d5d123f48e3e3da965cdd742d73a" => :el_capitan
   end
 
   head do
