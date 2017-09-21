68eace381ea1308cc45bd62679e288fceabd074f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 07:36:42 +0000

    jxrlib: update 1.1 bottle.

--- Formula/jxrlib.rb
+++ Formula/jxrlib.rb
@@ -8,6 +8,7 @@ class Jxrlib < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6d24053d591022594ab92f880c56da4057a9e4f8d8ecb6f942e3558900385def" => :high_sierra
     sha256 "0eb8602ab389d9fff5bce803b085af322309592dc41aa0902b223e353a9f8abc" => :sierra
     sha256 "47c308b47ec8227d813a21c8092b32ada9b7f862aef102bf619d6bb19f0144e1" => :el_capitan
     sha256 "0dae977caf9e34289c9dd09f7e12bdf7158ccc42d9fa2dc00b4164b82c1caf3f" => :yosemite
