8372cdb17af25fc657dd5c330cfd06551a97896d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 10:44:28 +0000

    partio: update 1.1.0 bottle.

--- Formula/partio.rb
+++ Formula/partio.rb
@@ -6,6 +6,7 @@ class Partio < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "42d4fe9271be76bca99f13bc73146328265995f707fdf50d3c274f5a65193cdd" => :high_sierra
     sha256 "1db67357f3ce32f14c84788605a167838753433a1a81e17f40758fb2f2630445" => :sierra
     sha256 "da106b6a4b5667f84b6528081510b12d0da2acb1bfd74afbf3f7af72316afe63" => :el_capitan
     sha256 "a496ac6afbd60f605e2d3347d06a1850ae2617651b748e28c33a7c4c9c3bf957" => :yosemite
