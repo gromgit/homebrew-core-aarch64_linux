60f8b041383cd81c0fe799e6b6433f020e61b267
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 23:04:31 +0000

    aldo: update 0.7.7 bottle.

--- Formula/aldo.rb
+++ Formula/aldo.rb
@@ -7,6 +7,7 @@ class Aldo < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "d30e5e60defc2e2d2110cf52a60898d94ae3331a679f1c228e0d598421a594d9" => :high_sierra
     sha256 "ad5216c04fce4d1f4da63af2fa4d298a3414073db186991ec4389a942799ddd1" => :sierra
     sha256 "0691c4b9b7ae5b6f104c5b5205f731d4348563b8a9a8c3631395f619ce00aabf" => :el_capitan
     sha256 "f5d55cefcfc65033f50bf2aedb30298db1540a8dd5f5c028feb3b4b1c7e5610b" => :yosemite
