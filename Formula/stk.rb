07ceb46fcf73e742e6ebd3c103709f11631ddba7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 04:41:53 +0000

    stk: update 4.5.1 bottle.

--- Formula/stk.rb
+++ Formula/stk.rb
@@ -6,6 +6,7 @@ class Stk < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0c19570e9011041eac20bcd40dc116bc080f7c3858bdceab773a61bd8565eab2" => :high_sierra
     sha256 "5c344b00f598b39142e6dcc1c89014285bbe262cb5da0fff0c541d3030fd2ec4" => :sierra
     sha256 "15e49e8bd743392c616b9d7b1475e90b42f0d0850062cb4ab04b7462da0162d3" => :el_capitan
     sha256 "d121a9d4cb0e4b9cde3bc8a2cf304e39bc2e5520506decb7e1464cdc151e4317" => :yosemite
