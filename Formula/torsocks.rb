6d4b4eeedf362300630ec6bcdd3c6fd9d68e0f46
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 18:50:07 +0000

    torsocks: update 2.2.0 bottle.

--- Formula/torsocks.rb
+++ Formula/torsocks.rb
@@ -8,6 +8,7 @@ class Torsocks < Formula
   head "https://git.torproject.org/torsocks.git"
 
   bottle do
+    sha256 "8ab3874d27a0f2e343b2fe19e596ce369d571055b6aae6cbcf5cf33609731262" => :high_sierra
     sha256 "aa753c8002d5dbd78aa75b5bfd284646dd724a65f4896cb3f902b1c7283fd96a" => :sierra
     sha256 "3b978df65102580b8817d6d92e517ecd3a9f7086970c10af279a8d57fea60cae" => :el_capitan
     sha256 "7f1fd0440c3775776bddcb55f153108d0715a3ac9ee8a6a13798d168fbd71e41" => :yosemite
