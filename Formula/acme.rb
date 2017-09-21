5687323b59cadd92babfeb932397471fe974d298
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 22:19:52 +0000

    acme: update 0.91 bottle.

--- Formula/acme.rb
+++ Formula/acme.rb
@@ -9,6 +9,7 @@ class Acme < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "679da82eb906eb814fc69e373fccc81d0c92563d88f2e8138cfe21eedb611c1a" => :high_sierra
     sha256 "a551c65f11021ede47269b1a29b09c601063267501134daa8213674a62c97615" => :sierra
     sha256 "84f0ba7e45580d5a28a9a0dd9d7a25a6e67a9bdc7407c5b91cf64b8b9cf0a165" => :el_capitan
     sha256 "1e7c7805ac21061637cd1ce964f976c6f68b9259e892ffc77ee71f2aa280f879" => :yosemite
