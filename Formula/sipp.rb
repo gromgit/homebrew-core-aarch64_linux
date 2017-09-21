d56841756a88e080e540b6122becf0bfffbaa0ee
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 02:22:54 +0000

    sipp: update 3.5.1 bottle.

--- Formula/sipp.rb
+++ Formula/sipp.rb
@@ -6,6 +6,7 @@ class Sipp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9994674701dd3dd16dad079fa7cfe9de0424f489787f9e13cba029db28196e51" => :high_sierra
     sha256 "b05bdba460b40aa7317696bcabeefcdeea576cf310e6c3f191b2fe26a1c52d40" => :sierra
     sha256 "5409102b801d5a0a5bc2e79ff5707b231b09e95d0a83b73fd5a279433318d0cd" => :el_capitan
     sha256 "0e0aaca33a67a4c8b2a371f9f17e46ffb0290633fbda201788ae2f1ba002b10e" => :yosemite
