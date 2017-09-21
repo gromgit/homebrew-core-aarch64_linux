3789a04ce71afa5b0e2717452a07652a5fd84fc5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 02:22:54 +0000

    sipsak: update 0.9.6 bottle.

--- Formula/sipsak.rb
+++ Formula/sipsak.rb
@@ -7,6 +7,7 @@ class Sipsak < Formula
 
   bottle do
     cellar :any
+    sha256 "7590f9703c8c1a70bcc03274f278a398a2bc0d0259c4e7a7fb91c524ec4153ec" => :high_sierra
     sha256 "4725693dba5edcb68df030b63cb738795e96d29668bc9bf512ba8e2800ec862a" => :sierra
     sha256 "9f42a09240891ecd9aa62dea1dd75dc1c5362d2f946fefecf13ce975a6c05626" => :el_capitan
     sha256 "873d8cd50cce684ad55abbdf834157b4464c70877de9d1c37ad3c4ec9aaf6e10" => :yosemite
