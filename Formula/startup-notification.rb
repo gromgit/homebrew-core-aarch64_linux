5ad6d0d69206945bcbf5f6db0bf3abb3246a967a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 03:21:18 +0000

    startup-notification: update 0.12 bottle.

--- Formula/startup-notification.rb
+++ Formula/startup-notification.rb
@@ -6,6 +6,7 @@ class StartupNotification < Formula
 
   bottle do
     cellar :any
+    sha256 "593deddd4f3398736ba818eaa224dcc5b5337c88ba13c7ff1676f96b5c3adfd2" => :high_sierra
     sha256 "1480bccd4d65d99905fdd5010bd156b0ff2ee2ada36ce0bcb4e7b74fa632c9da" => :sierra
     sha256 "7762bbbdb98d8f360e82a9ac5e94239f94433e7bb38e8eec309230270c5158e0" => :el_capitan
     sha256 "770f1ab8c0339c940b098d91989fbc06bacafabe1a91cc891e9891ef39e83781" => :yosemite
