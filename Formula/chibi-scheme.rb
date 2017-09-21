1bcaef78770ec579e4326bd21d3fd0e342be508c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 14:46:55 +0000

    chibi-scheme: update 0.7.3 bottle.

--- Formula/chibi-scheme.rb
+++ Formula/chibi-scheme.rb
@@ -10,6 +10,7 @@ class ChibiScheme < Formula
 
   bottle do
     rebuild 2
+    sha256 "12a60ba17fa174d9309b5488752073ffc25a0d7c46febf360b674ab6027bd933" => :high_sierra
     sha256 "094b459f06e4ef4a6285e65f79a2e980965010bbc1e26b5e7b02489c04ca43cc" => :sierra
     sha256 "6427ba91b6fd3e63591d9b1f9c4cd173a53dfbef21acfdf1e41612c07ad4a18f" => :el_capitan
     sha256 "a999ce57390290ecc5452be6bda5f6fb415e565b46eb6806650649a85edce99d" => :yosemite
