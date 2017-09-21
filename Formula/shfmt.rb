e7b8a2f386de6a086310713926ec6d9f574c58c3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 02:04:23 +0000

    shfmt: update 1.3.1 bottle.

--- Formula/shfmt.rb
+++ Formula/shfmt.rb
@@ -7,6 +7,7 @@ class Shfmt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7de33e97c91cc32f5ce4f63d6bd3511926b717cf209f024d24f8662e8174befa" => :high_sierra
     sha256 "3900ddb7c8a1e991088438c1192a8d677d3887af8e841d8e38102d54dca828b9" => :sierra
     sha256 "e9886469456fa743b89f9b586c3042479fe08291f8d52b91b1f3725df079b1b1" => :el_capitan
     sha256 "90a9775ab847b5d4bd6584c142f19a9d3d4c8073a409a3e0ff1781a880ded6f1" => :yosemite
