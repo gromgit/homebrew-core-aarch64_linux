c59e671b3306cd9f4620a55b2e24a597d5473c85
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 18:04:39 +0000

    tin: update 2.4.1 bottle.

--- Formula/tin.rb
+++ Formula/tin.rb
@@ -5,6 +5,7 @@ class Tin < Formula
   sha256 "58e714e130d32258a41ce829c3286c5d4edb9df642ca7a62328b006c1f756478"
 
   bottle do
+    sha256 "d4047844c177a458fc5ecef53823dd6e2ff70acc6d36c76cddf72c7e817f0d03" => :high_sierra
     sha256 "0007b4384c05e500560ae3ad0fb4f46a78ee1b93ff97c2e33404b0c2116cfc77" => :sierra
     sha256 "ce603fb1e70d75727c644da65e88b23c24fbc5bf9852d94df0e6e3bff30652af" => :el_capitan
     sha256 "e4e00f28bd2a58040dd0d35379bf756eecdfb6887c8a046c43d187d5180ac0ef" => :yosemite
