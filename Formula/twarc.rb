6a854d918102b74fd01f699b6a58a28040e776ac
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 19:25:15 +0000

    twarc: update 1.2.0 bottle.

--- Formula/twarc.rb
+++ Formula/twarc.rb
@@ -8,6 +8,7 @@ class Twarc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "aa2cfd6c5cac278763adf65fb32d9ed6b5b3abd44f4dfe6cbd03ed35ae0a5928" => :high_sierra
     sha256 "2dacf384642ff9c9ab298d1eec15e92c9cd05762a42c1620c75e465a6f0c9c7d" => :sierra
     sha256 "ae5c8036fd72462444178a0060bf3c016281c9737bbb44fdd454b2b4e8de8299" => :el_capitan
     sha256 "65d96d754bfe03c52b133841235f9185a84f1429dc523b9e6d3ea7b310169682" => :yosemite
