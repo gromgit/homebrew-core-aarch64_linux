e9c14a4cf349ead1fafc50fa00b408300103d4d9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 00:21:02 +0000

    soci: update 3.2.3 bottle.

--- Formula/soci.rb
+++ Formula/soci.rb
@@ -5,6 +5,7 @@ class Soci < Formula
   sha256 "ab0f82873b0c5620e0e8eb2ff89abad6517571fd63bae4bdcac64dd767ac9a05"
 
   bottle do
+    sha256 "b6186caa197c5111b9704cc4b4f133f0f5f656727ed3211c7351e2a97302f10f" => :high_sierra
     sha256 "61ac3ada591371a743198251fa35d70a18cc6018d23e4a36e3ec45aa6ec99db2" => :sierra
     sha256 "4b8d8acc29c2ed8e826be84c9dc777d947033396336885440fb823530460b470" => :el_capitan
     sha256 "b802ee253ceb25ebfd2b5a90ef4dc8b229a90c7b1cae1a33533c9bd2ff9e7d50" => :yosemite
