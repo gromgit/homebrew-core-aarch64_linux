fa23139d57098a20b208bdb5f630f6cdeb5e1a8a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 13:01:50 +0000

    nghttp2: update 1.26.0 bottle.

--- Formula/nghttp2.rb
+++ Formula/nghttp2.rb
@@ -5,6 +5,7 @@ class Nghttp2 < Formula
   sha256 "7ac7ef5c83449303bf38d16eb45caa9e1bd0bb3abb2afc0ab1e6b3aadbded6dd"
 
   bottle do
+    sha256 "5adc3e0c9b9aba9792e6b859dafa95b7d903afa2b90abba19dcc00657d2f8ba3" => :high_sierra
     sha256 "479448bb0461f6a08fb88b548e06094c1acad9dc62acf86e57f273180719d08c" => :sierra
     sha256 "6d56d799255e3e3a1ace71a3da12f20dbfeab3651ccf842c56239939d244c912" => :el_capitan
   end
