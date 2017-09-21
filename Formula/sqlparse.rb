68406675a6cff89e7444d5d303408717940997e9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 03:47:25 +0000

    sqlparse: update 0.2.3 bottle.

--- Formula/sqlparse.rb
+++ Formula/sqlparse.rb
@@ -8,6 +8,7 @@ class Sqlparse < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7a658338e493383550813c4065173cc6ecbb2a6d4e4e11e2855a7a7f8393bc33" => :high_sierra
     sha256 "71e3a2ec0d8e4768883630a863e88a39c0b8cd122ff9d0d720df5f2b5fdf82c6" => :sierra
     sha256 "ce87e30708dc8b0cfb7b0b3c69a49e230375ee67eadab2b7bf7524f87fd50976" => :el_capitan
     sha256 "7a152da6d6a9520b05f6784db3f2867580a3fc16dd20c99185cc5012771534d4" => :yosemite
