c2a34aa9441eac929d5e6d24430691c0eacb73bf
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 07:22:38 +0000

    jhead: update 3.00_1 bottle.

--- Formula/jhead.rb
+++ Formula/jhead.rb
@@ -8,6 +8,7 @@ class Jhead < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "91644d47cff25c954cba7ccaa0c243dcbde63e626e9e749571042952d8ab3337" => :high_sierra
     sha256 "1adb1ef54417bdf4bd235ab907e4c198508bc2cebadcb8602cdec7809bb9e3a6" => :sierra
     sha256 "6a15b6b97fae6971752afbd05aa07e94ccebf1b216c9e36a2ba7bbf6523482bc" => :el_capitan
     sha256 "b1d517e2de29ae9a906636f4ed18c99aa459b221d1bff65fc497f6e86eae53ba" => :yosemite
