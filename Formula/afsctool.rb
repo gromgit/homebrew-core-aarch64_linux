8ffb989a2bcf3e9d7a5d248c32b191736761d37c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 23:12:28 +0000

    afsctool: update 1.6.4_1 bottle.

--- Formula/afsctool.rb
+++ Formula/afsctool.rb
@@ -14,6 +14,7 @@ class Afsctool < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8f1d0020beff7bcfd80530f69d29ca7f6bf053623c84a9957ef7dd5cd4fa3536" => :high_sierra
     sha256 "7f11c9c16fb0f5f148fb80f1888cfa1053296e8f552b11cc196a6c3fcf0afade" => :sierra
     sha256 "0751efedf08e3d0c4efed48861aaddf150bec2fdabc7099306f576a8c63c4971" => :el_capitan
     sha256 "a05524c78e0153712e5a9d1a73fe70ed2e5f0e5e20a91ae38fedf9115d2e87a4" => :yosemite
