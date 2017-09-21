2b2e426252579176eeab5c1b40ac77e9b6dc14bd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 04:14:00 +0000

    srclib: update 0.2.5 bottle.

--- Formula/srclib.rb
+++ Formula/srclib.rb
@@ -9,6 +9,7 @@ class Srclib < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c70f80acb0d5ff85add633b002e79a235ec8645ac700a9dc12f422ad3d80464c" => :high_sierra
     sha256 "9139e4e75f80bec18ffe9c09404fdfc070c82060efae6db21fd51e8098c73ee7" => :sierra
     sha256 "ec633330429e97a2cb82dea7177937c304433c857e97d2b7c6e0ab408f5d3912" => :el_capitan
     sha256 "adfdc037e211208e22d08f422c628bd709a7896a35a6177f7c2b303135e709ae" => :yosemite
