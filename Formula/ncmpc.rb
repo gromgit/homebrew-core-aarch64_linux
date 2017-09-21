63aef617556361eb1e92a186d06e233d983f41de
ilovezfs <ilovezfs@icloud.com>
2017-09-21 08:30:06 -0700

    ncmpc 0.28
    Closes #18385.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/ncmpc.rb
+++ Formula/ncmpc.rb
@@ -1,9 +1,8 @@
 class Ncmpc < Formula
   desc "Curses Music Player Daemon (MPD) client"
   homepage "https://www.musicpd.org/clients/ncmpc/"
-  url "https://www.musicpd.org/download/ncmpc/0/ncmpc-0.27.tar.xz"
-  sha256 "f9a26a3fc869cfdf0a16b0ea3e6512c2fe28a031bbc71b1d24a2bf0bbd3e15d9"
-  revision 1
+  url "https://www.musicpd.org/download/ncmpc/0/ncmpc-0.28.tar.xz"
+  sha256 "f66e5b6fef83bdfda3b3efaf3fbad6a4d8c47bb1b3b6810bed44d3e35b007804"
 
   bottle do
     sha256 "31af467381a348580ef78519ec1eb63fd2c862b8964562a4a623af27319dc7a8" => :sierra
@@ -11,14 +10,29 @@ class Ncmpc < Formula
     sha256 "63e019d248f1060ac1f346ce2b32813420bdb1b343edaf85499f1453a8fb8c36" => :yosemite
   end
 
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "glib"
   depends_on "libmpdclient"
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
-    system "make", "install"
+    # Fix undefined symbols _COLORS, _COLS, etc.
+    # Reported 21 Sep 2017 https://github.com/MusicPlayerDaemon/ncmpc/issues/6
+    (buildpath/"ncurses.pc").write <<-EOS.undent
+      Name: ncurses
+      Description: ncurses
+      Version: 5.4
+      Libs: -L/usr/lib -lncurses
+      Cflags: -I#{MacOS.sdk_path}/usr/include
+    EOS
+    ENV.prepend_path "PKG_CONFIG_PATH", buildpath
+
+    mkdir "build" do
+      system "meson", "--prefix=#{prefix}", ".."
+      system "ninja", "install"
+    end
   end
 
   test do
