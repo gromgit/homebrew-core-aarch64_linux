d620382643c725f039a8d0f92c34f49bb29905cf
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 19:25:15 +0000

    twemcache: update 2.6.2_1 bottle.

--- Formula/twemcache.rb
+++ Formula/twemcache.rb
@@ -9,6 +9,7 @@ class Twemcache < Formula
 
   bottle do
     cellar :any
+    sha256 "f644983ebb4d4125cafe67f11f1cc8e0acd164778e4c99120f054432c23a16cd" => :high_sierra
     sha256 "24aa937f8757cab1bad5e0d774478c18f7baf6e2ddc2fee78a2d48a8bf66f381" => :sierra
     sha256 "08efcbeaabad2800d757213a7919b83772f09bbb852e17d8be5d8b70e332309d" => :el_capitan
     sha256 "afe9fcd8a6e8c4c8aac98b2484da12b44126a6070e810fcba3441bf1afa899cf" => :yosemite
