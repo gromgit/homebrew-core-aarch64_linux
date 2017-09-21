b817247f2dd4a7bb368d73916c14435a22bd501e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 22:23:20 +0000

    advancemame: update 3.5 bottle.

--- Formula/advancemame.rb
+++ Formula/advancemame.rb
@@ -5,6 +5,7 @@ class Advancemame < Formula
   sha256 "779b3b43e3fbdf4bb24e97c418499cd4550446494e19f5d576827242b0821814"
 
   bottle do
+    sha256 "68814ac2f24c61b03e395f0b39141b8d1a32a39f193c25a8a1e21000bdae7556" => :high_sierra
     sha256 "2a64703b056e341dd6ee4773038f5d4fa289ae120e64b852d103c41cffec3fee" => :sierra
     sha256 "1f67883e185c7d2afe1bfef2ab7eac425c15c4540d648cabb40b2cde8aba0e7f" => :el_capitan
     sha256 "1865a05529d60f846bb97b29c936aaf49f975d788d3687a7b7e87fcd0681bf3d" => :yosemite
