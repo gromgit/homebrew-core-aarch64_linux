6f53811a083854241dc6fc001e97d0979b7d9ef9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 02:04:23 +0000

    shmcat: update 1.8 bottle.

--- Formula/shmcat.rb
+++ Formula/shmcat.rb
@@ -6,6 +6,7 @@ class Shmcat < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "34ecdb9d84a95821f6e4653776d5e1b24b34658e2fc4cbef01b4017328f32b39" => :high_sierra
     sha256 "01cab982f77ba84084219d9106a7dc2e15ea8f1386b6b1dccadfb3f731c3a5be" => :sierra
     sha256 "418a5aca4e039528a55706db8063808a0fa1dafb3d913376177735bbce1dc836" => :el_capitan
     sha256 "1ce5eef9d77925041ea91bc2c8d49572525369df7a037928c3eb83698a58a8a4" => :yosemite
