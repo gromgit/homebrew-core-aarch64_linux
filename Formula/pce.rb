3126d6ed2b1ff7dd77fc57a189c2ac4ed989a463
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 11:04:10 +0000

    pce: update 0.2.2_1 bottle.

--- Formula/pce.rb
+++ Formula/pce.rb
@@ -9,6 +9,7 @@ class Pce < Formula
 
   bottle do
     cellar :any
+    sha256 "43d68fb33e9edfb191d49c5966954f30dd9805b793b8fe31f2f53c4e0b3306a1" => :high_sierra
     sha256 "5eac356a0cc4e27d748c3eaaba446f64005dd45cfdc7b7be915bdf186eb01655" => :sierra
     sha256 "8574731084cdd4efd9394e7b92d262e80dcb23dc5d9d8bc14746c81ea9f66bcf" => :el_capitan
     sha256 "5166b5f8b91d05e8d45f8683326e865cb75e2f574dff790d423ac4f4215d51f5" => :yosemite
