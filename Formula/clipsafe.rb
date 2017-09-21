8820b1c6c4d5db478ed8b58e9c606ba5241bec17
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 15:56:18 +0000

    clipsafe: update 1.1_1 bottle.

--- Formula/clipsafe.rb
+++ Formula/clipsafe.rb
@@ -7,6 +7,7 @@ class Clipsafe < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "211d670b61c6a68650736df3deb3ae783c320491ba3205e035cc28c014fca705" => :high_sierra
     sha256 "7c894e55e215fffa121aef718dbf7fa0f2c71531cf7970ae8d27f4b5eb939877" => :sierra
     sha256 "ffd255c6ff1bd4c403bc4040f1751eef61b4ae341c33b226bf9a696b2836b02e" => :el_capitan
     sha256 "01dbb7b4cf71ae2b174cfec45cdf4a69e211a5154bc6541fb19e1b5aa20f8389" => :yosemite
