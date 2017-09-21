22692e1f76d56759bc36b3025f00391051cdc5ef
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 18:17:20 +0000

    tmpwatch: update 2.11 bottle.

--- Formula/tmpwatch.rb
+++ Formula/tmpwatch.rb
@@ -6,6 +6,7 @@ class Tmpwatch < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "18ca8e16075315e3aca9cd4e2e445358c33d5911a647557ad8971edb232e4cc4" => :high_sierra
     sha256 "945dbe942b232586517a2fcd81faab1fc20d3936e53ddf9154158dd0b7d3d55c" => :sierra
     sha256 "d19ca779df5c019f840d0c186822fbab4758bf3635101aa975b4bad35bb2f184" => :el_capitan
     sha256 "6b7935b74c118e797b2cba298e0f546d4231ac2c5eb165f4a63bc6c2d0a372dc" => :yosemite
