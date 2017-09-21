c4ad4496df348b3143d13d3b109d83ff85b7b52e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 18:04:39 +0000

    tintin: update 2.01.2 bottle.

--- Formula/tintin.rb
+++ Formula/tintin.rb
@@ -6,6 +6,7 @@ class Tintin < Formula
 
   bottle do
     cellar :any
+    sha256 "415aa4fca1fcde634e92792546efaa0b200ed2fdbd1ff9a577a6ca12e7d3170c" => :high_sierra
     sha256 "83b285b7b301a92fab1edcf9123bd5a14c1e25b79e899d638a8ff6d0d624a05c" => :sierra
     sha256 "e58eb5055b943111c93dd2ede24df92b9b0d2ada69b128d43d4a837cc045ba5b" => :el_capitan
     sha256 "0fe0c128b0c4d22a6015e56220f3906a2e61e3b17294a5cf68e03104fbd33c06" => :yosemite
