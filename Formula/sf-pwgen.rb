2346437467847b3118f5ea6be7ba1cca5d0c91be
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 00:43:08 +0000

    sf-pwgen: update 1.4 bottle.

--- Formula/sf-pwgen.rb
+++ Formula/sf-pwgen.rb
@@ -8,6 +8,7 @@ class SfPwgen < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "05ac7530759e9f2cf3a0e94a5b1f4fea83a1710de8010fa9b3d9b60167018038" => :high_sierra
     sha256 "ff59281df8d8c0e341233cdffafe67d0dc78101afb90a8597b0a83605ef578a3" => :sierra
     sha256 "2c0e0523569aa25fe254012d3b86ae0bdc587c0f17c4a62d8d12917b7fa44fbf" => :el_capitan
     sha256 "2c6d133b3c9b079dc8c81407107a3c1fb4d5cb3d654afa7acef6f23b9f9df9a6" => :yosemite
