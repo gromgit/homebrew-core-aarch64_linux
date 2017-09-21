f5dd2fa004961fce49edb0b8efd14a502bb5a5c3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 16:43:42 +0000

    tbox: update 1.6.2 bottle.

--- Formula/tbox.rb
+++ Formula/tbox.rb
@@ -7,6 +7,7 @@ class Tbox < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6f0f21bf74192941533d179299f25027727c7e9f6f1478462d10762112a5cc09" => :high_sierra
     sha256 "06b1ebd6756e7bb2cedf152a5cf41b127fdeac7c4ac070bb6b70dc286aebbed3" => :sierra
     sha256 "fa1644f34db9e0e187448f06d5400c88493612dc1b78608dabcaff7eab6661a6" => :el_capitan
     sha256 "b9c2d0df34fc3062f4963765fe2524cd5378ad126f2f611d971b5efd7c727c75" => :yosemite
