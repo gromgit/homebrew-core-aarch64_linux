060e5654f544d232fc5e0a51bb7b52f08a2d860b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 16:43:42 +0000

    task-spooler: update 0.7.5 bottle.

--- Formula/task-spooler.rb
+++ Formula/task-spooler.rb
@@ -6,6 +6,7 @@ class TaskSpooler < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c0731e16f98483ad9cc2dddba0895fcaa1ace8cb78102fb108b84dc39ea33298" => :high_sierra
     sha256 "4e40910e93dbad52616762c4fd85eaa2ffe7f8c75c769f3d5c81ff2115b22869" => :sierra
     sha256 "2d54706f5ac7cd1f0ab18af122223fb63960cb04da9effb3100fb4e1c53fc891" => :el_capitan
     sha256 "bda2858d1071b3091c48b9774786c3b88b0ded7cb7a569054aa1774b141e5555" => :yosemite
