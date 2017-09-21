3b7f3d9b32756dc5753c4f83053013da2e586494
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 04:45:36 +0000

    libuvc: update 0.0.6 bottle.

--- Formula/libuvc.rb
+++ Formula/libuvc.rb
@@ -8,12 +8,8 @@ class Libuvc < Formula
 
   bottle do
     cellar :any
-    rebuild 2
-    sha256 "3f1aba3e516ad57e818147ad694f6fd4cbf37192380073e122015ec9e86d2eee" => :high_sierra
-    sha256 "87fe5bb38f766397c7dfd1a22529f781679dbf32f0bc2c1d2e1f5c53c1b124dd" => :sierra
-    sha256 "6893748683e4eef0e0bd595acc9d7dce1e70d06778e264514ffd0fd26f3cf22d" => :el_capitan
-    sha256 "5a20089a01f4c7f1d85bc91f6f2369ce5b1f1c4faf8c9beed2a764f80609ab41" => :yosemite
-    sha256 "c2b007f5dad961d8767cc129d69ba352e0c8d6dff9746ee45697ceff6208ed8a" => :mavericks
+    sha256 "0e2f8e177ecefcf0cd2a2c2be399d3e45fe18956642363b6f0e9b6632a7019da" => :sierra
+    sha256 "2223e66660b7c600b71cb13cc9921c687bcc33e9e85fee7ab351c383e1533657" => :el_capitan
   end
 
   depends_on "cmake" => :build
