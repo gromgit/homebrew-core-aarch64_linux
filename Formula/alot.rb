e4f14c21318878019aca64c20c6e31e944871e15
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 23:04:30 +0000

    alot: update 0.6 bottle.

--- Formula/alot.rb
+++ Formula/alot.rb
@@ -9,6 +9,7 @@ class Alot < Formula
 
   bottle do
     cellar :any
+    sha256 "118b98872b13118ef43e59378f439c903b19bfed5dca57a15a64a4564320f303" => :high_sierra
     sha256 "9939df295cbce9cb3e670696cc3fde360cd92f8a18659f406a8b59f10878ea23" => :sierra
     sha256 "52bd6a8cfb10e89efca23fbddaeb91b5efa3e1ff1ef9077696660c14cca69ff3" => :el_capitan
     sha256 "ba467d9c7bb1343ab3ab71e87342cd3f3a17504ec2efa1bcea667a82626e61d3" => :yosemite
