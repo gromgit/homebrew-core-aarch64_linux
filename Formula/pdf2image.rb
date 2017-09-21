e8ef2b440da98fc0145777a0c09bde8758c7ee34
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 11:17:58 +0000

    pdf2image: update 0.53 bottle.

--- Formula/pdf2image.rb
+++ Formula/pdf2image.rb
@@ -5,6 +5,7 @@ class Pdf2image < Formula
   sha256 "e8672c3bdba118c83033c655d90311db003557869c92903e5012cdb368a68982"
 
   bottle do
+    sha256 "59ba0fc004f64cafaeb8e9beb6c07964b8df8fb10bf653aeb83713e3610ac182" => :high_sierra
     sha256 "46df7ae58a1bfc73cd1a1c1075de032724514d29995e965169d69f51409cad7e" => :sierra
     sha256 "c12d781ab5136a717cb88cadb50b2dfcd1f67cf263b5b668b1e171f562bcb072" => :el_capitan
     sha256 "a0bb792123e4754d5cf80cf248e8932dd1885616af2c4c9c7f00e35cda962725" => :yosemite
