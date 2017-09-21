f39dc7a83525faa30220a356a07d3cb4bbdb71f5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 07:22:38 +0000

    jailkit: update 2.19 bottle.

--- Formula/jailkit.rb
+++ Formula/jailkit.rb
@@ -5,6 +5,7 @@ class Jailkit < Formula
   sha256 "bebbf6317a5a15057194dd2cf6201821c48c022dbc64c12756eb13b61eff9bf9"
 
   bottle do
+    sha256 "166941217a0df0400cd068f2b3d9859c8b1a30981488c28bc0d44b2a7d4eafb6" => :high_sierra
     sha256 "ac67228b1970793bc8f5e6b0f167bdda59890aa23717443baa58a5ee0e0efe98" => :sierra
     sha256 "9c46c69db017a018e9b4c92f613d99d22656d331962bef1da85c0ae782a172e1" => :el_capitan
     sha256 "b57b4205ede4e8dff0e09c386034e322f667ce4df739b02578579f844dfbe5e2" => :yosemite
