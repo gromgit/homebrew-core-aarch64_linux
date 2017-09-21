d31f976bff48e0adaec794fc656b98e309dd2272
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-21 23:44:44 +0000

    angband: update 4.1.0 bottle.

--- Formula/angband.rb
+++ Formula/angband.rb
@@ -6,6 +6,7 @@ class Angband < Formula
   head "https://github.com/angband/angband.git"
 
   bottle do
+    sha256 "40e581e4e548e5fc1cba2e445fb934164c0525db8c10f28efddd046428eef89b" => :high_sierra
     sha256 "87585187f01fa753e59d6b6f804c1436c96c356c6f82878d5f57c3e54d80d4ce" => :sierra
     sha256 "4aa7a0f0218a488f89b5b6a8068d51f32c6331f02aaf360c35690e52f0d337ff" => :el_capitan
     sha256 "0733eb9c331522191a5017cf3fa3d20c5acb9da308db825bd9aba7dba43b3b3f" => :yosemite
