89b83cddd57fec7f1bfe6b6c541ebe42166ae887
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 16:29:37 +0000

    nzbget: update 19.1 bottle.

--- Formula/nzbget.rb
+++ Formula/nzbget.rb
@@ -6,6 +6,7 @@ class Nzbget < Formula
   head "https://github.com/nzbget/nzbget.git"
 
   bottle do
+    sha256 "0f508d759d085ea42af708598eba3d2f589614f6025f8ca160b93c6170d5576b" => :high_sierra
     sha256 "b2b460f1f4a850d282b3faa56a0cdc66d7d9f2072e34528fe1fc875f615e3705" => :sierra
     sha256 "3aa8bd8510dbde22143fa6d9637d664951c4a48758c840548dcee0ce48f3b95f" => :el_capitan
     sha256 "f9731421aa1289d62d9f30691c3e643e8548eb066d55c72511ad74d994e826e7" => :yosemite
