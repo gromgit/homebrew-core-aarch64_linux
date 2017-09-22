efd8735e599e22aac7697a55c174e8aa12cea10d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 05:16:19 +0000

    doxymacs: update 1.8.0 bottle.

--- Formula/doxymacs.rb
+++ Formula/doxymacs.rb
@@ -6,6 +6,7 @@ class Doxymacs < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "29a4865170b12a2194c238c35ec5e0902b8e637e378f9013b7aef64fa21eb0fc" => :high_sierra
     sha256 "2fd3dc59a8c0c8fdccf8195265d320aaa7b5d67e9a81b5a085f27cc287e7370e" => :sierra
     sha256 "fb892db831aed57dbdcb2d3a81d78bd05c5b689376d4b7f14bffc56826205ce9" => :el_capitan
     sha256 "09eb19921c2ecce5bb02b185c1040caef07d18706866006bdd5fa428bf6b8560" => :yosemite
