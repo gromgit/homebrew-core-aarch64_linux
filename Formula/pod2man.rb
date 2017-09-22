c49fe5fad9cfd02dfcfc84873d82d0a78bcdc719
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 10:01:10 +0000

    pod2man: update 4.09 bottle.

--- Formula/pod2man.rb
+++ Formula/pod2man.rb
@@ -6,6 +6,7 @@ class Pod2man < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2d9608def9daa2bb795a56e5df80043d63eb008175718d990df9aff58ece80bd" => :high_sierra
     sha256 "2cf1dfe0b8b253e9512510a4d8ff6b978e9eafdd9385c3f2b798786c29fbf32e" => :sierra
     sha256 "d2e5d9ea75def3862ed5a22c17054b59c7be71e33795a1335e760f6674f64260" => :el_capitan
     sha256 "87c97323caaad4ff681c76cd00f18b0575615f9e5bdf01b6dae20bba5d95ae74" => :yosemite
