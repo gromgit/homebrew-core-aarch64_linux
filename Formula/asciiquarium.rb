5363933f88b552b4e48fbdd5a69134f23a93eeb3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 00:34:53 +0000

    asciiquarium: update 1.1_1 bottle.

--- Formula/asciiquarium.rb
+++ Formula/asciiquarium.rb
@@ -7,6 +7,7 @@ class Asciiquarium < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "10d2a74f8e447c87fa477de74aa692a1d0043ab508e9a924126e0a3d55ffe5a7" => :high_sierra
     sha256 "890b0e69b0261ff61b0d0666f2b3e0f579c1f63556c77c2d8d24bc1ef3f4e241" => :sierra
     sha256 "9120f02b70c63672af2752de536aeaeac5ef57bc2b3a388afe1ab9e12d40a59b" => :el_capitan
     sha256 "6b20abf264f40c7123e40f0f34cfc11f0c12a03b1a74a324e3f3a7ae75e94f3f" => :yosemite
