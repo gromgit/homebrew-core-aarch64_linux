e8b3be867944bfef1d38a8584b1fddf511f0d704
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 04:34:14 +0000

    dnstop: update 20140915 bottle.

--- Formula/dnstop.rb
+++ Formula/dnstop.rb
@@ -7,6 +7,7 @@ class Dnstop < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "4d6b9a2f15e3165ccf63b67752cd4f0d21b128f64b5f22beb2c2b0657e082709" => :high_sierra
     sha256 "dc995c2857fdd5093ae753844ce5c45ed00bae59184528a184e0313b25882802" => :sierra
     sha256 "1d5b1ad056475ce9a27f40b48cbbf58421e4eb66fd134ac318413de2d025db66" => :el_capitan
     sha256 "aa3b72d1432e7c13b9b7e0722cde3f7fafef17aff557489662029698929638dc" => :yosemite
