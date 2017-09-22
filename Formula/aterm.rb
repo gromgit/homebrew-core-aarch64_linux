6c0b84a962addad34bb4e23774bbc6f96dddbeae
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 00:48:19 +0000

    aterm: update 2.8 bottle.

--- Formula/aterm.rb
+++ Formula/aterm.rb
@@ -7,6 +7,7 @@ class Aterm < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "f56a13be464fa577fdad7fe82779f5e6bbe820995e1849b6741ca92807c10bf0" => :high_sierra
     sha256 "dd7b81b3bd9a31746ab461b8d79e4c32838b7e86f540769e4c17825a4b89c1c2" => :sierra
     sha256 "5140e20287eda941f8756dfdaf377663f84f6872d1ca3f6d70e04b554591d11a" => :el_capitan
     sha256 "d12bebbfa2e764abb9cfac1aecd6fc04e58f83eadf0fb3db298d5be03d7f8dca" => :yosemite
