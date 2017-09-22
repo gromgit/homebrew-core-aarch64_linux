efa12866624136e48fa323fa36315a300752536e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 15:08:59 +0000

    nanomsgxx: update 0.2 bottle.

--- Formula/nanomsgxx.rb
+++ Formula/nanomsgxx.rb
@@ -6,6 +6,7 @@ class Nanomsgxx < Formula
 
   bottle do
     cellar :any
+    sha256 "2b4216a092883e6459691d19070eecb323f65bdf3be5ace59c58b54324a419b5" => :high_sierra
     sha256 "d61b7a3a91b7568325a5b94382ac978a60fb4c190ef2627db9d3a563cc3cbc15" => :sierra
     sha256 "13abe2463ffcf219b5bcc3ae0ef48e0d99e2d9810ebb9e28c2344e9f4ef4cd6a" => :el_capitan
     sha256 "5d1ef8eed698ca5e10d28fa5609033f546f8d831130b58d17c3369098482230c" => :yosemite
