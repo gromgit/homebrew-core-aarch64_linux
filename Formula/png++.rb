50d19c708c2e27b892e45b0489ac3ff9200f558d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 10:01:10 +0000

    png++: update 0.2.5_1 bottle.

--- Formula/png++.rb
+++ Formula/png++.rb
@@ -7,6 +7,7 @@ class Pngxx < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4085580bff1ec2116c321e84a426d195943a002f84b4662c9814a64e5925c627" => :high_sierra
     sha256 "41785b523dc50e5b237d83d027b34b6532114a6751eff3f409ee0ba9e0730f04" => :sierra
     sha256 "7b01b3ff0af9e60f2887bb45ff5ba2f5823a9a440c2d78e51e69904d3edd80d8" => :el_capitan
     sha256 "de37d7fadb7308b45ba0448308256d00bf36442bacbab1e734ee8398aea8a8dd" => :yosemite
