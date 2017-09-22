2707de3b2081a4b2f80a712a85c336650baf7fb3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 00:03:59 +0000

    appledoc: update 2.2.1 bottle.

--- Formula/appledoc.rb
+++ Formula/appledoc.rb
@@ -7,6 +7,7 @@ class Appledoc < Formula
   head "https://github.com/tomaz/appledoc.git"
 
   bottle do
+    sha256 "53e67d5fb1067078a1fde8cc17c5836313d6303a9fe03b9df4d0727fb89974ca" => :high_sierra
     sha256 "2d986524eff2914b52f2336ab19dfd8bf21fb98a278e47690355c0cce525a06b" => :sierra
     sha256 "c723bec6cdeb0eb067d7c67cee472f20a6f1935e1bbd6b0a3aa0ec7f77fea583" => :el_capitan
     sha256 "ada12050d25be7a3c9920b1b4e2aa8d8a1efa7d59d9e67325f4e83dab14d0f59" => :yosemite
