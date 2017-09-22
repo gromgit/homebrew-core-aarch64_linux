eae05bcfbecd9b3c2bb812b4e7dbbb028191dd84
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 00:34:54 +0000

    arp-sk: update 0.0.16 bottle.

--- Formula/arp-sk.rb
+++ Formula/arp-sk.rb
@@ -6,6 +6,7 @@ class ArpSk < Formula
 
   bottle do
     cellar :any
+    sha256 "db71e1610feac13246511f6c67bbf224e20b49e9a130d76dc5ca3317fe755601" => :high_sierra
     sha256 "d933c37e26f227918a6e770dc3214a76f06ca79abbf1f646a6c00447ad9933ac" => :sierra
     sha256 "5d112e8d54329bff104270b7ca27cd4884e48f8c47904bb5838a2e107c035736" => :el_capitan
     sha256 "11253608a659d16a179c9c7b25050989991a29a68c9c9b4647fe9614e191fcff" => :yosemite
