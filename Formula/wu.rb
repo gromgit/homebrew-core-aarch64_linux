30231b90d01de56229159e05127a82ef6a0205b8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 14:13:24 +0000

    wu: update 3.10.0 bottle.

--- Formula/wu.rb
+++ Formula/wu.rb
@@ -6,6 +6,7 @@ class Wu < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "994a58ca1396928ba5a30189ca784417c510d1563ce64d3866e99f5fdc46c7cf" => :high_sierra
     sha256 "5efc0cdf39ff0d7ce0cb70be665118459cea9f8523f9a01bf67a341e1330fb56" => :sierra
     sha256 "bd297452f33d081c7720190bc9ef17f1c8b247de5a27c64e78fede64bd871050" => :el_capitan
     sha256 "61b96421517b20e4d6090e37ffe3b52a93ee0934ecc06e8282fd06d0bb457883" => :yosemite
