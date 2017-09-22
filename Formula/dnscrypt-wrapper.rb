d02bea9f295a0a0b7290313696f6e319e2415ec8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 04:34:14 +0000

    dnscrypt-wrapper: update 0.3 bottle.

--- Formula/dnscrypt-wrapper.rb
+++ Formula/dnscrypt-wrapper.rb
@@ -7,6 +7,7 @@ class DnscryptWrapper < Formula
 
   bottle do
     cellar :any
+    sha256 "adc0b376897d8d3ee716cddfa3bcdc66447a4c2bf217a6fc3e1a4b0324e6279e" => :high_sierra
     sha256 "c877cf6c9d926a9414f14dbf97096001cadc1d2c2daf634e08a36003d4898e0c" => :sierra
     sha256 "4b502c8e15050c876280cbf50c277f4e6e57609152e04a63450f5c25effd1ced" => :el_capitan
     sha256 "ec06eb2055c8a812f08e2cb7783f431864baa77d2375775ba36bec730a1b585b" => :yosemite
