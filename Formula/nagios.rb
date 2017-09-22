f3733fa50eb8db2ce8435e377945b8468a4ed17c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 15:08:59 +0000

    nagios: update 4.3.4 bottle.

--- Formula/nagios.rb
+++ Formula/nagios.rb
@@ -5,6 +5,7 @@ class Nagios < Formula
   sha256 "c90b7812d9e343db12be19a572e15c415c5d6353a91c5e226e432c2d4aaa44f1"
 
   bottle do
+    sha256 "d1d4071289fd037542a4f36f205ba522d3aae397e5379def5c402e6befb19f72" => :high_sierra
     sha256 "979dbfa690a58f825c7c06376e2c27ed0b680a1415a996a4666325a2dfaf9ed3" => :sierra
     sha256 "0f33981e12c49a84137d57bfd3c9163da1632ab9c1a9ab97f699d4a7f722811a" => :el_capitan
     sha256 "a521392cf589fab9d9dda30b4cb1e5a7c23baf22b898114a3d65b40a830b2a81" => :yosemite
