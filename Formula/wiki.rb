10ab8c61ff987cb408733a39586258885b87feb8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 13:36:31 +0000

    wiki: update 1.4.0 bottle.

--- Formula/wiki.rb
+++ Formula/wiki.rb
@@ -8,6 +8,7 @@ class Wiki < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b659c9bc28a5468e61a7af026f20fe8a9233b852e1165e5d900755db68fa85f6" => :high_sierra
     sha256 "0f3302cb5063486d6cb1beb1a25c771b50c03f0318f05e7d3520b1a2d05a445b" => :sierra
     sha256 "6e6d9036b7943ef08cbf92c5aec72b214599aa83bd0a038f4d7a0d19a90a70b0" => :el_capitan
     sha256 "b7f224cc011a63259a7ef24b2709a4fb3ba053b15f1861a6c3f03d29925251f8" => :yosemite
