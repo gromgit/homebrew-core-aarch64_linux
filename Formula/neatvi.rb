13eb0f8cd99ca9a4f2fc42f651b048ee969cdc76
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 15:39:47 +0000

    neatvi: update 05 bottle.

--- Formula/neatvi.rb
+++ Formula/neatvi.rb
@@ -8,6 +8,7 @@ class Neatvi < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "786ba6fcd9f3826fd7cbb5c0ba466f914e996043eaeebebac60f3f3b747a7187" => :high_sierra
     sha256 "4af6bb9a7d0b654f929665393e97e250b34eb6b678d107d36143f524f7dea173" => :sierra
     sha256 "0dcdf4372cf9a0c3d837df0471511838539c6250351c93711a981444ab1bb11d" => :el_capitan
     sha256 "c715f3a039031f83bd71eb6ce21caedba923ad3d78692df24ccfd9ed80b1b099" => :yosemite
