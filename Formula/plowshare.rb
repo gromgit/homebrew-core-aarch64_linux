aa28ce6269121ca22d09aebad313213cb9b655d0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 09:30:26 +0000

    plowshare: update 2.1.7 bottle.

--- Formula/plowshare.rb
+++ Formula/plowshare.rb
@@ -6,6 +6,7 @@ class Plowshare < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a8bb9186521eccae7675dbdaaa9ed34a11a7368e6c591a89f789f83cc05a405d" => :high_sierra
     sha256 "4b80e06e6ca689befeffdbf8a3f91b17cbe70e69507d45fb6f37629bd62e0ce4" => :sierra
     sha256 "4b80e06e6ca689befeffdbf8a3f91b17cbe70e69507d45fb6f37629bd62e0ce4" => :el_capitan
     sha256 "4b80e06e6ca689befeffdbf8a3f91b17cbe70e69507d45fb6f37629bd62e0ce4" => :yosemite
