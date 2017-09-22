32e3455cf444a7ee90cc29f168d54a46cd646e38
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 00:03:58 +0000

    apng2gif: update 1.8 bottle.

--- Formula/apng2gif.rb
+++ Formula/apng2gif.rb
@@ -7,6 +7,7 @@ class Apng2gif < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "810005bcbc32c60c7084b248eef3d007e756180842051f64385fb90cfac66c63" => :high_sierra
     sha256 "fa18274f18fb0d3a2b3f5c360c24587b805db3f4734972c350643c35b8677174" => :sierra
     sha256 "42d033ae0a661d75b588af8d7c0cdb67a81bfc481aa88665973d95d3e4fb64ec" => :el_capitan
     sha256 "5456ec2b90086c84f2094972fa0dacc11de0abdde5346e3445a6b7d64b49201c" => :yosemite
