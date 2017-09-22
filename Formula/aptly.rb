3ff6b938b769f96db7f204931d619a86762cf19e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 00:19:05 +0000

    aptly: update 1.1.1 bottle.

--- Formula/aptly.rb
+++ Formula/aptly.rb
@@ -7,6 +7,7 @@ class Aptly < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d2364c7151cad362fc4ed4cc628aff6b3fecead9696276527f78034576ec5658" => :high_sierra
     sha256 "7b2b48ae572475397cf32490422b5ac16b806063bf0280adeadf7e9fd135e0f0" => :sierra
     sha256 "9586b3a09cb4267f928b045cb13f2e210a30b971b5e9b0e04575128e616789b8" => :el_capitan
     sha256 "764451d229c92b48a3df79e074b761df632da13e661955ab1a767669ca631662" => :yosemite
