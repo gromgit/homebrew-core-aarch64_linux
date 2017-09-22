9a51e9e5f360c1ba24c0eed3f023973ef6f1bcec
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 03:19:20 +0000

    datamash: update 1.2 bottle.

--- Formula/datamash.rb
+++ Formula/datamash.rb
@@ -7,6 +7,7 @@ class Datamash < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f80a0f35f13dc924a42977fd5d3b69e871ff17a51e7d91ffef93ffc1fe33e37e" => :high_sierra
     sha256 "b0eaec94032f69086bbbdcc7d38a59d7f03ba9511a8ce47a965fcf9e18229cb6" => :sierra
     sha256 "d310a35d4ad9760fb4b936429f453b30baf6c5d474983c3928344f8dca145283" => :el_capitan
     sha256 "cf022df27f4f7cf13b171e84d05816e8eddbae6c0190b0e339b269178d935ada" => :yosemite
