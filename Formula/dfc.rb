c54a8beee356615a4b0e265664c78308b9817fba
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 03:58:25 +0000

    dfc: update 3.1.1 bottle.

--- Formula/dfc.rb
+++ Formula/dfc.rb
@@ -7,6 +7,7 @@ class Dfc < Formula
   head "https://github.com/Rolinh/dfc.git"
 
   bottle do
+    sha256 "11d5691f5ca6020dfb6cdb084529c1d9caa4be674da27fa2d395adaccd799f56" => :high_sierra
     sha256 "029104b15817e039032593cb9828b76f42f4362b63eeb7d582c7d0d90fc917b8" => :sierra
     sha256 "bd407f57305b87838f0083c5a3d65493a76097fda65f4ffeebacfbcf33baeddf" => :el_capitan
     sha256 "ba254d9a1213beb84728657e996c667eb9a87f61ea118d12cf022b389a2e35b0" => :yosemite
