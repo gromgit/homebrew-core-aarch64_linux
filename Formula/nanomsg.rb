d247b9d9e3b4ab5abbad53da119346567338eb1c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 15:08:59 +0000

    nanomsg: update 1.0.0 bottle.

--- Formula/nanomsg.rb
+++ Formula/nanomsg.rb
@@ -7,6 +7,7 @@ class Nanomsg < Formula
 
   bottle do
     cellar :any
+    sha256 "45c5998dec7d19819b77cdb74671b55eb5c3ebaf8e785ce7582baf90a0f44dc8" => :high_sierra
     sha256 "6dc4ea79f3c05afd0e28c4323905ca3611e1cfd633de74d9e9d65f61bb3fd8d1" => :sierra
     sha256 "fb85b321fc5d554c0bfd7b31c53dd7e6496ff5ad6dd94111298f23f45c6b5559" => :el_capitan
     sha256 "255ec0f726980ca16320ec1795e81ce275d8da730c5034e16c1134981d044522" => :yosemite
