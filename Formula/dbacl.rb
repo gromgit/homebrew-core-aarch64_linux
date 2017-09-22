fb92e10c0f139e505c6cfddfaeca1144bfd0a91d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 03:19:20 +0000

    dbacl: update 1.14.1 bottle.

--- Formula/dbacl.rb
+++ Formula/dbacl.rb
@@ -6,6 +6,7 @@ class Dbacl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "42c1c03e8df0b4db91dc99ace3ec87f3901f1aa6975430d597240ab5f9182c1f" => :high_sierra
     sha256 "c6e6d74e2f2a86325ee895f8ef6893d99e1463d0018ead0d0da46e0dfd95c272" => :sierra
     sha256 "750c29761c5784ddbd0d46643f2d462d8b22c14822773e2366db01be17a3e310" => :el_capitan
     sha256 "b3d759bba5d1e478bc3df9e829594f49fb37cdd96e589937ebf4a7da87416855" => :yosemite
