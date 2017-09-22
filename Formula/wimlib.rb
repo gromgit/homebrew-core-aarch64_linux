7c48a1b3c97a06fe9aa2f85b7755b6365bcff09b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 13:36:31 +0000

    wimlib: update 1.12.0 bottle.

--- Formula/wimlib.rb
+++ Formula/wimlib.rb
@@ -6,6 +6,7 @@ class Wimlib < Formula
 
   bottle do
     cellar :any
+    sha256 "bf98257d6f32313b3a57713ae6b2ad7c3f72ddd5b1e1ed609436fb337ba51d63" => :high_sierra
     sha256 "32a00c25f98932b84ac5304df29eb5f6edea0a2fc3a2b33ee83938e92c488549" => :sierra
     sha256 "6a2b65020b31dbda4499bffbe773e5596dbc130d6f91ee84c9a7c532dd858594" => :el_capitan
     sha256 "c26d19bd6a6994fae60000f329d136c991b6a1172141c6c047792175a2c79439" => :yosemite
