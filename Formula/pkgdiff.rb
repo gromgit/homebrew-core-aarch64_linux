5da5f1435658ab63783f45bb985cfc0849e15087
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 08:38:16 +0000

    pkgdiff: update 1.7.2 bottle.

--- Formula/pkgdiff.rb
+++ Formula/pkgdiff.rb
@@ -6,6 +6,7 @@ class Pkgdiff < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "28ed130d50f00ec5617be39a8bc627591d310d47e8023844b54f27fbf0daede8" => :high_sierra
     sha256 "fc92f55909e0499c1d0054100183567465603ac85fa9f8b20d5ab1d84e36ceae" => :sierra
     sha256 "18895054433b4b050c3a863306c62a910be7fa4e36b0020a742c5c7541c0df65" => :el_capitan
     sha256 "c566b1a44ed89a1b7b3547adf0a0e0fa784174e1acc4a1dd46a240ea6d09bbff" => :yosemite
