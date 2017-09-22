0b7b252214d46fa25e0fbc0e961eaf72dcdf655a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 01:08:00 +0000

    autorest: update 0.17.3 bottle.

--- Formula/autorest.rb
+++ Formula/autorest.rb
@@ -6,6 +6,7 @@ class Autorest < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "63768f566900eee86561a21b495b3627183fb1c05db98220561d58dad0d7a5d2" => :high_sierra
     sha256 "da1dc0e3a25b005db13ffbb95b145c060162648ad700998e4814a7969e17cbb1" => :sierra
     sha256 "da1dc0e3a25b005db13ffbb95b145c060162648ad700998e4814a7969e17cbb1" => :el_capitan
     sha256 "da1dc0e3a25b005db13ffbb95b145c060162648ad700998e4814a7969e17cbb1" => :yosemite
