70e7a136d4c7979387593c3cfed719381a2f296f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 07:11:13 +0000

    pdnsrec: update 4.0.6 bottle.

--- Formula/pdnsrec.rb
+++ Formula/pdnsrec.rb
@@ -5,6 +5,7 @@ class Pdnsrec < Formula
   sha256 "f2182ac644268bb08b865a71351f11d75c5015ac0608a1469eb4c1cd5494d60d"
 
   bottle do
+    sha256 "545c4e165dabc5ab3fb46a568b25307b5fc63a3c5cee97657053af5cfa6c72bf" => :high_sierra
     sha256 "cc9312578fb77bed4d92725ccec1a49d40a5113743117aff240d7a7021ca015c" => :sierra
     sha256 "bd074b480630fc4dab01fe3886c2430dc3617aec6e34d6e68cc8cec84ffaebe7" => :el_capitan
     sha256 "efe5c4b834cb36612741b353c4705fda3510f42f8ee09265b4077373e7a05669" => :yosemite
