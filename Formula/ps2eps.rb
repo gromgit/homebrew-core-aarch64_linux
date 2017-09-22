3dd97acc04ac64a5a2decfe28709a38f94e6f07a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 11:19:01 +0000

    ps2eps: update 1.68 bottle.

--- Formula/ps2eps.rb
+++ Formula/ps2eps.rb
@@ -6,6 +6,7 @@ class Ps2eps < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4e0d89ac5be6cb6357d84988a1adc26e6667f4e9cdd8047071d0204cbecee58e" => :high_sierra
     sha256 "55396ec4ff00cfc85c4e34f1f7b872834264d8640677cd430c16b10fe67f2fa9" => :sierra
     sha256 "a651d45a267206348a36d213620790b0951e5343070d8613548b80066ec5a584" => :el_capitan
     sha256 "99b3838d2a7135d8794e4f48e428bd8afc0f18db8998f071c74faa449591ad7f" => :yosemite
