2c1f540c7a5a229767d51a97902d6e41f5a18ad9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 05:16:19 +0000

    dopewars: update 1.5.12 bottle.

--- Formula/dopewars.rb
+++ Formula/dopewars.rb
@@ -5,6 +5,7 @@ class Dopewars < Formula
   sha256 "23059dcdea96c6072b148ee21d76237ef3535e5be90b3b2d8239d150feee0c19"
 
   bottle do
+    sha256 "86b78c8cee8505ad3bde0e2d52bf45a60ac388735c034a1450d3be1117937749" => :high_sierra
     sha256 "2cd2bcd5c69422ea725622831b054ce4d75656f085e7e919d2a5a055fed30037" => :sierra
     sha256 "019168fe18bb28f596bad5858bfd782473800920d9bb5cf71ce94ac90c4fcf5d" => :el_capitan
     sha256 "282643cc528cca8f01101c4f908e07889585c07d60b2da31fde88b41a07c1c2c" => :yosemite
