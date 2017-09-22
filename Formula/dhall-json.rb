d3f8431c7b45a46818fdccd0c6f7b34433cb421c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 03:58:25 +0000

    dhall-json: update 1.0.6 bottle.

--- Formula/dhall-json.rb
+++ Formula/dhall-json.rb
@@ -11,6 +11,7 @@ class DhallJson < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "246ebfce84d0a72db29f8c9be7faa7f3ccc3762affea1a3d5d9800e3922f26a6" => :high_sierra
     sha256 "f4a963e1953a61fde793cf0e5b3f6f73908d04791954afd0a7d86ebd857f830e" => :sierra
     sha256 "eddcc81830adc1e0a602f6ea1d8db8b8d1d3e2709140e53101ac70aa592df32a" => :el_capitan
   end
