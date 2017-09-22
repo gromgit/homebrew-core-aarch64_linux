203b56b79b3932e2f194b778a55a554bd972e5d1
Alex Dunn <dunn.alex@gmail.com>
2017-09-22 06:40:49 -0700

    avro-tools 1.8.2 (#18396)

--- Formula/avro-tools.rb
+++ Formula/avro-tools.rb
@@ -1,13 +1,17 @@
 class AvroTools < Formula
   desc "Avro command-line tools and utilities"
   homepage "https://avro.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=avro/avro-1.8.1/java/avro-tools-1.8.1.jar"
-  sha256 "3afcf3252596d48cf04569fdcb04d705bf4310479b10b84f11b9d9cdfd00033b"
+  url "https://www.apache.org/dyn/closer.cgi?path=avro/avro-1.8.2/java/avro-tools-1.8.2.jar"
+  sha256 "8a1e2cc40daf89a29a30f15309077e5d0b9e0434ec5521a1fdd1d005159b89c4"
 
   bottle :unneeded
 
   def install
     libexec.install "avro-tools-#{version}.jar"
     bin.write_jar_script libexec/"avro-tools-#{version}.jar", "avro-tools"
+  end
+
+  test do
+    assert_match "Version #{version}", shell_output("#{bin}/avro-tools 2>&1", 1)
   end
 end
