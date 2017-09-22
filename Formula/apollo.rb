c8e8892cbd19d2ac407fbb08fecaaf37b24d439d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 00:03:58 +0000

    apollo: update 1.7.1 bottle.

--- Formula/apollo.rb
+++ Formula/apollo.rb
@@ -7,6 +7,7 @@ class Apollo < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "48b09eb2c2be0ed37a27b6b4d6835c5db6d80c877ea10e46296ddd17f8e646ba" => :high_sierra
     sha256 "1d4d6ac835aa8f72d8fb3084780e215986737c6609dff27a552730f2df9f5fc7" => :sierra
     sha256 "1521942c30bd7443a79d944c384391cea0944089a0242b89f31c2c2e4dda1e81" => :el_capitan
     sha256 "1521942c30bd7443a79d944c384391cea0944089a0242b89f31c2c2e4dda1e81" => :yosemite
