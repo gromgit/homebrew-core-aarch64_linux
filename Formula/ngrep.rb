969b64b48a45d69a9def5fb67e313343db3e07b0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 16:11:59 +0000

    ngrep: update 1.45_1 bottle.

--- Formula/ngrep.rb
+++ Formula/ngrep.rb
@@ -8,6 +8,7 @@ class Ngrep < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "2a8f05ed5acd9e0c44d72c10c8da509b34f27ff892ded76e4b80a974630ea66f" => :high_sierra
     sha256 "76d47db6c682e4e6791591bb0dc3316d8cc102163f064257490e738da04577cc" => :sierra
     sha256 "f4688b249e7038b5ee288af4cc589a22914f1e1f2ba943fdbbebd9cc8acc078b" => :el_capitan
     sha256 "e028924a9424a6f61a53c2cb850da0e1adfbde9914c63d85e0cae7cc9a88ed82" => :yosemite
