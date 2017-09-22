37478bf35c05a384d35ac7e9df8ae7c3af313e38
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 11:01:46 +0000

    poster: update 1.0.0 bottle.

--- Formula/poster.rb
+++ Formula/poster.rb
@@ -6,6 +6,7 @@ class Poster < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "74db7055649cd3f68316b99db48139641f916b4434008300f2bfcd1146f92c77" => :high_sierra
     sha256 "caa5474e5d7baf13ae6495c01a7530146d55531e41c88a469b0e44ee892c4be4" => :sierra
     sha256 "07702fc6f1d43a3875637f8ff9d3509d6eb913abda301c24c23d824a76a858b6" => :el_capitan
     sha256 "718131fa123a69d0db610d95722d968fbda597da2477abe520146393ff0321c2" => :yosemite
