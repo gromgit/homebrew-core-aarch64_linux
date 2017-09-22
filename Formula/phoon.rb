1bef286af2b3b3fcdfc6597ae63477d47f36d189
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 07:39:20 +0000

    phoon: update 04A bottle.

--- Formula/phoon.rb
+++ Formula/phoon.rb
@@ -7,6 +7,7 @@ class Phoon < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b02d96f879caf26f69d807a5f7e4ede911e3d8d954fa3ab943e27efe6da15814" => :high_sierra
     sha256 "ef7e685f7a9894e894807a555c4a9d52dd947184c15986f0a5578a46d892e46a" => :sierra
     sha256 "ae91a1dc089678416ddeab9bd3dfbe76f0fd9f4789a86b187740167d0658926d" => :el_capitan
     sha256 "d6f259769364eab6cacb4e45301f0ab8cd6edab369da99b4ecfbef7927791adc" => :yosemite
