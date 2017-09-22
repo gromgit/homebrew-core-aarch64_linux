3b24467172acf34fde9da42cf5b8942a8858ed2e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 04:48:05 +0000

    docker-cloud: update 1.0.9 bottle.

--- Formula/docker-cloud.rb
+++ Formula/docker-cloud.rb
@@ -6,6 +6,7 @@ class DockerCloud < Formula
 
   bottle do
     cellar :any
+    sha256 "809594637760e2118111760fee5b298ee528dd862f845055a8f0e2a03aee10b5" => :high_sierra
     sha256 "b5337ce18f9053667a61f4873e3d1e8b7f910b0a668505c853531be48b95428a" => :sierra
     sha256 "90b1feb11ccb54a1481a2c23df1c70762b346d0907b7f873296b70fb6774b763" => :el_capitan
   end
