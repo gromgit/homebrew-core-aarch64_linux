5dd9900231c0db2f6c1a4dd59d84b1f886fd979e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 10:01:10 +0000

    pmccabe: update 2.6 bottle.

--- Formula/pmccabe.rb
+++ Formula/pmccabe.rb
@@ -8,6 +8,7 @@ class Pmccabe < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "054dd89d0934715b169875d8d0bcce39db919550752eab9cadc083eab0e148cf" => :high_sierra
     sha256 "220285c0f0ae07835785574504d1d7730fb2abc06ddacfb76e1fe73f999d2cc1" => :sierra
     sha256 "d6189f6ae7341da933653c687adec0bb8952b14ed8a2883b19aec4db90b65eea" => :el_capitan
     sha256 "cb369d2f04ce0fccdb22b2640f1f6e37fc056b6edda79767474040cb52f76936" => :yosemite
