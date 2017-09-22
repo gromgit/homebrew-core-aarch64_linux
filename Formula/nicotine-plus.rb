4648b7fd4ca8d43a02e6f8fb035ce868ca347505
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 16:12:00 +0000

    nicotine-plus: update 1.4.1 bottle.

--- Formula/nicotine-plus.rb
+++ Formula/nicotine-plus.rb
@@ -9,6 +9,7 @@ class NicotinePlus < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "73e16deee0243324179c00aeca11081de205f546423d4b6ed9fea63fef714faa" => :high_sierra
     sha256 "ec2e9aafc0d57642a4ab6555be5793aa47b87587cb44ee9e6999db17efb00606" => :sierra
     sha256 "52a83f042b67882b63eab539f98180b6b9e95e5c7bc29ed76648195d95f89304" => :el_capitan
     sha256 "0e73dda61bb19d51bafec22e6e269f0df5e9aa85722dc4af194850099a63f8b5" => :yosemite
