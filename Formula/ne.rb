aefd3a9cc7a0f45b02c8b3c281cf07c97738989e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 15:39:47 +0000

    ne: update 3.1.1 bottle.

--- Formula/ne.rb
+++ Formula/ne.rb
@@ -6,6 +6,7 @@ class Ne < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ebf14e2778e53688a4c1c0051187b79994e9374fc599332b20b0b362d6517c49" => :high_sierra
     sha256 "1aee5fa253900a888bfa27d92a3b0e262a01acf03da2987285064c916105a388" => :sierra
     sha256 "7bdd3016890a03f6bc006d924cf2373a97b3915bf8d7ddc1a6bb81741085ecff" => :el_capitan
     sha256 "00d0ed886fa94db6b33f26dd304f468c79748379ac95c49a96141594fa0b333a" => :yosemite
