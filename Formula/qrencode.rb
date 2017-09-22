a4c396ca2c5723fa2396c798a38a28ffd4c7af48
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 01:31:25 +0000

    qrencode: update 4.0.0 bottle.

--- Formula/qrencode.rb
+++ Formula/qrencode.rb
@@ -6,6 +6,7 @@ class Qrencode < Formula
 
   bottle do
     cellar :any
+    sha256 "7e875aa1e56114fede796291220d623c7b7dc178ef74f4ba666d0c3c03be369b" => :high_sierra
     sha256 "df4727cb66c9ca501675f7b169ac9c211e7b766a7908c69ee6248536f810bd81" => :sierra
     sha256 "f9bfb2e870776c67d8cfb735ac64162de374d5d123f48e3e3da965cdd742d73a" => :el_capitan
   end
