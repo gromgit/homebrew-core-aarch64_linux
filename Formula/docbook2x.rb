37b84f5e8ea9540cd4a4953bcad96ec78e05a573
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 04:48:05 +0000

    docbook2x: update 0.8.8 bottle.

--- Formula/docbook2x.rb
+++ Formula/docbook2x.rb
@@ -6,6 +6,7 @@ class Docbook2x < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "81734088203909fc5db96462d14116596058910cd1b7ab67389a7bf93c9bae63" => :high_sierra
     sha256 "a1110d4bd90cecf9ce8edacc27a3edc84dfcd4db7ab50b67269af0eb6a9bb00a" => :sierra
     sha256 "acfdd1c80cb523b213dea0125819b1b6fc783d6d740cc8fc0047f44756b57889" => :el_capitan
     sha256 "e3efe4afe190e126174c6e3bec0a9feb4ad37ddd0ecaef778b1e8df8a60e8717" => :yosemite
