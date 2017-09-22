031d31ed81f8d9eed1f6d22e6decfe9cdbc7a5a0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 10:47:39 +0000

    prips: update 1.0.1 bottle.

--- Formula/prips.rb
+++ Formula/prips.rb
@@ -7,6 +7,7 @@ class Prips < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "10f3802a208d576fc43ef60bdc37f610690fc2db3fd30af433ecce7df2992e90" => :high_sierra
     sha256 "7fd356876cb6ae47363d09405ac14e2dd129e51ded5b81603e9028132b96b86c" => :sierra
     sha256 "70eaa7f0bc91f0f6fbea259a2db276e15beea29ff4bd391f545bcb76b0588af1" => :el_capitan
     sha256 "37b66f0c4ccee70d7495e9f7bf822446214df33798af61b3e89ead65929de68b" => :yosemite
