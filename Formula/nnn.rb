f58f4848b32672c2414839f72f46c3479b10a222
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 16:12:37 +0000

    nnn: update 1.4 bottle.

--- Formula/nnn.rb
+++ Formula/nnn.rb
@@ -6,6 +6,7 @@ class Nnn < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e8c2e37730942ff7e4f7df409557c77e8d50eb5d2719e874cf060015d102d1b4" => :high_sierra
     sha256 "1b9b77488be80d90fd6b90c7ed8c93df02cad18211321ba8199c629d693b5e27" => :sierra
     sha256 "a5c1d353bdb32b395f827a3b86da9fa3f7336737063531cc5862eaf80fff7d2d" => :el_capitan
     sha256 "d10bca08ca2a0dde952c1704eac053210a6e1a0bcb44daf7447866f15b8397ba" => :yosemite
