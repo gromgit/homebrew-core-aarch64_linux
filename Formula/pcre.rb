e12be86f1cf3fbea55fa5f3dda5eb21989ae3131
Viktor Szakats <vszakats@users.noreply.github.com>
2017-09-22 02:15:48 +0000

    pcre: secure url(s)

--- Formula/pcre.rb
+++ Formula/pcre.rb
@@ -1,6 +1,6 @@
 class Pcre < Formula
   desc "Perl compatible regular expressions library"
-  homepage "http://www.pcre.org/"
+  homepage "https://www.pcre.org/"
   url "https://ftp.pcre.org/pub/pcre/pcre-8.41.tar.bz2"
   mirror "https://www.mirrorservice.org/sites/ftp.exim.org/pub/pcre/pcre-8.41.tar.bz2"
   sha256 "e62c7eac5ae7c0e7286db61ff82912e1c0b7a0c13706616e94a7dd729321b530"
