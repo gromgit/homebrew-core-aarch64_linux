c024506e5c6968460659ffbad46c75ea9f4d9159
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 06:14:12 +0000

    dxpy: update 0.191.0_1 bottle.

--- Formula/dxpy.rb
+++ Formula/dxpy.rb
@@ -9,6 +9,7 @@ class Dxpy < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "73f52983798c560445f0a18ef35c9e830ad4faa8b5d4808511ca09928f80ffc1" => :high_sierra
     sha256 "526ed51259393fb569a912b8865e1627af6ebae9bccd460d33cb403027c66e52" => :sierra
     sha256 "08e04fb3cdca5a1d50229e76713fd6a7f384996ed1e30c97a5260118dbf75a91" => :el_capitan
     sha256 "b5a5feb625e40d2bffca2a5be08bd4977a55129d7c9925129b50c51b782f5399" => :yosemite
