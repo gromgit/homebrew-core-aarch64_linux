af15c72b54b4d126f80ce0e32464230e98d49291
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 15:10:47 +0000

    ncompress: update 4.2.4.4 bottle.

--- Formula/ncompress.rb
+++ Formula/ncompress.rb
@@ -7,6 +7,7 @@ class Ncompress < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a690145266b23c89bc3d9762cc5f632bb15f685171f939108c1eebdda06a57f5" => :high_sierra
     sha256 "631c12b675f730ea3a7a170b214f63b1749fabb2d632ce76d76de4c8706cdf79" => :sierra
     sha256 "a046e625feb4e7dbf26136b6ba949f9883de14674571f2291794daf98106558f" => :el_capitan
     sha256 "613392aa88dbf86d8de3d6355dfeb72753a4040fd8c805907f434a7d1e5e78c4" => :yosemite
