2b5c1dbf7ed3a3151f1d18e510f47f8e18c27f43
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 09:30:26 +0000

    platypus: update 5.2 bottle.

--- Formula/platypus.rb
+++ Formula/platypus.rb
@@ -8,6 +8,7 @@ class Platypus < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ab371d96215d160ee7638d1dfe3d49cc5cbbf7fc03b01524f3fe18bb7c6ed767" => :high_sierra
     sha256 "11b199eb483328298a49792055306558182807fdf993b448c2f88145b57b14e0" => :sierra
     sha256 "edb6178b284a8701fb4e7e57ff57230269a774afc684776a557fa2572c2ac057" => :el_capitan
   end
