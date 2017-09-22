4593b38c1f513115bc4fba5d100f26be73be77c1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-22 11:01:46 +0000

    ppsspp: update 1.4.2_4 bottle.

--- Formula/ppsspp.rb
+++ Formula/ppsspp.rb
@@ -9,6 +9,7 @@ class Ppsspp < Formula
 
   bottle do
     cellar :any
+    sha256 "12b1eeaa7f1af24c9cb890752cec4a1c5780e5aaf5a1d6c9197832240e46d9d3" => :high_sierra
     sha256 "9ac8b7cdebd3383d57729af3632f14ada8916044ec323ee5d37e7019f04cee1d" => :sierra
     sha256 "f0336b16da129471ecabb39c345831606e7debd6d730d2e59ef895431051f629" => :el_capitan
     sha256 "5af87bf1ef46049413d62f8dc2745911096408dbfe201acee86d64e2d39739ad" => :yosemite
