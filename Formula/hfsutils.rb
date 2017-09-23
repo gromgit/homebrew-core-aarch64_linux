0cd0392a3307830e5b82eb8c27c404be2ae1afc6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 23:11:56 +0000

    hfsutils: update 3.2.6 bottle.

--- Formula/hfsutils.rb
+++ Formula/hfsutils.rb
@@ -6,6 +6,7 @@ class Hfsutils < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1a0fd0b0ac3529aac6c79b1f3b15fbeefa2cf05838de439929b8c5c61d49c077" => :high_sierra
     sha256 "4b9c18851c1fd5ce7049946cb583d4f8336c29bd48c76690df707c768a2879fd" => :sierra
     sha256 "61847361f9dac3c719ae8fb464fb9e45d7b64054c7d3c2ff23b37a698546f63d" => :el_capitan
     sha256 "2d0997b77b2bc7b3a0454c552c6ebd3b24c6efc01bc9e4814781f7971c8802f9" => :yosemite
