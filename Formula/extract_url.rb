0b076adeb88ff7e64a958e6a2bba9458554d2629
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 07:00:57 +0000

    extract_url: update 1.6.2 bottle.

--- Formula/extract_url.rb
+++ Formula/extract_url.rb
@@ -6,6 +6,7 @@ class ExtractUrl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2880b669c381e7c7a2420d71c673d68d988223dc63bad9f14b1c62495973f362" => :high_sierra
     sha256 "57b556a225f6ec03cee7166c1b4cbd2eb1c0eb2bd7819865bd9ed39620b81b68" => :sierra
     sha256 "96d599a0f724f6f09e261c8b0a1c8bbf69ce1b199d311527636f8a5d42f197c6" => :el_capitan
     sha256 "d16fcc4c81a2ffb7f384f104396aae674bb8f6f08d336056ab858924d545f205" => :yosemite
