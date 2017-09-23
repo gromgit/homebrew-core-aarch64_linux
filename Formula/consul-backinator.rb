5624a5e24117e11c4970fcb8ea2fdc7963ba1a34
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 16:00:28 +0000

    consul-backinator: update 1.6.2 bottle.

--- Formula/consul-backinator.rb
+++ Formula/consul-backinator.rb
@@ -7,6 +7,7 @@ class ConsulBackinator < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "619890e87ca8c4e999eef757921877b9681b8280275ddb3129cf5e8719b67384" => :high_sierra
     sha256 "354c3497e33d5c09e7b0d6aa635af387412facd8d3bd700781714199aea29a65" => :sierra
     sha256 "13447c998681eb2f334af03fd14b5fa6566c4c728e0f3083f0897d3ad33fcafd" => :el_capitan
     sha256 "efe3d460c5c8ed340aaa876d02c3b4463a22562370ddec6fc9ff9282f5ed5060" => :yosemite
