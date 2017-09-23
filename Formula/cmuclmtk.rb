0002875a95cc241214b85c50000fb69eedc3352b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 15:48:55 +0000

    cmuclmtk: update 0.7 bottle.

--- Formula/cmuclmtk.rb
+++ Formula/cmuclmtk.rb
@@ -6,6 +6,7 @@ class Cmuclmtk < Formula
 
   bottle do
     cellar :any
+    sha256 "85a6d2a8fcad4e2b6e7d9d22ec74dd5e5f463dabc5b2f01373d3a48178b2ce6e" => :high_sierra
     sha256 "716c78af6b276392a20fb02d58ff60e705509117da932d62d3ff8c6e4dd0bf5d" => :sierra
     sha256 "c647327d709c3b4a93d5541f8b340d2726540c9efdcbc53d1124043c8c4989bd" => :el_capitan
     sha256 "320a3590af1e9a1bee6827eb71e4d91fb283952f178b7f0393406a120046d4ee" => :yosemite
