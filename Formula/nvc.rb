ff3f73515a4455ee37a9e4919d6702ea87f42b23
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 08:10:36 +0000

    nvc: update 1.2.1 bottle.

--- Formula/nvc.rb
+++ Formula/nvc.rb
@@ -5,6 +5,7 @@ class Nvc < Formula
   sha256 "ff6b1067a665c6732286239e539ae448a52bb11e9ea193569af1c147d0b4fce0"
 
   bottle do
+    sha256 "f11933a05847b9433fd505c03755767043872e145f247b7d87603e3a9dc51dc4" => :high_sierra
     sha256 "23b8d156e8ee517b80e7736eeb31bfa3ac59e30bbdca157b53320b8cf987b633" => :sierra
     sha256 "fec8eab13df57ffec18283f6a83366afcf431640f9e25142a4da04f63e43e2bf" => :el_capitan
     sha256 "f33ffc17ef6123f97750b76fddedbb6ba3865fab02ebbca04c7441631740092d" => :yosemite
