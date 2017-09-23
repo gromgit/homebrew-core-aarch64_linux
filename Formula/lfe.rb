c561449787e276ff7d9f1ed5606deade405253db
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 11:56:27 +0000

    lfe: update 1.3 bottle.

--- Formula/lfe.rb
+++ Formula/lfe.rb
@@ -6,6 +6,7 @@ class Lfe < Formula
   head "https://github.com/rvirding/lfe.git", :branch => "develop"
 
   bottle do
+    sha256 "1a0f582845e8c0c87331c9e9148b06dde79483847cc1bb31674596fe2d3c3422" => :high_sierra
     sha256 "a6f27b9dd837d866fb471db3556677112c4f8ca3df386596f975083a939c8a16" => :sierra
     sha256 "47827019926bdbfb6b0fa0c7fe123b0007482670aa651408e736f00c4796ce01" => :el_capitan
     sha256 "aab3e33761e9db3c4e5cceb8769edca70f2eb618e0bed5e3658ab2fdc3bae2ac" => :yosemite
