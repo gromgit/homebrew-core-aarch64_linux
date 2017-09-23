e01489e3e43f49dcf9f0e674d7d7a705bdf69cb4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 08:10:35 +0000

    ntl: update 10.4.0 bottle.

--- Formula/ntl.rb
+++ Formula/ntl.rb
@@ -6,6 +6,7 @@ class Ntl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "dc9569041867dd7657d46515f7624fb469da2e07d02524a69ef70d4aa409ef2d" => :high_sierra
     sha256 "72c79a85c49ce2f21d8c051cf8fa89e51fbe5a433192ed0d1fcb3a351070a3cb" => :sierra
     sha256 "90ad337da72230a12d67e963656e28dd42af80d0410a0d1fb62c81db7ef9372d" => :el_capitan
     sha256 "a4688eec8932344ed3b399239271a1dbc0a4ee9df0ebec5413dded452e90a9ad" => :yosemite
