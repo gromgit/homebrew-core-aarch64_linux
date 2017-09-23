35511205f87f9a4d7e48eda5ca02d5956264e6a8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 20:10:58 +0000

    inform6: update 6.33-6.12.1 bottle.

--- Formula/inform6.rb
+++ Formula/inform6.rb
@@ -10,6 +10,7 @@ class Inform6 < Formula
   end
 
   bottle do
+    sha256 "99fbc0e56d7a8eb8b5dad3c7604332a04286908cbfe51c385c52b958a058ff37" => :high_sierra
     sha256 "9ba31815431daff2d34c193bc48acaeb7d97cf312162efd809a28ea53c0e3e04" => :sierra
     sha256 "7907b51b7c5b95d258a0418e6c8a455b814266ca72498d28573b9021761a57e2" => :el_capitan
     sha256 "517cb99fc16eb03a54deb203c90c891982d6bbcaa1a135dfec4f8b4bb79029ab" => :yosemite
