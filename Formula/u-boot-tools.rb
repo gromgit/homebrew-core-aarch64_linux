b196ae948df3434558d2ac436c9c4901b417f7d9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 22:51:12 +0000

    u-boot-tools: update 2017.01 bottle.

--- Formula/u-boot-tools.rb
+++ Formula/u-boot-tools.rb
@@ -6,6 +6,7 @@ class UBootTools < Formula
 
   bottle do
     cellar :any
+    sha256 "69accce4faa7c9375b8366906f1144e7da4f32e6d7f8df25c4c9bb3eabfd9f2d" => :high_sierra
     sha256 "5294bc120061f782522562f4c52c2a3b101a5cb995b48048efcbda69a7f2c5cf" => :sierra
     sha256 "8d9aeda40aba937d4363192d926487425c940d695b16d19bb23e07394f63d756" => :el_capitan
     sha256 "51e824bfac4532dfffda4a44e91cee69e7b6504897f4c184c36ce253ad2ee738" => :yosemite
