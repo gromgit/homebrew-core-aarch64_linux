95d9ecd90ee48c3f135039924082a71b0b202890
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 22:38:31 +0000

    unrtf: update 0.21.9 bottle.

--- Formula/unrtf.rb
+++ Formula/unrtf.rb
@@ -8,6 +8,7 @@ class Unrtf < Formula
   head "https://hg.savannah.gnu.org/hgweb/unrtf/", :using => :hg
 
   bottle do
+    sha256 "20b25e70ca6dce766d851b7bafef64adf81633ff8cf6cb5ed97c70a4abbc3c60" => :high_sierra
     sha256 "6d305effeb3f7b8196db7c0746c2efb3170a809186916d7380ee35390cc9786b" => :sierra
     sha256 "2d658e54c0f66ae90764c8588fa7181c68d69d505336747b9bd5e496ba7b99d6" => :el_capitan
     sha256 "42737f31a7ea06592c2ad22a48f0e2537c0cd025129870399bd4f0fbe7362a98" => :yosemite
