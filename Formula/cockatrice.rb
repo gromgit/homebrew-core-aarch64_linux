a04683de97dcedae3f5700fc9fb0c9960e34bf17
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 15:48:55 +0000

    cockatrice: update 2.3.17_1 bottle.

--- Formula/cockatrice.rb
+++ Formula/cockatrice.rb
@@ -10,6 +10,7 @@ class Cockatrice < Formula
   head "https://github.com/Cockatrice/Cockatrice.git"
 
   bottle do
+    sha256 "50874237da97941ef1d5a17bc855fc836e8ec2494d50dda1772a7045a171356b" => :high_sierra
     sha256 "714abed702cc11fe15727278f9feca8d3c231444c8894c5234678b95bd793d85" => :sierra
     sha256 "f72457bf545e44d9b8b9252d8459bfc16f1eb00d3242b6649508053f158b4dc6" => :el_capitan
     sha256 "a56d73a6cf4bf1ad4dd39747fdc2afc757be2fcd6f86997a4b4f8a752a47f8cf" => :yosemite
