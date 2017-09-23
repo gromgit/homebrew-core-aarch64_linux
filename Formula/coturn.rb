bdf9ce4fb33f942898864c5e6e5fa603c0a6614e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 16:32:38 +0000

    coturn: update 4.5.0.6 bottle.

--- Formula/coturn.rb
+++ Formula/coturn.rb
@@ -5,6 +5,7 @@ class Coturn < Formula
   sha256 "c0ff3224084ff9a9504147a7b87431ee815ebeea0de7c7cb67126859da7e25a6"
 
   bottle do
+    sha256 "4223eac517f9c5499d2fa27b2d8c42d2e8f6e6c27e3fabffb4e671a5f90196bb" => :high_sierra
     sha256 "1284bbf2a86fd2309901f357cc957de4d21156ab0c5a7c3e4c3e7d7bdaa39beb" => :sierra
     sha256 "dc713646ee013d0794759d1b540e7b2f6b458224b17623974283e27818449364" => :el_capitan
     sha256 "7dd68ad44ea4a7f69030bdf167b28fc854d56331b92d06798cb6dd4347043677" => :yosemite
