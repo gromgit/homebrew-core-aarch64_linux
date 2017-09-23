c9797afe3db5cd54fb691801fc47949bbce1648f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 23:28:33 +0000

    htmlcleaner: update 2.18 bottle.

--- Formula/htmlcleaner.rb
+++ Formula/htmlcleaner.rb
@@ -6,6 +6,7 @@ class Htmlcleaner < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3151b50796233ec0632ce0cb481d7a2960ff6858672c0c8b11766641d0e0004a" => :high_sierra
     sha256 "87838ab8f3acda2911416178fd1bdf11d37ac7d3b6e21007f6218d1ad7e7139b" => :sierra
     sha256 "cc0afb1dd3c56cd78700138590142baa370480ff979ff757a4b5f7a18f66219c" => :el_capitan
     sha256 "2082bbebc107e771ed502971cec401c9b23b5c977c2fcc9324cd54c28f78f5a8" => :yosemite
