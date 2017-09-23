4bef99d6a346abf004ce4fcb0c66a37293255479
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 05:39:41 +0000

    openslp: update 2.0.0 bottle.

--- Formula/openslp.rb
+++ Formula/openslp.rb
@@ -5,6 +5,7 @@ class Openslp < Formula
   sha256 "924337a2a8e5be043ebaea2a78365c7427ac6e9cee24610a0780808b2ba7579b"
 
   bottle do
+    sha256 "3a933a2c697a2b7a00d9b1f9cc3a58664c43c18f7b4ff3d99afa7bc11d721da5" => :high_sierra
     sha256 "fdd847dba24e5a96c30ccef98f0d035f39abc88617d779df627c132be5b648ae" => :sierra
     sha256 "1c19d8355ddda63b9259101a0b7b56ea0fd9fb8f343e2df19f7248542fbf38e5" => :el_capitan
     sha256 "95e41f7f42e80ab3234b460d90196389a0d275877195fe188ffc6249c0b762ce" => :yosemite
