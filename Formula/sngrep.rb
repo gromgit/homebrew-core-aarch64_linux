4e129d0d1774e8b04341334534990424f51ab994
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 20:54:46 +0000

    sngrep: update 1.4.4 bottle.

--- Formula/sngrep.rb
+++ Formula/sngrep.rb
@@ -5,6 +5,7 @@ class Sngrep < Formula
   sha256 "2379b8b3e9498d426a0bc03b90d74170a80f98f167f89c126d53dcc66bc5f60b"
 
   bottle do
+    sha256 "035b453e0a13dadfb3c23b55b57c923f28f4940aef05dd93cdb2f591fe416929" => :high_sierra
     sha256 "68d0733f5e1b07ebb3e63f8a7f72cfccbb196477a460d545769d21eab6568ff4" => :sierra
     sha256 "bbfd86b19188591ba77f89ef44dd4bd1fcbcc60d2bc709a3b1dfc5e538f17086" => :el_capitan
   end
