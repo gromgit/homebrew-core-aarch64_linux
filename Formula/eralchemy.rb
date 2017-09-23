80dc8e95bc14ece0ee3a5a8d351fa419b398f76d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 06:45:09 +0000

    eralchemy: update 1.1.0 bottle.

--- Formula/eralchemy.rb
+++ Formula/eralchemy.rb
@@ -8,6 +8,7 @@ class Eralchemy < Formula
 
   bottle do
     cellar :any
+    sha256 "fec3c64abf16687569a65989cabf5b679b304bed6e3d4500e7473018eb48b463" => :high_sierra
     sha256 "d6f58baabdf8db3224ff90ded790a18d805571e31d4a82c323d6b2b7a5af654c" => :sierra
     sha256 "35b67ce15da8c1189760249239518bcf56332674bf7def6113adcd57b68eb503" => :el_capitan
     sha256 "2b1556bf861cbc976d5eaa6f7cf4282bebf34b9f387dbaa29a7d7e1527a9d344" => :yosemite
