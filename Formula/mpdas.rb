88e3a64914c774da73886eefcd2c86bf6254734d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 17:37:00 +0000

    mpdas: update 0.4.4 bottle.

--- Formula/mpdas.rb
+++ Formula/mpdas.rb
@@ -7,6 +7,7 @@ class Mpdas < Formula
   head "https://github.com/hrkfdn/mpdas.git"
 
   bottle do
+    sha256 "80ebc0ce2fc3fbffb04bc077b7d2d42c6d6b70ed846c97705b4dcbdcf8a866bc" => :high_sierra
     sha256 "111a229f7c41d93993bca09e694c7dac91750f5d964e7024b621dcc1eb775548" => :sierra
     sha256 "d7bf128e8d9570c2a59ad0aacfd7a18d8686eee2e00ce6bbf1594baeb816f77a" => :el_capitan
     sha256 "1f78b379b6f3683ed0c95f62f146f484775a66ecd3afb852d72982aa21e6f13b" => :yosemite
