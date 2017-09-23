d81fd4e64b4f8b2c80b94921321088e9444c2ad9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 20:16:39 +0000

    ipbt: update 20170831 bottle.

--- Formula/ipbt.rb
+++ Formula/ipbt.rb
@@ -8,6 +8,7 @@ class Ipbt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ca5fe4bf38c28b6134ef995643328af82277bac9765064cafa95c7be096cbb51" => :high_sierra
     sha256 "f9dd24a2a7bd67ee00a4763b6931975f27443d906d91795962ca8a1eb647d269" => :sierra
     sha256 "3ebaf07effeb98490201a2c8cc38bfdaec25c0db5a6bf3f2936f72b944ee5309" => :el_capitan
     sha256 "511b256cd2f9eda3db809f2a46c47ed72c7a4fadef4fb16bb5197766cc966fe9" => :yosemite
