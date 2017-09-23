b21a12580b83e87a62618c89ae24b18f4fb236ab
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 06:28:38 +0000

    eot-utils: update 1.1 bottle.

--- Formula/eot-utils.rb
+++ Formula/eot-utils.rb
@@ -6,6 +6,7 @@ class EotUtils < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "56f30e1b637149b8b34d003ff4c43865175950eb25d528e0cda69dd4e9261b06" => :high_sierra
     sha256 "320909b9801c96b10491dca13de7c793dae8b0d0864839c6b7a65cbaa1e8e036" => :sierra
     sha256 "b2a4e0f385fa861baf54ac3c483f5599bc96994b3797fe00430653f1a5c28ba4" => :el_capitan
     sha256 "3276e755d84fda54851733b693e56922ddb597f1ac4f14792f4221ce794832da" => :yosemite
