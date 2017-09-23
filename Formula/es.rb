aa73365bf5dd87758b27e7d2be3de39946c8d6cc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 06:45:08 +0000

    es: update 0.9.1_1 bottle.

--- Formula/es.rb
+++ Formula/es.rb
@@ -7,6 +7,7 @@ class Es < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b8fdbd8dd642283af492c5f6c72d23b39cc9819f543908c0d3d3c2c71d86151d" => :high_sierra
     sha256 "7fa9e3a21662d59d5505fda8945435f71331647d6efdae0ce7112bd507af3398" => :sierra
     sha256 "53f0c0654ddecc62b99135fb96722b6467593248d884c1506ea93681367c611d" => :el_capitan
     sha256 "bceadf8a0ef4d3231d149f30cc53d2c7fe686cd8fdd0dafe83ceaabc35151d5e" => :yosemite
