b0ceee46602455822c0adedd9b9dd82aa924bd81
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 22:14:33 +0000

    freedink: update 108.4_1 bottle.

--- Formula/freedink.rb
+++ Formula/freedink.rb
@@ -6,6 +6,7 @@ class Freedink < Formula
   revision 1
 
   bottle do
+    sha256 "d78130e6917a14d7a5e367bada96e919915695d68d7796709d5bb99fbbc593f9" => :high_sierra
     sha256 "c3d0467dd6eb6e2070e488d468cf3953257260401722dd28037a2b37326d604a" => :sierra
     sha256 "97ba862a21ab764b5cf1c3c5d40604c8b006c81a88839c20199966a494725c16" => :el_capitan
     sha256 "e4fa882a22081243a88ca1303f4aa7fe3843e4cc523fe973a5158b117e7d868e" => :yosemite
