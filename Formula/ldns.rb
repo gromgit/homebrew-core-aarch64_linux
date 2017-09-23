85f4fb2f9d4b2ee3f80faf8e04957442fe280e36
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 11:56:28 +0000

    ldns: update 1.7.0 bottle.

--- Formula/ldns.rb
+++ Formula/ldns.rb
@@ -5,6 +5,7 @@ class Ldns < Formula
   sha256 "c19f5b1b4fb374cfe34f4845ea11b1e0551ddc67803bd6ddd5d2a20f0997a6cc"
 
   bottle do
+    sha256 "e6e49e3a79436caa5d07e8fe7ff8a3116cdccfa78a2005d8e0fb25b1f5b13a9b" => :high_sierra
     sha256 "294b0901da29bba11010f1ac2716ab111cb4fcc283779264cb857c1057270ebb" => :sierra
     sha256 "bee97f2127cacd4fef3fbd77d00b0bbfb4f2afc738c8242634b2de2e90ac4c8a" => :el_capitan
     sha256 "60d00b07a87aacace15aa4ac992e45e21b0d3827658c588e15643d3434f30304" => :yosemite
