bf5ceb3776333468b5a2840aebb8e8bc0893484c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 11:56:28 +0000

    lhasa: update 0.3.1 bottle.

--- Formula/lhasa.rb
+++ Formula/lhasa.rb
@@ -7,6 +7,7 @@ class Lhasa < Formula
 
   bottle do
     cellar :any
+    sha256 "36f6530ca2f2908bed047741ce52e41f4ec0d0d726bdd8ecb664958da821b527" => :high_sierra
     sha256 "d0abfc9315cfeff37781861e8c7ba2d3eb34003684560ee22a5dfb2acc4dfd5a" => :sierra
     sha256 "0d407f1058853c656a4aef717c1e72ff57472e0622fb344a5ef57c4c9ad8c3ee" => :el_capitan
     sha256 "afd0b2c24f4e4103c6ab8e918a972fda1b614890fc527bf314cc253e199013ee" => :yosemite
