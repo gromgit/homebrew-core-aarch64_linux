5fe5bc45725db5472e01daf7935545649d3f8af0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 21:27:48 +0000

    stoken: update 0.91 bottle.

--- Formula/stoken.rb
+++ Formula/stoken.rb
@@ -7,6 +7,7 @@ class Stoken < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "ee631ed7b16976bdb280c5538f9ff73de447cf98f88793284f4cf3952c634cc0" => :high_sierra
     sha256 "b818f8cfd4afba73a8a123dfa2dc7dc7b2c066c9dce164ab853a5a5a8b5b31de" => :sierra
     sha256 "a14908e16d1d104e46f9f606fd5a79514b08ad982a7cec5370ebad5072309b4a" => :el_capitan
   end
