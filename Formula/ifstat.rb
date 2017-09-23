145f8da7a30d47f4671e71e8d7a7b0e55c1229da
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 19:55:58 +0000

    ifstat: update 1.1 bottle.

--- Formula/ifstat.rb
+++ Formula/ifstat.rb
@@ -6,6 +6,7 @@ class Ifstat < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8e385a8fdf00266e63bd7a3af17cdcf07da3cb86a08bd3431cfa3b4cc88bb7a5" => :high_sierra
     sha256 "99eada14bfc555bd8f64d447bddd8a53c7325afed40446e5edbcfb5d7bdc7dd6" => :sierra
     sha256 "e3b3f843c9fba2770a49dd7abcdacc30aa6b5e57f06b5ed96f09d20ada58bd6f" => :el_capitan
     sha256 "ecce408a9ae1a82c7b2457e5a5263ec760096e7e0e71cf0da1ce98523787bcbc" => :yosemite
