17ccdf1df7d59c3adfe86718aa4cdc354f388c96
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 04:52:59 +0000

    tbb: update 2018_U1 bottle.

--- Formula/tbb.rb
+++ Formula/tbb.rb
@@ -7,9 +7,8 @@ class Tbb < Formula
 
   bottle do
     cellar :any
-    sha256 "9b68623effbb821670a5bfc1f93c160b21ec0bc1d55380625e2cb46aa1eac927" => :high_sierra
-    sha256 "ad77508883c17c264e3a7b8577efd771828cf02b9d1f8a4ba903b2e26be66422" => :sierra
-    sha256 "fa092abc70c26eb61072eba47ea51e4767a2a0c64d450e1e9993dc3addb87c5f" => :el_capitan
+    sha256 "7bb00f4041fb7945c675d58f6d0fa121ac28fb249a211567c0eade71d9e018a0" => :sierra
+    sha256 "b9ee2339d50a595189f9b9e9612b0cbf22f84572bf0a2de4081db698a2766de5" => :el_capitan
   end
 
   option :cxx11
