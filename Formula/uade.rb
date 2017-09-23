bfe262cbf08ec65e7586e5d846a35b150c2c3eb2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 22:51:12 +0000

    uade: update 2.13 bottle.

--- Formula/uade.rb
+++ Formula/uade.rb
@@ -11,6 +11,7 @@ class Uade < Formula
     patch :DATA
   end
   bottle do
+    sha256 "226dc1fbb9535b64f19e04310db19fb9a760024fc0b0c73e3c68cf7c72e508d9" => :high_sierra
     sha256 "432a5f95b33416c9bfc29ef4d81ea6d4fab2a568c71c00a9bda034985ed1276b" => :sierra
     sha256 "59ddaa5a6d841f436a5d297330ff62b613e446785ad17666c8fb4157d3a7c8db" => :el_capitan
     sha256 "454945f35580b0b2bc8f0c7ddeecfae091634f54ee3a367eb14acce7251e5779" => :yosemite
