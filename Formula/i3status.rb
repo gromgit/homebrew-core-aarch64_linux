fc9ed4eb52a83ef6045b152f4bddcce64cbe1c8c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 20:07:59 +0000

    i3status: update 2.11_2 bottle.

--- Formula/i3status.rb
+++ Formula/i3status.rb
@@ -8,6 +8,7 @@ class I3status < Formula
 
   bottle do
     cellar :any
+    sha256 "71f766c26a53b3797a42c8fe02210e92e1d0af31b482704a41a018149dd5649d" => :high_sierra
     sha256 "09050200d7660dcb5ab2b99684dd406978abf7738e609beeceec4cbef0761a6f" => :sierra
     sha256 "cfd08ff2e313ae35000904e9f3e8c7d471f781578b9b899d6f9bd8ccc225748e" => :el_capitan
     sha256 "b5f41db83b1b1f6b000b8de9e4499b2dffba1ceca33edbb9635d671d223ffbe8" => :yosemite
