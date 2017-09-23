ef3aaaace307b855690179d705927c9699515f8b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 18:49:43 +0000

    klavaro: update 3.03 bottle.

--- Formula/klavaro.rb
+++ Formula/klavaro.rb
@@ -5,6 +5,7 @@ class Klavaro < Formula
   sha256 "e0959f21e54e7f4700042a3a14987a7f8fc898701eab4f721ebcf4559a2c87b5"
 
   bottle do
+    sha256 "0194576e53ee64aa8b92fa4a8e2ff9e76dd4dc29016518128a4b7f70c19c51b5" => :high_sierra
     sha256 "4024187379c8dced802206da91fe6d771e618f6eeaaecf327dfd114908ece438" => :sierra
     sha256 "1fc81369acf6063bbd0905976443a3dff4bc053c4ae975675b9ff6ddfb00f10c" => :el_capitan
     sha256 "ecbf95acf3edaa13be06446c8e00a3236b4cd48417e6feeabe0f67363b721d89" => :yosemite
