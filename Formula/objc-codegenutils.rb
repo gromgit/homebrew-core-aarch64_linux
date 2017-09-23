5b842a2ea80302efe8bddbee093a34348b28f0fa
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 08:10:35 +0000

    objc-codegenutils: update 1.0 bottle.

--- Formula/objc-codegenutils.rb
+++ Formula/objc-codegenutils.rb
@@ -7,6 +7,7 @@ class ObjcCodegenutils < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "118c03e858a60fa17c71fbc84fb5a8b9c5f778a0c68531e3df576e1d85d9c91a" => :high_sierra
     sha256 "d7b3d3d26970add3af78b0820f3ef8b5e0290f1b2114f5bf06acddcd8d6bdb34" => :sierra
     sha256 "d7b945db595b07ee5677902586e01002ba555affdcae366f1fcbe919a6013772" => :el_capitan
     sha256 "46d389e6ec12462dfbdd97822ce7c6e8156bbe9fac7a3baf04c20cb1991d9f75" => :mavericks
