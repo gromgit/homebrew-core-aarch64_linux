ffe8bd71d4e52dd87e2a940aa7dcecb5990091fe
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 20:33:28 +0000

    ironcli: update 0.1.4 bottle.

--- Formula/ironcli.rb
+++ Formula/ironcli.rb
@@ -6,6 +6,7 @@ class Ironcli < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5e72dc98b8ae2a512d8610d3949cf1fa383d30494af3fced6af2a22d2df0bd34" => :high_sierra
     sha256 "a243445511d08268bac87dc4ee042366e9cb7ea5ded3a011431150b06b4dc894" => :sierra
     sha256 "0382a73bd8168bcc4dfefb7710e7d377f62e49e15b83aa6ded26284a51be63b8" => :el_capitan
     sha256 "2525bf5e2917ccf70c17c920739aaac88353390ba7ad15068c3963d2c5838389" => :yosemite
