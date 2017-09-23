d61c33ea1e511fdd57af30c50d657568b1e583e6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 16:32:38 +0000

    cpansearch: update 0.2 bottle.

--- Formula/cpansearch.rb
+++ Formula/cpansearch.rb
@@ -8,6 +8,7 @@ class Cpansearch < Formula
 
   bottle do
     cellar :any
+    sha256 "2e48150a0d449239bd223050d2bc3a9695c461543dc6e53d606292c8797afb1a" => :high_sierra
     sha256 "7806b7b02a7bd6e578a7cbfc41935e854ec91c1174722bcbcd45f2716be31174" => :sierra
     sha256 "f68927e2f114cb09d4c5f7057097f4685139dc16a58306b572b011dc11e5b27e" => :el_capitan
     sha256 "554213c4d54b3bebfabf4eb10d274a9f86ba6271607b464a74541593cf52b8ac" => :yosemite
