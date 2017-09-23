704ca393c67c2595b82e0cde1d81cde7dddd7e29
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 13:50:45 +0000

    lv: update 4.51 bottle.

--- Formula/lv.rb
+++ Formula/lv.rb
@@ -7,6 +7,7 @@ class Lv < Formula
 
   bottle do
     rebuild 1
+    sha256 "912eaa08af6da7ddba73f4169695073614641d67561f4e632e47960f0c07c6b3" => :high_sierra
     sha256 "01c44c5b3d18aa1602c00bc3ce8d0b71ae02cee6dfcff66d7e8df74b424b8de8" => :sierra
     sha256 "49ad4ebf6830c1ef3f6899486e711f99bc293d422317f8851f174cf18de2a98f" => :el_capitan
     sha256 "f31281558dc9da38402a86b2b3c03efb10ab471561bf72dd556c3cd8df23ba14" => :yosemite
