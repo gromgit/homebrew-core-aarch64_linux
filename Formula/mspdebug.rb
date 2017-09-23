74104d81f01370235d785b652477a5615f60a70d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 17:52:16 +0000

    mspdebug: update 0.25 bottle.

--- Formula/mspdebug.rb
+++ Formula/mspdebug.rb
@@ -5,6 +5,7 @@ class Mspdebug < Formula
   sha256 "347b5ae5d0ab0cddb54363b72abe482f9f5d6aedb8f230048de0ded28b7d1503"
 
   bottle do
+    sha256 "4124d4fbd9e191d941153962bb74aed50cc200c473b5ad5850610a1bc85f87b4" => :high_sierra
     sha256 "e16447e04c99d74b8cdc49a063c230c64d09e34402d0221542594f3aacac5940" => :sierra
     sha256 "22fc92bc5a594451eb0d0b943bce812619302c795fdad0ca4305c059ccf10a88" => :el_capitan
     sha256 "8b23c23287fc9ab143921257a1859f8ac0dbb9e093261dfe931ec7d6a3548d97" => :yosemite
