ae7163f33cfe870b7f0cbc1f9b39c581362dae52
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 07:07:02 +0000

    euler-py: update 1.3.0 bottle.

--- Formula/euler-py.rb
+++ Formula/euler-py.rb
@@ -8,6 +8,7 @@ class EulerPy < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f5ba1b2823b4d55d4ab4bb0724676496ce9d2f70e9e65fd69ba260ed57be3fe3" => :high_sierra
     sha256 "c5ee4cdc8330f84084b0b145b9183b6c21683de758a3f052158f3dc6d0d6adb8" => :sierra
     sha256 "4c887a4af203e4991ff844f4836663cbaf1afc835ae8c628d769e32ff0f1e4e1" => :el_capitan
     sha256 "69ffb29dd9b1f1fdf6fa7ecf31871c5010a06a94eda99e916681aac73d9bca15" => :yosemite
