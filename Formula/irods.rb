0b2592a8880c24350e54c4c7720447b3e6800028
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 20:27:49 +0000

    irods: update 3.3.1_1 bottle.

--- Formula/irods.rb
+++ Formula/irods.rb
@@ -7,6 +7,7 @@ class Irods < Formula
 
   bottle do
     cellar :any
+    sha256 "5ca48b1240e083236086127b0f8d5fdd64ad3bcf023ced6c684d04945b8555a1" => :high_sierra
     sha256 "22a5e9bf24a034976491b65af9b36c4311afa16612f072655c556fba7285112b" => :sierra
     sha256 "510a0c5691702971c670c57c9ed11c5ca9371139b50c623cc950c0f8391a9737" => :el_capitan
     sha256 "490adc71118dc93c087aa685ddb873c3670575c679c834adc0a95c0b013772bc" => :yosemite
