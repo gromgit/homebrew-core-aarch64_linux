4c9776f526c0207aafb1f015c93d929f7b92a366
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 20:07:59 +0000

    ical-buddy: update 1.8.10 bottle.

--- Formula/ical-buddy.rb
+++ Formula/ical-buddy.rb
@@ -8,6 +8,7 @@ class IcalBuddy < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "fa01f750eeb692d11ae0fa4cb6131e331446ad623bc8fc284b3721cf81621f39" => :high_sierra
     sha256 "8dc7ef559702a3c489b2905e5cfdd4c9be18decd9557c9034df920f0ef57761e" => :sierra
     sha256 "1603d15b5b643a25c98baebc7c7e799bf3176a74a139a3f5dfecb474daf9037d" => :el_capitan
     sha256 "b839dc603deeeaba18efd2c6704e174fc4593fbc4c100c8d655b70f327802407" => :yosemite
