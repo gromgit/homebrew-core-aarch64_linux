73d698897fe5c89b1a311fcdab02c5bbf1be7f0f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 22:12:36 +0000

    fsh: update 1.2 bottle.

--- Formula/fsh.rb
+++ Formula/fsh.rb
@@ -6,6 +6,7 @@ class Fsh < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "71abf994ecf91d4675daef2c6604e6d414d9e33c2b66b5dc6240ee44f888f442" => :high_sierra
     sha256 "13a7134ef9d20899642d8dd96e77603d74573cf3a0e1ef5063f6eefc856dbd37" => :sierra
     sha256 "cec52eb07f9db79b15ff5907f30363bbb538c01b7c4eb7ae8634e7ce17eb5431" => :el_capitan
     sha256 "8a49ad906b045a293259c199fd5d1737894099c487b1bfc83fb60d18acf065ac" => :yosemite
