2ffde2ca994c9eb57fc3a051a1620aa02aeded2e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 17:58:58 +0000

    multimarkdown: update 5.4.0 bottle.

--- Formula/multimarkdown.rb
+++ Formula/multimarkdown.rb
@@ -10,6 +10,7 @@ class Multimarkdown < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5a317e9e6bb5a56ff216228b449ce1644271641591ccc567097bdb6c9adb41b5" => :high_sierra
     sha256 "cc9f163eaa9eb53def1e66cd2e3871ea9f8274028a3d399d01e2944d8cf2ac6f" => :sierra
     sha256 "72571c5521bda002ce2b140bc7e8fd224c0545e9f21b6268ad5a2ecedfe4e025" => :el_capitan
     sha256 "7c5370be42b0e15b19da90d8ead5aec745e24c842aea2cf2c210f399d84b67d8" => :yosemite
