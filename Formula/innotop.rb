c3b9f0ead18155a0ffb19a0a40bc019077d257a5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 20:10:58 +0000

    innotop: update 1.11.4 bottle.

--- Formula/innotop.rb
+++ Formula/innotop.rb
@@ -7,6 +7,7 @@ class Innotop < Formula
 
   bottle do
     cellar :any
+    sha256 "9fade33d5b830033b3cbbe6698d9b5115c8eb931a3feecb6667f18481046af89" => :high_sierra
     sha256 "d1bf5e48162b71e8f19dca2d31f19140e5c4cc54f524c528cf0aa05b458626de" => :sierra
     sha256 "c9f84dbe941f5d6dcf9f0e4a8b2ef560b8565f21c431f039d445efe755c41a74" => :el_capitan
     sha256 "0c91367118f50ef4e624e31837418dd07d09bf24f8cc91a6f143538a61a56b95" => :yosemite
