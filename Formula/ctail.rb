96ca9c09f14eb6a2aa02f68971987e14f3255f78
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 16:40:38 +0000

    ctail: update 0.1.0 bottle.

--- Formula/ctail.rb
+++ Formula/ctail.rb
@@ -7,6 +7,7 @@ class Ctail < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "de6e121995f86ec3dbfddf8bee861d9389c548648316f4901cbde691ca26a8d5" => :high_sierra
     sha256 "829ed2ea1ac94bf32fd1817f714b87301abf2c488cf151675239d5d9bf6f6ef8" => :sierra
     sha256 "80a2ae43fba99e6eb5eb4b50b52ee0e32213d521f59e147a109444439b86365d" => :el_capitan
     sha256 "e23b4f67bc165d9d4549963b41b9ee89a5fa2e18040277750c4800da016a919e" => :yosemite
