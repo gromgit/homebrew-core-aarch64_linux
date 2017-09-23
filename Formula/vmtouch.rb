0782c4512a2c9f5b980e5e7f09750e3ae6bcb16e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 14:37:14 +0000

    vmtouch: update 1.3.0 bottle.

--- Formula/vmtouch.rb
+++ Formula/vmtouch.rb
@@ -7,6 +7,7 @@ class Vmtouch < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "94cbb48095dbd350e29591f055d01789df27991deda9419d42138641e7386274" => :high_sierra
     sha256 "036d04749746169bd38c71e8637b235edd6ac8b1cc007a884e094a3004ce1933" => :sierra
     sha256 "fa1dd70ebdef2556d84315c28c693f5de9f18ee4b545205a030d1216af58a34a" => :el_capitan
     sha256 "e1e4bd20e994a155ea892305cf6c87aac9206bea4a94b2a59439e836ce15a10a" => :yosemite
