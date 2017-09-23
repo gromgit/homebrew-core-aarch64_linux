69298b2dfc45156dcbf37f85e23a5b4987c7c655
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 16:06:55 +0000

    collector-sidecar: update 0.1.4 bottle.

--- Formula/collector-sidecar.rb
+++ Formula/collector-sidecar.rb
@@ -5,6 +5,7 @@ class CollectorSidecar < Formula
   sha256 "3d73f8054a52411ff6d71634bc93b23a55372477069fcfad699876f82ae22ce8"
 
   bottle do
+    sha256 "f0b0a86af0fa4a9f01fa42a4112484bb1057a236407b56c0b05e32ca0f52a632" => :high_sierra
     sha256 "a570422eab63c9476cbbf127fc878a1410fa0ac7ff44bbe55227df73fbba0713" => :sierra
     sha256 "3db2950f3b53860ffb1a7b98e0f87adb45d106c2c5070d4b2ef7cfbd9662be05" => :el_capitan
     sha256 "d8f11c6167e2b3d66f4e67e7d7e2e4d013a71cd3b591e37da21fe809abf21e01" => :yosemite
