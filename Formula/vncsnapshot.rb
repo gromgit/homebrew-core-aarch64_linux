95f46426a384b6ed48a6ca33454d30311064dbc2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 14:37:14 +0000

    vncsnapshot: update 1.2a_1 bottle.

--- Formula/vncsnapshot.rb
+++ Formula/vncsnapshot.rb
@@ -7,6 +7,7 @@ class Vncsnapshot < Formula
 
   bottle do
     cellar :any
+    sha256 "76511216d57e76f357ab805fc700f6d777db9d173436d1b65c3de0733352472b" => :high_sierra
     sha256 "534d56ed36faf618d5617a1f32b4d7e1fd7cd433a1fc4c64e8c1e9312d53b1c9" => :sierra
     sha256 "1069a6f396c574c9efa7f6ac41772807f3d5e53c152a94a42733d64522d0a31d" => :el_capitan
     sha256 "efd8f3255029bcb16a554855f3f3fdb7ea59604df888aec6380da1d7330e3094" => :yosemite
