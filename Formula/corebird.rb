fb4132e718b5ea6864935fdddf2a93bd08c4d487
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 16:32:38 +0000

    corebird: update 1.6 bottle.

--- Formula/corebird.rb
+++ Formula/corebird.rb
@@ -5,6 +5,7 @@ class Corebird < Formula
   sha256 "604ad690405b46c24e18968d40adb6aca49763d9fe2cad90b1741d83b46fe853"
 
   bottle do
+    sha256 "ec32ea3b6d8b400b04958307f0c3cca28c20cc0030464ea7d50323cafc8de200" => :high_sierra
     sha256 "9e5d3a0bec0d58044b35eafc2a6434440c7a88d8f6fdcb8498e60737fbb0ab79" => :sierra
     sha256 "0f56d671657ca272fe9a4ffab69e7c4e8f8fb9de74aacf3a3230cee00c973085" => :el_capitan
     sha256 "7f735dbdb4262c8c11ea67ff8de771eb1e4f4dfa3391c42c4904a41732a38b56" => :yosemite
