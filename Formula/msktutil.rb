3c2705a968f8c61994e9736f692b9641479a1695
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 17:52:15 +0000

    msktutil: update 1.0 bottle.

--- Formula/msktutil.rb
+++ Formula/msktutil.rb
@@ -6,6 +6,7 @@ class Msktutil < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "cba1afff1683efb3d6f6d500047c10691b0f46c904f404d8fbfdbed08200cc54" => :high_sierra
     sha256 "3bf390f92696706f4f6241230024c9bdb13c78c05c9f81faffc356cbcb4ed443" => :sierra
     sha256 "190a11fe9d63b99fe2982a79d83bd7ba59f4c7ef104ce860eb9ae48acb56335b" => :el_capitan
     sha256 "c88bffcb5bacc334333d1bf614005ce65acb4d40e8c73f249313762e017ee8bb" => :yosemite
