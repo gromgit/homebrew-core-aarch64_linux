26821fd4bfdad3c52606310e63edbf661ffd70c8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 14:23:15 +0000

    vc4asm: update 0.2.3 bottle.

--- Formula/vc4asm.rb
+++ Formula/vc4asm.rb
@@ -6,6 +6,7 @@ class Vc4asm < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "db9bbf5ee3cb47a0f3ffa1d9bf355205873237e9f2dbd26777546935401ef4b0" => :high_sierra
     sha256 "2547c982e3fde40316d01d802bd01bf49af208e6737ecafeaeb8ad988ea3255d" => :sierra
     sha256 "72d54a4237c4e0f952fd1a3d913725d84814ed5b657affa1d6dcafa19e1cdc44" => :el_capitan
     sha256 "871b3b109ac49b09056f83e4488105196060d2388dc5052c679776b43fab5927" => :yosemite
