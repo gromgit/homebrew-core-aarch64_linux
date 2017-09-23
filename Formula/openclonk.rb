064d27593bd48383c72573cd09606368fdbbbbaf
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 05:13:54 +0000

    openclonk: update 7.0_2 bottle.

--- Formula/openclonk.rb
+++ Formula/openclonk.rb
@@ -8,6 +8,7 @@ class Openclonk < Formula
 
   bottle do
     cellar :any
+    sha256 "056037f1d797495cce2b0c28be28e76c49a318a339443b48c141a43ca0d871f1" => :high_sierra
     sha256 "101621b7af46c82184ad302b2246a6d76abfa3c14ad46a7c810dafaedea87051" => :sierra
     sha256 "ce6e5259b6e04779c378221974cdfee36382976aa5ec266804e57f2bad1730f8" => :el_capitan
     sha256 "e3ebb625a585702c150f2f74aa71fca61d6bf5d61085fe31e6b42ac42d68ede6" => :yosemite
