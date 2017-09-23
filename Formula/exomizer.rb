6618980e1b9ed35783ec9e26dc16bb67b680c183
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 07:00:57 +0000

    exomizer: update 2.0.9 bottle.

--- Formula/exomizer.rb
+++ Formula/exomizer.rb
@@ -7,6 +7,7 @@ class Exomizer < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d5beac52aa6f4fddb89ec51697093fb97063cb25d5d175bfe92cb336ca0e4b85" => :high_sierra
     sha256 "caa6b6304ed82fffdfd0fc4b8996eba6daab2240f30f05b17b3daf95b0ba1322" => :sierra
     sha256 "7fc0744775aff849b63640a9ac89d63309eebf04dc544b2840d219f8331629c5" => :el_capitan
     sha256 "5516edd49987f155735a83ef3a87fd01fb685d30067dcd770d07b07a4ddbbf19" => :yosemite
