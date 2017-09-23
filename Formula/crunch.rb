611acbfae2b7d45f3e63502a29ac6c6545d45838
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 16:48:55 +0000

    crunch: update 3.6 bottle.

--- Formula/crunch.rb
+++ Formula/crunch.rb
@@ -7,6 +7,7 @@ class Crunch < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "c59cb398b0ed4f28e8d56c49709991f5ea61b61bad4d672f1a481730948cdeb0" => :high_sierra
     sha256 "737d46b90aaa933abe03e111ece79e3f6a0ecb372cc1903b9dba3a33208111b9" => :sierra
     sha256 "84c0c275e63cc5c27fd468587f67ae5f1ab31a3923fe2eda27b4e33477356844" => :el_capitan
     sha256 "406d94f00713b83bbf41b36453605a5f85f154f88aec9b3ae23e7646ddcc03c1" => :yosemite
