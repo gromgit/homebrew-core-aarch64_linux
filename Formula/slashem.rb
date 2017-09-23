76f6635fa5ab45629b11c46c9d873d72bf2980f3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 20:48:20 +0000

    slashem: update 0.0.8E0F1 bottle.

--- Formula/slashem.rb
+++ Formula/slashem.rb
@@ -9,6 +9,7 @@ class Slashem < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5bac56b4e76ea1db5b5e211ac88c4f10c2fa8b179ada29512f41868af1669b3d" => :high_sierra
     sha256 "80a4df38057ec2bef889b92b4edfc80158add542a1bd9f1ca50ed8d39eb21e2c" => :sierra
     sha256 "3b0ec09db5b1e2abccc22d2cc9282de211d9a15e4d2d66c404f898af2768d1b3" => :el_capitan
     sha256 "9220e4e678c8302cd7c1ae15b4af08a733899c38717021c867e35decf79f00a7" => :yosemite
