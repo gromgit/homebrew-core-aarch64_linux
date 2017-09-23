9b360946eec77679995e77e3a22062646bd41646
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 14:23:15 +0000

    vamp-plugin-sdk: update 2.7.1 bottle.

--- Formula/vamp-plugin-sdk.rb
+++ Formula/vamp-plugin-sdk.rb
@@ -7,6 +7,7 @@ class VampPluginSdk < Formula
 
   bottle do
     cellar :any
+    sha256 "b81ef33d608958bde47122893d48582417ce580599606bf8e893a8791b9e7b0c" => :high_sierra
     sha256 "f5b77eaf0b80183cf7c19b08c4734b49393ad38e382da03666a8c8a3b5063b5d" => :sierra
     sha256 "acd0d2d514e459907217d67a6a2652bce37e6b87564fc9383a1e22763b84472a" => :el_capitan
     sha256 "ada7d84cbd975d1857e83651815a8a3a465ee04299fd32e5d90eba6646d6325c" => :yosemite
