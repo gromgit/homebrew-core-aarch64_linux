83e64fdd19e1c5d512ef91fe08304b24caa92813
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 12:32:38 +0000

    log4cplus: update 1.2.0 bottle.

--- Formula/log4cplus.rb
+++ Formula/log4cplus.rb
@@ -6,6 +6,7 @@ class Log4cplus < Formula
 
   bottle do
     cellar :any
+    sha256 "e41ba73cf7b64b87432a2669e2aa724f751bd36262d18b56a29e1ddd557b3f2b" => :high_sierra
     sha256 "7e842fafbdeacd4cad0c7b806e151d521607fe953e5c674eee0e8dfb9fd31165" => :sierra
     sha256 "d051d31f4d76a18a70f21d10b3037e3fdad202d18acedbb158874f26a57ec104" => :el_capitan
     sha256 "47cbed5a69741494a419d04bebfe8755172f98d8cb66cc228174529630321373" => :yosemite
