f55fcc7740cd955c0664b0bb15307d66c487df0e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 08:10:36 +0000

    odo: update 0.2.2 bottle.

--- Formula/odo.rb
+++ Formula/odo.rb
@@ -7,6 +7,7 @@ class Odo < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "0bfc54617186d149c98593c74dfaa59a42b2edcc7df1855fd452594ec42f1476" => :high_sierra
     sha256 "06af025b0a2df201a9b79944dcc4809708b305242622a90c92a9906a18adf2d6" => :sierra
     sha256 "979cc7131a35180614e848fa5fa12a72f734da7321358c89dfbd425fc8dff837" => :el_capitan
     sha256 "ebfc6a2e616694a3862b1d6d11dda1a2c1cb4c966447678b342457490e0e0abc" => :yosemite
