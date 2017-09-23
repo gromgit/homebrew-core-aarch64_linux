132e784bf60fe7dd641151ae359513dcb020afd4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 17:52:15 +0000

    msmtp: update 1.6.6 bottle.

--- Formula/msmtp.rb
+++ Formula/msmtp.rb
@@ -5,6 +5,7 @@ class Msmtp < Formula
   sha256 "da15db1f62bd0201fce5310adb89c86188be91cd745b7cb3b62b81a501e7fb5e"
 
   bottle do
+    sha256 "e9161f534cfa50edb5beae511d7689fe02a53b68f90623ebc751a41ba34b4037" => :high_sierra
     sha256 "d0b8a2a76d7ee8ed6beda0c383acd28d7a85d9d677c8d89a8a2e6b717055fe70" => :sierra
     sha256 "115ce90fcc11a1fbda6bf4496200b50e89d4cccdb32f999cf6b3b749635f8e3e" => :el_capitan
     sha256 "6f5227576bf8ac42fed7190c22f2e62b0fb2a3af59fa085e783426661c606758" => :yosemite
