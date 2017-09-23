5b80e7cdeb27979d853e10a246e3f2ba91c621ed
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 19:29:44 +0000

    imapfilter: update 2.6.10 bottle.

--- Formula/imapfilter.rb
+++ Formula/imapfilter.rb
@@ -5,6 +5,7 @@ class Imapfilter < Formula
   sha256 "891674d4c605667ad5ce912913784ecffa3b31b67bcf76d81c3c555cf97e5294"
 
   bottle do
+    sha256 "5d07140727b968410332eafd18c9900f66177b28a8a73db2b22dfeaff8dfc692" => :high_sierra
     sha256 "055ea04ddb949c85cdf87b6341cb8ec63d4d94307e82fecc49d97037d3e17cd4" => :sierra
     sha256 "fff91a47a08cb9c0e10c4f0e88f69aa36b4c720a99432c38e94163ec665a96b9" => :el_capitan
     sha256 "390019ee63e7cc436534f679191f2e0e8d63fd84180337b061a5db4a1cb1f68f" => :yosemite
