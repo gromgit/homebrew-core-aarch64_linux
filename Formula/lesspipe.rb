65bff186a3aaedf0af22db3a85b6ef36aa421805
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 11:56:28 +0000

    lesspipe: update 1.83 bottle.

--- Formula/lesspipe.rb
+++ Formula/lesspipe.rb
@@ -6,6 +6,7 @@ class Lesspipe < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "01c6ea7862d5b23ef49ce9c271e9cecf49c7ecd5372d9602b3ceb88b9171312b" => :high_sierra
     sha256 "f22864d81a8eb648fc4501665af743d285fcf0fa7c81edb21fd71f2593addedd" => :sierra
     sha256 "f22864d81a8eb648fc4501665af743d285fcf0fa7c81edb21fd71f2593addedd" => :el_capitan
     sha256 "f22864d81a8eb648fc4501665af743d285fcf0fa7c81edb21fd71f2593addedd" => :yosemite
