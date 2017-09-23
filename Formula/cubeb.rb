bd18ed96e35b5ec806b852e324f4e86daf158c98
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 16:57:11 +0000

    cubeb: update 0.2 bottle.

--- Formula/cubeb.rb
+++ Formula/cubeb.rb
@@ -8,6 +8,7 @@ class Cubeb < Formula
 
   bottle do
     cellar :any
+    sha256 "618debffabe494dcde3e0d7e2231078df124ead8ee342886ab38ad7373f73e37" => :high_sierra
     sha256 "f89e89027370ea9da99f72f0af0529f9b63fbe31c434d3ccafdc7230664a41c2" => :sierra
     sha256 "f7e738b374bb07e1c420e56dfeb72caa814495b446c71d8158ef98c9b33d3a60" => :el_capitan
     sha256 "b3cff6ba7008cc764f94281f7759f5d6d2a09a3bdb92f5f6e93be7d6f3ec2405" => :yosemite
