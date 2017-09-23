ca806b02323c01947c85ee538b0ba7c984bb0933
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 23:29:26 +0000

    homeworlds: update 20141022 bottle.

--- Formula/homeworlds.rb
+++ Formula/homeworlds.rb
@@ -7,6 +7,7 @@ class Homeworlds < Formula
 
   bottle do
     cellar :any
+    sha256 "b26fbb5f7955afa48247667b0caeff13d504eee6ef744284c498702026f9528e" => :high_sierra
     sha256 "499e9a94e24c8965b9a31902ab2a14a021c780756451b82ac2313c7c86ac5756" => :sierra
     sha256 "2665c0ed4da2eb399314d044699385250ca5db54e6f8c22287222b7877881d22" => :el_capitan
     sha256 "47251f13fa79c98b3c41d45cafade044bded134256f56e6ee1a118f67eb325d8" => :yosemite
