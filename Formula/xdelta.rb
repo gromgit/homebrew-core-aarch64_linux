5a466e7b164fee38071399d5e8134bbf6fb6636a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 23:43:42 +0000

    xdelta: update 3.1.0 bottle.

--- Formula/xdelta.rb
+++ Formula/xdelta.rb
@@ -6,6 +6,7 @@ class Xdelta < Formula
 
   bottle do
     cellar :any
+    sha256 "a65a726ce73eeebb9abfdf8069b08658dc4fad13527d4d162d1119cc32702b11" => :high_sierra
     sha256 "7f51b76d06a6ac8aae36c10b41776a374d5fafa6b55c4908a885be7a88194676" => :sierra
     sha256 "e07f928aadf6a9d8beb8a19fb72cb673cf0ae13c339ccd75c5df134cb3bc5c09" => :el_capitan
     sha256 "2581a9d0aabf6f6b34d35aae4d7ab07b6aaebdb70fd3b00ef14eff3bd96aa6c7" => :yosemite
