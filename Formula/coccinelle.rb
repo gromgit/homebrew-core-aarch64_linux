117da346e896cd28b63871c4281cb8cb275dc7f5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 15:48:55 +0000

    coccinelle: update 1.0.6_1 bottle.

--- Formula/coccinelle.rb
+++ Formula/coccinelle.rb
@@ -6,6 +6,7 @@ class Coccinelle < Formula
   revision 1
 
   bottle do
+    sha256 "8acf83dcc7348da0c9701c6db5a06b361ad517d4d8a586973cd335f006408d23" => :high_sierra
     sha256 "52b8fd8d7e7c86c13eb407b0110ad3bac610762324bdc2995f58c9dab2efb711" => :sierra
     sha256 "11135808a7d74af55ae74a548a9ed92eaecc6b7c5ba2b201136ef94fcf6ea07b" => :el_capitan
     sha256 "be0bba4bdc821f24c031902d8e1bcfe72e6f208cbd728527a506f59b914da526" => :yosemite
