5d110cbd9a8ccc9d988d8a1232b1b165983f631e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 13:50:44 +0000

    lsh: update 2.1_1 bottle.

--- Formula/lsh.rb
+++ Formula/lsh.rb
@@ -7,6 +7,7 @@ class Lsh < Formula
   revision 1
 
   bottle do
+    sha256 "2db1726494a9ff362a3fd9644e6a966b9762ea62f921ecc217c5ca842f67864c" => :high_sierra
     sha256 "ec3239a890387f33979336941411a1fb9c26696ecaf1f3edd519c89b3f63f848" => :sierra
     sha256 "0fdcabfd979222e6156220f78077e404da9e43b4ce958c606b6b7bd52bb612d4" => :el_capitan
     sha256 "7250d308a5d19b50d44ff99c1863aac5e8123ca0733bfadd2c57c702a294cef7" => :yosemite
