e0a06155f83df72b5c056a5d28c047c6f17811fd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 05:34:55 +0000

    openfortivpn: update 1.5.0 bottle.

--- Formula/openfortivpn.rb
+++ Formula/openfortivpn.rb
@@ -5,6 +5,7 @@ class Openfortivpn < Formula
   sha256 "a96016826bf85435c26ef0e58d14ae91990a08aaf67c701dfa56345dd851c185"
 
   bottle do
+    sha256 "964ee366d0cd33ece25e9235761aa424dd01e3e17f18720c79aeac2091e5b3ff" => :high_sierra
     sha256 "64e74a9365601be04eceb6a9a6122aeac621360fb7b6c477f74dbb92991d1814" => :sierra
     sha256 "ecb13f914741c5a0edcb43a006f016731428d23ffebea94b4a7b20522a04ef09" => :el_capitan
     sha256 "dd96c9f517a603e2a352a632a801e4d87fbc5588f0f01b6b62764924482732fd" => :yosemite
