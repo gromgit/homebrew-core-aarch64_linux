635430af4c096f9740ce05525cfb27fdcfae6eb7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 15:48:55 +0000

    cnats: update 1.6.0 bottle.

--- Formula/cnats.rb
+++ Formula/cnats.rb
@@ -6,6 +6,7 @@ class Cnats < Formula
 
   bottle do
     cellar :any
+    sha256 "04057849239dfb92359445c7b35bf18a39b5fb068275f62ac38b6bd6c614b054" => :high_sierra
     sha256 "bcae1cd8aa2f660206b91d2493e4d8d2c62d37f35e4831e15bc4276d542e0d3c" => :sierra
     sha256 "2cb0cff286bad7230ba159f1d44f5b19a8d00078600ebe1110f4070e979fc09b" => :el_capitan
     sha256 "f9d09b229aa310ce11c6e19582b7905fd3e2f59200c4e985050377af9675ed55" => :yosemite
