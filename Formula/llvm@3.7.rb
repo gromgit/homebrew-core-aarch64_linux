9a14bcbbce60504d701689ac4da045d3791444fb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 12:24:33 +0000

    llvm@3.7: update 3.7.1 bottle.

--- Formula/llvm@3.7.rb
+++ Formula/llvm@3.7.rb
@@ -36,6 +36,7 @@ class LlvmAT37 < Formula
 
   bottle do
     rebuild 1
+    sha256 "e3c4c73ce9b493663d47e104da0117ca8525d834e05d11d3e0b97c731274ef0f" => :high_sierra
     sha256 "086d8f2aee762f69511d25f704579828428dafb37f308ade14cbb2cec106046e" => :sierra
     sha256 "f61418bf5315eb0b346c23b2dbb81f9d62cdb00f173b3b5eee9e25ac96dbe796" => :el_capitan
     sha256 "e006a9164903c96932e43c259cbd909f1316f4172a59e2057f28108e5aa87913" => :yosemite
