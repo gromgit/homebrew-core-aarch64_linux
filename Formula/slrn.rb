811a706aa4a35f4251d5d6db6424d17abfc4322b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 20:48:20 +0000

    slrn: update 1.0.3a bottle.

--- Formula/slrn.rb
+++ Formula/slrn.rb
@@ -7,6 +7,7 @@ class Slrn < Formula
   head "git://git.jedsoft.org/git/slrn.git"
 
   bottle do
+    sha256 "73238ccdd5f84b813446674f5be88f604c4e44107ab5bf367caee0bdd00ea410" => :high_sierra
     sha256 "46721d6d7a4ac469837a4e41bfdc279727ee7c3dd38351eb3d47aa7b43e64062" => :sierra
     sha256 "d45e3c8765302bd61709b091143becf4c7ce78913256620c956dcf2b95431910" => :el_capitan
     sha256 "06d71ffeb008854c63eeadf6f45633cf692e648490cb20c2ba5f3229cc3dc808" => :yosemite
