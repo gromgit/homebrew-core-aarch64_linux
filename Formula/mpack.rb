b6f9fe7d16bc36f620e42b9558220454bf8173e8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 17:36:59 +0000

    mpack: update 1.6 bottle.

--- Formula/mpack.rb
+++ Formula/mpack.rb
@@ -6,6 +6,7 @@ class Mpack < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3010b6b97b6388a250e18278f1ec45b27244898876856d53e776b75ecd0c6bf3" => :high_sierra
     sha256 "4dac8e937f170ddcae76e90143e7b1a6b0bd66e730d683ab83693d55dd670f2b" => :sierra
     sha256 "f0f9bd526ff3c7e8a1abea377e3716fee96916c0c54234d96b46a475b50b8c1e" => :el_capitan
     sha256 "f5455f95c52ffc59181037ea3fd8151006178dfd6abb9674be65b2996f876766" => :yosemite
