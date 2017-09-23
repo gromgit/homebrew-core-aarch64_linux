085136270aeacc5b2eff8809e528ff1656982483
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 07:07:03 +0000

    exact-image: update 0.9.2 bottle.

--- Formula/exact-image.rb
+++ Formula/exact-image.rb
@@ -5,6 +5,7 @@ class ExactImage < Formula
   sha256 "665b205740b17b4162fae73aa36eb7046f28bb216d612461ca0499ff47a566ba"
 
   bottle do
+    sha256 "ad9a08346cef8389013aaba11d374b70e4eee239e9d57b7c71485eed1cda838f" => :high_sierra
     sha256 "2dd2e178792d741119e2bb81a138f41ee89fbcfaf41ed0c46e8f81eaeb46e42c" => :sierra
     sha256 "7f437345feae9532b1313bb331eed57ce7a6cd4cbfca2c42aadba6204206744c" => :el_capitan
     sha256 "e259695a00a3962871e6b5030d579836d4f64e28daead6f297faf7783edd17af" => :yosemite
