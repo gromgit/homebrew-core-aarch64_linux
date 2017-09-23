805be39864486fbbd7fb52b52e83146ad7ab97e0
ilovezfs <ilovezfs@icloud.com>
2017-09-23 09:03:44 -0700

    faad2 2.8.3
    no longer fails with clang
    
    Closes #18468.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/faad2.rb
+++ Formula/faad2.rb
@@ -1,8 +1,8 @@
 class Faad2 < Formula
   desc "ISO AAC audio decoder"
   homepage "http://www.audiocoding.com/faad2.html"
-  url "https://downloads.sourceforge.net/project/faac/faad2-src/faad2-2.8.0/faad2-2.8.2.tar.gz"
-  sha256 "ec836434523ccabaf2acdef0309263f4f98fb1d7c6f7fc5ec87720889557771b"
+  url "https://downloads.sourceforge.net/project/faac/faad2-src/faad2-2.8.0/faad2-2.8.3.tar.gz"
+  sha256 "9e4fd094080c27f6f419f3fe1fce369621b9469de396e126405153784134da00"
 
   bottle do
     cellar :any
@@ -10,19 +10,7 @@ class Faad2 < Formula
     sha256 "4febae463c234004d14143635d955862584b03e0971fefb838eebeb8324d406b" => :el_capitan
   end
 
-  # Autotools shouldn't be required since it's a release tarball
-  # Reported 22 Sep 2017 https://sourceforge.net/p/faac/bugs/224/
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
-  depends_on "gcc"
-
-  # mp4read.c:253:5: error: function definition is not allowed here
-  # Reported 22 Sep 2017 https://sourceforge.net/p/faac/bugs/223/
-  fails_with :clang
-
   def install
-    system "autoreconf", "-fiv"
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
