3e2083fa40cdf0da7a5908b3056cff1c4fe55a75
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 22:51:12 +0000

    udis86: update 1.7.2 bottle.

--- Formula/udis86.rb
+++ Formula/udis86.rb
@@ -6,6 +6,7 @@ class Udis86 < Formula
 
   bottle do
     cellar :any
+    sha256 "cefad0043918886a862a040adf2450699c00cbef3fd561bbc8867e2328b16ac8" => :high_sierra
     sha256 "e3774a825eda78db57585c75b739dc60d0c069e35c8666575f5889908b0735d5" => :sierra
     sha256 "e763db7beca50f11ab341f13f5fd571513f4847772bb70ef83d94bb576427673" => :el_capitan
     sha256 "bcd6eb347f55bc856ceb64604d3bace30219e346de34caa8be7de2b52a1cb35d" => :yosemite
