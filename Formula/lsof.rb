28918343670940bdb2e6abaa45c8da26069609d9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 13:50:44 +0000

    lsof: update 4.89 bottle.

--- Formula/lsof.rb
+++ Formula/lsof.rb
@@ -15,6 +15,7 @@ class Lsof < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "553ae69481b898fa1125b5d9d97c51cf49c4c31f11e1fe5d33a86c415a845e8d" => :high_sierra
     sha256 "2312dceb501fa6d9301fa506370784456937404ac43381354457ce63d99ccd56" => :sierra
     sha256 "fb14a3aef899327098b92d6d2c283e8978fc34d55b42019b8a54338abe8d4853" => :el_capitan
     sha256 "8a63b65f74b992e4a70ac3e18a7e4b810a73e183b13086b40f30286256e646e0" => :yosemite
