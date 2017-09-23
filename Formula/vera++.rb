a959af78fb863cbd41401de9546497b3003375b6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 14:51:55 +0000

    vera++: update 1.3.0 bottle.

--- Formula/vera++.rb
+++ Formula/vera++.rb
@@ -7,6 +7,7 @@ class Veraxx < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "73b49e98703b820ffc65213f2e14d0932c5b08851165042811b3e3318bbc84f6" => :high_sierra
     sha256 "3a261328afd43c8c38f33802ced93557c58ae8903dab90e0ca4546004003447f" => :sierra
     sha256 "76dcb0b9340b8fc9413fc848dff27e8805d7b2a9c63d5128fc83ce5bd3bd1cd5" => :el_capitan
     sha256 "a2620392e9204964ecd0ec0bc6b90268d27e5e2a28ef304aff3d3719ed058b80" => :yosemite
