67b08f00e877524f6681febd1bb90a69682e9671
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 23:11:56 +0000

    hledger: update 1.3.1 bottle.

--- Formula/hledger.rb
+++ Formula/hledger.rb
@@ -10,6 +10,7 @@ class Hledger < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "92596ad58b3b57a3394f6df4a3d12d6da5cbf7be3855f529c56180496b43facc" => :high_sierra
     sha256 "cbc9f615c11543380667979883b5b93fc335b1891616a7b6f7569f0f18e1d394" => :sierra
     sha256 "95ec2ed7af8637bd2309d9fdfc651ec45ffd6ba13c76bb37c20a2ded98ae7c12" => :el_capitan
     sha256 "98432ae6db2c7320ccf87d471a3ef0263407669e9129f4c6433b1fd8d70e3d35" => :yosemite
