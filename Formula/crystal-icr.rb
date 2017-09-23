b246b9882335e26b9205cb2f5070947b3a518482
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 16:40:38 +0000

    crystal-icr: update 0.2.14_2 bottle.

--- Formula/crystal-icr.rb
+++ Formula/crystal-icr.rb
@@ -6,6 +6,7 @@ class CrystalIcr < Formula
   revision 2
 
   bottle do
+    sha256 "e7e329d7ddf2e5fd09c62e0df8d027d51bb926db14af813a720898688b9a0ae6" => :high_sierra
     sha256 "951ce7170516e7b5b5adc4b64f52b4053ad36ce581e2dbc43843f0e07ea2eedf" => :sierra
     sha256 "b6677ec4428770723ebee3a5059eee7a5a9a37b01943e09cce0654f7f6c58bac" => :el_capitan
     sha256 "bc7b3b94b127632b60dd14127749d16ef92ba97520163226e1a0ccf0910a5579" => :yosemite
