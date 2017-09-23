fb4dd7afcd4f5b45fd893b9d8a5f603f7ed8d32f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 07:07:03 +0000

    eventlog: update 0.2.13 bottle.

--- Formula/eventlog.rb
+++ Formula/eventlog.rb
@@ -6,6 +6,7 @@ class Eventlog < Formula
 
   bottle do
     cellar :any
+    sha256 "be5272b1fb50fb84ba175d4acdbe0632d46444df4e93fb913a9e3ee3ba2d6d33" => :high_sierra
     sha256 "266c920dec2b898e620a5de1bdcbcc68c3b06663c8b4f8d155138ba989958b99" => :sierra
     sha256 "9073fb11ae9c20375295c36b5bb6845639ea1f9c17a677c1d93ff206075ff871" => :el_capitan
     sha256 "2bdc1f762ea05e79f486e7e78b8a173ea99a5a76b4bedd28a03a1c8912f39925" => :yosemite
