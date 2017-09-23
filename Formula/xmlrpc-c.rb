a4058b6f7440d20cb08391ee8c4c5122e5c889b3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 23:59:22 +0000

    xmlrpc-c: update 1.39.12 bottle.

--- Formula/xmlrpc-c.rb
+++ Formula/xmlrpc-c.rb
@@ -6,6 +6,7 @@ class XmlrpcC < Formula
 
   bottle do
     cellar :any
+    sha256 "c6c39ccd6891e1e9ce07f153a2e16d843cc8c5a0ec9eace09c84a34543115933" => :high_sierra
     sha256 "19784c94e65b73ea524331452a28458eaa29b78419955805226a50f1632ce6d1" => :sierra
     sha256 "503a064edf4638c671b6377d91045c7f0990203b9b8d6f873d84415c8c98b614" => :el_capitan
     sha256 "6fb643c4bc7e7fdef6c276f533eedafe77d325fd505218bdbd8244af5577db31" => :yosemite
