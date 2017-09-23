39672d5b3f2e3fcf4bb4ba14239ffbef99f48598
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 22:38:31 +0000

    unnethack: update 5.1.0-20131208 bottle.

--- Formula/unnethack.rb
+++ Formula/unnethack.rb
@@ -7,6 +7,7 @@ class Unnethack < Formula
   head "https://github.com/UnNetHack/UnNetHack.git"
 
   bottle do
+    sha256 "7da0d5eada4191f5bfd1d3beb6c9d74ed3bc26c54c3ec0d7ff6620592504c131" => :high_sierra
     sha256 "2801362d487c397485b6849a2421c080ddb1261563e77bf64d16aa982843d332" => :sierra
     sha256 "f0e6315f7a8d6135f80290dd20d8e2d80dc8224ad865b073fb71c05771d799eb" => :el_capitan
     sha256 "a6345197d1067ce08e9220bd74701355d19add9c251794b3f12210cded3dce46" => :yosemite
