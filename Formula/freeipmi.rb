e73cb7bdca6164ad827dfd68dc9109bab3d37d4f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 22:14:33 +0000

    freeipmi: update 1.5.7 bottle.

--- Formula/freeipmi.rb
+++ Formula/freeipmi.rb
@@ -6,6 +6,7 @@ class Freeipmi < Formula
   sha256 "b46c9432e8649b87d4646bbf4da32f7e9039796fc256f4b229c94c3ac7d0bde5"
 
   bottle do
+    sha256 "a510f28794572ed659d6e709b44ee45d96d5c662d730084442d99771bca9d1ff" => :high_sierra
     sha256 "41c5cf3544953f6127dc312f09d08b8bfac2f551f298880a40e45cc260fac9bd" => :sierra
     sha256 "e008f84b3ef595205d47a81c6abcb32cbdbc7704ede7cd462dba41be292b963e" => :el_capitan
     sha256 "85d35f29c9f0be5849c6e7b6a8b6ef4f19c988a63edbe830240ea676f430422f" => :yosemite
