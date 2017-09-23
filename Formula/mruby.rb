a89c65803cc95b193e6b76f4db4477ede5756796
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 17:52:15 +0000

    mruby: update 1.3.0 bottle.

--- Formula/mruby.rb
+++ Formula/mruby.rb
@@ -8,6 +8,7 @@ class Mruby < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "87512993c1c274958500aaf45551572ea4897e081eefbccd9da34603e515f07f" => :high_sierra
     sha256 "2389720557c8a6421882561d096dfd84317f47c628b03852d86a34b2afb61aec" => :sierra
     sha256 "d9a8487acd5544562cfea50da211f5268f86630ed5d197ab1c9bab68b453ca9c" => :el_capitan
     sha256 "77e4dc3179a3e0f86c35fdf3aad37ff57c31cf9410f776cc953d9cfe797186a7" => :yosemite
