0cee64df73501d817928c9cb7d75f5f6b5c26657
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 08:10:35 +0000

    nsq: update 1.0.0 bottle.

--- Formula/nsq.rb
+++ Formula/nsq.rb
@@ -8,6 +8,7 @@ class Nsq < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0c7c75f413975e6ceca2cd6a4bd53cedbb1c5abe53e67725fda5f3918a7047f7" => :high_sierra
     sha256 "b5a2cd5b98623992f9b49edb1784d3d363d43be481fa4fbedab119421cc899e8" => :sierra
     sha256 "e5a1abbe2c2b5c64d12c01458c92d35b07904b07fccbcb8884be6cdad99c206b" => :el_capitan
     sha256 "3fbe85270e1dce66310d15c92d3088a8e96f4aa473ea5dc4c0b699d5758769cd" => :yosemite
