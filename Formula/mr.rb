81982e318da80bed2eea1a1d549e0cfb6b5d6126
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 18:04:28 +0000

    mr: update 1.20170129 bottle.

--- Formula/mr.rb
+++ Formula/mr.rb
@@ -5,6 +5,7 @@ class Mr < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9ccfbdcf62f340df01ef135f7d3a09d8f5177d6ab414bceed9425987ba526fe2" => :high_sierra
     sha256 "4ef14ce20e60b88a882e156aad6fbede00d0ae852355950bbb014391993edbe2" => :sierra
     sha256 "7c84fb8bb3d5ec8dd1b175a9805c9ee92be1cc0c1f0fca81f2557c0483301661" => :el_capitan
     sha256 "c7b3005af6e349badfa7f8f99911217ec0138b3236143189fdb715f61cb2ffe1" => :yosemite
