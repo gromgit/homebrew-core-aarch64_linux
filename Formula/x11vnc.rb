bbc406d9a0ac4f6473d92e8246d8bdd6398d6768
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 23:45:57 +0000

    x11vnc: update 0.9.13_1 bottle.

--- Formula/x11vnc.rb
+++ Formula/x11vnc.rb
@@ -7,6 +7,7 @@ class X11vnc < Formula
 
   bottle do
     cellar :any
+    sha256 "3ed573fd3d02c34f407858af3f6e01386d1af2fc9fee16258eea94a4b2c19137" => :high_sierra
     sha256 "3f840749807f57b4248c9ae202a214209c4bf4c4e7386d47da83b424153abe2a" => :sierra
     sha256 "f4a33301592ef159be8f999fce086875bf88afd6ad2b48d6709c2f32d4ab3be1" => :el_capitan
     sha256 "38e07e6c3a26cf1e8d60f1a4e7061da400afb9bb2803f0dd79566c5a3bfd0d22" => :yosemite
