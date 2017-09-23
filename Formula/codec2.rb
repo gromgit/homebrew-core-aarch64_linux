05d23797e43e745a02ea71ab75a0ccb7f0b570af
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 15:48:54 +0000

    codec2: update 0.7 bottle.

--- Formula/codec2.rb
+++ Formula/codec2.rb
@@ -6,6 +6,7 @@ class Codec2 < Formula
 
   bottle do
     cellar :any
+    sha256 "539032d65d38749f4dde5970d653761a3de4ce6d9007a4cc0f535847686596c7" => :high_sierra
     sha256 "ecba1a173dbda214953179e8c7200637934b905ec3b2ec3dc9fd8785a20bdb1c" => :sierra
     sha256 "6aeab3c08a575914a615f0051b73464ffe19a4bde2c7595d93db5698ff054fa0" => :el_capitan
     sha256 "78ccb93fb8936706594ef14d7b37762498ea52c7f00b2c91804608172b890dc8" => :yosemite
