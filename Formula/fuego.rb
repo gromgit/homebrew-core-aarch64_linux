09b22b64b5e7d3c2ae5f524732c51571c63faa84
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 22:31:48 +0000

    fuego: update 1.1.SVN bottle.

--- Formula/fuego.rb
+++ Formula/fuego.rb
@@ -11,6 +11,7 @@ class Fuego < Formula
   head "https://svn.code.sf.net/p/fuego/code/trunk"
 
   bottle do
+    sha256 "72603b0cc76547a838cadabc8ddd958bd642493364ab3d35eb7f918347c3c4e1" => :high_sierra
     sha256 "0d3274bc3c26894df8b01725486b3c8a66a33dc47e057974bb56b96b64165ab0" => :sierra
     sha256 "0f4eb59a935afffcd4a518d6d04751566ea712ec49906e1bdeea0be194883cde" => :el_capitan
     sha256 "1089ca13694e6774aaeef91ae23e2633b94420c154a9b94e1a45ae281bda3bee" => :yosemite
