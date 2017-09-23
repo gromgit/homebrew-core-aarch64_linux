588b18152bdd9ec70b9063bbb22cf54c7812c52c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-23 16:57:11 +0000

    cweb: update 3.64b bottle.

--- Formula/cweb.rb
+++ Formula/cweb.rb
@@ -6,6 +6,7 @@ class Cweb < Formula
   sha256 "038b0bf4d8297f0a98051ca2b4664abbf9d72b0b67963a2c7700d2f11cd25595"
 
   bottle do
+    sha256 "e6888449565ebd6620ebc6851dbef48765a0654b3e5d429cfffb617ca33e2479" => :high_sierra
     sha256 "377a987173b8274ab97de6d8978816372d6f380a0fe4c9e0b09cfcd7d27ab66e" => :sierra
     sha256 "86ff3ceca459e8f087644249378a19a7f53f4ebbd5c74ddfbbe6ea795003a1a2" => :el_capitan
     sha256 "27c017af8f2e004888240d99a14b29ea9ac8d1fa5339d228b6a79ecda8031e4e" => :yosemite
