69299a07cc33845c8df38a3b6f3180a7f23426fd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-24 00:06:38 +0000

    xshogi: update 1.4.2 bottle.

--- Formula/xshogi.rb
+++ Formula/xshogi.rb
@@ -6,6 +6,7 @@ class Xshogi < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c78625da6cc10502df9274bff0680a1af6316c125b6ce482ab9d79ad745e6e55" => :high_sierra
     sha256 "4877493db0e9536a1282b9aa79d3cd38357a4b8e767533cfb6c4dce941faed23" => :sierra
     sha256 "a4bfc78e6d2f10e6bf4c813a2e847963d6dd6d939a522934717ff6d35acafcc9" => :el_capitan
     sha256 "62c11f796225f439e5698ad87abafbfe55d2184d895b86fd13abe3dc924b2030" => :yosemite
