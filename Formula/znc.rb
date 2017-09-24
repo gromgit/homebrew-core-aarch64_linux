6bbcd3dec70f2c0ebdb4f3c75d29400f347e5438
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-24 01:08:07 +0000

    znc: update 1.6.5 bottle.

--- Formula/znc.rb
+++ Formula/znc.rb
@@ -5,6 +5,7 @@ class Znc < Formula
   sha256 "2f0225d49c53a01f8d94feea4619a6fe92857792bb3401a4eb1edd65f0342aca"
 
   bottle do
+    sha256 "282e0fddf7f3e13d891b92ca31b2fc3d9f216ffd8771550cc4551d3e21b3b547" => :high_sierra
     sha256 "c1d6ddcd9078631bcd94484f3796e95f32bf9e7b8d0886d9e2802b955b624cbf" => :sierra
     sha256 "fe47b2288ee78acd11b5d4dc5a4a43a255153a211775875975f458cfc370c7a9" => :el_capitan
     sha256 "61a0afcfcb021a7005d59f6d0a352fe27f05a6b2617eadb4482172d673666a67" => :yosemite
