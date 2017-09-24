94726d7e0519b8b2f6ac1551a907e3b1095dc375
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-24 08:20:44 +0000

    robotfindskitten: update 2.7182818.701 bottle.

--- Formula/robotfindskitten.rb
+++ Formula/robotfindskitten.rb
@@ -5,6 +5,7 @@ class Robotfindskitten < Formula
   sha256 "7749a370796fd23e3b306b00de5f7fb7997a35fef30e3910ff159448c932d719"
 
   bottle do
+    sha256 "0a8be5a0e0cbfd167bc18d9f2224610040102e65332dbd5cf3635487345a93d2" => :high_sierra
     sha256 "710b88a647ab9dfcbca6464deff11424fd854cd21e5fe8ae366389c909d8b2ea" => :sierra
     sha256 "ccbdb2706ad962c50eb417530835e651ec00469bef4467033bd1eab3b301adc3" => :el_capitan
     sha256 "c2e72aea983c21ca8a2559d61b9401047d16cead69ee52e75c36aa073b8f583f" => :yosemite
