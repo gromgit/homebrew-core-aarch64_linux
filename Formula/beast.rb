a09b574d545fbed0714b41372bd66876ddcf53cd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-24 07:05:29 +0000

    beast: update 1.8.4 bottle.

--- Formula/beast.rb
+++ Formula/beast.rb
@@ -7,6 +7,7 @@ class Beast < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7d04adbad7f141cc0e7465855a098e46c4c156e1ac59a400911e67ee1c62571d" => :high_sierra
     sha256 "fd0b8225774ad857e296ca5fe8540ab21de86a5e951d963e2af5473f6e85a5e2" => :sierra
     sha256 "30fa27c365bed00f62c399e7724044bfc2eef0e211ca7ca41211d01446c7a27d" => :el_capitan
   end
