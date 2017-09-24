c16326a923e1f6f1ff5ae2369e955d924cfbfb35
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-24 00:17:05 +0000

    latex2html: update 2017.2 bottle.

--- Formula/latex2html.rb
+++ Formula/latex2html.rb
@@ -6,6 +6,7 @@ class Latex2html < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "051de8192f7caaf9a2e2413e8cdb3f9878d1bb2e03200ffbb1ab42e3a78cb005" => :high_sierra
     sha256 "8ef1c505140cf839ae71708e3785812a538622a19ee76c19ec1901b6a4567edc" => :sierra
     sha256 "ee26a1cfa8ff76d1f1d7a0d3435e5a792fbc34e35e43cf84c22321c1df665f3a" => :el_capitan
     sha256 "8c53c049249208b6441c43500b8921c0869b4d0fef6226194ce4e1a8a8c8cbfe" => :yosemite
