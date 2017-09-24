fc638e863b29fc2d758462d64d84debe685786c1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-24 00:05:53 +0000

    xpa: update 2.1.18 bottle.

--- Formula/xpa.rb
+++ Formula/xpa.rb
@@ -6,6 +6,7 @@ class Xpa < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c9163ee736fde3dd56d4420121fc61a710bf88feab805e5b2c9cc31630a3be1f" => :high_sierra
     sha256 "6aa70ab2200ded59d1bbd466b915311c52313bf07f2c398362ce8f5bbb29c597" => :sierra
     sha256 "f8b188638f71cecd974279f07d55b932be7c657f83c7e55634c011df7cb44a63" => :el_capitan
     sha256 "2c79a17701b6c38017eb1e02954302912d6618fc7666b46bd7f1d7019632c318" => :yosemite
