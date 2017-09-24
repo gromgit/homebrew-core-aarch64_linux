56917b2ba7b1c33ed11bc03c7cb9e105d8959ff0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-24 00:23:14 +0000

    latexml: update 0.8.2 bottle.

--- Formula/latexml.rb
+++ Formula/latexml.rb
@@ -7,6 +7,7 @@ class Latexml < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "bbbeb393b7ed0258fdbf875e103a6f2f82103a6c19fce6b77ce5dd99fde9dc72" => :high_sierra
     sha256 "3d995988dc683269f6949f8071148ceaf7454e8e7eb37cd8d391a1eb4467fc76" => :sierra
     sha256 "5ae3ca257610559471ea0e1bbc9d5ff8f122790564a8e7027841e5b2356b6f8f" => :el_capitan
     sha256 "5205887f374d4bd15905f5f13b4c661c5a6cb2725fc631836cff0668e34085b5" => :yosemite
