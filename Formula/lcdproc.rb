8aed46ac2d812a67634aa8f712f1d50dd80f18fa
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-24 00:18:16 +0000

    lcdproc: update 0.5.9 bottle.

--- Formula/lcdproc.rb
+++ Formula/lcdproc.rb
@@ -5,6 +5,7 @@ class Lcdproc < Formula
   sha256 "d48a915496c96ff775b377d2222de3150ae5172bfb84a6ec9f9ceab962f97b83"
 
   bottle do
+    sha256 "59439a9e18e3e8e636a60e1710cd10f8a4bad8632d08782fc4442a2427fe1ddb" => :high_sierra
     sha256 "2371b10dc3bd2644ac83ced35dcde1960110217385f9f5547917ebbbb823e332" => :sierra
     sha256 "1de4bece6e781dc6d88d000039095cbf6edbd10313163ef9644152d67778171c" => :el_capitan
     sha256 "2ac794ede644c1c86b321af648eb2b0197762cb7e5eb09cd0a31e8eed842e2f9" => :yosemite
