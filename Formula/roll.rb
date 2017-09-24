37ef6c5f54eae752b1f5c750a062809304533c88
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-24 02:23:18 +0000

    roll: update 2.1.1 bottle.

--- Formula/roll.rb
+++ Formula/roll.rb
@@ -6,6 +6,7 @@ class Roll < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7b7feb2bbfc8c8b9fe26f5f0403353da6edc3d61d0e885b6f70c69a59a0b8c2c" => :high_sierra
     sha256 "bf42fe63830aa9b363962c6cfbdeb79065cad24b70b32710dfe0eaefe47a1dfc" => :sierra
     sha256 "67d9f2bfdfa266bcc01a76b494eb9d897f0a9385f9ae4d9dd12783ec675b8e99" => :el_capitan
     sha256 "018dc40d3339a486b49401fa7ba43fae3c27879dbec29c3fbea73cc64c51f647" => :yosemite
