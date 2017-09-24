cf58f565653c852d3fe479625e0bded18c5ce1b5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-24 03:16:28 +0000

    cmus: update 2.7.1_2 bottle.

--- Formula/cmus.rb
+++ Formula/cmus.rb
@@ -7,6 +7,7 @@ class Cmus < Formula
   head "https://github.com/cmus/cmus.git"
 
   bottle do
+    sha256 "7bd5e8f8d29efe7840caefc7f455a8e61bf02b467991a133a0b76cf89e8584fe" => :high_sierra
     sha256 "48171d7e6cd31ec1451a14c1c5275249f872c68c493910b39b1fa1d49eda04ad" => :sierra
     sha256 "deeed1d9ef93f0a8263b18a08239cab7a22983afe4278310f9944b79ab3df560" => :el_capitan
     sha256 "150534ed291aeb39a6c1a84a6efa6f1e4b518c0e3eae4e18efac9a5496e826af" => :yosemite
