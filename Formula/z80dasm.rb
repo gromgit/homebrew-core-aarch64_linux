5571640c7ade30a7ad8532563e226d4d78aab1b5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-24 00:59:43 +0000

    z80dasm: update 1.1.4 bottle.

--- Formula/z80dasm.rb
+++ Formula/z80dasm.rb
@@ -6,6 +6,7 @@ class Z80dasm < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "df2612812e22db187eb9fcb2a4cb1fd918e20a991bc478fae18493e9e7eb142d" => :high_sierra
     sha256 "de8792d3309324500758a7c173091573835308e0b4715b0e1ea48dd7a9c7c135" => :sierra
     sha256 "0309ecd2137730b8e756f3f3cdb364a16df1a03aa454684922706b0c6d705567" => :el_capitan
     sha256 "3219de661b1355cd130687c0f547bacf030dd1b2e499c5817b20bc5dbd8f1b54" => :yosemite
