4e0d5cef08684bbc8d6abc8d35f360141d16b7f1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-24 01:20:33 +0000

    uggconv: update 1.0 bottle.

--- Formula/uggconv.rb
+++ Formula/uggconv.rb
@@ -6,6 +6,7 @@ class Uggconv < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ed41635e3235d763c9dcad68db2e390821af5a62ec6709fca8b9c5e9b5d0b995" => :high_sierra
     sha256 "a22594f94bf7baa1908bd1225f52f1db3dd01daa17f99038ecfbd60e22d12b5d" => :sierra
     sha256 "5ab8b271f2ccc17e5229921f01b92ff7b0c297908902c83d24612bb47592af3c" => :el_capitan
     sha256 "a40a8a1adee9286acedba6e8eedf20bc53e4bf291fc73478bd3ba0314792c6ce" => :yosemite
