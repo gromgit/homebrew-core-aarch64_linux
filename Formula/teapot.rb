d3ff067ccca916e55e53d1f7c5c31494e38279e0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-24 02:56:28 +0000

    teapot: update 2.3.0 bottle.

--- Formula/teapot.rb
+++ Formula/teapot.rb
@@ -7,6 +7,7 @@ class Teapot < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "f918044ee7953b5cc9be50487cc86bf57b4e217990551cc67c6f0c9c41f0ac0a" => :high_sierra
     sha256 "f0dc057cdfa1aa5168898a22791ee72fd3e525fd750838e94147f8b3811c1b07" => :sierra
     sha256 "84673e8886e1f24250116d8c423383d0babbc53e1cb669ba46b45a37a2344399" => :el_capitan
     sha256 "0ffd7fa1ac31cc91c9c71d225d26970e23da5719d7505d4a1dcaf40617c44afb" => :yosemite
