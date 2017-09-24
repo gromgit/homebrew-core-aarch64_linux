ff14728a3646836cf9dbed982e73327aeb05c713
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-24 01:21:46 +0000

    zyre: update 2.0.0 bottle.

--- Formula/zyre.rb
+++ Formula/zyre.rb
@@ -6,6 +6,7 @@ class Zyre < Formula
 
   bottle do
     cellar :any
+    sha256 "7f9c25da501db588f3268e4f1fe99ec58357b41cd601a61859ebbce2eb875dee" => :high_sierra
     sha256 "f8f694368da98cd4781d43b1e4e18db94584ce1f8508b41d492d81194a15db3c" => :sierra
     sha256 "1402b11567fa689064366bf9f8fe9527dba8dfe9246e35b02130a344aa879a9b" => :el_capitan
     sha256 "1170a594d0eff7a57df26150d92daa37382ca6469d320e84957afb184560f691" => :yosemite
