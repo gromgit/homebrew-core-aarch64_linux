f53e6487b9e4e9e901ddf888ab0ca5bfba6c2f80
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-24 02:14:58 +0000

    qdae: update 0.0.10 bottle.

--- Formula/qdae.rb
+++ Formula/qdae.rb
@@ -5,6 +5,7 @@ class Qdae < Formula
   sha256 "780752c37c9ec68dd0cd08bd6fe288a1028277e10f74ef405ca200770edb5227"
 
   bottle do
+    sha256 "60fdb0801b8db3e5b36bc896aca8d4e71278ffe2106019e38f34ffd9605500ec" => :high_sierra
     sha256 "bf2425bdeea4a4ac407056410f6758d44f2b31a9c9afc13871f96914c1d17651" => :sierra
     sha256 "b8882074853ecfcbf6d2426361d5baa553380efbb5896777f0c829cac65811a4" => :el_capitan
     sha256 "1a4eeaff6ff2a86179b673c04837aebe2664ca1eeb065e3081296f653b762cc7" => :yosemite
