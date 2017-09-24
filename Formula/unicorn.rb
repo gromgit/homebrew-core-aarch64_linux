d1037567777679af3d40a6ea8d9777684e5ca753
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-24 01:20:33 +0000

    unicorn: update 1.0.1 bottle.

--- Formula/unicorn.rb
+++ Formula/unicorn.rb
@@ -7,6 +7,7 @@ class Unicorn < Formula
 
   bottle do
     cellar :any
+    sha256 "a12e18a0a334fa19a2dc54a43fc5d56861e31cf5a9ad352cb1150bb6ec61703c" => :high_sierra
     sha256 "81d29e7f28335317dd40976d904636ccd7d0679b71747ad13530dc991f327122" => :sierra
     sha256 "3519d8189333c5ae43eb618e18db7b6be4cf9cc288c6a45ca3b618964d62395c" => :el_capitan
     sha256 "f8c7cb546985c5e34dddb2c2e338314d024e266085fcbdc3f7e52e0f426e4e29" => :yosemite
