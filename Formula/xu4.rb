80de847d1db2fd467b582d958dcc2a02365b1e94
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-24 00:06:38 +0000

    xu4: update 1.0beta4+r3088 bottle.

--- Formula/xu4.rb
+++ Formula/xu4.rb
@@ -11,6 +11,7 @@ class Xu4 < Formula
 
   bottle do
     cellar :any
+    sha256 "e7aac43710b9180710c9af0dcd80a955aa095d6264f92eb482cbe2322c01146d" => :high_sierra
     sha256 "9d490768b07abfa8c105c88dc0d2a6be97e175f6535afcee0a4c63e37d976f6f" => :sierra
     sha256 "47700a341c1dc6a700d5a0f2925846b2457e3f470b25c2288c8c0c132202d027" => :el_capitan
     sha256 "aab6d22274a345209ff25516b2dd11b715088ccab2d31588b8253f65e29f0e96" => :yosemite
