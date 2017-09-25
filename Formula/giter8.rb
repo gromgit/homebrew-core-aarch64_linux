08c0a8912b88e1b3400681b9c04ea3141d76a275
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-25 07:19:20 +0000

    giter8: update 0.10.0 bottle.

--- Formula/giter8.rb
+++ Formula/giter8.rb
@@ -6,10 +6,9 @@ class Giter8 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "30635c5727deba2636dead6a08d8c898473ca5039dba4e3294b09213fbcbda9d" => :high_sierra
-    sha256 "9621b859cafeda7d9d6d52228b6d5c251147256b473db85fcfba9ac84c8d5317" => :sierra
-    sha256 "97ef6c779e1b99ebcdc474ff1ad07a3d2d4c510629d4c3b30e6abed9104e1d9b" => :el_capitan
-    sha256 "85b7e2737383ffdd21a6eba386b492bb2aa06d6084cf133b243755610533a06a" => :yosemite
+    sha256 "d802cf462c8283e0c9663a61dde82bce7d482da24b2ce71b2cb233e5c589e362" => :high_sierra
+    sha256 "3841b04b665c9121d698fadfe94d4dc0ccaa4c5b36d7a590d54186c7d7583afc" => :sierra
+    sha256 "1d45736f60b6fdb1af3e2ee607b256caa99536e27a237d2f834fe2d3fb15326f" => :el_capitan
   end
 
   depends_on :java => "1.6+"
