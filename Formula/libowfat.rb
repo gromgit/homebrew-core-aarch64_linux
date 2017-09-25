d735caa9f565d4f4e07a46f5d99eb6361f860ef3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-25 09:54:28 +0000

    libowfat: update 0.31 bottle.

--- Formula/libowfat.rb
+++ Formula/libowfat.rb
@@ -7,12 +7,9 @@ class Libowfat < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 2
-    sha256 "4be1305b60bb8f5d3e283d58f3d8649b98289946c5abf55c42fb174c00ed5e8e" => :high_sierra
-    sha256 "07d058c654a42d34821399ff800dfa22e928a64bcba4f465443197fe4168e97a" => :sierra
-    sha256 "b8f9b6f14ef4ca9e703ef458659604738f2f6b11899fb9546d8764dde47de4c0" => :el_capitan
-    sha256 "f84024088302c6452f32f2398d0c39a59ee7f2a2eda117e516608e0299763dda" => :yosemite
-    sha256 "a05b57c64730e4780012a06b61224a63e492173e3d6fa191516e408e9578a105" => :mavericks
+    sha256 "43e3968245f33399038ffb25f48618be370cb8242f38ddc36170b76cfd0da3fe" => :high_sierra
+    sha256 "4f719fe2a03651ecea7882464e5b8fd1f4f3b1e32a0f75f9e5cd9e66ad32a123" => :sierra
+    sha256 "be87e0da446834d6f8f808c434e854ff7c9eb88c3f899fc48a830b36117cac83" => :el_capitan
   end
 
   def install
