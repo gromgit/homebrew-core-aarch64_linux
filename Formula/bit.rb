d05b58a765ec0c109901144cdcaddb2ea8cd0687
Amit <amit@cocycles.com>
2017-09-25 08:27:17 +0300

    bit 0.10.7
    build from source
    
    Closes #18511.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/bit.rb
+++ Formula/bit.rb
@@ -1,9 +1,11 @@
 require "language/node"
+
 class Bit < Formula
   desc "Distributed Code Component Manager"
   homepage "https://www.bitsrc.io"
-  url "https://bitsrc.jfrog.io/bitsrc/bit-brew/stable/bit/0.10.3/bit-0.10.3-brew.tar.gz"
-  sha256 "e843e29b34ba8c93c68503f288be28c841c159b67a54bcc88063328167a32785"
+  url "https://registry.npmjs.org/bit-bin/-/bit-bin-0.10.7.tgz"
+  sha256 "d033b76974be7103933abe1fc937297d29c19aa1cdabbba8d5032ebd9d4f72f0"
+  head "https://github.com/teambit/bit.git"
 
   bottle do
     cellar :any_skip_relocation
@@ -13,11 +15,11 @@ class Bit < Formula
     sha256 "67a6a52d87f99c90171b82f1f10c784a686969a30366e5c119d6b450a04271bf" => :yosemite
   end
 
+  depends_on "node"
+
   def install
-    libexec.install Dir["*"]
-    bin.install_symlink Dir["#{libexec}/bin/bit"]
-    bin.install_symlink Dir["#{libexec}/bin/bit.js"]
-    bin.install_symlink "#{libexec}/bin/node" => "bitNode"
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
