b07d3f1adef01076ae3c3f92ccda0420639a78ad
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-25 08:42:06 +0000

    libofx: update 0.9.12 bottle.

--- Formula/libofx.rb
+++ Formula/libofx.rb
@@ -5,11 +5,9 @@ class Libofx < Formula
   sha256 "c15fa062fa11e759eb6d8c7842191db2185ee1b221a3f75e9650e2849d7b7373"
 
   bottle do
-    sha256 "81b676fdb2d88fb270bc189af82631dcdf4387ab296472ceea518b48bd307d1b" => :high_sierra
-    sha256 "f43a3d30d5490038591245675c739613720c50502630cf21574e2de401b5ad8f" => :sierra
-    sha256 "51e0d70b279d22394058c04cc4509788047d9d6cdb0ac7ed7a121f2863bddb7b" => :el_capitan
-    sha256 "f569247d3c0849ed8ebe0d639fb281f64ce175c6db38241f1c60e5d46dc0cff6" => :yosemite
-    sha256 "d2aedd050d47d0ca1274a34cf6ae14f3a2a4c8db65309ab3d46554c9d64ccd2b" => :mavericks
+    sha256 "b682be169269451309ccc7827782a66489e8cf3c9b1793a6b03aa51fd1a943e6" => :high_sierra
+    sha256 "74ccac1d72a7c16eb296c19ea7396f504ab7428aa0b302a84629cd00ef64d6ac" => :sierra
+    sha256 "1e5b91a3e74e5bf3f8ae5ac577a0ad1c81c679a328abee6770b9671fd678456d" => :el_capitan
   end
 
   depends_on "open-sp"
