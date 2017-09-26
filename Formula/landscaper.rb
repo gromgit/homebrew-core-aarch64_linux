a4069a12afd50056284091b1fe007cc549828885
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-26 11:37:39 +0000

    landscaper: add 1.0.10 bottle.

--- Formula/landscaper.rb
+++ Formula/landscaper.rb
@@ -6,6 +6,13 @@ class Landscaper < Formula
       :revision => "c715c3b05d73ee8baf6b1c141524ff90ed80af2e"
   head "https://github.com/Eneco/landscaper.git"
 
+  bottle do
+    cellar :any
+    sha256 "6bbefd914dca42b8e3024e910461b43b8d173d5c9eb91a99fca205414bd2ce95" => :high_sierra
+    sha256 "1c3f8a04554b7f3d71fd394ad8cffb8bc847be084477d52d55627e8e0e334a21" => :sierra
+    sha256 "9939bfe87fb3b24290410d0fdae6dd34bce440ee2c78d2e65aef215fcec93782" => :el_capitan
+  end
+
   depends_on "go" => :build
   depends_on "glide" => :build
   depends_on "kubernetes-cli" => :run
