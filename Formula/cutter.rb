9996105c5b1a1ae265ba81e23b5d88ce3d450a41
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2017-09-26 16:18:09 +0900

    cutter 1.2.6
    Closes #18555.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/cutter.rb
+++ Formula/cutter.rb
@@ -1,8 +1,8 @@
 class Cutter < Formula
   desc "Unit Testing Framework for C and C++"
   homepage "https://cutter.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/cutter/cutter/1.2.5/cutter-1.2.5.tar.gz"
-  sha256 "e53613445e8fe20173a656db5a70a7eb0c4586be1d9f33dc93e2eddd2f646b20"
+  url "https://downloads.sourceforge.net/project/cutter/cutter/1.2.6/cutter-1.2.6.tar.gz"
+  sha256 "459becce78ec4c568f4f5a5e20c73ea36be283bf955f4a252e8491b634339065"
   head "https://github.com/clear-code/cutter.git"
 
   bottle do
