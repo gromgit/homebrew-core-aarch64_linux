f4ed9eb21776437bcd6d82cda6b74cf0a1943776
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-26 13:35:04 +0000

    osc: update 0.160.0 bottle.

--- Formula/osc.rb
+++ Formula/osc.rb
@@ -9,9 +9,9 @@ class Osc < Formula
 
   bottle do
     cellar :any
-    sha256 "57f09ae1c049b500cf8fe4240aa0f093c4a6562635898a7af06950b052d0ba49" => :sierra
-    sha256 "5ad7d2739d48eb474aab764f6dc170a2bfd671d29338a4940f887467fdfe28dc" => :el_capitan
-    sha256 "0b0de3aa214f57e3a180c485c4649e34dbeed073ec038acb7178342c2a8e7ce3" => :yosemite
+    sha256 "8843621eaffda70bb244b612f684ccf214c3b2fdbdaa2da1ac635c447c8cc016" => :high_sierra
+    sha256 "c717e194594aff3e55d8ac71a7cea7aa0e58381493804a96e859e8fe48913a4b" => :sierra
+    sha256 "762d997c8b1d87b79e451c9f482ef4f67025be07bdaad50056d7de2b97499079" => :el_capitan
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
