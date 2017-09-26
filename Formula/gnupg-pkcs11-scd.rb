aab0ef26cad95ff52e8205a074c98d23fa4cea33
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-26 08:10:26 +0000

    gnupg-pkcs11-scd: update 0.9.1 bottle.

--- Formula/gnupg-pkcs11-scd.rb
+++ Formula/gnupg-pkcs11-scd.rb
@@ -6,10 +6,9 @@ class GnupgPkcs11Scd < Formula
 
   bottle do
     cellar :any
-    sha256 "74a757b255ac7eddf9fd786c2ae817d8ffcc9dba00db4d3fc8699ade1f63cba3" => :high_sierra
-    sha256 "3b41d4df2c52d4617316e4a34f803b4682c46d1138a6a13e8fda3912cb1dec5a" => :sierra
-    sha256 "932defd40d09b9c69c4eba55136d03fba708ec7eb078c4e0c3ec044e26169352" => :el_capitan
-    sha256 "ea494eb88c5cf518b1bd7e4190f81b20ba9cd29256137a8b2cb7bbe0524b0a52" => :yosemite
+    sha256 "b1b94851685ebb8dc920e4e5212c2b7effa2e831f184d8b8e66e44b1c9630d93" => :high_sierra
+    sha256 "eccf8f2df5f2007142529fd3b34085ef3f81500173dc6fccd0a94dfe76c7ad19" => :sierra
+    sha256 "0b236bc743da30e0db9458d55a1e511c792d9ed85c97c726a87b5932e8a21dc7" => :el_capitan
   end
 
   depends_on "autoconf" => :build
