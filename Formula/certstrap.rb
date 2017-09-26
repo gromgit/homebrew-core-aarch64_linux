c8ed0ab5e7a70219ab3a38759752207e912f959c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-26 08:17:48 +0000

    certstrap: update 1.1.1 bottle.

--- Formula/certstrap.rb
+++ Formula/certstrap.rb
@@ -5,10 +5,10 @@ class Certstrap < Formula
   sha256 "412ba90a4a48d535682f3c7529191cd30cd7a731e57065dcf4242155cec49d5e"
 
   bottle do
-    rebuild 1
-    sha256 "168d381366cc94bd625de836d50064320130a09a3da9c6c2f973aebceb80482a" => :high_sierra
-    sha256 "b6ae41f23e2e588ec637558a68ab4d0d279326713bbf443fc8a26c7f9340498f" => :sierra
-    sha256 "a6468f4513ef3490f786a1079701d3b22d44a59dc2b1ddf70b1e43b726365e70" => :el_capitan
+    cellar :any
+    sha256 "2151866d10f1ba703fbdc8b11632da00eb3588d4041be018721fcaf6278fec14" => :high_sierra
+    sha256 "58a68f5a88ff0dc4321aeac2aad21fef2edfa85564d6766d3a9149ceebb2cf4b" => :sierra
+    sha256 "dde1e9de937ea5cd7454bc7163a4fddd56ef75209edc7e6036121f57fa47fe23" => :el_capitan
   end
 
   depends_on "go" => :build
