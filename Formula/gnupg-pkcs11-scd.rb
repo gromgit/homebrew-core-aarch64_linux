f91e5d5cfe0271769a395cb008fe26a5dc171743
ilovezfs <ilovezfs@icloud.com>
2017-09-26 00:06:03 -0700

    gnupg-pkcs11-scd 0.9.1
    Closes #18547.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gnupg-pkcs11-scd.rb
+++ Formula/gnupg-pkcs11-scd.rb
@@ -1,8 +1,8 @@
 class GnupgPkcs11Scd < Formula
   desc "Enable the use of PKCS#11 tokens with GnuPG"
   homepage "https://gnupg-pkcs11.sourceforge.io"
-  url "https://github.com/alonbl/gnupg-pkcs11-scd/releases/download/gnupg-pkcs11-scd-0.9.0/gnupg-pkcs11-scd-0.9.0.tar.bz2"
-  sha256 "8f9a2b56ef9c1ae0f6c9146cc842c05a8b77da5be2548b1c92bd555c5e868814"
+  url "https://github.com/alonbl/gnupg-pkcs11-scd/releases/download/gnupg-pkcs11-scd-0.9.1/gnupg-pkcs11-scd-0.9.1.tar.bz2"
+  sha256 "abd3d13eb889c3793da319ddedd0f9b688572abb51b050d8284d1b44dfca94a9"
 
   bottle do
     cellar :any
@@ -20,14 +20,6 @@ class GnupgPkcs11Scd < Formula
   depends_on "libassuan"
   depends_on "libgcrypt"
   depends_on "pkcs11-helper"
-
-  # Remove for > 0.9.0
-  # Fix "incomplete type 'struct ucred' and undeclared identifier 'SO_PEERCRED'"
-  # Reported 26 Aug 2017 https://github.com/alonbl/gnupg-pkcs11-scd/issues/8
-  patch do
-    url "https://github.com/alonbl/gnupg-pkcs11-scd/pull/9.patch?full_index=1"
-    sha256 "d4a2d37e9d54eefd69244422ff8bfffc98b43816a2e24ea8b59b8cb1b04d7195"
-  end
 
   def install
     system "autoreconf", "-fiv"
