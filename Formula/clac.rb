7521295a321db968f29e6e52c8e8bca8708c4886
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-27 10:46:24 +0000

    clac: add 0.3.0 bottle.

--- Formula/clac.rb
+++ Formula/clac.rb
@@ -4,6 +4,13 @@ class Clac < Formula
   url "https://github.com/soveran/clac/archive/0.3.0.tar.gz"
   sha256 "a7a2397fdbac93924ca83c72e2df2027029f54c8921b813230208dd167ed0c20"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "47fcb34fd72b29618f56fffcd571541c1e799e7066807d130af6723dd5615af1" => :high_sierra
+    sha256 "0b3dfbfceadc4fd035b932e0d84437f0c35fb50001ecf33bf2d39732d9c41fd9" => :sierra
+    sha256 "495572ab079dd58563888ce31af20269497802f670429036bc41578bf9d90c8a" => :el_capitan
+  end
+
   def install
     system "make", "PREFIX=#{prefix}", "install"
   end
