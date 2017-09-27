99a579c4eeca5230e903b7048041d6721619bc42
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-27 10:24:54 +0000

    packetbeat: update 5.6.2 bottle.

--- Formula/packetbeat.rb
+++ Formula/packetbeat.rb
@@ -7,10 +7,11 @@ class Packetbeat < Formula
   head "https://github.com/elastic/beats.git"
 
   bottle do
-    cellar :any
-    sha256 "a3ccb75ac30f933362239035a280ea0127963e760cea972b7340eae29bff882d" => :high_sierra
-    sha256 "011ac832ad271ae3a9f3879fd6dc69f103be9f5a6cc88179e40e593dc0332b5b" => :sierra
-    sha256 "1c371fc105f7311f0a19084a55f1df6b8b937764d20650485cf7f39471a33eb8" => :el_capitan
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 "ac0bb249446b1b283855886db2dea7171ca03cc4c0922bb01c0632d8545d6843" => :high_sierra
+    sha256 "e897bb2b82a8666eacf7c3c1245c5506b98fbdbd0a1ec026b8499273c42654bb" => :sierra
+    sha256 "5147d16457ff751c073c5064a03cef73914503768a5baac52a01fb6d4d8ab3e2" => :el_capitan
   end
 
   depends_on "go" => :build
