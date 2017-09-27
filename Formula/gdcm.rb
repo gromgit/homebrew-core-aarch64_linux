ded9d8c9b6403f2730e8321b896ffd7309850573
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-27 10:30:16 +0000

    gdcm: add 2.8.2_1 bottle.

--- Formula/gdcm.rb
+++ Formula/gdcm.rb
@@ -5,6 +5,12 @@ class Gdcm < Formula
   sha256 "5462c7859e01e5d5d0fb86a19a6c775484a6c44abd8545ea71180d4c41bf0f89"
   revision 1
 
+  bottle do
+    sha256 "3fd15851c093b4e0c2481233268e41614b26dde24aa2c13f9107402ae8bf2ddd" => :high_sierra
+    sha256 "4fad6b9761ca2ab4c9b26b418d57ef26f2dd19bebb77a658b5b5aadda0212098" => :sierra
+    sha256 "dbfbcaa31ceaedc98985614899601f771e951cbd6c115223082ab73ef111f231" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "openssl"
   depends_on "vtk"
