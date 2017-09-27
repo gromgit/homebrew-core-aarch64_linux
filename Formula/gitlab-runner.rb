16cc041e238c30d1f0b67c89544f2c1d5cda43c3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-27 14:50:26 +0000

    gitlab-runner: update 10.0.0 bottle.

--- Formula/gitlab-runner.rb
+++ Formula/gitlab-runner.rb
@@ -10,10 +10,9 @@ class GitlabRunner < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "96b7bb2bac51c8494bc760291803846e140a2761e23e48b8b555e39bb2cbfc64" => :high_sierra
-    sha256 "3b5f6024763210b3f83a187ff903ded10051c29a974b53d19d74479dfc8df573" => :sierra
-    sha256 "810a9b4c7edd299bfc61ad44ceba7208c4e127e44baf778858a6f1af1500d4f8" => :el_capitan
-    sha256 "9e7473da8fd43a3025904ff09b2f95eb580d33f0e20c13d909888c2f1a94a768" => :yosemite
+    sha256 "8ce3f395f542c5abb98e9ab34014ee86f791287c11bec8a774cf22f94bfbeb38" => :high_sierra
+    sha256 "acb74e44d98d45fda10816b30f2bed34075162978e374d1f11148eeaddf19e64" => :sierra
+    sha256 "a3a7f46a1e240cfecaec968020da021450fad8bd5a8625a43b07ae685791c02d" => :el_capitan
   end
 
   depends_on "go" => :build
