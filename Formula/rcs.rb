d394dfac4e5ebd1f0ec3520418fb8faa40fc3336
ilovezfs <ilovezfs@icloud.com>
2017-09-26 21:34:42 -0700

    rcs: %n crash fix for 10.13
    Fix crash from the use of %n in dynamic format strings on High Sierra.
    
    Fixes #18566.
    Fixes #18593.
    
    Closes #18594.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/rcs.rb
+++ Formula/rcs.rb
@@ -4,6 +4,7 @@ class Rcs < Formula
   url "https://ftp.gnu.org/gnu/rcs/rcs-5.9.4.tar.xz"
   mirror "https://ftpmirror.gnu.org/rcs/rcs-5.9.4.tar.xz"
   sha256 "063d5a0d7da1821754b80c639cdae2c82b535c8ff4131f75dc7bbf0cd63a5dff"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -20,6 +21,13 @@ class Rcs < Formula
   patch do
     url "https://raw.githubusercontent.com/Homebrew/formula-patches/3fff7c990b8df4174045834b9c1210e7736ff5a4/rcs/noreturn.patch"
     sha256 "ac2f5ad1df932361e19c6184d2dfddfbe7664184ac4c24a3224c85707cd4da9f"
+  end
+
+  if MacOS.version >= :high_sierra
+    patch :p0 do
+      url "https://raw.githubusercontent.com/macports/macports-ports/b76d1e48dac/editors/nano/files/secure_snprintf.patch"
+      sha256 "57f972940a10d448efbd3d5ba46e65979ae4eea93681a85e1d998060b356e0d2"
+    end
   end
 
   def install
