9c144db062ad959ffa241bcd648c92ae895327e8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-27 13:53:13 +0000

    ndpi: update 2.0_1 bottle.

--- Formula/ndpi.rb
+++ Formula/ndpi.rb
@@ -8,9 +8,9 @@ class Ndpi < Formula
 
   bottle do
     cellar :any
-    sha256 "e0b6695cab0cadeac7383e7a86fae6795e5def53411ae6167668ac6b8f97231b" => :sierra
-    sha256 "ed0fcd50c950d724e1b32158c09832e0e04c5918b5e42d8131dd7f09ee655002" => :el_capitan
-    sha256 "71c9b7939c80c3854944d81f308e519f92e9f8d0e2a65e5a30ac78222c54d15d" => :yosemite
+    sha256 "0cc0a288965cf60916c9732edf73332797a1931314659786acb1a944fad43ee1" => :high_sierra
+    sha256 "ae229a6b0c648df903a9036ef9c5a8ce801a97e95158ece953669da55009a40b" => :sierra
+    sha256 "855da8002f3632da5d0b576c148eabe708e598b80d9f533e07c8d0f8e43ede72" => :el_capitan
   end
 
   depends_on "autoconf" => :build
