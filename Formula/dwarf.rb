62e1a055e2e60ef348a61242e996dc54dbcff9e9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-28 08:27:25 +0000

    dwarf: update 0.4.0 bottle.

--- Formula/dwarf.rb
+++ Formula/dwarf.rb
@@ -5,10 +5,9 @@ class Dwarf < Formula
   sha256 "a64656f53ded5166041ae25cc4b1ad9ab5046a5c4d4c05b727447e73c0d83da0"
 
   bottle do
-    sha256 "ef1d01f93b35c4661d08b9a8a710cd99092a533735c646db2e205c8f2db93b95" => :high_sierra
-    sha256 "84c3d641587619c55073a819edf62b23a6437aeed72075e257272df685e226aa" => :sierra
-    sha256 "053fba2171b46fe1d9fd22f52ca5eee61e462682e2b9340c671505e5351fd5d6" => :el_capitan
-    sha256 "1a798403cb54f055465e16fe67e7db63dd693ee993d0a871c32c6f143621d7f3" => :yosemite
+    sha256 "47af5ca1f6349c0d8c49f1ef72cbed7fbaa6e91c263c8a284afd841e8ced56f9" => :high_sierra
+    sha256 "69a1b11620f73e519cf6abade5a23e478a4c7c44cd4fbc9996b05c464c53e8d7" => :sierra
+    sha256 "042c140300ff583047ec5f51e3877b4ecf22a3c25a6108c11ae067b9e525d05b" => :el_capitan
   end
 
   depends_on "flex"
