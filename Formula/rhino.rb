d9a75d0c9944f0cf24b54cccf40e44753f948053
ilovezfs <ilovezfs@icloud.com>
2017-09-27 18:21:37 -0700

    rhino: update checksum (#18635)
    and remove the gymnastics from the install method since the zip file has
    been fixed.

--- Formula/rhino.rb
+++ Formula/rhino.rb
@@ -1,20 +1,14 @@
 class Rhino < Formula
   desc "JavaScript engine"
   homepage "https://www.mozilla.org/rhino/"
-  url "https://github.com/mozilla/rhino/releases/download/Rhino1_7_7_2_Release/rhino-1.7.7.2.zip",
-      :using => :nounzip
-  sha256 "20b5736a2e596cf9367266dc749791f590838e5236b6bd2d21d582a560b33c4d"
+  url "https://github.com/mozilla/rhino/releases/download/Rhino1_7_7_2_Release/rhino-1.7.7.2.zip"
+  sha256 "b223595717dd360061b35aaa7e952271fcbbd6a763a78927283480227aaa0247"
 
   bottle :unneeded
 
   conflicts_with "nut", :because => "both install `rhino` binaries"
 
   def install
-    # zip file contains duplicates
-    # Reported 1 Sep 2017 https://github.com/mozilla/rhino/issues/321
-    system "unzip", "-qqo", "rhino-#{version}.zip"
-    cd "rhino#{version}"
-
     rhino_jar = "rhino-#{version}.jar"
     libexec.install "lib/#{rhino_jar}"
     bin.write_jar_script libexec/rhino_jar, "rhino"
