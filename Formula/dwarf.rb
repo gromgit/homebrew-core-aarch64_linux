e394bbd7ff9b5f4473ea8f839c63c6c2dc18036d
ilovezfs <ilovezfs@icloud.com>
2017-09-27 21:18:03 -0700

    dwarf 0.4.0
    Closes #18645.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dwarf.rb
+++ Formula/dwarf.rb
@@ -1,8 +1,8 @@
 class Dwarf < Formula
   desc "Object file manipulation tool"
   homepage "https://github.com/elboza/dwarf-ng/"
-  url "https://github.com/elboza/dwarf-ng/archive/dwarf-0.3.2.tar.gz"
-  sha256 "dc3db5273c02f0b05beedada0a3935af0c60c011d7213f37c39f43ce088f9436"
+  url "https://github.com/elboza/dwarf-ng/archive/dwarf-0.4.0.tar.gz"
+  sha256 "a64656f53ded5166041ae25cc4b1ad9ab5046a5c4d4c05b727447e73c0d83da0"
 
   bottle do
     sha256 "ef1d01f93b35c4661d08b9a8a710cd99092a533735c646db2e205c8f2db93b95" => :high_sierra
@@ -32,7 +32,7 @@ class Dwarf < Formula
       }
     EOS
     system ENV.cc, "test.c", "-o", "test"
-    output = shell_output("#{bin}/dwarf -c 'print $mac' test")
-    assert_equal "magic: 0xfeedfacf (-17958193)", output.lines[1].chomp
+    output = shell_output("#{bin}/dwarf -c 'pp $mac' test")
+    assert_equal "magic: 0xfeedfacf (-17958193)", output.lines[0].chomp
   end
 end
