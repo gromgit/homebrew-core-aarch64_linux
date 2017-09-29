52ab423da9963de587d35b8ff872d3062b29c01f
Aimilius <aimilius@protonmail.com>
2017-09-29 18:26:02 +0200

    fbi-servefiles 2.4.13 (new formula)
    Closes #18718.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/fbi-servefiles.rb
@@ -0,0 +1,49 @@
+class FbiServefiles < Formula
+  include Language::Python::Virtualenv
+  desc "Serve local files to Nintendo 3DS via FBI remote installer"
+  homepage "https://github.com/Steveice10/FBI"
+  url "https://github.com/Steveice10/FBI/archive/2.4.13.tar.gz"
+  sha256 "f03d96a32622fd3b9732ff734d9d65b50d4af445eed8e6eb77fd9641265eb42e"
+
+  depends_on :python if MacOS.version <= :snow_leopard
+
+  def install
+    venv = virtualenv_create(libexec)
+    venv.pip_install_and_link buildpath/"servefiles"
+  end
+
+  test do
+    require "socket"
+
+    def test_socket
+      server = TCPServer.new(5000)
+      client = server.accept
+      client.puts "\n"
+      client_response = client.gets
+      client.close
+      server.close
+      client_response
+    end
+
+    begin
+      pid = fork do
+        system "#{bin}/sendurls.py", "127.0.0.1", "https://github.com"
+      end
+      assert_match "https://github.com", test_socket
+    ensure
+      Process.kill("TERM", pid)
+      Process.wait(pid)
+    end
+
+    begin
+      touch "test.cia"
+      pid = fork do
+        system "#{bin}/servefiles.py", "127.0.0.1", "test.cia", "127.0.0.1", "8080"
+      end
+      assert_match "127.0.0.1:8080/test.cia", test_socket
+    ensure
+      Process.kill("TERM", pid)
+      Process.wait(pid)
+    end
+  end
+end
