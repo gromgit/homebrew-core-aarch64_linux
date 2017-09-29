92a3a2749eed7e11c5d58912fa5c0a98cc14fb9a
ilovezfs <ilovezfs@icloud.com>
2017-09-28 23:12:07 -0700

    faac 1.29.7.7
    Closes #18693.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/faac.rb
+++ Formula/faac.rb
@@ -1,8 +1,8 @@
 class Faac < Formula
   desc "ISO AAC audio encoder"
   homepage "http://www.audiocoding.com/faac.html"
-  url "https://downloads.sourceforge.net/project/faac/faac-src/faac-1.29/faac-1.29.7.6.tar.gz"
-  sha256 "36298549deab66b4b9bb274ecbe74514bb7c83f309265f8f649cf49a44b9bd9f"
+  url "https://downloads.sourceforge.net/project/faac/faac-src/faac-1.29/faac-1.29.7.7.tar.gz"
+  sha256 "b898fcf55e7b52f964ee62d077f56fe9b3b35650e228f006fbef4ce903b4d731"
 
   bottle do
     cellar :any
