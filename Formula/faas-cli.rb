f43b123532608926af0ca7b8c5d5627b5d532d38
ilovezfs <ilovezfs@icloud.com>
2017-09-29 16:30:41 -0700

    faas-cli 0.4.16
    Closes #18750.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/faas-cli.rb
+++ Formula/faas-cli.rb
@@ -2,8 +2,8 @@ class FaasCli < Formula
   desc "CLI for templating and/or deploying FaaS functions"
   homepage "http://docs.get-faas.com/"
   url "https://github.com/openfaas/faas-cli.git",
-      :tag => "0.4.15",
-      :revision => "ae3b137914f8478a4b7bf14db24df6677f9f278f"
+      :tag => "0.4.16",
+      :revision => "8fead1a494318efcc51b97c17334038b8b53d871"
 
   bottle do
     cellar :any_skip_relocation
@@ -18,10 +18,10 @@ class FaasCli < Formula
     ENV["XC_OS"] = "darwin"
     ENV["XC_ARCH"] = MacOS.prefer_64_bit? ? "amd64" : "386"
     ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/alexellis/faas-cli").install buildpath.children
-    cd "src/github.com/alexellis/faas-cli" do
+    (buildpath/"src/github.com/openfaas/faas-cli").install buildpath.children
+    cd "src/github.com/openfaas/faas-cli" do
       commit = Utils.popen_read("git rev-list -1 HEAD").chomp
-      system "go", "build", "-ldflags", "-s -w -X github.com/alexellis/faas-cli/commands.GitCommit=#{commit}", "-a",
+      system "go", "build", "-ldflags", "-s -w -X github.com/openfaas/faas-cli/commands.GitCommit=#{commit}", "-a",
              "-installsuffix", "cgo", "-o", bin/"faas-cli"
       prefix.install_metafiles
     end
@@ -62,13 +62,14 @@ class FaasCli < Formula
       Deploying: dummy_function.
       Removing old service.
       Deployed.
-      200 OK
       URL: http://localhost:#{port}/function/dummy_function
+
+      200 OK
     EOS
 
     begin
       output = shell_output("#{bin}/faas-cli deploy -yaml test.yml")
-      assert_equal expected, output.chomp
+      assert_equal expected, output
 
       commit = Utils.popen_read("git rev-list -1 HEAD").chomp
       output = shell_output("#{bin}/faas-cli version")
