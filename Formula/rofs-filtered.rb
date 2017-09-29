7a44f894e383304007c40f8196fd9689424f05b1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-29 09:43:31 +0000

    rofs-filtered: update 1.7 bottle.

--- Formula/rofs-filtered.rb
+++ Formula/rofs-filtered.rb
@@ -5,6 +5,7 @@ class RofsFiltered < Formula
   sha256 "d66066dfd0274a2fb7b71dd929445377dd23100b9fa43e3888dbe3fc7e8228e8"
 
   bottle do
+    sha256 "2662076d6c938fd260ee304e80999db5e22a1c516fdc5139c7e477892a5cc927" => :high_sierra
     sha256 "c6f0661d382c415db63caf4512b18b526e5e75c25a5fa54de792d4365db4dfa1" => :sierra
     sha256 "5c29bf0f477b23c3e3291549cffe632d3222899a34483cd8c23fe25f4d9b30ca" => :el_capitan
     sha256 "194e5e00804165b7df68037bce7d6bd6b3f64a244b1f48c04b16e346bd70832c" => :yosemite
