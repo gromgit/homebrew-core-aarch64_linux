b37e2ba723e1f9bdfcc7f5e0bac8ce8e459084b3
ilovezfs <ilovezfs@icloud.com>
2017-09-28 23:51:27 -0700

    dtc 1.4.5
    Closes #18703.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dtc.rb
+++ Formula/dtc.rb
@@ -1,9 +1,9 @@
 class Dtc < Formula
   desc "Device tree compiler"
   homepage "https://www.devicetree.org/"
-  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/d/device-tree-compiler/device-tree-compiler_1.4.4.orig.tar.gz"
-  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/d/device-tree-compiler/device-tree-compiler_1.4.4.orig.tar.gz"
-  sha256 "2f2c0bf4d84763595953885bdcd2159b0b85410018c8ba48cc31b3d6e443e4d8"
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/d/device-tree-compiler/device-tree-compiler_1.4.5.orig.tar.gz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/d/device-tree-compiler/device-tree-compiler_1.4.5.orig.tar.gz"
+  sha256 "d13df67f5402c1905d2c24603471fe783965112ab5004025a50f7f852cd89bc8"
 
   bottle do
     cellar :any
@@ -13,8 +13,8 @@ class Dtc < Formula
   end
 
   def install
-    system "make"
-    system "make", "DESTDIR=#{prefix}", "PREFIX=", "install"
+    system "make", "NO_PYTHON=1"
+    system "make", "NO_PYTHON=1", "DESTDIR=#{prefix}", "PREFIX=", "install"
     mv lib/"libfdt.dylib.1", lib/"libfdt.1.dylib"
   end
 
