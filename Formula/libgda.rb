ba4ea377a01bd762ba2240fd395c62f9ff3c22a9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-29 11:30:58 +0000

    libgda: update 5.2.4_2 bottle.

--- Formula/libgda.rb
+++ Formula/libgda.rb
@@ -6,6 +6,7 @@ class Libgda < Formula
   revision 2
 
   bottle do
+    sha256 "39e348596409d69d57609d0a00c6e9506a9fd52a4f90e585e3b6840bf03ea67e" => :high_sierra
     sha256 "e165830cedc3a0955989746145b310cc03fe96b84f18b33c4c3f2b827bdd473c" => :sierra
     sha256 "7809bb97ebcd233a740c1e5b5cb0f291a902639a6479d5e53fdcfedd928b6582" => :el_capitan
     sha256 "01e46f8673fcf3fad0bccdd70e9bd6fac08f0f5b7035e85318a3add4db329a9b" => :yosemite
