388ce8a426523e5853b86e23417e924e40ca15a8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-30 09:00:37 +0000

    bwfmetaedit: add 1.3.2 bottle.

--- Formula/bwfmetaedit.rb
+++ Formula/bwfmetaedit.rb
@@ -5,6 +5,13 @@ class Bwfmetaedit < Formula
   version "1.3.2"
   sha256 "2109d625cc834e38bf05641d550fcb0fe8c095f3fdc0fa69283d26410e4a9f74"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f9856faf39690116683ebaa271d804fab3fc981f11043c52dd36b99af95f1b49" => :high_sierra
+    sha256 "65f5716802ac4751a41d3890fff61d709a9c53ab4c4f4f4875a35f28b08818f9" => :sierra
+    sha256 "861025e945a567a60d7409256d4108f121054d8c4335234a92b983e5bf195faf" => :el_capitan
+  end
+
   def install
     cd "Project/GNU/CLI" do
       system "./configure",  "--disable-debug", "--prefix=#{prefix}"
