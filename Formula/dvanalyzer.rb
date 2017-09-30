c90be9d5d6bc1973e3a901774d98b9b3e6c7bcf2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-30 08:10:37 +0000

    dvanalyzer: add 1.4.2 bottle.

--- Formula/dvanalyzer.rb
+++ Formula/dvanalyzer.rb
@@ -5,6 +5,13 @@ class Dvanalyzer < Formula
   version "1.4.2"
   sha256 "d2f3fdd98574f7db648708e1e46b0e2fa5f9e6e12ca14d2dfaa77c13c165914c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "59667b7174026e959f123ebbf8f8e30559dabb70814565f8bec8316c4b9c02b1" => :high_sierra
+    sha256 "fb066074dde3b6e94ba30bf37bc85c2e17ef30a7e2b8f874b1a09f3aca2275f7" => :sierra
+    sha256 "0e138c105a1f4604dbb4b7c911e83c660f2078cb24af6ba0ba12564a6e93d9c0" => :el_capitan
+  end
+
   def install
     cd "ZenLib/Project/GNU/Library" do
       args = ["--disable-debug",
