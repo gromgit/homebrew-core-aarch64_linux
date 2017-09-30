6d2f9f6fd82923814ac99f56aed674a38dfa4ff7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-30 12:30:24 +0000

    ssh-vault: add 0.12.3 bottle.

--- Formula/ssh-vault.rb
+++ Formula/ssh-vault.rb
@@ -7,6 +7,13 @@ class SshVault < Formula
 
   head "https://github.com/ssh-vault/ssh-vault.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "449945a60458fde34c31a6a8b97fd1e6572dc295ab70bee410a4c3ae2f4c57c1" => :high_sierra
+    sha256 "6fa89a87974ee2b4a791ba0a2f28249b7bd12abc64436a5015e207cb0d73b6a0" => :sierra
+    sha256 "57ab92a375411d4cc60796247a77ea8855ca6ca16411d051ebac75782f1d1403" => :el_capitan
+  end
+
   depends_on "dep" => :build
   depends_on "go" => :build
 
