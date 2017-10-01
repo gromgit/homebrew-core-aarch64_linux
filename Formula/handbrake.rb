3b6f76cc1acdf3e2767dd642adf4aab7b2145f07
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-01 13:18:14 +0000

    handbrake: update 1.0.7 bottle.

--- Formula/handbrake.rb
+++ Formula/handbrake.rb
@@ -6,6 +6,7 @@ class Handbrake < Formula
   head "https://github.com/HandBrake/HandBrake.git"
 
   bottle do
+    sha256 "defe7d13eebbcd54f269fff4fb0d600333bb14a25deea57a4605045356d74d45" => :high_sierra
     sha256 "61a9dce983af5963dc6233fadef0a4c4c79391057f71e584298172e6fe80d3dc" => :sierra
     sha256 "37c825fa81b75fa8ea85ff58b00fe152a71acaaa52d02a490a8c13766dfa33c1" => :el_capitan
     sha256 "b14c568a5f20f04f6f15805111df263dd231ad86f599af9a928aa6eaf0b4f9d7" => :yosemite
