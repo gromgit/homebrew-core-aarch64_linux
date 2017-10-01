be747150154ea88409a0de3b144d8501968a6f6d
Dominyk Tiller <dominyktiller@gmail.com>
2017-10-01 18:24:43 +0100

    vim@7.4: revision for perl

--- Formula/vim@7.4.rb
+++ Formula/vim@7.4.rb
@@ -3,7 +3,7 @@ class VimAT74 < Formula
   homepage "http://www.vim.org/"
   url "https://github.com/vim/vim/archive/v7.4.2367.tar.gz"
   sha256 "a9ae4031ccd73cc60e771e8bf9b3c8b7f10f63a67efce7f61cd694cd8d7cda5c"
-  revision 3
+  revision 4
 
   bottle do
     sha256 "8edc89ec3ff9f96044bcf4afbcdfaec9e22d090112eb396354316e2965ccef0a" => :high_sierra
