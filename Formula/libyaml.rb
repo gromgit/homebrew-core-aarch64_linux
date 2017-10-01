b16ab4fdb3b36f02cb7f2bccb5ab9e354ccf7d58
Viktor Szakats <vszakats@users.noreply.github.com>
2017-10-01 10:07:17 +0000

    libyaml: secure url(s)

--- Formula/libyaml.rb
+++ Formula/libyaml.rb
@@ -1,7 +1,7 @@
 class Libyaml < Formula
   desc "YAML Parser"
-  homepage "http://pyyaml.org/wiki/LibYAML"
-  url "http://pyyaml.org/download/libyaml/yaml-0.1.7.tar.gz"
+  homepage "https://pyyaml.org/wiki/LibYAML"
+  url "https://pyyaml.org/download/libyaml/yaml-0.1.7.tar.gz"
   mirror "https://mirrors.kernel.org/debian/pool/main/liby/libyaml/libyaml_0.1.7.orig.tar.gz"
   sha256 "8088e457264a98ba451a90b8661fcb4f9d6f478f7265d48322a196cec2480729"
 
