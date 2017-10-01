f11c5081ecdfb8449b36293e9ecd55db274ee659
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-01 13:38:52 +0000

    neatvi: update 06 bottle.

--- Formula/neatvi.rb
+++ Formula/neatvi.rb
@@ -8,10 +8,9 @@ class Neatvi < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "786ba6fcd9f3826fd7cbb5c0ba466f914e996043eaeebebac60f3f3b747a7187" => :high_sierra
-    sha256 "4af6bb9a7d0b654f929665393e97e250b34eb6b678d107d36143f524f7dea173" => :sierra
-    sha256 "0dcdf4372cf9a0c3d837df0471511838539c6250351c93711a981444ab1bb11d" => :el_capitan
-    sha256 "c715f3a039031f83bd71eb6ce21caedba923ad3d78692df24ccfd9ed80b1b099" => :yosemite
+    sha256 "2e34d03e212479064e86e8d1024447badeb61a54205f1fda24f9b6633e22afe7" => :high_sierra
+    sha256 "339a7880dea5f7ff0e290bc890f95719da7e5ba4b64a7205760c8f6cf64e10a2" => :sierra
+    sha256 "c773025ad559bb25cc095a7e1efc8950424a4cd86ff81ee4b0093a0e2e3c3c84" => :el_capitan
   end
 
   def install
