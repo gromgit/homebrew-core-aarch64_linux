5bd7c074191c114f8d2d25efb5819652822466c4
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-10-02 15:17:02 +0100

    gobject-introspection 1.54.1
    Closes #18849.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gobject-introspection.rb
+++ Formula/gobject-introspection.rb
@@ -1,8 +1,8 @@
 class GobjectIntrospection < Formula
   desc "Generate introspection data for GObject libraries"
   homepage "https://live.gnome.org/GObjectIntrospection"
-  url "https://download.gnome.org/sources/gobject-introspection/1.54/gobject-introspection-1.54.0.tar.xz"
-  sha256 "67e76742ce78821bce8a613183368222919b24e2dcf5fe26f17726c11c422ed7"
+  url "https://download.gnome.org/sources/gobject-introspection/1.54/gobject-introspection-1.54.1.tar.xz"
+  sha256 "b88ded5e5f064ab58a93aadecd6d58db2ec9d970648534c63807d4f9a7bb877e"
 
   bottle do
     sha256 "1801ae398f43d965edc607eb3fce1e9ba9010b42e6b43693e716ddf445d6b1cb" => :high_sierra
