c01ef2c926376a9b84c78abf0d125e1fc9749f22
ilovezfs <ilovezfs@icloud.com>
2017-10-02 00:48:10 -0700

    tracebox 0.4.2
    add a test
    
    Closes #18830.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/tracebox.rb
+++ Formula/tracebox.rb
@@ -1,9 +1,21 @@
 class Tracebox < Formula
   desc "Middlebox detection tool"
   homepage "http://www.tracebox.org/"
-  url "https://github.com/tracebox/tracebox.git", :tag => "v0.3.1",
-                                                  :revision => "aec062dcf7198c8b8f3b90ee4216e929ebf0ffcb"
   head "https://github.com/tracebox/tracebox.git"
+
+  stable do
+    url "https://github.com/tracebox/tracebox.git",
+        :tag => "v0.4.2",
+        :revision => "2e3326500ddf084bf761e83516909538d26240da"
+
+    # Remove for > 0.4.2
+    # Upstream commit from 2 Oct 2017 "Remove [--dirty] from the displayed
+    # version string"
+    patch do
+      url "https://github.com/tracebox/tracebox/commit/5ee627c.patch?full_index=1"
+      sha256 "af6fda9484e1188acf35c0fb5f871cebc608c8122e5ad1d94569fe30321549cc"
+    end
+  end
 
   bottle do
     cellar :any
@@ -40,5 +52,9 @@ class Tracebox < Formula
     You should be certain that you trust any software you are executing with
     elevated privileges.
     EOS
+  end
+
+  test do
+    system bin/"tracebox", "-v"
   end
 end
