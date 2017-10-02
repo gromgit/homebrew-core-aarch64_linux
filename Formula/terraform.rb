c1591edb6ddfd29270c3b7ef15967535185f0aa4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-02 21:21:25 +0000

    terraform: update 0.10.7 bottle.

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -9,9 +9,9 @@ class Terraform < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ece6bfbceef260a3e499c9b4d93a43f7062511ddf19c47c5c4eafa811a4a3b8b" => :high_sierra
-    sha256 "cdb4d39f9e2b0f653a58a3bbd3013977c314893fe503378f7a1ef10993a1d767" => :sierra
-    sha256 "9be4c4f86c7d079534bcc5891fe16ed372977761511b1f9493015f9ef71bb5f2" => :el_capitan
+    sha256 "b9735e70c4949199d0fcb6dbd5c5dbcc4311d6fa09a3d14994e9c20257546554" => :high_sierra
+    sha256 "a065e6bac77a8e40f489d3a201407301e977624ea2abeffde37021eb8da3f277" => :sierra
+    sha256 "3f047a1a024a9f815d4bb33f0eff9302eebe969a723ce924c7f789ebb13dcb28" => :el_capitan
   end
 
   depends_on "go" => :build
