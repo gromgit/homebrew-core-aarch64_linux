b6fa711280bf4a9bc9819d2e9738e15ecd9ed405
FX Coudert <fxcoudert@gmail.com>
2017-10-02 21:05:46 +0200

    solarus: fix build
    Closes #18860.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/solarus.rb
+++ Formula/solarus.rb
@@ -1,9 +1,19 @@
 class Solarus < Formula
   desc "Action-RPG game engine"
   homepage "http://www.solarus-games.org/"
-  url "http://www.solarus-games.org/downloads/solarus/solarus-1.5.3-src.tar.gz"
-  sha256 "7608f3bdc7baef36e95db5e4fa4c8c5be0a3f436c50c53ab72d70a92aa44cc1c"
   head "https://github.com/christopho/solarus.git"
+
+  stable do
+    url "http://www.solarus-games.org/downloads/solarus/solarus-1.5.3-src.tar.gz"
+    sha256 "7608f3bdc7baef36e95db5e4fa4c8c5be0a3f436c50c53ab72d70a92aa44cc1c"
+
+    # Upstream patch for build issue, remove in next version
+    # https://github.com/solarus-games/solarus/issues/1084
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/e6a26f3d/solarus/config.patch"
+      sha256 "7bb5c39dd97eca215a22a28dffe23dfac364252f7ff8221e5d76ae40d037be76"
+    end
+  end
 
   bottle do
     cellar :any
