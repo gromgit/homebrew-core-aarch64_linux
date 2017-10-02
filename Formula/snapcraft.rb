c42f9c48fbbd303b86199f5a127eb9886662cdc7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-02 05:34:09 +0000

    snapcraft: update 2.33_2 bottle.

--- Formula/snapcraft.rb
+++ Formula/snapcraft.rb
@@ -9,10 +9,9 @@ class Snapcraft < Formula
 
   bottle do
     cellar :any
-    sha256 "5c1dc3bbf1928ecbbe05a07745098ce52ab1134d2ef29380855db20a177adb76" => :high_sierra
-    sha256 "445403398fe50045431e192a0f97d2f104cac17c13d5b7571a21301cd3b14a21" => :sierra
-    sha256 "3f1b7fb71d58c827d5beba9162f06044455ed15ec3a348f3f6d3c28251592365" => :el_capitan
-    sha256 "c0a7b455994c9843e62205592d6822c6199f3726720c362aa790ea06fc2e6bb4" => :yosemite
+    sha256 "97a2ec3e37fc83e4c635a248c764b732db16c29110f52350f537068c20e0bbc2" => :high_sierra
+    sha256 "df156e747df49cfeb2a24df236e20caacc8940d8a08f7a7a8074a109ca001c06" => :sierra
+    sha256 "0bbae299f0d6bdfdd7d601994f29707b7bfea3a75184d8fe4a53aff50fbc0807" => :el_capitan
   end
 
   depends_on "libmagic"
