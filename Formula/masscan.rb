d88e876a6999ac67b2a24d105a38ad3d1f9f4bf0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-02 22:17:22 +0000

    masscan: update 1.0.4 bottle.

--- Formula/masscan.rb
+++ Formula/masscan.rb
@@ -7,6 +7,7 @@ class Masscan < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "97def50fe25a1b352b1808043693e230d6d0ab4a318389dfd68bd4b60e654ec4" => :high_sierra
     sha256 "d2770cd3ff575b998818f5586f182fe626bc336a48542d1c01656fd9617df1ed" => :sierra
     sha256 "8dbe578c48b421e03a264c82912e10009b1b6163f704f3388a017799accaf0a9" => :el_capitan
     sha256 "969e348d0a3738b1fcc5082a6c0feef0f18d1f462b3d9bec0cd1751781b263e3" => :yosemite
