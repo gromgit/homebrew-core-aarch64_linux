708557f9eaff666367ec01cbb7f59385ee927620
ilovezfs <ilovezfs@icloud.com>
2017-10-01 23:02:02 -0700

    wireguard-tools 0.0.20171001
    Closes #18822.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/wireguard-tools.rb
+++ Formula/wireguard-tools.rb
@@ -3,9 +3,20 @@ class WireguardTools < Formula
   homepage "https://www.wireguard.io/"
   # Please only update version when the tools have been modified/updated,
   # since the Linux module aspect isn't of utility for us.
-  url "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-0.0.20170918.tar.xz"
-  sha256 "e083f18596574fb7050167090bfb4db4df09a1a99f3c1adc77f820c166368881"
   head "https://git.zx2c4.com/WireGuard", :using => :git
+
+  stable do
+    url "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-0.0.20171001.tar.xz"
+    sha256 "ecff9a184685b7dd2d81576eba5bd96bb59031c9e9b5eeee05d6dc298f30998e"
+
+    # Remove for > 0.0.20171001
+    # Fix "error: use of undeclared identifier 'MNL_SOCKET_BUFFER_SIZE'"
+    # Upstream commit from 2 Oct 2017 "tools: compile on non-Linux"
+    patch do
+      url "https://git.zx2c4.com/WireGuard/patch/?id=eb578ac0cbfa0e9b6fd5f8f56ccdc407ca860e96"
+      sha256 "2c73ee2bedad3c1ac6803dbaef0867cb4f6cd9ec42c56fa1a1ba9831b9901f33"
+    end
+  end
 
   bottle do
     cellar :any_skip_relocation
