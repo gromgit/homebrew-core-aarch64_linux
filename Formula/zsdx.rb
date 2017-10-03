b4ad9fed632b8b7faa6f1507470e00a6b1d6df86
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-03 08:11:02 +0000

    zsdx: update 1.11.0 bottle.

--- Formula/zsdx.rb
+++ Formula/zsdx.rb
@@ -7,6 +7,7 @@ class Zsdx < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0460af05acc1c8beeceb5a6058ac0ad2cb27f423d43a87fb45aaf5b737c49f7b" => :high_sierra
     sha256 "2787c78e1b24669a1befa354724f77b6a86abf2aade492fe211c296482855cf8" => :sierra
     sha256 "2a1132ca3dc96d98332d99e1a37b1d2f46206fdad88066f96fedcfbf796452b3" => :el_capitan
     sha256 "c9fd0e90a1cf311d30a3e5b961e15a2e8a5a2400b1d985fc4f8c4591cca051d4" => :yosemite
