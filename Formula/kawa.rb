20f18a68eb0b74215683d265b91d997d19f2fb2b
ilovezfs <ilovezfs@icloud.com>
2017-10-03 00:40:50 -0700

    kawa 3.0 (#18892)

--- Formula/kawa.rb
+++ Formula/kawa.rb
@@ -1,9 +1,9 @@
 class Kawa < Formula
   desc "Programming language for Java (implementation of Scheme)"
   homepage "https://www.gnu.org/software/kawa/"
-  url "https://ftp.gnu.org/gnu/kawa/kawa-2.4.zip"
-  mirror "https://ftpmirror.gnu.org/kawa/kawa-2.4.zip"
-  sha256 "bab0f623a15ad2b3ab74253f446a670b3019d59edf7db7ec22059fe41fa1e6ab"
+  url "https://ftp.gnu.org/gnu/kawa/kawa-3.0.zip"
+  mirror "https://ftpmirror.gnu.org/kawa/kawa-3.0.zip"
+  sha256 "63116eec4b2b2dd8fae0b30127639aa42ad7a7430c4970d3fd76b42a148e423c"
 
   bottle :unneeded
 
@@ -11,7 +11,7 @@ class Kawa < Formula
 
   def install
     rm Dir["bin/*.bat"]
-    inreplace "bin/kawa", "thisfile=`type -p \$0`",
+    inreplace "bin/kawa", "thisfile=`command -v $0`",
                           "thisfile=#{libexec}/bin/kawa"
     libexec.install "bin", "lib"
     bin.install_symlink libexec/"bin/kawa"
