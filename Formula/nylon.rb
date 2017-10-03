5015dd02a5fb2154a2e21f5191105006d740a53c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-03 14:53:09 +0000

    nylon: update 1.21_1 bottle.

--- Formula/nylon.rb
+++ Formula/nylon.rb
@@ -6,6 +6,7 @@ class Nylon < Formula
   revision 1
 
   bottle do
+    sha256 "ad3ce3dde251f725f9659926ff529ca23e3e052a084221052bb6f4f1bf715abd" => :high_sierra
     sha256 "aea3b5d69a3a6b1046597e7d5b26ff3b2084e7bd47c3f2f44933d2ff351da1d2" => :sierra
     sha256 "e11dbfcb33533384db298fe84ed065f613db0c3503cd211b4404c586bfd19218" => :el_capitan
     sha256 "6bf95ff668064396bae3a677320425eb5ec66e820d2099dff37ed109c2f2dca6" => :yosemite
