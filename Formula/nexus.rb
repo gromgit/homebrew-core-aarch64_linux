c17e830c08f84ed58a50a1fcfc9ad7376dba276f
Edward Samson <edward@samson.ph>
2017-10-03 10:21:21 +0800

    nexus 2.14.5-02

--- Formula/nexus.rb
+++ Formula/nexus.rb
@@ -1,9 +1,9 @@
 class Nexus < Formula
   desc "Repository manager for binary software components"
   homepage "https://www.sonatype.org/"
-  url "https://sonatype-download.global.ssl.fastly.net/nexus/oss/nexus-2.14.3-02-bundle.tar.gz"
-  version "2.14.3-02"
-  sha256 "2587b495b088ea78a334ac2a9aaad3182b0fc827f884a1d4e3a783b89ae1db50"
+  url "https://sonatype-download.global.ssl.fastly.net/nexus/oss/nexus-2.14.5-02-bundle.tar.gz"
+  version "2.14.5-02"
+  sha256 "e63ecd36d2daec6f406793864510b1f516b9a6c9cf1842e204fca95b58070c22"
 
   bottle :unneeded
 
