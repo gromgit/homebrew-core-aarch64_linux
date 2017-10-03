1f946603707aef5855a287e75042d62e04e35e8b
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2017-10-03 16:15:16 -0700

    heroku 6.14.31
    Closes #18944.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Heroku < Formula
   desc "Command-line client for the cloud PaaS"
   homepage "https://cli.heroku.com"
-  url "https://registry.npmjs.org/heroku-cli/-/heroku-cli-6.14.30.tgz"
-  sha256 "d851958b6b45bdcaef92b0b90dba8d1f3b30e25e6ad640d7f44f1d28d832e840"
+  url "https://registry.npmjs.org/heroku-cli/-/heroku-cli-6.14.31.tgz"
+  sha256 "6f9e78e2fa01ed1d385e08da64864586f11a79f99c27f902a95be2cdb2fda884"
   head "https://github.com/heroku/cli.git"
 
   bottle do
@@ -17,9 +17,10 @@ class Heroku < Formula
   depends_on "node"
 
   def install
-    inreplace "bin/run.js", "npm update -g heroku-cli", "brew upgrade heroku"
-    inreplace "bin/run", "node \"$DIR/run.js\"",
-                         "#{Formula["node"].opt_bin}/node \"$DIR/run.js\""
+    inreplace "bin/run" do |s|
+      s.gsub! "npm update -g heroku-cli", "brew upgrade heroku"
+      s.gsub! "#!/usr/bin/env node", "#!#{Formula["node"].opt_bin}/node"
+    end
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
