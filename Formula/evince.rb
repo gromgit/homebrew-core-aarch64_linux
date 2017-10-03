e5a015fc25ce028f38d93b65cc4cd429dd2f73b9
ilovezfs <ilovezfs@icloud.com>
2017-10-03 00:50:57 -0700

    evince: revision for poppler

--- Formula/evince.rb
+++ Formula/evince.rb
@@ -3,6 +3,7 @@ class Evince < Formula
   homepage "https://wiki.gnome.org/Apps/Evince"
   url "https://download.gnome.org/sources/evince/3.26/evince-3.26.0.tar.xz"
   sha256 "79567bdb743cf0c3ed7b638da32afc9b850298f9b4edd532455df4a7e2a4c9d8"
+  revision 1
 
   bottle do
     rebuild 1
