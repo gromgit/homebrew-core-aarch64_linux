ff7b2667ff3949e034b967d07d9ba2ca342ff15a
FX Coudert <fxcoudert@gmail.com>
2017-10-03 09:48:43 +0200

    snap7: update sha256, bump revision
    Closes #18911.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/snap7.rb
+++ Formula/snap7.rb
@@ -2,7 +2,8 @@ class Snap7 < Formula
   desc "Ethernet communication suite that works natively with Siemens S7 PLCs"
   homepage "https://snap7.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/snap7/1.4.2/snap7-full-1.4.2.7z"
-  sha256 "65af129e11de4b0d942751bcd9c563f7012cae174931860c03dbb2cdf2e80ae7"
+  sha256 "1f4270cde8684957770a10a1d311c226e670d9589c69841a9012e818f7b9f80e"
+  revision 1
 
   bottle do
     cellar :any
