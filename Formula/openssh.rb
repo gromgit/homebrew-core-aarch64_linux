d85535c9ee053ef972dd505eda20f2e6e818651e
Alex Gaynor <alex.gaynor@gmail.com>
2017-10-03 17:16:43 -0400

    openssh 7.6p1
    deparallelize "make install"
    
    Closes #18935.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/openssh.rb
+++ Formula/openssh.rb
@@ -1,11 +1,10 @@
 class Openssh < Formula
   desc "OpenBSD freely-licensed SSH connectivity tools"
   homepage "https://www.openssh.com/"
-  url "https://www.mirrorservice.org/pub/OpenBSD/OpenSSH/portable/openssh-7.5p1.tar.gz"
-  mirror "https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-7.5p1.tar.gz"
-  version "7.5p1"
-  sha256 "9846e3c5fab9f0547400b4d2c017992f914222b3fd1f8eee6c7dc6bc5e59f9f0"
-  revision 1
+  url "https://www.mirrorservice.org/pub/OpenBSD/OpenSSH/portable/openssh-7.6p1.tar.gz"
+  mirror "https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-7.6p1.tar.gz"
+  version "7.6p1"
+  sha256 "a323caeeddfe145baaa0db16e98d784b1fbc7dd436a6bf1f479dfd5cd1d21723"
 
   bottle do
     sha256 "a7998e2c51b48845f74bfc925cb00b54778a0ccaa9d02ae40dbc98e4ba1f7963" => :high_sierra
@@ -49,6 +48,7 @@ class Openssh < Formula
                           "--with-pam",
                           "--with-ssl-dir=#{Formula["openssl"].opt_prefix}"
     system "make"
+    ENV.deparallelize
     system "make", "install"
 
     # This was removed by upstream with very little announcement and has
