ecb216397c24c90eb518d7909827b05841d386c8
ilovezfs <ilovezfs@icloud.com>
2017-10-02 21:49:02 -0700

    nifi 1.4.0 (#18895)

--- Formula/nifi.rb
+++ Formula/nifi.rb
@@ -1,8 +1,8 @@
 class Nifi < Formula
   desc "Easy to use, powerful, and reliable system to process and distribute data."
   homepage "https://nifi.apache.org"
-  url "https://www.apache.org/dyn/closer.cgi?path=/nifi/1.3.0/nifi-1.3.0-bin.tar.gz"
-  sha256 "f44407e86b370fe4851188100a7e9ae0b3eb347e6ffc78ae65208c0f59c1410e"
+  url "https://www.apache.org/dyn/closer.cgi?path=/nifi/1.4.0/nifi-1.4.0-bin.tar.gz"
+  sha256 "d67c90deb50ed5fee44e7ebf61fbf870f2e97cf1c82ad0f598ff5c4ef905ce99"
 
   bottle :unneeded
 
