62607ba2d762259f37e7f7d0edd2e829b1b9c929
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-04 15:52:09 +0000

    algernon: update 1.6 bottle.

--- Formula/algernon.rb
+++ Formula/algernon.rb
@@ -10,10 +10,9 @@ class Algernon < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e71e7b356af33562d6d048d9319c89050146bb33ad5222a3641ee233d2d9474b" => :high_sierra
-    sha256 "9d317313b69973d3cd40b03d177c1325f6c8289361642c8dedb1df56ed2e4321" => :sierra
-    sha256 "acea265a38c271d3127566b4ded7211a4ca21480d99a680dcf075589b4b53851" => :el_capitan
-    sha256 "bafaf993037f0ee03179f9cc7c0d1829a8a64b755ef16429f1bb668ce228c333" => :yosemite
+    sha256 "75251e63866d6e338b1a81ee6d5d007e9f4f6f80801cabe80a918be7cea976b7" => :high_sierra
+    sha256 "1435aa8e578bc70ef7847ae191747e75140d04150b1cbf6d2f9f318f326e7453" => :sierra
+    sha256 "b2a1778124b34030e3bf5aaf4f9356f8a07b4d0db9841a4cadd1b70e4e380fad" => :el_capitan
   end
 
   depends_on "go" => :build
