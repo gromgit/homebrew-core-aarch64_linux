a2c5e165a62eb76c8e2305357d9e1973d4c196ce
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-04 16:18:54 +0000

    pmdmini: update 1.0.1 bottle.

--- Formula/pmdmini.rb
+++ Formula/pmdmini.rb
@@ -6,11 +6,9 @@ class Pmdmini < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "b5f11fd04a2747d1c27e84003f9ee42aaf6a2f1dce34faf146a8531fa1d7831c" => :sierra
-    sha256 "0d0d98c981cc98801314be58685ef4019b2a0a73a2ff98e7353e4f1a311bd354" => :el_capitan
-    sha256 "f7faea3656b8ee02a2a51d8698f9906f9092ba9af2a884e8125de1840c9a00c0" => :yosemite
-    sha256 "0c59e61c2789433240c34f51e7e643559c3af24b9e588fe89140bbc5cdbe9d5e" => :mavericks
+    sha256 "c3195012d5b5333e76c1a8a44b3f734575540deee884dfb6685e139e1038c138" => :high_sierra
+    sha256 "59b287650c6e40c20da8000f5e73b910f8096bd949e4432b4f11e70b1c779a5d" => :sierra
+    sha256 "72afd84c66fef9f142a1922fd0995a6a173b46c40d06715808345cc1c71b6702" => :el_capitan
   end
 
   option "with-lib-only", "Do not build commandline player"
