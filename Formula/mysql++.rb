fee693ce844855b0ec063dc0b0e5794fa486bf2a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-04 14:50:55 +0000

    mysql++: update 3.2.3 bottle.

--- Formula/mysql++.rb
+++ Formula/mysql++.rb
@@ -6,6 +6,7 @@ class Mysqlxx < Formula
 
   bottle do
     cellar :any
+    sha256 "e896760f31b977fce886b2f9eb67ee70fa93491752ac48744458bfa74b5c1f35" => :high_sierra
     sha256 "44549b6b92ecf8288923b6111a67c3dcf16f5ba0a0ca47f4fd38a31b99545452" => :sierra
     sha256 "1f0e3bc7e6e25924bb95113ee0cbd7c99402dc51744682258b3548c756431239" => :el_capitan
     sha256 "f9837534007c15fdf73e607ddc58c1d5c0d1d20ff2de7e2d1dea20716b823cc9" => :yosemite
