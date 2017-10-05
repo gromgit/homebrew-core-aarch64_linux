ac754597df9eff2c4c3fe5265522ee4a5fe48595
ilovezfs <ilovezfs@icloud.com>
2017-10-04 22:47:18 -0700

    dshb 0.2.0
    Closes #19006.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dshb.rb
+++ Formula/dshb.rb
@@ -1,8 +1,8 @@
 class Dshb < Formula
   desc "macOS system monitor in Swift"
   homepage "https://github.com/beltex/dshb"
-  url "https://github.com/beltex/dshb/releases/download/v0.1.0/dshb-0.1.0-source.zip"
-  sha256 "efed42a2be0dbc6de3b22b314b582b1d6517922b72e08e063f3d1d3810a782f9"
+  url "https://github.com/beltex/dshb/releases/download/v0.2.0/dshb-0.2.0-source.zip"
+  sha256 "b2d512e743d2973ae4cddfead2c5b251c45e5f5d64baff0955bee88e94035c33"
 
   bottle do
     cellar :any_skip_relocation
@@ -10,12 +10,12 @@ class Dshb < Formula
     sha256 "94085328f6ef593ca0d00923fabfe43586c1c4b51eaf555ce9c7db9d7db1f486" => :yosemite
   end
 
-  depends_on :xcode => ["7.0", :build]
+  depends_on :xcode => ["8.0", :build]
 
   def install
     system "make", "release"
     bin.install "bin/dshb"
-    man1.install "doc/dshb.1"
+    man1.install "docs/dshb.1"
   end
 
   test do
