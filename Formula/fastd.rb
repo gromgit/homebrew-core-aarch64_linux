e426dbbc3230a1e556516ad37c09729ed3cc1034
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-05 01:16:47 +0000

    fastd: update 18_3 bottle.

--- Formula/fastd.rb
+++ Formula/fastd.rb
@@ -19,10 +19,9 @@ class Fastd < Formula
 
   bottle do
     cellar :any
-    sha256 "2fadce94f9be85aaa001ff349a883338afd84e92bf46e7304b5369f566c0bc4d" => :high_sierra
-    sha256 "c677903580193501b1c9f261db458439a4b1faf16bb6f2d653bfbded0c27573a" => :sierra
-    sha256 "101454fa20fdae38fc728f9a0fed65422b5931ccfac649c91e31e7bb52567fe0" => :el_capitan
-    sha256 "71271192803a9b291017cc6779219e771c547d45026327c065d9a997d7f6b562" => :yosemite
+    sha256 "e2ffc27c91c3f114e3aee16fa984508285ac470206987f57acbd71300f44c79f" => :high_sierra
+    sha256 "c4003d1015702e51335a6c283031427c8ba46297f70ea24173070276d5d47505" => :sierra
+    sha256 "b118c9000eaf4c64d8e2faedae20b9309c46c84042bb011c20c28786b507398b" => :el_capitan
   end
 
   depends_on "cmake" => :build
