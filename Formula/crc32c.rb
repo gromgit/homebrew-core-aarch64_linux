b3b1dc7db9002a121ee0647dcdccb0e15aa65c96
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-05 23:15:19 +0000

    crc32c: add 1.0.5 bottle.

--- Formula/crc32c.rb
+++ Formula/crc32c.rb
@@ -5,6 +5,13 @@ class Crc32c < Formula
   sha256 "c2c0dcc8d155a6a56cc8d56bc1413e076aa32c35784f4d457831e8ccebd9260b"
   head "https://github.com/google/crc32c.git"
 
+  bottle do
+    cellar :any
+    sha256 "b700bbade7a8f06565f8e19cb724ccdf485bccc9a1dbd56acd7a0bb80670ef1f" => :high_sierra
+    sha256 "35d06b0001644f7b697ba3df4b6a4d4f92d27277e311cb1438e348ca8f6a9ff2" => :sierra
+    sha256 "6cff4648b0ac7455335437fe1b36b246f8d547dfbde77e32bf81494d47e6af6b" => :el_capitan
+  end
+
   depends_on "cmake" => :build
 
   def install
