0becf3a64bf3807d15443ea4600782835a4b68a5
FX Coudert <fxcoudert@gmail.com>
2017-10-05 12:51:05 +0200

    ucommon 7.0.0
    Closes #19029.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/ucommon.rb
+++ Formula/ucommon.rb
@@ -1,9 +1,8 @@
 class Ucommon < Formula
   desc "GNU C++ runtime library for threads, sockets, and parsing"
   homepage "https://www.gnu.org/software/commoncpp/"
-  url "https://ftp.gnu.org/gnu/commonc++/ucommon-6.2.2.tar.gz"
-  sha256 "1ddcef26dc5c930225de6ab9adc3c389cb3f585eba0b0f164206f89d2dafea66"
-  revision 1
+  url "https://ftp.gnu.org/gnu/commonc++/ucommon-7.0.0.tar.gz"
+  sha256 "6ac9f76c2af010f97e916e4bae1cece341dc64ca28e3881ff4ddc3bc334060d7"
 
   bottle do
     sha256 "dc45edfafc90739e0bdbb5f44ac45151e2721737e5ea5fd8425d6f5398ef9889" => :sierra
@@ -15,13 +14,13 @@ class Ucommon < Formula
   depends_on "pkg-config" => :build
   depends_on "gnutls"
 
-  def install
-    # Replace the ldd with macOS's otool. This is unlikely to be merged upstream.
-    # Emailed upstream (dyfet at gnu dot org) querying this on 25/11/14.
-    # It generates a very minor runtime error without the inreplace, so...
-    inreplace "commoncpp-config.in", "ldd /bin/sh", "otool -L /bin/sh"
-    inreplace "ucommon-config.in", "ldd /bin/sh", "otool -L /bin/sh"
+  # Fix build, reported by email to bug-commoncpp@gnu.org on 2017-10-05
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/77f0d9d2/ucommon/cachelinesize.patch"
+    sha256 "46aef9108e2012362b6adcb3bea2928146a3a8fe5e699450ffaf931b6db596ff"
+  end
 
+  def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking",
                           "--disable-silent-rules", "--enable-socks",
                           "--with-sslstack=gnutls", "--with-pkg-config"
