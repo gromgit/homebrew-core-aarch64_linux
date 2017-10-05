e844f40114075d30f713bdb2966c4616abdcd10d
sakajunquality <jun.bj141400@gmail.com>
2017-10-06 02:07:44 +0900

    goenv 1.5.0 (#19020)

--- Formula/goenv.rb
+++ Formula/goenv.rb
@@ -1,8 +1,8 @@
 class Goenv < Formula
   desc "Go version management"
   homepage "https://github.com/syndbg/goenv"
-  url "https://github.com/syndbg/goenv/archive/1.4.0.tar.gz"
-  sha256 "ba9158e711f9ffc40c6e5f1d3371730ecf96dcb805f9540704c35a7463765c0d"
+  url "https://github.com/syndbg/goenv/archive/1.5.0.tar.gz"
+  sha256 "1a847972a8fb7c622372b3d9ecd772fa32a9fac95886c3cafbe90a2102010adb"
   version_scheme 1
   head "https://github.com/syndbg/goenv.git"
 
