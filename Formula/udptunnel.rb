602d95af1acae29a1c33ee95e61299fa6beb4d39
FX Coudert <fxcoudert@gmail.com>
2017-10-05 12:35:53 +0200

    udptunnel: add mirror

--- Formula/udptunnel.rb
+++ Formula/udptunnel.rb
@@ -2,6 +2,7 @@ class Udptunnel < Formula
   desc "Tunnel UDP packets over a TCP connection"
   homepage "http://www.cs.columbia.edu/~lennox/udptunnel"
   url "http://www.cs.columbia.edu/~lennox/udptunnel/udptunnel-1.1.tar.gz"
+  mirror "http://ftp.nsysu.edu.tw/FreeBSD/ports/local-distfiles/leeym/udptunnel-1.1.tar.gz"
   sha256 "45c0e12045735bc55734076ebbdc7622c746d1fe4e6f7267fa122e2421754670"
 
   bottle do
