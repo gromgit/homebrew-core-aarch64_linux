5c0aa68e61473ba14fbf7d22fbe2831f2fe5fb6e
The Alchemist <kap4020@gmail.com>
2017-10-06 15:50:05 -0400

    serd 0.28.0
    Closes #19125.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/serd.rb
+++ Formula/serd.rb
@@ -1,8 +1,8 @@
 class Serd < Formula
   desc "C library for RDF syntax"
   homepage "https://drobilla.net/software/serd/"
-  url "https://download.drobilla.net/serd-0.26.0.tar.bz2"
-  sha256 "e3e44a88f90a9971d55e6cbd59a7b9cfa97cfc17c512fed7166a4252d5209298"
+  url "https://download.drobilla.net/serd-0.28.0.tar.bz2"
+  sha256 "1df21a8874d256a9f3d51a18b8c6e2539e8092b62cc2674b110307e93f898aec"
 
   bottle do
     cellar :any
