f0fcfd5895c77b0988050b2f1c8d4513bbec54fa
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-06 14:10:48 +0000

    libxml2: update 2.9.6 bottle.

--- Formula/libxml2.rb
+++ Formula/libxml2.rb
@@ -8,10 +8,9 @@ class Libxml2 < Formula
 
   bottle do
     cellar :any
-    sha256 "c34f1308c810427cd88e8ac4cadd71b9e65fe6b44bc54e5249dfaa785aaae661" => :high_sierra
-    sha256 "b1121510780bcd6ae1a464f22312bb425daff8735dd2c647b41484461f646a7a" => :sierra
-    sha256 "09b8d61b8ae67820a739c240f4114079911a019390a6ed2fee8541439dc53660" => :el_capitan
-    sha256 "1fd3f3ec30d85289019b6845d5b394f4ab38c7543ef0a7aa3948c76c7dd3ce93" => :yosemite
+    sha256 "b08291d2d8b9328b55a7785128d57a893b5d0eb3c03cb528316d6cabc93dbf37" => :high_sierra
+    sha256 "7aae3bbeb817110901ddc03112b6a5af7c8f457149c7cc9852ff16c1740a8f75" => :sierra
+    sha256 "c3638a6edc119734a626d84d50d2bbc22bb170455dce1eff589560ad79cc5378" => :el_capitan
   end
 
   keg_only :provided_by_macos
