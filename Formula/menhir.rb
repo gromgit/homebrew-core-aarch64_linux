bf62c724949378d62e10e359d023f68ad454ff13
ilovezfs <ilovezfs@icloud.com>
2017-10-06 02:39:17 -0700

    menhir: use assert_predicate instead of File.exist?

--- Formula/menhir.rb
+++ Formula/menhir.rb
@@ -38,7 +38,7 @@ class Menhir < Formula
     EOS
 
     system "#{bin}/menhir", "--dump", "--explain", "--infer", "test.mly"
-    assert File.exist? "test.ml"
-    assert File.exist? "test.mli"
+    assert_predicate testpath/"test.ml", :exist?
+    assert_predicate testpath/"test.mli", :exist?
   end
 end
