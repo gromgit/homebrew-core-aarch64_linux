75fd085c3354936c46536c6b6365381f79016c37
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-06 20:05:27 +0000

    serd: update 0.28.0 bottle.

--- Formula/serd.rb
+++ Formula/serd.rb
@@ -6,10 +6,9 @@ class Serd < Formula
 
   bottle do
     cellar :any
-    sha256 "55d28db3e4e0c7814d1c229aeec20516afc8db91d4ec913b2d476d1296f1b8c0" => :high_sierra
-    sha256 "8ad52906db138cc480a3d75c1f9b6f245456a80c47de220c83acf7778bb55c48" => :sierra
-    sha256 "dfd1ede14146b8222cf65e39c7dafe368a4abd474b2c5babddcd883a9f112743" => :el_capitan
-    sha256 "654ac6e9843657faad3ec005d476f8812f9630e4ad56878c24bf93c17f66fa64" => :yosemite
+    sha256 "0c55076d7655da6d7d5154e24cd3d720fd9afcd610f5e7e9ee5af23859ab85f7" => :high_sierra
+    sha256 "7c7c1f9317f0fd595bfcdb8214e510c10648f99cdf9be1464b272abd4fa223ea" => :sierra
+    sha256 "8c0c40af6e4aaa222c37cd154d2ef80c6b13a50744d97e94139250a5e1a778a0" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
