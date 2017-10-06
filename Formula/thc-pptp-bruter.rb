474de2c5e98b55a30cb8908e427813871b117ed6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-06 14:44:22 +0000

    thc-pptp-bruter: update 0.1.4 bottle.

--- Formula/thc-pptp-bruter.rb
+++ Formula/thc-pptp-bruter.rb
@@ -7,6 +7,7 @@ class ThcPptpBruter < Formula
 
   bottle do
     cellar :any
+    sha256 "ef9ccd5b90616a66263fa9779c31fc3858a32fc5ec0bd69d98df3329072415a7" => :high_sierra
     sha256 "bf0734c5bac73d3948b6b10856c9cff316f28dcac7d5213323f42dcd7f210306" => :sierra
     sha256 "29311d0d9c7592bcaae2acbf529bc6cb836df622361a9343aeb37543a0ff7f48" => :el_capitan
     sha256 "9236475e9ec15fdddefd4b9ad01d619d645ad0227adc50e9ae7c5b01a68ff964" => :yosemite
