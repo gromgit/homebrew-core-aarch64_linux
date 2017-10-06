92cd7e0efb929d1142c313ac2580983e91ed792d
ilovezfs <ilovezfs@icloud.com>
2017-10-06 02:08:10 -0700

    pass: use assert_predicate instead of File.exist?

--- Formula/pass.rb
+++ Formula/pass.rb
@@ -42,7 +42,7 @@ class Pass < Formula
       system Formula["gnupg"].opt_bin/"gpg", "--batch", "--gen-key", "batch.gpg"
       system bin/"pass", "init", "Testing"
       system bin/"pass", "generate", "Email/testing@foo.bar", "15"
-      assert File.exist?(".password-store/Email/testing@foo.bar.gpg")
+      assert_predicate testpath/".password-store/Email/testing@foo.bar.gpg", :exist?
     ensure
       system Formula["gnupg"].opt_bin/"gpgconf", "--kill", "gpg-agent"
     end
