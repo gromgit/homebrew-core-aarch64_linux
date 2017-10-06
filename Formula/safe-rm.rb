6da1444788f31d62507dabfc314b5173cf052eed
ilovezfs <ilovezfs@icloud.com>
2017-10-06 04:17:00 -0700

    safe-rm: use refute_predicate instead of !File.exist?

--- Formula/safe-rm.rb
+++ Formula/safe-rm.rb
@@ -19,7 +19,7 @@ class SafeRm < Formula
     touch foo
     touch bar
     system "#{bin}/safe-rm", foo
-    assert !File.exist?(foo)
+    refute_predicate foo, :exist?
     shell_output("#{bin}/safe-rm #{bar} 2>&1", 64)
     assert_predicate bar, :exist?
   end
