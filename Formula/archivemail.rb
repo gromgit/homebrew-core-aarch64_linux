729223a42a8c85bff7080c243754906609d90430
ilovezfs <ilovezfs@icloud.com>
2017-10-06 02:47:48 -0700

    archivemail: use assert_predicate instead of File.exist?

--- Formula/archivemail.rb
+++ Formula/archivemail.rb
@@ -40,7 +40,7 @@ class Archivemail < Formula
       This is the second body.
     EOS
     system bin/"archivemail", "--no-compress", "--date", "2012-01-01", (testpath/"inbox")
-    assert File.exist?(testpath/"inbox_archive")
+    assert_predicate testpath/"inbox_archive", :exist?
     assert_match "Sample message 1", File.read("inbox_archive")
     assert !File.read("inbox").include?("Sample message 1")
     assert_match "Sample message 2", File.read("inbox")
