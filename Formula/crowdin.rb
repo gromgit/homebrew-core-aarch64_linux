02d438dcd59d08b356284449c8cc72f9c5bcf0fc
ipopyk <igor.popyk.te@gmail.com>
2017-10-06 09:41:00 +0300

    crowdin: fix url

--- Formula/crowdin.rb
+++ Formula/crowdin.rb
@@ -1,7 +1,7 @@
 class Crowdin < Formula
   desc "Command-line tool that allows to manage your resources with crowdin.com"
   homepage "https://support.crowdin.com/cli-tool/"
-  url "https://crowdin.com/downloads/crowdin-cli-2.0.19.zip"
+  url "https://downloads.crowdin.com/cli/v2/crowdin-cli-2.0.19.zip"
   sha256 "01601deebf7c83adb93d82a933b93a0ab4095c956e694947fadc02116debcb5e"
 
   bottle :unneeded
