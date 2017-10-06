baffe43018c01f4ac8ab67c246751daf81cdbaa0
ilovezfs <ilovezfs@icloud.com>
2017-10-06 02:08:03 -0700

    coffeescript: use assert_predicate instead of File.exist?

--- Formula/coffeescript.rb
+++ Formula/coffeescript.rb
@@ -37,6 +37,6 @@ class Coffeescript < Formula
     EOS
 
     system bin/"coffee", "--compile", "test.coffee"
-    assert File.exist?("test.js"), "test.js was not generated"
+    assert_predicate testpath/"test.js", :exist?, "test.js was not generated"
   end
 end
