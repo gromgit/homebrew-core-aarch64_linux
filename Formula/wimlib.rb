1298a9e425da902ddb0facafc3d694bfc4e2f5a0
ilovezfs <ilovezfs@icloud.com>
2017-10-06 02:08:16 -0700

    wimlib: use assert_predicate instead of File.exist?

--- Formula/wimlib.rb
+++ Formula/wimlib.rb
@@ -40,7 +40,7 @@ class Wimlib < Formula
     # capture an image
     ENV.append "WIMLIB_IMAGEX_USE_UTF8", "1"
     system "#{bin}/wimcapture", "foo", "bar.wim"
-    assert File.exist?("bar.wim")
+    assert_predicate testpath/"bar.wim", :exist?
 
     # get info on the image
     system "#{bin}/wiminfo", "bar.wim"
