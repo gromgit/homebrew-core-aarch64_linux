fc5feda951d45d6c2fe2a5c95155fe5711f8e367
ilovezfs <ilovezfs@icloud.com>
2017-10-06 03:00:27 -0700

    mp3wrap: use assert_predicate instead of File.exist?

--- Formula/mp3wrap.rb
+++ Formula/mp3wrap.rb
@@ -24,6 +24,6 @@ class Mp3wrap < Formula
   test do
     source = test_fixtures("test.mp3")
     system "#{bin}/mp3wrap", "#{testpath}/t.mp3", source, source
-    assert File.exist? testpath/"t_MP3WRAP.mp3"
+    assert_predicate testpath/"t_MP3WRAP.mp3", :exist?
   end
 end
