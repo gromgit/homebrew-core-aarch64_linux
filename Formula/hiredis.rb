99be6335346452ab8841e3dba4064d954d578ade
Dominyk Tiller <dominyktiller@gmail.com>
2017-10-06 21:37:52 +0100

    hiredis: use assert_predicate

--- Formula/hiredis.rb
+++ Formula/hiredis.rb
@@ -3,7 +3,6 @@ class Hiredis < Formula
   homepage "https://github.com/redis/hiredis"
   url "https://github.com/redis/hiredis/archive/v0.13.3.tar.gz"
   sha256 "717e6fc8dc2819bef522deaca516de9e51b9dfa68fe393b7db5c3b6079196f78"
-
   head "https://github.com/redis/hiredis.git"
 
   bottle do
@@ -28,6 +27,6 @@ class Hiredis < Formula
     # sure it compiles
     system ENV.cc, "-I#{include}/hiredis", "-L#{lib}", "-lhiredis",
            pkgshare/"examples/example.c", "-o", testpath/"test"
-    File.exist? testpath/"test"
+    assert_predicate testpath/"test", :exist?
   end
 end
