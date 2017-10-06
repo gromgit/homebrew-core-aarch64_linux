8291c893e614fd94401790f69e34c1ebfdf7a2b1
ilovezfs <ilovezfs@icloud.com>
2017-10-06 02:08:11 -0700

    psftools: use assert_predicate instead of File.exist?

--- Formula/psftools.rb
+++ Formula/psftools.rb
@@ -30,7 +30,7 @@ class Psftools < Formula
     # The zip file has a fon in it, use fon2fnts to extrat to fnt
     resource("pc8x8font").stage do
       system "#{bin}/fon2fnts", "pc8x8.fon"
-      assert File.exist?("PC8X8_9.fnt")
+      assert_predicate testpath/"PC8X8_9.fnt", :exist?
     end
   end
 end
