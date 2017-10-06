6938c5e3ed0b91bd63de7035b5d5afb57b5942a4
ilovezfs <ilovezfs@icloud.com>
2017-10-06 02:08:05 -0700

    gdbm: use assert_predicate instead of File.exist?

--- Formula/gdbm.rb
+++ Formula/gdbm.rb
@@ -33,7 +33,7 @@ class Gdbm < Formula
 
   test do
     pipe_output("#{bin}/gdbmtool --norc --newdb test", "store 1 2\nquit\n")
-    assert File.exist?("test")
+    assert_predicate testpath/"test", :exist?
     assert_match /2/, pipe_output("#{bin}/gdbmtool --norc test", "fetch 1\nquit\n")
   end
 end
