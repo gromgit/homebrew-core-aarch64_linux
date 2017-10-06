c9a125e031fef3a5c6133ab528a1a26beee33a62
ilovezfs <ilovezfs@icloud.com>
2017-10-06 02:08:16 -0700

    zlib: use assert_predicate instead of File.exist?

--- Formula/zlib.rb
+++ Formula/zlib.rb
@@ -34,6 +34,6 @@ class Zlib < Formula
     touch "foo.txt"
     output = "./zpipe < foo.txt > foo.txt.z"
     system output
-    assert File.exist?("foo.txt.z")
+    assert_predicate testpath/"foo.txt.z", :exist?
   end
 end
