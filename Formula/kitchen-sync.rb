e4b6301f9e0627443e0fb8821b29e37b841cf480
ilovezfs <ilovezfs@icloud.com>
2017-10-06 20:54:45 -0700

    kitchen-sync 0.99.1
    Closes #19132.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/kitchen-sync.rb
+++ Formula/kitchen-sync.rb
@@ -1,8 +1,8 @@
 class KitchenSync < Formula
   desc "Fast efficiently sync database without dumping & reloading"
   homepage "https://github.com/willbryant/kitchen_sync"
-  url "https://github.com/willbryant/kitchen_sync/archive/0.99.tar.gz"
-  sha256 "5536b351e98a25a568364580b991a4418c3c468838d569b69c833ecdec852fb5"
+  url "https://github.com/willbryant/kitchen_sync/archive/0.99.1.tar.gz"
+  sha256 "895b710f10e9399ce8e49efe1b24ca925b4a62510d817a0e4b60e11c4cefcaf6"
   head "https://github.com/willbryant/kitchen_sync.git"
 
   bottle do
