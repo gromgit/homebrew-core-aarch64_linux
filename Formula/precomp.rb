92dabb7c23aa603dcc413e7987b9ae0de8462685
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-07 05:48:10 +0000

    precomp: add 0.4.5 bottle.

--- Formula/precomp.rb
+++ Formula/precomp.rb
@@ -5,6 +5,13 @@ class Precomp < Formula
   sha256 "39add141554f0186200911ab61838b69f5777956b3dfd37d82bb6923607258dc"
   head "https://github.com/schnaader/precomp-cpp.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3984c5a8010fe9f82d3b8fdc757211def7af9675be330ba7883b867ca5d29c55" => :high_sierra
+    sha256 "d1d6345d53fe8a80389d3fcbb3aa3fcb3abf62ec16eca16023fc42bf3b9452aa" => :sierra
+    sha256 "327439d7580922ac4bbbdc521787a21327ec71e34b15bcbd8482322f3c196c55" => :el_capitan
+  end
+
   def install
     # HEAD already has that, but current stable version does not compile with clang without that patch
     inreplace "contrib/packmp3/Makefile", "-fsched-spec-load ", ""
