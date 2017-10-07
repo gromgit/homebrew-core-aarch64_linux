2afd7d0460f871faa02d32a45e161c3b386bf85b
John McCabe <john@johnmccabe.net>
2017-10-07 16:42:36 +0100

    faas-cli 0.4.18
    Closes #19142.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/faas-cli.rb
+++ Formula/faas-cli.rb
@@ -2,8 +2,8 @@ class FaasCli < Formula
   desc "CLI for templating and/or deploying FaaS functions"
   homepage "http://docs.get-faas.com/"
   url "https://github.com/openfaas/faas-cli.git",
-      :tag => "0.4.17",
-      :revision => "83f9316378be16cf7adbc3aee78d172af012d5c4"
+      :tag => "0.4.18",
+      :revision => "a4f03381443fd5c1e74ea7e17614df9b3b60abd1"
 
   bottle do
     cellar :any_skip_relocation
