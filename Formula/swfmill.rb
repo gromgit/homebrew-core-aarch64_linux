62c07aaac0ab7a6e465cf45894f8580e64c8249d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-08 16:56:34 +0000

    swfmill: update 0.3.4 bottle.

--- Formula/swfmill.rb
+++ Formula/swfmill.rb
@@ -6,10 +6,9 @@ class Swfmill < Formula
 
   bottle do
     cellar :any
-    sha256 "5e141351c1aadf19e71d254ced8200698d0c07e8bbac21db938fc4734a494c0f" => :sierra
-    sha256 "9c9674f4a986e2af30ffaa69c67276a43f0e0fcb4803a9463e66c63eb2398627" => :el_capitan
-    sha256 "eee9695134c883a9348a7c5c1e66b32b7b01b8ef1f24069688c727eaf146ad50" => :yosemite
-    sha256 "90cc5c0dcf27189d3f9cf0440c6a9e8ac4312d57c7afd60243f4496f1dd79541" => :mavericks
+    sha256 "38049fc1858f7002a7057bbf54501569c2e2f16b5c9dcb02d8972f0fb5d24adf" => :high_sierra
+    sha256 "d30d4ede107e5392f29ac200977c98479474e6c191ea2afeaefe33c197d96f15" => :sierra
+    sha256 "a4381d474ff02befed9fad1ff75aa7276c2da482ad5d39c9f46c5d133d340f22" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
