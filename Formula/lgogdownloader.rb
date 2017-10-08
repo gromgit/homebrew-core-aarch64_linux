281476223d325251ae44d559f5e1165c793697b3
Claudia <claui@users.noreply.github.com>
2017-10-09 00:35:34 +0200

    lgogdownloader 3.2 (new formula)
    Closes #19189.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/lgogdownloader.rb
@@ -0,0 +1,35 @@
+class Lgogdownloader < Formula
+  desc "Unofficial downloader for GOG.com games"
+  homepage "https://sites.google.com/site/gogdownloader/"
+  url "https://sites.google.com/site/gogdownloader/lgogdownloader-3.2.tar.gz"
+  sha256 "9e60af50c18533884ca317778b291152d5f04d93dfd3dc2579f6f965538b2031"
+
+  depends_on "cmake" => :build
+  depends_on "help2man" => :build
+  depends_on "pkg-config" => :build
+  depends_on "boost"
+  depends_on "htmlcxx"
+  depends_on "jsoncpp"
+  depends_on "liboauth"
+  depends_on "rhash"
+  depends_on "tinyxml2"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    require "pty"
+
+    ENV["XDG_CONFIG_HOME"] = testpath
+    reader, writer = PTY.spawn(bin/"lgogdownloader", "--list", "--retries", "1")
+    writer.write <<-EOS.undent
+      test@example.com
+      secret
+    EOS
+    writer.close
+    assert_equal "HTTP: Login failed", reader.read.lines.last.chomp
+    reader.close
+  end
+end
