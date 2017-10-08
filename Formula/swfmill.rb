25d2759c79c82a462c2887bcab47e5e26fbb8c4f
FX Coudert <fxcoudert@gmail.com>
2017-10-08 18:38:36 +0200

    swfmill 0.3.4
    Closes #19181.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/swfmill.rb
+++ Formula/swfmill.rb
@@ -1,9 +1,8 @@
 class Swfmill < Formula
   desc "xml2swf and swf2xml processor"
-  homepage "http://swfmill.org"
-  url "http://swfmill.org/releases/swfmill-0.3.3.tar.gz"
-  sha256 "f9e8529eed84962abf88c6457b59cbc6d230db068d1fdd977e7b234228beac96"
-  revision 1
+  homepage "https://swfmill.org"
+  url "https://www.swfmill.org/releases/swfmill-0.3.4.tar.gz"
+  sha256 "514843cdacd1c95a1a8b60a3f4f4fc6559932fb270c6a0585ad5c3275da03589"
 
   bottle do
     cellar :any
@@ -19,14 +18,12 @@ class Swfmill < Formula
 
   def install
     # Use inreplace instead of a patch due to newlines
-    # Reported usptream:
-    # https://github.com/djcsdy/swfmill/issues/32
+    # Reported usptream: https://github.com/djcsdy/swfmill/issues/32
     inreplace "src/swft/swft_import_ttf.cpp",
       "#include <freetype/tttables.h>",
       "#include FT_TRUETYPE_TABLES_H"
 
     system "./configure", "--prefix=#{prefix}"
-    # Has trouble linking against zlib unless we add it here - @adamv
-    system "make", "LIBS=-lz", "install"
+    system "make", "install"
   end
 end
