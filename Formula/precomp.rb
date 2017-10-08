a64b491bba308b2f036f82c6e116d7b7d3763ec2
ilovezfs <ilovezfs@icloud.com>
2017-10-08 05:59:06 -0700

    precomp 0.4.6
    Closes #19172.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/precomp.rb
+++ Formula/precomp.rb
@@ -1,8 +1,8 @@
 class Precomp < Formula
   desc "Command-line precompressor to achieve better compression"
   homepage "http://schnaader.info/precomp.php"
-  url "https://github.com/schnaader/precomp-cpp/archive/v0.4.5.tar.gz"
-  sha256 "39add141554f0186200911ab61838b69f5777956b3dfd37d82bb6923607258dc"
+  url "https://github.com/schnaader/precomp-cpp/archive/v0.4.6.tar.gz"
+  sha256 "673b9ceb0df62abb5ef12ab0600a18fc3b82003cc9af5e1cc2f196237ed350d3"
   head "https://github.com/schnaader/precomp-cpp.git"
 
   bottle do
@@ -13,9 +13,6 @@ class Precomp < Formula
   end
 
   def install
-    # HEAD already has that, but current stable version does not compile with clang without that patch
-    inreplace "contrib/packmp3/Makefile", "-fsched-spec-load ", ""
-
     # Seems like Yosemite does not like the -s flag
     inreplace "Makefile", " -s ", " "
 
