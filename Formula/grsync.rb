3dd59c91d77d98b30fe52150977c90c10e6b2452
ilovezfs <ilovezfs@icloud.com>
2017-10-09 02:32:37 -0700

    grsync: use assert_predicate

--- Formula/grsync.rb
+++ Formula/grsync.rb
@@ -27,6 +27,6 @@ class Grsync < Formula
   test do
     # running the executable always produces the GUI, which is undesirable for the test
     # so we'll just check if the executable exists
-    assert (bin/"grsync").exist?
+    assert_predicate bin/"grsync", :exist?
   end
 end
