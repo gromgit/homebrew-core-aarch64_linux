9ce7957f1ab114a18d7348cdb3723909de6e32df
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-09 12:53:57 +0000

    bench: add 1.0.6 bottle.

--- Formula/bench.rb
+++ Formula/bench.rb
@@ -8,6 +8,13 @@ class Bench < Formula
   url "https://hackage.haskell.org/package/bench-1.0.6/bench-1.0.6.tar.gz"
   sha256 "09c37660d68d103553b79336bfe20383d608cdbaedebfbe289e1b87055a4456b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b8d3039bacf0a9fa9b10188779b8c79bf7b309f79b7a98495cf04e7ef7f5f99b" => :high_sierra
+    sha256 "5b668c20f1e2ec14f81aee4c84df692741f46d5b1663cebac949c769eeb04ec3" => :sierra
+    sha256 "bad9f0285cc2f6307e001dc62950b35afd940f38ff1f6157bab2757aca8b9a3c" => :el_capitan
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
 
