4cf264c12a660fa46686e6102d482f4a6d4d7a6b
ilovezfs <ilovezfs@icloud.com>
2017-10-09 09:23:52 -0700

    go@1.6: use assert_predicate

--- Formula/go@1.6.rb
+++ Formula/go@1.6.rb
@@ -107,7 +107,7 @@ class GoAT16 < Formula
 
     if build.with? "godoc"
       assert_predicate libexec/"bin/godoc", :exist?
-      assert File.executable?(libexec/"bin/godoc")
+      assert_predicate libexec/"bin/godoc", :executable?
     end
   end
 end
