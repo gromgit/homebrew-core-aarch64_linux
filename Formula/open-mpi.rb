a7033e9d1c55b1d107918e2903034cdf647abfdc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-09 06:51:20 +0000

    open-mpi: update 2.1.2 bottle.

--- Formula/open-mpi.rb
+++ Formula/open-mpi.rb
@@ -5,10 +5,9 @@ class OpenMpi < Formula
   sha256 "3cc5804984c5329bdf88effc44f2971ed244a29b256e0011b8deda02178dd635"
 
   bottle do
-    sha256 "f98fe9297af17ec9485f36009ec79319a6682b449eaead015103721cb76cf7b2" => :high_sierra
-    sha256 "15d3d18ad8e4096b670fd9fee3f196cee0bbea4db663cab4eb58a24927daa1cf" => :sierra
-    sha256 "82b89bc9302d9ca10cf0dfa0a53a8298aa4cbb9a84b92139e5ca2a7583499947" => :el_capitan
-    sha256 "4b0437cdcf8e32cf205fb4fc3755eb8a8bb7e35b93d52070e9d3390d2e57d5fa" => :yosemite
+    sha256 "43e6c6143d990abecc5593715d2d5ad2b4e859540a9025cefd37bedc026b9c33" => :high_sierra
+    sha256 "899bfbcb02796f135715089b1323bef1924b435617af96153256971acb9812bd" => :sierra
+    sha256 "574e505e7e6c7089f0519486ed0d27d35cb6cfe16a2216295ba1566574f332df" => :el_capitan
   end
 
   head do
