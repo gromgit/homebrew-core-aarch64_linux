4cad1e547f7eeceb9f5e523b5c495e0edc729130
Bastian Hofmann <bastian.hofmann@researchgate.net>
2017-10-09 20:06:00 +0200

    linkerd 1.3.0

--- Formula/linkerd.rb
+++ Formula/linkerd.rb
@@ -1,8 +1,8 @@
 class Linkerd < Formula
   desc "Drop-in RPC proxy designed for microservices"
   homepage "https://linkerd.io/"
-  url "https://github.com/linkerd/linkerd/releases/download/1.1.3/linkerd-1.1.3.tgz"
-  sha256 "d66a6aec0a78b8af44a0aa8c2e707072180299999f01045442fb36df9766b37d"
+  url "https://github.com/linkerd/linkerd/releases/download/1.3.0/linkerd-1.3.0.tgz"
+  sha256 "1e0ddcc15779f04af61e13c265e97ee48f7d7e60c53e5c7afbf834e84a7e25ba"
 
   bottle :unneeded
 
