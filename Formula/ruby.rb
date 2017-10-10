56d59b7aeb22ab198963a3af88ad04208578c338
ilovezfs <ilovezfs@icloud.com>
2017-10-10 11:26:25 -0700

    ruby 2.5.0-preview1 (devel) (#19275)

--- Formula/ruby.rb
+++ Formula/ruby.rb
@@ -10,6 +10,12 @@ class Ruby < Formula
     sha256 "0f8cb64f36323fd90e53d54138b024296e09dc662e3969cd7e122a98e1eea3f3" => :el_capitan
   end
 
+  devel do
+    url "https://cache.ruby-lang.org/pub/ruby/2.5/ruby-2.5.0-preview1.tar.xz"
+    version "2.5.0-preview1"
+    sha256 "c2f518eb04b38bdd562ba5611abd2521248a1608fc466368563dd794ddeddd09"
+  end
+
   head do
     url "https://svn.ruby-lang.org/repos/ruby/trunk/"
     depends_on "autoconf" => :build
