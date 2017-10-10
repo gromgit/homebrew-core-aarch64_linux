ea3589f7a9d42d8e7d04444a6c569151ee1b238a
ilovezfs <ilovezfs@icloud.com>
2017-10-09 22:43:24 -0700

    exact-image 1.0.1
    Closes #19258.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/exact-image.rb
+++ Formula/exact-image.rb
@@ -1,8 +1,8 @@
 class ExactImage < Formula
   desc "Image processing library"
   homepage "https://exactcode.com/opensource/exactimage/"
-  url "https://dl.exactcode.de/oss/exact-image/exact-image-0.9.2.tar.bz2"
-  sha256 "665b205740b17b4162fae73aa36eb7046f28bb216d612461ca0499ff47a566ba"
+  url "https://dl.exactcode.de/oss/exact-image/exact-image-1.0.1.tar.bz2"
+  sha256 "3bf45d21e653f6a4664147eb4ba29178295d530400d5e16a2ab19ac79f62b76c"
 
   bottle do
     sha256 "ad9a08346cef8389013aaba11d374b70e4eee239e9d57b7c71485eed1cda838f" => :high_sierra
