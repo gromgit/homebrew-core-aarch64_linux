9dc3a87bfd7358efaaf5edb8e0966cca1c518113
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-10 16:53:35 +0000

    faad2: update 2.8.6 bottle.

--- Formula/faad2.rb
+++ Formula/faad2.rb
@@ -6,9 +6,9 @@ class Faad2 < Formula
 
   bottle do
     cellar :any
-    sha256 "56c848aeaea35d03a9c7a3fc32ccd0a10135bb18c2575480b398c6c97e586fda" => :high_sierra
-    sha256 "3245bfe9f53733c704631a49e46fc5db472e1545c3475a6747dc4f230af81d78" => :sierra
-    sha256 "435a26c7bb14b2f370bbde3b3535354df1cb5890b07a9a59abedd5353d106444" => :el_capitan
+    sha256 "ca4c735b958cc393e8ccfa90ca3b0c372febc7175c7be3070aee64fded14d593" => :high_sierra
+    sha256 "b543a97001b82b54a7b650be495394d73b3c81822ca989e154380630ce503ccb" => :sierra
+    sha256 "6dacb8f2ebff6a348a0b1be0a8bbec37a4cee81a28dad9fe2fa2f809461e9e89" => :el_capitan
   end
 
   def install
