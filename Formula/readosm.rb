2fbb562ff65e28bc0ef4de2b3d48a315db7156ad
ilovezfs <ilovezfs@icloud.com>
2017-10-11 04:03:01 -0700

    readosm 1.1.0
    Closes #19318.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/readosm.rb
+++ Formula/readosm.rb
@@ -1,8 +1,8 @@
 class Readosm < Formula
   desc "Extract valid data from an Open Street Map input file"
   homepage "https://www.gaia-gis.it/fossil/readosm/index"
-  url "https://www.gaia-gis.it/gaia-sins/readosm-1.0.0e.tar.gz"
-  sha256 "1fd839e05b411db6ba1ca6199bf3334ab9425550a58e129c07ad3c6d39299acf"
+  url "https://www.gaia-gis.it/gaia-sins/readosm-1.1.0.tar.gz"
+  sha256 "c508cde9c49b955613d9a30dcf622fa264a5c0e01f58074e93351ea39abd40ec"
 
   bottle do
     cellar :any
