6f9e624ba9cf7d88b726b51d6dfa23ea6dfa4d1f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-11 06:32:43 +0000

    netpbm: update 10.73.16 bottle.

--- Formula/netpbm.rb
+++ Formula/netpbm.rb
@@ -15,10 +15,9 @@ class Netpbm < Formula
 
   bottle do
     cellar :any
-    sha256 "b53cab9171a126529a9ab9d9e175daffa2dd7eb1aa3306b47efccfc3fdabf4b3" => :high_sierra
-    sha256 "0ca108d31bfc6e1a8589ec437772d08dd9c29cd090b640c0fae7e3deb5c550c6" => :sierra
-    sha256 "2413a8d4e01fe8df09467ccf11001bc143312ed347f6b0bdc95c3841e0490e3f" => :el_capitan
-    sha256 "cf7a8c90735bdb22001b4f8b1fddf0a7f532f7209d390c4b88a5f29140f48344" => :yosemite
+    sha256 "babaf6a816aa3314583b0892bda37efc2a39227ad20e03994fbaad1ae06d7d5e" => :high_sierra
+    sha256 "dfd98f92f53f38a8f6e98ab64c132c9f92f8e33228f9784c42909cac13ca9705" => :sierra
+    sha256 "433f9816c644f9282abd1b7290d8e0e0cf5f8c1090a36a8899471eaa8b3a2398" => :el_capitan
   end
 
   depends_on "libtiff"
