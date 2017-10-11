7ea202021857fd9c3487d6ca8bf047a93ca26bb4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-11 10:09:58 +0000

    urlview: update 0.9-20 bottle.

--- Formula/urlview.rb
+++ Formula/urlview.rb
@@ -8,12 +8,9 @@ class Urlview < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "29cf4a35b10d828ef693c2181b74c32c0481864757c11d4f0db2a722cccc7209" => :high_sierra
-    sha256 "0a65eaf4a1889d615e6abb61c0dd289df2054e1c2b224d842fd9170111f89bd1" => :sierra
-    sha256 "9dbd6e280b74de72eecfab8d51728bfe46fc6a02f22a5c2e6b1639b904680ebb" => :el_capitan
-    sha256 "dfbc0ddbb5705383fda6175930cb57bbc5b124c31e9b4ec879d86b6211efe3d0" => :yosemite
-    sha256 "5ece2167d692912f75c743425a9b0dc50d63863335b38c6ecb72175ab1bc7eb8" => :mavericks
+    sha256 "ded9ca6285a2c87ecfe9af130e5bc231adfa501c2aae5602c13f40b998077f6a" => :high_sierra
+    sha256 "0f75c9a2a4018f9bac7262314a9f391c63353abd707f820b92a389b21bbf08b8" => :sierra
+    sha256 "f748da269d23717c525cc0272bf90a1c286a16aaa868d9433505f6a72d28bfed" => :el_capitan
   end
 
   patch do
