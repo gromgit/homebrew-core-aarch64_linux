4c825b274de7e820bfcf13b2baf39ee28bc8cbbb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-11 09:56:14 +0000

    shapelib: update 1.4.1 bottle.

--- Formula/shapelib.rb
+++ Formula/shapelib.rb
@@ -6,13 +6,9 @@ class Shapelib < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "9f9e6921c851022b5ca269886a1a34e3dfcebb59eedb903cdf26b42145d03a8b" => :high_sierra
-    sha256 "5ccf254ad426070dc2876c92af6674648229faa6613800983c825e9d88a34105" => :sierra
-    sha256 "231ff7f01f4e050713d0525701a8478ff2de72e6f6a866f6a0b2a2f8ed3a7e29" => :el_capitan
-    sha256 "ff785bd9efdab4345d8e1409934e173b2b18a35c87f522c30eef097b20c662e1" => :yosemite
-    sha256 "30c19104eeb1a1d3f70ea80ed73a352e03e976610a63c5775d77a15eb7da355c" => :mavericks
-    sha256 "f8d87f694df8fec823efe62702e317737c53fd5c1407f1007b7d5fae9f37974f" => :mountain_lion
+    sha256 "cdc9cc715cc716357b9faa701b930117e85b13e89a940ad6dbd6413af6308384" => :high_sierra
+    sha256 "dd4f69f0833e460653959dee684d687bc2186031c5f7481dc5b6d3bc0f383e59" => :sierra
+    sha256 "5ec5a7f6ae4857b2b43bdbcc360558e4f90ca6e510b42edd3ac5961988db195e" => :el_capitan
   end
 
   depends_on "cmake" => :build
