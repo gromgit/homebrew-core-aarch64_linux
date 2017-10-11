9c77bad461bbdf99ec9d833ebbe5aae950aa1fd5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-11 08:37:24 +0000

    libwpd: update 0.10.2 bottle.

--- Formula/libwpd.rb
+++ Formula/libwpd.rb
@@ -6,10 +6,9 @@ class Libwpd < Formula
 
   bottle do
     cellar :any
-    sha256 "64f3ae80b2deda62803579ee34d72a8c1c7f63151996ef3bdd1646282bb98c10" => :high_sierra
-    sha256 "7a644799cdb284d66951f6244fa224a209a853996a06b79cab6eb3628ac86718" => :sierra
-    sha256 "1867e211c3161ea4060c3c0cf65b2017e85d3079b41512948b4c7273870e5712" => :el_capitan
-    sha256 "f997aadedf26546b3e11ccab753145bb645e61b49c35e8fbe0bc41bbe5646e57" => :yosemite
+    sha256 "b240b96a69dc164ef6f4cdc3cdff10339cb1ce5d1593380319e8f41004d82d26" => :high_sierra
+    sha256 "5e7bd127154ff012858b889ab8b40c47498887f7cf5ef5c9d71eb8230d7ac68e" => :sierra
+    sha256 "c5368f8e62e66db7f5afcf1fb6b807af0d4a2ac5673863a787b16329e484f457" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
