6b1052d885ce3a9b2ffa123de113a9fc55330802
JCount <JCount42@gmail.com>
2017-10-11 13:27:59 -0400

    landscaper 1.0.11
    Closes #19340.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/landscaper.rb
+++ Formula/landscaper.rb
@@ -2,8 +2,8 @@ class Landscaper < Formula
   desc "Manage the application landscape in a Kubernetes cluster"
   homepage "https://github.com/Eneco/landscaper"
   url "https://github.com/Eneco/landscaper.git",
-      :tag => "1.0.10",
-      :revision => "c715c3b05d73ee8baf6b1c141524ff90ed80af2e"
+      :tag => "1.0.11",
+      :revision => "4fd0758658b712e24f71a966eea7ab2bf0d8c15b"
   head "https://github.com/Eneco/landscaper.git"
 
   bottle do
