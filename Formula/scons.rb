6deb6595261464f950b9970ff3abd0f6c74a3e75
ilovezfs <ilovezfs@icloud.com>
2017-10-11 04:14:44 -0700

    scons: support python 2 print statements
    Fix is from upstream.
    
    Closes #19319.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/scons.rb
+++ Formula/scons.rb
@@ -3,12 +3,20 @@ class Scons < Formula
   homepage "http://www.scons.org"
   url "https://downloads.sourceforge.net/project/scons/scons/3.0.0/scons-3.0.0.tar.gz"
   sha256 "0f532f405b98c60b731d231b3c503ab5bf47d89a6f66f70cb62c9249e9f45216"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
     sha256 "7d4495f68be68e5a6756a326914c0b1046921e95cbea330b604319f27bb4fc1f" => :high_sierra
     sha256 "962065c07692369e009d8377f9ee19f2043d07d631093eb7f8aba6f55bccd5bd" => :sierra
     sha256 "962065c07692369e009d8377f9ee19f2043d07d631093eb7f8aba6f55bccd5bd" => :el_capitan
+  end
+
+  # Remove for > 3.0.0
+  # Upstream commit from 20 Sep 2017 "Support python 2 print statements in SConscripts"
+  patch :p2 do
+    url "https://github.com/SConsProject/scons/commit/2e0de3c5.patch?full_index=1"
+    sha256 "ca9348417478a729f6fdaae62cc25d73a30371c4d467415196246f1d0dcfd195"
   end
 
   def install
