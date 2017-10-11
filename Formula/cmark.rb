f1367988e6828f22f5b8fd1f0cd642d3672d2fb4
JCount <JCount42@gmail.com>
2017-10-11 05:47:31 -0400

    cmark: use CMAKE_INSTALL_LIBDIR to set pkg-config file install dir
    Closes #19299.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/cmark.rb
+++ Formula/cmark.rb
@@ -17,7 +17,7 @@ class Cmark < Formula
 
   def install
     mkdir "build" do
-      system "cmake", "..", *std_cmake_args
+      system "cmake", "..", "-DCMAKE_INSTALL_LIBDIR=lib", *std_cmake_args
       system "make"
       system "make", "test"
       system "make", "install"
