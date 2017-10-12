07a310400690031cf793afc4e5e1a7ddcc90ee75
Alex Gaynor <alex.gaynor@gmail.com>
2017-10-12 12:34:20 -0400

    rust 1.21.0
    Closes #19375.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/rust.rb
+++ Formula/rust.rb
@@ -3,13 +3,13 @@ class Rust < Formula
   homepage "https://www.rust-lang.org/"
 
   stable do
-    url "https://static.rust-lang.org/dist/rustc-1.20.0-src.tar.gz"
-    sha256 "2aa4875ff4472c6e35262bbb9052cb2623da3dae6084a858cc59d36f33f18214"
+    url "https://static.rust-lang.org/dist/rustc-1.21.0-src.tar.gz"
+    sha256 "1707c142244b5bd909993559c6116c81987c1de21d6207c05d3ecbe5bba548fa"
 
     resource "cargo" do
       url "https://github.com/rust-lang/cargo.git",
-          :tag => "0.21.0",
-          :revision => "5b4b8b2ae3f6a884099544ce66dbb41626110ece"
+          :tag => "0.21.1",
+          :revision => "6084a2ba03aaa73794e6b86199e463ea6df290fe"
     end
 
     resource "racer" do
@@ -53,8 +53,8 @@ class Rust < Formula
 
   resource "cargobootstrap" do
     # From https://github.com/rust-lang/rust/blob/#{version}/src/stage0.txt
-    url "https://static.rust-lang.org/dist/2017-07-20/cargo-0.20.0-x86_64-apple-darwin.tar.gz"
-    sha256 "a7f93a287c903b597705d484ab5ff20400503d71670a2e04460ee7fc86de36a0"
+    url "https://static.rust-lang.org/dist/2017-08-31/cargo-0.21.0-x86_64-apple-darwin.tar.gz"
+    sha256 "05720fe91343c6ed5c4995f7b06677a08a585209f6f504d192722f3bb585bc9d"
   end
 
   def install
