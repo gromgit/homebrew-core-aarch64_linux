f3c4ea99338b2c2b96003ba8ebdc5e754ca89f6b
ilovezfs <ilovezfs@icloud.com>
2017-10-11 23:52:16 -0700

    angular-cli 1.4.6
    Closes #19355.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/angular-cli.rb
+++ Formula/angular-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class AngularCli < Formula
   desc "CLI tool for Angular"
   homepage "https://cli.angular.io/"
-  url "https://registry.npmjs.org/@angular/cli/-/cli-1.4.5.tgz"
-  sha256 "1c400473cd08e80cbf9912fe0b4c62a86f72a617f48e40865361c2160d8432f7"
+  url "https://registry.npmjs.org/@angular/cli/-/cli-1.4.6.tgz"
+  sha256 "c591204cb83def5b24e732a1c35f7873491ec520b53be16469cc56a79cc5f745"
 
   bottle do
     sha256 "f0bc7a5e645e7de7a3ace5daf00d91c3f86938be7f0d841e5e70a4998cbcd07c" => :high_sierra
