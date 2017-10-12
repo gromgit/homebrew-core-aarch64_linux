8c3af6a9b0a2336bb2f0f26a29dccd1eba43e2ae
FX Coudert <fxcoudert@gmail.com>
2017-10-12 10:05:43 +0200

    Revert "mpv: add option to enable audio CD playback (#16549)"
    This reverts commit 6b43f46bc5641a8b84cf065a3b7941880ad3de40.
    This new option does not build.

--- Formula/mpv.rb
+++ Formula/mpv.rb
@@ -35,7 +35,6 @@ class Mpv < Formula
   depends_on "rubberband" => :optional
   depends_on "uchardet" => :optional
   depends_on "vapoursynth" => :optional
-  depends_on "libcdio" => :optional
   depends_on :x11 => :optional
 
   depends_on :macos => :mountain_lion
@@ -72,7 +71,6 @@ class Mpv < Formula
     args << "--enable-libbluray" if build.with? "libbluray"
     args << "--enable-dvdnav" if build.with? "libdvdnav"
     args << "--enable-dvdread" if build.with? "libdvdread"
-    args << "--enable-cdda" if build.with? "libcdio"
     args << "--enable-pulse" if build.with? "pulseaudio"
 
     system "./bootstrap.py"
