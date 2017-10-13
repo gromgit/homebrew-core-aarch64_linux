087fd3d0e41bd51ca858ed9e93d5f66bf8862a77
JCount <JCount42@gmail.com>
2017-10-12 22:49:40 -0400

    cmake: add missing patch, fixes symbol not found error on sierra
    Closes #19388.
    
    Signed-off-by: JCount <JCount42@gmail.com>

--- Formula/cmake.rb
+++ Formula/cmake.rb
@@ -1,6 +1,8 @@
 class Cmake < Formula
   desc "Cross-platform make"
   homepage "https://www.cmake.org/"
+  revision 1
+
   head "https://cmake.org/cmake.git"
 
   stable do
@@ -9,14 +11,21 @@ class Cmake < Formula
 
     # The two patches below fix cmake for undefined symbols check on macOS 10.12
     # They can be removed for cmake >= 3.10
-    patch do
-      url "https://gitlab.kitware.com/cmake/cmake/commit/96329d5dffdd5a22c5b4428119b5d3762a8857a7.diff"
-      sha256 "c394d1b6e59e9bcf8e5db8a0a1189203e056c230a22aa8d60079fea7be6026bd"
-    end
+    if MacOS.version == :sierra && DevelopmentTools.clang_build_version >= 900
+      patch do
+        url "https://gitlab.kitware.com/cmake/cmake/commit/96329d5dffdd5a22c5b4428119b5d3762a8857a7.diff"
+        sha256 "c394d1b6e59e9bcf8e5db8a0a1189203e056c230a22aa8d60079fea7be6026bd"
+      end
 
-    patch do
-      url "https://gitlab.kitware.com/cmake/cmake/commit/f1a4ecdc0c62b46c90df5e8d20e6f61d06063894.diff"
-      sha256 "d32fa9c342d88e53b009f1fbeecc5872a79eec4bf2c8399f0fc2eeda5b0a4f1e"
+      patch do
+        url "https://gitlab.kitware.com/cmake/cmake/commit/f1a4ecdc0c62b46c90df5e8d20e6f61d06063894.diff"
+        sha256 "d32fa9c342d88e53b009f1fbeecc5872a79eec4bf2c8399f0fc2eeda5b0a4f1e"
+      end
+
+      patch do
+        url "https://raw.githubusercontent.com/Homebrew/formula-patches/105060cf885/cmake/cmake-backport-kwsys-utimensat-fix.diff"
+        sha256 "3e8aa1a6a1039e7a9be6fd0ca6abf09ca00fb07e1275bb3e55dc44b8b9dc746c"
+      end
     end
   end
 
