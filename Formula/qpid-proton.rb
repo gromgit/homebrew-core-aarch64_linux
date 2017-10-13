d0d1139900d09468ae0abf7c842185052860cd75
Jason Frey <fryguy9@gmail.com>
2017-10-13 14:08:56 -0400

    qpid-proton 0.18.0 (new formula)
    Closes #19414.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/qpid-proton.rb
@@ -0,0 +1,36 @@
+class QpidProton < Formula
+  desc "High-performance, lightweight AMQP 1.0 messaging library"
+  homepage "https://qpid.apache.org/proton/"
+  url "https://www.apache.org/dyn/closer.lua?path=qpid/proton/0.18.0/qpid-proton-0.18.0.tar.gz"
+  sha256 "7f54c3555a8482d439759c087d656d881369252af23f209f1fd5a7e4a8c59bd7"
+  head "git://git.apache.org/qpid-proton.git"
+
+  depends_on "cmake" => :build
+  depends_on "libuv"
+  depends_on "openssl"
+
+  def install
+    system "cmake", ".", "-DBUILD_BINDINGS=",
+                         "-DLIB_INSTALL_DIR=#{lib}",
+                         *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include "proton/message.h"
+      #include "proton/messenger.h"
+      int main()
+      {
+          pn_message_t * message;
+          pn_messenger_t * messenger;
+          pn_data_t * body;
+          message = pn_message();
+          messenger = pn_messenger(NULL);
+          return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lqpid-proton", "-o", "test"
+    system "./test"
+  end
+end
