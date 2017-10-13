299a8d0b875524d09862f7ab97afb3d15a9e2b54
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-13 07:00:03 +0000

    ats2-postiats: update 0.3.7 bottle.

--- Formula/ats2-postiats.rb
+++ Formula/ats2-postiats.rb
@@ -6,10 +6,9 @@ class Ats2Postiats < Formula
 
   bottle do
     cellar :any
-    sha256 "1a6111d01d2edf77a0aaaeb3272289ea3707f7a26f5d9b6aacffc896ea57d20d" => :high_sierra
-    sha256 "de22f12b7bb115c9b8581fedd231e134698203ce62740b6a396eea90b410a434" => :sierra
-    sha256 "6957983d0683534c2db77df0e29f2c0d0079c4ecc298a9aade88a05a033e05e5" => :el_capitan
-    sha256 "42a23f6d7410ff603e52ce5fe8eed3ae015dfb4c6896c777534c65ca777fc6ce" => :yosemite
+    sha256 "42478768ce3516ce46d82c0df9cb7499b96e39f5cb527d2fb1f3e04da40eda98" => :high_sierra
+    sha256 "670a2c12297acc3627258522e925ee0b2cc47fd197cd78b970ba83b8ed06d581" => :sierra
+    sha256 "dc9f99d784c8b80d3903af019f78d70e2552c7f17f064d76cbabf8c45e5eb9be" => :el_capitan
   end
 
   depends_on "gmp"
