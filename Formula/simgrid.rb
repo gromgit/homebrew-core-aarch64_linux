0d565f4bd60e3f184d664799d1fb1a1ee5faba25
John Kitsantas <proteus@devoid.gr>
2017-10-14 11:48:09 +0300

    simgrid 3.17
    depend on doxygen at build time
    require python3
    add a test
    
    Closes #19446.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/simgrid.rb
+++ Formula/simgrid.rb
@@ -1,8 +1,8 @@
 class Simgrid < Formula
   desc "Studies behavior of large-scale distributed systems"
   homepage "http://simgrid.gforge.inria.fr"
-  url "https://gforge.inria.fr/frs/download.php/file/33686/SimGrid-3.11.1.tar.gz"
-  sha256 "7796ef6d4288462fdabdf5696c453ea6aabc433a813a384db2950ae26eff7956"
+  url "https://gforge.inria.fr/frs/download.php/file/37148/SimGrid-3.17.tar.gz"
+  sha256 "f5e44f41983e83f65261598ab0de1909d3a8a3feb77f28e37d38f04631dbb908"
 
   bottle do
     cellar :any
@@ -15,8 +15,10 @@ class Simgrid < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on "doxygen" => :build
   depends_on "boost"
   depends_on "pcre"
+  depends_on :python3
   depends_on "graphviz"
 
   def install
@@ -25,5 +27,21 @@ class Simgrid < Formula
                     "-Denable_compile_optimizations=off",
                     *std_cmake_args
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <stdio.h>
+      #include <stdlib.h>
+      #include <simgrid/msg.h>
+
+      int main(int argc, char* argv[]) {
+        printf("%f", MSG_get_clock());
+        return 0;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-lsimgrid", "-o", "test"
+    system "./test"
   end
 end
