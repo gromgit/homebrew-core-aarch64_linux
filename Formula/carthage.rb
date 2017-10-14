ff06e2f9794cdff3d93b1a42b36ec14f9f1f49e5
Sho Ikeda <suicaicoca@gmail.com>
2017-10-14 11:04:27 +0900

    carthage 0.26.2
    Closes #19436.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/carthage.rb
+++ Formula/carthage.rb
@@ -2,8 +2,8 @@ class Carthage < Formula
   desc "Decentralized dependency manager for Cocoa"
   homepage "https://github.com/Carthage/Carthage"
   url "https://github.com/Carthage/Carthage.git",
-      :tag => "0.26.1",
-      :revision => "ff76d2034d2d0e3ae37fcdb3338210ba55cdcb62",
+      :tag => "0.26.2",
+      :revision => "452f2d5a9917befe722b944360aefd7ae05fc6d5",
       :shallow => false
   head "https://github.com/Carthage/Carthage.git", :shallow => false
 
