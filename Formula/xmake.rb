7005b1a700d1899eab36c95e85c04c79c2992631
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-14 02:27:25 +0000

    xmake: update 2.1.7 bottle.

--- Formula/xmake.rb
+++ Formula/xmake.rb
@@ -7,10 +7,9 @@ class Xmake < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b5c5f60342a69f67df6677430b1fe8c510db61380a0e3b54ed2e3ac68bf70c2f" => :high_sierra
-    sha256 "eb544a2e89f167ccde77eb456fe0a6af0d11364a0e6f5543a366fab3f92335b6" => :sierra
-    sha256 "6afca5097e3718b9c8efa55c54dec7241a0be7b45405ca668db432959038a248" => :el_capitan
-    sha256 "e980cd27838bec8085559305af416cf9f55f059c8e3e74d452d4a89d41523d08" => :yosemite
+    sha256 "2496d281a4aa92e353bfbe5d03f8e026d24869a881b795e5d9732f531ba20d00" => :high_sierra
+    sha256 "2496d281a4aa92e353bfbe5d03f8e026d24869a881b795e5d9732f531ba20d00" => :sierra
+    sha256 "752d4ef4404d364ca1046851abe8d9023dad7143f21539a5fa3ed63f98c1faae" => :el_capitan
   end
 
   def install
