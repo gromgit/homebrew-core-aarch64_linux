8b16dc917eb81e36bc1e25978789bc0c4da16fcf
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-15 07:38:40 +0000

    simgrid: update 3.17 bottle.

--- Formula/simgrid.rb
+++ Formula/simgrid.rb
@@ -5,13 +5,9 @@ class Simgrid < Formula
   sha256 "f5e44f41983e83f65261598ab0de1909d3a8a3feb77f28e37d38f04631dbb908"
 
   bottle do
-    cellar :any
-    rebuild 1
-    sha256 "5854edfa8e3b5b75dffe0f7a800fab040a232b1657043b921edaf97d0cd6e740" => :high_sierra
-    sha256 "805c0f23b83c5f0808fe810e91d98e8c88a55acc1292481dc4aaa003b43aceb5" => :sierra
-    sha256 "37282c46131c5cfed6c3a12be3cb05487b800904a38c46cf3133a5b9a335d2fa" => :el_capitan
-    sha256 "ade94652c0b0157914e4a6e1e637cae0fedf57e036245680595d0913e7df815a" => :yosemite
-    sha256 "e36538092a178bed8956c4537095a17feb594846ee0dfe72c9baec8e9a52eb80" => :mavericks
+    sha256 "25156b23d0a2779e9d8207266d621c4328d83f1089005969991733e5007bb1d0" => :high_sierra
+    sha256 "5b383b0c5f6c6191a4843f7e419ca4739254d96d3c33bcba7cc19e05efd8b537" => :sierra
+    sha256 "a9a7b7d60cb9b7f586767d1225bd2a0ca10708285c2fb41ee84d8233b531d288" => :el_capitan
   end
 
   depends_on "cmake" => :build
