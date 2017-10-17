9d236acfe78eb226fcc30250564a1fee01bdc5e1
Alex Gaynor <alex.gaynor@gmail.com>
2017-10-17 19:38:22 -0400

    yubico-piv-tool 1.4.4
    Closes #19573.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/yubico-piv-tool.rb
+++ Formula/yubico-piv-tool.rb
@@ -1,8 +1,8 @@
 class YubicoPivTool < Formula
   desc "Command-line tool for the YubiKey NEO PIV applet"
   homepage "https://developers.yubico.com/yubico-piv-tool/"
-  url "https://developers.yubico.com/yubico-piv-tool/Releases/yubico-piv-tool-1.4.3.tar.gz"
-  sha256 "ea83a11f7db5ac81ebcafe71e102c3a5d694929bb95354c3185c081fa6a86dab"
+  url "https://developers.yubico.com/yubico-piv-tool/Releases/yubico-piv-tool-1.4.4.tar.gz"
+  sha256 "ab0eac3f78e9e01538723f631ed6042504409a1ab0342973e2bd52f2c68a3769"
 
   bottle do
     cellar :any
