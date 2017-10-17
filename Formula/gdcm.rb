74a46b3464bff975e4327caff83567cb2650d718
David Hall <dchall@mgh.harvard.edu>
2017-10-17 11:01:11 -0400

    gdcm: add python bindings
    Closes #19520.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gdcm.rb
+++ Formula/gdcm.rb
@@ -3,13 +3,18 @@ class Gdcm < Formula
   homepage "https://sourceforge.net/projects/gdcm/"
   url "https://downloads.sourceforge.net/project/gdcm/gdcm%202.x/GDCM%202.8.2/gdcm-2.8.2.tar.gz"
   sha256 "5462c7859e01e5d5d0fb86a19a6c775484a6c44abd8545ea71180d4c41bf0f89"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "3fd15851c093b4e0c2481233268e41614b26dde24aa2c13f9107402ae8bf2ddd" => :high_sierra
     sha256 "4fad6b9761ca2ab4c9b26b418d57ef26f2dd19bebb77a658b5b5aadda0212098" => :sierra
     sha256 "dbfbcaa31ceaedc98985614899601f771e951cbd6c115223082ab73ef111f231" => :el_capitan
   end
+
+  option "without-python", "Build without python2 support"
+
+  depends_on :python3 => :optional
+  depends_on "swig" => :build if build.with?("python") || build.with?("python3")
 
   depends_on "cmake" => :build
   depends_on "openssl"
@@ -20,16 +25,38 @@ class Gdcm < Formula
   def install
     ENV.cxx11
 
+    common_args = std_cmake_args + %w[
+      -DGDCM_BUILD_APPLICATIONS=ON
+      -DGDCM_BUILD_SHARED_LIBS=ON
+      -DGDCM_BUILD_TESTING=OFF
+      -DGDCM_BUILD_EXAMPLES=OFF
+      -DGDCM_BUILD_DOCBOOK_MANPAGES=OFF
+      -DGDCM_USE_VTK=ON
+      -DGDCM_USE_SYSTEM_OPENSSL=ON
+    ]
+
     mkdir "build" do
-      system "cmake", "..", "-DGDCM_BUILD_APPLICATIONS=ON",
-                            "-DGDCM_BUILD_SHARED_LIBS=ON",
-                            "-DGDCM_BUILD_TESTING=OFF",
-                            "-DGDCM_BUILD_EXAMPLES=OFF",
-                            "-DGDCM_BUILD_DOCBOOK_MANPAGES=OFF",
-                            "-DGDCM_USE_VTK=ON",
-                            "-DGDCM_USE_SYSTEM_OPENSSL=ON",
-                            *std_cmake_args
-      system "make", "install"
+      if build.without?("python") && build.without?("python3")
+        system "cmake", "..", *common_args
+        system "make", "install"
+      else
+        ENV.append "LDFLAGS", "-undefined dynamic_lookup"
+
+        Language::Python.each_python(build) do |python, py_version|
+          python_include = Utils.popen_read("#{python} -c 'from distutils import sysconfig;print(sysconfig.get_python_inc(True))'").chomp
+          args = common_args + %W[
+            -DGDCM_WRAP_PYTHON=ON
+            -DPYTHON_EXECUTABLE=#{python}
+            -DPYTHON_INCLUDE_DIR=#{python_include}
+            -DGDCM_INSTALL_PYTHONMODULE_DIR=#{lib}/python#{py_version}/site-packages
+            -DCMAKE_INSTALL_RPATH=#{lib}
+            -DGDCM_NO_PYTHON_LIBS_LINKING=ON
+          ]
+
+          system "cmake", "..", *args
+          system "make", "install"
+        end
+      end
     end
   end
 
@@ -46,5 +73,9 @@ class Gdcm < Formula
     system ENV.cxx, "-isystem", "#{include}/gdcm-2.8", "-o", "test.cxx.o", "-c", "test.cxx"
     system ENV.cxx, "test.cxx.o", "-o", "test", "-L#{lib}", "-lgdcmDSED"
     system "./test"
+
+    Language::Python.each_python(build) do |python, _py_version|
+      system python, "-c", "import gdcm"
+    end
   end
 end
