6608af83848ea03eca0cec7d43e8e9f06915c644
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-17 19:50:51 +0000

    git-appraise: add 0.6 bottle.

--- Formula/git-appraise.rb
+++ Formula/git-appraise.rb
@@ -5,6 +5,13 @@ class GitAppraise < Formula
   sha256 "5c674ee7f022cbc36c5889053382dde80b8e80f76f6fac0ba0445ed5313a36f1"
   head "https://github.com/google/git-appraise.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e515979b703cef062e19829399ddb441c91d835e25814614c938af36764fc0d4" => :high_sierra
+    sha256 "c048f2cce708e7c85c74d18758e47d3959cce29e2f8e70bca021b1564e65092d" => :sierra
+    sha256 "e12ce185286565f4f07f48f1deb2fd4a19043bcafb337de94b9ba7148291b91b" => :el_capitan
+  end
+
   depends_on "go" => :build
 
   def install
