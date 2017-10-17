63eb4e70612ff6ff4fff6b114f323ef23e05a5cb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-17 16:16:04 +0000

    aria2: update 1.33.0 bottle.

--- Formula/aria2.rb
+++ Formula/aria2.rb
@@ -6,10 +6,9 @@ class Aria2 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b3e033effeae03f864d277a9a9ea65a436b4a87afac3e0b96226ca973df5d875" => :high_sierra
-    sha256 "19d139a71749930d7bf15c084035943c1d9980d6e9782392bf24fe0068785ead" => :sierra
-    sha256 "fa1343fb305a888ae1b806c7057b50864661b728e89ed1c42288fe0ac837968e" => :el_capitan
-    sha256 "804fcc00bd3beab200b77a2e1c538cef4861fb27c97d3fe6ee8d3f67112f57b0" => :yosemite
+    sha256 "d97e97cf8f8cafceae11083b69911e9067ade8bf9112a4baffc79219580aa790" => :high_sierra
+    sha256 "87e3e594eebbf6820cb1af881a570cceee6fd8621c883033208fc970a4bc52fa" => :sierra
+    sha256 "dce6d7d93b447edbfbc47d5a5d5ea69129015de3a504f2cb20b78e91d5117ec1" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
