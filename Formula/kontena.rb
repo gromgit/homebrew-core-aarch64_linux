947c1fc5357cecde4a7c9691174534b6870071fe
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-17 10:02:25 +0000

    kontena: add 1.4.0 bottle.

--- Formula/kontena.rb
+++ Formula/kontena.rb
@@ -154,6 +154,13 @@ class Kontena < Formula
     end
   end
 
+  bottle do
+    cellar :any
+    sha256 "b24b6c27bd25f03a425a29b692534fc7c81b4144fa1c6b1fd1f8fbbf2b0b9c63" => :high_sierra
+    sha256 "529128d8ff36c0afcb33b7499d0b01cef550701c24c71db6115e92cb4d75b94f" => :sierra
+    sha256 "f0f44fde8847a0e94e9a3238d9cd8af92b2cf3a171401830ff569406b753789f" => :el_capitan
+  end
+
   depends_on :ruby => "2.1"
 
   def install
