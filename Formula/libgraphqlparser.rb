02fafdd940fbadd19c2a0948393c32e7b9c89fbc
ilovezfs <ilovezfs@icloud.com>
2017-10-16 23:22:22 -0700

    libgraphqlparser 0.7.0
    Closes #19535.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libgraphqlparser.rb
+++ Formula/libgraphqlparser.rb
@@ -1,8 +1,8 @@
 class Libgraphqlparser < Formula
   desc "GraphQL query parser in C++ with C and C++ APIs"
   homepage "https://github.com/graphql/libgraphqlparser"
-  url "https://github.com/graphql/libgraphqlparser/archive/v0.6.0.tar.gz"
-  sha256 "3d8f5878754203563d06a65175accb8ee9e4eb8b49cb51e6d5c6a1d9ff6aa5ad"
+  url "https://github.com/graphql/libgraphqlparser/archive/0.7.0.tar.gz"
+  sha256 "63dae018f970dc2bdce431cbafbfa0bd3e6b10bba078bb997a3c1a40894aa35c"
 
   bottle do
     cellar :any
@@ -27,23 +27,30 @@ class Libgraphqlparser < Formula
     EOS
 
     sample_ast = { "kind"=>"Document",
-                   "loc"=>{ "start"=>1, "end"=>9 },
+                   "loc"=>{ "start"=>{ "line"=>1, "column"=>1 },
+                            "end"=>{ "line"=>1, "column"=>9 } },
                    "definitions"=>
         [{ "kind"=>"OperationDefinition",
-           "loc"=>{ "start"=>1, "end"=>9 },
+           "loc"=>{ "start"=>{ "line"=>1, "column"=>1 },
+                    "end"=>{ "line"=>1, "column"=>9 } },
            "operation"=>"query",
            "name"=>nil,
            "variableDefinitions"=>nil,
            "directives"=>nil,
            "selectionSet"=>
            { "kind"=>"SelectionSet",
-             "loc"=>{ "start"=>1, "end"=>9 },
+             "loc"=>{ "start"=>{ "line"=>1, "column"=>1 },
+                      "end"=>{ "line"=>1, "column"=>9 } },
              "selections"=>
              [{ "kind"=>"Field",
-                "loc"=>{ "start"=>3, "end"=>7 },
+                "loc"=>{ "start"=>{ "line"=>1, "column"=>3 },
+                         "end"=>{ "line"=>1, "column"=>7 } },
                 "alias"=>nil,
                 "name"=>
-                { "kind"=>"Name", "loc"=>{ "start"=>3, "end"=>7 }, "value"=>"user" },
+                { "kind"=>"Name",
+                  "loc"=>{ "start"=>{ "line"=>1, "column"=>3 },
+                           "end"=>{ "line"=>1, "column"=>7 } },
+                  "value"=>"user" },
                 "arguments"=>nil,
                 "directives"=>nil,
                 "selectionSet"=>nil }] } }] }
