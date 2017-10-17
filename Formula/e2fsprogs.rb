9863581e1a9fbd2a7b3f7c8acdd21852efb8fc08
ilovezfs <ilovezfs@icloud.com>
2017-10-17 14:16:22 -0700

    e2fsprogs 1.43.7
    Closes #19563.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Formula/e2fsprogs.rb
+++ Formula/e2fsprogs.rb
@@ -1,8 +1,8 @@
 class E2fsprogs < Formula
   desc "Utilities for the ext2, ext3, and ext4 file systems"
   homepage "https://e2fsprogs.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/v1.43.6/e2fsprogs-1.43.6.tar.gz"
-  sha256 "60139d75802925b0c23d451da8e4fc84c5e7cf94626cc324b59295495c0fdc80"
+  url "https://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/v1.43.7/e2fsprogs-1.43.7.tar.gz"
+  sha256 "87035f2eae8da5f9869f78ffc177969b4e3cf75a5da489521c1ffe4268e1a1c4"
 
   head "https://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git"
 
