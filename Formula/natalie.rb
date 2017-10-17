9bae5f4cf662503c97ab2e1e201df8a3eefb9bc2
ilovezfs <ilovezfs@icloud.com>
2017-10-17 05:43:07 -0700

    natalie 0.6.0
    Closes #19545.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/natalie.rb
+++ Formula/natalie.rb
@@ -1,8 +1,8 @@
 class Natalie < Formula
   desc "Storyboard Code Generator (for Swift)"
   homepage "https://github.com/krzyzanowskim/Natalie"
-  url "https://github.com/krzyzanowskim/Natalie/archive/0.5.0.tar.gz"
-  sha256 "66e00a4095121255a9740a16a8a59daa289f878f2e1e77ba6a9f98d6a671a33c"
+  url "https://github.com/krzyzanowskim/Natalie/archive/0.6.0.tar.gz"
+  sha256 "e2f1eced8ce22b966169107a07c3978e12d512a9119f5ca88815b96f55c82f4a"
   head "https://github.com/krzyzanowskim/Natalie.git"
 
   bottle do
@@ -12,11 +12,11 @@ class Natalie < Formula
     sha256 "472b56132024812e48041b4ea4174e889f95eab70e8ff83d6182bde406865d88" => :el_capitan
   end
 
-  depends_on :xcode => ["8.0", :build]
+  depends_on :xcode => ["9.0", :build]
 
   def install
-    ENV["CC"] = which(ENV.cc)
-    system "swift", "build", "-c", "release", "-Xswiftc", "-static-stdlib"
+    system "swift", "build", "--disable-sandbox", "-c", "release", "-Xswiftc",
+           "-static-stdlib"
     bin.install ".build/release/natalie"
     share.install "NatalieExample"
   end
