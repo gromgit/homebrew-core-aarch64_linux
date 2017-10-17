4103b58dba71f3a849d057853982ef88afdb57ea
ilovezfs <ilovezfs@icloud.com>
2017-10-17 14:30:41 -0700

    root 6.10.08
    Closes #19567.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/root.rb
+++ Formula/root.rb
@@ -1,9 +1,9 @@
 class Root < Formula
   desc "Object oriented framework for large scale data analysis"
   homepage "https://root.cern.ch"
-  url "https://root.cern.ch/download/root_v6.10.06.source.tar.gz"
-  version "6.10.06"
-  sha256 "02ba62b2a732f4f8d7beecb29556545cc30d122bc87da904473de69a8972ed74"
+  url "https://root.cern.ch/download/root_v6.10.08.source.tar.gz"
+  version "6.10.08"
+  sha256 "2cd276d2ac365403c66f08edd1be62fe932a0334f76349b24d8c737c0d6dad8a"
   head "http://root.cern.ch/git/root.git"
 
   bottle do
@@ -25,24 +25,6 @@ class Root < Formula
   needs :cxx11
 
   skip_clean "bin"
-
-  # 3 upstream commits that fix compilation with Xcode 9
-  if DevelopmentTools.clang_build_version >= 900
-    patch do
-      url "https://github.com/root-project/root/commit/26350842.patch?full_index=1"
-      sha256 "27d29c775d8c8100ebd7f206eeb8364a30015f39a1af1a00203ef80ff1a04cd9"
-    end
-
-    patch do
-      url "https://github.com/root-project/root/commit/9339de9e.patch?full_index=1"
-      sha256 "f7386c626fcc64791cfcbe35a0efc10c245c10e4f0547687334592da70c99ab5"
-    end
-
-    patch do
-      url "https://github.com/root-project/root/commit/7d585604.patch?full_index=1"
-      sha256 "3ff15aa6f15621d14d07de06ed4e31f901d07da54695fae1948dba5e2884fc8f"
-    end
-  end
 
   def install
     # Work around "error: no member named 'signbit' in the global namespace"
