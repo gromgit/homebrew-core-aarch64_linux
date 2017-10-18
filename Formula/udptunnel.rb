bf9eac2560bd28358c2321293c104636f5b7b70b
Viktor Szakats <vszakats@users.noreply.github.com>
2017-10-18 14:24:43 +0000

    udptunnel: secure url(s)

--- Formula/udptunnel.rb
+++ Formula/udptunnel.rb
@@ -1,8 +1,8 @@
 class Udptunnel < Formula
   desc "Tunnel UDP packets over a TCP connection"
-  homepage "http://www.cs.columbia.edu/~lennox/udptunnel"
-  url "http://www.cs.columbia.edu/~lennox/udptunnel/udptunnel-1.1.tar.gz"
-  mirror "http://ftp.nsysu.edu.tw/FreeBSD/ports/local-distfiles/leeym/udptunnel-1.1.tar.gz"
+  homepage "https://web.archive.org/web/20161224191851/www.cs.columbia.edu/~lennox/udptunnel/"
+  url "https://web.archive.org/web/20161224191851/www.cs.columbia.edu/~lennox/udptunnel/udptunnel-1.1.tar.gz"
+  mirror "https://ftp.nsysu.edu.tw/FreeBSD/ports/local-distfiles/leeym/udptunnel-1.1.tar.gz"
   sha256 "45c0e12045735bc55734076ebbdc7622c746d1fe4e6f7267fa122e2421754670"
 
   bottle do
