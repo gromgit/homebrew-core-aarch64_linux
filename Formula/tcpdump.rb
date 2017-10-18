11e85175a72cf868ac1bb993218860e32b81358e
Viktor Szakats <vszakats@users.noreply.github.com>
2017-10-18 14:24:43 +0000

    tcpdump: secure url(s)

--- Formula/tcpdump.rb
+++ Formula/tcpdump.rb
@@ -1,9 +1,8 @@
 class Tcpdump < Formula
   desc "Command-line packet analyzer"
-  homepage "http://www.tcpdump.org/"
-  url "http://www.tcpdump.org/release/tcpdump-4.9.2.tar.gz"
+  homepage "https://www.tcpdump.org/"
+  url "https://www.tcpdump.org/release/tcpdump-4.9.2.tar.gz"
   sha256 "798b3536a29832ce0cbb07fafb1ce5097c95e308a6f592d14052e1ef1505fe79"
-
   head "https://github.com/the-tcpdump-group/tcpdump.git"
 
   bottle do
