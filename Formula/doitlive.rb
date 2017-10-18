517b878441166a1b13ad499e52c50544162d8a7c
Steven Loria <sloria1@gmail.com>
2017-10-17 21:10:35 -0400

    doitlive 3.0.2
    Closes #19581.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/doitlive.rb
+++ Formula/doitlive.rb
@@ -1,8 +1,8 @@
 class Doitlive < Formula
   desc "Replay stored shell commands for live presentations"
   homepage "https://doitlive.readthedocs.io/en/latest/"
-  url "https://files.pythonhosted.org/packages/49/71/6d566ac6b80402c81729f8347f643b01723e81f3dfc7fb94027231b9d292/doitlive-3.0.1.tar.gz"
-  sha256 "b70411f7af1041fc58f25a881e44d66ed7c481a7a8ef6f2bb914d76b012c3046"
+  url "https://files.pythonhosted.org/packages/65/2a/b12740bbd4ff45647b50e202236388c8bca5448ecd177ead1f244d07a617/doitlive-3.0.2.tar.gz"
+  sha256 "df5f595a7809f11dd3676c3e271e3cf1ff904b8f7e4191fb81b229b6d08f8bf5"
 
   bottle do
     cellar :any_skip_relocation
