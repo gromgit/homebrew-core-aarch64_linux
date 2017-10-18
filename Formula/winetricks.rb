364821d650f565d59edba0269033199b3ca01314
ilovezfs <ilovezfs@icloud.com>
2017-10-18 07:18:17 -0700

    winetricks 20171018 (#19597)

--- Formula/winetricks.rb
+++ Formula/winetricks.rb
@@ -1,8 +1,8 @@
 class Winetricks < Formula
   desc "Download and install various runtime libraries"
   homepage "https://github.com/Winetricks/winetricks"
-  url "https://github.com/Winetricks/winetricks/archive/20170823.tar.gz"
-  sha256 "0e7e007b0dd39f773213a5540e7c44e4105d9435ef067c0efdcc6fda70c029de"
+  url "https://github.com/Winetricks/winetricks/archive/20171018.tar.gz"
+  sha256 "ad60a2ffbffbe62c47e4f85767436814e67c5a42b838ba30b40a4fa36e5ab0cc"
   head "https://github.com/Winetricks/winetricks.git"
 
   bottle :unneeded
