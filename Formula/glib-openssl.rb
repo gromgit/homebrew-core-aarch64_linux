6ed6e78bb2b5fa18f2e155d9d8cd8d854064c213
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-10-18 12:20:51 +0100

    glib-openssl 2.50.4 (new formula)
    Closes #19601.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/glib-openssl.rb
@@ -0,0 +1,62 @@
+class GlibOpenssl < Formula
+  desc "OpenSSL GIO module for glib"
+  homepage "https://launchpad.net/glib-networking"
+  url "https://download.gnome.org/sources/glib-openssl/2.50/glib-openssl-2.50.4.tar.xz"
+  sha256 "eda914e29d9d72f03185aba92c33ec3949862c05b73b8eb04d1e1b6217b756c0"
+
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "openssl"
+
+  def install
+    # Install files to `lib` instead of `HOMEBREW_PREFIX/lib`.
+    inreplace "configure", "$($PKG_CONFIG --variable giomoduledir gio-2.0)", lib/"gio/modules"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--disable-static",
+                          "--prefix=#{prefix}",
+                          "--with-ca-certificates=#{etc}/openssl/cert.pem"
+    system "make", "install"
+
+    # Delete the cache, will regenerate it in post_install
+    rm lib/"gio/modules/giomodule.cache"
+  end
+
+  def post_install
+    system Formula["glib"].opt_bin/"gio-querymodules", HOMEBREW_PREFIX/"lib/gio/modules"
+  end
+
+  test do
+    (testpath/"gtls-test.c").write <<-EOS.undent
+      #include <gio/gio.h>
+      #include <string.h>
+      int main (int argc, char *argv[])
+      {
+        GType type = g_tls_backend_get_certificate_type(g_tls_backend_get_default());
+        if (strcmp(g_type_name(type), "GTlsCertificateOpenssl") == 0)
+          return 0;
+        else
+          return 1;
+      }
+    EOS
+
+    # From `pkg-config --cflags --libs gio-2.0`
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    flags = %W[
+      -D_REENTRANT
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/gio-unix-2.0/
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -lgio-2.0
+      -lgobject-2.0
+      -lglib-2.0
+      -lintl
+    ]
+    system ENV.cc, "gtls-test.c", "-o", "gtls-test", *flags
+    system "./gtls-test"
+  end
+end
