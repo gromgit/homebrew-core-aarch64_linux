520e1622a26f570b7a0edbd5daba4055940e7faf
Viktor Szakats <vszakats@users.noreply.github.com>
2017-10-18 14:24:42 +0000

    keychain: secure url(s)

--- Formula/keychain.rb
+++ Formula/keychain.rb
@@ -1,7 +1,7 @@
 class Keychain < Formula
   desc "User-friendly front-end to ssh-agent(1)"
-  homepage "http://www.funtoo.org/Keychain"
-  url "http://build.funtoo.org/distfiles/keychain/keychain-2.8.3.tar.bz2"
+  homepage "https://www.funtoo.org/Keychain"
+  url "https://build.funtoo.org/distfiles/keychain/keychain-2.8.3.tar.bz2"
   mirror "https://fossies.org/linux/privat/keychain-2.8.3.tar.bz2"
   sha256 "d05eb924efcaef78eddff8e3190154a39778f0eee4f90362528c81ad8dadde56"
 
