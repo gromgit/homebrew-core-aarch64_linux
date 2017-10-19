abcaa8409af77e0d12392b8e5fa61a98ed7c7cf8
Viktor Szakats <vszakats@users.noreply.github.com>
2017-10-19 01:46:52 +0000

    ecj: secure url(s)

--- Formula/ecj.rb
+++ Formula/ecj.rb
@@ -3,7 +3,7 @@
 class Ecj < Formula
   desc "Standalone version of the Eclipse Java compiler"
   homepage "https://gcc.gnu.org/"
-  url "ftp://sourceware.org/pub/java/ecj-4.9.jar"
+  url "https://sourceware.org/pub/java/ecj-4.9.jar"
   mirror "https://mirrors.kernel.org/sources.redhat.com/java/ecj-4.9.jar"
   sha256 "9506e75b862f782213df61af67338eb7a23c35ff425d328affc65585477d34cd"
 
