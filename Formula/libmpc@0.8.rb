facf6e85398ce918a0161adac5160419c0d78d75
Viktor Szakats <vszakats@users.noreply.github.com>
2017-10-19 01:46:53 +0000

    libmpc@0.8: secure url(s)

--- Formula/libmpc@0.8.rb
+++ Formula/libmpc@0.8.rb
@@ -1,9 +1,9 @@
 class LibmpcAT08 < Formula
   desc "C library for high precision complex numbers"
-  homepage "http://multiprecision.org"
+  homepage "http://multiprecision.org/"
   # Track gcc infrastructure releases.
-  url "http://multiprecision.org/mpc/download/mpc-0.8.1.tar.gz"
-  mirror "ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-0.8.1.tar.gz"
+  url "https://gcc.gnu.org/pub/gcc/infrastructure/mpc-0.8.1.tar.gz"
+  mirror "http://multiprecision.org/mpc/download/mpc-0.8.1.tar.gz"
   sha256 "e664603757251fd8a352848276497a4c79b7f8b21fd8aedd5cc0598a38fee3e4"
 
   bottle do
