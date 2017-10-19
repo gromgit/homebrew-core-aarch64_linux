c89b63aa24b7607edfc8e4b157dd1a3ccafc6a4c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-19 00:07:54 +0000

    sc-im: add 0.6.0 bottle.

--- Formula/sc-im.rb
+++ Formula/sc-im.rb
@@ -5,6 +5,12 @@ class ScIm < Formula
   sha256 "5da644d380ab3752de283b83cce18c3ba12b068d0762c44193c34367a0dcbc38"
   head "https://github.com/andmarti1424/sc-im.git", :branch => "freeze"
 
+  bottle do
+    sha256 "077da2cd3c11e733b98e001a0399d60bf4692bb77da4788ea10b1e1c7492bebe" => :high_sierra
+    sha256 "52537994a6624c403e51686d25b95c069678bfff060b6cd641e1d821791889b5" => :sierra
+    sha256 "b7b86baf81055e52762736acdd5b2f22985e160e66e231d424d1f99452008f6a" => :el_capitan
+  end
+
   depends_on "ncurses"
 
   def install
