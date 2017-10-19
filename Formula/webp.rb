6b1b332da555b913c39e3780b66b6162b3d6cff0
Viktor Szakats <vszakats@users.noreply.github.com>
2017-10-19 01:46:54 +0000

    webp: secure url(s)

--- Formula/webp.rb
+++ Formula/webp.rb
@@ -1,7 +1,7 @@
 class Webp < Formula
   desc "Image format providing lossless and lossy compression for web images"
   homepage "https://developers.google.com/speed/webp/"
-  url "http://downloads.webmproject.org/releases/webp/libwebp-0.6.0.tar.gz"
+  url "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-0.6.0.tar.gz"
   sha256 "c928119229d4f8f35e20113ffb61f281eda267634a8dc2285af4b0ee27cf2b40"
   revision 2
 
