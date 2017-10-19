504c447c7b76f27473e320876603a896e7b01e31
Leonardo Medici <leonardo_medici@me.com>
2017-10-19 12:08:05 +0200

    dislocker 0.7.1 (new formula)
    Closes #19360.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/dislocker.rb
@@ -0,0 +1,20 @@
+class Dislocker < Formula
+  desc "FUSE driver to read/write Windows' BitLocker-ed volumes"
+  homepage "https://github.com/Aorimn/dislocker"
+  url "https://github.com/Aorimn/dislocker/archive/v0.7.1.tar.gz"
+  sha256 "742fb5c1b3ff540368ced54c29eae8b488ae5a5fcaca092947e17c2d358a6762"
+
+  depends_on "cmake" => :build
+  depends_on "mbedtls"
+  depends_on :osxfuse
+
+  def install
+    system "cmake", "-DCMAKE_DISABLE_FIND_PACKAGE_Ruby=TRUE", *std_cmake_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/dislocker", "-h"
+  end
+end
