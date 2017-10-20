13d0bc6fe01ed83367d3c30b8ea40f5a38c9c80d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-20 17:38:35 +0000

    wine: update 2.0.3 bottle.

--- Formula/wine.rb
+++ Formula/wine.rb
@@ -28,10 +28,9 @@ class Wine < Formula
   end
 
   bottle do
-    sha256 "b3ea4bf10ef9e1ecd2d41719b71aff20992c893c2f258b54ee70df5729313fef" => :high_sierra
-    sha256 "63366bc4edfc655d5684b90b7de3eea48942ba19f96dd38b953a72685d1cb2ef" => :sierra
-    sha256 "938763b483907313d93b37f7dda6d8eb332c0d27ce94c14d62f9af3084a04ce6" => :el_capitan
-    sha256 "20ebde20c833412af76f424496f1a2eb0a29227069a3a240450f2caa7fe47c21" => :yosemite
+    sha256 "c367cba3bf371925b40c0aa36cd2413333fafebe06731bd5df3a006499a669a7" => :high_sierra
+    sha256 "982ea0e46c9845223ce145a2bc3d2e95a4007a302b31757585a38c10aa81ce02" => :sierra
+    sha256 "2fa4c44a3687cb2febec2b78ef6a76fa2589f75de9df564b339a2f7deaa05151" => :el_capitan
   end
 
   devel do
