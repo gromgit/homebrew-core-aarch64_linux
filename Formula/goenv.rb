4adf184cb734222a6a6e99fdf1237c8df6fe008b
ilovezfs <ilovezfs@icloud.com>
2017-10-19 22:22:30 -0700

    goenv 1.6.0
    Closes #19660.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/goenv.rb
+++ Formula/goenv.rb
@@ -1,8 +1,8 @@
 class Goenv < Formula
   desc "Go version management"
   homepage "https://github.com/syndbg/goenv"
-  url "https://github.com/syndbg/goenv/archive/1.5.0.tar.gz"
-  sha256 "1a847972a8fb7c622372b3d9ecd772fa32a9fac95886c3cafbe90a2102010adb"
+  url "https://github.com/syndbg/goenv/archive/1.6.0.tar.gz"
+  sha256 "fcaecbd6f99fb0cb40cbef9ae276fc77b9dc8f6873b1f8840f8b75bf94bda21d"
   version_scheme 1
   head "https://github.com/syndbg/goenv.git"
 
