6eca9e66c55ed2b948901ab209cbf0cee7f03b54
JCount <JCount42@gmail.com>
2017-10-20 15:51:57 -0400

    check 0.12.0
    Closes #19677.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/check.rb
+++ Formula/check.rb
@@ -1,8 +1,8 @@
 class Check < Formula
   desc "C unit testing framework"
   homepage "https://libcheck.github.io/check/"
-  url "https://github.com/libcheck/check/releases/download/0.11.0/check-0.11.0.tar.gz"
-  sha256 "24f7a48aae6b74755bcbe964ce8bc7240f6ced2141f8d9cf480bc3b3de0d5616"
+  url "https://github.com/libcheck/check/releases/download/0.12.0/check-0.12.0.tar.gz"
+  sha256 "464201098bee00e90f5c4bdfa94a5d3ead8d641f9025b560a27755a83b824234"
 
   bottle do
     cellar :any
