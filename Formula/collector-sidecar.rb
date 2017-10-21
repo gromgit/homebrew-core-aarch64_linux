2463cad93b121e4ba1fe1c7569fcefdb9128c5c2
jabenninghoff <jbenninghoff@mac.com>
2017-10-21 15:25:55 -0500

    collector-sidecar: add service plist
    Closes #19708.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/collector-sidecar.rb
+++ Formula/collector-sidecar.rb
@@ -38,6 +38,25 @@ class CollectorSidecar < Formula
     end
   end
 
+  plist_options :manual => "graylog-collector-sidecar"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN"
+    "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>Program</key>
+        <string>#{opt_bin}/graylog-collector-sidecar</string>
+        <key>RunAtLoad</key>
+        <true/>
+      </dict>
+    </plist>
+  EOS
+  end
+
   test do
     assert_match version.to_s, shell_output("#{bin}/graylog-collector-sidecar -version")
   end
