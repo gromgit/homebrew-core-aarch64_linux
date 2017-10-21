6333b3d716996a8e43cb66d2e789904d8d4abbc0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-21 18:10:37 +0000

    flake8: update 3.5.0 bottle.

--- Formula/flake8.rb
+++ Formula/flake8.rb
@@ -10,10 +10,9 @@ class Flake8 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5183704dc1be1eb7b69a799332701b3537dcd344cc1da5efb1a8c6d10c4f1d23" => :high_sierra
-    sha256 "52247f453420402adf92473646822df170eba8988869408536758a9dca3cf02a" => :sierra
-    sha256 "7cedbefbd6364386eaed992f81cfeb1dfca08997bda82e59d726a247d6fd6dea" => :el_capitan
-    sha256 "867901db2671873cfdbaec243d6a7e13f8fcc9e61b3b7f6353b7d9142c52c874" => :yosemite
+    sha256 "929140b14958e23321f395143c3ef31166f344058fdbaac843deca1a94b48782" => :high_sierra
+    sha256 "825dd5873edf54a9acbf35daa908559b4323f27e6800401d097217d9db28128c" => :sierra
+    sha256 "cfbc382496c31b5c57e31ac2487d022a07673d9efd2d64cbc956d3e05c8d9afe" => :el_capitan
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
