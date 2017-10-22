1436f5f9ee6728662ef377cc00377f0fa93bd00e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-22 15:37:02 +0000

    ripgrep: update 0.7.1 bottle.

--- Formula/ripgrep.rb
+++ Formula/ripgrep.rb
@@ -6,9 +6,9 @@ class Ripgrep < Formula
   head "https://github.com/BurntSushi/ripgrep.git"
 
   bottle do
-    sha256 "c40c41348fcea9cdc695730ac316960a0640505bd8d38a86397851e3f9309337" => :high_sierra
-    sha256 "5a0d45fe2bce48002f65b867c973a85cd54e83df2f942da64037aa71157a9e7b" => :sierra
-    sha256 "e7ca5276c76c581d2e95a1d6a0d8408abfe552f5352a9888602cc994a16afed4" => :el_capitan
+    sha256 "141f4a372665a161550835842cf967511cbad95487913fd28bb3fd87868c3755" => :high_sierra
+    sha256 "5d41c5d4322357b2d39e00d24c37f4dec08fb3b4f2cd7d5dfd944627871646d4" => :sierra
+    sha256 "8e183fb5763d350ae070756d3ff07e852ec624f6294d6657c067165328f7c6a5" => :el_capitan
   end
 
   depends_on "rust" => :build
