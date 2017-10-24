dead3ecdd134c1a609f3a6ceba84baebced120ae
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-24 13:37:24 +0000

    terraform-docs: update 0.3.0 bottle.

--- Formula/terraform-docs.rb
+++ Formula/terraform-docs.rb
@@ -9,9 +9,10 @@ class TerraformDocs < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c329dc0059e59ea6d5618143bb29b0fc069d934c3e8e7450dadf01e8806edbc8" => :high_sierra
-    sha256 "552bf2ee7d262f23b2b144cf7651f7f6e979996f33f1131a8a758b645791af09" => :sierra
-    sha256 "7cbf2efbe0140f32d4fd2a0fe7e324e365222c2be0c28ab479784b1d72c48e6c" => :el_capitan
+    rebuild 1
+    sha256 "d41872dfb6e58de57a05f2dafdd8254d5f24c318f7fa992026bf5f95af278a4f" => :high_sierra
+    sha256 "96e74e0e07f05e3bc416704e82def271f69f0707936191fec49e582fe4922fa0" => :sierra
+    sha256 "507d797efac42fd0ea8785364eda9eb9da2ddae656abca0766b49a90ff11699d" => :el_capitan
   end
 
   depends_on "go" => :build
