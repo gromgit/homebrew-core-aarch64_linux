0a32f798a1c944bc52b53a3fb36fb04cca99e386
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-24 12:15:56 +0000

    sdl2_image: update 2.0.2 bottle.

--- Formula/sdl2_image.rb
+++ Formula/sdl2_image.rb
@@ -6,10 +6,9 @@ class Sdl2Image < Formula
 
   bottle do
     cellar :any
-    sha256 "e5b0de5152954fe2a09b426a79a58ef2728b96c51be12bd809a1e1c6ac839011" => :high_sierra
-    sha256 "5d6a399de125a3de57dba84852063f08cba68cf07dc925c4ee671988f04f8a37" => :sierra
-    sha256 "464d0a95b54e9a0221f8fb972dde4238ae94bf30289c12a239252a2fbcde006d" => :el_capitan
-    sha256 "1411e639d93dab99c8e25c54f7e1262bfbb36df145f3664cf3af33c470336d65" => :yosemite
+    sha256 "bf5ff1c94ebde257e9d397fcab3d2ba42e870c771ff948ca54c25146f7da2f07" => :high_sierra
+    sha256 "a9cda2a4abd47e4084212fdbb90c601dce4a1e599c0dcaad95b0fe139a5dad1a" => :sierra
+    sha256 "cbe47c5ce034bea17df0ed477b143e6695ac81dda3c03de71252373ff0b1703f" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
