1d1f4b48e2b1b519f48e8217ddf32d838095735e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-24 16:09:18 +0000

    massren: add 1.5.4 bottle.

--- Formula/massren.rb
+++ Formula/massren.rb
@@ -4,6 +4,13 @@ class Massren < Formula
   url "https://github.com/laurent22/massren/archive/v1.5.4.tar.gz"
   sha256 "7a728d96a9e627c3609d147db64bba60ced33c407c75e9512147a5c83ba94f56"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "99afbeedc3d8ab1e3cf8ca525ac22f1b02efefbfd75b145b342f773cea639be6" => :high_sierra
+    sha256 "14874a768ef7f34aa638cdbd62aa32d2b07fc5c0e6668c86f6f080f172f0fe45" => :sierra
+    sha256 "ea67caccb6dacdbed8979f3dc243e224ff1900928dedf1ea8800f5256f3456b2" => :el_capitan
+  end
+
   depends_on "go" => :build
 
   def install
