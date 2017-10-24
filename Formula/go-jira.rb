cf09a0ed48103300270306832d7002044e6e1e57
fentie <brian.fenton@wheniwork.com>
2017-10-23 22:31:23 -0500

    go-jira 1.0.12 (new formula)
    removing autogenerated comments (oops)
    using a more involved test than --version
    removing bottle stanza that will be automatically added by brew test bot
    using built-in brew functionality for test env isolation
    no need to join static array vs using a string
    adding missing install step
    
    Closes #19773.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

---
+++ Formula/go-jira.rb
@@ -0,0 +1,32 @@
+class GoJira < Formula
+  desc "Simple jira command-line client in Go"
+  homepage "https://github.com/Netflix-Skunkworks/go-jira"
+  url "https://github.com/Netflix-Skunkworks/go-jira/archive/v1.0.12.tar.gz"
+  sha256 "7ce5d6dcc5021f2019b0107c4ad342979d18bb110188b810017b2d94ff384a33"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    dir = buildpath/"src/gopkg.in/Netflix-Skunkworks/go-jira.v1"
+    dir.install buildpath.children
+
+    cd dir do
+      system "go", "build", "-o", bin/"jira", "-ldflags", "-w -s", "cmd/jira/main.go"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    system "#{bin}/jira", "export-templates"
+    template_dir = testpath/".jira.d/templates/"
+
+    files = Dir.entries(template_dir)
+    # not an exhaustive list, see https://github.com/Netflix-Skunkworks/go-jira/blob/4d74554300fa7e5e660cc935a92e89f8b71012ea/jiracli/templates.go#L239
+    expected_templates = %w[comment components create edit issuetypes list view worklog debug]
+
+    assert_equal([], expected_templates - files)
+    debug = open(template_dir + "debug")
+    assert_equal("{{ . | toJson}}\n", debug.read)
+  end
+end
