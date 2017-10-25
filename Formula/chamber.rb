c1c57eb3b2dfaf1f2f1ca5f3ab38d0d2e2c531dd
Robin Joseph <robin@lob.com>
2017-10-25 14:03:57 -0700

    chamber 1.9.0 (new formula)
    Closes #20318.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/chamber.rb
@@ -0,0 +1,36 @@
+class Chamber < Formula
+  desc "CLI for managing secrets through AWS SSM Parameter Store"
+  homepage "https://github.com/segmentio/chamber"
+  url "https://github.com/segmentio/chamber/archive/v1.9.0.tar.gz"
+  sha256 "098568ddda04956b8dc270aa8e6b4eb83d8cfe92add8ea0172715eab7db050cb"
+  head "https://github.com/segmentio/chamber.git"
+
+  depends_on "go" => :build
+  depends_on "govendor" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GOOS"] = "darwin"
+    ENV["GOARCH"] = "amd64"
+    ENV["CGO_ENABLED"] = "0"
+
+    path = buildpath/"src/github.com/segmentio/chamber"
+    path.install Dir["{*,.git}"]
+
+    cd buildpath/"src/github.com/segmentio/chamber" do
+      system "govendor", "sync"
+      system "go", "build", "-o", bin/"chamber"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    ENV.delete "AWS_REGION"
+    output = shell_output("#{bin}/chamber list service 2>&1", 1)
+    assert_match "MissingRegion", output
+
+    ENV["AWS_REGION"] = "us-west-2"
+    output = shell_output("#{bin}/chamber list service 2>&1", 1)
+    assert_match "NoCredentialProviders", output
+  end
+end
