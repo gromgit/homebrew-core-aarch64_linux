3a23d432e771ceaecf22c3b8aab3449acb4aa0c6
Matt Blair <me@matthewblair.net>
2017-10-25 16:53:25 -0700

    go 1.9.2

--- Formula/go.rb
+++ Formula/go.rb
@@ -3,9 +3,9 @@ class Go < Formula
   homepage "https://golang.org"
 
   stable do
-    url "https://storage.googleapis.com/golang/go1.9.1.src.tar.gz"
-    mirror "https://fossies.org/linux/misc/go1.9.1.src.tar.gz"
-    sha256 "a84afc9dc7d64fe0fa84d4d735e2ece23831a22117b50dafc75c1484f1cb550e"
+    url "https://storage.googleapis.com/golang/go1.9.2.src.tar.gz"
+    mirror "https://fossies.org/linux/misc/go1.9.2.src.tar.gz"
+    sha256 "665f184bf8ac89986cfd5a4460736976f60b57df6b320ad71ad4cef53bb143dc"
 
     go_version = version.to_s.split(".")[0..1].join(".")
     resource "gotools" do
