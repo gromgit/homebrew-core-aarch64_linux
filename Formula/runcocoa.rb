5c5af0f9d44919ba3da696842a4515487043579c
Josh Parnham <joshparnham@gmail.com>
2017-10-27 01:24:32 +1100

    runcocoa: add a test (#19794)

--- Formula/runcocoa.rb
+++ Formula/runcocoa.rb
@@ -11,4 +11,16 @@ class Runcocoa < Formula
     bin.install "runcocoa.sh" => "runcocoa"
     bin.install "runc.sh" => "runc"
   end
+
+  test do
+    string = "Hello world!"
+
+    objc_code = "[[NSFileHandle fileHandleWithStandardOutput] writeData:[@\"#{string}\" dataUsingEncoding:NSNEXTSTEPStringEncoding]]"
+    objc_output = pipe_output("#{bin}/runcocoa", objc_code, 0)
+    assert_match string, objc_output
+
+    c_code = "printf(\"#{string}\");"
+    c_output = pipe_output("#{bin}/runc", c_code, 0)
+    assert_match string, c_output
+  end
 end
