4c23c22dc71eadaeb7b25d6e6c10fd53bfc26976
Brice Dutheil <brice.dutheil@gmail.com>
2017-10-26 09:07:07 +0200

    maven 3.5.2 (#19832)
    make the test Java 9 compatible

--- Formula/maven.rb
+++ Formula/maven.rb
@@ -3,9 +3,9 @@ class Maven < Formula
   homepage "https://maven.apache.org/"
 
   stable do
-    url "https://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz"
-    mirror "https://archive.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz"
-    sha256 "beb91419245395bd69a4a6edad5ca3ec1a8b64e41457672dc687c173a495f034"
+    url "https://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz"
+    mirror "https://archive.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz"
+    sha256 "707b1f6e390a65bde4af4cdaf2a24d45fc19a6ded00fff02e91626e3e42ceaff"
   end
 
   bottle :unneeded
@@ -42,6 +42,11 @@ class Maven < Formula
         <groupId>org.homebrew</groupId>
         <artifactId>maven-test</artifactId>
         <version>1.0.0-SNAPSHOT</version>
+        <properties>
+          <maven.compiler.source>1.8</maven.compiler.source>
+          <maven.compiler.target>1.8</maven.compiler.target>
+          <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+        </properties>
       </project>
     EOS
     (testpath/"src/main/java/org/homebrew/MavenTest.java").write <<~EOS
