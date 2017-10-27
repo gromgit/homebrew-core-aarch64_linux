163091ff3bc5965e9d18f2df661e0a1dd0bd1427
ilovezfs <ilovezfs@icloud.com>
2017-10-27 03:53:45 -0700

    icu4c: apply patch for CVE-2017-14952
    Closes #19926.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/icu4c.rb
+++ Formula/icu4c.rb
@@ -1,12 +1,24 @@
 class Icu4c < Formula
   desc "C/C++ and Java libraries for Unicode and globalization"
   homepage "http://site.icu-project.org/"
-  url "https://ssl.icu-project.org/files/icu4c/59.1/icu4c-59_1-src.tgz"
-  mirror "https://fossies.org/linux/misc/icu4c-59_1-src.tgz"
-  mirror "https://downloads.sourceforge.net/project/icu/ICU4C/59.1/icu4c-59_1-src.tgz"
-  version "59.1"
-  sha256 "7132fdaf9379429d004005217f10e00b7d2319d0fea22bdfddef8991c45b75fe"
+  revision 1
   head "https://ssl.icu-project.org/repos/icu/trunk/icu4c/", :using => :svn
+
+  stable do
+    url "https://ssl.icu-project.org/files/icu4c/59.1/icu4c-59_1-src.tgz"
+    mirror "https://fossies.org/linux/misc/icu4c-59_1-src.tgz"
+    mirror "https://downloads.sourceforge.net/project/icu/ICU4C/59.1/icu4c-59_1-src.tgz"
+    version "59.1"
+    sha256 "7132fdaf9379429d004005217f10e00b7d2319d0fea22bdfddef8991c45b75fe"
+
+    # Fix CVE-2017-14952
+    # Upstream commit from 9 Aug 2017 "Removed redundant UVector entry clean up
+    # function call."
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/fb441ea/icu4c/CVE-2017-14952.diff"
+      sha256 "1da1eec19cfe4907eb4766192ddbca689506ce44cfeb35c349af9609ae7f7203"
+    end
+  end
 
   bottle do
     cellar :any
