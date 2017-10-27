345dd111ae0ec89aa2196b0631cb66479a3aee4e
Mike McQuaid <mike@mikemcquaid.com>
2017-10-27 09:33:04 +0100

    radare2: fix rubocop warning.

--- Formula/radare2.rb
+++ Formula/radare2.rb
@@ -1,10 +1,9 @@
 class CodesignRequirement < Requirement
-  include FileUtils
   fatal true
 
   satisfy(:build_env => false) do
-    mktemp do
-      cp "/usr/bin/false", "radare2_check"
+    FileUtils.mktemp do
+      FileUtils.cp "/usr/bin/false", "radare2_check"
       quiet_system "/usr/bin/codesign", "-f", "-s", "org.radare.radare2", "--dryrun", "radare2_check"
     end
   end
