cc0ffad13575c90827950ecc93ebabd1f97c3d52
ilovezfs <ilovezfs@icloud.com>
2017-10-27 01:37:28 -0700

    sysbench 1.0.10
    Closes #19920.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/sysbench.rb
+++ Formula/sysbench.rb
@@ -1,8 +1,8 @@
 class Sysbench < Formula
   desc "System performance benchmark tool"
   homepage "https://github.com/akopytov/sysbench"
-  url "https://github.com/akopytov/sysbench/archive/1.0.9.tar.gz"
-  sha256 "3e472d5b175dcc5a8b0ebdf102dbe64bdcc712f6c41812849490e739bf8ce483"
+  url "https://github.com/akopytov/sysbench/archive/1.0.10.tar.gz"
+  sha256 "34cfe9989a4610c15359a2d88b59a09f5c18846f42ce49175953c3e600deebbe"
 
   bottle do
     sha256 "e0dc745e770984aa268b9fa6f0b893dce9570d7ec5be3f2a271874fd55e2cd45" => :high_sierra
