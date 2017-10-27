7c1436e722d7ae4008325ab10f31f8b4da3ca343
ilovezfs <ilovezfs@icloud.com>
2017-10-27 10:34:16 -0700

    phoronix-test-suite: set bottle unneeded

--- Formula/phoronix-test-suite.rb
+++ Formula/phoronix-test-suite.rb
@@ -4,13 +4,7 @@ class PhoronixTestSuite < Formula
   url "https://github.com/phoronix-test-suite/phoronix-test-suite/archive/v7.4.0.tar.gz"
   sha256 "beb0875ca74b62a5bb0768c337baceb3415817a6ef487dd14289ab2d84b6c504"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "9ffc35ffe7794b546806b5554a228296f495a398239636cf32a2eeace9c48eee" => :high_sierra
-    sha256 "a0342ec359f6be0bbfec1918f6e58684d66ef7fb05ed35e0f25bad527475fd30" => :sierra
-    sha256 "152a7ddddb515433d728f7d969c895da9b4c464518550d0ea6ba20755ea8f8c3" => :el_capitan
-    sha256 "152a7ddddb515433d728f7d969c895da9b4c464518550d0ea6ba20755ea8f8c3" => :yosemite
-  end
+  bottle :unneeded
 
   def install
     system "./install-sh", prefix
