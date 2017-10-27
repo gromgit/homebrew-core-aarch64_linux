7e0489e3c4281e4ed0fdb4c9ae0aa81f2838d79d
ilovezfs <ilovezfs@icloud.com>
2017-10-27 08:51:51 -0700

    lynis 2.5.6
    Closes #19945.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/lynis.rb
+++ Formula/lynis.rb
@@ -1,8 +1,8 @@
 class Lynis < Formula
   desc "Security and system auditing tool to harden systems"
   homepage "https://cisofy.com/lynis/"
-  url "https://cisofy.com/files/lynis-2.5.5.tar.gz"
-  sha256 "638c587396fbd2e857d6a3d2229db3b071704c0e217e03055c9268b495ab8102"
+  url "https://cisofy.com/files/lynis-2.5.6.tar.gz"
+  sha256 "3eb0a3b43a90df850a2d6b8f0e3c633ae8995181e44f7e9ced5dff29c5f74c40"
 
   bottle do
     cellar :any_skip_relocation
