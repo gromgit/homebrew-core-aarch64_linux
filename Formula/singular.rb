4b0c3c67171a2ca15de420981b91ce9d9ec0fd02
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-28 13:48:48 +0000

    singular: add 4.1.0p3 bottle.

--- Formula/singular.rb
+++ Formula/singular.rb
@@ -5,6 +5,12 @@ class Singular < Formula
   version "4.1.0p3"
   sha256 "440164c850d5a1575fcbfe95ab884088d03c0449570d40f465611932ffd0bf80"
 
+  bottle do
+    sha256 "293d84d3bac5d54eea0c541a7f23788332db2cc9bd7df87b3ef01755b9ed6be6" => :high_sierra
+    sha256 "3d87470f1711bd3e6335dc72b5f0b512686bbebe9374360a5f52f421eef315be" => :sierra
+    sha256 "b65566d5a29789f3601fc9074f4436ecc3b51428de7c4a4b7a118c6370b82d5b" => :el_capitan
+  end
+
   head do
     url "https://github.com/Singular/Sources.git"
 
