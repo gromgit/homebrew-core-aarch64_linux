b4933b0c9c188b1d101cac8ce4d82855f5aacdc8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-28 20:37:36 +0000

    libmxml: update 2.11 bottle.

--- Formula/libmxml.rb
+++ Formula/libmxml.rb
@@ -8,10 +8,9 @@ class Libmxml < Formula
 
   bottle do
     cellar :any
-    sha256 "ce2ace2a91e3d948911a6c44c378b092d8dc807aff0a328a705822ca5c391b41" => :high_sierra
-    sha256 "4287dd3c4346f82d9a410bf4bdce69329ef1f931e7639e6c27fa8f79e1e31cb4" => :sierra
-    sha256 "01d01bf5766e398837a416949f74eb405e57903dbc27a55fc589e0559e60c518" => :el_capitan
-    sha256 "bb2a6d25c76e7e65eb5a14728763a03f41cb7c5f68bb43cd73da4f92df0226cc" => :yosemite
+    sha256 "3ab68ae639d1b5f78b756d689c66303a0e1f2d2bb34a417ce374d01fb6a5b176" => :high_sierra
+    sha256 "03b417fb39a0293c2dad5fe18ddf36e7692e93cd35338d32013394cc6f1d34a7" => :sierra
+    sha256 "f33aab3398c00853fad6045acef1184c16fa6fa6bb5525ccaef45cd74460ee41" => :el_capitan
   end
 
   depends_on :xcode => :build # for docsetutil
