214277c87e21041acf0eedfe2957db05ddd13c12
s172262 <s172262@users.noreply.github.com>
2017-10-29 14:04:58 +0900

    mesalib-glw 8.0.0
    Closes #19996.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mesalib-glw.rb
+++ Formula/mesalib-glw.rb
@@ -1,8 +1,8 @@
 class MesalibGlw < Formula
   desc "Open-source implementation of the OpenGL specification"
   homepage "https://www.mesa3d.org"
-  url "https://downloads.sourceforge.net/project/mesa3d/MesaLib/7.2/MesaLib-7.2.tar.gz"
-  sha256 "a7b7cc8201006685184e7348c47cb76aecf71be81475c71c35e3f5fe9de909c6"
+  url "https://mesa.freedesktop.org/archive/glw/glw-8.0.0.tar.bz2"
+  sha256 "2da1d06e825f073dcbad264aec7b45c649100e5bcde688ac3035b34c8dbc8597"
 
   bottle do
     cellar :any
@@ -12,29 +12,14 @@ class MesalibGlw < Formula
     sha256 "d700939e346f00e21a71e273895ff61d6984924d85cf41de461fb6674e326f8e" => :yosemite
   end
 
+  depends_on "pkg-config" => :build
   depends_on :x11
 
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--with-driver=xlib",
-                          "--disable-gl-osmesa",
-                          "--disable-glu",
-                          "--disable-glut"
+                          "--prefix=#{prefix}"
 
-    inreplace "configs/autoconf" do |s|
-      s.gsub! /.so/, ".dylib"
-      s.gsub! /SRC_DIRS = mesa glw/, "SRC_DIRS = glw"
-      s.gsub! %r{-L\$\(TOP\)/\$\(LIB_DIR\)}, "-L#{MacOS::X11.lib}"
-    end
-
-    inreplace "src/glw/Makefile", %r{-I\$\(TOP\)/include }, ""
-
-    system "make"
-
-    (include+"GL").mkpath
-    (include+"GL").install Dir["src/glw/*.h"]
-    lib.install Dir["lib/*"]
+    system "make", "install"
   end
 end
