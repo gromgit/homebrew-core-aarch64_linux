345f2198d68f848c776a252f74791e58b62b3b25
ilovezfs <ilovezfs@icloud.com>
2017-10-29 09:48:29 -0700

    boost-mpi: revision for open-mpi

--- Formula/boost-mpi.rb
+++ Formula/boost-mpi.rb
@@ -3,6 +3,7 @@ class BoostMpi < Formula
   homepage "https://www.boost.org/"
   url "https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.bz2"
   sha256 "9807a5d16566c57fd74fb522764e0b134a8bbe6b6e8967b83afefd30dcd3be81"
+  revision 1
   head "https://github.com/boostorg/boost.git"
 
   bottle do
