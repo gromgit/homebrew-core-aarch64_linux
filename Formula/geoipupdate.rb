bdd1dfd4a2cc0336b11d0ef187aab499f4934371
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-30 15:24:41 +0000

    geoipupdate: update 2.5.0 bottle.

--- Formula/geoipupdate.rb
+++ Formula/geoipupdate.rb
@@ -5,10 +5,9 @@ class Geoipupdate < Formula
   sha256 "5119fd0e338cd083e886228b26679c64bcbaade8a815be092aecf865a610ab26"
 
   bottle do
-    sha256 "323ae245cabc43f6dfc5dd95625eb4aa8fd87cf33dfc3c0ad75f043f48ffff88" => :high_sierra
-    sha256 "ecc561b023e6ae9c57c4dcbe248407bfbf8b6f73e28da9e9a0395759b1fdb931" => :sierra
-    sha256 "5a56005484abf3771b79707fc79a669803e683f1573c12a854c23f4b92d78ee0" => :el_capitan
-    sha256 "e984990bc05d7f13585e6b19f309dbd56c181f15f09faa08333ba8020fb1876a" => :yosemite
+    sha256 "22feb6fb330b62c077482b443ac637c2fa3f60626b4113d815fc0fd85a255ee2" => :high_sierra
+    sha256 "d28503bce47a42732d54cece163675f6c6169539327a95bb8d4bbdfd16aa9e35" => :sierra
+    sha256 "ab7981329380d9bf0fee2efe0bfed2a1745cc3ecf681356ba21a02cd6604acf2" => :el_capitan
   end
 
   head do
