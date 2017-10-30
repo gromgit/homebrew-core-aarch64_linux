f794259a474c55fb4818eba8880697fee47f1f8e
ilovezfs <ilovezfs@icloud.com>
2017-10-29 23:01:11 -0700

    sslh: revision for libconfig

--- Formula/sslh.rb
+++ Formula/sslh.rb
@@ -3,6 +3,7 @@ class Sslh < Formula
   homepage "https://www.rutschle.net/tech/sslh.shtml"
   url "https://www.rutschle.net/tech/sslh/sslh-v1.18.tar.gz"
   sha256 "1601a5b377dcafc6b47d2fbb8d4d25cceb83053a4adcc5874d501a2d5a7745ad"
+  revision 1
   head "https://github.com/yrutschle/sslh.git"
 
   bottle do
