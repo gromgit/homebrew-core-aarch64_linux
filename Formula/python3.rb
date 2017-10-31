e76ed3606c8008d2b8d9636a7e4e6f62cfeb6120
Alex Bukin <abukin@users.noreply.github.com>
2017-10-31 09:08:26 +0000

    python3 3.7.0a2 (devel) (#20063)

--- Formula/python3.rb
+++ Formula/python3.rb
@@ -12,8 +12,8 @@ class Python3 < Formula
   end
 
   devel do
-    url "https://www.python.org/ftp/python/3.7.0/Python-3.7.0a1.tar.xz"
-    sha256 "aab053dc3c23b829c60e973ce5972c4e58ffb5e26210acc26611e0d766a06eea"
+    url "https://www.python.org/ftp/python/3.7.0/Python-3.7.0a2.tar.xz"
+    sha256 "3e5adaa8a264b0c8eeab7b8a0185acec053b0d1547d2712ebc915153c4a52f28"
   end
 
   option "with-tcl-tk", "Use Homebrew's Tk instead of macOS Tk (has optional Cocoa and threads support)"
