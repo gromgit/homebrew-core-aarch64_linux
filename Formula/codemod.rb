11b0185f7e187cdfd4080309297175ccfe48df75
Ori Peleg <orip@users.noreply.github.com>
2017-10-31 21:16:09 +0200

    codemod 1.0.0 (#20099)
    - fixed installation for 1.0.0 and HEAD
    - added meaningful test

--- Formula/codemod.rb
+++ Formula/codemod.rb
@@ -1,8 +1,9 @@
 class Codemod < Formula
   desc "Large-scale codebase refactors assistant tool"
   homepage "https://github.com/facebook/codemod"
-  url "https://github.com/facebook/codemod/archive/20151117.tar.gz"
-  sha256 "db1df8896baa58d745aca474b1c8f1a420a40a4a56f30ea783f57b6f5157351c"
+  url "https://files.pythonhosted.org/packages/9b/e3/cb31bfcf14f976060ea7b7f34135ebc796cde65eba923f6a0c4b71f15cc2/codemod-1.0.0.tar.gz"
+  sha256 "06e8c75f2b45210dd8270e30a6a88ae464b39abd6d0cab58a3d7bfd1c094e588"
+  version_scheme 1
 
   head "https://github.com/facebook/codemod.git"
 
@@ -12,13 +13,27 @@ class Codemod < Formula
 
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
-    (libexec/"lib/python2.7/site-packages").mkpath
     system "python", *Language::Python.setup_install_args(libexec)
-    bin.install libexec/"bin/codemod.py" => "codemod"
+    bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
   test do
-    system "#{bin}/codemod", "--test"
+    # work around some codemod terminal bugs
+    ENV["TERM"] = "xterm"
+    ENV["LINES"] = "25"
+    ENV["COLUMNS"] = "80"
+    (testpath/"file1").write <<~EOS
+      foo
+      bar
+      potato
+      baz
+    EOS
+    (testpath/"file2").write <<~EOS
+      eeny potato meeny miny moe
+    EOS
+    system bin/"codemod", "--include-extensionless", "--accept-all", "potato", "pineapple"
+    assert_equal %w[foo bar pineapple baz], File.read("file1").lines.map(&:strip)
+    assert_equal ["eeny pineapple meeny miny moe"], File.read("file2").lines.map(&:strip)
   end
 end
