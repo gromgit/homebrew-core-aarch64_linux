ed7074182e99b4f3f07eb5dfa02292e6246317bb
ilovezfs <ilovezfs@icloud.com>
2017-10-31 13:48:24 -0700

    tfenv 0.6.0
    Closes #20118.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/tfenv.rb
+++ Formula/tfenv.rb
@@ -1,8 +1,8 @@
 class Tfenv < Formula
   desc "Terraform version manager inspired by rbenv"
   homepage "https://github.com/kamatama41/tfenv"
-  url "https://github.com/kamatama41/tfenv/archive/v0.5.2.tar.gz"
-  sha256 "771679a32b221d3258e31d839bceffe56ff65567628a47e5831da9ca9f62d73b"
+  url "https://github.com/kamatama41/tfenv/archive/v0.6.0.tar.gz"
+  sha256 "f2ca32ade0e90d6988106e85efaa548ed116cac1ded24f88dd19f479c6381e2e"
   head "https://github.com/kamatama41/tfenv.git"
 
   bottle :unneeded
