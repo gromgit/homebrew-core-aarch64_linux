c170898285ce44453b4ac9ff88151c0330950dd1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-31 13:01:01 +0000

    htslib: add 1.6_1 bottle.

--- Formula/htslib.rb
+++ Formula/htslib.rb
@@ -5,6 +5,13 @@ class Htslib < Formula
   sha256 "9588be8be0c2390a87b7952d644e7a88bead2991b3468371347965f2e0504ccb"
   revision 1
 
+  bottle do
+    cellar :any
+    sha256 "15c9481d899d77e7d7eeb5c2d2cac09930fef9b28409ec9ea3ab57ec9cc70e87" => :high_sierra
+    sha256 "35a9e6c69ed762ee9d5ba2966a484f3a13557dbf23ef897fee6ba7d6f9db5659" => :sierra
+    sha256 "5fe4d1a962dd98c9c0497fa88da33f493fdec868a25e531def7c5506424571d0" => :el_capitan
+  end
+
   depends_on "xz"
 
   def install
