153320dc5fb8a06bde47637853981f02e27aab84
Laurent Cozic <laurent@cozic.net>
2017-11-01 21:40:32 +0000

    joplin 0.10.65 (new formula)
    Closes #20153.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/joplin.rb
@@ -0,0 +1,20 @@
+require "language/node"
+
+class Joplin < Formula
+  desc "Note taking and to-do application with synchronisation capabilities"
+  homepage "http://joplin.cozic.net/"
+  url "https://registry.npmjs.org/joplin/-/joplin-0.10.65.tgz"
+  sha256 "52dcd067e670ede25a74d06a10223a9d7e134e56606219621a21dc3915127f8c"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system bin/"joplin", "config", "editor", "subl"
+    assert_match "editor = subl", shell_output("#{bin}/joplin config")
+  end
+end
