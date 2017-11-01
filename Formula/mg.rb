3174ba9b80578171548af8d1a3bdb2a523309741
ilovezfs <ilovezfs@icloud.com>
2017-11-01 08:20:34 -0700

    mg 20170917
    Closes #20144.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mg.rb
+++ Formula/mg.rb
@@ -1,8 +1,8 @@
 class Mg < Formula
   desc "Small Emacs-like editor"
   homepage "https://devio.us/~bcallah/mg/"
-  url "https://devio.us/~bcallah/mg/mg-20161016.tar.gz"
-  sha256 "bcb4be59aaa30ae8dd0e9aed3c0a5ff8bf2dae6e6768396d37c11aaaab29d370"
+  url "https://devio.us/~bcallah/mg/mg-20170917.tar.gz"
+  sha256 "def9237a89ec6a14241abaf12714bc5fcb3b0e2f8d9d466ff7561628d35b7ff1"
 
   bottle do
     cellar :any_skip_relocation
