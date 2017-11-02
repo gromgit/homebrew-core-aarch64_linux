d005fb006e2e63a8704d7f26eb5e77ce3c608e4d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-02 11:15:46 +0000

    dcos-cli: update 0.5.6 bottle.

--- Formula/dcos-cli.rb
+++ Formula/dcos-cli.rb
@@ -9,10 +9,9 @@ class DcosCli < Formula
 
   bottle do
     cellar :any
-    sha256 "f3ccf45fda58c010f0593bc86dab48c8329e82108b5df5b4daae486a85086cd8" => :high_sierra
-    sha256 "b11cf4ac5eeaca6c5af674ec3d17774b43cc87877bfc646c4ccb9dc708a4c258" => :sierra
-    sha256 "29ef7b9e5dcd71c1b33920e450f406a459f37209a5a64a7f773ec9688c97c2f0" => :el_capitan
-    sha256 "33ebd1213a72d4d3d1a0134e8a94559b88caa9101fea6d965853394e2c9b126b" => :yosemite
+    sha256 "fe40dc5f529b7214b9db75b45d4a66ee92c0c76975bfd42e77824a144fc7f8d8" => :high_sierra
+    sha256 "bd5f0806237bafa5220d48b8d0adb9579570f7c905c03fc6851b5fda887285e3" => :sierra
+    sha256 "7032a4e70181ccd058d537ff3e95827da867599b4d3068568f1183b9cba5debc" => :el_capitan
   end
 
   depends_on :python3
