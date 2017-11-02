ec56120dab55c1f496ab206dfc59772ada0b4736
ilovezfs <ilovezfs@icloud.com>
2017-11-02 06:42:19 -0700

    mkvalidator 0.5.2
    Closes #20178.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mkvalidator.rb
+++ Formula/mkvalidator.rb
@@ -1,8 +1,8 @@
 class Mkvalidator < Formula
   desc "Tool to verify Matroska and WebM files for spec conformance"
   homepage "https://www.matroska.org/downloads/mkvalidator.html"
-  url "https://downloads.sourceforge.net/project/matroska/mkvalidator/mkvalidator-0.5.0.tar.bz2"
-  sha256 "c3e72e5b49d32174415b9273ea8d52380e09ac63c8dc7db684104021c711c104"
+  url "https://downloads.sourceforge.net/project/matroska/mkvalidator/mkvalidator-0.5.2.tar.bz2"
+  sha256 "2e2a91062f6bf6034e8049646897095b5fc7a1639787d5fe0fcef1f1215d873b"
 
   bottle do
     cellar :any_skip_relocation
@@ -14,15 +14,6 @@ class Mkvalidator < Formula
     sha256 "8a6c2abe6c63609e04f4855f25b336418d6ae9f10f95061c40efd811372afb0f" => :mavericks
   end
 
-  # see https://sourceforge.net/p/matroska/bugs/9/
-  # and https://sourceforge.net/p/matroska/patches/3/
-  if MacOS.prefer_64_bit?
-    patch do
-      url "https://sourceforge.net/p/matroska/patches/_discuss/thread/8899370c/81cc/attachment/mkvalidator-0.3.7.gcc_osx_x64.build.diff"
-      sha256 "c714977d5b68e90851ce59aacc789438b2455741eba43bd473a248a59932ce3c"
-    end
-  end
-
   resource "tests" do
     url "https://github.com/dunn/garbage/raw/c0e682836e5237eef42a000e7d00dcd4b6dcebdb/test.mka"
     sha256 "6d7cc62177ec3f88c908614ad54b86dde469dbd2b348761f6512d6fc655ec90c"
@@ -30,6 +21,10 @@ class Mkvalidator < Formula
 
   def install
     ENV.deparallelize # Otherwise there are races
+
+    # Reported 2 Nov 2017 https://github.com/Matroska-Org/foundation-source/issues/31
+    inreplace "configure", "\r", "\n"
+
     system "./configure"
     system "make", "-C", "mkvalidator"
     bindir = `corec/tools/coremake/system_output.sh`.chomp
