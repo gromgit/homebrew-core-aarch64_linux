9e498219955372ee2b9374c6036d05a61f25630c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-02 16:30:30 +0000

    openssl@1.1: update 1.1.0g bottle.

--- Formula/openssl@1.1.rb
+++ Formula/openssl@1.1.rb
@@ -8,10 +8,9 @@ class OpensslAT11 < Formula
   version_scheme 1
 
   bottle do
-    sha256 "462d236b6ca0a613241f38b9959768cb56e25987c0478140727751c51daff612" => :high_sierra
-    sha256 "131d4094a2714686a12b74cc653a80a0d9dc1dccf881675bd936ab31bdb6e06b" => :sierra
-    sha256 "88937e60df7629f63786d10f8afa1742611eb70eb1365b287b548eab51322864" => :el_capitan
-    sha256 "da7a695cb6b0ab30cb8cc3cf3d95186e7d9964713d66b920717e54d2dad0feea" => :yosemite
+    sha256 "5e42b3d59d4c7be3164a8f820cbc78c9611e0766d4326a9d0ab7961d31019f7c" => :high_sierra
+    sha256 "28fbe28a1aa1e36d577d8fd38336ffc9ed5af80c77c263a50eff825f9c5c2926" => :sierra
+    sha256 "5e598ed7560b4925dd7ae88da705428059180931b766110c4b6f1a41e6add256" => :el_capitan
   end
 
   keg_only :versioned_formula
