c22a377e34930b9bde2e52cb525daeb81411d4cb
JCount <JCount42@gmail.com>
2017-11-02 01:07:31 -0400

    zmqpp 4.2.0
    Closes #20169.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/zmqpp.rb
+++ Formula/zmqpp.rb
@@ -1,8 +1,8 @@
 class Zmqpp < Formula
   desc "High-level C++ binding for zeromq"
   homepage "https://zeromq.github.io/zmqpp/"
-  url "https://github.com/zeromq/zmqpp/archive/4.1.2.tar.gz"
-  sha256 "831ad02df64034268d910c30f9fb1b1e631ad810182951af9d7d622650831eb5"
+  url "https://github.com/zeromq/zmqpp/archive/4.2.0.tar.gz"
+  sha256 "c1d4587df3562f73849d9e5f8c932ca7dcfc7d8bec31f62d7f35073ef81f4d29"
 
   bottle do
     cellar :any
@@ -14,14 +14,19 @@ class Zmqpp < Formula
     sha256 "2392c3090822256c05a35a2e09e99096512838f51704281a2a3b26af10739813" => :mountain_lion
   end
 
+  depends_on "doxygen" => :build
   depends_on "zeromq"
 
   needs :cxx11
 
   def install
     ENV.cxx11
-    system "make", "PREFIX=#{prefix}"
+
+    system "make"
     system "make", "install", "PREFIX=#{prefix}"
+
+    system "doxygen"
+    (doc/"html").install Dir["docs/html/*.html"]
   end
 
   test do
