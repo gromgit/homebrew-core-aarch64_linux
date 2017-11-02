319f319e592530a7fcfd63b12a9ffef88a7fe23b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-02 17:36:42 +0000

    go-jira: add 1.0.12 bottle.

--- Formula/go-jira.rb
+++ Formula/go-jira.rb
@@ -4,6 +4,13 @@ class GoJira < Formula
   url "https://github.com/Netflix-Skunkworks/go-jira/archive/v1.0.12.tar.gz"
   sha256 "7ce5d6dcc5021f2019b0107c4ad342979d18bb110188b810017b2d94ff384a33"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ef68c2bb7f94975de52779639170aa82a3ee5972af954a473f5e37a99afb4447" => :high_sierra
+    sha256 "ea8c595380faa34039a9d9d7220c5548a7af0c0cc8a9dede1b4f3a542a15b252" => :sierra
+    sha256 "bb058a932373310e0c7d5be1bc703520daae9aec7a7ebdff10dfbb9e03d057ac" => :el_capitan
+  end
+
   depends_on "go" => :build
 
   def install
