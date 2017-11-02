4378061dcb4138670cdebae9ccd9294df424e67c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-02 12:56:56 +0000

    terraform_landscape: add 0.1.16 bottle.

--- Formula/terraform_landscape.rb
+++ Formula/terraform_landscape.rb
@@ -4,6 +4,13 @@ class TerraformLandscape < Formula
   url "https://github.com/coinbase/terraform-landscape/archive/v0.1.16.tar.gz"
   sha256 "93556adb4901a076d5900e7a6205375813cb332b4a59373da55fb3e45361bfb7"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "20410aab3385835d49d5bf1784d4e1ba51051056b2f63b28883ec2ede3472ab3" => :high_sierra
+    sha256 "238ff6d1b376b99dacf276896ef77b4240e7d3fc8d9babb2b70fc14e5dad8d43" => :sierra
+    sha256 "a84eab0be50b30cbf6cfebe1a19644c74bc7fb06095fd93f64428d71e0ffb78e" => :el_capitan
+  end
+
   depends_on :ruby => "2.0"
 
   resource "colorize" do
