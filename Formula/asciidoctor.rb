08e46e979d62fc8638049823ccf10c18f86d8d5e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-03 12:55:59 +0000

    asciidoctor: add 1.5.6.1 bottle.

--- Formula/asciidoctor.rb
+++ Formula/asciidoctor.rb
@@ -4,6 +4,13 @@ class Asciidoctor < Formula
   url "https://github.com/asciidoctor/asciidoctor/archive/v1.5.6.1.tar.gz"
   sha256 "27e238f4cc48c19e1060ec8770a1c6eb55c3b837d9063aa99bc37e38d76b4a48"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3a4781de566088ef6f3bca77353934e795294380bd67c56a1f30bdd4a8430e4b" => :high_sierra
+    sha256 "b88ab76f368387aa393758941d556cfec66c0415c2cad3a1c896b059996c0aa5" => :sierra
+    sha256 "9b4f5f4268400fc71ad4b31c7cb05135f342ab5f31df99a155cf3cf749572f94" => :el_capitan
+  end
+
   def install
     ENV["GEM_HOME"] = libexec
     system "gem", "build", "asciidoctor.gemspec"
