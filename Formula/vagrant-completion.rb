4fc10c2abc8e97a3dcdd322b16cb06a8f4b96c4c
commitay <commitay@users.noreply.github.com>
2017-11-03 18:47:15 +1000

    vagrant-completion: use canonical repository URLs (#20220)
    Vagrant has moved to the Hashicorp GitHub organisation.

--- Formula/vagrant-completion.rb
+++ Formula/vagrant-completion.rb
@@ -1,10 +1,10 @@
 class VagrantCompletion < Formula
   desc "Bash completion for Vagrant"
-  homepage "https://github.com/mitchellh/vagrant"
-  url "https://github.com/mitchellh/vagrant/archive/v2.0.1.tar.gz"
+  homepage "https://github.com/hashicorp/vagrant"
+  url "https://github.com/hashicorp/vagrant/archive/v2.0.1.tar.gz"
   sha256 "212b91c45f60a825fcfc656424021e2550833778a6d4ebe13458676201a04eba"
 
-  head "https://github.com/mitchellh/vagrant.git"
+  head "https://github.com/hashicorp/vagrant.git"
 
   bottle :unneeded
 
