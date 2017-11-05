4cd5b7d1aa314cc4637fff87321394a07b229baa
Viktor Szakats <vszakats@users.noreply.github.com>
2017-11-05 10:47:59 +0000

    sdl_gfx: secure url(s)

--- Formula/sdl_gfx.rb
+++ Formula/sdl_gfx.rb
@@ -1,7 +1,7 @@
 class SdlGfx < Formula
   desc "Graphics drawing primitives and other support functions"
-  homepage "http://www.ferzkopp.net/joomla/content/view/19/14/"
-  url "http://www.ferzkopp.net/Software/SDL_gfx-2.0/SDL_gfx-2.0.26.tar.gz"
+  homepage "https://www.ferzkopp.net/joomla/content/view/19/14/"
+  url "https://www.ferzkopp.net/Software/SDL_gfx-2.0/SDL_gfx-2.0.26.tar.gz"
   sha256 "7ceb4ffb6fc63ffba5f1290572db43d74386cd0781c123bc912da50d34945446"
 
   bottle do
