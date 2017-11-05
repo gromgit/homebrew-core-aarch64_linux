cfaad055907944ea0f5a520dd8a481c99fb0d447
Viktor Szakats <vszakats@users.noreply.github.com>
2017-11-05 12:46:05 +0000

    txt2tags: fix max page location, secure homepage

--- Formula/txt2tags.rb
+++ Formula/txt2tags.rb
@@ -1,14 +1,15 @@
 class Txt2tags < Formula
   desc "Conversion tool to generating several file formats"
-  homepage "http://txt2tags.org"
+  homepage "https://txt2tags.org/"
   url "https://github.com/txt2tags/txt2tags/archive/2.6.tar.gz"
   sha256 "c87b911ac5cc97dd2fdb6067601cf4fbd0094a065f6b7a593142fa75e3a924c1"
+  revision 1
 
   bottle :unneeded
 
   def install
     bin.install "txt2tags"
-    man1.install "doc/manpage.man" => "txt2tags.1"
+    man1.install "doc/English/manpage.man" => "txt2tags.1"
   end
 
   test do
