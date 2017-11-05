a91c906073bd9b3f7fb36134b79f4f689b237b47
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-05 16:50:15 +0000

    smartmontools: update 6.6 bottle.

--- Formula/smartmontools.rb
+++ Formula/smartmontools.rb
@@ -5,11 +5,9 @@ class Smartmontools < Formula
   sha256 "51f43d0fb064fccaf823bbe68cf0d317d0895ff895aa353b3339a3b316a53054"
 
   bottle do
-    sha256 "c4ad7926a0da73101e7ef83eda23cfd21c808af7f6d19b8d8b8ea4714432895b" => :high_sierra
-    sha256 "36d3ef7c8959108c52d0d3e8539c09f40547f73f7b30c295a66e741ff5ec110f" => :sierra
-    sha256 "026783b59f7fbea367d6fe845db61b84ad8ecbcea7b39277503bd5548ffc3e4b" => :el_capitan
-    sha256 "1f44588d95c27cf0d0a5efc4e1aa892d00bbd3b5d55515db026c0715a6254e70" => :yosemite
-    sha256 "87e1640444ba9717a2de2530a9a981705e9752f12a276bfc4dde606ab187e5a7" => :mavericks
+    sha256 "555457dd2f346be5471a4297610e6ed7807e7986ecef5bc9e1087a3a831137f8" => :high_sierra
+    sha256 "72708ed71cc68fb23516bc971dc1360ffdd63d20ca191b183f3abc10b976269b" => :sierra
+    sha256 "8868a3b550422f2366878f57fd823d69475880cca6a9ffe7c0324b3f78df3730" => :el_capitan
   end
 
   def install
