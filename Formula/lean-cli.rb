4a702802f561b5d7c100584b48f9e48af515ceca
ilovezfs <ilovezfs@icloud.com>
2017-11-06 05:04:15 -0800

    lean-cli 0.17.1
    Closes #20329.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/lean-cli.rb
+++ Formula/lean-cli.rb
@@ -1,8 +1,8 @@
 class LeanCli < Formula
   desc "Command-line tool to develop and manage LeanCloud apps"
   homepage "https://github.com/leancloud/lean-cli"
-  url "https://github.com/leancloud/lean-cli/archive/v0.16.2.tar.gz"
-  sha256 "70c31c81966bc12cf9de22a3192e55f03c64055a907e42995e5df98db2c89a32"
+  url "https://github.com/leancloud/lean-cli/archive/v0.17.1.tar.gz"
+  sha256 "dc78e98184270207bebafcb5d18a7e5785d7bc1437fcbbc40a7fbfd6a1bf27b4"
   head "https://github.com/leancloud/lean-cli.git"
 
   bottle do
