07950075f3d903046e6c8c8ec2fafeb377373146
Diogo Lemos <diogo.mbl@gmail.com>
2017-11-07 17:30:48 +0000

    calicoctl 1.6.1 (new formula)
    Closes #20374.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/calicoctl.rb
@@ -0,0 +1,27 @@
+class Calicoctl < Formula
+  desc "Calico CLI tool"
+  homepage "https://www.projectcalico.org"
+  url "https://github.com/projectcalico/calicoctl.git",
+      :tag => "v1.6.1",
+      :revision => "1724e011ac0e608190d7d5512ab8028bcd18ae7b"
+
+  depends_on "glide" => :build
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
+    dir = buildpath/"src/github.com/projectcalico/calicoctl"
+    dir.install buildpath.children
+    cd dir do
+      system "glide", "install", "-strip-vendor"
+      system "make", "binary"
+      bin.install "dist/calicoctl-darwin-amd64" => "calicoctl"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    system bin/"calicoctl", "--version"
+  end
+end
