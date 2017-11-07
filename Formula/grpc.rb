91437be56eb2502d2334f5c921d074bd0951238d
ilovezfs <ilovezfs@icloud.com>
2017-11-07 11:56:02 -0800

    grpc 1.7.2
    Closes #20379.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/grpc.rb
+++ Formula/grpc.rb
@@ -1,20 +1,9 @@
 class Grpc < Formula
   desc "Next generation open source RPC library and framework"
   homepage "https://www.grpc.io/"
+  url "https://github.com/grpc/grpc/archive/v1.7.2.tar.gz"
+  sha256 "0343496a3e79d5fb7ea7be5fa466d8e00ef0ad459394047b1a874100fd605711"
   head "https://github.com/grpc/grpc.git"
-
-  stable do
-    url "https://github.com/grpc/grpc/archive/v1.6.7.tar.gz"
-    sha256 "0ed08e406eb3ecb1ab88b1eb183c7c51a996dae8fa79a0e6fbb9561c9098bf96"
-
-    # Fix "error: use of undeclared identifier 'INT_MAX'"
-    # Upstream PR from 12 Sep 2017 "Fix headers on flow control"
-    # See https://github.com/grpc/grpc/pull/12525
-    patch do
-      url "https://github.com/grpc/grpc/commit/b2f490c565e.patch?full_index=1"
-      sha256 "47e44873afd5a157d7ff1527b5cdb0f08f236589a33de616d2d6d2690a5bc6b2"
-    end
-  end
 
   bottle do
     sha256 "6a5c18584c79c72376a976dac256558f6f4217c9ce852aac53d46342581db575" => :high_sierra
