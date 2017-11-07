8016f6daecf25ceeefe92e9defee5a6dd86e422a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-07 16:31:37 +0000

    just: add 0.3.1 bottle.

--- Formula/just.rb
+++ Formula/just.rb
@@ -4,6 +4,12 @@ class Just < Formula
   url "https://github.com/casey/just/archive/v0.3.1.tar.gz"
   sha256 "acb8e967906357476fc9328636292a1a2b369f84903a114573607ec0215bf0e7"
 
+  bottle do
+    sha256 "361597da1ad5ef750467ce377e8a31de483bae49fcc856b628ec9d396c297442" => :high_sierra
+    sha256 "ee37a3a4ff27c1f5e789c0e349738790db9ec6b99eb6f2b7b584451f7982a167" => :sierra
+    sha256 "e8d7cb56805d2c19a69ad463343e2f9ec70d0568b36d568116ae86e264d8a9c4" => :el_capitan
+  end
+
   depends_on "rust" => :build
 
   def install
