8819343cf75c85d56734abe2cc3c002508851c55
Chongyu Zhu <i@lembacon.com>
2017-11-08 20:45:34 +0800

    neovim 0.2.1
    Closes #20397.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/neovim.rb
+++ Formula/neovim.rb
@@ -1,34 +1,15 @@
 class Neovim < Formula
   desc "Ambitious Vim-fork focused on extensibility and agility"
   homepage "https://neovim.io/"
-  revision 1
-
-  stable do
-    url "https://github.com/neovim/neovim/archive/v0.2.0.tar.gz"
-    sha256 "72e263f9d23fe60403d53a52d4c95026b0be428c1b9c02b80ab55166ea3f62b5"
-
-    depends_on "luajit" => :build
-
-    # Remove for > 0.2.0
-    # Upstream commit from 7 Jul 2017 "Prefer the static jemalloc library by default on OSX"
-    # See https://github.com/neovim/neovim/pull/6979
-    patch do
-      url "https://github.com/neovim/neovim/commit/35fad15c8907f741ce21779393e4377de753e4f9.patch?full_index=1"
-      sha256 "b156fdc92a3850eca3047620087f25a021800b729a3c30fd0164404b2ff7848b"
-    end
-  end
+  url "https://github.com/neovim/neovim/archive/v0.2.1.tar.gz"
+  sha256 "9e2c068a8994c9023a5f84cde9eb7188d3c85996a7e42e611e3cd0996e345dd3"
+  head "https://github.com/neovim/neovim.git"
 
   bottle do
     sha256 "a382d197011bc49c374a915e6d5d8a98d07a92cccf7aa79032a873a578734f0c" => :high_sierra
     sha256 "6d0cbbaadd947b428f29fb28ba0f492f2d84a71d981ad22cf00032036118ddf9" => :sierra
     sha256 "fdb74c70048d2c9232525ab652464d9818d0134442c855168f399ab2dd9504a7" => :el_capitan
     sha256 "f563c067954a4c5f98ad2b01ee3219e5d1666f7000917474186127122cf5cc76" => :yosemite
-  end
-
-  head do
-    url "https://github.com/neovim/neovim.git"
-
-    depends_on "luajit"
   end
 
   depends_on "cmake" => :build
@@ -39,6 +20,7 @@ class Neovim < Formula
   depends_on "libtermkey"
   depends_on "libuv"
   depends_on "libvterm"
+  depends_on "luajit"
   depends_on "msgpack"
   depends_on "unibilium"
   depends_on :python if MacOS.version <= :snow_leopard
