67bc619d656021d6cb2429a045876144d834fb8c
Mike Farah <mike.farah@orchestrated.io>
2017-11-08 14:50:06 +1100

    yq 1.14.0 (new formula)
    Closes #20394.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/yq.rb
@@ -0,0 +1,25 @@
+class Yq < Formula
+  desc "Process YAML documents from the CLI"
+  homepage "https://github.com/mikefarah/yq"
+  url "https://github.com/mikefarah/yq/archive/1.14.0.tar.gz"
+  sha256 "cf61f84033ed8e1cb146798bfcedfabba5fa8852331a90122eafaae0f31c4b70"
+
+  depends_on "go" => :build
+  depends_on "govendor" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/mikefarah/yq").install buildpath.children
+
+    cd "src/github.com/mikefarah/yq" do
+      system "govendor", "sync"
+      system "go", "build", "-o", bin/"yq"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    assert_equal "key: cat", shell_output("#{bin}/yq n key cat").chomp
+    assert_equal "cat", pipe_output("#{bin}/yq r - key", "key: cat", 0).chomp
+  end
+end
