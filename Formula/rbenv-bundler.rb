dadf4e8eb2a3144fd431876593f212a05ae0e53f
Roy Liu <roy@eschatonlabs.com>
2017-11-09 06:25:15 -0500

    rbenv-bundler 1.00 (#20430)

--- Formula/rbenv-bundler.rb
+++ Formula/rbenv-bundler.rb
@@ -1,8 +1,8 @@
 class RbenvBundler < Formula
   desc "Makes shims aware of bundle install paths"
   homepage "https://github.com/carsomyr/rbenv-bundler"
-  url "https://github.com/carsomyr/rbenv-bundler/archive/0.99.tar.gz"
-  sha256 "4d5a0224b5050e5573ad42965182d391a6927553560d1953c56f4adf82a64e97"
+  url "https://github.com/carsomyr/rbenv-bundler/archive/1.00.tar.gz"
+  sha256 "84f5456b1ac8ea4554db8fa17c99b5f5d2eafcd647f85088a5d36e805c092ba7"
   head "https://github.com/carsomyr/rbenv-bundler.git"
 
   bottle :unneeded
