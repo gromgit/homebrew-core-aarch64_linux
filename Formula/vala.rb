2bee3ada2cd0e65b7d4a7e1e09acad4613ac3aee
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-09 12:10:31 +0000

    vala: update 0.38.3 bottle.

--- Formula/vala.rb
+++ Formula/vala.rb
@@ -5,9 +5,9 @@ class Vala < Formula
   sha256 "4addaff4625b203763c454e81b928219d41e152f9982c836c72094d3315d6854"
 
   bottle do
-    sha256 "938663a8ad34b6e4d3bf07a789324c1bbbde0488270e9784899d7715984cd4f1" => :high_sierra
-    sha256 "1e661d527bc74e31ad5ef398c7246cdeb779c33bda31bb033ebb56d2270d2d34" => :sierra
-    sha256 "cb8c2b6258a2b8dfde673ad1d6efdd89ce47ff392779d17f5095ada8256b656a" => :el_capitan
+    sha256 "26e83ce3d1dc33b56799555d24337d2a8269edb09b0afa8a19c7f1aa113e5327" => :high_sierra
+    sha256 "5e90527f89fa666ae63f0e32ba6faebe5c86c03c79567a42616b1f9c860b1ad0" => :sierra
+    sha256 "9cd49650197eb844f7332a18e87db04b8b1de70ebaeb8bf96a535a861f66e3f0" => :el_capitan
   end
 
   depends_on "pkg-config" => :run
