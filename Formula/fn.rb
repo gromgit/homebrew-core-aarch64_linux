05fe3a05501163988918a038c78371a64be36a5f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-09 11:16:08 +0000

    fn: update 0.4.16 bottle.

--- Formula/fn.rb
+++ Formula/fn.rb
@@ -6,9 +6,9 @@ class Fn < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7133eb88a2a6b328cac280afa6e44b53e779be304b3b7db7656e74b73c12de94" => :high_sierra
-    sha256 "a4e2661db716bc0060829ca3bfe3cf864832e8bf3c9a3871a13e08b20825feb6" => :sierra
-    sha256 "398dbe41c5964e46d919acc2c826a105796f27f69ee08e7f10dcc1f35a08a002" => :el_capitan
+    sha256 "3eba152a4bc60919e9ae662f9420aa27f5eeee83e3b1ba5d75b1ea2c3a7a7237" => :high_sierra
+    sha256 "6e5f8d5553f40e1748e642f0732d8197087c933e34a230f9cae0b7e1bf8157ae" => :sierra
+    sha256 "33f723f6dfbcfdc175147e0ccec763a88dbfd551005a8c6115ce729692db2633" => :el_capitan
   end
 
   depends_on "go" => :build
