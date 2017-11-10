92d823396d2ee8d1a7a88827bdbc7fa274dc88f5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-10 09:38:07 +0000

    nuttcp: update 8.1.4 bottle.

--- Formula/nuttcp.rb
+++ Formula/nuttcp.rb
@@ -6,12 +6,9 @@ class Nuttcp < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7bb9ab2f301204e093c71fb5d78fb940b8721adbfc4435e8887850a8f4582159" => :high_sierra
-    sha256 "478522cb3e50128eb9b7b09d519bfaa553ae05025d1708a3d5d0c7b76dcbac8a" => :sierra
-    sha256 "7fe34e31cd50393b055dde913785931696bb332bc1b5948720d7817e1247151c" => :el_capitan
-    sha256 "18cfa8910325006d83e53c94a845d611704ce882f3f48a5edd2ebc6e75def200" => :yosemite
-    sha256 "57c31d24a43302c4fb1b08f6a17a05da60c3486a52481b0580c3e1f5d75c7eaa" => :mavericks
-    sha256 "e289d316c0aea0a8b1a85bde5f72f648cfe2dbae26a9fee4f7614ccdd7aa983a" => :mountain_lion
+    sha256 "80b4a1b94e21a1b94d960fefddfbe235597ccc687f3f815a7aee989fb0d53a07" => :high_sierra
+    sha256 "50f44fadb6a471f65fac82065681ed42b44cf1c6ea78680db9e0ade3725577a0" => :sierra
+    sha256 "6b2eab6b736721543d321f9577ac8ee94c3cefd5026bb0d39084c4d721853249" => :el_capitan
   end
 
   def install
