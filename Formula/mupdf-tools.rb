6d7ae97985a89a6bc009308f17ca047045862552
Dominyk Tiller <dominyktiller@gmail.com>
2017-11-10 22:47:57 +0000

    mupdf-tools: patch CVE-2017-15587
    Closes #20521.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mupdf-tools.rb
+++ Formula/mupdf-tools.rb
@@ -1,7 +1,7 @@
 class MupdfTools < Formula
   desc "Lightweight PDF and XPS viewer"
   homepage "https://mupdf.com/"
-  revision 1
+  revision 2
   head "https://git.ghostscript.com/mupdf.git"
 
   stable do
@@ -10,12 +10,12 @@ class MupdfTools < Formula
 
     # Upstream already. Remove on next stable release.
     patch do
-      url "https://mirrors.ocf.berkeley.edu/debian/pool/main/m/mupdf/mupdf_1.11+ds1-1.1.debian.tar.xz"
-      mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/m/mupdf/mupdf_1.11+ds1-1.1.debian.tar.xz"
-      sha256 "cb274532e34f818b2f1871fee6303cfffda37251937dd7d731a898b2ca736433"
-      apply "patches/0003-Fix-698539-Don-t-use-xps-font-if-it-could-not-be-loa.patch",
-            "patches/0004-Fix-698540-Check-name-comment-and-meta-size-field-si.patch",
-            "patches/0005-Fix-698558-Handle-non-tags-in-tag-name-comparisons.patch"
+      url "https://mirrors.ocf.berkeley.edu/debian/pool/main/m/mupdf/mupdf_1.11+ds1-2.debian.tar.xz"
+      mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/m/mupdf/mupdf_1.11+ds1-2.debian.tar.xz"
+      sha256 "da7445a8063d7c81b97d2c373aa112df69d3ad29989b67621387e88d9c38b668"
+      apply "patches/0004-Fix-698539-Don-t-use-xps-font-if-it-could-not-be-loa.patch",
+            "patches/0005-Fix-698540-Check-name-comment-and-meta-size-field-si.patch",
+            "patches/0006-Fix-698558-Handle-non-tags-in-tag-name-comparisons.patch"
     end
   end
 
@@ -29,6 +29,14 @@ class MupdfTools < Formula
   def install
     # Work around bug: https://bugs.ghostscript.com/show_bug.cgi?id=697842
     inreplace "Makerules", "RANLIB_CMD := xcrun", "RANLIB_CMD = xcrun"
+
+    # We're using an inreplace here because Debian's version of this patch
+    # breaks when using Clang as a compiler rather than GCC. This fixes
+    # CVE-2017-15587.
+    if build.stable?
+      inreplace "source/pdf/pdf-xref.c", "if (i0 < 0 || i1 < 0)",
+                                         "if (i0 < 0 || i1 < 0 || i0 > INT_MAX - i1)"
+    end
 
     system "make", "install",
            "build=release",
