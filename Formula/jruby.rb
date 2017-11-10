333b46f7d3d9ed18785747c630cc28e1d8f0a054
ilovezfs <ilovezfs@icloud.com>
2017-11-10 05:10:55 -0800

    jruby 9.1.14.0 (#20504)

--- Formula/jruby.rb
+++ Formula/jruby.rb
@@ -1,8 +1,8 @@
 class Jruby < Formula
   desc "Ruby implementation in pure Java"
   homepage "http://www.jruby.org"
-  url "https://s3.amazonaws.com/jruby.org/downloads/9.1.13.0/jruby-bin-9.1.13.0.tar.gz"
-  sha256 "9d156646623ac2f27174721035b52572a4b05690db7c1293295aa2c04aad3908"
+  url "https://search.maven.org/remotecontent?filepath=org/jruby/jruby-dist/9.1.14.0/jruby-dist-9.1.14.0-bin.tar.gz"
+  sha256 "074057e672350a6652d92ccaaa5d517fc7d6b980bce8b947515fb64d114d1651"
 
   bottle :unneeded
 
