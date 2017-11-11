665bf9f626dbd926f5dfa0d00fd9029ee9f1bfff
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-11 14:24:44 +0000

    newsboat: add 2.10.1 bottle.

--- Formula/newsboat.rb
+++ Formula/newsboat.rb
@@ -5,6 +5,12 @@ class Newsboat < Formula
   sha256 "82d5e3e2a6dab845aac0bf72580f46c2756375d49214905a627284e5bc32e327"
   head "https://github.com/newsboat/newsboat.git"
 
+  bottle do
+    sha256 "9dcbf0d0a026bc0c0667ce52698365f2f172e1222334657a4327c1101304b8ff" => :high_sierra
+    sha256 "2094f90b7a64c045e408973420cec64805f4ec517a9397f5850d35d9c013151e" => :sierra
+    sha256 "9af1f9da45dcb359481827c3e61e2aef3e21c253b29d436962369b8816be25cc" => :el_capitan
+  end
+
   depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "json-c"
