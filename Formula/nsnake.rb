14946aa3b59c79ea6918fa03a7179adcd6dd4fae
ilovezfs <ilovezfs@icloud.com>
2017-11-10 21:08:09 -0800

    nsnake: use GitHub for homepage (#20525)
    See https://github.com/alexdantas/nSnake/issues/22.

--- Formula/nsnake.rb
+++ Formula/nsnake.rb
@@ -1,6 +1,6 @@
 class Nsnake < Formula
   desc "Classic snake game with textual interface"
-  homepage "http://nsnake.alexdantas.net/"
+  homepage "https://github.com/alexdantas/nSnake"
   url "https://downloads.sourceforge.net/project/nsnake/GNU-Linux/nsnake-3.0.1.tar.gz"
   sha256 "e0a39e0e188a6a8502cb9fc05de3fa83dd4d61072c5b93a182136d1bccd39bb9"
   head "https://github.com/alexdantas/nSnake.git"
