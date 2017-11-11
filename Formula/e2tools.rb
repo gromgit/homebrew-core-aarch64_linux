bbf8f9ec25135835fe9aeeeb7659e437f0cde459
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-11 13:27:32 +0000

    e2tools: add 0.0.16 bottle.

--- Formula/e2tools.rb
+++ Formula/e2tools.rb
@@ -4,6 +4,13 @@ class E2tools < Formula
   url "http://home.earthlink.net/~k_sheff/sw/e2tools/e2tools-0.0.16.tar.gz"
   sha256 "4e3c8e17786ccc03fc9fb4145724edf332bb50e1b3c91b6f33e0e3a54861949b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7a782af6e3883fe9badda9b579193be4068b71fc1c2c8530f6b207b30bd1f9c3" => :high_sierra
+    sha256 "5818dc7acdcb57aa0945c79280e118cd630ec45d0c3b1997c791158bf5c807e1" => :sierra
+    sha256 "058158b36410bb749abe3bce7476a7f1c837417a38e8d3fa1dd54924df2d80a7" => :el_capitan
+  end
+
   depends_on "e2fsprogs"
 
   def install
