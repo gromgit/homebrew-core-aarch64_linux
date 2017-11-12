ca40802b9c037e369db5d2bb7502835534880b57
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-12 22:04:03 +0000

    bit: update 0.11.0 bottle.

--- Formula/bit.rb
+++ Formula/bit.rb
@@ -8,9 +8,9 @@ class Bit < Formula
   head "https://github.com/teambit/bit.git"
 
   bottle do
-    sha256 "9f8c4c5a74cf4f16b2e3e7f94d7e1a5192113a3b1b849780a8e739dded55282f" => :high_sierra
-    sha256 "d5a6606933925ede7b83be76d0567c010654e20d2a60ffc241ffc274ca35bd5a" => :sierra
-    sha256 "cd883b34bedceb4866e1badaaa9dc5f6e5bdcdcb0071062525234b3bfdad8377" => :el_capitan
+    sha256 "5203f2bb3ec825f140741c43802178328bbe1cdac52842d4dfe42891b2c0d10e" => :high_sierra
+    sha256 "78dee314349a867117c9ebec759172b4604e9f074df22558205f66f01297c572" => :sierra
+    sha256 "b3545bbe297ae46ab0e3a5ed19de52bcd9e16ce9d4d1d1b47e83a17a9dba0d48" => :el_capitan
   end
 
   depends_on "node"
