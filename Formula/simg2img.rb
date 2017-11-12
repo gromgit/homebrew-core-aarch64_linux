bcc39e6611674b479e0673d1c5edc042c37b85c9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-12 07:10:17 +0000

    simg2img: add 1.1.1 bottle.

--- Formula/simg2img.rb
+++ Formula/simg2img.rb
@@ -5,6 +5,13 @@ class Simg2img < Formula
   sha256 "d096ca7e02b3ad5b87cbb6467d3766720355f32aa5ae9b9264d7ca7c486b0738"
   head "https://github.com/anestisb/android-simg2img.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1d86b8e4e7b13def022a642ef9a4198a1250cfb8fc0020b2fb5788e331fd92b7" => :high_sierra
+    sha256 "2a4e498b96fbb7b30b3b9bdeb3a3d13dbaed1278b768fd8a698f7b8edf9da452" => :sierra
+    sha256 "a482e1be3dc43507d87915919589604c1da1b99c9aa7ad204cb50291d2587d2c" => :el_capitan
+  end
+
   def install
     system "make", "PREFIX=#{prefix}", "install"
   end
