83231971843dfbe7599b3d6ac6273a2cb02189be
ilovezfs <ilovezfs@icloud.com>
2017-11-13 08:29:16 -0800

    libopusenc 0.1.1
    Closes #20592.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libopusenc.rb
+++ Formula/libopusenc.rb
@@ -1,8 +1,8 @@
 class Libopusenc < Formula
   desc "Convenience libraray for creating .opus files"
   homepage "https://opus-codec.org/"
-  url "https://archive.mozilla.org/pub/opus/libopusenc-0.1.tar.gz"
-  sha256 "bd6d14e8897a2f80065ef34a516c70e74f8e00060abdbc238e79e5f99bca3e96"
+  url "https://archive.mozilla.org/pub/opus/libopusenc-0.1.1.tar.gz"
+  sha256 "02e6e0b14cbbe0569d948a46420f9c9a81d93bba32dc576a4007cbf96da68ef3"
 
   bottle do
     cellar :any
