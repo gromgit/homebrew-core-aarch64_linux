ec39686bd114e12e7b1e029f69492995ac72fd07
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-13 20:52:31 +0000

    zenity: update 3.26.0 bottle.

--- Formula/zenity.rb
+++ Formula/zenity.rb
@@ -5,10 +5,9 @@ class Zenity < Formula
   sha256 "6a7f34626dd62b751fe22bcdb32f3558f8a8fdddcc9406893dd264f0ac18e830"
 
   bottle do
-    sha256 "9e4cafa7f463e97dc694293e45df1cf51f4da611f1ec26860a70876c28a44d4d" => :high_sierra
-    sha256 "67b736d9989fe2985ae58ad381259dddca8ed1a60e0f26a3f4f830d932b44f74" => :sierra
-    sha256 "97992076a537ea495dc2f17d3f5e6484d4fcdb805f0ce0f8a6e4284b9f11f84c" => :el_capitan
-    sha256 "07a58ac59c867e6db9f367ee6d516bde31a1fa78d4861b42b5d2147e870e32cc" => :yosemite
+    sha256 "8d21567ffffb86f7345993dcc93a865ded92d2df181dfa8c65f9e35fd7beaf00" => :high_sierra
+    sha256 "618ff56a163ed4094c60339853ab76492e8bfec46250a41a94258c13a09b38ed" => :sierra
+    sha256 "07292dedf997e00cf082909b10ba11d1aabf6ca82b1c1832808f31d34377781b" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
