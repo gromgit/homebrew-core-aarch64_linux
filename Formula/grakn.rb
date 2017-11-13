133a19f972e0506da2be60dedb34169d8fe37581
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-11-13 18:03:02 +0200

    grakn 0.17.1 (#20231)
    require Java 8 specifically
    don't set CASSANDRA_HOME
    update the test

--- Formula/grakn.rb
+++ Formula/grakn.rb
@@ -1,20 +1,20 @@
 class Grakn < Formula
   desc "The Database for AI"
   homepage "https://grakn.ai"
-  url "https://github.com/graknlabs/grakn/releases/download/v0.16.0/grakn-dist-0.16.0.tar.gz"
-  sha256 "1c7194f09cbf31949323b79b2fb8a5683381af5ecf37886fd45deb71585261ed"
+  url "https://github.com/graknlabs/grakn/releases/download/v0.17.1/grakn-dist-0.17.1.tar.gz"
+  sha256 "a4d190d95515b01ce736056e2d738d67c14298465ba7fb92df5295612bb91475"
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on :java => "1.8"
 
   def install
     libexec.install Dir["*"]
-    bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files(libexec/"bin", :CASSANDRA_HOME => ENV["CASSANDRA_HOME"])
+    bin.install libexec/"grakn", libexec/"graql"
+    bin.env_script_all_files(libexec, Language::Java.java_home_env("1.8"))
   end
 
   test do
-    assert_match /stopped/i, shell_output("#{bin}/grakn.sh status")
+    assert_match /RUNNING/i, shell_output("#{bin}/grakn server status")
   end
 end
