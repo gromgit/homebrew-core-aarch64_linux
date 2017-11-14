9e2b257db3d24c9e5bd7ce2248746688f7e3f6e9
JCount <JCount42@gmail.com>
2017-11-14 04:53:15 -0500

    apibuilder-cli 0.1.17 (#20602)

--- Formula/apibuilder-cli.rb
+++ Formula/apibuilder-cli.rb
@@ -1,8 +1,8 @@
 class ApibuilderCli < Formula
   desc "Command-line interface to generate clients for api builder"
   homepage "https://www.apibuilder.io"
-  url "https://github.com/apicollective/apibuilder-cli/archive/0.1.16.tar.gz"
-  sha256 "0b4368f3a156638ff6f7b070ff306f577c0ab0c410a084c99377a9671798e4a1"
+  url "https://github.com/apicollective/apibuilder-cli/archive/0.1.17.tar.gz"
+  sha256 "62427cd27525edf66448c07143253a73987a242e1a329e6cf91b20dad238eaae"
 
   bottle :unneeded
 
