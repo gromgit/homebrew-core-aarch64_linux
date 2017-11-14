0c0d87e8dc3f0e90594a844d8eb0689851cbec58
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-14 05:44:29 +0000

    zbackup: update 1.4.4_6 bottle.

--- Formula/zbackup.rb
+++ Formula/zbackup.rb
@@ -7,10 +7,9 @@ class Zbackup < Formula
 
   bottle do
     cellar :any
-    sha256 "ed2d33f9f599357eabedb62c70b5f44555cbc57f1bb2a08787b47abb9800b753" => :high_sierra
-    sha256 "5604f7d12355aa94dde2feae94dd7e361a76d637d0dd297e4d54f9bb149b9b51" => :sierra
-    sha256 "c12be8aa04a509b3877a8dd24a49dc59e2e8688760be562e61883e931c0eee70" => :el_capitan
-    sha256 "7f833746286b96a12225a6149a585745e9c5cd1903d97ad40cc38f67cf0e7a1e" => :yosemite
+    sha256 "e68198033bdb384d3f0988ff641a15ed74eca20b54d99176cd3c95215aa8371e" => :high_sierra
+    sha256 "9669c67980ca07319479ab94197678a1b2357bb8c77457e9886cb82af0a8d708" => :sierra
+    sha256 "a09a760ce9c152937146d68e113a99c0869b22b80a6fc9ae634f61c06e609126" => :el_capitan
   end
 
   depends_on "cmake" => :build
