c6059441b7b9a63c86db4ee85ba0e64da084e0aa
ilovezfs <ilovezfs@icloud.com>
2017-11-14 01:23:00 -0800

    git-cinnabar: mercurial 4.4 compat
    Closes #20614.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/git-cinnabar.rb
+++ Formula/git-cinnabar.rb
@@ -1,10 +1,27 @@
 class GitCinnabar < Formula
   desc "Git remote helper to interact with mercurial repositories"
   homepage "https://github.com/glandium/git-cinnabar"
-  url "https://github.com/glandium/git-cinnabar.git",
-      :tag => "0.4.0",
-      :revision => "6d374888ff0287517084c0ec7573963961f6acec"
+  revision 1
   head "https://github.com/glandium/git-cinnabar.git"
+
+  stable do
+    url "https://github.com/glandium/git-cinnabar.git",
+        :tag => "0.4.0",
+        :revision => "6d374888ff0287517084c0ec7573963961f6acec"
+
+    # 5 Nov 2017 "Support the batch API change from mercurial 4.4"
+    patch do
+      url "https://github.com/glandium/git-cinnabar/commit/7ea77b0.patch?full_index=1"
+      sha256 "e28fdf1b9afa94dbd17289e739cd68af34bf7ae708b827cfa9e23286dbbbb57c"
+    end
+
+    # 5 Nov 2017 "Adapt localpeer to sshpeer changes in mercurial 4.4"
+    # Backport of https://github.com/glandium/git-cinnabar/commit/5c59ae1
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/e56093e/git-cinnabar/mercurial-4.4-sshpeer.patch"
+      sha256 "9af333567ff4aec002c947906d9e5a62ce7358c4ffa1edf7be0b5fe0a96b87ae"
+    end
+  end
 
   bottle do
     cellar :any_skip_relocation
@@ -19,6 +36,18 @@ class GitCinnabar < Formula
         :tag => "0.5.0b1",
         :revision => "f4ce4ab5ae70c11f00fbc0964e1edf4da6fe7657"
     version "0.5.0b1"
+
+    # same as in stable
+    patch do
+      url "https://github.com/glandium/git-cinnabar/commit/7ea77b0.patch?full_index=1"
+      sha256 "e28fdf1b9afa94dbd17289e739cd68af34bf7ae708b827cfa9e23286dbbbb57c"
+    end
+
+    # same as in stable
+    patch do
+      url "https://github.com/glandium/git-cinnabar/commit/5c59ae1.patch?full_index=1"
+      sha256 "263c13fb9a59ed790957fcf337671b093e0b4d434c37b69cf1d0e03fd2a4102b"
+    end
   end
 
   depends_on :hg
