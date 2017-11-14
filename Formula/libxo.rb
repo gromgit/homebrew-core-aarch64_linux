9be42d65175e6774ce739374eb00aca2db983281
Jonathan Anderson <jonathan.anderson@ieee.org>
2017-11-14 09:41:25 -0330

    libxo 0.8.4 (new formula)
    Closes #20623.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/libxo.rb
@@ -0,0 +1,28 @@
+class Libxo < Formula
+  desc "Allows an application to generate text, XML, JSON, and HTML output"
+  homepage "https://juniper.github.io/libxo/libxo-manual.html"
+  url "https://github.com/Juniper/libxo/releases/download/0.8.4/libxo-0.8.4.tar.gz"
+  sha256 "3b3a33b706075fdc478782fdc65db3e7f9167550f7d71b95e704236a797e7fd3"
+
+  depends_on "libtool" => :build
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <libxo/xo.h>
+      int main() {
+        xo_set_flags(NULL, XOF_KEYS);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lxo", "-o", "test"
+    system "./test"
+  end
+end
