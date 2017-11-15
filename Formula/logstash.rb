0e2accda492feb169a8956cf500c2cd9896d72e5
Jason Tedor <jason@tedor.me>
2017-11-15 09:22:41 -0500

    logstash 6.0.0

--- Formula/logstash.rb
+++ Formula/logstash.rb
@@ -1,8 +1,8 @@
 class Logstash < Formula
   desc "Tool for managing events and logs"
   homepage "https://www.elastic.co/products/logstash"
-  url "https://artifacts.elastic.co/downloads/logstash/logstash-5.6.4.tar.gz"
-  sha256 "62067531ae5ea4878f84b68625cefcbe427c88d4975702d61cb3eecd80687b09"
+  url "https://artifacts.elastic.co/downloads/logstash/logstash-6.0.0.tar.gz"
+  sha256 "3e36257cf076c2da348dcc72765ea2cd6509cf3c2d6300257f8085cd43c21661"
   head "https://github.com/elastic/logstash.git"
 
   bottle :unneeded
