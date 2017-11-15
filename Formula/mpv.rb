6cce762c499c1e9f66c9bf78085b419e700cc4f2
Clément <porkepix@gmail.com>
2017-11-15 15:08:59 +0100

    mpv: unset PYTHONPATH on <= 10.9 (#20675)
    Prevents a conflict between python2 and python3 when 
    gobject-introspection is using the :python requirement.

--- Formula/mpv.rb
+++ Formula/mpv.rb
@@ -51,6 +51,10 @@ class Mpv < Formula
     # that's good enough for building the manpage.
     ENV["LC_ALL"] = "C"
 
+    # Prevents a conflict between python2 and python3 when gobject-introspection
+    # is using the :python requirement
+    ENV.delete("PYTHONPATH") if MacOS.version <= :mavericks
+
     ENV.prepend_create_path "PYTHONPATH", buildpath/"vendor/lib/python2.7/site-packages"
     resource("docutils").stage do
       system "python", *Language::Python.setup_install_args(buildpath/"vendor")
