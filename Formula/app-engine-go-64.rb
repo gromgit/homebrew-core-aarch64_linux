efd00597c8dbee1706dfebb5a6e145436e7142bd
grapswiz <ineeza@gmail.com>
2017-11-16 19:02:31 +0900

    app-engine-go-64 1.9.61 (#20708)

--- Formula/app-engine-go-64.rb
+++ Formula/app-engine-go-64.rb
@@ -1,8 +1,8 @@
 class AppEngineGo64 < Formula
   desc "Google App Engine SDK for Go (AMD64)"
   homepage "https://cloud.google.com/appengine/docs/go/"
-  url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_amd64-1.9.60.zip"
-  sha256 "b8b3e06629fc373619a23e427f43922eb39f87e07a75358925b259eed2f5c956"
+  url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_amd64-1.9.61.zip"
+  sha256 "4291ecc6939e557d73c44fe742dba4c5f42d3455e56480f96ee86b6ba34c99e3"
 
   bottle :unneeded
 
