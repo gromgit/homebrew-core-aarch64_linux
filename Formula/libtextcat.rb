d207341697a0c269c346730cb128581bf9a702ba
Viktor Szakats <vszakats@users.noreply.github.com>
2017-11-17 15:39:11 +0000

    libtextcat: secure url(s)

--- Formula/libtextcat.rb
+++ Formula/libtextcat.rb
@@ -1,6 +1,6 @@
 class Libtextcat < Formula
   desc "N-gram-based text categorization library"
-  homepage "http://software.wise-guys.nl/libtextcat/"
+  homepage "https://software.wise-guys.nl/libtextcat/"
   url "http://pkgs.fedoraproject.org/repo/pkgs/libtextcat/libtextcat-2.2.tar.gz/128cfc86ed5953e57fe0f5ae98b62c2e/libtextcat-2.2.tar.gz"
   sha256 "5677badffc48a8d332e345ea4fe225e3577f53fc95deeec8306000b256829655"
 
