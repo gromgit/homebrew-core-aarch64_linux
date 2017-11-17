ab7537133e87ac5db223a3d9ae9f975530773e6e
Viktor Szakats <vszakats@users.noreply.github.com>
2017-11-17 15:38:01 +0000

    dwdiff: secure url(s)

--- Formula/dwdiff.rb
+++ Formula/dwdiff.rb
@@ -1,7 +1,7 @@
 class Dwdiff < Formula
   desc "Diff that operates at the word level"
-  homepage "http://os.ghalkes.nl/dwdiff.html"
-  url "http://os.ghalkes.nl/dist/dwdiff-2.1.1.tar.bz2"
+  homepage "https://os.ghalkes.nl/dwdiff.html"
+  url "https://os.ghalkes.nl/dist/dwdiff-2.1.1.tar.bz2"
   sha256 "9745860faad6cb58744c7c45d16c0c7e222896c80d0cd7208dd36126d1a98c8b"
 
   bottle do
