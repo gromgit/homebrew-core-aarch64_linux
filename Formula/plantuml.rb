7ab807798bf8111e6c007592a52a05b3fe6c27c9
Markus Perndorfer <markus.perndorfer@sap.com>
2017-11-17 09:38:01 +0100

    plantuml 1.2017.19

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://downloads.sourceforge.net/project/plantuml/1.2017.18/plantuml.1.2017.18.jar"
-  sha256 "60e5928d1e3af5bc91e6b2aff19ea1f8c2a15862c69b083b6c3e9079a9794e6b"
+  url "https://downloads.sourceforge.net/project/plantuml/1.2017.19/plantuml.1.2017.19.jar"
+  sha256 "4cdf08019ec3df8dedb30d629291ff164c376ca900f5b8c7d8fe0bd0d0e347cb"
   version_scheme 1
 
   bottle :unneeded
