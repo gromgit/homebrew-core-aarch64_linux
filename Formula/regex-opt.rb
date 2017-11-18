b3c55b7d2525a74dbe25d2c592558499f0444b4e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-18 21:49:58 +0000

    regex-opt: update 1.2.4 bottle.

--- Formula/regex-opt.rb
+++ Formula/regex-opt.rb
@@ -6,11 +6,9 @@ class RegexOpt < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "be7eb7a1ab846967ae0185c1d998c87d1699b95407c34e6d7d5222912fb8faad" => :high_sierra
-    sha256 "6450d5a7ab5cc7a19e14753aa797e3044b7f0f29dba2cc22c4db32c2706d439d" => :sierra
-    sha256 "3f9bdf79820441d3a0941a8cb17b9d2c4f512b9e0fbdc36560681c3534e3615a" => :el_capitan
-    sha256 "01d8af104a70bc0bcf9c9fb1b93b0e4f3a7d08aaadd367e34ec223c38a00d58c" => :yosemite
-    sha256 "08ac62a2ea7522fb62f915bad274e2693b8e587e8266d705f2fa210c3405c19d" => :mavericks
+    sha256 "a7f1e5b8eb46eb11388c8eed77f24d547c1448b228cf7ca35f2e5cb0145bab88" => :high_sierra
+    sha256 "110befc20e434b5d6294d9f3ad2592cae2bfaedec4e8fa1c3cff7265e83acc27" => :sierra
+    sha256 "7c3d9a1af7a3797bd556cc66402c558cfaaffce4d956094fcdd4fcd1b3a4bc3c" => :el_capitan
   end
 
   def install
