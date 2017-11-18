1c7da0be3789fff53dadd1b42be5fdea1c413bc6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-18 07:00:05 +0000

    eye-d3: update 0.8.4 bottle.

--- Formula/eye-d3.rb
+++ Formula/eye-d3.rb
@@ -6,9 +6,9 @@ class EyeD3 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "08a8a9e08f3f38c04265018a36a9cadce33ba45a8b72e227bf7117aa6f645274" => :high_sierra
-    sha256 "3b829bbb5c965a30aacf076d20c780dd733ed703dbc5d267471091bd23e5b74a" => :sierra
-    sha256 "ac57832d180f09d58375681d06f5adc0457bef0732caa530b82313922fc26a13" => :el_capitan
+    sha256 "23bdb9dd77c6a9b32c0cf3519f40f9311a42a98caf12f1250ba07e523cb62df0" => :high_sierra
+    sha256 "23bdb9dd77c6a9b32c0cf3519f40f9311a42a98caf12f1250ba07e523cb62df0" => :sierra
+    sha256 "85f776951f8ac0691a2f3c446860075950845a2fde2bfc7ccc8100df11446f63" => :el_capitan
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
