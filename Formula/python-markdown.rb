25b34113e8b66bfa603d1572968f4a393c9e67f2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-18 21:12:40 +0000

    python-markdown: update 2.6.9 bottle.

--- Formula/python-markdown.rb
+++ Formula/python-markdown.rb
@@ -6,11 +6,9 @@ class PythonMarkdown < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f3e06a910f9bdc55265f2d5242577742394dfd081fae200da6faa5214d957fdf" => :high_sierra
-    sha256 "4673d2636e4327ee243f52fda84ba374b536cebf347cff430106b28a4d799f20" => :sierra
-    sha256 "ed951b7e851bdbf329a9718aea5b71b9ba0c49c1bb2898a81fcf095ccef00d33" => :el_capitan
-    sha256 "629aa5a47ad91c7d43d016129ec5acb20d535840975d606241a38d7fe8961a4b" => :yosemite
-    sha256 "2b0f3ce418e7e695f1e1d00d9b31abb61704b8a41761387bdc290e07202e308e" => :mavericks
+    sha256 "6f035a2d2740f243413f28430ce22610228712cae0fa9367f58564b5848eff63" => :high_sierra
+    sha256 "6f035a2d2740f243413f28430ce22610228712cae0fa9367f58564b5848eff63" => :sierra
+    sha256 "6f035a2d2740f243413f28430ce22610228712cae0fa9367f58564b5848eff63" => :el_capitan
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
