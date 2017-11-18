247914af4038a3df2c208a5f86ee7d358dde4b35
ilovezfs <ilovezfs@icloud.com>
2017-11-18 09:14:39 -0800

    ispc 1.9.2
    Closes #20787.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/ispc.rb
+++ Formula/ispc.rb
@@ -1,8 +1,8 @@
 class Ispc < Formula
   desc "Compiler for SIMD programming on the CPU"
   homepage "https://ispc.github.io"
-  url "https://downloads.sourceforge.net/project/ispcmirror/v1.9.1/ispc-v1.9.1-osx.tar.gz"
-  sha256 "ee949fcdf37ad3e3c0b946e6d6e32d7a7eafcec43e2cb908aea7e58e78bb0f55"
+  url "https://downloads.sourceforge.net/project/ispcmirror/v1.9.2/ispc-v1.9.2-osx.tar.gz"
+  sha256 "aa307b97bea67d71aff046e3f69c0412cc950eda668a225e6b909dba752ef281"
 
   bottle :unneeded
 
