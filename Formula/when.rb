51165be34a9d63e800a299d56611a2f7c8ee87cc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-18 23:08:42 +0000

    when: update 1.1.37 bottle.

--- Formula/when.rb
+++ Formula/when.rb
@@ -7,11 +7,9 @@ class When < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3db4eeafe5ba23621db24b8ec67e1b97041524e649d4ddd149f3288a5fd8ebad" => :high_sierra
-    sha256 "f7ed7b34c5d51139d447e0c02e3ca15986db8fcd279e9902dad3d23938d74c3a" => :sierra
-    sha256 "9e2fc9c54fdf0984a9f4bfde45441f45be9b1a491197527a68608e8619923dee" => :el_capitan
-    sha256 "d9c2db7f27a06e70583b88c17c299616b0b0b154874148b827ef97bde9e841ec" => :yosemite
-    sha256 "d9e387024317d27260e29bcf3ea6cf08eec55b0dba291f0d9328f1b9e2353233" => :mavericks
+    sha256 "5f1858e4c42591c1c30c54f4bf1fecd1961b1fc2a6990f45289720ebc1ad4892" => :high_sierra
+    sha256 "5f1858e4c42591c1c30c54f4bf1fecd1961b1fc2a6990f45289720ebc1ad4892" => :sierra
+    sha256 "5f1858e4c42591c1c30c54f4bf1fecd1961b1fc2a6990f45289720ebc1ad4892" => :el_capitan
   end
 
   def install
