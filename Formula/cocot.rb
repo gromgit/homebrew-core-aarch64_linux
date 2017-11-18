70af9f6c4c04a5dd1418a005e48ad104335d024c
ilovezfs <ilovezfs@icloud.com>
2017-11-17 22:48:00 -0800

    cocot 1.2-20171118
    Closes #20756.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/cocot.rb
+++ Formula/cocot.rb
@@ -1,8 +1,8 @@
 class Cocot < Formula
   desc "Code converter on tty"
   homepage "https://vmi.jp/software/cygwin/cocot.html"
-  url "https://github.com/vmi/cocot/archive/cocot-1.1-20120313.tar.gz"
-  sha256 "bc67576b04a753c49ec563c30fb0cc383e9ce7a3db9295a384b7f77fcc1a57b8"
+  url "https://github.com/vmi/cocot/archive/cocot-1.2-20171118.tar.gz"
+  sha256 "b718630ce3ddf79624d7dcb625fc5a17944cbff0b76574d321fb80c61bb91e4c"
 
   head "https://github.com/vmi/cocot.git"
 
