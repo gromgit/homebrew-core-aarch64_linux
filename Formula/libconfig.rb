18e9411e1326a7db4519dfbaa735a148ab9a30ee
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-18 00:02:07 +0000

    libconfig: update 1.7.1 bottle.

--- Formula/libconfig.rb
+++ Formula/libconfig.rb
@@ -6,9 +6,9 @@ class Libconfig < Formula
   head "https://github.com/hyperrealm/libconfig.git"
 
   bottle do
-    sha256 "b9b5c4811c278a7ea7e193685a5b352e80121d28defb23715df4b66e98e33af2" => :high_sierra
-    sha256 "31ed172343eb35e862fe8e8eb1d4850845ea376b8857a1e44239ba8873e9e4e3" => :sierra
-    sha256 "ac4deedc89f0c4712ffde498f1eae572c34a5ef1b804a18a1d087fda0f5b43d9" => :el_capitan
+    sha256 "393d13238b0259162bca7c1f61cc56f96370b3e84db24609bbcab6913bd6ac1e" => :high_sierra
+    sha256 "bb0326e6418c46f2c387e6e161e859d19e26efeefb86101d6056c66047f14523" => :sierra
+    sha256 "7f3cd7d160f43c050d05c462cb56ef8a31db13f06de7507b305a7cc14d244d65" => :el_capitan
   end
 
   depends_on "automake" => :build
