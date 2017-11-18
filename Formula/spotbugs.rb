a1a1070d333e197b35de4b04f0db304e0885c7cc
Stephen <stephengroat@users.noreply.github.com>
2017-11-18 02:41:41 -0800

    spotbugs 3.1.0 and rename from findbugs (#19886)

---
+++ Formula/spotbugs.rb
@@ -0,0 +1,45 @@
+class Spotbugs < Formula
+  desc "Tool for Java static analysis (FindBugs's successor)"
+  homepage "https://spotbugs.github.io/"
+  url "https://repo.maven.apache.org/maven2/com/github/spotbugs/spotbugs/3.1.0/spotbugs-3.1.0.tgz"
+  sha256 "c1eab4dc50cd9854c83fc4a90b4a9b9c63e5307bd0f0f7bb04720e17153b629c"
+
+  head do
+    url "https://github.com/spotbugs/spotbugs.git"
+
+    depends_on "gradle" => :build
+  end
+
+  bottle :unneeded
+
+  depends_on :java => "1.8+"
+
+  def install
+    if build.head?
+      system "gradle", "build"
+      system "gradle", "installDist"
+      libexec.install Dir["spotbugs/build/install/spotbugs/*"]
+    else
+      libexec.install Dir["*"]
+    end
+    bin.install_symlink "#{libexec}/bin/spotbugs"
+  end
+
+  test do
+    (testpath/"HelloWorld.java").write <<~EOS
+      public class HelloWorld {
+        private double[] myList;
+        public static void main(String[] args) {
+          System.out.println("Hello World");
+        }
+        public double[] getList() {
+          return myList;
+        }
+      }
+    EOS
+    system "javac", "HelloWorld.java"
+    system "jar", "cvfe", "HelloWorld.jar", "HelloWorld", "HelloWorld.class"
+    output = shell_output("#{bin}/spotbugs -textui HelloWorld.jar")
+    assert_match /M V EI.*\nM C UwF.*\n/, output
+  end
+end
