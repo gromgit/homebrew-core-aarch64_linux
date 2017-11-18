bb1501f48af7bf2e1a41b931b6c4fcdb4902e0dc
ilovezfs <ilovezfs@icloud.com>
2017-11-18 12:42:37 -0800

    pure-ftpd 1.0.47
    Closes #20810.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/pure-ftpd.rb
+++ Formula/pure-ftpd.rb
@@ -1,11 +1,8 @@
 class PureFtpd < Formula
   desc "Secure and efficient FTP server"
   homepage "https://www.pureftpd.org/"
-  url "https://download.pureftpd.org/pub/pure-ftpd/releases/pure-ftpd-1.0.46.tar.gz"
-  mirror "https://fossies.org/linux/misc/pure-ftpd-1.0.46.tar.gz"
-  mirror "https://ftp.pureftpd.org/pub/pure-ftpd/releases/pure-ftpd-1.0.46.tar.gz"
-  sha256 "0609807335aade4d7145abdbb5cb05c9856a3e626babe90658cb0df315cb0a5c"
-  revision 1
+  url "https://download.pureftpd.org/pub/pure-ftpd/releases/pure-ftpd-1.0.47.tar.gz"
+  sha256 "4740c316f5df879a2d68464489fb9b8b90113fe7dce58e2cdd2054a4768f27ad"
 
   bottle do
     sha256 "def15854f9eea616cfb9b22d003b44d6302f2115df8ee8b682f67431a3048e78" => :high_sierra
