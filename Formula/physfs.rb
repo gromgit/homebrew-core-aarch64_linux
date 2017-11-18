fbe40f34a757aa4af3d7f0a3da616f57b1cde296
ilovezfs <ilovezfs@icloud.com>
2017-11-18 03:05:01 -0800

    physfs 3.0.1
    Closes #20765.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/physfs.rb
+++ Formula/physfs.rb
@@ -1,8 +1,8 @@
 class Physfs < Formula
   desc "Library to provide abstract access to various archives"
   homepage "https://icculus.org/physfs/"
-  url "https://icculus.org/physfs/downloads/physfs-2.0.3.tar.bz2"
-  sha256 "ca862097c0fb451f2cacd286194d071289342c107b6fe69079c079883ff66b69"
+  url "https://icculus.org/physfs/downloads/physfs-3.0.1.tar.bz2"
+  sha256 "b77b9f853168d9636a44f75fca372b363106f52d789d18a2f776397bf117f2f1"
   head "https://hg.icculus.org/icculus/physfs/", :using => :hg
 
   bottle do
