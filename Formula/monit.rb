856b801ca4c6a89c8a6d175a254d49b92c484ee1
ilovezfs <ilovezfs@icloud.com>
2017-11-17 22:49:59 -0800

    monit 5.25.1
    Closes #20759.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/monit.rb
+++ Formula/monit.rb
@@ -1,8 +1,8 @@
 class Monit < Formula
   desc "Manage and monitor processes, files, directories, and devices"
   homepage "https://mmonit.com/monit/"
-  url "https://mmonit.com/monit/dist/monit-5.25.0.tar.gz"
-  sha256 "8949b99716581c5550bbbfb1009190f0e70f84b804d39902e55c5c3d095c2c32"
+  url "https://mmonit.com/monit/dist/monit-5.25.1.tar.gz"
+  sha256 "4b5c25ceb10825f1e5404f1d8a7b21507716b82bc20c3586f86603691c3b81bc"
 
   bottle do
     sha256 "f4c7174fa4f4dd3fce8adf34e74f42070141da499f1cd685486d1720a832a2bd" => :high_sierra
