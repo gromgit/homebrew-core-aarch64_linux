cae65ceed6996fb68006f917d76bba323d6fc27a
ilovezfs <ilovezfs@icloud.com>
2017-11-18 13:28:57 -0800

    vpcs 0.8
    Closes #20836.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/vpcs.rb
+++ Formula/vpcs.rb
@@ -1,8 +1,8 @@
 class Vpcs < Formula
   desc "Virtual PC simulator for testing IP routing"
   homepage "https://vpcs.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/vpcs/0.6/vpcs-0.6-src.tbz"
-  sha256 "cc311b0dea9ea02ef95f26704d73e34d293caa503600a0acca202d577afd3ceb"
+  url "https://downloads.sourceforge.net/project/vpcs/0.8/vpcs-0.8-src.tbz"
+  sha256 "dca602d0571ba852c916632c4c0060aa9557dd744059c0f7368860cfa8b3c993"
 
   bottle do
     cellar :any_skip_relocation
