ca3e4882158367079f73ec2b76bae6a04da26c68
Jenkins <jenkins@jenkins.brew.sh>
2017-11-18 10:39:30 +0000

    pgpool-ii: update 3.6.7 bottle.

--- Formula/pgpool-ii.rb
+++ Formula/pgpool-ii.rb
@@ -5,10 +5,9 @@ class PgpoolIi < Formula
   sha256 "09cfe2cb36b9171d4a987a8b100e298e46d6a369c1b61d9e1a47d442ce16e249"
 
   bottle do
-    sha256 "d3d150df92b7ff7d7956ac0f351a00284921c41e26d93ae14fe6d841b357b449" => :high_sierra
-    sha256 "87b327d37b58c8ac1c65351ee16b8a541525481d3d02701c2cc12eb6caeb100b" => :sierra
-    sha256 "24fddb94c6b055d242039bdeb9c3b36f8d3afd602fb2f7b41f0654d72edb7a39" => :el_capitan
-    sha256 "b4ef5cc69fdca07dd243a5654a2192a26039591bcb3a11a87bac559aa23dedfb" => :yosemite
+    sha256 "003329039ed94efc5ed99c2ff9c903323fe1061c7d545c791f62836ea1e94bd6" => :high_sierra
+    sha256 "ab710a008b3a1692f0773a15bd2a979f31588e64a0647536d273c529b1d700fb" => :sierra
+    sha256 "c1590bcbf5b65e0b4f4f0438fbc5f86e5c6c876d971443ade09bdbeeeab05eed" => :el_capitan
   end
 
   depends_on :postgresql
