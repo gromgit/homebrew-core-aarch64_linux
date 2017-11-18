34153df233237ec5c5cebc5136422f35620517aa
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-18 13:51:01 +0000

    mkvtoolnix: update 18.0.0 bottle.

--- Formula/mkvtoolnix.rb
+++ Formula/mkvtoolnix.rb
@@ -5,9 +5,9 @@ class Mkvtoolnix < Formula
   sha256 "b8282d6d279caa86038721d2a49471d1dd25a465ee1a779ee9c2a99bc8ff972f"
 
   bottle do
-    sha256 "3a6d4d4ae0ee431d0503d70ad48084992f22c35414a77bd4aa5c8850096e11e8" => :high_sierra
-    sha256 "7ae8bdf0ab644ff1c6f060227a2eccd05d67a9d87712d406e5e5cab7aa7d8439" => :sierra
-    sha256 "d11c8c6ba808f41ceccce4dc9ab8cbb9a6aa6483d578a882a24830b6d3c77b60" => :el_capitan
+    sha256 "de777359b55b4467f54570f40ee143e6575e5049163c74ed1c383f25c570b5d9" => :high_sierra
+    sha256 "64a0975c2cbbe26b1b012073a3f312e45e0bb90fefbdf944075af122edcfa181" => :sierra
+    sha256 "eac1ed9b45eb5020f7a0d662c71671bb8aa2adade63cd0ff2cc319c5c4661a16" => :el_capitan
   end
 
   head do
