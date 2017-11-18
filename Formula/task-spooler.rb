e47a5d112028ac897e653209e5d81a589f3616bb
ilovezfs <ilovezfs@icloud.com>
2017-11-18 13:21:09 -0800

    task-spooler 1.0
    Closes #20830.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/task-spooler.rb
+++ Formula/task-spooler.rb
@@ -1,8 +1,8 @@
 class TaskSpooler < Formula
   desc "Batch system to run tasks one after another"
   homepage "http://vicerveza.homeunix.net/~viric/soft/ts/"
-  url "http://vicerveza.homeunix.net/~viric/soft/ts/ts-0.7.5.tar.gz"
-  sha256 "216d09dcfbae2f9bfea7582a71494172fe91c33d65499ea01b3bcac0600de96d"
+  url "http://vicerveza.homeunix.net/~viric/soft/ts/ts-1.0.tar.gz"
+  sha256 "4f53e34fff0bb24caaa44cdf7598fd02f3e5fa7cacaea43fa0d081d03ffbb395"
 
   bottle do
     cellar :any_skip_relocation
