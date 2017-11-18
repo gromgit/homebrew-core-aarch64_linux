b76e026fe58276a4ec7782f211bb2d90e3e18788
ilovezfs <ilovezfs@icloud.com>
2017-11-18 12:45:54 -0800

    python-markdown 2.6.9
    Closes #20811.
    
    Signed-off-by: William Woodruff <william@tuffbizz.com>

--- Formula/python-markdown.rb
+++ Formula/python-markdown.rb
@@ -1,8 +1,8 @@
 class PythonMarkdown < Formula
   desc "Python implementation of Markdown"
   homepage "https://pypi.python.org/pypi/Markdown"
-  url "https://pypi.python.org/packages/source/M/Markdown/Markdown-2.6.5.tar.gz"
-  sha256 "8d94cf6273606f76753fcb1324623792b3738c7612c2b180c85cc5e88642e560"
+  url "https://files.pythonhosted.org/packages/29/82/dfe242bcfd9eec0e7bf93a80a8f8d8515a95b980c44f5c0b45606397a423/Markdown-2.6.9.tar.gz"
+  sha256 "73af797238b95768b3a9b6fe6270e250e5c09d988b8e5b223fd5efa4e06faf81"
 
   bottle do
     cellar :any_skip_relocation
