d63603fe190aab3794c117395c0399e0e003f21a
Perry Chou <mbcoguno@users.noreply.github.com>
2017-11-19 21:36:05 +0800

    mame: remove expat dependency
    use system expat instead

--- Formula/mame.rb
+++ Formula/mame.rb
@@ -4,6 +4,7 @@ class Mame < Formula
   url "https://github.com/mamedev/mame/archive/mame0191.tar.gz"
   version "0.191"
   sha256 "e1faed5ee39e977d9a4e60e439c4d36d55a476dbe70a3e8fbe6aadb2a63b1e31"
+  revision 1
   head "https://github.com/mamedev/mame.git"
 
   bottle do
@@ -17,7 +18,6 @@ class Mame < Formula
   depends_on "pkg-config" => :build
   depends_on "sphinx-doc" => :build
   depends_on "sdl2"
-  depends_on "expat"
   depends_on "jpeg"
   depends_on "flac"
   depends_on "sqlite"
