838ebee9b1687887134a042c1a99ee979f6f2ed9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-19 18:31:58 +0000

    nettle: update 3.4 bottle.

--- Formula/nettle.rb
+++ Formula/nettle.rb
@@ -7,10 +7,9 @@ class Nettle < Formula
 
   bottle do
     cellar :any
-    sha256 "70178e6de581d92f1f3458791687aa21676e1a3e94d86eb1733a8765569fd19f" => :high_sierra
-    sha256 "c111158ee75fde15a6b5a0417416f62358dfb0d06fcab0311b1d9d4849df5fa2" => :sierra
-    sha256 "b23a2c67db98f807d240fc581ee87b4eb4284b1eabda8d38e09b8723eb6b4b62" => :el_capitan
-    sha256 "fa2a4eb958c0f9a1ec019264e31c0c98a08c9f204f146458ca62a43e5c3029a0" => :yosemite
+    sha256 "d5f8ed7557a26c0a2a34031b10a34b12c0c8f518782ed1d17fb13930ecfcdf45" => :high_sierra
+    sha256 "d03831c4b2217900338b2316bf73b0074271b0007c2aaaa8fddf606a5f71d7ee" => :sierra
+    sha256 "a8f3221e9f9281d5493e09b9cbbddc7038de24fbb6375e0255294cae822b866a" => :el_capitan
   end
 
   depends_on "gmp"
