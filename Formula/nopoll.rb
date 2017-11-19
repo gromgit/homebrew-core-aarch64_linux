91ebe86dcc6e2580188f21b9f5c6acb9d6c23f56
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2017-11-19 16:53:56 +0100

    nopoll 0.4.5.b375 (new formula)
    Closes #20685.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/nopoll.rb
@@ -0,0 +1,30 @@
+class Nopoll < Formula
+  desc "Open-source C WebSocket toolkit"
+  homepage "https://www.aspl.es/nopoll/"
+  url "https://www.aspl.es/nopoll/downloads/nopoll-0.4.5.b375.tar.gz"
+  version "0.4.5.b375"
+  sha256 "4ecbd277714c9acd154277a46388689f9d651836ca5c69990e35d1eee8c7ceae"
+
+  depends_on "openssl"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <nopoll.h>
+      int main(void) {
+        noPollCtx *ctx = nopoll_ctx_new();
+        nopoll_ctx_unref(ctx);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}/nopoll", "-L#{lib}", "-lnopoll",
+           "-o", "test"
+    system "./test"
+  end
+end
