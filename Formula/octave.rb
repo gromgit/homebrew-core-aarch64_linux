58db90396edbd137f83c6a169046d5ef14ccf1e4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-19 17:58:02 +0000

    octave: update 4.2.1_7 bottle.

--- Formula/octave.rb
+++ Formula/octave.rb
@@ -7,11 +7,9 @@ class Octave < Formula
   revision 7
 
   bottle do
-    rebuild 1
-    sha256 "e4875e85815642672a9c7715283ea9d4c5fbc6d61c63eeb842780a86df38b52b" => :high_sierra
-    sha256 "4adc5c84a3eada5c44a65daabb7fdfefae6a3e492f3ba02b17a9205f942063f8" => :sierra
-    sha256 "06d173b089910c87709f1783b9d8fad8f624e4cbd2bd8cf93d39debd65ca1299" => :el_capitan
-    sha256 "ba00325662c815e50d533ba964051dc17441915f2c4c74944f95e410d0b6099a" => :yosemite
+    sha256 "66d1315c9927d4b4136fa24d6607650477a8ca2b0112610d76f18088afe3902e" => :high_sierra
+    sha256 "ca73f4bd3d64293811221644c75afcc658b68669c7ff5478c2743a05f599b558" => :sierra
+    sha256 "6c551e45beb0c3a3782a1d34d859a6005cad651274a04c95c88ceabd8a099fe9" => :el_capitan
   end
 
   head do
