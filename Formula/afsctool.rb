77810f6ddf3ec92501a79ae468b303685a73e169
flames of love <mechanicofthesequence@gmail.com>
2017-11-20 15:26:49 -0700

    afsctool: fix for 10.13
    Patch file type to allow file type 24 as well.
    See https://github.com/diimdeep/afsctool/issues/3
    
    Modified path in patch to afsctool_34/afsctool.c
    
    Closes #20898.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/afsctool.rb
+++ Formula/afsctool.rb
@@ -4,13 +4,17 @@ class Afsctool < Formula
   url "https://docs.google.com/uc?export=download&id=0BwQlnXqL939ZQjBQNEhRQUo0aUk"
   version "1.6.4"
   sha256 "bb6a84370526af6ec1cee2c1a7199134806e691d1093f4aef060df080cd3866d"
-  revision 1
+  revision 2
 
   # Fixes Sierra "Unable to compress" issue; reported upstream on 24 July 2017
   patch do
     url "https://github.com/vfx01j/afsctool/commit/26293a3809c9ad1db5f9bff9dffaefb8e201a089.diff?full_index=1"
     sha256 "a541526679eb5d2471b3f257dab6103300d950f7b2f9d49bbfeb9f27dfc48542"
   end
+
+  # Fixes High Sierra "Expecting f_type of 17 or 23. f_type is 24" issue
+  # Acknowledged by upstream 12 Apr 2018 https://github.com/Homebrew/homebrew-core/pull/20898#issuecomment-380727547
+  patch :DATA
 
   bottle do
     cellar :any_skip_relocation
@@ -35,3 +39,20 @@ class Afsctool < Formula
     system "#{bin}/afsctool", "-v", path
   end
 end
+
+__END__
+diff --git a/afsctool_34/afsctool.c b/afsctool_34/afsctool.c
+index 8713407fa673f216e69dfc36152c39bc1dea4fe7..7038859f43e035be44c9b8cfbb1bb76a93e26e0a 100644
+--- a/afsctool_34/afsctool.c
++++ b/afsctool_34/afsctool.c
+@@ -104,8 +104,8 @@ void compressFile(const char *inFile, struct stat *inFileInfo, long long int max
+
+	if (statfs(inFile, &fsInfo) < 0)
+		return;
+-	if (fsInfo.f_type != 17 && fsInfo.f_type != 23) {
+-		printf("Expecting f_type of 17 or 23. f_type is %i.\n", fsInfo.f_type);
++	if (fsInfo.f_type != 17 && fsInfo.f_type != 23 && fsInfo.f_type != 24) {
++		printf("Expecting f_type of 17, 23 or 24. f_type is %i.\n", fsInfo.f_type);
+		return;
+	}
+	if (!S_ISREG(inFileInfo->st_mode))
