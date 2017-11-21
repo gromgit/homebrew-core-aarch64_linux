6c2b1b8daa126dc7d934dcd850290dd01c145240
Viktor Szakats <vszakats@users.noreply.github.com>
2017-11-21 19:21:36 +0000

    mspdebug: secure url(s)

--- Formula/mspdebug.rb
+++ Formula/mspdebug.rb
@@ -1,6 +1,6 @@
 class Mspdebug < Formula
   desc "Debugger for use with MSP430 MCUs"
-  homepage "http://dlbeer.co.nz/mspdebug/"
+  homepage "https://dlbeer.co.nz/mspdebug/"
   url "https://github.com/dlbeer/mspdebug/archive/v0.25.tar.gz"
   sha256 "347b5ae5d0ab0cddb54363b72abe482f9f5d6aedb8f230048de0ded28b7d1503"
 
@@ -22,7 +22,7 @@ class Mspdebug < Formula
   def caveats; <<~EOS
     You may need to install a kernel extension if you're having trouble with
     RF2500-like devices such as the TI Launchpad:
-      http://dlbeer.co.nz/mspdebug/faq.html#rf2500_osx
+      https://dlbeer.co.nz/mspdebug/faq.html#rf2500_osx
     EOS
   end
 
