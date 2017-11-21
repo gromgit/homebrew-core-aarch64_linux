8fbfe2875e6ccce150d4fb823c4c5b4554aab6da
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-21 19:08:32 +0000

    docker-ls: add 0.3.1 bottle.

--- Formula/docker-ls.rb
+++ Formula/docker-ls.rb
@@ -5,6 +5,13 @@ class DockerLs < Formula
     :tag => "v0.3.1",
     :revision => "d80310976c9707e261e57ebfa9acf4e0b1781460"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "946e27ec3c35ec19d98c1a8efdf59a809db23926f97d3cd98b195d15c28491cd" => :high_sierra
+    sha256 "1815a65eab86116dfc1de21331352348d59f391cdbc2ed1454a1847f62b21845" => :sierra
+    sha256 "d011c19d2e36dd798ffa5723448cfeb1d95c25b7e78ad4b64207f4c79c5be383" => :el_capitan
+  end
+
   depends_on "go" => :build
 
   def install
