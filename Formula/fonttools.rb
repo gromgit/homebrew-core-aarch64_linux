283dacd176808fba89af2a6093ffa53590957350
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-21 12:08:12 +0000

    fonttools: update 3.20.0 bottle.

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -9,9 +9,9 @@ class Fonttools < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "83d6c9b755887ee7a65483c31431e859e74ff1ce1fcb0f5e36be420371f784aa" => :high_sierra
-    sha256 "f808975b1920bb1da2e9fe1166fc5502d42ca094b77deedfef22e5b68c1194ba" => :sierra
-    sha256 "51c4922da3dfdb44083ef8942aef8266e1b5f9290b94ba413896d50c01ce6159" => :el_capitan
+    sha256 "ec56994692fc19746a48dc7e5a06ca8f354f85b01a0af014a66acfb8216b4610" => :high_sierra
+    sha256 "e142baf768019073315cc978b5fd5f2e601de31b464f36ab6c56b86c7312da36" => :sierra
+    sha256 "c56073b8e6641cf9855341302b71db530bf76655feb1736d8aa45ce7dc2676f1" => :el_capitan
   end
 
   option "with-pygtk", "Build with pygtk support for pyftinspect"
