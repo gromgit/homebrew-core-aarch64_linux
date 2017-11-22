410fd32a5788b25b1d3e09023177fedc0819c0dc
ilovezfs <ilovezfs@icloud.com>
2017-11-22 05:45:43 -0800

    vcdimager: revision for libcdio
    patch for libcdio 1.0 compatibility
    
    Closes #20932.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/vcdimager.rb
+++ Formula/vcdimager.rb
@@ -4,7 +4,7 @@ class Vcdimager < Formula
   url "https://ftp.gnu.org/gnu/vcdimager/vcdimager-0.7.24.tar.gz"
   mirror "https://ftpmirror.gnu.org/vcdimager/vcdimager-0.7.24.tar.gz"
   sha256 "075d7a67353ff3004745da781435698b6bc4a053838d0d4a3ce0516d7d974694"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
@@ -17,6 +17,13 @@ class Vcdimager < Formula
   depends_on "pkg-config" => :build
   depends_on "libcdio"
   depends_on "popt"
+
+  # libcdio 1.0 compat
+  # Reported 24 Nov 2017 https://savannah.gnu.org/support/index.php?109421
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/3ad6c5d/vcdimager/libcdio-1.0-compat.diff"
+    sha256 "65798b9c6070d53957d46b0cac834e1ab9bd4014fab50773ac83b4d1babe00a6"
+  end
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
