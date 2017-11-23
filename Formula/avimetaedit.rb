be832116fe9239d06697dcd007eb57bb61e4d832
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-23 15:33:44 +0000

    avimetaedit: add 1.0.1 bottle.

--- Formula/avimetaedit.rb
+++ Formula/avimetaedit.rb
@@ -5,6 +5,13 @@ class Avimetaedit < Formula
   version "1.0.1"
   sha256 "3aa4ca6aed51c71684f3fd1bd3b4defc2f9e50a8bd0bdf10e55e86c8ba24aafd"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "30a2632fe1d48e4d42fd676dcb16dda6fec0fa7de91026031f0bbe269cec4229" => :high_sierra
+    sha256 "72c069835864beea09634216566cbaeac6f747368abb05aed393081aa91bf74c" => :sierra
+    sha256 "d110ea050c73fe9b397a3e225bd5a41f90fad95c4fc3f5af9e0c25f5aa2f50e7" => :el_capitan
+  end
+
   def install
     cd "Project/GNU/CLI" do
       system "./configure", "--disable-debug",
