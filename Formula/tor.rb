134cc7368af5f6e9f2d4921f2256078bd3f5327e
commitay <commitay@users.noreply.github.com>
2017-11-24 03:00:24 +1000

    tor 0.3.2.5-alpha (devel) (#20949)

--- Formula/tor.rb
+++ Formula/tor.rb
@@ -12,9 +12,9 @@ class Tor < Formula
   end
 
   devel do
-    url "https://tor.eff.org/dist/tor-0.3.2.4-alpha.tar.gz"
-    mirror "https://www.torproject.org/dist/tor-0.3.2.4-alpha.tar.gz"
-    sha256 "c3ea7826b783d4357a624248d8448480f27838ceb3c1bba02a4a0e5d36564fba"
+    url "https://tor.eff.org/dist/tor-0.3.2.5-alpha.tar.gz"
+    mirror "https://www.torproject.org/dist/tor-0.3.2.5-alpha.tar.gz"
+    sha256 "1b61d280310e2f5e1472e567cdae965392902b89babd33e211356a02c28c5c15"
   end
 
   depends_on "pkg-config" => :build
