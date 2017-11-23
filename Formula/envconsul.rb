7b3d28b568e7365198fbbd0a3e7c05067ec3331a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-23 14:08:23 +0000

    envconsul: add 0.7.2 bottle.

--- Formula/envconsul.rb
+++ Formula/envconsul.rb
@@ -4,6 +4,13 @@ class Envconsul < Formula
   url "https://github.com/hashicorp/envconsul/archive/v0.7.2.tar.gz"
   sha256 "c2b2723089f82f7b1623676fda8d378795bf87b4dbc6d4b297e5fc27aeab0aca"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a318db92f566f0d21aea9310394ce038ebc45e3a39e7010388c05e6481108f50" => :high_sierra
+    sha256 "f55d0bad4103ba982bc4d6bc5ef5a8eb82ed253d1d619a1fddb30149454b2baa" => :sierra
+    sha256 "0b3b4bfb1dd3dcf69cc0647ca2fca8864fa316e984dd201913f363dff85dba74" => :el_capitan
+  end
+
   depends_on "go" => :build
   depends_on "consul" => :recommended # only used in test
 
