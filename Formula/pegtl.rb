ca40d6627a6f80bc88204a6d2d6d909d49eccdc4
ilovezfs <ilovezfs@icloud.com>
2017-11-23 01:01:21 -0800

    pegtl 2.2.2 (#20960)

--- Formula/pegtl.rb
+++ Formula/pegtl.rb
@@ -1,8 +1,8 @@
 class Pegtl < Formula
   desc "Parsing Expression Grammar Template Library"
   homepage "https://github.com/taocpp/PEGTL"
-  url "https://github.com/taocpp/PEGTL/archive/2.2.0.tar.gz"
-  sha256 "ac9e4a1fe73e72bfe0c22e1d3cd05bb8ce9cfdfcc6e517bbdd4a17dd9731cac7"
+  url "https://github.com/taocpp/PEGTL/archive/2.2.2.tar.gz"
+  sha256 "303887ad62f8b09118349689d52507b921811f07c581ab7e7336eb1d7145cc3f"
 
   bottle :unneeded
 
