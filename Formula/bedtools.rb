9c57468f8d2bfad2b6a269d99bb747b69c51422b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-23 15:10:23 +0000

    bedtools: add 2.26.0 bottle.

--- Formula/bedtools.rb
+++ Formula/bedtools.rb
@@ -4,6 +4,13 @@ class Bedtools < Formula
   url "https://github.com/arq5x/bedtools2/archive/v2.26.0.tar.gz"
   sha256 "15db784f60a11b104ccbc9f440282e5780e0522b8d55d359a8318a6b61897977"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b1c22d44db6adf632fd5a2b71b21edfb4d2a097871e0609fe447f5b673db4fb1" => :high_sierra
+    sha256 "7d17967daf92bbcaabd2bfec258af5cd7fbd0d5a9918470ba2484189f5fd76bc" => :sierra
+    sha256 "5bd284164362a938830f9bbe1a9c964da513a1819ba9f9fef2a807c528b7e89c" => :el_capitan
+  end
+
   # Remove for > 2.26.0
   # 24 Sep 2017 "strandqueue: de-const for stricter Xcode 9 rules"
   if DevelopmentTools.clang_build_version >= 900
