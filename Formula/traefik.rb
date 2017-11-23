f1e1fa263d5952d7bb4255d2c1e80c296ec3c3df
ilovezfs <ilovezfs@icloud.com>
2017-11-23 10:54:59 -0800

    traefik 1.4.4
    Closes #20984.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/traefik.rb
+++ Formula/traefik.rb
@@ -3,9 +3,9 @@ require "language/go"
 class Traefik < Formula
   desc "Modern reverse proxy"
   homepage "https://traefik.io/"
-  url "https://github.com/containous/traefik/releases/download/v1.4.3/traefik-v1.4.3.src.tar.gz"
-  version "1.4.3"
-  sha256 "8d8b9aeb4cc304c95d43792d70436106b5a9128e95c47e6a3b290305ec43bd46"
+  url "https://github.com/containous/traefik/releases/download/v1.4.4/traefik-v1.4.4.src.tar.gz"
+  version "1.4.4"
+  sha256 "8fb8b986dd6e79a3d474904fe6524cd04b6c3247f1f42c992019532d48049561"
   head "https://github.com/containous/traefik.git"
 
   bottle do
