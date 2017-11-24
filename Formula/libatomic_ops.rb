4fac1749d1c153821c87573f8da7468606f1392e
ilovezfs <ilovezfs@icloud.com>
2017-11-23 23:43:05 -0800

    libatomic_ops 7.4.8
    Closes #20999.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libatomic_ops.rb
+++ Formula/libatomic_ops.rb
@@ -1,8 +1,8 @@
 class LibatomicOps < Formula
   desc "Implementations for atomic memory update operations"
   homepage "https://github.com/ivmai/libatomic_ops/"
-  url "https://github.com/ivmai/libatomic_ops/releases/download/v7.4.6/libatomic_ops-7.4.6.tar.gz"
-  sha256 "96e88ba450ae5fa10aa8e94e6b151a63ffbe47f8069574bd12da22ae80c686db"
+  url "https://github.com/ivmai/libatomic_ops/releases/download/v7.4.8/libatomic_ops-7.4.8.tar.gz"
+  sha256 "c405d5524b118c197499bc311b8ebe18f7fe47fc820f2c2fcefde1753fbd436a"
 
   bottle do
     cellar :any_skip_relocation
