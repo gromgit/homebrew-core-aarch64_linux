b954f5bc4c12d12a98565538c22cec445b2c8fbd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-24 19:22:01 +0000

    libcddb: update 1.3.2_2 bottle.

--- Formula/libcddb.rb
+++ Formula/libcddb.rb
@@ -7,10 +7,9 @@ class Libcddb < Formula
 
   bottle do
     cellar :any
-    sha256 "b1eeef97edb1fb137bf6fe4141c738e0f8a20ca78eb8b73c4e15c9550a4a5a96" => :high_sierra
-    sha256 "472626b30f5859a0e8526e49492f04e3886b1c5acda2605ac4d3e19015085d2b" => :sierra
-    sha256 "4bcb17aa31229692c090622fb31019cee6c6cf2f4936c2ff76e6a957d260449f" => :el_capitan
-    sha256 "31bac3b617b0a046126e6f0cd905e4acf95aa5c3910d3594472bd537c9686b1a" => :yosemite
+    sha256 "ac3157158ee8a29b8fbf0c9bd785c47e74308c0713a9a618054dd9f3e923c037" => :high_sierra
+    sha256 "886b8905d25218094b9a21626a0a7f0b6a20edf9285cdbc0a2e6724e05aac60b" => :sierra
+    sha256 "f4e146e63fc161edab98d70386c32550c1343b98a08f102fb0190e6372a24f7a" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
