9fe3367e415bceb2be9307053b3bca03d8a03ad5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-24 18:05:56 +0000

    plzip: update 1.6 bottle.

--- Formula/plzip.rb
+++ Formula/plzip.rb
@@ -6,11 +6,9 @@ class Plzip < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "64ffaa06460b13a4d53e14ae6ae923805b28bf66a9fdcbf8464ad6ffd6e5329d" => :high_sierra
-    sha256 "8bb211a7a090509a64c331dcdca1bbcd201bc7e1b6a8bcf59cde64637be5d053" => :sierra
-    sha256 "0da26f741281bcf54cd0ae9d443b867bdde9dde340bf83fcabc6c0f22c887016" => :el_capitan
-    sha256 "fe05c1a91ccf2cd262041facaed4fade1ea45b957eabb127070333d79efed3eb" => :yosemite
-    sha256 "3e7f98cc5a785d008c7fa9a9dac0b641240ca268089964730705601744e7ff38" => :mavericks
+    sha256 "f02744640715200d1114550c798c637eb851c8541dc2d204f31f41def197e338" => :high_sierra
+    sha256 "6575ae533b2d6b3c4a575d29e97abf688f2900be61b0f27d722fd098fb3a9b94" => :sierra
+    sha256 "ed2718fc204a2adda79b4e228ba64493225bd7f56edfcd08e300ff4074d570a2" => :el_capitan
   end
 
   depends_on "lzlib"
