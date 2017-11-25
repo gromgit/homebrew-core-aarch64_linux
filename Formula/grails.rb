01dae125105ec27975a410a9011537a1e19c7d44
ilovezfs <ilovezfs@icloud.com>
2017-11-25 01:57:47 -0800

    grails 3.3.2 (#21023)

--- Formula/grails.rb
+++ Formula/grails.rb
@@ -1,8 +1,8 @@
 class Grails < Formula
   desc "Web application framework for the Groovy language"
   homepage "https://grails.org"
-  url "https://github.com/grails/grails-core/releases/download/v3.3.1/grails-3.3.1.zip"
-  sha256 "a8e2b7f0e09de5b4069bad452ede486fb5487033104f2c5133871fc89fc19f51"
+  url "https://github.com/grails/grails-core/releases/download/v3.3.2/grails-3.3.2.zip"
+  sha256 "ed6fba70645d7ead04d8ca48e297b19ec697a24a98030e88df1de5f709c22167"
 
   bottle :unneeded
 
