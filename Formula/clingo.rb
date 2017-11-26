d5400bb9d5471f67e698e5bbaab5316fc3c6f20b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-26 21:08:33 +0000

    clingo: add 5.2.2 bottle.

--- Formula/clingo.rb
+++ Formula/clingo.rb
@@ -4,6 +4,12 @@ class Clingo < Formula
   url "https://github.com/potassco/clingo/archive/v5.2.2.tar.gz"
   sha256 "da1ef8142e75c5a6f23c9403b90d4f40b9f862969ba71e2aaee9a257d058bfcf"
 
+  bottle do
+    sha256 "0f3d85155c09630f853a8e0c96c370ee17b233311d1dcf4f4a26b2e8fd448b31" => :high_sierra
+    sha256 "8485fdbf8ee52b6009b4ae1bebeba6aa35a561f3b0fbd1f784a9bb5d9cc59f55" => :sierra
+    sha256 "d1582e8cce6bec425bb6d04c6c30223203daadb5abf14c62628219d8a569240a" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
   depends_on :python if MacOS.version <= :snow_leopard
