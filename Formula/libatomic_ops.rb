f9dc5be2a17aab0ed1d6b5d471fd65609b745214
Bryce Glover <RandomDSdevel@gmail.com>
2017-11-26 00:25:03 -0500

    libatomic_ops 7.6.0 (devel) (#21040)

--- Formula/libatomic_ops.rb
+++ Formula/libatomic_ops.rb
@@ -11,6 +11,11 @@ class LibatomicOps < Formula
     sha256 "4094450946626f7cd537cb4d821d7da0f8ae85d4dce46f1631799225506b9189" => :el_capitan
   end
 
+  devel do
+    url "https://github.com/ivmai/libatomic_ops/releases/download/v7.6.0/libatomic_ops-7.6.0.tar.gz"
+    sha256 "8e2c06d1d7a05339aae2ddceff7ac54552854c1cbf2bb34c06eca7974476d40f"
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make"
