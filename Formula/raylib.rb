e4c902199045035f86b4b8925518ccb0bef90550
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-26 09:20:03 +0000

    raylib: add 1.9.1-dev bottle.

--- Formula/raylib.rb
+++ Formula/raylib.rb
@@ -6,6 +6,13 @@ class Raylib < Formula
   sha256 "892357fb44d340eb7449c23c425d660d98b34b91434400e7610514ef02698600"
   head "https://github.com/raysan5/raylib.git", :branch => "develop"
 
+  bottle do
+    cellar :any
+    sha256 "f9d6fc5e2f92c15bb6da71df4e0344d9383e803c61a5322f0f202393814f48bb" => :high_sierra
+    sha256 "bd564c34749237dfbc28c490184fd0fc39aab313f19f53fe3d73160dbdaf63e0" => :sierra
+    sha256 "d650d0518043a7a38375d8816d1c13d2dfac3c6e5a3286f9dae379e8451d7ba2" => :el_capitan
+  end
+
   depends_on "cmake" => :build
 
   def install
