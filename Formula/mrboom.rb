74b1555b8cceacd58bd2246c66085b33239c906e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-26 15:31:58 +0000

    mrboom: add 3.9 bottle.

--- Formula/mrboom.rb
+++ Formula/mrboom.rb
@@ -4,6 +4,13 @@ class Mrboom < Formula
   url "https://github.com/Javanaise/mrboom-libretro/archive/3.9.tar.gz"
   sha256 "166593ceed047464650b2de3c377c3cac1c9e3d807abe374e03b901f8ed6ab6e"
 
+  bottle do
+    cellar :any
+    sha256 "c90ac297157ea0ef5b20602c4b72d880c0510c581ec4b27cf933aaece3fc3d73" => :high_sierra
+    sha256 "be0b87327561ee9744c20f82ed7a75e0e009e19772018c1930a195ac8b291fc7" => :sierra
+    sha256 "45f3fa69504692270ad5f7e5bd50b03132b37d558d77a2e0d4bd27f2f6f7d420" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "libmodplug"
   depends_on "minizip"
