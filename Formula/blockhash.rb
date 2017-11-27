be0986d85dd3022e1f35c2ec40348d26ace9d6bd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-27 09:06:02 +0000

    blockhash: update 0.3 bottle.

--- Formula/blockhash.rb
+++ Formula/blockhash.rb
@@ -7,10 +7,9 @@ class Blockhash < Formula
 
   bottle do
     cellar :any
-    sha256 "293664ab10135fca1a4cd16c5f429b080725370eba7e3bb4a7197397baf61b7b" => :high_sierra
-    sha256 "ad5041dd82571cda1572111c458f3119f87d5fd76f73ed183a06b5f9819785e4" => :sierra
-    sha256 "404542e114e192e4666946b6811e0c8c49369fe7ef0d885c572fafd3ba695534" => :el_capitan
-    sha256 "b00df389c1d5b91b13d1908bd9e1f491bd774799892799f5242388052ea7288f" => :yosemite
+    sha256 "2c1bd0b349770ebb96d240bb284bb35bad9d0e218290771fe18e1ec0ea9ad881" => :high_sierra
+    sha256 "6b7be5048231ce278951d52458d4c38a8cdfdd496843bf231d971fe69c0b2811" => :sierra
+    sha256 "52e4284db0b8bffeed973cd2c0bceb52fb6174256425a4fe689f2eba64a5b079" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
