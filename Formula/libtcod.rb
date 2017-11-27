21f8b43964bf267d586870998afb53e6aeab4669
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-27 08:57:57 +0000

    libtcod: update 1.6.4 bottle.

--- Formula/libtcod.rb
+++ Formula/libtcod.rb
@@ -6,9 +6,9 @@ class Libtcod < Formula
 
   bottle do
     cellar :any
-    sha256 "36ed32488096eede62f9ff1416feb35a0ea2c32df441218e657041bc8e888c58" => :high_sierra
-    sha256 "04348552c6e7ea7b3d4f9093fa6f6c063dfa6965bcdc39c7c8ff86a3eadf8ac1" => :sierra
-    sha256 "67f9bbe96cb955bccaacfe12c2d13705af17a107f03777a43e0b7b784aff1616" => :el_capitan
+    sha256 "c9e64cf1266592a440401c31028f97daf076d8bb9ace113a19da1b9c6a01b4bc" => :high_sierra
+    sha256 "360839e84034f149f3538be489273274a7db2f3b9020162dc94b9798e31dc402" => :sierra
+    sha256 "8728af3c1c018e2586708b66fdf8578260a83c875602ff6e796f96f44ea0382e" => :el_capitan
   end
 
   depends_on "autoconf" => :build
