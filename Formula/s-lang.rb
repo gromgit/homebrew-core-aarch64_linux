52ab93b2957c158a72ddd0938fc72a8c9a004ff3
Viktor Szakats <vszakats@users.noreply.github.com>
2017-11-28 11:29:09 +0000

    s-lang: secure url(s)

--- Formula/s-lang.rb
+++ Formula/s-lang.rb
@@ -1,7 +1,7 @@
 class SLang < Formula
   desc "Library for creating multi-platform software"
-  homepage "http://www.jedsoft.org/slang/"
-  url "http://www.jedsoft.org/releases/slang/slang-2.3.1a.tar.bz2"
+  homepage "https://www.jedsoft.org/slang/"
+  url "https://www.jedsoft.org/releases/slang/slang-2.3.1a.tar.bz2"
   mirror "http://pkgs.fedoraproject.org/repo/pkgs/slang/slang-2.3.1a.tar.bz2/sha512/e7236a189081ebcbaf4e7f0506671226a4d46aede8826e1a558f1a2f57bcbe3ad58eadeabe2df99cd3d8bacb4c93749996bcbce4f51d338fc9396e0f945933e7/slang-2.3.1a.tar.bz2"
   sha256 "54f0c3007fde918039c058965dffdfd6c5aec0bad0f4227192cc486021f08c36"
 
