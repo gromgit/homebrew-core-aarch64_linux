4f0620403dc0ecfb9845eed2dd951b1752848dd1
Jacob Bednarz <jacob.bednarz@gmail.com>
2017-11-29 15:43:17 +1100

    stress-ng 0.09.06 (new formula)
    Closes #21153.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/stress-ng.rb
@@ -0,0 +1,19 @@
+class StressNg < Formula
+  desc "Stress test a computer system in various selectable ways"
+  homepage "http://kernel.ubuntu.com/~cking/stress-ng/"
+  url "http://kernel.ubuntu.com/~cking/tarballs/stress-ng/stress-ng-0.09.06.tar.xz"
+  sha256 "59c09c7cccb34222dc124ce8622a0ac73fe9391ba16a4a561267ca55c02f2520"
+
+  depends_on :macos => :sierra
+
+  def install
+    inreplace "Makefile", "/usr", prefix
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    output = shell_output("#{bin}/stress-ng -c 1 -t 1 2>&1")
+    assert_match "successful run completed", output
+  end
+end
