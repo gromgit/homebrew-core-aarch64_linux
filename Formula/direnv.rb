d45fdd4f05f0229bc24254adae4227232cb155c2
ilovezfs <ilovezfs@icloud.com>
2017-11-28 20:42:01 -0800

    direnv 2.13.2
    Closes #21152.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/direnv.rb
+++ Formula/direnv.rb
@@ -1,8 +1,8 @@
 class Direnv < Formula
   desc "Load/unload environment variables based on $PWD"
   homepage "https://direnv.net/"
-  url "https://github.com/direnv/direnv/archive/v2.13.1.tar.gz"
-  sha256 "eea1d4eb4c95c1a6d41bb05a35ed0e106d497f10761e5d0e1c3b87d07c70c7b4"
+  url "https://github.com/direnv/direnv/archive/v2.13.2.tar.gz"
+  sha256 "04fdbd3fe7ddf496a3da41cb6e767100d8d6f6b52fef9e2217c9e330b0e6257d"
   head "https://github.com/direnv/direnv.git"
 
   bottle do
