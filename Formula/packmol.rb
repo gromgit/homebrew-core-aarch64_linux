a030abaa9ab383252fcf366174a10c451fea8040
ilovezfs <ilovezfs@icloud.com>
2017-11-28 20:37:23 -0800

    packmol 17.332
    Closes #21146.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/packmol.rb
+++ Formula/packmol.rb
@@ -1,8 +1,8 @@
 class Packmol < Formula
   desc "Packing optimization for molecular dynamics simulations"
   homepage "https://www.ime.unicamp.br/~martinez/packmol/"
-  url "https://github.com/leandromartinez98/packmol/archive/17.221.tar.gz"
-  sha256 "b5dcdeb694ffc17f620a4517e4eba67018145774d6fa06c4076bfbfe79400407"
+  url "https://github.com/leandromartinez98/packmol/archive/17.332.tar.gz"
+  sha256 "f7489327a8fe3e9eedd0630faf17e69777a9bf7f17190df48d3ea6ca641cd0ae"
   head "https://github.com/leandromartinez98/packmol.git"
 
   bottle do
