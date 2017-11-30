406738b229bf2e0b9a426ff66e1d304c0ca1fce5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-30 01:30:57 +0000

    bazaar: update 2.7.0_1 bottle.

--- Formula/bazaar.rb
+++ Formula/bazaar.rb
@@ -7,11 +7,9 @@ class Bazaar < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "184ea0c9b6dd3cb287257070325188828c669ecdf5f231b477182e9d616357cc" => :high_sierra
-    sha256 "506f81fddaadfb97bf1e923e16374d3bc6fba066e94d6cf4b0feadbc7bcf91a7" => :sierra
-    sha256 "75e4a4aa57887371b783839667da3a142805c60f13197d599f6c8ef95c104707" => :el_capitan
-    sha256 "7919335ffdd76bd54a3b58da3e7d9499f8bc6dd895b1d117390bb19ceaf08818" => :yosemite
-    sha256 "51a058a4ff6b070a62ce30d01a673bba74d65d4eab6a13a9d53696f24cceb80d" => :mavericks
+    sha256 "102d9ec9fbed0c83402195d77f20bbcf94210face42bcddf98c3feda6c9587f7" => :high_sierra
+    sha256 "4a927398b0b1fc8ce43e16a227f40c63409a752338fd36600d05b907d960cab5" => :sierra
+    sha256 "6d1409dc49d838c0209bc59ebeb4ec5b70c5c1caef3b27b97f0ebedd6d8ff515" => :el_capitan
   end
 
   # CVE-2017-14176
