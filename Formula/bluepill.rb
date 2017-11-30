c740eba675f665946c4af57f9fef2c1cae07c8a7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-30 05:01:04 +0000

    bluepill: update 2.1.0 bottle.

--- Formula/bluepill.rb
+++ Formula/bluepill.rb
@@ -7,8 +7,8 @@ class Bluepill < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3e67c31f8dfe739788e7078e5220e91ebfd956a290803c8e62167ad26d8ad909" => :high_sierra
-    sha256 "449c426e4d287ba952845568f79f1df59d7c21c6bf85716f745a68db5ff24fe1" => :sierra
+    sha256 "980d5a25230ceafedcbf1b6e6c0dc20f72bbd1db33fb1e0c68a4f4c4718ed485" => :high_sierra
+    sha256 "d5b0ea149d04431fddb399585d57bcd09faaa33572a4a5803ec2ac6a1cc29185" => :sierra
   end
 
   depends_on :xcode => ["9.0", :build]
