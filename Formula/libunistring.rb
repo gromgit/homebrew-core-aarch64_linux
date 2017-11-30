b406a7cee1391565e3f6c483150c679752e0c8e2
ilovezfs <ilovezfs@icloud.com>
2017-11-30 03:54:38 -0800

    libunistring 0.9.8
    Closes #21199.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libunistring.rb
+++ Formula/libunistring.rb
@@ -1,9 +1,9 @@
 class Libunistring < Formula
   desc "C string library for manipulating Unicode strings"
   homepage "https://www.gnu.org/software/libunistring/"
-  url "https://ftp.gnu.org/gnu/libunistring/libunistring-0.9.7.tar.xz"
-  mirror "https://ftpmirror.gnu.org/libunistring/libunistring-0.9.7.tar.xz"
-  sha256 "2e3764512aaf2ce598af5a38818c0ea23dedf1ff5460070d1b6cee5c3336e797"
+  url "https://ftp.gnu.org/gnu/libunistring/libunistring-0.9.8.tar.xz"
+  mirror "https://ftpmirror.gnu.org/libunistring/libunistring-0.9.8.tar.xz"
+  sha256 "7b9338cf52706facb2e18587dceda2fbc4a2a3519efa1e15a3f2a68193942f80"
 
   bottle do
     cellar :any
@@ -11,15 +11,6 @@ class Libunistring < Formula
     sha256 "d82c6b7c72707aa04eb00bd3e6a4a995ef830b41b02271111ee6006585eaca80" => :sierra
     sha256 "c80c64fdd7d05bf0e387b3286238e1740e7989098ba6bde403151a1c14d57812" => :el_capitan
     sha256 "e2143b25bf7bdc85ddb00b065cf1f72c665d77a6737563cd81a88420bc72e51f" => :yosemite
-  end
-
-  # Fix crash from usage of %n in dynamic format strings on High Sierra
-  # Repurposed patch, credit to Jeremy Huddleston Sequoia <jeremyhu@apple.com>
-  if MacOS.version >= :high_sierra
-    patch :p0 do
-      url "https://raw.githubusercontent.com/macports/macports-ports/edf0ee1e2cf/devel/m4/files/secure_snprintf.patch"
-      sha256 "57f972940a10d448efbd3d5ba46e65979ae4eea93681a85e1d998060b356e0d2"
-    end
   end
 
   def install
