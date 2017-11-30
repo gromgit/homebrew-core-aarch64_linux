42f3f75114e7a6e3a3236423c0cfed3b7ac78ae7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-30 13:23:22 +0000

    libunistring: update 0.9.8 bottle.

--- Formula/libunistring.rb
+++ Formula/libunistring.rb
@@ -7,10 +7,9 @@ class Libunistring < Formula
 
   bottle do
     cellar :any
-    sha256 "ab87b179bc5ac0d54cbf003d62fdae4e0924e7cf5e1dba67af70fdd65668f5cd" => :high_sierra
-    sha256 "d82c6b7c72707aa04eb00bd3e6a4a995ef830b41b02271111ee6006585eaca80" => :sierra
-    sha256 "c80c64fdd7d05bf0e387b3286238e1740e7989098ba6bde403151a1c14d57812" => :el_capitan
-    sha256 "e2143b25bf7bdc85ddb00b065cf1f72c665d77a6737563cd81a88420bc72e51f" => :yosemite
+    sha256 "ccc4789699aed620a7f961549e9888a77df38e608cfcff60b5d59b292b9807dc" => :high_sierra
+    sha256 "b685813e92dbd3a4eedcaac4b1a87ee3aae00a2dd4cf49cfc8d03cde079fac16" => :sierra
+    sha256 "ef72051cb81989e396175cbbf82cde4c62bf8dbb1ef3028c4658af6402ad6133" => :el_capitan
   end
 
   def install
