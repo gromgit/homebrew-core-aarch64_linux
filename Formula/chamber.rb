669fe4e52ffa8d13a706306633d94217b46ec426
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-02 01:41:25 +0000

    chamber: add 1.9.0 bottle.

--- Formula/chamber.rb
+++ Formula/chamber.rb
@@ -5,6 +5,13 @@ class Chamber < Formula
   sha256 "098568ddda04956b8dc270aa8e6b4eb83d8cfe92add8ea0172715eab7db050cb"
   head "https://github.com/segmentio/chamber.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "acecb58aaaadd6887644359150a8ac8ed094c69b3e7d8025a5381f4eb3c0471b" => :high_sierra
+    sha256 "ec834deb9e30e9cbe648dea1fec3eeade4cbb51cb45247be49827438984cc6f4" => :sierra
+    sha256 "5f481ee9d65657e9df8426f2d1e8f78195120fdbf7db7b9a0f7f3971815d9530" => :el_capitan
+  end
+
   depends_on "go" => :build
   depends_on "govendor" => :build
 
