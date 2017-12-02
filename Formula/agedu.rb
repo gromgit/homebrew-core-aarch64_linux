20fd21462b56ae4e7d806fc5d0d2182e5e65e019
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-02 12:03:12 +0000

    agedu: update 20171202 bottle.

--- Formula/agedu.rb
+++ Formula/agedu.rb
@@ -8,10 +8,9 @@ class Agedu < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "499d9876e961a92695f103d7e460c3429801028e54a278628ddd26a47da45396" => :high_sierra
-    sha256 "37e6846a31a0b84412e81f5da9d08e8a3a2f4cf47232318be0ef7dee26a8928b" => :sierra
-    sha256 "7e4559ad6310ba5a8503c08a148eabfacc5622a7ee23ab45bc061fe4e3ef830d" => :el_capitan
-    sha256 "3aefc8595aeef045386d807d53a07f80e1f1fd3955f2caa217e052aadc6c9b21" => :yosemite
+    sha256 "ffc34134229c14b1fe7d65fa01c90724715228738aecfef82cdb8f79be1550ef" => :high_sierra
+    sha256 "05d4afaee3fa14b005a7a4bab7c1c13b6d704b834fdd2f0ff63bb22721f1cb24" => :sierra
+    sha256 "ccba7a5902c861069e5bc82e565c34857cffaf98c408145d2b3917d23319bc61" => :el_capitan
   end
 
   depends_on "autoconf" => :build
