07bf0c93affced37c53eacbfc26f867b6f429404
ilovezfs <ilovezfs@icloud.com>
2017-12-03 08:40:07 -0800

    vowpal-wabbit 8.5.0
    Closes #21299.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/vowpal-wabbit.rb
+++ Formula/vowpal-wabbit.rb
@@ -1,9 +1,8 @@
 class VowpalWabbit < Formula
   desc "Online learning algorithm"
   homepage "https://github.com/JohnLangford/vowpal_wabbit"
-  url "https://github.com/JohnLangford/vowpal_wabbit/archive/8.4.0.tar.gz"
-  sha256 "8624c33fd516e8b738fbbc6fc49f62ac03f97af183dbecff298162b4bf5736d5"
-  revision 2
+  url "https://github.com/JohnLangford/vowpal_wabbit/archive/8.5.0.tar.gz"
+  sha256 "f90167312b0e12e85331e4fdd790268eab508c2a59764ae164bacc7cd6149732"
 
   bottle do
     cellar :any
@@ -23,14 +22,6 @@ class VowpalWabbit < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
   needs :cxx11
-
-  # Remove for > 8.4.0
-  # Fix "error: no member named 'codecvt_utf8_utf16' in namespace 'std'"
-  # Upstream commit from 22 Jul 2017 "make codecvt a universal include"
-  patch do
-    url "https://github.com/JohnLangford/vowpal_wabbit/commit/c632a1e09.patch?full_index=1"
-    sha256 "d7b7b3e594ad5ed4ee8c2a664ab98ec650df271d821b93842c1763c220459fe7"
-  end
 
   def install
     ENV.cxx11
