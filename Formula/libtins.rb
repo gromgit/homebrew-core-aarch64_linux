0c12eb8f5e21c6620113273111df0f9bd43dd7cd
Matias Fontanini <matias.fontanini@gmail.com>
2017-12-03 17:10:37 -0800

    libtins 4.0
    Closes #21301.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libtins.rb
+++ Formula/libtins.rb
@@ -1,8 +1,8 @@
 class Libtins < Formula
   desc "C++ network packet sniffing and crafting library"
   homepage "https://libtins.github.io/"
-  url "https://github.com/mfontanini/libtins/archive/v3.5.tar.gz"
-  sha256 "1b0624b2eea3ce077a86f3abd3e625661760c4cfd21cd8f3d3cd3622229ff2cd"
+  url "https://github.com/mfontanini/libtins/archive/v4.0.tar.gz"
+  sha256 "2a758d1bed51760bbd57fcaa00610534e0cc3a6d55d91983724e5f46739d66b8"
   head "https://github.com/mfontanini/libtins.git"
 
   bottle do
