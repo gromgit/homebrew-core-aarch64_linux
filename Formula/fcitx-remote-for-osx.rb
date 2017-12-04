70e40d89b8402335967b2fce0846f7aeb5907f37
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-04 07:48:09 +0000

    fcitx-remote-for-osx: update 0.3.0 bottle.

--- Formula/fcitx-remote-for-osx.rb
+++ Formula/fcitx-remote-for-osx.rb
@@ -6,11 +6,9 @@ class FcitxRemoteForOsx < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "23ca3b8325b509ccf9991009a7d34e3afff5a4a2f12d054d0034dc950ada1f05" => :high_sierra
-    sha256 "138ca03de1639465bb99bdac40a33fc57d21a53c53b25791493c8cbbd6675eab" => :sierra
-    sha256 "5dd74e46ad011a623dedf189e7e63c7ae5b037e60f728b5aac9389bf20628d63" => :el_capitan
-    sha256 "1a400d131adec21aea258c11c83038ac72ee6fe6cd0bc237af0f3238d5459984" => :yosemite
-    sha256 "7e64b4eb71352b65cb227017068c2a9ec22708a9ca612228cfafd8208b3e1fe9" => :mavericks
+    sha256 "d0d5e296210ad94fb80bdc8e8058a2ad32ba293f3a1d9bb54ab2dd2de573e5f8" => :high_sierra
+    sha256 "91611184e35f77a587eb1c36042660809564ff3fbae553d492b036a8b64b4f56" => :sierra
+    sha256 "8e54fa21fce7e9e363dba2a47f462d1b415d55e9687322d251e2e5995442599a" => :el_capitan
   end
 
   option "with-input-method=", "Select input method: general(default), baidu-pinyin, baidu-wubi, sogou-pinyin, qq-wubi, squirrel-rime, squirrel-rime-upstream, osx-pinyin"
