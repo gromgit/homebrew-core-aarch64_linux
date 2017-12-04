ff55f6649d0dbd64d081786f8b351e1f4bdf234d
Johannes Würbach <johannes.wuerbach@googlemail.com>
2017-12-04 09:57:25 +0100

    kops 1.8.0
    Closes #21305.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/kops.rb
+++ Formula/kops.rb
@@ -1,8 +1,8 @@
 class Kops < Formula
   desc "Production Grade K8s Installation, Upgrades, and Management"
   homepage "https://github.com/kubernetes/kops"
-  url "https://github.com/kubernetes/kops/archive/1.7.1.tar.gz"
-  sha256 "044c5c7a737ed3acf53517e64bb27d3da8f7517d2914df89efeeaf84bc8a722a"
+  url "https://github.com/kubernetes/kops/archive/1.8.0.tar.gz"
+  sha256 "dfab4c304247723d02cdabc83840ff0fbfd8ae4238d248839b06cd62f84400d7"
   head "https://github.com/kubernetes/kops.git"
 
   bottle do
@@ -11,11 +11,6 @@ class Kops < Formula
     sha256 "6a027731d8100218a3527395037d27f975b34cde7fe9759f9c1160e71bec2357" => :high_sierra
     sha256 "c3033753fbc5ce230279dd08982db353cb50b5338a7e34e2ccc389ab1acc9c07" => :sierra
     sha256 "189a6f76f16e9df99d24a86803dfde236cd1408ad1567c7e80b565babce8a707" => :el_capitan
-  end
-
-  devel do
-    url "https://github.com/kubernetes/kops/archive/1.8.0.tar.gz"
-    sha256 "dfab4c304247723d02cdabc83840ff0fbfd8ae4238d248839b06cd62f84400d7"
   end
 
   depends_on "go" => :build
