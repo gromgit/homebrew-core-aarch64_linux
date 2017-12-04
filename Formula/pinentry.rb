7378abe0a0afad22246a0952ffb5f8469ba2365b
chdiza <chdiza@gmail.com>
2017-12-04 15:29:08 -0500

    pinentry 1.1.0
    Also, for safety's sake disable the experimental, new, non-macOS
    GUI versions.
    
    Closes #21328.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pinentry.rb
+++ Formula/pinentry.rb
@@ -1,9 +1,9 @@
 class Pinentry < Formula
   desc "Passphrase entry dialog utilizing the Assuan protocol"
   homepage "https://www.gnupg.org/related_software/pinentry/"
-  url "https://www.gnupg.org/ftp/gcrypt/pinentry/pinentry-1.0.0.tar.bz2"
-  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/pinentry/pinentry-1.0.0.tar.bz2"
-  sha256 "1672c2edc1feb036075b187c0773787b2afd0544f55025c645a71b4c2f79275a"
+  url "https://www.gnupg.org/ftp/gcrypt/pinentry/pinentry-1.1.0.tar.bz2"
+  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/pinentry/pinentry-1.1.0.tar.bz2"
+  sha256 "68076686fa724a290ea49cdf0d1c0c1500907d1b759a3bcbfbec0293e8f56570"
 
   bottle do
     cellar :any
@@ -26,6 +26,8 @@ class Pinentry < Formula
       --disable-pinentry-qt
       --disable-pinentry-qt5
       --disable-pinentry-gnome3
+      --disable-pinentry-tqt
+      --disable-pinentry-fltk
     ]
 
     args << "--disable-pinentry-gtk2" if build.without? "gtk+"
