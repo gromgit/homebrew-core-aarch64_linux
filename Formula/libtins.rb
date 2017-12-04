e91b71bb7dbc8baf0e4fc78f6a985ea2c0f99b09
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-04 01:18:07 +0000

    libtins: update 4.0 bottle.

--- Formula/libtins.rb
+++ Formula/libtins.rb
@@ -7,10 +7,9 @@ class Libtins < Formula
 
   bottle do
     cellar :any
-    sha256 "36413e6fccbe2d790e224c4673f103a350973bf1832d4e241faa13e5ac7c998f" => :high_sierra
-    sha256 "01be883bfd9b9c2d11c26b47b768bdc22ff697d571d6369587298ccaa9b8ffb2" => :sierra
-    sha256 "b86f82447dcdb290967bcdb7728eaad65c418819418689bcaedee9f682bbec3b" => :el_capitan
-    sha256 "9a42acca6942a7414fd8625f9519f20e1c56cf465b13ddbf69da929c3d728187" => :yosemite
+    sha256 "54376ef52c7368bdf496274148185c317d5afd99d11504aece81ca4468a2140a" => :high_sierra
+    sha256 "fc4ff68bca0257330b92d22c2219066ec3f6a7fe3561649b088c9846ef7cf03e" => :sierra
+    sha256 "0989cb076d13ab1d40a0a581ab56f0891a469de25219677a1f9323b89e728f2d" => :el_capitan
   end
 
   option :cxx11
