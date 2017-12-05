e7be505122edc3a1c544ed95948c39f9a70fa696
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-05 22:37:36 +0000

    libmspub: update 0.1.2_6 bottle.

--- Formula/libmspub.rb
+++ Formula/libmspub.rb
@@ -7,10 +7,9 @@ class Libmspub < Formula
 
   bottle do
     cellar :any
-    sha256 "bad09b3cc4ebdabd4e2160683d97878d3568f67625c682fb2a50084f0e66b5a6" => :high_sierra
-    sha256 "667954cf7d852f4db33db4f1bb0cc38a620e5450bae1fc278da95ba2d0f51f2e" => :sierra
-    sha256 "1d40e6c8b2f62691dca7761ecb97b15a3bce8f3e8638b3df05057e61d063cfc7" => :el_capitan
-    sha256 "6e9c31fdf773518e58be86f562f1a39dde473c29c5fcdb0263fec08bf64f22a7" => :yosemite
+    sha256 "e55dcb9c756ddc685cc19803fd44f6d36933733aa437b93b73e49e1b2078ed73" => :high_sierra
+    sha256 "8d56db2b965ac02c90bb4d77497e3e4e3bf3a19ab353a40ad959604d8c7ec737" => :sierra
+    sha256 "f7c2fdc69621a94f1e5d21229fa5a608e6cdbac06135dedea31218f45476d89b" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
