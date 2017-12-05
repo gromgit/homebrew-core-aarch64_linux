77e44c45af75e4d4da958021c5b76ea6695d74a9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-05 22:27:11 +0000

    s-nail: update 14.9.6 bottle.

--- Formula/s-nail.rb
+++ Formula/s-nail.rb
@@ -5,9 +5,9 @@ class SNail < Formula
   sha256 "7f33452fa2848bae95fb7ab92a98e11315057104c5484626245316f5f5806acb"
 
   bottle do
-    sha256 "34faabb4bd4beafc0a10f053f56d4f90951b03b49577eef73c1688ec36fb1898" => :high_sierra
-    sha256 "b1bf20bfd8cccdc3f02cc0f90287ef68853ab3cb57a1467bfb6d6e9e686caf90" => :sierra
-    sha256 "639151574bbec202971224119ad523d81cf008afd00bee021ae181ea7b5b6c61" => :el_capitan
+    sha256 "758115888fe3d5451626401cf8dd9d6d11fc8397b33c82c8221b30fb4d21601e" => :high_sierra
+    sha256 "bf7f18df0e2834026b64edbb9d542d0884e5ac4179e2b23e96b93c0cf778696e" => :sierra
+    sha256 "c707a57fdff1f46ceadb84dfc5b7f7142eaf529e0fa2d0272b298d7b5f3d21f8" => :el_capitan
   end
 
   depends_on "libidn"
