a7469e594f7c17e900c4ef840b231450d3e7f213
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-05 21:25:43 +0000

    tasksh: update 1.2.0 bottle.

--- Formula/tasksh.rb
+++ Formula/tasksh.rb
@@ -7,10 +7,9 @@ class Tasksh < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "cc9cf043fcf84c05be38708d6217fe90836380075269b3ecebee12dc5d0096db" => :high_sierra
-    sha256 "f4fbdfe7e82a50bce8b9bb9b1455ed4928ab32ebfbb185d5bb6bba1e6dacd1c0" => :sierra
-    sha256 "0daecc2a8478e84ee4e84dc7bbca8888341ab021fa5f9379416862c28c2d1eb4" => :el_capitan
-    sha256 "09895c7c1a3da30eb510fe971854af62d31dcd57c08dd8f60a4556b9273ad237" => :yosemite
+    sha256 "17f99be8d0aea4c43877fbfb121b7989047f04650ccffa85e948859e8ee51e0b" => :high_sierra
+    sha256 "fd1b333c777401c53d5ddc8aaf52150a2a15fea4230a91d457d9d99ce2819ee7" => :sierra
+    sha256 "d695adcf10582123053612d98ed4bf988b22c50919598bf167bc2e62db142352" => :el_capitan
   end
 
   depends_on "cmake" => :build
