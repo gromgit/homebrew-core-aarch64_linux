8a4d6cd1b0343ca09165f66d562db20cfd3632ca
ilovezfs <ilovezfs@icloud.com>
2017-12-05 09:06:39 -0800

    mpd: improve test reliability (#21352)
    The test would intermittently fail due to an insufficient sleep.
    Hopefully, an additional second will be enough.

--- Formula/mpd.rb
+++ Formula/mpd.rb
@@ -145,7 +145,7 @@ class Mpd < Formula
 
     begin
       assert_match "OK MPD", shell_output("curl localhost:6600")
-      assert_match "ACK", shell_output("(sleep 1; echo playid foo) | nc localhost 6600")
+      assert_match "ACK", shell_output("(sleep 2; echo playid foo) | nc localhost 6600")
     ensure
       Process.kill "SIGINT", pid
       Process.wait pid
