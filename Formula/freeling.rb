6af450125cad13926b20ad000ff26eaf2bc659fd
ilovezfs <ilovezfs@icloud.com>
2017-12-05 00:26:21 -0800

    freeling: revision for icu4c

--- Formula/freeling.rb
+++ Formula/freeling.rb
@@ -3,7 +3,7 @@ class Freeling < Formula
   homepage "http://nlp.lsi.upc.edu/freeling/"
   url "https://github.com/TALP-UPC/FreeLing/releases/download/4.0/FreeLing-4.0.tar.gz"
   sha256 "c79d21c5af215105ba16eb69ee75b589bf7d41abce86feaa40757513e33c6ecf"
-  revision 6
+  revision 7
 
   bottle do
     sha256 "50eaf967e34770372cc6a948c6dbf03848bba0909e9dcbd2905115dbc5649b61" => :high_sierra
