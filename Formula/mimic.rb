8afeae6c347c9bdefc3d1e7b82cdb10eaf64c389
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-05 22:37:35 +0000

    mimic: update 1.2.0.2_1 bottle.

--- Formula/mimic.rb
+++ Formula/mimic.rb
@@ -7,10 +7,9 @@ class Mimic < Formula
 
   bottle do
     cellar :any
-    sha256 "df5c5b4e95e7e0f99b77167e8ec09f50e62021b3195dfb0c8b84a8f8aecb1ce9" => :high_sierra
-    sha256 "cb00a072b0730aa23c82d8f3b6208f5e20fdec69e557d2fe669ecbacc98dc3c3" => :sierra
-    sha256 "129d1b1e63b7d5ee583fb955286272297b4f159e74c746e319ffaf94aead0951" => :el_capitan
-    sha256 "ae14c6662d7241dc75258eac52090b09bd0a69f7bfc6465f47b040930f184afd" => :yosemite
+    sha256 "09d3e611608f56c7a64e4a072b5a0de0513310bc99127c84db1c82250968511c" => :high_sierra
+    sha256 "97afa7f2c1e7af748bdcc594cd381523dda0ebcba1571e4cef3c6f8325cd01cc" => :sierra
+    sha256 "152fe1ffcd399e1fe78021df4d4213ddaa2c47c603471271796283c6c85005d4" => :el_capitan
   end
 
   depends_on "autoconf" => :build
