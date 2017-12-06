7912b8aa8047db6f4be3ba3581ef6f6c95c3635f
ilovezfs <ilovezfs@icloud.com>
2017-12-06 06:38:15 -0800

    ydcv 0.5 (#21382)

--- Formula/ydcv.rb
+++ Formula/ydcv.rb
@@ -1,15 +1,15 @@
 class Ydcv < Formula
   desc "YouDao Console Version"
   homepage "https://github.com/felixonmars/ydcv"
-  url "https://github.com/felixonmars/ydcv/archive/0.4.tar.gz"
-  sha256 "2d9f6309bbf2d35c0c34c5ee945cf40769cc8201e6f374fa2a4f2d4b827fbdbb"
+  url "https://github.com/felixonmars/ydcv/archive/0.5.tar.gz"
+  sha256 "8f030c15d241ff317381cf97bacf3140080e34c1a4c4c685265dfdb05c8b8a10"
 
   bottle :unneeded
 
   depends_on :python if MacOS.version <= :snow_leopard
 
   def install
-    bin.install "ydcv.py" => "ydcv"
+    bin.install "src/ydcv.py" => "ydcv"
     zsh_completion.install "contrib/zsh_completion" => "_ydcv"
   end
 
