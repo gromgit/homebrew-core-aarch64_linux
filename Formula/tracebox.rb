5ce66efa294b6406e2cb31e7b66cdaefdd8fd528
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-06 11:40:00 +0000

    tracebox: update 0.4.2_1 bottle.

--- Formula/tracebox.rb
+++ Formula/tracebox.rb
@@ -20,9 +20,9 @@ class Tracebox < Formula
 
   bottle do
     cellar :any
-    sha256 "0ec57d1c7ba9701a394fda6f9e87c328673833fab0f72060e297716f55c7c67e" => :high_sierra
-    sha256 "9b42586adbd0b639e3722c9dd43f67d350b36cbab148aca454dfd57bc01c457f" => :sierra
-    sha256 "dfc1dd387e7b947007439897e55b2ab2c77270be143894dd90d8fb6d540019d2" => :el_capitan
+    sha256 "a48ec11757598455e6eff5603a5a0e6f4d2497efd4ebd0ba37829e148aa9d8df" => :high_sierra
+    sha256 "43bae78b21fc5a839c805004986b1ffd2e0ac8f5589f2d53990874874d1d7251" => :sierra
+    sha256 "887104507eb7457f644f78143758b4c570877e10d8a9a430d165911c6692febf" => :el_capitan
   end
 
   needs :cxx11
