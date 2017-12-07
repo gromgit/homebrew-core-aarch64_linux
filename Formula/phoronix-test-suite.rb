99b8ea3594d1f1f78b0fff1fd8ca7d782aa07e13
ilovezfs <ilovezfs@icloud.com>
2017-12-07 09:59:39 -0800

    phoronix-test-suite 7.6.0 (#21434)
    clean up usr/ and etc/ from the rack

--- Formula/phoronix-test-suite.rb
+++ Formula/phoronix-test-suite.rb
@@ -1,17 +1,24 @@
 class PhoronixTestSuite < Formula
   desc "Open-source automated testing/benchmarking software"
   homepage "https://www.phoronix-test-suite.com/"
-  url "https://github.com/phoronix-test-suite/phoronix-test-suite/archive/v7.4.0.tar.gz"
-  sha256 "beb0875ca74b62a5bb0768c337baceb3415817a6ef487dd14289ab2d84b6c504"
+  url "https://github.com/phoronix-test-suite/phoronix-test-suite/archive/v7.6.0.tar.gz"
+  sha256 "1bc1ffed0fbc11be0869affd72c5c1833dd8da16e2023e2e68baab3c2fa62cb5"
 
   bottle :unneeded
 
   def install
+    ENV["DESTDIR"] = buildpath/"dest"
     system "./install-sh", prefix
-    bash_completion.install "./pts-core/static/bash_completion"
+    prefix.install (buildpath/"dest/#{prefix}").children
+    bash_completion.install "dest/#{prefix}/../etc/bash_completion.d/phoronix-test-suite"
+  end
+
+  # 7.4.0 installed files in the formula's rack so clean up the mess.
+  def post_install
+    rm_rf [prefix/"../etc", prefix/"../usr"]
   end
 
   test do
-    assert_match "Tynset", shell_output("#{bin}/phoronix-test-suite version | grep -v ^$")
+    assert_match version.to_s, shell_output("#{bin}/phoronix-test-suite version")
   end
 end
