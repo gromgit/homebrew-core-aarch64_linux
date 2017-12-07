c17f5db89d1246695b80a8a5fd2a89d6b5819f84
ilovezfs <ilovezfs@icloud.com>
2017-12-07 05:19:13 -0800

    zbar: revision for imagemagick
    Closes #21415.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/zbar.rb
+++ Formula/zbar.rb
@@ -1,7 +1,7 @@
 class Zbar < Formula
   desc "Suite of barcodes-reading tools"
   homepage "https://zbar.sourceforge.io"
-  revision 7
+  revision 8
 
   stable do
     url "https://downloads.sourceforge.net/project/zbar/zbar/0.10/zbar-0.10.tar.bz2"
