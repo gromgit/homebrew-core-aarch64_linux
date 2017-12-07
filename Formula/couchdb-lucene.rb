b9b35a0f2b4a388c335a20272fba4098a1a1c252
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-07 00:25:56 +0000

    couchdb-lucene: update 2.1.0 bottle.

--- Formula/couchdb-lucene.rb
+++ Formula/couchdb-lucene.rb
@@ -6,10 +6,9 @@ class CouchdbLucene < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "465f3100b9dd77732312926533983a53eadfe6a1cfc0c820371ca3ae57920629" => :high_sierra
-    sha256 "e3cddc411b93a39bf753de4b26b256817b4288b2ea52f4631abb0f2050a1d98f" => :sierra
-    sha256 "850f5359429ce0ed64c852d52776aa5284221e2475889b5f2a8a72a3d0b10564" => :el_capitan
-    sha256 "6f0e7de6cfa7149edf58967d0dbd35f259d5a889ba5f36e9d00b9ae06e6efbbd" => :yosemite
+    sha256 "7e921fbcc3d95efef140e77283d8f6a2627f70afdcc02c7202f1c3a8d1042477" => :high_sierra
+    sha256 "772001fc7739ea21f359763b35125e4de4b2739872b7bba8fc933d1f59d25a18" => :sierra
+    sha256 "cd92c8cd8f4759a2525c02b54fbefccde7e15afd071f7bd9d3c2b1ef5dd00fef" => :el_capitan
   end
 
   depends_on "couchdb"
