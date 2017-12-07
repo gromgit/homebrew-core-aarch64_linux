3ed19fbb5dc089f478d91da4096a74860a191732
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-07 17:37:03 +0000

    securefs: update 0.8.1 bottle.

--- Formula/securefs.rb
+++ Formula/securefs.rb
@@ -7,10 +7,9 @@ class Securefs < Formula
 
   bottle do
     cellar :any
-    sha256 "e4d89492ab25d93425d0b483c3b8b44d3b82a707ef98ce0fa8ad05a74f4ff96e" => :high_sierra
-    sha256 "0e5ec538a9da8e9c002befd10578e783db1b61fd5351b4b6ee8de35bda81abbc" => :sierra
-    sha256 "5066deb7e5876f9ac7f039f68984a15b0cca44b7a4303bd40318fd9019141415" => :el_capitan
-    sha256 "98661d949df67ccfa5ea16eaf606a358f9e03d3b737276bf03ee4d65070905e7" => :yosemite
+    sha256 "6824b2cfcfdc55cc79d1e01d6a958d55c082533e74d697b1db77815083a1e31b" => :high_sierra
+    sha256 "9a347b474fb48789d148e22ee2165224166c00161b2447cf2c6a8cf0a23326cb" => :sierra
+    sha256 "b15c75803225335e0465ccd66215d09d8194ebb1320072020cf21189c34266b4" => :el_capitan
   end
 
   depends_on "cmake" => :build
