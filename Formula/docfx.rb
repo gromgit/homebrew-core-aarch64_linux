12121a5244996c60c516c270b3dabdddd0819970
JCount <JCount42@gmail.com>
2017-12-08 17:29:16 -0500

    docfx 2.28.2 (#21491)

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -1,8 +1,8 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/releases/download/v2.28.1/docfx.zip"
-  sha256 "0cbed08a4781e81ee1a20223c66f2aae4509ca410d52667912802ce5c13de70f"
+  url "https://github.com/dotnet/docfx/releases/download/v2.28.2/docfx.zip"
+  sha256 "e9111fcb104bd5e8262922b6929cf2acb69f0229cfc305cf8da40f8008a5d8d4"
 
   bottle :unneeded
 
