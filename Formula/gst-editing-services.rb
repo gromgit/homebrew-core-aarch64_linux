eee92b1a4cc2ef7eeaa00056871219c89879970d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-08 18:06:57 +0000

    gst-editing-services: update 1.12.4 bottle.

--- Formula/gst-editing-services.rb
+++ Formula/gst-editing-services.rb
@@ -5,9 +5,9 @@ class GstEditingServices < Formula
   sha256 "bd7eaa2c9572db9e7e535064024b6f69250de864fe8a5d5be86fa8c7edacca0c"
 
   bottle do
-    sha256 "fede2d7884a0dccc68b3ce37e15cb94d68f25b82944abec97bc7d5ef8b85053a" => :high_sierra
-    sha256 "a7e1edba4cfee20e354ad3a4ea655e1a4cd899a17ad0d11344bbe43ff1e64a7f" => :sierra
-    sha256 "ca9084be6a553b865212b7cdb916a971cd9e51a55b6705f53943687e1d760c58" => :el_capitan
+    sha256 "37e28bb8f36efa80c581272186f365201088cbb52f45b186e1e2775dbc2eb973" => :high_sierra
+    sha256 "8ca9dbc7b05c1baf13f550f297636016be20119d93343b985d9781e6353b490c" => :sierra
+    sha256 "c99951c37bd5b03778d57141b7fe6755ad7c54d524f995c23b48f99f219cb808" => :el_capitan
   end
 
   depends_on "gstreamer"
