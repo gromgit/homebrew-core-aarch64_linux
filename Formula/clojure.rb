30ca04590fb08695850c14d9b10b476d90d993dd
Alex Miller <alex@puredanger.com>
2017-12-08 13:33:32 -0600

    clojure 1.9.0.273 (#21470)
    add terminal setting to test block

--- Formula/clojure.rb
+++ Formula/clojure.rb
@@ -1,14 +1,8 @@
 class Clojure < Formula
   desc "The Clojure Programming Language"
   homepage "https://clojure.org"
-  url "https://download.clojure.org/install/clojure-scripts-1.8.0.193.tar.gz"
-  sha256 "82e671b252362e1bdff2d5ffc8b5ef758df29f4b6503b41a79ee646c8c2651e1"
-
-  devel do
-    url "https://download.clojure.org/install/clojure-scripts-1.9.0-RC2.263.tar.gz"
-    sha256 "62ec95e45d78548cc3b7ecd81dee6a697f81a86768c996a2c199af1664083a86"
-    version "1.9.0-RC2.263"
-  end
+  url "https://download.clojure.org/install/clojure-tools-1.9.0.273.tar.gz"
+  sha256 "64d1c714d758feee5fc76f3c044c229965d068f140868af0930280bcfbc0b976"
 
   bottle :unneeded
 
@@ -20,6 +14,7 @@ class Clojure < Formula
   end
 
   test do
+    ENV["TERM"] = "xterm"
     system("#{bin}/clj -e nil")
     %w[clojure clj].each do |clj|
       assert_equal "2", shell_output("#{bin}/#{clj} -e \"(+ 1 1)\"").strip
