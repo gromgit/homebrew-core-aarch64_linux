db9e5810918b8db4616567354ecea12a26773557
ilovezfs <ilovezfs@icloud.com>
2017-12-07 21:30:05 -0800

    planck 2.10.0
    Closes #21459.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/planck.rb
+++ Formula/planck.rb
@@ -1,8 +1,8 @@
 class Planck < Formula
   desc "Stand-alone ClojureScript REPL"
   homepage "http://planck-repl.org/"
-  url "https://github.com/mfikes/planck/archive/2.9.0.tar.gz"
-  sha256 "165e8d0fb952883162fa1029ad20f28915d9be5cb751d5302805fa38df237764"
+  url "https://github.com/mfikes/planck/archive/2.10.0.tar.gz"
+  sha256 "b33d2975ba84a2c598dc1ab4c427edd40d25bcf2a3ca019d9582b60145fe8f5c"
   head "https://github.com/mfikes/planck.git"
 
   bottle do
