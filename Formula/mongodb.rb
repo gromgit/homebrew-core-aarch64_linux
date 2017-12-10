9f9aac555cc330f08acb56de3599b3350c2bdb4f
Benedikt Huß <benedikt.huss@werkdigital.de>
2017-12-10 17:09:23 +0100

    mongodb 3.6.0
    Closes #21523.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mongodb.rb
+++ Formula/mongodb.rb
@@ -4,17 +4,8 @@ class Mongodb < Formula
   desc "High-performance, schema-free, document-oriented database"
   homepage "https://www.mongodb.org/"
 
-  stable do
-    url "https://fastdl.mongodb.org/src/mongodb-src-r3.4.10.tar.gz"
-    sha256 "443800ca4f52fa613b29052f5f76abc0ccc477451b55f3665b61819f28ace2f3"
-
-    go_resource "github.com/mongodb/mongo-tools" do
-      url "https://github.com/mongodb/mongo-tools.git",
-          :tag => "r3.4.10",
-          :revision => "4f093ae71cdb4c6a6e9de7cd1dc67ea4405f0013",
-          :shallow => false
-    end
-  end
+  url "https://fastdl.mongodb.org/src/mongodb-src-r3.6.0.tar.gz"
+  sha256 "1573e6e172a6a1e559e1156b0f059f3639774a9f3db33790cb826c56e1398efd"
 
   bottle do
     sha256 "9d9929d9502d958840929355daf8a3c71ce6779d63b0a3ab4123562e1c840b2c" => :high_sierra
@@ -22,55 +13,48 @@ class Mongodb < Formula
     sha256 "5bdb6c3a4fea9d74afd54497f3b508c2f85034ff9de6a92b15783dadb06e575a" => :el_capitan
   end
 
-  devel do
-    url "https://fastdl.mongodb.org/src/mongodb-src-r3.6.0.tar.gz"
-    sha256 "1573e6e172a6a1e559e1156b0f059f3639774a9f3db33790cb826c56e1398efd"
-
-    depends_on :xcode => ["8.3.2", :build]
-
-    resource "Cheetah" do
-      url "https://files.pythonhosted.org/packages/cd/b0/c2d700252fc251e91c08639ff41a8a5203b627f4e0a2ae18a6b662ab32ea/Cheetah-2.4.4.tar.gz"
-      sha256 "be308229f0c1e5e5af4f27d7ee06d90bb19e6af3059794e5fd536a6f29a9b550"
-    end
-
-    resource "PyYAML" do
-      url "https://files.pythonhosted.org/packages/4a/85/db5a2df477072b2902b0eb892feb37d88ac635d36245a72a6a69b23b383a/PyYAML-3.12.tar.gz"
-      sha256 "592766c6303207a20efc445587778322d7f73b161bd994f227adaa341ba212ab"
-    end
-
-    resource "typing" do
-      url "https://files.pythonhosted.org/packages/ca/38/16ba8d542e609997fdcd0214628421c971f8c395084085354b11ff4ac9c3/typing-3.6.2.tar.gz"
-      sha256 "d514bd84b284dd3e844f0305ac07511f097e325171f6cc4a20878d11ad771849"
-    end
-
-    go_resource "github.com/mongodb/mongo-tools" do
-      url "https://github.com/mongodb/mongo-tools.git",
-          :tag => "r3.6.0",
-          :revision => "12cce7433c480538ff26caa9c51cec3e04a07e90"
-    end
-  end
-
   option "with-boost", "Compile using installed boost, not the version shipped with mongodb"
   option "with-sasl", "Compile with SASL support"
 
+  depends_on :xcode => ["8.3.2", :build]
   depends_on "boost" => :optional
   depends_on "go" => :build
   depends_on :macos => :mountain_lion
   depends_on "scons" => :build
   depends_on "openssl" => :recommended
 
+  resource "Cheetah" do
+    url "https://files.pythonhosted.org/packages/cd/b0/c2d700252fc251e91c08639ff41a8a5203b627f4e0a2ae18a6b662ab32ea/Cheetah-2.4.4.tar.gz"
+    sha256 "be308229f0c1e5e5af4f27d7ee06d90bb19e6af3059794e5fd536a6f29a9b550"
+  end
+
+  resource "PyYAML" do
+    url "https://files.pythonhosted.org/packages/4a/85/db5a2df477072b2902b0eb892feb37d88ac635d36245a72a6a69b23b383a/PyYAML-3.12.tar.gz"
+    sha256 "592766c6303207a20efc445587778322d7f73b161bd994f227adaa341ba212ab"
+  end
+
+  resource "typing" do
+    url "https://files.pythonhosted.org/packages/ca/38/16ba8d542e609997fdcd0214628421c971f8c395084085354b11ff4ac9c3/typing-3.6.2.tar.gz"
+    sha256 "d514bd84b284dd3e844f0305ac07511f097e325171f6cc4a20878d11ad771849"
+  end
+
+  go_resource "github.com/mongodb/mongo-tools" do
+    url "https://github.com/mongodb/mongo-tools.git",
+        :tag => "r3.6.0",
+        :revision => "12cce7433c480538ff26caa9c51cec3e04a07e90",
+        :shallow => false
+  end
+
   needs :cxx11
 
   def install
     ENV.cxx11 if MacOS.version < :mavericks
 
-    if build.devel?
-      ENV.libcxx
+    ENV.libcxx
 
-      ["Cheetah", "PyYAML", "typing"].each do |r|
-        resource(r).stage do
-          system "python", *Language::Python.setup_install_args(buildpath/"vendor")
-        end
+    ["Cheetah", "PyYAML", "typing"].each do |r|
+      resource(r).stage do
+        system "python", *Language::Python.setup_install_args(buildpath/"vendor")
       end
     end
     (buildpath/".brew_home/Library/Python/2.7/lib/python/site-packages/vendor.pth").write <<~EOS
@@ -93,7 +77,7 @@ class Mongodb < Formula
       args << "sasl" if build.with? "sasl"
 
       # fix "stty: stdin isn't a terminal"
-      inreplace "build.sh", "stty sane", "" if build.devel?
+      inreplace "build.sh", "stty sane", ""
 
       system "./build.sh", *args
     end
@@ -106,14 +90,11 @@ class Mongodb < Formula
       -j#{ENV.make_jobs}
     ]
 
-    args << "--osx-version-min=#{MacOS.version}" if build.stable?
     args << "CC=#{ENV.cc}"
     args << "CXX=#{ENV.cxx}"
 
-    if build.devel?
-      args << "CCFLAGS=-mmacosx-version-min=#{MacOS.version}"
-      args << "LINKFLAGS=-mmacosx-version-min=#{MacOS.version}"
-    end
+    args << "CCFLAGS=-mmacosx-version-min=#{MacOS.version}"
+    args << "LINKFLAGS=-mmacosx-version-min=#{MacOS.version}"
 
     args << "--use-sasl-client" if build.with? "sasl"
     args << "--use-system-boost" if build.with? "boost"
