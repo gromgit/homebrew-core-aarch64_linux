7208a1998dabf37ab33bd438d75140cc46d5ed54
ilovezfs <ilovezfs@icloud.com>
2017-12-10 05:28:39 -0800

    python-markdown 2.6.10
    Closes #21527.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/python-markdown.rb
+++ Formula/python-markdown.rb
@@ -1,8 +1,8 @@
 class PythonMarkdown < Formula
   desc "Python implementation of Markdown"
   homepage "https://pypi.python.org/pypi/Markdown"
-  url "https://files.pythonhosted.org/packages/29/82/dfe242bcfd9eec0e7bf93a80a8f8d8515a95b980c44f5c0b45606397a423/Markdown-2.6.9.tar.gz"
-  sha256 "73af797238b95768b3a9b6fe6270e250e5c09d988b8e5b223fd5efa4e06faf81"
+  url "https://files.pythonhosted.org/packages/f0/68/d7d5503adbd302fb8e44c8ece2b8afff467a888efb8a0a116c432cc4f4fe/Markdown-2.6.10.zip"
+  sha256 "cfa536d1ee8984007fcecc5a38a493ff05c174cb74cb2341dafd175e6bc30851"
 
   bottle do
     cellar :any_skip_relocation
