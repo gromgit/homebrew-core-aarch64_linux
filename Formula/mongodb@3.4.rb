43611654f0043578fe01976ddb0becd72912cd54
Benedikt Huß <benedikt.huss@werkdigital.de>
2017-12-10 17:08:53 +0100

    mongodb@3.4 3.4.10 (new formula)

---
+++ Formula/mongodb@3.4.rb
@@ -0,0 +1,146 @@
+require "language/go"
+
+class MongodbAT34 < Formula
+  desc "High-performance, schema-free, document-oriented database"
+  homepage "https://www.mongodb.org/"
+
+  url "https://fastdl.mongodb.org/src/mongodb-src-r3.4.10.tar.gz"
+  sha256 "443800ca4f52fa613b29052f5f76abc0ccc477451b55f3665b61819f28ace2f3"
+
+  keg_only :versioned_formula
+
+  option "with-boost", "Compile using installed boost, not the version shipped with mongodb"
+  option "with-sasl", "Compile with SASL support"
+
+  depends_on "boost" => :optional
+  depends_on "go" => :build
+  depends_on :macos => :mountain_lion
+  depends_on "scons" => :build
+  depends_on "openssl" => :recommended
+
+  go_resource "github.com/mongodb/mongo-tools" do
+    url "https://github.com/mongodb/mongo-tools.git",
+        :tag => "r3.4.10",
+        :revision => "4f093ae71cdb4c6a6e9de7cd1dc67ea4405f0013",
+        :shallow => false
+  end
+
+  needs :cxx11
+
+  def install
+    ENV.cxx11 if MacOS.version < :mavericks
+
+    (buildpath/".brew_home/Library/Python/2.7/lib/python/site-packages/vendor.pth").write <<~EOS
+      import site; site.addsitedir("#{buildpath}/vendor/lib/python2.7/site-packages")
+    EOS
+
+    # New Go tools have their own build script but the server scons "install" target is still
+    # responsible for installing them.
+    Language::Go.stage_deps resources, buildpath/"src"
+
+    cd "src/github.com/mongodb/mongo-tools" do
+      args = %w[]
+
+      if build.with? "openssl"
+        args << "ssl"
+        ENV["LIBRARY_PATH"] = Formula["openssl"].opt_lib
+        ENV["CPATH"] = Formula["openssl"].opt_include
+      end
+
+      args << "sasl" if build.with? "sasl"
+
+      system "./build.sh", *args
+    end
+
+    mkdir "src/mongo-tools"
+    cp Dir["src/github.com/mongodb/mongo-tools/bin/*"], "src/mongo-tools/"
+
+    args = %W[
+      --prefix=#{prefix}
+      -j#{ENV.make_jobs}
+    ]
+
+    args << "--osx-version-min=#{MacOS.version}"
+    args << "CC=#{ENV.cc}"
+    args << "CXX=#{ENV.cxx}"
+
+    args << "--use-sasl-client" if build.with? "sasl"
+    args << "--use-system-boost" if build.with? "boost"
+    args << "--use-new-tools"
+    args << "--build-mongoreplay=true"
+    args << "--disable-warnings-as-errors" if MacOS.version >= :yosemite
+
+    if build.with? "openssl"
+      args << "--ssl"
+
+      args << "CCFLAGS=-I#{Formula["openssl"].opt_include}"
+      args << "LINKFLAGS=-L#{Formula["openssl"].opt_lib}"
+    end
+
+    scons "install", *args
+
+    (buildpath/"mongod.conf").write mongodb_conf
+    etc.install "mongod.conf"
+  end
+
+  def post_install
+    (var/"mongodb").mkpath
+    (var/"log/mongodb").mkpath
+  end
+
+  def mongodb_conf; <<~EOS
+    systemLog:
+      destination: file
+      path: #{var}/log/mongodb/mongo.log
+      logAppend: true
+    storage:
+      dbPath: #{var}/mongodb
+    net:
+      bindIp: 127.0.0.1
+    EOS
+  end
+
+  plist_options :manual => "mongod --config #{HOMEBREW_PREFIX}/etc/mongod.conf"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/mongod</string>
+        <string>--config</string>
+        <string>#{etc}/mongod.conf</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <false/>
+      <key>WorkingDirectory</key>
+      <string>#{HOMEBREW_PREFIX}</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/mongodb/output.log</string>
+      <key>StandardOutPath</key>
+      <string>#{var}/log/mongodb/output.log</string>
+      <key>HardResourceLimits</key>
+      <dict>
+        <key>NumberOfFiles</key>
+        <integer>4096</integer>
+      </dict>
+      <key>SoftResourceLimits</key>
+      <dict>
+        <key>NumberOfFiles</key>
+        <integer>4096</integer>
+      </dict>
+    </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    system "#{bin}/mongod", "--sysinfo"
+  end
+end
