f8079a93c83ba5c9e928f37cb54ae9700fea6c9b
Shaun Jackman <sjackman@gmail.com>
2017-12-11 12:23:39 -0800

    prodigal: import from Homebrew/science
    Closes #21574.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/prodigal.rb
@@ -0,0 +1,18 @@
+class Prodigal < Formula
+  desc "Microbial gene prediction"
+  homepage "https://github.com/hyattpd/Prodigal"
+  url "https://github.com/hyattpd/Prodigal/archive/v2.6.3.tar.gz"
+  sha256 "89094ad4bff5a8a8732d899f31cec350f5a4c27bcbdd12663f87c9d1f0ec599f"
+
+  def install
+    system "make", "install", "INSTALLDIR=#{bin}"
+  end
+
+  test do
+    fasta = <<~EOS
+      >U00096.2:1-70
+      AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC
+    EOS
+    assert_match "CDS", pipe_output("#{bin}/prodigal -q -p meta", fasta, 0)
+  end
+end
