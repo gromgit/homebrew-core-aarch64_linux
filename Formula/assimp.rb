6787b1417c4d793a19e0c33a7955f3b9f0725e7a
ilovezfs <ilovezfs@icloud.com>
2017-12-11 08:48:16 -0800

    assimp 4.1.0
    Closes #21567.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/assimp.rb
+++ Formula/assimp.rb
@@ -1,8 +1,8 @@
 class Assimp < Formula
   desc "Portable library for importing many well-known 3D model formats"
   homepage "http://www.assimp.org"
-  url "https://github.com/assimp/assimp/archive/v4.0.1.tar.gz"
-  sha256 "60080d8ab4daaab309f65b3cffd99f19eb1af8d05623fff469b9b652818e286e"
+  url "https://github.com/assimp/assimp/archive/v4.1.0.tar.gz"
+  sha256 "3520b1e9793b93a2ca3b797199e16f40d61762617e072f2d525fad70f9678a71"
   head "https://github.com/assimp/assimp.git"
 
   bottle do
@@ -17,6 +17,15 @@ class Assimp < Formula
 
   depends_on "cmake" => :build
   depends_on "boost" => [:recommended, :build]
+
+  # Fix "unzip.c:150:11: error: unknown type name 'z_crc_t'"
+  # Upstream PR from 12 Dec 2017 "unzip: fix build with older zlib"
+  if MacOS.version <= :el_capitan
+    patch do
+      url "https://github.com/assimp/assimp/pull/1634.patch?full_index=1"
+      sha256 "79b93f785ee141dc2f56d557b2b8ee290eed0afc7dd373ad84715c6c9aa23460"
+    end
+  end
 
   def install
     args = std_cmake_args
