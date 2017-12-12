2d3ad5ebf24423f09c78215aaffae8d93cce8a84
Adam Stokes <battlemidget@users.noreply.github.com>
2017-12-12 14:39:10 -0500

    juju-wait 2.6.2
    Closes #21605.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/juju-wait.rb
+++ Formula/juju-wait.rb
@@ -3,8 +3,8 @@ class JujuWait < Formula
 
   desc "Juju plugin for waiting for deployments to settle"
   homepage "https://launchpad.net/juju-wait"
-  url "https://pypi.python.org/packages/96/82/6b1b566b75f668605469d9af220bed0104bd4dc12c66160771b32f3aab58/juju-wait-2.5.0.tar.gz"
-  sha256 "05354b87e65b19a67176e470b4edf2588ae3ec301576b4a5214bc698c420671e"
+  url "https://pypi.python.org/packages/3d/c2/8cce9ec8386be418a76566fcd2e7dcbaa7138a92b0b9b463306d9191cfd7/juju-wait-2.6.2.tar.gz"
+  sha256 "86622804896e80f26a3ed15dff979584952ba484ccb5258d8bab6589e26dd46d"
 
   bottle do
     cellar :any
