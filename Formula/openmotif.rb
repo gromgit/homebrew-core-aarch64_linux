db3aaf2372f4b215173be3f19d0479cd99a43add
ilovezfs <ilovezfs@icloud.com>
2017-12-13 07:47:09 -0800

    openmotif 2.3.8
    remove Autotools dependencies
    make jpeg and libpng required instead of optional
    
    Closes #21654.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/openmotif.rb
+++ Formula/openmotif.rb
@@ -1,8 +1,8 @@
 class Openmotif < Formula
   desc "LGPL release of the Motif toolkit"
   homepage "https://motif.ics.com/motif"
-  url "https://downloads.sourceforge.net/project/motif/Motif%202.3.7%20Source%20Code/motif-2.3.7.tar.gz"
-  sha256 "8f7aadbb0f42df2093d4690735a2b9a02ea2bf69dfb15ae0a39cae28f1580d14"
+  url "https://downloads.sourceforge.net/project/motif/Motif%202.3.8%20Source%20Code/motif-2.3.8.tar.gz"
+  sha256 "859b723666eeac7df018209d66045c9853b50b4218cecadb794e2359619ebce7"
 
   bottle do
     sha256 "e90188438ef546f721a8818af80970ae9a7939b3d1a926d1805a266f4e4911e4" => :high_sierra
@@ -11,56 +11,20 @@ class Openmotif < Formula
     sha256 "3184a926a49f650bf07e100454401a777233986ea2c69eb1fef0e2e7922ebcab" => :yosemite
   end
 
-  depends_on "automake" => :build
-  depends_on "autoconf" => :build
-  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "fontconfig"
   depends_on "freetype"
-  depends_on "jpeg" => :optional
-  depends_on "libpng" => :optional
+  depends_on "jpeg"
+  depends_on "libpng"
   depends_on :x11
 
   conflicts_with "lesstif",
     :because => "Lesstif and Openmotif are complete replacements for each other"
 
-  # Removes a flag clang doesn't recognise/accept as valid
-  # From https://trac.macports.org/browser/trunk/dports/x11/openmotif/files/patch-configure.ac.diff
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/b10858b/openmotif/patch-configure.ac.diff"
-    sha256 "0cfff42cb7f37d4bd14fe778ba3d85e418586636b185b0c90e9e3c7d0a35feef"
-  end
-
-  # "Only weak aliases are supported on darwin"
-  # Adapted from https://trac.macports.org/browser/trunk/dports/x11/openmotif/files/patch-lib-XmP.h.diff
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/b10858b/openmotif/patch-lib-XmP.h.diff"
-    sha256 "320754bd0c1fa520c7576f3c7a22249a9b741c12f29606652add4a7a62c75d3f"
-  end
-
-  # Fix VendorShell reference for XQuartz 2.7.9+
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/b10858b/openmotif/patch-lib-VendorS.c.diff"
-    sha256 "71b0573aea2d53cc304f206e2d68e5fa7922782cc21cc404b72739b01bfc8034"
-  end
-
   def install
-    # https://trac.macports.org/browser/trunk/dports/x11/openmotif/Portfile#L59
-    # Compile breaks if these three files are present.
-    %w[demos/lib/Exm/String.h demos/lib/Exm/StringP.h demos/lib/Exm/String.c].each do |f|
-      rm_rf f
-    end
-
-    args = %W[
-      --prefix=#{prefix}
-      --disable-dependency-tracking
-      --disable-silent-rules
-    ]
-
-    args << "--disable-jpeg" if build.without? "jpeg"
-    args << "--disable-png" if build.without? "libpng"
-
-    system "./configure", *args
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules"
     system "make"
     system "make", "install"
 
