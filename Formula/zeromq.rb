46f0da84e2af9416e0b5ec410024c1a272a0ab9a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-13 21:21:53 +0000

    zeromq: update 4.2.3 bottle.

--- Formula/zeromq.rb
+++ Formula/zeromq.rb
@@ -6,10 +6,9 @@ class Zeromq < Formula
 
   bottle do
     cellar :any
-    sha256 "24c5d175563a388a81df502e0e56b0bc16e2d2becdff05bb74adefa091a82264" => :high_sierra
-    sha256 "e10289e3a44f78b2234347b04b9369f45455afa8b0fd61d538bc92a6de69b517" => :sierra
-    sha256 "72ca74ca177ff8ffd765d2e010b5027e3640b1e68964b1e5c7f7f3acc0a6acdd" => :el_capitan
-    sha256 "e98d0623b86749fe766a518d256493bd5cb78a73002a10dfb748a8c65763b45a" => :yosemite
+    sha256 "6977c4c28e6bf87938395821f6447316188de80fa61142a6256c218e3cf1f77c" => :high_sierra
+    sha256 "f1c00c7dd7aa192dd5d88bce737902b4530796259b535adf6e391c1f69a67970" => :sierra
+    sha256 "6b6abaf6b8466e97605de55d9158e54dd847a09635d0bee1ea00fed93e6f04d7" => :el_capitan
   end
 
   head do
