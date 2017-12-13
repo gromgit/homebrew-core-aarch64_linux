acd0832213d415cfd6f149e15e2221c5444feb93
ilovezfs <ilovezfs@icloud.com>
2017-12-13 08:38:25 -0800

    yubico-piv-tool 1.5.0
    depend on check at build time
    
    Closes #21664.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/yubico-piv-tool.rb
+++ Formula/yubico-piv-tool.rb
@@ -1,8 +1,8 @@
 class YubicoPivTool < Formula
   desc "Command-line tool for the YubiKey NEO PIV applet"
   homepage "https://developers.yubico.com/yubico-piv-tool/"
-  url "https://developers.yubico.com/yubico-piv-tool/Releases/yubico-piv-tool-1.4.4.tar.gz"
-  sha256 "ab0eac3f78e9e01538723f631ed6042504409a1ab0342973e2bd52f2c68a3769"
+  url "https://developers.yubico.com/yubico-piv-tool/Releases/yubico-piv-tool-1.5.0.tar.gz"
+  sha256 "c18375179ba25bf9d61365b3903f033f112897bbd54ca63c62fa153f2d05aaab"
 
   bottle do
     cellar :any
@@ -11,6 +11,7 @@ class YubicoPivTool < Formula
     sha256 "184f77d93b88564c7fd82b5c928f433a521bd3d2cd0da16a3915e307ab2528f6" => :el_capitan
   end
 
+  depends_on "check" => :build
   depends_on "pkg-config" => :build
   depends_on "openssl"
 
