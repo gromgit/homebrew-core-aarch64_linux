153bc294782b7ffa4085b77ee3f36f73e2da7ef2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-13 03:10:09 +0000

    qtads: update 2.1.7_1 bottle.

--- Formula/qtads.rb
+++ Formula/qtads.rb
@@ -34,6 +34,7 @@ class Qtads < Formula
 
   bottle do
     cellar :any
+    sha256 "ef218d294d01133003c6e52fc32f9482726d6f237b3b5b90add019960ffe9eb2" => :high_sierra
     sha256 "51fff5c39b8c234bb72b9a3865f7a067fb2dab902316c7943261ba66ed98ab19" => :sierra
     sha256 "fe8ab65019c324c13c9024291b3e6288aff3ec28049a0cf321da421b4c28f0f6" => :el_capitan
     sha256 "e2383ed761b051e337ed2a4a4162655cb9eaa19ed8ab0666e8a7d1efa236b9b2" => :yosemite
