4fda6c5368ef20d25a52531f68df01d760e9a4d8
ilovezfs <ilovezfs@icloud.com>
2017-12-13 07:35:50 -0800

    mupdf 1.12.0
    Closes #21650.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mupdf.rb
+++ Formula/mupdf.rb
@@ -1,23 +1,9 @@
 class Mupdf < Formula
   desc "Lightweight PDF and XPS viewer"
   homepage "https://mupdf.com/"
-  revision 2
+  url "https://mupdf.com/downloads/mupdf-1.12.0-source.tar.gz"
+  sha256 "5c6353a82f1512f4f5280cf69a3725d1adac9c8b22377ec2a447c4fc45528755"
   head "https://git.ghostscript.com/mupdf.git"
-
-  stable do
-    url "https://mupdf.com/downloads/mupdf-1.11-source.tar.gz"
-    sha256 "209474a80c56a035ce3f4958a63373a96fad75c927c7b1acdc553fc85855f00a"
-
-    # Upstream already. Remove on next stable release.
-    patch do
-      url "https://mirrors.ocf.berkeley.edu/debian/pool/main/m/mupdf/mupdf_1.11+ds1-2.debian.tar.xz"
-      mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/m/mupdf/mupdf_1.11+ds1-2.debian.tar.xz"
-      sha256 "da7445a8063d7c81b97d2c373aa112df69d3ad29989b67621387e88d9c38b668"
-      apply "patches/0004-Fix-698539-Don-t-use-xps-font-if-it-could-not-be-loa.patch",
-            "patches/0005-Fix-698540-Check-name-comment-and-meta-size-field-si.patch",
-            "patches/0006-Fix-698558-Handle-non-tags-in-tag-name-comparisons.patch"
-    end
-  end
 
   bottle do
     cellar :any_skip_relocation
@@ -33,17 +19,6 @@ class Mupdf < Formula
     :because => "mupdf and mupdf-tools install the same binaries."
 
   def install
-    # Work around bug: https://bugs.ghostscript.com/show_bug.cgi?id=697842
-    inreplace "Makerules", "RANLIB_CMD := xcrun", "RANLIB_CMD = xcrun"
-
-    # We're using an inreplace here because Debian's version of this patch
-    # breaks when using Clang as a compiler rather than GCC. This fixes
-    # CVE-2017-15587.
-    if build.stable?
-      inreplace "source/pdf/pdf-xref.c", "if (i0 < 0 || i1 < 0)",
-                                         "if (i0 < 0 || i1 < 0 || i0 > INT_MAX - i1)"
-    end
-
     system "make", "install",
            "build=release",
            "verbose=yes",
