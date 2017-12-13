b33f5fc71b383329b130fb300a9ea290ef995f42
ilovezfs <ilovezfs@icloud.com>
2017-12-13 07:42:45 -0800

    nginx 1.13.7 (devel)

--- Formula/nginx.rb
+++ Formula/nginx.rb
@@ -13,8 +13,8 @@ class Nginx < Formula
   end
 
   devel do
-    url "https://nginx.org/download/nginx-1.13.6.tar.gz"
-    sha256 "8512fc6f986a20af293b61f33b0e72f64a72ea5b1acbcc790c4c4e2d6f63f8f8"
+    url "https://nginx.org/download/nginx-1.13.7.tar.gz"
+    sha256 "beb732bc7da80948c43fd0bf94940a21a21b1c1ddfba0bd99a4b88e026220f5c"
   end
 
   # Before submitting more options to this formula please check they aren't
