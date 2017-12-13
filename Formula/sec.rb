975bb49f626d34ad13cd902e88d57107e3043ffa
ilovezfs <ilovezfs@icloud.com>
2017-12-13 01:43:33 -0800

    sec 2.7.12 (#21626)

--- Formula/sec.rb
+++ Formula/sec.rb
@@ -1,8 +1,8 @@
 class Sec < Formula
   desc "Event correlation tool for event processing of various kinds"
   homepage "https://simple-evcorr.sourceforge.io/"
-  url "https://github.com/simple-evcorr/sec/releases/download/2.7.11/sec-2.7.11.tar.gz"
-  sha256 "59cd744c36be43c0cb69f1570d2aa6911ebb3492ff01fc292347ec8876dfe991"
+  url "https://github.com/simple-evcorr/sec/releases/download/2.7.12/sec-2.7.12.tar.gz"
+  sha256 "4ab19f0e9499a071c1e07dddd453faad2cde5e7de2ff187f0eafb2d2a615aa38"
 
   bottle :unneeded
 
