6f192c3e292cacfab9161421c6361f03c9ba6e9c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-14 14:10:42 +0000

    node: update 9.3.0_1 bottle.

--- Formula/node.rb
+++ Formula/node.rb
@@ -7,9 +7,9 @@ class Node < Formula
   head "https://github.com/nodejs/node.git"
 
   bottle do
-    sha256 "17bc119bd070908aa80427bbdd98e4ed35764d30e14c4846f53c7589fad325a1" => :high_sierra
-    sha256 "b4580965a8b9cff06d27eb109b77ddb32c88bf75eab8e2f6c3cb20dd83975c16" => :sierra
-    sha256 "6736c632096bb82eef4e7800fc30cf6442f2e11e11849fb2a85ffcc21c483a6a" => :el_capitan
+    sha256 "188615153c48551f552428879e57805c1137e6fe69a38be68f5f0a6e6b3bd60e" => :high_sierra
+    sha256 "55f054e0aad43b4cfc06b5b6b2bc2f12c0aa4556fae670718e683dceac09adb8" => :sierra
+    sha256 "77590f6971b1340ba9038c0a6c5f8bc867ad20b0356944d42bad8be8d17389ac" => :el_capitan
   end
 
   option "with-debug", "Build with debugger hooks"
