193c67fc801c27ffed68e8e657e6d042b1b2c9f3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-14 21:51:01 +0000

    git-annex: update 6.20171214 bottle.

--- Formula/git-annex.rb
+++ Formula/git-annex.rb
@@ -10,9 +10,9 @@ class GitAnnex < Formula
   head "git://git-annex.branchable.com/"
 
   bottle do
-    sha256 "b53b48333a1bbf67bfdd40f6dd3a1d6073f2affc0fb573ff682a33641eca425d" => :high_sierra
-    sha256 "ab2877253e5094cace6f34781be999d67295d4e942a43f02a34d1e233cc62df3" => :sierra
-    sha256 "58527e50ce77fe3ca2e8b54540f406cf8e326ab4164b3e891be330d6d07025fb" => :el_capitan
+    sha256 "4223641674990d2833f4d69895e58f3636ed01f4e96fba696a93a751573f2c0c" => :high_sierra
+    sha256 "bd5fd8203998b44cd211d79b8a70e5a5ab42ebca18db0e7e5a936cbf77e156ee" => :sierra
+    sha256 "0e13b9adfc5f8833e4faee3ad65089489518b167a789ad0be95d6776b10fb3ac" => :el_capitan
   end
 
   option "with-git-union-merge", "Build the git-union-merge tool"
