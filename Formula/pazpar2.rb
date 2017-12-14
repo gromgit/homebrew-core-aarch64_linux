f7e7db4120e8052dca2b718dc78a34efe1c3231d
Chongyu Zhu <i@lembacon.com>
2017-12-14 15:44:15 +0800

    pazpar2: revision for icu4c

--- Formula/pazpar2.rb
+++ Formula/pazpar2.rb
@@ -3,7 +3,7 @@ class Pazpar2 < Formula
   homepage "https://www.indexdata.com/pazpar2"
   url "http://ftp.indexdata.dk/pub/pazpar2/pazpar2-1.13.0.tar.gz"
   sha256 "06adfbb0e215ae37bdbf7ddc173ba619359213c893d059e7a2203e91ac877f45"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
