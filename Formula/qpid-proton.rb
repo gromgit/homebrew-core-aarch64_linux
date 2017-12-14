ee69b026c114cbbc29f6c41cee2d0feabc7b2c1e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-14 11:35:02 +0000

    qpid-proton: add 0.18.0 bottle.

--- Formula/qpid-proton.rb
+++ Formula/qpid-proton.rb
@@ -5,6 +5,13 @@ class QpidProton < Formula
   sha256 "7f54c3555a8482d439759c087d656d881369252af23f209f1fd5a7e4a8c59bd7"
   head "git://git.apache.org/qpid-proton.git"
 
+  bottle do
+    cellar :any
+    sha256 "01cb4331c91536a040141f1066d4c8fb214736a0653916e15313ce689af7661a" => :high_sierra
+    sha256 "86a8ae2a3c173ca3ec44898f3dde3b60b44b089f22bc18a7070295cd27d8a1b9" => :sierra
+    sha256 "55974792bec574a06943581f22ad4f5861947fd0ea936b742a053f2695a98692" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "libuv"
   depends_on "openssl"
