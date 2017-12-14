68ed45eea38fc58de4fcfe7b659d5491784a9394
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-14 18:42:43 +0000

    nopoll: add 0.4.5.b375 bottle.

--- Formula/nopoll.rb
+++ Formula/nopoll.rb
@@ -5,6 +5,13 @@ class Nopoll < Formula
   version "0.4.5.b375"
   sha256 "4ecbd277714c9acd154277a46388689f9d651836ca5c69990e35d1eee8c7ceae"
 
+  bottle do
+    cellar :any
+    sha256 "528f0a9b956301f12f0b11b2782739c81fc4dbd107f0229b80ee6cc0c1c51dea" => :high_sierra
+    sha256 "4563d74b87406e001bf84e40e4494eef7c2becf47b20ef8bf470d28286239a64" => :sierra
+    sha256 "de062589bdb137cffbf9df05b7128eec12d443c5bea22153efd00f54e5ab2c43" => :el_capitan
+  end
+
   depends_on "openssl"
 
   def install
