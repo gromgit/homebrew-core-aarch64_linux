0398fb850210f43b31c9b3e26bfeb04a7c8ee448
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-14 18:05:20 +0000

    stern: update 1.6.0 bottle.

--- Formula/stern.rb
+++ Formula/stern.rb
@@ -9,10 +9,9 @@ class Stern < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "348f89e4731e9fe018dbdb2b241e9456424c059be847377f9f0b6d86ea73c31b" => :high_sierra
-    sha256 "84069ad767e638e916c00f13231a2f47170b79619e4cab1315aa85ceb7535fe4" => :sierra
-    sha256 "8212fa765da479b688496e1bb69a5d16510aef99acbe8403007f557cb6f52449" => :el_capitan
-    sha256 "7d57ea2096a2755619e99e75f0180307132663bb8e5fd9e5f3272cadb50ebe01" => :yosemite
+    sha256 "a68996e613063c260146902daa371db533ae911f96a4836e0ad797f7fcf750df" => :high_sierra
+    sha256 "c2499dd1dc239e477cf726824882505bcc8dec9dc88167c3ee8ce5bf97dd4c8e" => :sierra
+    sha256 "a859e5e08c7b14c4dc73d0af89327658af4731c9d522929e930741c326adc3f2" => :el_capitan
   end
 
   depends_on "go" => :build
