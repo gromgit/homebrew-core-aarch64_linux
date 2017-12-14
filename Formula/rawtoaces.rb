273982647e3e19aeb66772d1aaaba13f4a80765f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-14 15:21:19 +0000

    rawtoaces: add 1.0 bottle.

--- Formula/rawtoaces.rb
+++ Formula/rawtoaces.rb
@@ -4,6 +4,12 @@ class Rawtoaces < Formula
   url "https://github.com/ampas/rawtoaces/archive/v1.0.tar.gz"
   sha256 "9d15e7e30c4fe97baedfdafb5fddf95534eee26392002b23e81649bbe6e501e9"
 
+  bottle do
+    sha256 "eb16a4a34955b8579c618f065d1957ff3408324a4998707f0996e19ef213fd0c" => :high_sierra
+    sha256 "f698e4339e42be14e9a159b7a6a9c8a525a196a72b3426e8ec24b2e27947b0b0" => :sierra
+    sha256 "4d1b9e5f78e302a0953803e4a65af2d70517525c70d2c335bfc1c1ad36fc4f62" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "aces_container"
   depends_on "boost"
