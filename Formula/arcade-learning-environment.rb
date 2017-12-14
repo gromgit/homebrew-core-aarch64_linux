c3012f87c776c641d51c486880b29861efb5ebbd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-14 12:20:37 +0000

    arcade-learning-environment: add 0.5.2 bottle.

--- Formula/arcade-learning-environment.rb
+++ Formula/arcade-learning-environment.rb
@@ -5,6 +5,13 @@ class ArcadeLearningEnvironment < Formula
   sha256 "bc158b4fb8edc5034f8f8b4ca1a90d48590f516b33f9edec325fce8762418069"
   head "https://github.com/mgbellemare/Arcade-Learning-Environment.git"
 
+  bottle do
+    cellar :any
+    sha256 "d031a38caf79f301366df5cc5dfa6bca2408beb27c861ad00ee832f044d4b57b" => :high_sierra
+    sha256 "95345e80e773b61d36a31cf8f8e98666c8749dd498ed0923f1453310b2dfd4b0" => :sierra
+    sha256 "f12551af8c216c1bc17e87e97b1cd7024aeff2405a8e63b2fb3b73535e34372a" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on :python if MacOS.version <= :snow_leopard
   depends_on "sdl"
