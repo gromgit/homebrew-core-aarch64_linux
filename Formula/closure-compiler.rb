4d0d9e14fe84136c4d1b957e9cec9357ef597f4b
ilovezfs <ilovezfs@icloud.com>
2017-12-13 23:43:34 -0800

    closure-compiler 20171203 (#21692)

--- Formula/closure-compiler.rb
+++ Formula/closure-compiler.rb
@@ -1,8 +1,8 @@
 class ClosureCompiler < Formula
   desc "JavaScript optimizing compiler"
   homepage "https://github.com/google/closure-compiler"
-  url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20171112/closure-compiler-v20171112.jar"
-  sha256 "255878e9d42d266478b9a7a76a30fe6196df1b42cc3e57a98730d8c1345e506f"
+  url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20171203/closure-compiler-v20171203.jar"
+  sha256 "770841c20d89e5b0add3df85dc69ef7f0818b941f31c3527901abfb2ffa53a2e"
 
   bottle :unneeded
 
