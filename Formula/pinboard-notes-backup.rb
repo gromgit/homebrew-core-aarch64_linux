4a2013ff97457b14dfa2b8a9b1b9a1e9631ff752
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-14 14:40:19 +0000

    pinboard-notes-backup: add 1.0.3 bottle.

--- Formula/pinboard-notes-backup.rb
+++ Formula/pinboard-notes-backup.rb
@@ -9,6 +9,13 @@ class PinboardNotesBackup < Formula
   sha256 "bc3ab1a8a3d92fcbda86dd8b4756b035be89e1e5b50bdd61f998b67c89243ae3"
   head "https://github.com/bdesham/pinboard-notes-backup.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1138cbd05fc88464afb783d8509336fb8bb82495700b34199d5c16dee1cbac41" => :high_sierra
+    sha256 "77d4f8e3de1887bbae724aaaf2b72b7399adb8435153a192b7f39cabb8846c54" => :sierra
+    sha256 "357931ecd198a6fad58d6a9cf3d8543b1dbc3a2fd0ec60e1887725791a2e6c6b" => :el_capitan
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
 
