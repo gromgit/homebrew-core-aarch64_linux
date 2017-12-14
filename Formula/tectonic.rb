5d1e9feb475b87bb252f5fcfe7b7d8bc89c4937e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-14 14:10:41 +0000

    tectonic: update 0.1.7_2 bottle.

--- Formula/tectonic.rb
+++ Formula/tectonic.rb
@@ -6,9 +6,9 @@ class Tectonic < Formula
   revision 2
 
   bottle do
-    sha256 "abd542b5d03c0fd9c71fdd8b36d6c93c58d5c65b3316b9fa0f829caf9b11eca4" => :high_sierra
-    sha256 "b9d9a6442a534d421e459ebd38850475cf5ad5170d535226ae1c60d1821b434a" => :sierra
-    sha256 "e19fd0b57de2b8226a2e269fdf31d19c30810727a5eddb17ced35e3ce7b4bbc4" => :el_capitan
+    sha256 "fe69edcce8d1bee8aba0180d4ab4434073c4519b7a5677ba996246b24e8ff0ec" => :high_sierra
+    sha256 "9b0aa550333d388f6d7794a0cbad1e3ea077d1de501739ae9c69849cc239e81e" => :sierra
+    sha256 "34ce8a3d71046e782d43b247fb92a5407348f82959e7f13343365b3f0bab24f3" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
