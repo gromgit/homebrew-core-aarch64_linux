a33458abb4aeff6dab0f0ff99e3533650ca32231
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-15 13:10:36 +0000

    inspectrum: add 0.2 bottle.

--- Formula/inspectrum.rb
+++ Formula/inspectrum.rb
@@ -5,6 +5,12 @@ class Inspectrum < Formula
   sha256 "50b7db9b86208f414c387700a358eb58364094f3e8a4985f586f4f815645898a"
   head "https://github.com/miek/inspectrum.git"
 
+  bottle do
+    sha256 "c982163a1a70eb676d6fe8e9744a25e03edeea31744ce30beef75f6c47e924d1" => :high_sierra
+    sha256 "b34f9c01e3a888f97d1c832c7fbcf4f4a036c3bcf47f058b81f41affe4f0f94c" => :sierra
+    sha256 "f384448eac77bffb041890951b4ba401b11587eefc9d089162fbe63b7cd08e42" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "fftw"
