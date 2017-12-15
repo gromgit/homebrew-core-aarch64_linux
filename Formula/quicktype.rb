4f458cc24f3e75a737f93089890d83c5b0601c3b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-15 02:53:55 +0000

    quicktype: add 6.0.0 bottle.

--- Formula/quicktype.rb
+++ Formula/quicktype.rb
@@ -6,6 +6,13 @@ class Quicktype < Formula
   url "https://registry.npmjs.org/quicktype/-/quicktype-6.0.0.tgz"
   sha256 "f615dc2c026a3125039ba4463d8c931a7a771a496366c9aae252512a09a90854"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e477d04ce6766c778f8f9c36f01a6a01bba84944ff83d4e73e18821ae18c3cfc" => :high_sierra
+    sha256 "9d470510581e83df4b5b88384091002c3cbd3e3d4bc63c8639318ae1e3161adf" => :sierra
+    sha256 "0638e83312eb5ce1d861598a589cb25e592a03a3a4a468e3dde63c6b0c87e9af" => :el_capitan
+  end
+
   depends_on "node"
 
   def install
