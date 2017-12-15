a8d8e29d9be857968185c4472f90edd0ea51d0fc
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-12-15 16:10:07 +0200

    grakn 1.0.0 (#21758)

--- Formula/grakn.rb
+++ Formula/grakn.rb
@@ -1,5 +1,5 @@
 class Grakn < Formula
-  desc "The Database for AI"
+  desc "The distributed hyper-relational database for knowledge engineering"
   homepage "https://grakn.ai"
   url "https://github.com/graknlabs/grakn/releases/download/v1.0.0/grakn-dist-1.0.0.tar.gz"
   sha256 "bd616a01fe050898b2687985fac5ce2c834402d33de871112f894a726692a916"
