277df776d79f4bc902b5985cc76171bc49a4c35a
ilovezfs <ilovezfs@icloud.com>
2017-12-15 08:45:15 -0800

    abcmidi 2017-12-15
    Closes #21764.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/abcmidi.rb
+++ Formula/abcmidi.rb
@@ -1,9 +1,9 @@
 class Abcmidi < Formula
   desc "Converts abc music notation files to MIDI files"
   homepage "http://www.ifdo.ca/~seymour/runabc/top.html"
-  url "http://www.ifdo.ca/~seymour/runabc/abcMIDI-2017.12.10.zip"
-  version "2017-12-10"
-  sha256 "c4efbf0701929d7d591f0506e1130b5635fe4887ed956800c9197a1987927e6c"
+  url "http://www.ifdo.ca/~seymour/runabc/abcMIDI-2017.12.15.zip"
+  version "2017-12-15"
+  sha256 "7c95afc8d974a5307cc637e437e242302a1d575a874c1b8356e3eb4bad1cb322"
 
   bottle do
     cellar :any_skip_relocation
