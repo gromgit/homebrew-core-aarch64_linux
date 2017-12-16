769b8ac6382fe8641e9254407d8a27f0dd477866
ilovezfs <ilovezfs@icloud.com>
2017-12-16 04:50:14 -0800

    mapnik 3.0.17
    Closes #21797.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/mapnik.rb
+++ Formula/mapnik.rb
@@ -1,9 +1,8 @@
 class Mapnik < Formula
   desc "Toolkit for developing mapping applications"
   homepage "http://www.mapnik.org/"
-  url "https://github.com/mapnik/mapnik/releases/download/v3.0.16/mapnik-v3.0.16.tar.bz2"
-  sha256 "0a0e6351d1a32922327555b9835d4843aade752adecadde309fa856b72dfb1b1"
-  revision 2
+  url "https://github.com/mapnik/mapnik/releases/download/v3.0.17/mapnik-v3.0.17.tar.bz2"
+  sha256 "5ccd2ba7b82ca00028c6ce3ae5856b41d6f658217b680288f14b9c416d17ccb2"
   head "https://github.com/mapnik/mapnik.git"
 
   bottle do
