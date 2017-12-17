0697eaeb55bbf51fd3a0fe38fedaf1554dd926be
ilovezfs <ilovezfs@icloud.com>
2017-12-17 03:02:57 -0800

    tbb 2018_U2
    Closes #21819.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/tbb.rb
+++ Formula/tbb.rb
@@ -1,9 +1,9 @@
 class Tbb < Formula
   desc "Rich and complete approach to parallelism in C++"
   homepage "https://www.threadingbuildingblocks.org/"
-  url "https://github.com/01org/tbb/archive/2018_U1.tar.gz"
-  version "2018_U1"
-  sha256 "c6462217d4ecef2b44fce63cfdf31f9db4f6ff493869899d497a5aef68b05fc5"
+  url "https://github.com/01org/tbb/archive/2018_U2.tar.gz"
+  version "2018_U2"
+  sha256 "78bb9bae474736d213342f01fe1a6d00c6939d5c75b367e2e43e7bf29a6d8eca"
 
   bottle do
     cellar :any
