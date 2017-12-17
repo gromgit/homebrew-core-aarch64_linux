e137c37a0ccc5e246fdb477641f1e9326d1ac8b3
JCount <JCount42@gmail.com>
2017-12-17 15:59:28 -0500

    sdl_mixer: make libmikmod a required dependency

--- Formula/sdl_mixer.rb
+++ Formula/sdl_mixer.rb
@@ -3,7 +3,7 @@ class SdlMixer < Formula
   homepage "https://www.libsdl.org/projects/SDL_mixer/"
   url "https://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-1.2.12.tar.gz"
   sha256 "1644308279a975799049e4826af2cfc787cad2abb11aa14562e402521f86992a"
-  revision 2
+  revision 3
 
   bottle do
     cellar :any
@@ -14,13 +14,13 @@ class SdlMixer < Formula
   end
 
   depends_on "pkg-config" => :build
+  depends_on "libmikmod"
   depends_on "libogg"
   depends_on "libvorbis"
   depends_on "sdl"
   depends_on "flac" => :optional
   depends_on "fluid-synth" => :optional
   depends_on "smpeg" => :optional
-  depends_on "libmikmod" => :optional
 
   def install
     inreplace "SDL_mixer.pc.in", "@prefix@", HOMEBREW_PREFIX
@@ -30,9 +30,9 @@ class SdlMixer < Formula
       --disable-dependency-tracking
       --enable-music-ogg
       --disable-music-ogg-shared
+      --disable-music-mod-shared
     ]
 
-    args << "--disable-music-mod-shared" if build.with? "libmikmod"
     args << "--disable-music-fluidsynth-shared" if build.with? "fluid-synth"
     args << "--disable-music-flac-shared" if build.with? "flac"
     args << "--disable-music-mp3-shared" if build.with? "smpeg"
