19d9d26cf2a8a9c17f0a6afc153c1c4e3018bdfc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-19 13:05:18 +0000

    stubby: update 0.2.1 bottle.

--- Formula/stubby.rb
+++ Formula/stubby.rb
@@ -6,9 +6,9 @@ class Stubby < Formula
   head "https://github.com/getdnsapi/stubby.git", :branch => "develop"
 
   bottle do
-    sha256 "ea9006dcf1a1fdede8b5412d218c97fc2551a6758f0284a36f04fd1e086c9bd2" => :high_sierra
-    sha256 "b358957e60c25c4fa565aeefb133bce9b77868f4d08572cf23dde303f9015f21" => :sierra
-    sha256 "5886b66544b6dea6a4c6096bc04e2c895e25e620604ca310627aebd8fe591ff3" => :el_capitan
+    sha256 "4657b028b7d9b4e49f1cb438c3c94d5d78453f27163578a33bd30d3eb30362b8" => :high_sierra
+    sha256 "c7e3d574f433369eb30559c0ef69da9aef1ab9372d282193b91f2a3a63eaa664" => :sierra
+    sha256 "6016738fa81df80b73758597bde094002f910c4ae1b7e9211cbae75aff3ff57c" => :el_capitan
   end
 
   depends_on "autoconf" => :build
