6cf1f6357921956626ef079f1728a7119cc034c9
ilovezfs <ilovezfs@icloud.com>
2017-12-20 02:53:46 -0800

    snappy: patch for broken version API
    Closes #21925.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/snappy.rb
+++ Formula/snappy.rb
@@ -3,6 +3,7 @@ class Snappy < Formula
   homepage "https://google.github.io/snappy/"
   url "https://github.com/google/snappy/archive/1.1.7.tar.gz"
   sha256 "3dfa02e873ff51a11ee02b9ca391807f0c8ea0529a4924afa645fbf97163f9d4"
+  revision 1
   head "https://github.com/google/snappy.git"
 
   bottle do
@@ -15,6 +16,12 @@ class Snappy < Formula
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+
+  # Upstream PR from 20 Dec 2017 "Fix broken version API"
+  patch do
+    url "https://github.com/google/snappy/pull/61.patch?full_index=1"
+    sha256 "131404e4da2440c83115308f58c91b4a29f4ae93bf841284e0877135d122d7e2"
+  end
 
   def install
     system "cmake", ".", *std_cmake_args
