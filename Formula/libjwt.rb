2fce25142638190547a8c8f0672205ce6c3b7d01
José L. Patiño <jose.lpa@gmail.com>
2017-12-20 16:49:24 +0000

    libjwt 1.8.0 (new formula)
    Closes #21882.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/libjwt.rb
@@ -0,0 +1,37 @@
+class Libjwt < Formula
+  desc "JSON Web Token C library"
+  homepage "https://github.com/benmcollins/libjwt"
+  url "https://github.com/benmcollins/libjwt/archive/v1.8.0.tar.gz"
+  sha256 "e97c15c14dbd49d5ba435cb26e06e6cdd6e32cf9f0be6b6e9a2d2d330923ea17"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "jansson"
+  depends_on "openssl"
+
+  def install
+    system "autoreconf", "-fiv"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "all"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdlib.h>
+      #include <jwt.h>
+
+      int main() {
+        jwt_t *jwt = NULL;
+        if (jwt_new(&jwt) != 0) return 1;
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-I#{include}", "-ljwt", "-o", "test"
+    system "./test"
+  end
+end
