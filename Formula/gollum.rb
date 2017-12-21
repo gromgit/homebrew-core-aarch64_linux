1279cafe39d60fd450f09ef71b1e210fe17d1881
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-21 22:31:07 +0000

    gollum: update 0.5.0 bottle.

--- Formula/gollum.rb
+++ Formula/gollum.rb
@@ -7,10 +7,9 @@ class Gollum < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "8744ba1a7c88a2234b0d0ca8d5c80780c2e37e3c8081e46026af5d29aa0b1ba6" => :high_sierra
-    sha256 "cc66eef01c97ff1835cba0f8020ee591ac081313aed06096e205d3cd5b4fec67" => :sierra
-    sha256 "d7956cf66c1ddffb5b61e47980fd93238e950d93b125f79a295e39d7ba2c0689" => :el_capitan
+    sha256 "3c181df987bcf277691cfd548a855cff0c985adc50a7fec1642e40b6ca78caeb" => :high_sierra
+    sha256 "84d60ef65f15a9464415a163daa0cf99156c741091af926676e2954357ea4c30" => :sierra
+    sha256 "cd3860265a680566dd5922dc8c9af253162be979cb800dc92878ce2ee53f2af5" => :el_capitan
   end
 
   depends_on "go" => :build
