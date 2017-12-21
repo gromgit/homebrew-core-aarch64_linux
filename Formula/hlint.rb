5ddf84241d5a73f09b210d9d805781b76b82b6fb
Mikael Karlsson <i8myshoes@gmail.com>
2017-12-21 19:32:22 +0200

    hlint 2.0.12 (new formula)
    Closes #21996.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/hlint.rb
@@ -0,0 +1,26 @@
+require "language/haskell"
+
+class Hlint < Formula
+  include Language::Haskell::Cabal
+
+  desc "Haskell source code suggestions"
+  homepage "https://github.com/ndmitchell/hlint"
+  url "https://hackage.haskell.org/package/hlint-2.0.12/hlint-2.0.12.tar.gz"
+  sha256 "7e81be91b9ba30274658b95c30f02d5b2a08489363906371b787b002c323d8b1"
+  head "https://github.com/ndmitchell/hlint.git"
+
+  depends_on "cabal-install" => :build
+  depends_on "ghc" => :build
+
+  def install
+    install_cabal_package :using => "happy"
+    man1.install "data/hlint.1"
+  end
+
+  test do
+    (testpath/"test.hs").write <<~EOS
+      main = do putStrLn "Hello World"
+    EOS
+    assert_match "Redundant do", shell_output("#{bin}/hlint test.hs", 1)
+  end
+end
