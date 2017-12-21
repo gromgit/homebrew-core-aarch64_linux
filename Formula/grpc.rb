4943ece6c93b8f12801e63147b6e129d70d8aaff
ilovezfs <ilovezfs@icloud.com>
2017-12-21 00:09:22 -0800

    grpc 1.8.2
    Closes #21962.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/grpc.rb
+++ Formula/grpc.rb
@@ -1,8 +1,8 @@
 class Grpc < Formula
   desc "Next generation open source RPC library and framework"
   homepage "https://www.grpc.io/"
-  url "https://github.com/grpc/grpc/archive/v1.8.1.tar.gz"
-  sha256 "ae34c8255ea0aa4f893b9b2cb9f02d8e13bd9d7f2a90d1c39d56a5d6143a9f44"
+  url "https://github.com/grpc/grpc/archive/v1.8.2.tar.gz"
+  sha256 "ad58a5004242a865108f60f6348e677a1ffc8f99dd60e35d6a6cb6d809ae0769"
   head "https://github.com/grpc/grpc.git"
 
   bottle do
