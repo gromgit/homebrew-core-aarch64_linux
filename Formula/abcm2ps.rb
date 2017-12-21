4e58e1ef6ae92d4414058e8ccb05bf6fb9922481
ilovezfs <ilovezfs@icloud.com>
2017-12-21 06:33:31 -0800

    abcm2ps 8.13.18
    Closes #21977.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/abcm2ps.rb
+++ Formula/abcm2ps.rb
@@ -1,8 +1,8 @@
 class Abcm2ps < Formula
   desc "ABC music notation software"
   homepage "http://moinejf.free.fr"
-  url "https://github.com/leesavide/abcm2ps/archive/v8.13.17.tar.gz"
-  sha256 "639755b7760ddb3f293ab686b23ff7d5937df13f1c388d08e34ceaee319249e9"
+  url "https://github.com/leesavide/abcm2ps/archive/v8.13.18.tar.gz"
+  sha256 "4855a55ab2ee046e2d11ee30f453a5bef81870d32421fde4305c4850a3c68760"
 
   bottle do
     sha256 "4cc941a6caa74f2b2c0f7e816a657cbda6abaa6d9d8e20cb7bc36ffcc13dcbe5" => :high_sierra
@@ -14,7 +14,6 @@ class Abcm2ps < Formula
   depends_on "pango" => :optional
 
   def install
-    chmod 0755, "configure" # remove for > 8.13.15
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
