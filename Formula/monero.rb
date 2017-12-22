4a619d5001454be91fa9956fe261c88d9770db59
aimileus <aimilius@protonmail.com>
2017-12-22 21:04:32 +0100

    monero 0.11.1.0 (new formula)
    Closes #22056.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/monero.rb
@@ -0,0 +1,28 @@
+class Monero < Formula
+  desc "Official monero wallet and cpu miner"
+  homepage "https://getmonero.org/"
+  url "https://github.com/monero-project/monero/archive/v0.11.1.0.tar.gz"
+  sha256 "b5b48d3e5317c599e1499278580e9a6ba3afc3536f4064fcf7b20840066a509b"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "boost"
+  depends_on "openssl"
+  depends_on "readline"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    cmd = "#{bin}/monero-wallet-cli --restore-deterministic-wallet " \
+      "--password brew-test --restore-height 1 --generate-new-wallet wallet " \
+      "--electrum-seed 'baptism cousin whole exquisite bobsled fuselage left " \
+      "scoop emerge puzzled diet reinvest basin feast nautical upon mullet " \
+      "ponies sixteen refer enhanced maul aztec bemused basin'" \
+      "--command address"
+    address = "4BDtRc8Ym9wGzx8vpkQQvpejxBNVpjEmVBebBPCT4XqvMxW3YaCALFraiQibejyMAxUXB5zqn4pVgHVm3JzhP2WzVAJDpHf"
+    assert_equal address, shell_output(cmd).split[-1]
+  end
+end
