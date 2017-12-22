cba77ffd37526f02d12628fc51de6c5c810498c6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-22 15:57:11 +0000

    radamsa: update 0.5 bottle.

--- Formula/radamsa.rb
+++ Formula/radamsa.rb
@@ -6,11 +6,9 @@ class Radamsa < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f8e50d1dcb62a824b4ec3ae2c768a454deb3459d43ea57ef1fe610a0e1741c44" => :high_sierra
-    sha256 "6fdb159c26f40239fbc9cb382ac42770ffae948d043e6972ba6c23c922b88587" => :sierra
-    sha256 "44569c384623b1419a5431a8ba9c48743854bfb3ea79e3736bb1ad63d1fe04a1" => :el_capitan
-    sha256 "184f9a9d1c030ab7ee83b84236f3749e1d3e0de11214166b264c4f52af902fc6" => :yosemite
-    sha256 "fd10a5879e5d80d0decfc10b96a9a2bdbdbb3ce667b030a51ff152566020353b" => :mavericks
+    sha256 "a971e3bf09f3854d724549a31b98854458b8c49cdfd88593fb14c380066d7bc1" => :high_sierra
+    sha256 "d13369632654e12471ff029aa6c08f57e9572df60b9d5b18040ce341ca8b4b09" => :sierra
+    sha256 "3b09d787e73444964136ab042bc458610eb4cf08f4ba015cbe7e1d13ab8509f5" => :el_capitan
   end
 
   def install
