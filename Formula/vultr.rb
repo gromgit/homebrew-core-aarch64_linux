28a3efcf6ff9e708502222efe961a88e8a01b401
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-22 10:30:44 +0000

    vultr: update 1.13.0 bottle.

--- Formula/vultr.rb
+++ Formula/vultr.rb
@@ -7,10 +7,10 @@ class Vultr < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "68afe7d76e46923de67f5fe6b7391ff08d0c310937db2e54c04392f19f526dcf" => :high_sierra
-    sha256 "05a2602609fd20151e9ab712cd823239e125a5915e5e72f14afa9641a61dc673" => :sierra
-    sha256 "0f8ed829f501fad2a7f05e26cfbe7d8b32e3a5df4647021dbe8a9eed16bde438" => :el_capitan
-    sha256 "b2bf0d14a74da9e561510fd75694063c8d50ca253b6411940be4ce82a3d2e4ee" => :yosemite
+    rebuild 1
+    sha256 "afc91aa3d98447dcdc93892d94dc2b00ed5d4af0b6f1095a80ae4641f0abb23b" => :high_sierra
+    sha256 "87683a1ade9779c8431f08a76a3801890a437a499144646c5232db0567ea4712" => :sierra
+    sha256 "7ca573144e22487f7c506073cda826ba46e768ab0f34230b1f2375799580c39b" => :el_capitan
   end
 
   depends_on "go" => :build
