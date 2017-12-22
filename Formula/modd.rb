c1f4cbda870505104dd54514e609fff2fea50f59
commitay <commitay@users.noreply.github.com>
2017-12-22 18:09:40 +1000

    modd: switch to upstream vendoring
    Closes #22025.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/modd.rb
+++ Formula/modd.rb
@@ -1,5 +1,3 @@
-require "language/go"
-
 class Modd < Formula
   desc "Flexible tool for responding to filesystem changes"
   homepage "https://github.com/cortesi/modd"
@@ -16,23 +14,12 @@ class Modd < Formula
 
   depends_on "go" => :build
 
-  go_resource "github.com/cortesi/moddwatch" do
-    url "https://github.com/cortesi/moddwatch.git",
-        :revision => "a149019f9ed6f16033de28f66d8c1247593a0104"
-  end
-
-  go_resource "github.com/cortesi/termlog" do
-    url "https://github.com/cortesi/termlog.git",
-        :revision => "2ed14eb6ce62ec5bcc3fd25885a1d13d53f34fd1"
-  end
-
   def install
     ENV["GOOS"] = "darwin"
     ENV["GOARCH"] = MacOS.prefer_64_bit? ? "amd64" : "386"
     ENV["GOPATH"] = buildpath
     ENV["GOBIN"] = bin
     (buildpath/"src/github.com/cortesi/modd").install buildpath.children
-    Language::Go.stage_deps resources, buildpath/"src"
     cd "src/github.com/cortesi/modd" do
       system "go", "install", ".../cmd/modd"
       prefix.install_metafiles
