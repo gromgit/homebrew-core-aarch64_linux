d2653a4d40bae90e7d44bf7561c8004777ca6e2c
Mike Morris <mikemorris@users.noreply.github.com>
2017-12-23 11:04:49 -0500

    gr-osmosdr 0.1.4 (new formula)
    Closes #22083.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/gr-osmosdr.rb
@@ -0,0 +1,45 @@
+class GrOsmosdr < Formula
+  desc "Osmocom GNU Radio Blocks"
+  homepage "https://osmocom.org/projects/sdr/wiki/GrOsmoSDR"
+  url "https://cgit.osmocom.org/gr-osmosdr/snapshot/gr-osmosdr-0.1.4.tar.gz"
+  mirror "https://github.com/osmocom/gr-osmosdr/archive/v0.1.4.tar.gz"
+  sha256 "bcf9a9b1760e667c41a354e8cd41ef911d0929d5e4a18e0594ccb3320d735066"
+
+  depends_on "cmake" => :build
+  depends_on "swig" => :build
+  depends_on "airspy"
+  depends_on "boost"
+  depends_on "gnuradio"
+  depends_on "hackrf"
+  depends_on "librtlsdr"
+  depends_on "python"
+  depends_on "uhd"
+
+  resource "Cheetah" do
+    url "https://files.pythonhosted.org/packages/source/C/Cheetah/Cheetah-2.4.4.tar.gz"
+    sha256 "be308229f0c1e5e5af4f27d7ee06d90bb19e6af3059794e5fd536a6f29a9b550"
+  end
+
+  def install
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+
+    resource("Cheetah").stage do
+      system "python", *Language::Python.setup_install_args(libexec/"vendor")
+    end
+
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <osmosdr/device.h>
+      int main() {
+        osmosdr::device_t device();
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.cpp", "-L#{lib}", "-lgnuradio-osmosdr", "-o", "test"
+    system "./test"
+  end
+end
