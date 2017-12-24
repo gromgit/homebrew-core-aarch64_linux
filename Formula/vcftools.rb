7fb73e28f72f5c309cb25fd60b2349b820285098
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-24 18:31:43 +0000

    vcftools: add 0.1.15 bottle.

--- Formula/vcftools.rb
+++ Formula/vcftools.rb
@@ -4,6 +4,13 @@ class Vcftools < Formula
   url "https://github.com/vcftools/vcftools/releases/download/v0.1.15/vcftools-0.1.15.tar.gz"
   sha256 "31e47afd5be679d89ece811a227525925b6907cce4af2c86f10f465e080383e3"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8885f5eb915bd7bd7b270b421d23aa3b9b600f53e009ddb02ead70ec1a7d7488" => :high_sierra
+    sha256 "39f16509c8bd0f394044ea8a9f59e10c7764c1289975f6c0de6b1a4e2bd630ae" => :sierra
+    sha256 "d93943d5f24309df2165ea3dd507c4b00b75ef166d0b99a55e8244f98eb2bfbc" => :el_capitan
+  end
+
   depends_on "pkg-config" => :build
   depends_on "htslib"
 
