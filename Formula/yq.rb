0cdb488cf3501386e9fcc3341d17f2601688b061
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-24 17:31:34 +0000

    yq: add 1.14.0 bottle.

--- Formula/yq.rb
+++ Formula/yq.rb
@@ -4,6 +4,13 @@ class Yq < Formula
   url "https://github.com/mikefarah/yq/archive/1.14.0.tar.gz"
   sha256 "cf61f84033ed8e1cb146798bfcedfabba5fa8852331a90122eafaae0f31c4b70"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9a77152a4f6af9543e0f021297492a80ad790cc33c28a102052cef21941b2755" => :high_sierra
+    sha256 "c664274a8f439a63e0de3b74ba918df70412f8effc71d0ee5b998e9ebd88b716" => :sierra
+    sha256 "66137bf0dbc35984d6492e3e29f5aa7f0c5d04889f1fa7880aad93067d3692b5" => :el_capitan
+  end
+
   depends_on "go" => :build
   depends_on "govendor" => :build
 
