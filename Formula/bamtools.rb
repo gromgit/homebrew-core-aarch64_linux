6899e00237a950d165cd8bd004f09883caccd51f
ilovezfs <ilovezfs@icloud.com>
2017-12-23 21:39:13 -0800

    bamtools 2.5.1
    Closes #22087.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/bamtools.rb
+++ Formula/bamtools.rb
@@ -1,8 +1,8 @@
 class Bamtools < Formula
   desc "C++ API and command-line toolkit for BAM data"
   homepage "https://github.com/pezmaster31/bamtools"
-  url "https://github.com/pezmaster31/bamtools/archive/v2.5.0.tar.gz"
-  sha256 "85e02e04998a67cbda7ab68cdab36cee133db024e814b34e06bb617b627caf9c"
+  url "https://github.com/pezmaster31/bamtools/archive/v2.5.1.tar.gz"
+  sha256 "4abd76cbe1ca89d51abc26bf43a92359e5677f34a8258b901a01f38c897873fc"
   head "https://github.com/pezmaster31/bamtools.git"
 
   bottle do
