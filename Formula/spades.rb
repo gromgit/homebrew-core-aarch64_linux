2dce08e2a67f3dba710e1b9e154a8f05d4c79840
ilovezfs <ilovezfs@icloud.com>
2017-12-25 01:32:40 -0800

    spades: depend on gcc
    `needs :openmp` causes the bottles to be linked to gcc
    
    Closes #22115.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/spades.rb
+++ Formula/spades.rb
@@ -3,6 +3,7 @@ class Spades < Formula
   homepage "http://bioinf.spbau.ru/spades/"
   url "http://cab.spbu.ru/files/release3.11.1/SPAdes-3.11.1.tar.gz"
   sha256 "3ab85d86bf7d595bd8adf11c971f5d258bbbd2574b7c1703b16d6639a725b474"
+  revision 1
 
   bottle do
     cellar :any
@@ -12,6 +13,7 @@ class Spades < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on "gcc"
   depends_on :python if MacOS.version <= :snow_leopard
 
   needs :openmp
