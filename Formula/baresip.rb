af8c689bd6670ded35275bcbe48ba23bf1950bbb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-25 18:22:13 +0000

    baresip: update 0.5.7 bottle.

--- Formula/baresip.rb
+++ Formula/baresip.rb
@@ -5,9 +5,9 @@ class Baresip < Formula
   sha256 "e66c16c7cf13bb3f43113f04e4d19edea3e49fb010d067238d5dc4a1aa1b8b7d"
 
   bottle do
-    sha256 "52957fde6a1de8bb879bd6774284ba592f41a30d06b7abb3ba88c47ee8781009" => :high_sierra
-    sha256 "36ebce2b54bb048498c6fa0a5e180a54567e452405279ca1d36134787d0bd819" => :sierra
-    sha256 "71bdeef11d01598f0cb1e6e16846621b58161468bee5f53636e5167ab23fbf8f" => :el_capitan
+    sha256 "6c8df83b6b8be52a9abd1124fe448eb98a799de376b13836223947b3d0a49be0" => :high_sierra
+    sha256 "245523e5efe7d9937690f514bd0c9595ea5daf4e9534f1f3fc6bb3429be580b6" => :sierra
+    sha256 "b57c146a805fd2ea21355aeff0de7d668e8ab3007397324234737f903b5af887" => :el_capitan
   end
 
   depends_on "librem"
