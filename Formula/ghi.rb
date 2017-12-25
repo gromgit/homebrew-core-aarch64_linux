0cf882c2db648dce8385664c6564dca808a4fb3e
Chongyu Zhu <i@lembacon.com>
2017-12-25 22:14:46 +0800

    ghi: revision for ruby

--- Formula/ghi.rb
+++ Formula/ghi.rb
@@ -3,7 +3,7 @@ class Ghi < Formula
   homepage "https://github.com/stephencelis/ghi"
   url "https://github.com/stephencelis/ghi/archive/1.2.0.tar.gz"
   sha256 "ffc17cfbdc8b88bf208f5f762e62c211bf8fc837f447354ad53cce39b1400671"
-  revision 2
+  revision 3
   head "https://github.com/stephencelis/ghi.git"
 
   bottle do
