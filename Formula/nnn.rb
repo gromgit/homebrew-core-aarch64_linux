d49af62435bf2f9251e9169441b539c3061288c0
Zhiming Wang <zmwangx@gmail.com>
2017-12-25 09:43:22 -0500

    nnn 1.6
    depend on readline
    
    Closes #22119.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/nnn.rb
+++ Formula/nnn.rb
@@ -1,8 +1,8 @@
 class Nnn < Formula
   desc "Free, fast, friendly file browser"
   homepage "https://github.com/jarun/nnn"
-  url "https://github.com/jarun/nnn/archive/v1.5.tar.gz"
-  sha256 "f50f59953c29408963bbb961891155bd0a1fe2072d4441cc0ff927b128725c7f"
+  url "https://github.com/jarun/nnn/archive/v1.6.tar.gz"
+  sha256 "e8b10a3b9847ba7ad3317f608691aaebcdaf2b67219d732f7a5d468221d3e83e"
 
   bottle do
     cellar :any_skip_relocation
@@ -10,6 +10,8 @@ class Nnn < Formula
     sha256 "116b742a578044fe4309cee847b72d15b0687c19887b8dd65bc1fac1a5d27eeb" => :sierra
     sha256 "26901989e2a66d9ec8806ea21b931e66ab8b6f4a1a35544b4da473a8b490e692" => :el_capitan
   end
+
+  depends_on "readline"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
@@ -20,7 +22,7 @@ class Nnn < Formula
     require "pty"
     PTY.spawn(bin/"nnn") do |r, w, _pid|
       w.write "q"
-      assert_match "cwd: #{testpath.realpath}", r.read
+      assert_match testpath.realpath.to_s, r.read
     end
   end
 end
