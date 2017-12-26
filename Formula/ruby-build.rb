7bdcecbdcd9a9f87fd619c6fea16c32b1480e5cb
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2017-12-26 11:39:51 +0900

    ruby-build 20171226 (#22135)

--- Formula/ruby-build.rb
+++ Formula/ruby-build.rb
@@ -1,8 +1,8 @@
 class RubyBuild < Formula
   desc "Install various Ruby versions and implementations"
   homepage "https://github.com/rbenv/ruby-build"
-  url "https://github.com/rbenv/ruby-build/archive/v20171215.tar.gz"
-  sha256 "3aeafcfe6848c6cec620a49b72d6820a078254ef382dca7ec2174b925dc73c81"
+  url "https://github.com/rbenv/ruby-build/archive/v20171226.tar.gz"
+  sha256 "95f66250ea088f8f68743a27bc9f2e48caf6160816845ff9146212b6344632d6"
   head "https://github.com/rbenv/ruby-build.git"
 
   bottle :unneeded
