75bbd05e4179c6d936f2c14b9ae7d6c95b3ef1e3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-28 11:10:01 +0000

    snapraid: update 11.2 bottle.

--- Formula/snapraid.rb
+++ Formula/snapraid.rb
@@ -5,10 +5,9 @@ class Snapraid < Formula
   sha256 "735cdeb7656ac48cbb0b4a89a203dd566505071e465d5effbcc56bcb8fd3a0d7"
 
   bottle do
-    sha256 "9e637d417327e2d8db1619a67b7e2521f7602cf8968d76148563006071139625" => :high_sierra
-    sha256 "ee0f9a795f2fa1af515eee0733f7e9d20e10fe2971057264d54f748faebbb845" => :sierra
-    sha256 "ccff98a96d5136cbe1e8e64f50190100e909fa194ff623b4adf7e18b3dc201fd" => :el_capitan
-    sha256 "fcf97bda192b15c62638304046aef135f23766f6b9cfd0fa75e643fe69005251" => :yosemite
+    sha256 "a3cdc8a3975bf5f3073e21d089d5e3c067fb809ccf758724f25fc092bebf7b71" => :high_sierra
+    sha256 "eba431c9a6d92f379e0063c3c40cf6a655fa2a29256da707f851569046e7d7c9" => :sierra
+    sha256 "e6be5ae4ae3aa3bffbd4f57c4dab84602d3883673ce9f2b74189a0f39c82b4b2" => :el_capitan
   end
 
   head do
