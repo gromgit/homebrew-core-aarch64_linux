cc5554d2334c6bd8208cb33a083ff643e18c8fed
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-28 06:38:45 +0000

    mdp: update 1.0.11 bottle.

--- Formula/mdp.rb
+++ Formula/mdp.rb
@@ -7,10 +7,9 @@ class Mdp < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "33eba760559692e021171441d4c62444f503045150f65734b0d4c53fec4c8b9e" => :high_sierra
-    sha256 "5ede61a6bd6ece5dfc361f03b8bb9ef11fec560e4d49e2db66fd9d1df5e9693f" => :sierra
-    sha256 "aae896e1e1b4b5615d0440cfc92e541dabcc760d7a9f60527655fbcb7c61ac93" => :el_capitan
-    sha256 "1518c8a1c67a38cd92053823e21712712623d3637497f86b65807b982919d0c0" => :yosemite
+    sha256 "b6dda2a53ef5ef16249ced6daf96de3bab9fd742106fec8b7eeb23f954c70e5f" => :high_sierra
+    sha256 "17c2e33bb7953fe8cbdfb1e8a70c9d5f0b3346023d9dd4670ad8df82fb5c7fcd" => :sierra
+    sha256 "5c54089e38d0cb93bc24cc39fdb94148539955b6d682707dd263aef039cf15c8" => :el_capitan
   end
 
   def install
