95c0451799def088efebaee6933541239dc1a08d
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-28 09:28:01 +0000

    websocketd: update 0.3.0 bottle.

--- Formula/websocketd.rb
+++ Formula/websocketd.rb
@@ -8,12 +8,9 @@ class Websocketd < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "279b5cd3903dd5c7102f0816b8313da2db11ea8a2e0f939973ed931018569784" => :high_sierra
-    sha256 "d846ab4fd91bbaac4734b7b552f2ad2cf1e40768a1ecfdd0529d2f791726efd5" => :sierra
-    sha256 "560f7a2a5455bbd31f0f30f3beba292543bfb1e7fae6a1ab5d0b2c1b3ebe6904" => :el_capitan
-    sha256 "1b525264a32863baf094ec1e6e2a9b932b79aa51135cd20b26c086a970294d48" => :yosemite
-    sha256 "db7d13cd8b3888f830f2c2c566a045dce4ad3cfb18c193472c5f19fe26d1700d" => :mavericks
+    sha256 "69523ab9efd2f2b8f595b4ae4d282f6af64fbbe24fd3b16b7011210b8d1ea87b" => :high_sierra
+    sha256 "38ebd8e10260501352e81966e3bba46bf00d38e00b408f763ef8042a418099e3" => :sierra
+    sha256 "eec1080e0a40bf336ea48950c1c21e0ab50e038ee46874cb59bd6b16791309a3" => :el_capitan
   end
 
   depends_on "go" => :build
