c975f91c3662ab3e2f442645e17641b429dd38c5
Greg Hurrell <greg@hurrell.net>
2017-12-28 13:18:32 -0800

    clipper 1.0
    Closes #22209.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/clipper.rb
+++ Formula/clipper.rb
@@ -1,8 +1,8 @@
 class Clipper < Formula
   desc "Share macOS clipboard with tmux and other local and remote apps"
   homepage "https://wincent.com/products/clipper"
-  url "https://github.com/wincent/clipper/archive/0.4.2.tar.gz"
-  sha256 "e707e5f9295dd793755007fdcadae63073b91e949f115d01b1a6cc6b4642cd6d"
+  url "https://github.com/wincent/clipper/archive/1.0.tar.gz"
+  sha256 "f6f6aa069a941d71a4ddc33178caab1eef999c18f8c75ad379b16d97b05e3bf8"
 
   bottle do
     cellar :any_skip_relocation
