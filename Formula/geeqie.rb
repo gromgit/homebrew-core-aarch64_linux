e3329b60d2503bf88d49f03f47f695b573633164
Tommy Sparber <tommy@sparber.net>
2017-12-29 10:09:44 +0100

    geeqie: depend on adwaita-icon-theme
    Closes #22225.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/geeqie.rb
+++ Formula/geeqie.rb
@@ -3,7 +3,7 @@ class Geeqie < Formula
   homepage "http://www.geeqie.org/"
   url "http://www.geeqie.org/geeqie-1.3.tar.xz"
   sha256 "4b6f566dd1a8badac68c4353c7dd0f4de17f8627b85a7a70d5eb1ae3b540ec3f"
-  revision 2
+  revision 3
 
   bottle do
     sha256 "ee45d42101e3fdcee8f122a87e32e2b8e6e5e0ee93f6e826c4a2e89ce2ade87b" => :high_sierra
@@ -28,6 +28,7 @@ class Geeqie < Formula
   depends_on "imagemagick"
   depends_on "exiv2"
   depends_on "little-cms2"
+  depends_on "adwaita-icon-theme"
 
   def install
     ENV["NOCONFIGURE"] = "yes"
