4b45a6f5adb60f7b3f1cb3d167a729ec744fe381
ilovezfs <ilovezfs@icloud.com>
2017-12-29 08:49:52 -0800

    rbenv-communal-gems: depend on rbenv instead of :rbenv

--- Formula/rbenv-communal-gems.rb
+++ Formula/rbenv-communal-gems.rb
@@ -3,10 +3,11 @@ class RbenvCommunalGems < Formula
   homepage "https://github.com/tpope/rbenv-communal-gems"
   url "https://github.com/tpope/rbenv-communal-gems/archive/v1.0.1.tar.gz"
   sha256 "99f1c0be6721e25037f964015cbd2622d70603ceeeaef58f040410ac3697d766"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :rbenv
+  depends_on "rbenv"
 
   def install
     prefix.install Dir["*"]
