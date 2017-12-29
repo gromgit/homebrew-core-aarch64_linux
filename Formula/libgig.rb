86b09d657ebdca63dcc728459c68afaeac0f17cb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-29 06:39:20 +0000

    libgig: update 4.1.0 bottle.

--- Formula/libgig.rb
+++ Formula/libgig.rb
@@ -6,11 +6,9 @@ class Libgig < Formula
 
   bottle do
     cellar :any
-    sha256 "8d78da0e18660f690073643fec535ab972b7ab34a53136c3ab2c6d12063437c9" => :high_sierra
-    sha256 "d8f007c17aa5098d5f7a05d60403c141d21a66a78754639402549a4da2f6e624" => :sierra
-    sha256 "3f669e4d7c16bd6eff156c5e36c62969e68c06177a4518424dab1c7ed12e7f43" => :el_capitan
-    sha256 "b1483446d24800cffa43ff8d3fa94d0c2ee906fefbc84cf84ea93d046e58b2f4" => :yosemite
-    sha256 "52cfa0b813caee97ecbae5128f39329a0124ba60f3328c1db2bf296e839403ed" => :mavericks
+    sha256 "e76d0051885444442b71bd65228a5b777c83640d4088c6d8c07ef56a88ef68d6" => :high_sierra
+    sha256 "52a948f55300ca0b67e346fa987c4967cabcaf0c7e43698c86ef4dd2f5add67d" => :sierra
+    sha256 "ead7f19fe837a7b38ef753993ad3c73b36a07269d1c12c9d1220204fa46a046a" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
