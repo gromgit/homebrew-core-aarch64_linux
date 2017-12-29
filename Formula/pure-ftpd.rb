014e813653ef3b4ed68f13d06f82e443b1922d22
ilovezfs <ilovezfs@icloud.com>
2017-12-29 08:33:24 -0800

    pure-ftpd: depend on mysql instead of :mysql

--- Formula/pure-ftpd.rb
+++ Formula/pure-ftpd.rb
@@ -15,7 +15,7 @@ class PureFtpd < Formula
   depends_on "libsodium"
   depends_on "openssl"
   depends_on :postgresql => :optional
-  depends_on :mysql => :optional
+  depends_on "mysql" => :optional
 
   def install
     args = %W[
