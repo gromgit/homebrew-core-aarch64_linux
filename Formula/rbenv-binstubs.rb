a8b25faab8494029564910cb594375b4235eddf3
ilovezfs <ilovezfs@icloud.com>
2017-12-29 08:49:51 -0800

    rbenv-binstubs: depend on rbenv instead of :rbenv

--- Formula/rbenv-binstubs.rb
+++ Formula/rbenv-binstubs.rb
@@ -3,11 +3,12 @@ class RbenvBinstubs < Formula
   homepage "https://github.com/ianheggie/rbenv-binstubs"
   url "https://github.com/ianheggie/rbenv-binstubs/archive/v1.5.tar.gz"
   sha256 "305000b8ba5b829df1a98fc834b7868b9e817815c661f429b0e28c1f613f4d0c"
+  revision 1
   head "https://github.com/ianheggie/rbenv-binstubs.git"
 
   bottle :unneeded
 
-  depends_on :rbenv
+  depends_on "rbenv"
 
   def install
     prefix.install Dir["*"]
