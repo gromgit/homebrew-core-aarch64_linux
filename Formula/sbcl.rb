204ce9cd7e606c0aa60e36d4b3dabe7f1b0038fb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-29 14:43:16 +0000

    sbcl: update 1.4.3 bottle.

--- Formula/sbcl.rb
+++ Formula/sbcl.rb
@@ -6,9 +6,9 @@ class Sbcl < Formula
   head "https://git.code.sf.net/p/sbcl/sbcl.git"
 
   bottle do
-    sha256 "5b50498715ab1d7452af3bfa68fc8bbed8e8b73769e6df2003367ca72c4dd0db" => :high_sierra
-    sha256 "936689e97e116ff94295f6a4ecd92e9f0f53e24e54fab305effd3bf86a7105e7" => :sierra
-    sha256 "dabe32ef602a1cdb2acbb1a17b7c202128c82f11b6b41d36634a64d9959cc21b" => :el_capitan
+    sha256 "f1456046c87d863706143db8907f6c4c7ea36f3e01ae1fa409e44819bd92d2f8" => :high_sierra
+    sha256 "4a66a05728908b31f379cc2001cfcbaf9e492000fb090ec4bc0d949b7f198304" => :sierra
+    sha256 "6c3cf43dee10c81d6f13d4483da48b824702bb039f963d7f13e062db58ab6689" => :el_capitan
   end
 
   option "with-internal-xref", "Include XREF information for SBCL internals (increases core size by 5-6MB)"
