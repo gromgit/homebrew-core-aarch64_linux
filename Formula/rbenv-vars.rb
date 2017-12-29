47b8250f3e9a599bf4f1b5112e72ff321128cc82
ilovezfs <ilovezfs@icloud.com>
2017-12-29 08:49:53 -0800

    rbenv-vars: depend on rbenv instead of :rbenv

--- Formula/rbenv-vars.rb
+++ Formula/rbenv-vars.rb
@@ -3,11 +3,12 @@ class RbenvVars < Formula
   homepage "https://github.com/sstephenson/rbenv-vars"
   url "https://github.com/sstephenson/rbenv-vars/archive/v1.2.0.tar.gz"
   sha256 "9e6a5726aad13d739456d887a43c220ba9198e672b32536d41e884c0a54b4ddb"
+  revision 1
   head "https://github.com/sstephenson/rbenv-vars.git"
 
   bottle :unneeded
 
-  depends_on :rbenv
+  depends_on "rbenv"
 
   def install
     prefix.install Dir["*"]
