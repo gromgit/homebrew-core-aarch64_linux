82a9e1a2e826ee343f97fbb082f0624191001798
ilovezfs <ilovezfs@icloud.com>
2017-12-29 08:46:48 -0800

    sphinx: depend on postgresql instead of :postgresql

--- Formula/sphinx.rb
+++ Formula/sphinx.rb
@@ -23,7 +23,7 @@ class Sphinx < Formula
 
   depends_on "re2" => :optional
   depends_on "mysql" => :optional
-  depends_on :postgresql => :optional
+  depends_on "postgresql" => :optional
   depends_on "openssl" if build.with? "mysql"
 
   resource "stemmer" do
