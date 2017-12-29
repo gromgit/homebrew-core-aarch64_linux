8debbf97c4091b868a5452c0ee932434bf572f07
ilovezfs <ilovezfs@icloud.com>
2017-12-29 08:49:51 -0800

    rbenv-aliases: depend on rbenv instead of :rbenv

--- Formula/rbenv-aliases.rb
+++ Formula/rbenv-aliases.rb
@@ -3,11 +3,12 @@ class RbenvAliases < Formula
   homepage "https://github.com/tpope/rbenv-aliases"
   url "https://github.com/tpope/rbenv-aliases/archive/v1.1.0.tar.gz"
   sha256 "12e89bc4499e85d8babac2b02bc8b66ceb0aa3f8047b26728a3eca8a6030273d"
+  revision 1
   head "https://github.com/tpope/rbenv-aliases.git"
 
   bottle :unneeded
 
-  depends_on :rbenv
+  depends_on "rbenv"
 
   def install
     prefix.install Dir["*"]
