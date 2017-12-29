a55dadd0a27f70371eea05685d59b00257b09092
ilovezfs <ilovezfs@icloud.com>
2017-12-29 08:49:53 -0800

    rbenv-default-gems: depend on rbenv instead of :rbenv

--- Formula/rbenv-default-gems.rb
+++ Formula/rbenv-default-gems.rb
@@ -3,11 +3,12 @@ class RbenvDefaultGems < Formula
   homepage "https://github.com/sstephenson/rbenv-default-gems"
   url "https://github.com/sstephenson/rbenv-default-gems/archive/v1.0.0.tar.gz"
   sha256 "8271d58168ab10f0ace285dc4c394e2de8f2d1ccc24032e6ed5924f38dc24822"
+  revision 1
   head "https://github.com/sstephenson/rbenv-default-gems.git"
 
   bottle :unneeded
 
-  depends_on :rbenv
+  depends_on "rbenv"
 
   # Upstream patch: https://github.com/sstephenson/rbenv-default-gems/pull/3
   patch do
