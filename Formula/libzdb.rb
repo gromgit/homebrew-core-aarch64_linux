351779a00a555b0bca99d59701cb9b02be71505b
ilovezfs <ilovezfs@icloud.com>
2017-12-29 08:33:22 -0800

    libzdb: depend on mysql instead of :mysql

--- Formula/libzdb.rb
+++ Formula/libzdb.rb
@@ -3,7 +3,7 @@ class Libzdb < Formula
   homepage "http://tildeslash.com/libzdb/"
   url "http://tildeslash.com/libzdb/dist/libzdb-3.1.tar.gz"
   sha256 "0f01abb1b01d1a1f4ab9b55ad3ba445d203fc3b4757abdf53e1d85e2b7b42695"
-  revision 3
+  revision 4
 
   bottle do
     cellar :any
@@ -15,7 +15,7 @@ class Libzdb < Formula
 
   depends_on "openssl"
   depends_on :postgresql => :recommended
-  depends_on :mysql => :recommended
+  depends_on "mysql" => :recommended
   depends_on "sqlite" => :recommended
 
   def install
