a7cbc65f41581b02abf5d6734507d4a0b565114f
ilovezfs <ilovezfs@icloud.com>
2017-12-29 08:49:53 -0800

    rbenv-whatis: depend on rbenv instead of :rbenv

--- Formula/rbenv-whatis.rb
+++ Formula/rbenv-whatis.rb
@@ -3,10 +3,11 @@ class RbenvWhatis < Formula
   homepage "https://github.com/rkh/rbenv-whatis"
   url "https://github.com/rkh/rbenv-whatis/archive/v1.0.0.tar.gz"
   sha256 "1a09f824d1dcbca360565930fa66e93a9a2840c1bb45935e2ee989ce57d1f6e6"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :rbenv
+  depends_on "rbenv"
 
   def install
     prefix.install Dir["*"]
