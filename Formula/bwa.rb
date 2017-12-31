6f0cf4a7c05b6fba576d5500f1f63c974d82a9e5
ilovezfs <ilovezfs@icloud.com>
2017-12-31 01:32:49 -0800

    bwa 0.7.17
    Closes #22284.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/bwa.rb
+++ Formula/bwa.rb
@@ -1,8 +1,8 @@
 class Bwa < Formula
   desc "Burrow-Wheeler Aligner for pairwise alignment of DNA"
   homepage "https://github.com/lh3/bwa"
-  url "https://github.com/lh3/bwa/releases/download/v0.7.16/bwa-0.7.16a.tar.bz2"
-  sha256 "8fecdb5f88871351bbe050c18d6078121456c36ad75c5c78f33a926560ffc170"
+  url "https://github.com/lh3/bwa/releases/download/v0.7.17/bwa-0.7.17.tar.bz2"
+  sha256 "de1b4d4e745c0b7fc3e107b5155a51ac063011d33a5d82696331ecf4bed8d0fd"
 
   bottle do
     cellar :any_skip_relocation
