a96734e82aaf1dc9528c05dbdd7f7aca279021fc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-31 08:58:09 +0000

    kallisto: add 0.43.1_1 bottle.

--- Formula/kallisto.rb
+++ Formula/kallisto.rb
@@ -5,6 +5,13 @@ class Kallisto < Formula
   sha256 "2164938c2c61c04e338c4c132cf749f56d39e6f0b4c517121bca1fbc218e430e"
   revision 1
 
+  bottle do
+    cellar :any
+    sha256 "7def510bff1b872bff522fc561ccf197d2810f1d8df8fa3bf1870225ec74bd6a" => :high_sierra
+    sha256 "975be5f037dfc6a47b65ef79bc3c3a9a0583e7eaa36add776a034a7bdf7f3893" => :sierra
+    sha256 "9174135661c12414338e2a5280ee31951f97e8d55bd30ba423099e9b872da42e" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "hdf5"
 
