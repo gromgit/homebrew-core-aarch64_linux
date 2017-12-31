fe9011fa38ffc204bd20e6ffa1804350940a5eb5
ilovezfs <ilovezfs@icloud.com>
2017-12-31 03:50:19 -0800

    tclap 1.2.2
    Closes #22287.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/tclap.rb
+++ Formula/tclap.rb
@@ -1,8 +1,8 @@
 class Tclap < Formula
   desc "Templatized C++ command-line parser library"
   homepage "https://tclap.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/tclap/tclap-1.2.1.tar.gz"
-  sha256 "9f9f0fe3719e8a89d79b6ca30cf2d16620fba3db5b9610f9b51dd2cd033deebb"
+  url "https://downloads.sourceforge.net/project/tclap/tclap-1.2.2.tar.gz"
+  sha256 "f5013be7fcaafc69ba0ce2d1710f693f61e9c336b6292ae4f57554f59fde5837"
 
   bottle do
     cellar :any_skip_relocation
