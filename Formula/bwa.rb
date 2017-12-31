810e9824ad010c85edfa84441b9bbe5a39651c2b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-31 09:18:49 +0000

    bwa: add 0.7.16a bottle.

--- Formula/bwa.rb
+++ Formula/bwa.rb
@@ -4,6 +4,13 @@ class Bwa < Formula
   url "https://github.com/lh3/bwa/releases/download/v0.7.16/bwa-0.7.16a.tar.bz2"
   sha256 "8fecdb5f88871351bbe050c18d6078121456c36ad75c5c78f33a926560ffc170"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "37fe50e4da8282601d28639f6c9dc088363ed47c9f7068a067af529bdbb02f7d" => :high_sierra
+    sha256 "d43c7d65184ae3b31ea6bb3752080b6f5d52dad3cdd1492de453330b1dba219b" => :sierra
+    sha256 "9c72db876f099ef599bc4d77b07dbc5935aedba837fc7cb269e331c5ebfcfef2" => :el_capitan
+  end
+
   def install
     system "make"
 
