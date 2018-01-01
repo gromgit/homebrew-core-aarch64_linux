1e01fb58e693b5509fcdc127a2898ee438ba9216
ilovezfs <ilovezfs@icloud.com>
2018-01-01 11:42:22 -0800

    arcade-learning-environment: depend on python instead of :python

--- Formula/arcade-learning-environment.rb
+++ Formula/arcade-learning-environment.rb
@@ -13,7 +13,7 @@ class ArcadeLearningEnvironment < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on :python if MacOS.version <= :snow_leopard
+  depends_on "python" if MacOS.version <= :snow_leopard
   depends_on "sdl"
 
   def install
