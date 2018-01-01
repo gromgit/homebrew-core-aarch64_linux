f870cf1372c4dee5213120c2360af67cf4f216c8
ilovezfs <ilovezfs@icloud.com>
2018-01-01 11:42:41 -0800

    rst-lint: depend on python instead of :python

--- Formula/rst-lint.rb
+++ Formula/rst-lint.rb
@@ -13,7 +13,7 @@ class RstLint < Formula
     sha256 "e41c9a9a6aa7d3527686354d46dd2ec1b203063c282acd82f28154400bc4b5c9" => :el_capitan
   end
 
-  depends_on :python if MacOS.version <= :snow_leopard
+  depends_on "python" if MacOS.version <= :snow_leopard
 
   resource "docutils" do
     url "https://files.pythonhosted.org/packages/84/f4/5771e41fdf52aabebbadecc9381d11dea0fa34e4759b4071244fa094804c/docutils-0.14.tar.gz"
