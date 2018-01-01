1edea0729f4d758678b20ec76fc58bacd8ac1e3d
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-01 17:24:47 +0000

    libcdio: update 2.0.0 bottle.

--- Formula/libcdio.rb
+++ Formula/libcdio.rb
@@ -7,9 +7,9 @@ class Libcdio < Formula
 
   bottle do
     cellar :any
-    sha256 "0d55434bb20a118b144413e1d20c36433c12f8e3243994cf011f15d6648a7157" => :high_sierra
-    sha256 "d3cef83b0df1de2d41f1b52a0aaf3ac13a7c5e9629a327fd4cf1210fffa99466" => :sierra
-    sha256 "a950c4704df57215a64a64bb0168f74afd75826998917cfb2cf77988cf9e1208" => :el_capitan
+    sha256 "7a0da4716a8c2d0c15a474bb33df7bfaad1639b7bb80c3039b3d7e5af04c9c64" => :high_sierra
+    sha256 "d20beb64a3895d5c5b749b371cc8dc23ba50813a07c22ee10691c8b80c6ffc5c" => :sierra
+    sha256 "736e98f2264c013a19af40ca1db8695326fb96e5292d0f31accaed0fe74e50fe" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
