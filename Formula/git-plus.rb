47fbf7cdfeeff63fa4d7884d5a482df50ec6759b
ilovezfs <ilovezfs@icloud.com>
2018-01-01 11:42:30 -0800

    git-plus: depend on python instead of :python

--- Formula/git-plus.rb
+++ Formula/git-plus.rb
@@ -7,7 +7,7 @@ class GitPlus < Formula
 
   bottle :unneeded
 
-  depends_on :python if MacOS.version <= :snow_leopard
+  depends_on "python" if MacOS.version <= :snow_leopard
 
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
