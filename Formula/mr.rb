5cb3154ac5e5bd0f146313ffa154654889de890b
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-01 13:01:11 +0000

    mr: update 1.20171231 bottle.

--- Formula/mr.rb
+++ Formula/mr.rb
@@ -5,10 +5,9 @@ class Mr < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9ccfbdcf62f340df01ef135f7d3a09d8f5177d6ab414bceed9425987ba526fe2" => :high_sierra
-    sha256 "4ef14ce20e60b88a882e156aad6fbede00d0ae852355950bbb014391993edbe2" => :sierra
-    sha256 "7c84fb8bb3d5ec8dd1b175a9805c9ee92be1cc0c1f0fca81f2557c0483301661" => :el_capitan
-    sha256 "c7b3005af6e349badfa7f8f99911217ec0138b3236143189fdb715f61cb2ffe1" => :yosemite
+    sha256 "dd785ab43994c4209e80bcad7d676b641839ef6df50c5ec1bad08492c212c714" => :high_sierra
+    sha256 "952a63655282b3a8926920c103d61a37df79610ce4c03bdfe9259cc239556369" => :sierra
+    sha256 "b7af2995198e84cf2dce5ffceabd485e485c17054bd11e3f0032c07968d4b4c8" => :el_capitan
   end
 
   resource("test-repo") do
