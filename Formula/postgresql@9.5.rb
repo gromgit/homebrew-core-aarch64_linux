bc0e2f60cfc49d04a36381787e42603a5b1a4ebf
ilovezfs <ilovezfs@icloud.com>
2018-01-01 11:59:10 -0800

    postgresql@9.5: depend on python instead of :python

--- Formula/postgresql@9.5.rb
+++ Formula/postgresql@9.5.rb
@@ -20,7 +20,7 @@ class PostgresqlAT95 < Formula
 
   depends_on "openssl"
   depends_on "readline"
-  depends_on :python => :optional
+  depends_on "python" => :optional
   depends_on :python3 => :optional
 
   fails_with :clang do
