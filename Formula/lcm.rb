c08351804f52f3d1de6a1dd854a764ef2e9a9c5f
ilovezfs <ilovezfs@icloud.com>
2018-01-01 11:59:07 -0800

    lcm: depend on python instead of :python

--- Formula/lcm.rb
+++ Formula/lcm.rb
@@ -25,7 +25,7 @@ class Lcm < Formula
   depends_on "pkg-config" => :build
   depends_on "glib"
   depends_on :java => :recommended
-  depends_on :python => :optional
+  depends_on "python" => :optional
   depends_on :python3 => :optional
 
   def install
