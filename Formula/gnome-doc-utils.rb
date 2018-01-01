08d418cd8828dd5c1616490cc3445e38f4013485
ilovezfs <ilovezfs@icloud.com>
2018-01-01 12:17:07 -0800

    gnome-doc-utils: depend on python instead of :python

--- Formula/gnome-doc-utils.rb
+++ Formula/gnome-doc-utils.rb
@@ -3,6 +3,7 @@ class GnomeDocUtils < Formula
   homepage "https://wiki.gnome.org/Projects/GnomeDocUtils"
   url "https://download.gnome.org/sources/gnome-doc-utils/0.20/gnome-doc-utils-0.20.10.tar.xz"
   sha256 "cb0639ffa9550b6ddf3b62f3b1add92fb92ab4690d351f2353cffe668be8c4a6"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -15,7 +16,7 @@ class GnomeDocUtils < Formula
 
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
-  depends_on :python
+  depends_on "python" if MacOS.version <= :snow_leopard
   depends_on "docbook"
   depends_on "gettext"
   depends_on "libxml2"
