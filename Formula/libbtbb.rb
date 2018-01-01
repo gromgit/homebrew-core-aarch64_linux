58ba0a143578ca42144d483067c6d48efec92e7e
ilovezfs <ilovezfs@icloud.com>
2018-01-01 11:42:34 -0800

    libbtbb: depend on python instead of :python

--- Formula/libbtbb.rb
+++ Formula/libbtbb.rb
@@ -16,7 +16,7 @@ class Libbtbb < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on :python if MacOS.version <= :snow_leopard
+  depends_on "python" if MacOS.version <= :snow_leopard
 
   def install
     mkdir "build" do
