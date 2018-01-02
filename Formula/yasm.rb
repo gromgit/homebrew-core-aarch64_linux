22a02b1e113c0cb2b45f9d57e1346e422e6b7190
Alyssa Ross <hi@alyssa.is>
2018-01-02 03:37:59 +0000

    yasm: add mirror (#22329)

--- Formula/yasm.rb
+++ Formula/yasm.rb
@@ -2,6 +2,7 @@ class Yasm < Formula
   desc "Modular BSD reimplementation of NASM"
   homepage "http://yasm.tortall.net/"
   url "https://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz"
+  mirror "https://ftp.openbsd.org/pub/OpenBSD/distfiles/yasm-1.3.0.tar.gz"
   sha256 "3dce6601b495f5b3d45b59f7d2492a340ee7e84b5beca17e48f862502bd5603f"
   revision 1
 
