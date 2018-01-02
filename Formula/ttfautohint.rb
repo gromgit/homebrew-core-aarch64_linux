e940e2b3c3d63451ceb97aa4342185311e13bee9
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-02 07:53:22 +0000

    ttfautohint: update 1.8.1 bottle.

--- Formula/ttfautohint.rb
+++ Formula/ttfautohint.rb
@@ -6,10 +6,9 @@ class Ttfautohint < Formula
 
   bottle do
     cellar :any
-    sha256 "38656e34a51e8cbd668bec8b87fe79f438259937ca699b36865b93802596c9f6" => :high_sierra
-    sha256 "f33224ef2971fca34e8b765ca78b956cc0de3ddcaa2d2fe6c9dc789abe043803" => :sierra
-    sha256 "b3fadc4f292ddbfea133a4359a18d349970fc794415b2aeb774b38ccc021b641" => :el_capitan
-    sha256 "3c336390fbb0221d491e3d341e666259ed68fce7a8d147ef7b33b6eef0b8a9c2" => :yosemite
+    sha256 "7f2882b40456aa63032c9cc0cc605867fe1b57b483f67ee755aa2be6a93908ea" => :high_sierra
+    sha256 "538f6817293c6f4db3068aec0b946e27af6b2a74a8bae8e738d0f7ece568ed6f" => :sierra
+    sha256 "2123fde31eaeb430de611383e8107862f135e3feeaba5b2e2e2c99fd0292e61c" => :el_capitan
   end
 
   head do
