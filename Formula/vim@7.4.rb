72d94f77652457a4dc6cd5eaf9b07b1e16147fc1
ilovezfs <ilovezfs@icloud.com>
2018-01-02 09:03:15 -0800

    vim@7.4: depend on python3 instead of :python3

--- Formula/vim@7.4.rb
+++ Formula/vim@7.4.rb
@@ -35,7 +35,7 @@ class VimAT74 < Formula
   end
 
   depends_on :python => :recommended
-  depends_on :python3 => :optional
+  depends_on "python3" => :optional
   depends_on :ruby => "1.8" # Can be compiled against 1.8.x or >= 1.9.3-p385.
   depends_on :perl => "5.3"
   depends_on "lua" => :optional
