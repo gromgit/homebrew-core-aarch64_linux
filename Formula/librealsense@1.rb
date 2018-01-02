1cecc8826c5f5ea6421dfc8e37e90ca6ec06754e
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-02 21:37:20 +0000

    librealsense@1: add 1.12.1 bottle.

--- Formula/librealsense@1.rb
+++ Formula/librealsense@1.rb
@@ -5,6 +5,13 @@ class LibrealsenseAT1 < Formula
   sha256 "62fb4afac289ad7e25c81b6be584ee275f3d4d3742468dc7d80222ee2e4671bd"
   head "https://github.com/IntelRealSense/librealsense.git", :branch => "legacy"
 
+  bottle do
+    cellar :any
+    sha256 "6b085438da80732be8e7bea29cce84b6c8a2f6210819b5b735c2eaaced57ee56" => :high_sierra
+    sha256 "74f0a2f61d3d65f210412603445969d08de12808121e4dba7a71f1eb5831075f" => :sierra
+    sha256 "97a1d82d970a67bd1c8d10122a3e939909ea1e5ed4320e1f564da51a4e5456d1" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "glfw"
