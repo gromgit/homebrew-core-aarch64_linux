8b1d3b33c1b4dcfad4e98ca01e9571d413d7c9d7
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-03 18:46:36 +0000

    hh: update 1.24 bottle.

--- Formula/hh.rb
+++ Formula/hh.rb
@@ -7,9 +7,9 @@ class Hh < Formula
 
   bottle do
     cellar :any
-    sha256 "3806e269c5d29b06e157cb9aa9675cf5c9bda57a8546965de54d0a1da0aa93b6" => :high_sierra
-    sha256 "863eed15e12bafbe8023ef3d1a40ea2f8abcbbec03e2c09113b7cc7c63848e9b" => :sierra
-    sha256 "e8105d2183a21e922bd904d5af25eac6d123e5c2ccc8f9fa8a363af1aedd2510" => :el_capitan
+    sha256 "90be269fa8ed7ed3538f91d34cccc5fe87e4ce3e328b64a6c74f558d5f2d0633" => :high_sierra
+    sha256 "3a194279a8c4eda33d00398e6c3a2a854c9c15194449d1d0e813e57b17993cae" => :sierra
+    sha256 "4bbad864545de6e4f2a8e914fc20495983c0bde0feb2e2da9353a8169cfaa3fa" => :el_capitan
   end
 
   depends_on "autoconf" => :build
