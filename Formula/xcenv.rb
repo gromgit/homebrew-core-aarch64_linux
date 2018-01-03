346fe5987b345ba701d0acce9a76d91fa3d66df7
ilovezfs <ilovezfs@icloud.com>
2018-01-02 22:02:43 -0800

    xcenv: set bottle unneeded

--- Formula/xcenv.rb
+++ Formula/xcenv.rb
@@ -5,13 +5,7 @@ class Xcenv < Formula
   sha256 "9426dc1fa50fba7f31a2867c543751428768e0592e499fb7724da8dae45a32ec"
   head "https://github.com/xcenv/xcenv.git"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "801f48f671d9658695f7cdb00c1de4f49bff7231856112b597c398f744cc4264" => :high_sierra
-    sha256 "000e0d3d363de9398ba5655f5536cb817a6f1019f2679b4469556d86bf012a4b" => :sierra
-    sha256 "000e0d3d363de9398ba5655f5536cb817a6f1019f2679b4469556d86bf012a4b" => :el_capitan
-    sha256 "000e0d3d363de9398ba5655f5536cb817a6f1019f2679b4469556d86bf012a4b" => :yosemite
-  end
+  bottle :unneeded
 
   def install
     prefix.install ["bin", "libexec"]
