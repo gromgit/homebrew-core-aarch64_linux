84a2aa859d952410cba84acf5833fcfc7a85e1f3
ilovezfs <ilovezfs@icloud.com>
2018-01-03 04:29:15 -0800

    onioncat 0.2.2.r578
    Closes #22399.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/onioncat.rb
+++ Formula/onioncat.rb
@@ -1,9 +1,9 @@
 class Onioncat < Formula
   desc "VPN-adapter that provides location privacy using Tor or I2P"
   homepage "https://www.onioncat.org"
-  url "https://www.cypherpunk.at/ocat/download/Source/current/onioncat-0.2.2.r569.tar.gz"
-  version "0.2.2.r569"
-  sha256 "377777de0d3c731fd2253db02b25562a2ed17e82901d0569308754215223f0bb"
+  url "https://www.cypherpunk.at/ocat/download/Source/current/onioncat-0.2.2.r578.tar.gz"
+  version "0.2.2.r578"
+  sha256 "69c677e04987bd438495d575b566c358f449ff138b836925fd406cf6d6a400f5"
 
   bottle do
     sha256 "81840e780b37af0429f3e8f2797c87d6f45e1784d8480b602902259b3ce54716" => :high_sierra
