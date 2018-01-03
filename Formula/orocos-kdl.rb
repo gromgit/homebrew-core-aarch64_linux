1cae32b79656ecec26389d9ee3edfd5df488dcd4
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-03 08:26:14 +0000

    orocos-kdl: add 1.3.1 bottle.

--- Formula/orocos-kdl.rb
+++ Formula/orocos-kdl.rb
@@ -4,6 +4,13 @@ class OrocosKdl < Formula
   url "https://github.com/orocos/orocos_kinematics_dynamics/archive/v1.3.1.tar.gz"
   sha256 "aff361d2b4e2c6d30ae959308a124022eeef5dc5bea2ce779900f9b36b0537bd"
 
+  bottle do
+    cellar :any
+    sha256 "16245deef22a33a5a016eab85909ed7f5009cab563ee7d9efb33478a1520b52c" => :high_sierra
+    sha256 "b52666692aa6d28265454495d96638c6e3774f2de5b28c9d43a548070abaa0c6" => :sierra
+    sha256 "b83bf847ccc4417f990252b1452b3c3a77bfe7358f54c4b69a036267db99d3ee" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "eigen"
 
