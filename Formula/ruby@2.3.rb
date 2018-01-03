668979ed9177f4dbfec924e25c0b12cd66801141
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-03 18:24:10 +0000

    ruby@2.3: update 2.3.6_2 bottle.

--- Formula/ruby@2.3.rb
+++ Formula/ruby@2.3.rb
@@ -6,9 +6,9 @@ class RubyAT23 < Formula
   revision 2
 
   bottle do
-    sha256 "53f04a6214f07e60b08b5a7b75ba6fb81d15b4ffdfc0b042c14866709207231d" => :high_sierra
-    sha256 "890510617b95f14d38009e3243b09ef57d423b2b1ba24a4e3a0464e7f3200726" => :sierra
-    sha256 "d9e95e8b0e9e508b1becefc1884a773c4df414840b721ffc8af403158939698a" => :el_capitan
+    sha256 "69f685579f193bebdf1111600173ea7ff17c543193dd2fd2b38a9698d77c13de" => :high_sierra
+    sha256 "f95e1f8b7dfd394ffe20df7d980c191e7c389f10f6d206257988d21a4f735c86" => :sierra
+    sha256 "dc25e49e1a6bedf6d7b0df72fbbef68727dcb3c0591c00d728740cffbc163d64" => :el_capitan
   end
 
   keg_only :versioned_formula
