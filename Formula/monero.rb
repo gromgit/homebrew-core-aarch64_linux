45d84bb43c62dd2e56eebba0a90bc175d6c9c0dd
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-04 12:20:46 +0000

    monero: add 0.11.1.0 bottle.

--- Formula/monero.rb
+++ Formula/monero.rb
@@ -4,6 +4,12 @@ class Monero < Formula
   url "https://github.com/monero-project/monero/archive/v0.11.1.0.tar.gz"
   sha256 "b5b48d3e5317c599e1499278580e9a6ba3afc3536f4064fcf7b20840066a509b"
 
+  bottle do
+    sha256 "4605768a865c17daae09b3e1ddfd96babe0779126ff1ed90db26238e020d8283" => :high_sierra
+    sha256 "8c2edbf826bcb23015e050ac072ff64d9d400ed0ef223b8fc53ddd14d4bb1335" => :sierra
+    sha256 "6c4777177c56f4f1ef68393033421e0628a8c657cf4373ccbeb6c009c4b02430" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "boost"
