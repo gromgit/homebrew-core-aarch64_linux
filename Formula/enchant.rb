0755939c710c91314c32d0816833468f9d73be19
ilovezfs <ilovezfs@icloud.com>
2018-01-04 12:11:55 -0800

    enchant: update pyenchant resource (#22444)

--- Formula/enchant.rb
+++ Formula/enchant.rb
@@ -17,8 +17,8 @@ class Enchant < Formula
 
   # https://pythonhosted.org/pyenchant/
   resource "pyenchant" do
-    url "https://files.pythonhosted.org/packages/source/p/pyenchant/pyenchant-1.6.5.tar.gz"
-    sha256 "623f332a9fbb70ae6c9c2d0d4e7f7bae5922d36ba0fe34be8e32df32ebbb4f84"
+    url "https://files.pythonhosted.org/packages/9e/54/04d88a59efa33fefb88133ceb638cdf754319030c28aadc5a379d82140ed/pyenchant-2.0.0.tar.gz"
+    sha256 "fc31cda72ace001da8fe5d42f11c26e514a91fa8c70468739216ddd8de64e2a0"
   end
 
   def install
@@ -33,8 +33,8 @@ class Enchant < Formula
     if build.with? "python"
       resource("pyenchant").stage do
         # Don't download and install distribute now
-        inreplace "setup.py", "distribute_setup.use_setuptools()", ""
-        ENV["PYENCHANT_LIBRARY_PATH"] = lib/"libenchant.dylib"
+        inreplace "setup.py", "ez_setup.use_setuptools()", ""
+        ENV["PYENCHANT_LIBRARY_PATH"] = lib/"libenchant-2.dylib"
         system "python", "setup.py", "install", "--prefix=#{prefix}",
                               "--single-version-externally-managed",
                               "--record=installed.txt"
