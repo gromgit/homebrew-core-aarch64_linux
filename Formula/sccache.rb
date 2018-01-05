bda6bf935193d8965728601b92c1e73486b29e8d
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-05 17:25:55 +0000

    sccache: update 0.2.3 bottle.

--- Formula/sccache.rb
+++ Formula/sccache.rb
@@ -6,9 +6,9 @@ class Sccache < Formula
   head "https://github.com/mozilla/sccache.git"
 
   bottle do
-    sha256 "294dda365425fc050344d7dce9dbe53ce7b780e754b0cb6b87ad8d1c04860501" => :high_sierra
-    sha256 "6ca47428771877fd3ce58df83ec5dc7b245a3d0c00f50059e2eb0259cfc3fb09" => :sierra
-    sha256 "e995ed15ee3c3b6c9a54c66162e8dcba9dc4a8d401724e0c3ecba6636a9a6c65" => :el_capitan
+    sha256 "739775beb4c6fce238257d8a86fccbe4ac5a2ad6df529c3257927b3f82f8c2dd" => :high_sierra
+    sha256 "3b77c7d0a1ed12c8b87a23dc471b64728f75c18d8503ed5148533f3932587015" => :sierra
+    sha256 "4e35ce007525f9a207f70667af85313774cd0828b416d82a429e661342407943" => :el_capitan
   end
 
   depends_on "rust" => :build
