74f3c0831b4bfc4c2f35d009432ee87236cd21bb
Dennis Luxen <info@project-osrm.org>
2018-01-05 20:28:07 +0100

    exomizer 2.0.10
    Closes #22473.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/exomizer.rb
+++ Formula/exomizer.rb
@@ -1,9 +1,8 @@
 class Exomizer < Formula
   desc "6502 compressor with CBM PET 4032 support"
   homepage "https://bitbucket.org/magli143/exomizer/wiki/Home"
-  url "https://bitbucket.org/magli143/exomizer/wiki/downloads/exomizer209.zip"
-  version "2.0.9"
-  sha256 "d2a95d4d168e4007fc396295e2f30a21b58f4648c28d1aadf84e7d497c5c7a34"
+  url "https://bitbucket.org/magli143/exomizer/wiki/downloads/exomizer-2.0.10.zip"
+  sha256 "74afb08a51466e24e5e95b8672c9a152a1ab4eb31464f29cd630c1feb585b027"
 
   bottle do
     cellar :any_skip_relocation
@@ -17,7 +16,7 @@ class Exomizer < Formula
   def install
     cd "src" do
       system "make"
-      bin.install %w[b2membuf exobasic exomizer exoraw]
+      bin.install %w[exobasic exomizer]
     end
   end
 
