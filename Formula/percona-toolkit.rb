15a7272a78a4bde51a4a56ceb66ceab5b7d34114
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-05 12:55:31 +0000

    percona-toolkit: update 3.0.6 bottle.

--- Formula/percona-toolkit.rb
+++ Formula/percona-toolkit.rb
@@ -7,9 +7,9 @@ class PerconaToolkit < Formula
 
   bottle do
     cellar :any
-    sha256 "e267b79631c42b8245d476c7e2198ced18307b5cb7a4419f8e0f2296ed50923b" => :high_sierra
-    sha256 "9688d848141eb8de92ff5d6eeed2b941c277d6215d0184e95f8351deebdf76c0" => :sierra
-    sha256 "2ab342631cac807fb416e3be6f7edd89dd6042bcfa34779d9639569481077b36" => :el_capitan
+    sha256 "f4d7b44859ef9add3095b14a541007747c477001b73a5ae4d8d5436aeae0877e" => :high_sierra
+    sha256 "9e3601d932eb9a012c648bed73a9d01e886f96d709e51993fe2b188aa0e6ebf2" => :sierra
+    sha256 "f5199f52b1428b8a7f1086d9e7ba8f6a1279817997dcc694557c31f9888dce52" => :el_capitan
   end
 
   depends_on "mysql"
