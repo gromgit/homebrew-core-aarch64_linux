934d49fc4ac8ea662a111ba89c51955a1770741e
ilovezfs <ilovezfs@icloud.com>
2018-01-05 05:21:06 -0800

    cargo-completion 0.24.0 (#22462)

--- Formula/cargo-completion.rb
+++ Formula/cargo-completion.rb
@@ -1,8 +1,8 @@
 class CargoCompletion < Formula
   desc "Bash and Zsh completion for Cargo"
   homepage "https://github.com/rust-lang/cargo"
-  url "https://github.com/rust-lang/cargo/archive/0.22.0.tar.gz"
-  sha256 "6740e2f8d035a5a5246a12e84a141d37ad4c2b826355aff497e6f1fcfb637aef"
+  url "https://github.com/rust-lang/cargo/archive/0.24.0.tar.gz"
+  sha256 "a9d4291254a1d622d3c3cb942aaa56ab4059b8d04915c3af234f0362f14722e8"
   version_scheme 1
   head "https://github.com/rust-lang/cargo.git"
 
