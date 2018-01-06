9106ef08e64e21b29ff59902472b73b8bac8b59b
ilovezfs <ilovezfs@icloud.com>
2018-01-06 07:35:41 -0800

    ceres-solver: revision for suite-sparse

--- Formula/ceres-solver.rb
+++ Formula/ceres-solver.rb
@@ -3,7 +3,7 @@ class CeresSolver < Formula
   homepage "http://ceres-solver.org/"
   url "http://ceres-solver.org/ceres-solver-1.13.0.tar.gz"
   sha256 "1df490a197634d3aab0a65687decd362912869c85a61090ff66f073c967a7dcd"
-  revision 3
+  revision 4
   head "https://ceres-solver.googlesource.com/ceres-solver.git"
 
   bottle do
