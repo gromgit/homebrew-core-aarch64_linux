f70c3e29796548c31562af86940505f83f3f362d
ilovezfs <ilovezfs@icloud.com>
2018-01-06 03:22:24 -0800

    python: revision for gdbm

--- Formula/python.rb
+++ Formula/python.rb
@@ -3,7 +3,7 @@ class Python < Formula
   homepage "https://www.python.org/"
   url "https://www.python.org/ftp/python/2.7.14/Python-2.7.14.tar.xz"
   sha256 "71ffb26e09e78650e424929b2b457b9c912ac216576e6bd9e7d204ed03296a66"
-  revision 1
+  revision 2
   head "https://github.com/python/cpython.git", :branch => "2.7"
 
   bottle do
