9995fb52a958234c44b51c6202e71b49ead3200d
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-08 15:50:21 +0000

    bioawk: add 1.0 bottle.

--- Formula/bioawk.rb
+++ Formula/bioawk.rb
@@ -4,6 +4,13 @@ class Bioawk < Formula
   url "https://github.com/lh3/bioawk/archive/v1.0.tar.gz"
   sha256 "5cbef3f39b085daba45510ff450afcf943cfdfdd483a546c8a509d3075ff51b5"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "023f5cafaa31404e68b8fc6bcfbeee27e63eb5fbcab897d2f406fceda90ec9ff" => :high_sierra
+    sha256 "154d44dd9ea56db8170127711e991950d487e379ae12df76332e4b7512f79fe8" => :sierra
+    sha256 "df0810bc087f924cdddcdb73f00faf9772de9475e0e698c7af8a7d036b3a4c91" => :el_capitan
+  end
+
   def install
     # Fix make: *** No rule to make target `ytab.h', needed by `b.o'.
     ENV.deparallelize
