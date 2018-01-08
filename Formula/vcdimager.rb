89ac4db2ab967635f4cca2629c01fd268b4177e9
ilovezfs <ilovezfs@icloud.com>
2018-01-08 02:04:54 -0800

    vcdimager 2.0.1
    Closes #22562.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/vcdimager.rb
+++ Formula/vcdimager.rb
@@ -1,10 +1,9 @@
 class Vcdimager < Formula
   desc "(Super) video CD authoring solution"
   homepage "https://www.gnu.org/software/vcdimager/"
-  url "https://ftp.gnu.org/gnu/vcdimager/vcdimager-0.7.24.tar.gz"
-  mirror "https://ftpmirror.gnu.org/vcdimager/vcdimager-0.7.24.tar.gz"
-  sha256 "075d7a67353ff3004745da781435698b6bc4a053838d0d4a3ce0516d7d974694"
-  revision 2
+  url "https://ftp.gnu.org/gnu/vcdimager/vcdimager-2.0.1.tar.gz"
+  mirror "https://ftpmirror.gnu.org/vcdimager/vcdimager-2.0.1.tar.gz"
+  sha256 "67515fefb9829d054beae40f3e840309be60cda7d68753cafdd526727758f67a"
 
   bottle do
     cellar :any
@@ -18,26 +17,10 @@ class Vcdimager < Formula
   depends_on "libcdio"
   depends_on "popt"
 
-  # libcdio 1.0 compat
-  # Reported 24 Nov 2017 https://savannah.gnu.org/support/index.php?109421
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/3ad6c5d/vcdimager/libcdio-1.0-compat.diff"
-    sha256 "65798b9c6070d53957d46b0cac834e1ab9bd4014fab50773ac83b4d1babe00a6"
-  end
-
   def install
-    # libcdio 1.1/2.0 compat
-    # Reported 1 Jan 2018 https://savannah.gnu.org/support/?109436
-    inreplace %w[frontends/cli/vcd-info.c frontends/xml/vcd_xml_build.c
-                 frontends/xml/vcd_xml_rip.c frontends/xml/vcdxml.h
-                 lib/data_structures.c lib/dict.h lib/files.c lib/info.c
-                 lib/info_private.c lib/mpeg_stream.c lib/pbc.c lib/vcd.c] do |s|
-      s.gsub! /(_cdio_list_(node_)?free.*, +(true|false))\);/, "\\1, free\);", false
-      s.gsub! /(iso9660_fs_readdir.*)(, true)/, "\\1", false
-    end
-
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}", "--mandir=#{man}"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
     system "make", "install"
   end
 
