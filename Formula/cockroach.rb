083e51e246caaa919dc5bc11f74179a0101e8320
Ben Darnell <ben@bendarnell.com>
2018-01-08 12:50:56 -0500

    cockroach 1.1.4
    Closes #22586.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/cockroach.rb
+++ Formula/cockroach.rb
@@ -1,9 +1,9 @@
 class Cockroach < Formula
   desc "Distributed SQL database"
   homepage "https://www.cockroachlabs.com"
-  url "https://binaries.cockroachdb.com/cockroach-v1.1.3.src.tgz"
-  version "1.1.3"
-  sha256 "a5b8530312d781294232fe9054cce1326f4f7919a5d284fa714a435991b81645"
+  url "https://binaries.cockroachdb.com/cockroach-v1.1.4.src.tgz"
+  version "1.1.4"
+  sha256 "e60949bdc319593a94aeb3282bd5a0458ee4b5aa764e819d5ad779669d5714e6"
   head "https://github.com/cockroachdb/cockroach.git"
 
   bottle do
