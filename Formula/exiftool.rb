61c02491f31059e91cd615546d7b13e4b15deb3f
commitay <commitay@users.noreply.github.com>
2018-01-09 07:04:26 +1000

    exiftool 10.74 (devel)

--- Formula/exiftool.rb
+++ Formula/exiftool.rb
@@ -16,9 +16,9 @@ class Exiftool < Formula
   end
 
   devel do
-    url "https://sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-10.71.tar.gz"
-    mirror "https://downloads.sourceforge.net/project/exiftool/Image-ExifTool-10.71.tar.gz"
-    sha256 "5a0122b4b8282fe39456754e88cfb7baed1bef92a742b1e573218cf1dcbbbef4"
+    url "https://sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-10.74.tar.gz"
+    mirror "https://downloads.sourceforge.net/project/exiftool/Image-ExifTool-10.74.tar.gz"
+    sha256 "3948bf6fe4f55b87748c58756ebb83a3e15f9314f10e1b8dc67f2db8abfba56c"
   end
 
   def install
