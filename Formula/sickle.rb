dbfed2e80893afcbb904efbe13d46612b03a8f55
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-08 15:47:47 +0000

    sickle: add 1.33 bottle.

--- Formula/sickle.rb
+++ Formula/sickle.rb
@@ -4,6 +4,13 @@ class Sickle < Formula
   url "https://github.com/najoshi/sickle/archive/v1.33.tar.gz"
   sha256 "eab271d25dc799e2ce67c25626128f8f8ed65e3cd68e799479bba20964624734"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3aeaaa4393148876cc55cc9defbe82ae0fe0dabea18e418413b2aa8cff23dd0b" => :high_sierra
+    sha256 "844b063d1496d2a7c7f8a12b2239ae32766a538557d44f712c584a30b9775fae" => :sierra
+    sha256 "138b38a20aefc55ec4005ee4c4622ec332cbb13ff4ebc39ff45d91a2c12afde8" => :el_capitan
+  end
+
   def install
     system "make"
     bin.install "sickle"
