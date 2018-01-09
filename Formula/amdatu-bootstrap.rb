e03a33911f81f3f25dab066951a8ec5b47b78711
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2018-01-09 02:12:00 -0800

    amdatu-bootstrap: require Java 8 specifically (#22609)
    and update url

--- Formula/amdatu-bootstrap.rb
+++ Formula/amdatu-bootstrap.rb
@@ -1,14 +1,16 @@
 class AmdatuBootstrap < Formula
   desc "Bootstrapping OSGi development"
   homepage "http://www.amdatu.com/bootstrap/intro.html"
-  url "https://bitbucket.org/amdatu/amdatu-bootstrap/downloads/bootstrap-bin-r9.zip"
+  url "https://bitbucket.org/amdatuadm/amdatu-bootstrap/downloads/bootstrap-bin-r9.zip"
   sha256 "937ef932a740665439ea0118ed417ff7bdc9680b816b8b3c81ecfd6d0fc4773b"
 
   bottle :unneeded
 
+  depends_on :java => "1.8"
+
   def install
     libexec.install %w[amdatu-bootstrap bootstrap.jar conf]
-    bin.install_symlink "#{libexec}/amdatu-bootstrap" => "amdatu-bootstrap"
+    (bin/"amdatu-bootstrap").write_env_script libexec/"amdatu-bootstrap", Language::Java.java_home_env("1.8")
   end
 
   test do
