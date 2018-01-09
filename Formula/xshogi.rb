354820f8a3441405c2ae4f2773fb24ce2395d3ed
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2018-01-09 02:05:26 -0800

    xshogi: add checksum (#22613)

--- Formula/xshogi.rb
+++ Formula/xshogi.rb
@@ -3,6 +3,7 @@ class Xshogi < Formula
   homepage "https://www.gnu.org/software/gnushogi/"
   url "https://ftp.gnu.org/gnu/gnushogi/xshogi-1.4.2.tar.gz"
   mirror "https://ftpmirror.gnu.org/gnushogi/xshogi-1.4.2.tar.gz"
+  sha256 "2e2f1145e3317143615a764411178f538bd54945646b14fc2264aaeaa105dab6"
 
   bottle do
     cellar :any_skip_relocation
