a5785becac15035eb68d2aa48117212dee3733fa
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-09 14:43:31 +0000

    bartycrouch: update 3.9.3 bottle.

--- Formula/bartycrouch.rb
+++ Formula/bartycrouch.rb
@@ -6,8 +6,8 @@ class Bartycrouch < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "17f2224159454508ab5eca6036060da2a35578732adef128dfa8a2c297c50821" => :high_sierra
-    sha256 "86c0be0e0e907f82454b1d812a0e40cde473c4ede36ab37f514626d1720280fc" => :sierra
+    sha256 "61496a1b58dbb1c843147fcbd703d544ff884eb0b71fef11fb160577120f1840" => :high_sierra
+    sha256 "60d57a70647108d408a486fe4ebd0e79e8522613f65b367b93230a409882a274" => :sierra
   end
 
   depends_on :xcode => ["9.0", :build]
