56b5ddd583d0944e57d3f0d89c48b7e0c061103e
Alex Miller <alex.miller@cognitect.com>
2018-01-08 22:43:25 -0600

    clojure 1.9.0.302

--- Formula/clojure.rb
+++ Formula/clojure.rb
@@ -1,8 +1,8 @@
 class Clojure < Formula
   desc "The Clojure Programming Language"
   homepage "https://clojure.org"
-  url "https://download.clojure.org/install/clojure-tools-1.9.0.297.tar.gz"
-  sha256 "24996086d34f1436a35c4d336f702fb2af975874c2def968ae20fe3785194989"
+  url "https://download.clojure.org/install/clojure-tools-1.9.0.302.tar.gz"
+  sha256 "6a1fdc44f8cc38ca668d926a448e8213d280708dcf10a4a0485f5f743fa6de86"
 
   bottle :unneeded
 
