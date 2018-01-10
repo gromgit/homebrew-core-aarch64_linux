1c7310b101527abf03d5c8806263bf304a6aa28d
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-10 17:14:43 +0000

    proof-general: update 4.4_2 bottle.

--- Formula/proof-general.rb
+++ Formula/proof-general.rb
@@ -8,10 +8,9 @@ class ProofGeneral < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a9c3dca4c71aae78779bcf34fc98b914cee3d000dfbe18aaa29ec6f6c9f00af6" => :high_sierra
-    sha256 "3873ed2362ff17d7cc51fc5910bddb57f1fd23a294fe5747c4bc7b143c0b71d2" => :sierra
-    sha256 "2ceb9862d81f46f6ba815b58a1683ce8af5a95b6db0f4c1bfde488156c56ce62" => :el_capitan
-    sha256 "2ceb9862d81f46f6ba815b58a1683ce8af5a95b6db0f4c1bfde488156c56ce62" => :yosemite
+    sha256 "d6b5966b38f6b574805811d721af5d17afa4c8a7d944189703821aa564c0649b" => :high_sierra
+    sha256 "07982ffb7dacb8390875c82f4f7e9cbf440d51271b38c3d771e01baa8684edc0" => :sierra
+    sha256 "07982ffb7dacb8390875c82f4f7e9cbf440d51271b38c3d771e01baa8684edc0" => :el_capitan
   end
 
   depends_on "texi2html" => :build
