f546b413111d5742a7d76cec8d9d4d69777ebbaf
commitay <commitay@users.noreply.github.com>
2018-01-11 08:28:13 +1000

    betty: depend on ruby instead of :ruby

--- Formula/betty.rb
+++ Formula/betty.rb
@@ -3,10 +3,11 @@ class Betty < Formula
   homepage "https://github.com/pickhardt/betty"
   url "https://github.com/pickhardt/betty/archive/v0.1.7.tar.gz"
   sha256 "ed71e88a659725e0c475888df044c9de3ab1474ff483f0a3bb432949035e62d3"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :ruby => "1.9"
+  depends_on "ruby" if MacOS.version <= :mountain_lion
 
   def install
     libexec.install "lib", "main.rb" => "betty"
