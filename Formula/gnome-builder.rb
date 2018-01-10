67a00edf70dbe5779c347af4a871bd97479853e2
ilovezfs <ilovezfs@icloud.com>
2018-01-10 07:47:47 -0800

    gnome-builder: depend on python3 instead of :python3

--- Formula/gnome-builder.rb
+++ Formula/gnome-builder.rb
@@ -32,7 +32,7 @@ class GnomeBuilder < Formula
   depends_on "vala" => :recommended
   depends_on "ctags" => :recommended
   depends_on "meson" => :recommended
-  depends_on :python3 => :optional
+  depends_on "python3" => :optional
   depends_on "pygobject3" if build.with? "python3"
 
   needs :cxx11
