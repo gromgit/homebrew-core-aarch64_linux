711b5f93a5092d588679ea496eaa663543f15b86
ilovezfs <ilovezfs@icloud.com>
2018-01-10 07:47:47 -0800

    gst-python: depend on python3 instead of :python3

--- Formula/gst-python.rb
+++ Formula/gst-python.rb
@@ -12,7 +12,7 @@ class GstPython < Formula
 
   option "without-python", "Build without python 2 support"
 
-  depends_on :python3 => :optional
+  depends_on "python3" => :optional
   depends_on "gst-plugins-base"
 
   depends_on "pygobject3" if build.with? "python"
