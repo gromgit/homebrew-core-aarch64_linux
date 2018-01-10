ff154944747876edc3781969edc7d97ec55e5980
ilovezfs <ilovezfs@icloud.com>
2018-01-10 05:01:13 -0800

    shunit2 2.1.7 (#22695)

--- Formula/shunit2.rb
+++ Formula/shunit2.rb
@@ -1,13 +1,13 @@
 class Shunit2 < Formula
   desc "xUnit unit testing framework for Bourne-based shell scripts"
   homepage "https://github.com/kward/shunit2"
-  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/shunit2/shunit2-2.1.6.tgz"
-  sha256 "65a313a76fd5cc1c58c9e19fbc80fc0e418a4cbfbd46d54b35ed5b6e0025d4ee"
+  url "https://github.com/kward/shunit2/archive/v2.1.7.tar.gz"
+  sha256 "41a103aa984bfdeea3fec426edb0b3dda19153a5ce448d4b4bbe52a85c4d7fee"
 
   bottle :unneeded
 
   def install
-    bin.install "src/shunit2"
+    bin.install "shunit2"
   end
 
   test do
