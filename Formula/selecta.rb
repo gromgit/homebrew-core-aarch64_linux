f6ea6f15c0baa78d9766774b6c0b71097cf24f94
commitay <commitay@users.noreply.github.com>
2018-01-11 08:28:28 +1000

    selecta: depend on ruby instead of :ruby

--- Formula/selecta.rb
+++ Formula/selecta.rb
@@ -3,10 +3,11 @@ class Selecta < Formula
   homepage "https://github.com/garybernhardt/selecta"
   url "https://github.com/garybernhardt/selecta/archive/v0.0.6.tar.gz"
   sha256 "c5e0fdab53b3c4942e46c1a2c5a38158f21eb0520c6391f77d6d0a7d2a023318"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :ruby => "1.9"
+  depends_on "ruby" if MacOS.version <= :mountain_lion
 
   def install
     bin.install "selecta"
