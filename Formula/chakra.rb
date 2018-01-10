99663f615273c3b293dc100c6ebcfe822665fff9
ilovezfs <ilovezfs@icloud.com>
2018-01-10 00:47:51 -0800

    chakra 1.7.6
    Closes #22685.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/chakra.rb
+++ Formula/chakra.rb
@@ -1,9 +1,8 @@
 class Chakra < Formula
   desc "The core part of the JavaScript engine that powers Microsoft Edge"
   homepage "https://github.com/Microsoft/ChakraCore"
-  url "https://github.com/Microsoft/ChakraCore/archive/v1.7.5.tar.gz"
-  sha256 "3458224adeaf2de58e545ccc5b3da06a5b12c8e2bfd5c49be5c15a90c799265c"
-  revision 1
+  url "https://github.com/Microsoft/ChakraCore/archive/v1.7.6.tar.gz"
+  sha256 "545b60fd2ab2aae537c3582a13c40d105663b9a23de49ebcfdfa98e042fee87f"
 
   bottle do
     cellar :any
