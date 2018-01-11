fcc310bce95559643455e2ed95aef5c4112c7c80
ilovezfs <ilovezfs@icloud.com>
2018-01-11 07:06:40 -0800

    fades: depend on python3 instead of :python3

--- Formula/fades.rb
+++ Formula/fades.rb
@@ -3,6 +3,7 @@ class Fades < Formula
   homepage "https://fades.readthedocs.org/"
   url "https://files.pythonhosted.org/packages/59/18/cc80eb5c0a2e15c4b95df6a3c6158e06acb2e075ef2b811753936a6bdf57/fades-6.0.1.tar.gz"
   sha256 "747ee3a159be1cb7512fd7ea4163d22e2734209e474aa2dbbccc29a0f0c92d09"
+  revision 1
   head "https://github.com/PyAr/fades.git"
 
   bottle do
@@ -13,7 +14,7 @@ class Fades < Formula
     sha256 "8a6869cbb401853eff4aa5c176feebdcb0b493ec09052f9f7a516d87320d96f3" => :yosemite
   end
 
-  depends_on :python3
+  depends_on "python3"
 
   def install
     pyver = Language::Python.major_minor_version "python3"
