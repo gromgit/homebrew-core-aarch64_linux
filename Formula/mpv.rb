d996e5e644cab755b36cf55c0f5d7934715f1ce7
ilovezfs <ilovezfs@icloud.com>
2018-01-11 06:49:57 -0800

    mpv: depend on python3 instead of :python3

--- Formula/mpv.rb
+++ Formula/mpv.rb
@@ -16,7 +16,7 @@ class Mpv < Formula
   option "with-bundle", "Enable compilation of the .app bundle."
 
   depends_on "pkg-config" => :build
-  depends_on :python3 => :build
+  depends_on "python3" => :build
 
   depends_on "libass"
   depends_on "ffmpeg"
