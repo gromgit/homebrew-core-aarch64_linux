f8be9677d9a8e505c3a3d052ee001e07d9917da7
ilovezfs <ilovezfs@icloud.com>
2018-01-11 08:06:15 -0800

    you-get: depend on python3 instead of :python3
    Closes #22758.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/you-get.rb
+++ Formula/you-get.rb
@@ -5,6 +5,7 @@ class YouGet < Formula
   homepage "https://you-get.org/"
   url "https://github.com/soimort/you-get/releases/download/v0.4.1011/you-get-0.4.1011.tar.gz"
   sha256 "0f0504c7049ee8b664c60b184a49bf494d2783d6d86cdd13d260b0e8ecd5ca40"
+  revision 1
   head "https://github.com/soimort/you-get.git", :branch => "develop"
 
   bottle do
@@ -14,7 +15,7 @@ class YouGet < Formula
     sha256 "24c5d9053d7cc080c1c5fef9c341cf30f9c5a4e17f81579e9cb745102e0aac43" => :el_capitan
   end
 
-  depends_on :python3
+  depends_on "python3"
 
   depends_on "rtmpdump" => :optional
 
