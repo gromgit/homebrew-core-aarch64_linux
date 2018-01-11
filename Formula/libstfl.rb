70a56b7444dbbbfb7761faf85b7bce49b7699ad0
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-11 14:01:01 +0000

    libstfl: update 0.24_6 bottle.

--- Formula/libstfl.rb
+++ Formula/libstfl.rb
@@ -7,9 +7,9 @@ class Libstfl < Formula
 
   bottle do
     cellar :any
-    sha256 "a975bf5ce425a98a822baf0cd3fc4adbe50fa19ced10eba54b3fe8400326e56e" => :high_sierra
-    sha256 "f1ae9d6ff0abb4e4046f71d074722824752ffa5ec33a710903af47b4531bc237" => :sierra
-    sha256 "8f1c6903c6362792ba17df75ad88f8ad11fd40abe19efd16bea7b89e2208bc9d" => :el_capitan
+    sha256 "6b5656c0ec03676968efed42dfdd87c2ab27b37638461274fed870f131ae1321" => :high_sierra
+    sha256 "8e0258e39c281826514ba49c3add732ada3ae097cd6f840bf5e22849016e5a7d" => :sierra
+    sha256 "88971ef99df357abe7189391d3c6fe559dcd24c3faaf0e0b22d91a94ccbbeefb" => :el_capitan
   end
 
   option "without-perl", "Build without Perl support"
