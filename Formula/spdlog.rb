df8aeaa37b782db4460589b689ce2d512c661c2c
ilovezfs <ilovezfs@icloud.com>
2018-01-12 07:18:44 -0800

    spdlog 0.16.3
    Closes #22791.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/spdlog.rb
+++ Formula/spdlog.rb
@@ -1,8 +1,8 @@
 class Spdlog < Formula
   desc "Super fast C++ logging library"
   homepage "https://github.com/gabime/spdlog"
-  url "https://github.com/gabime/spdlog/archive/v0.16.2.tar.gz"
-  sha256 "2081e5df5e87402398847431e16b87c71dd5c4d632314bb976ace8161f4d32de"
+  url "https://github.com/gabime/spdlog/archive/v0.16.3.tar.gz"
+  sha256 "b88d7be261d9089c817fc8cee6c000d69f349b357828e4c7f66985bc5d5360b8"
   head "https://github.com/gabime/spdlog.git"
 
   bottle do
