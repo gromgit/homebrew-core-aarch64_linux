4d631a715f5ca0870af6dcbdadbe0aeddde31f05
Jason Karns <jason@karns.name>
2018-01-12 09:24:19 -0500

    node-build 2.6.23

--- Formula/node-build.rb
+++ Formula/node-build.rb
@@ -1,8 +1,8 @@
 class NodeBuild < Formula
   desc "Install NodeJS versions"
   homepage "https://github.com/nodenv/node-build"
-  url "https://github.com/nodenv/node-build/archive/v2.6.22.tar.gz"
-  sha256 "3c3bee29e1680bd9fd5bd7bc8d849d151b28392fe518091c865734a13dba06e2"
+  url "https://github.com/nodenv/node-build/archive/v2.6.23.tar.gz"
+  sha256 "a2b867d3f86ed8996871976fa17ecd8db5c6145dadae728a50279a74b0d33bd5"
   head "https://github.com/nodenv/node-build.git"
 
   bottle :unneeded
