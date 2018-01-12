a76c8dbe8c8fe83354479f8dfcc7812298e21ca7
Bart Skowron <x@bartskowron.com>
2018-01-12 07:36:05 +0100

    perl: fix caveat description for a custom local::lib path (#22765)

--- Formula/perl.rb
+++ Formula/perl.rb
@@ -70,7 +70,7 @@ class Perl < Formula
 
     You can set that up like this:
       PERL_MM_OPT="INSTALL_BASE=$HOME/perl5" cpan local::lib
-      echo 'eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)"' >> #{shell_profile}
+      echo 'eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib=$HOME/perl5)"' >> #{shell_profile}
     EOS
   end
 
