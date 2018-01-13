8d22453ffe3eacce81948a25888732b1e01be210
Viktor Szakats <vszakats@users.noreply.github.com>
2018-01-13 12:03:04 +0000

    apollo: secure url(s)

--- Formula/apollo.rb
+++ Formula/apollo.rb
@@ -23,7 +23,7 @@ class Apollo < Formula
 
   # https://www.oracle.com/technetwork/database/berkeleydb/overview/index-093405.html
   resource "bdb-je" do
-    url "http://download.oracle.com/maven/com/sleepycat/je/5.0.34/je-5.0.34.jar"
+    url "https://download.oracle.com/maven/com/sleepycat/je/5.0.34/je-5.0.34.jar"
     sha256 "025afa4954ed4e6f926af6e9015aa109528b0f947fcb3790b7bace639fe558fa"
   end
 
