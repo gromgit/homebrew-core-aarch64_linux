22985bf8a22ea17fe70c861e1e70e605ace691d8
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-13 11:04:52 +0000

    openblas: update 0.2.20_1 bottle.

--- Formula/openblas.rb
+++ Formula/openblas.rb
@@ -8,10 +8,9 @@ class Openblas < Formula
 
   bottle do
     cellar :any
-    sha256 "ce8b1b057a2cc93d9bcbaae0c9483200dd2e2f04ab533456dfe13eb38c6ab96b" => :high_sierra
-    sha256 "1bb0db6885551ec021c2267c8e7ea662ef4877ac8c2a9b590d920866f018aea3" => :sierra
-    sha256 "15b53cdfdc5028719e559612ea41f432cfbd2d4448cc29202273636ff3980bf5" => :el_capitan
-    sha256 "95bb17c1ffeb1f652d24d0d064241a822804818ab8a317a39918a4cfe740b905" => :yosemite
+    sha256 "4a62a8e0a79f1f06c30ea4743ee02eba1599fc66cfc9812967b838093dca048d" => :high_sierra
+    sha256 "31c10a9ff27fe87694c8ca729d7fdc919f872302479082cbb5e42d22a84d66b8" => :sierra
+    sha256 "6aa065aea2afd4573b8d20ac24ae8fec6fd6bf71562e33c6d25c397b3cea8187" => :el_capitan
   end
 
   keg_only :provided_by_osx,
