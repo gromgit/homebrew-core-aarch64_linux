d0c38af20acdd942801986b15b2b59ed5548800e
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-13 14:47:46 +0000

    bsponmpi: update 0.3_2 bottle.

--- Formula/bsponmpi.rb
+++ Formula/bsponmpi.rb
@@ -6,9 +6,9 @@ class Bsponmpi < Formula
   revision 2
 
   bottle do
-    sha256 "2d3c362d85b10c9a1bd1b4adc45ac2576abe0a86c15408d20c375bffd9e05494" => :high_sierra
-    sha256 "a3b6f8429913d14a42ac9174d9ba30d44f076b9e2b148e7a9409d9b352c66e2f" => :sierra
-    sha256 "1142baa7c3a19719304237927f4eb43f8aa405a260c808ef0332c2d8d979b51e" => :el_capitan
+    sha256 "b80c1f5b34b4530ae8cbf402418693e93bd400aa4fb14b998053a3c07024dd35" => :high_sierra
+    sha256 "caf59320e2eb005dc2de2daa363d15464e3c83519875503e3a498ab8963bb3aa" => :sierra
+    sha256 "10a864ad11ea1145898ae9bcd8897f107c034818e66faf3632d916d51a13a191" => :el_capitan
   end
 
   depends_on "scons" => :build
