b9d31104ab742a8b47c9ab2b1b32b370a2bd510f
ilovezfs <ilovezfs@icloud.com>
2018-01-12 23:54:27 -0800

    taktuk 3.7.7
    Closes #22815.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/taktuk.rb
+++ Formula/taktuk.rb
@@ -1,8 +1,8 @@
 class Taktuk < Formula
   desc "Deploy commands to (a potentially large set of) remote nodes"
   homepage "http://taktuk.gforge.inria.fr/"
-  url "https://gforge.inria.fr/frs/download.php/30903/taktuk-3.7.5.tar.gz"
-  sha256 "62d1b72616a1b260eb87cecde2e21c8cbb844939f2dcafad33507fcb16ef1cb1"
+  url "https://gforge.inria.fr/frs/download.php/file/37055/taktuk-3.7.7.tar.gz"
+  sha256 "56a62cca92670674c194e4b59903e379ad0b1367cec78244641aa194e9fe893e"
 
   bottle do
     cellar :any
