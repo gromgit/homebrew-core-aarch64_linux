f3a46e7a58487e85ba6c3e3ee7ba7a240f2c02b4
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-13 11:04:52 +0000

    r: update 3.4.3_1 bottle.

--- Formula/r.rb
+++ Formula/r.rb
@@ -6,9 +6,9 @@ class R < Formula
   revision 1
 
   bottle do
-    sha256 "5980c32c37d78545b85a745af26688c4b9b38748086597e9476d19782cab9200" => :high_sierra
-    sha256 "7362e6983438c23dd79cd5461836111da9c404c1b328be87226a0e55102cd070" => :sierra
-    sha256 "c8c3f89cba5a1bbc383ddacfba6b9dcdebe576fa9112dd30bacf390b8f67768a" => :el_capitan
+    sha256 "49f0c4d16ae8ee11cbb06da9d2ed10371e3d1099117f72b0850ab3a47d88c514" => :high_sierra
+    sha256 "d4cb1268d6adeb1ee6be0e85b7f1f257277f3fd9666f3a9ab0e1b24b216ce2ab" => :sierra
+    sha256 "361c2934f40bc20fca7e0db3c25291095155a473c663f64a0b028f9dd7c0940d" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
