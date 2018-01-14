0e4737f054fb7f1c7c009f03ee70f3d00e3ae91a
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-14 06:39:16 +0000

    mksh: update 56c bottle.

--- Formula/mksh.rb
+++ Formula/mksh.rb
@@ -8,10 +8,9 @@ class Mksh < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6098461be38111921ac20b4079fbf526e902335551db5390a36444cb35c1433c" => :high_sierra
-    sha256 "92b1f35f5311b97ed642b740a3f27b06b87b602004d75c401d342ad4f1622b8f" => :sierra
-    sha256 "869c03bacc72c9d7d56f9a1371bef7072df1d39213cf6cc23b924cb7056d7853" => :el_capitan
-    sha256 "f60810eeec945b44fccf6b64943666d0c4aab9fe0d0cd94c37cda411073ccc29" => :yosemite
+    sha256 "4ec398134339f30d26fe53dc2b8d8cf415b196c7cb84327c43cdf506a77376df" => :high_sierra
+    sha256 "79d49b6a5b52e1656443ef32c3a55566cc2ff7c1668e80f85011e9bee92d8567" => :sierra
+    sha256 "7343ace15a9f87d07cf35a4074c093104c68666c8443d9bb120e58dd660ccf70" => :el_capitan
   end
 
   def install
