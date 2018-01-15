cad01b61d432322c3e6e7a63b53885d43c0c134d
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-15 06:10:58 +0000

    tokei: update 7.0.0 bottle.

--- Formula/tokei.rb
+++ Formula/tokei.rb
@@ -5,9 +5,9 @@ class Tokei < Formula
   sha256 "e814f5d6efa0a887e4a1cdd514902518af35a750fea337eacfe096e00009e17a"
 
   bottle do
-    sha256 "b341ce693d9d52758372edc08bd4cef06883761536b2fd83f78115ddc2d71e73" => :high_sierra
-    sha256 "b5f2e5d8dcfc31e2a707f35b98e21b27577e928c4f72d4b1032b742c8140b401" => :sierra
-    sha256 "12898dbcc5ffc252f24c4a1cd27894011cab97c47f3dd0f569a788431983a962" => :el_capitan
+    sha256 "85543eb65dc14c1218c8eb2e7013b7991b4082f560b5f6ad08324d1bb0823413" => :high_sierra
+    sha256 "54c4979f14c8f857b700acee9879b806558a636519a874cf5db432399fcd0eef" => :sierra
+    sha256 "9724829ea70c535be23139cc56716acbdb017024a270a7f2c3263c3ed3ee3585" => :el_capitan
   end
 
   depends_on "rust" => :build
