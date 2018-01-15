0b6b034f5996b5bf6b6253fc5f2da2aa6c811bac
ilovezfs <ilovezfs@icloud.com>
2018-01-15 02:12:06 -0800

    whohas 0.29.1
    remove bottle unneeded
    add a test
    
    Closes #22880.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/whohas.rb
+++ Formula/whohas.rb
@@ -1,14 +1,42 @@
 class Whohas < Formula
   desc "Query multiple distributions' package archives"
   homepage "http://www.philippwesche.org/200811/whohas/intro.html"
-  url "http://www.philippwesche.org/200811/whohas/whohas-0.29.tar.gz"
-  sha256 "4382c53ccd27f2734f5981d58baf8f3442a9eb50709d49df3629d75f802af081"
+  url "https://github.com/whohas/whohas/releases/download/0.29.1/whohas-0.29.1.tar.gz"
+  sha256 "dbf2396838cb0f97726041213c04426b818d48cc510bd529faf30a8411682878"
 
-  bottle :unneeded
+  resource "Acme::Damn" do
+    url "https://cpan.metacpan.org/authors/id/I/IB/IBB/Acme-Damn-0.08.tar.gz"
+    sha256 "310d2d03ff912dcd42e4d946174099f41fe3a2dd57a497d6bd65baf1759b7e0e"
+  end
+
+  resource "forks" do
+    url "https://cpan.metacpan.org/authors/id/R/RY/RYBSKEJ/forks-0.36.tar.gz"
+    sha256 "61be24e44f4c6fea230e8354678beb5b7adcfefd909a47db8f0a251b0ab65993"
+  end
+
+  resource "Sys::SigAction" do
+    url "https://cpan.metacpan.org/authors/id/L/LB/LBAXTER/Sys-SigAction-0.23.tar.gz"
+    sha256 "c4ef6c9345534031fcbbe2adc347fc7194d47afc945e7a44fac7e9563095d353"
+  end
 
   def install
-    bin.install "program/whohas"
+    ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+
+    resources.each do |r|
+      r.stage do
+        system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+        system "make", "install"
+      end
+    end
+
+    bin.install "whohas"
+    bin.env_script_all_files(libexec/"bin", :PERL5LIB => ENV["PERL5LIB"])
+
     man1.install "usr/share/man/man1/whohas.1"
-    pkgshare.install "intro.txt"
+    doc.install "html_assets", "intro.html"
+  end
+
+  test do
+    assert_match "NetBSD", shell_output("#{bin}/whohas whohas 2>&1")
   end
 end
