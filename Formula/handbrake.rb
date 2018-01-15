1ad948a6467c2f93c2baa4c158982e50aaef1e82
Dave Dribin <dave@dribin.org>
2018-01-15 00:45:24 -0600

    handbrake: update url (#22865)

--- Formula/handbrake.rb
+++ Formula/handbrake.rb
@@ -1,7 +1,7 @@
 class Handbrake < Formula
   desc "Open-source video transcoder available for Linux, Mac, and Windows"
   homepage "https://handbrake.fr/"
-  url "https://handbrake.fr/mirror/HandBrake-1.0.7.tar.bz2"
+  url "https://download.handbrake.fr/releases/1.0.7/HandBrake-1.0.7.tar.bz2"
   sha256 "ffdee112f0288f0146b965107956cd718408406b75db71c44d2188f5296e677f"
   head "https://github.com/HandBrake/HandBrake.git"
 
