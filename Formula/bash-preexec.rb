351907d7695ed5b8fb255f15fe8eaa9a6ef707af
ilovezfs <ilovezfs@icloud.com>
2018-01-14 23:20:59 -0800

    bash-preexec 0.3.7 (#22872)

--- Formula/bash-preexec.rb
+++ Formula/bash-preexec.rb
@@ -1,8 +1,8 @@
 class BashPreexec < Formula
   desc "preexec and precmd functions for Bash (just like Zsh)"
   homepage "https://github.com/rcaloras/bash-preexec"
-  url "https://github.com/rcaloras/bash-preexec/archive/0.3.6.tar.gz"
-  sha256 "d2c0c7698d02e72cb47e8bf9394e1773c33414f8c845311db9bc1a1f7c2a089b"
+  url "https://github.com/rcaloras/bash-preexec/archive/0.3.7.tar.gz"
+  sha256 "56c33779763f9960dee863f4d87bc58f8da0ad9120b2c60dd12ba61c71c72da4"
 
   head "https://github.com/rcaloras/bash-preexec.git"
 
