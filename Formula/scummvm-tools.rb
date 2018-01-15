1c3d88c9d0b8bb6bf72c1f8d8076ab33ba88b27e
ilovezfs <ilovezfs@icloud.com>
2018-01-14 23:54:30 -0800

    scummvm-tools 2.0.0
    Closes #22874.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/scummvm-tools.rb
+++ Formula/scummvm-tools.rb
@@ -1,8 +1,8 @@
 class ScummvmTools < Formula
   desc "Collection of tools for ScummVM"
   homepage "https://www.scummvm.org/"
-  url "https://www.scummvm.org/frs/scummvm-tools/1.9.0/scummvm-tools-1.9.0.tar.xz"
-  sha256 "b7ab2e03c5a0efb71bb0c84434aa481331739b2b8759361d467e076b8410f938"
+  url "https://www.scummvm.org/frs/scummvm-tools/2.0.0/scummvm-tools-2.0.0.tar.xz"
+  sha256 "c2042ccdc6faaf745552bac2c00f213da382a7e382baa96343e508fced4451b3"
   head "https://github.com/scummvm/scummvm-tools.git"
 
   bottle do
