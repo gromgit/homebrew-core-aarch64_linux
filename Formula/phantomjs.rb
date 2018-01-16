69636c9ec60527cbcd2f0ca5449131b4dce381ec
ilovezfs <ilovezfs@icloud.com>
2018-01-15 22:28:16 -0800

    phantomjs: fix build on 10.12 with Xcode 9 (#22905)
    apply 10.13 Xcode 9 patch on 10.12 too

--- Formula/phantomjs.rb
+++ Formula/phantomjs.rb
@@ -14,7 +14,7 @@ class Phantomjs < Formula
 
   # Fix a variant of QTBUG-62266 in included Qt source
   # https://github.com/ariya/phantomjs/issues/15116
-  if MacOS.version >= :high_sierra
+  if MacOS::Xcode.version >= "9.0"
     patch do
       url "https://raw.githubusercontent.com/Homebrew/formula-patches/33dbb45f82/phantomjs/QTBUG-62266.diff"
       sha256 "d47b52c6a932139a448340244f66ea126412d210ab94dc19da7c468afaf5f45a"
