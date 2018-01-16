3eb5c26d40ca8828d0949f42cc1e3f55be903bc3
Elias J. Perez <eliasjpr@gmail.com>
2018-01-15 22:30:31 -0500

    amber 0.6.4 (new formula)
    Closes #21895.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/amber.rb
@@ -0,0 +1,35 @@
+class Amber < Formula
+  desc "CLI client for generating and scaffolding Amber web applications"
+  homepage "https://www.amberframework.org/"
+  url "https://github.com/amberframework/amber/archive/v0.6.4.tar.gz"
+  sha256 "4b7489d16afc26e2847d4dc74dad54d2f5dc20d1a148a5e0da6bea991bcac68f"
+
+  depends_on "crystal-lang"
+
+  def install
+    system "shards", "install"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    output = shell_output("#{bin}/amber new test_app")
+    %w[
+      config/environments
+      amber.yml
+      shard.yml
+      public
+      src/controllers
+      src/views
+      src/assets
+      src/test_app.cr
+    ].each do |path|
+      assert_match path, output
+    end
+
+    cd "test_app" do
+      build_app = shell_output("shards build test_app")
+      assert_match "Building", build_app
+      assert_predicate testpath/"test_app/bin/test_app", :exist?
+    end
+  end
+end
