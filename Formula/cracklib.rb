31c7c1ab62e5cfa5219d4611b1f0a599269fb654
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-16 15:05:51 +0000

    cracklib: update 2.9.6 bottle.

--- Formula/cracklib.rb
+++ Formula/cracklib.rb
@@ -5,13 +5,9 @@ class Cracklib < Formula
   sha256 "17cf76943de272fd579ed831a1fd85339b393f8d00bf9e0d17c91e972f583343"
 
   bottle do
-    cellar :any
-    sha256 "19ab6da7a72cc00ead3f9511462b2f18d8f2e00a95da849e85fd53101b744539" => :high_sierra
-    sha256 "9e915cdec5a6848d987a4bff2f5137ca1cf3aad9844461da4b08bf9f30b96b87" => :sierra
-    sha256 "1ba8ee5699bf4dde9e728d3138af3b7bd5d6811065cdd2ad9c398dffbd38c30d" => :el_capitan
-    sha256 "cdc073935379e4c2e14a5e98c7d16a4a6303d2682b9d77ed8addee555bdd9354" => :yosemite
-    sha256 "c974bd4f5515644268c6f9ff13bb392508d6148bc22be8f5fc2da3afe1fa6c89" => :mavericks
-    sha256 "3fe3f4737cee1d27465100119dc9f194a985cc8ec1f4a55cecb45d7669c04484" => :mountain_lion
+    sha256 "c7f22644746f0ea3abb20e772625b633ab8a73f0f773b57baa7d71a3620b367c" => :high_sierra
+    sha256 "d7d9452c277c47b1a7ad681a08c48f1e7ee5db4c9745520d271eb7f3a6f32757" => :sierra
+    sha256 "4e16e0eefb0560fab07c517fa54a6c79d77f989b725adff642c9c496c2879b25" => :el_capitan
   end
 
   depends_on "gettext"
