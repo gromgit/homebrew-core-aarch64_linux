81b402b6128966bf7d71c52da97c8fddc992953d
Rahul Malik <rmalik@pinterest.com>
2018-01-17 10:14:20 -0800

    plank 1.2.1 (new formula)
    Closes #22766.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/plank.rb
@@ -0,0 +1,31 @@
+class Plank < Formula
+  desc "Framework for generating immutable model objects"
+  homepage "https://pinterest.github.io/plank/"
+  url "https://github.com/pinterest/plank/archive/v1.2.1.tar.gz"
+  sha256 "21d15335bdf2f229a886611f3d18138b4a41ccbf420809dc3be1da41d3868104"
+
+  depends_on :xcode => ["9.0", :build]
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    (testpath/"pin.json").write <<~EOS
+      {
+        "id": "pin.json",
+        "title": "pin",
+        "description" : "Schema definition of a Pin",
+        "$schema": "http://json-schema.org/schema#",
+        "type": "object",
+        "properties": {
+          "id": { "type": "string" },
+          "link": { "type": "string", "format": "uri"}
+         }
+      }
+    EOS
+    system "#{bin}/plank", "--lang", "objc,flow", "--output_dir", testpath, "pin.json"
+    assert_predicate testpath/"Pin.h", :exist?, "[ObjC] Generated file does not exist"
+    assert_predicate testpath/"PinType.js", :exist?, "[Flow] Generated file does not exist"
+  end
+end
