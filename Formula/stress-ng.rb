2d94ac8e1466f102da079f0aa0f1536e4bcec8b1
ilovezfs <ilovezfs@icloud.com>
2018-01-18 05:44:05 -0800

    stress-ng 0.09.11
    Closes #23021.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/stress-ng.rb
+++ Formula/stress-ng.rb
@@ -1,8 +1,8 @@
 class StressNg < Formula
   desc "Stress test a computer system in various selectable ways"
   homepage "http://kernel.ubuntu.com/~cking/stress-ng/"
-  url "http://kernel.ubuntu.com/~cking/tarballs/stress-ng/stress-ng-0.09.09.tar.xz"
-  sha256 "e993fddb1d990a0cdb23b2af6cf6c417a1c09e155ada7c558ad80eae8a5feed3"
+  url "http://kernel.ubuntu.com/~cking/tarballs/stress-ng/stress-ng-0.09.11.tar.xz"
+  sha256 "49695dbd3260c0ddac96a73a8bfdecb6263d8e13dcaab0b386138e77fe04e425"
 
   bottle do
     cellar :any_skip_relocation
