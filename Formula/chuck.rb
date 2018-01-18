b8382d0b7a2cbd6a04e72f7c905d67c110988401
ilovezfs <ilovezfs@icloud.com>
2018-01-18 02:30:28 -0800

    chuck 1.3.6.0
    mirror to Bintray
    remove High Sierra and Xcode 9 fixes
    
    Closes #23008.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/chuck.rb
+++ Formula/chuck.rb
@@ -1,8 +1,9 @@
 class Chuck < Formula
   desc "Concurrent, on-the-fly audio programming language"
   homepage "http://chuck.cs.princeton.edu/"
-  url "http://chuck.cs.princeton.edu/release/files/chuck-1.3.5.2.tgz"
-  sha256 "e900b8545ffcb69c6d49354b18c43a9f9b8f789d3ae822f34b408eaee8d3e70b"
+  url "http://chuck.cs.princeton.edu/release/files/chuck-1.3.6.0.tgz"
+  mirror "https://dl.bintray.com/homebrew/mirror/chuck-1.3.6.0.tgz"
+  sha256 "5a68b427c0caf63719a903c544244ddb67415889278f975234d58c7583ec34b4"
 
   bottle do
     cellar :any_skip_relocation
@@ -15,25 +16,13 @@ class Chuck < Formula
 
   depends_on :xcode => :build
 
-  # Fix pointer comparison error with Xcode 9
-  # Reported by email to chuck-dev@lists.cs.princeton.edu on 2017-09-04
-  if DevelopmentTools.clang_build_version >= 900
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/b052485f5d/chuck/xcode9.patch"
-      sha256 "fa2e008c8d90321c8876a49f83d7566dead362740711442f8e983d07e98a220b"
-    end
-  end
-
   def install
-    # issue caused by the new macOS version, patch submitted upstream
-    # to the chuck-dev mailing list
-    inreplace "src/makefile.osx", '10\.(6|7|8|9|10|11)(\\.[0-9]+)?', MacOS.version
     system "make", "-C", "src", "osx"
     bin.install "src/chuck"
     pkgshare.install "examples"
   end
 
   test do
-    assert_match /probe \[success\]/m, shell_output("#{bin}/chuck --probe 2>&1")
+    assert_match "device", shell_output("#{bin}/chuck --probe 2>&1")
   end
 end
