29da20921ac9a283fcb76070288c648d56b897ae
Mike McQuaid <mike@mikemcquaid.com>
2018-01-18 09:42:13 +0000

    cctools: use *_by_macos.

--- Formula/cctools.rb
+++ Formula/cctools.rb
@@ -21,7 +21,7 @@ class Cctools < Formula
     sha256 "f9f74d98119d2efd5530f8d98eb7838a77be35576880ecec73fe9d535aa2afb0" => :mountain_lion
   end
 
-  keg_only :provided_by_osx, "this package duplicates tools shipped by Xcode"
+  keg_only :provided_by_macos, "this package duplicates tools shipped by Xcode"
 
   depends_on :ld64
 
