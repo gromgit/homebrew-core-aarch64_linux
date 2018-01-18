718ea8c397b06b969c343f47f32f2a41cc627c97
Mike McQuaid <mike@mikemcquaid.com>
2018-01-18 09:42:14 +0000

    libiconv: use *_by_macos.

--- Formula/libiconv.rb
+++ Formula/libiconv.rb
@@ -13,7 +13,7 @@ class Libiconv < Formula
     sha256 "19eb1ef341c6f45c08347bfb8d24e50d666a269523753862fa9a7a62691c0199" => :yosemite
   end
 
-  keg_only :provided_by_osx
+  keg_only :provided_by_macos
 
   patch do
     url "https://raw.githubusercontent.com/Homebrew/patches/9be2793af/libiconv/patch-utf8mac.diff"
