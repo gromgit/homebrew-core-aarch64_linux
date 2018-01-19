c9a2a8ea19e59a0b93da7cc22290acc1dbe2f21f
ilovezfs <ilovezfs@icloud.com>
2018-01-19 15:19:59 -0800

    bitcoin: fix build with Boost 1.66 (#23090)

--- Formula/bitcoin.rb
+++ Formula/bitcoin.rb
@@ -1,8 +1,18 @@
 class Bitcoin < Formula
   desc "Decentralized, peer to peer payment network"
   homepage "https://bitcoin.org/"
-  url "https://bitcoin.org/bin/bitcoin-core-0.15.1/bitcoin-0.15.1.tar.gz"
-  sha256 "34de2dbe058c1f8b6464494468ebe2ff0422614203d292da1c6458d6f87342b4"
+
+  stable do
+    url "https://bitcoin.org/bin/bitcoin-core-0.15.1/bitcoin-0.15.1.tar.gz"
+    sha256 "34de2dbe058c1f8b6464494468ebe2ff0422614203d292da1c6458d6f87342b4"
+
+    # Boost 1.66 compat
+    # Upstream commit 7 Dec 2017 "Make boost::multi_index comparators const"
+    patch do
+      url "https://github.com/bitcoin/bitcoin/commit/1ec0c0a01c31.patch?full_index=1"
+      sha256 "a1f761fe29f78e783cb4b55f8029900f94b45d1188cb81c80f73347ee2fdc025"
+    end
+  end
 
   bottle do
     cellar :any
