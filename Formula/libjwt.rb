ff19620fe0d1b5f46fd2d147fa117b106a3e61e7
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-19 13:12:10 +0000

    libjwt: add 1.8.0 bottle.

--- Formula/libjwt.rb
+++ Formula/libjwt.rb
@@ -4,6 +4,13 @@ class Libjwt < Formula
   url "https://github.com/benmcollins/libjwt/archive/v1.8.0.tar.gz"
   sha256 "e97c15c14dbd49d5ba435cb26e06e6cdd6e32cf9f0be6b6e9a2d2d330923ea17"
 
+  bottle do
+    cellar :any
+    sha256 "bda2b99bcf372026917957ee5d7724a13f2748504739d8a78e63efeb51a7b548" => :high_sierra
+    sha256 "223f3957505fb4c66e07ffa322d885ea78b38e26a8e0d62adfc9105216b9eb70" => :sierra
+    sha256 "ad5dbcc8369392c61e573e5e62b015dc67afdec61e311bf697a4b71c3d69fa51" => :el_capitan
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
