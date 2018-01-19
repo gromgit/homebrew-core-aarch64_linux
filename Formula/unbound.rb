af6fbb07e306d306982a261068af6deabeaa81dd
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-19 16:00:27 +0000

    unbound: update 1.6.8 bottle.

--- Formula/unbound.rb
+++ Formula/unbound.rb
@@ -5,9 +5,9 @@ class Unbound < Formula
   sha256 "e3b428e33f56a45417107448418865fe08d58e0e7fea199b855515f60884dd49"
 
   bottle do
-    sha256 "6c2088a0ce3d415e5364c3580bdd797c77f5302f054216d2e5f08455b0654f32" => :high_sierra
-    sha256 "aa331fd3f81151688aefe67289323cfeaf2e7ab176eb3f786f8bfd732337a681" => :sierra
-    sha256 "ffe70f8e112a42fd48128e8cbea512bbef73a04a4416b9137279c8de957930a3" => :el_capitan
+    sha256 "14bb3f5ce9567f835522a4cf278e843602def948c9c139c0dbe660dad666b6e9" => :high_sierra
+    sha256 "aa344f2853ef983890eed1eecd53c06e0c41e491253f8480bc944c62edc7ccc4" => :sierra
+    sha256 "8854838cff0d79d0b855b95c588588dc95b5eb1bda3a1db019717d832eebf35b" => :el_capitan
   end
 
   depends_on "openssl"
