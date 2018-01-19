6b1f17eff77fcd93a0ab6d397f80065299f65c2c
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-19 15:47:16 +0000

    sngrep: update 1.4.5 bottle.

--- Formula/sngrep.rb
+++ Formula/sngrep.rb
@@ -5,9 +5,9 @@ class Sngrep < Formula
   sha256 "16f1566f4507ba560c7461cc7ff1c1653beb14b8baf7846269bbb4880564e57f"
 
   bottle do
-    sha256 "035b453e0a13dadfb3c23b55b57c923f28f4940aef05dd93cdb2f591fe416929" => :high_sierra
-    sha256 "68d0733f5e1b07ebb3e63f8a7f72cfccbb196477a460d545769d21eab6568ff4" => :sierra
-    sha256 "bbfd86b19188591ba77f89ef44dd4bd1fcbcc60d2bc709a3b1dfc5e538f17086" => :el_capitan
+    sha256 "c922408522d0a44e37d24a6e76d9ad50ec78cdae2e4bdcac157eccf2f0bc1ee1" => :high_sierra
+    sha256 "84e4895d9674dde107d145b618e064923e2e473cdfc2b6258cddf7c982a014d5" => :sierra
+    sha256 "8a23ddb8172c6e882e525262aba6f1074dd67d2fd8a051adc27db843b1c3ca35" => :el_capitan
   end
 
   depends_on "autoconf" => :build
