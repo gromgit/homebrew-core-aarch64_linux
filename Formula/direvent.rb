ce30566cee57e29bc47782fe2f308fcbf6614463
Viktor Szakats <vszakats@users.noreply.github.com>
2018-01-20 00:55:59 +0000

    direvent: secure url(s)

--- Formula/direvent.rb
+++ Formula/direvent.rb
@@ -1,6 +1,6 @@
 class Direvent < Formula
   desc "Monitors events in the file system directories"
-  homepage "http://www.gnu.org.ua/software/direvent/direvent.html"
+  homepage "https://www.gnu.org.ua/software/direvent/direvent.html"
   url "https://ftp.gnu.org/gnu/direvent/direvent-5.1.tar.gz"
   mirror "https://ftpmirror.gnu.org/direvent/direvent-5.1.tar.gz"
   sha256 "c461600d24183563a4ea47c2fd806037a43354ea68014646b424ac797a959bdb"
