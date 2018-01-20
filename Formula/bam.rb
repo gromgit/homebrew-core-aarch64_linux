a42690a104b8b53026a3bbcc67bb3159c09b3d71
ilovezfs <ilovezfs@icloud.com>
2018-01-20 13:08:20 -0800

    bam 0.5.1
    Closes #23115.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/bam.rb
+++ Formula/bam.rb
@@ -1,8 +1,8 @@
 class Bam < Formula
   desc "Build system that uses Lua to describe the build process"
   homepage "https://matricks.github.io/bam/"
-  url "https://github.com/matricks/bam/archive/v0.5.0.tar.gz"
-  sha256 "16c0bccb6c5dee62f4381acaa004dd4f7bc9a32c10d0f2a40d83ea7e2ae25998"
+  url "https://github.com/matricks/bam/archive/v0.5.1.tar.gz"
+  sha256 "cc8596af3325ecb18ebd6ec2baee550e82cb7b2da19588f3f843b02e943a15a9"
   head "https://github.com/matricks/bam.git"
 
   bottle do
@@ -12,12 +12,6 @@ class Bam < Formula
     sha256 "bdf84408b791d7e5dca2e0f96cec0aea62f549611a8f6e1c0ea4b8dd8b03d7c0" => :el_capitan
     sha256 "1d76b73f0b46d0bb9f0ba998128abd1271f79ca5d2016703ac783a55369892e2" => :yosemite
     sha256 "505d0773336c1820057945fc016af04a1ea8b159d042725f435dd314e0f07f67" => :mavericks
-  end
-
-  # Fixes "[string "src/tools.lua"]:165: no driver set"; patch is from upstream
-  patch do
-    url "https://github.com/matricks/bam/commit/27b28f09.patch?full_index=1"
-    sha256 "9dabe77480156b49dda02378a936597e5f6b1aefaae2d88f2887c6161675d5ee"
   end
 
   def install
