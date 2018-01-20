223d731691012d3bdb651407dac0b84a3be43f86
ilovezfs <ilovezfs@icloud.com>
2018-01-20 06:28:49 -0800

    amazon-ecs-cli 1.3.0
    Closes #23103.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/amazon-ecs-cli.rb
+++ Formula/amazon-ecs-cli.rb
@@ -1,8 +1,8 @@
 class AmazonEcsCli < Formula
   desc "CLI for Amazon ECS to manage clusters and tasks for development"
   homepage "https://aws.amazon.com/ecs"
-  url "https://github.com/aws/amazon-ecs-cli/archive/v1.2.2.tar.gz"
-  sha256 "e25ffea7ea4c40205064ce9855595425325a1d3c88f2566d7578cc70281bb576"
+  url "https://github.com/aws/amazon-ecs-cli/archive/v1.3.0.tar.gz"
+  sha256 "d995b54b7a970880032e1c52d7d7c39672eacca575b5ca214d91c9230e31b57b"
 
   bottle do
     cellar :any_skip_relocation
