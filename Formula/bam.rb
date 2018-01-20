835f837b1ca2cbcaabdc2f2fdeceb06c4468fbed
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-20 21:30:25 +0000

    bam: update 0.5.1 bottle.

--- Formula/bam.rb
+++ Formula/bam.rb
@@ -7,11 +7,9 @@ class Bam < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "71b25578865fa044fdb5d233eee5ca327d602c9ef291af2e98780aef6b469ca2" => :high_sierra
-    sha256 "1ed38b80dc9aaad3e9a4655cd28ad95d8f2d7392f5b5bbdb1dbd210665aa4281" => :sierra
-    sha256 "bdf84408b791d7e5dca2e0f96cec0aea62f549611a8f6e1c0ea4b8dd8b03d7c0" => :el_capitan
-    sha256 "1d76b73f0b46d0bb9f0ba998128abd1271f79ca5d2016703ac783a55369892e2" => :yosemite
-    sha256 "505d0773336c1820057945fc016af04a1ea8b159d042725f435dd314e0f07f67" => :mavericks
+    sha256 "59aebec505aba51189ccedb1872affd1c48ca84598caa591c2e0c955817e7cd7" => :high_sierra
+    sha256 "f237da39dd743732f3cfa0a5029b3cce4b332fb08e4326183eece8fd50dcf789" => :sierra
+    sha256 "4ded8f152aa05211053796e77b9b7a9e5671b9d5871c374a85ee74e6b9cb8e50" => :el_capitan
   end
 
   def install
