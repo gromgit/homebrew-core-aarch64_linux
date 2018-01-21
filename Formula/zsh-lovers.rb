a19674619e9f0e1afffa24cce23664ed7eb5db87
ilovezfs <ilovezfs@icloud.com>
2018-01-21 03:59:48 -0800

    zsh-lovers 0.9.1 (#23124)
    use .deb file instead of unversioned URL
    also install html, pdf and metafiles

--- Formula/zsh-lovers.rb
+++ Formula/zsh-lovers.rb
@@ -1,14 +1,19 @@
 class ZshLovers < Formula
   desc "Tips, tricks, and examples for zsh"
   homepage "https://grml.org/zsh/#zshlovers"
-  url "https://grml.org/zsh/zsh-lovers.1"
-  version "0.9.0"
-  sha256 "b40802dd83f1f2eded9823776ae90af8e0b91488e97f98b3a0629f8d11953e81"
+  url "https://deb.grml.org/pool/main/z/zsh-lovers/zsh-lovers_0.9.1_all.deb",
+      :using => :nounzip
+  version "0.9.1"
+  sha256 "011b7931a555c77e98aa9cdd16b3c4670c0e0e3b5355e5fd60188885a6678de8"
 
   bottle :unneeded
 
   def install
-    man1.install "zsh-lovers.1"
+    system "tar", "xf", "zsh-lovers_#{version}_all.deb"
+    system "tar", "xf", "data.tar.xz"
+    system "gunzip", *Dir["usr/**/*.gz"]
+    prefix.install_metafiles "usr/share/doc/zsh-lovers"
+    prefix.install "usr/share"
   end
 
   test do
