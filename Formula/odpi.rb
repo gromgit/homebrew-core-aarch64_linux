7239489aa6b08d7e52e7754d3a48daf3273caf93
Gábor Sebestyén <segabor@gmail.com>
2018-01-24 16:52:28 +0100

    odpi 2.1.0 (new formula)
    Closes #23250.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/odpi.rb
@@ -0,0 +1,33 @@
+class Odpi < Formula
+  desc "Oracle Database Programming Interface for Drivers and Applications"
+  homepage "https://oracle.github.io/odpi/"
+  url "https://github.com/oracle/odpi/archive/v2.1.0.tar.gz"
+  sha256 "b74e06feb1a25d907b97739eda500d03af50c87cd5861b27a0e0baf79f739c06"
+
+  def install
+    system "make"
+
+    lib.install Dir["lib/*"]
+    include.install Dir["include/*"]
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <dpi.h>
+
+      int main()
+      {
+        dpiContext* context = NULL;
+        dpiErrorInfo errorInfo;
+
+        dpiContext_create(DPI_MAJOR_VERSION, DPI_MINOR_VERSION, &context, &errorInfo);
+
+        return 0;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lodpic", "-o", "test"
+    system "./test"
+  end
+end
