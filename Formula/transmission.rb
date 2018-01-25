5c4df46b2f246b3e10f6fef644cfff6c93b60d35
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-25 11:51:58 +0000

    transmission: update 2.93 bottle.

--- Formula/transmission.rb
+++ Formula/transmission.rb
@@ -5,9 +5,9 @@ class Transmission < Formula
   sha256 "8815920e0a4499bcdadbbe89a4115092dab42ce5199f71ff9a926cfd12b9b90b"
 
   bottle do
-    sha256 "10c8ab080a30f20bf2a777d71cecf5efdddbbf5ff99f8d1547ec6d1af892083a" => :high_sierra
-    sha256 "d92ff97c980b5b21f9a339add298a3c891ff899bd46c6875d5b082c99be5412b" => :sierra
-    sha256 "c899c5ae9a3041422939940425c5382af3b0f2ab1acac4ffa3a5bd33fc45cbda" => :el_capitan
+    sha256 "753d56b0ecb6e70ed2841b1abc23247c1b02666e2b95c5a6ac32e9815a0c6131" => :high_sierra
+    sha256 "5e190bda32de9a3ab3dbd7909a23cf79b1fbd891c10f9a537469a29a7d85d6dd" => :sierra
+    sha256 "ba5967f2f272d2376f14b1c0043853f0d103877cb865546fe919b8ae74316982" => :el_capitan
   end
 
   option "with-nls", "Build with native language support"
