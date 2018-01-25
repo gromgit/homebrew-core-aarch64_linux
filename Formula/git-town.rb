63c3dc8042d0010f7edbb6d2e3493c5b431a3d36
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-25 05:49:07 +0000

    git-town: update 6.0.1 bottle.

--- Formula/git-town.rb
+++ Formula/git-town.rb
@@ -6,9 +6,9 @@ class GitTown < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "17bf26bc0a68b03bec2ba4450d0730fe1636e6db043d6064436038564fa58100" => :high_sierra
-    sha256 "e967d48b8f65d1173965f3f8eabd95ba218d4224d83f4bd79f9d91dfb97fa66c" => :sierra
-    sha256 "6fc6244729e3487901c5c6b4c7c35a390786ce648617b0f6a951fbabfce8155a" => :el_capitan
+    sha256 "76f3d3e2cf6ea9b6026cc9521e5be5a5dd81d671a291e47d82763288d6f8ba76" => :high_sierra
+    sha256 "28320a9c1c3c6fee0b1d5fa724130bdc00b65d7f83d91973fc94047601ea1554" => :sierra
+    sha256 "636f1e8d156ef3c91a05a4d2b3a296bc1480ed7832ee478af18c4bdc3e8b9a89" => :el_capitan
   end
 
   depends_on "go" => :build
