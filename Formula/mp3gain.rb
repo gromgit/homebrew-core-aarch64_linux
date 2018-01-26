32ae0018ea589011b5b895b4e3c47ce06ab1ada4
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-26 06:55:24 +0000

    mp3gain: update 1.6.1 bottle.

--- Formula/mp3gain.rb
+++ Formula/mp3gain.rb
@@ -6,12 +6,10 @@ class Mp3gain < Formula
   sha256 "552e77f9333a04f35d68808565ec99b5eb6707985ea946f60f13c81a42daf25d"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "4c4fb949ec3332597b67bca10b87add64e0e2fd987dc4e3f1573ff21ad35358e" => :high_sierra
-    sha256 "a9548ef1c648c0aaa4d203726431a5425518a7a2f54df0b6d5e8c92715404e3d" => :sierra
-    sha256 "27506d45e11a7da5c9555cb17318cf99802d5a8749ad17ed66b7a4b1465790cd" => :el_capitan
-    sha256 "f14681110117c1762ef909dc0b282bb4c23f298c92eefe20d77fab0199cadbaa" => :yosemite
-    sha256 "17abc15792bc75fa186c6748f1cb666814334b2cfec11d708bb9431846746809" => :mavericks
+    cellar :any
+    sha256 "98b7ecffc65da0e71336ad8245194a53efc35f5a6800e6e150a4a5c71a064fc7" => :high_sierra
+    sha256 "614b1a01ffe49eb9efefa7a67d96be47ada351435518313ff0c5607e9fa41b15" => :sierra
+    sha256 "4c33387aa252d25779c2c0d15dee99fee03873f73a65200d2e69d74f0df48158" => :el_capitan
   end
 
   depends_on "mpg123"
