02d014912c33459ca55293b6ad05a923796c7c28
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-26 06:00:19 +0000

    augeas: update 1.10.0 bottle.

--- Formula/augeas.rb
+++ Formula/augeas.rb
@@ -5,9 +5,9 @@ class Augeas < Formula
   sha256 "2a90f6984c3cca1e64dfcad3af490f38ae86e2f3510ed9e46a391cd947860213"
 
   bottle do
-    sha256 "14cbc9147bda6bad9c014af9d4693873f47ce1d72fa6c29aa5b85a1702d632b4" => :high_sierra
-    sha256 "d7c5e0a0a9c26d56cf6b0f2b28568ee4b25454f33744dacb8aa79fe0c596f8a3" => :sierra
-    sha256 "cffbbcf9bb75bd0da58258d9a8996f40c470791fca616eff74485b351fbe0e63" => :el_capitan
+    sha256 "424a1bbaa89ca69cc7621834110df7eb1868cdc856727fbdbe05718d546cb844" => :high_sierra
+    sha256 "5aa2021efaa6b6899010ad94fe886e3b46a8268e1d90eab7429027011dfdf724" => :sierra
+    sha256 "28d3591c81fb7ae166d98cc11b64623875cc3fe886f650c9d80d85abf5473c83" => :el_capitan
   end
 
   head do
