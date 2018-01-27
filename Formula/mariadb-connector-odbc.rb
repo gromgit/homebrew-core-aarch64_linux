0930cec85d0e2f1a2a79d5a23fa855ae9708b456
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-27 08:01:26 +0000

    mariadb-connector-odbc: add 3.0.2 bottle.

--- Formula/mariadb-connector-odbc.rb
+++ Formula/mariadb-connector-odbc.rb
@@ -4,6 +4,13 @@ class MariadbConnectorOdbc < Formula
   url "https://downloads.mariadb.org/f/connector-odbc-3.0.2/mariadb-connector-odbc-3.0.2-ga-src.tar.gz"
   sha256 "eba4fbda21ae9d50c94d2cd152f0ec14dde3989522f41ef7d22aa0948882ff93"
 
+  bottle do
+    cellar :any
+    sha256 "c7ab8dfb656eb5918ea5b5844ff27a04e6ed62cb336ea193311e89830791f4b2" => :high_sierra
+    sha256 "00f033011f0d2c7d2921178d2e111709e6031d80cf8010710a1db2a054d076bf" => :sierra
+    sha256 "c9ad0e034c618baa79b40287a109cf6a9ac060106b1f64d1618f270ce2492134" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "mariadb-connector-c"
   depends_on "openssl"
