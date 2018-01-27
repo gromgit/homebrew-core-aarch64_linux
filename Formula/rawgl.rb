a29ff2e377e81e5f1caa536aa3cdcc455f6b6d07
Viktor Szakats <vszakats@users.noreply.github.com>
2018-01-27 18:30:40 +0000

    rawgl: secure/fix url(s)

--- Formula/rawgl.rb
+++ Formula/rawgl.rb
@@ -1,6 +1,6 @@
 class Rawgl < Formula
   desc "Rewritten engine for Another World"
-  homepage "http://cyxdown.free.fr/rawgl/"
+  homepage "https://github.com/cyxx/rawgl"
   url "https://github.com/cyxx/rawgl/archive/rawgl-0.2.1.tar.gz"
   sha256 "da00d4abbc266e94c3755c2fd214953c48698826011b1d4afbebb99396240073"
   head "https://github.com/cyxx/rawgl.git"
