56c7e879ee0258d1aa69dcc03f2c1697068439e1
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2018-01-27 01:46:06 -0800

    jhiccup 2.0.8 (#23344)
    add a test

--- Formula/jhiccup.rb
+++ Formula/jhiccup.rb
@@ -1,13 +1,13 @@
 class Jhiccup < Formula
   desc "Measure pauses and stalls of an app's Java runtime platform"
-  homepage "https://www.azul.com/products/open-source-tools/jhiccup-performance-measurement-tool/"
-  url "https://www.azulsystems.com/sites/default/files/images/jHiccup.1.3.7.zip"
-  sha256 "abc029bfb55bbe59824d0b6db6845bd0f08befba3b860747ed601a6c27573f24"
+  homepage "https://www.azul.com/jhiccup/"
+  url "https://www.azul.com/files/jHiccup-2.0.8-dist.zip"
+  sha256 "0ff7f919909f0ba27b236f3c18d90c8f002948591a47e5d325566200ff018832"
 
   bottle :unneeded
 
   def install
-    bin.install "jHiccup"
+    bin.install "jHiccup", "jHiccupLogProcessor"
 
     # Simple script to create and open a new plotter spreadsheet
     (bin+"jHiccupPlotter").write <<~EOS
@@ -17,11 +17,15 @@ class Jhiccup < Formula
       open $TMPFILE
     EOS
 
-    prefix.install "target"
+    prefix.install "jHiccup.jar"
     prefix.install "jHiccupPlotter.xls"
     inreplace "#{bin}/jHiccup" do |s|
       s.gsub! /^JHICCUP_JAR_FILE=.*$/,
-              "JHICCUP_JAR_FILE=#{prefix}/target/jHiccup.jar"
+              "JHICCUP_JAR_FILE=#{prefix}/jHiccup.jar"
     end
+  end
+
+  test do
+    assert_match "CSV", shell_output("#{bin}/jHiccup -h", 255)
   end
 end
