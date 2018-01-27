5ade0fe703aae4d09504f8e355041771fc035941
Viktor Szakats <vszakats@users.noreply.github.com>
2018-01-27 18:30:39 +0000

    herrie: secure/fix url(s)

--- Formula/herrie.rb
+++ Formula/herrie.rb
@@ -1,7 +1,7 @@
 class Herrie < Formula
   desc "Minimalistic audio player built upon Ncurses"
-  homepage "http://herrie.info/"
-  url "http://herrie.info/distfiles/herrie-2.2.tar.bz2"
+  homepage "https://herrie.info/"
+  url "https://github.com/EdSchouten/herrie/releases/download/herrie-2.2/herrie-2.2.tar.bz2"
   sha256 "142341072920f86b6eb570b8f13bf5fd87c06cf801543dc7d1a819e39eb9fb2b"
 
   bottle do
