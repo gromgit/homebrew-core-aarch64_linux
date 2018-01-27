351c9c60915e70b232177ece4c3809f865461b13
Viktor Szakats <vszakats@users.noreply.github.com>
2018-01-27 18:30:40 +0000

    sonarlint: secure/fix url(s)

--- Formula/sonarlint.rb
+++ Formula/sonarlint.rb
@@ -1,6 +1,6 @@
 class Sonarlint < Formula
   desc "Provides pre-commit feedback on quality issues injected into code"
-  homepage "http://www.sonarlint.org/commandline/index.html"
+  homepage "https://github.com/SonarSource/sonarlint-cli"
   url "https://bintray.com/sonarsource/Distribution/download_file?file_path=sonarlint-cli%2Fsonarlint-cli-2.1.0.566.zip"
   sha256 "75320a0629f3f7b83ccaa4098ccbbb8decedc518a1b8fc2503f753266dc71d70"
 
