73775ce332a94934e93b167aea32a67eb63c17c5
ilovezfs <ilovezfs@icloud.com>
2018-01-26 18:34:14 -0800

    zile: revision for bdw-gc
    Closes #23313.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/zile.rb
+++ Formula/zile.rb
@@ -4,7 +4,7 @@ class Zile < Formula
   url "https://ftp.gnu.org/gnu/zile/zile-2.4.14.tar.gz"
   mirror "https://ftpmirror.gnu.org/zile/zile-2.4.14.tar.gz"
   sha256 "7a78742795ca32480f2bab697fd5e328618d9997d6f417cf1b14e9da9af26b74"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "1594935cd47b6dc28f96067bb804a21a1015a2402212f0356e4b2254340d7ce2" => :high_sierra
