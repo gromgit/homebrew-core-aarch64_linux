7abde9a40395fc53d8ec08176581efcb4d51d611
Viktor Szakats <vszakats@users.noreply.github.com>
2018-01-27 18:30:40 +0000

    ilmbase: secure/fix url(s)

--- Formula/ilmbase.rb
+++ Formula/ilmbase.rb
@@ -1,6 +1,6 @@
 class Ilmbase < Formula
   desc "OpenEXR ILM Base libraries (high dynamic-range image file format)"
-  homepage "http://www.openexr.com/"
+  homepage "https://www.openexr.com/"
   url "https://savannah.nongnu.org/download/openexr/ilmbase-2.2.0.tar.gz"
   mirror "https://mirrorservice.org/sites/download.savannah.gnu.org/releases/openexr/ilmbase-2.2.0.tar.gz"
   sha256 "ecf815b60695555c1fbc73679e84c7c9902f4e8faa6e8000d2f905b8b86cedc7"
