bc752c98d704c677c5c5096031ff79598d9b388a
ilovezfs <ilovezfs@icloud.com>
2018-01-28 12:30:35 -0800

    checkstyle 8.8 (#23387)

--- Formula/checkstyle.rb
+++ Formula/checkstyle.rb
@@ -1,8 +1,8 @@
 class Checkstyle < Formula
   desc "Check Java source against a coding standard"
   homepage "https://checkstyle.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/checkstyle/checkstyle/8.7/checkstyle-8.7-all.jar"
-  sha256 "284edd0d33772fb0d11d4fbb64073074845e167c572f00598e900d365b9254ae"
+  url "https://downloads.sourceforge.net/project/checkstyle/checkstyle/8.8/checkstyle-8.8-all.jar"
+  sha256 "eff40175e846842f35825464f3d4dbdbe890803163b22a2fd8cd1b38aae50536"
 
   bottle :unneeded
 
