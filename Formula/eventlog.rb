4905387268d648357809de5429bc37e361e4044b
ilovezfs <ilovezfs@icloud.com>
2018-01-28 00:49:53 -0800

    eventlog: update url (#23379)

--- Formula/eventlog.rb
+++ Formula/eventlog.rb
@@ -1,7 +1,7 @@
 class Eventlog < Formula
   desc "Replacement for syslog API providing structure to messages"
-  homepage "https://www.balabit.com/downloads/files/eventlog/"
-  url "https://www.balabit.com/downloads/files/syslog-ng/sources/3.4.3/source/eventlog_0.2.13.tar.gz"
+  homepage "https://my.balabit.com/downloads/eventlog/"
+  url "https://my.balabit.com/downloads/syslog-ng/sources/3.4.3/source/eventlog_0.2.13.tar.gz"
   sha256 "7cb4e6f316daede4fa54547371d5c986395177c12dbdec74a66298e684ac8b85"
 
   bottle do
