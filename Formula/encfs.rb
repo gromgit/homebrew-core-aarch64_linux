f927a8dd91a432a5896143cd9aea1dff3653c9cf
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-28 17:32:47 +0000

    encfs: update 1.9.3 bottle.

--- Formula/encfs.rb
+++ Formula/encfs.rb
@@ -6,10 +6,9 @@ class Encfs < Formula
   head "https://github.com/vgough/encfs.git"
 
   bottle do
-    sha256 "05cbb2bdfac9bfd6bddbb48c007f6edd00cccd286c9c66addf3aaa0994e294d6" => :high_sierra
-    sha256 "c2a1b09f4e54c6a5325045004d4f9eba4f4f3ac75954ab79302f22f9835ed70f" => :sierra
-    sha256 "27f0e9e05a1f7eca238318e53eb6dd79a13875f84ea258250d15bf679ecc0f46" => :el_capitan
-    sha256 "37dcf80058b6db6d3dd9a0b18ea71310bf731c390c658f00de13b6a1db7fe879" => :yosemite
+    sha256 "4fe8170189664573b9f38d4397689f9c0564ac88a5a531755f6dc9bca8025cb9" => :high_sierra
+    sha256 "04ed26da2ec5b26a1862c40167eceb064b27cfaf4e2027c4b38e86713979b7aa" => :sierra
+    sha256 "34f6417041963eb692491d896467c8e9eff041d7c2ddcf6291a75397c111d4df" => :el_capitan
   end
 
   depends_on "cmake" => :build
