377722623feb7759825ba3effb07dfa5c4afc592
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-29 14:23:56 +0000

    qtkeychain: add 0.8.0 bottle.

--- Formula/qtkeychain.rb
+++ Formula/qtkeychain.rb
@@ -4,6 +4,13 @@ class Qtkeychain < Formula
   url "https://github.com/frankosterfeld/qtkeychain/archive/v0.8.0.tar.gz"
   sha256 "b492f603197538bc04b2714105b1ab2b327a9a98d400d53d9a7cb70edd2db12f"
 
+  bottle do
+    cellar :any
+    sha256 "7bb2cc22642e794684140108c996c3373c4ce248f230622a4039fe59f7b178fe" => :high_sierra
+    sha256 "3ff9191c8be9555c43ca5b4ba2c2aae4c93bf640c3d03a7e42bac76920f46c80" => :sierra
+    sha256 "3197c42afc2c347dfd25db4bf4b45ceba70d2b866e4ee3f23f57b3501323a3c9" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "qt"
 
