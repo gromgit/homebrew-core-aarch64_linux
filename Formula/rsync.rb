9105ffbc684a2d1fc95fbaa6dba8522c36235c80
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-29 01:57:59 +0000

    rsync: update 3.1.3 bottle.

--- Formula/rsync.rb
+++ Formula/rsync.rb
@@ -7,10 +7,9 @@ class Rsync < Formula
   sha256 "55cc554efec5fdaad70de921cd5a5eeb6c29a95524c715f3bbf849235b0800c0"
 
   bottle do
-    sha256 "51924d6d4c11681643fa8c41e3204104bbb356ba32984fa951fe7f0d65081ee6" => :high_sierra
-    sha256 "36a448b89f5d70680ea20f7235ff1003143f00734753d19e1d27c23515589671" => :sierra
-    sha256 "795d7a7ab57aa31fce67b053f5753e3b0b108f408ef95867fe03562b591d2eda" => :el_capitan
-    sha256 "93d8c8ae676a50d71b93b72ac4b2a54b91b3730308d20c303b2c1a0de238b78e" => :yosemite
+    sha256 "1b023b82b13c5afaaf0ef980465b660c7a3972f842b2a73cbb2fe541d0a7d210" => :high_sierra
+    sha256 "e33f81c60ee6abe14cec39696d5d7c0d1384fac41fe5d2ca0b8b65db006d637b" => :sierra
+    sha256 "b25d1d738aeb8c9964ade0962f84f47e8818be89bb476d683be5e4fbd7770c64" => :el_capitan
   end
 
   depends_on "autoconf" => :build
