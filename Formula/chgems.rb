8e43e598e08a4bc5f4d9f1de81bbb7251e04069b
Utkarsh Tripathi <f2016894@pilani.bits-pilani.ac.in>
2018-01-29 17:57:16 +0530

    chgems: add test (#23410)

--- Formula/chgems.rb
+++ Formula/chgems.rb
@@ -1,5 +1,5 @@
 class Chgems < Formula
-  desc "chroot for Ruby gems"
+  desc "Chroot for Ruby gems"
   homepage "https://github.com/postmodern/chgems#readme"
   url "https://github.com/postmodern/chgems/archive/v0.3.2.tar.gz"
   sha256 "515d1bfebb5d5183a41a502884e329fd4c8ddccb14ba8a6548a1f8912013f3dd"
@@ -16,5 +16,10 @@ class Chgems < Formula
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    output = shell_output("#{bin}/chgems . gem env")
+    assert_match "rubygems.org", output
   end
 end
