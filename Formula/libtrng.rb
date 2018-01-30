d42b381dc1cfa5aaef870656c970f872a14befe8
ilovezfs <ilovezfs@icloud.com>
2018-01-30 07:21:06 -0800

    libtrng 4.20
    Closes #23475.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libtrng.rb
+++ Formula/libtrng.rb
@@ -1,8 +1,8 @@
 class Libtrng < Formula
   desc "Tina's Random Number Generator Library"
   homepage "https://www.numbercrunch.de/trng/"
-  url "https://www.numbercrunch.de/trng/trng-4.19.tar.gz"
-  sha256 "36b49961c631ae01d770ff481796c8b280e18c6b6e6b5ca00f2b868533b0492e"
+  url "https://www.numbercrunch.de/trng/trng-4.20.tar.gz"
+  sha256 "8cffd03392a3e498fe9f93ccfa9ff0c9eacf9fd9d33e3655123852d701bbacbc"
 
   bottle do
     cellar :any
