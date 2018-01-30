1b50bd4708d022a6bb3ed4c0424d758ec2e7bd8b
ilovezfs <ilovezfs@icloud.com>
2018-01-30 02:20:34 -0800

    udpxy 1.0.23-12
    Closes #23463.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/udpxy.rb
+++ Formula/udpxy.rb
@@ -1,10 +1,10 @@
 class Udpxy < Formula
   desc "UDP-to-HTTP multicast traffic relay daemon"
   homepage "http://www.udpxy.com/"
-  url "http://www.udpxy.com/download/1_23/udpxy.1.0.23-9-prod.tar.gz"
-  mirror "https://fossies.org/linux/www/udpxy.1.0.23-9-prod.tar.gz"
-  version "1.0.23-9"
-  sha256 "6ce33b1d14a1aeab4bd2566aca112e41943df4d002a7678d9a715108e6b714bd"
+  url "http://www.udpxy.com/download/1_23/udpxy.1.0.23-12-prod.tar.gz"
+  mirror "https://fossies.org/linux/www/udpxy.1.0.23-12-prod.tar.gz"
+  version "1.0.23-12"
+  sha256 "16bdc8fb22f7659e0427e53567dc3e56900339da261199b3d00104d699f7e94c"
 
   bottle do
     cellar :any_skip_relocation
@@ -15,10 +15,6 @@ class Udpxy < Formula
     sha256 "7624631dffaa797191689b05fcb5d7c87c0ad233e49c308b10462c08c8a955e4" => :yosemite
     sha256 "45dcc2c1a7d1f0170ae44edf600fee1f6112fd1e11530548a7e3b1870d71a7d8" => :mavericks
   end
-
-  # Fix gzip path in Makefile for uname Darwin, this is needed to fix the install task
-  # https://sourceforge.net/p/udpxy/patches/4/
-  patch :DATA
 
   def install
     system "make"
@@ -51,18 +47,3 @@ class Udpxy < Formula
     EOS
   end
 end
-
-__END__
---- a/Makefile 2014-07-31 18:40:40.000000000 +0200
-+++ b/Makefile 2014-07-31 18:41:05.000000000 +0200
-@@ -32,7 +32,9 @@
- ALL_FLAGS = -W -Wall -Werror --pedantic $(CFLAGS)
-
- SYSTEM=$(shell uname 2>/dev/null)
--ifeq ($(SYSTEM), FreeBSD)
-+ifeq ($(SYSTEM), Darwin)
-+GZIP := /usr/bin/gzip
-+else ifeq ($(SYSTEM), FreeBSD)
- MAKE := gmake
- GZIP := /usr/bin/gzip
- endif
