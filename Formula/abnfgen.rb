57391b6ca410a5443551f5ed995273620d1b6d8e
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-30 14:12:31 +0000

    abnfgen: update 0.18 bottle.

--- Formula/abnfgen.rb
+++ Formula/abnfgen.rb
@@ -6,11 +6,9 @@ class Abnfgen < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "66283b4588de8a104eeb19c24e835b2af4c26b86f97f1d14b95b3ad99409af9b" => :high_sierra
-    sha256 "173a48d1e02cabc92904ad0b01842e90fc5b43411a1a00db59088b3840a1d3e3" => :sierra
-    sha256 "25aa694b994374b0194f65a01afdf1911ddd366d3195a6ac6451e81f87234768" => :el_capitan
-    sha256 "6020fe82950619f9ddf60265c80c2e8f2f68808618e87150d1634f5ec2c0eff8" => :yosemite
-    sha256 "004aa4f75ee0e51327a69a558cb3920b6859303b1d032e32e3caabe623dc35d4" => :mavericks
+    sha256 "fb10a0bd2410bcd5ecb53ee7c39798b1a8d87a8ac69ee11de3444c1f0af458d2" => :high_sierra
+    sha256 "31c28287fdf41f1c5767a267343c387b0208e615da3616877c8c6a1c694974af" => :sierra
+    sha256 "ac1d3849426bec85a2b5eaba4bac04111aed9c118fc9c76278ffeba2b42cd016" => :el_capitan
   end
 
   def install
