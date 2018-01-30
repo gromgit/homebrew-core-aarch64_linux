e22473e890bfdb37ef314469da43854dc1ffc247
commitay <commitay@users.noreply.github.com>
2018-01-31 02:57:09 +1000

    vagrant-completion 2.0.2 (#23445)

--- Formula/vagrant-completion.rb
+++ Formula/vagrant-completion.rb
@@ -1,8 +1,8 @@
 class VagrantCompletion < Formula
   desc "Bash completion for Vagrant"
   homepage "https://github.com/hashicorp/vagrant"
-  url "https://github.com/hashicorp/vagrant/archive/v2.0.1.tar.gz"
-  sha256 "212b91c45f60a825fcfc656424021e2550833778a6d4ebe13458676201a04eba"
+  url "https://github.com/hashicorp/vagrant/archive/v2.0.2.tar.gz"
+  sha256 "394670c06f8c4201d8a351a63e070d5b0325feeb08a2965bd0051b2ffce34eea"
 
   head "https://github.com/hashicorp/vagrant.git"
 
