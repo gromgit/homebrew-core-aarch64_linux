1a881fe22643810a7f5473bff8353f7b52568adc
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-01 06:09:30 +0000

    feh: update 2.23.1 bottle.

--- Formula/feh.rb
+++ Formula/feh.rb
@@ -5,9 +5,9 @@ class Feh < Formula
   sha256 "9bc164d0863d41201bd253a2652dcee5806a9c6a8b5918bb8ba09fcee6d7e9be"
 
   bottle do
-    sha256 "3a855680fc84154b2501d53dbb7a4e35b979c653bfa634cbb46b619140977a0f" => :high_sierra
-    sha256 "81d589730ed0bad7ba32eaf37886b411f61380322d186633fdc039a82ce1bb1b" => :sierra
-    sha256 "3662d978ff61df130e1d904d8707f35a4b6c7ce2f05a8dae29cf106ab4171ed5" => :el_capitan
+    sha256 "e5dae1c3f9ccbcaa1f531320d8ce731b305794246accc352ed54e58771c18ed9" => :high_sierra
+    sha256 "e79da7891f5fcaf66cac98bbf9aa2c4d082dd3491d9e64fbca0afceff186cb2c" => :sierra
+    sha256 "9c6ecf778acbfbd1accd3b720366c52553c1284d79a90d720742d343bd349730" => :el_capitan
   end
 
   depends_on :x11
