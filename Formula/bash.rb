c4612952bbb06335705df6b492d0adb020aa9a81
ilovezfs <ilovezfs@icloud.com>
2018-02-01 01:49:38 -0800

    bash 4.4.18
    Closes #23559.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/bash.rb
+++ Formula/bash.rb
@@ -12,7 +12,7 @@ class Bash < Formula
     mirror "https://gnu.cu.be/bash/bash-4.4.tar.gz"
     mirror "https://mirror.unicorncloud.org/gnu/bash/bash-4.4.tar.gz"
     sha256 "d86b3392c1202e8ff5a423b302e6284db7f8f435ea9f39b5b1b20fd3ac36dfcb"
-    version "4.4.12"
+    version "4.4.18"
 
     %w[
       001 3e28d91531752df9a8cb167ad07cc542abaf944de9353fe8c6a535c9f1f17f0f
@@ -27,6 +27,12 @@ class Bash < Formula
       010 8465c6f2c56afe559402265b39d9e94368954930f9aa7f3dfa6d36dd66868e06
       011 dd56426ef7d7295e1107c0b3d06c192eb9298f4023c202ca2ba6266c613d170d
       012 fac271d2bf6372c9903e3b353cb9eda044d7fe36b5aab52f21f3f21cd6a2063e
+      013 1b25efacbc1c4683b886d065b7a089a3601964555bcbf11f3a58989d38e853b6
+      014 a7f75cedb43c5845ab1c60afade22dcb5e5dc12dd98c0f5a3abcfb9f309bb17c
+      015 d37602ecbeb62d5a22c8167ea1e621fcdbaaa79925890a973a45c810dd01c326
+      016 501f91cc89fadced16c73aa8858796651473602c722bb29f86a8ba588d0ff1b1
+      017 773f90b98768d4662a22470ea8eec5fdd8e3439f370f94638872aaf884bcd270
+      018 5bc494b42f719a8b0d844b7bd9ad50ebaae560e97f67c833c9e7e9d53981a8cc
     ].each_slice(2) do |p, checksum|
       patch :p0 do
         url "https://ftp.gnu.org/gnu/bash/bash-4.4-patches/bash44-#{p}"
