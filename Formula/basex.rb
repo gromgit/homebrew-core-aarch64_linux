1d32a4e5fc788c8504a56c48dad0012eea20ea43
Alexander Holupirek <alex@holupirek.de>
2018-02-01 10:38:37 +0100

    basex 9.0-rc20180201.092127 (devel) (#23558)

--- Formula/basex.rb
+++ Formula/basex.rb
@@ -6,9 +6,9 @@ class Basex < Formula
   sha256 "60faecf417f1607780a70a237138a2f839f2218d2d7bee20fa30b7738245a244"
 
   devel do
-    url "http://files.basex.org/releases/latest/BaseX90-20180130.105237.zip"
-    version "9.0-rc20180130.105237"
-    sha256 "85d598a7e5d8ac5ba1fed175266493358b3fbdbead555c968e0b64ecb86a04f3"
+    url "http://files.basex.org/releases/latest/BaseX90-20180201.092127.zip"
+    version "9.0-rc20180201.092127"
+    sha256 "d811ad8a22c72168a246ab290cb05c67ad77d0d1f30a099f1478c9e23158a7fc"
   end
 
   bottle :unneeded
