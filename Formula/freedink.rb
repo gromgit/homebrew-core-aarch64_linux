30fc9c842ed1c04af842b0615ecb6bc94c35b7fa
ilovezfs <ilovezfs@icloud.com>
2018-02-02 07:29:01 -0800

    freedink: revision for libzip

--- Formula/freedink.rb
+++ Formula/freedink.rb
@@ -3,7 +3,7 @@ class Freedink < Formula
   homepage "https://www.gnu.org/software/freedink/"
   url "https://ftp.gnu.org/gnu/freedink/freedink-108.4.tar.gz"
   sha256 "82cfb2e019e78b6849395dc4750662b67087d14f406d004f6d9e39e96a0c8521"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "d78130e6917a14d7a5e367bada96e919915695d68d7796709d5bb99fbbc593f9" => :high_sierra
