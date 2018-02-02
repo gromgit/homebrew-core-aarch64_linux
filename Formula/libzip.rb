f4a768242525ec4ee5f18ca72dceb794abe9f60e
ilovezfs <ilovezfs@icloud.com>
2018-02-02 05:14:04 -0800

    libzip 1.4.0
    depend on cmake at build time

--- Formula/libzip.rb
+++ Formula/libzip.rb
@@ -1,8 +1,8 @@
 class Libzip < Formula
   desc "C library for reading, creating, and modifying zip archives"
   homepage "https://www.nih.at/libzip/"
-  url "https://www.nih.at/libzip/libzip-1.3.0.tar.xz"
-  sha256 "aa936efe34911be7acac2ab07fb5c8efa53ed9bb4d44ad1fe8bff19630e0d373"
+  url "https://www.nih.at/libzip/libzip-1.4.0.tar.xz"
+  sha256 "e508aba025f5f94b267d5120fc33761bcd98440ebe49dbfe2ed3df3afeacc7b1"
 
   bottle do
     cellar :any
@@ -12,13 +12,12 @@ class Libzip < Formula
     sha256 "132e57785f7973dfd451c8f44794a93e38f9f2040c0779b9eff6ba5a1f81711d" => :yosemite
   end
 
+  depends_on "cmake" => :build
+
   conflicts_with "libtcod", :because => "both install `zip.h` header"
 
   def install
-    system "./configure", "--prefix=#{prefix}",
-                          "--mandir=#{man}",
-                          "CXX=#{ENV.cxx}",
-                          "CXXFLAGS=#{ENV.cflags}"
+    system "cmake", ".", *std_cmake_args
     system "make", "install"
   end
 
