7f9e58b1b9c5004cdd58e83dffe58ffdef34a418
Adam Radabaugh <adam@walkingtoast.com>
2018-02-01 23:23:18 -0800

    hubflow 1.5.3 (#23574)

--- Formula/hubflow.rb
+++ Formula/hubflow.rb
@@ -3,8 +3,8 @@ class Hubflow < Formula
   desc "GitFlow for GitHub"
   homepage "https://datasift.github.io/gitflow/"
   url "https://github.com/datasift/gitflow.git",
-    :tag => "1.5.2",
-    :revision => "8bb7890b39f782864d55cfca5a156c926fa53c0d"
+    :tag => "1.5.3",
+    :revision => "ed032438d2100b826d2fd5c8281b5e07d88ab9eb"
   head "https://github.com/datasift/gitflow.git"
 
   bottle :unneeded
