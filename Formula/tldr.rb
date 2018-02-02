f2512d1b68bc531ca8046994234efabc9a3ab9b3
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-02 16:36:27 +0000

    tldr: update 1.3.0_2 bottle.

--- Formula/tldr.rb
+++ Formula/tldr.rb
@@ -8,10 +8,9 @@ class Tldr < Formula
 
   bottle do
     cellar :any
-    sha256 "48340f848b44b4bd6ffa3ee2c960b569966dd5446e1af8b301b06723a867f1ef" => :high_sierra
-    sha256 "5ff44173bde5f458335b89ba3e4f4c9a1d917dc3f0f4059477d82d91e079f691" => :sierra
-    sha256 "f8b2e7e60cc4ff8c212914f5bcf4b7d413ae7a69e095c6ce29ce79010d6354b5" => :el_capitan
-    sha256 "376962a72d11da559d25aabb3d22f7b2105e21809c58d09597ab312b3685719a" => :yosemite
+    sha256 "00f5ce204fa62e51ed3be821abba9fdda36335b6cecf65231158c02b9713f357" => :high_sierra
+    sha256 "06f9609695fb6b14bbd78f3246bb176b341aecda6ce70515723cee8e8e4ced69" => :sierra
+    sha256 "209c571e3472128665194e73451199087236758a0a7cfa6e88ffd7aa444abbac" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
