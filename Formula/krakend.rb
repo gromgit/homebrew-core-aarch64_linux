705354e4711076ddf56a45b9bf4bd88e7013d48a
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-02 16:40:17 +0000

    krakend: add 0.4.0 bottle.

--- Formula/krakend.rb
+++ Formula/krakend.rb
@@ -4,6 +4,13 @@ class Krakend < Formula
   url "https://github.com/devopsfaith/krakend-ce/archive/0.4.0.tar.gz"
   sha256 "c0e48b0e8234b1d7975c3dfa953f3c7568e4f1583c5ffb3fef95ee236a902afd"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4dcb92551e3b2b0436f70dc7b488580543081cc6ae398557534ad5034728247c" => :high_sierra
+    sha256 "f7c0b5af16433dd039d9d3a640e6919ad9ae32d73f636a23b3617db08bff0808" => :sierra
+    sha256 "78c6d4f5a54f9ecebaca0f9806ef9ff3a3588bb941e3a0e2c971173507e0a392" => :el_capitan
+  end
+
   depends_on "dep" => :build
   depends_on "go" => :build
 
