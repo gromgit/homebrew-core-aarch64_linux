808b183ccc33984905362199c795fe4d21e12fd0
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-03 17:16:55 +0000

    libimobiledevice: secure url(s)

--- Formula/libimobiledevice.rb
+++ Formula/libimobiledevice.rb
@@ -1,7 +1,7 @@
 class Libimobiledevice < Formula
   desc "Library to communicate with iOS devices natively"
-  homepage "http://www.libimobiledevice.org/"
-  url "http://www.libimobiledevice.org/downloads/libimobiledevice-1.2.0.tar.bz2"
+  homepage "https://www.libimobiledevice.org/"
+  url "https://www.libimobiledevice.org/downloads/libimobiledevice-1.2.0.tar.bz2"
   sha256 "786b0de0875053bf61b5531a86ae8119e320edab724fc62fe2150cc931f11037"
   revision 2
 
