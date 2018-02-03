300139007d303c371ec3e1bdfe90c04f8526f2e6
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-03 17:16:56 +0000

    texi2html: secure url(s)

--- Formula/texi2html.rb
+++ Formula/texi2html.rb
@@ -1,6 +1,6 @@
 class Texi2html < Formula
   desc "Convert TeXinfo files to HTML"
-  homepage "http://www.nongnu.org/texi2html/"
+  homepage "https://www.nongnu.org/texi2html/"
   url "https://download.savannah.gnu.org/releases/texi2html/texi2html-5.0.tar.gz"
   sha256 "e60edd2a9b8399ca615c6e81e06fa61946ba2f2406c76cd63eb829c91d3a3d7d"
 
