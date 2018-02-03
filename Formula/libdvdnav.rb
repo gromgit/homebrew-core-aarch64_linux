1726cd9e6ab411bdadbe287891a4b8d1babc7188
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-03 17:16:55 +0000

    libdvdnav: secure url(s)

--- Formula/libdvdnav.rb
+++ Formula/libdvdnav.rb
@@ -1,6 +1,6 @@
 class Libdvdnav < Formula
   desc "DVD navigation library"
-  homepage "http://dvdnav.mplayerhq.hu/"
+  homepage "https://www.videolan.org/developers/libdvdnav.html"
   url "https://download.videolan.org/pub/videolan/libdvdnav/6.0.0/libdvdnav-6.0.0.tar.bz2"
   sha256 "f0a2711b08a021759792f8eb14bb82ff8a3c929bf88c33b64ffcddaa27935618"
 
