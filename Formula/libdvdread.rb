8df47194cc0cf15789cef9d5ab10c8ab58f0833c
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-03 17:16:55 +0000

    libdvdread: secure url(s)

--- Formula/libdvdread.rb
+++ Formula/libdvdread.rb
@@ -1,6 +1,6 @@
 class Libdvdread < Formula
   desc "C library for reading DVD-video images"
-  homepage "http://dvdnav.mplayerhq.hu/"
+  homepage "https://www.videolan.org/developers/libdvdnav.html"
   url "https://download.videolan.org/pub/videolan/libdvdread/6.0.0/libdvdread-6.0.0.tar.bz2"
   sha256 "b33b1953b4860545b75f6efc06e01d9849e2ea4f797652263b0b4af6dd10f935"
 
