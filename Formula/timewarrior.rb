798096e37e6b9770dd79c0315835140cbefb0291
Federico Hernandez <f@ederi.co>
2018-02-04 09:47:00 +0100

    timewarrior 1.1.1
    Closes #23660.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/timewarrior.rb
+++ Formula/timewarrior.rb
@@ -1,8 +1,8 @@
 class Timewarrior < Formula
   desc "Command-line time tracking application"
   homepage "https://taskwarrior.org/docs/timewarrior/"
-  url "https://taskwarrior.org/download/timew-1.0.0.tar.gz"
-  sha256 "ac027910e1e8365bdd218a8b42389b26d017d38d3c96516c408db6d5a44e0bb5"
+  url "https://taskwarrior.org/download/timew-1.1.1.tar.gz"
+  sha256 "1f7d9a62e55fc5a3126433654ccb1fd7d2d135f06f05697f871897c9db77ccc9"
   head "https://git.tasktools.org/scm/tm/timew.git"
 
   bottle do
