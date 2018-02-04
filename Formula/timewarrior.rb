47553944573c81600e80fdc477ff4a0c7955cc16
Federico Hernandez <recollir@users.noreply.github.com>
2018-02-04 17:36:19 +0100

    timewarrior: update head url (#23672)
    repository moved to GitHub

--- Formula/timewarrior.rb
+++ Formula/timewarrior.rb
@@ -3,7 +3,7 @@ class Timewarrior < Formula
   homepage "https://taskwarrior.org/docs/timewarrior/"
   url "https://taskwarrior.org/download/timew-1.1.1.tar.gz"
   sha256 "1f7d9a62e55fc5a3126433654ccb1fd7d2d135f06f05697f871897c9db77ccc9"
-  head "https://git.tasktools.org/scm/tm/timew.git"
+  head "https://github.com/GothenburgBitFactory/timewarrior.git"
 
   bottle do
     cellar :any_skip_relocation
