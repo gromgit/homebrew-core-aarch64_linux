35ecc2782b00292ecf32ef4ce088714ddca2cca6
ilovezfs <ilovezfs@icloud.com>
2018-02-05 11:34:15 +0000

    wireguard-tools: add endian.h patch
    Closes #23676.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/wireguard-tools.rb
+++ Formula/wireguard-tools.rb
@@ -1,11 +1,22 @@
 class WireguardTools < Formula
   desc "Tools for the WireGuard secure network tunnel"
   homepage "https://www.wireguard.com/"
-  # Please only update version when the tools have been modified/updated,
-  # since the Linux module aspect isn't of utility for us.
-  url "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-0.0.20180202.tar.xz"
-  sha256 "ee3415b482265ad9e8721aa746aaffdf311058a2d1a4d80e7b6d11bbbf71c722"
   head "https://git.zx2c4.com/WireGuard", :using => :git
+
+  stable do
+    # Please only update version when the tools have been modified/updated,
+    # since the Linux module aspect isn't of utility for us.
+    url "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-0.0.20180202.tar.xz"
+    sha256 "ee3415b482265ad9e8721aa746aaffdf311058a2d1a4d80e7b6d11bbbf71c722"
+
+    # Fix "fatal error: 'endian.h' file not found"
+    # Equivalent to upstream commit from 5 Feb 2018 "tools: endian.h is not portable"
+    # See https://git.zx2c4.com/WireGuard/patch/?id=d99954e0376d50c31f052cd6455e8665d9d9dd66
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/4e4ca86/wireguard-tools/endian.patch"
+      sha256 "68d77d13a7fc748f02e4a78eb405599b04d1b99dcbee8af06cf8eb8f0ba9c908"
+    end
+  end
 
   bottle do
     cellar :any_skip_relocation
