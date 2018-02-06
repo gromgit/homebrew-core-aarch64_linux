e18eadd433549c5facc9daa90574fd08a00759bc
Sam Jones <github@samjones.io>
2018-02-05 20:58:11 -0500

    libbitcoin-network 3.5.0 (new formula)

---
+++ Formula/libbitcoin-network.rb
@@ -0,0 +1,39 @@
+class LibbitcoinNetwork < Formula
+  desc "Bitcoin P2P Network Library"
+  homepage "https://github.com/libbitcoin/libbitcoin-network"
+  url "https://github.com/libbitcoin/libbitcoin-network/archive/v3.5.0.tar.gz"
+  sha256 "e065bd95f64ad5d7b0f882e8759f6b0f81a5fb08f7e971d80f3592a1b5aa8db4"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libbitcoin"
+
+  def install
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["libbitcoin"].opt_libexec/"lib/pkgconfig"
+
+    system "./autogen.sh"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <bitcoin/network.hpp>
+      int main() {
+        const bc::network::settings configuration;
+        bc::network::p2p network(configuration);
+        assert(network.top_block().height() == 0);
+        assert(network.top_block().hash() == bc::null_hash);
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "test.cpp",
+                    "-lbitcoin", "-lbitcoin-network", "-lboost_system",
+                    "-o", "test"
+    system "./test"
+  end
+end
