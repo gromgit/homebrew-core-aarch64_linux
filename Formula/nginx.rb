8e70ac0569b6c337c829fe34770c5b18bd12ac62
ilovezfs <ilovezfs@icloud.com>
2018-02-06 08:17:21 -0800

    nginx 1.13.8
    Switch to the mainline branch for the formula's stable spec. Upstream
    says, "We generally recommend using the mainline branch."
    
    See https://www.nginx.com/blog/nginx-1-12-1-13-released/
    
    Also, always enable debug, http_dav_module, and http_gunzip_module,
    rather than having them be optional, and enable the following, which add
    no dependencies:
    
    - http_addition_module
    - http_auth_request_module
    - http_degradation_module
    - http_flv_module
    - http_mp4_module
    - http_random_index_module
    - http_realip_module
    - http_secure_link_module
    - http_slice_module
    - http_ssl_module
    - http_stub_status_module
    - http_sub_module
    - ipv6
    - mail
    - mail_ssl_module
    - pcre
    - pcre-jit
    - stream
    - stream_realip_module
    - stream_ssl_module
    - stream_ssl_preread_module
    
    Closes #23754.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/nginx.rb
+++ Formula/nginx.rb
@@ -1,9 +1,8 @@
 class Nginx < Formula
   desc "HTTP(S) server and reverse proxy, and IMAP/POP3 proxy server"
   homepage "https://nginx.org/"
-  url "https://nginx.org/download/nginx-1.12.2.tar.gz"
-  sha256 "305f379da1d5fb5aefa79e45c829852ca6983c7cd2a79328f8e084a324cf0416"
-  revision 1
+  url "https://nginx.org/download/nginx-1.13.8.tar.gz"
+  sha256 "8410b6c31ff59a763abf7e5a5316e7629f5a5033c95a3a0ebde727f9ec8464c5"
   head "https://hg.nginx.org/nginx/", :using => :hg
 
   bottle do
@@ -12,18 +11,7 @@ class Nginx < Formula
     sha256 "4e9d4c1ce74bc3b3fea4d90ba6c7c73c6d0457eff26da37163f9393dfc027ac0" => :el_capitan
   end
 
-  devel do
-    url "https://nginx.org/download/nginx-1.13.8.tar.gz"
-    sha256 "8410b6c31ff59a763abf7e5a5316e7629f5a5033c95a3a0ebde727f9ec8464c5"
-  end
-
-  # Before submitting more options to this formula please check they aren't
-  # already in Homebrew/homebrew-nginx/nginx-full:
-  # https://github.com/Homebrew/homebrew-nginx/blob/master/Formula/nginx-full.rb
   option "with-passenger", "Compile with support for Phusion Passenger module"
-  option "with-webdav", "Compile with support for WebDAV module"
-  option "with-debug", "Compile with support for debug log"
-  option "with-gunzip", "Compile with support for gunzip module"
 
   depends_on "openssl" # don't switch to 1.1 until passenger is switched, too
   depends_on "pcre"
@@ -44,8 +32,6 @@ class Nginx < Formula
 
     args = %W[
       --prefix=#{prefix}
-      --with-http_ssl_module
-      --with-pcre
       --sbin-path=#{bin}/nginx
       --with-cc-opt=#{cc_opt}
       --with-ld-opt=#{ld_opt}
@@ -59,18 +45,38 @@ class Nginx < Formula
       --http-scgi-temp-path=#{var}/run/nginx/scgi_temp
       --http-log-path=#{var}/log/nginx/access.log
       --error-log-path=#{var}/log/nginx/error.log
+      --with-debug
+      --with-http_addition_module
+      --with-http_auth_request_module
+      --with-http_dav_module
+      --with-http_degradation_module
+      --with-http_flv_module
+      --with-http_gunzip_module
       --with-http_gzip_static_module
+      --with-http_mp4_module
+      --with-http_random_index_module
+      --with-http_realip_module
+      --with-http_secure_link_module
+      --with-http_slice_module
+      --with-http_ssl_module
+      --with-http_stub_status_module
+      --with-http_sub_module
       --with-http_v2_module
+      --with-ipv6
+      --with-mail
+      --with-mail_ssl_module
+      --with-pcre
+      --with-pcre-jit
+      --with-stream
+      --with-stream_realip_module
+      --with-stream_ssl_module
+      --with-stream_ssl_preread_module
     ]
 
     if build.with? "passenger"
       nginx_ext = `#{Formula["passenger"].opt_bin}/passenger-config --nginx-addon-dir`.chomp
       args << "--add-module=#{nginx_ext}"
     end
-
-    args << "--with-http_dav_module" if build.with? "webdav"
-    args << "--with-debug" if build.with? "debug"
-    args << "--with-http_gunzip_module" if build.with? "gunzip"
 
     if build.head?
       system "./auto/configure", *args
