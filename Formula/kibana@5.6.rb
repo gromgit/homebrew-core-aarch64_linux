c9cc624da0cd66ce0a550d31b9ae260db54c218f
Tyler Langlois <tjl@byu.net>
2018-02-06 11:21:40 -0700

    kibana@5.6 5.6.7
    Closes #23757.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/kibana@5.6.rb
+++ Formula/kibana@5.6.rb
@@ -4,8 +4,8 @@ class KibanaAT56 < Formula
   desc "Analytics and search dashboard for Elasticsearch"
   homepage "https://www.elastic.co/products/kibana"
   url "https://github.com/elastic/kibana.git",
-      :tag => "v5.6.5",
-      :revision => "80f98787eb860a4e7ef9b6500cf84b65e331d1fc"
+      :tag => "v5.6.7",
+      :revision => "8221b17926966fd1e433367d6f725a69f321d5ad"
   head "https://github.com/elastic/kibana.git"
 
   bottle do
