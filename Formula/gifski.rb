59da6c20d1134832663251fb3c433ae05f1083fb
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-06 04:46:08 +0000

    gifski: update 0.8.0 bottle.

--- Formula/gifski.rb
+++ Formula/gifski.rb
@@ -5,9 +5,9 @@ class Gifski < Formula
   sha256 "f3cab822dd41de17ba4fdc3b8d379b469404573b1a4adcb5f9c9385a7a927760"
 
   bottle do
-    sha256 "1b0508c892d5509e80c9b1851dd0659453a3227638ff3fb2070510248020a819" => :high_sierra
-    sha256 "ee8baa305702999090f015ccec6b1e4fc336427834cf6c7e61505c3f56740354" => :sierra
-    sha256 "f7d95fd146b488fdd2a3a0a90575edbd5a3623222c4ec50e1b3aa7d453123bef" => :el_capitan
+    sha256 "a6d535fc0ac55df2e143c1f27f0ae8411654142094e99b56d569a65dfb5b13ef" => :high_sierra
+    sha256 "06399a4deeb177f4912d5def494e347857d7179410fb9b6c8cd6bd3bde4f60dd" => :sierra
+    sha256 "164fe31349cf7f9415750b2db4e16e813fecb42a8e68b584884dfa11295b05c5" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
