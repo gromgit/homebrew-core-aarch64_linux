ac2ba2b02772708fe648363e4ef9dad891d89ef6
Shaun Jackman <sjackman@gmail.com>
2018-02-06 20:58:10 -0800

    igv: install the executable in bin (#23781)

--- Formula/igv.rb
+++ Formula/igv.rb
@@ -3,6 +3,7 @@ class Igv < Formula
   homepage "https://www.broadinstitute.org/software/igv"
   url "https://data.broadinstitute.org/igv/projects/downloads/2.4/IGV_2.4.8.zip"
   sha256 "18acc8410085308b169c4442274848ed7e285196c8bfd0b43100005996a644b1"
+  revision 1
 
   bottle :unneeded
 
@@ -11,11 +12,11 @@ class Igv < Formula
   def install
     inreplace "igv.sh", /^prefix=.*/, "prefix=#{libexec}"
     libexec.install "igv.sh", Dir["*.jar"]
-    libexec.install_symlink "igv.sh" => "igv"
+    bin.install_symlink libexec/"igv.sh" => "igv"
   end
 
   test do
     (testpath/"script").write "exit"
-    assert_match "Version", `#{libexec}/igv -b script`
+    assert_match "Version", `#{bin}/igv -b script`
   end
 end
