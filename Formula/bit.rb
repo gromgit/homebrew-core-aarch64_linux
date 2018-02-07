0618af26a9f0aa893d8008fb21bcd08f0f0bcf79
ilovezfs <ilovezfs@icloud.com>
2018-02-07 03:24:07 -0800

    bit 0.12.5
    Closes #23785.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/bit.rb
+++ Formula/bit.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Bit < Formula
   desc "Distributed Code Component Manager"
   homepage "https://www.bitsrc.io"
-  url "https://registry.npmjs.org/bit-bin/-/bit-bin-0.12.4.tgz"
-  sha256 "2c5467c2ade676e451be0b996a7e02d3e6e7d0402a732159aa3371007429f610"
+  url "https://registry.npmjs.org/bit-bin/-/bit-bin-0.12.5.tgz"
+  sha256 "24736260d77f506a98cfcb45a746456a68c94b1c5d67c60d33c100909651d1c6"
   head "https://github.com/teambit/bit.git"
 
   bottle do
