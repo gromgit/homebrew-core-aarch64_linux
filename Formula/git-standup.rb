68c42822aefa6a7de926f7c23e9e68ff82abad0c
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-08 08:09:36 +0000

    git-standup: update 2.1.9 bottle.

--- Formula/git-standup.rb
+++ Formula/git-standup.rb
@@ -7,11 +7,9 @@ class GitStandup < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "51a88b45f96516d5950e1abc797504198817b7c12c7e3a50fc1f454b1d831630" => :high_sierra
-    sha256 "15ba95a266e75f91c131ac4ba97c8c1f2b8eee5f84e6bf3f8ae4b6c31d4e0046" => :sierra
-    sha256 "8da88a1d4e590d8e2ec6bef3f34c2f88817c89b1473cde291b8ee6c79be6cdef" => :el_capitan
-    sha256 "ab3b98ecf47de06d1d5093257bf63675495a5f57d2e94e1ae53757b933736c02" => :yosemite
-    sha256 "641e5f5e5dbd1761c88603a5c93d40c2b6b4c93d32ffdd1619acf6db8b14f464" => :mavericks
+    sha256 "a0eb22ac35b48f9696da523eb85270a5a0e6652d35c921a5c80b195d3e1f39c2" => :high_sierra
+    sha256 "a0eb22ac35b48f9696da523eb85270a5a0e6652d35c921a5c80b195d3e1f39c2" => :sierra
+    sha256 "a0eb22ac35b48f9696da523eb85270a5a0e6652d35c921a5c80b195d3e1f39c2" => :el_capitan
   end
 
   def install
