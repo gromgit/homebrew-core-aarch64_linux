d7ecfcb25035552e3aebe0e4f29b1a10abda0d08
Jeongseok Lee <jslee02@gmail.com>
2018-02-09 10:27:05 -0800

    dartsim 6.3.0 (new formula)
    Closes #23903.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/dartsim.rb
@@ -0,0 +1,46 @@
+class Dartsim < Formula
+  desc "Dynamic Animation and Robotics Toolkit"
+  homepage "https://dartsim.github.io/"
+  url "https://github.com/dartsim/dart/archive/v6.3.0.tar.gz"
+  sha256 "aa92634c1c97d99966cf16c4a0845792941358c063409fa00c28b4039c961c25"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "assimp"
+  depends_on "boost"
+  depends_on "bullet"
+  depends_on "eigen"
+  depends_on "fcl"
+  depends_on "flann"
+  depends_on "freeglut"
+  depends_on "libccd"
+  depends_on "nlopt"
+  depends_on "ode"
+  depends_on "open-scene-graph"
+  depends_on "tinyxml"
+  depends_on "tinyxml2"
+  depends_on "urdfdom"
+
+  needs :cxx11
+
+  def install
+    ENV.cxx11
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <dart/dart.hpp>
+      int main() {
+        auto world = std::make_shared<dart::simulation::World>();
+        assert(world != nullptr);
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-I#{Formula["eigen"].include}/eigen3",
+                    "-I#{include}", "-L#{lib}", "-ldart",
+                    "-lassimp", "-std=c++11", "-o", "test"
+    system "./test"
+  end
+end
