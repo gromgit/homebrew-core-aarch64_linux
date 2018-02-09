d91920460249bd0a7a3596ec96667a7031683b33
ilovezfs <ilovezfs@icloud.com>
2018-02-08 23:36:10 -0800

    fluent-bit 0.12.13
    Closes #23873.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/fluent-bit.rb
+++ Formula/fluent-bit.rb
@@ -1,8 +1,8 @@
 class FluentBit < Formula
   desc "Data Collector for IoT"
   homepage "https://github.com/fluent/fluent-bit"
-  url "https://github.com/fluent/fluent-bit/archive/v0.12.12.tar.gz"
-  sha256 "3fe945d3202ed582bef1f5d6f37c68f52addd7e5291634708ba930be0cdfc29a"
+  url "https://github.com/fluent/fluent-bit/archive/v0.12.13.tar.gz"
+  sha256 "45089e3377a689eb3608e2f7a019eff3c1f4099bbe8abe4a1ff23ebdcc7397ae"
   head "https://github.com/fluent/fluent-bit.git"
 
   bottle do
