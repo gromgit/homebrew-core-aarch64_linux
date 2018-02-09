9ce44ceb6082ab256ac0603ab02e369b28c34b40
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-09 07:32:42 +0000

    unibilium: update 2.0.0 bottle.

--- Formula/unibilium.rb
+++ Formula/unibilium.rb
@@ -6,10 +6,9 @@ class Unibilium < Formula
 
   bottle do
     cellar :any
-    sha256 "0eabdc993e3e9af53e634963ef18f3735216eaf6da7cb12802643f0b9fb3e7df" => :high_sierra
-    sha256 "53b708223a2e95298390b6313ff4917f10efefc3cf349eb57688938f982ee487" => :sierra
-    sha256 "5c4b97baf010a4d3482f7270e313a274a281df71beb55a8ce69b473611c955ae" => :el_capitan
-    sha256 "a43da6e759273b81f501077deb01633b54591a907e2cef40ba81b1611af5692e" => :yosemite
+    sha256 "5c29d645cd3e0ad950a7054c73b89cb76114b369476eb8bca26587c38571861d" => :high_sierra
+    sha256 "15338d452e5e09e7b8f3bedd6d557d735d06bfbc53204487d11b6c225a04ad71" => :sierra
+    sha256 "d8caea872f5f8ed11503e46fc37f17fafbc8a4c64a5382e2bf9e6d84feda2f98" => :el_capitan
   end
 
   depends_on "libtool" => :build
