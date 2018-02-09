4219b20e5b14c653deafe6b75395d92138adb3a6
ilovezfs <ilovezfs@icloud.com>
2018-02-09 04:09:09 -0800

    embulk 0.9.1 (#23881)
    require Java 8 specifically

--- Formula/embulk.rb
+++ Formula/embulk.rb
@@ -1,18 +1,19 @@
 class Embulk < Formula
   desc "Data transfer between various databases, file formats and services"
   homepage "http://www.embulk.org/"
-  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.8.30.jar"
-  sha256 "dc45a288e4bbbf98c93daca56470facd515b1d464b8ab524588ff5bc8f2e9c86"
+  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.9.1.jar"
+  sha256 "d91c02e258e8babaa0c9c81f435a74dcc8f8695a50de72bf56a3cb8fdcb7efb8"
 
   bottle :unneeded
 
-  depends_on :java => "1.7+"
+  depends_on :java => "1.8"
 
   def install
     # Execute through /bin/bash to be compatible with OS X 10.9.
     libexec.install "embulk-#{version}.jar" => "embulk.jar"
     (bin/"embulk").write <<~EOS
       #!/bin/bash
+      export JAVA_HOME=$(#{Language::Java.java_home_cmd("1.8")})
       exec /bin/bash "#{libexec}/embulk.jar" "$@"
     EOS
   end
