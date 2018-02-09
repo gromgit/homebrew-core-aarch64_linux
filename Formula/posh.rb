30965d6c9069f7078ecb7cf233492d780599b630
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-09 13:03:47 +0000

    posh: add 0.13.1 bottle.

--- Formula/posh.rb
+++ Formula/posh.rb
@@ -4,6 +4,13 @@ class Posh < Formula
   url "https://salsa.debian.org/clint/posh/repository/debian/0.13.1/archive.tar.gz"
   sha256 "c2c10db047294309a109f09e0c76a0cdc33af39563c847b84149b9ac5210f115"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "548e5330c43d793d53a5367a20c8538659c2b01ec89e1e38adb77af84220f56d" => :high_sierra
+    sha256 "b3c765a3f11f264415f1438db4c20e77c4202e8a894d67be5ab60ce2bbfb30a1" => :sierra
+    sha256 "d8018f086dea30a4225641c56255dff05a45d5eea16707822c1e13364b44ecbc" => :el_capitan
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
 
