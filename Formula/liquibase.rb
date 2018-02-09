d0cae0a4653c6e7663dbd350f7cfb5c4f9efeaa5
ilovezfs <ilovezfs@icloud.com>
2018-02-08 23:49:34 -0800

    liquibase 3.5.4 (#23869)

--- Formula/liquibase.rb
+++ Formula/liquibase.rb
@@ -1,8 +1,8 @@
 class Liquibase < Formula
   desc "Library for database change tracking"
   homepage "http://liquibase.org"
-  url "https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.5.3/liquibase-3.5.3-bin.tar.gz"
-  sha256 "fb85d27f1ef8aef0539a74eca3b71692292f2e961171701a38ba82cdbcb39711"
+  url "https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.5.4/liquibase-3.5.4-bin.tar.gz"
+  sha256 "bedb67174f1d9b024b6a323bedcec3c082426122f8f658becfd9594a4e70a324"
 
   bottle :unneeded
 
