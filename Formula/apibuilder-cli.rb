f0640311c160c26d6d30d77e39b784d0696db154
Sean Sullivan <ssullivan@gilt.com>
2018-02-09 17:19:59 -0800

    apibuilder-cli 0.1.19

--- Formula/apibuilder-cli.rb
+++ Formula/apibuilder-cli.rb
@@ -1,8 +1,8 @@
 class ApibuilderCli < Formula
   desc "Command-line interface to generate clients for api builder"
   homepage "https://www.apibuilder.io"
-  url "https://github.com/apicollective/apibuilder-cli/archive/0.1.18.tar.gz"
-  sha256 "6dc56fd401d2e6ce54642d070a268d436e9585487302589bfbadd3859b9ab32f"
+  url "https://github.com/apicollective/apibuilder-cli/archive/0.1.19.tar.gz"
+  sha256 "5351618032aa7498fd49da45c448a2c18b16f55c371f135b2bf3897cf33eb8a1"
 
   bottle :unneeded
 
