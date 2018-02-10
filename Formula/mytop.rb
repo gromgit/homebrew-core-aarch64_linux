371280ce60c531c873350e1ee1151cf7669b7e3f
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-10 03:06:38 +0000

    mytop: update 1.9.1_5 bottle.

--- Formula/mytop.rb
+++ Formula/mytop.rb
@@ -9,9 +9,9 @@ class Mytop < Formula
 
   bottle do
     cellar :any
-    sha256 "bb8d3779656f6133a1f798dfa604550de04d87f2bd270303e0b5d3967667dac7" => :high_sierra
-    sha256 "ef364e628b541df8a91dc6b9c13a637034b5bca24ada20a46654b6ef621bd7e1" => :sierra
-    sha256 "facf8e1d0c4cffd735bfcbe424c67647c5b1b5485bfad2eef9626ec12331e86b" => :el_capitan
+    sha256 "2d7ea53b0ae27d33d5106259b5c96fa878b4c2681af002e91d6432d151024918" => :high_sierra
+    sha256 "89536e5cdee657d9ea8a82d1940854ba52bfa4cf3e161aa148b31511f5268b14" => :sierra
+    sha256 "a7a00e844c2adb87eb4379faf0d3dd60443f9383cb295688ae2dac47051f3483" => :el_capitan
   end
 
   depends_on "mysql"
