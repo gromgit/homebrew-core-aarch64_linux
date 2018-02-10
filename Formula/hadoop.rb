363db5996de1a8e6d2f2780370c3aee3d67fc531
Dominyk Tiller <DomT4@users.noreply.github.com>
2018-02-10 08:39:09 +0000

    hadoop 3.0.0 (#23922)

--- Formula/hadoop.rb
+++ Formula/hadoop.rb
@@ -1,13 +1,13 @@
 class Hadoop < Formula
   desc "Framework for distributed processing of large data sets"
   homepage "https://hadoop.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=hadoop/common/hadoop-2.8.2/hadoop-2.8.2.tar.gz"
-  mirror "https://archive.apache.org/dist/hadoop/common/hadoop-2.8.2/hadoop-2.8.2.tar.gz"
-  sha256 "aea99c7ce8441749d81202bdea431f1024f17ee6e0efb3144226883207cc6292"
+  url "https://www.apache.org/dyn/closer.cgi?path=hadoop/common/hadoop-3.0.0/hadoop-3.0.0.tar.gz"
+  mirror "https://archive.apache.org/dist/hadoop/common/hadoop-3.0.0/hadoop-3.0.0.tar.gz"
+  sha256 "726e28fa7aea71e4587ce91ed3d96c56b15777fc859c09a7438a6d0092e08c74"
 
   bottle :unneeded
 
-  depends_on :java => "1.7+"
+  depends_on :java => "1.8+"
 
   conflicts_with "yarn", :because => "both install `yarn` binaries"
 
@@ -16,28 +16,6 @@ class Hadoop < Formula
     libexec.install %w[bin sbin libexec share etc]
     bin.write_exec_script Dir["#{libexec}/bin/*"]
     sbin.write_exec_script Dir["#{libexec}/sbin/*"]
-    # But don't make rcc visible, it conflicts with Qt
-    (bin/"rcc").unlink
-
-    inreplace "#{libexec}/etc/hadoop/hadoop-env.sh",
-      "export JAVA_HOME=${JAVA_HOME}",
-      "export JAVA_HOME=\"$(/usr/libexec/java_home)\""
-    inreplace "#{libexec}/etc/hadoop/yarn-env.sh",
-      "# export JAVA_HOME=/home/y/libexec/jdk1.6.0/",
-      "export JAVA_HOME=\"$(/usr/libexec/java_home)\""
-    inreplace "#{libexec}/etc/hadoop/mapred-env.sh",
-      "# export JAVA_HOME=/home/y/libexec/jdk1.6.0/",
-      "export JAVA_HOME=\"$(/usr/libexec/java_home)\""
-  end
-
-  def caveats; <<~EOS
-    In Hadoop's config file:
-      #{libexec}/etc/hadoop/hadoop-env.sh,
-      #{libexec}/etc/hadoop/mapred-env.sh and
-      #{libexec}/etc/hadoop/yarn-env.sh
-    $JAVA_HOME has been set to be the output of:
-      /usr/libexec/java_home
-    EOS
   end
 
   test do
