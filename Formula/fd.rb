bfe09777bb35e18d8d9abfa7fe99019cae620a70
ilovezfs <ilovezfs@icloud.com>
2018-02-10 09:03:50 -0800

    fd 6.3.0
    Closes #23964.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/fd.rb
+++ Formula/fd.rb
@@ -1,8 +1,8 @@
 class Fd < Formula
   desc "Simple, fast and user-friendly alternative to find"
   homepage "https://github.com/sharkdp/fd"
-  url "https://github.com/sharkdp/fd/archive/v6.2.0.tar.gz"
-  sha256 "ca9c3995456fcb6f28aafea7d2b6c7ccddef1e98e3f4963aaee4d041ceba0a1d"
+  url "https://github.com/sharkdp/fd/archive/v6.3.0.tar.gz"
+  sha256 "b178ea3018667766540a54703b15f01207f4626bc7162378a86972f2f43f2f60"
   head "https://github.com/sharkdp/fd.git"
 
   bottle do
