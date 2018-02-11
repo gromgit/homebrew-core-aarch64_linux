3cf217d5da4cdd1e67113fcddc0cecb599a5663f
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-11 21:55:45 +0000

    clblast: add 1.3.0 bottle.

--- Formula/clblast.rb
+++ Formula/clblast.rb
@@ -4,6 +4,13 @@ class Clblast < Formula
   url "https://github.com/CNugteren/CLBlast/archive/1.3.0.tar.gz"
   sha256 "fd5e06c261551366b46ccbc7841012cc69b58c74ed641db4f7a157c055d24223"
 
+  bottle do
+    cellar :any
+    sha256 "8254e3e21d6633170743bf46b035a00df6139ce0181ca58613fbea88b44dd1e6" => :high_sierra
+    sha256 "a80742b7dceee1d82f82b038138bf0a66844b11e67861405faa4999140f689d7" => :sierra
+    sha256 "6f1ceb76dd369fb8129c8ec252b2c8a54c95572e6e3d02d8df758296ba47e062" => :el_capitan
+  end
+
   depends_on "cmake" => :build
 
   def install
