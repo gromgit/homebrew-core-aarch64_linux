86f2508130dcc2a869a882d0f5fb621654d8d624
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-11 16:03:04 +0000

    fcl: add 0.5.0 bottle.

--- Formula/fcl.rb
+++ Formula/fcl.rb
@@ -4,6 +4,12 @@ class Fcl < Formula
   url "https://github.com/flexible-collision-library/fcl/archive/0.5.0.tar.gz"
   sha256 "8e6c19720e77024c1fbff5a912d81e8f28004208864607447bc90a31f18fb41a"
 
+  bottle do
+    sha256 "9339278a609d8fbcd24f911eafc78e580c965cd6159229eab12ded553c92d7d3" => :high_sierra
+    sha256 "bb60099d73cf3c322cb1453c735d3305fdc652b068bbd220c25f565ec825b537" => :sierra
+    sha256 "b93f6cf2087e3c9a1dbbca8552ff5b01913f229d3bfb4968490386fb0348f3ad" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "boost"
