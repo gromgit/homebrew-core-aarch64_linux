97214e8bb4d2b9a30d484a26d94c20960fb058bd
ilovezfs <ilovezfs@icloud.com>
2018-02-11 04:09:30 -0800

    storm 1.1.1 (#23994)

--- Formula/storm.rb
+++ Formula/storm.rb
@@ -1,8 +1,8 @@
 class Storm < Formula
   desc "Distributed realtime computation system to process data streams"
   homepage "https://storm.apache.org"
-  url "https://www.apache.org/dyn/closer.cgi?path=storm/apache-storm-1.1.0/apache-storm-1.1.0.tar.gz"
-  sha256 "6f584b45ec7f8d0cfd2fa78deb5de392bece07a09158a948b0ed3016ef689142"
+  url "https://www.apache.org/dyn/closer.cgi?path=storm/apache-storm-1.1.1/apache-storm-1.1.1.tar.gz"
+  sha256 "284b43c2e667ccb600605769e76dfc981140d769a684253f37309f85fea2a951"
 
   bottle :unneeded
 
