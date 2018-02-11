ae521f07de6978ecfc6368eba0db3ecb9e8192c5
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-11 00:50:59 +0000

    console_bridge: add 0.3.2 bottle.

--- Formula/console_bridge.rb
+++ Formula/console_bridge.rb
@@ -4,6 +4,13 @@ class ConsoleBridge < Formula
   url "https://github.com/ros/console_bridge/archive/0.3.2.tar.gz"
   sha256 "fd12e48c672cb9c5d516d90429c4a7ad605859583fc23d98258c3fa7a12d89f4"
 
+  bottle do
+    cellar :any
+    sha256 "dc5ba07f8af3c710d8a86536801d0770fb1755c9b32706b26bfe52b46c65f264" => :high_sierra
+    sha256 "31b0c5dae6dff5f796c3752e636cdf2fbbd4bc16d47288c06809668fde424dd0" => :sierra
+    sha256 "bbc636adfa1c9a7322c9eb9646e228d85b0a9c3802d6c9d46f90f34be8cb268e" => :el_capitan
+  end
+
   depends_on "cmake" => :build
 
   needs :cxx11
