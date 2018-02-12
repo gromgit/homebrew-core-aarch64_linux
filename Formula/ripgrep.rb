c2c15c6da2a6d51ff367fbf15a16b48336f921ae
commitay <commitay@users.noreply.github.com>
2018-02-12 12:05:05 +1000

    ripgrep 0.8.0
    Closes #24015.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/ripgrep.rb
+++ Formula/ripgrep.rb
@@ -1,8 +1,8 @@
 class Ripgrep < Formula
   desc "Search tool like grep and The Silver Searcher"
   homepage "https://github.com/BurntSushi/ripgrep"
-  url "https://github.com/BurntSushi/ripgrep/archive/0.7.1.tar.gz"
-  sha256 "e010693637acebb409f3dba7caf59ef093d1894a33b14015041b8d43547665f5"
+  url "https://github.com/BurntSushi/ripgrep/archive/0.8.0.tar.gz"
+  sha256 "c26391013522dfce3d870aec911fc602425e2eb385b75802b5b44440f4c32e24"
   head "https://github.com/BurntSushi/ripgrep.git"
 
   bottle do
@@ -11,18 +11,19 @@ class Ripgrep < Formula
     sha256 "8e183fb5763d350ae070756d3ff07e852ec624f6294d6657c067165328f7c6a5" => :el_capitan
   end
 
+  depends_on "asciidoc" => :build
   depends_on "rust" => :build
 
   def install
     system "cargo", "build", "--release"
 
     bin.install "target/release/rg"
-    man1.install "doc/rg.1"
 
-    # Completion scripts are generated in the crate's build directory, which
-    # includes a fingerprint hash. Try to locate it first
+    # Completion scripts and manpage are generated in the crate's build
+    # directory, which includes a fingerprint hash. Try to locate it first
     out_dir = Dir["target/release/build/ripgrep-*/out"].first
-    bash_completion.install "#{out_dir}/rg.bash-completion"
+    man1.install "#{out_dir}/rg.1"
+    bash_completion.install "#{out_dir}/rg.bash"
     fish_completion.install "#{out_dir}/rg.fish"
     zsh_completion.install "complete/_rg"
   end
