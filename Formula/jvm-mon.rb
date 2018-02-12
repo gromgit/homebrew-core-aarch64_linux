03ea57325c348f6dcc4201fbc019545d3539093a
Andrew Janke <floss@apjanke.net>
2018-02-12 11:10:02 -0500

    jvm-mon: require Java 8 specifically (#24035)

--- Formula/jvm-mon.rb
+++ Formula/jvm-mon.rb
@@ -3,15 +3,18 @@ class JvmMon < Formula
   homepage "https://github.com/ajermakovics/jvm-mon"
   url "https://github.com/ajermakovics/jvm-mon/releases/download/0.3/jvm-mon-0.3.tar.gz"
   sha256 "9b5dd3d280cb52b6e2a9a491451da2ee41c65c770002adadb61b02aa6690c940"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on :java => "1.8"
 
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install Dir["*"]
-    bin.install_symlink libexec/"bin/jvm-mon"
+
+    (bin/"jvm-mon").write_env_script "#{libexec}/bin/jvm-mon",
+      Language::Java.java_home_env("1.8")
     system "unzip", "-j", libexec/"lib/j2v8_macosx_x86_64-4.6.0.jar", "libj2v8_macosx_x86_64.dylib", "-d", libexec
   end
 
