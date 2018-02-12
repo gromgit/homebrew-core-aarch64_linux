00c8c00db1f14745abcec135e11ca8c2cecd9a66
ilovezfs <ilovezfs@icloud.com>
2018-02-12 08:23:43 -0800

    paket 5.136.0 (#24040)

--- Formula/paket.rb
+++ Formula/paket.rb
@@ -1,8 +1,8 @@
 class Paket < Formula
   desc "Dependency manager for .NET with support for NuGet and Git repositories"
   homepage "https://fsprojects.github.io/Paket/"
-  url "https://github.com/fsprojects/Paket/releases/download/5.135.0/paket.exe"
-  sha256 "0cfc17302191dccbf7053e0798c4256bb1f4b92fec63052feedbde21ff2a3b67"
+  url "https://github.com/fsprojects/Paket/releases/download/5.136.0/paket.exe"
+  sha256 "37d42125975b677783634453b5ff04eab2fe23e219f283221044bcd477685aa6"
 
   bottle :unneeded
 
