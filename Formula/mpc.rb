68e7f2ea829f69e5b51552b9e171df0b13a8de86
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-12 13:14:05 +0000

    mpc: update 0.29 bottle.

--- Formula/mpc.rb
+++ Formula/mpc.rb
@@ -5,11 +5,9 @@ class Mpc < Formula
   sha256 "02f1daec902cb48f8cdaa6fe21c7219f6231b091dddbe437a3a4fb12cb07b9d3"
 
   bottle do
-    cellar :any
-    sha256 "0402296ce3f6423ba0611059d6091151fd682d2508abaa17ba06c7e25276fe22" => :high_sierra
-    sha256 "aba27710d5d28f11e4df66e31baa307064c3ac6760083b0ee6411a57ff4920c1" => :sierra
-    sha256 "c963c018ed6d13b3bd46fd292fa42d29298499b96fc8cb60ae62accb292d6b7a" => :el_capitan
-    sha256 "10dbe56eb9a55d841001a9b7b553b80cd7287404b97f8526e2343b66cf6510e6" => :yosemite
+    sha256 "baffb4e58d39af2f18d3def335cb231826e1a94fafa4f31367e7372dbbfe769f" => :high_sierra
+    sha256 "400ac144b7f8ca93a2cf2489e0b25031a0086b95e283c6537b037acf008e80cf" => :sierra
+    sha256 "0ed8bc8a4a93e8df393e23d19229719ff057ebd3a6e27ff42324451753c399d0" => :el_capitan
   end
 
   depends_on "meson" => :build
