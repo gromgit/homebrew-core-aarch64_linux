78e912641ac6bd09b11eda36997637a3b4c48dac
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-12 13:14:05 +0000

    libmpdclient: update 2.14 bottle.

--- Formula/libmpdclient.rb
+++ Formula/libmpdclient.rb
@@ -7,10 +7,9 @@ class Libmpdclient < Formula
 
   bottle do
     cellar :any
-    sha256 "af530e739905c173c9327f560c7a04ba39f8c5a3476d1c2d0516be62dc967789" => :high_sierra
-    sha256 "30f5b095ddc086dbb8d15b82f0184f83308c7bd5d020facacee2b27c54757add" => :sierra
-    sha256 "56530667e499edce59960bf7e7ef3fe3cda08b96c9a8af1f6e056a98ae86c0fc" => :el_capitan
-    sha256 "a4eab9eb2e1e33ef07412d5edb202983245f2a5b285771d352eb87c5fa162f4b" => :yosemite
+    sha256 "03ad207b62c19b8b7e8368f28245cb6c939e15babb68fab3d6958ebb14e2f6a4" => :high_sierra
+    sha256 "5fc405af3eea66abec93720732c65cb6220c610bda522119a21b0d14f68e3baf" => :sierra
+    sha256 "b21a61625915e0e94fe52e16ea64a142588f621cb1a56e7cb931bcab1502af2f" => :el_capitan
   end
 
   depends_on "doxygen" => :build
