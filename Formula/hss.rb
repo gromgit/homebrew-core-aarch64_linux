0d8cf35aa2fdec1d4c068329f5f94e9b56e84736
dudongcheng <dongcheng.du@gmail.com>
2018-02-12 13:11:19 +0800

    hss 1.6 (new formula)
    Closes #24016.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/hss.rb
@@ -0,0 +1,36 @@
+class Hss < Formula
+  desc "Interactive parallel SSH client"
+  homepage "https://github.com/six-ddc/hss"
+  url "https://github.com/six-ddc/hss/archive/1.6.tar.gz"
+  sha256 "8516f3e24c9908f9c7ac02ee5247ce78f2a344e7fcca8a14081a92949db70049"
+
+  depends_on "readline"
+
+  def install
+    system "make"
+    system "make", "install", "INSTALL_BIN=#{bin}"
+  end
+
+  test do
+    require "socket"
+    begin
+      server = TCPServer.new(0)
+      port = server.addr[1]
+      accept_pid = fork do
+        msg = server.accept.gets
+        assert_match "SSH", msg
+      end
+      hss_read, hss_write = IO.pipe
+      hss_pid = fork do
+        exec "#{bin}/hss", "-H", "-p #{port} 127.0.0.1", "-u", "root", "true",
+          :out => hss_write
+      end
+      server.close
+      msg = hss_read.gets
+      assert_match "Connection closed by remote host", msg
+    ensure
+      Process.kill("TERM", accept_pid)
+      Process.kill("TERM", hss_pid)
+    end
+  end
+end
