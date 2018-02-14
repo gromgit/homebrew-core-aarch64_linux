34a2bc1bc2ac2f8e384134972487ed25e4348e19
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-14 19:18:13 +0000

    futhark: add 0.3.1 bottle.

--- Formula/futhark.rb
+++ Formula/futhark.rb
@@ -4,6 +4,13 @@ class Futhark < Formula
   url "https://github.com/diku-dk/futhark/archive/v0.3.1.tar.gz"
   sha256 "a3e8ab25dc53160da5e4bef58fe91107909ade6f93523227a935c5330d3ea8f7"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ea02afdd567ef66b13788b8ded07c12e7ee3d17e4289de694d293e2dd3ac16d3" => :high_sierra
+    sha256 "58fb132a709bfdb3d4aba3ca8f4bf8acc35f42847e1a6859b499f2e6c58dead5" => :sierra
+    sha256 "be8f8db352fdd341b9dc29dd0a11d15c6d12230f076852e854903f2643f2727e" => :el_capitan
+  end
+
   depends_on "haskell-stack" => :build
   depends_on "sphinx-doc" => :build
 
