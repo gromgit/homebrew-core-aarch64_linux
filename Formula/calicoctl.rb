66e88dd7d13d501d2de860ca43577a5bc045d2d1
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-14 15:55:14 +0000

    calicoctl: add 1.6.1 bottle.

--- Formula/calicoctl.rb
+++ Formula/calicoctl.rb
@@ -5,6 +5,13 @@ class Calicoctl < Formula
       :tag => "v1.6.1",
       :revision => "1724e011ac0e608190d7d5512ab8028bcd18ae7b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d2e732eab1aee4a1f90f3600435e39c8a63c246bae3e12eb1b806a64c8fa67c2" => :high_sierra
+    sha256 "878fd5693b2925e63a43e29a75c3ba548dc9dea979949dc9c4504c2463d7abe4" => :sierra
+    sha256 "eaca847212bc19388f185371851f3c90b928c3be521d9abdba1c141bdebc0502" => :el_capitan
+  end
+
   depends_on "glide" => :build
   depends_on "go" => :build
 
