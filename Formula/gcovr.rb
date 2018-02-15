449d380cf0b202a0fe7c072e8b6865d6a42037e8
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-15 18:16:25 +0000

    gcovr: secure url(s)

--- Formula/gcovr.rb
+++ Formula/gcovr.rb
@@ -1,6 +1,6 @@
 class Gcovr < Formula
   desc "Reports from gcov test coverage program"
-  homepage "http://gcovr.com/"
+  homepage "https://gcovr.com/"
   url "https://github.com/gcovr/gcovr/archive/3.4.tar.gz"
   sha256 "1c52a71f245adfe1b45e30fbe5015337fe66546f17f40038b3969b7b42acceed"
   head "https://github.com/gcovr/gcovr.git"
