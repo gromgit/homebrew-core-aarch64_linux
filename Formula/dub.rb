ef5c3e86f2692aed99c069a003717b7e1699a5c6
John Colvin <john.loughran.colvin@gmail.com>
2018-02-15 11:27:28 +0000

    dub 1.7.2
    Closes #24160.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/dub.rb
+++ Formula/dub.rb
@@ -1,8 +1,8 @@
 class Dub < Formula
   desc "Build tool for D projects"
   homepage "https://code.dlang.org/getting_started"
-  url "https://github.com/dlang/dub/archive/v1.7.1.tar.gz"
-  sha256 "baa8c533f59d83f74e89c06f5ec7e52daf3becb227c7177a9eeab7159ba86dbc"
+  url "https://github.com/dlang/dub/archive/v1.7.2.tar.gz"
+  sha256 "1801e2d8f0069ad68ef7c63ed44dc3923a8f7617fd3c9db5e5e748846b0d79cb"
   version_scheme 1
 
   head "https://github.com/dlang/dub.git"
@@ -11,11 +11,6 @@ class Dub < Formula
     sha256 "1ca19d9eb7c871a9e53e3d09c2d2bad8da41b20e4cb6f91041d034300840a6b6" => :high_sierra
     sha256 "d6df5aca7ffc58b4820dbd9c896d89aeeaa86624c348fc6af9b7018393c7b368" => :sierra
     sha256 "7a80d992855844a0c7ee7366f0065a0aac0a89fdd453e113903a7126a0363f2e" => :el_capitan
-  end
-
-  devel do
-    url "https://github.com/dlang/dub/archive/v1.7.2-beta.1.tar.gz"
-    sha256 "5f1f9a4f59bee5721b7e6f49a87c49732908743f0c0f30b31a746fca26b16489"
   end
 
   depends_on "pkg-config" => [:recommended, :run]
