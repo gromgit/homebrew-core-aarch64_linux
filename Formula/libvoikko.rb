b6b4054c0a039e9ce10d0a966cd8f560c0ac4937
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-15 18:16:26 +0000

    libvoikko: secure url(s)

--- Formula/libvoikko.rb
+++ Formula/libvoikko.rb
@@ -1,6 +1,6 @@
 class Libvoikko < Formula
   desc "Linguistic software and Finnish dictionary"
-  homepage "http://voikko.puimula.org/"
+  homepage "https://voikko.puimula.org/"
   url "https://www.puimula.org/voikko-sources/libvoikko/libvoikko-4.1.1.tar.gz"
   sha256 "bb179360abdb92f9459f4d4090e56c9d9d8a3ebe9161a4c4bcd19971d59f9124"
   revision 1
