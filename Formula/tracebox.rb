033292dfb17cdce0cf891552706e597714c8d4cf
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-15 18:16:27 +0000

    tracebox: secure url(s)

--- Formula/tracebox.rb
+++ Formula/tracebox.rb
@@ -1,6 +1,6 @@
 class Tracebox < Formula
   desc "Middlebox detection tool"
-  homepage "http://www.tracebox.org/"
+  homepage "https://www.tracebox.org/"
   url "https://github.com/tracebox/tracebox.git",
       :tag => "v0.4.4",
       :revision => "4fc12b2e330e52d340ecd64b3a33dbc34c160390"
