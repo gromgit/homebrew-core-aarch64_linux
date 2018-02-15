c5db59ae8e683d64cac72265caa81326e97c1507
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-15 18:16:24 +0000

    boxes: secure url(s)

--- Formula/boxes.rb
+++ Formula/boxes.rb
@@ -1,6 +1,6 @@
 class Boxes < Formula
   desc "Draw boxes around text"
-  homepage "http://boxes.thomasjensen.com/"
+  homepage "https://boxes.thomasjensen.com/"
   url "https://github.com/ascii-boxes/boxes/archive/v1.2.tar.gz"
   sha256 "ba237f6d4936bdace133d5f370674fd4c63bf0d767999a104bada6460c5d1913"
   head "https://github.com/ascii-boxes/boxes.git"
