bc04090432a43d9cfbc3a802bd2e14a026b1bf80
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-15 18:16:25 +0000

    gdub: secure url(s)

--- Formula/gdub.rb
+++ Formula/gdub.rb
@@ -1,6 +1,6 @@
 class Gdub < Formula
   desc "Gradlew/gradle wrapper"
-  homepage "http://www.gdub.rocks"
+  homepage "https://www.gdub.rocks/"
   url "https://github.com/dougborg/gdub/archive/v0.1.0.tar.gz"
   sha256 "ddf2572cc67b8df3293b1707720c6ef09d6caf73227fa869a73b16239df959c3"
 
