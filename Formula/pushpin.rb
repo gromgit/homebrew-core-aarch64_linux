21bdc04009f2912e6104fb0aeb00a69616c7f5d2
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-15 18:16:26 +0000

    pushpin: secure url(s)

--- Formula/pushpin.rb
+++ Formula/pushpin.rb
@@ -1,6 +1,6 @@
 class Pushpin < Formula
   desc "Reverse proxy for realtime web services"
-  homepage "http://pushpin.org"
+  homepage "https://pushpin.org/"
   url "https://dl.bintray.com/fanout/source/pushpin-1.17.2.tar.bz2"
   sha256 "f30c12b147d39c52617403b40f11737a14020dc93f223faa7214d73db8af1d77"
   head "https://github.com/fanout/pushpin.git"
