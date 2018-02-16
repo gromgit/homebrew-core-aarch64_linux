38d6e8c2c70545c8439608c612ec51aa75f9d583
ilovezfs <ilovezfs@icloud.com>
2018-02-15 20:51:46 -0800

    kallisto 0.44.0
    depend on autoconf and automake at build time
    
    Closes #24190.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/kallisto.rb
+++ Formula/kallisto.rb
@@ -1,9 +1,8 @@
 class Kallisto < Formula
   desc "Quantify abundances of transcripts from RNA-Seq data"
   homepage "https://pachterlab.github.io/kallisto/"
-  url "https://github.com/pachterlab/kallisto/archive/v0.43.1.tar.gz"
-  sha256 "2164938c2c61c04e338c4c132cf749f56d39e6f0b4c517121bca1fbc218e430e"
-  revision 1
+  url "https://github.com/pachterlab/kallisto/archive/v0.44.0.tar.gz"
+  sha256 "35a81201a56f4557697e6fe693dc6b701bbbd0a7b2b6e1c6c845ef816d67ca29"
 
   bottle do
     cellar :any
@@ -12,11 +11,24 @@ class Kallisto < Formula
     sha256 "9174135661c12414338e2a5280ee31951f97e8d55bd30ba423099e9b872da42e" => :el_capitan
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "cmake" => :build
   depends_on "hdf5"
 
   def install
+    # Upstream issue 15 Feb 2018 "cmake does not run autoreconf for htslib"
+    # https://github.com/pachterlab/kallisto/issues/159
+    system "autoreconf", "-fiv", "ext/htslib"
+
     system "cmake", ".", *std_cmake_args
+
+    # Upstream issue 15 Feb 2018 "parallelized build failure"
+    # https://github.com/pachterlab/kallisto/issues/160
+    # Upstream issue 15 Feb 2018 "cannot use system htslib"
+    # https://github.com/pachterlab/kallisto/issues/161
+    system "make", "htslib"
+
     system "make", "install"
   end
 
