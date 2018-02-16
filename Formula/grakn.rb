9970f6adf701c1500eed19f54d7839afaa9f02b2
Ganeshwara Herawan Hananda <soulmagic13@gmail.com>
2018-02-16 13:23:47 +0000

    grakn 1.1.0 (#24206)

--- Formula/grakn.rb
+++ Formula/grakn.rb
@@ -1,8 +1,8 @@
 class Grakn < Formula
   desc "The distributed hyper-relational database for knowledge engineering"
   homepage "https://grakn.ai"
-  url "https://github.com/graknlabs/grakn/releases/download/v1.0.0/grakn-dist-1.0.0.tar.gz"
-  sha256 "bd616a01fe050898b2687985fac5ce2c834402d33de871112f894a726692a916"
+  url "https://github.com/graknlabs/grakn/releases/download/v1.1.0/grakn-dist-1.1.0.tar.gz"
+  sha256 "6f741bfe04e1ee3b72fd0e107298aec609958ae51c6bfd1040f3aba8b05b4a94"
 
   bottle :unneeded
 
