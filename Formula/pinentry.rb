158034ea4cd520a38da3fc901b0f9e52924c6ec4
Riley Avron <riley.avron@gmail.com>
2018-02-17 00:03:51 -0800

    pinentry: build pinentry-tty
    Closes #24244.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/pinentry.rb
+++ Formula/pinentry.rb
@@ -4,6 +4,7 @@ class Pinentry < Formula
   url "https://www.gnupg.org/ftp/gcrypt/pinentry/pinentry-1.1.0.tar.bz2"
   mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/pinentry/pinentry-1.1.0.tar.bz2"
   sha256 "68076686fa724a290ea49cdf0d1c0c1500907d1b759a3bcbfbec0293e8f56570"
+  revision 1
 
   bottle do
     cellar :any
@@ -27,6 +28,7 @@ class Pinentry < Formula
       --disable-pinentry-gnome3
       --disable-pinentry-tqt
       --disable-pinentry-fltk
+      --enable-pinentry-tty
     ]
 
     args << "--disable-pinentry-gtk2" if build.without? "gtk+"
@@ -37,5 +39,6 @@ class Pinentry < Formula
 
   test do
     system "#{bin}/pinentry", "--version"
+    system "#{bin}/pinentry-tty", "--version"
   end
 end
