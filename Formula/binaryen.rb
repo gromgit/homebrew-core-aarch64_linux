20a66afbbdb03e812cf908b6d8774973523d3827
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-17 05:45:19 +0000

    binaryen: update 44 bottle.

--- Formula/binaryen.rb
+++ Formula/binaryen.rb
@@ -8,9 +8,9 @@ class Binaryen < Formula
 
   bottle do
     cellar :any
-    sha256 "56c0ad9fc87d9d6f836b53c6f376578c84603366a1d6b55f07982b36bcdc6bb5" => :high_sierra
-    sha256 "02c4f8554f8956d31f22cba45de32af4c0d3ec489f84cc1e740b59109447373c" => :sierra
-    sha256 "14b688abf43679334a7685350b93b9b90740fd97f8f5a21eaf96d016446cb804" => :el_capitan
+    sha256 "bf38bdad20ebad57ba63999bd2b6044fe81b3cc5b5aea83943cd15d1593435d5" => :high_sierra
+    sha256 "10759f18765ef48efd18a705d5bf718dbe2661caf3c56b610fd677d4418e8c1c" => :sierra
+    sha256 "edaa59c66b214dfc80145692257720fe76cc1e5051e65185a3755b7fee29193b" => :el_capitan
   end
 
   depends_on "cmake" => :build
