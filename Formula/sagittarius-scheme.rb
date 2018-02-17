cb742a734253255e4a4058e22d67282921f5cc3d
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-17 10:01:09 +0000

    sagittarius-scheme: update 0.9.0 bottle.

--- Formula/sagittarius-scheme.rb
+++ Formula/sagittarius-scheme.rb
@@ -6,9 +6,9 @@ class SagittariusScheme < Formula
 
   bottle do
     cellar :any
-    sha256 "a7a531aeb202a2dcba74bc04250ff0b7635eef5f41e65e69f4063dc7a4a7b139" => :high_sierra
-    sha256 "0fe1f11fdf6f149cb5cd9ba39d27a9f26fcce0bfd4226eec081c9d8f7d00cc82" => :sierra
-    sha256 "e2a7ad03b3100e50829fb6e6f26da1badc40940d4bc6358827cc9dcf1317dea0" => :el_capitan
+    sha256 "8e9a19e387b9f08cc38d3fac73f7cfd34bc62ae5415748afc87443ee0a634d5e" => :high_sierra
+    sha256 "9de62f9f0364afdc741ebea1af3fd82b1a2414f3f01235b9fa62d2106b49bb6e" => :sierra
+    sha256 "85cf1b76c1108c08469d2d695b8bef6f2c2bfda4125553d251c1fd26042f4c9f" => :el_capitan
   end
 
   depends_on "cmake" => :build
