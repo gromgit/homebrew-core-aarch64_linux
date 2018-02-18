47265abeb6be29677c00c07776f4a80aba1df328
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-18 16:56:38 +0000

    liblwgeom: secure url(s)

--- Formula/liblwgeom.rb
+++ Formula/liblwgeom.rb
@@ -1,7 +1,7 @@
 class Liblwgeom < Formula
   desc "Allows SpatiaLite to support ST_MakeValid() like PostGIS"
-  homepage "https://postgis.net"
-  url "http://download.osgeo.org/postgis/source/postgis-2.4.3.tar.gz"
+  homepage "https://postgis.net/"
+  url "https://download.osgeo.org/postgis/source/postgis-2.4.3.tar.gz"
   sha256 "ea5374c5db6b645ba5628ddcb08f71d3b3d90a464d366b4e1d20d5a268bde4b9"
 
   bottle do
