a7dc12aeec6106ca7e8bfb86633f31afcfefc27e
ilovezfs <ilovezfs@icloud.com>
2018-02-18 07:07:54 -0800

    goenv 1.11.0 (#24273)

--- Formula/goenv.rb
+++ Formula/goenv.rb
@@ -1,8 +1,8 @@
 class Goenv < Formula
   desc "Go version management"
   homepage "https://github.com/syndbg/goenv"
-  url "https://github.com/syndbg/goenv/archive/1.10.0.tar.gz"
-  sha256 "afb57c55822013dbdc9ac4431c9476edcd6be69c142292c9aa5fabe0f9210248"
+  url "https://github.com/syndbg/goenv/archive/1.11.0.tar.gz"
+  sha256 "c94ba06c3a5b853c5cb9b7cc72153bc4d0c8bc3751cd0af9f66eeacc5ecc3a23"
   version_scheme 1
   head "https://github.com/syndbg/goenv.git"
 
