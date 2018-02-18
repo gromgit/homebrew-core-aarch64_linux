5566503684553fe5dbd023bcdb8629390576e608
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-18 16:56:38 +0000

    shapelib: secure url(s)

--- Formula/shapelib.rb
+++ Formula/shapelib.rb
@@ -1,7 +1,7 @@
 class Shapelib < Formula
   desc "Library for reading and writing ArcView Shapefiles"
   homepage "http://shapelib.maptools.org/"
-  url "http://download.osgeo.org/shapelib/shapelib-1.4.1.tar.gz"
+  url "https://download.osgeo.org/shapelib/shapelib-1.4.1.tar.gz"
   sha256 "a4c94817365761a3a4c21bb3ca1c680a6bdfd3edd61df9fdd291d3e7645923b3"
 
   bottle do
