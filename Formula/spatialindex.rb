5ae77ce76ff5ea1fd82cdd8b3425c67da4895434
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-18 16:56:39 +0000

    spatialindex: secure url(s)

--- Formula/spatialindex.rb
+++ Formula/spatialindex.rb
@@ -1,7 +1,7 @@
 class Spatialindex < Formula
   desc "General framework for developing spatial indices"
-  homepage "https://libspatialindex.github.io"
-  url "http://download.osgeo.org/libspatialindex/spatialindex-src-1.8.5.tar.gz"
+  homepage "https://libspatialindex.github.io/"
+  url "https://download.osgeo.org/libspatialindex/spatialindex-src-1.8.5.tar.gz"
   sha256 "7caa46a2cb9b40960f7bc82c3de60fa14f8f3e000b02561b36cbf2cfe6a9bfef"
 
   bottle do
