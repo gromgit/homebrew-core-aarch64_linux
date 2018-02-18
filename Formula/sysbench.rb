11bbf7372ba6e816d18a959ab66925a6331bf9cf
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-18 07:07:50 +0000

    sysbench: update 1.0.13 bottle.

--- Formula/sysbench.rb
+++ Formula/sysbench.rb
@@ -5,9 +5,9 @@ class Sysbench < Formula
   sha256 "32082772dbb1a3c0ab7899b6678817f1386f2e9ffdd0da005ca59f904dafc12c"
 
   bottle do
-    sha256 "76fe69db22ac57b2876b47f3209c153b9f5765f0eda86a6b512d29c3f467df33" => :high_sierra
-    sha256 "633a4e7470a0db791ccdc4e6e070b188776df80814cf80919eb9dc31d5e3a440" => :sierra
-    sha256 "19c84895f37d3b8e63917b586bc453385a27ca740404a4b831a1aa1734cbb63e" => :el_capitan
+    sha256 "101c6991e10c7cc71e4c897091de5c749e7b99e4fb7770d8409f7cdbe8c67b56" => :high_sierra
+    sha256 "e07e64a74d5a498f0c771758af84a97e94bc4647460c49f69ffc28ac4e56c166" => :sierra
+    sha256 "8d5bca3db857d2be65ad1a764c4a3a1e152bb38a1d4c7d07bb8e6d02642030ad" => :el_capitan
   end
 
   depends_on "autoconf" => :build
