2b584f6d4d791aff54690fa93826ec1f5e5d982a
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-19 06:48:39 +0000

    wireguard-tools: update 0.0.20180218 bottle.

--- Formula/wireguard-tools.rb
+++ Formula/wireguard-tools.rb
@@ -9,9 +9,9 @@ class WireguardTools < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7f6e643686eddd744b23b8c1a0434ea74e1047682c73ab402a766db5001534d2" => :high_sierra
-    sha256 "fd847c82d3ea7eec4381d6fadbd6ca97776c5e8ac311ef9f3ec05c21966ee7db" => :sierra
-    sha256 "bd126a8c8be3ea810c02537681494fefe6c81b3bcd9e623abbb9c853eb51f0b8" => :el_capitan
+    sha256 "e2f31efffc7983c154771e78fdd0478cd3cced3b1c2fad138a5e283a077d89ae" => :high_sierra
+    sha256 "e94a9ab0234d02b32c36b19b635a8547409a9ca8e2701504487d771494fb4983" => :sierra
+    sha256 "b58bec450de2e169d78bac699f41c8b0f90299e0592a317c5b0d6dd0c921ff16" => :el_capitan
   end
 
   def install
