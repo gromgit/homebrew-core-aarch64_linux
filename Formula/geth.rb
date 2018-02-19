7b48228a03140f05927c100b8152e1951a6f48ee
ilovezfs <ilovezfs@icloud.com>
2018-02-19 04:50:55 -0800

    geth 1.8.1
    Closes #24307.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/geth.rb
+++ Formula/geth.rb
@@ -1,8 +1,8 @@
 class Geth < Formula
   desc "Official Go implementation of the Ethereum protocol"
   homepage "https://ethereum.github.io/go-ethereum/"
-  url "https://github.com/ethereum/go-ethereum/archive/v1.8.0.tar.gz"
-  sha256 "4e7ea90940b6f34f393c2158ac866f512590962f5d46c21b2f4d523dcf8b9f48"
+  url "https://github.com/ethereum/go-ethereum/archive/v1.8.1.tar.gz"
+  sha256 "be8bb83619f83a2e3acadbc82d2ae8064f8b0dbce17d61de72fdbf0a03480e75"
   head "https://github.com/ethereum/go-ethereum.git"
 
   bottle do
