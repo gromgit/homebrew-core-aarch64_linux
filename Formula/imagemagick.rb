52c21990bd2f6a950f3d8d686631a2ef8561c93d
Alex Gaynor <alex.gaynor@gmail.com>
2018-02-18 21:42:05 -0500

    imagemagick 7.0.7-23
    Closes #24295.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -4,9 +4,9 @@ class Imagemagick < Formula
   # Please always keep the Homebrew mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://dl.bintray.com/homebrew/mirror/imagemagick-7.0.7-22.tar.xz"
-  mirror "https://www.imagemagick.org/download/ImageMagick-7.0.7-22.tar.xz"
-  sha256 "49de9e08ea255a1f939158d85d50dfa29285bccbdcb7fee0fe4309061d438489"
+  url "https://dl.bintray.com/homebrew/mirror/imagemagick-7.0.7-23.tar.xz"
+  mirror "https://www.imagemagick.org/download/ImageMagick-7.0.7-23.tar.xz"
+  sha256 "8dee83fe4978a2d07a90c947508d734ddb175eeb299491f8560cea5ac0c26896"
   head "https://github.com/ImageMagick/ImageMagick.git"
 
   bottle do
