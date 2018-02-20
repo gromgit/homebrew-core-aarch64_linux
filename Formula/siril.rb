c500e5d5df881b0a4cc1de61843287de618a54b7
Cyril Richard <lock042@users.noreply.github.com>
2018-02-20 15:58:24 +0100

    siril 0.9.8.3
    Closes #24352.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/siril.rb
+++ Formula/siril.rb
@@ -1,8 +1,8 @@
 class Siril < Formula
   desc "Astronomical image processing tool"
   homepage "https://free-astro.org/index.php/Siril"
-  url "https://free-astro.org/download/siril-0.9.8.tar.bz2"
-  sha256 "ecb5477937afc02cc89cb07f4a7b99d2d0ab4cc5e715ec536e9be5c92a187170"
+  url "https://free-astro.org/download/siril-0.9.8.3.tar.bz2"
+  sha256 "f6ca57b668441505010673b153f85fa23efdf41fe74ee7ecb5a4926a572acfa3"
   head "https://free-astro.org/svn/siril/", :using => :svn
 
   bottle do
