2b57ccbf9c97c119ac616e99741361b800bc93fc
ilovezfs <ilovezfs@icloud.com>
2018-02-20 01:11:30 -0800

    dfmt 0.6.0
    Closes #24331.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/dfmt.rb
+++ Formula/dfmt.rb
@@ -2,8 +2,8 @@ class Dfmt < Formula
   desc "Formatter for D source code"
   homepage "https://github.com/dlang-community/dfmt"
   url "https://github.com/dlang-community/dfmt.git",
-      :tag => "v0.5.3",
-      :revision => "6e598c197df780c8cd3496c3a663f602a2bd1e58"
+      :tag => "v0.6.0",
+      :revision => "e9273c169ed1ce3bbd64535f0ddec5a6f370680f"
 
   head "https://github.com/dlang-community/dfmt.git", :shallow => false
 
@@ -11,13 +11,6 @@ class Dfmt < Formula
     sha256 "84f5df7c1349c5d386e58a9889c57f0c66e1ced4dedc3e4129b0334ea87990d8" => :high_sierra
     sha256 "2a3fbc12a391de3c297f379d51f7daa400457c6fc03f715d9a347292adb352c4" => :sierra
     sha256 "c1414f980bc31c286eac2144c61cc8f2ad8ef971f3076a323045e2dc0732c4c4" => :el_capitan
-  end
-
-  devel do
-    url "https://github.com/dlang-community/dfmt.git",
-      :tag => "v0.6.0-alpha.1",
-      :revision => "02a735cb0c10d711c5f08fc26572f98bc5fdf0ff"
-    version "0.6.0-alpha.1"
   end
 
   depends_on "dmd" => :build
