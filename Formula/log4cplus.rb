b89174b67fb27b4e447d690b55ebd7f2c061c26e
s-lm <s-lm@users.noreply.github.com>
2018-02-20 11:56:13 +0100

    log4cplus: always build with C++11
    Closes #24345.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/log4cplus.rb
+++ Formula/log4cplus.rb
@@ -11,7 +11,10 @@ class Log4cplus < Formula
     sha256 "8819df40a1477a3fb87a4a3dcd36ec8e2292b5aed362851549e8e2df74ffad9d" => :el_capitan
   end
 
+  needs :cxx11
+
   def install
+    ENV.cxx11
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
