86d3edd6ff536bf435254d74539b8d1033dccebe
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-20 10:02:14 +0000

    pinentry: update 1.1.0_1 bottle.

--- Formula/pinentry.rb
+++ Formula/pinentry.rb
@@ -8,9 +8,9 @@ class Pinentry < Formula
 
   bottle do
     cellar :any
-    sha256 "13cf37ad9d68a82907593bd0b82a8ecee114381c081fa4a600f439ae0b696546" => :high_sierra
-    sha256 "c2ec42c20d6919dea12322faf083db4724d2bfd269b8d435e80c6df0515a6bcc" => :sierra
-    sha256 "e95489766f86c53062781a3be19506660b1265c690a904692b49edac3b6516dd" => :el_capitan
+    sha256 "a1a0e526f622d7dc4e2b3e0dcde061dbb383050b8efbe424d916bf983ae66c74" => :high_sierra
+    sha256 "fd93c11a28d38ba1b78c7fe646f027f98ce29c08ba02c7a0e14e69f355614e35" => :sierra
+    sha256 "e8c6180d9d86f008d0d9cdf0bc9638f2c119bb426504955ecbd16ef6b108d01d" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
