a58edf9749c986118b9f69ae8c6e4f589619a7a9
ilovezfs <ilovezfs@icloud.com>
2018-02-21 07:59:02 -0800

    stubby 0.2.2
    Closes #24399.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/stubby.rb
+++ Formula/stubby.rb
@@ -1,8 +1,8 @@
 class Stubby < Formula
   desc "DNS privacy enabled stub resolver service based on getdns"
   homepage "https://dnsprivacy.org/wiki/display/DP/DNS+Privacy+Daemon+-+Stubby"
-  url "https://github.com/getdnsapi/stubby/archive/v0.2.1.tar.gz"
-  sha256 "adf030a55426918933870f2d49a0caed93023bb1ec806efb255c3e7494985821"
+  url "https://github.com/getdnsapi/stubby/archive/v0.2.2.tar.gz"
+  sha256 "d1418d2c4df3b2f32fac668423630f07dbfb38f6f8d402ddfda9304f16263b03"
   head "https://github.com/getdnsapi/stubby.git", :branch => "develop"
 
   bottle do
