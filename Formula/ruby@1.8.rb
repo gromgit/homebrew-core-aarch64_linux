7f715c97b216b62174ace5efdfd126be725ba456
Mike McQuaid <mike@mikemcquaid.com>
2018-02-21 09:10:28 +0000

    ruby@1.8: remove rubygems.

--- Formula/ruby@1.8.rb
+++ Formula/ruby@1.8.rb
@@ -23,29 +23,12 @@ class RubyAT18 < Formula
   depends_on "openssl"
   depends_on :x11 if build.with? "tcltk"
 
-  # This should be kept in sync with the main Ruby formula
-  # but a revision bump should not be forced every update
-  # unless there are security fixes in that Rubygems release.
-  #
-  # RubyGems 2.7.3 requires Psych of at least 2.0, unless
-  # such a newer version of Psych is vendored within this
-  # formula, RubyGems should not be upgraded until there
-  # is a known vulnerability.
-  resource "rubygems" do
-    url "https://rubygems.org/rubygems/rubygems-2.6.14.tgz"
-    sha256 "406a45d258707f52241843e9c7902bbdcf00e7edc3e88cdb79c46659b47851ec"
-  end
-
   def program_suffix
     build.with?("suffix") ? "187" : ""
   end
 
   def ruby
     "#{bin}/ruby#{program_suffix}"
-  end
-
-  def api_version
-    "1.8"
   end
 
   def install
@@ -66,8 +49,6 @@ class RubyAT18 < Formula
     args = %W[
       --prefix=#{prefix}
       --enable-shared
-      --with-sitedir=#{HOMEBREW_PREFIX}/lib/ruby/site_ruby
-      --with-vendordir=#{HOMEBREW_PREFIX}/lib/ruby/vendor_ruby
     ]
 
     args << "--program-suffix=#{program_suffix}" if build.with? "suffix"
@@ -76,143 +57,14 @@ class RubyAT18 < Formula
     rm_r "ext/tk" if build.without? "tcltk"
 
     system "./configure", *args
-
-    # Ruby has been configured to look in the HOMEBREW_PREFIX for the
-    # sitedir and vendordir directories; however we don't actually want to create
-    # them during the install.
-    #
-    # These directories are empty on install; sitedir is used for non-rubygems
-    # third party libraries, and vendordir is used for packager-provided libraries.
-    inreplace "instruby.rb" do |s|
-      s.gsub! "\n    makedirs [archlibdir, sitearchlibdir, vendorarchlibdir]\n",
-              "\n    makedirs [archlibdir]\n"
-      s.gsub! "\n    makedirs [rubylibdir, sitelibdir, vendorlibdir]\n",
-              "\n    makedirs [rubylibdir]\n"
-    end
-
     system "make"
     system "make", "install"
     system "make", "install-doc" if build.with? "doc"
-
-    # This is easier than trying to keep both current & versioned Ruby
-    # formulae repeatedly updated with Rubygem patches.
-    resource("rubygems").stage do
-      ENV.prepend_path "PATH", bin
-
-      system ruby, "setup.rb", "--prefix=#{buildpath}/vendor_gem"
-      rg_in = lib/"ruby/#{api_version}"
-
-      # Remove bundled Rubygem version.
-      rm_rf rg_in/"rubygems"
-      rm_f rg_in/"rubygems.rb"
-      rm_f rg_in/"ubygems.rb"
-      rm_f bin/"gem#{program_suffix}"
-
-      # Drop in the new version.
-      (rg_in/"rubygems").install Dir[buildpath/"vendor_gem/lib/rubygems/*"]
-      rg_in.install buildpath/"vendor_gem/lib/rubygems.rb"
-      rg_in.install buildpath/"vendor_gem/lib/ubygems.rb"
-      bin.install buildpath/"vendor_gem/bin/gem" => "gem#{program_suffix}"
-    end
-  end
-
-  def post_install
-    # Customize rubygems to look/install in the global gem directory
-    # instead of in the Cellar, making gems last across reinstalls
-    config_file = lib/"ruby/#{api_version}/rubygems/defaults/operating_system.rb"
-    config_file.unlink if config_file.exist?
-    config_file.write rubygems_config
-
-    # Create the sitedir and vendordir that were skipped during install
-    %w[sitearchdir vendorarchdir].each do |dir|
-      mkdir_p `#{ruby} -rrbconfig -e 'print RbConfig::CONFIG["#{dir}"]'`
-    end
-
-    # Create the version-specific bindir used by rubygems
-    mkdir_p rubygems_bindir
-  end
-
-  def rubygems_bindir
-    "#{HOMEBREW_PREFIX}/lib/ruby/gems/#{api_version}/bin"
-  end
-
-  def rubygems_config; <<~EOS
-    module Gem
-      class << self
-        alias :old_default_dir :default_dir
-        alias :old_default_path :default_path
-        alias :old_default_bindir :default_bindir
-        alias :old_ruby :ruby
-      end
-
-      def self.default_dir
-        path = [
-          "#{HOMEBREW_PREFIX}",
-          "lib",
-          "ruby",
-          "gems",
-          "#{api_version}"
-        ]
-
-        @default_dir ||= File.join(*path)
-      end
-
-      def self.private_dir
-        path = if defined? RUBY_FRAMEWORK_VERSION then
-                 [
-                   File.dirname(RbConfig::CONFIG['sitedir']),
-                   'Gems',
-                   RbConfig::CONFIG['ruby_version']
-                 ]
-               elsif RbConfig::CONFIG['rubylibprefix'] then
-                 [
-                  RbConfig::CONFIG['rubylibprefix'],
-                  'gems',
-                  RbConfig::CONFIG['ruby_version']
-                 ]
-               else
-                 [
-                   RbConfig::CONFIG['libdir'],
-                   ruby_engine,
-                   'gems',
-                   RbConfig::CONFIG['ruby_version']
-                 ]
-               end
-
-        @private_dir ||= File.join(*path)
-      end
-
-      def self.default_path
-        if Gem.user_home && File.exist?(Gem.user_home)
-          [user_dir, default_dir, private_dir]
-        else
-          [default_dir, private_dir]
-        end
-      end
-
-      def self.default_bindir
-        "#{rubygems_bindir}"
-      end
-
-      def self.ruby
-        "#{opt_bin}/ruby#{program_suffix}"
-      end
-    end
-    EOS
-  end
-
-  def caveats; <<~EOS
-    By default, binaries installed by gem will be placed into:
-      #{rubygems_bindir}
-
-    You may want to add this to your PATH.
-    EOS
   end
 
   test do
     hello_text = shell_output("#{bin}/ruby#{program_suffix} -e 'puts :hello'")
     assert_equal "hello\n", hello_text
     system "#{bin}/ruby#{program_suffix}", "-e", "require 'zlib'"
-    system "#{bin}/gem#{program_suffix}", "list", "--local"
   end
 end
