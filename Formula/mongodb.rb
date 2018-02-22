6a408954d84aae6a5223e1ca228bf1251e3a35b2
Benedikt Huß <ben305@users.noreply.github.com>
2018-02-22 21:13:34 +0100

    mongodb 3.6.3
    Closes #24437.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/mongodb.rb
+++ Formula/mongodb.rb
@@ -2,8 +2,8 @@ class Mongodb < Formula
   desc "High-performance, schema-free, document-oriented database"
   homepage "https://www.mongodb.org/"
 
-  url "https://fastdl.mongodb.org/src/mongodb-src-r3.6.2.tar.gz"
-  sha256 "018788bd31d6953e55cda0ad0771d23e6b78e024a8a6404a80a900c67215b0a7"
+  url "https://fastdl.mongodb.org/src/mongodb-src-r3.6.3.tar.gz"
+  sha256 "df2d5c05c569ca93eacf88b68e0feb3ff52ffbfc8ccd8736ff20d86850db207c"
 
   bottle do
     rebuild 1
@@ -32,8 +32,8 @@ class Mongodb < Formula
   end
 
   resource "typing" do
-    url "https://files.pythonhosted.org/packages/ca/38/16ba8d542e609997fdcd0214628421c971f8c395084085354b11ff4ac9c3/typing-3.6.2.tar.gz"
-    sha256 "d514bd84b284dd3e844f0305ac07511f097e325171f6cc4a20878d11ad771849"
+    url "https://files.pythonhosted.org/packages/ec/cc/28444132a25c113149cec54618abc909596f0b272a74c55bab9593f8876c/typing-3.6.4.tar.gz"
+    sha256 "d400a9344254803a2368533e4533a4200d21eb7b6b729c173bc38201a74db3f2"
   end
 
   needs :cxx11
