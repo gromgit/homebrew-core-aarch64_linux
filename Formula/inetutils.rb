f9505c4f27ccbd652fc71b815eba9079d7a9ee37
Yishen Miao <mys721tx@gmail.com>
2018-02-22 15:35:51 -0500

    inetutils: add caveats
    Closes #24467.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/inetutils.rb
+++ Formula/inetutils.rb
@@ -17,6 +17,13 @@ class Inetutils < Formula
 
   depends_on "libidn"
 
+  def noshadow
+    # List of binaries that do not shadow macOS utils
+    list = %w[dnsdomainname rcp rexec rlogin rsh]
+    list += %w[ftp telnet] if MacOS.version >= :high_sierra
+    list
+  end
+
   def install
     args = %W[
       --disable-dependency-tracking
@@ -31,8 +38,6 @@ class Inetutils < Formula
 
     if build.without? "default-names"
       # Binaries not shadowing macOS utils symlinked without 'g' prefix
-      noshadow = %w[dnsdomainname rcp rexec rlogin rsh]
-      noshadow += %w[ftp telnet] if MacOS.version >= :high_sierra
       noshadow.each do |cmd|
         bin.install_symlink "g#{cmd}" => cmd
         man1.install_symlink "g#{cmd}.1" => "#{cmd}.1"
@@ -46,6 +51,25 @@ class Inetutils < Formula
         (libexec/"gnubin").install_symlink bin/"g#{cmd}" => cmd
         (libexec/"gnuman"/"man1").install_symlink man1/"g#{cmd}" => cmd
       end
+    end
+  end
+
+  def caveats
+    if build.without? "default-names" then <<~EOS
+      The following commands have been installed with the prefix 'g'.
+
+          #{noshadow.sort.join("\n    ")}
+
+      If you really need to use these commands with their normal names, you
+      can add a "gnubin" directory to your PATH from your bashrc like:
+
+          PATH="#{opt_libexec}/gnubin:$PATH"
+
+      Additionally, you can access their man pages with normal names if you add
+      the "gnuman" directory to your MANPATH from your bashrc as well:
+
+          MANPATH="#{opt_libexec}/gnuman:$MANPATH"
+      EOS
     end
   end
 
