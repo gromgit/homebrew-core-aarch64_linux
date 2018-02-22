c0571c7c271590a51f6e1f7a94a7d6fbe0f5e7ea
Frank <FrankSansC@users.noreply.github.com>
2018-02-22 13:48:10 +0100

    picocom 3.1
    Closes #24426.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/picocom.rb
+++ Formula/picocom.rb
@@ -1,8 +1,8 @@
 class Picocom < Formula
   desc "Minimal dumb-terminal emulation program"
   homepage "https://github.com/npat-efault/picocom"
-  url "https://github.com/npat-efault/picocom/archive/3.0.tar.gz"
-  sha256 "a539db95bde3a5ebd52ae58a21f40d00cc2c97bf14b1f50caffc07257989112e"
+  url "https://github.com/npat-efault/picocom/archive/3.1.tar.gz"
+  sha256 "e6761ca932ffc6d09bd6b11ff018bdaf70b287ce518b3282d29e0270e88420bb"
 
   bottle do
     cellar :any_skip_relocation
