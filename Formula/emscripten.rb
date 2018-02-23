8f10dcd9a93294168aed825104d3bc79172c801b
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-23 21:21:49 +0000

    emscripten: update 1.37.35 bottle.

--- Formula/emscripten.rb
+++ Formula/emscripten.rb
@@ -19,9 +19,9 @@ class Emscripten < Formula
 
   bottle do
     cellar :any
-    sha256 "a916483c9c1999a94aa4bd4a786cd0c8070e05d6d93047a694a967f93c482a04" => :high_sierra
-    sha256 "ac5e93e6ceedf95d64de6d7e76a806570e96ce7028f0a4a00f09418a8d93d30c" => :sierra
-    sha256 "6db8195951dc3dd98bd8e5c6feedbfe84880d1671a816f2533d8efda920736ac" => :el_capitan
+    sha256 "cb78901a1ba6847628af1debffffc76904d30b40c8e5dc96f0a3be6b31cc7909" => :high_sierra
+    sha256 "77699147b1bdd49c811fd0add40188ae9cfb377306f0f5d25b78ba84e3c26f10" => :sierra
+    sha256 "98772109bade817d6f16e68f43ee7dfdd36f70c08ae583a8149823a3f92c6748" => :el_capitan
   end
 
   head do
