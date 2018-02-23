abc8f3f59c9f1bacdc5d63a71857958d1d3efbf5
commitay <commitay@users.noreply.github.com>
2018-02-23 10:07:44 +1000

    exiftool 10.80
    Closes #24439.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/exiftool.rb
+++ Formula/exiftool.rb
@@ -3,22 +3,15 @@ class Exiftool < Formula
   homepage "https://www.sno.phy.queensu.ca/~phil/exiftool/index.html"
   # Ensure release is tagged production before submitting.
   # https://www.sno.phy.queensu.ca/~phil/exiftool/history.html
-  url "https://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-10.55.tar.gz"
-  mirror "https://downloads.sourceforge.net/project/exiftool/Image-ExifTool-10.55.tar.gz"
-  sha256 "029b81a43f423332c00b76b5402fd8f85dee975fad41a734b494faeda4e41f7d"
-  revision 1
+  url "https://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-10.80.tar.gz"
+  mirror "https://downloads.sourceforge.net/project/exiftool/Image-ExifTool-10.80.tar.gz"
+  sha256 "bcf896600776d8ef39a867081b48426837ed5ee8ca19b53dd9d86a3179c93c93"
 
   bottle do
     cellar :any_skip_relocation
     sha256 "5039ab4f2bc31d2afc9b4cd1fab9ab5d6deec2bf2d1bba0a95f4d54e747dd117" => :high_sierra
     sha256 "5039ab4f2bc31d2afc9b4cd1fab9ab5d6deec2bf2d1bba0a95f4d54e747dd117" => :sierra
     sha256 "5039ab4f2bc31d2afc9b4cd1fab9ab5d6deec2bf2d1bba0a95f4d54e747dd117" => :el_capitan
-  end
-
-  devel do
-    url "https://sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-10.78.tar.gz"
-    mirror "https://downloads.sourceforge.net/project/exiftool/Image-ExifTool-10.78.tar.gz"
-    sha256 "06d34e17824db5f496760690705eeeb0aaa4f1cdb701d9ffb78e863a12c68e10"
   end
 
   def install
