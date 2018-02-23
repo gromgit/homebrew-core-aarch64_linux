0dfc1afdbfe89c383faf55d5aeb89accd9b6a6e7
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-23 15:12:01 +0000

    libtommath: secure url(s)

--- Formula/libtommath.rb
+++ Formula/libtommath.rb
@@ -1,6 +1,6 @@
 class Libtommath < Formula
   desc "C library for number theoretic multiple-precision integers"
-  homepage "http://www.libtom.net/LibTomMath/"
+  homepage "https://www.libtom.net/LibTomMath/"
   url "https://github.com/libtom/libtommath/releases/download/v1.0.1/ltm-1.0.1.tar.xz"
   sha256 "47032fb39d698ce4cf9c9c462c198e6b08790ce8203ad1224086b9b978636c69"
   head "https://github.com/libtom/libtommath.git"
