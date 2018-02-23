f4ad038c348e020bb939cdbd92db5280227f8dda
ilovezfs <ilovezfs@icloud.com>
2018-02-23 08:14:10 -0800

    libstrophe 0.9.2
    Closes #24477.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libstrophe.rb
+++ Formula/libstrophe.rb
@@ -1,8 +1,8 @@
 class Libstrophe < Formula
   desc "XMPP library for C"
   homepage "http://strophe.im/libstrophe/"
-  url "https://github.com/strophe/libstrophe/archive/0.9.1.tar.gz"
-  sha256 "c90493f986e5bd407132c5a3e174378c02cb80fa4eaee29875e06b4bba6afcc3"
+  url "https://github.com/strophe/libstrophe/archive/0.9.2.tar.gz"
+  sha256 "158145bc1565a5fd0bbd7f57e3e15d768e58b8a460897ab5918a5a689d67ae6f"
   head "https://github.com/strophe/libstrophe.git"
 
   bottle do
