c1ce1ac7b4d56fb67a56ef9f92e88299b9fc2f3b
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-23 15:12:01 +0000

    advancescan: secure url(s)

--- Formula/advancescan.rb
+++ Formula/advancescan.rb
@@ -1,6 +1,6 @@
 class Advancescan < Formula
   desc "Rom manager for AdvanceMAME/MESS"
-  homepage "http://www.advancemame.it/scan-readme.html"
+  homepage "https://www.advancemame.it/scan-readme.html"
   url "https://github.com/amadvance/advancescan/releases/download/v1.18/advancescan-1.18.tar.gz"
   sha256 "8c346c6578a1486ca01774f30c3e678058b9b8b02f265119776d523358d24672"
 
