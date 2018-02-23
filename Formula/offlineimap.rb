91fb325f2257f9d726e0c56c919e6300b851b9e5
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-23 15:12:01 +0000

    offlineimap: secure url(s)

--- Formula/offlineimap.rb
+++ Formula/offlineimap.rb
@@ -1,6 +1,6 @@
 class Offlineimap < Formula
   desc "Synchronizes emails between two repositories"
-  homepage "http://offlineimap.org/"
+  homepage "https://www.offlineimap.org/"
   url "https://github.com/OfflineIMAP/offlineimap/archive/v7.1.5.tar.gz"
   sha256 "8e28e786a00768e8a97d9f049406744829212cffb69903ffbb15faa1479d43e1"
   head "https://github.com/OfflineIMAP/offlineimap.git"
