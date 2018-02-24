4fb8d6ed82ca12ab29797b0dfffee083fb1904ba
Sergey Avseyev <sergey.avseyev@gmail.com>
2018-02-24 12:44:24 +0300

    libcouchbase 2.8.5
    Closes #24499.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/libcouchbase.rb
+++ Formula/libcouchbase.rb
@@ -1,8 +1,8 @@
 class Libcouchbase < Formula
   desc "C library for Couchbase"
   homepage "https://developer.couchbase.com/documentation/server/current/sdk/c/start-using-sdk.html"
-  url "https://s3.amazonaws.com/packages.couchbase.com/clients/c/libcouchbase-2.8.4.tar.gz"
-  sha256 "22dc8926893790f5d50e4de14526be4c812635e92ff3818c9116775479a1fb0a"
+  url "https://s3.amazonaws.com/packages.couchbase.com/clients/c/libcouchbase-2.8.5.tar.gz"
+  sha256 "81a68bcac5dddeee85464cb0562771c03efd20d86f603351b6b4070339f2ec50"
   head "https://github.com/couchbase/libcouchbase.git"
 
   bottle do
