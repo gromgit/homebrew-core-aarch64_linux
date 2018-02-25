7437c5ae454ab5a693b55b077525745912dd3760
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-25 22:58:44 +0000

    zig: secure url(s)

--- Formula/zig.rb
+++ Formula/zig.rb
@@ -1,6 +1,6 @@
 class Zig < Formula
   desc "Programming language designed for robustness, optimality, and clarity"
-  homepage "http://ziglang.org/"
+  homepage "https://ziglang.org/"
   url "https://github.com/zig-lang/zig/archive/0.1.1.tar.gz"
   sha256 "fabbfcb0bdb08539d9d8e8e1801d20f25cb0025af75ac996f626bb5e528e71f1"
 
