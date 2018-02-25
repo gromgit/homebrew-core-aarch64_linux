d83bd7f1695b2396902eb6bd59c2fd5673089082
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-25 22:26:56 +0000

    libart: secure/fix broken url(s)

--- Formula/libart.rb
+++ Formula/libart.rb
@@ -1,6 +1,6 @@
 class Libart < Formula
   desc "Library for high-performance 2D graphics"
-  homepage "http://freshmeat.net/projects/libart/"
+  homepage "https://people.gnome.org/~mathieu/libart/libart.html"
   url "https://download.gnome.org/sources/libart_lgpl/2.3/libart_lgpl-2.3.21.tar.bz2"
   sha256 "fdc11e74c10fc9ffe4188537e2b370c0abacca7d89021d4d303afdf7fd7476fa"
 
