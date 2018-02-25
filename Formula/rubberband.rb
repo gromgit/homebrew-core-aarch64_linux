1ff1a648192ecfb510487d7db6d245ee5f21aa18
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-25 22:26:57 +0000

    rubberband: secure/fix broken url(s)

--- Formula/rubberband.rb
+++ Formula/rubberband.rb
@@ -4,7 +4,7 @@ class Rubberband < Formula
   head "https://bitbucket.org/breakfastquay/rubberband/", :using => :hg
 
   stable do
-    url "http://code.breakfastquay.com/attachments/download/34/rubberband-1.8.1.tar.bz2"
+    url "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/r/rubberband/rubberband_1.8.1.orig.tar.bz2"
     sha256 "ff0c63b0b5ce41f937a8a3bc560f27918c5fe0b90c6bc1cb70829b86ada82b75"
 
     # replace vecLib.h by Accelerate.h
