b043cca1e5545c1ec0945bdb9241fa59d27927e4
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-25 22:26:56 +0000

    neon: secure/fix broken url(s)

--- Formula/neon.rb
+++ Formula/neon.rb
@@ -1,7 +1,7 @@
 class Neon < Formula
   desc "HTTP and WebDAV client library with a C interface"
-  homepage "http://www.webdav.org/neon/"
-  url "https://www.mirrorservice.org/sites/distfiles.macports.org/neon/neon-0.30.2.tar.gz"
+  homepage "https://web.archive.org/web/20070925040920/www.webdav.org/neon/"
+  url "https://mirrorservice.org/sites/distfiles.macports.org/neon/neon-0.30.2.tar.gz"
   mirror "https://fossies.org/linux/www/neon-0.30.2.tar.gz"
   sha256 "db0bd8cdec329b48f53a6f00199c92d5ba40b0f015b153718d1b15d3d967fbca"
 
