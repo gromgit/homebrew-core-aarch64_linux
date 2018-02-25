ee7256e413c1f379a4f3698f8d46fc1e80f65740
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-25 22:26:55 +0000

    cweb: secure/fix broken url(s)

--- Formula/cweb.rb
+++ Formula/cweb.rb
@@ -1,8 +1,9 @@
 class Cweb < Formula
   desc "Literate documentation system for C, C++, and Java"
   homepage "https://cs.stanford.edu/~knuth/cweb.html"
+  # curl: (9) Server denied you to change to the given directory
+  # ftp://ftp.cs.stanford.edu/pub/cweb/cweb-3.64b.tar.gz
   url "https://www.ctan.org/tex-archive/web/c_cpp/cweb/cweb-3.64b.tar.gz"
-  mirror "ftp://ftp.cs.stanford.edu/pub/cweb/cweb-3.64b.tar.gz"
   sha256 "038b0bf4d8297f0a98051ca2b4664abbf9d72b0b67963a2c7700d2f11cd25595"
 
   bottle do
