361c06a3563bb406a479ef6d633a7f3c3d81ce13
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-25 22:26:56 +0000

    pg_top: secure/fix broken url(s)

--- Formula/pg_top.rb
+++ Formula/pg_top.rb
@@ -1,7 +1,7 @@
 class PgTop < Formula
   desc "Monitor PostgreSQL processes"
-  homepage "http://ptop.projects.postgresql.org/"
-  url "https://www.mirrorservice.org/sites/ftp.postgresql.org/projects/pgFoundry/ptop/pg_top/3.7.0/pg_top-3.7.0.tar.bz2"
+  homepage "https://git.postgresql.org/gitweb/?p=pg_top.git"
+  url "https://mirrorservice.org/sites/ftp.postgresql.org/projects/pgFoundry/ptop/pg_top/3.7.0/pg_top-3.7.0.tar.bz2"
   mirror "http://pgfoundry.org/frs/download.php/3504/pg_top-3.7.0.tar.bz2"
   sha256 "c48d726e8cd778712e712373a428086d95e2b29932e545ff2a948d043de5a6a2"
   revision 3
