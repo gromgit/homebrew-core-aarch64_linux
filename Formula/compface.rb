81124fad8af64df2c624dbe56341392ce595b2b5
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-25 22:26:55 +0000

    compface: secure/fix broken url(s)

--- Formula/compface.rb
+++ Formula/compface.rb
@@ -1,9 +1,9 @@
 class Compface < Formula
   desc "Convert to and from the X-Face format"
-  homepage "http://freecode.com/projects/compface"
+  homepage "https://web.archive.org/web/20170720045032/freecode.com/projects/compface"
   url "https://mirrorservice.org/sites/ftp.xemacs.org/pub/xemacs/aux/compface-1.5.2.tar.gz"
   mirror "https://ftp.heanet.ie/mirrors/ftp.xemacs.org/aux/compface-1.5.2.tar.gz"
-  mirror "http://ftp.xemacs.org/pub/xemacs/aux/compface-1.5.2.tar.gz"
+  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/compface/compface-1.5.2.tar.gz"
   sha256 "a6998245f530217b800f33e01656be8d1f0445632295afa100e5c1611e4f6825"
 
   bottle do
