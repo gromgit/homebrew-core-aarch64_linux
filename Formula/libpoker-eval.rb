350ca166904e3d8f254107611be01febb34200e0
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-25 22:26:56 +0000

    libpoker-eval: secure/fix broken url(s)

--- Formula/libpoker-eval.rb
+++ Formula/libpoker-eval.rb
@@ -1,8 +1,8 @@
 class LibpokerEval < Formula
   desc "C library to evaluate poker hands"
-  homepage "https://pokersource.sourceforge.io"
-  url "http://download.gna.org/pokersource/sources/poker-eval-138.0.tar.gz"
-  mirror "http://ftp.nchc.org.tw/BSD/FreeBSD/ports/distfiles/poker-eval-138.0.tar.gz"
+  homepage "https://pokersource.sourceforge.io/"
+  # http://download.gna.org/pokersource/sources/poker-eval-138.0.tar.gz is offline
+  url "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/p/poker-eval/poker-eval_138.0.orig.tar.gz"
   sha256 "92659e4a90f6856ebd768bad942e9894bd70122dab56f3b23dd2c4c61bdbcf68"
 
   bottle do
