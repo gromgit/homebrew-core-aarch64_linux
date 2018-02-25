5402f036a66227ffe8d6daeb11e80ab98d876b0e
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-25 07:29:36 +0000

    sound-touch: update 2.0.0 bottle.

--- Formula/sound-touch.rb
+++ Formula/sound-touch.rb
@@ -6,11 +6,9 @@ class SoundTouch < Formula
 
   bottle do
     cellar :any
-    sha256 "fa2d750f0a133b42080ff0bb2c35a82612d99f0b6b0833c638ed6cc184c095c7" => :high_sierra
-    sha256 "b27295c5cfbc535639566fe162cf4ea386b9c2ee0f9a8016ad34c165b5f5faa8" => :sierra
-    sha256 "71d50484d79decdb52b05893e28d86a5996b73c9174cc9e266647f04d1afccca" => :el_capitan
-    sha256 "fdd99f6a7879b3b65ad8283f3072afccedf7cb9c82b126a1a96a242a6b20cc07" => :yosemite
-    sha256 "c2a721df1155a2a87de6c3e3f756b812ed37e69c98dfad170e5a1e327018578a" => :mavericks
+    sha256 "5d980f6d661a942650e8b7953e5a0710d0be708421cdf595b68f7da917cdc2be" => :high_sierra
+    sha256 "4861ccccd41fc57f2d553973ece79462e4c85897426f36c258b8fd1756416da1" => :sierra
+    sha256 "7572fff0564f78a49641ed7c5eb9ed062ff557d452d4515e07544a622eaa17e6" => :el_capitan
   end
 
   option "with-integer-samples", "Build using integer samples? (default is float)"
