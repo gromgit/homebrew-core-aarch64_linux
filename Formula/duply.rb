4ad09ca4c55fbbb3de528ea6724d541bf7d71be7
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-26 01:50:01 +0000

    duply: secure/fix broken url(s)

--- Formula/duply.rb
+++ Formula/duply.rb
@@ -1,6 +1,8 @@
 class Duply < Formula
   desc "Frontend to the duplicity backup system"
-  homepage "http://duply.net"
+  # Canonical domain: duply.net
+  # Historical homepage: https://web.archive.org/web/20131126005707/ftplicity.sourceforge.net
+  homepage "https://sourceforge.net/projects/ftplicity/"
   url "https://downloads.sourceforge.net/project/ftplicity/duply%20%28simple%20duplicity%29/2.0.x/duply_2.0.4.tgz"
   sha256 "efa450b976ff94d29b8f22b99d98aae108e032186c01ffdc526c85c87813048c"
 
