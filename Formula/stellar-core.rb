196802ea36ba65969d10679c54fe4905c872ae35
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-26 04:37:18 +0000

    stellar-core: add 9.1.0 bottle.

--- Formula/stellar-core.rb
+++ Formula/stellar-core.rb
@@ -6,6 +6,13 @@ class StellarCore < Formula
     :revision => "a278e95978bdac6d1015d82f4859dad780e752d3"
   head "https://github.com/stellar/stellar-core.git"
 
+  bottle do
+    cellar :any
+    sha256 "2eb5403e7fcdf217e3bde9ec09919c7dcbc90f317c6a6adb2913997bf47618a1" => :high_sierra
+    sha256 "e23598b805518739534c7f63b28b1612b7915db8569003fa2a10701cf65a7be3" => :sierra
+    sha256 "d82469269c1807b742106205179951e8887e68eecf5804b7b73399e332d34559" => :el_capitan
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
