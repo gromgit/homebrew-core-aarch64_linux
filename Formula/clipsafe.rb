4ac95a78c6f7b51692d301015fb63bb9f8b8d9cf
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-26 01:15:17 +0000

    clipsafe: remove broken mirror url(s)

--- Formula/clipsafe.rb
+++ Formula/clipsafe.rb
@@ -29,19 +29,16 @@ class Clipsafe < Formula
 
   resource "DateTime" do
     url "https://cpan.metacpan.org/authors/id/D/DR/DROLSKY/DateTime-1.35.tar.gz"
-    mirror "http://search.cpan.org/CPAN/authors/id/D/DR/DROLSKY/DateTime-1.35.tar.gz"
     sha256 "42da13b8b15373c1a1790ef4217742d34f7f80f7e5c51c377cc9eed3c7641525"
   end
 
   resource "DateTime::Locale" do
     url "https://cpan.metacpan.org/authors/id/D/DR/DROLSKY/DateTime-Locale-1.05.tar.gz"
-    mirror "http://search.cpan.org/CPAN/authors/id/D/DR/DROLSKY/DateTime-Locale-1.05.tar.gz"
     sha256 "692d674558589e2a6ad2d68cf58a6e4e4b1bfe2f7f76caf20cb91b975a51d155"
   end
 
   resource "DateTime::TimeZone" do
     url "https://cpan.metacpan.org/authors/id/D/DR/DROLSKY/DateTime-TimeZone-2.01.tar.gz"
-    mirror "http://search.cpan.org/CPAN/authors/id/D/DR/DROLSKY/DateTime-TimeZone-2.01.tar.gz"
     sha256 "1f673c8156fa42b2030aae46fd7e4c117f229da99dfbc7c57ef9eadd5eaa69e1"
   end
 
