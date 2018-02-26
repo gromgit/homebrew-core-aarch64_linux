071fa06995cc9fba7c7dbcd2227f899e77577a28
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-26 21:18:10 +0000

    jnettop: improve urls
    Skip using the canonical tarball, as it differs slightly
    from the source tarball used to generate the current binaries.
    Canonical contains an extra 4 lines in ./ChangeLog, otherwise
    they are identical.

--- Formula/jnettop.rb
+++ Formula/jnettop.rb
@@ -1,8 +1,8 @@
 class Jnettop < Formula
   desc "View hosts/ports taking up the most network traffic"
-  # http://jnettop.kubs.info/ is offline
-  homepage "https://packages.debian.org/sid/net/jnettop"
-  # http://jnettop.kubs.info/dist/jnettop-0.13.0.tar.gz is offline
+  homepage "https://web.archive.org/web/20161127214942/jnettop.kubs.info/wiki/"
+  # Please switch back to the canonical source on the next update:
+  # https://downloads.sourceforge.net/project/jnettop/jnettop/0.13/jnettop-0.13.0.tar.gz
   url "https://mirrors.ocf.berkeley.edu/debian/pool/main/j/jnettop/jnettop_0.13.0.orig.tar.gz"
   mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/j/jnettop/jnettop_0.13.0.orig.tar.gz"
   sha256 "e987a1a9325595c8a0543ab61cf3b6d781b4faf72dd0e0e0c70b2cc2ceb5a5a0"
