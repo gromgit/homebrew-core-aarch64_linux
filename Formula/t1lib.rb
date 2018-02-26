0ca71217d92f609d4f42a8db07eeb13dc547119f
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-26 21:20:08 +0000

    t1lib: secure mirror url

--- Formula/t1lib.rb
+++ Formula/t1lib.rb
@@ -2,7 +2,7 @@ class T1lib < Formula
   desc "C library to generate/rasterize bitmaps from Type 1 fonts"
   homepage "https://www.t1lib.org/"
   url "https://www.ibiblio.org/pub/linux/libs/graphics/t1lib-5.1.2.tar.gz"
-  mirror "ftp://ftp.ibiblio.org/pub/linux/libs/graphics/t1lib-5.1.2.tar.gz"
+  mirror "https://fossies.org/linux/misc/old/t1lib-5.1.2.tar.gz"
   sha256 "821328b5054f7890a0d0cd2f52825270705df3641dbd476d58d17e56ed957b59"
 
   bottle do
