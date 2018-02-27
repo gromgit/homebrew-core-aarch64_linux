9b906b577de3b82a10aae0c045e97dec7eb7370a
ilovezfs <ilovezfs@icloud.com>
2018-02-27 08:37:58 -0800

    py3cairo: rename python dependency

--- Formula/py3cairo.rb
+++ Formula/py3cairo.rb
@@ -3,6 +3,7 @@ class Py3cairo < Formula
   homepage "https://cairographics.org/pycairo/"
   url "https://github.com/pygobject/pycairo/releases/download/v1.16.3/pycairo-1.16.3.tar.gz"
   sha256 "5bb321e5d4f8b3a51f56fc6a35c143f1b72ce0d748b43d8b623596e8215f01f7"
+  revision 1
 
   bottle do
     cellar :any
@@ -13,7 +14,7 @@ class Py3cairo < Formula
 
   depends_on "pkg-config" => :build
   depends_on "cairo"
-  depends_on "python3"
+  depends_on "python"
 
   def install
     system "python3", *Language::Python.setup_install_args(prefix)
