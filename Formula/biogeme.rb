6ba807a42cdbee8c960a29a5678990af1fb40101
ilovezfs <ilovezfs@icloud.com>
2018-02-27 08:37:52 -0800

    biogeme: rename python dependency

--- Formula/biogeme.rb
+++ Formula/biogeme.rb
@@ -3,7 +3,7 @@ class Biogeme < Formula
   homepage "https://biogeme.epfl.ch/"
   url "https://biogeme.epfl.ch/distrib/biogeme-2.6a.tar.gz"
   sha256 "f6de0ea12f83ed183f31a41b9a56d1ec7226d2305549fb89ea7b1de8273ede49"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "ad1acb781c111abdf62634521e86ffe85e7bec8c2738218ee50925e5502af40b" => :high_sierra
@@ -11,7 +11,7 @@ class Biogeme < Formula
     sha256 "9f1ce4b715beb128766b714d52872c26e688c5b1960b17ce8ab480796946043e" => :el_capitan
   end
 
-  depends_on "python3"
+  depends_on "python"
   depends_on "gtkmm3"
 
   def install
