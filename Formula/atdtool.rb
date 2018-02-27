ed477d7eeaf7973131d3e8ffa4ba12d7b8235c40
ilovezfs <ilovezfs@icloud.com>
2018-02-26 22:47:05 -0800

    atdtool 1.3.1
    Closes #24578.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/atdtool.rb
+++ Formula/atdtool.rb
@@ -1,7 +1,7 @@
 class Atdtool < Formula
   desc "Command-line interface for After the Deadline language checker"
   homepage "https://github.com/lpenz/atdtool"
-  url "https://github.com/lpenz/atdtool/archive/upstream/1.3.tar.gz"
+  url "https://github.com/lpenz/atdtool/archive/upstream/1.3.1.tar.gz"
   sha256 "eb634fd9e8a57d5d5e4d8d2ca0dd9692610aa952e28fdf24909fd678a8f39155"
 
   bottle do
@@ -16,11 +16,7 @@ class Atdtool < Formula
   depends_on "txt2tags" => :build
 
   def install
-    # Change the PREFIX to match the homebrew one, since there is no way to
-    # pass it as an option for now edit the Makefile
-    # https://github.com/lpenz/atdtool/pull/8
-    inreplace "Makefile", "PREFIX=/usr/local", "PREFIX=#{prefix}"
-    system "make", "install"
+    system "make", "install", "PREFIX=#{prefix}"
   end
 
   test do
