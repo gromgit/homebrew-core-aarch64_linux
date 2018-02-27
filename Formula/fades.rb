a4db6bcffa6f80eb9bc079c48a362380726455cc
ilovezfs <ilovezfs@icloud.com>
2018-02-27 08:37:54 -0800

    fades: rename python dependency

--- Formula/fades.rb
+++ Formula/fades.rb
@@ -3,7 +3,7 @@ class Fades < Formula
   homepage "https://fades.readthedocs.org/"
   url "https://files.pythonhosted.org/packages/59/18/cc80eb5c0a2e15c4b95df6a3c6158e06acb2e075ef2b811753936a6bdf57/fades-6.0.1.tar.gz"
   sha256 "747ee3a159be1cb7512fd7ea4163d22e2734209e474aa2dbbccc29a0f0c92d09"
-  revision 1
+  revision 2
   head "https://github.com/PyAr/fades.git"
 
   bottle do
@@ -13,7 +13,7 @@ class Fades < Formula
     sha256 "13eceff41cb2181ead4f0993f3726b3f2f5887dede5b7013167360af923dc3dc" => :el_capitan
   end
 
-  depends_on "python3"
+  depends_on "python"
 
   def install
     pyver = Language::Python.major_minor_version "python3"
