3a70340926252c63af62192214a4d1442928b62f
ilovezfs <ilovezfs@icloud.com>
2018-02-27 08:38:00 -0800

    thefuck: rename python dependency

--- Formula/thefuck.rb
+++ Formula/thefuck.rb
@@ -5,7 +5,7 @@ class Thefuck < Formula
   homepage "https://github.com/nvbn/thefuck"
   url "https://files.pythonhosted.org/packages/ec/20/3f136313c27ea36ba38d31818b9f0a1a4656668fc6747b3bfa04f3debc98/thefuck-3.25.tar.gz"
   sha256 "70cbe6295d2d2d371a395619216d38eb1666e4b3c3b1a24f67d11b88e65fea78"
-  revision 1
+  revision 2
   head "https://github.com/nvbn/thefuck.git"
 
   bottle do
@@ -15,7 +15,7 @@ class Thefuck < Formula
     sha256 "7c78686d8277a148e9d10247df47bbe5d83b4d6265ce779ae97c039350e27d01" => :el_capitan
   end
 
-  depends_on "python3"
+  depends_on "python"
 
   resource "colorama" do
     url "https://files.pythonhosted.org/packages/e6/76/257b53926889e2835355d74fec73d82662100135293e17d382e2b74d1669/colorama-0.3.9.tar.gz"
