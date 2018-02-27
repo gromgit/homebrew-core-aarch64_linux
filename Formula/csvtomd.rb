ce26028a197bd2dddf3942a813494d226cbf0678
ilovezfs <ilovezfs@icloud.com>
2018-02-27 08:37:53 -0800

    csvtomd: rename python dependency

--- Formula/csvtomd.rb
+++ Formula/csvtomd.rb
@@ -5,7 +5,7 @@ class Csvtomd < Formula
   homepage "https://github.com/mplewis/csvtomd"
   url "https://files.pythonhosted.org/packages/2f/41/289bedde7fb32d817d5802eff68b99546842cb34df840665ec39b363f258/csvtomd-0.2.1.tar.gz"
   sha256 "d9fdf166c3c299ad5800b3cb1661f223b98237f38f22e9d253d45d321f70ec72"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any_skip_relocation
@@ -14,7 +14,7 @@ class Csvtomd < Formula
     sha256 "95e1e0ae2ba1fa1205a19822b5b79b2586ba9f36bf9fed7faf30e1a4223f27c6" => :el_capitan
   end
 
-  depends_on "python3"
+  depends_on "python"
 
   def install
     virtualenv_install_with_resources
