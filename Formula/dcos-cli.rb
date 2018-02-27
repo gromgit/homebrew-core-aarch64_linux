038eb05de1f21a5e46e753498ae92ff1a5001ff4
ilovezfs <ilovezfs@icloud.com>
2018-02-27 08:37:53 -0800

    dcos-cli: rename python dependency

--- Formula/dcos-cli.rb
+++ Formula/dcos-cli.rb
@@ -5,7 +5,7 @@ class DcosCli < Formula
   homepage "https://dcos.io/docs/latest/cli/"
   url "https://github.com/dcos/dcos-cli/archive/0.5.7.tar.gz"
   sha256 "437f5bdbf1be1f3e105c5df98e0423364fd01501115bd7b823122e30252a553d"
-  revision 1
+  revision 2
   head "https://github.com/dcos/dcos-cli.git"
 
   bottle do
@@ -16,7 +16,7 @@ class DcosCli < Formula
   end
 
   depends_on "openssl"
-  depends_on "python3"
+  depends_on "python"
 
   resource "asn1crypto" do
     url "https://files.pythonhosted.org/packages/31/53/8bca924b30cb79d6d70dbab6a99e8731d1e4dd3b090b7f3d8412a8d8ffbc/asn1crypto-0.23.0.tar.gz"
