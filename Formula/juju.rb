a4f48c36470d519cf55dfa2b71992d91026b55e0
Joseph Phillips <joseph@TELLAN.168.1.14>
2018-02-27 22:55:00 +0100

    juju 2.3.4
    Closes #24611.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -1,8 +1,8 @@
 class Juju < Formula
   desc "DevOps management tool"
   homepage "https://jujucharms.com/"
-  url "https://launchpad.net/juju/2.3/2.3.3/+download/juju-core_2.3.3.tar.gz"
-  sha256 "98b13d76c34118da110d3bc50931de9b7aa69594ebf2cb18b37cb187a9ffc6dd"
+  url "https://launchpad.net/juju/2.3/2.3.4/+download/juju-core_2.3.4.tar.gz"
+  sha256 "1b1057e9372a4cfc9689dcfb8ff88ee13dad953e0d82bbadfb9626e346c7592c"
 
   bottle do
     cellar :any_skip_relocation
