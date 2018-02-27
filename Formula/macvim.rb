1c8da1e7cf789cf9dde5f17f935d8eb078b0a33f
ilovezfs <ilovezfs@icloud.com>
2018-02-27 08:51:13 -0800

    macvim: rename python@2 dependency
    and make Python 3 the default

--- Formula/macvim.rb
+++ Formula/macvim.rb
@@ -5,6 +5,7 @@ class Macvim < Formula
   url "https://github.com/macvim-dev/macvim/archive/snapshot-145.tar.gz"
   version "8.0-145"
   sha256 "37ea193345421ea17731fe2a06806641ef6607d38829b195b596179f70810ce2"
+  revision 1
   head "https://github.com/macvim-dev/macvim.git"
 
   bottle do
@@ -22,11 +23,9 @@ class Macvim < Formula
   depends_on "python" => :recommended
   depends_on "lua" => :optional
   depends_on "luajit" => :optional
-  depends_on "python3" => :optional
+  depends_on "python@2" => :optional
 
   def install
-    ENV.prepend_path "PATH", Formula["python"].opt_libexec/"bin"
-
     # Avoid issues finding Ruby headers
     if MacOS.version == :sierra || MacOS.version == :yosemite
       ENV.delete("SDKROOT")
@@ -64,11 +63,10 @@ class Macvim < Formula
       args << "--with-luajit"
     end
 
-    # Allow python or python3, but not both; if the optional
-    # python3 is chosen, default to it; otherwise, use python2
-    if build.with? "python3"
-      args << "--enable-python3interp"
-    elsif build.with? "python"
+    # Allow python or python@2, but not both; if the optional
+    # python@2 is chosen, default to it; otherwise, use python
+    if build.with? "python@2"
+      ENV.prepend_path "PATH", Formula["python@2"].opt_libexec/"bin"
       ENV.prepend "LDFLAGS", `python-config --ldflags`.chomp
 
       # Needed for <= OS X 10.9.2 with Xcode 5.1
@@ -85,6 +83,8 @@ class Macvim < Formula
         ENV.prepend "CFLAGS", "-F#{framework_prefix}"
       end
       args << "--enable-pythoninterp"
+    else
+      args << "--enable-python3interp"
     end
 
     system "./configure", *args
@@ -100,21 +100,22 @@ class Macvim < Formula
   end
 
   def caveats
-    if build.with?("python") && build.with?("python3")
+    if build.with?("python") && build.with?("python@2")
       <<~EOS
         MacVim can no longer be brewed with dynamic support for both Python versions.
-        Only Python 3 support has been provided.
+        Only Python 2 support has been provided.
       EOS
     end
   end
 
   test do
-    ENV.prepend_path "PATH", Formula["python"].opt_libexec/"bin"
-    # Simple test to check if MacVim was linked to Python version in $PATH
+    output = shell_output("#{bin}/mvim --version")
+    assert_match "+ruby", output
+
+    # Simple test to check if MacVim was linked to Homebrew's Python 3
     if build.with? "python"
-      system_framework_path = `python-config --exec-prefix`.chomp
-      assert_match system_framework_path, `mvim --version`
+      py3_exec_prefix = Utils.popen_read("python3-config", "--exec-prefix")
+      assert_match py3_exec_prefix.chomp, output
     end
-    assert_match "+ruby", shell_output("#{bin}/mvim --version")
   end
 end
