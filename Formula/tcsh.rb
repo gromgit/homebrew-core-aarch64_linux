2362cb233ebfd49268b62a62f74df8c38c88402f
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-27 11:55:10 +0000

    tcsh: secure mirror(s)

--- Formula/tcsh.rb
+++ Formula/tcsh.rb
@@ -2,7 +2,7 @@ class Tcsh < Formula
   desc "Enhanced, fully compatible version of the Berkeley C shell"
   homepage "http://www.tcsh.org/"
   url "ftp://ftp.astron.com/pub/tcsh/tcsh-6.20.00.tar.gz"
-  mirror "http://ftp.funet.fi/pub/mirrors/ftp.astron.com/pub/tcsh/tcsh-6.20.00.tar.gz"
+  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/tcsh/tcsh-6.20.00.tar.gz"
   sha256 "b89de7064ab54dac454a266cfe5d8bf66940cb5ed048d0c30674ea62e7ecef9d"
 
   bottle do
