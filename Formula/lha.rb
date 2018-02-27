f574825c7494f10fb20fc0564f913a75cd4d5b18
Viktor Szakats <vszakats@users.noreply.github.com>
2018-02-27 10:02:09 +0000

    lha: secure url(s)

--- Formula/lha.rb
+++ Formula/lha.rb
@@ -1,8 +1,8 @@
 class Lha < Formula
   desc "Utility for creating and opening lzh archives"
   homepage "https://lha.osdn.jp/"
-  url "https://ja.osdn.net/frs/redir.php?f=%2Flha%2F22231%2Flha-1.14i-ac20050924p1.tar.gz"
-  mirror "http://dl.osdn.jp/lha/22231/lha-1.14i-ac20050924p1.tar.gz"
+  # Canonical: https://osdn.net/dl/lha/lha-1.14i-ac20050924p1.tar.gz
+  url "https://dotsrc.dl.osdn.net/osdn/lha/22231/lha-1.14i-ac20050924p1.tar.gz"
   version "1.14i-ac20050924p1"
   sha256 "b5261e9f98538816aa9e64791f23cb83f1632ecda61f02e54b6749e9ca5e9ee4"
 
