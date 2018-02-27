d4baff93a7bfe0c04cb6d268d2d45f33f20444af
ilovezfs <ilovezfs@icloud.com>
2018-02-27 08:37:57 -0800

    pastebinit: rename python dependency

--- Formula/pastebinit.rb
+++ Formula/pastebinit.rb
@@ -3,6 +3,7 @@ class Pastebinit < Formula
   homepage "https://launchpad.net/pastebinit"
   url "https://launchpad.net/pastebinit/trunk/1.5/+download/pastebinit-1.5.tar.gz"
   sha256 "0d931dddb3744ed38aa2d319dd2d8a2f38a391011ff99db68ce7c83ab8f5b62f"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -14,11 +15,11 @@ class Pastebinit < Formula
   end
 
   depends_on "docbook2x" => :build
-  depends_on "python3"
+  depends_on "python"
 
   def install
     inreplace "pastebinit" do |s|
-      s.gsub! "/usr/bin/python3", Formula["python3"].opt_bin/"python3"
+      s.gsub! "/usr/bin/python3", Formula["python"].opt_bin/"python3"
       s.gsub! "/usr/local/etc/pastebin.d", etc/"pastebin.d"
     end
 
