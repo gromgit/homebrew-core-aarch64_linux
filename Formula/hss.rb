ef72e7b22d26287b69229969a878c70f557ca19a
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-27 14:37:24 +0000

    hss: add 1.6 bottle.

--- Formula/hss.rb
+++ Formula/hss.rb
@@ -4,6 +4,13 @@ class Hss < Formula
   url "https://github.com/six-ddc/hss/archive/1.6.tar.gz"
   sha256 "8516f3e24c9908f9c7ac02ee5247ce78f2a344e7fcca8a14081a92949db70049"
 
+  bottle do
+    cellar :any
+    sha256 "9a41aa73345f45ad440ae92c188c9ea6ff868d128024fcfd2abe39b2a3aa027a" => :high_sierra
+    sha256 "b16182f23f97fbd1dac75f6b61264e39084c03a7864f37940127bf4460fd76f8" => :sierra
+    sha256 "e65e32761d3d0485a62ff9a8826318ab53a6f92aed084435b5dfe0cd9a09a92b" => :el_capitan
+  end
+
   depends_on "readline"
 
   def install
