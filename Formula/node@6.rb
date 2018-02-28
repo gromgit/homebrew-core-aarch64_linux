881c172acb0c2d960a0d3a8aa3becef948036e32
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-28 10:29:33 +0000

    node@6: update 6.13.0 bottle.

--- Formula/node@6.rb
+++ Formula/node@6.rb
@@ -6,10 +6,10 @@ class NodeAT6 < Formula
   head "https://github.com/nodejs/node.git", :branch => "v6.x-staging"
 
   bottle do
-    rebuild 1
-    sha256 "79b2bdf0dad86efab66aa2258a808baee005605a3e6048e211f5523a6e028cf5" => :high_sierra
-    sha256 "a14dc30ba521d3e030ae1eb5469d55b075e6b9390d66152ea3771ace55700fe4" => :sierra
-    sha256 "e6de6b5903d5ce214b4b4f5462ab64a7687240e755470f893eeb0981d506f688" => :el_capitan
+    rebuild 2
+    sha256 "0b37f5dd42ed7736580473140f4ae767d33f349c6d64f8a83d01f5ce388b2a2d" => :high_sierra
+    sha256 "70ac01d90ed6b3997c74793e45bd24ee4d7cfcf27a2305b1b26841e02f260f4c" => :sierra
+    sha256 "233c27e2a389b8b410cce0616ac059bed418975a174cbff0917fb54550953654" => :el_capitan
   end
 
   keg_only :versioned_formula
