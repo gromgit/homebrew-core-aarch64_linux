388384a3ada1893ddd101be148d5a19990c09c4c
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-28 16:05:50 +0000

    viewvc: update 1.1.26_1 bottle.

--- Formula/viewvc.rb
+++ Formula/viewvc.rb
@@ -7,10 +7,9 @@ class Viewvc < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a9a0b010a04b5af7a33c372dab11fa249b02fb6eea4ea583eda4af3730394eda" => :high_sierra
-    sha256 "d692c6f47134e21a0e0659ee854cb8ad6ed8e45bc88b0bcbaf3307e7fba24d1b" => :sierra
-    sha256 "cb3b467bbb5268c62e3ab2cc1870000fce3558e3443b33bb6fa17c2ce87147cc" => :el_capitan
-    sha256 "cb3b467bbb5268c62e3ab2cc1870000fce3558e3443b33bb6fa17c2ce87147cc" => :yosemite
+    sha256 "dd3b0a77e2eb08bbec7f5a0b14744f2dcc7894d4da9702d1e489e901c8e9a4f0" => :high_sierra
+    sha256 "dd3b0a77e2eb08bbec7f5a0b14744f2dcc7894d4da9702d1e489e901c8e9a4f0" => :sierra
+    sha256 "dd3b0a77e2eb08bbec7f5a0b14744f2dcc7894d4da9702d1e489e901c8e9a4f0" => :el_capitan
   end
 
   depends_on "subversion"
