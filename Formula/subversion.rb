0beff225b47c74e2049277aacf09de3c57b09c54
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-28 16:05:50 +0000

    subversion: update 1.9.7_3 bottle.

--- Formula/subversion.rb
+++ Formula/subversion.rb
@@ -7,9 +7,9 @@ class Subversion < Formula
   revision 3
 
   bottle do
-    sha256 "2de1e1d3691d8adc101cdb39a62792907f879fe5462c9870f07edef6c3c4160c" => :high_sierra
-    sha256 "ee2be3846eca442aea461878da9f5ef208092836b91bc38bd13fa5572040f5a5" => :sierra
-    sha256 "9445c1341c69187a4a28c5ff8ab341539beff082923729629abcc2699efda095" => :el_capitan
+    sha256 "7482d281e4e0ff5b10377a576f3e97ed867795645291308d35adf51747ff2162" => :high_sierra
+    sha256 "a181531a3a3ad04d429b7aead29fec814c6e042482ea35abc581cc9e4571a25a" => :sierra
+    sha256 "6cb9acb883f5d7045324846bab0d51315da6fbb462b9ef664e0c9d0c9b58ef8d" => :el_capitan
   end
 
   deprecated_option "java" => "with-java"
