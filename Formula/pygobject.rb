8e23a8b9f8d70dfa4384f80ef6c4271070d5d025
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-02 03:24:30 +0000

    pygobject: update 2.28.7_1 bottle.

--- Formula/pygobject.rb
+++ Formula/pygobject.rb
@@ -7,9 +7,10 @@ class Pygobject < Formula
 
   bottle do
     cellar :any
-    sha256 "44d2fbe065a06b2f874b8d0b770375b0e8628a77f67823a09cf354d4bf1fd3e3" => :high_sierra
-    sha256 "85854484b61200e295a9767b34bb86eb06b33045429f160c19f84cca39abaf17" => :sierra
-    sha256 "47b48d492fad94254508ce966c3e9261b74879049a5fc9a4fb34e3209c0f4ce5" => :el_capitan
+    rebuild 1
+    sha256 "c442ae4c5065422f9c522c9e1000dd50f1e9bd565d496cba14a0edb0f378bb6c" => :high_sierra
+    sha256 "d483ec396469442c8efb3a0936db2ac3027c131a70c8117f5a1ef6a97a007b00" => :sierra
+    sha256 "a1302d922cfb12f36c25ebe6f35438ccb06fe8e7a155d4a9f326f71e8033b644" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
