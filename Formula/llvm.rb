7b29b5d0196b69d38eb98beaa9d18098899b6847
ilovezfs <ilovezfs@icloud.com>
2018-03-02 07:20:48 -0800

    llvm: fix use of deprecated option name "python"

--- Formula/llvm.rb
+++ Formula/llvm.rb
@@ -245,7 +245,7 @@ class Llvm < Formula
 
     args << "-DLLVM_ENABLE_LIBCXX=ON" if build_libcxx?
 
-    if build.with?("lldb") && build.with?("python")
+    if build.with?("lldb") && build.with?("python@2")
       args << "-DLLDB_RELOCATABLE_PYTHON=ON"
       args << "-DPYTHON_LIBRARY=#{pylib}"
       args << "-DPYTHON_INCLUDE_DIR=#{pyinclude}"
