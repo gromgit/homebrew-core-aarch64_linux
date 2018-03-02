ade113ec4820f8757ea3df5fd39b524bc1afc523
ilovezfs <ilovezfs@icloud.com>
2018-03-01 23:47:21 -0800

    blast: rename python@2 dependency

--- Formula/blast.rb
+++ Formula/blast.rb
@@ -13,7 +13,7 @@ class Blast < Formula
   end
 
   depends_on "lmdb"
-  depends_on "python" if MacOS.version <= :snow_leopard
+  depends_on "python@2" if MacOS.version <= :snow_leopard
 
   def install
     cd "c++" do
