e7a7a6ef7f8297f1dc21861522ebd4381f446797
ilovezfs <ilovezfs@icloud.com>
2018-03-01 23:47:48 -0800

    vte: rename python@2 dependency

--- Formula/vte.rb
+++ Formula/vte.rb
@@ -18,7 +18,7 @@ class Vte < Formula
   depends_on "gtk+"
   depends_on "pygobject"
   depends_on "pygtk"
-  depends_on "python" if MacOS.version <= :snow_leopard
+  depends_on "python@2" if MacOS.version <= :snow_leopard
 
   def install
     args = %W[
