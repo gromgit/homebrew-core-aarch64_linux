c7a087275169a05ed5d29f67c7f01d8c43d514f3
ilovezfs <ilovezfs@icloud.com>
2018-03-01 23:19:13 -0800

    libpst: rename python@2 optional dependency

--- Formula/libpst.rb
+++ Formula/libpst.rb
@@ -15,14 +15,15 @@ class Libpst < Formula
   option "with-pst2dii", "Build pst2dii using gd"
 
   deprecated_option "pst2dii" => "with-pst2dii"
+  deprecated_option "with-python" => "with-python@2"
 
-  depends_on "python" => :optional
+  depends_on "python@2" => :optional
   depends_on "pkg-config" => :build
   depends_on "gd" if build.with? "pst2dii"
   depends_on "boost"
   depends_on "gettext"
   depends_on "libgsf"
-  depends_on "boost-python" if build.with? "python"
+  depends_on "boost-python" if build.with? "python@2"
 
   def install
     args = %W[
@@ -32,7 +33,7 @@ class Libpst < Formula
 
     args << "--disable-dii" if build.with? "pst2dii"
 
-    if build.with? "python"
+    if build.with? "python@2"
       args << "--enable-python" << "--with-boost-python=mt"
     else
       args << "--disable-python"
