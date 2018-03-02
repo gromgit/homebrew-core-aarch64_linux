a4976c040669bba73072570b4a184c0c4b45e479
ilovezfs <ilovezfs@icloud.com>
2018-03-01 22:46:46 -0800

    dateutils 0.4.3
    Closes #24707.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/dateutils.rb
+++ Formula/dateutils.rb
@@ -1,9 +1,8 @@
 class Dateutils < Formula
   desc "Tools to manipulate dates with a focus on financial data"
   homepage "https://www.fresse.org/dateutils/"
-  url "https://bitbucket.org/hroptatyr/dateutils/downloads/dateutils-0.4.2.tar.xz"
-  mirror "https://github.com/hroptatyr/dateutils/releases/download/v0.4.2/dateutils-0.4.2.tar.xz"
-  sha256 "ecdae892584098ee9d8f5b14bd555fd63e09d1199cb75aac6b02f09c7e2eb46b"
+  url "https://bitbucket.org/hroptatyr/dateutils/downloads/dateutils-0.4.3.tar.xz"
+  sha256 "b0d579062bc957823fb7efcb2c520fb6fb43ab79a352026402b06ecfc4c58f1a"
 
   bottle do
     sha256 "e9ba9bee465b5a7b062a978ad8a99aa8e157624da61ac2340b38a8faa6eb8955" => :high_sierra
