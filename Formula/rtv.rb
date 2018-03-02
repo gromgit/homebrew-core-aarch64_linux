2f12916fa19e393f540bed1626054e4e56bb36e5
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-02 03:24:29 +0000

    rtv: update 1.21.0_2 bottle.

--- Formula/rtv.rb
+++ Formula/rtv.rb
@@ -10,9 +10,9 @@ class Rtv < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c9ee98d9223e0410254b749751c2a175b900e133e5c029c5ae74ae36b1d7ce0f" => :high_sierra
-    sha256 "cb33c61261304d00d606f9666071234f8c8227a4fc7750c94864ac395322ab74" => :sierra
-    sha256 "fb36bbf09f27ca20d22cbdac955363333a5a67bca625237a5049e366091cd732" => :el_capitan
+    sha256 "3a969dd228fe35726e8f9e7a7543e6d9470cb5465bfbfe4b7465e60b63ad537b" => :high_sierra
+    sha256 "50a60bec5656f57b5ed68a84e77655db0df8dc6e7f27618424ea39fcece4375e" => :sierra
+    sha256 "992e14a4dd5ec10a78de48baf03c82c7de8123aacdd3993fc9d2fa82ed52e05f" => :el_capitan
   end
 
   depends_on "python"
