9975991e6d81b62c7df725adf2d356cf15cd82d8
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-02 03:24:40 +0000

    sip: update 4.19.7_2 bottle.

--- Formula/sip.rb
+++ Formula/sip.rb
@@ -9,9 +9,9 @@ class Sip < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0bc10cf64ea6b83f66709db93cf4a6e51b50a7d33e600ac4cb8beca20735f172" => :high_sierra
-    sha256 "ab3d22e71dffb65f4b4fc65a819794d6191ff1bfe09c1c922b7ff46f1caa723e" => :sierra
-    sha256 "de2e5a39e7cb776f2152f017a6d9fc32d6b859c50abbcaa57572b9b4f927bb70" => :el_capitan
+    sha256 "8f9ce1b13ca6d0533b13ff48ffa6fd27cacb20e3e084577b8a15ff4d8d764ef6" => :high_sierra
+    sha256 "f6e229251da69b2b309d48e3b4b45f8baf25f37c1a8c185b9584d4183e6cf884" => :sierra
+    sha256 "1e277a201a52d2f857f03aa7369f548bc5cc4063863b55cb804e3964c2fbc740" => :el_capitan
   end
 
   depends_on "python" => :recommended
