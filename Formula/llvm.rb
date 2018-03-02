a04f32334507a4e4c55dd520c11e214af59038d7
ilovezfs <ilovezfs@icloud.com>
2018-03-01 23:19:14 -0800

    llvm: rename python@2 optional dependency

--- Formula/llvm.rb
+++ Formula/llvm.rb
@@ -134,9 +134,11 @@ class Llvm < Formula
   option "without-libcxx", "Do not build libc++ standard library"
   option "with-toolchain", "Build with Toolchain to facilitate overriding system compiler"
   option "with-lldb", "Build LLDB debugger"
-  option "with-python", "Build bindings against custom Python"
+  option "with-python@2", "Build bindings against Homebrew's Python 2"
   option "with-shared-libs", "Build shared instead of static libraries"
   option "without-libffi", "Do not use libffi to call external functions"
+
+  deprecated_option "with-python" => "with-python@2"
 
   # https://llvm.org/docs/GettingStarted.html#requirement
   depends_on "libffi" => :recommended
@@ -151,9 +153,9 @@ class Llvm < Formula
   end
 
   if MacOS.version <= :snow_leopard
-    depends_on "python"
+    depends_on "python@2"
   else
-    depends_on "python" => :optional
+    depends_on "python@2" => :optional
   end
   depends_on "cmake" => :build
 
@@ -177,8 +179,8 @@ class Llvm < Formula
     # Apple's libstdc++ is too old to build LLVM
     ENV.libcxx if ENV.compiler == :clang
 
-    if build.with? "python"
-      ENV.prepend_path "PATH", Formula["python"].opt_libexec/"bin"
+    if build.with? "python@2"
+      ENV.prepend_path "PATH", Formula["python@2"].opt_libexec/"bin"
     end
 
     (buildpath/"tools/clang").install resource("clang")
@@ -190,7 +192,7 @@ class Llvm < Formula
     (buildpath/"tools/polly").install resource("polly")
 
     if build.with? "lldb"
-      if build.with? "python"
+      if build.with? "python@2"
         pyhome = `python-config --prefix`.chomp
         ENV["PYTHONHOME"] = pyhome
         pylib = "#{pyhome}/lib/libpython2.7.dylib"
