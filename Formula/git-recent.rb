7880fbf0b21a10eb6c8a3685c3f9aaee6749eec0
ilovezfs <ilovezfs@icloud.com>
2018-03-03 05:19:28 -0800

    git-recent 1.0.4 (#24775)

--- Formula/git-recent.rb
+++ Formula/git-recent.rb
@@ -1,10 +1,12 @@
 class GitRecent < Formula
   desc "See your latest local git branches, formatted real fancy"
   homepage "https://github.com/paulirish/git-recent"
-  url "https://github.com/paulirish/git-recent/archive/v1.0.3.tar.gz"
-  sha256 "2ea954f3c1cc3917ad1a0ff5cd361dff7c3f82410bd464a9f5decb0a539155ff"
+  url "https://github.com/paulirish/git-recent/archive/v1.0.4.tar.gz"
+  sha256 "5dcf4a6b4e947b9d9174be58223d14565ad4ddf00b2b00b5830eaa891a8ed868"
 
   bottle :unneeded
+
+  depends_on :macos => :sierra
 
   def install
     bin.install "git-recent"
