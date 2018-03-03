3d8ddaa22c6060dd46b9a1ece5b4fca80af02515
Alyssa Ross <hi@alyssa.is>
2018-03-03 17:40:45 +0000

    geckodriver: add patch for Rust 1.24
    and add a head spec
    
    Closes #24802.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/geckodriver.rb
+++ Formula/geckodriver.rb
@@ -1,8 +1,20 @@
 class Geckodriver < Formula
   desc "WebDriver <-> Marionette proxy"
   homepage "https://github.com/mozilla/geckodriver"
-  url "https://github.com/mozilla/geckodriver/archive/v0.19.1.tar.gz"
-  sha256 "f590ddfef42995a23e781b52befdbc2ac342bf829008e98d212f2e1e15d9f713"
+  head "https://hg.mozilla.org/mozilla-central/", :using => :hg
+
+  stable do
+    url "https://github.com/mozilla/geckodriver/archive/v0.19.1.tar.gz"
+    sha256 "f590ddfef42995a23e781b52befdbc2ac342bf829008e98d212f2e1e15d9f713"
+
+    # Remove for > 0.19.1
+    # Fixes E0277 when building slog with Rust > 1.23
+    # Fixed upstream in https://hg.mozilla.org/mozilla-central/rev/cbd3741a4bb0 on 2018-02-15.
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/e89f1ad/geckodriver/bug-1435830.patch"
+      sha256 "578cdb22803c2f6ee00e8e0b1ca6fcde622c743572aad7038dda0d63cbce4500"
+    end
+  end
 
   bottle do
     sha256 "469b6c642b0029195c94eacf68cc4b8d90a51fcd39f1c6eee7a38b74face0f35" => :high_sierra
@@ -13,7 +25,8 @@ class Geckodriver < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "build"
+    dir = build.head? ? "testing/geckodriver" : "."
+    cd(dir) { system "cargo", "build" }
     bin.install "target/debug/geckodriver"
     bin.install_symlink bin/"geckodriver" => "wires"
   end
