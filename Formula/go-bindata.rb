2d0aaa0caf40d5f67a80d80214fa02735481d909
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-03 18:34:28 +0000

    go-bindata: add 3.7.0 bottle.

--- Formula/go-bindata.rb
+++ Formula/go-bindata.rb
@@ -4,6 +4,13 @@ class GoBindata < Formula
   url "https://github.com/kevinburke/go-bindata/archive/v3.7.0.tar.gz"
   sha256 "28553a1a1490465731484ac3326574f35c8528efc5ca7dcea428f8245f5c83b0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "af5e236506d60e05686858efdb7b4649b7c0e94ac905b2a348883811daed82fe" => :high_sierra
+    sha256 "1e2fdfb1edf62dc81d79b2a382bef3539273aecf7c31dcd2ad25f25ba595691e" => :sierra
+    sha256 "3d517d3e8a614ba91062c05bf171d6f29eab735e92cec4cd647260a49bdab5b5" => :el_capitan
+  end
+
   depends_on "go"
 
   def install
