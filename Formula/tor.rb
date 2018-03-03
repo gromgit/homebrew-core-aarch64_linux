68087c4260fea46418b574f046dd9d04bc773b33
commitay <commitay@users.noreply.github.com>
2018-03-04 00:49:38 +1000

    tor 0.3.2.10
    Closes #24794.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/tor.rb
+++ Formula/tor.rb
@@ -1,9 +1,9 @@
 class Tor < Formula
   desc "Anonymizing overlay network for TCP"
   homepage "https://www.torproject.org/"
-  url "https://www.torproject.org/dist/tor-0.3.2.9.tar.gz"
-  mirror "https://tor.eff.org/dist/tor-0.3.2.9.tar.gz"
-  sha256 "435a7b91aa98d8b1a0ac1f60ca30c0ff3665b18a02e570bab5fe27935829160f"
+  url "https://www.torproject.org/dist/tor-0.3.2.10.tar.gz"
+  mirror "https://tor.eff.org/dist/tor-0.3.2.10.tar.gz"
+  sha256 "60df77c31dcf94fdd686c8ca8c34f3b70243b33a7344ecc0b719d5ca2617cbee"
 
   bottle do
     sha256 "5f19be9003f78463fdb3cae14a48cdd325b28ddb0c3218a804803b4f626129ad" => :high_sierra
