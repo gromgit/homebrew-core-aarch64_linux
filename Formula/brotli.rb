0ff0069a1f91ba8cfae1417b4283984178737775
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-03 08:26:51 +0000

    brotli: update 1.0.3 bottle.

--- Formula/brotli.rb
+++ Formula/brotli.rb
@@ -7,9 +7,9 @@ class Brotli < Formula
 
   bottle do
     cellar :any
-    sha256 "bcb0f43337dc57d0fe955bb60644855711c7659e28260cc22d5d8773b1327fe3" => :high_sierra
-    sha256 "6c883d4ae1b91389b8a3f427bce8a03c6ec75f25792455f8099c5512c849111e" => :sierra
-    sha256 "9d2081d456e4fcef0cff8d961ed090543e28c4f116e204b7154aa744d1bc35a7" => :el_capitan
+    sha256 "5b1ea08d06845a2d11516b5ccab54fa42f9716f72d893ae87920527a044c9ae4" => :high_sierra
+    sha256 "ba9bbd43aa4219408c007166005651568b03da3a4fc6590a5b7e4e45585e537c" => :sierra
+    sha256 "3cdd9cb0eeada350c4047f52007c4cba157b5d0411b3bfa00100cf641696518a" => :el_capitan
   end
 
   depends_on "cmake" => :build
