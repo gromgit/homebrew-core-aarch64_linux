4aa3404cba34fa94c42206e18738f49138704392
ilovezfs <ilovezfs@icloud.com>
2018-03-03 10:28:03 -0800

    go-bindata 3.7.0 (new formula)
    Closes #24801.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/go-bindata.rb
@@ -0,0 +1,22 @@
+class GoBindata < Formula
+  desc "Small utility that generates Go code from any file"
+  homepage "https://github.com/kevinburke/go-bindata"
+  url "https://github.com/kevinburke/go-bindata/archive/v3.7.0.tar.gz"
+  sha256 "28553a1a1490465731484ac3326574f35c8528efc5ca7dcea428f8245f5c83b0"
+
+  depends_on "go"
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/kevinburke").mkpath
+    ln_s buildpath, buildpath/"src/github.com/kevinburke/go-bindata"
+    system "go", "build", "-o", bin/"go-bindata", "./go-bindata"
+  end
+
+  test do
+    (testpath/"data").write "hello world"
+    system bin/"go-bindata", "-o", "data.go", "data"
+    assert_predicate testpath/"data.go", :exist?
+    assert_match '\xff\xff\x85\x11\x4a', (testpath/"data.go").read
+  end
+end
