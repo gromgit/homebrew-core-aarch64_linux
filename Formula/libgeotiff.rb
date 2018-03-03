5f20bf14b733be39907520a4c040ae97beaf3202
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-03 16:26:52 +0000

    libgeotiff: update 1.4.2_2 bottle.

--- Formula/libgeotiff.rb
+++ Formula/libgeotiff.rb
@@ -6,10 +6,9 @@ class Libgeotiff < Formula
   revision 2
 
   bottle do
-    sha256 "47f9a4c29186e4e4b17ea1598b22c85fff045a06c64cbe44a878966f32f5489c" => :high_sierra
-    sha256 "3e497773ae48cb38f7d7bdbaa19f137d23d87b5b1980d3feea2c818680acd145" => :sierra
-    sha256 "97b06d1759717ccfed9b2e21de20d256b8852d472ed2aaa7ce7ec16f260fbe1a" => :el_capitan
-    sha256 "90b680bcf7a45cd17ca3066713bea7c0162811ff5a20804a8afa282bf1c87638" => :yosemite
+    sha256 "5e071647442f998e8239426ee2d9dc7ff2131f02adfdd980b4a702c5316e9c78" => :high_sierra
+    sha256 "590457da69236c82347ee2037aada01123e835ac2169e9b8b7fe7c944319f31e" => :sierra
+    sha256 "590aed30a67b41c1ae71c2b4c93e976bdde6fda6d4dbff698481659ac6b6e32a" => :el_capitan
   end
 
   head do
