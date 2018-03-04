49554b8c5dc145b8ea4ae3151c88fe0dece5c699
Harald Fernengel <harry@kdevelop.org>
2018-03-04 22:31:41 +0100

    arm-linux-gnueabihf-binutils 2.30 (new formula)
    Useful for cross-compiling to e.g. a Raspberry Pi
    
    Closes #25022.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/arm-linux-gnueabihf-binutils.rb
@@ -0,0 +1,25 @@
+class ArmLinuxGnueabihfBinutils < Formula
+  desc "FSF/GNU binutils for cross-compiling to arm-linux"
+  homepage "https://www.gnu.org/software/binutils/binutils.html"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.30.tar.gz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.30.tar.gz"
+  sha256 "8c3850195d1c093d290a716e20ebcaa72eda32abf5e3d8611154b39cff79e9ea"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--enable-deterministic-archives",
+                          "--prefix=#{prefix}",
+                          "--disable-werror",
+                          "--target=arm-linux-gnueabihf",
+                          "--enable-gold=yes",
+                          "--enable-ld=yes",
+                          "--enable-interwork"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    assert_match "f()", shell_output("#{bin}/arm-linux-gnueabihf-c++filt _Z1fv")
+  end
+end
