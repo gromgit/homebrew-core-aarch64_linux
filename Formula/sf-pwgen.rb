805711b2b453a9cf15a061a4324170ad56c4040d
chdiza <chdiza@gmail.com>
2018-03-03 19:33:46 -0500

    sf-pwgen: update homepage (#24805)

--- Formula/sf-pwgen.rb
+++ Formula/sf-pwgen.rb
@@ -1,6 +1,6 @@
 class SfPwgen < Formula
   desc "Generate passwords using SecurityFoundation framework"
-  homepage "https://bitbucket.org/anders/sf-pwgen/"
+  homepage "https://github.com/anders/pwgen/"
   url "https://github.com/anders/pwgen/archive/v1.4.tar.gz"
   sha256 "1f4c7f514426305be2e1b893a586310d579e500e033938800afd2c98fedb84d9"
 
