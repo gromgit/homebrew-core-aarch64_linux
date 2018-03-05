9807c4dc339664601b47cd919d4af3181c8106f3
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-05 10:41:35 +0000

    traefik: update 1.5.3 bottle.

--- Formula/traefik.rb
+++ Formula/traefik.rb
@@ -8,9 +8,10 @@ class Traefik < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a94770c0cfdd82e419e2bdc0672a21aa9a1d592fe76997af9977793d9d628afe" => :high_sierra
-    sha256 "6dda878842677bf5b1fc96b2dc30d3ccca4593def4d7b8f345f59e0424b14a37" => :sierra
-    sha256 "e145a36f35101f8e5ebabeb56fa25b3083a426845b032f341df268506371c6a8" => :el_capitan
+    rebuild 1
+    sha256 "e03833a08674dacb0c498869be4a00f471c046c9d0ec0136a2d6502a0f1eb34f" => :high_sierra
+    sha256 "a419b68a45d103000db77949b1fe8e3e4b59e68e24a4bed412d9ab1b6a27e1ad" => :sierra
+    sha256 "d67ca87eb2d0f7ec2e173ac0f0d89a17752362c88b72cd07c820274f2cd9e49d" => :el_capitan
   end
 
   depends_on "go" => :build
