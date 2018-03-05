cbada0f105fdb9603880afdf6fa013c2edd469fd
commitay <commitay@users.noreply.github.com>
2018-03-05 20:12:49 +1000

    consul: remove go_resources
    Closes #24927.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/consul.rb
+++ Formula/consul.rb
@@ -1,5 +1,3 @@
-require "language/go"
-
 class Consul < Formula
   desc "Tool for service discovery, monitoring and configuration"
   homepage "https://www.consul.io"
@@ -20,36 +18,6 @@ class Consul < Formula
   depends_on "go" => :build
   depends_on "gox" => :build
 
-  go_resource "github.com/axw/gocov" do
-    url "https://github.com/axw/gocov.git",
-        :revision => "3a69a0d2a4ef1f263e2d92b041a69593d6964fe8"
-  end
-
-  go_resource "github.com/elazarl/go-bindata-assetfs" do
-    url "https://github.com/elazarl/go-bindata-assetfs.git",
-        :revision => "30f82fa23fd844bd5bb1e5f216db87fd77b5eb43"
-  end
-
-  go_resource "github.com/hashicorp/go-bindata" do
-    url "https://github.com/hashicorp/go-bindata.git",
-        :revision => "bf7910af899725e4938903fb32048c7c0b15f12e"
-  end
-
-  go_resource "github.com/magiconair/vendorfmt" do
-    url "https://github.com/magiconair/vendorfmt.git",
-        :revision => "0fde667441ebc14dbd64a1de758ab656b78c607b"
-  end
-
-  go_resource "github.com/matm/gocov-html" do
-    url "https://github.com/matm/gocov-html.git",
-        :revision => "f6dd0fd0ebc7c8cff8b24c0a585caeef250627a3"
-  end
-
-  go_resource "golang.org/x/tools" do
-    url "https://go.googlesource.com/tools.git",
-        :branch => "release-branch.go1.9"
-  end
-
   def install
     # Avoid running `go get`
     inreplace "GNUmakefile", "go get -u -v $(GOTOOLS)", ""
@@ -60,24 +28,7 @@ class Consul < Formula
     contents = Dir["{*,.git,.gitignore}"]
     (buildpath/"src/github.com/hashicorp/consul").install contents
 
-    ENV.prepend_create_path "PATH", buildpath/"bin"
-    Language::Go.stage_deps resources, buildpath/"src"
-
-    build_tools = [
-      "github.com/axw/gocov/gocov",
-      "github.com/elazarl/go-bindata-assetfs/go-bindata-assetfs",
-      "github.com/hashicorp/go-bindata/go-bindata",
-      "github.com/magiconair/vendorfmt/cmd/vendorfmt",
-      "github.com/matm/gocov-html",
-      "golang.org/x/tools/cmd/cover",
-      "golang.org/x/tools/cmd/stringer",
-    ]
-
-    build_tools.each do |tool|
-      cd "src/#{tool}" do
-        system "go", "install"
-      end
-    end
+    (buildpath/"bin").mkpath
 
     cd "src/github.com/hashicorp/consul" do
       system "make"
