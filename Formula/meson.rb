0f31a31d1e4ba01cc9d83fe10230059cffdbc9af
ilovezfs <ilovezfs@icloud.com>
2018-03-04 23:18:35 -0800

    meson 0.45.0
    Closes #24843.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/meson.rb
+++ Formula/meson.rb
@@ -1,9 +1,8 @@
 class Meson < Formula
   desc "Fast and user friendly build system"
   homepage "http://mesonbuild.com/"
-  url "https://github.com/mesonbuild/meson/releases/download/0.44.1/meson-0.44.1.tar.gz"
-  sha256 "2ea1a721574adb23160b6481191bcc1173f374e02b0ff3bb0ae85d988d97e4fa"
-  revision 1
+  url "https://github.com/mesonbuild/meson/releases/download/0.45.0/meson-0.45.0.tar.gz"
+  sha256 "3455abbc30a3fbd9cc8abb6d5fcdc42ce618665b95ac2c3ad7792a4a6ba47ce4"
   head "https://github.com/mesonbuild/meson.git"
 
   bottle do
