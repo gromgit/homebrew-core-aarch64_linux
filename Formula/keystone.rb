65c838de79431872ef93fa00ba15f46182b64c07
Viktor Szakats <vszakats@users.noreply.github.com>
2018-03-05 10:23:29 +0000

    keystone: secure url(s)

--- Formula/keystone.rb
+++ Formula/keystone.rb
@@ -1,6 +1,6 @@
 class Keystone < Formula
   desc "Assembler framework: Core + bindings"
-  homepage "http://www.keystone-engine.org"
+  homepage "https://www.keystone-engine.org/"
   url "https://github.com/keystone-engine/keystone/archive/0.9.1.tar.gz"
   sha256 "e9d706cd0c19c49a6524b77db8158449b9c434b415fbf94a073968b68cf8a9f0"
   head "https://github.com/keystone-engine/keystone.git"
