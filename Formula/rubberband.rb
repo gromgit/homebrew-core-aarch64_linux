9974721650b61121f41eb0e4f1155e50dc4f8f30
Viktor Szakats <vszakats@users.noreply.github.com>
2018-03-05 10:23:29 +0000

    rubberband: secure url(s)

--- Formula/rubberband.rb
+++ Formula/rubberband.rb
@@ -1,10 +1,10 @@
 class Rubberband < Formula
   desc "Audio time stretcher tool and library"
-  homepage "http://breakfastquay.com/rubberband/"
+  homepage "https://breakfastquay.com/rubberband/"
   head "https://bitbucket.org/breakfastquay/rubberband/", :using => :hg
 
   stable do
-    url "http://code.breakfastquay.com/attachments/download/34/rubberband-1.8.1.tar.bz2"
+    url "https://breakfastquay.com/files/releases/34/rubberband-1.8.1.tar.bz2"
     mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/r/rubberband/rubberband_1.8.1.orig.tar.bz2"
     sha256 "ff0c63b0b5ce41f937a8a3bc560f27918c5fe0b90c6bc1cb70829b86ada82b75"
 
