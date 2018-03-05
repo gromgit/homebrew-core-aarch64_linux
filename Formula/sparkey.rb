d3d3d746e2633efb17d2de896ccba076e2a2c5d0
ilovezfs <ilovezfs@icloud.com>
2018-03-05 05:52:16 -0800

    sparkey 1.0.0
    Closes #24868.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/sparkey.rb
+++ Formula/sparkey.rb
@@ -1,9 +1,8 @@
 class Sparkey < Formula
   desc "Constant key-value store, best for frequent read/infrequent write uses"
   homepage "https://github.com/spotify/sparkey/"
-  url "https://github.com/spotify/sparkey/archive/sparkey-0.2.0.tar.gz"
-  sha256 "a06caf23c64e7ebae5b8b67272b21ab4c57f21a66d190bfe0a95f5af1dc69154"
-  revision 2
+  url "https://github.com/spotify/sparkey/archive/sparkey-1.0.0.tar.gz"
+  sha256 "d607fb816d71d97badce6301dd56e2538ef2badb6530c0a564b1092788f8f774"
 
   bottle do
     cellar :any
