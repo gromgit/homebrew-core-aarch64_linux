0a5ec7dbccf6de7423725b575ebbbd8b6957c1a8
Viktor Szakats <vszakats@users.noreply.github.com>
2018-03-05 10:23:29 +0000

    tidy-html5: secure url(s)

--- Formula/tidy-html5.rb
+++ Formula/tidy-html5.rb
@@ -1,6 +1,6 @@
 class TidyHtml5 < Formula
   desc "Granddaddy of HTML tools, with support for modern standards"
-  homepage "http://www.html-tidy.org/"
+  homepage "https://www.html-tidy.org/"
   url "https://github.com/htacg/tidy-html5/archive/5.6.0.tar.gz"
   sha256 "08a63bba3d9e7618d1570b4ecd6a7daa83c8e18a41c82455b6308bc11fe34958"
   head "https://github.com/htacg/tidy-html5.git", :branch => "next"
