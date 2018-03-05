ab7b777f6ec3081ab9c40685f1e8257d1d48f408
Tyler Langlois <tjl@byu.net>
2018-03-05 11:41:18 -0800

    metricbeat: generate and install fields.yml into etc/
    use native Makefile env var to install pip wheels from source

--- Formula/metricbeat.rb
+++ Formula/metricbeat.rb
@@ -22,17 +22,33 @@ class Metricbeat < Formula
 
   depends_on "go" => :build
 
+  resource "virtualenv" do
+    url "https://files.pythonhosted.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz"
+    sha256 "02f8102c2436bb03b3ee6dede1919d1dac8a427541652e5ec95171ec8adbc93a"
+  end
+
   def install
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/elastic/beats").install buildpath.children
 
+    ENV.prepend_create_path "PYTHONPATH", buildpath/"vendor/lib/python2.7/site-packages"
+
+    resource("virtualenv").stage do
+      system "python", *Language::Python.setup_install_args(buildpath/"vendor")
+    end
+
+    ENV.prepend_path "PATH", buildpath/"vendor/bin"
+
     cd "src/github.com/elastic/beats/metricbeat" do
       system "make"
+      # prevent downloading binary wheels during python setup
+      system "make", "PIP_INSTALL_COMMANDS=--no-binary :all", "python-env"
+      system "make", "DEV_OS=darwin", "update"
       system "make", "kibana"
       (libexec/"bin").install "metricbeat"
       libexec.install "_meta/kibana"
 
-      (etc/"metricbeat").install Dir["metricbeat*.yml"]
+      (etc/"metricbeat").install Dir["metricbeat*.yml", "fields.yml"]
       prefix.install_metafiles
     end
 
