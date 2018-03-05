4b7c2e4d12c16320c61505474039af5c6f45c83b
Dominyk Tiller <dominyktiller@gmail.com>
2018-03-05 20:55:52 +0000

    getmail: use /usr/bin/python shebangs
    Closes https://github.com/Homebrew/homebrew-core/issues/24875.
    
    Closes #24879.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/getmail.rb
+++ Formula/getmail.rb
@@ -3,6 +3,7 @@ class Getmail < Formula
   homepage "http://pyropus.ca/software/getmail/"
   url "http://pyropus.ca/software/getmail/old-versions/getmail-5.5.tar.gz"
   sha256 "e0382ee59f1ec6ac2d6f01b71ca71db0826db0d267704b2bc2d97b9beda28350"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -13,6 +14,7 @@ class Getmail < Formula
 
   def install
     libexec.install %w[getmail getmail_fetch getmail_maildir getmail_mbox]
+    inreplace Dir[libexec/"*"], %r{^#!/usr/bin/env python$}, "#!/usr/bin/python"
     bin.install_symlink Dir["#{libexec}/*"]
     libexec.install "getmailcore"
     man1.install Dir["docs/*.1"]
