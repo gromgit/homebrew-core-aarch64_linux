e5f703101bfc6af90876d18877ae5f39a05b5a69
Christian Moritz <chrmoritz@gmail.com>
2018-03-05 17:09:27 +0100

    yarn: fix node-gyp after python3 -> python rename
    Fixes: #24869
    
    Closes #24870.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/yarn.rb
+++ Formula/yarn.rb
@@ -3,6 +3,7 @@ class Yarn < Formula
   homepage "https://yarnpkg.com/"
   url "https://yarnpkg.com/downloads/1.5.1/yarn-v1.5.1.tar.gz"
   sha256 "cd31657232cf48d57fdbff55f38bfa058d2fb4950450bd34af72dac796af4de1"
+  revision 1
 
   bottle :unneeded
 
@@ -12,13 +13,14 @@ class Yarn < Formula
 
   def install
     libexec.install Dir["*"]
-    (bin/"yarn").write_env_script "#{libexec}/bin/yarn.js", :PREFIX => HOMEBREW_PREFIX
-    (bin/"yarnpkg").write_env_script "#{libexec}/bin/yarn.js", :PREFIX => HOMEBREW_PREFIX
+    (bin/"yarn").write_env_script "#{libexec}/bin/yarn.js", :PREFIX => HOMEBREW_PREFIX, :NPM_CONFIG_PYTHON => "/usr/bin/python"
+    (bin/"yarnpkg").write_env_script "#{libexec}/bin/yarn.js", :PREFIX => HOMEBREW_PREFIX, :NPM_CONFIG_PYTHON => "/usr/bin/python"
     inreplace "#{libexec}/package.json", '"installationMethod": "tar"', '"installationMethod": "homebrew"'
   end
 
   test do
     (testpath/"package.json").write('{"name": "test"}')
     system bin/"yarn", "add", "jquery"
+    system bin/"yarn", "add", "fsevents", "--build-from-source=true"
   end
 end
