8883b33d8a15c0b11c0cbc6328c9794cd11894bd
ilovezfs <ilovezfs@icloud.com>
2018-03-05 22:26:57 -0800

    kerl 1.8.2 (#24897)

--- Formula/kerl.rb
+++ Formula/kerl.rb
@@ -1,8 +1,8 @@
 class Kerl < Formula
   desc "Easy building and installing of Erlang/OTP instances"
   homepage "https://github.com/kerl/kerl"
-  url "https://github.com/kerl/kerl/archive/1.8.1.tar.gz"
-  sha256 "e03ed489a0e1b2fb8dc00422e8093b1180069585f720b6121a8cbd58d4820fcf"
+  url "https://github.com/kerl/kerl/archive/1.8.2.tar.gz"
+  sha256 "70aba26e75f217cd47f86702018ec66efcc3a6092e60f21bfadf01394325ba7d"
   head "https://github.com/kerl/kerl.git"
 
   bottle :unneeded
