7044beae04f31e587f64cdb0e160592b2c45c776
Sasha Gerrand <github.com-homebrew-core@sgerrand.com>
2018-03-07 13:44:51 +0000

    git-sizer 1.0.0 (new formula)
    Closes #24963.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/git-sizer.rb
@@ -0,0 +1,23 @@
+class GitSizer < Formula
+  desc "Compute various size metrics for a Git repository"
+  homepage "https://github.com/github/git-sizer"
+  url "https://github.com/github/git-sizer/archive/v1.0.0.tar.gz"
+  sha256 "8ef6b2adb646ccb72f5ccfebd19a57371b119da1aac02ef06f1a46af6f7447a0"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/github/git-sizer").install buildpath.children
+    cd "src/github.com/github/git-sizer" do
+      system "go", "build", "-o", bin/"git-sizer"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    system "git", "init"
+    output = shell_output("#{bin}/git-sizer")
+    assert_match "No problems above the current threshold were found", output
+  end
+end
