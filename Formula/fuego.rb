5b4b8c7853c4267cf8d8d7b73cb1164629ddf98a
ilovezfs <ilovezfs@icloud.com>
2018-03-07 01:21:22 -0800

    fuego: revision for boost
    use svn protocol for El Capitan and below
    add a test
    
    Closes #24952.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/fuego.rb
+++ Formula/fuego.rb
@@ -4,9 +4,10 @@ class Fuego < Formula
   if MacOS.version >= :sierra
     url "https://svn.code.sf.net/p/fuego/code/trunk", :revision => 1981
   else
-    url "http://svn.code.sf.net/p/fuego/code/trunk", :revision => 1981
+    url "svn://svn.code.sf.net/p/fuego/code/trunk", :revision => 1981
   end
   version "1.1.SVN"
+  revision 1
 
   head "https://svn.code.sf.net/p/fuego/code/trunk"
 
@@ -28,5 +29,15 @@ class Fuego < Formula
                           "--prefix=#{prefix}",
                           "--with-boost=#{Formula["boost"].opt_prefix}"
     system "make", "install"
+  end
+
+  test do
+    input = <<~EOS
+      genmove white
+      genmove black
+    EOS
+    output = pipe_output("#{bin}/fuego 2>&1", input, 0)
+    assert_match "Forced opening move", output
+    assert_match "maxgames", shell_output("#{bin}/fuego --help")
   end
 end
