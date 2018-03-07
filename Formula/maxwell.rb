5c7e2f5e6d936a9400676d7caffc9583adc2c72c
Shashwat Anand <me@shashwat.me>
2018-03-07 12:31:02 +0530

    maxwell 1.13.2 (#24934)

--- Formula/maxwell.rb
+++ Formula/maxwell.rb
@@ -1,8 +1,8 @@
 class Maxwell < Formula
   desc "Maxwell's daemon, a mysql-to-json kafka producer"
   homepage "http://maxwells-daemon.io/"
-  url "https://github.com/zendesk/maxwell/releases/download/v1.13.1/maxwell-1.13.1.tar.gz"
-  sha256 "3ab7489d47f2ec04677122c8786cf573572b9bbb511b184802d2dcfe82e83781"
+  url "https://github.com/zendesk/maxwell/releases/download/v1.13.2/maxwell-1.13.2.tar.gz"
+  sha256 "db0d4dbf2c091e3f234bf7deb89e5a0397e5b0a49a1c4bdf8dbe6d62d95bb58e"
 
   bottle :unneeded
 
