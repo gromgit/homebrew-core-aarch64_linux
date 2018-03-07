a3011114468b887b36590776d7eae45323ab0641
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-07 23:02:35 +0000

    chronograf: update 1.4.2.2 bottle.

--- Formula/chronograf.rb
+++ Formula/chronograf.rb
@@ -9,9 +9,9 @@ class Chronograf < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "561d5d1dc2a7b9e8a8773ff526a53a1a4a2176007d579be19716718dba5421e3" => :high_sierra
-    sha256 "230b44a76ac298d6c038cbecf3545a76bca9c31c64edaf4b9cef7eb59abe6209" => :sierra
-    sha256 "29395681e61925fd30c16de15dd6f19a29f7285d05eb161f8bd7da7ff48bc6fa" => :el_capitan
+    sha256 "4bdc10662236fee65b8b7c8e7edcd718edf036ec36e055e9e3f8dcadef618298" => :high_sierra
+    sha256 "2ee0222f0e6ffa0b4fe53f1faf738aea28692d95bdb9f8685b912edba35521b4" => :sierra
+    sha256 "d2b07c88b75356d770d4b00f94eaf99f9b6b70e3fc60485168dda4adbc869f2b" => :el_capitan
   end
 
   depends_on "go" => :build
