37e48c7aeaa915c9c0ce82647d33e87689e0b66c
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-08 09:01:58 +0000

    autopep8: add 1.3.4 bottle.

--- Formula/autopep8.rb
+++ Formula/autopep8.rb
@@ -6,6 +6,13 @@ class Autopep8 < Formula
   url "https://github.com/hhatto/autopep8/archive/v1.3.4.tar.gz"
   sha256 "4d6c88bae7f008c70201d0178ed40c3bca9a63a4ad8ecceb69bf8b952dc86a1d"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2ab913aa82b2ea9906a2578c3346aaf0f455320e4b80644112b1b56dfcc90234" => :high_sierra
+    sha256 "e0b3f83d42e8dae3d8acce34fb58a10d05c85ef0df4ef39a7d2392223d43ca89" => :sierra
+    sha256 "3a0c5883c51beb734c5a50e27f5b97cf1baae9f8b33dd516cc56401b3c7999ef" => :el_capitan
+  end
+
   depends_on "python@2" if MacOS.version <= :snow_leopard
 
   def install
