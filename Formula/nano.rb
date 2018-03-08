e095bcf045cee62f441100aa8880445bdd667dc7
ilovezfs <ilovezfs@icloud.com>
2018-03-08 06:27:13 -0800

    nano 2.9.4
    Closes #25015.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/nano.rb
+++ Formula/nano.rb
@@ -1,8 +1,8 @@
 class Nano < Formula
   desc "Free (GNU) replacement for the Pico text editor"
   homepage "https://www.nano-editor.org/"
-  url "https://www.nano-editor.org/dist/v2.9/nano-2.9.3.tar.gz"
-  sha256 "f12058ead9955cb841c1c5e3b9aec6ba93114a807580e928de0eaf6144c91074"
+  url "https://www.nano-editor.org/dist/v2.9/nano-2.9.4.tar.gz"
+  sha256 "f20e57fa2cfbb2c016b82e6dba8ce0804dc4d9a21a931a82020af7466bb7927d"
 
   bottle do
     sha256 "09a16f42318317a39f08350a1e07240067c5268a3ad3046a0aafc0c922d43a36" => :high_sierra
