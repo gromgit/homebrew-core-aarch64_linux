a6f5c18962dd4fab0589587bb3bbe2d6d33237da
Dominyk Tiller <dominyktiller@gmail.com>
2018-03-08 01:03:02 +0000

    gpgme: fix test

--- Formula/gpgme.rb
+++ Formula/gpgme.rb
@@ -31,6 +31,6 @@ class Gpgme < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/gpgme-tool --lib-version")
-    system "python", "-c", "import gpg; print gpg.version.versionstr"
+    system "python2.7", "-c", "import gpg; print gpg.version.versionstr"
   end
 end
