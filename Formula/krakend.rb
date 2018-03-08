35351ca65f7319b47c041030fa5805cbc28b2840
Albert <alombarte@gmail.com>
2018-03-08 18:51:01 +0100

    krakend 0.4.2
    Closes #25021.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/krakend.rb
+++ Formula/krakend.rb
@@ -1,8 +1,8 @@
 class Krakend < Formula
   desc "Ultra-High performance API Gateway built in Go"
   homepage "http://www.krakend.io/"
-  url "https://github.com/devopsfaith/krakend-ce/archive/0.4.1.tar.gz"
-  sha256 "21e4a4f58c0ae2cd0a1629af06a3e8f073885924b48a0932f7aa35d3dd78cec9"
+  url "https://github.com/devopsfaith/krakend-ce/archive/0.4.2.tar.gz"
+  sha256 "5243037a757fbdbb0bf1bbfd5ad8e018cf2aad6ece30e6f8a58c7c24f36b19b1"
 
   bottle do
     cellar :any_skip_relocation
