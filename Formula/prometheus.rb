b898942b85c444f82e146cba22d9bc8760f2775b
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-09 07:29:25 +0000

    prometheus: update 2.2.0 bottle.

--- Formula/prometheus.rb
+++ Formula/prometheus.rb
@@ -6,9 +6,9 @@ class Prometheus < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f9ee5b022c62f57f07fc0e6beea33bcc1b5a8c21e65a5991093219fbcb3ba53c" => :high_sierra
-    sha256 "039f2098e144fec4c0f862627633c846ad3dd3691f4afba53fd6fc361ef03e20" => :sierra
-    sha256 "f9570427fa5e09e34c0c99ec0ffbdd71376cb561d80fc1402e7d9f8fca501ff5" => :el_capitan
+    sha256 "8faced338b6d2dbc10cfafa9a9f134dd1933730395cb8c6b8b74a24f5a941975" => :high_sierra
+    sha256 "2975767a3d41d09006681887dd89f0b7cf1696ff90d5283436530a5a4c8f2771" => :sierra
+    sha256 "efae144da3783630ff7869b115772a23a9d2596b67993b6f17b360cae308e211" => :el_capitan
   end
 
   depends_on "go" => :build
