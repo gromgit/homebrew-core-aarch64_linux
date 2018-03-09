8e24873a924e1b656c479dc63bba6e2ab6aa182e
ilovezfs <ilovezfs@icloud.com>
2018-03-09 09:27:52 -0800

    xonsh: revision for python

--- Formula/xonsh.rb
+++ Formula/xonsh.rb
@@ -5,7 +5,7 @@ class Xonsh < Formula
   homepage "http://xon.sh"
   url "https://github.com/xonsh/xonsh/archive/0.6.0.tar.gz"
   sha256 "7d63d040a6df8749480becab4b3bcb1c6589458bad272d5de06c6a063c06c5f1"
-  revision 2
+  revision 3
   head "https://github.com/scopatz/xonsh.git"
 
   bottle do
