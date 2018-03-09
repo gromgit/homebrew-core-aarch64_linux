9e6a200608e98d0054a3c4843c9ba1396fae8960
commitay <commitay@users.noreply.github.com>
2018-03-09 22:43:38 +1000

    masscan 1.0.5
    Closes #25058.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/masscan.rb
+++ Formula/masscan.rb
@@ -1,8 +1,8 @@
 class Masscan < Formula
   desc "TCP port scanner, scans entire Internet in under 5 minutes"
   homepage "https://github.com/robertdavidgraham/masscan/"
-  url "https://github.com/robertdavidgraham/masscan/archive/1.0.4.tar.gz"
-  sha256 "51de345f677f46595fc3bd747bfb61bc9ff130adcbec48f3401f8057c8702af9"
+  url "https://github.com/robertdavidgraham/masscan/archive/1.0.5.tar.gz"
+  sha256 "a0686929888674892f464014806444d26ded56838d45035221ff88ee9f6ead73"
   head "https://github.com/robertdavidgraham/masscan.git"
 
   bottle do
@@ -11,13 +11,6 @@ class Masscan < Formula
     sha256 "d2770cd3ff575b998818f5586f182fe626bc336a48542d1c01656fd9617df1ed" => :sierra
     sha256 "8dbe578c48b421e03a264c82912e10009b1b6163f704f3388a017799accaf0a9" => :el_capitan
     sha256 "969e348d0a3738b1fcc5082a6c0feef0f18d1f462b3d9bec0cd1751781b263e3" => :yosemite
-  end
-
-  if DevelopmentTools.clang_build_version >= 900
-    patch do
-      url "https://github.com/robertdavidgraham/masscan/pull/282.patch?full_index=1"
-      sha256 "0daa190200f5cf3e11e9e1c29ea65e7e8e8c0b13fbeccf9a2319cb166234d684"
-    end
   end
 
   def install
