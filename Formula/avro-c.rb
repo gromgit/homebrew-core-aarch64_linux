0beba1d2aaabbd95d376c100fdaa279dc7322596
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-10 21:27:37 +0000

    avro-c: update 1.8.2 bottle.

--- Formula/avro-c.rb
+++ Formula/avro-c.rb
@@ -5,11 +5,10 @@ class AvroC < Formula
   sha256 "4639982b2b8fbd91fc7128fef672207129c959bb7900dd64b077ce4206edf10e"
 
   bottle do
-    cellar :any
-    sha256 "7f3b7576204b4741e355ecb75ffc7cf8624a1d3d5b9b4763914b350dc44a4035" => :high_sierra
-    sha256 "babc65c0442620b00470abdda6d86d7035390b75b6ae113319c69df1fc24dc8b" => :sierra
-    sha256 "52ba34bf67cee89d559bd075baff35e7be512b5f53419783a82c1f0cf2ca3f8a" => :el_capitan
-    sha256 "f77867d708068930b40fb0e3e4e5bd5146487699599e87d4987c205fb6a3275d" => :yosemite
+    rebuild 1
+    sha256 "6f4d948dcf0e1700b7a770f9bb8612f5495ff0c5c01c9f0ee3a73f3d3c58165e" => :high_sierra
+    sha256 "b1340938b27551d73c1343d5323b9aa1a42a173fa10e4a7db9445e11e84fdc54" => :sierra
+    sha256 "8358b34088a77ba07d32505350e5b119274ed69f3adf1016cd1fbf0b15034227" => :el_capitan
   end
 
   option "with-snappy", "Build with Snappy codec support"
