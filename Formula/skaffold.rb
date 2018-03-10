6d2a793d73bf38acfbce47293fdb0e49bdac5c2c
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-10 16:57:13 +0000

    skaffold: add 0.1.0 bottle.

--- Formula/skaffold.rb
+++ Formula/skaffold.rb
@@ -5,6 +5,13 @@ class Skaffold < Formula
       :tag => "v0.1.0",
       :revision => "e265bb780f63894f649b8fb1ac53f8aaf89573b2"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "68a77f550ceb7ea5f4e4ef8f34601b292f7e1763ac7b7a18ef5dea2405dd831c" => :high_sierra
+    sha256 "9bda4e0fb918af6852642dfa15926d77519135269c4ea029e3f672bb8261180d" => :sierra
+    sha256 "21ca4166024ce968cf365deba3ea786e7815531752dc6403dddd007c29effde4" => :el_capitan
+  end
+
   depends_on "go" => :build
 
   def install
