4002fe991339e653741fef5c141a4e67ee08b76b
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-03-11 07:04:49 +0000

    gtksourceview@4 4.0.0 (new formula)
    Closes #25094.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/gtksourceview@4.rb
@@ -0,0 +1,82 @@
+class GtksourceviewAT4 < Formula
+  desc "Text view with syntax, undo/redo, and text marks"
+  homepage "https://projects.gnome.org/gtksourceview/"
+  url "https://download.gnome.org/sources/gtksourceview/4.0/gtksourceview-4.0.0.tar.xz"
+  sha256 "6e5c7a28a7fa456a89f289f0659c57100cc09fe692db6abfcc23f7ade5d5b32a"
+
+  depends_on "intltool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
+  depends_on "gtk+3"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <gtksourceview/gtksource.h>
+
+      int main(int argc, char *argv[]) {
+        gchar *text = gtk_source_utils_unescape_search_text("hello world");
+        return 0;
+      }
+    EOS
+    ENV.libxml2
+    atk = Formula["atk"]
+    cairo = Formula["cairo"]
+    fontconfig = Formula["fontconfig"]
+    freetype = Formula["freetype"]
+    gdk_pixbuf = Formula["gdk-pixbuf"]
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    gtkx3 = Formula["gtk+3"]
+    libepoxy = Formula["libepoxy"]
+    libpng = Formula["libpng"]
+    pango = Formula["pango"]
+    pixman = Formula["pixman"]
+    flags = %W[
+      -I#{atk.opt_include}/atk-1.0
+      -I#{cairo.opt_include}/cairo
+      -I#{fontconfig.opt_include}
+      -I#{freetype.opt_include}/freetype2
+      -I#{gdk_pixbuf.opt_include}/gdk-pixbuf-2.0
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/gio-unix-2.0/
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{gtkx3.opt_include}/gtk-3.0
+      -I#{include}/gtksourceview-4
+      -I#{libepoxy.opt_include}
+      -I#{libpng.opt_include}/libpng16
+      -I#{pango.opt_include}/pango-1.0
+      -I#{pixman.opt_include}/pixman-1
+      -D_REENTRANT
+      -L#{atk.opt_lib}
+      -L#{cairo.opt_lib}
+      -L#{gdk_pixbuf.opt_lib}
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{gtkx3.opt_lib}
+      -L#{lib}
+      -L#{pango.opt_lib}
+      -latk-1.0
+      -lcairo
+      -lcairo-gobject
+      -lgdk-3
+      -lgdk_pixbuf-2.0
+      -lgio-2.0
+      -lglib-2.0
+      -lgobject-2.0
+      -lgtk-3
+      -lgtksourceview-4.0
+      -lintl
+      -lpango-1.0
+      -lpangocairo-1.0
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
