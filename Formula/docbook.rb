b6e63ae884ccfb67803122c8321dbdb1f5a239f1
Viktor Szakats <vszakats@users.noreply.github.com>
2018-03-11 22:21:36 +0000

    docbook: secure url(s)

--- Formula/docbook.rb
+++ Formula/docbook.rb
@@ -1,7 +1,7 @@
 class Docbook < Formula
   desc "Standard SGML representation system for technical documents"
-  homepage "https://docbook.sourceforge.io/"
-  url "http://www.docbook.org/xml/5.0/docbook-5.0.zip"
+  homepage "https://docbook.org/"
+  url "https://docbook.org/xml/5.0/docbook-5.0.zip"
   sha256 "3dcd65e1f5d9c0c891b3be204fa2bb418ce485d32310e1ca052e81d36623208e"
 
   bottle do
@@ -15,33 +15,33 @@ class Docbook < Formula
   end
 
   resource "xml412" do
-    url "http://www.docbook.org/xml/4.1.2/docbkx412.zip"
+    url "https://docbook.org/xml/4.1.2/docbkx412.zip"
     sha256 "30f0644064e0ea71751438251940b1431f46acada814a062870f486c772e7772"
     version "4.1.2"
   end
 
   resource "xml42" do
-    url "http://www.docbook.org/xml/4.2/docbook-xml-4.2.zip"
+    url "https://docbook.org/xml/4.2/docbook-xml-4.2.zip"
     sha256 "acc4601e4f97a196076b7e64b368d9248b07c7abf26b34a02cca40eeebe60fa2"
   end
 
   resource "xml43" do
-    url "http://www.docbook.org/xml/4.3/docbook-xml-4.3.zip"
+    url "https://docbook.org/xml/4.3/docbook-xml-4.3.zip"
     sha256 "23068a94ea6fd484b004c5a73ec36a66aa47ea8f0d6b62cc1695931f5c143464"
   end
 
   resource "xml44" do
-    url "http://www.docbook.org/xml/4.4/docbook-xml-4.4.zip"
+    url "https://docbook.org/xml/4.4/docbook-xml-4.4.zip"
     sha256 "02f159eb88c4254d95e831c51c144b1863b216d909b5ff45743a1ce6f5273090"
   end
 
   resource "xml45" do
-    url "http://www.docbook.org/xml/4.5/docbook-xml-4.5.zip"
+    url "https://docbook.org/xml/4.5/docbook-xml-4.5.zip"
     sha256 "4e4e037a2b83c98c6c94818390d4bdd3f6e10f6ec62dd79188594e26190dc7b4"
   end
 
   resource "xml50" do
-    url "http://www.docbook.org/xml/5.0/docbook-5.0.zip"
+    url "https://docbook.org/xml/5.0/docbook-5.0.zip"
     sha256 "3dcd65e1f5d9c0c891b3be204fa2bb418ce485d32310e1ca052e81d36623208e"
   end
 
