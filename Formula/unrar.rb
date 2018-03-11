d3428f40e849450225b6262738b8695ffc7d3dd4
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-11 10:05:35 +0000

    unrar: update 5.6.1 bottle.

--- Formula/unrar.rb
+++ Formula/unrar.rb
@@ -6,10 +6,9 @@ class Unrar < Formula
 
   bottle do
     cellar :any
-    sha256 "ba87a2debd14ee02ebba3a2a9c687b0e64920651eab21d0ce305829cc02c1e1f" => :high_sierra
-    sha256 "fb54a22a18eb4e992fa5bec6a669f97a764919a5b78dce65407b6943d2bf0ebe" => :sierra
-    sha256 "82adba13791514beb06b9a7d659eb00a8c4f8057c5c9edd3df7ea8b085586372" => :el_capitan
-    sha256 "5e50649d2b6c717b36447f08d3dc1ce2d72da02603c6274327a24900030b6f5f" => :yosemite
+    sha256 "3547c62bfd6288e535e2e86c77e25cb98447ba6275935969c3883a1235a232c0" => :high_sierra
+    sha256 "ad895095304beab625f14f957b1b2b8f71abb5373e90b93ebd0fa5a9070b2d47" => :sierra
+    sha256 "66b7c4deff70612deb93e704d87c5002d56b132b07571dea0c48aae3be138183" => :el_capitan
   end
 
   def install
