40222a9f0f7a07a4e7a4ec0daa6fb2b43348a4f4
ilovezfs <ilovezfs@icloud.com>
2018-03-11 04:24:21 -0700

    druid 0.12.0 (#25039)

--- Formula/druid.rb
+++ Formula/druid.rb
@@ -1,8 +1,8 @@
 class Druid < Formula
   desc "High-performance, column-oriented, distributed data store"
   homepage "http://druid.io"
-  url "http://static.druid.io/artifacts/releases/druid-0.11.0-bin.tar.gz"
-  sha256 "d50218d11f97612acc1447914e113f1869997640de586daabd85828440c4c546"
+  url "http://static.druid.io/artifacts/releases/druid-0.12.0-bin.tar.gz"
+  sha256 "eab3ff44745966c81f9cd0ee46798b4d20489ec0aff7e75019003f4e5ee8ec87"
 
   bottle :unneeded
 
@@ -12,8 +12,8 @@ class Druid < Formula
   depends_on :java => "1.8"
 
   resource "mysql-metadata-storage" do
-    url "http://static.druid.io/artifacts/releases/mysql-metadata-storage-0.11.0.tar.gz"
-    sha256 "13fd978cdcfa7f9bac715642f37ddc8bdaf572ce05a2326c455ab8284d457141"
+    url "http://static.druid.io/artifacts/releases/mysql-metadata-storage-0.12.0.tar.gz"
+    sha256 "dbf9baddae13acf72d12bbcf7d8ac4b06a885b2f70d1b742a1505716792371fd"
   end
 
   def install
@@ -30,7 +30,7 @@ class Druid < Formula
     end
 
     inreplace libexec/"bin/node.sh" do |s|
-      s.gsub! "nohup java", "nohup java -Ddruid.extensions.directory=\"#{libexec}/extensions\""
+      s.gsub! "nohup $JAVA", "nohup $JAVA -Ddruid.extensions.directory=\"#{libexec}/extensions\""
       s.gsub! ":=lib", ":=#{libexec}/lib"
       s.gsub! ":=conf/druid", ":=#{libexec}/conf/druid"
       s.gsub! ":=log", ":=#{var}/druid/log"
