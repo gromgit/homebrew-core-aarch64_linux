537a89ad4325178aa67e9b996442afe60f276a5b
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-11 11:26:26 +0000

    tj: add 6.0.2 bottle.

--- Formula/tj.rb
+++ Formula/tj.rb
@@ -4,6 +4,13 @@ class Tj < Formula
   url "https://github.com/sgreben/tj/archive/6.0.2.tar.gz"
   sha256 "c667f8fb09839a677bd6b3d6c06103bce59ffb2e33ec1bd7b49abfc00f02f5dd"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "05fa676f8b78bdeb2b782d696dd0e7b0dc1aafaf4597f1bc259fc8ccd3b3f168" => :high_sierra
+    sha256 "26edd5e1c71f587c3aa9be749e2d18852cde5ad406c91a9402ecba7cf3ada798" => :sierra
+    sha256 "37a1cc78f74344b31c8e85dc32213f42b9bd43bb5cc015afee749254042906fa" => :el_capitan
+  end
+
   depends_on "go" => :build
 
   def install
