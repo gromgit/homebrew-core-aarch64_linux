5836b1ee5371cb4daf2dc74fb798e9a3d8d15bdc
FX Coudert <fxcoudert@gmail.com>
2018-03-13 09:42:38 +0100

    ntp 4.2.8p11
    Closes #25213.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/ntp.rb
+++ Formula/ntp.rb
@@ -1,19 +1,14 @@
 class Ntp < Formula
   desc "The Network Time Protocol (NTP) Distribution"
   homepage "https://www.eecis.udel.edu/~mills/ntp/html/"
-  url "https://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/ntp-4.2.8p10.tar.gz"
-  version "4.2.8p10"
-  sha256 "ddd2366e64219b9efa0f7438e06800d0db394ac5c88e13c17b70d0dcdf99b99f"
+  url "https://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/ntp-4.2.8p11.tar.gz"
+  version "4.2.8p11"
+  sha256 "f14a39f753688252d683ff907035ffff106ba8d3db21309b742e09b5c3cd278e"
 
   bottle do
     sha256 "5bda263067c4c0fe391c49734680e3f682f8c64672cf0628344db78df6cfa371" => :sierra
     sha256 "eca0868f7a11c02bd5007602a534257fbdaa06aca5348e32cd06264728569e14" => :el_capitan
     sha256 "766d2595c081694b7da0e5cfa6818e60483329f5ec7e2f1bf179f180735bbdc2" => :yosemite
-  end
-
-  devel do
-    url "https://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-dev/ntp-dev-4.3.93.tar.gz"
-    sha256 "a07e73d7a3ff139bba33ee4b1110d5f3f4567465505d6317c9b50eefb9720c42"
   end
 
   option "with-net-snmp", "Build ntpsnmpd, the SNMP MIB agent for ntpd"
@@ -22,8 +17,6 @@ class Ntp < Formula
   depends_on "net-snmp" => :optional
 
   def install
-    system "./bootstrap" if build.head?
-
     args = [
       "--disable-debug",
       "--disable-dependency-tracking",
@@ -39,7 +32,7 @@ class Ntp < Formula
     end
 
     system "./configure", *args
-    system "make", "install"
+    system "make", "install", "LDADD_LIBNTP=-lresolv -undefined dynamic_lookup"
   end
 
   test do
