1099963d39a861734f50a77faff63b5030c8b897
Zhiming Wang <zmwangx@gmail.com>
2018-03-13 13:07:17 -0400

    bcal 1.8 (new formula)
    Closes #25235.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/bcal.rb
@@ -0,0 +1,14 @@
+class Bcal < Formula
+  desc "Storage conversion and expression calculator"
+  homepage "https://github.com/jarun/bcal"
+  url "https://github.com/jarun/bcal/archive/v1.8.tar.gz"
+  sha256 "2bdf9057456e30e97ace3becbdbf7642bbc1689668e8aaad2ef6bfe9c4de9603"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    assert_match "9333353817", shell_output("#{bin}/bcal '56 gb / 6 + 4kib * 5 + 4 B'")
+  end
+end
