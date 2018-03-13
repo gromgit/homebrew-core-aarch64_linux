6c28d2c1511c98348105c29edf95a7216cadf362
ilovezfs <ilovezfs@icloud.com>
2018-03-12 21:54:48 -0700

    vim 8.0.1600
    Closes #25203.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -2,9 +2,8 @@ class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/v8.0.1553.tar.gz"
-  sha256 "c7d6e4b44be07601deedda747dd26cced2d6b8d36e109ce0cf7a134addaf3cf8"
-  revision 2
+  url "https://github.com/vim/vim/archive/v8.0.1600.tar.gz"
+  sha256 "a81b475fe8a6044a39f5333ab1e48629979e3327cfbe8124783b65a3335c6caf"
   head "https://github.com/vim/vim.git"
 
   bottle do
