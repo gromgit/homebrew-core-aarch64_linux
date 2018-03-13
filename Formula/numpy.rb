180d8af042835a3eafebcb03d298eae919b03f13
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-13 05:55:54 +0000

    numpy: update 1.14.2 bottle.

--- Formula/numpy.rb
+++ Formula/numpy.rb
@@ -5,9 +5,9 @@ class Numpy < Formula
   sha256 "facc6f925c3099ac01a1f03758100772560a0b020fb9d70f210404be08006bcb"
 
   bottle do
-    sha256 "60b6d4a20734d849394a15744341aa2919d61564d76878443a2b7dd98733abaf" => :high_sierra
-    sha256 "fa8a59395067c36d569f24d3a6f4834cdac453bd83476d722b6c1f33e8357b8a" => :sierra
-    sha256 "31c1ffd4ed481190a489049c874949cbde0d566e8603315b6c5eb863a876f4f5" => :el_capitan
+    sha256 "953c10a547b9af36436e7d31297fe2a15be912e3ae6bd70634957f9935bf6a8e" => :high_sierra
+    sha256 "aa3a48369f9869f17cf44fb2f87a49bb76e0adf04fb5cee52a5d5d18e938b999" => :sierra
+    sha256 "c22afeed38791bce599ff2b1c4263a90ed2ec08177396171e879ab4c70f3c3fb" => :el_capitan
   end
 
   head do
