61ec4e025a62676862bed86639d859a715cb27dd
ilovezfs <ilovezfs@icloud.com>
2018-03-12 21:41:24 -0700

    groovyserv 1.2.0
    remove head spec
    remove gradle build-time dependency
    require Java 8 specifically
    
    Closes #25189.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/groovyserv.rb
+++ Formula/groovyserv.rb
@@ -1,9 +1,8 @@
 class Groovyserv < Formula
   desc "Speed up Groovy startup time"
   homepage "https://kobo.github.io/groovyserv/"
-  url "https://bitbucket.org/kobo/groovyserv-mirror/downloads/groovyserv-1.1.0-src.zip"
-  sha256 "4d425c606fba54c6a9e3babc79f178de470fc3d5479a1561d947ca7045b40544"
-  head "https://github.com/kobo/groovyserv.git"
+  url "https://bitbucket.org/kobo/groovyserv-mirror/downloads/groovyserv-1.2.0-src.zip"
+  sha256 "235b38c6bb70721fa41b2c2cc6224eeaac09721e4d04b504148b83c40ea0bb27"
 
   bottle do
     cellar :any_skip_relocation
@@ -15,22 +14,17 @@ class Groovyserv < Formula
   end
 
   depends_on "go" => :build
-  depends_on "gradle" => :build
   depends_on "groovy"
+  depends_on :java => "1.8"
 
   def install
     # Sandbox fix to stop it ignoring our temporary $HOME variable.
     ENV["GRADLE_USER_HOME"] = buildpath/".brew_home"
-    system "gradle", "clean", "executables"
-
-    # Install executables in libexec to avoid conflicts
-    libexec.install Dir["build/executables/{bin,lib}"]
-
-    # Remove windows files
-    rm_f Dir["#{libexec}/bin/*.bat"]
-
-    # Symlink binaries except _common.sh
-    bin.install_symlink Dir["#{libexec}/bin/g*"]
+    system "./gradlew", "clean", "distLocalBin"
+    system "unzip", "build/distributions/groovyserv-#{version}-bin-local.zip"
+    libexec.install Dir["groovyserv-#{version}/{bin,lib}"]
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
   end
 
   test do
