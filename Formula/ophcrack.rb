5c1f34ce5de085b5d8e75339fe44079fa5460e62
ilovezfs <ilovezfs@icloud.com>
2018-03-12 21:46:59 -0700

    ophcrack 3.8.0
    Closes #25195.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/ophcrack.rb
+++ Formula/ophcrack.rb
@@ -1,9 +1,9 @@
 class Ophcrack < Formula
   desc "Microsoft Windows password cracker using rainbow tables"
   homepage "https://ophcrack.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/ophcrack/ophcrack/3.7.0/ophcrack-3.7.0.tar.bz2"
-  mirror "https://mirrors.kernel.org/debian/pool/main/o/ophcrack/ophcrack_3.7.0.orig.tar.bz2"
-  sha256 "7c28fd7dbb9c9e176ea51b48f826abe122022bbf5568d8ab3a2066fc2876b9b4"
+  url "https://downloads.sourceforge.net/project/ophcrack/ophcrack/3.8.0/ophcrack-3.8.0.tar.bz2"
+  mirror "https://mirrors.kernel.org/debian/pool/main/o/ophcrack/ophcrack_3.8.0.orig.tar.bz2"
+  sha256 "048a6df57983a3a5a31ac7c4ec12df16aa49e652a29676d93d4ef959d50aeee0"
 
   bottle do
     cellar :any
