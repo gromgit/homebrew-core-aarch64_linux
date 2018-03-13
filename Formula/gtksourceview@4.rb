4ff855fed86c7c8bb860d3add92dd3c137381ce8
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-13 12:54:33 +0000

    gtksourceview@4: add 4.0.0 bottle.

--- Formula/gtksourceview@4.rb
+++ Formula/gtksourceview@4.rb
@@ -4,6 +4,12 @@ class GtksourceviewAT4 < Formula
   url "https://download.gnome.org/sources/gtksourceview/4.0/gtksourceview-4.0.0.tar.xz"
   sha256 "6e5c7a28a7fa456a89f289f0659c57100cc09fe692db6abfcc23f7ade5d5b32a"
 
+  bottle do
+    sha256 "fe95c7508b0fbd9ff7af99709ee022d6cc8b0de456c923b77a9cc5436a0d98a3" => :high_sierra
+    sha256 "d5038b1aee6f8104b3f566d6af1bbb06cb6201f532b2b1df2364835fec119a9b" => :sierra
+    sha256 "40880b1821a43ec79dad4af509248b36c57e446420f193204935d2415fd38c0f" => :el_capitan
+  end
+
   depends_on "intltool" => :build
   depends_on "pkg-config" => :build
   depends_on "gettext"
