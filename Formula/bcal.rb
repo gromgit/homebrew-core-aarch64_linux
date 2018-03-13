1b411bfb0b293992d3061e80f6a97ae238d558d4
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-13 17:12:27 +0000

    bcal: add 1.8 bottle.

--- Formula/bcal.rb
+++ Formula/bcal.rb
@@ -4,6 +4,13 @@ class Bcal < Formula
   url "https://github.com/jarun/bcal/archive/v1.8.tar.gz"
   sha256 "2bdf9057456e30e97ace3becbdbf7642bbc1689668e8aaad2ef6bfe9c4de9603"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "72ceaeadbac4af0bb864ba38664fa348bda14ea4083c07a13ecd219466450b12" => :high_sierra
+    sha256 "48310c1a3421d0d94dc205927b5f36daa5eae939b337e110630a03743feb76ad" => :sierra
+    sha256 "a65d60bce6a1b902a84e3840ea74228be5c9ffe0e1b9fc2c3ed6aa5a3907d0c7" => :el_capitan
+  end
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
