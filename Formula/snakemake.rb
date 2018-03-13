acfcdce80a53b483f6f17b8d738c7d20d9e47e28
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-13 20:03:37 +0000

    snakemake: update 4.8.0 bottle.

--- Formula/snakemake.rb
+++ Formula/snakemake.rb
@@ -7,9 +7,9 @@ class Snakemake < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f73b9f6185fa34289db7081bcead201a49dee7623d03fdde7fe219c61b4c1ab8" => :high_sierra
-    sha256 "ccb841b30d6da65e3f61f46dd1dc11bee572e8e5ae746ca32b8fe5908c67ea82" => :sierra
-    sha256 "ccd3904155e9619fb7d63448aeeee6f1db3a6675600b238c8e73091e5d5ca7b2" => :el_capitan
+    sha256 "8029643099c871a5a9bc3f705fc6401823b197254ce782ac786cf77fcc9a0ac5" => :high_sierra
+    sha256 "e530b25b3138caa52e3191463555cf46c6daf3013ed233665a1479bb311d0818" => :sierra
+    sha256 "da8ff375d0a76b454fea294959ec5ec07e31fe6fdcb87b97879b0197232db056" => :el_capitan
   end
 
   depends_on "python"
