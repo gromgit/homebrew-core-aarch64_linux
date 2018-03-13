39cb721f1e7942f49b2d1d6f34e527d7469ba456
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-13 05:18:48 +0000

    gnu-time: update 1.9 bottle.

--- Formula/gnu-time.rb
+++ Formula/gnu-time.rb
@@ -7,9 +7,9 @@ class GnuTime < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b3763d0f3f85e55ce1e9dabaafba0bb4e5af5706b0184dddb96e5c888310c494" => :high_sierra
-    sha256 "96e4ff64f2754423ef0e333457097c025ce77f2f8103ff1d6eb4c7e40e3e0281" => :sierra
-    sha256 "37ee681d846bb5d11a31c7d53ee877ea3d30b0946775491c7952ea59a50f3293" => :el_capitan
+    sha256 "7be7fa7161f1c4256e7fd0427cc70bcb9942516cf087e3a4ce8bf25a8e9bda0e" => :high_sierra
+    sha256 "4340f98cd4edd0512a7462ad52180a3dd4bd282fce7c53cadfe56c45f5367095" => :sierra
+    sha256 "1f81b3521747ff00d913ac9f4c7e7d9ce8f19dd76a67c7d2b05d5dd228561883" => :el_capitan
   end
 
   option "with-default-names", "Do not prepend 'g' to the binary"
