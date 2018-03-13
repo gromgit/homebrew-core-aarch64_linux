fbea0c8acd8fd3d4e72405d244fa1c525c8c03a7
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-13 09:52:00 +0000

    ht: update 2.1.0 bottle.

--- Formula/ht.rb
+++ Formula/ht.rb
@@ -6,11 +6,10 @@ class Ht < Formula
 
   bottle do
     cellar :any
-    sha256 "122be4ebf9cc683744d3523fb87b4938de3a61ec4151005118c014b1f279939b" => :sierra
-    sha256 "5ec664d44de232a82b94ba1a2285f4c4a3b935b60f96ec83b75150e583323331" => :el_capitan
-    sha256 "7705bf13e35efb021252aaf509e82a88f8996ad65199440eb46f2271bf77a570" => :yosemite
-    sha256 "f41ba188255f61cd07be25969bce0a9667e81dfde9079ba17e7e329898cafed5" => :mavericks
-    sha256 "dac5a797ccbeb46b89c7677997a3640b5ba5ce274c5f928bde9be91dbcbdd88a" => :mountain_lion
+    rebuild 1
+    sha256 "8c604066c63fa1eba3bb547626bbc280ea4446bb2961cb54e8b4fc7b829af5c4" => :high_sierra
+    sha256 "197a62339202dd45529bbf42b67addc35939dbae43cc9704ff15d75e5ad62d01" => :sierra
+    sha256 "4556713b40bfd3846c7c03a02c174bff2a771fba4084721b6faed88437c3c1a2" => :el_capitan
   end
 
   depends_on "lzo"
