94d51012086bd4c4f7fc4c35789b3b7e39652709
Jeff Fowler <jeffowler@gmail.com>
2018-03-14 21:51:03 +0100

    zig 0.2.0
    Closes #25320.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/zig.rb
+++ Formula/zig.rb
@@ -1,9 +1,8 @@
 class Zig < Formula
   desc "Programming language designed for robustness, optimality, and clarity"
   homepage "https://ziglang.org/"
-  url "https://github.com/zig-lang/zig/archive/0.1.1.tar.gz"
-  sha256 "fabbfcb0bdb08539d9d8e8e1801d20f25cb0025af75ac996f626bb5e528e71f1"
-  revision 1
+  url "https://github.com/zig-lang/zig/archive/0.2.0.tar.gz"
+  sha256 "09843a3748bf8a5f1742fe93dbf45699f92051ecf479b23272b067dfc3837cc7"
 
   bottle do
     sha256 "3c2e3f738f28287a1fc68ac80d6bf4b0b047a51cc54d0239cd0e4c8a65b47714" => :high_sierra
@@ -12,7 +11,7 @@ class Zig < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "llvm@5"
+  depends_on "llvm"
 
   def install
     system "cmake", ".", *std_cmake_args
@@ -21,9 +20,10 @@ class Zig < Formula
 
   test do
     (testpath/"hello.zig").write <<~EOS
-      const io = @import("std").io;
-      pub fn main() -> %void {
-          %%io.stdout.printf("Hello, world!");
+      const std = @import("std");
+      pub fn main() !void {
+          var stdout_file = try std.io.getStdOut();
+          try stdout_file.write("Hello, world!");
       }
     EOS
     system "#{bin}/zig", "build-exe", "hello.zig"
