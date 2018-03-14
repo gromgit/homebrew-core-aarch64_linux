f070cf95993696a583c0f886a056769a6f9c8513
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-14 21:49:50 +0000

    duck: update 6.4.4.27722 bottle.

--- Formula/duck.rb
+++ Formula/duck.rb
@@ -7,9 +7,9 @@ class Duck < Formula
   head "https://svn.cyberduck.io/trunk/"
 
   bottle do
-    sha256 "75ab35638f01ac012a9e6e49f3b74b2940d0a102bf935896bea6c9a3e7605cb6" => :high_sierra
-    sha256 "f763c28c678876785f073507f35e49e4bd8a8cd6f9dda7b4ac56daeba9494c31" => :sierra
-    sha256 "baa7c7f7065c07b7b5f4915e7e9758ecf78ab0f17a0a3050b7eb82d1d9f80d93" => :el_capitan
+    sha256 "b6c346e117e400798bc1b1002ee764530f2d279619df2410730731993a3314cf" => :high_sierra
+    sha256 "ae102c755dcc3ee79fb1e3bc1179caaf329894f3fb67f324b1ab8176f1b05b55" => :sierra
+    sha256 "a833e9cc89c3785fda774e76774ff6dfe334731f4653fc06ccd8dfab5c99e6f9" => :el_capitan
   end
 
   depends_on :java => ["1.8+", :build]
