6fc441d4a42e37b149efd7911839893868d2c11b
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-14 19:31:11 +0000

    defaultbrowser: add 1.1 bottle.

--- Formula/defaultbrowser.rb
+++ Formula/defaultbrowser.rb
@@ -4,6 +4,13 @@ class Defaultbrowser < Formula
   url "https://github.com/kerma/defaultbrowser/archive/1.1.tar.gz"
   sha256 "56249f05da912bbe828153d775dc4f497f5a8b453210c2788d6a439418ac2ea3"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d07cc35e06f440584d698cde7ae5f27b09acfeafacc1499d331aa8553e945961" => :high_sierra
+    sha256 "d0279f8c05c0c7828c534517897346fc231864ea8534f0ae04878ecfd51ca72d" => :sierra
+    sha256 "f0ccf84abbd31469f80c4d232292dd280a978d3f04a1a6db46079902d9821d1e" => :el_capitan
+  end
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
