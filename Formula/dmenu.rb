57e76398a8d54a04da75c6278924f548f0ccebe4
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-15 05:41:32 +0000

    dmenu: update 4.8 bottle.

--- Formula/dmenu.rb
+++ Formula/dmenu.rb
@@ -8,10 +8,9 @@ class Dmenu < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "1f81d44aa27e4059f2bef9c768b6875c1f1ed3f462ad7582df976f5c50b518f2" => :high_sierra
-    sha256 "2ef9de864fabaaae5e7214658d0bae190e0e895353fccbb5cdebc2a94f22306f" => :sierra
-    sha256 "0c2038a9f53d43a393d6108f49465fb095f2854d4f31acb777645018d84a6cd8" => :el_capitan
-    sha256 "75598d67eac5f745e325ae0f0f5eed063873f92bbd64c02126cf2ea95682c2c3" => :yosemite
+    sha256 "3882f8888be8c8e4ba11e22bca628e6b5a58f409bc4a40cd9ce542ad936f74c7" => :high_sierra
+    sha256 "48dc8fccfd0ad4111582ee8795a906e2c22160f985c993c73f625cea4e814937" => :sierra
+    sha256 "d013c1af685745f8d4c00f1366d15ada35e6f3946682798cad0963e6bd3e07cd" => :el_capitan
   end
 
   depends_on :x11
