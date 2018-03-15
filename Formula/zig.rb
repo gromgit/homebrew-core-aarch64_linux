deb6a115996729a5012324ad0644ec1b92bd00b1
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-15 15:55:50 +0000

    zig: update 0.2.0 bottle.

--- Formula/zig.rb
+++ Formula/zig.rb
@@ -5,9 +5,9 @@ class Zig < Formula
   sha256 "09843a3748bf8a5f1742fe93dbf45699f92051ecf479b23272b067dfc3837cc7"
 
   bottle do
-    sha256 "3c2e3f738f28287a1fc68ac80d6bf4b0b047a51cc54d0239cd0e4c8a65b47714" => :high_sierra
-    sha256 "17bb8fa63298c108fc67839c80116ecad69c2d0b3f7ec5f19ea4a88eb09b52fe" => :sierra
-    sha256 "920c60bf1e853505349414aa7f24c95ae65ccc432b1db27211ddb7707a6a11de" => :el_capitan
+    sha256 "9985f61a3ad0913ff7ef4595b64ed940532720f2e6e07e0e535cdb660a758c1a" => :high_sierra
+    sha256 "4c2b272a5229f0b0d67759a075cc647e09860b29b0325d0feb38a9abbb58830f" => :sierra
+    sha256 "4244c50ca567e20e6a98a783b7cdc97455d5114ae0f9d754d6c72a21b5a45c1f" => :el_capitan
   end
 
   depends_on "cmake" => :build
