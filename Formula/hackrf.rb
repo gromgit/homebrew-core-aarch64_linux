31ba7be4a0acd1ad85293861de1914ad68b9baef
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-16 12:15:01 +0000

    hackrf: update 2018.01.1 bottle.

--- Formula/hackrf.rb
+++ Formula/hackrf.rb
@@ -7,10 +7,9 @@ class Hackrf < Formula
 
   bottle do
     cellar :any
-    sha256 "ee0c6147fb2e29120ec71c64b41db770d77c5a8718f18a952bd45e3bae041c66" => :high_sierra
-    sha256 "1b543cb3ea75d8a435b43df8e02e9468b1a6cf93fff2ab1a6605dcb0a514aacf" => :sierra
-    sha256 "0c67009ad5e60aae45c203392dcee3a87e653177d996d296543b9de8d71ec827" => :el_capitan
-    sha256 "d78f8872362611465da4cfddbadfece708195057b88894d713959afd06030dc6" => :yosemite
+    sha256 "430173362cc05912520a38f41ce465a0966f1c8d849fd492f0b40074425c3f88" => :high_sierra
+    sha256 "f33bc6bde41e6522d587bc574c01e1402ccbde6759dec5e9d1a1e5f593e189b3" => :sierra
+    sha256 "909a5a9aca6f81cbab08bb7c063f3ee0e666bb5b44af86ebbec62cbdaf3e3b33" => :el_capitan
   end
 
   depends_on "cmake" => :build
