04c72ec52a44d959f2721fea6367c6077970fc6d
ilovezfs <ilovezfs@icloud.com>
2018-03-15 22:31:46 -0700

    stellar-core 9.2.0
    Closes #25343.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/stellar-core.rb
+++ Formula/stellar-core.rb
@@ -2,8 +2,8 @@ class StellarCore < Formula
   desc "The backbone of the Stellar (XLM) network"
   homepage "https://www.stellar.org/"
   url "https://github.com/stellar/stellar-core.git",
-    :tag => "v9.1.0",
-    :revision => "a278e95978bdac6d1015d82f4859dad780e752d3"
+    :tag => "v9.2.0",
+    :revision => "a8b6a9dcb6550ce652dc9f81bc92a28be7c9baf8"
   head "https://github.com/stellar/stellar-core.git"
 
   bottle do
@@ -16,6 +16,7 @@ class StellarCore < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "pandoc" => :build
   depends_on "pkg-config" => :build
   depends_on "libsodium"
   depends_on "postgresql"
