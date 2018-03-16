3fcfeecbdba2a0a43009e010178f795e1d8988fa
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-16 17:54:07 +0000

    nats-streaming-server: update 0.9.0 bottle.

--- Formula/nats-streaming-server.rb
+++ Formula/nats-streaming-server.rb
@@ -7,9 +7,9 @@ class NatsStreamingServer < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3fae57b99948ba21df72cdc73d1d3463a081c234c068b90d57e1338737dd5e2f" => :high_sierra
-    sha256 "fe89a33e0529b5cc18586aadde08b1318dac86a18879754eb6ba6aa6806657f1" => :sierra
-    sha256 "e8eb41ab04bca3531bca427aa1fe606e9c69c095a69dfaad2ca09b7d60863027" => :el_capitan
+    sha256 "f256ff2b9628d6ab0eb5806021da67946ffcb7a59b4285b78bdc48b257beec6e" => :high_sierra
+    sha256 "669af50135610556d64e201c60feb3c12ca0b7a05e996e0c84aec665f838091c" => :sierra
+    sha256 "d164fafe66d70255503812c3b5836adbd325c5001dbb21400d0f01e047f87c10" => :el_capitan
   end
 
   depends_on "go" => :build
