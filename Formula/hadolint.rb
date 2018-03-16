2890cb010121e8d7e5c972330196c4b42acbc00f
ilovezfs <ilovezfs@icloud.com>
2018-03-16 13:40:52 -0700

    hadolint 1.5.1
    Closes #25385.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/hadolint.rb
+++ Formula/hadolint.rb
@@ -5,8 +5,8 @@ class Hadolint < Formula
 
   desc "Smarter Dockerfile linter to validate best practices"
   homepage "https://github.com/hadolint/hadolint"
-  url "https://github.com/hadolint/hadolint/archive/v1.5.0.tar.gz"
-  sha256 "ea67439c3b785219905a24e9c66185ebaabbfb6cc0a088142d93f34bb30e61b5"
+  url "https://github.com/hadolint/hadolint/archive/v1.5.1.tar.gz"
+  sha256 "e70e295246193a5b0f2a7d1fc57072ae0e6d5f1178e9fd4cd8abbbb292287f16"
 
   bottle do
     cellar :any_skip_relocation
