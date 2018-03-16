2aa9b688f0ff77a6abdfafa588fff728f44e124b
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-03-16 12:14:09 +0000

    gnome-recipes: update gnome-online-accounts resource (3.28.0)
    Closes #25364.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/gnome-recipes.rb
+++ Formula/gnome-recipes.rb
@@ -3,7 +3,7 @@ class GnomeRecipes < Formula
   homepage "https://wiki.gnome.org/Apps/Recipes"
   url "https://download.gnome.org/sources/gnome-recipes/2.0/gnome-recipes-2.0.2.tar.xz"
   sha256 "1be9d2fcb7404a97aa029d2409880643f15071c37039247a6a4320e7478cd5fb"
-  revision 4
+  revision 5
 
   bottle do
     sha256 "25705ef284a230f166ba6218863cc0b4a6919c09c05692d5b9fe3dd997e7adf6" => :high_sierra
@@ -23,15 +23,15 @@ class GnomeRecipes < Formula
   depends_on "gspell"
   depends_on "libsoup"
   depends_on "gnu-tar"
+  depends_on "libxml2"
 
   # dependencies for goa
-  depends_on "intltool" => :build
   depends_on "json-glib"
   depends_on "librest"
 
   resource "goa" do
-    url "https://download.gnome.org/sources/gnome-online-accounts/3.26/gnome-online-accounts-3.26.1.tar.xz"
-    sha256 "603c110405cb89a01497a69967f10e3f3f36add3dc175b062ec4c5ed4485621b"
+    url "https://download.gnome.org/sources/gnome-online-accounts/3.28/gnome-online-accounts-3.28.0.tar.xz"
+    sha256 "87bc4ef307604f1ce4f09f6e5c9996ef8d37ca5e0a3bf76f6b27d71844adb40c"
   end
 
   def install
