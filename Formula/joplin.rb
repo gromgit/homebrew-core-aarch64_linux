dcc100acd67712820c9cb2451da66daa01a2ed8c
ilovezfs <ilovezfs@icloud.com>
2018-03-16 13:41:14 -0700

    joplin 1.0.103
    Closes #25386.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/joplin.rb
+++ Formula/joplin.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Joplin < Formula
   desc "Note taking and to-do application with synchronisation capabilities"
   homepage "https://joplin.cozic.net/"
-  url "https://registry.npmjs.org/joplin/-/joplin-1.0.101.tgz"
-  sha256 "6b9d83f3b0812385168c1a9a444f98c1a34cfe95e7425ab9b6a7bc117ed8c48c"
+  url "https://registry.npmjs.org/joplin/-/joplin-1.0.103.tgz"
+  sha256 "2774b83752a67cbcc31d40fe6ac1c759dc519a5c6bf5855f6935e58a571e668a"
 
   bottle do
     sha256 "5a74bd3996fe0c24c43c8b9911e6062fadf0b667d9ed0a1ae2c14455b3c202b5" => :high_sierra
