3572c5b6ff251863b7e69dd4a326c5c7e259a38a
mikit <mikihito0906@gmail.com>
2018-03-19 06:48:51 +0900

    pktanon: add test

--- Formula/pktanon.rb
+++ Formula/pktanon.rb
@@ -26,4 +26,8 @@ class Pktanon < Formula
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install"
   end
+
+  test do
+    system "#{bin}/pktanon", "--version"
+  end
 end
