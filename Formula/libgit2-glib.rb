61f7f1682b964cb35da4bd9aeea098501050393e
ilovezfs <ilovezfs@icloud.com>
2018-03-18 00:10:57 -0700

    libgit2-glib 0.26.4
    depend on meson-internal and ninja at build time
    remove python@2 and vala optional dependencies
    remove head dependencies on Autotools, gnome-common and gtk-doc
    
    Closes #25428.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libgit2-glib.rb
+++ Formula/libgit2-glib.rb
@@ -1,9 +1,9 @@
 class Libgit2Glib < Formula
   desc "Glib wrapper library around libgit2 git access library"
   homepage "https://github.com/GNOME/libgit2-glib"
-  url "https://download.gnome.org/sources/libgit2-glib/0.26/libgit2-glib-0.26.2.tar.xz"
-  sha256 "2ad6f20db2e38bbfdb6cb452704fe8a911036b86de82dc75bb0f3b20db40ce9c"
-  revision 1
+  url "https://download.gnome.org/sources/libgit2-glib/0.26/libgit2-glib-0.26.4.tar.xz"
+  sha256 "97610e42427a0c86ac46b89d5020fb8decb39af47b9dc49f8d078310b4c21e5a"
+  head "https://github.com/GNOME/libgit2-glib.git"
 
   bottle do
     sha256 "3f54034bdc3f924e2755c7b70237bcca02d4360d40dd078f515636f5d76ad37a" => :high_sierra
@@ -11,41 +11,32 @@ class Libgit2Glib < Formula
     sha256 "7d29be8d0f0c8275a0d5d7a2cbb7bb68375f4476b2a1776f6e05f0902db10f04" => :el_capitan
   end
 
-  head do
-    url "https://github.com/GNOME/libgit2-glib.git"
-
-    depends_on "libtool" => :build
-    depends_on "automake" => :build
-    depends_on "autoconf" => :build
-    depends_on "gnome-common" => :build
-    depends_on "gtk-doc" => :build
-  end
-
-  deprecated_option "with-python" => "with-python@2"
-
   depends_on "gobject-introspection" => :build
+  depends_on "meson-internal" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "libgit2"
   depends_on "glib"
-  depends_on "vala" => :optional
-  depends_on "python@2" => :optional
 
   def install
-    args = %W[
-      --prefix=#{prefix}
-      --disable-silent-rules
-      --disable-dependency-tracking
-    ]
+    ENV.refurbish_args
 
-    args << "--enable-python=no" if build.without? "python@2"
-    args << "--enable-vala=no" if build.without? "vala"
+    # Fix "ld: unknown option: -Bsymbolic-functions"
+    # Reported 2 Apr 2018 https://bugzilla.gnome.org/show_bug.cgi?id=794889
+    inreplace "libgit2-glib/meson.build",
+              "libgit2_glib_link_args = [ '-Wl,-Bsymbolic-functions' ]",
+              "libgit2_glib_link_args = []"
 
-    system "./autogen.sh", *args if build.head?
-    system "./configure", *args if build.stable?
-    system "make", "install"
-
-    libexec.install "examples/.libs", "examples/clone", "examples/general", "examples/walk"
+    mkdir "build" do
+      system "meson", "--prefix=#{prefix}",
+                      "-Dpython=false",
+                      "-Dvapi=false",
+                      ".."
+      system "ninja"
+      system "ninja", "install"
+      libexec.install Dir["examples/*"]
+    end
   end
 
   test do
