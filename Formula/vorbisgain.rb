8add4df1bd7abe0e435f53aef188e68cd793b6c7
mikit <mikihito0906@gmail.com>
2018-03-19 06:29:16 +0900

    vorbisgain: add test

--- Formula/vorbisgain.rb
+++ Formula/vorbisgain.rb
@@ -20,4 +20,8 @@ class Vorbisgain < Formula
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
     system "make", "install"
   end
+
+  test do
+    system "#{bin}/vorbisgain", "--version"
+  end
 end
