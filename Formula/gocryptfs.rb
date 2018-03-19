0f61e0e0636ab137c9c09ee6e2d988f59dc18e84
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-19 05:07:39 +0000

    gocryptfs: update 1.4.4 bottle.

--- Formula/gocryptfs.rb
+++ Formula/gocryptfs.rb
@@ -6,9 +6,9 @@ class Gocryptfs < Formula
   sha256 "988258acd8c4105ebd660b1a411e1174d803f3765c4dc2721c8512356cbe3f3f"
 
   bottle do
-    sha256 "e78575f894959fc13b4a30e01a9f8fdb22d4774ee1d3b69293842f0f3a9593cf" => :high_sierra
-    sha256 "a172e3b1016cf339d46cb78b18a0fe0b1a1bd08fc2ec991bce9a5798b4d9ecaf" => :sierra
-    sha256 "6b59252b280b9dfa60bb0437cebf4ef6d1bdea4d333e22becb76ed63ffcbe545" => :el_capitan
+    sha256 "54e85a214d7f742c94f56c8726f2816e7fb5b5ff99ba318d9dd2a376adbdcd3b" => :high_sierra
+    sha256 "5ca878be5f42a65443d66ca1d41c0afc9ed59659fb0e8cb8c283e2da075cfcc3" => :sierra
+    sha256 "c5a6df44dd33df0f2962b3fdeb7a872bb9562c203279334a74eae3efdd224a2b" => :el_capitan
   end
 
   depends_on "go" => :build
