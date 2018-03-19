c67457160c3d3babb917a0dd9f7cebff8bb0a909
Mike McQuaid <mike@mikemcquaid.com>
2018-03-19 09:56:47 +0000

    collector-sidecar: remove :run.

--- Formula/collector-sidecar.rb
+++ Formula/collector-sidecar.rb
@@ -13,7 +13,7 @@ class CollectorSidecar < Formula
   depends_on "glide" => :build
   depends_on "go" => :build
   depends_on "mercurial" => :build
-  depends_on "filebeat" => :run
+  depends_on "filebeat"
 
   def install
     ENV["GOPATH"] = buildpath
