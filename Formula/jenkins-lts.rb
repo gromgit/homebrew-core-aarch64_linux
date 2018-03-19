287d7ef5b931211fdc07867d8ca7344b8c32b488
ryenus <ryenus@gmail.com>
2018-03-19 14:47:18 +0800

    jenkins-lts: require java 8
    Closes #22671.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/jenkins-lts.rb
+++ Formula/jenkins-lts.rb
@@ -6,13 +6,13 @@ class JenkinsLts < Formula
 
   bottle :unneeded
 
-  depends_on :java => "1.7+"
+  depends_on :java => "1.8"
 
   def install
     system "jar", "xvf", "jenkins.war"
     libexec.install "jenkins.war", "WEB-INF/jenkins-cli.jar"
-    bin.write_jar_script libexec/"jenkins.war", "jenkins-lts"
-    bin.write_jar_script libexec/"jenkins-cli.jar", "jenkins-lts-cli"
+    bin.write_jar_script libexec/"jenkins.war", "jenkins-lts", :java_version => "1.8"
+    bin.write_jar_script libexec/"jenkins-cli.jar", "jenkins-lts-cli", :java_version => "1.8"
   end
 
   def caveats; <<~EOS
@@ -31,7 +31,11 @@ class JenkinsLts < Formula
         <string>#{plist_name}</string>
         <key>ProgramArguments</key>
         <array>
-          <string>/usr/bin/java</string>
+          <string>/usr/libexec/java_home</string>
+          <string>-v</string>
+          <string>1.8</string>
+          <string>--exec</string>
+          <string>java</string>
           <string>-Dmail.smtp.starttls.enable=true</string>
           <string>-jar</string>
           <string>#{opt_libexec}/jenkins.war</string>
