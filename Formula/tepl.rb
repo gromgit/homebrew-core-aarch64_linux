4d271105e175d6e00274fc7a6983de2abcb9924d
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-03-19 09:26:15 +0000

    tepl: make gobject-introspection build-time dependency

--- Formula/tepl.rb
+++ Formula/tepl.rb
@@ -3,6 +3,7 @@ class Tepl < Formula
   homepage "https://wiki.gnome.org/Projects/Tepl"
   url "https://download.gnome.org/sources/tepl/3.0/tepl-3.0.0.tar.xz"
   sha256 "9eb6fceb0b521de7bf684508138a5b8428d9f575c395ba54f9e31597980fd0ef"
+  revision 1
 
   bottle do
     sha256 "bb36835284be2fc7e44840e193f6b17bd71afd09c61781c00e5648d6db16bbb4" => :high_sierra
@@ -10,8 +11,8 @@ class Tepl < Formula
     sha256 "d9043558401a3a9cf2188ffa7396b05c7c38cfa00ad1be4400cdb0de69dc633a" => :el_capitan
   end
 
+  depends_on "gobject-introspection" => :build
   depends_on "pkg-config" => :build
-  depends_on "gobject-introspection"
   depends_on "gtksourceview3"
   depends_on "uchardet"
 
