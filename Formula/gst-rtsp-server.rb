c071a0826d20f66adc8f5f46ce966ab312777d4d
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-03-19 08:39:34 +0000

    gst-rtsp-server: make gobject-introspection build-time dependency

--- Formula/gst-rtsp-server.rb
+++ Formula/gst-rtsp-server.rb
@@ -3,6 +3,7 @@ class GstRtspServer < Formula
   homepage "https://gstreamer.freedesktop.org/modules/gst-rtsp-server.html"
   url "https://gstreamer.freedesktop.org/src/gst-rtsp-server/gst-rtsp-server-1.14.0.tar.xz"
   sha256 "6b65a077bed815f6d3157ebea503cc9f3c32d289af2756b7ff7e3958744d9756"
+  revision 1
 
   bottle do
     sha256 "eed082877b8ed362e14d9fc0acc25988fd6e11991f3893f7937c62015636a9cd" => :high_sierra
@@ -10,12 +11,12 @@ class GstRtspServer < Formula
     sha256 "406f5450588bc7afedb525eac961d64b630145359d92cfdb59e353e46e7edc96" => :el_capitan
   end
 
+  depends_on "gobject-introspection" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "gstreamer"
   depends_on "gst-plugins-base"
-  depends_on "gobject-introspection"
 
   def install
     system "./configure", "--prefix=#{prefix}",
