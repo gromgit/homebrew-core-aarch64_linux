2777dfd480a45062bb1d9a12f92d43192d10c506
ilovezfs <ilovezfs@icloud.com>
2018-03-18 21:24:02 -0700

    fribidi 1.0.2
    remove pkg-config build-time dependency
    remove glib and pcre dependencies
    add patch to fix building the docs
    
    Closes #25449.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/fribidi.rb
+++ Formula/fribidi.rb
@@ -1,8 +1,8 @@
 class Fribidi < Formula
   desc "Implementation of the Unicode BiDi algorithm"
   homepage "https://github.com/fribidi/fribidi"
-  url "https://github.com/fribidi/fribidi/releases/download/v1.0.1/fribidi-1.0.1.tar.bz2"
-  sha256 "c1b182d70590b6cdb5545bab8149de33b966800f27f2d9365c68917ed5a174e4"
+  url "https://github.com/fribidi/fribidi/releases/download/v1.0.2/fribidi-1.0.2.tar.bz2"
+  sha256 "bd6d1b530c4f6066f42461200ed6a31f2db8db208570ea4ccaab2b935e88832b"
 
   bottle do
     cellar :any
@@ -12,13 +12,20 @@ class Fribidi < Formula
     sha256 "b99afe046883119dd1e1646297d67efb31e7981eb13ea4b1678718f9cce711cf" => :el_capitan
   end
 
-  depends_on "pkg-config" => :build
-  depends_on "glib"
-  depends_on "pcre"
+  # Remove for > 1.0.2
+  # Upstream commit from 19 Mar 2018 "Revert 'Add an option to disable building
+  # documentation'"; see https://github.com/fribidi/fribidi/commit/095d885
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/f9c1d7d/fribidi/fix-docs.diff"
+    sha256 "990777213ff47cfbf06f0342f66e84783bf5eec80419ff1582dd189352ef5f73"
+  end
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--with-glib", "--enable-static", "--prefix=#{prefix}"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--enable-static"
     system "make", "install"
   end
 
