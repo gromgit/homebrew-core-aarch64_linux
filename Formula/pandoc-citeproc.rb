e1f7f57cce62e4cde4f5a1f62ac9961c94a71ec1
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-19 04:50:58 +0000

    pandoc-citeproc: update 0.14.2 bottle.

--- Formula/pandoc-citeproc.rb
+++ Formula/pandoc-citeproc.rb
@@ -10,9 +10,10 @@ class PandocCiteproc < Formula
   head "https://github.com/jgm/pandoc-citeproc.git"
 
   bottle do
-    sha256 "62f470fb1a29cf38e2393508dd8bf53ae62f3721ba575fc11903817ebd8b2eac" => :high_sierra
-    sha256 "d9c043861168eb2a12de5a9dc1d2bea092b0dfdfe67b40ddf8d19e0210175f54" => :sierra
-    sha256 "9c09aa0b61d858b8a531f85b1e529db844edee90c9058fdad40eb6d6ee85dda5" => :el_capitan
+    rebuild 1
+    sha256 "0baecf261e6701b1ab7831f9bc0e7b0d2fc6cb238f944e418a738907db9983cf" => :high_sierra
+    sha256 "fad1437315d030c29abdb2e31a704750e20bc40230ded72666d368ec023732bd" => :sierra
+    sha256 "bbbac3aa232da8858a3c8699199120bb284ba675bd95d394a7726f4d58ddf8ca" => :el_capitan
   end
 
   depends_on "cabal-install" => :build
