7460b9ec143786d52d6a8dc660e20278a97d2c84
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-03-19 08:46:23 +0000

    gxml: make gobject-introspection build-time dependency

--- Formula/gxml.rb
+++ Formula/gxml.rb
@@ -3,6 +3,7 @@ class Gxml < Formula
   homepage "https://wiki.gnome.org/GXml"
   url "https://download.gnome.org/sources/gxml/0.16/gxml-0.16.3.tar.xz"
   sha256 "520d4d779b1d31591762b2a98f84072531b9e17ac401df9668493e189eafc6ba"
+  revision 1
 
   bottle do
     sha256 "faf9da56560023721c8f04fb1871281ae556b8017b42847be5265f38b462484e" => :high_sierra
@@ -10,6 +11,7 @@ class Gxml < Formula
     sha256 "d863844b85f0c0b67f40074108a690242989237dd3c98f16d34813c88e3b0958" => :el_capitan
   end
 
+  depends_on "gobject-introspection" => :build
   depends_on "gtk-doc" => :build
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
@@ -17,7 +19,6 @@ class Gxml < Formula
   depends_on "libxml2"
   depends_on "glib"
   depends_on "libgee"
-  depends_on "gobject-introspection"
 
   def install
     # ensures that the gobject-introspection files remain within the keg
