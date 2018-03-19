78aea5875f3c9fc46870d13936fb015296bef0d2
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-19 05:05:06 +0000

    giflossy: update 1.91 bottle.

--- Formula/giflossy.rb
+++ Formula/giflossy.rb
@@ -7,12 +7,9 @@ class Giflossy < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "56c413383c1f4978f1820086aca1d6f61a3b0b89d8df9ec946320d5aeb4a7868" => :high_sierra
-    sha256 "3cf3c0757688012aad91066611bf6851f26b6ca246e6014a43da7abeca1f6b77" => :sierra
-    sha256 "b1003fafaa5c7bbdf956a7ac4f8c0c06abf4c583b922bf67e1eacb1498db0b16" => :el_capitan
-    sha256 "2c4d49abc644aa4e98eaaae86002c7153bf9e3990186e730d9ebb3ff4ceb0b22" => :yosemite
-    sha256 "96c42819acabd42287e2e032f10969e9ac08bfb28848626ac0df4f926f116a99" => :mavericks
-    sha256 "1e71bcca774a3f01d8ede50d6040d06c2f25df7e6e3635efab4c5daf06f2bcb1" => :mountain_lion
+    sha256 "b81704f64e9bdac63646b92be361303215e24088f662a24c257a01de7fb4f734" => :high_sierra
+    sha256 "1b720c57508a15ac50a2f6340532115049a455098e2db1e98ea621e175dfd04a" => :sierra
+    sha256 "a770a479a8fcd961ceef0274fc8aa75f70fa7b1f4bb8cb34736c0d6902340e1c" => :el_capitan
   end
 
   option "with-x11", "Install gifview"
