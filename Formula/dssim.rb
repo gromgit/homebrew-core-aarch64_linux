60d43165a5998b4f295feae34827de97c8591f71
commitay <commitay@users.noreply.github.com>
2018-03-19 14:37:27 +1000

    dssim: use canonical repository URLs

--- Formula/dssim.rb
+++ Formula/dssim.rb
@@ -1,7 +1,7 @@
 class Dssim < Formula
   desc "RGBA Structural Similarity Rust implementation"
-  homepage "https://github.com/pornel/dssim"
-  url "https://github.com/pornel/dssim/archive/2.9.7.tar.gz"
+  homepage "https://github.com/kornelski/dssim"
+  url "https://github.com/kornelski/dssim/archive/2.9.7.tar.gz"
   sha256 "4ee60e125efae43f49bf9c4ca849f9cef2b1f86ee1d538da84907faae853eeeb"
 
   bottle do
