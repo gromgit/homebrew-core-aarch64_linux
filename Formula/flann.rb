679d8c224809fe0b82bd4e340fd2acdd329866d3
FX Coudert <fxcoudert@gmail.com>
2018-03-19 18:28:17 +0100

    flann: update URLs

--- Formula/flann.rb
+++ Formula/flann.rb
@@ -1,6 +1,6 @@
 class Flann < Formula
   desc "Fast Library for Approximate Nearest Neighbors"
-  homepage "https://www.cs.ubc.ca/~mariusm/index.php/FLANN/FLANN"
+  homepage "https://www.cs.ubc.ca/research/flann/"
   url "https://github.com/mariusmuja/flann/archive/1.9.1.tar.gz"
   sha256 "b23b5f4e71139faa3bcb39e6bbcc76967fbaf308c4ee9d4f5bfbeceaa76cc5d3"
   revision 3
@@ -21,17 +21,17 @@ class Flann < Formula
   depends_on "numpy" if build.with? "python@2"
 
   resource("dataset.dat") do
-    url "https://www.cs.ubc.ca/~mariusm/uploads/FLANN/datasets/dataset.dat"
+    url "https://www.cs.ubc.ca/research/flann/uploads/FLANN/datasets/dataset.dat"
     sha256 "dcbf0268a7ff9acd7c3972623e9da722a8788f5e474ae478b888c255ff73d981"
   end
 
   resource("testset.dat") do
-    url "https://www.cs.ubc.ca/~mariusm/uploads/FLANN/datasets/testset.dat"
+    url "https://www.cs.ubc.ca/research/flann/uploads/FLANN/datasets/testset.dat"
     sha256 "d9ff91195bf2ad8ced78842fa138b3cd4e226d714edbb4cb776369af04dda81b"
   end
 
   resource("dataset.hdf5") do
-    url "https://www.cs.ubc.ca/~mariusm/uploads/FLANN/datasets/dataset.hdf5"
+    url "https://www.cs.ubc.ca/research/flann/uploads/FLANN/datasets/dataset.hdf5"
     sha256 "64ae599f3182a44806f611fdb3c77f837705fcaef96321fb613190a6eabb4860"
   end
 
