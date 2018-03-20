e41b095fd35e2e8657d5c61deccb36c175c36cad
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-20 09:05:26 +0000

    gst-editing-services: update 1.14.0 bottle.

--- Formula/gst-editing-services.rb
+++ Formula/gst-editing-services.rb
@@ -5,9 +5,9 @@ class GstEditingServices < Formula
   sha256 "8d5f90eb532f4cf4aa1466807ef92b05bd1705970d7aabe10066929bbc698d91"
 
   bottle do
-    sha256 "37e28bb8f36efa80c581272186f365201088cbb52f45b186e1e2775dbc2eb973" => :high_sierra
-    sha256 "8ca9dbc7b05c1baf13f550f297636016be20119d93343b985d9781e6353b490c" => :sierra
-    sha256 "c99951c37bd5b03778d57141b7fe6755ad7c54d524f995c23b48f99f219cb808" => :el_capitan
+    sha256 "771d7b375ea4294c3775fd5e170972ec11dec3f64b1e02fb5173b42c47793af1" => :high_sierra
+    sha256 "ff1a6848c4119cad9ee0bb61befada7ef8cdb2ce45cb34c306e69a06b7c59598" => :sierra
+    sha256 "d97b2fe56704c11f0c32d41c1b73bffb5597aae6908535e7fc8301ec8103dc35" => :el_capitan
   end
 
   depends_on "gstreamer"
