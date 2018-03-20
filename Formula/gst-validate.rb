1638906dd5b0e95f58c0145449f76ccd5279af1f
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-20 09:05:25 +0000

    gst-validate: update 1.14.0 bottle.

--- Formula/gst-validate.rb
+++ Formula/gst-validate.rb
@@ -5,9 +5,9 @@ class GstValidate < Formula
   sha256 "33df08bf77f2895d64b7e8a957de3b930b4da0a8edabfbefcff2eab027eeffdf"
 
   bottle do
-    sha256 "0b8e22d0ada1de4fea3c343fe26858527bc4818cecef4850ab46967bea74fc80" => :high_sierra
-    sha256 "1edcd085b4b312ccbaf986e6eca15b2c7c8ed22655681f20947802bd1d3e2aab" => :sierra
-    sha256 "209e3247c566b3de9a2d81ed1c62197cf969d444144f17abcc4fcb092853d274" => :el_capitan
+    sha256 "c272836c10d8688d8fdfbd660f8a04e87ab796b088cbbee5d803ed9b11e7d2f0" => :high_sierra
+    sha256 "08dc1bdd726ea7e63979afedeea2683914ea0fe078cf941f2d4aa9909e68305b" => :sierra
+    sha256 "867ac9572fe3db7f985e69eb926f73e1d32be863213e82b155d2c71f1d97035a" => :el_capitan
   end
 
   head do
