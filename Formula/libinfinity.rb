49a7bca1d298cdba142652424922d48ed8bdc06d
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-21 17:13:50 +0000

    libinfinity: update 0.6.8_1 bottle.

--- Formula/libinfinity.rb
+++ Formula/libinfinity.rb
@@ -6,9 +6,9 @@ class Libinfinity < Formula
   revision 1
 
   bottle do
-    sha256 "1faad7e989ee0b318a6bc083d6dabc09b1bff20f29e85fb2eeaf0e204db77ed2" => :high_sierra
-    sha256 "a57e350ab3149f0992f1ae56953eb951dea73601c0837d8268679395f247f8ec" => :sierra
-    sha256 "b4d21db6a91949753057955b38fd47b8d289a2e5961ba6fc5e1fdc06c6f0d7a1" => :el_capitan
+    sha256 "5a82262c8519af2c73c3ee69c9632031cc6e7cc6bc52c2eef5defe74025b7a24" => :high_sierra
+    sha256 "d9791b732d6a5c1bbba29941cfc8e0847c92bacebc43cdf9aef26799e8bada8f" => :sierra
+    sha256 "ad024ec7122272de003ebfee98ffa2c0518d40061dbf7631627e720ef27ad4e9" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
