d2b0906b34052d71da33b448f11a42d8d42be413
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-21 11:46:47 +0000

    gssdp: update 1.0.2_1 bottle.

--- Formula/gssdp.rb
+++ Formula/gssdp.rb
@@ -6,10 +6,10 @@ class Gssdp < Formula
   revision 1
 
   bottle do
-    sha256 "17909e65f4cd4a34f89998e82f038b4a1167d5b3b81adc3b544d3190dfe5d7a4" => :high_sierra
-    sha256 "d29d7e15bd8ceae958e69a08b058ae9a9ccac273dee2182545d4371048efbb6e" => :sierra
-    sha256 "32cb5ef5c518bc5d1812a34ebade6a82cc43f036f4a40fc77719dfcafe6dad02" => :el_capitan
-    sha256 "540ceba3f55e06e642ab67d53dc84f94a4a55b4e0381f4d015f15991e102b255" => :yosemite
+    cellar :any
+    sha256 "c8ac9c7c755749b7a6ea9790efab2311c9fc3d62a1af62b719968f14a7c25b62" => :high_sierra
+    sha256 "3786f067d3b19ce3021618aaf434fd325862f90d03b7fd5ac12f6f37f8715e42" => :sierra
+    sha256 "7927b712f8f9570c0a7e21593786bd41edf0daf2e14b7998886af9a00a8c2ab0" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
