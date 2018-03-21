5cba704f14dd2c03096fa215169b2030661125a1
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-21 20:22:30 +0000

    yelp-tools: update 3.28.0_1 bottle.

--- Formula/yelp-tools.rb
+++ Formula/yelp-tools.rb
@@ -7,9 +7,9 @@ class YelpTools < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9be349bcaf8cef4ebfb969bde267d81dd8ff5e8f2ddf89b202b163357bea7866" => :high_sierra
-    sha256 "9be349bcaf8cef4ebfb969bde267d81dd8ff5e8f2ddf89b202b163357bea7866" => :sierra
-    sha256 "9be349bcaf8cef4ebfb969bde267d81dd8ff5e8f2ddf89b202b163357bea7866" => :el_capitan
+    sha256 "628fc21556d7cade65245e204db567ba929307a91637391a583698d3c281b129" => :high_sierra
+    sha256 "628fc21556d7cade65245e204db567ba929307a91637391a583698d3c281b129" => :sierra
+    sha256 "628fc21556d7cade65245e204db567ba929307a91637391a583698d3c281b129" => :el_capitan
   end
 
   depends_on "gettext" => :build
