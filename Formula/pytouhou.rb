76dcf0bf7993f825d47183f2cc96ba0a441135c9
ilovezfs <ilovezfs@icloud.com>
2018-03-21 01:21:33 -0700

    pytouhou: revision to fix install receipt

--- Formula/pytouhou.rb
+++ Formula/pytouhou.rb
@@ -3,7 +3,7 @@ class Pytouhou < Formula
   homepage "https://pytouhou.linkmauve.fr/"
   url "https://hg.linkmauve.fr/touhou", :revision => "5270c34b4c00", :using => :hg
   version "634"
-  revision 2
+  revision 3
   head "https://hg.linkmauve.fr/touhou", :using => :hg
 
   bottle do
