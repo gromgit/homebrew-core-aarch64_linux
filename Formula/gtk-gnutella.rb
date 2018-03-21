e759f7326d7a56f199eb398cdfec9c97f4a52f22
ilovezfs <ilovezfs@icloud.com>
2018-03-21 01:14:57 -0700

    gtk-gnutella: revision to fix install receipt

--- Formula/gtk-gnutella.rb
+++ Formula/gtk-gnutella.rb
@@ -3,6 +3,7 @@ class GtkGnutella < Formula
   homepage "https://gtk-gnutella.sourceforge.io"
   url "https://downloads.sourceforge.net/project/gtk-gnutella/gtk-gnutella/1.1.13/gtk-gnutella-1.1.13.tar.xz"
   sha256 "2126cd08941ba0847c06f1034c5e2a9428b67b2fd1685c0e3ab878cb0179f647"
+  revision 1
 
   bottle do
     sha256 "417164f40527b51744bfd3517e6a315a8f9ba5783fed7c34d0e4d159851a0d1f" => :high_sierra
