c67dc896bfd4332f2975c3699bd20ba0a472863c
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-21 13:23:29 +0000

    gtkextra: update 3.3.4_1 bottle.

--- Formula/gtkextra.rb
+++ Formula/gtkextra.rb
@@ -7,9 +7,9 @@ class Gtkextra < Formula
 
   bottle do
     cellar :any
-    sha256 "c83e9020038de86c26e74e2a87bdb8e382d668cfa7ee0c7560c3e653e776e52e" => :high_sierra
-    sha256 "19204f24b8d8bca289b41e662b8acb46a9522a9aecc70f3a13666b7d14290606" => :sierra
-    sha256 "6d9cadd386a89aee512dcf591efa08485341c2a36cde58091d2f1d51f74ce7ba" => :el_capitan
+    sha256 "dccb6576a9313eec96b7b7c7abd6e9411959b351b996f19592021c7c2348d518" => :high_sierra
+    sha256 "74d2f3a62c9a008d18f68e40aec68e94f6054ce0b94f5d0e5f67c79d88610411" => :sierra
+    sha256 "3bd2f9a0bfc2296316e69702a126b23dc126a513192ce709b6d8d5120bc16904" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
