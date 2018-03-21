be44ef7a64b7562b792a954f75fa0961f70600a3
ilovezfs <ilovezfs@icloud.com>
2018-03-20 22:53:32 -0700

    igv 2.4.10 (#25563)

--- Formula/igv.rb
+++ Formula/igv.rb
@@ -1,8 +1,8 @@
 class Igv < Formula
   desc "Interactive Genomics Viewer"
   homepage "https://www.broadinstitute.org/software/igv"
-  url "https://data.broadinstitute.org/igv/projects/downloads/2.4/IGV_2.4.9.zip"
-  sha256 "e18dd8372e34454ac51cf3fb846fa26f22c34e12e1c6c4aaf9573c01ccc19e29"
+  url "https://data.broadinstitute.org/igv/projects/downloads/2.4/IGV_2.4.10.zip"
+  sha256 "c21981dac516c01e2309a7b06474fd896c5018fb774438d0bad15403bc1ddbf3"
 
   bottle :unneeded
 
