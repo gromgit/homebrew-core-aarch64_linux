0dff7da84acdcf3a661a130083d98f70dfb3ffed
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-21 06:56:27 +0000

    libgee: update 0.20.1_1 bottle.

--- Formula/libgee.rb
+++ Formula/libgee.rb
@@ -7,9 +7,9 @@ class Libgee < Formula
 
   bottle do
     cellar :any
-    sha256 "ccf7c9df7b20315d949fdf91addc28944f0460033938b36e975070461a5bbf67" => :high_sierra
-    sha256 "873c75b548cd0fca3d09019618dd90e402aa86671006d5852931d8906644ce23" => :sierra
-    sha256 "c03c87fb2bd8aafdd2e5f8fd1c3a655ced1730c0d525a4fd369b063e2a29821c" => :el_capitan
+    sha256 "d4afb1bbccfd61b0a106595356d5f571a0df6a5fb0fd5e378c8321a46e152c1b" => :high_sierra
+    sha256 "b70794b96ce5d21dc3dac66f4e0e63177d7507c723d78a786110dc502d7777e1" => :sierra
+    sha256 "7602f76b0667a7dd1527c01285274c2cca24d5e638b9a0c4a90cbc004d5e097e" => :el_capitan
   end
 
   depends_on "gobject-introspection" => :build
