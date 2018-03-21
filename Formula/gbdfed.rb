46753d4612bdc6cf5d38bac0bb7edc234a07495f
ilovezfs <ilovezfs@icloud.com>
2018-03-21 01:04:07 -0700

    gbdfed: revision to fix install receipt

--- Formula/gbdfed.rb
+++ Formula/gbdfed.rb
@@ -3,7 +3,7 @@ class Gbdfed < Formula
   homepage "http://sofia.nmsu.edu/~mleisher/Software/gbdfed/"
   url "http://sofia.nmsu.edu/~mleisher/Software/gbdfed/gbdfed-1.6.tar.gz"
   sha256 "8042575d23a55a3c38192e67fcb5eafd8f7aa8d723012c374acb2e0a36022943"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
