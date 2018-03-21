25fd3e5dec3471d39b353b2664680ac5049fcf7c
ilovezfs <ilovezfs@icloud.com>
2018-03-21 01:15:01 -0700

    gtkextra: revision to fix install receipt

--- Formula/gtkextra.rb
+++ Formula/gtkextra.rb
@@ -3,6 +3,7 @@ class Gtkextra < Formula
   homepage "https://gtkextra.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/gtkextra/3.3/gtkextra-3.3.4.tar.gz"
   sha256 "651b738a78edbd5d6ccb64f5a256c39ec35fbbed898e54a3ab7e6cf8fd82f1d6"
+  revision 1
 
   bottle do
     cellar :any
