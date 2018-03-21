dc2e55a1c089a1db8325ec369e139b6d624f54fc
ilovezfs <ilovezfs@icloud.com>
2018-03-21 07:39:19 -0700

    docfx 2.33.1 (#25581)

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -1,8 +1,8 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/releases/download/v2.33/docfx.zip"
-  sha256 "99b849792fb404b0a80539313e88b0a6597404c2863390219f1138ba2c580a49"
+  url "https://github.com/dotnet/docfx/releases/download/v2.33.1/docfx.zip"
+  sha256 "e24680adf5029d5da36647a6cf9d0be3bc1e86737b78387ffb0cdc709b59ba5c"
 
   bottle :unneeded
 
