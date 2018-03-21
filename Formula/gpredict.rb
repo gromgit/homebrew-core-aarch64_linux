6b22e10a4d42f523d8bea31427bdb422f4ecd5a2
ilovezfs <ilovezfs@icloud.com>
2018-03-21 01:13:10 -0700

    gpredict: revision to fix install receipt

--- Formula/gpredict.rb
+++ Formula/gpredict.rb
@@ -3,6 +3,7 @@ class Gpredict < Formula
   homepage "http://gpredict.oz9aec.net/"
   url "https://github.com/csete/gpredict/releases/download/v2.2.1/gpredict-2.2.1.tar.bz2"
   sha256 "e759c4bae0b17b202a7c0f8281ff016f819b502780d3e77b46fe8767e7498e43"
+  revision 1
 
   bottle do
     sha256 "26b0216abfb1c2d1601d8b24667f557532f7462960a5f58ef73bf65ccc2a4981" => :high_sierra
