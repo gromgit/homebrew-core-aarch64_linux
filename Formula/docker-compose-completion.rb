bede62cbe08229381e16ddfdaef1e98953a9a01c
ilovezfs <ilovezfs@icloud.com>
2018-03-20 22:09:09 -0700

    docker-compose-completion 1.20.0 (#25547)

--- Formula/docker-compose-completion.rb
+++ Formula/docker-compose-completion.rb
@@ -1,20 +1,9 @@
 class DockerComposeCompletion < Formula
   desc "Docker-compose completion script"
   homepage "https://docs.docker.com/compose/completion/"
+  url "https://github.com/docker/compose/archive/1.20.0.tar.gz"
+  sha256 "676844cb0a910684c55a61f9236ef402c7458c8ccea8404f5bd4989419751b59"
   head "https://github.com/docker/compose.git"
-
-  stable do
-    url "https://github.com/docker/compose/archive/1.19.0.tar.gz"
-    sha256 "2f8eb50a1e71a9eed773456267d511cd77a463809e746d02d9366888ff30d8a2"
-
-    # Remove for > 1.19.0
-    # Upstream commit from 9 Feb 2018 "Fix bash completion on systems where
-    # extglob is not set"
-    patch do
-      url "https://github.com/docker/compose/commit/56b2a80d5.patch?full_index=1"
-      sha256 "eb4a8f2c407fb129464b249f1f50ee21672ca1d0addd401d6f5c933fb9ce82d3"
-    end
-  end
 
   bottle :unneeded
 
