600e0dda53ed63d9e9cad2a3853d1e742001e631
ilovezfs <ilovezfs@icloud.com>
2018-03-21 01:03:55 -0700

    easy-tag: revision to fix install receipt

--- Formula/easy-tag.rb
+++ Formula/easy-tag.rb
@@ -3,6 +3,7 @@ class EasyTag < Formula
   homepage "https://projects.gnome.org/easytag"
   url "https://download.gnome.org//sources/easytag/2.4/easytag-2.4.3.tar.xz"
   sha256 "fc51ee92a705e3c5979dff1655f7496effb68b98f1ada0547e8cbbc033b67dd5"
+  revision 1
 
   bottle do
     rebuild 1
