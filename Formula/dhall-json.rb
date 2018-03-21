4a660bc054e170b2d2ea5f03b8fa47e5ce79f133
ilovezfs <ilovezfs@icloud.com>
2018-03-20 22:06:43 -0700

    dhall-json 1.0.12
    depend on ghc@8.2 at build time
    
    Closes #25560.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dhall-json.rb
+++ Formula/dhall-json.rb
@@ -5,8 +5,8 @@ class DhallJson < Formula
 
   desc "Dhall to JSON compiler and a Dhall to YAML compiler"
   homepage "https://github.com/Gabriel439/Haskell-Dhall-JSON-Library"
-  url "https://hackage.haskell.org/package/dhall-json-1.0.10/dhall-json-1.0.10.tar.gz"
-  sha256 "f872132fdee24ba845a81b32ae5897f0e29662c8de3aaa7839148832202c0b7f"
+  url "https://hackage.haskell.org/package/dhall-json-1.0.12/dhall-json-1.0.12.tar.gz"
+  sha256 "4b493f17914f659ce42f656104b9ffbd7847f8d19455c447c8af33779cd39a9c"
   head "https://github.com/Gabriel439/Haskell-Dhall-JSON-Library.git"
 
   bottle do
@@ -16,8 +16,8 @@ class DhallJson < Formula
     sha256 "9104547362cd3e4fc62fb2935e485c5a21138f5b200c1abe87d41fdb8938c08c" => :el_capitan
   end
 
-  depends_on "ghc" => :build
   depends_on "cabal-install" => :build
+  depends_on "ghc@8.2" => :build
 
   def install
     install_cabal_package
