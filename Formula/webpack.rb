06a6b7a8977e6861a5b6ddc127be1e11d9becc28
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-21 14:43:59 +0000

    webpack: update 4.2.0 bottle.

--- Formula/webpack.rb
+++ Formula/webpack.rb
@@ -9,9 +9,9 @@ class Webpack < Formula
   head "https://github.com/webpack/webpack.git"
 
   bottle do
-    sha256 "81f147957ab294204108b3b0d6f93af0bb0639a85770647e535003367c229942" => :high_sierra
-    sha256 "cff956ccf712a90714b112b70d28d7e006f06623dd85e7412255196bb7522c89" => :sierra
-    sha256 "b63c1c6d91fcfa69239248dbd6746c3bbe0d4ef879b922c04330b8fecc25ad3b" => :el_capitan
+    sha256 "e7e203d228b8e1ef292042757cda099341e156da5f204a294e267cc88b97c077" => :high_sierra
+    sha256 "fe88622d32152ab40d7692fbcea1b2ad2a3c1ef9b463af3816936773a8864644" => :sierra
+    sha256 "a0bff460e34617045ab35e5195bd9df139caa46ebdfc45e0f3be7cc7dae593cd" => :el_capitan
   end
 
   depends_on "node"
