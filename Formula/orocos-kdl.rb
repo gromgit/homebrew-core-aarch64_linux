ba33a238963123ff835ae49a0f1550f8095287b8
ilovezfs <ilovezfs@icloud.com>
2018-03-21 21:16:41 -0700

    orocos-kdl 1.4.0
    Closes #25616.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/orocos-kdl.rb
+++ Formula/orocos-kdl.rb
@@ -1,8 +1,8 @@
 class OrocosKdl < Formula
   desc "Orocos Kinematics and Dynamics C++ library"
   homepage "http://www.orocos.org/kdl"
-  url "https://github.com/orocos/orocos_kinematics_dynamics/archive/v1.3.1.tar.gz"
-  sha256 "aff361d2b4e2c6d30ae959308a124022eeef5dc5bea2ce779900f9b36b0537bd"
+  url "https://github.com/orocos/orocos_kinematics_dynamics/archive/v1.4.0.tar.gz"
+  sha256 "05b93e759923684dc07433ccae1e476d158d89b3c2be5079c20062406da7b4dd"
 
   bottle do
     cellar :any
