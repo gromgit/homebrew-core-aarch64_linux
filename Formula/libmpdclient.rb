473bd3dee626917cd6e5457040522cb939934c3d
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-03-22 17:36:03 +0000

    libmpdclient: depend on meson-internal at build time

--- Formula/libmpdclient.rb
+++ Formula/libmpdclient.rb
@@ -3,6 +3,7 @@ class Libmpdclient < Formula
   homepage "https://www.musicpd.org/libs/libmpdclient/"
   url "https://www.musicpd.org/download/libmpdclient/2/libmpdclient-2.14.tar.xz"
   sha256 "0a84e2791bfe3077cf22ee1784c805d5bb550803dffe56a39aa3690a38061372"
+  revision 1
   head "https://github.com/MusicPlayerDaemon/libmpdclient.git"
 
   bottle do
@@ -13,7 +14,7 @@ class Libmpdclient < Formula
   end
 
   depends_on "doxygen" => :build
-  depends_on "meson" => :build
+  depends_on "meson-internal" => :build
   depends_on "ninja" => :build
 
   def install
