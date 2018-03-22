f460ad6f853c11b006ae57ac9d2dfd6afdf27e8f
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-22 20:25:03 +0000

    jthread: add 1.3.3 bottle.

--- Formula/jthread.rb
+++ Formula/jthread.rb
@@ -4,6 +4,13 @@ class Jthread < Formula
   url "https://research.edm.uhasselt.be/jori/jthread/jthread-1.3.3.tar.bz2"
   sha256 "17560e8f63fa4df11c3712a304ded85870227b2710a2f39692133d354ea0b64f"
 
+  bottle do
+    cellar :any
+    sha256 "2d9c8a2d9e52f9419cd1015d982e06d58963e29c43a44f7ddfbbf6f149e20cc0" => :high_sierra
+    sha256 "099b841458d4d6f4ac3f5e7b453d4ec5b2a50f4dd1a6ccac9614ac72a1c1c90f" => :sierra
+    sha256 "0e846e47e0350f6dc4ca15f5eb6f9e9d2cf7345c115476bc93fc78ac2cb056af" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
 
