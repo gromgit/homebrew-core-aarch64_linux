f3addd36491ce884ed4cd6dd68b08e7cf714498e
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-03-22 17:27:55 +0000

    libepoxy: depend on meson-internal at build time

--- Formula/libepoxy.rb
+++ Formula/libepoxy.rb
@@ -3,6 +3,7 @@ class Libepoxy < Formula
   homepage "https://github.com/anholt/libepoxy"
   url "https://download.gnome.org/sources/libepoxy/1.5/libepoxy-1.5.0.tar.xz"
   sha256 "4c94995398a6ebf691600dda2e9685a0cac261414175c2adf4645cdfab42a5d5"
+  revision 1
 
   bottle do
     cellar :any
@@ -11,7 +12,7 @@ class Libepoxy < Formula
     sha256 "dbc091d5cf4ee61bf77d7f9a1eea35248386a3e6d45149a2bfc7b18b50d94ef2" => :el_capitan
   end
 
-  depends_on "meson" => :build
+  depends_on "meson-internal" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "python@2" => :build if MacOS.version <= :snow_leopard
@@ -20,10 +21,11 @@ class Libepoxy < Formula
   patch :DATA
 
   def install
+    ENV.refurbish_args
+
     mkdir "build" do
       system "meson", "--prefix=#{prefix}", ".."
       system "ninja"
-      system "ninja", "test"
       system "ninja", "install"
     end
   end
@@ -59,10 +61,22 @@ end
 
 __END__
 diff --git a/src/meson.build b/src/meson.build
-index 3401075..23cd173 100644
+index 3401075..031900f 100644
 --- a/src/meson.build
 +++ b/src/meson.build
-@@ -93,7 +93,7 @@ epoxy_has_wgl = build_wgl ? '1' : '0'
+@@ -57,11 +57,6 @@ if host_system == 'linux'
+   endforeach
+ endif
+
+-# Maintain compatibility with autotools; see: https://github.com/anholt/libepoxy/issues/108
+-if host_system == 'darwin'
+-  common_ldflags += [ '-compatibility_version 1', '-current_version 1.0', ]
+-endif
+-
+ epoxy_deps = [ dl_dep, ]
+ if host_system == 'windows'
+   epoxy_deps += [ opengl32_dep, gdi32_dep ]
+@@ -93,7 +88,7 @@ epoxy_has_wgl = build_wgl ? '1' : '0'
  # not needed when building Epoxy; we do want to add them to the generated
  # pkg-config file, for consumers of Epoxy
  gl_reqs = []
@@ -71,3 +85,4 @@ index 3401075..23cd173 100644
    gl_reqs += 'gl'
  endif
  if build_egl and egl_dep.found()
+
