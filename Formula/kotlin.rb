37977bbc573c09e094320169bb825c1d017fda5c
ilovezfs <ilovezfs@icloud.com>
2018-03-23 08:26:28 -0700

    kotlin 1.2.31
    Closes #25673.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/kotlin.rb
+++ Formula/kotlin.rb
@@ -1,8 +1,8 @@
 class Kotlin < Formula
   desc "Statically typed programming language for the JVM"
   homepage "https://kotlinlang.org/"
-  url "https://github.com/JetBrains/kotlin/releases/download/v1.2.30/kotlin-compiler-1.2.30.zip"
-  sha256 "4d6965877301d44241ca6de36480140992dc8a6b1c1884baeb5239ce2c43e071"
+  url "https://github.com/JetBrains/kotlin/releases/download/v1.2.31/kotlin-compiler-1.2.31.zip"
+  sha256 "da99c6ba77a5222955292eafbfe8459a4696960ef8e5585cbe71cee49cdc8026"
 
   bottle :unneeded
 
