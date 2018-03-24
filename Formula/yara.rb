5b66a88e2f886871e0044c42b7453e739b739f8e
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-24 22:40:43 +0000

    yara: update 3.7.1 bottle.

--- Formula/yara.rb
+++ Formula/yara.rb
@@ -7,9 +7,10 @@ class Yara < Formula
 
   bottle do
     cellar :any
-    sha256 "6a995595a48f513196225445dc4ee2889b54d288eb19fb1adfddbeac0b8ac9a9" => :high_sierra
-    sha256 "8769d808d6360ac8440893c4fc2f8d81b4842b3d4631c65441bc81d35f872ce1" => :sierra
-    sha256 "deba43f8ebc4d52d0d8a17febdebf8cc5b0de6c9f81f28e7d6a03aea2a87abe7" => :el_capitan
+    rebuild 1
+    sha256 "cea34d77102cd791be85ce47e01a945b8b0fcf629a8b0c59b3bbd9a2874a1b2e" => :high_sierra
+    sha256 "4e473c3955f931aee46ac0af42e4370a66d9c024d8ac209500d3901bf3a1c036" => :sierra
+    sha256 "052f368216242605b274aff17dc5f97517830660c137c6083361544810ed7320" => :el_capitan
   end
 
   depends_on "autoconf" => :build
