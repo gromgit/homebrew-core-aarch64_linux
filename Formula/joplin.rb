f005b00cfa0123e177b9cab1da0e6546e8e1aa43
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-24 07:30:12 +0000

    joplin: update 1.0.104 bottle.

--- Formula/joplin.rb
+++ Formula/joplin.rb
@@ -7,9 +7,9 @@ class Joplin < Formula
   sha256 "7217367c61ff3626b68f76a1d46ec7a3c1d5d174668b67110cd45e47d52838fd"
 
   bottle do
-    sha256 "f9cec705db2be23f151de79a0cb6aa38e782686698e91e547a811b2ac81afcac" => :high_sierra
-    sha256 "d9a49e9e3d0497a0488f57eee8f4a70de7bc1d0c47eabf3a16321df7f3a42ad5" => :sierra
-    sha256 "8969b7e747118cf98343dae3864b6877f0890071435251e26d6b049d7fd4ee31" => :el_capitan
+    sha256 "495883e6ab9215d34af1943ada3cc98a6002681c97670c3d2d010714d3a1f91c" => :high_sierra
+    sha256 "b7aa58ce4f7dfc16d04962c509983eea9659a3d11c0cae7b498edeae6664bc8a" => :sierra
+    sha256 "204fc084b02f00a1d9be0b7e58dc20e791a4be2334658e2d0ff1533d06395b71" => :el_capitan
   end
 
   depends_on "node"
