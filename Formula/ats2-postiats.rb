7d6320b8b882d8aeed3d11c80760d7f900b449b9
ilovezfs <ilovezfs@icloud.com>
2018-03-24 21:11:18 -0700

    ats2-postiats 0.3.10
    Closes #25717.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/ats2-postiats.rb
+++ Formula/ats2-postiats.rb
@@ -1,8 +1,8 @@
 class Ats2Postiats < Formula
   desc "Programming language with formal specification features"
   homepage "http://www.ats-lang.org/"
-  url "https://downloads.sourceforge.net/project/ats2-lang/ats2-lang/ats2-postiats-0.3.9/ATS2-Postiats-0.3.9.tgz"
-  sha256 "c69a7c58964df26227e77656659129ca4c05205d2ebcacc7084edba818fb6e81"
+  url "https://downloads.sourceforge.net/project/ats2-lang/ats2-lang/ats2-postiats-0.3.10/ATS2-Postiats-0.3.10.tgz"
+  sha256 "a65d0dccbfa3f462ad9348ea400de958166c011197cbef66ecf1c5c5f8baa276"
 
   bottle do
     cellar :any
