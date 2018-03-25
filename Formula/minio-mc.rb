0bee06febd5825cb3475743ce1520e3246f96f84
ilovezfs <ilovezfs@icloud.com>
2018-03-24 21:21:37 -0700

    minio-mc 20180325012222
    Closes #25725.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/minio-mc.rb
+++ Formula/minio-mc.rb
@@ -2,9 +2,9 @@ class MinioMc < Formula
   desc "ls, cp, mkdir, diff and rsync for filesystems and object storage"
   homepage "https://github.com/minio/mc"
   url "https://github.com/minio/mc.git",
-    :tag => "RELEASE.2018-02-09T23-07-36Z",
-    :revision => "3987f1405aa7a0faaff019cc3b55da4ded9ebbe5"
-  version "20180209230736"
+    :tag => "RELEASE.2018-03-25T01-22-22Z",
+    :revision => "da5c19848d1e82a24eddb453b01e83d4a0660de4"
+  version "20180325012222"
 
   bottle do
     cellar :any_skip_relocation
