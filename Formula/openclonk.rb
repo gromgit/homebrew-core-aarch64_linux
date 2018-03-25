42161ece3a24f6610945fbba28101ca294cef1ac
Viktor Szakats <vszakats@users.noreply.github.com>
2018-03-25 19:19:05 +0000

    openclonk: secure url(s)

--- Formula/openclonk.rb
+++ Formula/openclonk.rb
@@ -1,7 +1,7 @@
 class Openclonk < Formula
   desc "Multiplayer action game"
-  homepage "http://www.openclonk.org"
-  url "http://www.openclonk.org/builds/release/7.0/openclonk-7.0-src.tar.bz2"
+  homepage "https://www.openclonk.org/"
+  url "https://www.openclonk.org/builds/release/7.0/openclonk-7.0-src.tar.bz2"
   sha256 "bc1a231d72774a7aa8819e54e1f79be27a21b579fb057609398f2aa5700b0732"
   revision 2
   head "https://github.com/openclonk/openclonk", :using => :git
