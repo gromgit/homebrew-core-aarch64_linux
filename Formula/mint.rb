2497ff030e030816b9f0151529e517e5ec4b983e
Wolfgang Lutz <wlut@num42.de>
2018-03-25 12:32:02 +0200

    mint 0.7.1 (new formula)
    Closes #25734.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/mint.rb
@@ -0,0 +1,19 @@
+class Mint < Formula
+  desc "Dependency manager that installs and runs Swift command-line tool packages"
+  homepage "https://github.com/yonaskolb/Mint"
+  url "https://github.com/yonaskolb/Mint/archive/0.7.1.tar.gz"
+  sha256 "986dd967b818ee044ed0568135db8be2083ad6c3995fe6281ada9b42b224a2ec"
+
+  depends_on :xcode => ["9.0", :build]
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    # Test by showing the help scree
+    system "#{bin}/mint", "--help"
+    # Test showing list of installed tools
+    system "#{bin}/mint", "list"
+  end
+end
