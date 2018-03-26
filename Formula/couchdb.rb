36d7cd454556506c2a70795d454a9e2a4e7fe3e6
commitay <commitay@users.noreply.github.com>
2018-03-27 09:21:28 +1000

    couchdb: revision for icu4c

--- Formula/couchdb.rb
+++ Formula/couchdb.rb
@@ -3,7 +3,7 @@ class Couchdb < Formula
   homepage "https://couchdb.apache.org/"
   url "https://www.apache.org/dyn/closer.cgi?path=/couchdb/source/1.7.1/apache-couchdb-1.7.1.tar.gz"
   sha256 "91200aa6fbc6fa5e2f3d78ef40e39d8c1ec7c83ea1c2cd730d270658735b2cad"
-  revision 3
+  revision 4
 
   bottle do
     sha256 "6215cd6abbf100a301579ef95d1c94a90c3c2bc84484a91edb0c88bb9b1ae626" => :high_sierra
