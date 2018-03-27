c027e935b2fde609b46e979272b08eb37a868950
ilovezfs <ilovezfs@icloud.com>
2018-03-26 23:02:34 -0700

    libgit2 0.27.0

--- Formula/libgit2.rb
+++ Formula/libgit2.rb
@@ -1,19 +1,14 @@
 class Libgit2 < Formula
   desc "C library of Git core methods that is re-entrant and linkable"
   homepage "https://libgit2.github.com/"
-  url "https://github.com/libgit2/libgit2/archive/v0.26.3.tar.gz"
-  sha256 "0da4e211dfb63c22e5f43f2a4a5373e86a140afa88a25ca6ba3cc2cae58263d2"
+  url "https://github.com/libgit2/libgit2/archive/v0.27.0.tar.gz"
+  sha256 "545b0458292c786aba334f1bf1c8f73600ae73dd7205a7bb791a187ee48ab8d2"
   head "https://github.com/libgit2/libgit2.git"
 
   bottle do
     sha256 "d65cbc0cd61400e9146a8b5902a340093aa2a488ed78e7d98562f3742c5ed8b3" => :high_sierra
     sha256 "c15620661180e29df16c5061f8b3a692f14c33624584ce6ca69ae0050e99c840" => :sierra
     sha256 "1ef3c29008e961a6b739a7658fe30aaa9d1898004c84075178bd13abdc97c410" => :el_capitan
-  end
-
-  devel do
-    url "https://github.com/libgit2/libgit2/archive/v0.27.0-rc2.tar.gz"
-    sha256 "7ba5b1155f3a35da63654f29465ab7e39e616a039b05bd639e38194e9c2784be"
   end
 
   depends_on "pkg-config" => :build
