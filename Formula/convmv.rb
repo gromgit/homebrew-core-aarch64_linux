3b19d74f3408145714295900fcdefc020940dd39
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-27 05:30:02 +0000

    convmv: update 2.05 bottle.

--- Formula/convmv.rb
+++ Formula/convmv.rb
@@ -6,9 +6,9 @@ class Convmv < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "805751537d94d301948209f644c89511b4a98128524256b057e6aed8953ee96c" => :high_sierra
-    sha256 "2487ad46c8aaee4e31ad2b8bc1e6f539810857ed339bfe3aab801549a170b2b8" => :sierra
-    sha256 "a39250872cd3158dd7a5de91c8ca45c77b6665aa721a8d7e14cec2670d616675" => :el_capitan
+    sha256 "856021a73afb22052e496ced9eb1a7386d810a6d75903aac99feb98298801ea8" => :high_sierra
+    sha256 "cc6cf7ff9cfd8909a76e29dd6ddbdddc9ad95638e154b72a36fe6c255e3a367d" => :sierra
+    sha256 "43278a7c7ef7720e20fed3179ff8519230678d3fd4e98f4b0e8e796b5fdb40ac" => :el_capitan
   end
 
   def install
