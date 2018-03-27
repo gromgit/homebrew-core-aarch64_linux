3ab8bf25db0e429219d457571e4b819204f39035
Alex Gaynor <alex.gaynor@gmail.com>
2018-03-27 10:13:24 -0400

    openssl@1.1 1.1.0h
    Closes #25837.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/openssl@1.1.rb
+++ Formula/openssl@1.1.rb
@@ -1,11 +1,10 @@
 class OpensslAT11 < Formula
   desc "Cryptography and SSL/TLS Toolkit"
   homepage "https://openssl.org/"
-  url "https://www.openssl.org/source/openssl-1.1.0g.tar.gz"
-  mirror "https://dl.bintray.com/homebrew/mirror/openssl@1.1-1.1.0g.tar.gz"
-  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.1.0g.tar.gz"
-  sha256 "de4d501267da39310905cb6dc8c6121f7a2cad45a7707f76df828fe1b85073af"
-  revision 1
+  url "https://www.openssl.org/source/openssl-1.1.0h.tar.gz"
+  mirror "https://dl.bintray.com/homebrew/mirror/openssl@1.1-1.1.0h.tar.gz"
+  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.1.0h.tar.gz"
+  sha256 "5835626cde9e99656585fc7aaa2302a73a7e1340bf8c14fd635a62c66802a517"
   version_scheme 1
 
   bottle do
