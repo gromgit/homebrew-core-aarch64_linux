9b7541b139a07c7fa89bdc386cbaafe2f711bcf8
Theodore Sentoukas <theodore.sentoukas@gmail.com>
2018-03-28 00:54:16 +0300

    xz: update test (#25848)

--- Formula/xz.rb
+++ Formula/xz.rb
@@ -31,7 +31,7 @@ class Xz < Formula
 
     # compress: data.txt -> data.txt.xz
     system bin/"xz", path
-    assert !path.exist?
+    refute_predicate path, :exist?
 
     # decompress: data.txt.xz -> data.txt
     system bin/"xz", "-d", "#{path}.xz"
