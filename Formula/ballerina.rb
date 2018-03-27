6a016a2ea11a6d007340f880151b81f48ef796f0
Rasika Perera <rasikap@wso2.com>
2018-03-28 03:24:40 +0530

    ballerina 0.964.0 (#25842)

--- Formula/ballerina.rb
+++ Formula/ballerina.rb
@@ -1,8 +1,8 @@
 class Ballerina < Formula
   desc "The flexible, powerful and beautiful programming language"
   homepage "https://ballerinalang.org/"
-  url "https://ballerinalang.org/downloads/ballerina-runtime/ballerina-0.96.0.zip"
-  sha256 "7bbaecbbce0f2132854104a7975dec7e9019ed699536dedca3499d42f908cc87"
+  url "https://ballerinalang.org/downloads/ballerina-tools/ballerina-tools-0.964.0.zip"
+  sha256 "0ea872b63807e7e59105a353e9f7b571d8321526e3defe6a1773dc44fb6c0c7c"
 
   bottle :unneeded
 
@@ -10,25 +10,32 @@ class Ballerina < Formula
 
   def install
     # Remove Windows files
-    rm "bin/ballerina.bat"
+    rm Dir["bin/*.bat"]
 
     chmod 0755, "bin/ballerina"
+    chmod 0755, "bin/composer"
 
     inreplace ["bin/ballerina"] do |s|
-      # Translate ballerina script
       s.gsub! /^BALLERINA_HOME=.*$/, "BALLERINA_HOME=#{libexec}"
-      # dos to unix (bug fix for version 2.3.11)
+      s.gsub! /\r?/, ""
+    end
+
+    inreplace ["bin/composer"] do |s|
+      s.gsub! /^BASE_DIR=.*$/, "BASE_DIR=#{libexec}/bin"
+      s.gsub! /^PRGDIR=.*$/, "PRGDIR=#{libexec}/bin"
       s.gsub! /\r?/, ""
     end
 
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/ballerina"
+    bin.install_symlink libexec/"bin/composer"
   end
 
   test do
     (testpath/"helloWorld.bal").write <<~EOS
+      import ballerina.io;
       function main (string[] args) {
-        println("Hello, World!");
+        io:println("Hello, World!");
       }
     EOS
     output = shell_output("#{bin}/ballerina run helloWorld.bal")
