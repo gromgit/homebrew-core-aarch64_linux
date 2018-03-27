c1974278eb1b369a3e421c70156b333bf71a8326
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-27 13:06:24 +0000

    media-info: update 18.03.1 bottle.

--- Formula/media-info.rb
+++ Formula/media-info.rb
@@ -7,9 +7,9 @@ class MediaInfo < Formula
 
   bottle do
     cellar :any
-    sha256 "6905393cf50025850fe7dacd7d2384c64e88800b8f130369499a1fccc176dad1" => :high_sierra
-    sha256 "75119d9a20239927ffa6b97a067e41bde291ef14537ba410f9d2a34f00e7fdf9" => :sierra
-    sha256 "3e3388365d22f77cc4fee27596c7a0e22849781e1e44f6bcb4abe2e09ed6a4b8" => :el_capitan
+    sha256 "be8dd1c8bfb6e0f7e9a25ab944a49a8ba833a5977a4748da2e2c00ffcc8e7a12" => :high_sierra
+    sha256 "eb7e5952204d280fc5edc5851a75530d5ce1e7d59c6ac5fd68ed69c55e623fdd" => :sierra
+    sha256 "e39b77f8806962c4244895252d3a6214de9e3eaae5bb7796f23069195824ba09" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
