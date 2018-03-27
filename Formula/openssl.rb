4f05aa7690651abb4e6f02d7d8aa802c545ae3d1
Alex Gaynor <alex.gaynor@gmail.com>
2018-03-27 10:14:42 -0400

    openssl 1.0.2o
    Closes #25838.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/openssl.rb
+++ Formula/openssl.rb
@@ -4,11 +4,11 @@
 class Openssl < Formula
   desc "SSL/TLS cryptography library"
   homepage "https://openssl.org/"
-  url "https://www.openssl.org/source/openssl-1.0.2n.tar.gz"
-  mirror "https://dl.bintray.com/homebrew/mirror/openssl-1.0.2n.tar.gz"
-  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.0.2n.tar.gz"
-  mirror "http://artfiles.org/openssl.org/source/openssl-1.0.2n.tar.gz"
-  sha256 "370babb75f278c39e0c50e8c4e7493bc0f18db6867478341a832a982fd15a8fe"
+  url "https://www.openssl.org/source/openssl-1.0.2o.tar.gz"
+  mirror "https://dl.bintray.com/homebrew/mirror/openssl-1.0.2o.tar.gz"
+  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.0.2o.tar.gz"
+  mirror "http://artfiles.org/openssl.org/source/openssl-1.0.2o.tar.gz"
+  sha256 "ec3f5c9714ba0fd45cb4e087301eb1336c317e0d20b575a125050470e8089e4d"
 
   bottle do
     sha256 "6d3f21c1f60c5fd77df43eb470fbe753ab030b565c9360ebb7335377cb13e047" => :high_sierra
