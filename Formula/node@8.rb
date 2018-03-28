e3377af209c420507efbc7efa21d07648e81e401
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-28 21:08:02 +0000

    node@8: update 8.11.0 bottle.

--- Formula/node@8.rb
+++ Formula/node@8.rb
@@ -6,9 +6,9 @@ class NodeAT8 < Formula
   head "https://github.com/nodejs/node.git", :branch => "v8.x-staging"
 
   bottle do
-    sha256 "e737c61870d5a320b81d97346037fb9d91f65dcf3f3bb6c9aa3cbb2cc233889e" => :high_sierra
-    sha256 "e8e1f9baa66f7ba57078ba1566b20a57e6e4ba7999eef591a2983eb328e8b1a9" => :sierra
-    sha256 "43f77dc6882cc921d389a2e6c121959dc55e971cdd9200c6cc8feedc0845f2e9" => :el_capitan
+    sha256 "4fb15af89339d7beb6d4e430718b95866a9d00241d50eff306ffa9d98940640d" => :high_sierra
+    sha256 "7fff6b5b2d7a8191a2c894597f87ed7fc2146cf4b9d612e1089c010b002b08e0" => :sierra
+    sha256 "34c5c7afec9cd3829190fc64a398db8481efd4ec85a2155bdf4332a6b245e3c0" => :el_capitan
   end
 
   keg_only :versioned_formula
