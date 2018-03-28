0a21a5e43f2720d44fdc69cc550a3f44fbccb1e3
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-28 05:14:45 +0000

    mame: update 0.196 bottle.

--- Formula/mame.rb
+++ Formula/mame.rb
@@ -8,9 +8,9 @@ class Mame < Formula
 
   bottle do
     cellar :any
-    sha256 "d1c047e4fae53ca8939fbcefaa32e2f5d4da8f40d29242b012550d8e39cab40d" => :high_sierra
-    sha256 "ca2dddf314d0810e3459c6ee364764b7457e8d69b147a2570df60b7ec4955a50" => :sierra
-    sha256 "df5792d89a5476b5c36f3a41c7368a53c8df7d5860452918102cb4e7e59bf7c2" => :el_capitan
+    sha256 "299cbf496f5aa2678559e8a724c41821905ad31334d6644c1ee064077f8aca37" => :high_sierra
+    sha256 "a58d9f8aefd2f17181eec23f32dcd12ca900e95c5522d1dfc246fb62620c4032" => :sierra
+    sha256 "923a484f1676a8a633516aea340141880917a85bd498124a4d1d04cdeee7f046" => :el_capitan
   end
 
   depends_on :macos => :yosemite
