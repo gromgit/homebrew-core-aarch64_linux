0e88f1b3be8fe6625225ce425f33e252abe6a38c
ilovezfs <ilovezfs@icloud.com>
2018-03-28 15:14:05 -0700

    htop: remove patch
    Closes #25887.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/htop.rb
+++ Formula/htop.rb
@@ -1,19 +1,9 @@
 class Htop < Formula
   desc "Improved top (interactive process viewer)"
   homepage "https://hisham.hm/htop/"
-
-  stable do
-    url "https://hisham.hm/htop/releases/2.1.0/htop-2.1.0.tar.gz"
-    sha256 "3260be990d26e25b6b49fc9d96dbc935ad46e61083c0b7f6df413e513bf80748"
-
-    # Remove for > 2.1.0
-    # Upstream commit from 25 Mar 2018 "Keep scanning threads for versions
-    # before High Sierra 13.0.0 and after 13.3.0."
-    patch do
-      url "https://github.com/hishamhm/htop/commit/52e2884f.patch?full_index=1"
-      sha256 "5daeb1a532cf8e4524a4f3093792a687d77b50c3f78c713b410ebb2bc9ae79b2"
-    end
-  end
+  url "https://hisham.hm/htop/releases/2.1.0/htop-2.1.0.tar.gz"
+  sha256 "3260be990d26e25b6b49fc9d96dbc935ad46e61083c0b7f6df413e513bf80748"
+  revision 1
 
   bottle do
     sha256 "2b821083315e53a46381552d574d078e02494692e8da6068f36e450f6800dd44" => :high_sierra
