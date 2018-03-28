d08760ea400c5a312fa6bd3d5cdc11e987d62d7c
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-28 06:20:47 +0000

    headphones: update 0.5.19 bottle.

--- Formula/headphones.rb
+++ Formula/headphones.rb
@@ -7,10 +7,9 @@ class Headphones < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6038f613b50ab4d7c3f67a1a45547ba2349d96fc158fd99d4e1a46357e5d7992" => :high_sierra
-    sha256 "913f1f0ea6600b6aa4bb6f4f9fcfd6c5447f072334b024faf40da2d7ed6b0600" => :sierra
-    sha256 "fb9288d3885ac6ee18b6ee3292d7fe886f8b5d538b4876f3bdd32309e3643431" => :el_capitan
-    sha256 "be688187ebdfd25f221b227a9aa6df3e8015788f564bdfdc524cd99834540d80" => :yosemite
+    sha256 "c22670540bfa23e92a25a0efa6af4fe9f29967a45d4b8c5d8de0b28c060ac9d5" => :high_sierra
+    sha256 "d64f16c7ce5484cd9e69902d1b4ea8299e9b093b8837c624ed0ef623aeb748fb" => :sierra
+    sha256 "a17c424aac1d91fb570d3e63db548c37b4c64f92ed988bb935cb4fa02c12fc5a" => :el_capitan
   end
 
   resource "Markdown" do
