00b2b5599ea6b21764ec9959851122583c44192c
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-29 06:16:06 +0000

    zebra: update 2.1.3_2 bottle.

--- Formula/zebra.rb
+++ Formula/zebra.rb
@@ -6,9 +6,9 @@ class Zebra < Formula
   revision 2
 
   bottle do
-    sha256 "6f7665efc555ed08e5d7b9dd0de741f4e014d26462b163adb29e606456803517" => :high_sierra
-    sha256 "45bda05a03c8c1219550081e21b8e9d3ff833bf0b47b1e1e12cf561c401636d7" => :sierra
-    sha256 "43750c886af867ffffefcaa2e21ac3ac2c1d20cb447fce35329320ff8a965b03" => :el_capitan
+    sha256 "487e9fddecbccdbf58985ee2b9c349fbd88cb108d74b4fb948ebb00ddaecf98e" => :high_sierra
+    sha256 "b939f3348f27580805472d5da3ef631728b4b1e0e438cbb0814326ea8362c2bb" => :sierra
+    sha256 "630d7efb1da41b6c24beae389a52649d645659564b08a2e321d340182dd5c9bc" => :el_capitan
   end
 
   depends_on "icu4c" => :recommended
