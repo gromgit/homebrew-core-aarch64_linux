f5ac3806ee7ba9c2c0089ac30da6b6dfdcb2428c
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-29 06:16:13 +0000

    tarantool: update 1.9.0.61 bottle.

--- Formula/tarantool.rb
+++ Formula/tarantool.rb
@@ -6,9 +6,9 @@ class Tarantool < Formula
   head "https://github.com/tarantool/tarantool.git", :branch => "2.0", :shallow => false
 
   bottle do
-    sha256 "3aaab86529a9a676ca66f84a15dea7700e70ae68d2722dbf222c51e83faefda9" => :high_sierra
-    sha256 "3584b932f7f04786313f2b385ac6db5880ebdbaa1e023ebccb82ca70ca1bad70" => :sierra
-    sha256 "37717b48bf4228c7806ec7200ed8fc0e2015e772983a04e2b487dc872b5be3c2" => :el_capitan
+    sha256 "d13407acd3af07a510b96448e8e9c3d28c73d5198c8ead7e9c7ccf2c761d6970" => :high_sierra
+    sha256 "187494a7e726c3e5e698f24f1b60f4badf2e6346c5a4aedcc004eb66a043067d" => :sierra
+    sha256 "626f2ffd8454ae39fc161da20522ccba5981bf54ac3e9c3f28447f630ad3cae2" => :el_capitan
   end
 
   depends_on "cmake" => :build
