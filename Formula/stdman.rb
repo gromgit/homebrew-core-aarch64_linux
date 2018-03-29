491a83e29db849f691042488aa949c161ee97099
ilovezfs <ilovezfs@icloud.com>
2018-03-28 22:58:19 -0700

    stdman 2018.03.11
    Closes #25909.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/stdman.rb
+++ Formula/stdman.rb
@@ -1,8 +1,8 @@
 class Stdman < Formula
   desc "Formatted C++11/14/17 stdlib man pages from cppreference.com"
   homepage "https://github.com/jeaye/stdman"
-  url "https://github.com/jeaye/stdman/archive/2017.04.02.tar.gz"
-  sha256 "64f33e843c646de54745f234df7688df400a6f65f738408406cb48bcae2c852d"
+  url "https://github.com/jeaye/stdman/archive/2018.03.11.tar.gz"
+  sha256 "d29e6b34cb5ba9905360cee6adcdf8c49e7f11272521bf2e10b42917486840e8"
   version_scheme 1
   head "https://github.com/jeaye/stdman.git"
 
