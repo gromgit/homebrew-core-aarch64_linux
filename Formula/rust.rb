4f2a2b9d9f054aa7277e222812aef7843bcdd729
Alex Gaynor <alex.gaynor@gmail.com>
2018-03-29 07:34:22 -0400

    rust 1.25.0
    Closes #25915.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/rust.rb
+++ Formula/rust.rb
@@ -1,11 +1,10 @@
 class Rust < Formula
   desc "Safe, concurrent, practical language"
   homepage "https://www.rust-lang.org/"
-  revision 1
 
   stable do
-    url "https://static.rust-lang.org/dist/rustc-1.24.1-src.tar.gz"
-    sha256 "3ea53d45e8d2e9a41afb3340cf54b9745f845b552d802d607707cf04450761ef"
+    url "https://static.rust-lang.org/dist/rustc-1.25.0-src.tar.gz"
+    sha256 "eef63a0aeea5147930a366aee78cbde248bb6e5c6868801bdf34849152965d2d"
 
     resource "cargo" do
       url "https://github.com/rust-lang/cargo.git",
@@ -56,8 +55,8 @@ class Rust < Formula
 
   resource "cargobootstrap" do
     # From https://github.com/rust-lang/rust/blob/#{version}/src/stage0.txt
-    url "https://static.rust-lang.org/dist/2018-01-04/cargo-0.24.0-x86_64-apple-darwin.tar.gz"
-    sha256 "b6f7c662ea75a94f5a5e41c2fee95f09a5ba168429ac8cdd41f6ba2c78d1b07f"
+    url "https://static.rust-lang.org/dist/2018-02-15/cargo-0.25.0-x86_64-apple-darwin.tar.gz"
+    sha256 "92782612068e796a6a85f5c2bc11ad748b961cb26e7b55f8f125d33cc402c1ca"
   end
 
   def install
@@ -111,7 +110,7 @@ class Rust < Formula
     end
 
     # Remove any binary files; as Homebrew will run ranlib on them and barf.
-    rm_rf Dir["src/{llvm,test,librustdoc,etc/snapshot.pyc}"]
+    rm_rf Dir["src/{llvm,llvm-emscripten,test,librustdoc,etc/snapshot.pyc}"]
     (pkgshare/"rust_src").install Dir["src/*"]
 
     rm_rf prefix/"lib/rustlib/uninstall.sh"
