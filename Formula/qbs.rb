34f3d1c049bc05c59e5e30aae1241fc5d0747796
Joerg Bornemann <joerg.bornemann@qt.io>
2018-03-29 10:55:09 +0200

    qbs 1.11.0
    Closes #25912.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/qbs.rb
+++ Formula/qbs.rb
@@ -1,9 +1,8 @@
 class Qbs < Formula
   desc "Build tool for developing projects across multiple platforms"
   homepage "https://wiki.qt.io/Qbs"
-  url "https://download.qt.io/official_releases/qbs/1.10.0/qbs-src-1.10.0.tar.gz"
-  sha256 "38afae3b697b96e07d53cfc64dd03533fddec052ecdc08d8e7779440c36c3ecd"
-  head "https://code.qt.io/qt-labs/qbs.git"
+  url "https://download.qt.io/official_releases/qbs/1.11.0/qbs-src-1.11.0.tar.gz"
+  sha256 "6ce30c468e688f6843468324a34a7191409b471c3ff6f3e834ef8bfa7b3467cd"
 
   bottle do
     cellar :any
