fd3523dc6d58ec65e8bbd6d60bb61363e20895cb
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-29 05:33:03 +0000

    fluent-bit: update 0.12.16 bottle.

--- Formula/fluent-bit.rb
+++ Formula/fluent-bit.rb
@@ -7,9 +7,9 @@ class FluentBit < Formula
 
   bottle do
     cellar :any
-    sha256 "8b257feac620d2d4f72b5725819be525b7abc742a35ccc006390429fcbb1a500" => :high_sierra
-    sha256 "9da5342fcfbfa29e31004efd7b873960124f67e4386ebc47e81f222ff79ba794" => :sierra
-    sha256 "739e9338f2d489b8d8b31a2937f810662e83fe5109a630ac4bd71fbaf30ab702" => :el_capitan
+    sha256 "bb11829682c5135708301fdb485d99e9744a74a154d30efa0da2773f6275d602" => :high_sierra
+    sha256 "4416c6776aa5288dfcecf76ba8960aa0dbf1b82bdce09db792b6da92b2254a95" => :sierra
+    sha256 "35e80b5a933635a3cf1409efb3953a03ce86208294c7658bc675bb9f11880d59" => :el_capitan
   end
 
   depends_on "cmake" => :build
