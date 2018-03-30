94e4d0e9fa12795237656d01b89fc74aa9f2caa1
ilovezfs <ilovezfs@icloud.com>
2018-03-30 07:20:49 -0700

    lmdb 0.9.22
    Closes #25965.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/lmdb.rb
+++ Formula/lmdb.rb
@@ -1,8 +1,8 @@
 class Lmdb < Formula
   desc "Lightning memory-mapped database: key-value data store"
   homepage "https://symas.com/mdb-and-sqlite/"
-  url "https://github.com/LMDB/lmdb/archive/LMDB_0.9.21.tar.gz"
-  sha256 "1187b635a4cc415bb6972bba346121f81edd996e99b8f0816151d4090f90b559"
+  url "https://github.com/LMDB/lmdb/archive/LMDB_0.9.22.tar.gz"
+  sha256 "f3927859882eb608868c8c31586bb7eb84562a40a6bf5cc3e13b6b564641ea28"
   version_scheme 1
   head "https://github.com/LMDB/lmdb.git", :branch => "mdb.master"
 
