22e1c2a206136c61e719fbf36beee8cb718ea211
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-30 05:27:34 +0000

    sysdig: update 0.21.0 bottle.

--- Formula/sysdig.rb
+++ Formula/sysdig.rb
@@ -5,9 +5,9 @@ class Sysdig < Formula
   sha256 "3ba35ca1e84dd4487bdeff078ff0e48c862fa6887688f9c8d4a076865c6f9a05"
 
   bottle do
-    sha256 "e4a6c6d3fdc2e2af66ef4a187dea9a1eef21726efffc6898d1489a1bcc4325b2" => :high_sierra
-    sha256 "f860f82acc1405a29e181582ed1abf6daeb99623e6186dd90e20e20ef7b10cde" => :sierra
-    sha256 "583116ada7e9981e62a776f8aa3aae66ac912e3a5c5b0da1f33da9dad98da520" => :el_capitan
+    sha256 "79a27d9234019ebb44a103d6c697c5fe5e47b4790c90bfbac75629f7125577fb" => :high_sierra
+    sha256 "2c33f831dcd89e94b1a232484f5eeb79c9fee3f65be1f7323e366d41e7e1a17a" => :sierra
+    sha256 "ab93f46d46f24ea996b3967e8158882da094e366d08b4a92f4e060943a1c726a" => :el_capitan
   end
 
   depends_on "cmake" => :build
