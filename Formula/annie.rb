97eab06a3a773d160f1e2d29425e0c9d5c84977f
iawia002 <z2d@jifangcheng.com>
2018-03-30 11:31:23 +0800

    annie 0.6.4 (new formula)
    Closes #25944.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/annie.rb
@@ -0,0 +1,21 @@
+class Annie < Formula
+  desc "Fast, simple and clean video downloader"
+  homepage "https://github.com/iawia002/annie"
+  url "https://github.com/iawia002/annie/archive/0.6.4.tar.gz"
+  sha256 "e7f9caf29bf4b48e9329f0c7efba7988bcd2f8d0361b710022a40cd033164d4f"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/iawia002/annie").install buildpath.children
+    cd "src/github.com/iawia002/annie" do
+      system "go", "build", "-o", bin/"annie"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    system bin/"annie", "-i", "https://www.bilibili.com/video/av20203945"
+  end
+end
