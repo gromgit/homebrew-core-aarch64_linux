af75d456177c2b5d6ef33872f10ff1ec77be176d
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2018-03-30 13:38:57 -0700

    kubectx 0.4.1 (#25972)
    - stop installing utils.bash, as it no longer exists
    - add fish completion script

--- Formula/kubectx.rb
+++ Formula/kubectx.rb
@@ -1,8 +1,8 @@
 class Kubectx < Formula
   desc "Tool that can switch between kubectl contexts easily and create aliases"
   homepage "https://github.com/ahmetb/kubectx"
-  url "https://github.com/ahmetb/kubectx/archive/v0.4.0.tar.gz"
-  sha256 "1db44fc6d5af97e2222f6d487f8c80b01fa40e14f50f2c3781f24bc33a58340e"
+  url "https://github.com/ahmetb/kubectx/archive/v0.4.1.tar.gz"
+  sha256 "bdde688a6382c7e0e23fdecb204cb48ce8204cad213fdaf45c0fb5d929f76937"
   head "https://github.com/ahmetb/kubectx.git"
 
   bottle :unneeded
@@ -14,12 +14,13 @@ class Kubectx < Formula
   def install
     bin.install "kubectx" => build.with?("short-names") ? "kctx" : "kubectx"
     bin.install "kubens" => build.with?("short-names") ? "kns" : "kubens"
-    include.install "utils.bash"
 
     bash_completion.install "completion/kubectx.bash" => "kubectx"
     bash_completion.install "completion/kubens.bash" => "kubens"
     zsh_completion.install "completion/kubectx.zsh" => "_kubectx"
     zsh_completion.install "completion/kubens.zsh" => "_kubens"
+    fish_completion.install "completion/kubectx.fish" => "_kubectx"
+    fish_completion.install "completion/kubens.fish" => "_kubens"
   end
 
   test do
