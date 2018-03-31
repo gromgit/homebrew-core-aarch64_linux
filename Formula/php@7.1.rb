eaa7df79f7c48d913e72ccc99e447948adfbe261
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-31 01:32:50 +0000

    php@7.1: update 7.1.16 bottle.

--- Formula/php@7.1.rb
+++ Formula/php@7.1.rb
@@ -5,9 +5,9 @@ class PhpAT71 < Formula
   sha256 "a5d67e477248a3911af7ef85c8400c1ba8cd632184186fd31070b96714e669f1"
 
   bottle do
-    sha256 "8bf2586cb4d7a9f7a78d66326bc571d3b0ee04a86356af8867cd9c2fad43e103" => :high_sierra
-    sha256 "93b691515a9b82e072872c120e73eaf0ffc7e61fd9895fab8f0e366ae0aeeb78" => :sierra
-    sha256 "c3b93272cebb1a01ad37be3353ca38186054f5491c9f0747539c9126af0e41f1" => :el_capitan
+    sha256 "cd22b9c7cd6986c7e80acaac6eb971acbf01ba9b3a4401df15d6d193b0f699a8" => :high_sierra
+    sha256 "e5076139cf49e588eaae0dc19bca48d1f64723332e7425511fd5d8a9df30c6bd" => :sierra
+    sha256 "bd8156ab99f193771156a04c642addb023b193d127c8d75e8f2fc916b4a5b5ec" => :el_capitan
   end
 
   keg_only :versioned_formula
