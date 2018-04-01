5f40d9fb3b07151dbe3d6b4af37bdc54a2be51b5
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-01 11:36:36 +0000

    jsonrpc-glib: add 3.28.0 bottle.

--- Formula/jsonrpc-glib.rb
+++ Formula/jsonrpc-glib.rb
@@ -4,6 +4,12 @@ class JsonrpcGlib < Formula
   url "https://download.gnome.org/sources/jsonrpc-glib/3.28/jsonrpc-glib-3.28.0.tar.xz"
   sha256 "ba0b0f05d58ba47d5938a54787b44ea875648214f032f75d4344162d8f736a25"
 
+  bottle do
+    sha256 "75857274eb9e324e75aba750264b6619851460ad946dd02f0f5ea455a036fced" => :high_sierra
+    sha256 "4b198f2c21d0eed24f6d8d99f84ec7e596947bcf9b693fa02c622cc7aa3c8b75" => :sierra
+    sha256 "3f82ca1afc7630f79e6f923d1c7964081171764f3de9a5905faf1d566bfb65f8" => :el_capitan
+  end
+
   depends_on "gobject-introspection" => :build
   depends_on "meson-internal" => :build
   depends_on "ninja" => :build
