503fe39c76d2893bb7719643c5913025f4bac51f
ilovezfs <ilovezfs@icloud.com>
2018-03-31 21:43:47 -0700

    libidn 1.34
    Closes #26012.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libidn.rb
+++ Formula/libidn.rb
@@ -1,9 +1,9 @@
 class Libidn < Formula
   desc "International domain name library"
   homepage "https://www.gnu.org/software/libidn/"
-  url "https://ftp.gnu.org/gnu/libidn/libidn-1.33.tar.gz"
-  mirror "https://ftpmirror.gnu.org/libidn/libidn-1.33.tar.gz"
-  sha256 "44a7aab635bb721ceef6beecc4d49dfd19478325e1b47f3196f7d2acc4930e19"
+  url "https://ftp.gnu.org/gnu/libidn/libidn-1.34.tar.gz"
+  mirror "https://ftpmirror.gnu.org/libidn/libidn-1.34.tar.gz"
+  sha256 "3719e2975f2fb28605df3479c380af2cf4ab4e919e1506527e4c7670afff6e3c"
 
   bottle do
     cellar :any
