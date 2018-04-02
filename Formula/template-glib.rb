2a8344a73c63b79dc17ff4cee3981ba73ed13789
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-02 17:42:26 +0000

    template-glib: add 3.28.0 bottle.

--- Formula/template-glib.rb
+++ Formula/template-glib.rb
@@ -4,6 +4,12 @@ class TemplateGlib < Formula
   url "https://download.gnome.org/sources/template-glib/3.28/template-glib-3.28.0.tar.xz"
   sha256 "6c74426efd4358fd91a52c32ed030c0cad1633f900fd55ac81a8b3e4026171a1"
 
+  bottle do
+    sha256 "e69fb0604b5389c1e52cef2eed885b167a836c251b4bedda056f10a1569e6867" => :high_sierra
+    sha256 "f3c4d2849cf3d95c885a278e4b1db431beae9d5b8099457ca45df55f54e1436e" => :sierra
+    sha256 "c20b6fc9631b3bbf72179a030c6e3ac01829d8513452c8bda55cc8096153491c" => :el_capitan
+  end
+
   depends_on "bison" => :build # does not appear to work with system bison
   depends_on "meson-internal" => :build
   depends_on "ninja" => :build
