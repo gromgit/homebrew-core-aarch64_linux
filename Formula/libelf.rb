07325403c05f55ef9179cadf02efd15105b60c10
Mike McQuaid <mike@mikemcquaid.com>
2018-04-02 10:14:08 +0100

    libelf: fix brew style. (#26060)
    Fix some weird indentation, use double-quotes and wrap a really long
    string.

--- Formula/libelf.rb
+++ Formula/libelf.rb
@@ -26,9 +26,12 @@ class Libelf < Formula
   end
 
   test do
-    elf_content = "7F454C460101010000000000000000000200030001000000548004083400000000000000000000003400200001000000000000000100000000000000008004080080040874000000740000000500000000100000B00431DB43B96980040831D2B20CCD8031C040CD8048656C6C6F20776F726C640A"
+    elf_content =  "7F454C460101010000000000000000000200030001000000548004083" \
+      "4000000000000000000000034002000010000000000000001000000000000000080040" \
+      "80080040874000000740000000500000000100000B00431DB43B96980040831D2B20CC" \
+      "D8031C040CD8048656C6C6F20776F726C640A"
     File.open(testpath/"elf", "w+b") do |file|
-      file.write([elf_content].pack('H*'))
+      file.write([elf_content].pack("H*"))
     end
 
     (testpath/"test.c").write <<~EOS
@@ -39,14 +42,14 @@ class Libelf < Formula
       int main(void) {
         GElf_Ehdr ehdr;
         int fd = open("elf", O_RDONLY, 0);
-	if (elf_version(EV_CURRENT) == EV_NONE) return 1;
+        if (elf_version(EV_CURRENT) == EV_NONE) return 1;
         Elf *e = elf_begin(fd, ELF_C_READ, NULL);
         if (elf_kind(e) != ELF_K_ELF) return 1;
         if (gelf_getehdr(e, &ehdr) == NULL) return 1;
         printf("%d-bit ELF\\n", gelf_getclass(e) == ELFCLASS32 ? 32 : 64);
-	return 0;
+        return 0;
       }
-      EOS
+    EOS
 
     system ENV.cc, "test.c", "-L#{lib}", "-I#{include}/libelf",
                    "-lelf", "-o", "test"
