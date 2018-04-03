dfd9896494fd72e2a2b4fbd3598537e6043cb067
Yonas Kolb <yonas.kolb@massive.co>
2018-04-03 15:27:38 +1000

    mint 0.8.0
    Closes #26103.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -1,8 +1,8 @@
 class Mint < Formula
   desc "Dependency manager that installs and runs Swift command-line tool packages"
   homepage "https://github.com/yonaskolb/Mint"
-  url "https://github.com/yonaskolb/Mint/archive/0.7.1.tar.gz"
-  sha256 "986dd967b818ee044ed0568135db8be2083ad6c3995fe6281ada9b42b224a2ec"
+  url "https://github.com/yonaskolb/Mint/archive/0.8.0.tar.gz"
+  sha256 "5e3427c73b07d5f307c410ab6ea4f66ef113a278c8c79f3d19544fdbc15c452b"
 
   bottle do
     cellar :any_skip_relocation
