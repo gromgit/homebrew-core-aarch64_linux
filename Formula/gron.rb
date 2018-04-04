50056156607a43fc74655c8ff30d0753627a5f78
ilovezfs <ilovezfs@icloud.com>
2018-04-04 05:20:58 -0700

    gron 0.5.2
    Closes #26156.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gron.rb
+++ Formula/gron.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Gron < Formula
   desc "Make JSON greppable"
   homepage "https://github.com/tomnomnom/gron"
-  url "https://github.com/tomnomnom/gron/archive/v0.5.1.tar.gz"
-  sha256 "062462b8b6e884cd5731b0bc870e9a45f450e056f4367acccddb926079686560"
+  url "https://github.com/tomnomnom/gron/archive/v0.5.2.tar.gz"
+  sha256 "3320bdf238833b1bb8b96bdb7f1b1f662b595d00af3aaaa35653a98a9b8ecad6"
   head "https://github.com/tomnomnom/gron.git"
 
   bottle do
@@ -18,7 +18,7 @@ class Gron < Formula
 
   go_resource "github.com/fatih/color" do
     url "https://github.com/fatih/color.git",
-        :revision => "5df930a27be2502f99b292b7cc09ebad4d0891f4"
+        :revision => "507f6050b8568533fb3f5504de8e5205fa62a114"
   end
 
   go_resource "github.com/nwidger/jsoncolor" do
@@ -28,7 +28,7 @@ class Gron < Formula
 
   go_resource "github.com/pkg/errors" do
     url "https://github.com/pkg/errors.git",
-        :revision => "e881fd58d78e04cf6d0de1217f8707c8cc2249bc"
+        :revision => "816c9085562cd7ee03e7f8188a1cfd942858cded"
   end
 
   def install
