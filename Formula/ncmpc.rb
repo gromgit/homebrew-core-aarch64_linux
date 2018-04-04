26a220be1d10dcff625b84b22072bd9625a444be
ilovezfs <ilovezfs@icloud.com>
2018-04-04 02:07:59 -0700

    ncmpc 0.30
    Closes #26151.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/ncmpc.rb
+++ Formula/ncmpc.rb
@@ -1,8 +1,8 @@
 class Ncmpc < Formula
   desc "Curses Music Player Daemon (MPD) client"
   homepage "https://www.musicpd.org/clients/ncmpc/"
-  url "https://www.musicpd.org/download/ncmpc/0/ncmpc-0.29.tar.xz"
-  sha256 "ef68a9b67172383ea80ee46579015109433fa058728812d2b0ebede660d85f12"
+  url "https://www.musicpd.org/download/ncmpc/0/ncmpc-0.30.tar.xz"
+  sha256 "e3fe0cb58b8a77f63fb1645c2f974b334f1614efdc834ec698ee7d861f1b12a3"
 
   bottle do
     sha256 "c3c7f8c6425c97cff94a2e4c4d243ca1a93eb290e25f9ecc0648f829b6eb2a28" => :high_sierra
@@ -13,11 +13,19 @@ class Ncmpc < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
+  depends_on "gcc" if DevelopmentTools.clang_build_version <= 800
   depends_on "gettext"
   depends_on "glib"
   depends_on "libmpdclient"
 
+  fails_with :clang do
+    build 800
+    cause "error: no matching constructor for initialization of 'value_type'"
+  end
+
   def install
+    sdk = MacOS::CLT.installed? ? "" : MacOS.sdk_path
+
     # Fix undefined symbols _COLORS, _COLS, etc.
     # Reported 21 Sep 2017 https://github.com/MusicPlayerDaemon/ncmpc/issues/6
     (buildpath/"ncurses.pc").write <<~EOS
@@ -25,7 +33,7 @@ class Ncmpc < Formula
       Description: ncurses
       Version: 5.4
       Libs: -L/usr/lib -lncurses
-      Cflags: -I#{MacOS.sdk_path}/usr/include
+      Cflags: -I#{sdk}/usr/include
     EOS
     ENV.prepend_path "PKG_CONFIG_PATH", buildpath
 
