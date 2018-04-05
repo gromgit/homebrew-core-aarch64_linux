cf641c18701f4af533f0155730f7fde6a5193fe4
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-05 16:06:51 +0000

    haproxy: update 1.8.6 bottle.

--- Formula/haproxy.rb
+++ Formula/haproxy.rb
@@ -6,9 +6,9 @@ class Haproxy < Formula
 
   bottle do
     cellar :any
-    sha256 "890c93949fc751be1e81cb392bf87bbfb602f161ac275206a0053748e2b6a598" => :high_sierra
-    sha256 "d6d4d19f2364b5f9958a7e13edfd0c445ce97797b10d759948bba07feb5afbd6" => :sierra
-    sha256 "35cb705f125cd7c177b7ff8355526abcc53c918e0d69dcad9daabb5697278287" => :el_capitan
+    sha256 "68e8066ce2c8515e30c0326f08b0aa8f1d8e34ab44aeffffc6ac0904dedfbf0d" => :high_sierra
+    sha256 "bc3196b68d9295971ab90c912bfb1289c1acd1d024f2e369044daaf67e373656" => :sierra
+    sha256 "ac9abc05081e7d2ccc2472532228e65750d47a79f92abefd6f37a54c6e7c3404" => :el_capitan
   end
 
   depends_on "openssl"
