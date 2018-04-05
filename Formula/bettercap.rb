6403527e9f5686a862f77ad9756a831dffa49c48
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-05 11:06:06 +0000

    bettercap: update 2.4 bottle.

--- Formula/bettercap.rb
+++ Formula/bettercap.rb
@@ -5,10 +5,10 @@ class Bettercap < Formula
   sha256 "c349d9b428da26b713847afc268d033ed3f6cc351a8fdfc7557811146b677e23"
 
   bottle do
-    cellar :any
-    sha256 "f37cb67f4deaf5433c117339ea68350c01f88516001484443872cb4d4be830b5" => :high_sierra
-    sha256 "5f6ed37beb8ad8525eb5fc226d17a8fe5893ecb29a0733347cfb5a7f8a2ade7f" => :sierra
-    sha256 "c15257bc4fcc3d23bdb6e1204757230f6ca843b97507ef4ceefa824e8ec4838a" => :el_capitan
+    cellar :any_skip_relocation
+    sha256 "2e26b07929e45bd16163a51dcd280875f090d716f272fe4ec6df23960e58a4b0" => :high_sierra
+    sha256 "ef93bf4777bb48125700cf2f795340e410f63d72f8ff5701f6523c5ceea6aea1" => :sierra
+    sha256 "dbe475c3a08e31da8e600bda55ee3c5128fe5421a55c351cf839d858d4ada921" => :el_capitan
   end
 
   depends_on "glide" => :build
