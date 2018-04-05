2165947fae211fdeb88b3782ffbf11a2061136c3
Dennis Luxen <dluxen@lyft.com>
2018-04-05 21:49:57 +0200

    exomizer 2.0.11
    Closes #26221.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/exomizer.rb
+++ Formula/exomizer.rb
@@ -1,8 +1,8 @@
 class Exomizer < Formula
   desc "6502 compressor with CBM PET 4032 support"
   homepage "https://bitbucket.org/magli143/exomizer/wiki/Home"
-  url "https://bitbucket.org/magli143/exomizer/wiki/downloads/exomizer-2.0.10.zip"
-  sha256 "74afb08a51466e24e5e95b8672c9a152a1ab4eb31464f29cd630c1feb585b027"
+  url "https://bitbucket.org/magli143/exomizer/wiki/downloads/exomizer-2.0.11.zip"
+  sha256 "8b31d6190308201b18af37509f3b1a0ec95bfc474bfad9743de64f8356d6b692"
 
   bottle do
     cellar :any_skip_relocation
