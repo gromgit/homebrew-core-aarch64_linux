a762dea3af63b44132445355fb1eed446ba35276
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-05 05:26:40 +0000

    ircd-hybrid: update 8.2.24 bottle.

--- Formula/ircd-hybrid.rb
+++ Formula/ircd-hybrid.rb
@@ -5,9 +5,9 @@ class IrcdHybrid < Formula
   sha256 "eaa42d8bf10c0e619e3bda96f35d31bb20715305a85a1386cfbc6b8761fed50e"
 
   bottle do
-    sha256 "9113d2a3fae2abbd0ed3f2c34d71d276a96f34de01703099dac22cfd9cac3d42" => :high_sierra
-    sha256 "0af883166ec878a5e57b6a0200812e777fffa39ee95341f065ecde4d8aca0d61" => :sierra
-    sha256 "5369177134d238aae8a496e10e086bc9f58b1b06f4a4e12b9399bab6df38118d" => :el_capitan
+    sha256 "a3c6846e6cdad9e8615f01d1539b857a4a02aa87b475f0b7e576c29716f9b4e2" => :high_sierra
+    sha256 "1be9a361ecfcadda2db262d7af21b3a4518fa2dbb1400fc9b6bfd21e3b080949" => :sierra
+    sha256 "cb2919d989ee6ae1fa2ed37b4cdc24ff32d10512d436ec29d47218b75ab3f3a3" => :el_capitan
   end
 
   # ircd-hybrid needs the .la files
