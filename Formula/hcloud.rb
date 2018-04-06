b8d1de006e94018d3cabe4883643c2e24e166645
Thomas Cyron <thomas@thcyron.de>
2018-04-06 12:52:49 +0200

    hcloud 1.3.2 (new formula)
    Closes #26256.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/hcloud.rb
@@ -0,0 +1,39 @@
+class Hcloud < Formula
+  desc "Command-line interface for Hetzner Cloud"
+  homepage "https://github.com/hetznercloud/cli"
+  url "https://github.com/hetznercloud/cli/archive/v1.3.2.tar.gz"
+  sha256 "1cac1e7a5618abce0e6e2d5da40afc153fd88508557afdf90d0f5be54abec9a9"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/hetznercloud/cli").install buildpath.children
+
+    cd "src/github.com/hetznercloud/cli" do
+      ldflags = "-w -X github.com/hetznercloud/cli.Version=v#{version}"
+      system "go", "build", "-o", bin/"hcloud", "-ldflags", ldflags,
+                   "./cmd/hcloud"
+      prefix.install_metafiles
+    end
+
+    output = Utils.popen_read("#{bin}/hcloud completion bash")
+    (bash_completion/"hcloud").write output
+    output = Utils.popen_read("#{bin}/hcloud completion zsh")
+    (zsh_completion/"_hcloud").write output
+  end
+
+  test do
+    config_path = testpath/".config/hcloud/cli.toml"
+    ENV["HCLOUD_CONFIG"] = config_path
+    assert_match "", shell_output("#{bin}/hcloud context active")
+    config_path.write <<~EOS
+      active_context = "test"
+      [[contexts]]
+      name = "test"
+      token = "foobar"
+    EOS
+    assert_match "test", shell_output("#{bin}/hcloud context list")
+    assert_match "test", shell_output("#{bin}/hcloud context active")
+  end
+end
