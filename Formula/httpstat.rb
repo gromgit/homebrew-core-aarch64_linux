bf2fde7ad61d0f100a605e68f32dba0fbbf8238e
ilovezfs <ilovezfs@icloud.com>
2018-04-07 01:36:53 -0700

    httpstat: depend on python@2

--- Formula/httpstat.rb
+++ Formula/httpstat.rb
@@ -6,7 +6,7 @@ class Httpstat < Formula
 
   bottle :unneeded
 
-  depends_on "python@2" if MacOS.version <= :snow_leopard
+  depends_on "python@2"
 
   def install
     bin.install "httpstat.py" => "httpstat"
