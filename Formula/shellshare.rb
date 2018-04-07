1ebc5f2f74e4e57cc9db32730d51b01b1d7f4ef0
ilovezfs <ilovezfs@icloud.com>
2018-04-07 01:37:07 -0700

    shellshare: depend on python@2

--- Formula/shellshare.rb
+++ Formula/shellshare.rb
@@ -6,7 +6,7 @@ class Shellshare < Formula
 
   bottle :unneeded
 
-  depends_on "python@2" if MacOS.version <= :snow_leopard
+  depends_on "python@2"
 
   def install
     bin.install "public/bin/shellshare"
