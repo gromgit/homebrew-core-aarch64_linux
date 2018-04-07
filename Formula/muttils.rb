04807d7f21b5e28fa4d07dd452e956b3225c9c99
ilovezfs <ilovezfs@icloud.com>
2018-04-07 01:36:58 -0700

    muttils: depend on python@2

--- Formula/muttils.rb
+++ Formula/muttils.rb
@@ -15,7 +15,7 @@ class Muttils < Formula
     sha256 "63e819c0bb96a56ed0f159ab816aeac84805a52333bd23298a17cd2abddcb17b" => :mountain_lion
   end
 
-  depends_on "python@2" if MacOS.version <= :snow_leopard
+  depends_on "python@2"
 
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
