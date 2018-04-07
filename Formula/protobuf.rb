a6ecd0a47cd5d73345e3b0d5eb5000fb623573d1
ilovezfs <ilovezfs@icloud.com>
2018-04-07 01:37:02 -0700

    protobuf: depend on python@2

--- Formula/protobuf.rb
+++ Formula/protobuf.rb
@@ -23,7 +23,7 @@ class Protobuf < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on "python@2" => :recommended if MacOS.version <= :snow_leopard
+  depends_on "python@2" => :recommended
   depends_on "python" => :optional
 
   resource "six" do
