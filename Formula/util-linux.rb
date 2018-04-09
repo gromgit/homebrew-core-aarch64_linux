041004778effcfa086c61ec35374d6aab69c3824
Peter van Dijk <peter@7bits.nl>
2018-04-09 18:15:00 +0200

    util-linux 2.32 (new formula)
    Closes #26353.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/util-linux.rb
@@ -0,0 +1,34 @@
+class UtilLinux < Formula
+  desc "Collection of Linux utilities"
+  homepage "https://github.com/karelzak/util-linux"
+  url "https://www.kernel.org/pub/linux/utils/util-linux/v2.32/util-linux-2.32.tar.xz"
+  sha256 "6c7397abc764e32e8159c2e96042874a190303e77adceb4ac5bd502a272a4734"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--disable-ipcs",        # does not build on macOS
+                          "--disable-ipcrm",       # does not build on macOS
+                          "--disable-wall",        # already comes with macOS
+                          "--disable-libuuid",     # conflicts with ossp-uuid
+                          "--disable-libsmartcols" # macOS already ships 'column'
+
+    system "make", "install"
+
+    # Remove binaries already shipped by macOS
+    %w[cal col colcrt colrm getopt hexdump logger nologin look mesg more renice rev ul whereis].each do |prog|
+      rm_f bin/prog
+      rm_f sbin/prog
+      rm_f man1/"#{prog}.1"
+      rm_f man8/"#{prog}.8"
+      rm_f share/"bash-completion"/"completions"/prog
+    end
+  end
+
+  test do
+    out = shell_output("#{bin}/namei -lx /usr").split("\n")
+    assert_equal ["f: /usr", "Drwxr-xr-x root wheel /", "drwxr-xr-x root wheel usr"], out
+  end
+end
