cd7f966b1ee2aab49b7a24e9aa5974931e88d853
ilovezfs <ilovezfs@icloud.com>
2018-04-09 14:26:45 -0700

    testssl 2.9.5-4 (#26344)

--- Formula/testssl.rb
+++ Formula/testssl.rb
@@ -1,9 +1,9 @@
 class Testssl < Formula
   desc "Tool which checks for the support of TLS/SSL ciphers and flaws"
   homepage "https://testssl.sh/"
-  url "https://github.com/drwetter/testssl.sh/archive/v2.9.5-3.tar.gz"
-  version "2.9.5-3"
-  sha256 "03abee78d1373098b2a99766bc57cf498e0d9ddaf8b9eb3389fac2540fd5af0d"
+  url "https://github.com/drwetter/testssl.sh/archive/v2.9.5-4.tar.gz"
+  version "2.9.5-4"
+  sha256 "5163f76d40acfe404d65145c498cbe8fb716bb49119e8d0773e063203cff9c03"
   head "https://github.com/drwetter/testssl.sh.git"
 
   bottle :unneeded
