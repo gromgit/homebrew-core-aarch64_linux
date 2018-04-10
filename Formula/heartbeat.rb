9b5fbac18563f6326bdd3b15afa2f5360fd158c0
commitay <commitay@users.noreply.github.com>
2018-04-10 15:33:01 +1000

    heartbeat: depend on python@2 at build time

--- Formula/heartbeat.rb
+++ Formula/heartbeat.rb
@@ -13,10 +13,11 @@ class Heartbeat < Formula
   end
 
   depends_on "go" => :build
+  depends_on "python@2" => :build
 
   resource "virtualenv" do
-    url "https://files.pythonhosted.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz"
-    sha256 "02f8102c2436bb03b3ee6dede1919d1dac8a427541652e5ec95171ec8adbc93a"
+    url "https://files.pythonhosted.org/packages/b1/72/2d70c5a1de409ceb3a27ff2ec007ecdd5cc52239e7c74990e32af57affe9/virtualenv-15.2.0.tar.gz"
+    sha256 "1d7e241b431e7afce47e77f8843a276f652699d1fa4f93b9d8ce0076fd7b0b54"
   end
 
   def install
