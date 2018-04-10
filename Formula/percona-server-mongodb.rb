44ea9e257caeb32a9683d73eefb199326cec4d6e
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-10 12:51:41 +0000

    percona-server-mongodb: update 3.4.13-2.11 bottle.

--- Formula/percona-server-mongodb.rb
+++ Formula/percona-server-mongodb.rb
@@ -6,10 +6,9 @@ class PerconaServerMongodb < Formula
   sha256 "fab82121f6e9cce1058d15ef3650813038cc4e1b768a1c0cd6bdf3bec40e8f45"
 
   bottle do
-    rebuild 1
-    sha256 "396da20f15f3e074496e159c49fb12ef0a412a891eafec0cd15edd7e2d54d5fb" => :high_sierra
-    sha256 "08266faa60f1b18830d269250c94f36cd808fc3915935159803366199ed8c97c" => :sierra
-    sha256 "7cf6b6ffd202ab5dfe1054b2128b5795706d67340b6acaa8311e23d0af8c8bd5" => :el_capitan
+    sha256 "b7c49e1664e9cb20380884e6338f75d7f3dd6766df1d82eed210b51ff66644a2" => :high_sierra
+    sha256 "4e517c014c1ccdd85b2791a72a5320a47fdc6bdfae7a8a2f4bba8056ba4f0990" => :sierra
+    sha256 "82c944d1b91d8dddbfa7586976295a97db947f1de151c832ef486fcdeac8c8ea" => :el_capitan
   end
 
   option "with-boost", "Compile using installed boost, not the version shipped with this formula"
