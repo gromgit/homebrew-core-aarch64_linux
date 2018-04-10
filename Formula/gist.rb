078dfc05294427e95ebbc51a950c99d08caead9d
commitay <commitay@users.noreply.github.com>
2018-04-10 19:57:11 +1000

    gist: depend on ruby if <= :sierra
    Closes #26401.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/gist.rb
+++ Formula/gist.rb
@@ -3,6 +3,7 @@ class Gist < Formula
   homepage "https://github.com/defunkt/gist"
   url "https://github.com/defunkt/gist/archive/v4.6.2.tar.gz"
   sha256 "149a57ba7e8a8751d6a55dc652ce3cf0af28580f142f2adb97d1ceeccb8df3ad"
+  revision 1
   head "https://github.com/defunkt/gist.git"
 
   bottle do
@@ -12,11 +13,14 @@ class Gist < Formula
     sha256 "5d8c49b8c75cec0f4556c30b98e9af9f6f9a9cde0b208d38f6e410538c0763b7" => :el_capitan
   end
 
+  depends_on "ruby" if MacOS.version <= :sierra
+
   def install
     system "rake", "install", "prefix=#{prefix}"
   end
 
   test do
-    assert_match %r{https:\/\/gist}, pipe_output("#{bin}/gist", "homebrew")
+    output = pipe_output("#{bin}/gist", "homebrew")
+    assert_match "Requires authentication", output
   end
 end
