23b2771d913deeedef6bc9231dc826f5ee546a6c
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-10 01:46:31 +0000

    hcloud: add 1.3.2 bottle.

--- Formula/hcloud.rb
+++ Formula/hcloud.rb
@@ -4,6 +4,13 @@ class Hcloud < Formula
   url "https://github.com/hetznercloud/cli/archive/v1.3.2.tar.gz"
   sha256 "1cac1e7a5618abce0e6e2d5da40afc153fd88508557afdf90d0f5be54abec9a9"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6a2721f2f0ad3a6159420016afd29eed297888921b44cbeec0476a10fa870320" => :high_sierra
+    sha256 "d3fbff2fc949ba19e4181d58b34a63c0aa0263540003acd33a37b652b660a7ef" => :sierra
+    sha256 "c6682a43394dd4f4bb262305469969db8b441170b0024748ff7c447b7e4a95dd" => :el_capitan
+  end
+
   depends_on "go" => :build
 
   def install
