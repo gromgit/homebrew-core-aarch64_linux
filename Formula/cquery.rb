245c444a8a37b80ca76e3fdaafa67691d5a846e7
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-10 09:18:35 +0000

    cquery: add 20180302 bottle.

--- Formula/cquery.rb
+++ Formula/cquery.rb
@@ -6,6 +6,13 @@ class Cquery < Formula
                                                       :revision => "f3e9e756e182b122bef8826a77047f6ccf5529b6"
   head "https://github.com/cquery-project/cquery.git"
 
+  bottle do
+    cellar :any
+    sha256 "0f567e5d88299f28d489b666fca2cf320b91062fc053e0c2a8a1ad9e1bc76598" => :high_sierra
+    sha256 "06bb25b1c91c76c894d3aebdc18b76591ab2ff1c5f8190c376b69e60d6c22787" => :sierra
+    sha256 "9692d28682c009a9bec583ff20aa16ce5c790975375755a5a48a28ec3e3953c4" => :el_capitan
+  end
+
   def install
     system "./waf", "configure", "--prefix=#{prefix}"
     system "./waf", "build"
