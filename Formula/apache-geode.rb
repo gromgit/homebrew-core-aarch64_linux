464e9602409e29d7c1a4bdfd9cee878d7fe897df
Anthony Baker <metatype@users.noreply.github.com>
2018-04-10 11:53:35 -0700

    apache-geode 1.5.0 (#26434)

--- Formula/apache-geode.rb
+++ Formula/apache-geode.rb
@@ -1,8 +1,8 @@
 class ApacheGeode < Formula
   desc "In-memory Data Grid for fast transactional data processing"
   homepage "https://geode.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=geode/1.4.0/apache-geode-1.4.0.tgz"
-  sha256 "7f880bed678c44e86e028a0d6e3465cfc8a2979a08c7c708a836425f6e6f6b98"
+  url "https://www.apache.org/dyn/closer.cgi?path=geode/1.5.0/apache-geode-1.5.0.tgz"
+  sha256 "397eb6bec1e2c101d96f68b2da1ae166839a632e9506d7daa87e9abad56aa17f"
 
   bottle :unneeded
 
