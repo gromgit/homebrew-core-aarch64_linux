c1dae39df80ee21d527840728ae2ad7e17b18002
ilovezfs <ilovezfs@icloud.com>
2018-04-10 23:18:20 -0700

    skafos 1.3.2
    Closes #26469.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/skafos.rb
+++ Formula/skafos.rb
@@ -1,8 +1,8 @@
 class Skafos < Formula
   desc "CLI for the Metis Machine A.I. and machine learning deployment platform"
   homepage "https://metismachine.com/"
-  url "https://github.com/MetisMachine/skafos/archive/1.3.1.tar.gz"
-  sha256 "c5302db83be82497fb850c93f7c578eace23037bd3cc81ae887db32549284fd5"
+  url "https://github.com/MetisMachine/skafos/archive/1.3.2.tar.gz"
+  sha256 "d597d8eef3389f8e0e20c6157f936d7775878ee4026ab26e7c4130034477dad1"
 
   bottle do
     cellar :any
