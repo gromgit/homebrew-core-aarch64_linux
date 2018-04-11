6564c566aeaeea44e8ee8c669cfcf92e1c8f3427
ilovezfs <ilovezfs@icloud.com>
2018-04-10 23:26:04 -0700

    yash 2.47
    Closes #26472.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/yash.rb
+++ Formula/yash.rb
@@ -1,9 +1,9 @@
 class Yash < Formula
   desc "Yet another shell: a POSIX-compliant command-line shell"
   homepage "https://yash.osdn.jp/"
-  # Canonical: https://osdn.net/dl/yash/yash-2.46.tar.xz
-  url "https://dotsrc.dl.osdn.net/osdn/yash/68578/yash-2.46.tar.xz"
-  sha256 "93431d897ce2b176c9f97b879c70a426ebc125b073d5894c00cd746f3a8455cb"
+  # Canonical: https://osdn.net/dl/yash/yash-*
+  url "https://dotsrc.dl.osdn.net/osdn/yash/69353/yash-2.47.tar.xz"
+  sha256 "931f2e7451d8b1eca2a98caeef7eda0527d96376f9f2c9bec90bc5938e39992e"
 
   bottle do
     sha256 "efa74af51e2b6eb275952a2ae604e722630b730073417b4277b58631c07ec233" => :high_sierra
