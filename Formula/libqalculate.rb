1ee781d15053ef70d4a40b0a4ac6937d561e63e1
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-12 05:09:32 +0000

    libqalculate: update 2.4.0 bottle.

--- Formula/libqalculate.rb
+++ Formula/libqalculate.rb
@@ -5,9 +5,9 @@ class Libqalculate < Formula
   sha256 "8a0a7176c9d9cf3fcf0c9fcd3d4d4f70c9c09248b41017b0a77e4fb932e6ff5c"
 
   bottle do
-    sha256 "643f7a1fbf973bf1fe0cfbf9a214700ec20ca50379cca8471e9ad01d4c3346b0" => :high_sierra
-    sha256 "33983c6bb3fa007a959aa62ce9b16567747eb468f761ea7125aa9fe2eab6acb3" => :sierra
-    sha256 "3d7ce65151e7832d399b7ff045c269da9fdf35a5ac62fd9c7ef110602f186012" => :el_capitan
+    sha256 "6e94803ac05a72c52e6f3634ef5f8fcdb51ff0c769e9bb054c2af7c5838df8fd" => :high_sierra
+    sha256 "4f191357f242180e3cbf25041c25a278f2bb7423e2c0b8f73a1e50abe5f510d7" => :sierra
+    sha256 "224643d64ac045ebace8e33f71fa6c7ce274eb73e66a6c45fc206ed0cede104c" => :el_capitan
   end
 
   depends_on "intltool" => :build
