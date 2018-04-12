46398f6922b7d37cbc3db910708f901853a89a63
Chris Long <christopherglong@mac.com>
2018-04-11 22:07:48 -0700

    mkl-dnn 0.13 (new formula)
    Closes #26512.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/mkl-dnn.rb
@@ -0,0 +1,29 @@
+class MklDnn < Formula
+  desc "Intel Math Kernel Library for Deep Neural Networks"
+  homepage "https://01.org/mkl-dnn"
+  url "https://github.com/intel/mkl-dnn/archive/v0.13.tar.gz"
+  sha256 "d2cfd93a70cfe86ebe054477c530c9b5c1218b70f75856eb6d1956c68ee89e8f"
+
+  depends_on "cmake" => :build
+  depends_on "doxygen" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    system "make", "doc"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <mkldnn.h>
+      int main() {
+        mkldnn_engine_t engine;
+        mkldnn_status_t status = mkldnn_engine_create(&engine, mkldnn_cpu, 0);
+        return !(status == mkldnn_success);
+      }
+    EOS
+    system ENV.cc, "-L#{lib}", "-lmkldnn", "test.c", "-o", "test"
+    system "./test"
+  end
+end
