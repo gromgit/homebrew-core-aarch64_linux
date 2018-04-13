f8f0e7250fdafc35b99fe62376dceba0ccbc9509
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-13 03:54:23 +0000

    mkl-dnn: add 0.13 bottle.

--- Formula/mkl-dnn.rb
+++ Formula/mkl-dnn.rb
@@ -4,6 +4,13 @@ class MklDnn < Formula
   url "https://github.com/intel/mkl-dnn/archive/v0.13.tar.gz"
   sha256 "d2cfd93a70cfe86ebe054477c530c9b5c1218b70f75856eb6d1956c68ee89e8f"
 
+  bottle do
+    cellar :any
+    sha256 "d15b58e48a219234b3ab9150eb4037054c1ceef41131b1dec963ded3a33f1160" => :high_sierra
+    sha256 "100918b030da7bc12b3097540334adefaba3db21e91e970a9dcd22ece323c4f1" => :sierra
+    sha256 "a7c0feef8748486474eea2efe45b50c7fa3ab0320d1447bd8d3e81cd83feffd2" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
 
