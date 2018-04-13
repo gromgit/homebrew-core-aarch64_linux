4e0942d668ba3c415003b8a1ad75cf1aa9714b98
ilovezfs <ilovezfs@icloud.com>
2018-04-12 21:33:40 -0700

    binaryen 46
    Closes #26546.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/binaryen.rb
+++ Formula/binaryen.rb
@@ -1,8 +1,8 @@
 class Binaryen < Formula
   desc "Compiler infrastructure and toolchain library for WebAssembly"
   homepage "http://webassembly.org/"
-  url "https://github.com/WebAssembly/binaryen/archive/version_45.tar.gz"
-  sha256 "7c2e05edeba44ead365f27ce0164d35d2212577fca0964a4f695402ece6e283b"
+  url "https://github.com/WebAssembly/binaryen/archive/version_46.tar.gz"
+  sha256 "bd5d1c7b8254e70849a94db688e28dedc95f87102126462c0b5ca755eca7c1b7"
 
   head "https://github.com/WebAssembly/binaryen.git"
 
