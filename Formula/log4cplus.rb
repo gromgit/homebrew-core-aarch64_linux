9fb53646b1911d0e3ce284d42296254cf2dbde51
ilovezfs <ilovezfs@icloud.com>
2018-04-13 03:04:15 -0700

    log4cplus 2.0.0
    Closes #26561.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/log4cplus.rb
+++ Formula/log4cplus.rb
@@ -1,8 +1,8 @@
 class Log4cplus < Formula
   desc "Logging Framework for C++"
   homepage "https://sourceforge.net/p/log4cplus/wiki/Home/"
-  url "https://downloads.sourceforge.net/project/log4cplus/log4cplus-stable/1.2.1/log4cplus-1.2.1.tar.xz"
-  sha256 "09899274d18af7ec845ef2c36a86b446a03f6b0e3b317d96d89447007ebed0fc"
+  url "https://downloads.sourceforge.net/project/log4cplus/log4cplus-stable/2.0.0/log4cplus-2.0.0.tar.xz"
+  sha256 "8c85e769c3dbec382ed4db91f15e5bc24ba979f810262723781f2fc596339bf4"
 
   bottle do
     cellar :any
