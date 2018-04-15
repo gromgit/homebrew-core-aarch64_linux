3d23c4482921fa723b3c96169cb4d0c751b06635
ilovezfs <ilovezfs@icloud.com>
2018-04-14 22:59:54 -0700

    phpunit 7.1.3 (#26610)

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -1,8 +1,8 @@
 class Phpunit < Formula
   desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-7.0.3.phar"
-  sha256 "5889db48d1cd666ecd6b8885f47c0bdef1778e81658b5e636e3ada1d2bd382c0"
+  url "https://phar.phpunit.de/phpunit-7.1.3.phar"
+  sha256 "8c1ae8f65c1a4b1b122506b3190dd54e2f334f977db54731235985c4f08d8fce"
 
   bottle :unneeded
 
