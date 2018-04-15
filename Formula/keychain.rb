b658d762781ce29928a9f525ee20911ae9eb1b7f
ilovezfs <ilovezfs@icloud.com>
2018-04-15 00:45:48 -0700

    keychain 2.8.5 (#26623)

--- Formula/keychain.rb
+++ Formula/keychain.rb
@@ -1,9 +1,9 @@
 class Keychain < Formula
   desc "User-friendly front-end to ssh-agent(1)"
   homepage "https://www.funtoo.org/Keychain"
-  url "https://build.funtoo.org/distfiles/keychain/keychain-2.8.4.tar.bz2"
-  mirror "https://fossies.org/linux/privat/keychain-2.8.4.tar.bz2"
-  sha256 "1746dac19f565a1489b5a8ee660af2d7097f44cb5bede3e9103782056592ae6b"
+  url "https://build.funtoo.org/distfiles/keychain/keychain-2.8.5.tar.bz2"
+  mirror "https://fossies.org/linux/privat/keychain-2.8.5.tar.bz2"
+  sha256 "16f5949b606691dea6e1832a77e697b8c0b2a536abfbd29e3a3f47222259c3b2"
 
   bottle :unneeded
 
