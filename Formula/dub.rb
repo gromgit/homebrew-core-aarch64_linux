7530164fbfd4517f3d893e71ecde281c7be6e9e8
JCount <JCount42@gmail.com>
2018-04-15 00:31:22 -0400

    dub 1.8.1
    Closes #26608.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dub.rb
+++ Formula/dub.rb
@@ -1,8 +1,8 @@
 class Dub < Formula
   desc "Build tool for D projects"
   homepage "https://code.dlang.org/getting_started"
-  url "https://github.com/dlang/dub/archive/v1.8.0.tar.gz"
-  sha256 "acffbdee967a20aba2c08d2a9de6a8b23b8fb5a703eece684781758db2831d50"
+  url "https://github.com/dlang/dub/archive/v1.8.1.tar.gz"
+  sha256 "79ad2dca0679f6d8b6a4d75e7ccea7930957134743bba290c949d5aa1aa53a14"
   version_scheme 1
 
   head "https://github.com/dlang/dub.git"
@@ -11,11 +11,6 @@ class Dub < Formula
     sha256 "55d6329cc5b3ff35a86afe1fe2dd344028f5720368b2352a1f31e0cf223d2ee2" => :high_sierra
     sha256 "29dff693089f35e3c09022b79ae153dbaf8457087b528d56ae0ac10feccd9024" => :sierra
     sha256 "43959b85a6cb66c18d4835eca8dc3183fa1cd80af3f71dc90f38b3ed54b30038" => :el_capitan
-  end
-
-  devel do
-    url "https://github.com/dlang/dub/archive/v1.8.1-beta.1.tar.gz"
-    sha256 "ea5be7a58ee4b25b0766fd73406e5ba256c9d8ee9cfa94ee921187b58e80cebd"
   end
 
   depends_on "pkg-config" => :recommended
