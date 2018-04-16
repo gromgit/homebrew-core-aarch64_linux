ae6dc0987888a31f02182e0c14927fb00792e1be
ilovezfs <ilovezfs@icloud.com>
2018-04-16 00:37:04 -0700

    phplint 3.1-20180416 (#26675)
    depend on php instead of php@7.1

--- Formula/phplint.rb
+++ Formula/phplint.rb
@@ -1,16 +1,16 @@
 class Phplint < Formula
   desc "Validator and documentator for PHP 5 and 7 programs"
   homepage "http://www.icosaedro.it/phplint/"
-  url "http://www.icosaedro.it/phplint/phplint-3.0_20160307.tar.gz"
-  version "3.0-20160307"
-  sha256 "7a361166d1a6de707e6728828a6002a6de69be886501853344601ab1da922e7b"
+  url "http://www.icosaedro.it/phplint/phplint-3.1_20180416.tar.gz"
+  version "3.1-20180416"
+  sha256 "7535bc6987d6079d537ca643c40ec9ebbc767c31ec2fc0f5811ed66736afa341"
 
   bottle :unneeded
 
-  depends_on "php@7.1"
+  depends_on "php"
 
   def install
-    inreplace "php", "/opt/php/bin/php", Formula["php@7.1"].opt_bin/"php"
+    inreplace "php", "/opt/php/bin/php", Formula["php"].opt_bin/"php"
     libexec.install "modules", "php", "phpl", "stdlib", "utils"
     bin.install_symlink libexec/"phpl"
   end
