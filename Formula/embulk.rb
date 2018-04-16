125b73b6ba645dc5efc2c49bfe94d87d1a7575d3
ilovezfs <ilovezfs@icloud.com>
2018-04-16 03:17:16 -0700

    embulk 0.9.7 (#26682)

--- Formula/embulk.rb
+++ Formula/embulk.rb
@@ -1,8 +1,8 @@
 class Embulk < Formula
   desc "Data transfer between various databases, file formats and services"
   homepage "https://www.embulk.org/"
-  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.9.6.jar"
-  sha256 "f77c1c907ac302bf7c056b43afebc789f38f65543ee6778bf65f9ecc1500d9c1"
+  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.9.7.jar"
+  sha256 "367a12fd14b2055e944dc0a90c45bb58789e73ce4f68ba5362ccd97a51146f3c"
 
   bottle :unneeded
 
