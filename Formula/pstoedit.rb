b98ac0ed153e5edecff7d73e101ad4745acd53b4
ilovezfs <ilovezfs@icloud.com>
2018-04-15 23:49:16 -0700

    pstoedit 3.71
    reparallelize the build
    
    Closes #26677.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pstoedit.rb
+++ Formula/pstoedit.rb
@@ -1,9 +1,8 @@
 class Pstoedit < Formula
   desc "Convert PostScript and PDF files to editable vector graphics"
   homepage "http://www.pstoedit.net/"
-  url "https://downloads.sourceforge.net/project/pstoedit/pstoedit/3.70/pstoedit-3.70.tar.gz"
-  sha256 "06b86113f7847cbcfd4e0623921a8763143bbcaef9f9098e6def650d1ff8138c"
-  revision 2
+  url "https://downloads.sourceforge.net/project/pstoedit/pstoedit/3.71/pstoedit-3.71.tar.gz"
+  sha256 "0589cd22cd9c23dee12d9bc9f26760f872185d8a1fb72a05bc58f6b824cfbc95"
 
   bottle do
     sha256 "319234b9126aeb338850b4d9bb2db68da75bee8d2025894726897976b5e4633a" => :high_sierra
@@ -18,29 +17,7 @@ class Pstoedit < Formula
   depends_on "imagemagick"
   depends_on "xz" if MacOS.version < :mavericks
 
-  # Fix for pstoedit search for plugins, thereby restoring formats that
-  # worked in 3.62 but now don't in 3.70, including PIC, DXF, FIG, and
-  # many others.
-  #
-  # This patch has been submitted upstream; see:
-  # https://sourceforge.net/p/pstoedit/bugs/19/
-  #
-  # Taken from:
-  # https://build.opensuse.org/package/view_file/openSUSE:Factory/pstoedit/pstoedit-pkglibdir.patch?expand=1
-  #
-  # This patch changes the behavior of "make install" so that:
-  # * If common/plugindir is defined, it checks only that directory.
-  # * It swaps the check order: First checks whether PSTOEDITLIBDIR exists. If
-  #   it exists, it skips blind attempts to find plugins.
-  # As PSTOEDITLIBDIR is always defined by makefile, the blind fallback will
-  # be attempted only in obscure environments.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/fa1823b/pstoedit/3.70.patch"
-    sha256 "9af1bbc9db97f5d5dc92816e5c5fdd5f98904f64d1ab0dd6fcdcde1fd8606ce6"
-  end
-
   def install
-    ENV.deparallelize
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
