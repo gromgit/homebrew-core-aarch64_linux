a23780430301f958387b85bab71e2ab18f156558
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-16 22:15:51 +0000

    source-highlight: update 3.1.8_9 bottle.

--- Formula/source-highlight.rb
+++ Formula/source-highlight.rb
@@ -8,9 +8,9 @@ class SourceHighlight < Formula
   revision 9
 
   bottle do
-    sha256 "3101cb3776039eba399f7a9d42cbd68120abaa2c3fd02401bd43128020bdf22c" => :high_sierra
-    sha256 "c8f9401b678b754e82d3cd7a78f6e7c164a1ece1832ca73ac77f11023de87de9" => :sierra
-    sha256 "d8f960586c5a0b5249ceacbe3996042fdfbce29a50c3adc65087e02f3daf361b" => :el_capitan
+    sha256 "88e245af7dfafc7cc0419fab616aa7d1ee16e3f47a36eab9e66bed809bbcd079" => :high_sierra
+    sha256 "2889da5f2a4ce82b711fc2903e888e9410a8ae4d93fd26885b8d6b0b6b178505" => :sierra
+    sha256 "85f7a45cd5ebde9700d332e72395a116287579e29ac000c80f8cb3fd6c8f843f" => :el_capitan
   end
 
   depends_on "boost"
