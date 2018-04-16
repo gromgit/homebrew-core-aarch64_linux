6ca1c8998f36f0b735aeaa918347652501ef15e6
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-16 09:47:37 +0000

    cockatrice: update 2.5.1 bottle.

--- Formula/cockatrice.rb
+++ Formula/cockatrice.rb
@@ -9,9 +9,9 @@ class Cockatrice < Formula
   head "https://github.com/Cockatrice/Cockatrice.git"
 
   bottle do
-    sha256 "a4d55a9b8500a209f45fb6f5dc51575b563f0a371247f9b07771cdbfbb9f5105" => :high_sierra
-    sha256 "0375aa07963f3475b613207ea7615f08c296439bf3f832b7c9ce1e0e54996981" => :sierra
-    sha256 "29369f9c3fcb3f8c16e738041236b9d7bcbf88a2c28859193a51a605f8833f30" => :el_capitan
+    sha256 "c42f80f3269a7226b40e52cf3af6be44b3952882c2d0c235b536120552c9d550" => :high_sierra
+    sha256 "67373a44512d250598df6cbe3f29c121c54519ea55233635f0d7991eee522f7c" => :sierra
+    sha256 "6caa34c3b0aed2e8451637eea27bc0f819924bec2d07bb22b7919730246d1b05" => :el_capitan
   end
 
   depends_on :macos => :el_capitan
