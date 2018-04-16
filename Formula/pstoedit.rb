3946327ab6341929e6ffcaf7bba13ea797709820
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-16 09:02:37 +0000

    pstoedit: update 3.71 bottle.

--- Formula/pstoedit.rb
+++ Formula/pstoedit.rb
@@ -5,10 +5,9 @@ class Pstoedit < Formula
   sha256 "0589cd22cd9c23dee12d9bc9f26760f872185d8a1fb72a05bc58f6b824cfbc95"
 
   bottle do
-    sha256 "319234b9126aeb338850b4d9bb2db68da75bee8d2025894726897976b5e4633a" => :high_sierra
-    sha256 "085860d9480d7d9558697d403f6628466a8fdfe52f568f21793568d1c71747f2" => :sierra
-    sha256 "ee6634e964c7687c5614c9e4358737154ab6f29d53a104416bdc1509b33e6930" => :el_capitan
-    sha256 "6330be58259fcfa74062eb1ebfa5eced31aea86ae5f1ce6b2bf49e2f544b3d73" => :yosemite
+    sha256 "c153dec1a76f7d6a829276145552fc6dc3756322a3023a24506740ee128d9a23" => :high_sierra
+    sha256 "3f4a82c73fcc2c44bfaa043babe85b8a4190de731752a86a2db553fa18c5bc5d" => :sierra
+    sha256 "5b4de0d105ec879d80202d8c523e6052769316e29e7eb975f5e2b77b2fc369f4" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
