a11eaef59bab1093f3d778b1bd2888d141f10a95
ilovezfs <ilovezfs@icloud.com>
2018-04-15 23:27:36 -0700

    file-formula 5.33
    Closes #26671.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/file-formula.rb
+++ Formula/file-formula.rb
@@ -2,9 +2,9 @@
 class FileFormula < Formula
   desc "Utility to determine file types"
   homepage "https://www.darwinsys.com/file/"
-  url "ftp://ftp.astron.com/pub/file/file-5.32.tar.gz"
-  mirror "https://fossies.org/linux/misc/file-5.32.tar.gz"
-  sha256 "8639dc4d1b21e232285cd483604afc4a6ee810710e00e579dbe9591681722b50"
+  url "ftp://ftp.astron.com/pub/file/file-5.33.tar.gz"
+  mirror "https://fossies.org/linux/misc/file-5.33.tar.gz"
+  sha256 "1c52c8c3d271cd898d5511c36a68059cda94036111ab293f01f83c3525b737c6"
   head "https://github.com/file/file.git"
 
   bottle do
@@ -38,21 +38,21 @@ end
 
 __END__
 diff --git a/src/Makefile.in b/src/Makefile.in
-index b6eeb20..4a7ae91 100644
+index c096c71..583a0ba 100644
 --- a/src/Makefile.in
 +++ b/src/Makefile.in
-@@ -150,7 +150,6 @@ libmagic_la_LINK = $(LIBTOOL) $(AM_V_lt) --tag=CC $(AM_LIBTOOLFLAGS) \
+@@ -115,7 +115,6 @@ libmagic_la_LINK = $(LIBTOOL) $(AM_V_lt) --tag=CC $(AM_LIBTOOLFLAGS) \
  PROGRAMS = $(bin_PROGRAMS)
- am_file_OBJECTS = file.$(OBJEXT)
+ am_file_OBJECTS = file.$(OBJEXT) seccomp.$(OBJEXT)
  file_OBJECTS = $(am_file_OBJECTS)
 -file_DEPENDENCIES = libmagic.la
  AM_V_P = $(am__v_P_@AM_V@)
  am__v_P_ = $(am__v_P_@AM_DEFAULT_V@)
  am__v_P_0 = false
-@@ -352,7 +351,7 @@ libmagic_la_LDFLAGS = -no-undefined -version-info 1:0:0
+@@ -311,7 +310,7 @@ libmagic_la_LDFLAGS = -no-undefined -version-info 1:0:0
  @MINGW_TRUE@MINGWLIBS = -lgnurx -lshlwapi
  libmagic_la_LIBADD = $(LTLIBOBJS) $(MINGWLIBS)
- file_SOURCES = file.c
+ file_SOURCES = file.c seccomp.c
 -file_LDADD = libmagic.la
 +file_LDADD = $(LDADD)
  CLEANFILES = magic.h
