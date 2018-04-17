4acaf98e42f70df9eecad0d7022fd9ebed3d4121
Samuel El-Borai <samuel.elborai@gmail.com>
2018-04-17 23:48:21 +0200

    glslang 6.2.2596
    Closes #26752.
    
    Signed-off-by: William Woodruff <william@yossarian.net>

--- Formula/glslang.rb
+++ Formula/glslang.rb
@@ -1,28 +1,9 @@
 class Glslang < Formula
   desc "OpenGL and OpenGL ES reference compiler for shading languages"
   homepage "https://www.khronos.org/opengles/sdk/tools/Reference-Compiler/"
+  url "https://github.com/KhronosGroup/glslang/archive/6.2.2596.tar.gz"
+  sha256 "fce7ec22e66043fb59e4a3c369445ea8c57f2631a04b6a0015f19a7bc954567e"
   head "https://github.com/KhronosGroup/glslang.git"
-
-  stable do
-    url "https://github.com/KhronosGroup/glslang/archive/3.0.tar.gz"
-    sha256 "91653d09a90440a0bc35aa490d0c44973501257577451d4c445b2df5e78d118c"
-
-    if DevelopmentTools.clang_build_version >= 900
-      # Fix ordered pointer comparison build warning/error
-      # https://github.com/KhronosGroup/glslang/pull/108
-      patch do
-        url "https://github.com/KhronosGroup/glslang/commit/a3f53e54d232f3bb345b501ab30a01a5507e5b4e.patch?full_index=1"
-        sha256 "0b25f25119eaf2abb62cd743fc47b587df18e12c69dbc84f4593d7992935e586"
-      end
-
-      # Fix: Failed std::map static assertion with libc++ 3.8
-      # https://github.com/KhronosGroup/glslang/issues/368
-      patch do
-        url "https://github.com/KhronosGroup/glslang/commit/ec1476b7060306fd9109faf7a4c70a20ea3b538c.patch?full_index=1"
-        sha256 "36e8986d8e506f3b85e652a048b3039d2758664f0068c6e466af314e9096a0b6"
-      end
-    end
-  end
 
   bottle do
     cellar :any_skip_relocation
@@ -36,19 +17,9 @@ class Glslang < Formula
   depends_on "cmake" => :build
 
   def install
-    mkdir "build" do
-      system "cmake", "..", *std_cmake_args
-      system "make"
-      system "make", "install"
-
-      # Version 3.0 of glslang does not respect the overridden CMAKE_INSTALL_PREFIX. This has
-      # been fixed in master [1] so when that is released, the manual install commands should
-      # be removed.
-      #
-      # 1. https://github.com/KhronosGroup/glslang/commit/4cbf748b133aef3e2532b9970d7365304347117a
-      bin.install Dir["install/bin/*"]
-      lib.install Dir["install/lib/*"]
-    end
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    system "make", "install"
   end
 
   test do
