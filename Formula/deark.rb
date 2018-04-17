ddfc6ecd076b4aded23cb6b3e257d677ef689b44
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-17 15:27:46 +0000

    deark: add 1.4.5 bottle.

--- Formula/deark.rb
+++ Formula/deark.rb
@@ -6,6 +6,13 @@ class Deark < Formula
   url "http://entropymine.com/deark/releases/deark-1.4.5.tar.gz"
   sha256 "2dfe61cc7bfa927e1702b2312b3edb9e5f9e67bfe460f5da3f82652f163e31e0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4c73f07c4cf4a414827ade6a4b982d478352c0f7bfb739481278198df8fbec02" => :high_sierra
+    sha256 "913ca3d03afc1c9ddd7f03a0ecf9a31245e11471c366d7ef8486a612aef3d328" => :sierra
+    sha256 "914a5f782e78b109b82d4d813ac2025e6499ce44c1d2b1b3fc5c78799ad4eebb" => :el_capitan
+  end
+
   def install
     system "make"
     bin.install "deark"
