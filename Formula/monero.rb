e7b109a22b007a2bac4326acc83046cdf7b5045d
ilovezfs <ilovezfs@icloud.com>
2018-04-17 04:37:04 -0700

    monero 0.12.0.0
    remove readline dependency
    depend on unbound and zeromq
    vendor cppzmq
    update test
    
    Closes #25726.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/monero.rb
+++ Formula/monero.rb
@@ -1,9 +1,8 @@
 class Monero < Formula
   desc "Official monero wallet and cpu miner"
   homepage "https://getmonero.org/"
-  url "https://github.com/monero-project/monero/archive/v0.11.1.0.tar.gz"
-  sha256 "b5b48d3e5317c599e1499278580e9a6ba3afc3536f4064fcf7b20840066a509b"
-  revision 2
+  url "https://github.com/monero-project/monero/archive/v0.12.0.0.tar.gz"
+  sha256 "5e8303900a39e296c4ebaa41d957ab9ee04e915704e1049f82a9cbd4eedc8ffb"
 
   bottle do
     sha256 "d3e4de0f017590398dfe8eb9c551f533e6c3b2242deff80249d7ab58fc447612" => :high_sierra
@@ -16,21 +15,28 @@ class Monero < Formula
   depends_on "boost@1.60"
   depends_on "miniupnpc"
   depends_on "openssl"
-  depends_on "readline"
+  depends_on "unbound"
+  depends_on "zeromq"
+
+  resource "cppzmq" do
+    url "https://github.com/zeromq/cppzmq/archive/v4.2.3.tar.gz"
+    sha256 "3e6b57bf49115f4ae893b1ff7848ead7267013087dc7be1ab27636a97144d373"
+  end
 
   def install
-    system "cmake", ".", *std_cmake_args
+    (buildpath/"cppzmq").install resource("cppzmq")
+    system "cmake", ".", "-DZMQ_INCLUDE_PATH=#{buildpath}/cppzmq", *std_cmake_args
     system "make", "install"
   end
 
   test do
-    cmd = "#{bin}/monero-wallet-cli --restore-deterministic-wallet " \
+    cmd = "yes '' | #{bin}/monero-wallet-cli --restore-deterministic-wallet " \
       "--password brew-test --restore-height 1 --generate-new-wallet wallet " \
       "--electrum-seed 'baptism cousin whole exquisite bobsled fuselage left " \
       "scoop emerge puzzled diet reinvest basin feast nautical upon mullet " \
       "ponies sixteen refer enhanced maul aztec bemused basin'" \
       "--command address"
     address = "4BDtRc8Ym9wGzx8vpkQQvpejxBNVpjEmVBebBPCT4XqvMxW3YaCALFraiQibejyMAxUXB5zqn4pVgHVm3JzhP2WzVAJDpHf"
-    assert_equal address, shell_output(cmd).split[-1]
+    assert_equal address, shell_output(cmd).lines.last.split[1]
   end
 end
