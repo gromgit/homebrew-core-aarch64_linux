6f792140f6b6c2728701ec46d3fa3f023840e11f
ilovezfs <ilovezfs@icloud.com>
2018-04-16 22:39:59 -0700

    oniguruma 6.8.2
    Closes #26715.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/oniguruma.rb
+++ Formula/oniguruma.rb
@@ -1,8 +1,8 @@
 class Oniguruma < Formula
   desc "Regular expressions library"
   homepage "https://github.com/kkos/oniguruma/"
-  url "https://github.com/kkos/oniguruma/releases/download/v6.8.1/onig-6.8.1.tar.gz"
-  sha256 "ddc8f551feecc0172aacf2f188d096134b8c8a21ede88a312f3a81871b7d7445"
+  url "https://github.com/kkos/oniguruma/releases/download/v6.8.2/onig-6.8.2.tar.gz"
+  sha256 "adeada5f6b54c2a6f58ff021831a01b18a62b55ea9935e972e36ebb19e7c4903"
 
   bottle do
     cellar :any
