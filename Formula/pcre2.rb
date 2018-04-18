4602a393cf8019e755eaaeb149526493c00733dd
ilovezfs <ilovezfs@icloud.com>
2018-04-18 03:35:20 -0700

    pcre2 10.31
    Closes #26776.
    
    Signed-off-by: William Woodruff <william@yossarian.net>

--- Formula/pcre2.rb
+++ Formula/pcre2.rb
@@ -1,8 +1,8 @@
 class Pcre2 < Formula
   desc "Perl compatible regular expressions library with a new API"
   homepage "https://www.pcre.org/"
-  url "https://ftp.pcre.org/pub/pcre/pcre2-10.30.tar.bz2"
-  sha256 "90bd41c605d30e3745771eb81928d779f158081a51b2f314bbcc1f73de5773db"
+  url "https://ftp.pcre.org/pub/pcre/pcre2-10.31.tar.bz2"
+  sha256 "e07d538704aa65e477b6a392b32ff9fc5edf75ab9a40ddfc876186c4ff4d68ac"
 
   head "svn://vcs.exim.org/pcre2/code/trunk"
 
