fa08c1263665004b8dec1ab62f97e80273f17f82
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-18 02:56:40 +0000

    kedge: update 0.12.0 bottle.

--- Formula/kedge.rb
+++ Formula/kedge.rb
@@ -6,9 +6,9 @@ class Kedge < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9ee6dcd1f7b3dfd28e2e3c84d708ea42af0758042950450e961e1389eb57bc02" => :high_sierra
-    sha256 "49330bed015b0d9ec9ade0a9708906273851c9fe5c297b31a7497b575350333c" => :sierra
-    sha256 "9e19bd2ee220a44ea866ddcb929e25c81f78282589d44a0ee38192f1d6d3d8f5" => :el_capitan
+    sha256 "ff1bf61801e5c5e17ba83abe714c4d30914a458291cdc0fc4654ee952a919c4c" => :high_sierra
+    sha256 "39f193757913fc743191091a86b5d162b4cb4af618975db1ecd649dce7a08941" => :sierra
+    sha256 "1ff9804be018e8bf5bd0668ce1e1a647ab04005b4ea34fb22f49c50c215b4e13" => :el_capitan
   end
 
   depends_on "go" => :build
