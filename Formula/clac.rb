9b72bdd47b7f135062efc85c6472988f8272629a
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-18 09:28:57 +0000

    clac: update 0.3.2 bottle.

--- Formula/clac.rb
+++ Formula/clac.rb
@@ -6,9 +6,9 @@ class Clac < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "fc5ecef046a8795d26192fab39253fc07692f16fcd456e9fc8e06e621d760c1d" => :high_sierra
-    sha256 "cd5a4f0ec0632c5b075b4183b4928b31c20899aef74fe0f481c4ffeb123c7068" => :sierra
-    sha256 "9c084fb7bf7246c9f6e02dc44c90ba51b97d58c76ca59c1f217a183d87d71211" => :el_capitan
+    sha256 "b6e5fd38ba066369ed384eb016cbc0a56411ec46f22b0e7399f21009a16b8720" => :high_sierra
+    sha256 "7e1fa4bcd0c7aed8586f7036e3d1fce149a5d4b321f496163ea5aaedbad60596" => :sierra
+    sha256 "21200708adb21d70fde9197daa3a73c9029b420a992878761e9054ec5389c6af" => :el_capitan
   end
 
   def install
