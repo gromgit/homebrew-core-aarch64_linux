08e47e02be13c7fa2d7215a84709aa1a322e59ad
Recep Aslantas <info@recp.me>
2018-04-18 22:36:58 +0300

    cglm 0.4.4 (new formula)
    Closes #26792.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/cglm.rb
@@ -0,0 +1,38 @@
+class Cglm < Formula
+  desc "Optimized OpenGL/Graphics Math (glm) for C"
+  homepage "https://github.com/recp/cglm"
+  url "https://github.com/recp/cglm/archive/v0.4.4.tar.gz"
+  sha256 "b62dff42202f9302ed1dfbad039134c45ff92c809052598aa1c469aab91a65d3"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
+  def install
+    system "autoreconf", "-fiv"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <cglm/cglm.h>
+      #include <assert.h>
+
+      int main() {
+        vec3 x = {1.0f, 0.0f, 0.0f},
+             y = {0.0f, 1.0f, 0.0f},
+             z = {0.0f, 0.0f, 1.0f};
+        vec3 r;
+
+        glm_cross(x, y, r);
+        assert(glm_vec_eqv_eps(r, z));
+        return 0;
+      }
+    EOS
+    system ENV.cc, "-I#{include}", testpath/"test.c", "-o", "test"
+    system "./test"
+  end
+end
