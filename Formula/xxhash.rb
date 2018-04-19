39ec6ca7981ae0314dba57e9f9b43b42caafe608
ilovezfs <ilovezfs@icloud.com>
2018-04-18 19:27:27 -0700

    xxhash 0.6.5
    Closes #26811.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/xxhash.rb
+++ Formula/xxhash.rb
@@ -1,9 +1,8 @@
 class Xxhash < Formula
   desc "Extremely fast non-cryptographic hash algorithm"
   homepage "https://github.com/Cyan4973/xxHash"
-  url "https://github.com/Cyan4973/xxHash/archive/v0.6.4.tar.gz"
-  sha256 "4570ccd111df6b6386502791397906bf69b7371eb209af7d41debc2f074cdb22"
-  revision 1
+  url "https://github.com/Cyan4973/xxHash/archive/v0.6.5.tar.gz"
+  sha256 "19030315f4fc1b4b2cdb9d7a317069a109f90e39d1fe4c9159b7aaa39030eb95"
 
   bottle do
     cellar :any
