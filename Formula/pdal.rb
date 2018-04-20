b9d76f4dec3e91f2cf4155131cf9a8235bff3f06
ilovezfs <ilovezfs@icloud.com>
2018-04-20 08:45:54 -0700

    pdal: revision for hdf5
    Closes #26877.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pdal.rb
+++ Formula/pdal.rb
@@ -3,7 +3,7 @@ class Pdal < Formula
   homepage "https://www.pdal.io/"
   url "https://github.com/PDAL/PDAL/archive/1.7.1.tar.gz"
   sha256 "a560d3962b5ffdf58876155b33f816f24dbaf9a313ae308d9bf63adb8edac951"
-  revision 1
+  revision 2
   head "https://github.com/PDAL/PDAL.git"
 
   bottle do
