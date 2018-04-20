624a0be0b73996835d47b78808f885e7d8212f1d
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-20 06:09:46 +0000

    qmmp: update 1.2.1 bottle.

--- Formula/qmmp.rb
+++ Formula/qmmp.rb
@@ -6,9 +6,9 @@ class Qmmp < Formula
   head "https://svn.code.sf.net/p/qmmp-dev/code/branches/qmmp-1.2/"
 
   bottle do
-    sha256 "fde6046322179080f7d691fead99fba034d1d7e2c215883cb5075800d6b63888" => :high_sierra
-    sha256 "af9bfaf97426f5c4610636605c3055827f8812ef0011c477548cbe9db58d8093" => :sierra
-    sha256 "1b5c09baa8ea2cb0dfed57048dca8b7c68a70eedcaf683d51d9d273fc653ba46" => :el_capitan
+    sha256 "221e26205bbbccf57d16f8317213c2ba9c120bd7766f07d68127f834e54fc3e1" => :high_sierra
+    sha256 "c834f256c2efbd8353e56dd27321b0e34087fd3e275e429ed135507d8c21aea1" => :sierra
+    sha256 "8be08514cb41775e7ede35d644b526f5066e3ad7d763dc954accb025379a2b1d" => :el_capitan
   end
 
   depends_on "cmake" => :build
