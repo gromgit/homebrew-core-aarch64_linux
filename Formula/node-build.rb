ee66c242598c273ba3730472e6e0ea94b678f666
ilovezfs <ilovezfs@icloud.com>
2018-04-19 23:44:02 -0700

    node-build 3.0.8 (#26850)

--- Formula/node-build.rb
+++ Formula/node-build.rb
@@ -1,8 +1,8 @@
 class NodeBuild < Formula
   desc "Install NodeJS versions"
   homepage "https://github.com/nodenv/node-build"
-  url "https://github.com/nodenv/node-build/archive/v3.0.7.tar.gz"
-  sha256 "74bbd151b1317d63e73fc27180b2607075138aef8b7c107e670e442069b9c390"
+  url "https://github.com/nodenv/node-build/archive/v3.0.8.tar.gz"
+  sha256 "c3287433e4e0d85613beb6ca2661db98c9da0ef1ebb856796039c8e3436245f1"
   head "https://github.com/nodenv/node-build.git"
 
   bottle :unneeded
