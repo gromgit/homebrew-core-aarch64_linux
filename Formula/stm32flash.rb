9bcf6f723d1b764fd09b26c0a07550093376e3a2
Rickard von Essen <rickard.von.essen@gmail.com>
2018-04-20 15:30:19 +0200

    stm32flash 0.5 (new formula)
    Closes #26874.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/stm32flash.rb
@@ -0,0 +1,22 @@
+class Stm32flash < Formula
+  desc "Open source flash program for STM32 using the ST serial bootloader"
+  homepage "https://sourceforge.net/projects/stm32flash/"
+  url "https://downloads.sourceforge.net/project/stm32flash/stm32flash-0.5.tar.gz"
+  sha256 "97aa9422ef02e82f7da9039329e21a437decf972cb3919ad817f70ac9a49e306"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2cbbf094a7f2777b674909a5f846bba0cb613a5c2c38e980b67bd769b924e5f5" => :high_sierra
+    sha256 "74a92cff8b8099a2b8ee8aa0a2a360639400eb53a24b625c149b052e3f26521e" => :sierra
+    sha256 "1e49a9386e4aac0260e3b24872714e59f3984c7f6fb2779e9bd89e0d23bc1655" => :el_capitan
+  end
+
+  def install
+    system "make", "PREFIX=#{prefix}", "install"
+  end
+
+  test do
+    output = shell_output("#{bin}/stm32flash -k /dev/tty.XYZ 2>&1", 1)
+    assert_match "Failed to open port: /dev/tty.XYZ", output
+  end
+end
