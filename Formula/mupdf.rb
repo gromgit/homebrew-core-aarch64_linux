91da94a2a601bd81351a7a22dfc252b93634a7f9
ilovezfs <ilovezfs@icloud.com>
2018-04-20 09:00:12 -0700

    mupdf 1.13.0
    Co-authored-by: Misty De Meo <mistydemeo@gmail.com>
    
    Closes #28091.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Formula/mupdf.rb
+++ Formula/mupdf.rb
@@ -1,8 +1,8 @@
 class Mupdf < Formula
   desc "Lightweight PDF and XPS viewer"
   homepage "https://mupdf.com/"
-  url "https://mupdf.com/downloads/mupdf-1.12.0-source.tar.gz"
-  sha256 "5c6353a82f1512f4f5280cf69a3725d1adac9c8b22377ec2a447c4fc45528755"
+  url "https://mupdf.com/downloads/mupdf-1.13.0-source.tar.gz"
+  sha256 "071c6962cbee1136188da62136596a9d704b81e35fe617cd34874bbb0ae7ca09"
   head "https://git.ghostscript.com/mupdf.git"
 
   bottle do
@@ -18,6 +18,15 @@ class Mupdf < Formula
 
   conflicts_with "mupdf-tools",
     :because => "mupdf and mupdf-tools install the same binaries."
+
+  # Reverts an upstream commit which is incompatible with the macOS GLUT;
+  # the commit in question adds the use of a freeglut-only function and constants.
+  # An earlier commit added explicit OS X GLUT support, so this looks like a bug.
+  # https://bugs.ghostscript.com/show_bug.cgi?id=699374
+  patch do
+    url "https://gist.githubusercontent.com/mistydemeo/af049b9151363cd5d5fb58b8ce9e26b6/raw/1c4448c7c0e7c165c5805fd37b4de03ffb7f26fd/0001-Revert-gl-Tell-glut-to-return-from-main-loop-when-th.patch"
+    sha256 "e5c5d00874f09c6f70a1fd8db7e86f0d386c88bc209dcb287f4eef644c1de44b"
+  end
 
   def install
     system "make", "install",
