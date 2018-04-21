f0fb8c42ff32e6e840a7a6c9efedf7b720d16734
ilovezfs <ilovezfs@icloud.com>
2018-04-20 21:40:38 -0700

    fn 0.4.74
    Closes #26905.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/fn.rb
+++ Formula/fn.rb
@@ -1,8 +1,8 @@
 class Fn < Formula
   desc "Command-line tool for the fn project"
   homepage "https://fnproject.github.io"
-  url "https://github.com/fnproject/cli/archive/0.4.73.tar.gz"
-  sha256 "9f1b1fe7e1a26785122d68784c18aa7a1d13c1fe7c587ef9b0177e183af59f8c"
+  url "https://github.com/fnproject/cli/archive/0.4.74.tar.gz"
+  sha256 "9042e469e206e94da0a386e9e59623051e986deb1154e467c1b83c0d93cd8bb3"
 
   bottle do
     cellar :any_skip_relocation
