38761862b16c471f63de0678109b91f72f835f6d
Ian Gallagher <github@cdine.org>
2018-04-21 02:39:14 -0500

    testssl: fix HEAD branch (#26913)

--- Formula/testssl.rb
+++ Formula/testssl.rb
@@ -4,7 +4,7 @@ class Testssl < Formula
   url "https://github.com/drwetter/testssl.sh/archive/v2.9.5-5.tar.gz"
   version "2.9.5-5"
   sha256 "836a7b45455c95f17c4d7eec9468028a7fc6b613fd4b3c8e8e125b7b8206b89d"
-  head "https://github.com/drwetter/testssl.sh.git"
+  head "https://github.com/drwetter/testssl.sh.git", :branch => "2.9dev"
 
   bottle :unneeded
 
