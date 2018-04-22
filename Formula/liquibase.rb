9d7256421499997eea68a5c2633e2f6453fbb6e4
ilovezfs <ilovezfs@icloud.com>
2018-04-22 00:53:54 -0700

    liquibase 3.6.1 (#26940)

--- Formula/liquibase.rb
+++ Formula/liquibase.rb
@@ -1,8 +1,8 @@
 class Liquibase < Formula
   desc "Library for database change tracking"
   homepage "https://www.liquibase.org/"
-  url "https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.5.5/liquibase-3.5.5-bin.tar.gz"
-  sha256 "da0df7f6ea7694fb62b1448c3c734644d8d49795ff9bb7417db18bb1da5ff2c6"
+  url "https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.6.1/liquibase-3.6.1-bin.tar.gz"
+  sha256 "0413e379f7d1deb5382f9fc4283ba1b7b085f7abaf6c1a3e45cbf9510accc557"
 
   bottle :unneeded
 
@@ -12,6 +12,7 @@ class Liquibase < Formula
     prefix.install_metafiles
     libexec.install Dir["*"]
     bin.install_symlink libexec/"liquibase"
+    (libexec/"lib").install_symlink Dir["#{libexec}/sdk/lib-sdk/slf4j*"]
   end
 
   def caveats; <<~EOS
