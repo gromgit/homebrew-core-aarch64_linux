6131a312b5670c1a7811518043cdd9f769e6f382
Gary Ewan Park <gep13@gep13.co.uk>
2018-04-22 06:34:12 +0100

    cake 0.27.1 (#26924)

--- Formula/cake.rb
+++ Formula/cake.rb
@@ -1,8 +1,8 @@
 class Cake < Formula
   desc "Cross platform build automation system with a C# DSL"
   homepage "https://cakebuild.net/"
-  url "https://github.com/cake-build/cake/releases/download/v0.27.0/Cake-bin-net461-v0.27.0.zip"
-  sha256 "b79e7bf7a620ac269cfee056bfccddadccb4e72ee12c41fc0815d4d8d223b28a"
+  url "https://github.com/cake-build/cake/releases/download/v0.27.1/Cake-bin-net461-v0.27.1.zip"
+  sha256 "e2072d68aa401cfd75fa05413ac677fa1c03c87c65011b5c369a53109d6551c1"
 
   bottle :unneeded
 
