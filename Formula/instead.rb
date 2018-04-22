6690a0c0b2e5087d08a604b478f40f3d4cc31850
ilovezfs <ilovezfs@icloud.com>
2018-04-22 08:21:25 -0700

    instead 3.2.1
    remove head spec
    depend on sdl2, sdl2_image, sdl2_mixer and sdl2_ttf
    remove dependencies on sdl, sdl_image, sdl_mixer and sdl_ttf
    
    Closes #26952.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/instead.rb
+++ Formula/instead.rb
@@ -1,10 +1,8 @@
 class Instead < Formula
   desc "Interpreter of simple text adventures"
   homepage "https://instead.syscall.ru/"
-  url "https://github.com/instead-hub/instead/archive/3.1.2.tar.gz"
-  sha256 "622c04a58fd780d8efdf0706c03596ab68672b31e97865dad6a1fc1540619754"
-  revision 1
-  head "https://github.com/instead-hub/instead.git"
+  url "https://github.com/instead-hub/instead/archive/3.2.1.tar.gz"
+  sha256 "2f5ee94393cf9f314bd9a8617a7f16d6500376223b920bf1821ba0e8a76c3696"
 
   bottle do
     sha256 "a0a3ea00027270c2e2c187875cf0d927ea3774e9d15cecc22ad79a71f5a4055d" => :high_sierra
@@ -14,14 +12,25 @@ class Instead < Formula
 
   depends_on "cmake" => :build
   depends_on "lua"
-  depends_on "sdl"
-  depends_on "sdl_image"
-  depends_on "sdl_mixer"
-  depends_on "sdl_ttf"
+  depends_on "sdl2"
+  depends_on "sdl2_image"
+  depends_on "sdl2_mixer"
+  depends_on "sdl2_ttf"
+
+  # Remove for > 3.2.1
+  # Fix undefined symbols errors for _CFBundleCopyResourcesDirectoryURL etc.
+  # Upstream commit from 22 Apr 2018 "CMake: link AppKit framework on macOS"
+  patch do
+    url "https://github.com/instead-hub/instead/commit/e00be1e2.patch?full_index=1"
+    sha256 "63213ebeb0136f5388edfc8d7f240282a225ce73ea50ff89b319282556551d74"
+  end
 
   def install
     mkdir "build" do
-      system "cmake", "..", "-DWITH_GTK2=OFF", *std_cmake_args
+      system "cmake", "..", "-DWITH_GTK2=OFF",
+                            "-DLUA_INCLUDE_DIR=#{Formula["lua"].opt_include}",
+                            "-DLUA_LIBRARY=#{Formula["lua"].opt_lib}/liblua.dylib",
+                            *std_cmake_args
       system "make", "install"
     end
   end
