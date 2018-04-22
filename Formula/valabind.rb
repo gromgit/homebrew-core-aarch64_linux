c45e219a28c584bb797386a85e000bf4f58ba77c
ilovezfs <ilovezfs@icloud.com>
2018-04-22 00:46:18 -0700

    valabind 1.6.0
    depend on vala
    make pkg-config a build-time only dependency
    remove vendored vala
    remove gettext and glib direct dependencies
    
    Closes #26941.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/valabind.rb
+++ Formula/valabind.rb
@@ -1,8 +1,8 @@
 class Valabind < Formula
   desc "Vala bindings for radare, reverse engineering framework"
   homepage "https://radare.org/"
-  url "https://github.com/radare/valabind/archive/1.4.0.tar.gz"
-  sha256 "b2e4939912feada6138b8269d228ea82fb0f1391fd2e2e7003f404677b0cdbc9"
+  url "https://github.com/radare/valabind/archive/1.6.0.tar.gz"
+  sha256 "0d266486655c257fd993758c3e4cc8e32f0ec6f45d0c0e15bb6e6be986e4b78e"
   head "https://github.com/radare/valabind.git"
 
   bottle do
@@ -11,32 +11,11 @@ class Valabind < Formula
     sha256 "bf466be7a14313608f3d68a86135b2b25094457c0fb89b7dd389f57fc4cd173c" => :el_capitan
   end
 
-  depends_on "pkg-config" # not :build, for vala
+  depends_on "pkg-config" => :build
   depends_on "swig"
-
-  # vala dependencies
-  depends_on "gettext"
-  depends_on "glib"
-
-  # Upstream issue "Build failure with vala 0.38.0"
-  # Reported 6 Sep 2017 https://github.com/radare/valabind/issues/43
-  resource "vala" do
-    url "https://download.gnome.org/sources/vala/0.36/vala-0.36.5.tar.xz"
-    sha256 "7ae7eb8a976005afecf4f647b9043f2bb11e8b263c7fe9e905ab740b3d8a9f40"
-  end
+  depends_on "vala"
 
   def install
-    resource("vala").stage do
-      system "./configure", "--disable-dependency-tracking",
-                            "--disable-silent-rules",
-                            "--prefix=#{libexec}"
-      system "make"
-      system "make", "install"
-    end
-
-    ENV.prepend_path "PATH", libexec/"bin"
-    ENV.prepend_path "PKG_CONFIG_PATH", libexec/"lib/pkgconfig"
-
     system "make"
     system "make", "install", "PREFIX=#{prefix}"
   end
