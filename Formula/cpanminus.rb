2c3cf26bdfcd1fded20296fff7c0067ec4d6973a
ilovezfs <ilovezfs@icloud.com>
2018-04-22 00:53:40 -0700

    cpanminus 1.7044 (#26930)

--- Formula/cpanminus.rb
+++ Formula/cpanminus.rb
@@ -1,8 +1,8 @@
 class Cpanminus < Formula
   desc "Get, unpack, build, and install modules from CPAN"
   homepage "https://github.com/miyagawa/cpanminus"
-  url "https://github.com/miyagawa/cpanminus/archive/1.7043.tar.gz"
-  sha256 "7f52a6487a2462b658164f431ae6cc0b78685df3bccfe4139823372cb5b5fd42"
+  url "https://github.com/miyagawa/cpanminus/archive/1.7044.tar.gz"
+  sha256 "a5407a85c2f3dda74dfc2241c68dafb9951f2a6eeada0a0eea9e7238a482c195"
   head "https://github.com/miyagawa/cpanminus.git"
 
   bottle :unneeded
