f8d88dd92efd17c74f2f385937067f6187d156ff
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-23 03:55:42 +0000

    libde265: add 1.0.3 bottle.

--- Formula/libde265.rb
+++ Formula/libde265.rb
@@ -4,6 +4,13 @@ class Libde265 < Formula
   url "https://github.com/strukturag/libde265/releases/download/v1.0.3/libde265-1.0.3.tar.gz"
   sha256 "e4206185a7c67d3b797d6537df8dcaa6e5fd5a5f93bd14e65a755c33cd645f7a"
 
+  bottle do
+    cellar :any
+    sha256 "318155fde344fe1742f354396bd65fbd2b1ed14f420131f3ed5ff569d5a6b38f" => :high_sierra
+    sha256 "5f247bee31e10b2217023a64e5ef841566f1ee5edc7227dc15110fb507405269" => :sierra
+    sha256 "942f19c7b70c6bc6510715c13752bb99e7a4793f1f028245fd2f2b798a8efe56" => :el_capitan
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
