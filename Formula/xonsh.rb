add9661d9172bb93dc6c06e47113fca973e539a8
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-24 04:25:57 +0000

    xonsh: update 0.6.2 bottle.

--- Formula/xonsh.rb
+++ Formula/xonsh.rb
@@ -9,9 +9,9 @@ class Xonsh < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "49d7592f793791ab573dc072d02fc1e4d8e9115f50500e55b38e7412a98decb4" => :high_sierra
-    sha256 "1bc169097c1f1adefd86bb681b22020f0c83f79aa0442ff492d55c17f3e30000" => :sierra
-    sha256 "172d779cb699d5c433217724cb7f61a8d9219649da36c89ff70fe33a8dc7540f" => :el_capitan
+    sha256 "27d8c76ace6e80bbe4dd4c901612a66c9e5e08010e64c33529b6b8104814bf2b" => :high_sierra
+    sha256 "0c70540bd1677d150c65ce85884eca725c397b0ba69e5e01145da76a25f34e90" => :sierra
+    sha256 "42d1b4d491fb322b6221c3f8b5bd23d76ffe7dd0fa32e079d2bdad0344912b6b" => :el_capitan
   end
 
   depends_on "python"
