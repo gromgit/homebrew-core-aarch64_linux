0ac4c5717e668a02c6633a0fd8afa41a8cda5288
commitay <commitay@users.noreply.github.com>
2018-04-24 12:26:07 +1000

    xpdf: update homepage (#26986)

--- Formula/xpdf.rb
+++ Formula/xpdf.rb
@@ -1,6 +1,6 @@
 class Xpdf < Formula
   desc "PDF viewer"
-  homepage "http://www.foolabs.com/xpdf/"
+  homepage "https://www.xpdfreader.com/"
   url "https://xpdfreader-dl.s3.amazonaws.com/xpdf-4.00.tar.gz"
   mirror "https://fossies.org/linux/misc/xpdf-4.00.tar.gz"
   sha256 "ff3d92c42166e35b1ba6aec9b5f0adffb5fc05a3eb95dc49505b6e344e4216d6"
