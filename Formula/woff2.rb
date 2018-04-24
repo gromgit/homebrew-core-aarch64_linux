d3166498194263ada54a9a78a28057c635065228
Rob Speed <speed.rob@gmail.com>
2018-04-24 15:48:39 -0400

    woff2 1.0.2 (new formula)
    Closes #27037.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/woff2.rb
@@ -0,0 +1,41 @@
+class Woff2 < Formula
+  desc "Utilities to create and convert Web Open Font File (WOFF) files"
+  homepage "https://github.com/google/woff2"
+  url "https://github.com/google/woff2/archive/v1.0.2.tar.gz"
+  sha256 "add272bb09e6384a4833ffca4896350fdb16e0ca22df68c0384773c67a175594"
+
+  depends_on "cmake" => :build
+  depends_on "brotli"
+
+  resource "roboto_1" do
+    url "https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxP.ttf"
+    sha256 "466989fd178ca6ed13641893b7003e5d6ec36e42c2a816dee71f87b775ea097f"
+  end
+
+  resource "roboto_2" do
+    url "https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu72xKKTU1Kvnz.woff2"
+    sha256 "90a0ad0b48861588a6e33a5905b17e1219ea87ab6f07ccc41e7c2cddf38967a8"
+  end
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+
+    # make install does not install binaries
+    bin.install "woff2_info", "woff2_decompress", "woff2_compress"
+  end
+
+  test do
+    # Convert a TTF to WOFF2
+    resource("roboto_1").stage testpath
+    system "#{bin}/woff2_compress", "KFOmCnqEu92Fr1Mu4mxP.ttf"
+    output = shell_output("#{bin}/woff2_info KFOmCnqEu92Fr1Mu4mxP.woff2")
+    assert_match "WOFF2Header", output
+
+    # Convert a WOFF2 to TTF
+    resource("roboto_2").stage testpath
+    system "#{bin}/woff2_decompress", "KFOmCnqEu92Fr1Mu72xKKTU1Kvnz.woff2"
+    output = shell_output("file --brief KFOmCnqEu92Fr1Mu72xKKTU1Kvnz.ttf")
+    assert_match "TrueType font data", output
+  end
+end
