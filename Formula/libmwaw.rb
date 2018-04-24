f9167a69e4873184f5b463b898109f6cd8979e07
ilovezfs <ilovezfs@icloud.com>
2018-04-24 05:24:51 -0700

    libmwaw 0.3.14
    Closes #27022.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libmwaw.rb
+++ Formula/libmwaw.rb
@@ -1,8 +1,8 @@
 class Libmwaw < Formula
   desc "Library for converting legacy Mac document formats"
   homepage "https://sourceforge.net/p/libmwaw/wiki/Home/"
-  url "https://downloads.sourceforge.net/project/libmwaw/libmwaw/libmwaw-0.3.13/libmwaw-0.3.13.tar.xz"
-  sha256 "db55c728448f9c795cd71a0bb6043f6d4744e3e001b955a018a2c634981d5aea"
+  url "https://downloads.sourceforge.net/project/libmwaw/libmwaw/libmwaw-0.3.14/libmwaw-0.3.14.tar.xz"
+  sha256 "aca8bf1ce55ed83adbea82c70d4c8bebe8139f334b3481bf5a6e407f91f33ce9"
 
   bottle do
     cellar :any
@@ -17,16 +17,6 @@ class Libmwaw < Formula
   resource "test_document" do
     url "https://github.com/openpreserve/format-corpus/raw/825c8a5af012a93cf7aac408b0396e03a4575850/office-examples/Old%20Word%20file/NEWSSLID.DOC"
     sha256 "df0af8f2ae441f93eb6552ed2c6da0b1971a0d82995e224b7663b4e64e163d2b"
-  end
-
-  # Remove for > 0.3.13
-  # Upstream commit from 15 Nov 2017 "fix call of explicit ctor"
-  # See https://sourceforge.net/p/libmwaw/libmwaw/ci/4bc8ec0481f89b989b0c34236c9d5d9b8038d4a9/
-  if DevelopmentTools.clang_build_version <= 800
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/0eedf78/libmwaw/ctor.patch"
-      sha256 "1a443f16bf8918c642ce92c623c94ecc11198711c062ed0637ec36dd6ff6bb8b"
-    end
   end
 
   def install
