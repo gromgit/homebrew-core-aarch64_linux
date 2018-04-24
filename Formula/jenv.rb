bdc8351999e4ff85072fd83d09d5b79e8a7e8384
Viktor Szakats <vszakats@users.noreply.github.com>
2018-04-24 17:56:44 +0000

    jenv: secure homepage

--- Formula/jenv.rb
+++ Formula/jenv.rb
@@ -1,6 +1,6 @@
 class Jenv < Formula
   desc "Manage your Java environment"
-  homepage "http://www.jenv.be"
+  homepage "https://www.jenv.be/"
   url "https://github.com/gcuisinier/jenv/archive/0.4.4.tar.gz"
   sha256 "74b48d9c33ceae4e141272c4096086c6ec1a8f10073da379b816518615c79881"
   head "https://github.com/gcuisinier/jenv.git"
