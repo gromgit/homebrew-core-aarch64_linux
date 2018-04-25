9493dc63dfd57a9da444001c9bfd4f917872814d
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-25 21:38:25 +0000

    vault: update 0.10.1 bottle.

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -11,9 +11,9 @@ class Vault < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "4ff1b5e8eb778ffc3ec619a5eec1a0de8ebbd8ad9fd7bd5dc5995b63a86d7ad3" => :high_sierra
-    sha256 "26806f016116d3e7b398c3a9103e7959d9f5e0df91f798c3f42f280806133b73" => :sierra
-    sha256 "6f1fb1b31efd2c5be1398ca564618dc9c3aa8ae37b58033014f12215c5a640d6" => :el_capitan
+    sha256 "d95f1776f9244eb4250f9623407f280f4ac4651fd488e54e113e24d7acb9fd85" => :high_sierra
+    sha256 "c1840e30d25c7e8b6b3aa31b2097c4786ce6e93bfaebf11dd4e985494cd6c928" => :sierra
+    sha256 "93ad8024af250d62a0469e9b7d6a871cf7c7acd21ec74460c4990516c335be95" => :el_capitan
   end
 
   option "with-dynamic", "Build dynamic binary with CGO_ENABLED=1"
