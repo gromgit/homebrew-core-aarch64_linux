b5951e864e965ecf0a4c4978db5dc8c532bd854c
JCount <JCount42@gmail.com>
2018-04-24 21:52:33 -0400

    libmatroska 1.4.9
    also update build system to use cmake

--- Formula/libmatroska.rb
+++ Formula/libmatroska.rb
@@ -1,9 +1,9 @@
 class Libmatroska < Formula
   desc "Extensible, open standard container format for audio/video"
   homepage "https://www.matroska.org/"
-  url "https://dl.matroska.org/downloads/libmatroska/libmatroska-1.4.8.tar.xz"
-  sha256 "d8c72b20d4c5bf888776884b0854f95e74139b5267494fae1f395f7212d7c992"
-  revision 1
+  url "https://dl.matroska.org/downloads/libmatroska/libmatroska-1.4.9.tar.xz"
+  sha256 "38a61dd5d87c070928b5deb3922b63b2b83c09e2e4a10f9393eecb6afa9795c8"
+  head "https://github.com/Matroska-Org/libmatroska.git"
 
   bottle do
     cellar :any
@@ -12,22 +12,13 @@ class Libmatroska < Formula
     sha256 "60c6aea336b9d18883da2bdccbd1979836e0803232f57c688806c5886969912c" => :el_capitan
   end
 
-  head do
-    url "https://github.com/Matroska-Org/libmatroska.git"
-    depends_on "automake" => :build
-    depends_on "autoconf" => :build
-    depends_on "libtool" => :build
-  end
-
-  depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
   depends_on "libebml"
 
   def install
-    system "autoreconf", "-fi" if build.head?
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    system "make", "install"
+    mkdir "build" do
+      system "cmake", "..", "-DBUILD_SHARED_LIBS=YES", *std_cmake_args
+      system "make", "install"
+    end
   end
 end
