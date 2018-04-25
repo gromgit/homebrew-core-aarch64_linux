9a748100689cf1dcdf4f3591f15ad9653c960bfb
ilovezfs <ilovezfs@icloud.com>
2018-04-24 20:58:01 -0700

    abcmidi 2018.04.24
    Closes #27049.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/abcmidi.rb
+++ Formula/abcmidi.rb
@@ -1,8 +1,8 @@
 class Abcmidi < Formula
   desc "Converts abc music notation files to MIDI files"
   homepage "https://www.ifdo.ca/~seymour/runabc/top.html"
-  url "https://www.ifdo.ca/~seymour/runabc/abcMIDI-2018.04.18.zip"
-  sha256 "58384f641feba9b7ab38155f5c81521242a2ddcdbfa4313bb9e2389088ac230d"
+  url "https://www.ifdo.ca/~seymour/runabc/abcMIDI-2018.04.24.zip"
+  sha256 "816367f511847cec0ad27eae953f8e8e594352f4341957ee86ca8da2c1d1e935"
 
   bottle do
     cellar :any_skip_relocation
