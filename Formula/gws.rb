8c85ac34e44e85a183163bb30a5e9418f47ce870
ilovezfs <ilovezfs@icloud.com>
2018-04-25 06:30:21 -0700

    gws 0.2.0 (#27053)

--- Formula/gws.rb
+++ Formula/gws.rb
@@ -1,8 +1,8 @@
 class Gws < Formula
   desc "Manage workspaces composed of git repositories"
   homepage "https://streakycobra.github.io/gws/"
-  url "https://github.com/StreakyCobra/gws/archive/0.1.16.tar.gz"
-  sha256 "695df2ca872fc0ab4b8175c74fdab4d843534574e05885c6a1a6328951ccca74"
+  url "https://github.com/StreakyCobra/gws/archive/0.2.0.tar.gz"
+  sha256 "f92b7693179c2522c57edd578abdb90b08f6e2075ed27abd4af56c1283deab1a"
 
   bottle :unneeded
 
@@ -19,6 +19,6 @@ class Gws < Formula
     system "git", "init", "project"
     system "#{bin}/gws", "init"
     output = shell_output("#{bin}/gws status")
-    assert_equal "project:\n                              Clean [Local only repository]\n", output
+    assert_equal "project:\n  *                           Clean [Local only repository]\n", output
   end
 end
