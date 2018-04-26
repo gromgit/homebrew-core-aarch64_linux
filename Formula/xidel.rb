2720c4c1724f81ae57448b75bdf4fdf1374a2204
Benito van der Zander <benito@benibela.de>
2018-04-26 23:02:20 +0200

    xidel 0.9.8 (new formula)
    Closes #27149.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/xidel.rb
@@ -0,0 +1,21 @@
+class Xidel < Formula
+  desc "XPath/XQuery 3.0, JSONiq interpreter to extract data from HTML/XML/JSON"
+  homepage "http://www.videlibri.de/xidel.html"
+  url "https://github.com/benibela/xidel/releases/download/Xidel_0.9.8/xidel-0.9.8.src.tar.gz"
+  sha256 "72b5b1a2fc44a0a61831e268c45bc6a6c28e3533b5445151bfbdeaf1562af39c"
+
+  depends_on "fpc"
+  depends_on "openssl"
+
+  def install
+    cd "programs/internet/xidel" do
+      system "./build.sh"
+      bin.install "xidel"
+      man1.install "meta/xidel.1"
+    end
+  end
+
+  test do
+    assert_equal "123\n", shell_output("#{bin}/xidel -e 123")
+  end
+end
