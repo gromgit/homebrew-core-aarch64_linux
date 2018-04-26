20ddd4c188ee6b438734fb0f3a0d72d77e92944b
ilovezfs <ilovezfs@icloud.com>
2018-04-25 21:14:02 -0700

    Revert "nginx 1.14.0"
    This reverts commit 85f85ddfd3d90edbb69bc8f2bf7874920e75a3ec.

--- Formula/nginx.rb
+++ Formula/nginx.rb
@@ -1,9 +1,8 @@
 class Nginx < Formula
   desc "HTTP(S) server and reverse proxy, and IMAP/POP3 proxy server"
   homepage "https://nginx.org/"
-  # Stable releases correspond to even minor version numbers
-  url "https://nginx.org/download/nginx-1.14.0.tar.gz"
-  sha256 "5d15becbf69aba1fe33f8d416d97edd95ea8919ea9ac519eff9bafebb6022cb5"
+  url "https://nginx.org/download/nginx-1.13.12.tar.gz"
+  sha256 "fb92f5602cdb8d3ab1ad47dbeca151b185d62eedb67d347bbe9d79c1438c85de"
   head "https://hg.nginx.org/nginx/", :using => :hg
 
   bottle do
