6ede4dda25f39f09acd742a46d478dd41f24b3db
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-27 06:02:46 +0000

    bedops: update 2.4.34 bottle.

--- Formula/bedops.rb
+++ Formula/bedops.rb
@@ -6,9 +6,9 @@ class Bedops < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7e6bca355ca206b71d5ece7213722d907cd782c907def2c6a6e464c090fb0389" => :high_sierra
-    sha256 "0331704de7ea757e4657a486e20f1467d4fdff657c5bec92bef80dd8eb9ddbc1" => :sierra
-    sha256 "e11490f1c098290b52b5c7575ca16b5e46180afc94492a7b41b1236cd0129780" => :el_capitan
+    sha256 "2fcdbba7e829549668ff85dfd33d281dd09824d6d9b7adc5b7f986c754bf39c0" => :high_sierra
+    sha256 "e6cb89dd6bb66f40c09b06e3b9c35184b040a0c2334b59c52e0615cca3cc11db" => :sierra
+    sha256 "ac524ad51227b7424026c42a11d2a04db385b3a87e06b6b4036e880002b98cc6" => :el_capitan
   end
 
   needs :cxx11
