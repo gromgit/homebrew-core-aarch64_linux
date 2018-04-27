72abd78a17bfd50805058c0c7fdff94ea9fdbe39
ilovezfs <ilovezfs@icloud.com>
2018-04-26 22:29:38 -0700

    mruby 1.4.1
    Closes #27169.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mruby.rb
+++ Formula/mruby.rb
@@ -1,8 +1,8 @@
 class Mruby < Formula
   desc "Lightweight implementation of the Ruby language"
   homepage "https://mruby.org/"
-  url "https://github.com/mruby/mruby/archive/1.4.0.tar.gz"
-  sha256 "2c4a318dbb05ec745c6c4d90b810e80385901fbeb1e7f8ca3e499f5935a82430"
+  url "https://github.com/mruby/mruby/archive/1.4.1.tar.gz"
+  sha256 "2469b1f3e3c97a34f8c3dca1bca9795f66d6b17c7be60ddfc1f3b502cdcbb400"
 
   head "https://github.com/mruby/mruby.git"
 
