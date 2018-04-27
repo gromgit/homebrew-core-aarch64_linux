169d6323af68371570b6f08b8d7804eb911ed4f9
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-27 05:31:08 +0000

    annie: update 0.6.9 bottle.

--- Formula/annie.rb
+++ Formula/annie.rb
@@ -6,9 +6,9 @@ class Annie < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ce331bcd5cc5181b63e7f875c28ebd98cda97110a6d14d98233b3d0c0936f2e3" => :high_sierra
-    sha256 "6fb6cc7648e7f975fd0a8887b6277f707f93f4bd64239e781eb564eadf7d34db" => :sierra
-    sha256 "44d7c1a200478c4ae37e71e42581c75e22294b7e5da2de7044ee8d84f2202ab6" => :el_capitan
+    sha256 "25a2f20ba454b3ab9ae49f7de5917ab4dacae34e5e179ef36ee3ea94811cac60" => :high_sierra
+    sha256 "d03dcb05e8e3dfd51dd6c2c3a232ee966f5c5925b2b9ca022f477a7598849713" => :sierra
+    sha256 "aef201163b1dc90a1a654beadd0b138015503a02728ba75232e237ba3d3d7c4a" => :el_capitan
   end
 
   depends_on "go" => :build
