8501a31313704ad0654c5fd8d9a8af55b15e4669
Sam Jones <github@samjones.io>
2018-04-28 18:33:43 -0400

    libbitcoin-client 3.5.0 (new formula)

---
+++ Formula/libbitcoin-client.rb
@@ -0,0 +1,101 @@
+class LibbitcoinClient < Formula
+  desc "Bitcoin Client Query Library"
+  homepage "https://github.com/libbitcoin/libbitcoin-client"
+  url "https://github.com/libbitcoin/libbitcoin-client/archive/v3.5.0.tar.gz"
+  sha256 "bafa26647f334ecad04fc4bbef507a1954d7e0682f07bd38b90ab66dba5fe0d2"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libbitcoin"
+  depends_on "zeromq"
+
+  resource "libbitcoin-protocol" do
+    url "https://github.com/libbitcoin/libbitcoin-protocol/archive/v3.5.0.tar.gz"
+    sha256 "9deac6908489e2d59fb9f89c895c49b00e01902d5fdb661f67d4dbe45b22af76"
+  end
+
+  def install
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["libbitcoin"].opt_libexec/"lib/pkgconfig"
+    ENV.prepend_create_path "PKG_CONFIG_PATH", libexec/"lib/pkgconfig"
+
+    resource("libbitcoin-protocol").stage do
+      system "./autogen.sh"
+      system "./configure", "--disable-dependency-tracking",
+                            "--disable-silent-rules",
+                            "--prefix=#{libexec}"
+      system "make", "install"
+    end
+
+    system "./autogen.sh"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <bitcoin/client.hpp>
+      class stream_fixture
+        : public libbitcoin::client::stream
+      {
+      public:
+        libbitcoin::data_stack out;
+
+        virtual int32_t refresh() override
+        {
+          return 0;
+        }
+
+        virtual bool read(stream& stream) override
+        {
+          return false;
+        }
+
+        virtual bool write(const libbitcoin::data_stack& data) override
+        {
+          out = data;
+          return true;
+        }
+      };
+      static std::string to_string(libbitcoin::data_slice data)
+      {
+        return std::string(data.begin(), data.end()) + "\0";
+      }
+      static void remove_optional_delimiter(libbitcoin::data_stack& stack)
+      {
+        if (!stack.empty() && stack.front().empty())
+          stack.erase(stack.begin());
+      }
+      static const uint32_t test_height = 0x12345678;
+      static const char address_satoshi[] = "1PeChFbhxDD9NLbU21DfD55aQBC4ZTR3tE";
+      #define PROXY_TEST_SETUP \
+        static const uint32_t retries = 0; \
+        static const uint32_t timeout_ms = 2000; \
+        static const auto on_error = [](const libbitcoin::code&) {}; \
+        static const auto on_unknown = [](const std::string&) {}; \
+        stream_fixture capture; \
+        libbitcoin::client::proxy proxy(capture, on_unknown, timeout_ms, retries)
+      #define HANDLE_ROUTING_FRAMES(stack) \
+        remove_optional_delimiter(stack);
+      int main() {
+        PROXY_TEST_SETUP;
+
+        const auto on_reply = [](const libbitcoin::chain::history::list&) {};
+        proxy.blockchain_fetch_history3(on_error, on_reply, libbitcoin::wallet::payment_address(address_satoshi), test_height);
+
+        HANDLE_ROUTING_FRAMES(capture.out);
+        assert(capture.out.size() == 3u);
+        assert(to_string(capture.out[0]) == "blockchain.fetch_history3");
+        assert(libbitcoin::encode_base16(capture.out[2]) == "f85beb6356d0813ddb0dbb14230a249fe931a13578563412");
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "test.cpp",
+                    "-I#{libexec}/include",
+                    "-lbitcoin", "-lbitcoin-client", "-lboost_system",
+                    "-o", "test"
+    system "./test"
+  end
+end
