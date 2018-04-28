777709bdda6364d4decb25e821c9c10506be9a8a
commitay <commitay@users.noreply.github.com>
2018-04-28 12:53:11 +1000

    pgweb 0.9.12 (new formula)
    Closes #27194.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

---
+++ Formula/pgweb.rb
@@ -0,0 +1,41 @@
+class Pgweb < Formula
+  desc "Web-based PostgreSQL database browser"
+  homepage "https://sosedoff.github.io/pgweb/"
+  url "https://github.com/sosedoff/pgweb/archive/v0.9.12.tar.gz"
+  sha256 "4c625bad8312dacf9bc9d64d40c2dea1e840175db9c60667a641c62289f9f174"
+
+  depends_on "go" => :build
+  depends_on "go-bindata" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/sosedoff/pgweb").install buildpath.children
+
+    cd "src/github.com/sosedoff/pgweb" do
+      # Avoid running `go get`
+      inreplace "Makefile", "go get", ""
+
+      system "make", "build"
+      bin.install "pgweb"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    require "socket"
+
+    server = TCPServer.new(0)
+    port = server.addr[1]
+    server.close
+
+    begin
+      pid = fork do
+        exec bin/"pgweb", "--listen=#{port}", "--skip-open"
+      end
+      sleep 2
+      assert_match "pgweb", shell_output("curl http://localhost:#{port}")
+    ensure
+      Process.kill("TERM", pid)
+    end
+  end
+end
