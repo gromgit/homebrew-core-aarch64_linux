7ddb10c0a4e6c06d0fc216de1ac44784102d514b
ilovezfs <ilovezfs@icloud.com>
2018-04-30 04:27:33 -0700

    amqp-cpp 3.0.2
    Closes #27264.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/amqp-cpp.rb
+++ Formula/amqp-cpp.rb
@@ -1,8 +1,8 @@
 class AmqpCpp < Formula
   desc "C++ library for communicating with a RabbitMQ message broker"
   homepage "https://github.com/CopernicaMarketingSoftware/AMQP-CPP"
-  url "https://github.com/CopernicaMarketingSoftware/AMQP-CPP/archive/v3.0.1.tar.gz"
-  sha256 "8fe0c6a6eea09abc0ba619cbc66d092f2537de5dce8f5868d7fabcdb040752ed"
+  url "https://github.com/CopernicaMarketingSoftware/AMQP-CPP/archive/v3.0.2.tar.gz"
+  sha256 "72d68d8450aa9f992c3401345e026050d69f008060ce1866fe9bb7e7f8468338"
   head "https://github.com/CopernicaMarketingSoftware/AMQP-CPP.git"
 
   bottle do
