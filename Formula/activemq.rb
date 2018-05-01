e72107f1dc355c03ad6a9bb4d34f57cd51668fa4
ilovezfs <ilovezfs@icloud.com>
2018-05-01 01:15:20 -0700

    activemq 5.15.3 (#27314)

--- Formula/activemq.rb
+++ Formula/activemq.rb
@@ -1,8 +1,8 @@
 class Activemq < Formula
   desc "Apache ActiveMQ: powerful open source messaging server"
   homepage "https://activemq.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=/activemq/5.15.2/apache-activemq-5.15.2-bin.tar.gz"
-  sha256 "a8ba99124df7bbd22b22d568d2cfbe4fb3feb310bd81a282dea0aa9d141c9548"
+  url "https://www.apache.org/dyn/closer.cgi?path=/activemq/5.15.3/apache-activemq-5.15.3-bin.tar.gz"
+  sha256 "5755d298bd136842b0e2479e5aa1c1dda4f61ad358af6f3b16231a3bf711fa61"
 
   bottle :unneeded
 
