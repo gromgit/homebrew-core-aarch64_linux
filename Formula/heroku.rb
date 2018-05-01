7d9b5149a944526b4f2e52beaf2bd6273db94257
ilovezfs <ilovezfs@icloud.com>
2018-04-30 20:44:25 -0700

    heroku 7.0.21
    Closes #27299.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Heroku < Formula
   desc "Command-line client for the cloud PaaS"
   homepage "https://cli.heroku.com"
-  url "https://registry.npmjs.org/heroku/-/heroku-7.0.17.tgz"
-  sha256 "73100aa1c94240dc16f398bb673efd3505eba0cb2bc296cc531783cde6364c97"
+  url "https://registry.npmjs.org/heroku/-/heroku-7.0.21.tgz"
+  sha256 "cb3fa5ce2b1b408358f831358fdb7f16728a9e3f63bff4e3f5ff40b45e9d6323"
   head "https://github.com/heroku/cli.git"
 
   bottle do
