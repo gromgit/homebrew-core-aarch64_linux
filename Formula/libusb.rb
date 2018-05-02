5d46b53c6a8511265938f1293b13f91f16c0d95b
Viktor Szakats <vszakats@users.noreply.github.com>
2018-05-02 22:06:09 +0000

    libusb: secure homepage

--- Formula/libusb.rb
+++ Formula/libusb.rb
@@ -1,6 +1,6 @@
 class Libusb < Formula
   desc "Library for USB device access"
-  homepage "http://libusb.info"
+  homepage "https://libusb.info/"
   url "https://github.com/libusb/libusb/releases/download/v1.0.22/libusb-1.0.22.tar.bz2"
   mirror "https://downloads.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.22/libusb-1.0.22.tar.bz2"
   sha256 "75aeb9d59a4fdb800d329a545c2e6799f732362193b465ea198f2aa275518157"
