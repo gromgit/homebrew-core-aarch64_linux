88a0542145640a06faf81b938aae970aa550c292
Viktor Szakats <vszakats@users.noreply.github.com>
2018-05-02 21:49:49 +0000

    zero-install: secure homepage

--- Formula/zero-install.rb
+++ Formula/zero-install.rb
@@ -1,6 +1,6 @@
 class ZeroInstall < Formula
   desc "Zero Install is a decentralised software installation system"
-  homepage "http://0install.net/"
+  homepage "https://0install.net/"
   url "https://github.com/0install/0install/archive/v2.12-1.tar.gz"
   version "2.12-1"
   sha256 "317ac6ac680d021cb475962b7f6c2bcee9c35ce7cf04ae00d72bba8113f13559"
