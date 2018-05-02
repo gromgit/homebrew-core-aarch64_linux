b1814bda721b02d0d196065cd79184afb47f9410
Sasha Gerrand <github.com-homebrew-core@sgerrand.com>
2018-05-02 16:24:47 +0100

    confluent-oss 4.1.0 (new formula)
    Closes #27380.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/confluent-oss.rb
@@ -0,0 +1,22 @@
+class ConfluentOss < Formula
+  desc "Developer-optimized distribution of Apache Kafka"
+  homepage "https://www.confluent.io/product/confluent-open-source/"
+  url "https://packages.confluent.io/archive/4.1/confluent-oss-4.1.0-2.11.tar.gz"
+  version "4.1.0"
+  sha256 "32c3aff688d6f24e1ea573efdc4e16a3a5f466f1641b770ae4ba0350e7be583a"
+
+  depends_on :java => "1.8"
+  conflicts_with "kafka", :because => "kafka also ships with identically named Kafka related executables"
+
+  def install
+    prefix.install "bin"
+    rm_rf "#{bin}/windows"
+    prefix.install "etc"
+  end
+
+  test do
+    system "#{bin}/confluent", "current"
+    output = shell_output("#{bin}/confluent list")
+    assert_match "schema-registry", output
+  end
+end
