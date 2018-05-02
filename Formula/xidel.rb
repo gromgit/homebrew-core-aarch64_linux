b008fe0c77102c5a5f6793263669882ed82a9bc7
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-02 15:54:07 +0000

    xidel: add 0.9.8 bottle.

--- Formula/xidel.rb
+++ Formula/xidel.rb
@@ -4,6 +4,13 @@ class Xidel < Formula
   url "https://github.com/benibela/xidel/releases/download/Xidel_0.9.8/xidel-0.9.8.src.tar.gz"
   sha256 "72b5b1a2fc44a0a61831e268c45bc6a6c28e3533b5445151bfbdeaf1562af39c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "20450073b91be33a6f863b5057952317d579c5835974c29bb6bee939d68dda5c" => :high_sierra
+    sha256 "13c10cdbe9158109ab5f4099805b6003fbe430a960b9956957717482651378d4" => :sierra
+    sha256 "62878dce5345f0b69db3102548e754be300063164f6dec7075d3402b73b3bc0b" => :el_capitan
+  end
+
   depends_on "fpc"
   depends_on "openssl"
 
