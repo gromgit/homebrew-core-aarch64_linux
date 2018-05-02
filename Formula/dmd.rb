d02d978ab498495b6089b6e58e151224ddf995e8
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-02 06:43:46 +0000

    dmd: update 2.080.0 bottle.

--- Formula/dmd.rb
+++ Formula/dmd.rb
@@ -23,9 +23,9 @@ class Dmd < Formula
   end
 
   bottle do
-    sha256 "806f580107fd5d3984d77c186408d8c1f244571f40e59e6bc699eb6e5977e1b7" => :high_sierra
-    sha256 "34e236cd3c59bc213a497d822642cd32023b2eaa95c31daab2d90073eb158830" => :sierra
-    sha256 "4fb028165c2f6afb94b1c930f31069b8607e74344d74dbdc1e5e84580fd5a7e9" => :el_capitan
+    sha256 "79c618dbcf2c252ee60ba8c33c5f1ab7a9ec410a6b12b825cddaf72028644f3d" => :high_sierra
+    sha256 "063a291a26c1a277ebd6a3fa18ead76971708bff3156c28e830ac9414d8c908b" => :sierra
+    sha256 "75496127e9ea51049752d858d217bff35043fa5c1daeedd755948ba89a0e3c7c" => :el_capitan
   end
 
   head do
