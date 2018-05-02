b078756178bdbb749e6e59366309034c1e942a36
ilovezfs <ilovezfs@icloud.com>
2018-05-01 22:00:19 -0700

    cjdns 20.2
    Closes #27351.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/cjdns.rb
+++ Formula/cjdns.rb
@@ -1,8 +1,8 @@
 class Cjdns < Formula
   desc "Advanced mesh routing system with cryptographic addressing"
   homepage "https://github.com/cjdelisle/cjdns/"
-  url "https://github.com/cjdelisle/cjdns/archive/cjdns-v20.1.tar.gz"
-  sha256 "feea3e3884f66731b5efe3d289d5215ad4be27acb6a5879fabed14246f649cd7"
+  url "https://github.com/cjdelisle/cjdns/archive/cjdns-v20.2.tar.gz"
+  sha256 "b114f4e89c971d2c288e3d8265396248a37134895b0e0468bf55030de84b4d2a"
   head "https://github.com/cjdelisle/cjdns.git"
 
   bottle do
