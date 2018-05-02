3de10c61a1b9cf4d41425a906965022afba772c4
Viktor Szakats <vszakats@users.noreply.github.com>
2018-05-02 22:06:09 +0000

    osm2pgrouting: secure homepage

--- Formula/osm2pgrouting.rb
+++ Formula/osm2pgrouting.rb
@@ -1,6 +1,6 @@
 class Osm2pgrouting < Formula
   desc "Import OSM data into pgRouting database"
-  homepage "http://pgrouting.org/docs/tools/osm2pgrouting.html"
+  homepage "https://pgrouting.org/docs/tools/osm2pgrouting.html"
   url "https://github.com/pgRouting/osm2pgrouting/archive/v2.3.4.tar.gz"
   sha256 "32aba345013e137e39cc7bf74466cc6c97b93e256f2754e617a00f61f57eb8c2"
   head "https://github.com/pgRouting/osm2pgrouting.git"
