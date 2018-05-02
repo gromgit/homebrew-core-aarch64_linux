354e9037f9a8c6900856d80e8e8532ad40c731c1
ilovezfs <ilovezfs@icloud.com>
2018-05-01 22:32:11 -0700

    dub 1.9.0
    Closes #27367.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/dub.rb
+++ Formula/dub.rb
@@ -1,8 +1,8 @@
 class Dub < Formula
   desc "Build tool for D projects"
   homepage "https://code.dlang.org/getting_started"
-  url "https://github.com/dlang/dub/archive/v1.8.1.tar.gz"
-  sha256 "79ad2dca0679f6d8b6a4d75e7ccea7930957134743bba290c949d5aa1aa53a14"
+  url "https://github.com/dlang/dub/archive/v1.9.0.tar.gz"
+  sha256 "48f7387e93977d0ece686106c9725add2c4f5f36250da33eaa0dbb66900f9d57"
   version_scheme 1
 
   head "https://github.com/dlang/dub.git"
@@ -11,11 +11,6 @@ class Dub < Formula
     sha256 "7484306cfaba14aff965a6a32ab47d194592ea8ae6720f617b545de586db3785" => :high_sierra
     sha256 "309c7a42cdf6b1eda8dfc8c3500c51ece625be157ee9ee6233b75655f9a14413" => :sierra
     sha256 "0f12587d134ada1588db6f6f228f8ff679101c0c53db4715bdb93a03292b65da" => :el_capitan
-  end
-
-  devel do
-    url "https://github.com/dlang/dub/archive/v1.9.0-beta.1.tar.gz"
-    sha256 "505729f13eb14845d0faf4665ae69f93430c49c03f4d67541a32052b98794c71"
   end
 
   depends_on "pkg-config" => :recommended
