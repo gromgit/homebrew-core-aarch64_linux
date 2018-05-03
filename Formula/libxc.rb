1f507d64c369a47a5469c01734ea4bcfc730b271
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-03 01:03:56 +0000

    libxc: update 3.0.1_2 bottle.

--- Formula/libxc.rb
+++ Formula/libxc.rb
@@ -7,9 +7,9 @@ class Libxc < Formula
 
   bottle do
     cellar :any
-    sha256 "10edb1b0047a9d08e4d86ebb49b3ff6f257649192be8f430893a29534c26cff7" => :high_sierra
-    sha256 "fb24b4f5ce8c2c2a438b26cdd33264850911ccd91494e038fd320b3cab8e4688" => :sierra
-    sha256 "7c5c77cef496e0d919fe0a99ee63c3594985be5f35d908bb4a1144f0dbad19bc" => :el_capitan
+    sha256 "9ea81e66b245bde49b75db6707a2e6114eb83e393dbc25bcc07e81e2cf0d1c8d" => :high_sierra
+    sha256 "8ac766961dcbb498caa01ca4a91a20dfadef0cba846ed82e02cc3da2f2bd41bc" => :sierra
+    sha256 "7a0fc81d08b6e574d12a3b0b4a1bd033cb63a698e8b2ad508de1b84db94ed005" => :el_capitan
   end
 
   depends_on "gcc" # for gfortran
