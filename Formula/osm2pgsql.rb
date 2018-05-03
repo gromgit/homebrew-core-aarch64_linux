66f9318963b13e55e196d1a22de837ac9041576b
ilovezfs <ilovezfs@icloud.com>
2018-05-02 21:57:30 -0700

    osm2pgsql 0.96.0
    Closes #27417.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/osm2pgsql.rb
+++ Formula/osm2pgsql.rb
@@ -1,9 +1,8 @@
 class Osm2pgsql < Formula
   desc "OpenStreetMap data to PostgreSQL converter"
   homepage "https://wiki.openstreetmap.org/wiki/Osm2pgsql"
-  url "https://github.com/openstreetmap/osm2pgsql/archive/0.94.0.tar.gz"
-  sha256 "9e67e400deca48185313921431884171fb087dfe9e0d21e31857b8b06f20d317"
-  revision 3
+  url "https://github.com/openstreetmap/osm2pgsql/archive/0.96.0.tar.gz"
+  sha256 "b6020e77d88772989279a69ae4678e9782989b630613754e483b5192cd39c723"
   head "https://github.com/openstreetmap/osm2pgsql.git"
 
   bottle do
