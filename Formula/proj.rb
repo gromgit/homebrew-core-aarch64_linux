46d264735aa0c7ad9beb76ac98983e8ebc01939e
Viktor Szakats <vszakats@users.noreply.github.com>
2018-05-03 22:47:10 +0000

    proj: secure homepage

--- Formula/proj.rb
+++ Formula/proj.rb
@@ -1,6 +1,6 @@
 class Proj < Formula
   desc "Cartographic Projections Library"
-  homepage "http://proj4.org/"
+  homepage "https://proj4.org/"
   url "https://download.osgeo.org/proj/proj-5.0.1.tar.gz"
   sha256 "a792f78897482ed2c4e2af4e8a1a02e294c64e32b591a635c5294cb9d49fdc8c"
 
