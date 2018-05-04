37fc3fafd820ea09733a225ae6aabb24fba03207
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-04 04:32:45 +0000

    conan: update 1.3.1 bottle.

--- Formula/conan.rb
+++ Formula/conan.rb
@@ -9,9 +9,9 @@ class Conan < Formula
 
   bottle do
     cellar :any
-    sha256 "2687cb16a7b2d453189db9bffc06ff1eb118ab07dd4015e7f92d51446963152e" => :high_sierra
-    sha256 "c9702d8e42d9761fd134dd1fbb5f5ba9486a04a379a2727934e0c0191d753d30" => :sierra
-    sha256 "0bc600a6385ce5b7a1dc23c8df5f72ac1e860f6de423774c9b9ce5223e4d22f7" => :el_capitan
+    sha256 "bba3bcfa8a7657a1e37a2b8b7e89d6ce14d146cc630365380034e61a85386c0e" => :high_sierra
+    sha256 "c568ab04c42dc051cc5b5c41b7200f64e45bdeef4f3caacc9b1746f968bae1e2" => :sierra
+    sha256 "6d47bd19d88b0ead907d1bb06c39dcd5857422f14f7fd53d858f60c089974a6c" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
