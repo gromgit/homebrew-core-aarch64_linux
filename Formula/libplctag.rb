124755363430311d19af2c113214dcc9d368e0bd
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-04 11:45:26 +0000

    libplctag: add 1.5.8 bottle.

--- Formula/libplctag.rb
+++ Formula/libplctag.rb
@@ -4,6 +4,13 @@ class Libplctag < Formula
   url "https://github.com/kyle-github/libplctag/archive/v1.5.8.tar.gz"
   sha256 "03847aeb4bdbfad255e62feff029b9b0c81758e66b840237023243b1c1b764ec"
 
+  bottle do
+    cellar :any
+    sha256 "a784c2b1d50c673cf3557e774f78b44bd1968a162f715afeca9b31e27f136d5c" => :high_sierra
+    sha256 "76e29f82f14a9f0a7b08cae7a8c13bdbbf46a8bf947ea1a2c7b1b7eea2a25165" => :sierra
+    sha256 "13703497dc39ded95c332fd2eb4cae4aa34bb0ee77c7027de1b37b0ec276052b" => :el_capitan
+  end
+
   depends_on "cmake" => :build
 
   def install
