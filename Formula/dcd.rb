08d5604d5efdba92c9826dad175a65c504c38e31
ilovezfs <ilovezfs@icloud.com>
2018-05-05 06:15:03 -0700

    dcd 0.9.5
    Closes #27519.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dcd.rb
+++ Formula/dcd.rb
@@ -2,8 +2,8 @@ class Dcd < Formula
   desc "Auto-complete program for the D programming language"
   homepage "https://github.com/dlang-community/DCD"
   url "https://github.com/dlang-community/DCD.git",
-      :tag => "v0.9.4",
-      :revision => "c90afe1ece9a7f5723acf284b59ab390f2b18a13"
+      :tag => "v0.9.5",
+      :revision => "723a3ee113941742b310010d221b9dfb38206e42"
 
   head "https://github.com/dlang-community/dcd.git", :shallow => false
 
