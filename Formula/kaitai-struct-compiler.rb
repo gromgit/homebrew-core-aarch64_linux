a3c1e966595377b530c3528b1065748f2f3826cc
Viktor Szakats <vszakats@users.noreply.github.com>
2018-05-07 11:11:47 +0000

    kaitai-struct-compiler: secure url(s)

--- Formula/kaitai-struct-compiler.rb
+++ Formula/kaitai-struct-compiler.rb
@@ -1,6 +1,6 @@
 class KaitaiStructCompiler < Formula
   desc "Compiler for generating binary data parsers"
-  homepage "http://kaitai.io/"
+  homepage "https://kaitai.io/"
   url "https://bintray.com/artifact/download/kaitai-io/universal/0.8/kaitai-struct-compiler-0.8.zip"
   sha256 "545fc10e134db2901cad8817be1b440fca6f2bad8b92b2948ebe0647f3ffa2c9"
 
