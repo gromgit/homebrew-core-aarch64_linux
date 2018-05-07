7ea834eb9b74cf7ceb8c4d697ecb996f721dcfed
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-07 07:55:03 +0000

    mikutter: update 3.7.0 bottle.

--- Formula/mikutter.rb
+++ Formula/mikutter.rb
@@ -6,9 +6,9 @@ class Mikutter < Formula
   head "git://toshia.dip.jp/mikutter.git", :branch => "develop"
 
   bottle do
-    sha256 "0e7dd7633e4ef9d6a3a052b0c3dc0b3dae1b5930165a90cae8cd14334a2b34e9" => :high_sierra
-    sha256 "977259873efd332c1e115c414463a21620bf830bf41eac8f2bf99c255db103e5" => :sierra
-    sha256 "54dae8717415c681a119085c80e5d6a6b448788373c36eb3e8c832ab74d8cca9" => :el_capitan
+    sha256 "80c9254c614fbf10a96fe52b010ea77414d05f0c34321e88c2988789876326bb" => :high_sierra
+    sha256 "c961947fbe0630c5bd4ab5a559ba6b9a89f0ee691f6c1dc569835e6928af46db" => :sierra
+    sha256 "836f45bba1056c7d5bc078d5a906669496cc0601140eb1b6fd4bac6677ab60a4" => :el_capitan
   end
 
   depends_on "gobject-introspection"
