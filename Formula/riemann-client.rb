28104a5ecb53d10d1763af6694b9a64c7f55ce54
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-08 21:31:39 +0000

    riemann-client: update 1.10.2_1 bottle.

--- Formula/riemann-client.rb
+++ Formula/riemann-client.rb
@@ -8,10 +8,9 @@ class RiemannClient < Formula
 
   bottle do
     cellar :any
-    sha256 "37c265236767d2343b258024f99c18b1e0fa823e46fe8fd694a43c1e936c3c5c" => :high_sierra
-    sha256 "cf5f268e934bbf16faf8fcb9dd042698daa1199d6f465b70d700482ee1c03d52" => :sierra
-    sha256 "5ea7d37d883608833a4f75ff024850a4754a8ecacc8246f3f689eb2f25dff564" => :el_capitan
-    sha256 "58c9d2d70c5523aa89060ee54d4d85de87c5c7aa6a8ae0b876f9ddefa7ceabf6" => :yosemite
+    sha256 "ba9cdc31d7fe2916f8e3372991b6220ff314cea3b3ec12a5a340b0ad248b3fcf" => :high_sierra
+    sha256 "c57193eb61597b2fc23bdba949187fcdb9501104be779f9478c6f59d58a0916a" => :sierra
+    sha256 "061be81a19aa935e9bc24c3fcd54f113de05f0e70c7e43bc6deb62180cacaf15" => :el_capitan
   end
 
   depends_on "autoconf" => :build
