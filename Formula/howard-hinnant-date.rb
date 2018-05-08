04d048bb164e75e8e91abb8c1c7aee0b41322c11
Carlos <carlos.alvaro@citelan.es>
2018-05-08 20:59:53 +0200

    howard-hinnant-date 2.4.1 (new formula)
    Closes #27621.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/howard-hinnant-date.rb
@@ -0,0 +1,31 @@
+class HowardHinnantDate < Formula
+  desc "C++ library for date and time operations based on <chrono>"
+  homepage "https://github.com/HowardHinnant/date"
+  url "https://github.com/HowardHinnant/date/archive/v2.4.1.tar.gz"
+  sha256 "98907d243397483bd7ad889bf6c66746db0d7d2a39cc9aacc041834c40b65b98"
+
+  depends_on "cmake" => :build
+  needs :cxx11
+
+  def install
+    system "cmake", ".", *std_cmake_args,
+                         "-DENABLE_DATE_TESTING=OFF",
+                         "-DUSE_SYSTEM_TZ_DB=ON",
+                         "-DBUILD_SHARED_LIBS=ON"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include "date/tz.h"
+      #include <iostream>
+
+      int main() {
+        auto t = date::make_zoned(date::current_zone(), std::chrono::system_clock::now());
+        std::cout << t << std::endl;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-std=c++1y", "-ltz", "-o", "test"
+    system "./test"
+  end
+end
