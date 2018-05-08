4c93ecd7fae6b6abb768fc33bd6b6bfbb14efe9a
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-08 08:55:27 +0000

    rabbitmq-c: update 0.9.0 bottle.

--- Formula/rabbitmq-c.rb
+++ Formula/rabbitmq-c.rb
@@ -7,11 +7,9 @@ class RabbitmqC < Formula
 
   bottle do
     cellar :any
-    sha256 "fe5c4ef74c41a2361ad4d3496fbc33a1deb84048ce0bc9ba552a3dc533d59638" => :high_sierra
-    sha256 "12a6f5822603bec5de04cbc2dae324bbd1cc48f81d641d3403143bcfad680915" => :sierra
-    sha256 "eb9a25f5371072c0f4833c7f4554f2b5e53b7e2b5ea10e33230cad8b6c9affe4" => :el_capitan
-    sha256 "c0775f463db385d302b4d73d6403842ce16654c2f7a2618e6c0d1aa7c0590a14" => :yosemite
-    sha256 "f4e4d641af6559ee49beec28a7620af68e643ac26429c5f031953e8d79c8b0b6" => :mavericks
+    sha256 "d95c6f2c892a815ac20fe9a57fac961c73390182abb748d95d5901a3cb45d7ab" => :high_sierra
+    sha256 "b2c77dd791f014dfd33983394a369f97e23e0c4519d451b552322df9dced4081" => :sierra
+    sha256 "892c266e4c6086c65b3e4cee8cf5116f59d682b178540ee3f78efeff1e9d912a" => :el_capitan
   end
 
   option "without-tools", "Build without command-line tools"
