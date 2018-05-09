0161f577996722df7de53a2b9ae0aa741d8dbb34
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-09 08:08:54 +0000

    bat: add 0.3.0 bottle.

--- Formula/bat.rb
+++ Formula/bat.rb
@@ -4,6 +4,12 @@ class Bat < Formula
   url "https://github.com/sharkdp/bat/archive/v0.3.0.tar.gz"
   sha256 "ffb3f2b20fb831e503c3db224a563dc30e766c627a08c069cf60e5182845207c"
 
+  bottle do
+    sha256 "02e24b89b950892f6fa7abda489a5232144fb78d7da88c6c6ba17d1724fdde89" => :high_sierra
+    sha256 "4781f4b8a80c3588a4bb18d293f7b59699049c028fc962cb69ebc941951f172d" => :sierra
+    sha256 "7bec128188eff3d5f74d5d019f3b752f1ce22b0db8efed0401db3624657f1a73" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "rust" => :build
 
