6512e916fe317e3e912d1294d77c1967aaed5be1
ilovezfs <ilovezfs@icloud.com>
2018-05-08 23:48:22 -0700

    vramsteg: update homepage, stable and head urls

--- Formula/vramsteg.rb
+++ Formula/vramsteg.rb
@@ -1,9 +1,9 @@
 class Vramsteg < Formula
   desc "Add progress bars to command-line applications"
-  homepage "https://tasktools.org/projects/vramsteg.html"
-  url "https://tasktools.org/download/vramsteg-1.1.0.tar.gz"
+  homepage "https://gothenburgbitfactory.org/projects/vramsteg.html"
+  url "https://gothenburgbitfactory.org/download/vramsteg-1.1.0.tar.gz"
   sha256 "9cc82eb195e4673d9ee6151373746bd22513033e96411ffc1d250920801f7037"
-  head "https://git.tasktools.org/scm/ut/vramsteg.git", :branch => "1.1.1"
+  head "https://github.com/GothenburgBitFactory/vramsteg.git", :branch => "1.1.1"
 
   bottle do
     cellar :any_skip_relocation
