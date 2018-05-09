5bb91e7ae4ae97ac437c2b23993584fa1ca8ac6c
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-09 16:53:56 +0000

    mpfi: update 1.5.3 bottle.

--- Formula/mpfi.rb
+++ Formula/mpfi.rb
@@ -6,9 +6,9 @@ class Mpfi < Formula
 
   bottle do
     cellar :any
-    sha256 "e829d1293ff9101516234d9c8d60f05a948da004f684baaa5c8bd9d591aa6156" => :high_sierra
-    sha256 "ee2d9a5b676f12ea283b660f45abf572ca84440ec9b497f4b481a605ba07b209" => :sierra
-    sha256 "02631fb35a90f64746031700c51f42dfb67a43206754d1f56196360ed8208f4e" => :el_capitan
+    sha256 "cf0a5912aafe1a39c596d9303f12361e403f96bd52c25db6aaee1dd9a6139360" => :high_sierra
+    sha256 "81e48e4445dbe6e7fa4ede18ba9c5a7a75c8816d3b0268333d4535afa4b5e7cd" => :sierra
+    sha256 "9345063d6fdde9da2d16c233251300aa563668ab17675dd2dc11e4c75de88fb4" => :el_capitan
   end
 
   depends_on "gmp"
