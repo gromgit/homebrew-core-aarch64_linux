816e0a751107dbdf5763bab7382cfe379e49156f
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-10 09:52:52 +0000

    pgweb: add 0.9.12 bottle.

--- Formula/pgweb.rb
+++ Formula/pgweb.rb
@@ -4,6 +4,13 @@ class Pgweb < Formula
   url "https://github.com/sosedoff/pgweb/archive/v0.9.12.tar.gz"
   sha256 "4c625bad8312dacf9bc9d64d40c2dea1e840175db9c60667a641c62289f9f174"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "31ef10e4430148ed0b70f32d1f6edf7c8741e02f4f2fbe21fd7c49783b3e0594" => :high_sierra
+    sha256 "1f85f4bdf34399632b1a64a79ef39810909ba5faa3022def736caed9a898ff9a" => :sierra
+    sha256 "640b9e3819d9915f00f39bb4319326a5d2f04ed75df4907f9a79db5231015812" => :el_capitan
+  end
+
   depends_on "go" => :build
   depends_on "go-bindata" => :build
 
