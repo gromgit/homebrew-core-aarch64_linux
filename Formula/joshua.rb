5b01fadbfdd7511008543c2071a54acf9b0c2ae4
ilovezfs <ilovezfs@icloud.com>
2018-05-10 03:24:42 -0700

    joshua: remove options (#27698)

--- Formula/joshua.rb
+++ Formula/joshua.rb
@@ -15,30 +15,11 @@ class Joshua < Formula
     sha256 "176fa47a6a2722fb5b6bf1e2efba8da32bab6355f3d844424a817882ed7b3a8e" => :mavericks
   end
 
-  option "with-es-en-phrase-pack", "Build with Spanish–English phrase-based model [1.9 GB]."
-  option "with-ar-en-phrase-pack", "Build with Arabic–English phrase-based model [2.1 GB]."
-  option "with-zh-en-hiero-pack", "Build with Chinese->English hiero-based model [2.4 GB]."
-
   depends_on :java
   depends_on "ant" => :build
   depends_on "boost" => :build
   depends_on "md5sha1sum" => :build
   depends_on "python@2" => :build
-
-  resource "es-en-phrase-pack" do
-    url "https://cs.jhu.edu/~post/language-packs/language-pack-es-en-phrase-2015-03-06.tgz"
-    sha256 "213e05bbdcfbfa05b31e263c31f10a0315695fee26c2f37b0a78fb918bad9b5d"
-  end
-
-  resource "ar-en-phrase-pack" do
-    url "https://cs.jhu.edu/~post/language-packs/language-pack-ar-en-phrase-2015-03-18.tgz"
-    sha256 "2b6665b58b11e4c25d48191d3d5b62b7c591851a9767b14f9ccebf1951fddf90"
-  end
-
-  resource "zh-en-hiero-pack" do
-    url "https://cs.jhu.edu/~post/language-packs/zh-en-hiero-2016-01-13.tgz"
-    sha256 "ded27fe639d019c91cfefce513abb762ad41483962b957474573e2042c786d46"
-  end
 
   def install
     rm Dir["lib/*.{gr,tar.gz}"]
@@ -46,21 +27,6 @@ class Joshua < Formula
     rm_rf "bin/.gitignore"
     head do
       system "ant"
-    end
-    if build.with? "es-en-phrase-pack"
-      resource("es-en-phrase-pack").stage do
-        (libexec/"language-pack-es-en-phrase-2015-03-06").install Dir["*"]
-      end
-    end
-    if build.with? "ar-en-phrase-pack"
-      resource("ar-en-phrase-pack").stage do
-        (libexec/"language-pack-ar-en-phrase-2015-03-18").install Dir["*"]
-      end
-    end
-    if build.with? "zh-en-hiero-pack"
-      resource("zh-en-hiero-pack").stage do
-        (libexec/"zh-en-hiero-pack-2016-01").install Dir["*"]
-      end
     end
     libexec.install Dir["*"]
     bin.install_symlink Dir["#{libexec}/bin/*"]
