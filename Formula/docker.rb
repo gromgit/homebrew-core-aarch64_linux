3f2e333ca08c6db2d58d27b6c50a806b884b749b
ilovezfs <ilovezfs@icloud.com>
2018-05-09 21:51:24 -0700

    docker 18.05.0
    Closes #27677.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -2,8 +2,8 @@ class Docker < Formula
   desc "Pack, ship and run any application as a lightweight container"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/docker-ce.git",
-      :tag => "v18.04.0-ce",
-      :revision => "3d479c0af67cb9ea43a9cfc1bf2ef097e06a3470"
+      :tag => "v18.05.0-ce",
+      :revision => "f150324782643a5268a04e7d1a675587125da20e"
 
   bottle do
     cellar :any_skip_relocation
