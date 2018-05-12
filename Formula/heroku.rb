ce4a66ea5c40abb978d5533d600673deef9edce4
ilovezfs <ilovezfs@icloud.com>
2018-05-11 21:06:52 -0700

    heroku 7.0.40
    Closes #27775.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -4,8 +4,8 @@ class Heroku < Formula
   desc "Command-line client for the cloud PaaS"
   homepage "https://cli.heroku.com"
   # heroku should only be updated every 10 releases on multiples of 10
-  url "https://registry.npmjs.org/heroku/-/heroku-7.0.30.tgz"
-  sha256 "a16bdc2e825ec41b6a3da57e9dc483ce90e37564173ae2c0d0ef4e622659bb7e"
+  url "https://registry.npmjs.org/heroku/-/heroku-7.0.40.tgz"
+  sha256 "a3ea3dec224e1e9dce60f286d2effe6ab01ac2df146b60215b07617368b2300a"
   head "https://github.com/heroku/cli.git"
 
   bottle do
