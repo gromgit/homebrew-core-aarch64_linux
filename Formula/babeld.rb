ac389518cd2dde284c3fac11b387e8e750399207
ilovezfs <ilovezfs@icloud.com>
2018-05-12 19:45:24 -0700

    babeld 1.8.2
    Closes #27797.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/babeld.rb
+++ Formula/babeld.rb
@@ -1,8 +1,8 @@
 class Babeld < Formula
   desc "Loop-avoiding distance-vector routing protocol"
   homepage "https://www.irif.univ-paris-diderot.fr/~jch/software/babel/"
-  url "https://www.irif.univ-paris-diderot.fr/~jch/software/files/babeld-1.8.1.tar.gz"
-  sha256 "9c249c73f5292ec18d1dd70934195edf2cbfa51f0a0fca937e16104f7fc006bf"
+  url "https://www.irif.univ-paris-diderot.fr/~jch/software/files/babeld-1.8.2.tar.gz"
+  sha256 "07edecb132386d5561a767482bc5200e04239b18e48c2f0f47ae1c78d60fe5dc"
   head "https://github.com/jech/babeld.git"
 
   bottle do
