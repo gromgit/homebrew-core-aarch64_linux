5a4690ae23695ea6454fd32cab414ec209a4d52e
ilovezfs <ilovezfs@icloud.com>
2018-05-12 19:48:57 -0700

    xsv 0.13.0
    Closes #27801.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/xsv.rb
+++ Formula/xsv.rb
@@ -1,8 +1,8 @@
 class Xsv < Formula
   desc "Fast CSV toolkit written in Rust"
   homepage "https://github.com/BurntSushi/xsv"
-  url "https://github.com/BurntSushi/xsv/archive/0.12.2.tar.gz"
-  sha256 "484e3d4a9fec0d4c8089a77cba3e122970113e2bf0277ab6a956bf12954bbca3"
+  url "https://github.com/BurntSushi/xsv/archive/0.13.0.tar.gz"
+  sha256 "2b75309b764c9f2f3fdc1dd31eeea5a74498f7da21ae757b3ffd6fd537ec5345"
   head "https://github.com/BurntSushi/xsv.git"
 
   bottle do
