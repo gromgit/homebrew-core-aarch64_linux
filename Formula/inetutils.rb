b053c188f256ca5b043c20a0b407d86b52a00874
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-13 06:52:08 +0000

    inetutils: update 1.9.4_1 bottle.

--- Formula/inetutils.rb
+++ Formula/inetutils.rb
@@ -7,11 +7,9 @@ class Inetutils < Formula
   revision 1
 
   bottle do
-    rebuild 1
-    sha256 "f57b7d080b62fc569e9f1085a2edda0f6c5bdf0ff93d76ee9df009983ae14c99" => :high_sierra
-    sha256 "e138ac43402b4a19991ad5fcbcdb7e1e5e2a4e13f71b4423db5102aef9da0db0" => :sierra
-    sha256 "5fdac142afc7e9a376534e825d90c91b89e7fcae4f9ccebaadf568017ee6de78" => :el_capitan
-    sha256 "1c9d32aa9b97b6119cad466b9a5efeef6bf74f54dbeccd096860e1500658d866" => :yosemite
+    sha256 "83a83d549780840164b9b3da28d3aef47e41378415d6cdb6c6aee48d04623340" => :high_sierra
+    sha256 "538f028274c5afbc0d4eb397b142f724d48c68a85acc6e1c5c30e00e652d5afb" => :sierra
+    sha256 "08419e32bd90cdc6c6b4715e64b2facae634a3cd45ecc7e54da87cab7b112458" => :el_capitan
   end
 
   option "with-default-names", "Do not prepend 'g' to the binary"
