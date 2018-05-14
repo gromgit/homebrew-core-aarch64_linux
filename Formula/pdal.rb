a23d8ad8850ed26f65e595106b47fa4e2cba3a17
ilovezfs <ilovezfs@icloud.com>
2018-05-13 17:06:04 -0700

    pdal 1.7.2
    Closes #27815.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pdal.rb
+++ Formula/pdal.rb
@@ -1,9 +1,8 @@
 class Pdal < Formula
   desc "Point data abstraction library"
   homepage "https://www.pdal.io/"
-  url "https://github.com/PDAL/PDAL/archive/1.7.1.tar.gz"
-  sha256 "a560d3962b5ffdf58876155b33f816f24dbaf9a313ae308d9bf63adb8edac951"
-  revision 2
+  url "https://github.com/PDAL/PDAL/archive/1.7.2.tar.gz"
+  sha256 "cedfefbe54ca61cbb33d100d619c53873d84f480ff53deec2cf6dd91580f6a61"
   head "https://github.com/PDAL/PDAL.git"
 
   bottle do
