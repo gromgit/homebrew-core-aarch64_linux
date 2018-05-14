05ff4980d7e69cad69f2a560ff285edefd815e52
Steve Huff <steve.huff@runkeeper.com>
2018-05-14 17:21:31 -0400

    safe 0.9.3 (new formula)
    safe 0.9.3
    
    Closes #27905.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ Formula/safe.rb
@@ -0,0 +1,34 @@
+class Safe < Formula
+  desc "Command-line interface for Vault"
+  homepage "https://github.com/starkandwayne/safe"
+  url "https://github.com/starkandwayne/safe/archive/v0.9.3.tar.gz"
+  sha256 "781a30d4bbb39881f510baf4b264013e883d6de9bc4eb205e9f361a1358b9c99"
+
+  depends_on "go" => :build
+  depends_on "vault"
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["VERSION"] = version
+
+    (buildpath/"src/github.com/starkandwayne/safe").install buildpath.children
+
+    cd "src/github.com/starkandwayne/safe" do
+      system "make", "build"
+      bin.install "safe"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    require "yaml"
+
+    pid = fork { exec "#{bin}/safe", "local", "--memory" }
+    sleep 1
+    handshake_yaml = Utils.popen_read("#{bin}/safe", "get", "secret/handshake")
+    Process.kill("TERM", pid)
+
+    parsed = YAML.safe_load(handshake_yaml)
+    assert_equal "knock", parsed["knock"]
+  end
+end
