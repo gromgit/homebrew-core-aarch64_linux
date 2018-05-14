2e4692137ae15b1a519deb5ad36815573b11d5ae
ilovezfs <ilovezfs@icloud.com>
2018-05-13 22:28:00 -0700

    wtf 20180513 (#27823)

--- Formula/wtf.rb
+++ Formula/wtf.rb
@@ -1,8 +1,8 @@
 class Wtf < Formula
   desc "Translate common Internet acronyms"
   homepage "http://cvsweb.netbsd.org/bsdweb.cgi/src/games/wtf/"
-  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20180510.tar.gz"
-  sha256 "1cbdd41fb75a55a915ea8691ab7710730643d544baafa0302d542d2464fb9def"
+  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20180513.tar.gz"
+  sha256 "26fa6c3d28507eb0046e7786076ea7c658a17e7f57d9f9bed25ff1f628e38602"
 
   bottle :unneeded
 
