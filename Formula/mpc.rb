22482d1281c0c29b7f99e37d4ad870317fb38f43
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-15 14:57:06 +0000

    mpc: update 0.30 bottle.

--- Formula/mpc.rb
+++ Formula/mpc.rb
@@ -5,9 +5,9 @@ class Mpc < Formula
   sha256 "65fc5b0a8430efe9acbe6e261127960682764b20ab994676371bdc797d867fce"
 
   bottle do
-    sha256 "baffb4e58d39af2f18d3def335cb231826e1a94fafa4f31367e7372dbbfe769f" => :high_sierra
-    sha256 "400ac144b7f8ca93a2cf2489e0b25031a0086b95e283c6537b037acf008e80cf" => :sierra
-    sha256 "0ed8bc8a4a93e8df393e23d19229719ff057ebd3a6e27ff42324451753c399d0" => :el_capitan
+    sha256 "1d83b43f782bfdf0094c3f756e187ad09582419f51856ee2b3f20a22db083304" => :high_sierra
+    sha256 "f8a0570ac6328165c03d502797d5d286787a1ff03ba16ede32ff166063000254" => :sierra
+    sha256 "46b3077b453c93b13e6dc58cb9e54cb170a8fd774565d0afce98368632e6f9ae" => :el_capitan
   end
 
   depends_on "meson" => :build
