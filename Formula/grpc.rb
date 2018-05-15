4000f499ab87835791b19b3aa7b3f1cb2fdbc5ef
ilovezfs <ilovezfs@icloud.com>
2018-05-14 23:53:37 -0700

    grpc 1.11.1
    Closes #27876.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/grpc.rb
+++ Formula/grpc.rb
@@ -1,8 +1,8 @@
 class Grpc < Formula
   desc "Next generation open source RPC library and framework"
   homepage "https://www.grpc.io/"
-  url "https://github.com/grpc/grpc/archive/v1.11.0.tar.gz"
-  sha256 "df9bb06e89d0f904176d4df76f38031711fa369283e5c731da26032526e775f1"
+  url "https://github.com/grpc/grpc/archive/v1.11.1.tar.gz"
+  sha256 "0068a0b11795ac89ba8f5cc7332cd8cd2870a2ee0016126f014ce0bc3004e590"
   head "https://github.com/grpc/grpc.git"
 
   bottle do
