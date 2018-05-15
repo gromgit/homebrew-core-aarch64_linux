902d90c76d778d7a49551e508c1a7ecddffc0b45
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-15 18:07:44 +0000

    wireguard-go: add 0.0.20180514 bottle.

--- Formula/wireguard-go.rb
+++ Formula/wireguard-go.rb
@@ -4,6 +4,13 @@ class WireguardGo < Formula
   url "https://git.zx2c4.com/wireguard-go/snapshot/wireguard-go-0.0.20180514.tar.xz"
   sha256 "5a2a0ac5a3c64a9d0c1811d349c6f3f5deb55c15f00f4c13054b897a8c4ac4ae"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0be24e1d3c45d5b81dbee640cbbff1c9a2fbc8985222efdd1b51dc958b81f6e7" => :high_sierra
+    sha256 "b4b281254c748ca5ee189ad0eb982dc60cd1dc482241db4f7ec5794d35f5336f" => :sierra
+    sha256 "6d981218bce9a716e97ba76c50b5cc3e85b189d21fea29d6e4d9c33be66654c9" => :el_capitan
+  end
+
   depends_on "dep" => :build
   depends_on "go" => :build
 
