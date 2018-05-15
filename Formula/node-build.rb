3a07423bf0ac7cc33fdea7b43f55db8fec7309ab
Jason Karns <jason.karns@gmail.com>
2018-05-15 11:36:11 -0400

    node-build 3.0.10 (#27894)

--- Formula/node-build.rb
+++ Formula/node-build.rb
@@ -1,8 +1,8 @@
 class NodeBuild < Formula
   desc "Install NodeJS versions"
   homepage "https://github.com/nodenv/node-build"
-  url "https://github.com/nodenv/node-build/archive/v3.0.9.tar.gz"
-  sha256 "d087f77fc18b38af9a064a4a5e1a43306f3782069b692ccb43ac509e18923fe3"
+  url "https://github.com/nodenv/node-build/archive/v3.0.10.tar.gz"
+  sha256 "9f6b52431aa97c5d85252a19fca173fb4baf993dfa28603f740dd5f97d6b717e"
   head "https://github.com/nodenv/node-build.git"
 
   bottle :unneeded
