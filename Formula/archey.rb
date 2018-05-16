4255640a902a8b22b2f7b477f923d170c8f76bf3
Camden Narzt <camden.narzt@hotmail.com>
2018-05-16 09:51:23 -0600

    archey: fix double percent sign for battery (#27638)

--- Formula/archey.rb
+++ Formula/archey.rb
@@ -3,9 +3,16 @@ class Archey < Formula
   homepage "https://obihann.github.io/archey-osx/"
   url "https://github.com/obihann/archey-osx/archive/1.6.0.tar.gz"
   sha256 "0f0ffcf8c5f07610b98f0351dcb38bb8419001f40906d5dc4bfd28ef12dbd0f8"
+  revision 1
   head "https://github.com/obihann/archey-osx.git"
 
   bottle :unneeded
+
+  # Fix double percent sign in battery output, remove in next release
+  patch do
+    url "https://github.com/obihann/archey-osx/commit/cd125547d0936f066b64616553269bf647348e53.diff?full_index=1"
+    sha256 "c03b80e4d5aa114b81ac04bfa77c46055fe01764ae877a8a061f3d43c9de8a72"
+  end
 
   def install
     bin.install "bin/archey"
