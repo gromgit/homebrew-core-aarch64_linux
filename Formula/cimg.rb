3e6826134cb2efc6c6366aa81de9d750dfe0754a
ilovezfs <ilovezfs@icloud.com>
2018-05-16 10:02:46 -0700

    cimg 2.2.3 (#27939)

--- Formula/cimg.rb
+++ Formula/cimg.rb
@@ -1,8 +1,8 @@
 class Cimg < Formula
   desc "C++ toolkit for image processing"
   homepage "http://cimg.eu/"
-  url "http://cimg.eu/files/CImg_2.2.2.zip"
-  sha256 "41fc0968bde3882824bfc2546b41b33fee22cbcca6d22917012c560dc3ad2a5d"
+  url "http://cimg.eu/files/CImg_2.2.3.zip"
+  sha256 "d63c3e8a5941df592f490f6973da00a7a9cb3ee394d20b4dc604fb65aa1ddd1c"
 
   bottle :unneeded
 
