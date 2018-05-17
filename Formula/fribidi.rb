f74979e2c759ce308e6625209863df5c7439a455
JCount <JCount42@gmail.com>
2018-05-16 18:50:13 -0700

    fribidi 1.0.3
    Closes #27951.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/fribidi.rb
+++ Formula/fribidi.rb
@@ -1,22 +1,14 @@
 class Fribidi < Formula
   desc "Implementation of the Unicode BiDi algorithm"
   homepage "https://github.com/fribidi/fribidi"
-  url "https://github.com/fribidi/fribidi/releases/download/v1.0.2/fribidi-1.0.2.tar.bz2"
-  sha256 "bd6d1b530c4f6066f42461200ed6a31f2db8db208570ea4ccaab2b935e88832b"
+  url "https://github.com/fribidi/fribidi/releases/download/v1.0.3/fribidi-1.0.3.tar.bz2"
+  sha256 "8d214b17b6eedcb416e53142c196c2896b9a685fca2a5bddc098a73d2b02ce12"
 
   bottle do
     cellar :any
     sha256 "139434ded4f6455fef216b33fb5cdc713cc1692f457ae56058ff512a5d64abc0" => :high_sierra
     sha256 "0588e9731bf34db77b4bfc4bea72d8d84196bebc9badb2edce6d8c37f02159de" => :sierra
     sha256 "ad4d83225cc078de5a60c3d5d76224761a712c6c521f548e9913ceea0b62c051" => :el_capitan
-  end
-
-  # Remove for > 1.0.2
-  # Upstream commit from 19 Mar 2018 "Revert 'Add an option to disable building
-  # documentation'"; see https://github.com/fribidi/fribidi/commit/095d885
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/f9c1d7d/fribidi/fix-docs.diff"
-    sha256 "990777213ff47cfbf06f0342f66e84783bf5eec80419ff1582dd189352ef5f73"
   end
 
   def install
