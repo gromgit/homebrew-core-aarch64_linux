913755a5ecb411fcbb6377b860a024635fb8e670
ilovezfs <ilovezfs@icloud.com>
2018-05-18 03:18:47 -0700

    ammonite-repl 1.1.2 (#27995)
    allow newer Java

--- Formula/ammonite-repl.rb
+++ Formula/ammonite-repl.rb
@@ -1,19 +1,17 @@
 class AmmoniteRepl < Formula
   desc "Ammonite is a cleanroom re-implementation of the Scala REPL"
   homepage "https://lihaoyi.github.io/Ammonite/#Ammonite-REPL"
-  url "https://github.com/lihaoyi/Ammonite/releases/download/1.1.0/2.12-1.1.0", :using => :nounzip
-  sha256 "d448fb8b846cddb0315e5cda03036a73427f2c7335f76faed75f61eb5ce0a537"
+  url "https://github.com/lihaoyi/Ammonite/releases/download/1.1.2/2.12-1.1.2", :using => :nounzip
+  sha256 "2cbbec93b8ace067f9fcaa30615b1d815b3a094b1c2c2ec5c09ef6a3fcc954ad"
 
   bottle :unneeded
 
-  # Upstream issue from 2 Aug 2017 "amm throws NPE on OpenJDK 9"
-  # See https://github.com/lihaoyi/Ammonite/issues/675
-  depends_on :java => "1.8"
+  depends_on :java => "1.8+"
 
   def install
     libexec.install Dir["*"].shift => "amm"
     chmod 0555, libexec/"amm"
-    (bin/"amm").write_env_script libexec/"amm", Language::Java.java_home_env("1.8")
+    bin.install_symlink libexec/"amm"
   end
 
   test do
