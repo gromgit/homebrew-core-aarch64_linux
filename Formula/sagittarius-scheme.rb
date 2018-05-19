ac0d2d7656477f0b3313076d2a59df6faf982724
ilovezfs <ilovezfs@icloud.com>
2018-05-19 16:44:48 -0700

    sagittarius-scheme 0.9.2
    Closes #28047.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/sagittarius-scheme.rb
+++ Formula/sagittarius-scheme.rb
@@ -1,8 +1,8 @@
 class SagittariusScheme < Formula
   desc "Free Scheme implementation supporting R6RS and R7RS"
   homepage "https://bitbucket.org/ktakashi/sagittarius-scheme/wiki/Home"
-  url "https://bitbucket.org/ktakashi/sagittarius-scheme/downloads/sagittarius-0.9.1.tar.gz"
-  sha256 "0f3db7eb9395dc823c832516ca1606ec581bfe2affe1bd952580e915fa9290a9"
+  url "https://bitbucket.org/ktakashi/sagittarius-scheme/downloads/sagittarius-0.9.2.tar.gz"
+  sha256 "d20cb780bdcadc12243ef971b0f6a7e92b4d104aeca9fef1447394ba764f8ea7"
 
   bottle do
     cellar :any
