3bbcbb3fc76ca30e0dcd62b1776f635b8d03d3cc
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-21 06:00:22 +0000

    osm2pgrouting: update 2.3.5 bottle.

--- Formula/osm2pgrouting.rb
+++ Formula/osm2pgrouting.rb
@@ -7,10 +7,9 @@ class Osm2pgrouting < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "cd220066b7f205b7b56627315bdfef56e67aaffc4f58ddf827882d99c2b21a8f" => :high_sierra
-    sha256 "62423bb28c431eac8f92406adaf71558eb80e7c52d1adf4511ff8b26c4472548" => :sierra
-    sha256 "05681f94a2e98ff7d3d06d6795cc708bf59eb33f7ca73bda186118754bfbccc0" => :el_capitan
+    sha256 "570f091f6fa641eaef5a1339d935c94e04aae1715e740ba29556f648b97dfba1" => :high_sierra
+    sha256 "5c9da0dbfe4592eee781221042abe4809b684fe06835732dd30c5e6b021cefa6" => :sierra
+    sha256 "f6d7fd453858b29b4299dc8860c68978210c8504a8108b9d3e5ad3e3bd9ea5cb" => :el_capitan
   end
 
   depends_on "cmake" => :build
