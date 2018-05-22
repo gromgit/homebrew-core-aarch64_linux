f1a72a6ba8c768e32e029fbfdf5210b0d78810d3
ilovezfs <ilovezfs@icloud.com>
2018-05-21 22:04:36 -0700

    pumba 0.5.0
    Closes #28108.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pumba.rb
+++ Formula/pumba.rb
@@ -1,8 +1,8 @@
 class Pumba < Formula
   desc "Chaos testing tool for Docker"
   homepage "https://github.com/alexei-led/pumba"
-  url "https://github.com/alexei-led/pumba/archive/0.4.8.tar.gz"
-  sha256 "a9688201e07299f0d9973a6b1570c99ac3e10312361718c0ba72c80801d95fe8"
+  url "https://github.com/alexei-led/pumba/archive/0.5.0.tar.gz"
+  sha256 "a748b9c676dcb5504322e653998f3c2faaba4fe27183314a2142ff6421006c40"
   head "https://github.com/alexei-led/pumba.git"
 
   bottle do
@@ -21,7 +21,7 @@ class Pumba < Formula
 
     cd "src/github.com/alexei-led/pumba" do
       system "go", "build", "-o", bin/"pumba", "-ldflags",
-             "-X main.Version=#{version}"
+             "-X main.Version=#{version}", "./cmd"
       prefix.install_metafiles
     end
   end
