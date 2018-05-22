f9c03e2c997ad169093335ffc647b4eff9932ace
ilovezfs <ilovezfs@icloud.com>
2018-05-21 22:18:01 -0700

    mill 0.2.2 (#28063)

--- Formula/mill.rb
+++ Formula/mill.rb
@@ -1,8 +1,8 @@
 class Mill < Formula
   desc "Scala build tool"
   homepage "https://www.lihaoyi.com/mill/"
-  url "https://github.com/lihaoyi/mill/releases/download/0.2.0/0.2.0", :using => :nounzip
-  sha256 "7a033b87e8132eeea040d61c114bf192215a21656b1b3ea563f019d1350ebe7f"
+  url "https://github.com/lihaoyi/mill/releases/download/0.2.2/0.2.2", :using => :nounzip
+  sha256 "114f6efd3efb5dbe078a3205b59c476ebc8a926b9f9c66a1a9bc6dd402d3da95"
 
   bottle :unneeded
 
