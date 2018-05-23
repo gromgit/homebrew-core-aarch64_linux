1be4a240c693144218a135fb0f5d6d987abb9c87
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2018-05-23 05:36:41 -0700

    speex: add test (#28182)

--- Formula/speex.rb
+++ Formula/speex.rb
@@ -29,4 +29,26 @@ class Speex < Formula
     system "./configure", *args
     system "make", "install"
   end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <speex/speex.h>
+
+      int main()
+      {
+          SpeexBits bits;
+          void *enc_state;
+
+          speex_bits_init(&bits);
+          enc_state = speex_encoder_init(&speex_nb_mode);
+
+          speex_bits_destroy(&bits);
+          speex_encoder_destroy(enc_state);
+
+          return 0;
+      }
+    EOS
+    system ENV.cc, "-L#{lib}", "-lspeex", "test.c", "-o", "test"
+    system "./test"
+  end
 end
