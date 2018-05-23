7ff79a05533166c6e6fceb17cfcbca4eea3743c7
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-23 17:23:09 +0000

    soundpipe: add 1.7.0 bottle.

--- Formula/soundpipe.rb
+++ Formula/soundpipe.rb
@@ -4,6 +4,13 @@ class Soundpipe < Formula
   url "https://github.com/PaulBatchelor/soundpipe/archive/v1.7.0.tar.gz"
   sha256 "2d6f6b155ad93d12f59ae30e2b0f95dceed27e0723147991da6defc6d65eadda"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4f0b6f6a15345ce24137c640ab6c7a3a6a8c27d2391db3349672008feaaf67e3" => :high_sierra
+    sha256 "2d4accbb857729269d28c0812f08bb329258622f2ef584e37f48c5a846001804" => :sierra
+    sha256 "134c366d9bc06559fcfd2fb3a9aadf779bd974da23884b3b87eb9edac9dd22c9" => :el_capitan
+  end
+
   depends_on "libsndfile"
 
   def install
