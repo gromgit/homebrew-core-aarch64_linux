829299816eb8509b9101537a8f3b1a3a1eb7a0c4
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-24 09:02:20 +0000

    density: add 0.14.2 bottle.

--- Formula/density.rb
+++ Formula/density.rb
@@ -4,6 +4,13 @@ class Density < Formula
   url "https://github.com/centaurean/density/archive/density-0.14.2.tar.gz"
   sha256 "0b130ea9da3ddaad78810a621a758b47c4135d91d5b5fd22d60bbaf04bc147da"
 
+  bottle do
+    cellar :any
+    sha256 "798b64ce99c516e735c76aea05944869124634488c82af5ba3f2930f60d83de3" => :high_sierra
+    sha256 "7740a6a2ba8aa701025c01553c57051e72b49e2f8f0774f064ca98832e1e58e9" => :sierra
+    sha256 "6853e909ed8057817becb03cbb5dbfdd3d4e2ea35348bf562e86dc51aed11a78" => :el_capitan
+  end
+
   resource "cputime" do
     url "https://github.com/centaurean/cputime.git",
         :revision => "d435d91b872a4824fb507a02d0d1814ed3e791b0"
