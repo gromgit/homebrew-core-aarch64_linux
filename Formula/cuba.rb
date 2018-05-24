291259f67a7a844bb566f6efca84261263975edb
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-24 10:10:06 +0000

    cuba: add 4.2 bottle.

--- Formula/cuba.rb
+++ Formula/cuba.rb
@@ -4,6 +4,13 @@ class Cuba < Formula
   url "http://www.feynarts.de/cuba/Cuba-4.2.tar.gz"
   sha256 "6b75bb8146ae6fb7da8ebb72ab7502ecd73920efc3ff77a69a656db9530a5eef"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a607f8cbcfa954ac20a407fce07dc6cc65bfaab6b06079cc5d1eb85400532e74" => :high_sierra
+    sha256 "b3317c5c1d2f902d60aaa175f3e35d906f2ac6ab179b87de09e0f9110125b4ff" => :sierra
+    sha256 "6f1bf18403892cea4d5d692256e31dd11d170ecadaf996f960c454084e51b243" => :el_capitan
+  end
+
   def install
     ENV.deparallelize # Makefile does not support parallel build
     system "./configure", "--prefix=#{prefix}"
