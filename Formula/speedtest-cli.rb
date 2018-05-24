84c2f9b6c73f4243ffa2a942d0ebb0dee86563a5
epistrephein <epistrephein@gmail.com>
2018-05-24 18:43:42 +0200

    speedtest-cli 2.0.2
    Closes #28242.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/speedtest-cli.rb
+++ Formula/speedtest-cli.rb
@@ -1,8 +1,8 @@
 class SpeedtestCli < Formula
   desc "Command-line interface for https://speedtest.net bandwidth tests"
   homepage "https://github.com/sivel/speedtest-cli"
-  url "https://github.com/sivel/speedtest-cli/archive/v2.0.1.tar.gz"
-  sha256 "ad07c93a92f5a08823600df19bc364fe96819a5b7b1d20168a68815f42f9920a"
+  url "https://github.com/sivel/speedtest-cli/archive/v2.0.2.tar.gz"
+  sha256 "5e22f2dcce1c9020f33faf559b22727483f022008a2912b85d573e578374b6a0"
   head "https://github.com/sivel/speedtest-cli.git"
 
   bottle :unneeded
