bbb45c3277b7ed1aef04dad3b3209038fa80ce8a
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-05-23 20:35:31 -0400

    bitwarden 1.0.0 (new formula)
    Closes #28201.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/bitwarden-cli.rb
@@ -0,0 +1,22 @@
+require "language/node"
+
+class BitwardenCli < Formula
+  desc "Secure and free password manager for all of your devices"
+  homepage "https://bitwarden.com/"
+  url "https://registry.npmjs.org/@bitwarden/cli/-/cli-1.0.0.tgz"
+  sha256 "75475a7eb9c728b0b16c1a69d397391019617cfbf73304bcc8724d9fd32aec47"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_equal 10, shell_output("#{bin}/bw generate --length 10").chomp.length
+
+    output = pipe_output("#{bin}/bw encode", "Testing", 0)
+    assert_equal "VGVzdGluZw==", output.chomp
+  end
+end
