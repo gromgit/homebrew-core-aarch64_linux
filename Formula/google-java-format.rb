5e5aa509dca67969ac54bd3aae936ad18eb8e35f
ilovezfs <ilovezfs@icloud.com>
2018-05-24 21:30:07 -0700

    google-java-format 1.6
    Closes #28257.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/google-java-format.rb
+++ Formula/google-java-format.rb
@@ -1,8 +1,8 @@
 class GoogleJavaFormat < Formula
   desc "Reformats Java source code to comply with Google Java Style"
   homepage "https://github.com/google/google-java-format"
-  url "https://github.com/google/google-java-format/archive/google-java-format-1.5.tar.gz"
-  sha256 "836086fb081086abf0286d2d70acdfe4fb8bccd12f78d7967bdf440e9bf71fea"
+  url "https://github.com/google/google-java-format/archive/google-java-format-1.6.tar.gz"
+  sha256 "d471a84c49ef33e1ab5059f7a04f5e1ac127ccf05db1c2d69d4c1733a256a15f"
 
   bottle do
     cellar :any_skip_relocation
