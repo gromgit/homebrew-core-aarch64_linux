b5bc9646d3d42467115bfb7a8bf1b4992d1bf637
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-26 05:00:07 +0000

    p11-kit: update 0.23.11 bottle.

--- Formula/p11-kit.rb
+++ Formula/p11-kit.rb
@@ -5,9 +5,9 @@ class P11Kit < Formula
   sha256 "b243c8daa573f85cb9873352a4c38563812fe3045e960593eb45db7dfdea4a2b"
 
   bottle do
-    sha256 "d1a28a27c565eb0b14687b64f4bb3a643e1289bd3e59e8f82b2e73bac021eea2" => :high_sierra
-    sha256 "d32f3d5d34f52c274a749d62bcaf3ce1814cc133fad89d09cec74f2227c43702" => :sierra
-    sha256 "82e3ee6e9dcafad244f0a0509500afbc714d35beba303fda57bbda00e2cf7fe9" => :el_capitan
+    sha256 "3fa3ce13ade57872341dd0b5881b3ed8c85e3a6f8c702310e296d3681b5a2747" => :high_sierra
+    sha256 "8ac0b7ddec553fcb2df9aae1f94f4905d34af0409c808e01f24f66b9474602a0" => :sierra
+    sha256 "003c89e65eb0a3e5e99b1eaf576f2455661623da2993c97a9b3fa37286575b3c" => :el_capitan
   end
 
   head do
