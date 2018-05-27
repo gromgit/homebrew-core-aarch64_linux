72bb3b061adefed6d58bfaf6d2d29344ea9cd945
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-27 18:55:46 +0000

    cglm: update 0.4.6 bottle.

--- Formula/cglm.rb
+++ Formula/cglm.rb
@@ -6,9 +6,9 @@ class Cglm < Formula
 
   bottle do
     cellar :any
-    sha256 "ff26619caecd2eacae903fc277490a08f21fae3343c53d87b9b616e7fbab3a42" => :high_sierra
-    sha256 "f735d214e24036ef585d636bdd74e78b7112a52339c61175c48876ed52d0221e" => :sierra
-    sha256 "d5d40f046cc551cfeffe575582d8212d113035d2cd37ae8112ae38d5fd8e9ad8" => :el_capitan
+    sha256 "c6f11d17fe50104c2fbfd3b3bfb2a16715e84e37c7ee8ec111e8a5cdd9ee2d56" => :high_sierra
+    sha256 "6fea3faa0f25d949a0d1f19674dfd10523801575e7068211e0c9114bdafd1621" => :sierra
+    sha256 "23d232f82e710a7c882c9be0f3f8d34b3a4949ebc238e7c4df46585a44cb42c0" => :el_capitan
   end
 
   depends_on "autoconf" => :build
