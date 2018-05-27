a8354a9e5299f200970ef1f4be772d89222136e8
Katja Durrani <mail@kdurrani.co.uk>
2018-05-27 11:17:58 +0100

    taskell 1.2.5 (new formula)

---
+++ Formula/taskell.rb
@@ -0,0 +1,38 @@
+require "language/haskell"
+
+class Taskell < Formula
+  include Language::Haskell::Cabal
+
+  desc "Command-line Kanban board/task manager with support for Trello"
+  homepage "https://taskell.app"
+  url "https://github.com/smallhadroncollider/taskell/archive/1.2.6.tar.gz"
+  sha256 "ffd2028ff18b08dba2a890b3e7d76d20a1f78698416d5061274ad062140a5731"
+
+  depends_on "cabal-install" => :build
+  depends_on "ghc" => :build
+
+  def install
+    cabal_sandbox do
+      cabal_install "hpack"
+      system "./.cabal-sandbox/bin/hpack"
+      install_cabal_package
+    end
+  end
+
+  test do
+    (testpath/"test.md").write <<~EOS
+      ## To Do
+
+      - A thing
+      - Another thing
+    EOS
+
+    expected = <<~EOS
+      test.md
+      Lists: 1
+      Tasks: 2
+    EOS
+
+    assert_match expected, shell_output("#{bin}/taskell -i test.md")
+  end
+end
