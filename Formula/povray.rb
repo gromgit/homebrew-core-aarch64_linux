96f52faf68a0b69cf91d59534a5dc1ec010c1075
ilovezfs <ilovezfs@icloud.com>
2018-05-27 05:37:54 -0700

    povray 3.7.0.8
    Closes #28337.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/povray.rb
+++ Formula/povray.rb
@@ -1,9 +1,8 @@
 class Povray < Formula
   desc "Persistence Of Vision RAYtracer (POVRAY)"
   homepage "http://www.povray.org/"
-  url "https://github.com/POV-Ray/povray/archive/v3.7.0.7.tar.gz"
-  sha256 "085746e891edbb2cfda22bb2a8b86043bd680a68ad9121bc568118c730ace7b9"
-  revision 1
+  url "https://github.com/POV-Ray/povray/archive/v3.7.0.8.tar.gz"
+  sha256 "53d11ebd2972fc452af168a00eb83aefb61387662c10784e81b63e44aa575de4"
 
   bottle do
     sha256 "d47c19e8816adf48fdcb70027a7c2d69cb33ca05abec36cdb7fb1b38fcbc20ef" => :high_sierra
