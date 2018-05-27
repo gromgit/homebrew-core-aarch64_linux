d427732bb8cf3a3b9dcf26756cf52f191f3410d2
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-27 19:55:46 +0000

    mk-configure: add 0.30.0 bottle.

--- Formula/mk-configure.rb
+++ Formula/mk-configure.rb
@@ -4,6 +4,13 @@ class MkConfigure < Formula
   url "https://downloads.sourceforge.net/project/mk-configure/mk-configure/mk-configure-0.30.0/mk-configure-0.30.0.tar.gz"
   sha256 "5c8f953d82a2d36f9492cafcad61d34e81b892d1d91b401af406b086046ef227"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1fbce32c61d2840795d80acbb69dba077a17190307e001e7297f34ba279bf2b4" => :high_sierra
+    sha256 "1fbce32c61d2840795d80acbb69dba077a17190307e001e7297f34ba279bf2b4" => :sierra
+    sha256 "1fbce32c61d2840795d80acbb69dba077a17190307e001e7297f34ba279bf2b4" => :el_capitan
+  end
+
   depends_on "bmake"
   depends_on "makedepend"
 
