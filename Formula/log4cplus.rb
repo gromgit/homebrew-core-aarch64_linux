99daf1a895157f57ce2a54cecb13d0d8011572b4
ilovezfs <ilovezfs@icloud.com>
2018-05-26 23:25:39 -0700

    log4cplus 2.0.1
    Closes #28330.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/log4cplus.rb
+++ Formula/log4cplus.rb
@@ -1,8 +1,8 @@
 class Log4cplus < Formula
   desc "Logging Framework for C++"
   homepage "https://sourceforge.net/p/log4cplus/wiki/Home/"
-  url "https://downloads.sourceforge.net/project/log4cplus/log4cplus-stable/2.0.0/log4cplus-2.0.0.tar.xz"
-  sha256 "8c85e769c3dbec382ed4db91f15e5bc24ba979f810262723781f2fc596339bf4"
+  url "https://downloads.sourceforge.net/project/log4cplus/log4cplus-stable/2.0.1/log4cplus-2.0.1.tar.xz"
+  sha256 "813580059fd91260376e411b3e09c740aa86dedc5f6a0bd975b9b39d5c4370e6"
 
   bottle do
     cellar :any
