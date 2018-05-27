f748dfdb0ab7dd8f7385efe1b3ff9ddf95ece936
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-27 23:02:18 +0000

    hbase: update 1.2.6_1 bottle.

--- Formula/hbase.rb
+++ Formula/hbase.rb
@@ -6,10 +6,9 @@ class Hbase < Formula
   revision 1
 
   bottle do
-    sha256 "d24c34f859029e79977587f0926ce6fbe14336a044311d4d97fddacf86e2d42e" => :high_sierra
-    sha256 "bb48048be73c3782d7d56725228998a0ee62673ba9d628ae9869e7e855109f5b" => :sierra
-    sha256 "546eba2acf3f87d88dc9208d687dfa02c88fcada2f3ede27d732b9415187b0f8" => :el_capitan
-    sha256 "49ca44b35f6d9239eb6f02ea504e2a83f78552faa85e30dd1b8f6b4664875494" => :yosemite
+    sha256 "7636268e52eb3c665b622d0082208d9b0a7a6996aa3f4013a0a175b523b0c6cc" => :high_sierra
+    sha256 "dc587058171735614818c876498ac034761f9a9cc5995399036b33b7563ed3cf" => :sierra
+    sha256 "acb79c92c8aee4a2daa8ddfbe4da6c6b47b622c714cc432bf1f70be58f5939cd" => :el_capitan
   end
 
   depends_on :java => "1.8"
