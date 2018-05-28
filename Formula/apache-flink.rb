441f2f8f8a15dc09948ddf3b1d8c3adfb8117b91
Till Rohrmann <trohrmann@apache.org>
2018-05-28 19:33:49 +0200

    apache-flink 1.5.0 (#28342)
    require Java 8 specifically
    update test

--- Formula/apache-flink.rb
+++ Formula/apache-flink.rb
@@ -1,25 +1,39 @@
 class ApacheFlink < Formula
   desc "Scalable batch and stream data processing"
   homepage "https://flink.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.3.2/flink-1.3.2-bin-hadoop27-scala_2.11.tgz"
-  version "1.3.2"
-  sha256 "378be2346990264eeff2ae012175523fc94ddbe59bead7e2a76e0d93c0c7c455"
+  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.5.0/flink-1.5.0-bin-hadoop27-scala_2.11.tgz"
+  version "1.5.0"
+  sha256 "9a8232c124c80ea447cbc2c8edcaa0b4850aecaa9d506169c968777956ccd8ff"
   head "https://github.com/apache/flink.git"
 
   bottle :unneeded
 
-  depends_on :java => "1.7+"
+  depends_on :java => "1.8"
 
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install Dir["*"]
-    bin.write_exec_script Dir["#{libexec}/bin/flink"]
+    (libexec/"bin").env_script_all_files(libexec/"libexec", Language::Java.java_home_env("1.8"))
+    chmod 0755, Dir["#{libexec}/bin/*"]
+    bin.write_exec_script "#{libexec}/bin/flink"
   end
 
   test do
+    (testpath/"log").mkpath
+    (testpath/"input").write "foo bar foobar"
+    expected = <<~EOS
+      (foo,1)
+      (bar,1)
+      (foobar,1)
+    EOS
     ENV.prepend "_JAVA_OPTIONS", "-Djava.io.tmpdir=#{testpath}"
-    input = "benv.fromElements(1,2,3).print()\n"
-    output = pipe_output("#{libexec}/bin/start-scala-shell.sh local", input, 1)
-    assert_match "FINISHED", output
+    ENV.prepend "FLINK_LOG_DIR", testpath/"log"
+    system libexec/"bin/start-cluster.sh"
+    system bin/"flink", "run", "-p", "1",
+           libexec/"examples/streaming/WordCount.jar", "--input", "input",
+           "--output", "result"
+    system libexec/"bin/stop-cluster.sh"
+    assert_predicate testpath/"result", :exist?
+    assert_equal expected, (testpath/"result").read
   end
 end
