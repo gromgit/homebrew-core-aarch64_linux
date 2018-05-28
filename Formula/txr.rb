bb28ce3922c93a419895debdfc173dff9d1f22e2
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-28 03:02:56 +0000

    txr: update 197 bottle.

--- Formula/txr.rb
+++ Formula/txr.rb
@@ -7,9 +7,9 @@ class Txr < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "8073b36ff119e61e5d42fe7e0ec359943b468ae4b25c7cb067eb103822c1cfe3" => :high_sierra
-    sha256 "3e773409ee776bee2cc4a0b82a337629bed1061c958894dc6d0006da98908c4e" => :sierra
-    sha256 "7a8a5be2a66b20d8e307fc9f9e9c2d66c7f004d69b7161795c5de13865b4d824" => :el_capitan
+    sha256 "c771779e8d7017dfcfbc3cc40e96d2aa0a703bba18e0c19f52373aaa8bfdba52" => :high_sierra
+    sha256 "133f30759d1d01b0d8c1a93db0d0c1da9af0b0e07bd426d549819b3566517f50" => :sierra
+    sha256 "916f2f1be1a65b004d694717e28a39aec865d72d44d1d6e9c854364b79425e69" => :el_capitan
   end
 
   def install
