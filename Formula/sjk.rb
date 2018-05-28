75d96ff10af919afd711ce13b88661bec6e1b82a
ilovezfs <ilovezfs@icloud.com>
2018-05-27 23:38:36 -0700

    sjk 0.10 (#28357)

--- Formula/sjk.rb
+++ Formula/sjk.rb
@@ -1,8 +1,8 @@
 class Sjk < Formula
   desc "Swiss Java Knife"
   homepage "https://github.com/aragozin/jvm-tools"
-  url "https://search.maven.org/remotecontent?filepath=org/gridkit/jvmtool/sjk-plus/0.9.3/sjk-plus-0.9.3.jar"
-  sha256 "0f7abd4321c0bc418aafb20a6c3097a1d5e4a1d5ea09347c6f8078f4c39ce7ee"
+  url "https://search.maven.org/remotecontent?filepath=org/gridkit/jvmtool/sjk-plus/0.10/sjk-plus-0.10.jar"
+  sha256 "182185f38ed3bb728a9536c438e767606969c698c0e976df907d8238c4f9182b"
 
   bottle :unneeded
 
