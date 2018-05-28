c473cbbb19510535a37587e6e2046afd64524f38
ilovezfs <ilovezfs@icloud.com>
2018-05-28 10:19:41 -0700

    mg: use Bintray as primary url and GitHub for the homepage (#28390)
    until https://devio.us/~bcallah/mg/ comes back online

--- Formula/mg.rb
+++ Formula/mg.rb
@@ -1,7 +1,9 @@
 class Mg < Formula
   desc "Small Emacs-like editor"
-  homepage "https://devio.us/~bcallah/mg/"
-  url "https://devio.us/~bcallah/mg/mg-20180421.tar.gz"
+  # https://devio.us/~bcallah/mg/ is temporarily offline
+  homepage "https://github.com/ibara/mg"
+  # https://devio.us/~bcallah/mg/mg-20180421.tar.gz is temporarily offline
+  url "https://dl.bintray.com/homebrew/mirror/mg-20180421.tar.gz"
   sha256 "11215613a360cf72ff16c2b241ea4e71b4b80b2be32c62a770c1969599e663b2"
 
   bottle do
