3ebff4665dbfa8fd1cf61733708229d77f2bbefb
ilovezfs <ilovezfs@icloud.com>
2018-05-28 01:58:19 -0700

    folly 2018.05.28.00
    Closes #28374.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/folly.rb
+++ Formula/folly.rb
@@ -1,8 +1,8 @@
 class Folly < Formula
   desc "Collection of reusable C++ library artifacts developed at Facebook"
   homepage "https://github.com/facebook/folly"
-  url "https://github.com/facebook/folly/archive/v2018.05.21.00.tar.gz"
-  sha256 "09caad62f909b88a92a5bbbfd881a083b08d7e4416dc1b30306c8be6c5958a0b"
+  url "https://github.com/facebook/folly/archive/v2018.05.28.00.tar.gz"
+  sha256 "e4b7e4274c252a6ddddd819002f4f8f8f84ed15e757f885c7971d4a157f68ba4"
   head "https://github.com/facebook/folly.git"
 
   bottle do
