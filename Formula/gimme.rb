16f31e4fa430ecd18e128bddbac1bc78b6da9718
ilovezfs <ilovezfs@icloud.com>
2018-05-29 23:44:16 -0700

    gimme 1.5.0 (#28445)

--- Formula/gimme.rb
+++ Formula/gimme.rb
@@ -1,8 +1,8 @@
 class Gimme < Formula
   desc "Shell script to install any Go version"
   homepage "https://github.com/travis-ci/gimme"
-  url "https://github.com/travis-ci/gimme/archive/v1.4.0.tar.gz"
-  sha256 "e972303f3287964e3c1626b1c966a69e671de0cd78d82ce0caecd2230ffb090e"
+  url "https://github.com/travis-ci/gimme/archive/v1.5.0.tar.gz"
+  sha256 "508f0c67a48cf1901c5a5a130918068076696a21638035b80c694eb8ff42f5ec"
 
   bottle :unneeded
 
