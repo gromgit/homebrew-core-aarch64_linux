8e8e9b83204f4bf150bc39351ed5c07434f186c2
ilovezfs <ilovezfs@icloud.com>
2018-05-31 14:46:07 -0700

    teleport 2.6.0
    Closes #28537.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/teleport.rb
+++ Formula/teleport.rb
@@ -1,8 +1,8 @@
 class Teleport < Formula
   desc "Modern SSH server for teams managing distributed infrastructure"
   homepage "https://gravitational.com/teleport"
-  url "https://github.com/gravitational/teleport/archive/v2.5.8.tar.gz"
-  sha256 "5a3c9523ed0e75959418241a84fb8e12a0344275ce058f64303adb095d22b1a4"
+  url "https://github.com/gravitational/teleport/archive/v2.6.0.tar.gz"
+  sha256 "961b2a7fab152a2703f7129f5339c416f5eb36c80683ebf5617d36c3970daf2b"
 
   bottle do
     sha256 "2aa6874de3be0019dd6d425d35971a8c220e64a747d891a346d03ac42e4d2da4" => :high_sierra
@@ -19,9 +19,6 @@ class Teleport < Formula
     ENV["GOARCH"] = MacOS.prefer_64_bit? ? "amd64" : "386"
     ENV["GOPATH"] = buildpath
     ENV["GOROOT"] = Formula["go"].opt_libexec
-
-    # Reported 21 Feb 2018 https://github.com/gravitational/teleport/issues/1708
-    inreplace "Makefile", "-j 3", "-j 1"
 
     (buildpath/"src/github.com/gravitational/teleport").install buildpath.children
     cd "src/github.com/gravitational/teleport" do
