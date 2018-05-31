9fddf1653971961bf5d507f2680d33e9381a7c58
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-31 21:50:45 +0000

    bat: update 0.4.0 bottle.

--- Formula/bat.rb
+++ Formula/bat.rb
@@ -5,9 +5,9 @@ class Bat < Formula
   sha256 "137cd95dba78fa33475c26de59e226c4c9296ec5a752e629229bd2b183665e45"
 
   bottle do
-    sha256 "02e24b89b950892f6fa7abda489a5232144fb78d7da88c6c6ba17d1724fdde89" => :high_sierra
-    sha256 "4781f4b8a80c3588a4bb18d293f7b59699049c028fc962cb69ebc941951f172d" => :sierra
-    sha256 "7bec128188eff3d5f74d5d019f3b752f1ce22b0db8efed0401db3624657f1a73" => :el_capitan
+    sha256 "2f7a44de55ec1868f4a5009b8369657dc4293b0b442aa57334163e266efc8ba8" => :high_sierra
+    sha256 "fe5730a0b8ab66fdac34ccc2581013f3f10f50d62157234000663be707b8de83" => :sierra
+    sha256 "f8bdb53a7058fdbbdee13561790e6636013379463a1a28a30315e89a8f9454b3" => :el_capitan
   end
 
   depends_on "cmake" => :build
