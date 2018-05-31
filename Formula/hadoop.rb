302d61309c49c093e55d81a813c9bd18a67659f2
ilovezfs <ilovezfs@icloud.com>
2018-05-31 05:45:01 -0700

    hadoop 3.1.0 (#28508)

--- Formula/hadoop.rb
+++ Formula/hadoop.rb
@@ -1,9 +1,8 @@
 class Hadoop < Formula
   desc "Framework for distributed processing of large data sets"
   homepage "https://hadoop.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=hadoop/common/hadoop-3.0.0/hadoop-3.0.0.tar.gz"
-  mirror "https://archive.apache.org/dist/hadoop/common/hadoop-3.0.0/hadoop-3.0.0.tar.gz"
-  sha256 "726e28fa7aea71e4587ce91ed3d96c56b15777fc859c09a7438a6d0092e08c74"
+  url "https://www.apache.org/dyn/closer.cgi?path=hadoop/common/hadoop-3.1.0/hadoop-3.1.0.tar.gz"
+  sha256 "670d2ced595fa42d9fa1a93c4e39b39f47002cad1553d9df163ee828ca5143e7"
 
   bottle :unneeded
 
