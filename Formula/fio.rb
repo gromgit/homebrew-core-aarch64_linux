b99aafbcf9cfa42c44a2e8a4dd171c37bf76becc
ilovezfs <ilovezfs@icloud.com>
2018-06-01 22:00:59 -0700

    fio 3.7
    Closes #28587.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/fio.rb
+++ Formula/fio.rb
@@ -1,8 +1,8 @@
 class Fio < Formula
   desc "I/O benchmark and stress test"
   homepage "http://git.kernel.dk/cgit/fio/"
-  url "https://github.com/axboe/fio/archive/fio-3.6.tar.gz"
-  sha256 "184982f5cdddada40b08b3d9a82f4de5e698374d9bfdfb7c85d10e9fddf51b8c"
+  url "https://github.com/axboe/fio/archive/fio-3.7.tar.gz"
+  sha256 "15643de9c02b8ccae0dac287c5f421a7271d81b2600d6ed87b40e430a9f444b3"
 
   bottle do
     cellar :any_skip_relocation
