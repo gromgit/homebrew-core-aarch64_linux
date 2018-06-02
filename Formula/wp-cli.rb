0b4bc3afdc858c250b8ca5bdc8ce5b149d03ce73
ilovezfs <ilovezfs@icloud.com>
2018-06-01 22:10:23 -0700

    wp-cli: mirror to Bintray (#28585)

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -1,7 +1,9 @@
 class WpCli < Formula
   desc "Command-line interface for WordPress"
   homepage "https://wp-cli.org/"
-  url "https://github.com/wp-cli/wp-cli/releases/download/v1.5.1/wp-cli-1.5.1.phar"
+  # Checksum mismatch for 1.5.1 https://github.com/wp-cli/wp-cli/releases/download/v1.5.1/wp-cli-1.5.1.phar
+  # See https://github.com/Homebrew/homebrew-core/pull/28579
+  url "https://dl.bintray.com/homebrew/mirror/wp-cli-1.5.1.phar"
   sha256 "c1114450e647ca0c72afcbb6d02b577a55ec9915a510399579626c960d5dd2d2"
 
   bottle :unneeded
