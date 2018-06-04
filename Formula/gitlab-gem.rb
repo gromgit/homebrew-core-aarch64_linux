1ed63c9dfbc4626b4d0a2010f4f690800d7a8539
ilovezfs <ilovezfs@icloud.com>
2018-06-03 21:35:19 -0700

    gitlab-gem 4.4.0
    Closes #28625.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gitlab-gem.rb
+++ Formula/gitlab-gem.rb
@@ -1,8 +1,8 @@
 class GitlabGem < Formula
   desc "Ruby client and CLI for GitLab API"
   homepage "https://github.com/NARKOZ/gitlab"
-  url "https://github.com/NARKOZ/gitlab/archive/v4.3.0.tar.gz"
-  sha256 "b2679d0696642f67ba84a4f9df21ad4a7bf1bc814d7f86218b72ebe5e56ff2d0"
+  url "https://github.com/NARKOZ/gitlab/archive/v4.4.0.tar.gz"
+  sha256 "4f560b818007989acfe25adbcf66c2d6ac21cf89c7c9f8d1666493cf38fd03cd"
 
   bottle do
     cellar :any_skip_relocation
@@ -14,8 +14,8 @@ class GitlabGem < Formula
   depends_on "ruby" if MacOS.version <= :mountain_lion
 
   resource "httparty" do
-    url "https://rubygems.org/gems/httparty-0.16.0.gem"
-    sha256 "c3e08fac9079fdbe175158782c61f6db6a1918446399560d0bfce1e752b5a5d2"
+    url "https://rubygems.org/gems/httparty-0.16.2.gem"
+    sha256 "fc67e5ba443b5ca822c2babccd3c6ed8bcc75fb67432b99652cb95972d204cff"
   end
 
   resource "terminal-table" do
@@ -24,13 +24,13 @@ class GitlabGem < Formula
   end
 
   resource "multi_xml" do
-    url "https://rubygems.org/gems/multi_xml-0.5.5.gem"
-    sha256 "08936dc294586ee1d178217cce577febe26315d7880e01e4f8e97cf2753b1945"
+    url "https://rubygems.org/gems/multi_xml-0.6.0.gem"
+    sha256 "d24393cf958adb226db884b976b007914a89c53ad88718e25679d7008823ad52"
   end
 
   resource "unicode-display_width" do
-    url "https://rubygems.org/gems/unicode-display_width-1.1.2.gem"
-    sha256 "d966add501d3c35fc5ba2cba50d78bf58567fa187e73b4a549de5bc3c6f6d351"
+    url "https://rubygems.org/gems/unicode-display_width-1.3.3.gem"
+    sha256 "38c078f93b1d2998574672913571e265c9346ba747d6e14217980cc39fb6e157"
   end
 
   def install
