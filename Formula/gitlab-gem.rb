bf976f7309350aa82e3a9b61f8f7dfffcbf4c507
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-04 05:53:56 +0000

    gitlab-gem: update 4.4.0 bottle.

--- Formula/gitlab-gem.rb
+++ Formula/gitlab-gem.rb
@@ -6,9 +6,9 @@ class GitlabGem < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "370ba4b9e70f7d780b50400aeff2ba11f7e0015b48a8989a86e8022aa6c1eeb3" => :high_sierra
-    sha256 "b196b8447773b7adbf9361e3e07fee0603330ac4ff01862d943f58b564156e2c" => :sierra
-    sha256 "099a1c60003659f690abea40da21dd8d72304fdf313eb900b5c0ac7f69629f84" => :el_capitan
+    sha256 "76d223287d5b8294fd114dd41f0608ade9620b48198d2e3ead600c765effa3c8" => :high_sierra
+    sha256 "9bff32c99d9ce98893550ee0944c6eaf35d38c7bb76904eaff7288c598a5035a" => :sierra
+    sha256 "d5a8c2194848ec298dba9304036d9af4e042aeab3154d86a211638415a0681bd" => :el_capitan
   end
 
   depends_on "ruby" if MacOS.version <= :mountain_lion
