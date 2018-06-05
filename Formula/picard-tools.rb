917e8a3c34df121e4ae03d07df1913ee6d4492b5
ilovezfs <ilovezfs@icloud.com>
2018-06-04 22:28:21 -0700

    picard-tools 2.18.6 (#28655)

--- Formula/picard-tools.rb
+++ Formula/picard-tools.rb
@@ -1,8 +1,8 @@
 class PicardTools < Formula
   desc "Tools for manipulating HTS data and formats"
   homepage "https://broadinstitute.github.io/picard/"
-  url "https://github.com/broadinstitute/picard/releases/download/2.18.5/picard.jar"
-  sha256 "21385476cf0f4fa36484b67f6fac3a2514abbcc2a4e7a12582923fc1b8c561d2"
+  url "https://github.com/broadinstitute/picard/releases/download/2.18.6/picard.jar"
+  sha256 "69b795f83c6a0c0ea989e57e15d6fbe16f7e61a008996336c51ef9c691a0950f"
 
   bottle :unneeded
 
