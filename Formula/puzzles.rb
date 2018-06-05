d6dcae2e3d653d179a2adcdb2e41a4c1eea5598c
ilovezfs <ilovezfs@icloud.com>
2018-06-04 21:17:54 -0700

    puzzles 20180602
    Closes #28656.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/puzzles.rb
+++ Formula/puzzles.rb
@@ -2,9 +2,9 @@ class Puzzles < Formula
   desc "Collection of one-player puzzle games"
   homepage "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/"
   # Extract https://www.chiark.greenend.org.uk/~sgtatham/puzzles/puzzles.tar.gz to get the version number
-  url "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/puzzles-20180429.31384ca.tar.gz"
-  version "20180429"
-  sha256 "3439c62d6803a5f6f42d3eaea110afd187e750e453f845d1e1fff50cbc9f2765"
+  url "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/puzzles-20180602.5a697b3.tar.gz"
+  version "20180602"
+  sha256 "b865a31efd5eb4caf5ad12e7f2c2a21897064d889b8c6cdac0c6cad45fcd54f6"
 
   head "https://git.tartarus.org/simon/puzzles.git"
 
@@ -18,12 +18,16 @@ class Puzzles < Formula
   depends_on "halibut"
 
   def install
+    # Prevent "lipo: Puzzles.i386.bin and Puzzles.x86_64.bin have the same
+    # architectures (x86_64) and can't be in the same fat output file"
+    ENV.permit_arch_flags
+
     system "perl", "mkfiles.pl"
     system "make", "-d", "-f", "Makefile.osx", "all"
     prefix.install "Puzzles.app"
   end
 
   test do
-    assert_predicate prefix/"Puzzles.app/Contents/MacOS/puzzles", :executable?
+    assert_predicate prefix/"Puzzles.app/Contents/MacOS/Puzzles", :executable?
   end
 end
