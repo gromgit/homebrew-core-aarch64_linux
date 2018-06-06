a7a0e5de8e5bbddeab165272b1747b5a1ed3984d
ilovezfs <ilovezfs@icloud.com>
2018-06-06 06:38:38 -0700

    nginx 1.15.0
    Closes #28719.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/nginx.rb
+++ Formula/nginx.rb
@@ -3,8 +3,8 @@ class Nginx < Formula
   homepage "https://nginx.org/"
   # Use "mainline" releases only (odd minor version number), not "stable"
   # See https://www.nginx.com/blog/nginx-1-12-1-13-released/ for why
-  url "https://nginx.org/download/nginx-1.13.12.tar.gz"
-  sha256 "fb92f5602cdb8d3ab1ad47dbeca151b185d62eedb67d347bbe9d79c1438c85de"
+  url "https://nginx.org/download/nginx-1.15.0.tar.gz"
+  sha256 "b0b58c9a3fd73aa8b89edf5cfadc6641a352e0e6d3071db1eb3215d72b7fb516"
   head "https://hg.nginx.org/nginx/", :using => :hg
 
   bottle do
