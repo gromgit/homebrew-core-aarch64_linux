37530d3259e29ae732778e97ef75625994d1d5f6
Dominyk Tiller <dominyktiller@gmail.com>
2018-06-06 04:49:21 +0100

    bash-completion: switch homepage/URL

--- Formula/bash-completion.rb
+++ Formula/bash-completion.rb
@@ -2,9 +2,8 @@
 # with 3.2.57. If you've upgraded bash, use bash-completion@2 instead.
 class BashCompletion < Formula
   desc "Programmable completion for Bash 3.2"
-  homepage "https://bash-completion.alioth.debian.org/"
-  url "https://bash-completion.alioth.debian.org/files/bash-completion-1.3.tar.bz2"
-  mirror "https://src.fedoraproject.org/repo/pkgs/bash-completion/bash-completion-1.3.tar.bz2/a1262659b4bbf44dc9e59d034de505ec/bash-completion-1.3.tar.bz2"
+  homepage "https://salsa.debian.org/debian/bash-completion"
+  url "https://src.fedoraproject.org/repo/pkgs/bash-completion/bash-completion-1.3.tar.bz2/a1262659b4bbf44dc9e59d034de505ec/bash-completion-1.3.tar.bz2"
   sha256 "8ebe30579f0f3e1a521013bcdd183193605dab353d7a244ff2582fb3a36f7bec"
   revision 3
 
@@ -19,15 +18,13 @@ class BashCompletion < Formula
   conflicts_with "bash-completion@2", :because => "Differing version of same formula"
 
   # Backports the following upstream patch from 2.x:
-  # https://anonscm.debian.org/gitweb/?p=bash-completion/bash-completion.git;a=commitdiff_plain;h=50ae57927365a16c830899cc1714be73237bdcb2
   # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=740971
   patch do
     url "https://raw.githubusercontent.com/Homebrew/formula-patches/c1d87451da3b5b147bed95b2dc783a1b02520ac5/bash-completion/bug-740971.patch"
     sha256 "bd242a35b8664c340add068bcfac74eada41ed26d52dc0f1b39eebe591c2ea97"
   end
 
-  # Backports (a variant of) the following upstream patch to fix man completion:
-  # https://anonscm.debian.org/cgit/bash-completion/bash-completion.git/patch/completions/man?id=fb2d657fac6be93a1c4ffa76018d8042859e0a03
+  # Backports (a variant of) an upstream patch to fix man completion.
   patch :DATA
 
   def install
