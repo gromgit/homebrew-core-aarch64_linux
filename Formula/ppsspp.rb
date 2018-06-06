bb6b79b06cd70cf74176f6aa6ff8cbcd9a9bb7fd
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-06 00:35:06 +0000

    ppsspp: update 1.6.3 bottle.

--- Formula/ppsspp.rb
+++ Formula/ppsspp.rb
@@ -8,9 +8,9 @@ class Ppsspp < Formula
 
   bottle do
     cellar :any
-    sha256 "2700cf1ad79f686dab67c9828fdf5e8b8046bf299f409fafccade2e577001289" => :high_sierra
-    sha256 "0a84024acc8cc8f8c1d6e73ed1afb6cb099a9c52cc69cef925762a2dee89949a" => :sierra
-    sha256 "a9090b51f4fd4126e91202550fc9c77e9dad62011769a79c2b01e5b472500447" => :el_capitan
+    sha256 "1a2fc8743a9fbaa0f77836bdd874eaebeb0117af5ca61e99dd9e44a8312f17dc" => :high_sierra
+    sha256 "e91b28f7e661e07d9656d94da7fa5aa4bfb1b4c639f7ba363e8e0bfa3d3d0e13" => :sierra
+    sha256 "8a21b9cc0971f4d774b915152f487253d1bd7dca062c1f4f8376dd6fac24710f" => :el_capitan
   end
 
   depends_on "cmake" => :build
