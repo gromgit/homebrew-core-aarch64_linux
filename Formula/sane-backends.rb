dfe4d8788cbd49c5fcb9ce308f1be75f5bfaab0d
Dominyk Tiller <dominyktiller@gmail.com>
2018-06-06 04:44:12 +0100

    sane-backends: change primary & HEAD URLs

--- Formula/sane-backends.rb
+++ Formula/sane-backends.rb
@@ -1,12 +1,11 @@
 class SaneBackends < Formula
   desc "Backends for scanner access"
   homepage "http://www.sane-project.org/"
-  url "https://alioth.debian.org/frs/download.php/file/4224/sane-backends-1.0.27.tar.gz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/s/sane-backends/sane-backends_1.0.27.orig.tar.gz"
+  url "https://mirrors.kernel.org/debian/pool/main/s/sane-backends/sane-backends_1.0.27.orig.tar.gz"
   mirror "https://fossies.org/linux/misc/sane-backends-1.0.27.tar.gz"
   sha256 "293747bf37275c424ebb2c833f8588601a60b2f9653945d5a3194875355e36c9"
   revision 4
-  head "https://anonscm.debian.org/cgit/sane/sane-backends.git"
+  head "https://salsa.debian.org/debian/sane-backends.git"
 
   bottle do
     sha256 "687b773de141d0d79fbf16d882be2066a2fcdf4056ba86803fb40669752e2c79" => :high_sierra
