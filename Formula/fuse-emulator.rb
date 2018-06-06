4f5d15ab6455a53e78c7823eacb36828d050bf5d
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-06 01:39:59 +0000

    fuse-emulator: update 1.5.4 bottle.

--- Formula/fuse-emulator.rb
+++ Formula/fuse-emulator.rb
@@ -5,9 +5,9 @@ class FuseEmulator < Formula
   sha256 "3d40f2e7374f5a3d5a40ce2e91912b1c064b33de8831d5979ec2b89a8e69abf3"
 
   bottle do
-    sha256 "48e80f0811b93b75de1fad0a3b1c9184a1cbd4635e9db0638c02b51ef8a08a16" => :high_sierra
-    sha256 "795b23cbc22c98ecf3afc12d7c1111cd5f2e10e0e025d1ceedfa4ac35d249bf7" => :sierra
-    sha256 "a4cd0a67c0412e8ff9bfcd358083a50a7c5ae8ed76d751aaeb871fbcea338146" => :el_capitan
+    sha256 "18d71cebf0b62bfaac50617afcebd2a2412a02356c06aec8eee58c410e42cff8" => :high_sierra
+    sha256 "b2587394de6205e0b0a3628357d282a3b6211cf96408b1542cb77b0a106cd361" => :sierra
+    sha256 "88176fe4db0280ab3f1e153cd012f2213196fd33a81ca31dc2d920c145bcae18" => :el_capitan
   end
 
   head do
