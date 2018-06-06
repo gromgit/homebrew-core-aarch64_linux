2c73a1c648d2b08ca98905d2760cce2e9b1fb467
Dominyk Tiller <dominyktiller@gmail.com>
2018-06-06 04:26:19 +0100

    pcsc-lite: update URLs for alioth's demise

--- Formula/pcsc-lite.rb
+++ Formula/pcsc-lite.rb
@@ -1,7 +1,7 @@
 class PcscLite < Formula
   desc "Middleware to access a smart card using SCard API"
-  homepage "https://pcsclite.alioth.debian.org"
-  url "https://alioth.debian.org/frs/download.php/file/4235/pcsc-lite-1.8.23.tar.bz2"
+  homepage "https://pcsclite.apdu.fr/"
+  url "https://pcsclite.apdu.fr/files/pcsc-lite-1.8.23.tar.bz2"
   sha256 "5a27262586eff39cfd5c19aadc8891dd71c0818d3d629539bd631b958be689c9"
 
   bottle do
