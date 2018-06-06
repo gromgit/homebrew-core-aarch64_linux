5af2c16eb24eeb64c4d280b92f9b47e401f1cc06
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-06 16:58:18 +0000

    gopass: update 1.8.0 bottle.

--- Formula/gopass.rb
+++ Formula/gopass.rb
@@ -7,9 +7,9 @@ class Gopass < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6d43cd331798143ded5b3ca0398bd3fc899e3440c2f526eb8227aeac31d6dc62" => :high_sierra
-    sha256 "15116df614f6143bd150deb20367b45a2da94fed263fbdd24feb9bdf57160c95" => :sierra
-    sha256 "728c4009e4c942de79814e8fe49babb7b060d141d80b2a9a25da881fad36abbe" => :el_capitan
+    sha256 "03af544c1fbc4ab575f6eb8817cfebdf1c769deb09d5fd297c3d2a3542beff8b" => :high_sierra
+    sha256 "7dc01d3a42ae35562b13b32e19121baca20971d764b9fe0865b02e4a5b0a07d9" => :sierra
+    sha256 "3f842a2a034dd1c673c5d9b917580751fb1a75f45f589121e00348587e6cc58b" => :el_capitan
   end
 
   depends_on "go" => :build
