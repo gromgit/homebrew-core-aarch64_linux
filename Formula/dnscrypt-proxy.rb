b3fd15722a9772ba6592c2be4029177791120968
commitay <commitay@users.noreply.github.com>
2018-06-07 18:24:25 +1000

    dnscrypt-proxy: update checksum
    Closes #28752.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dnscrypt-proxy.rb
+++ Formula/dnscrypt-proxy.rb
@@ -2,7 +2,8 @@ class DnscryptProxy < Formula
   desc "Secure communications between a client and a DNS resolver"
   homepage "https://github.com/jedisct1/dnscrypt-proxy"
   url "https://github.com/jedisct1/dnscrypt-proxy/archive/2.0.15.tar.gz"
-  sha256 "d9d7fc5138d31bfd252f18f004745b89d6f75466c63de068e5832328300ed3fe"
+  sha256 "84c9d94776422b539a2473e586e0cd4d3c65b059a1ec0531e916ebb1d44cce60"
+  revision 1
   head "https://github.com/jedisct1/dnscrypt-proxy.git"
 
   bottle do
