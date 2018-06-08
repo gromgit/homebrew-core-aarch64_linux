fb42cc65664b822c858c993d8639bd8eb3a7272d
Andrew Janke <floss@apjanke.net>
2018-06-08 01:59:53 -0400

    ceylon: require Java 8 specifically (#28775)
    Fixes run time failure under Java 10.

--- Formula/ceylon.rb
+++ Formula/ceylon.rb
@@ -3,19 +3,19 @@ class Ceylon < Formula
   homepage "https://ceylon-lang.org/"
   url "https://ceylon-lang.org/download/dist/1_3_3"
   sha256 "4ec1f1781043ee369c3e225576787ce5518685f2206eafa7d2fd5cfe6ac9923d"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.7+"
+  depends_on :java => "1.8"
 
   def install
     man1.install Dir["doc/man/man1/*"]
     doc.install Dir["doc/*"]
+    bin.install "bin/ceylon"
+    bin.install "bin/ceylon-sh-setup"
     libexec.install Dir["*"]
-
-    # Symlink shell scripts but not *.plugin
-    bin.install_symlink "#{libexec}/bin/ceylon"
-    bin.install_symlink "#{libexec}/bin/ceylon-sh-setup"
+    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
   end
 
   test do
