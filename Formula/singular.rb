fb6e1ad4cf03f84b52d7e17ee5d1f1bbea29a658
ilovezfs <ilovezfs@icloud.com>
2018-06-07 22:39:02 -0700

    singular: revision for ntl
    Closes #28767.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/singular.rb
+++ Formula/singular.rb
@@ -3,7 +3,7 @@ class Singular < Formula
   homepage "https://www.singular.uni-kl.de/"
   url "http://www.mathematik.uni-kl.de/ftp/pub/Math/Singular/SOURCES/4-1-1/singular-4.1.1.tar.gz"
   sha256 "3792c5707b60c1748298bf47e2277de20303d60563b797372cc0e1eff4bbc583"
-  revision 2
+  revision 3
 
   bottle do
     sha256 "cbad21630b787e410d0adb653aa6c56c2ac865cfeb695bb8ac2d23f2c82142ad" => :high_sierra
