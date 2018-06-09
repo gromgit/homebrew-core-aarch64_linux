aa7d9590a2fbbf93016af1f9514ddf6360903055
Archie L. Cobbs <archie.cobbs@gmail.com>
2018-06-09 15:58:15 -0500

    s3-backer 1.5.0
    Closes #28837.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/s3-backer.rb
+++ Formula/s3-backer.rb
@@ -1,8 +1,8 @@
 class S3Backer < Formula
   desc "FUSE-based single file backing store via Amazon S3"
   homepage "https://github.com/archiecobbs/s3backer"
-  url "https://s3.amazonaws.com/archie-public/s3backer/s3backer-1.4.4.tar.gz"
-  sha256 "7efa9347490e04bc22b4e62bed36864be1ba31bf8558d598cab57ef5096f2c02"
+  url "https://s3.amazonaws.com/archie-public/s3backer/s3backer-1.5.0.tar.gz"
+  sha256 "82d93c54acb1e85828b6b80a06e69a99c7e06bf6ee025dac720e980590d220d2"
 
   bottle do
     cellar :any
