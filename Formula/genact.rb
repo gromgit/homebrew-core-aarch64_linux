4839c33cf0f4faa03de34f05e06a1ff642e92a90
ilovezfs <ilovezfs@icloud.com>
2018-06-09 03:56:23 -0700

    genact 0.6.0
    Closes #28827.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/genact.rb
+++ Formula/genact.rb
@@ -1,8 +1,8 @@
 class Genact < Formula
   desc "Nonsense activity generator"
   homepage "https://github.com/svenstaro/genact"
-  url "https://github.com/svenstaro/genact/archive/0.5.0.tar.gz"
-  sha256 "ad308d362c8eded7865651806a1938c9e62b81a68af81c2dd1c967a4c638e78a"
+  url "https://github.com/svenstaro/genact/archive/0.6.0.tar.gz"
+  sha256 "ab20d1c9c20f3b25d0be62fdb8aba74c0e2f046e647327d85a0ecc84d40f8a94"
 
   bottle do
     sha256 "87d766593e0414423a19d44e2fcb390f1962cc28d936e572c04207fc8ae24dc8" => :high_sierra
