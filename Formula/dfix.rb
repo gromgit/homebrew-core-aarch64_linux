943f6a944ed015f7dccc64bf46f22b3404bfdb85
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-10 18:15:39 +0000

    dfix: update 0.3.4 bottle.

--- Formula/dfix.rb
+++ Formula/dfix.rb
@@ -7,9 +7,9 @@ class Dfix < Formula
   head "https://github.com/dlang-community/dfix.git", :shallow => false
 
   bottle do
-    sha256 "77c5f989b77e63a53aea939dce24c99c203073eba08c687eb0feb8146ea3d630" => :high_sierra
-    sha256 "75570e29a01423b0806c248a1daf6ff6c69681c3c530a34d296604b4663920a9" => :sierra
-    sha256 "6dbc07c50bbcd397af748e38fea9bbb63876caba64777ff63ae775764258f70f" => :el_capitan
+    sha256 "0b7f721031ba8a300a2fcd425d1920d6124993fae5ad6af3a79a9e0d882ff475" => :high_sierra
+    sha256 "63c88b055983c1f9e5e76a7c4bf349ce5f07adda884356fd25d2af0653b92732" => :sierra
+    sha256 "8e82902ec6075a0df5f09d17258f64fbbff88d4449d5704f9d66fbeae9d370b6" => :el_capitan
   end
 
   depends_on "dmd" => :build
