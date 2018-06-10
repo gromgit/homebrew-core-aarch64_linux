fdab469f3cbce4492841d0a8aa5a7906fb09b6cb
ilovezfs <ilovezfs@icloud.com>
2018-06-10 10:12:24 -0700

    tcl-tk: fix build on Mavericks and Mountain Lion (#28857)

--- Formula/tcl-tk.rb
+++ Formula/tcl-tk.rb
@@ -13,6 +13,15 @@ class TclTk < Formula
       mirror "ftp://ftp.tcl.tk/pub/tcl/tcl8_6/tk8.6.8-src.tar.gz"
       version "8.6.8"
       sha256 "49e7bca08dde95195a27f594f7c850b088be357a7c7096e44e1158c7a5fd7b33"
+
+      # Upstream issue 7 Jan 2018 "Build failure with Aqua support on OS X 10.8 and 10.9"
+      # See https://core.tcl.tk/tcl/tktview/95a8293a2936e34cc8d0658c21e5214f1ca9b435
+      if MacOS.version == :mavericks || MacOS.version == :mountain_lion
+        patch :p0 do
+          url "https://raw.githubusercontent.com/macports/macports-ports/0a883ad388b/x11/tk/files/patch-macosx-tkMacOSXXStubs.c.diff"
+          sha256 "2cdba6bbf2503307fe4f4d7200ad57c9926ebf0ff6ed3e65bf551067a30a04a9"
+        end
+      end
     end
   end
 
