94077b99483314a4de4c8d7bc0ede75a76db2985
commitay <commitay@users.noreply.github.com>
2018-06-11 12:10:41 +1000

    gst-plugins-good: remove libogg conditional dependency
    since flac already depends on libogg.
    
    Also, remove "with-libogg" option from flac optional dependency.
    
    Closes #28878.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gst-plugins-good.rb
+++ Formula/gst-plugins-good.rb
@@ -39,7 +39,7 @@ class GstPluginsGood < Formula
   depends_on "gdk-pixbuf" => :optional
   depends_on "aalib" => :optional
   depends_on "cairo" => :optional
-  depends_on "flac" => [:optional, "with-libogg"]
+  depends_on "flac" => :optional
   depends_on "gtk+3" => :optional
   depends_on "libcaca" => :optional
   depends_on "libdv" => :optional
@@ -51,8 +51,6 @@ class GstPluginsGood < Formula
   depends_on "libvpx" => :optional
   depends_on "pulseaudio" => :optional
   depends_on "jack" => :optional
-
-  depends_on "libogg" if build.with? "flac"
 
   def install
     args = %W[
