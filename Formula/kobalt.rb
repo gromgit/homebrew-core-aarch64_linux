0fb97f4635cb230178eb4d0d6ba8cbdc4e3bb227
Viktor Szakats <commit@vszakats.net>
2018-06-11 12:02:24 +0000

    kobalt: secure url(s)

--- Formula/kobalt.rb
+++ Formula/kobalt.rb
@@ -1,6 +1,6 @@
 class Kobalt < Formula
   desc "Build system"
-  homepage "http://beust.com/kobalt"
+  homepage "https://beust.com/kobalt/"
   url "https://github.com/cbeust/kobalt/releases/download/1.0.114/kobalt-1.0.114.zip"
   sha256 "2f1e3eab940ba778a43a1d0dcd1140f2a77d67c317ddbf6dc60eb1b4e7f2b3d2"
 
