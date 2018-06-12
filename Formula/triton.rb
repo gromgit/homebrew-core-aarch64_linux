b65de7d7a315e9341858ce55fc3f36bcce097291
Alyssa Ross <hi@alyssa.is>
2018-06-12 16:02:54 +0100

    triton 6.0.0 (new formula)
    Closes #28936.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/triton.rb
@@ -0,0 +1,20 @@
+require "language/node"
+
+class Triton < Formula
+  desc "Joyent Triton CLI"
+  homepage "https://www.npmjs.com/package/triton"
+  url "https://registry.npmjs.org/triton/-/triton-6.0.0.tgz"
+  sha256 "b5211e61732d3ea656da561e749b2b6b0c3076ba23deea7c7c8405c92f130740"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    output = shell_output("#{bin}/triton profile ls")
+    assert_match /\ANAME  CURR  ACCOUNT  USER  URL$/, output
+  end
+end
