8b5c33a63445f62689ddfe1d4862eebc213147fe
ilovezfs <ilovezfs@icloud.com>
2018-06-11 21:59:04 -0700

    openrct2 0.2.0
    depend on icu4c
    raise minimum macOS requirement to 10.13
    
    Closes #28897.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/openrct2.rb
+++ Formula/openrct2.rb
@@ -1,8 +1,8 @@
 class Openrct2 < Formula
   desc "Open source re-implementation of RollerCoaster Tycoon 2"
   homepage "https://openrct2.io/"
-  url "https://github.com/OpenRCT2/OpenRCT2/archive/v0.1.2.tar.gz"
-  sha256 "358fc0ceff848734b399dc63c1d7a0c096a54a793afe4522d6f0dd00ba9328c7"
+  url "https://github.com/OpenRCT2/OpenRCT2/archive/v0.2.0.tar.gz"
+  sha256 "bff3fcc728765b51d2498e685e2a7f28e2c1a830691fd2c3ea5dd82354962bfb"
   head "https://github.com/OpenRCT2/OpenRCT2.git", :branch => "develop"
 
   bottle do
@@ -13,10 +13,11 @@ class Openrct2 < Formula
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "icu4c"
   depends_on "jansson"
   depends_on "libpng"
   depends_on "libzip"
-  depends_on :macos => :sierra # undefined symbol ___cpu_model on El Capitan
+  depends_on :macos => :high_sierra # "missing: Threads_FOUND" on Sierra
   depends_on "openssl"
   depends_on "sdl2"
   depends_on "sdl2_ttf"
