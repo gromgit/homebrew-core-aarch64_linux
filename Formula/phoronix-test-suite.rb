df09dc8ba658b5f6649bc5e3d485ce40b93421f7
ilovezfs <ilovezfs@icloud.com>
2018-06-11 22:41:24 -0700

    phoronix-test-suite 8.0.0 (#28898)

--- Formula/phoronix-test-suite.rb
+++ Formula/phoronix-test-suite.rb
@@ -1,8 +1,8 @@
 class PhoronixTestSuite < Formula
   desc "Open-source automated testing/benchmarking software"
   homepage "https://www.phoronix-test-suite.com/"
-  url "https://github.com/phoronix-test-suite/phoronix-test-suite/archive/v7.8.0.tar.gz"
-  sha256 "7064b0b5428a99a4709780300aa4fc4d3c482326e74174689d1defd6a3588b69"
+  url "https://github.com/phoronix-test-suite/phoronix-test-suite/archive/v8.0.0.tar.gz"
+  sha256 "1a2352f3dfe37c1201553431bc41a5d8f0839712fe2c6dfc84aa31837ce0656a"
 
   bottle :unneeded
 
