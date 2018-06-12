f0b442d1046ef1d578c0d20b8c044189b117d19e
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-12 07:13:54 +0000

    pgpool-ii: update 3.7.4 bottle.

--- Formula/pgpool-ii.rb
+++ Formula/pgpool-ii.rb
@@ -5,9 +5,9 @@ class PgpoolIi < Formula
   sha256 "2312f0fb16baa4efc3b42c6f814c399c6856ddd60abc656cf0f82ed33dca9a6b"
 
   bottle do
-    sha256 "bc423203bfa50acfc5bf818514fe08cd715cdc1bba1f5bbef634d8eb3a5452b6" => :high_sierra
-    sha256 "b3089005f4794acf571590f7db65476dae7ac0c7c39ce8371ed1fb0b6d37ed52" => :sierra
-    sha256 "600623571d172e9d980500945233e2af293aeea8080e4fce5844932ab37640e6" => :el_capitan
+    sha256 "47dd6bdd5eff9d9705f8ebc27967baa8bb0caf11bdda5c62cef67406b1e07daf" => :high_sierra
+    sha256 "fbd2039a714413cc0678cd99135e9cfc6cb0d247c9c6b7b91579e92a8f47bc87" => :sierra
+    sha256 "05535a283da10c44b7718b357b868ec64229ac6eade3c62d01582b7d4964072f" => :el_capitan
   end
 
   depends_on "postgresql"
