ccd9f1197213296df0dc10939a937c6c432dbe3b
Yurii Kolesnykov <yurikoles@gmail.com>
2018-06-12 20:04:12 +0300

    tor 0.3.3.7
    Closes #28938.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/tor.rb
+++ Formula/tor.rb
@@ -1,9 +1,9 @@
 class Tor < Formula
   desc "Anonymizing overlay network for TCP"
   homepage "https://www.torproject.org/"
-  url "https://www.torproject.org/dist/tor-0.3.3.6.tar.gz"
-  mirror "https://tor.eff.org/dist/tor-0.3.3.6.tar.gz"
-  sha256 "99bc59f6dbf395894de12f3a83b3251a82dfd93dc7f6d3afcbbd80f6111433b7"
+  url "https://www.torproject.org/dist/tor-0.3.3.7.tar.gz"
+  mirror "https://tor.eff.org/dist/tor-0.3.3.7.tar.gz"
+  sha256 "ea6bb512c4adfbc4e05b22e4c2d06bddff5b358a53de982273fec846b75bde0c"
 
   bottle do
     sha256 "59aaedd430ab5d2c51ef67f22d1ced4a1ee6c302d99902bb765fe11154c7c824" => :high_sierra
