c045f2a9592b05f9c9fd78e0640c36e709d07e3b
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-12 15:48:58 +0000

    triton: add 6.0.0 bottle.

--- Formula/triton.rb
+++ Formula/triton.rb
@@ -6,6 +6,12 @@ class Triton < Formula
   url "https://registry.npmjs.org/triton/-/triton-6.0.0.tgz"
   sha256 "b5211e61732d3ea656da561e749b2b6b0c3076ba23deea7c7c8405c92f130740"
 
+  bottle do
+    sha256 "dff2adfb606119f4d3fcefcabc4e2b1e73783fbc357afc6eccd41a4d0599de5d" => :high_sierra
+    sha256 "8f654575429e7f236a2135aa5e909ceefce958ea209de986d4cfb5f5324e02ce" => :sierra
+    sha256 "3c9560f04c445b6cbf93ae2c816363a8673190fdd28908d7e459547236f23ffb" => :el_capitan
+  end
+
   depends_on "node"
 
   def install
