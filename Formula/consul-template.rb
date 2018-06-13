5605e84530a31c2f65de0e5e908d0560cdb0243a
ilovezfs <ilovezfs@icloud.com>
2018-06-12 21:50:03 -0700

    consul-template 0.19.5
    Closes #28962.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/consul-template.rb
+++ Formula/consul-template.rb
@@ -2,8 +2,8 @@ class ConsulTemplate < Formula
   desc "Generic template rendering and notifications with Consul"
   homepage "https://github.com/hashicorp/consul-template"
   url "https://github.com/hashicorp/consul-template.git",
-      :tag => "v0.19.4",
-      :revision => "111a80441153783544817b7c6a9c08633308b9a8"
+      :tag => "v0.19.5",
+      :revision => "f8c8205caf458dfd0ecab69d029ab112803aa587"
   head "https://github.com/hashicorp/consul-template.git"
 
   bottle do
