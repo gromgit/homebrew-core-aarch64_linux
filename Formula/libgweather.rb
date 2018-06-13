c1bbe33f56c299d487b05067755c9acfaa5adab4
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-13 13:42:54 +0000

    libgweather: update 3.28.2 bottle.

--- Formula/libgweather.rb
+++ Formula/libgweather.rb
@@ -5,9 +5,9 @@ class Libgweather < Formula
   sha256 "081ce81653afc614e12641c97a8dd9577c524528c63772407ae2dbcde12bde75"
 
   bottle do
-    sha256 "b96a79ab676438c7e657c17e0fbaacae2e3db041fc66254aae58dadf32b11d70" => :high_sierra
-    sha256 "9655e4d9cba44c13e48fa5b661c0c5a121d266ce002e8bcdf32c3c5f30d20b71" => :sierra
-    sha256 "9e5e4cd5ae1384d401cde7b3ec8b098b914230fdc3bbd584b382cd5fda0c181e" => :el_capitan
+    sha256 "2eb44ffb03a8d0ef04e3f60249136acb7ae5d6581b7eb2b3354f4e33dfae9b92" => :high_sierra
+    sha256 "9fc3c60b4f111e74e9d508ca95f85f16c347880cc3dd689a9b73ac772f316992" => :sierra
+    sha256 "d47872fbe5c9325b3a40cc922c75728651665b25585c9bf9610d98bd6e60ac0a" => :el_capitan
   end
 
   depends_on "gobject-introspection" => :build
