0e258474c63b5fe4668260e4ff1374d6a97dc3e9
commitay <commitay@users.noreply.github.com>
2018-06-14 09:16:49 +1000

    container-diff 0.10.0
    Closes #29006.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/container-diff.rb
+++ Formula/container-diff.rb
@@ -1,8 +1,8 @@
 class ContainerDiff < Formula
   desc "Diff your Docker containers"
-  homepage "https://github.com/GoogleCloudPlatform/container-diff"
-  url "https://github.com/GoogleCloudPlatform/container-diff/archive/v0.9.0.tar.gz"
-  sha256 "cf5957fd9c583030283982dafab0b70608c739408d036444d9acc0a867e7472f"
+  homepage "https://github.com/GoogleContainerTools/container-diff"
+  url "https://github.com/GoogleContainerTools/container-diff/archive/v0.10.0.tar.gz"
+  sha256 "55b62ee0081b9ef5bf5778a6ea30b61f2ee6ee16bfaa7b26bae793455486cc2c"
 
   bottle do
     cellar :any_skip_relocation
@@ -15,10 +15,10 @@ class ContainerDiff < Formula
 
   def install
     ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/GoogleCloudPlatform").mkpath
-    ln_sf buildpath, buildpath/"src/github.com/GoogleCloudPlatform/container-diff"
+    (buildpath/"src/github.com/GoogleContainerTools").mkpath
+    ln_sf buildpath, buildpath/"src/github.com/GoogleContainerTools/container-diff"
 
-    cd "src/github.com/GoogleCloudPlatform/container-diff" do
+    cd "src/github.com/GoogleContainerTools/container-diff" do
       system "make"
       bin.install "out/container-diff"
     end
@@ -27,6 +27,6 @@ class ContainerDiff < Formula
   test do
     image = "daemon://gcr.io/google-appengine/golang:2018-01-04_15_24"
     output = shell_output("#{bin}/container-diff analyze #{image} 2>&1", 1)
-    assert_match "Error loading image from docker engine", output
+    assert_match "Cannot connect to the Docker daemon", output
   end
 end
