4050a69ea7f9287189e0cf8e31ebd1e1a39f5e42
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-14 06:07:14 +0000

    redis@3.2: update 3.2.12 bottle.

--- Formula/redis@3.2.rb
+++ Formula/redis@3.2.rb
@@ -7,9 +7,9 @@ class RedisAT32 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c2b89b227eacd40e8f3d0ad2f8b01509a1f182252e1b91b36a5256a40649b70c" => :high_sierra
-    sha256 "78f7a36a2e4ac2bf8d5efdb70e9ac84eb50d9e6ce815d8c4d25e1268ce67aa5a" => :sierra
-    sha256 "a73d084f4aef70d0f7c614317c66cb9ca55614ccb2363f62af59276d431ee3e0" => :el_capitan
+    sha256 "2d1881b6367b5618b99d31cd3a9af00206b37fce9d36bfd787ebb892ea501707" => :high_sierra
+    sha256 "b4bf9c768ec911cf13d0a6479b4c8a2a100b322ed861c9fc763912f185cc5aa7" => :sierra
+    sha256 "ae4570363ca61057cffee4db90808574c71eb89d97c20250ca5da214d24fbd91" => :el_capitan
   end
 
   keg_only :versioned_formula
