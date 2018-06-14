6644fbe10fd934aaa647f1c38a16f7e902411212
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-14 09:13:37 +0000

    drafter: add 3.2.7 bottle.

--- Formula/drafter.rb
+++ Formula/drafter.rb
@@ -4,6 +4,13 @@ class Drafter < Formula
   url "https://github.com/apiaryio/drafter/releases/download/v3.2.7/drafter-v3.2.7.tar.gz"
   sha256 "a2b7061e2524804f153ac2e80f6367ae65dfcd367f4ee406eddecc6303f7f7ef"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5d350d1a7fb4aa5e9561933f0e9638ee59fd93b0d9168d07e665a806e472d5f1" => :high_sierra
+    sha256 "a90e9d4493f252d29d69e186dcc49eb116f6bc506d07769f03864f56d5840f08" => :sierra
+    sha256 "dc86d4e8dc44c2dead52e57c3bf6403d691926b9abf274f16b94c9649dd562fd" => :el_capitan
+  end
+
   def install
     system "./configure"
     system "make", "install", "DESTDIR=#{prefix}"
