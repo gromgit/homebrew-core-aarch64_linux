e401b876ce036c4e1b45350344324db460c96167
Camden Narzt <camden.narzt@hotmail.com>
2018-06-14 00:37:48 -0600

    emacs: keep daemon alive via launchd (#29003)

--- Formula/emacs.rb
+++ Formula/emacs.rb
@@ -124,6 +124,8 @@ class Emacs < Formula
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
     <dict>
+      <key>KeepAlive</key>
+      <true/>
       <key>Label</key>
       <string>#{plist_name}</string>
       <key>ProgramArguments</key>
