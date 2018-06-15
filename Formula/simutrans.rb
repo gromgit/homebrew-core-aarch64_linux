a467062340cb1d5955bf8e43ee025c25c7805a65
EricFromCanada <eric@lassosoft.com>
2018-06-15 14:24:18 -0400

    simutrans 120.3
    Closes #29089.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/simutrans.rb
+++ Formula/simutrans.rb
@@ -1,21 +1,10 @@
 class Simutrans < Formula
   desc "Transport simulator"
   homepage "https://www.simutrans.com/"
+  url "https://downloads.sourceforge.net/project/simutrans/simutrans/120-3/simutrans-src-120-3.zip"
+  version "120.3"
+  sha256 "6f68785798688bf956b0d7f5971a8d8fa42d12199011665b07b903164cb3929f"
   head "https://github.com/aburch/simutrans.git"
-
-  stable do
-    url "https://downloads.sourceforge.net/project/simutrans/simutrans/120-1-3/simutrans-src-120-1-3.zip"
-    version "120.1.3"
-    sha256 "2d29b849fc39d25a0580091e1377270bddb2cae36c0fc32bd7c2d0f1d7ccfb84"
-
-    # Port Mac audio code from QTKit to AVFoundation
-    # Required since 10.12 SDK no longer includes QTKit.
-    # Submitted upstream: https://forum.simutrans.com/index.php?topic=16675.0
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/bea80842a6ccc5639341add0d8abbca2d49b04c2/simutrans/avfoundation.patch"
-      sha256 "9b9c9e6d89de49f152faaf584fcbbeec628bb07315b7c767e1f8b6791ad1e3ee"
-    end
-  end
 
   bottle do
     cellar :any
@@ -29,13 +18,13 @@ class Simutrans < Formula
   depends_on "sdl2"
 
   resource "pak64" do
-    url "https://downloads.sourceforge.net/project/simutrans/pak64/120-1/simupak64-120-1-2.zip"
-    sha256 "125fa5c13a51bb0630ca651fddb8af06a823e8c4d4638bfa1bb2d89e92cc1d54"
+    url "https://downloads.sourceforge.net/project/simutrans/pak64/120-3/simupak64-120-3.zip"
+    sha256 "332ff947fdddf99e0c9b67d857b1ffd02c91dfea1edc98f4195ec1f1309060f5"
   end
 
   resource "text" do
     url "https://simutrans-germany.com/translator/data/tab/language_pack-Base+texts.zip"
-    sha256 "202816f67750cfb9decdfca3bacfebbc5bfc3474c8703239418edc093ce3774d"
+    sha256 "9c125325e14c6f19a5f6712be7267754e171565a5f49a185ed0b0edea774be0a"
   end
 
   def install
@@ -44,6 +33,7 @@ class Simutrans < Formula
       COLOUR_DEPTH=16
       OSTYPE=mac
     ]
+    args << "AV_FOUNDATION=1" if MacOS.version >= :sierra
     system "make", *args
     libexec.install "build/default/sim" => "simutrans"
     libexec.install Dir["simutrans/*"]
