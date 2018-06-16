49108520737e63a5f0e9465b5ccd59bb00a6f7c4
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-16 10:19:06 +0000

    bitcoin: update 0.16.1 bottle.

--- Formula/bitcoin.rb
+++ Formula/bitcoin.rb
@@ -6,9 +6,9 @@ class Bitcoin < Formula
 
   bottle do
     cellar :any
-    sha256 "0df28afa3424cd564072c92d92047e49f82136c02509b2979e645dd3559a7922" => :high_sierra
-    sha256 "de00614e3315fb047faa56140735c153d967b98150fba07a4b9bbd25a05739b2" => :sierra
-    sha256 "c1d5a0641573188ea59a06ed75498e4b779c5067662d07c853c1223dd085e3c1" => :el_capitan
+    sha256 "7eff6f76c7990a52faa4cb0a7be82037402f9f81940c7c200bed4a20d41f5fce" => :high_sierra
+    sha256 "f5ea06ad7c73fcc1edf4e184c0e363e1c6fc7b2d1a552df0f531aa99c3ebed17" => :sierra
+    sha256 "32923829ea8992ab174490e839b16b283d391a3426d95b00a6f7d31a6f7fbd9f" => :el_capitan
   end
 
   head do
