91b7cbcd04e1b129f03d12636c3b7961cca1ef3f
ilovezfs <ilovezfs@icloud.com>
2018-06-16 22:46:54 -0700

    openfortivpn 1.7.1
    Closes #29123.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/openfortivpn.rb
+++ Formula/openfortivpn.rb
@@ -1,8 +1,8 @@
 class Openfortivpn < Formula
   desc "Open Fortinet client for PPP+SSL VPN tunnel services"
   homepage "https://github.com/adrienverge/openfortivpn"
-  url "https://github.com/adrienverge/openfortivpn/archive/v1.7.0.tar.gz"
-  sha256 "1074e140c537c25f3da63501ebdb80671b770c5078e97aa9970b79d520009541"
+  url "https://github.com/adrienverge/openfortivpn/archive/v1.7.1.tar.gz"
+  sha256 "71222da680cdafbd48f648e4e1bc14822fa06eb84ab5e1fb58f28b6b4251baba"
 
   bottle do
     sha256 "6400f2fd4f043fa1d0d65c44c312406adab3556ec6e76dbf656b89962c37f0fe" => :high_sierra
@@ -14,14 +14,6 @@ class Openfortivpn < Formula
   depends_on "autoconf" => :build
   depends_on "pkg-config" => :build
   depends_on "openssl"
-
-  # Remove for > 1.7.0
-  # Fix "configure: error: Required function not present"
-  # Upstream PR from 21 Apr 2018 "Build with missing pthread_mutexattr_setrobust()"
-  patch do
-    url "https://github.com/adrienverge/openfortivpn/pull/298.patch?full_index=1"
-    sha256 "e70b74e583fdab584bda6be649d5a8853464552b160c132544dac0fe0841eb8c"
-  end
 
   def install
     system "./autogen.sh"
