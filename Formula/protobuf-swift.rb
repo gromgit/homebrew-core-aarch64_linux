55be8cec9268e541127837f58f6d7cba693366c5
Alexey Khokhlov <alexeyxo@gmail.com>
2018-06-18 17:46:20 +0300

    protobuf-swift 4.0.3
    Closes #29172.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/protobuf-swift.rb
+++ Formula/protobuf-swift.rb
@@ -1,8 +1,8 @@
 class ProtobufSwift < Formula
   desc "Implementation of Protocol Buffers in Swift"
   homepage "https://github.com/alexeyxo/protobuf-swift"
-  url "https://github.com/alexeyxo/protobuf-swift/archive/4.0.1.tar.gz"
-  sha256 "098434ab76cc489c92100c0352d4476259737ed925d2c98d267848e7c8be80aa"
+  url "https://github.com/alexeyxo/protobuf-swift/archive/4.0.3.tar.gz"
+  sha256 "c4580fb46018e994cbb2c8a810a251a1cced1bca84513bdd4364c97070e0c20c"
 
   bottle do
     cellar :any
