51d69e0249a21356071c1b2df049767312ac9c2c
ilovezfs <ilovezfs@icloud.com>
2018-06-17 23:25:54 -0700

    gcovr 4.0
    depend on python
    remove bottle unneeded
    
    Closes #29157.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gcovr.rb
+++ Formula/gcovr.rb
@@ -1,15 +1,36 @@
 class Gcovr < Formula
   desc "Reports from gcov test coverage program"
   homepage "https://gcovr.com/"
-  url "https://github.com/gcovr/gcovr/archive/3.4.tar.gz"
-  sha256 "1c52a71f245adfe1b45e30fbe5015337fe66546f17f40038b3969b7b42acceed"
+  url "https://github.com/gcovr/gcovr/archive/4.0.tar.gz"
+  sha256 "f9731a0ea516f2087e13b3bc310dbd8edaec4ce6a56b2462f5603ee925f40377"
   head "https://github.com/gcovr/gcovr.git"
 
-  bottle :unneeded
+  depends_on "python"
+
+  resource "Jinja2" do
+    url "https://files.pythonhosted.org/packages/56/e6/332789f295cf22308386cf5bbd1f4e00ed11484299c5d7383378cf48ba47/Jinja2-2.10.tar.gz"
+    sha256 "f84be1bb0040caca4cea721fcbbbbd61f9be9464ca236387158b0feea01914a4"
+  end
+
+  resource "MarkupSafe" do
+    url "https://files.pythonhosted.org/packages/4d/de/32d741db316d8fdb7680822dd37001ef7a448255de9699ab4bfcbdf4172b/MarkupSafe-1.0.tar.gz"
+    sha256 "a6be69091dac236ea9c6bc7d012beab42010fa914c459791d627dad4910eb665"
+  end
 
   def install
-    libexec.install Dir["*"]
-    bin.install_symlink libexec/"scripts/gcovr"
+    xy = Language::Python.major_minor_version "python3"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
+    resources.each do |r|
+      r.stage do
+        system "python3", *Language::Python.setup_install_args(libexec/"vendor")
+      end
+    end
+
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
+    system "python3", *Language::Python.setup_install_args(libexec)
+
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
   test do
