6db3f8a1d8fed9a4982beb721c4865f8a77da805
David Dollar <david@dollar.io>
2018-06-18 11:06:12 -0400

    convox 20180618130923
    Closes #29171.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -1,8 +1,8 @@
 class Convox < Formula
   desc "Command-line interface for the Rack PaaS on AWS"
   homepage "https://convox.com/"
-  url "https://github.com/convox/rack/archive/20180527180826.tar.gz"
-  sha256 "22cb5762d86960cd55272120afb5f8a8af007b9ce6f3557352aa21cf93493ee8"
+  url "https://github.com/convox/rack/archive/20180618130923.tar.gz"
+  sha256 "1c22ee967754ff182a34dd06341dafe7b8865e6fe163cd1590af167cb7019362"
 
   bottle do
     cellar :any_skip_relocation
