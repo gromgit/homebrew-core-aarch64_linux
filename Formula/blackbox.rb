ae01fac920a3849cb2dbdd30a224f82bac6b69cb
ilovezfs <ilovezfs@icloud.com>
2018-06-18 23:32:18 -0700

    blackbox 1.20180618 (#29184)

--- Formula/blackbox.rb
+++ Formula/blackbox.rb
@@ -1,8 +1,8 @@
 class Blackbox < Formula
   desc "Safely store secrets in Git/Mercurial/Subversion"
   homepage "https://github.com/StackExchange/blackbox"
-  url "https://github.com/StackExchange/blackbox/archive/v1.20180615.tar.gz"
-  sha256 "ff79e896cb4d5a31472899c9ed48ac3b234fc87486a90978f64e03be2c63ac06"
+  url "https://github.com/StackExchange/blackbox/archive/v1.20180618.tar.gz"
+  sha256 "dae06417d2aeaba41cf0f0f7bd8c07c7f65914c926b61f6047b389847e53794e"
 
   bottle :unneeded
 
