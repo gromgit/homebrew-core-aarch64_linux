1dac7c194594a4aa262646500abfe261b58574a5
ilovezfs <ilovezfs@icloud.com>
2018-06-19 05:23:15 -0700

    cake 0.28.1 (#29204)

--- Formula/cake.rb
+++ Formula/cake.rb
@@ -1,8 +1,8 @@
 class Cake < Formula
   desc "Cross platform build automation system with a C# DSL"
   homepage "https://cakebuild.net/"
-  url "https://github.com/cake-build/cake/releases/download/v0.28.0/Cake-bin-net461-v0.28.0.zip"
-  sha256 "806b3946575652f2ebd00980cf7a18a5500fa19f718cb362591c7ce2538d4ac4"
+  url "https://github.com/cake-build/cake/releases/download/v0.28.1/Cake-bin-net461-v0.28.1.zip"
+  sha256 "537f5b821e05ab947ea6947d547ced7dae51e3f1b34028815fc92f3cfe859f5d"
 
   bottle :unneeded
 
