d22359c53b8177113348cf9076adef06c95a4455
Dominyk Tiller <DomT4@users.noreply.github.com>
2018-06-19 12:24:58 +0100

    qemu: apply upstream fix for GTK build (#29203)
    also depend on gtk+3 optionally instead of gtk+
    
    Fixes #27146.

--- Formula/qemu.rb
+++ Formula/qemu.rb
@@ -1,9 +1,19 @@
 class Qemu < Formula
   desc "x86 and PowerPC Emulator"
   homepage "https://www.qemu.org/"
-  url "https://download.qemu.org/qemu-2.12.0.tar.bz2"
-  sha256 "c9f4a147bc915d24df9784affc611a115f42d24720a89210b479f1ba7a3f679c"
   head "https://git.qemu.org/git/qemu.git"
+
+  stable do
+    url "https://download.qemu.org/qemu-2.12.0.tar.bz2"
+    sha256 "c9f4a147bc915d24df9784affc611a115f42d24720a89210b479f1ba7a3f679c"
+
+    # https://lists.nongnu.org/archive/html/qemu-devel/2018-06/msg03856.html
+    # https://github.com/Homebrew/homebrew-core/issues/27146
+    patch do
+      url "https://git.qemu.org/?p=qemu.git;a=patch;h=656282d245b49b84d4a1a47d7b7ede482d541776"
+      sha256 "9f34465a06115bbffa171502e863e90a776ff6f897cd82ccebe287a91daad975"
+    end
+  end
 
   bottle do
     sha256 "ef4da4274e0e582cff223d46736d386ea78444e7943bcb47f4541bec9e6b7786" => :high_sierra
@@ -21,11 +31,12 @@ class Qemu < Formula
   depends_on "libpng" => :recommended
   depends_on "vde" => :optional
   depends_on "sdl2" => :optional
-  depends_on "gtk+" => :optional
+  depends_on "gtk+3" => :optional
   depends_on "libssh2" => :optional
   depends_on "libusb" => :optional
 
   deprecated_option "with-sdl" => "with-sdl2"
+  deprecated_option "with-gtk+" => "with-gtk+3"
 
   fails_with :gcc_4_0 do
     cause "qemu requires a compiler with support for the __thread specifier"
@@ -55,7 +66,7 @@ class Qemu < Formula
     ]
 
     # Cocoa and SDL2/GTK+ UIs cannot both be enabled at once.
-    if build.with?("sdl2") || build.with?("gtk+")
+    if build.with?("sdl2") || build.with?("gtk+3")
       args << "--disable-cocoa"
     else
       args << "--enable-cocoa"
@@ -63,7 +74,7 @@ class Qemu < Formula
 
     args << (build.with?("vde") ? "--enable-vde" : "--disable-vde")
     args << (build.with?("sdl2") ? "--enable-sdl" : "--disable-sdl")
-    args << (build.with?("gtk+") ? "--enable-gtk" : "--disable-gtk")
+    args << (build.with?("gtk+3") ? "--enable-gtk" : "--disable-gtk")
     args << (build.with?("libssh2") ? "--enable-libssh2" : "--disable-libssh2")
 
     system "./configure", *args
