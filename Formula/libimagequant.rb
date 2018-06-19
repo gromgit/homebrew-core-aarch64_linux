d3132af427050e7455c95f82e645147c28d86593
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-19 08:08:44 +0000

    libimagequant: add 2.12.1 bottle.

--- Formula/libimagequant.rb
+++ Formula/libimagequant.rb
@@ -4,6 +4,13 @@ class Libimagequant < Formula
   url "https://github.com/ImageOptim/libimagequant/archive/2.12.1.tar.gz"
   sha256 "7035eb281bc9a49cf36db8db807b713d03a0ffe8c5abfbb17a9ea8a038f21d5e"
 
+  bottle do
+    cellar :any
+    sha256 "714cab630af0e9d0e19ad0a30a1e07b4dfce3988f9c048e3b634cea85c018ec6" => :high_sierra
+    sha256 "41aa19e02e531883c60360556f94d404fb6be00bcb8b4c155368ec34c190ad10" => :sierra
+    sha256 "908ec28bdf4879c7527053bbec4b635e0cbb4f7c635251674e8f1723b4e16cd6" => :el_capitan
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
