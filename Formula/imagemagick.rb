a1f7276790ffdf9b8741f7627f74ad3e140c56f5
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-19 01:05:30 +0000

    imagemagick: update 7.0.8-2 bottle.

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -10,9 +10,9 @@ class Imagemagick < Formula
   head "https://github.com/ImageMagick/ImageMagick.git"
 
   bottle do
-    sha256 "8abafa00f51531de94bd5d5c1e63df6835109b6d16eb2ca016f083cd6f3ecc8b" => :high_sierra
-    sha256 "ad02a7eb275cc071d7355d8a73c5a112e2d9d58857dba8ed6bdfb386b1d310e1" => :sierra
-    sha256 "77c2ddbf5756b3948a4ae3a6460de960e24f092ed666107d6eef8290e5d354e3" => :el_capitan
+    sha256 "3eacc556188fae94285bb8bfc9458a661c6373f9858c3ba256d9becd18177e5c" => :high_sierra
+    sha256 "c7688694dac40854bfcc479d9dfa07e8c6e69166c4e6ca9fe15e5abb49459b2a" => :sierra
+    sha256 "0ce64eb7abbfd0bb52c7ed47265756309fbaeebbd34878c4581e9bb7251069fa" => :el_capitan
   end
 
   option "with-fftw", "Compile with FFTW support"
