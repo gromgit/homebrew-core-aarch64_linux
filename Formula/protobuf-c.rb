ddcfc5e50b618f0cece425b54bbcb115c6ef7ffe
ilovezfs <ilovezfs@icloud.com>
2018-06-20 09:53:25 -0700

    protobuf-c: revision for protobuf
    patch for protobuf 3.6.x compatibility
    build with C++11

--- Formula/protobuf-c.rb
+++ Formula/protobuf-c.rb
@@ -3,7 +3,7 @@ class ProtobufC < Formula
   homepage "https://github.com/protobuf-c/protobuf-c"
   url "https://github.com/protobuf-c/protobuf-c/releases/download/v1.3.0/protobuf-c-1.3.0.tar.gz"
   sha256 "5dc9ad7a9b889cf7c8ff6bf72215f1874a90260f60ad4f88acf21bb15d2752a1"
-  revision 2
+  revision 3
 
   bottle do
     sha256 "8f56aec07a20c79a5d01c7a8d30946d1b86c157d24468e8160edbec318046b71" => :high_sierra
@@ -14,7 +14,24 @@ class ProtobufC < Formula
   depends_on "pkg-config" => :build
   depends_on "protobuf"
 
+  # protobuf 3.6 compat
+  # Upstream PR 11 Mar 2018 "Add std:: to some types"
+  patch do
+    url "https://github.com/protobuf-c/protobuf-c/pull/309.patch?full_index=1"
+    sha256 "31aca86f6fd2f99a6dfd0e6ecd46e69db4128709024a33834749f4b1bbbfcaaa"
+  end
+
+  # Upstream PR 20 Jun 2018 "Fix build with protobuf 3.6.x"
+  patch do
+    url "https://github.com/protobuf-c/protobuf-c/pull/328.patch?full_index=1"
+    sha256 "a5f66087294eb5cc892985b8edf0114e579a9a1c68c30673f56b508cfce6450a"
+  end
+
+  needs :cxx11
+
   def install
+    ENV.cxx11
+
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install"
   end
