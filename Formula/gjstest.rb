d34f27d63b7be842ea21e60e8234eac20fb4a3a1
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-20 18:09:06 +0000

    gjstest: update 1.0.2_13 bottle.

--- Formula/gjstest.rb
+++ Formula/gjstest.rb
@@ -7,9 +7,9 @@ class Gjstest < Formula
   head "https://github.com/google/gjstest.git"
 
   bottle do
-    sha256 "23787738847f2dfbe0c3d6943c846ec14530b8b1a79441b3af8d118adc5b3671" => :high_sierra
-    sha256 "848873eb3f3b770f2e67ffe2747a91b0fcddabda910eb2764bcb1e1ce9060c38" => :sierra
-    sha256 "c487833143ddbab1244ce53771436683e6fe294d258b41b344331c5311e288e9" => :el_capitan
+    sha256 "1b61074dc9a8e50b39794cb984cf3c9de841a83f6f31c59b614eb751d545f0ab" => :high_sierra
+    sha256 "dbe651fdbf5d55fd4813a7ec46bb855651d6866078931e8da304a7c68e5fe270" => :sierra
+    sha256 "0ca5e3fe6dcc0d49844462285f8db439c4471d448e17e0a1f948147a21ae2064" => :el_capitan
   end
 
   depends_on :macos => :mavericks
