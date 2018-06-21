0e492da438e468241837485490ec0f6dcfffc271
Dominyk Tiller <dominyktiller@gmail.com>
2018-06-21 17:26:24 +0100

    jxrlib: update for codeplex's demise

--- Formula/jxrlib.rb
+++ Formula/jxrlib.rb
@@ -1,10 +1,9 @@
 class Jxrlib < Formula
   desc "Tools for JPEG-XR image encoding/decoding"
-  homepage "https://jxrlib.codeplex.com/"
+  homepage "https://archive.codeplex.com/?p=jxrlib"
   url "https://mirrors.ocf.berkeley.edu/debian/pool/main/j/jxrlib/jxrlib_1.1.orig.tar.gz"
   mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/j/jxrlib/jxrlib_1.1.orig.tar.gz"
   sha256 "c7287b86780befa0914f2eeb8be2ac83e672ebd4bd16dc5574a36a59d9708303"
-  head "https://git01.codeplex.com/jxrlib", :using => :git
 
   bottle do
     cellar :any_skip_relocation
@@ -16,18 +15,14 @@ class Jxrlib < Formula
   end
 
   def install
-    if build.head?
-      system "make", "install", "DIR_INSTALL=#{prefix}"
-    else
-      system "make"
-      # The current stable release (1.1) doesn't have a make 'install' target
-      lib.install %w[libjxrglue.a libjpegxr.a]
-      bin.install %w[JxrEncApp JxrDecApp]
-      include.install %w[common image]
-      include.install "jxrgluelib" => "glue"
-      include.install "jxrtestlib" => "test"
-      doc.install Dir["doc/*"]
-    end
+    system "make"
+    # The current stable release (1.1) doesn't have a make 'install' target
+    lib.install %w[libjxrglue.a libjpegxr.a]
+    bin.install %w[JxrEncApp JxrDecApp]
+    include.install %w[common image]
+    include.install "jxrgluelib" => "glue"
+    include.install "jxrtestlib" => "test"
+    doc.install Dir["doc/*"]
   end
 
   test do
