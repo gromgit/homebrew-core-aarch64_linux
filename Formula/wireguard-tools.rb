185df4e7d2f87dae5f1043f4f2c23b6ca9883b26
ilovezfs <ilovezfs@icloud.com>
2018-06-20 23:22:03 -0700

    wireguard-tools 0.0.20180620
    Closes #29275.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/wireguard-tools.rb
+++ Formula/wireguard-tools.rb
@@ -3,19 +3,9 @@ class WireguardTools < Formula
   homepage "https://www.wireguard.com/"
   # Please only update version when the tools have been modified/updated,
   # since the Linux module aspect isn't of utility for us.
+  url "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-0.0.20180620.tar.xz"
+  sha256 "b4db98ea751c8e667454f98ea1c15d704a784fe1bc093b03bd64575418a7c242"
   head "https://git.zx2c4.com/WireGuard", :using => :git
-
-  stable do
-    url "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-0.0.20180613.tar.xz"
-    sha256 "c120cdedc3967dcb4ad5c1c7eadd2a1b04ef5dbf2fe60cc8e7c0db337bcda7dc"
-
-    # Remove for > 0.0.20180613
-    # Upstream commit from 14 Jun 2018 "tools: getentropy requires 10.12"
-    patch do
-      url "https://git.zx2c4.com/WireGuard/patch/?id=5bb62fe22f45b5b5deef4db23ae47c95e1679d1d"
-      sha256 "a057926e50f4981857a9c07905d5c0488588e88b9f0a8f20d236a83a39150901"
-    end
-  end
 
   bottle do
     cellar :any_skip_relocation
