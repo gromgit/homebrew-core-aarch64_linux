53017939777618e9c8b3c16c4083c6c88fc81212
Chongyu Zhu <i@lembacon.com>
2018-06-22 01:10:46 +0800

    hfstospell: prevent from compiling under C++17 mode (#29269)

--- Formula/hfstospell.rb
+++ Formula/hfstospell.rb
@@ -17,6 +17,14 @@ class Hfstospell < Formula
   depends_on "libarchive"
   depends_on "libxml++"
 
+  # Fix "error: no template named 'auto_ptr' in namespace 'std'"
+  # Upstream PR 20 Jun 2018 "C++14 (C++1y) should be the highest supported standard."
+  # See https://github.com/hfst/hfst-ospell/pull/41
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/674a62d/hfstospell/no-cxx17.diff"
+    sha256 "0a3146e871ac0e3c71248b8671d09f6d8a8a69713b6f4857eab7bdb684709083"
+  end
+
   needs :cxx11
 
   def install
