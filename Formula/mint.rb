b98808db74c279bcbf7d439a262d8b55b220672d
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-23 03:45:29 +0000

    mint: update 0.10.2 bottle.

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -6,8 +6,8 @@ class Mint < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "4fcc244edc519247c82378563ce937a72a9aec0ac40d33e2a5140194f34d4e6b" => :high_sierra
-    sha256 "19efd0667200c49dff2c1bbb3987e2347cf2ad4d67b65aeb504802f0f7aff1a0" => :sierra
+    sha256 "1c7809eed9921e8d6b32a9d58656a7aebb593c00f4567bdea869e5be534e01c3" => :high_sierra
+    sha256 "9d328139982de23b815e2dc8ce08213c27f1b097eee8480dcc1ff2be1fd7973c" => :sierra
   end
 
   depends_on :xcode => ["9.2", :build]
