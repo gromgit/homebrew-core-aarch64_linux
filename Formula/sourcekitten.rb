0bd51b9218953a154906020019817dbf0e78f743
JP Simard <jp@jpsim.com>
2018-06-24 11:48:38 -0700

    sourcekitten 0.21.1
    Closes #29385.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/sourcekitten.rb
+++ Formula/sourcekitten.rb
@@ -2,8 +2,8 @@ class Sourcekitten < Formula
   desc "Framework and command-line tool for interacting with SourceKit"
   homepage "https://github.com/jpsim/SourceKitten"
   url "https://github.com/jpsim/SourceKitten.git",
-      :tag => "0.21.0",
-      :revision => "7c09176766d4bbc5da377ad857953fb49510a6aa"
+      :tag => "0.21.1",
+      :revision => "e6efd3d8702fe6668ac43aa882d56f82430c6caf"
   head "https://github.com/jpsim/SourceKitten.git"
 
   bottle do
