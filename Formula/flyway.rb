dc15e3b0ffe6f87320c8160aaf0518ab55f732c2
ilovezfs <ilovezfs@icloud.com>
2018-06-23 23:35:31 -0700

    flyway 5.1.3 (#29368)

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/5.1.1/flyway-commandline-5.1.1.tar.gz"
-  sha256 "ee2ca6771473d790cf1bbc370de1747d205671d443279b450333529e4b3db14d"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/5.1.3/flyway-commandline-5.1.3.tar.gz"
+  sha256 "9502535ae954df39307cbf4f1f107b747fb70edd4b3ef86015ce94c56e96d922"
 
   bottle :unneeded
 
