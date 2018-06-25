6f47301f01c5c6c01291d9c36f40b94f851ef42a
Viktor Szakats <commit@vszakats.net>
2018-06-25 19:55:49 +0000

    bbftp-client: secure url(s)

--- Formula/bbftp-client.rb
+++ Formula/bbftp-client.rb
@@ -2,7 +2,7 @@ class BbftpClient < Formula
   desc "Secure file transfer software, optimized for large files"
   homepage "http://doc.in2p3.fr/bbftp/"
   url "http://doc.in2p3.fr/bbftp/dist/bbftp-client-3.2.1.tar.gz"
-  mirror "http://ftp.riken.jp/net/bbftp/bbftp-client-3.2.1.tar.gz"
+  mirror "https://ftp.riken.jp/net/bbftp/bbftp-client-3.2.1.tar.gz"
   mirror "https://dl.bintray.com/homebrew/mirror/bbftp-client-3.2.1.tar.gz"
   sha256 "4000009804d90926ad3c0e770099874084fb49013e8b0770b82678462304456d"
   revision 1
