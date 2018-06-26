b377d67572e237029b678d96c72c33ca5b51f19c
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-26 10:50:41 +0000

    feh: update 2.26.4 bottle.

--- Formula/feh.rb
+++ Formula/feh.rb
@@ -5,9 +5,9 @@ class Feh < Formula
   sha256 "074f8527a17fc5add70018a7f3887d78d5bdf545611636b88641f27e9e844795"
 
   bottle do
-    sha256 "5437b331014daf075535ec5b3c59c350c3b91bb00a89e132360ef3e062213b3e" => :high_sierra
-    sha256 "f7f601e8cabce54efa69b98f40f3f0f4cce35d2736728a2b211edb55b7a5b0a7" => :sierra
-    sha256 "bce2f11ed546d827d04c7d449db6cedb631d9ae67e81d55187722e0a784591b4" => :el_capitan
+    sha256 "032ece9317e826add619ca79c213cdc8c736c3452493c9cce01232ddc496c091" => :high_sierra
+    sha256 "7c15b09949d13f2f68e44484d6337265dd5d6e19fd5f0610cadc35ac0dfc2a5b" => :sierra
+    sha256 "437b568cef2b39fc8a7e40d7c238ed0aac5addcea982cb6cb8ad634aadf77628" => :el_capitan
   end
 
   depends_on :x11
