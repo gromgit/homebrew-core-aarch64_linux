0d66e63050a39fde704587bc403f3f9576a69ab5
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-27 13:43:57 +0000

    openh264: update 1.8.0 bottle.

--- Formula/openh264.rb
+++ Formula/openh264.rb
@@ -7,10 +7,9 @@ class Openh264 < Formula
 
   bottle do
     cellar :any
-    sha256 "140ea346ab069463ad1c4c588d12636413d4459a0f5c30a891b1cec7273cdf06" => :high_sierra
-    sha256 "d3daaa5995cb2ad25fb3a4e0a9e99de2c4ca5c98f3da4f455b5e58d04aa3e6b2" => :sierra
-    sha256 "1b9c6a03cd9dfe776d1de7c23e1b210f25bbeb1b99e4a12d1f4b8a354498a3ab" => :el_capitan
-    sha256 "28303efdce99fbcc37ee2045398ea060170e6690936acfb8c2f4bb1378dac079" => :yosemite
+    sha256 "2af5e6f5ef4dc7dcce2a420c7ed12ed06a252397ee0c24867a738c1b26ca73c5" => :high_sierra
+    sha256 "a74aaa0c2ead93b8fbc31c6fd031be257188cd012583311541a2bc8b66dda4b2" => :sierra
+    sha256 "ee2db0d05f6f3897fa6ece63a509ac1fffb8f33c7b02f8872af20e69b0e679b0" => :el_capitan
   end
 
   depends_on "nasm" => :build
