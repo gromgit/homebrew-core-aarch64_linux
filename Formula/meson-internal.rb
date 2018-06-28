91252d5b535e497fe7431f2fa07aacbb0563f968
Aleks Bunin <github@compuix.com>
2018-06-27 21:47:18 -0400

    meson-internal: revision for python

--- Formula/meson-internal.rb
+++ Formula/meson-internal.rb
@@ -4,6 +4,7 @@ class MesonInternal < Formula
   homepage "https://mesonbuild.com/"
   url "https://github.com/mesonbuild/meson/releases/download/0.45.1/meson-0.45.1.tar.gz"
   sha256 "4d0bb0dbb1bb556cb7a4092fdfea3d6e76606bd739a4bc97481c2d7bc6200afb"
+  revision 1
   head "https://github.com/mesonbuild/meson.git"
 
   bottle do
