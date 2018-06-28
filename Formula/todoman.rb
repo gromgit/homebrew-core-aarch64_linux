8a681db4985c0ad461797b19ce7e9868a2e7b8b4
Aleks Bunin <github@compuix.com>
2018-06-27 21:47:19 -0400

    todoman: revision for python

--- Formula/todoman.rb
+++ Formula/todoman.rb
@@ -5,6 +5,7 @@ class Todoman < Formula
   homepage "https://todoman.readthedocs.io/"
   url "https://files.pythonhosted.org/packages/bb/49/25511ca1f2762eda6cd95ccf1bd62c9ca5009469445b9263064a5ffbff78/todoman-3.4.0.tar.gz"
   sha256 "dc01585fda6ee492ee41b6db676c365f9c118af6fcda9e97ce517c9e9b0b8424"
+  revision 1
   head "https://github.com/pimutils/todoman.git"
 
   bottle do
