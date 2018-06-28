1a861e822085209b96ae0727a51ed354b8be1344
Aleks Bunin <github@compuix.com>
2018-06-27 21:47:18 -0400

    py3cairo: revision for python

--- Formula/py3cairo.rb
+++ Formula/py3cairo.rb
@@ -3,6 +3,7 @@ class Py3cairo < Formula
   homepage "https://cairographics.org/pycairo/"
   url "https://github.com/pygobject/pycairo/releases/download/v1.17.0/pycairo-1.17.0.tar.gz"
   sha256 "cdd4d1d357325dec3a21720b85d273408ef83da5f15c184f2eff3212ff236b9f"
+  revision 1
 
   bottle do
     cellar :any
