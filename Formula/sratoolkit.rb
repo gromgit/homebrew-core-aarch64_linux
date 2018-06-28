e5fa92ed505befc319586f064b401a06ed370668
ilovezfs <ilovezfs@icloud.com>
2018-06-27 23:46:11 -0700

    sratoolkit 2.9.1-1
    Closes #29506.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/sratoolkit.rb
+++ Formula/sratoolkit.rb
@@ -1,8 +1,9 @@
 class Sratoolkit < Formula
   desc "Data tools for INSDC Sequence Read Archive"
   homepage "https://github.com/ncbi/sra-tools"
-  url "https://github.com/ncbi/sra-tools/archive/2.9.1.tar.gz"
-  sha256 "61f35be7e26cb538a93b8e7e0afd6bc593e97333638dc0de176a6a828b9b504e"
+  url "https://github.com/ncbi/sra-tools/archive/2.9.1-1.tar.gz"
+  version "2.9.1-1"
+  sha256 "561dea9bb00f76a6f9a65cb20d6794379353d02a9e33b0db4f10eb38a42465db"
   head "https://github.com/ncbi/sra-tools.git"
 
   bottle do
@@ -21,8 +22,9 @@ class Sratoolkit < Formula
   end
 
   resource "ncbi-vdb" do
-    url "https://github.com/ncbi/ncbi-vdb/archive/2.9.1.tar.gz"
-    sha256 "ef55c5dac0e9f0f05cfff6c9e56061b72aa034153895990bf2b7daaba8f06739"
+    url "https://github.com/ncbi/ncbi-vdb/archive/2.9.1-1.tar.gz"
+    version "2.9.1-1"
+    sha256 "c1ee7443599d2cedab13eb44af0a2d29a4c3e8bf0130c2a6b34a9aa7016287e4"
   end
 
   def install
