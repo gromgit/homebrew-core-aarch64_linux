cbbf804d26d793ce28efd79567f18177c4f66b46
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-28 08:09:27 +0000

    container-diff: update 0.11.0 bottle.

--- Formula/container-diff.rb
+++ Formula/container-diff.rb
@@ -6,9 +6,9 @@ class ContainerDiff < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "755d9916daee6ca4896632b469c81ec81a5499442170832b27be48e9e5827512" => :high_sierra
-    sha256 "8ca23f36cae8664496b98ae89b76f02a30845a524712a5f4ec0781571a803b84" => :sierra
-    sha256 "f18a40fbbe48824f1316343fc0a00372ba002a20ca4ffa68f0b0e7264d23ce70" => :el_capitan
+    sha256 "c47685c267cbe10f383dd09f7b0435d54a84684ef52ef81c4f917a32ca4656bc" => :high_sierra
+    sha256 "97e074a8be3cf34f6bf4271fe16817701096df34e2a541d47758edb5e5ee788e" => :sierra
+    sha256 "fe16c0f1a6f78a2488782d09321d1b70765c6192bf56afd483f0f22c47845b5a" => :el_capitan
   end
 
   depends_on "go" => :build
