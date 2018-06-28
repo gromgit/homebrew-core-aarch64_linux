2380fb94054901dec912398f33db9cc501beeb7b
Aleks Bunin <github@compuix.com>
2018-06-27 21:47:18 -0400

    meson: revision for python

--- Formula/meson.rb
+++ Formula/meson.rb
@@ -3,6 +3,7 @@ class Meson < Formula
   homepage "https://mesonbuild.com/"
   url "https://github.com/mesonbuild/meson/releases/download/0.46.1/meson-0.46.1.tar.gz"
   sha256 "19497a03e7e5b303d8d11f98789a79aba59b5ad4a81bd00f4d099be0212cee78"
+  revision 1
   head "https://github.com/mesonbuild/meson.git"
 
   bottle do
