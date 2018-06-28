bcf4af698bfa328132bedc11e51c8e30e5a5cd41
Aleks Bunin <github@compuix.com>
2018-06-27 21:47:16 -0400

    fades: revision for python

--- Formula/fades.rb
+++ Formula/fades.rb
@@ -3,6 +3,7 @@ class Fades < Formula
   homepage "https://fades.readthedocs.org/"
   url "https://files.pythonhosted.org/packages/27/89/dd5b165986f4064d2a3285461e1a15fb7006793b453a7b2a3b518377b8f2/fades-7.0.tar.gz"
   sha256 "b91f2221f1ae8e9eae35f58dac279d2d3935e66de428e79e1509814392449dda"
+  revision 1
   head "https://github.com/PyAr/fades.git"
 
   bottle do
