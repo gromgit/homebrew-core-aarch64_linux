06fa8a2be1a61ce435196111ddb78f347bb378f3
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-28 13:50:43 +0000

    arangodb: update 3.3.11 bottle.

--- Formula/arangodb.rb
+++ Formula/arangodb.rb
@@ -6,9 +6,9 @@ class Arangodb < Formula
   head "https://github.com/arangodb/arangodb.git", :branch => "unstable"
 
   bottle do
-    sha256 "eb756d602a258ccf439d30058064eaaa9936d41b80d0338673ea4fa82e6bfd15" => :high_sierra
-    sha256 "b222d60afb8a83d001f9da59b59943bc568ebb083127b9b1ac4d84d205bd19cc" => :sierra
-    sha256 "81d57bea1d8e64d824067924609e6305e7cd46d630170206cb49d11d13cf897c" => :el_capitan
+    sha256 "b6427dee8e790a0bf2af8735061293efa13093bef1a992a7bf52835b7d1bd305" => :high_sierra
+    sha256 "cba6dab4e1e6d4b788c605590e1e901324805f64dc66504c8688fddaa205927a" => :sierra
+    sha256 "6047b3f19b4d296470a55d00e9c35002c9afcbcd747a66ca79d79ea5a9cf9e9c" => :el_capitan
   end
 
   depends_on :macos => :yosemite
