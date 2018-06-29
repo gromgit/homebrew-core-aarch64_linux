75fc8a4c6e6259256631a59918f5a38712001d99
ilovezfs <ilovezfs@icloud.com>
2018-06-28 23:48:04 -0700

    fn 0.4.116
    Closes #29544.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/fn.rb
+++ Formula/fn.rb
@@ -1,8 +1,8 @@
 class Fn < Formula
   desc "Command-line tool for the fn project"
   homepage "https://fnproject.github.io"
-  url "https://github.com/fnproject/cli/archive/0.4.115.tar.gz"
-  sha256 "124c41496682fbb7fe3bf61a5f61dffdfa09e078f5e159268acf4f9773623a2d"
+  url "https://github.com/fnproject/cli/archive/0.4.116.tar.gz"
+  sha256 "25dda9e223ae7dd3a1ddc29d0d69069f9997cd8234692e24d09ed7d6a9842931"
 
   bottle do
     cellar :any_skip_relocation
