14e646289478eb22409a466732178863b7904a4d
ilovezfs <ilovezfs@icloud.com>
2018-06-28 23:48:53 -0700

    gopass 1.8.2
    Closes #29545.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gopass.rb
+++ Formula/gopass.rb
@@ -1,8 +1,8 @@
 class Gopass < Formula
   desc "The slightly more awesome Standard Unix Password Manager for Teams"
   homepage "https://www.justwatch.com/gopass"
-  url "https://github.com/gopasspw/gopass/releases/download/v1.8.1/gopass-1.8.1.tar.gz"
-  sha256 "0234ad374925e13f8e59b9a63820819e69ba186b4d7512ed4fbc9093a2dc6f7b"
+  url "https://github.com/gopasspw/gopass/releases/download/v1.8.2/gopass-1.8.2.tar.gz"
+  sha256 "ca6d24db014a760f71fbf0edaad0c97c283a670b260275a78eda3a7456fbea93"
   head "https://github.com/gopasspw/gopass.git"
 
   bottle do
