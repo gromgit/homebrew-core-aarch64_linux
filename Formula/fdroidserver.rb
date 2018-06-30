45e175f111c43600ac421e08f4a9a5bce2a3e6bd
ilovezfs <ilovezfs@icloud.com>
2018-06-30 06:38:15 -0700

    fdroidserver: update jedi and parso resources
    remove jedi resource patch
    
    Closes #29599.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/fdroidserver.rb
+++ Formula/fdroidserver.rb
@@ -139,14 +139,8 @@ class Fdroidserver < Formula
   end
 
   resource "jedi" do
-    url "https://files.pythonhosted.org/packages/ff/c9/781449489b743c67ad063e33aa68139afaa8a1a5bc348eee9f5cab39b4e1/jedi-0.12.0.tar.gz"
-    sha256 "1972f694c6bc66a2fac8718299e2ab73011d653a6d8059790c3476d2353b99ad"
-
-    # https://github.com/davidhalter/jedi/pull/1143
-    patch do
-      url "https://github.com/davidhalter/jedi/commit/ff4a77391.patch?full_index=1"
-      sha256 "f3f0df1dfeecfbe48c559dcdbd4500013571dcc384074fc94cfcc27dbdfe41d2"
-    end
+    url "https://files.pythonhosted.org/packages/49/2f/cdfb8adc8cfc9fc2e5673e724d9b9098619dc1a2772cc6b8af34c6b7bef9/jedi-0.12.1.tar.gz"
+    sha256 "b409ed0f6913a701ed474a614a3bb46e6953639033e31f769ca7581da5bd1ec1"
   end
 
   resource "kiwisolver" do
@@ -190,8 +184,8 @@ class Fdroidserver < Formula
   end
 
   resource "parso" do
-    url "https://files.pythonhosted.org/packages/4b/86/bc857ddcc892968b954620f18ce90a943368ee9fe84595911501da898798/parso-0.2.1.tar.gz"
-    sha256 "f0604a40b96e062b0fd99cf134cc2d5cdf66939d0902f8267d938b0d5b26707f"
+    url "https://files.pythonhosted.org/packages/29/c1/fd8a3e5eec85bf160c2b1ea369fdfa585620cf753db021d5db895801e701/parso-0.3.0.tar.gz"
+    sha256 "d250235e52e8f9fc5a80cc2a5f804c9fefd886b2e67a2b1099cf085f403f8e33"
   end
 
   resource "pexpect" do
