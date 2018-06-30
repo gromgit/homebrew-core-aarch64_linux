2abdeaecc47ca3356c5390132e5e6b2dd93ebec4
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-30 11:43:03 +0000

    fswatch: update 1.12.0 bottle.

--- Formula/fswatch.rb
+++ Formula/fswatch.rb
@@ -6,9 +6,9 @@ class Fswatch < Formula
 
   bottle do
     cellar :any
-    sha256 "542ed7f8f6b66806524a6ddfab9c9be79d98d741a0cae229bb8e76829f17f954" => :high_sierra
-    sha256 "772fe9d50aff488e4cbf24af9d15c0f1fafe6629abb12d107d850cf4c751264c" => :sierra
-    sha256 "087d3325d1faf25c8f1b25920a25d169c255bc17ee357e4734c2b999139bc0ce" => :el_capitan
+    sha256 "8575f86b8f91844ed193cbd0b76a0f391e2e7e57bf3e00f37f800fb6a4847ed2" => :high_sierra
+    sha256 "058f1aa3b1ce82e8fb35a5893e4d639992e5a2ffe0e10780e4ea165a5394f5a7" => :sierra
+    sha256 "8338851b10412b958318c6582654ff092ca04af3dff83c5abaa14284c4b4f8de" => :el_capitan
   end
 
   needs :cxx11
