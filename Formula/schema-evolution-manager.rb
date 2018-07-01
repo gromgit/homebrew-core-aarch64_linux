87e3111c55e6ed82e611d35ffcc682203e89fae9
Gregor Heine <gheine@users.noreply.github.com>
2018-07-01 18:05:42 +0200

    schema-evolution-manager 0.9.40 (new formula) (#28816)

---
+++ Formula/schema-evolution-manager.rb
@@ -0,0 +1,20 @@
+class SchemaEvolutionManager < Formula
+  desc "Manage postgresql database schema migrations"
+  homepage "https://github.com/mbryzek/schema-evolution-manager"
+  url "https://github.com/mbryzek/schema-evolution-manager/archive/0.9.40.tar.gz"
+  sha256 "cf5aab3f2fe81e3daca400811f46a11d4ebd25d7325eca92a83dbe0494e25d5a"
+
+  bottle :unneeded
+
+  def install
+    system "./install.sh", prefix
+  end
+
+  test do
+    (testpath/"new.sql").write <<~EOS
+      CREATE TABLE IF NOT EXISTS test (id text);
+    EOS
+    system "git", "init", "."
+    assert_match "File staged in git", shell_output("#{bin}/sem-add ./new.sql")
+  end
+end
