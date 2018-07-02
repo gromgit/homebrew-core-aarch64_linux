fa1cbd54aa39df295861c06ea58bb65e805b740c
BrewTestBot <brew-test-bot@googlegroups.com>
2018-07-02 07:49:40 +0000

    wandio: update 1.0.5 bottle.

--- Formula/wandio.rb
+++ Formula/wandio.rb
@@ -6,9 +6,9 @@ class Wandio < Formula
 
   bottle do
     cellar :any
-    sha256 "b2f80ac0b72c74b74529e7d9dc8e7a9d5ec130e0405c4a9316993c9df44b4bc1" => :high_sierra
-    sha256 "18377dd9738246878e3357ea95e0164e7ec2f66eab9708b662a5961678355873" => :sierra
-    sha256 "a05a9f0b56ccb80fd8de1cd2d3921a08599b88bcd9e9770df81a5fddf2163d24" => :el_capitan
+    sha256 "e60bcff18c497ea0e9466faf177850726847098c6d0a54d6366c540e7724f478" => :high_sierra
+    sha256 "caed8a09bce36cd9f5ccb4682daf2425fef364d835febee57a8becab1fcb284d" => :sierra
+    sha256 "ccf0bf929eb54000fb6b6603ac853be4f4cb8c3579fe07830b7e3001b4d795e7" => :el_capitan
   end
 
   def install
