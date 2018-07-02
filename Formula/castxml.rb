1af8e4c331ae1997721d8158d9516712b27be64d
ilovezfs <ilovezfs@icloud.com>
2018-07-01 22:52:28 -0700

    castxml 0.1+git20180702
    depend on llvm at build time instead of llvm@5
    
    Closes #29636.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/castxml.rb
+++ Formula/castxml.rb
@@ -1,11 +1,10 @@
 class Castxml < Formula
   desc "C-family Abstract Syntax Tree XML Output"
   homepage "https://github.com/CastXML/CastXML"
-  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/c/castxml/castxml_0.1+git20170823.orig.tar.xz"
-  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/c/castxml/castxml_0.1+git20170823.orig.tar.xz"
-  version "0.1+git20170823"
-  sha256 "aa10c17f703ef46a88f9772205d8f51285fd3567aa91931ee1a7a5abfff95b11"
-  revision 2
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/c/castxml/castxml_0.1+git20180702.orig.tar.xz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/c/castxml/castxml_0.1+git20180702.orig.tar.xz"
+  version "0.1+git20180702"
+  sha256 "00c58b28523496fbeda731656bb022ad55e7c390609f189ebe03b4468292da40"
   head "https://github.com/CastXML/castxml.git"
 
   bottle do
@@ -16,7 +15,7 @@ class Castxml < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "llvm@5"
+  depends_on "llvm"
 
   def install
     mkdir "build" do
