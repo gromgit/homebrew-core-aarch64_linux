c213e57dc1817ebdc0e4c69f6dc27387d8a7789a
ilovezfs <ilovezfs@icloud.com>
2018-07-02 21:44:04 -0700

    firebase-cli 3.19.2
    Closes #29671.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class FirebaseCli < Formula
   desc "Firebase command-line tools"
   homepage "https://firebase.google.com/docs/cli/"
-  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-3.19.1.tgz"
-  sha256 "0d39a4971b7da3a3b5f06199e28c2ec4ad8baea5c8bbdc247b8f3ae4bc47d534"
+  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-3.19.2.tgz"
+  sha256 "3390e16996bde30df2aa680e983f2178493da9f2668d4f862bcda81c0406d549"
   head "https://github.com/firebase/firebase-tools.git"
 
   bottle do
