e6f567aa8b358837e3584fa20aa0956cd147d502
ilovezfs <ilovezfs@icloud.com>
2018-07-03 21:57:21 -0700

    dub 1.10.0
    Closes #29722.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/dub.rb
+++ Formula/dub.rb
@@ -1,8 +1,8 @@
 class Dub < Formula
   desc "Build tool for D projects"
   homepage "https://code.dlang.org/getting_started"
-  url "https://github.com/dlang/dub/archive/v1.9.0.tar.gz"
-  sha256 "48f7387e93977d0ece686106c9725add2c4f5f36250da33eaa0dbb66900f9d57"
+  url "https://github.com/dlang/dub/archive/v1.10.0.tar.gz"
+  sha256 "db6c3a9e45408d2431bc3d1138a31b561fb71665c1d89db4b0cb3725b2b12faa"
   version_scheme 1
   head "https://github.com/dlang/dub.git"
 
@@ -10,11 +10,6 @@ class Dub < Formula
     sha256 "d2793e9348aa4acd0d9a5ce4497e8e5d8233809a2e7656e4f15f3bf3a6618d1d" => :high_sierra
     sha256 "a69c553d1148894029ee83a7a642b3774bd27860b49a07188b93e2ca7b58f7fd" => :sierra
     sha256 "e50dadec35b96a9ef9791ae670b482cb9ab3cc293c249aeab575ad4ce38db304" => :el_capitan
-  end
-
-  devel do
-    url "https://github.com/dlang/dub/archive/v1.10.0-rc.1.tar.gz"
-    sha256 "672f3027ac51381fa26146554cc7cbb5103271a4239a43e35b4ad7ef8ab70345"
   end
 
   depends_on "pkg-config" => :recommended
