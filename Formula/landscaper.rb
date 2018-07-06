261adf16e4e615e0caf1a073093692a06917792b
BrewTestBot <brew-test-bot@googlegroups.com>
2018-07-06 05:10:42 +0000

    landscaper: update 1.0.18 bottle.

--- Formula/landscaper.rb
+++ Formula/landscaper.rb
@@ -8,9 +8,9 @@ class Landscaper < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "39cb038165465ccef0aea6389b341cf01590b53482194143b2bddde0cd5df8d0" => :high_sierra
-    sha256 "9866d86d75adb68bcbec8ee0da54bae7da4052f6939f448f7a2f2063d85ee65a" => :sierra
-    sha256 "72b598e9a06b1501a7a9c5b57a8bd01dd59e70bec67a2a3efbbd9ab46abb8b7d" => :el_capitan
+    sha256 "e124d54f5f5775ccb6fdeb38708d4a5eda94bcc75c3b4d8fb72bb74b8e7f46a5" => :high_sierra
+    sha256 "56bd51ee0b2659fd7ececa51d09bd7a16cfc72f2fedb12bcb890c4eccaa9e4e9" => :sierra
+    sha256 "457910fab6350bd9155c49d76c9b7d150b9bdd29f8d0191a5a9088ec902a3bbe" => :el_capitan
   end
 
   depends_on "dep" => :build
