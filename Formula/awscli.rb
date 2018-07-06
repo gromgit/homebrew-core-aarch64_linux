a6dd32a2461f11a7884d3da6ed50c7f939675d99
ilovezfs <ilovezfs@icloud.com>
2018-07-05 22:36:48 -0700

    awscli: use PyYAML 3.13

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -22,7 +22,7 @@ class Awscli < Formula
   def install
     venv = virtualenv_create(libexec, "python3")
     system libexec/"bin/pip", "install", "-v", "--no-binary", ":all:",
-                              "--ignore-installed", "PyYAML==4.2b1", buildpath
+                              "--ignore-installed", "PyYAML==3.13", buildpath
     system libexec/"bin/pip", "uninstall", "-y", "awscli"
     venv.pip_install_and_link buildpath
     pkgshare.install "awscli/examples"
