a69a338c06fa2bd1272e1fc2cb5fff95a5b0b033
ilovezfs <ilovezfs@icloud.com>
2018-07-07 00:23:12 -0700

    libtrace: revision for wandio
    Closes #29833.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libtrace.rb
+++ Formula/libtrace.rb
@@ -3,6 +3,7 @@ class Libtrace < Formula
   homepage "https://research.wand.net.nz/software/libtrace.php"
   url "https://research.wand.net.nz/software/libtrace/libtrace-4.0.4.tar.bz2"
   sha256 "6099fad7b2b684e4eb716b1cb3fcac70baad5848e8643b0a39bade382a59acac"
+  revision 1
 
   bottle do
     sha256 "1a0ad845512f003e9ff6e5030dadd2b55068416b20ff6752afb138ecec34bb90" => :high_sierra
