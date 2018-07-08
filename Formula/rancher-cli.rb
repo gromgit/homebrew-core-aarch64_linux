a90262952a9778274beb85c6ab2dad2f0b69f929
ilovezfs <ilovezfs@icloud.com>
2018-07-07 21:54:12 -0700

    rancher-cli 2.0.3
    Closes #29850.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/rancher-cli.rb
+++ Formula/rancher-cli.rb
@@ -1,8 +1,8 @@
 class RancherCli < Formula
   desc "The Rancher CLI is a unified tool to manage your Rancher server"
   homepage "https://github.com/rancher/cli"
-  url "https://github.com/rancher/cli/archive/v2.0.2.tar.gz"
-  sha256 "13ac7fa1ad4d25ea283f92ed6f2a1bd772a11a4644727d50a275ae496b965efb"
+  url "https://github.com/rancher/cli/archive/v2.0.3.tar.gz"
+  sha256 "f806581989e96d13c18813629b3582684ee0ceeb8f4d8cee94b0b78be1c8a67f"
 
   bottle do
     cellar :any_skip_relocation
