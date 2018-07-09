a70cad368bc5a14be037d24018ec9114d6b32f90
ilovezfs <ilovezfs@icloud.com>
2018-07-09 13:57:38 +0100

    jpeg: mirror to Bintray and use Bintray as primary (#29885)
    also add fossies mirror

--- Formula/jpeg.rb
+++ Formula/jpeg.rb
@@ -1,7 +1,9 @@
 class Jpeg < Formula
   desc "Image manipulation library"
   homepage "http://www.ijg.org"
-  url "http://www.ijg.org/files/jpegsrc.v9c.tar.gz"
+  # http://www.ijg.org/files/jpegsrc.v9c.tar.gz is temporarily unavailable
+  url "https://dl.bintray.com/homebrew/mirror/jpeg-9c.tar.gz"
+  mirror "https://fossies.org/linux/misc/jpegsrc.v9c.tar.gz"
   sha256 "650250979303a649e21f87b5ccd02672af1ea6954b911342ea491f351ceb7122"
 
   bottle do
