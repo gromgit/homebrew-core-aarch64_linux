a9cb0d0006e8def377028d5739838666f46a8755
ilovezfs <ilovezfs@icloud.com>
2018-07-09 22:02:07 -0700

    libuv 1.22.0
    Closes #29909.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/libuv.rb
+++ Formula/libuv.rb
@@ -1,8 +1,8 @@
 class Libuv < Formula
   desc "Multi-platform support library with a focus on asynchronous I/O"
   homepage "https://github.com/libuv/libuv"
-  url "https://github.com/libuv/libuv/archive/v1.21.0.tar.gz"
-  sha256 "c6f53e4a75d2601db8efe0e837839969de7667d36569110429a0bc8f8229bf59"
+  url "https://github.com/libuv/libuv/archive/v1.22.0.tar.gz"
+  sha256 "c72f0401e1298f948daf310abafce38373a926fb9438464d1bf7fe90784544e0"
   head "https://github.com/libuv/libuv.git", :branch => "v1.x"
 
   bottle do
