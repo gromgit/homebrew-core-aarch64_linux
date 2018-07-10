ddf45f91346e5514d74059548393b2683f04b608
BrewTestBot <brew-test-bot@googlegroups.com>
2018-07-10 06:42:59 +0000

    math-comp: update 1.7.0_2 bottle.

--- Formula/math-comp.rb
+++ Formula/math-comp.rb
@@ -8,9 +8,9 @@ class MathComp < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b1a765625508585ca3768544628b9a71c9c92e8509f3e8a1019ee56c5691d0a0" => :high_sierra
-    sha256 "121eaa1a5669511683adf613241dcf2460b75366afb52b3c7f4844b16a10ac10" => :sierra
-    sha256 "6634ee5946a464872dec8a580c4dfcf7431af864e4629d333ad62298f4ef8fd9" => :el_capitan
+    sha256 "88e40a31f5283eb0294b1aa8a9eec3d65dd6bdf1063b8430db6bd88130d85ec6" => :high_sierra
+    sha256 "3549c73d20721800376ef55f291ec4ed02c7168832bf096683d8301336aa387a" => :sierra
+    sha256 "50c23dadbf291c8c82af2a2ae9a109dc4266d37e985bf1b90d766e518fa776ad" => :el_capitan
   end
 
   depends_on "ocaml" => :build
