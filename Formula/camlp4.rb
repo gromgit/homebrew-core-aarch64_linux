b06793143d41585d0609dcf9162a7a0b7caf342b
ilovezfs <ilovezfs@icloud.com>
2018-07-11 05:40:59 -0700

    camlp4 4.07+1
    Closes #29978.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/camlp4.rb
+++ Formula/camlp4.rb
@@ -1,20 +1,10 @@
 class Camlp4 < Formula
   desc "Tool to write extensible parsers in OCaml"
   homepage "https://github.com/ocaml/camlp4"
-  revision 1
+  url "https://github.com/ocaml/camlp4/archive/4.07+1.tar.gz"
+  version "4.07+1"
+  sha256 "ecdb8963063f41b387412317685f79823a26b3f53744f0472058991876877090"
   head "https://github.com/ocaml/camlp4.git", :branch => "trunk"
-
-  stable do
-    url "https://github.com/ocaml/camlp4/archive/4.06+1.tar.gz"
-    version "4.06+1"
-    sha256 "b1cc51449da0537f6886e380815b716e7adc9d9a12d6098a06db2b2525bab922"
-
-    # Remove for > 4.06+1
-    patch do
-      url "https://github.com/ocaml/camlp4/commit/aa57573.patch?full_index=1"
-      sha256 "201d3b8ae1dc983679ad0ef93d6742bcd4453a3719288f6a738cf6c492fd694d"
-    end
-  end
 
   bottle do
     cellar :any_skip_relocation
