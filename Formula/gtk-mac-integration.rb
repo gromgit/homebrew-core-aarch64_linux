64a8b34f949a2701ee96c54693272efb7be75542
Mitsutoshi Aoe <maoe@foldr.in>
2018-07-11 23:24:41 +0900

    gtk-mac-integration: add patches and head
    Closes #29979.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/gtk-mac-integration.rb
+++ Formula/gtk-mac-integration.rb
@@ -1,13 +1,35 @@
 class GtkMacIntegration < Formula
   desc "Integrates GTK macOS applications with the Mac desktop"
   homepage "https://wiki.gnome.org/Projects/GTK+/OSX/Integration"
-  url "https://download.gnome.org/sources/gtk-mac-integration/2.1/gtk-mac-integration-2.1.2.tar.xz"
-  sha256 "68e682a3ba952e7d4b1cfa2c7147c5fcd76f8bd9792a567e175a619af5954af1"
+
+  stable do
+    url "https://download.gnome.org/sources/gtk-mac-integration/2.1/gtk-mac-integration-2.1.2.tar.xz"
+    sha256 "68e682a3ba952e7d4b1cfa2c7147c5fcd76f8bd9792a567e175a619af5954af1"
+
+    patch do
+      url "https://github.com/jralls/gtk-mac-integration/pull/11.patch?full_index=1"
+      sha256 "4523207ea652b9048d01a58c05369c871def4e187db267ab7bad85ae1e102c31"
+    end
+
+    patch do
+      url "https://github.com/jralls/gtk-mac-integration/pull/13.patch?full_index=1"
+      sha256 "b0ecfb9a3c5cd9651584b33191b69915627cc9e09e78fb4623fcaa64915ee13d"
+    end
+  end
 
   bottle do
     sha256 "7c6926dc885b7c031398552069dc16bd63d2fe938d281d3f18a90a80de48ce16" => :high_sierra
     sha256 "5856dcaf405cffb88541a0d9918285603978c334284e1cff41c9d0d08801936c" => :sierra
     sha256 "23213e9b20f36cc8c95a344064e29968cd3dbca73f1d433c25636b6056e357ac" => :el_capitan
+  end
+
+  head do
+    url "https://github.com/jralls/gtk-mac-integration.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "gtk-doc" => :build
+    depends_on "libtool" => :build
   end
 
   depends_on "gobject-introspection" => :build
@@ -28,7 +50,11 @@ class GtkMacIntegration < Formula
     ]
 
     args << (build.without?("gtk+3") ? "--without-gtk3" : "--with-gtk3")
-    system "./configure", *args
+    if build.head?
+      system "./autogen.sh", *args
+    else
+      system "./configure", *args
+    end
     system "make", "install"
   end
 
