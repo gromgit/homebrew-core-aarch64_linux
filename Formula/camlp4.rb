ca8b8b8173e41d9d81c1da256525bc9dcaa70337
JCount <JCount42@gmail.com>
2018-07-11 00:41:07 -0400

    camlp4: revision for ocaml
    patch for OCaml 4.07

--- Formula/camlp4.rb
+++ Formula/camlp4.rb
@@ -1,10 +1,20 @@
 class Camlp4 < Formula
   desc "Tool to write extensible parsers in OCaml"
   homepage "https://github.com/ocaml/camlp4"
-  url "https://github.com/ocaml/camlp4/archive/4.06+1.tar.gz"
-  version "4.06+1"
-  sha256 "b1cc51449da0537f6886e380815b716e7adc9d9a12d6098a06db2b2525bab922"
+  revision 1
   head "https://github.com/ocaml/camlp4.git", :branch => "trunk"
+
+  stable do
+    url "https://github.com/ocaml/camlp4/archive/4.06+1.tar.gz"
+    version "4.06+1"
+    sha256 "b1cc51449da0537f6886e380815b716e7adc9d9a12d6098a06db2b2525bab922"
+
+    # Remove for > 4.06+1
+    patch do
+      url "https://github.com/ocaml/camlp4/commit/aa57573.patch?full_index=1"
+      sha256 "201d3b8ae1dc983679ad0ef93d6742bcd4453a3719288f6a738cf6c492fd694d"
+    end
+  end
 
   bottle do
     cellar :any_skip_relocation
