04166f513d3b5c2649e8ccbc839194fee739ccb3
ilovezfs <ilovezfs@icloud.com>
2018-07-11 01:42:36 -0700

    lynx 2.8.9rel.1
    Closes #29967.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/lynx.rb
+++ Formula/lynx.rb
@@ -2,7 +2,7 @@ class Lynx < Formula
   desc "Text-based web browser"
   homepage "https://invisible-island.net/lynx/"
   url "https://invisible-mirror.net/archives/lynx/tarballs/lynx2.8.9rel.1.tar.bz2"
-  version "2.8.9"
+  version "2.8.9rel.1"
   sha256 "387f193d7792f9cfada14c60b0e5c0bff18f227d9257a39483e14fa1aaf79595"
 
   bottle do
