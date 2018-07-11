1fd177a0c321980ba7e1ae5e5f9f0a325df6c4f0
ilovezfs <ilovezfs@icloud.com>
2018-07-11 10:53:38 +0100

    igv 2.4.12 (#29948)
    require Java 8 specifically

--- Formula/igv.rb
+++ Formula/igv.rb
@@ -1,17 +1,17 @@
 class Igv < Formula
   desc "Interactive Genomics Viewer"
   homepage "https://www.broadinstitute.org/software/igv"
-  url "https://data.broadinstitute.org/igv/projects/downloads/2.4/IGV_2.4.10.zip"
-  sha256 "c21981dac516c01e2309a7b06474fd896c5018fb774438d0bad15403bc1ddbf3"
+  url "https://data.broadinstitute.org/igv/projects/downloads/2.4/IGV_2.4.12.zip"
+  sha256 "835e91c475af288a0883fcad93f18c4ba13d41125fa5910e4506ffafea9f69e2"
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on :java => "1.8"
 
   def install
     inreplace "igv.sh", /^prefix=.*/, "prefix=#{libexec}"
     libexec.install "igv.sh", Dir["*.jar"]
-    bin.install_symlink libexec/"igv.sh" => "igv"
+    (bin/"igv").write_env_script libexec/"igv.sh", Language::Java.java_home_env("1.8")
   end
 
   test do
