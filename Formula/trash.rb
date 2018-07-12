31a62dd09f48d83e526601e2cec8be4d9c298cf5
Nick Gravelyn <nick@gravelyn.com>
2018-07-12 00:52:23 -0700

    trash 0.9.2
    Closes #29787.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/trash.rb
+++ Formula/trash.rb
@@ -1,8 +1,8 @@
 class Trash < Formula
   desc "CLI tool that moves files or folder to the trash"
   homepage "https://hasseg.org/trash/"
-  url "https://github.com/ali-rantakari/trash/archive/v0.8.5.tar.gz"
-  sha256 "1e08fdcdeaa216be1aee7bf357295943388d81e62c2c68c30c830ce5c43aae99"
+  url "https://github.com/ali-rantakari/trash/archive/v0.9.2.tar.gz"
+  sha256 "e8739c93d710ac4da721e16878e7693019d3a2ad7d8acd817f41426601610083"
   head "https://github.com/ali-rantakari/trash.git"
 
   bottle do
