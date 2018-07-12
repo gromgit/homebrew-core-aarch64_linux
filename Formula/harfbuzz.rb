0dbc7f1d4f7cca9de0c3eae5d9436599a021a95b
ilovezfs <ilovezfs@icloud.com>
2018-07-11 21:48:36 -0700

    harfbuzz 1.8.3
    Closes #29990.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/harfbuzz.rb
+++ Formula/harfbuzz.rb
@@ -1,8 +1,8 @@
 class Harfbuzz < Formula
   desc "OpenType text shaping engine"
   homepage "https://wiki.freedesktop.org/www/Software/HarfBuzz/"
-  url "https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-1.8.2.tar.bz2"
-  sha256 "c8a3f88f416185a82f899a68b5714d6a40d7dc4d1d7379057348918d55aac657"
+  url "https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-1.8.3.tar.bz2"
+  sha256 "2ac2ad1ca26253bfe7f437adcd034c84dad0e7c2c0fe008d6051b754fa987a9e"
 
   bottle do
     sha256 "1a4ac71fdedb66d25edf080a5ae642646b4582642445f6f3a335990aaa92850b" => :high_sierra
