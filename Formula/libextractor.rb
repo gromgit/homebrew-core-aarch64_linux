e34054a1cfcf33894615cc33ee5ed1c850d00aad
BrewTestBot <brew-test-bot@googlegroups.com>
2018-07-13 04:45:29 +0000

    libextractor: update 1.7 bottle.

--- Formula/libextractor.rb
+++ Formula/libextractor.rb
@@ -6,9 +6,9 @@ class Libextractor < Formula
   sha256 "e0a6fde824cf2212c4f217a5e0fc03391251cfb46ca000117f66cf7ae4368e8f"
 
   bottle do
-    sha256 "d547400beebeae3290adfa6d63add9b9c722d4dd65533e77a4be0b8bf315b8c5" => :high_sierra
-    sha256 "af73939dbc95fb9f2ed2bfcbf8217cff2b07a9f0716cfedbaaa57c03321f3d16" => :sierra
-    sha256 "10267b6dd613e42a5335b2ed4b06f47319cfbfcee2a4455f32fe216d9cbd630d" => :el_capitan
+    sha256 "85a1c132a16157d6d5b4836591b4a2921c7cc15a7a7facc3c33653e183530765" => :high_sierra
+    sha256 "e371b24c935eb79d24edd5a9a65d2865dee6219c3e5c4f2f65b40de703b8ee00" => :sierra
+    sha256 "8b59442f5b46f6dea1e1e5df34943db009dcf017b1e47f2fe3cc45b0384a5c40" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
