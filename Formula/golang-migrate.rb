cb71b547926fd225f44c3051b6ea3429278a96f5
ahxxm <i@ahxxm.com>
2018-07-13 15:13:45 +0800

    golang-migrate 3.4.0 (new formula)
    Closes #31743.
    
    Signed-off-by: Claudia <claui@users.noreply.github.com>

---
+++ Formula/golang-migrate.rb
@@ -0,0 +1,28 @@
+class GolangMigrate < Formula
+  desc "Database migrations CLI tool"
+  homepage "https://github.com/golang-migrate/migrate"
+  url "https://github.com/golang-migrate/migrate/archive/v3.4.0.tar.gz"
+  sha256 "7a0789bf611d1efda95528cc581495fd9590cfa25d1dfc0be9e33eaf4c62e33e"
+
+  depends_on "dep" => :build
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/golang-migrate/migrate").install buildpath.children
+
+    # Build and install CLI as "migrate"
+    cd "src/github.com/golang-migrate/migrate" do
+      system "dep", "ensure", "-vendor-only"
+      system "make", "build-cli", "VERSION=v#{version}"
+      bin.install "cli/build/migrate.darwin-amd64" => "migrate"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    touch "0001_migtest.up.sql"
+    output = shell_output("#{bin}/migrate -database stub: -path . up 2>&1")
+    assert_match "1/u migtest", output
+  end
+end
