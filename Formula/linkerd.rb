fa082e786d17d45200d336958cde5e0dbc8f67c8
ilovezfs <ilovezfs@icloud.com>
2018-07-14 08:58:46 +0100

    linkerd 1.4.5 (#30070)

--- Formula/linkerd.rb
+++ Formula/linkerd.rb
@@ -1,8 +1,8 @@
 class Linkerd < Formula
   desc "Drop-in RPC proxy designed for microservices"
   homepage "https://linkerd.io/"
-  url "https://github.com/linkerd/linkerd/releases/download/1.4.4/linkerd-1.4.4.tgz"
-  sha256 "e5c0973d09281cad28f6f2c13f778dd67f888f7f31a176384de0cdfa240204d9"
+  url "https://github.com/linkerd/linkerd/releases/download/1.4.5/linkerd-1.4.5.tgz"
+  sha256 "c78de1c5908f2e8bd0378eec2496b9d8a93b335568cc9c5267b01ae82cbacaf7"
 
   bottle :unneeded
 
