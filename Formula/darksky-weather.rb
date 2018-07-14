e582b4de2f29c2e127656d463fefead1947abbee
BrewTestBot <brew-test-bot@googlegroups.com>
2018-07-14 20:48:45 +0000

    darksky-weather: update 0.15.4 bottle.

--- Formula/darksky-weather.rb
+++ Formula/darksky-weather.rb
@@ -6,9 +6,9 @@ class DarkskyWeather < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "4715a19399501e6418096418cae5c6798de81f397333fde968828604fb48d281" => :high_sierra
-    sha256 "d17d8161f0e29ec3431dd72740352483ee0d80c7b168e9e8b58382ea69687923" => :sierra
-    sha256 "cad678d22987a1143a9633d219c103de85b5ab84110bcb2fc1f21e57f8f4a9c3" => :el_capitan
+    sha256 "4dfe8972a8944752362a8432caa3ec5d3c519bd06af1d82285c23df68b43bdcc" => :high_sierra
+    sha256 "5c680cb4606c5807b5fab4f885f8f995659c449d9c52adf74ccd90f452eb5af9" => :sierra
+    sha256 "d8e6d2cd8d298f845f6f706c911f1a1a3c564f441e0a425e12f42d2c9f16e45a" => :el_capitan
   end
 
   depends_on "go" => :build
