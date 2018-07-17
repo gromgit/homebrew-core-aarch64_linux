6b4acc18f26284018c7cdf860e051dac8e1e8da8
ilovezfs <ilovezfs@icloud.com>
2018-07-17 02:19:15 -0700

    net-snmp 5.8

--- Formula/net-snmp.rb
+++ Formula/net-snmp.rb
@@ -1,8 +1,8 @@
 class NetSnmp < Formula
   desc "Implements SNMP v1, v2c, and v3, using IPv4 and IPv6"
   homepage "http://www.net-snmp.org/"
-  url "https://downloads.sourceforge.net/project/net-snmp/net-snmp/5.7.3/net-snmp-5.7.3.tar.gz"
-  sha256 "12ef89613c7707dc96d13335f153c1921efc9d61d3708ef09f3fc4a7014fb4f0"
+  url "https://downloads.sourceforge.net/project/net-snmp/net-snmp/5.8/net-snmp-5.8.tar.gz"
+  sha256 "b2fc3500840ebe532734c4786b0da4ef0a5f67e51ef4c86b3345d697e4976adf"
 
   bottle do
     rebuild 3
@@ -40,12 +40,6 @@ class NetSnmp < Formula
       args << "--with-python-modules"
       ENV["PYTHONPROG"] = which("python2.7")
     end
-
-    # https://sourceforge.net/p/net-snmp/bugs/2504/
-    ln_s "darwin13.h", "include/net-snmp/system/darwin14.h"
-    ln_s "darwin13.h", "include/net-snmp/system/darwin15.h"
-    ln_s "darwin13.h", "include/net-snmp/system/darwin16.h"
-    ln_s "darwin13.h", "include/net-snmp/system/darwin17.h"
 
     system "./configure", *args
     system "make"
