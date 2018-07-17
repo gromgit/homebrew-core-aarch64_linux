06551c81e06b72a29acc31acbad0df7a80ac80e9
Ludwig Schwardt <ludwig@ska.ac.za>
2018-07-17 16:07:12 +0200

    pgplot: restore C bindings and improve the test
    Even though PGPLOT is a Fortran library, it also supports C bindings
    via a wrapper library (libcpgplot). This wasn't built because of a
    typo in the formula. Restore the wrapper library and also add a test
    for the C version.
    
    See http://www.astro.caltech.edu/~tjp/pgplot/cbinding.html for more
    details. The test currently uses gfortran to link the final executable
    as suggested there, although clang also seems to work. The test plots
    also work with X11 even without explicitly linking against it in the
    executable, so go for the simplest setup that works.
    
    Also run both Fortran and C test programs, produce PNG output and check
    that the output files are identical. Hardcode the output driver as /PNG
    to avoid user interaction in the test. Use the newer and simpler PGOPEN
    and PGCLOS instead of PGBEG and PGEND. Let all Fortran products start
    with 'pg' and all C products start with 'cpg'. Simplify compilation of
    the Fortran test program by ignoring X11.
    
    Closes #30186.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/pgplot.rb
+++ Formula/pgplot.rb
@@ -6,7 +6,7 @@ class Pgplot < Formula
   mirror "https://gentoo.osuosl.org/distfiles/pgplot522.tar.gz"
   version "5.2.2"
   sha256 "a5799ff719a510d84d26df4ae7409ae61fe66477e3f1e8820422a9a4727a5be4"
-  revision 6
+  revision 7
 
   bottle do
     sha256 "bf57239170c6561ac52e61af82eeb2ed44abbcac2b8a5f26ad85359efa78dc21" => :high_sierra
@@ -66,7 +66,7 @@ class Pgplot < Formula
       MFLAGC=""
       SYSDIR="$SYSDIR"
       CSHARED_LIB="libcpgplot.dylib"
-      CSHARED_LD="gfortan -dynamiclib -single_module $LDFLAGS -lX11"
+      CSHARED_LD="gfortran -dynamiclib -single_module $LDFLAGS -lX11"
     EOS
 
     mkdir "build" do
@@ -91,15 +91,16 @@ class Pgplot < Formula
   end
 
   test do
-    (testpath/"test.f90").write <<~EOS
+    # build Fortran version of test program
+    (testpath/"pgtest.f90").write <<~EOS
          PROGRAM SIMPLE
          INTEGER I, IER, PGBEG
          REAL XR(100), YR(100)
          REAL XS(5), YS(5)
          data XS/1.,2.,3.,4.,5./
          data YS/1.,4.,9.,16.,25./
-         IER = PGBEG(0,'?',1,1)
-         IF (IER.NE.1) STOP
+         IER = PGOPEN('pgtest.png/PNG')
+         IF (IER.LE.0) STOP
          CALL PGENV(0.,10.,0.,20.,0,1)
          CALL PGLAB('(x)', '(y)', 'A Simple Graph')
          CALL PGPT(5,XS,YS,9)
@@ -108,10 +109,49 @@ class Pgplot < Formula
              YR(I) = XR(I)**2
       10 CONTINUE
          CALL PGLINE(60,XR,YR)
-         CALL PGEND
+         CALL PGCLOS
          END
     EOS
-    system "gfortran", "-o", "test", "test.f90", "-I/usr/X11/include",
-           "-L/usr/X11/lib", "-L#{lib}", "-lpgplot", "-lX11"
+    system "gfortran", "-o", "pgtest", "pgtest.f90", "-L#{lib}", "-lpgplot"
+
+    # build C version of test program
+    (testpath/"cpgtest.c").write <<~EOS
+      #include "cpgplot.h"
+
+      #include <stdio.h>
+      #include <stdlib.h>
+      #include <math.h>
+
+      int main()
+      {
+        int i;
+        static float xs[] = {1.0, 2.0, 3.0, 4.0, 5.0 };
+        static float ys[] = {1.0, 4.0, 9.0, 16.0, 25.0 };
+        float xr[60], yr[60];
+        int n = sizeof(xr) / sizeof(xr[0]);
+        if(cpgopen("cpgtest.png/PNG") <= 0)
+          return EXIT_FAILURE;
+        cpgenv(0.0, 10.0, 0.0, 20.0, 0, 1);
+        cpglab("(x)", "(y)", "A Simple Graph");
+        cpgpt(5, xs, ys, 9);
+        for(i=0; i<n; i++) {
+          xr[i] = 0.1*i;
+          yr[i] = xr[i]*xr[i];
+        }
+        cpgline(n, xr, yr);
+        cpgclos();
+        return EXIT_SUCCESS;
+      }
+    EOS
+    system ENV.cc, "-c", "-I#{include}", "cpgtest.c"
+    system "gfortran", "-o", "cpgtest", "cpgtest.o",
+           "-L#{lib}", "-lcpgplot", "-lpgplot"
+
+    # Produce PNG output with both programs and check if identical
+    system "./pgtest"
+    system "./cpgtest"
+    assert_predicate testpath/"pgtest.png", :exist?
+    assert_predicate testpath/"cpgtest.png", :exist?
+    assert_equal (testpath/"pgtest.png").read, (testpath/"cpgtest.png").read
   end
 end
