3122f8f36016d73915d5accd53f33692d2e6e013
ilovezfs <ilovezfs@icloud.com>
2018-07-16 21:18:22 -0700

    charm 2.3.0
    Closes #30134.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/charm.rb
+++ Formula/charm.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Charm < Formula
   desc "Tool for managing Juju Charms"
   homepage "https://github.com/juju/charmstore-client"
-  url "https://github.com/juju/charmstore-client/archive/2.2.3.tar.gz"
-  sha256 "1b6342577fbdebadc01e3b63739fb4c55dcf3321740119486d4886ba308963f2"
+  url "https://github.com/juju/charmstore-client/archive/v2.3.0.tar.gz"
+  sha256 "778d20848939a8162fa19acb5284b3c761047c3fa5ab49b36a83464eb4904261"
 
   bottle do
     cellar :any_skip_relocation
@@ -18,12 +18,22 @@ class Charm < Formula
 
   go_resource "github.com/kisielk/gotool" do
     url "https://github.com/kisielk/gotool.git",
-        :revision => "d6ce6262d87e3a4e153e86023ff56ae771554a41"
+        :revision => "80517062f582ea3340cd4baf70e86d539ae7d84d"
+  end
+
+  go_resource "github.com/pelletier/go-toml" do
+    url "https://github.com/pelletier/go-toml.git",
+        :revision => "603baefff989777996bf283da430d693e78eba3a"
+  end
+
+  go_resource "golang.org/x/tools" do
+    url "https://go.googlesource.com/tools.git",
+        :revision => "fd2d2c45eb2dff7b87eab4303a1016b4dbf95e81"
   end
 
   go_resource "github.com/rogpeppe/godeps" do
     url "https://github.com/rogpeppe/godeps.git",
-        :revision => "e444a191d9b826975e788bb3c95511447393706d"
+        :revision => "404a7e748cd352bb0d7449dedc645546eebbfc6e"
   end
 
   def install
