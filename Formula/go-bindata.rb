313c6d452b7b1b795ed63ff246693713f7869c62
Kevin Burke <kev@inburke.com>
2018-07-17 20:46:19 -0700

    go-bindata 3.8.0
    Closes #30193.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/go-bindata.rb
+++ Formula/go-bindata.rb
@@ -1,8 +1,8 @@
 class GoBindata < Formula
   desc "Small utility that generates Go code from any file"
   homepage "https://github.com/kevinburke/go-bindata"
-  url "https://github.com/kevinburke/go-bindata/archive/v3.7.0.tar.gz"
-  sha256 "28553a1a1490465731484ac3326574f35c8528efc5ca7dcea428f8245f5c83b0"
+  url "https://github.com/kevinburke/go-bindata/archive/v3.8.0.tar.gz"
+  sha256 "bd5fde139d566313e1afabf12553cc85861e67e2bd8fed3ac2f565b11f2e9e5f"
 
   bottle do
     cellar :any_skip_relocation
