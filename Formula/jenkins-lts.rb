18130e5993b87a7c8e52adb8235264755757ef44
commitay <commitay@users.noreply.github.com>
2018-07-19 01:49:45 +1000

    jenkins-lts 2.121.2 (#30234)

--- Formula/jenkins-lts.rb
+++ Formula/jenkins-lts.rb
@@ -1,8 +1,8 @@
 class JenkinsLts < Formula
   desc "Extendable open-source CI server"
   homepage "https://jenkins.io/index.html#stable"
-  url "http://mirrors.jenkins.io/war-stable/2.121.1/jenkins.war"
-  sha256 "5bb075b81a3929ceada4e960049e37df5f15a1e3cfc9dc24d749858e70b48919"
+  url "http://mirrors.jenkins.io/war-stable/2.121.2/jenkins.war"
+  sha256 "da0f9d106e936246841a898471783fb4fbdbbacc8d42a156b7306a0855189602"
 
   bottle :unneeded
 
