c1f5ee185b654ee75f0ee291e4c5f34a5cb723a5
ilovezfs <ilovezfs@icloud.com>
2018-07-17 23:04:33 -0700

    feh 2.27.1
    Closes #30204.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/feh.rb
+++ Formula/feh.rb
@@ -1,8 +1,8 @@
 class Feh < Formula
   desc "X11 image viewer"
   homepage "https://feh.finalrewind.org/"
-  url "https://feh.finalrewind.org/feh-2.27.tar.bz2"
-  sha256 "af326845ef7483ecbffde697fdadb39298a68069d79644e249c69c9ad464c64e"
+  url "https://feh.finalrewind.org/feh-2.27.1.tar.bz2"
+  sha256 "6ec338f80c3f4c30d715f44780f1a09ebfbb99e92a1bb43316428744a839f383"
 
   bottle do
     sha256 "31663d3cf333f653f0deed2396af70789e8c1b12b6825c1785187a503d35d268" => :high_sierra
