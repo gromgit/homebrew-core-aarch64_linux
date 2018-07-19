22044dddfc3aa6708864c1be8cd82d99481a301e
ryenus <ryenus@gmail.com>
2018-07-19 20:10:28 +0800

    sonarqube-lts 6.7.4 (new formula) (#30237)

---
+++ Formula/sonarqube-lts.rb
@@ -0,0 +1,46 @@
+class SonarqubeLts < Formula
+  desc "Manage code quality"
+  homepage "https://www.sonarqube.org/"
+  url "https://sonarsource.bintray.com/Distribution/sonarqube/sonarqube-6.7.4.zip"
+  sha256 "a551b555d5ccb0b62a6604b37bad1ee17c5081ab2f67ddbac4fb58fa0eaeaa0e"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8+"
+
+  conflicts_with "sonarqube", :because => "both install the same binaries"
+
+  def install
+    # Delete native bin directories for other systems
+    rm_rf Dir["bin/{linux,windows}-*"]
+
+    libexec.install Dir["*"]
+
+    bin.install_symlink "#{libexec}/bin/macosx-universal-64/sonar.sh" => "sonar"
+  end
+
+  plist_options :manual => "sonar console"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+        <string>#{opt_bin}/sonar</string>
+        <string>start</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+    </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    assert_match "SonarQube", shell_output("#{bin}/sonar status", 1)
+  end
+end
