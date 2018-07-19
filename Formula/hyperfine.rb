e56689fc38e53c94f1946795e482246990888e0f
ilovezfs <ilovezfs@icloud.com>
2018-07-18 21:50:50 -0700

    hyperfine 1.2.0
    Closes #30262.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/hyperfine.rb
+++ Formula/hyperfine.rb
@@ -1,8 +1,8 @@
 class Hyperfine < Formula
   desc "Command-line benchmarking tool"
   homepage "https://github.com/sharkdp/hyperfine"
-  url "https://github.com/sharkdp/hyperfine/archive/v1.1.0.tar.gz"
-  sha256 "2922bfd7128d8bac329eb73672dafc008bfb3bb01f94b81958f4d26a85d8a2cf"
+  url "https://github.com/sharkdp/hyperfine/archive/v1.2.0.tar.gz"
+  sha256 "324ab37890a1cc64680b0e5eae03eebda53b6005166148150a7c82f045e7015b"
 
   bottle do
     sha256 "0a3b5f88912fcf91ea254097d15bf6e8b4d6ec41aeb89b79280e8a95d9dc4093" => :high_sierra
