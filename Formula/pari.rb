e0de1d3b295df3e888433692b3a42e20e7726a36
BrewTestBot <brew-test-bot@googlegroups.com>
2018-07-19 07:07:14 +0000

    pari: update 2.11.0 bottle.

--- Formula/pari.rb
+++ Formula/pari.rb
@@ -5,9 +5,9 @@ class Pari < Formula
   sha256 "3835caccaa3e0c64764521032d89efeb8773cce841f6655fec6d58e790f4c9a1"
 
   bottle do
-    sha256 "c8e1e3c1da64c35a7ecb8696e4cf31113112887fff6f1b16a193900f50ac73e6" => :high_sierra
-    sha256 "1ae613ef98c71ce691906dc7cf5282374bfa18cc669e8570b77152648bc3d8c7" => :sierra
-    sha256 "b5426e62d5db7181898d371c444889127e6e7a9336b40020ca65d51cb0f55006" => :el_capitan
+    sha256 "695ad213a9fbb81ca3ae948b5b597f5bd992e4f016fd04631aa031f455a2a218" => :high_sierra
+    sha256 "5763ae6604f64710145363b7eaf3e6a2c14f759f05329222da4ef973da1ceeed" => :sierra
+    sha256 "0445d9d8f3cfe90c13ed276b869b847667a4e610dc99477f02e4af356c02eff7" => :el_capitan
   end
 
   depends_on "gmp"
