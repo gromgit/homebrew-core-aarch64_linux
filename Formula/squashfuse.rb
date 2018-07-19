5e3e881a7488cfd40da69e3bec6555551ac2e952
BrewTestBot <brew-test-bot@googlegroups.com>
2018-07-19 14:08:56 +0000

    squashfuse: add 0.1.103 bottle.

--- Formula/squashfuse.rb
+++ Formula/squashfuse.rb
@@ -4,6 +4,13 @@ class Squashfuse < Formula
   url "https://github.com/vasi/squashfuse/releases/download/0.1.103/squashfuse-0.1.103.tar.gz"
   sha256 "42d4dfd17ed186745117cfd427023eb81effff3832bab09067823492b6b982e7"
 
+  bottle do
+    cellar :any
+    sha256 "ada7e939ff42fcd9fb6b1fb81ab596463d6149ff592f73ca924b5b9dca5ddfc4" => :high_sierra
+    sha256 "c1898c81ae091097ae2502ecbdebdd1831db302dd74b814003191007a4d5f018" => :sierra
+    sha256 "bf4e6ca88d094fd7d92fbab61dd1c3a4e71b60d7668d23b6044c90e8167833c5" => :el_capitan
+  end
+
   depends_on "pkg-config" => :build
   depends_on "lz4"
   depends_on "lzo"
