a3648973bcc98bb62a941d6c35b98b1f7879d67d
ilovezfs <ilovezfs@icloud.com>
2018-07-19 08:33:38 +0100

    docker-completion 18.06.0 (#30258)

--- Formula/docker-completion.rb
+++ Formula/docker-completion.rb
@@ -2,8 +2,8 @@ class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/docker-ce.git",
-      :tag => "v18.05.0-ce",
-      :revision => "f150324782643a5268a04e7d1a675587125da20e"
+      :tag => "v18.06.0-ce",
+      :revision => "0ffa8257ec673ed6849b73b03fb01b0cac90fdb3"
 
   bottle :unneeded
 
