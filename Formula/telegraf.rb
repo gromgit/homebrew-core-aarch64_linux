ab3b276e0492b32108a5f2c6affe198c319b52dc
BrewTestBot <brew-test-bot@googlegroups.com>
2018-07-19 11:51:33 +0000

    telegraf: update 1.7.2 bottle.

--- Formula/telegraf.rb
+++ Formula/telegraf.rb
@@ -7,9 +7,9 @@ class Telegraf < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ea6abd371147ebc076a3b71d7877bece2a091e73e01251f1b8d501379eed757e" => :high_sierra
-    sha256 "4223f815958cf9e50afe62945c67c06958e3dab050f4648c48d97d6c08d40d20" => :sierra
-    sha256 "5ec1cf7050bdab1245c9442d2b3aeffdb363cc27b5f48c3bb7fa9091e7b7cf7c" => :el_capitan
+    sha256 "ca3f5e27fc3e0bcabd3512b05b74c84834f7b8f68ad256d629be79fa08ed86e5" => :high_sierra
+    sha256 "9723e75e2abb7a518eab6dc477792846b7b61648a13e5a0e11c7ac828f364058" => :sierra
+    sha256 "53e2e37fd41cbd3758ba9117f55a8f7f8889c26581f0cbf58563b013d65ce6f0" => :el_capitan
   end
 
   depends_on "gdm" => :build
