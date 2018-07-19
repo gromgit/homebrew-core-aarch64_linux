81b55cbff7257b91f7e3bbd8a3964a23dc77db60
BrewTestBot <brew-test-bot@googlegroups.com>
2018-07-19 06:21:13 +0000

    hyperfine: update 1.2.0 bottle.

--- Formula/hyperfine.rb
+++ Formula/hyperfine.rb
@@ -5,9 +5,9 @@ class Hyperfine < Formula
   sha256 "324ab37890a1cc64680b0e5eae03eebda53b6005166148150a7c82f045e7015b"
 
   bottle do
-    sha256 "0a3b5f88912fcf91ea254097d15bf6e8b4d6ec41aeb89b79280e8a95d9dc4093" => :high_sierra
-    sha256 "0c93a7b1fd86aa3ccdf4c9eca7adc1205d4c2b63c11a40312a9e580ed590d230" => :sierra
-    sha256 "bd8ac6ece5f7e519dbeac177b60b3900e22582fda5d047dd3d3987997eabe318" => :el_capitan
+    sha256 "355b50d978d6ae1f8bfd5b963e84396b86023ebd96c7b2b1839981f61fb7da9a" => :high_sierra
+    sha256 "7d510caac6e35c45b1472279e82b237f3e5058d9691ced30a801ed87b35aabc1" => :sierra
+    sha256 "a82d2806e80a0e222f82bde4ea4cd74cd30b9819a4735b064f08ae40ba2e34d9" => :el_capitan
   end
 
   depends_on "rust" => :build
