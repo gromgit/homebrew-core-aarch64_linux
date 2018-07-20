543a4abfb11e6d2d021696c6d2a2b57233b9cf86
ilovezfs <ilovezfs@icloud.com>
2018-07-19 21:48:42 -0700

    xapian 1.4.7
    Closes #30306.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/xapian.rb
+++ Formula/xapian.rb
@@ -1,9 +1,9 @@
 class Xapian < Formula
   desc "C++ search engine library with many bindings"
   homepage "https://xapian.org/"
-  url "https://oligarchy.co.uk/xapian/1.4.6/xapian-core-1.4.6.tar.xz"
-  mirror "https://fossies.org/linux/www/xapian-core-1.4.6.tar.xz"
-  sha256 "1e0ef1c1d3e2119874d545b7edbb60e6e17d7d18fb802eb890d9ef7bb0bbd898"
+  url "https://oligarchy.co.uk/xapian/1.4.7/xapian-core-1.4.7.tar.xz"
+  mirror "https://fossies.org/linux/www/xapian-core-1.4.7.tar.xz"
+  sha256 "13f08a0b649c7afa804fa0e85678d693fd6069dd394c9b9e7d41973d74a3b5d3"
 
   bottle do
     cellar :any
@@ -28,8 +28,8 @@ class Xapian < Formula
   skip_clean :la
 
   resource "bindings" do
-    url "https://oligarchy.co.uk/xapian/1.4.6/xapian-bindings-1.4.6.tar.xz"
-    sha256 "71693c803c4b12a7f11ec3e1b8b9d79b1a66cc1cf10ca07d204290638d51b57c"
+    url "https://oligarchy.co.uk/xapian/1.4.7/xapian-bindings-1.4.7.tar.xz"
+    sha256 "4519751376dc5b59893b812495e6004fd80eb4a10970829aede71a35264b4e6a"
   end
 
   def install
