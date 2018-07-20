2cb58f757e8dfcf4614c07b9b0e898429d960503
BrewTestBot <brew-test-bot@googlegroups.com>
2018-07-20 09:13:41 +0000

    diff-pdf: update 0.2_32 bottle.

--- Formula/diff-pdf.rb
+++ Formula/diff-pdf.rb
@@ -7,9 +7,9 @@ class DiffPdf < Formula
 
   bottle do
     cellar :any
-    sha256 "1c3f935573f83f451c6b584e357ee69bec9c5bafd00ff29324b4233bf776d951" => :high_sierra
-    sha256 "c6b89e9e450202ad45152b5f6bb78416484ca62e7e50ebdc4803c90ca96e8b81" => :sierra
-    sha256 "3f54a8562101f2e5377b1dd89f466578a94ad0f28e441de34a07e3da09662171" => :el_capitan
+    sha256 "3bb76335ff0e6f9c53a2529638157a1473097908ac052a1a69e7b066251c06d8" => :high_sierra
+    sha256 "e2803bdd43f5f318c0c7e18e650adf1e32d84652c202d1593dbad2405a5720d4" => :sierra
+    sha256 "65c08db3a51ea6d91a9f0d119e74bfbd5b83b1fc846e4f94e450815ac2e3fa4d" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
