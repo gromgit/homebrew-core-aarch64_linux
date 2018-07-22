6c7cde743003efc6b6f80c00568a2b07cba86840
ilovezfs <ilovezfs@icloud.com>
2018-07-22 16:04:56 +0200

    tkdiff 4.3.4 (#30356)

--- Formula/tkdiff.rb
+++ Formula/tkdiff.rb
@@ -1,9 +1,9 @@
 class Tkdiff < Formula
   desc "Graphical side by side diff utility"
   homepage "https://tkdiff.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/tkdiff/tkdiff/4.3.3/tkdiff-4-3-3.zip"
-  version "4.3.3"
-  sha256 "a85652b03f17b88ad438f0feb76dd2cbc88fe817df27ed2279d940e973ca6aa9"
+  url "https://downloads.sourceforge.net/project/tkdiff/tkdiff/4.3.4/tkdiff-4-3-4.zip"
+  version "4.3.4"
+  sha256 "b75413f1832b2d6f28dca21445eb5661d7aad50fe5ea7d34e19da53f34955f0f"
 
   bottle :unneeded
 
