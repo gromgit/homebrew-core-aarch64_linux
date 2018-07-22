175469cb7c053992f89ff346feec34a894918e34
Leonardo Medici <leonardo_medici@me.com>
2018-07-22 13:28:32 +0200

    stanford-parser 3.9.1

--- Formula/stanford-parser.rb
+++ Formula/stanford-parser.rb
@@ -1,9 +1,9 @@
 class StanfordParser < Formula
   desc "Statistical NLP parser"
   homepage "https://nlp.stanford.edu/software/lex-parser.shtml"
-  url "https://nlp.stanford.edu/software/stanford-parser-full-2017-06-09.zip"
-  version "3.8.0"
-  sha256 "d03ed9a823c3001dde3069e96de25f322b7da12c4ea070b969e98ad467c4959f"
+  url "https://nlp.stanford.edu/software/stanford-parser-full-2018-02-27.zip"
+  version "3.9.1"
+  sha256 "c360eb294e52870a1df06878df65ed21c68145065d21f73c0028d717252a9bd7"
 
   bottle :unneeded
 
