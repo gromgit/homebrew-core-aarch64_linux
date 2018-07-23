451e3a00d2742c6a1f8bbb6814261680892946e1
Achim Herwig <achim.herwig@tngtech.com>
2018-07-23 21:50:34 +0200

    xonsh 0.7.1
    Bumped the version to 0.7.1 and, following the recommendation of the
    xonsh development team, included the installation of some packages for
    better command-line experience.
    (https://github.com/xonsh/xonsh/issues/2734#issuecomment-406782617)
    
    Closes #30402.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/xonsh.rb
+++ Formula/xonsh.rb
@@ -3,8 +3,8 @@ class Xonsh < Formula
 
   desc "Python-ish, BASHwards-compatible shell language and command prompt"
   homepage "http://xon.sh"
-  url "https://github.com/xonsh/xonsh/archive/0.7.0.tar.gz"
-  sha256 "75fef71b4367a4bef3e4a54cf83dbb9d78a2ea580c03398dc64b2af756813003"
+  url "https://github.com/xonsh/xonsh/archive/0.7.1.tar.gz"
+  sha256 "87a9407b90e297d57fbb92a0c1e675cf5a10a65410c05902b90e77c0345b6887"
   head "https://github.com/xonsh/xonsh.git"
 
   bottle do
@@ -15,6 +15,31 @@ class Xonsh < Formula
   end
 
   depends_on "python"
+
+  resource "prompt_toolkit" do
+    url "https://files.pythonhosted.org/packages/77/bf/5d7664605c91db8f39a3e49abb57a3c933731a90b7a58cdcafd4a9bcbe97/prompt_toolkit-2.0.4.tar.gz"
+    sha256 "ff58ce8bb82c11c43416dd3eec7701dcbe8c576e2d7649f1d2b9d21a2fd93808"
+  end
+
+  resource "Pygments" do
+    url "https://files.pythonhosted.org/packages/71/2a/2e4e77803a8bd6408a2903340ac498cb0a2181811af7c9ec92cb70b0308a/Pygments-2.2.0.tar.gz"
+    sha256 "dbae1046def0efb574852fab9e90209b23f556367b5a320c0bcb871c77c3e8cc"
+  end
+
+  resource "setproctitle" do
+    url "https://files.pythonhosted.org/packages/5a/0d/dc0d2234aacba6cf1a729964383e3452c52096dc695581248b548786f2b3/setproctitle-1.1.10.tar.gz"
+    sha256 "6283b7a58477dd8478fbb9e76defb37968ee4ba47b05ec1c053cb39638bd7398"
+  end
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/16/d8/bc6316cf98419719bd59c91742194c111b6f2e85abac88e496adefaf7afe/six-1.11.0.tar.gz"
+    sha256 "70e8a77beed4562e7f14fe23a786b54f6296e34344c23bc42f07b15018ff98e9"
+  end
+
+  resource "wcwidth" do
+    url "https://files.pythonhosted.org/packages/55/11/e4a2bb08bb450fdbd42cc709dd40de4ed2c472cf0ccb9e64af22279c5495/wcwidth-0.1.7.tar.gz"
+    sha256 "3df37372226d6e63e1b1e1eda15c594bca98a22d33a23832a90998faa96bc65e"
+  end
 
   def install
     virtualenv_install_with_resources
