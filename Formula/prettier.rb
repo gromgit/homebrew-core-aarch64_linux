bb02fff3973bd8c18e23721370cf4abd84faa1e1
Tommy Yang <yt940928@gmail.com>
2018-07-24 01:50:04 -0700

    prettier 1.13.7 (new formula)
    Closes #30430.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ Formula/prettier.rb
@@ -0,0 +1,21 @@
+require "language/node"
+
+class Prettier < Formula
+  desc "Code formatter for JavaScript, CSS, JSON, GraphQL, Markdown, YAML"
+  homepage "https://prettier.io/"
+  url "https://registry.npmjs.org/prettier/-/prettier-1.13.7.tgz"
+  sha256 "a380bed5eda687946afceb09ed56ca960dc88292cc512303f426d9640522b47a"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"test.js").write("const arr = [1,2];")
+    output = shell_output("#{bin}/prettier test.js")
+    assert_equal "const arr = [1, 2];", output.chomp
+  end
+end
