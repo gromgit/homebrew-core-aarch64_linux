01578d5274f7245fefb22fcb9272a7ab2e48a96e
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2018-07-25 15:25:05 -0700

    elixir 1.7.0
    Closes #30475.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/elixir.rb
+++ Formula/elixir.rb
@@ -1,8 +1,8 @@
 class Elixir < Formula
   desc "Functional metaprogramming aware language built on Erlang VM"
   homepage "https://elixir-lang.org/"
-  url "https://github.com/elixir-lang/elixir/archive/v1.6.6.tar.gz"
-  sha256 "74507b0646bf485ee3af0e7727e3fdab7123f1c5ecf2187a52a928ad60f93831"
+  url "https://github.com/elixir-lang/elixir/archive/v1.7.0.tar.gz"
+  sha256 "791f726f7e3bb05a4620beb6191f2d758332ea7a169861b03580a16022c49a75"
   head "https://github.com/elixir-lang/elixir.git"
 
   bottle do
