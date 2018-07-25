eeaf28f2ad519586686b34bcc1f179aa3ab7dc46
ilovezfs <ilovezfs@icloud.com>
2018-07-24 21:50:47 -0700

    sratoolkit 2.9.2
    Closes #30456.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/sratoolkit.rb
+++ Formula/sratoolkit.rb
@@ -1,9 +1,8 @@
 class Sratoolkit < Formula
   desc "Data tools for INSDC Sequence Read Archive"
   homepage "https://github.com/ncbi/sra-tools"
-  url "https://github.com/ncbi/sra-tools/archive/2.9.1-2.tar.gz"
-  version "2.9.1-2"
-  sha256 "6f3be8e4781804b8689a085ce979594da1ea2d1873864641cedf9e8cbf953181"
+  url "https://github.com/ncbi/sra-tools/archive/2.9.2.tar.gz"
+  sha256 "e055091ee1c0b8163c6e470d24e11575884a3c7e829759be38d2239366c3cf3b"
   head "https://github.com/ncbi/sra-tools.git"
 
   bottle do
@@ -17,14 +16,13 @@ class Sratoolkit < Formula
   depends_on "libmagic"
 
   resource "ngs-sdk" do
-    url "https://github.com/ncbi/ngs/archive/2.9.1.tar.gz"
-    sha256 "c24c93bd70ed198a44c6b9dba0d6704edc90f228f832f2fef21fa5342303fc50"
+    url "https://github.com/ncbi/ngs/archive/2.9.2.tar.gz"
+    sha256 "ac559009b2021f56bbfaea680c8207f1608637435136277dad9796516be0385a"
   end
 
   resource "ncbi-vdb" do
-    url "https://github.com/ncbi/ncbi-vdb/archive/2.9.1-1.tar.gz"
-    version "2.9.1-1"
-    sha256 "c1ee7443599d2cedab13eb44af0a2d29a4c3e8bf0130c2a6b34a9aa7016287e4"
+    url "https://github.com/ncbi/ncbi-vdb/archive/2.9.2.tar.gz"
+    sha256 "7db26cc71f7742e693ba8cff8b06fefd2244f42bda4dba4ab315aaea9b9194f0"
   end
 
   def install
