5b7fc00070e2fe9ca3f31e8bd9c59a1597acd61e
izaid <hi@irwinzaid.com>
2018-07-26 14:32:15 +0100

    petsc-complex 3.9.3 (new formula)
    Closes #30500.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/petsc-complex.rb
+++ Formula/petsc-complex.rb
@@ -1,14 +1,8 @@
-class Petsc < Formula
-  desc "Portable, Extensible Toolkit for Scientific Computation (real)"
+class PetscComplex < Formula
+  desc "Portable, Extensible Toolkit for Scientific Computation (complex)"
   homepage "https://www.mcs.anl.gov/petsc/"
   url "http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.9.3.tar.gz"
   sha256 "8828fe1221f038d78a8eee3325cdb22ad1055a2f0671871815ee9f47365f93bb"
-
-  bottle do
-    sha256 "733080c80cec9ad2c7da2fb5d922f94889421779eac6c291c9a62152f333fb6b" => :high_sierra
-    sha256 "e8858a1cec95cc42417773aac1b7e85db1c4938e149f7b27a20ad739a51cb395" => :sierra
-    sha256 "4b8ddec36720b2778df1fcdcf765bcf7a847abbeec6a7df9f09067b4ca4fb505" => :el_capitan
-  end
 
   depends_on "hdf5"
   depends_on "hwloc"
@@ -18,7 +12,7 @@ class Petsc < Formula
   depends_on "scalapack"
   depends_on "suite-sparse"
 
-  conflicts_with "petsc-complex", :because => "petsc must be installed with either real or complex support, not both"
+  conflicts_with "petsc", :because => "petsc must be installed with either real or complex support, not both"
 
   def install
     ENV["CC"] = "mpicc"
@@ -27,14 +21,14 @@ class Petsc < Formula
     ENV["FC"] = "mpif90"
     system "./configure", "--prefix=#{prefix}",
                           "--with-debugging=0",
-                          "--with-scalar-type=real",
+                          "--with-scalar-type=complex",
                           "--with-x=0"
     system "make", "all"
     system "make", "install"
   end
 
   test do
-    test_case = "#{pkgshare}/examples/ksp/ksp/examples/tutorials/ex1.c"
+    test_case = "#{share}/petsc/examples/ksp/ksp/examples/tutorials/ex1.c"
     system "mpicc", test_case, "-I#{include}", "-L#{lib}", "-lpetsc", "-o", "test"
     output = shell_output("./test")
     # This PETSc example prints several lines of output. The last line contains
