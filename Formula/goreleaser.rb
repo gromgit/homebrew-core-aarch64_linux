3fc99d4b11e7151397b913d3fb55be272c27ffa6
BrewTestBot <brew-test-bot@googlegroups.com>
2018-07-27 06:24:25 +0000

    goreleaser: update 0.82.0 bottle.

--- Formula/goreleaser.rb
+++ Formula/goreleaser.rb
@@ -6,9 +6,9 @@ class Goreleaser < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "8785196e498b76003a0bbf630d695f76c9beccbe07ae13bc361909f32de44fb9" => :high_sierra
-    sha256 "8c982971acfede75dc11f9cf23f064e7f04ce32e65a8162a0ec701cab6670d42" => :sierra
-    sha256 "b003af1570ebec5db63984425a30cdaea0c36ab68088c1d81983d7757a03b61e" => :el_capitan
+    sha256 "e7cd3feb7e6772f03acd698d85069f2aa48063d35f256145a5160552969307b0" => :high_sierra
+    sha256 "69bb8f0e71f2e8f525130bccf1e56a817ace211be2a8ea1f87fdb98221688163" => :sierra
+    sha256 "9a9433b578651267b9e6ca37af5b7aafa97e1c47fb294c5b515afdd9c7f79181" => :el_capitan
   end
 
   depends_on "dep" => :build
