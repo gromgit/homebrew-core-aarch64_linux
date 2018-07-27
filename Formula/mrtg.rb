8dcbe8dc190e1595a004942f7cf5f9cb76b72fa4
ilovezfs <ilovezfs@icloud.com>
2018-07-27 07:42:29 -0700

    mrtg 2.17.7
    Closes #30536.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/mrtg.rb
+++ Formula/mrtg.rb
@@ -1,8 +1,8 @@
 class Mrtg < Formula
   desc "Multi router traffic grapher"
   homepage "https://oss.oetiker.ch/mrtg/"
-  url "https://oss.oetiker.ch/mrtg/pub/mrtg-2.17.4.tar.gz"
-  sha256 "5efa7fae8040159208472e5f889be5b41d8c8a2ea6b31616f0f75cc7f48d2365"
+  url "https://oss.oetiker.ch/mrtg/pub/mrtg-2.17.7.tar.gz"
+  sha256 "9b94cb268fb15b0304ad3bb3ec92b9a8a16dacfcee72baac19298224a2c332c3"
 
   bottle do
     cellar :any
