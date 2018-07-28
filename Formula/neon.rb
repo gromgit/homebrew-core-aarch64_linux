2803c847b0f5e8c967bbd212d37ce0994f64da5a
Viktor Szakats <commit@vszakats.net>
2018-07-28 14:54:53 +0000

    neon: secure url(s)

--- Formula/neon.rb
+++ Formula/neon.rb
@@ -1,6 +1,6 @@
 class Neon < Formula
   desc "HTTP and WebDAV client library with a C interface"
-  homepage "https://web.archive.org/web/20070925040920/www.webdav.org/neon/"
+  homepage "https://web.archive.org/web/webdav.org/neon/"
   url "https://mirrorservice.org/sites/distfiles.macports.org/neon/neon-0.30.2.tar.gz"
   mirror "https://fossies.org/linux/www/neon-0.30.2.tar.gz"
   sha256 "db0bd8cdec329b48f53a6f00199c92d5ba40b0f015b153718d1b15d3d967fbca"
