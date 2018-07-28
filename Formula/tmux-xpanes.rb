183e891ae22e27863e3de83498782ea0d8b1ac43
ilovezfs <ilovezfs@icloud.com>
2018-07-28 17:09:45 +0200

    tmux-xpanes 3.0.1 (#30554)

--- Formula/tmux-xpanes.rb
+++ Formula/tmux-xpanes.rb
@@ -1,8 +1,8 @@
 class TmuxXpanes < Formula
   desc "Ultimate terminal divider powered by tmux"
   homepage "https://github.com/greymd/tmux-xpanes"
-  url "https://github.com/greymd/tmux-xpanes/archive/v3.0.0.tar.gz"
-  sha256 "7874daf62fd9fb85fb510e6ae60ae99146ff65c5648b8421b946cd0d021f6b3f"
+  url "https://github.com/greymd/tmux-xpanes/archive/v3.0.1.tar.gz"
+  sha256 "68b652c0330a77a29f8af922ea88be293acfeb45a0e443da75c8a465099ab93c"
 
   bottle :unneeded
 
