3d4c1c6c3ab95f13be40e81073e55734515aeb64
Viktor Szakats <commit@vszakats.net>
2018-07-28 14:54:53 +0000

    qt: secure url(s)

--- Formula/qt.rb
+++ Formula/qt.rb
@@ -5,7 +5,7 @@ class Qt < Formula
   homepage "https://www.qt.io/"
   url "https://download.qt.io/official_releases/qt/5.11/5.11.1/single/qt-everywhere-src-5.11.1.tar.xz"
   mirror "https://qt.mirror.constant.com/archive/qt/5.11/5.11.1/single/qt-everywhere-src-5.11.1.tar.xz"
-  mirror "http://qt.mirrors.tds.net/qt/archive/qt/5.11/5.11.1/single/qt-everywhere-src-5.11.1.tar.xz"
+  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/qt5/qt-everywhere-src-5.11.1.tar.xz"
   sha256 "39602cb08f9c96867910c375d783eed00fc4a244bffaa93b801225d17950fb2b"
   head "https://code.qt.io/qt/qt5.git", :branch => "5.11", :shallow => false
 
