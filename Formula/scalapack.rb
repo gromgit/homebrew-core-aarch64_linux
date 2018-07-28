716ce744f9b88ccf0a4b4a8ac0446d5b828777ef
Viktor Szakats <commit@vszakats.net>
2018-07-28 14:54:53 +0000

    scalapack: secure url(s)

--- Formula/scalapack.rb
+++ Formula/scalapack.rb
@@ -1,7 +1,7 @@
 class Scalapack < Formula
   desc "High-performance linear algebra for distributed memory machines"
   homepage "https://www.netlib.org/scalapack/"
-  url "http://www.netlib.org/scalapack/scalapack-2.0.2.tgz"
+  url "https://www.netlib.org/scalapack/scalapack-2.0.2.tgz"
   sha256 "0c74aeae690fe5ee4db7926f49c5d0bb69ce09eea75beb915e00bba07530395c"
   revision 11
 
