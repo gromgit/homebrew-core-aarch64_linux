a98d887f751f7ba5f8c445dd37f886fa3602552f
ilovezfs <ilovezfs@icloud.com>
2018-07-30 07:53:56 +0200

    pmd 6.6.0 (#30596)

--- Formula/pmd.rb
+++ Formula/pmd.rb
@@ -1,8 +1,8 @@
 class Pmd < Formula
   desc "Source code analyzer for Java, JavaScript, and more"
   homepage "https://pmd.github.io"
-  url "https://github.com/pmd/pmd/releases/download/pmd_releases/6.5.0/pmd-bin-6.5.0.zip"
-  sha256 "d7d0a66f539878fde9049a9e462e6a070fee54161d07ce4c5f84065baad3a1c2"
+  url "https://github.com/pmd/pmd/releases/download/pmd_releases/6.6.0/pmd-bin-6.6.0.zip"
+  sha256 "61138e877233edd793fbb00f949bbb24ec02041673b94d1274887c8c218dcc98"
 
   bottle :unneeded
 
