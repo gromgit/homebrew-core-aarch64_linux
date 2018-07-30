2162a162a7cb097af6da9731c6681208b92ecd36
Viktor Szakats <commit@vszakats.net>
2018-07-30 15:24:15 +0000

    quantlib: secure url(s)

--- Formula/quantlib.rb
+++ Formula/quantlib.rb
@@ -1,6 +1,6 @@
 class Quantlib < Formula
   desc "Library for quantitative finance"
-  homepage "http://quantlib.org/"
+  homepage "https://www.quantlib.org/"
   url "https://dl.bintray.com/quantlib/releases/QuantLib-1.13.tar.gz"
   sha256 "bb52df179781f9c19ef8e976780c4798b0cdc4d21fa72a7a386016e24d1a86e6"
 
