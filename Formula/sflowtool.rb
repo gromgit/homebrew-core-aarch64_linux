ce5e3e19bde1ea9785b7884d2cd3e17d6802c61c
Viktor Szakats <commit@vszakats.net>
2018-07-30 15:24:15 +0000

    sflowtool: secure url(s)

--- Formula/sflowtool.rb
+++ Formula/sflowtool.rb
@@ -1,6 +1,6 @@
 class Sflowtool < Formula
   desc "Utilities and scripts for analyzing sFlow data"
-  homepage "http://www.inmon.com/technology/sflowTools.php"
+  homepage "https://inmon.com/technology/sflowTools.php"
   url "https://github.com/sflow/sflowtool/releases/download/v3.41/sflowtool-3.41.tar.gz"
   sha256 "0e807f182db5ca5b37d0fb55b77e970b1d51fee0cd8cb845646211befb35ca24"
 
@@ -13,7 +13,7 @@ class Sflowtool < Formula
   end
 
   resource "scripts" do
-    url "http://www.inmon.com/bin/sflowutils.tar.gz"
+    url "https://inmon.com/bin/sflowutils.tar.gz"
     sha256 "45f6a0f96bdb6a1780694b9a4ef9bbd2fd719b9f7f3355c6af1427631b311d56"
   end
 
