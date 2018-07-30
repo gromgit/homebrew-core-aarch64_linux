8cff6a784cd25d10804506b6cff4293275ae8f3c
Viktor Szakats <commit@vszakats.net>
2018-07-30 12:11:24 +0000

    dvorak7min: secure/fix url(s)

--- Formula/dvorak7min.rb
+++ Formula/dvorak7min.rb
@@ -1,6 +1,6 @@
 class Dvorak7min < Formula
   desc "Dvorak (simplified keyboard layout) typing tutor"
-  homepage "http://dvorak7min.sourcearchive.com/"
+  homepage "https://web.archive.org/web/dvorak7min.sourcearchive.com/"
   url "https://mirrors.ocf.berkeley.edu/debian/pool/main/d/dvorak7min/dvorak7min_1.6.1+repack.orig.tar.gz"
   mirror "https://mirrors.kernel.org/debian/pool/main/d/dvorak7min/dvorak7min_1.6.1+repack.orig.tar.gz"
   version "1.6.1"
