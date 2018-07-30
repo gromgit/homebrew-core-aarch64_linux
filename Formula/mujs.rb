de109e0067f368ffe5426385f628d033db20df8f
Viktor Szakats <commit@vszakats.net>
2018-07-30 12:11:25 +0000

    mujs: secure/fix url(s)

--- Formula/mujs.rb
+++ Formula/mujs.rb
@@ -1,6 +1,6 @@
 class Mujs < Formula
   desc "Embeddable Javascript interpreter"
-  homepage "http://dev.mujs.com/"
+  homepage "https://www.mujs.com/"
   # use tag not tarball so the version in the pkg-config file isn't blank
   url "https://github.com/ccxvii/mujs.git",
       :tag => "1.0.3",
