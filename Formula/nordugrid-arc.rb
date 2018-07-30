2d084d9fd5b88c81bd299eea0bf5595bf0db031e
Viktor Szakats <commit@vszakats.net>
2018-07-30 16:18:26 +0000

    nordugrid-arc: secure url(s)

--- Formula/nordugrid-arc.rb
+++ Formula/nordugrid-arc.rb
@@ -1,6 +1,6 @@
 class NordugridArc < Formula
   desc "Grid computing middleware"
-  homepage "http://www.nordugrid.org"
+  homepage "http://www.nordugrid.org/"
   url "http://download.nordugrid.org/packages/nordugrid-arc/releases/5.0.2/src/nordugrid-arc-5.0.2.tar.gz"
   sha256 "d7306d91b544eeba571ede341e43760997c46d4ccdacc8b785c64f594780a9d1"
 
@@ -25,7 +25,7 @@ class NordugridArc < Formula
   # libc++ and libstdc++
   depends_on :macos => :yosemite
 
-  # bug filed upstream at http://bugzilla.nordugrid.org/show_bug.cgi?id=3514
+  # bug filed upstream at https://bugzilla.nordugrid.org/show_bug.cgi?id=3514
   patch do
     url "https://gist.githubusercontent.com/tschoonj/065dabc33be5ec636058/raw/beee466cdf5fe56f93af0b07022532b1945e9d2e/nordugrid-arc.diff"
     sha256 "5561ea013ddd03ee4f72437f2e01f22b2c0cac2806bf837402724be281ac2b6d"
