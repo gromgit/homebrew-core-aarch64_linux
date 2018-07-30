fdc63aaa2e87340eb4e7899362df20b4c16324b5
Nick Lockwood <nick@charcoaldesign.co.uk>
2018-07-30 11:31:51 +0100

    swiftformat 0.34.0
    Closes #30606.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/swiftformat.rb
+++ Formula/swiftformat.rb
@@ -1,8 +1,8 @@
 class Swiftformat < Formula
   desc "Formatting tool for reformatting Swift code"
   homepage "https://github.com/nicklockwood/SwiftFormat"
-  url "https://github.com/nicklockwood/SwiftFormat/archive/0.33.13.tar.gz"
-  sha256 "f2141305bebe54f6cf9ea24067f0e3a083b12de16abdbaa0f4dfde197791d905"
+  url "https://github.com/nicklockwood/SwiftFormat/archive/0.34.0.tar.gz"
+  sha256 "8dc75a6f57ca500e070579cab47874eecb8e5d50eefbd0cd08d08e4d06547309"
   head "https://github.com/nicklockwood/SwiftFormat.git", :shallow => false
 
   bottle do
@@ -10,7 +10,7 @@ class Swiftformat < Formula
     sha256 "ac8d9f78abbda30f048636a8029869b318ff2a99a7e5a653fa0d4d319e6bbced" => :high_sierra
   end
 
-  depends_on :xcode => ["9.3", :build]
+  depends_on :xcode => ["9.2", :build]
 
   def install
     xcodebuild "-project",
