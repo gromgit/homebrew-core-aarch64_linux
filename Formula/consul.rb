1c86f305951037aeceb91fe05f0a110e0b5cf83d
commitay <commitay@users.noreply.github.com>
2018-07-31 08:47:56 +1000

    consul 1.2.2
    Closes #30620.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/consul.rb
+++ Formula/consul.rb
@@ -2,8 +2,8 @@ class Consul < Formula
   desc "Tool for service discovery, monitoring and configuration"
   homepage "https://www.consul.io"
   url "https://github.com/hashicorp/consul.git",
-      :tag => "v1.2.1",
-      :revision => "39f93f011e591c842acc8053a7f5972aa6e592fd"
+      :tag => "v1.2.2",
+      :revision => "e716d1b5f8be252b3e53906c6d5632e0228f30fa"
   head "https://github.com/hashicorp/consul.git",
        :shallow => false
 
