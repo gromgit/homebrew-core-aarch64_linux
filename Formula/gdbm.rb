504efbe6b756c43f9b05c73bab0fa03baffd7f50
commitay <commitay@users.noreply.github.com>
2018-07-31 08:49:46 +1000

    gdbm 1.17
    Closes #30621.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/gdbm.rb
+++ Formula/gdbm.rb
@@ -1,9 +1,9 @@
 class Gdbm < Formula
   desc "GNU database manager"
   homepage "https://www.gnu.org/software/gdbm/"
-  url "https://ftp.gnu.org/gnu/gdbm/gdbm-1.16.tar.gz"
-  mirror "https://ftpmirror.gnu.org/gdbm/gdbm-1.16.tar.gz"
-  sha256 "c8a18bc6259da0c3eefefb018f8aa298fddc6f86c6fc0f0dec73270896ab512f"
+  url "https://ftp.gnu.org/gnu/gdbm/gdbm-1.17.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gdbm/gdbm-1.17.tar.gz"
+  sha256 "7cd8cc2e35b1aaede6084ea57cc9447752f498daaea854100a4bad567614977d"
 
   bottle do
     cellar :any
@@ -13,6 +13,13 @@ class Gdbm < Formula
   end
 
   option "with-libgdbm-compat", "Build libgdbm_compat, a compatibility layer which provides UNIX-like dbm and ndbm interfaces."
+
+  # Remove for > 1.17
+  # Upstream commit from 31 July 2018: "(gdbm_sync): Always return a meaningful value"
+  patch do
+    url "http://git.gnu.org.ua/cgit/gdbm.git/patch/?id=1059526e357da1aa92e5c020332f4b39ceb37503"
+    sha256 "c7b13e3779b6701fa3c802e22d383341a483db7419c533ffbca9766ee4688575"
+  end
 
   # Use --without-readline because readline detection is broken in 1.13
   # https://github.com/Homebrew/homebrew-core/pull/10903
