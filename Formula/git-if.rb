2871df4f6b3256659083ccd4440ab049d1ecc8bd
Viktor Szakats <commit@vszakats.net>
2018-07-30 16:18:26 +0000

    git-if: secure url(s)

--- Formula/git-if.rb
+++ Formula/git-if.rb
@@ -1,7 +1,7 @@
 class GitIf < Formula
   desc "Glulx interpreter that is optimized for speed"
-  homepage "http://ifarchive.org/indexes/if-archiveXprogrammingXglulxXinterpretersXgit.html"
-  url "http://ifarchive.org/if-archive/programming/glulx/interpreters/git/git-135.zip"
+  homepage "https://ifarchive.org/indexes/if-archiveXprogrammingXglulxXinterpretersXgit.html"
+  url "https://ifarchive.org/if-archive/programming/glulx/interpreters/git/git-135.zip"
   version "1.3.5"
   sha256 "4bdfae2e1ab085740efddf99d43ded6a044f1f2df274f753737e5f0e402fc4e9"
   head "https://github.com/DavidKinder/Git.git"
