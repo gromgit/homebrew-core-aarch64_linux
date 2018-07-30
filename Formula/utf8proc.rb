eb0e0da3489be2861ca7639a2cbf9f9f5e0558a0
commitay <commitay@users.noreply.github.com>
2018-07-30 15:12:30 +1000

    utf8proc: update homepage (#30580)

--- Formula/utf8proc.rb
+++ Formula/utf8proc.rb
@@ -1,7 +1,6 @@
 class Utf8proc < Formula
   desc "Clean C library for processing UTF-8 Unicode data"
-  # Canonical homepage is https://julialang.org/utf8proc
-  homepage "https://github.com/JuliaStrings/utf8proc"
+  homepage "https://juliastrings.github.io/utf8proc/"
   url "https://github.com/JuliaStrings/utf8proc/archive/v2.2.0.tar.gz"
   sha256 "3f8fd1dbdb057ee5ba584a539d5cd1b3952141c0338557cb0bdf8cb9cfed5dbf"
 
