b8e64bcf4dc6c2a32586596482d8693f1912185e
BrewTestBot <brew-test-bot@googlegroups.com>
2018-07-31 09:49:28 +0000

    gst-editing-services: update 1.14.2_1 bottle.

--- Formula/gst-editing-services.rb
+++ Formula/gst-editing-services.rb
@@ -6,10 +6,9 @@ class GstEditingServices < Formula
   revision 1
 
   bottle do
-    cellar :any
-    sha256 "67704afaf2671b482fa52394a73d6636059f1bf8046604980da05598caf49969" => :high_sierra
-    sha256 "31f21382b2c7869e061e86e24ec14604f570b5b2b5f5cb76f1186a6eb7e104c7" => :sierra
-    sha256 "ac06a59d8c48026c42c86c8ee216b98e0ca4b490144c4b9c5c4efd59cef0c84d" => :el_capitan
+    sha256 "831ffe600c209b6cf5de232ec88d855766c266ddc583a72552c06c8c1070d5cb" => :high_sierra
+    sha256 "2165a0043372021c8c264fcb5a388fc5b01ed1b54f7afcef17cc5ba19a2051fd" => :sierra
+    sha256 "58a5e50c51b8db69d11a1e2d4e7f92f87f53deadc68606bc307766b246df4d8f" => :el_capitan
   end
 
   depends_on "gobject-introspection" => :build
