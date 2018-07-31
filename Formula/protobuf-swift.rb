dba98580b039e1dfed065b9edb41a4aa919b02a7
JCount <JCount42@gmail.com>
2018-07-31 19:47:40 -0400

    protobuf-swift: revision for protobuf
    now needs to be built with cxx11

--- Formula/protobuf-swift.rb
+++ Formula/protobuf-swift.rb
@@ -3,6 +3,7 @@ class ProtobufSwift < Formula
   homepage "https://github.com/alexeyxo/protobuf-swift"
   url "https://github.com/alexeyxo/protobuf-swift/archive/4.0.4.tar.gz"
   sha256 "5f95c202b3e600717577bec410e19329322291b30d3c4ecb828a370a42d13d3a"
+  revision 1
 
   bottle do
     cellar :any
@@ -17,10 +18,14 @@ class ProtobufSwift < Formula
   depends_on "libtool" => :build
   depends_on "protobuf"
 
+  needs :cxx11
+
   conflicts_with "swift-protobuf",
     :because => "both install `protoc-gen-swift` binaries"
 
   def install
+    ENV.cxx11
+
     system "protoc", "-Iplugin/compiler",
                      "plugin/compiler/google/protobuf/descriptor.proto",
                      "plugin/compiler/google/protobuf/swift-descriptor.proto",
