eb22697db4dc8e6429bcaf7b59cb74ee7901ef2e
Marcelo Fabri <me@marcelofabri.com>
2018-07-31 09:46:27 -0700

    swiftlint 0.27.0
    Closes #30651.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/swiftlint.rb
+++ Formula/swiftlint.rb
@@ -2,8 +2,8 @@ class Swiftlint < Formula
   desc "Tool to enforce Swift style and conventions"
   homepage "https://github.com/realm/SwiftLint"
   url "https://github.com/realm/SwiftLint.git",
-      :tag => "0.26.0",
-      :revision => "8c5d2afa7c8f58bb8302cc5cc679b64741490982"
+      :tag => "0.27.0",
+      :revision => "12996ef1f54002e5daa45148944ad9219dacef8a"
   head "https://github.com/realm/SwiftLint.git"
 
   bottle do
@@ -23,8 +23,8 @@ class Swiftlint < Formula
 
   test do
     (testpath/"Test.swift").write "import Foundation"
-    assert_match "#{testpath}/Test.swift:1: warning: Trailing Newline Violation: Files should have a single trailing newline. (trailing_newline)",
-                 shell_output("SWIFTLINT_SWIFT_VERSION=3 #{bin}/swiftlint").chomp
+    assert_match "Test.swift:1: warning: Trailing Newline Violation: Files should have a single trailing newline. (trailing_newline)",
+                 shell_output("SWIFTLINT_SWIFT_VERSION=3 #{bin}/swiftlint --no-cache").chomp
     assert_match version.to_s,
                  shell_output("#{bin}/swiftlint version").chomp
   end
