eb45220a48f472dc8e7f6b430f0c247280b34c39
ilovezfs <ilovezfs@icloud.com>
2018-07-31 22:39:47 -0700

    nativefier 7.6.7
    Closes #30664.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/nativefier.rb
+++ Formula/nativefier.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Nativefier < Formula
   desc "Wrap web apps natively"
   homepage "https://github.com/jiahaog/nativefier"
-  url "https://registry.npmjs.org/nativefier/-/nativefier-7.6.4.tgz"
-  sha256 "77de0f138cdbc0cdf35ff0fe23cc79b4272db3f05cd34cdece00fffb8911b3f5"
+  url "https://registry.npmjs.org/nativefier/-/nativefier-7.6.7.tgz"
+  sha256 "58445f81fc9ca671cfffa12cf7cf2dcc1e5a011da7bee4297f18de497123b5e3"
 
   bottle do
     cellar :any_skip_relocation
