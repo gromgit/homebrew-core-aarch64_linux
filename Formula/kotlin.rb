8620054dc177a97f12246a0521a0d1d3fed473c3
ilovezfs <ilovezfs@icloud.com>
2018-08-02 01:17:31 -0700

    kotlin 1.2.60 (#30698)

--- Formula/kotlin.rb
+++ Formula/kotlin.rb
@@ -1,8 +1,8 @@
 class Kotlin < Formula
   desc "Statically typed programming language for the JVM"
   homepage "https://kotlinlang.org/"
-  url "https://github.com/JetBrains/kotlin/releases/download/v1.2.51/kotlin-compiler-1.2.51.zip"
-  sha256 "8a74711c805d3d265b93c13d8c40af5b4dad324591450d2eef0eafc1c9a6f92c"
+  url "https://github.com/JetBrains/kotlin/releases/download/v1.2.60/kotlin-compiler-1.2.60.zip"
+  sha256 "ff79f695c9be0a500d4b56a69d0b8657b3d106d78a35101e792248af904a623d"
 
   bottle :unneeded
 
