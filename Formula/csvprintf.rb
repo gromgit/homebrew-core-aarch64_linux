e81330415e1eec1f91a903ef86988ea4ce46f9da
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-02 00:13:20 +0000

    csvprintf: update 1.0.4 bottle.

--- Formula/csvprintf.rb
+++ Formula/csvprintf.rb
@@ -6,12 +6,9 @@ class Csvprintf < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "71ef91eb05b9478dc8b86974ca260bd3f17e842f61a0ef5121b7d831fdcbad94" => :high_sierra
-    sha256 "93a0c9b38609eda338ac1bd7126a7b3974b9c8485b2cde88ae1070ac5a46872f" => :sierra
-    sha256 "6ad1c8501cb1f51bd6910edb2a9da1c55507186020d7340e83a178a4a036df4d" => :el_capitan
-    sha256 "58691d8d4819618731ebdd034ae390ae8668b7e26017ef74d5edfe2c6daa4eb1" => :yosemite
-    sha256 "269a3ceaa3acb2a0f8c3df760af3647cb64215d5dee4c8907af7b20200aab418" => :mavericks
-    sha256 "762b98a66e435d7a9d4661565c73df656d8f7adc8ae7defe84e0d1ed0a31d280" => :mountain_lion
+    sha256 "99340f4265ad7a952d3f4fdcce154a10e46c7c8c06979b9e7c5d5016810a8c87" => :high_sierra
+    sha256 "236d5de27a77cadffa4cc014b9f2416b7d952f8cedb96ced98096f0bbdc928cb" => :sierra
+    sha256 "79c0236e6eeb86a283831f3123430f700862d229bc0b5d64b0a0251d8832e092" => :el_capitan
   end
 
   depends_on "autoconf" => :build
