eb130df662f2cfba4fd1aefd814776b93b7a76cf
Misty De Meo <mistydemeo@gmail.com>
2018-08-02 12:57:18 -0700

    imagesnap: backport upstream 10.13 fix
    Closes #30722.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Formula/imagesnap.rb
+++ Formula/imagesnap.rb
@@ -13,6 +13,20 @@ class Imagesnap < Formula
 
   depends_on :xcode => :build
 
+  # Fixes running on 10.13+: https://github.com/rharder/imagesnap/issues/16
+  # Merged into master, will be in the next release.
+  patch do
+    url "https://github.com/rharder/imagesnap/commit/cd33ff8963006c37170872a7bdd0f29a7eae9a29.patch?full_index=1"
+    sha256 "2747d93a27892fcc585e014365f6081e56904e23dcdb84c581ba94b0c061f41a"
+  end
+
+  # Fixes filename specification: https://github.com/rharder/imagesnap/issues/19
+  # Merged into master, will be in the next release.
+  patch do
+    url "https://github.com/rharder/imagesnap/commit/c727968f278d09a792fd0dbbb19903c48518ba24.patch?full_index=1"
+    sha256 "b43cb2be1a577a472af1bc990007411860c451c0bca9528340598eeb2cb36ff5"
+  end
+
   def install
     xcodebuild "-project", "ImageSnap.xcodeproj", "SYMROOT=build", "-sdk", MacOS.sdk_path
     bin.install "build/Release/imagesnap"
