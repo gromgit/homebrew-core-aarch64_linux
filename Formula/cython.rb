1da7e9c07696e5010642cff69c6a442dee46408e
Chongyu Zhu <i@lembacon.com>
2018-08-03 15:38:13 +0800

    cython 0.28.5
    Closes #30757.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- Formula/cython.rb
+++ Formula/cython.rb
@@ -1,8 +1,8 @@
 class Cython < Formula
   desc "Compiler for writing C extensions for the Python language"
   homepage "http://cython.org"
-  url "https://files.pythonhosted.org/packages/d2/12/8ef44cede251b93322e8503fd6e1b25a0249fa498bebec191a5a06adbe51/Cython-0.28.4.tar.gz"
-  sha256 "76ac2b08d3d956d77b574bb43cbf1d37bd58b9d50c04ba281303e695854ebc46"
+  url "https://files.pythonhosted.org/packages/21/89/ca320e5b45d381ae0df74c4b5694f1471c1b2453c5eb4bac3449f5970481/Cython-0.28.5.tar.gz"
+  sha256 "b64575241f64f6ec005a4d4137339fb0ba5e156e826db2fdb5f458060d9979e0"
 
   bottle do
     cellar :any_skip_relocation
