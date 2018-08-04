b88458e85f95ad45af29416e84e2fec8d16ea5a6
commitay <commitay@users.noreply.github.com>
2018-08-04 13:40:33 +1000

    xml-tooling-c: vendor curl
    Closes #30774.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/xml-tooling-c.rb
+++ Formula/xml-tooling-c.rb
@@ -3,6 +3,7 @@ class XmlToolingC < Formula
   homepage "https://wiki.shibboleth.net/confluence/display/OpenSAML/XMLTooling-C"
   url "https://shibboleth.net/downloads/c++-opensaml/3.0.0/xmltooling-3.0.2.tar.bz2"
   sha256 "5709cf30c9d7cfc786599ac2433653fac8cc64d425781068af86019c8ce8d689"
+  revision 1
 
   bottle do
     cellar :any
@@ -17,13 +18,38 @@ class XmlToolingC < Formula
   depends_on "xml-security-c"
   depends_on "boost"
   depends_on "openssl"
-  depends_on "curl" => "with-openssl"
+
+  resource "curl" do
+    url "https://curl.haxx.se/download/curl-7.61.0.tar.bz2"
+    mirror "https://curl.askapache.com/download/curl-7.61.0.tar.bz2"
+    sha256 "5f6f336921cf5b84de56afbd08dfb70adeef2303751ffb3e570c936c6d656c9c"
+  end
 
   needs :cxx11
 
   def install
     ENV.O2 # Os breaks the build
     ENV.cxx11
+
+    ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["openssl"].opt_lib}/pkgconfig"
+
+    resource("curl").stage do
+      system "./configure", "--disable-debug",
+                            "--disable-dependency-tracking",
+                            "--disable-silent-rules",
+                            "--prefix=#{libexec}/curl",
+                            "--with-ssl=#{Formula["openssl"].opt_prefix}",
+                            "--with-ca-bundle=#{etc}/openssl/cert.pem",
+                            "--with-ca-path=#{etc}/openssl/certs",
+                            "--without-libssh2",
+                            "--without-libmetalink",
+                            "--without-gssapi",
+                            "--without-librtmp",
+                            "--disable-ares"
+      system "make", "install"
+    end
+
+    ENV.prepend_path "PKG_CONFIG_PATH", libexec/"curl/lib/pkgconfig"
 
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
