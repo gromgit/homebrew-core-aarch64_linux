3b1efb5c318e46fc5fb7d812b9171140711734af
Jeremy Long <jeremy.long@gmail.com>
2018-08-06 08:51:15 -0400

    dependency-check 3.3.1 (#30838)

--- Formula/dependency-check.rb
+++ Formula/dependency-check.rb
@@ -1,8 +1,8 @@
 class DependencyCheck < Formula
   desc "OWASP dependency-check"
   homepage "https://www.owasp.org/index.php/OWASP_Dependency_Check"
-  url "https://dl.bintray.com/jeremy-long/owasp/dependency-check-3.3.0-release.zip"
-  sha256 "b4fce50ece9c4b8d2e7214babb6630926a8d5cb4ffc8389d45996c2d8c0cd2bf"
+  url "https://dl.bintray.com/jeremy-long/owasp/dependency-check-3.3.1-release.zip"
+  sha256 "0e895d2667643b08f59935aef7c1e53ec59577a68c3ecfa110d7d5bdcc6fa362"
 
   bottle :unneeded
 
