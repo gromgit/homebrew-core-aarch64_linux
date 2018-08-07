6418d534b7a807e408794923364b3aa99d06f7ed
Michihito Shigemura <i.am.shige@gmail.com>
2018-08-07 16:16:54 +0900

    the_silver_searcher 2.2.0
    Closes #30865.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/the_silver_searcher.rb
+++ Formula/the_silver_searcher.rb
@@ -1,8 +1,8 @@
 class TheSilverSearcher < Formula
   desc "Code-search similar to ack"
   homepage "https://github.com/ggreer/the_silver_searcher"
-  url "https://github.com/ggreer/the_silver_searcher/archive/2.1.0.tar.gz"
-  sha256 "cb416a0da7fe354a009c482ae709692ed567f8e7d2dad4d242e726dd7ca202f0"
+  url "https://github.com/ggreer/the_silver_searcher/archive/2.2.0.tar.gz"
+  sha256 "6a0a19ca5e73b2bef9481c29a508d2413ca1a0a9a5a6b1bd9bbd695a7626cbf9"
   head "https://github.com/ggreer/the_silver_searcher.git"
 
   bottle do
