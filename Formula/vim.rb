fdc655222d3c84a3dbd60431e5cd988a1ba483ec
Chongyu Zhu <i@lembacon.com>
2018-08-08 04:50:40 +0800

    vim 8.1.0250
    Closes #30881.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/v8.1.0202.tar.gz"
-  sha256 "08da8d0d0d24d2477fac35318b3c6f856eadc289efd74b4a53ae84203f4fef9e"
+  url "https://github.com/vim/vim/archive/v8.1.0250.tar.gz"
+  sha256 "f35193627469f4dc307e9286b21b2c818faaa2c67f03fb4983b58010f26f7f78"
   head "https://github.com/vim/vim.git"
 
   bottle do
