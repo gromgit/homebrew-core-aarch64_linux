16ad6f44c81623fe57e3ab2c8ab0cdefe813a52e
BePsvPT <freedom@csie.io>
2018-08-07 15:00:11 +0800

    phpunit 7.3.1

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -1,8 +1,8 @@
 class Phpunit < Formula
   desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-7.3.0.phar"
-  sha256 "a544ef63a728e1e9a1c6eaa91bff203a2f265d0462540b1c52ee1e46436e303f"
+  url "https://phar.phpunit.de/phpunit-7.3.1.phar"
+  sha256 "fd89749ab7c12b947607a899f9496436f6648564f7501f380f4d8a2fe6dcc391"
 
   bottle :unneeded
 
