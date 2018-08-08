35b98d0b7938c07bbc856c5d55943db8296e11a2
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-08 21:45:14 +0000

    helmfile: add 0.21.0 bottle.

--- Formula/helmfile.rb
+++ Formula/helmfile.rb
@@ -4,6 +4,13 @@ class Helmfile < Formula
   url "https://github.com/roboll/helmfile/archive/v0.21.0.tar.gz"
   sha256 "75503bd10fbff95fa5c4d84ef2aff8f62774eeec2d5a73d1025271883cd330f2"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "475db028fdce313b9546069929a092c5ad7c322ad4f1e067da6341c9bbf94b73" => :high_sierra
+    sha256 "d294b4b24b638cbe8be50b74c13c71d214a8b57926b7dc042acc0793c1dee2ba" => :sierra
+    sha256 "cb297ed4c9b8536ed35df937332b2cc826e3ff149102df36016f933ab7b13b37" => :el_capitan
+  end
+
   depends_on "go" => :build
   depends_on "kubernetes-helm"
 
