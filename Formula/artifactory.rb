e7530b33d2037e9d8b7e13ed92974b757249f9d0
Dominyk Tiller <DomT4@users.noreply.github.com>
2018-08-08 20:01:31 +0100

    artifactory 6.2.0 (#30904)

--- Formula/artifactory.rb
+++ Formula/artifactory.rb
@@ -1,8 +1,8 @@
 class Artifactory < Formula
   desc "Manages binaries"
   homepage "https://www.jfrog.com/artifactory/"
-  url "https://dl.bintray.com/jfrog/artifactory/jfrog-artifactory-oss-6.1.0.zip"
-  sha256 "5dc029b50a11670d3c0250a7d811929ff875c1980932f7227c45c1d32e1d607a"
+  url "https://dl.bintray.com/jfrog/artifactory/jfrog-artifactory-oss-6.2.0.zip"
+  sha256 "45c08f56159a82c0b2f72832bac6af0dfcef14b6672356f3b78640fdd2fb9480"
 
   bottle :unneeded
 
