cb63cf4fcacef97100026c688ea2e9e6a573d8ee
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-08 17:44:43 +0100

    hashcat 4.2.1
    Closes #30899.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/hashcat.rb
+++ Formula/hashcat.rb
@@ -2,9 +2,9 @@ class Hashcat < Formula
   desc "World's fastest and most advanced password recovery utility"
   homepage "https://hashcat.net/hashcat/"
   # Note the mirror will return 301 until the version becomes outdated.
-  url "https://hashcat.net/files/hashcat-4.2.0.tar.gz"
-  mirror "https://hashcat.net/files_legacy/hashcat-4.2.0.tar.gz"
-  sha256 "ed23c7188b6fc6f111052f1bb4cc56a2f26cfd37470ee3b466b77a26efccaa38"
+  url "https://hashcat.net/files/hashcat-4.2.1.tar.gz"
+  mirror "https://hashcat.net/files_legacy/hashcat-4.2.1.tar.gz"
+  sha256 "7dad73c3406e66756b19e15ae8bcc482a52a422e8fb905feb6db4d2eb32e5320"
   version_scheme 1
   head "https://github.com/hashcat/hashcat.git"
 
