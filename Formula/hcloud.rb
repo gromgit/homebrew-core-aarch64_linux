a8f9ba80e76f7dbb6b62e38b60c1cc78879e8dc9
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-08 10:40:26 +0000

    hcloud: update 1.6.0 bottle.

--- Formula/hcloud.rb
+++ Formula/hcloud.rb
@@ -6,10 +6,9 @@ class Hcloud < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "5c6ff02f7c5aa341b3a7cf640e66355a16083cb64a32bee32eec1aeaf0252400" => :high_sierra
-    sha256 "a389b4bf39549713809acdcec93ccc8c907c5b56a2817380fcb0946a270e8071" => :sierra
-    sha256 "e6fd5b0b4a9db1fb1db687e045c035c8cd552720ec887d7858436d5e2a3bb0bf" => :el_capitan
+    sha256 "0c710c7c12c15a186411ccf4e919227f6615557400f127f51ddbb1aed2f6110b" => :high_sierra
+    sha256 "91fbe41966cd0e151a79eaa0440e07f7baec9557bb870e6457a35694ddace58e" => :sierra
+    sha256 "907f1daf1241855d5007bad92f8401521d2a0d1a8c7bf883e39777f4a6236d8f" => :el_capitan
   end
 
   depends_on "go" => :build
