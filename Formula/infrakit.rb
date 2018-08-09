ffc6f82dd4fb3edae8ff13af9a269148094a25d8
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-09 02:22:15 +0000

    infrakit: add 0.5 bottle.

--- Formula/infrakit.rb
+++ Formula/infrakit.rb
@@ -5,6 +5,13 @@ class Infrakit < Formula
       :tag => "v0.5",
       :revision => "3d2670e484176ce474d4b3d171994ceea7054c02"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "577be79865b3ee5eb331fc80e79112e71110b2c2a34887a4efb43ac5fa0ac67f" => :high_sierra
+    sha256 "46da3285072da2574ed804c2c243b377f794642ea280744abd7e91fed5577048" => :sierra
+    sha256 "b4581c8bf2de6220369a79ab9928ee09cf43038dc6c03169416a13a959de3ffd" => :el_capitan
+  end
+
   depends_on "go" => :build
   depends_on "libvirt" => :build
 
