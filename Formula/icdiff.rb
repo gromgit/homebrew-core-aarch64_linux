068c6f0a590326a148fb493448371fb4a8550ee0
Chongyu Zhu <i@lembacon.com>
2018-08-09 21:46:29 +0800

    icdiff 1.9.3

--- Formula/icdiff.rb
+++ Formula/icdiff.rb
@@ -1,8 +1,8 @@
 class Icdiff < Formula
   desc "Improved colored diff"
   homepage "https://github.com/jeffkaufman/icdiff"
-  url "https://github.com/jeffkaufman/icdiff/archive/release-1.9.1.tar.gz"
-  sha256 "18cf11866150eba1ecec1370611921246918310f4b5cb101045db523a05fb37d"
+  url "https://github.com/jeffkaufman/icdiff/archive/release-1.9.3.tar.gz"
+  sha256 "090b0f96c5df5ae5d71f6e5325bca566dd47e8b61ec4b59d4e40acb9ffcb878f"
 
   bottle :unneeded
 
