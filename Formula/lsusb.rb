4257cb76a87f331f3bee7f91dfb9754bb00fd826
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-09 10:13:37 +0000

    lsusb: add 1.0 bottle.

--- Formula/lsusb.rb
+++ Formula/lsusb.rb
@@ -4,6 +4,13 @@ class Lsusb < Formula
   url "https://github.com/jlhonora/lsusb/releases/download/1.0/lsusb-1.0.tar.gz"
   sha256 "68cfa4a820360ecf3bbd2a24a58f287d41f66c62ada99468c36d5bf33f9a3b94"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e696db36d09169064b3e97852d07464125e5bc6e400cb2a4cc186e6aa606574a" => :high_sierra
+    sha256 "e696db36d09169064b3e97852d07464125e5bc6e400cb2a4cc186e6aa606574a" => :sierra
+    sha256 "e696db36d09169064b3e97852d07464125e5bc6e400cb2a4cc186e6aa606574a" => :el_capitan
+  end
+
   def install
     bin.install "lsusb"
     man8.install "man/lsusb.8"
