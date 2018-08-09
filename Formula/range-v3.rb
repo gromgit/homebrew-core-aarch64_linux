8cbe1335fa9aac13261e1912ea4bba35d12960f7
Ron Mordechai <ronmrdechai@gmail.com>
2018-08-09 23:23:01 +0300

    range-v3 0.3.6 (new formula)
    Closes #30948.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ Formula/range-v3.rb
@@ -0,0 +1,35 @@
+class RangeV3 < Formula
+  desc "Experimental range library for C++11/14/17"
+  homepage "https://ericniebler.github.io/range-v3/"
+  url "https://github.com/ericniebler/range-v3/archive/0.3.6.tar.gz"
+  sha256 "ce6e80c6b018ca0e03df8c54a34e1fd04282ac1b068cd39e902e2e5201ac117f"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".",
+                    "-DRANGE_V3_TESTS=OFF",
+                    "-DRANGE_V3_HEADER_CHECKS=OFF",
+                    "-DRANGE_V3_EXAMPLES=OFF",
+                    "-DRANGE_V3_PERF=OFF",
+                    *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <range/v3/all.hpp>
+      #include <iostream>
+      #include <string>
+
+      int main() {
+        std::string s{ "hello" };
+        ranges::for_each( s, [](char c){ std::cout << c << " "; });
+        std::cout << std::endl;
+      }
+    EOS
+    system ENV.cc, "-std=c++11", "-stdlib=libc++", "-lc++",
+                   "-o", "test", "test.cpp"
+    assert_equal "h e l l o \n", shell_output("./test")
+  end
+end
