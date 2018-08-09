4c1d4a3d3b1a1fa62787087fe4314dc61e635e7d
Till Rohrmann <trohrmann@apache.org>
2018-08-09 14:43:16 +0200

    apache-flink 1.6.0
    Closes #30924.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/apache-flink.rb
+++ Formula/apache-flink.rb
@@ -1,9 +1,9 @@
 class ApacheFlink < Formula
   desc "Scalable batch and stream data processing"
   homepage "https://flink.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.5.1/flink-1.5.1-bin-hadoop27-scala_2.11.tgz"
-  version "1.5.1"
-  sha256 "f7554d458f166e58c1bcf9b28274cbad4ef0b9c5bd06925dad27dbddd9b74770"
+  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.6.0/flink-1.6.0-bin-hadoop27-scala_2.11.tgz"
+  version "1.6.0"
+  sha256 "460da13ba3cc51cd1ceaaa2d286215056321c769ed2a5720d8fb5bebf0ac1b99"
   head "https://github.com/apache/flink.git"
 
   bottle :unneeded
