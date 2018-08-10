da5f4037eb594677df1ff5c6dd9f21a57653c785
Chongyu Zhu <i@lembacon.com>
2018-08-10 21:59:52 +0800

    jfrog-cli-go 1.18.0
    Closes #30972.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/jfrog-cli-go.rb
+++ Formula/jfrog-cli-go.rb
@@ -1,8 +1,8 @@
 class JfrogCliGo < Formula
   desc "Command-line interface for Jfrog Artifactory and Bintray"
   homepage "https://github.com/jfrog/jfrog-cli-go"
-  url "https://github.com/jfrog/jfrog-cli-go/archive/1.17.1.tar.gz"
-  sha256 "29ee3448eba3f97ab4f9f148e841a4007e4d5269c8f882e84ab5c9c81a2e94fc"
+  url "https://github.com/jfrog/jfrog-cli-go/archive/1.18.0.tar.gz"
+  sha256 "ff47adaa9b033fe43da5c165481cbaf216ab0f0714e8f23b8b3a81be3fdb2b5e"
 
   bottle do
     cellar :any_skip_relocation
