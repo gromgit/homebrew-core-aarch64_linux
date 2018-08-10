4c93459784691ed20cab2cd66bd633646297a7b8
Chongyu Zhu <i@lembacon.com>
2018-08-10 22:40:53 +0800

    twoping 4.1.2
    Closes #30988.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/twoping.rb
+++ Formula/twoping.rb
@@ -1,9 +1,8 @@
 class Twoping < Formula
   desc "Ping utility to determine directional packet loss"
   homepage "https://www.finnie.org/software/2ping/"
-  url "https://www.finnie.org/software/2ping/2ping-4.1.tar.gz"
-  sha256 "85331ae57f445bc8895ba4e61da4c7300b67fde55642f5e42ea02f2daf07b1ed"
-  revision 3
+  url "https://www.finnie.org/software/2ping/2ping-4.1.2.tar.gz"
+  sha256 "961c1b67b2d63599e439ff00facc0270e60b67809b26e6fcc048dcbed2d5966d"
   head "https://github.com/rfinnie/2ping.git"
 
   bottle do
