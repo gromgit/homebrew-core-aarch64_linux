367058ffb74912918fd6a213684c1435cda7e0c7
Alain Schlesser <alain.schlesser@gmail.com>
2018-08-10 11:40:44 +0200

    wp-cli 2.0.0
    Closes #30963.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -1,10 +1,8 @@
 class WpCli < Formula
   desc "Command-line interface for WordPress"
   homepage "https://wp-cli.org/"
-  # Checksum mismatch for 1.5.1 https://github.com/wp-cli/wp-cli/releases/download/v1.5.1/wp-cli-1.5.1.phar
-  # See https://github.com/Homebrew/homebrew-core/pull/28579
-  url "https://dl.bintray.com/homebrew/mirror/wp-cli-1.5.1.phar"
-  sha256 "c1114450e647ca0c72afcbb6d02b577a55ec9915a510399579626c960d5dd2d2"
+  url "https://github.com/wp-cli/wp-cli/releases/download/v2.0.0/wp-cli-2.0.0.phar"
+  sha256 "577ff95cd9597389346872999bedb956b0fe034d8575cdaf9d3c72d936da17e9"
 
   bottle :unneeded
 
