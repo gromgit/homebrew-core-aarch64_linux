f8b55705e2da3fda4936350915d2dd43b0722c25
Colin Dean <git@cad.cx>
2018-08-09 22:24:13 -0400

    miniserve 0.2.0 (new formula)
    Closes #30953.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/miniserve.rb
@@ -0,0 +1,34 @@
+class Miniserve < Formula
+  desc "High performance static file server"
+  homepage "https://github.com/svenstaro/miniserve"
+  url "https://github.com/svenstaro/miniserve/archive/v0.2.0.tar.gz"
+  sha256 "2a0d4c7563b3b1b75ba0cccc69236976dc916378a41e7f994e28f8a766bded4c"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--root", prefix
+  end
+
+  test do
+    require "socket"
+
+    server = TCPServer.new(0)
+    port = server.addr[1]
+    server.close
+
+    pid = fork do
+      exec "#{bin}/miniserve", "#{bin}/miniserve", "--if", "127.0.0.1", "--port", port.to_s
+    end
+
+    sleep 2
+
+    begin
+      read = (bin/"miniserve").read
+      assert_equal read, shell_output("curl localhost:#{port}")
+    ensure
+      Process.kill("SIGINT", pid)
+      Process.wait(pid)
+    end
+  end
+end
