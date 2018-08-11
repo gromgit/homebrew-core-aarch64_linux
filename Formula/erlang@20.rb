696a28b9d80ed67e9c12686f2fa067f31f333bd6
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-11 04:55:50 +0000

    erlang@20: update 20.3.8.5 bottle.

--- Formula/erlang@20.rb
+++ Formula/erlang@20.rb
@@ -7,9 +7,9 @@ class ErlangAT20 < Formula
 
   bottle do
     cellar :any
-    sha256 "d6c7319f204df640871f55424d4ccb5c19a08fd844d6c5893899965744284b7f" => :high_sierra
-    sha256 "ae4952a4e831c38f2ac5b220bd4ca0c71c04199170d9f79c8c34160601d97c80" => :sierra
-    sha256 "4cf23a13e5e7608f56830088b80e9a9f2f3a21a0cd2ddf2d7ca7d8896f79e943" => :el_capitan
+    sha256 "2bbd6e8f2fa8876ae3b92828df0fb2d34ab19b7d955a6a8f782b3a8bb85670f9" => :high_sierra
+    sha256 "3941343b426d343ed42d3133c57aee3bccac2efcd68629d45f54edbdcbe2431d" => :sierra
+    sha256 "faacf22bda8ea94c698da8597b4a9c0e5e77782a7d2bc58685a7500a3b86d1b0" => :el_capitan
   end
 
   keg_only :versioned_formula
