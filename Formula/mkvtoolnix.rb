91fd83027d1ba29fd5628db6c504f74c609a3adb
Alexara Wu <alexarawu@outlook.com>
2018-08-11 12:59:20 +0800

    mkvtoolnix: fix HEAD url

--- Formula/mkvtoolnix.rb
+++ Formula/mkvtoolnix.rb
@@ -11,7 +11,7 @@ class Mkvtoolnix < Formula
   end
 
   head do
-    url "https://github.com/mbunkus/mkvtoolnix.git"
+    url "https://gitlab.com/mbunkus/mkvtoolnix.git"
     depends_on "automake" => :build
     depends_on "autoconf" => :build
     depends_on "libtool" => :build
