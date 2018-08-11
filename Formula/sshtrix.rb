d20b554ae6920a02ec88d690f09176de43feb15c
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-11 05:13:31 +0100

    sshtrix: revision for libssh

--- Formula/sshtrix.rb
+++ Formula/sshtrix.rb
@@ -3,6 +3,7 @@ class Sshtrix < Formula
   homepage "http://www.nullsecurity.net/tools/cracker.html"
   url "https://github.com/nullsecuritynet/tools/raw/master/cracker/sshtrix/release/sshtrix-0.0.2.tar.gz"
   sha256 "dc90a8b2fbb62689d1b59333413b56a370a0715c38bf0792f517ed6f9763f5df"
+  revision 1
 
   bottle do
     cellar :any
@@ -17,6 +18,9 @@ class Sshtrix < Formula
   depends_on "libssh"
 
   def install
+    # https://github.com/nullsecuritynet/tools/issues/6
+    inreplace "Makefile", "-lssh_threads", ""
+
     bin.mkpath
     system "make", "sshtrix", "CC=#{ENV.cc}"
     system "make", "DISTDIR=#{prefix}", "install"
