5849bc9049fb1f934a0b808b2ea1ebf5b005f038
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-11 08:49:45 +0000

    unixodbc: update 2.3.7 bottle.

--- Formula/unixodbc.rb
+++ Formula/unixodbc.rb
@@ -5,9 +5,9 @@ class Unixodbc < Formula
   sha256 "45f169ba1f454a72b8fcbb82abd832630a3bf93baa84731cf2949f449e1e3e77"
 
   bottle do
-    sha256 "cd5a1ea2f0ba6db321b63514b2c33c0c1d74ada9541de8390cca0bc349f4845d" => :high_sierra
-    sha256 "2ccf3f7384697dd2460631a4afb685465a93c8f64fb0217c7485ab8919606e7d" => :sierra
-    sha256 "3330aee67c21712979a5e0cca8360f691b3e3bd0dd751fdeb15af998d2e6814c" => :el_capitan
+    sha256 "6f16f12d3463655c3b3fc8251083f77a31b0a690ecf6ac88f4b0daea2f060044" => :high_sierra
+    sha256 "4cf86c20705681ed7978e0a390d84df26264d1c41b21899e034da47c8e1803ad" => :sierra
+    sha256 "85be7365deb1229df2f46ccaa71ed1a5f6083135649e42a4b345ce9e55db4140" => :el_capitan
   end
 
   depends_on "libtool"
