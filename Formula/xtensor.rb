eacc96af24d50cac6623557e5465110d9c0bf8e6
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-11 05:41:22 +0100

    xtensor 0.17.1
    Closes #31019.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/xtensor.rb
+++ Formula/xtensor.rb
@@ -1,8 +1,8 @@
 class Xtensor < Formula
   desc "Multi-dimensional arrays with broadcasting and lazy computing"
   homepage "http://quantstack.net/xtensor"
-  url "https://github.com/QuantStack/xtensor/archive/0.17.0.tar.gz"
-  sha256 "8a9f028e0d36428ad2536b5d9bfb1b47b464d03b886cb42f56ce4936080a151a"
+  url "https://github.com/QuantStack/xtensor/archive/0.17.1.tar.gz"
+  sha256 "4c7df152ec712c581dac448c90918a6d1c659959cb59c079cb4ee2da1425ac01"
 
   bottle do
     cellar :any_skip_relocation
@@ -11,12 +11,13 @@ class Xtensor < Formula
     sha256 "dc113235fa1c774e30dfc781bfbc0aec45c9afa3e7fb535ca4b583555653d4d2" => :el_capitan
   end
 
-  needs :cxx14
   depends_on "cmake" => :build
 
+  needs :cxx14
+
   resource "xtl" do
-    url "https://github.com/QuantStack/xtl/archive/0.4.13.tar.gz"
-    sha256 "60dd545ea9054d313437ec74cbbe62048803ec9f80086998200cf4fa9d31aabb"
+    url "https://github.com/QuantStack/xtl/archive/0.4.14.tar.gz"
+    sha256 "f32390060315a7a941d9601ea1f0200c723e21313bd034f36dd5aae52b7a58c8"
   end
 
   def install
