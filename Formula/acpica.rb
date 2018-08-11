fdbcda107cce9c606f86b0400514943873654903
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-11 22:14:21 +0000

    acpica: update 20180810 bottle.

--- Formula/acpica.rb
+++ Formula/acpica.rb
@@ -7,9 +7,9 @@ class Acpica < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b005ade6f4bff5392f7b89107031d61bc48bc0db9c6cceb9c01736af8ec424d9" => :high_sierra
-    sha256 "b972b7bad6eb5c4b582c1980dae0400c2fb3925b8a4cbedd6008fe008199e33a" => :sierra
-    sha256 "b6034ee6f001ab5a2f33856a826c63c2d735d0d73dc9dfc41da627e15696183c" => :el_capitan
+    sha256 "ead5eaa7c0fda1e28aba6dde8932f6cd87e6ed41cc24ca7cae3a988be8472ed2" => :high_sierra
+    sha256 "b8fc8e51abb8bee1a22fc0cfbcefbd16c717f8248e17b5228dcc4eede15da4ab" => :sierra
+    sha256 "e1e59d509d939e868ec40fb0a572f6c8c5facf52f2a1e4a6e44e987f3b1e82b0" => :el_capitan
   end
 
   def install
