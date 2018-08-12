9d36aff9220e42328d283a5cef08d681418ebee8
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-12 03:01:53 +0100

    checkstyle 8.12
    Closes #31042.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/checkstyle.rb
+++ Formula/checkstyle.rb
@@ -1,8 +1,8 @@
 class Checkstyle < Formula
   desc "Check Java source against a coding standard"
   homepage "https://checkstyle.sourceforge.io/"
-  url "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.11/checkstyle-8.11-all.jar"
-  sha256 "9bbbaa85a9d2efca3c504bb8282d1a72feb8b0149551a616313de66333a9a48f"
+  url "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.12/checkstyle-8.12-all.jar"
+  sha256 "7ef49298cc5cf7fd70bce90e38b4089560631f3f00ae33acb47c424c1d220000"
 
   bottle :unneeded
 
@@ -15,7 +15,7 @@ class Checkstyle < Formula
     path = testpath/"foo.java"
     path.write "public class Foo{ }\n"
 
-    output = `#{bin}/checkstyle -c /sun_checks.xml #{path}`
+    output = Utils.popen_read("#{bin}/checkstyle -c /sun_checks.xml #{path}")
     errors = output.lines.select { |line| line.start_with?("[ERROR] #{path}") }
     assert_match "#{path}:1:17: '{' is not preceded with whitespace.", errors.join(" ")
     assert_equal errors.size, $CHILD_STATUS.exitstatus
