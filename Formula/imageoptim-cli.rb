c8a4240b97888e7f54bb703fe7ea3460adccc806
Christian Moritz <chrmoritz@gmail.com>
2018-08-13 21:10:10 +0200

    imageoptim-cli: build nexe node from source
    Closes #31185.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/imageoptim-cli.rb
+++ Formula/imageoptim-cli.rb
@@ -5,6 +5,7 @@ class ImageoptimCli < Formula
   homepage "https://jamiemason.github.io/ImageOptim-CLI/"
   url "https://github.com/JamieMason/ImageOptim-CLI/archive/2.0.3.tar.gz"
   sha256 "47fc8a1f14478389cb71dc8a03ac6b3176ba311d1a2390867b792b60ef209fb3"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -15,8 +16,18 @@ class ImageoptimCli < Formula
 
   depends_on "node" => :build
 
+  resource "node" do
+    url "https://nodejs.org/dist/v10.9.0/node-v10.9.0.tar.xz"
+    sha256 "d17ef8eb72d6a31f50a663d554beb9bcb55aa2ce57cf189abfc9b1ba20530d02"
+  end
+
   def install
-    system "npm", "install", *Language::Node.local_npm_install_args
+    # build node from source instead of downloading precompiled nexe node binary
+    resource("node").stage buildpath/".brew_home/.nexe"
+    inreplace "package.json", "\"build:bin\": \"nexe --target 'mac-x64-10.0.0' --input",
+      "\"build:bin\": \"nexe --build --target 'mac-x64-#{resource("node").version}' --loglevel=verbose --input"
+
+    system "npm", "ci", *Language::Node.local_npm_install_args
     system "npm", "run-script", "build"
     libexec.install "dist", "osascript"
     bin.install_symlink libexec/"dist/imageoptim"
