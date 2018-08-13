e41b3eba01955297a6d4918a625150fcc8cd703b
Antonio Juan Querol <anquegi@gmail.com>
2018-08-13 12:07:38 +0200

    gnu-smalltalk: update HEAD url
    Closes #31060.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/gnu-smalltalk.rb
+++ Formula/gnu-smalltalk.rb
@@ -5,7 +5,7 @@ class GnuSmalltalk < Formula
   mirror "https://ftpmirror.gnu.org/smalltalk/smalltalk-3.2.5.tar.xz"
   sha256 "819a15f7ba8a1b55f5f60b9c9a58badd6f6153b3f987b70e7b167e7755d65acc"
   revision 7
-  head "https://github.com/bonzini/smalltalk.git"
+  head "https://github.com/gnu-smalltalk/smalltalk.git"
 
   bottle do
     sha256 "9d086ae46e600651c937a8602fbb3a2c1fbe44be15517fe210dd46d21d7a391c" => :high_sierra
