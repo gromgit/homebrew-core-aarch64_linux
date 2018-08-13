da4f45ea1c8b8d03ef768c0b3a7b555e61ec7343
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-13 16:52:15 +0000

    pcapplusplus: add 18.08 bottle.

--- Formula/pcapplusplus.rb
+++ Formula/pcapplusplus.rb
@@ -4,6 +4,13 @@ class Pcapplusplus < Formula
   url "https://github.com/seladb/PcapPlusPlus/archive/v18.08.tar.gz"
   sha256 "dff6f7c677b2050f880043b125e984238cd8af0f1c25864e09e87fb8d71ec9ab"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2e9f6a851da52dd885e284b0393392dfbb2dec52b56517a553087fa67a608ddd" => :high_sierra
+    sha256 "abdd89390998f292ed04f518471e815767285bd004165933de082d753dc44af1" => :sierra
+    sha256 "bf6c8b35cc86e7acd5ddbed6ac0051a175e3329befa670fa432c6f4dc8eb6575" => :el_capitan
+  end
+
   def install
     system "./configure-mac_os_x.sh", "--install-dir", prefix
 
