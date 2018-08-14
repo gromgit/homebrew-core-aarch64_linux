1d9db35d2d0e688e514495abaffd5e87643aace9
Chongyu Zhu <i@lembacon.com>
2018-08-14 19:22:46 +0800

    wtf 20180813
    Closes #31088.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/wtf.rb
+++ Formula/wtf.rb
@@ -1,8 +1,8 @@
 class Wtf < Formula
   desc "Translate common Internet acronyms"
   homepage "http://cvsweb.netbsd.org/bsdweb.cgi/src/games/wtf/"
-  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20180704.tar.gz"
-  sha256 "3c305d6f4eae67705ffc0743cb4a28db7369096bfb9bba3fb80050954f673069"
+  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20180813.tar.gz"
+  sha256 "194b49de7d45fbd416ad43a1f8854157fdaf26b12553dffdf07e3ac8cddaf2d5"
 
   bottle :unneeded
 
