d95d05b34a36731295b20a683a290acc48eb30b1
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-14 20:57:22 +0000

    lxc: update 3.4 bottle.

--- Formula/lxc.rb
+++ Formula/lxc.rb
@@ -6,9 +6,9 @@ class Lxc < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "1551762d624891d174fa7261b4427154583d5cdb70f9ee63545abf2d25b25c39" => :high_sierra
-    sha256 "00c6c492cd9f99c19deeeafb0fbce62e69f8103c675f16890b8684371e8e3e5a" => :sierra
-    sha256 "c00f87d8f8696e95f67743e8ae0e22a78004e02b7f76bafd817325e5fa4f356a" => :el_capitan
+    sha256 "c6df3c61a2e3d6345f5d219e0153be0c2bddea141101aa20cfb5649851a82c08" => :high_sierra
+    sha256 "7584a5da68b9f3be6febf6e88c3353fc51a9d3d7d3ced74da1a8609cf2fd224d" => :sierra
+    sha256 "49e32a3821d790f65ee7017ad40e0fa1a6a84af56f509d0e86cc17051c2a754d" => :el_capitan
   end
 
   depends_on "go" => :build
