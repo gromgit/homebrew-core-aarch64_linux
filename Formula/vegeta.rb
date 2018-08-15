e2c469dc2fa4fd9f5358b872151cd57c679aed2d
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-15 18:09:50 +0000

    vegeta: update 9.0.0 bottle.

--- Formula/vegeta.rb
+++ Formula/vegeta.rb
@@ -7,9 +7,9 @@ class Vegeta < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "52401d4882d98aef57d695cb3481a6158e8b0ddfc8bc192ff6029fc9a3f678dd" => :high_sierra
-    sha256 "25c88db731fb87d4681a314b3b87cc92c5862c0f9bfb2f29c00e536b24d38f16" => :sierra
-    sha256 "b2d85acb18a407655a1472a12569af65880c8ed1746669fc30c5b309f8e15298" => :el_capitan
+    sha256 "47132327a0429026325be9a2337574983bff9c4eb936120817ddba497588a0f1" => :high_sierra
+    sha256 "3229a8a72dd0f32c7569f7969580c8881ad5869ec78f9d8c6477385bf44874bc" => :sierra
+    sha256 "e39875b2e23dcca9f086e0880ce87a3289f551d91381edae0472923d78ef1a1f" => :el_capitan
   end
 
   depends_on "dep" => :build
