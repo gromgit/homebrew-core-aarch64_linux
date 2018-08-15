9d58dd0a44d1790268031dd43c126df74d8d2093
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-15 03:39:52 +0100

    igv 2.4.14
    Closes #31116.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/igv.rb
+++ Formula/igv.rb
@@ -1,8 +1,8 @@
 class Igv < Formula
   desc "Interactive Genomics Viewer"
   homepage "https://www.broadinstitute.org/software/igv"
-  url "https://data.broadinstitute.org/igv/projects/downloads/2.4/IGV_2.4.13.zip"
-  sha256 "41e1a15dcb0afd7d4fa97e28dee2860b86936fa8a07b4db0fc13e7b483768632"
+  url "https://data.broadinstitute.org/igv/projects/downloads/2.4/IGV_2.4.14.zip"
+  sha256 "0c6e466736ac08b2aba28f1ffa4c9f136b3948dcbb61b04a89afcdcf0298307d"
 
   bottle :unneeded
 
@@ -10,7 +10,7 @@ class Igv < Formula
 
   def install
     inreplace "igv.sh", /^prefix=.*/, "prefix=#{libexec}"
-    libexec.install "igv.sh", Dir["*.jar"]
+    libexec.install "igv.sh", "lib"
     (bin/"igv").write_env_script libexec/"igv.sh", Language::Java.java_home_env("1.8")
   end
 
