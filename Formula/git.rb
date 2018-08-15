db579a4f633c3dfde12c5236b9ea0695dce6cc5e
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-15 21:51:16 +0000

    git: update 2.18.0 bottle.

--- Formula/git.rb
+++ Formula/git.rb
@@ -6,6 +6,7 @@ class Git < Formula
   head "https://github.com/git/git.git", :shallow => false
 
   bottle do
+    sha256 "e79618cad4a991104ca426ae84858880f37b1a5b528ad00975ba069b76b40834" => :mojave
     sha256 "1fdceaff995cc18ddff3e56cdfc30bb363f69aecde01925985f3951ad21ae340" => :high_sierra
     sha256 "05653ebe3f1d154be4b86ea73728a36d45c021cda81c9508534c433601ea3509" => :sierra
     sha256 "cb0c4eac4a49fbd1936bae4070ecac3c3543aecfb2ba09869f6f6fd2aeb11015" => :el_capitan
