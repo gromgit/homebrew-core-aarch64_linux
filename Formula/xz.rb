67ef6903d9b8440bdba27dd71f786b2d277b4a7a
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-15 21:51:10 +0000

    xz: update 5.2.4 bottle.

--- Formula/xz.rb
+++ Formula/xz.rb
@@ -9,6 +9,7 @@ class Xz < Formula
 
   bottle do
     cellar :any
+    sha256 "010667293df282c8bceede3bcd36953dd57c56cef608d09a5b50694ab7d4b96b" => :mojave
     sha256 "e7be50f4ee00e35887f3957263334eb3baba59e8c061919060f9259351be6880" => :high_sierra
     sha256 "bcc71ee69e2c43bf56b9c9ece5a53dc3439652f355620a25b020f794cd447fb7" => :sierra
     sha256 "974aae83ba7ceb62040c5bf02b1fb277a919212714c8da2a4c5eb3d1d119a465" => :el_capitan
