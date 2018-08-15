a48ad8ae99ad66da3951e5e918f14c6d53943180
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-15 16:29:51 +0000

    heroku: update 7.7.10 bottle.

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -10,9 +10,9 @@ class Heroku < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "71abf71f556dab06bec160f71bd8ef992c2fe7bae209ad7258c3335efe281dea" => :high_sierra
-    sha256 "b26dd90e75cfbae5c32fb25f80a42a9261dfe60e9c3725637c7a703c7ed7a4e1" => :sierra
-    sha256 "808af2964b70ba829c6426bd6e86e90b9bf1bbcdd736ba6f02de10b36524f586" => :el_capitan
+    sha256 "5c829f8a8f7ef0ceeee7e74f8be246d79068fcd17fe0642fae39138e5a9950be" => :high_sierra
+    sha256 "084c3c7b276058ee040614013fa732a0eceab9ec1364f2afb2ac85172845677c" => :sierra
+    sha256 "a4b0d608feff126f01d7118ee7cd306d74f10956ec9894d7200eba7de2b91d6d" => :el_capitan
   end
 
   depends_on "node"
