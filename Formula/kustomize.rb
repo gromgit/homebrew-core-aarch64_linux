36dd0c0348a9689dceb72d76ba44023f7b5d51e7
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-15 18:48:41 +0000

    kustomize: add 1.0.6 bottle.

--- Formula/kustomize.rb
+++ Formula/kustomize.rb
@@ -5,6 +5,13 @@ class Kustomize < Formula
       :tag => "v1.0.6",
       :revision => "017c4ae0aa19195db2a51ecc5aa82c56a1f1c99b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d4f64eef42f2f1e2bc6228814c34fa89b981c8b5fcb0944bcc3de48f521f8221" => :high_sierra
+    sha256 "9dcf245e8c3f44b9214137f658c672517ce720c73f57cb9d005b96de0654ebc1" => :sierra
+    sha256 "a13b5ca673dd8ee8105dd74350d9da517fdb4f931834cc0647c578de7e3c86a2" => :el_capitan
+  end
+
   depends_on "go" => :build
 
   def install
