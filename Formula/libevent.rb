6304e664ae4ebdec8ec9369f79ad621ca80334d0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-16 17:11:13 +0000

    libevent: update 2.1.8 bottle.

--- Formula/libevent.rb
+++ Formula/libevent.rb
@@ -6,6 +6,7 @@ class Libevent < Formula
 
   bottle do
     cellar :any
+    sha256 "9ab81f4ca9902042f7ea95d02bf36394c5cdc11d715b7f928badc9cf5724ca8b" => :mojave
     sha256 "61a8cf2df6d58f79678fad0f798b6a9d368245097f908e2f911f25cb3f7916cf" => :high_sierra
     sha256 "cdd11d67b5f49b94cf3fbfd24753f84082957e3d55680e2b5979eec19091e694" => :sierra
     sha256 "136a93a91c3724d0403b0d43d0b9a4bf6b857278c4ebb7c7585ef70a19b0964c" => :el_capitan
