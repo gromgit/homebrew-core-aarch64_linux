2808cd3c6a03dae0afa79720b4909e1285dc9789
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-16 11:58:20 +0000

    docbook: update 5.0 bottle.

--- Formula/docbook.rb
+++ Formula/docbook.rb
@@ -7,6 +7,7 @@ class Docbook < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 3
+    sha256 "f02d3ab50c375f42e90c1d8f8e8e3703372344b90cc3a9f652f876dc39f5cbf2" => :mojave
     sha256 "0f99e9e2e42f4a21bdb9066f02e247c919c21f586cea0bcd787c9112659df030" => :high_sierra
     sha256 "40b2740609c1586d030d3b9a131761821425c211989bee297af29900981b3aba" => :sierra
     sha256 "3fb7e4070eaa9250fa947d38e3d7803d37c159d9765e3f71397702d5ad6bb578" => :el_capitan
