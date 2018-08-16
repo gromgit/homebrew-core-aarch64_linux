1b4e0d4dafc2b52f214191aaf6904d5dfb3c7e03
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-16 08:37:14 +0000

    libssh2: update 1.8.0 bottle.

--- Formula/libssh2.rb
+++ Formula/libssh2.rb
@@ -7,6 +7,7 @@ class Libssh2 < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "9705f2a153a854b15bff89663eca46dd211f5fc025031b9851d64874f83c8f53" => :mojave
     sha256 "22327eb5bbff660935db0c5106d5a43069ee23e5cb33d5125bad4e144e83ee34" => :high_sierra
     sha256 "4a1e39137bc9461d779a7a84626354928788aeb0650fb0fed75e0fbecb95c0cd" => :sierra
     sha256 "d6693c1417f0deb8f1b0c6a7c338491a7f60f2cc516675186e572329c1fcaa6c" => :el_capitan
