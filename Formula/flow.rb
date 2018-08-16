13795eef79087468056e7c8e96ba4efb7fada016
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-16 20:36:12 +0000

    flow: update 0.79.0 bottle.

--- Formula/flow.rb
+++ Formula/flow.rb
@@ -7,9 +7,9 @@ class Flow < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "823d2985153026624d2513fd5b1da3fda1ce5ef22aa93a5be13dded46df31743" => :high_sierra
-    sha256 "9ef77c602d075a629242e658728b30f560a5ff27f3386953aa2360d608815d78" => :sierra
-    sha256 "507622ee800f33d9ca8ccd031855d1643f024f09af948cdda0e74cdbee233e6d" => :el_capitan
+    sha256 "e8d35e3a50022de2a83945bf6f2c40f27467070cc089180c4045d2e32d7e1ffb" => :high_sierra
+    sha256 "fb8559cec27450f6a775f71f2a8019a0100ebca1cee8c99a9745fc606b5e8c75" => :sierra
+    sha256 "ce2db31a868a7d229a3188e0a2206c7a840b32e641fd97073d01e889ebd493b4" => :el_capitan
   end
 
   depends_on "ocaml" => :build
