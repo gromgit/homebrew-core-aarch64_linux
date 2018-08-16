fd9d476d0e8e2608d8204275f777d8106ce7604f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-16 19:07:09 +0000

    npth: update 1.6 bottle.

--- Formula/npth.rb
+++ Formula/npth.rb
@@ -7,6 +7,7 @@ class Npth < Formula
 
   bottle do
     cellar :any
+    sha256 "bb0232908eedb717f98d636b910478ef4ce044866545725344ecae0b85251e1d" => :mojave
     sha256 "51a68f02a29f9b1a596048894be6425696872ddbbc928b372c07a5e256df8ba8" => :high_sierra
     sha256 "930defbdfa6136f82abdaa7efea0328390079d13f284798756997217eb31427d" => :sierra
     sha256 "8b2591ec804a0e410e8bf8657487f2d26248307e7cf74b2e49906037618ebc03" => :el_capitan
