45e782f9d9e3a9b9846462cac0359f1a829da56d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-16 17:11:13 +0000

    ffmpeg: update 4.0.2 bottle.

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -6,6 +6,7 @@ class Ffmpeg < Formula
   head "https://github.com/FFmpeg/FFmpeg.git"
 
   bottle do
+    sha256 "83f8f5c5b79b7bbe6e13ab918f4bd8effb1eba2e7091a5776eeb31db84e74102" => :mojave
     sha256 "5522a58020c3b5c68e5628818ee746822fe38747810c29b4aa865f4fe2803d57" => :high_sierra
     sha256 "98cdb186d1dd9c9eb2149b5d5d5c15dc13cecb58fed25232abcbeeb1236c92b8" => :sierra
     sha256 "4ef0ba0bd512fff69004bb33417a8516d1d8e9dc5e3eb05ea9f2ac15f557cdb2" => :el_capitan
