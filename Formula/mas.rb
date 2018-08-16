15004382d954b477c66ed6d524c53d399567663b
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-16 08:52:11 +0000

    mas: update 1.4.1 bottle.

--- Formula/mas.rb
+++ Formula/mas.rb
@@ -7,6 +7,7 @@ class Mas < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "fb05286adfb9ab0a63a8027391647fb874fa810c07d412505f5bc7911139a3f2" => :mojave
     sha256 "b7585ced3a93d60e95357e93d729913a6f628fda82359e77c6553c2e802c50dc" => :high_sierra
     sha256 "af5be6aa9902d9cfc2aa69dbf313441a7c201463d516face721f900ceae9556b" => :sierra
   end
