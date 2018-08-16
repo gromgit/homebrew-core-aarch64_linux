75f25c6f97e32d018202a5bc3439b499d792e549
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-16 11:58:22 +0000

    libassuan: update 2.5.1 bottle.

--- Formula/libassuan.rb
+++ Formula/libassuan.rb
@@ -7,6 +7,7 @@ class Libassuan < Formula
 
   bottle do
     cellar :any
+    sha256 "3734fe43c829fd0586e71c1f458a87d5485bdef24db4d63541995d0ff1d8495e" => :mojave
     sha256 "a05af2506efccbd153b216c2ed6b17c7b28b1c50fce16489166782877695b8ee" => :high_sierra
     sha256 "3aefafdbaf62a8319b618c73654aef88f9ebe8b9cba51a304aebaa09bfc92c4c" => :sierra
     sha256 "6e4ef55130d531e2e24226379f70c6ea59e62caf4fcc8fabffdecf8b2a0d018e" => :el_capitan
