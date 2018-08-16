b4fb606b51f28cd715d6db96929b0b513e0278e5
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-16 08:37:13 +0000

    automake: update 1.16.1_1 bottle.

--- Formula/automake.rb
+++ Formula/automake.rb
@@ -8,6 +8,7 @@ class Automake < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0a359c2385d0673ce1ab3cdaf39dd22af191f7b74732105ca5751e08a334e061" => :mojave
     sha256 "fb32c065aaf91661380af32ed301edcf209ba453635c79ca945353b67e54af10" => :high_sierra
     sha256 "fb32c065aaf91661380af32ed301edcf209ba453635c79ca945353b67e54af10" => :sierra
     sha256 "d552844779f0dc4062f27203f7facfbd74c9d1780724ac76a86791e401aa73bd" => :el_capitan
