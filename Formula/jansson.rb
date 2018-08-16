9e25966c269437ba7130ae618d1f5ccfcddb7c44
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-16 08:52:11 +0000

    jansson: update 2.11 bottle.

--- Formula/jansson.rb
+++ Formula/jansson.rb
@@ -6,6 +6,7 @@ class Jansson < Formula
 
   bottle do
     cellar :any
+    sha256 "073c0c6625eed1682fa386b2ed235bb2d62f4003060736991c5b81531f7ce319" => :mojave
     sha256 "d25f04f7b7b68d880cb22bfd9f0c2d071419e5e90f7899337a534c4feadffbf4" => :high_sierra
     sha256 "2136b357ac1d0df8ff75e8ff7602cbc390c58908d4394dc1a38fe1be98601347" => :sierra
     sha256 "421a63d722386003c678c7fd18de6114b9a8ffa5d8c0994db5f117e3cb8247ec" => :el_capitan
