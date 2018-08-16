aaa69d507eff8c0296433611031fc4d0cc926f27
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-16 21:17:58 +0000

    vrpn: update 07.34 bottle.

--- Formula/vrpn.rb
+++ Formula/vrpn.rb
@@ -7,9 +7,9 @@ class Vrpn < Formula
 
   bottle do
     cellar :any
-    sha256 "a254843eaac154ae39da044efacb4725c8028e42914c4fb10cc7d524b6ecd7d0" => :high_sierra
-    sha256 "3f392e7ab5c352a3da3e60795a4255280cd8a53e2110e588eac2fb9c910685e0" => :sierra
-    sha256 "b9a6caa7c8ac287479ef8a09bd54362a56b73fe80b8aac1a233864be97fc1567" => :el_capitan
+    sha256 "9b9f4a31161dbc0a4a9ea0759122f0a3725a361dde0b5f1def9bab4e59de12e7" => :high_sierra
+    sha256 "4e03c131adba54f74742151ee269d2d0c1716e307294679ed2366c0e6cb5fd41" => :sierra
+    sha256 "36e5273f8006b1fe5f1655e258f8937e06e9abc4ad849e2c9b1e7a1462fe790d" => :el_capitan
   end
 
   option "with-clients", "Build client apps and tests"
