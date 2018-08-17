542e0410274623a6ff3dc0308faf36f79254b178
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-17 02:59:01 +0000

    cash-cli: update 2.0.7 bottle.

--- Formula/cash-cli.rb
+++ Formula/cash-cli.rb
@@ -8,6 +8,7 @@ class CashCli < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2860ba578814b3b4a2928e705f6214be6a947f5c078ce9dd1feb53d196455cd3" => :mojave
     sha256 "82ce4c7f0b52ea68df08614453a5d6c5ead0a46bafcd02bc6b3db13dfc355fb3" => :high_sierra
     sha256 "eccf8aa349797727e8332b7f7990000d49edd3ca5d0de991b8c6b23911ed9f81" => :sierra
     sha256 "3b353f58868294256d20c00fff8ee2b7af9dab8281bdeebeace7d59ced2e103a" => :el_capitan
