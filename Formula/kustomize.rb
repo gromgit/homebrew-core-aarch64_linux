df96223959931c6a0eb769f748aa1c440aa67cd5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-17 02:59:02 +0000

    kustomize: update 1.0.6 bottle.

--- Formula/kustomize.rb
+++ Formula/kustomize.rb
@@ -7,6 +7,7 @@ class Kustomize < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3623863ee12493f5cb769d628b3f028467dfd77c7f9d7031a145bba2babf879c" => :mojave
     sha256 "d4f64eef42f2f1e2bc6228814c34fa89b981c8b5fcb0944bcc3de48f521f8221" => :high_sierra
     sha256 "9dcf245e8c3f44b9214137f658c672517ce720c73f57cb9d005b96de0654ebc1" => :sierra
     sha256 "a13b5ca673dd8ee8105dd74350d9da517fdb4f931834cc0647c578de7e3c86a2" => :el_capitan
