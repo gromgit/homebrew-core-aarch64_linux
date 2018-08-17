a66584f36a7587121cc86adb7fa933c5aa09dff2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-17 16:14:38 +0000

    dxpy: update 0.260.0 bottle.

--- Formula/dxpy.rb
+++ Formula/dxpy.rb
@@ -8,9 +8,9 @@ class Dxpy < Formula
 
   bottle do
     cellar :any
-    sha256 "c869d10399117eb2aea0697edb62a90ecf9a7fcadedf2b678b67573cfa7f3687" => :high_sierra
-    sha256 "3c16eff2c86136e13972174958b80dc996deb4e1d3702de601c6be7fef3cde24" => :sierra
-    sha256 "940c866937735a98c7f78073ac674261de34740537c5673ffe7f403ba75644f0" => :el_capitan
+    sha256 "b40850ab3b154bd9fa731b4cf48405be60225bc75c333f285d08231baf566c67" => :high_sierra
+    sha256 "58f07c8b25fa496be28e90b7e0aab624698e2cafbef31c239f20a9dccf4166f9" => :sierra
+    sha256 "0adfd5a74e4965f844b92ac3446e1ae7468827f66a544f7c9a01e625b6c15a06" => :el_capitan
   end
 
   depends_on "python@2"
