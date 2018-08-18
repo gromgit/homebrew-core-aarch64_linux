8eb4601a25c94c76fe877341d98a7688e145e238
mattrussi <mattrussi@users.noreply.github.com>
2018-08-18 15:42:58 -0400

    perltidy 20180220 (new formula)
    Closes #31257.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ Formula/perltidy.rb
@@ -0,0 +1,34 @@
+class Perltidy < Formula
+  desc "Indents and reformats Perl scripts to make them easier to read"
+  homepage "https://perltidy.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/perltidy/20180220/Perl-Tidy-20180220.tar.gz"
+  sha256 "e9973ce28b7518108c1e68fa767c6566822480e739df275375a0dfcc9c2b3370"
+
+  def install
+    ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+    system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}",
+                                  "INSTALLSITESCRIPT=#{bin}",
+                                  "INSTALLSITEMAN1DIR=#{man1}",
+                                  "INSTALLSITEMAN3DIR=#{man3}"
+    system "make"
+    system "make", "test"
+    system "make", "install"
+    bin.env_script_all_files(libexec/"bin", :PERL5LIB => ENV["PERL5LIB"])
+  end
+
+  test do
+    (testpath/"testfile.pl").write <<~EOS
+      print "Help Desk -- What Editor do you use?";
+      chomp($editor = <STDIN>);
+      if ($editor =~ /emacs/i) {
+        print "Why aren't you using vi?\n";
+      } elsif ($editor =~ /vi/i) {
+        print "Why aren't you using emacs?\n";
+      } else {
+        print "I think that's the problem\n";
+      }
+    EOS
+    system bin/"perltidy", testpath/"testfile.pl"
+    assert_predicate testpath/"testfile.pl.tdy", :exist?
+  end
+end
