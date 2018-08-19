3d355917b7b2a0024d9729e52d4b796a50d21b44
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 10:17:58 +0000

    wget: update 1.19.5 bottle.

--- Formula/wget.rb
+++ Formula/wget.rb
@@ -6,6 +6,7 @@ class Wget < Formula
   sha256 "b39212abe1a73f2b28f4c6cb223c738559caac91d6e416a6d91d4b9d55c9faee"
 
   bottle do
+    sha256 "0e7d518c4c110edec0c684f2be9d44e31ec1542343392b712ef378b4e131ebd4" => :mojave
     sha256 "af8880a424be0cde4d2891b0027ae9991ebb0e6f48ae60b369e3c9f0bdfcd04a" => :high_sierra
     sha256 "d61954cc95b1f60b64b86afa2a71a18d6d1676dbc3d566a3f9ba2249ad028b54" => :sierra
     sha256 "473408a17dfea7393f2c8c96264535717328c3879db679fc09fc1007bac26113" => :el_capitan
