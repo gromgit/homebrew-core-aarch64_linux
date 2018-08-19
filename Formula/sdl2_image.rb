e8021738f1df7e6c584fd333a6313fb42f6267cf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 15:52:14 +0000

    sdl2_image: update 2.0.3 bottle.

--- Formula/sdl2_image.rb
+++ Formula/sdl2_image.rb
@@ -6,6 +6,7 @@ class Sdl2Image < Formula
 
   bottle do
     cellar :any
+    sha256 "4043fef6622dbe98c1eba0fe8936c718498bd608d351283931f4349c4009d86e" => :mojave
     sha256 "5e85d55eb46958bfba548130aebcccfab0a98f048063b0074980940851fa59a1" => :high_sierra
     sha256 "feeb2704291485affd5d3c4dee9842d4fdc006b2c0d8a49ab8010f2393dd2580" => :sierra
     sha256 "9172af9ae56b649b51f987a2ebee06f0c58ca070d63556fd65260ffbb1f11b76" => :el_capitan
