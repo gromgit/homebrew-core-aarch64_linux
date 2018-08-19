b76347c21bbe20accf0a514b138515e30a48ad12
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 15:13:51 +0000

    jq: update 1.5_3 bottle.

--- Formula/jq.rb
+++ Formula/jq.rb
@@ -7,6 +7,7 @@ class Jq < Formula
 
   bottle do
     cellar :any
+    sha256 "e76e19d5bcf1a6eebae188846f5656b5f615771beed5748897f4eb0fabd3151b" => :mojave
     sha256 "25be689b9bca3cef2ee0cb647388200d25f045f651679ef8871b8a86100f9e43" => :high_sierra
     sha256 "11e169f340dc1f93fbb3c21a87c3aaa7d1242967ed11672663e6e827e622ef0d" => :sierra
     sha256 "b3f95569c5d67db9c9c1e9eeff670e8769bd5b40ebf1e170bd64be7e62b8e576" => :el_capitan
