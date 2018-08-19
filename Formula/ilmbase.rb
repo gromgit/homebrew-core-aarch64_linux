63bf3ab117c43157295c7828f64a345f5d04c836
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 14:58:58 +0000

    ilmbase: update 2.2.1 bottle.

--- Formula/ilmbase.rb
+++ Formula/ilmbase.rb
@@ -6,6 +6,7 @@ class Ilmbase < Formula
 
   bottle do
     cellar :any
+    sha256 "63e5e1e6f1e17b8aa264b14cb1e8120a7e606b4f6173c85c945ea35ab911f8bc" => :mojave
     sha256 "5621509767a95332eff8e26f7fe80c6bce9c3c271fa8521e234263b3c3d67454" => :high_sierra
     sha256 "7b40da5907be805067a7af87b5a5af2dac9e446478de06316a059fa9c4f9a9c0" => :sierra
     sha256 "402fe7453b9ca2c4c4a3fbdb9557156819e26c959e18c096dcceab8b1b6ce9a5" => :el_capitan
