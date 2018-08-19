3a722f133c217a9c0b89d44428a47242ff1255b6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 19:32:51 +0000

    librdkafka: update 0.11.5 bottle.

--- Formula/librdkafka.rb
+++ Formula/librdkafka.rb
@@ -7,6 +7,7 @@ class Librdkafka < Formula
 
   bottle do
     cellar :any
+    sha256 "ff7dc8fd37b6a998330c63744e0a4f65568eb08154c8752786bc9fb113777926" => :mojave
     sha256 "9ac390ab143923e3cdc3cece266fab5d24cbe1f65f5f6865b706252abee4c6bb" => :high_sierra
     sha256 "7da3cc5dc1ba920c43a80ef36d635284dbdb03d74f83fbd8ed6a77d0d3200c51" => :sierra
     sha256 "ce900fd3577e37b7d2ed4a92c1ce09af84e5f6e11080cf324157a27e2aed755e" => :el_capitan
