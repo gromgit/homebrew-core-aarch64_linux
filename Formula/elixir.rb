228468c4b450bc166833bd77a1e684bc38d713f8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 15:17:04 +0000

    elixir: update 1.7.2 bottle.

--- Formula/elixir.rb
+++ Formula/elixir.rb
@@ -6,6 +6,7 @@ class Elixir < Formula
   head "https://github.com/elixir-lang/elixir.git"
 
   bottle do
+    sha256 "14dbf5d3ba0eee6867c9dc816c13b6852e75310ec1287c0d3ae04e8626adbfe3" => :mojave
     sha256 "3cd820e9f0ec6a3cc75302b4bf40528a7dc0412104cf93838121609872b806b8" => :high_sierra
     sha256 "d08bb4ae8f2d619d3350f0ba87430247ef593b9a47d8b982fbd36445831391cd" => :sierra
     sha256 "a8b1602ae833557331db2ff654801039336f7af098b5fd853ccf0d48796e3bf5" => :el_capitan
