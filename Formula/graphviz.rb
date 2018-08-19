d763fdd78dc07f2de5cfffc22e5c5e5f335b1c17
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 15:17:04 +0000

    graphviz: update 2.40.1 bottle.

--- Formula/graphviz.rb
+++ Formula/graphviz.rb
@@ -9,6 +9,7 @@ class Graphviz < Formula
 
   bottle do
     rebuild 1
+    sha256 "668c64749620f556cf7d26ac96088005b4439acd9488be4c640fdc9cfe66d563" => :mojave
     sha256 "b592ce51c2a929c3da82e96ec856571ebfc54cf4dac90c2924cd3845078d7082" => :high_sierra
     sha256 "41b5811054f03978db12525919540fe41e073fb2c20e899247ed9c2a191f7a66" => :sierra
     sha256 "cab27f92a59d543e2f2c1494c28c7563a4c2d7e0dce4c4fbc22587db91cafc5b" => :el_capitan
