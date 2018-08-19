8be9c1d99f13be5a5abd4e9cd635937dbc578af4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 15:55:53 +0000

    socat: update 1.7.3.2_2 bottle.

--- Formula/socat.rb
+++ Formula/socat.rb
@@ -7,6 +7,7 @@ class Socat < Formula
 
   bottle do
     cellar :any
+    sha256 "492cd9ef5b2f6bc959a8c5618a627f40b8e386d337ed4f5c4fc534d4daeb8c8b" => :mojave
     sha256 "8440c4f765cf7c068461e33088e80057fbfa6be598107666714ba7cd0bb8c97d" => :high_sierra
     sha256 "c630c4273615556c167da5875a67e28d79e51bc7493eb07ba73b7f7fead14372" => :sierra
     sha256 "78787f35cdbc55bc5bce5f12c253eace7db1ae39767d55ad1e032eadc4f33ad3" => :el_capitan
