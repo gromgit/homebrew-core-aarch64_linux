805fc389989b377efb052b563938042183be2261
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 15:55:54 +0000

    boost-build: update 1.67.0 bottle.

--- Formula/boost-build.rb
+++ Formula/boost-build.rb
@@ -8,6 +8,7 @@ class BoostBuild < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "632210e339a03240347ab9e57b35220d0f0c3a6222b68d65f6eda7134fcb15c0" => :mojave
     sha256 "d95eb10205664a7cf1c6acad39fbb90cb4510bc8a8d55466c20094f50f1a9ed4" => :high_sierra
     sha256 "ce84cea580da009b087e31b1de52add90798d4daa9b90222211069f90f1edecf" => :sierra
     sha256 "d16363f1e6d300e3a082fa62a16b6241dc3297ed092c14c6ffb06bbfdf1d241a" => :el_capitan
