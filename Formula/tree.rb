fc5ddb7eda2398dbfa145182b006cc72c37c19b1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 14:25:04 +0000

    tree: update 1.7.0 bottle.

--- Formula/tree.rb
+++ Formula/tree.rb
@@ -8,6 +8,7 @@ class Tree < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "a6f52904af4d3a3096f465b9f5b0cc778b98655da0298699b39d593c5eafa9fd" => :mojave
     sha256 "1a9ac89dc194918786c3175bdf439e74d16f7d27bd5fa4be7b006e5d0b077a1e" => :high_sierra
     sha256 "f7c8a080435445e17c254ca53b016d9012bf768851df67c3d892ba71fe98e924" => :sierra
     sha256 "2721bafcbe1db12e444f5025bdae81aa3c7cff6572aa7423e401baeb10a43157" => :el_capitan
