150464e9ea244ff4b9799248c1ec9ebca28a4b59
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 19:59:49 +0000

    neofetch: update 5.0.0 bottle.

--- Formula/neofetch.rb
+++ Formula/neofetch.rb
@@ -7,6 +7,7 @@ class Neofetch < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c119a528c4d573c284c42512d3e6ef6bf781c05b730c608150998dc92b9eb099" => :mojave
     sha256 "f87e12ccbe4ef1de1757f4f0d2f03c4aebee3db21d0f27cb2e54451f572f3836" => :high_sierra
     sha256 "f87e12ccbe4ef1de1757f4f0d2f03c4aebee3db21d0f27cb2e54451f572f3836" => :sierra
     sha256 "f87e12ccbe4ef1de1757f4f0d2f03c4aebee3db21d0f27cb2e54451f572f3836" => :el_capitan
