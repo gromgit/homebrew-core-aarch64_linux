2dc0fee07234496bf46c3adfc06c92dc211d5feb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 11:45:10 +0000

    little-cms2: update 2.9 bottle.

--- Formula/little-cms2.rb
+++ Formula/little-cms2.rb
@@ -9,6 +9,7 @@ class LittleCms2 < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "1c69f212b9754cbc1700e822ceb659103cb692afe2e26366c9ae9eb9e3fc612d" => :mojave
     sha256 "c232c3e514ef478c4fab797dab8db675045eae3611043063d338c256f4ecb941" => :high_sierra
     sha256 "a0ce195a712977870d9ddc414c0c5cd1b373d4e04b7130b80d00f911d04fe5b4" => :sierra
     sha256 "fa72bb1ce13889405ee93519be86ff1cede056d8c74e1d1671cca52013762ec0" => :el_capitan
