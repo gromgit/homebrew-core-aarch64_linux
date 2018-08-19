832d78610eaaa368d6d8881e41374600ec3296f0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 09:41:28 +0000

    libmpc: update 1.1.0 bottle.

--- Formula/libmpc.rb
+++ Formula/libmpc.rb
@@ -6,6 +6,7 @@ class Libmpc < Formula
 
   bottle do
     cellar :any
+    sha256 "1cb3a09238830d45d64a87f520f06122d24a020403fac1800c831d15c605282e" => :mojave
     sha256 "3b28ec506ab53ef5f3163e87fb72ae735b7f91ee2fc20fe184cf1241481b72a5" => :high_sierra
     sha256 "18d620a1612bc51b1fbd1b3b62c9c73766b90549c746740c5a27d2ab1ec5ede7" => :sierra
     sha256 "6f19f936781dae0db248abdd84a72c3e25451c44379706bc3800760f0aa43888" => :el_capitan
