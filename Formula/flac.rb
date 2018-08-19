fd2154cb06e1f106a415d1750dddd8a3dd650390
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 13:00:25 +0000

    flac: update 1.3.2_1 bottle.

--- Formula/flac.rb
+++ Formula/flac.rb
@@ -8,6 +8,7 @@ class Flac < Formula
 
   bottle do
     cellar :any
+    sha256 "e3972de751c58d0b835ef606f5d21c4ef22bbac927b17f75440259fb2da06bd6" => :mojave
     sha256 "b439d1a0321f9488ad0a2edfe3307402cca4e7f5e560a833078fa29561fadacd" => :high_sierra
     sha256 "27aef309b675e9946f6ac4d090a0322d0888789087b2e38cbaaabc527eb3f22b" => :sierra
     sha256 "17fb6eec1e71416a0000e507953babd4fca2a0204f48ae064d02b76b906dc096" => :el_capitan
