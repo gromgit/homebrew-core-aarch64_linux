50104761d0269a9f03f2b6101c1a2bce22b31bb0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 19:47:45 +0000

    gpp: update 2.25 bottle.

--- Formula/gpp.rb
+++ Formula/gpp.rb
@@ -6,6 +6,7 @@ class Gpp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1321760033d09fb720739a4f261b55873ff68f76241074bd2608c4d9b07cb3c1" => :mojave
     sha256 "41c9826ea93e97915c36193f637c3835e165fa90aee154d29b708895bbdd73af" => :high_sierra
     sha256 "ceec5eeca75c3ad3e2192f9fcef09857533de93e92413cc501e23e9b494da0f7" => :sierra
     sha256 "2a7f2e1371f6bcbff03db5b9b260c47cd41cb25ec1e5726dfb5ef838f3c417e0" => :el_capitan
