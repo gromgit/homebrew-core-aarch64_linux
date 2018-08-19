6084130f2024708fe6a8ad62b21eda18fa3da312
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 21:55:12 +0000

    influxdb: update 1.6.1 bottle.

--- Formula/influxdb.rb
+++ Formula/influxdb.rb
@@ -8,6 +8,7 @@ class Influxdb < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "33ca20a6d80aaa33ed3b53cf59c74b666a2062ebefa5c01f98c7d380f178ed4e" => :mojave
     sha256 "1939a952dd1bb623a9fa8e388e50b6f5062970475f35abe791eae5aaacee182d" => :high_sierra
     sha256 "0da1f369e02bdeac1c61c572183cabf3bde91e4b3323903e0655dcb2b3e2da4f" => :sierra
     sha256 "20e545b5ad265253f90e13afe004c89fbca03a1232c852d9400cd1413c41a6f9" => :el_capitan
