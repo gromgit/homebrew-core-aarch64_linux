f29f5614bc871936e693a39a24a0a9778a0e8e05
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 15:44:34 +0000

    libwmf: update 0.2.8.4_2 bottle.

--- Formula/libwmf.rb
+++ Formula/libwmf.rb
@@ -6,6 +6,7 @@ class Libwmf < Formula
   revision 2
 
   bottle do
+    sha256 "3e0e8ecd7072819856393b472017d1e7fc3995a6d8568c6ad65b7d1055efc2e7" => :mojave
     sha256 "a96fe2e0aef8cd0f8eecce05b8789c2637f973a6ae358924c451b8f36b3a70ef" => :high_sierra
     sha256 "9df806eb6a4a3ca1a2b4b656ff02623175892981fbf136c89d4df5b5853bd20c" => :sierra
     sha256 "205bf519460576ecf73e9314ba1171542be58ea22cea81c26424d661734f2d2f" => :el_capitan
