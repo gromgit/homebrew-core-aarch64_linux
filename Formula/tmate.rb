63a4d6fd0b157f91a4e11ac2af6d214103554c3c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 04:26:02 +0000

    tmate: update 2.2.1_4 bottle.

--- Formula/tmate.rb
+++ Formula/tmate.rb
@@ -8,10 +8,9 @@ class Tmate < Formula
 
   bottle do
     cellar :any
-    sha256 "d2c18c43ddcc8ce25d01353d58d9b93f38fe066a2250cdfd99f70b9f4f724c36" => :high_sierra
-    sha256 "6fcea69412c5d4f42017af3912a1d95c8d9c3284784c82fa88b98c679c955e16" => :sierra
-    sha256 "55baab86d5d34769fede15c104f4db817718c2398a7ab03e765c543059f5b049" => :el_capitan
-    sha256 "a1d0188c1829d71c17b6b29c2a71299dfc37ba65d1f05f5cf8e655afbea08a51" => :yosemite
+    sha256 "9f6b37c2a0011b255d3a5c39e0bf653e8271d4f8cfc4306bc615ddf77946b442" => :high_sierra
+    sha256 "a3a692cff9eba1a68eef998577050169da2eb3930e9e12902a33c689ed904548" => :sierra
+    sha256 "95dac4cc5d9fc02d7a7d832d471b4911b52e97ced0ab98e902c84d33ee54dc7c" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
