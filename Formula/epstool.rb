2747ed7d7fd6070146a30e651722b452dad0eea7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 19:49:11 +0000

    epstool: update 3.08 bottle.

--- Formula/epstool.rb
+++ Formula/epstool.rb
@@ -7,6 +7,7 @@ class Epstool < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "99cd88b7e087c3f55b3a0fc49ef08901a9e79745191e5ce551f268148a011dd1" => :mojave
     sha256 "4e41615a63d60d963029768b0618d43e29142e5495fb693a7f5844d2cb6f0d6b" => :high_sierra
     sha256 "d53d1f3d353d0f68f52b87603a67ca97942fb714f766a54636c142550568cbd0" => :sierra
     sha256 "92efa66cd268f0447dc52c14e9da04ae8af01b1691ec8eec3df61bbeb947b713" => :el_capitan
