2db20a3386b2ff601f46617548a3fb8fa3d4cfa9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 12:25:32 +0000

    libzip: update 1.5.1 bottle.

--- Formula/libzip.rb
+++ Formula/libzip.rb
@@ -5,6 +5,7 @@ class Libzip < Formula
   sha256 "47eaa45faa448c72bd6906e5a096846c469a185f293cafd8456abb165841b3f2"
 
   bottle do
+    sha256 "c3527d9364aaca72f5bbd0486962639b421efcbd7ba7209b8a14a7900e52607b" => :mojave
     sha256 "4ffb9ac04f1fc2c98e5ba902999ed6f4bc5d7d9133d22fc183fd8fa13b7fd9be" => :high_sierra
     sha256 "a6d0dbd2370c97597948976414a48f544dc8a8e3e674e7faacef1bdc942d3161" => :sierra
     sha256 "312f87c8f28237b69080b511d696ba72027e774655eacdd159159001c7941c0d" => :el_capitan
