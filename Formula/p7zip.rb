a94128e81899dd311223f3c72bd804082b56c957
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 15:30:14 +0000

    p7zip: update 16.02_1 bottle.

--- Formula/p7zip.rb
+++ Formula/p7zip.rb
@@ -7,6 +7,7 @@ class P7zip < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "bb715042a9067df735cd7d032a15988da430fbf5a297d9624b9a4a021af6fea2" => :mojave
     sha256 "fb52fc214eb4ecd032666997976a514212adcb3c33ca23f15547310d5dc14a6e" => :high_sierra
     sha256 "a2b914eebce9108f278e33b53ca798999eb81397c370bd1eaa7f63ddc5e51867" => :sierra
     sha256 "229fc3a0badd5325e69b93121c9d55e7860110093e57ef46af063daccb2af372" => :el_capitan
