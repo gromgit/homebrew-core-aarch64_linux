b319c846bde0053846eeb3c364a887aec940c7cd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 09:41:27 +0000

    mpfr: update 4.0.1 bottle.

--- Formula/mpfr.rb
+++ Formula/mpfr.rb
@@ -7,6 +7,7 @@ class Mpfr < Formula
 
   bottle do
     cellar :any
+    sha256 "a51f47143ac39b4e9073946b08e846adf2509a4447555aa4c9dceea363d8e429" => :mojave
     sha256 "223ac973eb796b275cf751111c458dc1466f82f302e96ca764ea091309d1a963" => :high_sierra
     sha256 "a29b4585cb97715cdab177ea586b3fbee2c578248b115e3b86505724f2b0fc76" => :sierra
     sha256 "33b9a9b53120eae11bf2d241d0285f1bb0410ff133d2ecc1d4df07846da93018" => :el_capitan
