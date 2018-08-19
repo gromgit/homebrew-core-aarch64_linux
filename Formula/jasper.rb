0965b9045913e5f256945a7c7bc3e1311793023c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 15:44:35 +0000

    jasper: update 2.0.14 bottle.

--- Formula/jasper.rb
+++ Formula/jasper.rb
@@ -5,6 +5,7 @@ class Jasper < Formula
   sha256 "85266eea728f8b14365db9eaf1edc7be4c348704e562bb05095b9a077cf1a97b"
 
   bottle do
+    sha256 "799c0986353251ad4b1d6d8cbdcfae357ef428cbc534a89ed0be5a5de3dcaf80" => :mojave
     sha256 "086de22e8e8a01299962f3bea5374c90490b66e84b7e10a4078f172e64b0079f" => :high_sierra
     sha256 "86058296fb5efea3ca509bd335bf7da48a83078fc237c1ccb1bb2d428ef2343b" => :sierra
     sha256 "c481b8887b8d29e3c63735dd2151c9246e08f21bf50334033de4a054f700a6db" => :el_capitan
