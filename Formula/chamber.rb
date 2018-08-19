385de0ec482d341ecae96f2576616679fd106153
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 22:51:47 +0000

    chamber: update 2.1.0 bottle.

--- Formula/chamber.rb
+++ Formula/chamber.rb
@@ -7,6 +7,7 @@ class Chamber < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "049834dba6d904db515db1675a613a7545646d2ebb3df21eb7c6c4c05c3ee2e9" => :mojave
     sha256 "1eaa1dd2acde6d889557b0f713c7ec9655cb8715f5526f50bfe7c63b9ce63d3c" => :high_sierra
     sha256 "413cbf130fbd5bef2c69f74eb9ec2bf077ce1aa90a935d825cc55e0c88b983c3" => :sierra
     sha256 "a44294004e2aaf3bd9514edf0ba7bddeff1e10b198722c9d69cff7f0244cd9c5" => :el_capitan
