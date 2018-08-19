2fcba3f6d4904d9bd8f5cd5a7544b3bc80f99790
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 22:00:56 +0000

    thrift: update 0.11.0 bottle.

--- Formula/thrift.rb
+++ Formula/thrift.rb
@@ -6,6 +6,7 @@ class Thrift < Formula
 
   bottle do
     cellar :any
+    sha256 "a26c4c6e39b346dc74c5d29ba271b9f64c537914eb3228e446e0ae2e34fa106b" => :mojave
     sha256 "d1c648d84f21b567f1468625523b78d496d49954a3f5f28ce127f3eca7c0e2e4" => :high_sierra
     sha256 "710f79cf150713e4e24ce03b605fcd3ea56651b58bb7afe64d8b4a948842616f" => :sierra
     sha256 "e6f40c95f93331dda62d7cbfe0ce4f467c17e73e4a4a05f859e29a58533b52d8" => :el_capitan
