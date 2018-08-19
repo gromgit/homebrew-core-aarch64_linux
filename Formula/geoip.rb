e4ff04ee12d9e59ce108e6c12ee1c8427753c4d9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 21:55:11 +0000

    geoip: update 1.6.12 bottle.

--- Formula/geoip.rb
+++ Formula/geoip.rb
@@ -6,6 +6,7 @@ class Geoip < Formula
   head "https://github.com/maxmind/geoip-api-c.git"
 
   bottle do
+    sha256 "d987cf2490017aaf11d23a8ec2f443c408020159e7521064ffec3ac5fb840fe8" => :mojave
     sha256 "f6a895c01acdcc30efb1efe0b6a663e89cd86ce93a1543fb4e39b37c9f2ab2b0" => :high_sierra
     sha256 "00d3f48b250bddd07224b7d3422d36fa5374e27130ec6002f321146920ce711b" => :sierra
     sha256 "9bf37c2b6e7fa5971cd5a029fd6390a830a063b746b0556384019fced6dff534" => :el_capitan
