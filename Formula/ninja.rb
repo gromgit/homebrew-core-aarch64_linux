e12c3d678a6e10a922928eeefcca2824dd56ae2f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 17:38:06 +0000

    ninja: update 1.8.2 bottle.

--- Formula/ninja.rb
+++ Formula/ninja.rb
@@ -7,6 +7,7 @@ class Ninja < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b40a7da5db26b6bb32537185bd79861cce13f723cc02ad503e8501b6fca2d6f4" => :mojave
     sha256 "eeba4fff08b3ed4b308250fb650f7d06630acd18465900ba0e27cecfe925a6cc" => :high_sierra
     sha256 "90ecf90948f0fa65c82011d79338d7c5ca2a4d0cb7cb8dc3892243f749fbe2eb" => :sierra
     sha256 "675165ce642fa811e1a0a363be0ba66a7b907d46056f89fd20938aa33e7d59f7" => :el_capitan
