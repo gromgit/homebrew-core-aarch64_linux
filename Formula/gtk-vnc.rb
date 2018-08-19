4b2f5cb8e97d998ea5a8372e4e87cbe231c59929
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 14:33:14 +0000

    gtk-vnc: update 0.9.0 bottle.

--- Formula/gtk-vnc.rb
+++ Formula/gtk-vnc.rb
@@ -5,9 +5,9 @@ class GtkVnc < Formula
   sha256 "3a9a88426809a5df2c14353cd9839b8c8163438cb708b31d8048c79d180fcab7"
 
   bottle do
-    sha256 "81136476954cb2ba4d3b80741b58c0bd7451abd77da9280d53555dbbe9f96971" => :high_sierra
-    sha256 "bf2f234421c9225964722e57e94922592ad80e16398bd761b2eb6464ae09d058" => :sierra
-    sha256 "16bcd76a3a39dff96e28a97e179f6161d64a3a23a0ab3ea9a6252eea2dda9ef3" => :el_capitan
+    sha256 "46f9946e8e7fbfe9003b615350e750795514d0be5ab2ce93232daccc556cd0e1" => :high_sierra
+    sha256 "d519139009c47cc5673efd583ed08d022babfa9315f78d87a0c0d35260f33a6f" => :sierra
+    sha256 "e7c133b6fa20f0c68cb50bb773c80c7e747571dfeaddded32b1fee52c45a5c4b" => :el_capitan
   end
 
   # Fails with Xcode 7.1 or older
