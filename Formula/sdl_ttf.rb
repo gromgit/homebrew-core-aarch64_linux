c81f42b49993be1a8ea1c1ec8b43824380f218b7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 13:49:54 +0000

    sdl_ttf: update 2.0.11_1 bottle.

--- Formula/sdl_ttf.rb
+++ Formula/sdl_ttf.rb
@@ -7,6 +7,7 @@ class SdlTtf < Formula
 
   bottle do
     cellar :any
+    sha256 "09d3328d31341d4c76fa07e42480b283ee8f7ddb6518128e871debb84410521e" => :mojave
     sha256 "544d9fe4053cf2a83f9c34b91773518b8bffefeea6337f5d293f6064c3260972" => :high_sierra
     sha256 "22972859bc6ab2f2a6fd8a4cf5394e647336e4b83d982b02e7015ceb7799e59a" => :sierra
     sha256 "981960db1d2539b57bc42deb12ab59e163214d881612c1fffea72e4927e1c82a" => :el_capitan
