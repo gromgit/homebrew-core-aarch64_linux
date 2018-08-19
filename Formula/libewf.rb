a9b2e28809932d310a207f1cf6a763990e930214
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 14:31:40 +0000

    libewf: update 20140608_2 bottle.

--- Formula/libewf.rb
+++ Formula/libewf.rb
@@ -9,6 +9,7 @@ class Libewf < Formula
 
   bottle do
     cellar :any
+    sha256 "2a93f99c3ff1dea02ea18505644e57aa688248c19dcf410bb0073b07c80d6e0c" => :mojave
     sha256 "10efe12416e50457d968107669bfd2b1bb6e79865301950eb4335ffd6ed43c59" => :high_sierra
     sha256 "c77f644a80bf109f62a9b410917954a79e03ed47fff73b3d0da4f25de6afdf95" => :sierra
     sha256 "5a9c6ce83af6069f84aaf30a5a6ae42eff2e98078835af1c06555852c696b5b4" => :el_capitan
