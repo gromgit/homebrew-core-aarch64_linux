86c38591d984ca4351ea717f03b81d3b692c50d1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 22:51:51 +0000

    dockviz: update 0.6.3 bottle.

--- Formula/dockviz.rb
+++ Formula/dockviz.rb
@@ -8,6 +8,7 @@ class Dockviz < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a9c6471cc7405fe1a2f0dcb8eab9b171b52aeb67e4a64ce07e0b4d9f7c8aaad7" => :mojave
     sha256 "7334e941bcf3841a724103ecc7bd1cf028dc5e41bd9e80283c6f34637515cd02" => :high_sierra
     sha256 "8fbcc273dbbf14a33f44cb9d56d350440168c7943d6c29ba489c2db7cc8f293b" => :sierra
     sha256 "0df5881e825be40043790f3759fa2f3976d75c3c8273e69c4bfbd4cc039744b3" => :el_capitan
