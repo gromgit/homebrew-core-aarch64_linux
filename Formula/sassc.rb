96a6eae33a6741b0da110fc77a7bb36a05b63920
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 15:08:49 +0000

    sassc: update 3.5.0 bottle.

--- Formula/sassc.rb
+++ Formula/sassc.rb
@@ -8,6 +8,7 @@ class Sassc < Formula
 
   bottle do
     cellar :any
+    sha256 "0776912423914ce74e8451f0b66004cae6f2e00a8c40e4a35f77fdbe0d036b72" => :mojave
     sha256 "efda15371cdf37716f508cbbbc7512bd4db103cbdd25a0a5b27ae74e6f5a4705" => :high_sierra
     sha256 "8160ead69c6f3e7665cb474e719cc244412c37335013e4d98bf10bfdfcff6a32" => :sierra
     sha256 "32edbfd6543029224ea742a23f0bac7f675a9aa8e7a0511cee09561518591854" => :el_capitan
