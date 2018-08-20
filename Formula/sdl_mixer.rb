288291d479b4e216264bc59f0a554308c0801054
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 09:25:12 +0000

    sdl_mixer: update 1.2.12_3 bottle.

--- Formula/sdl_mixer.rb
+++ Formula/sdl_mixer.rb
@@ -7,6 +7,7 @@ class SdlMixer < Formula
 
   bottle do
     cellar :any
+    sha256 "c957ec0e2f17c89e78b2b6aff735ec7abe19bd0e10a0e2aaad227746c26dd910" => :mojave
     sha256 "e08902563c6265ac480323c9f22a26ee0e51f42fb1c36efb9b66b1f6b34a032d" => :high_sierra
     sha256 "69e8726e5b9bb858e069d244844411fc43930d3a7d0b78c6177bf36c76bb04c2" => :sierra
     sha256 "9c9a55fe416bc37a14e9a58439093b85274bf12c2bab24660a3f7c4b0fff70c3" => :el_capitan
