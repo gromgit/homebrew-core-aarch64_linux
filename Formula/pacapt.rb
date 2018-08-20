9824262b017627c98ff381546330938331582b85
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 23:16:02 +0000

    pacapt: update 2.3.15 bottle.

--- Formula/pacapt.rb
+++ Formula/pacapt.rb
@@ -6,6 +6,7 @@ class Pacapt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2ad28a5d3416ac9be738ee60626a75f91c126a5b2d12f33a2e92a3d14542bcee" => :mojave
     sha256 "f56e9b1cb09ec56792706fe1364ebcd50e655d45ba7f6dd9d13969f11c1b7100" => :high_sierra
     sha256 "f56e9b1cb09ec56792706fe1364ebcd50e655d45ba7f6dd9d13969f11c1b7100" => :sierra
     sha256 "f56e9b1cb09ec56792706fe1364ebcd50e655d45ba7f6dd9d13969f11c1b7100" => :el_capitan
