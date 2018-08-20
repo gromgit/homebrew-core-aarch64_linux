acc332f21de70078ae6a0c1a420614413bd76935
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 19:35:53 +0000

    bioawk: update 1.0 bottle.

--- Formula/bioawk.rb
+++ Formula/bioawk.rb
@@ -6,6 +6,7 @@ class Bioawk < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7082d4073e07ba3dfa849f95eb126d966a45f9fceb1d197595119a216e465727" => :mojave
     sha256 "023f5cafaa31404e68b8fc6bcfbeee27e63eb5fbcab897d2f406fceda90ec9ff" => :high_sierra
     sha256 "154d44dd9ea56db8170127711e991950d487e379ae12df76332e4b7512f79fe8" => :sierra
     sha256 "df0810bc087f924cdddcdb73f00faf9772de9475e0e698c7af8a7d036b3a4c91" => :el_capitan
