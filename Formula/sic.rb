858669c513b0324d7fb040dca3a1c946c35af16a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 22:14:12 +0000

    sic: update 1.2 bottle.

--- Formula/sic.rb
+++ Formula/sic.rb
@@ -7,6 +7,7 @@ class Sic < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2c50dd89e57fa0764576417365933792e7599dfb8899ec75957be0fb6d46dd5a" => :mojave
     sha256 "f7e19c7d87f5f13e736edcf7f8cb821b4b644f78208c87f2f6655e5b7541abcc" => :high_sierra
     sha256 "8ec385f1fa892a80c51dca477f469dfe69864d0d5538b652c45ac17914aa5f89" => :sierra
     sha256 "efeb0f7a31a6d4f0ac4c065a4646b5a523788b5edbddd9f99ffa04f00aa41f97" => :el_capitan
