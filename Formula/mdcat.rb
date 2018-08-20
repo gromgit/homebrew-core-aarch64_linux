73d2cea4d2358c6f10f20d3a2fb104999949bfb3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 14:53:34 +0000

    mdcat: update 0.8.0 bottle.

--- Formula/mdcat.rb
+++ Formula/mdcat.rb
@@ -5,6 +5,7 @@ class Mdcat < Formula
   sha256 "fb4ce097fff72fb53734568a8a1b96797cbedfbee2aabc2d2a5e8c794b1d5887"
 
   bottle do
+    sha256 "be68d10152352df81716bf3a756e4b12176dc4516d77c8b3ca500d6c6f1896c4" => :mojave
     sha256 "77ed15875a6379f7c95a95af9eee53b8f7cb6c295d2df43ef49f4932865bb5fe" => :high_sierra
     sha256 "a363e294b323dfc469b7f7f22519457eb3df834f6407ffec8f40c1935c1e5e24" => :sierra
     sha256 "9c84c8b1c3d6269b0c9fd4badfb79e7e9fa5cd332d8e7345acfdf69122b7bb95" => :el_capitan
