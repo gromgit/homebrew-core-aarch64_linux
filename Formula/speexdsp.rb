667d2018149371cefb6cdcf41a199c9c7e2136c2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 14:55:34 +0000

    speexdsp: update 1.2rc3 bottle.

--- Formula/speexdsp.rb
+++ Formula/speexdsp.rb
@@ -6,6 +6,7 @@ class Speexdsp < Formula
 
   bottle do
     cellar :any
+    sha256 "976f26d11b921a994d65f4f1d911e7b181ee19107c6c591613bcd23eb5e0077d" => :mojave
     sha256 "4a56292d0a64f7e1e9f9227d6c6aff652e6de5263cf7ba1a3d571321ee5cea09" => :high_sierra
     sha256 "367e34d4ba6e7087762193a18910da38905496ec41c0f329604d310a09e4f5ed" => :sierra
     sha256 "4891bd2a89fc9369d9c573afd80a1e7593e9414a0d6a956921af1b65f61ab264" => :el_capitan
