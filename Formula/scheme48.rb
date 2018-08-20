5884d65b5948fbbff3be667c96ae95c1ffb311a5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 22:24:44 +0000

    scheme48: update 1.9.2 bottle.

--- Formula/scheme48.rb
+++ Formula/scheme48.rb
@@ -6,6 +6,7 @@ class Scheme48 < Formula
 
   bottle do
     rebuild 1
+    sha256 "42cacccaf71990813012cdc819702fe24a93555998ac86d54e389ea40f6f2a87" => :mojave
     sha256 "590f06c7c31910eed48da06080959628982226e7b09e2aedd352fa6e4a6c2007" => :high_sierra
     sha256 "e9751df2e3cfd1a007d74d541ca494a439645e3006ad354ddf65b0abfb370864" => :sierra
     sha256 "af2ced8a13fdad5478f745c698b09071e71d84daca01c6e3e3c35961b06cbea4" => :el_capitan
