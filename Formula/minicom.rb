83afb1a673779f5f3072ab70b3ed3754cd3093ea
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 10:03:53 +0000

    minicom: update 2.7.1 bottle.

--- Formula/minicom.rb
+++ Formula/minicom.rb
@@ -6,6 +6,7 @@ class Minicom < Formula
   sha256 "532f836b7a677eb0cb1dca8d70302b73729c3d30df26d58368d712e5cca041f1"
 
   bottle do
+    sha256 "77cc043be40a99634caa99d5f309741d85309fed07c1c21f313c6d99c4732966" => :mojave
     sha256 "ec4beca9c9816db86a3bb7a11d7507fe0740feb62461341232a425a5156cd4a1" => :high_sierra
     sha256 "63584b5ee8463dfb6cef69ad32308c51a4e83778dd44b80fc4c1e7c40cb48b2e" => :sierra
     sha256 "820aae10f1c298350f51f7571d4d6becb4b0cfc876fb77126ea1e43bec8466e4" => :el_capitan
