eccdb09a2d7590063c51a8c1b6da0b1006eebc00
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 22:24:49 +0000

    rsnapshot: update 1.4.2 bottle.

--- Formula/rsnapshot.rb
+++ Formula/rsnapshot.rb
@@ -7,6 +7,7 @@ class Rsnapshot < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "58aaff911593d284a63426d0ec4f1328910b9a6cb5a6ecb64f4853d0300a3184" => :mojave
     sha256 "27e99660b73118874b5ba4ba355a0bf9b5db71933138a6b719869da01164a12a" => :high_sierra
     sha256 "998faa778dcfdef5b362b10aeaffadc97e40f63ec6df72d9bd25029ab82f2550" => :sierra
     sha256 "21823489c045150e8d8e51addba52b6cd75eedaec93357732db859ba738f59d5" => :el_capitan
