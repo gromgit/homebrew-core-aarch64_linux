18bb9a21b74d235c9cfc08467379763de27e8c33
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 20:26:25 +0000

    vttest: update 20140305 bottle.

--- Formula/vttest.rb
+++ Formula/vttest.rb
@@ -6,6 +6,7 @@ class Vttest < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8ff835e1beb71c0e221c87be184b6afdf1c8d6cb050c8d8f0d9d22b46c5df88f" => :mojave
     sha256 "6a3972df6f5ab23fb7480ffda28b8b1e8059854b837f1c947744e5dfb542c6bb" => :high_sierra
     sha256 "360d6eaa04e1a147eee360fed7d66ab779acc0a0ab3b51b1916cdf4d4a7f11f1" => :sierra
     sha256 "e758b4d59d1322b736f247c9ebbabe3c73ad06b324120997c6af784b8a3ab3f7" => :el_capitan
