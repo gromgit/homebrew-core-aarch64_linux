0e1166efe7779ea7d5c04c2f206e50b6990840d4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 20:33:24 +0000

    userspace-rcu: update 0.10.1 bottle.

--- Formula/userspace-rcu.rb
+++ Formula/userspace-rcu.rb
@@ -6,6 +6,7 @@ class UserspaceRcu < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1a620b36743e9afeb795192e4d7defc2e581646afab160336f30182ef5458359" => :mojave
     sha256 "9e3e9a7e4615f206faab5567f4eeab37dfa0aad7bdb9113803716df70abb9e0e" => :high_sierra
     sha256 "a9e38da39a4afa118c7eeb9cadbb0466caea3f77a8525473a6603297b0d32a9f" => :sierra
     sha256 "e44fe1d83cedac0ccf9e22a406e8efac399ac281fbc858dbc20d7b57fe564503" => :el_capitan
