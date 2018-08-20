2872288925cd6569de5f3bb7eccb44d8e9872389
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 10:03:49 +0000

    qpdf: update 8.2.1 bottle.

--- Formula/qpdf.rb
+++ Formula/qpdf.rb
@@ -6,6 +6,7 @@ class Qpdf < Formula
 
   bottle do
     cellar :any
+    sha256 "d1dc8195734a59a37b403d4354d5a1df78d9e546ee48b54333f55bdce2fb8f05" => :mojave
     sha256 "375e32a6c852c44b16fbc143b3b15185607b2bbbcd1084343999839deff1dd0b" => :high_sierra
     sha256 "dcfc26a15e4cc9031901701cd708440d38cbf92bd3908c941331ade49a94ea5d" => :sierra
     sha256 "62d7195d5e9aa34dfd55ae65d554d55c4a8ef3f74a3abe1e85c2abfd8bd63c70" => :el_capitan
