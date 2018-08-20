9e7f8a637e6c594984cb921783a273c21d25741f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 22:23:06 +0000

    sqtop: update 2015-02-08 bottle.

--- Formula/sqtop.rb
+++ Formula/sqtop.rb
@@ -7,6 +7,7 @@ class Sqtop < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "27786a7ca63d00ecb47e9f1e3e844a077d38048871cec3c03308831716338dc1" => :mojave
     sha256 "fe9a704fdf7f24dadba6b4f7cc20f8f07d93c19450701e01b408ea2f7574ec63" => :high_sierra
     sha256 "30f51d2886adf914eef22af21dfac92f544c59c88b6e7961972eb6702e48d0c0" => :sierra
     sha256 "6d838378cae0971561da60dff1e887bf03b60d1a0ff198a5d468654ef790d9e9" => :el_capitan
