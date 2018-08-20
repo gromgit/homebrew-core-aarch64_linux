a7498bc5f02755d6d54d2d739f17a9e51b813612
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 14:16:48 +0000

    syncthing: update 0.14.49 bottle.

--- Formula/syncthing.rb
+++ Formula/syncthing.rb
@@ -8,6 +8,7 @@ class Syncthing < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3a26783cde5ad67e0b8b98a5ec0e5f3328fedea532f4c240b7c243b41c7f3aba" => :mojave
     sha256 "a8b3a24dfd2c2cc05370999d0366e4ccc286d26f6527f6070592e9a6be30f263" => :high_sierra
     sha256 "734eb4c3b62015ef1a8cec32b6cb18a9563f00ac9aacd19f90dbfc1ea3956f6c" => :sierra
     sha256 "a491e90ab72f271bf5275df4988d6f0e1eb817cbbb9f93c07fc2361637a83dba" => :el_capitan
