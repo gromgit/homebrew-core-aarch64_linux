5a7cdbd6058816a11abfc5ce75304c5d57bb932d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 22:24:45 +0000

    screenfetch: update 3.8.0 bottle.

--- Formula/screenfetch.rb
+++ Formula/screenfetch.rb
@@ -7,6 +7,7 @@ class Screenfetch < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3e69c6119f9b9b4133817ce2d06de71317866c0b4aa08773aec038abef2d31ef" => :mojave
     sha256 "8e0571c592a9ae4b20d7e88e12dcbde9b722856721c3227d3334800cfd8445f5" => :high_sierra
     sha256 "62441999c591325de600f4cbd5d46e4157c443cfb514a3b1dfce8764c911785d" => :sierra
     sha256 "62441999c591325de600f4cbd5d46e4157c443cfb514a3b1dfce8764c911785d" => :el_capitan
