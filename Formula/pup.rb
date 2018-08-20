d5ec764eced450e49fd3f30629174f17b4bf435b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 16:40:18 +0000

    pup: update 0.4.0 bottle.

--- Formula/pup.rb
+++ Formula/pup.rb
@@ -8,6 +8,7 @@ class Pup < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "baeef002d46ed4c9872242419ed991b9d9f26d8e5b296f54b2ffb9e1e6bcfc84" => :mojave
     sha256 "f470de75187b994ef9612c5404dc7622a356c8ee6af21f6b2549b5d7c5d88d32" => :high_sierra
     sha256 "4ba84cffa7cfd01bd252223055abdf5fd8b6cfc27474131cf313e688ea8eeecf" => :sierra
     sha256 "a1aa49640871c127c76f4aea6db65487db964a055e2aa4d86ee2d8b7f5dcb561" => :el_capitan
