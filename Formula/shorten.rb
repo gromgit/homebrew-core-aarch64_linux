d76520657038e5133de40932d960f339414325cf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 22:14:15 +0000

    shorten: update 3.6.1 bottle.

--- Formula/shorten.rb
+++ Formula/shorten.rb
@@ -6,6 +6,7 @@ class Shorten < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ed0a7482bebdc53827e6932bdc70d8897d00b4ce87ac2cf84bee7b0cec2229a5" => :mojave
     sha256 "2247094c6f41ad5ce941c84335a45aaaabe0bef43ffeb89a544793957c157ba9" => :high_sierra
     sha256 "a54b8263dfbd2aab185df1888193dc0ceb602d9df82758cf5ef31b3df52ae697" => :sierra
     sha256 "66cf7cabae065035e9c3c4a8c139439384fb9f26ea0ee433e336c18ba2f8383e" => :el_capitan
