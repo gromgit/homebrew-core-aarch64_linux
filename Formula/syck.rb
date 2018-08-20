8e8a042ffa5d86173d7a897b9f4d81ff6c54c671
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 21:32:53 +0000

    syck: update 0.70 bottle.

--- Formula/syck.rb
+++ Formula/syck.rb
@@ -6,6 +6,7 @@ class Syck < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4baab04685bbb2fb536170a1bd069cfdec7fb5b2bc802ce6a80a4be933d8530a" => :mojave
     sha256 "a5a983847ae0e83a5f1d83adac736c602f7cce9ae00b9700d81ed766b4fe54bb" => :high_sierra
     sha256 "5a5f8c0f9a9def7ab59a052d85fbb128bc9a1c9c9b7ea2f9639109147eeba252" => :sierra
     sha256 "a2270f693ce6e8f0542f7d57dc6c8fcb731a268aee3c75a314523446c68602d6" => :el_capitan
