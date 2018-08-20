0571af74257f61c63196f5c8d1e48a68e4acda71
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 21:26:00 +0000

    tidyp: update 1.04 bottle.

--- Formula/tidyp.rb
+++ Formula/tidyp.rb
@@ -6,6 +6,7 @@ class Tidyp < Formula
 
   bottle do
     cellar :any
+    sha256 "234868d17b7ca6e890d0adc4e339e2a35c81b3c72f0fd8487d1d3352b03fddc5" => :mojave
     sha256 "52ef5e1d78ef5a3404e2345683fc03a61b0fad8084b74508473a042b1858b54c" => :high_sierra
     sha256 "6b5b65c1476004cc973fff0992dfaf77887b5a5df583ac31fc22665d250b538a" => :sierra
     sha256 "5274bb4cd33d9c15d8c73dbe4cfb54e686da29cd29093adba549024fe520b82c" => :el_capitan
