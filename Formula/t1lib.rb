28ef00022f2b05a60ced96e979e586fc552ee826
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 21:32:56 +0000

    t1lib: update 5.1.2 bottle.

--- Formula/t1lib.rb
+++ Formula/t1lib.rb
@@ -7,6 +7,7 @@ class T1lib < Formula
 
   bottle do
     rebuild 2
+    sha256 "2fc10925d1618b809de806ee87722c96d8c03655e3d586f0a37b3d049ee2e082" => :mojave
     sha256 "a36bc3913f6b51cb7772609a52049f90fc6241ffca3bf18c4295455ef5f4df4c" => :high_sierra
     sha256 "94789287c849a04f05a40c79940aee6efe3e03318c95db9c2be69ee4e6806d82" => :sierra
     sha256 "fa356a5405f5b0cf57c15ebd5b680c215e1e498189914e9b9663eb132655a8c1" => :el_capitan
