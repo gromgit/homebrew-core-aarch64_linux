b863d28b291da7e3a534c6901eb79fd8644a3c25
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 23:47:34 +0000

    ipinfo: update 1.2 bottle.

--- Formula/ipinfo.rb
+++ Formula/ipinfo.rb
@@ -6,6 +6,7 @@ class Ipinfo < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "33fdb805793a8566f7f6adca7a1c3b7d0c67071fc846977bacf6629a8e63c9b2" => :mojave
     sha256 "c06a0c771b66def2758aad30e8331cc56f751478715e12b25b9e46d9b64090f9" => :high_sierra
     sha256 "255c10eb2f0f885ba301fa2977ae3c45b5f7117388739adb58ce4312515ff98f" => :sierra
     sha256 "ecb331ae035cf5963afc8e8adf371d80f936960bf0d5ba379b18761263a1b040" => :el_capitan
