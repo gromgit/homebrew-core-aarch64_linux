a46b9dde83db89bfd95eda30eff26ccb878b721f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 15:46:16 +0000

    rpm2cpio: update 1.3 bottle.

--- Formula/rpm2cpio.rb
+++ Formula/rpm2cpio.rb
@@ -7,6 +7,7 @@ class Rpm2cpio < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ab485e451668ede8eea2b1a9f8ac1781aae218399604d72e28587ece7674f0b0" => :mojave
     sha256 "82c3021a2e882829d635ff45fc9780e72d0199777aa5dbbeb6f07b1a17944369" => :high_sierra
     sha256 "864c4e9533aaf62e67a119006017df772a961bb0c407a0184fb0c9d8e1202509" => :sierra
     sha256 "d26a07db8b6c1293171dc4937d81b2dc9936da0d52718d5f94ddd83524d5974e" => :el_capitan
