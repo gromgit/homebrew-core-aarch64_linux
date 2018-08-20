48710f7632da30f1a8842bf572932049ff7d308f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 20:26:25 +0000

    when: update 1.1.37 bottle.

--- Formula/when.rb
+++ Formula/when.rb
@@ -7,6 +7,7 @@ class When < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "654603a2d0ab3dc849a4221a9d1c4d8638ebf56b41d539efde05e04c6791a5be" => :mojave
     sha256 "5f1858e4c42591c1c30c54f4bf1fecd1961b1fc2a6990f45289720ebc1ad4892" => :high_sierra
     sha256 "5f1858e4c42591c1c30c54f4bf1fecd1961b1fc2a6990f45289720ebc1ad4892" => :sierra
     sha256 "5f1858e4c42591c1c30c54f4bf1fecd1961b1fc2a6990f45289720ebc1ad4892" => :el_capitan
