fab043f026de25939e8d998e74b5ee9bfdc8a439
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 21:32:58 +0000

    taktuk: update 3.7.7 bottle.

--- Formula/taktuk.rb
+++ Formula/taktuk.rb
@@ -6,6 +6,7 @@ class Taktuk < Formula
 
   bottle do
     cellar :any
+    sha256 "6ff23461c51c77612a5c00fc4caf40d9c91aa3e7b2f409e9a86f57f27f305f01" => :mojave
     sha256 "9cc466f8a75eea1974143fedecd42547eb14401d772e527776f387aec4832f77" => :high_sierra
     sha256 "0ffc0bb09703bbf32afbcd302850803f94ecbb311eaa77353275e7dcb1549f62" => :sierra
     sha256 "4a731d243e6915729240deb75dc99cfee513bb7d0f69169981623b14ce6601c1" => :el_capitan
