095f6c7fc01284c00f05e84d01baac8ac8655b5e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 16:10:47 +0000

    cvs: update 1.12.13_1 bottle.

--- Formula/cvs.rb
+++ Formula/cvs.rb
@@ -11,6 +11,7 @@ class Cvs < Formula
   revision 1
 
   bottle do
+    sha256 "6a2788027cadb1df5de3581f4e6c0a78e30a2b98dd8cbf26fc7ec030d9a8fc18" => :mojave
     sha256 "11b8be2fda1de3c8b77b20bdc283ceb12ba511826a0d3b79147dbfaeb83420db" => :high_sierra
     sha256 "01f9517d330037a248bc6d36c8127a4f99eb364a8a0d1cc5f8520cca261b7163" => :sierra
     sha256 "32dcf27cf028e270e826ba9850bde2f403f77c2c16a4b534d59cf68c0446e1fb" => :el_capitan
