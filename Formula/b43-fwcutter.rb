1e6986708f452e4f92d709177b936871ce82b4be
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 19:28:35 +0000

    b43-fwcutter: update 019 bottle.

--- Formula/b43-fwcutter.rb
+++ Formula/b43-fwcutter.rb
@@ -7,6 +7,7 @@ class B43Fwcutter < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "51b5702dd05e2afc1f8f0e4c10ccf38023610706fae51112a220284bd9976daa" => :mojave
     sha256 "abbde23073e94982c1f47ea1884abf4b855f8ab0ed8d0e2434de4ca32bfc8dca" => :high_sierra
     sha256 "f5238356964126642e9dd5c88de5564f221db86d488ca826055629d504a71426" => :sierra
     sha256 "d24bbb9e1669ef33319b23a37a8ebd4dddaf8d1ce2c9e24abff13d678e3633d4" => :el_capitan
