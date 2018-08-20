0ceabb910162634d38894fff9abd1a5966dbfaea
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 21:17:28 +0000

    unifdef: update 2.11 bottle.

--- Formula/unifdef.rb
+++ Formula/unifdef.rb
@@ -7,6 +7,7 @@ class Unifdef < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a4f1251e49839d04791e90a1a6c72d72679ebad399ca8cc184aae7f729ce518e" => :mojave
     sha256 "a7e42f5a8e2c467e84dbf9edc4f14cad362850e324088844d6b11f1cae88ce9f" => :high_sierra
     sha256 "be948ffc760273a09943faa7c156e4014534da82c78bc2c3c4f2a4a4416e962e" => :sierra
     sha256 "a89e5cc9b179fa5135077ad0c27e34cebe13a33dc02adccab4969855ba173357" => :el_capitan
