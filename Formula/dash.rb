e40788930db4f4e1a328fc6fc95b6291d08c876f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 18:50:32 +0000

    dash: update 0.5.10.2 bottle.

--- Formula/dash.rb
+++ Formula/dash.rb
@@ -7,6 +7,7 @@ class Dash < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "08b5b8cb7c8ba7946a800dc477faede8d13fe877af4305b11d91ea9e5afba4cb" => :mojave
     sha256 "11463b8d52c2d9616c2deaf40a4a4a186baf6c2b4724c3c8b35efc527f6ad0ef" => :high_sierra
     sha256 "ab44bf2512098fca3db4d0655191c1deaa0d0d8311d531976603242b1c7136e4" => :sierra
     sha256 "c634e0bb97fc544fc425c62d429e239519d7ecf447df4debc6406792ba1eb476" => :el_capitan
