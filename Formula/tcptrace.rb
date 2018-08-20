95a33ab7678fe087a0c930127c95b25d45ffe0d7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 21:32:56 +0000

    tcptrace: update 6.6.7 bottle.

--- Formula/tcptrace.rb
+++ Formula/tcptrace.rb
@@ -7,6 +7,7 @@ class Tcptrace < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b927868b2addc93b296fb8f31b08147014e9a81a356d4f18b0d4134db40081de" => :mojave
     sha256 "39916506fcd6385aee6375813128a126a84f947623594011f6c2c9df1b6dc8b2" => :high_sierra
     sha256 "7ccc5e6859be970a5a8a064630704111d37b03a7e3cf3a9874e16a60e4abe02b" => :sierra
     sha256 "e46775d7cc808b5b52a0a36a33142b824a9b2d8bce5b0557bc1041c2e55c5ffb" => :el_capitan
