8de64145ca48b35c43b3a93dc653532a91011dd8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 16:40:18 +0000

    badtouch: update 0.6.1 bottle.

--- Formula/badtouch.rb
+++ Formula/badtouch.rb
@@ -5,6 +5,7 @@ class Badtouch < Formula
   sha256 "62181ac05a68a552e1984dd42206f6a5ca195e51addc48cbfdf55a60afc7c3ae"
 
   bottle do
+    sha256 "0adb1b9292c93963822e090e4860c6deadbdd2cc7da9d0e49639fa4a181cb425" => :mojave
     sha256 "4965a902bbe8a22452039b145d5937f785fdac750674dc3c44ada3ca822c4aa4" => :high_sierra
     sha256 "0d24c83275c2988cb09befda551377ee9b803fffdfca094bb4649dbdc2624899" => :sierra
     sha256 "f579fb45cbfd1ee3908f5afd25c91c017d68ab380a8d2d36dc125fcd5031b293" => :el_capitan
