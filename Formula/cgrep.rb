87407b6b385e94e1fee81d89ee8618fe409b6296
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 14:06:19 +0000

    cgrep: update 6.6.27 bottle.

--- Formula/cgrep.rb
+++ Formula/cgrep.rb
@@ -11,6 +11,7 @@ class Cgrep < Formula
 
   bottle do
     cellar :any
+    sha256 "a5a9e0f032374564c3c4636c2aef31dc41c5a0b441a69356bfdc4db27d30e934" => :mojave
     sha256 "d5c98ba9f21a7946ca53394fc66f7145593a8d33427e4d0867b73f2342dbe5af" => :high_sierra
     sha256 "8fc2929f882103c18dae410dcf66f667d4c32dae6dd13a9c7b0ae0c87c980ef9" => :sierra
     sha256 "a3570fdcf951b804efe6994162af327c2984650e3d1fe4a1a54e3a473af8630a" => :el_capitan
