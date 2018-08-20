f01fd0c9ac6cdddc977ceca30a31c1406ffb4e7e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 22:24:48 +0000

    rush: update 1.8 bottle.

--- Formula/rush.rb
+++ Formula/rush.rb
@@ -6,6 +6,7 @@ class Rush < Formula
   sha256 "7f321e5556435b12638982c8d94c587982122f28c220f8bc679d7817035aadef"
 
   bottle do
+    sha256 "07ea3d94a8d34880b65ef978c5619e7bee5c67568d14a2afa7b3818541fe955d" => :mojave
     sha256 "421c4ff7a56f4d434ce69551fcdbfc9ec39bec8e472b4d64923be0a93adc6405" => :high_sierra
     sha256 "6147781c7c30c744d3e3aa69b55e72bb27a320aec6bf6e8d4a8360d5982aad20" => :sierra
     sha256 "a33fca1d7ccc86d125771c7f4e983244abe182d3a3731faa86fa12f21f7ab18d" => :el_capitan
