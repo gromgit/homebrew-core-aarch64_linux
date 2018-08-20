087e14391eb40d414266ba8e9d4a2f0c534b7c8e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 18:52:08 +0000

    abnfgen: update 0.20 bottle.

--- Formula/abnfgen.rb
+++ Formula/abnfgen.rb
@@ -6,6 +6,7 @@ class Abnfgen < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b553651b5500f66d10a369f4d8862ed9c6d2b39d395c43e372b346b4c7bfead0" => :mojave
     sha256 "3a62e72bec09b9bfff637710db366f713abc95de45437aeadbfa87a87dfc040c" => :high_sierra
     sha256 "0d69f39473838a8e46fb02009329e05be6eeaed579ff5533a09cbbecd8d46a2d" => :sierra
     sha256 "fd51cb760ed8afb8a9e3dd5d05c8efa832361b238ad95410fb2864c91c081825" => :el_capitan
