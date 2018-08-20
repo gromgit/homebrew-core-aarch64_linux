f2efa3e47b23656a684487f76a4180c93e6a4fcd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 19:33:02 +0000

    arm-linux-gnueabihf-binutils: update 2.31.1 bottle.

--- Formula/arm-linux-gnueabihf-binutils.rb
+++ Formula/arm-linux-gnueabihf-binutils.rb
@@ -6,6 +6,7 @@ class ArmLinuxGnueabihfBinutils < Formula
   sha256 "e88f8d36bd0a75d3765a4ad088d819e35f8d7ac6288049780e2fefcad18dde88"
 
   bottle do
+    sha256 "133d41e8d94e272a87b71ee4f73c35aa954a38de1b13d9340b9756f56c62cc7d" => :mojave
     sha256 "7603bd2dc2d5ff4ee8671ab3444d0d20d4f907fbb469fe3cd628540f68caf1ea" => :high_sierra
     sha256 "e7179a5847a494bc9e141f6a863d3b4912c71981686e1a456d2368431a9ae33a" => :sierra
     sha256 "4b727d9fc8ffe961eb071e69430367e217be6bd3d967d66960ad1903fe2b9ebf" => :el_capitan
