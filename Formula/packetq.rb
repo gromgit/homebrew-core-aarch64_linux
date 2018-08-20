dbbbfdc4c3b5c71df269c34d4aac2d6de6d0047a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 23:14:20 +0000

    packetq: update 1.4.1 bottle.

--- Formula/packetq.rb
+++ Formula/packetq.rb
@@ -6,6 +6,7 @@ class Packetq < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "58d903042f0fb1c352bdd422f087c444796dc11161d50de69093e4a583d02b59" => :mojave
     sha256 "299409c99a253199b8b5896ecc8f1490ef4989ff72b10abd2651981a0bd47974" => :high_sierra
     sha256 "eda7fdbbfc232e370b03e762c24551e92c3207f23f66e42db1c9ec10c448e1e1" => :sierra
     sha256 "1cc4d10a81e062c41d7641d8fc3a7e94a1bf3b9e92ad6c4d70c8bdc730cee1d4" => :el_capitan
