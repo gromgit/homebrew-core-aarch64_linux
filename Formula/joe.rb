f6e554497184eb6e84267f3131ef978be6e9f7bb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 23:47:31 +0000

    joe: update 4.6 bottle.

--- Formula/joe.rb
+++ Formula/joe.rb
@@ -5,6 +5,7 @@ class Joe < Formula
   sha256 "495a0a61f26404070fe8a719d80406dc7f337623788e445b92a9f6de512ab9de"
 
   bottle do
+    sha256 "aa448106d8769cf8d1b9adc8154dc420c94dbdc434be45b27e6a8a3268d2740b" => :mojave
     sha256 "02c1d1372565747bc21abe3b28ea7b3f2461068041e2d67037a9c1cbce12779d" => :high_sierra
     sha256 "f97df02a316a9e137e3391f42ff1118f67cd051008ef92c030fe54b5948a29bb" => :sierra
     sha256 "1d02a0b1f7df9846b0472bb3a5ea69aece88007fed4b32843318caa41cae3f9d" => :el_capitan
