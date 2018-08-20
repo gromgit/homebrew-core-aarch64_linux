85f1511b2045d1cdaeeed80df7c75bf1a83ed422
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 07:46:08 +0000

    sox: update 14.4.2_1 bottle.

--- Formula/sox.rb
+++ Formula/sox.rb
@@ -7,6 +7,7 @@ class Sox < Formula
 
   bottle do
     cellar :any
+    sha256 "850cd95c4baea2d91467637582f120ab9eff066c7414847512ea9cf49ba218f1" => :mojave
     sha256 "f1ba26233464499f58cc0cab34c5472d8b9e30a83cbc6e185b451a9476d560f4" => :high_sierra
     sha256 "de9126013c1bbd7ec728197cc471231cf17c51e7620e995590b8a6ade6c07521" => :sierra
     sha256 "f3b3ac24d351edb82fcd30119f13f6b86848c62b9130b6d20c8538e8b2b17d89" => :el_capitan
