3bb2b07abd935165846de555efd92caf43315692
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 18:52:16 +0000

    afl-fuzz: update 2.52b bottle.

--- Formula/afl-fuzz.rb
+++ Formula/afl-fuzz.rb
@@ -5,6 +5,7 @@ class AflFuzz < Formula
   sha256 "43614b4b91c014d39ef086c5cc84ff5f068010c264c2c05bf199df60898ce045"
 
   bottle do
+    sha256 "33a83a8fd62e5a4d4ae6fa098580b3f3926e6517debe77368e7c0290cf73f39f" => :mojave
     sha256 "4cd6b08ecfe35a62136b7a52222c59e055b80aef599404e29c7c9b8bf4f8fd50" => :high_sierra
     sha256 "b45ff3036dddc75cf64689b3f2660938834f393256315ed33ba72ed1924c695e" => :sierra
     sha256 "ef5e7c2e25020bf2d468c81ced4fdd9014dbdc0bb523f5acd8d91d8badc97d59" => :el_capitan
