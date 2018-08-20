584498e58d0eb1c1b22204a2cc36eb20bdce724b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 19:33:04 +0000

    apachetop: update 0.12.6 bottle.

--- Formula/apachetop.rb
+++ Formula/apachetop.rb
@@ -8,6 +8,7 @@ class Apachetop < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "f4520c12643b7b9d6afc80729217ecbe7f17298790dbe783e7909c436559ac30" => :mojave
     sha256 "e41dce58ad184e880c1f198ae1d5c0d0d1f1fc9fd27f1296a02a1b23e33c09cb" => :high_sierra
     sha256 "3a3f3b20db8183a8c642ce732d9ecc3eac68ea1c292cab0594c3d5000c181442" => :sierra
     sha256 "f1dd6f8ac7cb973228227b4cb678ef0bb61f618c482dc8d7d3144acccfebcf5b" => :el_capitan
