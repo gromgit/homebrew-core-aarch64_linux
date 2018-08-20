3d71a34d6e13c0daf7398e29fa8d2a0d8f7f9bee
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 23:47:37 +0000

    ired: update 0.6 bottle.

--- Formula/ired.rb
+++ Formula/ired.rb
@@ -6,6 +6,7 @@ class Ired < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7821d818af4c7d28b4cbf26c627685b77f18a1004369d4a57bee2582620008b7" => :mojave
     sha256 "f6af714455a74c02769c9726855a92832e43c37c79a0c589a0c7744beac8956c" => :high_sierra
     sha256 "5d10dfac87e4a4ca589a9fa76e8f9aff62625ef6358b6ab29360e79fe4a6dc35" => :sierra
     sha256 "4fc558225913b629b144661385841e63ebb167beb9900475fadb0c0e886b4997" => :el_capitan
