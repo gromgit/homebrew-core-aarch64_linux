f25729c1c4c7adcdc7f5c688fb7f3c09b10553a1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 19:35:56 +0000

    bcrypt: update 1.1 bottle.

--- Formula/bcrypt.rb
+++ Formula/bcrypt.rb
@@ -6,6 +6,7 @@ class Bcrypt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ef0fbaf77cad63f0450bde11bd6ba89fe2217ecb0f95b1952dd93c56730f615e" => :mojave
     sha256 "70235a007382bbbaeddbfc52b503e86b6cadcb7d07b752d97c8ce0861bccd3a8" => :high_sierra
     sha256 "913cfce96b6de1fce20ee1ff771ef22e3663f3da6c7529d7efc3a43b0e1d92b8" => :sierra
     sha256 "d674203ce681f17519eee1ce7a3258615b2de5a8a12460d7de284af09028d7da" => :el_capitan
