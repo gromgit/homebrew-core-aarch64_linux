1f74b228f6fba399d1e62d9ccfa7293a73cede64
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 21:17:26 +0000

    ttf2eot: update 0.0.3 bottle.

--- Formula/ttf2eot.rb
+++ Formula/ttf2eot.rb
@@ -6,6 +6,7 @@ class Ttf2eot < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "54d328636bcb7d9fe1e28bf46115f0b718fc9f4d8e18c48b39d5b2e87bb3930b" => :mojave
     sha256 "7b44ec925ee2bbeeaba775befc77c0c22f2f690ecd94edb72e471c631da80f43" => :high_sierra
     sha256 "26f40d7a58de2ee396fc04dd47c41e9b65640570fa1ca8b71134dd88e6e88c06" => :sierra
     sha256 "5fc89e642b7d51c0c7965d9a952d1b697f94b4ec16d7711ff37387979ce47f5d" => :el_capitan
