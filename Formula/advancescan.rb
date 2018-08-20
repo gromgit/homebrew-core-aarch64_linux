8b2c6088eda75965181771835f5620ece45c0767
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 18:52:09 +0000

    advancescan: update 1.18 bottle.

--- Formula/advancescan.rb
+++ Formula/advancescan.rb
@@ -6,6 +6,7 @@ class Advancescan < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3aa20db4c47b16166b385d3e7e0c7af903833333757af7b1e0909dec00824ce2" => :mojave
     sha256 "d0a8416434aa03573dcbadebd135fbcfa6f4829934622ab8afe68aa496ec5e48" => :high_sierra
     sha256 "0bc4290c65271b84aec455adbaf85795857b19102e6efb152a64623420ae5757" => :sierra
     sha256 "e4295866cda2370aa37cb1144ff1269ada4df6b76145a25efaf072d7a6b09b5c" => :el_capitan
