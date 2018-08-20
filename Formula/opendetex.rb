15d5426869a9c3f6dc9c78e83c220787edd21d9a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 23:15:59 +0000

    opendetex: update 2.8.4 bottle.

--- Formula/opendetex.rb
+++ Formula/opendetex.rb
@@ -6,6 +6,7 @@ class Opendetex < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "995070391cee23f402d6e067985230d467e48b5a20b5122e05474bec73cfeb24" => :mojave
     sha256 "c668bd3fd940b6f27ce4162b5625ff28e45df24e34f7f66b6a2158546a47e6d9" => :high_sierra
     sha256 "4ce5d750a06de0c96682042e88aea55707e5c0b28cbea66396ec1020df130420" => :sierra
     sha256 "79e56e9e50f90d6b534f29c556a648743ee10ab494d5f7cd049031eb4833f122" => :el_capitan
