c23ceccbaa3f94ae20a33fd5315bfc5ff1178cfe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 18:52:11 +0000

    aespipe: update 2.4e bottle.

--- Formula/aespipe.rb
+++ Formula/aespipe.rb
@@ -6,6 +6,7 @@ class Aespipe < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "abd6ee4463960a4283fa5fd97b45f7ca49400bdee3182d1248ced9ef45626189" => :mojave
     sha256 "0031c9dbcf93fb4faa3b4176491cb0917426f0b5c146e0dff9a1f51a0d2cc9c9" => :high_sierra
     sha256 "7569237a2bd31170d52f161afdb3b46cc23acdc4a35c2662d0b79aac831ac2b8" => :sierra
     sha256 "5a70398747999348647c7577da0c4fb2274413f53ee65fd6fce3845cbc27e0d9" => :el_capitan
