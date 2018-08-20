c4fd45efc013097ee34b1578278d502546ad4a63
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 19:35:53 +0000

    bib-tool: update 2.67 bottle.

--- Formula/bib-tool.rb
+++ Formula/bib-tool.rb
@@ -5,6 +5,7 @@ class BibTool < Formula
   sha256 "5b6c4160975a926356e8e59d0e5c01ac2a7be337ecace2494918fc2a46d9d784"
 
   bottle do
+    sha256 "029ff0d913560a01dd20fa05f788feb8189660d7099be656c52ce829b89d9749" => :mojave
     sha256 "dfd0882e8a92f9d883d838261ced3085f843b6faa439df00fa8988fc719c6946" => :high_sierra
     sha256 "9611f8ab9db1f0ae2918bf22d3169a80eeacd0a4e457437b4b131c36e7687d41" => :sierra
     sha256 "5f7813e70bfaa668da0b3f438237e0fef7628808c3b5cc56c26012e2cf54f09d" => :el_capitan
