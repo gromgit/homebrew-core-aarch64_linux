08f577940076089363abc7f789067f3549d9c0e2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 10:47:30 +0000

    media-info: update 18.05 bottle.

--- Formula/media-info.rb
+++ Formula/media-info.rb
@@ -7,6 +7,7 @@ class MediaInfo < Formula
 
   bottle do
     cellar :any
+    sha256 "82817f3df51d73634e7ced9e347747f1c8660bb0b53dd73ff83f7b24716c1826" => :mojave
     sha256 "99bc2eae9ac0f372663c2ad2de1970f0d3bda860061240d6dd20a8eb8332b475" => :high_sierra
     sha256 "77c90fb595990beed8f5436e2dec309f065c5dddeacea648e2b8d7b081ead289" => :sierra
     sha256 "726ed97a08cfa889a344b5177ac76512f1a07d85bfaa06ab49bdefa287f54e23" => :el_capitan
