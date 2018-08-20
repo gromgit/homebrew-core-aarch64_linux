d6b4b632a96a7fb1cf56ab9237435074ebcdbbbd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 22:23:02 +0000

    stdman: update 2018.03.11 bottle.

--- Formula/stdman.rb
+++ Formula/stdman.rb
@@ -8,6 +8,7 @@ class Stdman < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "538451fbb366f727f89919bc2056b4b4bde46351d07509559c1793d5816c9099" => :mojave
     sha256 "0f795424e68e066cc1f6c567a5513001481cd610cded75dfab77aa8db42cf9ed" => :high_sierra
     sha256 "d3e640c191d2cf471b37b2121d20ebab97ff353f2d69616cfa7f7227db069beb" => :sierra
     sha256 "d4b15103ae4011c1f2c9a4e3dcb0b205bfa45595d7ad25d6cb87ec1dc4f395ab" => :el_capitan
