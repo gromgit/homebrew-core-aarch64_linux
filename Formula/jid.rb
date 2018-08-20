469059986d8bd77588620ab34a443089930e9a53
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 16:08:36 +0000

    jid: update 0.7.2 bottle.

--- Formula/jid.rb
+++ Formula/jid.rb
@@ -8,6 +8,7 @@ class Jid < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "049d37984ecbeba231d96f23caf3dda6ef50dd8a55b91a4e0d62f7975a244c1c" => :mojave
     sha256 "b6de24c980241a14be660440dc9aba403841f5cae50c5aa973bce92ebd1c2081" => :high_sierra
     sha256 "59624c107014497a6596ce33a6344c1456731a2784ffb4e92bf0075ddda16ffa" => :sierra
     sha256 "a77b38789f565878c3dd89b031693fcb35ed60404172a4fb3702b6e0f69f76e5" => :el_capitan
