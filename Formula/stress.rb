1193d81cba61ab4f22abeea4374dd34b7d821b48
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 21:32:58 +0000

    stress: update 1.0.4 bottle.

--- Formula/stress.rb
+++ Formula/stress.rb
@@ -7,6 +7,7 @@ class Stress < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "57923f0549beb3e528ad7683a09d615096d875512fb46b9ed7c54aa6382ca793" => :mojave
     sha256 "3087af5469f5867b82ab85158c7268d0172b58da91baec06916659bb0fd2f930" => :high_sierra
     sha256 "1a18d667f7804579758079e3a3f94683a64687120b5f4b77cf25a63f3f8cec39" => :sierra
     sha256 "b4635c185bfba65271d74aaff155161d2df388be303d135315066260e9699c5e" => :el_capitan
