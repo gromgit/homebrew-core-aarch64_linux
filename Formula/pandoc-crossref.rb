82725bd486f327dc7f8493ddae5d1ca598ab1803
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 14:06:21 +0000

    pandoc-crossref: update 0.3.2.1 bottle.

--- Formula/pandoc-crossref.rb
+++ Formula/pandoc-crossref.rb
@@ -10,6 +10,7 @@ class PandocCrossref < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "73859b8d304c7b81bea3ddefa262f22bff00067d15c4b6be41b9282ab8c8dac2" => :mojave
     sha256 "2f282d0c583bf6b6a66972d653980c615e172a4f076b39ab3a31d1945c187ef2" => :high_sierra
     sha256 "ee2b34d681026c77d7415d66e258254d5ce8d9ab7562cd6b21db93fec8b832ba" => :sierra
     sha256 "53e9ec314ee9e35f100d1d628301079354308b2cd53eeadae3bd3bfaefc402e5" => :el_capitan
