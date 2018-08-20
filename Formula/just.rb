56b773eabb6bb26b0d2a82df33e7ced51f64b233
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 14:53:34 +0000

    just: update 0.3.12 bottle.

--- Formula/just.rb
+++ Formula/just.rb
@@ -5,6 +5,7 @@ class Just < Formula
   sha256 "e5ca71efd19db8f157131cddb14d75ba161851e79e1cc9fc709e2c674d2c943b"
 
   bottle do
+    sha256 "b47d8d2d3702f5d98e5537f393893594818bdaceb4e42ef093e55ddf530c098e" => :mojave
     sha256 "a3cf6e47a38bd3cbf8bfb09dce648f447dffec708eed07994491b1509bf857a8" => :high_sierra
     sha256 "b1e19274a73e5bf4d0b3b38bd26270d9b4ff97e4191ffd3ecdee353e0dae468e" => :sierra
     sha256 "a46cd86aa51e106d8025ab98cc560b3c1adee3524a7121d81ce63ae274403573" => :el_capitan
