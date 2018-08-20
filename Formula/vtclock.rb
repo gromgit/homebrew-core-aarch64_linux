e7ab98420a34033acbddf6b4f3d8722cff9f671f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 20:26:22 +0000

    vtclock: update 2005-02-20 bottle.

--- Formula/vtclock.rb
+++ Formula/vtclock.rb
@@ -6,6 +6,7 @@ class Vtclock < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "380055ab532f75e4cb03f91d5e0cbceb14371d4b57ddad9771d562cf572e5746" => :mojave
     sha256 "d72cbcb6862c0030433a2413fbf1d1773d6ed8207a5c681c9ffd206fa5798f45" => :high_sierra
     sha256 "766e69763326b8a8c5cfdc636cbba9f6fcffde0739be56612c54a2904d95d456" => :sierra
     sha256 "f87c685e59533a0085b439c4153c2734d4091447f5a81c627ccc0d2e589ac65d" => :el_capitan
