9d32787bd5edf6c565e0b56893759c2e681d2cd6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 22:14:13 +0000

    since: update 1.1 bottle.

--- Formula/since.rb
+++ Formula/since.rb
@@ -6,6 +6,7 @@ class Since < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6c0290f3500966bb4155352bf277ae127eb341796729dfcc2b9ca968df20b9c4" => :mojave
     sha256 "a5b4f42858c41ad5d60850a3a01b8658fb4e58d2473fe2d36938f4ab66eb05c6" => :high_sierra
     sha256 "ff4ba4b7cad5fa4211bff04d5868521bc21b60995cf40f15bd507abb7c4cbaab" => :sierra
     sha256 "ec4898462899cb632329f71dc0b4dd9a13a051aafd6da7dfd22e940e9d1ce01a" => :el_capitan
