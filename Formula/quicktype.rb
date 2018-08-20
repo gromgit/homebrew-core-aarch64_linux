f2d8075fbd17dd4feb76585615b2218153563721
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 14:08:52 +0000

    quicktype: update 15.0.110 bottle.

--- Formula/quicktype.rb
+++ Formula/quicktype.rb
@@ -9,9 +9,9 @@ class Quicktype < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9c54e23a2adb0af9e4751717fb0125656b964369d42055e1f8b0c59c84649324" => :high_sierra
-    sha256 "e6ae6d2e6ed8152f441ee86c8887b9646fcf4ce846cf3ddb8541486b7c867585" => :sierra
-    sha256 "4a2f95ffd4784b743d80c8a41a70dc72cc3f156c4208f399807cb382e16947cb" => :el_capitan
+    sha256 "a8149409f6914ca0b552507f6bc0d58ee96f7ac938a41a071d35fd98ee35c2f1" => :high_sierra
+    sha256 "b6a0307ec9064fc8134ef8b1921f422665ae71f9554f0a1b91a271512ea6132e" => :sierra
+    sha256 "0dfe47a5dd54ee42fc5278ea7cd9bfde228a8afdf76ca4f0b3a3402f4a6cafaa" => :el_capitan
   end
 
   depends_on "node"
