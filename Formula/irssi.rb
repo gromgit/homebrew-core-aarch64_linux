2d999526df857493a6caf4700a7332edf82ddcb9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 10:03:49 +0000

    irssi: update 1.1.1 bottle.

--- Formula/irssi.rb
+++ Formula/irssi.rb
@@ -5,6 +5,7 @@ class Irssi < Formula
   sha256 "784807e7a1ba25212347f03e4287cff9d0659f076edfb2c6b20928021d75a1bf"
 
   bottle do
+    sha256 "e27d4bf8c70368533d9f6e9094a29c4b5f018014930c8e29f924a4b960841d5e" => :mojave
     sha256 "6698db08f5b35df7bf0acbb8f005f79e3598999efcd78bdc3f82cc0eaaac2a6f" => :high_sierra
     sha256 "dc216333ed72a035a6f817e6970b74d82c06994a2bbaeda7bfe3e2c1b7e087dc" => :sierra
     sha256 "10a3178cc7c7542d2350830d8e2d08b16afa1d33d923193de46cabf58086509e" => :el_capitan
