b3a62a3466575bf8760f7f64f21546bcc08adabb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 19:28:40 +0000

    autoconf@2.13: update 2.13 bottle.

--- Formula/autoconf@2.13.rb
+++ Formula/autoconf@2.13.rb
@@ -6,6 +6,7 @@ class AutoconfAT213 < Formula
   sha256 "f0611136bee505811e9ca11ca7ac188ef5323a8e2ef19cffd3edb3cf08fd791e"
 
   bottle do
+    sha256 "1ea8c751806adc1ee85dcfef1b66d2d8aaed52d9848362635bed0251b7437fa4" => :mojave
     sha256 "180a5d234c513e34952d639cfbcb5c486d64ad814d3d422b6cf86d048c0267c8" => :high_sierra
     sha256 "1631e2f720355ef11b150ebddde824c02eab44a336203bcdb5282e4784ecbcd9" => :sierra
     sha256 "f6b148c2bcf08f0e143a4757fa6784bf9e0d780d5ca8ec8cd97c042316c50d84" => :el_capitan
