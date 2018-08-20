4aea743c76222825be11cbd34452ef44ba2e5f23
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 09:25:11 +0000

    axel: update 2.15 bottle.

--- Formula/axel.rb
+++ Formula/axel.rb
@@ -6,6 +6,7 @@ class Axel < Formula
   head "https://github.com/eribertomota/axel.git"
 
   bottle do
+    sha256 "9a113a4e26408726833f2b0b84cd420944d633b4bebba1982ee96a65176bccdf" => :mojave
     sha256 "18d458adef55854c33e4be487ce77eaa294fa9b7c8f09bcd3aff68cea063c2ab" => :high_sierra
     sha256 "2ed9747656442072e684c56a6354fcbfd1179b01cd0873cc77760a6e64270662" => :sierra
     sha256 "10257917ed87edf070064ad51dac4a3685415f969a6999a5a55938aab355f584" => :el_capitan
