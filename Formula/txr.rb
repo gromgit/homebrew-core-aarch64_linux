1c0de90e55a361790994dc06486c500612853886
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 21:17:25 +0000

    txr: update 198 bottle.

--- Formula/txr.rb
+++ Formula/txr.rb
@@ -7,6 +7,7 @@ class Txr < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "896a215273823e5d0fc3a3169b85fa669f81e65e1a2e8a8d691896cdb73e3029" => :mojave
     sha256 "80a3df0768847b56f4d7f9e06db6ffe0e248f6d6be33aaaaaf305202bdbf1a7e" => :high_sierra
     sha256 "69ac09578a8a6e2aa8113b6795553b9be2266ee2f4803435d0904658992b8a7e" => :sierra
     sha256 "164024941a33530d80cfedf4f0b3f585863686dbe623036bcfde4d9112276b5e" => :el_capitan
