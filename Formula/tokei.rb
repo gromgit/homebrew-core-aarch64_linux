d16615ba5b6196f64882eca1031c44603ce7f4b4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 14:53:38 +0000

    tokei: update 7.0.3 bottle.

--- Formula/tokei.rb
+++ Formula/tokei.rb
@@ -6,6 +6,7 @@ class Tokei < Formula
 
   bottle do
     rebuild 1
+    sha256 "e476d8501d7f2e2595fb877a87076f84444a2efd0908f967f2cbe0d8d230960f" => :mojave
     sha256 "573fad477decddf656f666848a3da572a8d903ff8b5352f712fb6535f52c83d2" => :high_sierra
     sha256 "fbc0cf15f958c8e4a5aeaf8b270aefbe97f2f2058e3c6a3dbc9b3268f032de38" => :sierra
     sha256 "ecb70caf0becbd065ebb3f23d8fa188e072c20b279f385e4bf118276eda53c12" => :el_capitan
