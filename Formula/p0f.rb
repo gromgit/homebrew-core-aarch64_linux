0e0880aff645dd80afb2c672bef14c9600dfdcbd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 23:15:59 +0000

    p0f: update 3.09b bottle.

--- Formula/p0f.rb
+++ Formula/p0f.rb
@@ -6,6 +6,7 @@ class P0f < Formula
 
   bottle do
     rebuild 1
+    sha256 "616522a36fb167db7a3e36c2113d6214e0e054be9c8fe7dc67a9c9da1b9a1c23" => :mojave
     sha256 "ccd5b804de7e6fd430540283d6064d4647e1224dd2663f21e309a4077b1c30b9" => :high_sierra
     sha256 "bd25792c98fd8c88599ab373ed8b9265fe4b69c47b6b3ebc84911750f48f190d" => :sierra
     sha256 "37aea629cea6430b8516ea80eaaf687844a2a1656eebe7744ba6f3746381ce48" => :el_capitan
