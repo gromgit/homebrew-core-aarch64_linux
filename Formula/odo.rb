b4a178c808b594d99d217503fdc3610d1bc13fb6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 23:16:01 +0000

    odo: update 0.2.2 bottle.

--- Formula/odo.rb
+++ Formula/odo.rb
@@ -7,6 +7,7 @@ class Odo < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "f2bee7fa62ba66589fb75b3eb9b32c843e0bfc4f054521876fd891388765eec9" => :mojave
     sha256 "0bfc54617186d149c98593c74dfaa59a42b2edcc7df1855fd452594ec42f1476" => :high_sierra
     sha256 "06af025b0a2df201a9b79944dcc4809708b305242622a90c92a9906a18adf2d6" => :sierra
     sha256 "979cc7131a35180614e848fa5fa12a72f734da7321358c89dfbd425fc8dff837" => :el_capitan
