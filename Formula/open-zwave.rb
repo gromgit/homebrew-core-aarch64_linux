2fe5752af3b2ae5df126d4ee12775477c936f65f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 23:16:02 +0000

    open-zwave: update 1.2.919 bottle.

--- Formula/open-zwave.rb
+++ Formula/open-zwave.rb
@@ -5,6 +5,7 @@ class OpenZwave < Formula
   sha256 "473229f3dd3d6b260e6584b17e5c5f2e09e61805f89763f486a9f7aa2b4181ba"
 
   bottle do
+    sha256 "cf06f87e2f334038f09897ed3bc91aba37e037bd3a2a8850b81ec58d4b3bad45" => :mojave
     sha256 "ee97a2cce9fed3f63fb917e09e448f35815eff5120ebae16711777be4796d5c0" => :high_sierra
     sha256 "68502b64e9fb7031f9fc5fe4fa7e6714fd5e5ab93d467169e7e47e90c322ff64" => :sierra
     sha256 "4bf1d8a8ba3fcf4ee39df9bfe09017d3a432047e5e6bf0dce6f6e612ad174b95" => :el_capitan
