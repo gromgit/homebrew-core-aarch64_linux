531374328010304f80eb8818f87c0d126f89f183
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 14:06:22 +0000

    bench: update 1.0.12 bottle.

--- Formula/bench.rb
+++ Formula/bench.rb
@@ -10,6 +10,7 @@ class Bench < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "cd770017ec21cd829d97013812172c21dc6ce8b00426029a692f5dd5d82dc0fe" => :mojave
     sha256 "c6cfcfc380c022f98a32a061268dca710f109600aa6d52c5b5e541855889ea14" => :high_sierra
     sha256 "54eb5c88dc8c8d8fa43a16fa5439b2592f0fac57bf1eeb895b181de6b2d56f94" => :sierra
     sha256 "7081e2d83fb8dee4d72fa203e0d6751eb70e1d38d64c5366f1f04cdd72fec90b" => :el_capitan
