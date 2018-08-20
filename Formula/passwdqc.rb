2976ddc6b11061ffd4410489e8c1e4d04502ba25
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 23:14:17 +0000

    passwdqc: update 1.3.1 bottle.

--- Formula/passwdqc.rb
+++ Formula/passwdqc.rb
@@ -6,6 +6,7 @@ class Passwdqc < Formula
 
   bottle do
     cellar :any
+    sha256 "e0f9450b595fb6935ed0cba174755cda95a7ff2fc7f9cc0e9e9027f69e5c1b6c" => :mojave
     sha256 "f3225da4795b1f3c89e25aac62101021f533faebe52fda91101a497da156f797" => :high_sierra
     sha256 "e63d866e12db3c5b031b33681a8a6b5163908cbbedde6d33e72e2543a4a75ef2" => :sierra
     sha256 "607a5adfb33eca79f847569357c77d643b9be4b17ba73c915575990ad676bddd" => :el_capitan
