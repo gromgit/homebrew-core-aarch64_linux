82bb75502fa479a0f03cfe4fe0c105cb623d4c67
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 23:14:21 +0000

    pakchois: update 0.4 bottle.

--- Formula/pakchois.rb
+++ Formula/pakchois.rb
@@ -5,6 +5,7 @@ class Pakchois < Formula
   sha256 "d73dc5f235fe98e4d1e8c904f40df1cf8af93204769b97dbb7ef7a4b5b958b9a"
 
   bottle do
+    sha256 "40909ddce4f17e53dd3b6bd68b61c45cfd824ccc486fe2bf0e76be5f55d5681d" => :mojave
     sha256 "ee7978dad7998e747e468f1b9afaa692304efb2ca857d4c0903945f030841fb7" => :high_sierra
     sha256 "84a90b245c59676817d4c9100d64d7747068e0d3557fc5c3218d8a83a98f78fe" => :sierra
     sha256 "b02057a2cc01daa05754c537820b58d7c77b632fc5fdb2a6f6dcec77341fe65b" => :el_capitan
