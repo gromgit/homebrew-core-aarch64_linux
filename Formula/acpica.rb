1eb34d4e813a7f866a005b8d751be61e39b5a3c4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 18:52:08 +0000

    acpica: update 20180810 bottle.

--- Formula/acpica.rb
+++ Formula/acpica.rb
@@ -7,6 +7,7 @@ class Acpica < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ca895ad2ed43004463000c0dd86f2ed94773565cf16bf5026f4f2e01e95d304c" => :mojave
     sha256 "ead5eaa7c0fda1e28aba6dde8932f6cd87e6ed41cc24ca7cae3a988be8472ed2" => :high_sierra
     sha256 "b8fc8e51abb8bee1a22fc0cfbcefbd16c717f8248e17b5228dcc4eede15da4ab" => :sierra
     sha256 "e1e59d509d939e868ec40fb0a572f6c8c5facf52f2a1e4a6e44e987f3b1e82b0" => :el_capitan
