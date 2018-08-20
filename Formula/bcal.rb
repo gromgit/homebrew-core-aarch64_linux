137a1aaf05668035c041fbbdc16c599ce9b3f422
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 19:28:32 +0000

    bcal: update 1.9 bottle.

--- Formula/bcal.rb
+++ Formula/bcal.rb
@@ -6,6 +6,7 @@ class Bcal < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7e4e6c646db9a2f18743618e221a45deb5a2a14a3204a62a07081b5f6123c079" => :mojave
     sha256 "3c00afb8282817dbd475d0611972a67583e38b61aba69197c754443a1c65c2fc" => :high_sierra
     sha256 "1a192e98388164010d63d19f9f858bc6c60473ce0abe1b3aea21130f5b2f366d" => :sierra
     sha256 "4b75a0f2083440568deecc1328a1106617f8dbd3342a558e755109b00aa6fbfc" => :el_capitan
