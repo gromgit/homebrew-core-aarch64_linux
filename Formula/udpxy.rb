97896944c3bf536814d654e2ecdbfcd793ef207e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 21:17:28 +0000

    udpxy: update 1.0.23-12 bottle.

--- Formula/udpxy.rb
+++ Formula/udpxy.rb
@@ -8,6 +8,7 @@ class Udpxy < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "37defc78c8d90754343c9d0c1b69bf1939ba081c42868daa41572551f5d60e4a" => :mojave
     sha256 "46de795b585d88c658554fb943931885db85d75c7f838d9db6d11d98e46538d0" => :high_sierra
     sha256 "ee35787b2877b8ac1a9fa967e9f8fbf466f8c107e28cc61fee59c26aef9bf44d" => :sierra
     sha256 "0d4a899340bdee7f4497d68fe3bc59213ad83382d205aa08ada871d9d08c010d" => :el_capitan
