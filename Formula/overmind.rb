7d97c505cbf2477679cb16a7ff1e361fee016ced
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 14:55:36 +0000

    overmind: update 1.2.1 bottle.

--- Formula/overmind.rb
+++ Formula/overmind.rb
@@ -7,6 +7,7 @@ class Overmind < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2c5aa09dbc0d6d00ec9e4b6f4ae56a9ec65b21992cad837996c50db403a0c261" => :mojave
     sha256 "1d46dfcd6a60c9e504251302ff2c65f84f575c68f1b7315cd847e52762d83c63" => :high_sierra
     sha256 "db203925a49561a4841a7c888b5a3d8b15857b62fa1d4398bfc5ade14481bf1b" => :sierra
     sha256 "daf31f46ad521fdb79d74e6c7ec995e109f4714b68ac0fa9ae7d4a010793f0a7" => :el_capitan
