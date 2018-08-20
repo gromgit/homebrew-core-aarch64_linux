3ab7c456b0c1338d105403489e2fcf93914b0e37
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 20:35:03 +0000

    xmlstarlet: update 1.6.1 bottle.

--- Formula/xmlstarlet.rb
+++ Formula/xmlstarlet.rb
@@ -6,6 +6,7 @@ class Xmlstarlet < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6e5d11ee1419a61a9f043663c1236d064ee692fd187ae15bf2114b42d8f0889e" => :mojave
     sha256 "56ce0e3190080e6e1111ebb31aa06aea53f16cde50359a356c24ff86a4df72b3" => :high_sierra
     sha256 "4958bf868beefb9a2b391c0fe05f5289b67a4cded708d71c4cc5fced130bac55" => :sierra
     sha256 "2d9a9b5f875b91c78378e7f3df12595528d8e4b9ec9e321131b7f9f78f30acd8" => :el_capitan
