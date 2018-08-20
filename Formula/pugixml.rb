960db93ab3f799f81e47d31037dd6b14236dfba0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 18:48:55 +0000

    pugixml: update 1.9 bottle.

--- Formula/pugixml.rb
+++ Formula/pugixml.rb
@@ -6,6 +6,7 @@ class Pugixml < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "18c71dfca0583f9896fe1215b90772d6619f698c5e432bdab5a1afdf75c901af" => :mojave
     sha256 "53ad542db23961c7ed6e7e7e6974c54198823ddce6644ae172d1f798136a0218" => :high_sierra
     sha256 "879ed1d86051a4230499b368e2a8d7b9ebcf5792ff0dd40aa711b311426bf6e1" => :sierra
     sha256 "a8fe6aafdb9826497121f3b616f382a7447a04dc252770b0828de1e4b4c99b99" => :el_capitan
