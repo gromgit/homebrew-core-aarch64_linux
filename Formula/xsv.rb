5bb4ec758408df3a58244f160330545dc1cf53b2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 14:53:33 +0000

    xsv: update 0.13.0 bottle.

--- Formula/xsv.rb
+++ Formula/xsv.rb
@@ -6,6 +6,7 @@ class Xsv < Formula
   head "https://github.com/BurntSushi/xsv.git"
 
   bottle do
+    sha256 "e55868cc4f9f6284ef7aecdff580dcc93699a8ceb03beb7ed27d29ad9ee23707" => :mojave
     sha256 "9837a63d7a48c82a723294a24e66e64920c0b0ead2bc92c408e502d76cd482c7" => :high_sierra
     sha256 "c909a6c8c825a231d0148fd3776d764bf6c0a67663f2944e6b66ae492b206387" => :sierra
     sha256 "34848a92e9e392a942e31cb8af2c725e80742c27ecd6f0579a7242015b50259a" => :el_capitan
