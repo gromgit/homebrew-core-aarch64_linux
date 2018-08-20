e0cd924505d72bbabf6844158dacac0db6922a9c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 22:24:48 +0000

    sersniff: update 0.0.5 bottle.

--- Formula/sersniff.rb
+++ Formula/sersniff.rb
@@ -8,6 +8,7 @@ class Sersniff < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "89c553505287f0cbd3ef2d46c6b04eb328d0748db6e60511b25d24cefcab83ce" => :mojave
     sha256 "eb3cf737a135049c3f7b8bacf4d71670dfc755a1b266f41f0865fb8983a53d55" => :high_sierra
     sha256 "077112b0300e14f956fbe45ff650cf973e04c355707a3add63b8efc7fc047737" => :sierra
     sha256 "abde8af644fecfa883abf597486fd269b379001ae29161fbd21777d0506edc86" => :el_capitan
