b4c186164c94c3f9ac9bfdab95d6c49abb558aea
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 21:25:58 +0000

    tractorgen: update 0.31.7 bottle.

--- Formula/tractorgen.rb
+++ Formula/tractorgen.rb
@@ -6,6 +6,7 @@ class Tractorgen < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0416b04f09a509f3912de4cac964fb96e2a54246f8ffb9d170d4f2bb16b6f959" => :mojave
     sha256 "936883746158534e9650a0b26f18e680eed527fb56f71ad51e5ec203d8f7f451" => :high_sierra
     sha256 "646d87ca0cb1a5ec93a8aa1ddaa1f28233347ca0a1f56e49c323809ec8295432" => :sierra
     sha256 "ccac503b4577fc81e69d3e778c27c31fad9a1c5fa8627e97f293d87ab1177f8d" => :el_capitan
