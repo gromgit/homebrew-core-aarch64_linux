ecc69c996d3d4a63a9bfbad33b86729fdbe2f17e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 09:25:10 +0000

    w3m: update 0.5.3_5 bottle.

--- Formula/w3m.rb
+++ Formula/w3m.rb
@@ -20,6 +20,7 @@ class W3m < Formula
   end
 
   bottle do
+    sha256 "e8aa7de4835538616cd9902bb49cbc1bf5c897969c9fdcc8aed9ecd42175e376" => :mojave
     sha256 "4b0be4259e778c1c7df97d43b9e176e019e6de5d7bdeceec89baa34bdc0392db" => :high_sierra
     sha256 "1e2305cc1e1e717648d331a44d3f0867ad20e63a50fbdd0d824e4482e4d8a6f7" => :sierra
     sha256 "745f4a07f511c4a9ecbf4cb12bba58eac9c36a92291945134cbebe4fe03dc747" => :el_capitan
