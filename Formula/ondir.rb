fe60abd6fa7c4c85813fe27495313c48903cfe86
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 23:16:03 +0000

    ondir: update 0.2.3 bottle.

--- Formula/ondir.rb
+++ Formula/ondir.rb
@@ -7,6 +7,7 @@ class Ondir < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d0887254ee09aa205791efded5cdec39cdd2d997132fd5b4bf3c7fa4c4f90337" => :mojave
     sha256 "5f1e570b6cd0ef892deaf6f04c90d752ff976dcca8d3be31d6d6ddb546241995" => :high_sierra
     sha256 "90e85060a76337368083c889379b71cda5994ab163b73337050819472f41800c" => :sierra
     sha256 "8d841a2a8b98a512265dc05deb3ea74e7458a4d5412da786f595c31420b7fadd" => :el_capitan
