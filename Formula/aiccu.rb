5532be8125d43e49446e0d7ee9badb598ec9e72c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 19:33:07 +0000

    aiccu: update 20070115 bottle.

--- Formula/aiccu.rb
+++ Formula/aiccu.rb
@@ -9,6 +9,7 @@ class Aiccu < Formula
 
   bottle do
     rebuild 1
+    sha256 "f6c90e2ecdcd0d676abe4fb32f98a8592d348ac3794de62fa64f403e5ecbbf17" => :mojave
     sha256 "9033bb99bd8fbaa3b74abb0fa850b2220c317628851af361180c0c764732d49c" => :high_sierra
     sha256 "ee19bef55805a8562bddb41a3af66e5bce9589b1e4d96b05348a37b5ada2c091" => :sierra
     sha256 "572e103e9de9c872eb202e18d5c4f352f0b9dc26d284d5979b83ff6fa3daa5b2" => :el_capitan
