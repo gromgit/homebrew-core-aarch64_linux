a4f9a37ad03fc1651ca88c151525db9487236412
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 18:50:34 +0000

    imagesnap: update 0.2.6 bottle.

--- Formula/imagesnap.rb
+++ Formula/imagesnap.rb
@@ -7,6 +7,7 @@ class Imagesnap < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "f407afef9b42d250115f21c69a28a9e4fd143619d71cac9f4d92c0d17d3512ad" => :mojave
     sha256 "6b40f134d9180d7874db92f4a79dad69a74669791f13233e435eed8710c2f4d2" => :high_sierra
     sha256 "6cd7d838362754709f98d28c3fe45736f188bfdc8662cf1986089091c5d1e3d0" => :sierra
     sha256 "bbe0115174e191a6eaeedcdb3136e4c9248e7bab649bb30ddd4e07d27ea4e553" => :el_capitan
