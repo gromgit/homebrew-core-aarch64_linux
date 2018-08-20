6d109a5b8cd89b0b41345b5db6674497cbbe63ec
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 20:33:23 +0000

    uniutils: update 2.27 bottle.

--- Formula/uniutils.rb
+++ Formula/uniutils.rb
@@ -6,6 +6,7 @@ class Uniutils < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2ea235c47ef4ede643ccb1adaccbc376b0cbe39bbdeb3d5623bcbea210cd7519" => :mojave
     sha256 "6717501e797865b956ded03f433b9353e033328727bcdd6263b1f3721c8ef30d" => :high_sierra
     sha256 "5f6609e92596f48fbb20bd0245437f4e967ebc9d06d9fc5e130584f394dce21a" => :sierra
     sha256 "9e83bffe9268c4be17f6e37254da13d2edfeee19869bd075580eeaa8f770078b" => :el_capitan
