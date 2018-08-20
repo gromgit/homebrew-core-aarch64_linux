eafa48fa6745b231caf2c436e0ffbd8c057f1e0b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 15:46:16 +0000

    libccd: update 2.0_3 bottle.

--- Formula/libccd.rb
+++ Formula/libccd.rb
@@ -7,6 +7,7 @@ class Libccd < Formula
 
   bottle do
     cellar :any
+    sha256 "2940a850669c09d0cf91c156ef8ad0bbf7d3706c9e3584d84ac0afedab457c05" => :mojave
     sha256 "16412e782ac7bf2b0b488e495189663b75e04fdd51156c389ff580e7757f55d5" => :high_sierra
     sha256 "0bc8ed0f2e20d71f778dbf23a01fec029685f444eb4c50b9eaf7144ef907126b" => :sierra
     sha256 "506cfff7d53b7ad38174f240d156e46d0a79f336f859e5c8faca03e68b2a850b" => :el_capitan
