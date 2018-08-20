622da3ae7126798dc4c6e6cef0eb3f09cab2b5eb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 20:33:22 +0000

    vbindiff: update 3.0_beta5 bottle.

--- Formula/vbindiff.rb
+++ Formula/vbindiff.rb
@@ -6,6 +6,7 @@ class Vbindiff < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "907ff3fce1fcb1cba6e746ea624f84234fe55703caa380b7ee19c1f8bb6cd193" => :mojave
     sha256 "c5d1025c94e7fe141a9522ed1460bfba8047393d63d59f54b391dec063c05c68" => :high_sierra
     sha256 "d6474b9e6a00c71f2c207c07dbfb015aa428d8f32e193b69c7b4a1f534f128db" => :sierra
     sha256 "b2f557094c03f5870173b32cdf9e8ff8bd7fd74340adfea7f1db6b6eced367d9" => :el_capitan
