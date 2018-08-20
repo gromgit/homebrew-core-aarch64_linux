b54aae21c7bfb9bd9a6be72f5ea864c09f7fb467
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 22:23:03 +0000

    stgit: update 0.18 bottle.

--- Formula/stgit.rb
+++ Formula/stgit.rb
@@ -7,6 +7,7 @@ class Stgit < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "93befd3ca1f31141139803efff2369ae20e9cbcbd58f662bf56f6ce9361f569f" => :mojave
     sha256 "cf5d28fd456e9d28a4dae935ce96968cfa019801381e9d4ba50ed978d42fe791" => :high_sierra
     sha256 "0eee6dda264e8d497079d734b3127dbc84931a831edcb488a6ea55f80cc1cce5" => :sierra
     sha256 "0eee6dda264e8d497079d734b3127dbc84931a831edcb488a6ea55f80cc1cce5" => :el_capitan
