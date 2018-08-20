882c39f5efbe3969cc7058fe64fe9672331360f2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 11:16:58 +0000

    netcat: update 0.7.1 bottle.

--- Formula/netcat.rb
+++ Formula/netcat.rb
@@ -7,6 +7,7 @@ class Netcat < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "3ac133de6b67a147954d78b9bd0c4c4cf4e0f43bdbbb98f51d8d962bb752d973" => :mojave
     sha256 "879d9c32f09e9ef31cb672983707f9d95341f6639bb8a4db54d7a6ea0878b946" => :high_sierra
     sha256 "9027fd429d5407fba0b3206bd0cd198c669f4744155efcf8e0dbdd6ba69b6d34" => :sierra
     sha256 "1f346605e0236ea7880258da2abf0bde1d7d8d8735a07d6d32feaf12425ff6da" => :el_capitan
