be5bf557cbecfe9b6fea439747d4da829e286eea
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 20:35:01 +0000

    yydecode: update 0.2.10 bottle.

--- Formula/yydecode.rb
+++ Formula/yydecode.rb
@@ -7,6 +7,7 @@ class Yydecode < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "18d815befe31bcdeaac8edff43cb878f53c34a608fa946c13a14143139bf887a" => :mojave
     sha256 "e2e7285f1f2b18b4b99800602d15932dba435f6480c5776e5b57b734727f652f" => :high_sierra
     sha256 "91dc4fe34931d45fcebaead39ce505116322c7866e786cf86a7065f9e57b42ac" => :sierra
     sha256 "07aa31cabc4f2533df3b5670beed1ba99e3e7dcc3ffb3cf55fff56866e7bf11e" => :el_capitan
