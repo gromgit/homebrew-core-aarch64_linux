1699bd69bdd3f79d952d53060e3816a2690994e1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 02:03:25 +0000

    multitail: update 6.4.2 bottle.

--- Formula/multitail.rb
+++ Formula/multitail.rb
@@ -7,6 +7,7 @@ class Multitail < Formula
 
   bottle do
     rebuild 1
+    sha256 "9d561e3132f6749aec218961f65d84516cc6a99a2247ee6ef4abf6ed1accc942" => :mojave
     sha256 "b8d2ab93cd8bf7954ffc49ed929aa0cf2c60a2ebeb872e9d3686d314af043be2" => :high_sierra
     sha256 "ec1007a1ab7ffc9394f25d1a838d54051c04c14667721e8db9f49803084f6dcc" => :sierra
     sha256 "b9c5e200dab1bf30ef4bd9bb257d5728c6779552aab97a1ddc65d02ad697cd8f" => :el_capitan
