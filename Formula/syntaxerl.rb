9fa74241daa24793d38247552094ccfca8e213e6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 14:34:37 +0000

    syntaxerl: update 0.14.0 bottle.

--- Formula/syntaxerl.rb
+++ Formula/syntaxerl.rb
@@ -6,6 +6,7 @@ class Syntaxerl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "569cf5e87181031c9b13dd6cb2f4fe17967d4392c87a2db30d498883a05106c6" => :mojave
     sha256 "2b4c6e6d7e7413fe81e5107e80841f5a8c4bbddec9e12f3f53c92bedd5fced3b" => :high_sierra
     sha256 "0e0ac8402dd61720eeca50047b173f7500dc38bd7cceb37915c4425b7492044e" => :sierra
     sha256 "648f30c4fb2db062f4c9f27d3c73cf679181e7183a5b7b83b5670624d88b27d5" => :el_capitan
