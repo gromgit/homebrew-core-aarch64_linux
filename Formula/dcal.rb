5a60ebdb90cad105b0290ac3f7417d08e8de09af
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 09:04:38 +0000

    dcal: update 0.1.0 bottle.

--- Formula/dcal.rb
+++ Formula/dcal.rb
@@ -7,6 +7,7 @@ class Dcal < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "95160b46c7fe651d0a808cba358c75b0346145e0ac9240dc1c91cb634e6f74f8" => :mojave
     sha256 "74f5e51d4dca180bf93dc6d6cd1a147245db0b03a49992b4e0850e0af991020e" => :high_sierra
     sha256 "e36329914f9b602d565480fbe28e4da3e581fc2d0623465666cb430590cc2519" => :sierra
     sha256 "4135558d3a30c78364170daf22dc36f3c22a14b2574754edc27d2483e711b948" => :el_capitan
