ec1753dc6a713f522102588c6f2b2586f57e1137
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 00:58:30 +0000

    libupnp: update 1.6.25 bottle.

--- Formula/libupnp.rb
+++ Formula/libupnp.rb
@@ -6,6 +6,7 @@ class Libupnp < Formula
 
   bottle do
     cellar :any
+    sha256 "28cc0a51186e7a4a52709620ec37e923002a8537e1e2db5c2096fed4987731a7" => :mojave
     sha256 "cbca37b45cb652c73d4c5ae0ae087338bb4c606f5be4306c5d998c39c382bb4b" => :high_sierra
     sha256 "b870845572dd6d11ed90fedfb367bbd53066f6b9c90e522b97ce88ae53ccddfe" => :sierra
     sha256 "9b660881232e6ce94a375962c1df55f179f69335c7d11907b9a9c5dd81693360" => :el_capitan
