8dae5c5607c4dbeae8e0854b6ebe8e9806557fa4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 00:32:08 +0000

    libdsk: update 1.4.2 bottle.

--- Formula/libdsk.rb
+++ Formula/libdsk.rb
@@ -5,6 +5,7 @@ class Libdsk < Formula
   sha256 "71eda9d0e33ab580cea1bb507467877d33d887cea6ec042b8d969004db89901a"
 
   bottle do
+    sha256 "47485db7001965531b700308a3d464a616703ddd8fdca64c8a7d2b5049481eb5" => :mojave
     sha256 "b4fa361c1800fd348c804873fd03f8663f7324eed228c3ba2e2d809a58fbbb97" => :high_sierra
     sha256 "d46bdf8e9c779b22a2a21c123572c08130aa36b8a817365ee3bd76219478aad3" => :sierra
     sha256 "b14fb001603c2ba33a26c0f49c7b008659ca5aa05ffaa01ab8147bac4da40d46" => :el_capitan
