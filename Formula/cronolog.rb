40e4ff41b3c8bff9c3aa970ed9f234899fe5a15b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 08:57:34 +0000

    cronolog: update 1.6.2 bottle.

--- Formula/cronolog.rb
+++ Formula/cronolog.rb
@@ -8,6 +8,7 @@ class Cronolog < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "c99140b690aae4c8e28b53ba787ed5aef53d3fbc867186aca47cab021068db40" => :mojave
     sha256 "47a40bdccb74cb45e3df9e73306162ecc7206c26760521c6a9d8760872769b6b" => :high_sierra
     sha256 "66ad5bfa0080775875d2b72cc2bbd66bc8ee8de7ca1d482217414ba5b805f977" => :sierra
     sha256 "964df15660a5c0ec25bedec56aeb128ae93794a8ad721c1c600e377df9be1c2d" => :el_capitan
