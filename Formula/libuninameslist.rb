3b986e6f2323435a747091cfa87f2617379d088b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 20:15:14 +0000

    libuninameslist: update 20180701 bottle.

--- Formula/libuninameslist.rb
+++ Formula/libuninameslist.rb
@@ -6,6 +6,7 @@ class Libuninameslist < Formula
 
   bottle do
     cellar :any
+    sha256 "a71c341da75240eb39ce630c7420a9ab0aeb0bd14a550e6c881137f442ce837f" => :mojave
     sha256 "6ddb46e2ce0578c33caff01908983536fcf5b4d15e8f71d2df98d69c63846a33" => :high_sierra
     sha256 "99824aa3d96ac74329faff3efb8be400ab2041f8a5f1111a81b23ee342711e8e" => :sierra
     sha256 "6e612d55283ea85566bbbade489cab1b99d566816ee35d9e412a1cf23931587e" => :el_capitan
