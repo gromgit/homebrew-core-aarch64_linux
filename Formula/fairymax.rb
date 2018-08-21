a77b484561832f5efdffe9ff4ae0d3ec6749f7fe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 09:41:27 +0000

    fairymax: update 4.8V bottle.

--- Formula/fairymax.rb
+++ Formula/fairymax.rb
@@ -6,6 +6,7 @@ class Fairymax < Formula
   head "http://hgm.nubati.net/git/fairymax.git"
 
   bottle do
+    sha256 "6cfd303feede9a2a37279581a1981b028ba551ed9e5c151491623b16c1d915c7" => :mojave
     sha256 "53deff51f5281ba65ea84597cc4827725871fa6ce1031cea529bb82cfd6ee3de" => :high_sierra
     sha256 "95706f3f4968922cffae144d91e64d26a4c88026f228d555f144a4ad27c37007" => :sierra
     sha256 "9a829afcded25d1e895e09e91e39ca44b4aa9a6fa97f5315e348228224765d7d" => :el_capitan
