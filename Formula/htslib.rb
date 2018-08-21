a8d28456f30fe19af26f06df60d11ea12e64cb87
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 22:25:26 +0000

    htslib: update 1.9 bottle.

--- Formula/htslib.rb
+++ Formula/htslib.rb
@@ -6,6 +6,7 @@ class Htslib < Formula
 
   bottle do
     cellar :any
+    sha256 "708e1bf70be4d14c4cc937984cb56cc7f47048a266f62988e630fe874e4bc848" => :mojave
     sha256 "8200a468ef3bc3fd85523e80ed7d3c99b86ea8b120ea06acbfc8c8f36bd878e8" => :high_sierra
     sha256 "bdbd67c240d87c70d045067bd29de21eddaa73b13ec7e855776467df6167562b" => :sierra
     sha256 "fa53384739075cd0bc825a32dfd35b2dcdf363dbf2c6d081dfd515b5cbd5c722" => :el_capitan
