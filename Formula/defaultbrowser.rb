220826f28c06b584f90d654a5016b7de2e0d24bc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 09:04:32 +0000

    defaultbrowser: update 1.1 bottle.

--- Formula/defaultbrowser.rb
+++ Formula/defaultbrowser.rb
@@ -6,6 +6,7 @@ class Defaultbrowser < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3a02a1fd0321f5070fa4ec9088a4a58a28ddb561bef5b94a0ccc31fdb896efa6" => :mojave
     sha256 "d07cc35e06f440584d698cde7ae5f27b09acfeafacc1499d331aa8553e945961" => :high_sierra
     sha256 "d0279f8c05c0c7828c534517897346fc231864ea8534f0ae04878ecfd51ca72d" => :sierra
     sha256 "f0ccf84abbd31469f80c4d232292dd280a978d3f04a1a6db46079902d9821d1e" => :el_capitan
