bcb746ce8332adb9666cb6625fca3ffe9ae3187a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 11:35:26 +0000

    npush: update 0.7 bottle.

--- Formula/npush.rb
+++ Formula/npush.rb
@@ -7,6 +7,7 @@ class Npush < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "fb3618689797a95b8296a7b37f3c8f2e9cb29fdcbd9b2fc9ac9d585e46d6eab3" => :mojave
     sha256 "c3d40f8709487c01053f5ea09e35c047ae6bfede34d21e97703d38c9985d67b0" => :high_sierra
     sha256 "ce2f958ef8d766791137266e74b7c2cd0843755d080ecbbd6a7074bc7d035c19" => :sierra
     sha256 "c37e743784c68e9c1bb1527d4c6161a5653831de44b3203be8c1cb07d9eeb7c2" => :el_capitan
