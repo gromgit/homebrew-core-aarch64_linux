e9b4ad06cdfe59f5a75ccb8c3ecb1f94a80accad
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 22:04:27 +0000

    libming: update 0.4.8 bottle.

--- Formula/libming.rb
+++ Formula/libming.rb
@@ -6,6 +6,7 @@ class Libming < Formula
 
   bottle do
     cellar :any
+    sha256 "a752e52ef310c943317df0e8adfbd31fe44724123f4a11e7b7ed7055e15730c8" => :mojave
     sha256 "e8e2645d0bfafa6b62bf209f6259a1a433fb23632fa9b5048cd407d13fa9b2d2" => :high_sierra
     sha256 "38b7d494355b3b2368dffe806814b62accd9bb8a2bcdd2c3d000449a7cb0a316" => :sierra
     sha256 "c9a220b978be081b1f202b5964c2231a38eb8ea415746bb4d9b4f73bd03325ae" => :el_capitan
