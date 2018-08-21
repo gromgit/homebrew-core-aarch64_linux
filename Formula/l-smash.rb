0325e7ca5fff0de182a3c3f3dcaef6762ff7847c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 00:19:08 +0000

    l-smash: update 2.9.1 bottle.

--- Formula/l-smash.rb
+++ Formula/l-smash.rb
@@ -9,6 +9,7 @@ class LSmash < Formula
 
   bottle do
     cellar :any
+    sha256 "811e696583af5a78ec288d46f8815d5a5db246f335d2ba2e0d4f3fce9a98e2a2" => :mojave
     sha256 "eae1dfce4f50c3b48d2a3fabf415ad7ec98de0937d610fec98d700e517e18934" => :high_sierra
     sha256 "57802892865529a99658bd4da1b29eb5287259183658131cc215ef80fcd0cfbe" => :sierra
     sha256 "5751796e42e7d544f4976bc304a0ae7407dc5217b2b4218b0a6afdc18ea3eeaf" => :el_capitan
