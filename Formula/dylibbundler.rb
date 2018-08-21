f3ad291f7827ab7d069d5b1eb90485daa8be41d0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 09:39:25 +0000

    dylibbundler: update 0.4.4 bottle.

--- Formula/dylibbundler.rb
+++ Formula/dylibbundler.rb
@@ -8,6 +8,7 @@ class Dylibbundler < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "4a5ee8ce4167301c5af472a253d615a67cdb276b58f961ef031845c3ec41fae7" => :mojave
     sha256 "54e0ca248e9030351699e9ff31ed02dfe280a9dd97e1446966d0c81b14f644ee" => :high_sierra
     sha256 "3d3297c9884ca982ce3a2ef9d31a53a1f96390268d0d9b378e3a954e0ad74996" => :sierra
     sha256 "5dff018e62a9787871e45f4ae976358cfc3f7f85972a0aa0d4e039f97d4b8e0f" => :el_capitan
