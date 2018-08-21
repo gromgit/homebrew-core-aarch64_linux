370875eb5bc73b1916aef7bd1d96db671d750716
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 08:48:23 +0000

    cliclick: update 4.0.1 bottle.

--- Formula/cliclick.rb
+++ Formula/cliclick.rb
@@ -7,6 +7,7 @@ class Cliclick < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "bc373ee9ec978d416491fd82b3c25d02755b004b0abc1f5a73ff0b39e57b6b33" => :mojave
     sha256 "c371cb5b700e4a5f6495545b5f83e5414fc0e0cb98be05231a7abdf6c6607ff4" => :high_sierra
     sha256 "bb47d1d8f4c81f31dc9372b5c06d3f74ecb25026805936f62e08744d1b888ecb" => :sierra
     sha256 "e8eca6037032ad2b68b9e8c4faa40adb770b10f868448e47852571f1829aa462" => :el_capitan
