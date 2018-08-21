11f31c24668c00175c09292f3a63c1d9cbbfe1b2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 00:19:07 +0000

    jxrlib: update 1.1 bottle.

--- Formula/jxrlib.rb
+++ Formula/jxrlib.rb
@@ -7,6 +7,7 @@ class Jxrlib < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "923729f8257b2ff225affe4a2a822f2dd40e799ca45be9ac423f04b5e4e603f0" => :mojave
     sha256 "6d24053d591022594ab92f880c56da4057a9e4f8d8ecb6f942e3558900385def" => :high_sierra
     sha256 "0eb8602ab389d9fff5bce803b085af322309592dc41aa0902b223e353a9f8abc" => :sierra
     sha256 "47c308b47ec8227d813a21c8092b32ada9b7f862aef102bf619d6bb19f0144e1" => :el_capitan
