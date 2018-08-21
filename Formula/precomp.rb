e7392983061af4ce38715b752bc42fab38972664
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 11:54:00 +0000

    precomp: update 0.4.6 bottle.

--- Formula/precomp.rb
+++ Formula/precomp.rb
@@ -7,6 +7,7 @@ class Precomp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "007ecd8dbf6db55a52ca49765d1aafd9e4f72c6a6f377e8eec8b28641fd486ed" => :mojave
     sha256 "83f6d3eec9ce5b05a5cc7197e89fc6fe968c7ecb71c795343ead7ec98edcf758" => :high_sierra
     sha256 "93eaa1f454f3af8d1c9872c263e3e67b56aac5e6772a65ccce8c9ec493c6989c" => :sierra
     sha256 "62c8a72958b6fa8ef0208560ff0ad6abf187aae842715bb84cddb21d46a69d35" => :el_capitan
