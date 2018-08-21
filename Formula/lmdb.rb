690e7b4751c728b0579f17bab09346c9e98509f0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 01:09:27 +0000

    lmdb: update 0.9.22 bottle.

--- Formula/lmdb.rb
+++ Formula/lmdb.rb
@@ -8,6 +8,7 @@ class Lmdb < Formula
 
   bottle do
     cellar :any
+    sha256 "b93558612553bb8ee487b9ff433b89316b5908acf0cddee2830b2198d2876367" => :mojave
     sha256 "446eacbb6ae70705b8ef850a143a19378f840ed4070a6beb7ec80c0876913987" => :high_sierra
     sha256 "5eb74dfb0f982df132fb1c4edd45de857cba72bf461b213c4565b54a8cb9fe67" => :sierra
     sha256 "6ca8e50908dfd7feb3e405c6f348a950c798747d57d02bd426e76b1cf1b8500f" => :el_capitan
