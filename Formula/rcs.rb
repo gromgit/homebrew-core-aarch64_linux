a028beec46bfd3a308b11ac3ca932f8178df6243
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 12:25:07 +0000

    rcs: update 5.9.4_1 bottle.

--- Formula/rcs.rb
+++ Formula/rcs.rb
@@ -9,6 +9,7 @@ class Rcs < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "f24fb3b2f14d19bf02bc5b4c325f7735ec4657b43fac8e6a8a7e6a2e5551851c" => :mojave
     sha256 "f43c9160cbc605578af4473892f71f733dc05a9ab836d280400acece9cb75708" => :high_sierra
     sha256 "f082af49e1b1570892fa76b91bed0246e9ad63e59f953e0388b20dbf55edc485" => :sierra
     sha256 "4681c5fae05b4f4b267a9bccc9032de2b216437105d591ec5de7a10ca31e0441" => :el_capitan
