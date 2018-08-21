8e674bebb7b496fbebbcf9bf596aa4f1799048c3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 02:03:21 +0000

    msktutil: update 1.0 bottle.

--- Formula/msktutil.rb
+++ Formula/msktutil.rb
@@ -6,6 +6,7 @@ class Msktutil < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9f77290c5288ec446836d564636482abccb88b110a98cdd6499e97733d4fc39b" => :mojave
     sha256 "cba1afff1683efb3d6f6d500047c10691b0f46c904f404d8fbfdbed08200cc54" => :high_sierra
     sha256 "3bf390f92696706f4f6241230024c9bdb13c78c05c9f81faffc356cbcb4ed443" => :sierra
     sha256 "190a11fe9d63b99fe2982a79d83bd7ba59f4c7ef104ce860eb9ae48acb56335b" => :el_capitan
