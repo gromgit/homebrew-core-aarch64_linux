7e965cb357ae1774c4a65d43fa863e497357173f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 10:15:36 +0000

    flake: update 0.11 bottle.

--- Formula/flake.rb
+++ Formula/flake.rb
@@ -6,6 +6,7 @@ class Flake < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "aa54eed5e2c9b94a6cba5cd2aa7491d7b8ca61d3b89b72f64c1b7343e973baf2" => :mojave
     sha256 "c5a8fe6d24125870a5d420ebf39ab2acf14d5646e86df61632dc8d2e77887114" => :high_sierra
     sha256 "b2eca0967c020b082f7d7f8c4d15765204ea63aa9332012074c852967a0fee07" => :sierra
     sha256 "e521dae57260b2a71f161f16609530c854ff8ebee4252b0addf3c16b5abc982c" => :el_capitan
