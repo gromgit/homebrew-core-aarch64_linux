7c5d194f57c2411e3590988f2a2fd17ba9088a13
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 09:39:29 +0000

    dxflib: update 2.5.0.0-1 bottle.

--- Formula/dxflib.rb
+++ Formula/dxflib.rb
@@ -7,6 +7,7 @@ class Dxflib < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "1b9e667aa5bb30e050f41370afbbfaa91a563ab015a4ab4930c7dbb99fccc956" => :mojave
     sha256 "fb790fe1b9357907e77f50650ed0d696e855c311320d726472ac511297994573" => :high_sierra
     sha256 "db45aa2b00f82b996370eaf1321e0cce79fc3868c42a9524e10adce478139bc2" => :sierra
     sha256 "aff6c3f5e5bca552c5962e8ef5c43d1dd5fb0630d091e206a164e99ed8b70637" => :el_capitan
