92412d233a963aeb2864f77744dd1a18314ffeaf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 08:17:23 +0000

    chmlib: update 0.40 bottle.

--- Formula/chmlib.rb
+++ Formula/chmlib.rb
@@ -8,6 +8,7 @@ class Chmlib < Formula
   bottle do
     cellar :any
     rebuild 2
+    sha256 "1718a0a9343788718b4207596ebff457f5214879319292cc1608254374720944" => :mojave
     sha256 "426b95744d071ad76399ee240400ab74bcec9057735cbfeb2d433501105060ef" => :high_sierra
     sha256 "9781c76f933beca002df542d2db0644e51766568d9399f9e73dc39b9e896f539" => :sierra
     sha256 "6b834a6ae6e95f8daaa726fd6ae1a2d3e60335f98862fea9e790c24e5a6411d1" => :el_capitan
