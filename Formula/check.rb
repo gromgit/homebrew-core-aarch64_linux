0622edf1487354dd9490856731742ac059d785ee
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 08:17:16 +0000

    check: update 0.12.0 bottle.

--- Formula/check.rb
+++ Formula/check.rb
@@ -6,6 +6,7 @@ class Check < Formula
 
   bottle do
     cellar :any
+    sha256 "57498a48acaa07afcea73fc831986e4dbd8dd8742d35a600e6bbe3f328c32f08" => :mojave
     sha256 "fd175fded31ecc36ad06beeb18e05fd4d5f5bc538e1a445e86b703bf34373fd8" => :high_sierra
     sha256 "6ad1ff9e52d767968efb2b73b563b171561421818a86185c03639f65f0a22ab3" => :sierra
     sha256 "5de09e615daf7e12f1b10485b7bc8cb5382e04f856dc516056bae0a30b5f6b49" => :el_capitan
