f400fe1490844f5b32c5b5c9bf58b2412930c33b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 10:21:09 +0000

    git-hooks: update 1.00.0 bottle.

--- Formula/git-hooks.rb
+++ Formula/git-hooks.rb
@@ -7,6 +7,7 @@ class GitHooks < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a66bf94650a35829721b07c4f6a497154c9e667917ea8c28418b870c0de15697" => :mojave
     sha256 "710495206af282348fa5e311f825bdbbcb7a891345ff467468908e16b3dbc090" => :high_sierra
     sha256 "aaceeb7b390f71c45e3c1db15c23ab664a06bfc34de1c629a2b2f5b29e1bdec2" => :sierra
     sha256 "bdfffb820e5a7574169b91113ed59c578ebe88bcea8c890166a33fb9af17c0ce" => :el_capitan
