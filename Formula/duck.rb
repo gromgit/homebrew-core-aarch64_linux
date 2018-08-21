6c477ea3d4de8a57b0f12c2cb05a838fb50dab48
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 23:14:14 +0000

    duck: update 6.7.1.28683 bottle.

--- Formula/duck.rb
+++ Formula/duck.rb
@@ -7,6 +7,7 @@ class Duck < Formula
   head "https://svn.cyberduck.io/trunk/"
 
   bottle do
+    sha256 "7d510f90f81c1bc6f4a29bd662e50e44aa9e8de9dc4f086fc6c277bf0b224f06" => :mojave
     sha256 "e10ce4d54a2b9dfe0f0c1b3bcd7c053d3dc4cde1363e454b1b681e3d33f8acb7" => :high_sierra
     sha256 "0ed5c77ec0381165ef7b8cf7d45c3a473343c69cce4073784cd8ae0ab8c4cdad" => :sierra
     sha256 "470f922732207586a55c84141aa438d367613b96edcbb3043fe1c1e20a333244" => :el_capitan
