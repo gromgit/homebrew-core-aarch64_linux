efa2016e8ce05f7e06b6a539844ec5f58b0e99f6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 08:48:23 +0000

    conserver: update 8.2.2 bottle.

--- Formula/conserver.rb
+++ Formula/conserver.rb
@@ -5,6 +5,7 @@ class Conserver < Formula
   sha256 "05ea1693bf92b42ad2f0a9389c60352ccd35c2ea93c8fc8e618d0153362a7d81"
 
   bottle do
+    sha256 "3332bc506c85754b3b601989941613c83932ea28b4fb8aef87e45dfa576807c2" => :mojave
     sha256 "3fca2dc202bf6d68cc8294be8c2703bef813ad6583d51c3d9c4da12dbc975a89" => :high_sierra
     sha256 "c3885b3a01be7c4d0a4ed943906c4cb13be485b770271ac94cf9e439fdb9ca9a" => :sierra
     sha256 "d9f2d169e4a3adf0e46b7047991dba382147da1bd0923969bd0bec7bf7f14900" => :el_capitan
