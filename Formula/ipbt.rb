22373abde1683bbc237ded4b0becddf37349dd6a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 11:07:57 +0000

    ipbt: update 20170831 bottle.

--- Formula/ipbt.rb
+++ Formula/ipbt.rb
@@ -8,6 +8,7 @@ class Ipbt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e6116c578d1327c1c70b7a7b4640360f0a9cbe824b749df0a8a5dcdf67142e6b" => :mojave
     sha256 "ca5fe4bf38c28b6134ef995643328af82277bac9765064cafa95c7be096cbb51" => :high_sierra
     sha256 "f9dd24a2a7bd67ee00a4763b6931975f27443d906d91795962ca8a1eb647d269" => :sierra
     sha256 "3ebaf07effeb98490201a2c8cc38bfdaec25c0db5a6bf3f2936f72b944ee5309" => :el_capitan
