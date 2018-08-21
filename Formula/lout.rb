81dc698bf0205d364dda8c3bf2177b6c7374a428
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 01:09:26 +0000

    lout: update 3.40 bottle.

--- Formula/lout.rb
+++ Formula/lout.rb
@@ -5,6 +5,7 @@ class Lout < Formula
   sha256 "3d16f1ce3373ed96419ba57399c2e4d94f88613c2cb4968cb0331ecac3da68bd"
 
   bottle do
+    sha256 "2f69e0d4097fbf53f05855b5eeb2def0efcaf08c3a5b2487b1fa041031c2eacc" => :mojave
     sha256 "2de1b1b7526f7427b8a57b6239a5a8c199ee05365ead7ed8d722a9e7e3123a0e" => :high_sierra
     sha256 "2cfc68ddba21e6f485a4a57df9e810b6996d5364374c66e77b06d41ce230f060" => :sierra
     sha256 "2fbc90ffc3f12312dc11e31996ba94da3b8a4ba1c55f33ca60a5d81aef4e137f" => :el_capitan
