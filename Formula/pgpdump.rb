56b9574fdd60fddfb1421eede99e1d3950225d1d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 11:44:18 +0000

    pgpdump: update 0.33 bottle.

--- Formula/pgpdump.rb
+++ Formula/pgpdump.rb
@@ -7,6 +7,7 @@ class Pgpdump < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8141ac85359c7be7ac5ef51075823612582ecd0e02f0048cace4b4bae2217771" => :mojave
     sha256 "2d5ad982f29c20cad30f5a90d4fcd8af3d369432e2c4ab4f35fcfa3b31712a1f" => :high_sierra
     sha256 "9c2ed5f4eb7e0c833a90d53fc8d96d613b781b36c3524959fa102ae62a4d167e" => :sierra
     sha256 "1cfd7cb5b0cdbc7e70031841d7efb1196ddbbd6f11f5af3cce4b38b6f7358ae2" => :el_capitan
