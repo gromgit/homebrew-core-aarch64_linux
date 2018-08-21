f97f01e5167dd1b1de8482f58bd1eb18ac02cf81
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 09:34:32 +0000

    duff: update 0.5.2 bottle.

--- Formula/duff.rb
+++ Formula/duff.rb
@@ -6,6 +6,7 @@ class Duff < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b2f5b9c19bb74d92c6b43482b77bf6d852355b83ddfda7ca4f6340a8075067f4" => :mojave
     sha256 "a30c57c79b3cef30518fccc5227e954dd9a2383e15458f85706733dcc1fe188a" => :high_sierra
     sha256 "2af1262a9b02e687c0efc14eed3d837920ab746fe8fca9b12b9361c4729f06ef" => :sierra
     sha256 "8a469e92a6303d80752ebc80ade382261d263b9c7226ca6652eddc8954e5ff2f" => :el_capitan
