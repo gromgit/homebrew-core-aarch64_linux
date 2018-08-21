7eff02be931cf64df518924fae4669b7bb57b619
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 20:15:24 +0000

    libtrace: update 4.0.4_1 bottle.

--- Formula/libtrace.rb
+++ Formula/libtrace.rb
@@ -6,6 +6,7 @@ class Libtrace < Formula
   revision 1
 
   bottle do
+    sha256 "c7bc6a929811138840780586ef8959f50ae87d884a4b6bb60fcc6d52212a05cc" => :mojave
     sha256 "f39cdd5655d54fee98f26a5b4130e470aba254426b48207e079713ce57b8babd" => :high_sierra
     sha256 "32d96b54db8cc86fc6c81969a3265424cb2d8ffb9eabf45f6c0d370b5689df60" => :sierra
     sha256 "a01441fd221fb814ef647fadf1ebe2f7cc705bacb3d89845b2c4c02c24caf478" => :el_capitan
