5c994c0d907b8f4fadf0cb938f1dfa1a083e7a98
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 01:13:33 +0000

    man2html: update 3.0.1 bottle.

--- Formula/man2html.rb
+++ Formula/man2html.rb
@@ -7,6 +7,7 @@ class Man2html < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5096761bebb1f08c8eb8bac3e99b35884116086b7e39bf3d3daad3340645ff75" => :mojave
     sha256 "9fe2dcdd8c5f344106dfe57db3e70bec51f78594fb2c968f5561506d1bd7dbbe" => :high_sierra
     sha256 "ced5194219735226831e611db4247de1affdec0c2e53e813d5c1a7c5d3bce928" => :sierra
     sha256 "37bfcf3cab42938fff23a66429872e935b59cf769caf238928bd4acc6544d8d4" => :el_capitan
