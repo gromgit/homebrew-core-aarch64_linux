6a9866645d712570c0b49b65acdf72e9f89db96a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 12:05:09 +0000

    liblinear: update 2.20 bottle.

--- Formula/liblinear.rb
+++ Formula/liblinear.rb
@@ -7,6 +7,7 @@ class Liblinear < Formula
 
   bottle do
     cellar :any
+    sha256 "75e0289a341887b4db477a08c25eee23e9b07d00811c05c0a5922992da13f985" => :mojave
     sha256 "b95e7df876deaac16211dfa14ddc291925372fe2021629ac17827e7ec90c658b" => :high_sierra
     sha256 "f9caf0fc0bfd566faf7f2076e2a2d903d9ec3fad350b8e19307d13e3783d8f91" => :sierra
     sha256 "ea9543b48babe41506ef52ce61aee5659a50ad61e75ac21eb51832e1d7c8f306" => :el_capitan
