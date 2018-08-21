9e6d90273af0b1a4699c86138857ff46c1ac2539
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 09:41:22 +0000

    ffe: update 0.3.8 bottle.

--- Formula/ffe.rb
+++ Formula/ffe.rb
@@ -6,6 +6,7 @@ class Ffe < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1d9c309dcdcda9aeecf1818242e759ec97482abb6e3e093991cd7671fc80f5c1" => :mojave
     sha256 "42240b29cbf5a1f54881c37d1062d8466f763dcefdef56690830012a585c1c5c" => :high_sierra
     sha256 "a023c41a3bbef25ab1eb38b2e7ff202f93b580242df0b883b39e30e82e966d92" => :sierra
     sha256 "192298bb0e1b0594b64831a943cf4d0e490d17d633b2ea4d3b225fa119705e6a" => :el_capitan
