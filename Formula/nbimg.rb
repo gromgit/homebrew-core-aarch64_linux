e716e32db86843d64b04dbfdd102aa81770aa63e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 02:03:24 +0000

    nbimg: update 1.2.1 bottle.

--- Formula/nbimg.rb
+++ Formula/nbimg.rb
@@ -6,6 +6,7 @@ class Nbimg < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f78297721594cdf2c825c589c193fc29f01bfd2e3bfe6f63c3c788ed2699fcc5" => :mojave
     sha256 "20d4ae1588773f9ccd4ff2181def08297ea1119ca70f39392ef11648cb72270f" => :high_sierra
     sha256 "75fd1505a68d1c499ddcf73e912947910659d9bd127c208cafeb3e8899664fbd" => :sierra
     sha256 "402904e3588fe5a8ae00d7131fe29821880f31a8ec19fb89e70a79f76e067452" => :el_capitan
