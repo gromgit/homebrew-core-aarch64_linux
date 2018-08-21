8bffddf8bdf0921df67bdcae284b082acfd26f9a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 01:58:40 +0000

    mlogger: update 1.2.0 bottle.

--- Formula/mlogger.rb
+++ Formula/mlogger.rb
@@ -6,6 +6,7 @@ class Mlogger < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "003cc065352384eeb31109f19c9be3223b5e94cbe859dc3c55c9b1f4e3bd0cb3" => :mojave
     sha256 "9eec751c684f9043f667bf5e9d793379ca3a9824a05b359ed91af2d7e41d52b7" => :high_sierra
     sha256 "1f7392a3d16a2bf595487a4b35bf5c866fa00c0967629eef46f07cbf6e696ff4" => :sierra
     sha256 "e1f78a9ef569085efcac8c41bd2a70feda85e7fcba5eb7b46a9ee5341cf8cb2d" => :el_capitan
