afeb01954a7d891a6b3a1a3bca17e68fb64f44cf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 00:19:11 +0000

    kakasi: update 2.3.6 bottle.

--- Formula/kakasi.rb
+++ Formula/kakasi.rb
@@ -6,6 +6,7 @@ class Kakasi < Formula
 
   bottle do
     rebuild 1
+    sha256 "01b3ca16f856fce7cd71cc752b083589f8267ecc3b44bd6b5800cd1a53cb7700" => :mojave
     sha256 "4ac657051323e642a248aaa4cdf7bc464374e0901bf0b87f458e89b0c3233f76" => :high_sierra
     sha256 "a50761a65d9b64f65d81b6045e992dbfb99746815433f7fc187b43bb0aa36f85" => :sierra
     sha256 "7fca04e65ce14fa8d18d19e197525063274057a2760e4841d4e8a9b06f4b0fa3" => :el_capitan
