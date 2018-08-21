09d1a18143548ba2312b1bc75461f042613fe150
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 18:23:07 +0000

    libmatio: update 1.5.12_1 bottle.

--- Formula/libmatio.rb
+++ Formula/libmatio.rb
@@ -7,6 +7,7 @@ class Libmatio < Formula
 
   bottle do
     cellar :any
+    sha256 "1d277bdc978bcee72b9aa7d5710730f0229ad644cfe41672def51a92c9c2f1ce" => :mojave
     sha256 "bed9878c176c12564d49675f8d19eef60169a7b572f3582c79265b672f40b248" => :high_sierra
     sha256 "eda178bb23fbcd2bccf768b8734251b3c9682a5a9c3b138a6bae8c84a81af9bf" => :sierra
     sha256 "3970377b4a94623cb5b8cc635558c557a8570027524624adc8dd30756d756316" => :el_capitan
