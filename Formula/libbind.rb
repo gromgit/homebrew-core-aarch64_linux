a2f0d9588197bbabbb6f57bab0aecb52ad3ac4df
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 00:08:26 +0000

    libbind: update 6.0 bottle.

--- Formula/libbind.rb
+++ Formula/libbind.rb
@@ -7,6 +7,7 @@ class Libbind < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "504319b451c5cfa27ffc46ffdae91ba7a75b3fce9d78c9a02c3a1214d377b1b2" => :mojave
     sha256 "4cbde1938720903104389cb24f164f5fc3791953984888d62e0831a695dfeb2d" => :high_sierra
     sha256 "b5a4bc4b247b5cce0c637bb96335c5df27e2210c40cc89e19f9957f5aef1cbdb" => :sierra
     sha256 "d0a71d129904ac0529c6f8e789a41307caaeb0e6d9f33f30f23f4b3dbc61456d" => :el_capitan
