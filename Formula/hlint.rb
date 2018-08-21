ab50d944cd92a092983a2671495ffe6e2a50a3bc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 17:54:59 +0000

    hlint: update 2.1.8 bottle.

--- Formula/hlint.rb
+++ Formula/hlint.rb
@@ -10,6 +10,7 @@ class Hlint < Formula
   head "https://github.com/ndmitchell/hlint.git"
 
   bottle do
+    sha256 "46d326207501872cab48d7f88828abe89dcfd0cf7a61313a1fe4f97db49c4e84" => :mojave
     sha256 "a48d46cdd3aaee9ed2ae2d0decc4f9a4ddfe3526095917eff61c010b7a1b545f" => :high_sierra
     sha256 "ccf6f37b1d44cc063bdc08b03cceaa6589c39266e54d406096661ddc48d8a368" => :sierra
     sha256 "2610f48e027a3a47e510920b5f55f1791009ff4e164b4a021aafd9113cefb3c4" => :el_capitan
