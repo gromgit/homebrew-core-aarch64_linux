060a2bc6d19c38cec0e1c740da010dc27f3b569d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 08:10:23 +0000

    bwfmetaedit: update 1.3.3 bottle.

--- Formula/bwfmetaedit.rb
+++ Formula/bwfmetaedit.rb
@@ -7,6 +7,7 @@ class Bwfmetaedit < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4f8d7dff09666995600734b89ff826cab0a95029b89682600bedc84e47a03adb" => :mojave
     sha256 "85ea1c310484ad3377240e91fccb9945f43f4aee701d51b14e61eb23a32a9bb1" => :high_sierra
     sha256 "79245545f860b2e00d41413bcf6c8f8bb2c6d21e9dc4902e2f69893ee182abee" => :sierra
     sha256 "0c61f8118a0a6ae3eda3c4504ea546504e437b07aed61c51225f75e4a2e700d6" => :el_capitan
