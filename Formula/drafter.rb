9661bdaae9337f0165398207cf49b3467006bdc2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 09:34:29 +0000

    drafter: update 3.2.7 bottle.

--- Formula/drafter.rb
+++ Formula/drafter.rb
@@ -6,6 +6,7 @@ class Drafter < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "886a7b2595eebd07cee223f5d5a53a4748e91719c7f4f3a37c236c8476b3c532" => :mojave
     sha256 "5d350d1a7fb4aa5e9561933f0e9638ee59fd93b0d9168d07e665a806e472d5f1" => :high_sierra
     sha256 "a90e9d4493f252d29d69e186dcc49eb116f6bc506d07769f03864f56d5840f08" => :sierra
     sha256 "dc86d4e8dc44c2dead52e57c3bf6403d691926b9abf274f16b94c9649dd562fd" => :el_capitan
