2f34128e2b4d5ee4015ba2ac07b2c42cd82726f2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 10:51:46 +0000

    netperf: update 2.7.0 bottle.

--- Formula/netperf.rb
+++ Formula/netperf.rb
@@ -8,6 +8,7 @@ class Netperf < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "cdd840b5e300383245d703973fcd238d58b4bd89d2ae3ba6769db297b2ddb1f9" => :mojave
     sha256 "cf086e0d276a572aba8318f7080cedc94b36a7b612cdbb4bcc3ceefef0080c53" => :high_sierra
     sha256 "4d3f648081c84ad697d608b56bcfce3237de7c34c4e4a53d9851628f9d50cd5d" => :sierra
     sha256 "c6e96625b1f83a7f83d3c9b53b8584ab65d73cfd59bc38672588ba82d37ecc1d" => :el_capitan
