a0dcc241a88a7744be1b09c019d2efc09b490fb1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 08:10:22 +0000

    brightness: update 1.2 bottle.

--- Formula/brightness.rb
+++ Formula/brightness.rb
@@ -6,6 +6,7 @@ class Brightness < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "de0ebf57bf951ab4e90a8bb90eeb0ec659a696a7c2f10a5c32d269cceee44dce" => :mojave
     sha256 "08b29843308b1cd3603aba3f2e5e3d2e7dec34dbe62bdb5e506b7bacdcff8df5" => :high_sierra
     sha256 "edd4123953a961e94ce78b076b116c987f668ca73e0a67339e908ead6ded8441" => :sierra
     sha256 "675d9a1b7e39b75d2b569fa4f148fbc2342dbcd4a1b23045763c0103058ecc26" => :el_capitan
