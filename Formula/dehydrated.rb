2a8ecbf1e361dea1b9fbbdb9301baa2b262eb8d1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 09:04:33 +0000

    dehydrated: update 0.6.2 bottle.

--- Formula/dehydrated.rb
+++ Formula/dehydrated.rb
@@ -6,6 +6,7 @@ class Dehydrated < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d21b6d1cd29019957b14e7dcfb73a95efd9dcff917cdf983227b4b1defcc3a80" => :mojave
     sha256 "d15ef3ebfbc9bfc48c16192a885bf5580f0269f05a62e569b18e18049cbe917d" => :high_sierra
     sha256 "d15ef3ebfbc9bfc48c16192a885bf5580f0269f05a62e569b18e18049cbe917d" => :sierra
     sha256 "d15ef3ebfbc9bfc48c16192a885bf5580f0269f05a62e569b18e18049cbe917d" => :el_capitan
