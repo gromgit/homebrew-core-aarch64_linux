71e1471a467a6a8dfe324d3c4acf0e624d7398d5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 20:15:25 +0000

    libvidstab: update 1.1.0 bottle.

--- Formula/libvidstab.rb
+++ Formula/libvidstab.rb
@@ -6,6 +6,7 @@ class Libvidstab < Formula
 
   bottle do
     cellar :any
+    sha256 "fc020f4ca29d8aeaab68a06d7f182cd0e4261ad761fd2cbde64a028f77b28ba5" => :mojave
     sha256 "2d23e6352bda6577251800282d57be5520df2493a01a48d0f5716e1e1e95a465" => :high_sierra
     sha256 "5ff2357b04a9bb854b9a09d3b6974f7c665e032948a95981357bf62b857a4099" => :sierra
     sha256 "e3ed904feb361f98bc78ee3211ab6a13a4913f56e09213fecfb73f305c95dc45" => :el_capitan
