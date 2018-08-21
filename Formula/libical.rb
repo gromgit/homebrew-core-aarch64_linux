701d2957c83a714ffce4e393e9cc89d8179190bc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 18:23:09 +0000

    libical: update 3.0.4 bottle.

--- Formula/libical.rb
+++ Formula/libical.rb
@@ -5,6 +5,7 @@ class Libical < Formula
   sha256 "72b216e10233c3f60cb06062facf41f3b0f70615e5a60b47f9853341a0d5d145"
 
   bottle do
+    sha256 "205026b787a55ff63c648c35f9d1de0236fc699ee7498bc2b09ab3899431b170" => :mojave
     sha256 "edc96595f6d0d8ca5c7c42b6ed8eda3435d5f3a6f4c6673890ecc1ee79243529" => :high_sierra
     sha256 "05033e4e3adb9b269a69061cf8b3b62d21095f4bd7a724a34a6966b7c814f9a9" => :sierra
     sha256 "d313668570afe5b0eb40c5f7f1037d181596b1d172975e5ceaa69cbeaced87b2" => :el_capitan
