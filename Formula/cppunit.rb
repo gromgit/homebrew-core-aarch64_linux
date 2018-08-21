c2f6f6050c7e40c9f667a919a1c08a5d33f836be
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 08:48:21 +0000

    cppunit: update 1.14.0 bottle.

--- Formula/cppunit.rb
+++ Formula/cppunit.rb
@@ -6,6 +6,7 @@ class Cppunit < Formula
 
   bottle do
     cellar :any
+    sha256 "3a62b16c2f36115f58f28277602c2cda352bb8e3dc5d559708db19f22ac5eb9e" => :mojave
     sha256 "2740f149428280c1dd20ac94612eacca944d29dac0838eee4eeb0eef2ae1fdd6" => :high_sierra
     sha256 "3c620068fba4bf15b6138ffc4042ab2111a67201310523104c07c314115520bb" => :sierra
     sha256 "a2d2bf8be8ffb614f0490801e38558681b8b01a9fc7ff4be5f785d3db7f71157" => :el_capitan
