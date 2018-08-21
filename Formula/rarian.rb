fef87f5e5c39d1434916fd79154deccc66911d24
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 12:25:06 +0000

    rarian: update 0.8.1 bottle.

--- Formula/rarian.rb
+++ Formula/rarian.rb
@@ -5,6 +5,7 @@ class Rarian < Formula
   sha256 "aafe886d46e467eb3414e91fa9e42955bd4b618c3e19c42c773026b205a84577"
 
   bottle do
+    sha256 "e727630f28efcdcb1a577f67525992f00a00c25ee1582277e1e91e2fa060187d" => :mojave
     sha256 "815aafc0d05198cd4e3880715a6ad5de21b3bf47ccf25ef4b91aa918848a67ee" => :high_sierra
     sha256 "9266addbd38ed67b7394d05702d2be69d44ccafeb8132ef75470a816614a9f8e" => :sierra
     sha256 "7784dc13b95c0c2f5818bc3657da52f0365bbe9c6ddf8871d81b8638cb89390c" => :el_capitan
