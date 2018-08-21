e0b298505b56c97a4c537a252550e72236647c6e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 17:28:48 +0000

    libagg: update 2.5 bottle.

--- Formula/libagg.rb
+++ Formula/libagg.rb
@@ -7,6 +7,7 @@ class Libagg < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "97e0bd763cda63b61cefba2e46048275dda4d03cdaed251be5ebd0b7369b8e38" => :mojave
     sha256 "de1daeb1b324b1797f46ff6e6799498019de9256b4e09a128cf686e2572f6f60" => :high_sierra
     sha256 "872f49f0fd96ee65dca4bedba3e82c4fcf0e0b0c45de15afc82a9e70e0f0623c" => :sierra
     sha256 "5b9ab7a9ef2f4075bd55561f0fda99c7203a70020288747ebf90cfc1b2ee626b" => :el_capitan
