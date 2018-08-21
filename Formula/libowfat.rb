bcc55a9dbd2225659d842c483e116ee29ba22daa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 00:58:25 +0000

    libowfat: update 0.31 bottle.

--- Formula/libowfat.rb
+++ Formula/libowfat.rb
@@ -7,6 +7,7 @@ class Libowfat < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "86a90bda438ddf8d328a4377ae661911e830b42e4cfdd699d6712845e7dc75b1" => :mojave
     sha256 "43e3968245f33399038ffb25f48618be370cb8242f38ddc36170b76cfd0da3fe" => :high_sierra
     sha256 "4f719fe2a03651ecea7882464e5b8fd1f4f3b1e32a0f75f9e5cd9e66ad32a123" => :sierra
     sha256 "be87e0da446834d6f8f808c434e854ff7c9eb88c3f899fc48a830b36117cac83" => :el_capitan
