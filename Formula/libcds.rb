493ae17c303f38673a020f40c60d06d85e066ed2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 17:28:44 +0000

    libcds: update 2.3.2 bottle.

--- Formula/libcds.rb
+++ Formula/libcds.rb
@@ -6,6 +6,7 @@ class Libcds < Formula
 
   bottle do
     cellar :any
+    sha256 "2471fc57450574f25269e25a5734ee21c504389ed417d5559491a194439f6f98" => :mojave
     sha256 "b1fb46232f36fd9bc2fef02b1118e5398609b3343b3376d5130a5c7633c7c9ae" => :high_sierra
     sha256 "cf6b35c96cdf54f090fb02f519bb00581ae9e4214416dbeabdc89235a5c94e57" => :sierra
     sha256 "5f6db23a55e3bc9ce3358662b1f9122e9eb032f7eedef4b47e6bf9f27ebc052d" => :el_capitan
