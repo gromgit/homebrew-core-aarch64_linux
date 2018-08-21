356deb75b885368154d8373b06b13b7716231652
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 18:53:16 +0000

    libsbol: update 2.3.0.0 bottle.

--- Formula/libsbol.rb
+++ Formula/libsbol.rb
@@ -6,6 +6,7 @@ class Libsbol < Formula
 
   bottle do
     cellar :any
+    sha256 "974ce842996ac65fcb31874389e0659451fe6cabc06a1b3c679e6fff9a2a27a0" => :mojave
     sha256 "ee8e8a563435076bdee91fafe19832750ec91f25d0a32d727cdb505d7c437c3a" => :high_sierra
     sha256 "cfe93d1e977048c7502a5aa1b43514deca305dc66451aafb386f68a304c12492" => :sierra
     sha256 "61efb175a5ae38717e9f939c172e9a249e9138bfa3531e71471d6314c7b61b16" => :el_capitan
