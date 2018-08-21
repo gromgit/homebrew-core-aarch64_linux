7add1d554e5ce13f07fc8ef41309c640c1286a92
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 16:03:22 +0000

    crystal-icr: update 0.5.0_2 bottle.

--- Formula/crystal-icr.rb
+++ Formula/crystal-icr.rb
@@ -7,6 +7,7 @@ class CrystalIcr < Formula
 
   bottle do
     rebuild 1
+    sha256 "0c4ce6dfda9ab46076fb9c4ad8e0f934559e2ac7ba87579f900ced4d6977a0cb" => :mojave
     sha256 "f57ef8c0e048007c3d387fd21ad982b6ada957e24b77effa4aea18fe197d59f3" => :high_sierra
     sha256 "cd5fd88fd935f1481a562e62058c6c8570f56894b757eca01c2dc3d6d171b88e" => :sierra
     sha256 "bb812144655843a6e3a772467ea7178a9e78ce3352feda83a0107615019eda1f" => :el_capitan
