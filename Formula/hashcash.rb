987aeb6df5b87eeff679bcda0dc180d053038be0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 08:17:26 +0000

    hashcash: update 1.22 bottle.

--- Formula/hashcash.rb
+++ Formula/hashcash.rb
@@ -6,6 +6,7 @@ class Hashcash < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "775184aba3e61dcabed2020c4f2bdda029561badd41aae6d75c56b7bb564a7a3" => :mojave
     sha256 "acb58644b209a262a1f8aea8c4f40e078f4e76742d0339c4e240f92bdd2fb290" => :high_sierra
     sha256 "af78a79c6b0dbf5267781eb209cc3115f43dcdfd7a389c2740262bbab3be3c20" => :sierra
     sha256 "b9ab067b3001c71dc5cfa3085bfcd204cb4837fd6c87f5ce722bd77b8a629850" => :el_capitan
