79c52b599bed0ae93a90f7c304599f6c277812a9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 11:07:58 +0000

    httpry: update 0.1.8 bottle.

--- Formula/httpry.rb
+++ Formula/httpry.rb
@@ -8,6 +8,7 @@ class Httpry < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "32bdf2c6b873fc531455da9f4658746c650203a017c8b367172efde8aa93f9ba" => :mojave
     sha256 "349ba4f39066cb02c151ab0f274f6bb9f4ee2cf558abdb2c5a3ecf0e563874fc" => :high_sierra
     sha256 "71014794d2a136fea229dd19d6fe7dc136037c074a817d70bd7b13713653f19f" => :sierra
     sha256 "56d6a77e429bf9dde3d5e5edb9959fc7ed913430236cf628e0aec6445c07c85a" => :el_capitan
