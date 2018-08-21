3be072feb7231f23e75276b59cf60454e6170743
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 01:13:35 +0000

    ltl2ba: update 1.2b1 bottle.

--- Formula/ltl2ba.rb
+++ Formula/ltl2ba.rb
@@ -7,6 +7,7 @@ class Ltl2ba < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "754dd15206872409b2af919b68dfc65a30d6343ba31aead8a8db16416af9f916" => :mojave
     sha256 "9d1e6e9bca1073e604a7aa1e693c2df5a0636c8e6a5b82f85db491929520ae1b" => :high_sierra
     sha256 "151f207ca2627b0916371b49918f3b97174f5c2bab56abab0eb11d17d604037d" => :sierra
     sha256 "45916a60bb32849e1bc3709d019dcf6ac9d140d92e6f6b65bb5ca05de5c63e3b" => :el_capitan
