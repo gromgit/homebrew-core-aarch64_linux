40d718db024d7f95c230d02f898d2b29d73b7d78
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 09:34:30 +0000

    dnsmap: update 0.30 bottle.

--- Formula/dnsmap.rb
+++ Formula/dnsmap.rb
@@ -6,6 +6,7 @@ class Dnsmap < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4172919faa189face592aa272a8a45b62c05dc3056f75ed26e505d1e35118b5c" => :mojave
     sha256 "3c45ae4ec1149775df005f3438c0a24971291094bc47814a8a60e4ca67dbce79" => :high_sierra
     sha256 "2015e3fdee32fcdbc0501f522ba720280e8cb07a75f8166fd62a9f876ea12124" => :sierra
     sha256 "57d5f7ea4a8a73b454bbc60e38fdb9a55e07c2d97f0b5acf38db0408e83b197b" => :el_capitan
