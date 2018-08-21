8fed74384720cfbd6d002a6d2dab1fdc4c2b77bc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 23:16:34 +0000

    transmission: update 2.94 bottle.

--- Formula/transmission.rb
+++ Formula/transmission.rb
@@ -5,6 +5,7 @@ class Transmission < Formula
   sha256 "35442cc849f91f8df982c3d0d479d650c6ca19310a994eccdaa79a4af3916b7d"
 
   bottle do
+    sha256 "69526246bba21ede8f14975da19ca64a185a5e58af78432bf013aacf7711297d" => :mojave
     sha256 "f9d035ba6b2e2fe96080f368b9b396c05a643680bb1e6714b6eb4d2161ca63e0" => :high_sierra
     sha256 "cf4f5ccc5069e03addf77882152e6358f2aeb514ffab78d7ed20e54b22d42936" => :sierra
     sha256 "e31b76dc003d69cfeb5f67478a95dd8e824e26606b585270b15bfffe79cae59c" => :el_capitan
