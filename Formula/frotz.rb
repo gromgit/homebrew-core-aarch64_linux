ffa25a0c42afb8a396a18b9ff57a0661469b2dde
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 10:15:35 +0000

    frotz: update 2.44 bottle.

--- Formula/frotz.rb
+++ Formula/frotz.rb
@@ -6,6 +6,7 @@ class Frotz < Formula
   head "https://github.com/DavidGriffith/frotz.git"
 
   bottle do
+    sha256 "bd7d173c661883a5ad370c4f2437dfcda10e2852988e4a1b0b681ee19335ed97" => :mojave
     sha256 "f1be9afe41e33396475d99511760690d3a46f9362fb2229e42ba48146d92f8f0" => :high_sierra
     sha256 "e190264a540f03ec98b7be45d1edfc73c0bd4946b4d2c4aacdf98521354e4ca0" => :sierra
     sha256 "6506ec53859a859eb71ab33511a11e9c957e0749cb25efa06d63d6c80d000b4a" => :el_capitan
