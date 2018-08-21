def36415352714905611424a07c3a1ca49515df0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 01:13:32 +0000

    mailcheck: update 1.91.2 bottle.

--- Formula/mailcheck.rb
+++ Formula/mailcheck.rb
@@ -6,6 +6,7 @@ class Mailcheck < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7ea23945f9750c34d71ff05c5f41c0f5352e3eecaf1c7cf485d4f51096b9dd4e" => :mojave
     sha256 "c630704fee3dea86402e7486295a13601077bd991e45f23d3ac841c95a9c4474" => :high_sierra
     sha256 "8d33e3b08eef4dfaa7fa3d2c4e5f4a697cd2e5eb950c963f1f0845c0651da5ea" => :sierra
     sha256 "b7c134dc23431dfaa3f402b859b7154cab5e176711363bd884dc82ce896d7c7a" => :el_capitan
