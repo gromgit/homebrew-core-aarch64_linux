655846c24dcd69ac56b0ff5c869bf7bb27b7615a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 00:32:04 +0000

    liblockfile: update 1.14 bottle.

--- Formula/liblockfile.rb
+++ Formula/liblockfile.rb
@@ -6,6 +6,7 @@ class Liblockfile < Formula
   sha256 "ab40d4a3e8cbc204f7e87fea637a4e4ddf9a1149aaa0a723a4267febd0b1d060"
 
   bottle do
+    sha256 "26b5675b6797bb7d652bedd7795bc8b141e4bc91f21fdd0e143f24cc2f76ef0d" => :mojave
     sha256 "49db28fa47ee7012be39fbb8ab03948c050a95525808aa27ad2f8521ff8fadc6" => :high_sierra
     sha256 "45b9f5fee9e1a0efa2439027cd72d5bc6fede1ec9391a46d0a9eb024e675b31c" => :sierra
     sha256 "f6ca20b97a651114986b485e950859e733a08384bd9ea08ef12eb8cd29f2e697" => :el_capitan
