9d4454c77618bffc147a256a3dd92b4333988660
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 11:35:26 +0000

    perltidy: update 20180220 bottle.

--- Formula/perltidy.rb
+++ Formula/perltidy.rb
@@ -6,6 +6,7 @@ class Perltidy < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3eea0c9c4f72dfe772301c153a0aaed43105020086cda3721b785389bd3515ce" => :mojave
     sha256 "74062f9591fa421b637127521c7aa79b140d7741042671a1f21d2148cb50c3f0" => :high_sierra
     sha256 "62a1de49e77f4214b8f689c74f0b5de888e6df0d2e51741d2e5650c2df09ed02" => :sierra
     sha256 "ed1fbd3fcb92487349a6d5431a32f1c9824ce63ecce9e7d2d9371636da42a3a6" => :el_capitan
