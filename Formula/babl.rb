6d6352c94be5b841800662ed7113dca3e3d9e610
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 22:25:31 +0000

    babl: update 0.1.56 bottle.

--- Formula/babl.rb
+++ Formula/babl.rb
@@ -5,6 +5,7 @@ class Babl < Formula
   sha256 "8ad26ca717ec3c74e261f454dd6bb316333a39fd1f87db4ac44706a860dc4d28"
 
   bottle do
+    sha256 "d4919702e5918f9f47b76cf9dbff294efbd26c909eea3084e0ca6991f212738e" => :mojave
     sha256 "be17bcd75c569fe2b90f81c2b7dd8e051c07ead256eb8665c739b4abd1b86b40" => :high_sierra
     sha256 "225d7ef87fe7dcfe9ba9134531b9d5420afc9354bf8fe7bde11bb7bbf4ecf2f0" => :sierra
     sha256 "6aadd8934b96132fd208f0526a0caacb4df6e7c13f3507ea8078a070d5914ea6" => :el_capitan
