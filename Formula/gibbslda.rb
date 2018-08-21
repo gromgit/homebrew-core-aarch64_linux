992a71bd9fb98e7cc6134f10ecb8d7b9b1a67fdb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 10:21:14 +0000

    gibbslda: update 0.2 bottle.

--- Formula/gibbslda.rb
+++ Formula/gibbslda.rb
@@ -6,6 +6,7 @@ class Gibbslda < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2d8cab4cd368d2c12c301dae37449d9b5ce6e625b39bfa7f96d542e6390c6848" => :mojave
     sha256 "a92cdb534bb1061948417a9840addb3bda01fcbdca63ca290b34e818bd4e695c" => :high_sierra
     sha256 "9244041821944e45946fcf6a491ee1579293b4c154c7b9921b38fd6159567552" => :sierra
     sha256 "c8a95c74f3c9e967506fb386a1343459ecae8362cbf91362a7955ba017bea5fc" => :el_capitan
