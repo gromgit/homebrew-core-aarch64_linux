543e0b68c83cbc5d172e0b25083b9096ffed97f6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 08:17:18 +0000

    hoedown: update 3.0.7 bottle.

--- Formula/hoedown.rb
+++ Formula/hoedown.rb
@@ -6,6 +6,7 @@ class Hoedown < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4028b7bb88b6da75f735c58f3497d354dda4bc7ce33288a0ae71932878991c5b" => :mojave
     sha256 "1be6101d978f2df1749712dd39d3fc8b9c7cc014c2402eab5060e8656f6b22cf" => :high_sierra
     sha256 "f940a418b3ca712a91e8b782d61618a2b1cf2c662a98f636e4df1318fbb9f508" => :sierra
     sha256 "7076f6f7c091919a3619a5a5655270d79dab42fdb6d7dfdc3f1324318ca4ec6d" => :el_capitan
