9218976cc30d3402c0e4d82caeac21b1c14aff68
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 11:54:02 +0000

    primer3: update 2.4.0 bottle.

--- Formula/primer3.rb
+++ Formula/primer3.rb
@@ -6,6 +6,7 @@ class Primer3 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "42d8c134f8dde43bc127a0f5f66eda246de195604b952ed9b8ac6b3fa8aba373" => :mojave
     sha256 "f72fac01bb380b5ea55b41249b2d6bc2f799e9cb7cef55fae0a1f92e1de7ba64" => :high_sierra
     sha256 "0337aa96c5d5f25caa15177236c5f5d269adaaad01cb63a77c933eb01f7a6ed0" => :sierra
     sha256 "45ca3618888becc12b4d6be0ab9957ba5c8fdf2e818f74dc5312900c641b06c9" => :el_capitan
