5899c8e366f76082dc54dc6280ad2f7003d58a3c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 09:04:35 +0000

    di: update 4.47 bottle.

--- Formula/di.rb
+++ Formula/di.rb
@@ -6,6 +6,7 @@ class Di < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "fff63e3d3c7b90005d39cb7ff0ee142758a28828f28e5ebeaca2c32bcc0c59f6" => :mojave
     sha256 "ade98ec67db4be6998cf0cf62a99d58cb67e357eac09c1b9590be4980050f742" => :high_sierra
     sha256 "b41b06335b72784f20a71e8932f58c751ad15496508a7362953b51c210a435ad" => :sierra
     sha256 "9f684e70615dde634dd78feaa4b32e03b203301c4ef888b12816182416e2817f" => :el_capitan
