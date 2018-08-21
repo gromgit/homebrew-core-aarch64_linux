125c34ef851abdcb5b737f868d2897b246e78eef
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 00:32:02 +0000

    libgadu: update 1.12.2 bottle.

--- Formula/libgadu.rb
+++ Formula/libgadu.rb
@@ -6,6 +6,7 @@ class Libgadu < Formula
 
   bottle do
     cellar :any
+    sha256 "394b7c3b78e1aa4f7960d7ffc62cefe91069a0e50b7442b62f68d2e68f5d01ad" => :mojave
     sha256 "65f828f98715efbb7bb351d47e11df0fd0279b8c060233138721c119abf0879f" => :high_sierra
     sha256 "4cf4bb4fa157bff6ce4e1fa58a79c372df6b0a00c5e5fd621f6396b3d55451e6" => :sierra
     sha256 "1feb9c3c574632f9324fdfc8bc5ed49f2817e7a58ae280e44b0ae8735e89caca" => :el_capitan
