12cc3383392b352ab40eb2a617845b5c6e5cfc75
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 08:30:52 +0000

    cdpr: update 2.4 bottle.

--- Formula/cdpr.rb
+++ Formula/cdpr.rb
@@ -6,6 +6,7 @@ class Cdpr < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ae75b31d4fb195d0735784d7fb86924821ad07dfc5c5b4ff91597f6e0ceb5fba" => :mojave
     sha256 "ce836a4189c94a1441cb417f36699fca01e3cf30b69bcc5a3ec8307c51d0f66e" => :high_sierra
     sha256 "c6603372329fd2dc0c60266b3f3eb6c9f7cc5c0ce7f351b05977ab39a18cde7c" => :sierra
     sha256 "0bdc868c9b11510e2d9e6551dee970c20406215153906d8bc42790d8510ac429" => :el_capitan
