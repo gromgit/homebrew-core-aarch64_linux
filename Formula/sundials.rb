77b805ab9b87df9376f0dc6ef821f79b1f2a7263
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 16:30:12 +0000

    sundials: update 3.1.2 bottle.

--- Formula/sundials.rb
+++ Formula/sundials.rb
@@ -6,6 +6,7 @@ class Sundials < Formula
 
   bottle do
     cellar :any
+    sha256 "395df599c4b62d2db1cc65c1823b0711f7d4171f2bc41815354b9ec6a476409f" => :mojave
     sha256 "85d26a0fe45cc223074d986abfcdb70c06363a6cdbf5df8bff1f4f8a2ff7103d" => :high_sierra
     sha256 "9b679f316ad602b3b5057bb39a81b398f65bdae5fd7f80cb8d8ec3cba08cf7fa" => :sierra
     sha256 "2213f3ae580feabe0874cf229f39e9d9434cbaefe960f88616496a66a11512be" => :el_capitan
