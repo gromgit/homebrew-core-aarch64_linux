efed8926ace7ea838e8d624bf4316c4795a81cb8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 00:32:07 +0000

    libimagequant: update 2.12.1 bottle.

--- Formula/libimagequant.rb
+++ Formula/libimagequant.rb
@@ -6,6 +6,7 @@ class Libimagequant < Formula
 
   bottle do
     cellar :any
+    sha256 "f7d95445314ffdca4ec26f8386d8401357eadc0e171478d3439ddb356b4b57c0" => :mojave
     sha256 "714cab630af0e9d0e19ad0a30a1e07b4dfce3988f9c048e3b634cea85c018ec6" => :high_sierra
     sha256 "41aa19e02e531883c60360556f94d404fb6be00bcb8b4c155368ec34c190ad10" => :sierra
     sha256 "908ec28bdf4879c7527053bbec4b635e0cbb4f7c635251674e8f1723b4e16cd6" => :el_capitan
