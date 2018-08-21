d51fe162954fe60f8cb7e2295462eb74c59a4391
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 08:57:36 +0000

    csmith: update 2.3.0 bottle.

--- Formula/csmith.rb
+++ Formula/csmith.rb
@@ -7,6 +7,7 @@ class Csmith < Formula
 
   bottle do
     cellar :any
+    sha256 "7c3759ccefa73b295acd5e7e631c40594f6983e26e903b54a88a9e0dfdfcaa96" => :mojave
     sha256 "e8e818a9898b4145c5622810958fa8616f8b57156f09aeaf3045873210f0856a" => :high_sierra
     sha256 "2e78da57153124cb3feca12955d0bbadbc4e90dbff6c34a08532aea55c75ba8e" => :sierra
     sha256 "472992fd577ec20b025397c840823abf8f88d719e7d86bba427446a38cc5584d" => :el_capitan
