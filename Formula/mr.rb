fc2089df1fe811d204dc5f00c1629a435c6c4516
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 02:03:20 +0000

    mr: update 1.20180726 bottle.

--- Formula/mr.rb
+++ Formula/mr.rb
@@ -7,6 +7,7 @@ class Mr < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "73c8b9b421ea776366f9ded68d90c6c3b75b50401172b5c5248556f6f7f47d6e" => :mojave
     sha256 "a41bcee5b050ec9f98cf5960a457421528b05773867d8c8dbb8eb32716e09fd5" => :high_sierra
     sha256 "bcac4176692f69d47a83cd961cee92e096f6b35f19cb7206973f77b15a1ba71c" => :sierra
     sha256 "75fd9c6fbf6dcf833243e4dc9baf0afe81c422e55d3e251f5cfe040b8bc6a254" => :el_capitan
