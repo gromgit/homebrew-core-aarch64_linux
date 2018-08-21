70173b841548da6679f25c7bc57c33553b9d845d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 10:51:48 +0000

    netcat6: update 1.0 bottle.

--- Formula/netcat6.rb
+++ Formula/netcat6.rb
@@ -6,6 +6,7 @@ class Netcat6 < Formula
   sha256 "db7462839dd135ff1215911157b666df8512df6f7343a075b2f9a2ef46fe5412"
 
   bottle do
+    sha256 "d0f6d99434ded31aebdc49dfa8e918112e816421247ada8dc23ba89614fc8278" => :mojave
     sha256 "42c8ecdb7495f55311d806e7dd3695c2dc664c536d5c6c0f7f0a84f150299a44" => :high_sierra
     sha256 "134b64d2bca03d774a1375c9898504b5aba30de50dff543ca37033b7334fcaa5" => :sierra
     sha256 "75fd93f36d745618b9c87a1f635a100db7fc2c3d659ecf39e77a282bf0efdb1b" => :el_capitan
