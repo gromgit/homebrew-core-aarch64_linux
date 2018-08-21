ede015c6ed3d33305403b140e06c227d50399043
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 08:17:24 +0000

    clib: update 1.8.1 bottle.

--- Formula/clib.rb
+++ Formula/clib.rb
@@ -7,6 +7,7 @@ class Clib < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e7fa3d823b125697f59f95c5671ddc591b91223e45cc59555ee62de7e5df752d" => :mojave
     sha256 "0dee597cc5870323ea38804337d3c3bd7b681a53303031399d31c12e1c1f1f55" => :high_sierra
     sha256 "054b42c0cb78315e454759303b7f25945a9ed18ee76f32a14d58a6911861f37d" => :sierra
     sha256 "64a97a9de695bc96f596d5a626428b8758ae0365b67c161bcd9519ccdf7dcfc4" => :el_capitan
