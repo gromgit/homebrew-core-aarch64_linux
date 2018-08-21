09b3753cc63bdd065ce1efa04fe3b2223721ec90
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 11:07:56 +0000

    id3ed: update 1.10.4 bottle.

--- Formula/id3ed.rb
+++ Formula/id3ed.rb
@@ -6,6 +6,7 @@ class Id3ed < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2079b26fd26395f4eb016c61afafa007045d7b87b5030b05650705959c3bd87a" => :mojave
     sha256 "c31762b13640d1e9713ea26df41d5e9cb675a8d3565cd84b70efc526663ddfb8" => :high_sierra
     sha256 "e930552e37599e7926efebaf0d893f888576a26bddef6a91e356cf1b5de15b9e" => :sierra
     sha256 "6448c8e19c8e0874ed5141193c7db06c443ac6c33ab2f6bbe8811098b063c0d1" => :el_capitan
