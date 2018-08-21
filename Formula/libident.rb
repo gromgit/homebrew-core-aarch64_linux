beb55cbe78ce80fbd0f209010b3f241f74a47ef3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 00:32:09 +0000

    libident: update 0.32 bottle.

--- Formula/libident.rb
+++ Formula/libident.rb
@@ -7,6 +7,7 @@ class Libident < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "5afab4111e356d2c88632a89a6ffcf82b96530737b9c5c38ba0622900322ab79" => :mojave
     sha256 "f6bc989df22a80f3b0f8c6a2d458b5a00d9a4d48247cb7892877bd287e804a50" => :high_sierra
     sha256 "4fb8a991f9f83d499b32a814b2d68465327b7b77c0108764e58e4296a968100f" => :sierra
     sha256 "6236d3b4ee424795859cc64da30997ff67f7ac5bd42702e8eabe10f99339ca48" => :el_capitan
