459e8b6f36bc794437019900258560f6189c9db9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 22:25:31 +0000

    shfmt: update 2.5.1 bottle.

--- Formula/shfmt.rb
+++ Formula/shfmt.rb
@@ -7,6 +7,7 @@ class Shfmt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "cee542502f7455a41011ffcaaa050ecfd53fa65c9cfa04e86565525383cf8600" => :mojave
     sha256 "63d6071aa8743fb1b596adbe070bacc309f041ac8fc63a25cc9afdb98ac77fee" => :high_sierra
     sha256 "2b6be3da62abec1a9809065c463582338bf30d836404fc4b64a71eaeb7a6b3f7" => :sierra
     sha256 "13b18dce374485d0608859d77c3c7bc7232b13a39c51cdd0c54d05cb9488c0a1" => :el_capitan
