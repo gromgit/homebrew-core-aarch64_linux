076585d278ceb7516792a460b430ed6b136274ae
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 00:08:25 +0000

    lft: update 3.79 bottle.

--- Formula/lft.rb
+++ Formula/lft.rb
@@ -6,6 +6,7 @@ class Lft < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "68dfe5d46b55f399681245146a4ceee9b47c54b900d914e11c7897fb47c9f421" => :mojave
     sha256 "42f8215a6ef6acc8cb0ac860c8f671bacfb844c4ba8fa6ce0e305a7367d4ef1a" => :high_sierra
     sha256 "2d0f6de7ba8bc85736396ff20e8fa2a5d53fedc22a17d71a7858fcdb39afd0b0" => :sierra
     sha256 "664ce1a522f401dbff33f8e77e1443514e05c98924e1d44cbaee8403ecb3c2e0" => :el_capitan
