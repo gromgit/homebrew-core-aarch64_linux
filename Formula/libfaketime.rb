faaf6a0178b203a699120895c21df3443b9384f3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 17:26:58 +0000

    libfaketime: update 0.9.7_1 bottle.

--- Formula/libfaketime.rb
+++ Formula/libfaketime.rb
@@ -8,6 +8,7 @@ class Libfaketime < Formula
 
   bottle do
     rebuild 1
+    sha256 "74f8e3fb7f54bfdb8b2962dfde0428819b03772ee9138d39d30005c9c5c974d2" => :mojave
     sha256 "af523cc7dad3a5594fe1ff4fd8af3d8d21be0dff3c65bb309292d79233080795" => :high_sierra
     sha256 "e341fabbbfd9b0b9f2750b69c4926adf8116344b1a35fd655bd5e81b1c67d950" => :sierra
   end
