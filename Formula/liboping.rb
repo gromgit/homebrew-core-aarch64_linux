2dc4cf9d8ae12b592791d4350efbdd5c07ce4be8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 00:58:27 +0000

    liboping: update 1.10.0 bottle.

--- Formula/liboping.rb
+++ Formula/liboping.rb
@@ -5,6 +5,7 @@ class Liboping < Formula
   sha256 "eb38aa93f93e8ab282d97e2582fbaea88b3f889a08cbc9dbf20059c3779d5cd8"
 
   bottle do
+    sha256 "7b0258598b329b83ce9fd0cf18be77aa027226c8391a013cb3699faeeb1fd71f" => :mojave
     sha256 "accffc91ab24ccba1214727abadb59c497f403e3bcad1dfe8ff0377d32e05ebc" => :high_sierra
     sha256 "42b80e23afe4fb4f296d039b0bdd4ccd0da21937514fdd04a90bc01d39da7aec" => :sierra
     sha256 "de0bb72a0752469b262db3a24a41c84746930858462cd08993c057caadd46264" => :el_capitan
