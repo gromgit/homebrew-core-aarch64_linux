d9bfd2d1363595cf75cca118f4c6661ed17812f9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 16:30:11 +0000

    qrupdate: update 1.1.2_8 bottle.

--- Formula/qrupdate.rb
+++ Formula/qrupdate.rb
@@ -7,6 +7,7 @@ class Qrupdate < Formula
 
   bottle do
     cellar :any
+    sha256 "3e9beaab01eb493f63fd2e472397423bdb6eb4da9ad21d77142450823b971ffa" => :mojave
     sha256 "b5f9fcfd7ddaca8e64b8b200bd413588a7fe608b31b7d1b9a23be53a2084bd3a" => :high_sierra
     sha256 "f1213f270e9c6a84e8c1707d3b956e7bb2a6670f53bbf405cdba4d8da2393846" => :sierra
     sha256 "c84f04635d00f139bcc5114b36395e7347c675ccb10fbf88a1779de5c6816c3a" => :el_capitan
