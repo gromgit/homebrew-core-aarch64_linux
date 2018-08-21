081c7205a082f05428846e5d4b03dbb455956631
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 08:57:33 +0000

    ctemplate: update 2.3 bottle.

--- Formula/ctemplate.rb
+++ Formula/ctemplate.rb
@@ -7,6 +7,7 @@ class Ctemplate < Formula
 
   bottle do
     cellar :any
+    sha256 "0f0dd5a902d31e3ff1e6868b425e4aeb9ec9d094337fea48f85cad7b7e0d747f" => :mojave
     sha256 "6cddb070bbc9b662305513c142119cb83fac09d1c1579b925f6f2bbddb0773fe" => :high_sierra
     sha256 "f2bbb674557034e487ad218f871145c9f27b02b908e10b2fd15f457c960191e0" => :sierra
     sha256 "3cb0314574a76b022e365c293f50eff604def9a9171b45c4e3af1c56f5310927" => :el_capitan
