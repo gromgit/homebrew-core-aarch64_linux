0afca4dc4f04f168eb2582f4ef8cd7c1e9425461
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 09:39:27 +0000

    ecasound: update 2.9.1 bottle.

--- Formula/ecasound.rb
+++ Formula/ecasound.rb
@@ -5,6 +5,7 @@ class Ecasound < Formula
   sha256 "39fce8becd84d80620fa3de31fb5223b2b7d4648d36c9c337d3739c2fad0dcf3"
 
   bottle do
+    sha256 "f948edb013b464b984a44dee9a0bd6e8fc9d6e5f1deba13c8d57e668d0b1b92f" => :mojave
     sha256 "cc433f924ea12f9450d064be622a290cab97bdad35e83ed98658335ae61f47c0" => :high_sierra
     sha256 "789fd275a49c7017ee25d1f5e00a802b3c5f2baa5d54db3753c566e04cd335c2" => :sierra
     sha256 "a0bfadb79c1b81c2764290a4cc6e2eae09bae34e4ec54f06e6d4d669bceed331" => :el_capitan
