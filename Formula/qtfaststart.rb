b2deb23f572eb6fa603223cf5c399eaac50f837c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 11:53:56 +0000

    qtfaststart: update 12.3 bottle.

--- Formula/qtfaststart.rb
+++ Formula/qtfaststart.rb
@@ -6,6 +6,7 @@ class Qtfaststart < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "72a7b46a652dc99b3c24df9a868008927e795f438ef2e68fb3d7456abbc3724a" => :mojave
     sha256 "4534f204dcc950ad18c3a141119fcd8d9a9623645092ae3f003b08dd94851dcf" => :high_sierra
     sha256 "a11b3b623a2682567830cb05810ed4445037f5b43a52f2378a10291aad70a2c6" => :sierra
     sha256 "00ed1702b08f3fe7d0660aab963f15baab6a5bac8db02048fe5d0b4d80b4abe0" => :el_capitan
