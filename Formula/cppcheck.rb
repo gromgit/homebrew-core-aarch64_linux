32d5ec24be0c4e72d7669e67c1641d51767e5ca7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 23:16:32 +0000

    cppcheck: update 1.84 bottle.

--- Formula/cppcheck.rb
+++ Formula/cppcheck.rb
@@ -6,6 +6,7 @@ class Cppcheck < Formula
   head "https://github.com/danmar/cppcheck.git"
 
   bottle do
+    sha256 "f1a746cd9b65d7c4b69e14bf58e623caefc35d28c577306265a688cc2f2605c6" => :mojave
     sha256 "94fdbee6373e6a4f3102110b522be26e7b2b4a285f91115ba3a82a0d85643124" => :high_sierra
     sha256 "a6ced4322c5114c84999697cd1e07ebd50905a9ca9477398e46a359a6e0b7b8f" => :sierra
     sha256 "55c59fc078a4c2311ed7d4c1f048ae1114dbcf30956249441979e6bc91e7762b" => :el_capitan
