cf2188c6bd50a897c78d4658a3ebba6c099ac728
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 08:57:32 +0000

    cuba: update 4.2 bottle.

--- Formula/cuba.rb
+++ Formula/cuba.rb
@@ -6,6 +6,7 @@ class Cuba < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4520616f7170177dd546ec6d71d3c52e592c93e8a7e24b9c2e252382011a7b4d" => :mojave
     sha256 "a607f8cbcfa954ac20a407fce07dc6cc65bfaab6b06079cc5d1eb85400532e74" => :high_sierra
     sha256 "b3317c5c1d2f902d60aaa175f3e35d906f2ac6ab179b87de09e0f9110125b4ff" => :sierra
     sha256 "6f1bf18403892cea4d5d692256e31dd11d170ecadaf996f960c454084e51b243" => :el_capitan
