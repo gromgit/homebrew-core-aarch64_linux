32fa8dd6999c4a18c3053477c540d98503c6fef9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 17:28:43 +0000

    libbdplus: update 0.1.2 bottle.

--- Formula/libbdplus.rb
+++ Formula/libbdplus.rb
@@ -7,6 +7,7 @@ class Libbdplus < Formula
 
   bottle do
     cellar :any
+    sha256 "d8f4b53ec0ea12bbc02b2962e94dfe5df98ef55005f10209f4fd40213a80f601" => :mojave
     sha256 "478e405b0f9687edcea3f651f4ec922a1bd12c12476c3aa14d1a35d0bb0362bb" => :high_sierra
     sha256 "8205ed5218393f7aa7f2035f089e91a417f13d73f4b7e3d46f3afc5073ce7e37" => :sierra
     sha256 "7136cdf433318efb9691d43d078eb20e9647f2ae4999b42cf791736d95047a81" => :el_capitan
