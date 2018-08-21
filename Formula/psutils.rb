0126c927fce7f21add3c1492ce8450660a6d7d13
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 11:53:57 +0000

    psutils: update p17 bottle.

--- Formula/psutils.rb
+++ Formula/psutils.rb
@@ -8,6 +8,7 @@ class Psutils < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "d2ba48c88116be774d989d71c791ef97f8eac3723e63a0924e08ea48f4b3ab39" => :mojave
     sha256 "d9408c8f70db105a621195339f357107d6f234c75be581b1ca8365d0e82e62c2" => :high_sierra
     sha256 "1319662888a509ceee3993bf17e7fb2f9dfaea5ce25c983c0bcda13283b5d612" => :sierra
     sha256 "def5b3fc8cef9b4c532cc26ae216d1c6b0dae54da5a39acbdb818d53a04bf697" => :el_capitan
