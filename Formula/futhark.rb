1bf1e7dcb86278cc11fa59d9268420b07d8f20e9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 17:55:01 +0000

    futhark: update 0.6.2 bottle.

--- Formula/futhark.rb
+++ Formula/futhark.rb
@@ -11,6 +11,7 @@ class Futhark < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "49441eb8baa1a7ed4501cbcdd93e1fdd0c1c549b5e82b8b6e765f48f71af2ed2" => :mojave
     sha256 "10fea6ba4847bfca7c9ee78c4791884bfc24643ae6ae39091cddd9d7ccbaf1f9" => :high_sierra
     sha256 "fc6a3a50a77c96d1b119847f3fda9335c5d5002d6d3200663b9644aab4b9745d" => :sierra
     sha256 "119b615bc635798260530da1670abce212799b48f849cfba9869e0df85bebec0" => :el_capitan
