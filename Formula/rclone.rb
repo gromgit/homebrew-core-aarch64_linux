5ffba742307f7c24e85c0789f3d63e46e03190dd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 16:30:12 +0000

    rclone: update 1.42 bottle.

--- Formula/rclone.rb
+++ Formula/rclone.rb
@@ -7,6 +7,7 @@ class Rclone < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d7457142ea5a4e25b32548ee40f5fc472dc5e46c413f15a0a05ac861af7a8eab" => :mojave
     sha256 "c5a24cebbff63e768439a63db1e824e2d68b3d1f1cb4b8721bfeece777756aac" => :high_sierra
     sha256 "f5bd438d038265787effce38849c9b9d2b390516a9aac38b3130e2f7dd5feafb" => :sierra
     sha256 "098a51e85a1bcaeac18295e55678f1d5345fbbbb44875b6320659f540650eb97" => :el_capitan
