22551219bd8e7d287424653abf0ef924a1462b35
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 17:26:54 +0000

    libdmtx: update 0.7.5 bottle.

--- Formula/libdmtx.rb
+++ Formula/libdmtx.rb
@@ -6,6 +6,7 @@ class Libdmtx < Formula
 
   bottle do
     cellar :any
+    sha256 "4631cea68d83f274390ff023591256a92fb108b685c0528448a43a48d583c09b" => :mojave
     sha256 "eb892feb7d29f9291a0edc2be6c34b4584614103d4af9d1c62eb54370decd8e1" => :high_sierra
     sha256 "c93913cd5aff29278c538957fd6890d990f760abaff1b14cea6f6f171194b706" => :sierra
     sha256 "ebcd82bf4d9da2a71bd066722ce6750d6cf064b1c8f477ba9aca47987acd330c" => :el_capitan
