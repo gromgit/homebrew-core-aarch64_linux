e1eebc7151bda6cc9bcbee5aee295172abd0f0c7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 18:23:08 +0000

    liblqr: update 0.4.2 bottle.

--- Formula/liblqr.rb
+++ Formula/liblqr.rb
@@ -9,6 +9,7 @@ class Liblqr < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "e4823b883ffc65f35e9404499fa0293e7c1b1559f0b4a6bda8393cb018736f23" => :mojave
     sha256 "b54e684b469c952a453ec72ca7e8559198b2de0bf0b7a572b7191f266bbcda41" => :high_sierra
     sha256 "ce5899d11af881965bed731baac1ef6f35e77bf5d4daa1e0fa579e90b82e3d35" => :sierra
     sha256 "9d47668f2c1b428499931a32bcb55c957d837e677ce14215cd4d9a674eff1485" => :el_capitan
