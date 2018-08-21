8f71d4b81c53e331b53547783435493443a8f11e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 22:41:56 +0000

    mkcert: update 1.1.1 bottle.

--- Formula/mkcert.rb
+++ Formula/mkcert.rb
@@ -6,6 +6,7 @@ class Mkcert < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7b4b4be9c8cc00efcd4ec786de3bde2905c8f12ea5ce16abb793c89b26c20f3a" => :mojave
     sha256 "a72c7d0ed6880960fa7124ad1e68e67a9c471f100fc856a794bff15721b19620" => :high_sierra
     sha256 "976ef4ed36ecbde9e13d540781e4cf1a9844c173ba4f75bfb9d6737fe83cdea4" => :sierra
     sha256 "664d321bf8892d67f3506aaa2cd54b496536111fd779513c893e267a7a99bf29" => :el_capitan
