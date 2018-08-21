78af24ccfef31d879f0db25a4461c8e94e35f940
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 10:15:37 +0000

    fsw: update 1.3.9 bottle.

--- Formula/fsw.rb
+++ Formula/fsw.rb
@@ -7,6 +7,7 @@ class Fsw < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "90779855faefd63a20e1e60406430bfec63d4ce766e253dae595f01acbebbf62" => :mojave
     sha256 "d16086899f7ae88e0fd4eeaac5ede4e5749d688e9bb2385686f824f0a0e24677" => :high_sierra
     sha256 "71b5da385bf9d59d33e6c331f23cab5676284d627129ee4f0352976b8ce13fe8" => :sierra
     sha256 "3d02fa0e6e8a6f9518341fc3934e7b53e13ac42304b07b7088ce54384ed64371" => :el_capitan
