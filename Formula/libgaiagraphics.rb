adb1a6231b364752cbb07cb91d0001786babf15f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 17:26:56 +0000

    libgaiagraphics: update 0.5_4 bottle.

--- Formula/libgaiagraphics.rb
+++ Formula/libgaiagraphics.rb
@@ -7,6 +7,7 @@ class Libgaiagraphics < Formula
 
   bottle do
     cellar :any
+    sha256 "96fa1b8c18f022531ebb36212bb27346aef3f563a66d1ee66b77608fe2019f84" => :mojave
     sha256 "0214b2e415ead8026fc796a6f743304188b4453877de09114d3d02ed14bdb13c" => :high_sierra
     sha256 "e84cb82edf2b0a3926f2f6de212107256932643d89f188ddb67f71c08c4218b1" => :sierra
     sha256 "b3419626a7acd62b6afc604769013b661d3bc57736cdf79a262c287432194693" => :el_capitan
