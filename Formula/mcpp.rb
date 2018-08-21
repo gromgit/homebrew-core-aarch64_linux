81644dc5180e24a8cab9b1c4c120cc68dcd04e8b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 01:50:49 +0000

    mcpp: update 2.7.2 bottle.

--- Formula/mcpp.rb
+++ Formula/mcpp.rb
@@ -7,6 +7,7 @@ class Mcpp < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "40a63165c2df3feab3ed58c09a3f4b60daef5e112ec2f101f056aee56ca9819f" => :mojave
     sha256 "fe1489ca47b0d9e551b4aa1b6cb2a4135848be79e3982856442080f75fcb45d7" => :high_sierra
     sha256 "cdd368c63dc6403832c938967f8f099ec3d02acfcc5c75ab0426ad1cd213b045" => :sierra
     sha256 "0be73930b3dbc8bc247c9a26acbc6115d3f5f665daaabc9ab64606ac6793ace9" => :el_capitan
