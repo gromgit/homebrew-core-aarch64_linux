323d9f18ad84af4b3e36ea2fddeb8f3f45c4bbda
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 01:58:37 +0000

    movgrab: update 1.2.1 bottle.

--- Formula/movgrab.rb
+++ Formula/movgrab.rb
@@ -6,6 +6,7 @@ class Movgrab < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "cde671dc6401286e39179deff8598a78e7df1184b420178a178900f505ac4221" => :mojave
     sha256 "32d047668237fab4e3dea9987bce5d568a86b7846d671dffabcecd5aaf42ddfe" => :high_sierra
     sha256 "aa40b9cb2e4c947ced14535bcb0e4bf189cf90ca786a7386b372abc6e92ed90a" => :sierra
     sha256 "82cb6ea3423aabf6ae277fcdb2d6ae497021ba5ea7b9c58d6f3553ecebe1bb17" => :el_capitan
