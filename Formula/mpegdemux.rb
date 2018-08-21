fbc56b9d70f1d797a90ac13eca7958410eb919d8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 02:03:23 +0000

    mpegdemux: update 0.1.4 bottle.

--- Formula/mpegdemux.rb
+++ Formula/mpegdemux.rb
@@ -6,6 +6,7 @@ class Mpegdemux < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "daa143e087fe5d35e5deda8dd37637b54ba908ba34d050beb0b42b3697b1f67c" => :mojave
     sha256 "af6cd127b024079f9584533b143094cbcdb97693007eaffcb6f967942d471712" => :high_sierra
     sha256 "2a1bae657b91607e47b201f2d97749def3c771911208db70c278bf4a47f4fd7e" => :sierra
     sha256 "4f00eabfededc549d4f419b6e6bf7896dbff0c95e83fda5f47067bf73294289b" => :el_capitan
