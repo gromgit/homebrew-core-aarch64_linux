dffaa19440ba1ca04d3367f1a0163ced28c7cdac
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 17:54:58 +0000

    mighttpd2: update 3.4.3 bottle.

--- Formula/mighttpd2.rb
+++ Formula/mighttpd2.rb
@@ -10,6 +10,7 @@ class Mighttpd2 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f6a29c3f904aee4500991a66dae9b106d946fcc0ac98c3a40d9c3fe9e966bcaa" => :mojave
     sha256 "20b0a49d9442615903fb9532cd2ba070d5936b2cea69b9e5db2ca7be1052c47f" => :high_sierra
     sha256 "5d21c1640f273f3a57c9509c52750da51830fa21c79e6a69015001ed8f6f1f98" => :sierra
     sha256 "53b61f1044d2ef60b1dde3ebe5b5adcce01838c6c962fac14e9ba1e92b123d31" => :el_capitan
