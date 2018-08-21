1a92f7a0cf0e79c067847728016fca27900df85c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 00:58:29 +0000

    libtrng: update 4.20 bottle.

--- Formula/libtrng.rb
+++ Formula/libtrng.rb
@@ -6,6 +6,7 @@ class Libtrng < Formula
 
   bottle do
     cellar :any
+    sha256 "5ec98840f9e339911ef1cd9d666160005ad73dc9191e8954d5f96bead5ae404c" => :mojave
     sha256 "c6ed745a330d0da3123467cb19dd6f4c8f6871aed54b4f6addd813271599a2d6" => :high_sierra
     sha256 "54e596853cd0ea1b49dd62d0d3fc5f559063572a6f19e3fb26ef09ed19a01564" => :sierra
     sha256 "e821f8b59abe5f15689ac8720539bd258eab64f83ecfa6047406e4c11884bdef" => :el_capitan
