26a3d66c7fd9f9bd7706f323799ea917ca84a0e7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 01:09:25 +0000

    libvbucket: update 1.8.0.4 bottle.

--- Formula/libvbucket.rb
+++ Formula/libvbucket.rb
@@ -7,6 +7,7 @@ class Libvbucket < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "99703e4bfe795310481070096dd496b41185cdde224aa2381a2a7b2dcc618278" => :mojave
     sha256 "bce41a629d7cf6504a1d5fa518d31cc46fcc93b8c82187167d4ab9306f2d593d" => :high_sierra
     sha256 "69c96d5758926202939b79930d82da27bd65d50e0ef19d844ac705cacd99ba58" => :sierra
     sha256 "cc3333f73161a96deb410ae0b8185a74e0c2a5bc8e62018929b17efe6ef732b8" => :el_capitan
