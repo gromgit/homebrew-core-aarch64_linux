901df245e64a328ca1b63d4aa0d970cf3c5684d8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 00:58:31 +0000

    libtextcat: update 2.2 bottle.

--- Formula/libtextcat.rb
+++ Formula/libtextcat.rb
@@ -8,6 +8,7 @@ class Libtextcat < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "02d7f744996abfda8bd85b4580c5a92a8bd89ad6cc06e2848caa9b3b0e858144" => :mojave
     sha256 "7997ea512b672f165e1e53e941147e9a520a9ab5d71b8b22e4a71622690e7cdb" => :high_sierra
     sha256 "24fe8791549204d8ef6e596fc327fbd3a645c729b440ba31ef47cf545f6f5b30" => :sierra
     sha256 "afa51f83d0a3c96ffc6f6c35011c864347f31d2c3aea987102c59f0257177072" => :el_capitan
