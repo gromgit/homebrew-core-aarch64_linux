442c74005e841235e0ab5b1e1a551fdd8d59c416
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 11:53:57 +0000

    pth: update 2.0.7 bottle.

--- Formula/pth.rb
+++ Formula/pth.rb
@@ -8,6 +8,7 @@ class Pth < Formula
   bottle do
     cellar :any
     rebuild 2
+    sha256 "e7ed86c562756b07fcf9bb148c76f17c6cb4f3b02bf84ffe82285e3b279e7836" => :mojave
     sha256 "da4549f9e89a71478b47f4454f9a259dc3a56a109f24083ce8f4ea69b11ac9c5" => :high_sierra
     sha256 "583d6ae1681974c7461650151253c5a302f33fb16dae74b5546a4a693cec71d1" => :sierra
     sha256 "bac7f73c061797768be28e21bec2e7773cfd70ff7c3f46eafd464b9632d5eae4" => :el_capitan
