cfc78af98ecff7233fd6c94163352569e244b811
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 10:51:48 +0000

    neatvi: update 06 bottle.

--- Formula/neatvi.rb
+++ Formula/neatvi.rb
@@ -7,6 +7,7 @@ class Neatvi < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c9dfd1c69ab95ee511c1b27768166e5cba516b6c283e5b168db2451d3865fc44" => :mojave
     sha256 "2e34d03e212479064e86e8d1024447badeb61a54205f1fda24f9b6633e22afe7" => :high_sierra
     sha256 "339a7880dea5f7ff0e290bc890f95719da7e5ba4b64a7205760c8f6cf64e10a2" => :sierra
     sha256 "c773025ad559bb25cc095a7e1efc8950424a4cd86ff81ee4b0093a0e2e3c3c84" => :el_capitan
