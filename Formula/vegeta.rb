69555e01e140ee28924860f61ee14c322a70fbac
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 23:16:35 +0000

    vegeta: update 11.0.0 bottle.

--- Formula/vegeta.rb
+++ Formula/vegeta.rb
@@ -7,6 +7,7 @@ class Vegeta < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5a6c09a58457da6d6de746607edb4d763c23db3a862172583d8693ef597cd776" => :mojave
     sha256 "73d90510f49a30fcb6cd3740e57ed13f1f178b3881d574bb26c15b9caf980cdd" => :high_sierra
     sha256 "53eacf1e201b33df7fba7c6cd659b145e747ea1008a1f9e045aec79cf4ef3404" => :sierra
     sha256 "e28238ab34c4899bc646c8ab9c1ff1fb0976b379f527bf30ed3aa1fae2d54e00" => :el_capitan
