54aea0dd72d1849d4bf957d3ce4abc4aa4038106
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 08:10:27 +0000

    bvi: update 1.4.0 bottle.

--- Formula/bvi.rb
+++ Formula/bvi.rb
@@ -5,6 +5,7 @@ class Bvi < Formula
   sha256 "015a3c2832c7c097d98a5527deef882119546287ba8f2a70c736227d764ef802"
 
   bottle do
+    sha256 "0a8c034737e4b28d4f6c83f3ff222a797f4488503ed44b9b0df9d8966c19d9ac" => :mojave
     sha256 "832a212daacc98df05797371ff7fb6ff0bbe5b7ac824a430c04b2b7f04d0e1e7" => :high_sierra
     sha256 "f918f3425865c4b8df97c32cf700f631d2c19e285adf01cc6ceadd429f7d0bfb" => :sierra
     sha256 "47c35b91db7052ee938daa6ddd95e96d49982e8767a48a0821f054b543fedb7f" => :el_capitan
