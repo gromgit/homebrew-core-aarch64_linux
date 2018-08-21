fddc4eef4dfa9aad325d620df1d9ee0fcfa9926f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 09:34:33 +0000

    dmd: update 2.081.2 bottle.

--- Formula/dmd.rb
+++ Formula/dmd.rb
@@ -23,6 +23,7 @@ class Dmd < Formula
   end
 
   bottle do
+    sha256 "8b2428199cef0f5d3eadd0e7f947d24c530597ae21d2b544afae54bb8f9509cc" => :mojave
     sha256 "cd78f901eae3971a0bc6967ebf7da89d58f86a0a23a59b1c7afc95179b0a2b4d" => :high_sierra
     sha256 "0916214f85d530de232bf969209a9326e5b5cd83421b581025c142f0a745b4fe" => :sierra
     sha256 "2e0357261b2b1d3ece749d287c6496790831c9aa343499ff5b4b8384ba5b5423" => :el_capitan
