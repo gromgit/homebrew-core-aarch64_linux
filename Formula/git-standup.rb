901993786ce085565607f40ddc82d2deb5bac7c8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 10:21:11 +0000

    git-standup: update 2.1.9 bottle.

--- Formula/git-standup.rb
+++ Formula/git-standup.rb
@@ -7,6 +7,7 @@ class GitStandup < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "31dd987e8a3e49e804fde9d67a48cbeec54cfbcbc8e7ee6ce5fc8c344519eb28" => :mojave
     sha256 "a0eb22ac35b48f9696da523eb85270a5a0e6652d35c921a5c80b195d3e1f39c2" => :high_sierra
     sha256 "a0eb22ac35b48f9696da523eb85270a5a0e6652d35c921a5c80b195d3e1f39c2" => :sierra
     sha256 "a0eb22ac35b48f9696da523eb85270a5a0e6652d35c921a5c80b195d3e1f39c2" => :el_capitan
