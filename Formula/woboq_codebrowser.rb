654619b9a8539cfa4b320001470abbf00d14acf9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 16:01:58 +0000

    woboq_codebrowser: update 2.1_2 bottle.

--- Formula/woboq_codebrowser.rb
+++ Formula/woboq_codebrowser.rb
@@ -7,6 +7,7 @@ class WoboqCodebrowser < Formula
 
   bottle do
     cellar :any
+    sha256 "553acd46ed5042664933e571c5507701c0a44fe64066955c7e5771154d63b7b7" => :mojave
     sha256 "540266c775e25379110f82e933649cbf2f2582e6541bbaba57f94fe88da5a4e4" => :high_sierra
     sha256 "e667ed2580d16bfcc1ebfc80669d22b67105625f49190d9e2c965817e6870c22" => :sierra
     sha256 "c04a6cd9fd7102e2ca5858dce5068bbfcede09e3d655e0512a2942d83f5a75e8" => :el_capitan
