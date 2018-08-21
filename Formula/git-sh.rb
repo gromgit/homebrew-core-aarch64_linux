ce004eed4d689dbbce59c298766deab730380985
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 10:21:13 +0000

    git-sh: update 1.3 bottle.

--- Formula/git-sh.rb
+++ Formula/git-sh.rb
@@ -7,6 +7,7 @@ class GitSh < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "379e7d57000e9eda710d764516e3c4a98e3aafb71020e15faafe3e20e12a82d3" => :mojave
     sha256 "69491a98476b0e745107e6be3623d678e0dcdf33dda7f76d541ba773881e74cd" => :high_sierra
     sha256 "d371fba61367507f5e88818eb1f0630e388d198c37faa957ce410d97675a7f5d" => :sierra
     sha256 "e30e7836919a5d79712e3fd51a118279b412c44da909053b9b185eb48963323f" => :el_capitan
