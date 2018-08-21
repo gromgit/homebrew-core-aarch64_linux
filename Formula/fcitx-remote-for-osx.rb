706d435d6ac44458290d48385ebe1416a6cfaf50
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 09:41:25 +0000

    fcitx-remote-for-osx: update 0.3.0 bottle.

--- Formula/fcitx-remote-for-osx.rb
+++ Formula/fcitx-remote-for-osx.rb
@@ -6,6 +6,7 @@ class FcitxRemoteForOsx < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5e2fea3fccdb10411448b49a188eadf5684e5c36d34a64025a215ea1e2554dac" => :mojave
     sha256 "d0d5e296210ad94fb80bdc8e8058a2ad32ba293f3a1d9bb54ab2dd2de573e5f8" => :high_sierra
     sha256 "91611184e35f77a587eb1c36042660809564ff3fbae553d492b036a8b64b4f56" => :sierra
     sha256 "8e54fa21fce7e9e363dba2a47f462d1b415d55e9687322d251e2e5995442599a" => :el_capitan
