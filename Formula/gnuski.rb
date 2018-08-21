7eb20240ffb611d52d41f45192653d2744d20978
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 10:22:59 +0000

    gnuski: update 0.3 bottle.

--- Formula/gnuski.rb
+++ Formula/gnuski.rb
@@ -6,6 +6,7 @@ class Gnuski < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4fe7b21e4b40ee72c7825c1e0330a958694b98529121385b78b7af9aff229d6d" => :mojave
     sha256 "6f15bd497951ea784e84b2ec888be83343ad1ad96eb6bab9ba343bff31246700" => :high_sierra
     sha256 "3874907a4ad715492c026d969ec3265dcd5f71424dde07a83aa1c21a1e36fa38" => :sierra
     sha256 "ce14d8ee8b8d58c710b93adb2f4cedfb9d78fb64746f38daee4ea38aa977ae43" => :el_capitan
