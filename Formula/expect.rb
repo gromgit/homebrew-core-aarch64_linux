403dc9714793d1ccc5e5cb2a6ab04172cc0f831f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 16:16:12 +0000

    expect: update 5.45.4 bottle.

--- Formula/expect.rb
+++ Formula/expect.rb
@@ -6,6 +6,7 @@ class Expect < Formula
 
   bottle do
     rebuild 1
+    sha256 "668b4fb12eed5bbf783e8b4ec52dad24b88f38af5577ba1e45ed9947e50e50ef" => :mojave
     sha256 "a0c6ffe797dc0bbe512b628819acee67a7a9b00573b6433fe0672285d41a9df1" => :high_sierra
     sha256 "fc9ad781caaf8d45f47a87d4303645faa2e600852c73fd5432f0be2e588e95f2" => :sierra
     sha256 "4fcb163b1b1e7e209b632c43ba03106ca1c4e4d6a745260b813d28a803581e58" => :el_capitan
