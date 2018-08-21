2c795d593d5dfeeae342b0efce77cdeddaa3b206
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 00:08:22 +0000

    libcsv: update 3.0.3 bottle.

--- Formula/libcsv.rb
+++ Formula/libcsv.rb
@@ -7,6 +7,7 @@ class Libcsv < Formula
   bottle do
     cellar :any
     rebuild 2
+    sha256 "ad3c84168c138aef88134f7666f870dcb17f8b779b5e5b54417515f7c9b740af" => :mojave
     sha256 "6946a6ff37a03f75d464cdc1229eb72251ae6b5d2726a658a016e39e862f0e33" => :high_sierra
     sha256 "6d89efd634be6551134f099e458225325d76d69f55ba37676a3ccf7bea6c4e59" => :sierra
     sha256 "3f69bb369fafd5c207f1c8ea500dc1e725e8e7dfe005215ff14b61fc25ac28e6" => :el_capitan
