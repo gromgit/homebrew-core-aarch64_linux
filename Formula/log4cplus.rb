3e1690b8922e2bf662e8b7c50d7a496771378664
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-22 04:23:38 +0100

    log4cplus 2.0.2
    Closes #31344.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/log4cplus.rb
+++ Formula/log4cplus.rb
@@ -1,8 +1,8 @@
 class Log4cplus < Formula
   desc "Logging Framework for C++"
   homepage "https://sourceforge.net/p/log4cplus/wiki/Home/"
-  url "https://downloads.sourceforge.net/project/log4cplus/log4cplus-stable/2.0.1/log4cplus-2.0.1.tar.xz"
-  sha256 "813580059fd91260376e411b3e09c740aa86dedc5f6a0bd975b9b39d5c4370e6"
+  url "https://downloads.sourceforge.net/project/log4cplus/log4cplus-stable/2.0.2/log4cplus-2.0.2.tar.xz"
+  sha256 "8ff4055be749f17f3648694bd5778bfd86d33158cceaa616a50c0299d6035b41"
 
   bottle do
     cellar :any
@@ -19,5 +19,30 @@ class Log4cplus < Formula
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  test do
+    # https://github.com/log4cplus/log4cplus/blob/65e4c3/docs/examples.md
+    (testpath/"test.cpp").write <<~EOS
+      #include <log4cplus/logger.h>
+      #include <log4cplus/loggingmacros.h>
+      #include <log4cplus/configurator.h>
+      #include <log4cplus/initializer.h>
+
+      int main()
+      {
+        log4cplus::Initializer initializer;
+        log4cplus::BasicConfigurator config;
+        config.configure();
+
+        log4cplus::Logger logger = log4cplus::Logger::getInstance(
+          LOG4CPLUS_TEXT("main"));
+        LOG4CPLUS_WARN(logger, LOG4CPLUS_TEXT("Hello, World!"));
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "-I#{include}", "-L#{lib}",
+                    "-llog4cplus", "test.cpp", "-o", "test"
+    assert_match "Hello, World!", shell_output("./test")
   end
 end
