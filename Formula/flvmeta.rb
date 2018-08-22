172d64a906f462349834d9c715da1e066fb85481
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 20:38:58 +0000

    flvmeta: update 1.2.1 bottle.

--- Formula/flvmeta.rb
+++ Formula/flvmeta.rb
@@ -7,6 +7,7 @@ class Flvmeta < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "67e0f288fe10e2446cdb9bbbc2991256c69cf9622aba58d6c153b37308744d29" => :mojave
     sha256 "378420e627f64559bc36e9b80645b6aa4628e09823e408817e05a7cbfb202f2c" => :high_sierra
     sha256 "2413315dd7ef5bc4e3cde1710d8d54a5b8481c655e3097d7fa3cebd74aae1dfb" => :sierra
     sha256 "e38ef52c18acdf32d1a61e2604220eeff05b3ca1eb8ca8b215f20371b7b09f37" => :el_capitan
