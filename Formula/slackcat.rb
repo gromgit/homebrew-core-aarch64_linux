36df20d6a38f171a098bee294bd911aa19f88550
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 16:03:12 +0000

    slackcat: update 1.4 bottle.

--- Formula/slackcat.rb
+++ Formula/slackcat.rb
@@ -7,6 +7,7 @@ class Slackcat < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "6b46f20293da99226fdeb610dc78780cc3b6e8ec66390d982a5b08f574aed3b5" => :mojave
     sha256 "81bf29fac1f47bfaa4064b9b03aae96c4ea179158b0baeb0868203c6b7144221" => :high_sierra
     sha256 "7a0331e03695488f07eba24c4ebef11b406face8dcdbfc632beb8c5eb3227395" => :sierra
     sha256 "bd75f396807917734adec3c541faebee3789841474fdd81c343c61c6a739d293" => :el_capitan
