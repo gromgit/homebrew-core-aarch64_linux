88b6edbbb857ec8c5cccd3deda351882e698b489
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 12:38:18 +0000

    roswell: update 18.8.10.93 bottle.

--- Formula/roswell.rb
+++ Formula/roswell.rb
@@ -6,6 +6,7 @@ class Roswell < Formula
   head "https://github.com/roswell/roswell.git"
 
   bottle do
+    sha256 "e22f2f83d23b8158bdc83552dcfcabfb594a6c526e7741e115b5f784cff4193d" => :mojave
     sha256 "cc4614def8e563ae2bc0440c112f6a52d3a9de49ec8d8b812dbf50c92aca0916" => :high_sierra
     sha256 "181da84c21efeb9ee8b83cc06820c9383db50d2b9e73c163d5bd4d8597be44f7" => :sierra
     sha256 "b06a44335b3153ec53c260db673f71116d12e70312ce32a5f6afa59caca382c3" => :el_capitan
