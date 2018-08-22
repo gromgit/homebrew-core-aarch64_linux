5f7d68155d92f4ab5a83249d7501005a44c8f993
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 07:22:14 +0000

    double-conversion: update 3.0.0 bottle.

--- Formula/double-conversion.rb
+++ Formula/double-conversion.rb
@@ -7,6 +7,7 @@ class DoubleConversion < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b31ab13dc2ee0c0e6892dfec9f96647009c7dba5b607a1fe4659fd384b225ad8" => :mojave
     sha256 "a90962bc9b0d25bd4d1c69e8892af60ffa4c8c18efd5653265debdad1736996e" => :high_sierra
     sha256 "bb2a51610649c9f86c06adc2ae66764a42b66e4d95166d306b05dff35e91e422" => :sierra
     sha256 "30a844eb4530cbe35e05fefed118fe096bf34de61c536283e6a1845f2f087639" => :el_capitan
