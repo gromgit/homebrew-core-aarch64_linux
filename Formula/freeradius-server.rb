93aef62e9e180d05c68ae8fc77cec3b70a9b445b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 11:03:36 +0000

    freeradius-server: update 3.0.17 bottle.

--- Formula/freeradius-server.rb
+++ Formula/freeradius-server.rb
@@ -6,6 +6,7 @@ class FreeradiusServer < Formula
   head "https://github.com/FreeRADIUS/freeradius-server.git"
 
   bottle do
+    sha256 "3966eb7c2b7d70695df7c8a9bae3476d94f47d66eded6cbe3e718e8d404198aa" => :mojave
     sha256 "038ea0b2497cf0460473eb660fe8dc770cfa7e1d9fc603015194e6a9c3a8dfd0" => :high_sierra
     sha256 "2ef3f4689ca57e28836b3501a520b731b5510e7ec62a601ae6af4f1bec54067d" => :sierra
     sha256 "4a8d696e3532c98b027a6fb9e4b9a8a6472a80800c187902cbd5d37d44579fe7" => :el_capitan
