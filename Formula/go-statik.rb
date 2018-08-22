47c0824f33e246498340c61614a8446ebaccface
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 05:38:14 +0000

    go-statik: update 0.1.4 bottle.

--- Formula/go-statik.rb
+++ Formula/go-statik.rb
@@ -6,9 +6,9 @@ class GoStatik < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "df1233d8622a603cfc1376df589dde68eacfed61d936adb5ef5b76fc3f7498c2" => :high_sierra
-    sha256 "a8d36c584b4ec26387492ad32470847890bc853734f072253050429d64430457" => :sierra
-    sha256 "925e6aefe2b2a928cd79cdde8d7d840b52b4d9766daad7e331025eb2823f80af" => :el_capitan
+    sha256 "0d487e1428fd79d04c28d9b355c4ce22f090868b35f90e6f21c7bf6c0e801ff6" => :high_sierra
+    sha256 "f6e4f1d7b34a2598e75bd8172b948646d78d08e2f6b096404fd365e3673d2a48" => :sierra
+    sha256 "5c07aec9cad77dc1d0ace389456cb30c331f943f63eb2d5fa4af117bd6c4b960" => :el_capitan
   end
 
   depends_on "go" => :build
