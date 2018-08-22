3298f47d5940702a38f19875dda5345ce708f95d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 09:14:36 +0000

    blueutil: update 2.1.0 bottle.

--- Formula/blueutil.rb
+++ Formula/blueutil.rb
@@ -7,6 +7,7 @@ class Blueutil < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1ff16c4be6a2291cda156244f72035cb4d4a9127696559f726f59e8004612308" => :mojave
     sha256 "b3b44b557c9f1ebfad62a3a8dbe52b64ff5995bfe03003d10ef94fbdf95e6b6c" => :high_sierra
     sha256 "3c5cbdb52c7a528ad23ad9bc99a286ecb03554c71aaf8216ec2a891c16ec6c97" => :sierra
     sha256 "d5af9f8a05af98fa5a5dd6555673a878617573e0036ae42552e93f8758edc814" => :el_capitan
