28654508308c96da20a801c2f60e76358e8e7591
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 12:14:27 +0000

    xclip: update 0.13 bottle.

--- Formula/xclip.rb
+++ Formula/xclip.rb
@@ -6,6 +6,7 @@ class Xclip < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7bb1acc9b968eba155874f614dbfea960e883121321b063faf81f106f2521014" => :mojave
     sha256 "0963015158b7d4ae2981503edc18427737a0586b7155da5cd2ddaa93fb3b92bd" => :high_sierra
     sha256 "bb26c2bb6d7ce8f15ab50144f38d11ddde113bb400326ccea990ca9a5d0a9c69" => :sierra
     sha256 "9e17790e9a94ae1e29317f013a65f2d639ae9063db48ed7fa0aed7449f221abb" => :el_capitan
