0a3b2ca7ef31df05fda0a7343a0bffc0a680694e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 20:38:56 +0000

    docker2aci: update 0.17.2 bottle.

--- Formula/docker2aci.rb
+++ Formula/docker2aci.rb
@@ -6,6 +6,7 @@ class Docker2aci < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "38c55da3d7dae54ac615b1ef70d3b793ace880a3df8324c94586cbdcb0069a47" => :mojave
     sha256 "786e30d746607eea372c8eaa2705f850320dd74e28385fd3b75946e6e8c8e52d" => :high_sierra
     sha256 "6cfeb751ff7db4e703938e2bfc4e28d4ec9a30e59261e75aa5adf690d0f33061" => :sierra
     sha256 "b1a61fc4d329ef1e3ad97ea701e2c0be392f29e8d4a8bd2f1934bf7bac620121" => :el_capitan
