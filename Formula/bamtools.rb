ed2a65bbff013cc9f84d66bddb609bd70a6549c5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 16:03:09 +0000

    bamtools: update 2.5.1 bottle.

--- Formula/bamtools.rb
+++ Formula/bamtools.rb
@@ -7,6 +7,7 @@ class Bamtools < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5c1195094d84ce0e5ec63660597029dad111639e7212537c1d083444714ac294" => :mojave
     sha256 "cd3886cfb77b71ef9924d5475e4dbae2d42c4c66ef3880de33ca202855ce92b0" => :high_sierra
     sha256 "5e72d5b1b5b18551bbd91c7f3b7a2dd6e763b13add38b9a3a798bb5a450be64e" => :sierra
     sha256 "920e533776328d79f47bb562af5cfa00d855223818916e00614ee81d821f211c" => :el_capitan
