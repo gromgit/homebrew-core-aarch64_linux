3259a1a857d23a7c03b00f673760e0990eb28646
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 18:20:54 +0000

    sslh: update 1.19c bottle.

--- Formula/sslh.rb
+++ Formula/sslh.rb
@@ -7,6 +7,7 @@ class Sslh < Formula
 
   bottle do
     cellar :any
+    sha256 "d997067745470836e2a30e2a030122a5d101fd4f2768312164b7b94e53e78081" => :mojave
     sha256 "4f3429c456314ead9330497258f58dbc07c620a497101d451b37538f773e7138" => :high_sierra
     sha256 "4c02e4c94f732f382abb18e3831f629a0dd108211aca169ae7b93c3e387a6b70" => :sierra
     sha256 "a457cede9fdc5903b4d4cf390ccf34caf716c1cf8ddfef315197876c250438c0" => :el_capitan
