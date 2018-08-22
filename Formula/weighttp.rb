005264e93e0a739c2b6a8c6f97f8a184856dc3c5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 19:29:54 +0000

    weighttp: update 0.4 bottle.

--- Formula/weighttp.rb
+++ Formula/weighttp.rb
@@ -7,6 +7,7 @@ class Weighttp < Formula
 
   bottle do
     cellar :any
+    sha256 "2ab4f5e31f9411d55c4a4653f78bb381b70f53f49d07efaf6e99b5a86281b62a" => :mojave
     sha256 "4225f653fe64067e3330c33202a15ad65a6b194ce23619ae045cbe50528a9b02" => :high_sierra
     sha256 "242f14d7a7fb477e4722a3818a98ad25ffedd5d2c80e7c97d67c80fe2a20366c" => :sierra
     sha256 "e96be0135f552ddde0547ca914c2bc6635dcc59ce4bdeb803ab9412100d8d15b" => :el_capitan
