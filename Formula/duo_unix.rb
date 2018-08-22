30e9fe4b410153b859016fde9d6d95fafb7e9fc7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 18:53:04 +0000

    duo_unix: update 1.10.3 bottle.

--- Formula/duo_unix.rb
+++ Formula/duo_unix.rb
@@ -5,6 +5,7 @@ class DuoUnix < Formula
   sha256 "8259af6e24d618401886f6cbd0c8f72ca6ba8ba9cffe04dd3110e672933388ce"
 
   bottle do
+    sha256 "2a12bca90639ebf2a1e483c7b75c2c6659677761f60a0952693eac754a2d6fb8" => :mojave
     sha256 "30393ba2e84c7eb00111f3eccc3f6b214cea93c58ffb2cbdc39ca88f4c7be285" => :high_sierra
     sha256 "5db71e87da7c8b4db91ade92664bc0b8328757391ccf2c68778b55feae23b589" => :sierra
     sha256 "76d1b26e83386c57cbcec73dfcd52ffb1769e1974f9608222ae88b0d978016b0" => :el_capitan
