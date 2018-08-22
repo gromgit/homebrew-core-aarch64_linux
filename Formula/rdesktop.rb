87db62f9bf84e72ef062fc6a41a4f0070581c381
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 12:14:28 +0000

    rdesktop: update 1.8.3 bottle.

--- Formula/rdesktop.rb
+++ Formula/rdesktop.rb
@@ -6,6 +6,7 @@ class Rdesktop < Formula
   sha256 "88b20156b34eff5f1b453f7c724e0a3ff9370a599e69c01dc2bf0b5e650eece4"
 
   bottle do
+    sha256 "f9594ed7f111a6422773469b9ed7e6b3bf31d1f5cab6eff297f4a8be739427fc" => :mojave
     sha256 "7a70e9cd3c541121b9ae55eabb7036f34c3818a952ab5104365bd6437ebb9420" => :high_sierra
     sha256 "2a09f53bccef981e542de0c2a3066ccb6e438fe0c11341281cb2803ce09f7bb8" => :sierra
     sha256 "46b1a3070669d5f0e2f1e70e387ae4a3c7d956a0991378138ab5de39e6be3b9e" => :el_capitan
