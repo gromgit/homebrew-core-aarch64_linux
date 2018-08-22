0868824a1b01e0af8a2efa0e3123f8c253f75353
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 17:32:16 +0000

    tintin: update 2.01.4_2 bottle.

--- Formula/tintin.rb
+++ Formula/tintin.rb
@@ -7,6 +7,7 @@ class Tintin < Formula
 
   bottle do
     cellar :any
+    sha256 "fbdf58d5dbba8cf3acf14c2691b7bc2408ef46cc2d1d720ee2baf0386da95f12" => :mojave
     sha256 "552223fd7efc3d68a99a1d49135a6537091f0d893e67f08eaf876e2385a0460d" => :high_sierra
     sha256 "58dbcc694fb3b80f10951d06f24fb4fd0e94eac1a3dc84017cd82e38610ff6a7" => :sierra
     sha256 "bd1fe99b7c2a458acd81a5a567ba57c664c99f9f5f73adb8845bae7d7363a151" => :el_capitan
