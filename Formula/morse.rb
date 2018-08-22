0cb92ccb02a17eff462dff48f6b86a054ff693cf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 19:29:58 +0000

    morse: update 2.5 bottle.

--- Formula/morse.rb
+++ Formula/morse.rb
@@ -6,6 +6,7 @@ class Morse < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5fe911c0c4d71783759f9d8c4e6269c873a830d0511e0813edf7ec86f3c7f62f" => :mojave
     sha256 "fb58a8af73002f98fe7ff1274c1712eb4bf0cab8b08640d2836fc6951c5cb2e9" => :high_sierra
     sha256 "d779902b961e9ebbfa41b0906d8d41357232fd4da83a393e112cde87f5bcdcaa" => :sierra
     sha256 "491a1ea5455d058af9adb607e0e49d95b94e52f0068cd5fb197c1ea71666b524" => :el_capitan
