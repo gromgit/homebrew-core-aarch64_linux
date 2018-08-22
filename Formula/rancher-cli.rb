7575a76716a8a1415a068aa46197aecd6fe3ca1f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 15:15:45 +0000

    rancher-cli: update 2.0.4 bottle.

--- Formula/rancher-cli.rb
+++ Formula/rancher-cli.rb
@@ -6,6 +6,7 @@ class RancherCli < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1994ceeb62639571dae6fbd7d9b8968ba5634fea7ef7e94f265e192a8277ed75" => :mojave
     sha256 "637bc7f664fd8c9b84bfddb31e47cc074049434160aa7fdd8db89b1f0e10983f" => :high_sierra
     sha256 "29b6bb40da3c8e72e1fb0a6ef734e13817b02f0a697749902dd48efddf80ae10" => :sierra
     sha256 "4587f6c4e7b452db3d6545e637222e2f572f69668fb29db9fdfa14dc53de370a" => :el_capitan
