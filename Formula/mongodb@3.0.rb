5027dc20c9923727c8f98e87cbe55548516c9ea6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 15:15:44 +0000

    mongodb@3.0: update 3.0.15 bottle.

--- Formula/mongodb@3.0.rb
+++ Formula/mongodb@3.0.rb
@@ -8,6 +8,7 @@ class MongodbAT30 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "053e7f96eac6cbc5201683fdc0e06f2fa6c8fdd903ed73ec63aadbcb52198b8f" => :mojave
     sha256 "a3c4c9fcf5c44c34f96395923937c65e7cf6cb52bd937ef2878afe91633672fe" => :high_sierra
     sha256 "51f78308884b822d7406e12c897b613445cd0cb41add89b4835a203190c658d5" => :sierra
     sha256 "3a8d91eba9e8342e325e6fe1643b63687429ce2866c61a27bef836928b264c6b" => :el_capitan
