20d9605ae4c02ad9c114e188270344ea0b05b41c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 21:32:37 +0000

    perkeep: update 0.10 bottle.

--- Formula/perkeep.rb
+++ Formula/perkeep.rb
@@ -7,6 +7,7 @@ class Perkeep < Formula
   head "https://github.com/perkeep/perkeep.git"
 
   bottle do
+    sha256 "1e51d2d991309cec65b66411ff9dbbd1d93ec40c4c595d1b91a786541c15d738" => :mojave
     sha256 "4db15262421ce0cefad97fd2df3affba61cba2e1ba1e4153614940cec138ae10" => :high_sierra
     sha256 "9ab629c218f4af8f769520ec269c81c6499c6c9676c5ae43da8fc7ac8212c0ff" => :sierra
     sha256 "e4e42c68017500af8a8aa7b542e89905849b09b398a547da818323f08a6e680a" => :el_capitan
