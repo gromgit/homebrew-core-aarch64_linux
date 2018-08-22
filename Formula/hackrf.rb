cea6130327c7d65de4c802d50ae72b7e34d0b195
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 13:38:24 +0000

    hackrf: update 2018.01.1 bottle.

--- Formula/hackrf.rb
+++ Formula/hackrf.rb
@@ -7,6 +7,7 @@ class Hackrf < Formula
 
   bottle do
     cellar :any
+    sha256 "9c0610e7d8fe8f1e840b38d3ce6eeab741842a95f227025fbca24c417ae30549" => :mojave
     sha256 "430173362cc05912520a38f41ce465a0966f1c8d849fd492f0b40074425c3f88" => :high_sierra
     sha256 "f33bc6bde41e6522d587bc574c01e1402ccbde6759dec5e9d1a1e5f593e189b3" => :sierra
     sha256 "909a5a9aca6f81cbab08bb7c063f3ee0e666bb5b44af86ebbec62cbdaf3e3b33" => :el_capitan
