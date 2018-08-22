4f9c23701d0757c99830d1f1c6362b27e8a8e5d4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 19:28:14 +0000

    xalan-c: update 1.11_1 bottle.

--- Formula/xalan-c.rb
+++ Formula/xalan-c.rb
@@ -7,6 +7,7 @@ class XalanC < Formula
 
   bottle do
     cellar :any
+    sha256 "5b00fab72d4db7db40495ff5331e6cd9539b30f21d6b1357d9dcc2e7275421ae" => :mojave
     sha256 "24ddfd8ff41dbe54a5570db2a004247f92ef4bc1c897554ea83dfe7c138a172f" => :high_sierra
     sha256 "dfe6413a8d4cba234c105d0936a671a34742d2ac0103db863a644bf78538c28c" => :sierra
     sha256 "0b99ebef6e23b1c0d1e67d4ed8130130ad5c7b6af03f43ea9248c2d78e19a5cc" => :el_capitan
