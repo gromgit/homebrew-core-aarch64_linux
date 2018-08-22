3a71b6ed576201eeb07d02502c22c3ad74f11808
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 12:14:25 +0000

    xrootd: update 4.8.3 bottle.

--- Formula/xrootd.rb
+++ Formula/xrootd.rb
@@ -7,6 +7,7 @@ class Xrootd < Formula
 
   bottle do
     cellar :any
+    sha256 "114e764f63d15b02ddd6091f7d28f126584f5abbe493b6684bdcb1d39bc91f1d" => :mojave
     sha256 "9d0c1ebbf9d77d4afcd96aa8e12562a9678297306a000f2e1ac6d5a4fc5c2ad4" => :high_sierra
     sha256 "6c27e086bfab2dfda91e34814d588fd294c18f0cede4240b69582abc693f0a99" => :sierra
     sha256 "70bdb9f69fcda66b1ea863d55360ac65657cc6bfafb3adafef07115bd7fe0cfa" => :el_capitan
