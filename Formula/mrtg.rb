1c25d89640a6738a39cb35d414edec211cf1da31
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 17:15:01 +0000

    mrtg: update 2.17.7 bottle.

--- Formula/mrtg.rb
+++ Formula/mrtg.rb
@@ -6,6 +6,7 @@ class Mrtg < Formula
 
   bottle do
     cellar :any
+    sha256 "cca3b58b58a453636d67bd041f56df487ce86eb7f592178bd9f8745e35e0f5f0" => :mojave
     sha256 "8a76630f04f1c8b6883d7621cf9c72456e22fa5206a8b2b3eb6527547d048fbb" => :high_sierra
     sha256 "71561d0dbd12007514597dc85bf8ecb5e76e32f0feb35eb126b0998a30db49c8" => :sierra
     sha256 "0d434588f3d22e55db11fe23baa82a5e1a4a8ccbcab84dfd74ddc1a0dafc085c" => :el_capitan
