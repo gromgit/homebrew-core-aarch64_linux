f03d9bb06c20931b95bd737ec8305936a4d18e97
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 17:49:13 +0000

    qca: update 2.1.3_2 bottle.

--- Formula/qca.rb
+++ Formula/qca.rb
@@ -32,6 +32,7 @@ class Qca < Formula
 
   bottle do
     rebuild 1
+    sha256 "b1232ad866b1e40e28a3f84674570741c0f9f8356ca43ead89574b00306f1875" => :mojave
     sha256 "7fca5c9a591a204813356e3314077a628cbbff1cb5e6669355a2e26cd92765aa" => :high_sierra
     sha256 "8dd6479be1f5cacb740915646bf9dd2fb8103df38e9f75ecfbb507ed3a0b201e" => :sierra
     sha256 "7790fd8de8b6ee98ca8d4f687894437137d774538c209a80a340f513a8fbc159" => :el_capitan
