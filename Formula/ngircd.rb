13b39108c9cc7bd0183b3ae230830ef91ff6dfad
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 20:38:59 +0000

    ngircd: update 24 bottle.

--- Formula/ngircd.rb
+++ Formula/ngircd.rb
@@ -6,6 +6,7 @@ class Ngircd < Formula
   sha256 "3e00a7da52c81fc1e02bb996a27bf43da905ba7037bf8c6bb3bd13321e0c85ab"
 
   bottle do
+    sha256 "f7bd9059bc934e317cda8491cc6ed08c1a86da0da8e52a1424842892ce03a45b" => :mojave
     sha256 "6832dfa5480e2f089c32468c14eaa0a4d2ef3a1945eb53b1544068c2957fecd2" => :high_sierra
     sha256 "a5303a11814a311d639f585645808f9378b660982c5e40f4c8d025353d35001b" => :sierra
     sha256 "761f7fdf0da86e1926cfe17ed298610cd1eb20607232ea1b75cc4f14c1966ae0" => :el_capitan
