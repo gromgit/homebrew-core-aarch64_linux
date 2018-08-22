957ce16879c0c26c645636e5552f7a94f69d2e62
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 12:38:19 +0000

    ed: update 1.14.2 bottle.

--- Formula/ed.rb
+++ Formula/ed.rb
@@ -7,6 +7,7 @@ class Ed < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "63b861cc99b23d057c96445a514b1b7a9834e0816d26289e17a85d3f57cecaea" => :mojave
     sha256 "8f81d744b94ceb94b3042369ff93321b2cbd1904cf04f0289b22542d6b68d0cf" => :high_sierra
     sha256 "2bcab4bf26dae3b57ffe1097744191315941b6e26c6a43157e2595bb64f40c17" => :sierra
     sha256 "b891b87205fbfcc593673726193ceb44535155237d09dc17b38359ed5abee125" => :el_capitan
