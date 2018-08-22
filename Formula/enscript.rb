38c41fe0d42f6a2ab2dc9ffe3858ad1d88de68ec
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 18:14:08 +0000

    enscript: update 1.6.6 bottle.

--- Formula/enscript.rb
+++ Formula/enscript.rb
@@ -8,6 +8,7 @@ class Enscript < Formula
 
   bottle do
     rebuild 1
+    sha256 "5e66a847d27142da7b24603988a3f31cc841fdab66c9cb658a8a0f5f4da9a82c" => :mojave
     sha256 "4b9fefd4bafc5f190272b8ed5bc4c0fbdaf4e1cdfa03aee7b7ef2f829500bc0b" => :high_sierra
     sha256 "dea5f069c92bd1b5d2e3c1f0440d8ed5281d9ee44225e28295a1f682ff43a934" => :sierra
     sha256 "e55d3f93f7a4eb89d8007d9c0c49d6b7f52778191f2601da648afff0098a6663" => :el_capitan
