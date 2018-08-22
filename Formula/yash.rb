f21db7a38adc2891ad7b32b21f660da8b2d4510e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 19:29:55 +0000

    yash: update 2.47 bottle.

--- Formula/yash.rb
+++ Formula/yash.rb
@@ -6,6 +6,7 @@ class Yash < Formula
   sha256 "931f2e7451d8b1eca2a98caeef7eda0527d96376f9f2c9bec90bc5938e39992e"
 
   bottle do
+    sha256 "a80b7e4adc5bb6bbfdb85ad01c761889026617455eb686eca20849c26bfbc159" => :mojave
     sha256 "8ceb88372ff9dbf2461994e646b7743c3fb927115fbd540fdd8a163a9c929916" => :high_sierra
     sha256 "3b13c76147e4835eeff1b1794eb534deedaf6e787d3871a233f8dd34b9003fe9" => :sierra
     sha256 "9fc6669b72248884e4274f695b9188745e5ae8fac359862373efa9a6f98b7469" => :el_capitan
