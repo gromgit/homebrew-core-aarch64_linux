10dc54e97e98ebb91c01df48fbcfd377f2427828
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 12:14:25 +0000

    binaryen: update 50 bottle.

--- Formula/binaryen.rb
+++ Formula/binaryen.rb
@@ -7,6 +7,7 @@ class Binaryen < Formula
 
   bottle do
     cellar :any
+    sha256 "0290a0cc23369ee0c100e72b57a31e0ae982433deec1b7088b79d917d987f5ff" => :mojave
     sha256 "b47da6ca77a7d84fede943f98a3605ce48c980623d0650d8df7d400c40c2f2dd" => :high_sierra
     sha256 "0f8a79f92b1f073bb176996fe1fd41d23885118ffe81a9e0c9a9ca3bb52723c9" => :sierra
     sha256 "3b31a7503ff228c86de61932cf6c897a3e4b3b4755b91cf3364a88118b102e8e" => :el_capitan
