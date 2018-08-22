9444aa2b40f0684021bae1854811e2c545dbed2b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 08:31:46 +0000

    uncrustify: update 0.67 bottle.

--- Formula/uncrustify.rb
+++ Formula/uncrustify.rb
@@ -7,6 +7,7 @@ class Uncrustify < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "22735c84609412aa9ab41ba94f64b73fa5944d4dbf44cee001357785e45ad80d" => :mojave
     sha256 "44e40ac96092d82efd5d7b31f99ce8a8f87df5e400c156ca061a6996780598d1" => :high_sierra
     sha256 "d60af01b3d784640f05b5dd38360186ca7280d386bb02d8526177f69228adea7" => :sierra
     sha256 "285780ad3a0df290ea3a36d166b0c191c9bc5065afbd2294935660c85b741572" => :el_capitan
