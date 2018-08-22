dc9404b590772a540545b072076b8b49f96d4efe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 11:03:39 +0000

    wabt: update 1.0.5 bottle.

--- Formula/wabt.rb
+++ Formula/wabt.rb
@@ -6,6 +6,7 @@ class Wabt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "efb9416a4a4e8a998f9ec18c0c82b2f045afe3c3b09ef8caabe99fb6bd1999b5" => :mojave
     sha256 "d58d97a203abef2dbfbfc32c1eb8dfbdd50ba0c5277b40511a9b3361f02490a0" => :high_sierra
     sha256 "f6f3b20fa00c6c95295ba4ee5e0cbb84f87a70e33650e14d88887fbfe2ddaaf7" => :sierra
     sha256 "b3e1d1dd273319bc165b9e25736353298786709ec55d459b6390944fc856b8bd" => :el_capitan
