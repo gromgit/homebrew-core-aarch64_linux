f0be3878f59aaa2b8aba054ab8ce29ddc6944f9d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 11:03:38 +0000

    editorconfig: update 0.12.2 bottle.

--- Formula/editorconfig.rb
+++ Formula/editorconfig.rb
@@ -7,6 +7,7 @@ class Editorconfig < Formula
 
   bottle do
     cellar :any
+    sha256 "35354362ec018a97bf33a912114c4720774c8875a8637465e0058728ecaff631" => :mojave
     sha256 "af22a9ade368239770c4e31b492755e432d4d588ae5b537fd05b05434fdf9624" => :high_sierra
     sha256 "fa30d3abbe6bc2e740050aa7f762de370edeff44b34b2676e77607ab34cc72a0" => :sierra
     sha256 "51227b53ae32e062299baf504cabf4ebd9291c07c1a28b620d26493f3019817d" => :el_capitan
