72f334cde11ebf9fb8e12b69612cceaf86a7984e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 17:49:10 +0000

    csvprintf: update 1.0.4 bottle.

--- Formula/csvprintf.rb
+++ Formula/csvprintf.rb
@@ -6,6 +6,7 @@ class Csvprintf < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "42e857e3fe76d351204fb76fd017a257f3362382af49f6c7c03e156af6bd5bb0" => :mojave
     sha256 "99340f4265ad7a952d3f4fdcce154a10e46c7c8c06979b9e7c5d5016810a8c87" => :high_sierra
     sha256 "236d5de27a77cadffa4cc014b9f2416b7d952f8cedb96ced98096f0bbdc928cb" => :sierra
     sha256 "79c0236e6eeb86a283831f3123430f700862d229bc0b5d64b0a0251d8832e092" => :el_capitan
