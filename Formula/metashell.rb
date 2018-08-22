a138bb60d13801bc936dfa0d6c406bf1391d5e0f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 21:32:32 +0000

    metashell: update 3.0.0 bottle.

--- Formula/metashell.rb
+++ Formula/metashell.rb
@@ -7,6 +7,7 @@ class Metashell < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "eea9e8e3467f067df5c18021e70d529fca81444b1b5a58987d585fea9c14e234" => :mojave
     sha256 "ea62126c211e42451b2272b250b877c2714c470a654d4d81dbf40a26c79fd4f0" => :high_sierra
     sha256 "b2ed4c805b490c689c9d2f8f8407b19a936a9d8bcc26b167d3a7727b1d503a5b" => :sierra
     sha256 "4d55dfb291e53beb8397f88d49121f8fb403c451a5f0eea05b819240449d740e" => :el_capitan
