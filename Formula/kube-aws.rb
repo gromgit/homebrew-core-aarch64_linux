063ca8790c2773886d4f65cb292e99ffc795276c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 08:58:40 +0000

    kube-aws: update 0.10.2 bottle.

--- Formula/kube-aws.rb
+++ Formula/kube-aws.rb
@@ -8,6 +8,7 @@ class KubeAws < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c1813734f98b1a08a20bdb776d258b8f772d23a56e510494555600b8111f886a" => :mojave
     sha256 "ebc6c3d0bada00c947dbc928516bb2419779d234bd8fc5da3a79b6d6559c7340" => :high_sierra
     sha256 "35ec742eb54ae1ec5a874871bce526a94a73b920f8d2ed0a2294e37518d1ed04" => :sierra
     sha256 "8f946fae1cd903ca0125f4f3b655c96ed96a1c0970581a8a993ee31093bcadfa" => :el_capitan
