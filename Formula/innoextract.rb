a46e6d18e4d1ceae7ff30aea49705d7cb97580ed
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 08:58:41 +0000

    innoextract: update 1.7 bottle.

--- Formula/innoextract.rb
+++ Formula/innoextract.rb
@@ -7,6 +7,7 @@ class Innoextract < Formula
 
   bottle do
     cellar :any
+    sha256 "5830af5a88bb0556f417c8138932d8e0a05f037cc69948ec2fa1b78ca69ca1f3" => :mojave
     sha256 "335b723497138e18a8b9b6545c646a72cf4fabfd45aef47959b1a3c37043aab8" => :high_sierra
     sha256 "d0945359761fca563e0a2c42f8283db4d35edfb6c43f9de1865c43a43bd08573" => :sierra
     sha256 "e4df8d0d57062480392e178596d0fba27121af745ba5651a88fd1059330d7129" => :el_capitan
