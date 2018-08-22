a2a162905460d9806cb35d66ec949648c166f48e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 11:03:37 +0000

    mogenerator: update 1.31 bottle.

--- Formula/mogenerator.rb
+++ Formula/mogenerator.rb
@@ -7,6 +7,7 @@ class Mogenerator < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c9beea63b98941e36f8beae4afb36fda4ca8efe3e4d74d0142efbeaab4273b86" => :mojave
     sha256 "b5313f7dd8eeb5b379f6c64615104c23250b97c7c05de779427a29c573464f32" => :high_sierra
     sha256 "19c0ec0b7f39deec9c99f4ede5ae1538acad0b3768db7b0557e15660efb41789" => :sierra
     sha256 "eb94c192bb659183c72f970f1230948ce875ed49b53f0e7bb540660e7dae2353" => :el_capitan
