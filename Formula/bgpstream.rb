5f32dab7084cf70239336123cdc109a45b273ac4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 18:20:55 +0000

    bgpstream: update 1.1.0_1 bottle.

--- Formula/bgpstream.rb
+++ Formula/bgpstream.rb
@@ -7,6 +7,7 @@ class Bgpstream < Formula
 
   bottle do
     cellar :any
+    sha256 "5de3479fc0a9c0d4e97c852584ac29f0b0df86302dba981c1120a6b94bdbe58b" => :mojave
     sha256 "f6db5e754c3c44b70aa1f2af5436fce55ec8046d70d51fcce8f6f2f7e4bfa4a8" => :high_sierra
     sha256 "56f712d3a9f3540b0056c9496c9a038b195eddcde7656cf8599d3a450858f12f" => :sierra
     sha256 "28b012b8368630b46e02d0d94c9099e06db88022f02ce0bee6596fdd21755649" => :el_capitan
