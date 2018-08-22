5d457168affb1c68552258d1ff0de2307a15040c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 20:43:06 +0000

    cconv: update 0.6.3 bottle.

--- Formula/cconv.rb
+++ Formula/cconv.rb
@@ -6,6 +6,7 @@ class Cconv < Formula
 
   bottle do
     cellar :any
+    sha256 "ffaf8b5cab0618e52cfedff14a5084cfe54e0b1b6480433e2ffb4beee8e47ec9" => :mojave
     sha256 "c4d197f979340a89d5a87e05eae6a39db38863f89b6ddda42f924472d87a5b0d" => :high_sierra
     sha256 "2e885b9571a8814f2b23b088f3f0d45f47b1fe762f040c3e66b1a81f84673646" => :sierra
     sha256 "bda78602260276dd3e5187a5a9d6bbcfb95ff40aa513840569e490d5dc96aab2" => :el_capitan
