8a9f19cdcc0b7cca1f0231de5b9006965bbf7d6e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 09:14:39 +0000

    source-highlight: update 3.1.8_9 bottle.

--- Formula/source-highlight.rb
+++ Formula/source-highlight.rb
@@ -8,6 +8,7 @@ class SourceHighlight < Formula
   revision 9
 
   bottle do
+    sha256 "ed1d149a9f97855caee3f659c371a1478b68454efdadee059e34597218b982fe" => :mojave
     sha256 "88e245af7dfafc7cc0419fab616aa7d1ee16e3f47a36eab9e66bed809bbcd079" => :high_sierra
     sha256 "2889da5f2a4ce82b711fc2903e888e9410a8ae4d93fd26885b8d6b0b6b178505" => :sierra
     sha256 "85f7a45cd5ebde9700d332e72395a116287579e29ac000c80f8cb3fd6c8f843f" => :el_capitan
