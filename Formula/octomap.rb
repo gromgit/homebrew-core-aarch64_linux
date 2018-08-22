3b2b5110e1aa5e00865edb180113ae84ff81a8e1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 12:14:28 +0000

    octomap: update 1.9.0 bottle.

--- Formula/octomap.rb
+++ Formula/octomap.rb
@@ -5,6 +5,7 @@ class Octomap < Formula
   sha256 "5f81c9a8cbc9526b2e725251cd3a829e5222a28201b394314002146d8b9214dd"
 
   bottle do
+    sha256 "ebcc39c4935eea656d1c9b3f4f8e4360b2cf559255cec9d84c8cf22d0ac6f661" => :mojave
     sha256 "3e72d0af26510eb79310138dc80a2b2dea541e90ac58a953e5294251bca14728" => :high_sierra
     sha256 "eb292c0c7005c2fd75b65f50bc2a361181ee7c3c5113f6da56f80b4f03bf75fc" => :sierra
     sha256 "d29abe7987fdb2901996e8e6c4aaf2c8a0daeb98b6cee2a585f048769082a4ad" => :el_capitan
