8ea01a52767c2a58799e0df1e4874ca4622b5c9f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 18:20:57 +0000

    bogofilter: update 1.2.4_2 bottle.

--- Formula/bogofilter.rb
+++ Formula/bogofilter.rb
@@ -6,6 +6,7 @@ class Bogofilter < Formula
   revision 2
 
   bottle do
+    sha256 "97a2da30e1c196c31eadbe5a3404258c79fdc9caed9b7c1daba9497442b02aee" => :mojave
     sha256 "188368373c8bb9719a459b0f7905818dd4c8ce4303a59152cd47cc43557ff10b" => :high_sierra
     sha256 "6366c9e1c254bbf94d2ada013d13fc78dc56c190e1ad148d5a0dded5bd263dbe" => :sierra
     sha256 "9c717b7628a0c0428528338880d6cd5d480b3246c3888cbab5a87d3b8b3689e2" => :el_capitan
