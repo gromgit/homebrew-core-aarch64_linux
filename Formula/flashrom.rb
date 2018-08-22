eb645ec3089c143cde1476a48fccacf18e47f23a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 15:37:22 +0000

    flashrom: update 1.0 bottle.

--- Formula/flashrom.rb
+++ Formula/flashrom.rb
@@ -7,6 +7,7 @@ class Flashrom < Formula
 
   bottle do
     cellar :any
+    sha256 "268fed961b0db79dd20c9d7d0618d80a2b8fb8b81d19b31911bac5b61b89f557" => :mojave
     sha256 "9b52a8eed203c15987554132d532210c22e97f9a0eab02d842830c7339b544b7" => :high_sierra
     sha256 "ae6bb14a2a03c1a44515702f01c06e49e2526e831f0fd7e7cee3761a9bdfb6cd" => :sierra
     sha256 "7a41ea01cb4dfa262082f96f8a20eeccb45117aee72f806643a9783d92b247ef" => :el_capitan
