86badf8c07d4065a50ec6923795add7178ffed8b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 08:31:45 +0000

    lxc: update 3.4 bottle.

--- Formula/lxc.rb
+++ Formula/lxc.rb
@@ -6,6 +6,7 @@ class Lxc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7586204cab5b2bb28c01e393569f89ec6c9df7bcad35147d7749db36285ea38b" => :mojave
     sha256 "c6df3c61a2e3d6345f5d219e0153be0c2bddea141101aa20cfb5649851a82c08" => :high_sierra
     sha256 "7584a5da68b9f3be6febf6e88c3353fc51a9d3d7d3ced74da1a8609cf2fd224d" => :sierra
     sha256 "49e32a3821d790f65ee7017ad40e0fa1a6a84af56f509d0e86cc17051c2a754d" => :el_capitan
