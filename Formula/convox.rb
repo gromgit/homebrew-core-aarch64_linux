7b0c277c53e12e281335dba2d587c31ee6ae3455
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 12:38:19 +0000

    convox: update 20180815173013 bottle.

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -6,6 +6,7 @@ class Convox < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1fc678e1656a1a00dc3c9aeaea76f6114bd5fec9c0f40b520a1a25c97ce946d9" => :mojave
     sha256 "9b88842d39f15077debfbad25af2bc8dfe265ee0af308f56d33f68b0ffdf2be2" => :high_sierra
     sha256 "387ba32a7a03842f90d6569d772358ad956137242318195dd3fcd60ca580f9a2" => :sierra
     sha256 "26638eaf61040da3066ad620b7d2df6a48a83cf156139cf26ad258307d98f5b6" => :el_capitan
