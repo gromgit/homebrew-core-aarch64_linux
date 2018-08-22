346915f0263267b2a7a9a12ed5549987ab0a545f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 08:31:44 +0000

    ddrescue: update 1.23 bottle.

--- Formula/ddrescue.rb
+++ Formula/ddrescue.rb
@@ -7,6 +7,7 @@ class Ddrescue < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1c229db4c0e74144c629f0e95014216468d05eae87ded6d5d730745edd98c51b" => :mojave
     sha256 "cea23ba7f2730135b634d896d835c9c55572900fbc8263993697e1273b67dfb0" => :high_sierra
     sha256 "b0759371cbeedf705c56867910da5536f4aa8b6560d2ebdf8fd6c9f2ba71199e" => :sierra
     sha256 "eab68f77a7570e59f007ea4b6fe47b9882c24ad2b626db375f9f42888476cf5c" => :el_capitan
