081a069ed2b9b7126f556ce2930319e82aaaa53c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 16:03:11 +0000

    minisign: update 0.8 bottle.

--- Formula/minisign.rb
+++ Formula/minisign.rb
@@ -6,6 +6,7 @@ class Minisign < Formula
 
   bottle do
     cellar :any
+    sha256 "b2862f35643f396fafff03c76697b2d25b8907bd14b4dc4c31c913b60ab88c82" => :mojave
     sha256 "6826fa79c308cc321871704d79baab4deb71e916c00656c0b6cc656042889103" => :high_sierra
     sha256 "bb3f8c4d58e195960606e1f155775da72ba7c019d9dd72b818ed7d232bf4e22c" => :sierra
     sha256 "2884120953aeac755a39365d5905ece08b90b1bfd9568677cd1ed45046fe8491" => :el_capitan
