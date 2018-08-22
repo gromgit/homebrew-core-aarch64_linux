df3b501f775337f589a6816d4a39b7db34f2155c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 17:32:18 +0000

    catimg: update 2.4.0 bottle.

--- Formula/catimg.rb
+++ Formula/catimg.rb
@@ -7,6 +7,7 @@ class Catimg < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9ca0b3ff26790b12fde4382bba62032d422b0c8148817faf553bf0fa850dc37b" => :mojave
     sha256 "acdb443c28828a2516dd31976f04e75cc849a4db282a177752a99385d925af55" => :high_sierra
     sha256 "b003fe4bb7c6f605143cfae9d061abb64669759bcb691c7bc063b6a71409f7b8" => :sierra
     sha256 "6bc7df392cf75f39e2d0a81080ebd7ca56f6a878c3108617a9831cdb74dffacb" => :el_capitan
