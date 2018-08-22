fd31bb507a2445430855739a370fe06aec88bf00
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 16:03:12 +0000

    docker-ls: update 0.3.1 bottle.

--- Formula/docker-ls.rb
+++ Formula/docker-ls.rb
@@ -7,6 +7,7 @@ class DockerLs < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "eb0ddf264090d9380c760768d5e46c970b43b1c73256e4c899cfeaf5a3dcdad8" => :mojave
     sha256 "946e27ec3c35ec19d98c1a8efdf59a809db23926f97d3cd98b195d15c28491cd" => :high_sierra
     sha256 "1815a65eab86116dfc1de21331352348d59f391cdbc2ed1454a1847f62b21845" => :sierra
     sha256 "d011c19d2e36dd798ffa5723448cfeb1d95c25b7e78ad4b64207f4c79c5be383" => :el_capitan
