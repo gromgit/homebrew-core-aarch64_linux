eea44928f3476bca9c0ab6f91bc6f82372eb2a7a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 15:15:46 +0000

    swi-prolog: update 7.6.4 bottle.

--- Formula/swi-prolog.rb
+++ Formula/swi-prolog.rb
@@ -5,6 +5,7 @@ class SwiProlog < Formula
   sha256 "2d3d7aabd6d99a02dcc2da5d7604e3500329e541c6f857edc5aa06a3b1267891"
 
   bottle do
+    sha256 "5076f120b7f2775fc0968885d2d0e82cb7a93f3040c1c39243abdd8ec3ba1e59" => :mojave
     sha256 "7a1a76d4b9160e0fea1899a8af0dcd448f71efef8476b1732d75e8d0339ac419" => :high_sierra
     sha256 "af00bfcc0da68a800dd50e608aabc6620db00de1a7bf1b986a7bc49ae58ea234" => :sierra
     sha256 "2016d9b076b252805f48f705181d03cd26183b0f74a026c029cd34f9e8afb79d" => :el_capitan
