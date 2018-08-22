036b59c2a63897f94bf752c83c25eda6ee9b14a4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 12:43:16 +0000

    toilet: update 0.3 bottle.

--- Formula/toilet.rb
+++ Formula/toilet.rb
@@ -7,6 +7,7 @@ class Toilet < Formula
   sha256 "89d4b530c394313cc3f3a4e07a7394fa82a6091f44df44dfcd0ebcb3300a81de"
 
   bottle do
+    sha256 "27c9e1fe38ec012c5dd9199c8100d49c56e386c65c336a4fbcaaa25a9341cab2" => :mojave
     sha256 "dda87a313d7398dd3157ca74d752b3d364647fc56c3238fb5bd320fcc904ebd5" => :high_sierra
     sha256 "24008d251358aa73e7e597b203e360857fec5b88278e6ea6de08d4eef3865f80" => :sierra
     sha256 "93822fde3d2e69f46143dcb9d8551e7e4301c7a470ae53b3fda8ec6cb44584dd" => :el_capitan
