84c1ee3e7b4bfe58df0f1d16e9f059763e732108
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 15:35:28 +0000

    fwup: update 1.2.5 bottle.

--- Formula/fwup.rb
+++ Formula/fwup.rb
@@ -6,6 +6,7 @@ class Fwup < Formula
 
   bottle do
     cellar :any
+    sha256 "0291bad0603b96826a9518710b41261b17cd093d0f78bb6a4a80b7113e72465a" => :mojave
     sha256 "38de88b1f028e49c4405eb2e9553efb4a18443ec33e67536c820aa0249084c11" => :high_sierra
     sha256 "14af77644be3f531267721495c8e26a99ff95fc7474ea316be9138f8e6b5ac8a" => :sierra
     sha256 "86fede8a00c63a115abb4845152f7eeb9ebdbfe016eb6551d2d25dfed3a65084" => :el_capitan
