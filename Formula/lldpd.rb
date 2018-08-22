2bdb62c96c67d92cd0cac8aa3e60c581ee2c433d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 12:38:18 +0000

    lldpd: update 1.0.1 bottle.

--- Formula/lldpd.rb
+++ Formula/lldpd.rb
@@ -5,6 +5,7 @@ class Lldpd < Formula
   sha256 "450b622aac7ae1758f1ef82f3b7b94ec47f2ff33abfb0e6ac82555b9ee55f151"
 
   bottle do
+    sha256 "1c66fc798da96bf31629f986bb396e91a81541087b833b6adb59a98919b4819d" => :mojave
     sha256 "d770ef1c3507d3d2373f8706acf4f8d3c41ae07f594192db02483184e497c510" => :high_sierra
     sha256 "fd38d0022374f5960183ec7afde1a5c9ffc85047c017a667f09b92cda25b1c7a" => :sierra
     sha256 "1158b121a76df10a688d9907ab0843886ce7798f6d54ad8eaf9d21b17cb7b01f" => :el_capitan
