959fab9d4e6ea783ba1bf84b9c93c18edcf4d6e2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 07:49:23 +0000

    xml-security-c: update 2.0.1 bottle.

--- Formula/xml-security-c.rb
+++ Formula/xml-security-c.rb
@@ -6,6 +6,7 @@ class XmlSecurityC < Formula
 
   bottle do
     cellar :any
+    sha256 "a4c67e70aa812e3801f0a06036bf9789a7d83067894e78633ac2afe7d407fbbb" => :mojave
     sha256 "0abac2dae270972195b05a448fbdbda33715debbc6d4dbc8f13635fe677abce2" => :high_sierra
     sha256 "775625bac9adfbfc9112f82f70e716c4d8053354ac62de4bde39416ff4bf7a7a" => :sierra
     sha256 "a1b7250c76b65481fdb7d4f368ade7111ec495dde8c77b3739887c7d17415303" => :el_capitan
