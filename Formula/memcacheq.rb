76cf983659bf0fe09c0f98d615ab4c99afdcbbd2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 19:28:12 +0000

    memcacheq: update 0.2.0_3 bottle.

--- Formula/memcacheq.rb
+++ Formula/memcacheq.rb
@@ -7,6 +7,7 @@ class Memcacheq < Formula
 
   bottle do
     cellar :any
+    sha256 "5b7bfd2266c6573684654167ae9d62d4e0c7132e3ee718e65203dde90a40f67d" => :mojave
     sha256 "cd1b3c570a517706bd3f96db3299dadfb5451d82130a9e781f522aafc0b4e924" => :high_sierra
     sha256 "f8799f00891f07bac41323e83d6c78cadc49c4a00946edfacece742f0730f7d6" => :sierra
     sha256 "babecb3a320c440e7aed14d3878cc9b65fd954263dc0707dbbeff7fa2aab7abe" => :el_capitan
