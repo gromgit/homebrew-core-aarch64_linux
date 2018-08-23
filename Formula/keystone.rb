804df30a3fbd534d4052c7c7a6f8bac896a1724e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 23:40:10 +0000

    keystone: update 0.9.1 bottle.

--- Formula/keystone.rb
+++ Formula/keystone.rb
@@ -7,6 +7,7 @@ class Keystone < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c914d5799915bc7c40fa1e82a407d8e40dcc583c904a3da24042b1a4abdeb5bc" => :mojave
     sha256 "bb281d9882f991ce15f0c7c421af9af9ed7f9ac1d563bc6bbe5ff7ce5352617d" => :high_sierra
     sha256 "b6cd1a7208fa16627366dfa4ba297edbf1dba6baf01a031d830b292e0e6cd019" => :sierra
     sha256 "25a45e702238530539973a7a28ffdfe5aa512be3cd639b3247895d11d6f9576f" => :el_capitan
