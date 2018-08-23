c7fd91ab6ccb57340f8a939dd30254b5f03419df
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 18:49:17 +0000

    dvdbackup: update 0.4.2 bottle.

--- Formula/dvdbackup.rb
+++ Formula/dvdbackup.rb
@@ -6,6 +6,7 @@ class Dvdbackup < Formula
 
   bottle do
     cellar :any
+    sha256 "65cf4593546ec639bfc42a46d9f22a3e54ee8bc6496581c0cb15cb3678fecb3d" => :mojave
     sha256 "c1451c94d778d2096df258679da916279cc117642128196cb487ac67c4c58f01" => :high_sierra
     sha256 "b47019e2fbfd855f1658b3edef96bf5a6794e6d8d6a863278b4512e4490e7e51" => :sierra
     sha256 "5f2f6cffd99459f429f53731c03528972648816997818bb4b8ec9df0a96c4d01" => :el_capitan
