05bf213ef4011ec8991bbd0813746f4b4b32432b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 18:20:35 +0000

    dgen: update 1.33 bottle.

--- Formula/dgen.rb
+++ Formula/dgen.rb
@@ -6,6 +6,7 @@ class Dgen < Formula
 
   bottle do
     cellar :any
+    sha256 "7ebcef39d27013a647585ab076ca31fc1be468c85aeafe2d504242b4194a3d2e" => :mojave
     sha256 "bac08b08f7cfb9c108ccf0bfe2d4623324e5038f01e508c1fb5da6b3c4d58dff" => :high_sierra
     sha256 "50383807ec76387aa156cf6157ea537465bf20ad35e4e9eddda7d34685ded635" => :sierra
     sha256 "ebcab68ba8d0aa9c6aacae94d43a67ce016dcdd219c5770c3b7d6d9c3590ef9f" => :el_capitan
