fe3334485b5d15a55c568c429e76f1f59797033a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 11:03:32 +0000

    flatcc: update 0.5.1 bottle.

--- Formula/flatcc.rb
+++ Formula/flatcc.rb
@@ -7,6 +7,7 @@ class Flatcc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "90786b882f51b0a90df7a1297159edba31a2e99c2d67b04440fb31938494355f" => :mojave
     sha256 "17b33c3326512047102e7055fa8d7d8b8b6781dde0ed65dd2c38525283f1ce5c" => :high_sierra
     sha256 "972fd44d9828746a29c4eef6f64cfa5fa67508601384117b979d39837e2498d7" => :sierra
     sha256 "ea29799a542aa866c8fe1e5f9416e7bd274edf74da7f400c1d63690c1acd5930" => :el_capitan
