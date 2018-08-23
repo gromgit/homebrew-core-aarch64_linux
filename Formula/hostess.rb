7a1d9f4d53290f3cfee216a8476d2351f64c8605
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 22:51:00 +0000

    hostess: update 0.3.0 bottle.

--- Formula/hostess.rb
+++ Formula/hostess.rb
@@ -7,6 +7,7 @@ class Hostess < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d9d5adeeb0551a9bb230b09b8463aa098abf42e2e5df4392ed7ae8236c6b4ce2" => :mojave
     sha256 "955705aca89353471ac63e07a9254ca4252546c8071eda85a8b95ac6aa0b6331" => :high_sierra
     sha256 "6a5ca47a7d0047d1595c79008ffc5d9a43ce8ef11cea5b6fc7deeaa239102216" => :sierra
     sha256 "e77cffa19bdf7feb4973d59c4aa0f1551dc74b8caaf35c8a6330e71e223e2bdf" => :el_capitan
