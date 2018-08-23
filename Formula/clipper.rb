1e022b1038d931b8fcf2946848c20447b20000e0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 17:15:00 +0000

    clipper: update 1.0 bottle.

--- Formula/clipper.rb
+++ Formula/clipper.rb
@@ -6,6 +6,7 @@ class Clipper < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "787838cd79467dfd50a8fffd58c3b1572d0e8571b7e8ec4c2f9a62ac3d66e4e2" => :mojave
     sha256 "fe416ac6365e397532b5c16f472a1b526cb990404582b1f0f7289bbc24338755" => :high_sierra
     sha256 "8fdbf7ebc996d137fe3fef2132c47e050b81fc8a0bb0e01d693b75d6c0e9d1b5" => :sierra
     sha256 "cbd9145484b4792f23b3da4f8da672af188ae8fbf294d20266d39a669d0a4b29" => :el_capitan
