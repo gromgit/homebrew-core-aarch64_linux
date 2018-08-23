a3fe4c715ef4cf93c4647d69e91b98a50882ce3b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 09:16:33 +0000

    ctunnel: update 0.7_1 bottle.

--- Formula/ctunnel.rb
+++ Formula/ctunnel.rb
@@ -7,6 +7,7 @@ class Ctunnel < Formula
 
   bottle do
     cellar :any
+    sha256 "7d3debbf6a8fe8155e846e255e8330502a400c4d2da4d1c30c038fdbb9ba30cd" => :mojave
     sha256 "9a2394bdea7bf5ca99f5e92f6d99e8729fc6e81da0d6353f22cefe3e8df4baa4" => :high_sierra
     sha256 "dfcf3349529b7b2ab47298a9090d916be9eb3b9c0d72cd644f0e4b1de09796be" => :sierra
     sha256 "6a5de1059ec2e138e68530fd62f51b102e29d6ecfb83eaa22e3f79bc0d9acf4c" => :el_capitan
