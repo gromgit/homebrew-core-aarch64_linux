27f110780936aabb33d92d9c249067ac70ed36e5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 11:19:11 +0000

    remctl: update 3.15 bottle.

--- Formula/remctl.rb
+++ Formula/remctl.rb
@@ -5,6 +5,7 @@ class Remctl < Formula
   sha256 "873c9fbba51ff721acb666e927f58f4407f08eb79f53b5a058801f5f404f4db2"
 
   bottle do
+    sha256 "f2497816a9b4f36e804de521937b994292369f2c28099430cbc81e00ecfd8f45" => :mojave
     sha256 "699be918b80d41c46ec8aabb4e04219123fec8beb60f1169c33b8a14ef7e24c1" => :high_sierra
     sha256 "3dfefe916442dcae4f049f56673ce735ee84b931423b184f4d7fab770d7a5f39" => :sierra
     sha256 "301065db3a4020b2237268eee8709a639d33dac24b21a3294641f7d9beef0ab0" => :el_capitan
