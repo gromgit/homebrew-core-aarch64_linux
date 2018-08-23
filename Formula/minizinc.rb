497cbb28c62b26c564f4ae3ec227313d82a96cf6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 11:19:09 +0000

    minizinc: update 2.1.7 bottle.

--- Formula/minizinc.rb
+++ Formula/minizinc.rb
@@ -7,6 +7,7 @@ class Minizinc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "845c90c782f19894a72e3c6b3022d9aff95bbffb35a126ee4aa022c2274bd794" => :mojave
     sha256 "970176e584ec4e246b2e2435e14f95037a2a3d9f607c6ff29d6e024ba5371e8a" => :high_sierra
     sha256 "d4d6ce59e834a8c3429ae4d1ce46063148d7c07b9fc02aa30d4cb8b27d2cf469" => :sierra
     sha256 "d76a0ec9eac191b1aee519540dcff60a40400099f5c1a3b0fbf5880340c5bc5e" => :el_capitan
