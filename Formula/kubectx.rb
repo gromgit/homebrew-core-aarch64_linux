98958e872552009a7ac908bb83b5944df43c86bc
Ahmet Alp Balkan <ahmetb@google.com>
2018-08-23 10:25:28 -0700

    kubectx 0.6.0
    This release has changed --help exit code from 1 to 0. Therefore updating the
    test.
    
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>
    
    Closes #31398.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/kubectx.rb
+++ Formula/kubectx.rb
@@ -1,8 +1,8 @@
 class Kubectx < Formula
   desc "Tool that can switch between kubectl contexts easily and create aliases"
   homepage "https://github.com/ahmetb/kubectx"
-  url "https://github.com/ahmetb/kubectx/archive/v0.5.1.tar.gz"
-  sha256 "05bdd786a91d1b16c6792c2d68029b4400f1da59b834206e5e2e61255d1cfe64"
+  url "https://github.com/ahmetb/kubectx/archive/v0.6.0.tar.gz"
+  sha256 "ce289e4a781d248a6474a2c79e55c067a037f5c9bc84dd2c82620a346fe77c39"
   head "https://github.com/ahmetb/kubectx.git"
 
   bottle :unneeded
@@ -24,7 +24,7 @@ class Kubectx < Formula
   end
 
   test do
-    assert_match "USAGE:", shell_output("#{bin}/kubectx -h 2>&1", 1)
-    assert_match "USAGE:", shell_output("#{bin}/kubens -h 2>&1", 1)
+    assert_match "USAGE:", shell_output("#{bin}/kubectx -h 2>&1")
+    assert_match "USAGE:", shell_output("#{bin}/kubens -h 2>&1")
   end
 end
