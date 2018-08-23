455c38b765a7d1ec29579040ad0181ae4216c919
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 20:56:45 +0000

    gor: update 0.16.1 bottle.

--- Formula/gor.rb
+++ Formula/gor.rb
@@ -8,6 +8,7 @@ class Gor < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c7c9758b1a058ae433baefbc0e9d02cb84e2e5e6df16a9f5b376cac0a3e9df93" => :mojave
     sha256 "204f649341531e36b220221f6dd76b3b637ea4880720111cddda6bb5224be5ed" => :high_sierra
     sha256 "78689bec0668532515a42e5274733ad296998e0e623bdbd3bbd66d2d0fb8f1e7" => :sierra
     sha256 "dd3721a8686fb9e08074a6787d2bbefc5d3f3a585b99e52f40734a4516564754" => :el_capitan
