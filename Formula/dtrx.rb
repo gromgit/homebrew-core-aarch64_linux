3201e845dc6b73e00c4264e2249571b6c330510e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 18:49:16 +0000

    dtrx: update 7.1 bottle.

--- Formula/dtrx.rb
+++ Formula/dtrx.rb
@@ -6,6 +6,7 @@ class Dtrx < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c88f2dfca39e978e4d49d24e88fcdb479936adcc4773cec913b312a0c6f4dc0a" => :mojave
     sha256 "c159f3897c3de8b840beb2dba4aba058262b932b06650ae72ef4c86841fcfdbe" => :high_sierra
     sha256 "812e92d3899f795921dced2d1085dec1258a659595e1a0545e67a03be653b9ca" => :sierra
     sha256 "5419cb8b5327a5770d1d8a541a11c47f9195885964af3977110a4bd752a63ff7" => :el_capitan
