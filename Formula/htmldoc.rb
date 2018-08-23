84e40577c49ea2acdffb7de021b32d43b2f9ed71
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 01:39:10 +0000

    htmldoc: update 1.9.3 bottle.

--- Formula/htmldoc.rb
+++ Formula/htmldoc.rb
@@ -6,6 +6,7 @@ class Htmldoc < Formula
   head "https://github.com/michaelrsweet/htmldoc.git"
 
   bottle do
+    sha256 "293e519b1f45009a7af3751e6373eb5e001b233d3de349b47522b48206d374f0" => :mojave
     sha256 "127a8c2832a800ad89e5925cb0a34b84a405c535d46267b80e8c53eecc363f2b" => :high_sierra
     sha256 "bab767a8ea4093422d480a9fde4d0156eb9509f3bac026886f174d43cfdf325d" => :sierra
     sha256 "7a1a44691173eb534dad23b2991fc7f0d4cfa2471dba7cea1d24ab16ce058f58" => :el_capitan
