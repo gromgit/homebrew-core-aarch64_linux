7151310f871a5846013c0c950f564e4e947595ab
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 16:00:53 +0000

    aptly: update 1.3.0 bottle.

--- Formula/aptly.rb
+++ Formula/aptly.rb
@@ -7,6 +7,7 @@ class Aptly < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1d648543b78ab122fd96276a9e70f3edf90bbabb4b6190ab9a742262ee9e4460" => :mojave
     sha256 "8ee9d2d3b4c072cad39487f6dfcf37189c4258eed4496845d20d8621806ae0db" => :high_sierra
     sha256 "6425b01749d2cd2c6e9e748e59b71f26a47945d43a300858a305179dc370d61c" => :sierra
     sha256 "64ad68557e92d24163204792cee94b606c30f3ca584979a1f4158cdec102bb9a" => :el_capitan
