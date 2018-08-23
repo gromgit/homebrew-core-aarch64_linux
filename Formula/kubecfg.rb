3da46f9c792cb818b60eba0ce7c91933058e0fdc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 23:44:58 +0000

    kubecfg: update 0.9.0 bottle.

--- Formula/kubecfg.rb
+++ Formula/kubecfg.rb
@@ -6,6 +6,7 @@ class Kubecfg < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "74b6a842325a2c17866bfe74c7183c34bddb53cfa0af4d56ea0bc972f4d8cfbe" => :mojave
     sha256 "c70bb19117bf8fc90b8d6ca73beb6606dc2be4078c48168d574358298e70f43a" => :high_sierra
     sha256 "04fc44f3869f25d396791233ffae3ae50a172adc4fc2ef4b70fe70931f54f454" => :sierra
     sha256 "b1e86a9bd8512770cf754280962870643d370f06e7eb962babbe15290bacb6e3" => :el_capitan
