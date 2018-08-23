0a74311a70fb5775b1529e43f1d246cf56d0ca46
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 11:14:21 +0000

    webfs: update 1.21_1 bottle.

--- Formula/webfs.rb
+++ Formula/webfs.rb
@@ -7,6 +7,7 @@ class Webfs < Formula
 
   bottle do
     cellar :any
+    sha256 "867fbe1f213380efb695d89fe6d4cc3e5197d2d303b3d70da72a45814aacc669" => :mojave
     sha256 "bd713d90ce0e67a1cddf2b3274ddc7467e4b7b5c8ad8b80382e5c0c955c383c3" => :high_sierra
     sha256 "cbdc088ed6befb87ecda08bf4a23a9eff3bf3d61932d98aa1f8441bb1c0fd3b5" => :sierra
     sha256 "63755af9ac88711d4eb2e80016ef541ed40a1422f2fb2de3ded5c72b362cb39e" => :el_capitan
