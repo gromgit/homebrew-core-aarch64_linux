8dbac312a55b40de5be1184c28f7cb00d05be140
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 22:42:08 +0000

    icbirc: update 2.1 bottle.

--- Formula/icbirc.rb
+++ Formula/icbirc.rb
@@ -6,6 +6,7 @@ class Icbirc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c353062cf16183b658ca999e477f2f4ac6040dd8d3a995fe2736a382d989ca8e" => :mojave
     sha256 "e258e2ca2bf835d76b7d509eac5417629451068c85fe729cbab7fc64e89df9c0" => :high_sierra
     sha256 "cbec4e472c640a63081f12723fc9d144f00aa00c9229ce5bfc2edd99199aee74" => :sierra
     sha256 "2f943e4af7a9c1e2524d9583b0ef5539988f68f56a8f8c483b2c2d1990fff21d" => :el_capitan
