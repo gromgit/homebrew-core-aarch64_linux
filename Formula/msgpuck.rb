4c78b26ad8e9b8f727d26f1c2452699dac688983
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 09:35:40 +0000

    msgpuck: update 2.0 bottle.

--- Formula/msgpuck.rb
+++ Formula/msgpuck.rb
@@ -7,6 +7,7 @@ class Msgpuck < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0fedf815d4ba46d10e5fe7910cbcc06f1ea2906e40a4ef994ffd3aa04289c423" => :mojave
     sha256 "50197e08a5b55fbe804109ad01dfa815a6dde2b11b688d89a58154fed2d8d54f" => :high_sierra
     sha256 "6f4011d177bf2e42f94f853bc93283ada6c48df8fdb7269135def453e65e598d" => :sierra
     sha256 "b0accfedd2582109acec3297878bb943360282520a31b0d1c16c4ec1aa70a362" => :el_capitan
