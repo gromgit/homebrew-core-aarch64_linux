f2f6f6486940f72b91216669e9ad09ac2f2c0989
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 08:18:33 +0000

    moe: update 1.9 bottle.

--- Formula/moe.rb
+++ Formula/moe.rb
@@ -7,6 +7,7 @@ class Moe < Formula
 
   bottle do
     rebuild 1
+    sha256 "b1b91b2a30d506381f31c96f63e75bc78e32bec6051dd2e62d78fa19775975b8" => :mojave
     sha256 "8795bf38abe1ee929d3573e25bd54bb73220d4d2b30d0f2af4b6242ae5f862f7" => :high_sierra
     sha256 "c5015c950c74f73093aacac9e9f3b42f741b5bc57e864006c2a62f3c6739c538" => :sierra
     sha256 "4b7e08c089bcf431efba6be217df5a30e7ade7fa2813ae53152e5671958abb69" => :el_capitan
