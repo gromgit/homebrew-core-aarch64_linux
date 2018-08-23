f63e084c1a3cc44a1cd511f6c89f234e1a0e1825
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 20:52:53 +0000

    gts: update 0.7.6 bottle.

--- Formula/gts.rb
+++ Formula/gts.rb
@@ -6,6 +6,7 @@ class Gts < Formula
 
   bottle do
     cellar :any
+    sha256 "b06c43a504c4eb51317113e7c2849250bce0110be6ec9c7df832ae0d8e8e4771" => :mojave
     sha256 "d52dda8d2163b4116aa2bbfd9ea87cf468e58933059529f18089e7b82dfdeb95" => :high_sierra
     sha256 "00e18cd7e72fe3d9c2dbb4610d1bb6c2a7653b8831bd3b4b15dd8c2736ee62db" => :sierra
     sha256 "c958735937a398843e9a2b2cc1a1b9bc5305834de4257f1715a12dcc84b25f75" => :el_capitan
