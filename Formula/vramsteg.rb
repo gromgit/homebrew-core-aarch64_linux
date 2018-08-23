9876db14239710a07abc6d60f7612697a0f379e5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 08:27:41 +0000

    vramsteg: update 1.1.0 bottle.

--- Formula/vramsteg.rb
+++ Formula/vramsteg.rb
@@ -7,6 +7,7 @@ class Vramsteg < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a868fba582ce440a14ae18d4be193209e7d25fd3291b568bea7f123e61aa044d" => :mojave
     sha256 "0c9aff3582ad05a388cba8c43770ead295d921a8e419323a3c4115f09e609ba1" => :high_sierra
     sha256 "7f65668b7bb036fb19e69bdc9cbc2ec48728bc8c1936253f6d5e8d74a113a3fd" => :sierra
     sha256 "e4b3e2e66c2f772a38de529b884091a2ffa1f920af6604696129d21cc9e70b99" => :el_capitan
