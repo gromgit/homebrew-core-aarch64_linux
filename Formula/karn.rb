879d978338925ae95271bb5dd51db09e8d642ee3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 23:40:09 +0000

    karn: update 0.0.4 bottle.

--- Formula/karn.rb
+++ Formula/karn.rb
@@ -6,6 +6,7 @@ class Karn < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3a353fc809699b904cdbc845138518fddabfac7250b22d68a47f9ecdd98de967" => :mojave
     sha256 "a837fd65265db402d67fda5ff5bb4337822d1efd945bee56f7a664e6bc67c343" => :high_sierra
     sha256 "0b29500ed8d75753402ea041190021d679624b739665b3a4d11df3d4a3100e59" => :sierra
     sha256 "bc217bf56d073ffabd11c81382387029aa09d216ef8060b3134a6190997ef0f5" => :el_capitan
