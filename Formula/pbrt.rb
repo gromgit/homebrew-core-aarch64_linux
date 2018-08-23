4b43fd5e958f35f200203da22b61abf2fef477e0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 08:32:59 +0000

    pbrt: update 2.0.342_1 bottle.

--- Formula/pbrt.rb
+++ Formula/pbrt.rb
@@ -7,6 +7,7 @@ class Pbrt < Formula
 
   bottle do
     cellar :any
+    sha256 "bbef8afbd930c10498a65d8e70621eba607489a97994ba1b875523eec2db8b88" => :mojave
     sha256 "c1914092491b0a3e9b8f0aa53c08c290e4548d4c8236652f6987f585506c840b" => :high_sierra
     sha256 "e55782038c1b74d847254afae36fbfe11d3af774431b84bd7d56f16bbe73986b" => :sierra
     sha256 "f9104d3cf2dc87dc5b8cdc6b43b7c32447508ad4282d4caa850136e9742b61be" => :el_capitan
