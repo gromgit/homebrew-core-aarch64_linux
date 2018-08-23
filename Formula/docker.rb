7dc0879288ad46c3364f06f3a7e625e8b12940af
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 12:26:14 +0000

    docker: update 18.06.1 bottle.

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -7,6 +7,7 @@ class Docker < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ed98f2df25ff4735143d43c1f9e428f2a7d2577b7a684ac3c4da321a0d5178a8" => :mojave
     sha256 "57cc8cf1a0f1ff947ef7c5047d4d5aed4a703799235cb99e86d2679478a24738" => :high_sierra
     sha256 "7aba18b840ae091eadda46cf26fbd5200c4b3706c2cad239a7ee3a692c66ab99" => :sierra
     sha256 "90f9c41d4a57ab52cf606629d8875d5d2b602a377e30b76aa7b5d341ef444c85" => :el_capitan
