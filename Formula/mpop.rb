26335380b4ebb79200aa18f4c2b056d5793322d7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 10:10:48 +0000

    mpop: update 1.2.8 bottle.

--- Formula/mpop.rb
+++ Formula/mpop.rb
@@ -6,6 +6,7 @@ class Mpop < Formula
 
   bottle do
     cellar :any
+    sha256 "b2594cbd367ac7c922b279764df9f51ce8684218cd800509dc1a0c13c35a7b25" => :mojave
     sha256 "648cdecc606fd477c7949a81b0c4dd4d83ea4e37fb44fa046562563b29653bcd" => :high_sierra
     sha256 "4f059bd22ca42847af9e0a109d1753d50a9dc08dc2241e6efb009d5589919bff" => :sierra
     sha256 "4a11baf2addbf2be8122ea578cea50ebacaabb0f5ba60565b5d42c58c9e889b5" => :el_capitan
