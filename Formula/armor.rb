fb048eaae4184864e5138f05b1c6c07abf562762
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 12:27:59 +0000

    armor: update 0.4.11 bottle.

--- Formula/armor.rb
+++ Formula/armor.rb
@@ -7,6 +7,7 @@ class Armor < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b33151dd4c356859d9c861ed802f4a2a68281aa6acb31ac310b1db2bdeb419cc" => :mojave
     sha256 "1eb6ccc128ce585652c5f1189cd7a9e93ee2a9bd064c64b3f70e67722976322b" => :high_sierra
     sha256 "7b6376c03caba58a23de9a0027ab1272764a94bc6ed7b9caf879201837843697" => :sierra
     sha256 "cbf3a72453a8585e3bd72b3738f01413b544442c8d635aa0994a99747b39d575" => :el_capitan
