784bb7fce5cef718ea5e1adbd9a6d947f13ea892
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 11:14:17 +0000

    rt-audio: update 5.0.0 bottle.

--- Formula/rt-audio.rb
+++ Formula/rt-audio.rb
@@ -7,6 +7,7 @@ class RtAudio < Formula
 
   bottle do
     cellar :any
+    sha256 "c3ad32ecc289ff770964ca2d89511eba3d184787797324219a8b68a78ed3f3dd" => :mojave
     sha256 "497769e931f08d51367dae46ad3f7c3ac52f7d87ed79698c4e564aed74433d4d" => :high_sierra
     sha256 "d573b5a8e5e832e4b10445a05fe7502e62d42b5f8ae74796944ef3b30af7c3eb" => :sierra
     sha256 "b47e9e56e72e2e7969f811e8cb11db6a4436e4f5b5363db917043083b8dda612" => :el_capitan
