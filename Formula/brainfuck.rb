4d950819c75b6544ebf7b4f882332327d6a41e2e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 17:29:29 +0000

    brainfuck: update 2.7.1 bottle.

--- Formula/brainfuck.rb
+++ Formula/brainfuck.rb
@@ -7,6 +7,7 @@ class Brainfuck < Formula
 
   bottle do
     cellar :any
+    sha256 "a9e9509e4f0cd5345ffeac741c3a74d5575e17c99ea53822dd77fd4f98687b57" => :mojave
     sha256 "cf3c31fcf7c4cf099b348d01e619d1791aa3a255199de80afbc637e331947abf" => :high_sierra
     sha256 "354bb3372301325b49bfd4bd9b53084061af3bc3a3d6375e1c4635297c0dd008" => :sierra
     sha256 "f8289bed7e6455b63f05baf367069f60fe478f6c78f064c06ab1e571a181c3b7" => :el_capitan
