87db5196fe31c65b0e18c4f0f7213061bfbbc175
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 10:08:16 +0000

    lgogdownloader: update 3.3_3 bottle.

--- Formula/lgogdownloader.rb
+++ Formula/lgogdownloader.rb
@@ -7,6 +7,7 @@ class Lgogdownloader < Formula
 
   bottle do
     cellar :any
+    sha256 "4cc1eaa425aaff13aa77be6be1370d41fafce047f8578c6f160412b2a51b4f43" => :mojave
     sha256 "1951f964ade47905f12ea91fd479ae8a32925b1319438ee6539d33d726aeebd4" => :high_sierra
     sha256 "8a1dbf21474f6d9957f6e43db1bb2b3023a773ae8d197a28a56bbc405190384d" => :sierra
     sha256 "faad3335e0a60363fa890c823700fb934e323226aee405587042d1fb61a55dc3" => :el_capitan
