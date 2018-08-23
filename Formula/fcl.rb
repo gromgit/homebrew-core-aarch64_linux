402ab8db5539a91367e8ed1d6dfd2703204040ea
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 19:53:42 +0000

    fcl: update 0.5.0_1 bottle.

--- Formula/fcl.rb
+++ Formula/fcl.rb
@@ -6,6 +6,7 @@ class Fcl < Formula
   revision 1
 
   bottle do
+    sha256 "392131d9e9aea1fdd2e727161a7c4909dbe5efad7742e88ccc1afbc9090725bd" => :mojave
     sha256 "72a5ca040739722599576b579a6f864ca3307bf01ad8403765d739813a3e1fd0" => :high_sierra
     sha256 "8fd76b19ab4408397f161947d7da47f619ec710ddcdc2c012579440a6885f192" => :sierra
     sha256 "a2bc2115c9cc18a7f155583e6209ade38e73b80a10704e4d26e44ed177bcf5a5" => :el_capitan
