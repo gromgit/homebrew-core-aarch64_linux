6335ec69315e52d2d40e7ae82619f3acd8e977a1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 08:27:42 +0000

    stormlib: update 9.22 bottle.

--- Formula/stormlib.rb
+++ Formula/stormlib.rb
@@ -7,6 +7,7 @@ class Stormlib < Formula
 
   bottle do
     cellar :any
+    sha256 "c550620a6b13ac72d11763612582483514156a4ffdb1cf5a602b71ae186cc588" => :mojave
     sha256 "b76d57a2af0971b96c0cc46b3e24078486f732131b6845e92cbd776b4d5c20c7" => :high_sierra
     sha256 "aa27fedf8877032a52c85f9a7a57a94149e9fd2c18f25351fd6a61203e5a797d" => :sierra
     sha256 "71e3d5e94f69c23c8f0d5181b4bc130c3202aa6c99e0e42c4b6508b71be0167a" => :el_capitan
