fbeedd463f94c84d5fb6f41f4624db12d0a385f3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 15:56:39 +0000

    audiofile: update 0.3.6_1 bottle.

--- Formula/audiofile.rb
+++ Formula/audiofile.rb
@@ -19,6 +19,7 @@ class Audiofile < Formula
 
   bottle do
     cellar :any
+    sha256 "9d1038463e8eaa68f1cee8c447d566dc5acd32e2697f41837a9c08fedb0b2088" => :mojave
     sha256 "cf1f732ca5565a0e5d24a8d90714e554f95b4fbd3662da18ec843c0c356fff16" => :high_sierra
     sha256 "0a6cc39d6cce2c4436008f3d5679dbac6a8e0c0a1a91ea5db34597737fd5fb54" => :sierra
     sha256 "8e725b2809f539e2382b07a2fb64a551cbb09fdbaad168dd05784142e07ce495" => :el_capitan
