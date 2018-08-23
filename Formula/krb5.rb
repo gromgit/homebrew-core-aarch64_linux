8a3112b1ecc42f58cea9f5883f8c27598e836bb9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 23:40:15 +0000

    krb5: update 1.16.1 bottle.

--- Formula/krb5.rb
+++ Formula/krb5.rb
@@ -5,6 +5,7 @@ class Krb5 < Formula
   sha256 "214ffe394e3ad0c730564074ec44f1da119159d94281bbec541dc29168d21117"
 
   bottle do
+    sha256 "9aef9157b57acb216b15e7517fabcbc721febf558749e77729f500c95a02fbb7" => :mojave
     sha256 "ac3539420efc0db874142ff42a58b31a8e95d91ee3f6387a2e52ff91f74a0dbf" => :high_sierra
     sha256 "5345fa53b64215a02f28ca3a6603275ab922cc6c9ae34d462cd16645db736862" => :sierra
     sha256 "bea43fed7870e621af7ebb5438f1fe3eaa84f5d6aed68cb2429e71155da397b9" => :el_capitan
