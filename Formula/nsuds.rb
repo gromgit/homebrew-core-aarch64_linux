9df196b4bb3c523054577f2ddd7500eecb03f7b7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 09:35:39 +0000

    nsuds: update 0.7B bottle.

--- Formula/nsuds.rb
+++ Formula/nsuds.rb
@@ -5,6 +5,7 @@ class Nsuds < Formula
   sha256 "6d9b3e53f3cf45e9aa29f742f6a3f7bc83a1290099a62d9b8ba421879076926e"
 
   bottle do
+    sha256 "60d318290bb60415eb4abfdd7ffad468a24294892ac4ff90895cc0e589ea3da6" => :mojave
     sha256 "26e82eae22288d51eda3742c0ae4f3e1b0b17a003461f1baec38ccaa52495d9f" => :high_sierra
     sha256 "89ae2f310d8b21d98ababce7110f20d3d41da06b7a751447c56aa6dbd13a1950" => :sierra
     sha256 "596fc55d7e2cc63e8fdc4f3648a23d2c3c9c9eee9775a6579410c28708c0a358" => :el_capitan
