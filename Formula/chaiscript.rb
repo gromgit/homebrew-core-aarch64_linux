08277343a056b0e011abd43a69070cd779f5e08a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 08:33:04 +0000

    chaiscript: update 6.1.0 bottle.

--- Formula/chaiscript.rb
+++ Formula/chaiscript.rb
@@ -7,6 +7,7 @@ class Chaiscript < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "37f73c985ecbb3d1050f73c5020080fd6b8632780b3cacdc635c6198d9afd7d8" => :mojave
     sha256 "905850906c705182fe0c3011314d52b852585121f91c91a03ad20cc1b4a1a830" => :high_sierra
     sha256 "ce45ec71bbf6917d01c5d3ac872b31637189b90216848166ec91df5c65a82d07" => :sierra
     sha256 "18a4b79b3b413b01d2801e0a49b054137c3307bc0fc930353b63e0746e43c16d" => :el_capitan
