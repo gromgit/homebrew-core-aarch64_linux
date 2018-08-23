d298aa9c65daec2688e3e842c1c825ba6cc253a4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 20:56:42 +0000

    gource: update 0.49 bottle.

--- Formula/gource.rb
+++ Formula/gource.rb
@@ -5,6 +5,7 @@ class Gource < Formula
   sha256 "a9dd23693861e224322708a8e26c9e3aaa3e4c9ec41264b1ee2d5dcd6e2c0e8a"
 
   bottle do
+    sha256 "6efd53e0c1ceb57a85f9083d69303b8b01ff648be422f3a6cccf9f7eba1eef5f" => :mojave
     sha256 "d207627927335768487a9da97c079f462894657d780ab983a28617084f66cf8b" => :high_sierra
     sha256 "1c51103eec9b5bc824534c9f90fa8711718f793a3b91646c3da97e24acc5a5b1" => :sierra
     sha256 "20859d4ec9642ebb2b4d9a141d21029038dcef38c6955d3b7c03d044f76d0682" => :el_capitan
