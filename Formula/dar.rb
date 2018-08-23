53af2dcceba06b7f4502190923689684068bf9c5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 18:28:50 +0000

    dar: update 2.5.16 bottle.

--- Formula/dar.rb
+++ Formula/dar.rb
@@ -5,6 +5,7 @@ class Dar < Formula
   sha256 "e957c97101a17dc91dca00078457f225d2fa375d0db0ead7a64035378d4fc33b"
 
   bottle do
+    sha256 "ade6a894717bc00148d259f5c48b19172f12a307b84d2003f9a56ee95017d0a4" => :mojave
     sha256 "b8e79c81345db6df1b985d0b05d7fef3f0f72c3a0dfd356cd048de5e483f31ed" => :high_sierra
     sha256 "235d95dab2ef03ef746019989ca0939602903d79505bc3767d974f411996bb6b" => :sierra
     sha256 "0f3d68d33877c3d98b9d8fbacdbd52c54569ccc1a46130ecec244415c58c5c73" => :el_capitan
