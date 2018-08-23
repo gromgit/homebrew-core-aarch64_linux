57de2b84a0f5ea3873107e55cbb641a6fc46fd3a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 23:40:10 +0000

    kitchen-sync: update 1.1 bottle.

--- Formula/kitchen-sync.rb
+++ Formula/kitchen-sync.rb
@@ -7,6 +7,7 @@ class KitchenSync < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f2be7a9ccba914e89ef7b798e18b0a17493522daf3a07172e30b1c8d39002f55" => :mojave
     sha256 "8e70fee2dbfc450bfc614c2248227642378537c1b04c915e2c9f7c2b1f187c48" => :high_sierra
     sha256 "8af29ed55e0208c32d20ede87d2d50a70cb719731c2f864e0f5eb5d5ab993c7b" => :sierra
     sha256 "65066a29bde3ea01ec5e38b1631f8eca2f3e1f7e1ac3ff1ce24f06e8b00a136e" => :el_capitan
