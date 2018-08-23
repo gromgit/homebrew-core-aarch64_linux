1a0c34857d2d5c0371a37a1a9def0891adec32dd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 11:14:18 +0000

    gwenhywfar: update 4.20.0 bottle.

--- Formula/gwenhywfar.rb
+++ Formula/gwenhywfar.rb
@@ -5,6 +5,7 @@ class Gwenhywfar < Formula
   sha256 "5a88daabba1388f9528590aab5de527a12dd44a7da4572ce48469a29911b0fb0"
 
   bottle do
+    sha256 "6e6850e6a2e0edcd03665b11f66d51a2a0cb351037fdbd8b8677f4ae53595640" => :mojave
     sha256 "229eac2112343d4616576f947213706ebe5f28e0030dd5bef89ee4efa3043ac9" => :high_sierra
     sha256 "97aaff3937e4551227a66a55963d3dda7e24e53b9e9aa1ffca78ae455be00ab1" => :sierra
     sha256 "1760007de92dd5e7282a82f2534feb65380338fd8cb11632eef8914c2ddb56a4" => :el_capitan
