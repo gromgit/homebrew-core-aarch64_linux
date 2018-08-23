0990a24373e3310ce1256d581f479c41dcf5c95d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 09:54:55 +0000

    sword: update 1.8.1 bottle.

--- Formula/sword.rb
+++ Formula/sword.rb
@@ -5,6 +5,7 @@ class Sword < Formula
   sha256 "ce9aa8f721a737f406115d35ff438bd07c829fce1605f0d6dcdabc4318bc5e93"
 
   bottle do
+    sha256 "ab228fd2df3f45e696e50224872a2fd80d24fcebc92f2b6ba5ff6e36d8534bc6" => :mojave
     sha256 "3279c77477c21d1636f573202df976bd37fbaca39ed7e3b310158dad4e961641" => :high_sierra
     sha256 "794afe687eb7933fd3aeaee7e480224295614fc0138b8d89b7d9b81be55239a1" => :sierra
     sha256 "032c83b3302b78c198d1e346258a1d09c542a1361e1b0f000f82306d8c82acb4" => :el_capitan
