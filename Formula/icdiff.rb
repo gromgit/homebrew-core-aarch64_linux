967ef41f57bc317980925b099aefe128bb186f09
Brad Beyenhof <bbeyenhof@icloud.com>
2018-08-23 13:21:55 -0700

    icdiff: add HEAD
    Closes #31401.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/icdiff.rb
+++ Formula/icdiff.rb
@@ -3,6 +3,7 @@ class Icdiff < Formula
   homepage "https://github.com/jeffkaufman/icdiff"
   url "https://github.com/jeffkaufman/icdiff/archive/release-1.9.3.tar.gz"
   sha256 "090b0f96c5df5ae5d71f6e5325bca566dd47e8b61ec4b59d4e40acb9ffcb878f"
+  head "https://github.com/jeffkaufman/icdiff.git"
 
   bottle :unneeded
 
