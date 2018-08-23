5ba6f8306a40db798ac8e65f298cb5e2d210e246
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 23:40:10 +0000

    kpcli: update 3.2 bottle.

--- Formula/kpcli.rb
+++ Formula/kpcli.rb
@@ -6,6 +6,7 @@ class Kpcli < Formula
 
   bottle do
     cellar :any
+    sha256 "574b8a9be68de6d4c1efc825b362854a42c21f9f5d6ac65336e7678582d6c975" => :mojave
     sha256 "4485a38bd908295b8f738d866280d35ab0216bfa3de58c1feb0833b90fac0acd" => :high_sierra
     sha256 "7b0dafa06f46018a7c339debafdc192b062d0056700d804f4750759beae31528" => :sierra
     sha256 "2c2fad454c54b2ce0cab0ae750345d28a96e24e6fb985db0497f701cbe246bea" => :el_capitan
