0b86c52a8adb28df19d98a659c88531a7a44b878
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 09:54:54 +0000

    unac: update 1.8.0 bottle.

--- Formula/unac.rb
+++ Formula/unac.rb
@@ -7,6 +7,7 @@ class Unac < Formula
 
   bottle do
     cellar :any
+    sha256 "29753f2d4ea3f9a56f9a3d8fdca4c4fe47044ff1bc986d9ecc06d5f376197da6" => :mojave
     sha256 "eade4a2fba6e5828dccd3779b5e6681ca2558dbde421639624f089be835c55e8" => :high_sierra
     sha256 "b97f2799eafd917f8fe1cc47c39634bc91a19ca452ce11ec8fd5edf37ea1dba3" => :sierra
     sha256 "6c9d63dde182a55e237e63cfa4ab625164ce275e343fd88003483227bd7439bc" => :el_capitan
