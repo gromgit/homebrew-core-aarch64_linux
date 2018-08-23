c687936c2915f424e30b1a504f953c0a510f77d5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 22:51:04 +0000

    hqx: update 1.1_2 bottle.

--- Formula/hqx.rb
+++ Formula/hqx.rb
@@ -7,6 +7,7 @@ class Hqx < Formula
 
   bottle do
     cellar :any
+    sha256 "1a1f91371ce5bc0455725379f796243029016a1e866a71ff395d8f8a4e301481" => :mojave
     sha256 "5a548a5e9b16d78b7f913c4b6fece78202b13e416962a9a5a965c3e2d27461a8" => :high_sierra
     sha256 "7e58bc40ff9214f1b074595ac85c842ebafe676c6f3db42e3e0712c77c3377f4" => :sierra
     sha256 "6bf8b3b1b203ae43cc833480c8b395776d1369a38bb78fe2b47034ff8a8a0645" => :el_capitan
