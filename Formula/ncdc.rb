3d963f3adeec4543c138c30e70205e963a8b6116
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 07:26:18 +0000

    ncdc: update 1.20 bottle.

--- Formula/ncdc.rb
+++ Formula/ncdc.rb
@@ -6,6 +6,7 @@ class Ncdc < Formula
 
   bottle do
     cellar :any
+    sha256 "08a9bceea86394663db9b7abc8b5d8033afd1dbbcdfde15cac5b4b1c43e9b0d4" => :mojave
     sha256 "8c9d1049ef2493d21aba8eb961e98de27b187de8b7cbd18181a26e7889a2e030" => :high_sierra
     sha256 "1e882285c9367191a63d62cea2f539b22f9d209593649d6f3fb6c7a472cc2eaa" => :sierra
     sha256 "05b153207799eae9250516bfd51b41e0069f94155d727ec69a016db96bdfa349" => :el_capitan
