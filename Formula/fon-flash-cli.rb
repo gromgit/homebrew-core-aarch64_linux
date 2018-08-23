0f765dc7588085b9b4b9ebaeb5ba8ab8ef9bcc03
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 07:30:58 +0000

    fon-flash-cli: update 1.10.0 bottle.

--- Formula/fon-flash-cli.rb
+++ Formula/fon-flash-cli.rb
@@ -8,6 +8,7 @@ class FonFlashCli < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "168f12dbfc3d6b58a094389b31e744267166fa5e1cf682970e6342c6c65d7b3e" => :mojave
     sha256 "eefb911151deebc71f57de54004b2d8622223b60c8bf6313fcf94a4309c7ce9a" => :high_sierra
     sha256 "9aa03dd80f5796ef9087b1e109660fa2c9dc612d399e24c5d61905934d087ac8" => :sierra
     sha256 "c612973622cb2b87b5043fecd3aa3419bfc75d469dd7c470e25b72a2d346b834" => :el_capitan
