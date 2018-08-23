a88841f816666444d5269d52b3754e4eecd74300
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 08:33:02 +0000

    bdsup2sub: update 5.1.2 bottle.

--- Formula/bdsup2sub.rb
+++ Formula/bdsup2sub.rb
@@ -7,6 +7,7 @@ class Bdsup2sub < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "85a39af781d4dccdd6e2915d90cd892a2c6874d70e37bcd82d5c530d64f95a88" => :mojave
     sha256 "178ef2ffa1129b31b099c923f6842302efc2b34bcbdd0e89782bf4ffc82c3c71" => :high_sierra
     sha256 "36971d050455a728d555831b7b1c554ae658bbceb6a7a26687435e04861c2bb1" => :sierra
     sha256 "59b0eccde943ecb9e614dc409ab7ddf7064479b75310c8a782bd7b777d7e25f2" => :el_capitan
