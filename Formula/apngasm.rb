0a5f4482bef27c3459d00af7d95bc24e998a12c1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 16:00:51 +0000

    apngasm: update 3.1.6 bottle.

--- Formula/apngasm.rb
+++ Formula/apngasm.rb
@@ -8,6 +8,7 @@ class Apngasm < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "9385f652fd7fae83e373a823d7cd7b090213f3e6e17d1eee415022a4eb454cf4" => :mojave
     sha256 "df11f91ea8d27997410b38d42e435b58c34d54dd2bb58a714745ffcfaaacdda2" => :high_sierra
     sha256 "87cb9f81d1ec12b561e8750d259e27d1d97daa654742fcce032863e0185baf0f" => :sierra
     sha256 "073f74cacea8907e430113f4aae80d248887fc1d18de36f64889e683c08e3441" => :el_capitan
