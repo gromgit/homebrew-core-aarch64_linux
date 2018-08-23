e16f3398ca938e77ba6a17ad50519ae1ebdaccdd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 11:03:33 +0000

    gmic: update 2.1.5_1 bottle.

--- Formula/gmic.rb
+++ Formula/gmic.rb
@@ -8,6 +8,7 @@ class Gmic < Formula
 
   bottle do
     cellar :any
+    sha256 "09322413b52ce5865967d68dc2f5a6153a147b83a69f20a3f5b5ac9498676b6b" => :mojave
     sha256 "13ccaf356dc8be85d6d60078c5c10ad3ae6fd515169c396b6cb1c28b2a348c15" => :high_sierra
     sha256 "7ed192f9ad04036d236cbe9b854ea54325bb366ec392d4cb977e227167ce1ebf" => :sierra
     sha256 "db45390cb89c9a1d1280f05543555917d161be399223e79d99f6fcacae6532bf" => :el_capitan
