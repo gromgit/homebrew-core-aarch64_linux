2f88f0c106a1312fe748a89dce5c4358ed4d4a27
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 20:56:46 +0000

    gosu: update 1.14.11 bottle.

--- Formula/gosu.rb
+++ Formula/gosu.rb
@@ -7,6 +7,7 @@ class Gosu < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "55fc6ada2c2bfc353dfdbd2d52543adffa98d8c1d0587adb5b5053612bdf5b4f" => :mojave
     sha256 "1084dd28beda4453f255abca80ec9b8bc79f168b3a5a4cd3d3623e5c68e6fa5c" => :high_sierra
     sha256 "1ca6df5384e8c35339096b55c398f7201e4a0e8b53d959ea1e431c6af0b2fd06" => :sierra
     sha256 "ee811bdaa15babb338a88e9625750113c94abfe8f3c0b07e6da0080d1ef55b72" => :el_capitan
