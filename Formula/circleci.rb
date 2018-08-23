8b4f7aac7d1ccfc2daff7a30325f2e395dda362e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 01:17:24 +0000

    circleci: add 0.1.1430 bottle.

--- Formula/circleci.rb
+++ Formula/circleci.rb
@@ -5,6 +5,13 @@ class Circleci < Formula
       :tag => "v0.1.1430",
       :revision => "9788d7512e7e4018f3f98f471d874cab66475175"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "dd57d668341d8b7cf038c0deb84b62c7f33dad6b1bc062e3d2555e1f31a11808" => :high_sierra
+    sha256 "2b5313b8046e4e2a57c42b91eb0ee774bedc973d5f48e0f4c40d6b3a806e5fa1" => :sierra
+    sha256 "c7ddaab682323694fbf54de39ec56f47a5f9a7c420841f5b3a13e517c774c9ee" => :el_capitan
+  end
+
   depends_on "go" => :build
   depends_on "docker"
 
