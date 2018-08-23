3e72b426b175cbe9915ac58087d67d3e79a499f8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 09:16:32 +0000

    autorest: update 0.17.3 bottle.

--- Formula/autorest.rb
+++ Formula/autorest.rb
@@ -6,6 +6,7 @@ class Autorest < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "71f76d0fdc89273b98b90fc4cdc2282980a380396ca3f5d483917a3f355320bc" => :mojave
     sha256 "63768f566900eee86561a21b495b3627183fb1c05db98220561d58dad0d7a5d2" => :high_sierra
     sha256 "da1dc0e3a25b005db13ffbb95b145c060162648ad700998e4814a7969e17cbb1" => :sierra
     sha256 "da1dc0e3a25b005db13ffbb95b145c060162648ad700998e4814a7969e17cbb1" => :el_capitan
