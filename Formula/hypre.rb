dd371efa8551c8d1f509a7b03fcc5096b9c0f255
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 22:42:09 +0000

    hypre: update 2.11.2_3 bottle.

--- Formula/hypre.rb
+++ Formula/hypre.rb
@@ -8,6 +8,7 @@ class Hypre < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "01be3b67dc76cde96a42254a49b1bf14cdbf87e841ded452d9d50f7883eccbfd" => :mojave
     sha256 "7b0ee7e6a754e583739aa29b055d812a4d77834c0871e38c38f931731c860818" => :high_sierra
     sha256 "1702f97e71696f76192e2be7719020999810984515e777964053c2f6d2541ddb" => :sierra
     sha256 "9f8162b5b6119c7a1015388a054415c5d77fae6cf37a24b4bd58b7e3d0ef885b" => :el_capitan
