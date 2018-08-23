0cb75fa49ee6d79d9d76c3913e5ed3bd57bc78ac
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 08:27:39 +0000

    wait_on: update 1.1 bottle.

--- Formula/wait_on.rb
+++ Formula/wait_on.rb
@@ -7,6 +7,7 @@ class WaitOn < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "500c8192d5cc65f906844fcca12f837d1e498d396ac60f368c195cf773bb6d4d" => :mojave
     sha256 "ca2dfaf2a1449ae1066826846cbdc9963efb837277f258c57304669f8615ad0f" => :high_sierra
     sha256 "31a21b915c4f8d901ed5d51d14f3f2f81993ed07891832fdbfb314b7cc8599e5" => :sierra
     sha256 "f362694f30021c32557e23b5add36ac4bdfbcabc6056996c5d23fc125527d85d" => :el_capitan
