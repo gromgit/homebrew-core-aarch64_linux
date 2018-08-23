e59dd895aff5c3bb2d5c5b7881a9d9adf1b4f3db
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 23:55:38 +0000

    libngspice: update 28 bottle.

--- Formula/libngspice.rb
+++ Formula/libngspice.rb
@@ -5,6 +5,7 @@ class Libngspice < Formula
   sha256 "94804fa78c8db2f90f088902e8c27f7b732a66767a58c70f37612bff5a16df66"
 
   bottle do
+    sha256 "62d5c4b9ee7a4126e7dfb18b3fd762c6d4b7325283702b58646d640a1c1773ec" => :mojave
     sha256 "ad14b322a63580cbee9940bb67546cce89d654072446cbb90bd75020f359e3f6" => :high_sierra
     sha256 "bfae0f3f17b4f5493b1c3dab4f2ad45f33d276d9da2ebe44ec68c808d5204ffb" => :sierra
     sha256 "81fa4c4a1da3fb720f921274fde37cdd540ffce16d6bec9c35bbae8399aa728c" => :el_capitan
