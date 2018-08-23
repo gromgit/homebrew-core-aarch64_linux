03fb05e2f3ec17ea72733c811d4a8958f72dc393
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 23:40:14 +0000

    kafkacat: update 1.3.1_1 bottle.

--- Formula/kafkacat.rb
+++ Formula/kafkacat.rb
@@ -8,6 +8,7 @@ class Kafkacat < Formula
 
   bottle do
     cellar :any
+    sha256 "faaf8dfca36c061b0ef0be2c42fafd5cc426198dec0762238d10fd216409bdad" => :mojave
     sha256 "d38492eb3083e50f91bb50d0a77c59bf559821a6dbc958b3b8d3bac4b192dead" => :high_sierra
     sha256 "14038aa18ab2f7f982000b3a5ad347f9519809b3bab0cfe47424bb73f333de6e" => :sierra
     sha256 "ef21329872a809c9a01e6361fcc2d0830f5b9c33455286260b45713acf19285f" => :el_capitan
