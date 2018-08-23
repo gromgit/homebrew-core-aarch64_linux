0a633936b259ac0f5fa33a63196604f0acf7ec4d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 20:56:43 +0000

    gphoto2: update 2.5.17 bottle.

--- Formula/gphoto2.rb
+++ Formula/gphoto2.rb
@@ -6,6 +6,7 @@ class Gphoto2 < Formula
 
   bottle do
     cellar :any
+    sha256 "2eed246e98c89ad02f275548c56b9ef02df5c255b09fe680e244c4a8c0aebb57" => :mojave
     sha256 "1dbc447cbf64977572f5cdb3196b82937f27661f999c9fb7b0ab93985a891fe2" => :high_sierra
     sha256 "bf68526ad17bed7a7d0c72b29862ccdae97adda5c660acb72745cb71d55213bc" => :sierra
     sha256 "687ef6fcc186936acfe9e4d186c83175094ecfe231b76fff3eee5054b6217602" => :el_capitan
