53f71d2ec9d6bec219a8f0dffccba532fb5c03b9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 23:40:13 +0000

    jsonnet: update 0.11.2 bottle.

--- Formula/jsonnet.rb
+++ Formula/jsonnet.rb
@@ -6,6 +6,7 @@ class Jsonnet < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "085740ce03c6801c58895b554ab23eae2ac8a8e2a1c528519324ccf543965e42" => :mojave
     sha256 "c146c572e65547f42d39580bbaed91539928c7d54ed6c1970d24e4507c53d638" => :high_sierra
     sha256 "b35454f33de80b8129bab932aa4979374bc729a4b69211e3ac2800ac4a47849a" => :sierra
     sha256 "813122fb2e3cb40932a4c270ed9b9163055df3898a2868bda9e18cf06b403e24" => :el_capitan
