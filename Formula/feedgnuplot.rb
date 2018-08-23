7db7504dbafb6f59dcba14a40f163afa86f5a8bb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 19:53:41 +0000

    feedgnuplot: update 1.49 bottle.

--- Formula/feedgnuplot.rb
+++ Formula/feedgnuplot.rb
@@ -6,6 +6,7 @@ class Feedgnuplot < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4851b4a16627d5beb56108c1b28b3800f0d55894419ce07d86db94d7ae539233" => :mojave
     sha256 "33db12a4bdcc89874062b1c591cedb3e44c61013eae0363ce2d5f1d1e68d7d04" => :high_sierra
     sha256 "6aa7778687e758c348d8e71351d0147d2b46df0668e699d1b197dccd927a28d7" => :sierra
     sha256 "835b905b32f65cfd4fe3a82e7ddce7d77536edc949a146bbdff79fce2cf69cce" => :el_capitan
