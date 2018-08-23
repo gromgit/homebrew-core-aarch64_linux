8c91f5eb672a433edd70b53bd52cdb1c6dde8de4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 20:20:50 +0000

    gnupg-pkcs11-scd: update 0.9.1 bottle.

--- Formula/gnupg-pkcs11-scd.rb
+++ Formula/gnupg-pkcs11-scd.rb
@@ -6,6 +6,7 @@ class GnupgPkcs11Scd < Formula
 
   bottle do
     cellar :any
+    sha256 "ce2127047e7bb4f07cdc7702fa399d2c357eac20a8b9b2b7fa879908b836257a" => :mojave
     sha256 "b1b94851685ebb8dc920e4e5212c2b7effa2e831f184d8b8e66e44b1c9630d93" => :high_sierra
     sha256 "eccf8f2df5f2007142529fd3b34085ef3f81500173dc6fccd0a94dfe76c7ad19" => :sierra
     sha256 "0b236bc743da30e0db9458d55a1e511c792d9ed85c97c726a87b5932e8a21dc7" => :el_capitan
