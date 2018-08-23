a599b1e6d17e94f50b081d3cb4bbb33cb9b34e92
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 11:19:10 +0000

    antlr4-cpp-runtime: update 4.7.1 bottle.

--- Formula/antlr4-cpp-runtime.rb
+++ Formula/antlr4-cpp-runtime.rb
@@ -6,6 +6,7 @@ class Antlr4CppRuntime < Formula
 
   bottle do
     cellar :any
+    sha256 "7c55dcee97d1d509f6f602a2e4002ec6df85a1bab231284c343e22a917aa41eb" => :mojave
     sha256 "bdb81df9b567f1d0584712616f4e5e8d4028c0875c79c0e4479e3db98be498f7" => :high_sierra
     sha256 "3b41cda988528eaa6f9c8792464f5a71786674774dc3ab370b6bb1171d5758cb" => :sierra
     sha256 "5cc0d3c4ef8111228b058c52a1645168d772d797ed2cbf7747f38d1803c08dc9" => :el_capitan
