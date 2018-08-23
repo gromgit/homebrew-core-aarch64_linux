e3499f68f1b6bc27553649b303590afa6df02977
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 22:42:08 +0000

    imageworsener: update 1.3.2_1 bottle.

--- Formula/imageworsener.rb
+++ Formula/imageworsener.rb
@@ -7,6 +7,7 @@ class Imageworsener < Formula
 
   bottle do
     cellar :any
+    sha256 "7ecb9c599da11d86cc8bc9e46d2758422cad1bb39e94c3b398536b7c144d4def" => :mojave
     sha256 "1b8964b45f496d8e35c4dc72f2c26b51fa47d301fcd951f109e5062b9dbac13d" => :high_sierra
     sha256 "e3da0b7bd45f393eb8dd514473a956f7954a6b1e7d5af7e5382b67b9a21d1510" => :sierra
     sha256 "4bf452a3350cf9121ba3bd7dff9f63bc83bcda4c5e4be194cb1e8cd521a0a0b2" => :el_capitan
