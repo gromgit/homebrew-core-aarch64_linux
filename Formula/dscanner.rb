0039af93511e600fa98e921d8a067119ce9678b6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 18:49:14 +0000

    dscanner: update 0.5.8 bottle.

--- Formula/dscanner.rb
+++ Formula/dscanner.rb
@@ -7,6 +7,7 @@ class Dscanner < Formula
   head "https://github.com/dlang-community/Dscanner.git"
 
   bottle do
+    sha256 "becb20ad4902e671a1b6e6dec4cf65c1749375a8e100ae9c3b61f6ddb3fc3a16" => :mojave
     sha256 "82f4f4431c569e21ab2d340c749fbae7f65a09dfcbc29822d7599ed9eff5c4d8" => :high_sierra
     sha256 "9191d8114416f8629e8ad43c1676c90e15f866092c1cb8cea7edd15e8e03c22e" => :sierra
     sha256 "2437e819fac2bfec60117af405ec3df43fa7ae29a71b22b6e5795c0b627d5d5e" => :el_capitan
