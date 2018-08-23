e64194f38aecd3fbb5c1eafc356078e523e14b4c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 07:26:21 +0000

    sispmctl: update 4.0 bottle.

--- Formula/sispmctl.rb
+++ Formula/sispmctl.rb
@@ -5,6 +5,7 @@ class Sispmctl < Formula
   sha256 "442d9bb9774da7214c222144035ac68ad5d25171040ce2731cfdf49b3365cfd5"
 
   bottle do
+    sha256 "cec81af9b77fc4c08d7530aac3aea21f0dcf85ae192428803815c80dc3b30e3a" => :mojave
     sha256 "3ad48ee1db3c177b4f93bedfa6163b0dafa96cfb1b1f349dc5d9682e78d67d98" => :high_sierra
     sha256 "c4e130a2484f40a4803e1eccac3c0411fde2c881b64a0220a60dede14bf9fdf7" => :sierra
     sha256 "4082f505586a99a70dc76516e505ca83d15d794a7c87a87629089d1c50ca44b9" => :el_capitan
