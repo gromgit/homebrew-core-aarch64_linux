01ad4fcb8c285c4cde6530ccaeabb7564fef69c0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 11:19:07 +0000

    mecab-unidic: update 2.1.2 bottle.

--- Formula/mecab-unidic.rb
+++ Formula/mecab-unidic.rb
@@ -8,6 +8,7 @@ class MecabUnidic < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "15b1cd2eb3cc04747ee04b71aaaf7b99323e58d5ebdb4e79b4c9c3d36e656f1d" => :mojave
     sha256 "9062af2305cf97efe00b42bda1b89253b487cea1ef3675e6f4c187dea1e37033" => :high_sierra
     sha256 "083c740c7c309410266eb793755477b6d37da6f6c85dddbb62e31b27dcbae135" => :sierra
     sha256 "9ece990d89f8949c82003296bd256ebafddaf5d9caf03a63ea692f2009d52783" => :el_capitan
