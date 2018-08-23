e97ecb235d6bece6bf949a3f330be06099acc948
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 11:03:29 +0000

    lean: update 3.4.1 bottle.

--- Formula/lean.rb
+++ Formula/lean.rb
@@ -7,6 +7,7 @@ class Lean < Formula
 
   bottle do
     cellar :any
+    sha256 "f9b31e72b4a0c314c09af988e21172bcfce644487a2e7f4d0bafb898be3a8ee0" => :mojave
     sha256 "5afcb08796f1e4bf4600006c7b3ebcb76e90f15a14bce73af97f55f1896a4ef1" => :high_sierra
     sha256 "4c25bc5a6233614a706323e9a4bfec26bb83f10d67a96f05272ad2c0fe2c7e71" => :sierra
     sha256 "711ed18ebd939e8db24c1c87cba002fd6e590fbc92e110aae7e78310852343b2" => :el_capitan
