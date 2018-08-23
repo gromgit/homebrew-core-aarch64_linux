45a84afd06f5c8851c3a21c82dcb85023197fa45
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 18:27:16 +0000

    dosbox-x: update 0.801_1 bottle.

--- Formula/dosbox-x.rb
+++ Formula/dosbox-x.rb
@@ -9,6 +9,7 @@ class DosboxX < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "5bccafd80948d5ef2ef3059eae718d1c025880a83c065533daa7befe1bd4cae2" => :mojave
     sha256 "8cbaa0cf9658118b4b4ba32f4d1718f9bf49d0aec71cc7846463f37966559656" => :high_sierra
     sha256 "d3fc4b2bd340ed6f7d2624b8daf95397891f8e142d6219437f2cae215f538216" => :sierra
     sha256 "0b5098e3397a15804a300540be53c98f862c4f7276eb4c1de7966152421a9392" => :el_capitan
