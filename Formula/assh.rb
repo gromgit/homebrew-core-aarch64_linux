c3befcd2bfe2bbbfd464d8c5520dd74701e230f7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 15:56:38 +0000

    assh: update 2.8.0 bottle.

--- Formula/assh.rb
+++ Formula/assh.rb
@@ -7,6 +7,7 @@ class Assh < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "bea473073d77ae8be2329b1c868242b6d5098c46516a6e16669752b8742d56b2" => :mojave
     sha256 "7e48c5089af70b883059a68a4e8be6b3a4a973306067d16983bc1130e23979fc" => :high_sierra
     sha256 "0087bab708bd409d1cd00dcaba1ed40c4ad9e4f11f3b6db275a827fe2ff69011" => :sierra
     sha256 "f791dad7d95875ae19db21305c2d5d3632ebfb7783c837d212a08099d15c4d7a" => :el_capitan
