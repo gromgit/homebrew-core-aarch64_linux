51758b369ea4691370d693ad0db285d9b33c040c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 11:53:10 +0000

    docker: update 18.06.1 bottle.

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -7,10 +7,9 @@ class Docker < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d2ca3d569b0541a36da371966d50c5462982b0aea0a01adf85b517fc242464c8" => :mojave
-    sha256 "1a3991834c66511c6d8da9bbdcffe54d5d10de9df26792481e1761dd45ee4a88" => :high_sierra
-    sha256 "145e5145e54495086c1d48e1cb6836aa27f0a4dbb411ee54e31cab7e44e56d36" => :sierra
-    sha256 "2f2aa289dae63b32bed8e43d1d1f80757b49e0a54c41bbde34d44adf5e3c1f92" => :el_capitan
+    sha256 "57cc8cf1a0f1ff947ef7c5047d4d5aed4a703799235cb99e86d2679478a24738" => :high_sierra
+    sha256 "7aba18b840ae091eadda46cf26fbd5200c4b3706c2cad239a7ee3a692c66ab99" => :sierra
+    sha256 "90f9c41d4a57ab52cf606629d8875d5d2b602a377e30b76aa7b5d341ef444c85" => :el_capitan
   end
 
   option "with-experimental", "Enable experimental features"
