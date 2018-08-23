623a14ca52fee03d17001d37523f02279e49012d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 09:35:40 +0000

    lcdproc: update 0.5.9 bottle.

--- Formula/lcdproc.rb
+++ Formula/lcdproc.rb
@@ -5,6 +5,7 @@ class Lcdproc < Formula
   sha256 "d48a915496c96ff775b377d2222de3150ae5172bfb84a6ec9f9ceab962f97b83"
 
   bottle do
+    sha256 "1a875cd265136e02f28f31cd9138c8814deaf793704792be5f2cccf5aa6736fb" => :mojave
     sha256 "59439a9e18e3e8e636a60e1710cd10f8a4bad8632d08782fc4442a2427fe1ddb" => :high_sierra
     sha256 "2371b10dc3bd2644ac83ced35dcde1960110217385f9f5547917ebbbb823e332" => :sierra
     sha256 "1de4bece6e781dc6d88d000039095cbf6edbd10313163ef9644152d67778171c" => :el_capitan
