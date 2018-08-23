472867b9438a0f280660c64d0004e3a78ebd810a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 15:11:43 +0000

    exercism: update 3.0.8 bottle.

--- Formula/exercism.rb
+++ Formula/exercism.rb
@@ -7,10 +7,9 @@ class Exercism < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "592d1c6ea90da7930185a969a9e70481eb3d9d81c276bba78586ca760a3bf397" => :mojave
-    sha256 "0797f3e428dd6793ceabf3d53b21719a19b13b41e13f8a7e687efb9f0fa8de75" => :high_sierra
-    sha256 "faf2b756e594c79e3808fbf6d26e7bbd255fc0c7a443054f779c8e8edf5f369c" => :sierra
-    sha256 "c26332bf4bbd8a00b5c3bd2a924dfd7d54b097131c653c577b43e865917d8e6f" => :el_capitan
+    sha256 "d72972acaea2a65b58dcdb7eee822ecd7903818789b3a3c536f5a5782450dad0" => :high_sierra
+    sha256 "5059f764e394f48bd7459bf5de9608d883cb7a05ceebc7849392d6c284dac100" => :sierra
+    sha256 "d957ffe5a898df25c87ac2ca35ca150fa8dd96c932180e16bda2acbfa0a8b199" => :el_capitan
   end
 
   depends_on "dep" => :build
