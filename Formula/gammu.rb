8197d8deaaa156e8878838e5c1a66063739baac5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 19:51:37 +0000

    gammu: update 1.39.0 bottle.

--- Formula/gammu.rb
+++ Formula/gammu.rb
@@ -6,6 +6,7 @@ class Gammu < Formula
   head "https://github.com/gammu/gammu.git"
 
   bottle do
+    sha256 "89c6abf8223d40d556f4e68954fd87513e27173accbbb1e0474153b8556b3768" => :mojave
     sha256 "d18ac85aafe7b0808af71def3812d623223234d17ce2f8281a3c673079e6509a" => :high_sierra
     sha256 "eeb48b7fe0b0b97f96aa3580c92b76e0cd59ece002eb1bc076efa30c85327ce8" => :sierra
     sha256 "abe55374ac1e3898e321af01126d0da4ad436be24ea2bd14e44cf61a386f4692" => :el_capitan
