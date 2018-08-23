447cee5350d65576baf70a3602805e7ee45180c4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 18:20:36 +0000

    dieharder: update 3.31.1_2 bottle.

--- Formula/dieharder.rb
+++ Formula/dieharder.rb
@@ -7,6 +7,7 @@ class Dieharder < Formula
 
   bottle do
     cellar :any
+    sha256 "c02799fe41057f9de24178686e7cce9666d6e5650104a41a8955d7af2461ef2f" => :mojave
     sha256 "f1f36b404203561f04b05aef3bce6f483980eee5ee8898b1f535ccade28ef369" => :high_sierra
     sha256 "758c782ab9ba74df2bf493296435eafc24b97fdda7493485bf367f4afd7a50e7" => :sierra
     sha256 "7adbcdbabc0c75df4394b7934dc5d8b33ef325ebf58a082e061fc333b0f82b1d" => :el_capitan
