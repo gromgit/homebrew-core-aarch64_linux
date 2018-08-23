8cd66deb9e67c7a55b21bac528127aac6cef0102
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 20:19:20 +0000

    get-flash-videos: update 1.25.99.03 bottle.

--- Formula/get-flash-videos.rb
+++ Formula/get-flash-videos.rb
@@ -6,6 +6,7 @@ class GetFlashVideos < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ce0853c6ed07dfec3abce3ae4ca0cf073f5a8a38d530a227ffd966e7f1d946c4" => :mojave
     sha256 "10e25788bbd00ffd0f0a5281c9c4c493c2957947ba4690cf92460c98aa72dc1b" => :high_sierra
     sha256 "6c6e60aff2308881f4ef896b5a5c2dd1b05db9146d224be024793f042534dc6d" => :sierra
     sha256 "0b7edca9b6518af848a6cb5f84bb34014cd017391279398ea9d796dc89ea7a57" => :el_capitan
