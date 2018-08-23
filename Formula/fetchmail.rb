fff62e1c1fd9908dd7a9b5d859707dc79c4a0762
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 19:53:44 +0000

    fetchmail: update 6.3.26 bottle.

--- Formula/fetchmail.rb
+++ Formula/fetchmail.rb
@@ -7,6 +7,7 @@ class Fetchmail < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "3be3279ed30d08cdf4e533ec9ebad92398cea90a8f364851f82d5666b89bf25a" => :mojave
     sha256 "8e037ee9a367c9c4a26d5491e4db84285390d0ef1949c30d56c0726706dab6de" => :high_sierra
     sha256 "63692fabb3746b739ce3b81f2d1ce964ca4e74613ea63f307e734f90dbcea513" => :sierra
     sha256 "663d68d69a865daa6311f70ed2412abe79affa23b8fd76b44eaca5d9735fba36" => :el_capitan
