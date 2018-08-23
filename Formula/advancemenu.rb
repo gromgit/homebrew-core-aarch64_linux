531b717dd66eb4b480e6275ceeefe72bd4fb6305
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 07:24:44 +0000

    advancemenu: update 2.9 bottle.

--- Formula/advancemenu.rb
+++ Formula/advancemenu.rb
@@ -5,6 +5,7 @@ class Advancemenu < Formula
   sha256 "c7599da6ff715eb3ad9f7a55973a9aaac2f26740a4e12daf744cf08963d652c1"
 
   bottle do
+    sha256 "b61eed32b7dbf12a05a73e825df47adab2ee86da445808c0e9b4db5597d2ad23" => :mojave
     sha256 "8c96b4bef369fe6b52d58c9c6d8025a64450fad965af6209c99c7b1edcec58eb" => :high_sierra
     sha256 "875396a5eb77a17ee1cda679fc782d5fa15072a206331d15cb186e52bf4d5369" => :sierra
     sha256 "5e4a2e320562c0d92c3d61049f097d0a7ee054b76bacc8dbf81095c2758bed6a" => :el_capitan
