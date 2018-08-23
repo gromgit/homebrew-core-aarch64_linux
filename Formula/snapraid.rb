a964f8088092abc43a085b224921ded6a1b4c571
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 09:54:55 +0000

    snapraid: update 11.2 bottle.

--- Formula/snapraid.rb
+++ Formula/snapraid.rb
@@ -5,6 +5,7 @@ class Snapraid < Formula
   sha256 "735cdeb7656ac48cbb0b4a89a203dd566505071e465d5effbcc56bcb8fd3a0d7"
 
   bottle do
+    sha256 "f76d78e93603e753551db7f3460c36d8bcbfb17a70dbf213f50c1b037d8d1add" => :mojave
     sha256 "a3cdc8a3975bf5f3073e21d089d5e3c067fb809ccf758724f25fc092bebf7b71" => :high_sierra
     sha256 "eba431c9a6d92f379e0063c3c40cf6a655fa2a29256da707f851569046e7d7c9" => :sierra
     sha256 "e6be5ae4ae3aa3bffbd4f57c4dab84602d3883673ce9f2b74189a0f39c82b4b2" => :el_capitan
