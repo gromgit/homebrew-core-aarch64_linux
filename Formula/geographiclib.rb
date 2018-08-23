3c7b0bc0fa3365c2c143228a32ddcf9d6a23143d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 20:19:19 +0000

    geographiclib: update 1.49 bottle.

--- Formula/geographiclib.rb
+++ Formula/geographiclib.rb
@@ -6,6 +6,7 @@ class Geographiclib < Formula
 
   bottle do
     cellar :any
+    sha256 "a97fd12c98b8aa7d6bb7187a7bcb87a5728cbc8d09cdfbec8a890a50539ba02a" => :mojave
     sha256 "69f55b7d549bf61aa3867cd5451a3df11ca71f823c5baef79631ed9edfc32417" => :high_sierra
     sha256 "d583748cacffdef26dca690a0f8e87d02166ce7ef1907e9bbf282f7e0c099b1c" => :sierra
     sha256 "a730f21ffa4e9503f15d8b3ecd51c846cac5e79581ef5ec86297c4c10739e4a3" => :el_capitan
