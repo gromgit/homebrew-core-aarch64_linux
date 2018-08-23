1446fb055c26d09f2b2c61045ac2eb30ef8205f7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 12:28:02 +0000

    wiredtiger: update 3.1.0 bottle.

--- Formula/wiredtiger.rb
+++ Formula/wiredtiger.rb
@@ -6,6 +6,7 @@ class Wiredtiger < Formula
 
   bottle do
     cellar :any
+    sha256 "1d595522272e3c03534ca4299f214b76d65d3712fdceadd341cb0386ab0b42a3" => :mojave
     sha256 "4afe18e05ee55cf263b57026e8fecaba3084ef76dd92b5aa1620c6eca85948e2" => :high_sierra
     sha256 "e040bb1adc571f9f8d0929898dcf326bb97572fbe4bc9c52810e70b9d7e75afa" => :sierra
     sha256 "b141f6e07a7f6d0a1e1b7db637eb12fe908ded0a75b2415ed5f1b90a937959f6" => :el_capitan
