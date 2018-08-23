01e635f915e5b22af5cd6616f435685515c42a6c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-23 17:54:13 +0000

    csup: update 20120305_1 bottle.

--- Formula/csup.rb
+++ Formula/csup.rb
@@ -8,6 +8,7 @@ class Csup < Formula
 
   bottle do
     rebuild 1
+    sha256 "3aa3e72c6f104818b1d20d3da7a91082372f1a2291dd4df44a646091b7cdbb92" => :mojave
     sha256 "14207f528521fa0ebec31802647c97a7fc68705333b9b85e13b1ca49abc3ecfa" => :high_sierra
     sha256 "48e552e85834b44fbaf0c09fef9e0f6e4b8768684973dd8ad632e778d228aa75" => :sierra
     sha256 "e5fc8b0e31fa2f93dccde05ad0b431150c8fb925ee2ab357878f01f1ba5264c2" => :el_capitan
