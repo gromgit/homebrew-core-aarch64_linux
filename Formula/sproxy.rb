46734aca8e368232d2d83ba97d3db68acbd8ea6e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 10:09:04 +0000

    sproxy: update 1.02 bottle.

--- Formula/sproxy.rb
+++ Formula/sproxy.rb
@@ -7,6 +7,7 @@ class Sproxy < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "2d689087925622e4f7e2c2572c2339c62a6c2b891bce7093bcd664f1a15c28d9" => :mojave
     sha256 "326b01fa9a1370c54929ae4c11d1b67b2238875eca8188365486b9c2a374264f" => :high_sierra
     sha256 "8d57317644b76b465adc5caf984f1e3cf57f9486f642705eee66128adbcf3589" => :sierra
     sha256 "4ed786b0b05ca3c88d5904e3119d84725a9f9bedf5d952c055f22a81661a825c" => :el_capitan
