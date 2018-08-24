896aa2747e67dabf7170f5f808109bd9b9b5500e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 00:42:10 +0000

    lrzip: update 0.631 bottle.

--- Formula/lrzip.rb
+++ Formula/lrzip.rb
@@ -6,6 +6,7 @@ class Lrzip < Formula
 
   bottle do
     cellar :any
+    sha256 "c4fd1cfc9b09ab7f175bd056865c8712f9e6310c918cd03cfdf6e30f283c8761" => :mojave
     sha256 "97797937ad456c0658fe24399dc757f30771e971647395fe1fefaa227f615fea" => :high_sierra
     sha256 "b0c60e0773da9cf70d3164f362b3b527a7a87acd10b632291055d58ca2da7cfc" => :sierra
     sha256 "c0ea3854495bd5d98f040f1a6b5a08e01857436aac25ead3f7a3fb44841f738a" => :el_capitan
