598c597e70966bef8e81b90af023fc606673f5a8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 11:00:55 +0000

    sstp-client: update 1.0.12 bottle.

--- Formula/sstp-client.rb
+++ Formula/sstp-client.rb
@@ -5,6 +5,7 @@ class SstpClient < Formula
   sha256 "487eb406579689803ce0397f6102b18641e4572ac7bc9b9e5f3027c84dcf67ff"
 
   bottle do
+    sha256 "361a2e8e315b16b5444338c13e11db2d0b78bb8ad0f73de18526d164f87b237e" => :mojave
     sha256 "f910157864b32a5cd004366b0e8ee6cefd09b0edd36afb89d751ede34e27df96" => :high_sierra
     sha256 "1bbbe9f5558b4757f8244064144106b0bda636c2adcc3da734634895b960ff68" => :sierra
     sha256 "c4b0b6b1ebc16783d22327173bfe1e90bd5fd329786a613422c858fc352672eb" => :el_capitan
