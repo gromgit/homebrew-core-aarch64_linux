be2d189ed87900765855d18b1d4834337a698c58
Troels Henriksen <athas@sigkill.dk>
2018-08-24 11:10:14 +0200

    futhark 0.6.3
    Closes #31427.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/futhark.rb
+++ Formula/futhark.rb
@@ -5,8 +5,8 @@ class Futhark < Formula
 
   desc "Data-parallel functional programming language"
   homepage "https://futhark-lang.org/"
-  url "https://github.com/diku-dk/futhark/archive/v0.6.2.tar.gz"
-  sha256 "c6e537cf1458b941aeeb0b002a6ee6f8eb566cc9de6096d295d86595a7747414"
+  url "https://github.com/diku-dk/futhark/archive/v0.6.3.tar.gz"
+  sha256 "9534d293e4d54234ed6a43bf99626ea1ceeaf769b6cdbb73599bdc17fa26f893"
   head "https://github.com/diku-dk/futhark.git"
 
   bottle do
