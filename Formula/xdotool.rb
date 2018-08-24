02e931d2011b50f0a98c2d126c1de860d5c1ebfd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 14:43:58 +0000

    xdotool: update 3.20160805.1 bottle.

--- Formula/xdotool.rb
+++ Formula/xdotool.rb
@@ -5,6 +5,7 @@ class Xdotool < Formula
   sha256 "ddafca1239075c203769c17a5a184587731e56fbe0438c09d08f8af1704e117a"
 
   bottle do
+    sha256 "02edb6e55146177191ec888e7886878b2bf93defb20a2e6a01546bce111859b8" => :mojave
     sha256 "2f949fc70d828db23364beed16bdbd15c728d790601e5e0a59b110f8f6eb3826" => :high_sierra
     sha256 "13b1b017e94c76bde510b06427cf517c0d78028994e3b1bb8501ec2cbd5c7ef1" => :sierra
     sha256 "d7fad4610977a3a5f8879b4f51d35e08e4ef3e65cfbc04353e67bdc14b279867" => :el_capitan
