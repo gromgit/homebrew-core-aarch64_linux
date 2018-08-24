e7a7fa579fe099d8e2d5c13189ecca9d1421cfb5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 12:56:28 +0000

    zita-convolver: update 4.0.0 bottle.

--- Formula/zita-convolver.rb
+++ Formula/zita-convolver.rb
@@ -6,6 +6,7 @@ class ZitaConvolver < Formula
 
   bottle do
     cellar :any
+    sha256 "affeedb6852f411c86d7219df7614a1d308e8d46c703cb2dc914188593a79076" => :mojave
     sha256 "d65f312660d0ca92081a0d217a696a8e03a64f604f6924cd95b9e7a4956979f6" => :high_sierra
     sha256 "39433124f4b7d8fa9b6eb20f40708ba9ded27322049b09178f3a3ef8e5ce5c1e" => :sierra
     sha256 "d040044f83b7cd6d2bc2e4d8625a38c08820723744b9a653fc23fc9e3f33e5d2" => :el_capitan
