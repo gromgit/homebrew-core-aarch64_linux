e5338d759c76f76e915c504bff26c00429ddd088
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 08:32:39 +0000

    macosvpn: update 0.3.5 bottle.

--- Formula/macosvpn.rb
+++ Formula/macosvpn.rb
@@ -6,6 +6,7 @@ class Macosvpn < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f3265e0c3da8a09a691f6cff3b8408184c7423ed49d90a7b2b2290b3b3674d99" => :mojave
     sha256 "478bec17d2000763fde198848b85478aa8ece4414499a1cfc2dd8925071f15e9" => :high_sierra
     sha256 "fd31b7b30a4867a7693a35ce96a6d74034e160f8b7cae1bc37e8ca5083552a04" => :sierra
     sha256 "4cf716851d7a1fda3ed5387504d01d8832b53f54fd7b683267af29fc5d18f7a1" => :el_capitan
