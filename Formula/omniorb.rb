4d753167c685fbcd742b1986228bc15d201ee02a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 03:47:57 +0000

    omniorb: update 4.2.2 bottle.

--- Formula/omniorb.rb
+++ Formula/omniorb.rb
@@ -5,6 +5,7 @@ class Omniorb < Formula
   sha256 "ddd909ce31014be2beebf67a5e9fabbf03b5bb0c26b8c53ab64d470d77348ece"
 
   bottle do
+    sha256 "f6b45b799e2266812a33098aeeb1352c009f500ada7e660104dee8c04e6486c1" => :mojave
     sha256 "4d6e50aa2b7e921f95de4423576ad039549cbb10ed4ff034e4ebfac1ff2914fd" => :high_sierra
     sha256 "989ec3dfa6d2cffa72b377822404ff212c9c52b10c6ce1cf7e77ebd5a26a96b4" => :sierra
     sha256 "3c53f669e4832ee9e423bd1c124d3b3e7abd2f061b824fae408f5d553c61060a" => :el_capitan
