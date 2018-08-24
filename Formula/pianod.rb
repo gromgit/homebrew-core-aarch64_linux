0c15f16473bf2cb119d9ed1df12cb27b0d422cf3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 04:53:04 +0000

    pianod: update 176_1 bottle.

--- Formula/pianod.rb
+++ Formula/pianod.rb
@@ -6,6 +6,7 @@ class Pianod < Formula
   revision 1
 
   bottle do
+    sha256 "01b47d23674efaf3ddefced7f4dfee0db683d956ea455ebd9a8bd0e8f3cf7d9d" => :mojave
     sha256 "d0aa3614b2b70ca8140820bd059c1ebdbb54799b69ab6346f36a42a4dac600de" => :high_sierra
     sha256 "fe7983c06b7fe6163792e7e0eeed5990b9d09e44ffc19d0c62808503ca409f5f" => :sierra
     sha256 "3ed572ebaf767ba924cc7b402362775b77a19cd69337f940344988f86d2a1936" => :el_capitan
