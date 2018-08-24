812479bfe6f7a3a3f27e06f5c724a34bf853887a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 13:26:29 +0000

    vstr: update 1.0.15 bottle.

--- Formula/vstr.rb
+++ Formula/vstr.rb
@@ -6,6 +6,7 @@ class Vstr < Formula
 
   bottle do
     cellar :any
+    sha256 "8927c49aa4daba57ffab9a9ea332504346467cf22c137af3e1a16b859318a0f5" => :mojave
     sha256 "af6d9cc097c4eb9c1719496b2e29593763b5b17b279ef4c234d681cfe4174b37" => :high_sierra
     sha256 "07e2b05d9908a847c72950532d3ed12771c856365c8747c8c5917da9a5ea4413" => :sierra
     sha256 "d2d5b14e9ac589c782307e058e06815ad2408bbcf418ac721d3fac3be8b832a7" => :el_capitan
