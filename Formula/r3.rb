dbd32689813042cce8cd6351b73258d15d393d29
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 08:52:22 +0000

    r3: update 1.3.4 bottle.

--- Formula/r3.rb
+++ Formula/r3.rb
@@ -7,6 +7,7 @@ class R3 < Formula
 
   bottle do
     cellar :any
+    sha256 "f136221b1d7a0a4ee057ea0551a2b742d1a49cb50011e5651e8fa5c96327b0b0" => :mojave
     sha256 "5239e5302b1952367f6cdc066e43483de6b0d30fa70f1dcf2e9f03b10983890f" => :high_sierra
     sha256 "d39c22ae9e69454cc7c205ff0cecc3dd6084a38a1e1742091f55df389e5a8f4a" => :sierra
     sha256 "6122bbc3566581f130e54cd563ed69f169598f5ce62d6319e7b5a95b10b802ef" => :el_capitan
