9dcc94bdcab0be72bb07ae0988c5952a63c0fc14
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 12:08:10 +0000

    vcdimager: update 2.0.1 bottle.

--- Formula/vcdimager.rb
+++ Formula/vcdimager.rb
@@ -7,6 +7,7 @@ class Vcdimager < Formula
 
   bottle do
     cellar :any
+    sha256 "1dada6e9157e7e89d903a2cbc82ec12199ad7afb8384af90a4601bd99d4b18fd" => :mojave
     sha256 "a990e94922c3b6e779907c421cf9aafc10d2f5bce3196580d0274909a18f6cb7" => :high_sierra
     sha256 "e78e0d7842f71d68f5ef23ea13c64a04c012a523e0759b78a1395a21281c7b73" => :sierra
     sha256 "9094f54dbd4234a5fb6bd23ab39c2b4f87f7bebb5ccfb398942fb98df37813b0" => :el_capitan
