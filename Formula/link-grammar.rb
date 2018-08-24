aa0b531af78717d8607c4e5662fd0e573985f423
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 00:42:11 +0000

    link-grammar: update 5.5.1 bottle.

--- Formula/link-grammar.rb
+++ Formula/link-grammar.rb
@@ -5,6 +5,7 @@ class LinkGrammar < Formula
   sha256 "ce8934e3be937611e3dff4e2b61a2675380752ad6815b63336ebac917d9013f5"
 
   bottle do
+    sha256 "550961c684c2ff78765d558024d180cdfc044ff22655f242746ca7b4987b6c2a" => :mojave
     sha256 "bff34f809816e033a10581fee1c99ca4adbedeb30968fdb466ade860584bbf3e" => :high_sierra
     sha256 "68fafbe26912384cb709f2c107fa463c8844753ec7396e94d43b5ee36dc7f42e" => :sierra
     sha256 "fc038b1a0ab421ed785705cf4bc7e9a64113c735d60a270f4f3771b755a43612" => :el_capitan
