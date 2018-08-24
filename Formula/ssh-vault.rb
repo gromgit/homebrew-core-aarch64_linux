fafc88b952b7e26285ec2ea7cd467b01a14e1f21
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 11:00:53 +0000

    ssh-vault: update 0.12.4 bottle.

--- Formula/ssh-vault.rb
+++ Formula/ssh-vault.rb
@@ -8,6 +8,7 @@ class SshVault < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0626c48246281ef41d5153ed6b50349adef603a944b5b24e0575bb40b3712785" => :mojave
     sha256 "178173c00bedfa7a48c5381d03596e8164caeb7c3ccb4447579fcdf66149192f" => :high_sierra
     sha256 "886172cdae4a8cbbb7ab4caef859cc397c39de3b216044d920153e11d01cc828" => :sierra
     sha256 "74a103aab77feb90dede16015dfefac1cb27483630658fa6c0f5a93aeb7fefb9" => :el_capitan
