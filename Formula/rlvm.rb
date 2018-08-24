5f8db48ce741e2abbfe61012e52b091b543a874c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 08:30:49 +0000

    rlvm: update 0.14_3 bottle.

--- Formula/rlvm.rb
+++ Formula/rlvm.rb
@@ -9,6 +9,7 @@ class Rlvm < Formula
   bottle do
     cellar :any
     rebuild 2
+    sha256 "b24e490e6c84a78799ebf623ceef5905180a614ec87ad2a4ced072021a366d25" => :mojave
     sha256 "ffbe5c1893759c495572d8338f4b7d18bb37a8fb8a88e4d0ef11e1f552e591e8" => :high_sierra
     sha256 "bf5a864796942f72edf91b4ca86696050e694edd718418b01c23227f23c5fe33" => :sierra
     sha256 "d6d62997e9ca3378380e96fc7f1f633b755b88956d009195b3e91832b8466c64" => :el_capitan
