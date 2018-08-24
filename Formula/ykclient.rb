25b5690cb8d0b40741bc8ecca174b1d30c210f6f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 12:56:31 +0000

    ykclient: update 2.15 bottle.

--- Formula/ykclient.rb
+++ Formula/ykclient.rb
@@ -6,6 +6,7 @@ class Ykclient < Formula
 
   bottle do
     rebuild 2
+    sha256 "73b153bb1d9f5df3aa3a4ffed206c60bd1f207946b9cb43116985ed0cf76de8e" => :mojave
     sha256 "3e1459f192f7f1df756e2071c78ee41fd163b3dee1f09254e8e5ffc0442a2205" => :high_sierra
     sha256 "aec1bc9640c8a84089b1d749d689b59862fce858478d180cd6a34a93a34eb370" => :sierra
     sha256 "deee73fbd68f44bd86fb07d1f2179313dac4679395d861b821ccf218745ab1c8" => :el_capitan
