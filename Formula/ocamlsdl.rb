a510db316a72138fbd8ade991cdcb138e31a4b7c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 03:47:59 +0000

    ocamlsdl: update 0.9.1_10 bottle.

--- Formula/ocamlsdl.rb
+++ Formula/ocamlsdl.rb
@@ -7,6 +7,7 @@ class Ocamlsdl < Formula
 
   bottle do
     cellar :any
+    sha256 "e6d8087ac27d4c45abded02c279e898619be45bd8693e2ed649ae8a764798db1" => :mojave
     sha256 "5b3c887b0120e2c2bd245039a61112515d589ec612b5aa9549997c86ff0c7e95" => :high_sierra
     sha256 "d17971236af3fe65db605b25423399e239d622428b34157e6c4e636ed0a52652" => :sierra
     sha256 "c59f56c6176d701173eb224d430889132a57053cc1a4856940c272bda9d51cac" => :el_capitan
