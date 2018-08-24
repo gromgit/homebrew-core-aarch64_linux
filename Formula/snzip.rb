d04899bb41cb0714d235a7eb7b5ce778ac91f210
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 10:09:05 +0000

    snzip: update 1.0.4_2 bottle.

--- Formula/snzip.rb
+++ Formula/snzip.rb
@@ -7,6 +7,7 @@ class Snzip < Formula
 
   bottle do
     cellar :any
+    sha256 "0304142d75d2495662ea2dae386948830aada6a7f653a90a74a746e56a7e9ff8" => :mojave
     sha256 "fd4c734255707e1695f5d89a6dccc7d8b6a302771a71f6f6db0a054b9655d287" => :high_sierra
     sha256 "953a79f0aa028d4b5f13cc606ead6e225c290972db683947dabed58bb6748257" => :sierra
     sha256 "fdc031ce925717ee49048f3ffab3015f1039a06299f5093f7949e9a41cab975e" => :el_capitan
