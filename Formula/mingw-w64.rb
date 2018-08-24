a475299694c2b6d6d8c28ec8af9b2c04e53ab744
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 03:28:36 +0000

    mingw-w64: update 5.0.4_1 bottle.

--- Formula/mingw-w64.rb
+++ Formula/mingw-w64.rb
@@ -6,6 +6,7 @@ class MingwW64 < Formula
   revision 1
 
   bottle do
+    sha256 "99643788f39f714277782d585854b426be3a05b3ca4ea6ad976bddf04fad10e2" => :mojave
     sha256 "d8ee55c3d49dab59470fcd0c2d748faca388d65ac6a339b32f55c61bbcaa7563" => :high_sierra
     sha256 "123187cb05418b0dea7fc52ed884d0c29af3794a4729befeed645384816c9d8d" => :sierra
     sha256 "3d17f00fef77e7ca948989413a97955c6628d0dde124ef461d21f6b8528e592c" => :el_capitan
