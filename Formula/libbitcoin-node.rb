db0a308af135b70974fb6946e60609eda69a3e40
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 15:31:52 +0000

    libbitcoin-node: update 3.5.0_1 bottle.

--- Formula/libbitcoin-node.rb
+++ Formula/libbitcoin-node.rb
@@ -6,6 +6,7 @@ class LibbitcoinNode < Formula
   revision 1
 
   bottle do
+    sha256 "752a5c48f26edfba5d7684c25b25d9329efb91ce2875d368c62da19e3e8556b5" => :mojave
     sha256 "f2798e9c9fbd9e2ea2bbbb7233965bb50d02a96e90b2ef87c57a7c8bf0917752" => :high_sierra
     sha256 "7405c42f379048caada917eead1e620d9fe7683c8d6ce96f59fd0699d90bed54" => :sierra
     sha256 "62977c7dbcd04d4c4bc1c50a64f5e10ac20ae36fb9eff7f2ac34acb97259bad3" => :el_capitan
