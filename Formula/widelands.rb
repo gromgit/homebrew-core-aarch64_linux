dcb2271cc3b9c3a1a4f2ad85e11712a65e4b8eb4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 14:06:21 +0000

    widelands: update build19_11 bottle.

--- Formula/widelands.rb
+++ Formula/widelands.rb
@@ -6,6 +6,7 @@ class Widelands < Formula
   revision 11
 
   bottle do
+    sha256 "43e42b0992062b533fcd329b1a54182e23efbefdcef94ac9c8a1e9943c3de682" => :mojave
     sha256 "145386353807d7475c91a805a6024a844e5326116ede9195f98d717f5933bdcd" => :high_sierra
     sha256 "fe7fb90d75ea8e8f190860d4c5aed75f3cd4bf03b5b8d07e68ddb60d1c8b9422" => :sierra
     sha256 "4e3859c9f2025e83216237ebf326cb2d6baf658d950e51bb7d80ce2fc2827ac7" => :el_capitan
