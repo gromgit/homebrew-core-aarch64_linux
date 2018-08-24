542446ba8b1e8e589653e8df648ca91fc327f59c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 05:14:50 +0000

    ptex: update 2.1.33 bottle.

--- Formula/ptex.rb
+++ Formula/ptex.rb
@@ -6,6 +6,7 @@ class Ptex < Formula
 
   bottle do
     cellar :any
+    sha256 "d7c01bb272e283de0f1bba6f923f42a63513b419fbd2c02f512ad3dbd35b9a9c" => :mojave
     sha256 "4da50057c397b08a7dabbc621947f607b98cfbc887cd43f0bfa41960b36c6466" => :high_sierra
     sha256 "227c2b01f8398714d38c494061a630385dfd630e1b30bf5a651c14667c9f0621" => :sierra
     sha256 "12d030b336d928a80776188daa86fe4de608e5bc4516b8382d52a508a5a4091f" => :el_capitan
