8d015733995adc6a96ae7bb487ec01ac4c2f72a1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 02:43:35 +0000

    ncrack: update 0.6.0 bottle.

--- Formula/ncrack.rb
+++ Formula/ncrack.rb
@@ -6,6 +6,7 @@ class Ncrack < Formula
   head "https://github.com/nmap/ncrack.git"
 
   bottle do
+    sha256 "899782e40fc6cce6aaa19f37719a914d5a255e50bce22f1d834f383e2b5288fc" => :mojave
     sha256 "710a7c1bbc131cce0e0b5ceac7bee02c829362489a56a881b2011df7bcab7dfb" => :high_sierra
     sha256 "887e071b2c1468c82c1d4ab390f0bbb6c03a482f0c39e89a573105d2ce5cb38c" => :sierra
     sha256 "1ca0625ada66ae28f5bc488ecc7ca246d68711ab6b314c4f6998ab47ccba1ec0" => :el_capitan
