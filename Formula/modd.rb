51b14da4b4c8d1bb0fd89156131969320b1a13bd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 01:31:00 +0000

    modd: update 0.7 bottle.

--- Formula/modd.rb
+++ Formula/modd.rb
@@ -7,6 +7,7 @@ class Modd < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "33b9f69c73faa25b4bfec3ae6103cca7cd41862a693292fbe934c244b89e3aac" => :mojave
     sha256 "52d2eb11b69f8c6a3eec06e2d60864fd6bb32263f4f47f78d8ccd94f2f595759" => :high_sierra
     sha256 "68214544aa3797bdc878ba397bbb1ca5e100a5159c3b90640f27f5d7ea7215b3" => :sierra
     sha256 "300e0802cff775ae987e3036691b0474aececfb7f83819fdccd7fd1cce88df0e" => :el_capitan
