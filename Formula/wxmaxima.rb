42b43a8f1e2722d2a58e2f607d2003925983f2cc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 14:06:24 +0000

    wxmaxima: update 18.02.0 bottle.

--- Formula/wxmaxima.rb
+++ Formula/wxmaxima.rb
@@ -7,6 +7,7 @@ class Wxmaxima < Formula
 
   bottle do
     cellar :any
+    sha256 "c9d3d8af90adaa844bd47709ca0e1aafb9bfc879c50281f918c1051d3f565264" => :mojave
     sha256 "1cab5b45e11c9a53b04e88e49226d2e9eb0965f1367c86ad30915694cea1eba6" => :high_sierra
     sha256 "b44af0b7c1a8aac7d5a5270eaafa9d21c1d3e84940784e6fe343860d9b1eefbd" => :sierra
     sha256 "f742fa1359c964822066034bf08d7a454e5d73546e2138f63deb4de79ae5c9d6" => :el_capitan
