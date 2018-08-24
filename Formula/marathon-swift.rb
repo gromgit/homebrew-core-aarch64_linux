2d4cd490c0ca3fccf171a0e7a71eca014d674242
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 08:32:38 +0000

    marathon-swift: update 1.0.1 bottle.

--- Formula/marathon-swift.rb
+++ Formula/marathon-swift.rb
@@ -6,6 +6,7 @@ class MarathonSwift < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d17beec1d5a1723abfad9656c8ead0f727ea4af509dea5c57bba53719f9a4892" => :mojave
     sha256 "14541e2edb49c0eb2118b61ff0245e0654dc49d4b48e461df6cb1d8c6698112e" => :high_sierra
     sha256 "80fc6f4b1ff33081f4b27091ea42901c3921e00f6ffc3a2017662e6905e562df" => :sierra
   end
