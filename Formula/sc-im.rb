22685f96f6415a274c42ac89a4e5a3038755dbb6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 09:52:01 +0000

    sc-im: update 0.7.0 bottle.

--- Formula/sc-im.rb
+++ Formula/sc-im.rb
@@ -6,6 +6,7 @@ class ScIm < Formula
   head "https://github.com/andmarti1424/sc-im.git", :branch => "freeze"
 
   bottle do
+    sha256 "67180ab11eedd56f8eaffb0d2f12a90ca9636bbd93ff693914450be8248702ce" => :mojave
     sha256 "275a0a9dbd1a1271119e36b2767a54587aae57a65ee92278e701e0e1236a192c" => :high_sierra
     sha256 "f346970ef805cec01ae6485365d8fb5002533255c01e81bdd44d072058d00081" => :sierra
     sha256 "50e8d50e0373ac626ad617057eb1246c779e1e3b05171f4be2aa547c5b8ddf4c" => :el_capitan
