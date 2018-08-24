77c04affc3835799ae9571e3275d99071f1dced4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 13:26:27 +0000

    vgmstream: update r1040 bottle.

--- Formula/vgmstream.rb
+++ Formula/vgmstream.rb
@@ -9,6 +9,7 @@ class Vgmstream < Formula
   bottle do
     cellar :any
     rebuild 2
+    sha256 "dd7dae465bb50700fcf448c3a5fc0478600c6151eeade7196c88202aa19c2f4f" => :mojave
     sha256 "970428e954d82c2aaef2da083320fabd21ae7ba866fd2055d5fdcdd21f2989b0" => :high_sierra
     sha256 "b85d6942270fc9024de56e4fe08618f7ffce1499f4c9faf3f31b246310a17511" => :sierra
     sha256 "980226be71f7ba16f71e7cd4ba53a4160c03cf9308036d014538b1feb8285d08" => :el_capitan
