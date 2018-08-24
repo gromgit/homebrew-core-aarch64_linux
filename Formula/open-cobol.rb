b85e2650a1f01a1bb77f8a3688c047ddec638feb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 03:47:56 +0000

    open-cobol: update 1.1_2 bottle.

--- Formula/open-cobol.rb
+++ Formula/open-cobol.rb
@@ -7,6 +7,7 @@ class OpenCobol < Formula
   revision 2
 
   bottle do
+    sha256 "633c325469f52c99c437a44dc2647e18fd8a841caa1779e1443cef1b898d1b75" => :mojave
     sha256 "c4f335dae8e5235f9615a87a018fe89b05857974023ff1973ec198705b3cd44b" => :high_sierra
     sha256 "c05a8ff66d8ad775049c2e1f400c4bde52d7f0ec13d7b2b40d94d9fc4b451c01" => :sierra
     sha256 "c9fc1e375ec061b9a90a5cc29446a61fe7f49e75d1a78c6888adee889e9258d5" => :el_capitan
