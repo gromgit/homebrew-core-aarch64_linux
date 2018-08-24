e1f51a4895d28992f29d034c2360bb5194298ef3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 12:10:02 +0000

    tpp: update 1.3.1 bottle.

--- Formula/tpp.rb
+++ Formula/tpp.rb
@@ -7,6 +7,7 @@ class Tpp < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "bd92be45cec9c61438618155e9d22fe58c03b0feedbd8272a214d20edf37f16a" => :mojave
     sha256 "e132735b420b285a5ffd5f6946d93a2e67f8797e07f00d2bce40f8c7989ff65a" => :high_sierra
     sha256 "8736306dac4a3d2a2ed8bb4dcd1c08c77fb9026b9cde5ad07791eb90eef2392f" => :sierra
     sha256 "25e92e9f229433131cc82cf48a3cec90d19a28a08a56fadcc095b1ecf4df2304" => :el_capitan
