95496153a619f8b222c7dde9ff8f08ccdce01314
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 12:09:59 +0000

    ttyd: update 1.4.0_1 bottle.

--- Formula/ttyd.rb
+++ Formula/ttyd.rb
@@ -8,6 +8,7 @@ class Ttyd < Formula
 
   bottle do
     cellar :any
+    sha256 "a43dd374f6b62d64e41b869b30657b9b823c81fbffefdc67ead877bcec15f2b1" => :mojave
     sha256 "4985fdd0d5d2ac07bb33733b34aceefaa0dc5cb5ae1195a01414679782d3c663" => :high_sierra
     sha256 "5f33c79299fdd719ac076a067733a45aa15793e59a1ebf761cb5fd55cd2424d2" => :sierra
     sha256 "999e8c8645e1d053802f376819d09846445749673ba60bc1450b6b8ea9b9b4e1" => :el_capitan
