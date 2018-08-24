9537aea7c649a49bf080b5614a0c1e466a775012
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 08:32:38 +0000

    mdxmini: update 1.0.0 bottle.

--- Formula/mdxmini.rb
+++ Formula/mdxmini.rb
@@ -7,6 +7,7 @@ class Mdxmini < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "e93281dc0c64642e33763f0dc2a4cfa0a6da0dd4739222b0411e54913435ee27" => :mojave
     sha256 "5bf36e82084146ab4604b4746bcf6634cfe4268f2044712e4d13519b21ab5165" => :high_sierra
     sha256 "8e0daf3d508dad59074c567b8c8e60bd88c8026b7dfe1305e4e9c50ec5d8fbbd" => :sierra
     sha256 "d20b94107c25833096401be6336544f283e6956758d4238e207e6a4e34fa5fdf" => :el_capitan
