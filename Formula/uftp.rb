c00de10999142dbec42364585e2fc7600dee11a2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 12:08:15 +0000

    uftp: update 4.9.7 bottle.

--- Formula/uftp.rb
+++ Formula/uftp.rb
@@ -6,6 +6,7 @@ class Uftp < Formula
 
   bottle do
     cellar :any
+    sha256 "3dbedc792991b8fc5216041948eaa8a0ccc0ac8a30868f472442d7e689e4805a" => :mojave
     sha256 "daee417f3a56167dc5a40341c510119d1bc5af8daba600ad02d2d25afcb9fa0f" => :high_sierra
     sha256 "662cb4503e70bb2a3e369f4f012e0f96ecff7a7f6d929aa2a8080524b95a9f51" => :sierra
     sha256 "723329b24e43142966981c56d6bc843f228fe1504171afdd9425e50d26b9a22e" => :el_capitan
