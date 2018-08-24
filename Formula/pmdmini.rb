1ae1d7275ffa9712a8c4602323965de70fc56558
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 04:53:02 +0000

    pmdmini: update 1.0.1 bottle.

--- Formula/pmdmini.rb
+++ Formula/pmdmini.rb
@@ -6,6 +6,7 @@ class Pmdmini < Formula
 
   bottle do
     cellar :any
+    sha256 "fe87429ee546fa0629d178c52476c4cc5696abac76b21abcd3e4977c7527bd22" => :mojave
     sha256 "c3195012d5b5333e76c1a8a44b3f734575540deee884dfb6685e139e1038c138" => :high_sierra
     sha256 "59b287650c6e40c20da8000f5e73b910f8096bd949e4432b4f11e70b1c779a5d" => :sierra
     sha256 "72afd84c66fef9f142a1922fd0995a6a173b46c40d06715808345cc1c71b6702" => :el_capitan
