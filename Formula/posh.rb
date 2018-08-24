a109a5b8454af11bf6eaab53ae5d754125619fbf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 05:09:56 +0000

    posh: update 0.13.1 bottle.

--- Formula/posh.rb
+++ Formula/posh.rb
@@ -6,6 +6,7 @@ class Posh < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9ae1e9c7c227d5c7ce6e5aede848668c1268ffddd66029a57ae6352fd75d179b" => :mojave
     sha256 "548e5330c43d793d53a5367a20c8538659c2b01ec89e1e38adb77af84220f56d" => :high_sierra
     sha256 "b3c765a3f11f264415f1438db4c20e77c4202e8a894d67be5ab60ce2bbfb30a1" => :sierra
     sha256 "d8018f086dea30a4225641c56255dff05a45d5eea16707822c1e13364b44ecbc" => :el_capitan
