f60a7aa2542c98cd92816d25400f6a58e1250f80
Viktor Szakats <commit@vszakats.net>
2018-08-24 09:27:36 +0000

    libevent: secure url(s)

--- Formula/libevent.rb
+++ Formula/libevent.rb
@@ -1,6 +1,6 @@
 class Libevent < Formula
   desc "Asynchronous event library"
-  homepage "http://libevent.org"
+  homepage "https://libevent.org/"
   url "https://github.com/libevent/libevent/archive/release-2.1.8-stable.tar.gz"
   sha256 "316ddb401745ac5d222d7c529ef1eada12f58f6376a66c1118eee803cb70f83d"
 
