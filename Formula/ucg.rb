dce36d2bf782f05e800544a899905f571db9bfc3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 12:08:11 +0000

    ucg: update 0.3.3 bottle.

--- Formula/ucg.rb
+++ Formula/ucg.rb
@@ -8,6 +8,7 @@ class Ucg < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "2b073d5a0ee4cb2fe71822ce27c014b00974298cfc656f1fe0279f58bde398f9" => :mojave
     sha256 "3642e741bc9ffd1cf1982bfc2c373e0ed54a417c08ac46f43e4cf8a09a0a1a8a" => :high_sierra
     sha256 "dddf2d62a2e582a9356a4da6434ddb0432ce6d6b1d50a5dc6728f9b6254531a5" => :sierra
     sha256 "e4699a681ae9d2e9d68ce7da85fc050907070bc9650fad0c4f76faf991f3a422" => :el_capitan
