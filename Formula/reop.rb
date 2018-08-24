9fa7049601a1a40bc0755fe9653111692b19c095
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 08:30:46 +0000

    reop: update 2.1.0_3 bottle.

--- Formula/reop.rb
+++ Formula/reop.rb
@@ -8,6 +8,7 @@ class Reop < Formula
 
   bottle do
     cellar :any
+    sha256 "bde75b5da1a958623875e6b05c1d208ea22b712549164a37c7e68f9ee2d5aadf" => :mojave
     sha256 "e83e45b44f322b38e9476623d7a2e595b593a8499e06c94dd1124a10d59c0d49" => :high_sierra
     sha256 "8c0d3dd8ebe6732ec4d7820da74e12d7dc6b57ab79548063b1619213dbe79c19" => :sierra
     sha256 "a0d7ad0c9059426b6400b9294d27a7672789da0cabe051faf11b46b1121684d0" => :el_capitan
