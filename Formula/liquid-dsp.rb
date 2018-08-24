9fa21bacdc00a78f73b1c5c75b27937a8fef3e0e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 00:42:09 +0000

    liquid-dsp: update 1.3.1 bottle.

--- Formula/liquid-dsp.rb
+++ Formula/liquid-dsp.rb
@@ -6,6 +6,7 @@ class LiquidDsp < Formula
 
   bottle do
     cellar :any
+    sha256 "f8f9a384db2f8832e7f31fa61223a3119a9278d9d6af8a2b11b6ec76c477edad" => :mojave
     sha256 "5d12243dc96220b6cc34ea32e440c91fd99c9e39f91f08afbbdd889005d06ecd" => :high_sierra
     sha256 "d58108029dc01dbf26931044f74afcf75d81b7de867fc1904e281ea02acd2dfe" => :sierra
     sha256 "8e979793bbc8c8101195a9107d1be8d10f6809d14982eb4e464019cf1914f577" => :el_capitan
