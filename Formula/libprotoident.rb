e3fb4cb0348fb5054f7e32c9f1681e05ba848f7e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 15:24:36 +0000

    libprotoident: update 2.0.12 bottle.

--- Formula/libprotoident.rb
+++ Formula/libprotoident.rb
@@ -6,6 +6,7 @@ class Libprotoident < Formula
 
   bottle do
     cellar :any
+    sha256 "1b68bd65266f6d2eedf238579d25abd4be78bbdde83a4947bb9c66668c783505" => :mojave
     sha256 "48a4b98343c8a875dc429c11f0e65b2d4afbd66e631aa390deb71eca16dfc55d" => :high_sierra
     sha256 "e353b002c9c3180965daadadf13273ab2f74fc6ec4701ba99a8231aaf4e9e3e0" => :sierra
     sha256 "9a631567b1642854a7d8572f047cf4e02fff34bae11c43e7d91dbf16c0529a3b" => :el_capitan
