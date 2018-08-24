698f7a756a543092af83bd84b7584a33632c145a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 04:53:03 +0000

    png++: update 0.2.5_1 bottle.

--- Formula/png++.rb
+++ Formula/png++.rb
@@ -7,6 +7,7 @@ class Pngxx < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0a5221ea49fb845c830698b2d1c170cb8d59d18916ca843a5c80ef7d48b37435" => :mojave
     sha256 "4085580bff1ec2116c321e84a426d195943a002f84b4662c9814a64e5925c627" => :high_sierra
     sha256 "41785b523dc50e5b237d83d027b34b6532114a6751eff3f409ee0ba9e0730f04" => :sierra
     sha256 "7b01b3ff0af9e60f2887bb45ff5ba2f5823a9a440c2d78e51e69904d3edd80d8" => :el_capitan
