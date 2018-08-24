53141d17bcb7187e808739f1a26fdcd563a9b696
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 05:09:55 +0000

    psftools: update 1.1.10 bottle.

--- Formula/psftools.rb
+++ Formula/psftools.rb
@@ -6,6 +6,7 @@ class Psftools < Formula
 
   bottle do
     cellar :any
+    sha256 "710ec6da54d533e4ccc37c87093408ec5519d67852bda95485c1ac35564e0a07" => :mojave
     sha256 "40c3b6b56dfa842d6a3058d5082bbe16dd3ef0360258279cca98f9da05ec9cd4" => :high_sierra
     sha256 "532260f14b0b77301c7bf6b89ae1cf7f5948f26dc9d27898bb5588cb9cd4f99e" => :sierra
     sha256 "ecfe3cabea7cb93be49b1189394253c09f8614877e76f8d7f59a7c1e05bc0128" => :el_capitan
