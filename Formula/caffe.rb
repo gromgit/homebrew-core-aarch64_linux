6379434544ce8700396750e1cb6afa2e4fe1ad4c
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-24 02:52:39 +0100

    caffe: revision for hdf5

--- Formula/caffe.rb
+++ Formula/caffe.rb
@@ -3,7 +3,7 @@ class Caffe < Formula
   homepage "https://caffe.berkeleyvision.org/"
   url "https://github.com/BVLC/caffe/archive/1.0.tar.gz"
   sha256 "71d3c9eb8a183150f965a465824d01fe82826c22505f7aa314f700ace03fa77f"
-  revision 2
+  revision 3
 
   bottle do
     sha256 "37b3112edbfcdf5ded4d914d10fe29c9b676fb339f18fd9bc7f361be80083aa0" => :high_sierra
