9fe4097216d7c8dacb5a895eee6c18c3a36dbb2a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 08:32:35 +0000

    mednafen: update 1.21.3 bottle.

--- Formula/mednafen.rb
+++ Formula/mednafen.rb
@@ -5,6 +5,7 @@ class Mednafen < Formula
   sha256 "2e761e8834b098b7f1ab35dccaa6d2be715ee9106cf40af4919f6ca4b99ee3c6"
 
   bottle do
+    sha256 "1817b1b46cc5e573d9719d5619e9e78b8ea4e9bfbe7d911b808ddcefbd5a701f" => :mojave
     sha256 "d4e4e7db27ba03192573ffd25cc1001aa6fee082c125f0aed68aebf6c84068f3" => :high_sierra
     sha256 "ff2cdee178008a503e56b4ea5d607d4bee39360232c220eeda0f4fb572dbad92" => :sierra
   end
