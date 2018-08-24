83dda4fe2614ad89c3d9715ce0a17f3f580874bf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 12:56:33 +0000

    yaws: update 2.0.6 bottle.

--- Formula/yaws.rb
+++ Formula/yaws.rb
@@ -6,6 +6,7 @@ class Yaws < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "68a2f3e85c7cac4a6377c5eb58bd0a3ae5dc6fb7a72a682e507dbcddf5fa4c48" => :mojave
     sha256 "6dddeec2cbce08b47d7b14e31d04b6e2e803965c47f44258dfd136b23f2e531c" => :high_sierra
     sha256 "5eb3a9b15641e43e00b9ffb4626d32fa2931a57b1f492c2b2a641036005d6f1e" => :sierra
     sha256 "f04163aed1bfe0397bc639d9903f80de457ca4391789049fc13e4f8a6410c798" => :el_capitan
