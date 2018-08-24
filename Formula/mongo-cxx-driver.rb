c6d96af0b32e15a63453d30e08b9add7499a7a2c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 01:31:04 +0000

    mongo-cxx-driver: update 3.3.1 bottle.

--- Formula/mongo-cxx-driver.rb
+++ Formula/mongo-cxx-driver.rb
@@ -7,6 +7,7 @@ class MongoCxxDriver < Formula
 
   bottle do
     cellar :any
+    sha256 "69ef208cfae61367e8aa0ffa3890a1102d2fed7ce85d030815baf15220274fea" => :mojave
     sha256 "98fdbc9e04c35f3d215fdc1e84a0fc6f41ee50ee8c50fd6682124d1f56e17198" => :high_sierra
     sha256 "5f6c5545d72aeb81fab6c5a1ae2d285f7063d970018c71f3e62c9731b14555eb" => :sierra
     sha256 "4a6132cb86de1ce4bfd2bb171b9a935ae90e8e0d59512a79bffbdceee3113849" => :el_capitan
