062e70d326921bfd1e453ab2d12537bb8605a63f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 09:12:10 +0000

    rsstail: update 2.1 bottle.

--- Formula/rsstail.rb
+++ Formula/rsstail.rb
@@ -7,6 +7,7 @@ class Rsstail < Formula
 
   bottle do
     cellar :any
+    sha256 "043bb4c59d45bf3d10e8a80c16dd684267c7ee905fd3384d6f197dff9d9ca686" => :mojave
     sha256 "972ee73523d6f2f90ad8cefea3be3d800ed56ef86f5ab0aa27a9868959e8c4f6" => :high_sierra
     sha256 "288a7d9668a50fc1db8525be5c1ef3a932e134b095bb6d0b5a785353ca8b6d59" => :sierra
     sha256 "671b60685eabd26014203b5cd2eff8a94ee940e139c77793b379e1114c23a912" => :el_capitan
