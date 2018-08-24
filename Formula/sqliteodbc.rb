803b7041b508f53b14323953b5bc1acdcc0aaa2a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 10:09:05 +0000

    sqliteodbc: update 0.9996 bottle.

--- Formula/sqliteodbc.rb
+++ Formula/sqliteodbc.rb
@@ -5,7 +5,8 @@ class Sqliteodbc < Formula
   sha256 "8afbc9e0826d4ff07257d7881108206ce31b5f719762cbdb4f68201b60b0cb4e"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "2ef4d4e9285b65e4efb94592cb10f175c95c0f050f48b5ab326aa2b804761c28" => :mojave
     sha256 "7d825f83232825a51c8fd871368a2ff8cce3a76fe9ba1646f20e15f121ecf79e" => :high_sierra
     sha256 "907c1b32398eb7f3276e1da956723ac27868d9bcae27fb55ef76277cf2f67cb7" => :sierra
     sha256 "7c550f77c2db4e4927b9f23ed8a57610727b438f6a0fad98e1adecee3c8c1aa1" => :el_capitan
