35dc36d6e0354e5dfacb14772e7a154475855be7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 05:14:51 +0000

    pyexiv2: update 0.3.2_5 bottle.

--- Formula/pyexiv2.rb
+++ Formula/pyexiv2.rb
@@ -6,6 +6,7 @@ class Pyexiv2 < Formula
   revision 5
 
   bottle do
+    sha256 "c54c8221f94715c9226e26f5d050bec8619c66d5e553f090ab58b720b135887a" => :mojave
     sha256 "2dfb1f1cb5f11fe496f5b873a37d05fee2530da136d6f2df4152ba6263e6f9fd" => :high_sierra
     sha256 "01d50cbe2ddad34cb412b20de81c6594ff1a9ed5f139894fa14a120717c7b08b" => :sierra
     sha256 "53fb18abf4fc93c31ec50ed8d1c6ac51772176846ce7a07674058b9e78e59538" => :el_capitan
