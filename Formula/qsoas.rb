c2c0a800f4934b77d00351e56fa8f13d11613488
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 08:52:24 +0000

    qsoas: update 2.2.0 bottle.

--- Formula/qsoas.rb
+++ Formula/qsoas.rb
@@ -5,6 +5,7 @@ class Qsoas < Formula
   sha256 "acefcbb4286a6e0bf96353f924115d04a77d241962ceda890508bca19ee3b4f6"
 
   bottle do
+    sha256 "95be99b152857cd84d23c847d66ec90ed5dbccb245032aeaa63f9cb1c000c72f" => :mojave
     sha256 "2864c431780caeff59926d6c51d2a5a925814b33d172b03065f8713d2b9d49ee" => :high_sierra
     sha256 "8fd2e6ddb2224a9b00218572dc9e6f74ae45df730e468250b8674cf116975471" => :sierra
     sha256 "34be95fb5a3919dad8d41579e1822bd9ba46a553c96b4f8f90268ef3491e228a" => :el_capitan
