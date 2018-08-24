a5f5b41f05daa51c79387f1d7c415784a0ee4150
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 11:00:53 +0000

    star: update 1.5.3 bottle.

--- Formula/star.rb
+++ Formula/star.rb
@@ -6,6 +6,7 @@ class Star < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8d1e4d304f4ac9c281f3b445f31a1268271eebba6a58f098b4f9339be51218b9" => :mojave
     sha256 "9f4a24f592647071a2ead26c2dba4d86cb664f71cdf4d280037a94748c92ec0c" => :high_sierra
     sha256 "ec7a276b68c0dc946d3320e3cd9cf923d0affdbfa72587ecccb2efa3dc7276cc" => :sierra
     sha256 "64288e33524b1d1afcc5ae7e6ff5dc1488f1793eba9452e54279054d55e93db3" => :el_capitan
