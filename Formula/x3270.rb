17d71ba812193b860e83a4c8bb5cf826d4dc3ec3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 14:06:22 +0000

    x3270: update 3.6ga5 bottle.

--- Formula/x3270.rb
+++ Formula/x3270.rb
@@ -5,6 +5,7 @@ class X3270 < Formula
   sha256 "bebd0770e23a87997fe1d2353e4f1473aabe461fdddedccbb41fd241e8b5b424"
 
   bottle do
+    sha256 "910ce0d5e1b99a3bf06f74da9a73bca06478e2b18a719b1b9b7d2f45d50eeda4" => :mojave
     sha256 "f67129f6e0d14448939953d4c8966a5770d8a65aecdf42108f43dd4914dda9d8" => :high_sierra
     sha256 "08de119ad4c2626d8f8b5da84976601b89f1428af4274c2f5e1ed48b3805254d" => :sierra
     sha256 "c82c4f5ceb379a44acab4592f3d1d9cd05d499541b806397bd656e2152474815" => :el_capitan
