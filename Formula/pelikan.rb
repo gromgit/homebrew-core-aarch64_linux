141818201b8caea59989a7ec22f0bef45dcdb3bc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 06:02:39 +0000

    pelikan: update 0.1.2 bottle.

--- Formula/pelikan.rb
+++ Formula/pelikan.rb
@@ -7,6 +7,7 @@ class Pelikan < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a313660eb003974995537cef07e391d3051218f7c65f3326c270b68f0855a59f" => :mojave
     sha256 "a80ae1b508d4eae75d03fc5ad07477039a50a37419681b2472af4f9dc5f240ea" => :high_sierra
     sha256 "37a675674b7ef33f07099029042f56c054f09b5d22400010d583fbfa41c0ce50" => :sierra
     sha256 "e314ce6288bf76e271bf69ce844e2e846b16cad68ce635faf1e5130c3c6911d0" => :el_capitan
