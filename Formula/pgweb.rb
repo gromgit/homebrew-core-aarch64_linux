be55ea3a6879a6b45a2f46f08b63151a562035d0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 06:02:37 +0000

    pgweb: update 0.9.12 bottle.

--- Formula/pgweb.rb
+++ Formula/pgweb.rb
@@ -6,6 +6,7 @@ class Pgweb < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a71e56d151da1f158c24f669cfb44f03a7f2b80d8171e67e03e12bd8d1ddfa3e" => :mojave
     sha256 "31ef10e4430148ed0b70f32d1f6edf7c8741e02f4f2fbe21fd7c49783b3e0594" => :high_sierra
     sha256 "1f85f4bdf34399632b1a64a79ef39810909ba5faa3022def736caed9a898ff9a" => :sierra
     sha256 "640b9e3819d9915f00f39bb4319326a5d2f04ed75df4907f9a79db5231015812" => :el_capitan
