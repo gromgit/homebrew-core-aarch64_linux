4c6b1be198e9d734a155b9706d72087c1debd42b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 02:43:37 +0000

    ncp: update 1.2.4 bottle.

--- Formula/ncp.rb
+++ Formula/ncp.rb
@@ -8,6 +8,7 @@ class Ncp < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "345fcd7aaa3fbe0352a0109c986389db0cec91b4603829b4f36998ad866e5aca" => :mojave
     sha256 "2c56e482f1608eeb3925f4db0b1ef782fb1644d2a7318e522a4a3c7ba7efcdd4" => :high_sierra
     sha256 "d261de84549f890b4a3ca4c9f9d72ec25dd5a66c77fbccf6c29577c49a3c0866" => :sierra
     sha256 "08f17ef57ee62eb3ca90c7c033fba68edc2822d93689ddabdcaa49913c98f369" => :el_capitan
