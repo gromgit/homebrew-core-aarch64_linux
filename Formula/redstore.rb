e06e2c88c79405889b425f9d92909cd4fd45b1c0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 08:30:49 +0000

    redstore: update 0.5.4 bottle.

--- Formula/redstore.rb
+++ Formula/redstore.rb
@@ -6,6 +6,7 @@ class Redstore < Formula
 
   bottle do
     cellar :any
+    sha256 "a17c99ed5d7162eb742eef7b8764c64082fff26895baa81cb26cb75ced03db5e" => :mojave
     sha256 "fbd9814ed5e35fb1c964d6f581edebfc35e7d35cba0b89ea735247131c5559ac" => :high_sierra
     sha256 "e507eab072e33f0cee1ca08efb51ab06d65cee3a64248ec6badbd4f601f5c674" => :sierra
     sha256 "5ae64e4a536011ef3f68d2e8b4253624f60995025064de38f3a38308dd005421" => :el_capitan
