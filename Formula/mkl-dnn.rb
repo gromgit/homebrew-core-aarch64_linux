7e7064a1ce14b689ac3c68bfc6da3cef7e4764da
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-24 03:28:33 +0000

    mkl-dnn: update 0.16 bottle.

--- Formula/mkl-dnn.rb
+++ Formula/mkl-dnn.rb
@@ -6,6 +6,7 @@ class MklDnn < Formula
 
   bottle do
     cellar :any
+    sha256 "ea0a6be86a6e6f2028289e7af731a593d36a5d2f13679434c1e8af7dd24dec48" => :mojave
     sha256 "c7380ab2f4619ab06e6995ed889043cd9d7afe6d746f3edb4a00fefe5dcc01e3" => :high_sierra
     sha256 "2bf8dd79f82d7733052274611827a4afa3fde74f6633378fce36b4b54fae7f19" => :sierra
     sha256 "d870ac03f81c43d715dc5f026ec3acc9fb949986bf2ca8c2f28902f3f62dbac7" => :el_capitan
