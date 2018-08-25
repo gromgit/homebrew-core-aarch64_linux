a43545bfab6a2dccd70ba47c11342fb950256ed0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 14:19:24 +0000

    nativefier: update 7.6.7 bottle.

--- Formula/nativefier.rb
+++ Formula/nativefier.rb
@@ -8,6 +8,7 @@ class Nativefier < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6ff8f0781e4d2f0c0eb19cdf118bb461120537b32ef852e6a83986877b1a4ad8" => :mojave
     sha256 "0103844189063e290aef722bd5f0741414e1234d4ca0d41f4a070ab8b338e075" => :high_sierra
     sha256 "a8a8ab5edec987caa1cb7cff560411592da6ccc3499a8b2ed5230f929ce2342a" => :sierra
     sha256 "6c9614291d3936c7ead5d050968748bfe557e5888852c9685df8dddb61ac55ce" => :el_capitan
