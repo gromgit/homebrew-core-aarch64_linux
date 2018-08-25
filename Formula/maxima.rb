6c31aa6d041a90c035adeefa59c962df4757aeed
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 16:42:30 +0000

    maxima: update 5.41.0 bottle.

--- Formula/maxima.rb
+++ Formula/maxima.rb
@@ -5,6 +5,7 @@ class Maxima < Formula
   sha256 "daf8254600f4c7d9f079a0084178aaf036bd97a63b1e77b945f4120d7e401674"
 
   bottle do
+    sha256 "252b0b5b0d355c288f85bfa403212431a7a0c7455f4f616cda5960657a676f74" => :mojave
     sha256 "557d50c32c4af36b4e0328678e57eccb5c90a4f926d47df1e9943821c933e90a" => :high_sierra
     sha256 "5ae7804b062b9bdaf3d8983e90f6370509558158ed5a7b5fec9e7ea9516be3bd" => :sierra
     sha256 "3a5894e1ff75af2e3bc1a6fdbb277bc17fa90d05f23d782f4c5b879dc6c9f48c" => :el_capitan
