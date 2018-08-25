d405b3c6f0d3bc4744f8442a14c8d3b7d5df421b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 13:44:07 +0000

    cayley: update 0.7.4 bottle.

--- Formula/cayley.rb
+++ Formula/cayley.rb
@@ -7,6 +7,7 @@ class Cayley < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0f04d33fbc9b14445fde2b9fb7015b18e9559c9e90fbcb29c116dfdd3ab81e64" => :mojave
     sha256 "9a1b558fe6b45116bba953495630eaa0690cfd85c13ff5dbf1a152ec28d17278" => :high_sierra
     sha256 "58ef82f20c9db1fa8c472d7c9958bea9a58f693c879b934c526636a3d852340a" => :sierra
     sha256 "45699dd103618510a86c61a57cb32c7d3dabec1718d05e2b0dee278b3f193ee3" => :el_capitan
