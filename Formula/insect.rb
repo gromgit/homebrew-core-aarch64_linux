c2ab8cd1750acaaa8864fb2da7944721997ae74f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 14:22:13 +0000

    insect: update 5.0.0 bottle.

--- Formula/insect.rb
+++ Formula/insect.rb
@@ -8,6 +8,7 @@ class Insect < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b00b7c4ab9f8edfd724ec2d3b602454669b4c29b0a5b64fed513754a1c6d150a" => :mojave
     sha256 "150801ee2a294e0dc6d875bb1c251084f201934a8e7edb4c8cd703dc0be305af" => :high_sierra
     sha256 "85ec8ee95c28e34ce1fffea11f6d127325e287ca846f9ab3f9f50f15dbfe635c" => :sierra
     sha256 "5f063d4b5c7b31738bc0f7c6789a9a8ea4e91bb2981d4326ea3224690a4d66c5" => :el_capitan
