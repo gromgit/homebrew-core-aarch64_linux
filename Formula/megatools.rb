81e634d3d54bd00ef9fdb9e4a6e21bb0a9f557e9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 13:19:27 +0000

    megatools: update 1.10.1 bottle.

--- Formula/megatools.rb
+++ Formula/megatools.rb
@@ -6,6 +6,7 @@ class Megatools < Formula
 
   bottle do
     cellar :any
+    sha256 "84018a1925024622eb353c091014f2f103248270a19ebf80fd4e89114e9a71fc" => :mojave
     sha256 "b907af7c88a5b1f23643bd5576050ba6e228a0475246e8645b8f0d71dd97d81e" => :high_sierra
     sha256 "67fc6d534eb92276f21feea903ae682f8b5318da9eb59b1a94cc3887f9855c1f" => :sierra
     sha256 "e0bae7222466e43ae48615a9cea006527be27d4d44b9943b0b37d7e45f623704" => :el_capitan
