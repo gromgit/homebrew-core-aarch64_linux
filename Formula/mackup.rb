40352a95df587d6ff00a4267b22213a9382d4960
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 12:56:57 +0000

    mackup: update 0.8.18 bottle.

--- Formula/mackup.rb
+++ Formula/mackup.rb
@@ -7,6 +7,7 @@ class Mackup < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0d65e929160426fb570d378bdfadada7378cce9a73c053e2ae38a54cc7383973" => :mojave
     sha256 "b2087f95402ac5cb65f9e26636e86989f66922a24e22938444bb0ee9c84216b1" => :high_sierra
     sha256 "b2087f95402ac5cb65f9e26636e86989f66922a24e22938444bb0ee9c84216b1" => :sierra
     sha256 "de89332a8a2ab48d0b71f1d24f444f6376cbba4077f9fa1214b03a20a58538ec" => :el_capitan
