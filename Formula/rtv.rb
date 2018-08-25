3a4373a1031c115f95d493541f2e35e2051cf676
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 12:56:55 +0000

    rtv: update 1.24.0 bottle.

--- Formula/rtv.rb
+++ Formula/rtv.rb
@@ -9,6 +9,7 @@ class Rtv < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3395c998fd2fc8e47ad4b61844e705b31d2df9bb10a3ac5d5c1b7737b040c0a9" => :mojave
     sha256 "43547bbcb11ace05aa7d017528321da7ae2221be9dfc5cae7e75e5a2c6d27147" => :high_sierra
     sha256 "9794fc2530b06c1f4bb4c4f437ed1c94c12ff1848c40cefc62faa628e2cd0bca" => :sierra
     sha256 "fde14e71bc242873257fce9f81c4f73f3c6536a439813df0dfe52339166f0472" => :el_capitan
