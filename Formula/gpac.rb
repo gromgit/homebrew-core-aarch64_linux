587a0ea316c6aa37bef361a6a780a327146889e7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 21:11:47 +0000

    gpac: update 0.7.1_1 bottle.

--- Formula/gpac.rb
+++ Formula/gpac.rb
@@ -41,6 +41,7 @@ class Gpac < Formula
   end
 
   bottle do
+    sha256 "e035d74171aa4179277bf62734f21fb06688f0a7b10bb6ac57bcdc7124b0453f" => :mojave
     sha256 "1e369830820f2e7553bcc498af2a839005c3f8a6327781cd4f6f46252ae42ca6" => :high_sierra
     sha256 "c9d6d00656293f4d40185460807b7c489d8d2e7db4bc4e95af9ede968260f4e1" => :sierra
     sha256 "46428c9cb8fdf15ebe9014667f9158444b263e60b5836688625b300f389c64e4" => :el_capitan
