6f30445b3bfcd8568d3414fa69e0636ef35227f0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 14:22:17 +0000

    gitter-cli: update 0.8.5 bottle.

--- Formula/gitter-cli.rb
+++ Formula/gitter-cli.rb
@@ -8,6 +8,7 @@ class GitterCli < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "07256d350f14363a87f7bcf2457bd963f3dbe8f87cbb988319319340b02911d4" => :mojave
     sha256 "8a9a8bea2541bd954d589f4195665c8f2a36090fba16a6b652f88337a029c117" => :high_sierra
     sha256 "6b0c1af334ab94692271f4e88b3f3b44adb8f2e7738cd68cdc20719dbb4f315f" => :sierra
     sha256 "4503b65ec4122d7cb51e8173168dc41dc4e57f978f4246697f9a3bf768f8c9cb" => :el_capitan
