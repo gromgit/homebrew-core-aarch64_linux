664f1dfde0ac86efe5dde9b8e6aa358d61641d33
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 14:57:25 +0000

    opencv: update 3.4.2 bottle.

--- Formula/opencv.rb
+++ Formula/opencv.rb
@@ -5,6 +5,7 @@ class Opencv < Formula
   sha256 "81dbd5e7e9f8a4c936b94629bf4765745942a1d634ae38ec08bc57b73b28ffc5"
 
   bottle do
+    sha256 "75c1eaf814866f9478c92e44e671046305c356cadc6b80bdd925a023a2d36070" => :mojave
     sha256 "e8a878f981fbd9ae7daf6f6f73be39dcd20baaf34911b1937ef9b91e0d6d889e" => :high_sierra
     sha256 "7d42a502f14163663c985247cea157a19f714e1fd2641bd5fc2f653f85592473" => :sierra
     sha256 "3ca5f6be0e49fe59a3b9274ebda00741de5e8265b6de35813fef43c00086148c" => :el_capitan
