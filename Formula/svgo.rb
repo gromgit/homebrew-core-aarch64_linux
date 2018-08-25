ee0de1c45084e7ba0f88bf360c5b54c47ee60892
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 14:19:21 +0000

    svgo: update 1.0.5 bottle.

--- Formula/svgo.rb
+++ Formula/svgo.rb
@@ -8,6 +8,7 @@ class Svgo < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "56dd992871e910b590cef6facabc033b1737d7ec12a8cc366a491ddcc9e7b338" => :mojave
     sha256 "33251a1558bc0d5479d49189d1dd3d9dd27af77d0a4e03d97f8f905cda2bfd72" => :high_sierra
     sha256 "5553d5bca19b519bfaed0edf43f33839371983b7b9840d4804a51a365281eca1" => :sierra
     sha256 "b255cdc0b1d51ddf9103fd3bc2e527c7556715c91c6e79f65806b3b6bbe475ef" => :el_capitan
