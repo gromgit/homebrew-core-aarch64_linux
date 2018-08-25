8211f22cf5921d6c3f879fac1f7ba4a5bdf370fd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 14:39:46 +0000

    babel: update 6.26.0 bottle.

--- Formula/babel.rb
+++ Formula/babel.rb
@@ -7,6 +7,7 @@ class Babel < Formula
   sha256 "81ac501721ff18200581c58542fa6226986766c53be35ad8f921fabd47834d02"
 
   bottle do
+    sha256 "8bef30b9b787762a98410e78ad4e7901a8e12eb3af780076cacf25c4c007cd2a" => :mojave
     sha256 "283507db612687957e8855de052f97ebb8eafa9559f7edab627cb334012743c8" => :high_sierra
     sha256 "c4e2ca0a3d4f9d77b4d6c78c3982b333a8751fa36e5e0ceb506ef3d0e207c4f2" => :sierra
     sha256 "79ed0bda434eccc0d634407fcafebc419f529246472a807d16fb05739b9dc4f6" => :el_capitan
