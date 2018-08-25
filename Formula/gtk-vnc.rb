62621d6b502f4d8d97fe62c9c60527aeda3efcc4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 17:51:18 +0000

    gtk-vnc: update 0.9.0 bottle.

--- Formula/gtk-vnc.rb
+++ Formula/gtk-vnc.rb
@@ -5,6 +5,7 @@ class GtkVnc < Formula
   sha256 "3a9a88426809a5df2c14353cd9839b8c8163438cb708b31d8048c79d180fcab7"
 
   bottle do
+    sha256 "e097421c1c9e04dbccc1afdf8878d9ef906e20a19f70dfc04268b5f5a9e7c151" => :mojave
     sha256 "46f9946e8e7fbfe9003b615350e750795514d0be5ab2ce93232daccc556cd0e1" => :high_sierra
     sha256 "d519139009c47cc5673efd583ed08d022babfa9315f78d87a0c0d35260f33a6f" => :sierra
     sha256 "e7c133b6fa20f0c68cb50bb773c80c7e747571dfeaddded32b1fee52c45a5c4b" => :el_capitan
