7d942ffb5278e6b1c6706375f9b467868cb76e52
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 10:58:48 +0000

    gtk+: update 2.24.32_2 bottle.

--- Formula/gtk+.rb
+++ Formula/gtk+.rb
@@ -9,6 +9,7 @@ class Gtkx < Formula
   end
 
   bottle do
+    sha256 "1cf12a33a310ade188f96a927d2e916eab0b94785e2c4c98e400a20caa71e213" => :mojave
     sha256 "f1e4965a3aa5655e628e46c1ffd9421ec1abf9ba4f0757be1ce7b3a8009fec58" => :high_sierra
     sha256 "77275da434ff045cab02f69e3847b983510bd2f45e022b0af0adb620c6a6821a" => :sierra
     sha256 "f6117acd04e65a2ec378ad21b8b6519dc0d048f24b398456c09474af74ec2c11" => :el_capitan
