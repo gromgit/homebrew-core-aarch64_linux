12adb6722e0fd8fc9316aee0ff158a7b03804588
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 16:56:15 +0000

    dict: update 1.12.1_1 bottle.

--- Formula/dict.rb
+++ Formula/dict.rb
@@ -6,6 +6,7 @@ class Dict < Formula
   revision 1
 
   bottle do
+    sha256 "e9b8fcd267281afc7144d63d38b6540a21dd3fb47a1fb0875b69b9162cb4c977" => :mojave
     sha256 "5a386048d25567d5c913fc46ca926d8f1f8abb152c00eb9c448552594a590bd4" => :high_sierra
     sha256 "5b4f21f0abaf403bc6599d5cf05dc0c9f0dedbc5d520940b5c5fccb3dc69326b" => :sierra
     sha256 "899c97bb0af187edb16eb91e516403f12977dc00293c5b86a6a3f6aeafb74ac8" => :el_capitan
