678f430f54f05ae11619a187289cb288a3def4ef
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 09:56:47 +0000

    libplist: update 2.0.0 bottle.

--- Formula/libplist.rb
+++ Formula/libplist.rb
@@ -6,6 +6,7 @@ class Libplist < Formula
 
   bottle do
     cellar :any
+    sha256 "2a36e4f8dabe7a115f74c90ae6d4bb309172ea27ab0b1c7787cbb3429ef5eb93" => :mojave
     sha256 "af4e7e2fe8cc73190aecccdfb918db0aed2c4e2397b8d6d86a7e5dbec1fcf767" => :high_sierra
     sha256 "da5d4dedb8a981298f8c67bf116b92dd178ed834208f6fb7a0a55987ff8cfc95" => :sierra
     sha256 "34e757ae78d7a84a8fdee4fe158409f9ebd690c477400eb836fc2ed88c1353e9" => :el_capitan
