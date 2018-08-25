a91e14b5776a1a0664cfdd716707e8e95671506e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 19:04:19 +0000

    gst-libav: update 1.14.2 bottle.

--- Formula/gst-libav.rb
+++ Formula/gst-libav.rb
@@ -5,6 +5,7 @@ class GstLibav < Formula
   sha256 "8a351c39c5cfc2bbd31ca434ec4a290a730a26efbdea962fdd8306dce5c576de"
 
   bottle do
+    sha256 "81c974a732052365bb79e7aef9b16f70c255577e7260fe2fb338c43ad0f89b52" => :mojave
     sha256 "de3fedcd2025f2c50cc1f77b8c056dab48d8daf6362aff332e69b5f0afab8a3f" => :high_sierra
     sha256 "11464cc32952ed9fd9a07ef8e72110bf493bddf5f8c9fc9ce515a94a1da47904" => :sierra
     sha256 "5ba6ba3e6df6deb610627da38ec1d1cd84d66f487966b5fbf65836304c28ff73" => :el_capitan
