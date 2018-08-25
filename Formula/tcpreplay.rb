1b7faf9a847b87011cea6b11d7a4a64708c55fe6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 21:47:44 +0000

    tcpreplay: update 4.2.6 bottle.

--- Formula/tcpreplay.rb
+++ Formula/tcpreplay.rb
@@ -6,6 +6,7 @@ class Tcpreplay < Formula
 
   bottle do
     cellar :any
+    sha256 "02519ad95fb3934c06b0f3485e8bc1624cc0cf3b57af188eb1f75e03de0650d6" => :mojave
     sha256 "9be61ec3aeeac7be8cd51225d5914a7ba7ee8f0c9fbd4393e452f6b9447a53c7" => :high_sierra
     sha256 "569bdb4ac12e4ff62c723b1fdabad4b037c54423a70742306ba852f9bc43e25d" => :sierra
     sha256 "b5ba1668dddf52946866c866bc1ba2ab2983b67d99c69b6c41fabe91e816139a" => :el_capitan
