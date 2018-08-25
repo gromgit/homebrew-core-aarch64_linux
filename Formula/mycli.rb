de7db3eb229c0f78f01d4e050c7f411f4dd9e1e6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 12:56:58 +0000

    mycli: update 1.16.0 bottle.

--- Formula/mycli.rb
+++ Formula/mycli.rb
@@ -8,6 +8,7 @@ class Mycli < Formula
 
   bottle do
     cellar :any
+    sha256 "bf79d034424102875a80970389a4870cc79a34fd05ec6b71804f1eb1d523372a" => :mojave
     sha256 "0e595019db782a382f315a1c449334391b17630194193b45c61bfdd4e20efeb2" => :high_sierra
     sha256 "a1d590e59cd55c5539032bbb6e3682de3ac687c924418a57875fdac7cc7042c1" => :sierra
     sha256 "efb1798af243e65cc470d754439b5d82fde609081d5e77b40e0a29ea0d36b5d1" => :el_capitan
