762aa2655c11182eaf713326b2a3f082a059c978
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 21:45:21 +0000

    zmap: update 2.1.1_1 bottle.

--- Formula/zmap.rb
+++ Formula/zmap.rb
@@ -7,6 +7,7 @@ class Zmap < Formula
   head "https://github.com/zmap/zmap.git"
 
   bottle do
+    sha256 "47d8698c87325c5b3c546d42da897fb093b58e5cc872f47bb968008e05da9d70" => :mojave
     sha256 "5bf98e6e2fea460c2b456f7017aff0064590994b47058ae5296738445cc37999" => :high_sierra
     sha256 "0f02a61d2563ec4359b90eaa3d637d53e2a5aa8bbbfbc78a8ba76780e3f565d1" => :sierra
     sha256 "517ccb75b370f3deee62725a9e74b53a7d3949f3ef214a8769983c7eab72f83e" => :el_capitan
