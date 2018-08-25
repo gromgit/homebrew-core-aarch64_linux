79b5ae01ba32f47fab7521cdbdbe1aacb7c09dee
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 13:19:26 +0000

    nestopia-ue: update 1.49 bottle.

--- Formula/nestopia-ue.rb
+++ Formula/nestopia-ue.rb
@@ -6,6 +6,7 @@ class NestopiaUe < Formula
   head "https://github.com/rdanbrook/nestopia.git"
 
   bottle do
+    sha256 "8c13d064015561a0707b4f47989d193527773af0021cbae1120b53d6a688ad20" => :mojave
     sha256 "2be015b071a5d17bd3a28aa2348949eeb91659c94efa12226adff66e8934356d" => :high_sierra
     sha256 "148a7754f387640b112f447327d23322e9d43b938d7aba9a584c311850fc284c" => :sierra
     sha256 "43b5dd65950c2bf19aebbb77f307e96933557597379b83023fd3914a26d4666c" => :el_capitan
