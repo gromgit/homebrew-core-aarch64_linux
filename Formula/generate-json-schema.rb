162e161092cef79b0bce7931be0a0c8c53dd5a0b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 14:22:14 +0000

    generate-json-schema: update 2.6.0 bottle.

--- Formula/generate-json-schema.rb
+++ Formula/generate-json-schema.rb
@@ -9,6 +9,7 @@ class GenerateJsonSchema < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e049d098796be43aa340eca884fa71ec90f4fbeda02031142f66752df005de97" => :mojave
     sha256 "3461301c038b8bb6e15b8e183661976e95ea7b7e0659d57f0f21ea2c0eb4e67c" => :high_sierra
     sha256 "a6ff075810774d44030a59a12032d302c64834d03c7aabeb32efb8dc86d276de" => :sierra
     sha256 "5a5b34d8e233d9b75648c39f8edada5077c8f6c6466bd3358f3f661062ccbe83" => :el_capitan
