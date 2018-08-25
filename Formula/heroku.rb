a2d7a1cf6e7a62d224168a4b289ca6e4532e4b7f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 14:22:15 +0000

    heroku: update 7.12.1 bottle.

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -10,6 +10,7 @@ class Heroku < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "457d4d451237bc588a9fa7053c3298478e336d77b32f27eaf44c56e871df5762" => :mojave
     sha256 "ff88c602b7a8dc6f65acd122f3546ccb80db6bd5ac8b4ed0351d16b2993c4cf1" => :high_sierra
     sha256 "0cb7b8d611ddaf471215176cdab6138a05ca9ee5ca393b3efd3f16a62e0967bb" => :sierra
     sha256 "1018bd6c084dc07012e01993e8f7699d5ecc1df1c09a69150bc264c2dca4bc91" => :el_capitan
