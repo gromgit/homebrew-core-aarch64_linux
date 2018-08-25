5beb5d0b8aeef1637d6eb0b5077051f519efd616
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 19:04:20 +0000

    libgit2-glib: update 0.26.4_1 bottle.

--- Formula/libgit2-glib.rb
+++ Formula/libgit2-glib.rb
@@ -7,6 +7,7 @@ class Libgit2Glib < Formula
   head "https://github.com/GNOME/libgit2-glib.git"
 
   bottle do
+    sha256 "b8d58cb52170bbb4a4c387a124db4e2be995bdb2b5ecd4973d73456ebd1f121e" => :mojave
     sha256 "20e1cf8538d46014cf5533510d52c2bb4e98cbfbf6e7a56817fc2e072466200c" => :high_sierra
     sha256 "fc9f0fa31794d7f4c0b67fd68ff0378de0c1f5668b1bc093937ee0c94a7808a8" => :sierra
     sha256 "de784ae7a8154369d33eb13cd6fdca5979e2144cc1561ab4a3c85118b0cbbb20" => :el_capitan
