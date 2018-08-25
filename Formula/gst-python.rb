2a8d5945225461a7e704ced7f25ee51278b444ad
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 20:15:12 +0000

    gst-python: update 1.14.2 bottle.

--- Formula/gst-python.rb
+++ Formula/gst-python.rb
@@ -5,6 +5,7 @@ class GstPython < Formula
   sha256 "dc40be5ab4f1a433ff3f0af2b3d2d79a363009020c41ec10f9747ba64200cb22"
 
   bottle do
+    sha256 "374dc4679f41d2cd28ac4c66e7a44724a12d239071174dd6c8d105fddd5f35a6" => :mojave
     sha256 "b8c2e02553b454040b965c5e40b3c634a1d3861b689fed011d8baedc18ccdaea" => :high_sierra
     sha256 "ed0da2e642e6b9c67b4985fe4a8552e0ff8b40c68cb44c2ad42f5a0b065031e7" => :sierra
     sha256 "89e380ff6173e59b29442cb558b8849499ae64c15375087fb49fd4c7abe5cf7f" => :el_capitan
