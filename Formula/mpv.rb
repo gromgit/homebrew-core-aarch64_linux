6c92fd5783831961d233e68af729516ca6a23668
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 16:57:50 +0000

    mpv: update 0.28.2 bottle.

--- Formula/mpv.rb
+++ Formula/mpv.rb
@@ -6,6 +6,7 @@ class Mpv < Formula
   head "https://github.com/mpv-player/mpv.git"
 
   bottle do
+    sha256 "c19d5eb5999281e98e2a36f1e44af44f3bf2f91d6b0469388651a81269d0fcce" => :mojave
     sha256 "1dee05ae50b43c059c11c667a2e70b51bccfb373248ad8f877eeef9a8afe7c67" => :high_sierra
     sha256 "9c43d85b504b5edd2cbcf87ed219dc7a75c771b763e66fd64305b93c5070c993" => :sierra
     sha256 "301203ddeb374ecce692a47b59ff8396b63e2f40530f6f82dc037992a53a0ebc" => :el_capitan
