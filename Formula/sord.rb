ffd596fff4d2baf91c75e2b4fb636135ce6c93ff
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 16:25:33 +0000

    sord: update 0.16.2 bottle.

--- Formula/sord.rb
+++ Formula/sord.rb
@@ -6,6 +6,7 @@ class Sord < Formula
 
   bottle do
     cellar :any
+    sha256 "d9fb150f83839e9179f5a011cba23885d6fd369912d876b066ab545eff993777" => :mojave
     sha256 "c51813aa91714be7ff72372606538ccf028f3019a2e3b1c9eb1af184d925cd2c" => :high_sierra
     sha256 "cdc2488f62b363e161df7412fc459fc4b6aa564d5b7d92c841e71d462629751a" => :sierra
     sha256 "f83681f6abcdf47721ed6a8af9d6e2a17219808bd08f7951c4c145920e9fdf94" => :el_capitan
