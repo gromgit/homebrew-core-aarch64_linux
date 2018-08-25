2e78dd7597e406277fd792c33caf34fa99c6cbd9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 11:57:19 +0000

    yara: update 3.8.1 bottle.

--- Formula/yara.rb
+++ Formula/yara.rb
@@ -7,6 +7,7 @@ class Yara < Formula
 
   bottle do
     cellar :any
+    sha256 "b1da7a088423fe04c3f9378f035371f740d3a2704b92c4b651bf9b6da29d1b1b" => :mojave
     sha256 "f937b52ce311ad61726d374c7be876dae61fd61f9f85f86db207d30b08d70f26" => :high_sierra
     sha256 "3fc4f5981873940a9cd6518f923575e0d9ce2bc51d078881c4fc11d0a7618161" => :sierra
     sha256 "9219080a0d50dd78539fb6f0ac7fb0f620462bd2cad8cbcbd1c334a0af38af1d" => :el_capitan
