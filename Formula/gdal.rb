4e9f48c52c0dcb20d51b5bcf3834805abaaf5d75
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 15:17:47 +0000

    gdal: update 2.3.1_1 bottle.

--- Formula/gdal.rb
+++ Formula/gdal.rb
@@ -6,6 +6,7 @@ class Gdal < Formula
   revision 1
 
   bottle do
+    sha256 "5fa9c5dde1d07ca4b273bb39eba94e7e10060299ce3784777d8d5689781c3586" => :mojave
     sha256 "8fd9f4bea010ab1a734b9e93e7768ba1232929f0c55584e55f4a0f518933edd2" => :high_sierra
     sha256 "b4a94d1d2cb9944db72ceacf3eae46b16ffedd0e23690d993f877962b1496899" => :sierra
     sha256 "0329fd8ff5722be6e43b4e16ed56ff71096f5464e76141dd633c08a553eb31d4" => :el_capitan
