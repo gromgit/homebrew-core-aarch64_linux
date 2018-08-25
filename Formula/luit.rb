648943187b21daa57d99a547259517ab548e9ef8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 14:56:09 +0000

    luit: add 20180628 bottle.

--- Formula/luit.rb
+++ Formula/luit.rb
@@ -4,6 +4,13 @@ class Luit < Formula
   url "ftp://ftp.invisible-island.net/luit/luit-20180628.tgz"
   sha256 "7b84f63072589e9b03bb3e99e01ef344bb37793b76ad1cbb7b11f05000d64844"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e2b66941a0cf7dfb7fe0b1c15917ca1586ccc77999a4f9e9a150fc08e4fd6f7c" => :high_sierra
+    sha256 "a365a4654be845ee683827113bff9efa96832fbbd248234f3a785fd4d1ebb2ba" => :sierra
+    sha256 "323633917450a82e0f86958f77be43d6fbfba4e262d8ea393473f72b0db9d304" => :el_capitan
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
