7ad407d9968c471f769afdec7e17f5df7817a8b4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 16:20:01 +0000

    grpc: update 1.14.1 bottle.

--- Formula/grpc.rb
+++ Formula/grpc.rb
@@ -6,6 +6,7 @@ class Grpc < Formula
   head "https://github.com/grpc/grpc.git"
 
   bottle do
+    sha256 "16a61aa304757dc72f172d8771c86ce1992d0c4df7c215ef9404b76c0bc19f7c" => :mojave
     sha256 "e1466a0ffb702f2ba76e03875701ed7ec88624f55497b5de47f5727cfc2dfa1e" => :high_sierra
     sha256 "ca43a0fdc5e5f4265300cef6f9b1e70449a529a2d050d5e49f53e1852af2d311" => :sierra
     sha256 "2520997527906e3e7f74463d6c6a5cd80db5cad9e4dc840b53ae73cad158b629" => :el_capitan
