c95ae5b6a86ce28189b11845aa9f5caa63c5550a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 16:56:14 +0000

    ponysay: update 3.0.3_3 bottle.

--- Formula/ponysay.rb
+++ Formula/ponysay.rb
@@ -7,6 +7,7 @@ class Ponysay < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9aaf7cdbe355ecc5f41ed2ab753ef18b848a36a5e475b9bda38e4551af886203" => :mojave
     sha256 "481d6431bc586203d237787eaceafc116d9eccbf8d11489e1197a6eb0e034710" => :high_sierra
     sha256 "e51c96a3bf6997b73150b75eb758eb8359ca89a27a5b171b50eba3628192a31c" => :sierra
     sha256 "594b78b627cad84edef6de6dba32879cf5547215b33e7946bb3ee44c73e49214" => :el_capitan
