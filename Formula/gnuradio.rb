63333ff81321a2fd2db7613494da65b3a632d77f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 16:20:00 +0000

    gnuradio: update 3.7.13.4 bottle.

--- Formula/gnuradio.rb
+++ Formula/gnuradio.rb
@@ -6,6 +6,7 @@ class Gnuradio < Formula
   head "https://github.com/gnuradio/gnuradio.git"
 
   bottle do
+    sha256 "776bf89ad97f91a5dfde51675515b59dc40715130a09039c713a291bc11e4708" => :mojave
     sha256 "bd03698a2b1a89f865eceb8d22eb335ec0b80cc5afb6a03c4c96f71560390e6c" => :high_sierra
     sha256 "8967e3a4fb54a2f0a191d42fefa74066ffc3384290792c325b5d6ce72e1819d5" => :sierra
     sha256 "dc19d373b871c7cf3325e5ff041a5a46aa3fe75cee2c8efa36b33b70bb492b73" => :el_capitan
