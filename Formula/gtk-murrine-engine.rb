83ae9b2f51694bfb31152672422f976dd6172a8b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-26 20:47:15 +0000

    gtk-murrine-engine: update 0.98.2_2 bottle.

--- Formula/gtk-murrine-engine.rb
+++ Formula/gtk-murrine-engine.rb
@@ -7,6 +7,7 @@ class GtkMurrineEngine < Formula
 
   bottle do
     cellar :any
+    sha256 "15f0163bd29d64bc306bc60862174d58c61f7fe8f1a369f105c7b3ffa4c5bf06" => :mojave
     sha256 "f83119dad928f2a2dae783d0963c81d72656851ae18df542584fab2e415bf708" => :high_sierra
     sha256 "08bda579c315be5f604086c0e912adab1bc21ae3167031cb081140d1ca34708e" => :sierra
     sha256 "2444bf86ebdf7288a4179bbc7cb4a77b2c8321c901be7d1d1d2430c8fedd1ce8" => :el_capitan
