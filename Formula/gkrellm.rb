48f82621372d7c5890158905dc817d4b298e72da
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-26 20:15:31 +0000

    gkrellm: update 2.3.10_1 bottle.

--- Formula/gkrellm.rb
+++ Formula/gkrellm.rb
@@ -7,6 +7,7 @@ class Gkrellm < Formula
   head "https://git.srcbox.net/gkrellm", :using => :git
 
   bottle do
+    sha256 "7deb82cd7fe3c9cb715f1be733b8f89692ab66fe0e603fd36d617d946d4e319b" => :mojave
     sha256 "e84742cdd42e7584a6814c9c4cf2b8d7245d48ae0723b77c236f011354ac71e1" => :high_sierra
     sha256 "53888d3166533669a8649ef295c28813dcb0c051ed4146a33452f90c860978a8" => :sierra
     sha256 "7cc7e94022669c80d1035efa738388fb264f50e4edaf0720db216f58b0ad3dab" => :el_capitan
