559a5f4ef89949a696c0adda3f511aded8e8c413
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-26 21:27:47 +0000

    libgweather: update 3.28.2 bottle.

--- Formula/libgweather.rb
+++ Formula/libgweather.rb
@@ -5,6 +5,7 @@ class Libgweather < Formula
   sha256 "081ce81653afc614e12641c97a8dd9577c524528c63772407ae2dbcde12bde75"
 
   bottle do
+    sha256 "c05c1a3b582b0910587d135d29c8ebfb731ba11cca9ec99e05b1ecc8f0f0e9e3" => :mojave
     sha256 "2eb44ffb03a8d0ef04e3f60249136acb7ae5d6581b7eb2b3354f4e33dfae9b92" => :high_sierra
     sha256 "9fc3c60b4f111e74e9d508ca95f85f16c347880cc3dd689a9b73ac772f316992" => :sierra
     sha256 "d47872fbe5c9325b3a40cc922c75728651665b25585c9bf9610d98bd6e60ac0a" => :el_capitan
