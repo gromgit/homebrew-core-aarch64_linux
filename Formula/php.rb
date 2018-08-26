e79a3dad4f1163067fcff3b6c673d161f8de8fdb
Jan Viljanen <jan.a.viljanen@gmail.com>
2018-08-26 18:05:16 +0200

    php: build fix for Mojave
    Closes #31498.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/php.rb
+++ Formula/php.rb
@@ -33,14 +33,13 @@ class Php < Formula
   depends_on "gmp"
   depends_on "icu4c"
   depends_on "jpeg"
+  depends_on "libiconv" if DevelopmentTools.clang_build_version >= 1000
   depends_on "libpng"
   depends_on "libpq"
   depends_on "libsodium"
-  # libxml2 required due to https://bugs.php.net/bug.php?id=76403, can be
-  # removed once resolved.
-  depends_on "libxml2"
   depends_on "libzip"
   depends_on "openssl"
+  depends_on "openldap" if DevelopmentTools.clang_build_version >= 1000
   depends_on "pcre"
   depends_on "sqlite"
   depends_on "unixodbc"
@@ -48,11 +47,18 @@ class Php < Formula
 
   needs :cxx11
 
+  # PHP build system incorrectly links system libraries
+  # see https://github.com/php/php-src/pull/3472
+  patch :DATA
+
   def install
     # Ensure that libxml2 will be detected correctly in older MacOS
     if MacOS.version == :el_capitan || MacOS.version == :sierra
       ENV["SDKROOT"] = MacOS.sdk_path
     end
+
+    # buildconf required due to system library linking bug patch
+    system "./buildconf", "--force"
 
     inreplace "configure" do |s|
       s.gsub! "APACHE_THREADED_MPM=`$APXS_HTTPD -V | grep 'threaded:.*yes'`",
@@ -86,6 +92,10 @@ class Php < Formula
     # Prevent homebrew from harcoding path to sed shim in phpize script
     ENV["lt_cv_path_SED"] = "sed"
 
+    # Each extension that is built on Mojave needs a direct reference to the
+    # sdk path or it won't find the headers
+    headers_path = "=#{MacOS.sdk_path_if_needed}/usr"
+
     args = %W[
       --prefix=#{prefix}
       --localstatedir=#{var}
@@ -117,7 +127,7 @@ class Php < Formula
       --enable-wddx
       --enable-zip
       --with-apxs2=#{Formula["httpd"].opt_bin}/apxs
-      --with-bz2
+      --with-bz2#{headers_path}
       --with-fpm-user=_www
       --with-fpm-group=_www
       --with-freetype-dir=#{Formula["freetype"].opt_prefix}
@@ -126,16 +136,16 @@ class Php < Formula
       --with-gmp=#{Formula["gmp"].opt_prefix}
       --with-icu-dir=#{Formula["icu4c"].opt_prefix}
       --with-jpeg-dir=#{Formula["jpeg"].opt_prefix}
-      --with-kerberos
+      --with-kerberos#{headers_path}
       --with-layout=GNU
-      --with-libxml-dir=#{Formula["libxml2"].opt_prefix}
-      --with-ldap-sasl
-      --with-libedit
+      --with-ldap-sasl#{headers_path}
+      --with-libxml-dir#{headers_path}
+      --with-libedit#{headers_path}
       --with-libzip
-      --with-mhash
+      --with-mhash#{headers_path}
       --with-mysql-sock=/tmp/mysql.sock
       --with-mysqli=mysqlnd
-      --with-ndbm
+      --with-ndbm#{headers_path}
       --with-openssl=#{Formula["openssl"].opt_prefix}
       --with-password-argon2=#{Formula["argon2"].opt_prefix}
       --with-pdo-dblib=#{Formula["freetds"].opt_prefix}
@@ -152,20 +162,24 @@ class Php < Formula
       --with-unixODBC=#{Formula["unixodbc"].opt_prefix}
       --with-webp-dir=#{Formula["webp"].opt_prefix}
       --with-xmlrpc
-      --with-xsl
-      --with-zlib
+      --with-xsl#{headers_path}
+      --with-zlib#{headers_path}
     ]
 
     if MacOS.version < :lion
       args << "--with-curl=#{Formula["curl"].opt_prefix}"
     else
-      args << "--with-curl"
+      args << "--with-curl#{headers_path}"
     end
 
-    if build.devel?
+    if build.devel? || MacOS.sdk_path_if_needed
       args << "--with-ldap=#{Formula["openldap"].opt_prefix}"
     else
       args << "--with-ldap"
+    end
+
+    if MacOS.sdk_path_if_needed
+      args << "--with-iconv=#{Formula["libiconv"].opt_prefix}"
     end
 
     system "./configure", *args
@@ -416,3 +430,33 @@ class Php < Formula
     end
   end
 end
+
+__END__
+diff --git a/acinclude.m4 b/acinclude.m4
+index 1deb50d2983c..d0e66c8b6344 100644
+--- a/acinclude.m4
++++ b/acinclude.m4
+@@ -441,7 +441,11 @@ dnl
+ dnl Adds a path to linkpath/runpath (LDFLAGS)
+ dnl
+ AC_DEFUN([PHP_ADD_LIBPATH],[
+-  if test "$1" != "/usr/$PHP_LIBDIR" && test "$1" != "/usr/lib"; then
++  case "$1" in
++  "/usr/$PHP_LIBDIR"|"/usr/lib"[)] ;;
++  /Library/Developer/CommandLineTools/SDKs/*/usr/lib[)] ;;
++  /Applications/Xcode*.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/*/usr/lib[)] ;;
++  *[)]
+     PHP_EXPAND_PATH($1, ai_p)
+     ifelse([$2],,[
+       _PHP_ADD_LIBPATH_GLOBAL([$ai_p])
+@@ -452,8 +456,8 @@ AC_DEFUN([PHP_ADD_LIBPATH],[
+       else
+         _PHP_ADD_LIBPATH_GLOBAL([$ai_p])
+       fi
+-    ])
+-  fi
++    ]) ;;
++  esac
+ ])
+
+ dnl
