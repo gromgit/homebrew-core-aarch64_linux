381e8e183c2fedc0a6e5c3061bad8d7aab6d27d3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-26 10:42:36 +0000

    docbook: update 5.0 bottle.

--- Formula/docbook.rb
+++ Formula/docbook.rb
@@ -6,13 +6,10 @@ class Docbook < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 3
-    sha256 "f02d3ab50c375f42e90c1d8f8e8e3703372344b90cc3a9f652f876dc39f5cbf2" => :mojave
-    sha256 "0f99e9e2e42f4a21bdb9066f02e247c919c21f586cea0bcd787c9112659df030" => :high_sierra
-    sha256 "40b2740609c1586d030d3b9a131761821425c211989bee297af29900981b3aba" => :sierra
-    sha256 "3fb7e4070eaa9250fa947d38e3d7803d37c159d9765e3f71397702d5ad6bb578" => :el_capitan
-    sha256 "dfdb315404c98dca2682f63260f2996de101cb6b41de69ac268dcded110e2a3f" => :yosemite
-    sha256 "65925fda670fdb020fe9d52cd5891f8e3a2a44619e9129b30031127c7c2e998c" => :mavericks
+    rebuild 4
+    sha256 "6ac70ee56739ffbe8d99e18164bc42d8d0df9ce62cc2a5c55be4b65cd74092aa" => :high_sierra
+    sha256 "6ac70ee56739ffbe8d99e18164bc42d8d0df9ce62cc2a5c55be4b65cd74092aa" => :sierra
+    sha256 "6ac70ee56739ffbe8d99e18164bc42d8d0df9ce62cc2a5c55be4b65cd74092aa" => :el_capitan
   end
 
   resource "xml412" do
