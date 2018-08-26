c35f2aa97901ac9df0454f204d3f3688c34d2e4a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-26 16:22:27 +0000

    cfengine: update 3.12.0 bottle.

--- Formula/cfengine.rb
+++ Formula/cfengine.rb
@@ -5,6 +5,7 @@ class Cfengine < Formula
   sha256 "d71ba98a272390c6fa8bc20e8ea27f0050a0a72a3e6b206a4762b4646be332ec"
 
   bottle do
+    sha256 "b1e2079b0bd8aa12ab6232d555c4707f4ccb0cb5ec8f771dffcc29d7bc900f6a" => :mojave
     sha256 "6d17d250d32a04ba47bd234abb1c5ee8d7fd69df1dfc517b1ab95c3995a270f3" => :high_sierra
     sha256 "67ee78d55c3b1e1877f0e03954dc270ff1874e0151d05d9a402365d118aa2bfa" => :sierra
     sha256 "468f1d8fc3726456fe25f14af8795e0410aa7bc558f7e841ce99082e738de149" => :el_capitan
