7f62d7e9bc6fc16a537bc2a66098d0b9a9015e1f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-26 15:53:44 +0000

    blast: update 2.7.1 bottle.

--- Formula/blast.rb
+++ Formula/blast.rb
@@ -7,6 +7,7 @@ class Blast < Formula
   sha256 "10a78d3007413a6d4c983d2acbf03ef84b622b82bd9a59c6bd9fbdde9d0298ca"
 
   bottle do
+    sha256 "1be9406bb0acaae777b61c4c85ab56bb7338cfbcc0279fc86fdb0c86c98ed8f2" => :mojave
     sha256 "13e0286489e2b28adbe3fc6ef82a26f3e081b44a0ce99f3212a0c4d7da981b3a" => :high_sierra
     sha256 "a868633034dc12160109ee44e5415577967e2ceabaad3246881abaf07c57a198" => :sierra
     sha256 "110a62423f43f3618aadc5a149c238302af30399426346e571e23f252f8e6bab" => :el_capitan
