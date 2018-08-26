844cf3f03921bc5ad86602f7c55f357333ae6cb6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-26 17:08:20 +0000

    dropbear: update 2018.76_1 bottle.

--- Formula/dropbear.rb
+++ Formula/dropbear.rb
@@ -17,6 +17,7 @@ class Dropbear < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3f63323d955182c40f86c6e0c681da9628780db8803f8812e422c72af5e6ddfd" => :mojave
     sha256 "c2440ba5427b079dc3ca2a56f84c53fed4c1cff689df0fc53d2e0c6de1f983c7" => :high_sierra
     sha256 "5764f9837f9a8a330cba257657b93b410d76b5e184dab8971b6772594ff5182c" => :sierra
     sha256 "51840945255b14f841843c9061448542054589284d907632c0fbd41cd38be405" => :el_capitan
