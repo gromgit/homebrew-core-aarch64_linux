4a1945a5bd83b61642036129a4ba893c9fefc78a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-26 11:21:43 +0000

    open-sp: update 1.5.2 bottle.

--- Formula/open-sp.rb
+++ Formula/open-sp.rb
@@ -6,6 +6,7 @@ class OpenSp < Formula
 
   bottle do
     rebuild 4
+    sha256 "0c34e7815d7b7f654210102518787bf4c07e6ef811bae5167967747a99b762ab" => :mojave
     sha256 "41deb89bf8fd39c9d99eb171039a949fba4e82eb86d674d2584ae70a0e3ecc73" => :high_sierra
     sha256 "77f282ed97f428763c7952365353a6b915ff3315d7808db73a51e785961e989c" => :sierra
     sha256 "03629f243a1598b2b26fc07f8b747c77b62efe88ce435d8e018167140d22b86e" => :el_capitan
