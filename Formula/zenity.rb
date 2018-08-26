fcef3a2be726caa344a255dcc05319e5b11e726b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-26 13:59:59 +0000

    zenity: update 3.28.1 bottle.

--- Formula/zenity.rb
+++ Formula/zenity.rb
@@ -5,6 +5,7 @@ class Zenity < Formula
   sha256 "db179354721fb4e2d5c418e0dc41f09c831a6b2dd440e33f7743dfc266de8a6b"
 
   bottle do
+    sha256 "131e608af29f2a459abeeeb7d97e181d38a236fa5782079096fdfda22467ba41" => :mojave
     sha256 "33e96b6ef96ab194ced8ec552e5bb1de8212bc782d82320873ba0a6413547366" => :high_sierra
     sha256 "e6a508f02eae50cad3ba7c1f39bb712a4136c8caba9569f563cb2d0fa016f428" => :sierra
     sha256 "eb4386c1857fb2b4a41f5d0c8fb596b58add38a4e3f30d78b7223233aefa18c9" => :el_capitan
