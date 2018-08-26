5f0f003c03d529f459dbbcddb067b76197ea920c
BePsvPT <freedom@csie.io>
2018-08-26 23:00:13 +0800

    goenv 1.21.0
    Closes #31496.
    
    Signed-off-by: Jan Viljanen <jan.a.viljanen@gmail.com>

--- Formula/goenv.rb
+++ Formula/goenv.rb
@@ -1,8 +1,8 @@
 class Goenv < Formula
   desc "Go version management"
   homepage "https://github.com/syndbg/goenv"
-  url "https://github.com/syndbg/goenv/archive/1.20.0.tar.gz"
-  sha256 "a53bff2502805455a36c6c42ba8818fd1ff07d0263f456045832f3b2dee2f89b"
+  url "https://github.com/syndbg/goenv/archive/1.21.0.tar.gz"
+  sha256 "e1a286638d25935bed6e6ff02fa1ddfce59f36b80e29966b2e0b728e270fca3c"
   version_scheme 1
   head "https://github.com/syndbg/goenv.git"
 
