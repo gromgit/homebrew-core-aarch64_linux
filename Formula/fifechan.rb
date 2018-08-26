c3bb1681af8618fb53c2ab7d1bfdccbf3ec169bf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-26 18:22:39 +0000

    fifechan: update 0.1.4_1 bottle.

--- Formula/fifechan.rb
+++ Formula/fifechan.rb
@@ -7,6 +7,7 @@ class Fifechan < Formula
 
   bottle do
     cellar :any
+    sha256 "06da9e38eb6a8d87b29a73e14073a396b650057905589a185cc8e7935197e899" => :mojave
     sha256 "b3f6cd4d4da46b31b05065e92c0aa8159e809e4d6e9a17cb8f8a3d9abdf4a76a" => :high_sierra
     sha256 "357c467807ac368322803f4774441a8679983715efbe7ab2b89b22bbc4c03f1c" => :sierra
     sha256 "a12266c07e0109b9ad91c8747a25a2a0bb9469db382718f037d5a01175c8bad5" => :el_capitan
