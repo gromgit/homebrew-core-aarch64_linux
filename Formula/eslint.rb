8c1c23d80cf33391f2419ab73e3e1a42b8afa293
Tommy Yang <yt940928@gmail.com>
2018-08-25 18:14:23 -0700

    eslint 5.4.0 (new formula)
    Closes #31478.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ Formula/eslint.rb
@@ -0,0 +1,23 @@
+require "language/node"
+
+class Eslint < Formula
+  desc "AST-based pattern checker for JavaScript"
+  homepage "https://eslint.org"
+  url "https://registry.npmjs.org/eslint/-/eslint-5.4.0.tgz"
+  sha256 "9ae7c52b2974a0bcc8c969f7ed9444058afd0352fd3c6f7772fae9fac50bf6c5"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/".eslintrc.json").write("{}") # minimal config
+    (testpath/"syntax-error.js").write("{}}")
+    # https://eslint.org/docs/user-guide/command-line-interface#exit-codes
+    output = shell_output("#{bin}/eslint syntax-error.js", 1)
+    assert_match "Unexpected token }", output
+  end
+end
