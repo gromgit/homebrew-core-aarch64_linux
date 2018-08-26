c4bd23a2d7a20bcc26dd7e41ef0c3e6cf86c39c3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-26 20:13:32 +0000

    gqview: update 2.0.4_2 bottle.

--- Formula/gqview.rb
+++ Formula/gqview.rb
@@ -6,6 +6,7 @@ class Gqview < Formula
   revision 2
 
   bottle do
+    sha256 "70c9791ca8c487ce98719f09ef52016ec31f801746ee52b9971bfe13bcd7119d" => :mojave
     sha256 "996c7675700a1d21e4ce3e0f009a8c0cc7c82f3d8272b18c66b389a96c9458b1" => :high_sierra
     sha256 "175f47cf8461ba1d389bc9532ba4e458b1531a452ff873f1a7a20c165e0900d0" => :sierra
     sha256 "9d80117cb9375f410886275318f9373f91ff29114cb5d4a62e1f801548364f47" => :el_capitan
