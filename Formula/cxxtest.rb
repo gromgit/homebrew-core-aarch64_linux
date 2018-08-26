e0f20fb9a44acf5553f4f040b1e9bb4000497b9b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-26 16:43:09 +0000

    cxxtest: update 4.4 bottle.

--- Formula/cxxtest.rb
+++ Formula/cxxtest.rb
@@ -7,6 +7,7 @@ class Cxxtest < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e6238d8ea9693ce3c98a32c56541220e4bdcb89b8492a3d02124a4200e6e09fb" => :mojave
     sha256 "b6016aba933e8e047299e4a4dae83549eb8679a9e18e11937d90877b9e94af62" => :high_sierra
     sha256 "09aa93c60544867a44c3ad711f7ad9207f3f097505ce658e12d4d8ae11287c82" => :sierra
     sha256 "d35cfbbea5de989734e9f859531b203dffc870fdf931a5a7f12302adc7354c87" => :el_capitan
