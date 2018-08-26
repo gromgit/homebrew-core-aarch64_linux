e4478a55e05e698bfc38658ee1c446f74707f65d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-26 16:43:08 +0000

    daemonlogger: update 1.2.1 bottle.

--- Formula/daemonlogger.rb
+++ Formula/daemonlogger.rb
@@ -7,6 +7,7 @@ class Daemonlogger < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "1cac9c8c17cd804206440d35ec88f49e8162ec102a4e561aa103f528b6d49382" => :mojave
     sha256 "04242956845e71d839b050dd765829a217268486eb625a481a3fae85bd577f0d" => :high_sierra
     sha256 "c3ac14ab04174e06129fc0a51d31ad992f3d11f362ecb1cf3803092b6c68b146" => :sierra
     sha256 "582aa8e07f269bdfa00b1f66157c06339b62285d94f6b8ffa6a472eac063e5e5" => :el_capitan
