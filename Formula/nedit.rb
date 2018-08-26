47d202d01c03d81cc41e5dd8dd7407553d1de630
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-26 23:33:10 +0000

    nedit: update 5.7 bottle.

--- Formula/nedit.rb
+++ Formula/nedit.rb
@@ -5,6 +5,7 @@ class Nedit < Formula
   sha256 "add9ac79ff973528ad36c86858238bac4f59896c27dbf285cbe6a4d425fca17a"
 
   bottle do
+    sha256 "5502d540128448aabe9f8f10af15bf7b0dc949821bab827fc9127ec8acdfbd26" => :mojave
     sha256 "e0b25b1e4de1406581d398d1159f19b806328f1dc82b90874eab044dd8369162" => :high_sierra
     sha256 "0150b964f9436f9e97e8a969379240ba50612354e36883bb851cc66f4fea6f74" => :sierra
     sha256 "eecf056af8aa2b0062a5c6568a30c6f8e0120fbe5bdef718d5b30f76894e3f36" => :el_capitan
