d88e9c6a051cf047b322cca244a4b17071394460
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-26 17:44:52 +0000

    duc: update 1.4.3_1 bottle.

--- Formula/duc.rb
+++ Formula/duc.rb
@@ -8,6 +8,7 @@ class Duc < Formula
 
   bottle do
     cellar :any
+    sha256 "ba9af8cc944e89dcfe55e2dd081e49da64c68bdd6025516d8be0155319af652e" => :mojave
     sha256 "73b517bb32cbe3df05827770993ea8cfa721f9ac252878ad940fd3c45bebcdc8" => :high_sierra
     sha256 "d8c89b0441c3d4b4321ad9c833d1ed91fa0ec7758e67d87ce44b656e75e1931d" => :sierra
     sha256 "cc192c66f235d906f672dc56984a5a3fadcf60823e4c8bf2195759fb048526a5" => :el_capitan
