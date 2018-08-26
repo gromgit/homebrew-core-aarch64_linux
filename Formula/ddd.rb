0b337d451e2accf29f34a9048d6f9caf863d70c5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-26 16:48:00 +0000

    ddd: update 3.3.12_1 bottle.

--- Formula/ddd.rb
+++ Formula/ddd.rb
@@ -8,6 +8,7 @@ class Ddd < Formula
 
   bottle do
     rebuild 1
+    sha256 "41917b105d1329eaa9421fe314e449fca4c9b9f27b5c4a2ad10d0dbb746a8cea" => :mojave
     sha256 "381ae07c96a67534b05a03ca72741d99aa3437a01c0fef603336ea218c470df9" => :high_sierra
     sha256 "af12e95b5b4326906236559a40f6715e896d164d5c18d9448384e0e22d089abf" => :sierra
     sha256 "68864faf1967b400bc5df5809ab9ee03a0d632f3736071131dd5469be715c58f" => :el_capitan
