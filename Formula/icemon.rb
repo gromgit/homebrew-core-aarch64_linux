de6ac7002e80eddc098c05dac53b77dcb45af4d6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-26 21:14:23 +0000

    icemon: update 3.1.0 bottle.

--- Formula/icemon.rb
+++ Formula/icemon.rb
@@ -6,6 +6,7 @@ class Icemon < Formula
 
   bottle do
     cellar :any
+    sha256 "45f7516bfe7650c8241da282b864e4e8e87f1086dc73b6536a505a70483812c6" => :mojave
     sha256 "091c0de73c69ae36a5bff744e7cf81b2321630f3499d67de139cea5782e3b0a0" => :high_sierra
     sha256 "8f4bb166755f10d04f3a20e5792ed108c86286e4451a7e87b79864835732e78d" => :sierra
     sha256 "7422122b9818ac86761695845bb7e4a63abfd483d9959b26570f9624569442c3" => :el_capitan
