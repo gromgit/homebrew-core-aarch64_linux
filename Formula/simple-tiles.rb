1b5852d67f1267eae122634520d7110a15691a51
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 02:52:57 +0000

    simple-tiles: update 0.6.1_2 bottle.

--- Formula/simple-tiles.rb
+++ Formula/simple-tiles.rb
@@ -8,6 +8,7 @@ class SimpleTiles < Formula
 
   bottle do
     cellar :any
+    sha256 "25fa849815c7f5f5ea98c36e61a6a967d43377c163e78f6731c06de5f98e73d6" => :mojave
     sha256 "5032c76efb7ae602dcafda7a048110ea123662b06835a077d329558c58fdddc8" => :high_sierra
     sha256 "30efcc0e5fcb844511c341015f251fb81226e434d16b0c5451fc7381da2a841a" => :sierra
     sha256 "101c3442589eb596e46e5df2a64f69bbb6d12909ef4fa0d0d4f86607258b3194" => :el_capitan
