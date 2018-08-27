7c740ee076dba1a11f1b7ec6c221b83443fc3438
commitay <commitay@users.noreply.github.com>
2018-08-27 11:42:29 +1000

    protobuf@2.5: use canonical repository URLs

--- Formula/protobuf@2.5.rb
+++ Formula/protobuf@2.5.rb
@@ -1,7 +1,7 @@
 class ProtobufAT25 < Formula
   desc "Protocol buffers (Google's data interchange format)"
-  homepage "https://github.com/google/protobuf"
-  url "https://github.com/google/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.bz2"
+  homepage "https://github.com/protocolbuffers/protobuf"
+  url "https://github.com/protocolbuffers/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.bz2"
   sha256 "13bfc5ae543cf3aa180ac2485c0bc89495e3ae711fc6fab4f8ffe90dfb4bb677"
 
   bottle do
