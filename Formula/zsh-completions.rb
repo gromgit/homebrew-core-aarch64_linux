a725b0415f469253d98229102679d8b63dcedc6d
Chongyu Zhu <i@lembacon.com>
2018-08-27 21:31:22 +0800

    zsh-completions 0.28.0
    Closes #31518.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/zsh-completions.rb
+++ Formula/zsh-completions.rb
@@ -1,8 +1,8 @@
 class ZshCompletions < Formula
   desc "Additional completion definitions for zsh"
   homepage "https://github.com/zsh-users/zsh-completions"
-  url "https://github.com/zsh-users/zsh-completions/archive/0.27.0.tar.gz"
-  sha256 "9b817b73e709aca0e7e5a41471b5b63467d1e7aa69ef755b6ce39b99e61cd47a"
+  url "https://github.com/zsh-users/zsh-completions/archive/0.28.0.tar.gz"
+  sha256 "f5e6fbd3d67a8c149f8e332e916329def53459158b9075a6147daff3ae88d3d0"
   head "https://github.com/zsh-users/zsh-completions.git"
 
   bottle :unneeded
