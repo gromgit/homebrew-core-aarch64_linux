2e9545e51fd28d5cf3ae6e69f623f7e33df34505
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 05:40:19 +0000

    zurl: update 1.9.1 bottle.

--- Formula/zurl.rb
+++ Formula/zurl.rb
@@ -6,6 +6,7 @@ class Zurl < Formula
 
   bottle do
     cellar :any
+    sha256 "16717a421d3a1f80a4bac2cbee9444d87afba83aad445df4e998996522e031a8" => :mojave
     sha256 "f94e3fbd570a122222ad66c0013b9b3425c5d49ca8f0e082e0377b1b7e6a538c" => :high_sierra
     sha256 "9ebde17a2751ce4b04a0215fe90ffebe880b4e07ab822897b5b8f7e44f4f273e" => :sierra
     sha256 "9deb04a87b09d9805a2fdfd443744fca5e61bcd00c79ecd067dc67c6319ef88f" => :el_capitan
