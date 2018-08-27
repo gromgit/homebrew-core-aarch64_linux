a161cec09031102f20b8b1f1a97b38020804e2e4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 05:40:21 +0000

    xsane: update 0.999_3 bottle.

--- Formula/xsane.rb
+++ Formula/xsane.rb
@@ -7,6 +7,7 @@ class Xsane < Formula
   revision 3
 
   bottle do
+    sha256 "028fa4b1496e6e0a0d6f0443bff7157b5bb70e56b1a9fd1e1b83072aa076f6a1" => :mojave
     sha256 "197b36716d2df9e81581650549571ce379ce74290fb45c703e6173c190024dad" => :high_sierra
     sha256 "cb6cd9fabadf727b9414d4a252fa648f579bfe168eb44dd25fb2cb18a84395ed" => :sierra
     sha256 "4346e2b40f260128b2ac562cff15be61c96bcc31b04457f44e41d28aeb7d6be2" => :el_capitan
