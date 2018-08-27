583d52114355c20f125f03f0d84b26b4df98f5e2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 20:10:58 +0000

    luit: update 20180628 bottle.

--- Formula/luit.rb
+++ Formula/luit.rb
@@ -6,6 +6,7 @@ class Luit < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1c3dacfce73760ac50146bee7befa165a4ddf7f6bddae148777dfd9425f6cc9e" => :mojave
     sha256 "e2b66941a0cf7dfb7fe0b1c15917ca1586ccc77999a4f9e9a150fc08e4fd6f7c" => :high_sierra
     sha256 "a365a4654be845ee683827113bff9efa96832fbbd248234f3a785fd4d1ebb2ba" => :sierra
     sha256 "323633917450a82e0f86958f77be43d6fbfba4e262d8ea393473f72b0db9d304" => :el_capitan
