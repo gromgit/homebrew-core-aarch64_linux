b721e6a69b5bf8648750e7ba85507c9af1b3f1fa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 02:35:12 +0000

    scour: update 0.37 bottle.

--- Formula/scour.rb
+++ Formula/scour.rb
@@ -9,6 +9,7 @@ class Scour < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "597e6782def8fd0c968f389b5b932ab750d8cb327b1bc66e33ef594c9f93bd88" => :mojave
     sha256 "d9940790ad4ae3872d69ca7f9a2c527731ee9ffa0cee469b04e817fb037f76cf" => :high_sierra
     sha256 "93a3f7709a108cfcd233510889209e8bbfca244679aed9c7b1e905a9bd25ba01" => :sierra
     sha256 "5f3067a66b5415bb5b5514e1a35298efb917e2114f9c369e8cd271977115b2a2" => :el_capitan
