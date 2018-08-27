2815000f4d32537dbcfaa9076da4ea0d6c1cfd6c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 03:20:00 +0000

    stoken: update 0.92 bottle.

--- Formula/stoken.rb
+++ Formula/stoken.rb
@@ -6,6 +6,7 @@ class Stoken < Formula
 
   bottle do
     cellar :any
+    sha256 "3a362165a5fac3e9c9117f0f247449aad1c1c6dfa76dfd6dfd652ed5137d7c09" => :mojave
     sha256 "cb6b6bb8d97abca20a4395fe9686717186ad1e4fc99f470e74a3b7748bde4d4d" => :high_sierra
     sha256 "005b85ff93aa190fbe9590324b1579d960c3207cd144cf860cfc6050eaf10e71" => :sierra
     sha256 "f29de39d399ca3b9acbdae0e170e82173f2fbcd5806663a8697c10695b5d1b82" => :el_capitan
