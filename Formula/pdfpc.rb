a419e0e34ddf37f72268440671f6673188ded3b0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 06:59:20 +0000

    pdfpc: update 4.1.2 bottle.

--- Formula/pdfpc.rb
+++ Formula/pdfpc.rb
@@ -6,6 +6,7 @@ class Pdfpc < Formula
   head "https://github.com/pdfpc/pdfpc.git"
 
   bottle do
+    sha256 "fe211f5e2a05d5ec68dbf8fe100c14bbb8557b2e9b9421dfef80e1e7005e90d9" => :mojave
     sha256 "ba05dcf5419fa5e23a64c2469b4d0a2110650dc75b19c6750533d860ac771e10" => :high_sierra
     sha256 "5ecb69f0c8268691f3e8d0b19123ba6b715182b78dfa66cade71712ee53a2567" => :sierra
     sha256 "c3bfbe06af39c2882944751ad0f7ddbbe9001a6a907077bbbaa0a3b894d21d9f" => :el_capitan
