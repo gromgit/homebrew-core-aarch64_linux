760ee43d2cbb88241441e24dede745a91a7f008b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 01:51:11 +0000

    pioneers: update 15.5 bottle.

--- Formula/pioneers.rb
+++ Formula/pioneers.rb
@@ -5,6 +5,7 @@ class Pioneers < Formula
   sha256 "3ee1415e7c48dc144fbdb99105a6ef8a818e67ed34e9d0f8e01224c3636cef0c"
 
   bottle do
+    sha256 "5fb252c345552e1d485f2dbc833cf47bf37037a0c71856c02fa05d2ec6a37f21" => :mojave
     sha256 "e65fd2300b96f0339eb72d955b7a44c3d941d1275828a9f9a2fcfb31fe5f5f3b" => :high_sierra
     sha256 "b93d58fda500c7774afe281663c69e09b846d59e5e8c44481195bbf715a39387" => :sierra
     sha256 "e366345048bf9d0ea67815050233846432d6bb8ef15bb5efd92e78e2a2384d1e" => :el_capitan
