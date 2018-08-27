2ddb00e25a35e9ac53c3d2ab24b3c909649c6c0b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 00:25:48 +0000

    osm-pbf: update 1.3.3_5 bottle.

--- Formula/osm-pbf.rb
+++ Formula/osm-pbf.rb
@@ -7,6 +7,7 @@ class OsmPbf < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3199cc807995df84916d63c216d0a2793ed8af5513bed9488d397e4efcf745c2" => :mojave
     sha256 "68e5bf0c9924719525b0522da2656ae43a7cdb11bcdf3a6c05e481c3f5b242ec" => :high_sierra
     sha256 "c4f104fa72861e982b9071e656675a3ed3c4bf2d37fddeab3c5eb952d7864d9b" => :sierra
     sha256 "6e6902ebcdb50d95ab0aeeb9fcc086956eb768110f1646839dba876b1f31c643" => :el_capitan
