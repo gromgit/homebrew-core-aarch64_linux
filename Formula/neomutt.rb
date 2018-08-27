a928ac0fcb6a5a2fbdd07c26041c60c4459a196b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 20:35:18 +0000

    neomutt: update 20180716 bottle.

--- Formula/neomutt.rb
+++ Formula/neomutt.rb
@@ -6,6 +6,7 @@ class Neomutt < Formula
   head "https://github.com/neomutt/neomutt.git"
 
   bottle do
+    sha256 "a6b1860ba4b48441468139da5d1dd3b17beef51797281eac8f7060317b114c43" => :mojave
     sha256 "c7ae26b5c57241beb1cd32b878840935441f2655d4eb774d7ea381c4846f771c" => :high_sierra
     sha256 "2bf5f6453b264880744c695a544e6f0c81dcea750dd1695039032557373cf2e1" => :sierra
     sha256 "b72ed1a178545bed52a3436c86c9339a0727f7653519ed91da95be43b2a402a7" => :el_capitan
