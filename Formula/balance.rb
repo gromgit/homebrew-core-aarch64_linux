a3c840e75be7f631ae8e85654cc032f31578d129
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-27 20:52:14 +0100

    balance: update homepage/URL
    Closes #31524.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/balance.rb
+++ Formula/balance.rb
@@ -1,7 +1,7 @@
 class Balance < Formula
   desc "Software load balancer"
-  homepage "https://www.inlab.de/balance.html"
-  url "https://www.inlab.de/balance-3.57.tar.gz"
+  homepage "https://www.inlab.net/balance/"
+  url "https://www.inlab.net/wp-content/uploads/2018/05/balance-3.57.tar.gz"
   sha256 "b355f98932a9f4c9786cb61012e8bdf913c79044434b7d9621e2fa08370afbe1"
 
   bottle do
