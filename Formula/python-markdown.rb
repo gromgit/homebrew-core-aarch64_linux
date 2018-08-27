726ecba5f2424dd21ca3f2bc3991d987e28dda74
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 01:35:48 +0000

    python-markdown: update 2.6.11 bottle.

--- Formula/python-markdown.rb
+++ Formula/python-markdown.rb
@@ -6,6 +6,7 @@ class PythonMarkdown < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "54fb6ed768838ad2c373d434894953adbcd15c09bcd9e18c89d4f42642ebf7c8" => :mojave
     sha256 "1bfa222753f8f3151769b20deed2b00af1d60e0fb6a6eb25a84c6374fab8385e" => :high_sierra
     sha256 "1bfa222753f8f3151769b20deed2b00af1d60e0fb6a6eb25a84c6374fab8385e" => :sierra
     sha256 "1bfa222753f8f3151769b20deed2b00af1d60e0fb6a6eb25a84c6374fab8385e" => :el_capitan
