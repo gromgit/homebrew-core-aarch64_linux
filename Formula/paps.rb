b31941ffbb5b23c59f3244d01f7d137e3d672fde
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 00:25:51 +0000

    paps: update 0.7.0 bottle.

--- Formula/paps.rb
+++ Formula/paps.rb
@@ -6,6 +6,7 @@ class Paps < Formula
 
   bottle do
     cellar :any
+    sha256 "d83539a5f07741571e46050c7fe444cd62ff0c8c51909a26e193e2abdd5ecd41" => :mojave
     sha256 "b5599742d6b369366a9bc00f42d6f3513cfb9c670bf6199222b7580c19c1706b" => :high_sierra
     sha256 "985afbb83ca67f6190f299ac8d22d929cc4b2bd12173954dab8d0ab4e7e485ef" => :sierra
     sha256 "4164c3d56b5b572d38f8f30d852e56a541b587f67c063213133842421302390b" => :el_capitan
