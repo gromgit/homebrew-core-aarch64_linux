338fc1b75f1854c23042cc8e3e1c38b8d82aca81
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 04:04:51 +0000

    tvnamer: update 2.4 bottle.

--- Formula/tvnamer.rb
+++ Formula/tvnamer.rb
@@ -7,6 +7,7 @@ class Tvnamer < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "15f31af4fe2f3411449ccd1fc07ca5b439fcc4b3331a5b11dfc230aee50246a4" => :mojave
     sha256 "4f1ae20737bed54785fce92bbdd67c78f3b20553312d02051f72b8906211d507" => :high_sierra
     sha256 "c1a03bc94b59d7e044445daa9c0037b46230271d457d658aba3e7e55e9291dd0" => :sierra
     sha256 "546b13ef89b777d7076948d993adc9a57c664b2bbc9cb2f429cf042f3d61d165" => :el_capitan
