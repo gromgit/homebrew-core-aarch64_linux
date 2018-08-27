5e5eee5398d7c8ddad6b96820c3263c15b31babe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 00:02:22 +0000

    mftrace: update 1.2.19_2 bottle.

--- Formula/mftrace.rb
+++ Formula/mftrace.rb
@@ -8,6 +8,7 @@ class Mftrace < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "80eaec2570ed7efc70d82e7a2b8ca9413d6a6b0ab8157c8364c9ae0cfb2a163a" => :mojave
     sha256 "f81be9cddecffd87a3066f23f90cb1be41ad2d43709a44e59c2d8f645ec4136c" => :high_sierra
     sha256 "60d36f222820ef0ffa00c3c67f8fc7d9063472062c8771b99bf783781e9c5613" => :sierra
     sha256 "ca26dc49c3a032568dea9c0a5a7639bd8a117f8f8e052a411679b78d01811e1f" => :el_capitan
