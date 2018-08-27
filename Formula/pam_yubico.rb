b4ffc885fb710a0f0f6e7211b45fd6d95165c7d5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 00:25:49 +0000

    pam_yubico: update 2.26 bottle.

--- Formula/pam_yubico.rb
+++ Formula/pam_yubico.rb
@@ -6,6 +6,7 @@ class PamYubico < Formula
 
   bottle do
     cellar :any
+    sha256 "bb57ab33d7b79a6461648cd510d5524ea636db738c46bba5aa8f1fe8451c4e49" => :mojave
     sha256 "a9a21d8b84d95c76d13118d1c525b93f9bbdf5c49b8aae5304df966d1a1ea669" => :high_sierra
     sha256 "7b0027b330f0f2bb6da798a7b405d80e09db71892c403206ba6c31bed5a3c804" => :sierra
     sha256 "102680d53f9ef736089e1e4a4eeaab60962fbbf4156c1fb3e77d82f425b0eed7" => :el_capitan
