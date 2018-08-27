fa8312466e26291b673a37737c788ddce8740243
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 06:28:39 +0000

    at-spi2-atk: update 2.26.2 bottle.

--- Formula/at-spi2-atk.rb
+++ Formula/at-spi2-atk.rb
@@ -6,6 +6,7 @@ class AtSpi2Atk < Formula
 
   bottle do
     cellar :any
+    sha256 "a4d80ea77ada0be681e165d9bcf3836677036b9b7e7a2516475d015da37055fc" => :mojave
     sha256 "fa992dfbe9bcb014cb0d5bc3fa2700cd6cb51d82ff18a3998cde6b9fa3ea75a6" => :high_sierra
     sha256 "958cc79b52ac7917259dd5dd4dd47b770722447843b25ae4c8272b36efa868d8" => :sierra
     sha256 "01c0e01277843d36e5cacb8b8c07e1096f1a3c53d92ed49aba4af8a96e0e15dc" => :el_capitan
