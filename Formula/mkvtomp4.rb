88d4c3d7f04e86e65d3b88e5570af75def921862
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 00:02:22 +0000

    mkvtomp4: update 1.3_1 bottle.

--- Formula/mkvtomp4.rb
+++ Formula/mkvtomp4.rb
@@ -7,6 +7,7 @@ class Mkvtomp4 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "06b96194e09c4e952de56492047f83a6af1a173aba059ba9edd3ac2664e2cbf4" => :mojave
     sha256 "2efab72b382b03ac47c70b1878587afafdd8de2b7361d96f98e837692d5b4ca4" => :high_sierra
     sha256 "4c085a7e2cbfada2a722dc1d676fab80dacc1f490c14d2a2aff10a4fa60f5225" => :sierra
     sha256 "f7610334538d3e3df8cfeab0a5cd7d9a44acfb141212b4852e340064657e50a8" => :el_capitan
