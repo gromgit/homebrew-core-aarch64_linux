cbdc99660b143f964ff7f7ddeaa390e2a28b6d0e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 00:02:21 +0000

    muttils: update 1.3 bottle.

--- Formula/muttils.rb
+++ Formula/muttils.rb
@@ -7,6 +7,7 @@ class Muttils < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "44469a59b620c95a16ce8df8f83865d52bacf1fc67ce2e662e70f7a220aed363" => :mojave
     sha256 "cc13c555f3f6fe0527b1b2ef61f50d3e01fe99663d9ba76d8ea912c7114bb9f0" => :high_sierra
     sha256 "5c8920713f0a4abc1e6834dda10b40295010dfb96589196ffb319476801f3178" => :sierra
     sha256 "6843b7372098864c53199d84759724bcbf2c1ab36fc74c15e56405351e43fbc9" => :el_capitan
