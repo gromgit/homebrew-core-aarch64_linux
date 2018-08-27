9417ff172ef495152cfd180904f380e6fa01b826
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 04:29:20 +0000

    suil: update 0.10.0_1 bottle.

--- Formula/suil.rb
+++ Formula/suil.rb
@@ -6,6 +6,7 @@ class Suil < Formula
   revision 1
 
   bottle do
+    sha256 "9c4394af565bb5bceadf67d921c6ee9434b12ba3c7c053168422b926a61b3711" => :mojave
     sha256 "836f6d8ad66cca4de66cd78c45205829627ee370b5485c0ef878bc9473d24ba6" => :high_sierra
     sha256 "babe1c998fd93f29b86a767e01ad1518b95e74e17769b517c092ed4cbc0878bf" => :sierra
     sha256 "639934ea8fd85b8968cf33838be1f399b7479e844fef972d4fca0ba2ab8a4bf4" => :el_capitan
