16a0860c13928520e61936a3993ef4e822e34a85
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-28 01:55:34 +0000

    fortio: update 1.2.0 bottle.

--- Formula/fortio.rb
+++ Formula/fortio.rb
@@ -6,10 +6,9 @@ class Fortio < Formula
       :revision => "36bffaa50c7cc47038fc7572c83221640a603425"
 
   bottle do
-    sha256 "c6e60b4774edcecbb8452175d4b62ee157f2743ffc729c8ac8655d4c314056ab" => :mojave
-    sha256 "9f383a159618023334af96f850ca159115f4be8c52cd9ff0bb752d79fd962ea5" => :high_sierra
-    sha256 "0cae47563c0bf4373858329aae4bb60ae19f6b78ec3d084cc36a36e891a0e01c" => :sierra
-    sha256 "b60701c47ad35b607779a2b2396b6367bb005ccd12b769089cb1ac6d0cff1be0" => :el_capitan
+    sha256 "ec027b129bf54d10cc153a617df246bc5a356cfb2c39915340b45f5cd54ea024" => :high_sierra
+    sha256 "69846957b5049c2fa3eb835a21651e8c9675d36d69a042b464e929c166111c18" => :sierra
+    sha256 "95a65023e84f7ce6bc05bca350c2355e21ab8f35af8c04546e0a4482119cc491" => :el_capitan
   end
 
   depends_on "go" => :build
