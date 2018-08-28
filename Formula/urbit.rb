c6597812bed6fa163323b0936f29ebf3b5b4796d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-28 21:28:54 +0000

    urbit: update 0.6.0 bottle.

--- Formula/urbit.rb
+++ Formula/urbit.rb
@@ -7,6 +7,7 @@ class Urbit < Formula
 
   bottle do
     rebuild 1
+    sha256 "d8b27d852fa3508ce51c34ec4a66f885449a2c4b0a2b8cbfe6b31ed580dcceaa" => :mojave
     sha256 "b13f985e22cb451d6bd2bf0060b0d106f706550100805388bc295d0553a4272e" => :high_sierra
     sha256 "b983f7e4aa657b96409603575589f3c411cf639c93586930b796ac278ad6e3ac" => :sierra
     sha256 "02b46f9c7977498538159ecfbcb394a7b5cf77a6a547676534c8fd04a6d0e987" => :el_capitan
