5a8f883e367c0daaae2ed86d3a51e8935554fb36
commitay <commitay@users.noreply.github.com>
2018-08-29 08:42:44 +1000

    rebar3: rename from rebar@3
    Closes #31556.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/rebar3.rb
+++ Formula/rebar3.rb
@@ -1,16 +1,8 @@
-class RebarAT3 < Formula
+class Rebar3 < Formula
   desc "Erlang build tool"
   homepage "https://github.com/erlang/rebar3"
   url "https://github.com/erlang/rebar3/archive/3.6.1.tar.gz"
   sha256 "40b3c85440f3235c7b149578d0211bdf57d1c66390f888bb771704f8abc71033"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "2aa772910fdc0e58935b3c9d59d18790a357ee8ea20fcf61acedc91ca22a9c1b" => :mojave
-    sha256 "93810f629b55e7bb96b63596b98aa1914c0d06091c988af2b03a7ea33ee74fe8" => :high_sierra
-    sha256 "f6316f92f20c6a266c412ac49ec731743f7b43afecc44939049802c45ebee4e4" => :sierra
-    sha256 "461a1ba52650feef1ed4ef6d831cfacc752b126298565ac6b469d067740b7e3b" => :el_capitan
-  end
 
   depends_on "erlang"
 
