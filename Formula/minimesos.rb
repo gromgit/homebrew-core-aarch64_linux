7481927646082a31b8a25685f0ef8abf95c3372c
commitay <commitay@users.noreply.github.com>
2018-08-29 00:46:03 +1000

    minimesos: remove docker dependency
    also, update the test

--- Formula/minimesos.rb
+++ Formula/minimesos.rb
@@ -3,16 +3,16 @@ class Minimesos < Formula
   homepage "https://minimesos.org/"
   url "https://github.com/ContainerSolutions/minimesos/archive/0.13.0.tar.gz"
   sha256 "806a2e7084d66431a706e365814fca8603ba64780ac6efc90e52cbf7ef592250"
+  revision 1
 
   bottle :unneeded
-
-  depends_on "docker"
 
   def install
     bin.install "bin/minimesos"
   end
 
   test do
-    assert_match "Docker", shell_output("#{bin}/minimesos --help 2>&1", 1)
+    output = shell_output("#{bin}/minimesos --help 2>&1", 127)
+    assert_match "docker: command not found", output
   end
 end
