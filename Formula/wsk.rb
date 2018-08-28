2d92aaa14293376d4903724efc7efdc20ec56c8b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-28 20:09:40 +0000

    wsk: add 0.9.0-incubating bottle.

--- Formula/wsk.rb
+++ Formula/wsk.rb
@@ -5,6 +5,14 @@ class Wsk < Formula
   version "0.9.0-incubating"
   sha256 "76ec64d1a505c88f7d13df898b07cdea7b13b9799747d432e0bde55f7dc2c8b9"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "01efb1cc64b33be3d77aa477638179d75b159be1d956f95a84d04b7586175214" => :mojave
+    sha256 "3a0e83ba6602152b582acc334c71a7aaf375dcc46730bc66aad3212fd840a348" => :high_sierra
+    sha256 "f80044426ee56b6e70bb9eb8f63c95772e3d60f9aa579572e17bbf168ea91eca" => :sierra
+    sha256 "12b2a1282a36a66f9abc148dcc6dd93cce4e34e381088d0f3b519132776a43e8" => :el_capitan
+  end
+
   depends_on "go" => :build
   depends_on "go-bindata" => :build
   depends_on "govendor" => :build
