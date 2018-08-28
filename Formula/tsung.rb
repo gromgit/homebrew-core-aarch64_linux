e327f34e794304ee15e4f1649793e13d6de62f79
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-28 21:28:53 +0000

    tsung: update 1.7.0 bottle.

--- Formula/tsung.rb
+++ Formula/tsung.rb
@@ -8,6 +8,7 @@ class Tsung < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "bfd02c24483727832c624e5de2e289efec4eaf30b651be8da85696c1c896c091" => :mojave
     sha256 "64dba403e11577b28f3a80114158b96d8c74f58d09a4d9930801674031d4a7d9" => :high_sierra
     sha256 "e52abdb35507ceff03804d29a1ecf4e64d11e3345a9f095462cb653bba6cac6d" => :sierra
     sha256 "11f06a010b1a56d7a751bf5379d7d053c1befdf41f73aabeb79330761566724d" => :el_capitan
