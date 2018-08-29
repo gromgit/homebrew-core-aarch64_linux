dffa85173b94f46a78b9e53909e5bde10311ee2e
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-29 13:36:18 +0100

    mdcat: avoid cargo deprecated syntax warning

--- Formula/mdcat.rb
+++ Formula/mdcat.rb
@@ -15,7 +15,7 @@ class Mdcat < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "--root", prefix
+    system "cargo", "install", "--root", prefix, "--path", "."
   end
 
   test do
