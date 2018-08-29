23e8508d9b1211caf25bcbd29f62904d73a1cb45
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-29 13:35:46 +0100

    ktmpl: avoid cargo deprecated syntax warning

--- Formula/ktmpl.rb
+++ Formula/ktmpl.rb
@@ -16,7 +16,7 @@ class Ktmpl < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "--root", prefix
+    system "cargo", "install", "--root", prefix, "--path", "."
   end
 
   test do
