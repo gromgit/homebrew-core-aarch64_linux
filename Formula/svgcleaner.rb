9cd7cf8597ca46d23eeb491f176b31f1e648345d
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-29 13:40:20 +0100

    svgcleaner: avoid cargo deprecated syntax warning

--- Formula/svgcleaner.rb
+++ Formula/svgcleaner.rb
@@ -15,7 +15,7 @@ class Svgcleaner < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "--root", prefix
+    system "cargo", "install", "--root", prefix, "--path", "."
   end
 
   test do
