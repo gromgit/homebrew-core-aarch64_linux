6ff8b8e4e13717cd5284926267598109237082fa
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-29 13:46:25 +0100

    xsv: avoid cargo deprecated syntax warning
    Closes #31567.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/xsv.rb
+++ Formula/xsv.rb
@@ -15,7 +15,7 @@ class Xsv < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "--root", prefix
+    system "cargo", "install", "--root", prefix, "--path", "."
   end
 
   test do
