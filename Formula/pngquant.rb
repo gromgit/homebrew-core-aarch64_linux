3ab51954231ed7fdefa78445b85a46414a6513a1
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-29 13:37:21 +0100

    pngquant: avoid cargo deprecated syntax warning

--- Formula/pngquant.rb
+++ Formula/pngquant.rb
@@ -19,7 +19,7 @@ class Pngquant < Formula
   depends_on "little-cms2"
 
   def install
-    system "cargo", "install", "--root", prefix
+    system "cargo", "install", "--root", prefix, "--path", "."
     man1.install "pngquant.1"
   end
 
