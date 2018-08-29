80be4f8b116920240b43d32b45c404d10bb49b13
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-29 21:56:18 +0000

    wireshark: update 2.6.3 bottle.

--- Formula/wireshark.rb
+++ Formula/wireshark.rb
@@ -7,10 +7,10 @@ class Wireshark < Formula
   head "https://code.wireshark.org/review/wireshark", :using => :git
 
   bottle do
-    sha256 "e6dc271e3128fee46dfef4150b61e3907462de6bb955f4a40326c800ca577c19" => :mojave
-    sha256 "a0e08a86c763b6d102b09a4bcef154b4959e13e38f978132fd5f0d28b0c1282c" => :high_sierra
-    sha256 "28ed5d2ad2b272d17dd104f0a58dbfb63b37a91438b70f631a9cec398d3f1626" => :sierra
-    sha256 "1b2cea3e894914ee3b853eb9d082720d988182c862c0c9b6514b7fc8685e7dda" => :el_capitan
+    sha256 "9fed2716bed0a8315f9c9a0f30f82d478e98fe111862f04a8464fca6e46bc450" => :mojave
+    sha256 "872a932622c6c2cc36add68a16b25ddedc694e5409d8a43b167df7a6266793b1" => :high_sierra
+    sha256 "0bc571ddc4ffce33a9573343f4ffd7b50e5cb074b7774daa934237c35ff0c031" => :sierra
+    sha256 "2965b3cbbace6a701c9e0c164af91ae432aaa77311e955b6dee763ac4c456467" => :el_capitan
   end
 
   deprecated_option "with-qt5" => "with-qt"
