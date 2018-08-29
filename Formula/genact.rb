e77441ec7ecaecbcb3d8164d92c50ac9eb7c647c
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-29 13:32:35 +0100

    genact: avoid cargo deprecated syntax warning

--- Formula/genact.rb
+++ Formula/genact.rb
@@ -14,7 +14,7 @@ class Genact < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "--root", prefix
+    system "cargo", "install", "--root", prefix, "--path", "."
   end
 
   test do
