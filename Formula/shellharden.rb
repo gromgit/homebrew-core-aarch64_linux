f77377dfca76dccb33cd70954c4c3bbba107f4ab
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-29 13:39:43 +0100

    shellharden: avoid cargo deprecated syntax warning

--- Formula/shellharden.rb
+++ Formula/shellharden.rb
@@ -14,7 +14,7 @@ class Shellharden < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "--root", prefix
+    system "cargo", "install", "--root", prefix, "--path", "."
   end
 
   test do
