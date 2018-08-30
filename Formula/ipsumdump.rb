38078c715d094a362f442049a405f4aa192975ab
Viktor Szakats <commit@vszakats.net>
2018-08-30 18:02:27 +0000

    ipsumdump: secure url(s)

--- Formula/ipsumdump.rb
+++ Formula/ipsumdump.rb
@@ -1,7 +1,7 @@
 class Ipsumdump < Formula
   desc "Summarizes TCP/IP dump files into a self-describing ASCII format"
-  homepage "http://www.read.seas.harvard.edu/~kohler/ipsumdump/"
-  url "http://www.read.seas.harvard.edu/~kohler/ipsumdump/ipsumdump-1.86.tar.gz"
+  homepage "https://read.seas.harvard.edu/~kohler/ipsumdump/"
+  url "https://read.seas.harvard.edu/~kohler/ipsumdump/ipsumdump-1.86.tar.gz"
   sha256 "e114cd01b04238b42cd1d0dc6cfb8086a6b0a50672a866f3d0d1888d565e3b9c"
   head "https://github.com/kohler/ipsumdump.git"
 
