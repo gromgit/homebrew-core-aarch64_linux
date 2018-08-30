145d2303170ad2d05ddb0aa845a5cb916003a49b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-30 17:54:48 +0000

    stellar-core: update 9.2.0_1 bottle.

--- Formula/stellar-core.rb
+++ Formula/stellar-core.rb
@@ -9,6 +9,7 @@ class StellarCore < Formula
 
   bottle do
     cellar :any
+    sha256 "94c8b7f5d72e88d7f772ffd74ccaf8e51c90add0f3665f2c4db09f95e1cd16cf" => :mojave
     sha256 "42973d1b0d23fa0d64d27c171a3feeb7256545030e678637b7ba428df6d066ed" => :high_sierra
     sha256 "1f6be307febae9e4711669efe0e04f02d88884e10dc6a590e5c8c2e7ade321d5" => :sierra
     sha256 "441e28cb07e72faf6d89e15a47b0bfc4469c2faa95ee2671ce33e2184b843914" => :el_capitan
