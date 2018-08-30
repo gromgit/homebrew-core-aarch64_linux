9c7ac66ad27fa3a396d3cbe29649f5fccca09ad8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-30 03:10:51 +0000

    ephemeralpg: update 2.5 bottle.

--- Formula/ephemeralpg.rb
+++ Formula/ephemeralpg.rb
@@ -7,6 +7,7 @@ class Ephemeralpg < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "537dbb19980fce982e2859e14852d31fb5f4f91fc62ef062dab427890bf334ce" => :mojave
     sha256 "fa778995e1b3d3adb26a3ebd0a584376dd85e5239cdf8417643cb2984040bb6c" => :high_sierra
     sha256 "23c036094f518eb3d98539e410ea95bbe237bda31b205f3741fb46bb7d5e32c5" => :sierra
     sha256 "fe44d3d4814322c408e59944d86d56214c6056547111957efdbf08e382d4672b" => :el_capitan
