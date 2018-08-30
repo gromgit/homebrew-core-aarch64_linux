6bd81e0dd585a07af2db4049b711681b5bb071a1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-30 18:13:45 +0000

    erlang: update 21.0.7 bottle.

--- Formula/erlang.rb
+++ Formula/erlang.rb
@@ -8,10 +8,10 @@ class Erlang < Formula
 
   bottle do
     cellar :any
-    sha256 "3723fa8dcf35584e6b5ca5c02f40c599aa560816f5b7c336b3b8c1481ddfb60f" => :mojave
-    sha256 "9c58a22b3c86f14c8b5eecf444ea61dea1fa731f86222f9e9106f9ae6b804503" => :high_sierra
-    sha256 "9eab6e65753afe104bb79b5be8322dd418e2354a160c8af987707be58b36216e" => :sierra
-    sha256 "0a3cca80b71459a243ecc55cb6f9d580c16c7be6ca1687509433e390fdc73a27" => :el_capitan
+    sha256 "c10c220e31a5cc40710b31de4d82e7c3a02415437d03b5fc9f4db4f9a445f077" => :mojave
+    sha256 "0d55548580458ef7d752f89776dea3a453d37a4849e8a4d20f901826bd51d0b0" => :high_sierra
+    sha256 "a4a5b52946ff12524587b4e22eed25b0b72aaeded681caf8168d0258ff9fb78b" => :sierra
+    sha256 "88373295c456ed9800c4ca780c865076196f95a08bd066e7e800a465f839a5df" => :el_capitan
   end
 
   option "without-hipe", "Disable building hipe; fails on various macOS systems"
