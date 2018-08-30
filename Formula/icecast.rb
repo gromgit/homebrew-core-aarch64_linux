d22a03046d7e4beb2a54f6441ccf14c26742785f
Viktor Szakats <commit@vszakats.net>
2018-08-30 18:02:27 +0000

    icecast: secure url(s)

--- Formula/icecast.rb
+++ Formula/icecast.rb
@@ -1,6 +1,6 @@
 class Icecast < Formula
   desc "Streaming MP3 audio server"
-  homepage "http://www.icecast.org/"
+  homepage "https://icecast.org/"
   url "https://downloads.xiph.org/releases/icecast/icecast-2.4.3.tar.gz"
   sha256 "c85ca48c765d61007573ee1406a797ae6cb31fb5961a42e7f1c87adb45ddc592"
 
