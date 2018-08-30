7ce69d6dc23119f88ea59d94dd61f92ef2d15e7a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-30 23:58:38 +0000

    efl: update 1.21.0 bottle.

--- Formula/efl.rb
+++ Formula/efl.rb
@@ -5,6 +5,7 @@ class Efl < Formula
   sha256 "7e65be78a537aa67e447b945f01f4ecf9ddfa14d509bf6bbf53a60253ecbae4b"
 
   bottle do
+    sha256 "46b40afaea0994eea79a05e464361339d9fd4b1c03fc03368e5ea449f7f2f234" => :mojave
     sha256 "dbc8f35200613ced92cd39217955de3370259c3f158ff35603f9bf5de65fbd73" => :high_sierra
     sha256 "33002e4aef9bccaa09d7412c045277966fd2093bf76e7cc8c66d27a37b15d1ed" => :sierra
     sha256 "04ee0cab98b1c770ecca86ee6e5935d8e23b5732d1afd663f09d5bb9a7da10d1" => :el_capitan
