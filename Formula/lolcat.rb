4a4e25323b21575dd1de4f1cd8bf41ce7bdf83e3
Chongyu Zhu <i@lembacon.com>
2018-08-31 01:53:01 +0800

    lolcat 99.9.19
    Closes #31606.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/lolcat.rb
+++ Formula/lolcat.rb
@@ -2,8 +2,8 @@ class Lolcat < Formula
   desc "Rainbows and unicorns in your console!"
   homepage "https://github.com/busyloop/lolcat"
   url "https://github.com/busyloop/lolcat.git",
-      :tag => "v99.9.11",
-      :revision => "3c870f31462dd1381adb0f4dee28997903f1ecdb"
+      :tag => "v99.9.19",
+      :revision => "40692d01733c5d360022a14f08a4415668544f37"
 
   bottle do
     cellar :any_skip_relocation
@@ -12,6 +12,8 @@ class Lolcat < Formula
     sha256 "ae227d19279381bb06454be4ab732955f0c98aefe4ec5096af51f89afefb22e2" => :sierra
     sha256 "720bcfcc342a16b3c7df9b5a558561d42d09f268f9708fac4f7f24af9fec81df" => :el_capitan
   end
+
+  depends_on "ruby" if MacOS.version <= :sierra
 
   def install
     ENV["GEM_HOME"] = libexec
