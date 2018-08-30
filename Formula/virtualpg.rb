d80dd7edb05ca611c3973750fe62c7bec08a984c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-30 20:30:07 +0000

    virtualpg: update 1.0.2 bottle.

--- Formula/virtualpg.rb
+++ Formula/virtualpg.rb
@@ -6,6 +6,7 @@ class Virtualpg < Formula
 
   bottle do
     cellar :any
+    sha256 "11f87cc36cdd1bc00799f4d877b7c25c6ffae17c32322041831cec002d927c8d" => :mojave
     sha256 "2e7d844424706349b16cbbf1f7ab6fd257703ed8de3347277090c75cadb491b8" => :high_sierra
     sha256 "1d61bc6b83f9584e6a787b614f475177fe98472bc6fa697c1a6695dfe6f65701" => :sierra
     sha256 "47b8d302bde32565c4b1a627041fda8df645b4349d3eb5ff250b266309d47271" => :el_capitan
