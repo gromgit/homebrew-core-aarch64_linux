80bd6a9581ad8986c2b81a22bb6bc82efd4a1110
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-30 03:54:37 +0000

    pg_top: update 3.7.0_3 bottle.

--- Formula/pg_top.rb
+++ Formula/pg_top.rb
@@ -8,6 +8,7 @@ class PgTop < Formula
 
   bottle do
     cellar :any
+    sha256 "d31b2fb44c6d363f0f635bf6a16427968ca610ea285569bfa867bea6d0437549" => :mojave
     sha256 "32637c635d8da78d4910df2dabd474f4115c31cba57890ad053b3a43cb38a758" => :high_sierra
     sha256 "5f06ae8b8ef1c979143e19c0527c31c8d649d23e1e9612c63bc6c5ff05bf8276" => :sierra
     sha256 "6d0104d461d7187ad02e1085098f2dad4fa00c4f2db93b1b910a6a072517ca54" => :el_capitan
