d1c9e30ea1ea8ba5bfe8b555f06b1551decbb1f9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-31 00:08:39 +0000

    zsxd: update 1.11.0 bottle.

--- Formula/zsxd.rb
+++ Formula/zsxd.rb
@@ -7,6 +7,7 @@ class Zsxd < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f98aa8c723cfddb79aead81d9a4c9ede96d2c7d19ea3dba87d8f2ee069f67877" => :mojave
     sha256 "771e8ae2db1b6ab59d0ecaceee0265540a9a9d7f46b0be1fc7017842e743158a" => :high_sierra
     sha256 "cf2a0976c81fd8e06116d7728804ce266265447ebede2f80182f9f07071ec2da" => :sierra
     sha256 "56bd3750bfa55261fc43cb13144d3b81db7ae6743eb14ab0d2efa0783efb7e26" => :el_capitan
