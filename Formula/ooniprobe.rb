63327718576de723c9063eec235350a4397bbae4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-31 10:02:47 +0000

    ooniprobe: update 2.3.0 bottle.

--- Formula/ooniprobe.rb
+++ Formula/ooniprobe.rb
@@ -8,9 +8,11 @@ class Ooniprobe < Formula
 
   bottle do
     cellar :any
-    sha256 "63c467de1c93e89d6eb555587ccf5048fc7d48646a1359ae51549635d619fe27" => :high_sierra
-    sha256 "190aa6d0fc5824a8f34c364ba6c436c6a7c4a6485afd10b9aaf39885ee2e870c" => :sierra
-    sha256 "21c9bbeaf1f5ab28751714147db3257824535eda0068c701707c8ad02fe50372" => :el_capitan
+    rebuild 1
+    sha256 "5190bd77c40eecf39da965167329fc552690965ec9d7930352c2828814e35d0d" => :mojave
+    sha256 "32e3e7e10bfd2105caafb57a2c91d23eb1077c6508e155766ae618ed20e66a3c" => :high_sierra
+    sha256 "1482b823d3d9ee2fa338fc9b08cf30de5d29f770a1aedf339115283d1c07465e" => :sierra
+    sha256 "38b51f8ef56029ca251c100887a1b66df0dfdd099f582913ca020027b3010f83" => :el_capitan
   end
 
   depends_on "geoip"
