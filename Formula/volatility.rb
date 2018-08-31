167caa1ff2d5c7e42d5d291a8ee04abdbb0b5d50
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-31 03:35:56 +0100

    volatility: fix Mojave build
    Closes #31619.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/volatility.rb
+++ Formula/volatility.rb
@@ -152,7 +152,7 @@ class Volatility < Formula
 
     resource("Pillow").stage do
       inreplace "setup.py" do |s|
-        sdkprefix = MacOS::CLT.installed? ? "" : MacOS.sdk_path
+        sdkprefix = MacOS.sdk_path_if_needed ? MacOS.sdk_path : ""
         s.gsub! "openjpeg.h", "probably_not_a_header_called_this_eh.h"
         s.gsub! "ZLIB_ROOT = None", "ZLIB_ROOT = ('#{sdkprefix}/usr/lib', '#{sdkprefix}/usr/include')"
         s.gsub! "JPEG_ROOT = None", "JPEG_ROOT = ('#{Formula["jpeg"].opt_prefix}/lib', '#{Formula["jpeg"].opt_prefix}/include')"
