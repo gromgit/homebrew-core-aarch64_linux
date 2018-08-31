4059c7b500a809b6e9d0af1c25736e30eb6969c7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-31 00:08:41 +0000

    snort: update 2.9.11.1 bottle.

--- Formula/snort.rb
+++ Formula/snort.rb
@@ -6,6 +6,7 @@ class Snort < Formula
 
   bottle do
     cellar :any
+    sha256 "7eb1bf8de87e1f55412cbc27e4b7e25d37819506481a036058d666ca2c62052e" => :mojave
     sha256 "ac8a7b3007b307d8368da7e3879e3c6cd6c718746b9443d544f5e8adcb286be6" => :high_sierra
     sha256 "b152a9869efd30d40e0d3750a0fd12f825360bf7c1d7cb39156320d32381a649" => :sierra
     sha256 "00de9088a3e7471026430cf17a7cc7d3d9787496398662c2e2c6a7c766c212cd" => :el_capitan
