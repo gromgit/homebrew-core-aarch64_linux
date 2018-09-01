e1346ddb9ec8181b7f55f9fcc3eec7dcb2f75996
Dominyk Tiller <dominyktiller@gmail.com>
2018-09-01 04:13:32 +0100

    picard-tools 2.18.12
    Closes #31653.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/picard-tools.rb
+++ Formula/picard-tools.rb
@@ -1,8 +1,8 @@
 class PicardTools < Formula
   desc "Tools for manipulating HTS data and formats"
   homepage "https://broadinstitute.github.io/picard/"
-  url "https://github.com/broadinstitute/picard/releases/download/2.18.11/picard.jar"
-  sha256 "09adee14d1585951cf25bd32df79c8e18804324d1faeaaf8427674b9c6f7930f"
+  url "https://github.com/broadinstitute/picard/releases/download/2.18.12/picard.jar"
+  sha256 "b5d39868efe65b72bbab6721a744e9c6445c3795e946d8641f9244c95c38bc64"
 
   bottle :unneeded
 
