9e4e0b2146d10c29bec027a797967cc747624d93
Viktor Szakats <commit@vszakats.net>
2018-09-01 20:49:49 +0000

    wcslib: secure url(s)

--- Formula/wcslib.rb
+++ Formula/wcslib.rb
@@ -1,7 +1,7 @@
 class Wcslib < Formula
   desc "Library and utilities for the FITS World Coordinate System"
   homepage "https://www.atnf.csiro.au/people/mcalabre/WCS/"
-  url "ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib-5.19.1.tar.bz2"
+  url "https://www.atnf.csiro.au/pub/software/wcslib/wcslib-5.19.1.tar.bz2"
   sha256 "59b9f0e5a2c040773cc846c684d84c09b986c1393e97b378a41b92d9d3df0f98"
 
   bottle do
