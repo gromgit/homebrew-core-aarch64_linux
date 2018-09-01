be3f2c8f7eea436342c8b7f95161896fde814c9c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-01 03:19:50 +0000

    hebcal: update 4.14 bottle.

--- Formula/hebcal.rb
+++ Formula/hebcal.rb
@@ -6,11 +6,10 @@ class Hebcal < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "05e964234b746361d9272ce88944e652cc000ff4a3afb03f202bc7000d1fe607" => :mojave
-    sha256 "95fd596027fcf571cd1091be417f28ad59ee838aab4c0769f8f36ae10896eeb0" => :high_sierra
-    sha256 "b520aadc300e1660d66e8ee26712405b3581ad32b20c119908c5890d90f721e9" => :sierra
-    sha256 "55630d52364af42172ae1abc10847110fabe93f94ae21708863803be7c35fdc9" => :el_capitan
-    sha256 "37d26466a7deb14cb2f38cc7eff1621eef652239bb973b3f5c5c870cb624399d" => :yosemite
+    sha256 "be0d8c1322fd71526a645a5de6e99e98fbe902381c8461d599b2cab296efe57f" => :mojave
+    sha256 "6cac34a093eef64232de52d8a8bbb73df044cb0932f2904885eebb259e283629" => :high_sierra
+    sha256 "a99efba435b3c1cfd0fc99414fa529fa47d511ce64d031f575ee6eeddb85d6a6" => :sierra
+    sha256 "9cb1ce12d1e70adf3bb6c7114079253155c4a494682be0c3ac9ef8c0390da226" => :el_capitan
   end
 
   depends_on "autoconf" => :build
