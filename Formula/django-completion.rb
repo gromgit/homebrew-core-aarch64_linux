21411932487f060f04faef73af96d945a2239ef3
commitay <commitay@users.noreply.github.com>
2018-09-01 19:15:19 +1000

    django-completion 2.1.1 (#31663)

--- Formula/django-completion.rb
+++ Formula/django-completion.rb
@@ -1,8 +1,8 @@
 class DjangoCompletion < Formula
   desc "Bash completion for Django"
   homepage "https://github.com/django/django"
-  url "https://github.com/django/django/archive/2.1.tar.gz"
-  sha256 "9e1750ba6caa78d982d283e2bc2a153892a1e288f0c1eb36ab95c4061eb7cbe5"
+  url "https://github.com/django/django/archive/2.1.1.tar.gz"
+  sha256 "55be7d98121fbff07e503a762748943c1de637608d8db6ed81bc9e97e6924df5"
   head "https://github.com/django/django.git"
 
   bottle :unneeded
