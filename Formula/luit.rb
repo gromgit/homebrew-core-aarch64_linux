8ed7ac3c9c334fb61af73729cb340e4bb2d71291
Viktor Szakats <commit@vszakats.net>
2018-09-01 23:12:25 +0000

    luit: secure url(s)

--- Formula/luit.rb
+++ Formula/luit.rb
@@ -1,7 +1,7 @@
 class Luit < Formula
   desc "Filter run between arbitrary application and UTF-8 terminal emulator"
   homepage "https://invisible-island.net/luit/"
-  url "ftp://ftp.invisible-island.net/luit/luit-20180628.tgz"
+  url "https://invisible-mirror.net/archives/luit/luit-20180628.tgz"
   sha256 "7b84f63072589e9b03bb3e99e01ef344bb37793b76ad1cbb7b11f05000d64844"
 
   bottle do
