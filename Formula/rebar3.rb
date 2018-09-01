a4831ddf6162eb0e166f113f8903dc14e1b385d7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-01 09:33:48 +0000

    rebar3: add 3.6.1 bottle.

--- Formula/rebar3.rb
+++ Formula/rebar3.rb
@@ -4,6 +4,14 @@ class Rebar3 < Formula
   url "https://github.com/erlang/rebar3/archive/3.6.1.tar.gz"
   sha256 "40b3c85440f3235c7b149578d0211bdf57d1c66390f888bb771704f8abc71033"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2e456bf255e7e2555a47f13cbc880f7f9f74bc094b1200c3a8d3f14263720af2" => :mojave
+    sha256 "f05c4808e13a18f6670a7f57a5f5b7dbeb85d528881c41a62f80998b57b56d82" => :high_sierra
+    sha256 "e8019293528b825fa3cae8d43ef08d42cc4864a24921aec510e68a054e4749bf" => :sierra
+    sha256 "2355b23bc209911c1154c1f0a80b2a7e474dcb9959e612bf145b278e1ea930dc" => :el_capitan
+  end
+
   depends_on "erlang"
 
   def install
