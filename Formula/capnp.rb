3004c676b6382f88b83099a086a99959667dd32d
Dominyk Tiller <dominyktiller@gmail.com>
2018-09-01 12:34:43 +0100

    capnp 0.7.0
    Closes #31673.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/capnp.rb
+++ Formula/capnp.rb
@@ -1,8 +1,8 @@
 class Capnp < Formula
   desc "Data interchange format and capability-based RPC system"
   homepage "https://capnproto.org/"
-  url "https://capnproto.org/capnproto-c++-0.6.1.tar.gz"
-  sha256 "8082040cd8c3b93c0e4fc72f2799990c72fdcf21c2b5ecdae6611482a14f1a04"
+  url "https://capnproto.org/capnproto-c++-0.7.0.tar.gz"
+  sha256 "c9a4c0bd88123064d483ab46ecee777f14d933359e23bff6fb4f4dbd28b4cd41"
   head "https://github.com/capnproto/capnproto.git"
 
   bottle do
@@ -14,11 +14,11 @@ class Capnp < Formula
     sha256 "726278b97a0fab5be359b604b08dc8ea9b5cd7a8a1e350e6724aaa40b7bbd5a2" => :yosemite
   end
 
-  needs :cxx11
   depends_on "cmake" => :build
 
+  needs :cxx14
+
   def install
-    ENV.cxx11
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
       system "make", "install"
