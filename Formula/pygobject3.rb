ee44ad9b9c734acd1d491155a10e88d95ed345b6
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-09-01 14:51:07 +0100

    pygobject3 3.30.0
    Closes #31681.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/pygobject3.rb
+++ Formula/pygobject3.rb
@@ -1,9 +1,8 @@
 class Pygobject3 < Formula
   desc "GNOME Python bindings (based on GObject Introspection)"
   homepage "https://wiki.gnome.org/Projects/PyGObject"
-  url "https://download.gnome.org/sources/pygobject/3.28/pygobject-3.28.3.tar.xz"
-  sha256 "3dd3e21015d06e00482ea665fc1733b77e754a6ab656a5db5d7f7bfaf31ad0b0"
-  revision 1
+  url "https://download.gnome.org/sources/pygobject/3.30/pygobject-3.30.0.tar.xz"
+  sha256 "7d20ba1475df922f4c26c69274ab89f7e7730d2101e46846caaddc53afd56bd0"
 
   bottle do
     cellar :any
@@ -16,22 +15,32 @@ class Pygobject3 < Formula
   option "without-python", "Build without python3 support"
   option "with-python@2", "Build with python2 support"
 
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "libffi" => :optional
-  depends_on "glib"
+  depends_on "python" => [:build, :recommended]
+  depends_on "gobject-introspection"
   depends_on "python@2" => :optional
-  depends_on "python" => :recommended
   depends_on "py2cairo" if build.with? "python@2"
   depends_on "py3cairo" if build.with? "python"
-  depends_on "gobject-introspection"
 
   def install
-    Language::Python.each_python(build) do |python, _version|
-      system "./configure", "--disable-dependency-tracking",
-                            "--prefix=#{prefix}",
-                            "PYTHON=#{python}"
-      system "make", "install"
-      system "make", "clean"
+    Language::Python.each_python(build) do |python, version|
+      mkdir "build#{version}" do
+        system "meson", "--prefix=#{prefix}",
+                        "-Dpycairo=true",
+                        "-Dpython=#{python}",
+                        ".."
+
+        # avoid linking against python framework
+        # reported at https://gitlab.gnome.org/GNOME/pygobject/issues/253
+        libs = Utils.popen_read("pkg-config --libs python-#{version}").chomp.split
+        dylib = libs[0][2..-1] + "/lib" + libs[1][2..-1] + ".dylib"
+        inreplace "build.ninja", dylib, ""
+
+        system "ninja", "-v"
+        system "ninja", "install"
+      end
     end
   end
 
