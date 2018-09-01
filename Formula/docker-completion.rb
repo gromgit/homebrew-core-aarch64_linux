f9c4a09e53cc7bbd12653a0ffa487e02b82ffe8b
commitay <commitay@users.noreply.github.com>
2018-09-01 19:17:45 +1000

    docker-completion 18.06.1 (#31664)

--- Formula/docker-completion.rb
+++ Formula/docker-completion.rb
@@ -2,8 +2,8 @@ class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/docker-ce.git",
-      :tag => "v18.06.0-ce",
-      :revision => "0ffa8257ec673ed6849b73b03fb01b0cac90fdb3"
+      :tag => "v18.06.1-ce",
+      :revision => "e68fc7a215d7133c34aa18e3b72b4a21fd0c6136"
 
   bottle :unneeded
 
