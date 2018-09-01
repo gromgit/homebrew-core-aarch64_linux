27151510aa06309721e043c7540eb958bf696608
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-01 12:01:10 +0000

    buku: update 3.9 bottle.

--- Formula/buku.rb
+++ Formula/buku.rb
@@ -8,10 +8,10 @@ class Buku < Formula
 
   bottle do
     cellar :any
-    sha256 "cc4e23af04567f3c6f94a689bdc7312c8a1d88494546bd09b6a12164afa48f11" => :mojave
-    sha256 "6aa9a96919d8cfc367c5102c2ccf1cf2ce0a4f6a30d374881328e9daefc023b0" => :high_sierra
-    sha256 "f0773f347a371b107124290fb1cd9c46831442fcd0126edfe10376b61603644b" => :sierra
-    sha256 "f782412acfeda802e36d8ea037e07ef0f132973dcf1845e86269b2078a12cd2d" => :el_capitan
+    sha256 "c456ad1c4a94d84a9f3027cdf2bd24c27f3d190194f0708e6f77017cae4aba89" => :mojave
+    sha256 "767a54fc3724058bc84d0ae7c2d53065c8b1ddb50a1dc8b58d94395e28c715de" => :high_sierra
+    sha256 "2a509cf47639dab303f1c84943c322062285ba3ea94a0f2061d8e9c46f6e6632" => :sierra
+    sha256 "19c526c4af8d3893fb3a6b1dbcbb6d4aacb0caaba4157284f17581fd1b1eb482" => :el_capitan
   end
 
   depends_on "openssl"
