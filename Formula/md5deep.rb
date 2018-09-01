fa2c07c21478795183a06e772394350f60521d28
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-01 15:47:59 +0000

    md5deep: update 4.4 bottle.

--- Formula/md5deep.rb
+++ Formula/md5deep.rb
@@ -7,6 +7,7 @@ class Md5deep < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "48fe3167c6211f51af6d8c1e39062438a7385e1b136078fbc0215170842ecbbe" => :mojave
     sha256 "5f5636f7731398f775d757cb4ae913762f725d4d7bd3060a2640c155207d7a2a" => :high_sierra
     sha256 "4ee90230c25f9872541d3f895fbe010765dd2e5449e56a0987e3652f89014916" => :sierra
     sha256 "986dad46d2945aac775eb625e41b0236f2413b3924244d5e9aba445994c38687" => :el_capitan
