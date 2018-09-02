d93e4df9a2d9d0641b71072cb1c1b6d86facd1fc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-02 21:30:37 +0000

    baobab: update 3.30.0 bottle.

--- Formula/baobab.rb
+++ Formula/baobab.rb
@@ -5,11 +5,10 @@ class Baobab < Formula
   sha256 "5e4dd06f241eb32f00850efa1a4541cee088de480be2b52d788143187410a74f"
 
   bottle do
-    cellar :any
-    sha256 "82714fced60aab9783af682b720830e4b0bed8b4cfb816372f387ac3d41cb715" => :mojave
-    sha256 "df91083504c615b21a90f0d1a8ec164f711fa336c5884feec211ac46678aee32" => :high_sierra
-    sha256 "1e5b3391a333627116594477b5fe8a8bc8348312f84a518a757f53b1d12a89cb" => :sierra
-    sha256 "6b271f297efc4b0e0c27537b2334f3aff80e8a3ac4486c5abfe48737ab4f2251" => :el_capitan
+    sha256 "b63fcc8dd3de87dc5ccb37b51a60038eb481f457ef93af3418600b0cbcf600f6" => :mojave
+    sha256 "11f6b1bc93bc07d3d029437fca774875c253d0979b689e0e3515da2cd7f62faa" => :high_sierra
+    sha256 "2ae6e39ef772e7a43fd04a58c65576b2fd828398f05138656ff48475a9f92626" => :sierra
+    sha256 "fac932ecf76732c1284ab91d1da849a514e5b818a684de9b2d4b792cfc6d9a37" => :el_capitan
   end
 
   depends_on "meson" => :build
