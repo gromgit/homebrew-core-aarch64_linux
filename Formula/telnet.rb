7bfcab22ae62b477938f1373e57a670ace9dcf42
FX Coudert <fxcoudert@gmail.com>
2018-09-02 20:14:08 +0200

    telnet 60
    Closes #31711.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/telnet.rb
+++ Formula/telnet.rb
@@ -1,8 +1,8 @@
 class Telnet < Formula
   desc "User interface to the TELNET protocol (built from macOS Sierra sources)"
   homepage "https://opensource.apple.com/"
-  url "https://opensource.apple.com/tarballs/remote_cmds/remote_cmds-54.50.1.tar.gz"
-  sha256 "156ddec946c81af1cbbad5cc6e601135245f7300d134a239cda45ff5efd75930"
+  url "https://opensource.apple.com/tarballs/remote_cmds/remote_cmds-60.tar.gz"
+  sha256 "9d27417d5032113e93edebc37f82a060536bc557b119544e59c46aeb1be92820"
 
   bottle do
     cellar :any_skip_relocation
@@ -25,24 +25,26 @@ class Telnet < Formula
     resource("libtelnet").stage do
       ENV["SDKROOT"] = MacOS.sdk_path
       ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version
-      xcodebuild "SYMROOT=build"
+
+      # Force 64 bit-only build, otherwise it fails on Mojave
+      xcodebuild "SYMROOT=build", "-arch", "x86_64"
 
       libtelnet_dst = buildpath/"telnet.tproj/build/Products"
       libtelnet_dst.install "build/Release/libtelnet.a"
       libtelnet_dst.install "build/Release/usr/local/include/libtelnet/"
     end
 
-    system "make",
-      "-C", "telnet.tproj",
-      "OBJROOT=build/Intermediates",
-      "SYMROOT=build/Products",
-      "DSTROOT=build/Archive",
-      "CFLAGS=$(CC_Flags) -isystembuild/Products/",
-      "LDFLAGS=$(LD_Flags) -Lbuild/Products/",
-      "install"
+    system "make", "-C", "telnet.tproj",
+                   "OBJROOT=build/Intermediates",
+                   "SYMROOT=build/Products",
+                   "DSTROOT=build/Archive",
+                   "CFLAGS=$(CC_Flags) -isystembuild/Products/",
+                   "LDFLAGS=$(LD_Flags) -Lbuild/Products/",
+                   "RC_ARCHS=x86_64", # Force 64-bit build for Mojave
+                   "install"
 
-    bin.install "telnet.tproj/build/Archive/usr/bin/telnet"
-    man.install "telnet.tproj/build/Archive/usr/share/man/man1/"
+    bin.install "telnet.tproj/build/Archive/usr/local/bin/telnet"
+    man1.install "telnet.tproj/telnet.1"
   end
 
   test do
