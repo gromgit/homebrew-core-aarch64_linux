adc87f436b3c9a1fd897e6e6e7eec311c7691f0c
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-09-02 10:11:42 +0100

    gjs 1.54.0
    Closes #31696.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/gjs.rb
+++ Formula/gjs.rb
@@ -1,8 +1,8 @@
 class Gjs < Formula
   desc "JavaScript Bindings for GNOME"
   homepage "https://wiki.gnome.org/Projects/Gjs"
-  url "https://download.gnome.org/sources/gjs/1.52/gjs-1.52.3.tar.xz"
-  sha256 "ce21d8a83f6077b011b8834c4936281be65b2b62387f0745c3eb9adf780996fc"
+  url "https://download.gnome.org/sources/gjs/1.54/gjs-1.54.0.tar.xz"
+  sha256 "e22c63f4dbf243d7a5a6660f6443ca3e5768695a48fc9fdf078bce7cf7aa657d"
 
   bottle do
     sha256 "15ccae3e0b90603b357f06c702d2b66892a71bb5c3d8502b317989cdc92de923" => :high_sierra
@@ -19,16 +19,16 @@ class Gjs < Formula
 
   needs :cxx11
 
-  resource "mozjs52" do
-    url "https://archive.mozilla.org/pub/firefox/releases/52.3.0esr/source/firefox-52.3.0esr.source.tar.xz"
-    sha256 "c16bc86d6cb8c2199ed1435ab80a9ae65f9324c820ea0eeb38bf89a97d253b5b"
+  resource "mozjs60" do
+    url "https://archive.mozilla.org/pub/firefox/releases/60.1.0esr/source/firefox-60.1.0esr.source.tar.xz"
+    sha256 "a4e7bb80e7ebab19769b2b8940966349136a99aabd497034662cffa54ea30e40"
   end
 
   def install
     ENV.cxx11
     ENV["_MACOSX_DEPLOYMENT_TARGET"] = ENV["MACOSX_DEPLOYMENT_TARGET"]
 
-    resource("mozjs52").stage do
+    resource("mozjs60").stage do
       inreplace "config/rules.mk", "-install_name $(_LOADER_PATH)/$(SHARED_LIBRARY) ", "-install_name #{lib}/$(SHARED_LIBRARY) "
       inreplace "old-configure", "-Wl,-executable_path,${DIST}/bin", ""
       mkdir("build") do
@@ -43,10 +43,10 @@ class Gjs < Formula
                               "--enable-optimize",
                               "--enable-pie",
                               "--enable-release",
-                              "--without-intl-api"
+                              "--with-intl-api",
+                              "--disable-jemalloc"
         system "make"
         system "make", "install"
-        lib.install "./mozglue/build/libmozglue.dylib"
         rm Dir["#{bin}/*"]
       end
       # headers were installed as softlinks, which is not acceptable
@@ -58,7 +58,6 @@ class Gjs < Formula
         end
       end
       ENV.append_path "PKG_CONFIG_PATH", "#{lib}/pkgconfig"
-      # remove mozjs static lib
       rm "#{lib}/libjs_static.ajs"
     end
 
