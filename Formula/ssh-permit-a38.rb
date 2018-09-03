87181a96cb5b182c9de6ea82c0d6a6370c8cbb7a
commitay <commitay@users.noreply.github.com>
2018-09-03 14:55:39 +1000

    ssh-permit-a38: update openssl env var and comment

--- Formula/ssh-permit-a38.rb
+++ Formula/ssh-permit-a38.rb
@@ -16,9 +16,10 @@ class SshPermitA38 < Formula
   depends_on "openssl"
 
   def install
-    ENV["OPENSSL_INCLUDE_DIR"] = Formula["openssl"].opt_include
-    ENV["DEP_OPENSSL_INCLUDE"] = Formula["openssl"].opt_include
-    ENV["OPENSSL_LIB_DIR"] = Formula["openssl"].opt_lib
+    # Ensure that the `openssl` crate picks up the intended library.
+    # https://crates.io/crates/openssl#manual-configuration
+    ENV["OPENSSL_DIR"] = Formula["openssl"].opt_prefix
+
     system "cargo", "install", "--root", prefix, "--path", "."
   end
 
