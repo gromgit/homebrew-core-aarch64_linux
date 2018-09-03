b826d1f91112fdfb6e02b65d5be42a051e070186
Dominyk Tiller <dominyktiller@gmail.com>
2018-09-03 02:14:56 +0100

    glslviewer 1.5.5
    Closes #31723.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/glslviewer.rb
+++ Formula/glslviewer.rb
@@ -1,8 +1,8 @@
 class Glslviewer < Formula
   desc "Live-coding console tool that renders GLSL Shaders"
   homepage "http://patriciogonzalezvivo.com/2015/glslViewer/"
-  url "https://github.com/patriciogonzalezvivo/glslViewer/archive/1.5.4.tar.gz"
-  sha256 "22ae0d32592d8864f77c6b04944b3b38210be36e47e7c8231b1e56ceba5a652f"
+  url "https://github.com/patriciogonzalezvivo/glslViewer/archive/1.5.5.tar.gz"
+  sha256 "28a784d701294fd335031ab293c5f4764a498f84714b1ae677dbc4e05ed94b23"
   head "https://github.com/patriciogonzalezvivo/glslViewer.git"
 
   bottle do
