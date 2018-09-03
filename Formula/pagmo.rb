f914e6a8c5d33d856bc8deb1d076f7835fb47ff0
Jeongseok Lee <jslee02@users.noreply.github.com>
2018-09-03 09:53:10 -0700

    pagmo 2.9 (new formula)
    Closes #31752.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

---
+++ Formula/pagmo.rb
@@ -0,0 +1,63 @@
+class Pagmo < Formula
+  desc "Scientific library for massively parallel optimization"
+  homepage "https://esa.github.io/pagmo2/"
+  url "https://github.com/esa/pagmo2/archive/v2.9.tar.gz"
+  sha256 "d482650e0c79a49ce0312c7e9e5722f3a1b24327e08af11daa66c59374bed3b4"
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "eigen"
+  depends_on "nlopt"
+
+  needs :cxx11
+
+  def install
+    ENV.cxx11
+    system "cmake", ".", "-DPAGMO_WITH_EIGEN3=ON", "-DPAGMO_WITH_NLOPT=ON",
+                         *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <iostream>
+
+      #include <pagmo/algorithm.hpp>
+      #include <pagmo/algorithms/sade.hpp>
+      #include <pagmo/archipelago.hpp>
+      #include <pagmo/problem.hpp>
+      #include <pagmo/problems/schwefel.hpp>
+
+      using namespace pagmo;
+
+      int main()
+      {
+          // 1 - Instantiate a pagmo problem constructing it from a UDP
+          // (user defined problem).
+          problem prob{schwefel(30)};
+
+          // 2 - Instantiate a pagmo algorithm
+          algorithm algo{sade(100)};
+
+          // 3 - Instantiate an archipelago with 16 islands having each 20 individuals
+          archipelago archi{16, algo, prob, 20};
+
+          // 4 - Run the evolution in parallel on the 16 separate islands 10 times.
+          archi.evolve(10);
+
+          // 5 - Wait for the evolutions to be finished
+          archi.wait_check();
+
+          // 6 - Print the fitness of the best solution in each island
+          for (const auto &isl : archi) {
+              std::cout << isl.get_population().champion_f()[0] << std::endl;
+          }
+
+          return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-I#{Formula["eigen"].include}/eigen3",
+                    "-I#{include}", "-std=c++11", "-o", "test"
+    system "./test"
+  end
+end
