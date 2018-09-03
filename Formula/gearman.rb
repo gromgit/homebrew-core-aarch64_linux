a31bff9e23fb74cac8a1cd6f21ba96f944791831
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-03 20:18:41 +0000

    gearman: update 1.1.18 bottle.

--- Formula/gearman.rb
+++ Formula/gearman.rb
@@ -5,6 +5,7 @@ class Gearman < Formula
   sha256 "d789fa24996075a64c5af5fd2adef10b13f77d71f7d44edd68db482b349c962c"
 
   bottle do
+    sha256 "df5a14dc5c29d07d373d42888d589f6d1ec053b8965d939b6647052aa694ebf5" => :mojave
     sha256 "ecabdc718b87f1c8772a86c5cdcbfb69c538891c842132ab2a88b92fb7ebe176" => :high_sierra
     sha256 "1c1de51b9c2445c05df372a9e0f5c7d8595b4160df0515b96d014925e3e85ac8" => :sierra
     sha256 "b2d1ba15ccdf0688decb8ecb0503ffa9fe507dccb5828de1007f130266ef98b1" => :el_capitan
