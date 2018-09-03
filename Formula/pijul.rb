2e2b4cd96c902dbe7c80d62f666210af3c92469a
commitay <commitay@users.noreply.github.com>
2018-09-03 14:55:38 +1000

    pijul: update openssl env var and comment

--- Formula/pijul.rb
+++ Formula/pijul.rb
@@ -17,8 +17,8 @@ class Pijul < Formula
   depends_on "openssl"
 
   def install
-    # Ensure that the `openssl-sys` crate picks up the intended library.
-    # (If we're not careful, LibreSSL or OpenSSL 1.1 gets used instead.)
+    # Ensure that the `openssl` crate picks up the intended library.
+    # https://crates.io/crates/openssl#manual-configuration
     ENV["OPENSSL_DIR"] = Formula["openssl"].opt_prefix
 
     cd "pijul" do
