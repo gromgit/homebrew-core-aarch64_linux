b5644416db4ed0250c4d0c6d3fff3f521c24abaa
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-09-03 16:43:57 +0100

    libpsl 0.20.2 (new formula)

---
+++ Formula/libpsl.rb
@@ -0,0 +1,40 @@
+class Libpsl < Formula
+  desc "C library for the Public Suffix List"
+  homepage "https://rockdaboot.github.io/libpsl"
+  url "https://github.com/rockdaboot/libpsl/releases/download/libpsl-0.20.2/libpsl-0.20.2.tar.gz"
+  sha256 "f8fd0aeb66252dfcc638f14d9be1e2362fdaf2ca86bde0444ff4d5cc961b560f"
+
+  depends_on "pkg-config" => :build
+  depends_on "libidn2"
+
+  def install
+    system "./configure", "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <libpsl.h>
+      #include <assert.h>
+
+      int main(void)
+      {
+          const char *domain = ".eu";
+          const char *cookie_domain = ".eu";
+          const psl_ctx_t *psl = psl_builtin();
+
+          assert(psl_is_public_suffix(psl, domain));
+
+          psl_free(psl);
+
+          return 0;
+      }
+    EOS
+    system ENV.cc, "-o", "test", "test.c", "-I#{include}",
+                   "-L#{lib}", "-lpsl"
+    system "./test"
+    system "#{bin}/psl", "--help"
+  end
+end
