784d04fa7855834b9936dd4100c80a0433a67608
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-03 21:37:22 +0000

    gdrive: update 2.1.0 bottle.

--- Formula/gdrive.rb
+++ Formula/gdrive.rb
@@ -8,6 +8,7 @@ class Gdrive < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "be226948f3fe5607a3411403a2ed641edcb7cce170d3ae02720c7dc58313e941" => :mojave
     sha256 "f0971622df162e07eadb14f3aa2947edb088c05791fc2931d6cc3ee93e6bd9a3" => :high_sierra
     sha256 "49e6aafb46c7267d013c4c45edaf24da14b184e53e841ebb825a87f545ce13b9" => :sierra
     sha256 "fd217d3f50e967972eccef6b4d0fd537b9f7c0c0b230fa83ff6ac9ee94a77ea5" => :el_capitan
