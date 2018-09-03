06141c265d042a0fe28e5cd41b403b68ee4d24ee
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-03 11:59:26 +0000

    jhipster: update 5.3.0 bottle.

--- Formula/jhipster.rb
+++ Formula/jhipster.rb
@@ -8,10 +8,10 @@ class Jhipster < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0aa519f7c582cc4f4a1bf240f6d475e8d1ce49b766083dc6cad0c7c31db326a0" => :mojave
-    sha256 "c9480b34d4f5df598113504fcae922b788d11f5d49251e851eac34351d701f2c" => :high_sierra
-    sha256 "86fbdce7ae298e4c21a68079bd85f278f151509414cc0455a352c58ce378a992" => :sierra
-    sha256 "c0040c782683a5bfec8bb9e076dc903477fd99fa17e8d62cbfbecbf773028613" => :el_capitan
+    sha256 "ce15f31f730f51c876ab8b4b4d190349872062bf4760bcf5538dad1a234c5a68" => :mojave
+    sha256 "8e7d57d9fcca0c5c9afdee0ca12c0d7656bef06612aba752e0a2f2173e092c75" => :high_sierra
+    sha256 "c01930f7a252d6e526cd33fcbc4dab0fdf1f03fc719af10ecf7739f64eb110b3" => :sierra
+    sha256 "2fe4c84b19bff56d08d94322fe43357a9dd9c229ee61bdac11d62ad6185ca673" => :el_capitan
   end
 
   depends_on "node"
