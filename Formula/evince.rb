f911657b0efc9586e306c2bb71e3617ee5b97cc3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-04 05:57:17 +0000

    evince: update 3.30.0 bottle.

--- Formula/evince.rb
+++ Formula/evince.rb
@@ -5,10 +5,10 @@ class Evince < Formula
   sha256 "cbd02d1c515fd7f17af1c96935e456d6ccba4d612d2d972a12079cc6b24e8cb9"
 
   bottle do
-    sha256 "c330bc3a2c64ffc3803a017f474a9c489d8f69d8f0f5224bbe097048c4327394" => :mojave
-    sha256 "466d27c9e13812ae6c8af2724f17b441e5b13276169ae210063684ca0a58fcfc" => :high_sierra
-    sha256 "ceaff8decdf09f5b4caa2456d4b5fba3d01f59d0434c764f5e8250383a1edbac" => :sierra
-    sha256 "e98ecffeb5353979ed7c433de951711cbb337efd1450491025feb7faa872a7d4" => :el_capitan
+    sha256 "535311ae119d82bd286af496be7f791a9bfc5f13e17c2d8ff2a67703ce161011" => :mojave
+    sha256 "56bd311b5b76ac27e0ec8928dc377a59c8691e5eb3a93dd921305afbedb479fd" => :high_sierra
+    sha256 "7244f3eab755a9a4b393b975a20866af26906cc9c995212834cf9547519472a8" => :sierra
+    sha256 "447f3f7d244a9d05a16e79b53e2e619aeb9c59701cf55875c25f7bc8966eb45d" => :el_capitan
   end
 
   depends_on "gobject-introspection" => :build
