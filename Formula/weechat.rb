95f69e0987efda7c8fec852c49e22833bac71c72
FX Coudert <fxcoudert@gmail.com>
2018-09-04 22:58:07 +0200

    weechat: remove some options

--- Formula/weechat.rb
+++ Formula/weechat.rb
@@ -15,21 +15,19 @@ class Weechat < Formula
   option "with-perl", "Build the perl module"
   option "with-ruby", "Build the ruby module"
   option "with-curl", "Build with brewed curl"
-  option "with-debug", "Build with debug information"
-  option "without-tcl", "Do not build the tcl module"
 
   depends_on "asciidoctor" => :build
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "gettext"
   depends_on "gnutls"
   depends_on "libgcrypt"
-  depends_on "gettext"
   depends_on "aspell" => :optional
+  depends_on "curl" => :optional
   depends_on "lua" => :optional
   depends_on "perl" => :optional
   depends_on "python@2" => :optional
   depends_on "ruby" => :optional if MacOS.version <= :sierra
-  depends_on "curl" => :optional
 
   def install
     args = std_cmake_args + %W[
@@ -38,10 +36,6 @@ class Weechat < Formula
       -DCA_FILE=#{etc}/openssl/cert.pem
       -DENABLE_JAVASCRIPT=OFF
     ]
-    if build.with? "debug"
-      args -= %w[-DCMAKE_BUILD_TYPE=Release]
-      args << "-DCMAKE_BUILD_TYPE=Debug"
-    end
 
     if build.without? "ruby"
       args << "-DENABLE_RUBY=OFF"
@@ -53,7 +47,6 @@ class Weechat < Formula
     args << "-DENABLE_LUA=OFF" if build.without? "lua"
     args << "-DENABLE_PERL=OFF" if build.without? "perl"
     args << "-DENABLE_ASPELL=OFF" if build.without? "aspell"
-    args << "-DENABLE_TCL=OFF" if build.without? "tcl"
     args << "-DENABLE_PYTHON=OFF" if build.without? "python@2"
 
     mkdir "build" do
