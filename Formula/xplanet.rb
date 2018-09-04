93ad2d466588fc6f2939d64de126b1e80183c4f1
FX Coudert <fxcoudert@gmail.com>
2018-09-04 22:58:07 +0200

    xplanet: remove some options

--- Formula/xplanet.rb
+++ Formula/xplanet.rb
@@ -12,31 +12,12 @@ class Xplanet < Formula
     sha256 "4c0e0c1b025079129f808e85d3d5e76280799929fa6e9d119000e94283769a8d" => :yosemite
   end
 
-  option "with-x11", "Build for X11 instead of Aqua"
-  option "with-all", "Build with default Xplanet configuration dependencies"
-  option "with-pango", "Build Xplanet to support Internationalized text library"
-  option "with-netpbm", "Build Xplanet with PNM graphic support"
-  option "with-cspice", "Build Xplanet with JPLs SPICE toolkit support"
-
   depends_on "pkg-config" => :build
-
-  depends_on "giflib" => :recommended
-  depends_on "jpeg" => :recommended
-  depends_on "libpng" => :recommended
-  depends_on "libtiff" => :recommended
-
-  if build.with?("all")
-    depends_on "netpbm"
-    depends_on "pango"
-    depends_on "cspice"
-  end
-
-  depends_on "netpbm" => :optional
-  depends_on "pango" => :optional
-  depends_on "cspice" => :optional
-
   depends_on "freetype"
-  depends_on :x11 => :optional
+  depends_on "giflib"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
 
   # patches bug in 1.3.1 with flag -num_times=2 (1.3.2 will contain fix, when released)
   # https://sourceforge.net/p/xplanet/code/208/tree/trunk/src/libdisplay/DisplayOutput.cpp?diff=5056482efd48f8457fc7910a:207
@@ -53,35 +34,20 @@ class Xplanet < Formula
   end
 
   def install
-    args = %W[
-      --disable-dependency-tracking
-      --prefix=#{prefix}
-      --without-cygwin
-    ]
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-aqua",
+                          "--without-cspice",
+                          "--without-cygwin",
+                          "--without-gif",
+                          "--without-jpeg",
+                          "--without-libpng",
+                          "--without-libtiff",
+                          "--without-pango",
+                          "--without-pnm",
+                          "--without-x",
+                          "--without-xscreensaver"
 
-    if build.without?("all")
-      args << "--without-gif" if build.without?("giflib")
-      args << "--without-jpeg" if build.without?("jpeg")
-      args << "--without-libpng" if build.without?("libpng")
-      args << "--without-libtiff" if build.without?("libtiff")
-      args << "--without-pnm" if build.without?("netpbm")
-      args << "--without-pango" if build.without?("pango")
-      args << "--without-cspice" if build.without?("cspice")
-    end
-
-    if build.with?("x11")
-      args << "--with-x" << "--with-xscreensaver" << "--without-aqua"
-    else
-      args << "--with-aqua" << "--without-x" << "--without-xscreensaver"
-    end
-
-    if build.with?("netpbm") || build.with?("all")
-      netpbm = Formula["netpbm"].opt_prefix
-      ENV.append "CPPFLAGS", "-I#{netpbm}/include/netpbm"
-      ENV.append "LDFLAGS", "-L#{netpbm}/lib"
-    end
-
-    system "./configure", *args
     system "make", "install"
   end
 
