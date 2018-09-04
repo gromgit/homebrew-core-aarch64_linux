2caab36437ad7670f120ac60999b6d1a9dfaa893
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-04 13:21:53 +0000

    gupnp-av: update 0.12.10_1 bottle.

--- Formula/gupnp-av.rb
+++ Formula/gupnp-av.rb
@@ -6,6 +6,7 @@ class GupnpAv < Formula
   revision 1
 
   bottle do
+    sha256 "ed327db58bf2e4ab852c749b2de61ed441f1c20bc80c0656f9ee9304850a4d6e" => :mojave
     sha256 "022dbc5e4b3f60c9e32d55315ae2191a49f574172f550b74ad18625ebf326467" => :high_sierra
     sha256 "f65e6ee6a9d7e68bd261ccc56501db503088e3e63c9b42277bb07e30ab8cda1f" => :sierra
     sha256 "d5adf2d8f3eeb96a9910b2c41e0cc732226d5beeb4ad8a3edf091036feb3399d" => :el_capitan
