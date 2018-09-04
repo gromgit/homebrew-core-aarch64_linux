2788c047dcdcfbe3d7b0a9285d777e420428c923
FX Coudert <fxcoudert@gmail.com>
2018-09-04 16:41:33 +0200

    jerm: add mirror

--- Formula/jerm.rb
+++ Formula/jerm.rb
@@ -2,6 +2,7 @@ class Jerm < Formula
   desc "Communication terminal through serial and TCP/IP interfaces"
   homepage "http://www.bsddiary.net/jerm/"
   url "http://www.bsddiary.net/jerm/jerm-8096.tar.gz"
+  mirror "https://dotsrc.dl.osdn.net/osdn/fablib/62057/jerm-8096.tar.gz"
   version "0.8096"
   sha256 "8a63e34a2c6a95a67110a7a39db401f7af75c5c142d86d3ba300a7b19cbcf0e9"
 
