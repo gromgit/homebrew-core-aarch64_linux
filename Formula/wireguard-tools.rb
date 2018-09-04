35a2f262422ee941d46934dbfb8997159e4da225
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-04 20:05:03 +0000

    wireguard-tools: update 0.0.20180904 bottle.

--- Formula/wireguard-tools.rb
+++ Formula/wireguard-tools.rb
@@ -9,10 +9,10 @@ class WireguardTools < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d27dc906309373106e2d35fe0e6524f0f91fa9734013635a1c76b450d2bbd8b3" => :mojave
-    sha256 "ba73ad87b744cc4283ee2aa409336b1dda2cac1922a79578d1144d077236a8f3" => :high_sierra
-    sha256 "52f535902df6a3f7c926e5fe7a7614c75a1a97777fbcbaa9ec8b68f23bf5647f" => :sierra
-    sha256 "631fa59397ceddfa898bdf18b76107f38908fe3aa6fd808c2d560e25269bc6b9" => :el_capitan
+    sha256 "49cd583a448b205559cab5cfbd6bdf95607d20bb387f12a6f167257987593172" => :mojave
+    sha256 "e240f7a5d8413f345cde924acbfddb7b12add2397c45933be00fdc6df070a8c3" => :high_sierra
+    sha256 "82e7b390f5e93eecf6ffe073f815447a896796ff10bcf44096b5a5bcdd992799" => :sierra
+    sha256 "bf7118378e59bfe73574cd824c137907c47973f995b60b71388ff55e4fc03e69" => :el_capitan
   end
 
   depends_on "bash"
