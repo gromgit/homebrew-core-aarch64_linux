b631ec9f8e3d46428b75ff30d739454db342cdcb
FX Coudert <fxcoudert@gmail.com>
2018-09-04 22:58:06 +0200

    dar: remove some options

--- Formula/dar.rb
+++ Formula/dar.rb
@@ -11,39 +11,23 @@ class Dar < Formula
     sha256 "0f3d68d33877c3d98b9d8fbacdbd52c54569ccc1a46130ecec244415c58c5c73" => :el_capitan
   end
 
-  option "with-doxygen", "build libdar API documentation and html man page"
-  option "with-libgcrypt", "enable strong encryption support"
-  option "with-lzo", "enable lzo compression support"
-  option "with-upx", "make executables compressed at installation time"
-
-  deprecated_option "with-docs" => "with-doxygen"
-
   depends_on :macos => :el_capitan # needs thread-local storage
-  depends_on "doxygen" => [:build, :optional]
-  depends_on "upx" => [:build, :optional]
-  depends_on "libgcrypt" => :optional
-  depends_on "lzo" => :optional
-  depends_on "xz" => :optional
 
   needs :cxx11
 
   def install
     ENV.cxx11
 
-    args = %W[
-      --enable-mode=64
-      --disable-debug
-      --disable-dependency-tracking
-      --disable-dar-static
-      --prefix=#{prefix}
-    ]
-    args << "--disable-build-html" if build.without? "doxygen"
-    args << "--disable-upx" if build.without? "upx"
-    args << "--disable-libgcrypt-linking" if build.without? "libgcrypt"
-    args << "--disable-liblzo2-linking" if build.without? "lzo"
-    args << "--disable-libxz-linking" if build.without? "xz"
-
-    system "./configure", *args
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-build-html",
+                          "--disable-dar-static",
+                          "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-libgcrypt-linking",
+                          "--disable-liblzo2-linking",
+                          "--disable-libxz-linking",
+                          "--disable-upx",
+                          "--enable-mode=64"
     system "make", "install"
   end
 
