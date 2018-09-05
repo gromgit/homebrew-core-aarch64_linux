f6e820d7dac23c14aed1be2067986c081f41b83a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-05 11:12:19 +0000

    prototool: add 1.2.0 bottle.

--- Formula/prototool.rb
+++ Formula/prototool.rb
@@ -4,6 +4,14 @@ class Prototool < Formula
   url "https://github.com/uber/prototool/archive/v1.2.0.tar.gz"
   sha256 "16654242f1f22eaeb2df0c33366ef7a22fda674b51bd4c8da38e0ffab62ce236"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9267ebafbf8b194ba5800e6211f61432abd0a25f04fe245a504141d14170850e" => :mojave
+    sha256 "2ea17c28849ebb1212f3a2c4d63ca9adffa1147781f7f43f9aaefa18124ff431" => :high_sierra
+    sha256 "aa4f8820b92a22358fff4b0f35a10fdd5b2a56a7dad9def55fc51b84d847c509" => :sierra
+    sha256 "38ca6e71116f21fba39601114cdba08c24b7a8455246c748847e9abbf28ef6b3" => :el_capitan
+  end
+
   depends_on "glide" => :build
   depends_on "go" => :build
 
