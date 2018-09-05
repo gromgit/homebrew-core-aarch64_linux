32c92ea2a25b0e6e7108fceb30eec4af15ad1ae0
FX Coudert <fxcoudert@gmail.com>
2018-09-05 23:55:20 +0200

    qscintilla2: remove some options

--- Formula/qscintilla2.rb
+++ Formula/qscintilla2.rb
@@ -13,15 +13,11 @@ class Qscintilla2 < Formula
     sha256 "24f06d0a2019efd91ef51954e850ddcb1b6d2581071a08b0e9763e3b3e483fa3" => :el_capitan
   end
 
-  option "with-plugin", "Build the Qt Designer plugin"
-  option "without-python", "Do not build Python3 bindings"
-  option "without-python@2", "Do not build Python2 bindings"
-
   depends_on "pyqt"
+  depends_on "python"
+  depends_on "python@2"
   depends_on "qt"
   depends_on "sip"
-  depends_on "python" => :recommended
-  depends_on "python@2" => :recommended
 
   def install
     spec = (ENV.compiler == :clang && MacOS.version >= :mavericks) ? "macx-clang" : "macx-g++"
@@ -49,38 +45,23 @@ class Qscintilla2 < Formula
     # Add qscintilla2 features search path, since it is not installed in Qt keg's mkspecs/features/
     ENV["QMAKEFEATURES"] = prefix/"data/mkspecs/features"
 
-    if build.with?("python") || build.with?("python@2")
-      cd "Python" do
-        Language::Python.each_python(build) do |python, version|
-          (share/"sip").mkpath
-          system python, "configure.py", "-o", lib, "-n", include,
-                           "--apidir=#{prefix}/qsci",
-                           "--destdir=#{lib}/python#{version}/site-packages/PyQt5",
-                           "--stubsdir=#{lib}/python#{version}/site-packages/PyQt5",
-                           "--qsci-sipdir=#{share}/sip",
-                           "--qsci-incdir=#{include}",
-                           "--qsci-libdir=#{lib}",
-                           "--pyqt=PyQt5",
-                           "--pyqt-sipdir=#{Formula["pyqt"].opt_share}/sip/Qt5",
-                           "--sip-incdir=#{Formula["sip"].opt_include}",
-                           "--spec=#{spec}"
-          system "make"
-          system "make", "install"
-          system "make", "clean"
-        end
-      end
-    end
-
-    if build.with? "plugin"
-      mkpath prefix/"plugins/designer"
-      cd "designer-Qt4Qt5" do
-        inreplace "designer.pro" do |s|
-          s.sub! "$$[QT_INSTALL_PLUGINS]", "#{lib}/qt/plugins"
-          s.sub! "$$[QT_INSTALL_LIBS]", lib
-        end
-        system "qmake", "designer.pro", *args
+    cd "Python" do
+      Language::Python.each_python(build) do |python, version|
+        (share/"sip").mkpath
+        system python, "configure.py", "-o", lib, "-n", include,
+                       "--apidir=#{prefix}/qsci",
+                       "--destdir=#{lib}/python#{version}/site-packages/PyQt5",
+                       "--stubsdir=#{lib}/python#{version}/site-packages/PyQt5",
+                       "--qsci-sipdir=#{share}/sip",
+                       "--qsci-incdir=#{include}",
+                       "--qsci-libdir=#{lib}",
+                       "--pyqt=PyQt5",
+                       "--pyqt-sipdir=#{Formula["pyqt"].opt_share}/sip/Qt5",
+                       "--sip-incdir=#{Formula["sip"].opt_include}",
+                       "--spec=#{spec}"
         system "make"
         system "make", "install"
+        system "make", "clean"
       end
     end
   end
