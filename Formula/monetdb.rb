b02dfc4eb77d586f2accba96be5700524ebe1929
FX Coudert <fxcoudert@gmail.com>
2018-09-05 14:21:16 +0200

    monetdb: remove some options

--- Formula/monetdb.rb
+++ Formula/monetdb.rb
@@ -1,18 +1,3 @@
-class RRequirement < Requirement
-  fatal true
-
-  satisfy { which("r") }
-
-  def message; <<~EOS
-    R not found. The R integration module requires R.
-    Do one of the following:
-    - install R
-    -- run brew install r or brew cask install r-app
-    - remove the --with-r option
-  EOS
-  end
-end
-
 class Monetdb < Formula
   desc "Column-store database"
   homepage "https://www.monetdb.org/"
@@ -35,34 +20,23 @@ class Monetdb < Formula
     depends_on "autoconf" => :build
   end
 
-  option "with-r", "Build the R integration module"
-
-  depends_on RRequirement => :optional
-
   depends_on "pkg-config" => :build
-  depends_on "libatomic_ops" => [:build, :recommended]
+  depends_on "libatomic_ops" => :build
   depends_on "pcre"
-  depends_on "readline" # Compilation fails with libedit.
+  depends_on "readline" # Compilation fails with libedit
   depends_on "openssl"
-
-  depends_on "unixodbc" => :optional # Build the ODBC driver
-  depends_on "geos" => :optional # Build the GEOM module
-  depends_on "cfitsio" => :optional
 
   def install
     ENV["M4DIRS"] = "#{Formula["gettext"].opt_share}/aclocal" if build.head?
     system "./bootstrap" if build.head?
 
-    args = ["--prefix=#{prefix}",
-            "--enable-debug=no",
-            "--enable-assert=no",
-            "--enable-optimize=yes",
-            "--enable-testing=no",
-            "--with-readline=#{Formula["readline"].opt_prefix}"]
-
-    args << "--disable-rintegration" if build.without? "r"
-
-    system "./configure", *args
+    system "./configure", "--prefix=#{prefix}",
+                          "--enable-assert=no",
+                          "--enable-debug=no",
+                          "--enable-optimize=yes",
+                          "--enable-testing=no",
+                          "--with-readline=#{Formula["readline"].opt_prefix}",
+                          "--disable-rintegration"
     system "make", "install"
   end
 end
