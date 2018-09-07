c3adc9fe92b2db7326063d6de2e782115e43aa60
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-07 13:24:39 +0000

    ucloud: add 0.1.2 bottle.

--- Formula/ucloud.rb
+++ Formula/ucloud.rb
@@ -4,6 +4,14 @@ class Ucloud < Formula
   url "https://ucloud-sdk.dl.ufileos.com/ucloud-cli-0.1.2.tar.gz"
   sha256 "f41aaa6bf8063b3ccd0c5e41d3dc90f46f9fb45066513da8e306456f1f332071"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8c06cd38566d23fc3cbe34fe5a26b6212fc6f1cc2dfe73552ba90229dc07294e" => :mojave
+    sha256 "d65675203d124f39d9d45d880604e63bff3234bf8e98373e176588f7ab640ca7" => :high_sierra
+    sha256 "6a60ad6f58e6aabce9b23924f6e5863702f090d3ca8a940b4acdfd368958b799" => :sierra
+    sha256 "0818bc13c03b59ba6189deda71d3cdfefae07670cd2d978c3a30f79b3b793fb0" => :el_capitan
+  end
+
   depends_on "go" => :build
 
   def install
