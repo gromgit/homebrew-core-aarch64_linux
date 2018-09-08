59af8d29c7e4e4db9b2a0800e2324eb79230b38f
Chongyu Zhu <i@lembacon.com>
2018-09-08 19:35:59 +0800

    gnu-indent 2.2.12
    Closes #31908.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/gnu-indent.rb
+++ Formula/gnu-indent.rb
@@ -1,9 +1,9 @@
 class GnuIndent < Formula
   desc "C code prettifier"
   homepage "https://www.gnu.org/software/indent/"
-  url "https://ftp.gnu.org/gnu/indent/indent-2.2.10.tar.gz"
-  mirror "https://ftpmirror.gnu.org/indent/indent-2.2.10.tar.gz"
-  sha256 "8a9b41be5bfcab5d8c1be74204b10ae78789fc3deabea0775fdced8677292639"
+  url "https://ftp.gnu.org/gnu/indent/indent-2.2.12.tar.gz"
+  mirror "https://ftpmirror.gnu.org/indent/indent-2.2.12.tar.gz"
+  sha256 "e77d68c0211515459b8812118d606812e300097cfac0b4e9fb3472664263bb8b"
 
   bottle do
     sha256 "df1f57f1896c8b5d88d9f2e6e072dd9fc3800e2057b78879a49b67118173cad6" => :mojave
@@ -19,9 +19,6 @@ class GnuIndent < Formula
   depends_on "gettext"
 
   deprecated_option "default-names" => "with-default-names"
-
-  # Fix broken include and missing build dependency
-  patch :DATA
 
   def install
     args = %W[
@@ -49,29 +46,3 @@ class GnuIndent < Formula
     EOS
   end
 end
-
-__END__
-diff --git a/man/Makefile.in b/man/Makefile.in
-index 76839bc..8a5fc6e 100644
---- a/man/Makefile.in
-+++ b/man/Makefile.in
-@@ -507,7 +507,7 @@ uninstall-man: uninstall-man1
- 	uninstall-man uninstall-man1
- 
- 
--@PACKAGE@.1: ${srcdir}/@PACKAGE@.1.in  ${srcdir}/../doc/@PACKAGE@.texinfo texinfo2man.c  Makefile.am
-+@PACKAGE@.1: ${srcdir}/@PACKAGE@.1.in  ${srcdir}/../doc/@PACKAGE@.texinfo texinfo2man.c  Makefile.am texinfo2man
- 	./texinfo2man ${srcdir}/@PACKAGE@.1.in ${srcdir}/../doc/@PACKAGE@.texinfo > $@
- # Tell versions [3.59,3.63) of GNU make to not export all variables.
- # Otherwise a system limit (for SysV at least) may be exceeded.
-diff --git a/man/texinfo2man.c b/man/texinfo2man.c
-index e7d82e1..c95266f 100644
---- a/man/texinfo2man.c
-+++ b/man/texinfo2man.c
-@@ -1,6 +1,5 @@
- #include <stdio.h>
- #include <stdlib.h>
--#include <malloc.h>
- #include <string.h>
- #include <ctype.h>
- 
