42be2179d9c355687ebaf69f46d7c880a3880f9b
commitay <commitay@users.noreply.github.com>
2018-09-08 20:32:31 +1000

    cquery: fix tag/revision indentation

--- Formula/cquery.rb
+++ Formula/cquery.rb
@@ -2,8 +2,9 @@ class Cquery < Formula
   desc "C/C++ language server"
   homepage "https://github.com/cquery-project/cquery"
   # pull from git tag to get submodules
-  url "https://github.com/cquery-project/cquery.git", :tag => "v20180718",
-                                                      :revision => "b523aa928acf8ffb3de6b22c79db7366a9672489"
+  url "https://github.com/cquery-project/cquery.git",
+      :tag => "v20180718",
+      :revision => "b523aa928acf8ffb3de6b22c79db7366a9672489"
   head "https://github.com/cquery-project/cquery.git"
 
   bottle do
