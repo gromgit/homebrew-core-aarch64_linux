82e8c763cea324431c1125dffd8973249c188e76
commitay <commitay@users.noreply.github.com>
2018-09-08 20:32:34 +1000

    torsocks: fix tag/revision indentation

--- Formula/torsocks.rb
+++ Formula/torsocks.rb
@@ -2,8 +2,8 @@ class Torsocks < Formula
   desc "Use SOCKS-friendly applications with Tor"
   homepage "https://gitweb.torproject.org/torsocks.git/"
   url "https://git.torproject.org/torsocks.git",
-    :tag => "v2.2.0",
-    :revision => "e54d80bc9595beeceac637b03e5c5395c07e62f7"
+      :tag => "v2.2.0",
+      :revision => "e54d80bc9595beeceac637b03e5c5395c07e62f7"
   head "https://git.torproject.org/torsocks.git"
 
   bottle do
