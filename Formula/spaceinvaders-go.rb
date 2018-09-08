df4cf2397785831f1660aab46c1a52ef098bc40d
commitay <commitay@users.noreply.github.com>
2018-09-08 20:32:33 +1000

    spaceinvaders-go: fix revision indentation

--- Formula/spaceinvaders-go.rb
+++ Formula/spaceinvaders-go.rb
@@ -19,17 +19,17 @@ class SpaceinvadersGo < Formula
 
   go_resource "github.com/mattn/go-runewidth" do
     url "https://github.com/mattn/go-runewidth.git",
-      :revision => "12e0ff74603c9a3209d8bf84f8ab349fe1ad9477"
+        :revision => "12e0ff74603c9a3209d8bf84f8ab349fe1ad9477"
   end
 
   go_resource "github.com/nsf/termbox-go" do
     url "https://github.com/nsf/termbox-go.git",
-      :revision => "347ab0bc907040257edaf8b580f729e12c93ab6b"
+        :revision => "347ab0bc907040257edaf8b580f729e12c93ab6b"
   end
 
   go_resource "github.com/simulatedsimian/joystick" do
     url "https://github.com/simulatedsimian/joystick.git",
-      :revision => "6aa8abe045a796cf36b720d0484809e3f70dc5bd"
+        :revision => "6aa8abe045a796cf36b720d0484809e3f70dc5bd"
   end
 
   def install
