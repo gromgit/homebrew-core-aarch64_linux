84e5e32d712ab02b1a36cb75577ad5bba0bad78d
commitay <commitay@users.noreply.github.com>
2018-09-08 20:32:33 +1000

    neatvi: fix tag/revision indentation

--- Formula/neatvi.rb
+++ Formula/neatvi.rb
@@ -2,7 +2,8 @@ class Neatvi < Formula
   desc "ex/vi clone for editing bidirectional uft-8 text"
   homepage "http://repo.or.cz/w/neatvi.git"
   url "http://repo.or.cz/neatvi.git",
-    :tag => "06", :revision => "5ed4bbc7f12686bb480ab8b2b05c94e12b1c71d8"
+      :tag => "06",
+      :revision => "5ed4bbc7f12686bb480ab8b2b05c94e12b1c71d8"
   head "http://repo.or.cz/neatvi.git"
 
   bottle do
