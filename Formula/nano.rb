85f1c1d0dbab6c72151e055c484d1f4b56c3262f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-09 21:06:02 +0000

    nano: update 3.0 bottle.

--- Formula/nano.rb
+++ Formula/nano.rb
@@ -5,10 +5,10 @@ class Nano < Formula
   sha256 "3f5f7713665155d9089ffde814ca89b3795104892af2ce8db2c1a70b82fa84a3"
 
   bottle do
-    sha256 "8a56c675b6f0fd55e5e4e2944759bb4811b894074cf1c3089cd92b7f4f14462a" => :mojave
-    sha256 "1b7f5f7842d849709b209d48346bc2dc502b538eeee460ef63513775d82f70ec" => :high_sierra
-    sha256 "0f14e7427708f6105143a6c03f962bf047f7162adfc1a9a7f67749d13a42614c" => :sierra
-    sha256 "f2ec710b129cb095c1107a53c632c5009f8ecddb53db4b4b95eb08edca0037ae" => :el_capitan
+    sha256 "5276fda4dfb3454c34c5efe63d33b1817d1e8097a39a3c9fe2e0c909bf1c7b4a" => :mojave
+    sha256 "18594f4be25300515830a5112cd5ea112fad9004c6651a5ace902603143a2ada" => :high_sierra
+    sha256 "a5607cb79cc8b599b3a2bd368af2d037c17b62b03319152ef3f810a7aa40bc49" => :sierra
+    sha256 "1ae56cb5aa66f4ec3d4a29c3dd5f1e0b1c9650a36278422ab497d224b4972054" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
