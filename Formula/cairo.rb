074b624bdaa8c92971eeace4dcf42f49628fb0d6
Linus Unnebäck <linus@folkdatorn.se>
2018-09-10 09:22:28 +0200

    cairo 1.15.12 (devel) (#31912)

--- Formula/cairo.rb
+++ Formula/cairo.rb
@@ -12,6 +12,11 @@ class Cairo < Formula
     sha256 "bec85433a35605164bdbf5f8913e29eb6d9ceb5acc5569dd9d864706ae6c8d49" => :el_capitan
   end
 
+  devel do
+    url "https://cairographics.org/snapshots/cairo-1.15.12.tar.xz"
+    sha256 "7623081b94548a47ee6839a7312af34e9322997806948b6eec421a8c6d0594c9"
+  end
+
   head do
     url "https://anongit.freedesktop.org/git/cairo", :using => :git
     depends_on "automake" => :build
