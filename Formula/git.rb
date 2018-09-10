7326b4fb904fdaf1cd430dbaaa5cdb74ed5c4b73
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-10 20:09:23 +0000

    git: update 2.19.0 bottle.

--- Formula/git.rb
+++ Formula/git.rb
@@ -6,10 +6,10 @@ class Git < Formula
   head "https://github.com/git/git.git", :shallow => false
 
   bottle do
-    sha256 "e79618cad4a991104ca426ae84858880f37b1a5b528ad00975ba069b76b40834" => :mojave
-    sha256 "1fdceaff995cc18ddff3e56cdfc30bb363f69aecde01925985f3951ad21ae340" => :high_sierra
-    sha256 "05653ebe3f1d154be4b86ea73728a36d45c021cda81c9508534c433601ea3509" => :sierra
-    sha256 "cb0c4eac4a49fbd1936bae4070ecac3c3543aecfb2ba09869f6f6fd2aeb11015" => :el_capitan
+    sha256 "1c90a0ac7d9f027bd24cc54043b996dd0d3d11462c83d8b8c1660299a53cf23d" => :mojave
+    sha256 "b10bcf50b981b0d5b0407fc0fc5d87054b0dbeab401f6eaedfa7d8f4b278a603" => :high_sierra
+    sha256 "60f02d9b13bed6e99197d66e6cbb39af1f037ebbce3c1594959012ad93aa9e4b" => :sierra
+    sha256 "bec8956a4e80bd8eeff0abc640b776afe637bfeb3b6d323f5a95d00074565df4" => :el_capitan
   end
 
   deprecated_option "with-pcre" => "with-pcre2"
