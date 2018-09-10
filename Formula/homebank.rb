b5e0cb07974051c39f1a5294ccae5732f464a40b
aas <alexander.ashurkov@gmail.com>
2018-09-10 21:45:49 +0500

    homebank 5.2
    Closes #31954.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/homebank.rb
+++ Formula/homebank.rb
@@ -1,9 +1,8 @@
 class Homebank < Formula
   desc "Manage your personal accounts at home"
   homepage "http://homebank.free.fr"
-  url "http://homebank.free.fr/public/homebank-5.1.8.tar.gz"
-  sha256 "1083fcbc609cbc981a42c63d84a09cc7dc3dd40f57d29e08b720be2b3eaff23b"
-  revision 1
+  url "http://homebank.free.fr/public/homebank-5.2.tar.gz"
+  sha256 "8e5421e0f3e782bb403f26d713791671e0e51d887157169d915637e54e720908"
 
   bottle do
     sha256 "95c5ffa07316efe39f9d4a786bc665520dd93230dd7e2f5ad3fe126da5949b1f" => :mojave
