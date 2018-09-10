47633a76f80e90c7f069e66402d89b74375a3c22
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-10 13:21:13 +0000

    bwfmetaedit: update 1.3.6 bottle.

--- Formula/bwfmetaedit.rb
+++ Formula/bwfmetaedit.rb
@@ -7,10 +7,10 @@ class Bwfmetaedit < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "59c1e6289726ea39e173afd6418d26e9b46feeee03d01b6caaf5b6ffa0e69711" => :mojave
-    sha256 "8d4491da134ad7946b9f4dafb93762e8cc3ef5ae7472de25ddcdda49bc3a1264" => :high_sierra
-    sha256 "c3ac3fded8f13d7bf277a4c76b586d1fc6fc6c90b14842f36c586b36a3241b97" => :sierra
-    sha256 "e0162b43a0806a785da4f4838ed5fcf0d60bd809b3fcc915c58589c9e6938798" => :el_capitan
+    sha256 "95e67d6b1350f285fe7b6f08a5c6608fac81fbcc4d7f764a6dcaa04c3e01e956" => :mojave
+    sha256 "595ee66280c74d459e1db88d1b8afbace3a9ef8947e47a60f9f412eeceb29265" => :high_sierra
+    sha256 "2c76ff728a6ad6f372d5a55df1c22af9c5f52f90694a1bd6775d1fc466ecbead" => :sierra
+    sha256 "f6428c53fdf2b807b63584db27285984e479c1023d6cb8bf441bad9ac09766b3" => :el_capitan
   end
 
   def install
