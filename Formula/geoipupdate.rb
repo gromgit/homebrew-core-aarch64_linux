733a9d03c6091c29bd3da01ef4f63ad6d62c867b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-10 19:00:34 +0000

    geoipupdate: update 3.1.1 bottle.

--- Formula/geoipupdate.rb
+++ Formula/geoipupdate.rb
@@ -5,10 +5,10 @@ class Geoipupdate < Formula
   sha256 "3de22e3fe3282024288a00807bbea9a1ffa2d1e8fe9c611f4b14a5b4d8ebe08a"
 
   bottle do
-    sha256 "a7a81c2afd7db293331659d1fbdff9105536aed24eaffa8eac929e504aa2e884" => :mojave
-    sha256 "21e6eb895c21437b2e4e47f353fc6974dea63e2494e1daedc89f8a86dfca344f" => :high_sierra
-    sha256 "7c4bb242c2967ebb8aae34180fffef614bb24827a08a4bc4911fe0d848281698" => :sierra
-    sha256 "e9e1a7d8ac8129b9ef393f140599b325028ea945d5b3775b9908357892903018" => :el_capitan
+    sha256 "9cf68c91a00193bed2bd55b3c2d8add1fa937433a429af7c01829b14a58eb7fa" => :mojave
+    sha256 "1e45c489ab5d3022b71e00adca105287de4175ae999552a1f0f62981a16cbf4c" => :high_sierra
+    sha256 "d13fd47dd374265af81e2824335b714bd647359671f82ed6643ae988722647fc" => :sierra
+    sha256 "a8fdd107eca63868f7ae9e216f3ecab4bb1c0ce635d9bda0fdb03d35f37d1380" => :el_capitan
   end
 
   head do
