21f75ba3f6792763fdb6390c2e75fdfd5b60dd43
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-11 01:08:09 +0000

    gutenberg: update 0.4.2 bottle.

--- Formula/gutenberg.rb
+++ Formula/gutenberg.rb
@@ -6,10 +6,10 @@ class Gutenberg < Formula
   head "https://github.com/Keats/gutenberg.git"
 
   bottle do
-    sha256 "8be1ae1ef65b68d173283815af6d5a018d610cfd1ac6d225b493760d81d65a64" => :mojave
-    sha256 "0b03919653820481e36c1749954f2bd555c0c511ca579e3f45078f811eb185ab" => :high_sierra
-    sha256 "d63a11f8c299a9acc7231e9ac80855033a325a0b09b839c0064cab1f2b9d920b" => :sierra
-    sha256 "4f0c0dbfe7ab3ee735dfc4a1327e13a81688a57934f5ddd3939880ac9bfb6f35" => :el_capitan
+    sha256 "3c13efb0ed3263a70a81036d55575be92e18c4f4b1e0652b906fa32ccfef48e7" => :mojave
+    sha256 "3cb0122fc3082758478d8fea2c9933bc20882b7e33ea67b0e62019d7eb447c76" => :high_sierra
+    sha256 "f0a8fa2af6664964168e8a2325cfb69c01af5009d2f1d1c65fd857772969d738" => :sierra
+    sha256 "5489f83ea724f922d7ebda5bf6c7664c0fe46ef0e3e5e59b854826b2870839d7" => :el_capitan
   end
 
   depends_on "cmake" => :build
