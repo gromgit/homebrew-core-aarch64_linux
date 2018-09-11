ed6ccf53e3b4809addd527ab88667dc007cb931e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-11 01:25:10 +0000

    hlint: update 2.1.10 bottle.

--- Formula/hlint.rb
+++ Formula/hlint.rb
@@ -10,10 +10,10 @@ class Hlint < Formula
   head "https://github.com/ndmitchell/hlint.git"
 
   bottle do
-    sha256 "46d326207501872cab48d7f88828abe89dcfd0cf7a61313a1fe4f97db49c4e84" => :mojave
-    sha256 "a48d46cdd3aaee9ed2ae2d0decc4f9a4ddfe3526095917eff61c010b7a1b545f" => :high_sierra
-    sha256 "ccf6f37b1d44cc063bdc08b03cceaa6589c39266e54d406096661ddc48d8a368" => :sierra
-    sha256 "2610f48e027a3a47e510920b5f55f1791009ff4e164b4a021aafd9113cefb3c4" => :el_capitan
+    sha256 "a801749db3aa79d4fe4eb2af80428d917c2c5100c99baece8d680b648e159b56" => :mojave
+    sha256 "8460e3872f551304867eabc8bab6def0eab159b85a56fab7e83088ebe1a8840e" => :high_sierra
+    sha256 "600c695052a301710778baf8131fb1dd1e138fd6b12ae6660610981b94c1d4dc" => :sierra
+    sha256 "1d0650d58fb30e7f00a05d059e2948b17af09eea1162e17fbdb905b4d15ee752" => :el_capitan
   end
 
   depends_on "cabal-install" => :build
