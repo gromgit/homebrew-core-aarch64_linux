635968423422f75771ff35e708a0360b0aee75db
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-12 16:11:42 +0000

    wumpus: update 1.6 bottle.

--- Formula/wumpus.rb
+++ Formula/wumpus.rb
@@ -6,6 +6,7 @@ class Wumpus < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e8e59a1fc042fdcaf087f8330b3c7083707892d8bed1f9317f5ab20777461576" => :mojave
     sha256 "5a1435e4545eea1ab436a0fba4844ac2236ea6e49e045f3b59d19adcff5de97c" => :high_sierra
     sha256 "fa693e01a17d31e9e5a5b51f27ea18b69e30f5613615c95ddb89c7dbc4bbe75d" => :sierra
     sha256 "3b6751d8a871e44c033e4a6d83009bb8b499952385d8c1743be3d26bfe37b312" => :el_capitan
