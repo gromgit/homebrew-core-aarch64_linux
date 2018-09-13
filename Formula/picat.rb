45cd344a374121212a798d3ecd63bc0c5228e388
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-13 22:04:04 +0000

    picat: add 2.4.8 bottle.

--- Formula/picat.rb
+++ Formula/picat.rb
@@ -5,6 +5,14 @@ class Picat < Formula
   version "2.4.8"
   sha256 "72b452a8ba94d6187d837dcdb46aab0d7dc724651bac99a8cf2ada5c0a3543dd"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2a1211162f9dc2e87211ac452d8451a3f7e75dbc7b4312463ed0caa82ccaa55c" => :mojave
+    sha256 "acbaf9e2dd1fb8b707213271a41eac2a33e2a0247f8ee3187a81f72fafdfc807" => :high_sierra
+    sha256 "15adabe5e7cb8c19cea32893da7f56d192503307458b6c8ed145e7cf6da8fa9d" => :sierra
+    sha256 "95985a5a22784c13dba594b1b3e6433c7705def3712cd97a7da99c4ce30bfd20" => :el_capitan
+  end
+
   def install
     # Hardcode in Makefile issue is reported to upstream in the official Google Groups
     # https://groups.google.com/d/msg/picat-lang/0kZYUJKgnkY/3Vig5X1NCAAJ
