05ba2c3ddf59a2ec8a5eee6f8edf90da8f1f818e
Michael Novak <michaelnovak@computer.org>
2018-09-13 13:01:57 -0400

    git-credential-manager 2.0.4 (#32047)

--- Formula/git-credential-manager.rb
+++ Formula/git-credential-manager.rb
@@ -1,8 +1,8 @@
 class GitCredentialManager < Formula
   desc "Stores Git credentials for Visual Studio Team Services"
   homepage "https://docs.microsoft.com/vsts/git/set-up-credential-managers"
-  url "https://github.com/Microsoft/Git-Credential-Manager-for-Mac-and-Linux/releases/download/git-credential-manager-2.0.3/git-credential-manager-2.0.3.jar"
-  sha256 "ee7a11486dffbea366c79500395d9f1384fe4dd3f91b49e09e6bc2ed8ab5f65a"
+  url "https://github.com/Microsoft/Git-Credential-Manager-for-Mac-and-Linux/releases/download/git-credential-manager-2.0.4/git-credential-manager-2.0.4.jar"
+  sha256 "fb8536aac9b00cdf6bdeb0dd152bb1306d88cd3fdb7a958ac9a144bf4017cad7"
 
   bottle :unneeded
 
