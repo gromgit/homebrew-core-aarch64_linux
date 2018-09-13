fbb3ae10139972ccf98af90bfcc36df8b4ca394e
FX Coudert <fxcoudert@gmail.com>
2018-09-13 14:40:27 +0200

    libiscsi: remove options

--- Formula/libiscsi.rb
+++ Formula/libiscsi.rb
@@ -3,6 +3,7 @@ class Libiscsi < Formula
   homepage "https://github.com/sahlberg/libiscsi"
   url "https://sites.google.com/site/libiscsitarballs/libiscsitarballs/libiscsi-1.18.0.tar.gz"
   sha256 "367ad1514d1640e4e72ca6754275ec226650a128ca108f61a86d766c94d63d23"
+  revision 1
   head "https://github.com/sahlberg/libiscsi.git"
 
   bottle do
@@ -14,23 +15,12 @@ class Libiscsi < Formula
     sha256 "c89e40197b9aebd712c67d43fbe7a4e085ddb7a1b58861c6e98444271fd9e383" => :yosemite
   end
 
-  option "with-noinst", "Install the noinst binaries (examples, tests)"
-  option "with-cunit", "Install iscsi-test-cu, the iSCSI target test suite"
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on "cunit" => :optional
+  depends_on "cunit"
 
   def install
-    if build.without? "cunit"
-      inreplace "test-tool/Makefile.am", "bin_PROGRAMS =", "noinst_PROGRAMS ="
-    end
-    if build.with? "noinst"
-      # Install the noinst binaries
-      inreplace ["tests/Makefile.am", "examples/Makefile.am"], "noinst_PROGRAMS =", "bin_PROGRAMS ="
-    end
-
     system "./autogen.sh"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
@@ -39,7 +29,6 @@ class Libiscsi < Formula
 
   test do
     system bin/"iscsi-ls", "--usage"
-    system bin/"iscsi-test-cu", "--list" if build.with? "cunit"
-    system bin/"prog_noop_reply", "--usage" if build.with? "noinst"
+    system bin/"iscsi-test-cu", "--list"
   end
 end
