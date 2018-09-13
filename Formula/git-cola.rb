8b636d266676964b7abe4be918292758f5796b6f
FX Coudert <fxcoudert@gmail.com>
2018-09-13 12:07:24 +0200

    git-cola: remove options

--- Formula/git-cola.rb
+++ Formula/git-cola.rb
@@ -3,6 +3,7 @@ class GitCola < Formula
   homepage "https://git-cola.github.io/"
   url "https://github.com/git-cola/git-cola/archive/v3.2.tar.gz"
   sha256 "4005e714db78b073c1ef8bde55485452dc7a31e3b8cc0b4d60d6112ffb5ea228"
+  revision 1
   head "https://github.com/git-cola/git-cola.git"
 
   bottle do
@@ -13,20 +14,15 @@ class GitCola < Formula
     sha256 "23f64ddbebe8299219d29c41c024b7b8af65f1fb949458997f04d52557d77bf5" => :el_capitan
   end
 
-  option "with-docs", "Build manpages and HTML docs"
-
+  depends_on "sphinx-doc" => :build
   depends_on "pyqt"
   depends_on "python"
-  depends_on "sphinx-doc" => :build if build.with? "docs"
 
   def install
     ENV.delete("PYTHONPATH")
     system "make", "PYTHON=python3", "prefix=#{prefix}", "install"
-
-    if build.with? "docs"
-      system "make", "install-doc", "PYTHON=python3", "prefix=#{prefix}",
-             "SPHINXBUILD=#{Formula["sphinx-doc"].opt_bin}/sphinx-build"
-    end
+    system "make", "install-doc", "PYTHON=python3", "prefix=#{prefix}",
+           "SPHINXBUILD=#{Formula["sphinx-doc"].opt_bin}/sphinx-build"
   end
 
   test do
