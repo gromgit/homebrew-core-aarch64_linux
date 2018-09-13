4e21e33bc7139a2739693a2ad95be7a8620c9ebc
Alex Gaynor <alex.gaynor@gmail.com>
2018-09-13 09:48:40 -0400

    opensc 0.19.0
    Closes #32041.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/opensc.rb
+++ Formula/opensc.rb
@@ -1,8 +1,8 @@
 class Opensc < Formula
   desc "Tools and libraries for smart cards"
   homepage "https://github.com/OpenSC/OpenSC/wiki"
-  url "https://github.com/OpenSC/OpenSC/releases/download/0.18.0/opensc-0.18.0.tar.gz"
-  sha256 "9bc0ff030dd1c10f646d54415eae1bb2b1c72dda710378343f027e17cd8c3757"
+  url "https://github.com/OpenSC/OpenSC/releases/download/0.19.0/opensc-0.19.0.tar.gz"
+  sha256 "2c5a0e4df9027635290b9c0f3addbbf0d651db5ddb0ab789cb0e978f02fd5826"
   head "https://github.com/OpenSC/OpenSC.git"
 
   bottle do
