2511fb7c1543f2cabc293d90321845db54a98b7c
FX Coudert <fxcoudert@gmail.com>
2018-09-14 16:19:25 +0200

    hyperestraier: remove options

--- Formula/hyperestraier.rb
+++ Formula/hyperestraier.rb
@@ -1,58 +1,3 @@
-class EucjpMecabIpadicRequirement < Requirement
-  fatal true
-
-  def initialize(tags = [])
-    super
-    @mecab_ipadic_installed = Formula["mecab-ipadic"].installed?
-  end
-
-  satisfy(:build_env => false) { @mecab_ipadic_installed && mecab_dic_charset == "euc" }
-
-  def message
-    if @mecab_ipadic_installed
-      <<~EOS
-        Hyper Estraier supports only the EUC-JP version of MeCab-IPADIC.
-        However, you have installed the #{mecab_dic_charset} version so far.
-
-        You have to reinstall your mecab-ipadic package manually with the
-        --with-charset=euc option before resuming the hyperestraier installation,
-        or you have to build hyperestraier without MeCab support.
-
-        To reinstall your mecab-ipadic and resume the hyperestraier installation:
-
-            $ brew uninstall mecab-ipadic
-            $ brew install mecab-ipadic --with-charset=euc
-            $ brew install hyperestraier --enable-mecab
-
-        To build hyperestraier without MeCab support:
-
-            $ brew install hyperestraier
-      EOS
-    else
-      <<~EOS
-        An EUC-JP version of MeCab-IPADIC is required. You have to install your
-        mecab-ipadic package manually with the --with-charset=euc option before
-        resuming the hyperestraier installation, or you have to build hyperestraier
-        without MeCab support.
-
-        To install an EUC-JP version of mecab-ipadic and resume the hyperestraier
-        installation:
-
-            $ brew install mecab-ipadic --with-charset=euc
-            $ brew install hyperestraier --enable-mecab
-
-        To build hyperestraier without MeCab support:
-
-            $ brew install hyperestraier
-      EOS
-    end
-  end
-
-  def mecab_dic_charset
-    /^charset:\t(\S+)$/ =~ `mecab -D` && Regexp.last_match[1]
-  end
-end
-
 class Hyperestraier < Formula
   desc "Full-text search system for communities"
   homepage "http://fallabs.com/hyperestraier/index.html"
@@ -70,25 +15,10 @@ class Hyperestraier < Formula
   end
 
   depends_on "qdbm"
-  depends_on "mecab" => :optional
-
-  if build.with? "mecab"
-    depends_on "mecab-ipadic"
-    depends_on EucjpMecabIpadicRequirement
-  end
-
-  deprecated_option "enable-mecab" => "with-mecab"
 
   def install
-    args = %W[
-      --disable-debug
-      --disable-dependency-tracking
-      --prefix=#{prefix}
-    ]
-
-    args << "--enable-mecab" if build.with? "mecab"
-
-    system "./configure", *args
+    system "./configure", "--disable-debug", "--prefix=#{prefix}",
+                          "--disable-dependency-tracking"
     system "make", "mac"
     system "make", "check-mac"
     system "make", "install-mac"
