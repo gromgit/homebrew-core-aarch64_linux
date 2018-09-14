08579abc552717d8b176d2770aee560ebd453f68
FX Coudert <fxcoudert@gmail.com>
2018-09-14 14:51:59 +0200

    eralchemy: remove options

--- Formula/eralchemy.rb
+++ Formula/eralchemy.rb
@@ -14,11 +14,10 @@ class Eralchemy < Formula
     sha256 "9bd0f4f745b3934599ac0b4286aa96202b3610e224630ecd5fc0870cf23f9d2f" => :el_capitan
   end
 
-  depends_on "python@2"
   depends_on "pkg-config" => :build
   depends_on "graphviz"
   depends_on "openssl"
-  depends_on "postgresql" => :optional
+  depends_on "python@2"
 
   resource "pygraphviz" do
     url "https://files.pythonhosted.org/packages/98/bb/a32e33f7665b921c926209305dde66fe41003a4ad934b10efb7c1211a419/pygraphviz-1.3.1.tar.gz"
@@ -30,11 +29,6 @@ class Eralchemy < Formula
     sha256 "6997507af46b10630e13b605ac278b78885fd683d038896dbee0e7ec41d809d2"
   end
 
-  resource "psycopg2" do
-    url "https://files.pythonhosted.org/packages/74/83/51580322ed0e82cba7ad8e0af590b8fb2cf11bd5aaa1ed872661bd36f462/psycopg2-2.7.4.tar.gz"
-    sha256 "8bf51191d60f6987482ef0cfe8511bbf4877a5aa7f313d7b488b53189cf26209"
-  end
-
   resource "er_example" do
     url "https://raw.githubusercontent.com/Alexis-benoist/eralchemy/v1.1.0/example/newsmeme.er"
     sha256 "5c475bacd91a63490e1cbbd1741dc70a3435e98161b5b9458d195ee97f40a3fa"
@@ -43,13 +37,11 @@ class Eralchemy < Formula
   def install
     venv = virtualenv_create(libexec)
 
-    res = resources.map(&:name).to_set - ["er_example", "psycopg2"]
+    res = resources.map(&:name).to_set - ["er_example"]
 
     res.each do |r|
       venv.pip_install resource(r)
     end
-
-    venv.pip_install resource("psycopg2") if build.with? "postgresql"
 
     venv.pip_install_and_link buildpath
   end
