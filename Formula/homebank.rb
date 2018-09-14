daa38cf2baf6f659b6eebf246d9d564079d9d5a4
FX Coudert <fxcoudert@gmail.com>
2018-09-14 14:51:59 +0200

    homebank: remove options

--- Formula/homebank.rb
+++ Formula/homebank.rb
@@ -3,6 +3,7 @@ class Homebank < Formula
   homepage "http://homebank.free.fr"
   url "http://homebank.free.fr/public/homebank-5.2.tar.gz"
   sha256 "8e5421e0f3e782bb403f26d713791671e0e51d887157169d915637e54e720908"
+  revision 1
 
   bottle do
     sha256 "6714b6331c71bcde205ec1ec301331fe3355d64ca29801f09f9a547e343cb1d6" => :mojave
@@ -11,23 +12,20 @@ class Homebank < Formula
     sha256 "9766b5b98e8386e815d4851be914cc0306526de66cb35d99eb022371af0a0708" => :el_capitan
   end
 
-  depends_on "pkg-config" => :build
   depends_on "intltool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "adwaita-icon-theme"
+  depends_on "fontconfig"
+  depends_on "freetype"
   depends_on "gettext"
   depends_on "gtk+3"
-  depends_on "adwaita-icon-theme"
   depends_on "hicolor-icon-theme"
-  depends_on "freetype"
-  depends_on "fontconfig"
+  depends_on "libofx"
   depends_on "libsoup"
-  depends_on "libofx" => :optional
 
   def install
-    args = ["--disable-dependency-tracking",
-            "--prefix=#{prefix}"]
-    args << "--with-ofx" if build.with? "libofx"
-
-    system "./configure", *args
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}", "--with-ofx"
     chmod 0755, "./install-sh"
     system "make", "install"
   end
