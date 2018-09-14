ae04169f04b1b4b345e2b18a33d2e75c2502a61a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-14 22:02:15 +0000

    qd: update 2.3.21 bottle.

--- Formula/qd.rb
+++ Formula/qd.rb
@@ -5,10 +5,11 @@ class Qd < Formula
   sha256 "185c97e38b00edf6f5dea3dfc48c5c0e3e8386f6ad2d815a2b93b72998d509f2"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "43f6d1436bdc32bd004306741adeb192f5d6d2ae9e8b71426b9503b0e8ad3fe8" => :high_sierra
-    sha256 "7c51a39a1e50a8f64f35b42f7155be032bb9ab2fad4d319894ed17b6d9d00cce" => :sierra
-    sha256 "13d95068f7a9ff0ec7bb4837dd82b0bf09ce8816dd2fb09ef6acc91ad6cb1f6a" => :el_capitan
+    cellar :any
+    sha256 "f6a06e197e6d4b466a1d1f11f2505dc3c46b97370ae46cdbe008472006db8369" => :mojave
+    sha256 "e265f9098008e2bf26e2f1b1e5ca1e5ec7309575a98f0eac9cb73aa2273896a0" => :high_sierra
+    sha256 "cafc8e6f65cebd1c01f90adb18bc481efa5b8b3d4acf4c0f122940c6088192ba" => :sierra
+    sha256 "7665a3c6b50383b20369c304038df2c75c874229cc79bf96bdd4c12c2efa937f" => :el_capitan
   end
 
   depends_on "gcc" # for gfortran
