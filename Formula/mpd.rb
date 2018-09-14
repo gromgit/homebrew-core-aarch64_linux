49e3f51c672e95839f641cac167cd7ac6483557e
FX Coudert <fxcoudert@gmail.com>
2018-09-14 16:57:29 +0200

    mpd: remove options

--- Formula/mpd.rb
+++ Formula/mpd.rb
@@ -3,6 +3,7 @@ class Mpd < Formula
   homepage "https://www.musicpd.org/"
   url "https://www.musicpd.org/download/mpd/0.20/mpd-0.20.21.tar.xz"
   sha256 "8322764dc265c20f05c8c8fdfdd578b0722e74626bef56fcd8eebfb01acc58dc"
+  revision 1
 
   bottle do
     sha256 "e8745fde74d53001ceab4881a52b8484d2251e9bd695a7ca0b49fd2fdd1fcdf0" => :mojave
@@ -17,56 +18,26 @@ class Mpd < Formula
     depends_on "automake" => :build
   end
 
-  option "with-wavpack", "Build with wavpack support (for .wv files)"
-  option "with-lastfm", "Build with last-fm support (for experimental Last.fm radio)"
-  option "with-lame", "Build with lame support (for MP3 encoding when streaming)"
-  option "with-two-lame", "Build with two-lame support (for MP2 encoding when streaming)"
-  option "with-flac", "Build with flac support (for Flac encoding when streaming)"
-  option "with-libvorbis", "Build with vorbis support (for Ogg encoding)"
-  option "with-yajl", "Build with yajl support (for playing from soundcloud)"
-  option "with-opus", "Build with opus support (for Opus encoding and decoding)"
-  option "with-libmodplug", "Build with modplug support (for decoding modules supported by MODPlug)"
-  option "with-pulseaudio", "Build with PulseAudio support (for sending audio output to a PulseAudio sound server)"
-  option "with-upnp", "Build with upnp database plugin support"
-
-  deprecated_option "with-vorbis" => "with-libvorbis"
-
-  depends_on "pkg-config" => :build
   depends_on "boost" => :build
+  depends_on "pkg-config" => :build
+  depends_on "expat"
+  depends_on "faad2"
+  depends_on "ffmpeg"
+  depends_on "flac"
+  depends_on "fluid-synth"
   depends_on "glib"
-  depends_on "libid3tag"
-  depends_on "sqlite"
-  depends_on "libsamplerate"
   depends_on "icu4c"
+  depends_on "lame"
+  depends_on "libao"
+  depends_on "libid3tag"
+  depends_on "libmpdclient"
+  depends_on "libsamplerate"
+  depends_on "libupnp"
+  depends_on "libvorbis"
+  depends_on "opus"
+  depends_on "sqlite"
 
   needs :cxx11
-
-  depends_on "libmpdclient"
-  depends_on "ffmpeg" # lots of codecs
-  # mpd also supports mad, mpg123, libsndfile, and audiofile, but those are
-  # redundant with ffmpeg
-  depends_on "fluid-synth"              # MIDI
-  depends_on "faad2"                    # MP4/AAC
-  depends_on "wavpack" => :optional     # WavPack
-  depends_on "libshout" => :optional    # Streaming (also pulls in Vorbis encoding)
-  depends_on "lame" => :optional        # MP3 encoding
-  depends_on "two-lame" => :optional    # MP2 encoding
-  depends_on "flac" => :optional        # Flac encoding
-  depends_on "jack" => :optional        # Output to JACK
-  depends_on "libmms" => :optional      # MMS input
-  depends_on "libzzip" => :optional     # Reading from within ZIPs
-  depends_on "yajl" => :optional        # JSON library for SoundCloud
-  depends_on "opus" => :optional        # Opus support
-  depends_on "libvorbis" => :optional
-  depends_on "libnfs" => :optional
-  depends_on "mad" => :optional
-  depends_on "libmodplug" => :optional  # MODPlug decoder
-  depends_on "pulseaudio" => :optional
-  depends_on "libao" => :optional       # Output to libao
-  if build.with? "upnp"
-    depends_on "expat"
-    depends_on "libupnp"
-  end
 
   def install
     # mpd specifies -std=gnu++0x, but clang appears to try to build
@@ -81,28 +52,19 @@ class Mpd < Formula
       --disable-dependency-tracking
       --prefix=#{prefix}
       --sysconfdir=#{etc}
+      --disable-libwrap
+      --disable-mad
+      --disable-mpc
+      --disable-soundcloud
+      --enable-ao
       --enable-bzip2
+      --enable-expat
       --enable-ffmpeg
       --enable-fluidsynth
       --enable-osx
-      --disable-libwrap
-      --disable-mpc
+      --enable-upnp
+      --enable-vorbis-encoder
     ]
-
-    args << "--disable-mad" if build.without? "mad"
-    args << "--enable-zzip" if build.with? "libzzip"
-    args << "--enable-lastfm" if build.with? "lastfm"
-    args << "--disable-lame-encoder" if build.without? "lame"
-    args << "--disable-soundcloud" if build.without? "yajl"
-    args << "--enable-vorbis-encoder" if build.with? "libvorbis"
-    args << "--enable-nfs" if build.with? "libnfs"
-    args << "--enable-modplug" if build.with? "libmodplug"
-    args << "--enable-pulse" if build.with? "pulseaudio"
-    args << "--enable-ao" if build.with? "libao"
-    if build.with? "upnp"
-      args << "--enable-upnp"
-      args << "--enable-expat"
-    end
 
     system "./configure", *args
     system "make"
