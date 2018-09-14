2cc14c9a023311d959157460a699040ba415e419
FX Coudert <fxcoudert@gmail.com>
2018-09-14 14:51:59 +0200

    oysttyer: remove options

--- Formula/oysttyer.rb
+++ Formula/oysttyer.rb
@@ -7,25 +7,8 @@ class Oysttyer < Formula
 
   bottle :unneeded
 
-  depends_on "readline" => :optional
-
-  resource "Term::ReadLine::TTYtter" do
-    url "https://cpan.metacpan.org/authors/id/C/CK/CKAISER/Term-ReadLine-TTYtter-1.4.tar.gz"
-    sha256 "ac373133cee1b2122a8273fe7b4244613d0eecefe88b668bd98fe71d1ec4ac93"
-  end
-
   def install
     bin.install "oysttyer.pl" => "oysttyer"
-
-    if build.with? "readline"
-      ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
-      resource("Term::ReadLine::TTYtter").stage do
-        system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
-        system "make"
-        system "make", "install"
-      end
-      bin.env_script_all_files(libexec/"bin", :PERL5LIB => ENV["PERL5LIB"])
-    end
   end
 
   test do
