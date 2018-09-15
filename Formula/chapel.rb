ce7d5309c92aa5d47f844723ee87f35e9abbe1b2
Tony Wallace <awallace@cray.com>
2018-09-15 13:29:45 -0700

    chapel 1.18.0
    Closes #32272.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/chapel.rb
+++ Formula/chapel.rb
@@ -1,9 +1,8 @@
 class Chapel < Formula
   desc "Emerging programming language designed for parallel computing"
   homepage "https://chapel-lang.org/"
-  url "https://github.com/chapel-lang/chapel/releases/download/1.17.1/chapel-1.17.1.tar.gz"
-  sha256 "f0a4671ca23832d69f831d230ca82264500ad1836d240854ba0835b95137f4f1"
-  head "https://github.com/chapel-lang/chapel.git"
+  url "https://github.com/chapel-lang/chapel/releases/download/1.18.0/chapel-1.18.0.tar.gz"
+  sha256 "68471e1f398b074edcc28cae0be26a481078adc3edea4df663f01c6bd3b6ae0d"
 
   bottle do
     sha256 "4d9a754da40ca8fed0a9ffbd93e8b7478484b3e62bbafc07d5e06a592665017d" => :mojave
@@ -16,6 +15,8 @@ class Chapel < Formula
     libexec.install Dir["*"]
     # Chapel uses this ENV to work out where to install.
     ENV["CHPL_HOME"] = libexec
+    # This is for mason
+    ENV["CHPL_REGEXP"] = "re2"
 
     # Must be built from within CHPL_HOME to prevent build bugs.
     # https://github.com/Homebrew/legacy-homebrew/pull/35166
@@ -23,6 +24,7 @@ class Chapel < Formula
       system "make"
       system "make", "chpldoc"
       system "make", "test-venv"
+      system "make", "mason"
       system "make", "cleanall"
     end
 
