ea39b207fa9bc4355142f46c674b9a9e50f488e4
theirix <theirix@gmail.com>
2018-09-15 11:33:37 +0300

    conan 1.7.3
    Closes #32102.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/conan.rb
+++ Formula/conan.rb
@@ -3,8 +3,8 @@ class Conan < Formula
 
   desc "Distributed, open source, package manager for C/C++"
   homepage "https://github.com/conan-io/conan"
-  url "https://github.com/conan-io/conan/archive/1.6.1.tar.gz"
-  sha256 "fbc59dd4c20a9940c72b0a415892d38823a7e2f6f7a7ee6cd84fbeb9b7a2b10c"
+  url "https://github.com/conan-io/conan/archive/1.7.3.tar.gz"
+  sha256 "f0ea9bd04120eaa3ad2eec3096c61dbf6392e50c1aba20e90c387de06b4f4d44"
   head "https://github.com/conan-io/conan.git"
 
   bottle do
@@ -21,7 +21,7 @@ class Conan < Formula
   depends_on "python"
 
   def install
-    inreplace "conans/requirements.txt", "PyYAML>=3.11, <3.13.0", "PyYAML>=3.11"
+    inreplace "conans/requirements.txt", "PyYAML>=3.11, <3.14.0", "PyYAML>=3.11"
     venv = virtualenv_create(libexec, "python3")
     system libexec/"bin/pip", "install", "-v", "--no-binary", ":all:",
                               "--ignore-installed", "PyYAML==3.13", buildpath
