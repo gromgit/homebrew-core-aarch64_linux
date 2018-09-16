6324bef3cd0e4c7b6172647c00501c6166646396
Viktor Szakats <commit@vszakats.net>
2018-09-16 10:52:41 +0000

    povray: secure url(s)

--- Formula/povray.rb
+++ Formula/povray.rb
@@ -1,6 +1,6 @@
 class Povray < Formula
   desc "Persistence Of Vision RAYtracer (POVRAY)"
-  homepage "http://www.povray.org/"
+  homepage "https://www.povray.org/"
   url "https://github.com/POV-Ray/povray/archive/v3.7.0.8.tar.gz"
   sha256 "53d11ebd2972fc452af168a00eb83aefb61387662c10784e81b63e44aa575de4"
   revision 1
