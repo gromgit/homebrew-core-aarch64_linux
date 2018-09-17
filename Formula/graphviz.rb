9a088dc44bfc854bfcb6438171d2f3357c67a334
commitay <commitay@users.noreply.github.com>
2018-09-17 21:03:12 +1000

    graphviz: fix dependency order

--- Formula/graphviz.rb
+++ Formula/graphviz.rb
@@ -19,8 +19,8 @@ class Graphviz < Formula
   head do
     url "https://github.com/ellson/graphviz.git"
 
-    depends_on "automake" => :build
     depends_on "autoconf" => :build
+    depends_on "automake" => :build
     depends_on "libtool" => :build
   end
 
@@ -34,14 +34,14 @@ class Graphviz < Formula
 
   depends_on "pkg-config" => :build
   depends_on :xcode => :build if build.with? "app"
-  depends_on "libtool"
-  depends_on "pango" => :optional
-  depends_on "gts" => :optional
-  depends_on "librsvg" => :optional
-  depends_on "freetype" => :optional
-  depends_on :x11 => :optional
   depends_on "gd"
   depends_on "libpng"
+  depends_on "libtool"
+  depends_on "freetype" => :optional
+  depends_on "gts" => :optional
+  depends_on "librsvg" => :optional
+  depends_on "pango" => :optional
+  depends_on :x11 => :optional
 
   if build.with? "bindings"
     depends_on "swig" => :build
