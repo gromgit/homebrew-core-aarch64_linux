040d5984fc3cb857d83356f2c789217bd7e26d88
FX Coudert <fxcoudert@gmail.com>
2018-09-17 11:10:10 +0200

    pypy3: remove options

--- Formula/pypy3.rb
+++ Formula/pypy3.rb
@@ -11,13 +11,13 @@ class Pypy3 < Formula
     sha256 "1650e0f3cfcc61793e853d1c738577a37259ea4351a8766f783afafefd0722e5" => :el_capitan
   end
 
-  depends_on :arch => :x86_64
   depends_on "pkg-config" => :build
   depends_on "pypy" => :build
-  depends_on "gdbm" => :recommended
-  depends_on "sqlite" => :recommended
+  depends_on :arch => :x86_64
+  depends_on "gdbm"
   depends_on "openssl"
-  depends_on "xz" => :recommended
+  depends_on "sqlite"
+  depends_on "xz"
 
   # packaging depends on pyparsing
   resource "pyparsing" do
@@ -89,10 +89,7 @@ class Pypy3 < Formula
 
     libexec.mkpath
     cd "pypy/tool/release" do
-      package_args = %w[--archive-name pypy3 --targetdir .]
-      package_args << "--without-gdbm" if build.without? "gdbm"
-      package_args << "--without-lzma" if build.without? "xz"
-      system python, "package.py", *package_args
+      system python, "package.py", "--archive-name", "pypy3", "--targetdir", "."
       system "tar", "-C", libexec.to_s, "--strip-components", "1", "-xf", "pypy3.tar.bz2"
     end
 
