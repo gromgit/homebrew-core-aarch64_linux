e23bd3ae73f5afb01db4a8ad74d1da26cca4034e
commitay <commitay@users.noreply.github.com>
2018-09-18 08:11:04 +1000

    mozjpeg: fix dependency order

--- Formula/mozjpeg.rb
+++ Formula/mozjpeg.rb
@@ -17,8 +17,8 @@ class Mozjpeg < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on "pkg-config" => :build
   depends_on "nasm" => :build
+  depends_on "pkg-config" => :build
   depends_on "libpng" => :optional
 
   def install
