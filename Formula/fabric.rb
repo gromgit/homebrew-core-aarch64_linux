7065630f5ee443cb7b413fb11b4a56239c5a63fe
commitay <commitay@users.noreply.github.com>
2018-09-17 20:23:26 +1000

    fabric: fix dependency order

--- Formula/fabric.rb
+++ Formula/fabric.rb
@@ -15,8 +15,8 @@ class Fabric < Formula
     sha256 "a2b96e26930efd22aff054c45c62db2af074dd3feb444ea367ffcaf67d2ecbc3" => :el_capitan
   end
 
-  depends_on "python@2"
   depends_on "openssl"
+  depends_on "python@2"
 
   resource "asn1crypto" do
     url "https://files.pythonhosted.org/packages/fc/f1/8db7daa71f414ddabfa056c4ef792e1461ff655c2ae2928a2b675bfed6b4/asn1crypto-0.24.0.tar.gz"
