4783cdbaf97b6f6c1390ba787c62172e13a7f322
commitay <commitay@users.noreply.github.com>
2018-09-17 21:33:28 +1000

    httperf: fix dependency order

--- Formula/httperf.rb
+++ Formula/httperf.rb
@@ -20,8 +20,8 @@ class Httperf < Formula
   head do
     url "https://github.com/httperf/httperf.git"
 
-    depends_on "automake" => :build
     depends_on "autoconf" => :build
+    depends_on "automake" => :build
     depends_on "libtool" => :build
   end
 
