cb42d01c037cb672267ea46c46946561c6d831a9
commitay <commitay@users.noreply.github.com>
2018-09-18 08:11:02 +1000

    mkvtoolnix: fix dependency order

--- Formula/mkvtoolnix.rb
+++ Formula/mkvtoolnix.rb
@@ -13,8 +13,8 @@ class Mkvtoolnix < Formula
 
   head do
     url "https://gitlab.com/mbunkus/mkvtoolnix.git"
-    depends_on "automake" => :build
     depends_on "autoconf" => :build
+    depends_on "automake" => :build
     depends_on "libtool" => :build
   end
 
