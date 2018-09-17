fd522ba9ea43b2214fbe773e353900a295732ba6
commitay <commitay@users.noreply.github.com>
2018-09-18 07:56:30 +1000

    libuninameslist: fix dependency order

--- Formula/libuninameslist.rb
+++ Formula/libuninameslist.rb
@@ -15,8 +15,8 @@ class Libuninameslist < Formula
   head do
     url "https://github.com/fontforge/libuninameslist.git"
 
-    depends_on "automake" => :build
     depends_on "autoconf" => :build
+    depends_on "automake" => :build
     depends_on "libtool" => :build
   end
 
