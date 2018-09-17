922eb3c1a08db9b1d9d06cf66edb483b693a34a8
commitay <commitay@users.noreply.github.com>
2018-09-18 08:10:58 +1000

    makepkg: fix dependency order

--- Formula/makepkg.rb
+++ Formula/makepkg.rb
@@ -14,21 +14,20 @@ class Makepkg < Formula
     sha256 "64718b5dff7f979eaabd7f9d6aa18d56156bdfbd90f767820f1de6823361d870" => :yosemite
   end
 
+  depends_on "asciidoc" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "docbook-xsl" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "bash"
+  depends_on "fakeroot"
+  depends_on "gettext"
+  depends_on "libarchive"
   # libalpm now calls fstatat, which is only available for >= 10.10
   # Regression due to https://git.archlinux.org/pacman.git/commit/?id=16718a21
   # Reported 19 Jun 2016: https://bugs.archlinux.org/task/49771
   depends_on :macos => :yosemite
-
-  depends_on "automake" => :build
-  depends_on "autoconf" => :build
-  depends_on "asciidoc" => :build
-  depends_on "docbook-xsl" => :build
-  depends_on "libtool" => :build
-  depends_on "pkg-config" => :build
-  depends_on "libarchive"
-  depends_on "bash"
-  depends_on "fakeroot"
-  depends_on "gettext"
   depends_on "openssl"
   depends_on "gpgme" => :optional
 
