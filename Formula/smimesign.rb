05eeda27779944b8448f2b26ffb57779683a5af9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-17 22:16:03 +0000

    smimesign: add 0.0.3 bottle.

--- Formula/smimesign.rb
+++ Formula/smimesign.rb
@@ -4,6 +4,13 @@ class Smimesign < Formula
   url "https://github.com/github/smimesign/archive/0.0.3.tar.gz"
   sha256 "69dfa16737c715679e067d0d929dd964790060b33981695d71bd6b047ae64e97"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9ad7ba446a3a48d463eacab9335c76b81edaa1f5712dc86de783d7bba99ca5d3" => :mojave
+    sha256 "6430de59344c81a0ff4d45769d3b4920bb926814d948123d1b8a1b7db640c9a2" => :high_sierra
+    sha256 "368bb35bcedfa1576518f9e64ad3d045e61f44ac5c2fd620e9833bc9d996b08e" => :sierra
+  end
+
   depends_on "go" => :build
   depends_on :macos => :sierra
 
