6c876568f1cf8a63b6fa970c9e0f1d17f2ba3af1
commitay <commitay@users.noreply.github.com>
2018-09-17 19:05:53 +1000

    cairo: fix dependency order

--- Formula/cairo.rb
+++ Formula/cairo.rb
@@ -19,17 +19,17 @@ class Cairo < Formula
 
   head do
     url "https://anongit.freedesktop.org/git/cairo", :using => :git
-    depends_on "automake" => :build
     depends_on "autoconf" => :build
+    depends_on "automake" => :build
     depends_on "libtool" => :build
   end
 
   depends_on "pkg-config" => :build
-  depends_on "freetype"
   depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "glib"
   depends_on "libpng"
   depends_on "pixman"
-  depends_on "glib"
 
   def install
     if build.head?
