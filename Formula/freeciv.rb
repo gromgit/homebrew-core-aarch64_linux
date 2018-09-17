e3fc3df3c2dd7dc43419429bf31f48a4f73467ea
commitay <commitay@users.noreply.github.com>
2018-09-17 20:23:30 +1000

    freeciv: fix dependency order

--- Formula/freeciv.rb
+++ Formula/freeciv.rb
@@ -14,8 +14,8 @@ class Freeciv < Formula
   head do
     url "https://github.com/freeciv/freeciv.git"
 
-    depends_on "automake" => :build
     depends_on "autoconf" => :build
+    depends_on "automake" => :build
     depends_on "gettext" => :build
     depends_on "libtool" => :build
   end
