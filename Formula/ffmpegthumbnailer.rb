5e78d7278b36993b513349c4cf9bd42240e9af18
commitay <commitay@users.noreply.github.com>
2018-09-17 20:23:27 +1000

    ffmpegthumbnailer: fix dependency order

--- Formula/ffmpegthumbnailer.rb
+++ Formula/ffmpegthumbnailer.rb
@@ -16,9 +16,9 @@ class Ffmpegthumbnailer < Formula
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "ffmpeg"
   depends_on "jpeg"
   depends_on "libpng"
-  depends_on "ffmpeg"
 
   def install
     ENV.cxx11 if MacOS.version < :mavericks
