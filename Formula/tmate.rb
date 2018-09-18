6c19430faf6628c81c3abd8aee09d9d5a131a616
commitay <commitay@users.noreply.github.com>
2018-09-18 12:26:15 +1000

    tmate: fix dependency order

--- Formula/tmate.rb
+++ Formula/tmate.rb
@@ -14,10 +14,10 @@ class Tmate < Formula
     sha256 "95dac4cc5d9fc02d7a7d832d471b4911b52e97ced0ab98e902c84d33ee54dc7c" => :el_capitan
   end
 
-  depends_on "pkg-config" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
   depends_on "libevent"
   depends_on "libssh"
   depends_on "msgpack"
