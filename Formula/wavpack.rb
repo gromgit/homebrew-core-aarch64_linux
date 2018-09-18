675f917f384a6a7c15c6d64384621297b75187b9
commitay <commitay@users.noreply.github.com>
2018-09-18 13:45:12 +1000

    wavpack: fix dependency order

--- Formula/wavpack.rb
+++ Formula/wavpack.rb
@@ -15,8 +15,8 @@ class Wavpack < Formula
 
   head do
     url "https://github.com/dbry/WavPack.git"
-    depends_on "automake" => :build
     depends_on "autoconf" => :build
+    depends_on "automake" => :build
     depends_on "libtool" => :build
   end
 
