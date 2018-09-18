bcdc146be28bfa06b2632467535aa0b7a0ca5a57
commitay <commitay@users.noreply.github.com>
2018-09-18 12:17:35 +1000

    scipy: fix dependency order

--- Formula/scipy.rb
+++ Formula/scipy.rb
@@ -18,8 +18,8 @@ class Scipy < Formula
   depends_on "swig" => :build
   depends_on "gcc" # for gfortran
   depends_on "numpy"
-  depends_on "python@2" => :recommended
   depends_on "python" => :recommended
+  depends_on "python@2" => :recommended
 
   cxxstdlib_check :skip
 
