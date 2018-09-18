de55e1b650c51da23bfa7fa0e1a1596ef41a1e51
commitay <commitay@users.noreply.github.com>
2018-09-18 13:31:13 +1000

    unac: fix dependency order

--- Formula/unac.rb
+++ Formula/unac.rb
@@ -17,8 +17,8 @@ class Unac < Formula
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "libtool" => :build
   depends_on "gettext" => :build
+  depends_on "libtool" => :build
 
   # configure.ac doesn't properly detect Mac OS's iconv library. This patch fixes that.
   patch :DATA
