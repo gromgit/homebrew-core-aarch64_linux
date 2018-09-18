0619b7684c7733263257ab78e64ee1ab9f81bee5
Jose Luis Salas <josacar@users.noreply.github.com>
2018-09-18 08:32:35 +0200

    asdf: add --HEAD support (#32159)

--- Formula/asdf.rb
+++ Formula/asdf.rb
@@ -3,6 +3,7 @@ class Asdf < Formula
   homepage "https://github.com/asdf-vm"
   url "https://github.com/asdf-vm/asdf/archive/v0.5.1.tar.gz"
   sha256 "5e7d4d61256e9f8185fcf04ef2b7f060a821569764d5c1212dc7d0ef7eb2edee"
+  head "https://github.com/asdf-vm/asdf.git"
 
   bottle :unneeded
 
@@ -36,6 +37,7 @@ class Asdf < Formula
   end
 
   test do
-    system "#{bin}/asdf", "plugin-list"
+    output = shell_output("#{bin}/asdf plugin-list")
+    assert_match "Oohes nooes ~! No plugins installed", output
   end
 end
