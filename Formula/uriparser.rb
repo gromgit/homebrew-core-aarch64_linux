337b51f38672a963afe4b440bc33664531d36b52
commitay <commitay@users.noreply.github.com>
2018-09-18 13:31:13 +1000

    uriparser: fix dependency order

--- Formula/uriparser.rb
+++ Formula/uriparser.rb
@@ -15,8 +15,8 @@ class Uriparser < Formula
   head do
     url "https://github.com/uriparser/uriparser.git"
 
-    depends_on "automake" => :build
     depends_on "autoconf" => :build
+    depends_on "automake" => :build
     depends_on "libtool" => :build
   end
 
