2df44f6e91434221e0f1e23411e391cfcd1540dc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-18 17:38:17 +0000

    fonttools: update 3.30.0 bottle.

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -9,10 +9,10 @@ class Fonttools < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "00430eef1d01782d4ed3919adbb93c55f62788356fd9eb417eed72c0d4dfa00a" => :mojave
-    sha256 "8182d441738a92f27c590d7c566f5eaa873ed3c9e57ad9071d805e7244a86df3" => :high_sierra
-    sha256 "6030f7ed2286083fa03f9b7fb5f3eab521655fc19efd57cb7a53b4cf93874a0e" => :sierra
-    sha256 "f2bb6b5c488f1db4b55d63626f2603927d697192234ce44b2d826416280b09e4" => :el_capitan
+    sha256 "685b7390ca68852875ff92082eeb17f44d86bf4630bac1b3aaef17b0ca7c5b3e" => :mojave
+    sha256 "1abc47b913c59c0812f84629a3fc0abfd998c26336df32dd8b9feb9b6dced86e" => :high_sierra
+    sha256 "36cb7c155c4f18e963647a41024fcf0bff82293281b94ebd4acb35f8c4235ab0" => :sierra
+    sha256 "b44296025ff97aa5ae3852bd70be5e682c1e767d55fa5a8d5654f29b81eb2499" => :el_capitan
   end
 
   option "with-pygtk", "Build with pygtk support for pyftinspect"
