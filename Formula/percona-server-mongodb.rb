e18240fa214282235534c2869a480a1e84c339cc
commitay <commitay@users.noreply.github.com>
2018-09-18 11:26:19 +1000

    percona-server-mongodb: fix dependency order

--- Formula/percona-server-mongodb.rb
+++ Formula/percona-server-mongodb.rb
@@ -14,12 +14,12 @@ class PerconaServerMongodb < Formula
   option "with-boost", "Compile using installed boost, not the version shipped with this formula"
   option "with-sasl", "Compile with SASL support"
 
-  depends_on "pkg-config" => :build
-  depends_on "boost" => :optional
   depends_on "go" => :build
-  depends_on :macos => :sierra
+  depends_on "pkg-config" => :build
   depends_on "scons" => :build
+  depends_on :macos => :sierra
   depends_on "openssl" => :recommended
+  depends_on "boost" => :optional
 
   conflicts_with "mongodb",
     :because => "percona-server-mongodb and mongodb install the same binaries."
