2e3992d5cd18d0817f8b8293f8eef67a03d8c907
FX Coudert <fxcoudert@gmail.com>
2018-09-18 22:16:44 +0200

    avfs: remove options

--- Formula/avfs.rb
+++ Formula/avfs.rb
@@ -3,6 +3,7 @@ class Avfs < Formula
   homepage "https://avf.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/avf/avfs/1.0.5/avfs-1.0.5.tar.bz2"
   sha256 "e5ce6b1f4193c37148b1b8a021f4f3d05e88f725cf11b16b95a58e8fdae50176"
+  revision 1
 
   bottle do
     sha256 "18dd2a2958a2a07b74309e3ec832dcc4c99de70b73e5d5b263be8833cc820ebb" => :high_sierra
@@ -11,25 +12,24 @@ class Avfs < Formula
 
   depends_on "pkg-config" => :build
   depends_on :macos => :sierra # needs clock_gettime
+  depends_on "openssl"
   depends_on :osxfuse
-  depends_on "xz" => :recommended # Upstream recommends building with lzma support.
-  depends_on "openssl" => :optional
+  depends_on "xz"
 
   # Fix scripts to work on Mac OS X.
   # Nothing the patch fixes has been changed in 1.0.2, so still necessary.
   patch :DATA
 
   def install
-    args = [
-      "--prefix=#{prefix}",
-      "--disable-debug",
-      "--disable-dependency-tracking",
-      "--disable-silent-rules",
-      "--enable-fuse",
-      "--enable-library",
+    args = %W[
+      --prefix=#{prefix}
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --enable-fuse
+      --enable-library
+      --with-ssl=#{Formula["openssl"].opt_prefix}
     ]
-
-    args << "--with-ssl=#{Formula["openssl"].opt_prefix}" if build.with? "openssl"
 
     system "./configure", *args
     system "make", "install"
