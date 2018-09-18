21208d5bbd09c3d2a732cf3020a8657b20e964f0
FX Coudert <fxcoudert@gmail.com>
2018-09-18 22:16:45 +0200

    gloox: remove options

--- Formula/gloox.rb
+++ Formula/gloox.rb
@@ -3,6 +3,7 @@ class Gloox < Formula
   homepage "https://camaya.net/gloox/"
   url "https://camaya.net/download/gloox-1.0.21.tar.bz2"
   sha256 "3c13155c10e3182a1a57779134cc524efb3657545849534b2831fae0e2c3a7cc"
+  revision 1
 
   bottle do
     cellar :any
@@ -14,8 +15,8 @@ class Gloox < Formula
   end
 
   depends_on "pkg-config" => :build
+  depends_on "libidn"
   depends_on "openssl"
-  depends_on "libidn" => :optional
 
   def install
     system "./configure", "--prefix=#{prefix}",
