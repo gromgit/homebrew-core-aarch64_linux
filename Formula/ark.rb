6b2d7f22f2bc95b8251e4b398a240796d6eeacc7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-19 20:51:03 +0000

    ark: update 0.9.5 bottle.

--- Formula/ark.rb
+++ Formula/ark.rb
@@ -6,10 +6,10 @@ class Ark < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c55d48ef8452819692e951340ff617f8d281d3d7187b118ee5253c59c31a41e8" => :mojave
-    sha256 "ef40fb816bf82fca4fa06efe2c7843499a7facd62687f327ed85440e1c350660" => :high_sierra
-    sha256 "cb398dbac48c3329ac5a6794d560fa456d2a7d81726852883f41db5173e4fb1f" => :sierra
-    sha256 "3ebbfe8c0580b2405ded45862e4aa2b2e699410efa23f5c1de8d868ebbe7f0c1" => :el_capitan
+    sha256 "e50cb6c48e065c22a5a042f0260b89475a9e509bff44449d5f71573aadebe479" => :mojave
+    sha256 "46829baea5dd3b52e51cf707aaa595e4c047c752899a9d822bb0743728a30885" => :high_sierra
+    sha256 "da7ecdcde9e3bb843a82e85ed8f5d52b6604c3469b307c9da5ad8e8f3fe4f25d" => :sierra
+    sha256 "1a519d097185828305f9ecfaac924d53efd4ad90e9d572931fa807f7ab74cdaf" => :el_capitan
   end
 
   depends_on "go" => :build
