6f3bdace6095018a5b17ad2154691423386fbef1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-19 20:42:47 +0000

    aravis: add 0.6.0 bottle.

--- Formula/aravis.rb
+++ Formula/aravis.rb
@@ -4,6 +4,13 @@ class Aravis < Formula
   url "https://github.com/AravisProject/aravis/archive/ARAVIS_0_6_0.tar.gz"
   sha256 "d7c047af9c845a3e036557b6e5d715da0c21eeb314f704a257f8636fcb634af6"
 
+  bottle do
+    sha256 "535b46c36a805622b39db2f1eb0ee30b9a16a3b1cccc99af0fea4253f58a2b76" => :mojave
+    sha256 "e4b43a8840f5e21a9acc48faaced67a58432deb26d38c632d5b8ccda129f2dca" => :high_sierra
+    sha256 "33f2fb3607b7547ac3505c42f74743fa4cd281044bda83a3348e20e3fc5c0a79" => :sierra
+    sha256 "1594fd0d1117da017a5934b5c1444b7ff0f1642817640f33dc12bf23b3a8e5ba" => :el_capitan
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "gtk-doc" => :build
