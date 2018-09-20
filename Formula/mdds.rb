dd88003d17eef114591c5e4c9715f7e2f6bf8ec1
Chongyu Zhu <i@lembacon.com>
2018-09-21 01:34:54 +0800

    mdds 1.4.2
    Closes #32291.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/mdds.rb
+++ Formula/mdds.rb
@@ -1,8 +1,8 @@
 class Mdds < Formula
   desc "Multi-dimensional data structure and indexing algorithm"
   homepage "https://gitlab.com/mdds/mdds"
-  url "https://kohei.us/files/mdds/src/mdds-1.4.0.tar.bz2"
-  sha256 "496b11564e2d95615090bf31a3524718260c5e8246e9d552216c4c56f5a24529"
+  url "https://kohei.us/files/mdds/src/mdds-1.4.2.tar.bz2"
+  sha256 "82f38750248c007956c38ffefcc549932c8b257b76c72fb79a06eabc50107369"
 
   bottle do
     cellar :any_skip_relocation
