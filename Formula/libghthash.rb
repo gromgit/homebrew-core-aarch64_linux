6004c0b2288be46eb9d7f903d60230dd801feefd
FX Coudert <fxcoudert@gmail.com>
2018-09-20 21:31:13 +0200

    libghthash: add mirror

--- Formula/libghthash.rb
+++ Formula/libghthash.rb
@@ -2,6 +2,7 @@ class Libghthash < Formula
   desc "Generic hash table for C++"
   homepage "https://www.bth.se/people/ska/sim_home/libghthash.html"
   url "https://www.bth.se/people/ska/sim_home/filer/libghthash-0.6.2.tar.gz"
+  mirror "http://ftp.internat.freebsd.org/pub/FreeBSD/distfiles/libghthash-0.6.2.tar.gz"
   sha256 "d1ccbb81f4c8afd7008f56ecb874f5cf497de480f49ee06929b4303d5852a7dd"
 
   bottle do
