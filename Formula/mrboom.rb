4d4d5fcd3cbec71654d31f9d2f99e8d8c75d95cb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-20 20:46:43 +0000

    mrboom: update 4.5 bottle.

--- Formula/mrboom.rb
+++ Formula/mrboom.rb
@@ -6,6 +6,7 @@ class Mrboom < Formula
 
   bottle do
     cellar :any
+    sha256 "fc10a261927a560776acad38740577b232f87470dc2a225cd973e9c85ee91a17" => :mojave
     sha256 "6b6fc5f88179c60f448e240744d5759410ad986c1bbae54fa180b187d88086ac" => :high_sierra
     sha256 "1e0f68ba7b12bc2bb879f99d90ec990c6d493c623a7fc787671e80dd8bac6884" => :sierra
     sha256 "d8169f04a040dee4151b850c0bf087146f3401110ac10e7651f2b0986ea6a504" => :el_capitan
