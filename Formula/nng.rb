6c7571f1ef592539949eb220b296321d5e00e141
Nathan Kent <nkent2@cs.rochester.edu>
2018-09-19 22:43:22 -0400

    nng 1.0.1 (new formula)
    Closes #32256.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/nng.rb
@@ -0,0 +1,35 @@
+class Nng < Formula
+  desc "Nanomsg-next-generation -- light-weight brokerless messaging"
+  homepage "https://nanomsg.github.io/nng/"
+  url "https://github.com/nanomsg/nng/archive/v1.0.1.tar.gz"
+  sha256 "c08ef670d472eb6fd50a2f863c6a4432b2963addd47f35d54cfb9fd7c543895b"
+
+  depends_on "asciidoctor" => :build
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", "-GNinja", "-DNNG_ENABLE_DOC=ON", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
+      system "ninja"
+      system "ninja", "install"
+    end
+  end
+
+  test do
+    bind = "tcp://127.0.0.1:8000"
+
+    pid = fork do
+      exec "#{bin}/nngcat --rep --bind #{bind} --format ascii --data home"
+    end
+    sleep 2
+
+    begin
+      output = shell_output("#{bin}/nngcat --req --connect #{bind} --format ascii --data brew")
+      assert_match /home/, output
+    ensure
+      Process.kill("SIGINT", pid)
+      Process.wait(pid)
+    end
+  end
+end
