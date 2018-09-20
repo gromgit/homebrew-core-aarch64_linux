d63ecc56b4358f8ce5d6976215d51f1c8a6ad6ed
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-20 19:18:21 +0000

    gupnp-tools: update 0.8.14_1 bottle.

--- Formula/gupnp-tools.rb
+++ Formula/gupnp-tools.rb
@@ -6,6 +6,7 @@ class GupnpTools < Formula
   revision 1
 
   bottle do
+    sha256 "4e56b83a3164e3ffa9adae38accc5099ae421727dde982f38dbbc912566b5e90" => :mojave
     sha256 "45ebb44e94ed47a5e47e6eb7de6fa4abc68029ca96a9f5217dfb3176c95c343f" => :high_sierra
     sha256 "270aa92cd6ae9fd83560c997d444afff984615c71bf81600701c22fe0216c058" => :sierra
     sha256 "dd76de959f08a89e5cba3c135607af53a922413f584397cd141fb6a93b52587c" => :el_capitan
