6eebb4d5367d135edcede4e0fbc37243aac2f51e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-20 23:34:32 +0000

    unison: update 2.51.2 bottle.

--- Formula/unison.rb
+++ Formula/unison.rb
@@ -6,6 +6,7 @@ class Unison < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "55a361bda2f5a43073508d25702eea6b16470f74619e4a6d1058f7fc89f901e5" => :mojave
     sha256 "1622a673b2bc8ef6d00dd754c26cfd3d5f737ed1dc48a46bbce486fa0f766181" => :high_sierra
     sha256 "fd00605490100ec224f5d86a79da341ce639e1d0772fb48cc133672d1bc2a812" => :sierra
     sha256 "53c945e143961a5d158a36980664d5be7153b84074bfa4220eee2db35404b7d3" => :el_capitan
