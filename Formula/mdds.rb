5d48b9a21c267ef548003738141f7fc5b7eccd07
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-20 23:25:11 +0000

    mdds: update 1.4.2 bottle.

--- Formula/mdds.rb
+++ Formula/mdds.rb
@@ -6,10 +6,10 @@ class Mdds < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "93d86496d4e693f4ea63c77af613af5b0be2b0da6026bb2a9aa7a7a59d32b811" => :mojave
-    sha256 "2cefe5ebe9c9580d93cdc439a0e3f17ec44e8cc88cd3243317c8e7711544e669" => :high_sierra
-    sha256 "2cefe5ebe9c9580d93cdc439a0e3f17ec44e8cc88cd3243317c8e7711544e669" => :sierra
-    sha256 "2cefe5ebe9c9580d93cdc439a0e3f17ec44e8cc88cd3243317c8e7711544e669" => :el_capitan
+    sha256 "a04c09f5f16e79b77edcf6385658ee28ae8d4187abe10d937acc2e31de33d83f" => :mojave
+    sha256 "f6f6896bcffcedd91647cf06bd924b1a08be6e6654f7a0cba7d1f471744f217c" => :high_sierra
+    sha256 "f6f6896bcffcedd91647cf06bd924b1a08be6e6654f7a0cba7d1f471744f217c" => :sierra
+    sha256 "f6f6896bcffcedd91647cf06bd924b1a08be6e6654f7a0cba7d1f471744f217c" => :el_capitan
   end
 
   depends_on "autoconf" => :build
