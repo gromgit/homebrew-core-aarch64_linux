5855f47dd363ecf869df9792a711009239bb3d8a
FX Coudert <fxcoudert@gmail.com>
2018-09-20 21:33:08 +0200

    eventlog: add mirror

--- Formula/eventlog.rb
+++ Formula/eventlog.rb
@@ -2,6 +2,7 @@ class Eventlog < Formula
   desc "Replacement for syslog API providing structure to messages"
   homepage "https://my.balabit.com/downloads/eventlog/"
   url "https://my.balabit.com/downloads/syslog-ng/sources/3.4.3/source/eventlog_0.2.13.tar.gz"
+  mirror "https://src.fedoraproject.org/lookaside/extras/eventlog/eventlog_0.2.13.tar.gz/68ec8d1ea3b98fa35002bb756227c315/eventlog_0.2.13.tar.gz"
   sha256 "7cb4e6f316daede4fa54547371d5c986395177c12dbdec74a66298e684ac8b85"
 
   bottle do
