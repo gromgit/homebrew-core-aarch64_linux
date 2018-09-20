ad2e314ee1813099a70463a52f551df66862a259
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-20 21:34:14 +0000

    restund: update 0.4.12 bottle.

--- Formula/restund.rb
+++ Formula/restund.rb
@@ -6,6 +6,7 @@ class Restund < Formula
 
   bottle do
     cellar :any
+    sha256 "489f651680461d466ab162ecbbc9f4e7acc554e3701b13828a5998de3e8accc4" => :mojave
     sha256 "05a527dfb0207ebd9b8a94ce4307ec0f106fa3c085851476eb36c27515d9b9fb" => :high_sierra
     sha256 "b54c91bb6457a8af37e55064d9f5093212ae7dc7c53863f4344f0862a0e6706e" => :sierra
     sha256 "520b20cfdfb1cb5da1ee3a147a707802f6cc77a289c80e2b04a4299446e67408" => :el_capitan
