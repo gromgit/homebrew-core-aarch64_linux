1b4c114f82f0efa2b470bdb8901ea1a738fd5d5c
Chongyu Zhu <i@lembacon.com>
2018-09-20 21:12:58 +0800

    wtf 20180919

--- Formula/wtf.rb
+++ Formula/wtf.rb
@@ -1,8 +1,8 @@
 class Wtf < Formula
   desc "Translate common Internet acronyms"
   homepage "http://cvsweb.netbsd.org/bsdweb.cgi/src/games/wtf/"
-  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20180816.tar.gz"
-  sha256 "f3c2aa795a388aee5b9cbaa8f0c4636c1f49b29eeb463fef1de68194cea13e39"
+  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20180919.tar.gz"
+  sha256 "e9787feaff2b31fe67370e0b42c6a36c3895eb730575f82c0f41392422edac58"
 
   bottle :unneeded
 
