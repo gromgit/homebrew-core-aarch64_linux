e37cd73c114233f99a1e9c19b216ef90ea4d2256
commitay <commitay@users.noreply.github.com>
2018-09-21 13:17:51 +1000

    folly: fix component order

--- Formula/folly.rb
+++ Formula/folly.rb
@@ -29,11 +29,11 @@ class Folly < Formula
   depends_on "snappy"
   depends_on "xz"
 
-  needs :cxx11
-
   # Known issue upstream. They're working on it:
   # https://github.com/facebook/folly/pull/445
   fails_with :gcc => "6"
+
+  needs :cxx11
 
   def install
     ENV.cxx11
