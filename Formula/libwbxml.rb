13c5296910115ee42685b12d6880284969d01219
FX Coudert <fxcoudert@gmail.com>
2018-09-21 15:08:50 +0200

    libwbxml: remove options

--- Formula/libwbxml.rb
+++ Formula/libwbxml.rb
@@ -3,6 +3,7 @@ class Libwbxml < Formula
   homepage "https://sourceforge.net/projects/libwbxml/"
   url "https://downloads.sourceforge.net/project/libwbxml/libwbxml/0.11.6/libwbxml-0.11.6.tar.bz2"
   sha256 "2f5ffe6f59986b34f9032bfbf013e32cabf426e654c160d208a99dc1b6284d29"
+  revision 1
   head "https://github.com/libwbxml/libwbxml.git"
 
   bottle do
@@ -14,28 +15,19 @@ class Libwbxml < Formula
     sha256 "6d3e97ce2d8a218780186f5be0005682768eb823ed0aec2c2275dabca8caafe3" => :yosemite
   end
 
-  option "with-docs", "Build the documentation with Doxygen and Graphviz"
-  option "with-verbose", "Build with verbose logging support"
-  deprecated_option "docs" => "with-docs"
-
   depends_on "cmake" => :build
-  depends_on "wget" => :optional
-
-  if build.with? "docs"
-    depends_on "doxygen" => :build
-    depends_on "graphviz" => :build
-  end
+  depends_on "doxygen" => :build
+  depends_on "graphviz" => :build
+  depends_on "wget"
 
   def install
     # Sandbox fix:
-    # Install in Cellar & then automatically symlink into top-level Module path.
-    inreplace "cmake/CMakeLists.txt", "${CMAKE_ROOT}/Modules/", "#{share}/cmake/Modules"
+    # Install in Cellar & then automatically symlink into top-level Module path
+    inreplace "cmake/CMakeLists.txt", "${CMAKE_ROOT}/Modules/",
+                                      "#{share}/cmake/Modules"
 
     mkdir "build" do
-      args = std_cmake_args
-      args << "-DBUILD_DOCUMENTATION=ON" if build.with? "docs"
-      args << "-DWBXML_LIB_VERBOSE=ON" if build.with? "verbose"
-      system "cmake", "..", *args
+      system "cmake", "..", *std_cmake_args, "-DBUILD_DOCUMENTATION=ON"
       system "make", "install"
     end
   end
