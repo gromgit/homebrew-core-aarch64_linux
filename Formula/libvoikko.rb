f0b8cdac184fd0cd9669e7ca47f10dae9173306b
commitay <commitay@users.noreply.github.com>
2018-09-21 14:12:52 +1000

    libvoikko: fix component order

--- Formula/libvoikko.rb
+++ Formula/libvoikko.rb
@@ -18,12 +18,12 @@ class Libvoikko < Formula
   depends_on "python" => :build
   depends_on "hfstospell"
 
-  needs :cxx11
-
   resource "voikko-fi" do
     url "https://www.puimula.org/voikko-sources/voikko-fi/voikko-fi-2.1.tar.gz"
     sha256 "71a823120a35ade6f20eaa7d00db27ec7355aa46a45a5b1a4a1f687a42134496"
   end
+
+  needs :cxx11
 
   def install
     system "./configure", "--disable-debug",
