ffbbabee2e1498866eb20901ba7f17bdbd8dfff5
commitay <commitay@users.noreply.github.com>
2018-09-21 14:36:01 +1000

    pypy: fix component order

--- Formula/pypy.rb
+++ Formula/pypy.rb
@@ -24,6 +24,9 @@ class Pypy < Formula
   depends_on "gdbm" => :recommended
   depends_on "sqlite" => :recommended
 
+  # https://bugs.launchpad.net/ubuntu/+source/gcc-4.2/+bug/187391
+  fails_with :gcc
+
   resource "bootstrap" do
     url "https://bitbucket.org/pypy/pypy/downloads/pypy2-v6.0.0-osx64.tar.bz2"
     version "6.0.0"
@@ -39,9 +42,6 @@ class Pypy < Formula
     url "https://files.pythonhosted.org/packages/ae/e8/2340d46ecadb1692a1e455f13f75e596d4eab3d11a57446f08259dee8f02/pip-10.0.1.tar.gz"
     sha256 "f2bd08e0cd1b06e10218feaf6fef299f473ba706582eb3bd9d52203fdbd7ee68"
   end
-
-  # https://bugs.launchpad.net/ubuntu/+source/gcc-4.2/+bug/187391
-  fails_with :gcc
 
   def install
     # Having PYTHONPATH set can cause the build to fail if another
