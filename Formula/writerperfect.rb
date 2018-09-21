bf076b555e90b747bfba0092aafc850760c2c168
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-21 00:32:33 +0000

    writerperfect: update 0.9.6 bottle.

--- Formula/writerperfect.rb
+++ Formula/writerperfect.rb
@@ -7,6 +7,7 @@ class Writerperfect < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "5e8658459f44238800ff490331d50aa6a71b48115157893c78901a4441a34dd3" => :mojave
     sha256 "12f30a1f15f5887da7675026656f59dd74b7fbffdf4129a2c1778578dbf4cc4a" => :high_sierra
     sha256 "36981e968c146d8aeca47d96327b3f3e909a3f58ca15bed17202e93fef6e17db" => :sierra
     sha256 "549f41525d1a5cf4cad493650ea0f8daae0208246f36984d6a56a4af533fc881" => :el_capitan
