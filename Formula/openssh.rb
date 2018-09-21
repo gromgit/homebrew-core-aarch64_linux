fa86274e3b5f516f46cb6f37903ea5284ef93e4a
commitay <commitay@users.noreply.github.com>
2018-09-21 14:25:45 +1000

    openssh: fix component order

--- Formula/openssh.rb
+++ Formula/openssh.rb
@@ -20,6 +20,11 @@ class Openssh < Formula
   depends_on "ldns" => :optional
   depends_on "pkg-config" => :build if build.with? "ldns"
 
+  resource "com.openssh.sshd.sb" do
+    url "https://opensource.apple.com/source/OpenSSH/OpenSSH-209.50.1/com.openssh.sshd.sb"
+    sha256 "a273f86360ea5da3910cfa4c118be931d10904267605cdd4b2055ced3a829774"
+  end
+
   # Both these patches are applied by Apple.
   patch do
     url "https://raw.githubusercontent.com/Homebrew/patches/1860b0a74/openssh/patch-sandbox-darwin.c-apple-sandbox-named-external.diff"
@@ -29,11 +34,6 @@ class Openssh < Formula
   patch do
     url "https://raw.githubusercontent.com/Homebrew/patches/d8b2d8c2/openssh/patch-sshd.c-apple-sandbox-named-external.diff"
     sha256 "3505c58bf1e584c8af92d916fe5f3f1899a6b15cc64a00ddece1dc0874b2f78f"
-  end
-
-  resource "com.openssh.sshd.sb" do
-    url "https://opensource.apple.com/source/OpenSSH/OpenSSH-209.50.1/com.openssh.sshd.sb"
-    sha256 "a273f86360ea5da3910cfa4c118be931d10904267605cdd4b2055ced3a829774"
   end
 
   def install
