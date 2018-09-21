712e5d49d8cc484a827814594b5ab26f545fb9b3
commitay <commitay@users.noreply.github.com>
2018-09-21 13:12:56 +1000

    cracklib: fix component order

--- Formula/cracklib.rb
+++ Formula/cracklib.rb
@@ -14,15 +14,15 @@ class Cracklib < Formula
 
   depends_on "gettext"
 
+  resource "cracklib-words" do
+    url "https://github.com/cracklib/cracklib/releases/download/cracklib-2.9.6/cracklib-words-2.9.6.bz2"
+    sha256 "460307bb9b46dfd5068d62178285ac2f70279e64b968972fe96f5ed07adc1a77"
+  end
+
   # Upstream commit from 25 Aug 2016 "Apply patch to fix CVE-2016-6318"
   patch :p2 do
     url "https://github.com/cracklib/cracklib/commit/47e5dec.patch?full_index=1"
     sha256 "7b3604d503208365951038b04990eef24b8ef90ce845fd84e2d2ab88a9a4f56b"
-  end
-
-  resource "cracklib-words" do
-    url "https://github.com/cracklib/cracklib/releases/download/cracklib-2.9.6/cracklib-words-2.9.6.bz2"
-    sha256 "460307bb9b46dfd5068d62178285ac2f70279e64b968972fe96f5ed07adc1a77"
   end
 
   def install
