e5e65be8137b938f12f612264b533eb95c4f6d0a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-21 20:09:58 +0000

    vorbis-tools: update 1.4.0_2 bottle.

--- Formula/vorbis-tools.rb
+++ Formula/vorbis-tools.rb
@@ -6,12 +6,10 @@ class VorbisTools < Formula
   revision 2
 
   bottle do
-    rebuild 1
-    sha256 "92c11dabfc26b7fd84178700b02329fac05ab8eaff5eb55a613132ad93fe8e65" => :mojave
-    sha256 "b764cae12c12c9338b96023d5e855aa6f39b989c19dea650d43edc219135b17d" => :high_sierra
-    sha256 "a062b8dbfe05458dc18c311b16260da2ae12b00b3537643b4336094d731f8808" => :sierra
-    sha256 "5ec349e8c68d23599b9e3185c6b8b1a6a3294d3f0056b740e7b29f141a4c70b3" => :el_capitan
-    sha256 "643822a271f6748dc635cede3cdf7b53558cc25f4663014006d46cda817a7c8c" => :yosemite
+    sha256 "ebcf801412fdea4ebf8e3dc850317fe6c8f8c4d9eeb10e3ba9c09bff3300b5cb" => :mojave
+    sha256 "fd042bd1be3987bfd945e4e681d246b179af2f67ec139c51d62ce7ab27ff0fdd" => :high_sierra
+    sha256 "6f6d8a2ac7093b409aae173ac42877fd9b3360284c17eb8789923c36e88acf00" => :sierra
+    sha256 "d5ef7448a1d2373a418c87dbd5a74975b0979c0c3c411e49414fe54869f31482" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
