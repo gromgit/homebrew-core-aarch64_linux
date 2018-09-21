bc97105dea56d835fe4a7e847307361285dd7592
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-21 20:09:59 +0000

    giflossy: update 1.91 bottle.

--- Formula/giflossy.rb
+++ Formula/giflossy.rb
@@ -7,10 +7,11 @@ class Giflossy < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "83353790febd1f793da50d6f4e04e4b04bc07031263d2b9d172b1b39cb33b7d0" => :mojave
-    sha256 "b81704f64e9bdac63646b92be361303215e24088f662a24c257a01de7fb4f734" => :high_sierra
-    sha256 "1b720c57508a15ac50a2f6340532115049a455098e2db1e98ea621e175dfd04a" => :sierra
-    sha256 "a770a479a8fcd961ceef0274fc8aa75f70fa7b1f4bb8cb34736c0d6902340e1c" => :el_capitan
+    rebuild 1
+    sha256 "9a0c85d6e19b61c8f232ab36211f8952220a62e5ca0f1c1f57de8280782dbc36" => :mojave
+    sha256 "492ddf714381465db00b10689af119d8c225ca5138304d8f1e6c946b9cf67e76" => :high_sierra
+    sha256 "464798aef709d62f6bfb4cefd3cb9d084e3aced492766e63b113762f15347d19" => :sierra
+    sha256 "50e8538008faf1bb05e8d44801cacd6e8f41fbf392ed23b639c7d05d36b5c8d8" => :el_capitan
   end
 
   depends_on "autoconf" => :build
