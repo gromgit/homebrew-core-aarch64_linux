4fa4ac3e77982b86a92ad82cced3b054af542b65
commitay <commitay@users.noreply.github.com>
2018-09-21 14:12:52 +1000

    libcec: fix component order

--- Formula/libcec.rb
+++ Formula/libcec.rb
@@ -15,12 +15,12 @@ class Libcec < Formula
 
   depends_on "cmake" => :build
 
-  needs :cxx11
-
   resource "p8-platform" do
     url "https://github.com/Pulse-Eight/platform/archive/p8-platform-2.1.0.1.tar.gz"
     sha256 "064f8d2c358895c7e0bea9ae956f8d46f3f057772cb97f2743a11d478a0f68a0"
   end
+
+  needs :cxx11
 
   def install
     ENV.cxx11
