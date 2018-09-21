26923dd1186677727e63bfe5b4c17c2da10641e6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-21 20:09:57 +0000

    smpeg: update 0.4.5 bottle.

--- Formula/smpeg.rb
+++ Formula/smpeg.rb
@@ -5,13 +5,11 @@ class Smpeg < Formula
 
   bottle do
     cellar :any
-    sha256 "cb3025371e326912b67b611c8c199c8ce0691e367b4ecbe676458beb30e23ac9" => :mojave
-    sha256 "ff2e2fa327d7bfb2f5a0d370e912865b7936248dda469d1df3a1a31b4bd26924" => :high_sierra
-    sha256 "7c4a228fa509d8f55b14ecf64e9c2eecc7a025c1143d983b4bb0efe379699143" => :sierra
-    sha256 "a3259cfd8367ab200203429135ada050db88f8fafc58bf601a6ab0539c4292ed" => :el_capitan
-    sha256 "b205e203c6942fcf32e16696eeca2e38416b226e9f737ad6b53e21a3130e7fc8" => :yosemite
-    sha256 "9230641a8af9ce9c7da7102ea957a764d22185981e123604f81f2260a9f75dcb" => :mavericks
-    sha256 "0923b3aae2d9854152ccd50b94a838e590e482d93a2cee4ed010c9810e7dabd7" => :mountain_lion
+    rebuild 1
+    sha256 "6e826bd49ceb171cc36877c0498d8ccfc3c614f39b684728e0c307b69942d58c" => :mojave
+    sha256 "2779c8aba2aed376076e53fc9e2e694e8b5fabca0096ae91eed786b73ef3704f" => :high_sierra
+    sha256 "9ac1a1c83a9861b8762ab711d709e67bf020204fb1c1b5907b244d83ced4ab2c" => :sierra
+    sha256 "a4bf36f39959150e1f0cd83c8f58761ce59acdee50f591a1f695665b7517728a" => :el_capitan
   end
 
   depends_on "autoconf" => :build
