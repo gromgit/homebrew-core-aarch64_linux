83d6cb2df530c0e40b896bf4d629f8419eaa0985
commitay <commitay@users.noreply.github.com>
2018-09-21 14:36:00 +1000

    phantomjs: fix component order

--- Formula/phantomjs.rb
+++ Formula/phantomjs.rb
@@ -5,6 +5,20 @@ class Phantomjs < Formula
       :tag => "2.1.1",
       :revision => "d9cda3dcd26b0e463533c5cc96e39c0f39fc32c1"
 
+  bottle do
+    cellar :any
+    rebuild 1
+    sha256 "0634bc067a6e257d15e57bc65d0c1660fce797c005ab83990300d4b177583ae6" => :high_sierra
+    sha256 "7833bb1de72bd88057d2260a9021ce4ec0d5f2925273d537c69549d8e30ebc6d" => :sierra
+    sha256 "370e6f729ac20091c408dc5a1be14361b861bef78f1a52efb201e27e7440cfa4" => :el_capitan
+    sha256 "ec65660b5c4097886d52fe0b4928aaefd6d09fb0e6ab707b1fa4d762acf873e1" => :yosemite
+    sha256 "d837e04d137ae8ddc8eb807b7ca5a08a0fccdfd513f4fdd4f1d610ce8abc0874" => :mavericks
+  end
+
+  depends_on :xcode => :build
+  depends_on :macos => :lion
+  depends_on "openssl"
+
   # Fixes build.py for non-standard Homebrew prefixes.  Applied
   # upstream, can be removed in next release.
   patch do
@@ -20,20 +34,6 @@ class Phantomjs < Formula
       sha256 "d47b52c6a932139a448340244f66ea126412d210ab94dc19da7c468afaf5f45a"
     end
   end
-
-  bottle do
-    cellar :any
-    rebuild 1
-    sha256 "0634bc067a6e257d15e57bc65d0c1660fce797c005ab83990300d4b177583ae6" => :high_sierra
-    sha256 "7833bb1de72bd88057d2260a9021ce4ec0d5f2925273d537c69549d8e30ebc6d" => :sierra
-    sha256 "370e6f729ac20091c408dc5a1be14361b861bef78f1a52efb201e27e7440cfa4" => :el_capitan
-    sha256 "ec65660b5c4097886d52fe0b4928aaefd6d09fb0e6ab707b1fa4d762acf873e1" => :yosemite
-    sha256 "d837e04d137ae8ddc8eb807b7ca5a08a0fccdfd513f4fdd4f1d610ce8abc0874" => :mavericks
-  end
-
-  depends_on :xcode => :build
-  depends_on :macos => :lion
-  depends_on "openssl"
 
   def install
     ENV["OPENSSL"] = Formula["openssl"].opt_prefix
