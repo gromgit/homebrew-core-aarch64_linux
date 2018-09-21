127a8c5ae13934cda9b8c664b2816e575ef420c7
FX Coudert <fxcoudert@gmail.com>
2018-09-21 15:08:50 +0200

    qcachegrind: remove options

--- Formula/qcachegrind.rb
+++ Formula/qcachegrind.rb
@@ -3,6 +3,7 @@ class Qcachegrind < Formula
   homepage "https://kcachegrind.github.io/"
   url "https://download.kde.org/stable/applications/18.04.3/src/kcachegrind-18.04.3.tar.xz"
   sha256 "2370827f8d3c29ec931fc3ebf34726e42d25aaaab6c2f10dc5dd87f57056acfd"
+  revision 1
 
   bottle do
     cellar :any
@@ -12,8 +13,8 @@ class Qcachegrind < Formula
     sha256 "7a9e4dac8bf9d741b18391c8d2e7f05f407caf1f30e6619b64cd3a9cce22cd1b" => :el_capitan
   end
 
+  depends_on "graphviz"
   depends_on "qt"
-  depends_on "graphviz" => :optional
 
   def install
     cd "qcachegrind" do
