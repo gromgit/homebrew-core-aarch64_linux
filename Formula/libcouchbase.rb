4ee5f4f711c52e2a2192fc6f01d8935b3915c849
Sergey Avseyev <sergey.avseyev@gmail.com>
2018-09-21 13:07:12 +0300

    libcouchbase 2.9.5
    Closes #32321.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/libcouchbase.rb
+++ Formula/libcouchbase.rb
@@ -1,8 +1,8 @@
 class Libcouchbase < Formula
   desc "C library for Couchbase"
   homepage "https://developer.couchbase.com/documentation/server/current/sdk/c/start-using-sdk.html"
-  url "https://packages.couchbase.com/clients/c/libcouchbase-2.9.3.tar.gz"
-  sha256 "cd3bba99d5e4935240e502535c3275e0756b480c6bc6746a46fb745d5e7066e4"
+  url "https://packages.couchbase.com/clients/c/libcouchbase-2.9.5.tar.gz"
+  sha256 "2d8c1df339066619e2491ccca5d50164190abfacb38cab24a3190e73649707bd"
   head "https://github.com/couchbase/libcouchbase.git"
 
   bottle do
@@ -12,25 +12,19 @@ class Libcouchbase < Formula
     sha256 "9e172a43648d20d7aa7bdd75c8ff6ebfcce3eb0916ae7a6304687584b633fed4" => :el_capitan
   end
 
-  option "with-libev", "Build libev plugin"
-
-  deprecated_option "with-libev-plugin" => "with-libev"
-
   depends_on "cmake" => :build
+  depends_on "libev"
   depends_on "libevent"
+  depends_on "libuv"
   depends_on "openssl"
-  depends_on "libev" => :optional
-  depends_on "libuv" => :optional
 
   def install
-    args = std_cmake_args << "-DLCB_NO_TESTS=1" << "-DLCB_BUILD_LIBEVENT=ON"
-
-    ["libev", "libuv"].each do |dep|
-      args << "-DLCB_BUILD_#{dep.upcase}=" + (build.with?(dep) ? "ON" : "OFF")
-    end
-
-    mkdir "LCB-BUILD" do
-      system "cmake", "..", *args
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args,
+                            "-DLCB_NO_TESTS=1",
+                            "-DLCB_BUILD_LIBEVENT=ON",
+                            "-DLCB_BUILD_LIBEV=ON",
+                            "-DLCB_BUILD_LIBUV=ON"
       system "make", "install"
     end
   end
