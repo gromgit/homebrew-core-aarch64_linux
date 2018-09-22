9c6cd7ba5aae88abc2ed2dce04c4a78a49f691f2
Giorgio Gallo <giorgio.gallo@bitnic.it>
2018-09-22 17:15:33 +0200

    megatools 1.10.2
    Closes #32353.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/megatools.rb
+++ Formula/megatools.rb
@@ -1,8 +1,8 @@
 class Megatools < Formula
   desc "Command-line client for Mega.co.nz"
   homepage "https://megatools.megous.com/"
-  url "https://megatools.megous.com/builds/megatools-1.10.1.tar.gz"
-  sha256 "9938127db27b579d49aa8722bfdddd17833fe90668aa2670ce88d1c889452d67"
+  url "https://megatools.megous.com/builds/megatools-1.10.2.tar.gz"
+  sha256 "179e84c68e24696c171238a72bcfe5e28198e4c4e9f9043704f36e5c0b17c38a"
 
   bottle do
     cellar :any
