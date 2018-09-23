3055c3622116061fad612d4f0abebd7600f8f145
Ilya Puchka <ilya.puchka@babylonhealth.com>
2018-09-23 13:36:33 +0100

    sourcery 0.15.0
    Closes #32367.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/sourcery.rb
+++ Formula/sourcery.rb
@@ -1,8 +1,8 @@
 class Sourcery < Formula
   desc "Meta-programming for Swift, stop writing boilerplate code"
   homepage "https://github.com/krzysztofzablocki/Sourcery"
-  url "https://github.com/krzysztofzablocki/Sourcery/archive/0.14.0.tar.gz"
-  sha256 "890548cd42d0ba08ebc78c182304b956ae3ccc0f6cda9ae2545ace6f313f9817"
+  url "https://github.com/krzysztofzablocki/Sourcery/archive/0.15.0.tar.gz"
+  sha256 "b695713996fff2de8390fae42b81686eac85f1603554cc202edbbd8693f8a32e"
   head "https://github.com/krzysztofzablocki/Sourcery.git"
 
   bottle do
@@ -10,7 +10,7 @@ class Sourcery < Formula
     sha256 "80364efc5f696093c2a938f219362acf24da7aa4e1c11a582ee2c43324330f44" => :high_sierra
   end
 
-  depends_on :xcode => ["9.3", :build]
+  depends_on :xcode => ["10.0", :build]
   depends_on :xcode => "6.0"
 
   def install
