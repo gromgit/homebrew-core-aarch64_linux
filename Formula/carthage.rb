aed3552ab30d5dfb430cd4034f674d05dc01d56c
J.D. Healy <jdhealy@gmail.com>
2018-09-25 01:08:22 +0200

    carthage 0.31.0
    Closes #32544.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/carthage.rb
+++ Formula/carthage.rb
@@ -2,8 +2,8 @@ class Carthage < Formula
   desc "Decentralized dependency manager for Cocoa"
   homepage "https://github.com/Carthage/Carthage"
   url "https://github.com/Carthage/Carthage.git",
-      :tag => "0.30.1",
-      :revision => "49c7901f851384a358bac2e5d2333cabc4f91900",
+      :tag => "0.31.0",
+      :revision => "04994e9e844d53220d8796a648a7dad12a5808c9",
       :shallow => false
   head "https://github.com/Carthage/Carthage.git", :shallow => false
 
@@ -13,9 +13,13 @@ class Carthage < Formula
     sha256 "1d7ffb4d63ad048712d6b81f7e26fa4c5613d5dd6d34c3e5c46a086854d3a59e" => :sierra
   end
 
-  depends_on :xcode => ["9.0", :build]
+  depends_on :xcode => ["9.4", :build]
 
   def install
+    match = "XCODEFLAGS=-workspace 'Carthage.xcworkspace' -scheme 'carthage' DSTROOT=$(CARTHAGE_TEMPORARY_FOLDER)"
+    inreplace "Makefile" do |s|
+      s.sub!(match, match + " OTHER_LDFLAGS=-Wl,-headerpad_max_install_names")
+    end
     system "make", "prefix_install", "PREFIX=#{prefix}"
     bash_completion.install "Source/Scripts/carthage-bash-completion" => "carthage"
     zsh_completion.install "Source/Scripts/carthage-zsh-completion" => "_carthage"
