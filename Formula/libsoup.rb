f81fe45b721ee239a505351fc7d96907de824ec1
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-09-24 17:55:35 +0100

    libsoup 2.64.1
    Closes #32394.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libsoup.rb
+++ Formula/libsoup.rb
@@ -1,8 +1,8 @@
 class Libsoup < Formula
   desc "HTTP client/server library for GNOME"
   homepage "https://wiki.gnome.org/Projects/libsoup"
-  url "https://download.gnome.org/sources/libsoup/2.64/libsoup-2.64.0.tar.xz"
-  sha256 "d95bc9602c97ec805d80e5ddf75605665c4c4d6b2c56893c416e707ef578e727"
+  url "https://download.gnome.org/sources/libsoup/2.64/libsoup-2.64.1.tar.xz"
+  sha256 "359bd02a909bfd43f68712146e9c4f4c643bffa84e513d8b0af1be215a92e1f3"
 
   bottle do
     sha256 "69352931117cbf3fb98df53d69a6a60aeb9c08fae92536980c178136a1918935" => :mojave
