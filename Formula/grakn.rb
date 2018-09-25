60a3e9141501b2c2bcb58bd4acf916b6f960ae30
Ganeshwara Herawan Hananda <ganeshwara@grakn.ai>
2018-09-25 12:06:23 +0100

    grakn: revision for upstream re-upload (#32399)

--- Formula/grakn.rb
+++ Formula/grakn.rb
@@ -2,7 +2,8 @@ class Grakn < Formula
   desc "The distributed hyper-relational database for knowledge engineering"
   homepage "https://grakn.ai"
   url "https://github.com/graknlabs/grakn/releases/download/v1.4.0/grakn-core-1.4.0.zip"
-  sha256 "b88d75265ff238ecad6ad7e2505fc2b0223b803e798eea29a9649ccde2e6d850"
+  sha256 "b52b758f01dc861b3f8628afeb48fb7dfba23ee8025a2de23f448e60cf0b42a0"
+  revision 1
 
   bottle :unneeded
 
