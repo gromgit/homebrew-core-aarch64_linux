62414534681702b63dce1cce2919eade1df5a599
commitay <commitay@users.noreply.github.com>
2018-09-26 15:19:36 +1000

    remove workaround needed for `rdar://40724445`
    Closes #32401.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/weaver.rb
+++ Formula/weaver.rb
@@ -13,23 +13,7 @@ class Weaver < Formula
   depends_on :xcode => ["10.0", :build]
 
   def install
-    # libxml2 has to be included in ISYSTEM_PATH for building one of
-    # dependencies. It didn't happen automatically before Xcode 9.3
-    # so homebrew patched environment variable to get it work.
-    # But since Xcode 9.3 includes it already, the build will fail
-    # because of redefinition of libxml2 module.
-    # It's a bug of homebrew but before it's fixed, it's easier
-    # to provide in-place workaround for now.
-    # Please remove this once homebrew is patched.
-    # https://github.com/Homebrew/brew/pull/4147
-    if OS::Mac::Xcode.version >= Version.new("9.3")
-      old_isystem_paths = ENV["HOMEBREW_ISYSTEM_PATHS"]
-      ENV["HOMEBREW_ISYSTEM_PATHS"] = old_isystem_paths.gsub("/usr/include/libxml2", "")
-    end
-
     system "make", "install", "PREFIX=#{prefix}"
-
-    ENV["HOMEBREW_ISYSTEM_PATHS"] = old_isystem_paths if defined? old_isystem_paths
   end
 
   test do
