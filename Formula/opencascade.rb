1de3e0d2fd0921adba04df80f8d10251daf0e7c4
FX Coudert <fxcoudert@gmail.com>
2018-09-26 12:10:22 +0200

    opencascade 7.3.0
    Closes #32454.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/opencascade.rb
+++ Formula/opencascade.rb
@@ -1,9 +1,9 @@
 class Opencascade < Formula
   desc "3D modeling and numerical simulation software for CAD/CAM/CAE"
-  homepage "https://www.opencascade.com/content/core-technology"
-  url "https://git.dev.opencascade.org/gitweb/?p=occt.git;a=snapshot;h=refs/tags/V7_2_0;sf=tgz"
-  version "7.2.0"
-  sha256 "adb6fc44efebdb09e30a487fa412afae9599992dc771b0039b4ebf306891a558"
+  homepage "https://www.opencascade.com/content/overview"
+  url "https://git.dev.opencascade.org/gitweb/?p=occt.git;a=snapshot;h=refs/tags/V7_3_0;sf=tgz"
+  version "7.3.0"
+  sha256 "7298c5eadc6dd0aeb6265ff2958e8e742d6e3aa65227acce8094f96f1bf6d2ac"
 
   bottle do
     cellar :any
@@ -20,22 +20,19 @@ class Opencascade < Formula
   depends_on "tbb"
 
   def install
-    # Fix build on case-sensitive file systems
-    inreplace "adm/cmake/occt_csf.cmake", "NAMES Appkit", "NAMES AppKit"
-
-    sdk = MacOS::CLT.installed? ? "" : MacOS.sdk_path
-    system "cmake", ".", "-DUSE_FREEIMAGE=ON",
-                         "-DUSE_GL2PS=ON",
-                         "-DUSE_TBB=ON",
-                         "-DINSTALL_DOC_Overview=ON",
-                         "-D3RDPARTY_FREEIMAGE_DIR=#{Formula["freeimage"].opt_prefix}",
-                         "-D3RDPARTY_FREETYPE_DIR=#{Formula["freetype"].opt_prefix}",
-                         "-D3RDPARTY_GL2PS_DIR=#{Formula["gl2ps"].opt_prefix}",
-                         "-D3RDPARTY_TBB_DIR=#{Formula["tbb"].opt_prefix}",
-                         "-D3RDPARTY_TCL_DIR:PATH=/usr",
-                         "-D3RDPARTY_TCL_INCLUDE_DIR=#{sdk}/usr/include",
-                         "-D3RDPARTY_TK_INCLUDE_DIR=#{sdk}/usr/include",
-                         *std_cmake_args
+    system "cmake", ".",
+                    "-DUSE_FREEIMAGE=ON",
+                    "-DUSE_GL2PS=ON",
+                    "-DUSE_TBB=ON",
+                    "-DINSTALL_DOC_Overview=ON",
+                    "-D3RDPARTY_FREEIMAGE_DIR=#{Formula["freeimage"].opt_prefix}",
+                    "-D3RDPARTY_FREETYPE_DIR=#{Formula["freetype"].opt_prefix}",
+                    "-D3RDPARTY_GL2PS_DIR=#{Formula["gl2ps"].opt_prefix}",
+                    "-D3RDPARTY_TBB_DIR=#{Formula["tbb"].opt_prefix}",
+                    "-D3RDPARTY_TCL_DIR:PATH=#{MacOS.sdk_path_if_needed}/usr",
+                    "-D3RDPARTY_TCL_INCLUDE_DIR=#{MacOS.sdk_path_if_needed}/usr/include",
+                    "-D3RDPARTY_TK_INCLUDE_DIR=#{MacOS.sdk_path_if_needed}/usr/include",
+                    *std_cmake_args
     system "make", "install"
 
     bin.env_script_all_files(libexec/"bin", :CASROOT => prefix)
