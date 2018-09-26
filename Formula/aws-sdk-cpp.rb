67a4f5d85f839adc48b6eef47de2c88b8fcb45ea
FX Coudert <fxcoudert@gmail.com>
2018-09-26 16:50:49 +0200

    aws-sdk-cpp: remove options

--- Formula/aws-sdk-cpp.rb
+++ Formula/aws-sdk-cpp.rb
@@ -15,14 +15,12 @@ class AwsSdkCpp < Formula
   end
 
   option "with-static", "Build with static linking"
-  option "without-http-client", "Don't include the libcurl HTTP client"
 
   depends_on "cmake" => :build
 
   def install
     args = std_cmake_args
     args << "-DSTATIC_LINKING=1" if build.with? "static"
-    args << "-DNO_HTTP_CLIENT=1" if build.without? "http-client"
 
     mkdir "build" do
       system "cmake", "..", *args
