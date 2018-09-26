03c0cf6b5b9b4cb45959fa735a9f610b68a67e00
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-26 12:36:58 +0000

    nginx: update 1.15.4 bottle.

--- Formula/nginx.rb
+++ Formula/nginx.rb
@@ -8,10 +8,9 @@ class Nginx < Formula
   head "https://hg.nginx.org/nginx/", :using => :hg
 
   bottle do
-    sha256 "8b8c9bf42173d933764db310a46da71796685ef559fd04c86c775a175ec1f534" => :mojave
-    sha256 "cf9ba487a9900301e2d1cd8dd70ff23f2788e67b2519489874c9df76dd38e573" => :high_sierra
-    sha256 "c87d6607c4d5448749075ad65d490042d20a2773c31769621767c5bd031e8d09" => :sierra
-    sha256 "65b3cb83abbe4d0e5b297aadd60bc4b48811f72caa72d461b5c503546f50df8a" => :el_capitan
+    sha256 "05f53e506ec0ef3a2f6d458d858055182ac4346be138cbbe4c10aa3df3b29b41" => :mojave
+    sha256 "f506291fd2b3d85d6b86a6611aff4fd6d082549e35a7632513230e221ce7c646" => :high_sierra
+    sha256 "08ba38c8dcf815c616a0f2ab257d7f145c924858a1fd0bc23832cc8a3a3b071e" => :sierra
   end
 
   option "with-passenger", "Compile with support for Phusion Passenger module"
