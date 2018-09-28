ec8e13acdfa46cafb1425304a1b92ba793568aad
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-28 12:06:40 +0000

    alexjs: update 6.0.1 bottle.

--- Formula/alexjs.rb
+++ Formula/alexjs.rb
@@ -8,10 +8,9 @@ class Alexjs < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a73abe634cca6ab63b5bba0158773097cdc91b3bcd89e2199714afb69e1bf393" => :mojave
-    sha256 "63837a78ef2e29112d814512c5cc1e2f4f84838755a48282818d524a2163b510" => :high_sierra
-    sha256 "57ec3f1ac7588459ee8f169dcef16fa9e89154a32b12c4bdf4d668e0d2fa2b84" => :sierra
-    sha256 "80725e1dba5b2ffa15bf1efb55e65f8098f827ae6c3edefadfa0b4f1d1bb373b" => :el_capitan
+    sha256 "8e2108b653293d18ab50c95597f16cfd13029883e783b8114d2944933583f979" => :mojave
+    sha256 "6a3613906ada284c99b6a0dbbd486446ba1cb2ee07786139ef1da8d8889b1352" => :high_sierra
+    sha256 "858ac6fdcf4d13ee13580f6e9d6ba653e0809fb1ed5298e3b06a64976faabac0" => :sierra
   end
 
   depends_on "node"
