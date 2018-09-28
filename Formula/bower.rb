84c20c2a4e013dab31ed3f96ca7160e7c0836506
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-28 14:13:07 +0000

    bower: update 1.8.4 bottle.

--- Formula/bower.rb
+++ Formula/bower.rb
@@ -9,9 +9,10 @@ class Bower < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "02a7d64987c14db303484c1dc329f485ad9596726038bf30667f9f28e995a772" => :high_sierra
-    sha256 "341e7e76f3536cb8b2ddbbcc4d2e96c004e76bced2d375a3c636914c0ec11fa2" => :sierra
-    sha256 "58a39bd911c39d6e1d319fb94d6073b91566ff73f361cf41a9ee5006c546050e" => :el_capitan
+    rebuild 1
+    sha256 "71091cd752679276938b3d522b3a3208028ac39d7567621c6be01d9ff8176208" => :mojave
+    sha256 "055539589006d7eeb83b1ce11bf54e120660790c5795fc73acf5842691984e6e" => :high_sierra
+    sha256 "aaaf74c6e92afb1cb2f2907a8e8380414915aac75a00e03c2356a6099b33520e" => :sierra
   end
 
   depends_on "node"
