c8481b9c2acb462cf2341a97a2ed858c5acaea3c
Johannes Pfeiffer <johannespfeiffer@netnexus.de>
2018-09-28 13:18:00 +0200

    jmeter 5.0
    Closes #32526.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/jmeter.rb
+++ Formula/jmeter.rb
@@ -1,8 +1,8 @@
 class Jmeter < Formula
   desc "Load testing and performance measurement application"
   homepage "https://jmeter.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=jmeter/binaries/apache-jmeter-4.0.tgz"
-  sha256 "845b8325726171a991cc13072275ec64b2ce4bbc6fa8e2aa350b2369e27e76b7"
+  url "https://www.apache.org/dyn/closer.cgi?path=jmeter/binaries/apache-jmeter-5.0.tgz"
+  sha256 "b29e9dc6351394a458fdb9270f5df5636175fe9c433bc86b8ee394bfcabfb142"
 
   bottle do
     cellar :any_skip_relocation
