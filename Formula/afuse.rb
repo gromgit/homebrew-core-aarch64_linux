af328f13b8705621df6f0f85db168ae301fd9cb4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-28 08:41:43 +0000

    afuse: update 0.4.1 bottle.

--- Formula/afuse.rb
+++ Formula/afuse.rb
@@ -6,6 +6,7 @@ class Afuse < Formula
 
   bottle do
     cellar :any
+    sha256 "d28d229c3bc7317681e538388a3d87df170aa56dc8e5e9ced6bf964e6fafba71" => :mojave
     sha256 "5596df8a8351206809f4b047e9d357e36273f5d505e531db3f14d320cf7eec28" => :high_sierra
     sha256 "900e55a47834181f518e87e7cbaaf0f3f078b0d40631ffccfc776e82c7c61f87" => :sierra
     sha256 "a4c0f86a179ca8c5d1e3977ff167dbcd1abff4ec1ee17fd5700a3fb602c781a3" => :el_capitan
