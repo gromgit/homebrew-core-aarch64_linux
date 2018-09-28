4c6b99b11044b0a3df8ea4f8aa01fc336dd0253d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-28 09:18:10 +0000

    s3-backer: update 1.5.0 bottle.

--- Formula/s3-backer.rb
+++ Formula/s3-backer.rb
@@ -6,6 +6,7 @@ class S3Backer < Formula
 
   bottle do
     cellar :any
+    sha256 "f75e20c85f2604d4f607628a0fe166e687d2d17b120b14a417b8ad47a9523531" => :mojave
     sha256 "2f3c95347ce448161e36012c2ec08c2ac3b2f4fcabb683b5018f0f8088560bdb" => :high_sierra
     sha256 "aa290ecca1c6d57252b80669f112eed4386f6b679ad92de4f3b7c5a0a575f54a" => :sierra
     sha256 "2c0b02a7f3d7bfd5902dafc64d54aba03dd2ab5f07441ad14089033146563793" => :el_capitan
