6ac0cbca9dda619f061b28f6801d2100c5cad026
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-28 15:13:01 +0000

    calicoctl: update 3.1.3 bottle.

--- Formula/calicoctl.rb
+++ Formula/calicoctl.rb
@@ -7,6 +7,7 @@ class Calicoctl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f16fc8a7934c3dd46552224353a1a36a5ed1fc9c5265a81e48f1dc4226646b9e" => :mojave
     sha256 "0a050b4fd951cd8cf7593a436fe99571d6e7ce325a2449a12d6168972a1c714c" => :high_sierra
     sha256 "1d8f80446c3395ea05dce58b3ff04f9ec9b5bafee6598c0c5704475e00f1786d" => :sierra
     sha256 "f3c800eb604d967120d6b26897b25a85b2e877d05502baaa17ecd1d8dbbdb24a" => :el_capitan
