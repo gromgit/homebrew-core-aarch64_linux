7ebc3cfb0b4558e5395d93100e9156f41a2e6efc
Chongyu Zhu <i@lembacon.com>
2018-09-29 00:29:04 +0800

    exiftool 11.11
    Closes #32536.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/exiftool.rb
+++ Formula/exiftool.rb
@@ -3,9 +3,9 @@ class Exiftool < Formula
   homepage "https://www.sno.phy.queensu.ca/~phil/exiftool/index.html"
   # Ensure release is tagged production before submitting.
   # https://www.sno.phy.queensu.ca/~phil/exiftool/history.html
-  url "https://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-11.01.tar.gz"
-  mirror "https://downloads.sourceforge.net/project/exiftool/Image-ExifTool-11.01.tar.gz"
-  sha256 "705fbf4a42d772c548afff4c64b3db3e95eb347e5b48acd66dd7d7712c19bc9c"
+  url "https://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-11.11.tar.gz"
+  mirror "https://downloads.sourceforge.net/project/exiftool/Image-ExifTool-11.11.tar.gz"
+  sha256 "e64eee75cdbc0944ea598bade44a2c100e7c940ce7d163cbbd087d2bd00cefeb"
 
   bottle do
     cellar :any_skip_relocation
