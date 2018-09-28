82be33a7d5587249902b30a36e30c8a2bd6ce94a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-28 09:58:46 +0000

    iamy: update 2.2.0 bottle.

--- Formula/iamy.rb
+++ Formula/iamy.rb
@@ -7,11 +7,9 @@ class Iamy < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5ef63fa4468700787b0e655453a503db2b949385c7efdbfe07073d04ae9f671a" => :mojave
-    sha256 "65aec3b7fda4554e1ae2d68a1f1828b237767d9d1b03e1a4010147dec671d07f" => :high_sierra
-    sha256 "0032e8af26d5f19e127680b3c2a46edf8acfcdcc43705ec28c59c29b5cb7cf8e" => :sierra
-    sha256 "e423ebfa60cb94e4af9d2bbe396c10791201f5be000da123981dbbce8bfca5fe" => :el_capitan
-    sha256 "7711c9a181026bbb9f662fa04b08da34cff6a3bc7744b5e7546e47bf1615b1b0" => :yosemite
+    sha256 "b53242a77b9f929dae108b63f5c98dacd8dbbb012c5b84aba2a32e3a568997dd" => :mojave
+    sha256 "47472140d1784e319eea2d203722d957b588655234e266a3dab26be9fb103598" => :high_sierra
+    sha256 "4d1e5b3b9bb838117293c18979370d395cd111f3c32f9b00030fbed7ae9c2f3c" => :sierra
   end
 
   depends_on "go" => :build
