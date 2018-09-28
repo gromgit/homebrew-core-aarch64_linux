7ea7f1516490b45fc5177207aaa070482059dc19
Scott Crooks <scott.crooks@gmail.com>
2018-09-28 11:07:57 +0200

    skaffold 0.15.0
    Closes #32522.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/skaffold.rb
+++ Formula/skaffold.rb
@@ -2,8 +2,8 @@ class Skaffold < Formula
   desc "Easy and Repeatable Kubernetes Development"
   homepage "https://github.com/GoogleContainerTools/skaffold"
   url "https://github.com/GoogleContainerTools/skaffold.git",
-      :tag => "v0.14.0",
-      :revision => "0f99b31ce724e971af347e9a6f9582f68b9ac250"
+      :tag => "v0.15.0",
+      :revision => "1b3f44b8952a1772f36d51a8f3568817e2569c87"
   head "https://github.com/GoogleContainerTools/skaffold.git"
 
   bottle do
