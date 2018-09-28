388cb1802fd65859f12528b8c6b94f1983874bf4
Emiel Wiedijk <me@aimileus.nl>
2018-09-28 12:52:53 +0200

    pass 1.7.3
    Closes #32525.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/pass.rb
+++ Formula/pass.rb
@@ -1,8 +1,8 @@
 class Pass < Formula
   desc "Password manager"
   homepage "https://www.passwordstore.org/"
-  url "https://git.zx2c4.com/password-store/snapshot/password-store-1.7.2.tar.xz"
-  sha256 "4768c5e1965c4d2aeb28818681e484fb105b6f46cbd75a97608615c4ec6980ea"
+  url "https://git.zx2c4.com/password-store/snapshot/password-store-1.7.3.tar.xz"
+  sha256 "2b6c65846ebace9a15a118503dcd31b6440949a30d3b5291dfb5b1615b99a3f4"
   head "https://git.zx2c4.com/password-store", :using => :git
 
   bottle do
