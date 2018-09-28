8ce498aad22a72701f382d094e84ed67fef5cc00
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-28 09:11:24 +0000

    encfs: update 1.9.5 bottle.

--- Formula/encfs.rb
+++ Formula/encfs.rb
@@ -6,6 +6,7 @@ class Encfs < Formula
   head "https://github.com/vgough/encfs.git"
 
   bottle do
+    sha256 "15cb157add4120367495039e7afd8a14f7a3177a5bae0d90f9a1a19ae15f47da" => :mojave
     sha256 "6fb4502bafeefe1e2a92f3a0b2dc16aa246344fe781b2b080d8895ee9f4d631d" => :high_sierra
     sha256 "f2d430dea3a7794c14d865a4ae9426f44254dfc9f84ece48b6699fd64a5305d0" => :sierra
     sha256 "e41a174da5447d3381a8fe39b1740d633c53c5819545ec53878743aad925b6b0" => :el_capitan
