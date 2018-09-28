84828dc2ac020317f406494fd521d9d8e9a30ffd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-28 09:18:09 +0000

    securefs: update 0.8.2 bottle.

--- Formula/securefs.rb
+++ Formula/securefs.rb
@@ -7,6 +7,7 @@ class Securefs < Formula
 
   bottle do
     cellar :any
+    sha256 "df79931139f2fe9ad9714c4304b9c2c5d3322b53d7e187f6118fa709097f7f00" => :mojave
     sha256 "d851b6a0987ac11b3ff5a6ede8b88bfa9e09251ef1fdd76237ad7e9910688242" => :high_sierra
     sha256 "13d67e18b133621ddb49ddc5b34caff453aec85d95607d2ab83f03cdf0c79b76" => :sierra
     sha256 "f42c0d8e15ddcd2212235b4954e8ce0896e8f3fc1f07c388cfcf49dca96888f3" => :el_capitan
