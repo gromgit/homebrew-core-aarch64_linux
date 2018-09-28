17aea8b02aaf7b2713ac024036bdc0fb12e479dc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-28 09:11:20 +0000

    dislocker: update 0.7.1_3 bottle.

--- Formula/dislocker.rb
+++ Formula/dislocker.rb
@@ -6,6 +6,7 @@ class Dislocker < Formula
   revision 3
 
   bottle do
+    sha256 "b3ea749c44b20b4b20a5a5e74079dd4c71b192db4e070f7d445b6a5eebdeadeb" => :mojave
     sha256 "9f591c911314d86b773311386f3ce327af3b9f01bb1d4539f07841380aa2249f" => :high_sierra
     sha256 "731f8224ccd50671c0b12795e3d95f5b96868d0d76373cc270c5d6fc1a86c6c4" => :sierra
     sha256 "c99aecc4388117f11cee11d6333137aea5a1359a229149b55200e06a2e836914" => :el_capitan
