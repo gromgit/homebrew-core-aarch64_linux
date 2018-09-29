283b1e64e27a2d8639e189ee2de678fe6ab8dd8c
sergey <sergey.chelombitko@gmail.com>
2018-09-29 17:35:49 +0300

    bundletool 0.6.0 (new formula)

---
+++ Formula/bundletool.rb
@@ -0,0 +1,33 @@
+class Bundletool < Formula
+  desc "Command-line tool to manipulate Android App Bundles"
+  homepage "https://github.com/google/bundletool"
+  url "https://github.com/google/bundletool/releases/download/0.6.0/bundletool-all-0.6.0.jar"
+  sha256 "79fcfd992d87a350d5f4e38c56f3daa09897b069f1f7ba82ec028a5249f216a8"
+
+  bottle :unneeded
+  depends_on :java => "1.8+"
+
+  resource "bundle" do
+    url "https://gist.githubusercontent.com/raw/ca85ede7ac072a44f48c658be55ff0d3/sample.aab"
+    sha256 "aac71ad62e1f20dd19b80eba5da5cb5e589df40922f288fb6a4b37a62eba27ef"
+  end
+
+  def install
+    libexec.install "bundletool-all-#{version}.jar"
+    bin.write_jar_script libexec/"bundletool-all-#{version}.jar", "bundletool"
+  end
+
+  test do
+    resource("bundle").stage do
+      expected = <<~EOS
+        App Bundle information
+        ------------
+        Modules:
+        	Module: base
+        		File: dex/classes.dex
+      EOS
+
+      assert_equal expected, shell_output("#{bin}/bundletool validate --bundle sample.aab")
+    end
+  end
+end
