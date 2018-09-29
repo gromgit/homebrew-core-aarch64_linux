826d3d44a8ffe0635031526aab7cbdc005f58387
Stefan Sundin <git@stefansundin.com>
2018-09-28 20:13:36 -0700

    jsonnet: update homepage
    Closes #32547.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/jsonnet.rb
+++ Formula/jsonnet.rb
@@ -1,6 +1,6 @@
 class Jsonnet < Formula
   desc "Domain specific configuration language for defining JSON data"
-  homepage "https://google.github.io/jsonnet/doc/"
+  homepage "https://jsonnet.org/"
   url "https://github.com/google/jsonnet/archive/v0.11.2.tar.gz"
   sha256 "c7c33f159a9391e90ab646b3b5fd671dab356d8563dc447ee824ecd77f4609f8"
 
