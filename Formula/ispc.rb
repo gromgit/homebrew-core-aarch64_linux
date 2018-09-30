ca6406087d9dd3883b5539c3b31b531cdc3830a4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-30 11:49:40 +0000

    ispc: update 1.9.2_1 bottle.

--- Formula/ispc.rb
+++ Formula/ispc.rb
@@ -7,6 +7,7 @@ class Ispc < Formula
 
   bottle do
     cellar :any
+    sha256 "69c12c8e3a068b9d3c1361ad5af915316875d50ffd91eeca03d54de81692e4fa" => :mojave
     sha256 "3aed8cf86cb9cb7329b5fcb5cb81fae74d2d5fb61bc65b5e6d36525b0f7ae12c" => :high_sierra
     sha256 "21ecdffa7a0594da5882b01c83b60d17e97d588fe5f676d87fdc3d0491039592" => :sierra
     sha256 "403bd415fab378038524a4981c4ac21aac6bfa72b77348a454d89ef27b554a4f" => :el_capitan
