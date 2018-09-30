546cda60eab063eacd67af4523335df0ba06f411
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-30 13:26:14 +0000

    le: update 1.16.5 bottle.

--- Formula/le.rb
+++ Formula/le.rb
@@ -5,6 +5,7 @@ class Le < Formula
   sha256 "258d586f83e8abd55144dc3f09d9ddaf6ef55e8a90543fdb0932fb77d089dd78"
 
   bottle do
+    sha256 "8f923693a632a447888ac16d766e2318aed6af1400663de3a1ad9f27da850109" => :mojave
     sha256 "614516096f3fa5bb72f68fa747e611401098239d72c7e31274b1fdf41a4d0c59" => :high_sierra
     sha256 "bb670b11e7bf2813cf89cf35ad7e04b67c330cef6c1c07ab5b65fbe1879af67a" => :sierra
     sha256 "285e9bf7c3debedb6286ca34971d939c673a35d4553817fdc48dc915e649ba0c" => :el_capitan
