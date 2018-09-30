e03d498a928df6ffdc2a3c839e151fc03dfcf3d7
FX Coudert <fxcoudert@gmail.com>
2018-09-30 14:06:19 +0200

    puzzles 20180924
    Closes #32569.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/puzzles.rb
+++ Formula/puzzles.rb
@@ -2,9 +2,9 @@ class Puzzles < Formula
   desc "Collection of one-player puzzle games"
   homepage "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/"
   # Extract https://www.chiark.greenend.org.uk/~sgtatham/puzzles/puzzles.tar.gz to get the version number
-  url "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/puzzles-20180725.1db5961.tar.gz"
-  version "20180725"
-  sha256 "8be48ff12294686a82f62f4cacd5d278a47be352cd56586a8ebfa893197d92b6"
+  url "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/puzzles-20180924.d8d5064.tar.gz"
+  version "20180924"
+  sha256 "e040e965c52dd7b60603737e2530dda654b2d25f79d2a0a4ee06ab757e62ac6d"
   head "https://git.tartarus.org/simon/puzzles.git"
 
   bottle do
@@ -17,9 +17,8 @@ class Puzzles < Formula
   depends_on "halibut"
 
   def install
-    # Prevent "lipo: Puzzles.i386.bin and Puzzles.x86_64.bin have the same
-    # architectures (x86_64) and can't be in the same fat output file"
-    ENV.permit_arch_flags
+    # Do not build for i386
+    inreplace "mkfiles.pl", /@osxarchs = .*/, "@osxarchs = ('x86_64');"
 
     system "perl", "mkfiles.pl"
     system "make", "-d", "-f", "Makefile.osx", "all"
