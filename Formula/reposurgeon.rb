a953a68e5c60ac4045ff83c6cc66145531617b83
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-30 14:28:34 +0000

    reposurgeon: update 3.44 bottle.

--- Formula/reposurgeon.rb
+++ Formula/reposurgeon.rb
@@ -8,6 +8,7 @@ class Reposurgeon < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9666a908f723015481c74de2aa895ff09f55a8a66cda57317c681593d0cf87f2" => :mojave
     sha256 "8dae663f9138b383b4fdbe1f8a66b87cbb05f518ae929441ff46707b5bade762" => :high_sierra
     sha256 "2dc5be9011fa4d7aad8f5ecc6ff125876a61769341d15661f93513e5603a8733" => :sierra
     sha256 "d0ff9f9c06bd124bc9e8dc31bf59eaae1a28b124234d4b359ad719144cdff9ab" => :el_capitan
