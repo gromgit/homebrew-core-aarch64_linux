1c8fb3ec3779fd7e0e1181852e4cf108d24b0f27
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-30 14:28:34 +0000

    pcapplusplus: update 18.08 bottle.

--- Formula/pcapplusplus.rb
+++ Formula/pcapplusplus.rb
@@ -6,6 +6,7 @@ class Pcapplusplus < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ed81e6545d41b9fe6e2b5712c0ad6210a31346144912d8503ff7c06ffd91000f" => :mojave
     sha256 "2e9f6a851da52dd885e284b0393392dfbb2dec52b56517a553087fa67a608ddd" => :high_sierra
     sha256 "abdd89390998f292ed04f518471e815767285bd004165933de082d753dc44af1" => :sierra
     sha256 "bf6c8b35cc86e7acd5ddbed6ac0051a175e3329befa670fa432c6f4dc8eb6575" => :el_capitan
