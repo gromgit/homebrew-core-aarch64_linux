08ce0a8bee493f703a029d27c865c4188feb28da
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-01 14:25:58 +0000

    opentracing-cpp: add 1.5.0 bottle.

--- Formula/opentracing-cpp.rb
+++ Formula/opentracing-cpp.rb
@@ -4,6 +4,13 @@ class OpentracingCpp < Formula
   url "https://github.com/opentracing/opentracing-cpp/archive/v1.5.0.tar.gz"
   sha256 "4455ca507936bc4b658ded10a90d8ebbbd61c58f06207be565a4ffdc885687b5"
 
+  bottle do
+    cellar :any
+    sha256 "77e90986d743b410c54ba63a30cf0741eb8f2faa68392a5c726c613c6113695c" => :mojave
+    sha256 "e88447ef67b65da415b9c6b747eb38e18a6ef7abc268334bd5b561d825b7eedb" => :high_sierra
+    sha256 "5466485c5f18de7452c2d4b043e0dcf0c23f3e0fa9fc93d8d84177b38eae8c34" => :sierra
+  end
+
   depends_on "cmake" => :build
 
   def install
