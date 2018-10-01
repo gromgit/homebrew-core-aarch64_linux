53d1f029cb5694a41640cff05a3791e5ffac37c6
William Cheng <wing328hk@gmail.com>
2018-10-02 01:54:33 +0800

    openapi-generator 3.3.0

--- Formula/openapi-generator.rb
+++ Formula/openapi-generator.rb
@@ -1,8 +1,8 @@
 class OpenapiGenerator < Formula
   desc "Generate clients, server & docs from an OpenAPI spec (v2, v3)"
   homepage "https://openapi-generator.tech/"
-  url "https://search.maven.org/remotecontent?filepath=org/openapitools/openapi-generator-cli/3.2.3/openapi-generator-cli-3.2.3.jar"
-  sha256 "8579b4f1d02c78a4736d451963b3417a318e35dbe0032e192385ad7e26ab92a6"
+  url "https://search.maven.org/remotecontent?filepath=org/openapitools/openapi-generator-cli/3.3.0/openapi-generator-cli-3.3.0.jar"
+  sha256 "7ff681aa50617778e3e52e05afbcfe21166dd08b4d866fc0d1e2246b85aaef3c"
 
   head do
     url "https://github.com/OpenAPITools/openapi-generator.git"
