c623ee184461c962a7db33116fcb4cdf7d3eeb5a
Viktor Szakats <commit@vszakats.net>
2018-10-01 21:41:18 +0000

    neatvi: secure url(s)

--- Formula/neatvi.rb
+++ Formula/neatvi.rb
@@ -1,10 +1,10 @@
 class Neatvi < Formula
-  desc "ex/vi clone for editing bidirectional uft-8 text"
-  homepage "http://repo.or.cz/w/neatvi.git"
-  url "http://repo.or.cz/neatvi.git",
+  desc "ex/vi clone for editing bidirectional utf-8 text"
+  homepage "https://repo.or.cz/neatvi.git"
+  url "https://repo.or.cz/neatvi.git",
       :tag => "06",
       :revision => "5ed4bbc7f12686bb480ab8b2b05c94e12b1c71d8"
-  head "http://repo.or.cz/neatvi.git"
+  head "https://repo.or.cz/neatvi.git"
 
   bottle do
     cellar :any_skip_relocation
