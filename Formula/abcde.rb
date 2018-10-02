58677724350a4651471c501989e15e204c8cc404
FX Coudert <fxcoudert@gmail.com>
2018-10-02 13:53:22 +0200

    abcde: remove options

--- Formula/abcde.rb
+++ Formula/abcde.rb
@@ -3,6 +3,7 @@ class Abcde < Formula
   homepage "https://abcde.einval.com"
   url "https://abcde.einval.com/download/abcde-2.9.2.tar.gz"
   sha256 "34356c6ea4cc39b33c807261bfdf8e8da8905b2ed50313147c78b283eef6858d"
+  revision 1
   head "https://git.einval.com/git/abcde.git"
 
   bottle do
@@ -15,12 +16,12 @@ class Abcde < Formula
 
   depends_on "cd-discid"
   depends_on "cdrtools"
+  depends_on "flac"
+  depends_on "glyr"
   depends_on "id3v2"
+  depends_on "lame"
   depends_on "mkcue"
-  depends_on "flac" => :optional
-  depends_on "glyr" => :optional
-  depends_on "lame" => :optional
-  depends_on "vorbis-tools" => :optional
+  depends_on "vorbis-tools"
 
   def install
     system "make", "install", "prefix=#{prefix}", "sysconfdir=#{etc}"
