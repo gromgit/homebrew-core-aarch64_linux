bebb614d6db6fa781ecf423ddfd1c3109b8b867c
Viktor Szakats <commit@vszakats.net>
2018-10-02 15:24:29 +0000

    tcsh: replace broken homepage with archive link

--- Formula/tcsh.rb
+++ Formula/tcsh.rb
@@ -1,6 +1,6 @@
 class Tcsh < Formula
   desc "Enhanced, fully compatible version of the Berkeley C shell"
-  homepage "http://www.tcsh.org/"
+  homepage "https://web.archive.org/web/20170609182511/www.tcsh.org/Home"
   url "ftp://ftp.astron.com/pub/tcsh/tcsh-6.20.00.tar.gz"
   mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/tcsh/tcsh-6.20.00.tar.gz"
   sha256 "b89de7064ab54dac454a266cfe5d8bf66940cb5ed048d0c30674ea62e7ecef9d"
