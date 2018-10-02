8e3ee6c2c2f3d492cbc45df81a482cc9e3b66e30
Viktor Szakats <commit@vszakats.net>
2018-10-02 15:15:17 +0000

    udunits: add secure mirror(s)

--- Formula/udunits.rb
+++ Formula/udunits.rb
@@ -2,6 +2,7 @@ class Udunits < Formula
   desc "Unidata unit conversion library"
   homepage "https://www.unidata.ucar.edu/software/udunits/"
   url "ftp://ftp.unidata.ucar.edu/pub/udunits/udunits-2.2.26.tar.gz"
+  mirror "https://fossies.org/linux/privat/udunits-2.2.26.tar.gz"
   sha256 "368f4869c9c7d50d2920fa8c58654124e9ed0d8d2a8c714a9d7fdadc08c7356d"
 
   bottle do
