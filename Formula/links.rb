a8d7deb19cb9576c5303b460bf8fee666804f1de
FX Coudert <fxcoudert@gmail.com>
2018-10-02 13:53:46 +0200

    links: remove options

--- Formula/links.rb
+++ Formula/links.rb
@@ -3,6 +3,7 @@ class Links < Formula
   homepage "http://links.twibright.com/"
   url "http://links.twibright.com/download/links-2.17.tar.bz2"
   sha256 "d8389763784a531acf7f18f93dd0324563bba2f5fa3df203f27d22cefe7a0236"
+  revision 1
 
   bottle do
     cellar :any
@@ -13,10 +14,10 @@ class Links < Formula
   end
 
   depends_on "pkg-config" => :build
+  depends_on "jpeg"
+  depends_on "librsvg"
+  depends_on "libtiff"
   depends_on "openssl"
-  depends_on "jpeg" => :optional
-  depends_on "librsvg" => :optional
-  depends_on "libtiff" => :optional
   depends_on :x11 => :optional
 
   def install
@@ -30,9 +31,6 @@ class Links < Formula
     ]
 
     args << "--enable-graphics" if build.with? "x11"
-    args << "--without-libtiff" if build.without? "libtiff"
-    args << "--without-libjpeg" if build.without? "jpeg"
-    args << "--without-librsvg" if build.without? "librsvg"
 
     system "./configure", *args
     system "make", "install"
