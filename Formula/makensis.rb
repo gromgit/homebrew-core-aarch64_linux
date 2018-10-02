ed69b8290d92b313543e7ff75cc6820a7d0c683d
Jan T. Sott <jan@idleberg.com>
2018-10-02 08:39:05 +0200

    makensis: simplify build (#31891)

--- Formula/makensis.rb
+++ Formula/makensis.rb
@@ -12,15 +12,6 @@ class Makensis < Formula
     sha256 "f4516cec938568eb2bea2b162247a10cbd68dedd85c439f5d77170dbc7c5b81b" => :el_capitan
   end
 
-  # From https://nsis.sourceforge.io/Special_Builds#Advanced_logging
-  option "with-advanced-logging", "Enable advanced logging of all installer actions"
-
-  # Build makensis so installers can handle strings > 1024 characters
-  # From https://nsis.sourceforge.io/Special_Builds#Large_strings
-  # Upstream RFE to make this default the default behavior is
-  # https://sourceforge.net/p/nsis/feature-requests/542/
-  option "with-large-strings", "Enable strings up to 8192 characters instead of default 1024"
-
   depends_on "mingw-w64" => :build
   depends_on "scons" => :build
 
@@ -35,22 +26,22 @@ class Makensis < Formula
     sha256 "a03fd15af45e91964fb980a30422073bc3f3f58683e9fdafadad3f7db10762b1"
   end
 
-  # scons appears to have no builtin way to override the compiler selection,
-  # and the only options supported on macOS are 'gcc' and 'g++'.
-  # Use the right compiler by forcibly altering the scons config to set these
-  patch :DATA
-
   def install
     # requires zlib (win32) to build utils
     resource("zlib-win32").stage do
       @zlib_path = Dir.pwd
     end
 
-    # Don't strip, see https://github.com/Homebrew/homebrew/issues/28718
-    args = ["STRIP=0", "ZLIB_W32=#{@zlib_path}", "SKIPUTILS=NSIS Menu",
-            "PREFIX_DOC=#{share}/nsis/Docs", "VERSION=#{version}"]
-    args << "NSIS_CONFIG_LOG=yes" if build.with? "advanced-logging"
-    args << "NSIS_MAX_STRLEN=8192" if build.with? "large-strings"
+    args = [
+      "CC=#{ENV.cc}",
+      "CXX=#{ENV.cxx}",
+      "PREFIX_DOC=#{share}/nsis/Docs",
+      "SKIPUTILS=NSIS Menu",
+      # Don't strip, see https://github.com/Homebrew/homebrew/issues/28718
+      "STRIP=0",
+      "VERSION=#{version}",
+      "ZLIB_W32=#{@zlib_path}",
+    ]
     scons "makensis", *args
     bin.install "build/urelease/makensis/makensis"
     (share/"nsis").install resource("nsis")
@@ -69,24 +60,3 @@ class Makensis < Formula
     system "#{bin}/makensis", "#{testpath}/test.nsi"
   end
 end
-
-__END__
-diff --git a/SCons/config.py b/SCons/config.py
-index a344456..37c575b 100755
---- a/SCons/config.py
-+++ b/SCons/config.py
-@@ -1,3 +1,5 @@
-+import os
-+
- Import('defenv')
- 
- ### Configuration options
-@@ -440,6 +442,9 @@ Help(cfg.GenerateHelpText(defenv))
- env = Environment()
- cfg.Update(env)
- 
-+defenv['CC'] = os.environ['CC']
-+defenv['CXX'] = os.environ['CXX']
-+
- def AddValuedDefine(define):
-   defenv.Append(NSIS_CPPDEFINES = [(define, env[define])])
