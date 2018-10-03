5a4dab94fd7c8d82501188d74cfbefa244f6c3fe
commitay <commitay@users.noreply.github.com>
2018-10-04 08:51:01 +1000

    container-diff 0.12.0
    Closes #32678.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/container-diff.rb
+++ Formula/container-diff.rb
@@ -1,8 +1,8 @@
 class ContainerDiff < Formula
   desc "Diff your Docker containers"
   homepage "https://github.com/GoogleContainerTools/container-diff"
-  url "https://github.com/GoogleContainerTools/container-diff/archive/v0.11.0.tar.gz"
-  sha256 "b86361c6cd091c0d25809743f7ca883b856438a20b9e100c1925638c8296698d"
+  url "https://github.com/GoogleContainerTools/container-diff/archive/v0.12.0.tar.gz"
+  sha256 "3f457c62073fac50493e7e237c247b30e1d1209ffb4c5b703def1a07a7822376"
 
   bottle do
     cellar :any_skip_relocation
