7b453a969ac1f14126a5b0b60758c37a2703772a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-03 22:18:23 +0000

    kibana: update 6.4.2 bottle.

--- Formula/kibana.rb
+++ Formula/kibana.rb
@@ -7,10 +7,9 @@ class Kibana < Formula
   head "https://github.com/elastic/kibana.git"
 
   bottle do
-    sha256 "4592014baeec58962946a94deaf108c9c0c58670205ce0930aacd4f9e2b72aee" => :mojave
-    sha256 "0bf531fe125f35eaba50902916e3c469220f66a402c83c6802f9cfd90e85b209" => :high_sierra
-    sha256 "75b3a83605396fe8bb347fca5c9bd00279e8fa39b65e837994db309644c329a7" => :sierra
-    sha256 "99bc5bb7e1e01216dfbf49357794fb87423ef27eee539050bd3901c955e4e021" => :el_capitan
+    sha256 "41dd827f2533381d056a116ae85dacba36654651739304d7b9af4d44840e4034" => :mojave
+    sha256 "32b066460d9edaac0b4c2c1b8efe911c960c5ef81444643d025da6da0a567d81" => :high_sierra
+    sha256 "b6a1fd07e6d5db71491e2a37ee402b86990198acb78718865594c795c04895a1" => :sierra
   end
 
   resource "node" do
