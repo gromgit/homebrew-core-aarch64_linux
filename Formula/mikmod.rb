69fdc7585f266c9724fd1485026d1244b357635d
Viktor Szakats <commit@vszakats.net>
2018-10-03 12:25:44 +0000

    mikmod: secure url(s)

--- Formula/mikmod.rb
+++ Formula/mikmod.rb
@@ -1,6 +1,6 @@
 class Mikmod < Formula
   desc "Portable tracked music player"
-  homepage "http://mikmod.raphnet.net/"
+  homepage "https://mikmod.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/mikmod/mikmod/3.2.8/mikmod-3.2.8.tar.gz"
   sha256 "dbb01bc36797ce25ffcab2b3bf625537b85b42534344e1808236ca612fbaa4cc"
 
