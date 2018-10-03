02b50e1fb414f2f0dba0bdef7450aea9584e0e70
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-03 13:37:41 +0000

    cgal: update 4.13 bottle.

--- Formula/cgal.rb
+++ Formula/cgal.rb
@@ -6,10 +6,9 @@ class Cgal < Formula
 
   bottle do
     cellar :any
-    sha256 "c8645dcc07c0c2be479d8521a835b824cc25a05a3be3857c8ab3c2240af265b3" => :mojave
-    sha256 "4996b3af224bece9348288878c47f4b1a6de780311c711a044ed85ee25b21d47" => :high_sierra
-    sha256 "b21b64f5f7806d599edf51dc030ec367aa420160d505e6ba8abb1752bacc9321" => :sierra
-    sha256 "ccbaa0179fe268ce00bb467759c29e626ff606b537963a6ee099980789c9f519" => :el_capitan
+    sha256 "d969597f4c3fb993bf19363e4e92f0f74427e25f6f855fa28d1f9792084aea9b" => :mojave
+    sha256 "e6978f966bdd5a050f80185d8e506a38016a8adad6010b039b9ef42558ed14eb" => :high_sierra
+    sha256 "349890c9c6f40272b3173c15412701f6588003047e5ebc3e0cd2a03c870ba83d" => :sierra
   end
 
   option "with-qt", "Build ImageIO and Qt components of CGAL"
