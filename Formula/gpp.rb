6061e523ee55bcb1ed98abe92c53728f9683327a
Viktor Szakats <commit@vszakats.net>
2018-10-03 12:25:43 +0000

    gpp: secure url(s)

--- Formula/gpp.rb
+++ Formula/gpp.rb
@@ -1,6 +1,6 @@
 class Gpp < Formula
   desc "General-purpose preprocessor with customizable syntax"
-  homepage "http://en.nothingisreal.com/wiki/GPP"
+  homepage "https://logological.org/gpp"
   url "https://files.nothingisreal.com/software/gpp/gpp-2.25.tar.bz2"
   sha256 "16ba9329208f587f96172f951ad3d24a81afea6a5b7836fe87955726eacdd19f"
 
