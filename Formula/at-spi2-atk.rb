a79d5b71b135ba66089c75ee056086643a626858
Viktor Szakats <commit@vszakats.net>
2018-10-03 12:25:43 +0000

    at-spi2-atk: secure url(s)

--- Formula/at-spi2-atk.rb
+++ Formula/at-spi2-atk.rb
@@ -1,6 +1,6 @@
 class AtSpi2Atk < Formula
   desc "Accessibility Toolkit GTK+ module"
-  homepage "http://a11y.org"
+  homepage "https://wiki.linuxfoundation.org/accessibility/"
   url "https://download.gnome.org/sources/at-spi2-atk/2.30/at-spi2-atk-2.30.0.tar.xz"
   sha256 "e2e1571004ea7b105c969473ce455a95be4038fb2541471714aeb33a26da8a9a"
 
