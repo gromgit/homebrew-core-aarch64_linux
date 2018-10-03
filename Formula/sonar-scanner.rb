57b804fcc5d5e776c2f5ccd8a79c626b5e743928
Xavier Bourguignon <xavier.bourguignon@sonarsource.com>
2018-10-03 11:46:17 +0200

    sonar-scanner: download url is now binaries.sonarsource.com

--- Formula/sonar-scanner.rb
+++ Formula/sonar-scanner.rb
@@ -1,7 +1,7 @@
 class SonarScanner < Formula
   desc "Launcher to analyze a project with SonarQube"
   homepage "https://docs.sonarqube.org/display/SCAN/Analyzing+with+SonarQube+Scanner"
-  url "https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.2.0.1227.zip"
+  url "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.2.0.1227.zip"
   sha256 "f0e05102a3e98aceb141577c08896c49e3bff9520d1e2f75a688a0ce0d099bc0"
   head "https://github.com/SonarSource/sonar-scanner-cli.git"
 
