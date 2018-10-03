3bceae3ba10334345a6f14b7e3777080c00a179d
Roger Sheen <roger@infotexture.net>
2018-10-03 18:51:01 +0200

    dita-ot 3.1.3 (#32664)

--- Formula/dita-ot.rb
+++ Formula/dita-ot.rb
@@ -1,16 +1,15 @@
 class DitaOt < Formula
   desc "DITA Open Toolkit is an implementation of the OASIS DITA specification"
   homepage "https://www.dita-ot.org/"
-  url "https://github.com/dita-ot/dita-ot/releases/download/3.1.2/dita-ot-3.1.2.zip"
-  sha256 "0f66f45cb6a3de2e2b7a463da15486b6c37c0d6b609052fcd517f19c0490e5e7"
+  url "https://github.com/dita-ot/dita-ot/releases/download/3.1.3/dita-ot-3.1.3.zip"
+  sha256 "f1a6a52f459fb7743a6d0a5db2e7fbdd2b294910a7aa3537edd81a6d2cf993c4"
 
   bottle :unneeded
 
   depends_on :java => "1.8+"
 
   def install
-    rm_f Dir["bin/*.bat", "startcmd.*"]
-    rm_rf "temp"
+    rm_f Dir["bin/*.bat", "config/env.bat", "startcmd.*"]
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/dita"
   end
