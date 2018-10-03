c774db44013931a2f69885d0e117d739d2fce58b
Clement Escoffier <clement.escoffier@gmail.com>
2018-10-03 17:16:03 +0200

    vert.x 3.5.4 (#32663)

--- Formula/vert.x.rb
+++ Formula/vert.x.rb
@@ -1,8 +1,8 @@
 class VertX < Formula
   desc "Toolkit for building reactive applications on the JVM"
   homepage "https://vertx.io/"
-  url "https://dl.bintray.com/vertx/downloads/vert.x-3.5.3-full.tar.gz"
-  sha256 "10562ad91a3ccaa9a975e8e52d1f782c0aed6ba402e53d24d50686b1cbdb535c"
+  url "https://dl.bintray.com/vertx/downloads/vert.x-3.5.4-full.tar.gz"
+  sha256 "9a23895f45c5951c53368d3b5b511d760183617aff76e1693d359a5e928220a2"
 
   bottle :unneeded
   depends_on :java => "1.8+"
