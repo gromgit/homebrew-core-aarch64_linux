02f671779a5b7e0b4d0726d782fb5197f19514c3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-03 07:13:30 +0000

    tdlib: add 1.3.0 bottle.

--- Formula/tdlib.rb
+++ Formula/tdlib.rb
@@ -4,6 +4,13 @@ class Tdlib < Formula
   url "https://github.com/tdlib/td/archive/v1.3.0.tar.gz"
   sha256 "2953fe75027ac531248b359123aa4812666377ac874c1db506fa74176f2d2338"
 
+  bottle do
+    cellar :any
+    sha256 "b4814dca4a43f82588ce6fbe98aba1aea02b030a979deb68b42c4330c69ad282" => :mojave
+    sha256 "45114d88ad51f18337f09debc02165757349811c1022dd0849ef1adf74a9d40f" => :high_sierra
+    sha256 "e5fec3448dccf46ab20db066d2699cdfb79beb62592f57ff59f7520ed02103ec" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "gperf"
   depends_on "openssl"
