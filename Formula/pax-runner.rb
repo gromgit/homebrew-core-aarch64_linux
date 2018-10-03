e41fcc05b08506f1c02b1209f0401eb9c7ae9c83
Viktor Szakats <commit@vszakats.net>
2018-10-03 12:25:44 +0000

    pax-runner: secure url(s)

--- Formula/pax-runner.rb
+++ Formula/pax-runner.rb
@@ -1,6 +1,6 @@
 class PaxRunner < Formula
   desc "Tool to provision OSGi bundles"
-  homepage "http://team.ops4j.org"
+  homepage "https://ops4j1.jira.com/"
   url "https://search.maven.org/remotecontent?filepath=org/ops4j/pax/runner/pax-runner-assembly/1.8.6/pax-runner-assembly-1.8.6-jdk15.tar.gz"
   version "1.8.6"
   sha256 "42a650efdedcb48dca89f3e4272a9e2e1dcc6bc84570dbb176b5e578ca1ce2d4"
