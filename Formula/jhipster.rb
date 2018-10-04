12f13acd8687b92efbf21cb486ac1d29a4fbfa57
Julien Dubois <julien.dubois@gmail.com>
2018-10-04 22:05:37 +0200

    jhipster 5.4.1
    Closes #32718.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/jhipster.rb
+++ Formula/jhipster.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Jhipster < Formula
   desc "Generate, develop and deploy Spring Boot + Angular/React applications"
   homepage "https://www.jhipster.tech/"
-  url "https://registry.npmjs.org/generator-jhipster/-/generator-jhipster-5.4.0.tgz"
-  sha256 "fa0fb732735c155c66199cdfea2a030feb8b7c5af2e240e4448a31521caf090d"
+  url "https://registry.npmjs.org/generator-jhipster/-/generator-jhipster-5.4.1.tgz"
+  sha256 "ea97c06f4f6e85937a0879116f6e3c9add4907b639d774159004e49b23ff552d"
 
   bottle do
     cellar :any_skip_relocation
