9ec0b941328881816b9bb81bac8b8f66ec609cd2
dmofot@gmail.com <dmofot@gmail.com>
2018-10-04 10:05:04 -0400

    shpotify 2.0.3
    Closes #32703.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/shpotify.rb
+++ Formula/shpotify.rb
@@ -1,8 +1,8 @@
 class Shpotify < Formula
   desc "Command-line interface for Spotify on a Mac"
   homepage "https://harishnarayanan.org/projects/shpotify/"
-  url "https://github.com/hnarayanan/shpotify/archive/2.0.2.tar.gz"
-  sha256 "29d420311569f9c63e43cb9bd499c4975724e6c13e47e317be1dd44c8bbd16a6"
+  url "https://github.com/hnarayanan/shpotify/archive/2.0.3.tar.gz"
+  sha256 "d3e51719fb039ad70a16b78994ded68eaa991b4328df0f5d8b16a6e87393cae3"
   head "https://github.com/hnarayanan/shpotify.git"
 
   bottle :unneeded
