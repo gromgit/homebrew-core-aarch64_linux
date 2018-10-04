0a7d8562c3c6dbdab6b7a079810e3b41736e2802
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-04 10:06:37 +0000

    ldc: update 1.11.0_1 bottle.

--- Formula/ldc.rb
+++ Formula/ldc.rb
@@ -14,10 +14,9 @@ class Ldc < Formula
   end
 
   bottle do
-    sha256 "2883f5d1da9d605b0e2cd1b6dc710f986ac618dcdc5c440f95cf8c495c1eefb2" => :mojave
-    sha256 "0995bbd559274edf5ca88e702f0eca2dadec42804f5e7a11d6708447474c3663" => :high_sierra
-    sha256 "6e0fd2df7225ce4595ce4bb0003ceff4d0d63aef713d2a283192216467bd4696" => :sierra
-    sha256 "5d0dcb4e222e1586d4d3d9cbb9a106f70d07d150f64e73cb6daa864e815229af" => :el_capitan
+    sha256 "614747501f8c18c48be99a35cad7c8d0cc387d23e34f061d6f372a0e0f9ce581" => :mojave
+    sha256 "ab5b1cb9864886ad0f56ce5116c271042912724aa609004e7fbf52616c944a4a" => :high_sierra
+    sha256 "fbc84d197eed292519c5dd4f7fd5ed90c116cf5ec8753d5488533149c007f6c7" => :sierra
   end
 
   head do
