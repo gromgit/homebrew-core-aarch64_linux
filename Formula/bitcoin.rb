a82fb034c8c1a2c2c9916bbca7de0a7a8199ad8e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-04 18:36:44 +0000

    bitcoin: update 0.17.0 bottle.

--- Formula/bitcoin.rb
+++ Formula/bitcoin.rb
@@ -6,10 +6,8 @@ class Bitcoin < Formula
 
   bottle do
     cellar :any
-    sha256 "0036ef31623df2e23673c4f7ddcb8a10a6f39c14d1f37251d0afb350cfcff9ad" => :mojave
-    sha256 "b5dd1e541138711af6c445778c015d7d0fd5c52f47922b0e4c09eda9722a2f07" => :high_sierra
-    sha256 "514c648b977a5eeae33576b234a579bf503549b00c1c618923e7a6af7516375c" => :sierra
-    sha256 "d16b0be096d586e62d91e13b29231bc5a498bda28f84e3c18cb0bcee6a3fa88d" => :el_capitan
+    sha256 "9706142fea73ff186a4de1db3180f7dc1abe826ca48ba3ac421ed916cc991999" => :mojave
+    sha256 "9fb110426bfc2e4cedb33bf8de7439450934dd6c81abcc183f12bdf3a5c72656" => :sierra
   end
 
   head do
