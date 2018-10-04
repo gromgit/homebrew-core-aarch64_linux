e6ec7a96d88d637c31e9e16f38b3326665ba4323
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-04 01:41:15 +0000

    safe: update 0.9.7 bottle.

--- Formula/safe.rb
+++ Formula/safe.rb
@@ -6,10 +6,9 @@ class Safe < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e1d24c5e4fff23b059b01d2db0d0751990746f7d6bf059ebdd6addf6c094136b" => :mojave
-    sha256 "0ea87154eb0e27459437405ffc69f0808cba26260d90b058578e4a4560e1b4d6" => :high_sierra
-    sha256 "2c8f4aa792bebf2b3d2fb7640c434b0753c31f5a135ad7171f9cea3f90df6991" => :sierra
-    sha256 "5dc57faafb0a65cf222223edf2d3dc3cc4cc21fe022863934c1fe984d7eef794" => :el_capitan
+    sha256 "87f9ff12f2050e6cf81ea6f0718dab4cdcff165dbfc9c68e38ae795f3642deb6" => :mojave
+    sha256 "840167416cbae1138531b15856b3b1c592f70beccdc21f0c400f25aae66ebecb" => :high_sierra
+    sha256 "4c0f6ee002042d208f02ec2d31681710db275d30c5468186d02090c9ca272f82" => :sierra
   end
 
   depends_on "go" => :build
