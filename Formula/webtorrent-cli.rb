232582072a0a08d8299a25de71b9e86e9ba4a538
Viktor Szakats <commit@vszakats.net>
2018-10-04 09:21:43 +0000

    webtorrent-cli: secure url(s) in test

--- Formula/webtorrent-cli.rb
+++ Formula/webtorrent-cli.rb
@@ -27,7 +27,7 @@ class WebtorrentCli < Formula
       magnet:?xt=urn:btih:9eae210fe47a073f991c83561e75d439887be3f3
       &dn=archlinux-2017.02.01-x86_64.iso
       &tr=udp://tracker.archlinux.org:6969
-      &tr=http://tracker.archlinux.org:6969/announce
+      &tr=https://tracker.archlinux.org:443/announce
     EOS
 
     assert_equal <<~EOS.chomp, shell_output("#{bin}/webtorrent info '#{magnet_uri}'")
@@ -35,13 +35,13 @@ class WebtorrentCli < Formula
         "xt": "urn:btih:9eae210fe47a073f991c83561e75d439887be3f3",
         "dn": "archlinux-2017.02.01-x86_64.iso",
         "tr": [
-          "http://tracker.archlinux.org:6969/announce",
+          "https://tracker.archlinux.org:443/announce",
           "udp://tracker.archlinux.org:6969"
         ],
         "infoHash": "9eae210fe47a073f991c83561e75d439887be3f3",
         "name": "archlinux-2017.02.01-x86_64.iso",
         "announce": [
-          "http://tracker.archlinux.org:6969/announce",
+          "https://tracker.archlinux.org:443/announce",
           "udp://tracker.archlinux.org:6969"
         ],
         "urlList": []
