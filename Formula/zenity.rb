222f1d546ac2912085793825cf0a71c2d457728c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-05 11:38:22 +0000

    zenity: update 3.30.0 bottle.

--- Formula/zenity.rb
+++ Formula/zenity.rb
@@ -5,10 +5,9 @@ class Zenity < Formula
   sha256 "995ef696616492c40be6da99919851d41faed6643a97c9d24743b46bc8b537f2"
 
   bottle do
-    sha256 "131e608af29f2a459abeeeb7d97e181d38a236fa5782079096fdfda22467ba41" => :mojave
-    sha256 "33e96b6ef96ab194ced8ec552e5bb1de8212bc782d82320873ba0a6413547366" => :high_sierra
-    sha256 "e6a508f02eae50cad3ba7c1f39bb712a4136c8caba9569f563cb2d0fa016f428" => :sierra
-    sha256 "eb4386c1857fb2b4a41f5d0c8fb596b58add38a4e3f30d78b7223233aefa18c9" => :el_capitan
+    sha256 "1f45ac15943a7b3493d7833072c253ffa5158aa124fcc734c890f5906496396b" => :mojave
+    sha256 "ef38b762ac8036cf3055f772b661c856e04a387c5e29b4be9b108e5b19485b17" => :high_sierra
+    sha256 "6efdbda681d0357aa8da3cd76fb047d2c66745b75b05a388cc56da5e7e0c5b46" => :sierra
   end
 
   depends_on "itstool" => :build
