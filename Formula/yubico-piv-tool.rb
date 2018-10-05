e259efcd9cf2d81a0746dd72b08a048f68b0d2e5
Dag Heyman <dag@yubico.com>
2018-10-05 10:34:30 +0200

    yubico-piv-tool: update description (#32743)

--- Formula/yubico-piv-tool.rb
+++ Formula/yubico-piv-tool.rb
@@ -1,5 +1,5 @@
 class YubicoPivTool < Formula
-  desc "Command-line tool for the YubiKey NEO PIV applet"
+  desc "Command-line tool for the YubiKey PIV application"
   homepage "https://developers.yubico.com/yubico-piv-tool/"
   url "https://developers.yubico.com/yubico-piv-tool/Releases/yubico-piv-tool-1.6.2.tar.gz"
   sha256 "ea61bcd5c75471ed21903967d0121fb090aa4d325ec279a24633e3235fdf231b"
