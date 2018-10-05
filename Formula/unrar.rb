63fa845b6d1bce4ec7aaf35d63e62b3bdb648db6
Chongyu Zhu <i@lembacon.com>
2018-10-05 14:49:14 +0800

    unrar 5.6.8
    Closes #32736.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/unrar.rb
+++ Formula/unrar.rb
@@ -1,8 +1,8 @@
 class Unrar < Formula
   desc "Extract, view, and test RAR archives"
   homepage "https://www.rarlab.com/"
-  url "https://www.rarlab.com/rar/unrarsrc-5.6.7.tar.gz"
-  sha256 "ff4613db80a7177da660b20225b2a5162409d4b6a5c5c4dc072e43a3dcf2226b"
+  url "https://www.rarlab.com/rar/unrarsrc-5.6.8.tar.gz"
+  sha256 "a4cc0ac14a354827751912d2af4a0a09e2c2129df5766576fa7e151791dd3dff"
 
   bottle do
     cellar :any
