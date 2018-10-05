b813322eddacbadaf6612600581a5749f0e84614
seph <seph@directionless.org>
2018-10-05 13:31:24 -0400

    mscgen: make x11 optional
    Closes #32756.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/mscgen.rb
+++ Formula/mscgen.rb
@@ -3,7 +3,7 @@ class Mscgen < Formula
   homepage "http://www.mcternan.me.uk/mscgen/"
   url "http://www.mcternan.me.uk/mscgen/software/mscgen-src-0.20.tar.gz"
   sha256 "3c3481ae0599e1c2d30b7ed54ab45249127533ab2f20e768a0ae58d8551ddc23"
-  revision 2
+  revision 3
 
   bottle do
     cellar :any
@@ -17,7 +17,7 @@ class Mscgen < Formula
   depends_on "pkg-config" => :build
   depends_on "freetype"
   depends_on "gd"
-  depends_on :x11
+  depends_on :x11 => :optional
 
   def install
     system "./configure", "--prefix=#{prefix}",
