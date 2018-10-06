486f03d492e69afd7f9d4c76f7d73d3080a9dfd5
Scott Crooks <scott.crooks@gmail.com>
2018-10-06 16:15:00 +0200

    sonobuoy 0.12.1 (new formula)
    Closes #32772.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/sonobuoy.rb
@@ -0,0 +1,36 @@
+class Sonobuoy < Formula
+  desc "Kubernetes component that generates reports on cluster conformance"
+  homepage "https://github.com/heptio/sonobuoy"
+  url "https://github.com/heptio/sonobuoy/archive/v0.12.1.tar.gz"
+  sha256 "dca43d4cdf1ecfbb67da129c3759f72beeae07a24710fb6d10eb98411325f373"
+
+  depends_on "go" => :build
+
+  resource "sonobuoyresults" do
+    url "https://raw.githubusercontent.com/heptio/sonobuoy/master/pkg/client/results/testdata/results-0.10.tar.gz"
+    sha256 "a945ba4d475e33820310a6138e3744f301a442ba01977d38f2b635d2e6f24684"
+  end
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/heptio/sonobuoy").install buildpath.children
+
+    cd "src/github.com/heptio/sonobuoy" do
+      system "go", "build", "-o", bin/"sonobuoy", "-installsuffix", "static",
+                   "-ldflags",
+                   "-s -w -X github.com/heptio/sonobuoy/pkg/buildinfo.Version=#{version}",
+                   "./"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    output = shell_output("#{bin}/sonobuoy 2>&1")
+    assert_match "Sonobuoy is an introspective kubernetes component that generates reports on cluster conformance", output
+    assert_match version.to_s, shell_output("#{bin}/sonobuoy version 2>&1")
+    output = shell_output("#{bin}/sonobuoy gen 2>&1")
+    assert_match "name: heptio-sonobuoy", output
+    output = shell_output("#{bin}/sonobuoy e2e --show=all " + resource("sonobuoyresults").cached_download + " 2>&1")
+    assert_match "all tests", output
+  end
+end
