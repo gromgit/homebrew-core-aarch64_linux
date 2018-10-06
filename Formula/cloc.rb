346ca20ecf823752169652849e63b4862158b9c9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-06 16:35:06 +0000

    cloc: update 1.80 bottle.

--- Formula/cloc.rb
+++ Formula/cloc.rb
@@ -7,10 +7,9 @@ class Cloc < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d6cdda5baedc22ce655e3710c0941bada186684ead1c60501de2d78e5007681e" => :mojave
-    sha256 "d1b6fc4ce381936384a97e9481c19f77133bdf7b310dde360d8a548f43c9ad7e" => :high_sierra
-    sha256 "ec9ac848b778b9c55ab25f56b4ba12227cfceabdebd424b50e411861f5f54544" => :sierra
-    sha256 "dc92ea3a174ca3e27a5e3def7a579a3a87d7e682701c0859306e10d6a250a7b3" => :el_capitan
+    sha256 "bfab4c34974f66e6f48442cfc06fb9421547ab9c53aceddce2ddda408e77ffbd" => :mojave
+    sha256 "6f0f297361bcaaba9b0924ab4a7401932cd8b208cc06587bc6c0881bb58935a3" => :high_sierra
+    sha256 "9651d13839bda437d0cd39e8a07e6f6688957525c049565d40d01287ae7185c9" => :sierra
   end
 
   resource "Regexp::Common" do
