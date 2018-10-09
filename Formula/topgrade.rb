9c426824ee7075d94c58d22885f0051f9b6c10bc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-09 08:07:35 +0000

    topgrade: update 0.16.0 bottle.

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -5,9 +5,9 @@ class Topgrade < Formula
   sha256 "5cb12fd944a26da71581c78e19b320c056b8ad9cf0f867f4b1c352a1bc3eccc1"
 
   bottle do
-    sha256 "4b1250a32e4f54a08c9f8327f2440749e504e8f76b9bd3c7eb60bceb7f264774" => :mojave
-    sha256 "0c43097bde30d85f0541ec370a2d61ada00de5485afbcc646c7b7dfcc34d9831" => :high_sierra
-    sha256 "38d5215dacc2da6d2b13870f462c8a07c73b19277b04e35562021cbaa7bf1bb7" => :sierra
+    sha256 "b71341a085ac93dda023c1cf4eb4475950fb5da3a49ec554c3579d668ea3b029" => :mojave
+    sha256 "609a19975e9a4d67035aa1814f73bcfa1a9cb66232eecbb343dda526c9a20d31" => :high_sierra
+    sha256 "526ef20dcba2fe9b0ea69a2fec3537e874629358132e5424370a185d40fca1ed" => :sierra
   end
 
   depends_on "rust" => :build
