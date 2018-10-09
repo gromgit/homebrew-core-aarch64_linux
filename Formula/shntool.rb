cac7bbf67ce60e605f9c2c8086ba77c691d1a2d4
Viktor Szakats <commit@vszakats.net>
2018-10-09 02:55:56 +0000

    shntool: secure/fix/follow url(s)

--- Formula/shntool.rb
+++ Formula/shntool.rb
@@ -1,7 +1,7 @@
 class Shntool < Formula
   desc "Multi-purpose tool for manipulating and analyzing WAV files"
-  homepage "http://etree.org/shnutils/shntool/"
-  url "http://etree.org/shnutils/shntool/dist/src/shntool-3.0.10.tar.gz"
+  homepage "https://web.archive.org/web/20180903160820/etree.org/shnutils/shntool/"
+  url "https://web.archive.org/web/20180903160820/etree.org/shnutils/shntool/dist/src/shntool-3.0.10.tar.gz"
   mirror "https://www.mirrorservice.org/sites/download.salixos.org/x86_64/extra-14.2/source/audio/shntool/shntool-3.0.10.tar.gz"
   sha256 "74302eac477ca08fb2b42b9f154cc870593aec8beab308676e4373a5e4ca2102"
 
