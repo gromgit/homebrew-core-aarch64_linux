4ef30bd5611bc5889f28bff97c610204f40f47d5
Claudio Fantacci <claudio.fantacci@gmail.com>
2018-10-09 11:16:16 +0200

    librealsense 2.16.1
    Closes #32842.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/librealsense.rb
+++ Formula/librealsense.rb
@@ -1,8 +1,8 @@
 class Librealsense < Formula
   desc "Intel RealSense D400 series and SR300 capture"
   homepage "https://github.com/IntelRealSense/librealsense"
-  url "https://github.com/IntelRealSense/librealsense/archive/v2.15.0.tar.gz"
-  sha256 "c855fcce74b686efb09caeb6b8d306d90027ded428f1434ed8ba982e36bcb98f"
+  url "https://github.com/IntelRealSense/librealsense/archive/v2.16.1.tar.gz"
+  sha256 "001787d51398160a4b9285ffa74df08e22615a8278a3c994fc55c1584644584a"
   head "https://github.com/IntelRealSense/librealsense.git"
 
   bottle do
@@ -13,18 +13,14 @@ class Librealsense < Formula
     sha256 "445caed7e761e5d5d03c76cb48820118021a4313db61cf5b2594a122c714ba7b" => :el_capitan
   end
 
-  option "with-glfw", "Build & install examples"
-
-  deprecated_option "with-examples" => "with-glfw"
-
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "glfw"
   depends_on "libusb"
-  depends_on "glfw" => :optional
 
   def install
     args = std_cmake_args
-    args << "-DBUILD_EXAMPLES=OFF" if build.without? "glfw"
+    args << "-DENABLE_CCACHE=OFF"
 
     system "cmake", ".", "-DBUILD_WITH_OPENMP=OFF", *args
     system "make", "install"
