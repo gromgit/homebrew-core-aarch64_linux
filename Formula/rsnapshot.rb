61363a67df1596ab813043437f9d351b7a3066e1
Viktor Szakats <commit@vszakats.net>
2018-10-09 02:55:56 +0000

    rsnapshot: secure/fix/follow url(s)

--- Formula/rsnapshot.rb
+++ Formula/rsnapshot.rb
@@ -1,6 +1,6 @@
 class Rsnapshot < Formula
   desc "File system snapshot utility (based on rsync)"
-  homepage "http://rsnapshot.org"
+  homepage "https://www.rsnapshot.org/"
   url "https://github.com/rsnapshot/rsnapshot/releases/download/1.4.2/rsnapshot-1.4.2.tar.gz"
   sha256 "042a81c45b325296b21c363f417985d857f083f87c5c27f5a64677a052f24e16"
   head "https://github.com/DrHyde/rsnapshot.git"
