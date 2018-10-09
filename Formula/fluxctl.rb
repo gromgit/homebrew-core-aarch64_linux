0af9c27134b7b9f919386a163f4d6e13a3f8f073
Daniel Holbach <daniel@weave.works>
2018-10-09 13:41:01 +0200

    fluxctl 1.8.0 (new formula)
    Signed-off-by: Daniel Holbach <daniel@weave.works>
    
    Closes #32848.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/fluxctl.rb
@@ -0,0 +1,48 @@
+class Fluxctl < Formula
+  desc "Command-line tool to access Weave Flux, the Kubernetes GitOps operator"
+  homepage "https://github.com/weaveworks/flux"
+  url "https://github.com/weaveworks/flux.git",
+      :tag => "1.8.0",
+      :revision => "9f2be4bf2d762034a5118a0a7e974bf91089afea"
+
+  depends_on "dep" => :build
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    dir = buildpath/"src/github.com/weaveworks/flux"
+    dir.install buildpath.children - [buildpath/".brew_home"]
+
+    cd dir do
+      system "dep", "ensure", "-vendor-only"
+      system "make", "release-bins"
+      bin.install "build/fluxctl_darwin_amd64" => "fluxctl"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    run_output = shell_output("#{bin}/fluxctl 2>&1")
+    assert_match "fluxctl helps you deploy your code.", run_output
+
+    version_output = shell_output("#{bin}/fluxctl version 2>&1")
+    assert_match version.to_s, version_output
+
+    # As we can't bring up a Kubernetes cluster in this test, we simply
+    # run "fluxctl sync" and check that it 1) errors out, and 2) complains
+    # about a missing .kube/config file.
+    require "pty"
+    require "timeout"
+    r, _w, pid = PTY.spawn("#{bin}/fluxctl sync", :err=>:out)
+    begin
+      Timeout.timeout(5) do
+        assert r.gets.chomp =~ %r{Error: stat .*\/.kube\/config: no such file or directory}
+        Process.wait pid
+        assert_equal 1, $CHILD_STATUS.exitstatus
+      end
+    rescue Timeout::Error
+      puts "process not finished in time, killing it"
+      Process.kill("TERM", pid)
+    end
+  end
+end
