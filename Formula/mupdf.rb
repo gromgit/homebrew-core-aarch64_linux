a93a7fc765ec1320b2e2f5a471ed3c8ac37277a2
Xu Cheng <git@xuc.me>
2018-10-09 18:33:27 +0800

    mupdf 1.14.0

--- Formula/mupdf.rb
+++ Formula/mupdf.rb
@@ -1,8 +1,8 @@
 class Mupdf < Formula
   desc "Lightweight PDF and XPS viewer"
   homepage "https://mupdf.com/"
-  url "https://mupdf.com/downloads/mupdf-1.13.0-source.tar.gz"
-  sha256 "071c6962cbee1136188da62136596a9d704b81e35fe617cd34874bbb0ae7ca09"
+  url "https://mupdf.com/downloads/archive/mupdf-1.14.0-source.tar.gz"
+  sha256 "c443483a678c3fc258fa4adc124146225d0bb443c522619faadebf6b363d7724"
   head "https://git.ghostscript.com/mupdf.git"
 
   bottle do
@@ -19,22 +19,16 @@ class Mupdf < Formula
   conflicts_with "mupdf-tools",
     :because => "mupdf and mupdf-tools install the same binaries."
 
-  # Reverts an upstream commit which is incompatible with the macOS GLUT;
-  # the commit in question adds the use of a freeglut-only function and constants.
-  # An earlier commit added explicit OS X GLUT support, so this looks like a bug.
-  # https://bugs.ghostscript.com/show_bug.cgi?id=699374
-  patch do
-    url "https://gist.githubusercontent.com/mistydemeo/af049b9151363cd5d5fb58b8ce9e26b6/raw/1c4448c7c0e7c165c5805fd37b4de03ffb7f26fd/0001-Revert-gl-Tell-glut-to-return-from-main-loop-when-th.patch"
-    sha256 "e5c5d00874f09c6f70a1fd8db7e86f0d386c88bc209dcb287f4eef644c1de44b"
-  end
-
   def install
     system "make", "install",
            "build=release",
            "verbose=yes",
            "CC=#{ENV.cc}",
            "prefix=#{prefix}"
-    bin.install_symlink "mutool" => "mudraw"
+
+    # Symlink `mutool` as `mudraw` (a popular shortcut for `mutool draw`).
+    bin.install_symlink bin/"mutool" => "mudraw"
+    man1.install_symlink man1/"mutool.1" => "mudraw.1"
   end
 
   test do
