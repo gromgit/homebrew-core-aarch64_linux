e0bce6a66eb701666dbfc749a8fa7be47d0281c6
Viktor Szakats <commit@vszakats.net>
2018-10-09 02:55:55 +0000

    maven-shell: secure/fix/follow url(s)

--- Formula/maven-shell.rb
+++ Formula/maven-shell.rb
@@ -1,6 +1,6 @@
 class MavenShell < Formula
   desc "Shell for Maven"
-  homepage "http://shell.sonatype.org/"
+  homepage "https://github.com/jdillon/mvnsh"
   url "https://search.maven.org/remotecontent?filepath=org/sonatype/maven/shell/dist/mvnsh-assembly/1.1.0/mvnsh-assembly-1.1.0-bin.tar.gz"
   sha256 "584008d726bf6f90271f4ccd03b549773cbbe62ba7e92bf131e67df3ac5a41ac"
 
