e02f82da6941d818e5c20fab6d841953ac76edf6
Viktor Szakats <commit@vszakats.net>
2018-10-09 02:55:55 +0000

    libxc: secure/fix/follow url(s)

--- Formula/libxc.rb
+++ Formula/libxc.rb
@@ -1,6 +1,6 @@
 class Libxc < Formula
   desc "Library of exchange and correlation functionals for codes"
-  homepage "http://octopus-code.org/wiki/Libxc"
+  homepage "http://www.tddft.org/programs/libxc/"
   url "http://www.tddft.org/programs/octopus/down.php?file=libxc/3.0.1/libxc-3.0.1.tar.gz"
   sha256 "836692f2ab60ec3aca0cca105ed5d0baa7d182be07cc9d0daa7b80ee1362caf7"
   revision 2
