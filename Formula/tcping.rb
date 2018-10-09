bb4cba06ee72e1773d4defb4264ef0b3f533ba51
Viktor Szakats <commit@vszakats.net>
2018-10-09 02:55:56 +0000

    tcping: secure/fix/follow url(s)

--- Formula/tcping.rb
+++ Formula/tcping.rb
@@ -1,6 +1,6 @@
 class Tcping < Formula
   desc "TCP connect to the given IP/port combo"
-  homepage "http://linuxco.de/tcping/tcping.html"
+  homepage "https://web.archive.org/web/20171102193822/linuxco.de/tcping/tcping.html"
   url "https://mirrors.kernel.org/gentoo/distfiles/tcping-1.3.5.tar.gz"
   sha256 "1ad52e904094d12b225ac4a0bc75297555e931c11a1501445faa548ff5ecdbd0"
 
