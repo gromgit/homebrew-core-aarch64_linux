8762e00f5ec9a7ddd2f7b6c2dd54a8dc0ab9c45f
FX Coudert <fxcoudert@gmail.com>
2018-10-10 14:49:56 +0200

    libpeas: remove options

--- Formula/libpeas.rb
+++ Formula/libpeas.rb
@@ -12,22 +12,14 @@ class Libpeas < Formula
     sha256 "e564ba0cc81e732fdd1e5632d5628ab1ac9c9a45d094b17e22d01dde345f3946" => :el_capitan
   end
 
-  option "with-python@2", "Build with support for python2 plugins"
-
   depends_on "gettext" => :build
   depends_on "intltool" => :build
   depends_on "pkg-config" => :build
   depends_on "glib"
   depends_on "gobject-introspection"
   depends_on "gtk+3"
+  depends_on "pygobject3"
   depends_on "python"
-  depends_on "python@2" => :optional
-
-  if build.with? "python@2"
-    depends_on "pygobject3" => "with-python@2"
-  else
-    depends_on "pygobject3"
-  end
 
   def install
     args = %W[
@@ -36,19 +28,12 @@ class Libpeas < Formula
       --prefix=#{prefix}
       --enable-gtk
       --enable-python3
+      --disable-python2
     ]
 
     xy = Language::Python.major_minor_version "python3"
     py3_lib = Formula["python"].opt_frameworks/"Python.framework/Versions/#{xy}/lib"
     ENV.append "LDFLAGS", "-L#{py3_lib}"
-
-    if build.with? "python@2"
-      py2_lib = Formula["python@2"].opt_frameworks/"Python.framework/Versions/2.7/lib"
-      ENV.append "LDFLAGS", "-L#{py2_lib}"
-      args << "--enable-python2"
-    else
-      args << "--disable-python2"
-    end
 
     system "./configure", *args
     system "make", "install"
