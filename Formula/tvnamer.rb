164fdbf3da0edab6f1a9051711cba880cdb4fea1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-10 15:51:59 +0000

    tvnamer: update 2.4_1 bottle.

--- Formula/tvnamer.rb
+++ Formula/tvnamer.rb
@@ -8,11 +8,9 @@ class Tvnamer < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "15f31af4fe2f3411449ccd1fc07ca5b439fcc4b3331a5b11dfc230aee50246a4" => :mojave
-    sha256 "4f1ae20737bed54785fce92bbdd67c78f3b20553312d02051f72b8906211d507" => :high_sierra
-    sha256 "c1a03bc94b59d7e044445daa9c0037b46230271d457d658aba3e7e55e9291dd0" => :sierra
-    sha256 "546b13ef89b777d7076948d993adc9a57c664b2bbc9cb2f429cf042f3d61d165" => :el_capitan
-    sha256 "546b13ef89b777d7076948d993adc9a57c664b2bbc9cb2f429cf042f3d61d165" => :yosemite
+    sha256 "4f9eaff367dac5d210af44c8a1b768bbd34b5584f755980a9e1b6348c983263f" => :mojave
+    sha256 "96c779980beab409c042f22ed2fecfaca0f9d10d3ca2421ec9fdad94e6011363" => :high_sierra
+    sha256 "295f7a2e93d3aa599cf23bcf0c90630d0ab5b16508af7b90813c8582623ac085" => :sierra
   end
 
   depends_on "python"
