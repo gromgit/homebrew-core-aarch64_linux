264066a7cbdb7ec84362421dfe50d1a169dddec6
Sven-S. Porst <ssp-web@earthlingsoft.net>
2018-10-11 00:58:05 +0200

    yaz 5.27.0
    Closes #32892.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/yaz.rb
+++ Formula/yaz.rb
@@ -1,8 +1,8 @@
 class Yaz < Formula
   desc "Toolkit for Z39.50/SRW/SRU clients/servers"
   homepage "https://www.indexdata.com/yaz"
-  url "http://ftp.indexdata.dk/pub/yaz/yaz-5.26.1.tar.gz"
-  sha256 "ceb4367e3098bce8e09734f09c0410bd679fdc3f9423bb28d70cc15cc95b66cc"
+  url "http://ftp.indexdata.dk/pub/yaz/yaz-5.27.0.tar.gz"
+  sha256 "3db834b30aad941aaee67a3daf0cfc33d6f81e724dd8d54227be201ad93c18f3"
 
   bottle do
     cellar :any
