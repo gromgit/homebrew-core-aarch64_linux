68f939f2036e24c9ef6c33b0252806db46f73908
FX Coudert <fxcoudert@gmail.com>
2018-10-10 14:25:38 +0200

    doitlive: move to Python 3

--- Formula/doitlive.rb
+++ Formula/doitlive.rb
@@ -3,6 +3,7 @@ class Doitlive < Formula
   homepage "https://doitlive.readthedocs.io/en/latest/"
   url "https://files.pythonhosted.org/packages/67/21/d97f70d6afb56b146175ee4af92ff61ee4ab98a75c36a09b4e4a83c70411/doitlive-4.0.1.tar.gz"
   sha256 "ec1a31b203c9b274fe6f978f4560f82e9ce22d965157172e1b5114ffd99496c5"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -12,14 +13,15 @@ class Doitlive < Formula
     sha256 "66326431fffbc6858f121521297581c2e944b901f798bba6f66774bce947cc20" => :el_capitan
   end
 
-  depends_on "python@2"
+  depends_on "python"
 
   def install
-    ENV.prepend_create_path "PYTHONPATH", libexec+"lib/python2.7/site-packages"
-    system "python", "setup.py", "install", "--prefix=#{libexec}"
+    xy = Language::Python.major_minor_version "python3"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
+    system "python3", "setup.py", "install", "--prefix=#{libexec}"
 
     bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec+"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
 
     output = Utils.popen_read("SHELL=bash #{libexec}/bin/doitlive completion")
     (bash_completion/"doitlive").write output
