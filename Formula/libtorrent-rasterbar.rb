aa3addebdb895b5301e988d7aa9226631d812c9a
FX Coudert <fxcoudert@gmail.com>
2018-10-10 14:42:31 +0200

    libtorrent-rasterbar: remove options

--- Formula/libtorrent-rasterbar.rb
+++ Formula/libtorrent-rasterbar.rb
@@ -3,6 +3,7 @@ class LibtorrentRasterbar < Formula
   homepage "https://www.libtorrent.org/"
   url "https://github.com/arvidn/libtorrent/releases/download/libtorrent-1_1_8/libtorrent-rasterbar-1.1.8.tar.gz"
   sha256 "6bbf8fd0430e27037b09a870c89cfc330ea41816102fe1d1d16cc7428df08d5d"
+  revision 1
 
   bottle do
     cellar :any
@@ -19,28 +20,25 @@ class LibtorrentRasterbar < Formula
     depends_on "libtool" => :build
   end
 
-  deprecated_option "with-python" => "with-python@2"
-
   depends_on "pkg-config" => :build
   depends_on "boost"
+  depends_on "boost-python"
   depends_on "openssl"
-  depends_on "python@2" => :optional
-  depends_on "boost-python" if build.with? "python@2"
+  depends_on "python@2"
 
   def install
     ENV.cxx11
-    args = ["--disable-debug",
-            "--disable-dependency-tracking",
-            "--disable-silent-rules",
-            "--enable-encryption",
-            "--prefix=#{prefix}",
-            "--with-boost=#{Formula["boost"].opt_prefix}"]
 
-    # Build python bindings requires forcing usage of the mt version of boost_python.
-    if build.with? "python@2"
-      args << "--enable-python-binding"
-      args << "--with-boost-python=boost_python27-mt"
-    end
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --enable-encryption
+      --enable-python-binding
+      --with-boost=#{Formula["boost"].opt_prefix}
+      --with-boost-python=boost_python27-mt
+    ]
 
     if build.head?
       system "./bootstrap.sh", *args
@@ -54,7 +52,8 @@ class LibtorrentRasterbar < Formula
 
   test do
     system ENV.cxx, "-L#{lib}", "-ltorrent-rasterbar",
-           "-I#{Formula["boost"].include}/boost", "-lboost_system",
+           "-I#{Formula["boost"].include}/boost",
+           "-L#{Formula["boost"].lib}", "-lboost_system",
            libexec/"examples/make_torrent.cpp", "-o", "test"
     system "./test", test_fixtures("test.mp3"), "-o", "test.torrent"
     assert_predicate testpath/"test.torrent", :exist?
