7c630c75cc2714e53e04eeef8fbe22967cef2b6d
FX Coudert <fxcoudert@gmail.com>
2018-10-11 11:25:37 +0200

    wakatime-cli 10.4.1
    Closes #32904.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/wakatime-cli.rb
+++ Formula/wakatime-cli.rb
@@ -1,8 +1,8 @@
 class WakatimeCli < Formula
   desc "Command-line interface to the WakaTime api"
   homepage "https://wakatime.com/"
-  url "https://files.pythonhosted.org/packages/8f/b7/a575a758cf33c8c1ce0ddd642a6c26d94105ca407d68a2074e6731c878b3/wakatime-8.0.3.tar.gz"
-  sha256 "bb178937a762a1250dd2e22a62ec28dc7c1ebe2dabcc9bbe05cb781afe86dd47"
+  url "https://files.pythonhosted.org/packages/04/db/9917533f8fd503f636f8c5655d9055f2fbef6f90a12a0210b92994f6f823/wakatime-10.4.1.tar.gz"
+  sha256 "8acef3ab83dbf9b5cc7592510b9dea1b75f20f4bdf60650329431b81e7f125a9"
 
   bottle do
     cellar :any_skip_relocation
@@ -13,12 +13,13 @@ class WakatimeCli < Formula
     sha256 "ee933f36da072757c1c186494b2d5f2efd93b76f76784defb4b8a5a05a1ffd70" => :yosemite
   end
 
-  depends_on "python@2"
+  depends_on "python"
 
   def install
-    ENV["PYTHONPATH"] = libexec/"lib/python2.7/site-packages"
+    xy = Language::Python.major_minor_version "python3"
+    ENV["PYTHONPATH"] = libexec/"lib/python#{xy}/site-packages"
 
-    system "python", *Language::Python.setup_install_args(libexec)
+    system "python3", *Language::Python.setup_install_args(libexec)
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
