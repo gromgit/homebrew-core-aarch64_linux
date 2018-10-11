9cf7a9fe3becf00735bb069a063abbe1d51ad045
FX Coudert <fxcoudert@gmail.com>
2018-10-11 08:52:30 +0200

    atdtool: move to Python 3

--- Formula/atdtool.rb
+++ Formula/atdtool.rb
@@ -3,6 +3,7 @@ class Atdtool < Formula
   homepage "https://github.com/lpenz/atdtool"
   url "https://github.com/lpenz/atdtool/archive/upstream/1.3.3.tar.gz"
   sha256 "3e928721388cf6f58b7e663ebc5508f26d180b1c07d5b8119212356c66e57fe8"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -12,11 +13,12 @@ class Atdtool < Formula
     sha256 "c1c1b8c7468e0b649ba93da43344eaed88f3e8db4e09bd656398f91b6ebaeef4" => :el_capitan
   end
 
-  depends_on "python@2"
+  depends_on "python"
 
   def install
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
-    system "python", *Language::Python.setup_install_args(libexec)
+    xy = Language::Python.major_minor_version "python3"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
+    system "python3", *Language::Python.setup_install_args(libexec)
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
     prefix.install libexec/"share"
