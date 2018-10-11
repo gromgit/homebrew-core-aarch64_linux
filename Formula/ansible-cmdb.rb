73f8a1ea9852e726e08c127fb005f6ed74b1bc8e
FX Coudert <fxcoudert@gmail.com>
2018-10-11 09:01:33 +0200

    ansible-cmdb: use system python

--- Formula/ansible-cmdb.rb
+++ Formula/ansible-cmdb.rb
@@ -3,11 +3,11 @@ class AnsibleCmdb < Formula
   homepage "https://github.com/fboender/ansible-cmdb"
   url "https://github.com/fboender/ansible-cmdb/releases/download/1.28/ansible-cmdb-1.28.tar.gz"
   sha256 "88de6007988fe3438277ac2870c01973cadf0fc051a6e3f61769511ae50c348f"
+  revision 1
 
   bottle :unneeded
 
   depends_on "libyaml"
-  depends_on "python@2"
 
   def install
     prefix.install_metafiles
