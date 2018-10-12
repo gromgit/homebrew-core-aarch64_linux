1b25ed36cb841b27c7d9307d90ce89d4e3e0f3c2
Alex Merkel <mail@alexmerkel.com>
2018-10-12 17:24:01 +0200

    testssl 2.9.5-7
    Closes #32946.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/testssl.rb
+++ Formula/testssl.rb
@@ -1,9 +1,9 @@
 class Testssl < Formula
   desc "Tool which checks for the support of TLS/SSL ciphers and flaws"
   homepage "https://testssl.sh/"
-  url "https://github.com/drwetter/testssl.sh/archive/v2.9.5-5.tar.gz"
-  version "2.9.5-5"
-  sha256 "836a7b45455c95f17c4d7eec9468028a7fc6b613fd4b3c8e8e125b7b8206b89d"
+  url "https://github.com/drwetter/testssl.sh/archive/v2.9.5-7.tar.gz"
+  version "2.9.5-7"
+  sha256 "cefa572026119fbc872d24dc0fcec64a105b0e11a85291b48f0e5ef494f55517"
   head "https://github.com/drwetter/testssl.sh.git", :branch => "2.9dev"
 
   bottle :unneeded
