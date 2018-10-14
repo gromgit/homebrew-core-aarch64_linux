c5c3413723b3984d6cfefb109df57201521bbb60
Matt Wean <matthew.wean@gmail.com>
2018-10-14 05:46:40 -0700

    asdf 0.6.0 (#32955)

--- Formula/asdf.rb
+++ Formula/asdf.rb
@@ -1,8 +1,8 @@
 class Asdf < Formula
   desc "Extendable version manager with support for Ruby, Node.js, Erlang & more"
   homepage "https://github.com/asdf-vm"
-  url "https://github.com/asdf-vm/asdf/archive/v0.5.1.tar.gz"
-  sha256 "5e7d4d61256e9f8185fcf04ef2b7f060a821569764d5c1212dc7d0ef7eb2edee"
+  url "https://github.com/asdf-vm/asdf/archive/v0.6.0.tar.gz"
+  sha256 "a17a198b1581fb8f26dadf03985b71287989f0cfc5da40f5c9e1937ec8f9f74b"
   head "https://github.com/asdf-vm/asdf.git"
 
   bottle :unneeded
@@ -37,7 +37,7 @@ class Asdf < Formula
   end
 
   test do
-    output = shell_output("#{bin}/asdf plugin-list")
+    output = shell_output("#{bin}/asdf plugin-list 2>&1", 1)
     assert_match "Oohes nooes ~! No plugins installed", output
   end
 end
