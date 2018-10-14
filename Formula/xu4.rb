ed8154378b37005c7433cf554e392e7797ce43fb
Mike McQuaid <mike@mikemcquaid.com>
2018-10-13 18:22:50 -0700

    xu4: shorten long lines.

--- Formula/xu4.rb
+++ Formula/xu4.rb
@@ -24,7 +24,8 @@ class Xu4 < Formula
   end
 
   resource "u4upgrad" do
-    url "https://downloads.sourceforge.net/project/xu4/Ultima%204%20VGA%20Upgrade/1.3/u4upgrad.zip", :using => :nounzip
+    url "https://downloads.sourceforge.net/project/xu4/Ultima%204%20VGA%20Upgrade/1.3/u4upgrad.zip",
+      :using => :nounzip
     sha256 "400ac37311f3be74c1b2d7836561b2ead2b146f5162586865b0f4881225cca58"
   end
 
@@ -39,12 +40,17 @@ class Xu4 < Formula
       # Copy over SDL's ObjC main files
       cp_r Dir[Formula["sdl"].libexec/"*"], "macosx"
 
-      system "make", "bundle", "-f", "Makefile.macosx",
-                               "CC=#{ENV.cc}",
-                               "CXX=#{ENV.cxx}",
-                               "PREFIX=#{HOMEBREW_PREFIX}",
-                               "UILIBS=-framework Cocoa -L#{Formula["sdl"].lib} -lSDL -L#{Formula["sdl_mixer"].lib} -lSDL_mixer -L#{Formula["libpng"].lib} -lpng",
-                               "UIFLAGS=-I#{Formula["sdl"].include}/SDL -I#{Formula["sdl_mixer"].include}/SDL -I#{Formula["libpng"].include}"
+      system "make", "bundle",
+             "-f", "Makefile.macosx",
+             "CC=#{ENV.cc}",
+             "CXX=#{ENV.cxx}",
+             "PREFIX=#{HOMEBREW_PREFIX}",
+             "UILIBS=-framework Cocoa -L#{Formula["sdl"].lib} -lSDL " \
+                    "-L#{Formula["sdl_mixer"].lib} -lSDL_mixer " \
+                    "-L#{Formula["libpng"].lib} -lpng",
+             "UIFLAGS=-I#{Formula["sdl"].include}/SDL " \
+                     "-I#{Formula["sdl_mixer"].include}/SDL " \
+                     "-I#{Formula["libpng"].include}"
       prefix.install "XU4.app"
       bin.write_exec_script "#{prefix}/XU4.app/Contents/MacOS/u4"
     end
