dff0bf8fbc792ca89b77a11c292a236e936e3dd1
Mike McQuaid <mike@mikemcquaid.com>
2018-10-13 18:22:47 -0700

    lammps: shorten long lines.

--- Formula/lammps.rb
+++ Formula/lammps.rb
@@ -40,7 +40,8 @@ class Lammps < Formula
                        "FFT_INC=-DFFT_FFTW3 -I#{Formula["fftw"].opt_include}",
                        "FFT_PATH=-L#{Formula["fftw"].opt_lib}",
                        "FFT_LIB=-lfftw3",
-                       "JPG_INC=-DLAMMPS_JPEG -I#{Formula["jpeg"].opt_include} -DLAMMPS_PNG -I#{Formula["libpng"].opt_include}",
+                       "JPG_INC=-DLAMMPS_JPEG -I#{Formula["jpeg"].opt_include} " \
+                       "-DLAMMPS_PNG -I#{Formula["libpng"].opt_include}",
                        "JPG_PATH=-L#{Formula["jpeg"].opt_lib} -L#{Formula["libpng"].opt_lib}",
                        "JPG_LIB=-ljpeg -lpng"
 
