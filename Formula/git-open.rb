9f5fe4dc22b11ea4de59b92c41614666ea2171c4
Elan Ruusamäe <glen@pld-linux.org>
2018-10-15 15:44:39 +0300

    git-open 1.2 (#32995)

--- Formula/git-open.rb
+++ Formula/git-open.rb
@@ -1,8 +1,8 @@
 class GitOpen < Formula
   desc "Open GitHub webpages from a terminal"
   homepage "https://github.com/jeffreyiacono/git-open"
-  url "https://github.com/jeffreyiacono/git-open/archive/v1.1.tar.gz"
-  sha256 "21b4b8f9394d5315b3790704ecb3bb031bce13a90da4a34af553391641421073"
+  url "https://github.com/jeffreyiacono/git-open/archive/v1.2.tar.gz"
+  sha256 "4bc50a5fa019e8306c93deb46b284f1883ab4fc5de88770d7d89755dcf8e0a5d"
 
   bottle :unneeded
 
