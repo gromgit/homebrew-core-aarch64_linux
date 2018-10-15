acc7f0d3bdee2e0e02bf6b86391b07939d732ede
Viktor Szakats <commit@vszakats.net>
2018-10-15 22:15:01 +0000

    deark: secure url(s)

--- Formula/deark.rb
+++ Formula/deark.rb
@@ -2,8 +2,8 @@ require "base64"
 
 class Deark < Formula
   desc "File conversion utility for older formats"
-  homepage "http://entropymine.com/deark/"
-  url "http://entropymine.com/deark/releases/deark-1.4.7.tar.gz"
+  homepage "https://entropymine.com/deark/"
+  url "https://entropymine.com/deark/releases/deark-1.4.7.tar.gz"
   sha256 "f26441f7f361e9f4bf584866d46995e35441ea88f269cb6c024881a5d17c11ff"
 
   bottle do
