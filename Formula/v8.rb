b96f5e632fbc0e896423c4a565c8bb960bb9baa3
Christian Moritz <chrmoritz@gmail.com>
2018-10-16 21:31:05 +0200

    v8 7.0.276.28

--- Formula/v8.rb
+++ Formula/v8.rb
@@ -3,8 +3,8 @@ class V8 < Formula
   desc "Google's JavaScript engine"
   homepage "https://github.com/v8/v8/wiki"
   url "https://chromium.googlesource.com/chromium/tools/depot_tools.git",
-      :revision => "0425ebd2b395dd754606e3639c296c5619757451"
-  version "6.9.427.22" # the version of the v8 checkout, not a depot_tools version
+      :revision => "74b702466046e0a59366cea26290af6590007675"
+  version "7.0.276.28" # the version of the v8 checkout, not a depot_tools version
 
   bottle do
     cellar :any
