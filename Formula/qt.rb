25ffe1ce6fe6c419238b04949f104b54cb14d029
Yurii Kolesnykov <root@yurikoles.com>
2018-10-17 13:31:54 +0300

    qt 5.12.0-beta2 (devel) (#32975)

--- Formula/qt.rb
+++ Formula/qt.rb
@@ -3,17 +3,45 @@
 class Qt < Formula
   desc "Cross-platform application and UI framework"
   homepage "https://www.qt.io/"
-  url "https://download.qt.io/official_releases/qt/5.11/5.11.2/single/qt-everywhere-src-5.11.2.tar.xz"
-  mirror "https://qt.mirror.constant.com/archive/qt/5.11/5.11.2/single/qt-everywhere-src-5.11.2.tar.xz"
-  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/qt5/qt-everywhere-src-5.11.2.tar.xz"
-  sha256 "c6104b840b6caee596fa9a35bc5f57f67ed5a99d6a36497b6fe66f990a53ca81"
+
   head "https://code.qt.io/qt/qt5.git", :branch => "5.12", :shallow => false
+
+  stable do
+    url "https://download.qt.io/official_releases/qt/5.11/5.11.2/single/qt-everywhere-src-5.11.2.tar.xz"
+    mirror "http://qt.mirror.constant.com/archive/qt/5.11/5.11.2/single/qt-everywhere-src-5.11.2.tar.xz"
+    mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/qt5/qt-everywhere-src-5.11.2.tar.xz"
+    sha256 "c6104b840b6caee596fa9a35bc5f57f67ed5a99d6a36497b6fe66f990a53ca81"
+    # Restore `.pc` files for framework-based build of Qt 5 on macOS, partially
+    # reverting <https://codereview.qt-project.org/#/c/140954/>
+    # Core formulae known to fail without this patch (as of 2016-10-15):
+    #   * gnuplot (with `--with-qt` option)
+    #   * mkvtoolnix (with `--with-qt` option, silent build failure)
+    #   * poppler (with `--with-qt` option)
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/e8fe6567/qt5/restore-pc-files.patch"
+      sha256 "48ff18be2f4050de7288bddbae7f47e949512ac4bcd126c2f504be2ac701158b"
+    end
+
+    # Chromium build failures with Xcode 10, fixed upstream:
+    # https://bugs.chromium.org/p/chromium/issues/detail?id=840251
+    # https://bugs.chromium.org/p/chromium/issues/detail?id=849689
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/962f0f/qt/xcode10.diff"
+      sha256 "c064398411c69f2e1c516c0cd49fcd0755bc29bb19e65c5694c6d726c43389a6"
+    end
+  end
 
   bottle do
     sha256 "8c77b5762267b127cc31346ac4da805bbfd59e0180d90e1e8b77fb463e929d60" => :mojave
     sha256 "096d8894b25b0fdec9b77150704491993872a7848397a04870627534fb95c9e3" => :high_sierra
     sha256 "0464be51d0eb0a45de4a1d1c6200e1d9768eec5e9737050755497a4f4de66a08" => :sierra
     sha256 "22e9abc0b47541bb03b2da7f6a19c5d7640ea2314322564551adc3d22305806e" => :el_capitan
+  end
+
+  devel do
+    url "https://download.qt.io/development_releases/qt/5.12/5.12.0-beta2/single/qt-everywhere-src-5.12.0-beta2.tar.xz"
+    mirror "https://ftp.acc.umu.se/mirror/qt.io/qtproject/development_releases/qt/5.12/5.12.0-beta2/single/qt-everywhere-src-5.12.0-beta2.tar.xz"
+    sha256 "1ea63d6395c4cd99ae2edd28d43648e6b0432ae23d64be57f08fd3d0eeace368"
   end
 
   keg_only "Qt 5 has CMake issues when linked"
@@ -27,25 +55,6 @@ class Qt < Formula
   depends_on :xcode => :build
   depends_on "mysql-client" => :optional
   depends_on "postgresql" => :optional
-
-  # Restore `.pc` files for framework-based build of Qt 5 on macOS, partially
-  # reverting <https://codereview.qt-project.org/#/c/140954/>
-  # Core formulae known to fail without this patch (as of 2016-10-15):
-  #   * gnuplot (with `--with-qt` option)
-  #   * mkvtoolnix (with `--with-qt` option, silent build failure)
-  #   * poppler (with `--with-qt` option)
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/e8fe6567/qt5/restore-pc-files.patch"
-    sha256 "48ff18be2f4050de7288bddbae7f47e949512ac4bcd126c2f504be2ac701158b"
-  end
-
-  # Chromium build failures with Xcode 10, fixed upstream:
-  # https://bugs.chromium.org/p/chromium/issues/detail?id=840251
-  # https://bugs.chromium.org/p/chromium/issues/detail?id=849689
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/962f0f/qt/xcode10.diff"
-    sha256 "c064398411c69f2e1c516c0cd49fcd0755bc29bb19e65c5694c6d726c43389a6"
-  end
 
   def install
     args = %W[
