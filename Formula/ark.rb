05c5383abb98bedc90b37c203e788452efa6ab22
Scott Crooks <scott.crooks@gmail.com>
2018-10-18 20:30:56 +0200

    ark 0.9.8
    Closes #33140.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/ark.rb
+++ Formula/ark.rb
@@ -1,8 +1,8 @@
 class Ark < Formula
   desc "Disaster recovery for Kubernetes cluster resources and persistent volumes"
   homepage "https://github.com/heptio/ark"
-  url "https://github.com/heptio/ark/archive/v0.9.7.tar.gz"
-  sha256 "35405b1a0ea244730adc95292ba42f3010e5ef5c624f00277ac6c28d1204e7c6"
+  url "https://github.com/heptio/ark/archive/v0.9.8.tar.gz"
+  sha256 "9e293363f4a5f53a77561f204b2055a29beae353b351e993fb62672f30cc93c5"
 
   bottle do
     cellar :any_skip_relocation
