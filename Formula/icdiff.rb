f7c2b5e97a88d24201bea27571cf1197ddca2a4f
Chongyu Zhu <i@lembacon.com>
2018-10-18 20:14:05 +0800

    icdiff 1.9.4
    Closes #33118.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/icdiff.rb
+++ Formula/icdiff.rb
@@ -1,8 +1,8 @@
 class Icdiff < Formula
   desc "Improved colored diff"
   homepage "https://github.com/jeffkaufman/icdiff"
-  url "https://github.com/jeffkaufman/icdiff/archive/release-1.9.3.tar.gz"
-  sha256 "090b0f96c5df5ae5d71f6e5325bca566dd47e8b61ec4b59d4e40acb9ffcb878f"
+  url "https://github.com/jeffkaufman/icdiff/archive/release-1.9.4.tar.gz"
+  sha256 "dabceff1986d45f1e0e6a396ed71836586acfb99092a91303f14052b879ca59a"
   head "https://github.com/jeffkaufman/icdiff.git"
 
   bottle :unneeded
