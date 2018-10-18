e82cf19cd23539fc8f632d5b4e6da10b414d2abc
commitay <commitay@users.noreply.github.com>
2018-10-19 08:32:21 +1000

    git-annex 6.20181011
    Closes #33148.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/git-annex.rb
+++ Formula/git-annex.rb
@@ -5,8 +5,8 @@ class GitAnnex < Formula
 
   desc "Manage files with git without checking in file contents"
   homepage "https://git-annex.branchable.com/"
-  url "https://hackage.haskell.org/package/git-annex-6.20180926/git-annex-6.20180926.tar.gz"
-  sha256 "ad9c32dc2d01bc0b521936156f8ab8247ce8c131f083899d06c30f0391cca188"
+  url "https://hackage.haskell.org/package/git-annex-6.20181011/git-annex-6.20181011.tar.gz"
+  sha256 "ee319b305b9279cd11fd5da08882d62cf533a577243c3a37a4cda55766de284c"
   head "git://git-annex.branchable.com/"
 
   bottle do
@@ -15,15 +15,13 @@ class GitAnnex < Formula
     sha256 "3c51fa068af0798623d4e23ee763fe0503d4b880c4822c053b4a7381dec0ac3b" => :sierra
   end
 
-  option "with-git-union-merge", "Build the git-union-merge tool"
-
   depends_on "cabal-install" => :build
   depends_on "ghc@8.2" => :build
   depends_on "pkg-config" => :build
   depends_on "gsasl"
   depends_on "libmagic"
   depends_on "quvi"
-  depends_on "xdot" => :recommended
+  depends_on "xdot"
 
   def install
     # Reported 28 Feb 2018 to aws upstream https://github.com/aristidb/aws/issues/244
@@ -33,14 +31,7 @@ class GitAnnex < Formula
     install_cabal_package "--constraint", "http-conduit<2.3",
                           "--constraint", "network<2.7.0.1",
                           :using => ["alex", "happy", "c2hs"],
-                          :flags => ["s3", "webapp"] do
-      # this can be made the default behavior again once git-union-merge builds properly when bottling
-      if build.with? "git-union-merge"
-        system "make", "git-union-merge", "PREFIX=#{prefix}"
-        bin.install "git-union-merge"
-        system "make", "git-union-merge.1", "PREFIX=#{prefix}"
-      end
-    end
+                          :flags => ["s3", "webapp"]
     bin.install_symlink "git-annex" => "git-annex-shell"
   end
 
