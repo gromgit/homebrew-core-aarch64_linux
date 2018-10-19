9ac2e6e670f3ad74477ba45f7468935d39fd7b98
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-19 14:19:38 +0000

    tealdeer: add 1.0.0 bottle.

--- Formula/tealdeer.rb
+++ Formula/tealdeer.rb
@@ -4,6 +4,12 @@ class Tealdeer < Formula
   url "https://github.com/dbrgn/tealdeer/archive/v1.0.0.tar.gz"
   sha256 "9d9712e1b1a17c23793e81691ca6f8e4d45b7fd77efa300261e066c2d254705b"
 
+  bottle do
+    sha256 "6527b431ae61c93de802a0d2908f0602fe18dc938aae8e04c550a7c139fae814" => :mojave
+    sha256 "6fe1c7f4b93ce556af3e5c75bb5124646ce9ceaec80db82ce75538a98cf29a18" => :high_sierra
+    sha256 "505491bf49d382dd39c53a6df1ddce99d1a63d9202e23c9d7156dabb9a069fee" => :sierra
+  end
+
   depends_on "rust" => :build
 
   conflicts_with "tldr", :because => "both install `tldr` binaries"
