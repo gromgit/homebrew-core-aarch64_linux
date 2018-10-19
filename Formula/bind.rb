0115c9deb8f0c8b66eb2b374ac1ab22d0067c380
commitay <commitay@users.noreply.github.com>
2018-10-19 12:44:25 +1000

    bind 9.12.3
    Closes #33155.
    
    Signed-off-by: commitay <commitay@users.noreply.github.com>

--- Formula/bind.rb
+++ Formula/bind.rb
@@ -1,10 +1,8 @@
 class Bind < Formula
   desc "Implementation of the DNS protocols"
   homepage "https://www.isc.org/downloads/bind/"
-  url "https://ftp.isc.org/isc/bind9/9.12.2-P2/bind-9.12.2-P2.tar.gz"
-  version "9.12.2-P2"
-  sha256 "87027826e98bab90ead31f45ce7653cb3116ebe64ab8202a08b6b64531df693e"
-  revision 1
+  url "https://ftp.isc.org/isc/bind9/9.12.3/bind-9.12.3.tar.gz"
+  sha256 "0e80762631258e1c193552efa7c56c05ec5e8c2f98e4b2a3b91a61fd8d96b938"
   head "https://gitlab.isc.org/isc-projects/bind9.git"
 
   bottle do
