0dbc49a99c7f1cc76c0d4422fc9af02fc024b2bf
commitay <commitay@users.noreply.github.com>
2018-10-20 00:30:43 +1000

    erlang@19 19.3.6.12
    Closes #33177.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/erlang@19.rb
+++ Formula/erlang@19.rb
@@ -2,8 +2,8 @@ class ErlangAT19 < Formula
   desc "Programming language for highly scalable real-time systems"
   homepage "https://www.erlang.org/"
   # Download tarball from GitHub; it is served faster than the official tarball.
-  url "https://github.com/erlang/otp/archive/OTP-19.3.6.11.tar.gz"
-  sha256 "c857ea6d2c901bfb633d9ceeb5e05332475357f185dd5112b7b6e4db80072827"
+  url "https://github.com/erlang/otp/archive/OTP-19.3.6.12.tar.gz"
+  sha256 "f5ff65cde3f2db6f1dd61f6985136fcf87b55fa3f85e17ee513cc05dbe2da635"
 
   bottle do
     cellar :any
