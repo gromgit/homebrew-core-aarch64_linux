9aee8759562baac3e3c7b5b766bd2166b34fa0bc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-20 00:14:27 +0000

    vtk: update 8.1.1_2 bottle.

--- Formula/vtk.rb
+++ Formula/vtk.rb
@@ -7,10 +7,9 @@ class Vtk < Formula
   head "https://github.com/Kitware/VTK.git"
 
   bottle do
-    rebuild 1
-    sha256 "2f6b343c4fa0f875c3701792a0f148b619cb2da288c14b0606c4ef00ae5caf73" => :mojave
-    sha256 "91c0de753f55355f86297a4e9f14b32e18d65b32d23143f7aa0e9c7de4704cee" => :high_sierra
-    sha256 "9885f9cc2f509a4181c5783449945588ffcfb2086126decd18aa21958dc5244f" => :sierra
+    sha256 "9ebecbab78563a12b4a55f62d7c15afeab7b288891ba2b71d8f461b1d77b8b33" => :mojave
+    sha256 "f17ccc19e0183f06c07e6d7b2ac23f21a5056bb8b8bcb57f5411f656639d168a" => :high_sierra
+    sha256 "b0cce7d37ebea9901e98519412b7d8078499ca03ee4cc6ec283525ba9dd2f121" => :sierra
   end
 
   option "without-python@2", "Build without python2 support"
