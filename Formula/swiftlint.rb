5c5d0a3c10185419c5aedd2c4e0673e0d2d7c490
commitay <commitay@users.noreply.github.com>
2018-10-20 17:07:12 +1000

    swiftlint: remove workaround needed for `rdar://40724445` (#32449)

--- Formula/swiftlint.rb
+++ Formula/swiftlint.rb
@@ -17,8 +17,6 @@ class Swiftlint < Formula
   depends_on :xcode => "8.0"
 
   def install
-    ENV["CC"] = Utils.popen_read("xcrun -find clang").chomp # rdar://40724445
-
     system "make", "prefix_install", "PREFIX=#{prefix}", "TEMPORARY_FOLDER=#{buildpath}/SwiftLint.dst"
   end
 
