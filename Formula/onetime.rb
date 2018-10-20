552919bebe8e23911eeb4972c86528be3f06ac38
commitay <commitay@users.noreply.github.com>
2018-10-20 22:54:47 +1000

    onetime: remove devel spec

--- Formula/onetime.rb
+++ Formula/onetime.rb
@@ -1,25 +1,9 @@
 class Onetime < Formula
   desc "Encryption with one-time pads"
   homepage "https://www.red-bean.com/onetime/"
+  url "https://www.red-bean.com/onetime/onetime-1.81.tar.gz"
+  sha256 "36a83a83ac9f4018278bf48e868af00f3326b853229fae7e43b38d167e628348"
   revision 1
-
-  stable do
-    url "https://www.red-bean.com/onetime/onetime-1.81.tar.gz"
-    sha256 "36a83a83ac9f4018278bf48e868af00f3326b853229fae7e43b38d167e628348"
-
-    # Fixes the Makefile to permit destination specification
-    # https://github.com/kfogel/OneTime/pull/12
-    patch do
-      url "https://github.com/kfogel/OneTime/commit/61e534e2.patch?full_index=1"
-      sha256 "b74d1769e8719f06755c7c3c4ac759063b31d9d0554b64c5fb600c7edf5cc5ea"
-    end
-
-    # Follow up to PR12 to fix my clumsiness in a variable call.
-    patch do
-      url "https://github.com/kfogel/OneTime/commit/fb0a12f2.patch?full_index=1"
-      sha256 "11417d66886630f7a3c527f63227a75a39aee18029e60de99d7cb68ebe7769f5"
-    end
-  end
 
   bottle do
     cellar :any_skip_relocation
@@ -29,12 +13,17 @@ class Onetime < Formula
     sha256 "9f73f9cdb465fce1aefc3cf80c00bc8e43b41a33c3e999fb3ec531251cfc3da0" => :el_capitan
   end
 
-  devel do
-    url "https://www.red-bean.com/onetime/onetime-2.0-beta13.tar.gz"
-    # FIXME: I can't rememeber why the custom version was added now, but
-    # we're stuck with it now as 2.0-beta(n) is "less" than 2.0.0(n).
-    version "2.0.13"
-    sha256 "573c7f3a380a9c844dce7f8fe9dcc0086a7b0614b11a56f0b740617bc3dbfe52"
+  # Fixes the Makefile to permit destination specification
+  # https://github.com/kfogel/OneTime/pull/12
+  patch do
+    url "https://github.com/kfogel/OneTime/commit/61e534e2.patch?full_index=1"
+    sha256 "b74d1769e8719f06755c7c3c4ac759063b31d9d0554b64c5fb600c7edf5cc5ea"
+  end
+
+  # Follow up to PR12 to fix my clumsiness in a variable call.
+  patch do
+    url "https://github.com/kfogel/OneTime/commit/fb0a12f2.patch?full_index=1"
+    sha256 "11417d66886630f7a3c527f63227a75a39aee18029e60de99d7cb68ebe7769f5"
   end
 
   def install
