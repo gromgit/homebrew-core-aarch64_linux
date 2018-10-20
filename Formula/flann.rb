6461b224c95f0c4dbcb55c0b61a01dbd253ecfca
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-20 00:14:27 +0000

    flann: update 1.9.1_6 bottle.

--- Formula/flann.rb
+++ Formula/flann.rb
@@ -7,10 +7,9 @@ class Flann < Formula
 
   bottle do
     cellar :any
-    sha256 "e37ff0a9e3f91a64e1385381231b5b2eb348adb53342fcf7874ba24d5e409fd6" => :mojave
-    sha256 "afe4526a8147f967deb0499b9e3f63e9898e82f9904463852e00885311b4431b" => :high_sierra
-    sha256 "41e6706b4e0523632ee9116f76d33f0b2ed54ce8941da8a8219227bd943b9a1e" => :sierra
-    sha256 "ece27f1635df0469b200f62a026aa13a27b45ce62ddf1c3146c1933066a48ce2" => :el_capitan
+    sha256 "d63b6b851376c199e86938129b34616500be62354904b586e367af6108541609" => :mojave
+    sha256 "dc63d79be691c9b18d98b8c900fc5eb6d824feb2d4e2a8b7e5fd085827f70cfb" => :high_sierra
+    sha256 "9c825c76c662f0743adad10544fd7572a6d2449a59cc92da674da244ea2e209b" => :sierra
   end
 
   deprecated_option "with-python" => "with-python@2"
