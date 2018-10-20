62950ef4af6c7b6c58edc49b0b5c14555bf25a98
commitay <commitay@users.noreply.github.com>
2018-10-20 22:46:27 +1000

    freerdp: remove devel spec

--- Formula/freerdp.rb
+++ Formula/freerdp.rb
@@ -23,24 +23,13 @@ class Freerdp < Formula
       sha256 "2e8f68a0dbe6e2574dec3353e65a4f03d76a3398f8fac536fda08c24748aec2b"
     end
   end
+
   bottle do
     sha256 "4617cb1c7c5ba4e37d7a490d8712dae6be05167cf61de4e5f56e03752d625658" => :mojave
     sha256 "849254a5206feace01a07302a2e483c82cdc2403b85c6ad6a13917da19a53fb4" => :high_sierra
     sha256 "429e7a072afb895dbb2dd35a5d0ec64d6225c42af1e81c415454edd94336a5cf" => :sierra
     sha256 "6f7c7f1ccfe392cdd983e53ff662f7dae3c2ea94f47637760dc2e41fb707f172" => :el_capitan
     sha256 "a371c32a6fd8c6ccd28dad7260ecc84b042128f157aa85f2e2ffe9efcc19e0bc" => :yosemite
-  end
-
-  devel do
-    url "https://github.com/FreeRDP/FreeRDP/archive/4c69c3ea1489f09e1c3e698eaebd67b6d8d25785.tar.gz" # stable-1.1 branch as of Aug 13, 2016
-    sha256 "1a4b03daca9bd82ef2e3827e1cdb469121f62aab1061f79ffa587babf9fe080c"
-    version "1.1.0-beta1"
-    depends_on :xcode => :build # for "ibtool"
-
-    patch do
-      url "https://github.com/untoldone/FreeRDP/commit/65e53694eb5d6faa5d57a31a54defd1c5233ac09.diff?full_index=1"
-      sha256 "997d4ae77151bd500db4494492b6ab7d71cf3dfe187b2ba9db595fc13de5a3ff"
-    end
   end
 
   head do
@@ -55,7 +44,7 @@ class Freerdp < Formula
 
   def install
     cmake_args = std_cmake_args
-    cmake_args << "-DWITH_X11=ON" << "-DBUILD_SHARED_LIBS=ON" if build.head? || build.devel?
+    cmake_args << "-DWITH_X11=ON" << "-DBUILD_SHARED_LIBS=ON" if build.head?
     system "cmake", ".", *cmake_args
     system "make", "install"
   end
