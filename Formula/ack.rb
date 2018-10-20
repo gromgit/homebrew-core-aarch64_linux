ce6e7c04715de55e5eb20327a4dac13b2d757189
commitay <commitay@users.noreply.github.com>
2018-10-20 17:30:50 +1000

    ack: remove devel spec

--- Formula/ack.rb
+++ Formula/ack.rb
@@ -3,27 +3,20 @@ class Ack < Formula
   homepage "https://beyondgrep.com/"
   url "https://beyondgrep.com/ack-2.24-single-file"
   sha256 "8361e5a2654bc575db27bfa40470c4182d74d51098d390944d98fe7cd5b20d49"
-  head "https://github.com/petdance/ack2.git", :branch => "dev"
 
-  devel do
-    url "https://beyondgrep.com/ack-2.999_03-single-file"
-    version "2.999-03"
-    sha256 "7e75bbe0baffac14aa7798198faa2184241990a72ff0150f157ef6296a148e3c"
+  head do
+    url "https://github.com/petdance/ack2.git", :branch => "dev"
+
+    resource "File::Next" do
+      url "https://cpan.metacpan.org/authors/id/P/PE/PETDANCE/File-Next-1.16.tar.gz"
+      sha256 "6965f25c2c132d0ba7a6f72b57b8bc6d25cf8c1b7032caa3a9bda8612e41d759"
+    end
   end
 
   bottle :unneeded
 
-  resource "File::Next" do
-    url "https://cpan.metacpan.org/authors/id/P/PE/PETDANCE/File-Next-1.16.tar.gz"
-    sha256 "6965f25c2c132d0ba7a6f72b57b8bc6d25cf8c1b7032caa3a9bda8612e41d759"
-  end
-
   def install
-    if build.stable? || build.devel?
-      bin.install "ack-#{version.to_s.tr("-", "_")}-single-file" => "ack"
-      system "pod2man", "#{bin}/ack", "ack.1"
-      man1.install "ack.1"
-    else
+    if build.head?
       ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
       ENV.prepend_path "PERL5LIB", libexec/"lib"
 
@@ -40,6 +33,10 @@ class Ack < Formula
       (libexec/"lib").install "blib/lib/App"
       (bin/"ack").write_env_script("#{libexec}/ack", :PERL5LIB => ENV["PERL5LIB"])
       man1.install "blib/man1/ack.1"
+    else
+      bin.install "ack-#{version.to_s.tr("-", "_")}-single-file" => "ack"
+      system "pod2man", "#{bin}/ack", "ack.1"
+      man1.install "ack.1"
     end
   end
 
