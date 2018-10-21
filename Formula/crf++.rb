2ec6b29d13b9ccfbbea197cd9051cce1e81c1b04
FX Coudert <fxcoudert@gmail.com>
2018-10-21 13:30:12 +0200

    crf++: update URL
    Closes #33232.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/crf++.rb
+++ Formula/crf++.rb
@@ -1,8 +1,8 @@
 class Crfxx < Formula
   desc "Conditional random fields for segmenting/labeling sequential data"
   homepage "https://taku910.github.io/crfpp/"
-  url "https://drive.google.com/uc?id=0B4y35FiV1wh7QVR6VXJ5dWExSTQ&export=download"
-  version "0.58"
+  url "https://ftp.heanet.ie/mirrors/gentoo.org/distfiles/CRF++-0.58.tar.gz"
+  mirror "https://drive.google.com/uc?id=0B4y35FiV1wh7QVR6VXJ5dWExSTQ&export=download"
   sha256 "9d1c0a994f25a5025cede5e1d3a687ec98cd4949bfb2aae13f2a873a13259cb2"
 
   bottle do
@@ -21,6 +21,7 @@ class Crfxx < Formula
 
   test do
     # Using "; true" because crf_test -v and -h exit nonzero under normal operation
-    assert_match "CRF++: Yet Another CRF Tool Kit", shell_output("crf_test --help; true")
+    output = shell_output("#{bin}/crf_test --help; true")
+    assert_match "CRF++: Yet Another CRF Tool Kit", output
   end
 end
