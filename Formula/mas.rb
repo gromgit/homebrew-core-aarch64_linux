e80654ba997232fba804781bf5d8da14283de044
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-21 01:01:35 +0000

    mas: update 1.4.3 bottle.

--- Formula/mas.rb
+++ Formula/mas.rb
@@ -7,10 +7,9 @@ class Mas < Formula
   head "https://github.com/mas-cli/mas.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "fb05286adfb9ab0a63a8027391647fb874fa810c07d412505f5bc7911139a3f2" => :mojave
-    sha256 "b7585ced3a93d60e95357e93d729913a6f628fda82359e77c6553c2e802c50dc" => :high_sierra
-    sha256 "af5be6aa9902d9cfc2aa69dbf313441a7c201463d516face721f900ceae9556b" => :sierra
+    cellar :any
+    sha256 "d3668e4d128dfc8e062adc30c543ded35e7726dd9e021696e32a97d484e465fd" => :mojave
+    sha256 "fc6658113d785a660e3f4d2e4e134ad02fe003ffa7d69271a2c53f503aaae726" => :high_sierra
   end
 
   depends_on "carthage" => :build
