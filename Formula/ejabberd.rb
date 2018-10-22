8a615777994515c8225260e8ffcf6e249e54966c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-22 21:07:10 +0000

    ejabberd: update 18.06 bottle.

--- Formula/ejabberd.rb
+++ Formula/ejabberd.rb
@@ -5,9 +5,10 @@ class Ejabberd < Formula
   sha256 "ac9dbe8b58aec5403ecfe48cb90f11e7f55664185094bac94615c6c9323690b0"
 
   bottle do
-    sha256 "0a4bc49006e4d917c28848056ef3abf54cb9057a6fc953e2caaf9586b5c4aacd" => :high_sierra
-    sha256 "12380faf294c170e1e8475717b8072c3a52388583868366a05000817c6e62584" => :sierra
-    sha256 "e80a35aae6b8109ad5ed4b1f127eb80c03b9c92c10ee104be5ecd24f6d002a4a" => :el_capitan
+    rebuild 1
+    sha256 "0098730930d77aaf10b75359db1517dee2d55abd92b229776a3b0757413728aa" => :mojave
+    sha256 "5665a7f8986e4d2f032697fd84b9731cb171865fa48da6a360cbe68c2527fc46" => :high_sierra
+    sha256 "d73f35ef3c7aa4761ad22b743d083ca8a19c8520fd40be9bda1803a6e60f0887" => :sierra
   end
 
   head do
