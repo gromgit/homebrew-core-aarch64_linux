e7a5ec31fdb6dfb88f9b74cf25023a8b0916ab42
John Colvin <john.loughran.colvin@gmail.com>
2018-10-22 13:29:35 +0100

    sqliteodbc: fix install
    Closes #33265.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/sqliteodbc.rb
+++ Formula/sqliteodbc.rb
@@ -1,7 +1,7 @@
 class Sqliteodbc < Formula
   desc "SQLite ODBC driver"
   homepage "http://www.ch-werner.de/sqliteodbc/"
-  url "http://www.ch-werner.de/sqliteodbc/sqliteodbc-0.9996.tar.gz"
+  url "http://www.ch-werner.homepage.t-online.de/sqliteodbc/sqliteodbc-0.9996.tar.gz"
   sha256 "8afbc9e0826d4ff07257d7881108206ce31b5f719762cbdb4f68201b60b0cb4e"
 
   bottle do
@@ -16,8 +16,10 @@ class Sqliteodbc < Formula
   depends_on "unixodbc"
 
   def install
+    ENV["SDKROOT"] = MacOS.sdk_path if MacOS.version == :sierra
+
     lib.mkdir
-    system "./configure", "--prefix=#{prefix}", "--with-odbc=#{Formula["unixodbc"].opt_prefix}"
+    system "./configure", "--prefix=#{prefix}", "--with-odbc=#{Formula["unixodbc"].opt_prefix}", "--with-sqlite3=#{Formula["sqlite"].opt_prefix}"
     system "make"
     system "make", "install"
     lib.install_symlink "#{lib}/libsqlite3odbc.dylib" => "libsqlite3odbc.so"
