8b79dffbd7c962fd4e3ea68994d20201d4870629
Roman Ivanov <me@roivanov.com>
2018-10-22 22:30:00 -0700

    groovy 2.5.3
    Closes #33292.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/groovy.rb
+++ Formula/groovy.rb
@@ -1,8 +1,8 @@
 class Groovy < Formula
   desc "Java-based scripting language"
   homepage "http://www.groovy-lang.org"
-  url "https://dl.bintray.com/groovy/maven/apache-groovy-binary-2.5.2.zip"
-  sha256 "495f96f1be35ef838abb5f1c10fc6f642460cacd94c7095eb3a9f1fbf62b282e"
+  url "https://dl.bintray.com/groovy/maven/apache-groovy-binary-2.5.3.zip"
+  sha256 "47942bdb31a9f7ebf0db5b324bc99a289fd6ff87723e21e06f4701fa7c8c85c7"
 
   bottle :unneeded
 
