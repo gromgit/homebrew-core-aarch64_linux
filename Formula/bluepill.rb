785847deb184d35170f3d17cecd595259fc07574
Keqiu Hu <khu@linkedin.com>
2018-10-23 14:00:22 -0700

    bluepill 3.1.0
    Closes #33322.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/bluepill.rb
+++ Formula/bluepill.rb
@@ -1,8 +1,8 @@
 class Bluepill < Formula
   desc "iOS testing tool that runs UI tests using multiple simulators"
   homepage "https://github.com/linkedin/bluepill"
-  url "https://github.com/linkedin/bluepill/archive/2.4.0.tar.gz"
-  sha256 "fe021b742e04ace637fe2795848644842ab049e2e2c6d861a088785ef6821461"
+  url "https://github.com/linkedin/bluepill/archive/3.1.0.tar.gz"
+  sha256 "9fe609d15cb6329df9cb02f1360b895e873f70816b13c2cb4c0ca6dd7641a9c5"
   head "https://github.com/linkedin/bluepill.git"
 
   bottle do
@@ -11,7 +11,7 @@ class Bluepill < Formula
     sha256 "a5fb40247ab03c018bf249dc9ec665f57e328c28afb1fbcda6cff9d739888968" => :sierra
   end
 
-  depends_on :xcode => ["9.0", :build]
+  depends_on :xcode => ["10.0", :build]
 
   def install
     xcodebuild "-workspace", "Bluepill.xcworkspace",
