4820e4597aa8701469a042e090f675b37254af2a
Viktor Szakats <commit@vszakats.net>
2018-10-23 00:30:48 +0000

    wait_on: secure url(s)

--- Formula/wait_on.rb
+++ Formula/wait_on.rb
@@ -1,7 +1,7 @@
 class WaitOn < Formula
   desc "Provides shell scripts with access to kqueue(3)"
   homepage "https://www.freshports.org/sysutils/wait_on/"
-  url "http://distcache.freebsd.org/ports-distfiles/wait_on-1.1.tar.gz"
+  url "https://pkg.freebsd.org/ports-distfiles/wait_on-1.1.tar.gz"
   mirror "https://mirrorservice.org/sites/distfiles.macports.org/wait_on/wait_on-1.1.tar.gz"
   sha256 "d7f40655f5c11e882890340826d1163050e2748de66b292c15b10d32feb6490f"
 
