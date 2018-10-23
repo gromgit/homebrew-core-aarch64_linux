c7d99fb3ead1f38e7e8a59152aa37e393a203167
Viktor Szakats <commit@vszakats.net>
2018-10-23 00:30:48 +0000

    wtf: secure url(s)

--- Formula/wtf.rb
+++ Formula/wtf.rb
@@ -1,6 +1,6 @@
 class Wtf < Formula
   desc "Translate common Internet acronyms"
-  homepage "http://cvsweb.netbsd.org/bsdweb.cgi/src/games/wtf/"
+  homepage "https://sourceforge.net/projects/bsdwtf/"
   url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20180925.tar.gz"
   sha256 "87c695c166de5d47bd41661fbd3487e313bee5811a7d5697e05d35fcd2af9eac"
 
