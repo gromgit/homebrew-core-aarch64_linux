10e1e1d5c8c6db0486962d2607312696f98be554
FX Coudert <fxcoudert@gmail.com>
2018-10-24 12:42:02 +0200

    source-highlight: revision for boost

--- Formula/source-highlight.rb
+++ Formula/source-highlight.rb
@@ -5,7 +5,7 @@ class SourceHighlight < Formula
   mirror "https://ftpmirror.gnu.org/src-highlite/source-highlight-3.1.8.tar.gz"
   mirror "https://fossies.org/linux/www/source-highlight-3.1.8.tar.gz"
   sha256 "01336a7ea1d1ccc374201f7b81ffa94d0aecb33afc7d6903ebf9fbf33a55ada3"
-  revision 9
+  revision 10
 
   bottle do
     sha256 "ed1d149a9f97855caee3f659c371a1478b68454efdadee059e34597218b982fe" => :mojave
