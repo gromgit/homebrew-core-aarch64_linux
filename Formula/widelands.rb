d5cab4b1b23066efaad5c9551c54ad498194dfa3
FX Coudert <fxcoudert@gmail.com>
2018-10-24 12:42:02 +0200

    widelands: revision for boost

--- Formula/widelands.rb
+++ Formula/widelands.rb
@@ -3,7 +3,7 @@ class Widelands < Formula
   homepage "https://wl.widelands.org/"
   url "https://launchpad.net/widelands/build19/build19/+download/widelands-build19-src.tar.bz2"
   sha256 "e511f9d26828a2b71b64cdfc6674e6e847543b2da73961ab882acca36c7c01a6"
-  revision 11
+  revision 12
 
   bottle do
     sha256 "43e42b0992062b533fcd329b1a54182e23efbefdcef94ac9c8a1e9943c3de682" => :mojave
