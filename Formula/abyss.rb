c1f231f23c907db3befeaead5219585e275df0c5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-24 23:39:49 +0000

    abyss: update 2.1.2 bottle.

--- Formula/abyss.rb
+++ Formula/abyss.rb
@@ -5,10 +5,9 @@ class Abyss < Formula
   sha256 "15823fbfcdadf61f93b07a7d040106e51a7cbd091d4d71fd29809475ca0f4e71"
 
   bottle do
-    sha256 "80ab108b5ce547a63be711284367e2a29c7657eeee4f23ac0da1ec621113fe5f" => :mojave
-    sha256 "47829fcb41eca26386d6c5db4b2d3d7ff999205ddea5db86059911cbd426fa3f" => :high_sierra
-    sha256 "48729e38eecf5f49df1f05654a80e0539f5b44732f6ec43856e68b108dd96bce" => :sierra
-    sha256 "e82ef97ceb8f9e93afd70c435fbf5798b15a96d7dd9c86e6cd79713c0064cc15" => :el_capitan
+    sha256 "20117882aaab7c5ed8b14c6f0dcd99469fc9c1647635784ced685336229a2a0b" => :mojave
+    sha256 "98b4de087149b934c42d56137c744a70d9f46f03a7620ef42df62c0029789ecb" => :high_sierra
+    sha256 "2dae278d8ab9d91b0bf7057db60e903c7979686ea3aa77f6deff3eda3e19111c" => :sierra
   end
 
   head do
