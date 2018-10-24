5a6ea5cebaba947a235275ddb1dfbde2aaab6c61
FX Coudert <fxcoudert@gmail.com>
2018-10-24 12:42:00 +0200

    cclive: revision for boost

--- Formula/cclive.rb
+++ Formula/cclive.rb
@@ -3,7 +3,7 @@ class Cclive < Formula
   homepage "https://cclive.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/cclive/0.7/cclive-0.7.16.tar.xz"
   sha256 "586a120faddcfa16f5bb058b5c901f1659336c6fc85a0d3f1538882a44ee10e1"
-  revision 2
+  revision 3
 
   bottle do
     cellar :any
