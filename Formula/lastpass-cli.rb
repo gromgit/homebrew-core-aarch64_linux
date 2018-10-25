4b6109620e95807e2b08cc0d8a8894863c935be7
Pete Woods <email@pete-woods.com>
2018-10-25 21:29:24 +0100

    lastpass-cli: fix for Mojave
    Closes #32695.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/lastpass-cli.rb
+++ Formula/lastpass-cli.rb
@@ -3,6 +3,7 @@ class LastpassCli < Formula
   homepage "https://github.com/lastpass/lastpass-cli"
   url "https://github.com/lastpass/lastpass-cli/archive/v1.3.1.tar.gz"
   sha256 "25dc9a0c99a10ee70b5b3991d525448c25f312cc69fa0216d7ac70c4ae384b1b"
+  revision 1
   head "https://github.com/lastpass/lastpass-cli.git"
 
   bottle do
@@ -18,13 +19,18 @@ class LastpassCli < Formula
   depends_on "cmake" => :build
   depends_on "docbook-xsl" => :build
   depends_on "pkg-config" => :build
+  # Avoid crashes on Mojave's version of libcurl (https://github.com/lastpass/lastpass-cli/issues/427)
+  depends_on "curl" if MacOS.version >= :mojave
   depends_on "openssl"
-  depends_on "pinentry" => :optional
+  depends_on "pinentry"
 
   def install
     ENV["XML_CATALOG_FILES"] = etc/"xml/catalog"
-    system "make", "PREFIX=#{prefix}", "install"
-    system "make", "MANDIR=#{man}", "install-doc"
+
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args, "-DCMAKE_INSTALL_MANDIR:PATH=#{man}"
+      system "make", "all", "lpass-test", "test", "install", "install-doc"
+    end
 
     bash_completion.install "contrib/lpass_bash_completion"
     zsh_completion.install "contrib/lpass_zsh_completion" => "_lpass"
