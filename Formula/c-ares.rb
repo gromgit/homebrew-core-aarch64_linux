390dec033b181d797cd17f857253e0b553d1057d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-26 16:38:16 +0000

    c-ares: update 1.15.0 bottle.

--- Formula/c-ares.rb
+++ Formula/c-ares.rb
@@ -6,10 +6,9 @@ class CAres < Formula
 
   bottle do
     cellar :any
-    sha256 "81aa80ae8e49b0a8d44f8753b94424e6f044144361e13a9f7fe138ea09ef16f7" => :mojave
-    sha256 "90ff616a7b816ce090d1319e541ad45d7b515c2787007d10b7eace9601fb2396" => :high_sierra
-    sha256 "82c529566254c10dd71d09ae23281cf4a2c75d16200cc4df6d406ddecde5ba16" => :sierra
-    sha256 "3c9943c455dbb8a0f641f78fd93dec6a27937494021b8f94f1bfe381ded46ca3" => :el_capitan
+    sha256 "6c1782cd4a17e74f8f7b6258faa754ddcf9bf3a388cb862d6b5da8832668f9ef" => :mojave
+    sha256 "45fcb6953de6f43026bc28f3d8798ced5e5a0bbd28c18240fc0e9bc66174fc1e" => :high_sierra
+    sha256 "3d2c45de57a6c1e9fe867d67fa4509d2d42aae5fef8f5d5fc1ea34afff2ff22b" => :sierra
   end
 
   head do
