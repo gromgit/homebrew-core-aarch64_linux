e0c0772d4c6e22b6cbef70276fdbfbf442413055
Chongyu Zhu <i@lembacon.com>
2018-10-26 23:10:40 +0800

    fonttools 3.31.0
    Closes #33408.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -3,8 +3,8 @@ class Fonttools < Formula
 
   desc "Library for manipulating fonts"
   homepage "https://github.com/fonttools/fonttools"
-  url "https://github.com/fonttools/fonttools/releases/download/3.30.0/fonttools-3.30.0.zip"
-  sha256 "95e86519b18183dc3c230e9b2233a69add3f631ec43c8725a553844a7d12c2c4"
+  url "https://github.com/fonttools/fonttools/releases/download/3.31.0/fonttools-3.31.0.zip"
+  sha256 "c9a726a29fb4e573ee18b296e0a193ca18bc3d3ecf1c78bd4d92c77aa7a92752"
   head "https://github.com/fonttools/fonttools.git"
 
   bottle do
@@ -15,10 +15,7 @@ class Fonttools < Formula
     sha256 "b44296025ff97aa5ae3852bd70be5e682c1e767d55fa5a8d5654f29b81eb2499" => :el_capitan
   end
 
-  option "with-pygtk", "Build with pygtk support for pyftinspect"
-
-  depends_on "python@2"
-  depends_on "pygtk" => :optional
+  depends_on "python"
 
   def install
     virtualenv_install_with_resources
