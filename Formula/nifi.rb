98404f59043f135871d5c2e0b0c55c7a956f5dc8
Dustin Rodrigues <dust.rod@gmail.com>
2018-10-26 23:32:00 -0400

    nifi 1.8.0
    Closes #33431.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/nifi.rb
+++ Formula/nifi.rb
@@ -1,8 +1,8 @@
 class Nifi < Formula
   desc "Easy to use, powerful, and reliable system to process and distribute data"
   homepage "https://nifi.apache.org"
-  url "https://www.apache.org/dyn/closer.cgi?path=/nifi/1.7.1/nifi-1.7.1-bin.tar.gz"
-  sha256 "14441c02c47541981746adc8a6c5d8759a9bf8324a08bc03c8487b1112e2c515"
+  url "https://www.apache.org/dyn/closer.cgi?path=/nifi/1.8.0/nifi-1.8.0-bin.tar.gz"
+  sha256 "11561e8d505e76af5060a8b039d7ba1ecfe966722e5b824b4d4eed60ea796071"
 
   bottle :unneeded
 
