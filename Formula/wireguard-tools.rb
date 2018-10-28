f7ddc28fb36a5763bb7838680b9f8b7ee4cd7e34
Guillaume Mazoyer <guillaume@mazoyer.eu>
2018-10-28 15:44:10 +0100

    wireguard-tools 0.0.20181018
    Closes #33459.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/wireguard-tools.rb
+++ Formula/wireguard-tools.rb
@@ -3,8 +3,8 @@ class WireguardTools < Formula
   homepage "https://www.wireguard.com/"
   # Please only update version when the tools have been modified/updated,
   # since the Linux module aspect isn't of utility for us.
-  url "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-0.0.20181007.tar.xz"
-  sha256 "d26e0d1216594871b5947e76d64c2fa50e9b34b68cdcfa3fdad588cbb314af89"
+  url "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-0.0.20181018.tar.xz"
+  sha256 "af05824211b27cbeeea2b8d6b76be29552c0d80bfe716471215e4e43d259e327"
   head "https://git.zx2c4.com/WireGuard", :using => :git
 
   bottle do
