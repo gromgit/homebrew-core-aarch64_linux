854981eb0515bdd5237c00de5bcea5d9bc96aeef
Priti Desai <pdesai@us.ibm.com>
2018-10-29 16:07:26 -0700

    wskdeploy 0.9.9
    Closes #33494.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/wskdeploy.rb
+++ Formula/wskdeploy.rb
@@ -1,9 +1,8 @@
 class Wskdeploy < Formula
   desc "Apache OpenWhisk project deployment utility"
   homepage "https://openwhisk.apache.org/"
-  url "https://github.com/apache/incubator-openwhisk-wskdeploy/archive/0.9.8-incubating.tar.gz"
-  version "0.9.8"
-  sha256 "10ff548bf43448d734e0e6f4b6aee8d306759a4ff8a5a7deaa61329c0f3376a6"
+  url "https://github.com/apache/incubator-openwhisk-wskdeploy/archive/0.9.9.tar.gz"
+  sha256 "fa0164b9262b90c57cee868de000459ae8461042c0984d40ce22bf0c0ce4a49f"
 
   bottle do
     cellar :any_skip_relocation
