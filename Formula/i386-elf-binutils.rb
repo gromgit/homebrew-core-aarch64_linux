95f87ac9de77746df28eec7ea66514681c525d74
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-30 14:13:15 +0000

    i386-elf-binutils: add 2.31.1 bottle.

--- Formula/i386-elf-binutils.rb
+++ Formula/i386-elf-binutils.rb
@@ -4,6 +4,12 @@ class I386ElfBinutils < Formula
   url "https://ftp.gnu.org/gnu/binutils/binutils-2.31.1.tar.xz"
   sha256 "5d20086ecf5752cc7d9134246e9588fa201740d540f7eb84d795b1f7a93bca86"
 
+  bottle do
+    sha256 "89312270dc465d7d2c622d7253acfecc7e8b0d5a5cc7688822c1dbb10ee7166e" => :mojave
+    sha256 "1f7b4aa5dd270750954ef0a177223843affc45214cb7f19fc5e85743608fc8a6" => :high_sierra
+    sha256 "7ca42ad9603b7e336f029ae87407c789f1533af638daa54d3db3f2bde2afbf0c" => :sierra
+  end
+
   def install
     system "./configure", "--target=i386-elf",
                           "--disable-multilib",
