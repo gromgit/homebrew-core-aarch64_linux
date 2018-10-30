114d2384db1d6fd86c3d6c6a916c85a141c2b6ae
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-30 18:53:47 +0000

    bison: update 3.2 bottle.

--- Formula/bison.rb
+++ Formula/bison.rb
@@ -6,10 +6,9 @@ class Bison < Formula
   sha256 "d1a42d81cd708fedf943d4d8b2f9d22871526ba0d781563231045fc6edb2617d"
 
   bottle do
-    sha256 "d12f195dfc10e7ddbe3ad79e646f43da607d1b57068b6c728e01c2029445743b" => :mojave
-    sha256 "07aeea960757c9db4633f532ffa57b526a8c0ade186caae63d985474e68e643f" => :high_sierra
-    sha256 "0deeabe5fe3de5d81b5940bb9f2c6a457dd6eac0a10d518c1483f618a6bef19e" => :sierra
-    sha256 "99c319dc82e2a823f9c16a3909931e0970dc5cd189badb224c57a5786eb44016" => :el_capitan
+    sha256 "fa0524d2ce9d270a754bec391da539b04130934af50196bbd56c6363a25a7922" => :mojave
+    sha256 "640477dd11d7e6a481b096607867b103bf2d929af21e5aa4b1e9258360cbea64" => :high_sierra
+    sha256 "80da8c6f0bbb8dfcf6d823a8e5bb58135cb2246e1c6856993b0f65435ee130d0" => :sierra
   end
 
   keg_only :provided_by_macos, "some formulae require a newer version of bison"
