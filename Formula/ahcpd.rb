387769e99fd8fa22964ae9524de32796929b76a8
Mauro Morales <contact@mauromorales.com>
2018-10-30 10:18:30 +0100

    ahcpd: add test (#33469)

--- Formula/ahcpd.rb
+++ Formula/ahcpd.rb
@@ -20,6 +20,31 @@ class Ahcpd < Formula
     system "make", "LDLIBS=''"
     system "make", "install", "PREFIX=", "TARGET=#{prefix}"
   end
+
+  test do
+    pid_file = testpath/"ahcpd.pid"
+    log_file = testpath/"ahcpd.log"
+    mkdir testpath/"leases"
+
+    (testpath/"ahcpd.conf").write <<~EOS
+      mode server
+
+      prefix fde6:20f5:c9ac:358::/64
+      prefix 192.168.4.128/25
+      lease-dir #{testpath}/leases
+      name-server fde6:20f5:c9ac:358::1
+      name-server 192.168.4.1
+      ntp-server 192.168.4.2
+    EOS
+
+    system "#{bin}/ahcpd", "-c", "ahcpd.conf", "-I", pid_file, "-L", log_file, "-D", "lo0"
+    sleep(2)
+
+    assert_predicate pid_file, :exist?, "The file containing the PID of the child process was not created."
+    assert_predicate log_file, :exist?, "The file containing the log was not created."
+
+    Process.kill("TERM", pid_file.read.to_i)
+  end
 end
 
 __END__
