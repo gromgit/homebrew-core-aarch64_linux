69d91448cdee0e62d12dde92bd3b8b34634a2a11
Mike McQuaid <mike@mikemcquaid.com>
2018-10-30 12:58:09 +0000

    braid: revision for keg-only ruby.

--- Formula/braid.rb
+++ Formula/braid.rb
@@ -4,6 +4,7 @@ class Braid < Formula
   url "https://github.com/cristibalan/braid.git",
       :tag      => "v1.1.2",
       :revision => "4a7eea721fd9c841e305b19ebd6e8c7006c52f53"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
