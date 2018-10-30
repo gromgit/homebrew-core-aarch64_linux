92479a26a35ac4897576074c4b964ac46b256b5b
Mauro Morales <contact@mauromorales.com>
2018-10-30 10:27:21 +0100

    grepcidr: add test (#33473)

--- Formula/grepcidr.rb
+++ Formula/grepcidr.rb
@@ -19,4 +19,16 @@ class Grepcidr < Formula
     bin.install "grepcidr"
     man1.install "grepcidr.1"
   end
+
+  test do
+    (testpath/"access.log").write <<~EOS
+      127.0.0.1 duck
+      8.8.8.8 duck
+      66.249.64.123 goose
+      192.168.0.1 duck
+    EOS
+
+    output = shell_output("#{bin}/grepcidr 66.249.64.0/19 access.log")
+    assert_equal "66.249.64.123 goose", output.strip
+  end
 end
