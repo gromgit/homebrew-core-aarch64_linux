83fcf50cb0227b1363115f283752aa3cc761f682
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-31 08:38:56 +0000

    xml-tooling-c: update 3.0.2_3 bottle.

--- Formula/xml-tooling-c.rb
+++ Formula/xml-tooling-c.rb
@@ -6,10 +6,9 @@ class XmlToolingC < Formula
   revision 3
 
   bottle do
-    sha256 "f9abea4ed4e7ecebb332288f4341f817c3fbd57853938ef9a115c5abd16e164c" => :mojave
-    sha256 "a84eb9b4d9ec6d1112e90f5a7db858ddaf174a44d296ae83992da42f74007d07" => :high_sierra
-    sha256 "bd80c1aadca3759c995f39d16291623752df1566f37e8f5b75bbffcaad2a0f15" => :sierra
-    sha256 "d9ce481e3c1a53f864ddd93b03573acff1003639a2816bd4a1bfc5c984e33e5b" => :el_capitan
+    sha256 "102b92e9c2bbaa974b28672e9059494686c6fbdff6a6200a8a81522b357b475d" => :mojave
+    sha256 "5ffb76b87609c1fb643efc243978e1fca42db49748a811a1e71b5d96f66a6841" => :high_sierra
+    sha256 "b3ac28676aa355c5fd9ae8802ab9d26e6df0cbd187d9f1a3648c92773d04235a" => :sierra
   end
 
   depends_on "pkg-config" => :build
