a08bfcfc99d3e230afd78c8e2d8928560bb60e4f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-31 16:07:38 +0000

    solid: update 3.5.6 bottle.

--- Formula/solid.rb
+++ Formula/solid.rb
@@ -6,13 +6,10 @@ class Solid < Formula
 
   bottle do
     cellar :any
-    sha256 "4ba23271be7dc1c41419901ee6a0c5195f7ebf773164091225d1220e21ebbccd" => :mojave
-    sha256 "010d867aa6dfba7ae6ff9b5aa1a504285d45ccd9b682d512f70bc79cde208f8a" => :high_sierra
-    sha256 "a79692fbc3d3c150bedef4cbb5455ca175f9534703da91bc27abaac157f1414d" => :sierra
-    sha256 "217b3ce0111e94edfbbae5f82c07d3a8c129e7c5733a7707eb592b80eeb94a75" => :el_capitan
-    sha256 "e54bf158f4e91171c55d5514abe6fd36ca358e39e8271985f446be4002b6a5e7" => :yosemite
-    sha256 "b22bde511d5fa7a1112ef122fa70e5260ac6491bdfd3f8eeb19848b5b769621d" => :mavericks
-    sha256 "db9bd9a409bc447657f759852aa02feb0bc19cd84c552b9c16dfed130c2a3829" => :mountain_lion
+    rebuild 1
+    sha256 "8c7fd219da510e1821b50069ffbcc3025bee102a1ada47fe4b3f9464507fb1bc" => :mojave
+    sha256 "30954dffe6674f98523b3cb299f909aefbe554b70000cd777df75c326edf80d0" => :high_sierra
+    sha256 "2836475cd2195c3906950c2a62ed618302e3f57ba4c348a82b737fcb0956fc07" => :sierra
   end
 
   # This patch fixes a broken build on clang-600.0.56.
