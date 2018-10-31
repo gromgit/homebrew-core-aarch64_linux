fe1af5f3c23cdcb6c4650a95d1d9d09b4c6043ec
Paulo Moura <pmoura@logtalk.org>
2018-10-31 19:18:06 +0000

    logtalk 3.21.0
    Closes #33569.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/logtalk.rb
+++ Formula/logtalk.rb
@@ -1,9 +1,9 @@
 class Logtalk < Formula
   desc "Object-oriented logic programming language"
   homepage "https://logtalk.org/"
-  url "https://github.com/LogtalkDotOrg/logtalk3/archive/lgt3200stable.tar.gz"
-  version "3.20.0"
-  sha256 "800b66ad4b7d803f06c666430c8586e4f40b1cc78ffe6e66b223e1705a7839eb"
+  url "https://github.com/LogtalkDotOrg/logtalk3/archive/lgt3210stable.tar.gz"
+  version "3.21.0"
+  sha256 "8f3edcbd0d6dacbb38cdf6b11c0578bf8082dfc09cafd171cd565cfcd638e23a"
 
   bottle do
     cellar :any_skip_relocation
@@ -13,17 +13,7 @@ class Logtalk < Formula
     sha256 "dba02ef1ea3cfbe61cf2dee60595649066bc4fd56d14879c726a8d7f0167b2a5" => :el_capitan
   end
 
-  option "with-swi-prolog", "Build using SWI Prolog as backend"
-  option "with-gnu-prolog", "Build using GNU Prolog as backend (Default)"
-
-  deprecated_option "swi-prolog" => "with-swi-prolog"
-  deprecated_option "gnu-prolog" => "with-gnu-prolog"
-
-  if build.with? "swi-prolog"
-    depends_on "swi-prolog"
-  else
-    depends_on "gnu-prolog"
-  end
+  depends_on "gnu-prolog"
 
   def install
     cd("scripts") { system "./install.sh", "-p", prefix }
