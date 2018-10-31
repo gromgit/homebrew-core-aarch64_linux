d490f95b317f0f121b88b53d3bc9d073d448d1cb
FX Coudert <fxcoudert@gmail.com>
2018-10-31 11:28:02 +0100

    freetds@0.91: remove options

--- Formula/freetds@0.91.rb
+++ Formula/freetds@0.91.rb
@@ -3,6 +3,7 @@ class FreetdsAT091 < Formula
   homepage "http://www.freetds.org/"
   url "http://www.freetds.org/files/stable/freetds-0.91.112.tar.gz"
   sha256 "be4f04ee57328c32e7e7cd7e2e1483e535071cec6101e46b9dd15b857c5078ed"
+  revision 1
 
   bottle do
     sha256 "fbe4fd7ae0a297de1d74dc880b4cf188567f7f36d9d4677bb2b8af839da9d6aa" => :mojave
@@ -14,36 +15,19 @@ class FreetdsAT091 < Formula
 
   keg_only :versioned_formula
 
-  option "with-msdblib", "Enable Microsoft behavior in the DB-Library API where it diverges from Sybase's"
-  option "with-sybase-compat", "Enable close compatibility with Sybase's ABI, at the expense of other features"
-  option "with-odbc-wide", "Enable odbc wide, prevent unicode - MemoryError's"
-  option "with-krb5", "Enable Kerberos support"
-  option "without-openssl", "Build without OpenSSL support (default is to use brewed OpenSSL)"
-
   depends_on "pkg-config" => :build
-  depends_on "openssl" => :recommended
-  depends_on "unixodbc" => :optional
+  depends_on "openssl"
+  depends_on "unixodbc"
 
   def install
     args = %W[
       --prefix=#{prefix}
-      --with-tdsver=7.1
       --mandir=#{man}
+      --enable-krb5
+      --with-openssl=#{Formula["openssl"].opt_prefix}
+      --with-tdsver=7.1
+      --with-unixodbc=#{Formula["unixodbc"].opt_prefix}
     ]
-
-    if build.with? "openssl"
-      args << "--with-openssl=#{Formula["openssl"].opt_prefix}"
-    end
-
-    if build.with? "unixodbc"
-      args << "--with-unixodbc=#{Formula["unixodbc"].opt_prefix}"
-    end
-
-    # Translate formula's "--with" options to configuration script's "--enable"
-    # options
-    %w[msdblib sybase-compat odbc-wide krb5].each do |option|
-      args << "--enable-#{option}" if build.with? option
-    end
 
     system "./configure", *args
     system "make"
