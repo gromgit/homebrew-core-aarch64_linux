da1975623922cf2d64ced525ddee7e36511d42ab
FX Coudert <fxcoudert@gmail.com>
2018-10-31 11:28:03 +0100

    groff: remove options

--- Formula/groff.rb
+++ Formula/groff.rb
@@ -13,46 +13,12 @@ class Groff < Formula
     sha256 "55da66201119dd58fa96bbdb94a421d40fc6daba86bbc10ab6abc33763be17df" => :yosemite
   end
 
-  option "with-gropdf", "Enable PDF output support"
-  option "with-grohtml", "Enable HTML output support (implies --with-gropdf)"
-  option "with-gpresent", "Install macros for the presentations document format"
-
-  if build.with? "grohtml"
-    depends_on "ghostscript"
-    depends_on "netpbm"
-    depends_on "psutils"
-  elsif build.with? "gropdf"
-    depends_on "ghostscript"
-  end
-
-  resource "gpresent" do
-    url "https://staff.fnwi.uva.nl/b.diertens/useful/gpresent/gpresent-2.3.tar.gz"
-    sha256 "a76b9b939f8107d53d7a3c9c1f403a3af23868c1e6b2e609ef70b30d12d70c51"
-  end
-
   patch :DATA # fix parallel build, https://savannah.gnu.org/bugs/index.php?43581
 
   def install
     system "./configure", "--prefix=#{prefix}", "--without-x"
     system "make" # Separate steps required
     system "make", "install"
-
-    if build.with? "gpresent"
-      resource("gpresent").stage do
-        (pkgshare/"site-tmac").install Dir["*.tmac"]
-        (pkgshare/"examples").install Dir["*.rof", "*.pdf"]
-        man7.install Dir["*.7"]
-        man1.install Dir["*.1"]
-        bin.install "presentps"
-      end
-    end
-  end
-
-  def caveats
-    <<~EOS
-      Attempting to use PDF or HTML output support without using --with-gropdf or
-      --with-grohtml may result in errors.
-    EOS
   end
 
   test do
