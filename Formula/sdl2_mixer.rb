0b6608ee95ef78b44173aa30ccb005f26ddc7375
commitay <commitay@users.noreply.github.com>
2018-11-01 08:03:18 +1000

    sdl2_mixer 2.0.4
    Closes #33582.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/sdl2_mixer.rb
+++ Formula/sdl2_mixer.rb
@@ -1,9 +1,8 @@
 class Sdl2Mixer < Formula
   desc "Sample multi-channel audio mixer library"
   homepage "https://www.libsdl.org/projects/SDL_mixer/"
-  url "https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.2.tar.gz"
-  sha256 "4e615e27efca4f439df9af6aa2c6de84150d17cbfd12174b54868c12f19c83bb"
-  revision 3
+  url "https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.tar.gz"
+  sha256 "b4cf5a382c061cd75081cf246c2aa2f9df8db04bdda8dcdc6b6cca55bede2419"
   head "https://hg.libsdl.org/SDL_mixer", :using => :hg
 
   bottle do
