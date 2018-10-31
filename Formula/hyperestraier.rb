062b962d3f5c02043884cc91397741cf3d323e0e
Viktor Szakats <commit@vszakats.net>
2018-10-31 14:58:32 +0000

    hyperestraier: secure url(s)

--- Formula/hyperestraier.rb
+++ Formula/hyperestraier.rb
@@ -1,7 +1,7 @@
 class Hyperestraier < Formula
   desc "Full-text search system for communities"
-  homepage "http://fallabs.com/hyperestraier/index.html"
-  url "http://fallabs.com/hyperestraier/hyperestraier-1.4.13.tar.gz"
+  homepage "https://fallabs.com/hyperestraier/"
+  url "https://fallabs.com/hyperestraier/hyperestraier-1.4.13.tar.gz"
   sha256 "496f21190fa0e0d8c29da4fd22cf5a2ce0c4a1d0bd34ef70f9ec66ff5fbf63e2"
 
   bottle do
