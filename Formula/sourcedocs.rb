b77414ba0b16edc6ed48b7dd5fd8012c2f3d155b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-31 17:15:33 +0000

    sourcedocs: add 0.5.1 bottle.

--- Formula/sourcedocs.rb
+++ Formula/sourcedocs.rb
@@ -4,6 +4,12 @@ class Sourcedocs < Formula
   url "https://github.com/eneko/SourceDocs/archive/0.5.1.tar.gz"
   sha256 "3c2e2de695d49dbdd5acb49f8876042bdc97e8d6b95584d3ef6b592b8f10affc"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "256b06c1425620af744d17bac21d993c54f12f1543206ae08b29e09fe28d1fc5" => :mojave
+    sha256 "7a7c4205340af10ab3cb41ea097f3be5fcf134c461c84869216b568aaa86b429" => :high_sierra
+  end
+
   depends_on :xcode => ["9.3", :build, :test]
 
   def install
