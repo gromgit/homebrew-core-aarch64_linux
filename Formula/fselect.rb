9168009f55111217d941a65b6dcbbb823fb07c6e
jhspetersson <jhspetersson@gmail.com>
2018-10-31 16:00:33 +0300

    fselect 0.5.0
    Closes #33559.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/fselect.rb
+++ Formula/fselect.rb
@@ -1,8 +1,8 @@
 class Fselect < Formula
   desc "Find files with SQL-like queries"
   homepage "https://github.com/jhspetersson/fselect"
-  url "https://github.com/jhspetersson/fselect/archive/0.4.5.tar.gz"
-  sha256 "d08e6f34b52f87604b575daba7a48a8803181f5605fa689e4f4ebec0c4e96f2c"
+  url "https://github.com/jhspetersson/fselect/archive/0.5.0.tar.gz"
+  sha256 "0bc9cf437ef4bdb7ff8abb18cae5b8e8489aa3ab66f5312acd9593a30361e6d5"
 
   bottle do
     sha256 "a2ffc67b732c117931798264751a7197c231cdf171c8898f9d2fceeeaabb475f" => :mojave
