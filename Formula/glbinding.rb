b154f28609233c14d744bfe7a6b7e914fa1a6f87
FX Coudert <fxcoudert@gmail.com>
2018-11-01 22:41:02 +0100

    glbinding: remove options

--- Formula/glbinding.rb
+++ Formula/glbinding.rb
@@ -12,19 +12,13 @@ class Glbinding < Formula
     sha256 "206ed78988d76389de106d58d8b8ffb8417fc1e48ed32141f58e3ebc1f41b77e" => :el_capitan
   end
 
-  option "with-glfw", "Enable tools that display OpenGL information for your system"
-  option "with-static", "Build static instead of shared glbinding libraries"
-
   depends_on "cmake" => :build
-  depends_on "glfw" => :optional
+
   needs :cxx11
 
   def install
     ENV.cxx11
-    args = std_cmake_args
-    args << "-DGLFW_LIBRARY_RELEASE=" if build.without? "glfw"
-    args << "-DBUILD_SHARED_LIBS:BOOL=OFF" if build.with? "static"
-    system "cmake", ".", *args
+    system "cmake", ".", *std_cmake_args, "-DGLFW_LIBRARY_RELEASE="
     system "cmake", "--build", ".", "--target", "install"
   end
 
