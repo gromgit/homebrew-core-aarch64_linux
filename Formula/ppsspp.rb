a74bd142ac48c04fd5b219323ce3ff1e0b91b393
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-01 20:50:16 +0000

    ppsspp: update 1.7.1 bottle.

--- Formula/ppsspp.rb
+++ Formula/ppsspp.rb
@@ -8,10 +8,9 @@ class Ppsspp < Formula
 
   bottle do
     cellar :any
-    sha256 "3e003edaf5ba25ab8f2c87d70673534f0d2e79ed1fbeadc7a4a80951630f7619" => :mojave
-    sha256 "1a2fc8743a9fbaa0f77836bdd874eaebeb0117af5ca61e99dd9e44a8312f17dc" => :high_sierra
-    sha256 "e91b28f7e661e07d9656d94da7fa5aa4bfb1b4c639f7ba363e8e0bfa3d3d0e13" => :sierra
-    sha256 "8a21b9cc0971f4d774b915152f487253d1bd7dca062c1f4f8376dd6fac24710f" => :el_capitan
+    sha256 "b17535d5ec300b6a3951255d647c8f3ac435c8e612589f109f2b1ce87bb1f410" => :mojave
+    sha256 "3a22cadfdd18d480dfd03afec60e238b413082da2cff96fef006d6b8fda3a464" => :high_sierra
+    sha256 "a93c6d6d90d039c2de981e6ea6ccfbcd27ab3228f50bd8262df3e8837b202b65" => :sierra
   end
 
   depends_on "cmake" => :build
