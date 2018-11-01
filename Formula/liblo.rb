f51601f31ec984ce57e125c89a654ecd09ec3c52
FX Coudert <fxcoudert@gmail.com>
2018-11-01 22:41:03 +0100

    liblo: remove options

--- Formula/liblo.rb
+++ Formula/liblo.rb
@@ -3,6 +3,7 @@ class Liblo < Formula
   homepage "https://liblo.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/liblo/liblo/0.29/liblo-0.29.tar.gz"
   sha256 "ace1b4e234091425c150261d1ca7070cece48ee3c228a5612d048116d864c06a"
+  revision 1
 
   bottle do
     cellar :any
@@ -21,18 +22,12 @@ class Liblo < Formula
     depends_on "libtool" => :build
   end
 
-  option "with-ipv6", "Compile with support for ipv6"
-
-  deprecated_option "enable-ipv6" => "with-ipv6"
-
   def install
     args = %W[
       --disable-debug
       --disable-dependency-tracking
       --prefix=#{prefix}
     ]
-
-    args << "--enable-ipv6" if build.with? "ipv6"
 
     if build.head?
       system "./autogen.sh", *args
