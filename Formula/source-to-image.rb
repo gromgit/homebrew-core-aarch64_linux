58c57ac490a34d83504608e01bcb86a54ef68d7f
Igor Kapkov <igasgeek@me.com>
2018-11-02 06:56:02 +1100

    source-to-image 1.1.12
    Closes #33605.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/source-to-image.rb
+++ Formula/source-to-image.rb
@@ -2,8 +2,8 @@ class SourceToImage < Formula
   desc "Tool for building source and injecting into docker images"
   homepage "https://github.com/openshift/source-to-image"
   url "https://github.com/openshift/source-to-image.git",
-      :tag => "v1.1.10",
-      :revision => "27f0729d81fe0641486257252189b8dca856ea28"
+      :tag => "v1.1.12",
+      :revision => "2a783420bb648df7bdd7e1fbf99b8e53bbe4e415"
   head "https://github.com/openshift/source-to-image.git"
 
   bottle do
