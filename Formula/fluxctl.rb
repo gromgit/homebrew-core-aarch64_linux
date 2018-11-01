388425f191fb3dc7effd3863d86aec7ee5700f6d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-01 13:59:27 +0000

    fluxctl: add 1.8.0 bottle.

--- Formula/fluxctl.rb
+++ Formula/fluxctl.rb
@@ -5,6 +5,13 @@ class Fluxctl < Formula
       :tag => "1.8.0",
       :revision => "9f2be4bf2d762034a5118a0a7e974bf91089afea"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ef7fa7a2780d0242559f9fa0bf887face52f0bff2fcb9cc91e04260291390272" => :mojave
+    sha256 "8f195ceb9f1150729e1f53ccccd2e99beadbd280e31d045b44b2212189fb00a0" => :high_sierra
+    sha256 "33e068bf2969b3f6bd5812c858f2e0bbe8b62f60854555aae490826d76e2693a" => :sierra
+  end
+
   depends_on "dep" => :build
   depends_on "go" => :build
 
