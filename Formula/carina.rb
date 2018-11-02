9cd638eaf0ff70f348b3918c8ac3e8055377a1c3
Mike McQuaid <mike@mikemcquaid.com>
2018-11-02 17:22:34 +0000

    carina: fix RuboCop Layout/AlignHash.

--- Formula/carina.rb
+++ Formula/carina.rb
@@ -2,7 +2,7 @@ class Carina < Formula
   desc "Command-line client for Carina"
   homepage "https://github.com/getcarina/carina"
   url "https://github.com/getcarina/carina.git",
-        :tag => "v2.1.3",
+        :tag      => "v2.1.3",
         :revision => "2b3ec267e298e095d7c2f81a2d82dc50a720e81c"
   head "https://github.com/getcarina/carina.git"
 
