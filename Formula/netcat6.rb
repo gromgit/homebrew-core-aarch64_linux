172662a209c3e6e02fb2ce3d199d57a5be32f657
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-02 15:35:12 +0000

    netcat6: update 1.0 bottle.

--- Formula/netcat6.rb
+++ Formula/netcat6.rb
@@ -6,13 +6,10 @@ class Netcat6 < Formula
   sha256 "db7462839dd135ff1215911157b666df8512df6f7343a075b2f9a2ef46fe5412"
 
   bottle do
-    sha256 "d0f6d99434ded31aebdc49dfa8e918112e816421247ada8dc23ba89614fc8278" => :mojave
-    sha256 "42c8ecdb7495f55311d806e7dd3695c2dc664c536d5c6c0f7f0a84f150299a44" => :high_sierra
-    sha256 "134b64d2bca03d774a1375c9898504b5aba30de50dff543ca37033b7334fcaa5" => :sierra
-    sha256 "75fd93f36d745618b9c87a1f635a100db7fc2c3d659ecf39e77a282bf0efdb1b" => :el_capitan
-    sha256 "7020abcd43b4b1714a43e42f468895c6c02ad2a8a214bc36761b6b5f615cd127" => :yosemite
-    sha256 "7b1d4a701e8fadedea4e5cc89d7cbcb5bf476476557975a71a681850c50bf872" => :mavericks
-    sha256 "361c72d301addec6d417b52535da84dd924fdcdf9794889dc5ac0f240bb31b02" => :mountain_lion
+    rebuild 1
+    sha256 "aba098730e397f84b6ed7534b41bd7f65f5f6182189d890ac93216faff2fe9b7" => :mojave
+    sha256 "b3fe44c42b33bc668cdaa0f05eb10a5f9b67891b1947b98abe9cad6464182835" => :high_sierra
+    sha256 "bdb853a9a63a03555682eae734d9d9a7725591dfd16128cf59f208968ef16ef2" => :sierra
   end
 
   def install
