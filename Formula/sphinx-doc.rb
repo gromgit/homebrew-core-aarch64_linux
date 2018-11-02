e4c907e18738d91ed9ce568206a201fc1602c6ba
Viktor Szakats <commit@vszakats.net>
2018-11-02 02:45:16 +0000

    sphinx-doc: secure homepage

--- Formula/sphinx-doc.rb
+++ Formula/sphinx-doc.rb
@@ -1,6 +1,6 @@
 class SphinxDoc < Formula
   desc "Tool to create intelligent and beautiful documentation"
-  homepage "http://sphinx-doc.org"
+  homepage "https://www.sphinx-doc.org/"
   url "https://files.pythonhosted.org/packages/c7/e9/b1bed881847680cecc70159b8b9d5fd1cd4e85627c534712c2c7b339f8b6/Sphinx-1.8.1.tar.gz"
   sha256 "652eb8c566f18823a022bb4b6dbc868d366df332a11a0226b5bc3a798a479f17"
 
