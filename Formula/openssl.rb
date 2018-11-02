537c3cde76755019d3ec3445a317651f2f143404
Mike McQuaid <mike@mikemcquaid.com>
2018-11-02 17:23:03 +0000

    openssl: fix RuboCop Layout/AlignHash.

--- Formula/openssl.rb
+++ Formula/openssl.rb
@@ -29,7 +29,7 @@ class Openssl < Formula
   def arch_args
     {
       :x86_64 => %w[darwin64-x86_64-cc enable-ec_nistp_64_gcc_128],
-      :i386 => %w[darwin-i386-cc],
+      :i386   => %w[darwin-i386-cc],
     }
   end
 
