653f3ae37b3fe1a18c1a77420a69c773c636c78b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-02 01:37:48 +0000

    bpm-tools: update 0.3 bottle.

--- Formula/bpm-tools.rb
+++ Formula/bpm-tools.rb
@@ -7,12 +7,10 @@ class BpmTools < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a88947295e0cf8da50a4de7aa5c7dc92c90bf10aa86bb947ad404ee8d213650a" => :mojave
-    sha256 "4c9ca860e2541613e5c87cfc02906a9675e5f26318fb7c8ceb0cd12e3d3e781f" => :high_sierra
-    sha256 "de818373ff7067c15db709ba64f11df8bacaf02935754859ff6c14ba4defcde9" => :sierra
-    sha256 "0348970a3f89990ed97a15e10dff48f07b853d01d32a640a98e6c835d78f09d7" => :el_capitan
-    sha256 "d6bcb8fe9273b0640c2272bd9f4255797eec40a7369362600a62473c3cfd1c27" => :yosemite
-    sha256 "702aa6266adb11c4aada4711f32a45e93cd2bff67e62c9d420ecd748d7d80ead" => :mavericks
+    rebuild 1
+    sha256 "cbfaeff9a82a4363fc1d3b329f490c05fd89b2eb7534e295daf9cbee0bc3d930" => :mojave
+    sha256 "12cab436d4ef0983a5961ebbb3daee3ad6965ef956fb5837708c8f1451b03670" => :high_sierra
+    sha256 "1ae085b87f736d2b87214589e44f248697bd7136789407b487adc1b83674f72e" => :sierra
   end
 
   def install
