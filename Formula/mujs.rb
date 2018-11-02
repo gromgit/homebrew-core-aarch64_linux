ab38b51c866ebb7f9e5ec868f28f5693ff57ca69
Mike McQuaid <mike@mikemcquaid.com>
2018-11-02 17:23:01 +0000

    mujs: fix RuboCop Layout/AlignHash.

--- Formula/mujs.rb
+++ Formula/mujs.rb
@@ -3,7 +3,7 @@ class Mujs < Formula
   homepage "https://www.mujs.com/"
   # use tag not tarball so the version in the pkg-config file isn't blank
   url "https://github.com/ccxvii/mujs.git",
-      :tag => "1.0.5",
+      :tag      => "1.0.5",
       :revision => "7448a82448aa4eff952a4fdb836f197b844e3d1d"
   head "https://github.com/ccxvii/mujs.git"
 
