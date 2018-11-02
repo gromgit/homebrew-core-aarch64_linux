23883d897f6a20b1dd4a012fdf59e3de6abc091d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-02 15:35:13 +0000

    ecasound: update 2.9.1 bottle.

--- Formula/ecasound.rb
+++ Formula/ecasound.rb
@@ -5,12 +5,10 @@ class Ecasound < Formula
   sha256 "39fce8becd84d80620fa3de31fb5223b2b7d4648d36c9c337d3739c2fad0dcf3"
 
   bottle do
-    sha256 "f948edb013b464b984a44dee9a0bd6e8fc9d6e5f1deba13c8d57e668d0b1b92f" => :mojave
-    sha256 "cc433f924ea12f9450d064be622a290cab97bdad35e83ed98658335ae61f47c0" => :high_sierra
-    sha256 "789fd275a49c7017ee25d1f5e00a802b3c5f2baa5d54db3753c566e04cd335c2" => :sierra
-    sha256 "a0bfadb79c1b81c2764290a4cc6e2eae09bae34e4ec54f06e6d4d669bceed331" => :el_capitan
-    sha256 "087b99f6242cfb60eaf73b8f79643d3a78ea53dffccddeb2f89757c3835380bd" => :yosemite
-    sha256 "e1230a9513d1011c60a51569fa8cee5671dc79867c75c8b548005b5949984a7f" => :mavericks
+    rebuild 1
+    sha256 "6166bd8bfe46c9cd397ebbdd309228ebdaf4ba85be03b3b5190ee85a78a149c9" => :mojave
+    sha256 "b0d1ebfd5db18a8141c0a9ddc9a9a62f155d4db16d6f68e7162cd9cf27a0a02d" => :high_sierra
+    sha256 "fe062d9a9f6c5072c2a6dd8778c1fd842694e43fb91dede0f877628a2d8eb27f" => :sierra
   end
 
   def install
