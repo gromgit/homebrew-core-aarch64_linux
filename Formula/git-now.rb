f3b8c6a74fed41299ce141d07c175d6e03568db2
Mike McQuaid <mike@mikemcquaid.com>
2018-11-02 17:22:45 +0000

    git-now: fix RuboCop Layout/AlignHash.

--- Formula/git-now.rb
+++ Formula/git-now.rb
@@ -2,7 +2,7 @@ class GitNow < Formula
   desc "Light, temporary commits for git"
   homepage "https://github.com/iwata/git-now"
   url "https://github.com/iwata/git-now.git",
-      :tag => "v0.1.1.0",
+      :tag      => "v0.1.1.0",
       :revision => "a07a05893b9ddf784833b3d4b410c843633d0f71"
   head "https://github.com/iwata/git-now.git"
 
