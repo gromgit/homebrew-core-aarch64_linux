a6146b4e9773b82f66bd9423bf2b5ca107d51074
Mike McQuaid <mike@mikemcquaid.com>
2018-11-02 17:23:02 +0000

    neatvi: fix RuboCop Layout/AlignHash.

--- Formula/neatvi.rb
+++ Formula/neatvi.rb
@@ -2,7 +2,7 @@ class Neatvi < Formula
   desc "ex/vi clone for editing bidirectional utf-8 text"
   homepage "https://repo.or.cz/neatvi.git"
   url "https://repo.or.cz/neatvi.git",
-      :tag => "06",
+      :tag      => "06",
       :revision => "5ed4bbc7f12686bb480ab8b2b05c94e12b1c71d8"
   head "https://repo.or.cz/neatvi.git"
 
