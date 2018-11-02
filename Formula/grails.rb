fc48e8442b15068bfa2ec32fae782b35a94774f9
Igor Kapkov <igasgeek@me.com>
2018-11-02 15:20:33 +1100

    grails 3.3.8
    Closes #33629.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/grails.rb
+++ Formula/grails.rb
@@ -1,8 +1,8 @@
 class Grails < Formula
   desc "Web application framework for the Groovy language"
   homepage "https://grails.org"
-  url "https://github.com/grails/grails-core/releases/download/v3.3.7/grails-3.3.7.zip"
-  sha256 "1d4bd4127fbfdc9c07465b85deecfec5c746fb97468df891823e5063909d764f"
+  url "https://github.com/grails/grails-core/releases/download/v3.3.8/grails-3.3.8.zip"
+  sha256 "c0de7ce97ea156a7fd43e190479814d61ecd91a0399e88fd353526b1a3a2d8c1"
 
   bottle :unneeded
 
