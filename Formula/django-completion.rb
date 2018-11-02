9d3fdb8fdbfac3137f2d4ee027d65c01e4cfef7e
Igor Kapkov <igasgeek@me.com>
2018-11-02 19:23:18 +1100

    django-completion 2.1.3
    Closes #33631.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/django-completion.rb
+++ Formula/django-completion.rb
@@ -1,8 +1,8 @@
 class DjangoCompletion < Formula
   desc "Bash completion for Django"
   homepage "https://github.com/django/django"
-  url "https://github.com/django/django/archive/2.1.1.tar.gz"
-  sha256 "55be7d98121fbff07e503a762748943c1de637608d8db6ed81bc9e97e6924df5"
+  url "https://github.com/django/django/archive/2.1.3.tar.gz"
+  sha256 "c6f47e87426c324e206b7083bcdf99e0b91a992ba14cb91ded8f3a57cff8f4f1"
   head "https://github.com/django/django.git"
 
   bottle :unneeded
