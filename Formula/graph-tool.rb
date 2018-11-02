a28c2a4ad21ee8df6f13bc61c3849f3359989cc6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-02 15:40:04 +0000

    graph-tool: update 2.27_1 bottle.

--- Formula/graph-tool.rb
+++ Formula/graph-tool.rb
@@ -8,10 +8,9 @@ class GraphTool < Formula
   revision 1
 
   bottle do
-    rebuild 1
-    sha256 "cf89561d96c920f6ec6f0185557d2f0b95ec530c956519f3a8a53f7c96d08c6e" => :high_sierra
-    sha256 "7423ab8435383694efd8b12e3260b9b39d60e1d28ac8dd475ae361ec618ccef3" => :sierra
-    sha256 "2581ddd563891c629a26ea658b4168d4687f736a6b0bc46638b51af3e8a036f1" => :el_capitan
+    rebuild 2
+    sha256 "3e597fc5be9337361df2450506f4c7791012b909b691f5255ca59ca5509a66bf" => :mojave
+    sha256 "b144039513cef5c49f1dede1ba575e2df83238dab9f5c0a4ce6be678d56c8f79" => :sierra
   end
 
   depends_on "pkg-config" => :build
