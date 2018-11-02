34649f9c06dcd7816f6dba62e509c7f12ed56ec9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-02 01:37:47 +0000

    liblo: update 0.29_1 bottle.

--- Formula/liblo.rb
+++ Formula/liblo.rb
@@ -7,11 +7,9 @@ class Liblo < Formula
 
   bottle do
     cellar :any
-    sha256 "47694db74756045244bdd32db28174e143f3cc0812809d340fafa862c7f8b540" => :mojave
-    sha256 "961d1559e59bdcf92134a61c799456db776434d4309c2b1dc0ba17581fca7084" => :high_sierra
-    sha256 "a618d8405b4e13bc1871a5a24da2489a2b64f3d08bff3b7920bf8a6b2b3d6302" => :sierra
-    sha256 "4d5fb18d769550dd5415441b78e167533813c36391819f3c67ea7ea1e69ce7a2" => :el_capitan
-    sha256 "2e2a5f7bc6e95657cec57fc1b969c32dac1405a9731dffec1983c69cb941dde2" => :yosemite
+    sha256 "bec4aa8cf37050f2b35123d1ca894801e5ae75d95034e7ee2d3b365922180332" => :mojave
+    sha256 "e69e5f3405e2c55b25864a62dcdd3d454c9e460fe54d87447a1821973f9a6e67" => :high_sierra
+    sha256 "c39875452da500b334cb532d903c284b178cb3f9dd15e0201228dfbe78511985" => :sierra
   end
 
   head do
