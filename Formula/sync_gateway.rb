aef24fe739f48f07d9771ea0190978507ef37b2e
Mike McQuaid <mike@mikemcquaid.com>
2018-11-02 17:23:14 +0000

    sync_gateway: fix RuboCop Layout/AlignHash.

--- Formula/sync_gateway.rb
+++ Formula/sync_gateway.rb
@@ -2,7 +2,7 @@ class SyncGateway < Formula
   desc "Make Couchbase Server a replication endpoint for Couchbase Lite"
   homepage "https://docs.couchbase.com/sync-gateway"
   url "https://github.com/couchbase/sync_gateway.git",
-      :tag => "1.3.1",
+      :tag      => "1.3.1",
       :revision => "660b1c92fadce1a9c7e692dfe7c5b741772d1dd2"
   head "https://github.com/couchbase/sync_gateway.git"
 
