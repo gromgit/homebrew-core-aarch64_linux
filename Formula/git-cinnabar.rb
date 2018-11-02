8cc5e13cade7bde5eb405f867de2e0a764796e1e
Zhiming Wang <i@zhimingwang.org>
2018-11-02 10:00:08 -0400

    git-cinnabar 0.5.0
    Closes #33644.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/git-cinnabar.rb
+++ Formula/git-cinnabar.rb
@@ -1,27 +1,9 @@
 class GitCinnabar < Formula
   desc "Git remote helper to interact with mercurial repositories"
   homepage "https://github.com/glandium/git-cinnabar"
-  revision 1
+  url "https://github.com/glandium/git-cinnabar/archive/0.5.0.tar.gz"
+  sha256 "1e09c7c24a34eb172681283ca243677ea06f4da179916a907f43f9027ca59dea"
   head "https://github.com/glandium/git-cinnabar.git"
-
-  stable do
-    url "https://github.com/glandium/git-cinnabar.git",
-        :tag => "0.4.0",
-        :revision => "6d374888ff0287517084c0ec7573963961f6acec"
-
-    # 5 Nov 2017 "Support the batch API change from mercurial 4.4"
-    patch do
-      url "https://github.com/glandium/git-cinnabar/commit/7ea77b0.patch?full_index=1"
-      sha256 "e28fdf1b9afa94dbd17289e739cd68af34bf7ae708b827cfa9e23286dbbbb57c"
-    end
-
-    # 5 Nov 2017 "Adapt localpeer to sshpeer changes in mercurial 4.4"
-    # Backport of https://github.com/glandium/git-cinnabar/commit/5c59ae1
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/e56093e/git-cinnabar/mercurial-4.4-sshpeer.patch"
-      sha256 "9af333567ff4aec002c947906d9e5a62ce7358c4ffa1edf7be0b5fe0a96b87ae"
-    end
-  end
 
   bottle do
     cellar :any_skip_relocation
