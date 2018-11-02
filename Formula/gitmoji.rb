8b4efc6895a53a9cd0e491bf43156de18f58cc2c
Benjamin Brabant <bbrabant@octo.com>
2018-11-02 23:37:52 +0100

    gitmoji 1.9.2 (new formula)
    Closes #33660.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/gitmoji.rb
@@ -0,0 +1,18 @@
+require "language/node"
+
+class Gitmoji < Formula
+  desc "Emoji guide for your commit messages"
+  homepage "https://gitmoji.carloscuesta.me"
+  url "https://registry.npmjs.org/gitmoji-cli/-/gitmoji-cli-1.9.2.tgz"
+  sha256 "af7a6c7b0874d2971be909c9c2166b94db4fe1c3d1500f49cfac30d0a5d1b433"
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match ":bug:", shell_output("#{bin}/gitmoji --search bug")
+  end
+end
