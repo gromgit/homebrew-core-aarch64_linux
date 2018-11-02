d1ba31fc72facd99904645563ee980f14b70fe54
Mike McQuaid <mike@mikemcquaid.com>
2018-11-02 17:22:35 +0000

    cling: fix RuboCop Layout/AlignHash.

--- Formula/cling.rb
+++ Formula/cling.rb
@@ -2,7 +2,7 @@ class Cling < Formula
   desc "The cling C++ interpreter"
   homepage "https://root.cern.ch/cling"
   url "https://github.com/root-project/cling.git",
-      :tag => "v0.5",
+      :tag      => "v0.5",
       :revision => "0f1d6d24d4417fc02b73589c8b1d813e92de1c3f"
   revision 1
 
@@ -17,13 +17,13 @@ class Cling < Formula
 
   resource "clang" do
     url "http://root.cern.ch/git/clang.git",
-        :tag => "cling-patches-r302975",
+        :tag      => "cling-patches-r302975",
         :revision => "1f8b137c7eb06ed8e321649ef7e3f3e7a96f361c"
   end
 
   resource "llvm" do
     url "http://root.cern.ch/git/llvm.git",
-        :tag => "cling-patches-r302975",
+        :tag      => "cling-patches-r302975",
         :revision => "2a34248cb945d63ded5ee55128e68efd7e5b87c8"
   end
 
