4c9e17e001be1595b72eba56528cdd79150af631
Mike McQuaid <mike@mikemcquaid.com>
2018-11-02 17:22:53 +0000

    l-smash: fix RuboCop Layout/AlignHash.

--- Formula/l-smash.rb
+++ Formula/l-smash.rb
@@ -2,8 +2,8 @@ class LSmash < Formula
   desc "Tool for working with MP4 files"
   homepage "https://l-smash.github.io/l-smash/"
   url "https://github.com/l-smash/l-smash.git",
-      :shallow => false,
-      :tag => "v2.9.1",
+      :shallow  => false,
+      :tag      => "v2.9.1",
       :revision => "4cea08d264933634db5bc06da9d8d88fb5ddae07"
   head "https://github.com/l-smash/l-smash.git"
 
