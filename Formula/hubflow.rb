f55a0ebd05fef00891058bea07389dab9e797023
Mike McQuaid <mike@mikemcquaid.com>
2018-11-02 17:22:47 +0000

    hubflow: fix RuboCop Layout/AlignHash.

--- Formula/hubflow.rb
+++ Formula/hubflow.rb
@@ -3,7 +3,7 @@ class Hubflow < Formula
   desc "GitFlow for GitHub"
   homepage "https://datasift.github.io/gitflow/"
   url "https://github.com/datasift/gitflow.git",
-      :tag => "1.5.3",
+      :tag      => "1.5.3",
       :revision => "ed032438d2100b826d2fd5c8281b5e07d88ab9eb"
   head "https://github.com/datasift/gitflow.git"
 
