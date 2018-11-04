819a79323897cb76eb803d27fe95fa5e0b19593e
Viktor Szakats <commit@vszakats.net>
2018-11-04 15:56:35 +0000

    openrtsp 2018.10.17
    Closes #33713.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/openrtsp.rb
+++ Formula/openrtsp.rb
@@ -1,10 +1,10 @@
 class Openrtsp < Formula
   desc "Command-line RTSP client"
   homepage "http://www.live555.com/openRTSP"
-  url "http://www.live555.com/liveMedia/public/live.2018.08.28a.tar.gz"
+  url "http://www.live555.com/liveMedia/public/live.2018.10.17.tar.gz"
   # Keep a mirror as upstream tarballs are removed after each version
-  mirror "https://download.videolan.org/pub/videolan/testing/contrib/live555/live.2018.08.28a.tar.gz"
-  sha256 "0aa6f60c8acf8a309119c02ad1bafca40af8105ce411ba2e6fdfbed9222f91ba"
+  mirror "https://download.videolan.org/pub/videolan/testing/contrib/live555/live.2018.10.17.tar.gz"
+  sha256 "7c68d9c95b39acd309a2b6a4fc14c3837544a9be3f64062ed38d1ad6f68dc9e8"
 
   bottle do
     cellar :any_skip_relocation
