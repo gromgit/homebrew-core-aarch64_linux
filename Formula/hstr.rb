9638a43c79e84e21bea94f02541e4abbf0cf4745
Igor Kapkov <igasgeek@me.com>
2018-11-05 06:56:53 +1100

    hh 2.0
    Closes #33719.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/hstr.rb
@@ -0,0 +1,25 @@
+class Hstr < Formula
+  desc "Bash and zsh history suggest box"
+  homepage "https://github.com/dvorka/hstr"
+  url "https://github.com/dvorka/hstr/archive/2.0.tar.gz"
+  sha256 "8d93ed8bfee1a979e8d06646e162b70316e2097e16243636d81011ba1000627a"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "readline"
+
+  def install
+    system "autoreconf", "-fvi"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    ENV["HISTFILE"] = testpath/".hh_test"
+    (testpath/".hh_test").write("test\n")
+    assert_equal "test", shell_output("#{bin}/hh -n").chomp
+  end
+end
