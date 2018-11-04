a725c571859f1a7a90ded744b96cd862462555fe
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-11-04 11:03:55 +0000

    libgit2-glib 0.27.7
    Closes #33707.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libgit2-glib.rb
+++ Formula/libgit2-glib.rb
@@ -1,9 +1,8 @@
 class Libgit2Glib < Formula
   desc "Glib wrapper library around libgit2 git access library"
   homepage "https://github.com/GNOME/libgit2-glib"
-  url "https://download.gnome.org/sources/libgit2-glib/0.26/libgit2-glib-0.26.4.tar.xz"
-  sha256 "97610e42427a0c86ac46b89d5020fb8decb39af47b9dc49f8d078310b4c21e5a"
-  revision 2
+  url "https://download.gnome.org/sources/libgit2-glib/0.27/libgit2-glib-0.27.7.tar.xz"
+  sha256 "1131df6d45e405756ef2d9b7613354d542ce99883f6a89582d6236d01bd2efc2"
   head "https://github.com/GNOME/libgit2-glib.git"
 
   bottle do
@@ -30,13 +29,6 @@ class Libgit2Glib < Formula
     inreplace "libgit2-glib/meson.build",
               "libgit2_glib_link_args = [ '-Wl,-Bsymbolic-functions' ]",
               "libgit2_glib_link_args = []"
-
-    # Upstream issue from 2 Apr 2018 "libgit2 0.27.0 FTB: ggit-config.c:298:41:
-    # error: too few arguments to function call"
-    # See https://bugzilla.gnome.org/show_bug.cgi?id=794890
-    inreplace "libgit2-glib/ggit-config.c",
-              /(\(git_config_level_t\)level,)(\n\s+)(force\);)/,
-              "\\1\\2NULL,\\2\\3"
 
     mkdir "build" do
       system "meson", "--prefix=#{prefix}",
