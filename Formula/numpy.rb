c78b1c0ac06ddb1a44595c14faccb496ba4991ce
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-05 05:10:29 +0000

    numpy: update 1.15.4 bottle.

--- Formula/numpy.rb
+++ Formula/numpy.rb
@@ -5,9 +5,9 @@ class Numpy < Formula
   sha256 "3d734559db35aa3697dadcea492a423118c5c55d176da2f3be9c98d4803fc2a7"
 
   bottle do
-    sha256 "f09037da31fe744c79455c27993b0496e638e9e817bd1a1d6b1bd7f750b7dfcc" => :mojave
-    sha256 "c8b98a056e29916cfe172acb5426734ed136486e31476b5e932353341dbf8de8" => :high_sierra
-    sha256 "6c49d36ada8d285b711a108b0d5a7547abdc0374448437b40f508f502430d6d0" => :sierra
+    sha256 "5b718dd8e7b82c90212e5bb7f8c19abde83aba114a3ccce7c12af857a4841978" => :mojave
+    sha256 "84ae5941c9d952657ea49708f2e705fac035c410dc991790edd6779432e357f2" => :high_sierra
+    sha256 "d4267f45ef17ecc2827960f4c063bf0cd2b99f2eeb99e2ec24d4f335595ea268" => :sierra
   end
 
   head do
