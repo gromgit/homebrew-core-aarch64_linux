115a846e06b26420c457e35f69535fa4c4f33f7f
Igor Kapkov <igasgeek@me.com>
2018-11-05 14:02:10 +1100

    sec 2.8.1
    Closes #33736.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/sec.rb
+++ Formula/sec.rb
@@ -1,8 +1,8 @@
 class Sec < Formula
   desc "Event correlation tool for event processing of various kinds"
   homepage "https://simple-evcorr.sourceforge.io/"
-  url "https://github.com/simple-evcorr/sec/releases/download/2.7.12/sec-2.7.12.tar.gz"
-  sha256 "4ab19f0e9499a071c1e07dddd453faad2cde5e7de2ff187f0eafb2d2a615aa38"
+  url "https://github.com/simple-evcorr/sec/releases/download/2.8.1/sec-2.8.1.tar.gz"
+  sha256 "342464b2494f4c64eaac3c0f4c53486ea5464f24d8a190c27615850b9d4fe100"
 
   bottle :unneeded
 
