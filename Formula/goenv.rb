d32acbb8d5bbea532c434591f1636d411a376814
BePsvPT <BePsvPT@users.noreply.github.com>
2018-11-05 08:17:37 +0800

    goenv 1.23.0 (#33724)

--- Formula/goenv.rb
+++ Formula/goenv.rb
@@ -1,8 +1,8 @@
 class Goenv < Formula
   desc "Go version management"
   homepage "https://github.com/syndbg/goenv"
-  url "https://github.com/syndbg/goenv/archive/1.22.0.tar.gz"
-  sha256 "80319af8ea83a38a35bd975ff4cabfcf66bf44b97484b20b516506b2fa69d869"
+  url "https://github.com/syndbg/goenv/archive/1.23.0.tar.gz"
+  sha256 "be3108d5cfebf480324c5864b5d7b25130c671e3b7caad9f86beb231494d70b6"
   version_scheme 1
   head "https://github.com/syndbg/goenv.git"
 
