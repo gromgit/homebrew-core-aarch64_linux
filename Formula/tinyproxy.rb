24fef80be4b57e78c27583b7cade8568f3fd5abb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-05 23:16:28 +0000

    tinyproxy: update 1.10.0 bottle.

--- Formula/tinyproxy.rb
+++ Formula/tinyproxy.rb
@@ -5,10 +5,9 @@ class Tinyproxy < Formula
   sha256 "59be87689c415ba0d9c9bc6babbdd3df3b372d60b21e526b118d722dbc995682"
 
   bottle do
-    rebuild 2
-    sha256 "8441582a64edc6c8a8c048b5a430631bba42adba9b59af52d5e27c3ade369f83" => :mojave
-    sha256 "aed9559849395b90fc0fea4b1ff045418ffd108d83bb94474c1e1d31e738b9b4" => :high_sierra
-    sha256 "c2ee54093bf8f4c6fb9a1dddaf878332f3827988df18e4a203fa45a88139a8a7" => :sierra
+    sha256 "019c8a6afd4789cebd3682409699acb906b94948420364435d25efaccc4c0257" => :mojave
+    sha256 "c78480a24306ad91dda1c64b9816ab1e942004911e2a95d325b626225ed7f6db" => :high_sierra
+    sha256 "b2dcc354e2b7fad3a48befe1e07c230355b30ab522ff19f7e1f5b19342ef0b47" => :sierra
   end
 
   depends_on "asciidoc" => :build
