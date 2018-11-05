09a0dc1e5c806706ab0d735be7284eea666960b5
Igor Kapkov <igasgeek@me.com>
2018-11-05 18:03:56 +1100

    tundra 2.08
    Closes #33755.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/tundra.rb
+++ Formula/tundra.rb
@@ -1,8 +1,8 @@
 class Tundra < Formula
   desc "Code build system that tries to be fast for incremental builds"
   homepage "https://github.com/deplinenoise/tundra"
-  url "https://github.com/deplinenoise/tundra/archive/v2.07.tar.gz"
-  sha256 "dc3cbaf977bdbd0b70e70c455656a6ed69ecd110a5ca9eb938789c044774ddbd"
+  url "https://github.com/deplinenoise/tundra/archive/v2.08.tar.gz"
+  sha256 "b27aa8c13a606559ab757f0409ed8260d3349424923926d3add50f58213d6ca6"
 
   bottle do
     cellar :any_skip_relocation
