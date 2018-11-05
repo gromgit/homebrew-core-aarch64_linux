6c523b2b76d94f2a2866727fe012a09756fd5655
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-05 02:34:48 +0000

    shairport-sync: update 3.2.2 bottle.

--- Formula/shairport-sync.rb
+++ Formula/shairport-sync.rb
@@ -6,10 +6,9 @@ class ShairportSync < Formula
   head "https://github.com/mikebrady/shairport-sync.git", :branch => "development"
 
   bottle do
-    sha256 "cbfaed8600347d959dbaed883fca89c6086aa5808c647014e5f63fcbec2fc43d" => :mojave
-    sha256 "facc9f1d5d61ddf4779b1c531b392057d5143691af1ed5dc169dbe93fd6be6d6" => :high_sierra
-    sha256 "bf4b79f69209e61d32b9427e072bca48f4089b7de849430b6576d936ba42bf1f" => :sierra
-    sha256 "0bd51322cd1cbd3f2d875061b8382d223817e1bb5de89e8d10c4ff2c0ce2b870" => :el_capitan
+    sha256 "7dc8276b51f1ef0ef4cf38c27228d46e80678425626783efc0e71646b8a6c60f" => :mojave
+    sha256 "8ed5e98d394f2dc6136abbbc15ecdc27a471244ef2c67c69558fbf9c3fae77ed" => :high_sierra
+    sha256 "72a828bd11b9c6a28d0c4b3c90e92ded0c4ee4ba5aa095f06a22362e5c78212d" => :sierra
   end
 
   depends_on "autoconf" => :build
