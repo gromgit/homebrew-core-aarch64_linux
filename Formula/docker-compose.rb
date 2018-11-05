45c35bf84d8ac8176f35e4a357302eced835b181
Chongyu Zhu <i@lembacon.com>
2018-11-05 21:48:16 +0800

    docker-compose 1.23.1
    Closes #33766.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/docker-compose.rb
+++ Formula/docker-compose.rb
@@ -3,8 +3,8 @@ class DockerCompose < Formula
 
   desc "Isolated development environments using Docker"
   homepage "https://docs.docker.com/compose/"
-  url "https://github.com/docker/compose/archive/1.23.0.tar.gz"
-  sha256 "7feb501d4e33d3c5771a68e49f6823901a414ad43514c60afa9eae43fe93e12f"
+  url "https://github.com/docker/compose/archive/1.23.1.tar.gz"
+  sha256 "843a458d366ca1d41ceec08b3387ba55c6953eba3a171d6af90db0b01f494312"
   head "https://github.com/docker/compose.git"
 
   bottle do
