967d9e44108798a4637de2cfdb767125d3e09e2d
Igor Kapkov <igasgeek@me.com>
2018-11-05 18:07:33 +1100

    muparser 2.2.6.1
    Closes #33757.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/muparser.rb
+++ Formula/muparser.rb
@@ -1,8 +1,8 @@
 class Muparser < Formula
   desc "C++ math expression parser library"
   homepage "http://beltoforion.de/article.php?a=muparser"
-  url "https://github.com/beltoforion/muparser/archive/v2.2.5.tar.gz"
-  sha256 "0666ef55da72c3e356ca85b6a0084d56b05dd740c3c21d26d372085aa2c6e708"
+  url "https://github.com/beltoforion/muparser/archive/v2.2.6.1.tar.gz"
+  sha256 "d2562853d972b6ddb07af47ce8a1cdeeb8bb3fa9e8da308746de391db67897b3"
   head "https://github.com/beltoforion/muparser.git"
 
   bottle do
