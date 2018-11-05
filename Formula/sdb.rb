a2d66c34cd4217b7e17ed0147927e5cce7d9614e
Igor Kapkov <igasgeek@me.com>
2018-11-05 18:05:16 +1100

    sdb 1.3.0
    Closes #33756.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/sdb.rb
+++ Formula/sdb.rb
@@ -1,8 +1,8 @@
 class Sdb < Formula
   desc "Ondisk/memory hashtable based on CDB"
   homepage "https://github.com/radare/sdb"
-  url "https://github.com/radare/sdb/archive/1.2.0.tar.gz"
-  sha256 "7512f0e5a205937a11a5dbe0905eacb3b695c7fb71bb4b6d25b14706fa31a63f"
+  url "https://github.com/radare/sdb/archive/1.3.0.tar.gz"
+  sha256 "e44ee7266a9f35cf81297739f9a41de89394166cc908494a4d5b7d81d6aedbc6"
   head "https://github.com/radare/sdb.git"
 
   bottle do
