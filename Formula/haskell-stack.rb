31463d0277a42f9bc52b194c3384601c0fab12c4
Steve Purcell <steve@sanityinc.com>
2018-11-07 08:53:31 +1300

    haskell-stack 1.9.1
    Closes #33855.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/haskell-stack.rb
+++ Formula/haskell-stack.rb
@@ -5,9 +5,9 @@ class HaskellStack < Formula
 
   desc "The Haskell Tool Stack"
   homepage "https://haskellstack.org/"
-  url "https://github.com/commercialhaskell/stack/releases/download/v1.7.1/stack-1.7.1-sdist-1.tar.gz"
-  version "1.7.1"
-  sha256 "a548fb549b2b1e539e257732596508e4c3d43e3a9d62bd22ecc19dda67c30ce6"
+  url "https://github.com/commercialhaskell/stack/releases/download/v1.9.1/stack-1.9.1-sdist-1.tar.gz"
+  version "1.9.1"
+  sha256 "2628d0a02cb9d48a41f7b257d2619c8ba2e333cd91df03d0729029da53cf6855"
   head "https://github.com/commercialhaskell/stack.git"
 
   bottle do
@@ -18,40 +18,35 @@ class HaskellStack < Formula
     sha256 "d303035dc4e8fcdc8d1aecd5c0c56a8db0421cd21cc5d168442289b9d2e10168" => :el_capitan
   end
 
-  option "without-bootstrap", "Don't bootstrap a stage 2 stack"
-
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
 
-  # Remove when stack.yaml uses GHC 8.4.x
-  resource "stack_nightly_yaml" do
-    url "https://raw.githubusercontent.com/commercialhaskell/stack/v1.7.1/stack-nightly.yaml"
-    version "1.7.1"
-    sha256 "08cf289b53983b72d88f48004b58a3154728125f7cff174c0364f1dd936c607e"
+  # Build using a stack config that matches the default Homebrew version of GHC
+  resource "stack_lts_12_yaml" do
+    url "https://raw.githubusercontent.com/commercialhaskell/stack/v1.9.1/stack-lts-12.yaml"
+    version "1.9.1"
+    sha256 "331381740e857aac1986b0954ccf09b31e8fec8afd1ab48e394a221c09f2755d"
   end
 
   def install
-    buildpath.install resource("stack_nightly_yaml")
+    buildpath.install resource("stack_lts_12_yaml")
 
     cabal_sandbox do
       cabal_install "happy"
 
-      if build.with? "bootstrap"
-        cabal_install
+      # The flag works around https://github.com/commercialhaskell/stack/issues/4363
+      cabal_install "--flags=disable-git-info"
 
-        # Let `stack` handle its own parallelization
-        # Prevents "install: mkdir ... ghc-7.10.3/lib: File exists"
-        jobs = ENV.make_jobs
-        ENV.deparallelize
+      # Let `stack` handle its own parallelization
+      # Prevents "install: mkdir ... ghc-7.10.3/lib: File exists"
+      jobs = ENV.make_jobs
+      ENV.deparallelize
 
-        system "stack", "-j#{jobs}", "--stack-yaml=stack-nightly.yaml",
-               "--system-ghc", "--no-install-ghc", "setup"
-        system "stack", "-j#{jobs}", "--stack-yaml=stack-nightly.yaml",
-               "--system-ghc", "--no-install-ghc", "--local-bin-path=#{bin}",
-               "install"
-      else
-        install_cabal_package
-      end
+      system "stack", "-j#{jobs}", "--stack-yaml=stack-lts-12.yaml",
+             "--system-ghc", "--no-install-ghc", "setup"
+      system "stack", "-j#{jobs}", "--stack-yaml=stack-lts-12.yaml",
+             "--system-ghc", "--no-install-ghc", "--local-bin-path=#{bin}",
+             "install", "--flag", "stack:disable-git-info"
     end
   end
 
