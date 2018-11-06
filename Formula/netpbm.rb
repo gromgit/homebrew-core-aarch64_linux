ff219a4ca5a1cd873f5c6982b084a71c203b469a
Chongyu Zhu <i@lembacon.com>
2018-11-07 01:41:45 +0800

    netpbm 10.73.22
    Closes #33846.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/netpbm.rb
+++ Formula/netpbm.rb
@@ -3,8 +3,8 @@ class Netpbm < Formula
   homepage "https://netpbm.sourceforge.io/"
   # Maintainers: Look at https://sourceforge.net/p/netpbm/code/HEAD/tree/
   # for stable versions and matching revisions.
-  url "http://svn.code.sf.net/p/netpbm/code/stable", :revision => 3269
-  version "10.73.20"
+  url "http://svn.code.sf.net/p/netpbm/code/stable", :revision => 3413
+  version "10.73.22"
   version_scheme 1
   head "http://svn.code.sf.net/p/netpbm/code/trunk"
 
