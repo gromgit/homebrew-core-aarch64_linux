313d29842da4118c08c019c4034254e3eb79d394
Igor Kapkov <igasgeek@me.com>
2018-11-06 12:05:04 +1100

    py3cairo 1.18.0
    Closes #33800.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/py3cairo.rb
+++ Formula/py3cairo.rb
@@ -1,8 +1,8 @@
 class Py3cairo < Formula
   desc "Python 3 bindings for the Cairo graphics library"
   homepage "https://cairographics.org/pycairo/"
-  url "https://github.com/pygobject/pycairo/releases/download/v1.17.1/pycairo-1.17.1.tar.gz"
-  sha256 "0f0a35ec923d87bc495f6753b1e540fd046d95db56a35250c44089fbce03b698"
+  url "https://github.com/pygobject/pycairo/releases/download/v1.18.0/pycairo-1.18.0.tar.gz"
+  sha256 "abd42a4c9c2069febb4c38fe74bfc4b4a9d3a89fea3bc2e4ba7baff7a20f783f"
 
   bottle do
     cellar :any
