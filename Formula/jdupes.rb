4bb18b95bf394afa83cf323f6a10c18228337046
Igor Kapkov <igasgeek@me.com>
2018-11-06 17:28:26 +1100

    jdupes 1.11
    Closes #33819.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/jdupes.rb
+++ Formula/jdupes.rb
@@ -1,8 +1,8 @@
 class Jdupes < Formula
   desc "Duplicate file finder and an enhanced fork of 'fdupes'"
   homepage "https://github.com/jbruchon/jdupes"
-  url "https://github.com/jbruchon/jdupes/archive/v1.10.2.tar.gz"
-  sha256 "6c79035e2d349d0b1d749881b06a24ca43afc5b8f7e714c99b90a34b4618ed4f"
+  url "https://github.com/jbruchon/jdupes/archive/v1.11.tar.gz"
+  sha256 "6cf46a8befef414b99933d3c0f9f58f3b3c7339dc11a744de7380c540d0f1ed4"
 
   bottle do
     cellar :any_skip_relocation
@@ -10,6 +10,13 @@ class Jdupes < Formula
     sha256 "22083be28c6a7d610b2c04f766710f2f07367a3f1a571eeac1b287267ffba44c" => :high_sierra
     sha256 "0467bdff1e91566ec56ada84240068ca882500a3aecd33fd98b5901df9496ae9" => :sierra
     sha256 "080393ee6408d004a629c2427455eae484bb7f2edeaf993e2528a16e1c0055c0" => :el_capitan
+  end
+
+  # Fix for build issue, remove in next version
+  # https://github.com/jbruchon/jdupes/issues/89
+  patch do
+    url "https://github.com/jbruchon/jdupes/commit/1a88f0ed.diff?full_index=1"
+    sha256 "f086733421f08f93a96f88d7f6bc688761a498e8382250c30b02b0df869fa4f9"
   end
 
   def install
