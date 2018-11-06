bba2d717f7c3ddf02b9dbaae4f558653225ff2f1
Frank Shotwell <frank@replicated.com>
2018-11-05 22:37:01 -0800

    ship 0.29.0 (new formula)
    Closes #33820.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/ship.rb
@@ -0,0 +1,28 @@
+class Ship < Formula
+  desc "Reducing the overhead of maintaining 3rd-party applications in Kubernetes"
+  homepage "https://www.replicated.com/ship"
+  url "https://github.com/replicatedhq/ship/archive/v0.29.0.tar.gz"
+  sha256 "5a82fde0ecec241f404536573937e8123a79f40aa6ca78c79b8d89f75a5e78d0"
+
+  depends_on "go" => :build
+  depends_on "node" => :build
+  depends_on "yarn" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    srcpath = buildpath/"src/github.com/replicatedhq/ship"
+    srcpath.install buildpath.children
+    srcpath.cd do
+      system "make", "VERSION=#{version}", "build-minimal"
+      bin.install "bin/ship"
+    end
+  end
+
+  test do
+    assert_match(/#{version}/, shell_output("#{bin}/ship version"))
+    assert_match(/Usage:/, shell_output("#{bin}/ship --help"))
+
+    test_chart = "https://github.com/replicatedhq/test-charts/tree/master/plain-k8s"
+    system bin/"ship", "init", "--headless", test_chart
+  end
+end
