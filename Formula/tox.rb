867a1b04351e5b5f26ccbd4fb6b2715cae6ffe7e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-06 21:52:05 +0000

    tox: update 3.5.3_1 bottle.

--- Formula/tox.rb
+++ Formula/tox.rb
@@ -9,9 +9,9 @@ class Tox < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3f9df6096039e4d6df2f8aa21c9b530a6d76298994f7663d559907e225c1df61" => :mojave
-    sha256 "56fe350321d5c3333c6df29f1d2b1c9fd095199cadfa7b8b8f704a08bd950a74" => :high_sierra
-    sha256 "571fdddadff742860b529dd717449ef6d2db80d4aeb3b2ec01e05d1c6c41ec2e" => :sierra
+    sha256 "20c79df3b9ae22b0513f869cc66e8d1b4737eadd78c3f52ab20b37f645189ac3" => :mojave
+    sha256 "d13642d0a3b80a3adcda7a08bd583bd035b48f8fa0316e06c1e66db35023a006" => :high_sierra
+    sha256 "8d0f20de4f46c2730812cb4cd0ad31c8eb79ca8cf54e3fe3369a944bc1b2c89e" => :sierra
   end
 
   depends_on "python"
