092c6d24faead26a50ea0c28b6099a4a2b20cb7d
dt <daniel@teunis.cc>
2018-11-07 16:20:46 +0100

    youtube-dl 2018.11.07 (#33891)

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -1,8 +1,8 @@
 class YoutubeDl < Formula
   desc "Download YouTube videos from the command-line"
   homepage "https://rg3.github.io/youtube-dl/"
-  url "https://github.com/rg3/youtube-dl/releases/download/2018.11.03/youtube-dl-2018.11.03.tar.gz"
-  sha256 "920aff8c13a3fdf68f5d9975c3e4d9d64acd9e96a51561080405bc902187f086"
+  url "https://github.com/rg3/youtube-dl/releases/download/2018.11.07/youtube-dl-2018.11.07.tar.gz"
+  sha256 "69f6f7bc3280fe8fc89347050b07072e51fcca63d31da531f353d43545156ce7"
 
   head do
     url "https://github.com/rg3/youtube-dl.git"
