7cdfa480582cf86a35a43d510b7388ae28a89252
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-07 05:56:21 +0000

    gitmoji: add 1.9.2 bottle.

--- Formula/gitmoji.rb
+++ Formula/gitmoji.rb
@@ -5,6 +5,13 @@ class Gitmoji < Formula
   homepage "https://gitmoji.carloscuesta.me"
   url "https://registry.npmjs.org/gitmoji-cli/-/gitmoji-cli-1.9.2.tgz"
   sha256 "af7a6c7b0874d2971be909c9c2166b94db4fe1c3d1500f49cfac30d0a5d1b433"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "809f082f95b3ba8a3fb85557313556e94e6e730f6bf27bfb590ae96e7f431140" => :mojave
+    sha256 "9f254dfce3f33088a04924771633cc724dae7686ee80ae9ddd3c13542da2a614" => :high_sierra
+    sha256 "9bfb1c3478817213c023ed69a644dfd0446662f6045f0e34a383e3b91126c2dd" => :sierra
+  end
+
   depends_on "node"
 
   def install
