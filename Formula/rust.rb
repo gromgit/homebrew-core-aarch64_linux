04d8f28ff7193877b02e98777fa19b255ee56880
Chongyu Zhu <i@lembacon.com>
2018-11-09 02:02:41 +0800

    rust 1.30.1
    Closes #33929.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/rust.rb
+++ Formula/rust.rb
@@ -3,13 +3,13 @@ class Rust < Formula
   homepage "https://www.rust-lang.org/"
 
   stable do
-    url "https://static.rust-lang.org/dist/rustc-1.30.0-src.tar.gz"
-    sha256 "cd0ba83fcca55b64c0c9f23130fe731dfc1882b73ae21bef96be8f2362c108ee"
+    url "https://static.rust-lang.org/dist/rustc-1.30.1-src.tar.gz"
+    sha256 "36a38902dbd9a3e1240d46ab0f2ca40d2fd07c2ab6508ed7970c6c4c036b5b29"
 
     resource "cargo" do
       url "https://github.com/rust-lang/cargo.git",
-          :tag      => "0.31.0",
-          :revision => "36d96825d0f288c6d1bb2219919a277968bd365f"
+          :tag      => "0.31.1",
+          :revision => "a1a4ad37271b61209cd55d21f2c83f2773cbe113"
     end
 
     resource "racer" do
