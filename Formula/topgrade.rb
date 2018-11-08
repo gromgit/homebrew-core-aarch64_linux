406f7147c242497e4f9dd67fe9d9001eadaf7e69
Claudia <claui@users.noreply.github.com>
2018-11-08 10:43:08 +0100

    topgrade 0.19.0
    Closes #33917.
    
    Signed-off-by: Claudia <claui@users.noreply.github.com>

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -1,8 +1,8 @@
 class Topgrade < Formula
   desc "Upgrade all the things"
   homepage "https://github.com/r-darwish/topgrade"
-  url "https://github.com/r-darwish/topgrade/archive/v0.18.0.tar.gz"
-  sha256 "c72cefd01d2bad53ba48fabef8c0920813bac810920426f3de08d776dfbd7dbe"
+  url "https://github.com/r-darwish/topgrade/archive/v0.19.0.tar.gz"
+  sha256 "0b2ebb65f3fe4884186d2a47811b7036937a0e197de34786fc99c77ce4563f0f"
 
   bottle do
     sha256 "b7087d33d3b2fec5fbd2b61d684f2dac4fa6957a0508682de22db38a212d3297" => :mojave
@@ -19,5 +19,6 @@ class Topgrade < Formula
   test do
     output = shell_output("#{bin}/topgrade -n")
     assert_match "Dry running: #{HOMEBREW_PREFIX}/bin/brew upgrade", output
+    assert_not_match /\sSelf update\s/, output
   end
 end
