53251b071d5b77d2a95bd758a874894c1f201c7b
Jason Felice <jason.m.felice@gmail.com>
2018-11-08 09:32:54 -0500

    dash: fix shell quoting
    Closes #33920.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/dash.rb
+++ Formula/dash.rb
@@ -4,6 +4,7 @@ class Dash < Formula
   url "http://gondor.apana.org.au/~herbert/dash/files/dash-0.5.10.2.tar.gz"
   mirror "https://dl.bintray.com/homebrew/mirror/dash-0.5.10.2.tar.gz"
   sha256 "3c663919dc5c66ec991da14c7cf7e0be8ad00f3db73986a987c118862b5f6071"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -24,9 +25,7 @@ class Dash < Formula
 
     system "./configure", "--prefix=#{prefix}",
                           "--with-libedit",
-                          "--disable-dependency-tracking",
-                          "--enable-fnmatch",
-                          "--enable-glob"
+                          "--disable-dependency-tracking"
     system "make"
     system "make", "install"
   end
