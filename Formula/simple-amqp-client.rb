0511231b749a1791a44f45797fc7b7cc95868480
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-08 20:28:56 +0000

    simple-amqp-client: update 2.4.0_2 bottle.

--- Formula/simple-amqp-client.rb
+++ Formula/simple-amqp-client.rb
@@ -8,13 +8,9 @@ class SimpleAmqpClient < Formula
 
   bottle do
     cellar :any
-    sha256 "ae17788dba33efa3b41cb14ded5b483689002cedbf00693a8e348f3236ad759b" => :mojave
-    sha256 "50bbaa2902d2893692e28465666f39405ecd4481e37fbbb5b99d2ef7d3690bfa" => :high_sierra
-    sha256 "32ae102c9883a9a6464084284d34371e6538c75fdfbcd1f8230e9889be4a2956" => :sierra
-    sha256 "b2ea2a0ed47a8eff9498d3e90fd01e5e466cc1317515a8fa63b6262d88af6800" => :el_capitan
-    sha256 "4ce0051362b24556e552aadf852dc98910414ff9ed81d9c9efbbeafb863c8cb6" => :yosemite
-    sha256 "37b12090418d4423810cff30c484d0a11736bf856119e9757d9923a381db61bc" => :mavericks
-    sha256 "92986f1969aa18e48035b57dedcaec0bf5c098f597b8fa6d573112e4d266958a" => :mountain_lion
+    sha256 "3afc1f1345a391514200462825dcecf87bde3d25476d5f9bf87b787877c632c5" => :mojave
+    sha256 "fa561d92f855cf6613343e33741ac715d3fb42fce6ac2adf1fbc00fc641434df" => :high_sierra
+    sha256 "4f9fc0338fe628eaeb02c4cfb377a8f5e90da37896e6ec91968f30649069b129" => :sierra
   end
 
   depends_on "cmake" => :build
