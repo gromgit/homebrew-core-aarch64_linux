55e13925c0971c490c2b6dd8f20a30d9d26349d6
Roman Bolshakov <r.bolshakov@yadro.com>
2018-11-09 19:08:24 +0300

    crosstool-ng: add head, fix build
    Closes #33961.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/crosstool-ng.rb
+++ Formula/crosstool-ng.rb
@@ -3,7 +3,8 @@ class CrosstoolNg < Formula
   homepage "https://crosstool-ng.github.io/"
   url "http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-1.23.0.tar.xz"
   sha256 "68a43ea98ccf9cb345cb6eec494a497b224fee24c882e8c14c6713afbbe79196"
-  revision 2
+  revision 3
+  head "https://github.com/crosstool-ng/crosstool-ng.git"
 
   bottle do
     cellar :any
@@ -23,12 +24,25 @@ class CrosstoolNg < Formula
   depends_on "grep"
   depends_on "libtool"
   depends_on "m4"
+  depends_on "make"
   depends_on "ncurses" if DevelopmentTools.clang_build_version >= 1000
   depends_on "xz"
 
+  if build.head?
+    depends_on "bash"
+    depends_on "bison"
+    depends_on "gettext"
+  end
+
   def install
+    if build.head?
+      system "./bootstrap"
+      ENV["BISON"] = "#{Formula["bison"].opt_bin}/bison"
+      ENV.append "LDFLAGS", "-lintl"
+    end
+
     ENV["M4"] = "#{Formula["m4"].opt_bin}/m4"
-    ENV["MAKE"] = "/usr/bin/make" # prevent hardcoding make path from superenv
+    ENV["MAKE"] = "#{Formula["make"].opt_bin}/gmake"
 
     system "./configure", "--prefix=#{prefix}"
 
