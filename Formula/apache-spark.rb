3f87077fc54590e73051360d1a60c610d5590e3a
Jason Piper <j.piper@me.com>
2018-11-09 14:39:02 +0000

    apache-spark 2.4.0
    Closes #33955.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/apache-spark.rb
+++ Formula/apache-spark.rb
@@ -1,9 +1,9 @@
 class ApacheSpark < Formula
   desc "Engine for large-scale data processing"
   homepage "https://spark.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=spark/spark-2.3.2/spark-2.3.2-bin-hadoop2.7.tgz"
-  version "2.3.2"
-  sha256 "6246b20d95c7596a29fb26d5b50a3ae3163a35915bec6c515a8e183383bedc43"
+  url "https://www.apache.org/dyn/closer.lua?path=spark/spark-2.4.0/spark-2.4.0-bin-hadoop2.7.tgz"
+  version "2.4.0"
+  sha256 "c93c096c8d64062345b26b34c85127a6848cff95a4bb829333a06b83222a5cfa"
   head "https://github.com/apache/spark.git"
 
   bottle :unneeded
@@ -21,6 +21,6 @@ class ApacheSpark < Formula
   end
 
   test do
-    assert_match "Long = 1000", pipe_output(bin/"spark-shell", "sc.parallelize(1 to 1000).count()")
+    assert_match "Long = 1000", pipe_output(bin/"spark-shell --conf spark.driver.bindAddress=127.0.0.1", "sc.parallelize(1 to 1000).count()")
   end
 end
