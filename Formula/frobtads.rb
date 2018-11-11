def087b51b53d8a205bd33d03e05ce8a9a65f91b
Viktor Szakats <commit@vszakats.net>
2018-11-11 14:39:57 +0000

    frobtads: secure url(s)

--- Formula/frobtads.rb
+++ Formula/frobtads.rb
@@ -1,6 +1,6 @@
 class Frobtads < Formula
   desc "TADS interpreter and compilers"
-  homepage "http://www.tads.org/frobtads.htm"
+  homepage "https://www.tads.org/frobtads.htm"
   url "https://github.com/realnc/frobtads/releases/download/1.2.4/frobtads-1.2.4.tar.bz2"
   sha256 "705be5849293844f499a85280e793941b0eacb362b90d49d85ae8308e4c5b63c"
 
