c544818217dd71673190a6576a407b6e8f41dcdc
Chongyu Zhu <i@lembacon.com>
2018-11-13 01:05:06 +0800

    gflags 2.2.2
    Closes #34008.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/gflags.rb
+++ Formula/gflags.rb
@@ -1,8 +1,8 @@
 class Gflags < Formula
   desc "Library for processing command-line flags"
   homepage "https://gflags.github.io/gflags/"
-  url "https://github.com/gflags/gflags/archive/v2.2.1.tar.gz"
-  sha256 "ae27cdbcd6a2f935baa78e4f21f675649271634c092b1be01469440495609d0e"
+  url "https://github.com/gflags/gflags/archive/v2.2.2.tar.gz"
+  sha256 "34af2f15cf7367513b352bdcd2493ab14ce43692d2dcd9dfc499492966c64dcf"
 
   bottle do
     cellar :any
