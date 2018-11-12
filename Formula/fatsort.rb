8f9a7191d2ccd2b2d2ed0802a6efb3be1e2837b9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-12 01:06:45 +0000

    fatsort: update 1.5.0 bottle.

--- Formula/fatsort.rb
+++ Formula/fatsort.rb
@@ -7,11 +7,9 @@ class Fatsort < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6f9f2b6c33f3020aab2b7edad6ba01be97170cc128515179df3d03cab55ff894" => :mojave
-    sha256 "83d3f92d45b4ed4cf71453c07a6728841d5329957ff681ef29419067397d7e83" => :high_sierra
-    sha256 "d4742a8453bf67a815b862328b3714b1b30d7633688496b18350114e7c2acdd4" => :sierra
-    sha256 "96cc7c6ad5e64d86121b27e9d86ef33e1d2a6e9abde741253c0f06cf76249c8f" => :el_capitan
-    sha256 "717b1b1c912dd49ee1d034e640f9fcdd556634e2a29fd01da195e5e6a1e9f48b" => :yosemite
+    sha256 "30317b1edc9c062183ca98bf20baca935bf129405e15fc89056659968e33a83b" => :mojave
+    sha256 "d9e23a82a00b9747f24e78e2fc11f40ea8bebadfafa41fa7d5c2e6838d6514b7" => :high_sierra
+    sha256 "fd941f1bc940edcd9d23860ce723355be2249e330ecc8bbab4c5713525c78963" => :sierra
   end
 
   depends_on "help2man"
