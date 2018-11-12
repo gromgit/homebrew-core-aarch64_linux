e23215247db4df9e06b7f18eef2bdadff7bed40c
Roman Ivanov <me@roivanov.com>
2018-11-12 12:33:14 -0800

    groovy 2.5.4 (#34020)

--- Formula/groovy.rb
+++ Formula/groovy.rb
@@ -1,16 +1,17 @@
 class Groovy < Formula
   desc "Java-based scripting language"
   homepage "http://www.groovy-lang.org"
-  url "https://dl.bintray.com/groovy/maven/apache-groovy-binary-2.5.3.zip"
-  sha256 "47942bdb31a9f7ebf0db5b324bc99a289fd6ff87723e21e06f4701fa7c8c85c7"
+  url "https://dl.bintray.com/groovy/maven/apache-groovy-binary-2.5.4.zip"
+  sha256 "b2c936069831861d89cb8cf771bfa8a739b1d03c4be01b62d94e453e4b0bc6e8"
 
   bottle :unneeded
 
-  option "with-invokedynamic", "Install the InvokeDynamic version of Groovy (only works with Java 1.7+)"
+  option "with-invokedynamic", "Install the InvokeDynamic version of Groovy"
 
   deprecated_option "invokedynamic" => "with-invokedynamic"
 
-  depends_on :java => "1.6+"
+  # Groovy 2.5 requires JDK8+ to build and JDK7 is the minimum version of the JRE that we support.
+  depends_on :java => "1.7+"
 
   conflicts_with "groovysdk", :because => "both install the same binaries"
 
