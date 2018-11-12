c413a954b49bcd09481ac1668440c405c9357422
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-12 18:03:19 +0000

    prettier: update 1.15.2 bottle.

--- Formula/prettier.rb
+++ Formula/prettier.rb
@@ -8,9 +8,9 @@ class Prettier < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a199b4d24f64a59f00a126d153075843b7b1eb67d6d272234ad3a8d3d3d1ad21" => :mojave
-    sha256 "c61f4688e7cc3739bd6be61e5f0c6d1a20a3451e93348892a002e195d03f9d7d" => :high_sierra
-    sha256 "ada28c750e4dd3c3fb2889d95332a8a215d6079dc7db9797419b03b778965d45" => :sierra
+    sha256 "4e4a2211d03f015572fcd666195c3c19d7837bbc40e121053b8a52197d5fa638" => :mojave
+    sha256 "f9e155b58930c66d1661eae87524f36d914a1e37db5efcdb39e2056863124e6f" => :high_sierra
+    sha256 "db328a0adbd2977252018eddd49b98dbd1525148fd2f484bb04204101a67eede" => :sierra
   end
 
   depends_on "node"
