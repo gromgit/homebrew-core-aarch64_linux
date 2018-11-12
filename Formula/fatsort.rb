d63baed4a07f7929cb232779563dc5d6f89adf59
Matt Sephton <matt.sephton@gmail.com>
2018-11-12 00:59:37 +0000

    fatsort 1.5.0
    dated 08-09-2019
    
    Closes #33997.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/fatsort.rb
+++ Formula/fatsort.rb
@@ -1,9 +1,9 @@
 class Fatsort < Formula
   desc "Sorts FAT16 and FAT32 partitions"
   homepage "https://fatsort.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/fatsort/fatsort-1.4.2.439.tar.xz"
-  version "1.4.2"
-  sha256 "bdbcf99307baef3e76d99700691ac525c9a9cf96d8433b45c89314940cc6a1e0"
+  url "https://downloads.sourceforge.net/project/fatsort/fatsort-1.5.0.456.tar.xz"
+  version "1.5.0"
+  sha256 "a835b47814fd30d5bad464b839e9fc404bc1a6f5a5b1f6ed760ce9744915de95"
 
   bottle do
     cellar :any_skip_relocation
