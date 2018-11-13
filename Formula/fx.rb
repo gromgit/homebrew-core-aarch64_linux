1a9ed6bb2b04fd254f3a314963d868f7b52ae6f3
Anton Medvedev <anton@medv.io>
2018-11-13 12:50:39 +0700

    fx 3.1.0 (new formula)
    Closes #34027.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

---
+++ Formula/fx.rb
@@ -0,0 +1,19 @@
+require "language/node"
+
+class Fx < Formula
+  desc "Command-line JSON processing tool"
+  homepage "https://github.com/antonmedv/fx"
+  url "https://registry.npmjs.org/fx/-/fx-3.1.0.tgz"
+  sha256 "ec8a23b02511bd21554bfcdd7424695c1c28e950997ddde13bb665e6fdd92f76"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match "bar", shell_output("echo '{\"foo\": \"bar\"}' #{bin}/fx .foo")
+  end
+end
