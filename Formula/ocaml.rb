d1a0a805a9273ac2b1560b65b7847a099522447f
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2018-11-14 12:38:05 +0200

    ocaml 4.07.1

--- Formula/ocaml.rb
+++ Formula/ocaml.rb
@@ -14,9 +14,8 @@
 class Ocaml < Formula
   desc "General purpose programming language in the ML family"
   homepage "https://ocaml.org/"
-  url "https://caml.inria.fr/pub/distrib/ocaml-4.07/ocaml-4.07.0.tar.xz"
-  sha256 "f45bf720aadf5c6408400d5179b92ea7130b7e55e501a1768c2d136ef69f9f0f"
-  revision 1
+  url "https://caml.inria.fr/pub/distrib/ocaml-4.07/ocaml-4.07.1.tar.xz"
+  sha256 "dfe48b1da31da9c82d77612582fae74c80e8d1ac650e1c24f5ac9059e48307b8"
   head "https://github.com/ocaml/ocaml.git", :branch => "trunk"
 
   bottle do
@@ -39,11 +38,6 @@ class Ocaml < Formula
 
   depends_on :x11 => :optional
 
-  # Doesn't apply cleanly so cherry-picked the necessary part of:
-  # https://github.com/ocaml/ocaml/commit/3e265f97ecd4252b911d4a328ad16026b4afdb26
-  # Fixes: https://caml.inria.fr/mantis/view.php?id=7824
-  patch :DATA
-
   def install
     ENV.deparallelize # Builds are not parallel-safe, esp. with many cores
 
@@ -63,18 +57,3 @@ class Ocaml < Formula
     assert_match HOMEBREW_PREFIX.to_s, shell_output("#{bin}/ocamlc -where")
   end
 end
-
-__END__
-diff --git a/typing/ctype.ml b/typing/ctype.ml
-index f24e46d15a2f77b685800e9d8096b083ae58087a..9f21bf2913164effb3acdf227904c5df3c28a370 100644
---- a/typing/ctype.ml
-+++ b/typing/ctype.ml
-@@ -4132,6 +4132,8 @@ and subtype_row env trace row1 row2 cstrs =
-   let row1 = row_repr row1 and row2 = row_repr row2 in
-   let r1, r2, pairs =
-     merge_row_fields row1.row_fields row2.row_fields in
-+  let r1 = if row2.row_closed then filter_row_fields false r1 else r1 in
-+  let r2 = if row1.row_closed then filter_row_fields false r2 else r2 in
-   let more1 = repr row1.row_more
-   and more2 = repr row2.row_more in
-   match more1.desc, more2.desc with
