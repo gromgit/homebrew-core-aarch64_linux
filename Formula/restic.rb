63a2be37e1b450be77339c536411053a19e6bde3
Eli Young <elyscape@gmail.com>
2018-11-15 15:02:02 -0800

    restic: add completions and man pages
    Closes #34095.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/restic.rb
+++ Formula/restic.rb
@@ -3,6 +3,7 @@ class Restic < Formula
   homepage "https://restic.github.io/"
   url "https://github.com/restic/restic/archive/v0.9.3.tar.gz"
   sha256 "b95a258099aee9a56e620ccebcecabc246ee7f8390e3937ccedadd609c6d2dd0"
+  revision 1
   head "https://github.com/restic/restic.git"
 
   bottle do
@@ -18,7 +19,18 @@ class Restic < Formula
     ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
 
     system "go", "run", "build.go"
+
+    mkdir "completions"
+    system "./restic", "generate", "--bash-completion", "completions/restic"
+    system "./restic", "generate", "--zsh-completion", "completions/_restic"
+
+    mkdir "man"
+    system "./restic", "generate", "--man", "man"
+
     bin.install "restic"
+    bash_completion.install "completions/restic"
+    zsh_completion.install "completions/_restic"
+    man1.install Dir["man/*.1"]
   end
 
   test do
