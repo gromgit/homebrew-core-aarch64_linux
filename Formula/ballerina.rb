30cfa4b3eb17c880888799a5a55734449eec8ae9
Rasika <info.rasika@gmail.com>
2018-11-15 21:17:50 +0530

    ballerina 0.983.0
    Closes #34078.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/ballerina.rb
+++ Formula/ballerina.rb
@@ -1,8 +1,8 @@
 class Ballerina < Formula
   desc "The flexible, powerful and beautiful programming language"
   homepage "https://ballerina.io/"
-  url "https://product-dist.ballerina.io/downloads/0.982.0/ballerina-platform-0.982.0.zip"
-  sha256 "c766a21f831c97e6d99e315dc8b0b1a9047e34df8ae0042146c1d6e5862191e7"
+  url "https://product-dist.ballerina.io/downloads/0.983.0/ballerina-0.983.0.zip"
+  sha256 "faad9e8f71a6a2c9fe9bf36f27da02aa7e00d95955570e1a84fc95435f7ff3ae"
 
   bottle :unneeded
 
@@ -13,20 +13,13 @@ class Ballerina < Formula
     rm Dir["bin/*.bat"]
 
     chmod 0755, "bin/ballerina"
-    chmod 0755, "bin/composer"
 
     inreplace ["bin/ballerina"] do |s|
       s.gsub! /^BALLERINA_HOME=.*$/, "BALLERINA_HOME=#{libexec}"
       s.gsub! /\r?/, ""
     end
 
-    inreplace ["bin/composer"] do |s|
-      s.gsub! /^BASE_DIR=.*$/, "BASE_DIR=#{libexec}/bin"
-      s.gsub! /^PRGDIR=.*$/, "PRGDIR=#{libexec}/bin"
-      s.gsub! /\r?/, ""
-    end
-
-    bin.install "bin/ballerina", "bin/composer"
+    bin.install "bin/ballerina"
     libexec.install Dir["*"]
     lib.install_symlink libexec/"lib/resources"
     bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
