dc223c18b99ddeabaa8db06cce00b67c09e46d1e
Viktor Szakats <commit@vszakats.net>
2018-11-15 19:01:25 +0000

    zdelta: fix url(s)

--- Formula/zdelta.rb
+++ Formula/zdelta.rb
@@ -1,7 +1,7 @@
 class Zdelta < Formula
   desc "Lossless delta compression library"
-  homepage "http://cis.poly.edu/zdelta/"
-  url "http://cis.poly.edu/zdelta/downloads/zdelta-2.1.tar.gz"
+  homepage "https://web.archive.org/web/20150804051750/cis.poly.edu/zdelta/"
+  url "https://web.archive.org/web/20150804051752/cis.poly.edu/zdelta/downloads/zdelta-2.1.tar.gz"
   sha256 "03e6beb2e1235f2091f0146d7f41dd535aefb6078a48912d7d11973d5306de4c"
   head "https://github.com/snej/zdelta.git"
 
