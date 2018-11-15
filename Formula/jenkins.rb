d2109794559c5abf5d0d73c99f094a8232896ad1
Jeremiah Njoroge <jnjoroge@gmail.com>
2018-11-14 16:02:29 -0800

    jenkins 2.150
    Closes #34070.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/jenkins.rb
+++ Formula/jenkins.rb
@@ -1,8 +1,8 @@
 class Jenkins < Formula
   desc "Extendable open source continuous integration server"
   homepage "https://jenkins.io/"
-  url "http://mirrors.jenkins.io/war/2.149/jenkins.war"
-  sha256 "9283a72b5560802134c52ea2c46608cfb9d2f6cdae7c5dd2f08da09fc7ae4eca"
+  url "http://mirrors.jenkins.io/war/2.150/jenkins.war"
+  sha256 "90e827e570d013551157e78249b50806f5c3953f9845b634f5c0fc542bf54b9a"
 
   head do
     url "https://github.com/jenkinsci/jenkins.git"
