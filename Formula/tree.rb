6293a6f7da588d80eedf357dd118d33dd39b6380
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-16 19:08:23 +0000

    tree: update 1.8.0 bottle.

--- Formula/tree.rb
+++ Formula/tree.rb
@@ -6,14 +6,9 @@ class Tree < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "a6f52904af4d3a3096f465b9f5b0cc778b98655da0298699b39d593c5eafa9fd" => :mojave
-    sha256 "1a9ac89dc194918786c3175bdf439e74d16f7d27bd5fa4be7b006e5d0b077a1e" => :high_sierra
-    sha256 "f7c8a080435445e17c254ca53b016d9012bf768851df67c3d892ba71fe98e924" => :sierra
-    sha256 "2721bafcbe1db12e444f5025bdae81aa3c7cff6572aa7423e401baeb10a43157" => :el_capitan
-    sha256 "c1ad4f32c4922cbd1e37aa55b8fa6e0cc8a04c96a24f0d9e7957b50b311d854d" => :yosemite
-    sha256 "0bcb8a1de3ed51295917d6dd997ea492048f90a5ee1084676171a39dbd489654" => :mavericks
-    sha256 "c3bd091797c487bb1ac5586d0e524da341c6043b0298269a9ded271646274fa3" => :mountain_lion
+    sha256 "7152288c457dd893de50fa9d6ac9a8599748564e1b3586eec8eff7057089051a" => :mojave
+    sha256 "107d965994381d34e90b58a62f1c306c1b8a698db2696cdd905ba65c801ecc3b" => :high_sierra
+    sha256 "07d980571469a0cc699c69a8726eee338f782ba61c041e58f01ddb2924d08aeb" => :sierra
   end
 
   def install
