a0366d14ac12097d9addbc47cc6bad986fe7615f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-16 08:11:17 +0000

    sratoolkit: update 2.9.3 bottle.

--- Formula/sratoolkit.rb
+++ Formula/sratoolkit.rb
@@ -7,9 +7,9 @@ class Sratoolkit < Formula
 
   bottle do
     cellar :any
-    sha256 "fff1344bef86ad07532b7b9f0c9178a635de6e058f01c42639832ea5af1c009e" => :mojave
-    sha256 "3cfaf2f6059ae2195cd62221df0196c6d15b781f8650a5d871f8bde249ede08a" => :high_sierra
-    sha256 "d91cf9462326a3145a52ba2c14e3f8c092fe1efdb776e6f5f0627a15fe93e0a2" => :sierra
+    sha256 "3f096fe6b0e114fa80664808855571f1f4b5a90791d0ff27344045541fabcc1f" => :mojave
+    sha256 "eae0fff7e32f0c681c804686b5dc7dacbd8d051cc2538a3344b43c3e3c7c7b8d" => :high_sierra
+    sha256 "0f6ea3b9f3138766a401fa0c892c888eeaeabc95409d26669985ce72aca3f123" => :sierra
   end
 
   depends_on "hdf5"
