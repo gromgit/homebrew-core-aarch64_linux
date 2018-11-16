a47fae8e2ecb37dbd99e83539cb5fbfcbb6ecec5
Igor Kapkov <igasgeek@me.com>
2018-11-16 11:55:23 +1100

    range-v3 0.4.0
    Closes #34104.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/range-v3.rb
+++ Formula/range-v3.rb
@@ -1,8 +1,8 @@
 class RangeV3 < Formula
   desc "Experimental range library for C++11/14/17"
   homepage "https://ericniebler.github.io/range-v3/"
-  url "https://github.com/ericniebler/range-v3/archive/0.3.6.tar.gz"
-  sha256 "ce6e80c6b018ca0e03df8c54a34e1fd04282ac1b068cd39e902e2e5201ac117f"
+  url "https://github.com/ericniebler/range-v3/archive/0.4.0.tar.gz"
+  sha256 "5dbc878b7dfc500fb04b6b9f99d63993a2731ea34b0a4b8d5f670a5a71a18e39"
 
   bottle do
     cellar :any_skip_relocation
