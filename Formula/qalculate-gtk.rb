e7ee67a2bf2af5b2718e2102b570787e6aaa9077
Kieran Ramos <ramos.kieran@gmail.com>
2018-11-16 13:21:00 -0500

    qalculate-gtk 2.8.1 (new formula)
    Closes #34227.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/qalculate-gtk.rb
@@ -0,0 +1,21 @@
+class QalculateGtk < Formula
+  desc "Multi-purpose desktop calculator"
+  homepage "https://qalculate.github.io/"
+  url "https://github.com/Qalculate/qalculate-gtk/releases/download/v2.8.1/qalculate-gtk-2.8.1.tar.gz"
+  sha256 "d22a77ce0b480f3a6f7c1288eed1c7969d74bb4f1bd395f6a45335dae3f50115"
+
+  depends_on "intltool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "adwaita-icon-theme"
+  depends_on "gtk+3"
+  depends_on "libqalculate"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/qalculate-gtk", "-v"
+  end
+end
