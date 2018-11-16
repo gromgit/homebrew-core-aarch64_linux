246ff86e0c0793fcaca1f3b54ba79a81a865ca46
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-16 00:50:07 +0000

    fio: update 3.12 bottle.

--- Formula/fio.rb
+++ Formula/fio.rb
@@ -6,10 +6,9 @@ class Fio < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "39a042775bc8e7f84c18cf714080ca0b70768625f41dc085a6ceb1c116a2875e" => :mojave
-    sha256 "0fddb60bfc65a89bce2b7f9a0226e8f792302c29bd3c18684e28a943c13bd6af" => :high_sierra
-    sha256 "528b4c5cde0ef580ad4dc192b54588c9c5302f48d4ef84bf7c3063a2eb50761c" => :sierra
-    sha256 "2474d07db0d530e110500dbc7d64e7b43cbb527b9c0d42b439b551fe6779b3bf" => :el_capitan
+    sha256 "85f5b161461e6379af7cbc8b08f549cb56af1b073437165975c3c1cd754027ca" => :mojave
+    sha256 "c4c5d539d96b00a5cafaee423102fa45b8761d9aa128656ff82ae6ba95f680b6" => :high_sierra
+    sha256 "34754b4ae14c129cb9483f038d6cc885a2ea0701ef6eb771bcf6b430afafe606" => :sierra
   end
 
   def install
