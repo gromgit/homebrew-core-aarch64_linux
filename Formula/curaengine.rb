5673314d473db7d206249329624094b907ad6039
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-16 07:57:46 +0000

    curaengine: update 15.04.6 bottle.

--- Formula/curaengine.rb
+++ Formula/curaengine.rb
@@ -7,12 +7,9 @@ class Curaengine < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "f3e4523d845e891ef23e56d95ab48454d22bebcc0f179a0806096009833e6acb" => :mojave
-    sha256 "750b6ca6224c47f14cafa72bdca8f204348fdffda46289234b6f1e3c7f8b53ce" => :high_sierra
-    sha256 "6319dc4f7e2648f801728e4aaf0aff747ae305ca9f6130181d164f222f40d160" => :sierra
-    sha256 "73def7a0bbe0e297fd6490c0b0fd481265814918ffa42bf83febd5f1e68d7149" => :el_capitan
-    sha256 "4252263a845ca5fc5631c4211eac9a95ad53e20cc07dfa0ea6422ea83ca178e7" => :yosemite
+    sha256 "03459dc8fd06bc5522b8d0084ac2dd05f238316f009e8fa792f5e55218f5852e" => :mojave
+    sha256 "b29d96d522832565490392c1945bec5be2fe5b48f2331fd121f706e06d7b6912" => :high_sierra
+    sha256 "2f2c5d334057a9e99ef969f7f2cb66d357ab0c98e501a22103b4c53faa0ca8e8" => :sierra
   end
 
   def install
