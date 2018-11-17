cb9b4befdd7eb8fb9fa1403cd5ee2ee59811e61c
Igor Kapkov <igasgeek@me.com>
2018-11-17 13:59:22 +1100

    sfcgal 1.3.6
    Closes #34208.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/sfcgal.rb
+++ Formula/sfcgal.rb
@@ -1,9 +1,8 @@
 class Sfcgal < Formula
   desc "C++ wrapper library around CGAL"
   homepage "http://sfcgal.org/"
-  url "https://github.com/Oslandia/SFCGAL/archive/v1.3.5.tar.gz"
-  sha256 "e36937d1d8421134c601e80a42bd535b1d9d7e7dd5bdf4da355e58942ba56006"
-  revision 1
+  url "https://github.com/Oslandia/SFCGAL/archive/v1.3.6.tar.gz"
+  sha256 "5840192eb4a1a4e500f65eedfebacd4bc4b9192c696ea51d719732dc2c75530a"
 
   bottle do
     sha256 "991c98fba3828ffab869051ee2dadcfc909f10c49fb3fa26a6df902a161c75b6" => :mojave
@@ -16,20 +15,6 @@ class Sfcgal < Formula
   depends_on "cgal"
   depends_on "gmp"
   depends_on "mpfr"
-
-  # These two upstream commits fix build failures with CGAL 4.12.
-  # Remove with next version.
-  # https://github.com/Oslandia/SFCGAL/pull/168
-  patch do
-    url "https://github.com/Oslandia/SFCGAL/commit/e47828f7.diff?full_index=1"
-    sha256 "5ce8b251a73f9a2f1803ca5d8a455007baedf1a2b278a2d3465af9955d79c09e"
-  end
-
-  # https://github.com/Oslandia/SFCGAL/pull/169
-  patch do
-    url "https://github.com/Oslandia/SFCGAL/commit/2ef10f25.diff?full_index=1"
-    sha256 "bc53ce8405b0400d8c37af7837a945dfd96d73cd4a876114f48441fbaeb9d96d"
-  end
 
   def install
     system "cmake", ".", *std_cmake_args
