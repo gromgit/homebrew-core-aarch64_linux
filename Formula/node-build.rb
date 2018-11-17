954813a74c7c45844c6acf2355c39273cb2742a8
Igor Kapkov <igasgeek@me.com>
2018-11-17 13:43:04 +1100

    node-build 4.0.0
    Closes #34204.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/node-build.rb
+++ Formula/node-build.rb
@@ -1,8 +1,8 @@
 class NodeBuild < Formula
   desc "Install NodeJS versions"
   homepage "https://github.com/nodenv/node-build"
-  url "https://github.com/nodenv/node-build/archive/v3.1.0.tar.gz"
-  sha256 "155a2d01a34f15cc6b60dda6c724134ad2e16ad5c5ea07ba59d03ac906030e05"
+  url "https://github.com/nodenv/node-build/archive/v4.0.0.tar.gz"
+  sha256 "b13404558812df45983d7ea794c4c0468bca1abc0ce0e6fe7b96cf9e44b91d97"
   head "https://github.com/nodenv/node-build.git"
 
   bottle :unneeded
