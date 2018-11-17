775ae24db9a12bbab1f845371fdbdc4b211af71a
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-11-17 10:54:43 +0100

    root 6.14.06
    Closes #34243.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/root.rb
+++ Formula/root.rb
@@ -1,10 +1,9 @@
 class Root < Formula
   desc "Object oriented framework for large scale data analysis"
   homepage "https://root.cern.ch/"
-  url "https://root.cern.ch/download/root_v6.14.04.source.tar.gz"
-  version "6.14.04"
-  sha256 "463ec20692332a422cfb5f38c78bedab1c40ab4d81be18e99b50cf9f53f596cf"
-  revision 2
+  url "https://root.cern.ch/download/root_v6.14.06.source.tar.gz"
+  version "6.14.06"
+  sha256 "0fb943b61396f282b289e35c455a9ab60126229be1bd3f04a8f00b37c13ab432"
   head "https://github.com/root-project/root.git"
 
   bottle do
