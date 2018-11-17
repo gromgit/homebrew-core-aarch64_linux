1d0038712ab6a84e3fd18b0d8353fa2b360452e7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-17 07:43:35 +0000

    tty-solitaire: update 1.1.1 bottle.

--- Formula/tty-solitaire.rb
+++ Formula/tty-solitaire.rb
@@ -6,10 +6,9 @@ class TtySolitaire < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5c52c1b3615d4ae14895908b92bbe2e944ce43818813c8aabac1445062503453" => :mojave
-    sha256 "32537d5e73f3c0201338362905d532887e0cf5ae4c62c263f066bf201070d0d5" => :high_sierra
-    sha256 "5707d42484dfc0135f818fae404970c8b95bde7453c8f7cad91610f37d2b351d" => :sierra
-    sha256 "8ca0cd759bb29d030e91a9c7b0d4043cc7a98ab7c2ffabc010f1f842d65e085b" => :el_capitan
+    sha256 "9433dbbb39b808bfbe06f1b58b1aff782ca2bdeced389aaf0207fee720cfb0ce" => :mojave
+    sha256 "6451237a79182add9dfa4678f54473adfac2a5fc47918d10eb188debcd5d9add" => :high_sierra
+    sha256 "f6df872618485b7e759b8de0d3e890d1e465b0fad7e2f0373227d98ed857ebea" => :sierra
   end
 
   def install
