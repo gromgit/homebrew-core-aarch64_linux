50c4ae03d933a76b2671bfae6162be16e049ceb5
Igor Kapkov <igasgeek@me.com>
2018-11-17 15:40:07 +1100

    vagrant-completion 2.2.1
    Closes #34230.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/vagrant-completion.rb
+++ Formula/vagrant-completion.rb
@@ -1,8 +1,8 @@
 class VagrantCompletion < Formula
   desc "Bash completion for Vagrant"
   homepage "https://github.com/hashicorp/vagrant"
-  url "https://github.com/hashicorp/vagrant/archive/v2.2.0.tar.gz"
-  sha256 "cb1476683f0e8479334be20b26c49d516a0e1c739b9f6f35a8c05f68e4a048f1"
+  url "https://github.com/hashicorp/vagrant/archive/v2.2.1.tar.gz"
+  sha256 "50ecee4e9362fa99115bdbc66d15e80f2440d6479204ac7abf7fb8e77c0fbc03"
   head "https://github.com/hashicorp/vagrant.git"
 
   bottle :unneeded
