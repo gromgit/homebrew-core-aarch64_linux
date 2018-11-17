9a5971184689d76ed0a4dea0e93723ee48d6e8c7
Igor Kapkov <igasgeek@me.com>
2018-11-17 15:26:11 +1100

    tty-solitaire 1.1.1
    Closes #34226.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/tty-solitaire.rb
+++ Formula/tty-solitaire.rb
@@ -1,8 +1,8 @@
 class TtySolitaire < Formula
   desc "Ncurses-based klondike solitaire game"
   homepage "https://github.com/mpereira/tty-solitaire"
-  url "https://github.com/mpereira/tty-solitaire/archive/v1.1.0.tar.gz"
-  sha256 "46c531e2439733282cd6a1e09646d7b9c03b25fb5c0e888b36443c42cc5ee6f2"
+  url "https://github.com/mpereira/tty-solitaire/archive/v1.1.1.tar.gz"
+  sha256 "146f9ed6ee9d79dfd936c213e3a413a967daba3978fb1021e05a1c52c7684e9c"
 
   bottle do
     cellar :any_skip_relocation
