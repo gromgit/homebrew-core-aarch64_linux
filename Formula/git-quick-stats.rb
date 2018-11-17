6843377a01e1f26ef0a8bdb12a1d8ced1e103536
Igor Kapkov <igasgeek@me.com>
2018-11-17 15:33:59 +1100

    git-quick-stats 1.0.14
    Closes #34228.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/git-quick-stats.rb
+++ Formula/git-quick-stats.rb
@@ -1,8 +1,8 @@
 class GitQuickStats < Formula
   desc "Simple and efficient way to access statistics in git"
   homepage "https://github.com/arzzen/git-quick-stats"
-  url "https://github.com/arzzen/git-quick-stats/archive/1.0.11.tar.gz"
-  sha256 "b67f567e6f1dd1bb39943f31b4e9d8fc2c1c9414f91aeddc9b55c15018d9c5d5"
+  url "https://github.com/arzzen/git-quick-stats/archive/1.0.14.tar.gz"
+  sha256 "835f6a207203d8ccbf35acaa1ccb5f7de4fe2e98ff7ea72f52be7b1fbffe951c"
 
   bottle :unneeded
 
