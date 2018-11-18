2a67d4a3b22f0d69597d62519c537f548037c604
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-18 19:11:35 +0000

    libvirt: update 4.9.0 bottle.

--- Formula/libvirt.rb
+++ Formula/libvirt.rb
@@ -6,9 +6,9 @@ class Libvirt < Formula
   head "https://github.com/libvirt/libvirt.git"
 
   bottle do
-    sha256 "82b492f1b78ff924c2f17a271c5d24b8147ef5d1350fc7ded796edf7241306a3" => :mojave
-    sha256 "b8d09cc19613f1bae96c986caf24bf9f0485e479bdfdf8a141a1b157e3346b14" => :high_sierra
-    sha256 "c884c9f923fb6c5dd124c8802dbeb0bad7b46f58ba64462747c6794c67bccf55" => :sierra
+    sha256 "28567968e4eea5fb6fcde271745f4011591620173a444be047f62fa5e37e6aaa" => :mojave
+    sha256 "f14f8eefa731eb7761dd03d63f2fc937ebc0dcc26f13b7acad75ac9d27ca29e7" => :high_sierra
+    sha256 "c51bcefec712847ce9ae8e36bc2a6536109a53884499023f84c70b46fd8d205a" => :sierra
   end
 
   depends_on "pkg-config" => :build
