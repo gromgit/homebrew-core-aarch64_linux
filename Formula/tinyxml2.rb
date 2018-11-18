a4648630a8b8b90addf5ed9ca0074def145406af
Steven Peters <scpeters@openrobotics.org>
2018-11-17 22:53:20 -0800

    tinyxml2 7.0.1
    Closes #34249.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/tinyxml2.rb
+++ Formula/tinyxml2.rb
@@ -1,8 +1,8 @@
 class Tinyxml2 < Formula
   desc "Improved tinyxml (in memory efficiency and size)"
   homepage "http://grinninglizard.com/tinyxml2"
-  url "https://github.com/leethomason/tinyxml2/archive/7.0.0.tar.gz"
-  sha256 "fa0d1c745d65d4d833e62cb183e23c2034dc7a35ec1a4977e808bdebb9b4fe60"
+  url "https://github.com/leethomason/tinyxml2/archive/7.0.1.tar.gz"
+  sha256 "a381729e32b6c2916a23544c04f342682d38b3f6e6c0cad3c25e900c3a7ef1a6"
   head "https://github.com/leethomason/tinyxml2.git"
 
   bottle do
