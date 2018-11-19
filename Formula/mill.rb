43a0ce2fd3099abc8753b9e5436016d801bf189a
Igor Kapkov <igasgeek@me.com>
2018-11-19 11:55:42 +1100

    mill 0.3.5
    Closes #34273.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/mill.rb
+++ Formula/mill.rb
@@ -1,8 +1,8 @@
 class Mill < Formula
   desc "Scala build tool"
   homepage "https://www.lihaoyi.com/mill/"
-  url "https://github.com/lihaoyi/mill/releases/download/0.3.4/0.3.4"
-  sha256 "6ff09ffe4f774b19e0314c0c9702f02a3cfda5b14f5af2fe11eb0d22b1ae8acd"
+  url "https://github.com/lihaoyi/mill/releases/download/0.3.5/0.3.5"
+  sha256 "c869f584eeadd3a9a7a37a6ad1af6e0c8308b603b62986da2b28cb6d5c406aa6"
 
   bottle :unneeded
 
