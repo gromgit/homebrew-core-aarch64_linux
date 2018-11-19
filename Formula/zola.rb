088efc4fb0bf8f4b0cf0f41eab246b4607fba9cb
Igor Kapkov <igasgeek@me.com>
2018-11-19 12:44:59 +1100

    gutenberg: rename to zola
    Closes #34283.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

---
+++ Formula/zola.rb
@@ -0,0 +1,37 @@
+class Zola < Formula
+  desc "Fast static site generator in a single binary with everything built-in"
+  homepage "https://www.getzola.org/"
+  url "https://github.com/getzola/zola/archive/v0.5.0.tar.gz"
+  sha256 "718aaec6e0db6c0e6fb310d8a1ef3978f41a173e39b1c2515ca7365690b09cff"
+
+  depends_on "cmake" => :build
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--root", prefix, "--path", "."
+
+    bash_completion.install "completions/zola.bash"
+    zsh_completion.install "completions/_zola"
+    fish_completion.install "completions/zola.fish"
+  end
+
+  test do
+    system "yes '' | #{bin}/zola init mysite"
+    (testpath/"mysite/content/blog/index.md").write <<~EOS
+      +++
+      +++
+
+      Hi I'm Homebrew.
+    EOS
+    (testpath/"mysite/templates/page.html").write <<~EOS
+      {{ page.content | safe }}
+    EOS
+
+    cd testpath/"mysite" do
+      system bin/"zola", "build"
+    end
+
+    assert_equal "<p>Hi I'm Homebrew.</p>",
+      (testpath/"mysite/public/blog/index.html").read.strip
+  end
+end
