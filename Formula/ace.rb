81681b96a83c26c631752929ec014770733892c7
Johnny Willemsen <jwillemsen@remedy.nl>
2018-11-19 11:34:07 +0100

    ace 6.5.3
    Closes #34289.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/ace.rb
+++ Formula/ace.rb
@@ -1,9 +1,8 @@
 class Ace < Formula
   desc "ADAPTIVE Communication Environment: OO network programming in C++"
   homepage "https://www.dre.vanderbilt.edu/~schmidt/ACE.html"
-  url "https://github.com/DOCGroup/ACE_TAO/releases/download/ACE%2BTAO-6_5_2/ACE.tar.bz2"
-  version "6.5.2"
-  sha256 "f0393d6df25ee92e0cbc6539c68ccf122caae0ffd5ae9a786163403bb2306cc5"
+  url "https://github.com/DOCGroup/ACE_TAO/releases/download/ACE%2BTAO-6_5_3/ACE-6.5.3.tar.bz2"
+  sha256 "b1d6a716394bd15c21bb90037b8a12a4d8034cc9d8878b0ad39b3c467df19b1a"
 
   bottle do
     cellar :any
