0ceae8b0e39e5b10c64bb500f1d56431e5f5d2e9
Wietse Muizelaar <wmuizelaar@bol.com>
2018-11-20 13:25:32 +0100

    opa 0.10.1 (new formula)
    Closes #34312.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/opa.rb
@@ -0,0 +1,26 @@
+class Opa < Formula
+  desc "Open source, general-purpose policy engine"
+  homepage "https://www.openpolicyagent.org"
+  url "https://github.com/open-policy-agent/opa/archive/v0.10.1.tar.gz"
+  sha256 "6b6121cbf7efc42b6ebc3f5ba6a2533e4e6eaf4b9eec446fb6f8fce4e6c02ae7"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/open-policy-agent/opa").install buildpath.children
+
+    cd "src/github.com/open-policy-agent/opa" do
+      system "go", "build", "-o", bin/"opa", "-installsuffix", "static",
+                   "-ldflags",
+                   "-X github.com/open-policy-agent/opa/version.Version=#{version}"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    output = shell_output("#{bin}/opa eval -f pretty '[x, 2] = [1, y]' 2>&1")
+    assert_equal "+---+---+\n| x | y |\n+---+---+\n| 1 | 2 |\n+---+---+\n", output
+    assert_match "Version: #{version}", shell_output("#{bin}/opa version 2>&1")
+  end
+end
