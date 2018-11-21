7eda8ba88a3c6e2b42ab3e92a3800cce463f6da6
Mriyam Tamuli <mbtamuli@gmail.com>
2018-11-22 04:18:12 +0530

    easyengine 4.0.1 (new formula)
    Closes #34378.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

---
+++ Formula/easyengine.rb
@@ -0,0 +1,26 @@
+class Easyengine < Formula
+  desc "Command-line control panel to manage WordPress sites"
+  homepage "https://easyengine.io/"
+  url "https://github.com/EasyEngine/easyengine/releases/download/v4.0.1/easyengine.phar"
+  sha256 "247f2a58f414cc003467538f1c60139b9fef6d41a520aeafc872e55058bad1db"
+
+  bottle :unneeded
+
+  depends_on "php"
+
+  def install
+    bin.install "easyengine.phar" => "ee"
+  end
+
+  test do
+    system bin/"ee config set locale hi_IN"
+    output = shell_output("#{bin}/ee config get locale")
+    assert_match "hi_IN", output
+
+    output = shell_output("#{bin}/ee cli version")
+    assert_match "EE #{version}", output
+
+    output = shell_output("#{bin}/ee cli info")
+    assert_match "Darwin", output
+  end
+end
