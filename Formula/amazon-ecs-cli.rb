5d1984dae4996a78eab8c144e85600502310edba
Igor Kapkov <igasgeek@me.com>
2018-11-22 09:32:19 +1100

    amazon-ecs-cli 1.12.0
    Closes #34376.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/amazon-ecs-cli.rb
+++ Formula/amazon-ecs-cli.rb
@@ -1,8 +1,8 @@
 class AmazonEcsCli < Formula
   desc "CLI for Amazon ECS to manage clusters and tasks for development"
   homepage "https://aws.amazon.com/ecs"
-  url "https://github.com/aws/amazon-ecs-cli/archive/v1.11.1.tar.gz"
-  sha256 "df14915043aecec4c36106326b1fe6f6c50c801c5995b3172209f1b12fd36cc2"
+  url "https://github.com/aws/amazon-ecs-cli/archive/v1.12.0.tar.gz"
+  sha256 "6eb2dc138b10ea6f205f1855a11c333405592ab4683dc1a044561299d7d597c5"
 
   bottle do
     cellar :any_skip_relocation
