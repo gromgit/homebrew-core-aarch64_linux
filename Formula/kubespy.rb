2add906922a1801b5458c5e1eed842a5dd99c3d5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-22 04:59:07 +0000

    kubespy: add 0.4.0 bottle.

--- Formula/kubespy.rb
+++ Formula/kubespy.rb
@@ -5,6 +5,13 @@ class Kubespy < Formula
       :tag      => "v0.4.0",
       :revision => "9cca3f1b07e33c5c8a11c804c9276cbb75338641"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e3fff5273f5719aa828347b1d1b825874faac7989f240326ecf1ade567c1f47f" => :mojave
+    sha256 "1f08465867b5108de94ec3e8bc68beb1af8af1300a3bcff246eff662c9d92c65" => :high_sierra
+    sha256 "236a51cb10fd31ecd508ca2deb18c770edc9f850d9a3b30d85c737251269deba" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
