3150377108dfa960cadc0ac3daa814ed4007046e
Mike McQuaid <mike@mikemcquaid.com>
2018-11-24 12:47:22 +0000

    vim@7.4: remove options.

--- Formula/vim@7.4.rb
+++ Formula/vim@7.4.rb
@@ -3,7 +3,7 @@ class VimAT74 < Formula
   homepage "https://www.vim.org/"
   url "https://github.com/vim/vim/archive/v7.4.2367.tar.gz"
   sha256 "a9ae4031ccd73cc60e771e8bf9b3c8b7f10f63a67efce7f61cd694cd8d7cda5c"
-  revision 15
+  revision 16
 
   bottle do
     sha256 "b70d1d137779745209320006a000d07889414b84e4d9163e239b7b41ef0f5754" => :mojave
@@ -13,28 +13,10 @@ class VimAT74 < Formula
 
   keg_only :versioned_formula
 
-  option "with-override-system-vi", "Override system vi"
-  option "without-nls", "Build vim without National Language Support (translated messages, keymaps)"
-  option "with-client-server", "Enable client/server mode"
-
-  LANGUAGES_OPTIONAL = %w[lua mzscheme python@2 tcl].freeze
-  LANGUAGES_DEFAULT  = %w[python].freeze
-
-  option "with-python@2", "Build vim with python@2 instead of python[3] support"
-  LANGUAGES_OPTIONAL.each do |language|
-    option "with-#{language}", "Build vim with #{language} support"
-  end
-  LANGUAGES_DEFAULT.each do |language|
-    option "without-#{language}", "Build vim without #{language} support"
-  end
-
+  depends_on "lua"
   depends_on "perl"
+  depends_on "python"
   depends_on "ruby"
-  depends_on :x11 if build.with? "client-server"
-  depends_on "python" => :recommended
-  depends_on "lua" => :optional
-  depends_on "luajit" => :optional
-  depends_on "python@2" => :optional
 
   # Python 3.7 compat
   # Equivalent to upstream commit 24 Mar 2018 "patch 8.0.1635: undefining
@@ -52,36 +34,6 @@ class VimAT74 < Formula
     # vim doesn't require any Python package, unset PYTHONPATH.
     ENV.delete("PYTHONPATH")
 
-    opts = ["--enable-perlinterp", "--enable-rubyinterp"]
-
-    (LANGUAGES_OPTIONAL + LANGUAGES_DEFAULT).each do |language|
-      feature = { "python" => "python3", "python@2" => "python" }
-      if build.with? language
-        opts << "--enable-#{feature.fetch(language, language)}interp"
-      end
-    end
-
-    if opts.include?("--enable-pythoninterp") && opts.include?("--enable-python3interp")
-      # only compile with either python or python@2 support, but not both
-      # (if vim74 is compiled with +python3/dyn, the Python[3] library lookup segfaults
-      # in other words, a command like ":py3 import sys" leads to a SEGV)
-      opts -= %w[--enable-python3interp]
-    end
-
-    opts << "--disable-nls" if build.without? "nls"
-    opts << "--enable-gui=no"
-
-    if build.with? "client-server"
-      opts << "--with-x"
-    else
-      opts << "--without-x"
-    end
-
-    if build.with? "luajit"
-      opts << "--with-luajit"
-      opts << "--enable-luainterp"
-    end
-
     # We specify HOMEBREW_PREFIX as the prefix to make vim look in the
     # the right place (HOMEBREW_PREFIX/share/vim/{vimrc,vimfiles}) for
     # system vimscript files. We specify the normal installation prefix
@@ -94,7 +46,13 @@ class VimAT74 < Formula
                           "--with-tlib=ncurses",
                           "--enable-cscope",
                           "--with-compiledby=Homebrew",
-                          *opts
+                          "--enable-perlinterp",
+                          "--enable-rubyinterp",
+                          "--enable-python3interp",
+                          "--enable-gui=no",
+                          "--without-x",
+                          "--enable-luainterp",
+                          "--with-lua-prefix=#{Formula["lua"].opt_prefix}"
     system "make"
     # Parallel install could miss some symlinks
     # https://github.com/vim/vim/issues/1031
@@ -131,9 +89,9 @@ index 02d913492c..59c115dd8d 100644
 --- a/src/if_python3.c
 +++ b/src/if_python3.c
 @@ -34,11 +34,6 @@
- 
+
  #include <limits.h>
- 
+
 -/* Python.h defines _POSIX_THREADS itself (if needed) */
 -#ifdef _POSIX_THREADS
 -# undef _POSIX_THREADS
