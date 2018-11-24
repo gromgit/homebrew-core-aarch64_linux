f27db6ae2ca2cb00d928edc856071f88bb5df1a0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-24 18:02:36 +0000

    node@8: update 8.12.0 bottle.

--- Formula/node@8.rb
+++ Formula/node@8.rb
@@ -5,11 +5,10 @@ class NodeAT8 < Formula
   sha256 "5a9dff58016c18fb4bf902d963b124ff058a550ebcd9840c677757387bce419a"
 
   bottle do
-    rebuild 1
-    sha256 "46b430483d0541dd6b8d5f4bc834fcba14719c53233dfd59d9a9e24283c0f80e" => :mojave
-    sha256 "2c13559186878d8562fe5acea407aa9f1a57dd33a5b147b3e3f0aac61661f2fd" => :high_sierra
-    sha256 "60c20bd219f2dab1593a3624749b8f6fa1db25fb0a17c4a79e0b762f6f2407f0" => :sierra
-    sha256 "e24f5ad36356819ce8728108440a3cd1e1b6938cc2beaa9a86516cf7fe8ea2d0" => :el_capitan
+    rebuild 2
+    sha256 "851b2a7c918c4d39ca2015892ca3d35ed8d8f8a4bda1705667e3cf906a1e4cf4" => :mojave
+    sha256 "b206862d22ca610de419d4870e8dc776937281dd73908c784f6bcf1f7937235b" => :high_sierra
+    sha256 "50da69326edd9b6a5333f553cc636e0b8042eb142686337e2d9b7221e222df3c" => :sierra
   end
 
   keg_only :versioned_formula
