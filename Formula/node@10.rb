3f90e8ef969b04002f8fbd3fd3ca31cfb7f02613
Mike McQuaid <mike@mikemcquaid.com>
2018-11-24 12:45:07 +0000

    node@10: remove options.

--- Formula/node@10.rb
+++ Formula/node@10.rb
@@ -12,16 +12,9 @@ class NodeAT10 < Formula
 
   keg_only :versioned_formula
 
-  option "with-openssl", "Build against Homebrew's OpenSSL instead of the bundled OpenSSL"
-  option "without-npm", "npm will not be installed"
-  option "without-icu4c", "Build with small-icu (English only) instead of system-icu (all locales)"
-
-  deprecated_option "with-openssl" => "with-openssl@1.1"
-
   depends_on "pkg-config" => :build
   depends_on "python@2" => :build
-  depends_on "icu4c" => :recommended
-  depends_on "openssl" => :optional
+  depends_on "icu4c"
 
   # Per upstream - "Need g++ 4.8 or clang++ 3.4".
   fails_with :clang if MacOS.version <= :snow_leopard
@@ -32,28 +25,12 @@ class NodeAT10 < Formula
   end
 
   def install
-    args = ["--prefix=#{prefix}"]
-    args << "--without-npm" if build.without? "npm"
-    args << "--with-intl=system-icu" if build.with? "icu4c"
-    args << "--shared-openssl" << "--openssl-use-def-ca-store" if build.with? "openssl"
-
-    system "./configure", *args
+    system "./configure", "--prefix=#{prefix}", "--with-intl=system-icu"
     system "make", "install"
   end
 
   def post_install
-    return if build.without? "npm"
     (lib/"node_modules/npm/npmrc").atomic_write("prefix = #{HOMEBREW_PREFIX}\n")
-  end
-
-  def caveats
-    if build.without? "npm"
-      <<~EOS
-        Homebrew has NOT installed npm. If you later install it, you should supplement
-        your NODE_PATH with the npm module folder:
-          #{HOMEBREW_PREFIX}/lib/node_modules
-      EOS
-    end
   end
 
   test do
@@ -64,24 +41,21 @@ class NodeAT10 < Formula
     assert_equal "hello", output
     output = shell_output("#{bin}/node -e 'console.log(new Intl.NumberFormat(\"en-EN\").format(1234.56))'").strip
     assert_equal "1,234.56", output
-    if build.with? "icu4c"
-      output = shell_output("#{bin}/node -e 'console.log(new Intl.NumberFormat(\"de-DE\").format(1234.56))'").strip
-      assert_equal "1.234,56", output
-    end
 
-    if build.with? "npm"
-      # make sure npm can find node
-      ENV.prepend_path "PATH", opt_bin
-      ENV.delete "NVM_NODEJS_ORG_MIRROR"
-      assert_equal which("node"), opt_bin/"node"
-      assert_predicate bin/"npm", :exist?, "npm must exist"
-      assert_predicate bin/"npm", :executable?, "npm must be executable"
-      npm_args = ["-ddd", "--cache=#{HOMEBREW_CACHE}/npm_cache", "--build-from-source"]
-      system "#{bin}/npm", *npm_args, "install", "npm@latest"
-      system "#{bin}/npm", *npm_args, "install", "bignum"
-      assert_predicate bin/"npx", :exist?, "npx must exist"
-      assert_predicate bin/"npx", :executable?, "npx must be executable"
-      assert_match "< hello >", shell_output("#{bin}/npx cowsay hello")
-    end
+    output = shell_output("#{bin}/node -e 'console.log(new Intl.NumberFormat(\"de-DE\").format(1234.56))'").strip
+    assert_equal "1.234,56", output
+
+    # make sure npm can find node
+    ENV.prepend_path "PATH", opt_bin
+    ENV.delete "NVM_NODEJS_ORG_MIRROR"
+    assert_equal which("node"), opt_bin/"node"
+    assert_predicate bin/"npm", :exist?, "npm must exist"
+    assert_predicate bin/"npm", :executable?, "npm must be executable"
+    npm_args = ["-ddd", "--cache=#{HOMEBREW_CACHE}/npm_cache", "--build-from-source"]
+    system "#{bin}/npm", *npm_args, "install", "npm@latest"
+    system "#{bin}/npm", *npm_args, "install", "bignum"
+    assert_predicate bin/"npx", :exist?, "npx must exist"
+    assert_predicate bin/"npx", :executable?, "npx must be executable"
+    assert_match "< hello >", shell_output("#{bin}/npx cowsay hello")
   end
 end
