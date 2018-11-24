6a53970ecbba04a6620a41772276e1d38115b31a
Mike McQuaid <mike@mikemcquaid.com>
2018-11-24 12:44:33 +0000

    node@6: default full-icu, remove other options.

--- Formula/node@6.rb
+++ Formula/node@6.rb
@@ -3,6 +3,7 @@ class NodeAT6 < Formula
   homepage "https://nodejs.org/"
   url "https://nodejs.org/dist/v6.14.4/node-v6.14.4.tar.xz"
   sha256 "9a4bfc99787f8bdb07d5ae8b1f00ec3757e7b09c99d11f0e8a5e9a16a134ec0f"
+  revision 1
 
   bottle do
     sha256 "ecc570659de3cf194eb2ec670adb6046c109d1af4e34fe01b2d456da321af900" => :mojave
@@ -13,13 +14,8 @@ class NodeAT6 < Formula
 
   keg_only :versioned_formula
 
-  option "with-openssl", "Build against Homebrew's OpenSSL instead of the bundled OpenSSL"
-  option "without-npm", "npm will not be installed"
-  option "with-full-icu", "Build with full-icu (all locales) instead of small-icu (English only)"
-
   depends_on "pkg-config" => :build
   depends_on "python@2" => :build
-  depends_on "openssl" => :optional
 
   # Per upstream - "Need g++ 4.8 or clang++ 3.4".
   fails_with :clang if MacOS.version <= :snow_leopard
@@ -36,46 +32,13 @@ class NodeAT6 < Formula
   end
 
   def install
-    args = ["--prefix=#{prefix}"]
-    args << "--without-npm" if build.without? "npm"
-    args << "--shared-openssl" << "--openssl-use-def-ca-store" if build.with? "openssl"
-
-    if build.with? "full-icu"
-      resource("icu4c").stage buildpath/"deps/icu"
-      args << "--with-intl=full-icu"
-    end
-
-    system "./configure", *args
+    resource("icu4c").stage buildpath/"deps/icu"
+    system "./configure", "--prefix=#{prefix}", "--with-intl=full-icu"
     system "make", "install"
   end
 
   def post_install
-    return if build.without? "npm"
     (lib/"node_modules/npm/npmrc").atomic_write("prefix = #{HOMEBREW_PREFIX}\n")
-  end
-
-  def caveats
-    s = ""
-
-    if build.without? "npm"
-      s += <<~EOS
-        Homebrew has NOT installed npm. If you later install it, you should supplement
-        your NODE_PATH with the npm module folder:
-          #{HOMEBREW_PREFIX}/lib/node_modules
-      EOS
-    end
-
-    if build.without? "full-icu"
-      s += <<~EOS
-        Please note by default only English locale support is provided. If you need
-        full locale support you should either rebuild with full icu:
-          `brew reinstall node --with-full-icu`
-        or add full icu data at runtime following:
-          https://github.com/nodejs/node/wiki/Intl#using-and-customizing-the-small-icu-build
-      EOS
-    end
-
-    s
   end
 
   test do
@@ -86,21 +49,18 @@ class NodeAT6 < Formula
     assert_equal "hello", output
     output = shell_output("#{bin}/node -e 'console.log(new Intl.NumberFormat(\"en-EN\").format(1234.56))'").strip
     assert_equal "1,234.56", output
-    if build.with? "full-icu"
-      output = shell_output("#{bin}/node -e 'console.log(new Intl.NumberFormat(\"de-DE\").format(1234.56))'").strip
-      assert_equal "1.234,56", output
-    end
 
-    if build.with? "npm"
-      # make sure npm can find node
-      ENV.prepend_path "PATH", opt_bin
-      ENV.delete "NVM_NODEJS_ORG_MIRROR"
-      assert_equal which("node"), opt_bin/"node"
-      assert_predicate bin/"npm", :exist?, "npm must exist"
-      assert_predicate bin/"npm", :executable?, "npm must be executable"
-      npm_args = ["-ddd", "--cache=#{HOMEBREW_CACHE}/npm_cache", "--build-from-source"]
-      system "#{bin}/npm", *npm_args, "install", "npm@latest"
-      system "#{bin}/npm", *npm_args, "install", "bignum"
-    end
+    output = shell_output("#{bin}/node -e 'console.log(new Intl.NumberFormat(\"de-DE\").format(1234.56))'").strip
+    assert_equal "1.234,56", output
+
+    # make sure npm can find node
+    ENV.prepend_path "PATH", opt_bin
+    ENV.delete "NVM_NODEJS_ORG_MIRROR"
+    assert_equal which("node"), opt_bin/"node"
+    assert_predicate bin/"npm", :exist?, "npm must exist"
+    assert_predicate bin/"npm", :executable?, "npm must be executable"
+    npm_args = ["-ddd", "--cache=#{HOMEBREW_CACHE}/npm_cache", "--build-from-source"]
+    system "#{bin}/npm", *npm_args, "install", "npm@latest"
+    system "#{bin}/npm", *npm_args, "install", "bignum"
   end
 end
