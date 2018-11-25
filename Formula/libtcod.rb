68ac9e82e5a4df5f9a3e063f7dea8b40c9ffa5fa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-25 16:58:54 +0000

    libtcod: update 1.8.2 bottle.

--- Formula/libtcod.rb
+++ Formula/libtcod.rb
@@ -6,10 +6,9 @@ class Libtcod < Formula
 
   bottle do
     cellar :any
-    sha256 "d32d2e923b6dec98fc17d75b4abc59080ad831e7b17c9e4e973bd22652205ac6" => :mojave
-    sha256 "c9e64cf1266592a440401c31028f97daf076d8bb9ace113a19da1b9c6a01b4bc" => :high_sierra
-    sha256 "360839e84034f149f3538be489273274a7db2f3b9020162dc94b9798e31dc402" => :sierra
-    sha256 "8728af3c1c018e2586708b66fdf8578260a83c875602ff6e796f96f44ea0382e" => :el_capitan
+    sha256 "6376b266e1523c732e6465d50b988c8d7964e567f8ba27eeedb585217c1f1d87" => :mojave
+    sha256 "40ec101abf6440dfcea154c4c57af89a0ec813cf99f810156e6bb53e27d22d14" => :high_sierra
+    sha256 "ee03cac48e6b29dcf7c84860436ee61105fad31888d81791f4f519773e0d73b9" => :sierra
   end
 
   depends_on "autoconf" => :build
