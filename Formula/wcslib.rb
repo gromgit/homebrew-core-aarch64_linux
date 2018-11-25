67f60e18d015e050f4bce7bd0f23f3f26742838f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-25 10:21:23 +0000

    wcslib: update 6.2 bottle.

--- Formula/wcslib.rb
+++ Formula/wcslib.rb
@@ -6,10 +6,9 @@ class Wcslib < Formula
 
   bottle do
     cellar :any
-    sha256 "bb97c202d67ecf98cf74431e6eef36a7e4272e7549765ffa34139daff5173b07" => :mojave
-    sha256 "3f65a03fa9f939560f4bc2776d1c4349a641b8e5386fae073a551413049bfbde" => :high_sierra
-    sha256 "a4a31772504b45bd2bd7b4a955bc2624c6c1849ba56cf9382d4f65765b7627c8" => :sierra
-    sha256 "b465fcf1d06195230abb08abf6dfe38aec48707a5f78bb9bc006f83cd866c006" => :el_capitan
+    sha256 "791f633e087db6056f23904df39a7b0489a7c18c2fea5e3e25c71bf7365518e1" => :mojave
+    sha256 "313a28f2170b0622ea7f72fe4371894be0ffb79663541bfeb5ed51c6a02571df" => :high_sierra
+    sha256 "98ce5c443e70f687b3754f7ab01fca8248ab760d76145da546a46132e67bd7de" => :sierra
   end
 
   depends_on "cfitsio"
