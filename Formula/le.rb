e81d75fec16a7a48b830a0c81b9429010fb66d64
Viktor Szakats <commit@vszakats.net>
2018-11-26 12:28:49 +0000

    le: secure url(s)

--- Formula/le.rb
+++ Formula/le.rb
@@ -1,7 +1,7 @@
 class Le < Formula
   desc "Text editor with block and binary operations"
   homepage "https://github.com/lavv17/le"
-  url "http://lav.yar.ru/download/le/le-1.16.5.tar.xz"
+  url "https://github.com/lavv17/le/releases/download/v1.16.5/le-1.16.5.tar.xz"
   sha256 "258d586f83e8abd55144dc3f09d9ddaf6ef55e8a90543fdb0932fb77d089dd78"
 
   bottle do
