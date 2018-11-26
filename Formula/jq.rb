a3552e4dc38889b61308af3cf8a91cbb18690722
Alexander Artemenko <svetlyak.40wt@gmail.com>
2018-11-26 10:36:05 +0300

    jq: fix checksum
    Closes #34464.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/jq.rb
+++ Formula/jq.rb
@@ -2,7 +2,7 @@ class Jq < Formula
   desc "Lightweight and flexible command-line JSON processor"
   homepage "https://stedolan.github.io/jq/"
   url "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-1.6.tar.gz"
-  sha256 "9625784cf2e4fd9842f1d407681ce4878b5b0dcddbcd31c6135114a30c71e6a8"
+  sha256 "5de8c8e29aaa3fb9cc6b47bb27299f271354ebb72514e3accadc7d38b5bbaa72"
 
   bottle do
     cellar :any
