71f299b7cd79993eccbd7b75b2d5024823a1af09
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-26 07:04:02 +0000

    davix: update 0.7.1 bottle.

--- Formula/davix.rb
+++ Formula/davix.rb
@@ -9,10 +9,9 @@ class Davix < Formula
 
   bottle do
     cellar :any
-    sha256 "7266206226db685a5bab507dee4d2e69e213da29d3035d0f5dfe1d9159f4e5ce" => :mojave
-    sha256 "81684262b74eb8206b2775e5387cb693ff3b8dc64cb08a7f556a105a27d9eb52" => :high_sierra
-    sha256 "3870bc1e60426091f1a0631fdf36536a66c2b3772a6fc69cd4f91630b79693e7" => :sierra
-    sha256 "f59c81532a3c82c01fc6b5743bd505eeee06f7c4369c7cf5f82e112aec66870e" => :el_capitan
+    sha256 "86fff80d2ff8aae77220e2bc996151a34c0f4abd0f70abe330572564a71e0a39" => :mojave
+    sha256 "f8c3f240c37c06dd16ab41b9fd458a2884296981ce898c13a5911ab628f3a41c" => :high_sierra
+    sha256 "cf1a6eaba6c3208b20cc44174ddaa716f39cd9fe5394ade09681749c987c6198" => :sierra
   end
 
   depends_on "cmake" => :build
