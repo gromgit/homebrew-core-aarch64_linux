0045cdc267062c6d207eaafb35109574bb039bbc
Igor Kapkov <igasgeek@me.com>
2018-11-27 10:56:27 +1100

    cpanminus 1.9018 (#34137)

--- Formula/cpanminus.rb
+++ Formula/cpanminus.rb
@@ -1,14 +1,16 @@
 class Cpanminus < Formula
   desc "Get, unpack, build, and install modules from CPAN"
   homepage "https://github.com/miyagawa/cpanminus"
-  url "https://github.com/miyagawa/cpanminus/archive/1.7044.tar.gz"
-  sha256 "a5407a85c2f3dda74dfc2241c68dafb9951f2a6eeada0a0eea9e7238a482c195"
+  url "https://github.com/miyagawa/cpanminus/archive/1.9019.tar.gz"
+  sha256 "d0a37547a3c4b6dbd3806e194cd6cf4632158ebed44d740ac023e0739538fb46"
   head "https://github.com/miyagawa/cpanminus.git"
 
   bottle :unneeded
 
   def install
-    bin.install "cpanm"
+    cd "App-cpanminus" do
+      bin.install "cpanm"
+    end
   end
 
   test do
