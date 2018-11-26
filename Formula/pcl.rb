42d21bc2231d20848f909ca963bccf35c8a94a8b
Igor Kapkov <igasgeek@me.com>
2018-11-27 09:15:24 +1100

    pcl 1.9.1

--- Formula/pcl.rb
+++ Formula/pcl.rb
@@ -1,21 +1,9 @@
 class Pcl < Formula
   desc "Library for 2D/3D image and point cloud processing"
   homepage "http://www.pointclouds.org/"
-  revision 6
+  url "https://github.com/PointCloudLibrary/pcl/archive/pcl-1.9.1.tar.gz"
+  sha256 "0add34d53cd27f8c468a59b8e931a636ad3174b60581c0387abb98a9fc9cddb6"
   head "https://github.com/PointCloudLibrary/pcl.git"
-
-  stable do
-    url "https://github.com/PointCloudLibrary/pcl/archive/pcl-1.8.1.tar.gz"
-    sha256 "5a102a2fbe2ba77c775bf92c4a5d2e3d8170be53a68c3a76cfc72434ff7b9783"
-
-    # VTK 8.1 compat
-    # Upstream commit from 10 Nov 2017 "VTK function change since version 7.1 (#2063)"
-    # See https://github.com/PointCloudLibrary/pcl/pull/2063
-    patch do
-      url "https://github.com/PointCloudLibrary/pcl/commit/6555b9a91f.patch?full_index=1"
-      sha256 "860a7b8e7964725b2d11a4237d6a6b65a42f049bc855ecbfcdc406b8e505b478"
-    end
-  end
 
   bottle do
     sha256 "7f1f32c0e548d03c7c421cdc3dc015d1e016f418171bfec6097bf7da6704c6f4" => :mojave
@@ -34,19 +22,7 @@ class Pcl < Formula
   depends_on "qhull"
   depends_on "vtk"
 
-  # Upstream fix for boost 1.68, remove in next version
-  patch do
-    url "https://github.com/PointCloudLibrary/pcl/commit/491b7c7e.diff?full_index=1"
-    sha256 "3665d0a6c110faf1976b8a05a2c7081c8878417b6c03370005c844efa6f52af6"
-  end
-
   def install
-    # Fix "error: no matching constructor for initialization of
-    # 'boost::uuids::random_generator' (aka 'boost::uuids::random_generator_pure')"
-    # Upstream issue 18 Apr 2018 "Fails to build against Boost 1.67"
-    # See https://github.com/PointCloudLibrary/pcl/issues/2284
-    ENV.append "CXXFLAGS", "-DBOOST_UUID_RANDOM_GENERATOR_COMPAT"
-
     args = std_cmake_args + %w[
       -DBUILD_SHARED_LIBS:BOOL=ON
       -DBUILD_apps=AUTO_OFF
