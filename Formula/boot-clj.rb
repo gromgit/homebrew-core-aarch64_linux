057d6bfed8c34d9d9aab0afd8a4da9903d704ae6
Viktor Szakats <commit@vszakats.net>
2018-11-26 12:28:49 +0000

    boot-clj: secure url(s)

--- Formula/boot-clj.rb
+++ Formula/boot-clj.rb
@@ -1,6 +1,6 @@
 class BootClj < Formula
   desc "Build tooling for Clojure"
-  homepage "http://boot-clj.com"
+  homepage "https://boot-clj.com/"
   url "https://github.com/boot-clj/boot-bin/releases/download/2.7.2/boot.sh"
   sha256 "0ccd697f2027e7e1cd3be3d62721057cbc841585740d0aaa9fbb485d7b1f17c3"
 
