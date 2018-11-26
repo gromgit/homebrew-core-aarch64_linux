e0b95a0e316a49a8345d51b46ec2b36d816940f1
ober <jaimef@linbsd.org>
2018-11-26 15:44:33 -0800

    gambit-scheme 4.9.1 (new formula)
    Closes #34518.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/gambit-scheme.rb
@@ -0,0 +1,28 @@
+class GambitScheme < Formula
+  desc "Gambit Scheme is an implementation of the Scheme Language"
+  homepage "http://gambitscheme.org"
+  url "https://github.com/gambit/gambit/archive/v4.9.1.tar.gz"
+  sha256 "667ae2ee657c22621a60b3eda6e242224d41853adb841e6ff9bc779f19921c18"
+
+  depends_on "openssl"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --enable-single-host
+      --enable-multiple-versions
+      --enable-default-runtime-options=f8,-8,t8
+      --enable-poll
+      --enable-openssl
+    ]
+
+    system "./configure", *args
+    system "make"
+    ENV.deparallelize
+    system "make", "install"
+  end
+
+  test do
+    assert_equal "0123456789", shell_output("#{prefix}/v4.9.1/bin/gsi -e \"(for-each write '(0 1 2 3 4 5 6 7 8 9))\"")
+  end
+end
