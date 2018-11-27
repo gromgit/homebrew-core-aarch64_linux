74e0b18324567266008dda6dc0f95d47279d8c45
Felix Lapalme <felix@lapal.me>
2018-11-27 00:24:56 -0500

    bluetoothconnector 1.1.1 (new formula)
    Closes #34530.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/bluetoothconnector.rb
@@ -0,0 +1,22 @@
+class Bluetoothconnector < Formula
+  desc "Connect and disconnect Bluetooth devices"
+  homepage "https://github.com/lapfelix/BluetoothConnector"
+  url "https://github.com/lapfelix/BluetoothConnector/archive/1.1.1.tar.gz"
+  sha256 "8ea61b26ebab18b6ee8b8960d016d41f514d26ef9a7b4452c467017ab1859056"
+
+  depends_on :xcode => ["10.0", :build]
+
+  def install
+    system "swift", "build", "--disable-sandbox", "-c", "release", "-Xswiftc", "-static-stdlib"
+    bin.install ".build/release/BluetoothConnector"
+  end
+
+  test do
+    output = shell_output("#{bin}/BluetoothConnector")
+    assert_match "Usage: BluetoothConnector", output
+    assert_match "Get the MAC address from the list below", output
+
+    output_fail = shell_output("#{bin}/BluetoothConnector --connect 00-00-00-00-00-00", 252)
+    assert_equal "Not paired to device\n", output_fail
+  end
+end
