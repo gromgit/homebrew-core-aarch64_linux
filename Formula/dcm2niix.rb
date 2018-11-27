8c3454f8ddd5f4b6fd738a139398ff58c5709036
Igor Kapkov <igasgeek@me.com>
2018-11-27 15:18:31 +1100

    dcm2niix 1.0.20181125
    Closes #34498.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/dcm2niix.rb
+++ Formula/dcm2niix.rb
@@ -1,8 +1,8 @@
 class Dcm2niix < Formula
   desc "DICOM to NIfTI converter"
   homepage "https://www.nitrc.org/plugins/mwiki/index.php/dcm2nii:MainPage"
-  url "https://github.com/rordenlab/dcm2niix/archive/v1.0.20180622.tar.gz"
-  sha256 "e9f79509f44aac82c9663381f8f4bfb18a9a3c3eb112d418c92629a871bbb13c"
+  url "https://github.com/rordenlab/dcm2niix/archive/v1.0.20181125.tar.gz"
+  sha256 "7b17a1333423b383deb2235685390fb0f30495262eb7d0aed293c32cf669936c"
   head "https://github.com/rordenlab/dcm2niix.git"
 
   bottle do
