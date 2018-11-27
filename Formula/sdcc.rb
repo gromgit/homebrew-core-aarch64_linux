54d87a9abd125fd8d27cab8c8d79f10768fff38c
HoangDuong <duonghuuhoang@gmail.com>
2018-11-27 21:20:38 +0700

    sdcc 3.8.0
    Closes #34513.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/sdcc.rb
+++ Formula/sdcc.rb
@@ -1,8 +1,8 @@
 class Sdcc < Formula
   desc "ANSI C compiler for Intel 8051, Maxim 80DS390, and Zilog Z80"
   homepage "https://sdcc.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/sdcc/sdcc/3.7.0/sdcc-src-3.7.0.tar.bz2"
-  sha256 "854d47094698b06142df3d5fc646bb540d497ab4073ad2f051b8ec2141df948e"
+  url "https://downloads.sourceforge.net/project/sdcc/sdcc/3.8.0/sdcc-src-3.8.0.tar.bz2"
+  sha256 "b331668deb7bd832efd112052e5b0ed2313db641a922bd39280ba6d47adbbb21"
   head "http://svn.code.sf.net/p/sdcc/code/trunk/sdcc"
 
   bottle do
