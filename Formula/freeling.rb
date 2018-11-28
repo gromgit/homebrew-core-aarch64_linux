9b97ddfa90f57cd8a1917494bad524fc8c1e5240
FX Coudert <fxcoudert@gmail.com>
2018-11-28 10:04:08 +0100

    freeling: bump revision for icu4c

--- Formula/freeling.rb
+++ Formula/freeling.rb
@@ -3,7 +3,7 @@ class Freeling < Formula
   homepage "http://nlp.lsi.upc.edu/freeling/"
   url "https://github.com/TALP-UPC/FreeLing/releases/download/4.1/FreeLing-4.1.tar.gz"
   sha256 "ccb3322db6851075c9419bb5e472aa6b2e32cc7e9fa01981cff49ea3b212247e"
-  revision 1
+  revision 2
 
   bottle do
     rebuild 1
