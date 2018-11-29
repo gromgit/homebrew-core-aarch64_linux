10a7080610c7dbf858d49ef3c4aaa40aa22e2d67
Clement Escoffier <clement.escoffier@gmail.com>
2018-11-29 10:51:12 +0000

    vert.x 3.6.0
    Closes #34591.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/vert.x.rb
+++ Formula/vert.x.rb
@@ -1,8 +1,8 @@
 class VertX < Formula
   desc "Toolkit for building reactive applications on the JVM"
   homepage "https://vertx.io/"
-  url "https://dl.bintray.com/vertx/downloads/vert.x-3.5.4-full.tar.gz"
-  sha256 "9a23895f45c5951c53368d3b5b511d760183617aff76e1693d359a5e928220a2"
+  url "https://dl.bintray.com/vertx/downloads/vert.x-3.6.0-full.tar.gz"
+  sha256 "df2737aa6d5e5a921abec487263fa82354c22468677c61baafbfb05bcdbdcd79"
 
   bottle :unneeded
   depends_on :java => "1.8+"
