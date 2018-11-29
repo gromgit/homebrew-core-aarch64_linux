cd6406b06333b0039a809ce75de329b8a057e5f0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-29 06:17:07 +0000

    libmspub: update 0.1.4_3 bottle.

--- Formula/libmspub.rb
+++ Formula/libmspub.rb
@@ -7,10 +7,9 @@ class Libmspub < Formula
 
   bottle do
     cellar :any
-    sha256 "8db32d56cbf7fc562024580618868a70d9d4e90301de35ffe74db4d0ba12491f" => :mojave
-    sha256 "3a5055e8ff1f55de6c79d4793767aa3c881a4ee25f662850801db1db56e289aa" => :high_sierra
-    sha256 "2b2f16add39ca5bdbb26a2698513757eddc711ed2330e197258a8d94fccc04f3" => :sierra
-    sha256 "77d32faa735ab867c48b947a83c9cf12ba3bbe747e0c94c43615d721a94b8d2d" => :el_capitan
+    sha256 "88fcaf08da9f11bedb81f2e5f7ce489325109e4481729cda57f168f2b7eaae75" => :mojave
+    sha256 "6d4a84da0d0cfc06f07e61043ff3f36310f06a3f2eff259c619b1c188f5113bd" => :high_sierra
+    sha256 "857c89b5a164acf971221125715acd419bccd092ca8ab2683f68b4174c6dd859" => :sierra
   end
 
   depends_on "boost" => :build
