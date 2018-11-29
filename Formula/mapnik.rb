42b71ae24f17f5b97f851ea3eaaab21681a82c40
FX Coudert <fxcoudert@gmail.com>
2018-11-29 12:50:52 +0100

    mapnik 3.0.21
    Closes #34593.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/mapnik.rb
+++ Formula/mapnik.rb
@@ -1,9 +1,8 @@
 class Mapnik < Formula
   desc "Toolkit for developing mapping applications"
   homepage "https://mapnik.org/"
-  url "https://github.com/mapnik/mapnik/releases/download/v3.0.20/mapnik-v3.0.20.tar.bz2"
-  sha256 "77b9de029d59fbb7eebb7e5884dff03074eb4eeaa238e3f4c8ff5a61e01a9f04"
-  revision 1
+  url "https://github.com/mapnik/mapnik/releases/download/v3.0.21/mapnik-v3.0.21.tar.bz2"
+  sha256 "6db7918e8fd24346dfc81745e455c383a718335ffa64015cf625fed5ed92b524"
   head "https://github.com/mapnik/mapnik.git"
 
   bottle do
@@ -15,17 +14,23 @@ class Mapnik < Formula
 
   depends_on "pkg-config" => :build
   depends_on "boost"
+  depends_on "cairo"
   depends_on "freetype"
+  depends_on "gdal"
   depends_on "harfbuzz"
   depends_on "icu4c"
   depends_on "jpeg"
   depends_on "libpng"
   depends_on "libtiff"
+  depends_on "postgresql"
   depends_on "proj"
   depends_on "webp"
-  depends_on "cairo" => :optional
-  depends_on "gdal" => :optional
-  depends_on "postgresql" => :optional
+
+  # Upstream commit to fix build with boost >= 1.68
+  patch do
+    url "https://github.com/mapnik/mapnik/commit/c067eb7eec32fdd6d1c3d0e90b13a889459f2756.diff?full_index=1"
+    sha256 "e00e8475f04e9010dbb1724e5ae10403d2e7f1da8a83e67dfb54a7a969d81669"
+  end
 
   needs :cxx11
 
@@ -36,49 +41,44 @@ class Mapnik < Formula
     # encountered when trying to detect boost regex in configure
     ENV.delete("SDKROOT") if DevelopmentTools.clang_build_version >= 900
 
-    icu = Formula["icu4c"].opt_prefix
     boost = Formula["boost"].opt_prefix
-    proj = Formula["proj"].opt_prefix
+    freetype = Formula["freetype"].opt_prefix
+    harfbuzz = Formula["harfbuzz"].opt_prefix
+    icu = Formula["icu4c"].opt_prefix
     jpeg = Formula["jpeg"].opt_prefix
     libpng = Formula["libpng"].opt_prefix
     libtiff = Formula["libtiff"].opt_prefix
-    freetype = Formula["freetype"].opt_prefix
-    harfbuzz = Formula["harfbuzz"].opt_prefix
+    proj = Formula["proj"].opt_prefix
     webp = Formula["webp"].opt_prefix
 
-    args = ["CC=\"#{ENV.cc}\"",
-            "CXX=\"#{ENV.cxx}\"",
-            "PREFIX=#{prefix}",
-            "CUSTOM_CXXFLAGS=\"-DBOOST_EXCEPTION_DISABLE\"",
-            "CUSTOM_DEFINES=-DU_USING_ICU_NAMESPACE=1", # icu4c 61.1 compatability
-            "ICU_INCLUDES=#{icu}/include",
-            "ICU_LIBS=#{icu}/lib",
-            "JPEG_INCLUDES=#{jpeg}/include",
-            "JPEG_LIBS=#{jpeg}/lib",
-            "PNG_INCLUDES=#{libpng}/include",
-            "PNG_LIBS=#{libpng}/lib",
-            "HB_INCLUDES=#{harfbuzz}/include",
-            "HB_LIBS=#{harfbuzz}/lib",
-            "WEBP_INCLUDES=#{webp}/include",
-            "WEBP_LIBS=#{webp}/lib",
-            "TIFF_INCLUDES=#{libtiff}/include",
-            "TIFF_LIBS=#{libtiff}/lib",
-            "BOOST_INCLUDES=#{boost}/include",
-            "BOOST_LIBS=#{boost}/lib",
-            "PROJ_INCLUDES=#{proj}/include",
-            "PROJ_LIBS=#{proj}/lib",
-            "FREETYPE_CONFIG=#{freetype}/bin/freetype-config",
-            "NIK2IMG=False",
-            "CPP_TESTS=False", # too long to compile to be worth it
-            "INPUT_PLUGINS=all"]
-
-    if build.with? "cairo"
-      args << "CAIRO=True" # cairo paths will come from pkg-config
-    else
-      args << "CAIRO=False"
-    end
-    args << "GDAL_CONFIG=#{Formula["gdal"].opt_bin}/gdal-config" if build.with? "gdal"
-    args << "PG_CONFIG=#{Formula["postgresql"].opt_bin}/pg_config" if build.with? "postgresql"
+    args = %W[
+      CC=#{ENV.cc}
+      CXX=#{ENV.cxx}
+      PREFIX=#{prefix}
+      BOOST_INCLUDES=#{boost}/include
+      BOOST_LIBS=#{boost}/lib
+      CAIRO=True
+      CPP_TESTS=False
+      FREETYPE_CONFIG=#{freetype}/bin/freetype-config
+      GDAL_CONFIG=#{Formula["gdal"].opt_bin}/gdal-config
+      HB_INCLUDES=#{harfbuzz}/include
+      HB_LIBS=#{harfbuzz}/lib
+      ICU_INCLUDES=#{icu}/include
+      ICU_LIBS=#{icu}/lib
+      INPUT_PLUGINS=all
+      JPEG_INCLUDES=#{jpeg}/include
+      JPEG_LIBS=#{jpeg}/lib
+      NIK2IMG=False
+      PG_CONFIG=#{Formula["postgresql"].opt_bin}/pg_config
+      PNG_INCLUDES=#{libpng}/include
+      PNG_LIBS=#{libpng}/lib
+      PROJ_INCLUDES=#{proj}/include
+      PROJ_LIBS=#{proj}/lib
+      TIFF_INCLUDES=#{libtiff}/include
+      TIFF_LIBS=#{libtiff}/lib
+      WEBP_INCLUDES=#{webp}/include
+      WEBP_LIBS=#{webp}/lib
+    ]
 
     system "./configure", *args
     system "make"
@@ -86,6 +86,7 @@ class Mapnik < Formula
   end
 
   test do
-    assert_equal prefix.to_s, shell_output("#{bin}/mapnik-config --prefix").chomp
+    output = shell_output("#{bin}/mapnik-config --prefix").chomp
+    assert_equal prefix.to_s, output
   end
 end
