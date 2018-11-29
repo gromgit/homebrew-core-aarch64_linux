4d178393bdd465c1139ab08e73d38b651ea7e840
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-29 10:14:49 +0000

    opa: add 0.10.1 bottle.

--- Formula/opa.rb
+++ Formula/opa.rb
@@ -4,6 +4,13 @@ class Opa < Formula
   url "https://github.com/open-policy-agent/opa/archive/v0.10.1.tar.gz"
   sha256 "6b6121cbf7efc42b6ebc3f5ba6a2533e4e6eaf4b9eec446fb6f8fce4e6c02ae7"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d8f3c750a3e0a598cb98b4e243e9fbbab0af152dfe2c161aeda695dd50482bae" => :mojave
+    sha256 "984600d03971491fa38c132713665a2829149693e75daf53dc60b80e20042005" => :high_sierra
+    sha256 "f3f31c3a2ed1edabf26cb1aa862c9338f99343c7c0f1e412e40ce2b4d4773d2f" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
