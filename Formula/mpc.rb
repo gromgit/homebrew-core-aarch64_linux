9a226df2e717fdf2a0fc6134ba89d6f1456e61a1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-29 05:19:28 +0000

    mpc: update 0.31 bottle.

--- Formula/mpc.rb
+++ Formula/mpc.rb
@@ -5,10 +5,9 @@ class Mpc < Formula
   sha256 "62373e83a8a165b2ed43967975efecd3feee530f4557d6b861dd08aa89d52b2d"
 
   bottle do
-    rebuild 1
-    sha256 "f8aacc306782434f8f8a681701a1180cec977c687f599f9c575ec2bd5b68c581" => :mojave
-    sha256 "59b14683a0acd8f11bb1e7b56e9f5e5c604b055a111c0f8e6ad48089248ef6f2" => :high_sierra
-    sha256 "e606214d00ed9c69c1877efa543d8e5120fef6f1df238de48776aacb59af00de" => :sierra
+    sha256 "a0978aee21d1d4326cac85904ad4dc381ffd0fae96c66b98912ecd5260b548a4" => :mojave
+    sha256 "70bf0925e52814689cc58579288dafbefcd512caf3e4bcaa48f78d6c84fd7f36" => :high_sierra
+    sha256 "34dc577f9ac9551204d7c36448468da5a69c152b2de21622fc5e649c051a98de" => :sierra
   end
 
   depends_on "meson" => :build
