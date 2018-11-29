90091f0b93a029e3ea8397f3e9d027102717d222
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-29 06:17:08 +0000

    zorba: update 3.1_9 bottle.

--- Formula/zorba.rb
+++ Formula/zorba.rb
@@ -6,10 +6,9 @@ class Zorba < Formula
   revision 9
 
   bottle do
-    sha256 "a24259b380ad74230a29a68a0166f9c88832a69d1c62e941a2864102df88a848" => :mojave
-    sha256 "1ee6bcb9160599deba3286a3a32e6682c6ab9f49dd29bf3f2b7379799aa4461e" => :high_sierra
-    sha256 "0611b46146ef4e6da55d5c8a2e37c9df60eab408abf0c71b86a702e4825120a5" => :sierra
-    sha256 "9dc07aa5daadc49cbbdeee1fee3ef14800a604069e996859ec58b441c1738bdd" => :el_capitan
+    sha256 "cb24e884cb56af293bf9b10e7280aff091e72540d6d67d610674bcd0c5dfcdd9" => :mojave
+    sha256 "35316a8148a7c4b89be1b2acbfc891541cdd994d535764db5ad88b858b99f8b0" => :high_sierra
+    sha256 "167fd942d96909452d63be186c76b0f2c9b8d575b9afcb896b6d7ba15c4af798" => :sierra
   end
 
   depends_on "cmake" => :build
