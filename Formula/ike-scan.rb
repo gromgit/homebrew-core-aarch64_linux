1ddb5c5e07c0a6ee05271cee2fe27a5604faf934
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-30 21:07:25 +0000

    ike-scan: update 1.9.4 bottle.

--- Formula/ike-scan.rb
+++ Formula/ike-scan.rb
@@ -7,13 +7,9 @@ class IkeScan < Formula
   head "https://github.com/royhills/ike-scan.git"
 
   bottle do
-    sha256 "7e6dde3a63dd30648a77deb4caf5ef6c85d102604a94f0a1db6f7f00e2d7c171" => :mojave
-    sha256 "a6497e65e3fd817d9f55e8a92999ea94e80ae8a68e7c7d1aaf00264482f617f2" => :high_sierra
-    sha256 "36d4250aba9099fefd630e3a585f4dc035ccb43d53d3270fc41bb08e92242a49" => :sierra
-    sha256 "65df0ddc049fadf6c9deb605442092026ec1c4ef5a6358a63ed96928085ac155" => :el_capitan
-    sha256 "70276baa029ee6c1a451175e3254cc74754fd5de83fe748d17ba4fcf0586349a" => :yosemite
-    sha256 "a0967f6ce6e9903068149c9b379926a25b315c3bdb9aca451f28b0d9cc744abd" => :mavericks
-    sha256 "2281b3d5f1f590e69e435a39c1032872db80439411ea74410a58243096645668" => :mountain_lion
+    sha256 "d6deea8b99eb570833d63feb09776a3fc1584ce83f78808fe0730aec8cacc467" => :mojave
+    sha256 "67c79247795879dc39dee4f7264f9665e5fa5fb4b5e75a9f153d1dffa1cc88ce" => :high_sierra
+    sha256 "49efb4387beb1f0edcba655b845f74b802e4b2f4fb3b57028b59fa3b44717e74" => :sierra
   end
 
   depends_on "autoconf" => :build
