0a66ce31b1d17636248d12df2ac37142f869f9d2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-30 09:54:54 +0000

    skaffold: update 0.19.0 bottle.

--- Formula/skaffold.rb
+++ Formula/skaffold.rb
@@ -8,9 +8,9 @@ class Skaffold < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "85874bf1826ae861181cb132dc3dfb0937820b184e977529df451be9313e4e1c" => :mojave
-    sha256 "291a04d5877b3f3055d9a1ee26d7df48a2431307f445b595ab6b0e398d98fe98" => :high_sierra
-    sha256 "6f743845fc2f0619ec338cba95bf3d314833d21b437542e53645885c0ae5378b" => :sierra
+    sha256 "f0d1e5c797c90b2ec95fda3b09ec081fec10a3559d13cc1c92fb99c4dcba5a7e" => :mojave
+    sha256 "b77027bef1d2e81a988e908d7f79792d0be2196d7b5de1884991a7d259838657" => :high_sierra
+    sha256 "efc931749e9535749c85eb6a5995a55e056e52e8b7f214ce5186abad861c6810" => :sierra
   end
 
   depends_on "go" => :build
