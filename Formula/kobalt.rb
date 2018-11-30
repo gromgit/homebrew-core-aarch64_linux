c1ab06a2b00cafd77f18cbcf3308aebb51455bad
Igor Kapkov <igasgeek@me.com>
2018-11-30 20:31:33 +1100

    kobalt 1.0.122 (#34615)

--- Formula/kobalt.rb
+++ Formula/kobalt.rb
@@ -1,8 +1,8 @@
 class Kobalt < Formula
   desc "Build system"
   homepage "https://beust.com/kobalt/"
-  url "https://github.com/cbeust/kobalt/releases/download/1.0.120/kobalt-1.0.120.zip"
-  sha256 "6036607feb29a97c5d30f216ecf8fd8ea5fac1ed50943b8a20002b6a4ca5df3b"
+  url "https://github.com/cbeust/kobalt/releases/download/1.0.122/kobalt-1.0.122.zip"
+  sha256 "0fb65c41f025b9c65c7191e77bb5d91bb2e8ae1869cc6fcdb9318a002a28add7"
 
   bottle :unneeded
 
