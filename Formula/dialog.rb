7d6ae2960b845259f39ee9fa8776b48c2abe16a0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-02 05:03:36 +0000

    dialog: update 1.3-20181107 bottle.

--- Formula/dialog.rb
+++ Formula/dialog.rb
@@ -6,10 +6,9 @@ class Dialog < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0e77b7badccfba997b5a8a31d1128b4288fdac369d16b68dd16dbfd5cb8ade7c" => :mojave
-    sha256 "4863f5204f24a5787638fdcb2c396bca5457f751f2dc822a91f8a43307051f67" => :high_sierra
-    sha256 "86825e138c39c2fa33aa3100974a8ab3d3421a8923ae483bb0fb9bd077768c4d" => :sierra
-    sha256 "4081b1e69bcc2521e86ac5bef7dba0b990a45017c0bc7bf50fc6f82d82fc190e" => :el_capitan
+    sha256 "c356e3a474ebf3d580ec4674d87aec9c88ba7137e0f6fefd8c73cbf99d327b76" => :mojave
+    sha256 "cf169dff8e776ac85d3a662db2abd9fbd578affc994dcb866547ee06630b68f6" => :high_sierra
+    sha256 "68b0c43919cde70242f56ba550b666c2a6f9e6baefca50615923efa262052d5b" => :sierra
   end
 
   def install
