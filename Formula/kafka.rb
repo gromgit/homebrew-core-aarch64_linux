800d080e17984a9f24503c673b6205ff1249b58e
Logan Gorence <loganjohngorence@gmail.com>
2018-12-02 16:03:18 -0800

    kafka 2.1.0
    Closes #34721.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/kafka.rb
+++ Formula/kafka.rb
@@ -1,8 +1,8 @@
 class Kafka < Formula
   desc "Publish-subscribe messaging rethought as a distributed commit log"
   homepage "https://kafka.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=/kafka/2.0.0/kafka_2.12-2.0.0.tgz"
-  sha256 "b5f1539c4030e6f6e64d0c14a9acea31156e7cbf2cb66c93ca2b6ca732ba7955"
+  url "https://www.apache.org/dyn/closer.cgi?path=/kafka/2.1.0/kafka_2.12-2.1.0.tgz"
+  sha256 "a70b44a682f5082f5c69182d4596e7ded2df38629f09b50a629070ddb1ef4f86"
 
   bottle do
     cellar :any_skip_relocation
