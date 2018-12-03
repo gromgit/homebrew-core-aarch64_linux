0dde46e558d13017f5e7758dc4cf08c944abbcd9
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-12-03 20:26:57 +0000

    libgda 5.2.8
    Closes #34751.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libgda.rb
+++ Formula/libgda.rb
@@ -1,8 +1,8 @@
 class Libgda < Formula
   desc "Provides unified data access to the GNOME project"
   homepage "http://www.gnome-db.org/"
-  url "https://download.gnome.org/sources/libgda/5.2/libgda-5.2.5.tar.xz"
-  sha256 "e3d2e4c28c08a22efd520767fa9d16e92cc1821f693261d7cb2892cc23ec90c8"
+  url "https://download.gnome.org/sources/libgda/5.2/libgda-5.2.8.tar.xz"
+  sha256 "e2876d987c00783ac3c1358e9da52794ac26f557e262194fcba60ac88bafa445"
 
   bottle do
     sha256 "6dc574ad6963f7779266af499e740d663fd70d26dd3e3212c57cdc556abc7060" => :mojave
@@ -17,12 +17,10 @@ class Libgda < Formula
   depends_on "gettext"
   depends_on "glib"
   depends_on "libgcrypt"
+  depends_on "libgee"
   depends_on "openssl"
   depends_on "readline"
   depends_on "sqlite"
-
-  # Bug reported at https://gitlab.gnome.org/GNOME/libgda/issues/142
-  patch :DATA
 
   def install
     system "./configure", "--disable-debug",
@@ -32,24 +30,8 @@ class Libgda < Formula
                           "--disable-binreloc",
                           "--disable-gtk-doc",
                           "--without-java",
-                          "--enable-introspection",
-                          "--enable-gi-system-install=no"
+                          "--enable-introspection"
     system "make"
     system "make", "install"
   end
 end
-
-__END__
-diff --git a/libgda/sqlite/virtual/gda-vprovider-data-model.c b/libgda/sqlite/virtual/gda-vprovider-data-model.c
-index d6674de..31c7993 100644
---- a/libgda/sqlite/virtual/gda-vprovider-data-model.c
-+++ b/libgda/sqlite/virtual/gda-vprovider-data-model.c
-@@ -280,7 +280,7 @@ virtual_filtered_data_free (VirtualFilteredData *data)
- static VirtualFilteredData *
- virtual_filtered_data_ref (VirtualFilteredData *data)
- {
--	g_return_if_fail (data != NULL);
-+	g_return_val_if_fail (data != NULL, NULL);
-	data->refcount ++;
-	return data;
- }
