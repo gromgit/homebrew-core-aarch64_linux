8315b576cad2bf28fabaf084e397fd1398bff89d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-03 13:07:12 +0000

    eprover: update 2.2 bottle.

--- Formula/eprover.rb
+++ Formula/eprover.rb
@@ -5,10 +5,10 @@ class Eprover < Formula
   sha256 "2c2c45a57e69daa571307a89746228194f0144a5884741f2d487823f1fbf3022"
 
   bottle do
-    sha256 "f093c240c7a2b59841706a8e14b257b1a4e21d2042c19bed5a5a3ccf55908608" => :mojave
-    sha256 "25b002f9ebea09a19061dc34ecdf9df013c8e2dc10a591cfa52a35a817702c79" => :high_sierra
-    sha256 "3bc21a8ded282e64ee8ed8062eec514133af36d2418e18f7821c4c678e288b54" => :sierra
-    sha256 "3c91a8bc24e98889448a78fdccc888d5f6489a9715e903b255026b11dd5ff1b7" => :el_capitan
+    cellar :any_skip_relocation
+    sha256 "0f45c36c2ba8a4c3793b4078b9948a735853d9e91e568d7a5ffcc2ec195998ca" => :mojave
+    sha256 "77cd34eeb515299ddd4f3db8e6fa70580c64b144d9e82718d4e3a5c5b4f7a25a" => :high_sierra
+    sha256 "b2ab8b9aaebb1755f8f77c5f1820f795a15c5daa77dd42b718e194404cec70f8" => :sierra
   end
 
   def install
