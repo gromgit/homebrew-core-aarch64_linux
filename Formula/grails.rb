ffdaaa54ff65965f384fd85c9cd5a23fca3f52b6
Igor Kapkov <igasgeek@me.com>
2018-12-05 08:34:06 +1100

    grails 3.3.9 (#34791)

--- Formula/grails.rb
+++ Formula/grails.rb
@@ -1,8 +1,8 @@
 class Grails < Formula
   desc "Web application framework for the Groovy language"
   homepage "https://grails.org"
-  url "https://github.com/grails/grails-core/releases/download/v3.3.8/grails-3.3.8.zip"
-  sha256 "c0de7ce97ea156a7fd43e190479814d61ecd91a0399e88fd353526b1a3a2d8c1"
+  url "https://github.com/grails/grails-core/releases/download/v3.3.9/grails-3.3.9.zip"
+  sha256 "d6c214642dadbb450db306a17fc6ff3a60fd9d1f515cc91b273f239a6a8adc76"
 
   bottle :unneeded
 
