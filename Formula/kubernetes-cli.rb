f887a82418bc9fe609c2e4e7c410b69347f43cb8
Igor Kapkov <igasgeek@me.com>
2018-12-04 18:44:02 +1100

    kubernetes-cli 1.13.0
    Closes #34775.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/kubernetes-cli.rb
+++ Formula/kubernetes-cli.rb
@@ -2,8 +2,8 @@ class KubernetesCli < Formula
   desc "Kubernetes command-line interface"
   homepage "https://kubernetes.io/"
   url "https://github.com/kubernetes/kubernetes.git",
-      :tag      => "v1.12.3",
-      :revision => "435f92c719f279a3a67808c80521ea17d5715c66"
+      :tag      => "v1.13.0",
+      :revision => "ddf47ac13c1a9483ea035a79cd7c10005ff21a6d"
   head "https://github.com/kubernetes/kubernetes.git"
 
   bottle do
