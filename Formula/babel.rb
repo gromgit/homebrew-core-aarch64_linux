7ee772907a8df5e7297bb28adcb63db2ce78317f
Chongyu Zhu <i@lembacon.com>
2018-12-05 22:58:53 +0800

    babel 7.2.0
    Closes #34825.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/babel.rb
+++ Formula/babel.rb
@@ -4,8 +4,8 @@ require "json"
 class Babel < Formula
   desc "Compiler for writing next generation JavaScript"
   homepage "https://babeljs.io/"
-  url "https://registry.npmjs.org/@babel/cli/-/cli-7.1.5.tgz"
-  sha256 "c64924446fdfa0b2df720bbb01f6c73de76987af0044c477965c3b418cfaef78"
+  url "https://registry.npmjs.org/@babel/cli/-/cli-7.2.0.tgz"
+  sha256 "151d36d3069dde1eb4aea9ae5501d969edaa99ee46b6cbb8fd591562833e8651"
 
   bottle do
     sha256 "e23a0ae41ec34b7f21229b6d1890e2d78cd3714ae3484804cde1c12679007ca7" => :mojave
@@ -16,8 +16,8 @@ class Babel < Formula
   depends_on "node"
 
   resource "babel-core" do
-    url "https://registry.npmjs.org/@babel/core/-/core-7.1.5.tgz"
-    sha256 "936682de3bdf0e602512e14756e6f275369bb45af4a64acbbd8316f2a56f667e"
+    url "https://registry.npmjs.org/@babel/core/-/core-7.2.0.tgz"
+    sha256 "7f60c1a95f03c86b0e9329989045f460c3fb86ce6d4f545327b6091f1ca37eae"
   end
 
   def install
