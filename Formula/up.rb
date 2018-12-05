694339277a0bc61a0d0368693a3818c08a0a8884
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-05 06:31:39 +0000

    up: update 0.3.2 bottle.

--- Formula/up.rb
+++ Formula/up.rb
@@ -6,9 +6,9 @@ class Up < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "05a4f4be50e78924d65b7711d41fe835f724db6543bb5400314edba8f3c9ca80" => :mojave
-    sha256 "fdaf32376df28ef7231d4894f9ce7ce2eb3e4da25007ce82d341c98cef4aa31d" => :high_sierra
-    sha256 "d2eb809466efe4f91b71a1bbdfd3c0e259865e231d3d4da0b40acec3e93d54ff" => :sierra
+    sha256 "0c453761279cdc6a995ae471841b2e8513215c3d30f4f448c3cf82f548376fa5" => :mojave
+    sha256 "f9ea40f11e458e2bda259fa428a9f390d9a9efce1d7983f9325eda17b4655501" => :high_sierra
+    sha256 "558f89d83bd23a28ef31a1d72f7749521f68ebf0d767a8cffb2c6b9311461e13" => :sierra
   end
 
   depends_on "go" => :build
