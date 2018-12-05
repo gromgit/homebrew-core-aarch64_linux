6a500598753eeb6ff86f58b569d452faa8ce9666
Viktor Szakats <commit@vszakats.net>
2018-12-05 13:07:24 +0000

    xmlformat: fix url(s)

--- Formula/xmlformat.rb
+++ Formula/xmlformat.rb
@@ -1,7 +1,7 @@
 class Xmlformat < Formula
   desc "Format XML documents"
-  homepage "http://www.kitebird.com/software/xmlformat/"
-  url "http://www.kitebird.com/software/xmlformat/xmlformat-1.04.tar.gz"
+  homepage "https://web.archive.org/web/20180624200351/www.kitebird.com/software/xmlformat/"
+  url "https://web.archive.org/web/20180624200351/www.kitebird.com/software/xmlformat/xmlformat-1.04.tar.gz"
   sha256 "71a70397e44760d67645007ad85fea99736f4b6f8679067a3b5f010589fd8fef"
 
   bottle :unneeded
