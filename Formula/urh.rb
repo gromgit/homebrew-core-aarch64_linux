9150880c3c5d4aeb6bc3d2cdc40cbceb0c165806
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-06 06:34:17 +0000

    urh: update 2.2.3_2 bottle.

--- Formula/urh.rb
+++ Formula/urh.rb
@@ -7,10 +7,9 @@ class Urh < Formula
   head "https://github.com/jopohl/urh.git"
 
   bottle do
-    sha256 "31e1196e53213644c17bc3df1c2af4ba5159dd2b8312150a56a3ea37d8c660ca" => :mojave
-    sha256 "c77f3080e796ed94595a9de1743491aed7396214070163d9e4cf11571f5b95d1" => :high_sierra
-    sha256 "696d51839952863780505a3ff51bb5c0cce60498241a73cfe88203fbc3440b39" => :sierra
-    sha256 "d390fd9273b25cfd8b8211bde9b1d1f39bd63d0eb6898a2c67103a18cbdc9a58" => :el_capitan
+    sha256 "647c15cb2380d3e5843d6ce388fc1f8da31eaf64769125476c036af7c90ddb0c" => :mojave
+    sha256 "38da736184970a7a747e5f64c54e3dbb33d7f4030bb1bf91997db2a4df9f294b" => :high_sierra
+    sha256 "e72bea1865547efd2dbb596b48df5a2aa78763064bea26bf06ff541b5aeab3b2" => :sierra
   end
 
   depends_on "pkg-config" => :build
