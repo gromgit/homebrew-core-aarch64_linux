d1d55cb3f7af16b5f1b6e7755bb1565db8292990
Igor Kapkov <igasgeek@me.com>
2018-12-06 11:21:51 +1100

    aws-apigateway-importer: update github url

--- Formula/aws-apigateway-importer.rb
+++ Formula/aws-apigateway-importer.rb
@@ -1,7 +1,7 @@
 class AwsApigatewayImporter < Formula
   desc "AWS API Gateway Importer"
-  homepage "https://github.com/awslabs/aws-apigateway-importer"
-  url "https://github.com/awslabs/aws-apigateway-importer/archive/aws-apigateway-importer-1.0.1.tar.gz"
+  homepage "https://github.com/amazon-archives/aws-apigateway-importer"
+  url "https://github.com/amazon-archives/aws-apigateway-importer/archive/aws-apigateway-importer-1.0.1.tar.gz"
   sha256 "8371e3fb1b6333cd50a76fdcdc1280ee8e489aec4bf9a1869325f9b8ebb73b54"
   revision 1
 
@@ -19,7 +19,7 @@ class AwsApigatewayImporter < Formula
 
   # Pin aws-sdk-java-core for JSONObject compatibility
   patch do
-    url "https://github.com/awslabs/aws-apigateway-importer/commit/660e3ce.diff?full_index=1"
+    url "https://github.com/amazon-archives/aws-apigateway-importer/commit/660e3ce.diff?full_index=1"
     sha256 "6ff63c504b906e1fb6d0f2a9772761edeef3b37b3dca1e48bba72432d863a852"
   end
 
