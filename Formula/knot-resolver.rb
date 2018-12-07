52c65c92cc57ccb6e469c0b8e7e6cceccb38f9c4
Michael Burns <michael.burns@corusent.com>
2018-12-07 10:55:21 -0500

    knot-resolver 3.1.0
    Closes #34884.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/knot-resolver.rb
+++ Formula/knot-resolver.rb
@@ -1,8 +1,8 @@
 class KnotResolver < Formula
   desc "Minimalistic, caching, DNSSEC-validating DNS resolver"
   homepage "https://www.knot-resolver.cz"
-  url "https://secure.nic.cz/files/knot-resolver/knot-resolver-3.0.0.tar.xz"
-  sha256 "68a0137e0e15061ee7dec53a2e424aa3266611720db3843853c6e7774a414f40"
+  url "https://secure.nic.cz/files/knot-resolver/knot-resolver-3.1.0.tar.xz"
+  sha256 "8f3deba4695784a666cde317bc6af80ecf42ce1047b01f4b9c582fdc021c7492"
   head "https://gitlab.labs.nic.cz/knot/knot-resolver.git"
 
   bottle do
