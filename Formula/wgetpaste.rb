fe73c782e9999663f3f6cdb478adeaf95b2b8844
Viktor Szakats <commit@vszakats.net>
2018-12-07 14:10:00 +0000

    wgetpaste: secure url(s)

--- Formula/wgetpaste.rb
+++ Formula/wgetpaste.rb
@@ -1,7 +1,7 @@
 class Wgetpaste < Formula
   desc "Automate pasting to a number of pastebin services"
-  homepage "http://wgetpaste.zlin.dk/"
-  url "http://wgetpaste.zlin.dk/wgetpaste-2.28.tar.bz2"
+  homepage "https://wgetpaste.zlin.dk/"
+  url "https://wgetpaste.zlin.dk/wgetpaste-2.28.tar.bz2"
   sha256 "538d38bab491544bdf6f05f7a38f83d4c3dfee77de7759cb6b9be1ebfdd609c2"
 
   bottle :unneeded
