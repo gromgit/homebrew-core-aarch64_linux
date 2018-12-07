20961a94db81c88c9c980efa95b31316b5600287
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-07 10:10:26 +0000

    stunnel: update 5.50 bottle.

--- Formula/stunnel.rb
+++ Formula/stunnel.rb
@@ -6,9 +6,10 @@ class Stunnel < Formula
   sha256 "951d92502908b852a297bd9308568f7c36598670b84286d3e05d4a3a550c0149"
 
   bottle do
-    sha256 "c52648f9e03fbb924d84e76fd73c7d1685f9be88773ea0578dd655761943e76c" => :mojave
-    sha256 "633395c17c2850e77ffceb4ed17adf9547e2596dccbe9555b7f667da1ba926b2" => :high_sierra
-    sha256 "906b154e9fde4003da80ee99c1e5bbd224da1c465ca23713102ea4f1c2ec8d64" => :sierra
+    cellar :any
+    sha256 "e0fb492f1f33313f9b826df1c215b3e9249838ca6b86fc0f16ad82a06aa360ff" => :mojave
+    sha256 "992e36736d37859b9554a2c31ad76ed48d2ab112f1d93c440f1302cf6ce560fe" => :high_sierra
+    sha256 "ce75cbd34d1aa2d22faadbc78666e8663c6b5a629ccd76f1e7a343486aea95d8" => :sierra
   end
 
   depends_on "openssl"
