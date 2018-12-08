8c24bdcc8109445e8cafbcdc4c71769f9bdea9a4
Igor Kapkov <igasgeek@me.com>
2018-12-08 23:03:09 +1100

    osrm-backend 5.20.0
    Closes #34905.
    
    Signed-off-by: Jason Tedor <jason@tedor.me>

--- Formula/osrm-backend.rb
+++ Formula/osrm-backend.rb
@@ -1,8 +1,8 @@
 class OsrmBackend < Formula
   desc "High performance routing engine"
   homepage "http://project-osrm.org/"
-  url "https://github.com/Project-OSRM/osrm-backend/archive/v5.19.0.tar.gz"
-  sha256 "d3671630712c6b3e6a47e12ce0376a75c8915a0072bfec17ed10f3767549be93"
+  url "https://github.com/Project-OSRM/osrm-backend/archive/v5.20.0.tar.gz"
+  sha256 "5329c12e705c03e4504f8a5cb9e09a2c1855ceb9f74c03e32930cd974fad3bbe"
   head "https://github.com/Project-OSRM/osrm-backend.git"
 
   bottle do
