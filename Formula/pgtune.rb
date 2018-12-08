2b5f34a1d86d977bbc320e8a6878b990afa13ff1
Viktor Szakats <commit@vszakats.net>
2018-12-08 13:44:44 +0000

    pgtune: restore live urls (from archive.org)

--- Formula/pgtune.rb
+++ Formula/pgtune.rb
@@ -1,6 +1,6 @@
 class Pgtune < Formula
   desc "Tuning wizard for postgresql.conf"
-  homepage "https://web.archive.org/web/20180903060537/pgfoundry.org/projects/pgtune"
+  homepage "http://pgfoundry.org/projects/pgtune"
   url "https://ftp.postgresql.org/pub/projects/pgFoundry/pgtune/pgtune/0.9.3/pgtune-0.9.3.tar.gz"
   mirror "https://mirrorservice.org/sites/ftp.postgresql.org/projects/pgFoundry/pgtune/pgtune/0.9.3/pgtune-0.9.3.tar.gz"
   mirror "https://deb.debian.org/debian/pool/main/p/pgtune/pgtune_0.9.3.orig.tar.gz"
