8f3cd0070ba7ecfe797924228c859719ad70dc5a
Viktor Szakats <commit@vszakats.net>
2018-12-08 13:44:44 +0000

    orocos-kdl: restore live urls (from archive.org)

--- Formula/orocos-kdl.rb
+++ Formula/orocos-kdl.rb
@@ -1,6 +1,6 @@
 class OrocosKdl < Formula
   desc "Orocos Kinematics and Dynamics C++ library"
-  homepage "https://web.archive.org/web/20180831030030/www.orocos.org/kdl"
+  homepage "http://www.orocos.org/kdl"
   url "https://github.com/orocos/orocos_kinematics_dynamics/archive/v1.4.0.tar.gz"
   sha256 "05b93e759923684dc07433ccae1e476d158d89b3c2be5079c20062406da7b4dd"
 
