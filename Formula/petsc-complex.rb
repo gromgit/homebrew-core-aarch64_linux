f30071033d15193d4d8073be538693dd87581b48
Thierry Moisan <thierry.moisan@gmail.com>
2018-12-10 17:34:02 -0500

    petsc-complex: bump revision for open-mpi-4.0.0

--- Formula/petsc-complex.rb
+++ Formula/petsc-complex.rb
@@ -3,6 +3,7 @@ class PetscComplex < Formula
   homepage "https://www.mcs.anl.gov/petsc/"
   url "http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.9.3.tar.gz"
   sha256 "8828fe1221f038d78a8eee3325cdb22ad1055a2f0671871815ee9f47365f93bb"
+  revision 1
 
   bottle do
     sha256 "3159d87b2fb881890e3cbb8c2dcd815400321a0041c5291dbdb29585628cbf5e" => :mojave
