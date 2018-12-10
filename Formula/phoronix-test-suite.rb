153fea8fb06dc684b9cfc625b0bb8ef49e1136dd
Igor Kapkov <igasgeek@me.com>
2018-12-10 14:31:15 +1100

    phoronix-test-suite 8.4.1 (#34947)

--- Formula/phoronix-test-suite.rb
+++ Formula/phoronix-test-suite.rb
@@ -1,8 +1,8 @@
 class PhoronixTestSuite < Formula
   desc "Open-source automated testing/benchmarking software"
   homepage "https://www.phoronix-test-suite.com/"
-  url "https://github.com/phoronix-test-suite/phoronix-test-suite/archive/v8.4.0.tar.gz"
-  sha256 "10d8a7e37b41719b81ae5c24c15762d0080cfb2abf34bbd079096d6194a53285"
+  url "https://github.com/phoronix-test-suite/phoronix-test-suite/archive/v8.4.1.tar.gz"
+  sha256 "29e2221ce0db9d11f98cbdab3f45c60e87868b366b00ad698e1a45fee8a96764"
   head "https://github.com/phoronix-test-suite/phoronix-test-suite.git"
 
   bottle :unneeded
