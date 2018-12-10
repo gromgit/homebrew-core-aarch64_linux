99286aad013bd37502af8cb5566da3b5b5353124
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-10 03:29:01 +0000

    github-markdown-toc: update 1.0.0 bottle.

--- Formula/github-markdown-toc.rb
+++ Formula/github-markdown-toc.rb
@@ -6,11 +6,9 @@ class GithubMarkdownToc < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f1954c607fab0ee457851918418d40fc6d5ada7396058bba445fd0e47e2db657" => :mojave
-    sha256 "8853e0adc3d20ec4838d84e954bfb90e343154e86545843a1641c8424029565b" => :high_sierra
-    sha256 "aa55273d31e7668f919e05084ee86d020079ea88c960e50acd040e03f54f1e6e" => :sierra
-    sha256 "0a625d0131a7f928c82194ee3aeec6cfbaffb81e690ae9a66b8bc95267493c9c" => :el_capitan
-    sha256 "15d2549f9ec4c3d5a38c6de1c965808d407df5c05ee78a54093d050bb48993a5" => :yosemite
+    sha256 "0f96d95ab270819393d74cd6daac7cae2b9b996d1ef1f13a575e59e3f355f27c" => :mojave
+    sha256 "633c4cbbc1dd8d1194bfbefe0883266f6b61121ba343b83707d77c3cce8d9922" => :high_sierra
+    sha256 "c462dd1fdbb04f213f6ad1423b8e659766082760e0fbdfa4451807d97a8ce567" => :sierra
   end
 
   depends_on "go" => :build
