72df486005753a3bfb29489631613d4435323039
Chongyu Zhu <i@lembacon.com>
2018-12-11 20:30:23 +0800

    rustup-init 1.16.0
    Closes #34987.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/rustup-init.rb
+++ Formula/rustup-init.rb
@@ -1,8 +1,8 @@
 class RustupInit < Formula
   desc "The Rust toolchain installer"
-  homepage "https://github.com/rust-lang-nursery/rustup.rs"
-  url "https://github.com/rust-lang-nursery/rustup.rs/archive/1.15.0.tar.gz"
-  sha256 "470441d59dbb33f4e3e52e3f7420734dae0066598802c2b3b4f89f5b3a6a9e45"
+  homepage "https://github.com/rust-lang/rustup.rs"
+  url "https://github.com/rust-lang/rustup.rs/archive/1.16.0.tar.gz"
+  sha256 "8c4ffeda2088dbdd5ea2eac8acef5ddd57dfcfe1f06a503e3da790f93161e1a6"
 
   bottle do
     sha256 "7c64015c8bca7660a92879073984fc5bdd8de896588c6ca6a15b15fdcce3c345" => :mojave
@@ -13,10 +13,10 @@ class RustupInit < Formula
   depends_on "rust" => :build
 
   # Fixes `rustup-init` not working when it is relative symlink.
-  # https://github.com/rust-lang-nursery/rustup.rs/issues/1512
-  # https://github.com/rust-lang-nursery/rustup.rs/pull/1521
+  # https://github.com/rust-lang/rustup.rs/issues/1512
+  # https://github.com/rust-lang/rustup.rs/pull/1521
   patch do
-    url "https://github.com/rust-lang-nursery/rustup.rs/pull/1521/commits/ebbfced7ee4aeb5be4c775f8f85e351093d5818f.diff?full_index=1"
+    url "https://github.com/rust-lang/rustup.rs/pull/1521/commits/ebbfced7ee4aeb5be4c775f8f85e351093d5818f.diff?full_index=1"
     sha256 "34dc006d75d747b99dd9fe49bdde396a8bf5443d1e3df1a47c4776c06ec3f3c7"
   end
 
