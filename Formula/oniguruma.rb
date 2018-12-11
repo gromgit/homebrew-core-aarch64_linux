283a773a3f53ff8fec00656e21aa3cb859b34c11
Chongyu Zhu <i@lembacon.com>
2018-12-11 20:05:57 +0800

    oniguruma 6.9.1
    Closes #34984.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/oniguruma.rb
+++ Formula/oniguruma.rb
@@ -1,8 +1,8 @@
 class Oniguruma < Formula
   desc "Regular expressions library"
   homepage "https://github.com/kkos/oniguruma/"
-  url "https://github.com/kkos/oniguruma/releases/download/v6.9.0/onig-6.9.0.tar.gz"
-  sha256 "91bfb25e050ce3c301483204dd3f0d03a7c05472e20d48fe227a383d4534e7c9"
+  url "https://github.com/kkos/oniguruma/releases/download/v6.9.1/onig-6.9.1.tar.gz"
+  sha256 "c7c3feb7be45a5cc9f2dec239b4a317a422e6ffea299cf91ffab1b926633ea12"
 
   bottle do
     cellar :any
