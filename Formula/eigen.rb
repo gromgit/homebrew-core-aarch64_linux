c7ffa46285a35dc51ad9201de7b5624220fd3dcf
Thierry Moisan <thierry.moisan@gmail.com>
2018-12-11 18:29:20 -0500

    eigen 3.3.7

--- Formula/eigen.rb
+++ Formula/eigen.rb
@@ -1,8 +1,8 @@
 class Eigen < Formula
   desc "C++ template library for linear algebra"
   homepage "https://eigen.tuxfamily.org/"
-  url "https://bitbucket.org/eigen/eigen/get/3.3.5.tar.bz2"
-  sha256 "7352bff3ea299e4c7d7fbe31c504f8eb9149d7e685dec5a12fbaa26379f603e2"
+  url "https://bitbucket.org/eigen/eigen/get/3.3.7.tar.bz2"
+  sha256 "9f13cf90dedbe3e52a19f43000d71fdf72e986beb9a5436dddcd61ff9d77a3ce"
   head "https://bitbucket.org/eigen/eigen", :using => :hg
 
   bottle do
