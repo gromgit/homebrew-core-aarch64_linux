147477b444ff7a476d6b94a96f4101bd82496c7f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-11 10:45:02 +0000

    dnscontrol: add 0.2.7 bottle.

--- Formula/dnscontrol.rb
+++ Formula/dnscontrol.rb
@@ -4,6 +4,13 @@ class Dnscontrol < Formula
   url "https://github.com/StackExchange/dnscontrol/archive/v0.2.7.tar.gz"
   sha256 "2889c6bec5c4b77a21bda82a1a3760ad64667f8a30ed009b4750f605c9827598"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f73c7d1bfb445b7cb48471889130e59403c9d65de23f6856b0bf996b598e0a7a" => :mojave
+    sha256 "6b6f2ab020128b43a82a51ce7358756657dd7440b0b42daa30be63520501f5e8" => :high_sierra
+    sha256 "b43bbcea38da57e4007d4db52ab62b390d680f0979b9c9163fe32cf768d44a8a" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
