edd2e81f9c23d8a5d8372a1238e7e650c2d64b42
Steve Mao <maochenyan@gmail.com>
2018-12-12 11:15:11 +1100

    improve nvm bash profile scripts
    Closes #35013.
    
    Signed-off-by: Sean Molenaar <smillerdev@me.com>

--- Formula/nvm.rb
+++ Formula/nvm.rb
@@ -25,7 +25,8 @@ class Nvm < Formula
     configuration file:
 
       export NVM_DIR="$HOME/.nvm"
-      . "#{opt_prefix}/nvm.sh"
+      [ -s "#{opt_prefix}/nvm.sh" ] && \. "#{opt_prefix}/nvm.sh"  # This loads nvm
+      [ -s "#{opt_prefix}/etc/bash_completion" ] && \. "#{opt_prefix}/etc/bash_completion"  # This loads nvm bash_completion
 
     You can set $NVM_DIR to any location, but leaving it unchanged from
     #{prefix} will destroy any nvm-installed Node installations
