ec361bc7710ffffa46ad7aceb76bfad8513a7399
Igor Kapkov <igasgeek@me.com>
2018-12-12 20:16:49 +1100

    curl 7.63.0
    Closes #35025.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -1,9 +1,9 @@
 class Curl < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.haxx.se/"
-  url "https://curl.haxx.se/download/curl-7.62.0.tar.bz2"
-  mirror "http://curl.mirror.anstey.ca/curl-7.62.0.tar.bz2"
-  sha256 "7802c54076500be500b171fde786258579d60547a3a35b8c5a23d8c88e8f9620"
+  url "https://curl.haxx.se/download/curl-7.63.0.tar.bz2"
+  mirror "http://curl.mirror.anstey.ca/curl-7.63.0.tar.bz2"
+  sha256 "9bab7ed4ecff77020a312d84cc5fb7eb02d58419d218f267477a724a17fd8dd8"
 
   bottle do
     cellar :any
