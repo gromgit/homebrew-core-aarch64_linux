debb939757d2c498b531f8e1da6ce50299c13dda
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-12 13:32:27 +0000

    dav1d: add 0.1.0 bottle.

--- Formula/dav1d.rb
+++ Formula/dav1d.rb
@@ -4,6 +4,13 @@ class Dav1d < Formula
   url "https://code.videolan.org/videolan/dav1d/-/archive/0.1.0/dav1d-0.1.0.tar.bz2"
   sha256 "923f78d17aed08252608790eaed06a3d530763168fd6a8fc74e3306d7e01e6a5"
 
+  bottle do
+    cellar :any
+    sha256 "f1b2efe50ba43547c9a51e5b1190c4aad90684f83ad42275c68db7fbb82b2331" => :mojave
+    sha256 "6d2163128de08761e95cc5a08864375d065f55cdc3afeb94b4dd222c9a5940d0" => :high_sierra
+    sha256 "a95b01e359731fffbd55a81edaae6293d70c1111cc548dc88e6254d942eb0f31" => :sierra
+  end
+
   depends_on "meson" => :build
   depends_on "nasm" => :build
   depends_on "ninja" => :build
