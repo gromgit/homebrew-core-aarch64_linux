f4b864c34288d037120008586cb3fe98005e91c5
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-12-12 07:05:06 +0000

    pixman 0.36.0
    Closes #35019.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/pixman.rb
+++ Formula/pixman.rb
@@ -1,9 +1,8 @@
 class Pixman < Formula
   desc "Low-level library for pixel manipulation"
   homepage "https://cairographics.org/"
-  url "https://cairographics.org/releases/pixman-0.34.0.tar.gz"
-  sha256 "21b6b249b51c6800dc9553b65106e1e37d0e25df942c90531d4c3997aa20a88e"
-  revision 1
+  url "https://cairographics.org/releases/pixman-0.36.0.tar.gz"
+  sha256 "1ca19c8d4d37682adfbc42741d24977903fec1169b4153ec05bb690d4acf9fae"
 
   bottle do
     cellar :any
@@ -15,16 +14,6 @@ class Pixman < Formula
   end
 
   depends_on "pkg-config" => :build
-
-  # Fix "error: use of unknown builtin '__builtin_shuffle'"
-  # Upstream issue 31 Jan 2018 "Fails to build pixman-0.34.0 with clang 5.x or later"
-  # See https://bugs.freedesktop.org/show_bug.cgi?id=104886
-  if DevelopmentTools.clang_build_version >= 902
-    patch do
-      url "https://bugs.freedesktop.org/attachment.cgi?id=137100"
-      sha256 "2af5b3700e38600297f2cb66059218b1128337d995cba799b385ad09942c934f"
-    end
-  end
 
   def install
     system "./configure", "--disable-dependency-tracking",
