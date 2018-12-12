10677217bce0dfc35f636f2bddd1325db0c71d95
Igor Kapkov <igasgeek@me.com>
2018-12-12 17:11:15 +1100

    autorest 2.0.4283
    Closes #35016.
    
    Signed-off-by: Igor Kapkov <igasgeek@me.com>

--- Formula/autorest.rb
+++ Formula/autorest.rb
@@ -1,8 +1,10 @@
+require "language/node"
+
 class Autorest < Formula
   desc "Swagger (OpenAPI) Specification code generator"
   homepage "https://github.com/Azure/autorest"
-  url "https://api.nuget.org/packages/autorest.0.17.3.nupkg"
-  sha256 "b3f5b67ae1a8aa4f0fd6cf1e51df27ea1867f0c845dbb13c1c608b148bd86296"
+  url "https://registry.npmjs.org/autorest/-/autorest-2.0.4283.tgz"
+  sha256 "a655719fa6dd20b11db4a3d9c5853e9ed0454429bd6371f0bc6a5a9014b1bb8d"
 
   bottle do
     cellar :any_skip_relocation
@@ -13,25 +15,24 @@ class Autorest < Formula
     sha256 "da1dc0e3a25b005db13ffbb95b145c060162648ad700998e4814a7969e17cbb1" => :yosemite
   end
 
-  depends_on "mono"
+  depends_on "node"
 
-  resource "swagger" do
-    url "https://raw.githubusercontent.com/Azure/autorest/764d308b3b75ba83cb716708f5cef98e63dde1f7/Samples/petstore/petstore.json"
-    sha256 "8de4043eff83c71d49f80726154ca3935548bd974d915a6a9b6aa86da8b1c87c"
+  resource "petstore" do
+    url "https://raw.githubusercontent.com/Azure/autorest/5c170a02c009d032e10aa9f5ab7841e637b3d53b/Samples/1b-code-generation-multilang/petstore.yaml"
+    sha256 "e981f21115bc9deba47c74e5c533d92a94cf5dbe880c4304357650083283ce13"
   end
 
   def install
-    libexec.install Dir["tools/*"]
-    (bin/"autorest").write <<~EOS
-      #!/bin/bash
-      mono #{libexec}/AutoRest.exe "$@"
-    EOS
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
-    resource("swagger").stage do
-      assert_match "Finished generating CSharp code for petstore.json.",
-        shell_output("#{bin}/autorest -n test -i petstore.json")
+    resource("petstore").stage do
+      system (bin/"autorest"), "--input-file=petstore.yaml",
+                               "--nodejs",
+                               "--output-folder=petstore"
+      assert_includes File.read("petstore/package.json"), "Microsoft Corporation"
     end
   end
 end
