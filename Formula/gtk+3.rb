24b70975c0097b30516362768601b6d078d7cd56
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-12-12 20:14:39 +0000

    gtk+3 3.24.2
    Closes #35041.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/gtk+3.rb
+++ Formula/gtk+3.rb
@@ -1,8 +1,8 @@
 class Gtkx3 < Formula
   desc "Toolkit for creating graphical user interfaces"
   homepage "https://gtk.org/"
-  url "https://download.gnome.org/sources/gtk+/3.22/gtk+-3.22.30.tar.xz"
-  sha256 "a1a4a5c12703d4e1ccda28333b87ff462741dc365131fbc94c218ae81d9a6567"
+  url "https://download.gnome.org/sources/gtk+/3.24/gtk+-3.24.2.tar.xz"
+  sha256 "5b3b05e427cc928d103561ed2e91b2b2881fe88b1f167b0b1c9990da6aac8892"
 
   bottle do
     sha256 "bc0243c2284d6c7b45350f0530b704b8bbf3643ffc6f83eb247db94cc53a79b1" => :mojave
@@ -20,6 +20,15 @@ class Gtkx3 < Formula
   depends_on "libepoxy"
   depends_on "pango"
   depends_on "gsettings-desktop-schemas" => :recommended
+
+  # see https://gitlab.gnome.org/GNOME/gtk/issues/1517
+  patch :DATA
+
+  # see https://gitlab.gnome.org/GNOME/gtk/issues/1518
+  patch do
+    url "https://gitlab.gnome.org/jralls/gtk/commit/efb3888af770937c6c2c184d9beea19fbc24bb4a.patch"
+    sha256 "d847d1b4659153f0d815189039776054bccc73f85cfb967c5cc2cf0e0061d0d7"
+  end
 
   def install
     args = %W[
@@ -107,3 +116,21 @@ class Gtkx3 < Formula
     system "./test"
   end
 end
+
+__END__
+diff --git a/gdk/quartz/gdkevents-quartz.c b/gdk/quartz/gdkevents-quartz.c
+index 952d4a8189..642fe7f1ee 100644
+--- a/gdk/quartz/gdkevents-quartz.c
++++ b/gdk/quartz/gdkevents-quartz.c
+@@ -1551,8 +1551,10 @@ gdk_event_translate (GdkEvent *event,
+           grab = _gdk_display_get_last_device_grab (_gdk_display,
+                                                     gdk_seat_get_pointer (seat));
+         }
+-      return_val = TRUE;
+     }
++
++  return_val = TRUE;
++
+   switch (event_type)
+     {
+     case GDK_QUARTZ_LEFT_MOUSE_DOWN:
