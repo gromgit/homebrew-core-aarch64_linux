ae66a56a69edb5b6e50df742dccf99ef8abf4b10
Igor Kapkov <igasgeek@me.com>
2018-12-13 20:22:19 +1100

    riemann-client 1.10.4
    Closes #35064.
    
    Signed-off-by: Sean Molenaar <smillerdev@me.com>

--- Formula/riemann-client.rb
+++ Formula/riemann-client.rb
@@ -1,8 +1,8 @@
 class RiemannClient < Formula
   desc "C client library for the Riemann monitoring system"
   homepage "https://github.com/algernon/riemann-c-client"
-  url "https://github.com/algernon/riemann-c-client/archive/riemann-c-client-1.10.3.tar.gz"
-  sha256 "dc00d817d3f6fe214d21365d2b236b73d67fb317ea96c069222c11384d8b2508"
+  url "https://github.com/algernon/riemann-c-client/archive/riemann-c-client-1.10.4.tar.gz"
+  sha256 "334874f0b9a507a8abbc7138df719cba4f28f12c02c39d5e55090b8edb86f9d2"
   head "https://github.com/algernon/riemann-c-client.git"
 
   bottle do
