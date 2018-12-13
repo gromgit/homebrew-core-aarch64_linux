1c61f6534584cdac6d9a53f79ff7328b487e2f1d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-13 08:30:02 +0000

    minio-mc: update 20181212190222 bottle.
    Closes #35059.
    
    Signed-off-by: Sean Molenaar <smillerdev@me.com>

--- Formula/minio-mc.rb
+++ Formula/minio-mc.rb
@@ -8,9 +8,9 @@ class MinioMc < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f9ecc88363b54970c74d6445a10ea0a6fc3af62b33a8e2d1ff77f6f6b45d6048" => :mojave
-    sha256 "7135465a15873c1d8833a0340d38d541d678a29ef396fcdd8d67aa06f781ef75" => :high_sierra
-    sha256 "92b450dfa4c72744c89cd18c1b58576bc441ae34b7ae2b497f347e8c9661912e" => :sierra
+    sha256 "c3e9be1fa1e44aa0f63f948a369183b52b8dac4d83202b1c41e5ecc2f4660cc9" => :mojave
+    sha256 "dfd08cecab94e89b9a6a72e2b9919fc15d04beeb2c1db40bb5e6e212937ff84e" => :high_sierra
+    sha256 "2df981f9a7de3edcbe93d83ffe66cb81f51334e04da9c8d9197493c8d801d562" => :sierra
   end
 
   depends_on "go" => :build
