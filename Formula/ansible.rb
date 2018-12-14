0b33ebc253ff9acb8d8365bd569e38d04c236f41
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-14 02:59:21 +0000

    ansible: update 2.7.5 bottle.

--- Formula/ansible.rb
+++ Formula/ansible.rb
@@ -9,9 +9,9 @@ class Ansible < Formula
 
   bottle do
     cellar :any
-    sha256 "ed2c87e456605982ace03391793f201c48cadf92a973d6d2b635b92837390fbe" => :mojave
-    sha256 "2fb9de519ae038c6520011865eca369105d9b30d8defaf7192829a6f3f8cb564" => :high_sierra
-    sha256 "0d5380ec9d1fe93b626f346135cddf3cd489f410bd92f8e015d4125c0a3a1155" => :sierra
+    sha256 "861921d1b648ddecd14ffc65564f0d50e37bb5f82d74cbb9629ff2f9a48f55e0" => :mojave
+    sha256 "a7cf6719982444d2dd1987530d9eb94b3c2ac151aca14f236e664c59eda881c2" => :high_sierra
+    sha256 "821ce76f6f847ca0a021cebcf78deae8e4304f0adba2cb38d70610b205ba9f54" => :sierra
   end
 
   depends_on "pkg-config" => :build
