049cbf7ed61ab2ab724a234ec4e9132fdbdc100c
Michka Popoff <michkapopoff@gmail.com>
2018-12-14 23:28:27 +0100

    when: fix mirror url
    Closes #35129.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/when.rb
+++ Formula/when.rb
@@ -1,7 +1,7 @@
 class When < Formula
   desc "Tiny personal calendar"
   homepage "http://www.lightandmatter.com/when/when.html"
-  url "https:/deb.debian.org/debian/pool/main/w/when/when_1.1.38.orig.tar.gz"
+  url "https://deb.debian.org/debian/pool/main/w/when/when_1.1.38.orig.tar.gz"
   sha256 "139834945142f5e3ea6b20f43ba740d30b4a87b42ce5767026094e633dca999f"
   head "https://github.com/bcrowell/when.git"
 
