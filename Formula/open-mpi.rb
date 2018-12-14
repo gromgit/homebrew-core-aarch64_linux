81d6d31b95723deb72c71e9b6c7020c27447cedc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-14 18:39:12 +0000

    open-mpi: update 4.0.0 bottle.

--- Formula/open-mpi.rb
+++ Formula/open-mpi.rb
@@ -5,9 +5,9 @@ class OpenMpi < Formula
   sha256 "2f0b8a36cfeb7354b45dda3c5425ef8393c9b04115570b615213faaa3f97366b"
 
   bottle do
-    sha256 "ba17c7e6a3ca1e6776e7d3049f0e75d5f3393bb4322885f9b1464717c4ceb012" => :mojave
-    sha256 "8bdfd640afbc48f13f1f3621e809edb139dd69309b5dfa3dd68274b6f7626864" => :high_sierra
-    sha256 "e399bdf2f73bde3257979938db92d3bc9229746a29f7957ef9a11d0588e5b4f3" => :sierra
+    sha256 "144bf979a8e13639714b4d0b55a4f7a49b74f4942fc655b29c8cc56879d3e95e" => :mojave
+    sha256 "ba0dbe6719d413637ad2fa1557126c88bc0ac49191e4efefea75508de568311a" => :high_sierra
+    sha256 "b862fdf7f2c21dddc9e7e1f180622f9a398cb76091cb84fb2181cead9073eb6f" => :sierra
   end
 
   head do
