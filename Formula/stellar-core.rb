80ca8aedebd3ee0df986c210d781d59162456720
Igor Kapkov <igasgeek@me.com>
2018-12-14 18:07:38 +1100

    stellar-core 10.1.0
    Closes #35112.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/stellar-core.rb
+++ Formula/stellar-core.rb
@@ -2,8 +2,8 @@ class StellarCore < Formula
   desc "The backbone of the Stellar (XLM) network"
   homepage "https://www.stellar.org/"
   url "https://github.com/stellar/stellar-core.git",
-      :tag      => "v10.0.0",
-      :revision => "1fc018b4f52e8c7e716b023ccf30600af5b4f66d"
+      :tag      => "v10.1.0",
+      :revision => "1fe2e8a768ecc4db2d53a4a67fc733bb1e99ecd1"
   head "https://github.com/stellar/stellar-core.git"
 
   bottle do
