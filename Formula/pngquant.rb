db575c1bdf6974fc76f363b711e6a0dbbbf67ab7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-15 03:27:17 +0000

    pngquant: update 2.12.2 bottle.

--- Formula/pngquant.rb
+++ Formula/pngquant.rb
@@ -7,10 +7,9 @@ class Pngquant < Formula
 
   bottle do
     cellar :any
-    sha256 "abdd1c8fa2450b5ee05aebc0ca23c0af892365efaed1e289e437ddc85ee89788" => :mojave
-    sha256 "f052f75b1b03c360dfa13a80322c5e24d60e74d7e330cc77c29e7a9ea9dfcc0b" => :high_sierra
-    sha256 "cbd79a30ce1a5302c158bb623d79e90d4637f0a4267973731fa1501c6351adb8" => :sierra
-    sha256 "62aca3d86ea5a1a8db27cb073f3ed715e53d1734981d14124019eddd419cb60c" => :el_capitan
+    sha256 "71656950218e1bc4382a43615f41b02b12539db9b7352e2e423212bec6d7a07e" => :mojave
+    sha256 "a71a5b271f72aafce935ebf665afc158b5a1e8d3b62fa916773363dfd43d4ce6" => :high_sierra
+    sha256 "898ad660558ca14a04cb465ccf374e24439c6067f428cb665f99486ddb41dcea" => :sierra
   end
 
   depends_on "pkg-config" => :build
