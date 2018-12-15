04ce80b16329f36da0e3cdaf7db9f7191cf7d0a6
FX Coudert <fxcoudert@gmail.com>
2018-12-15 08:40:22 +0100

    gcc@5: remove options

--- Formula/gcc@5.rb
+++ Formula/gcc@5.rb
@@ -39,12 +39,6 @@ class GccAT5 < Formula
     satisfy { MacOS::CLT.installed? }
   end
 
-  option "with-all-languages", "Enable all compilers and languages, except Ada"
-  option "with-nls", "Build with native language support (localization)"
-  option "with-profiled-build", "Make use of profile guided optimization when bootstrapping GCC"
-  option "with-jit", "Build the jit compiler"
-  option "without-fortran", "Build without the gfortran compiler"
-
   depends_on :maximum_macos => [:high_sierra, :build]
 
   depends_on "gmp"
@@ -58,13 +52,6 @@ class GccAT5 < Formula
     url "https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.14.tar.bz2"
     mirror "https://mirrorservice.org/sites/distfiles.macports.org/isl/isl-0.14.tar.bz2"
     sha256 "7e3c02ff52f8540f6a85534f54158968417fd676001651c8289c705bd0228f36"
-  end
-
-  # Fix for libgccjit.so linkage on Darwin.
-  # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=64089
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/64fd2d52/gcc%405/5.4.0.patch"
-    sha256 "1e126048d9a6b29b0da04595ffba09c184d338fe963cf9db8d81b47222716bc4"
   end
 
   # Fix build with Xcode 9
@@ -93,18 +80,8 @@ class GccAT5 < Formula
     # Build ISL 0.14 from source during bootstrap
     resource("isl").stage buildpath/"isl"
 
-    if build.with? "all-languages"
-      # Everything but Ada, which requires a pre-existing GCC Ada compiler
-      # (gnat) to bootstrap. GCC 4.6.0 add go as a language option, but it is
-      # currently only compilable on Linux.
-      languages = %w[c c++ fortran objc obj-c++ jit]
-    else
-      # C, C++, ObjC compilers are always built
-      languages = %w[c c++ objc obj-c++]
-
-      languages << "fortran" if build.with? "fortran"
-      languages << "jit" if build.with? "jit"
-    end
+    # C, C++, ObjC and Fortran compilers are always built
+    languages = %w[c c++ fortran objc obj-c++]
 
     version_suffix = version.to_s.slice(/\d/)
 
@@ -132,11 +109,10 @@ class GccAT5 < Formula
       # A no-op unless --HEAD is built because in head warnings will
       # raise errors. But still a good idea to include.
       "--disable-werror",
+      "--disable-nls",
       "--with-pkgversion=Homebrew GCC #{pkg_version} #{build.used_options*" "}".strip,
       "--with-bugurl=https://github.com/Homebrew/homebrew-core/issues",
     ]
-
-    args << "--disable-nls" if build.without? "nls"
 
     # The pre-Mavericks toolchain requires the older DWARF-2 debugging data
     # format to avoid failure during the stage 3 comparison of object files.
@@ -148,8 +124,6 @@ class GccAT5 < Formula
     else
       args << "--disable-multilib"
     end
-
-    args << "--enable-host-shared" if build.with?("jit") || build.with?("all-languages")
 
     # Ensure correct install names when linking against libgcc_s;
     # see discussion in https://github.com/Homebrew/homebrew/pull/34303
@@ -164,14 +138,7 @@ class GccAT5 < Formula
       end
 
       system "../configure", *args
-
-      if build.with? "profiled-build"
-        # Takes longer to build, may bug out. Provided for those who want to
-        # optimise all the way to 11.
-        system "make", "profiledbootstrap"
-      else
-        system "make", "bootstrap"
-      end
+      system "make", "bootstrap"
 
       # At this point `make check` could be invoked to run the testsuite. The
       # deja-gnu and autogen formulae must be installed in order to do this.
