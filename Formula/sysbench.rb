211266ffc862c06a77df070bc4ef0393e6af307c
Igor Kapkov <igasgeek@me.com>
2018-12-17 09:34:05 +1100

    sysbench 1.0.16
    Closes #35175.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/sysbench.rb
+++ Formula/sysbench.rb
@@ -1,8 +1,8 @@
 class Sysbench < Formula
   desc "System performance benchmark tool"
   homepage "https://github.com/akopytov/sysbench"
-  url "https://github.com/akopytov/sysbench/archive/1.0.15.tar.gz"
-  sha256 "7f004534ae58311a010480af8852b3ab4fdacd2292688e678bed9cbfe68c3c06"
+  url "https://github.com/akopytov/sysbench/archive/1.0.16.tar.gz"
+  sha256 "c537d0129151584b361efbd4ce9ed0b2195a69b72ce65912e2efa3a68f28b609"
 
   bottle do
     rebuild 1
@@ -11,15 +11,12 @@ class Sysbench < Formula
     sha256 "8aae1f5f0966a4ad0915d1443cbc2dd0aeac5e919fbc094af545ad78c2686c5b" => :sierra
   end
 
-  deprecated_option "without-mysql" => "without-mysql-client"
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
+  depends_on "mysql-client"
   depends_on "openssl"
-  depends_on "mysql-client" => :recommended
-  depends_on "postgresql" => :optional
 
   def install
     system "./autogen.sh"
@@ -29,15 +26,7 @@ class Sysbench < Formula
     # is not set then it's forced to 10.4, which breaks compile on Mojave.
     ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version
 
-    args = ["--prefix=#{prefix}"]
-    if build.with? "mysql-client"
-      args << "--with-mysql"
-    else
-      args << "--without-mysql"
-    end
-    args << "--with-psql" if build.with? "postgresql"
-
-    system "./configure", *args
+    system "./configure", "--prefix=#{prefix}", "--with-mysql"
     system "make", "install"
   end
 
