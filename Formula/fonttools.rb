500d55dbc0c28c905911b4629e9f1d32e647e008
Igor Kapkov <igasgeek@me.com>
2018-12-18 08:39:57 +1100

    fonttools 3.34.2
    Closes #35204.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -3,8 +3,8 @@ class Fonttools < Formula
 
   desc "Library for manipulating fonts"
   homepage "https://github.com/fonttools/fonttools"
-  url "https://github.com/fonttools/fonttools/releases/download/3.34.0/fonttools-3.34.0.zip"
-  sha256 "d0f66bc7ca003eb99626f5732790ed8031f4eee105f839bf7c32ddb5c28b0534"
+  url "https://github.com/fonttools/fonttools/releases/download/3.34.2/fonttools-3.34.2.zip"
+  sha256 "9fa6596ec04410dc75f2b9a5df9ca71abac85a7a5a8502f57cc72166a5401aaa"
   head "https://github.com/fonttools/fonttools.git"
 
   bottle do
