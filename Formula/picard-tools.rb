a735845c8a158741aa24e106eb89cbf344d51b89
Igor Kapkov <igasgeek@me.com>
2018-12-18 08:42:02 +1100

    picard-tools 2.18.21
    Closes #35205.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/picard-tools.rb
+++ Formula/picard-tools.rb
@@ -1,8 +1,8 @@
 class PicardTools < Formula
   desc "Tools for manipulating HTS data and formats"
   homepage "https://broadinstitute.github.io/picard/"
-  url "https://github.com/broadinstitute/picard/releases/download/2.18.20/picard.jar"
-  sha256 "f3962a74deda89a9d27204804a6afdeda2ca536c4e1001d5e4aa9e666c77a637"
+  url "https://github.com/broadinstitute/picard/releases/download/2.18.21/picard.jar"
+  sha256 "36e216986e5354920b6ea79289ae01c3117d10012020d46ce8e14f70d351365c"
 
   bottle :unneeded
 
