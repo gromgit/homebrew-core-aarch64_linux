9ff919cb5ef34dd59c880d89233f7d510462d9fc
FX Coudert <fxcoudert@gmail.com>
2018-12-17 18:04:23 +0100

    qemu: remove options

--- Formula/qemu.rb
+++ Formula/qemu.rb
@@ -3,6 +3,7 @@ class Qemu < Formula
   homepage "https://www.qemu.org/"
   url "https://download.qemu.org/qemu-3.1.0.tar.xz"
   sha256 "6a0508df079a0a33c2487ca936a56c12122f105b8a96a44374704bef6c69abfc"
+  revision 1
   head "https://git.qemu.org/git/qemu.git"
 
   bottle do
@@ -19,14 +20,14 @@ class Qemu < Formula
   depends_on "glib"
   depends_on "gnutls"
   depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libssh2"
+  depends_on "libusb"
   depends_on "ncurses"
   depends_on "pixman"
-  depends_on "libpng" => :recommended
+  depends_on "vde"
   depends_on "gtk+3" => :optional
-  depends_on "libssh2" => :optional
-  depends_on "libusb" => :optional
   depends_on "sdl2" => :optional
-  depends_on "vde" => :optional
 
   fails_with :gcc_4_0 do
     cause "qemu requires a compiler with support for the __thread specifier"
@@ -52,6 +53,8 @@ class Qemu < Formula
       --disable-bsd-user
       --disable-guest-agent
       --enable-curses
+      --enable-libssh2
+      --enable-vde
       --extra-cflags=-DNCURSES_WIDECHAR=1
     ]
 
@@ -69,10 +72,8 @@ class Qemu < Formula
     # Samba installations from external taps.
     args << "--smbd=#{HOMEBREW_PREFIX}/sbin/samba-dot-org-smbd"
 
-    args << (build.with?("vde") ? "--enable-vde" : "--disable-vde")
     args << (build.with?("sdl2") ? "--enable-sdl" : "--disable-sdl")
     args << (build.with?("gtk+3") ? "--enable-gtk" : "--disable-gtk")
-    args << (build.with?("libssh2") ? "--enable-libssh2" : "--disable-libssh2")
 
     system "./configure", *args
     system "make", "V=1", "install"
