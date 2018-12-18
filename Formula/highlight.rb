df78b99e327c7adf6291d70dcdc733cf414f9ff7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-18 19:07:25 +0000

    highlight: update 3.48 bottle.

--- Formula/highlight.rb
+++ Formula/highlight.rb
@@ -6,9 +6,9 @@ class Highlight < Formula
   head "https://gitlab.com/saalen/highlight.git"
 
   bottle do
-    sha256 "75fd94388aeb276ced3d627159a4d807db241fa088de362e1ead07bcb878b3ed" => :mojave
-    sha256 "1f5b6d789ee42f132f3387da7a459396315b03ad64fa0726a8faa3094f72e616" => :high_sierra
-    sha256 "2e1fbf7d14c171f487e8b00b649014ada94e9149e9cbaa68d917d5475f9eccee" => :sierra
+    sha256 "cef2ac3e71e17a4f11ed512cb50f3c37bc16001e4800a19985f2c370a118069e" => :mojave
+    sha256 "66fa9326e507db6407d6013a230cf459064c2b09186129e2dfb79a9002da4f6c" => :high_sierra
+    sha256 "c8a473714718b83f6284b02035d4cdff9168976006fd0654d504b833e7c2f51e" => :sierra
   end
 
   depends_on "boost" => :build
