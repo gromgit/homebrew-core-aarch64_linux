0d5c3b4fc510425b264e95d0594cc04113268a02
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-18 11:08:45 +0000

    cfitsio: update 3.450_1 bottle.

--- Formula/cfitsio.rb
+++ Formula/cfitsio.rb
@@ -8,10 +8,9 @@ class Cfitsio < Formula
 
   bottle do
     cellar :any
-    sha256 "d2f30916f14efa1e721e39374778cae8dd7b3e7a032846657816036c022aad12" => :mojave
-    sha256 "a74d1db66c52504a7b9e0f72f06ea8c2a96eb988b9882473139447a2db865830" => :high_sierra
-    sha256 "e916758becbd9c480f685c44343d99f2d0d01329bf1687f4f2b711dc51782612" => :sierra
-    sha256 "f5e9a01028f38a17ddd05488dd261eb235c34512b0ef6bade5c875525644ea54" => :el_capitan
+    sha256 "bfb59c9b5b42df93624a3f4a6eca92d89ea58667b560ace653dae5a726f7fb93" => :mojave
+    sha256 "9426e79aa95e40fa1f1e785738cc91df524ad040d4d25cca351ab29f7624f5fe" => :high_sierra
+    sha256 "a7b46f6352f302b0302ba5c5ce5edc89d5d0d1b4b231ed87954493c721d2f9a7" => :sierra
   end
 
   def install
