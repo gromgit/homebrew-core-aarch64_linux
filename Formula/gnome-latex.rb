0b4b1c7292fd780ab09b91dc70463961f1b9ecd6
FX Coudert <fxcoudert@gmail.com>
2018-12-18 08:52:15 +0100

    gnome-latex: remove options

--- Formula/gnome-latex.rb
+++ Formula/gnome-latex.rb
@@ -3,7 +3,7 @@ class GnomeLatex < Formula
   homepage "https://wiki.gnome.org/Apps/LaTeXila"
   url "https://download.gnome.org/sources/gnome-latex/3.30/gnome-latex-3.30.2.tar.xz"
   sha256 "558bbd574d3d5a71b9ecde47d7cb5e9ddf7cdbfd21f8f117f09c84c38ddfc33a"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "4f7bb1153bdce4fde041b5812e39edd311ef1a20b6cff3237a2449fbf22ea2f6" => :mojave
@@ -17,10 +17,10 @@ class GnomeLatex < Formula
   depends_on "pkg-config" => :build
   depends_on "vala" => :build
   depends_on "adwaita-icon-theme"
+  depends_on "gnome-themes-standard"
   depends_on "gspell"
   depends_on "libgee"
   depends_on "tepl"
-  depends_on "gnome-themes-standard" => :optional
 
   def install
     system "./configure", "--disable-schemas-compile",
@@ -34,10 +34,7 @@ class GnomeLatex < Formula
   def post_install
     system "#{Formula["glib"].opt_bin}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
     system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
-    # HighContrast is provided by gnome-themes-standard
-    if File.file?("#{HOMEBREW_PREFIX}/share/icons/HighContrast/.icon-theme.cache")
-      system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/HighContrast"
-    end
+    system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/HighContrast"
   end
 
   test do
