e33074ef6c8d69d2147503d71f2d99a0d30ba00a
Jason Karns <jason@karns.name>
2018-12-19 00:00:28 -0500

    node-build 4.2.0
    Closes #35249.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/node-build.rb
+++ Formula/node-build.rb
@@ -1,8 +1,8 @@
 class NodeBuild < Formula
   desc "Install NodeJS versions"
   homepage "https://github.com/nodenv/node-build"
-  url "https://github.com/nodenv/node-build/archive/v4.1.0.tar.gz"
-  sha256 "c80313dde32c596f5634542a3ccdb536df78c2df2b8c9bf95d978ca65732b840"
+  url "https://github.com/nodenv/node-build/archive/v4.2.0.tar.gz"
+  sha256 "f2d3ef2adb9c4ab7fa128170c8123ae48bcb1d09cb2af079c9df8db763f388a4"
   head "https://github.com/nodenv/node-build.git"
 
   bottle :unneeded
