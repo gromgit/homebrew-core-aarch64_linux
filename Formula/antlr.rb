c53f882b7d911ea03835255b260f8d88a3c00567
René Schwaiger <sanssecours@me.com>
2018-12-19 08:54:21 +0100

    antlr 4.7.2
    Closes #35252.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/antlr.rb
+++ Formula/antlr.rb
@@ -1,9 +1,8 @@
 class Antlr < Formula
   desc "ANother Tool for Language Recognition"
   homepage "https://www.antlr.org/"
-  url "https://www.antlr.org/download/antlr-4.7.1-complete.jar"
-  sha256 "f41dce7441d523baf9769cb7756a00f27a4b67e55aacab44525541f62d7f6688"
-  revision 1
+  url "https://www.antlr.org/download/antlr-4.7.2-complete.jar"
+  sha256 "6852386d7975eff29171dae002cc223251510d35f291ae277948f381a7b380b4"
 
   bottle :unneeded
 
