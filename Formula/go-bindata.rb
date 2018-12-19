90d821b71c5b7eb5f76b3303a1607e4f086b5277
Igor Kapkov <igasgeek@me.com>
2018-12-19 12:19:11 +1100

    go-bindata 3.13.0
    Closes #35244.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/go-bindata.rb
+++ Formula/go-bindata.rb
@@ -1,8 +1,8 @@
 class GoBindata < Formula
   desc "Small utility that generates Go code from any file"
   homepage "https://github.com/kevinburke/go-bindata"
-  url "https://github.com/kevinburke/go-bindata/archive/v3.12.0.tar.gz"
-  sha256 "9b4ffd65bfad2cf7e574f04db944f3b40d31fe76cc5cbd9bfb18ede5a07040ee"
+  url "https://github.com/kevinburke/go-bindata/archive/v3.13.0.tar.gz"
+  sha256 "16d23471f8b092d36261fc6b162e4cc30b245bf3b9c28b0f548788e6180a5729"
 
   bottle do
     cellar :any_skip_relocation
