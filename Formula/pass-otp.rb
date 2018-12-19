76b7eae657f74eaca7b5506616486f0c9a0492b5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-19 23:36:09 +0000

    pass-otp: add 1.1.1 bottle.

--- Formula/pass-otp.rb
+++ Formula/pass-otp.rb
@@ -4,6 +4,13 @@ class PassOtp < Formula
   url "https://github.com/tadfisher/pass-otp/releases/download/v1.1.1/pass-otp-1.1.1.tar.gz"
   sha256 "edb3142ab81d70af4e6d1c7f13abebd7c349be474a3f9293d9648ee91b75b458"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bc63fb7142bc11d6fcb3fd86691c520d322955ca18fa639610b4f8c741ec47a1" => :mojave
+    sha256 "fee46f4b49b58c1a689be63e16eaa813471bcd435b21c46519ac0a8af9f555c7" => :high_sierra
+    sha256 "fee46f4b49b58c1a689be63e16eaa813471bcd435b21c46519ac0a8af9f555c7" => :sierra
+  end
+
   depends_on "oath-toolkit"
   depends_on "pass"
 
