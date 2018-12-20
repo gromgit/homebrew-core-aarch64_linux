5ddd5d9e96c21de29036dfd0e1b7fea344039278
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-20 16:40:51 +0000

    libcouchbase: update 2.10.3 bottle.

--- Formula/libcouchbase.rb
+++ Formula/libcouchbase.rb
@@ -6,9 +6,9 @@ class Libcouchbase < Formula
   head "https://github.com/couchbase/libcouchbase.git"
 
   bottle do
-    sha256 "3318ef1004b7a30355ea9f7e5f85fddc67d0f126899abb9376e2bcb69ad273c8" => :mojave
-    sha256 "7b8911d3027662f3903f2d741057a9bbf1c99374fc672e07207897dbe8a0c52e" => :high_sierra
-    sha256 "11c68b0215d3a382affc23dd93839a5b22b949eb29493397fddc7c8f478b5639" => :sierra
+    sha256 "713b002219e8982e08d95afde3aebf9d3c8795d117851121078e6aff031e6370" => :mojave
+    sha256 "06b4d45bf1788071f5ee30358dbbfd1a7217e2a23bc84810714e8cb87c1270f2" => :high_sierra
+    sha256 "6b1c1f546afc5d4d65dc8d1685de6410655414f3810280af3c85b9f31e0a9470" => :sierra
   end
 
   depends_on "cmake" => :build
