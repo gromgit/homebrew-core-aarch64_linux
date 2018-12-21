fabe1244e5048f382f31df1f329d4ff283960e7e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-21 05:14:56 +0000

    kubeseal: add 0.7.0 bottle.

--- Formula/kubeseal.rb
+++ Formula/kubeseal.rb
@@ -4,6 +4,13 @@ class Kubeseal < Formula
   url "https://github.com/bitnami-labs/sealed-secrets/archive/v0.7.0.tar.gz"
   sha256 "9254c11ecbb55a87fd6cfc6025a1a41d6fb7ad6bb11d248a3e6c2f9ee9cc74bd"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "99ee78122f26f43c836cf29fa746138997896df6b4a01bd3dce9a4b6525cb267" => :mojave
+    sha256 "2df510d796d6d1a0b58bc5786e659efb5a8b497f7aa0219d9417fbcbe89c09a0" => :high_sierra
+    sha256 "e2f578be802be280ee0d6ce581a5a357b2c48cba0d2d5f8ba2ab11aafd8c5f82" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
