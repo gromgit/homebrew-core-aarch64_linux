2c362945915c0124fd38756487286e33e37b23fc
Igor Kapkov <igasgeek@me.com>
2018-12-21 16:21:40 +1100

    z3 4.8.4
    Closes #35314.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/z3.rb
+++ Formula/z3.rb
@@ -1,8 +1,8 @@
 class Z3 < Formula
   desc "High-performance theorem prover"
   homepage "https://github.com/Z3Prover/z3"
-  url "https://github.com/Z3Prover/z3/archive/z3-4.8.3.tar.gz"
-  sha256 "21620b68c373cdea0d3b2cf24020be4ecfb22eddc6629663f6e9ce31cfdc78de"
+  url "https://github.com/Z3Prover/z3/archive/z3-4.8.4.tar.gz"
+  sha256 "5a18fe616c2a30b56e5b2f5b9f03f405cdf2435711517ff70b076a01396ef601"
   head "https://github.com/Z3Prover/z3.git"
 
   bottle do
