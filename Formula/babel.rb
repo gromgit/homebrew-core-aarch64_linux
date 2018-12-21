07b59e1badb1fbd59cfd3804579c2f0db95f2a60
Chongyu Zhu <i@lembacon.com>
2018-12-21 08:12:50 +0800

    babel 7.2.3
    Closes #35304.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/babel.rb
+++ Formula/babel.rb
@@ -4,8 +4,8 @@ require "json"
 class Babel < Formula
   desc "Compiler for writing next generation JavaScript"
   homepage "https://babeljs.io/"
-  url "https://registry.npmjs.org/@babel/cli/-/cli-7.2.0.tgz"
-  sha256 "151d36d3069dde1eb4aea9ae5501d969edaa99ee46b6cbb8fd591562833e8651"
+  url "https://registry.npmjs.org/@babel/cli/-/cli-7.2.3.tgz"
+  sha256 "c11ee392ed2d7ee30126028c5daccc5d98ae6ba4753ae693114591101c98c144"
 
   bottle do
     sha256 "9bebd39e19923716ca500d25cd0e84158ff8aeb1f2700f23a92e656e881207de" => :mojave
@@ -16,8 +16,8 @@ class Babel < Formula
   depends_on "node"
 
   resource "babel-core" do
-    url "https://registry.npmjs.org/@babel/core/-/core-7.2.0.tgz"
-    sha256 "7f60c1a95f03c86b0e9329989045f460c3fb86ce6d4f545327b6091f1ca37eae"
+    url "https://registry.npmjs.org/@babel/core/-/core-7.2.2.tgz"
+    sha256 "b632e5a565b000645d7bc0010331773481cc5dee7d4360360dccaa9e51c4785e"
   end
 
   def install
@@ -41,6 +41,8 @@ class Babel < Formula
     system bin/"babel", "script.js", "--out-file", "script-compiled.js"
     assert_predicate testpath/"script-compiled.js", :exist?, "script-compiled.js was not generated"
 
-    assert_equal version, resource("babel-core").version
+    # Uncomment this for the next release,
+    # see https://github.com/Homebrew/homebrew-core/pull/35304#issuecomment-449335627.
+    # assert_equal version, resource("babel-core").version
   end
 end
