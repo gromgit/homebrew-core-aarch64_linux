d795f4a0f5497d64eb1d8f4fd80b35f0b7c8a73e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-21 20:59:12 +0000

    anycable-go: add 0.6.1 bottle.

--- Formula/anycable-go.rb
+++ Formula/anycable-go.rb
@@ -4,6 +4,13 @@ class AnycableGo < Formula
   url "https://github.com/anycable/anycable-go/archive/v0.6.1.tar.gz"
   sha256 "609e09d3f6f37900ee02590efe75e5792c1fda52bce4bbb4075d2523c8fbd3bf"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7135382ab3246162a827b707e4eb1b6197d061b06d83fc4b580b8e9bec8f3ca5" => :mojave
+    sha256 "5ee790ab1c455cb125e1c3a52e4cbb47426c6a5bd51f74f00853bebd75ed9523" => :high_sierra
+    sha256 "03f1dc7e44d72166e75de663a9506fb6de48d2c60a1331dc60d9184c4f884358" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
