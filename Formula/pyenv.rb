95c502732d25f0953fe0cee2a434d9c4230dc7a7
Tsukasa OMOTO <henry0312@gmail.com>
2018-12-21 14:57:57 +0900

    pyenv: fix shell completions
    Closes #35320.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/pyenv.rb
+++ Formula/pyenv.rb
@@ -30,9 +30,9 @@ class Pyenv < Formula
       bin.install_symlink "#{prefix}/plugins/python-build/bin/#{cmd}"
     end
 
-    bash_completion.install "#{prefix}/completions/pyenv.bash"
-    zsh_completion.install "#{prefix}/completions/pyenv.zsh"
-    fish_completion.install "#{prefix}/completions/pyenv.fish"
+    # Do not manually install shell completions. See:
+    #   - https://github.com/pyenv/pyenv/issues/1056#issuecomment-356818337
+    #   - https://github.com/Homebrew/homebrew-core/pull/22727
   end
 
   test do
