694599465186d0f3ef2f2f4c3faa67b2e12799ed
Chongyu Zhu <i@lembacon.com>
2018-12-22 10:54:14 +0800

    parallel 20181222
    Closes #35341.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/parallel.rb
+++ Formula/parallel.rb
@@ -1,9 +1,9 @@
 class Parallel < Formula
   desc "Shell command parallelization utility"
   homepage "https://savannah.gnu.org/projects/parallel/"
-  url "https://ftp.gnu.org/gnu/parallel/parallel-20181122.tar.bz2"
-  mirror "https://ftpmirror.gnu.org/parallel/parallel-20181122.tar.bz2"
-  sha256 "2f06580c833ca30f434511ea28a0c097e75e3ee35a87b3a58512bd6d5cf598d5"
+  url "https://ftp.gnu.org/gnu/parallel/parallel-20181222.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/parallel/parallel-20181222.tar.bz2"
+  sha256 "058c9f7cf599652aef5b54cf5c95c9a5b6d57d59ce4deebebf7938f7be4da369"
   head "https://git.savannah.gnu.org/git/parallel.git"
 
   bottle do
