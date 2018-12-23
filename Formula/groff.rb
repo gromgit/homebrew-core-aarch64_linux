d3add287809f156e87f63fec00aedaa6fe450c1f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-23 16:15:39 +0000

    groff: update 1.22.4 bottle.

--- Formula/groff.rb
+++ Formula/groff.rb
@@ -6,10 +6,9 @@ class Groff < Formula
   sha256 "e78e7b4cb7dec310849004fa88847c44701e8d133b5d4c13057d876c1bad0293"
 
   bottle do
-    rebuild 1
-    sha256 "117230db80bea766e9bdd3f0af02911d824ac333a14c466762ef475dc7ffc5bb" => :mojave
-    sha256 "cbcd60c91851bfeb7d32d292bc2f1838ee130b1e9b87c4bac535142b7c8dc4de" => :high_sierra
-    sha256 "39945f37f43ad6ad93d87469847dff4d75f720a9209c0e4c5596c61eb611b6ae" => :sierra
+    sha256 "1ee2ce419f4d59f098f0804e1dea42524ef72a88b69ce891c42f13d5f19be5f9" => :mojave
+    sha256 "24fac4b672946970b70c6e308311e87a6686fed50d4d0909228afb252531065d" => :high_sierra
+    sha256 "2966f4b562c30eb6679d6940b43f4b99b2b625433e6a218489f160eb76c7c360" => :sierra
   end
 
   def install
