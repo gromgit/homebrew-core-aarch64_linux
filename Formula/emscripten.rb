832cd4a775a59317d082a841d488b3472d013356
Igor Kapkov <igasgeek@me.com>
2018-12-25 17:55:27 +1100

    emscripten 1.38.21
    Closes #35431.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/emscripten.rb
+++ Formula/emscripten.rb
@@ -3,17 +3,17 @@ class Emscripten < Formula
   homepage "https://kripken.github.io/emscripten-site/"
 
   stable do
-    url "https://github.com/kripken/emscripten/archive/1.38.20.tar.gz"
-    sha256 "9f486c3b9516a82e2cbc6968d07746ae4bad013e4358ac6f2a5c1bc829ca6700"
+    url "https://github.com/kripken/emscripten/archive/1.38.21.tar.gz"
+    sha256 "f3e6f1e6039256968131bc491ea8c2e1bfc31c41c1ec1370e2f1fae9a8e56faa"
 
     resource "fastcomp" do
-      url "https://github.com/kripken/emscripten-fastcomp/archive/1.38.20.tar.gz"
-      sha256 "23c3944271e3cd15c9ee8998ae8690f8f07b340ecde9ba222e3af039228d2376"
+      url "https://github.com/kripken/emscripten-fastcomp/archive/1.38.21.tar.gz"
+      sha256 "d77aa245747df5d807ab36f91cd27495e5c8d08b0addf6becdc618d80025ba0b"
     end
 
     resource "fastcomp-clang" do
-      url "https://github.com/kripken/emscripten-fastcomp-clang/archive/1.38.20.tar.gz"
-      sha256 "16ff2ec08987d98d1763433f402b158311445d81368984e79aaf9d3d99eb2d68"
+      url "https://github.com/kripken/emscripten-fastcomp-clang/archive/1.38.21.tar.gz"
+      sha256 "a14e445d295c31e998b171eb81f15fab389936b11a548ddb33fc9fb6c425d045"
     end
   end
 
