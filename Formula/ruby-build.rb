92af2775fd60a613cae8f1c8f1b0e5eef97a2db9
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2018-12-25 19:15:45 +0900

    ruby-build 20181225
    Closes #35439.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/ruby-build.rb
+++ Formula/ruby-build.rb
@@ -1,8 +1,8 @@
 class RubyBuild < Formula
   desc "Install various Ruby versions and implementations"
   homepage "https://github.com/rbenv/ruby-build"
-  url "https://github.com/rbenv/ruby-build/archive/v20181207.tar.gz"
-  sha256 "74b4ffac4e8d6e68685afad035c64573280e93dfb2ab81fd7b613085e8a69c23"
+  url "https://github.com/rbenv/ruby-build/archive/v20181225.tar.gz"
+  sha256 "5ace4787ace47384dc419b20f5eb5a59f1174e00bfabcfed74a175033cd0b18a"
   head "https://github.com/rbenv/ruby-build.git"
 
   bottle :unneeded
