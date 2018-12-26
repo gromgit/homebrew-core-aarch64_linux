71372a488de3f258e73e6117117f17f75674782b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-26 03:01:31 +0000

    cocoapods: update 1.5.3_1 bottle.

--- Formula/cocoapods.rb
+++ Formula/cocoapods.rb
@@ -7,10 +7,9 @@ class Cocoapods < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "65f3a3385f039d28d4851d32cfaa248458f7730241fa7f8b8d4981e04ae80ce2" => :mojave
-    sha256 "3a81b6752d559e01b6aaab830aaca5d8e89e946bb2f62793e7fa51a144b3f683" => :high_sierra
-    sha256 "d62738d8793c6de966233eccfc2d9cb08de1a932ff9be672add02b89124be6af" => :sierra
-    sha256 "825dcdb5aed45f84a5bd6c0a7dc43569e9e87ce1e705af3198015a6966697e5e" => :el_capitan
+    sha256 "5e0fa01f44b64e0c230fa768cd211a86d0cf28c7415128cd46b162e90616a199" => :mojave
+    sha256 "e711a156660e3580237f315c2cfa38d5fd54d4211cc471f77de75c9db3f47321" => :high_sierra
+    sha256 "2c50c930d1680af39c7a4f21d2ef3714b04074aa6c3d586d4026925d7e30b452" => :sierra
   end
 
   depends_on "ruby" if MacOS.version <= :sierra
