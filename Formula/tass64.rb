e7d3879331c35fdbe1bd48fd6913170d7727ffad
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-26 22:09:48 +0000

    tass64: add 1.53.1515 bottle.

--- Formula/tass64.rb
+++ Formula/tass64.rb
@@ -4,6 +4,13 @@ class Tass64 < Formula
   url "https://downloads.sourceforge.net/project/tass64/source/64tass-1.53.1515-src.zip"
   sha256 "f18e5d3f7f27231c1f8ce781eee8b585fe5aaec186eccdbdb820c1b8c323eb6c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "662059bcfe282b613ebf0bb9e0e6f0ce4dacd447fdab932ac3b184f45895977c" => :mojave
+    sha256 "3726bb4afcd89279e27bcabd10a662b93c7da52732eca84343240913689f6b75" => :high_sierra
+    sha256 "43a54d0b972b0173af35552d5e6fc497ae09c9ad181ef3f544aed38e50152633" => :sierra
+  end
+
   def install
     system "make", "install", "CPPFLAGS=-D_XOPEN_SOURCE", "prefix=#{prefix}"
 
