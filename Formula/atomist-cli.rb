7b5f72d716035a29a9b4a665b9deee29f7191add
David Dooling <david@atomist.com>
2018-12-26 11:05:54 -0600

    atomist-cli 1.1.0 (new formula)
    Closes #35463.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/atomist-cli.rb
@@ -0,0 +1,36 @@
+require "language/node"
+
+class AtomistCli < Formula
+  desc "The Atomist CLI"
+  homepage "https://github.com/atomist/cli#readme"
+  url "https://registry.npmjs.org/@atomist/cli/-/@atomist/cli-1.1.0.tgz"
+  sha256 "bdc37fc2216745b1156f7038de66f755a355fc9328ee72ec53630d95c1b22be9"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+    bash_completion.install "#{libexec}/lib/node_modules/@atomist/cli/assets/bash_completion/atomist"
+  end
+
+  test do
+    assert_predicate bin/"atomist", :exist?
+    assert_predicate bin/"atomist", :executable?
+    assert_predicate bin/"@atomist", :exist?
+    assert_predicate bin/"@atomist", :executable?
+
+    run_output = shell_output("#{bin}/atomist 2>&1")
+    assert_match "Not enough non-option arguments", run_output
+    assert_match "Specify --help for available options", run_output
+
+    version_output = shell_output("#{bin}/atomist --version")
+    assert_match "@atomist/cli", version_output
+    assert_match "@atomist/sdm ", version_output
+    assert_match "@atomist/sdm-core", version_output
+    assert_match "@atomist/sdm-local", version_output
+
+    skill_output = shell_output("#{bin}/atomist show skills")
+    assert_match(/\d+ commands are available from \d+ connected SDMs/, skill_output)
+  end
+end
