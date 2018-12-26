599c0d8e680acbd1b09def2f6450d69d4d6ea14d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-26 07:39:42 +0000

    cointop: update 1.1.1 bottle.

--- Formula/cointop.rb
+++ Formula/cointop.rb
@@ -6,10 +6,9 @@ class Cointop < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "92f04b4718cddb3a07984ff8636f57032a1bf03790f024a5c30842e820618c37" => :mojave
-    sha256 "d325a2cabff5f820bfa29b1b0bf5f8fe95e10aa0910a41a26b4696e00525349d" => :high_sierra
-    sha256 "4d3688ea6e5da782ed7fb3f0fdc37f6dea31a6d213bdc826cbba9e0cc6762199" => :sierra
-    sha256 "bc0d504ddde42b525af627d5e4b7f9041e8d49179dd822ccedd46f673822fc65" => :el_capitan
+    sha256 "221ff054d2034edf7a53fe0a17cd49c884bd094fceabdf66d9bd1704cfc62ca1" => :mojave
+    sha256 "d85db44fb5a9691991d0a413368cda6e99911bb92ab80437933c98673c2f82f4" => :high_sierra
+    sha256 "0f0b9038f212eeb29b14693f05cc5cbdb48ae40f26f159d92d0744fdbc1fc27b" => :sierra
   end
 
   depends_on "go" => :build
