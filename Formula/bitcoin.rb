f2c9eb18588efe62a6ad8d27b25d8573beffcd10
mecampbellsoup <mecampbell25@gmail.com>
2018-12-26 11:14:59 -0500

    bitcoin 0.17.1
    Closes #35461.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/bitcoin.rb
+++ Formula/bitcoin.rb
@@ -1,9 +1,8 @@
 class Bitcoin < Formula
   desc "Decentralized, peer to peer payment network"
   homepage "https://bitcoin.org/"
-  url "https://bitcoin.org/bin/bitcoin-core-0.17.0/bitcoin-0.17.0.tar.gz"
-  sha256 "98ef39e851644bba1ac51edaa53c7c47c20a3980333def7bc642c8261a12785e"
-  revision 1
+  url "https://bitcoin.org/bin/bitcoin-core-0.17.1/bitcoin-0.17.1.tar.gz"
+  sha256 "3e564fb5cf832f39e930e19c83ea53e09cfe6f93a663294ed83a32e194bda42a"
 
   bottle do
     cellar :any
