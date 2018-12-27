25f780758a87864e0dd6a6dad71b85f826069757
FX Coudert <fxcoudert@gmail.com>
2018-12-27 21:16:34 +0100

    itstool 2.0.5

--- Formula/itstool.rb
+++ Formula/itstool.rb
@@ -1,19 +1,9 @@
 class Itstool < Formula
   desc "Make XML documents translatable through PO files"
   homepage "http://itstool.org/"
-  revision 1
-
-  stable do
-    url "http://files.itstool.org/itstool/itstool-2.0.4.tar.bz2"
-    sha256 "97c208b51da33e0b553e830b92655f8deb9132f8fbe9a646771f95c33226eb60"
-
-    # Upstream commit from 25 Oct 2017 "Be more careful about libxml2 memory management"
-    # See https://github.com/itstool/itstool/issues/17
-    patch do
-      url "https://github.com/itstool/itstool/commit/9b84c00.patch?full_index=1"
-      sha256 "c33f44affc27604c6a91a8ae2e992273bf588c228e635ea46d958e2c3046e9ca"
-    end
-  end
+  url "https://github.com/itstool/itstool/archive/2.0.5.tar.gz"
+  sha256 "97f98e1a5f8f49239e4256570ecfe12caf88b7cfdb4fcb40f4b761ce7ea2a8c3"
+  head "https://github.com/itstool/itstool.git"
 
   bottle do
     cellar :any_skip_relocation
@@ -23,21 +13,17 @@ class Itstool < Formula
     sha256 "9dc3edc35150bd1701f9107b2248a5b275d1842447aa58f77341c4af8e478d7e" => :el_capitan
   end
 
-  head do
-    url "https://github.com/itstool/itstool.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-  end
-
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "libxml2"
-  depends_on "python@2"
+  depends_on "python"
 
   def install
-    ENV.append_path "PYTHONPATH", "#{Formula["libxml2"].opt_lib}/python2.7/site-packages"
+    xy = Language::Python.major_minor_version "python3"
+    ENV.append_path "PYTHONPATH", "#{Formula["libxml2"].opt_lib}/python#{xy}/site-packages"
 
-    system "./autogen.sh" if build.head?
-    system "./configure", "--prefix=#{libexec}"
+    system "./autogen.sh", "--prefix=#{libexec}",
+                           "PYTHON=#{Formula["python"].opt_bin}/python3"
     system "make", "install"
 
     bin.install Dir["#{libexec}/bin/*"]
