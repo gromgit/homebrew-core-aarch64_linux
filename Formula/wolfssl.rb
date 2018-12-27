4ec74476c60b1375353b0c3b6224baa06835f0c6
Igor Kapkov <igasgeek@me.com>
2018-12-27 18:53:54 +1100

    wolfssl 3.15.7
    Closes #35472.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/wolfssl.rb
+++ Formula/wolfssl.rb
@@ -2,8 +2,8 @@ class Wolfssl < Formula
   desc "Embedded SSL Library written in C"
   homepage "https://www.wolfssl.com/wolfSSL/Home.html"
   url "https://github.com/wolfSSL/wolfssl.git",
-      :tag      => "v3.15.6",
-      :revision => "249306f08c77943c9144a2d3abf3b513b0a0f49b"
+      :tag      => "v3.15.7-stable",
+      :revision => "a7350b7d2ed15648443253cb39ad9195df184139"
   sha256 "4e15f494604e41725499f8b708798f8ddc2fcaa8f39b4369bcd000b3cab482d8"
   head "https://github.com/wolfSSL/wolfssl.git"
 
