3888a218cc6f024fcd292f3dcaceb7131a2a5dde
Ben Chatelain <ben@octop.ad>
2018-12-27 17:07:11 -0700

    mas 1.5.0
    Closes #35503.
    
    Signed-off-by: Igor Kapkov <igasgeek@me.com>

--- Formula/mas.rb
+++ Formula/mas.rb
@@ -2,8 +2,8 @@ class Mas < Formula
   desc "Mac App Store command-line interface"
   homepage "https://github.com/mas-cli/mas"
   url "https://github.com/mas-cli/mas.git",
-      :tag      => "v1.4.4",
-      :revision => "6096d231196bafdfba3a8ac325455b057c5898a4"
+      :tag      => "v1.5.0",
+      :revision => "ccaaa74c9593d04dc41fcff40af196fdad49f517"
   head "https://github.com/mas-cli/mas.git"
 
   bottle do
@@ -13,11 +13,13 @@ class Mas < Formula
   end
 
   depends_on "carthage" => :build
-  depends_on :xcode => ["10.0", :build]
+  depends_on :xcode => ["10.1", :build]
+  depends_on "trash"
 
   def install
-    # Prevent warnings from causing build failures
-    # Prevent linker errors by telling all lib builds to use max size install names
+    # Working around build issues in dependencies
+    # - Prevent warnings from causing build failures
+    # - Prevent linker errors by telling all lib builds to use max size install names
     xcconfig = buildpath/"Overrides.xcconfig"
     xcconfig.write <<~EOS
       GCC_TREAT_WARNINGS_AS_ERRORS = NO
@@ -26,20 +28,12 @@ class Mas < Formula
     ENV["XCODE_XCCONFIG_FILE"] = xcconfig
 
     system "carthage", "bootstrap", "--platform", "macOS"
-
-    xcodebuild "install",
-               "-project", "mas-cli.xcodeproj",
-               "-scheme", "mas-cli Release",
-               "-configuration", "Release",
-               "OBJROOT=build",
-               "SYMROOT=build"
-
     system "script/install", prefix
 
     bash_completion.install "contrib/completion/mas-completion.bash" => "mas"
   end
 
   test do
-    assert_equal version.to_s, shell_output("#{bin}/mas version").chomp
+    assert_include shell_output("#{bin}/mas info 497799835"), "By: Apple Inc."
   end
 end
