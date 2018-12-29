523589bf5b03053e40fb28681a5eac42655ce4cc
Viacheslav Ostrovskiy <chelout@gmail.com>
2018-12-29 23:31:21 +0300

    mysql 8.0.13
    Closes #35544.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/mysql.rb
+++ Formula/mysql.rb
@@ -1,8 +1,8 @@
 class Mysql < Formula
   desc "Open source relational database management system"
   homepage "https://dev.mysql.com/doc/refman/8.0/en/"
-  url "https://cdn.mysql.com/Downloads/MySQL-8.0/mysql-boost-8.0.12.tar.gz"
-  sha256 "99abae6660b53a462cff7c9fefb56d17f52823e9a964831aee1ae5633d9a2982"
+  url "https://cdn.mysql.com/Downloads/MySQL-8.0/mysql-boost-8.0.13.tar.gz"
+  sha256 "61f97906050c2a0cc008be347f70c2c6612425c85342466f549088c570b35ff4"
 
   bottle do
     rebuild 1
@@ -45,8 +45,8 @@ class Mysql < Formula
     # -DINSTALL_* are relative to `CMAKE_INSTALL_PREFIX` (`prefix`)
     args = %W[
       -DCOMPILATION_COMMENT=Homebrew
-      -DDEFAULT_CHARSET=utf8
-      -DDEFAULT_COLLATION=utf8_general_ci
+      -DDEFAULT_CHARSET=utf8mb4
+      -DDEFAULT_COLLATION=utf8mb4_general_ci
       -DINSTALL_DOCDIR=share/doc/#{name}
       -DINSTALL_INCLUDEDIR=include/mysql
       -DINSTALL_INFODIR=share/info
