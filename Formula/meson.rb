0c4031d5fbd76f38d86b58fa8a5da276fc5559e3
Luca Weiss <luca@z3ntu.xyz>
2018-12-29 14:46:04 +0100

    meson: apply patch to fix Qt detection
    Closes #35540.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/meson.rb
+++ Formula/meson.rb
@@ -3,6 +3,7 @@ class Meson < Formula
   homepage "https://mesonbuild.com/"
   url "https://github.com/mesonbuild/meson/releases/download/0.49.0/meson-0.49.0.tar.gz"
   sha256 "fb0395c4ac208eab381cd1a20571584bdbba176eb562a7efa9cb17cace0e1551"
+  revision 1
   head "https://github.com/mesonbuild/meson.git"
 
   bottle do
@@ -14,6 +15,13 @@ class Meson < Formula
 
   depends_on "ninja"
   depends_on "python"
+
+  # Fix issues with Qt, remove in 0.49.1
+  # https://github.com/mesonbuild/meson/pull/4652
+  patch do
+    url "https://github.com/mesonbuild/meson/commit/c1e416ff.patch?full_index=1"
+    sha256 "3be708cc65d2b6e54d01e64031c83b06abad2eca1c658b97b2230d1aa7d1062b"
+  end
 
   def install
     version = Language::Python.major_minor_version("python3")
