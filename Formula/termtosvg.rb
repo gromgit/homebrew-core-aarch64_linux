2b57ca77a58ed85664788ec71d966f39243b3849
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-30 20:31:02 +0000

    termtosvg: add 0.7.0 bottle.

--- Formula/termtosvg.rb
+++ Formula/termtosvg.rb
@@ -6,6 +6,13 @@ class Termtosvg < Formula
   url "https://github.com/nbedos/termtosvg/archive/0.7.0.tar.gz"
   sha256 "a90cbd2ea29e1bdf4059aa8feb8f19e9f32da5620b447289ac78cde38682c7c2"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b6feb245add3b66176dc35cba270d818d6ab9404fad35b820b6d896f3c03b3f0" => :mojave
+    sha256 "d225f3c7c759e062d1e54e201455c7849c900f4c9431cde19218c3075316d02b" => :high_sierra
+    sha256 "e466333346009ad6a43efdcf1c55a1d3b4c72811046e80e67918f9dff7848d22" => :sierra
+  end
+
   depends_on "python"
 
   def install
