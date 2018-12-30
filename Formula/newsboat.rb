f36a9902b508abbad2b282faa21d570a8f579511
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-30 20:23:57 +0000

    newsboat: update 2.14 bottle.

--- Formula/newsboat.rb
+++ Formula/newsboat.rb
@@ -6,9 +6,9 @@ class Newsboat < Formula
   head "https://github.com/newsboat/newsboat.git"
 
   bottle do
-    sha256 "dfede6575e701116cc059743f58c014ad6adcb6995dbd2cec194b6ae1ef8fee6" => :mojave
-    sha256 "d83a1212683139dd4c5858f8236b71e70ba38724b8f20abb5b1025e1c0e94c82" => :high_sierra
-    sha256 "60cea17e5dd6e8578196aa34266f058521f5f72b279e2ba6c93b1cdcad035baa" => :sierra
+    sha256 "a9ea0ec030fa5247224ad2aaf39d355d1b89251e1786d9bd457e1773aabd9478" => :mojave
+    sha256 "3b626ed21880078c5de05fd69e7228d45b30427e88b05ca797f5f48c1319a33a" => :high_sierra
+    sha256 "c150c11f1f1d6b500e863403ab3d6fda66af62fc6507edeca8fbfbc1f1577107" => :sierra
   end
 
   depends_on "asciidoc" => :build
