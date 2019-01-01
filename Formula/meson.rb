e68ac40614fe09f78f20e6b024212f5f9e9997e8
Luca Weiss <luca@z3ntu.xyz>
2019-01-01 11:47:57 +0100

    meson: scope patch (#35587)

--- Formula/meson.rb
+++ Formula/meson.rb
@@ -1,10 +1,20 @@
 class Meson < Formula
   desc "Fast and user friendly build system"
   homepage "https://mesonbuild.com/"
-  url "https://github.com/mesonbuild/meson/releases/download/0.49.0/meson-0.49.0.tar.gz"
-  sha256 "fb0395c4ac208eab381cd1a20571584bdbba176eb562a7efa9cb17cace0e1551"
   revision 1
   head "https://github.com/mesonbuild/meson.git"
+
+  stable do
+    url "https://github.com/mesonbuild/meson/releases/download/0.49.0/meson-0.49.0.tar.gz"
+    sha256 "fb0395c4ac208eab381cd1a20571584bdbba176eb562a7efa9cb17cace0e1551"
+
+    # Fix issues with Qt, remove in 0.49.1
+    # https://github.com/mesonbuild/meson/pull/4652
+    patch do
+      url "https://github.com/mesonbuild/meson/commit/c1e416ff.patch?full_index=1"
+      sha256 "3be708cc65d2b6e54d01e64031c83b06abad2eca1c658b97b2230d1aa7d1062b"
+    end
+  end
 
   bottle do
     cellar :any_skip_relocation
@@ -15,13 +25,6 @@ class Meson < Formula
 
   depends_on "ninja"
   depends_on "python"
-
-  # Fix issues with Qt, remove in 0.49.1
-  # https://github.com/mesonbuild/meson/pull/4652
-  patch do
-    url "https://github.com/mesonbuild/meson/commit/c1e416ff.patch?full_index=1"
-    sha256 "3be708cc65d2b6e54d01e64031c83b06abad2eca1c658b97b2230d1aa7d1062b"
-  end
 
   def install
     version = Language::Python.major_minor_version("python3")
