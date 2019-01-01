8c48b70bda856d44e5f48c6d2e57e20f156d1be7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-01 03:17:57 +0000

    nnn: update 2.2 bottle.

--- Formula/nnn.rb
+++ Formula/nnn.rb
@@ -7,9 +7,9 @@ class Nnn < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "56012aba00b1e0595bb10e80b5f8726f304b5ec066b642b456e227a4ac3082a9" => :mojave
-    sha256 "2da013eba898c6367c940b77e70eb8ae176788f3522d6ff292896dd0ce563329" => :high_sierra
-    sha256 "1b35c3ee88b07b0ff98a38fb0aff3811212d92ccd4bee2ff68237bcc8cf52b55" => :sierra
+    sha256 "f678ffcf42043c17dd4aaf4857d5d6cb738ae50e35485b81eca6382b2c1562a0" => :mojave
+    sha256 "4337e0b9968a516f0518eb722f0757d34330d5967fec6a69840ce91b0592d6ac" => :high_sierra
+    sha256 "f4d378f77db6f6b0cfaf65b530e6e0ed6cf39b58ce1924981ae60fc7f2d5f548" => :sierra
   end
 
   depends_on "readline"
