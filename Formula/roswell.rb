54bdcaed5b39445aef95f5ca21033240bf60edb3
Igor Kapkov <igasgeek@me.com>
2019-01-02 15:15:36 +1100

    roswell 19.1.10.96
    Closes #35617.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/roswell.rb
+++ Formula/roswell.rb
@@ -1,8 +1,8 @@
 class Roswell < Formula
   desc "Lisp installer and launcher for major environments"
   homepage "https://github.com/roswell/roswell"
-  url "https://github.com/roswell/roswell/archive/v18.10.10.95.tar.gz"
-  sha256 "9ad88ae0aed94feda679527f6ceb2732c39fd645921e91c048442be9874fff67"
+  url "https://github.com/roswell/roswell/archive/v19.1.10.96.tar.gz"
+  sha256 "e295f63f37c1d479af97d711fef925e2177066c2491be51c1fe0ab098cab9dee"
   head "https://github.com/roswell/roswell.git"
 
   bottle do
