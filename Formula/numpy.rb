f1fb8672c2a5c4e7bf9aefe910ff1d335ddd0965
Igor Kapkov <igasgeek@me.com>
2019-01-02 17:21:32 +1100

    numpy: revision bump for openblas

--- Formula/numpy.rb
+++ Formula/numpy.rb
@@ -3,7 +3,7 @@ class Numpy < Formula
   homepage "https://www.numpy.org/"
   url "https://files.pythonhosted.org/packages/2d/80/1809de155bad674b494248bcfca0e49eb4c5d8bee58f26fe7a0dd45029e2/numpy-1.15.4.zip"
   sha256 "3d734559db35aa3697dadcea492a423118c5c55d176da2f3be9c98d4803fc2a7"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
