768a6edb4da68b587d98757a7adf14cbc12b0a08
Michka Popoff <michkapopoff@gmail.com>
2019-01-03 13:21:41 +0100

    exiv2 0.27.0

--- Formula/exiv2.rb
+++ Formula/exiv2.rb
@@ -1,8 +1,9 @@
 class Exiv2 < Formula
   desc "EXIF and IPTC metadata manipulation library and tools"
   homepage "http://www.exiv2.org/"
-  url "http://www.exiv2.org/builds/exiv2-0.26-trunk.tar.gz"
-  sha256 "c75e3c4a0811bf700d92c82319373b7a825a2331c12b8b37d41eb58e4f18eafb"
+  url "http://www.exiv2.org/builds/exiv2-0.27.0a-Source.tar.gz"
+  sha256 "a4adfa7aaf295b0383adead476f8e0493b9d6c6c7570d5884d2ebf8a2871902f"
+  head "https://github.com/Exiv2/exiv2.git"
 
   bottle do
     cellar :any
@@ -14,44 +15,30 @@ class Exiv2 < Formula
     sha256 "1d14797afa32ff75b50ff2737baa8ac27ab7bf90da38359a9721f7e15c398481" => :yosemite
   end
 
-  head do
-    url "https://github.com/Exiv2/exiv2.git"
-    depends_on "cmake" => :build
-    depends_on "gettext" => :build
-    depends_on "libssh"
-  end
+  depends_on "cmake" => :build
+  depends_on "gettext"
+  depends_on "libssh"
 
   def install
-    if build.head?
-      args = std_cmake_args
-      args += %W[
-        -DEXIV2_ENABLE_SHARED=ON
-        -DEXIV2_ENABLE_XMP=ON
-        -DEXIV2_ENABLE_LIBXMP=ON
-        -DEXIV2_ENABLE_VIDEO=ON
-        -DEXIV2_ENABLE_PNG=ON
-        -DEXIV2_ENABLE_NLS=ON
-        -DEXIV2_ENABLE_PRINTUCS2=ON
-        -DEXIV2_ENABLE_LENSDATA=ON
-        -DEXIV2_ENABLE_COMMERCIAL=OFF
-        -DEXIV2_ENABLE_BUILD_SAMPLES=ON
-        -DEXIV2_ENABLE_BUILD_PO=ON
-        -DEXIV2_ENABLE_VIDEO=ON
-        -DEXIV2_ENABLE_WEBREADY=ON
-        -DEXIV2_ENABLE_CURL=ON
-        -DEXIV2_ENABLE_SSH=ON
-        -DSSH_LIBRARY=#{Formula["libssh"].opt_lib}/libssh.dylib
-        -DSSH_INCLUDE_DIR=#{Formula["libssh"].opt_include}
-        ..
-      ]
-      mkdir "build.cmake" do
-        system "cmake", "-G", "Unix Makefiles", ".", *args
-        system "make", "install"
-        # `-DCMAKE_INSTALL_MANDIR=#{man}` doesn't work
-        mv prefix/"man", man
-      end
-    else
-      system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    args = std_cmake_args
+    args += %W[
+      -DEXIV2_ENABLE_XMP=ON
+      -DEXIV2_ENABLE_VIDEO=ON
+      -DEXIV2_ENABLE_PNG=ON
+      -DEXIV2_ENABLE_NLS=ON
+      -DEXIV2_ENABLE_PRINTUCS2=ON
+      -DEXIV2_ENABLE_LENSDATA=ON
+      -DEXIV2_ENABLE_VIDEO=ON
+      -DEXIV2_ENABLE_WEBREADY=ON
+      -DEXIV2_ENABLE_CURL=ON
+      -DEXIV2_ENABLE_SSH=ON
+      -DEXIV2_BUILD_SAMPLES=OFF
+      -DSSH_LIBRARY=#{Formula["libssh"].opt_lib}/libssh.dylib
+      -DSSH_INCLUDE_DIR=#{Formula["libssh"].opt_include}
+      ..
+    ]
+    mkdir "build.cmake" do
+      system "cmake", "-G", "Unix Makefiles", ".", *args
       system "make", "install"
     end
   end
