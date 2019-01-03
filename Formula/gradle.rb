b113e0dd77942be1efead43dff26cc08bc8a22b7
Svyatoslav Chatchenko <mydogtom@gmail.com>
2019-01-03 11:16:02 +0100

    gradle 5.1 (#35659)

--- Formula/gradle.rb
+++ Formula/gradle.rb
@@ -1,19 +1,16 @@
 class Gradle < Formula
   desc "Open-source build automation tool based on the Groovy and Kotlin DSL"
   homepage "https://www.gradle.org/"
-  url "https://services.gradle.org/distributions/gradle-5.0-all.zip"
-  sha256 "17847c8e12b2bcfce26a79f425f082c31d4ded822f99a66127eee2d96bf18216"
+  url "https://services.gradle.org/distributions/gradle-5.1-all.zip"
+  sha256 "7b8a8b9cce0406733d2d3fab3874386c530657c73c3f4e9a3837d081e26060d8"
 
   bottle :unneeded
-
-  option "with-all", "Installs Javadoc, examples, and source in addition to the binaries"
 
   depends_on :java => "1.7+"
 
   def install
     rm_f Dir["bin/*.bat"]
-    libexec.install %w[bin lib]
-    libexec.install %w[docs media samples src] if build.with? "all"
+    libexec.install %w[bin docs lib media samples src]
     (bin/"gradle").write_env_script libexec/"bin/gradle", Language::Java.overridable_java_home_env
   end
 
