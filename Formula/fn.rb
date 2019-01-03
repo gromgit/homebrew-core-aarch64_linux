ac5b59ca551edd10b61c03067fbafac7a5b7d1bf
Igor Kapkov <igasgeek@me.com>
2019-01-03 17:59:56 +1100

    fn 0.5.40
    Closes #35651.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/fn.rb
+++ Formula/fn.rb
@@ -1,8 +1,8 @@
 class Fn < Formula
   desc "Command-line tool for the fn project"
   homepage "https://fnproject.io"
-  url "https://github.com/fnproject/cli/archive/0.5.39.tar.gz"
-  sha256 "ac2dcd74255be22997e4c3b484d46bdd0b7a99c9572cb744833f3f7f4cbe5923"
+  url "https://github.com/fnproject/cli/archive/0.5.40.tar.gz"
+  sha256 "dc250e5dda67c222b878621494a5258eb131171d63e6c907549a9f44a6f0b3e3"
 
   bottle do
     cellar :any_skip_relocation
