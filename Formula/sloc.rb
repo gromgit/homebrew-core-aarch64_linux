fdf32077147008410f44388048429a459590dc45
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-03 02:31:55 +0000

    sloc: add 0.2.1 bottle.

--- Formula/sloc.rb
+++ Formula/sloc.rb
@@ -6,6 +6,13 @@ class Sloc < Formula
   url "https://registry.npmjs.org/sloc/-/sloc-0.2.1.tgz"
   sha256 "fb56f1763b7dadfd0566f819665efc0725ba8dfbec13c75da3839edf309596e6"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f241a7bf03cb7bb97bb061f5f46442d7a40de893697a5335c821049d471e9466" => :mojave
+    sha256 "345308d671b83edb390c143554c64958135cf37bc7cd365ce613011da682a8b7" => :high_sierra
+    sha256 "1386a024efebe74829d85c8d75d07ae9f09f8c8a8104aa41424a5ea8c425fca5" => :sierra
+  end
+
   depends_on "node"
 
   def install
