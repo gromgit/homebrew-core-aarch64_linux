9e6c35ddd4ec3f89edb395555737571c9fce7b68
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-04 07:02:49 +0000

    rakudo: add 2018.12 bottle.

--- Formula/rakudo.rb
+++ Formula/rakudo.rb
@@ -4,6 +4,12 @@ class Rakudo < Formula
   url "https://rakudo.perl6.org/downloads/rakudo/rakudo-2018.12.tar.gz"
   sha256 "67bb02b9afd4f2a2a8542e25ce2691bc2d77864668f0fc82e1d39dea31a584fa"
 
+  bottle do
+    sha256 "ff76c570e4ee24d0fe5f3cee06994e9acaeb6b6398bca06efab140e7135aeb1a" => :mojave
+    sha256 "17a8570054d279c922068afe4772062f4b47977203b101ee180c3b6908b9115c" => :high_sierra
+    sha256 "fd71fce5030a6b05098d1918306f8cf40f286fe5e98b4f57b2e0f22585bf2ee2" => :sierra
+  end
+
   depends_on "nqp"
 
   def install
