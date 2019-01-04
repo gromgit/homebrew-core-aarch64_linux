c9eab8b130e35d095dd9d1a274599ecb4f3be085
JP Hastings-Spital <jphastings@gmail.com>
2019-01-04 08:57:03 +0000

    libnova 0.15.0 (new formula)
    Closes #35681.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/libnova.rb
@@ -0,0 +1,37 @@
+class Libnova < Formula
+  desc "Celestial mechanics, astrometry and astrodynamics library"
+  homepage "https://libnova.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/libnova/libnova/v%200.15.0/libnova-0.15.0.tar.gz"
+  sha256 "7c5aa33e45a3e7118d77df05af7341e61784284f1e8d0d965307f1663f415bb1"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
+  def install
+    system "autoreconf", "-fiv"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <libnova/julian_day.h>
+
+      int main(void)
+      {
+        double JD;
+
+        JD = ln_get_julian_from_sys();
+        return 0;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lnova", "-o", "test"
+    system "./test"
+  end
+end
