1ff00ddff80a24fd56f3c6fe9b7a9f06666ed55d
Brian Zwahr <echosa@icloud.com>
2019-01-04 11:59:39 -0600

    recutils 1.8
    Closes #35696.
    
    Signed-off-by: Igor Kapkov <igasgeek@me.com>

--- Formula/recutils.rb
+++ Formula/recutils.rb
@@ -1,10 +1,9 @@
 class Recutils < Formula
   desc "Tools to work with human-editable, plain text data files"
   homepage "https://www.gnu.org/software/recutils/"
-  url "https://ftp.gnu.org/gnu/recutils/recutils-1.7.tar.gz"
-  mirror "https://ftpmirror.gnu.org/recutils/recutils-1.7.tar.gz"
-  sha256 "233dc6dedb1916b887de293454da7e36a74bed9ebea364f7e97e74920051bc31"
-  revision 1
+  url "https://ftp.gnu.org/gnu/recutils/recutils-1.8.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gnu/recutils/recutils-1.8.tar.gz"
+  sha256 "df8eae69593fdba53e264cbf4b2307dfb82120c09b6fab23e2dad51a89a5b193"
 
   bottle do
     cellar :any
@@ -12,13 +11,6 @@ class Recutils < Formula
     sha256 "577f81582b338d44b2b231ab7a353ceaa0b01cf899b9a5042924568d95b20ec5" => :high_sierra
     sha256 "13ded271da7be4d07cce7f454d5c6ad871151c08b319f5a1670bdefd882a62da" => :sierra
     sha256 "e11f1ef2e988f963ddf52088c8d50626c4179b4065b19e254315ae75486439c1" => :el_capitan
-  end
-
-  if MacOS.version >= :high_sierra
-    patch :p0 do
-      url "https://raw.githubusercontent.com/macports/macports-ports/b76d1e48dac/editors/nano/files/secure_snprintf.patch"
-      sha256 "57f972940a10d448efbd3d5ba46e65979ae4eea93681a85e1d998060b356e0d2"
-    end
   end
 
   def install
