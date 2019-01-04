4fc4bf16637faeb132b644c414f768cfbd9ab329
Mike McQuaid <mike@mikemcquaid.com>
2019-01-04 20:21:42 +0000

    subversion: replace deprecated scons call.

--- Formula/subversion.rb
+++ Formula/subversion.rb
@@ -78,7 +78,7 @@ class Subversion < Formula
         APU=#{Formula["apr-util"].opt_prefix}
       ]
       scons(*args)
-      scons "install"
+      system "scons", "install"
     end
 
     if build.with? "java"
