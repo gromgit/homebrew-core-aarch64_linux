2a061cb1815f02f9d4faf7b424ede597ee0d1853
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-05 14:22:21 +0000

    hevea: update 2.32 bottle.

--- Formula/hevea.rb
+++ Formula/hevea.rb
@@ -5,10 +5,10 @@ class Hevea < Formula
   sha256 "de55c6809ae077cd9b2cd5c758449907845677258815c96a713764e6e0c59ee8"
 
   bottle do
-    sha256 "0f9ecfeeca0ddc19b5dc05c1439be84c6956fb4f60507d933215f17f8c98c892" => :mojave
-    sha256 "3449e59de08edb173696ded7a21127ea8bb7cae445eb2d0db6e814d5c84ae6bf" => :high_sierra
-    sha256 "ac0e31ca981ad566e978ab954a324aead767b60ebab55be36f081689d3ffd8c3" => :sierra
-    sha256 "b036e8c8233b6800187efaab19171c39f25c4d82b8ba32442abdec688aa61296" => :el_capitan
+    rebuild 1
+    sha256 "f02097ca2ff65b7ee466ba597bb6d1c0a1fbbf381de3c3922cb31150fba941b7" => :mojave
+    sha256 "527209a56a45e2e490978db70e8b2c6343a7e8e283fefeedfc6bb9cfe37843c0" => :high_sierra
+    sha256 "b8b5d9cd3c34fe23a704e143b831284beda8601f6f6d037d37695016dc034a55" => :sierra
   end
 
   depends_on "ocamlbuild" => :build
