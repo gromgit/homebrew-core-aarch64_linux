5034ca01da39634dcee4ecf83bbc037fd6da102a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-05 14:22:18 +0000

    sox: update 14.4.2_2 bottle.

--- Formula/sox.rb
+++ Formula/sox.rb
@@ -7,10 +7,9 @@ class Sox < Formula
 
   bottle do
     cellar :any
-    sha256 "850cd95c4baea2d91467637582f120ab9eff066c7414847512ea9cf49ba218f1" => :mojave
-    sha256 "f1ba26233464499f58cc0cab34c5472d8b9e30a83cbc6e185b451a9476d560f4" => :high_sierra
-    sha256 "de9126013c1bbd7ec728197cc471231cf17c51e7620e995590b8a6ade6c07521" => :sierra
-    sha256 "f3b3ac24d351edb82fcd30119f13f6b86848c62b9130b6d20c8538e8b2b17d89" => :el_capitan
+    sha256 "89c40d6f9fd1c0e081aed9f480a43b77d7c58c1b90ec81f5ca544c1e3b7c7411" => :mojave
+    sha256 "38609dce27c7889e851be81af9d0ab83f4cfe8547b464eac4f55d7208f5d4f38" => :high_sierra
+    sha256 "f3fae1b9652b4dec7d751e4b7c80a88314621bf967a0d503a91e87d27c4d1d82" => :sierra
   end
 
   depends_on "pkg-config" => :build
