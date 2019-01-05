4d40cebf6bcf6f5692134c0ea9637dbaf84cb179
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-05 14:22:16 +0000

    libao: update 1.2.2 bottle.

--- Formula/libao.rb
+++ Formula/libao.rb
@@ -6,11 +6,10 @@ class Libao < Formula
   head "https://git.xiph.org/libao.git"
 
   bottle do
-    sha256 "03cf21b875f62dd6ab2bc410788e2421b5d07481a5f0e900a862f8b327c48b2d" => :mojave
-    sha256 "8d823e6f7d3bf6ae310d84f6509d25370592a1b940a121a83918d1659439e008" => :high_sierra
-    sha256 "91469bf8242cf3115d65f6bf39caa77226fc21840309ee5f3fad009379a6fbec" => :sierra
-    sha256 "d1e17337705d098d76e4bfd4b71f2bb01278a85a87d58ad0711636a2050c9049" => :el_capitan
-    sha256 "a44490fce22700be3b09bc6c75de39d90f7860e2486723bb1fa655e22c0a2771" => :yosemite
+    rebuild 1
+    sha256 "68e4c903f9da763a466dbec546a50d6d835045879b37dc0e5ad6ca0edd7cb6ae" => :mojave
+    sha256 "5ab0149864a6dd0955bc782d4f67dd1c6d1343fcc2ce6983bf4cb3b4ec11d9b1" => :high_sierra
+    sha256 "91d709008100b42cc05e28b835155432ab61beee1a8b8337eff5fe0266cba1a1" => :sierra
   end
 
   depends_on "autoconf" => :build
