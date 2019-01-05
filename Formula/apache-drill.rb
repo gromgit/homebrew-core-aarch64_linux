4dc72624dcc889a78084568def184292e4aeb707
bucweat <bucweat@rushpost.com>
2019-01-05 08:36:05 -0800

    Updated for apache drill 1.15.0 (#35714)
    This version uses the right servers...which didn't work yesterday but appear to work today.

--- Formula/apache-drill.rb
+++ Formula/apache-drill.rb
@@ -1,9 +1,9 @@
 class ApacheDrill < Formula
   desc "Schema-free SQL Query Engine for Hadoop, NoSQL and Cloud Storage"
   homepage "https://drill.apache.org"
-  url "https://www.apache.org/dyn/closer.cgi?path=drill/drill-1.14.0/apache-drill-1.14.0.tar.gz"
-  mirror "https://archive.apache.org/dist/drill/drill-1.14.0/apache-drill-1.14.0.tar.gz"
-  sha256 "1145bdbb723119f271d32daf4cdd77cdeebe88ddcb7d04facd585b715bb5723b"
+  url "https://www.apache.org/dyn/closer.cgi?path=drill/drill-1.15.0/apache-drill-1.15.0.tar.gz"
+  mirror "https://archive.apache.org/dist/drill/drill-1.15.0/apache-drill-1.15.0.tar.gz"
+  sha256 "188c1d0df28e50f0265f4bc3c5871b4e7abc9450a4e5a7dbe7f0b23146bec76b"
 
   bottle :unneeded
 
