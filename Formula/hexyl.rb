2552d2333974aecc914f40eae2a3602b93504c47
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-06 13:40:21 +0000

    hexyl: add 0.3.0 bottle.

--- Formula/hexyl.rb
+++ Formula/hexyl.rb
@@ -4,6 +4,13 @@ class Hexyl < Formula
   url "https://github.com/sharkdp/hexyl/archive/v0.3.0.tar.gz"
   sha256 "7a6b8c6058fa887105dcd5f85b2808b2e0e7557f13b28d6fe802ce5609ff473e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "85b2472bc4724d5cc4ef11f1b8935e911c4b143ccf1c6b202d92400dd8349f7f" => :mojave
+    sha256 "03791f8da7804f151509713f410cbb8fc7cbb0a44da1408fcbba90f4702ab9d5" => :high_sierra
+    sha256 "8629112956e0c522c1b16a947e541757c1079ba4ce99e4aac3d0fdb0dd0eda60" => :sierra
+  end
+
   depends_on "rust" => :build
 
   def install
