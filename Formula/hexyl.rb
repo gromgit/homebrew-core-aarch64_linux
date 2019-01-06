8322ec31e6d63e16aac9db318c28e8acec485103
Yasuaki Uechi <y@uechi.io>
2019-01-06 21:03:05 +0900

    hexyl 0.3.0 (new formula)
    Closes #35741.
    
    Signed-off-by: Igor Kapkov <igasgeek@me.com>

---
+++ Formula/hexyl.rb
@@ -0,0 +1,18 @@
+class Hexyl < Formula
+  desc "Command-line hex viewer"
+  homepage "https://github.com/sharkdp/hexyl"
+  url "https://github.com/sharkdp/hexyl/archive/v0.3.0.tar.gz"
+  sha256 "7a6b8c6058fa887105dcd5f85b2808b2e0e7557f13b28d6fe802ce5609ff473e"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--root", prefix, "--path", "."
+  end
+
+  test do
+    pdf = test_fixtures("test.pdf")
+    output = shell_output("#{bin}/hexyl -n 100 #{pdf}")
+    assert_match "00000000", output
+  end
+end
