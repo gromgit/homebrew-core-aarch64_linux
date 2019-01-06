607a0fe132c3ce357423f3d6074608adbd723a16
Mike McQuaid <mike@mikemcquaid.com>
2019-01-06 14:57:48 +0000

    i2p: remove old code.

--- Formula/i2p.rb
+++ Formula/i2p.rb
@@ -14,7 +14,7 @@ class I2p < Formula
 
     system "java", "-jar", "i2pinstall_#{version}.jar", "-options", "path.conf"
 
-    wrapper_name = "i2psvc-macosx-universal-#{MacOS.prefer_64_bit? ? 64 : 32}"
+    wrapper_name = "i2psvc-macosx-universal-64"
     libexec.install_symlink libexec/wrapper_name => "i2psvc"
     bin.write_exec_script Dir["#{libexec}/{eepget,i2prouter}"]
     man1.install Dir["#{libexec}/man/*"]
