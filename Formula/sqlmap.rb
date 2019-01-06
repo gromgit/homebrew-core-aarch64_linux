2aa1ace563db664f953cb5010af91e801645a13d
Igor Kapkov <igasgeek@me.com>
2019-01-07 00:38:26 +1100

    sqlmap 1.3 (#35737)

--- Formula/sqlmap.rb
+++ Formula/sqlmap.rb
@@ -1,8 +1,8 @@
 class Sqlmap < Formula
   desc "Penetration testing for SQL injection and database servers"
   homepage "http://sqlmap.org"
-  url "https://github.com/sqlmapproject/sqlmap/archive/1.2.11.tar.gz"
-  sha256 "a9d7bfaaf92f19c1ed60b95a08605587d7c898a4d417af93dce53aef37b14ae0"
+  url "https://github.com/sqlmapproject/sqlmap/archive/1.3.tar.gz"
+  sha256 "7ea4684e01956e411960a5fd8a6d5b74b3f5972c2a3c6553fbd1e729819190ff"
   head "https://github.com/sqlmapproject/sqlmap.git"
 
   bottle :unneeded
