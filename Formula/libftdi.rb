90d029caebcf2f6996335c1a7851c6d7a6cda117
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-06 06:55:14 +0000

    libftdi: update 1.4 bottle.

--- Formula/libftdi.rb
+++ Formula/libftdi.rb
@@ -6,12 +6,10 @@ class Libftdi < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "7cc50de6d35d5c7dd6d73a9ff075696dfe82ce57db9464c37b0a35dc0cf8707f" => :mojave
-    sha256 "a28c4f481c8174edfb845492c4155511cac868082fd65cce625b55a82f3b54c0" => :high_sierra
-    sha256 "ef603f79c7791bac035bf1afe21dcea4b6323372140a246f9aa52031b3631f6b" => :sierra
-    sha256 "c023c826c533fd60e664a8f604f54017f84b356e625bbbafb8e5365e4319fa7a" => :el_capitan
-    sha256 "7fc5addb888f58fbbd2a2e2e414ead4819a48b7fff4f6b0ebed274b499a38a40" => :yosemite
+    rebuild 2
+    sha256 "196d095a5381b46c318b9bfba33e9c219b99c9d56d5059ca047acd957132c746" => :mojave
+    sha256 "7fe432b58790def951944e5731cf4f1f8939d6b3e7c68390bed12106d16c52d0" => :high_sierra
+    sha256 "86c84e08da72a016f132fe314f1fec7450e28fae18468d77669961831f6ee246" => :sierra
   end
 
   depends_on "cmake" => :build
