137759d678c2c064cf414f8b75561f2a8acf9ae9
Mike McQuaid <mike@mikemcquaid.com>
2019-01-06 14:57:44 +0000

    eiffelstudio: remove old code.

--- Formula/eiffelstudio.rb
+++ Formula/eiffelstudio.rb
@@ -16,35 +16,25 @@ class Eiffelstudio < Formula
   depends_on "pkg-config" => :build
   depends_on "gtk+"
 
-  def ise_platform
-    if Hardware::CPU.ppc?
-      "macosx-ppc"
-    elsif MacOS.prefer_64_bit?
-      "macosx-x86-64"
-    else
-      "macosx-x86"
-    end
-  end
-
   def install
-    system "./compile_exes", ise_platform
-    system "./make_images", ise_platform
+    system "./compile_exes", "macosx-x86-64"
+    system "./make_images", "macosx-x86-64"
     prefix.install Dir["Eiffel_17.01/*"]
     bin.mkpath
-    env = { :ISE_EIFFEL => prefix, :ISE_PLATFORM => ise_platform }
-    (bin/"ec").write_env_script(prefix/"studio/spec/#{ise_platform}/bin/ec", env)
-    (bin/"ecb").write_env_script(prefix/"studio/spec/#{ise_platform}/bin/ecb", env)
-    (bin/"estudio").write_env_script(prefix/"studio/spec/#{ise_platform}/bin/estudio", env)
-    (bin/"finish_freezing").write_env_script(prefix/"studio/spec/#{ise_platform}/bin/finish_freezing", env)
-    (bin/"compile_all").write_env_script(prefix/"tools/spec/#{ise_platform}/bin/compile_all", env)
-    (bin/"iron").write_env_script(prefix/"tools/spec/#{ise_platform}/bin/iron", env)
-    (bin/"syntax_updater").write_env_script(prefix/"tools/spec/#{ise_platform}/bin/syntax_updater", env)
-    (bin/"vision2_demo").write_env_script(prefix/"vision2_demo/spec/#{ise_platform}/bin/vision2_demo", env)
+    env = { :ISE_EIFFEL => prefix, :"macosx-x86-64" => "macosx-x86-64" }
+    (bin/"ec").write_env_script(prefix/"studio/spec/macosx-x86-64/bin/ec", env)
+    (bin/"ecb").write_env_script(prefix/"studio/spec/macosx-x86-64/bin/ecb", env)
+    (bin/"estudio").write_env_script(prefix/"studio/spec/macosx-x86-64/bin/estudio", env)
+    (bin/"finish_freezing").write_env_script(prefix/"studio/spec/macosx-x86-64/bin/finish_freezing", env)
+    (bin/"compile_all").write_env_script(prefix/"tools/spec/macosx-x86-64/bin/compile_all", env)
+    (bin/"iron").write_env_script(prefix/"tools/spec/macosx-x86-64/bin/iron", env)
+    (bin/"syntax_updater").write_env_script(prefix/"tools/spec/macosx-x86-64/bin/syntax_updater", env)
+    (bin/"vision2_demo").write_env_script(prefix/"vision2_demo/spec/macosx-x86-64/bin/vision2_demo", env)
   end
 
   test do
     # More extensive testing requires the full test suite
     # which is not part of this package.
-    system prefix/"studio/spec/#{ise_platform}/bin/ec", "-version"
+    system prefix/"studio/spec/macosx-x86-64/bin/ec", "-version"
   end
 end
