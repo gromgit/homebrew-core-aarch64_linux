fb7bc4d09d0e219433a0299342336b82656bddc9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-06 22:43:16 +0000

    libvirt-glib: add 2.0.0 bottle.

--- Formula/libvirt-glib.rb
+++ Formula/libvirt-glib.rb
@@ -4,6 +4,12 @@ class LibvirtGlib < Formula
   url "https://libvirt.org/sources/glib/libvirt-glib-2.0.0.tar.gz"
   sha256 "94e8c410c67501303d3b32ca8ce2c36edf898511ec4de9b7f29cd35d274b3d6a"
 
+  bottle do
+    sha256 "712a3bdbb396b15d73fd842cb7042db78aeb3c2c3f60bf38101f4082f8e70d00" => :mojave
+    sha256 "fbb9aa27837e6207508c346f6be03bfa80cae82317f41fda8061535207ab9f21" => :high_sierra
+    sha256 "79e0178f530a2df9140749b34079497c856327c9126eaf0628043375c1172ac0" => :sierra
+  end
+
   depends_on "gobject-introspection" => :build
   depends_on "intltool" => :build
   depends_on "pkg-config" => :build
