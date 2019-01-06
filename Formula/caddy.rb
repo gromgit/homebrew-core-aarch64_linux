a6230af359e594d684e2642434e017a752ce590d
Mike McQuaid <mike@mikemcquaid.com>
2019-01-06 14:57:42 +0000

    caddy: remove old code.

--- Formula/caddy.rb
+++ Formula/caddy.rb
@@ -17,7 +17,7 @@ class Caddy < Formula
   def install
     ENV["GOPATH"] = buildpath
     ENV["GOOS"] = "darwin"
-    ENV["GOARCH"] = MacOS.prefer_64_bit? ? "amd64" : "386"
+    ENV["GOARCH"] = "amd64"
 
     (buildpath/"src/github.com/mholt").mkpath
     ln_s buildpath, "src/github.com/mholt/caddy"
