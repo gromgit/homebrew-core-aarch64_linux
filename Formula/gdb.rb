fa7e2f143d2656e49700dcb4a63dd33fd52ceba3
FX Coudert <fxcoudert@gmail.com>
2019-01-07 00:36:06 +0100

    gdb: add head (#35754)

--- Formula/gdb.rb
+++ Formula/gdb.rb
@@ -1,9 +1,28 @@
 class Gdb < Formula
   desc "GNU debugger"
   homepage "https://www.gnu.org/software/gdb/"
-  url "https://ftp.gnu.org/gnu/gdb/gdb-8.2.1.tar.xz"
-  mirror "https://ftpmirror.gnu.org/gdb/gdb-8.2.1.tar.xz"
-  sha256 "0a6a432907a03c5c8eaad3c3cffd50c00a40c3a5e3c4039440624bae703f2202"
+  head "git://sourceware.org/git/binutils-gdb.git"
+
+  stable do
+    url "https://ftp.gnu.org/gnu/gdb/gdb-8.2.1.tar.xz"
+    mirror "https://ftpmirror.gnu.org/gdb/gdb-8.2.1.tar.xz"
+    sha256 "0a6a432907a03c5c8eaad3c3cffd50c00a40c3a5e3c4039440624bae703f2202"
+
+    # Fix build with all targets. Remove for 8.3
+    # https://sourceware.org/git/gitweb.cgi?p=binutils-gdb.git;a=commitdiff;h=0c0a40e0abb9f1a584330a1911ad06b3686e5361
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/d457e55/gdb/all-targets.diff"
+      sha256 "1cb8a1b8c4b4833212e16ba8cfbe620843aba0cba0f5111c2728c3314e10d8fd"
+    end
+
+    # Fix debugging of executables of Xcode 10 and later
+    # created for 10.14 and newer versions of macOS. Remove for 8.3
+    # https://sourceware.org/git/gitweb.cgi?p=binutils-gdb.git;h=fc7b364aba41819a5d74ae0ac69f050af282d057
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/d457e55/gdb/mojave.diff"
+      sha256 "6264c71b57a0d5d4aed11430d352b03639370b7d36a5b520e189a6a1f105e383"
+    end
+  end
 
   bottle do
     sha256 "01b06c2983503c78bc346b5f5e2c2bdccbc41d6f5ca759542eef712bf123ca30" => :mojave
@@ -29,21 +48,6 @@ class Gdb < Formula
     EOS
   end
 
-  # Fix build with all targets. Remove for 8.3
-  # https://sourceware.org/git/gitweb.cgi?p=binutils-gdb.git;a=commitdiff;h=0c0a40e0abb9f1a584330a1911ad06b3686e5361
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/d457e55/gdb/all-targets.diff"
-    sha256 "1cb8a1b8c4b4833212e16ba8cfbe620843aba0cba0f5111c2728c3314e10d8fd"
-  end
-
-  # Fix debugging of executables of Xcode 10 and later
-  # created for 10.14 and newer versions of macOS. Remove for 8.3
-  # https://sourceware.org/git/gitweb.cgi?p=binutils-gdb.git;h=fc7b364aba41819a5d74ae0ac69f050af282d057
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/d457e55/gdb/mojave.diff"
-    sha256 "6264c71b57a0d5d4aed11430d352b03639370b7d36a5b520e189a6a1f105e383"
-  end
-
   def install
     args = %W[
       --prefix=#{prefix}
@@ -51,15 +55,14 @@ class Gdb < Formula
       --disable-dependency-tracking
       --enable-targets=all
       --with-python=/usr
+      --disable-binutils
     ]
 
     system "./configure", *args
     system "make"
 
     # Don't install bfd or opcodes, as they are provided by binutils
-    inreplace ["bfd/Makefile", "opcodes/Makefile"], /^install:/, "dontinstall:"
-
-    system "make", "install"
+    system "make", "install-gdb"
   end
 
   def caveats; <<~EOS
