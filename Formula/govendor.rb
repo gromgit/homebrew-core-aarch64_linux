6886ab20133e751c04d90f9f1cc66bc2896c19ce
Mike McQuaid <mike@mikemcquaid.com>
2019-01-06 14:57:47 +0000

    govendor: remove old code.

--- Formula/govendor.rb
+++ Formula/govendor.rb
@@ -18,7 +18,7 @@ class Govendor < Formula
   def install
     ENV["GOPATH"] = buildpath
     ENV["GOOS"] = "darwin"
-    ENV["GOARCH"] = MacOS.prefer_64_bit? ? "amd64" : "386"
+    ENV["GOARCH"] = "amd64"
 
     (buildpath/"src/github.com/kardianos/").mkpath
     ln_sf buildpath, buildpath/"src/github.com/kardianos/govendor"
