bf8c056a0c4ff940e87ad321881c99a48d20fbc0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-06 23:32:18 +0000

    spice-protocol: add 0.12.14 bottle.

--- Formula/spice-protocol.rb
+++ Formula/spice-protocol.rb
@@ -4,6 +4,13 @@ class SpiceProtocol < Formula
   url "https://www.spice-space.org/download/releases/spice-protocol-0.12.14.tar.bz2"
   sha256 "20350bc4309039fdf0d29ee4fd0033cde27bccf33501e13b3c1befafde9d0c9c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1240657df2775e902ca34294b48e4163ba0abdbacec45bd5e4c1f19829f362b0" => :mojave
+    sha256 "9b9013afafa2d15479d49e2c5397321d287fa9861754f3aab232b33112d3b081" => :high_sierra
+    sha256 "9b9013afafa2d15479d49e2c5397321d287fa9861754f3aab232b33112d3b081" => :sierra
+  end
+
   def install
     system "./configure", "--disable-silent-rules",
                           "--prefix=#{prefix}"
