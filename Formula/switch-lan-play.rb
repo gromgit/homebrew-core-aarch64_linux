e64526e0030fdccbef83ab602e7fc75902b1adb6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-06 13:28:42 +0000

    switch-lan-play: add 0.0.7 bottle.

--- Formula/switch-lan-play.rb
+++ Formula/switch-lan-play.rb
@@ -4,6 +4,13 @@ class SwitchLanPlay < Formula
   url "https://github.com/spacemeowx2/switch-lan-play/archive/v0.0.7.tar.gz"
   sha256 "0b645fa8bbff18b4b2d1dfd8d975790c6597bc0520c0ab1368387d08f8c2c900"
 
+  bottle do
+    cellar :any
+    sha256 "378a55da07264e83e58925d3205a0cdb0916e2afd4e8b4c387dd46de3748a897" => :mojave
+    sha256 "c4a6f2655ec51819c5ea197c3d30899e4c0208dd836e09c912e012dd275f8d2e" => :high_sierra
+    sha256 "3f20bf2ec6ce400d56b4bcffe1c7a362ea50cb7ef9c2a55bffd04864254d071f" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "libuv"
 
