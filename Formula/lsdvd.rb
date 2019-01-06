9787abac6b4b88d4f7ad72819af6f4a88411cabe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-06 06:55:15 +0000

    lsdvd: update 0.17_1 bottle.

--- Formula/lsdvd.rb
+++ Formula/lsdvd.rb
@@ -7,11 +7,9 @@ class Lsdvd < Formula
 
   bottle do
     cellar :any
-    sha256 "3db304db8d99a4e4b0c6f164b68fef1a0a961a5805af1775c5b0cc6be6b8f81a" => :mojave
-    sha256 "ada8d8a799f6138e42cef34f1592e7ff01278ae6b7c7e3a8e519509db2a24ce6" => :high_sierra
-    sha256 "d64473d3ff0f1b1b7dce0435da6305aa384a374ebca7154498770a9c66297cb7" => :sierra
-    sha256 "eefa4b673d38a87354cdac631ee7e7a1054e69e29e912ff52c2fa84995f7e189" => :el_capitan
-    sha256 "4662e19252627e7f8e344fc0f8b52e83e908f26e9253aad7590eef126ebae7f0" => :yosemite
+    sha256 "dedcdf6af7a20307df4b86fd6dee6842031ecc99fcadfcb2691b2a36ecaa2ab6" => :mojave
+    sha256 "9a413bfe449780947f185252ad35d6aad1fcee718e561f283ee12a7761267299" => :high_sierra
+    sha256 "9ef88f96d637feeaf0b58d1160f0b2a19fd1531083916d89d1c7e09b1e68a1ab" => :sierra
   end
 
   depends_on "pkg-config" => :build
