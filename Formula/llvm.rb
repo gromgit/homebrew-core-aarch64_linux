3bf916dcd1cf335f50d448c91ae352284243fde0
Mike McQuaid <mike@mikemcquaid.com>
2019-01-06 14:57:49 +0000

    llvm: remove old code.

--- Formula/llvm.rb
+++ Formula/llvm.rb
@@ -125,7 +125,6 @@ class Llvm < Formula
   end
 
   # According to the official llvm readme, GCC 4.7+ is required
-  fails_with :gcc_4_0
   fails_with :gcc_4_2
   ("4.3".."4.6").each do |n|
     fails_with :gcc => n
