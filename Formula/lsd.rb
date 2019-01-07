27f4a30aaa76649b0825074a294e12d2f84da6d8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-07 11:45:32 +0000

    lsd: add 0.11.1 bottle.

--- Formula/lsd.rb
+++ Formula/lsd.rb
@@ -4,6 +4,13 @@ class Lsd < Formula
   url "https://github.com/Peltoche/lsd/archive/0.11.1.tar.gz"
   sha256 "7c9cdde049e02433a94cbfc3e7efd2b2ff7b08b9e5343849069b896ea646e40e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d9c592376ddcf9f8634ddae8c0fe80e8e50eec9bb33237e8706f6746f8fa5756" => :mojave
+    sha256 "7e35bf909cf5ad90c317f6d5d73aa72caa767298ed773e21e203ba4635c253b8" => :high_sierra
+    sha256 "3da3d3bbc6fbb235f789276e21144b76147ad5617c774cda798d78c8cb60994a" => :sierra
+  end
+
   depends_on "rust" => :build
 
   def install
