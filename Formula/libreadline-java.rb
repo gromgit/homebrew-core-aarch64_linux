54c9cb6d69c5cafeebc06b099b12c97f0fc0bd46
FX Coudert <fxcoudert@gmail.com>
2019-01-08 10:36:39 +0100

    libreadline-java: revision for readline

--- Formula/libreadline-java.rb
+++ Formula/libreadline-java.rb
@@ -3,7 +3,7 @@ class LibreadlineJava < Formula
   homepage "https://java-readline.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/java-readline/java-readline/0.8.0/libreadline-java-0.8.0-src.tar.gz"
   sha256 "cdcfd9910bfe2dca4cd08b2462ec05efee7395e9b9c3efcb51e85fa70548c890"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
