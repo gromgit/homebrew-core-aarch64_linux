b60468db359a2ca51f33d9de98efda3abe91dad5
Igor Kapkov <igasgeek@me.com>
2019-01-09 17:06:15 +1100

    neofetch 6.0.0
    Closes #35835.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/neofetch.rb
+++ Formula/neofetch.rb
@@ -1,8 +1,8 @@
 class Neofetch < Formula
   desc "Fast, highly customisable system info script"
   homepage "https://github.com/dylanaraps/neofetch"
-  url "https://github.com/dylanaraps/neofetch/archive/5.0.0.tar.gz"
-  sha256 "2a4f4853bf83b88a037994dbc53a90c8bd5708f5eeb3392f56d4e49c49d995b3"
+  url "https://github.com/dylanaraps/neofetch/archive/6.0.0.tar.gz"
+  sha256 "264a7689561bb498f97f10231959bdd8f7c873671bac2ffb660de9a5863b1c76"
   head "https://github.com/dylanaraps/neofetch.git"
 
   bottle do
