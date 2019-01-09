ad08996a15f4848832b1078d45444f33f282d056
Igor Kapkov <igasgeek@me.com>
2019-01-10 10:53:43 +1100

    qpdf 8.3.0
    Closes #35863.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/qpdf.rb
+++ Formula/qpdf.rb
@@ -1,8 +1,8 @@
 class Qpdf < Formula
   desc "Tools for and transforming and inspecting PDF files"
   homepage "https://github.com/qpdf/qpdf"
-  url "https://github.com/qpdf/qpdf/releases/download/release-qpdf-8.2.1/qpdf-8.2.1.tar.gz"
-  sha256 "f445d3ebda833fe675b7551378f41fa1971cc6f7a7921bbbb94d3a71a404abc9"
+  url "https://github.com/qpdf/qpdf/releases/download/release-qpdf-8.3.0/qpdf-8.3.0.tar.gz"
+  sha256 "46c2a10690b86ba680db1c828e9ce44bb8e2993f69aa96d5a52fd1d98dc391f7"
 
   bottle do
     cellar :any
