0914542d9ce2e4e5210b3755197571148fcf27e4
Igor Kapkov <igasgeek@me.com>
2019-01-10 10:43:04 +1100

    docker-machine-completion 0.16.1
    Closes #35860.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/docker-machine-completion.rb
+++ Formula/docker-machine-completion.rb
@@ -1,8 +1,8 @@
 class DockerMachineCompletion < Formula
   desc "Docker-machine completion script"
   homepage "https://docs.docker.com/machine/completion/"
-  url "https://github.com/docker/machine/archive/v0.16.0.tar.gz"
-  sha256 "42342b21410cc29d71ca4d5b25e17eeaf20f21a0f3b825e702a7ddcff5adcb16"
+  url "https://github.com/docker/machine/archive/v0.16.1.tar.gz"
+  sha256 "b3989ed5b2829c87885b40b209fe475d90d63fbb9bd10879f866441d23cbf495"
   head "https://github.com/docker/machine.git"
 
   bottle :unneeded
