718774b8cf009fe3b633b22f781eff92fde4eeb5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-10 09:40:23 +0000

    oxipng: add 2.1.8 bottle.

--- Formula/oxipng.rb
+++ Formula/oxipng.rb
@@ -4,6 +4,13 @@ class Oxipng < Formula
   url "https://github.com/shssoichiro/oxipng/archive/v2.1.8.tar.gz"
   sha256 "556c85fb05baa0c05aacbcc531586a9077b58162281744d2f09e9b6cc906a606"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7ea1af061a0ea0cc9995d14560888b4707a07d5970f0378964fe1a9710b77e72" => :mojave
+    sha256 "958aa7690a265a5b47cb24671972b8d89f1eef3d7fe4e25f7cf9b2e1c65b8335" => :high_sierra
+    sha256 "2f01a2dc5512754034c7b1cf0ddc8369ce60fc6e47cd189a0cc311edfcfd1415" => :sierra
+  end
+
   depends_on "rust" => :build
 
   def install
