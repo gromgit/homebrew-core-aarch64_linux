87302a411ed54d2db8c1f2cc62da95e7f4d07288
Ian Max Andolina <iandol@users.noreply.github.com>
2019-01-11 00:22:19 +0800

    bash-completion: update caveats (#35817)

--- Formula/bash-completion.rb
+++ Formula/bash-completion.rb
@@ -40,7 +40,7 @@ class BashCompletion < Formula
 
   def caveats; <<~EOS
     Add the following line to your ~/.bash_profile:
-      [ -f #{etc}/bash_completion ] && . #{etc}/bash_completion
+      [ -d #{etc}/bash_completion.d ] && . #{etc}/bash_completion.d/*
   EOS
   end
 
