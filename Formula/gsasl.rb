dcef91dabc8d42a59d53e966040dd116dcd747b3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-10 04:10:53 +0000

    gsasl: update 1.8.0 bottle.

--- Formula/gsasl.rb
+++ Formula/gsasl.rb
@@ -7,14 +7,10 @@ class Gsasl < Formula
 
   bottle do
     cellar :any
-    rebuild 2
-    sha256 "f54e55bdb4dcdc2e8bf7b537ecdc317feaff5701a35d96e072a90d82be3ac93e" => :mojave
-    sha256 "0cac4a51f7ebbeb960744f0a48374b8800403fd773935f123810038da1d2d316" => :high_sierra
-    sha256 "f37b1e2171acd1ed587c75827854750f2be04302147fb3fc05672c3e3cfe42f0" => :sierra
-    sha256 "8dbca0c2938ab3b5077fe7ed572437a0f724e479a7f102d9c40f959b1483f09d" => :el_capitan
-    sha256 "afc44c1161ffa2ae09bee4a82c25e626562d950ae092356fba204789e4b4752e" => :yosemite
-    sha256 "df498ac7247b3f54bdc9720249fa4a1cee72bf8c5011d06889d8ecdebaff1aaa" => :mavericks
-    sha256 "5585b8bddf849b2b4b3f67da253c97556bfa526b8345006595cdefddf3385dd5" => :mountain_lion
+    rebuild 3
+    sha256 "d52080e846621b8b297fb8f100c077a859545b933ec527573a02addddca0b40d" => :mojave
+    sha256 "9e6544e25c68eff0e352ba84355a9a6ea201b9b533cbb9420eee3bdfd1531eb3" => :high_sierra
+    sha256 "22c6d1dd63deb2e26dd8e3b2b51b30735ada08b6972222eed3d1868fddf1ed3c" => :sierra
   end
 
   def install
