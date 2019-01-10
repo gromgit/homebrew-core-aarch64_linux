18bcb1e3c903f8fcdb9319736d1fe8f0ec1a2578
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-10 03:13:58 +0000

    simple-scan: add 3.30.2 bottle.

--- Formula/simple-scan.rb
+++ Formula/simple-scan.rb
@@ -4,6 +4,12 @@ class SimpleScan < Formula
   url "https://download.gnome.org/sources/simple-scan/3.30/simple-scan-3.30.2.tar.xz"
   sha256 "71144fa985846e974dbe486bfab825694fc2a4e728fbd1ba4d37774b65db7636"
 
+  bottle do
+    sha256 "7ef5e1f022286f0736b071f32b60b3ae9e4135c4ea7c8402489cbed466ad7d78" => :mojave
+    sha256 "720a1f1e2928c1361b77804269817942c6cdd757ddd7b1becd02fb0af9dfb173" => :high_sierra
+    sha256 "6c3a0cf468aff936f5c5365c31bde767cf94005bc8dc7e885f94dc8250894dca" => :sierra
+  end
+
   depends_on "itstool" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
