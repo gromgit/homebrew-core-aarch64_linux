5ea236239a63831ab53a98179f2278b3c10d7e31
chrisdeeming <chris@xenforo.com>
2019-01-10 12:59:51 +0000

    php 7.3.1
    Closes #35875.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/php.rb
+++ Formula/php.rb
@@ -1,9 +1,8 @@
 class Php < Formula
   desc "General-purpose scripting language"
   homepage "https://secure.php.net/"
-  url "https://php.net/get/php-7.3.0.tar.xz/from/this/mirror"
-  sha256 "7d195cad55af8b288c3919c67023a14ff870a73e3acc2165a6d17a4850a560b5"
-  revision 1
+  url "https://php.net/get/php-7.3.1.tar.xz/from/this/mirror"
+  sha256 "cfe93e40be0350cd53c4a579f52fe5d8faf9c6db047f650a4566a2276bf33362"
 
   bottle do
     sha256 "74ba1ffaa12631b3413617915d8de0ab54e9714530ba31530e228580cc91327a" => :mojave
