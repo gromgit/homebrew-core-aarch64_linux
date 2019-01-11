256b4c7b4d0f5be2976e3831ac0237838c5d7d3d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-11 03:14:33 +0000

    gnu-which: update 2.21 bottle.

--- Formula/gnu-which.rb
+++ Formula/gnu-which.rb
@@ -8,10 +8,10 @@ class GnuWhich < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 2
-    sha256 "83fb2814c1a81fa381f5462565141ff1f4fd0580847288c7047019353bad0fa4" => :mojave
-    sha256 "1c2174cbc0d721dec7f86f920dbdda09109c84fdf8b8cada6a10b01d496e411b" => :high_sierra
-    sha256 "ebd500ad7b851205dd25c7ec78441f029b9982b8a0f3df081266e58f6df7c6ec" => :sierra
+    rebuild 3
+    sha256 "170008e80a4cc5f1e45b3445f9fb6f099d7700aa6dd825602f6d32316c27735b" => :mojave
+    sha256 "66446416b0dc367076ab38cfc9775d8c201fc571b1a2cd2fc0197daa6b83882a" => :high_sierra
+    sha256 "68ea3522ec318c9b25d711ce4405b4cd6a41edca20b7df008adc499ab794c4fa" => :sierra
   end
 
   def install
