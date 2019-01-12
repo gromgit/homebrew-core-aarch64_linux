805473294e363d711e0b499830ac80561d16ca2d
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2019-01-12 18:34:56 -0500

    joplin 1.0.120
    Closes #35945.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/joplin.rb
+++ Formula/joplin.rb
@@ -3,9 +3,8 @@ require "language/node"
 class Joplin < Formula
   desc "Note taking and to-do application with synchronisation capabilities"
   homepage "https://joplin.cozic.net/"
-  url "https://registry.npmjs.org/joplin/-/joplin-1.0.116.tgz"
-  sha256 "ddb4cd6246c5396064506cc23ab5773ec6d8c6075bba3ff338f68797ea2665e6"
-  revision 1
+  url "https://registry.npmjs.org/joplin/-/joplin-1.0.120.tgz"
+  sha256 "80a96934f099ef45a35389184de718d683aabdd9b3371c179c0ae6fee5db9ab2"
 
   bottle do
     sha256 "d9b4ce17add87eab53f2068a9dbad86cd502f30860b4212d59b2c348e99fd861" => :mojave
