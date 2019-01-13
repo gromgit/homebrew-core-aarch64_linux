3a750fba723078fbaab1e70737a58a7e8268b6eb
FX Coudert <fxcoudert@gmail.com>
2019-01-13 21:31:05 +0100

    gst-plugins-ugly: remove options

--- Formula/gst-plugins-ugly.rb
+++ Formula/gst-plugins-ugly.rb
@@ -32,44 +32,19 @@ class GstPluginsUgly < Formula
   depends_on "theora"
   depends_on "x264"
 
-  # The set of optional dependencies is based on the intersection of
-  # gst-plugins-ugly-0.10.17/REQUIREMENTS and Homebrew formulae
-  depends_on "a52dec" => :optional
-  depends_on "aalib" => :optional
-  depends_on "cdparanoia" => :optional
-  depends_on "dirac" => :optional
-  depends_on "gtk+" => :optional
-  depends_on "libcaca" => :optional
-  depends_on "libdvdread" => :optional
-  depends_on "libmpeg2" => :optional
-  depends_on "liboil" => :optional
-  depends_on "mad" => :optional
-  depends_on "opencore-amr" => :optional
-  depends_on "two-lame" => :optional
-  # Does not work with libcdio 0.9
-
   def install
     args = %W[
       --prefix=#{prefix}
       --mandir=#{man}
       --disable-debug
       --disable-dependency-tracking
+      --disable-amrnb
+      --disable-amrwb
     ]
 
     if build.head?
       ENV["NOCONFIGURE"] = "yes"
       system "./autogen.sh"
-    end
-
-    if build.with? "opencore-amr"
-      # Fixes build error, missing includes.
-      # https://github.com/Homebrew/homebrew/issues/14078
-      nbcflags = `pkg-config --cflags opencore-amrnb`.chomp
-      wbcflags = `pkg-config --cflags opencore-amrwb`.chomp
-      ENV["AMRNB_CFLAGS"] = nbcflags + "-I#{HOMEBREW_PREFIX}/include/opencore-amrnb"
-      ENV["AMRWB_CFLAGS"] = wbcflags + "-I#{HOMEBREW_PREFIX}/include/opencore-amrwb"
-    else
-      args << "--disable-amrnb" << "--disable-amrwb"
     end
 
     system "./configure", *args
