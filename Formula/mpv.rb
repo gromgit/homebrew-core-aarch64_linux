1fdd9b152760e9d743964dd955c1ae816a3ca191
FX Coudert <fxcoudert@gmail.com>
2019-01-13 21:31:05 +0100

    mpv: remove options

--- Formula/mpv.rb
+++ Formula/mpv.rb
@@ -3,7 +3,7 @@ class Mpv < Formula
   homepage "https://mpv.io"
   url "https://github.com/mpv-player/mpv/archive/v0.29.1.tar.gz"
   sha256 "f9f9d461d1990f9728660b4ccb0e8cb5dce29ccaa6af567bec481b79291ca623"
-  revision 1
+  revision 2
   head "https://github.com/mpv-player/mpv.git"
 
   bottle do
@@ -11,9 +11,6 @@ class Mpv < Formula
     sha256 "581bfc39b0c3d8e91f2f5d2529d4b27d6db854ded5daf3b7fa3f71260034ba1d" => :high_sierra
     sha256 "01dd3eb9eef24754c2279b16607bfed281a481c6a0e6af357b63b78a8bf50053" => :sierra
   end
-
-  option "with-bundle", "Enable compilation of the .app bundle."
-  option "with-lgpl", "Build with LGPLv2.1 or later license"
 
   depends_on "pkg-config" => :build
   depends_on "python" => :build
@@ -25,19 +22,8 @@ class Mpv < Formula
   depends_on "lua@5.1"
   depends_on :macos => :mountain_lion
   depends_on "mujs"
+  depends_on "vapoursynth"
   depends_on "youtube-dl"
-
-  depends_on "jack" => :optional
-  depends_on "libaacs" => :optional
-  depends_on "libarchive" => :optional
-  depends_on "libbluray" => :optional
-  depends_on "libcaca" => :optional
-  depends_on "libdvdnav" => :optional
-  depends_on "libdvdread" => :optional
-  depends_on "pulseaudio" => :optional
-  depends_on "rubberband" => :optional
-  depends_on "uchardet" => :optional
-  depends_on "vapoursynth" => :optional
 
   resource "docutils" do
     url "https://files.pythonhosted.org/packages/84/f4/5771e41fdf52aabebbadecc9381d11dea0fa34e4759b4071244fa094804c/docutils-0.14.tar.gz"
@@ -67,28 +53,16 @@ class Mpv < Formula
       --datadir=#{pkgshare}
       --mandir=#{man}
       --docdir=#{doc}
+      --enable-zsh-comp
+      --zshdir=#{zsh_completion}
     ]
-    args << "--enable-libarchive" if build.with? "libarchive"
-    args << "--enable-libbluray" if build.with? "libbluray"
-    args << "--enable-dvdnav" if build.with? "libdvdnav"
-    args << "--enable-dvdread" if build.with? "libdvdread"
-    args << "--enable-pulse" if build.with? "pulseaudio"
-
-    if build.with? "lgpl"
-      args << "--enable-lgpl"
-    else
-      args << "--enable-zsh-comp"
-      args << "--zshdir=#{zsh_completion}"
-    end
 
     system "./bootstrap.py"
     system "python3", "waf", "configure", *args
     system "python3", "waf", "install"
 
-    if build.with? "bundle"
-      system "python3", "TOOLS/osxbundle.py", "build/mpv"
-      prefix.install "build/mpv.app"
-    end
+    system "python3", "TOOLS/osxbundle.py", "build/mpv"
+    prefix.install "build/mpv.app"
   end
 
   test do
