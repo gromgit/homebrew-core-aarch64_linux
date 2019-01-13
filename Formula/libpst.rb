d6c3b6cdff94a22c61d9a6243af69e2945d8f84e
FX Coudert <fxcoudert@gmail.com>
2019-01-13 16:36:56 +0100

    libpst: remove Python bindings
    Closes #35951.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libpst.rb
+++ Formula/libpst.rb
@@ -3,7 +3,7 @@ class Libpst < Formula
   homepage "https://www.five-ten-sg.com/libpst/"
   url "https://www.five-ten-sg.com/libpst/packages/libpst-0.6.72.tar.gz"
   sha256 "8a19d891eb077091c507d98ed8e2d24b7f48b3e82743bcce2b00a12040f5d507"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
@@ -14,17 +14,14 @@ class Libpst < Formula
 
   depends_on "pkg-config" => :build
   depends_on "boost"
-  depends_on "boost-python"
   depends_on "gettext"
   depends_on "libgsf"
-  depends_on "python@2"
 
   def install
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
-      --enable-python
-      --with-boost-python=mt
+      --disable-python
     ]
 
     system "./configure", *args
