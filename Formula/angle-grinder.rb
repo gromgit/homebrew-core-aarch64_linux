244e57318f0eba7167f9b8932fb801ae2e691c22
Igor Kapkov <igasgeek@me.com>
2019-01-14 09:34:06 +1100

    angle-grinder 0.9.0
    Closes #35974.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/angle-grinder.rb
+++ Formula/angle-grinder.rb
@@ -1,8 +1,8 @@
 class AngleGrinder < Formula
   desc "Slice and dice log files on the command-line"
   homepage "https://github.com/rcoh/angle-grinder"
-  url "https://github.com/rcoh/angle-grinder/archive/v0.8.0.tar.gz"
-  sha256 "d90d96075db1a84863eb56e0a3061bbf8928ed795324b2b5373e4dbfa7043eac"
+  url "https://github.com/rcoh/angle-grinder/archive/v0.9.0.tar.gz"
+  sha256 "230eb2bfbab73eb3ac5b37d25ab7521bc50f7640d9c12fcf50c3526eb4ba3423"
 
   bottle do
     cellar :any_skip_relocation
