7ef16fd3ca773c18b6f4a2a2d43246356ca4c285
Igor Kapkov <igasgeek@me.com>
2019-01-14 09:13:16 +1100

    emscripten 1.38.23
    Closes #35962.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/emscripten.rb
+++ Formula/emscripten.rb
@@ -3,17 +3,17 @@ class Emscripten < Formula
   homepage "https://kripken.github.io/emscripten-site/"
 
   stable do
-    url "https://github.com/kripken/emscripten/archive/1.38.22.tar.gz"
-    sha256 "7bd3fbf8df368ccfaacc9a2f28778e5e4bb14b1e5a03461c33055b69b9f4dc7c"
+    url "https://github.com/kripken/emscripten/archive/1.38.23.tar.gz"
+    sha256 "e72a0bd1885804615614e19da7b1b25cb2b9cc6b24f9619fa1692764698f6545"
 
     resource "fastcomp" do
-      url "https://github.com/kripken/emscripten-fastcomp/archive/1.38.22.tar.gz"
-      sha256 "0cf71847df17d13af83a1381461407f50d761a505aa870c970ff3bd9656b00f6"
+      url "https://github.com/kripken/emscripten-fastcomp/archive/1.38.23.tar.gz"
+      sha256 "94a302d7fe1698fca55bf2519c4af2f8dfd9c01fd06ae43fd506dfa09da44d66"
     end
 
     resource "fastcomp-clang" do
-      url "https://github.com/kripken/emscripten-fastcomp-clang/archive/1.38.22.tar.gz"
-      sha256 "939900f5fc0b5e7bcf01607c4171cc1c1fd02bad041c3f3d66b03471cb104c4e"
+      url "https://github.com/kripken/emscripten-fastcomp-clang/archive/1.38.23.tar.gz"
+      sha256 "4c748ce363fa183fe4df80182d8a40f8d46988adff191dafc4682025c4d9880a"
     end
   end
 
