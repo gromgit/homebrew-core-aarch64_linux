da05bb9c543151af71b84296aa5d4aad4eaf2a41
Josh Rabinowitz <joshr@joshr.com>
2019-01-15 13:50:20 -0500

    git-secret 0.2.5
    Closes #36041.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/git-secret.rb
+++ Formula/git-secret.rb
@@ -4,15 +4,8 @@ class GitSecret < Formula
   head "https://github.com/sobolevn/git-secret.git"
 
   stable do
-    url "https://github.com/sobolevn/git-secret/archive/v0.2.4.tar.gz"
-    sha256 "dd9962935f242a94bb00af6a31171de0fdba357171a6c626efc2635751d52bc4"
-
-    # Remove for > 0.2.4
-    # Upstream PR from 12 Jun 2018 "Revert 'migrate from bats to bats-core'"
-    patch do
-      url "https://github.com/sobolevn/git-secret/pull/203.patch?full_index=1"
-      sha256 "ee4e263b9725aee59f8b52196d4749a09791dbb4c745a9e81da523d27fcf3f09"
-    end
+    url "https://github.com/sobolevn/git-secret/archive/v0.2.5.tar.gz"
+    sha256 "02224b360ef560d1a41ae758d16ef2f2657af8424711d52443ca7ac01367ad29"
   end
 
   bottle do
