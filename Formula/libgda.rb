c884bfe5e8354a9472e9e4e8d68871fd2fcf616e
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-01-16 11:18:17 +0530

    libgda: use internal sqlite library
    libgda does not support using the sqlite dylib on macOS, so use the
    internal sqlite library, which will be linked statically during the
    build.
    
    Closes #36475.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libgda.rb
+++ Formula/libgda.rb
@@ -3,7 +3,7 @@ class Libgda < Formula
   homepage "http://www.gnome-db.org/"
   url "https://download.gnome.org/sources/libgda/5.2/libgda-5.2.8.tar.xz"
   sha256 "e2876d987c00783ac3c1358e9da52794ac26f557e262194fcba60ac88bafa445"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "31bc1f2cecf14e6a2d751f257a09426d5f00ef0c46910154fe2ec1180dbe059e" => :mojave
@@ -21,9 +21,12 @@ class Libgda < Formula
   depends_on "libgee"
   depends_on "openssl"
   depends_on "readline"
-  depends_on "sqlite"
 
   def install
+    # this build uses the sqlite source code that comes with libgda,
+    # as opposed to using the system or brewed sqlite3, which is not supported on macOS,
+    # as mentioned in https://github.com/GNOME/libgda/blob/95eeca4b0470f347c645a27f714c62aa6e59f820/libgda/sqlite/README#L31
+
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
@@ -31,8 +34,13 @@ class Libgda < Formula
                           "--disable-binreloc",
                           "--disable-gtk-doc",
                           "--without-java",
-                          "--enable-introspection"
+                          "--enable-introspection",
+                          "--enable-system-sqlite=no"
     system "make"
     system "make", "install"
+  end
+
+  test do
+    system "#{bin}/gda-sql", "-v"
   end
 end
