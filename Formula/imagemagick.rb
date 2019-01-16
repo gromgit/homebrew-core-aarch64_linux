f53609a6c42ddf2d27611eb02920e77452cd0ccb
Mike McQuaid <mike@mikemcquaid.com>
2019-01-16 20:21:34 +0000

    imagemagick: remove options.

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -16,12 +16,6 @@ class Imagemagick < Formula
     sha256 "268d7a5ba387992a19500b5b30aa21dd52415a1d0ee9584f37e78f315243a1cd" => :sierra
   end
 
-  option "with-fftw", "Compile with FFTW support"
-  option "with-libheif", "Compile with HEIF support"
-  option "with-perl", "Compile with PerlMagick"
-
-  deprecated_option "with-libde265" => "with-libheif"
-
   depends_on "pkg-config" => :build
 
   depends_on "freetype"
@@ -34,19 +28,6 @@ class Imagemagick < Formula
   depends_on "openjpeg"
   depends_on "webp"
   depends_on "xz"
-
-  depends_on "fftw" => :optional
-  depends_on "fontconfig" => :optional
-  depends_on "ghostscript" => :optional
-  depends_on "libheif" => :optional
-  depends_on "liblqr" => :optional
-  depends_on "librsvg" => :optional
-  depends_on "libwmf" => :optional
-  depends_on "little-cms" => :optional
-  depends_on "openexr" => :optional
-  depends_on "pango" => :optional
-  depends_on "perl" => :optional
-  depends_on :x11 => :optional
 
   skip_clean :la
 
@@ -63,34 +44,22 @@ class Imagemagick < Formula
       --with-modules
       --with-openjp2
       --with-webp=yes
+      --without-gslib
+      --with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts
+      --without-fftw
+      --without-pango
+      --without-x
+      --without-wmf
       --enable-openmp
       ac_cv_prog_c_openmp=-Xpreprocessor\ -fopenmp
       ac_cv_prog_cxx_openmp=-Xpreprocessor\ -fopenmp
       LDFLAGS=-lomp
     ]
 
-    args << "--without-gslib" if build.without? "ghostscript"
-    args << "--with-perl" << "--with-perl-options='PREFIX=#{prefix}'" if build.with? "perl"
-    args << "--with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts" if build.without? "ghostscript"
-    args << "--without-fftw" if build.without? "fftw"
-    args << "--without-pango" if build.without? "pango"
-    args << "--with-rsvg" if build.with? "librsvg"
-    args << "--without-x" if build.without? "x11"
-    args << "--with-fontconfig=yes" if build.with? "fontconfig"
-    args << "--without-wmf" if build.without? "libwmf"
-
     # versioned stuff in main tree is pointless for us
     inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_VERSION}", "${PACKAGE_NAME}"
     system "./configure", *args
     system "make", "install"
-  end
-
-  def caveats
-    s = <<~EOS
-      For full Perl support you may need to adjust your PERL5LIB variable:
-        export PERL5LIB="#{HOMEBREW_PREFIX}/lib/perl5/site_perl":$PERL5LIB
-    EOS
-    s if build.with? "perl"
   end
 
   test do
