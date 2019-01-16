7f9bfa679229a6837d2d8ba8a08bc6154f0ed4d4
Mike McQuaid <mike@mikemcquaid.com>
2019-01-16 17:01:13 +0000

    curl: remove options.
    Closes #36066.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -22,20 +22,9 @@ class Curl < Formula
 
   keg_only :provided_by_macos
 
-  option "with-rtmpdump", "Build with RTMP support"
-  option "with-libssh2", "Build with scp and sftp support"
-  option "with-c-ares", "Build with C-Ares async DNS support"
-  option "with-gssapi", "Build with GSSAPI/Kerberos authentication support."
-  option "with-libmetalink", "Build with libmetalink support."
-  option "with-nghttp2", "Build with HTTP/2 support (requires OpenSSL)"
-
-  deprecated_option "with-rtmp" => "with-rtmpdump"
-  deprecated_option "with-ssh" => "with-libssh2"
-  deprecated_option "with-ares" => "with-c-ares"
-
   # HTTP/2 support requires OpenSSL 1.0.2+ or LibreSSL 2.1.3+ for ALPN Support
   # which is currently not supported by Secure Transport (DarwinSSL).
-  if MacOS.version < :mountain_lion || build.with?("nghttp2")
+  if MacOS.version < :mountain_lion
     depends_on "openssl"
   else
     option "with-openssl", "Build with OpenSSL instead of Secure Transport"
@@ -43,11 +32,6 @@ class Curl < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on "c-ares" => :optional
-  depends_on "libmetalink" => :optional
-  depends_on "libssh2" => :optional
-  depends_on "nghttp2" => :optional
-  depends_on "rtmpdump" => :optional
 
   def install
     system "./buildconf" if build.head?
@@ -65,7 +49,7 @@ class Curl < Formula
     # cURL has a new firm desire to find ssl with PKG_CONFIG_PATH instead of using
     # "--with-ssl" any more. "when possible, set the PKG_CONFIG_PATH environment
     # variable instead of using this option". Multi-SSL choice breaks w/o using it.
-    if MacOS.version < :mountain_lion || build.with?("openssl") || build.with?("nghttp2")
+    if MacOS.version < :mountain_lion || build.with?("openssl")
       ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["openssl"].opt_lib}/pkgconfig"
       args << "--with-ssl=#{Formula["openssl"].opt_prefix}"
       args << "--with-ca-bundle=#{etc}/openssl/cert.pem"
@@ -74,17 +58,6 @@ class Curl < Formula
       args << "--with-darwinssl"
       args << "--without-ca-bundle"
       args << "--without-ca-path"
-    end
-
-    args << (build.with?("libssh2") ? "--with-libssh2" : "--without-libssh2")
-    args << (build.with?("libmetalink") ? "--with-libmetalink" : "--without-libmetalink")
-    args << (build.with?("gssapi") ? "--with-gssapi" : "--without-gssapi")
-    args << (build.with?("rtmpdump") ? "--with-librtmp" : "--without-librtmp")
-
-    if build.with? "c-ares"
-      args << "--enable-ares=#{Formula["c-ares"].opt_prefix}"
-    else
-      args << "--disable-ares"
     end
 
     system "./configure", *args
