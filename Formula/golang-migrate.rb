1cda0355abc490136149f5372556d8de65593c99
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-17 00:49:04 +0000

    golang-migrate: update 4.2.2 bottle.

--- Formula/golang-migrate.rb
+++ Formula/golang-migrate.rb
@@ -6,9 +6,9 @@ class GolangMigrate < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e7340ff1ca29d910ab17c555e96214b49b3910377e98d11c1643d774392875cc" => :mojave
-    sha256 "8925ffc0a34381d864c67d985c847fb2fabdb03f927849b1cd011598e49f721d" => :high_sierra
-    sha256 "368a25c3a31be3c167fbe71bf9d8e2ddb5732c6d88e558e2c678f6c21e622aaa" => :sierra
+    sha256 "bd7893fc3c44b1f910ecd649c4ac44cf12db2ce1f9987aef37eabbe27a2f418a" => :mojave
+    sha256 "09d2d19328a2677a7add78f156916ae18c105967c2f484b88688859fb20d1fbe" => :high_sierra
+    sha256 "2b3f40c091f3d30fef384985e27b3c7755f6a85e914e2f80ff7699a1a1c77f3f" => :sierra
   end
 
   depends_on "go" => :build
