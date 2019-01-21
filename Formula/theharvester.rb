d54deb92632d72cda335da67634a54ecc68eced4
Mike McQuaid <mike@mikemcquaid.com>
2019-01-21 10:37:39 +0000

    theharvester: fix test.

--- Formula/theharvester.rb
+++ Formula/theharvester.rb
@@ -76,7 +76,7 @@ class Theharvester < Formula
   end
 
   test do
-    output = shell_output("#{bin}/theharvester -d brew.sh -l 1 -b pgp 2>&1")
-    assert_match "security@brew.sh", output
+    output = shell_output("#{bin}/theharvester -d brew.sh -l 1 -b google 2>&1")
+    assert_match "docs.brew.sh:", output
   end
 end
