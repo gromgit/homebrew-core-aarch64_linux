21abf895c2f1a83c056a3457a9e0b46f0e448327
Mike McQuaid <mike@mikemcquaid.com>
2019-01-21 16:36:52 +0000

    notmuch: remove options.
    Closes #36212.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/notmuch.rb
+++ Formula/notmuch.rb
@@ -13,58 +13,36 @@ class Notmuch < Formula
     sha256 "aa6bb5fe775c2034e5ababa0ca796853ad52e8e58328eef29fc728fc1b1ddc29" => :sierra
   end
 
-  option "without-python@2", "Build without python2 support"
-
-  deprecated_option "with-python3" => "with-python"
-  deprecated_option "without-python" => "without-python@2"
-
   depends_on "doxygen" => :build
   depends_on "libgpg-error" => :build
   depends_on "pkg-config" => :build
   depends_on "sphinx-doc" => :build
+  depends_on "emacs"
   depends_on "glib"
   depends_on "gmime"
+  depends_on "python@2"
   depends_on "talloc"
   depends_on "xapian"
   depends_on "zlib"
-  depends_on "python@2" => :recommended
-  depends_on "emacs" => :optional
-  depends_on "python" => :optional
-  depends_on "ruby" => :optional
-
-  # Fix SIP issue with python bindings
-  # A more comprehensive patch has been submitted upstream
-  # https://notmuchmail.org/pipermail/notmuch/2016/022631.html
-  patch :DATA
 
   def install
-    args = %W[--prefix=#{prefix} --mandir=#{man}]
+    args = %W[
+      --prefix=#{prefix}
+      --mandir=#{man}
+      --with-emacs
+      --emacslispdir=#{elisp}
+      --emacsetcdir=#{elisp}
+      --without-ruby
+    ]
 
-    if build.with? "emacs"
-      ENV.deparallelize # Emacs and parallel builds aren't friends
-      args << "--with-emacs" << "--emacslispdir=#{elisp}" << "--emacsetcdir=#{elisp}"
-    else
-      args << "--without-emacs"
-    end
-
-    args << "--without-ruby" if build.without? "ruby"
+    # Emacs and parallel builds aren't friends
+    ENV.deparallelize
 
     system "./configure", *args
     system "make", "V=1", "install"
 
-    if build.with? "ruby"
-      cd "bindings/ruby" do
-        # Prevent Makefile from trying to break free of the
-        # sandbox and mkdir in HOMEBREW_PREFIX.
-        inreplace "Makefile", HOMEBREW_PREFIX/"lib/ruby", lib/"ruby"
-        system "make", "install"
-      end
-    end
-
-    Language::Python.each_python(build) do |python, _version|
-      cd "bindings/python" do
-        system python, *Language::Python.setup_install_args(prefix)
-      end
+    cd "bindings/python" do
+      system "python2.7", *Language::Python.setup_install_args(prefix)
     end
   end
 
@@ -74,18 +52,3 @@ class Notmuch < Formula
     assert_match "0 total", shell_output("#{bin}/notmuch new")
   end
 end
-
-__END__
-diff --git a/bindings/python/notmuch/globals.py b/bindings/python/notmuch/globals.py
-index b1eec2c..bce5190 100644
---- a/bindings/python/notmuch/globals.py
-+++ b/bindings/python/notmuch/globals.py
-@@ -25,7 +25,7 @@ from notmuch.version import SOVERSION
- try:
-     from os import uname
-     if uname()[0] == 'Darwin':
--        nmlib = CDLL("libnotmuch.{0:s}.dylib".format(SOVERSION))
-+        nmlib = CDLL("HOMEBREW_PREFIX/lib/libnotmuch.{0:s}.dylib".format(SOVERSION))
-     else:
-         nmlib = CDLL("libnotmuch.so.{0:s}".format(SOVERSION))
- except:
