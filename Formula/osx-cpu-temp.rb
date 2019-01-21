87dc98d60caac6707f0df0bf48df208b03391a98
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-21 09:54:47 +0000

    osx-cpu-temp: add 1.1.0 bottle.

--- Formula/osx-cpu-temp.rb
+++ Formula/osx-cpu-temp.rb
@@ -4,6 +4,13 @@ class OsxCpuTemp < Formula
   url "https://github.com/lavoiesl/osx-cpu-temp/archive/1.1.0.tar.gz"
   sha256 "94b90ce9a1c7a428855453408708a5557bfdb76fa45eef2b8ded4686a1558363"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c0301d2c47c23bc8ed0042fbaf447e82ca8dbbf10b1939d9a4f684961a24d0d2" => :mojave
+    sha256 "2255aa28242ce07a62fc0eabaf146592fb70745e641cfc775a21f99841cec625" => :high_sierra
+    sha256 "d68a47b126eaee8f75d281785322877055187f89540eb2744b9cd4da15ca6a69" => :sierra
+  end
+
   def install
     system "make"
     bin.install "osx-cpu-temp"
