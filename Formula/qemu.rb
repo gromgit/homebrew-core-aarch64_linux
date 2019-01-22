a4c40666f4569eef47e0e765afd6f401f82577ef
Mike McQuaid <mike@mikemcquaid.com>
2019-01-22 18:59:50 +0000

    qemu: remove options. (#36288)
    See https://github.com/Homebrew/homebrew-core/issues/31510.

--- Formula/qemu.rb
+++ Formula/qemu.rb
@@ -13,9 +13,6 @@ class Qemu < Formula
     sha256 "ff6d0904a871d605aefda6cd0574a0ccfb09b758cca832d347ff843eb52f97fd" => :sierra
   end
 
-  deprecated_option "with-sdl" => "with-sdl2"
-  deprecated_option "with-gtk+" => "with-gtk+3"
-
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "glib"
@@ -27,8 +24,6 @@ class Qemu < Formula
   depends_on "ncurses"
   depends_on "pixman"
   depends_on "vde"
-  depends_on "gtk+3" => :optional
-  depends_on "sdl2" => :optional
 
   fails_with :gcc do
     cause "qemu requires a compiler with support for the __thread specifier"
@@ -53,24 +48,16 @@ class Qemu < Formula
       --enable-libssh2
       --enable-vde
       --extra-cflags=-DNCURSES_WIDECHAR=1
+      --enable-cocoa
+      --disable-sdl
+      --disable-gtk
     ]
-
-    # Cocoa and SDL2/GTK+ UIs cannot both be enabled at once.
-    if build.with?("sdl2") || build.with?("gtk+3")
-      args << "--disable-cocoa"
-    else
-      args << "--enable-cocoa"
-    end
-
     # Sharing Samba directories in QEMU requires the samba.org smbd which is
     # incompatible with the macOS-provided version. This will lead to
     # silent runtime failures, so we set it to a Homebrew path in order to
     # obtain sensible runtime errors. This will also be compatible with
     # Samba installations from external taps.
     args << "--smbd=#{HOMEBREW_PREFIX}/sbin/samba-dot-org-smbd"
-
-    args << (build.with?("sdl2") ? "--enable-sdl" : "--disable-sdl")
-    args << (build.with?("gtk+3") ? "--enable-gtk" : "--disable-gtk")
 
     system "./configure", *args
     system "make", "V=1", "install"
