e8326af573e0bf3d8315b74335d5d491801f58ff
Leon Klingele <git@leonklingele.de>
2019-01-22 21:28:29 +0100

    bash 5.0.2
    Closes #36312.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/bash.rb
+++ Formula/bash.rb
@@ -1,15 +1,33 @@
 class Bash < Formula
   desc "Bourne-Again SHell, a UNIX command interpreter"
   homepage "https://www.gnu.org/software/bash/"
-  url "https://ftp.gnu.org/gnu/bash/bash-5.0.tar.gz"
-  mirror "https://mirrors.ocf.berkeley.edu/gnu/bash/bash-5.0.tar.gz"
-  mirror "https://mirrors.kernel.org/gnu/bash/bash-5.0.tar.gz"
-  mirror "https://ftpmirror.gnu.org/bash/bash-5.0.tar.gz"
-  mirror "https://gnu.cu.be/bash/bash-5.0.tar.gz"
-  mirror "https://mirror.unicorncloud.org/gnu/bash/bash-5.0.tar.gz"
-  version "5.0.0"
-  sha256 "b4a80f2ac66170b2913efbfb9f2594f1f76c7b1afd11f799e22035d63077fb4d"
   head "https://git.savannah.gnu.org/git/bash.git"
+
+  stable do
+    url "https://ftp.gnu.org/gnu/bash/bash-5.0.tar.gz"
+    mirror "https://mirrors.ocf.berkeley.edu/gnu/bash/bash-5.0.tar.gz"
+    mirror "https://mirrors.kernel.org/gnu/bash/bash-5.0.tar.gz"
+    mirror "https://ftpmirror.gnu.org/bash/bash-5.0.tar.gz"
+    mirror "https://gnu.cu.be/bash/bash-5.0.tar.gz"
+    mirror "https://mirror.unicorncloud.org/gnu/bash/bash-5.0.tar.gz"
+    sha256 "b4a80f2ac66170b2913efbfb9f2594f1f76c7b1afd11f799e22035d63077fb4d"
+    version "5.0.2"
+
+    %w[
+      001 f2fe9e1f0faddf14ab9bfa88d450a75e5d028fedafad23b88716bd657c737289
+      002 87e87d3542e598799adb3e7e01c8165bc743e136a400ed0de015845f7ff68707
+    ].each_slice(2) do |p, checksum|
+      patch :p0 do
+        url "https://ftp.gnu.org/gnu/bash/bash-5.0-patches/bash50-#{p}"
+        mirror "https://mirrors.ocf.berkeley.edu/gnu/bash/bash-5.0-patches/bash50-#{p}"
+        mirror "https://mirrors.kernel.org/gnu/bash/bash-5.0-patches/bash50-#{p}"
+        mirror "https://ftpmirror.gnu.org/bash/bash-5.0-patches/bash50-#{p}"
+        mirror "https://gnu.cu.be/bash/bash-5.0-patches/bash50-#{p}"
+        mirror "https://mirror.unicorncloud.org/gnu/bash/bash-5.0-patches/bash50-#{p}"
+        sha256 checksum
+      end
+    end
+  end
 
   bottle do
     sha256 "d868e75449043299465b836dd340447d9db3d0a13f318c3ba1d3a57f02cad45b" => :mojave
