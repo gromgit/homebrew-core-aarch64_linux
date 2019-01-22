bc5050ba1301f6e0dec6cde20fb98bad831c9bad
Ivan Malopinsky <imsky@users.noreply.github.com>
2019-01-22 03:25:30 -0500

    git-fresh 1.12.0 (#36252)

--- Formula/git-fresh.rb
+++ Formula/git-fresh.rb
@@ -1,13 +1,14 @@
 class GitFresh < Formula
   desc "Utility to keep git repos fresh"
   homepage "https://github.com/imsky/git-fresh"
-  url "https://github.com/imsky/git-fresh/archive/v1.11.0.tar.gz"
-  sha256 "b8504e633c679b5112fc7ccfea354c4ceb01f9d6aa681abf09dc7c9564a7e38f"
+  url "https://github.com/imsky/git-fresh/archive/v1.12.0.tar.gz"
+  sha256 "199a35e695a839609159b08667c19d361716d882fc5e44a8ec164dc580fb1edc"
 
   bottle :unneeded
 
   def install
     system "./install.sh", bin
+    man1.install "git-fresh.1"
   end
 
   test do
