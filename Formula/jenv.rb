8aa2d2f2b452650d1210a24aff048d303be0c025
Gildas Cuisinier <hikage@hikage.be>
2019-01-23 04:48:15 +0100

    jenv 0.5.1 (#36314)

--- Formula/jenv.rb
+++ Formula/jenv.rb
@@ -1,8 +1,9 @@
 class Jenv < Formula
   desc "Manage your Java environment"
   homepage "https://www.jenv.be/"
-  url "https://github.com/jenv/jenv/archive/0.5.0.tar.gz"
-  sha256 "50834edaa5bef58261206f4fac154def57e1829b033fd30b5da607dbe58639bc"
+  url "https://github.com/jenv/jenv/archive/0.5.1.tar.gz"
+  sha256 "2962ed7d46f95259c4748c7cae6172dc54da52e782fe8e69c4867f4a70a88156"
+
   head "https://github.com/gcuisinier/jenv.git"
 
   bottle :unneeded
