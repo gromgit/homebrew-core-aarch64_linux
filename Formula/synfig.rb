710903a73942a4d342116d6156bec864b8af33bd
Viktor Szakats <commit@vszakats.net>
2019-01-23 23:09:51 +0000

    synfig: restore https for svn url(s)

--- Formula/synfig.rb
+++ Formula/synfig.rb
@@ -4,7 +4,7 @@ class Synfig < Formula
   url "https://downloads.sourceforge.net/project/synfig/releases/1.0.2/source/synfig-1.0.2.tar.gz"
   sha256 "34cdf9eac90aadea29fb2997e82da1c32713ab02940f7c8873330f894e167fb4"
   revision 5
-  head "http://svn.code.sf.net/p/synfig/code/"
+  head "https://svn.code.sf.net/p/synfig/code/"
 
   bottle do
     sha256 "7051446f2836f7de2f71508639cd7e82de5b71013e55801124f6e5ecf426cca4" => :mojave
