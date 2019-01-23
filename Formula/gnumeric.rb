81d66b7d18a7875ffe1371585938fc6fb5514975
Mike McQuaid <mike@mikemcquaid.com>
2019-01-23 09:06:41 +0000

    gnumeric: remove vendored formulae resources.
    This looks from the linked issue like it should now be able to use a
    modern `itstool` and `libxml2`.
    
    Closes #36325.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/gnumeric.rb
+++ Formula/gnumeric.rb
@@ -16,36 +16,11 @@ class Gnumeric < Formula
   depends_on "adwaita-icon-theme"
   depends_on "gettext"
   depends_on "goffice"
+  depends_on "itstool"
+  depends_on "libxml2"
   depends_on "rarian"
 
-  # Issue from 26 Nov 2017 "itstool-2.0.4: problem with gnumeric-1.12.35"
-  # See https://github.com/itstool/itstool/issues/22
-  resource "itstool" do
-    url "http://files.itstool.org/itstool/itstool-2.0.2.tar.bz2"
-    sha256 "bf909fb59b11a646681a8534d5700fec99be83bb2c57badf8c1844512227033a"
-  end
-
-  # For itstool
-  resource "py_libxml2" do
-    url "http://xmlsoft.org/sources/libxml2-2.9.7.tar.gz"
-    sha256 "f63c5e7d30362ed28b38bfa1ac6313f9a80230720b7fb6c80575eeab3ff5900c"
-  end
-
   def install
-    resource("py_libxml2").stage do
-      cd "python" do
-        system "python", "setup.py", "install", "--prefix=#{buildpath}/vendor"
-      end
-    end
-
-    resource("itstool").stage do
-      ENV.append_path "PYTHONPATH", "#{buildpath}/vendor/lib/python2.7/site-packages"
-      system "./configure", "--prefix=#{buildpath}/vendor"
-      system "make", "install"
-    end
-
-    ENV.prepend_path "PATH", buildpath/"vendor/bin"
-
     # ensures that the files remain within the keg
     inreplace "component/Makefile.in",
               "GOFFICE_PLUGINS_DIR = @GOFFICE_PLUGINS_DIR@",
