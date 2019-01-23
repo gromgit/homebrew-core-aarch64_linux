f9196caff26503ff1dbd55b44756e437ad7eb62b
Max <msal.coding@gmail.com>
2019-01-24 00:07:51 +0100

    lsd 0.12.0
    Fix test: Workaround for "terminal size" error.
    
    Closes #36349.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/lsd.rb
+++ Formula/lsd.rb
@@ -1,8 +1,8 @@
 class Lsd < Formula
   desc "Clone of ls with colorful output, file type icons, and more"
   homepage "https://github.com/Peltoche/lsd"
-  url "https://github.com/Peltoche/lsd/archive/0.11.1.tar.gz"
-  sha256 "7c9cdde049e02433a94cbfc3e7efd2b2ff7b08b9e5343849069b896ea646e40e"
+  url "https://github.com/Peltoche/lsd/archive/0.12.0.tar.gz"
+  sha256 "9b9a05452c23ccc94676e2e6b86f57805262496b14b6ec018df08996131eeec5"
 
   bottle do
     cellar :any_skip_relocation
@@ -18,7 +18,7 @@ class Lsd < Formula
   end
 
   test do
-    output = shell_output("#{bin}/lsd #{prefix}")
+    output = shell_output("#{bin}/lsd -l #{prefix}")
     assert_match "README.md", output
   end
 end
