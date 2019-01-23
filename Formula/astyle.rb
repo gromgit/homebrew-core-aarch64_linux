16350953faa4e004b5967723122e247a93f3a8da
Viktor Szakats <commit@vszakats.net>
2019-01-23 23:09:49 +0000

    astyle: restore https for svn url(s)

--- Formula/astyle.rb
+++ Formula/astyle.rb
@@ -3,7 +3,7 @@ class Astyle < Formula
   homepage "https://astyle.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/astyle/astyle/astyle%203.1/astyle_3.1_macos.tar.gz"
   sha256 "c4eebbe082eb2cb98f90aafcce3da2daeb774dd092e4cf8b728102fded8d1dcf"
-  head "http://svn.code.sf.net/p/astyle/code/trunk/AStyle"
+  head "https://svn.code.sf.net/p/astyle/code/trunk/AStyle"
 
   bottle do
     cellar :any_skip_relocation
