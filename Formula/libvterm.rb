ed47f274e3f4db27b8b469725eed67cf5ba5a503
Tobias Witt <tobias.witt@hhu.de>
2019-01-23 16:51:32 +0100

    libvterm 726
    Closes #36333.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/libvterm.rb
+++ Formula/libvterm.rb
@@ -1,8 +1,8 @@
 class Libvterm < Formula
   desc "C99 library which implements a VT220 or xterm terminal emulator"
   homepage "http://www.leonerd.org.uk/code/libvterm/"
-  url "http://www.leonerd.org.uk/code/libvterm/libvterm-0+bzr681.tar.gz"
-  sha256 "abea46d1b0b831dec2af5d582319635cece63d260f8298d9ccce7c1c2e62a6e8"
+  url "http://www.leonerd.org.uk/code/libvterm/libvterm-0+bzr726.tar.gz"
+  sha256 "6344eca01c02e2270348b79e033c1e0957028dbcd76bc784e8106bea9ec3029d"
 
   bottle do
     cellar :any
