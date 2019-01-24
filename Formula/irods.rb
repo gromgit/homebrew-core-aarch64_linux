35213ad0f73d96a61f6ae3e21e5e18bf28f906d9
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:15 +0000

    irods: remove references to pre-Mavericks.

--- Formula/irods.rb
+++ Formula/irods.rb
@@ -13,7 +13,6 @@ class Irods < Formula
     sha256 "510a0c5691702971c670c57c9ed11c5ca9371139b50c623cc950c0f8391a9737" => :el_capitan
     sha256 "490adc71118dc93c087aa685ddb873c3670575c679c834adc0a95c0b013772bc" => :yosemite
     sha256 "5d77816c581d12c4c30eb247e9b4a05f096347aa42ff4c069fa9aeff94678f87" => :mavericks
-    sha256 "6b0aa76607c2fec9b0007a6ad4fdca8ab53e7615edc01e3dccd35facbea9bb39" => :mountain_lion
   end
 
   depends_on "openssl"
