41c0b589e72f6650ad965cebc285ef344c39aaf2
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:35:24 +0000

    sslmate: remove references to pre-Mavericks.

--- Formula/sslmate.rb
+++ Formula/sslmate.rb
@@ -13,51 +13,15 @@ class Sslmate < Formula
 
   depends_on "python"
 
-  if MacOS.version <= :snow_leopard
-    depends_on "perl"
-    depends_on "curl"
-
-    resource "URI" do
-      url "https://cpan.metacpan.org/authors/id/E/ET/ETHER/URI-1.67.tar.gz"
-      sha256 "ab7f5fbc80da4ed9c46d63ed956c68a09e83dae30f20c2778c3e056d41883f9d"
-    end
-
-    resource "Term::ReadKey" do
-      url "https://cpan.metacpan.org/authors/id/J/JS/JSTOWE/TermReadKey-2.32.tar.gz"
-      sha256 "58b90e8908e686d03a161590c1dd870e8a1b005715ca8e6d5080a32459e1e9f8"
-    end
-  end
-
-  if MacOS.version <= :mountain_lion
-    resource "JSON::PP" do
-      url "https://cpan.metacpan.org/authors/id/M/MA/MAKAMAKA/JSON-PP-2.27300.tar.gz"
-      sha256 "5feef3067be4acd99ca0ebb29cf1ac1cdb338fe46977585bd1e473ea4bab71a3"
-    end
-  end
-
   resource "boto" do
     url "https://files.pythonhosted.org/packages/c8/af/54a920ff4255664f5d238b5aebd8eedf7a07c7a5e71e27afcfe840b82f51/boto-2.49.0.tar.gz"
     sha256 "ea0d3b40a2d852767be77ca343b58a9e3a4b00d9db440efb8da74b4e58025e5a"
   end
 
   def install
-    if MacOS.version <= :snow_leopard
-      ENV.prepend_path "PATH", Formula["perl"].bin
-    end
     ENV.prepend_create_path "PERL5LIB", libexec/"vendor/lib/perl5"
     xy = Language::Python.major_minor_version "python3"
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
-
-    perl_resources = []
-    perl_resources << "URI" << "Term::ReadKey" if MacOS.version <= :snow_leopard
-    perl_resources << "JSON::PP" if MacOS.version <= :mountain_lion
-    perl_resources.each do |r|
-      resource(r).stage do
-        system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}/vendor"
-        system "make"
-        system "make", "install"
-      end
-    end
 
     resource("boto").stage do
       system "python3", *Language::Python.setup_install_args(libexec/"vendor")
@@ -67,17 +31,12 @@ class Sslmate < Formula
     system "make", "install", "PREFIX=#{prefix}"
 
     env = { :PERL5LIB => ENV["PERL5LIB"] }
-    if MacOS.version <= :snow_leopard
-      env[:PATH] = "#{Formula["perl"].bin}:#{Formula["curl"].bin}:$PATH"
-    end
     env[:PYTHONPATH] = ENV["PYTHONPATH"]
     bin.env_script_all_files(libexec/"bin", env)
 
     # Fix failure when Homebrew perl is selected at runtime
-    unless MacOS.version <= :snow_leopard
-      inreplace libexec/"bin/sslmate",
-        "#!/usr/bin/env perl", "#!/usr/bin/perl"
-    end
+    inreplace libexec/"bin/sslmate",
+      "#!/usr/bin/env perl", "#!/usr/bin/perl"
   end
 
   test do
