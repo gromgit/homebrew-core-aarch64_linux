0c9a3106bf74b31c037a51ba853b7f950bfa376c
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:07 +0000

    gmtl: remove references to pre-Mavericks.

--- Formula/gmtl.rb
+++ Formula/gmtl.rb
@@ -20,8 +20,6 @@ class Gmtl < Formula
     sha256 "ee8d0c9f5f52453421a189c040459b5126a5b739231493a3e39d331c934c6478" => :sierra
     sha256 "8aa9f0f1fb77376dd333bb03e9c5a07f6457b76008a74018a932dca930148606" => :el_capitan
     sha256 "5e6d70f957f11e58d8b3cd24d5474a8bedc73e0aec6df13f85322f4fda8a1164" => :mavericks
-    sha256 "ffeb26dd58a9b05a4427ca02392f93f9d5b352af790e536e4d2989baa81e4faf" => :mountain_lion
-    sha256 "568a43df4aebd32ab9638d2725721b9c062bca0ecb778dbffb67fafd926d4a1a" => :lion
   end
 
   depends_on "scons" => :build
@@ -44,18 +42,18 @@ index 8326a89..2eb7ff0 100644
 --- a/SConstruct
 +++ b/SConstruct
 @@ -126,7 +126,9 @@ def BuildDarwinEnvironment():
- 
+
     exp = re.compile('^(.*)\/Python\.framework.*$')
     m = exp.search(distutils.sysconfig.get_config_var('prefix'))
 -   framework_opt = '-F' + m.group(1)
 +   framework_opt = None
 +   if m:
 +      framework_opt = '-F' + m.group(1)
- 
+
     CXX = os.environ.get("CXX", WhereIs('g++'))
- 
+
 @@ -138,7 +140,10 @@ def BuildDarwinEnvironment():
- 
+
     LINK = CXX
     CXXFLAGS = ['-ftemplate-depth-256', '-DBOOST_PYTHON_DYNAMIC_LIB',
 -               '-Wall', framework_opt, '-pipe']
@@ -63,12 +61,12 @@ index 8326a89..2eb7ff0 100644
 +
 +   if framework_opt is not None:
 +      CXXFLAGS.append(framework_opt)
- 
+
     compiler_ver       = match_obj.group(1)
     compiler_major_ver = int(match_obj.group(2))
 @@ -152,7 +157,10 @@ def BuildDarwinEnvironment():
           CXXFLAGS += ['-Wno-long-double', '-no-cpp-precomp']
- 
+
     SHLIBSUFFIX = distutils.sysconfig.get_config_var('SO')
 -   SHLINKFLAGS = ['-bundle', framework_opt, '-framework', 'Python']
 +   SHLINKFLAGS = ['-bundle']
@@ -76,5 +74,5 @@ index 8326a89..2eb7ff0 100644
 +   if framework_opt is not None:
 +      SHLINKFLAGS.extend([framework_opt, '-framework', 'Python'])
     LINKFLAGS = []
- 
+
     # Enable profiling?
