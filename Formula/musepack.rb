33511eba2d45afe334591524785951be7c09643d
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:55 +0000

    musepack: remove references to pre-Mavericks.

--- Formula/musepack.rb
+++ Formula/musepack.rb
@@ -13,7 +13,6 @@ class Musepack < Formula
     sha256 "33e4734a8714484a2c506241d91ab45145c19228abe51d355e0cbd60020ee11a" => :el_capitan
     sha256 "2e6bad894063f1178ff43b5cd10a899b82baa757228c3a51488793a4a22acdf2" => :yosemite
     sha256 "4ff9d23d8631300ff2619d9f1a7d99e07f8636c6e8d8db8dbf1335a21c1f7e27" => :mavericks
-    sha256 "ac50f1100cdb2c91846a5d5a8d364cdb31abaeb7823ccbfc6614829f96540c6d" => :mountain_lion
   end
 
   depends_on "cmake" => :build
