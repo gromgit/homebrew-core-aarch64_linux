056d0d6833ce950b4f1d35311fd2c1de1a1dff53
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:35:14 +0000

    rem: remove references to pre-Mavericks.

--- Formula/rem.rb
+++ Formula/rem.rb
@@ -12,7 +12,6 @@ class Rem < Formula
     sha256 "c9892df4f6aa5d58097e4cc4d62388ccbb1e0c02604b1139cfe829d47d992442" => :el_capitan
     sha256 "d9a6303ff3935923ba53d093e95387caaf24460a4cd7fb7d330fa5c3988b551c" => :yosemite
     sha256 "bf65e89ec4ca486b95f04c1c737627b2e0091af8a5c137795e521b96664d75e2" => :mavericks
-    sha256 "3c858e09bce1941b84ca3e5d77163cac4e3b7efcd6a1afcc72354a450c8ee495" => :mountain_lion
   end
 
   depends_on :xcode => :build
