bf18fcb2de4edd1eb65f263f8c0ee784ff0108b2
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:48 +0000

    md5deep: remove references to pre-Mavericks.

--- Formula/md5deep.rb
+++ Formula/md5deep.rb
@@ -13,7 +13,6 @@ class Md5deep < Formula
     sha256 "986dad46d2945aac775eb625e41b0236f2413b3924244d5e9aba445994c38687" => :el_capitan
     sha256 "227b8b8e4f4dd71972cd02062faefef90515b44ef5c3ce55f5c665cf679a26d1" => :yosemite
     sha256 "1bacd45d420975ff8b90d633e361b54c7f6a14776a41f175313360d31fb03ba4" => :mavericks
-    sha256 "5d68af238b95cffd8959418d804212c50a69cbae90c203d4abb56ceafea93399" => :mountain_lion
   end
 
   depends_on "autoconf" => :build
