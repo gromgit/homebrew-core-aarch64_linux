7b37e99c2a0c91bd824d475485926c30e6ecba72
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:15 +0000

    isc-dhcp: remove references to pre-Mavericks.

--- Formula/isc-dhcp.rb
+++ Formula/isc-dhcp.rb
@@ -38,7 +38,7 @@ class IscDhcp < Formula
     end
 
     # See discussion at: https://gist.github.com/1157223
-    ENV.append "CFLAGS", "-D__APPLE_USE_RFC_3542" if MacOS.version >= :lion
+    ENV.append "CFLAGS", "-D__APPLE_USE_RFC_3542"
 
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
