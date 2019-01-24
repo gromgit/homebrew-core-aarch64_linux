8dd932fbd345b0bea1809d6327b8acfc989eb1b4
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:17 +0000

    kanif: remove references to pre-Mavericks.

--- Formula/kanif.rb
+++ Formula/kanif.rb
@@ -12,7 +12,6 @@ class Kanif < Formula
     sha256 "ebe67d82aa8745a8ff29a82b9520e0fb4892a9645e17a12e8aeae4fda96cc8b4" => :el_capitan
     sha256 "7fc9517feb23867afe77730cdf28bb386570ccb5368d3b0c6b396214abf07e69" => :yosemite
     sha256 "3c003b7621fdef7d8451c3bab928e5b7bb0db0ff1fbe2c5d83b160a021ef93e6" => :mavericks
-    sha256 "86f8813209c51d84889c8d5a3d024137a1bdb6d9e95a68042ab875ee00e396bf" => :mountain_lion
   end
 
   depends_on "taktuk"
