26b9ad14bdaf0fa6c00e6636d766028b2663ba8f
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:33:45 +0000

    cmatrix: remove references to pre-Mavericks.

--- Formula/cmatrix.rb
+++ Formula/cmatrix.rb
@@ -13,7 +13,6 @@ class Cmatrix < Formula
     sha256 "da919a1964d6ef0633eac14bd7138ab91f6676d4dfc36fd5e27f956943714d22" => :el_capitan
     sha256 "14ae5c06eac81783ee61e3547d9de174f6742c688a254e172d7c2e566f14b426" => :yosemite
     sha256 "8479d25ddc608462c974bbc1a9fb229f6ffa99d19368fcd43f667bc6a6d8493f" => :mavericks
-    sha256 "49e2833e6d6967528475c3124f4b2927b7169030704d63811b34d20a01ea79e3" => :mountain_lion
   end
 
   def install
