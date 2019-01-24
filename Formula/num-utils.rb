39efe87dcc674c8d7255b94ab930fab01c4a1193
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:35:00 +0000

    num-utils: remove references to pre-Mavericks.

--- Formula/num-utils.rb
+++ Formula/num-utils.rb
@@ -13,7 +13,6 @@ class NumUtils < Formula
     sha256 "f2021913b67fe8ece09b204aa2715a4c8067121908695cee830b363a52a987eb" => :el_capitan
     sha256 "41a55ac6c46aca45473ca365443fb1fd2d77fdb6e4540edbe849d723d31ba0e0" => :yosemite
     sha256 "188ff1f94691f8bf5099ec1012d4732be8fa385bf738671f86780376dd2597b9" => :mavericks
-    sha256 "ef3654fed50aff148b0264fdfc75f25310702e8931cc6dcecb4bb3ebce657f7a" => :mountain_lion
   end
 
   conflicts_with "normalize", :because => "both install `normalize` binaries"
