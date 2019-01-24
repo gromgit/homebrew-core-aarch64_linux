6aa44c2dd062961594e6ccca4804784aa14b2587
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:27 +0000

    libidl: remove references to pre-Mavericks.

--- Formula/libidl.rb
+++ Formula/libidl.rb
@@ -13,7 +13,6 @@ class Libidl < Formula
     sha256 "0c81487e7c93733097a62e6c693943fda8b13ce03ff6a6972a320c711d561e39" => :el_capitan
     sha256 "356ad466ab477116e8f030ef75b70bc46321f68ec1f077f9b06dca3bb076e499" => :yosemite
     sha256 "dffe56b6e1076ac49a43ff662517b974f97e47707906362629176cfdc09b6f91" => :mavericks
-    sha256 "7aee2ea7f3760687e9acd4a35c393e240f8f59e94a36c2a31812d15a41cfcac8" => :mountain_lion
   end
 
   depends_on "pkg-config" => :build
