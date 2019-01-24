2c4197e2bc1584db0d9c6221c72fcad5230b2870
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:44 +0000

    m2c: remove references to pre-Mavericks.

--- Formula/m2c.rb
+++ Formula/m2c.rb
@@ -12,7 +12,6 @@ class M2c < Formula
     sha256 "4aa6ac4f5fd855f4f84d5577ff6f79495fda9edc2ce5335c64bd96a881975eb0" => :el_capitan
     sha256 "67659bd6a5fe922c1b34d5068a5cecbfee1f804e5ff432e32c8682a04029ccac" => :yosemite
     sha256 "7bf62153eeb0976851785db04e1319f745709294aa9d0bc99e47ffee3eba1315" => :mavericks
-    sha256 "6db02ad1e1a355edfd1770d29952812baac666d6d2a7d3a599357f796eb7d891" => :mountain_lion
   end
 
   # Hacks purely for this 0.7 release. Git head already fixes installation glitches.
