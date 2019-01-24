9ab9678c1aeaff9ef8e7ecd41ee9c7ef09b1852e
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:35:38 +0000

    wirouter_keyrec: remove references to pre-Mavericks.

--- Formula/wirouter_keyrec.rb
+++ Formula/wirouter_keyrec.rb
@@ -12,7 +12,6 @@ class WirouterKeyrec < Formula
     sha256 "3982522d8e15ced547c4f1d3019fe3ca6ddaa88d33fad03d2c97a53c849ec217" => :el_capitan
     sha256 "65d21ba4cb167dd2cca395dd5b51edc1ddd0df06fc65843cd2d2eba9e307ea11" => :yosemite
     sha256 "b5740e7929fc911881e007103921c712483971accb581bd5fdb86357f65b8312" => :mavericks
-    sha256 "fb6bc3066b31fde62280752b2795e13d8850011f5910ad28dbb1c9e15208af74" => :mountain_lion
   end
 
   def install
