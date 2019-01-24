9575d91382eb82e5eaa17afac1a2f85041fdc7df
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:35:07 +0000

    pipebench: remove references to pre-Mavericks.

--- Formula/pipebench.rb
+++ Formula/pipebench.rb
@@ -15,7 +15,6 @@ class Pipebench < Formula
     sha256 "353cabdaf04a41e2169c1e489cd038f9fbe7f33cfd24a5a0b3068449ccc3446d" => :el_capitan
     sha256 "a999c7ba2978e60d38bdeec63d1f1a8b2667cb0d77d35c4da2e57212a37b85ac" => :yosemite
     sha256 "c5b0805588d8a8d9047d30b9330dd0be0215bfe694ed662cf11fa84595bba85c" => :mavericks
-    sha256 "217b9b59e5592718c530e4843cb0dfdc830f9f8fc175766abfd3cade97c66c80" => :mountain_lion
   end
 
   def install
