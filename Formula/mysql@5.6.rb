8978dc9c66a490e91486a671299459c4c5b383c2
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:57 +0000

    mysql@5.6: remove references to pre-Mavericks.

--- Formula/mysql@5.6.rb
+++ Formula/mysql@5.6.rb
@@ -14,7 +14,6 @@ class MysqlAT56 < Formula
   keg_only :versioned_formula
 
   depends_on "cmake" => :build
-  depends_on "pidof" unless MacOS.version >= :mountain_lion
   depends_on "openssl"
 
   def datadir
