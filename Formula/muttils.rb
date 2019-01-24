0ae67a2d74c3d4cbcb3a7b7a8a45a7e697d66a81
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:56 +0000

    muttils: remove references to pre-Mavericks.

--- Formula/muttils.rb
+++ Formula/muttils.rb
@@ -13,7 +13,6 @@ class Muttils < Formula
     sha256 "6843b7372098864c53199d84759724bcbf2c1ab36fc74c15e56405351e43fbc9" => :el_capitan
     sha256 "d976f7445a3142ff1c311cf19302b5fcc2976b0a26c0bb48e57e157bd4c7002f" => :yosemite
     sha256 "01f0c26274540336fa829a8718bb0c3a2a5b5aa3c96c1f4ec7cf79e6263b837b" => :mavericks
-    sha256 "63e819c0bb96a56ed0f159ab816aeac84805a52333bd23298a17cd2abddcb17b" => :mountain_lion
   end
 
   depends_on "python@2" # does not support Python 3
