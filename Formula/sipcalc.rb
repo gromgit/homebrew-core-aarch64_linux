dd921aa0221bd792395728142915d748d3a4d84f
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:35:20 +0000

    sipcalc: remove references to pre-Mavericks.

--- Formula/sipcalc.rb
+++ Formula/sipcalc.rb
@@ -12,7 +12,6 @@ class Sipcalc < Formula
     sha256 "56aa686252ac703ed3dbe91f5737ec4d4b95d52516f4ab52947df15b77d1c58f" => :el_capitan
     sha256 "6b2fc300755693d382fd5ea971c272a7c8c7bff49614dd88d8db4270aa496012" => :yosemite
     sha256 "7ddf7b200984de97143828faf6385314a2ff3f4436432d810e5aaf7dfe44e78c" => :mavericks
-    sha256 "be6f69bdc8613a2f6f98279445ac443517b7ffb2746268c09e0d2cdc61bbd0e8" => :mountain_lion
   end
 
   def install
