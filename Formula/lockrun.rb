60db2393aeb6492746b0195c9c304e0f7e69722f
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:42 +0000

    lockrun: remove references to pre-Mavericks.

--- Formula/lockrun.rb
+++ Formula/lockrun.rb
@@ -13,7 +13,6 @@ class Lockrun < Formula
     sha256 "10782346442c28b235f80579c1b0dac3ec784fb151f7bef475757c1bde944b16" => :el_capitan
     sha256 "8f2914ed87c42a369b3870b5688720cf0cc7382ae6428452ba32fdf0e422ab57" => :yosemite
     sha256 "c319dba85122ea12d120a7ea3acbdc1c50ee35f2eadb274aa5ec59622b026ca0" => :mavericks
-    sha256 "382ed13c3dcbf64143ea6058f593338af2b6449a2cba889d64ac988d08e2b139" => :mountain_lion
   end
 
   def install
