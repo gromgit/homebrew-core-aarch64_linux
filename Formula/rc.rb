97a62ee0519aa8990178b86a82d5fba79daed92a
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:35:12 +0000

    rc: remove references to pre-Mavericks.

--- Formula/rc.rb
+++ Formula/rc.rb
@@ -13,7 +13,6 @@ class Rc < Formula
     sha256 "34b3426f92ff6b50490a479cb55473e227b67351a853e8627c8e487b4fe21989" => :el_capitan
     sha256 "1628bbad2fa8417318ee488a748a1ae769606baf950300c895e4592cbe013edf" => :yosemite
     sha256 "6cd2807091b6e1e8359ebedab8d211f00d0ac84825f16d116bd4500c6dd5b3f4" => :mavericks
-    sha256 "c09ab4ed5d45ef028bb235e246e32b7e5d0603952a7c9e068ba64faf78e16cf1" => :mountain_lion
   end
 
   def install
