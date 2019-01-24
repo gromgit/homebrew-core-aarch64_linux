19bccfc773aa0e432dfa4e3cffd519a86668ba11
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:35:16 +0000

    rpl: remove references to pre-Mavericks.

--- Formula/rpl.rb
+++ Formula/rpl.rb
@@ -12,7 +12,6 @@ class Rpl < Formula
     sha256 "175e1f127c8c707b0d90c3c7e4399cc5c1e18410bf8b7f6ec9340dbca4c16e4b" => :el_capitan
     sha256 "d718355e56dd13c690f1d5a0541b5f051518f65b953aade9c525853a19266a61" => :yosemite
     sha256 "b0c4dbf06500053703ee4a8c8e751d43c435f0e3b0e25d2d328d310dcf490c23" => :mavericks
-    sha256 "c59b98ff51670e96114daa8aa2c8984e35bd2bc1c2c01db9206bef9728a13624" => :mountain_lion
   end
 
   def install
