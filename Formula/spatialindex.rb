3395708cd6db6e62a9b27824c429a2e6c7805427
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:35:22 +0000

    spatialindex: remove references to pre-Mavericks.

--- Formula/spatialindex.rb
+++ Formula/spatialindex.rb
@@ -12,7 +12,6 @@ class Spatialindex < Formula
     sha256 "34d1e02dd4133ed67a8a4c299044e277e1e9cfc982962c50c44c751723eb85cb" => :el_capitan
     sha256 "907f40e614218622fd9fecc0a542adcdf768446a198ef4cc972b30a7eb5e6cd3" => :yosemite
     sha256 "33e053a03ea77bc87c3aab4f8319461baab56824e3c933cb09398e6df1b542ba" => :mavericks
-    sha256 "401c416f243996ac4f6bca61a871aec7fb8811f8aef6073dfc62c64dfe2650f7" => :mountain_lion
   end
 
   def install
