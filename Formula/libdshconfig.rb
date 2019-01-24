c6e9f3fa346b05e627c1cb2b438064a9358df6ed
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:23 +0000

    libdshconfig: remove references to pre-Mavericks.

--- Formula/libdshconfig.rb
+++ Formula/libdshconfig.rb
@@ -13,7 +13,6 @@ class Libdshconfig < Formula
     sha256 "a26ea1d1cefed24fd890bbc65f9a11d171cdbcb1c00936562255e2adfe29205f" => :el_capitan
     sha256 "7f4e5e77fc14d6920bd212e9c812c7dad51ad1dbe1542f1c0e4999928db9ad3f" => :yosemite
     sha256 "c1c35337d7b2bebb59d07a8f792493f1da89f2701eae58d382bb24edfd2c73d3" => :mavericks
-    sha256 "b02e3d90eb1b2a6ee33dd0b6c7648baf9dea117be5b111841ea3fdb98ba931b4" => :mountain_lion
   end
 
   def install
