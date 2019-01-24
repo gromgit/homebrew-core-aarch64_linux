db52a0cf6ec35a8038aa5487a44262e3cdab971c
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:33 +0000

    libreplaygain: remove references to pre-Mavericks.

--- Formula/libreplaygain.rb
+++ Formula/libreplaygain.rb
@@ -14,7 +14,6 @@ class Libreplaygain < Formula
     sha256 "58b52d360c2f37f3ab3a50c4a2fe72b9a370bd951d52939f8853a5ef49fcc322" => :el_capitan
     sha256 "d47338c5b86daabf3e2e05ab9dd2443c04c1233f3319307e8e5d545b24dcf722" => :yosemite
     sha256 "dc3f2c3823c5552bddad7b1727b9086dc2fe79e8fa13987b420d1621c97e2bce" => :mavericks
-    sha256 "4ce4390dc0c3ba503381bf256b942207dc706fc2e8e9e464aceb7ecf916f9841" => :mountain_lion
   end
 
   depends_on "cmake" => :build
