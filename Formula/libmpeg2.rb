5d4b539d43f7a4e8d2edb0771d85e8853c071fc3
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:28 +0000

    libmpeg2: remove references to pre-Mavericks.

--- Formula/libmpeg2.rb
+++ Formula/libmpeg2.rb
@@ -13,7 +13,6 @@ class Libmpeg2 < Formula
     sha256 "841e93dd99b97b96b475aedff29b58f5be5c4156869b1c0212e5d7ed8dd7f481" => :el_capitan
     sha256 "3d07c45554ff34036b9eae5a31dc5417c15109ba134d414035b1bf6f9dda7c79" => :yosemite
     sha256 "f6a868beb10fbf84d3eb1af556478ecbfb238d28608a53b99e607c02910e5e49" => :mavericks
-    sha256 "eb8eb837e902d0fbdd6c89c72a2d7f534e0edf9a0895807f7f05a2505b900e5f" => :mountain_lion
   end
 
   depends_on "sdl"
