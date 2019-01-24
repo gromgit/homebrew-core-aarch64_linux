d60479e117b8498f8120130b4c74073507bbb61f
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 13:11:20 +0000

    task: remove checks for old compilers.

--- Formula/task.rb
+++ Formula/task.rb
@@ -17,8 +17,6 @@ class Task < Formula
   depends_on "cmake" => :build
   depends_on "gnutls"
 
-  needs :cxx11
-
   def install
     system "cmake", ".", *std_cmake_args
     system "make", "install"
