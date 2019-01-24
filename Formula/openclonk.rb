83d7316b61528142faec594d190b8af1595f2c11
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 13:11:02 +0000

    openclonk: remove checks for old compilers.

--- Formula/openclonk.rb
+++ Formula/openclonk.rb
@@ -27,8 +27,6 @@ class Openclonk < Formula
   # Requires some C++14 features missing in Mavericks
   depends_on :macos => :yosemite
 
-  needs :cxx11
-
   def install
     ENV.cxx11
     system "cmake", ".", *std_cmake_args
