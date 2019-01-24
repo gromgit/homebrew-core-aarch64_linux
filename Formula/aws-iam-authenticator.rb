054f532e3f3af8eebc06c8db6e777d8a3ebc2fc8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-24 23:37:08 +0000

    aws-iam-authenticator: add 0.3.0 bottle.

--- Formula/aws-iam-authenticator.rb
+++ Formula/aws-iam-authenticator.rb
@@ -3,6 +3,13 @@ class AwsIamAuthenticator < Formula
   homepage "https://github.com/kubernetes-sigs/aws-iam-authenticator"
   url "https://github.com/kubernetes-sigs/aws-iam-authenticator/archive/v0.3.0.tar.gz"
   sha256 "91e801c1b1c097f663ebd75897cd5ea19e709bc5ab806e1169ffc508c5a79271"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d5a35beb29c75a100afad5bb2706ac57fe5cc92ee56296612c34348c2ea4c8d7" => :mojave
+    sha256 "6b25272261dfd33c355ff55f0928499aa9d71a62d6609729274207578e4562e4" => :high_sierra
+    sha256 "c57e49a16d76e4af19fb05ff4547511639dac76aa1cb9e82948af0b1433f866f" => :sierra
+  end
+
   depends_on "dep" => :build
   depends_on "go" => :build
 
