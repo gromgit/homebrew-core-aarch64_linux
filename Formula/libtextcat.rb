0a64f986dfd26b91721753eca857969b32f6aeab
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:37 +0000

    libtextcat: remove references to pre-Mavericks.

--- Formula/libtextcat.rb
+++ Formula/libtextcat.rb
@@ -14,7 +14,6 @@ class Libtextcat < Formula
     sha256 "afa51f83d0a3c96ffc6f6c35011c864347f31d2c3aea987102c59f0257177072" => :el_capitan
     sha256 "1a63f24b16949843f6a3f6c17d9467208a471cfa6bf1b193738fa94c2d320f02" => :yosemite
     sha256 "e7880fa731747f117a943fd99bd41da25bae3e6440316d782c4798cf3f55e0b7" => :mavericks
-    sha256 "645dab2e1766e0ea0fc3f6bc897e372ca26826a8ca136463ac864da1ddf74202" => :mountain_lion
   end
 
   def install
