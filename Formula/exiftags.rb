fe63f6b237ba63ed9f637f85cad5502a5c683d2d
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:33:56 +0000

    exiftags: remove references to pre-Mavericks.

--- Formula/exiftags.rb
+++ Formula/exiftags.rb
@@ -12,7 +12,6 @@ class Exiftags < Formula
     sha256 "47d75e83f89d0db4a54d779d9c9820fbb788c102738824e86b83a441d9a60af8" => :el_capitan
     sha256 "23a94f2c2694d52ef393e751e23a01c4ed23c0ca7004b6597546047310e73f53" => :yosemite
     sha256 "2ca339b45b3ea518ca5b39262b4c68cc54187a2bfca7d7a52eded5685c81b3c9" => :mavericks
-    sha256 "d5c5f565b2b7f0d6f7161801680095c6fefb3c7234cb9a9046a263dd8d983cff" => :mountain_lion
   end
 
   def install
