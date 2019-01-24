70e1763cf93495ec07cbb9845255ac5e89c35992
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:10 +0000

    hamlib: remove references to pre-Mavericks.

--- Formula/hamlib.rb
+++ Formula/hamlib.rb
@@ -12,7 +12,6 @@ class Hamlib < Formula
     sha256 "d4e86dbc6d9bf5e0b4a1c1bce2471e90becf05b19b1c595952c94b3bda91e0db" => :el_capitan
     sha256 "31a75a43cf17a17d35ee0c57048522e73de7c69f43279b45c766a903b5239372" => :yosemite
     sha256 "6d9dd131db4baa70355822033257f822e029aa167b6c43643419bd75ef06395a" => :mavericks
-    sha256 "0e78439f806f68ae1ae36fa3e5315d13cbabf6da714571eec9816945a90b2985" => :mountain_lion
   end
 
   depends_on "pkg-config" => :build
