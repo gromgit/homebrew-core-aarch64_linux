2560f52bb7c470a48f93f07882958a74c2937531
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:35:11 +0000

    quotatool: remove references to pre-Mavericks.

--- Formula/quotatool.rb
+++ Formula/quotatool.rb
@@ -12,7 +12,6 @@ class Quotatool < Formula
     sha256 "da5c90f85204fa90a38da073765ec5c0f0a20333bcdcd131d79b682afa74233f" => :el_capitan
     sha256 "8af3549d42247f0b79458c96978f8f5e5fbe04cc1c0dd86f84accdf03e8e510f" => :yosemite
     sha256 "724a3fc561188de5e0e050f7459480cc8c613d399faee5290ec7a68b9715960d" => :mavericks
-    sha256 "29271c3bc97b9623264fe2f67e821f1810442f11b15ad1b7f231472905d078e4" => :mountain_lion
   end
 
   def install
