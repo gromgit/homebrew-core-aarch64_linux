b1033552c57a57e275e220f335c20f56eed94e22
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:33:34 +0000

    avce00: remove references to pre-Mavericks.

--- Formula/avce00.rb
+++ Formula/avce00.rb
@@ -12,7 +12,6 @@ class Avce00 < Formula
     sha256 "45f18e289431af4de0d1e96c1fadd6a056e80907a1650654f8ee0dd1dafab401" => :el_capitan
     sha256 "56e15b29411b2947d9a842d91ae713e16566aa59e297e06f7d4de4b301847e66" => :yosemite
     sha256 "55990b93f7fe4639c6fdf29c4cc6c5791c6178c8661e22ef9e0dd64606532f56" => :mavericks
-    sha256 "4f114d3d8872cbf9e2df2c2ed2d4962b65b39efc568faf78eb5b2f47552a39da" => :mountain_lion
   end
 
   conflicts_with "gdal", :because => "both install a cpl_conv.h header"
