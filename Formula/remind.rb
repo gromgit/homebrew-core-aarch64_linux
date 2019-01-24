2b6178ecc521c8a23fad21fcd55a28978213ddbc
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:35:14 +0000

    remind: remove references to pre-Mavericks.

--- Formula/remind.rb
+++ Formula/remind.rb
@@ -12,7 +12,6 @@ class Remind < Formula
     sha256 "c720948c84996b651176968c096492da2242cb6b57226e04e25f9251c782491a" => :el_capitan
     sha256 "b72ffda6998a1c203686b82b8e07c3132bc380fb9126a2ca22254608d3c418c8" => :yosemite
     sha256 "958eafdd458799e788457837d01ef387c5368ffee6f9a6b1ce363678a9cbc8a5" => :mavericks
-    sha256 "fb78fa7e3df893822473b56d79d64d48ff5827c7df3ce6d518985262c99d3056" => :mountain_lion
   end
 
   conflicts_with "rem", :because => "both install `rem` binaries"
