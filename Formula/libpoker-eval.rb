cd0cb7236b4287591391cd6a71a14bd532352689
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:33 +0000

    libpoker-eval: remove references to pre-Mavericks.

--- Formula/libpoker-eval.rb
+++ Formula/libpoker-eval.rb
@@ -14,7 +14,6 @@ class LibpokerEval < Formula
     sha256 "67b105600a8e29ed2d38421bc27340ff6e9092806f6458f0ddd6a27de0bcfb9c" => :el_capitan
     sha256 "b15086546ac1ac0310e3113231bfcc2c9de0d23474be8a1a1b4663e6bc8f713f" => :yosemite
     sha256 "9bbfb3886a4e530455dbf53581aecd0df8c86a2f80a444692441449c30f76d92" => :mavericks
-    sha256 "32281cedd42b9a1a99ff146ad97f00ae682f75249f7e16166ab8bd4ca0378e31" => :mountain_lion
   end
 
   def install
