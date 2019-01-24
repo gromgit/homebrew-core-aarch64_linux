828fa0c1ff0b30f9d20379d98cea0fb1e69ff61a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-24 19:49:06 +0000

    monetdb: update 11.31.13 bottle.

--- Formula/monetdb.rb
+++ Formula/monetdb.rb
@@ -5,9 +5,9 @@ class Monetdb < Formula
   sha256 "f9fbf63ed7e6c306868b289c3fda8c3a8b6d3fc6bef589418940b2a21fd7c283"
 
   bottle do
-    sha256 "47e2a900b158e95ec44580c381e5bb74798859e1171e60e980e7ca05bbeb5173" => :mojave
-    sha256 "5ad55a48403afa11ba67d7429b002ca3c880ed9b19d669fda02905554d2368ce" => :high_sierra
-    sha256 "96567ed0d7ed70b80b97dc140f0ca2e05c4bd92af8fa8ef113c485e5e16f901f" => :sierra
+    sha256 "c289a7faa889c3adf228d183a5310ef7187d679f93fde340d263f4ce3fb835d6" => :mojave
+    sha256 "16557092c3c437c8b2cb316c692a1e52b21db6370caf03af060a364b6550184b" => :high_sierra
+    sha256 "309eadb339243a3c262115df347685a4111fac62963fea437676db78d4f4d978" => :sierra
   end
 
   head do
