1af275528f0014286596478b5d0bcbab2ac1a7ee
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-24 20:05:52 +0000

    ctop: update 0.7.2 bottle.

--- Formula/ctop.rb
+++ Formula/ctop.rb
@@ -6,10 +6,9 @@ class Ctop < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3c581ea1e2572f43ac4410fcbb5f39e706a031d45998ad54cc1ba474fa2c1cb1" => :mojave
-    sha256 "0d2f00fb47786a5a95578da90f80979683e76d431ad90c62088d082af4ecdeff" => :high_sierra
-    sha256 "3d3968570b78e240fe94f16eb4235f016ba1f5943e3030eb177b0a7a71989934" => :sierra
-    sha256 "149adb4dfa66b7492992c07f1007743f69a6069d380a43d0e14dfffba7bc8988" => :el_capitan
+    sha256 "0e168b17be6893484012554531584934ddcb4c41a81ff5ffe9ff6b72f8a24d39" => :mojave
+    sha256 "eccc5c1766fc7c558b313f3c4d928615176e252e12dcc329d985c4320feb5813" => :high_sierra
+    sha256 "fa9113c826584235dfbe6e96092c3c0fe74dfb5ae37719b0f5f1726191f3f051" => :sierra
   end
 
   depends_on "dep" => :build
