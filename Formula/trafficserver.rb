aad1c4b48cbf0417b284f6c20b1d5ec48775f51b
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 13:11:21 +0000

    trafficserver: remove checks for old compilers.

--- Formula/trafficserver.rb
+++ Formula/trafficserver.rb
@@ -5,8 +5,6 @@ class Trafficserver < Formula
   stable do
     url "https://www.apache.org/dyn/closer.cgi?path=trafficserver/trafficserver-7.1.4.tar.bz2"
     sha256 "1c5213f8565574ec8a66e08529fd20060c1b9a6cd9b803ba9bbb3b9847651b53"
-
-    needs :cxx11
   end
 
   bottle do
