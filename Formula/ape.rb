d0b874243c31485ef6f8c915ded221bf84dc21ce
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:33:32 +0000

    ape: remove references to pre-Mavericks.

--- Formula/ape.rb
+++ Formula/ape.rb
@@ -11,7 +11,6 @@ class Ape < Formula
     sha256 "259b19e211ff6d6ffc376db0b3696a912a6ac48dca83cbcbe525c78e56755c82" => :el_capitan
     sha256 "3859216e566e6faaccc7183d737527dd4785260a698c8344520e7951baebca76" => :yosemite
     sha256 "83c7ef23309dec2e7bd4bec3ae75b6f0e04fcfecbda489c90810b6948eb3bb28" => :mavericks
-    sha256 "136d65f58cb956b95a6ccb9517a5affe69c3491d283478708ca88091abb3103f" => :mountain_lion
   end
 
   def install
