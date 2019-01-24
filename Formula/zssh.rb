7c7c6aaaa14d9fa86fd74cea6fc0cdd5142beeb1
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:35:39 +0000

    zssh: remove references to pre-Mavericks.

--- Formula/zssh.rb
+++ Formula/zssh.rb
@@ -12,7 +12,6 @@ class Zssh < Formula
     sha256 "04212f19c1d9a6b97fd56ffe937606f1779849fdf04b93e3f285889599845c8f" => :el_capitan
     sha256 "94280569f9e1c1deb9d8c3be4256cd501399fd51758f8e2ea6d77fd9f1b6ef2e" => :yosemite
     sha256 "94b16bb29616a839134527fd869ac40a8fb5fa88b0048d1a93a828e306c2a270" => :mavericks
-    sha256 "e81d8a0d4c8107898aff0cda9abbf4a2caaf098f16c37bd92aa168943c7e6554" => :mountain_lion
   end
 
   depends_on "lrzsz"
