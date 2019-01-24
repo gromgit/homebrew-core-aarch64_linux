92d3fd9f863b4a4b36712a83176bde2aee248c23
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:33:43 +0000

    cidrmerge: remove references to pre-Mavericks.

--- Formula/cidrmerge.rb
+++ Formula/cidrmerge.rb
@@ -12,7 +12,6 @@ class Cidrmerge < Formula
     sha256 "7e607252679cd1648e6c9f48ebbeaa2379ce089ad87815bd6636e65dcedebc7b" => :el_capitan
     sha256 "20c6f57fc6081c8d27d2e68b81e3d4c5cd68e7c799dc30e076f45ee71b42e69d" => :yosemite
     sha256 "89b2d5b31bd190e0aa8837b84f3a684cf01b4501321e898507e5d8dd809f09d7" => :mavericks
-    sha256 "c4d8b6b3a17f6117b9df0c9159cc55b11d060b3abf475cbd1962b35d0ef3292a" => :mountain_lion
   end
 
   def install
