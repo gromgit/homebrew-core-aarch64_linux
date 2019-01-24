72ab1e0ec9f6d2b574164b5cb6d2d328af505029
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:35:35 +0000

    vilistextum: remove references to pre-Mavericks.

--- Formula/vilistextum.rb
+++ Formula/vilistextum.rb
@@ -12,7 +12,6 @@ class Vilistextum < Formula
     sha256 "d46bae51c7e9a7193a735660af8583960c98e50f03aa33c8a9d81c22b2d9bf61" => :el_capitan
     sha256 "77ab66b58db8649e9444584b5ee85e6c6c7badeb665425263c50282367eea003" => :yosemite
     sha256 "be608cbb3892c81f92392253a86456f042e804b55c58e1331a01531781137f74" => :mavericks
-    sha256 "40d71172fde61fe9bb297b411fabb64bcc2b915036da2a531c56e1d84382131d" => :mountain_lion
   end
 
   def install
