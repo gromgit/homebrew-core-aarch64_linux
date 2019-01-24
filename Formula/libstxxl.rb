e7554ea3dadfc9538192957a0fd3c69f4fb766d8
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:34 +0000

    libstxxl: remove references to pre-Mavericks.

--- Formula/libstxxl.rb
+++ Formula/libstxxl.rb
@@ -12,7 +12,6 @@ class Libstxxl < Formula
     sha256 "8753b0c9ec11f1f6195fa52c346b4d6015652931908156dcdb4468cc94103d1c" => :el_capitan
     sha256 "fbefd1ceb6f328ef77b9a005f5bc3c93fe23fa68d60b4a5e126a68a6a9d8e17d" => :yosemite
     sha256 "5a64626281265368e41f51089271c031ae8782651d65fe012f8a86949e37573b" => :mavericks
-    sha256 "2d5d3e4c697422777d98708aa5db58a1d26b55165f0e633700bf9934fbde5c9e" => :mountain_lion
   end
 
   depends_on "cmake" => :build
