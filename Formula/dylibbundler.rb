35e899572e7e57a689be0fc8ca8e98465035f22a
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:33:54 +0000

    dylibbundler: remove references to pre-Mavericks.

--- Formula/dylibbundler.rb
+++ Formula/dylibbundler.rb
@@ -14,7 +14,6 @@ class Dylibbundler < Formula
     sha256 "5dff018e62a9787871e45f4ae976358cfc3f7f85972a0aa0d4e039f97d4b8e0f" => :el_capitan
     sha256 "eee9c829e932d8d25ded1e249bbf372ebfa0c9911dd3adc11a642184ecb6a6b7" => :yosemite
     sha256 "49c1600c49ff7b10bdae8dc7351496680a9a4fb434ce6cd7393d10008506393e" => :mavericks
-    sha256 "351213e36062eee71018bb8ff9c0fa02d351fad63993c46f984223426ffe3efd" => :mountain_lion
   end
 
   def install
