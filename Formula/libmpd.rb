84e8dc42323b30f8be6d078be5829880408185ca
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:28 +0000

    libmpd: remove references to pre-Mavericks.

--- Formula/libmpd.rb
+++ Formula/libmpd.rb
@@ -13,7 +13,6 @@ class Libmpd < Formula
     sha256 "d4b932dc975f7fe87d8e26ebe9080d3633c33a66438c29d0403160adb6c7ada5" => :el_capitan
     sha256 "36471b19608eea97bc9916fdb65937fbb385ade1bf43aac4c01031d3c3c1192f" => :yosemite
     sha256 "8e79457e677bf003a8e5374f1f7ccffba5ef237e577a0e0831ccb2036101b357" => :mavericks
-    sha256 "85c97dbfb2a3a419495e702df451b00bf84e355d69c2e8512a54014ff702f45c" => :mountain_lion
   end
 
   depends_on "pkg-config" => :build
