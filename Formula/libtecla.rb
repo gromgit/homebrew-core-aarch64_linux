fd4a53916d818bd62f3f1ca2b3c07e480c3ce47e
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:37 +0000

    libtecla: remove references to pre-Mavericks.

--- Formula/libtecla.rb
+++ Formula/libtecla.rb
@@ -12,7 +12,6 @@ class Libtecla < Formula
     sha256 "3ceb3942ea4ae1434dcc0aea00fa58b6f16787bc1a0067e9497ad4cb050f771a" => :el_capitan
     sha256 "836d6100343197540f079ea7f6b9e5641fd8efc4e331d3492f8be4cd41ced6e9" => :yosemite
     sha256 "d7f9b95bbe7540504751d42589e8500a77d15dc3e6b2f7fe501ed872172f1129" => :mavericks
-    sha256 "a7caf2863506fcb0c8ded748375116957c0a12e45212dea9cce16b47adf00883" => :mountain_lion
   end
 
   def install
