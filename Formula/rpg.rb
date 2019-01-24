39dc5653d83e96daebd8f73e6bb44f02a33522a2
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:35:16 +0000

    rpg: remove references to pre-Mavericks.

--- Formula/rpg.rb
+++ Formula/rpg.rb
@@ -13,7 +13,6 @@ class Rpg < Formula
     sha256 "be6550c915e361dcc0e020048f9a5f4ea37f1c5890a526282a3d70c610594981" => :el_capitan
     sha256 "5c1af29955697dcd46ff58fd70f9aca986b977f3cc17f638822c81289f180df2" => :yosemite
     sha256 "2ebf1a744c3c072c107883f565c04154b3e530c93868bb438bb90a1be35a4cb7" => :mavericks
-    sha256 "d32135e52bef3d16d6538dd8050cef4e1081474cc1156462a900ab2afa28b448" => :mountain_lion
   end
 
   def install
