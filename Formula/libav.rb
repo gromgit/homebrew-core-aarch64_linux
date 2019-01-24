f8373a16114cc2768a24140a7d8e070a2af7bb41
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:20 +0000

    libav: remove references to pre-Mavericks.

--- Formula/libav.rb
+++ Formula/libav.rb
@@ -16,7 +16,7 @@ class Libav < Formula
 
   depends_on "pkg-config" => :build
   # manpages won't be built without texi2html
-  depends_on "texi2html" => :build if MacOS.version >= :mountain_lion
+  depends_on "texi2html" => :build
   depends_on "yasm" => :build
 
   depends_on "faac"
