eb4d5beb6831b5fbd3861b5658ecb5ace70784b3
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:35:32 +0000

    tre: remove references to pre-Mavericks.

--- Formula/tre.rb
+++ Formula/tre.rb
@@ -12,7 +12,6 @@ class Tre < Formula
     sha256 "8a1762dbd40b98869e01a19c29cdb1cfa5a127543b3e132fb0fdff996e46f566" => :el_capitan
     sha256 "6fada15a2fd1c5905f8ed45d3c966da5e14efeb10522f82c26d2a23a918abaad" => :yosemite
     sha256 "c57f9bfa724cd20843a672f5e8bd384e05e65bac062dd7d7b676db9b1c11f998" => :mavericks
-    sha256 "1159edaece87806b8141cb351d896641b6de1667b0f62d6214ff0412ffae7fc4" => :mountain_lion
   end
 
   def install
