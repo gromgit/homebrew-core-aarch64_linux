269702dd537db5a7484a13ee9408832f60cd440f
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:33:55 +0000

    epstool: remove references to pre-Mavericks.

--- Formula/epstool.rb
+++ Formula/epstool.rb
@@ -13,7 +13,6 @@ class Epstool < Formula
     sha256 "92efa66cd268f0447dc52c14e9da04ae8af01b1691ec8eec3df61bbeb947b713" => :el_capitan
     sha256 "bb2aefa17b699127f2f6ed65004c9acbf7e5e5122f6f4b920d1e03fb9bd87b2e" => :yosemite
     sha256 "ae3c4b14dd19d3ac43947eff025a0fa3eabbe832333922359f9f74e0fe5e1d3d" => :mavericks
-    sha256 "408c2744b730a01eed68f19d4f643b060f026bbbc2616cf4b6afcb08ef608b90" => :mountain_lion
   end
 
   depends_on "ghostscript"
