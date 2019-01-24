f6471afa2e27defd5477e94d628458983c5b8b75
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:26 +0000

    libident: remove references to pre-Mavericks.

--- Formula/libident.rb
+++ Formula/libident.rb
@@ -13,7 +13,6 @@ class Libident < Formula
     sha256 "6236d3b4ee424795859cc64da30997ff67f7ac5bd42702e8eabe10f99339ca48" => :el_capitan
     sha256 "53db8e889d8efa34b4a3b6a145bcec2bcb53595e7db0cfdd55c8d857dff3a442" => :yosemite
     sha256 "d8e20cc9d2015b83785d4eb05110afc082a6389267df82546cc89e693dd2db6a" => :mavericks
-    sha256 "0bb13ba0c69e3bcf77da2edaac0b500c074724a7a191674bc2914525dbf7dcd0" => :mountain_lion
   end
 
   def install
