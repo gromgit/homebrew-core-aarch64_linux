ac5b204f97c7df43ae61245e9e4930ccc03a9997
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:58 +0000

    norm: remove references to pre-Mavericks.

--- Formula/norm.rb
+++ Formula/norm.rb
@@ -13,7 +13,6 @@ class Norm < Formula
     sha256 "985bbdc34e0f8f16f2d377bea4c0442abb0f7cbaf67b56cb40b924bb09c394b5" => :el_capitan
     sha256 "2c165178bfce5879bb6e031b4d54f741cad2868d67b03783f89a13d15503f28d" => :yosemite
     sha256 "b5f802ff09e68b712f472f45aea9b634f6c45868bccaf708d565ff98a95b145e" => :mavericks
-    sha256 "e04bed5670762178e4415296b95829b9e3c75da265f9375c6c6efac310b7b3d4" => :mountain_lion
   end
 
   def install
