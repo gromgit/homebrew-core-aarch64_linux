c52ea86c282f23751866d96b9e79575f5509c0ce
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:32 +0000

    libpgm: remove references to pre-Mavericks.

--- Formula/libpgm.rb
+++ Formula/libpgm.rb
@@ -14,7 +14,6 @@ class Libpgm < Formula
     sha256 "24765bd6efa0aa65a333e3d5bb5a48159875b81cae8ca99c479fbda4133f49b9" => :el_capitan
     sha256 "ae0d1d980f84677fcaa08b1d9f35f1c9d4858e4239598530b7485e9f248def73" => :yosemite
     sha256 "87ac77e422ffd9b72d1070c991064d0a8a9b5eb2d124f5cdd9911590b48bd291" => :mavericks
-    sha256 "682fb0731817ab2f01c9247b1579dcd4a5ff8e28a938ddcd7045ee30acc81499" => :mountain_lion
   end
 
   def install
