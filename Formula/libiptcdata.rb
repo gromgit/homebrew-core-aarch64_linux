b8354bf437dd19625887032e073c26f484b84300
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:27 +0000

    libiptcdata: remove references to pre-Mavericks.

--- Formula/libiptcdata.rb
+++ Formula/libiptcdata.rb
@@ -12,7 +12,6 @@ class Libiptcdata < Formula
     sha256 "37747abe5597a40dec5a741919eaa46bedc42472bb909b9a09c4a200e5d592d4" => :el_capitan
     sha256 "14f6b3a649e0d944768e5e3a1e4d44e1efd0389fdeaa5740993b10ee7a42c718" => :yosemite
     sha256 "e88aff2cc7949c8c05608811f894bd6816d797eeffe74f35118168168512738c" => :mavericks
-    sha256 "3656316c8836547affe91fa29e9ea4067ac6774a9c9a08aef5fb296f4330e5f2" => :mountain_lion
   end
 
   depends_on "gettext"
