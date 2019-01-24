9bb0f5dd300818211b5b13a4436794c25c8218d3
Mike McQuaid <mike@mikemcquaid.com>
2019-01-24 18:34:18 +0000

    kytea: remove references to pre-Mavericks.

--- Formula/kytea.rb
+++ Formula/kytea.rb
@@ -11,7 +11,6 @@ class Kytea < Formula
     sha256 "3e0c66a7efb34ddb8e4f80d9b95562779e224271b8d63d38f9bc8176103427e2" => :el_capitan
     sha256 "2f2dda314728cd74750db339ebc2d166b8b611ad54668cc3e7b6225d39aec3f5" => :yosemite
     sha256 "045d0c9ad0cf35e003b8839cb0213e3f49d9107dfbc955e449b36fd4b6596640" => :mavericks
-    sha256 "3f15b353a447519bfb7f602b29db18e7eff945e0b68998af35cc37a745328182" => :mountain_lion
   end
 
   head do
