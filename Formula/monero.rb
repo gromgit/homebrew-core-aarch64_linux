de01380c4f3d5ebb5b724e5f98048690419f9228
Iulian Costan <iulian.costan@gmail.com>
2019-01-25 16:33:02 +0200

    monero: add plist (#35854)

--- Formula/monero.rb
+++ Formula/monero.rb
@@ -40,6 +40,27 @@ class Monero < Formula
     rm_rf include/"miniupnpc"
   end
 
+  plist_options :manual => "monerod"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/monerod</string>
+        <string>--non-interactive</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+    </dict>
+    </plist>
+  EOS
+  end
+
   test do
     cmd = "yes '' | #{bin}/monero-wallet-cli --restore-deterministic-wallet " \
       "--password brew-test --restore-height 1 --generate-new-wallet wallet " \
