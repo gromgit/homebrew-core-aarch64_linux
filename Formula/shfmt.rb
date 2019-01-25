946de4fc7335394cf040f20580998d60aeca9453
René Schwaiger <sanssecours@me.com>
2019-01-25 10:25:29 +0100

    shfmt 2.6.3
    Closes #36381.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/shfmt.rb
+++ Formula/shfmt.rb
@@ -1,8 +1,8 @@
 class Shfmt < Formula
   desc "Autoformat shell script source code"
   homepage "https://github.com/mvdan/sh"
-  url "https://github.com/mvdan/sh/archive/v2.6.2.tar.gz"
-  sha256 "69f5a801239299be95b26aa9342339432934eb49c41137022ef39aaf58c903da"
+  url "https://github.com/mvdan/sh/archive/v2.6.3.tar.gz"
+  sha256 "12b4e79495f5bb091c19e601d8af5c06574e680a621d015dac9f6b8357ee901a"
   head "https://github.com/mvdan/sh.git"
 
   bottle do
