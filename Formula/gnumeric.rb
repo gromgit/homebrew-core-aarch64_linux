eff6a1a5b482d2a17fb8ce797c96313a4e5bf49f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-25 16:05:06 +0000

    gnumeric: update 1.12.43 bottle.

--- Formula/gnumeric.rb
+++ Formula/gnumeric.rb
@@ -5,10 +5,10 @@ class Gnumeric < Formula
   sha256 "87c9abd6260cf29401fa1e0fcce374e8c7bcd1986608e4049f6037c9d32b5fd5"
 
   bottle do
-    sha256 "1317c0b0a36c781ea587b3411a2b7c7ccff9206fed085e002e4bbef339130e99" => :mojave
-    sha256 "f4a007bcb9422d69d6217c08f9af877ed61cdf51d919fc88eb6d905c1aedaf3f" => :high_sierra
-    sha256 "d7db9d5d98fb58473c411d22683e8de5e73fe6e965dae5bdbd8d3fd243a59826" => :sierra
-    sha256 "c987c86ec64c80322d7f602234ca1e3e144889bf72d6144f9d1b5ffa76879ec8" => :el_capitan
+    rebuild 1
+    sha256 "915075749528bd3b0f31651fbdfac38a6e06134eea6ed99f97b7b777fba0f3ac" => :mojave
+    sha256 "9c9e678a2d3f25b34c1896d197a7cd3a6da6207fee9b3a64a5f3526ed742bf1c" => :high_sierra
+    sha256 "65ff2bf41de0d4b5665e619720cf045b00177f345ec80198d0190cf0a1c3f66b" => :sierra
   end
 
   depends_on "intltool" => :build
