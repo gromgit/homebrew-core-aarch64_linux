c7ee23d166ce4d480517f6c5a29b5dfef04a23a4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-25 15:01:01 +0000

    anyenv: add 1.0.1 bottle.

--- Formula/anyenv.rb
+++ Formula/anyenv.rb
@@ -4,6 +4,13 @@ class Anyenv < Formula
   url "https://github.com/anyenv/anyenv/archive/v1.0.1.tar.gz"
   sha256 "5d42ac8748db41b8ec0cb3ad986bb10084e55df0153366403ea458e7a958a1a7"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e151eaa92c93a0c8e33a1f285cf3fc3abf98b7eab601df1cafa1b57d98d543a3" => :mojave
+    sha256 "288be8d02cec3886f56eea01f494fd0b3ffdf28ed6d323566a1b533b2cce9e43" => :high_sierra
+    sha256 "288be8d02cec3886f56eea01f494fd0b3ffdf28ed6d323566a1b533b2cce9e43" => :sierra
+  end
+
   def install
     prefix.install %w[bin completions libexec]
   end
