0ca066bd74115f63a55d5ab2b47e3b80fec2cac2
Chongyu Zhu <i@lembacon.com>
2019-01-26 14:18:35 +0800

    cargo-completion 0.33.0
    Closes #36400.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/cargo-completion.rb
+++ Formula/cargo-completion.rb
@@ -1,8 +1,8 @@
 class CargoCompletion < Formula
   desc "Bash and Zsh completion for Cargo"
   homepage "https://github.com/rust-lang/cargo"
-  url "https://github.com/rust-lang/cargo/archive/0.32.0.tar.gz"
-  sha256 "c62e41489179a9b8e1ae0dbed379c278f9de805abba41608a05c069478a739fd"
+  url "https://github.com/rust-lang/cargo/archive/0.33.0.tar.gz"
+  sha256 "914d715ff1a4ac4280b63d05c0f848d38889900a5559888d4a20c9600f87e6a4"
   version_scheme 1
   head "https://github.com/rust-lang/cargo.git"
 
