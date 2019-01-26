b96210f2bdeb36bd602d5809f91e9c63a81920ed
Tobias Fischer <tobias.carl.fischer@gmail.com>
2019-01-26 23:03:14 +0100

    epubcheck 4.1.1
    Closes #36427.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/epubcheck.rb
+++ Formula/epubcheck.rb
@@ -1,8 +1,8 @@
 class Epubcheck < Formula
-  desc "Validate IDPF EPUB files, version 2.0 and later"
-  homepage "https://github.com/IDPF/epubcheck"
-  url "https://github.com/IDPF/epubcheck/releases/download/v4.1.0/epubcheck-4.1.0.zip"
-  sha256 "8be4f038326099ef95fcbd9dc02a36fb01373833f644573cb964a4442ae727ad"
+  desc "Validate EPUB files, version 2.0 and later"
+  homepage "https://github.com/w3c/epubcheck"
+  url "https://github.com/w3c/epubcheck/releases/download/v4.1.1/epubcheck-4.1.1.zip"
+  sha256 "0d07de09644d40a373bd7b6a35ab08bbf5537095c4f58a667ab98421e703b16a"
 
   bottle :unneeded
 
