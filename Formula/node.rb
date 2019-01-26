b1a8568b1ba72c00298872f4c948d14124e17d6f
Tommy Yang <yt940928@gmail.com>
2019-01-26 07:03:28 -0800

    node 11.8.0
    Closes #36416.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/node.rb
+++ Formula/node.rb
@@ -1,8 +1,8 @@
 class Node < Formula
   desc "Platform built on V8 to build network applications"
   homepage "https://nodejs.org/"
-  url "https://nodejs.org/dist/v11.7.0/node-v11.7.0.tar.gz"
-  sha256 "4d95471083735c275ed3fb4d61458940887edbaae663d0c96ea5395c087ab9dc"
+  url "https://nodejs.org/dist/v11.8.0/node-v11.8.0.tar.gz"
+  sha256 "958d1bbd08fb8d2ae6aad1c4e644fef61e502409fe6c7d0e742013ce0a2d2f26"
   head "https://github.com/nodejs/node.git"
 
   bottle do
