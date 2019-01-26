c3b2c74f8f01c720294dacda4d8244191cd03bd0
Chongyu Zhu <i@lembacon.com>
2019-01-26 14:20:19 +0800

    iso-codes 4.2
    Closes #36401.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/iso-codes.rb
+++ Formula/iso-codes.rb
@@ -1,9 +1,8 @@
 class IsoCodes < Formula
   desc "Provides lists of various ISO standards"
   homepage "https://salsa.debian.org/iso-codes-team/iso-codes"
-  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/i/iso-codes/iso-codes_4.1.orig.tar.xz"
-  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/i/iso-codes/iso-codes_4.1.orig.tar.xz"
-  sha256 "67117fb76f32c8fb5e37d2d60bce238f1f8e865cc7b569a57cbc3017ca15488a"
+  url "https://deb.debian.org/debian/pool/main/i/iso-codes/iso-codes_4.2.orig.tar.xz"
+  sha256 "2b7f66c81808ac52e1ed0efe4ce8ae8e43309eedcc411f94f71a3f603cc21f42"
   head "https://salsa.debian.org/iso-codes-team/iso-codes.git"
 
   bottle do
