bd87bfd9c308965c91f59afa70f6b9b3fbe3f212
Hsiao-nan Cheung <niheaven@gmail.com>
2019-01-27 13:04:00 +0800

    libraw: Bump version to 0.19.2 and add OpenMP support

--- Formula/libraw.rb
+++ Formula/libraw.rb
@@ -1,8 +1,8 @@
 class Libraw < Formula
   desc "Library for reading RAW files from digital photo cameras"
   homepage "https://www.libraw.org/"
-  url "https://www.libraw.org/data/LibRaw-0.19.0.tar.gz"
-  sha256 "e83f51e83b19f9ba6b8bd144475fc12edf2d7b3b930d8d280bdebd8a8f3ed259"
+  url "https://www.libraw.org/data/LibRaw-0.19.2.tar.gz"
+  sha256 "400d47969292291d297873a06fb0535ccce70728117463927ddd9452aa849644"
 
   bottle do
     cellar :any
@@ -15,6 +15,7 @@ class Libraw < Formula
   depends_on "pkg-config" => :build
   depends_on "jasper"
   depends_on "jpeg"
+  depends_on "libomp"
   depends_on "little-cms2"
 
   resource "librawtestfile" do
@@ -24,7 +25,10 @@ class Libraw < Formula
 
   def install
     system "./configure", "--prefix=#{prefix}",
-                          "--disable-dependency-tracking"
+                          "--disable-dependency-tracking",
+                          "ac_cv_prog_c_openmp=-Xpreprocessor -fopenmp",
+                          "ac_cv_prog_cxx_openmp=-Xpreprocessor -fopenmp",
+                          "LDFLAGS=-lomp"
     system "make"
     system "make", "install"
     doc.install Dir["doc/*"]
