6b12e14835f7589750862dc26cc506134f42c671
spacemeowx2 <spacemeowx2@gmail.com>
2019-01-28 16:46:26 +0800

    switch-lan-play 0.1.0
    Closes #36470.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/switch-lan-play.rb
+++ Formula/switch-lan-play.rb
@@ -1,8 +1,9 @@
 class SwitchLanPlay < Formula
   desc "Make you and your friends play games like in a LAN"
   homepage "https://github.com/spacemeowx2/switch-lan-play"
-  url "https://github.com/spacemeowx2/switch-lan-play/archive/v0.0.7.tar.gz"
-  sha256 "0b645fa8bbff18b4b2d1dfd8d975790c6597bc0520c0ab1368387d08f8c2c900"
+  url "https://github.com/spacemeowx2/switch-lan-play.git",
+    :tag      => "v0.1.0",
+    :revision => "eda2c4dcb1db7948fcc0ae5d129fc4b8f4369ca2"
 
   bottle do
     cellar :any
@@ -12,11 +13,12 @@ class SwitchLanPlay < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "libuv"
 
   def install
-    system "cmake", ".", *std_cmake_args
-    system "make", "install"
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
   end
 
   test do
