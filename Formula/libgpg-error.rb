dfe1cfba1151f78c507a05b1ca45c8c314cb8d3d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-29 19:33:23 +0000

    libgpg-error: update 1.35 bottle.

--- Formula/libgpg-error.rb
+++ Formula/libgpg-error.rb
@@ -5,9 +5,9 @@ class LibgpgError < Formula
   sha256 "cbd5ee62a8a8c88d48c158fff4fc9ead4132aacd1b4a56eb791f9f997d07e067"
 
   bottle do
-    sha256 "c6a6405ecb74bbc7d296b791a8398e91d213e1ce6f67df0d3b61481ed8bac377" => :mojave
-    sha256 "21de798829b5f774b28100d3ab139f1a0d4e9c2d1235c4fc631e83ece1190cab" => :high_sierra
-    sha256 "6df062fa53c63231b88d8b8b89d7e0f54a16039d621679c7e90bb1b273834a3c" => :sierra
+    sha256 "1a96796605d2d7dc8a558848965a09678b13e50d52a971729bdf9a96b6f824c7" => :mojave
+    sha256 "168ef933ffd337d5a7ead3417b3bedec4e09982de3ed067f1b7302022c7fec00" => :high_sierra
+    sha256 "6566112d47fe7d6914158d530072d9feb1dae6262289ffb7eca4d16b54851d05" => :sierra
   end
 
   def install
