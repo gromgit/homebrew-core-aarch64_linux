188b2a70772a973ee46a83b7eb9a626fc7dfb858
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2019-01-29 06:40:10 +0530

    easyengine 4.0.10 (#36481)

--- Formula/easyengine.rb
+++ Formula/easyengine.rb
@@ -1,8 +1,8 @@
 class Easyengine < Formula
   desc "Command-line control panel to manage WordPress sites"
   homepage "https://easyengine.io/"
-  url "https://github.com/EasyEngine/easyengine/releases/download/v4.0.9/easyengine.phar"
-  sha256 "c28b6407132ad3c6f69b359b6c22a61f08578b628e7319190ab2c377add049ab"
+  url "https://github.com/EasyEngine/easyengine/releases/download/v4.0.10/easyengine.phar"
+  sha256 "0f4646a548b9e6e7f3f99c8c91eac4a3f56fd0aa4f39306f9fbb27179778c910"
 
   bottle :unneeded
 
