09e2b6bf57fedb862dc7754a7111db8d9a90067d
kpcyrd <git@rxv.cc>
2019-01-30 03:42:19 +0100

    sn0int 0.9.0 (new formula)
    Closes #36533.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/sn0int.rb
@@ -0,0 +1,37 @@
+class Sn0int < Formula
+  desc "Semi-automatic OSINT framework and package manager"
+  homepage "https://github.com/kpcyrd/sn0int"
+  url "https://github.com/kpcyrd/sn0int/archive/v0.9.0.tar.gz"
+  sha256 "53e41ea299b4be6c999d2410c7ba5ae9cba48fef58e0778318e321c1090f4c0e"
+
+  depends_on "rust" => :build
+  depends_on "sphinx-doc" => :build
+
+  def install
+    system "cargo", "install", "--root", prefix, "--path", "."
+
+    system "#{bin}/sn0int completions bash > sn0int.bash"
+    system "#{bin}/sn0int completions fish > sn0int.fish"
+    system "#{bin}/sn0int completions zsh > _sn0int"
+
+    bash_completion.install "sn0int.bash"
+    fish_completion.install "sn0int.fish"
+    zsh_completion.install "_sn0int"
+
+    system "make", "-C", "docs", "man"
+    man1.install "docs/_build/man/sn0int.1"
+  end
+
+  test do
+    (testpath/"true.lua").write <<~EOS
+      -- Description: basic selftest
+      -- Version: 0.1.0
+      -- License: GPL-3.0
+
+      function run()
+          -- nothing to do here
+      end
+    EOS
+    system "#{bin}/sn0int", "run", "-vvxf", testpath/"true.lua"
+  end
+end
