fb277dd1edcdd124748e348b219603d06deff370
logikal <sean.kilgore@sendgrid.com>
2019-01-30 15:01:05 -0800

    aws-okta 0.19.5
    Closes #36600.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/aws-okta.rb
+++ Formula/aws-okta.rb
@@ -1,8 +1,8 @@
 class AwsOkta < Formula
   desc "Authenticate with AWS using your Okta credentials"
   homepage "https://github.com/segmentio/aws-okta"
-  url "https://github.com/segmentio/aws-okta/archive/v0.19.4.tar.gz"
-  sha256 "5bd6f81376cfa45ea5d0a067bed68e8dec14fe1a2f4bd7f4954db89172b32710"
+  url "https://github.com/segmentio/aws-okta/archive/v0.19.5.tar.gz"
+  sha256 "dbcee4a2a6ed538c59b0ce4af9a09a486e69f060110c1d4500fdaf6785e76066"
 
   bottle do
     cellar :any_skip_relocation
@@ -12,11 +12,13 @@ class AwsOkta < Formula
   end
 
   depends_on "go" => :build
+  depends_on "govendor" => :build
 
   def install
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/segmentio/aws-okta").install buildpath.children
     cd "src/github.com/segmentio/aws-okta" do
+      system "govendor", "sync"
       system "go", "build", "-ldflags", "-X main.Version=#{version}"
       bin.install "aws-okta"
       prefix.install_metafiles
@@ -29,11 +31,18 @@ class AwsOkta < Formula
     PTY.spawn("#{bin}/aws-okta --backend file add") do |input, output, _pid|
       output.puts "organization\n"
       input.gets
+      output.puts "us\n"
+      input.gets
+      output.puts "fakedomain.okta.com\n"
+      input.gets
       output.puts "username\n"
       input.gets
       output.puts "password\n"
       input.gets
-      output.puts "\n"
+      input.gets
+      input.gets
+      input.gets
+      input.gets
       input.gets
       input.gets
       assert_match "Failed to validate credentials", input.gets.chomp
