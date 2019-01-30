f0746e345b1d5fa2092d5e6e43cf1c18bb0eedba
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-30 05:29:37 +0000

    sn0int: add 0.9.0 bottle.

--- Formula/sn0int.rb
+++ Formula/sn0int.rb
@@ -4,6 +4,13 @@ class Sn0int < Formula
   url "https://github.com/kpcyrd/sn0int/archive/v0.9.0.tar.gz"
   sha256 "53e41ea299b4be6c999d2410c7ba5ae9cba48fef58e0778318e321c1090f4c0e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "36f28ef3002a3cf77f0617979528b39c4a4087fe2d42e5f40ed7576075952a52" => :mojave
+    sha256 "9322ce859cccb92d18bb512ce602c0527a448d2dba92703b2b77e14770fa3150" => :high_sierra
+    sha256 "2ddd6e6de3e9b0be8231a58df745261bdc4b82cea0dbbbf147f9cc8202a2521e" => :sierra
+  end
+
   depends_on "rust" => :build
   depends_on "sphinx-doc" => :build
 
