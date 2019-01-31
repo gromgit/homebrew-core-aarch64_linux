a7086015a2f237cc60fd027f23d324eb43dc885e
willi123yao <willi123yao@gmail.com>
2019-01-31 09:56:43 +0800

    enet 1.3.14
    Closes #36574.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/enet.rb
+++ Formula/enet.rb
@@ -1,8 +1,8 @@
 class Enet < Formula
   desc "Provides a network communication layer on top of UDP"
   homepage "http://enet.bespin.org"
-  url "http://enet.bespin.org/download/enet-1.3.13.tar.gz"
-  sha256 "e36072021faa28731b08c15b1c3b5b91b911baf5f6abcc7fe4a6d425abada35c"
+  url "http://enet.bespin.org/download/enet-1.3.14.tar.gz"
+  sha256 "98f6f57aab0a424469619ed3047728f0d3901ce8f0dea919c11e7966d807e870"
 
   bottle do
     cellar :any
