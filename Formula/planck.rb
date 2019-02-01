eb938897ef9a6fdcb017ffd5863affa5f2b4cd29
Mike Fikes <mike@fikesfarm.com>
2019-02-01 12:22:21 -0500

    planck 2.20.0
    Closes #36628.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/planck.rb
+++ Formula/planck.rb
@@ -1,9 +1,8 @@
 class Planck < Formula
   desc "Stand-alone ClojureScript REPL"
   homepage "http://planck-repl.org/"
-  url "https://github.com/planck-repl/planck/archive/2.19.0.tar.gz"
-  sha256 "bb33984f986a7ac68b331cfd64bd0f9e41daf5391b1a36e158e15d94d886dd04"
-  revision 1
+  url "https://github.com/planck-repl/planck/archive/2.20.0.tar.gz"
+  sha256 "9d982024ac8db3ecc2771c6d0ba0c572143785e8eff9a0a6a7129bb0e41e96c7"
   head "https://github.com/planck-repl/planck.git"
 
   bottle do
