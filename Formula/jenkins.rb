a9586e31880825c34716de5934b40f8bc97691e7
Jeremiah Njoroge <jnjoroge@gmail.com>
2019-02-04 10:06:13 -0800

    jenkins 2.163
    Closes #36705.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/jenkins.rb
+++ Formula/jenkins.rb
@@ -1,8 +1,8 @@
 class Jenkins < Formula
   desc "Extendable open source continuous integration server"
   homepage "https://jenkins.io/"
-  url "http://mirrors.jenkins.io/war/2.162/jenkins.war"
-  sha256 "df740fc219ccff4e61e023e4c18f4ae4606a2a3074e50fa6449f341d8833964b"
+  url "http://mirrors.jenkins.io/war/2.163/jenkins.war"
+  sha256 "fdf78f2348d88e6abd7807a3d61c02f2c2faa6b917a2a6018eaf498afd4a9454"
 
   head do
     url "https://github.com/jenkinsci/jenkins.git"
