41bc8d7e7fda959daacc64198977b28ec9c3c8a4
Alex Gaynor <agaynor@mozilla.com>
2019-02-04 10:04:18 -0500

    sccache 0.2.8
    Closes #36701.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/sccache.rb
+++ Formula/sccache.rb
@@ -1,8 +1,8 @@
 class Sccache < Formula
   desc "Used as a compiler wrapper and avoids compilation when possible"
   homepage "https://github.com/mozilla/sccache"
-  url "https://github.com/mozilla/sccache/archive/0.2.7.tar.gz"
-  sha256 "0f7e3ad60a93759a35623aa954633c94154fd00d17fe29f9413933e1c5545a52"
+  url "https://github.com/mozilla/sccache/archive/0.2.8.tar.gz"
+  sha256 "e0ed93c5ce26d240e0f737e9409decf80cdae762f21eb51d3d4c26539bdbd468"
   head "https://github.com/mozilla/sccache.git"
 
   bottle do
