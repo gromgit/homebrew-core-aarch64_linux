50d77d74edc34a54bcc6b35bbec0a3dda0adcda3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-05 20:23:23 +0000

    bluetoothconnector: update 1.2.0 bottle.

--- Formula/bluetoothconnector.rb
+++ Formula/bluetoothconnector.rb
@@ -6,8 +6,8 @@ class Bluetoothconnector < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "1c3b876a95fe14f0716239d9eadf882b40ce9d0df71080502581800231904340" => :mojave
-    sha256 "282773ff899326dc1c0dfc1fa7941da80c46d314ae1b0930a68d7e3e5758a73b" => :high_sierra
+    sha256 "5fac038a2fda3fdb144fb764166eeda3b31799caf1ce8e44a8f77bd5881daf1e" => :mojave
+    sha256 "5fac038a2fda3fdb144fb764166eeda3b31799caf1ce8e44a8f77bd5881daf1e" => :high_sierra
   end
 
   depends_on :xcode => ["10.0", :build]
