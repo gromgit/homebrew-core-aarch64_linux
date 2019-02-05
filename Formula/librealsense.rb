ec80b5e6ee97b0211c9f4346b034328eae81e4a9
Igor Kapkov <igasgeek@me.com>
2019-02-05 15:53:30 +1100

    librealsense 2.18.0
    Closes #36727.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/librealsense.rb
+++ Formula/librealsense.rb
@@ -1,8 +1,8 @@
 class Librealsense < Formula
   desc "Intel RealSense D400 series and SR300 capture"
   homepage "https://github.com/IntelRealSense/librealsense"
-  url "https://github.com/IntelRealSense/librealsense/archive/v2.17.1.tar.gz"
-  sha256 "438fc35cca150d1cd9e6633d6212cc6970b738e1fd13c40343874e67bed2cbeb"
+  url "https://github.com/IntelRealSense/librealsense/archive/v2.18.0.tar.gz"
+  sha256 "e9ff4c56e00e4bd423a272e2a5750b277f2de630f0ad07bf1d04a9e2a36df5fe"
   head "https://github.com/IntelRealSense/librealsense.git"
 
   bottle do
