6a870a788482465adcd2d1150e9f0a9def3674b1
Yurii Kolesnykov <root@yurikoles.com>
2019-02-07 00:09:33 +0200

    vulkan-headers 1.1.99 (new formula)
    Closes #36781.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/vulkan-headers.rb
@@ -0,0 +1,27 @@
+class VulkanHeaders < Formula
+  desc "Vulkan Header files and API registry"
+  homepage "https://github.com/KhronosGroup/Vulkan-Headers"
+  url "https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.1.99.tar.gz"
+  sha256 "37506deaf56b254193e4fa83195add2617e881efcac3d2ff5f0cf49353eb9aa8"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <vulkan/vulkan_core.h>
+
+      int main() {
+        printf("vulkan version %d", VK_VERSION_1_0);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-o", "test"
+    system "./test"
+  end
+end
