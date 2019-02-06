583fb5ca9afc82fe191bd336d80fc9ccd5baacb7
equal-l2 <eng.equall2@gmail.com>
2019-02-06 21:03:43 +0900

    opendetex 2.8.5
    Closes #36777.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/opendetex.rb
+++ Formula/opendetex.rb
@@ -1,8 +1,8 @@
 class Opendetex < Formula
   desc "Tool to strip TeX or LaTeX commands from documents"
   homepage "https://github.com/pkubowicz/opendetex"
-  url "https://github.com/pkubowicz/opendetex/archive/v2.8.4.tar.gz"
-  sha256 "d1ca2ba332d0b948b3316052476d3699a7378ab83505fe906a2ba80828778f84"
+  url "https://github.com/pkubowicz/opendetex/releases/download/v2.8.5/opendetex-2.8.5.tar.bz2"
+  sha256 "90be111ec1f47af25317d1dbe2f07a350cc46e1bc4cdc995dde79219d7c2623d"
 
   bottle do
     cellar :any_skip_relocation
@@ -12,13 +12,11 @@ class Opendetex < Formula
     sha256 "79e56e9e50f90d6b534f29c556a648743ee10ab494d5f7cd049031eb4833f122" => :el_capitan
   end
 
-  patch :DATA
-
   def install
     system "make"
     bin.install "detex"
     bin.install "delatex"
-    man1.install "detex.1l" => "detex.1"
+    man1.install "detex.1"
   end
 
   test do
@@ -33,24 +31,3 @@ class Opendetex < Formula
     assert_equal "Simple text.\n", output
   end
 end
-
-__END__
-diff --git a/detex.1l b/detex.1l
-index a70c813..7033b44 100644
---- a/detex.1l
-+++ b/detex.1l
-@@ -1,4 +1,4 @@
--.TH DETEX 1L "12 August 1993" "Purdue University"
-+.TH DETEX 1 "12 August 1993" "Purdue University"
- .SH NAME
- detex \- a filter to strip \fITeX\fP commands from a .tex file.
- .SH SYNOPSIS
-@@ -103,7 +103,7 @@ The old functionality can be essentially duplicated by using the
- .B \-s
- option.
- .SH SEE ALSO
--tex(1L)
-+tex(1)
- .SH DIAGNOSTICS
- Nesting of \\input is allowed but the number of opened files must not
- exceed the system's limit on the number of simultaneously opened files.
