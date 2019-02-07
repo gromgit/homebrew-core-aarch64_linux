883766107c6219a6527eeab0f4ee6d0a596c5865
Matthieu Prat <matthieuprat@gmail.com>
2019-02-07 18:47:29 +0000

    postgresql@9.4 9.4.20
    PostgreSQL 9.4.20 now seems to have proper support for Mojave [1] so we
    can revert ad06b1a04e.
    
    [1]: https://github.com/postgres/postgres/commit/a5361b5933f7b0e0349c34ce11cc7a9438a04c23.
    
    Closes #36797.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/postgresql@9.4.rb
+++ Formula/postgresql@9.4.rb
@@ -1,8 +1,8 @@
 class PostgresqlAT94 < Formula
   desc "Object-relational database system"
   homepage "https://www.postgresql.org/"
-  url "https://ftp.postgresql.org/pub/source/v9.4.19/postgresql-9.4.19.tar.bz2"
-  sha256 "03776b036b2a05371083558e10c21cc4b90bde9eb3aff60299c4ce7c084c168b"
+  url "https://ftp.postgresql.org/pub/source/v9.4.20/postgresql-9.4.20.tar.bz2"
+  sha256 "eeb1d8ddb2854c9e4d8b5cbd65665260c0ae8cbcb911003f24c2d82ccb97f87f"
   revision 1
 
   bottle do
@@ -24,6 +24,7 @@ class PostgresqlAT94 < Formula
 
     ENV.prepend "LDFLAGS", "-L#{Formula["openssl"].opt_lib} -L#{Formula["readline"].opt_lib}"
     ENV.prepend "CPPFLAGS", "-I#{Formula["openssl"].opt_include} -I#{Formula["readline"].opt_include}"
+    ENV.prepend "PG_SYSROOT", MacOS.sdk_path
     ENV.append_to_cflags "-D_XOPEN_SOURCE"
 
     args = %W[
@@ -49,36 +50,8 @@ class PostgresqlAT94 < Formula
       args << "--with-tclconfig=#{MacOS.sdk_path}/System/Library/Frameworks/Tcl.framework"
     end
 
-    # As of Xcode/CLT 10.x the Perl headers were moved from /System
-    # to inside the SDK, so we need to use `-iwithsysroot` instead
-    # of `-I` to point to the correct location.
-    # https://www.postgresql.org/message-id/153558865647.1483.573481613491501077%40wrigleys.postgresql.org
-    if DevelopmentTools.clang_build_version >= 1000
-      inreplace "configure",
-                "-I$perl_archlibexp/CORE",
-                "-iwithsysroot $perl_archlibexp/CORE"
-      inreplace "src/pl/plperl/GNUmakefile",
-                "-I$(perl_archlibexp)/CORE",
-                "-iwithsysroot $(perl_archlibexp)/CORE"
-    end
-
     system "./configure", *args
-
-    # Temporarily disable building/installing the documentation.
-    # Postgresql seems to "know" the build system has been altered and
-    # tries to regenerate the documentation when using `install-world`.
-    # This results in the build failing:
-    #  `ERROR: `osx' is missing on your system.`
-    # Attempting to fix that by adding a dependency on `open-sp` doesn't
-    # work and the build errors out on generating the documentation, so
-    # for now let's simply omit it so we can package Postgresql for Mojave.
-    if DevelopmentTools.clang_build_version >= 1000
-      system "make", "all"
-      system "make", "-C", "contrib", "install", "all"
-      system "make", "install", "all"
-    else
-      system "make", "install-world"
-    end
+    system "make", "install-world"
   end
 
   def post_install
