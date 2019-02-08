78ecae434a2a0aeeadf597a02f9f954180881799
Tommy Yang <yt940928@gmail.com>
2019-02-07 21:16:46 -0800

    sqlite 3.27.1
    Closes #36811.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/sqlite.rb
+++ Formula/sqlite.rb
@@ -1,10 +1,9 @@
 class Sqlite < Formula
   desc "Command-line interface for SQLite"
   homepage "https://sqlite.org/"
-  url "https://sqlite.org/2018/sqlite-autoconf-3260000.tar.gz"
-  version "3.26.0"
-  sha256 "5daa6a3fb7d1e8c767cd59c4ded8da6e4b00c61d3b466d0685e35c4dd6d7bf5d"
-  revision 1
+  url "https://sqlite.org/2019/sqlite-autoconf-3270100.tar.gz"
+  version "3.27.1"
+  sha256 "54a92b8ff73ff6181f89b9b0c08949119b99e8cccef93dbef90e852a8b10f4f8"
 
   bottle do
     cellar :any
