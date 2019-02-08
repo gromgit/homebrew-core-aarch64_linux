297f3384cdba484072508e178bd0557d0061ae78
Michael Goodness <mgoodness@gmail.com>
2019-02-07 19:11:55 -0600

    kustomize 2.0.1
    Signed-off-by: Michael Goodness <mgoodness@gmail.com>
    
    Closes #36807.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/kustomize.rb
+++ Formula/kustomize.rb
@@ -2,8 +2,8 @@ class Kustomize < Formula
   desc "Template-free customization of Kubernetes YAML manifests"
   homepage "https://github.com/kubernetes-sigs/kustomize"
   url "https://github.com/kubernetes-sigs/kustomize.git",
-      :tag      => "v1.0.11",
-      :revision => "8f701a00417a812558a7b785e8354957afa469ae"
+      :tag      => "v2.0.1",
+      :revision => "ce7e5ee2c30cc5856fea01fe423cf167f2a2d0c3"
   head "https://github.com/kubernetes-sigs/kustomize.git"
 
   bottle do
