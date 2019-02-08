f0dd2d115e3f5b5cf9eb6d5981869b4387045c24
Son Luong Ngoc <son.luongngoc@lazada.com>
2019-02-09 00:05:13 +0800

    bat 0.10.0
    Closes #36824.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/bat.rb
+++ Formula/bat.rb
@@ -1,9 +1,8 @@
 class Bat < Formula
   desc "Clone of cat(1) with syntax highlighting and Git integration"
   homepage "https://github.com/sharkdp/bat"
-  url "https://github.com/sharkdp/bat/archive/v0.9.0.tar.gz"
-  sha256 "4ce9c118cf5da1159a882dea389f3c5737b5d98192e9a619b0fe8c1730341cc6"
-  revision 1
+  url "https://github.com/sharkdp/bat/archive/v0.10.0.tar.gz"
+  sha256 "54dd396e8f20d44c6032a32339f45eab46a69b6134e74a704f8d4a27c18ddc3e"
 
   bottle do
     cellar :any_skip_relocation
@@ -18,11 +17,6 @@ class Bat < Formula
     ENV["SHELL_COMPLETIONS_DIR"] = buildpath
     system "cargo", "install", "--root", prefix, "--path", "."
     man1.install "doc/bat.1"
-    bash_completion.install "bat.bash"
-    # Temporary disable fish completions due to upstream issues. The completions might not work on
-    # some systems. See https://github.com/sharkdp/bat/issues/372
-    # fish_completion.install "bat.fish"
-    zsh_completion.install "_bat"
   end
 
   test do
