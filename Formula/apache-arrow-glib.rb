e8384ecac45d4f90a9563f019a4630f0fa6d0da8
Kouhei Sutou <kou@clear-code.com>
2019-02-08 09:21:32 +0900

    apache-arrow-glib 0.12.0 (new formula)
    Closes #36806.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/apache-arrow-glib.rb
@@ -0,0 +1,49 @@
+class ApacheArrowGlib < Formula
+  desc "GLib bindings for Apache Arrow"
+  homepage "https://arrow.apache.org/"
+  url "https://www.apache.org/dyn/closer.cgi?path=arrow/arrow-0.12.0/apache-arrow-0.12.0.tar.gz"
+  sha256 "34dae7e4dde9274e9a52610683e78a80f3ca312258ad9e9f2c0973cf44247a98"
+
+  depends_on "gobject-introspection" => :build
+  depends_on "pkg-config" => :build
+  depends_on "apache-arrow"
+  depends_on "glib"
+
+  def install
+    cd "c_glib" do
+      system "./configure", "--prefix=#{prefix}"
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<~SOURCE
+      #include <arrow-glib/arrow-glib.h>
+      int main(void) {
+        GArrowNullArray *array = garrow_null_array_new(10);
+        g_object_unref(array);
+        return 0;
+      }
+    SOURCE
+    apache_arrow = Formula["apache-arrow"]
+    glib = Formula["glib"]
+    flags = %W[
+      -I#{include}
+      -I#{apache_arrow.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -L#{lib}
+      -L#{apache_arrow.opt_lib}
+      -L#{glib.opt_lib}
+      -DNDEBUG
+      -larrow-glib
+      -larrow
+      -lglib-2.0
+      -lgobject-2.0
+      -lgio-2.0
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
