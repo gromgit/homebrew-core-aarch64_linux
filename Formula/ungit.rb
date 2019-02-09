e42cc61f6c491bd8d9ae3104c80a423689b71b90
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-09 00:52:25 +0000

    ungit: add 1.4.36 bottle.

--- Formula/ungit.rb
+++ Formula/ungit.rb
@@ -6,6 +6,13 @@ class Ungit < Formula
   url "https://registry.npmjs.org/ungit/-/ungit-1.4.36.tgz"
   sha256 "3bb07b99e2616afd3059809768036eaefd92d1a62cfab9dc087b9405933ec7f4"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "002cb1b980124683c08a20ce48492eee37eb861f5606441678a7d694e3f99126" => :mojave
+    sha256 "f6763612763f62ea554c4aba66340d7bfe6a1181729e1d72be28d1eda1175849" => :high_sierra
+    sha256 "384dc56bc235de67cc8c97d465f8759f2d24eb54b2d65dc8abaf6a7213658293" => :sierra
+  end
+
   depends_on "node"
 
   def install
