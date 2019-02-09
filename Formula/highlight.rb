39fa56470aaa190d70053c7f6e6e8bdb4f212666
Chongyu Zhu <i@lembacon.com>
2019-02-10 03:05:39 +0800

    highlight 3.49
    Closes #36853.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/highlight.rb
+++ Formula/highlight.rb
@@ -1,8 +1,8 @@
 class Highlight < Formula
   desc "Convert source code to formatted text with syntax highlighting"
   homepage "http://www.andre-simon.de/doku/highlight/en/highlight.php"
-  url "http://www.andre-simon.de/zip/highlight-3.48.tar.bz2"
-  sha256 "fdc5d20e4d4f3f3c68b04b7b979595a45e005e7c6bb3bf45c2153106518fa008"
+  url "http://www.andre-simon.de/zip/highlight-3.49.tar.bz2"
+  sha256 "5299774e0d554c83c96001596d942b3c0a4e2884cc61026aed5d5b5730ad90fe"
   head "https://gitlab.com/saalen/highlight.git"
 
   bottle do
