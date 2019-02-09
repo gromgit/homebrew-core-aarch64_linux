d79befe534290491e5a78f29f02a0e961bb090be
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-09 00:42:25 +0000

    pony-stable: update 0.2.0 bottle.

--- Formula/pony-stable.rb
+++ Formula/pony-stable.rb
@@ -7,10 +7,9 @@ class PonyStable < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "91185d0a73628759d5e3462ab943d5af75d0d93924f342d3a1dc482218375eca" => :mojave
-    sha256 "571fd986d8e2bd5fc89e382b14d8753c8ce1c3192c9965685564ee5cdb31e697" => :high_sierra
-    sha256 "5d0b08013d2cf38ffbd4e0fe521caf03281e897b3c64e565fd169c885bf56c22" => :sierra
-    sha256 "2396f10782e12343c245196d0265df84e106cd2a26496f1e7f1c90f9b55a99b3" => :el_capitan
+    sha256 "35714dcf19ff2245090f7651430cfb0c1216cc3e599e6cda7edb9b964db4be6f" => :mojave
+    sha256 "77768ecada4cbab8da58ff06f639a43eec4cf1fbe328330dc0073971bdbacfc9" => :high_sierra
+    sha256 "b04c2a2da1a13bb3cf965742c4c6228ccf8e7befbe734bd2f461633eaf6e46ba" => :sierra
   end
 
   depends_on "ponyc"
