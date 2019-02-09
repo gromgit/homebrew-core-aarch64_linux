57d9bf1640bf9575e7b6a38d246747d32cac5ab7
Ryan S. Elliott <relliott@umn.edu>
2019-02-08 22:18:10 -0600

    kim-api (new formula).

---
+++ Formula/kim-api.rb
@@ -0,0 +1,30 @@
+class KimApi < Formula
+  desc "The Knowledgebase of Interatomic Models (KIM) API"
+  homepage "https://openkim.org"
+  url "https://s3.openkim.org/kim-api/kim-api-2.0.2.txz"
+  sha256 "26e7cf91066692f316b8ba1548ccb7152bf56aad75902bce2338cff53e74e63d"
+
+  depends_on "cmake" => :build
+  depends_on "doxygen" => :build
+  depends_on "gcc" # for gfortran
+
+  def install
+    args = std_cmake_args
+    # adjust compiler settings for package
+    args << "-DKIM_API_CMAKE_C_COMPILER=/usr/bin/clang"
+    args << "-DKIM_API_CMAKE_CXX_COMPILER=/usr/bin/clang++"
+    # adjust directories for system collection
+    args << "-DKIM_API_SYSTEM_MODEL_DRIVERS_DIR=:#{HOMEBREW_PREFIX}/lib/openkim-models/model-drivers"
+    args << "-DKIM_API_SYSTEM_MODELS_DIR=:#{HOMEBREW_PREFIX}/lib/openkim-models/models"
+    args << "-DKIM_API_SYSTEM_SIMULATOR_MODELS_DIR=:#{HOMEBREW_PREFIX}/lib/openkim-models/simulator-models"
+    system "cmake", ".", *args
+    system "make"
+    system "make", "docs"
+    system "make", "install"
+  end
+
+  test do
+    output = shell_output("#{bin}/kim-api-collections-management list")
+    assert_match "ex_model_Ar_P_Morse_07C_w_Extensions", output
+  end
+end
