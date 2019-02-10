c5b7c329d87f141a0198e8b0dd95cc37cb49ba3d
Gehrig Wilcox <lougehrig10@gmail.com>
2019-02-10 10:43:33 -0600

    fff 2.1 (new formula) (#35994)

---
+++ Formula/fff.rb
@@ -0,0 +1,17 @@
+class Fff < Formula
+  desc "Simple file manager written in bash"
+  homepage "https://github.com/dylanaraps/fff"
+  url "https://github.com/dylanaraps/fff/archive/2.1.tar.gz"
+  sha256 "776870d11c022fa40468d5d582831c0ab5beced573489097deaaf5dd690e7eab"
+
+  bottle :unneeded
+
+  def install
+    bin.install "fff"
+    man1.install "fff.1"
+  end
+
+  test do
+    system bin/"fff", "-v"
+  end
+end
