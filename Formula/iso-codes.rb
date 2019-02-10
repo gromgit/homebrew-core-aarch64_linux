967e16e853d45e9abad92a389f25ebfc1ca99ec4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-10 15:56:09 +0000

    iso-codes: add 4.2_1 bottle.

--- Formula/iso-codes.rb
+++ Formula/iso-codes.rb
@@ -6,6 +6,13 @@ class IsoCodes < Formula
   revision 1
   head "https://salsa.debian.org/iso-codes-team/iso-codes.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a85d3ffaa71d35872da9c4ce021ec92986a447f3e3b4e7a1a23306be9239b4b0" => :mojave
+    sha256 "a85d3ffaa71d35872da9c4ce021ec92986a447f3e3b4e7a1a23306be9239b4b0" => :high_sierra
+    sha256 "240191588d2e014bfe26550e1151cdaa2f364a8e4859f2d39b17ed91507fe2a3" => :sierra
+  end
+
   depends_on "gettext" => :build
   depends_on "python" => :build
 
