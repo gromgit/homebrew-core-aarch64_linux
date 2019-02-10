ed0324ad10c859e8f2f0cf84b35e30d7c52f956b
FX Coudert <fxcoudert@gmail.com>
2019-02-10 18:27:56 +0100

    qpdf 8.4.0

--- Formula/qpdf.rb
+++ Formula/qpdf.rb
@@ -1,8 +1,8 @@
 class Qpdf < Formula
   desc "Tools for and transforming and inspecting PDF files"
   homepage "https://github.com/qpdf/qpdf"
-  url "https://github.com/qpdf/qpdf/releases/download/release-qpdf-8.3.0/qpdf-8.3.0.tar.gz"
-  sha256 "46c2a10690b86ba680db1c828e9ce44bb8e2993f69aa96d5a52fd1d98dc391f7"
+  url "https://github.com/qpdf/qpdf/releases/download/release-qpdf-8.4.0/qpdf-8.4.0.tar.gz"
+  sha256 "39018f3bff700c68e41f6d44ea9f7842e2a5af200a80b2cdec7fa32a4abac4a0"
 
   bottle do
     cellar :any
