79286e7e5221e358e1ea944ea89678ad9b7cee53
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-12 13:25:14 +0000

    hivemind: update 1.0.5 bottle.

--- Formula/hivemind.rb
+++ Formula/hivemind.rb
@@ -7,10 +7,9 @@ class Hivemind < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "682249a669799d961d5288d8973fc1f4a69acdabbc195371c3b38c71fde96b82" => :mojave
-    sha256 "45d2ce2f8a66a697ed67f7e080e47dcd56d3692632e1d07e316455b2a16329c4" => :high_sierra
-    sha256 "114b5cd253f904f2222899887a016058922b566ba366ad2d7d66521066e02180" => :sierra
-    sha256 "3cf03e49f0b3d8bb2b96966485ad1182c11996bab09d487a1ffcab8967494992" => :el_capitan
+    sha256 "e76b38e91f2a8143a01b72d7428e4b869fcffb9a9332cd20d89a3d667058fa72" => :mojave
+    sha256 "04b2c5ea90cac6a2c552e7209650bfdaa3a16b43a6bd2f8492c1fdcca852a7a6" => :high_sierra
+    sha256 "bb2ac3328d179d26a6d02cc952b9efb7352aea0f9f8faf6c45c84e29ceb5e298" => :sierra
   end
 
   depends_on "go" => :build
