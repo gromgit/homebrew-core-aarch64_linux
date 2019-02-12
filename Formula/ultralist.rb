428eef6b35d4cba9b40bd3fc1a805051e9fb7434
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-12 15:05:13 +0000

    ultralist: add 0.9.2.1 bottle.

--- Formula/ultralist.rb
+++ Formula/ultralist.rb
@@ -4,6 +4,13 @@ class Ultralist < Formula
   url "https://github.com/ultralist/ultralist/archive/0.9.2.1.tar.gz"
   sha256 "6662174315a4551ccd395f271c1a456ff982e56c653f8ef4cbbaa6649b4d7475"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "716b2d4f39ddc41aeddeff419454fce764d0bdad6eeced76e1fb3a42e75f26e0" => :mojave
+    sha256 "cc5d2192153e416785293d9db8a4af6379c5e2385a78e02d58eb74d68316f8f9" => :high_sierra
+    sha256 "c343ff19e0fb099fc13b5110425a4bab7d495cf7467297acaf860250a27f9697" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
