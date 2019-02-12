28cdd38ab1f104ce45b0bf4b8d71cf3d84bb18ea
Chongyu Zhu <i@lembacon.com>
2019-02-12 17:47:57 +0800

    netpbm 10.73.25
    Closes #36928.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/netpbm.rb
+++ Formula/netpbm.rb
@@ -3,8 +3,8 @@ class Netpbm < Formula
   homepage "https://netpbm.sourceforge.io/"
   # Maintainers: Look at https://sourceforge.net/p/netpbm/code/HEAD/tree/
   # for stable versions and matching revisions.
-  url "https://svn.code.sf.net/p/netpbm/code/stable", :revision => 3459
-  version "10.73.24"
+  url "https://svn.code.sf.net/p/netpbm/code/stable", :revision => 3546
+  version "10.73.25"
   version_scheme 1
   head "https://svn.code.sf.net/p/netpbm/code/trunk"
 
