f4f709b0df5ad8654b7315482d3cb1150a202ae2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-13 04:48:45 +0000

    protoc-gen-go: add 1.2.0 bottle.

--- Formula/protoc-gen-go.rb
+++ Formula/protoc-gen-go.rb
@@ -4,6 +4,13 @@ class ProtocGenGo < Formula
   url "https://github.com/golang/protobuf/archive/v1.2.0.tar.gz"
   sha256 "157a148ae4e132eb169ec794b6cb43f1002780eeacaea8b0694811d1948fb1ec"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d3beffd1377caff7c98221ddf726fb58ba4e5871394c589038dabd3703b58696" => :mojave
+    sha256 "3ebb0640cd69cf804a31ca05c88b55c1c9459017102931d068787d889b2d90d5" => :high_sierra
+    sha256 "45d29451aec60e81f6862c366ce03d5e7fd4fdcd717fe7108ae7b41c5f1c036c" => :sierra
+  end
+
   depends_on "go" => :build
   depends_on "protobuf"
 
