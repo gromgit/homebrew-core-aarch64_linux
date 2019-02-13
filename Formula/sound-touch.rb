07e1c723b075bc25760bac4ee946ce5710968236
Martin Delille <martin@phonations.com>
2019-02-13 20:08:32 +0100

    sound-touch 2.1.2
    Closes #36963.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/sound-touch.rb
+++ Formula/sound-touch.rb
@@ -1,8 +1,8 @@
 class SoundTouch < Formula
   desc "Audio processing library"
   homepage "https://www.surina.net/soundtouch/"
-  url "https://www.surina.net/soundtouch/soundtouch-2.0.0.tar.gz"
-  sha256 "d224f7d1421b5f8e74a74c85741345bd9802618a40ae30ce5b427a5705c89d25"
+  url "https://gitlab.com/soundtouch/soundtouch/-/archive/2.1.2/soundtouch-2.1.2.tar.gz"
+  sha256 "2826049e2f34efbc4c8a47d00c93649822b0c14e1f29f5569835704814590732"
 
   bottle do
     cellar :any
