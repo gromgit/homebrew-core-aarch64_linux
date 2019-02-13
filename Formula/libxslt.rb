a5f08bb2b672d95398e0cdf37697e6d544184fdc
Michka Popoff <michkapopoff@gmail.com>
2019-02-13 23:53:29 +0100

    libxslt: disable building Python2 bindings
    The macOS version does not have Python2 bindings, so this aligns the
    formula with what is provided by macOS.
    
    This change removes the following files:
    ls /usr/local/Cellar/libxslt/1.1.33/lib/python2.7/site-packages
    libxslt.py    libxsltmod.a  libxsltmod.so
    
    libxslt does not work with Python3
    https://stackoverflow.com/a/43395461
    and we want to be ready for the Python2 EOL.
    
    Closes #36966.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libxslt.rb
+++ Formula/libxslt.rb
@@ -3,6 +3,7 @@ class Libxslt < Formula
   homepage "http://xmlsoft.org/XSLT/"
   url "http://xmlsoft.org/sources/libxslt-1.1.33.tar.gz"
   sha256 "8e36605144409df979cab43d835002f63988f3dc94d5d3537c12796db90e38c8"
+  revision 1
 
   bottle do
     cellar :any
@@ -26,12 +27,10 @@ class Libxslt < Formula
   def install
     system "autoreconf", "-fiv" if build.head?
 
-    # https://bugzilla.gnome.org/show_bug.cgi?id=762967
-    inreplace "configure", /PYTHON_LIBS=.*/, 'PYTHON_LIBS="-undefined dynamic_lookup"'
-
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}",
+                          "--without-python",
                           "--with-libxml-prefix=#{Formula["libxml2"].opt_prefix}"
     system "make"
     system "make", "install"
