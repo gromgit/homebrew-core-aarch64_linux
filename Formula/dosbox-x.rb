92521351546061d692202f05520479d0d93120ac
Kyungdahm Yun <tomyun@gmail.com>
2019-02-14 11:48:53 -0800

    dosbox-x 0.82.15
    Closes #36990.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/dosbox-x.rb
+++ Formula/dosbox-x.rb
@@ -1,8 +1,8 @@
 class DosboxX < Formula
   desc "DOSBox with accurate emulation and wide testing"
   homepage "http://dosbox-x.com/"
-  url "https://github.com/joncampbell123/dosbox-x/archive/dosbox-x-v0.82.14.tar.gz"
-  sha256 "eda33f10a369fe81d5a4bc6250ca97d3b707cc45e79be66ccfa410174ba77aef"
+  url "https://github.com/joncampbell123/dosbox-x/archive/dosbox-x-v0.82.15.tar.gz"
+  sha256 "60e196ee49e4532327d0786a9e3b4f1d28820906b0b868abecf89a19300fb3da"
   version_scheme 1
   head "https://github.com/joncampbell123/dosbox-x.git"
 
