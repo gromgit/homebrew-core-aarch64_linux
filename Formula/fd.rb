7110c7acf7bd939bef1cc2e1cd0b6ae3fb66ac9e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-14 16:25:52 +0000

    fd: update 7.3.0 bottle.

--- Formula/fd.rb
+++ Formula/fd.rb
@@ -6,9 +6,10 @@ class Fd < Formula
   head "https://github.com/sharkdp/fd.git"
 
   bottle do
-    sha256 "0c4e3e81448c0c1fe41d3ee84c7f0fd28f2fb0a57888107b400be99032d3123c" => :mojave
-    sha256 "42c3bf9ff769177c0ae456008a0b9f80c8a8851189838e3a0b74efa84d0b34db" => :high_sierra
-    sha256 "4fe626ac8a1968b05a6d85789401589040c8170916e1b8f3a70475b3473efc5b" => :sierra
+    cellar :any_skip_relocation
+    sha256 "a5f33efc94ca927a3670dda123b86f08c516c8cbb3c29e4ffef0a46f79a534e8" => :mojave
+    sha256 "217ba35b4607e60c84970bfc6d84a665e9d19e8b5a82826550120d8cf59c09bb" => :high_sierra
+    sha256 "c64ac7b88a979f3eca36324a215eab26989c15c5ee5dbfbba61578600fdf85cd" => :sierra
   end
 
   depends_on "rust" => :build
