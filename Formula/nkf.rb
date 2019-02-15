2ee91876eaaca44ef7631147cac66f13571a5515
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-15 10:36:55 +0000

    nkf: update 2.1.5 bottle.

--- Formula/nkf.rb
+++ Formula/nkf.rb
@@ -7,12 +7,9 @@ class Nkf < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "4d3956870eb3cc800ffa0fbe527ce6012a14b81f98d4af9485fab8e99316560c" => :mojave
-    sha256 "1078899ada2ce3d45e4025e0c41f46f5e97856acfabb1c1d07f78fa1bb6f7048" => :high_sierra
-    sha256 "f0f532f5f6bba92842ed4da8d9cddbbb949c95933bc6251e6c58575aa5e27949" => :sierra
-    sha256 "71a4b7a8a2eef8ae33e8c6f81ebfd8afbcae8237c03720783d2157dcd307b600" => :el_capitan
-    sha256 "de741a793e59bc390e3b919f3d3bab245694e7c92c1a821ff60480078f1ae67d" => :yosemite
-    sha256 "8b34cb6acebf1a6a446572924c3a585e48ad0007dc394670ee94b9c1fe5d61c2" => :mavericks
+    sha256 "85183c457daaecd9a3ce59cea556189ad0131c6134d77e7890643a3fb75e3965" => :mojave
+    sha256 "9af47f293d4531c8d7ec5a81bd041349773f982b9710edca03eb3eb59b02a8b5" => :high_sierra
+    sha256 "8d908ee97c34e85ed85c268c895e143d57c7afdd9bc232a75b690067281765fc" => :sierra
   end
 
   def install
