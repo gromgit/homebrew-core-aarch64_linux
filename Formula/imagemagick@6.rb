49914034ac26ba7bc0881a5bb383b0bd3b8677e1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-15 14:10:31 +0000

    imagemagick@6: update 6.9.10-27 bottle.

--- Formula/imagemagick@6.rb
+++ Formula/imagemagick@6.rb
@@ -10,10 +10,9 @@ class ImagemagickAT6 < Formula
   head "https://github.com/imagemagick/imagemagick6.git"
 
   bottle do
-    rebuild 1
-    sha256 "612e81d039932db06743b0a860e5fd8aa52271cfcac1a0caf67c5688de5ee86c" => :mojave
-    sha256 "93e72634afe574bb635eaa699f6388ce76b23c94c52457805593a73555cc18ad" => :high_sierra
-    sha256 "c2bf971e666ebe2a82084503290d72132c61b186c7076afd6a614abc1c90c742" => :sierra
+    sha256 "5640bf5ead64a2b7e5bd0501cf6743803f580ce159157dbf7547d08e4f897469" => :mojave
+    sha256 "bd4d29599418d3ccbe54b96d4d8cc884e54d9f4123bbd1ff6b6353bc3c0474d2" => :high_sierra
+    sha256 "3e3e9af787a495db523b75f10663eecfca9cd08f1af427f608b162e58e62d1cf" => :sierra
   end
 
   keg_only :versioned_formula
