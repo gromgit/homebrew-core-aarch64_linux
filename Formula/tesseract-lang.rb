467154d65928080b302e88b0ec90bc70a30a24de
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-15 12:14:57 +0000

    tesseract-lang: add 4.0.0 bottle.

--- Formula/tesseract-lang.rb
+++ Formula/tesseract-lang.rb
@@ -4,6 +4,13 @@ class TesseractLang < Formula
   url "https://github.com/tesseract-ocr/tessdata_fast/archive/4.0.0.tar.gz"
   sha256 "f1b71e97f27bafffb6a730ee66fd9dc021afc38f318fdc80a464a84a519227fe"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "631211ef37fcafa9a3fac6a7cd6ca94aaeca83ae28543716a7aaa9cf1072d414" => :mojave
+    sha256 "631211ef37fcafa9a3fac6a7cd6ca94aaeca83ae28543716a7aaa9cf1072d414" => :high_sierra
+    sha256 "4c69eedd24721f0e47a645ae20a08bf8c8083f805615c7b46a73406a9f593cb1" => :sierra
+  end
+
   depends_on "tesseract"
 
   resource "testfile" do
