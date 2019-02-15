5ea5509e1ce88a3660c1443df08f2b7a26aefb71
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-15 19:05:51 +0000

    sha3sum: add 1.1.5 bottle.

--- Formula/sha3sum.rb
+++ Formula/sha3sum.rb
@@ -4,6 +4,13 @@ class Sha3sum < Formula
   url "https://github.com/maandree/sha3sum/archive/1.1.5.tar.gz"
   sha256 "1a4eef5b09b7f70af1f6970840475d4735a14b4fab21937b9fd104b8606654ce"
 
+  bottle do
+    cellar :any
+    sha256 "261621253f9915637cb0e49ce33764bc7c7fa80ac54481ba897b9cfffcb82965" => :mojave
+    sha256 "4042da1f6559af01943de4bca3c86b9b4b5729cae9a0cb5009b849870fb086b2" => :high_sierra
+    sha256 "99c160dd3fab4dd776d940ec369b9d11e5948376755f12f8c0248a3f5ab8b223" => :sierra
+  end
+
   depends_on "libkeccak"
 
   def install
