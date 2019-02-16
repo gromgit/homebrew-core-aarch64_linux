d297eb020bb68a4cc5ae39a65ca6fb7d17dc876a
iawia002 <xuxinzhao@caicloud.io>
2019-02-16 21:41:47 +0800

    annie 0.9.2
    Closes #37027.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/annie.rb
+++ Formula/annie.rb
@@ -1,8 +1,8 @@
 class Annie < Formula
   desc "Fast, simple and clean video downloader"
   homepage "https://github.com/iawia002/annie"
-  url "https://github.com/iawia002/annie/archive/0.9.1.tar.gz"
-  sha256 "20b43ceb46663513b164cb581ddae4276ee9b371f122e52ae4baef65c56e4a3b"
+  url "https://github.com/iawia002/annie/archive/0.9.2.tar.gz"
+  sha256 "075439747b1641683bd2d9a8d7d7e3d1ad916417576ccb4b89d01cfd73a0fcc6"
 
   bottle do
     cellar :any_skip_relocation
