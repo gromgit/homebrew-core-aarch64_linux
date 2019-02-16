0d57d17c8b289f356645c51a199650759b029761
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-16 12:55:45 +0000

    openconnect: update 8.02_1 bottle.

--- Formula/openconnect.rb
+++ Formula/openconnect.rb
@@ -7,9 +7,9 @@ class Openconnect < Formula
   revision 1
 
   bottle do
-    sha256 "53521355595b9b5ad7c67c5d37ea772eb4e8a47131e4431e95ae1473bacc70f2" => :mojave
-    sha256 "54aa04eb941ed606b81340e16b2ea50d53f7adb9be2df3bd4e309bdc6b34a400" => :high_sierra
-    sha256 "25cdd4355fa850a16dfd16498f1034b9bd6f8a8b753727fa9401a7d88934a2fc" => :sierra
+    sha256 "2c60626bb359263b1b3b00f22dc743e244140117c70e50496493ecaccf292411" => :mojave
+    sha256 "761a9e26152f64337384b7acdd494ecb15b4923e3135c2798039c57d1c55b962" => :high_sierra
+    sha256 "9bb30d4ad068501a75822ba735aafd43b4d7e5b211acad6bf8cfca26879c5195" => :sierra
   end
 
   head do
