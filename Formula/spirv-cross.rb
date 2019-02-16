02c2af290160a27f042427634701d8618296dc90
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-16 13:21:06 +0000

    spirv-cross: add 2019-01-31 bottle.

--- Formula/spirv-cross.rb
+++ Formula/spirv-cross.rb
@@ -5,6 +5,13 @@ class SpirvCross < Formula
   version "2019-01-31"
   sha256 "1876b044cd21942752492c99f12fcd7e69debb6a63c52277ae83303bd6437f59"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d6ad044641c877f3ea71fea105efffa962e0fbbc08bcaba727f9981fc8f0f04f" => :mojave
+    sha256 "ba72585aceb2f14ab986b8706ede32793aacbe6892ef7de92fe860152fcb868f" => :high_sierra
+    sha256 "d01225d532e3f12e071581c82d05b59e80d29cb7a3e35a135bdfc7637a01dad2" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "glm" => :test
   depends_on "glslang" => :test
