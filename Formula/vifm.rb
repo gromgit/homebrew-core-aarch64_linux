fd71e3585937db4bf92342a2acae09504f06b278
Diogo Lemos <diogo.mbl@gmail.com>
2019-02-17 15:46:17 +0000

    vifm 0.10
    Closes #37047.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/vifm.rb
+++ Formula/vifm.rb
@@ -1,8 +1,8 @@
 class Vifm < Formula
   desc "Ncurses based file manager with vi like keybindings"
   homepage "https://vifm.info/"
-  url "https://github.com/vifm/vifm/releases/download/v0.9.1/vifm-0.9.1.tar.bz2"
-  sha256 "28b9a4b670d9ddc9af8c9804dc22fa93f4fd0adabce94d43ebedc157a5dce7b3"
+  url "https://github.com/vifm/vifm/releases/download/v0.10/vifm-0.10.tar.bz2"
+  sha256 "e05a699f58279f69467d75d8cd3d6c8d2f62806c467fd558eda45ae9590768b8"
 
   bottle do
     sha256 "f390e5effa7cc533944c35e9ea4f389b0ab38780f2011c69de759e8bc7dbe784" => :mojave
@@ -10,6 +10,10 @@ class Vifm < Formula
     sha256 "7123769c4a1a3ea3e59871d3150182bbf3da2d98a4036b3a06a39ebacfaf65ed" => :sierra
     sha256 "88bda24c638a68880447a6e10dcfa06ac2e49a2b77415b6a85ac3dfd33c20114" => :el_capitan
   end
+
+  # Upstream fix: https://github.com/vifm/vifm/commit/4ad11aca
+  # Remove in next version
+  patch :DATA
 
   def install
     system "./configure", "--disable-dependency-tracking",
@@ -19,6 +23,7 @@ class Vifm < Formula
                           "--without-X11"
     system "make"
     system "make", "check"
+
     ENV.deparallelize { system "make", "install" }
   end
 
@@ -26,3 +31,20 @@ class Vifm < Formula
     assert_match version.to_s, shell_output("#{bin}/vifm --version")
   end
 end
+
+__END__
+diff --git a/src/ui/ui.c b/src/ui/ui.c
+index 117ca8b86..ce82b17e8 100644
+--- a/src/ui/ui.c
++++ b/src/ui/ui.c
+@@ -345,6 +345,11 @@ ui_update_term_state(void)
+ int
+ ui_char_pressed(wint_t c)
+ {
++	if(curr_stats.load_stage < 2)
++	{
++		return 0;
++	}
++
+	wint_t pressed = L'\0';
+	const int cancellation_state = ui_cancellation_pause();
