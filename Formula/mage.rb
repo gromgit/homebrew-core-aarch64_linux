94e779465d268475a9599b7bde0d542f0eede633
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-17 07:50:04 +0000

    mage: add 1.8.0 bottle.

--- Formula/mage.rb
+++ Formula/mage.rb
@@ -6,6 +6,13 @@ class Mage < Formula
       :revision => "aedfce64c122eef47009b7f80c9771044753215d"
   sha256 "e8fdfa30f68c8a90fcadd4e82f49c9136011accabff55e073ea26f5ee4280cf0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8833b1072a45cee90428727a5d12f535ea47c88fad1af229404b9e928eed885b" => :mojave
+    sha256 "25e0320d8267416873745b7236ea3744a845e88e42a802039d4d43acc8d29a26" => :high_sierra
+    sha256 "830b0ba9dcfd80c1d10ce587ceafb9585f4d98cb072dc30107ca34958f0858af" => :sierra
+  end
+
   depends_on "go"
 
   def install
