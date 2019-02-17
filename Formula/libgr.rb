1f9f39af2495603823875eec4e998fb7edf45849
Gabriel Mazetto <brodock@gmail.com>
2019-02-17 12:47:03 +0100

    libgr 0.37.0 (new formula)
    Closes #37046.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/libgr.rb
@@ -0,0 +1,37 @@
+class Libgr < Formula
+  desc "GR framework: a graphics library for visualisation applications"
+  homepage "https://gr-framework.org/"
+  url "https://github.com/sciapp/gr/archive/v0.37.0.tar.gz"
+  sha256 "9120c4215cd2c10239f45f4cbf79dcb199a9e7e3bbbf36e5ce8f4e5eb2e70e24"
+
+  depends_on :xcode => :build
+  depends_on "cairo"
+
+  def install
+    system "make", "self", "GRDIR=#{prefix}"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <gr.h>
+
+      int main(void) {
+          gr_opengks();
+          gr_openws(1, "test.png", 140);
+          double x[] = {0, 0.2, 0.4, 0.6, 0.8, 1.0};
+          double y[] = {0.3, 0.5, 0.4, 0.2, 0.6, 0.7};
+          gr_polyline(6, x, y);
+          gr_axes(gr_tick(0, 1), gr_tick(0, 1), 0, 0, 1, 1, -0.01);
+          gr_updatews();
+          gr_emergencyclosegks();
+          return 0;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-o", "test", "-I#{include}", "-L#{lib}", "-lGR"
+    system "./test"
+
+    assert_predicate testpath/"test.png", :exist?
+  end
+end
