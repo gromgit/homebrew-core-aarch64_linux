cedae906bad7f2ea1427f5ee3f91247d221d1dde
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-18 22:54:46 +0000

    reprepro: add 5.3.0 bottle.

--- Formula/reprepro.rb
+++ Formula/reprepro.rb
@@ -4,6 +4,13 @@ class Reprepro < Formula
   url "https://deb.debian.org/debian/pool/main/r/reprepro/reprepro_5.3.0.orig.tar.gz"
   sha256 "5a5404114b43a2d4ca1f8960228b1db32c41fb55de1996f62bc1b36001f3fab4"
 
+  bottle do
+    cellar :any
+    sha256 "0c031982eeb209bb7df512a9d80fbe90eea9bb1f1dfabe5239b99bd33807aa03" => :mojave
+    sha256 "c1d32f22872c366dec7849f15b3a4c53787b7406e97df68545f150ce9f98ae98" => :high_sierra
+    sha256 "5b0c6328f2cec66382e9202561e28b44ad61a03ad4b4f987d56467d523f00cfe" => :sierra
+  end
+
   depends_on "berkeley-db@4"
   depends_on "gcc"
   depends_on "gpgme"
