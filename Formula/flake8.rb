e83fd195b2e988532d66073cb4c59ca73c476327
Chongyu Zhu <i@lembacon.com>
2019-02-18 22:01:55 +0800

    flake8 3.7.5
    Closes #36733.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/flake8.rb
+++ Formula/flake8.rb
@@ -3,8 +3,8 @@ class Flake8 < Formula
 
   desc "Lint your Python code for style and logical errors"
   homepage "http://flake8.pycqa.org/"
-  url "https://gitlab.com/pycqa/flake8/repository/archive.tar.gz?ref=3.6.0"
-  sha256 "c19a9954dd8121ace467d605e63188dad7ce34b77a1e8dfc7c4d967016a85bd6"
+  url "https://gitlab.com/pycqa/flake8/-/archive/3.7.5/flake8-3.7.5.tar.gz"
+  sha256 "64014a82b5d81d30393b505b71ec6457c7e8f2761d78bc1b27921bc9f0842580"
   head "https://gitlab.com/PyCQA/flake8.git", :shallow => false
 
   bottle do
@@ -16,11 +16,38 @@ class Flake8 < Formula
 
   depends_on "python"
 
+  resource "entrypoints" do
+    url "https://files.pythonhosted.org/packages/b4/ef/063484f1f9ba3081e920ec9972c96664e2edb9fdc3d8669b0e3b8fc0ad7c/entrypoints-0.3.tar.gz"
+    sha256 "c70dd71abe5a8c85e55e12c19bd91ccfeec11a6e99044204511f9ed547d48451"
+  end
+
+  resource "mccabe" do
+    url "https://files.pythonhosted.org/packages/06/18/fa675aa501e11d6d6ca0ae73a101b2f3571a565e0f7d38e062eec18a91ee/mccabe-0.6.1.tar.gz"
+    sha256 "dd8d182285a0fe56bace7f45b5e7d1a6ebcbf524e8f3bd87eb0f125271b8831f"
+  end
+
+  resource "pycodestyle" do
+    url "https://files.pythonhosted.org/packages/1c/d1/41294da5915f4cae7f4b388cea6c2cd0d6cd53039788635f6875dfe8c72f/pycodestyle-2.5.0.tar.gz"
+    sha256 "e40a936c9a450ad81df37f549d676d127b1b66000a6c500caa2b085bc0ca976c"
+  end
+
+  resource "pyflakes" do
+    url "https://files.pythonhosted.org/packages/48/6d/7bfd617b21292397e10e24af4cf42947a359b0c425b66f194cf5d14b1444/pyflakes-2.1.0.tar.gz"
+    sha256 "5e8c00e30c464c99e0b501dc160b13a14af7f27d4dffb529c556e30a159e231d"
+  end
+
   def install
     venv = virtualenv_create(libexec, "python3")
-    system libexec/"bin/pip", "install", "-v", "--no-binary", ":all:",
-                              "--ignore-installed", buildpath
-    system libexec/"bin/pip", "uninstall", "-y", name
+    resource("entrypoints").stage do
+      # Without removing this file, `pip` will ignore the `setup.py` file and
+      # attempt to download the [`flit`](https://github.com/takluyver/flit)
+      # build system.
+      rm_f "pyproject.toml"
+      venv.pip_install Pathname.pwd
+    end
+    (resources.map(&:name).to_set - ["entrypoints"]).each do |r|
+      venv.pip_install resource(r)
+    end
     venv.pip_install_and_link buildpath
   end
 
