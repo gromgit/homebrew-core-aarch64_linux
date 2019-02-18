b7514ca35e1ba8f7d78eb8a1b074d80ddd05e08b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-18 14:18:45 +0000

    fonttools: update 3.38.0 bottle.

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -9,9 +9,9 @@ class Fonttools < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b820dc8de84bbafb401633cfdd367b3e60baed238672f58d2ec7858c20ec4cca" => :mojave
-    sha256 "3ae99498d300dddce3c4fba48c159fc654b1d2b3268aaaba58cef51f061b26d8" => :high_sierra
-    sha256 "42a149616f0b23745bff2478b8df0a00608d02c4b4074377b0802aecb51b0e8d" => :sierra
+    sha256 "cb9a3f0516ec5f72349411ec5d7c9bf8589a8b3b72d0450a86105beec402a9a3" => :mojave
+    sha256 "45954f4cbdaecbd6370ed1733ff262a61f15b292e6919584d6b83cb09c4001de" => :high_sierra
+    sha256 "404d6fd4e3ddd02962f74c6b25894cc6749646a02ea17ff3dfddfafb77efd810" => :sierra
   end
 
   depends_on "python"
