886ed68c16cbd86f87e445cda3c1d7d302622644
xykong <xy.kong@gmail.com>
2019-02-18 20:55:41 +0800

    frpc 0.26.0 (new formula)
    Closes #37062.
    
    Signed-off-by: Izaak Beekman <zbeekman@gmail.com>

---
+++ Formula/frpc.rb
@@ -0,0 +1,65 @@
+class Frpc < Formula
+  desc "Client app of fast reverse proxy to expose a local server to the internet"
+  homepage "https://github.com/fatedier/frp"
+  url "https://github.com/fatedier/frp.git",
+      :tag      => "v0.26.0",
+      :revision => "6a7efc81c9fcd410ba603573cdbe501056b1b04a"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    contents = Dir["{*,.git,.gitignore}"]
+    (buildpath/"src/github.com/fatedier/frp").install contents
+
+    (buildpath/"bin").mkpath
+    (etc/"frp").mkpath
+
+    cd "src/github.com/fatedier/frp" do
+      system "make", "frpc"
+      bin.install "bin/frpc"
+      etc.install "conf/frpc.ini" => "frp/frpc.ini"
+      etc.install "conf/frpc_full.ini" => "frp/frpc_full.ini"
+      prefix.install_metafiles
+    end
+  end
+
+  plist_options :manual => "frpc -c #{HOMEBREW_PREFIX}/etc/frp/frpc.ini"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>KeepAlive</key>
+        <true/>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/frpc</string>
+          <string>-c</string>
+          <string>#{etc}/frp/frpc.ini</string>
+        </array>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/frpc.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/frpc.log</string>
+      </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    system bin/"frpc", "-v"
+    assert_match "Commands", shell_output("#{bin}/frpc help")
+    assert_match "local_port", shell_output("#{bin}/frpc http", 1)
+    assert_match "local_port", shell_output("#{bin}/frpc https", 1)
+    assert_match "local_port", shell_output("#{bin}/frpc stcp", 1)
+    assert_match "local_port", shell_output("#{bin}/frpc tcp", 1)
+    assert_match "local_port", shell_output("#{bin}/frpc udp", 1)
+    assert_match "local_port", shell_output("#{bin}/frpc xtcp", 1)
+    assert_match "admin_port", shell_output("#{bin}/frpc status -c #{etc}/frp/frpc.ini", 1)
+    assert_match "admin_port", shell_output("#{bin}/frpc reload -c #{etc}/frp/frpc.ini", 1)
+  end
+end
