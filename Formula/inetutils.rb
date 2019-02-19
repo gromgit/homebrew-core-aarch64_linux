eb355ae1ab783153e9154da02e466fe1beca18d8
Mike McQuaid <mike@mikemcquaid.com>
2019-02-19 13:28:11 +0000

    inetutils: fix rubocop 0.65.0 style.

--- Formula/inetutils.rb
+++ Formula/inetutils.rb
@@ -43,6 +43,7 @@ class Inetutils < Formula
     # man pages into libexec/gnuman
     bin.find.each do |path|
       next unless File.executable?(path) && !File.directory?(path)
+
       cmd = path.basename.to_s.sub(/^g/, "")
       (libexec/"gnubin").install_symlink bin/"g#{cmd}" => cmd
       (libexec/"gnuman"/"man1").install_symlink man1/"g#{cmd}.1" => "#{cmd}.1"
