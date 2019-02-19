6c90cd9316bfd7741e6022304f0969b44b540de4
Mike McQuaid <mike@mikemcquaid.com>
2019-02-19 13:28:12 +0000

    maven: fix rubocop 0.65.0 style.

--- Formula/maven.rb
+++ Formula/maven.rb
@@ -24,8 +24,10 @@ class Maven < Formula
     # file will be found relative to it
     Pathname.glob("#{libexec}/bin/*") do |file|
       next if file.directory?
+
       basename = file.basename
       next if basename.to_s == "m2.conf"
+
       (bin/basename).write_env_script file, Language::Java.overridable_java_home_env
     end
   end
