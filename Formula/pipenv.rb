d0702b58d5e7f2b546493c1262a6431070d43830
Mike McQuaid <mike@mikemcquaid.com>
2019-02-19 13:28:14 +0000

    pipenv: fix rubocop 0.65.0 style.

--- Formula/pipenv.rb
+++ Formula/pipenv.rb
@@ -61,6 +61,7 @@ class Pipenv < Formula
     lib_python_path = Pathname.glob(libexec/"lib/python*").first
     lib_python_path.each_child do |f|
       next unless f.symlink?
+
       realpath = f.realpath
       rm f
       ln_s realpath, f
