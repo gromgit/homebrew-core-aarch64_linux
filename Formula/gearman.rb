817d653a510fd61071021d96451e8b8eef6a86d1
Mike McQuaid <mike@mikemcquaid.com>
2019-02-19 13:28:11 +0000

    gearman: fix rubocop 0.65.0 style.

--- Formula/gearman.rb
+++ Formula/gearman.rb
@@ -25,6 +25,7 @@ class Gearman < Formula
     # https://bugs.launchpad.net/gearmand/+bug/1368926
     Dir["tests/**/*.cc", "libtest/main.cc"].each do |test_file|
       next unless /std::unique_ptr/ =~ File.read(test_file)
+
       inreplace test_file, "std::unique_ptr", "std::auto_ptr"
     end
 
