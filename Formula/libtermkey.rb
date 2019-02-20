81395a39cf110700a276a0632732f929d7d0ae44
Chongyu Zhu <i@lembacon.com>
2019-02-20 08:55:51 +0800

    libtermkey 0.21.1
    Closes #37114.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libtermkey.rb
+++ Formula/libtermkey.rb
@@ -1,8 +1,8 @@
 class Libtermkey < Formula
   desc "Library for processing keyboard entry from the terminal"
   homepage "http://www.leonerd.org.uk/code/libtermkey/"
-  url "http://www.leonerd.org.uk/code/libtermkey/libtermkey-0.20.tar.gz"
-  sha256 "6c0d87c94ab9915e76ecd313baec08dedf3bd56de83743d9aa923a081935d2f5"
+  url "http://www.leonerd.org.uk/code/libtermkey/libtermkey-0.21.1.tar.gz"
+  sha256 "cecbf737f35d18f433c8d7864f63c0f878af41f8bd0255a3ebb16010dc044d5f"
 
   bottle do
     cellar :any
