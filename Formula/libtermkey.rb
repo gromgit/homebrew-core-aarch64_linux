daca575b8e6376bc2faf895f6bd48d494ab76ca7
Chongyu Zhu <i@lembacon.com>
2019-02-20 16:54:49 +0800

    libtermkey: depend on unibilium
    Closes #37118.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/libtermkey.rb
+++ Formula/libtermkey.rb
@@ -3,6 +3,7 @@ class Libtermkey < Formula
   homepage "http://www.leonerd.org.uk/code/libtermkey/"
   url "http://www.leonerd.org.uk/code/libtermkey/libtermkey-0.21.1.tar.gz"
   sha256 "cecbf737f35d18f433c8d7864f63c0f878af41f8bd0255a3ebb16010dc044d5f"
+  revision 1
 
   bottle do
     cellar :any
@@ -13,6 +14,7 @@ class Libtermkey < Formula
 
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
+  depends_on "unibilium"
 
   def install
     system "make", "PREFIX=#{prefix}"
