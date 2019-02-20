c447bb95c1e0a7006d8a26e9e30f83d69a0b0099
ideal <idealities@gmail.com>
2019-02-20 18:41:38 +0800

    ydcv 0.6.2
    Closes #37120.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/ydcv.rb
+++ Formula/ydcv.rb
@@ -1,14 +1,18 @@
 class Ydcv < Formula
+  include Language::Python::Virtualenv
+
   desc "YouDao Console Version"
   homepage "https://github.com/felixonmars/ydcv"
-  url "https://github.com/felixonmars/ydcv/archive/0.6.1.tar.gz"
-  sha256 "0a01fed567a1045cf6c81da9532ed07852f99dc93c862f59255fda74aab05714"
+  url "https://github.com/felixonmars/ydcv/archive/0.6.2.tar.gz"
+  sha256 "45a237fba401771c5ad8455938e6cf360beab24655a4961db368eb2fbbbfb546"
 
-  bottle :unneeded
+  depends_on "python"
 
   def install
-    bin.install "src/ydcv.py" => "ydcv"
+    ENV["SETUPTOOLS_SCM_PRETEND_VERSION"] = version
+
     zsh_completion.install "contrib/zsh_completion" => "_ydcv"
+    virtualenv_install_with_resources
   end
 
   test do
