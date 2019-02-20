041ceb441750bf9e4ccca37d254e625f9be0e44d
mattcurrie <me@mattcurrie.com>
2019-02-20 13:44:24 +1300

    rgbds: revised due to re-tag
    Closes #37113.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/rgbds.rb
+++ Formula/rgbds.rb
@@ -2,7 +2,8 @@ class Rgbds < Formula
   desc "Rednex GameBoy Development System"
   homepage "https://github.com/rednex/rgbds"
   url "https://github.com/rednex/rgbds/archive/v0.3.8.tar.gz"
-  sha256 "fbf983cff2246b5169a66a61c182c3dd2b8d484bd683c5af94ede74ad983cc1f"
+  sha256 "264f5e98ccdccb51b05dac80d9d37ddae769863c97f726b87be692edd5612256"
+  revision 1
   head "https://github.com/rednex/rgbds.git"
 
   bottle do
