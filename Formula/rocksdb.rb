d38e4b445ea1525fba69b1d7b88dc63e9faaa21b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-21 09:22:30 +0000

    rocksdb: update 5.18.3 bottle.

--- Formula/rocksdb.rb
+++ Formula/rocksdb.rb
@@ -6,10 +6,9 @@ class Rocksdb < Formula
 
   bottle do
     cellar :any
-    sha256 "df69fa36b2b025f62cc0d30da46e072512d424cc66c242834fcfba7a60cb913c" => :mojave
-    sha256 "b4dd4f1c3324876dcf7a9524d6440a0891f162db3c19b97284100aa4b13cabbe" => :high_sierra
-    sha256 "8d3515c91f5eecb1828af7825ddadb0169720b47b2cd76411ad7ca3792699e20" => :sierra
-    sha256 "e07d5748ec17ea316d7b669bf22e69db5b5c12c488030abbc164d17f004ace44" => :el_capitan
+    sha256 "5cd6d6d08b6e2d9f137984f74396bed857c88686f69496b3963703d3301a4fa9" => :mojave
+    sha256 "29b90ff030110657a824dcb76e4c71ce27af8803e1a8ab19c540d07c4f581b5e" => :high_sierra
+    sha256 "5e2bbca2e528f5ebe654d5ad9d6b1057cc5901c169e63dcb99b104ce6e468d5f" => :sierra
   end
 
   depends_on "gflags"
