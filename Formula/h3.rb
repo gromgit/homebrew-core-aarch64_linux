a379bec817d3b4a8d51deb39e67530db98ef11d7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-23 14:30:20 +0000

    h3: add 3.4.2 bottle.

--- Formula/h3.rb
+++ Formula/h3.rb
@@ -4,6 +4,13 @@ class H3 < Formula
   url "https://github.com/uber/h3/archive/v3.4.2.tar.gz"
   sha256 "c6ff7bb9d93e25e5133f8c79c515407994349ad1606d0fd7f9f4e90592c1bd7c"
 
+  bottle do
+    cellar :any
+    sha256 "d6b222684aa002d2299354eac1afbf84864ae0180f767b67fef6adbe63a80bfc" => :mojave
+    sha256 "aedb21b22ef839267946e2ca37ec13eea92bac2519390e9e98f4151ff16a1070" => :high_sierra
+    sha256 "6a32d2da626b73de95d0625f7d2f1cdeef78229d7aae35e4a71554a3a8001f53" => :sierra
+  end
+
   depends_on "cmake" => :build
 
   def install
