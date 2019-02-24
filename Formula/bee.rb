61106f84d504a211aa3cdaaec5b0be738b6c2e3f
Caleb Xu <calebcenter@live.com>
2019-02-24 17:48:16 -0500

    bee 1.64 (#37260)

--- Formula/bee.rb
+++ Formula/bee.rb
@@ -1,8 +1,8 @@
 class Bee < Formula
   desc "Tool for managing database changes"
   homepage "https://github.com/bluesoft/bee"
-  url "https://github.com/bluesoft/bee/releases/download/1.63/bee-1.63.zip"
-  sha256 "8c17b9896ad2072e9a112a94fb8d5d8b4e09430da38b56bb29aaafb0713cb8b1"
+  url "https://github.com/bluesoft/bee/releases/download/1.64/bee-1.64.zip"
+  sha256 "31a0f5cef32fc2e2221ded3b1ff02cd0f47d141e8433a1b11f8d7159f7d9bb5c"
 
   bottle :unneeded
 
@@ -22,6 +22,6 @@ class Bee < Formula
       test-database.password=
     EOS
     (testpath/"bee").mkpath
-    system bin/"bee", "dbchange:create new-file"
+    system bin/"bee", "-d", testpath/"bee", "dbchange:create new-file"
   end
 end
