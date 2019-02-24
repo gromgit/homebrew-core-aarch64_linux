da435cd99e37c447b6cc2f08bf9074a007ba607e
Charlie Jung <32102793+charliecjung@users.noreply.github.com>
2019-02-24 02:24:04 -0600

    ncompress 4.2.4.5
    Closes #37277.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/ncompress.rb
+++ Formula/ncompress.rb
@@ -1,8 +1,8 @@
 class Ncompress < Formula
   desc "Fast, simple LZW file compressor"
   homepage "https://github.com/vapier/ncompress"
-  url "https://github.com/vapier/ncompress/archive/v4.2.4.4.tar.gz"
-  sha256 "2670439935e7639c3a767087da99810e45bc3997d0638b3094396043571e5aec"
+  url "https://github.com/vapier/ncompress/archive/v4.2.4.5.tar.gz"
+  sha256 "f575bbcdd5f844ce22d753b9acd23d2a6b73ffc15f204bebbaf8bd6f6574903b"
   head "https://github.com/vapier/ncompress.git"
 
   bottle do
