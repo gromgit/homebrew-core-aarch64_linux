69935f3eaa2572bb4edbc340c83e49a407c508cf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-24 02:58:31 +0000

    gx-go: update 1.9.0 bottle.

--- Formula/gx-go.rb
+++ Formula/gx-go.rb
@@ -9,10 +9,9 @@ class GxGo < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "18d61c9186cba473aea8e552aaa5f0042715f35c60e224889ae1cb5946ed1ddc" => :mojave
-    sha256 "7f169d9c48b0170a759ce56d9b14ea6975a2b120270276304254b88201767339" => :high_sierra
-    sha256 "523c2412e982efd6a1107b1a123981eed3b9fb9e2eb73d9118312e4638e520bd" => :sierra
-    sha256 "b55dab0e39dae445bed065f555535457b1f20cb22a4bcc3d0ec4584ed28c20c9" => :el_capitan
+    sha256 "8c86c8465cde5c6189e67f2d3b758604ef579d064f398cd48eff6ab6ce092bdb" => :mojave
+    sha256 "57eb4c948ce99ebca79f938539c1b5e096aef6c16554c30f5744b4e1fc93016d" => :high_sierra
+    sha256 "7eb7a89b575a1cb12464f1a0a4d14c5983333a79fb6e4fbb9c5b5240e540020d" => :sierra
   end
 
   depends_on "go" => :build
