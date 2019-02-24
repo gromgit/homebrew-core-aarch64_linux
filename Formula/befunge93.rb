07b5f69e19f2cda184b7eb1f51c54518d0e1478c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-24 06:48:25 +0000

    befunge93: update 2.25 bottle.

--- Formula/befunge93.rb
+++ Formula/befunge93.rb
@@ -8,12 +8,9 @@ class Befunge93 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b9119902c751f024359e163539e6d496d7e807929f7ddefa15c3ed0e99b10dd5" => :mojave
-    sha256 "90ce43353a46615332cf3a4228786f986ea93cac15dba1aeb79684e4f084afe4" => :high_sierra
-    sha256 "0b2b344570f71b3fa702675b2305b96632fde0a8da44085a15b15fe72863f66a" => :sierra
-    sha256 "fcebeb62391bd6e3eef571123af573766666ce9c40f139c889cc350bf6410d8b" => :el_capitan
-    sha256 "825c5d86e93d7cf0ecc2f3f16f626c27e658f1d4792bd6e74092b11f815097d7" => :yosemite
-    sha256 "a4f6102ac80c19ef969e7b2bbe70bdfd4f192df08d455b2b6162ce16e3616564" => :mavericks
+    sha256 "f77d79e0bc06317441221cf22696f3be81f75221531f63bcb71d819954e2884b" => :mojave
+    sha256 "3e82d14825ded095e92ebec797f682a65c7ada55ec178e069d0093eb29da8e4a" => :high_sierra
+    sha256 "302f0c69592719782676dc2873a9a9faef3f72b6ec7bb225d60e74ca1cc0f640" => :sierra
   end
 
   def install
