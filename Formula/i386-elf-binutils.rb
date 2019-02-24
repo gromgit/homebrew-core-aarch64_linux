fc743a5d7eec912c9f8fcd998ec8617e2e4a0b9e
Caleb Xu <calebcenter@live.com>
2019-02-24 01:53:26 -0500

    i386-elf-binutils 2.32
    Closes #37267.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/i386-elf-binutils.rb
+++ Formula/i386-elf-binutils.rb
@@ -1,8 +1,9 @@
 class I386ElfBinutils < Formula
   desc "FSF Binutils for i386-elf cross development"
   homepage "https://www.gnu.org/software/binutils/"
-  url "https://ftp.gnu.org/gnu/binutils/binutils-2.31.1.tar.xz"
-  sha256 "5d20086ecf5752cc7d9134246e9588fa201740d540f7eb84d795b1f7a93bca86"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.32.tar.gz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.32.tar.gz"
+  sha256 "9b0d97b3d30df184d302bced12f976aa1e5fbf4b0be696cdebc6cca30411a46e"
 
   bottle do
     sha256 "89312270dc465d7d2c622d7253acfecc7e8b0d5a5cc7688822c1dbb10ee7166e" => :mojave
