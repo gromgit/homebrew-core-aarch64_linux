efd9ebfa5b65d10ff897204aaebf61f074e3748d
Caleb Xu <calebcenter@live.com>
2019-02-23 23:41:41 -0500

    backupninja 1.1.0
    Closes #37245.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/backupninja.rb
+++ Formula/backupninja.rb
@@ -1,10 +1,9 @@
 class Backupninja < Formula
   desc "Backup automation tool"
   homepage "https://0xacab.org/riseuplabs/backupninja"
-  url "https://sourcearchive.raspbian.org/main/b/backupninja/backupninja_1.0.2.orig.tar.gz"
-  mirror "https://debian.ethz.ch/ubuntu/ubuntu/pool/universe/b/backupninja/backupninja_1.0.2.orig.tar.gz"
-  mirror "https://dl.bintray.com/homebrew/mirror/backupninja_1.0.2.orig.tar.gz"
-  sha256 "fdb399de331493c8f959a784318349b19a01fbeece275da2ecd70ec9847a80b6"
+  url "https://sourcearchive.raspbian.org/main/b/backupninja/backupninja_1.1.0.orig.tar.gz"
+  mirror "https://debian.ethz.ch/ubuntu/ubuntu/pool/universe/b/backupninja/backupninja_1.1.0.orig.tar.gz"
+  sha256 "abe444d0c7520ede7847b9497da4b1253a49579f59293b043c47b1dd9833280a"
 
   bottle do
     cellar :any_skip_relocation
@@ -14,6 +13,7 @@ class Backupninja < Formula
     sha256 "9018c721fb3774083fac0c4994afe6a0151bafaa5459242e37eec3c7c67a26dc" => :el_capitan
   end
 
+  depends_on "bash"
   depends_on "dialog"
   depends_on "gawk"
 
