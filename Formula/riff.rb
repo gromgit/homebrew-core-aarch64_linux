4131377d79d0a8f31615aba9e7919f3ede2678ba
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-25 10:21:00 +0000

    riff: add 0.2.0 bottle.

--- Formula/riff.rb
+++ Formula/riff.rb
@@ -7,6 +7,13 @@ class Riff < Formula
       :tag      => "v0.2.0",
       :revision => "1ae190ff3c7edf4b375ee935f746ebfd1d8eaf5c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "325694273e94fa659ef043da822687080048aed91ea560b54222539c08d3a39e" => :mojave
+    sha256 "e306adf4f271b2d4174defcd8b0feb58da47904d4fc29846ca160ad399d2f895" => :high_sierra
+    sha256 "6f1e171d2a60d05539fb10172625653039c787a8476198cab79a6638bdec3ad6" => :sierra
+  end
+
   depends_on "go" => :build
   depends_on "kubernetes-cli"
 
