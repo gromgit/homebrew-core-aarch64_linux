4bfa068b5e8aa892106a630340c43c94f2ff76b7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-25 17:41:48 +0000

    chkrootkit: update 0.53 bottle.

--- Formula/chkrootkit.rb
+++ Formula/chkrootkit.rb
@@ -7,11 +7,9 @@ class Chkrootkit < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "2563d001fbe410534061a3d3d0476dea7ecb5da2d54828badaee6e547c680d94" => :mojave
-    sha256 "97e158e6ce44a0f9a96b7cdaf827446632f1c74a51068417b6588dd80c15116d" => :high_sierra
-    sha256 "2ddd7a341ced3bdfd2a622e48e19627fccaeaff174c7241c42cc0e3305557a16" => :sierra
-    sha256 "18d66e7ae08f00fef387e72c8f8f9f74042f32247e52adabf5b838c4ffc83d58" => :el_capitan
-    sha256 "889be3584a64397ade2ccf4bf373a1e9c85b73880d209f6e10d7e19eecc122e7" => :yosemite
+    sha256 "286de88eef77a53b9c7fab85ef3cec8b9876cf49a48910cbb591e44c9ca5d631" => :mojave
+    sha256 "55ab9957505513fd81d670c54e5ad1834fb72ae9cda7bd7cbc63f98feeccf24a" => :high_sierra
+    sha256 "f16966e93433cb877b04be8ea086c8a23905290643099229ffa3d665b2d11994" => :sierra
   end
 
   def install
