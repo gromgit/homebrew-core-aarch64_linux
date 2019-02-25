37251d33d4c005bfa46f8763cc385262015478be
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-25 18:10:14 +0000

    cli53: update 0.8.14 bottle.

--- Formula/cli53.rb
+++ Formula/cli53.rb
@@ -6,9 +6,9 @@ class Cli53 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6638c669af3f120a6b641706702dc529f34836e2a7776b724ad4acead6aaeed9" => :mojave
-    sha256 "e39a63d966ad1e667711f5d4ae7e344785d9aedf659dea7fb8460b737c07a60a" => :high_sierra
-    sha256 "04b4f7c4b3e8c55230e358cdb32ea3844652946769cc81121e3688cd2f1c3918" => :sierra
+    sha256 "82e7f1b17ab07fc2ce7b70ee460d762c3044ea9321de7f1290a33d931e0d629f" => :mojave
+    sha256 "aa503927d31112aef3c44118762a9b3bb30a3ee5329947427903c25eae6baea7" => :high_sierra
+    sha256 "045133ce67ca54c26ee6b64045c9e7c954f169ce683293350305773e20f2db23" => :sierra
   end
 
   depends_on "go" => :build
