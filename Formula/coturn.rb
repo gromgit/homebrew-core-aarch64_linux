af314f038542eb9850f66f26e7230351d8fa0013
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-25 20:58:07 +0000

    coturn: update 4.5.1.0 bottle.

--- Formula/coturn.rb
+++ Formula/coturn.rb
@@ -5,10 +5,9 @@ class Coturn < Formula
   sha256 "b84581a46dd40ad674f2905d680e9d0be9743fbbc001198dd498a584fd2fbd15"
 
   bottle do
-    sha256 "33e924b07dcc67b74ab28fb262f5bfe09e526ca2fb17cf9c195962b167e654af" => :mojave
-    sha256 "275b217ccdf59713da3850782079387a78f24d0c1a35a9b23e67e01695f9e340" => :high_sierra
-    sha256 "079bdbb8abae75b20318710269f57b16c53088bae5c55d15073e6199f0526008" => :sierra
-    sha256 "2aef5b4ca2b27abfefaa9b90fec75328839f7e02b601d83d9b3afe984240c49f" => :el_capitan
+    sha256 "daaa92dccec4095cc801e36318cd63dd64ab31626a21b46f4ba323766aad93b2" => :mojave
+    sha256 "8f556656bccc21c5042b1fad00b6242ea783851a00141aa8cb54ff0e12cbb4fe" => :high_sierra
+    sha256 "5debb5ffa15e2ecd75b14a79d4077cd2d4b47520c282033507286953226aeda0" => :sierra
   end
 
   depends_on "libevent"
