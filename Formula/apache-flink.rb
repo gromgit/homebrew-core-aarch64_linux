1ec955a6e7141139c3c7b071b0813ca64108a20c
Martin Schimandl <martin.schimandl@gmail.com>
2019-02-26 22:51:56 +0100

    apache-flink: update URL (#37356)

--- Formula/apache-flink.rb
+++ Formula/apache-flink.rb
@@ -1,7 +1,7 @@
 class ApacheFlink < Formula
   desc "Scalable batch and stream data processing"
   homepage "https://flink.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.7.1/flink-1.7.1-bin-hadoop27-scala_2.11.tgz"
+  url "https://archive.apache.org/dist/flink/flink-1.7.1/flink-1.7.1-bin-hadoop27-scala_2.11.tgz"
   version "1.7.1"
   sha256 "e7ed4c76e59454bb6cb2a513b3ceb57a55b37a9ce02a9bdb3ff25ad84e19f328"
   head "https://github.com/apache/flink.git"
