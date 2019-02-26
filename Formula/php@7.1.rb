1ea5dcf2842f875f9937886fdb1347b8b5ed65d2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-26 11:00:30 +0000

    php@7.1: update 7.1.26 bottle.

--- Formula/php@7.1.rb
+++ Formula/php@7.1.rb
@@ -5,9 +5,10 @@ class PhpAT71 < Formula
   sha256 "10b7ae634c12852fae52a22dc2262e5f12418ad59fd20da2d00d71a212235d31"
 
   bottle do
-    sha256 "dbf95aea6a9d8a7ec374bb639f25d1cd6a4b89aa6d7228a4d4e5d7c6450b6dae" => :mojave
-    sha256 "be85308f59f525c7f81d9378195d04896c94728f5938325ef27fb189df39aec5" => :high_sierra
-    sha256 "71d49d3653487e3b93b3c90bc1aed65c4365642833163ddd0dc46d3222f279f2" => :sierra
+    rebuild 1
+    sha256 "98ac53f607efcfc7434353b125712069cba0f4a1064d59d71f14f140a5846992" => :mojave
+    sha256 "1cbf9e4ab19c09ed4c159f70ec6c94b6e8fb7b118b4cc5fe66ecee992dde5da1" => :high_sierra
+    sha256 "930ac107ad987bdf90c63407ee7b99d02a491bd01a2deead29889c85c39e253d" => :sierra
   end
 
   keg_only :versioned_formula
