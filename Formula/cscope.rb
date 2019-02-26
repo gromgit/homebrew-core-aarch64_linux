0dd3f47fb58576cee3b138959f6a4f770814851c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-26 07:24:24 +0000

    cscope: update 15.9 bottle.

--- Formula/cscope.rb
+++ Formula/cscope.rb
@@ -6,12 +6,9 @@ class Cscope < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d13055e5d40c7b29ca273be19d611db2d9c157198bddbaf85bf6ca58323e0dc4" => :mojave
-    sha256 "c341d01145d24da1e42347e3c8924569e97158c826ab5a118c2708baa3733ecc" => :high_sierra
-    sha256 "7065f35c7c69268a0c293c7108e936c92c83fa6c1aff78d9fb6eb4d6178cb3c9" => :sierra
-    sha256 "97930be35cbcd08980651597acf931adbb50a80df10960d6564497f19d9c8032" => :el_capitan
-    sha256 "71d86771790165c777341e4457dd193008cfb2fb24628a138cb45fec61e6b42d" => :yosemite
-    sha256 "cb2f63522d072307cacf63e8eabf4c284f2e8c1b2ff8c6de3aeb6fb8759a1212" => :mavericks
+    sha256 "0a8c76e372e2c965e654b5024cbf872931e6204b7e2ba79623d5d7d002cd3c2f" => :mojave
+    sha256 "ae7b5f716debeb937c3472add41f69c7176e9c4a9a0668090afd63313eabbe86" => :high_sierra
+    sha256 "7eef899511b0d7eb0d6a35acf677d9b19f89528aae0272d5c414bbafbe5daaaf" => :sierra
   end
 
   def install
