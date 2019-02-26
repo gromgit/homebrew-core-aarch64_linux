05132d775aaf550873f627c971927678dd8d6117
Alex Gaynor <agaynor@mozilla.com>
2019-02-26 10:00:39 -0500

    openssl 1.0.2r
    Closes #37353.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/openssl.rb
+++ Formula/openssl.rb
@@ -4,10 +4,10 @@
 class Openssl < Formula
   desc "SSL/TLS cryptography library"
   homepage "https://openssl.org/"
-  url "https://www.openssl.org/source/openssl-1.0.2q.tar.gz"
-  mirror "https://dl.bintray.com/homebrew/mirror/openssl--1.0.2q.tar.gz"
-  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.0.2q.tar.gz"
-  sha256 "5744cfcbcec2b1b48629f7354203bc1e5e9b5466998bbccc5b5fcde3b18eb684"
+  url "https://www.openssl.org/source/openssl-1.0.2r.tar.gz"
+  mirror "https://dl.bintray.com/homebrew/mirror/openssl--1.0.2r.tar.gz"
+  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.0.2r.tar.gz"
+  sha256 "ae51d08bba8a83958e894946f15303ff894d75c2b8bbd44a852b64e3fe11d0d6"
 
   bottle do
     sha256 "cdbee2befd8f2e178ff0c5f9e8796a73a7de20055aae51cba7cc749429e8c90f" => :mojave
