66ceff2021741a339417fa5f69c820b5359ebd48
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-27 07:09:13 +0000

    diffstat: update 1.62 bottle.

--- Formula/diffstat.rb
+++ Formula/diffstat.rb
@@ -7,11 +7,9 @@ class Diffstat < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "33b2a3e33397f24b695c98fd986ac90394e30f72eacd53ff5338ba4bd392835e" => :mojave
-    sha256 "19e885ba26c8b1d9df6e1577a8a419946e9c0e8f1e19223fef5d5b988125c22a" => :high_sierra
-    sha256 "30255ba9338a70f51fb80f44cc3993b98e44bd7946f5b598252f9a7d1c6800e9" => :sierra
-    sha256 "4b383a964ff74029f6555162d7548e11c1fe8a9f2295671484419c8e32016ede" => :el_capitan
-    sha256 "fb1b7c5b2802e7f13afcf58bd694eec31577c76ec9e32bbdef8254d08ca9866f" => :yosemite
+    sha256 "f2ddd2775174056c48eab541d32b99cfd2cc586e0227c4f2eec4b15bf5ce7128" => :mojave
+    sha256 "ac1e5199d1776d52adc03842b378da475f1db1282150ed9ce22c365a5b0cf7dd" => :high_sierra
+    sha256 "c6f9fd47c9736faf0cfb2f3e0ab6490e3974b4dca06d36f4bf01967c56aa1c14" => :sierra
   end
 
   def install
