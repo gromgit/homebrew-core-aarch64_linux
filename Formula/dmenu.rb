1f9ce3884cedc269739640933500dca03994f774
Caleb Xu <calebcenter@live.com>
2019-02-27 09:19:47 -0500

    dmenu 4.9

--- Formula/dmenu.rb
+++ Formula/dmenu.rb
@@ -1,8 +1,8 @@
 class Dmenu < Formula
   desc "Dynamic menu for X11"
   homepage "https://tools.suckless.org/dmenu/"
-  url "https://dl.suckless.org/tools/dmenu-4.8.tar.gz"
-  sha256 "fe615a5c3607061e2106700862e82ac62a9fa1e6a7ac3d616a9c76106476db61"
+  url "https://dl.suckless.org/tools/dmenu-4.9.tar.gz"
+  sha256 "b3971f4f354476a37b2afb498693649009b201550b0c7c88e866af8132b64945"
   head "https://git.suckless.org/dmenu/", :using => :git
 
   bottle do
