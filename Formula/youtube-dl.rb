4f9cdcc79fb543125b51826ee6187a43a59f0175
404NetworkError <404NetworkError@users.noreply.github.com>
2019-02-28 13:56:05 -0500

    youtube-dl 2019.03.01
    Closes #37441.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -1,8 +1,8 @@
 class YoutubeDl < Formula
   desc "Download YouTube videos from the command-line"
   homepage "https://rg3.github.io/youtube-dl/"
-  url "https://github.com/rg3/youtube-dl/releases/download/2019.02.18/youtube-dl-2019.02.18.tar.gz"
-  sha256 "61bbffc7b9b2dab71f93548f69f952ce0e4f50e7fa27e1d8b81eabdba37120eb"
+  url "https://github.com/rg3/youtube-dl/releases/download/2019.03.01/youtube-dl-2019.03.01.tar.gz"
+  sha256 "ae523a75bf41b3faa40d6f7dd44c7b8e1bc6919a91956b2528bbddeb9b32b32f"
 
   head do
     url "https://github.com/rg3/youtube-dl.git"
