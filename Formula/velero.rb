55ee56f3a1845496e5692be2d7b17c09f47aa390
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-28 22:56:18 +0000

    velero: add 0.11.0 bottle.

--- Formula/velero.rb
+++ Formula/velero.rb
@@ -4,6 +4,13 @@ class Velero < Formula
   url "https://github.com/heptio/velero/archive/v0.11.0.tar.gz"
   sha256 "366f4c1ed5800dbdddefa60ed88bdd82b406b69b76a214b1d7108997a2f973ac"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "fe9cf23132d9c9920fd26749ec96784df10671caf6d601cf1eeda3767ca799dd" => :mojave
+    sha256 "60a4d0fbacd56890542f2fea48ce779cad85cb5aa4686043865f3410bc5b5b47" => :high_sierra
+    sha256 "32eab0a60cd6c568f5784012318a7c3bc3d4bf7739ba9f358a270b839d0724bf" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
