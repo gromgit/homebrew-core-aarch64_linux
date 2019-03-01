d09a39dbdc5eb45b00d6a5db59212830639327d1
Michihito Shigemura <i.am.shige@gmail.com>
2019-03-01 13:10:24 +0900

    docker 18.09.3
    Closes #37462.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -2,8 +2,8 @@ class Docker < Formula
   desc "Pack, ship and run any application as a lightweight container"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/docker-ce.git",
-      :tag      => "v18.09.2",
-      :revision => "62479626f213818ba5b4565105a05277308587d5"
+      :tag      => "v18.09.3",
+      :revision => "774a1f4eee66e29a71ca12e88ac2220670990f7e"
 
   bottle do
     cellar :any_skip_relocation
