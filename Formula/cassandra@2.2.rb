6c2b3c0774f7afc7d80077a0be67b1f7f9b8f86b
hyuraku <yu@hill-wood.com>
2019-03-02 02:02:25 +0900

    cassandra@2.2 2.2.14
    Closes #37472.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/cassandra@2.2.rb
+++ Formula/cassandra@2.2.rb
@@ -1,9 +1,8 @@
 class CassandraAT22 < Formula
   desc "Eventually consistent, distributed key-value db"
   homepage "https://cassandra.apache.org"
-  url "https://www.apache.org/dyn/closer.cgi?path=/cassandra/2.2.13/apache-cassandra-2.2.13-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/cassandra/2.2.13/apache-cassandra-2.2.13-bin.tar.gz"
-  sha256 "58a97b4830d45e27f4d81aea7a22d2b395ca37125b7c2acd7d5a163825f459a6"
+  url "https://www.apache.org/dyn/closer.cgi?path=/cassandra/2.2.14/apache-cassandra-2.2.14-bin.tar.gz"
+  sha256 "38b5a1785cb2957147cb4d8852b819160485dac91c417dc0c38440a6021e5dac"
 
   bottle do
     cellar :any_skip_relocation
