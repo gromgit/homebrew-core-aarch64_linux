81ce3fd2767a0d77c1d2704ce1c0a0f1b933cd90
Caleb Xu <calebcenter@live.com>
2019-03-01 12:09:00 -0500

    elasticsearch@5.6 5.6.15
    Closes #37473.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/elasticsearch@5.6.rb
+++ Formula/elasticsearch@5.6.rb
@@ -1,8 +1,8 @@
 class ElasticsearchAT56 < Formula
   desc "Distributed search & analytics engine"
   homepage "https://www.elastic.co/products/elasticsearch"
-  url "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.14.tar.gz"
-  sha256 "6c8b46498186bb2f4183660d8653375fc38bddd043302ddb20c516b42ab0125e"
+  url "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.15.tar.gz"
+  sha256 "ac91825bc5f7facaaa4f3358267abca0717e986c7ef3c19c5267214a8c5c5032"
 
   bottle :unneeded
 
