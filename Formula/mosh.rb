7811f041971fbcaec6fa675d41101e6ce6f64ad5
Chongyu Zhu <i@lembacon.com>
2019-03-02 23:50:28 +0800

    mosh: revision for protobuf

--- Formula/mosh.rb
+++ Formula/mosh.rb
@@ -3,7 +3,7 @@ class Mosh < Formula
   homepage "https://mosh.org"
   url "https://mosh.org/mosh-1.3.2.tar.gz"
   sha256 "da600573dfa827d88ce114e0fed30210689381bbdcff543c931e4d6a2e851216"
-  revision 4
+  revision 5
 
   bottle do
     sha256 "5e05a95d972b509c0469ca933de7a522b74b049cc0dccfe5cb1aa6db34b54fc4" => :mojave
