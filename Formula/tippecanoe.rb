3d4bd15f6da24bc1e3d4d4adc295382b6c6e3504
BePsvPT <freedom@csie.io>
2019-03-02 09:00:24 +0800

    tippecanoe 1.33.0
    Closes #37485.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/tippecanoe.rb
+++ Formula/tippecanoe.rb
@@ -1,8 +1,8 @@
 class Tippecanoe < Formula
   desc "Build vector tilesets from collections of GeoJSON features"
   homepage "https://github.com/mapbox/tippecanoe"
-  url "https://github.com/mapbox/tippecanoe/archive/1.32.10.tar.gz"
-  sha256 "adbf6bbe67f57ecb9bee9dc9cb313c0564a8745912f1febd061da3036341edaa"
+  url "https://github.com/mapbox/tippecanoe/archive/1.33.0.tar.gz"
+  sha256 "c0f8885eee6c2357d24916055bac0752b8cccf1baa36c34fff9a7832bcbb2377"
 
   bottle do
     cellar :any_skip_relocation
