5f774af5ce61a85bb5e454e1298cdab838e462ed
hyuraku <yu@hill-wood.com>
2019-03-02 12:39:33 +0900

    solr 7.7.1
    Closes #37492.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/solr.rb
+++ Formula/solr.rb
@@ -1,8 +1,8 @@
 class Solr < Formula
   desc "Enterprise search platform from the Apache Lucene project"
   homepage "https://lucene.apache.org/solr/"
-  url "https://www.apache.org/dyn/closer.cgi?path=lucene/solr/7.7.0/solr-7.7.0.tgz"
-  sha256 "ad9eaa29449701fcabad497cc5f0506f8a8f76da714dc4a5429a549fcac24dc7"
+  url "https://www.apache.org/dyn/closer.cgi?path=lucene/solr/7.7.1/solr-7.7.1.tgz"
+  sha256 "4fb85f12af045b28f6cb935b2f3739d59ec61ad1288ffe44c7ede64e614f28c4"
 
   bottle :unneeded
 
