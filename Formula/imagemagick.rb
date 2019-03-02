c1ad0f21bd9abe9fa8f7932c21cb5a2d8d966072
Chongyu Zhu <i@lembacon.com>
2019-03-02 23:26:22 +0800

    imagemagick 7.0.8-29
    Closes #37511.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -4,9 +4,9 @@ class Imagemagick < Formula
   # Please always keep the Homebrew mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://dl.bintray.com/homebrew/mirror/ImageMagick-7.0.8-28.tar.xz"
-  mirror "https://www.imagemagick.org/download/ImageMagick-7.0.8-28.tar.xz"
-  sha256 "d7aeb906b1c6b8d3292d4a8a94f6ac58683e25d972bbb8ebae8b300d8d707a1b"
+  url "https://dl.bintray.com/homebrew/mirror/ImageMagick-7.0.8-29.tar.xz"
+  mirror "https://www.imagemagick.org/download/ImageMagick-7.0.8-29.tar.xz"
+  sha256 "41590d506e2b4a7df7325934cb906db067d5cbb387c80fb05643186c8b6439e3"
   head "https://github.com/ImageMagick/ImageMagick.git"
 
   bottle do
