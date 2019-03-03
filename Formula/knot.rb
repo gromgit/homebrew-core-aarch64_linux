af0bc8190d89c9795759924969c6338afc98744f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-03 23:08:41 +0000

    knot: update 2.7.6 bottle.

--- Formula/knot.rb
+++ Formula/knot.rb
@@ -5,9 +5,9 @@ class Knot < Formula
   sha256 "a1cb1877f04f7c2549c977c2658cfafd07c7e0e924f8e8aa8d4ae4b707f697a2"
 
   bottle do
-    sha256 "76131c932aef5e03d54f725fbda012f61b51e8401604e5c0819d942e174a3c80" => :mojave
-    sha256 "00228062e04b23590ea26a28bbfc01742971e773b9b8b0bfe7199a1663bff7b4" => :high_sierra
-    sha256 "1ac28e8dd3b0840493b93348949a45da87c61bc4934ff951400d0af33fe42966" => :sierra
+    sha256 "a4aada91becc36fed0c3b9416325b4e31bde2bcd6954fa5ccf284f341ed01ccf" => :mojave
+    sha256 "9e56321341856b71fa43a724274a64b42ef75c434a9a9eec141e065252418489" => :high_sierra
+    sha256 "a647a0de0f7879a0d3d0e7b71d2713b7cfde7df9b038d9f714cb91c46d5fc95d" => :sierra
   end
 
   head do
