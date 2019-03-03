8054553b105df661d369eb99059bffd94b8e137e
Dustin Rodrigues <dust.rod@gmail.com>
2019-03-02 20:35:39 -0500

    nifi 1.9.0

--- Formula/nifi.rb
+++ Formula/nifi.rb
@@ -1,8 +1,8 @@
 class Nifi < Formula
   desc "Easy to use, powerful, and reliable system to process and distribute data"
   homepage "https://nifi.apache.org"
-  url "https://www.apache.org/dyn/closer.cgi?path=/nifi/1.8.0/nifi-1.8.0-bin.tar.gz"
-  sha256 "11561e8d505e76af5060a8b039d7ba1ecfe966722e5b824b4d4eed60ea796071"
+  url "https://www.apache.org/dyn/closer.cgi?path=/nifi/1.9.0/nifi-1.9.0-bin.tar.gz"
+  sha256 "2ea3e0a3a0488d5ac582f350322d0d44b05e5bcfc3cf3a303c85270de90707b1"
 
   bottle :unneeded
 
