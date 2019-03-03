7ce02806e299d7651b3cf5103150f2e324a36fb1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-03 23:26:25 +0000

    serve: add 0.3.0 bottle.

--- Formula/serve.rb
+++ Formula/serve.rb
@@ -4,6 +4,13 @@ class Serve < Formula
   url "https://github.com/syntaqx/serve/archive/v0.3.0.tar.gz"
   sha256 "6bdedf3e3a83ad23cd867c480d32a9f1a91e4e7a289f81579ee4ff308c12d3eb"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9fe1762cda8205639c0a5dc717c940847d2e2cef2aa59ece09294de3d78a1605" => :mojave
+    sha256 "ea14b31d9f2264d6cb52e760713412009a9ee2a90e9c36b460e2658b4e26ac53" => :high_sierra
+    sha256 "f901ff85f818ae1bb852cd0f0a12ca41e2d9c427410f1af35701216f50dcb6fd" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
