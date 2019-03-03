33812c6276c2597f1fee0c9b42320ca50f45129f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-03 16:44:33 +0000

    gloo-ctl: add 0.8.3 bottle.

--- Formula/gloo-ctl.rb
+++ Formula/gloo-ctl.rb
@@ -5,6 +5,13 @@ class GlooCtl < Formula
       :tag      => "v0.8.3",
       :revision => "0e749d0fd9b41f8606dca85b70f0a9981cb0d5ca"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f02d27980329ca2062f7e40d25039ba12ff742df29673cd5c863f0e8412bb7ad" => :mojave
+    sha256 "94e899341c9ff3de2063edf03d9268eb3653f3b6062c3406f07815f3bf354086" => :high_sierra
+    sha256 "b88446a61f585fafe883b516f349fd4c33b5d39acbafbf9f7e11da084b1f5014" => :sierra
+  end
+
   depends_on "dep" => :build
   depends_on "go" => :build
 
