557c689c0518670a0204ce1cf0ee81136e5cfec4
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-03-04 14:49:51 +0000

    at-spi2-atk 2.30.1
    Closes #37585.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/at-spi2-atk.rb
+++ Formula/at-spi2-atk.rb
@@ -1,8 +1,8 @@
 class AtSpi2Atk < Formula
   desc "Accessibility Toolkit GTK+ module"
   homepage "https://wiki.linuxfoundation.org/accessibility/"
-  url "https://download.gnome.org/sources/at-spi2-atk/2.30/at-spi2-atk-2.30.0.tar.xz"
-  sha256 "e2e1571004ea7b105c969473ce455a95be4038fb2541471714aeb33a26da8a9a"
+  url "https://download.gnome.org/sources/at-spi2-atk/2.30/at-spi2-atk-2.30.1.tar.xz"
+  sha256 "8b48ea5902a2b3e333ab21a2602e531e86bffbb8e58708b2a8a8dfa5d325bd6f"
 
   bottle do
     cellar :any
