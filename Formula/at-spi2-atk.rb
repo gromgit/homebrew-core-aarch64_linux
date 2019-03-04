ea0b6fbb69a2e3ba408ead5b3cbf57412fe1fcbf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-04 18:17:17 +0000

    at-spi2-atk: update 2.30.1 bottle.

--- Formula/at-spi2-atk.rb
+++ Formula/at-spi2-atk.rb
@@ -6,10 +6,9 @@ class AtSpi2Atk < Formula
 
   bottle do
     cellar :any
-    sha256 "c2027dd839ea861984aee1587108e89be54661d4ceada586a48b4ca4f6cd0d41" => :mojave
-    sha256 "93b22bd8e17c750e71f8afbe96d2ed1011884f3a9461eeeeff7689b7473675ad" => :high_sierra
-    sha256 "a017537217c5c1533adc6a9c2ed209349186380ccda808375d10a3e8d7b393ed" => :sierra
-    sha256 "ac94ce79b4199253f019f7fd270e60931e2fbc301b86ec7e45aca16f58d0fc58" => :el_capitan
+    sha256 "6622e03beadde94a89f5a7916480bba649e9b3cf6379dd6dd8399c08a27aeaf8" => :mojave
+    sha256 "a6eeeb0fbd0094b0c9f85bf15f3cddaeb7de826f27630ba31a4dd8573f364464" => :high_sierra
+    sha256 "c69b12100018e7eef4133520e58bbf8dbc0c4e709ab509a3dcd70834910e0913" => :sierra
   end
 
   depends_on "meson-internal" => :build
