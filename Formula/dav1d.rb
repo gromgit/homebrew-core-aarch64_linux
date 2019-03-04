644f85dd8d90efc2cb14434d4b9403e4bb77fe35
David Michael Barr <b@rr-dav.id.au>
2019-03-05 02:33:39 +0900

    dav1d 0.2.0
    Closes #37588.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/dav1d.rb
+++ Formula/dav1d.rb
@@ -1,8 +1,8 @@
 class Dav1d < Formula
   desc "AV1 decoder targeted to be small and fast"
   homepage "https://code.videolan.org/videolan/dav1d"
-  url "https://code.videolan.org/videolan/dav1d/-/archive/0.1.0/dav1d-0.1.0.tar.bz2"
-  sha256 "923f78d17aed08252608790eaed06a3d530763168fd6a8fc74e3306d7e01e6a5"
+  url "https://code.videolan.org/videolan/dav1d/-/archive/0.2.0/dav1d-0.2.0.tar.bz2"
+  sha256 "b7d0db946e112f46140043360a0d7dacd5b3087cc888fffcb4d2eb90e85a0d60"
 
   bottle do
     cellar :any
@@ -16,7 +16,7 @@ class Dav1d < Formula
   depends_on "ninja" => :build
 
   resource "00000000.ivf" do
-    url "https://people.xiph.org/~tterribe/av1/samples-all/00000000.ivf"
+    url "https://code.videolan.org/videolan/dav1d-test-data/raw/master/8-bit/data/00000000.ivf"
     sha256 "52b4351f9bc8a876c8f3c9afc403d9e90f319c1882bfe44667d41c8c6f5486f3"
   end
 
