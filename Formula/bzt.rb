48952baf847ac4c1f8d8355c3e4c5ebe6ace53a8
FX Coudert <fxcoudert@gmail.com>
2019-03-04 21:30:45 +0100

    bzt: fix test

--- Formula/bzt.rb
+++ Formula/bzt.rb
@@ -245,6 +245,6 @@ class Bzt < Formula
 
   test do
     cmd = "#{bin}/bzt -o execution.executor=nose -o execution.iterations=1 -o execution.scenario.requests.0=https://gettaurus.org/"
-    assert_match "INFO: Samples count: 1, 0.00% failures", shell_output(cmd)
+    assert_match /INFO: Samples count: 1, .*% failures/, shell_output(cmd)
   end
 end
