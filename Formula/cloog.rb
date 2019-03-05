527d7873b1da39903413984cace756f8245f54d9
Nathaniel Suchy <me@lunorian.is>
2019-03-05 06:40:52 -0500

    Switch to Debian.org from a Hard Coded mirror (#37596)

--- Formula/cloog.rb
+++ Formula/cloog.rb
@@ -18,7 +18,7 @@ class Cloog < Formula
 
   resource "isl" do
     url "http://isl.gforge.inria.fr/isl-0.18.tar.xz"
-    mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/i/isl/isl_0.18.orig.tar.xz"
+    mirror "https://deb.debian.org/debian/pool/main/i/isl/isl_0.18.orig.tar.xz"
     sha256 "0f35051cc030b87c673ac1f187de40e386a1482a0cfdf2c552dd6031b307ddc4"
   end
 
