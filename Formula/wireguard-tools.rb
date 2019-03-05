699d22a862987e3267edf492b6646513ed30ef04
csardothien <45049244+csardothien@users.noreply.github.com>
2019-03-05 10:19:14 -0500

    wireguard-tools 0.0.20190227
    Closes #37619.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/wireguard-tools.rb
+++ Formula/wireguard-tools.rb
@@ -3,8 +3,8 @@ class WireguardTools < Formula
   homepage "https://www.wireguard.com/"
   # Please only update version when the tools have been modified/updated,
   # since the Linux module aspect isn't of utility for us.
-  url "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-0.0.20190123.tar.xz"
-  sha256 "edd13c7631af169e3838621b1a1bff3ef73cf7bc778eec2bd55f7c1089ffdf9b"
+  url "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-0.0.20190227.tar.xz"
+  sha256 "fcdb26fd2692d9e1dee54d14418603c38fbb973a06ce89d08fbe45292ff37f79"
   head "https://git.zx2c4.com/WireGuard", :using => :git
 
   bottle do
