075ef9a2ca6d94e7c42e2356e7885e9def043927
Chongyu Zhu <i@lembacon.com>
2019-03-07 17:23:56 +0800

    dtc 1.5.0
    Closes #37682.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>
    
    Co-authored-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/dtc.rb
+++ Formula/dtc.rb
@@ -1,8 +1,8 @@
 class Dtc < Formula
   desc "Device tree compiler"
   homepage "https://www.devicetree.org/"
-  url "https://www.kernel.org/pub/software/utils/dtc/dtc-1.4.7.tar.xz"
-  sha256 "6643e8f00ff86350f465bb54b2185058b5b1b7bac01a0842c81a52b86589cde7"
+  url "https://www.kernel.org/pub/software/utils/dtc/dtc-1.5.0.tar.xz"
+  sha256 "c672e443c9f7e39f5a7c8e602da6777f9ad55ad70de87de300a43828c8050172"
 
   bottle do
     cellar :any
@@ -11,6 +11,8 @@ class Dtc < Formula
     sha256 "ea5ae5a503636ed53d3dbc87e835c9fa2f3e004b06d871003a9a4617afc87163" => :sierra
     sha256 "6755bc2af0a42c3bb4834c468f36db8248b983d20cb47783afded95ac1267aac" => :el_capitan
   end
+
+  depends_on "pkg-config" => :build
 
   def install
     system "make", "NO_PYTHON=1"
