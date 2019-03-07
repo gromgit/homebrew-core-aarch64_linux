4e31bdb1c66ba6c53753c24b80ba3221c7658420
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-07 15:58:20 +0000

    dtc: update 1.5.0 bottle.

--- Formula/dtc.rb
+++ Formula/dtc.rb
@@ -6,10 +6,9 @@ class Dtc < Formula
 
   bottle do
     cellar :any
-    sha256 "5468b1d1b4467b1e5fcc58013c76b938fdb33db45f10d0683f6eb7a72d5c007a" => :mojave
-    sha256 "c230e06edc7710720e75e77319d3982f2d8e1e873018df6fdfa25268ae3ea2e1" => :high_sierra
-    sha256 "ea5ae5a503636ed53d3dbc87e835c9fa2f3e004b06d871003a9a4617afc87163" => :sierra
-    sha256 "6755bc2af0a42c3bb4834c468f36db8248b983d20cb47783afded95ac1267aac" => :el_capitan
+    sha256 "a153daba5d7e2d361cc072a310379a939c8a83986584a1899edba80371e04b56" => :mojave
+    sha256 "d64946a773558b22387e1e251185180d386b592828d87c166fa43dbd35cdbf1f" => :high_sierra
+    sha256 "21635cfd955fbcd9de3a48a3e8203725c1a194e48d11eade6c95d8f481fc8e7f" => :sierra
   end
 
   depends_on "pkg-config" => :build
