f2387195c246db8c910f143d3cc8112ede9271c6
Igor Kapkov <igasgeek@me.com>
2019-03-07 12:50:22 +1100

    micronaut 1.0.4
    Closes #37670.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/micronaut.rb
+++ Formula/micronaut.rb
@@ -1,8 +1,8 @@
 class Micronaut < Formula
   desc "Modern JVM-based framework for building modular microservices"
   homepage "https://micronaut.io/"
-  url "https://github.com/micronaut-projects/micronaut-core/releases/download/v1.0.2/micronaut-1.0.2.zip"
-  sha256 "164eca3ffde6d4e3019dee28528e916a0ca13ffb3d49e00dbdefe003a10c78db"
+  url "https://github.com/micronaut-projects/micronaut-core/releases/download/v1.0.4/micronaut-1.0.4.zip"
+  sha256 "d537a774ba1e073dbeca14611a0fc9fc296e1231e128d9f62577537f46373151"
 
   bottle :unneeded
 
