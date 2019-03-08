73e3325fa1f83dc9e9464f357bee73c57570baa5
Xavier Lacoste <xavier.lacoste@total.com>
2019-03-08 14:25:20 +0100

    cubelib 4.4.3 (new formula)
    new formula: cubelib 4.4.2
    
    Closes #37720.
    
    Signed-off-by: Izaak Beekman <zbeekman@gmail.com>

---
+++ Formula/cubelib.rb
@@ -0,0 +1,28 @@
+class Cubelib < Formula
+  desc "Cube, is a performance report explorer for Scalasca and Score-P"
+  homepage "https://scalasca.org/software/cube-4.x/download.html"
+  url "https://apps.fz-juelich.de/scalasca/releases/cube/4.4/dist/cubelib-4.4.3.tar.gz"
+  sha256 "bcd4fa81a5ba37194e590a5d7c3e6c44b448f5e156a175837b77c21206847a8d"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--with-nocross-compiler-suite=clang",
+                          "CXXFLAGS=-stdlib=libc++",
+                          "LDFLAGS=-stdlib=libc++",
+                          "--prefix=#{prefix}"
+    system "make"
+    ENV.deparallelize
+    system "make", "install"
+  end
+
+  test do
+    cp_r "#{share}/doc/cubelib/example/", testpath
+    chdir "#{testpath}/example" do
+      # build and run tests
+      system "make", "-f", "Makefile.frontend", "all"
+      system "make", "-f", "Makefile.frontend", "run"
+    end
+  end
+end
