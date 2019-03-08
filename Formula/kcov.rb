a493ca16e68e39e792cba4354ea17ff81eabea55
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-08 17:49:22 +0000

    kcov: add 36 bottle.

--- Formula/kcov.rb
+++ Formula/kcov.rb
@@ -5,6 +5,13 @@ class Kcov < Formula
   sha256 "29ccdde3bd44f14e0d7c88d709e1e5ff9b448e735538ae45ee08b73c19a2ea0b"
   head "https://github.com/SimonKagstrom/kcov.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "eb5bc53867f7dc0ebb53216daf6b1ec62ee45036fa36d60ccfa03722644176ed" => :mojave
+    sha256 "e5c56807425593829fa59be83f58ba473cdde545216c06fdda54e7e59996f413" => :high_sierra
+    sha256 "75e27a93b88782772c9d54db0afcb079f5f1b3d89a6ab1e457306e269c2951a3" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
 
