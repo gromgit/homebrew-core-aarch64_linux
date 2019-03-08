d67367932e41b08712949e8e38d694d8481559d2
Viktor Szakats <commit@vszakats.net>
2019-03-08 14:16:50 +0000

    babl: secure url(s)

--- Formula/babl.rb
+++ Formula/babl.rb
@@ -1,6 +1,6 @@
 class Babl < Formula
   desc "Dynamic, any-to-any, pixel format translation library"
-  homepage "http://www.gegl.org/babl/"
+  homepage "https://www.gegl.org/babl/"
   url "https://download.gimp.org/pub/babl/0.1/babl-0.1.62.tar.bz2"
   sha256 "dc279f174edbcb08821cf37e4ab0bc02e6949369b00b150c759a6c24bfd3f510"
 
