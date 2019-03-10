12e22ca40687b1c981f4a5a385747146f831be4e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-10 10:56:19 +0000

    git-flow-avh: update 1.12.2 bottle.

--- Formula/git-flow-avh.rb
+++ Formula/git-flow-avh.rb
@@ -14,9 +14,9 @@ class GitFlowAvh < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "40d9fbe7b2edc783565efe4d7fa5842fb8e855dbb9d6af8871cec797ff84fd14" => :mojave
-    sha256 "d1f0b434f4931a703155c274daad453f3c6f75122f42090d0c6231ef62365dc9" => :high_sierra
-    sha256 "d1f0b434f4931a703155c274daad453f3c6f75122f42090d0c6231ef62365dc9" => :sierra
+    sha256 "9e602aae685b1d0fa3db2a5d4a76b6bb73fa612dc69fe86e6571076e11d11290" => :mojave
+    sha256 "9e602aae685b1d0fa3db2a5d4a76b6bb73fa612dc69fe86e6571076e11d11290" => :high_sierra
+    sha256 "dfe4055f4e0aa48af4239d45645c920c700005556b8e1a0917d441c55e2b0a53" => :sierra
   end
 
   head do
