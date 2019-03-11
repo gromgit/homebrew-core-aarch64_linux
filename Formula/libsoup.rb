35e40199a5b20362d9c67a84276f670b6cbb55aa
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-03-11 16:47:24 +0000

    libsoup: improve test

--- Formula/libsoup.rb
+++ Formula/libsoup.rb
@@ -39,11 +39,17 @@ class Libsoup < Formula
   end
 
   test do
+    # if this test start failing, the problem might very well be in glib-networking instead of libsoup
     (testpath/"test.c").write <<~EOS
       #include <libsoup/soup.h>
 
       int main(int argc, char *argv[]) {
-        guint version = soup_get_major_version();
+        SoupMessage *msg = soup_message_new("GET", "https://brew.sh");
+        SoupSession *session = soup_session_new();
+        soup_session_send_message(session, msg); // blocks
+        g_assert_true(SOUP_STATUS_IS_SUCCESSFUL(msg->status_code));
+        g_object_unref(msg);
+        g_object_unref(session);
         return 0;
       }
     EOS
