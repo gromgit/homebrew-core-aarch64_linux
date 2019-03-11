b077f165a89404fc4a6df76eca92884aeb0b22d7
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-03-11 13:15:47 +0000

    gtranslator 3.32.0
    Closes #37783.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/gtranslator.rb
+++ Formula/gtranslator.rb
@@ -1,8 +1,8 @@
 class Gtranslator < Formula
   desc "GNOME gettext PO file editor"
   homepage "https://wiki.gnome.org/Design/Apps/Translator"
-  url "https://download.gnome.org/sources/gtranslator/3.30/gtranslator-3.30.1.tar.xz"
-  sha256 "c77afffa588c453fcf2241acc6d68a636975b69cc4aab8057a3382cb218cf42c"
+  url "https://download.gnome.org/sources/gtranslator/3.32/gtranslator-3.32.0.tar.xz"
+  sha256 "aa8b6ce7a6ea199a50ee8f65258e640af80407a7433359b853039a7ea07a11b7"
 
   bottle do
     sha256 "9abc09ece4d2f3cecb23ae250cd629944635cf99a383a9c6fa2d586aa752397d" => :mojave
@@ -28,8 +28,8 @@ class Gtranslator < Formula
     ENV["DESTDIR"] = "/"
     mkdir "build" do
       system "meson", "--prefix=#{prefix}", ".."
-      system "ninja"
-      system "ninja", "install"
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
     end
   end
 
