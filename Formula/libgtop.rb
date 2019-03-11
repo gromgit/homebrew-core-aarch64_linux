e0d3657217ff1c5eaffb82f1c31886e9f64268c6
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-03-11 10:41:09 +0000

    libgtop 2.40.0
    Closes #37780.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/libgtop.rb
+++ Formula/libgtop.rb
@@ -1,9 +1,8 @@
 class Libgtop < Formula
   desc "Library for portably obtaining information about processes"
   homepage "https://library.gnome.org/devel/libgtop/stable/"
-  url "https://download.gnome.org/sources/libgtop/2.38/libgtop-2.38.0.tar.xz"
-  sha256 "4f6c0e62bb438abfd16b4559cd2eca0251de19e291c888cdc4dc88e5ffebb612"
-  revision 1
+  url "https://download.gnome.org/sources/libgtop/2.40/libgtop-2.40.0.tar.xz"
+  sha256 "78f3274c0c79c434c03655c1b35edf7b95ec0421430897fb1345a98a265ed2d4"
 
   bottle do
     sha256 "3a648a7dc340187b64384a2fb04738cd9da41773f6f8237e242d3354adcf5eca" => :mojave
@@ -12,31 +11,16 @@ class Libgtop < Formula
     sha256 "fe1a65675f609a65a3aa8938080472df2f84159f163de616a50683de3ab6a3f3" => :el_capitan
   end
 
-  # Some build deps were added for the patch below,
-  # and can be removed on the next release:
-  # autoconf, automake, gnome-common, gtk-doc, libtool
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "gnome-common" => :build
   depends_on "gobject-introspection" => :build
-  depends_on "gtk-doc" => :build
   depends_on "intltool" => :build
-  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "glib"
 
-  # Fixes the build on OS X by providing a stub implementation of a new feature
-  # https://gitlab.gnome.org/GNOME/libgtop/issues/36
-  patch do
-    url "https://github.com/GNOME/libgtop/commit/42b049f338363f92c1e93b4549fc944098eae674.diff?full_index=1"
-    sha256 "1a208c5a246f51b16413ef5ca370afc6aba3ceebae0c616da1dcf1d8b9c0e2f1"
-  end
-
   def install
-    system "./autogen.sh", "--disable-debug", "--disable-dependency-tracking",
-                           "--prefix=#{prefix}",
-                           "--without-x"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--without-x"
     system "make", "install"
   end
 
