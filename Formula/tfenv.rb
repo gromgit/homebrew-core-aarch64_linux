45f5cbcfa0d03761c5ee75d381f9ef5d98d0ba22
Isabell (Issy) Long <me@issyl0.co.uk>
2019-03-11 00:37:23 +0000

    tfenv: update homepage and url
    Closes #37799.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/tfenv.rb
+++ Formula/tfenv.rb
@@ -1,9 +1,9 @@
 class Tfenv < Formula
   desc "Terraform version manager inspired by rbenv"
-  homepage "https://github.com/kamatama41/tfenv"
-  url "https://github.com/kamatama41/tfenv/archive/v0.6.0.tar.gz"
+  homepage "https://github.com/tfutils/tfenv"
+  url "https://github.com/tfutils/tfenv/archive/v0.6.0.tar.gz"
   sha256 "f2ca32ade0e90d6988106e85efaa548ed116cac1ded24f88dd19f479c6381e2e"
-  head "https://github.com/kamatama41/tfenv.git"
+  head "https://github.com/tfutils/tfenv.git"
 
   bottle :unneeded
 
