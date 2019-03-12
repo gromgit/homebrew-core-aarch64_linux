c4d3762205ce9c6939d3b1efd4d45acc072b9c79
404NetworkError <404NetworkError@users.noreply.github.com>
2019-03-12 14:29:57 -0400

    speedtest-cli 2.1.1
    Closes #37822.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/speedtest-cli.rb
+++ Formula/speedtest-cli.rb
@@ -1,8 +1,8 @@
 class SpeedtestCli < Formula
   desc "Command-line interface for https://speedtest.net bandwidth tests"
   homepage "https://github.com/sivel/speedtest-cli"
-  url "https://github.com/sivel/speedtest-cli/archive/v2.1.0.tar.gz"
-  sha256 "5586dfd4aee546e244d54fe17df4c0c93e943fd33dde987b3813e97759c41971"
+  url "https://github.com/sivel/speedtest-cli/archive/v2.1.1.tar.gz"
+  sha256 "433675c19bd40c059da5d98c512874742405e8185afed47f8aa11a02ac61aa06"
   head "https://github.com/sivel/speedtest-cli.git"
 
   bottle :unneeded
