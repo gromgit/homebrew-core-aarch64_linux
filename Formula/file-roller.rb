8b8a11f8ece52333e14b06d4079f783102593d54
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-12 07:32:37 +0000

    file-roller: update 3.32.0 bottle.

--- Formula/file-roller.rb
+++ Formula/file-roller.rb
@@ -5,9 +5,9 @@ class FileRoller < Formula
   sha256 "03283027f1b9747c5fc5747eb672e0c155023c7fd74bec7b8faeb7b7b071a7c5"
 
   bottle do
-    sha256 "b8fbd6bf80150c9cd31fd6453f915041628c281c16833e8b43c07764bec191cc" => :mojave
-    sha256 "cb871a853668327b61f38cc1d9358f24b6085349ea42248b3b184ee9bfff9a8e" => :high_sierra
-    sha256 "0533e7a196c6c364894c5510cab68f598b092ec8891018a774f8b6c4e6b2f73e" => :sierra
+    sha256 "6f2475abf43f50f6f7eb5706ba15477dc9b335f319a66694d026ae5f7bc84cd7" => :mojave
+    sha256 "710ca33e691bfb0baf8cb7c8f1e80c34ff878e4e2a2351770add0775efb32a35" => :high_sierra
+    sha256 "32e05b664c7348b1dbf2ea95a7ac61e1fc148fd91aaef463ec83ee30b72e3cd1" => :sierra
   end
 
   depends_on "itstool" => :build
