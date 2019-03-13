2cbd30abda3d26e6f3b3ad9109130c47942090d0
Igor Kapkov <igasgeek@me.com>
2019-03-13 17:25:47 +1100

    shmux 1.0.3
    Closes #37859.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/shmux.rb
+++ Formula/shmux.rb
@@ -1,8 +1,8 @@
 class Shmux < Formula
   desc "Execute the same command on many hosts in parallel"
   homepage "https://github.com/shmux/shmux"
-  url "https://github.com/shmux/shmux/archive/v1.0.2.tar.gz"
-  sha256 "4b84dc3e0d72d054ed4730d130a509f43441fb61561c11a444d6ee65cbff9c04"
+  url "https://github.com/shmux/shmux/archive/v1.0.3.tar.gz"
+  sha256 "c9f8863e2550e23e633cf5fc7a9c4c52d287059f424ef78aba6ecd98390fb9ab"
 
   bottle do
     cellar :any_skip_relocation
