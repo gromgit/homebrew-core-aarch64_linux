1ac5a30d69d7f5d699b14e3bac48245ff3d955df
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-13 04:20:57 +0000

    treefrog: update 1.23.0 bottle.

--- Formula/treefrog.rb
+++ Formula/treefrog.rb
@@ -6,10 +6,9 @@ class Treefrog < Formula
   head "https://github.com/treefrogframework/treefrog-framework.git"
 
   bottle do
-    rebuild 1
-    sha256 "48c4597ce3646a2481946824dcb6aee5b1bcec9f7449c1054c265b70f66800d9" => :mojave
-    sha256 "5c271befbf580381e9676937716a4c215b09b83ea75416cf6229d495956ce5d3" => :high_sierra
-    sha256 "04a85f1d383d87e7d37be11b91360341c0686c758a4eda6b0185121f88824684" => :sierra
+    sha256 "bc4b2cde6f08f0c88352cfa798efbdbf8f7ed17c974f59cce4c6a99472723f61" => :mojave
+    sha256 "bc4b2cde6f08f0c88352cfa798efbdbf8f7ed17c974f59cce4c6a99472723f61" => :high_sierra
+    sha256 "9f32c83a28d3c73dbc3424dfab5d5643174adca1838752cca607f27a85c4f72e" => :sierra
   end
 
   depends_on :xcode => ["8.0", :build]
