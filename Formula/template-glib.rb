f04989ae412e44541fd60e10c1306092c1bf3a11
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-03-13 16:12:47 +0000

    template-glib 3.32.0

--- Formula/template-glib.rb
+++ Formula/template-glib.rb
@@ -1,8 +1,8 @@
 class TemplateGlib < Formula
   desc "GNOME templating library for GLib"
   homepage "https://gitlab.gnome.org/GNOME/template-glib"
-  url "https://download.gnome.org/sources/template-glib/3.30/template-glib-3.30.0.tar.xz"
-  sha256 "cf690d391bfc71036e31867df6da90315a3db83f739a8657f580b941b96e3649"
+  url "https://download.gnome.org/sources/template-glib/3.32/template-glib-3.32.0.tar.xz"
+  sha256 "39a334f5db404fa8b225224766684f2f63f5ec4cf4e971cfc513f1db35e81fbc"
 
   bottle do
     sha256 "bc65703062e59b6f45e161ec7e3c6c050ed6961e046498880cf2de28bf2aa4bb" => :mojave
@@ -12,19 +12,26 @@ class TemplateGlib < Formula
   end
 
   depends_on "bison" => :build # does not appear to work with system bison
-  depends_on "meson-internal" => :build
+  depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "python" => :build
   depends_on "glib"
   depends_on "gobject-introspection"
 
+  # submitted upstream at https://gitlab.gnome.org/GNOME/template-glib/merge_requests/5
+  patch :DATA
+
   def install
     mkdir "build" do
       system "meson", "--prefix=#{prefix}", "-Dwith_vapi=false", ".."
-      system "ninja"
-      system "ninja", "install"
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
     end
+
+    # to be removed when https://gitlab.gnome.org/GNOME/gobject-introspection/issues/222 is fixed
+    inreplace share/"gir-1.0/Template-1.0.gir", "@rpath", lib.to_s
+    system "g-ir-compiler", "--output=#{lib}/girepository-1.0/Template-1.0.typelib", share/"gir-1.0/Template-1.0.gir"
   end
 
   test do
@@ -63,3 +70,38 @@ class TemplateGlib < Formula
     system "./test"
   end
 end
+
+__END__
+diff --git a/meson.build b/meson.build
+index 050c202..d705657 100644
+--- a/meson.build
++++ b/meson.build
+@@ -26,6 +26,8 @@ current = template_glib_version_minor * 100 + template_glib_version_micro - temp
+ revision = template_glib_interface_age
+ libversion = '@0@.@1@.@2@'.format(soversion, current, revision)
+
++darwin_versions = [current + 1, '@0@.@1@'.format(current + 1, revision)]
++
+ config_h = configuration_data()
+ config_h.set_quoted('GETTEXT_PACKAGE', 'libtemplate_glib')
+ config_h.set_quoted('LOCALEDIR', join_paths(get_option('prefix'), get_option('localedir')))
+diff --git a/src/meson.build b/src/meson.build
+index 5adef72..b3eb57a 100644
+--- a/src/meson.build
++++ b/src/meson.build
+@@ -145,10 +145,11 @@ libtemplate_glib = library(
+   'template_glib-' + apiversion,
+   libtemplate_glib_sources,
+
+-  dependencies: libtemplate_glib_deps,
+-     soversion: soversion,
+-       version: libversion,
+-       install: true,
++   dependencies: libtemplate_glib_deps,
++      soversion: soversion,
++        version: libversion,
++darwin_versions: darwin_versions,
++        install: true,
+ )
+
+ libtemplate_glib_dep = declare_dependency(
