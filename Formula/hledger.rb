0d922cf09ce4344db4996c32060233cc8b58d424
Albin Stjerna <albin.stjerna@gmail.com>
2019-03-14 19:42:40 +0100

    hledger 1.14.1
    Closes #37915.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/hledger.rb
+++ Formula/hledger.rb
@@ -5,8 +5,8 @@ class Hledger < Formula
 
   desc "Command-line accounting tool"
   homepage "https://hledger.org/"
-  url "https://hackage.haskell.org/package/hledger-1.13.2/hledger-1.13.2.tar.gz"
-  sha256 "9b57e78c9c51f06c0fbc8dd499baad8fe2f323635c95159f55b041ebe12fbc37"
+  url "https://hackage.haskell.org/package/hledger-1.14.1/hledger-1.14.1.tar.gz"
+  sha256 "5e5df9de8efa4133a543c4f47aeff2db1efb16ac77713572cac2da3b8fc2318d"
 
   bottle do
     cellar :any_skip_relocation
@@ -18,18 +18,18 @@ class Hledger < Formula
   depends_on "ghc" => :build
 
   resource "hledger_web" do
-    url "https://hackage.haskell.org/package/hledger-web-1.13/hledger-web-1.13.tar.gz"
-    sha256 "d7621eccc151841458b7a2b6b1ce86585d98d88d4aa50e4dca22e9be2985ab10"
+    url "https://hackage.haskell.org/package/hledger-web-1.14/hledger-web-1.14.tar.gz"
+    sha256 "eb10512a8dd6af6b046d65f2d1dfcdd37eb22e1040108e29212ab0ca0abc5295"
   end
 
   resource "hledger_ui" do
-    url "https://hackage.haskell.org/package/hledger-ui-1.13.1/hledger-ui-1.13.1.tar.gz"
-    sha256 "d7f773cf626e0d67df505e2f1f10111e49097690324eb4d4272223c4ec7e4e49"
+    url "https://hackage.haskell.org/package/hledger-ui-1.14/hledger-ui-1.14.tar.gz"
+    sha256 "531ab28284370cc67316d409d41aef706eeb3d53a1d2c032f9147b5f095bdfde"
   end
 
   resource "hledger_api" do
-    url "https://hackage.haskell.org/package/hledger-api-1.13/hledger-api-1.13.tar.gz"
-    sha256 "e3f2387fe4be671fcbef10184e4b0a4d895d29a40470aaedb36baad731a871de"
+    url "https://hackage.haskell.org/package/hledger-api-1.14/hledger-api-1.14.tar.gz"
+    sha256 "ad7a714201cf912a6c756e40a25116e2352b86a81b048599c15f403b2a65f7a3"
   end
 
   def install
