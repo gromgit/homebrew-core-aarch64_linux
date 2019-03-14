c12fa6bee1e2510f6e502f4f2f03c07b3b9e389a
Till Toenshoff <toenshoff@me.com>
2019-03-14 23:34:14 +0100

    rtags 2.31
    Closes #37923.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/rtags.rb
+++ Formula/rtags.rb
@@ -2,8 +2,8 @@ class Rtags < Formula
   desc "Source code cross-referencer like ctags with a clang frontend"
   homepage "https://github.com/Andersbakken/rtags"
   url "https://github.com/Andersbakken/rtags.git",
-      :tag      => "v2.22",
-      :revision => "c4dea89941554688d4b02509c5cb0c5eeca47981"
+      :tag      => "v2.31",
+      :revision => "fe31db984968d323653da238b0c403951cd1d704"
   head "https://github.com/Andersbakken/rtags.git"
 
   bottle do
