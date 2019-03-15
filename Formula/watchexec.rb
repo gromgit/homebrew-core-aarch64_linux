881c8f4695742519bdac31138d5136f2d5061d62
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-15 23:24:34 +0000

    watchexec: update 1.10.1 bottle.

--- Formula/watchexec.rb
+++ Formula/watchexec.rb
@@ -5,10 +5,10 @@ class Watchexec < Formula
   sha256 "48de75d1a85e3675078e144ea34f4edcd3707751e67370f3f6386e79ca9ba4fa"
 
   bottle do
-    sha256 "48cda48ee0e5b26728764c35f1a21bb8e6cbef5aa34067deae20a48f0a8a5a68" => :mojave
-    sha256 "db7cdbf5129c247378278b41f231bc88f44224134b28a0049e91eea6d5f3ee31" => :high_sierra
-    sha256 "c653234a0a182b6d973211c728687485977de5740f6f6bddd474bf7429b220b7" => :sierra
-    sha256 "5c8b4d95cdabf5136cc74e564e10b0bea7975e36b197126a73ed88c46006871b" => :el_capitan
+    cellar :any_skip_relocation
+    sha256 "b8f31ade640fe036bab0b7d246195b85f77d6154f5af15cdd34481e17c635ced" => :mojave
+    sha256 "25793f00659f605cde2c852df2a64390fa77967329bb5a9e93c24ff8c3c2d32f" => :high_sierra
+    sha256 "78a9129bea6954c9d0b0076b061bae7999e71aec29aea53712220264ccba2ac4" => :sierra
   end
 
   depends_on "rust" => :build
