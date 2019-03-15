f61a061ad8ef9f09cd6e2fab8c75514a0f091dfb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-15 14:09:43 +0000

    r: update 3.5.3 bottle.

--- Formula/r.rb
+++ Formula/r.rb
@@ -5,9 +5,9 @@ class R < Formula
   sha256 "2bfa37b7bd709f003d6b8a172ddfb6d03ddd2d672d6096439523039f7a8e678c"
 
   bottle do
-    sha256 "a1d8588a967294aa86a5aa233e51360d47643722e802d287cfd9c15bea49bd04" => :mojave
-    sha256 "63aee750beb4a60a4508ee25cf0913151e2df8aeb81dc2fcf75e0ae569010ba6" => :high_sierra
-    sha256 "ea8f566f2f19e0aa0d962eea36f38eca1bc37ad281d5c79feb2aab18a47491a0" => :sierra
+    sha256 "8d1c5161ae03b34d8ed55bd7aa60c708b196b39929cd867bf80973640915f86e" => :mojave
+    sha256 "e1d29ef3229094d533527fa8caaf1d747548b06ef03260dbda3b490f8ee85904" => :high_sierra
+    sha256 "e3da4551f917e2846fb7c887c1bffdd923fcd8abab3f035d69c592ef74d9a18a" => :sierra
   end
 
   depends_on "pkg-config" => :build
