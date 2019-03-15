644e21551f8ff97ad068f2a1e3dbe06ab810a240
Toru Nayuki <tnayuki@icloud.com>
2019-03-16 00:07:06 +0900

    serverless 1.39.0
    Closes #37942.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/serverless.rb
+++ Formula/serverless.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Serverless < Formula
   desc "Build applications with serverless architectures"
   homepage "https://serverless.com"
-  url "https://registry.npmjs.org/serverless/-/serverless-1.38.0.tgz"
-  sha256 "bb17098cc2d1e58965908eeff2c3c914260fcdd2ee3cb80ec3863b4ace29825f"
+  url "https://registry.npmjs.org/serverless/-/serverless-1.39.0.tgz"
+  sha256 "cb404813af4acb6531861efc46de2b10a661627bab35da78522e704410d2db5e"
 
   bottle do
     cellar :any_skip_relocation
