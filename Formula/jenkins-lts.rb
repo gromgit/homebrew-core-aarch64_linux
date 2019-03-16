44b75ef03ece67c13c27bf3d228adef46031f8ca
Kent Sutherland <git@ksuther.com>
2019-03-16 09:42:24 -0500

    jenkins-lts 2.164.1
    Closes #37972.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/jenkins-lts.rb
+++ Formula/jenkins-lts.rb
@@ -1,8 +1,8 @@
 class JenkinsLts < Formula
   desc "Extendable open-source CI server"
   homepage "https://jenkins.io/index.html#stable"
-  url "http://mirrors.jenkins.io/war-stable/2.150.3/jenkins.war"
-  sha256 "4fc2700a27a6ccc53da9d45cc8b2abd41951b361e562e1a1ead851bea61630fd"
+  url "http://mirrors.jenkins.io/war-stable/2.164.1/jenkins.war"
+  sha256 "65543f5632ee54344f3351b34b305702df12393b3196a95c3771ddb3819b220b"
 
   bottle :unneeded
 
