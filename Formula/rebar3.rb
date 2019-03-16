a1d0d1b71c723339abe351d9845127091a62eba7
Igor Kapkov <igasgeek@me.com>
2019-03-16 19:37:32 +1100

    rebar3 3.9.1
    Closes #37966.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/rebar3.rb
+++ Formula/rebar3.rb
@@ -1,8 +1,8 @@
 class Rebar3 < Formula
   desc "Erlang build tool"
   homepage "https://github.com/erlang/rebar3"
-  url "https://github.com/erlang/rebar3/archive/3.9.0.tar.gz"
-  sha256 "9ea73ce4e60ad4b3108641eae73b4098fadb510142e672ad8e3a793f57e9f992"
+  url "https://github.com/erlang/rebar3/archive/3.9.1.tar.gz"
+  sha256 "b7330a67a8cb5d6fb3b53a3246208b7c2b248546bcf62ef71a9a27b1d541c2d8"
 
   bottle do
     cellar :any_skip_relocation
