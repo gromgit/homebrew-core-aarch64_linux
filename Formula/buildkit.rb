eb802c37b554cfa7d177075df9b8d4efbb4ec020
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-18 21:51:15 +0000

    buildkit: add 0.4.0 bottle.

--- Formula/buildkit.rb
+++ Formula/buildkit.rb
@@ -4,6 +4,13 @@ class Buildkit < Formula
   url "https://github.com/moby/buildkit/archive/v0.4.0.tar.gz"
   sha256 "8847b4ece48a40dd17c23f288152bbde90c623a7c8162e40ebe7f3e25dffb54e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f1d4b627eb74827bd3730c690dc1efb0a29a28c4ec7eaabe8c2b7dfc2a4261ff" => :mojave
+    sha256 "b7c7421f5e52c3d1f73eb8c659bb2da0dcc2ce75b44a13a6922dbe30b0c369a8" => :high_sierra
+    sha256 "4eca0585defe3f79b0f436e54056e36b0c130c917727cac6be7eae6a86ad33e0" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
