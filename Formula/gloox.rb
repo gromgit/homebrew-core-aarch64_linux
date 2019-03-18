a50b9e141349abb64808661c1374016afb721f4c
Caleb Xu <calebcenter@live.com>
2019-03-18 10:39:41 -0400

    gloox 1.0.22
    Closes #38016.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/gloox.rb
+++ Formula/gloox.rb
@@ -1,9 +1,8 @@
 class Gloox < Formula
   desc "C++ Jabber/XMPP library that handles the low-level protocol"
   homepage "https://camaya.net/gloox/"
-  url "https://camaya.net/download/gloox-1.0.21.tar.bz2"
-  sha256 "3c13155c10e3182a1a57779134cc524efb3657545849534b2831fae0e2c3a7cc"
-  revision 1
+  url "https://camaya.net/download/gloox-1.0.22.tar.bz2"
+  sha256 "4ade1b14848ccfcd89a505a4fff05116c24f13cef8d02fab0ade2717117ec964"
 
   bottle do
     cellar :any
