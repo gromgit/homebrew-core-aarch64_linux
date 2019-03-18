5cbd0aa3ee176258bef4feee415223db6d9b2727
hyuraku <yu@hill-wood.com>
2019-03-18 22:15:47 +0900

    pypy 7.1.0
    Closes #38014.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/pypy.rb
+++ Formula/pypy.rb
@@ -1,8 +1,8 @@
 class Pypy < Formula
   desc "Highly performant implementation of Python 2 in Python"
   homepage "https://pypy.org/"
-  url "https://bitbucket.org/pypy/pypy/downloads/pypy2.7-v7.0.0-src.tar.bz2"
-  sha256 "f51d8bbfc4e73a8a01820b7871a45d13c59f1399822cdf8a19388c69eb20c18c"
+  url "https://bitbucket.org/pypy/pypy/downloads/pypy2.7-v7.1.0-src.tar.bz2"
+  sha256 "84ce5bc2867b224e2516ef431d78c6908d0182bc89444f7c1ef707443763754f"
   head "https://bitbucket.org/pypy/pypy", :using => :hg
 
   bottle do
