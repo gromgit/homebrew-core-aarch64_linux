b367389e9cd96581b8dab9282f1c0803059eba0d
Chongyu Zhu <i@lembacon.com>
2019-03-20 00:16:28 +0800

    libgpg-error 1.36
    Closes #38055.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/libgpg-error.rb
+++ Formula/libgpg-error.rb
@@ -1,8 +1,8 @@
 class LibgpgError < Formula
   desc "Common error values for all GnuPG components"
   homepage "https://www.gnupg.org/related_software/libgpg-error/"
-  url "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.35.tar.bz2"
-  sha256 "cbd5ee62a8a8c88d48c158fff4fc9ead4132aacd1b4a56eb791f9f997d07e067"
+  url "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.36.tar.bz2"
+  sha256 "babd98437208c163175c29453f8681094bcaf92968a15cafb1a276076b33c97c"
 
   bottle do
     sha256 "1a96796605d2d7dc8a558848965a09678b13e50d52a971729bdf9a96b6f824c7" => :mojave
