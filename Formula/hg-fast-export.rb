5ff9dc1aae0954179c35432cf0a2e19ac6f1343d
Igor Kapkov <igasgeek@me.com>
2019-03-19 19:34:08 +1100

    hg-fast-export 190107 (#38042)

--- Formula/hg-fast-export.rb
+++ Formula/hg-fast-export.rb
@@ -1,8 +1,8 @@
 class HgFastExport < Formula
   desc "Fast Mercurial to Git converter"
   homepage "https://repo.or.cz/fast-export.git"
-  url "https://github.com/frej/fast-export/archive/v180610.tar.gz"
-  sha256 "b5ee4fa5b797c4941b447fe3bff543f868b19e24c09c1dbbccf33438120dc36a"
+  url "https://github.com/frej/fast-export/archive/v190107.tar.gz"
+  sha256 "2b9ff09308071ac2642ffe411b834f5028ee23ca6d1deb237a39ed9e63ecd5b1"
 
   bottle :unneeded
 
