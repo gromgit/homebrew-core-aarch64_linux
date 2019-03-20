0c8147ec6bfde029ac35110c9951d751bbc9815f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-20 22:30:44 +0000

    libssh2: update 1.8.1 bottle.

--- Formula/libssh2.rb
+++ Formula/libssh2.rb
@@ -6,12 +6,9 @@ class Libssh2 < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "9705f2a153a854b15bff89663eca46dd211f5fc025031b9851d64874f83c8f53" => :mojave
-    sha256 "22327eb5bbff660935db0c5106d5a43069ee23e5cb33d5125bad4e144e83ee34" => :high_sierra
-    sha256 "4a1e39137bc9461d779a7a84626354928788aeb0650fb0fed75e0fbecb95c0cd" => :sierra
-    sha256 "d6693c1417f0deb8f1b0c6a7c338491a7f60f2cc516675186e572329c1fcaa6c" => :el_capitan
-    sha256 "f7fab0024a104c43a3139b0e70cbc04606c20409b36ffb6deebb326c168c4547" => :yosemite
+    sha256 "f927577d783406a3b0fa010b39acbfc39c29d262dd7a1fa38c4abe69e58139a9" => :mojave
+    sha256 "98f643593c20a04b588b0c96332f83a9106ccffbfee2d8b45aa033ce741c0903" => :high_sierra
+    sha256 "57327cbb9408518a9ee7b578bd30192730c730bdb0508f53e14671dea5965a92" => :sierra
   end
 
   head do
