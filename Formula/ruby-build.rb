cf0721f6aef4b3f46f6d93bcfb4f3415554fe3cf
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2019-03-20 22:26:52 +0900

    ruby-build 20190320 (#38091)

--- Formula/ruby-build.rb
+++ Formula/ruby-build.rb
@@ -1,8 +1,8 @@
 class RubyBuild < Formula
   desc "Install various Ruby versions and implementations"
   homepage "https://github.com/rbenv/ruby-build"
-  url "https://github.com/rbenv/ruby-build/archive/v20190314.tar.gz"
-  sha256 "2cc0f9fdb232042e71edad93a5c3ae108bcd090ea0b6db4e5bb6325547e07968"
+  url "https://github.com/rbenv/ruby-build/archive/v20190320.tar.gz"
+  sha256 "efaede8d5310f6acdfa162a6d7af4bed19740745712a9c5f4dd19540e45754be"
   head "https://github.com/rbenv/ruby-build.git"
 
   bottle :unneeded
