bf220c130c1272a006628320426cfda84e0f6835
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-20 10:39:46 +0000

    re-flex: update 1.1.2 bottle.

--- Formula/re-flex.rb
+++ Formula/re-flex.rb
@@ -6,9 +6,9 @@ class ReFlex < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "be67a520e18f912a3b292371270020b3537240135b2da12573b6dc054b75be32" => :mojave
-    sha256 "a3231afa635eb51dd4a08ab997547fa8bf00f41d4800fe1dd0b157b4facd3d81" => :high_sierra
-    sha256 "2cba3e4a8f73fb3c612549cf664b02e909cc94792c9440189074051157d2ccee" => :sierra
+    sha256 "31b0293a7ac19c77d00f7441fa3b7bb69013bed19328bb163b7c34d365290007" => :mojave
+    sha256 "8bc106f9b3a10fcafce9390a4d9f3a811c95222e1188ff98af344ee1ea14bc60" => :high_sierra
+    sha256 "b41459674440a7941774ae3752a232bdef0d8819eddec21c2a5e073c683ec4ee" => :sierra
   end
 
   depends_on "boost"
