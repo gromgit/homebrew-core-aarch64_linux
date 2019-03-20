b085da6a671ce794aa9b53287743e8ea4fbf41cd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-20 09:47:49 +0000

    nicovideo-dl: add 0.0.20190126 bottle.

--- Formula/nicovideo-dl.rb
+++ Formula/nicovideo-dl.rb
@@ -5,6 +5,13 @@ class NicovideoDl < Formula
   url "https://dotsrc.dl.osdn.net/osdn/nicovideo-dl/70568/nicovideo-dl-0.0.20190126.tar.gz"
   sha256 "886980d154953bc5ff5d44758f352ce34d814566a83ceb0b412b8d2d51f52197"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "21a964141ae24de2635828bde4ac6f46ed09a8b03b5564050e36ab3df0b9bceb" => :mojave
+    sha256 "21a964141ae24de2635828bde4ac6f46ed09a8b03b5564050e36ab3df0b9bceb" => :high_sierra
+    sha256 "ff5c5d5a91e5384c20cd068a1857c83322e2ab27ac6ee0baf01cfabad44b9dd9" => :sierra
+  end
+
   depends_on "python"
 
   def install
