e2e094b1df0a69f3e094fc7b6eb46f9bed68d872
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-21 00:00:20 +0000

    ffsend: add 0.2.30 bottle.

--- Formula/ffsend.rb
+++ Formula/ffsend.rb
@@ -4,6 +4,13 @@ class Ffsend < Formula
   url "https://github.com/timvisee/ffsend/archive/v0.2.30.tar.gz"
   sha256 "53130e6b50c6b14c8ec04d6d4077800a0b1855ffd0b2bdbb475ae8ec208695bd"
 
+  bottle do
+    cellar :any
+    sha256 "547a7c5fabd3111ee422c993127dfa281b25b2866ed6fbf82edd3fde478c90b8" => :mojave
+    sha256 "60070578e9f9a187767b172bc1de242405063641b0cb2092a539f7f02709e85d" => :high_sierra
+    sha256 "4abb91e3d0ab8e03a1b54e116414601cc98503a881cdc790a23508b17ad10064" => :sierra
+  end
+
   depends_on "rust" => :build
   depends_on "openssl"
 
