96edcf36e3cc39d03c7f3c529e83b4d78fdec4ff
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-21 03:14:25 +0000

    gr-osmosdr: update 0.1.4_6 bottle.

--- Formula/gr-osmosdr.rb
+++ Formula/gr-osmosdr.rb
@@ -8,9 +8,9 @@ class GrOsmosdr < Formula
 
   bottle do
     cellar :any
-    sha256 "7a48bbf6b603fd73038501573bcc8b674f3d27525cfff3e6a051d3684b5c5c36" => :mojave
-    sha256 "5a001726a68f39b756787e14977c2429835f84b50ddbb6412c7bc9b7d2d645bb" => :high_sierra
-    sha256 "6c02b13ca92243d639b5642dd07914db4e582b85127f92eb27205178b3a7989a" => :sierra
+    sha256 "fcaf8709a3fafc759518ff097549f26acce8032b6af2417a9790fbfdb804048d" => :mojave
+    sha256 "8070724f0a7afdc21c2acc2c9c1abbf8fd12a29ccee40ed1a4e668015bf514aa" => :high_sierra
+    sha256 "2b1ea0f5b661256c860743352549ccbd3e95fee09330a67b2c3e2d44532a5558" => :sierra
   end
 
   depends_on "cmake" => :build
