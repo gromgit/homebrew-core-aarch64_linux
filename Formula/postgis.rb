258f42036516cab8fbe36f4bc73dea675643d184
FX Coudert <fxcoudert@gmail.com>
2019-03-22 10:29:22 +0100

    postgis 2.5.2
    Closes #38171.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/postgis.rb
+++ Formula/postgis.rb
@@ -1,8 +1,8 @@
 class Postgis < Formula
   desc "Adds support for geographic objects to PostgreSQL"
   homepage "https://postgis.net/"
-  url "https://download.osgeo.org/postgis/source/postgis-2.5.1.tar.gz"
-  sha256 "fb137056f43aae0e9d475dc5b7934eccce466f86f5ceeb69ec8b5cea26817a91"
+  url "https://download.osgeo.org/postgis/source/postgis-2.5.2.tar.gz"
+  sha256 "b6cb286c5016029d984f8c440947bf9178da72e1f6f840ed639270e1c451db5e"
 
   bottle do
     cellar :any
