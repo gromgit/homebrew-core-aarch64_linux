bcd613deccd2473b273ef43bb73704c2e7ebe098
Sean T Allen <sean@monkeysnatchbanana.com>
2019-03-22 16:04:15 -0400

    ponyc 0.28.0
    Closes #38186.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/ponyc.rb
+++ Formula/ponyc.rb
@@ -1,8 +1,8 @@
 class Ponyc < Formula
   desc "Object-oriented, actor-model, capabilities-secure programming language"
   homepage "https://www.ponylang.org/"
-  url "https://github.com/ponylang/ponyc/archive/0.27.0.tar.gz"
-  sha256 "52afc7b2fa03e97b1e6535ccf78c2811df15fe8e37247f32c4b8cabd1b638448"
+  url "https://github.com/ponylang/ponyc/archive/0.28.0.tar.gz"
+  sha256 "4075c1d3848ba128d7c88a83a839215ccdb930edf32d47c48f6b547ce082fbb8"
   head "https://github.com/ponylang/ponyc.git"
 
   bottle do
@@ -22,13 +22,13 @@ class Ponyc < Formula
   end
 
   depends_on "libressl"
-  depends_on "llvm"
+  depends_on "llvm@7"
   depends_on :macos => :yosemite
   depends_on "pcre2"
 
   def install
     ENV.cxx11
-    ENV["LLVM_CONFIG"] = "#{Formula["llvm"].opt_bin}/llvm-config"
+    ENV["LLVM_CONFIG"] = "#{Formula["llvm@7"].opt_bin}/llvm-config"
     system "make", "install", "verbose=1", "config=release",
            "ponydir=#{prefix}", "prefix="
   end
