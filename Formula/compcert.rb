adf0a34b520e07b2b0c9b7e14296adb04cf1bade
Igor Kapkov <igasgeek@me.com>
2019-03-22 14:15:28 +1100

    compcert 3.5

--- Formula/compcert.rb
+++ Formula/compcert.rb
@@ -1,8 +1,8 @@
 class Compcert < Formula
   desc "Formally verified C compiler"
   homepage "http://compcert.inria.fr"
-  url "https://github.com/AbsInt/CompCert/archive/v3.4.tar.gz"
-  sha256 "7098136318ea1db90d9807a58fd27e90306e8022cb7d6e335ca2c13f3787ec89"
+  url "https://github.com/AbsInt/CompCert/archive/v3.5.tar.gz"
+  sha256 "1ea01f9fe9dcfb664dec3b0076bd9ae0d54bb9d79279b5351ab12cc32369b509"
 
   bottle do
     cellar :any_skip_relocation
