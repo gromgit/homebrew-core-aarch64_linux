6ae75035ffa6309c85a29e22c0c22099f196d54e
Viktor Szakats <commit@vszakats.net>
2019-03-22 12:29:49 +0000

    taktuk: secure homepage

--- Formula/taktuk.rb
+++ Formula/taktuk.rb
@@ -1,6 +1,6 @@
 class Taktuk < Formula
   desc "Deploy commands to (a potentially large set of) remote nodes"
-  homepage "http://taktuk.gforge.inria.fr/"
+  homepage "https://taktuk.gforge.inria.fr/"
   url "https://gforge.inria.fr/frs/download.php/file/37055/taktuk-3.7.7.tar.gz"
   sha256 "56a62cca92670674c194e4b59903e379ad0b1367cec78244641aa194e9fe893e"
 
