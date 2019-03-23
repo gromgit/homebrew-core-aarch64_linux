d9ddb38c247ebb63c7388c0dcfe9581c3dfef874
FX Coudert <fxcoudert@gmail.com>
2019-03-23 17:48:13 +0100

    gdal: revision for proj

--- Formula/gdal.rb
+++ Formula/gdal.rb
@@ -3,7 +3,7 @@ class Gdal < Formula
   homepage "https://www.gdal.org/"
   url "https://download.osgeo.org/gdal/2.4.0/gdal-2.4.0.tar.xz"
   sha256 "c3791dcc6d37e59f6efa86e2df2a55a4485237b0a48e330ae08949f0cdf00f27"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "448a4cc4dde65b98bae3abaa94e7a037d95cc002ba12e9dc5fd56ed21b7b7a3e" => :mojave
