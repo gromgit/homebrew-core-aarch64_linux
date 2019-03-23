d7fc909db972f1c28300346c2234836d23bed7b4
FX Coudert <fxcoudert@gmail.com>
2019-03-23 17:48:26 +0100

    points2grid: revision for proj

--- Formula/points2grid.rb
+++ Formula/points2grid.rb
@@ -3,7 +3,7 @@ class Points2grid < Formula
   homepage "https://github.com/CRREL/points2grid"
   url "https://github.com/CRREL/points2grid/archive/1.3.1.tar.gz"
   sha256 "6e2f2d3bbfd6f0f5c2d0c7d263cbd5453745a6fbe3113a3a2a630a997f4a1807"
-  revision 3
+  revision 4
 
   bottle do
     cellar :any
@@ -17,17 +17,16 @@ class Points2grid < Formula
   depends_on "gdal"
 
   def install
-    args = std_cmake_args + ["-DWITH_GDAL=ON"]
+    ENV.cxx11
     libexec.install "test/data/example.las"
-    system "cmake", ".", *args
+    system "cmake", ".", *std_cmake_args, "-DWITH_GDAL=ON"
     system "make", "install"
   end
 
   test do
-    system bin/"points2grid",
-           "-i", libexec/"example.las",
-           "-o", "example",
-           "--max", "--output_format", "grid"
+    system bin/"points2grid", "-i", libexec/"example.las",
+                              "-o", "example",
+                              "--max", "--output_format", "grid"
     assert_equal 13, File.read("example.max.grid").scan("423.820000").size
   end
 end
