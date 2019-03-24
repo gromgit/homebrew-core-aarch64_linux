45e94e584cea78c0babd73a368592adb7e5132b6
Chris Baker <ChrisBaker97@users.noreply.github.com>
2019-03-23 20:05:00 -0400

    asciidoctor v2.0.6
     - 2.0.0 requires ruby >= 2.3
     - add ruby dependency for MacOS.version <= :sierra
    
    Closes #38220.
    
    Signed-off-by: Izaak Beekman <zbeekman@gmail.com>

--- Formula/asciidoctor.rb
+++ Formula/asciidoctor.rb
@@ -1,8 +1,10 @@
 class Asciidoctor < Formula
   desc "Text processor and publishing toolchain for AsciiDoc"
   homepage "https://asciidoctor.org/"
-  url "https://github.com/asciidoctor/asciidoctor/archive/v1.5.8.tar.gz"
-  sha256 "bc225145feb7876bce5188aa3ef511fad49b141ec18e1bb60e69b33b0a100da0"
+  url "https://github.com/asciidoctor/asciidoctor/archive/v2.0.6.tar.gz"
+  sha256 "203d1902d67bf659715b4667347719be6ab0dcb902fff3eea5e70ad2324b0a19"
+
+  depends_on "ruby" if MacOS.version <= :sierra
 
   bottle do
     cellar :any_skip_relocation
