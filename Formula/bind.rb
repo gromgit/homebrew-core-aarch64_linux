c47a87e372e57972edb7f9233c700726cccd20b9
Chongyu Zhu <i@lembacon.com>
2019-03-24 20:19:19 +0800

    bind: explicitly specify the path for the python3 interpreter
    Fixes #38022.
    
    Closes #38233.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/bind.rb
+++ Formula/bind.rb
@@ -3,6 +3,7 @@ class Bind < Formula
   homepage "https://www.isc.org/downloads/bind/"
   url "https://ftp.isc.org/isc/bind9/9.14.0/bind-9.14.0.tar.gz"
   sha256 "4edd459830bb97f749e25a5d42a2a4a093d7800e9962fca4300996cf7ea680af"
+  revision 1
   head "https://gitlab.isc.org/isc-projects/bind9.git"
 
   bottle do
@@ -41,7 +42,8 @@ class Bind < Formula
                           "--enable-threads",
                           "--enable-ipv6",
                           "--with-openssl=#{Formula["openssl"].opt_prefix}",
-                          "--with-libjson=#{Formula["json-c"].opt_prefix}"
+                          "--with-libjson=#{Formula["json-c"].opt_prefix}",
+                          "--with-python=#{Formula["python"].opt_bin}/python3"
 
     # From the bind9 README: "Do not use a parallel "make"
     ENV.deparallelize
