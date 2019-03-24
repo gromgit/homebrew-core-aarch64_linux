6a64bff6116e6fddce52cc0974d496f63728fdd3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-24 21:04:22 +0000

    spatialite-gui: update 1.7.1_5 bottle.

--- Formula/spatialite-gui.rb
+++ Formula/spatialite-gui.rb
@@ -7,11 +7,9 @@ class SpatialiteGui < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "877a0b1d4981a2b13031769a812f25549f824817978c6d30b57b2ddecd734f14" => :mojave
-    sha256 "329a43210fc2f99c3d2c8090bb304bff81e5fc2450778f15f052b32100230151" => :high_sierra
-    sha256 "1ab0a3f3a0ce183dac239210c4bf2f632cdfe827a59a86f34bf3229bf2df93e4" => :sierra
-    sha256 "e04f2392b64ad916bb50e62d2267b075eab00ca44f7b8001f7a9f5e30c447e3e" => :el_capitan
+    sha256 "f1540e6cb0e8565039043767ba8e4d15de2068054832570456cb10760ffddd30" => :mojave
+    sha256 "f8821bf0bc2b6e1aed35937cd6a3d94a9208828a961dea9308ba25d78ddf14b8" => :high_sierra
+    sha256 "f6531922f0cb1d731f8450e55469990ba8e0dde3451d51560522c853f8c4a345" => :sierra
   end
 
   depends_on "pkg-config" => :build
