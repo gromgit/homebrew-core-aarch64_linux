02248b811552ce69c9869203c62f0d7d5c7d1e72
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-24 12:28:29 +0000

    ori: update 0.8.2 bottle.

--- Formula/ori.rb
+++ Formula/ori.rb
@@ -6,11 +6,9 @@ class Ori < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "2c40de6a29e9aa747f6370b93122a437c8ff3e35fd93129141b467ad18414894" => :mojave
-    sha256 "d96b3f8a41b879772d0b2e3f9eff4e43c73cb268b782c0ddbbdd82bd9680d804" => :high_sierra
-    sha256 "3851019914d6ea9efc9115c100b73d24775b6d5d6c517399aea4259e74097670" => :sierra
-    sha256 "c19677cd17c419810b6389aa7c2815f0f8da16d7035dbdad5e209a96913e1f45" => :el_capitan
+    sha256 "2937393d4e11d7b6cff0f93e2ff4e9dff2357b705547efdee1bd7b653b8e1b66" => :mojave
+    sha256 "9f56ea8889aa6d5e7f7bf205b1d67e26fb25929eee6e524aeb72dbbfcaec4aab" => :high_sierra
+    sha256 "bdaaa086155ab57411b5b35547623f04f847eaca4478b0aebd8455f3b3d18fe2" => :sierra
   end
 
   depends_on "pkg-config" => :build
