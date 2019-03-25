a98332c055d405078baf940f49b39a2a615909cb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-25 03:18:12 +0000

    gearboy: update 2.7.0 bottle.

--- Formula/gearboy.rb
+++ Formula/gearboy.rb
@@ -7,11 +7,9 @@ class Gearboy < Formula
 
   bottle do
     cellar :any
-    sha256 "cbb37a8ffbb9649763f36f75236c1f8f2b4001a16c1efc56af7069247a0408cb" => :mojave
-    sha256 "1d670f7aa0fabb668cdd96c5f3ef369f96f791e70586191e23ee244ffc024bfb" => :high_sierra
-    sha256 "91bfab6b8f83e80620d72c40cc1ca4256d7f15910d48d2919a6ba9c152939425" => :sierra
-    sha256 "35e8aeca3e1204cfa1fd105634994d94e4c69f86bca4bf791fcd937c9da2ab29" => :el_capitan
-    sha256 "2572d0286af3eefda16352a9c74050bc62b76bf91b64be9d74af0c82230df9da" => :yosemite
+    sha256 "b92c7577a7287232e161ba437b22c8764be7af8925a19797af63926f8527210c" => :mojave
+    sha256 "b92c7577a7287232e161ba437b22c8764be7af8925a19797af63926f8527210c" => :high_sierra
+    sha256 "cf8ee78a09a077f9cb37a6dd636882f3732eb2fbab911a47ad77ecf2a9272775" => :sierra
   end
 
   depends_on "qt"
