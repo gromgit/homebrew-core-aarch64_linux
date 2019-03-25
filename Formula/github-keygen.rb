54554d3918c1e89893d06d00a050853eefe58e39
Igor Kapkov <igasgeek@me.com>
2019-03-25 19:38:37 +1100

    github-keygen 1.305 (#38251)

--- Formula/github-keygen.rb
+++ Formula/github-keygen.rb
@@ -1,8 +1,8 @@
 class GithubKeygen < Formula
   desc "Bootstrap GitHub SSH configuration"
   homepage "https://github.com/dolmen/github-keygen"
-  url "https://github.com/dolmen/github-keygen/archive/v1.304.tar.gz"
-  sha256 "463b630e395bddad8cc570bd93f5fb42d4d3b6b68de7724cac536381f18b3567"
+  url "https://github.com/dolmen/github-keygen/archive/v1.305.tar.gz"
+  sha256 "5a0a68ed9e3eb1c0e3b783c250e0790fffe17fc1a663cefa1348560ff040b940"
   head "https://github.com/dolmen/github-keygen.git"
 
   bottle :unneeded
