baa328c7c8390a9fc10ecb9838f54ca2e6205de7
Yonas Kolb <yonaskolb@users.noreply.github.com>
2019-03-25 22:45:06 +1100

    xcodegen 2.3.0
    Closes #38259.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/xcodegen.rb
+++ Formula/xcodegen.rb
@@ -1,8 +1,8 @@
 class Xcodegen < Formula
   desc "Generate your Xcode project from a spec file and your folder structure"
   homepage "https://github.com/yonaskolb/XcodeGen"
-  url "https://github.com/yonaskolb/XcodeGen/archive/2.2.0.tar.gz"
-  sha256 "04fcc6b77a354c03b5ce7fff96ea2b55580f79a4dde43132fd9d992436ed01cf"
+  url "https://github.com/yonaskolb/XcodeGen/archive/2.3.0.tar.gz"
+  sha256 "214bb1535b54056d9e67a7b12820797ad4d7ad38976750f4851c874dc2c8b802"
   head "https://github.com/yonaskolb/XcodeGen.git"
 
   bottle do
