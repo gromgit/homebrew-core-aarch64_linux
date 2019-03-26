47a23d4798068c3a0fe329cc3f788fdc5820cbef
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-26 23:09:58 +0000

    swagger-codegen@2: update 2.4.4 bottle.

--- Formula/swagger-codegen@2.rb
+++ Formula/swagger-codegen@2.rb
@@ -6,9 +6,9 @@ class SwaggerCodegenAT2 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "484bb243a1701a06c6f1357ea526d928ab38e1cc085af1015fcc3014b609302a" => :mojave
-    sha256 "19ef8c8852e3bce455f863946375c4ff407f696b861add6d8d9ec6657dc1b75a" => :high_sierra
-    sha256 "d4d5a6c2eec943d9fdf96029769a3bb4b11c944dc13911068168f3b5db2ac18d" => :sierra
+    sha256 "7c43f95eb12320836718af25f031aad9178d4d1696dab8e630b4be30e0a89fa1" => :mojave
+    sha256 "98b073f8ef4a729c9c2a0279a2c13f71f59469932d6f8ba71d5ebdc2a97d85f5" => :high_sierra
+    sha256 "513d63d48f63f4f479de0f1ae71d0b3d49466dfb3ab654a5f793bc3571f98dba" => :sierra
   end
 
   keg_only :versioned_formula
