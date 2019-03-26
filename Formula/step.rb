b6a62ff879e65dcaae8d9bf84a4c4fd3eeef1831
max furman <mx.furman@gmail.com>
2019-03-26 15:31:23 -0700

    step 0.9.0
    Closes #38344.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/step.rb
+++ Formula/step.rb
@@ -1,8 +1,8 @@
 class Step < Formula
   desc "Crypto and x509 Swiss-Army-Knife"
   homepage "https://smallstep.com"
-  url "https://github.com/smallstep/cli/archive/v0.8.6.tar.gz"
-  sha256 "8eb5099550d510b24ba1e5a53ccd4141e545286274c6abceb712be702d428820"
+  url "https://github.com/smallstep/cli/archive/v0.9.0.tar.gz"
+  sha256 "0a0497535b80c4357e5412ce41c691567c24e42dc0513ade3ae341b6981627b3"
 
   bottle do
     cellar :any_skip_relocation
@@ -15,8 +15,8 @@ class Step < Formula
   depends_on "go" => :build
 
   resource "certificates" do
-    url "https://github.com/smallstep/certificates/archive/v0.8.5.tar.gz"
-    sha256 "dc2b007247baeaa020a626813aea2b15fc2eb7f970cf0002a4b421fdf3ab294f"
+    url "https://github.com/smallstep/certificates/archive/v0.9.0.tar.gz"
+    sha256 "426475d286ea52679e0fed425a3da683b0f12b94b23274cc4f373bf75e6a0469"
   end
 
   def install
