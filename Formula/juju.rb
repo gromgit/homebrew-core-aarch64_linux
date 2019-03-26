739321bf6dc8ec12c5dfb7fa8f3f6bcdf243555b
Christopher Lee <veebers@gmail.com>
2019-03-26 15:47:46 +1300

    juju 2.5.3
    Juju 2.5.3
    
    Closes #38284.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -1,8 +1,8 @@
 class Juju < Formula
   desc "DevOps management tool"
   homepage "https://jujucharms.com/"
-  url "https://launchpad.net/juju/2.5/2.5.2/+download/juju-core_2.5.2.tar.gz"
-  sha256 "76c52c263d598e0a87c7a7a70b92030b31d751e0f7a068d388187df59312038e"
+  url "https://launchpad.net/juju/2.5/2.5.3/+download/juju-core_2.5.3.tar.gz"
+  sha256 "95e3290e4f761377eeef27998565b8d582406be4cdbd4c3f23b3a73b48015b12"
 
   bottle do
     cellar :any_skip_relocation
