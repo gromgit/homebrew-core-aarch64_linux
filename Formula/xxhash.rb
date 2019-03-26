97d3907331006414ec33ff1d28ecb030cc0aaba9
Igor Kapkov <igasgeek@me.com>
2019-03-26 15:53:24 +1100

    xxhash 0.7.0
    Closes #38294.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/xxhash.rb
+++ Formula/xxhash.rb
@@ -1,8 +1,8 @@
 class Xxhash < Formula
   desc "Extremely fast non-cryptographic hash algorithm"
   homepage "https://github.com/Cyan4973/xxHash"
-  url "https://github.com/Cyan4973/xxHash/archive/v0.6.5.tar.gz"
-  sha256 "19030315f4fc1b4b2cdb9d7a317069a109f90e39d1fe4c9159b7aaa39030eb95"
+  url "https://github.com/Cyan4973/xxHash/archive/v0.7.0.tar.gz"
+  sha256 "b34792646d5e19964bb7bba24f06cb13aecaac623ab91a54da08aa19d3686d7e"
 
   bottle do
     cellar :any
