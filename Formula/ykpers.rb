d2f88446be4659bd58ab188d0c5ee1b61268f1e0
Eli Young <elyscape@gmail.com>
2019-03-26 09:41:34 -0700

    ykpers 1.19.3
    Closes #38321.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/ykpers.rb
+++ Formula/ykpers.rb
@@ -1,9 +1,8 @@
 class Ykpers < Formula
   desc "YubiKey personalization library and tool"
   homepage "https://developers.yubico.com/yubikey-personalization/"
-  url "https://developers.yubico.com/yubikey-personalization/Releases/ykpers-1.19.0.tar.gz"
-  sha256 "2bc8afa16d495a486582bad916d16de1f67c0cce9bb0a35c3123376c2d609480"
-  revision 1
+  url "https://developers.yubico.com/yubikey-personalization/Releases/ykpers-1.19.3.tar.gz"
+  sha256 "d9513ec7576bea36be37e3fb87a51d4522a28b6b0611ce64ad9fdf2cb7b21b4a"
 
   bottle do
     cellar :any
