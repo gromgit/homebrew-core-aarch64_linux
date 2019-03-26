575695b8ad87d32f445956d532103a9e4c6067bd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-26 00:15:47 +0000

    kubernetes-cli: update 1.14.0 bottle.

--- Formula/kubernetes-cli.rb
+++ Formula/kubernetes-cli.rb
@@ -8,9 +8,9 @@ class KubernetesCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f4814aba4f5165608754e5ceb8f51785c38c1ee2a45d72a8734f177785688b34" => :mojave
-    sha256 "49d95c79f338fa72ff7515151419fcdc6aba40fc124471c71e1ec850147de8f4" => :high_sierra
-    sha256 "dfedb368c32ba9f05b587dc0a49fcbc34ec9353a352dc1888b9a9cd019fe6fd6" => :sierra
+    sha256 "4dae09bb6a2aec286aec1d591dc36d14f474ecc95cbe4cddcbc1f06387223d78" => :mojave
+    sha256 "2ee48a4a1ce24c8f281283734559cb96f1aaa06cff7c1d055beb167e96e23f36" => :high_sierra
+    sha256 "2287abdf3c352691fbd8849dd7f3e76fc92b0a94f6fa32e9f677e1d01c9b3e4e" => :sierra
   end
 
   depends_on "go" => :build
