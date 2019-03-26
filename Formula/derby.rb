42a8f024aedafd5ae7bc149f01497a1c42505543
Igor Kapkov <igasgeek@me.com>
2019-03-26 16:14:16 +1100

    derby 10.15.1.3
    Closes #38298.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/derby.rb
+++ Formula/derby.rb
@@ -1,8 +1,8 @@
 class Derby < Formula
   desc "Apache Derby is an embedded relational database running on JVM"
   homepage "https://db.apache.org/derby/"
-  url "https://www.apache.org/dyn/closer.cgi?path=db/derby/db-derby-10.14.2.0/db-derby-10.14.2.0-bin.tar.gz"
-  sha256 "980fb0534c38edf4a529a13fb4a12b53d32054827b57b6c5f0307d10f17d25a8"
+  url "https://www.apache.org/dyn/closer.cgi?path=db/derby/db-derby-10.15.1.3/db-derby-10.15.1.3-bin.tar.gz"
+  sha256 "eedb0293fea8b7d9cc813371c34935661e42ea8270e72fedd0ffe2a6a29c61ad"
 
   bottle :unneeded
 
