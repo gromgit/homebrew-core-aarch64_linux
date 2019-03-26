7ac36d2c16b728f83f40c882e972bf9bc5fff28e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-26 05:43:28 +0000

    polyml: update 5.8 bottle.

--- Formula/polyml.rb
+++ Formula/polyml.rb
@@ -6,10 +6,9 @@ class Polyml < Formula
   head "https://github.com/polyml/polyml.git"
 
   bottle do
-    sha256 "ac906a03567d473a5827a8c6ccb8dc12944e4c28ed85925d62fd09b8d2c401d9" => :mojave
-    sha256 "5a1f478b3b2b26ab1f2326031c5ea930aa97ac08b83881151e976942c674df65" => :high_sierra
-    sha256 "d4c027b336791cc932cebddb6ea901af8a17f482511e9858a4766b380a1b3391" => :sierra
-    sha256 "5adaaa6d45b674090dd49918532a5035737e7d7d2a1acf861a053ac96d297596" => :el_capitan
+    sha256 "7d004ce1e0cd9920cc0d66a690be780e11365f8725bbfaa68222ddf324d1d0f8" => :mojave
+    sha256 "11835db3f5848077c9b0e6af52bceffaf4c356892c9e3dc795976bcf9711a06b" => :high_sierra
+    sha256 "b3dd5bae8d8cef36c6258c2620a4ae894a7a1c66fe56330c48e19a1f81aa66d9" => :sierra
   end
 
   def install
