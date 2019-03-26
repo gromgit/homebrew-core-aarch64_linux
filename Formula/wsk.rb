93b90648caf168b9d6c02b9afbf5f6e5178a4d26
David Grove <groved@us.ibm.com>
2019-03-26 09:35:53 -0400

    wsk 0.10.0-incubating
    Closes #38315.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/wsk.rb
+++ Formula/wsk.rb
@@ -1,9 +1,9 @@
 class Wsk < Formula
   desc "OpenWhisk Command-Line Interface (CLI)"
   homepage "https://openwhisk.apache.org/"
-  url "https://github.com/apache/incubator-openwhisk-cli/archive/0.9.0-incubating.tar.gz"
-  version "0.9.0-incubating"
-  sha256 "76ec64d1a505c88f7d13df898b07cdea7b13b9799747d432e0bde55f7dc2c8b9"
+  url "https://github.com/apache/incubator-openwhisk-cli/archive/0.10.0-incubating.tar.gz"
+  version "0.10.0-incubating"
+  sha256 "0f0052ea85b10aea8902d4ccb9393fd523b96d5b2477b1c38d486366edc9535c"
 
   bottle do
     cellar :any_skip_relocation
@@ -16,17 +16,6 @@ class Wsk < Formula
   depends_on "go" => :build
   depends_on "go-bindata" => :build
   depends_on "govendor" => :build
-
-  # Add govendor support. Remove both of these on next release.
-  patch do
-    url "https://github.com/apache/incubator-openwhisk-cli/pull/362.patch?full_index=1"
-    sha256 "5f8dba945cc6684846f77a2c628ca295755b97231e5ae4a7d3d870e8a2933ad6"
-  end
-
-  patch do
-    url "https://github.com/apache/incubator-openwhisk-cli/pull/363.patch?full_index=1"
-    sha256 "eef2bc85a8a8581baba590284c47eee6c033e53e0e93d1a65608f9faad9ba0d2"
-  end
 
   def install
     ENV["GOPATH"] = buildpath
