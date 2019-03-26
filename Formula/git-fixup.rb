cd6f14a7797cf373fdf6e7c9e2f63bf07c978f34
Igor Kapkov <igasgeek@me.com>
2019-03-26 15:32:05 +1100

    git-fixup 1.2.0
    Closes #38287.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/git-fixup.rb
+++ Formula/git-fixup.rb
@@ -1,8 +1,8 @@
 class GitFixup < Formula
   desc "Alias for git commit --fixup <ref>"
   homepage "https://github.com/keis/git-fixup"
-  url "https://github.com/keis/git-fixup/archive/v1.1.2.tar.gz"
-  sha256 "d34b5b45591bfd08e1091d7435b07b1a60af1e53463b94e9ce727a341ee6bdbb"
+  url "https://github.com/keis/git-fixup/archive/v1.2.0.tar.gz"
+  sha256 "894989bd31b52fcfbfe226bc86bd1baea06820aa86938561b14772929f261337"
   head "https://github.com/keis/git-fixup.git", :branch => "master"
 
   bottle do
