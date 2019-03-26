93a00ab33a60b3ea8281177ba3b2775ce2d1c138
Caleb Xu <calebcenter@live.com>
2019-03-26 19:02:14 -0400

    bowtie2 2.3.5
    Closes #38348.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/bowtie2.rb
+++ Formula/bowtie2.rb
@@ -1,8 +1,8 @@
 class Bowtie2 < Formula
   desc "Fast and sensitive gapped read aligner"
   homepage "https://bowtie-bio.sourceforge.io/bowtie2/"
-  url "https://github.com/BenLangmead/bowtie2/archive/v2.3.4.3.tar.gz"
-  sha256 "f10fc386277677329f4b9d1cb6951e6b8e6c125f07438476bd3653c79ad00b07"
+  url "https://github.com/BenLangmead/bowtie2/archive/v2.3.5.tar.gz"
+  sha256 "c5daeb0249e98dfe8f70dac7d660b100365a00a22df4cad94d3e383f489911ae"
 
   bottle do
     cellar :any
