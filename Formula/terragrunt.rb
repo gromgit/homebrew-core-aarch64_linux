fb99f1df15e1ae81db9a194e0913ec8a04c9ce79
Rui Chen <rchen@meetup.com>
2019-03-26 11:34:00 -0400

    terragrunt 0.18.3
    terragrunt v0.18.3
    
    Closes #38319.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -1,8 +1,8 @@
 class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state"
   homepage "https://github.com/gruntwork-io/terragrunt"
-  url "https://github.com/gruntwork-io/terragrunt/archive/v0.18.2.tar.gz"
-  sha256 "527f5dc60f92600875413c8557202f178d7ecd96073b74f8fb535bb2c8a8812d"
+  url "https://github.com/gruntwork-io/terragrunt/archive/v0.18.3.tar.gz"
+  sha256 "b38c7f72ece72bfbe719486f4196c225a02d5e0c1345ba8ec6dacadb8eb58c59"
   head "https://github.com/gruntwork-io/terragrunt.git"
 
   bottle do
