b1b5476ae65e223a5d46f4b7a42716e23ec4592e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-26 20:49:20 +0000

    aide: update 0.16.1 bottle.

--- Formula/aide.rb
+++ Formula/aide.rb
@@ -5,11 +5,10 @@ class Aide < Formula
   sha256 "0f2b7cecc70c1a27d35c06c98804fcdb9f326630de5d035afc447122186010b7"
 
   bottle do
-    rebuild 1
-    sha256 "0c952b922666c241fd3fe1249b29bf88c2149378511218d76dc30de1097a04f1" => :mojave
-    sha256 "c9429f028f2627c8ae1b76737b26741cecc4c18507139b02bcc6c487bc5e15a7" => :high_sierra
-    sha256 "71d151f2f389cbbc5884eff30261d0691d020c0983411962c1ba42927d0ae052" => :sierra
-    sha256 "2860850684659f15f8d5dc01127a7a9f2bfc21f773d99c4a8897585b4542723d" => :el_capitan
+    cellar :any
+    sha256 "53b1dfabc76d6e54db56ec24f7f91b6cc9dcdd18210d17d2df92f86225fb9c9f" => :mojave
+    sha256 "79a2d4ce92526516891c844a4852161d39421f9dc31d2eba5ea0e48d79496053" => :high_sierra
+    sha256 "b626fcf7e52a0ea66fbed58bdc00cb08484f7bce8e84e61edf6740fbad7fabc5" => :sierra
   end
 
   head do
