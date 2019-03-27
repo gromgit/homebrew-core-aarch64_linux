aeac511c7ca8f6448bb2da258a471ac4cf97676c
rpaulucci3 <rafaelpaulucci2016@gmail.com>
2019-03-26 23:45:51 -0400

    faudio 19.04
    Closes #38367.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/faudio.rb
@@ -0,0 +1,33 @@
+class Faudio < Formula
+  desc "Accuracy-focused XAudio reimplementation for open platforms"
+  homepage "https://fna-xna.github.io/"
+  url "https://github.com/FNA-XNA/FAudio/archive/19.04.tar.gz"
+  sha256 "8d28c7a67bf67953c27173f33393ccee10f7b3ad52b9767a7e33c98387b67bdf"
+  head "https://github.com/FNA-XNA/FAudio.git"
+
+  depends_on "cmake" => :build
+  depends_on "sdl2"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  def caveats; <<~EOS
+    FAudio is built without FFmpeg support for decoding xWMA resources.
+  EOS
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <FAudio.h>
+      int main(int argc, char const *argv[])
+      {
+        FAudio *audio;
+        return FAudioCreate(&audio, 0, FAUDIO_DEFAULT_PROCESSOR);
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lfaudio", "-o", "test"
+    system "./test"
+  end
+end
