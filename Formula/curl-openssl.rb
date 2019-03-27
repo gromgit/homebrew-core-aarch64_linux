3c5cd79bbfd05207a98514d4fd72e4a2d8dd48cd
Chongyu Zhu <i@lembacon.com>
2019-03-28 03:07:07 +0800

    curl-openssl 7.64.1
    Closes #38424.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/curl-openssl.rb
+++ Formula/curl-openssl.rb
@@ -1,8 +1,8 @@
 class CurlOpenssl < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.haxx.se/"
-  url "https://curl.haxx.se/download/curl-7.64.0.tar.bz2"
-  sha256 "d573ba1c2d1cf9d8533fadcce480d778417964e8d04ccddcc76e591d544cf2eb"
+  url "https://curl.haxx.se/download/curl-7.64.1.tar.bz2"
+  sha256 "4cc7c738b35250d0680f29e93e0820c4cb40035f43514ea3ec8d60322d41a45d"
 
   bottle do
     sha256 "97d32ac24c5a62f93c832fcb881ade6fc8a895e36bf1af0837b535216dce16f3" => :mojave
