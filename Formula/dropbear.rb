67101a67bd49dc4e227c66dc15e66a69dff98a19
Caleb Xu <calebcenter@live.com>
2019-03-27 13:37:23 -0400

    dropbear 2019.78
    Closes #38421.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/dropbear.rb
+++ Formula/dropbear.rb
@@ -1,19 +1,8 @@
 class Dropbear < Formula
   desc "Small SSH server/client for POSIX-based system"
   homepage "https://matt.ucc.asn.au/dropbear/dropbear.html"
-  revision 1
-
-  stable do
-    url "https://matt.ucc.asn.au/dropbear/releases/dropbear-2018.76.tar.bz2"
-    sha256 "f2fb9167eca8cf93456a5fc1d4faf709902a3ab70dd44e352f3acbc3ffdaea65"
-
-    # Fixes CVE-2018-15599. Safe to remove on next release.
-    # https://lists.ucc.gu.uwa.edu.au/pipermail/dropbear/2018q3/002108.html
-    patch do
-      url "https://secure.ucc.asn.au/hg/dropbear/raw-rev/5d2d1021ca00"
-      sha256 "42b5720cf6c888638cfb84fdd862fc0d323b2e023cbe5f9ccdaa2e0c35b6873e"
-    end
-  end
+  url "https://matt.ucc.asn.au/dropbear/releases/dropbear-2019.78.tar.bz2"
+  sha256 "525965971272270995364a0eb01f35180d793182e63dd0b0c3eb0292291644a4"
 
   bottle do
     cellar :any_skip_relocation
