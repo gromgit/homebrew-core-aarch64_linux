fb6b8141e8a9b52967766d07f8f569f943acfffa
Camden Narzt <c.narzt@me.com>
2019-03-27 16:01:42 -0600

    passenger: bump revision and patch.
    Closes #38435.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Formula/passenger.rb
+++ Formula/passenger.rb
@@ -3,7 +3,7 @@ class Passenger < Formula
   homepage "https://www.phusionpassenger.com/"
   url "https://github.com/phusion/passenger/releases/download/release-6.0.2/passenger-6.0.2.tar.gz"
   sha256 "56b2273312e6dc9880f6ba83e381583b8759085a0b41338b782c9575d58346bc"
-  revision 1
+  revision 2
   head "https://github.com/phusion/passenger.git", :branch => "stable-6.0"
 
   bottle do
@@ -17,6 +17,11 @@ class Passenger < Formula
   depends_on "nginx" => [:build, :test]
   depends_on "openssl"
   depends_on "pcre"
+
+  patch do
+    url "https://github.com/phusion/passenger/commit/09df7df0.patch?full_index=1"
+    sha256 "397707a788029f4abac4780d2e04ddba37ec9285b44c9d3e4ff0c91c5121d2b7"
+  end
 
   def install
     # https://github.com/Homebrew/homebrew-core/pull/1046
