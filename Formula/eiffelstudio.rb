ff27a81d25bd3c6e2305969ea244ea85c0a807ca
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-28 08:27:17 +0000

    eiffelstudio: update 18.11.10.2592 bottle.

--- Formula/eiffelstudio.rb
+++ Formula/eiffelstudio.rb
@@ -6,9 +6,9 @@ class Eiffelstudio < Formula
 
   bottle do
     cellar :any
-    sha256 "dd72c10b3b814fd0a5f2658f24b9959f216fd1aab9ee0e52f5105c4803362259" => :mojave
-    sha256 "7b03fd689acdc6a9b477eb569f6438fb8648c43cd79b87fe1a25db1d96bc8d76" => :high_sierra
-    sha256 "8e2cb58f4954fb7e1906d4ad62865d1104509d3ae303d6e4f5021f3225726727" => :sierra
+    sha256 "1d10a333b4a0c02bcb3349c0e08cc098044c910f41918d568ef64abf3a60c56e" => :mojave
+    sha256 "b8b9e63265767087d299fca6947226e01b619dcb675921736dba47e44422b783" => :high_sierra
+    sha256 "da85e81c76e071da3642ad3fa7f01414c5485cbea3320a2ec4081358dd0b9e05" => :sierra
   end
 
   depends_on "pkg-config" => :build
