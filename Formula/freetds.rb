5d00dd42f3e13c5b68438adbb3f6173c066f5ede
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-28 11:43:52 +0000

    freetds: update 1.1.3 bottle.

--- Formula/freetds.rb
+++ Formula/freetds.rb
@@ -5,9 +5,9 @@ class Freetds < Formula
   sha256 "bd08d2b7e6b7819fec611f02ff5b48a53298d46f733385f12e83289c017f2d1c"
 
   bottle do
-    sha256 "fd9b19bd8f1706e31f8aa64f03845f9e22aeff5a94532c78fce1a067f4060b23" => :mojave
-    sha256 "669574d37d29bd6a2f6d4cdc6ffc77b98b81dbc00d867c347d6e2adb2905583b" => :high_sierra
-    sha256 "3666a4d54c227e39ec309d786da7f5c06de0ef8fa92c2e3ab767a81fc03107f3" => :sierra
+    sha256 "66ce6291c83495f7372ae00559eccc8044c19d61642f5f02a375c22e63ac88d5" => :mojave
+    sha256 "79172589792275fd0ae469364a01b8ffc617080687a6523f1401c210d122cb08" => :high_sierra
+    sha256 "d1d7e4eb2fe06e76e8b257770477969b70d4bc3159bd1d08b8c759ca1affdfac" => :sierra
   end
 
   head do
