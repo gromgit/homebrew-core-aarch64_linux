17f317bcbece3080d2b2b9870fa870e2b04a54dd
Martin Harrigan <martinharrigan@gmail.com>
2019-03-28 09:12:35 +0000

    procyon-decompiler 0.5.34 (#38442)

--- Formula/procyon-decompiler.rb
+++ Formula/procyon-decompiler.rb
@@ -1,8 +1,8 @@
 class ProcyonDecompiler < Formula
   desc "Modern decompiler for Java 5 and beyond"
   homepage "https://bitbucket.org/mstrobel/procyon/wiki/Java%20Decompiler"
-  url "https://bitbucket.org/mstrobel/procyon/downloads/procyon-decompiler-0.5.30.jar"
-  sha256 "cd9a2177f72f490842b0b84909f58b5548614a78ecd135ce28b833f93429e5a0"
+  url "https://bitbucket.org/mstrobel/procyon/downloads/procyon-decompiler-0.5.34.jar"
+  sha256 "ff575a42133a19a44635f2ab2808662b648a67cf5033f94dd6bad4b24335c843"
 
   bottle :unneeded
 
