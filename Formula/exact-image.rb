778bc410df9479ba05a2432b823f7c711825f1ba
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-28 18:39:22 +0000

    exact-image: update 1.0.2 bottle.

--- Formula/exact-image.rb
+++ Formula/exact-image.rb
@@ -5,10 +5,9 @@ class ExactImage < Formula
   sha256 "0694c66be5dec41377acead475de69b3d7ffb42c702402f8b713f8b44cdc2791"
 
   bottle do
-    rebuild 1
-    sha256 "3c2f60adaefd294c52d8e55c5781eed44b93382776cb08b898a4b02b708954fe" => :mojave
-    sha256 "d9a2cdd3a88e19bb6e3c0f4c1712d58b80b4003b3fe676186a90a0e3827cb556" => :high_sierra
-    sha256 "22d98724dae572e22a2801ee3a5675d393588364ff5c950a0d323a6ca6411ecd" => :sierra
+    sha256 "942bfd38bf5fd52613c936077eee5d5f71530325c7337e9db84e44e0b6c643a0" => :mojave
+    sha256 "b182c3fa086d336ee9e6688bb341ea3df8ace70cac451fb757e88ba15c925365" => :high_sierra
+    sha256 "1a9fc0dbba69ee471deabc6759ca52f3d669a535e021ef2defa33321261010ca" => :sierra
   end
 
   depends_on "pkg-config" => :build
