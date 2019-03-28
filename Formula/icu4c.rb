44895fce117ab92a44d733315b702c48dbb3898d
Chongyu Zhu <i@lembacon.com>
2019-03-28 09:47:20 +0800

    icu4c 64.2

--- Formula/icu4c.rb
+++ Formula/icu4c.rb
@@ -1,10 +1,10 @@
 class Icu4c < Formula
   desc "C/C++ and Java libraries for Unicode and globalization"
   homepage "https://ssl.icu-project.org/"
-  url "https://ssl.icu-project.org/files/icu4c/63.1/icu4c-63_1-src.tgz"
-  mirror "https://github.com/unicode-org/icu/releases/download/release-63-1/icu4c-63_1-src.tgz"
-  version "63.1"
-  sha256 "05c490b69454fce5860b7e8e2821231674af0a11d7ef2febea9a32512998cb9d"
+  url "https://ssl.icu-project.org/files/icu4c/64.2/icu4c-64_2-src.tgz"
+  mirror "https://github.com/unicode-org/icu/releases/download/release-64-2/icu4c-64_2-src.tgz"
+  version "64.2"
+  sha256 "627d5d8478e6d96fc8c90fed4851239079a561a6a8b9e48b0892f24e82d31d6c"
 
   bottle do
     cellar :any
