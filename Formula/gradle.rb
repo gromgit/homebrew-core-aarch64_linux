b196924abf02be1b43bcfbe5d023a72882648943
Daniel Lacasse <lacasseio@users.noreply.github.com>
2019-03-28 14:52:38 +0100

    gradle 5.3.1 (#38447)

--- Formula/gradle.rb
+++ Formula/gradle.rb
@@ -1,8 +1,8 @@
 class Gradle < Formula
   desc "Open-source build automation tool based on the Groovy and Kotlin DSL"
   homepage "https://www.gradle.org/"
-  url "https://services.gradle.org/distributions/gradle-5.3-all.zip"
-  sha256 "f4d820c2a9685710eba5b92f10e0e4fb20e0d6c0dd1f46971e658160f25e7147"
+  url "https://services.gradle.org/distributions/gradle-5.3.1-all.zip"
+  sha256 "b018a7308cb43633662363d100c14a3c41c66fd4e32b59e1dfc644d6fd2109f6"
 
   bottle :unneeded
 
