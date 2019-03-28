6b78711a3b8e6e005a754a5059a96b9648449356
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-28 17:59:03 +0000

    glui: update 2.37 bottle.

--- Formula/glui.rb
+++ Formula/glui.rb
@@ -6,12 +6,9 @@ class Glui < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e22ebbb1c0e2065dc69936bea4c7afd080ad7849a84816142284c071348a1f20" => :mojave
-    sha256 "c6968d950e93bb81b88c656ecaddd5400a924386fcdeecb6d185cf4ec4d8d7cf" => :high_sierra
-    sha256 "36971571cceb35b758732131c7c9495d01b95fb2d4130000039c1b9d9a3922d8" => :sierra
-    sha256 "eae0f5aab2883fa397f09bb314a90672e41c39c24e0c43e49259c8016b1c50db" => :el_capitan
-    sha256 "617c71a79c2ce69ff31e7bd84e9e4f41b09e8cf3d039190bef92ad6bf1ae416c" => :yosemite
-    sha256 "1a5b5bc92fbb7077f11f53fcbd16d1e63b1e04850d029afa2a8e82822a5e82e4" => :mavericks
+    sha256 "24c323dbaa5f6f1b01fbf8f837c379ef503d323a448d2bb3d673c31ced622f0d" => :mojave
+    sha256 "7cd9b9d6bffa3b6b6ff806c4041f495d5a7ef40296cb50097db25d17eb616265" => :high_sierra
+    sha256 "c087de27b46b86a14d583904e0a9d293428af37d8710b521ae7aeeb5174fc8fd" => :sierra
   end
 
   # Fix compiler warnings in glui.h. Merged into master on November 28, 2016.
