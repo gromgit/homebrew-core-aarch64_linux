74c9e60f4e4aafec2a8a8c457ed0579d84ac55a3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-29 20:48:54 +0000

    gssdp: update 1.2.0 bottle.

--- Formula/gssdp.rb
+++ Formula/gssdp.rb
@@ -5,10 +5,9 @@ class Gssdp < Formula
   sha256 "22cbef547f522f0b062933e302482ebdb397e2f3703899757562ddffbbfd00d1"
 
   bottle do
-    cellar :any
-    sha256 "0b33c442e3ac602474980603f72ae51cd003e338d89aaedebbdc858dda2a6b93" => :mojave
-    sha256 "7c745644a57ff5a9d80d7422830770aad3c80aabef6009a4db107612b97d2536" => :high_sierra
-    sha256 "fd8130caac892fab33a2abf2db7f7089455d700fa5454d36820d54781adae7be" => :sierra
+    sha256 "ba0e685fdd43e1a7077acc89f426094cbac4bbd6ef8a7c9a41c019d51edb48a6" => :mojave
+    sha256 "f8ffc61329914be5de59373b9d2aacc7a762bc902730e0202ddf82f1c6cf6186" => :high_sierra
+    sha256 "e5e69427ba9125550e2e07d0ce98c02311d9cf21535244b5ea78bd01e8494271" => :sierra
   end
 
   depends_on "gobject-introspection" => :build
