f20f52b88f28cadf773407f8524a7ee86c3bfe1f
Michihito Shigemura <i.am.shige@gmail.com>
2019-03-29 11:51:14 +0900

    docker-completion 18.09.4
    Closes #38479.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/docker-completion.rb
+++ Formula/docker-completion.rb
@@ -2,8 +2,8 @@ class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/docker-ce.git",
-      :tag      => "v18.09.3",
-      :revision => "774a1f4eee66e29a71ca12e88ac2220670990f7e"
+      :tag      => "v18.09.4",
+      :revision => "d14af54266dfeb55872100e28d14231b1baafe85"
 
   bottle :unneeded
 
