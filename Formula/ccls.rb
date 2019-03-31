4e9154450dae29a067bcfc35515de1fb780a2bf5
Xu Cheng <git@xuc.me>
2019-03-31 20:49:18 +0800

    ccls 0.20190314 (new formula)
    Closes #38117.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

---
+++ Formula/ccls.rb
@@ -0,0 +1,21 @@
+class Ccls < Formula
+  desc "C/C++/ObjC language server"
+  homepage "https://github.com/MaskRay/ccls"
+  url "https://github.com/MaskRay/ccls/archive/0.20190314.tar.gz"
+  sha256 "aaefa603a76325bb94e5222d144e19c432771346990c8b84165832bf37d15bb3"
+  head "https://github.com/MaskRay/ccls.git"
+
+  depends_on "cmake" => :build
+  depends_on "rapidjson" => :build
+  depends_on "llvm"
+  depends_on :macos => :high_sierra # C++ 17 is required
+
+  def install
+    system "cmake", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system bin/"ccls", "-index=#{testpath}"
+  end
+end
