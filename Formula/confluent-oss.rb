83ad76b9f9a6d6df531e85aa51a9cf5f95d1e39f
thepythonista <39398718+thepythonista@users.noreply.github.com>
2019-04-02 15:28:11 +0530

    confluent-oss 5.2.0 (#38577)

--- Formula/confluent-oss.rb
+++ Formula/confluent-oss.rb
@@ -1,9 +1,9 @@
 class ConfluentOss < Formula
   desc "Developer-optimized distribution of Apache Kafka"
   homepage "https://www.confluent.io/product/confluent-open-source/"
-  url "https://packages.confluent.io/archive/5.1/confluent-5.1.2-2.11.tar.gz"
-  version "5.1.2"
-  sha256 "dceea4333367b4513f971a78064bec9a12b977bb309957e1d4e863f9aae3dac0"
+  url "https://packages.confluent.io/archive/5.2/confluent-5.2.0-2.12.tar.gz"
+  version "5.2.0"
+  sha256 "0b469eabbfe283cbdf8b8f98a2966d3344a95344bc08ca4ca86a6ca52d8cf2d4"
 
   bottle :unneeded
 
