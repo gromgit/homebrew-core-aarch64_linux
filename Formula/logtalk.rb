415c623efb10e15518b9800a032e22d4a2ac9230
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-02 11:17:50 +0000

    logtalk: update 3.25.0 bottle.

--- Formula/logtalk.rb
+++ Formula/logtalk.rb
@@ -7,9 +7,9 @@ class Logtalk < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "10473d34fe7e542f93c9f759cfe12f911975eaf7e6c088307d8386f00da7ea75" => :mojave
-    sha256 "0ebb74f7daa175173360a277d72d7165d4537d1825387d9d03e491d078100e78" => :high_sierra
-    sha256 "8ff3172100f81eab17e01f14ba0640097968e80e4ff2da09f74c1062394a8657" => :sierra
+    sha256 "8db8435080b7493e418f42173098d296486c67b6de183995a8680cd5859fcca5" => :mojave
+    sha256 "a2414773437e60d53c9f657d5cc8cfabe514e73302960355b9afe02178a58dd1" => :high_sierra
+    sha256 "5dbf80032c447a35c8135963c86571cb8f69498a391befb2e9ef82c535a83a97" => :sierra
   end
 
   depends_on "gnu-prolog"
