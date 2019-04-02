fc392c9e261fd6b84c8fa577bfef076f0bd022c9
Michihito Shigemura <i.am.shige@gmail.com>
2019-04-02 16:27:48 +0900

    embulk 0.9.17
    Closes #38599.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/embulk.rb
+++ Formula/embulk.rb
@@ -1,8 +1,8 @@
 class Embulk < Formula
   desc "Data transfer between various databases, file formats and services"
   homepage "https://www.embulk.org/"
-  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.9.16.jar"
-  sha256 "a6c6d1414b7d1234e8565d66c18f4912d80b8b9ba60e297ef0bad22a19e5cc8d"
+  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.9.17.jar"
+  sha256 "595cf166d99f6a3650ac87390d64e230b0878f17a43cac275706c7f4a2fc6989"
 
   bottle :unneeded
 
