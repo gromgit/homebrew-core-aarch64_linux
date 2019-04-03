4ddac2776ed4da5fe63925071587353324996918
Caleb Xu <calebcenter@live.com>
2019-04-03 10:44:23 -0400

    hatari 2.2.1
    Closes #38640.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/hatari.rb
+++ Formula/hatari.rb
@@ -1,9 +1,9 @@
 class Hatari < Formula
   desc "Atari ST/STE/TT/Falcon emulator"
   homepage "https://hatari.tuxfamily.org"
-  url "https://download.tuxfamily.org/hatari/2.1.0/hatari-2.1.0.tar.bz2"
-  sha256 "eb299460e92db4a8a2983a0725cbbc8c185f1470b8ecd791b3d102815da20924"
-  head "https://hg.tuxfamily.org/mercurialroot/hatari/hatari", :using => :hg, :branch => "default"
+  url "https://download.tuxfamily.org/hatari/2.2.1/hatari-2.2.1.tar.bz2"
+  sha256 "11afef30a274be84696588583d5a1d65c8046934670f718c311c956ef7106f60"
+  head "https://git.tuxfamily.org/hatari/hatari.git"
 
   bottle do
     cellar :any
@@ -20,15 +20,8 @@ class Hatari < Formula
 
   # Download EmuTOS ROM image
   resource "emutos" do
-    url "https://downloads.sourceforge.net/project/emutos/emutos/0.9.9.1/emutos-512k-0.9.9.1.zip"
-    sha256 "ab94cd249aebd7fb1696cbd5992734042450d8b96525f707e9ad8a2283185341"
-  end
-
-  # Fix build by providing missing NSAlertStyleInformational
-  # Remove in next version.
-  patch do
-    url "https://hg.tuxfamily.org/mercurialroot/hatari/hatari/raw-rev/21bc2b0ebae4"
-    sha256 "0d6ca48030749061246d5edbf61e4bf2ad0311d5f004c1df8bee1d662b581dc6"
+    url "https://downloads.sourceforge.net/project/emutos/emutos/0.9.10/emutos-512k-0.9.10.zip"
+    sha256 "773bbbfc418827d863c313c4f1d3c73ef3d296c5f23b4d00ee4f38f080a9c255"
   end
 
   def install
