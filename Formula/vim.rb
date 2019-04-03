cf619210c7cedb70adac9dc060ecb608b0c5a01d
Maxim Usikov <mu.usikov@gmail.com>
2019-04-03 08:49:21 +0300

    vim 8.1.1100
    Closes #38634.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/v8.1.1050.tar.gz"
-  sha256 "b87ecef09e9a9151ad60d570bce64991ddc399c21594929e2029db57ff29f98b"
+  url "https://github.com/vim/vim/archive/v8.1.1100.tar.gz"
+  sha256 "d7f59c2a0a16983d71f0bb27560356fd1602134037abdecd90e4280f3cafa8e3"
   head "https://github.com/vim/vim.git"
 
   bottle do
