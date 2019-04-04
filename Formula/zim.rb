3a5120c2be9c090181fcaa2248a44207fe623e77
Yung Shen <kaxing@ymail.com>
2019-04-04 20:06:32 +0800

    zim 0.70
    Closes #38658.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/zim.rb
+++ Formula/zim.rb
@@ -1,8 +1,8 @@
 class Zim < Formula
   desc "Graphical text editor used to maintain a collection of wiki pages"
   homepage "https://zim-wiki.org/"
-  url "https://github.com/jaap-karssenberg/zim-desktop-wiki/archive/0.69.tar.gz"
-  sha256 "48205c533f1df4d8b143d5bc5adc09a6cd979c53a5d6b504922d8ac28b57e532"
+  url "https://github.com/jaap-karssenberg/zim-desktop-wiki/archive/0.70.tar.gz"
+  sha256 "3c828cf02c9ba7ca542e0a941e0a0b602e408e05453bcd4d5389f669d48bb764"
   head "https://github.com/jaap-karssenberg/zim-desktop-wiki.git"
 
   bottle do
@@ -15,25 +15,25 @@ class Zim < Formula
 
   depends_on "pkg-config" => :build
   depends_on "graphviz"
-  depends_on "gtk+"
-  depends_on "pygobject"
+  depends_on "gtk+3"
+  depends_on "pygobject3"
   depends_on "pygtk"
   depends_on "pygtksourceview"
-  depends_on "python@2"
+  depends_on "python"
 
   resource "pyxdg" do
-    url "https://files.pythonhosted.org/packages/source/p/pyxdg/pyxdg-0.25.tar.gz"
-    sha256 "81e883e0b9517d624e8b0499eb267b82a815c0b7146d5269f364988ae031279d"
+    url "https://files.pythonhosted.org/packages/47/6e/311d5f22e2b76381719b5d0c6e9dc39cd33999adae67db71d7279a6d70f4/pyxdg-0.26.tar.gz"
+    sha256 "fe2928d3f532ed32b39c32a482b54136fe766d19936afc96c8f00645f9da1a06"
   end
 
   def install
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python3.7/site-packages"
     resource("pyxdg").stage do
       system "python", *Language::Python.setup_install_args(libexec/"vendor")
     end
     ENV["XDG_DATA_DIRS"] = libexec/"share"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
-    system "python", "./setup.py", "install", "--prefix=#{libexec}", "--skip-xdg-cmd"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python3.7/site-packages"
+    system "python3", "./setup.py", "install", "--prefix=#{libexec}"
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"], :XDG_DATA_DIRS => libexec/"share"
     pkgshare.install "zim"
