fb624828e646c351e2ab5ff31d7315d70bea5a84
Igor Kapkov <igasgeek@me.com>
2019-04-05 10:16:51 +1100

    docfx 2.41
    Closes #38675.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -1,8 +1,8 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/releases/download/v2.40.12/docfx.zip"
-  sha256 "7364323e072a8a70e3349122a8bc18a8e48456367d054cd9e392957a935d3354"
+  url "https://github.com/dotnet/docfx/releases/download/v2.41/docfx.zip"
+  sha256 "e6c5351be6b6daebc672b84b529ed82006582cb9134a4b5a9b5c135fc5a62aeb"
 
   bottle :unneeded
 
