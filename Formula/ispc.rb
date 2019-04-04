946ba0b0d422220ba9ca22edc362902b3a91f438
Jamie Snape <jamie.snape@kitware.com>
2019-04-03 21:34:55 -0400

    ispc 1.10.0: fix standard library generation and add meaningful test
    Closes #38643.
    
    Signed-off-by: Izaak Beekman <zbeekman@gmail.com>

--- Formula/ispc.rb
+++ Formula/ispc.rb
@@ -3,6 +3,7 @@ class Ispc < Formula
   homepage "https://ispc.github.io"
   url "https://github.com/ispc/ispc/archive/v1.10.0.tar.gz"
   sha256 "0aa30e989f8d446b2680c9078d5c5db70634f40b9aa07db387aa35aa08dd0b81"
+  revision 1
 
   bottle do
     cellar :any
@@ -17,9 +18,16 @@ class Ispc < Formula
   depends_on "llvm@4"
 
   def install
+    # The standard include paths for clang supplied by the llvm@4 formula do not include
+    # C headers such as unistd.h. Add the path to those headers explicitly so that
+    # generation of the ispc builtins and standard library do not silently fail.
+    inreplace "cmake/GenerateBuiltins.cmake", "${CLANG_EXECUTABLE}",
+      "${CLANG_EXECUTABLE} -I#{MacOS.sdk_path}/usr/include"
+
     args = std_cmake_args + %W[
       -DISPC_INCLUDE_EXAMPLES=OFF
       -DISPC_INCLUDE_TESTS=OFF
+      -DISPC_INCLUDE_UTILS=OFF
       -DLLVM_TOOLS_BINARY_DIR='#{Formula["llvm@4"]}'
     ]
 
@@ -31,6 +39,33 @@ class Ispc < Formula
   end
 
   test do
-    system "#{bin}/ispc", "-v"
+    (testpath/"simple.ispc").write <<~EOS
+      export void simple(uniform float vin[], uniform float vout[], uniform int count) {
+        foreach (index = 0 ... count) {
+          float v = vin[index];
+          if (v < 3.)
+            v = v * v;
+          else
+            v = sqrt(v);
+          vout[index] = v;
+        }
+      }
+    EOS
+    system bin/"ispc", "--arch=x86-64", "--target=sse2", testpath/"simple.ispc",
+      "-o", "simple_ispc.o", "-h", "simple_ispc.h"
+
+    (testpath/"simple.cpp").write <<~EOS
+      #include "simple_ispc.h"
+      int main() {
+        float vin[9], vout[9];
+        for (int i = 0; i < 9; ++i) vin[i] = static_cast<float>(i);
+        ispc::simple(vin, vout, 9);
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-I#{testpath}", "-c", "-o", testpath/"simple.o", testpath/"simple.cpp"
+    system ENV.cxx, "-o", testpath/"simple", testpath/"simple.o", testpath/"simple_ispc.o"
+
+    system testpath/"simple"
   end
 end
