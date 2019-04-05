56393f34b0cb2dbd972ab2f08e14ba2f15c708b7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-05 01:15:23 +0000

    erlang: update 21.3.3 bottle.

--- Formula/erlang.rb
+++ Formula/erlang.rb
@@ -8,9 +8,9 @@ class Erlang < Formula
 
   bottle do
     cellar :any
-    sha256 "2c4e861d2b80c4fbd4bec6d9b71c2d4ed06117303f832462b94f9b191cc68201" => :mojave
-    sha256 "e198e71b219a66b8d5b4e2b4c28638cc11a5f59c8dab990a5e0af464f96b805c" => :high_sierra
-    sha256 "224101bf8268f497f372b1198cdf296d2918c84e0fc14511f280daefbf7f183b" => :sierra
+    sha256 "53c23e582b83f5c17f2bf1052d3a13744d15c09ec732199da3f353c5ed7318a4" => :mojave
+    sha256 "f703080728aeddefa3288a860080b995960231cbe84da9fade6b3880b79511a0" => :high_sierra
+    sha256 "a5be2d41fbf879ee32efd0f79efc48564c57dae3b9c636b00a36d58e93aa6a2c" => :sierra
   end
 
   depends_on "autoconf" => :build
