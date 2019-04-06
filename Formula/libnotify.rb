d96a23f024a07714a20e34905d4ffbefb55585a4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-06 06:49:10 +0000

    libnotify: update 0.7.8 bottle.

--- Formula/libnotify.rb
+++ Formula/libnotify.rb
@@ -5,10 +5,9 @@ class Libnotify < Formula
   sha256 "69209e0b663776a00c7b6c0e560302a8dbf66b2551d55616304f240bba66e18c"
 
   bottle do
-    sha256 "d45147d1218ee584b2d67ce23e6bd60553a83424c3c0a69cd8a14f6e238a398c" => :mojave
-    sha256 "f88f445a6b5719c695aa606da3050d3e24ddc1c7a586f6541186025551d273e6" => :high_sierra
-    sha256 "09166e1efa743eed930b7aca87b084de644735f8af17133bf697e24b70f8018f" => :sierra
-    sha256 "0139ddf8047c88ee6c29abfa8a112dce897150d54a0d83b9d2892d5033829a6d" => :el_capitan
+    sha256 "890c7a9086b1e6bc54cc28fbcae17c8f1a2106953ecb06a41b72bb134dc8052f" => :mojave
+    sha256 "e3089bf440369fc09f2498402a235f96ea0de37c7a39e7d1b089e4594381acd6" => :high_sierra
+    sha256 "7adeffb01878acf29de20e4c22714597ba09c80aee07755c31c1287116c7618a" => :sierra
   end
 
   depends_on "autoconf" => :build
