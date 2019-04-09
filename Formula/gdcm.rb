6a34a9215a15ecc66ec43e789b74bc3ba59e5905
Thiago Franco de Moraes <totonixsame@gmail.com>
2019-04-09 11:11:10 -0300

    gdcm 2.8.9
    update to gdcm 2.8.9
    
    Closes #38785.
    
    Signed-off-by: Izaak Beekman <zbeekman@gmail.com>

--- Formula/gdcm.rb
+++ Formula/gdcm.rb
@@ -1,9 +1,8 @@
 class Gdcm < Formula
   desc "Grassroots DICOM library and utilities for medical files"
   homepage "https://sourceforge.net/projects/gdcm/"
-  url "https://downloads.sourceforge.net/project/gdcm/gdcm%202.x/GDCM%202.8.7/gdcm-2.8.7.tar.gz"
-  sha256 "7a08baa93e90bce17d9999d59b95876808801a287812348e27a23decb1ebc58c"
-  revision 1
+  url "https://downloads.sourceforge.net/project/gdcm/gdcm%202.x/GDCM%202.8.9/gdcm-2.8.9.tar.gz"
+  sha256 "a2da88b7b3cbf9e76a9df3e89d06d057cca9ce54fc62fb059e04f47bf056b727"
 
   bottle do
     sha256 "111837bb50a109600314eb44ad9466423ca13cd44ad4f4746e7ea3f63f7de5bd" => :mojave
@@ -23,6 +22,7 @@ class Gdcm < Formula
 
     xy = Language::Python.major_minor_version "python3"
     python_include = Utils.popen_read("python3 -c 'from distutils import sysconfig;print(sysconfig.get_python_inc(True))'").chomp
+    python_executable = Utils.popen_read("python3 -c 'import sys;print(sys.executable)'").chomp
 
     args = std_cmake_args + %W[
       -DGDCM_BUILD_APPLICATIONS=ON
@@ -34,7 +34,7 @@ class Gdcm < Formula
       -DGDCM_USE_SYSTEM_OPENJPEG=ON
       -DGDCM_USE_SYSTEM_OPENSSL=ON
       -DGDCM_WRAP_PYTHON=ON
-      -DPYTHON_EXECUTABLE=python3
+      -DPYTHON_EXECUTABLE=#{python_executable}
       -DPYTHON_INCLUDE_DIR=#{python_include}
       -DGDCM_INSTALL_PYTHONMODULE_DIR=#{lib}/python#{xy}/site-packages
       -DCMAKE_INSTALL_RPATH=#{lib}
