5dcefb6da2d8e914dafdba790d09a4025cd77cb9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-09 21:26:07 +0000

    zabbix-cli: add 2.0.1 bottle.

--- Formula/zabbix-cli.rb
+++ Formula/zabbix-cli.rb
@@ -7,6 +7,13 @@ class ZabbixCli < Formula
   sha256 "237e4ec82f8e070ebd69d6f2df6cecd086663320c67e7afbab39ebb6460ea132"
   head "https://github.com/usit-gd/zabbix-cli.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "44f00b786590c8cf2c8d0cb85015537b0ee478ccb75ce0c2b8100e10f5a5e74e" => :mojave
+    sha256 "60f771c174df15328657938e91a3bfd98291b2854a378fa14df6742ea9a1ed3f" => :high_sierra
+    sha256 "c696cd8b4f8aa3a78e9c77c7b7b813df9b9459eb8ee76252f4e74975af0b5ed9" => :sierra
+  end
+
   depends_on "python"
 
   ## direct dependencies
