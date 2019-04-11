9312171d224f9ab2f32b57abea3f1c99d5fc4332
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2019-04-11 19:37:03 +0200

    Merge pull request #38807 from aljoscha/update-flink-1.8.0
    apache-flink 1.8.0

--- Formula/apache-flink.rb
+++ Formula/apache-flink.rb
@@ -1,9 +1,9 @@
 class ApacheFlink < Formula
   desc "Scalable batch and stream data processing"
   homepage "https://flink.apache.org/"
-  url "https://archive.apache.org/dist/flink/flink-1.7.2/flink-1.7.2-bin-hadoop27-scala_2.11.tgz"
-  version "1.7.2"
-  sha256 "a59a57db7be2a1170f03583ce23f805f3a204214393b68211cc982457952989f"
+  url "https://archive.apache.org/dist/flink/flink-1.8.0/flink-1.8.0-bin-scala_2.11.tgz"
+  version "1.8.0"
+  sha256 "88935984bb57a84b26065035a46c2d69e3ef6805c2780ac8cd3f1f3759bf6925"
   head "https://github.com/apache/flink.git"
 
   bottle :unneeded
