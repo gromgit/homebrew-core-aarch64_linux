0f12b78db8938247e63dd977dc2cd8732d6fda0f
Chongyu Zhu <i@lembacon.com>
2019-04-12 02:37:11 +0800

    hyperscan 5.1.1
    Closes #38859.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/hyperscan.rb
+++ Formula/hyperscan.rb
@@ -1,8 +1,8 @@
 class Hyperscan < Formula
   desc "High-performance regular expression matching library"
   homepage "https://www.hyperscan.io/"
-  url "https://github.com/intel/hyperscan/archive/v5.1.0.tar.gz"
-  sha256 "c751e85a537bc2cebb699f42a66faaf42edf10468f0315cb0719d2051eefa4d8"
+  url "https://github.com/intel/hyperscan/archive/v5.1.1.tar.gz"
+  sha256 "e3bb509d4002f2d75e1804e754efa6334316d1ee110a3b85c8156c08fe5e2369"
 
   bottle do
     cellar :any
