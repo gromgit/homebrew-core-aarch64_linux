3a57f159014dd31ea2d3ac23e6eb723cb6c7d887
Dustin Wheeler <3818510+mskblackbelt@users.noreply.github.com>
2019-04-11 17:47:51 -0400

    most 5.1.0
    Closes #38874.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/most.rb
+++ Formula/most.rb
@@ -1,8 +1,8 @@
 class Most < Formula
   desc "Powerful paging program"
   homepage "https://www.jedsoft.org/most/"
-  url "https://www.jedsoft.org/releases/most/most-5.0.0a.tar.bz2"
-  sha256 "94cb5a2e71b6b9063116f4398a002a757e59cd1499f1019dde8874f408485aa9"
+  url "https://www.jedsoft.org/releases/most/most-5.1.0.tar.gz"
+  sha256 "db805d1ffad3e85890802061ac8c90e3c89e25afb184a794e03715a3ed190501"
   head "git://git.jedsoft.org/git/most.git"
 
   bottle do
