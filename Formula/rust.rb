659e5eadaee2a00b416d96e7ed6826d8a32b25e0
Alex Gaynor <agaynor@mozilla.com>
2019-04-11 14:13:42 -0400

    rust 1.34.0
    Closes #38850.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/rust.rb
+++ Formula/rust.rb
@@ -3,13 +3,13 @@ class Rust < Formula
   homepage "https://www.rust-lang.org/"
 
   stable do
-    url "https://static.rust-lang.org/dist/rustc-1.33.0-src.tar.gz"
-    sha256 "5a01a8d7e65126f6079042831385e77485fa5c014bf217e9f3e4aff36a485d94"
+    url "https://static.rust-lang.org/dist/rustc-1.34.0-src.tar.gz"
+    sha256 "7ac85acffd79dd3a7c44305d9eaabd1f1e7116e2e6e11e770e4bf5f92c0f1f59"
 
     resource "cargo" do
       url "https://github.com/rust-lang/cargo.git",
-          :tag      => "0.34.0",
-          :revision => "f099fe94b66f0a2f80370be8f2d3db2a55b97050"
+          :tag      => "0.35.0",
+          :revision => "6789d8a0a54a96d95365c4e1fb01d47a5eed9937"
     end
 
     resource "racer" do
@@ -47,8 +47,8 @@ class Rust < Formula
 
   resource "cargobootstrap" do
     # From https://github.com/rust-lang/rust/blob/#{version}/src/stage0.txt
-    url "https://static.rust-lang.org/dist/2019-01-17/cargo-0.33.0-x86_64-apple-darwin.tar.gz"
-    sha256 "25cf75479da383d2307e1d6204e915f628ec3f1c185b124f57839cfd622f54b1"
+    url "https://static.rust-lang.org/dist/2019-02-28/cargo-0.34.0-x86_64-apple-darwin.tar.gz"
+    sha256 "5349b04cd783942d39be3223629efbf0325de04ee72751c4a1a1388e858caac6"
   end
 
   def install
@@ -98,7 +98,7 @@ class Rust < Formula
     end
 
     # Remove any binary files; as Homebrew will run ranlib on them and barf.
-    rm_rf Dir["src/{llvm,llvm-emscripten,test,librustdoc,etc/snapshot.pyc}"]
+    rm_rf Dir["src/{llvm-project,llvm-emscripten,test,librustdoc,etc/snapshot.pyc}"]
     (pkgshare/"rust_src").install Dir["src/*"]
 
     rm_rf prefix/"lib/rustlib/uninstall.sh"
