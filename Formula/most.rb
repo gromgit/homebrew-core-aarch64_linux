afd94f38052ee254d5f57a922659a08d64fe74fe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-12 14:45:57 +0000

    most: update 5.1.0 bottle.

--- Formula/most.rb
+++ Formula/most.rb
@@ -6,12 +6,10 @@ class Most < Formula
   head "git://git.jedsoft.org/git/most.git"
 
   bottle do
-    sha256 "af9a922f4a08dbd0afd272fbe8eef56bd2437691721cdff0571531255a61b0ca" => :mojave
-    sha256 "90678b6798fb5eaadf38359fc8f1652fb8dbe3c96c58113742a0727a3d3fb51c" => :high_sierra
-    sha256 "33ff883a9327d71e8d8eaffeb7e12e224ac1e04f06bd3d940317e7d9c431145b" => :sierra
-    sha256 "9e645b60950d18dea0b58c95b0525992cb55bbddc5cdc664dce11e94b552e568" => :el_capitan
-    sha256 "7b2828c656ba7ef31fc03d5570f8d6701f365fd4a96252bcdfae66b266713bc3" => :yosemite
-    sha256 "f7d99563678653a673eddee924ca90f76819eed8a25a47780762571f35187241" => :mavericks
+    cellar :any
+    sha256 "aa9766e4fa0be084108b370c639060b7a27e5ff8eb90c649cbc643160659932f" => :mojave
+    sha256 "192ccb3fe86ae7766bd1aadb8e92d8bc7a28cb666fffe52d0750c6c2a4450657" => :high_sierra
+    sha256 "9a9d74a50ade82af787d47e5f6514df01a47b5159dc1521d93c470ce8554743e" => :sierra
   end
 
   depends_on "s-lang"
