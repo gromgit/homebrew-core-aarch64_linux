93f5cf49f11aded0bc1be5e622157fedfb8856b8
Chongyu Zhu <i@lembacon.com>
2019-04-14 01:06:17 +0800

    fontforge 20190413
    Closes #38907.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/fontforge.rb
+++ Formula/fontforge.rb
@@ -1,8 +1,8 @@
 class Fontforge < Formula
   desc "Command-line outline and bitmap font editor/converter"
   homepage "https://fontforge.github.io"
-  url "https://github.com/fontforge/fontforge/releases/download/20190317/fontforge-20190317.tar.gz"
-  sha256 "b711adec0da9ee490bbc2698b33e6630150931e08e826d1b63eb3131d85db8b5"
+  url "https://github.com/fontforge/fontforge/releases/download/20190413/fontforge-20190413.tar.gz"
+  sha256 "6762a045aba3d6ff1a7b856ae2e1e900a08a8925ccac5ebf24de91692b206617"
 
   bottle do
     cellar :any
