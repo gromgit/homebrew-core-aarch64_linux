3becb00d647595a23956ef68e572421d9e7e285e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-14 20:32:03 +0000

    sk: add 0.6.6 bottle.

--- Formula/sk.rb
+++ Formula/sk.rb
@@ -5,6 +5,13 @@ class Sk < Formula
   sha256 "5975b21a7c3792e910ee9439b41b417e92f2fc7bc92d033e2d4c6d6c48a469c8"
   head "https://github.com/lotabout/skim.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2181bedc3675e45f36e0e81d0af742cb0a298358d16d308032a583f4a0471991" => :mojave
+    sha256 "225b8c28c12cf38ae37086707019e9828eb18472f39a28e0e93c984d6d8dcd82" => :high_sierra
+    sha256 "757c865a849246a6a75776071053ef629698505adca904e432a0bc634c978e98" => :sierra
+  end
+
   depends_on "rust" => :build
 
   def install
