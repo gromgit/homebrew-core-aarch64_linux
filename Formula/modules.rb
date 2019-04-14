7ca5a4eaf247bf60d92a28d233be4d39ff9383d4
FX Coudert <fxcoudert@gmail.com>
2019-04-14 19:26:44 +0200

    modules 4.2.3
    Closes #38925.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/modules.rb
+++ Formula/modules.rb
@@ -1,8 +1,8 @@
 class Modules < Formula
   desc "Dynamic modification of a user's environment via modulefiles"
   homepage "https://modules.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/modules/Modules/modules-4.1.4/modules-4.1.4.tar.bz2"
-  sha256 "7eaf26b66cbf3ba101ec5a693b7bfb3a47f3c86cad09e47c4126f3d785864c55"
+  url "https://downloads.sourceforge.net/project/modules/Modules/modules-4.2.3/modules-4.2.3.tar.bz2"
+  sha256 "83a4afdd3784278cb86aa3fbf82bcda8fea46b12fae616d865cfe7e8d357e4ac"
 
   bottle do
     sha256 "1bfdf504b019fa4c8ffe7db7d9cef79a1a2c23e381b04d7eed98f041b3507987" => :mojave
@@ -11,21 +11,13 @@ class Modules < Formula
     sha256 "b3a8d7c48c0b6c56a706357da2a0b6087c593f2254acd7b84956e6870053b8e1" => :el_capitan
   end
 
-  depends_on "grep" => :build # configure checks for ggrep
-
   def install
-    ENV.prepend_path "PATH", Formula["coreutils"].opt_libexec/"gnubin"
-
-    # -DUSE_INTERP_ERRORLINE fixes
-    # error: no member named 'errorLine' in 'struct Tcl_Interp'
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
       --datarootdir=#{share}
-      --disable-versioning
       --with-tcl=#{MacOS.sdk_path}/System/Library/Frameworks/Tcl.framework
       --without-x
-      CPPFLAGS=-DUSE_INTERP_ERRORLINE
     ]
     system "./configure", *args
     system "make", "install"
