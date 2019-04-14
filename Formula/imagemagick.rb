421b767aba93ed8481aa9a16bc67d1e60cc08a63
Leon Klingele <git@leonklingele.de>
2019-04-14 14:39:03 +0200

    imagemagick 7.0.8-40
    Closes #38916.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -4,9 +4,9 @@ class Imagemagick < Formula
   # Please always keep the Homebrew mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://dl.bintray.com/homebrew/mirror/ImageMagick-7.0.8-39.tar.xz"
-  mirror "https://www.imagemagick.org/download/ImageMagick-7.0.8-39.tar.xz"
-  sha256 "e6964b764bf90e9f41619f198f28207cc73efe2e309ec2275134f449f5b6a0a7"
+  url "https://dl.bintray.com/homebrew/mirror/ImageMagick-7.0.8-40.tar.xz"
+  mirror "https://www.imagemagick.org/download/ImageMagick-7.0.8-40.tar.xz"
+  sha256 "8b7bb75fdac3fbe8f847eb127d471989d505e44ac6664fa030ef86992c12f26d"
   head "https://github.com/ImageMagick/ImageMagick.git"
 
   bottle do
