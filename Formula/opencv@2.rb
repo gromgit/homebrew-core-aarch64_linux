68ef4f2fa5db1ec7659bc4d483cf1961005869f8
FX Coudert <fxcoudert@gmail.com>
2019-04-15 21:15:50 +0200

    opencv@2: revision for ilmbase

--- Formula/opencv@2.rb
+++ Formula/opencv@2.rb
@@ -3,7 +3,7 @@ class OpencvAT2 < Formula
   homepage "https://opencv.org/"
   url "https://github.com/opencv/opencv/archive/2.4.13.7.tar.gz"
   sha256 "192d903588ae2cdceab3d7dc5a5636b023132c8369f184ca89ccec0312ae33d0"
-  revision 2
+  revision 3
 
   bottle do
     sha256 "6580055d021fcabea83a3d2878cba7a6a7851fd2463f769e98138a099b3cc82d" => :mojave
