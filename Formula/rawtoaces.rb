e011a606ac5343c615d30896cf6b37c1357c9e14
FX Coudert <fxcoudert@gmail.com>
2019-04-15 21:15:50 +0200

    rawtoaces: revision for ilmbase

--- Formula/rawtoaces.rb
+++ Formula/rawtoaces.rb
@@ -3,7 +3,7 @@ class Rawtoaces < Formula
   homepage "https://github.com/ampas/rawtoaces"
   url "https://github.com/ampas/rawtoaces/archive/v1.0.tar.gz"
   sha256 "9d15e7e30c4fe97baedfdafb5fddf95534eee26392002b23e81649bbe6e501e9"
-  revision 5
+  revision 6
 
   bottle do
     sha256 "957c43a55ae4bb6eff96bbe84ebfc59ed57ef85be8224fb2847aadebdba0fcf2" => :mojave
