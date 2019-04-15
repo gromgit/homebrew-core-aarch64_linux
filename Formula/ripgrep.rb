dfc6443c5cfaa959c357d210f8315bea39b7c044
dana <dana@dana.is>
2019-04-15 18:43:56 -0500

    ripgrep 11.0.0
    Closes #38955.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/ripgrep.rb
+++ Formula/ripgrep.rb
@@ -1,8 +1,8 @@
 class Ripgrep < Formula
   desc "Search tool like grep and The Silver Searcher"
   homepage "https://github.com/BurntSushi/ripgrep"
-  url "https://github.com/BurntSushi/ripgrep/archive/0.10.0.tar.gz"
-  sha256 "a2a6eb7d33d75e64613c158e1ae450899b437e37f1bfbd54f713b011cd8cc31e"
+  url "https://github.com/BurntSushi/ripgrep/archive/11.0.0.tar.gz"
+  sha256 "be7a7f4a39bd6d408691c65b74f78f5329115085fa1ea2045079d71d0c0abbd8"
   head "https://github.com/BurntSushi/ripgrep.git"
 
   bottle do
