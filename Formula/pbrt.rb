d0ac7ca05513a0cb57b84a10a721dae8e4d9f111
FX Coudert <fxcoudert@gmail.com>
2019-04-15 21:15:50 +0200

    pbrt: revision for ilmbase

--- Formula/pbrt.rb
+++ Formula/pbrt.rb
@@ -3,7 +3,7 @@ class Pbrt < Formula
   homepage "https://pbrt.org/"
   url "https://github.com/mmp/pbrt-v2/archive/2.0.342.tar.gz"
   sha256 "397941435d4b217cd4a4adaca12ab6add9a960d46984972f259789d1462fb6d5"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
