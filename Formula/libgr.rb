0661ad607c1307108aca19d10dbefbf39ce63fd7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-16 03:09:23 +0000

    libgr: add 0.37.0 bottle.

--- Formula/libgr.rb
+++ Formula/libgr.rb
@@ -4,6 +4,12 @@ class Libgr < Formula
   url "https://github.com/sciapp/gr/archive/v0.37.0.tar.gz"
   sha256 "9120c4215cd2c10239f45f4cbf79dcb199a9e7e3bbbf36e5ce8f4e5eb2e70e24"
 
+  bottle do
+    sha256 "cf43f68b60d8c2084f84e35fc66daa8d88b66b13b2380bee6b7807d622f440ef" => :mojave
+    sha256 "66e7569c1ab5112a958b7d986ce0aae23ca939d40d6967707d0806bd64d479e8" => :high_sierra
+    sha256 "00a6481d88b6c4dc5d1557ae8b249521009234410e2d546d5d39e0013acd4645" => :sierra
+  end
+
   depends_on :xcode => :build
   depends_on "cairo"
 
