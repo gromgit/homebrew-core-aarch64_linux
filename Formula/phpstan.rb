ef058b7b1dd0779c0f0979680035397d84e8ba9e
Csaba Maulis <csaba.maulis@gmail.com>
2019-04-16 23:58:48 +0800

    phpstan 0.11.5 (new formula) (#38386)

---
+++ Formula/phpstan.rb
@@ -0,0 +1,76 @@
+class Phpstan < Formula
+  desc "PHP Static Analysis Tool"
+  homepage "https://github.com/phpstan/phpstan"
+  url "https://github.com/phpstan/phpstan/releases/download/0.11.5/phpstan.phar"
+  sha256 "49b0ccea56a27cd379747a5f5a24b75e8c8f382fd593188c075bb5921da5538d"
+
+  bottle :unneeded
+
+  depends_on "php" => :test
+
+  def install
+    bin.install "phpstan.phar" => "phpstan"
+  end
+
+  test do
+    (testpath/"src/autoload.php").write <<~EOS
+      <?php
+      spl_autoload_register(
+          function($class) {
+              static $classes = null;
+              if ($classes === null) {
+                  $classes = array(
+                      'email' => '/Email.php'
+                  );
+              }
+              $cn = strtolower($class);
+              if (isset($classes[$cn])) {
+                  require __DIR__ . $classes[$cn];
+              }
+          },
+          true,
+          false
+      );
+    EOS
+
+    (testpath/"src/Email.php").write <<~EOS
+      <?php
+        declare(strict_types=1);
+
+        final class Email
+        {
+            private $email;
+
+            private function __construct(string $email)
+            {
+                $this->ensureIsValidEmail($email);
+
+                $this->email = $email;
+            }
+
+            public static function fromString(string $email): self
+            {
+                return new self($email);
+            }
+
+            public function __toString(): string
+            {
+                return $this->email;
+            }
+
+            private function ensureIsValidEmail(string $email): void
+            {
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    throw new InvalidArgumentException(
+                        sprintf(
+                            '"%s" is not a valid email address',
+                            $email
+                        )
+                    );
+                }
+            }
+        }
+    EOS
+    assert_match /^\n\n \[OK\] No errors/, shell_output("#{bin}/phpstan analyse --level max --autoload-file src/autoload.php src/Email.php")
+  end
+end
