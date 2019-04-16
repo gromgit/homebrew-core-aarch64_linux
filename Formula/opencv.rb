e7e73dcbc76170a8d5b53ac7021f014d433bfa0d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-16 16:21:43 +0000

    opencv: update 4.1.0_1 bottle.

--- Formula/opencv.rb
+++ Formula/opencv.rb
@@ -6,9 +6,9 @@ class Opencv < Formula
   revision 1
 
   bottle do
-    sha256 "f7783a1f2e99c5bbe358e73a087e871e518e34b72761f5f99d7de042dbeacba0" => :mojave
-    sha256 "d003d4bcd8d3038e38f1446e83270e912628342c68ff257a2c481c2a4f5f6165" => :high_sierra
-    sha256 "2ebd51e12222d60c5724fe8a9b04fbfae9d9311ee9af192f39c5fd48d22a7782" => :sierra
+    sha256 "e480ec68ccb914da357f8c3b799552fdfbfe38e24a75a2c091b28382e1801f70" => :mojave
+    sha256 "b1e6aa6491c192384f163505702dc227dbd182e00a21b370f50e3c5b74cea7de" => :high_sierra
+    sha256 "e2b359e6062e5614e3a6758e51f9768cc99f5c034ab6d1ffa116765b10781f14" => :sierra
   end
 
   depends_on "cmake" => :build
