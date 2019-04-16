c6a769d2e91f71d4dde457cb042bc8cfbf6c4469
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-16 17:58:57 +0000

    faudio: add 19.04 bottle.

--- Formula/faudio.rb
+++ Formula/faudio.rb
@@ -5,6 +5,13 @@ class Faudio < Formula
   sha256 "8d28c7a67bf67953c27173f33393ccee10f7b3ad52b9767a7e33c98387b67bdf"
   head "https://github.com/FNA-XNA/FAudio.git"
 
+  bottle do
+    cellar :any
+    sha256 "75ca99ddc44fb3b92689efd1383ed41b21cab6213acfe0f049951ff2e2d6660e" => :mojave
+    sha256 "cfec9c098ef89f7497c4e3235ed4ca43a51d4730b3dbb05ab2aff237a68b2dc3" => :high_sierra
+    sha256 "0e457600f79e5a466976931982946f71b6f249c73a0d4d669012e07b461a2b55" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "sdl2"
 
