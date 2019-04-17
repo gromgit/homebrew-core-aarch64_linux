808d5992911ff1b7fba93c239355c2a88ba5220e
Chongyu Zhu <i@lembacon.com>
2019-04-18 03:39:41 +0800

    pcre2 10.33
    Closes #39023.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/pcre2.rb
+++ Formula/pcre2.rb
@@ -1,8 +1,8 @@
 class Pcre2 < Formula
   desc "Perl compatible regular expressions library with a new API"
   homepage "https://www.pcre.org/"
-  url "https://ftp.pcre.org/pub/pcre/pcre2-10.32.tar.bz2"
-  sha256 "f29e89cc5de813f45786580101aaee3984a65818631d4ddbda7b32f699b87c2e"
+  url "https://ftp.pcre.org/pub/pcre/pcre2-10.33.tar.bz2"
+  sha256 "35514dff0ccdf02b55bd2e9fa586a1b9d01f62332c3356e379eabb75f789d8aa"
   head "svn://vcs.exim.org/pcre2/code/trunk"
 
   bottle do
