7aa2a0addbd3afc23319cb24aaf08f6a63ecc430
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2019-04-17 07:08:57 -0400

    mypy 0.701
    Closes #39013.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/mypy.rb
+++ Formula/mypy.rb
@@ -2,8 +2,8 @@ class Mypy < Formula
   desc "Experimental optional static type checker for Python"
   homepage "http://www.mypy-lang.org/"
   url "https://github.com/python/mypy.git",
-      :tag      => "v0.620",
-      :revision => "67b260f0fec0b0d97c92529359796e9ad5c1a30d"
+      :tag      => "v0.701",
+      :revision => "7aaa435063f7bb81019740f0b56be359374b02fc"
   head "https://github.com/python/mypy.git"
 
   bottle do
@@ -28,8 +28,13 @@ class Mypy < Formula
   end
 
   resource "typed-ast" do
-    url "https://files.pythonhosted.org/packages/52/cf/2ebc7d282f026e21eed4987e42e10964a077c13cfc168b42f3573a7f178c/typed-ast-1.1.0.tar.gz"
-    sha256 "57fe287f0cdd9ceaf69e7b71a2e94a24b5d268b35df251a88fef5cc241bf73aa"
+    url "https://files.pythonhosted.org/packages/a6/4e/ff9d7b7091e2308d2cdb04a1a317e13f293f4408990ee4a52b7028657917/typed-ast-1.3.4.tar.gz"
+    sha256 "68c362848d9fb71d3c3e5f43c09974a0ae319144634e7a47db62f0f2a54a7fa7"
+  end
+
+  resource "mypy_extensions" do
+    url "https://files.pythonhosted.org/packages/c2/92/3cc05d1206237d54db7b2565a58080a909445330b4f90a6436302a49f0f8/mypy_extensions-0.4.1.tar.gz"
+    sha256 "37e0e956f41369209a3d5f34580150bcacfabaa57b33a15c0b25f4b5725e0812"
   end
 
   def install
