4e0102c80e48d200b9acfb901e772d6a2884fb5f
Emil Tullstedt <emil.tullstedt@tink.se>
2019-04-17 14:28:22 +0200

    fasttext 0.2.0 (new formula)
    Closes #39015.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/fasttext.rb
@@ -0,0 +1,20 @@
+class Fasttext < Formula
+  desc "Library for fast text representation and classification"
+  homepage "https://fasttext.cc"
+  url "https://github.com/facebookresearch/fastText/archive/v0.2.0.tar.gz"
+  sha256 "71d24ffec9fcc4364554ecac2b3308d834178c903d16d090aa6be9ea6b8e480c"
+  head "https://github.com/facebookresearch/fastText.git"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"trainingset").write("__label__brew brew")
+    system "#{bin}/fasttext", "supervised", "-input", "trainingset", "-output", "model"
+    assert_predicate testpath/"model.bin", :exist?
+  end
+end
