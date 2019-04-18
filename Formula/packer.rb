a15fefb632bddaf5ebe85e793d5cd0addc20259e
Thierry Moisan <thierry.moisan@gmail.com>
2019-04-17 21:51:07 -0400

    packer 1.4.0
    Closes #39030.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/packer.rb
+++ Formula/packer.rb
@@ -2,8 +2,8 @@ class Packer < Formula
   desc "Tool for creating identical machine images for multiple platforms"
   homepage "https://packer.io"
   url "https://github.com/hashicorp/packer.git",
-      :tag      => "v1.3.5",
-      :revision => "542cb1d872b4fbba57f34ebb5913a266017a38df"
+      :tag      => "v1.4.0",
+      :revision => "54d2b32d8c96aeefd6cdc59f4064ef68143fc035"
   head "https://github.com/hashicorp/packer.git"
 
   bottle do
@@ -29,7 +29,6 @@ class Packer < Formula
     cd packerpath do
       # Avoid running `go get`
       inreplace "Makefile" do |s|
-        s.gsub! "go get github.com/kardianos/govendor", ""
         s.gsub! "go get github.com/mitchellh/gox", ""
         s.gsub! "go get -u github.com/mna/pigeon", ""
         s.gsub! "go get golang.org/x/tools/cmd/goimports", ""
