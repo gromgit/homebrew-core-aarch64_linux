1fc9467cf347ff92b08c92d9a316befaf31c6b30
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-18 10:48:26 +0000

    cql: add 0.5.0 bottle.

--- Formula/cql.rb
+++ Formula/cql.rb
@@ -5,6 +5,13 @@ class Cql < Formula
   sha256 "ebee3e8fca672d6f3d3d607e380ec7a1f3a31501e5c80b85c660f50eda7a2240"
   head "https://github.com/CovenantSQL/CovenantSQL.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7d2e72c5336cce4e8d6e46c084f2111fc35567dc26ab6cbfc4f5a25a570f73ef" => :mojave
+    sha256 "83aaf67025bcfb0715233bfaa8c4306129d7a025b08d18a465afb47ad630abed" => :high_sierra
+    sha256 "01bfd33b8f17397f446eda57e1a652595c4e63099023c4e7b638ed1f51cd1d88" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
