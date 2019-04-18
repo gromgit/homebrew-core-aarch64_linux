fb0a715053341b68794027a03d0675f92c83fa94
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-18 12:16:23 +0000

    nlohmann-json: add 3.6.1 bottle.

--- Formula/nlohmann-json.rb
+++ Formula/nlohmann-json.rb
@@ -5,6 +5,13 @@ class NlohmannJson < Formula
   sha256 "80c45b090e40bf3d7a7f2a6e9f36206d3ff710acfa8d8cc1f8c763bb3075e22e"
   head "https://github.com/nlohmann/json.git", :branch => "develop"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a1f0ef0b4ad6f160936a31eb6c2ac4344d25483d9a2e85e9c1b9f0b551d52911" => :mojave
+    sha256 "a1f0ef0b4ad6f160936a31eb6c2ac4344d25483d9a2e85e9c1b9f0b551d52911" => :high_sierra
+    sha256 "6550e6790dea47b3af4cbb4b91ef3361612ecf20c46d635807443d71772c8ba8" => :sierra
+  end
+
   depends_on "cmake" => :build
 
   def install
