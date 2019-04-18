e9fa8d3fcf8362b52725fd028814c3668aed2a18
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-18 20:12:18 +0000

    csound: add 6.12.2 bottle.

--- Formula/csound.rb
+++ Formula/csound.rb
@@ -4,6 +4,12 @@ class Csound < Formula
   url "https://github.com/csound/csound/archive/6.12.2.tar.gz"
   sha256 "39f4872b896eb1cbbf596fcacc0f2122fd3e5ebbb5cec14a81b4207d6b8630ff"
 
+  bottle do
+    sha256 "c61580b06971b50952504c4c7ec913998531c1dc253f9f187d2a643ab92d3d55" => :mojave
+    sha256 "8663be5e850243c2fdd345d26741657d261903922784bdfba449a6b9c356bfbc" => :high_sierra
+    sha256 "4cee30249722264ebaca435af3798a2bffa3a88095212e6699e16fc811c03418" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "fltk"
   depends_on "liblo"
