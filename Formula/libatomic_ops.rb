19a8b30a6ba59806ca2bf0fbe395128f8f77d953
FX Coudert <fxcoudert@gmail.com>
2019-04-18 12:55:22 +0200

    libatomic_ops 7.6.10
    Closes #39039.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libatomic_ops.rb
+++ Formula/libatomic_ops.rb
@@ -1,8 +1,8 @@
 class LibatomicOps < Formula
   desc "Implementations for atomic memory update operations"
   homepage "https://github.com/ivmai/libatomic_ops/"
-  url "https://github.com/ivmai/libatomic_ops/releases/download/v7.6.8/libatomic_ops-7.6.8.tar.gz"
-  sha256 "1d6a279edf81767e74d2ad2c9fce09459bc65f12c6525a40b0cb3e53c089f665"
+  url "https://github.com/ivmai/libatomic_ops/releases/download/v7.6.10/libatomic_ops-7.6.10.tar.gz"
+  sha256 "587edf60817f56daf1e1ab38a4b3c729b8e846ff67b4f62a6157183708f099af"
 
   bottle do
     cellar :any_skip_relocation
