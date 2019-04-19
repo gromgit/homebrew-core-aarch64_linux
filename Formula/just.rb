e928d5a08e0d711d8db74d726a29e72a8d2aab0e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-19 17:35:53 +0000

    just: update 0.4.2 bottle.

--- Formula/just.rb
+++ Formula/just.rb
@@ -5,9 +5,10 @@ class Just < Formula
   sha256 "7bcde022994eaa06ce17974f494e4cf98a1d00f4e5791c6ae61f8dd45685da25"
 
   bottle do
-    sha256 "24bb1b62a58e031353ca5509ea3f0fb97de0f05a094e6a69f184a3bf78fd6c3c" => :mojave
-    sha256 "6e64aea89c680ed2c470c3ab7047e2cbb5ac7721c64719b78e1a33de81fd50c3" => :high_sierra
-    sha256 "d4f8aa39aec18902b328a81d03483c724dab02fffe2529c802f5cc6f7dc4a883" => :sierra
+    cellar :any_skip_relocation
+    sha256 "57d43f3d4c61eb0cff35b2bd0f6c6094b1a9437dfab8df79ce7c5d096f97430f" => :mojave
+    sha256 "715fb7af58659ce90e2a6a241f2d622df112488c334c88166bc2b2377b57d6fe" => :high_sierra
+    sha256 "f0956c2e6e4ba10ba1bf3a1182d2641f6cba6091cc08c1d75b8023345dd6c419" => :sierra
   end
 
   depends_on "rust" => :build
