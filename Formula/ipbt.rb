ed1b04914ab5b111b7581a4024e755c2151a573e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-19 06:08:19 +0000

    ipbt: update 20180824 bottle.

--- Formula/ipbt.rb
+++ Formula/ipbt.rb
@@ -8,11 +8,9 @@ class Ipbt < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e6116c578d1327c1c70b7a7b4640360f0a9cbe824b749df0a8a5dcdf67142e6b" => :mojave
-    sha256 "ca5fe4bf38c28b6134ef995643328af82277bac9765064cafa95c7be096cbb51" => :high_sierra
-    sha256 "f9dd24a2a7bd67ee00a4763b6931975f27443d906d91795962ca8a1eb647d269" => :sierra
-    sha256 "3ebaf07effeb98490201a2c8cc38bfdaec25c0db5a6bf3f2936f72b944ee5309" => :el_capitan
-    sha256 "511b256cd2f9eda3db809f2a46c47ed72c7a4fadef4fb16bb5197766cc966fe9" => :yosemite
+    sha256 "bd283b7c7c23687fe8d2805171c1abf50924e53d4072fe5bb3ca96e6fa16c206" => :mojave
+    sha256 "8a353ed14fde241fdc9894083f36298f5374c06c264129b4f1468188633e2467" => :high_sierra
+    sha256 "81dfccfb83c374d509ab37f7ad4cf8cc0d40bfb3b47e45d9d05ea3880e3d03aa" => :sierra
   end
 
   def install
