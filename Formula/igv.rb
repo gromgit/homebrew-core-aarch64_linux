fe4eba7569c6a6052ce1b3ac4319c4e4011606b8
Caleb Xu <calebcenter@live.com>
2019-04-19 00:35:01 -0400

    igv 2.5.1
    Closes #39071.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/igv.rb
+++ Formula/igv.rb
@@ -1,8 +1,8 @@
 class Igv < Formula
   desc "Interactive Genomics Viewer"
   homepage "https://www.broadinstitute.org/software/igv"
-  url "https://data.broadinstitute.org/igv/projects/downloads/2.4/IGV_2.4.18.zip"
-  sha256 "a34e5155b2f162afaa977be17f9241c4d5be2140bf93fa71f205338f2c2cf641"
+  url "https://data.broadinstitute.org/igv/projects/downloads/2.5/IGV_2.5.1.zip"
+  sha256 "568d56300ebca72297db9fbc5c0169817dc9d94f7411b6e2b12ad156d39c89dc"
 
   bottle :unneeded
 
