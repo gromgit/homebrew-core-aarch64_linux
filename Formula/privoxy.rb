18c6367ce57c83953833d81df1711c35b49c2c55
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-19 04:14:37 +0000

    privoxy: update 3.0.28 bottle.

--- Formula/privoxy.rb
+++ Formula/privoxy.rb
@@ -6,12 +6,9 @@ class Privoxy < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "89c7bd4cfc6bce1cdcae9d05bac4a316ec03a32b8ff23640d44b4f76f9a83e5f" => :mojave
-    sha256 "6d73d0568bf02dc31bce3e87a92ed8a90c67d19340ecbc2d22102522dc12a74b" => :high_sierra
-    sha256 "bd606ba22bca049b7f0457cdfa846aefa09eaf2c9d1e18ff3584254e1fc05048" => :sierra
-    sha256 "4aa50d19fa164c7bb5b6a14f5ef562fb9b40acebe8575cd5af4dffac78fa3400" => :el_capitan
-    sha256 "8a0e661df5d221ae65b6367791a923d1f2b769b0206b3fb50c1e6a84f2830d7b" => :yosemite
+    sha256 "01d3b6f679a5819786936626ed093773d68094aa16a8969bf912a507690043f1" => :mojave
+    sha256 "1dfa322367c0f6e5013f2a08fe12a825d4627b2c23aba0aecc94e65e10904700" => :high_sierra
+    sha256 "cd9a919132c032f335f6c7bce15fc5a6abb24fbd56f7ee51884ea30aac710b67" => :sierra
   end
 
   depends_on "autoconf" => :build
