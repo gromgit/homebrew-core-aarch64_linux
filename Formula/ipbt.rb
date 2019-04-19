66b14719e6dfdd0f2a80148f360c1660534ab75d
Caleb Xu <calebcenter@live.com>
2019-04-19 01:17:12 -0400

    ipbt 20180824
    Closes #39077.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/ipbt.rb
+++ Formula/ipbt.rb
@@ -1,10 +1,10 @@
 class Ipbt < Formula
   desc "Program for recording a UNIX terminal session"
   homepage "https://www.chiark.greenend.org.uk/~sgtatham/ipbt/"
-  url "https://www.chiark.greenend.org.uk/~sgtatham/ipbt/ipbt-20170831.3c40fd3.tar.gz"
-  mirror "https://dl.bintray.com/homebrew/mirror/ipbt-20170831.tar.gz"
-  version "20170831"
-  sha256 "237394f7074a8e540495bdcb8b82b5a932f8597505e2ee3ba3d96b0671cfeae3"
+  url "https://www.chiark.greenend.org.uk/~sgtatham/ipbt/ipbt-20180824.dc543d3.tar.gz"
+  mirror "https://dl.bintray.com/homebrew/mirror/ipbt-20180824.tar.gz"
+  version "20180824"
+  sha256 "8347b373252a4c7054d476a39933287a146bb0e0f9dbfe86cd23931cf9eabb5c"
 
   bottle do
     cellar :any_skip_relocation
