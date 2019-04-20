f9dee859ac6763094d28ad3ecc075b366c1682d3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-20 04:38:46 +0000

    knot-resolver: update 3.2.1_1 bottle.

--- Formula/knot-resolver.rb
+++ Formula/knot-resolver.rb
@@ -7,10 +7,9 @@ class KnotResolver < Formula
   head "https://gitlab.labs.nic.cz/knot/knot-resolver.git"
 
   bottle do
-    rebuild 1
-    sha256 "44db61bf058ba0af440c3ebe47fc54d78bde402ea4971d3c2176ce9c9587e910" => :mojave
-    sha256 "32368f405c27c1d738e5050662f2bf8e6f325f0ac474ad548839345d8462c41c" => :high_sierra
-    sha256 "97e6ad5aae70aa156046fdd0e55a19375cabc0f984031c125be6310381515e06" => :sierra
+    sha256 "d10e18db53db4a96db665cb04a04d84d9050e8f2cd4989420bab6dbb13a1f6be" => :mojave
+    sha256 "c6d685f41c1ce47998c825c89c228013e4303fd14271c3a204a13b6ac9008bdc" => :high_sierra
+    sha256 "527254894d8ffb9c97a9e00dfbe19a6509c009d901e4e3be2410ee80636e0991" => :sierra
   end
 
   depends_on "cmocka" => :build
