896c9bc48e4d8ad3117934b5671bc26c49f6eb63
FX Coudert <fxcoudert@gmail.com>
2019-04-20 14:00:26 +0200

    cgal: fix and revision for boost

--- Formula/cgal.rb
+++ Formula/cgal.rb
@@ -3,6 +3,7 @@ class Cgal < Formula
   homepage "https://www.cgal.org/"
   url "https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.14/CGAL-4.14.tar.xz"
   sha256 "59464b1eaee892f2223ba570a7642892c999e29524ab102a6efd7c29c94a29f7"
+  revision 1
 
   bottle do
     cellar :any
@@ -26,6 +27,7 @@ class Cgal < Formula
       -DWITH_CGAL_Qt5=OFF
       -DWITH_CGAL_ImageIO=OFF
     ]
+
     system "cmake", ".", *args
     system "make", "install"
   end
