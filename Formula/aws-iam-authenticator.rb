934bf94e3d0089602e9192d3a69a9f423d28ac35
Scott Crooks <scott.crooks@gmail.com>
2019-04-20 14:30:25 +0200

    aws-iam-authenticator 0.4.0
    Closes #39125.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/aws-iam-authenticator.rb
+++ Formula/aws-iam-authenticator.rb
@@ -1,8 +1,8 @@
 class AwsIamAuthenticator < Formula
   desc "Use AWS IAM credentials to authenticate to Kubernetes"
   homepage "https://github.com/kubernetes-sigs/aws-iam-authenticator"
-  url "https://github.com/kubernetes-sigs/aws-iam-authenticator/archive/v0.3.0.tar.gz"
-  sha256 "91e801c1b1c097f663ebd75897cd5ea19e709bc5ab806e1169ffc508c5a79271"
+  url "https://github.com/kubernetes-sigs/aws-iam-authenticator/archive/v0.4.0.tar.gz"
+  sha256 "d077ce973e5917fab7cbad46bc2d19264e8d0ae23321afd97b1bc481075a31fa"
   bottle do
     cellar :any_skip_relocation
     sha256 "d5a35beb29c75a100afad5bb2706ac57fe5cc92ee56296612c34348c2ea4c8d7" => :mojave
@@ -18,15 +18,9 @@ class AwsIamAuthenticator < Formula
 
     (buildpath/"src/github.com/kubernetes-sigs/aws-iam-authenticator").install buildpath.children
 
-    # this works around an upstream bug
-    # already fixed, not yet released
-    # see https://github.com/kubernetes-sigs/aws-iam-authenticator/commit/678cdff0f43d3ae4a1d9d68a5347042f14a168a8
-    mkdir buildpath/"src/github.com/heptio"
-    ln_s buildpath/"src/github.com/kubernetes-sigs/aws-iam-authenticator", buildpath/"src/github.com/heptio/authenticator"
-
     cd "src/github.com/kubernetes-sigs/aws-iam-authenticator" do
       system "dep", "ensure", "-vendor-only"
-      cd "cmd/heptio-authenticator-aws" do
+      cd "cmd/aws-iam-authenticator" do
         system "go", "build", "-o", "aws-iam-authenticator"
         bin.install "aws-iam-authenticator"
       end
@@ -37,7 +31,7 @@ class AwsIamAuthenticator < Formula
   test do
     system "#{bin}/aws-iam-authenticator", "init", "-i", "test"
     contents = Dir.entries(".")
-    ["cert.pem", "key.pem", "heptio-authenticator-aws.kubeconfig"].each do |created|
+    ["cert.pem", "key.pem", "aws-iam-authenticator.kubeconfig"].each do |created|
       assert_include contents, created
     end
   end
