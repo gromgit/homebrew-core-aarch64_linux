631a757c1234fd37d48596d78a0c6759bc04863c
jfoster <jfoster@users.noreply.github.com>
2019-04-22 00:03:30 +0100

    ios-sim 8.0.1
    Closes #39144.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/ios-sim.rb
+++ Formula/ios-sim.rb
@@ -3,8 +3,8 @@ require "language/node"
 class IosSim < Formula
   desc "Command-line application launcher for the iOS Simulator"
   homepage "https://github.com/phonegap/ios-sim"
-  url "https://registry.npmjs.org/ios-sim/-/ios-sim-7.0.0.tgz"
-  sha256 "6893781e5caa9e036bfe2044b3044c3a937b2b96b5de18bed3e93b7508a87615"
+  url "https://github.com/ios-control/ios-sim/archive/8.0.1.tar.gz"
+  sha256 "aa60e5c8428b6f5a4a75a1e6a5836b92e2f524f3ee93428271c0f342c7fc01f4"
   head "https://github.com/phonegap/ios-sim.git"
 
   bottle do
