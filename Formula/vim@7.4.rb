f9c1bd49e4bbfe74574cc06579acc9f1150c231f
Chongyu Zhu <i@lembacon.com>
2019-04-23 04:32:05 +0800

    vim@7.4: revision for perl

--- Formula/vim@7.4.rb
+++ Formula/vim@7.4.rb
@@ -3,7 +3,7 @@ class VimAT74 < Formula
   homepage "https://www.vim.org/"
   url "https://github.com/vim/vim/archive/v7.4.2367.tar.gz"
   sha256 "a9ae4031ccd73cc60e771e8bf9b3c8b7f10f63a67efce7f61cd694cd8d7cda5c"
-  revision 20
+  revision 21
 
   bottle do
     sha256 "962c40704fd24c488252ffaa01f2019736e70b63e7e14b09c57dbe1eca93ee70" => :mojave
