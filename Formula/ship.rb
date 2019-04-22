764d5047f2ef73073a1b08450a01f270e5863011
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-22 23:53:49 +0000

    ship: update 0.41.0 bottle.

--- Formula/ship.rb
+++ Formula/ship.rb
@@ -6,8 +6,10 @@ class Ship < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9a5713e858d26e422b0b1faa884e8644cff7d21689c18f58d368e86e03f6b226" => :mojave
-    sha256 "df290f68671d175fcf33ffbc91a7c3a2e6cfcca20b1d236b068a1875cb6190cf" => :high_sierra
+    rebuild 1
+    sha256 "6f89b086839d19d642d4c64c19d87be4102b48676c6c3fb1715681597a92edd6" => :mojave
+    sha256 "628e57d3faccdfec9e3d1f90b674c60a1df262cd30f8d64ad5881957f694db39" => :high_sierra
+    sha256 "7cda06b6ab842d5a163a865f40fe4341ba175a421a00f784a066785bdb660828" => :sierra
   end
 
   depends_on "go" => :build
