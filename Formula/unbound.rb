3e0e7801653422c40a5dddad53ed46f2a58244fd
Izaak Beekman <zbeekman@gmail.com>
2019-04-22 12:48:01 -0400

    unbound: fix DOCTYPE in plist file

--- Formula/unbound.rb
+++ Formula/unbound.rb
@@ -46,7 +46,7 @@ class Unbound < Formula
 
   def plist; <<~EOS
     <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-/Apple/DTD PLIST 1.0/EN" "http:/www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
       <dict>
         <key>Label</key>
