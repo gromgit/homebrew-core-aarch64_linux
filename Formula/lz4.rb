d4ea15cb614302f7370aa69cc84b2c55f7bcee2d
Chongyu Zhu <i@lembacon.com>
2019-04-24 03:19:40 +0800

    lz4 1.9.1
    Closes #39193.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/lz4.rb
+++ Formula/lz4.rb
@@ -1,8 +1,8 @@
 class Lz4 < Formula
   desc "Extremely Fast Compression algorithm"
   homepage "https://lz4.org/"
-  url "https://github.com/lz4/lz4/archive/v1.9.0.tar.gz"
-  sha256 "f8b6d5662fa534bd61227d313535721ae41a68c9d84058b7b7d86e143572dcfb"
+  url "https://github.com/lz4/lz4/archive/v1.9.1.tar.gz"
+  sha256 "f8377c89dad5c9f266edc0be9b73595296ecafd5bfa1000de148096c50052dc4"
   head "https://github.com/lz4/lz4.git"
 
   bottle do
@@ -10,6 +10,12 @@ class Lz4 < Formula
     sha256 "0319b38bd566413a2ce58bf49e288411be1c5f38198018f8821d6117426b0c57" => :mojave
     sha256 "5413e031d46bc35f40d6ca17bee04cd382ad1549d4693a0e6a8fa701b346ccc2" => :high_sierra
     sha256 "575ebf1f65b8c9e28c4bd769ed0bfa4ee4f575d2e0cd40d60f2a2041d40b3f21" => :sierra
+  end
+
+  # Pull request submitted 24 Apr 2019 https://github.com/lz4/lz4/pull/694
+  patch do
+    url "https://github.com/lz4/lz4/pull/694.patch?full_index=1"
+    sha256 "9a4a4a94fe27492fe9e90c4c656e6ba7a0476f509433aa559958292ee2147850"
   end
 
   def install
