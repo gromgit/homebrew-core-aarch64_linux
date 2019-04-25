17fda38b329e6e4d1ccd206cf3d30b9c02901f7b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-25 22:02:13 +0000

    hebcal: update 4.17 bottle.

--- Formula/hebcal.rb
+++ Formula/hebcal.rb
@@ -6,9 +6,9 @@ class Hebcal < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "88b3552aa73695025b19df5e10a2debc8cfeec869e514ae4900ea15d06cae695" => :mojave
-    sha256 "1e8b79908fdfaa02b7799faa44c244eacef01725f632fa287a2c3ffe665c3a0b" => :high_sierra
-    sha256 "9d037f49508775a3ebbd42e44e6bb05af4cdf169446b168a2893340994ec618e" => :sierra
+    sha256 "0eb121db050ab5db96e49341acba87c59c62657339b467eccff3cb9d0b7c902c" => :mojave
+    sha256 "cec68da04c7d3f3e3749b48f63d4c3c38c55c2dda778ed4b4c9526e9d61ddf46" => :high_sierra
+    sha256 "63ca2b40ae69ff4527553913ca84113d4cac068f26152c55aa22ec327f299e4e" => :sierra
   end
 
   depends_on "autoconf" => :build
