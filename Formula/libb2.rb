a9506fc1774c04892285dc52f2d6945bf5466824
Caleb Xu <calebcenter@live.com>
2019-04-25 17:46:19 -0400

    libb2 0.98.1
    Closes #39303.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/libb2.rb
+++ Formula/libb2.rb
@@ -1,8 +1,8 @@
 class Libb2 < Formula
   desc "Secure hashing function"
   homepage "https://blake2.net/"
-  url "https://blake2.net/libb2-0.97.tar.gz"
-  sha256 "7829c7309347650239c76af7f15d9391af2587b38f0a65c250104a2efef99051"
+  url "https://github.com/BLAKE2/libb2/releases/download/v0.98.1/libb2-0.98.1.tar.gz"
+  sha256 "53626fddce753c454a3fea581cbbc7fe9bbcf0bc70416d48fdbbf5d87ef6c72e"
 
   bottle do
     cellar :any
