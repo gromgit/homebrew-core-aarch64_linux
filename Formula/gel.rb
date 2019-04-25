a7e26cff24287e30b9121031995001d4839cbba2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-25 19:41:49 +0000

    gel: add 0.2.0 bottle.

--- Formula/gel.rb
+++ Formula/gel.rb
@@ -4,6 +4,13 @@ class Gel < Formula
   url "https://github.com/gel-rb/gel/archive/v0.2.0.tar.gz"
   sha256 "7d69f745986c9c33272f080496aea53719d69d4f465993c740f432ef5f0a3bd3"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ca4a4081014f2baab6285c44e1fddd6975cb33f22f76bddbc5eaa4a3a1956867" => :mojave
+    sha256 "ca4a4081014f2baab6285c44e1fddd6975cb33f22f76bddbc5eaa4a3a1956867" => :high_sierra
+    sha256 "628fbe3459425b7c30690ce3c4e2f5cadd39e2726833e866c54ef4fdb25f30b3" => :sierra
+  end
+
   def install
     ENV["PATH"] = "bin:#{ENV["HOME"]}/.local/gel/bin:#{ENV["PATH"]}"
     system "gel", "install"
