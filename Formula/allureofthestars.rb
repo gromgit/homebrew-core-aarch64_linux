5df6223b587f4fb18300498c5cf6fb9d9c9480f3
Viktor Szakats <commit@vszakats.net>
2019-04-25 10:51:00 +0000

    allureofthestars: secure url(s)

--- Formula/allureofthestars.rb
+++ Formula/allureofthestars.rb
@@ -4,7 +4,7 @@ class Allureofthestars < Formula
   include Language::Haskell::Cabal
 
   desc "Near-future Sci-Fi roguelike and tactical squad combat game"
-  homepage "http://allureofthestars.com"
+  homepage "https://www.allureofthestars.com/"
   url "https://hackage.haskell.org/package/Allure-0.9.4.0/Allure-0.9.4.0.tar.gz"
   sha256 "503cd08dd6dd71d0afe63920b8fa171047449e95a35369dab0936c490d3dabf4"
   head "https://github.com/AllureOfTheStars/Allure.git"
