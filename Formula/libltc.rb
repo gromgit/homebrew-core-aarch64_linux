cbf903a3f14c4edc2bf6420d14ca77812660784e
Igor Kapkov <igasgeek@me.com>
2019-04-26 07:29:42 +1000

    libltc 1.3.1
    Closes #39298.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libltc.rb
+++ Formula/libltc.rb
@@ -1,8 +1,8 @@
 class Libltc < Formula
   desc "POSIX-C Library for handling Linear/Logitudinal Time Code (LTC)"
   homepage "https://x42.github.io/libltc/"
-  url "https://github.com/x42/libltc/releases/download/v1.3.0/libltc-1.3.0.tar.gz"
-  sha256 "aab1de052bc61fbac6ea66d88f04e95b7d5faa1ef297b95ea6a1a548c87dee5c"
+  url "https://github.com/x42/libltc/releases/download/v1.3.1/libltc-1.3.1.tar.gz"
+  sha256 "50e63eb3b767151bc0159a3cc5d426d03a42fd69029bc9b3b7c346555f4b709c"
 
   bottle do
     cellar :any
