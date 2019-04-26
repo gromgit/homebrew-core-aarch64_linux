29ad9145a5f218897cff3120be5d9dccc9afb1a8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-26 15:03:23 +0000

    libfixbuf: update 2.3.1 bottle.

--- Formula/libfixbuf.rb
+++ Formula/libfixbuf.rb
@@ -5,11 +5,9 @@ class Libfixbuf < Formula
   sha256 "2ba7877c5b09c120a20eb320d5d9e2ac93520c8308624eac3064aaece239bad3"
 
   bottle do
-    cellar :any
-    sha256 "c548bdfc48db5ee2b971849ab0bf52c856c96f40de77146f7d796842f8689e99" => :mojave
-    sha256 "8c0d314feb7ee75cfe4ef108d15285e6da0fdc7d3c5bcf0dd2a44c059af42bda" => :high_sierra
-    sha256 "1cd8bd7f2552f30c27120a4a7ba803a2cc95e6b4d356e5489eda61b59ded001c" => :sierra
-    sha256 "92f84cb8a1c4541693ff62533464fe2d04ff6b92c930cb4bb84eab6bac33fedb" => :el_capitan
+    sha256 "e880e6bba9e44a26a962fc5dcec07b1b1083c23d46036f39bc330e4b75616c12" => :mojave
+    sha256 "ae3d11e94915eb3d2bb48061d0d3cc8ec7749cb833137a9648190c486850a5d7" => :high_sierra
+    sha256 "fab9df238cb38319886f2ecd02d724fd481c1634d9d35ef5d616337ad1c1ab87" => :sierra
   end
 
   depends_on "pkg-config" => :build
