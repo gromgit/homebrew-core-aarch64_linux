5a36a2199a70bd4bee480c04d87c2044ef18294c
Isabell (Issy) Long <me@issyl0.co.uk>
2019-04-26 19:59:40 +0100

    webalizer: change mirror URL to deb.debian.org

--- Formula/webalizer.rb
+++ Formula/webalizer.rb
@@ -2,7 +2,7 @@ class Webalizer < Formula
   desc "Web server log file analysis"
   homepage "http://www.webalizer.org"
   url "ftp://ftp.mrunix.net/pub/webalizer/webalizer-2.23-08-src.tgz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/w/webalizer/webalizer_2.23.08.orig.tar.gz"
+  mirror "https://deb.debian.org/debian/pool/main/w/webalizer/webalizer_2.23.08.orig.tar.gz"
   sha256 "edaddb5aa41cc4a081a1500e3fa96615d4b41bc12086bcedf9938018ce79ed8d"
   revision 2
 
