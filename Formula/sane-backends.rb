e664e5466a72e490f2998fb61bcf6b955c333e37
Isabell (Issy) Long <me@issyl0.co.uk>
2019-04-26 19:59:36 +0100

    sane-backends: change mirror URL to deb.debian.org

--- Formula/sane-backends.rb
+++ Formula/sane-backends.rb
@@ -1,7 +1,7 @@
 class SaneBackends < Formula
   desc "Backends for scanner access"
   homepage "http://www.sane-project.org/"
-  url "https://mirrors.kernel.org/debian/pool/main/s/sane-backends/sane-backends_1.0.27.orig.tar.gz"
+  url "https://deb.debian.org/debian/pool/main/s/sane-backends/sane-backends_1.0.27.orig.tar.gz"
   mirror "https://fossies.org/linux/misc/sane-backends-1.0.27.tar.gz"
   sha256 "293747bf37275c424ebb2c833f8588601a60b2f9653945d5a3194875355e36c9"
   revision 5
