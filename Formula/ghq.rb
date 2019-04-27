9979f876a302e99ee5c6b80874b41e3f328451cf
Songmu <y.songmu@gmail.com>
2019-04-27 22:48:08 +0900

    ghq 0.10.0
    Closes #39384.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/ghq.rb
+++ Formula/ghq.rb
@@ -1,10 +1,8 @@
-require "language/go"
-
 class Ghq < Formula
   desc "Remote repository management made easy"
   homepage "https://github.com/motemen/ghq"
-  url "https://github.com/motemen/ghq/archive/v0.8.0.tar.gz"
-  sha256 "e3df98794a423000676987812ded7dab30367dfbc82a2b7a8d2ebbea12fa81b2"
+  url "https://github.com/motemen/ghq/archive/v0.10.0.tar.gz"
+  sha256 "3384dcf14c710405cd66584aa52bb08a85334d70beb4a62b5a4c45af527948c8"
 
   bottle do
     cellar :any_skip_relocation
@@ -16,34 +14,9 @@ class Ghq < Formula
 
   depends_on "go" => :build
 
-  go_resource "github.com/daviddengcn/go-colortext" do
-    url "https://github.com/daviddengcn/go-colortext.git",
-        :revision => "805cee6e0d43c72ba1d4e3275965ff41e0da068a"
-  end
-
-  go_resource "github.com/mitchellh/go-homedir" do
-    url "https://github.com/mitchellh/go-homedir.git",
-        :revision => "b8bc1bf767474819792c23f32d8286a45736f1c6"
-  end
-
-  go_resource "github.com/motemen/go-colorine" do
-    url "https://github.com/motemen/go-colorine.git",
-        :revision => "49ff36b8fa42db28092361cd20dcefd0b03b1472"
-  end
-
-  go_resource "github.com/urfave/cli" do
-    url "https://github.com/urfave/cli.git",
-        :revision => "f017f86fccc5a039a98f23311f34fdf78b014f78"
-  end
-
   def install
-    mkdir_p buildpath/"src/github.com/motemen/"
-    ln_s buildpath, buildpath/"src/github.com/motemen/ghq"
-    ENV["GOPATH"] = buildpath
-    Language::Go.stage_deps resources, buildpath/"src"
-
-    system "go", "build", "-ldflags", "-X main.Version=#{version}",
-                          "-o", bin/"ghq"
+    system "make", "build"
+    bin.install "ghq"
     zsh_completion.install "zsh/_ghq"
   end
 
