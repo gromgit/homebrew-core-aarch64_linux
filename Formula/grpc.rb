3933d934a6eb838d2cc8aa0b467f605ab11c8bca
Igor Kapkov <igasgeek@me.com>
2019-04-27 17:29:20 +1000

    grpc 1.20.1
    Closes #39374.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>
    
    Co-authored-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/grpc.rb
+++ Formula/grpc.rb
@@ -1,8 +1,8 @@
 class Grpc < Formula
   desc "Next generation open source RPC library and framework"
-  homepage "https://www.grpc.io/"
-  url "https://github.com/grpc/grpc/archive/v1.20.0.tar.gz"
-  sha256 "01c5e617d098a33672ddb640d0e50831fb7c613999435e5dcf115021abde6b9a"
+  homepage "https://grpc.io/"
+  url "https://github.com/grpc/grpc/archive/v1.20.1.tar.gz"
+  sha256 "ba8b08a697b66e14af35da07753583cf32ff3d14dcd768f91b1bbe2e6c07c349"
   head "https://github.com/grpc/grpc.git"
 
   bottle do
