0cbba1f5c013f7bd02cfeb5f962298e8477a41c4
Perry Chou <906135+mbcoguno@users.noreply.github.com>
2019-04-28 15:50:49 +0800

    mame 0.209
    * Use Homebrew's asio, gdlm, pugixml and rapidjson instead of mame
      bundled in build stage.
    * Remove sphinx-doc's additional extension in docs config to prevent
      error when loading missing extension.
    * Skip build on Sierra.

--- Formula/mame.rb
+++ Formula/mame.rb
@@ -1,9 +1,9 @@
 class Mame < Formula
   desc "Multiple Arcade Machine Emulator"
   homepage "https://mamedev.org/"
-  url "https://github.com/mamedev/mame/archive/mame0205.tar.gz"
-  version "0.205"
-  sha256 "80b7f9feb3a4da34c5c452de13d4f7db12381b8a17a90f41884ea2ca797d92ff"
+  url "https://github.com/mamedev/mame/archive/mame0209.tar.gz"
+  version "0.209"
+  sha256 "9442e88bd87cfe407eb093a2ecb42a3850cabe31cd52c4efdef1bf7f584a8eab"
   head "https://github.com/mamedev/mame.git"
 
   bottle do
@@ -13,39 +13,51 @@ class Mame < Formula
     sha256 "7c7c759a4be74b6dc3f6ab28cee7c242e497244d7161b026f7b659a7caf1fac0" => :sierra
   end
 
+  depends_on "asio" => :build
+  depends_on "glm" => :build
   depends_on "pkg-config" => :build
+  depends_on "pugixml" => :build
+  depends_on "rapidjson" => :build
   depends_on "sphinx-doc" => :build
   depends_on "flac"
   depends_on "jpeg"
   depends_on "lua"
-  depends_on :macos => :yosemite
+  # Need C++ compiler and standard library support C++14.
+  # Build failure on Sierra, see:
+  # https://github.com/Homebrew/homebrew-core/pull/39388
+  depends_on :macos => :high_sierra
   depends_on "portaudio"
   depends_on "portmidi"
   depends_on "sdl2"
   depends_on "sqlite"
   depends_on "utf8proc"
 
-  # Need C++ compiler and standard library support C++14.
-
   def install
     inreplace "scripts/src/osd/sdl.lua", "--static", ""
 
-    # 3rdparty/sol2/sol/compatibility/version.hpp:30:10
-    # fatal error: 'lua.hpp' file not found
+    # Mame's build system genie can't find headers and libraries with version suffix.
     ENV.append "CPPFLAGS", "-I#{Formula["lua"].opt_include}/lua"
+    ENV.append "CPPFLAGS", "-I#{Formula["pugixml"].opt_include}/pugixml-1.9"
+    ENV.append "LDFLAGS", "-L#{Formula["pugixml"].opt_lib}/pugixml-1.9"
 
     system "make", "USE_LIBSDL=1",
                    "USE_SYSTEM_LIB_EXPAT=1",
                    "USE_SYSTEM_LIB_ZLIB=1",
-                   "USE_SYSTEM_LIB_JPEG=1",
+                   "USE_SYSTEM_LIB_ASIO=1",
                    "USE_SYSTEM_LIB_FLAC=1",
+                   "USE_SYSTEM_LIB_GLM=1",
+                   "USE_SYSTEM_LIB_JPEG=1",
                    "USE_SYSTEM_LIB_LUA=1",
-                   "USE_SYSTEM_LIB_SQLITE3=1",
                    "USE_SYSTEM_LIB_PORTMIDI=1",
                    "USE_SYSTEM_LIB_PORTAUDIO=1",
+                   "USE_SYSTEM_LIB_PUGIXML=1",
+                   "USE_SYSTEM_LIB_RAPIDJSON=1",
+                   "USE_SYSTEM_LIB_SQLITE3=1",
                    "USE_SYSTEM_LIB_UTF8PROC=1"
     bin.install "mame64" => "mame"
     cd "docs" do
+      # We don't convert SVG files into PDF files, don't load the related extensions.
+      inreplace "source/conf.py", "'sphinxcontrib.rsvgconverter'", ""
       system "make", "text"
       doc.install Dir["build/text/*"]
       system "make", "man"
