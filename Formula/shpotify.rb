7339c5e578845bcb99eedb4f1c370cf8c326b0b2
dmofot@gmail.com <dmofot@gmail.com>
2019-04-29 15:40:11 -0400

    shpotify 2.1
    Closes #39417.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/shpotify.rb
+++ Formula/shpotify.rb
@@ -1,8 +1,8 @@
 class Shpotify < Formula
   desc "Command-line interface for Spotify on a Mac"
   homepage "https://harishnarayanan.org/projects/shpotify/"
-  url "https://github.com/hnarayanan/shpotify/archive/2.0.3.tar.gz"
-  sha256 "d3e51719fb039ad70a16b78994ded68eaa991b4328df0f5d8b16a6e87393cae3"
+  url "https://github.com/hnarayanan/shpotify/archive/2.1.tar.gz"
+  sha256 "b41d8798687be250d0306ac0c5a79420fa46619c5954286711a5d63c86a6c071"
   head "https://github.com/hnarayanan/shpotify.git"
 
   bottle :unneeded
