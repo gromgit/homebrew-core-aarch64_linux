16064b3090fa0073dce9fc44d3939aa9e98a4947
Paul Klemm <paulklemm@t-online.de>
2019-04-30 09:31:43 +0200

    proteinortho 6.0 (new formula)

---
+++ Formula/proteinortho.rb
@@ -0,0 +1,20 @@
+class Proteinortho < Formula
+  desc "Detecting orthologous genes within different species"
+  homepage "https://gitlab.com/paulklemm_PHD/proteinortho"
+  url "https://gitlab.com/paulklemm_PHD/proteinortho/-/archive/v6.0/proteinortho-v6.0.tar.gz"
+  sha256 "c49337f8b737408deaa04a391595cce892415fd1aa8623eb3c88507b1b8ed3b1"
+
+  depends_on "diamond"
+  depends_on "openblas"
+
+  def install
+    bin.mkpath
+    system "make", "install", "PREFIX=#{bin}"
+    doc.install "manual.html"
+  end
+
+  test do
+    system "#{bin}/proteinortho", "-test"
+    system "#{bin}/proteinortho_clustering", "-test"
+  end
+end
