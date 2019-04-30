2aa3fe22f27386e27d86e61ceecb2a741423dfa4
Reece Como <reece@hubr.io>
2019-04-30 23:39:15 +0800

    bluepill 4.1.1
    Closes #39434.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/bluepill.rb
+++ Formula/bluepill.rb
@@ -1,8 +1,8 @@
 class Bluepill < Formula
   desc "iOS testing tool that runs UI tests using multiple simulators"
   homepage "https://github.com/linkedin/bluepill"
-  url "https://github.com/linkedin/bluepill/archive/v3.1.1.tar.gz"
-  sha256 "35666939199643261cd50ec1044fc978a1835e3c77fe6cb0e3d49b4567d7ed26"
+  url "https://github.com/linkedin/bluepill/archive/v4.1.1.tar.gz"
+  sha256 "960d5fc980c5f92f9b05211b78ae1cf5798289da29e87bfe55af86e7b1876a18"
   head "https://github.com/linkedin/bluepill.git"
 
   bottle do
