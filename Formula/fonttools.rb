406a96fc1a25e3b68e1c9345213875d47cbaa9ce
Chongyu Zhu <i@lembacon.com>
2019-05-01 02:51:39 +0800

    fonttools 3.41.0
    Closes #39437.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -3,8 +3,8 @@ class Fonttools < Formula
 
   desc "Library for manipulating fonts"
   homepage "https://github.com/fonttools/fonttools"
-  url "https://github.com/fonttools/fonttools/releases/download/3.40.0/fonttools-3.40.0.zip"
-  sha256 "b1c5f2aa4ed4849ec62f32154118aa46429dc95d8be22d3941369ec9a79ca787"
+  url "https://github.com/fonttools/fonttools/releases/download/3.41.0/fonttools-3.41.0.zip"
+  sha256 "d0113965f0c31717a8a585184ae449cd35c103d5f26516a5ced378de944e78b8"
   head "https://github.com/fonttools/fonttools.git"
 
   bottle do
