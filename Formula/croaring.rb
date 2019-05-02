a14d6abe2d6aa52466bc963923e044a197724c8d
Caleb Xu <calebcenter@live.com>
2019-05-02 00:23:22 -0400

    croaring 0.2.66 (new formula)
    Closes #58071.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: chenrui <chenrui333@gmail.com>

---
+++ Formula/croaring.rb
@@ -0,0 +1,35 @@
+class Croaring < Formula
+  desc "Roaring bitmaps in C (and C++)"
+  homepage "https://roaringbitmap.org"
+  url "https://github.com/RoaringBitmap/CRoaring/archive/v0.2.66.tar.gz"
+  sha256 "df98bd8f6ff09097ada529a004af758ff4d33faf6a06fadf8fad9a6533afc241"
+  license "Apache-2.0"
+  head "https://github.com/RoaringBitmap/CRoaring.git"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+    system "make", "clean"
+    system "cmake", ".", "-DROARING_BUILD_STATIC=ON", *std_cmake_args
+    system "make"
+    lib.install "src/libroaring.a"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <roaring/roaring.h>
+      int main() {
+          roaring_bitmap_t *r1 = roaring_bitmap_create();
+          for (uint32_t i = 100; i < 1000; i++) roaring_bitmap_add(r1, i);
+          printf("cardinality = %d\\n", (int) roaring_bitmap_get_cardinality(r1));
+          roaring_bitmap_free(r1);
+          return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lroaring", "-o", "test"
+    assert_equal "cardinality = 900\n", shell_output("./test")
+  end
+end
