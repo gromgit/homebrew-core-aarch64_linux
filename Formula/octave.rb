06473a6f04eb075f5b86696aca5a5305bddeee78
FX Coudert <fxcoudert@gmail.com>
2019-05-03 14:21:15 +0200

    octave: revision for gcc

--- Formula/octave.rb
+++ Formula/octave.rb
@@ -4,7 +4,7 @@ class Octave < Formula
   url "https://ftp.gnu.org/gnu/octave/octave-5.1.0.tar.xz"
   mirror "https://ftpmirror.gnu.org/octave/octave-5.1.0.tar.xz"
   sha256 "87b4df6dfa28b1f8028f69659f7a1cabd50adfb81e1e02212ff22c863a29454e"
-  revision 2
+  revision 3
 
   bottle do
     sha256 "6bb8497839d6f7872efcd6acad0216f443420e097a9b7fad44835823e1c0e735" => :mojave
