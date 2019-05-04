3d02fb4ba08dbfac93a3b25c657a836ba0dd78b4
Viktor Szakats <commit@vszakats.net>
2019-05-04 06:48:53 +0000

    s3-backer: switch to virtual-hosted style URLs for S3

--- Formula/s3-backer.rb
+++ Formula/s3-backer.rb
@@ -1,7 +1,7 @@
 class S3Backer < Formula
   desc "FUSE-based single file backing store via Amazon S3"
   homepage "https://github.com/archiecobbs/s3backer"
-  url "https://s3.amazonaws.com/archie-public/s3backer/s3backer-1.5.0.tar.gz"
+  url "https://archie-public.s3.amazonaws.com/s3backer/s3backer-1.5.0.tar.gz"
   sha256 "82d93c54acb1e85828b6b80a06e69a99c7e06bf6ee025dac720e980590d220d2"
 
   bottle do
