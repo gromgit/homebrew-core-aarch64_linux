57e8a7346e26368833e881dbcbe62e07d3e0b530
Isabell (Issy) Long <me@issyl0.co.uk>
2019-05-06 13:21:41 +0100

    dvorak7min: change url to deb.debian.org from mirrors.ocf.berkeley.edu

--- Formula/dvorak7min.rb
+++ Formula/dvorak7min.rb
@@ -1,8 +1,7 @@
 class Dvorak7min < Formula
   desc "Dvorak (simplified keyboard layout) typing tutor"
   homepage "https://web.archive.org/web/dvorak7min.sourcearchive.com/"
-  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/d/dvorak7min/dvorak7min_1.6.1+repack.orig.tar.gz"
-  mirror "https://deb.debian.org/debian/pool/main/d/dvorak7min/dvorak7min_1.6.1+repack.orig.tar.gz"
+  url "https://deb.debian.org/debian/pool/main/d/dvorak7min/dvorak7min_1.6.1+repack.orig.tar.gz"
   version "1.6.1"
   sha256 "4cdef8e4c8c74c28dacd185d1062bfa752a58447772627aded9ac0c87a3b8797"
 
