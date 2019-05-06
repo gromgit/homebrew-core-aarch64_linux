e60f3371d24bb694f17f1a4b248a3b613e46ebd2
Isabell (Issy) Long <me@issyl0.co.uk>
2019-05-06 14:06:42 +0100

    urlview: change mirror URL to deb.debian.org

--- Formula/urlview.rb
+++ Formula/urlview.rb
@@ -1,8 +1,7 @@
 class Urlview < Formula
   desc "URL extractor/launcher"
   homepage "https://packages.debian.org/sid/misc/urlview"
-  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/u/urlview/urlview_0.9.orig.tar.gz"
-  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/u/urlview/urlview_0.9.orig.tar.gz"
+  url "https://deb.debian.org/debian/pool/main/u/urlview/urlview_0.9.orig.tar.gz"
   version "0.9-20"
   sha256 "746ff540ccf601645f500ee7743f443caf987d6380e61e5249fc15f7a455ed42"
 
@@ -15,8 +14,7 @@ class Urlview < Formula
   end
 
   patch do
-    url "https://mirrors.ocf.berkeley.edu/debian/pool/main/u/urlview/urlview_0.9-20.diff.gz"
-    mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/u/urlview/urlview_0.9-20.diff.gz"
+    url "https://deb.debian.org/debian/pool/main/u/urlview/urlview_0.9-20.diff.gz"
     sha256 "0707956fd7195aefe6d6ff2eaabe8946e3d18821a1ce97c0f48d0f8d6e37514e"
   end
 
