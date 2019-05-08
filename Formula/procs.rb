52bbc0d2e5bf3b643684b4ebddbf1aab1b4c9918
dalance <dalance@gmail.com>
2019-05-08 13:45:40 +0900

    procs 0.8.5 (new formula)
    Closes #39584.
    
    Signed-off-by: Sean Molenaar <smillerdev@me.com>

---
+++ Formula/procs.rb
@@ -0,0 +1,19 @@
+class Procs < Formula
+  desc "Modern replacement for ps written by Rust"
+  homepage "https://github.com/dalance/procs"
+  url "https://github.com/dalance/procs/archive/v0.8.5.tar.gz"
+  sha256 "981ff89f8034616a86c4d8bef08a9d835d7d21e0681fd7a39fbf8fb83b7e015c"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--root", prefix, "--path", "."
+  end
+
+  test do
+    output = shell_output("#{bin}/procs")
+    count = output.lines.count
+    assert count > 2
+    assert output.start_with?(" PID:")
+  end
+end
