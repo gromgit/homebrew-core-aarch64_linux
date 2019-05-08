41bbd06aa082dd4226b2918f12eb7411ac66b5b9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-08 08:18:43 +0000

    pgplot: update 5.2.2_8 bottle.

--- Formula/pgplot.rb
+++ Formula/pgplot.rb
@@ -9,10 +9,10 @@ class Pgplot < Formula
   revision 8
 
   bottle do
-    sha256 "6cad7a8a0a86d1da33229741e3eaef5076f7d74b543e86c804c4be8f7e87dc7f" => :mojave
-    sha256 "6d5b648916ed3ba123ef88b51634ddb83e095cf6439fa8ed4b2ac948e8da57b7" => :high_sierra
-    sha256 "ae03d19097ec74e38a52ccef1782da14024cbd5df13f2d1b58cab1d0b1e43ee2" => :sierra
-    sha256 "91bd322f1fd2220e95fddad56b024c93ae6f446cd501cb6fbeafa4619fee9588" => :el_capitan
+    cellar :any
+    sha256 "3d1afcf5d6a2dbd3a0707a984aa173787f1e58ed8b75139464d59bc28d9f31c4" => :mojave
+    sha256 "e38e9fca27499543c9239d9c655c1cf328364d127aa028d48c6a92a19d85c41f" => :high_sierra
+    sha256 "70aa46b991b8f502aa5c73c6fb56a0f9851396c147384ebd40a4b316d6c1c196" => :sierra
   end
 
   depends_on "gcc" # for gfortran
