4a7133758df1cd63287f384fd8ecba9ffe2dc131
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-08 07:29:32 +0000

    ngspice: update 30_1 bottle.

--- Formula/ngspice.rb
+++ Formula/ngspice.rb
@@ -6,9 +6,10 @@ class Ngspice < Formula
   revision 1
 
   bottle do
-    sha256 "3fe040d87dbefc7c99c284e302ec32090174eaf2f5f1627828f912314f056c10" => :mojave
-    sha256 "f98ab7a1785fd27815f3d974eff684c7573881486f5b18d6230ce871ce30dbc6" => :high_sierra
-    sha256 "3b8b53596b5ce1d72961220049fa3f9c1b33fb0a57eaac564feb5448ed89cdb7" => :sierra
+    rebuild 1
+    sha256 "ad1218af03e9711b74cbf8919b8ef9c77ec216ee43d40648423c5043f4feb393" => :mojave
+    sha256 "1241cc934814f62c3abcc148817b1eb10ec84db5a7ec65c7d0f5316e4bb7f831" => :high_sierra
+    sha256 "89983540f2878f500431a0bec70dfcc2bcfc695e382499586e7f44189a978caa" => :sierra
   end
 
   head do
