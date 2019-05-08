ca6c41a3920efd459eaaffa1fde17c7e9a6f97ab
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-08 18:47:21 +0000

    travis: update 1.8.10 bottle.

--- Formula/travis.rb
+++ Formula/travis.rb
@@ -5,9 +5,10 @@ class Travis < Formula
   sha256 "77f43de7c1e686e2b8eca3f467047de9687c4021c4a12f46dcf1e0f3e63a96c3"
 
   bottle do
-    sha256 "d695085c0886f6db8d5a1afeb6cae27e61d7fcc41d2e069c16ce0909048dba64" => :mojave
-    sha256 "32634e86fdc04e7ca0df1834cf1ee6e8cdd3bfb0f89c0e4ed36a0d55b08483b3" => :high_sierra
-    sha256 "1d21554de55885fd08e4433772663410044f088cf346b63533142019a2d865f5" => :sierra
+    cellar :any
+    sha256 "42d8323b7ebb3687b3ea96b1051fd9e02c22732ebd2964d6bea064fb443f99ba" => :mojave
+    sha256 "d66aa769ff2b66c634ead1d76966015f852d39c3b6baf5ef01e80f2da5c66c45" => :high_sierra
+    sha256 "01b6473c4a5d559e0920c8badd3fd182d0ce9896db2e0017419176814c800d99" => :sierra
   end
 
   depends_on "ruby" if MacOS.version <= :sierra
