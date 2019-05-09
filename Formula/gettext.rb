8624056cacf707faa38357d9210e36ed9f0ea717
Chongyu Zhu <i@lembacon.com>
2019-05-10 02:15:59 +0800

    gettext 0.20.1

--- Formula/gettext.rb
+++ Formula/gettext.rb
@@ -1,9 +1,9 @@
 class Gettext < Formula
   desc "GNU internationalization (i18n) and localization (l10n) library"
   homepage "https://www.gnu.org/software/gettext/"
-  url "https://ftp.gnu.org/gnu/gettext/gettext-0.19.8.1.tar.xz"
-  mirror "https://ftpmirror.gnu.org/gettext/gettext-0.19.8.1.tar.xz"
-  sha256 "105556dbc5c3fbbc2aa0edb46d22d055748b6f5c7cd7a8d99f8e7eb84e938be4"
+  url "https://ftp.gnu.org/gnu/gettext/gettext-0.20.1.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gettext/gettext-0.20.1.tar.xz"
+  sha256 "53f02fbbec9e798b0faaf7c73272f83608e835c6288dd58be6c9bb54624a3800"
 
   bottle do
     sha256 "afc6a6120632b98d58b11fab82ae5e081206b89684dd948abf2d29caeb813ffd" => :mojave
