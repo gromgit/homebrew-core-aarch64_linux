e51d22444cb85a78c8a0e56f70aef561131c7cbd
Thierry Moisan <thierry.moisan@gmail.com>
2019-05-08 22:13:02 -0400

    qpdf 8.4.1
    Closes #39608.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- Formula/qpdf.rb
+++ Formula/qpdf.rb
@@ -1,8 +1,8 @@
 class Qpdf < Formula
   desc "Tools for and transforming and inspecting PDF files"
   homepage "https://github.com/qpdf/qpdf"
-  url "https://github.com/qpdf/qpdf/releases/download/release-qpdf-8.4.0/qpdf-8.4.0.tar.gz"
-  sha256 "39018f3bff700c68e41f6d44ea9f7842e2a5af200a80b2cdec7fa32a4abac4a0"
+  url "https://github.com/qpdf/qpdf/releases/download/release-qpdf-8.4.1/qpdf-8.4.1.tar.gz"
+  sha256 "07fd02b8b32f760ea86c1999f5a67086126437ea854b4064f02c55dbd0f04ebb"
 
   bottle do
     cellar :any
