dcf5c0577ed6e9b21567eef3c4e05804aea2f736
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-05-09 20:11:28 +0100

    gtk-doc 1.30
    Closes #39632.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/gtk-doc.rb
+++ Formula/gtk-doc.rb
@@ -1,9 +1,8 @@
 class GtkDoc < Formula
   desc "GTK+ documentation tool"
   homepage "https://www.gtk.org/gtk-doc/"
-  url "https://download.gnome.org/sources/gtk-doc/1.28/gtk-doc-1.28.tar.xz"
-  sha256 "911e29e302252c96128965ee1f4067d5431a88e00ad1023a8bc1d6b922af5715"
-  revision 1
+  url "https://download.gnome.org/sources/gtk-doc/1.30/gtk-doc-1.30.tar.xz"
+  sha256 "a4f6448eb838ccd30d76a33b1fd095f81aea361f03b12c7b23df181d21b7069e"
 
   bottle do
     cellar :any_skip_relocation
@@ -21,18 +20,7 @@ class GtkDoc < Formula
   depends_on "python"
   depends_on "source-highlight"
 
-  resource "six" do
-    url "https://files.pythonhosted.org/packages/16/d8/bc6316cf98419719bd59c91742194c111b6f2e85abac88e496adefaf7afe/six-1.11.0.tar.gz"
-    sha256 "70e8a77beed4562e7f14fe23a786b54f6296e34344c23bc42f07b15018ff98e9"
-  end
-
   def install
-    xy = Language::Python.major_minor_version "python3"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
-    resource("six").stage do
-      system "python3", *Language::Python.setup_install_args(libexec/"vendor")
-    end
-
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
