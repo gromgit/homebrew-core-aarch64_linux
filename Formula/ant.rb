498466a587c00faa56297db7d9f69d08061a89cc
Thierry Moisan <thierry.moisan@gmail.com>
2019-05-08 22:00:35 -0400

    ant 1.10.6 (#39603)

--- Formula/ant.rb
+++ Formula/ant.rb
@@ -1,8 +1,8 @@
 class Ant < Formula
   desc "Java build tool"
   homepage "https://ant.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=ant/binaries/apache-ant-1.10.5-bin.tar.xz"
-  sha256 "cebb705dbbe26a41d359b8be08ec066caba4e8686670070ce44bbf2b57ae113f"
+  url "https://www.apache.org/dyn/closer.cgi?path=ant/binaries/apache-ant-1.10.6-bin.tar.xz"
+  sha256 "9fc0aef3247485cc406f7bb24d922706e627fca53aed00f2c76810536045d5ec"
   head "https://git-wip-us.apache.org/repos/asf/ant.git"
 
   bottle :unneeded
