77de5a493101cd17327b4b329dafe471081ab8ac
Viktor Szakats <commit@vszakats.net>
2019-05-10 00:22:41 +0000

    libgadu: secure url(s)

--- Formula/libgadu.rb
+++ Formula/libgadu.rb
@@ -1,6 +1,6 @@
 class Libgadu < Formula
   desc "Library for ICQ instant messenger protocol"
-  homepage "http://libgadu.net/"
+  homepage "https://libgadu.net/"
   url "https://github.com/wojtekka/libgadu/releases/download/1.12.2/libgadu-1.12.2.tar.gz"
   sha256 "28e70fb3d56ed01c01eb3a4c099cc84315d2255869ecf08e9af32c41d4cbbf5d"
 
