7f9841880febc843964bbd1ffc4920957c2406a0
FX Coudert <fxcoudert@gmail.com>
2019-05-10 09:12:22 +0200

    libcroco: revision for gettext

--- Formula/libcroco.rb
+++ Formula/libcroco.rb
@@ -3,6 +3,7 @@ class Libcroco < Formula
   homepage "http://www.linuxfromscratch.org/blfs/view/svn/general/libcroco.html"
   url "https://download.gnome.org/sources/libcroco/0.6/libcroco-0.6.13.tar.xz"
   sha256 "767ec234ae7aa684695b3a735548224888132e063f92db585759b422570621d4"
+  revision 1
 
   bottle do
     cellar :any
