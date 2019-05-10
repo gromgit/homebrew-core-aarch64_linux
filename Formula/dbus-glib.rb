a3ee218a97dfefb380030a436e3f539f0af5ff3a
FX Coudert <fxcoudert@gmail.com>
2019-05-10 09:12:14 +0200

    dbus-glib: revision for gettext

--- Formula/dbus-glib.rb
+++ Formula/dbus-glib.rb
@@ -3,6 +3,7 @@ class DbusGlib < Formula
   homepage "https://wiki.freedesktop.org/www/Software/DBusBindings/"
   url "https://dbus.freedesktop.org/releases/dbus-glib/dbus-glib-0.110.tar.gz"
   sha256 "7ce4760cf66c69148f6bd6c92feaabb8812dee30846b24cd0f7395c436d7e825"
+  revision 1
 
   bottle do
     cellar :any
