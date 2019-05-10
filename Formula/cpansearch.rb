e489cd73900892d5706410eeed559482650e5dbd
FX Coudert <fxcoudert@gmail.com>
2019-05-10 09:12:14 +0200

    cpansearch: revision for gettext

--- Formula/cpansearch.rb
+++ Formula/cpansearch.rb
@@ -3,6 +3,7 @@ class Cpansearch < Formula
   homepage "https://github.com/c9s/cpansearch"
   url "https://github.com/c9s/cpansearch/archive/0.2.tar.gz"
   sha256 "09e631f361766fcacd608a0f5b3effe7b66b3a9e0970a458d418d58b8f3f2a74"
+  revision 1
   head "https://github.com/c9s/cpansearch.git"
 
   bottle do
