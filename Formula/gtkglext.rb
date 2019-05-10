1c1e3b28f5372633f8f0ddc2190586d838e60948
FX Coudert <fxcoudert@gmail.com>
2019-05-10 09:12:20 +0200

    gtkglext: revision for gettext

--- Formula/gtkglext.rb
+++ Formula/gtkglext.rb
@@ -3,7 +3,7 @@ class Gtkglext < Formula
   homepage "https://projects.gnome.org/gtkglext/"
   url "https://download.gnome.org/sources/gtkglext/1.2/gtkglext-1.2.0.tar.gz"
   sha256 "e5073f3c6b816e7fa67d359d9745a5bb5de94a628ac85f624c992925a46844f9"
-  revision 2
+  revision 3
 
   bottle do
     cellar :any
