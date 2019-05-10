d01be364c218a997c1d60688706da914ca5ff99b
FX Coudert <fxcoudert@gmail.com>
2019-05-10 09:12:24 +0200

    libpst: revision for gettext

--- Formula/libpst.rb
+++ Formula/libpst.rb
@@ -3,7 +3,7 @@ class Libpst < Formula
   homepage "https://www.five-ten-sg.com/libpst/"
   url "https://www.five-ten-sg.com/libpst/packages/libpst-0.6.72.tar.gz"
   sha256 "8a19d891eb077091c507d98ed8e2d24b7f48b3e82743bcce2b00a12040f5d507"
-  revision 2
+  revision 3
 
   bottle do
     cellar :any
