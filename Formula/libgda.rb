3a2394501229c10968a2f32c1d277d3debfcbf15
FX Coudert <fxcoudert@gmail.com>
2019-05-10 09:12:23 +0200

    libgda: revision for gettext

--- Formula/libgda.rb
+++ Formula/libgda.rb
@@ -3,6 +3,7 @@ class Libgda < Formula
   homepage "https://www.gnome-db.org/"
   url "https://download.gnome.org/sources/libgda/5.2/libgda-5.2.9.tar.xz"
   sha256 "59caed8ca72b1ac6437c9844f0677f8a296d52cfd1c0049116026abfb1d87d9b"
+  revision 1
 
   bottle do
     sha256 "0eef60c3caf722f54a6930f425b4579439145fa418640ac03fe949ebd0d14ce9" => :mojave
