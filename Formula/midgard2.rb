92ce015e1c2255b05ae3551f18c2bb313985ef2f
FX Coudert <fxcoudert@gmail.com>
2019-05-10 09:12:26 +0200

    midgard2: revision for gettext

--- Formula/midgard2.rb
+++ Formula/midgard2.rb
@@ -3,7 +3,7 @@ class Midgard2 < Formula
   homepage "http://www.midgard-project.org/"
   url "https://github.com/downloads/midgardproject/midgard-core/midgard2-core-12.09.tar.gz"
   sha256 "7c1d17e061df8f3b39fd8944ab97ab7220219b470f7874e74471702d2caca2cb"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "2172742db4d9b9a311cce2d5d96a31ddaa3b924361b5e524ab1c841cf0a3f8fa" => :mojave
