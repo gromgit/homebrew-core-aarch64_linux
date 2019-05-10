171e71f7103a1eda495418cb503547d0fd867548
FX Coudert <fxcoudert@gmail.com>
2019-05-10 09:12:18 +0200

    gpa: revision for gettext

--- Formula/gpa.rb
+++ Formula/gpa.rb
@@ -4,6 +4,7 @@ class Gpa < Formula
   url "https://gnupg.org/ftp/gcrypt/gpa/gpa-0.10.0.tar.bz2"
   mirror "https://deb.debian.org/debian/pool/main/g/gpa/gpa_0.10.0.orig.tar.bz2"
   sha256 "95dbabe75fa5c8dc47e3acf2df7a51cee096051e5a842b4c9b6d61e40a6177b1"
+  revision 1
 
   bottle do
     sha256 "fa40434b7e39fb6fd15f67ea757c9b18f3e3c0cdb0eb533940517d51793e3cfd" => :mojave
