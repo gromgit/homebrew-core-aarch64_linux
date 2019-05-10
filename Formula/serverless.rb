991a579a14aa87feefb0d95044a02c14c85dab2f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-10 18:58:54 +0000

    serverless: update 1.42.2 bottle.

--- Formula/serverless.rb
+++ Formula/serverless.rb
@@ -8,9 +8,9 @@ class Serverless < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "97885f316266948c830f218e99260457427de0bd6f2f8208b110a673dbdc0883" => :mojave
-    sha256 "9586bffb00fefe95b7827d86bbe084e2e65da9c6908a57cd0a2585bdd315a8c5" => :high_sierra
-    sha256 "0f89e059d344dd9e127476b988545675ef342073513806b2738729447ca1d06d" => :sierra
+    sha256 "0b4d308424deff33b019671b2bb3d5f2e7287822da26c258ff8e1bc6d8832abc" => :mojave
+    sha256 "ada91f7cc9cbebd27ebdd19d77150981a3449cf1a331698493b360c75bd39c00" => :high_sierra
+    sha256 "91e4cee04342773e03fa8fc62bd50dfefb69785806fd401abc42f64a7d89cacf" => :sierra
   end
 
   depends_on "node"
