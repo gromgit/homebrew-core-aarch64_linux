b3baabe43bff39d89a14c7eec54287027b8b16c5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-11 04:01:46 +0000

    jhipster: update 6.0.1 bottle.

--- Formula/jhipster.rb
+++ Formula/jhipster.rb
@@ -8,9 +8,9 @@ class Jhipster < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b6b825762f8c2fca4bf93bd5ff250cbbe0b1c30774ca90d9128a47595f2b9049" => :mojave
-    sha256 "eed470613019026525142d32f5ac228c909dafc5ab3dae2ccce7abff6f5587d3" => :high_sierra
-    sha256 "cba4b884da504ce15abaac2e692c59fa50068282d3e38ae5ec96756dafec0386" => :sierra
+    sha256 "f858d17b0122f8bf26697940b2faecdaf01bf16f938080b85d83b83c600fb1c3" => :mojave
+    sha256 "b78918e8ad154c54dc3d69fcded024bdf9008093ebd98a7a73adc5e9b9fc5a77" => :high_sierra
+    sha256 "480f30288cd1400a8b7a289431e3f47b1297efa9a0d940fc04416ee5bd345b71" => :sierra
   end
 
   depends_on :java => "1.8+"
