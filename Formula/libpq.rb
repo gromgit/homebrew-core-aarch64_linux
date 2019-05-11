c6672c44fbe2a45462a6c26aabcebdbb40422d91
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-11 16:51:47 +0000

    libpq: update 11.3 bottle.

--- Formula/libpq.rb
+++ Formula/libpq.rb
@@ -5,9 +5,9 @@ class Libpq < Formula
   sha256 "2a85e082fc225944821dfd23990e32dfcd2284c19060864b0ad4ca537d30522d"
 
   bottle do
-    sha256 "7683b433dcc0a91262c223e58b5881339d9c8d424642f5fa27ad4db565ad8f36" => :mojave
-    sha256 "7748348f3edfaf8b3838d80374ab80f715dd7e0bbb45b4a75af5994ac2582d29" => :high_sierra
-    sha256 "1abee44829b2d83b96c144d0c54bb0efe1d4f2d05bdb0236965a32df2c23c69f" => :sierra
+    sha256 "464e74593da2d7b8c9375d74346e7d6216c61d0d37359e78d2fc354e3e84e4d0" => :mojave
+    sha256 "ba645036b965e3e777d3c2c3b73078e67e40d4a215db26caff3e27536c7381a3" => :high_sierra
+    sha256 "825b052e67e74725c99a7539fbcfccd0111d7f288e67559f0a5c634e9d83704e" => :sierra
   end
 
   keg_only "conflicts with postgres formula"
