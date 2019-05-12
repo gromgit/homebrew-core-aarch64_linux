e771d2b44e807ec99ba6f40e9b8ccfdc8589ebe0
Viktor Szakats <commit@vszakats.net>
2019-05-12 21:53:58 +0000

    mpack: secure/fix/follow url(s)

--- Formula/mpack.rb
+++ Formula/mpack.rb
@@ -1,7 +1,8 @@
 class Mpack < Formula
   desc "MIME mail packing and unpacking"
-  homepage "http://ftp.andrew.cmu.edu/pub/mpack/"
-  url "http://ftp.andrew.cmu.edu/pub/mpack/mpack-1.6.tar.gz"
+  homepage "https://web.archive.org/web/20190220145801/ftp.andrew.cmu.edu/pub/mpack/"
+  url "https://web.archive.org/web/20190220145801/ftp.andrew.cmu.edu/pub/mpack/mpack-1.6.tar.gz"
+  mirror "https://fossies.org/linux/misc/old/mpack-1.6.tar.gz"
   sha256 "274108bb3a39982a4efc14fb3a65298e66c8e71367c3dabf49338162d207a94c"
 
   bottle do
