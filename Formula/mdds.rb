3ba07b8752f1912b4bc25a264ddacd2fc74984ab
Caleb Xu <calebcenter@live.com>
2019-05-13 01:31:00 -0400

    mdds 1.4.3
    Closes #39773.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/mdds.rb
+++ Formula/mdds.rb
@@ -1,8 +1,8 @@
 class Mdds < Formula
   desc "Multi-dimensional data structure and indexing algorithm"
   homepage "https://gitlab.com/mdds/mdds"
-  url "https://kohei.us/files/mdds/src/mdds-1.4.2.tar.bz2"
-  sha256 "82f38750248c007956c38ffefcc549932c8b257b76c72fb79a06eabc50107369"
+  url "https://kohei.us/files/mdds/src/mdds-1.4.3.tar.bz2"
+  sha256 "25ce3d5af9f6609e1de05bb22b2316e57b74a72a5b686fbb2da199da72349c81"
 
   bottle do
     cellar :any_skip_relocation
