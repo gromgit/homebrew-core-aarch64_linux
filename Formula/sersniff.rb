50802066edafab23376bb228135636c8d76828c4
Viktor Szakats <commit@vszakats.net>
2019-05-13 03:24:34 +0000

    sersniff: secure git repository access

--- Formula/sersniff.rb
+++ Formula/sersniff.rb
@@ -3,7 +3,7 @@ class Sersniff < Formula
   homepage "https://www.earth.li/projectpurple/progs/sersniff.html"
   url "https://www.earth.li/projectpurple/files/sersniff-0.0.5.tar.gz"
   sha256 "8aa93f3b81030bcc6ff3935a48c1fd58baab8f964b1d5e24f0aaecbd78347209"
-  head "git://the.earth.li/sersniff"
+  head "https://the.earth.li/git/sersniff.git"
 
   bottle do
     cellar :any_skip_relocation
