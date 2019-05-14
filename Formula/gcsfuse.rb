e6eeca158a8791f5774c6270b06a8eb529e8c5a0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-14 03:53:01 +0000

    gcsfuse: update 0.27.0 bottle.

--- Formula/gcsfuse.rb
+++ Formula/gcsfuse.rb
@@ -7,11 +7,9 @@ class Gcsfuse < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "eead935b1d85374d650e4f1d0b9edbd5c7a65626bf2513592823be60b92795f8" => :mojave
-    sha256 "4b0e7e4e145c199f18d21293e4002b8a6d3e626e5b5f330daee08ccfc3f1d7d9" => :high_sierra
-    sha256 "9f42846472bbe6bc06b1016f6494415b16e148de6262fdcbb5bf9677fbee0a36" => :sierra
-    sha256 "f4a837589708aed85da5762394004fa2398a2e907b37cb1c9554282578e635e7" => :el_capitan
-    sha256 "4f1e5e0db02f9f148a61d9062626f754ba3f55dcf8df9ff69fc0f84cf5363577" => :yosemite
+    sha256 "7a4cd4e709432ca2078685e8729ecf8f5b93f54cc47bbc236ba82f5378c04c56" => :mojave
+    sha256 "e7e27dfed6f1d55255df55b2880bf1b930d20a0ba3b848bf659fd96683ef2134" => :high_sierra
+    sha256 "28dde861009219d5f9ef963eb83b4471cee045696c8321720fe342844119d750" => :sierra
   end
 
   depends_on "go" => :build
