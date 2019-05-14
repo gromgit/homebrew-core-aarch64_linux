ad96bd77813533342db546d7dd1e9c336826b37f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-14 09:00:32 +0000

    mpck: update 0.21 bottle.

--- Formula/mpck.rb
+++ Formula/mpck.rb
@@ -6,12 +6,9 @@ class Mpck < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "967795ec22c59a0154c76daaad88a601d425d4304fba6aca0336bb4c5d796b53" => :mojave
-    sha256 "10a102462923d52990581c999b2645727071500968a02af4c79427a20d1e0ad7" => :high_sierra
-    sha256 "fbdc8fda90c022cff2e3289b13926c503f83198b3c04abacc8ee5c310bbe7dda" => :sierra
-    sha256 "06b69481ecd910bf2c1daff65dd7d952a8759dd90e3d76f646f82ac79812d216" => :el_capitan
-    sha256 "e158ae142375591d361c6204ccc08072a29b52cee927726f9c30b063b51a0664" => :yosemite
-    sha256 "fcbf3745544824e2569004626c6a307800ef55aa2fcd667d278eaa5c34340452" => :mavericks
+    sha256 "e819ac8ce7eab3b4f83bcdf83cfbb129a9e3cebb36e314dabca646f808ed6257" => :mojave
+    sha256 "3ecd47f83f5645cfaf2bfef23b5b9a1b14bb36f2ec146409ca44d9d5f25c3401" => :high_sierra
+    sha256 "cd283270b83cf83c3e3a3c393404c1eca16e1620ced195821b97fe5ad6b39236" => :sierra
   end
 
   def install
