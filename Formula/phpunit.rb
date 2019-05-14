11cc29c8b8f530ad0ed8d3735d39298f3fba88a5
BePsvPT <BePsvPT@users.noreply.github.com>
2019-05-14 20:40:42 +0800

    phpunit 8.1.5 (#39840)

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -1,8 +1,8 @@
 class Phpunit < Formula
   desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-8.1.4.phar"
-  sha256 "ef718e436395baea420296479ae083ada282aa30cdda9f35d5b216bea30a1420"
+  url "https://phar.phpunit.de/phpunit-8.1.5.phar"
+  sha256 "cb749965b5abc29819d54fb2e056f30320d63827217538f2756b9d773afdce0f"
 
   bottle :unneeded
 
