0fd5d22ec543c05df0822a99000e962f558227f5
Caleb Xu <calebcenter@live.com>
2019-05-13 22:06:04 -0400

    serd 0.30.0

--- Formula/serd.rb
+++ Formula/serd.rb
@@ -1,8 +1,8 @@
 class Serd < Formula
   desc "C library for RDF syntax"
   homepage "https://drobilla.net/software/serd/"
-  url "https://download.drobilla.net/serd-0.28.0.tar.bz2"
-  sha256 "1df21a8874d256a9f3d51a18b8c6e2539e8092b62cc2674b110307e93f898aec"
+  url "https://download.drobilla.net/serd-0.30.0.tar.bz2"
+  sha256 "6efb0efa5c2155e6bbac941cddeeabb7ed26d70a57d24178894ff169d8f6cefb"
 
   bottle do
     cellar :any
