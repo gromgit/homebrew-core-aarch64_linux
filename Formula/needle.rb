f08178aafe49778039c602d759de3a99b5c2863a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-14 22:00:25 +0000

    needle: update 0.10.0 bottle.

--- Formula/needle.rb
+++ Formula/needle.rb
@@ -7,8 +7,8 @@ class Needle < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "85eeb9823bb921011090ac9f3d03859da4e8799815601191dc8096782ac4cd1e" => :mojave
-    sha256 "7a94d05e63913eaa6527dbd5fbad96e1d572b6fed270d7d2136440206e2829a2" => :high_sierra
+    sha256 "18dee69387e7a101967583d29541dc93a5fb65db526eaf04379109eb8625d408" => :mojave
+    sha256 "8f10782b33daaabc306c43c9b10bb8eba09987aa9e15d94dee6ebde167e418d2" => :high_sierra
   end
 
   depends_on :xcode => ["10.0", :build]
