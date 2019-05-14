a366b4aa961ac7ea00d4db89f94b1d55bc6df7c8
Caleb Xu <calebcenter@live.com>
2019-05-13 21:50:44 -0400

    lv2 1.16.0

--- Formula/lv2.rb
+++ Formula/lv2.rb
@@ -1,8 +1,8 @@
 class Lv2 < Formula
   desc "Portable plugin standard for audio systems"
   homepage "http://lv2plug.in"
-  url "http://lv2plug.in/spec/lv2-1.14.0.tar.bz2"
-  sha256 "b8052683894c04efd748c81b95dd065d274d4e856c8b9e58b7c3da3db4e71d32"
+  url "http://lv2plug.in/spec/lv2-1.16.0.tar.bz2"
+  sha256 "dec3727d7bd34a413a344a820678848e7f657b5c6019a0571c61df76d7bdf1de"
 
   bottle do
     cellar :any_skip_relocation
