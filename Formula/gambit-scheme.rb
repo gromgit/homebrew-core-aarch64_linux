0ddddd95934432b8803d81c1d484c6e960497b80
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-14 21:05:50 +0000

    gambit-scheme: update 4.9.3 bottle.

--- Formula/gambit-scheme.rb
+++ Formula/gambit-scheme.rb
@@ -5,9 +5,10 @@ class GambitScheme < Formula
   sha256 "a5e4e5c66a99b6039fa7ee3741ac80f3f6c4cff47dc9e0ff1692ae73e13751ca"
 
   bottle do
-    sha256 "2a2592928fe3f2fdf5def2c02ba9556ef54568de08ea243f559cb08b32d2c010" => :mojave
-    sha256 "50cc16d8db324fe12f46a7d8b7038c859fafd60ca7e0e1d806903b267281fa1d" => :high_sierra
-    sha256 "c7932e439c932992c2ca48c929b50ca2540b633155c7000ab72d1b9b143c4fac" => :sierra
+    rebuild 1
+    sha256 "a8b9f1adce4260059b05505e13fd9036a9bdb689aa8e7a56ef8d0804199988b0" => :mojave
+    sha256 "8626389fe8f07074733a80f85d6da64b5961258f7f5a6c8258427a5378842f01" => :high_sierra
+    sha256 "e0f5ba1f66edf7b2639280d1e954b43cd539e5501b8c69b543993c85e3f9db90" => :sierra
   end
 
   depends_on "openssl"
