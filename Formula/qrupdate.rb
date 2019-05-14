95f313795296ff182718dc1fddceeb287904b85f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-14 23:34:12 +0000

    qrupdate: update 1.1.2_10 bottle.

--- Formula/qrupdate.rb
+++ Formula/qrupdate.rb
@@ -7,9 +7,9 @@ class Qrupdate < Formula
 
   bottle do
     cellar :any
-    sha256 "16fc7829c3154593813d8087d222a8e081f78afc90a508521022b0835b954978" => :mojave
-    sha256 "c0f02275cee5d373ef3462afdaa0798a38bdea058e5c23b6ea0f3e87f0d95fc7" => :high_sierra
-    sha256 "0aeaf652b412ba913a8f98e37a32f90544185122ccc9b885ec1e6003c60f56ab" => :sierra
+    sha256 "cbaca0a12ab0f6527739e37a9100b82e3aaa5b35760739e679395dbfd63dca44" => :mojave
+    sha256 "fee484380157553a368f516ad9b8cbe01be53bca3bd20068a0d255c33d8ccf94" => :high_sierra
+    sha256 "2e7760a64f95a7243f0ac9dfc85be775943b40b462c00c7fc25241ea2b2eef36" => :sierra
   end
 
   depends_on "gcc" # for gfortran
