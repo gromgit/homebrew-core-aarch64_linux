519d963bd81c94762bde28374dc32a69dda4c596
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-15 07:16:31 +0000

    sratom: update 0.6.2 bottle.

--- Formula/sratom.rb
+++ Formula/sratom.rb
@@ -6,11 +6,9 @@ class Sratom < Formula
 
   bottle do
     cellar :any
-    sha256 "68d2e3350e8fc8f53e6f90892c1fd08c0c0929f352970d55f01367ca86ab43e9" => :mojave
-    sha256 "7e094cd5a67863974ef62270e7ad71a9b20648bb480b755c4df0e7d35bd04b9b" => :high_sierra
-    sha256 "9426748f5689b34336762037f1c594b0284ca071abb64f3abc2a4fd5b01866d2" => :sierra
-    sha256 "eec5d4022ef66509c8a5f6b37b772b5a288dfe2986eb58c00da1c69c959306eb" => :el_capitan
-    sha256 "e05f2f4d4f22571f883c1a41498aee63bd1afe8227f6817b80c6cc57f9d22bb8" => :yosemite
+    sha256 "ec520015be09bdd89bd9081aac42426dbe66fd935a5cc8a71fd9bf64cc971a71" => :mojave
+    sha256 "1db4ed5d8d3dd5f85406e8394da49ecab4d8ecca7fafd8e02fb87c76b0e24d3f" => :high_sierra
+    sha256 "cb62fd202ce3c33cda2529bc957681ffa70037e99d54df8a81999e890b9fcb65" => :sierra
   end
 
   depends_on "pkg-config" => :build
