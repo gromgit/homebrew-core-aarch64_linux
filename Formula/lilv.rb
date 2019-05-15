1e8e12c73c465f15ae25ed8bc5f321018b9107b1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-15 07:16:31 +0000

    lilv: update 0.24.4 bottle.

--- Formula/lilv.rb
+++ Formula/lilv.rb
@@ -6,11 +6,9 @@ class Lilv < Formula
 
   bottle do
     cellar :any
-    sha256 "a1d348209b1ddb44062fb1503c4abbf7ee6173810e7f3a284cadc2e8925becc3" => :mojave
-    sha256 "ea7d9e940e919d52ac64f21364a7498b0c25642ddc2056d83220e14eab4b01a3" => :high_sierra
-    sha256 "4ee314969758f0d53d10eae544d858cca171cceacb5119698368ac9c6b8d7765" => :sierra
-    sha256 "860fd323f1c16c13857584328ebd97b4603986d405e6f8fe64cfc2394084243d" => :el_capitan
-    sha256 "e31927820fa0da477314a4756497cfc2ac6f96d0c279c14e69985725e31f792e" => :yosemite
+    sha256 "6b1d5e743fb5ea653a41df22339f5ec95e1ca906d2dbbdb726f4c37dac5980cb" => :mojave
+    sha256 "3878a061cb63137874620567165c7ba656d3e27972486d3c795088bb98b83980" => :high_sierra
+    sha256 "db6587400999d5febf3a2cad3f79c17b51537388b8eb8cebbc0e0751c545b763" => :sierra
   end
 
   depends_on "pkg-config" => :build
