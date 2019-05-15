e11d582801aaf953160564b2cbcb201500dc3c83
Rui Chen <chenrui333@gmail.com>
2019-05-14 22:25:09 -0400

    kubernetes-service-catalog-client 0.2.0
    Closes #39864.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/kubernetes-service-catalog-client.rb
+++ Formula/kubernetes-service-catalog-client.rb
@@ -2,8 +2,8 @@ class KubernetesServiceCatalogClient < Formula
   desc "Consume Services in k8s using the OSB API"
   homepage "https://svc-cat.io/"
   url "https://github.com/kubernetes-incubator/service-catalog.git",
-      :tag      => "v0.1.43",
-      :revision => "2f4d37413ff5ebf816788aa94fa2613682f3b543"
+      :tag      => "v0.2.0",
+      :revision => "ab3bc6c3e6da3a53c000f7744fd05a13b20189e6"
 
   bottle do
     cellar :any_skip_relocation
