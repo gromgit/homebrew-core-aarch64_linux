284e84b5d2f93ce10c7f41035519ff78a7fef1b4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-15 14:40:31 +0000

    proteinortho: update 6.0.1 bottle.

--- Formula/proteinortho.rb
+++ Formula/proteinortho.rb
@@ -6,9 +6,9 @@ class Proteinortho < Formula
 
   bottle do
     cellar :any
-    sha256 "63d822bb350f68d92545699b38bb52ad664d26d9af1d03ea4166c7eba04a5863" => :mojave
-    sha256 "a30242255d6921a204885655e9c825db5cbac8b1b81001efc4dd87614c1e73dc" => :high_sierra
-    sha256 "e8d46227d0e30f8c83a59d0445e93a7d81fd852cbab3b9760f638c9c2b25b715" => :sierra
+    sha256 "7e9ee7c46efd1ba8fed06d37d7035178a135471a79c7cf7864d4fadb2e9752d6" => :mojave
+    sha256 "b297fca02b8117e5c3dd7f8fedc53f434a19b4052064bec8e788e7976330f527" => :high_sierra
+    sha256 "b681f87a67b9d27077f3c9a915c4cce5104525ad015f51b7975fedb1d11c254c" => :sierra
   end
 
   depends_on "diamond"
