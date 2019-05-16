e26ef15136e7979f18bbaab3eeecfc35cfc62cc3
Caleb Xu <calebcenter@live.com>
2019-05-15 23:02:11 -0400

    quantlib 1.15
    Closes #39918.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/quantlib.rb
+++ Formula/quantlib.rb
@@ -1,8 +1,8 @@
 class Quantlib < Formula
   desc "Library for quantitative finance"
   homepage "https://www.quantlib.org/"
-  url "https://dl.bintray.com/quantlib/releases/QuantLib-1.13.tar.gz"
-  sha256 "bb52df179781f9c19ef8e976780c4798b0cdc4d21fa72a7a386016e24d1a86e6"
+  url "https://dl.bintray.com/quantlib/releases/QuantLib-1.15.tar.gz"
+  sha256 "1f651b210d3046bffa9635864906423d40f9fff87faeaf417c9b76fd88f0ee64"
 
   bottle do
     cellar :any
