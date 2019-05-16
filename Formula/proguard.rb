93a06a064e41b05e69cfd1edba4e5b2a4aa5c957
Caleb Xu <calebcenter@live.com>
2019-05-15 22:16:51 -0400

    proguard 6.1.0
    Closes #39917.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/proguard.rb
+++ Formula/proguard.rb
@@ -1,8 +1,8 @@
 class Proguard < Formula
   desc "Java class file shrinker, optimizer, and obfuscator"
   homepage "https://proguard.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/proguard/proguard/6.0/proguard6.0.3.tar.gz"
-  sha256 "db175575313d11eb75a3ab68c079123d2787529b63c5cb434b1f653ececb3e48"
+  url "https://downloads.sourceforge.net/project/proguard/proguard/6.1/proguard6.1.0.tar.gz"
+  sha256 "0b71d441ac2df4d5186277c6050328903dd9b0062cb9d9f02937d1a00b55ea18"
 
   bottle :unneeded
 
