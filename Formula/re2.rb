69fe357d5b0d6766fb8466acfcac9bf4ad84f40b
Caleb Xu <calebcenter@live.com>
2019-05-16 21:04:57 -0400

    re2 20190401
    Closes #39966.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/re2.rb
+++ Formula/re2.rb
@@ -1,9 +1,9 @@
 class Re2 < Formula
   desc "Alternative to backtracking PCRE-style regular expression engines"
   homepage "https://github.com/google/re2"
-  url "https://github.com/google/re2/archive/2019-01-01.tar.gz"
-  version "20190101"
-  sha256 "bee07121ce72ce10ccacb84e49b29d091ca18d5d14d950ab7c4657a0903655db"
+  url "https://github.com/google/re2/archive/2019-04-01.tar.gz"
+  version "20190401"
+  sha256 "2ed94072145272012bb5b7054afcbe707447d49dcd79fd6d1689e6f3dc589def"
   head "https://github.com/google/re2.git"
 
   bottle do
