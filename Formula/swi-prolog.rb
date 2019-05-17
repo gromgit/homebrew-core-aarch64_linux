77cdc3ad840544e200738529a169fef632aa780c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-17 15:49:48 +0000

    swi-prolog: update 8.0.2_2 bottle.

--- Formula/swi-prolog.rb
+++ Formula/swi-prolog.rb
@@ -7,9 +7,9 @@ class SwiProlog < Formula
   head "https://github.com/SWI-Prolog/swipl-devel.git"
 
   bottle do
-    sha256 "3c2f9fd145c39173b04d1b90679079831e6d372e0c6e54d891f96b99dc2a4bf9" => :mojave
-    sha256 "cdf03ec44361238c35cd12d71f409cd1b15dbe63ee53a66bd3e8049eee5dbb91" => :high_sierra
-    sha256 "40c07a6edfc19c6180c5ea74424932c6845405199382e8fffedfeb300f493d10" => :sierra
+    sha256 "1f939859822bad18d36ab1a65bb51fa9eafad219cb5f3602ff3f5da5fdc57342" => :mojave
+    sha256 "648648853bafd06553bdaf3e508f4eac9364ed317f305268eae70b133eb39a98" => :high_sierra
+    sha256 "05a806650cd1a311eda4ab03e5ac941da4cf6e090a8e98be3f13ee5fa61c9441" => :sierra
   end
 
   depends_on "cmake" => :build
