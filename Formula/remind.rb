e59b9532fc464633f078c02a23fc5287ec9f5c23
Caleb Xu <calebcenter@live.com>
2019-05-16 21:43:19 -0400

    remind 03.01.16
    Closes #39970.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/remind.rb
+++ Formula/remind.rb
@@ -1,8 +1,8 @@
 class Remind < Formula
   desc "Sophisticated calendar and alarm"
-  homepage "https://www.roaringpenguin.com/products/remind"
-  url "https://www.roaringpenguin.com/files/download/remind-03.01.15.tar.gz"
-  sha256 "8adab4c0b30a556c34223094c5c74779164d5f3b8be66b8039f44b577e678ec1"
+  homepage "https://dianne.skoll.ca/projects/remind/"
+  url "https://dianne.skoll.ca/projects/remind/download/remind-03.01.16.tar.gz"
+  sha256 "eeb79bd4019d23a033fe3e86c672d960399db6a27c747e5b466ad55831dfca93"
 
   bottle do
     cellar :any_skip_relocation
@@ -23,14 +23,12 @@ class Remind < Formula
       s.gsub! "sleep(5);", ""
       s.gsub! /rkrphgvba\(.\);/, ""
     end
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
 
   test do
-    reminders = "reminders"
-    (testpath/reminders).write "ONCE 2015-01-01 Homebrew Test"
-    assert_equal "Reminders for Thursday, 1st January, 2015:\n\nHomebrew Test\n\n", shell_output("#{bin}/remind #{reminders} 2015-01-01")
+    (testpath/"reminders").write "ONCE 2015-01-01 Homebrew Test"
+    assert_equal "Reminders for Thursday, 1st January, 2015:\n\nHomebrew Test\n\n", shell_output("#{bin}/remind reminders 2015-01-01")
   end
 end
