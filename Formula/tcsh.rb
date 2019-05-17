79c43e1b913720572663bd6dd8f0b7479e83a5e2
Caleb Xu <calebcenter@live.com>
2019-05-16 22:35:04 -0400

    tcsh 6.21.00
    Closes #39976.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/tcsh.rb
+++ Formula/tcsh.rb
@@ -1,9 +1,9 @@
 class Tcsh < Formula
   desc "Enhanced, fully compatible version of the Berkeley C shell"
   homepage "https://web.archive.org/web/20170609182511/www.tcsh.org/Home"
-  url "ftp://ftp.astron.com/pub/tcsh/tcsh-6.20.00.tar.gz"
-  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/tcsh/tcsh-6.20.00.tar.gz"
-  sha256 "b89de7064ab54dac454a266cfe5d8bf66940cb5ed048d0c30674ea62e7ecef9d"
+  url "ftp://ftp.astron.com/pub/tcsh/tcsh-6.21.00.tar.gz"
+  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/tcsh/tcsh-6.21.00.tar.gz"
+  sha256 "c438325448371f59b12a4c93bfd3f6982e6f79f8c5aef4bc83aac8f62766e972"
 
   bottle do
     sha256 "26605931ba06f06560ff0efbbec205d80a8129eee50b5106cf508d3f2ad08f65" => :mojave
