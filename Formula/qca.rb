71fadab07eddcdb9372dfbefe476a704ce591814
Caleb Xu <calebcenter@live.com>
2019-05-16 22:05:33 -0400

    qca 2.2.1
    Closes #39971.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/qca.rb
+++ Formula/qca.rb
@@ -1,26 +1,11 @@
 class Qca < Formula
   desc "Qt Cryptographic Architecture (QCA)"
   homepage "https://userbase.kde.org/QCA"
-  revision 2
   head "https://anongit.kde.org/qca.git"
 
   stable do
-    url "https://github.com/KDE/qca/archive/v2.1.3.tar.gz"
-    sha256 "a5135ffb0250a40e9c361eb10cd3fe28293f0cf4e5c69d3761481eafd7968067"
-
-    # upstream fixes for macOS building (remove on 2.2.0 upgrade)
-    patch do
-      url "https://github.com/KDE/qca/commit/7ba0ee591e0f50a7e7b532f9eb7e500e7da784fb.diff?full_index=1"
-      sha256 "3f6c8a8bbd246556c690142c209a34973981be66e46fee991a456fb2e8b66d72"
-    end
-    patch do
-      url "https://github.com/KDE/qca/commit/b435c1b87b14ac2d2de9f83e586bfd6d8c2a755e.diff?full_index=1"
-      sha256 "9ea01ad6b21282ff62b18ac02588f7106b75056ab8379dff3fdfcff13a6c122f"
-    end
-    patch do
-      url "https://github.com/KDE/qca/commit/f4b2eb0ced5310f3c43398eb1f03e0c065e08a82.diff?full_index=1"
-      sha256 "d6c27ebfd8fec5284e4a0a39faf62e44764be5baff08141bd7f4da6d0b9f438d"
-    end
+    url "https://github.com/KDE/qca/archive/v2.2.1.tar.gz"
+    sha256 "c67fc0fa8ae6cb3d0ba0fbd8fca8ee8e4c5061b99f1fd685fd7d9800cef17f6b"
 
     # use major version for framework, instead of full version
     # see: https://github.com/KDE/qca/pull/3
