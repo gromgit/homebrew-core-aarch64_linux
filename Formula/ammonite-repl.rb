6396a57dc28520a33d234c8b92c7f60c68f4b292
Rui Chen <chenrui333@gmail.com>
2019-05-18 19:34:28 -0400

    ammonite-repl 1.6.7
    Closes #40037.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/ammonite-repl.rb
+++ Formula/ammonite-repl.rb
@@ -1,8 +1,8 @@
 class AmmoniteRepl < Formula
   desc "Ammonite is a cleanroom re-implementation of the Scala REPL"
   homepage "https://lihaoyi.github.io/Ammonite/#Ammonite-REPL"
-  url "https://github.com/lihaoyi/Ammonite/releases/download/1.6.6/2.12-1.6.6"
-  sha256 "4fa8da2fed0ee25f0ce0c9981284620b41f2599c545e262e75669d823f9a9bf0"
+  url "https://github.com/lihaoyi/Ammonite/releases/download/1.6.7/2.12-1.6.7"
+  sha256 "fc92e1769420be8c17fdf246ceffd6ac8f8343b85b33be04a8887abdfdc3f134"
 
   bottle :unneeded
 
