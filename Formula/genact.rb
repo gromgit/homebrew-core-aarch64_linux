89d7685449f1df3ee822baa76d91847379240629
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-19 21:27:24 +0000

    genact: update 0.7.0 bottle.

--- Formula/genact.rb
+++ Formula/genact.rb
@@ -5,10 +5,10 @@ class Genact < Formula
   sha256 "9bfb241d8d3e77dae63fa3f5c84ef67e459a03a8fc18ed4661e53765264288ce"
 
   bottle do
-    sha256 "4cdc64c4dd871ae661146692db42cbbb11985e5c33f2ebe5972a7b8f947911ae" => :mojave
-    sha256 "d0e5347b24fbd59bf0ca8119c9a4c9a5790e552bf076e8d9a0f986877b6da1f0" => :high_sierra
-    sha256 "c387de211ad08b4e5f1b071683bda99886a810327cdcf141d8868fcb68ea95e8" => :sierra
-    sha256 "e30beb8e8f46f2a4f53b6c6e74252ed88b9b141594afb978384efbab00ea12f8" => :el_capitan
+    cellar :any_skip_relocation
+    sha256 "fd5ef1de4fea2b1c08785d77eacaf40936764bf843e627977b2ab348f1c190ef" => :mojave
+    sha256 "efeb250335f9dc2ef86be2814243554933a99003a2f08aebf70339580917ff1d" => :high_sierra
+    sha256 "adeebbec88a70c4e06eece55a38b71e4d7031d85add9cf4069744f4a8b4cd395" => :sierra
   end
 
   depends_on "rust" => :build
