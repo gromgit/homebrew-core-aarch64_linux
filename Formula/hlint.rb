f0cf66e7b97e3808cfa1c417603918987ec9a6ed
Rui Chen <rchen@meetup.com>
2019-05-19 18:38:32 -0400

    hlint 2.1.21
    Closes #40070.
    
    Signed-off-by: Izaak Beekman <zbeekman@gmail.com>

--- Formula/hlint.rb
+++ Formula/hlint.rb
@@ -5,8 +5,8 @@ class Hlint < Formula
 
   desc "Haskell source code suggestions"
   homepage "https://github.com/ndmitchell/hlint"
-  url "https://hackage.haskell.org/package/hlint-2.1.17/hlint-2.1.17.tar.gz"
-  sha256 "431a6de94f4636253ffc1def7a588fec0d30c5c7cf468f204ec178e9c6b2312f"
+  url "https://hackage.haskell.org/package/hlint-2.1.21/hlint-2.1.21.tar.gz"
+  sha256 "447209a1781a65ec2a4502e04f7a3eb1979fc4100b32d7124986c7326fb688c6"
   head "https://github.com/ndmitchell/hlint.git"
 
   bottle do
@@ -19,7 +19,7 @@ class Hlint < Formula
   depends_on "ghc" => :build
 
   def install
-    install_cabal_package :using => "happy"
+    install_cabal_package :using => ["alex", "happy"]
     man1.install "data/hlint.1"
   end
 
