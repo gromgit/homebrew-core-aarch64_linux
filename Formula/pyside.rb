5c5c09c1dffe9ce7a9795f6e5034a3704400b346
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-20 13:55:15 +0000

    pyside: update 5.12.3 bottle.

--- Formula/pyside.rb
+++ Formula/pyside.rb
@@ -5,9 +5,9 @@ class Pyside < Formula
   sha256 "4f7aab7d4bbaf1b3573cc989d704e87b0de55cce656ae5e23418a88baa4c6842"
 
   bottle do
-    sha256 "e7fe88572d06f3466c7292847c31ba8597b2708d5785c63e18353bd9143a7738" => :mojave
-    sha256 "6a26cc11e5ac8fdad247545ca6da90f0aed371480647c84de0332085f2d4734f" => :high_sierra
-    sha256 "f597396d4f95f91c9262c8dc0a7e7f4d01ec6912db8bdff1af5e6b2baa91394b" => :sierra
+    sha256 "406393d0ea64456804d192f4948e49cf05ac205d653ccf67f71c68ba9b031ba2" => :mojave
+    sha256 "9f7b0a4631c41473e970b700f89aac3d3873d7461ccd3ceb52b4cad255f15821" => :high_sierra
+    sha256 "d0d8054cec40e93e029fd9d6e98882f63303d5718f8e5a9ed7f259ffb7bc7d05" => :sierra
   end
 
   depends_on "cmake" => :build
