f9c32e597c6384a579c12b4f6bd440359f514991
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-21 14:43:27 +0000

    nlopt: update 2.6.1 bottle.

--- Formula/nlopt.rb
+++ Formula/nlopt.rb
@@ -6,11 +6,9 @@ class Nlopt < Formula
   head "https://github.com/stevengj/nlopt.git"
 
   bottle do
-    cellar :any
-    rebuild 1
-    sha256 "6b0c91aa1a690600875d5e00aafa407f2a1394f803b31fde0867d5162a587c15" => :mojave
-    sha256 "6865527f6074f1ea737a80bd2b4fd5e6e39df01e744fd111e593fd1dd1c4e0de" => :high_sierra
-    sha256 "e3ea4064beff9f39ab2fed6a9e5f643a18fd7abb2b2302713ce7f58f970ceb09" => :sierra
+    sha256 "7a81f1b9a7f5f60d805ce298d1f97b974126a7db3be54bd7471e09de5256f248" => :mojave
+    sha256 "232738c4999669c84b8c0c1414a5a89847329c6a9492d8f74f5940f0803688e9" => :high_sierra
+    sha256 "00464736f9872cd6a67ee005ba85c1536219e973855c8fa81c46f1515e1409f9" => :sierra
   end
 
   depends_on "cmake" => [:build, :test]
