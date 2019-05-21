861d9f641cb3c762a2f581ad97e4e1f01f3037fe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-21 07:12:03 +0000

    aws-sdk-cpp: update 1.7.100 bottle.

--- Formula/aws-sdk-cpp.rb
+++ Formula/aws-sdk-cpp.rb
@@ -8,9 +8,9 @@ class AwsSdkCpp < Formula
 
   bottle do
     cellar :any
-    sha256 "761c1e4127dfe4aaca4cd83fcac7e2bb3d495f1939ef35194c1d43a6d0b1fef9" => :mojave
-    sha256 "3a5cf0499b653609337102231061810adb2faca1ca6af5e37820ff9fcc49ddd3" => :high_sierra
-    sha256 "5e776f11dfb2151ff87713a84631d5ac3e0a0485e2a4286f8737167f867484b0" => :sierra
+    sha256 "c568ec52f94dfa073bf548a0d6eb77ba75f2a52ff93e9ed3873bdc3a00a09ba5" => :mojave
+    sha256 "708f61f9dbd5d05582f93323847c1f084ee4423f60afb02c6d20134c2d9be195" => :high_sierra
+    sha256 "d0ccd9ecc348bfa4eb790f3ed24ce4fa7a0f337a442cb3b353e41e09558fe0df" => :sierra
   end
 
   depends_on "cmake" => :build
