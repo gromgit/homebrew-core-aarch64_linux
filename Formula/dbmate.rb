3fdcaedfece50a6a3a8aa440d6025786766f6f38
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-21 01:04:42 +0000

    dbmate: add 1.5.0 bottle.

--- Formula/dbmate.rb
+++ Formula/dbmate.rb
@@ -5,6 +5,13 @@ class Dbmate < Formula
   sha256 "ec9a19c74ce100094f729dec1877d34097856360c88a4371959a4a5bbb4d6429"
   head "https://github.com/amacneil/dbmate.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "946932c1571f2ed959a588035991b70533e5d9141b5489baf78d52bcf7412541" => :mojave
+    sha256 "b21f89fb8c5ec25db5a8c7d12fae7e1405d0268b6cbe2d4f823a2440a6885391" => :high_sierra
+    sha256 "84be32d3501f2d87d77a0c843c841a1583c0a142bc287e85fb63ee900b29284c" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
