a0941db4603596f54730b1613284bf381130c3fe
Chongyu Zhu <i@lembacon.com>
2019-05-22 16:08:14 +0800

    libuv 1.29.1
    Closes #40167.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/libuv.rb
+++ Formula/libuv.rb
@@ -1,8 +1,8 @@
 class Libuv < Formula
   desc "Multi-platform support library with a focus on asynchronous I/O"
   homepage "https://github.com/libuv/libuv"
-  url "https://github.com/libuv/libuv/archive/v1.29.0.tar.gz"
-  sha256 "ca62158fd35dfc06e37400f5ae5d0f18dde1ffc4c140738fff6f95b036a8b6f1"
+  url "https://github.com/libuv/libuv/archive/v1.29.1.tar.gz"
+  sha256 "bdde1140087ce97080ea323c3598553ece00a24ae63ac568be78bef3e97f3e25"
   head "https://github.com/libuv/libuv.git", :branch => "v1.x"
 
   bottle do
