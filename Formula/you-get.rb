ddcf5866a066b585b6b136aeb75d08eeeb46544e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-22 13:11:36 +0000

    you-get: update 0.4.1302 bottle.

--- Formula/you-get.rb
+++ Formula/you-get.rb
@@ -9,9 +9,9 @@ class YouGet < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "38a070da39386a48f670de54ee9b6fff27a8616bcf244962e015c828ca25fc7a" => :mojave
-    sha256 "cc612cfd1b25815a813af6ccfaee554cfe94d9f9f708612087e4762766354ad4" => :high_sierra
-    sha256 "f445b4de8abfd2b1f8416265e5125eae00d0092140fdefc923198f099af686e1" => :sierra
+    sha256 "a54a7ff078de174f7e8623d303f28d13c05b850f95e2c896d0044665b0f68611" => :mojave
+    sha256 "a2ef7d9d09a9ff9addcf2408e44e25214b1827c8b11fad0c3ff9d64a642cf960" => :high_sierra
+    sha256 "0300d9d29539389791977b7ad9c3fe91e56c7b0f299669b19762454703e175f8" => :sierra
   end
 
   depends_on "python"
