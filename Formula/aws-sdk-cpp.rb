b6ceffbfbe8dd7a7994568836610d617a71d9417
Igor Kapkov <igasgeek@me.com>
2019-05-24 09:46:16 +1000

    aws-sdk-cpp 1.7.110
    Closes #40232.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/aws-sdk-cpp.rb
+++ Formula/aws-sdk-cpp.rb
@@ -2,8 +2,8 @@ class AwsSdkCpp < Formula
   desc "AWS SDK for C++"
   homepage "https://github.com/aws/aws-sdk-cpp"
   # aws-sdk-cpp should only be updated every 10 releases on multiples of 10
-  url "https://github.com/aws/aws-sdk-cpp/archive/1.7.100.tar.gz"
-  sha256 "d502d5b93eaeed574dc4af27c9af69498a0ffcd5435e961b60df1986319c8a11"
+  url "https://github.com/aws/aws-sdk-cpp/archive/1.7.110.tar.gz"
+  sha256 "0e85cef16240f870128916191ae3fe95e921b3f2d3087888642a466005997712"
   head "https://github.com/aws/aws-sdk-cpp.git"
 
   bottle do
