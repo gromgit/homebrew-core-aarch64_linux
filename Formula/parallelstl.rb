3822137516008514419581839cc358827a23d272
Igor Kapkov <igasgeek@me.com>
2019-05-24 09:35:25 +1000

    parallelstl 20190522
    Closes #40231.
    
    Signed-off-by: Izaak Beekman <zbeekman@gmail.com>

--- Formula/parallelstl.rb
+++ Formula/parallelstl.rb
@@ -1,8 +1,8 @@
 class Parallelstl < Formula
   desc "C++ standard library algorithms with support for execution policies"
   homepage "https://github.com/intel/parallelstl"
-  url "https://github.com/intel/parallelstl/archive/20190429.tar.gz"
-  sha256 "596c870e994441d66ff8db75cfabb24b7cdb1cc8e3a40d99bd34675dcd9211a8"
+  url "https://github.com/intel/parallelstl/archive/20190522.tar.gz"
+  sha256 "37a83f26299c66a9988e85f06149487cfb2d69fb41568c64b7ad7c7903bcaac1"
 
   bottle :unneeded
 
