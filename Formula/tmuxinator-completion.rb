f53aad5c3edb5bd614a1604c9cc314dacaf81426
Igor Kapkov <igasgeek@me.com>
2019-05-23 15:26:40 +1000

    tmuxinator-completion 1.1.1
    Closes #40205.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/tmuxinator-completion.rb
+++ Formula/tmuxinator-completion.rb
@@ -1,8 +1,8 @@
 class TmuxinatorCompletion < Formula
   desc "Shell completion for Tmuxinator"
   homepage "https://github.com/tmuxinator/tmuxinator"
-  url "https://github.com/tmuxinator/tmuxinator/archive/v1.1.0.tar.gz"
-  sha256 "6be149b523cda3b009ef4426e6b32b9ce798724578930b4e1568b804614eb5b4"
+  url "https://github.com/tmuxinator/tmuxinator/archive/v1.1.1.tar.gz"
+  sha256 "cc293578bca43ba5cf0d60c1355c6aa1da9d923a0acc274a47ceab03812a6ef4"
   head "https://github.com/tmuxinator/tmuxinator.git"
 
   bottle :unneeded
