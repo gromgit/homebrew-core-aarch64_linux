f81882e130027f6c337c04ed896777bb7e80ca29
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-23 05:32:07 +0000

    csvq: add 1.11.1 bottle.

--- Formula/csvq.rb
+++ Formula/csvq.rb
@@ -4,6 +4,13 @@ class Csvq < Formula
   url "https://github.com/mithrandie/csvq/archive/v1.11.1.tar.gz"
   sha256 "e6423098bc9ef4bd54d482331a3a8a88dc90b15ed2978ba7860fffba46765c68"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "accfa2b74f9979c47efc859d61b68ea51fdc157b6ab7e90822947d165e4989f3" => :mojave
+    sha256 "e31fa0fa47e97c22c26016b1da891de17af1c460a9e0605a394369f8b8644c43" => :high_sierra
+    sha256 "ffbe532cd043b30ce29362dc00f6cd29ce2470237e1cb275b8059f7b8a4097dd" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
