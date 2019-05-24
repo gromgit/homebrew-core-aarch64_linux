6464847b6e801ee2917c01bf2538bbfecb62c9c9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-24 06:37:28 +0000

    git-cinnabar: update 0.5.1 bottle.

--- Formula/git-cinnabar.rb
+++ Formula/git-cinnabar.rb
@@ -7,9 +7,9 @@ class GitCinnabar < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "2e3f534c222993f479e8e2db49e54b0692deb394d07ea79b52eceee5bbc1ad04" => :mojave
-    sha256 "cbd5edcb40be1b5cdf8fb93de3521ba409397e807f575e5a7feeebde40170be1" => :high_sierra
-    sha256 "c5a0fc3194a4d481b9429cab610ec7ba5ef849f0b76b8cfcb1c5fe3bc97025db" => :sierra
+    sha256 "b9c5be58e2f01142e7161b42274e9e71c3d11abb4bb51b55d30a3ed78abd71c8" => :mojave
+    sha256 "850ccdd7ac691bcaeabeeb2095005086ff8328229ca55ac17fff018b895d5088" => :high_sierra
+    sha256 "6d993a4b947aee4ce6770d4b0eebe2529ee112e5cc4bdbecf5e02a4b73956f01" => :sierra
   end
 
   depends_on "mercurial"
