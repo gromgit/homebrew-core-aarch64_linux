6a05d175e21e291ec8e87d4815d40bccc10fe22a
Caleb Xu <calebcenter@live.com>
2019-05-25 00:38:17 -0400

    mpop 1.4.4
    Closes #40307.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/mpop.rb
+++ Formula/mpop.rb
@@ -1,8 +1,8 @@
 class Mpop < Formula
   desc "POP3 client"
   homepage "https://marlam.de/mpop/"
-  url "https://marlam.de/mpop/releases/mpop-1.2.8.tar.xz"
-  sha256 "16c3794507673bb1e6e8a9231787594155f32e5c68a901368045cf458f6a79ea"
+  url "https://marlam.de/mpop/releases/mpop-1.4.4.tar.xz"
+  sha256 "7d9cffe30999a7b2ea503df9b23ccbf42439b62271c45ebd7b5b04bed0654148"
 
   bottle do
     cellar :any
