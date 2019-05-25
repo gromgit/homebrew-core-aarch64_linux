afc52563d7d60cb26cb801fe6e0c545d8a3afb65
Mike Roy (on iVulcan) <mike@mikeroysoft.com>
2019-05-24 17:12:57 -0700

    docker-machine-driver-vmware 0.1.1 (new formula)
    Closes #40301.
    
    Signed-off-by: Sean Molenaar <smillerdev@me.com>

---
+++ Formula/docker-machine-driver-vmware.rb
@@ -0,0 +1,29 @@
+class DockerMachineDriverVmware < Formula
+  desc "VMware Fusion & Workstation docker-machine driver"
+  homepage "https://www.vmware.com/products/personal-desktop-virtualization.html"
+  url "https://github.com/machine-drivers/docker-machine-driver-vmware.git",
+    :tag      => "v0.1.1",
+    :revision => "cd992887ede19ae63e030c63dda5593f19ed569c"
+
+  depends_on "go" => :build
+  depends_on "docker-machine"
+
+  def install
+    ENV["GOPATH"] = buildpath
+
+    dir = buildpath/"src/github.com/machine-drivers/docker-machine-driver-vmware"
+    dir.install buildpath.children
+
+    cd dir do
+      system "go", "build", "-o", "#{bin}/docker-machine-driver-vmware",
+            "-ldflags", "-X main.version=#{version}"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    docker_machine = Formula["docker-machine"].opt_bin/"docker-machine"
+    output = shell_output("#{docker_machine} create --driver vmware -h")
+    assert_match "engine-env", output
+  end
+end
