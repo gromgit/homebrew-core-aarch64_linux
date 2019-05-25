0f98bbd37d73f09e76a659d178d9d103316c5db5
Caleb Xu <calebcenter@live.com>
2019-05-25 00:30:19 -0400

    nco 4.8.0
    Closes #40306.
    
    Signed-off-by: Izaak Beekman <zbeekman@gmail.com>

--- Formula/nco.rb
+++ Formula/nco.rb
@@ -1,8 +1,8 @@
 class Nco < Formula
   desc "Command-line operators for netCDF and HDF files"
   homepage "https://nco.sourceforge.io/"
-  url "https://github.com/nco/nco/archive/4.7.9.tar.gz"
-  sha256 "048f6298bceb40913c3ae433f875dea1e9129b1c86019128e7271d08f274a879"
+  url "https://github.com/nco/nco/archive/4.8.0.tar.gz"
+  sha256 "91f95ebfc9baa888adaec3016ca18a6297e2881b1429d74543a27fdfbe15fcab"
 
   bottle do
     cellar :any
