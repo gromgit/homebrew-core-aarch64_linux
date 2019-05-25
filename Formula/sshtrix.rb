3c21f7ee59a44f85165c67622c6a73b3c052cc9c
Caleb Xu <calebcenter@live.com>
2019-05-25 12:46:57 -0400

    sshtrix 0.0.3
    Closes #40324.
    
    Signed-off-by: Izaak Beekman <zbeekman@gmail.com>

--- Formula/sshtrix.rb
+++ Formula/sshtrix.rb
@@ -1,9 +1,8 @@
 class Sshtrix < Formula
   desc "SSH login cracker"
   homepage "http://www.nullsecurity.net/tools/cracker.html"
-  url "https://github.com/nullsecuritynet/tools/raw/master/cracker/sshtrix/release/sshtrix-0.0.2.tar.gz"
-  sha256 "dc90a8b2fbb62689d1b59333413b56a370a0715c38bf0792f517ed6f9763f5df"
-  revision 1
+  url "https://github.com/nullsecuritynet/tools/raw/master/cracker/sshtrix/release/sshtrix-0.0.3.tar.gz"
+  sha256 "30d1d69c1cac92836e74b8f7d0dc9d839665b4994201306c72e9929bee32e2e0"
 
   bottle do
     cellar :any
@@ -16,9 +15,6 @@ class Sshtrix < Formula
   depends_on "libssh"
 
   def install
-    # https://github.com/nullsecuritynet/tools/issues/6
-    inreplace "Makefile", "-lssh_threads", ""
-
     bin.mkpath
     system "make", "sshtrix", "CC=#{ENV.cc}"
     system "make", "DISTDIR=#{prefix}", "install"
