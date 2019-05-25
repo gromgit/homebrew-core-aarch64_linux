61bacb63f9e05d88a1161a8cf4703dac68bbd0bd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-25 09:23:41 +0000

    swimat: update 1.6.2 bottle.

--- Formula/swimat.rb
+++ Formula/swimat.rb
@@ -7,9 +7,7 @@ class Swimat < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "12ffe4eee9c6d9abe8e84abf777f74ed7ab276206ad9f06dca801f4cdbcbca52" => :mojave
-    sha256 "fc96c111dfbe508667feb7611c9c7d313fa8409c84d5a98d6bcfd99ef67e76d1" => :high_sierra
-    sha256 "7a14a1bb3c7d1b7421b2bef0d24bf23c98375fa2dfbfd866c7bdc9517162bdbb" => :sierra
+    sha256 "197ba3d1db2bfbee29a10e4735925b4fdac3035c486b7899bc564cea5f019839" => :mojave
   end
 
   depends_on :xcode => ["10.2", :build]
