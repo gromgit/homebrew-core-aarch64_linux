737dfd6c50cd5a6ca4ecd07b1dd148e75c2fda43
chenrui <rchen@meetup.com>
2019-05-26 15:02:14 -0400

    checkstyle 8.21 (#40348)

--- Formula/checkstyle.rb
+++ Formula/checkstyle.rb
@@ -1,8 +1,8 @@
 class Checkstyle < Formula
   desc "Check Java source against a coding standard"
   homepage "https://checkstyle.sourceforge.io/"
-  url "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.20/checkstyle-8.20-all.jar"
-  sha256 "d7db544afefb57cc9bafb4e6cf729b77f06c34aa84639c18e54dfadb6afb5aef"
+  url "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.21/checkstyle-8.21-all.jar"
+  sha256 "ed543a0f5efcd9234079244467b537438e18a40b19423951ae23bf5ddd9aa6c9"
 
   bottle :unneeded
 
