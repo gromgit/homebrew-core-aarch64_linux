08180c4d2839817e943bbe01957a0bcacc82ce92
Arun Prakash Jana <engineerarun@gmail.com>
2019-05-27 20:53:36 +0530

    nnn 2.5
    Closes #40368.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/nnn.rb
+++ Formula/nnn.rb
@@ -1,8 +1,8 @@
 class Nnn < Formula
   desc "Tiny, lightning fast, feature-packed file manager"
   homepage "https://github.com/jarun/nnn"
-  url "https://github.com/jarun/nnn/archive/v2.4.tar.gz"
-  sha256 "859ba5bd4892016aed9a4830ee1e1b03eb74e94c4f1bd82f0288dc559f7327eb"
+  url "https://github.com/jarun/nnn/archive/v2.5.tar.gz"
+  sha256 "3636f172a024de5c12420a80dbe3d006d42b5e0a17e70a527963c864af22655c"
   head "https://github.com/jarun/nnn.git"
 
   bottle do
