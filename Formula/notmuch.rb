37f255310d04b49f97ede5ce07aa8921c6c0b6e2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-27 21:51:49 +0000

    notmuch: update 0.28.4 bottle.

--- Formula/notmuch.rb
+++ Formula/notmuch.rb
@@ -7,10 +7,9 @@ class Notmuch < Formula
 
   bottle do
     cellar :any
-    rebuild 2
-    sha256 "85ed63058e3f8a62375e9df08963fdcf06423cfa8e8e42b1b43952b93d5828b6" => :mojave
-    sha256 "241b8649b30055854bfea7f0540099895e7915402c5b278ef5db838545f006a7" => :high_sierra
-    sha256 "bc53f8af373350cb3a993ca381eaaf1245d70874e24611c9a7e95568e96f922a" => :sierra
+    sha256 "7a28cc4dc79666ee4a026d7b14cbf1f4ecfd533beca9b74f41b00bf83fb281a4" => :mojave
+    sha256 "f47dc47e3cb0dccc48ad57e07bafbe7303189f98fe4f772fd7c1684cc71ac978" => :high_sierra
+    sha256 "0c1426a919d84933ea3d0c95466bf7119eb42a1c1e4485bb76de05d63c675f2f" => :sierra
   end
 
   depends_on "doxygen" => :build
