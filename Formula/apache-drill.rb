9dbc5893317547ee06552eae02545ab484f1cb50
John Norrington <john.norrington@fanduel.com>
2019-05-28 22:27:52 +0100

    apache-drill 1.16.0
    Closes #40423.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/apache-drill.rb
+++ Formula/apache-drill.rb
@@ -1,9 +1,9 @@
 class ApacheDrill < Formula
   desc "Schema-free SQL Query Engine for Hadoop, NoSQL and Cloud Storage"
   homepage "https://drill.apache.org"
-  url "https://www.apache.org/dyn/closer.cgi?path=drill/drill-1.15.0/apache-drill-1.15.0.tar.gz"
-  mirror "https://archive.apache.org/dist/drill/drill-1.15.0/apache-drill-1.15.0.tar.gz"
-  sha256 "188c1d0df28e50f0265f4bc3c5871b4e7abc9450a4e5a7dbe7f0b23146bec76b"
+  url "https://www.apache.org/dyn/closer.cgi?path=drill/drill-1.16.0/apache-drill-1.16.0.tar.gz"
+  mirror "https://archive.apache.org/dist/drill/drill-1.16.0/apache-drill-1.16.0.tar.gz"
+  sha256 "fd195d2b38f393459b37d8f13ac1f36cdbe38495eabb08252da38e3544e87839"
 
   bottle :unneeded
 
