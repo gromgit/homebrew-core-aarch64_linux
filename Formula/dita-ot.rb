b4f587c6d8ef419cd37b09cdfb4ff0360bdfaac3
Roger Sheen <infotexture@gmail.com>
2019-05-28 18:10:49 +0200

    dita-ot 3.3.2
    Closes #40400.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/dita-ot.rb
+++ Formula/dita-ot.rb
@@ -1,8 +1,8 @@
 class DitaOt < Formula
   desc "DITA Open Toolkit is an implementation of the OASIS DITA specification"
   homepage "https://www.dita-ot.org/"
-  url "https://github.com/dita-ot/dita-ot/releases/download/3.3.1/dita-ot-3.3.1.zip"
-  sha256 "e09237374d710db38e854c7ed397827c44d5e4d111963c43dabcfc0cd06897eb"
+  url "https://github.com/dita-ot/dita-ot/releases/download/3.3.2/dita-ot-3.3.2.zip"
+  sha256 "8b74260bcee714e898431bec91abe8fafd9c86410548bf928e6ca289263fc962"
 
   bottle :unneeded
 
