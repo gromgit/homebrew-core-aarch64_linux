e2e026d3aeaa3add826b6826d8e4d5f0c4a2c2c8
Sean Molenaar <smillerdev@me.com>
2019-05-29 21:22:30 +0200

    phpstan 0.11.8
    Closes #40464.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/phpstan.rb
+++ Formula/phpstan.rb
@@ -1,8 +1,8 @@
 class Phpstan < Formula
   desc "PHP Static Analysis Tool"
   homepage "https://github.com/phpstan/phpstan"
-  url "https://github.com/phpstan/phpstan/releases/download/0.11.7/phpstan.phar"
-  sha256 "e3b86914709c706b28a75434a705481c1f07ffc04e473108559c2359edf38804"
+  url "https://github.com/phpstan/phpstan/releases/download/0.11.8/phpstan.phar"
+  sha256 "9604e84b47a515f860f8dd96d94dc0527636da7b4e6ccf9bd1785d59f004ae75"
 
   bottle :unneeded
 
