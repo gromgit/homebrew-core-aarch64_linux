dbc484fcf1a2b82a4f2168ed897ba47337a3978a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-29 04:12:32 +0000

    catch2: add 2.8.0 bottle.

--- Formula/catch2.rb
+++ Formula/catch2.rb
@@ -4,6 +4,13 @@ class Catch2 < Formula
   url "https://github.com/catchorg/Catch2/archive/v2.8.0.tar.gz"
   sha256 "b567c37446cd22c8550bfeb7e2fe3f981b8f3ab8b2148499a522e7f61b8a481d"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "532fa3ba6c740b4b0179a83028eb1d1430ceb38023c7fb2903d96da24f8f5d6a" => :mojave
+    sha256 "532fa3ba6c740b4b0179a83028eb1d1430ceb38023c7fb2903d96da24f8f5d6a" => :high_sierra
+    sha256 "c32fb10216b8c4aead008d2f515ed690f753dd2dfa440e41525beafd037aa1f7" => :sierra
+  end
+
   depends_on "cmake" => :build
 
   def install
