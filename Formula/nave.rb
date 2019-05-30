259e1c88cb602d1986bcf272719054edb977d7e7
Igor Kapkov <igasgeek@me.com>
2019-05-30 22:09:49 +1000

    nave 2.4.1
    Closes #40493.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/nave.rb
+++ Formula/nave.rb
@@ -1,8 +1,8 @@
 class Nave < Formula
   desc "Virtual environments for Node.js"
   homepage "https://github.com/isaacs/nave"
-  url "https://github.com/isaacs/nave/archive/v2.2.4.tar.gz"
-  sha256 "ea14cd9f8e9fc8c8ce9b1a6aec4b25148bc742d979724bc15a2ef2b0c0bb9dc7"
+  url "https://github.com/isaacs/nave/archive/v2.4.1.tar.gz"
+  sha256 "68fe582a46a66ea5f04f1ffe85836b41e9918d3110dfe930f9f537623a8effe7"
   head "https://github.com/isaacs/nave.git"
 
   bottle :unneeded
