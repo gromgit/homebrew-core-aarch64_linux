dfe1e847f6011ca6e0114e68afcc84f7b88f1d5e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-30 17:32:24 +0000

    hexyl: update 0.5.1 bottle.

--- Formula/hexyl.rb
+++ Formula/hexyl.rb
@@ -6,9 +6,9 @@ class Hexyl < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "4662a02fa6c3e47e9d1376a177c74c7127dc16a7ee6939538d01ef425b2cbf3f" => :mojave
-    sha256 "652e4e4a0a661d3dd97aaab96a4d33ffc8096a58773621e612e80f22a9df32f7" => :high_sierra
-    sha256 "2b52aef13a792296811d7ec74719b56f0aec3e3302b19d48f82731ab952aeda4" => :sierra
+    sha256 "2ea951a75e72e97075b3d4db45edbfc6cbb27b67c4655fcd1483ce6ad2c4165d" => :mojave
+    sha256 "52d0981ef2edc478f11e8c66a959f2868c0ee7e20a3178064ec289dfaa71e397" => :high_sierra
+    sha256 "688108b44f0fb529f1085a1e7416d445b6b6c12885a9a941727c950233f8b6eb" => :sierra
   end
 
   depends_on "rust" => :build
