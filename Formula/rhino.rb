dfd5c56b0a78879f67c53ce423feefd3dba2c8a6
chenrui <rchen@meetup.com>
2019-05-30 18:24:58 -0400

    rhino 1.7.11 (#40504)

--- Formula/rhino.rb
+++ Formula/rhino.rb
@@ -1,8 +1,8 @@
 class Rhino < Formula
   desc "JavaScript engine"
   homepage "https://www.mozilla.org/rhino/"
-  url "https://github.com/mozilla/rhino/releases/download/Rhino1_7_9_Release/rhino-1.7.9.zip"
-  sha256 "6fb72a1e92e99c96b34a7b2c860dd3a6e7ef7f3492f4d9678eb5339d67ad7f32"
+  url "https://github.com/mozilla/rhino/releases/download/Rhino1_7_11_Release/rhino-1.7.11.zip"
+  sha256 "94c16e4de06fc08b5f7bf492284b8dc94c6357d3f96cc06fc06dcd6ae3f39547"
 
   bottle :unneeded
 
