55b59d7320080d3990672a4321fb5c675554c578
Jason Karns <jason@karns.name>
2019-05-30 10:04:57 -0400

    node-build 4.6.0 (#40497)

--- Formula/node-build.rb
+++ Formula/node-build.rb
@@ -1,8 +1,8 @@
 class NodeBuild < Formula
   desc "Install NodeJS versions"
   homepage "https://github.com/nodenv/node-build"
-  url "https://github.com/nodenv/node-build/archive/v4.5.0.tar.gz"
-  sha256 "1bdb9a208a6ca0c59c2d0d14eb23e9a5d33b7cbc5bb0cc80e790857d5c5964ac"
+  url "https://github.com/nodenv/node-build/archive/v4.6.0.tar.gz"
+  sha256 "64108f5836beeaac5314813d62a36daab289207fbecc40fb5b998cac4bb2cb0b"
   head "https://github.com/nodenv/node-build.git"
 
   bottle :unneeded
