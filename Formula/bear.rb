9ce6c216f3d545eb6defdcdd2236cba65de181d7
Igor Kapkov <igasgeek@me.com>
2019-05-31 17:21:45 +1000

    bear 2.4.0
    Closes #40548.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/bear.rb
+++ Formula/bear.rb
@@ -1,9 +1,8 @@
 class Bear < Formula
   desc "Generate compilation database for clang tooling"
   homepage "https://github.com/rizsotto/Bear"
-  url "https://github.com/rizsotto/Bear/archive/2.3.13.tar.gz"
-  sha256 "dc14c28bfbe0beef5ec93b4614a00bd419d5a793c8a678ba3b5544bd1dd580b6"
-  revision 1
+  url "https://github.com/rizsotto/Bear/archive/2.4.0.tar.gz"
+  sha256 "76dd23ac2e216651f2d24a1478262c274378ef54d87021a2a4050382a0de8f96"
   head "https://github.com/rizsotto/Bear.git"
 
   bottle do
