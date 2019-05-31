05d33f3ef8d04e40099b7984f76dea62103f5b3e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-31 06:00:37 +0000

    ccextractor: update 0.88 bottle.

--- Formula/ccextractor.rb
+++ Formula/ccextractor.rb
@@ -7,9 +7,9 @@ class Ccextractor < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d08337e5455f14055cc355013190441bbf0238c19d0d2b1669d86a973a9f8fc1" => :mojave
-    sha256 "44c4b3599f3e4f9ca362473751b6cd040d0a06b32f2a22e2087f22413ff6e45b" => :high_sierra
-    sha256 "8d5bbddf6f2f173e1e65dab7b43ae70987938ef6b255f7d61d99cb9427357e2a" => :sierra
+    sha256 "87a9b43c6ac20b2dc270cc35b1da0df4a92758bc722882407dcacbacb7e806d7" => :mojave
+    sha256 "35be2be5fd71b1784b85a7bd5ba7ccf35f987fd6ed0548df7864cd686e28c5c0" => :high_sierra
+    sha256 "9c78ad6dd2f3c02ef7ca508af88114f8deffa691cf3a546e6ca09ed279b80758" => :sierra
   end
 
   def install
