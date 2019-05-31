0c23423837a0f095e3f3b3e395e6d563cf1877c6
Viktor Szakats <commit@vszakats.net>
2019-05-31 12:09:09 +0000

    pkg-config: remove broken HTTP mirror

--- Formula/pkg-config.rb
+++ Formula/pkg-config.rb
@@ -3,7 +3,6 @@ class PkgConfig < Formula
   homepage "https://freedesktop.org/wiki/Software/pkg-config/"
   url "https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.2.tar.gz"
   mirror "https://dl.bintray.com/homebrew/mirror/pkg-config-0.29.2.tar.gz"
-  mirror "http://fco.it.distfiles.macports.org/mirrors/macports-distfiles/pkgconfig/pkg-config-0.29.2.tar.gz"
   sha256 "6fc69c01688c9458a57eb9a1664c9aba372ccda420a02bf4429fe610e7e7d591"
 
   bottle do
