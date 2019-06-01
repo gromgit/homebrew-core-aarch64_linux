312a45a12743962cab08ab25b99788c8e26bfeb1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-01 23:57:33 +0000

    mailutils: update 3.6 bottle.

--- Formula/mailutils.rb
+++ Formula/mailutils.rb
@@ -6,9 +6,9 @@ class Mailutils < Formula
   sha256 "a0dc30361195594f0bf08a2a2b4906222a36e3ec8936056abe808184ec513ef1"
 
   bottle do
-    sha256 "086213ef133468e2b97c4d0cd65b919751127b86bac79633de0f4134b2393d53" => :mojave
-    sha256 "9025ac552ffa4ec245af7d6db38a95eadb3876ff28146ba8dea4a954648d9798" => :high_sierra
-    sha256 "b7447a61f03f813b4b862c7b0d7bc51007ff0e514107036f0dbdc2b67b852bcb" => :sierra
+    sha256 "d1e191fc899b56957fe5b81ead95466cb8332025226ca8c98e23171f45eb6cb0" => :mojave
+    sha256 "e5971d7a4667cf9780fa723e92519524c3c60a2210db74ca57b91b8d94486978" => :high_sierra
+    sha256 "79b96a7b7d055a1ff58735fcba664e35b5ca38d50d1ddb78685c8d1d002a8278" => :sierra
   end
 
   depends_on "gnutls"
