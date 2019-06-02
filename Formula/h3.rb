9e71e74c7bf75fb7fa4c147dfef6155539d2c47a
Igor Kapkov <igasgeek@me.com>
2019-06-02 17:36:49 +1000

    h3 3.4.4
    Closes #40604.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/h3.rb
+++ Formula/h3.rb
@@ -1,8 +1,8 @@
 class H3 < Formula
   desc "Hexagonal hierarchical geospatial indexing system"
   homepage "https://uber.github.io/h3/"
-  url "https://github.com/uber/h3/archive/v3.4.3.tar.gz"
-  sha256 "92fad4467e0af343dbf3d003da09bd2df6a1350f46fcc569ca33648593d78f25"
+  url "https://github.com/uber/h3/archive/v3.4.4.tar.gz"
+  sha256 "9cc84d472bd1352af5bf645ba8087392ca9cbc5abb4f64094a11ee44c675082a"
 
   bottle do
     cellar :any
@@ -21,7 +21,7 @@ class H3 < Formula
   end
 
   test do
-    result = pipe_output("#{bin}/geoToH3 10", "40.689167 -74.044444")
+    result = pipe_output("#{bin}/geoToH3 -r 10 --lat 40.689167 --lon -74.044444")
     assert_equal "8a2a1072b59ffff", result.chomp
   end
 end
