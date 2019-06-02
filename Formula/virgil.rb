b2826cc5990f14e5912e095c071fb539f6dd27bb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-02 13:46:30 +0000

    virgil: add 5.0.1 bottle.

--- Formula/virgil.rb
+++ Formula/virgil.rb
@@ -6,6 +6,13 @@ class Virgil < Formula
      :revision => "03ddee65e9cecbdd7ef55b1285c0ca70758d6d40"
   head "https://github.com/VirgilSecurity/virgil-cli.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "93aafd187b3594c56920b5f59df7d88b6de8d10bf5302618690370e898e801a9" => :mojave
+    sha256 "69c30a792aedf2422d32a0d4fc0ff272fee46e8a0277eaf8865c3d47a50f62b2" => :high_sierra
+    sha256 "5b71177f0535c11b793e7700660779da6fda6a5fdbd5165ddff57efe7eac9aa7" => :sierra
+  end
+
   depends_on "dep" => :build
   depends_on "go" => :build
 
