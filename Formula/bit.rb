f4c989e342f4c05dde243001f2f1b0734319dbf2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-02 22:25:47 +0000

    bit: update 14.1.2 bottle.

--- Formula/bit.rb
+++ Formula/bit.rb
@@ -8,9 +8,9 @@ class Bit < Formula
   head "https://github.com/teambit/bit.git"
 
   bottle do
-    sha256 "125bbd3f6b13b4ca89aaec49b53cf154b9073a6b0bb760f0039d1c281fe4786c" => :mojave
-    sha256 "14e4ef32cc67a6ed3254b2abea1b8b38d1179f3ce0057184bce90845b2e45932" => :high_sierra
-    sha256 "22abd49020c104f268975b959c47f41dd85986cb74bb3ea66bfb3c93d95117b9" => :sierra
+    sha256 "d5424891a7c89e5d89268c3a62a02357f475f20754208fc768e517f0ccd06dd1" => :mojave
+    sha256 "5d088d0d560ce98283552174314d33caaa527c1a6ea728fea0e0a957944a637b" => :high_sierra
+    sha256 "18b61ccca1e13c99e6a71236d61663381135ab80df2957147a474ce2bd682cf5" => :sierra
   end
 
   depends_on "node"
