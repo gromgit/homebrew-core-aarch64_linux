55ba1115214fda8a1f0a02f36fde7dd87482afa0
Igor Kapkov <igasgeek@me.com>
2019-06-03 13:30:56 +1000

    sassc 3.6.0
    Closes #40621.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/sassc.rb
+++ Formula/sassc.rb
@@ -2,8 +2,8 @@ class Sassc < Formula
   desc "Wrapper around libsass that helps to create command-line apps"
   homepage "https://github.com/sass/sassc"
   url "https://github.com/sass/sassc.git",
-      :tag      => "3.5.0",
-      :revision => "aa6d5c635ea8faf44d542a23aaf85d27e5777d48"
+      :tag      => "3.6.0",
+      :revision => "3f84e2358019dab2fdba5fe1fc0ecff23aa24608"
   head "https://github.com/sass/sassc.git"
 
   bottle do
