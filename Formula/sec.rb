d5fa4db020148a4305f0727ddd6e428dffd54836
Igor Kapkov <igasgeek@me.com>
2019-06-03 20:24:08 +1000

    sec 2.8.2 (#40630)

--- Formula/sec.rb
+++ Formula/sec.rb
@@ -1,8 +1,8 @@
 class Sec < Formula
   desc "Event correlation tool for event processing of various kinds"
   homepage "https://simple-evcorr.sourceforge.io/"
-  url "https://github.com/simple-evcorr/sec/releases/download/2.8.1/sec-2.8.1.tar.gz"
-  sha256 "342464b2494f4c64eaac3c0f4c53486ea5464f24d8a190c27615850b9d4fe100"
+  url "https://github.com/simple-evcorr/sec/releases/download/2.8.2/sec-2.8.2.tar.gz"
+  sha256 "ec7b30b77221f58de4c91df115b07a9f5de91813f904fd01007632389672aa3c"
 
   bottle :unneeded
 
