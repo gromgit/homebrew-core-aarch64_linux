550704bc6a1b652460a311a507bd7e17cbb67a1f
Rui Chen <rchen@meetup.com>
2019-06-03 01:24:35 -0400

    newman 4.5.0 (new Formula)
    Closes #40626.
    
    Signed-off-by: Sean Molenaar <smillerdev@me.com>

---
+++ Formula/newman.rb
@@ -0,0 +1,57 @@
+require "language/node"
+
+class Newman < Formula
+  desc "Command-line collection runner for Postman"
+  homepage "https://www.getpostman.com"
+  url "https://registry.npmjs.org/newman/-/newman-4.5.0.tgz"
+  sha256 "6c6c19dc1a20baa6f353249079429259e951104326df0b9f00b46ec13d83c5ff"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    path = testpath/"test-collection.json"
+    path.write <<~EOS
+      {
+        "info": {
+          "_postman_id": "db95eac2-6e1c-48c0-8c3a-f83c5341d4dd",
+          "name": "Homebrew",
+          "description": "Homebrew formula test",
+          "schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json"
+        },
+        "item": [
+          {
+            "name": "httpbin-get",
+            "request": {
+              "method": "GET",
+              "header": [],
+              "body": {
+                "mode": "raw",
+                "raw": ""
+              },
+              "url": {
+                "raw": "https://httpbin.org/get",
+                "protocol": "https",
+                "host": [
+                  "httpbin",
+                  "org"
+                ],
+                "path": [
+                  "get"
+                ]
+              }
+            },
+            "response": []
+          }
+        ]
+      }
+    EOS
+
+    assert_match /newman/, shell_output("#{bin}/newman run #{path}")
+    assert_equal version.to_s, shell_output("#{bin}/newman --version").strip
+  end
+end
