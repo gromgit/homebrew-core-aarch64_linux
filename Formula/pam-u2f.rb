affd258999ae0477859c0c9e41c1f19c3e83aee7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-04 13:51:00 +0000

    pam-u2f: update 1.0.8 bottle.

--- Formula/pam-u2f.rb
+++ Formula/pam-u2f.rb
@@ -7,10 +7,9 @@ class PamU2f < Formula
 
   bottle do
     cellar :any
-    sha256 "ed80bbd945c357cf4cd184fb97a742f975cbd49f10eee7ab78bea19a13dd0d63" => :mojave
-    sha256 "aeea6df4ee9fd8a1625a15aa5a1d1a09429cf4b48c697e0a423dd9874c4b868e" => :high_sierra
-    sha256 "873567775f9cf3a1d87c8dcc93d7a5cf1d41e536595b8b2e2b3bca12eed1e595" => :sierra
-    sha256 "10730971bc1100e4288204cec75f3115235e0cbbaef19f8acad1ddf9a4aeb0d2" => :el_capitan
+    sha256 "5284e43e79ed3e1031484265ef76eb5f02dfd4782de5752f336927feae235750" => :mojave
+    sha256 "07e3f722d932e932de1af8e871940b90fe1340c1332274d1cbc6cceb2e981bda" => :high_sierra
+    sha256 "aba234dff72d3e2c9f51484e294776ca51bc7e03fe390f80539aa5b3e759d1a1" => :sierra
   end
 
   depends_on "asciidoc" => :build
