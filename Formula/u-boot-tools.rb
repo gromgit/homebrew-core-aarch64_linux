ae234be8032aa77848596ed215fab02b17585c1a
Rui Chen <rchen@meetup.com>
2019-06-05 00:05:52 -0400

    u-boot-tools 2019.04
    Closes #40711.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/u-boot-tools.rb
+++ Formula/u-boot-tools.rb
@@ -1,8 +1,8 @@
 class UBootTools < Formula
   desc "Universal boot loader"
   homepage "https://www.denx.de/wiki/U-Boot/"
-  url "http://ftp.denx.de/pub/u-boot/u-boot-2018.07.tar.bz2"
-  sha256 "9f10df88bc91b35642e461217f73256bbaeeca9ae2db8db56197ba5e89e1f6d4"
+  url "http://ftp.denx.de/pub/u-boot/u-boot-2019.04.tar.bz2"
+  sha256 "76b7772d156b3ddd7644c8a1736081e55b78828537ff714065d21dbade229bef"
 
   bottle do
     cellar :any
