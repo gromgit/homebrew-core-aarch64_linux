331c2538413af58e77b8e6f064c996058baeafbf
Igor Kapkov <igasgeek@me.com>
2019-06-05 11:37:37 +1000

    micronaut 1.1.3 (#40703)

--- Formula/micronaut.rb
+++ Formula/micronaut.rb
@@ -1,8 +1,8 @@
 class Micronaut < Formula
   desc "Modern JVM-based framework for building modular microservices"
   homepage "https://micronaut.io/"
-  url "https://github.com/micronaut-projects/micronaut-core/releases/download/v1.1.2/micronaut-1.1.2.zip"
-  sha256 "07b9712e98ceb1b884306cf0903e46317f3f9ab6583154e47c36b40977098a6d"
+  url "https://github.com/micronaut-projects/micronaut-core/releases/download/v1.1.3/micronaut-1.1.3.zip"
+  sha256 "e4b6f4c277f5d82eb09bb5d2c2fff0425a8a3cbd882324c67ac181efa220d8bd"
 
   bottle :unneeded
 
