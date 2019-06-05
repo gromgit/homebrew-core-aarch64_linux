adde641f22ce8f1e51abbe59de02541cb0e84491
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-05 18:02:59 +0000

    terraformer: add 0.7.4 bottle.

--- Formula/terraformer.rb
+++ Formula/terraformer.rb
@@ -5,6 +5,13 @@ class Terraformer < Formula
     :tag      => "0.7.4",
     :revision => "e2dba7410c90d887918fa36eedace8c555115a47"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7fc251c6c6b88a7f6878aaba2d28a5eda6d86356d59eebcf6b508f8485b75910" => :mojave
+    sha256 "8d35fd49630cd0ae536c1239f2a8a79ddac03e776f9f1c8770117b80dc52c8ba" => :high_sierra
+    sha256 "6dd19a2e9adc5140690cbbf1ac468b817d63281df612e4df7e60aa2ad1e85cdb" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
