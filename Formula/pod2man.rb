8651572f26bc240b63df283f62f128de38896938
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-06 22:52:20 +0000

    pod2man: update 4.12 bottle.

--- Formula/pod2man.rb
+++ Formula/pod2man.rb
@@ -6,10 +6,9 @@ class Pod2man < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c7e444c6b55bc4943f4508a3f9f8d5999895f6f0242216c53854c55998156102" => :mojave
-    sha256 "6c780c4466590c859810bda4c1e851f3d36bd6b053302cef045ddcfb94a0d964" => :high_sierra
-    sha256 "94d9f00a8c1226b62a0d3d729770eb4e9a8be0a51a3e60ef7dd466152710bbe0" => :sierra
-    sha256 "54894e5d48ada5a3746777469f95b7ab83d59f409812d4cf48284ab2a8bb14fb" => :el_capitan
+    sha256 "ed36edc4949a7487f04ee78b158c717e52201a697047285cc3ac3cb8f66688b6" => :mojave
+    sha256 "7ec281093812bd9cfe65b774bed8831b615fcb309f04810e70b726321119e562" => :high_sierra
+    sha256 "fd37f0663f60e32c126772b3c37f52ca101938b69c2ebd5182af61b64a18118b" => :sierra
   end
 
   keg_only :provided_by_macos
