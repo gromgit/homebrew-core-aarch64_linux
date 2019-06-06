9b887c0d453297f5fc7ce7bec41fc4ecaf5fb907
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-06 22:25:43 +0000

    gengetopt: update 2.23 bottle.

--- Formula/gengetopt.rb
+++ Formula/gengetopt.rb
@@ -7,12 +7,9 @@ class Gengetopt < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "db306b0c86e66795ae1cbead1eda1777d3386ae87a1f1bbc49ab8ed7cbba6ef6" => :mojave
-    sha256 "dce4c1fd06b1a70d2aece23379ae49dd24ccc9b2059a428774484d7bdbde2a3a" => :high_sierra
-    sha256 "2ce885fcf985bb56f993894cff3cbf94067b55dc9d59ba63d63cfdfcb4ea6e5d" => :sierra
-    sha256 "b551e59fe1e883d0c5e06ddb441777d3c07a60a9305fa08eaedcfd326f55ef26" => :el_capitan
-    sha256 "74b81ccbfb9811f2f5a91218685f2fa19bef0c0970458fa2d2e07b3ea72bd5ef" => :yosemite
-    sha256 "55332e9f5e24e737976f0212776ddc0d6609bed62eaef4ac80884eaa5171ef76" => :mavericks
+    sha256 "2ae5eeef439a6abc4d1f65965e1bafa9ac5ad0620cb4ef5e9444a4b2dbef1872" => :mojave
+    sha256 "00f2578e7697c01d060a422e1be0ce8f4c6d23b365967ff7b5501d5cd6306dd1" => :high_sierra
+    sha256 "57acd0ca20988a1b4f0f16383edb985549597b8a5266316e3a314b7775bab3c0" => :sierra
   end
 
   def install
