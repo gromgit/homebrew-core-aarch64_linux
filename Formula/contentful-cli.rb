0ec0e04dccfc8dfc692a7709b3db9c5b3ec03b3a
Sean Molenaar <smillerdev@me.com>
2019-06-07 10:09:54 +0200

    contentful-cli 0.27.0 (new formula)
    Closes #40784.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/contentful-cli.rb
@@ -0,0 +1,23 @@
+require "language/node"
+
+class ContentfulCli < Formula
+  desc "Contentful command-line tools"
+  homepage "https://github.com/contentful/contentful-cli"
+  url "https://registry.npmjs.org/contentful-cli/-/contentful-cli-0.27.0.tgz"
+  sha256 "83dd533631719f50959652f805b6c08e93a9bf0dafc9cc9dedd11b47425dea15"
+  head "https://github.com/contentful/contentful-cli.git"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    output = shell_output("#{bin}/contentful space list 2>&1", 1)
+    assert_match "🚨  Error: You have to be logged in to do this.", output
+    assert_match "You can log in via contentful login", output
+    assert_match "Or provide a managementToken via --management-Token argument", output
+  end
+end
