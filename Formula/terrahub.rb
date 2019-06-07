8d79418ce71abf6e5d88e2772c9f92ed6c3afa99
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-07 19:23:00 +0000

    terrahub: update 0.2.74 bottle.

--- Formula/terrahub.rb
+++ Formula/terrahub.rb
@@ -8,9 +8,9 @@ class Terrahub < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "731e7bdf877210157a6580c72b61d116b9935da1f7693726655759bf92efefd8" => :mojave
-    sha256 "40ef9ad8e625efdb93b56b059b1a4c1a75e767128d7863be57b07641ecbad439" => :high_sierra
-    sha256 "bfa0260c4d16c51f36ce04dc44fe979400f12f550a11447c11b0bff744502768" => :sierra
+    sha256 "93605d312f1d2d28e97a059f125b204cf14f42f08fdcd9c0253bcf5c30fb7df8" => :mojave
+    sha256 "ca78d18ade34dc27c6a9adbb17b5715487bdc6b3886ba23f8c9512548e8795d0" => :high_sierra
+    sha256 "79de547287a24d7c24d96b79ed483b52452575dc259d58e6bceced166a6471bb" => :sierra
   end
 
   depends_on "node"
