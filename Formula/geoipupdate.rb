a571173ac06d6b7194a4cf69f6110a86e1ca9f7f
Gregory Oschwald <goschwald@maxmind.com>
2019-06-07 08:10:19 -0700

    geoipupdate 4.0.3
    Closes #40788.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/geoipupdate.rb
+++ Formula/geoipupdate.rb
@@ -1,8 +1,8 @@
 class Geoipupdate < Formula
   desc "Automatic updates of GeoIP2 and GeoIP Legacy databases"
   homepage "https://github.com/maxmind/geoipupdate"
-  url "https://github.com/maxmind/geoipupdate/archive/v4.0.2.tar.gz"
-  sha256 "76a2bd8e75fbe1d88cfdf0ab7f00e90cfea7c87b8757d6f5147a3d4d2d9773b3"
+  url "https://github.com/maxmind/geoipupdate/archive/v4.0.3.tar.gz"
+  sha256 "d8257e53091cef5591745f2cf5c730a0431e3b8440903709f1707ae4e291bb4a"
   head "https://github.com/maxmind/geoipupdate.git"
 
   bottle do
@@ -19,7 +19,7 @@ class Geoipupdate < Formula
     (buildpath/"src/github.com/maxmind/geoipupdate").install buildpath.children
 
     cd "src/github.com/maxmind/geoipupdate" do
-      system "make", "CONFFILE=#{etc}/GeoIP.conf", "DATADIR=#{var}/GeoIP"
+      system "make", "CONFFILE=#{etc}/GeoIP.conf", "DATADIR=#{var}/GeoIP", "VERSION=#{version} (homebrew)"
 
       bin.install  "build/geoipupdate"
       etc.install  "build/GeoIP.conf"
