43a5af8d6644b9fd86c6f47b8e537c87837c9da8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-07 11:58:09 +0000

    dust: add 0.3.1 bottle.

--- Formula/dust.rb
+++ Formula/dust.rb
@@ -5,6 +5,13 @@ class Dust < Formula
   sha256 "a10e0b2bc5862928a257e05866e077866193cc673d97a711ddd63eeecd075867"
   head "https://github.com/bootandy/dust.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "caf307790a9330aff377bf16ae36bb252c035fbf1d85d588265960ffac90c3d9" => :mojave
+    sha256 "3c80e045eb935b00192a88e5d00dece572506b23428d795dcc1ed93a3ff01ef5" => :high_sierra
+    sha256 "7e703fb0f7cee5e151432fe9522e3050f90de399dc2502d8618edf9ab58ada86" => :sierra
+  end
+
   depends_on "rust" => :build
 
   def install
