8039c1f89d0147fad1bcb8570337c93948862899
Rui Chen <rchen@meetup.com>
2019-06-08 19:48:08 -0400

    tfenv 1.0.0
    Closes #40813.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/tfenv.rb
+++ Formula/tfenv.rb
@@ -1,8 +1,8 @@
 class Tfenv < Formula
   desc "Terraform version manager inspired by rbenv"
   homepage "https://github.com/tfutils/tfenv"
-  url "https://github.com/tfutils/tfenv/archive/v0.6.0.tar.gz"
-  sha256 "f2ca32ade0e90d6988106e85efaa548ed116cac1ded24f88dd19f479c6381e2e"
+  url "https://github.com/tfutils/tfenv/archive/v1.0.0.tar.gz"
+  sha256 "a05756db765903aad63c8ce7661c8006768937d1b1414b7e4913ed35012c4b4e"
   head "https://github.com/tfutils/tfenv.git"
 
   bottle :unneeded
