bc638a61e13df6ef65415e0a16b355239b140024
Steven Peters <scpeters@openrobotics.org>
2019-06-09 16:24:33 -0700

    caffe: revision for protobuf

--- Formula/caffe.rb
+++ Formula/caffe.rb
@@ -3,7 +3,7 @@ class Caffe < Formula
   homepage "https://caffe.berkeleyvision.org/"
   url "https://github.com/BVLC/caffe/archive/1.0.tar.gz"
   sha256 "71d3c9eb8a183150f965a465824d01fe82826c22505f7aa314f700ace03fa77f"
-  revision 13
+  revision 14
 
   bottle do
     sha256 "a31068f71a8b250436e2d16d0f62e742b26d81e1eab9630da819a86471d18de1" => :mojave
