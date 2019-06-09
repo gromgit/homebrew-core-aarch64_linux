80745d7975635f4895ab9a04efce2b23bbe46f31
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-09 03:32:11 +0000

    boost-python: update 1.70.0 bottle.

--- Formula/boost-python.rb
+++ Formula/boost-python.rb
@@ -7,9 +7,9 @@ class BoostPython < Formula
 
   bottle do
     cellar :any
-    sha256 "b377edc8e5bc2ea6b3873d54d694298590278c291eb2820d408691e7491b853a" => :mojave
-    sha256 "51810bd5962d2ed44d696fb583de2a9f4b35084ad768d0789490701f748365a4" => :high_sierra
-    sha256 "23a5aadabf25a54196083be6478c105efd8c1503fc36330cfd5cafff62b16a80" => :sierra
+    sha256 "bcbd397976693b28ef44efb7012dd4fdfe758f480630df98d7645349d1590260" => :mojave
+    sha256 "7e990bb5bf4cc95ce30c5c99b7cfbbfb765434675dca94cbf3b42796c94bc865" => :high_sierra
+    sha256 "6d2a7161aff7e404b815ba853dea4b30f2c4691b8181d997b384c7bc4f7ca5dd" => :sierra
   end
 
   depends_on "boost"
