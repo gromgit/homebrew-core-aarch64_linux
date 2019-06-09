4b16aa5eb954c1801d6c7abf2e9d87a0a4a9ba00
Rui Chen <rchen@meetup.com>
2019-06-09 16:09:58 -0400

    helmsman 1.9.1 (new formula)
    Closes #40832.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/helmsman.rb
@@ -0,0 +1,33 @@
+class Helmsman < Formula
+  desc "Helm Charts as Code tool"
+  homepage "https://github.com/Praqma/helmsman"
+  url "https://github.com/Praqma/helmsman.git",
+    :tag      => "v1.9.1",
+    :revision => "2df5cca073e34c966e807727d29a4b2046e52c48"
+
+  depends_on "dep" => :build
+  depends_on "go" => :build
+  depends_on "kubernetes-cli"
+  depends_on "kubernetes-helm"
+
+  def install
+    ENV["GOPATH"] = buildpath
+
+    dir = buildpath/"src/github.com/Praqma/helmsman"
+    dir.install buildpath.children
+
+    cd dir do
+      system "dep", "ensure", "-vendor-only"
+      system "go", "build", "-o", bin/"helmsman"
+      prefix.install_metafiles
+      pkgshare.install "example.yaml"
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/helmsman version")
+
+    output = shell_output("#{bin}/helmsman --apply -f #{pkgshare}/example.yaml 2>&1", 1)
+    assert_match "helm diff plugin is not installed", output
+  end
+end
