f942f18ffcf00925dbb94886ba3697f6160b88bc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-10 02:30:54 +0000

    drone-cli: add 1.1.1 bottle.

--- Formula/drone-cli.rb
+++ Formula/drone-cli.rb
@@ -5,6 +5,13 @@ class DroneCli < Formula
     :tag      => "v1.1.1",
     :revision => "b796291a8a07a0300696ee3944418236751788e1"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c45a40ac12130a4dc15a6bcb41a33674b532736b5e30494f196e6c1dd0b5f38d" => :mojave
+    sha256 "7c30b8d8e8b257501d55e478fab4b9ef7601cc04f26c83b5d89e34f7e0f5a038" => :high_sierra
+    sha256 "7acd0eab3d379a33206e17d9c99386d9412ac38d65aeb871b7a6c426a3cf559f" => :sierra
+  end
+
   depends_on "dep" => :build
   depends_on "go" => :build
 
