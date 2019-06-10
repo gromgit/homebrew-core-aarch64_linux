b061f2742de1da4cb4883208af3119d30d791e5d
chenrui <rchen@meetup.com>
2019-06-10 05:35:50 -0400

    jenkins 2.180 (#40834)

--- Formula/jenkins.rb
+++ Formula/jenkins.rb
@@ -1,8 +1,8 @@
 class Jenkins < Formula
   desc "Extendable open source continuous integration server"
   homepage "https://jenkins.io/"
-  url "http://mirrors.jenkins.io/war/2.179/jenkins.war"
-  sha256 "30a6710977675c10197a2fc6de30187aec47d18609dc2679abe317758db66417"
+  url "http://mirrors.jenkins.io/war/2.180/jenkins.war"
+  sha256 "72ddb57dc6a7e7174d89c8ea5e9dd5aa7fbf31304f44101acd238734bcc12da8"
 
   head do
     url "https://github.com/jenkinsci/jenkins.git"
