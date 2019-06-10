5c8b7ec2c39f5801b5c5f1d5c610e599b62c56f2
Jason Karns <jason@karns.name>
2019-06-10 10:23:24 -0400

    node-build 4.6.1
    Closes #40721.
    
    Signed-off-by: Izaak Beekman <zbeekman@gmail.com>

--- Formula/node-build.rb
+++ Formula/node-build.rb
@@ -1,8 +1,8 @@
 class NodeBuild < Formula
   desc "Install NodeJS versions"
   homepage "https://github.com/nodenv/node-build"
-  url "https://github.com/nodenv/node-build/archive/v4.6.0.tar.gz"
-  sha256 "64108f5836beeaac5314813d62a36daab289207fbecc40fb5b998cac4bb2cb0b"
+  url "https://github.com/nodenv/node-build/archive/v4.6.1.tar.gz"
+  sha256 "b78de73430d9b2e7c0b4478c0ac297b5efb0fcabfa14b3f9f5f2adb89d552223"
   head "https://github.com/nodenv/node-build.git"
 
   bottle :unneeded
