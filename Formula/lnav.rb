5391453b535a71f3a568b00bc603dcea18194b49
Timothy Stack <timothyshanestack@gmail.com>
2019-06-11 16:48:06 -0700

    lnav: use brew sqlite
    Closes #40887.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/lnav.rb
+++ Formula/lnav.rb
@@ -1,9 +1,9 @@
 class Lnav < Formula
   desc "Curses-based tool for viewing and analyzing log files"
-  # lnav.org has an SSL issue: https://github.com/tstack/lnav/issues/401
-  homepage "https://github.com/tstack/lnav"
+  homepage "https://lnav.org/"
   url "https://github.com/tstack/lnav/releases/download/v0.8.5/lnav-0.8.5.tar.gz"
   sha256 "bb809bc8198d8f7395f3de76efdc1a08a5c2c97dc693040faee38802c38945de"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -22,15 +22,13 @@ class Lnav < Formula
 
   depends_on "pcre"
   depends_on "readline"
-  depends_on "sqlite" if MacOS.version < :sierra
+  depends_on "sqlite"
 
   def install
-    # Fix errors such as "use of undeclared identifier 'sqlite3_value_subtype'"
-    ENV.delete("SDKROOT")
-
     system "./autogen.sh" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
+                          "--with-sqlite=#{Formula["sqlite"].opt_prefix}",
                           "--with-readline=#{Formula["readline"].opt_prefix}"
     system "make", "install"
   end
