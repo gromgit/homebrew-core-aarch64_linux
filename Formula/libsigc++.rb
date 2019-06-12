2257d109c905f85eda3afc3b642b071fd2b90a90
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-06-12 20:01:40 +0100

    libsigc++ 2.10.2
    Closes #40931.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libsigc++.rb
+++ Formula/libsigc++.rb
@@ -1,8 +1,8 @@
 class Libsigcxx < Formula
   desc "Callback framework for C++"
   homepage "https://libsigcplusplus.github.io/libsigcplusplus/"
-  url "https://download.gnome.org/sources/libsigc++/2.10/libsigc++-2.10.1.tar.xz"
-  sha256 "c9a25f26178c6cbb147f9904d8c533b5a5c5111a41ac2eb781eb734eea446003"
+  url "https://download.gnome.org/sources/libsigc++/2.10/libsigc++-2.10.2.tar.xz"
+  sha256 "b1ca0253379596f9c19f070c83d362b12dfd39c0a3ea1dd813e8e21c1a097a98"
 
   bottle do
     cellar :any
