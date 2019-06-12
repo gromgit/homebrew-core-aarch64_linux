d63ed489e5a4640781c838c87bdb774190021afc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-12 14:51:30 +0000

    citus: update 8.2.2 bottle.

--- Formula/citus.rb
+++ Formula/citus.rb
@@ -7,9 +7,9 @@ class Citus < Formula
 
   bottle do
     cellar :any
-    sha256 "7862c0fbb653a9391541d56c36470f47d4709be74d5662549a6204171f608492" => :mojave
-    sha256 "5411ef1ac97150e4b406db86adf30d7a7f1a26b4bba415b094445c1ee6a5d2c5" => :high_sierra
-    sha256 "1ff38090d485d2f729ca243519042b3805642d917c6b95645c60107c4ac13f0a" => :sierra
+    sha256 "5c63f6d0d6ae9c1c9a52c9884195e821c2ffd4fe1c3f4bb6d1008d9b69a7e89c" => :mojave
+    sha256 "14d46f3abcffbc0036d146155fcc6948c5e8630a658faaabff2a46ea23e79f77" => :high_sierra
+    sha256 "ab414b49fec673f35b441c9be99dc3c3bad4562533a49ab04f07985711858d61" => :sierra
   end
 
   depends_on "postgresql"
