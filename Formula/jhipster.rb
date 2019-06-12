40b4335dfc47ba3dafe24f05128d7b7073479909
Rui Chen <rchen@meetup.com>
2019-06-11 21:53:25 -0400

    jhipster 6.1.0
    Closes #40891.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/jhipster.rb
+++ Formula/jhipster.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Jhipster < Formula
   desc "Generate, develop and deploy Spring Boot + Angular/React applications"
   homepage "https://www.jhipster.tech/"
-  url "https://registry.npmjs.org/generator-jhipster/-/generator-jhipster-6.0.1.tgz"
-  sha256 "a1f8d9d91fbc77f46209b5023f41a50fccf3bb31d7de3d3d4ae8ca667530bd21"
+  url "https://registry.npmjs.org/generator-jhipster/-/generator-jhipster-6.1.0.tgz"
+  sha256 "aed5edaa0e044bfbd74556383afbfd3d7146e66cd27f93dfed5f70825de74906"
 
   bottle do
     cellar :any_skip_relocation
