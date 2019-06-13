db83309a948a00eb480281c5a203a74546843ae4
Chongyu Zhu <i@lembacon.com>
2019-06-13 20:14:06 +0800

    gibo 2.2.2
    Closes #40944.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/gibo.rb
+++ Formula/gibo.rb
@@ -1,8 +1,8 @@
 class Gibo < Formula
   desc "Access GitHub's .gitignore boilerplates"
   homepage "https://github.com/simonwhitaker/gibo"
-  url "https://github.com/simonwhitaker/gibo/archive/2.2.1.tar.gz"
-  sha256 "f0de59f793fabf17cea8acf925fbe3f4c7627153b93b7c28d68cd96f0ea84c95"
+  url "https://github.com/simonwhitaker/gibo/archive/2.2.2.tar.gz"
+  sha256 "1ce2497b0d8a5ad165d7428c4e87e40072581f3ba6ff56b3fceeb53c52630442"
 
   bottle :unneeded
 
