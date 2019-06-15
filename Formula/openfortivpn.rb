ba44673fe39606eea8caa90fb3d95c51ec42e656
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-15 13:47:23 +0000

    openfortivpn: update 1.10.0 bottle.

--- Formula/openfortivpn.rb
+++ Formula/openfortivpn.rb
@@ -5,9 +5,9 @@ class Openfortivpn < Formula
   sha256 "d6ea0c84c0cf811530073fa19865334bb42ab10a780157fe95c4efb3476ad58d"
 
   bottle do
-    sha256 "5875f3c8a5099a1f452f8d6e6bbe387e2005c5874b7d10e735c75bb0fecde78e" => :mojave
-    sha256 "b3dd47124399a997924fcce9509803ccbcf69dabb93181c7f378975214b76f1d" => :high_sierra
-    sha256 "1fc3445fceb394dfa2d63a9036cc510d2f83e111ae82c1650c3ad5991e8497db" => :sierra
+    sha256 "30d87b41db1047d53b87f9fe79f706516426bf52f34e6af0f602dc69ebeb05ad" => :mojave
+    sha256 "52a83eaaeea9aa0bdf52d72f9976207ac6c18088c3cb60765e1567f8b2bff4ee" => :high_sierra
+    sha256 "878207725419186bfcf93ec05c8bc7348e4e68b65a6f92ef29a8245879aa0c0f" => :sierra
   end
 
   depends_on "autoconf" => :build
