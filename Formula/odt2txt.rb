346b35a3a0f3b844e0387ff563d849e6aa8ba255
Mike McQuaid <mike@mikemcquaid.com>
2019-06-15 17:27:20 +0100

    odt2txt: use new Resource#fetch API.

--- Formula/odt2txt.rb
+++ Formula/odt2txt.rb
@@ -25,7 +25,7 @@ class Odt2txt < Formula
 
   test do
     resources.each do |r|
-      r.verify_download_integrity(r.fetch)
+      r.fetch
       system "#{bin}/odt2txt", r.cached_download
     end
   end
