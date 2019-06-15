0bd4d3ada61c823dc4dccdbcd4507d6f8d4da7cd
Mike McQuaid <mike@mikemcquaid.com>
2019-06-15 17:27:21 +0100

    travis: use new Resource#fetch API.

--- Formula/travis.rb
+++ Formula/travis.rb
@@ -103,7 +103,7 @@ class Travis < Formula
   def install
     ENV["GEM_HOME"] = libexec
     resources.each do |r|
-      r.verify_download_integrity(r.fetch)
+      r.fetch
       system "gem", "install", r.cached_download, "--ignore-dependencies",
              "--no-document", "--install-dir", libexec
     end
