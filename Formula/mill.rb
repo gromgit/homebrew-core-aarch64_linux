db4fa1a19caef8969ae2593fa54941c0a56c3f77
Igor Kapkov <igasgeek@me.com>
2019-06-16 18:45:53 +1000

    mill 0.4.1 (#41007)

--- Formula/mill.rb
+++ Formula/mill.rb
@@ -1,8 +1,8 @@
 class Mill < Formula
   desc "Scala build tool"
   homepage "https://www.lihaoyi.com/mill/"
-  url "https://github.com/lihaoyi/mill/releases/download/0.4.0/0.4.0"
-  sha256 "745756498e5a67c55277534c62959c6fdfa382cda2c1a6e34ef0ea6b49c90880"
+  url "https://github.com/lihaoyi/mill/releases/download/0.4.1/0.4.1"
+  sha256 "9da400d31d3aebff1a68a7c1ab89a599955747b6635d42e75db0b4351930359e"
 
   bottle :unneeded
 
