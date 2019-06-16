de15a54ebd7bb2ed81dbb5bf9cd3666f7bd17f6c
lcdtyph <lcdtyph@gmail.com>
2019-06-17 01:30:09 +0800

    cxxopts 2.1.2 (new formula)
    Closes #41016.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/cxxopts.rb
@@ -0,0 +1,52 @@
+class Cxxopts < Formula
+  desc "Lightweight C++ command-line option parser"
+  homepage "https://github.com/jarro2783/cxxopts"
+  url "https://github.com/jarro2783/cxxopts/archive/v2.1.2.tar.gz"
+  sha256 "95f524c6615a2067e935e02ef74b013b17efa339df0a3c9db3e91fc0afbaf269"
+  head "https://github.com/jarro2783/cxxopts.git"
+
+  depends_on "cmake" => :build
+
+  def install
+    ENV.cxx11
+
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args,
+                            "-DCXXOPTS_BUILD_EXAMPLES=OFF",
+                            "-DCXXOPTS_BUILD_TESTS=OFF"
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cc").write <<~EOS
+      #include <iostream>
+      #include <cstdlib>
+      #include <cxxopts.hpp>
+
+      int main(int argc, char *argv[]) {
+          cxxopts::Options options(argv[0]);
+
+          std::string input;
+          options.add_options()
+              ("e,echo", "String to be echoed", cxxopts::value(input))
+              ("h,help", "Print this help", cxxopts::value<bool>()->default_value("false"));
+
+          auto result = options.parse(argc, argv);
+
+          if (result.count("help")) {
+              std::cout << options.help() << std::endl;
+              std::exit(0);
+          }
+
+          std::cout << input << std::endl;
+
+          return 0;
+      }
+    EOS
+
+    system ENV.cxx, "-std=c++11", "test.cc", "-I#{include}", "-o", "test"
+    assert_equal "echo string", shell_output("./test -e 'echo string'").strip
+    assert_equal "echo string", shell_output("./test --echo='echo string'").strip
+  end
+end
