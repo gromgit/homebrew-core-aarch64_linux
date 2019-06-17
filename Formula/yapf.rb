97dcaa7b212aadba912ce7911469920972ed5d51
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-17 14:37:11 +0000

    yapf: add 0.27.0 bottle.

--- Formula/yapf.rb
+++ Formula/yapf.rb
@@ -6,6 +6,13 @@ class Yapf < Formula
   url "https://files.pythonhosted.org/packages/0c/ad/1dd7e729e9d707c602267ed9a6ca9b771a507862f85456bf18f5fff8f0d1/yapf-0.27.0.tar.gz"
   sha256 "34f6f80c446dcb2c44bd644c4037a2024b6645e293a4c9c4521983dd0bb247a1"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "67911f42c81041f0d536ca48fec3aaeacaed7dae99c62729c8eb6d35760a8c7e" => :mojave
+    sha256 "2c886ed4aafbd5052a3d1221c282de5c7a5aa927dd3ba5bed2bc3ec29ac8946b" => :high_sierra
+    sha256 "cc84f98a4cdfd183c2ba7b86ce21612e063d22d2574559474dfeaa64281f8d03" => :sierra
+  end
+
   depends_on "python"
 
   def install
