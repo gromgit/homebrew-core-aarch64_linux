e668cea3b2cfa7c107dc6338f75052a26480c41c
Igor Kapkov <igasgeek@me.com>
2019-06-17 17:17:11 +1000

    monero 0.14.1.0
    Closes #41036.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/monero.rb
+++ Formula/monero.rb
@@ -2,9 +2,8 @@ class Monero < Formula
   desc "Official monero wallet and cpu miner"
   homepage "https://getmonero.org/"
   url "https://github.com/monero-project/monero.git",
-      :tag      => "v0.14.0.2",
-      :revision => "6cadbdcd2d952433db3c2422511ed4d13b2cc824"
-  revision 2
+      :tag      => "v0.14.1.0",
+      :revision => "29a505d1c1cfd3baa7d3a0c4433db8d7b043e341"
 
   bottle do
     cellar :any
@@ -26,11 +25,6 @@ class Monero < Formula
     url "https://github.com/zeromq/cppzmq/archive/v4.3.0.tar.gz"
     sha256 "27d1f56406ba94ee779e639203218820975cf68174f92fbeae0f645df0fcada4"
   end
-
-  # Upstream commit for Boost 1.70.0
-  # https://github.com/monero-project/monero/pull/5328
-  # reworked to apply on 0.14.0.2
-  patch :DATA
 
   def install
     (buildpath/"cppzmq").install resource("cppzmq")
@@ -77,48 +71,3 @@ class Monero < Formula
     assert_equal address, shell_output(cmd).lines.last.split[1]
   end
 end
-
-__END__
-diff -pur a/contrib/epee/include/net/abstract_tcp_server2.inl b/contrib/epee/include/net/abstract_tcp_server2.inl
---- a/contrib/epee/include/net/abstract_tcp_server2.inl	2019-03-07 14:25:34.000000000 +0100
-+++ b/contrib/epee/include/net/abstract_tcp_server2.inl	2019-06-08 17:02:47.000000000 +0200
-@@ -60,6 +60,12 @@
- #define DEFAULT_TIMEOUT_MS_REMOTE 300000 // 5 minutes
- #define TIMEOUT_EXTRA_MS_PER_BYTE 0.2
-
-+#if BOOST_VERSION >= 107000
-+#define GET_IO_SERVICE(s) ((boost::asio::io_context&)(s).get_executor().context())
-+#else
-+#define GET_IO_SERVICE(s) ((s).get_io_service())
-+#endif
-+
- PRAGMA_WARNING_PUSH
- namespace epee
- {
-@@ -215,7 +221,7 @@ PRAGMA_WARNING_DISABLE_VS(4355)
-   template<class t_protocol_handler>
-   boost::asio::io_service& connection<t_protocol_handler>::get_io_service()
-   {
--    return socket_.get_io_service();
-+    return GET_IO_SERVICE(socket_);
-   }
-   //---------------------------------------------------------------------------------
-   template<class t_protocol_handler>
-@@ -383,7 +389,7 @@ PRAGMA_WARNING_DISABLE_VS(4355)
-     if(!m_is_multithreaded)
-     {
-       //single thread model, we can wait in blocked call
--      size_t cnt = socket_.get_io_service().run_one();
-+      size_t cnt = GET_IO_SERVICE(socket_).run_one();
-       if(!cnt)//service is going to quit
-         return false;
-     }else
-@@ -393,7 +399,7 @@ PRAGMA_WARNING_DISABLE_VS(4355)
-       //if no handlers were called
-       //TODO: Maybe we need to have have critical section + event + callback to upper protocol to
-       //ask it inside(!) critical region if we still able to go in event wait...
--      size_t cnt = socket_.get_io_service().poll_one();
-+      size_t cnt = GET_IO_SERVICE(socket_).poll_one();
-       if(!cnt)
-         misc_utils::sleep_no_w(0);
-     }
