76811eab705692156ae715a7a44bf66a1c4c777b
Sean Molenaar <SMillerDev@users.noreply.github.com>
2019-06-18 14:45:09 +0200

    phpunit 8.2.2 (#41067)

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -1,8 +1,8 @@
 class Phpunit < Formula
   desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-8.2.1.phar"
-  sha256 "70be38d269c4ea8d098566535d33961ba2b8fca8e0e43a0e1d6288c63471e2f0"
+  url "https://phar.phpunit.de/phpunit-8.2.2.phar"
+  sha256 "ad18642ebf5e230da790e4f53b3ed7d226834bb22c8a6e0fc273f59bb7ba521f"
 
   bottle :unneeded
 
