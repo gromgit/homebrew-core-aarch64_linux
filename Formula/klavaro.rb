6fb746832c1b9ed4f0b557bb31a7460a3cbb75bc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-18 00:21:29 +0000

    klavaro: update 3.05_2 bottle.

--- Formula/klavaro.rb
+++ Formula/klavaro.rb
@@ -6,9 +6,9 @@ class Klavaro < Formula
   revision 2
 
   bottle do
-    sha256 "4d33d082a523b398afd28e9d22d5210b2b46a8ce436785363ecfe4047f7b49fc" => :mojave
-    sha256 "4ee0e35412caf80c0f9edf92153436919e6e673d696955a090a57d94565732b8" => :high_sierra
-    sha256 "b0b978b9d20523a2b36eca2f2da5ddec4f979908e4714ffcf7f2bb2ea6927592" => :sierra
+    sha256 "e7e5581d3c799500af68003a23c4c1235b24e86e1500b68ea9698977d1e03f96" => :mojave
+    sha256 "42b361645e169c196e5cf3efe0dcc4dca90e1cb36bd6a3fa46b477b7b88b2a77" => :high_sierra
+    sha256 "e169711632e0b63cdea433288dc29e6860cd52241cd7681afc7aea0eeb70c1ca" => :sierra
   end
 
   depends_on "intltool" => :build
