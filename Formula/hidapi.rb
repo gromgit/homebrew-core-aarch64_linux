4b8f7606fa06a718f914c96389918763e4aa547f
Luca Weiss <luca@z3ntu.xyz>
2019-06-18 20:05:56 +0200

    hidapi 0.9.0
    The original author is not active anymore and the project was forked to the libusb organization.
    
    See signal11/hidapi#373 and Homebrew/homebrew-core#41122
    
    Closes #41122.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/hidapi.rb
+++ Formula/hidapi.rb
@@ -1,9 +1,9 @@
 class Hidapi < Formula
   desc "Library for communicating with USB and Bluetooth HID devices"
-  homepage "https://github.com/signal11/hidapi"
-  url "https://github.com/signal11/hidapi/archive/hidapi-0.8.0-rc1.tar.gz"
-  sha256 "3c147200bf48a04c1e927cd81589c5ddceff61e6dac137a605f6ac9793f4af61"
-  head "https://github.com/signal11/hidapi.git"
+  homepage "https://github.com/libusb/hidapi"
+  url "https://github.com/libusb/hidapi/archive/hidapi-0.9.0.tar.gz"
+  sha256 "630ee1834bdd5c5761ab079fd04f463a89585df8fcae51a7bfe4229b1e02a652"
+  head "https://github.com/libusb/hidapi.git"
 
   bottle do
     cellar :any
@@ -20,17 +20,6 @@ class Hidapi < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-
-  # This patch addresses a bug discovered in the HidApi IOHidManager back-end
-  # that is being used with Macs.
-  # The bug was dramatically changing the behaviour of the function
-  # "hid_get_feature_report". As a consequence, many applications working
-  # with HidApi were not behaving correctly on OSX.
-  # pull request on Hidapi's repo: https://github.com/signal11/hidapi/pull/219
-  patch do
-    url "https://github.com/signal11/hidapi/pull/219.patch?full_index=1"
-    sha256 "c0ff6eb370d6b875c06d72724a1a12fa0bafcbd64b2610014abc50a516760240"
-  end
 
   def install
     system "./bootstrap"
