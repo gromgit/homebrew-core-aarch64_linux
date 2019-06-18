7c5a7bd50ef5e194e18e351ca8a6792659c311da
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-18 00:21:35 +0000

    gpa: update 0.10.0_1 bottle.

--- Formula/gpa.rb
+++ Formula/gpa.rb
@@ -7,9 +7,9 @@ class Gpa < Formula
   revision 1
 
   bottle do
-    sha256 "fa40434b7e39fb6fd15f67ea757c9b18f3e3c0cdb0eb533940517d51793e3cfd" => :mojave
-    sha256 "ba20aa21492d9b334cbf4f1dc2ea072d0302f8a97fcd2537fc38f2539e4a19b8" => :high_sierra
-    sha256 "5ef7f508f0b6a8bb688b417abd865e53da69dea0d5e048ba0da0686bd2a67043" => :sierra
+    sha256 "183d889ae94650931d7ed470ec643a9e5b02843e037a97de2669f2d48076e996" => :mojave
+    sha256 "dc9d49486a627330931c0d2caeb6af4629e3871bdbff840ae33e5afb97c087ba" => :high_sierra
+    sha256 "73e3e3c27b72b36ed733b01435dd4eb9538f036cf1a817d48a171e58b70de395" => :sierra
   end
 
   depends_on "pkg-config" => :build
