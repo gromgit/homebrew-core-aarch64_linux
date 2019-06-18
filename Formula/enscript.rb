e903eb59327d03f49a0d05ecf8fe3d0cac725439
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-18 00:21:20 +0000

    enscript: update 1.6.6_1 bottle.

--- Formula/enscript.rb
+++ Formula/enscript.rb
@@ -8,13 +8,9 @@ class Enscript < Formula
   head "https://git.savannah.gnu.org/git/enscript.git"
 
   bottle do
-    rebuild 1
-    sha256 "5e66a847d27142da7b24603988a3f31cc841fdab66c9cb658a8a0f5f4da9a82c" => :mojave
-    sha256 "4b9fefd4bafc5f190272b8ed5bc4c0fbdaf4e1cdfa03aee7b7ef2f829500bc0b" => :high_sierra
-    sha256 "dea5f069c92bd1b5d2e3c1f0440d8ed5281d9ee44225e28295a1f682ff43a934" => :sierra
-    sha256 "e55d3f93f7a4eb89d8007d9c0c49d6b7f52778191f2601da648afff0098a6663" => :el_capitan
-    sha256 "d1c1bfc90a9e140a3d257d976729fc9b6e55118a10364ce1e3dc3dd26388edc9" => :yosemite
-    sha256 "f2be9be9caeff58dbec3c9abf3ff5554865e6a3ee4db91d156edce8ddf5e666e" => :mavericks
+    sha256 "a8bbba8f7d64eed40dd59a9db980b049ec786e148d31a0aeb92556959b4ad0b0" => :mojave
+    sha256 "00045dff3bdf7ac98a19236838d7af7101cc1fc002e55550312042bb2e4d7426" => :high_sierra
+    sha256 "c14fad6cfd67fa782beb7a425eb03c3ed0b8090ed751c37f5f5ec426808df25c" => :sierra
   end
 
   depends_on "gettext"
