56e64323268650d6cdd82ea267fcb7c443833cc0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-18 00:21:05 +0000

    httping: update 2.5_1 bottle.

--- Formula/httping.rb
+++ Formula/httping.rb
@@ -8,11 +8,9 @@ class Httping < Formula
 
   bottle do
     cellar :any
-    sha256 "3f625be44bdc094c374ba25d0f0ecd8c209b96f40e00eac874929edfe71a94c7" => :mojave
-    sha256 "99b687e9e7cbadbb3e1774e89538395e7630152631fa90471d6f784927759c4c" => :high_sierra
-    sha256 "b209aa24927ed620ce6a7e676c7358ca94d17ec456c8b7b49b72b1aec57c44ed" => :sierra
-    sha256 "f283d8cde06988fa6117d7cb1e008ae480851e8f4e9ba60fd8f429864499f983" => :el_capitan
-    sha256 "c4784fc08f239a6fd0f778657fe11509f445ee889f6d6b305c30c533be25f35c" => :yosemite
+    sha256 "f328421637732811f7c15d5a8c7d33cc9bdf415a95cb795197914287a539001b" => :mojave
+    sha256 "876b72e65eda11adf593340ca26d71cf8df798e0673ab2a1c5cd3451bef90e6a" => :high_sierra
+    sha256 "e72f17a2e9cd1a77330984b9037e9feba6abd3fd4d45a041b93a0e81a3439a81" => :sierra
   end
 
   depends_on "gettext"
