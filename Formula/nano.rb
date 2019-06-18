76c61dd5d09bc31878f660d417a0fbcca80c4b3c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-18 16:26:03 +0000

    nano: update 4.3 bottle.

--- Formula/nano.rb
+++ Formula/nano.rb
@@ -5,9 +5,9 @@ class Nano < Formula
   sha256 "63b6dd621e3e21bb130ca931d761f84b48d974a7b5b4d489189c2df354ca2b4a"
 
   bottle do
-    sha256 "a9fe516b43923e6a4214880e585444a5cc85fae01a7ea3635c134edea758235d" => :mojave
-    sha256 "f59cab3a05fe138f2d04ca967e51a40cf78f397f4fc89719f498580b86559d5d" => :high_sierra
-    sha256 "df3ab79f5a15e849597930370c7c0c71baf107142f0a6a0a4aae78b2f761d80e" => :sierra
+    sha256 "7c9dd05e91de4291d7b5781e028f3d3389fee61f73d63d0a4a9c0fd6aa8a0e09" => :mojave
+    sha256 "66dcf75d2f320c4990d769a82588c579dd5e979c4397a203c3bbf3f25e18d823" => :high_sierra
+    sha256 "333b2cd8fb4c88833dc59061838c4fbd671d0e519d04d21a886b85ab144c4a79" => :sierra
   end
 
   depends_on "pkg-config" => :build
