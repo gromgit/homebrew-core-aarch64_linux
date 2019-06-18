6c477766c0cb52edd16eb4a6920b05961fe2b3cd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-18 19:03:05 +0000

    terraformer: update 0.7.5 bottle.

--- Formula/terraformer.rb
+++ Formula/terraformer.rb
@@ -7,9 +7,9 @@ class Terraformer < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7fc251c6c6b88a7f6878aaba2d28a5eda6d86356d59eebcf6b508f8485b75910" => :mojave
-    sha256 "8d35fd49630cd0ae536c1239f2a8a79ddac03e776f9f1c8770117b80dc52c8ba" => :high_sierra
-    sha256 "6dd19a2e9adc5140690cbbf1ac468b817d63281df612e4df7e60aa2ad1e85cdb" => :sierra
+    sha256 "cfc63b42ced4a3b9b7f6e6487258100c03ae4f7e7dc574b6b5dceb7135efcee9" => :mojave
+    sha256 "1811cbc5c59aaeec3ab972836240da115e75fa4ff588a94fbc780d0de10b85cb" => :high_sierra
+    sha256 "c19ea7f9c303ed2c35ddaefb59358c791b3ddc5f5c6be5d3c42aea2d20de7861" => :sierra
   end
 
   depends_on "go" => :build
