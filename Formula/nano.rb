5dc8d710e42f9119b2ca885f22254285449f350c
Marc Schwede <git@schwedenmut.de>
2019-06-18 17:39:32 +0200

    nano 4.3
    Closes #41077.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/nano.rb
+++ Formula/nano.rb
@@ -1,9 +1,8 @@
 class Nano < Formula
   desc "Free (GNU) replacement for the Pico text editor"
   homepage "https://www.nano-editor.org/"
-  url "https://www.nano-editor.org/dist/v4/nano-4.2.tar.gz"
-  sha256 "c06e456890a93e7a61317a938c70caacaa2bfd6161a52b2731df83875ebbcf80"
-  revision 1
+  url "https://www.nano-editor.org/dist/v4/nano-4.3.tar.gz"
+  sha256 "63b6dd621e3e21bb130ca931d761f84b48d974a7b5b4d489189c2df354ca2b4a"
 
   bottle do
     sha256 "a9fe516b43923e6a4214880e585444a5cc85fae01a7ea3635c134edea758235d" => :mojave
