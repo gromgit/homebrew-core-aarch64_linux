9c7bc8cc1c041ad887f1a5be156f4a07fa038e42
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-18 00:21:05 +0000

    streamripper: update 1.64.6_1 bottle.

--- Formula/streamripper.rb
+++ Formula/streamripper.rb
@@ -7,12 +7,9 @@ class Streamripper < Formula
 
   bottle do
     cellar :any
-    sha256 "79591e5c81bf59b36533ff8d7ebcab361df3a4c8809d4adf6086d0b1dee49372" => :mojave
-    sha256 "e0f65bd98aea085a8c2cc09e8d4a4f383a84bc18ebe39d73f466e538bea7bad4" => :high_sierra
-    sha256 "3f055510dc825aa663f35c91aa5f4e5a57baacd69c00f0c428df4a98ad9b6a7e" => :sierra
-    sha256 "eff1bb37cd652e9b3194e2fda3c682bda9da12f413a11c4e5e337c9bc849b2ea" => :el_capitan
-    sha256 "3465e96b5f17000df88a85729674f911097ab9f1b0170a0c3c89f4892dba6fbb" => :yosemite
-    sha256 "a92b924639b9210e83cae1e63baa8f9b45ab4ec38816e19e32ad6fbae420a510" => :mavericks
+    sha256 "559e6ce06f450c306178c1e361154f134c3478ad1bc35ca70d0d3f000938043d" => :mojave
+    sha256 "9df7827f89ef7f517ccfdb52be976b358ede1ceb2690f8617b4cc52da7c4cf41" => :high_sierra
+    sha256 "2ccd049ca0ce6720055a86b726bfb1388b4e3784b2cd597bc7b26fd1e593a60c" => :sierra
   end
 
   depends_on "pkg-config" => :build
