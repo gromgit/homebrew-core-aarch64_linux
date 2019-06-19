ba3999c8e3983a1d7cdbc51be5f0dc21052cec10
Rui Chen <rchen@meetup.com>
2019-06-18 21:07:23 -0400

    grakn 1.5.5
    Closes #41103.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/grakn.rb
+++ Formula/grakn.rb
@@ -1,8 +1,8 @@
 class Grakn < Formula
   desc "The distributed hyper-relational database for knowledge engineering"
   homepage "https://grakn.ai"
-  url "https://github.com/graknlabs/grakn/releases/download/1.5.4/grakn-core-all-mac-1.5.4.zip"
-  sha256 "5dd4b278a2da8d91613acda1ad2d1a797cd190a21c40fb483db9cbcaa0b81876"
+  url "https://github.com/graknlabs/grakn/releases/download/1.5.5/grakn-core-all-mac-1.5.5.zip"
+  sha256 "155f762140babd722c0918eac4a2bc4ea3afbf1dbfaef33cebb8254810eb3db1"
 
   bottle :unneeded
 
