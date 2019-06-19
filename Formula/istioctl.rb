dc71d2895b23a552ebbdb78613e919089dfd1299
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-19 00:46:25 +0000

    istioctl: update 1.2.0 bottle.

--- Formula/istioctl.rb
+++ Formula/istioctl.rb
@@ -7,9 +7,9 @@ class Istioctl < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e7ac23ec1c8e84fc36c31e6edfcfbd7aafbd9c39f3e4a3e935541d90544c88e9" => :mojave
-    sha256 "21ff69eff51e99fe6f974232796df2bc48cde9d1fdce1e6ef88137e2276bf1ad" => :high_sierra
-    sha256 "93c21981e32a4c539a3b73d7de401380049328e417b57dd1fb62757adbd1e60b" => :sierra
+    sha256 "6986d77e7093e7eb5cf8944cae6eef7cf1c102f940fcf9447a06fd7f50332a9f" => :mojave
+    sha256 "0cfa110527586939105bd1d8c369594ed430be8142bd650a3d9301c0b917f24a" => :high_sierra
+    sha256 "8362b4875d0f78f42f0ffc6531d4c1ca5fd16662fa7d1f5d83e7f3e99a68451a" => :sierra
   end
 
   depends_on "go" => :build
