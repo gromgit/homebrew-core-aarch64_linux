a0fb853ed322801c3312faa15061ff4a27c68a43
Igor Kapkov <igasgeek@me.com>
2019-06-19 17:25:32 +1000

    clozure-cl 1.11.6
    Closes #41112.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/clozure-cl.rb
+++ Formula/clozure-cl.rb
@@ -1,8 +1,8 @@
 class ClozureCl < Formula
   desc "Common Lisp implementation with a long history"
   homepage "https://ccl.clozure.com"
-  url "https://github.com/Clozure/ccl/archive/v1.11.5.tar.gz"
-  sha256 "07c7e05c1d50ccbf1f7602a1d436b6d6da5dcda7656b89b74daa4cf833fc7929"
+  url "https://github.com/Clozure/ccl/archive/v1.11.6.tar.gz"
+  sha256 "6a496d35e05dc3e6e7637884552b1f14c82296525546f28337b222e4c3d7d50b"
   head "https://github.com/Clozure/ccl.git"
 
   bottle do
@@ -36,7 +36,8 @@ class ClozureCl < Formula
                          "-e", "(ccl:xload-level-0)",
                          "-e", "(ccl:compile-ccl)",
                          "-e", "(quit)"
-    system "echo \"(ccl:save-application \\\"dx86cl64.image\\\")\\n(quit)\" | ./dx86cl64 -n --image-name x86-boot64.image"
+    (buildpath/"image").write('(ccl:save-application "dx86cl64.image")\n(quit)\n')
+    system "cat image | ./dx86cl64 -n --image-name x86-boot64.image"
 
     prefix.install "doc/README"
     doc.install Dir["doc/*"]
