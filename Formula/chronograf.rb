01c4b4d78a4823668ef955284d4a92ab7fbdc590
Rui Chen <rchen@meetup.com>
2019-06-21 06:57:31 +0800

    chronograf 1.7.12
    Closes #41186.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/chronograf.rb
+++ Formula/chronograf.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Chronograf < Formula
   desc "Open source monitoring and visualization UI for the TICK stack"
   homepage "https://docs.influxdata.com/chronograf/latest/"
-  url "https://github.com/influxdata/chronograf/archive/1.7.11.tar.gz"
-  sha256 "576e24ab31f3134edaf72651fa288eb4e6f1461662730a57af63096a088d0664"
+  url "https://github.com/influxdata/chronograf/archive/1.7.12.tar.gz"
+  sha256 "b98b63577a039d3d505bcf953b3067fe180d09a91688ec162f9c8f00ee816dd3"
   head "https://github.com/influxdata/chronograf.git"
 
   bottle do
@@ -27,10 +27,10 @@ class Chronograf < Formula
     chronograf_path = buildpath/"src/github.com/influxdata/chronograf"
     chronograf_path.install buildpath.children
 
-    # fixes yarn + upath@1.0.4 incompatibility, remove once upath is upgraded to 1.0.5+
-    Pathname.new("#{ENV["HOME"]}/.yarnrc").write("ignore-engines true\n")
-
     cd chronograf_path do
+      cd "ui" do # fix node 12 compatibility
+        system "yarn", "upgrade", "parcel@1.11.0", "node-sass@4.12.0"
+      end
       system "make", "dep"
       system "make", ".jssrc"
       system "make", "chronograf"
@@ -75,7 +75,7 @@ class Chronograf < Formula
       pid = fork do
         exec "#{bin}/chronograf"
       end
-      sleep 1
+      sleep 3
       output = shell_output("curl -s 0.0.0.0:8888/chronograf/v1/")
       sleep 1
       assert_match %r{/chronograf/v1/layouts}, output
