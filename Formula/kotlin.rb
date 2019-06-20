840ea6fde37781b979f2c5d85da1f2a771e465b5
Ian Brandt <ian@ianbrandt.com>
2019-06-19 23:34:25 -0700

    kotlin 1.3.40 (#41141)

--- Formula/kotlin.rb
+++ Formula/kotlin.rb
@@ -1,8 +1,8 @@
 class Kotlin < Formula
   desc "Statically typed programming language for the JVM"
   homepage "https://kotlinlang.org/"
-  url "https://github.com/JetBrains/kotlin/releases/download/v1.3.31/kotlin-compiler-1.3.31.zip"
-  sha256 "107325d56315af4f59ff28db6837d03c2660088e3efeb7d4e41f3e01bb848d6a"
+  url "https://github.com/JetBrains/kotlin/releases/download/v1.3.40/kotlin-compiler-1.3.40.zip"
+  sha256 "48c0b1041b07b96dd9851388beade8f3c545263de94566b10f0ab4700ee8c392"
 
   bottle :unneeded
 
