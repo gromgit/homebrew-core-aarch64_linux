4d4ac766b880458c51cdb8fef534e1cb57bdae02
Jonathan Chang <jchang641@gmail.com>
2019-06-20 13:12:39 -0400

    nethack: revision for curses
    * Add curses window support
    * Drop support for pre-Mavericks version
    * Terser specification for build options
    * Add test
    
    Closes #39649
    
    Closes #41177.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/nethack.rb
+++ Formula/nethack.rb
@@ -6,6 +6,7 @@ class Nethack < Formula
   url "https://www.nethack.org/download/3.6.2/nethack-362-src.tgz"
   version "3.6.2"
   sha256 "fbd00ada6a4ee347ecd4a350a5b2995b4b4ab5dcc63881b3bc4485b0479ddb1d"
+  revision 1
   head "https://git.code.sf.net/p/nethack/NetHack.git", :branch => "NetHack-3.6.2"
 
   bottle do
@@ -13,6 +14,8 @@ class Nethack < Formula
     sha256 "ab98815750c7bf5c38e7bbdaf2c8e55588a0c73070868b929cff096918c6f6f8" => :high_sierra
     sha256 "6ab73c6cca85d88e4a9d8fd0456f1a8bb98089c9bd9fff43f22cd0b457ab9684" => :sierra
   end
+
+  uses_from_macos "ncurses"
 
   # Don't remove save folder
   skip_clean "libexec/save"
@@ -23,26 +26,20 @@ class Nethack < Formula
 
     # Generate makefiles for OS X
     cd "sys/unix" do
-      if MacOS.version >= :yosemite
-        hintfile = "macosx10.10"
+      if MacOS.version >= :mojave
+        hintfile = "macosx10.14"
       else
-        hintfile = "macosx10.7"
+        hintfile = "macosx10.10"
       end
 
-      inreplace "hints/#{hintfile}",
-                /^HACKDIR=.*/,
-                "HACKDIR=#{libexec}"
+      # Enable wizard mode for all users
+      inreplace "sysconf", /^WIZARDS=.*/, "WIZARDS=*"
 
       system "sh", "setup.sh", "hints/#{hintfile}"
     end
 
-    # Enable wizard mode for all users
-    inreplace "sys/unix/sysconf",
-      /^WIZARDS=.*/,
-      "WIZARDS=*"
-
-    # Make the game
-    system "make", "install"
+    # Make the game with curses
+    system "make", "install", "HACKDIR=#{libexec}", "WANT_WIN_CURSES=1"
     bin.install "src/nethack"
     (libexec+"save").mkpath
 
@@ -52,5 +49,9 @@ class Nethack < Formula
     # These need to be group-writable in multi-user situations
     chmod "g+w", libexec
     chmod "g+w", libexec+"save"
+  end
+
+  test do
+    system "#{bin}/nethack", "-s"
   end
 end
