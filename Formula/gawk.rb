52c2111bdb1847bdb81581c38ee3c687c03892eb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-20 10:20:41 +0000

    gawk: update 5.0.1 bottle.

--- Formula/gawk.rb
+++ Formula/gawk.rb
@@ -6,9 +6,9 @@ class Gawk < Formula
   sha256 "8e4e86f04ed789648b66f757329743a0d6dfb5294c3b91b756a474f1ce05a794"
 
   bottle do
-    sha256 "47559c1ecaa9ee95cac1f7ae5c2a56c16e206c4f1b0bf1b25d6c0ee5ad8f5ac3" => :mojave
-    sha256 "1871a29927d77bc704a088ab5750af3ad56c08ae4762c8faafca1a39b0be0bac" => :high_sierra
-    sha256 "d0ea7024988dc4c6d43e3bf65b48640fcb67160bddbfd3bb08888be642bfeeff" => :sierra
+    sha256 "344c60c2d5c4fa952f2ae0c43efbef0e447b045e2ae648d11886483b3375dc36" => :mojave
+    sha256 "9aaa755a220119fabb40dbb4b0edecabd976d34f0ac5d4f1277eb0f0fc8e4933" => :high_sierra
+    sha256 "a69334c37cf4c58c44f2974bf092a2531798ff4ace6aa0ee884798b375dd9708" => :sierra
   end
 
   depends_on "gettext"
