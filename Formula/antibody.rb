e5164b1d5a3b2810711aab13a12fb85899615186
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-21 14:53:33 +0000

    antibody: add 4.1.2 bottle.

--- Formula/antibody.rb
+++ Formula/antibody.rb
@@ -4,6 +4,13 @@ class Antibody < Formula
   url "https://github.com/getantibody/antibody/archive/v4.1.2.tar.gz"
   sha256 "79e857c79cf51bff0bf42ef970a31341445911dc19cf24efb8faa01584855905"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d85294b01d76ac4fae8c9ae9c331f4cd26d3c397a024f5edc2891b4a881fe6a8" => :mojave
+    sha256 "140c847a4b90705e0b2c4e84ab4a3998e0800b646f79034420679a63644d9154" => :high_sierra
+    sha256 "c7cbfedc71307426d7d83602be02d5f46f3ad8d53ec6c46cd6f26e07c226f641" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
