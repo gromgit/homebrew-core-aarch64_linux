12ca9334178e23e3f1b53e7d64d0d1b0dfa9df84
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-22 14:13:40 +0000

    devspace: add 3.5.8 bottle.

--- Formula/devspace.rb
+++ Formula/devspace.rb
@@ -5,6 +5,13 @@ class Devspace < Formula
     :tag      => "v3.5.8",
     :revision => "3efff768ae61fffbfb224a51406ba54e6b670a02"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c4fd2de185f666e08638c9fb26aca9bed0fbdd1243bf9e6e5e7a678a2d975843" => :mojave
+    sha256 "d03af3dd5f4b7dd11cd9a2e15b7025bbcb014a2fabd35090880216da300dc1b0" => :high_sierra
+    sha256 "3e80c774662a3df41c0ce0e974dac983e58ce9977a0de0d8508e01edbd52da28" => :sierra
+  end
+
   depends_on "go" => :build
   depends_on "kubernetes-cli"
 
