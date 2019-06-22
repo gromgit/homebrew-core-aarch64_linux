f58ad1d3876b6d9eba04e1e7f263632bb0935f45
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-22 17:03:55 +0000

    openkim-models: add 2019-03-29 bottle.

--- Formula/openkim-models.rb
+++ Formula/openkim-models.rb
@@ -4,6 +4,12 @@ class OpenkimModels < Formula
   url "https://s3.openkim.org/archives/collection/openkim-models-2019-03-29.txz"
   sha256 "053dda2023fe4bb6d7c1d66530c758c4e633bbf1f1be17b6b075b276fe8874f6"
 
+  bottle do
+    sha256 "5df2dd7cafd40fa451fb1cd12df7b32f1de2fbde7d8f5de4c64fd5cd63b72ce5" => :mojave
+    sha256 "38f72e300dec247950382a5ea5c88950fb5832ef591c28aa49747109efc20576" => :high_sierra
+    sha256 "f98efcd8602275d5831503dbb319de6b0189776c54612808c3ec53776ba9ccee" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "kim-api"
 
