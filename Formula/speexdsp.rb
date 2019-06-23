ba35d720b651c1e6d3d2c9b417a66b55de541b7e
Igor Kapkov <igasgeek@me.com>
2019-06-23 20:03:52 +1000

    speexdsp 1.2.0
    Closes #41258.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/speexdsp.rb
+++ Formula/speexdsp.rb
@@ -1,8 +1,8 @@
 class Speexdsp < Formula
   desc "Speex audio processing library"
   homepage "https://github.com/xiph/speexdsp"
-  url "https://github.com/xiph/speexdsp/archive/SpeexDSP-1.2rc3.tar.gz"
-  sha256 "e8be7482df7c95735e5466efb371bd7f21115f39eb45c20ab7264d39c57b6413"
+  url "https://github.com/xiph/speexdsp/archive/SpeexDSP-1.2.0.tar.gz"
+  sha256 "d7032f607e8913c019b190c2bccc36ea73fc36718ee38b5cdfc4e4c0a04ce9a4"
 
   bottle do
     cellar :any
