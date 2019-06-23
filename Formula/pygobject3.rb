748d2a7a2f759eb028d95cc814640a09148bc3d6
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-06-23 16:51:24 +0100

    pygobject3 3.32.2
    Closes #41263.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/pygobject3.rb
+++ Formula/pygobject3.rb
@@ -1,8 +1,8 @@
 class Pygobject3 < Formula
   desc "GNOME Python bindings (based on GObject Introspection)"
   homepage "https://wiki.gnome.org/Projects/PyGObject"
-  url "https://download.gnome.org/sources/pygobject/3.32/pygobject-3.32.1.tar.xz"
-  sha256 "32c99def94b8dea5ce9e4bc99576ef87591ea779b4db77cfdca7af81b76d04d8"
+  url "https://download.gnome.org/sources/pygobject/3.32/pygobject-3.32.2.tar.xz"
+  sha256 "c39ca2a28364b57fa00549c6e836346031e6b886c3ceabfd8ab4b4fed0a83611"
 
   bottle do
     cellar :any
