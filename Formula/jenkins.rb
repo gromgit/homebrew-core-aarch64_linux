65f94bb6073e8860280ad03093fed53859289bdb
Michihito Shigemura <i.am.shige@gmail.com>
2019-06-24 16:39:01 +0900

    jenkins 2.182 (#41278)

--- Formula/jenkins.rb
+++ Formula/jenkins.rb
@@ -1,8 +1,8 @@
 class Jenkins < Formula
   desc "Extendable open source continuous integration server"
   homepage "https://jenkins.io/"
-  url "http://mirrors.jenkins.io/war/2.181/jenkins.war"
-  sha256 "90bc35938d30d30440fcd871317a6ec5f3aff9160f30660d63d7e22df1fb4d9b"
+  url "http://mirrors.jenkins.io/war/2.182/jenkins.war"
+  sha256 "fc337a3c7d88fbee994f9f76edc84fc801b47b87b8a7cfd6337b40d98aecfb84"
 
   head do
     url "https://github.com/jenkinsci/jenkins.git"
