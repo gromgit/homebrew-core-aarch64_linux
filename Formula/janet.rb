4d67b68d5fd20b397ba3dd15f7727b9a59fa0c81
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-24 18:32:16 +0000

    janet: add 0.6.0 bottle.

--- Formula/janet.rb
+++ Formula/janet.rb
@@ -5,6 +5,13 @@ class Janet < Formula
   sha256 "1226240f8ed7f5faafbff6d93e5802c7959c4b40f9212ac6f020d67ef3aa599e"
   head "https://github.com/janet-lang/janet.git"
 
+  bottle do
+    cellar :any
+    sha256 "d71616a90f9325ff880f49ee33160634b52e2ffa1775e1f6f718690acf8b430f" => :mojave
+    sha256 "14cc9e8261b68bebff521ee58337caea76f6ad825e2ea96539f7d34e13ebee19" => :high_sierra
+    sha256 "1f1af1a159b155f3190e57a6143cccad6d7393affd47048f3c319dc136d6044f" => :sierra
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
 
