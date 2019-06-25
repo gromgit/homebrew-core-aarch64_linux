609c95812bcbf1f8f17bb6bc7f80e4d319bec3ec
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-25 14:45:56 +0000

    corectl: update 0.7.18_2 bottle.

--- Formula/corectl.rb
+++ Formula/corectl.rb
@@ -8,11 +8,9 @@ class Corectl < Formula
 
   bottle do
     cellar :any
-    sha256 "0b7b837ae2b500b08b342dd0b2cf13cf5a21b56321e9da285767cde2b5c3b56d" => :mojave
-    sha256 "468a78c0f43c9d150313edadd23e4a373fa689243fcd0f6c48af79feb0b1854d" => :high_sierra
-    sha256 "9b83542911995f649091f49cd839c949e975ae71caa220cdc224dbb86f8fd638" => :sierra
-    sha256 "1d90b568db5c0ec1025b0bacb5b06b794b8e24d198cdcf036d2ff81ebd6168da" => :el_capitan
-    sha256 "ae67e7433832ac259736eaa9879e18d7b724f6ffc823f487b31eb1447780f72a" => :yosemite
+    sha256 "b3d030cf97c738ef427b24cd492a7b746b738be84f234f5904eedbff14661570" => :mojave
+    sha256 "74527235d27b207a4b4331f16cfbb4f5b72b1dac36f9c9a4470626c32e882d5f" => :high_sierra
+    sha256 "89e963f61102d26d5fe756b06f50aa73bf9f827f81f92cefa2da6c195b7865da" => :sierra
   end
 
   depends_on "aspcud" => :build
