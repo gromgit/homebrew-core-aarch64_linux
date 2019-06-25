aa5d9bf4562d099368073792cb8ad5f50edcce12
Rui Chen <rchen@meetup.com>
2019-06-26 06:33:04 +0800

    grakn 1.5.6
    Closes #41339.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/grakn.rb
+++ Formula/grakn.rb
@@ -1,8 +1,8 @@
 class Grakn < Formula
   desc "The distributed hyper-relational database for knowledge engineering"
   homepage "https://grakn.ai"
-  url "https://github.com/graknlabs/grakn/releases/download/1.5.5/grakn-core-all-mac-1.5.5.zip"
-  sha256 "155f762140babd722c0918eac4a2bc4ea3afbf1dbfaef33cebb8254810eb3db1"
+  url "https://github.com/graknlabs/grakn/releases/download/1.5.6/grakn-core-all-mac-1.5.6.zip"
+  sha256 "bba92e7cd49abcf373e66301c2712bef70677560017d707e695c31d36631dd80"
 
   bottle :unneeded
 
