c75e6e54b79b5db390f6f1d470d81d04365d3844
Rui Chen <rchen@meetup.com>
2019-06-26 06:40:20 +0800

    istioctl 1.2.1
    Closes #41342.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/istioctl.rb
+++ Formula/istioctl.rb
@@ -2,8 +2,8 @@ class Istioctl < Formula
   desc "Istio configuration command-line utility"
   homepage "https://github.com/istio/istio"
   url "https://github.com/istio/istio.git",
-      :tag      => "1.2.0",
-      :revision => "f8295503296ec6bae1b1047cc1491469d5e72754"
+      :tag      => "1.2.1",
+      :revision => "bbe7afd4d157ac105e84b6b9fc9cb695f188c684"
 
   bottle do
     cellar :any_skip_relocation
