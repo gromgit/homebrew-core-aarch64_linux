ee1c71656bdfdc93ba38b5d803e8f6edf7400465
Chris Long <christopherglong@mac.com>
2019-06-27 01:49:25 -0700

    netlify-cli 2.11.23 (new formula)
    Closes #41453.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/netlify-cli.rb
@@ -0,0 +1,24 @@
+require "language/node"
+
+class NetlifyCli < Formula
+  desc "Netlify command-line tool"
+  homepage "https://www.netlify.com/docs/cli"
+  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-2.11.23.tgz"
+  sha256 "d96ac4eed83655080853e06ce2017efac73f9fa687689c45b880ef61ac198088"
+  head "https://github.com/netlify/cli.git"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"test.exp").write <<~EOS
+      spawn #{bin}/netlify login
+      expect "Opening"
+    EOS
+    assert_match "Logging in", shell_output("expect -f test.exp")
+  end
+end
