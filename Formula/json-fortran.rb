b672fff2950ef81ed7046d66a7436b27a851345a
Igor Kapkov <igasgeek@me.com>
2019-06-27 13:18:34 +1000

    json-fortran 7.1.0
    Closes #41383.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/json-fortran.rb
+++ Formula/json-fortran.rb
@@ -1,9 +1,8 @@
 class JsonFortran < Formula
   desc "Fortran 2008 JSON API"
   homepage "https://github.com/jacobwilliams/json-fortran"
-  url "https://github.com/jacobwilliams/json-fortran/archive/7.0.0.tar.gz"
-  sha256 "9b5b6235489b27d572bbc7620ed8e039fa9d4d14d41b1581b279be9db499f32c"
-  revision 1
+  url "https://github.com/jacobwilliams/json-fortran/archive/7.1.0.tar.gz"
+  sha256 "e7aa1f6e09b25ebacb17188147380c3f8c0a254754cd24869c001745fcecc9e6"
   head "https://github.com/jacobwilliams/json-fortran.git"
 
   bottle do
