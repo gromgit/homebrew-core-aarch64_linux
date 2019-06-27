ba5d1dca76b948803fb9cf327f05a4aac5773e7d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-27 23:39:33 +0000

    gpac: update 0.8.0 bottle.

--- Formula/gpac.rb
+++ Formula/gpac.rb
@@ -14,10 +14,10 @@ class Gpac < Formula
   head "https://github.com/gpac/gpac.git"
 
   bottle do
-    sha256 "e035d74171aa4179277bf62734f21fb06688f0a7b10bb6ac57bcdc7124b0453f" => :mojave
-    sha256 "1e369830820f2e7553bcc498af2a839005c3f8a6327781cd4f6f46252ae42ca6" => :high_sierra
-    sha256 "c9d6d00656293f4d40185460807b7c489d8d2e7db4bc4e95af9ede968260f4e1" => :sierra
-    sha256 "46428c9cb8fdf15ebe9014667f9158444b263e60b5836688625b300f389c64e4" => :el_capitan
+    cellar :any
+    sha256 "f2c2011bf39b446799dc060821df0947997583f2ec4793488aa98964fe6d6cb5" => :mojave
+    sha256 "817c6ab9614c89b887bd2887faa03e1aaa505cf25861c9ca4c53e598b56b5396" => :high_sierra
+    sha256 "d1860839d41ed5922e40c1f2b1ff6215083cfaba19d9751db985541b2cf475b8" => :sierra
   end
 
   depends_on "pkg-config" => :build
