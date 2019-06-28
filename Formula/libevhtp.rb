d613e3d948989d42b568151784de27a4ef1d5c64
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-28 11:29:42 +0000

    libevhtp: add 1.2.18 bottle.

--- Formula/libevhtp.rb
+++ Formula/libevhtp.rb
@@ -4,6 +4,13 @@ class Libevhtp < Formula
   url "https://github.com/criticalstack/libevhtp/archive/1.2.18.tar.gz"
   sha256 "316ede0d672be3ae6fe489d4ac1c8c53a1db7d4fe05edaff3c7c853933e02795"
 
+  bottle do
+    cellar :any
+    sha256 "916fe12982a34b35481418e162137670873951bca1f39cf6235560777ca99f67" => :mojave
+    sha256 "f16d78564abe1a2b163832ef1449b4e2acab610ef118979c0c5933dd5a3b3f62" => :high_sierra
+    sha256 "0aef17632623cb4c15f9bed34272a95b478b25b770290f3d70c523e8efadb545" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
   depends_on "libevent"
