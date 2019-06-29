9c4c83560910c18e18e9fa41c2a38f9b63d411ef
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-29 21:37:34 +0000

    gammu: update 1.40.0_2 bottle.

--- Formula/gammu.rb
+++ Formula/gammu.rb
@@ -7,9 +7,9 @@ class Gammu < Formula
   head "https://github.com/gammu/gammu.git"
 
   bottle do
-    sha256 "024da26d637286903f0456997dc4d0231f8163163106d23fae95085e33cb7862" => :mojave
-    sha256 "1cb822252b422c771c13a5283cd6a1ea3283cb6d897adc730e7631ad1a788cfa" => :high_sierra
-    sha256 "2da2dba2c832236e0c2ef1bb43fdd4fa96dd3862868b0ef7cf009c62986f8390" => :sierra
+    sha256 "2101eb2a42043451ccb30f7bb2626f5d4c653ad1d025e8778155c4e26099f1b5" => :mojave
+    sha256 "b7592492869a4e30486465923ac75de80ececb472caaf11315a320b68aa30a74" => :high_sierra
+    sha256 "7f3e7d721eadda817e77e463bccf34fdbf2574796eefc4da53e2d2cb5ef49839" => :sierra
   end
 
   depends_on "cmake" => :build
