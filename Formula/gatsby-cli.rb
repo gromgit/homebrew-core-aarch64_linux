0c2d663385fa5fb78baf0c9c8ede8b1a4b679111
Chris Long <christopherglong@mac.com>
2019-06-28 22:45:40 -0700

    gatsby-cli 2.7.6 (new formula)
    Closes #41455.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/gatsby-cli.rb
@@ -0,0 +1,20 @@
+require "language/node"
+
+class GatsbyCli < Formula
+  desc "Gatsby command-line interface"
+  homepage "https://www.gatsbyjs.org/docs/gatsby-cli/"
+  url "https://registry.npmjs.org/gatsby-cli/-/gatsby-cli-2.7.6.tgz"
+  sha256 "8e8ca5d1cdc99ac1242cc1a0449c877ff28468c3d39b399347e5c1a8647e98b7"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system bin/"gatsby", "new", "hello-world", "https://github.com/gatsbyjs/gatsby-starter-hello-world"
+    assert_predicate testpath/"hello-world/package.json", :exist?, "package.json was not cloned"
+  end
+end
