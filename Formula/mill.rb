42be47fac76e3966c25f8a5a362a09df74855863
Igor Kapkov <igasgeek@me.com>
2019-07-01 09:49:08 +1000

    mill 0.4.2
    Closes #41502.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/mill.rb
+++ Formula/mill.rb
@@ -1,8 +1,8 @@
 class Mill < Formula
   desc "Scala build tool"
   homepage "https://www.lihaoyi.com/mill/"
-  url "https://github.com/lihaoyi/mill/releases/download/0.4.1/0.4.1"
-  sha256 "9da400d31d3aebff1a68a7c1ab89a599955747b6635d42e75db0b4351930359e"
+  url "https://github.com/lihaoyi/mill/releases/download/0.4.2/0.4.2"
+  sha256 "39c080df57338ac0e554ca9942b5e886f71300f53127a56a2a147e6c2620c656"
 
   bottle :unneeded
 
