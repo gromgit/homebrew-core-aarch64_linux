6faa8c03823a590c6039ae916fff9f261c3fa83b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-01 07:31:11 +0000

    topgrade: update 2.4.0 bottle.

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -6,9 +6,9 @@ class Topgrade < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "4393b280cf995f9b3af785744fd61e2f1b611edba626ea480c6ec50eb6103ece" => :mojave
-    sha256 "d7970d56a7b95610c54d341ceb3b6fdffeb7f4b55b6d5010e44aed104ae54917" => :high_sierra
-    sha256 "01ccbf1997d380113700610146680ef775cf02dfb4332fa2dd05b61ceb07f9c6" => :sierra
+    sha256 "fd53280d27c8d952bcd0131bed94d9b877a3c9ba779a41476793b3a95ac1471c" => :mojave
+    sha256 "106995c6f9deed2418c0e8035c09429e156fb2f03e303f7f6726de601a1df41b" => :high_sierra
+    sha256 "4d5a581846697c85c9897beae5f8cce48973527a78d92c80481940eb8304cd56" => :sierra
   end
 
   depends_on "rust" => :build
