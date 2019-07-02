44c1bf90a826df398cd0a2146792bdf7b08a06a9
Igor Kapkov <igasgeek@me.com>
2019-07-02 20:49:27 +1000

    argon2 20190702
    Closes #41550.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/argon2.rb
+++ Formula/argon2.rb
@@ -1,8 +1,8 @@
 class Argon2 < Formula
   desc "Password hashing library and CLI utility"
   homepage "https://github.com/P-H-C/phc-winner-argon2"
-  url "https://github.com/P-H-C/phc-winner-argon2/archive/20171227.tar.gz"
-  sha256 "eaea0172c1f4ee4550d1b6c9ce01aab8d1ab66b4207776aa67991eb5872fdcd8"
+  url "https://github.com/P-H-C/phc-winner-argon2/archive/20190702.tar.gz"
+  sha256 "daf972a89577f8772602bf2eb38b6a3dd3d922bf5724d45e7f9589b5e830442c"
   head "https://github.com/P-H-C/phc-winner-argon2.git"
 
   bottle do
