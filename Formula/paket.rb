1f01ef58e1682040dce822c9eedbeaffcc39e37d
Igor Kapkov <igasgeek@me.com>
2019-07-02 10:42:43 +1000

    paket 5.212.1
    Closes #41526.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/paket.rb
+++ Formula/paket.rb
@@ -1,8 +1,8 @@
 class Paket < Formula
   desc "Dependency manager for .NET with support for NuGet and Git repositories"
   homepage "https://fsprojects.github.io/Paket/"
-  url "https://github.com/fsprojects/Paket/releases/download/5.212.0/paket.exe"
-  sha256 "41c1493ce9af59d30817a1aaace95f41227569b53113eb3fdbb953695207d88b"
+  url "https://github.com/fsprojects/Paket/releases/download/5.212.1/paket.exe"
+  sha256 "2d30c345cb8742e604bf186c4eb40b92dbee0bdee4f38b7868db1201a4be8d59"
 
   bottle :unneeded
 
