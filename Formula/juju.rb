3586ec03adc76a1d5d50ab062332210a73cf6605
Christian Muirhead <christian.muirhead@canonical.com>
2019-07-02 15:22:23 +1200

    juju 2.6.5
    Closes #41540.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -1,8 +1,8 @@
 class Juju < Formula
   desc "DevOps management tool"
   homepage "https://jujucharms.com/"
-  url "https://launchpad.net/juju/2.6/2.6.4/+download/juju-core_2.6.4.tar.gz"
-  sha256 "feeaa406d8f4eefd5c22972fb95a8bbdd75d1ab34bf9766ad1af89038ec5d44a"
+  url "https://launchpad.net/juju/2.6/2.6.5/+download/juju-core_2.6.5.tar.gz"
+  sha256 "2222b5cfec023d7f6dca1f3f4dd4aa78e8d626d8debbb1f721b723f5ed584482"
 
   bottle do
     cellar :any_skip_relocation
