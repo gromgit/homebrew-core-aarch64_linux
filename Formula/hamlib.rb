7963341d142d19e596b125576bb0b5dc017bdb4e
sirhc808 <sirhc808@gmail.com>
2019-07-04 13:44:57 -0500

    hamlib 3.3
    Closes #41630.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/hamlib.rb
+++ Formula/hamlib.rb
@@ -1,8 +1,8 @@
 class Hamlib < Formula
   desc "Ham radio control libraries"
   homepage "https://hamlib.sourceforge.io/"
-  url "https://src.fedoraproject.org/repo/pkgs/hamlib/hamlib-1.2.15.3.tar.gz/3cad8987e995a00e5e9d360e2be0eb43/hamlib-1.2.15.3.tar.gz"
-  sha256 "a2ca4549e4fd99d6e5600e354ebcb57502611aa63c6921c1b8a825289833f75e"
+  url "https://src.fedoraproject.org/repo/pkgs/hamlib/hamlib-3.3.tar.gz/sha512/4cf6c94d0238c8a13aed09413b3f4a027c8ded07f8840cdb2b9d38b39b6395a4a88a8105257015345f6de0658ab8c60292d11a9de3e16a493e153637af630a80/hamlib-3.3.tar.gz"
+  sha256 "c90b53949c767f049733b442cd6e0a48648b55d99d4df5ef3f852d985f45e880"
 
   bottle do
     rebuild 1
