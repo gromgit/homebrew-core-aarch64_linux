693acaa6826ab46dc6ac61225ddc49047a22ef81
Caleb Xu <calebcenter@live.com>
2019-07-04 18:51:30 -0400

    libsixel 1.8.2 (new formula)
    Closes #41633.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/libsixel.rb
@@ -0,0 +1,22 @@
+class Libsixel < Formula
+  desc "SIXEL encoder/decoder implementation"
+  homepage "https://github.com/saitoha/sixel"
+  url "https://github.com/saitoha/libsixel/releases/download/v1.8.2/libsixel-1.8.2.tar.gz"
+  sha256 "c464d2a6fcf35e9e6bad1876729e853a8b9f6abfe97d9e3487c9bfac45cf2a5f"
+
+  depends_on "jpeg"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--with-jpeg=#{Formula["jpeg"].prefix}",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    fixture = test_fixtures("test.png")
+    system "#{bin}/img2sixel", fixture
+  end
+end
