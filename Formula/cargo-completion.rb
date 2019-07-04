d50e8f2d77e6c371bca1433c3b5881c7b3ab05ec
Rui Chen <rchen@meetup.com>
2019-07-04 21:56:24 +0800

    cargo-completion 0.37.0
    Closes #41628.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/cargo-completion.rb
+++ Formula/cargo-completion.rb
@@ -1,19 +1,12 @@
 class CargoCompletion < Formula
   desc "Bash and Zsh completion for Cargo"
   homepage "https://github.com/rust-lang/cargo"
-  url "https://github.com/rust-lang/cargo/archive/0.36.0.tar.gz"
-  sha256 "532a39ce9acc1436c5d33ce0643d050bc29183c46abe69934622c4f24f4c6831"
+  url "https://github.com/rust-lang/cargo/archive/0.37.0.tar.gz"
+  sha256 "377e1090e9ce21206270576193746499a26e8ffbd8b89ccd5f8eb1085ca00e3b"
   version_scheme 1
   head "https://github.com/rust-lang/cargo.git"
 
   bottle :unneeded
-
-  # Upstream patch that fixes the compatibility with the macOS stock bash.
-  # See rust-lang/cargo#6905.
-  patch do
-    url "https://github.com/rust-lang/cargo/commit/e2c519dd7ac61e4d2f94cad60ef920ce4aa1718f.patch?full_index=1"
-    sha256 "3dd6a7914ac133b51c4004ac1d43ffb9fc6b87d2635e761ff488170bf06584ec"
-  end
 
   def install
     bash_completion.install "src/etc/cargo.bashcomp.sh" => "cargo"
