7fcf75a90dd98466510e4780b0b9a11e2d8981ab
chenrui <rchen@meetup.com>
2019-07-06 18:36:13 +0800

    grakn 1.5.7 (#41659)

--- Formula/grakn.rb
+++ Formula/grakn.rb
@@ -1,8 +1,8 @@
 class Grakn < Formula
   desc "The distributed hyper-relational database for knowledge engineering"
   homepage "https://grakn.ai"
-  url "https://github.com/graknlabs/grakn/releases/download/1.5.6/grakn-core-all-mac-1.5.6.zip"
-  sha256 "bba92e7cd49abcf373e66301c2712bef70677560017d707e695c31d36631dd80"
+  url "https://github.com/graknlabs/grakn/releases/download/1.5.7/grakn-core-all-mac-1.5.7.zip"
+  sha256 "0c406be4442f272eb8a31fce189a4f970b499cadf4eac8bf91d0287a0e08eafa"
 
   bottle :unneeded
 
