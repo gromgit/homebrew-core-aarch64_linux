bfec778207453bceba597d76b84fa89711a29d3d
Igor Kapkov <igasgeek@me.com>
2019-07-08 08:20:53 +1000

    vips 8.8.1
    Closes #41696.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/vips.rb
+++ Formula/vips.rb
@@ -1,9 +1,8 @@
 class Vips < Formula
   desc "Image processing library"
   homepage "https://github.com/libvips/libvips"
-  url "https://github.com/libvips/libvips/releases/download/v8.8.0/vips-8.8.0.tar.gz"
-  sha256 "8e78b451adfe59288bded74c9ec6b8c5eb0574ecbba7a0352de4f34266e021b0"
-  revision 1
+  url "https://github.com/libvips/libvips/releases/download/v8.8.1/vips-8.8.1.tar.gz"
+  sha256 "a0ee255a2a1ebfea5b2dff2a780824d7157a78c010d7ddd531279aacefbf2539"
 
   bottle do
     sha256 "75e6bd18a991e1623f45c5a878f9a570e47a099af1c5f1cd14c0d9ac94cfccdd" => :mojave
