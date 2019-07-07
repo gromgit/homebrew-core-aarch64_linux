f85946837edb8458185a8eadb54a0b4781b3c37d
Loki <Loki.Astari@gmail.com>
2019-07-07 00:44:16 -0700

    thors-serializer 1.13.4
    Closes #41680.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/thors-serializer.rb
+++ Formula/thors-serializer.rb
@@ -2,8 +2,8 @@ class ThorsSerializer < Formula
   desc "Declarative serialization library (JSON/YAML) for C++"
   homepage "https://github.com/Loki-Astari/ThorsSerializer"
   url "https://github.com/Loki-Astari/ThorsSerializer.git",
-      :tag      => "1.13.2",
-      :revision => "d2496003c58701ea01f4ebfbb4acff084cc170a6"
+      :tag      => "1.13.4",
+      :revision => "b3aa5587c37198ba10cca4190f385c33db31b8dc"
 
   bottle do
     cellar :any
