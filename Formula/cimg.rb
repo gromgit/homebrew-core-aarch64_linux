143459cb931bffe4b2e06393f1e2f0a3c523bb46
Caleb Xu <calebcenter@live.com>
2019-07-07 16:39:29 -0400

    cimg 2.6.7 (#41693)

--- Formula/cimg.rb
+++ Formula/cimg.rb
@@ -1,8 +1,8 @@
 class Cimg < Formula
   desc "C++ toolkit for image processing"
   homepage "http://cimg.eu/"
-  url "http://cimg.eu/files/CImg_2.5.4.zip"
-  sha256 "1036dc2d1c13bc578c4391e58fec53e87dc8a5591a370a8c99bcc6e99f8abe22"
+  url "http://cimg.eu/files/CImg_2.6.7.zip"
+  sha256 "d5c021982a88cf3b036c771cda653b3e49246ea85e9943d154f95e490df65aca"
 
   bottle :unneeded
 
