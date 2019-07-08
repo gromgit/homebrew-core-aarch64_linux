8c10ca319f1eb0d07083a16b2890c78e356c2e02
Leon Klingele <git@leonklingele.de>
2019-07-09 00:13:24 +0200

    go 1.12.7
    Closes #41724.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/go.rb
+++ Formula/go.rb
@@ -3,9 +3,9 @@ class Go < Formula
   homepage "https://golang.org"
 
   stable do
-    url "https://dl.google.com/go/go1.12.6.src.tar.gz"
-    mirror "https://fossies.org/linux/misc/go1.12.6.src.tar.gz"
-    sha256 "c96c5ccc7455638ae1a8b7498a030fe653731c8391c5f8e79590bce72f92b4ca"
+    url "https://dl.google.com/go/go1.12.7.src.tar.gz"
+    mirror "https://fossies.org/linux/misc/go1.12.7.src.tar.gz"
+    sha256 "95e8447d6f04b8d6a62de1726defbb20ab203208ee167ed15f83d7978ce43b13"
 
     go_version = version.to_s.split(".")[0..1].join(".")
     resource "gotools" do
@@ -36,24 +36,6 @@ class Go < Formula
     url "https://storage.googleapis.com/golang/go1.7.darwin-amd64.tar.gz"
     version "1.7"
     sha256 "51d905e0b43b3d0ed41aaf23e19001ab4bc3f96c3ca134b48f7892485fc52961"
-  end
-
-  # Prevents Go from building malformed binaries. Fixed upstream, should
-  # be in a future release.
-  # https://github.com/golang/go/issues/32673
-  patch do
-    url "https://github.com/golang/go/commit/26954bde4443c4bfbfe7608f35584b6b810f3f2c.patch?full_index=1"
-    sha256 "25a361bd4aa1155be06e2239c1974aa9c59f971210f19e16a3b7b576b9d4f677"
-  end
-
-  patch do
-    url "https://github.com/golang/go/commit/0fe1986a72ea578390d4909988a1d7cb3a687544.patch?full_index=1"
-    sha256 "320c11208313fc74e0bba7f323791416e5316451b109c440f56be361df8306ea"
-  end
-
-  patch do
-    url "https://github.com/golang/go/commit/3f1422c799edb143303c86c0e875d44c3612df64.patch?full_index=1"
-    sha256 "d071f0415cd2712cbed373682c4a84661147df1aabf38bbc0f3179532a988a4f"
   end
 
   def install
