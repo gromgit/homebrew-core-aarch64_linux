1ab29c12572c5a94fe41d20d4c652fd0fce4bf71
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-09 01:59:13 +0000

    ask-cli: update 1.7.6 bottle.

--- Formula/ask-cli.rb
+++ Formula/ask-cli.rb
@@ -8,9 +8,9 @@ class AskCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "1417b530b07f477d8c68dd7a215819d9f65eb7dfa35f89fa5a3160619798edac" => :mojave
-    sha256 "972bf5c3e74662e53b1e14d0e7a4a060b59070f5ff09889e24d729454b6c64aa" => :high_sierra
-    sha256 "05e7bc9f9f21bf96716e1911f3cd51ee5907b27473f5a81a8740bcf722e7f670" => :sierra
+    sha256 "4f75cdb3997ff3930241c2957f9b9248a9bdc9a114043baa51cd65f26755cdb1" => :mojave
+    sha256 "49b1fed5eac9e00a101c90ddbf93679a40d87b0228b27a8ce71976565ca041b7" => :high_sierra
+    sha256 "6ebbf27120e306b1e2c7dcf731ef0639ac330778afee76291c8ff4b88c269c17" => :sierra
   end
 
   depends_on "node"
