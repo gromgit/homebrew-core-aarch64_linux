493ba3842d8123faddcf6efd72764439b42422f4
Caleb Xu <calebcenter@live.com>
2019-07-08 22:17:16 -0400

    eccodes 2.13.0
    Closes #41738.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/eccodes.rb
+++ Formula/eccodes.rb
@@ -1,9 +1,8 @@
 class Eccodes < Formula
   desc "Decode and encode messages in the GRIB 1/2 and BUFR 3/4 formats"
   homepage "https://confluence.ecmwf.int/display/ECC"
-  url "https://software.ecmwf.int/wiki/download/attachments/45757960/eccodes-2.12.0-Source.tar.gz"
-  sha256 "f75ae5ce9e543622e8e40c3037619f8d9e6542c902933adb371bac82aee91367"
-  revision 1
+  url "https://software.ecmwf.int/wiki/download/attachments/45757960/eccodes-2.13.0-Source.tar.gz"
+  sha256 "c5ce1183b5257929fc1f1c8496239e52650707cfab24f4e0e1f1a471135b8272"
 
   bottle do
     sha256 "5e06fab2d6e9bf162d09de8e3c487d33843575316e865ea0bf20eddb8a5ecca6" => :mojave
