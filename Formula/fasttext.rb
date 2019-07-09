82da79723ccb9a2e038d2d67eb3cbdd6612c14dc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-09 02:34:19 +0000

    fasttext: update 0.9.1 bottle.

--- Formula/fasttext.rb
+++ Formula/fasttext.rb
@@ -7,9 +7,9 @@ class Fasttext < Formula
 
   bottle do
     cellar :any
-    sha256 "ebe964b6cd19c3a6c742e77ca7c3ceee7bf7d5bd509d8fc6e57d2ed90a345771" => :mojave
-    sha256 "ff2e7e838e290b4f18140cb60e0420561938229bf393ac6b1e6572560e31cb7e" => :high_sierra
-    sha256 "a2c9b33eec2908d205e13c13dd68dea64ada417d41984080e9bd74e949f53985" => :sierra
+    sha256 "a4b6d2be804597582fcc3f0be630bdd8c791e2c1d530880e98f54251decac960" => :mojave
+    sha256 "638910cb1f3a73c6cb9ca9ecab9b790e8f2254965ee86eeddc5dd79ee4bd3001" => :high_sierra
+    sha256 "c1775db7dd27a8fac380bcd4944eda4ec1e162b92a9329ff48debf76c3834426" => :sierra
   end
 
   depends_on "cmake" => :build
