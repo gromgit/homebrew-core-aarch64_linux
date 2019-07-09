ede0a3edda318e10947383c6a813096319f9a0d7
Caleb Xu <calebcenter@live.com>
2019-07-09 17:30:58 -0400

    gpsim 0.31.0
    Closes #41793.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/gpsim.rb
+++ Formula/gpsim.rb
@@ -1,9 +1,8 @@
 class Gpsim < Formula
   desc "Simulator for Microchip's PIC microcontrollers"
   homepage "https://gpsim.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/gpsim/gpsim/0.30.0/gpsim-0.30.0.tar.gz"
-  sha256 "e1927312c37119bc26d6abf2c250072a279a9c764c49ae9d71b4ccebb8154f86"
-  revision 1
+  url "https://downloads.sourceforge.net/project/gpsim/gpsim/0.31.0/gpsim-0.31.0.tar.gz"
+  sha256 "110ee6be3a5d02b32803a91e480cbfc9d423ef72e0830703fc0bc97b9569923f"
   head "https://svn.code.sf.net/p/gpsim/code/trunk"
 
   bottle do
@@ -15,10 +14,17 @@ class Gpsim < Formula
 
   depends_on "gputils" => :build
   depends_on "pkg-config" => :build
+  depends_on "gettext"
   depends_on "glib"
   depends_on "popt"
+  depends_on "readline"
 
   def install
+    ENV.cxx11
+
+    # Upstream bug filed: https://sourceforge.net/p/gpsim/bugs/245/
+    inreplace "src/modules.cc", "#include \"error.h\"", ""
+
     system "./configure", "--disable-dependency-tracking",
                           "--disable-gui",
                           "--disable-shared",
