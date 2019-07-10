06345aaad94cbacfad730211bde345c8f686d812
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-10 03:02:50 +0000

    amazon-ecs-cli: update 1.15.0 bottle.

--- Formula/amazon-ecs-cli.rb
+++ Formula/amazon-ecs-cli.rb
@@ -7,9 +7,9 @@ class AmazonEcsCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "2200fa7e99a3ae207c356279ab06086af138a95990b7e544995b1c8ef78f4f89" => :mojave
-    sha256 "f49ba7cb9b353cf8dffc6447571b3213dc3caa2fd0288269d7ab39b56d1cd856" => :high_sierra
-    sha256 "573bcbf83df485348e132e1c6243b7f5b596ee5a3763eefd85d7fcc7335e25a2" => :sierra
+    sha256 "99913aa0bb43466d7a73729b852efdb27bf132bf7738248fc9b8df74192047f1" => :mojave
+    sha256 "4f72234956bd19a087e14be6924a511d25dcc7152c1073bde3d0b8455f7b6290" => :high_sierra
+    sha256 "fa15884ce10a9626ab482d8906d87b75139abbeb087be98b1cdaeec2aedf97f5" => :sierra
   end
 
   depends_on "go" => :build
