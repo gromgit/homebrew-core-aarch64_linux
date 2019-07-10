4ab19017f65a99ee39e8a283c2d129d6b9289611
Tomoyuki Sahara <sahara@caddr.net>
2019-07-10 21:16:09 +0900

    gauche 0.9.8
    Closes #41820.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/gauche.rb
+++ Formula/gauche.rb
@@ -1,13 +1,20 @@
 class Gauche < Formula
   desc "R7RS Scheme implementation, developed to be a handy script interpreter"
   homepage "https://practical-scheme.net/gauche/"
-  url "https://downloads.sourceforge.net/gauche/Gauche/Gauche-0.9.7.tgz"
-  sha256 "2d33bd942e3fc2f2dcc8e5217c9130c885a0fd1cb11a1856e619a83a23f336a0"
+  url "https://downloads.sourceforge.net/gauche/Gauche/Gauche-0.9.8.tgz"
+  sha256 "3eb30d1051d8b48999fe46511c9f6983057735312c9832b7db13f9db140db74b"
 
   bottle do
     sha256 "c04f0c743c998cd8c63483c9f4d47311e9df465144390dc8d6db266499f9aaf5" => :mojave
     sha256 "7aa642136a7d5be56e21b2ffa424d0f0dbf09f1f4789733f2fd73bb551212ace" => :high_sierra
     sha256 "9aa6541d4840bfcb1c5b62a4f66c2c9a071cbeb03d7913798dc6e86261ea29f6" => :sierra
+  end
+
+  # Fix build on macOS (and other libressl-based systems).
+  # https://github.com/shirok/Gauche/pull/483
+  patch do
+    url "https://github.com/shirok/Gauche/commit/891f40ae195565de803c2aaf2db27db0e11300a0.diff?full_index=1"
+    sha256 "0bbbf0bb24ba4a88a8c4895632a59f75a89c8306d58a8877c3de536cfa926f8e"
   end
 
   def install
