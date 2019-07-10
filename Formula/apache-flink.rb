704666803e1c315c4ba7244443755163a54e7aac
Sung Gon Yi <skonmeme@gmail.com>
2019-07-10 17:34:46 +0900

    apache-flink 1.8.1 (#41813)

--- Formula/apache-flink.rb
+++ Formula/apache-flink.rb
@@ -1,9 +1,9 @@
 class ApacheFlink < Formula
   desc "Scalable batch and stream data processing"
   homepage "https://flink.apache.org/"
-  url "https://archive.apache.org/dist/flink/flink-1.8.0/flink-1.8.0-bin-scala_2.11.tgz"
-  version "1.8.0"
-  sha256 "88935984bb57a84b26065035a46c2d69e3ef6805c2780ac8cd3f1f3759bf6925"
+  url "https://archive.apache.org/dist/flink/flink-1.8.1/flink-1.8.1-bin-scala_2.11.tgz"
+  version "1.8.1"
+  sha256 "4fc0d0f163174ec43e160fdf21a91674979b978793e60361e2fce5dddba4ddfa"
   head "https://github.com/apache/flink.git"
 
   bottle :unneeded
