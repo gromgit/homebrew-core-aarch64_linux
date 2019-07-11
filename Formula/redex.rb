d016f5100c3735081583a9e9a6556d8c1c036c2a
Igor Kapkov <igasgeek@me.com>
2019-07-12 09:03:05 +1000

    redex: revision bump for jsoncpp

--- Formula/redex.rb
+++ Formula/redex.rb
@@ -3,7 +3,7 @@ class Redex < Formula
   homepage "https://fbredex.com"
   url "https://github.com/facebook/redex/archive/v2017.10.31.tar.gz"
   sha256 "18a840e4db0fc51f79e17dfd749b2ffcce65a28e7ef9c2b3c255c5ad89f6fd6f"
-  revision 2
+  revision 3
   head "https://github.com/facebook/redex.git"
 
   bottle do
