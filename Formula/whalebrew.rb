0b05cb62870b53aeff8ca7ee8aa4497fef5a40ee
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-11 08:25:27 +0000

    whalebrew: add 0.2.2 bottle.

--- Formula/whalebrew.rb
+++ Formula/whalebrew.rb
@@ -6,6 +6,13 @@ class Whalebrew < Formula
     :revision => "eef2cdc8483cc740752cf1a698dd9e349f1b1a49"
   head "https://github.com/whalebrew/whalebrew.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4788986bffaaad6defb524a5e2f8d7093f77fbd0004fa0f4cf52ebcb4134e240" => :mojave
+    sha256 "0c0a716fc4755fe07bf3594ec29fa937b1b553f9cdd76ec929551a6be55f2e19" => :high_sierra
+    sha256 "5cf2de32568a395555ea80737756f616866e6109f19f561d8b19562918293103" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
