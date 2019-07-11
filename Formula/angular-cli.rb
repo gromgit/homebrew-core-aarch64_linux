78f4f0c5a2727198a5c02ce8cd52343502caec1a
Igor Kapkov <igasgeek@me.com>
2019-07-11 11:44:06 +1000

    angular-cli 8.1.1
    Closes #41839.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/angular-cli.rb
+++ Formula/angular-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class AngularCli < Formula
   desc "CLI tool for Angular"
   homepage "https://cli.angular.io/"
-  url "https://registry.npmjs.org/@angular/cli/-/cli-8.1.0.tgz"
-  sha256 "cc9bbc9c9a56be53bb062862a340f3d7c99d425f5e085dbb928bb961d0673116"
+  url "https://registry.npmjs.org/@angular/cli/-/cli-8.1.1.tgz"
+  sha256 "02cbf3d0d15fb5ca6d5d36d8f38d9b8c8d17b1bf142445bd5c8c0436e5d04498"
 
   bottle do
     cellar :any_skip_relocation
