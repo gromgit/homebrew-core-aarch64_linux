09a5b7a6afe9bf14243ea880ceeda5e9f0606f48
Caleb Xu <calebcenter@live.com>
2019-07-12 08:34:58 -0400

    i386-elf-gdb 8.3

--- Formula/i386-elf-gdb.rb
+++ Formula/i386-elf-gdb.rb
@@ -1,8 +1,8 @@
 class I386ElfGdb < Formula
   desc "GNU debugger for i386-elf cross development"
   homepage "https://www.gnu.org/software/gdb/"
-  url "https://ftp.gnu.org/gnu/gdb/gdb-8.2.tar.xz"
-  sha256 "c3a441a29c7c89720b734e5a9c6289c0a06be7e0c76ef538f7bbcef389347c39"
+  url "https://ftp.gnu.org/gnu/gdb/gdb-8.3.tar.xz"
+  sha256 "802f7ee309dcc547d65a68d61ebd6526762d26c3051f52caebe2189ac1ffd72e"
 
   bottle do
     sha256 "744add2a9c9a8fb1fcc81b27d67c13f7cc41b0a739abba325ff26c0d88994142" => :mojave
@@ -16,7 +16,9 @@ class I386ElfGdb < Formula
                              "--prefix=#{prefix}",
                              "--disable-werror"
       system "make"
-      system "make", "install"
+
+      # Don't install bfd or opcodes, as they are provided by binutils
+      system "make", "install-gdb"
     end
   end
 
