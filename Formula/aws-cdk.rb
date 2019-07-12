5a87147898285d3b83589fd6926bba9595505042
Nick Hollingshead <nickh@alamy.com>
2019-07-12 15:09:44 +0100

    aws-cdk 1.7.0 (new formula)
    Closes #41901.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/aws-cdk.rb
@@ -0,0 +1,24 @@
+require "language/node"
+
+class AwsCdk < Formula
+  desc "AWS Cloud Development Kit - framework for defining AWS infra as code"
+  homepage "https://github.com/aws/aws-cdk"
+  url "https://registry.npmjs.org/aws-cdk/-/aws-cdk-1.7.0.tgz"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    mkdir "testapp"
+    cd testpath/"testapp"
+    shell_output("#{bin}/cdk init app --language=javascript")
+    list = shell_output("#{bin}/cdk list")
+    cdkversion = shell_output("#{bin}/cdk --version")
+    assert_match "TestappStack", list
+    assert_match version.to_s, cdkversion
+  end
+end
