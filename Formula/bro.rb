dabc87d841e1203c02e7ab7aad51e83cf512bacc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-12 00:14:22 +0000

    bro: update 2.6.2 bottle.

--- Formula/bro.rb
+++ Formula/bro.rb
@@ -6,9 +6,9 @@ class Bro < Formula
   head "https://github.com/bro/bro.git"
 
   bottle do
-    sha256 "4eaec5d1fc2855bde91148877cfe88a96301cfbae32b7b725d6eb0f01b85cde5" => :mojave
-    sha256 "55e8c800317bef47a7e703872e91f0a34e87607b7c9f9f31857770b9530f44b3" => :high_sierra
-    sha256 "82cf612f0d7b63fc2a348ccb4f69005e1608db298a592abe7245ec0999bd7a6c" => :sierra
+    sha256 "69c4fa4c4b9842e9a5f2aa07c1e02838b88ae73ee9cc02090ff532baef02d682" => :mojave
+    sha256 "eff43b1c0b15404696ff6d2323cfb77be9c5d0f7e7a9e65fc11c56056a209e83" => :high_sierra
+    sha256 "5ae17948baf2acc7aed24432ef399a3297fa9500e06854b4669434c669c92fb7" => :sierra
   end
 
   depends_on "bison" => :build
