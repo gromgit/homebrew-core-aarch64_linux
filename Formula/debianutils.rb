9fae7cf6f188f0f48ce2b38eec57d9ade016d72d
Caleb Xu <calebcenter@live.com>
2019-07-11 21:32:30 -0400

    debianutils 4.8.6.2
    Closes #41882.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/debianutils.rb
+++ Formula/debianutils.rb
@@ -1,8 +1,8 @@
 class Debianutils < Formula
   desc "Miscellaneous utilities specific to Debian"
   homepage "https://packages.debian.org/sid/debianutils"
-  url "https://deb.debian.org/debian/pool/main/d/debianutils/debianutils_4.8.6.1.tar.xz"
-  sha256 "099f1e8a7278b26145a2ba2dda84c4118403bfab38c8d7070a6235a7ffcb55ed"
+  url "https://deb.debian.org/debian/pool/main/d/debianutils/debianutils_4.8.6.2.tar.xz"
+  sha256 "aca388ba52d19f7c2f7991183a1c15c4d375c692a6cb4bae62cd17b485b10bba"
 
   bottle do
     cellar :any_skip_relocation
