14f1b2a71e12d5a3e45003167b7aa4b6d07cba50
EricFromCanada <enk3@outlook.com>
2019-07-13 15:01:59 -0400

    xrootd 4.10.0
    Closes #41931.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/xrootd.rb
+++ Formula/xrootd.rb
@@ -1,8 +1,8 @@
 class Xrootd < Formula
   desc "High performance, scalable, fault-tolerant access to data"
   homepage "http://xrootd.org"
-  url "http://xrootd.org/download/v4.8.3/xrootd-4.8.3.tar.gz"
-  sha256 "9cd30a343758b8f50aea4916fa7bd37de3c37c5b670fe059ae77a8b2bbabf299"
+  url "http://xrootd.org/download/v4.10.0/xrootd-4.10.0.tar.gz"
+  sha256 "f07f85e27d72e9e8ff124173c7b53619aed8fcd36f9d6234c33f8f7fd511995b"
   head "https://github.com/xrootd/xrootd.git"
 
   bottle do
@@ -15,6 +15,7 @@ class Xrootd < Formula
 
   depends_on "cmake" => :build
   depends_on "openssl"
+  depends_on "readline"
 
   def install
     mkdir "build" do
