e370f15e943787b360f3def0eebb69b408742330
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-14 03:54:42 +0000

    tflint: add 0.9.1 bottle.

--- Formula/tflint.rb
+++ Formula/tflint.rb
@@ -5,6 +5,13 @@ class Tflint < Formula
   sha256 "a4f9f845584c51b3d0db3d2ee9ab48435db52ade3ddde1a8689e2ad8cf9acdca"
   head "https://github.com/wata727/tflint.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "99110c6d7310a93a4d3c9f45b9bc267a748be06503fb6123c0f563d42b91e603" => :mojave
+    sha256 "12ad24bd01528b1f1063abbaad0eaa5c2ba8d11612fb28179311c5afe3cc1285" => :high_sierra
+    sha256 "723e9d1357d02c7cedf50657b06c82ff34d9f09dc1f3674664734e0a2915528c" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
