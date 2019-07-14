778382e396eb8b4293b12f405c8fc84826327b80
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-14 20:09:02 +0000

    ocrad: update 0.27 bottle.

--- Formula/ocrad.rb
+++ Formula/ocrad.rb
@@ -7,11 +7,9 @@ class Ocrad < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ec28597359fb7399667a71919805981b06fed05571ecebcdd982a1b0de61b0cf" => :mojave
-    sha256 "9f6a1c40b39e78418342c54ca981648d0b2fe8db03c1fb81bf44aff3225e1dd9" => :high_sierra
-    sha256 "219b9bf172b5f7379c849a8e55b04dabd74f054564fdaec2fb40502f7e996967" => :sierra
-    sha256 "eb1f66d4cfb2c2c6768c89053fca5fc762c12b401b745cbbd1e6faad3ae25dcf" => :el_capitan
-    sha256 "fb3e7e6ab86d242c309a1bb2eaa83451b6fa7369a63222b8025de70a3dbb8615" => :yosemite
+    sha256 "3d1c85bb36faedf5ab12f78e8c3511dcc4164561ba8bc09924b48f6aa3fa0b37" => :mojave
+    sha256 "ba9b30eeabc11634502e30fd9a730d5727668550f9708d46fbefc03bcb3917de" => :high_sierra
+    sha256 "903ce6530395c0973418020561ddd60da739f3a36e865500776922e18975460b" => :sierra
   end
 
   def install
