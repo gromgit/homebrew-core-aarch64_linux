0737d51b1ccf33d992323489572951b9904a7074
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-15 00:55:58 +0000

    bzip2: update 1.0.8 bottle.

--- Formula/bzip2.rb
+++ Formula/bzip2.rb
@@ -6,9 +6,9 @@ class Bzip2 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0e2b2fd73f5cc63d50278b89a66da9313b058e43a5621a9a61c204b908f3af18" => :mojave
-    sha256 "0c791995ffa600c0f5d2d6566619ceffd309fee6f45b867bb0823e5812dc9425" => :high_sierra
-    sha256 "48ba103cc8c92c4f63f0ff8557368d73625e767908accd69ac830df840b8561a" => :sierra
+    sha256 "b8683b824f4cc702d06031c3762ba079e8bc1ea27413f6d08f10e93c539d89fd" => :mojave
+    sha256 "c7f2266c2d354c706de5163c23bb7b7204f1f15a85027ea486877a0c5d253336" => :high_sierra
+    sha256 "1f11350ccb9a3bd1dd250b5e440d68a5ea65408d4b91f9eae2aa7628e899b7c5" => :sierra
   end
 
   keg_only :provided_by_macos
