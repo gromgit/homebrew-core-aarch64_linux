75937dc178fae1ea18f80d6598b701af7554acb2
Caleb Xu <calebcenter@live.com>
2019-07-15 00:35:31 -0400

    stanford-parser 3.9.2
    Closes #41975.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/stanford-parser.rb
+++ Formula/stanford-parser.rb
@@ -1,9 +1,9 @@
 class StanfordParser < Formula
   desc "Statistical NLP parser"
   homepage "https://nlp.stanford.edu/software/lex-parser.shtml"
-  url "https://nlp.stanford.edu/software/stanford-parser-full-2018-02-27.zip"
-  version "3.9.1"
-  sha256 "c360eb294e52870a1df06878df65ed21c68145065d21f73c0028d717252a9bd7"
+  url "https://nlp.stanford.edu/software/stanford-parser-full-2018-10-17.zip"
+  version "3.9.2"
+  sha256 "92d852af54c0727c2367b9ce267c53bf08f1551a08ec5dd92c357b8cc7b2bcd9"
 
   bottle :unneeded
 
