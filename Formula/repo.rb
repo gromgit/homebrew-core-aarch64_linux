b9dfb2264c0fed68352bea333df11f462cfd7bdd
Caleb Xu <calebcenter@live.com>
2019-07-14 22:25:12 -0400

    repo 1.13.3
    Closes #41969.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/repo.rb
+++ Formula/repo.rb
@@ -2,8 +2,8 @@ class Repo < Formula
   desc "Repository tool for Android development"
   homepage "https://source.android.com/source/developing.html"
   url "https://gerrit.googlesource.com/git-repo.git",
-      :tag      => "v1.13.2",
-      :revision => "13f323b2c221db4e69e3f5a671455954b65f1fb3"
+      :tag      => "v1.13.3",
+      :revision => "c5b0e23490478b4e5d22335d7015f40a8c187518"
   version_scheme 1
 
   bottle :unneeded
