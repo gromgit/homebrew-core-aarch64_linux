19118d88dbb3a9c72615367e775543fd24e397e6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-16 19:51:06 +0000

    glooctl: update 0.17.1 bottle.

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -8,9 +8,9 @@ class Glooctl < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "793766e5d46c5cd1e42bc9f243fab074ae1c8e5089c5e0d9b3687444b135a4e4" => :mojave
-    sha256 "001535595f8f34762f4d6245b5b034ce2c2ffef17bd55058dfdae7d369734cb8" => :high_sierra
-    sha256 "cdc8b3c36d572d9933fdabbfd44f7b09781a0c899e577873d110a742487bb156" => :sierra
+    sha256 "e6a583696b3fc7fbd311e11308b149070b8faffce8a94d6da82b83a1bf664066" => :mojave
+    sha256 "7e0db2aae7f627b7535bf82bfe5ab7aab255653673299cafa3ba9b4f5c1fc4ec" => :high_sierra
+    sha256 "8d4a7113f64776f95ed0a52ab86dbb01192da8552ed71fb914b26cd2007ac2d9" => :sierra
   end
 
   depends_on "dep" => :build
