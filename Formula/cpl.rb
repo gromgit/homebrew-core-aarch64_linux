d5d2c81d36089b077ca7997998f6b31d2243589d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-16 23:18:21 +0000

    cpl: add 7.1 bottle.

--- Formula/cpl.rb
+++ Formula/cpl.rb
@@ -4,6 +4,13 @@ class Cpl < Formula
   url "ftp://ftp.eso.org/pub/dfs/pipelines/libraries/cpl/cpl-7.1.tar.gz"
   sha256 "ba7f762fac3550e9d9246b7fbacba54bfba4c47b833720834bd4cd21ed0e8b00"
 
+  bottle do
+    cellar :any
+    sha256 "3dc5ca15d5d77e85f0110ef70f97904759587c3a4bc35b6b967ab6d0a346d31d" => :mojave
+    sha256 "699080f73aa0a34d32d29d984057cc4f5f6ce8377951979687149fad93e063a2" => :high_sierra
+    sha256 "20e309b283fa09e0ee0adf876f2659c6f40ad2114101ebe4807ef9965c3b7fde" => :sierra
+  end
+
   depends_on "cfitsio"
   depends_on "fftw"
   depends_on "wcslib"
