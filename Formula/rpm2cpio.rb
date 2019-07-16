251bf38f562e365e96538b98fa0f8728f2b771f0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-16 23:54:07 +0000

    rpm2cpio: update 1.4 bottle.

--- Formula/rpm2cpio.rb
+++ Formula/rpm2cpio.rb
@@ -7,12 +7,9 @@ class Rpm2cpio < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ab485e451668ede8eea2b1a9f8ac1781aae218399604d72e28587ece7674f0b0" => :mojave
-    sha256 "82c3021a2e882829d635ff45fc9780e72d0199777aa5dbbeb6f07b1a17944369" => :high_sierra
-    sha256 "864c4e9533aaf62e67a119006017df772a961bb0c407a0184fb0c9d8e1202509" => :sierra
-    sha256 "d26a07db8b6c1293171dc4937d81b2dc9936da0d52718d5f94ddd83524d5974e" => :el_capitan
-    sha256 "623651ab4e150e1f05ba3a42aa6a18a1e7cb2c023cc3327cd3388f178e65d80a" => :yosemite
-    sha256 "fcb6787ea96ae09d99bb6a7e28f653966277543b7a832661bb6da1a2903567cf" => :mavericks
+    sha256 "081902485154a2061d890e6421a55d15bfe5072c05109c79e0ef50f2a11b96e5" => :mojave
+    sha256 "804dccff2726a9ac18a1002cd8adb06aacd07ce1fff93b995c042d4e78775176" => :high_sierra
+    sha256 "05f2a6011c554efb2c2196fdf08bfc6f7c6fd6d4e32530399888aabcc73ca339" => :sierra
   end
 
   depends_on "xz"
