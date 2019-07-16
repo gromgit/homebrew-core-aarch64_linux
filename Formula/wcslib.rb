897fafafae2956ce2765d2d38b13d760a46aab4e
Caleb Xu <calebcenter@live.com>
2019-07-16 00:19:41 -0400

    wcslib 6.3

--- Formula/wcslib.rb
+++ Formula/wcslib.rb
@@ -1,8 +1,8 @@
 class Wcslib < Formula
   desc "Library and utilities for the FITS World Coordinate System"
   homepage "https://www.atnf.csiro.au/people/mcalabre/WCS/"
-  url "https://www.atnf.csiro.au/pub/software/wcslib/wcslib-6.2.tar.bz2"
-  sha256 "bb4dfe242959bc4e5540890e0475754ad4a027dba971903dc4d82df8d564d805"
+  url "https://www.atnf.csiro.au/pub/software/wcslib/wcslib-6.3.tar.bz2"
+  sha256 "4c37f07d64d51abaf30093238fdd426a321ffaf5a9575f7fd81e155d973822ea"
 
   bottle do
     cellar :any
@@ -21,12 +21,6 @@ class Wcslib < Formula
                           "--with-cfitsioinc=#{Formula["cfitsio"].opt_include}",
                           "--without-pgplot",
                           "--disable-fortran"
-
-    # Currently doesn't support parallel make.  Patch sent to author 2018/08/31.
-    # Author (mcalabre@atnf.csiro.au) expects to integrate by end of 2018/09.
-    # Patch: https://gist.github.com/dstndstn/0492f69eb27a11cdd622d01105643dd0
-    ENV.deparallelize
-    system "make"
     system "make", "install"
   end
 
