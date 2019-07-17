0aca861c37b377f1433b45b6e302be2af0eac175
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-17 07:59:43 +0000

    libspng: add 0.4.5 bottle.

--- Formula/libspng.rb
+++ Formula/libspng.rb
@@ -4,6 +4,13 @@ class Libspng < Formula
   url "https://gitlab.com/randy408/libspng/uploads/6ddcaa59367b2cea474213a994b82012/libspng-0.4.5.tar.xz"
   sha256 "a6b18bf8bdef479d7556dba7e19d781cb9032aef04b2f95e855e0f563c5aee45"
 
+  bottle do
+    cellar :any
+    sha256 "c0a033d31ecd804286f453b78ed915479c645f3cdc9582eb9292304945ffc8c3" => :mojave
+    sha256 "733decd9549a1c7926bd962e58d5affee6042fea46a7668cc4d2f7f5920e1062" => :high_sierra
+    sha256 "c98ef1891f03bc2a0c3c6649bec9a4d07a4fbc52a10e2b853dfcbd7ced6b9446" => :sierra
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
