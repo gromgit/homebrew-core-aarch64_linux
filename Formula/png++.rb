a305261d6c5f08bbfd8c72d5c544e97c8fdd159d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-17 19:39:40 +0000

    png++: update 0.2.9 bottle.

--- Formula/png++.rb
+++ Formula/png++.rb
@@ -6,12 +6,9 @@ class Pngxx < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0a5221ea49fb845c830698b2d1c170cb8d59d18916ca843a5c80ef7d48b37435" => :mojave
-    sha256 "4085580bff1ec2116c321e84a426d195943a002f84b4662c9814a64e5925c627" => :high_sierra
-    sha256 "41785b523dc50e5b237d83d027b34b6532114a6751eff3f409ee0ba9e0730f04" => :sierra
-    sha256 "7b01b3ff0af9e60f2887bb45ff5ba2f5823a9a440c2d78e51e69904d3edd80d8" => :el_capitan
-    sha256 "de37d7fadb7308b45ba0448308256d00bf36442bacbab1e734ee8398aea8a8dd" => :yosemite
-    sha256 "f2e242ee428f191645418a9897eb2fd729408dd67d04e7af4cefc7dcb5715250" => :mavericks
+    sha256 "fa03e888c8eff093815be8b459671194c292af6bfbc247210c42aed349b142f5" => :mojave
+    sha256 "fa03e888c8eff093815be8b459671194c292af6bfbc247210c42aed349b142f5" => :high_sierra
+    sha256 "bed85536ebfec14b98b056e874d116527f154eff2b77024aee52fc407d66791f" => :sierra
   end
 
   depends_on "libpng"
