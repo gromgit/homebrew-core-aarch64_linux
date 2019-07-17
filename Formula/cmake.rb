1a49766cc49fdad1b2538633aed6b30e198d277a
Jamie Snape <jamie.snape@kitware.com>
2019-07-17 12:13:27 -0400

    cmake 3.15.0
    Closes #42084.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/cmake.rb
+++ Formula/cmake.rb
@@ -1,8 +1,8 @@
 class Cmake < Formula
   desc "Cross-platform make"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v3.14.6/cmake-3.14.6.tar.gz"
-  sha256 "4e8ea11cabe459308671b476469eace1622e770317a15951d7b55a82ccaaccb9"
+  url "https://github.com/Kitware/CMake/releases/download/v3.15.0/cmake-3.15.0.tar.gz"
+  sha256 "0678d74a45832cacaea053d85a5685f3ed8352475e6ddf9fcb742ffca00199b5"
   head "https://cmake.org/cmake.git"
 
   bottle do
