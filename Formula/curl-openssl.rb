51760a30a9d65c458c7f234f63d44a3557b007c7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-17 09:14:14 +0000

    curl-openssl: update 7.65.2 bottle.

--- Formula/curl-openssl.rb
+++ Formula/curl-openssl.rb
@@ -5,9 +5,9 @@ class CurlOpenssl < Formula
   sha256 "8093398b51e7d8337dac6f8fa6f1f77d562bdd9eca679dff9d9c3b8160ebfd28"
 
   bottle do
-    sha256 "5f265df38e121f3514c2923e016b843f2062b738e3b5659d40030f4fb6b22294" => :mojave
-    sha256 "2cde93c00a3db5578bc9af336bee3d0c45255279d600d4bcfb259a9586d193fd" => :high_sierra
-    sha256 "9068eedd390a3ae63e582e4bd556c1858707d91cc77ed0f0c2b3082f199d5e6b" => :sierra
+    sha256 "e5c37bd7de02bbf8e493314b7efd2d3581e594640e6757cc0b3224433b9540cf" => :mojave
+    sha256 "ebbfb605e86dd445fbd5191f711ef340eb9e150c6c32a986fb65f100a641f9ff" => :high_sierra
+    sha256 "5899ccfda83d6592e36609ab8eb993eecf18a2067d265aaa330926ccb9bd7667" => :sierra
   end
 
   head do
