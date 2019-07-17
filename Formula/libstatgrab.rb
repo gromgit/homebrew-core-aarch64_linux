2cfb42053859eb52756b214c86b581ae54345be1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-17 21:18:50 +0000

    libstatgrab: update 0.92 bottle.

--- Formula/libstatgrab.rb
+++ Formula/libstatgrab.rb
@@ -7,12 +7,9 @@ class Libstatgrab < Formula
 
   bottle do
     cellar :any
-    sha256 "c8326d6713914ba30322309ed22bf7b43a602a052d3512c77d1ddf32a05c1655" => :mojave
-    sha256 "100b7116b96c56d4643954b0bb9558b77480ee03f639239b32ed331f3b51d720" => :high_sierra
-    sha256 "00a91b891f940b01a5ca5d341fb6c9248da6c87312b9ecefbd0aae9cc49b62c3" => :sierra
-    sha256 "5cb49282a895e5b5229a59eedaae8a7ce7517ff6d56dc4837ddbd1601b72eaa9" => :el_capitan
-    sha256 "a2175221c61a952b2ed4590c1b5496dcb10d4e9547db4bba8ac69968c2c2a586" => :yosemite
-    sha256 "611bd2051960ea32dcc1a814ec93638bd2feb83e76141b715cb47b28d1cc0c92" => :mavericks
+    sha256 "bb1778c08b1b91cff873016e3a6f314d3a97a55db378e0870354bb64337ea50b" => :mojave
+    sha256 "d7d932298fe68980389bf5b2c8f1d6ef41a6037630b4951996139c2277fbf6f4" => :high_sierra
+    sha256 "17efc663227f42859add13c81e0b5fac1f3f3a0418c3d15b83363ea90c0b4a91" => :sierra
   end
 
   def install
