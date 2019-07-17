82802f24e205563f1f07635435f586aa1bb8f8a8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-17 09:36:45 +0000

    rush: update 2.1 bottle.

--- Formula/rush.rb
+++ Formula/rush.rb
@@ -6,11 +6,9 @@ class Rush < Formula
   sha256 "c2ff487d44ce3d14854d0269eb0aa4c0f98bcca35390fad5ea52da75d9e4abdf"
 
   bottle do
-    sha256 "07ea3d94a8d34880b65ef978c5619e7bee5c67568d14a2afa7b3818541fe955d" => :mojave
-    sha256 "421c4ff7a56f4d434ce69551fcdbfc9ec39bec8e472b4d64923be0a93adc6405" => :high_sierra
-    sha256 "6147781c7c30c744d3e3aa69b55e72bb27a320aec6bf6e8d4a8360d5982aad20" => :sierra
-    sha256 "a33fca1d7ccc86d125771c7f4e983244abe182d3a3731faa86fa12f21f7ab18d" => :el_capitan
-    sha256 "5fcd6654497adb320a87fcf9b2b67361f157449f029a918148d90f60089920a1" => :yosemite
+    sha256 "5cdc9e464c7086e99e26063787dfefafd4805d90b0ea5aa40044b81f23d10db1" => :mojave
+    sha256 "a76250fc5b34898050b9e18abd00dffbefd2c37dcd021b37d30bef75574abe49" => :high_sierra
+    sha256 "55acb177bf3b6c2d041341b9a625ac10c6aba1237974febd66e40f1a7ec23319" => :sierra
   end
 
   def install
