8bb7892d657673d949ab1e1ad7b8af52bb512b88
FX Coudert <fxcoudert@gmail.com>
2019-07-17 11:27:28 +0200

    rush 2.1
    Closes #42069.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/rush.rb
+++ Formula/rush.rb
@@ -1,9 +1,9 @@
 class Rush < Formula
   desc "GNU's Restricted User SHell"
   homepage "https://www.gnu.org/software/rush/"
-  url "https://ftp.gnu.org/gnu/rush/rush-1.8.tar.gz"
-  mirror "https://ftpmirror.gnu.org/rush/rush-1.8.tar.gz"
-  sha256 "7f321e5556435b12638982c8d94c587982122f28c220f8bc679d7817035aadef"
+  url "https://ftp.gnu.org/gnu/rush/rush-2.1.tar.xz"
+  mirror "https://ftpmirror.gnu.org/rush/rush-2.1.tar.xz"
+  sha256 "c2ff487d44ce3d14854d0269eb0aa4c0f98bcca35390fad5ea52da75d9e4abdf"
 
   bottle do
     sha256 "07ea3d94a8d34880b65ef978c5619e7bee5c67568d14a2afa7b3818541fe955d" => :mojave
