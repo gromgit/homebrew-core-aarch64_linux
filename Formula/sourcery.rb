2da5c6cf9f9771b38217e863b21b855a2425610b
Liquidsoul <liquidsoul@liquidsoul.fr>
2019-07-18 22:08:02 +0200

    sourcery 0.16.2
    Closes #42125.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/sourcery.rb
+++ Formula/sourcery.rb
@@ -1,8 +1,8 @@
 class Sourcery < Formula
   desc "Meta-programming for Swift, stop writing boilerplate code"
   homepage "https://github.com/krzysztofzablocki/Sourcery"
-  url "https://github.com/krzysztofzablocki/Sourcery/archive/0.16.1.tar.gz"
-  sha256 "2a015af9e501eeabfae72033e91620365c1ebdf4dc401ae2971081d479b6186b"
+  url "https://github.com/krzysztofzablocki/Sourcery/archive/0.16.2.tar.gz"
+  sha256 "a04ca9958d8a3d4570059cd71b3df1fda8db27027ef1340f28449a94f8e9ccc8"
   head "https://github.com/krzysztofzablocki/Sourcery.git"
 
   bottle do
@@ -11,8 +11,7 @@ class Sourcery < Formula
     sha256 "8e030ca0e08585b2c58db2504da93631a754dd38331615d12350ac8a1873ca33" => :high_sierra
   end
 
-  depends_on :xcode => ["10.0", :build]
-  depends_on :xcode => "6.0"
+  depends_on :xcode => "10.2"
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release", "-Xswiftc", "-target", "-Xswiftc", "x86_64-apple-macosx10.11"
