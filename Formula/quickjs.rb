d059f0af2c62d24a2cab290b4f06f5e810d214a2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-18 17:21:53 +0000

    quickjs: add 2019-07-09 bottle.

--- Formula/quickjs.rb
+++ Formula/quickjs.rb
@@ -4,6 +4,12 @@ class Quickjs < Formula
   url "https://bellard.org/quickjs/quickjs-2019-07-09.tar.xz"
   sha256 "350c1cd9dd318ad75e15c9991121c80b85c2ef873716a8900f811554017cd564"
 
+  bottle do
+    sha256 "989215f264fc3240904342524457b923da278d6edd53e8fb44dad1f370b4e64f" => :mojave
+    sha256 "0f472d5f083df02c3516f42c43333f09fd6e2734da64ce7a4ae027a7e4dc808f" => :high_sierra
+    sha256 "3fbf3dfc902c9e68415088325ec7ec6f16e1c69204b3cd3432046d6bed14896e" => :sierra
+  end
+
   def install
     system "make", "install", "prefix=#{prefix}"
   end
