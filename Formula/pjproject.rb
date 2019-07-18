94e1ca84fef20c6e38aba30be8964044c39f290c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-18 06:32:07 +0000

    pjproject: update 2.9 bottle.

--- Formula/pjproject.rb
+++ Formula/pjproject.rb
@@ -7,10 +7,8 @@ class Pjproject < Formula
 
   bottle do
     cellar :any
-    sha256 "2aa9d3fa63190d540d0a97203d086a096d7806db0d3fdb2995b1f1cc85674998" => :mojave
-    sha256 "eec232d8fe6c5b4e05521a87a4bd1a5cb4fcc6a4f0a996ddaacc33bd2d93255f" => :high_sierra
-    sha256 "f7d12f6bcb2a628df0e87e4bbaffc368ca004ed6de12d11d4d019b080257a2d0" => :sierra
-    sha256 "52a55c49ef0d0d53abf447fd060e7eaf8bd1d10c8f6b419582c5e453634d5b61" => :el_capitan
+    sha256 "32547d3d6ca05978e26ed421bb1464494217964a218b5e76149ddd14c1c33f30" => :mojave
+    sha256 "276438da8f875128a0ff0240a661bc2665fac9fb6ad97885666ccf81e5e56ffb" => :high_sierra
   end
 
   depends_on :macos => :high_sierra # Uses Security framework API enum cases introduced in 10.13.4
