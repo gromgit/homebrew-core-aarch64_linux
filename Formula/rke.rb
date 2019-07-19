1782bfdf1b5f9d30f12a138ad13c91ca2066bbee
Rui Chen <rchen@meetup.com>
2019-07-19 14:34:43 -0400

    rke 0.2.6
    Closes #42186.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -2,8 +2,8 @@ class Rke < Formula
   desc "Rancher Kubernetes Engine, a Kubernetes installer that works everywhere"
   homepage "https://rancher.com/docs/rke/v0.1.x/en/"
   url "https://github.com/rancher/rke.git",
-      :tag      => "v0.2.5",
-      :revision => "960d353a08dec21f56aacf7dd4112db9f70a8ecc"
+      :tag      => "v0.2.6",
+      :revision => "14fa77df0d3f438f668451122dc9c082e5ce785d"
 
   bottle do
     cellar :any_skip_relocation
