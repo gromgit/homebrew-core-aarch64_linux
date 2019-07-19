0e4a7e2766e7d138d8940b9e3b45e92d27d1c48e
Caleb Xu <calebcenter@live.com>
2019-07-19 17:38:09 -0400

    s6 2.8.0.1
    Closes #42196.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/s6.rb
+++ Formula/s6.rb
@@ -1,21 +1,8 @@
 class S6 < Formula
   desc "Small & secure supervision software suite"
   homepage "https://skarnet.org/software/s6/"
-
-  stable do
-    url "https://skarnet.org/software/s6/s6-2.7.2.0.tar.gz"
-    sha256 "af54fcbae7028a90bd12c7ee71a8f3954a74c6a4de376a427cc664587fb68a09"
-
-    resource "skalibs" do
-      url "https://skarnet.org/software/skalibs/skalibs-2.7.0.0.tar.gz"
-      sha256 "96494d76669d2f8622511d5d616b6367801a42683c0bb11a8855114e5ccbd756"
-    end
-
-    resource "execline" do
-      url "https://skarnet.org/software/execline/execline-2.5.0.1.tar.gz"
-      sha256 "8d07d14e9e9abb1301e08be271313c4ffa5ddf7248fd262dda19588e78e31049"
-    end
-  end
+  url "https://skarnet.org/software/s6/s6-2.8.0.1.tar.gz"
+  sha256 "dbe08f5b76c15fa32a090779b88fb2de9a9a107c3ac8ce488931dd39aa1c31d8"
 
   bottle do
     sha256 "de84ce2daa762ee81c1ba5580030e912072ec3659ff3ba876c4da11c27e146ed" => :mojave
@@ -24,16 +11,14 @@ class S6 < Formula
     sha256 "760b7806a4f4f9d7e78369406033fca48821933d3f7bdca70298efce5469471f" => :el_capitan
   end
 
-  head do
-    url "https://git.skarnet.org/cgi-bin/cgit.cgi/s6", :using => :git
+  resource "skalibs" do
+    url "https://skarnet.org/software/skalibs/skalibs-2.8.1.0.tar.gz"
+    sha256 "431c6507b4a0f539b6463b4381b9b9153c86ad75fa3c6bfc9dc4722f00b166ba"
+  end
 
-    resource "skalibs" do
-      url "https://git.skarnet.org/cgi-bin/cgit.cgi/skalibs", :using => :git
-    end
-
-    resource "execline" do
-      url "https://git.skarnet.org/cgi-bin/cgit.cgi/execline", :using => :git
-    end
+  resource "execline" do
+    url "https://skarnet.org/software/execline/execline-2.5.1.0.tar.gz"
+    sha256 "b1a756842947488404db8173bbae179d6e78b6ef551ec683acca540ecaf22677"
   end
 
   def install
