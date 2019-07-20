4fc4982599373e4138f973a570f1c0e59792b108
Viktor Szakats <commit@vszakats.net>
2019-07-20 11:57:03 +0000

    verilator: secure url(s)

--- Formula/verilator.rb
+++ Formula/verilator.rb
@@ -11,7 +11,7 @@ class Verilator < Formula
   end
 
   head do
-    url "http://git.veripool.org/git/verilator", :using => :git
+    url "https://git.veripool.org/git/verilator", :using => :git
     depends_on "autoconf" => :build
     depends_on "automake" => :build
   end
