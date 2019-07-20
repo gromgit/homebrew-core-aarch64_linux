509b8fffada411da416dbf3c5b2878b5f940e3b2
Viktor Szakats <commit@vszakats.net>
2019-07-20 11:57:03 +0000

    sgrep: secure url(s)

--- Formula/sgrep.rb
+++ Formula/sgrep.rb
@@ -1,9 +1,7 @@
 class Sgrep < Formula
   desc "Search SGML, XML, and HTML"
   homepage "https://www.cs.helsinki.fi/u/jjaakkol/sgrep.html"
-  # curl: (9) Server denied you to change to the given directory
-  # ftp://ftp.cs.helsinki.fi/pub/Software/Local/Sgrep/sgrep-1.94a.tar.gz
-  url "https://www.mirrorservice.org/sites/distfiles.macports.org/sgrep2/sgrep-1.94a.tar.gz"
+  url "https://www.cs.helsinki.fi/pub/Software/Local/Sgrep/sgrep-1.94a.tar.gz"
   mirror "https://fossies.org/linux/misc/old/sgrep-1.94a.tar.gz"
   sha256 "d5b16478e3ab44735e24283d2d895d2c9c80139c95228df3bdb2ac446395faf9"
 
