c94b147c33340f063e4a32fca9b457e28915df02
Caleb Xu <calebcenter@live.com>
2019-07-20 11:58:18 -0400

    ecasound 2.9.2
    Closes #42229.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/ecasound.rb
+++ Formula/ecasound.rb
@@ -1,8 +1,8 @@
 class Ecasound < Formula
   desc "Multitrack-capable audio recorder and effect processor"
   homepage "https://www.eca.cx/ecasound/"
-  url "https://ecasound.seul.org/download/ecasound-2.9.1.tar.gz"
-  sha256 "39fce8becd84d80620fa3de31fb5223b2b7d4648d36c9c337d3739c2fad0dcf3"
+  url "https://ecasound.seul.org/download/ecasound-2.9.2.tar.gz"
+  sha256 "c14991dfc77eb5f7e3851aaeaf71290b21dcd451b65c63b4db8e0bec90d02c97"
 
   bottle do
     rebuild 1
@@ -11,14 +11,25 @@ class Ecasound < Formula
     sha256 "fe062d9a9f6c5072c2a6dd8778c1fd842694e43fb91dede0f877628a2d8eb27f" => :sierra
   end
 
+  depends_on "jack"
+  depends_on "libsamplerate"
+  depends_on "libsndfile"
+
   def install
     args = %W[
       --disable-debug
       --disable-dependency-tracking
       --prefix=#{prefix}
       --enable-rubyecasound=no
+      --enable-sys-readline=no
     ]
     system "./configure", *args
     system "make", "install"
+  end
+
+  test do
+    fixture = test_fixtures("test.wav")
+    system bin/"ecasound", "-i", "resample,auto,#{fixture}", "-o", testpath/"test.cdr"
+    assert_predicate testpath/"test.cdr", :exist?
   end
 end
