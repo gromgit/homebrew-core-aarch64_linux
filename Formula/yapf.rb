af5c5f69c5557731979b645e4d5492ea9b1edea1
Rui Chen <rchen@meetup.com>
2019-07-20 18:47:46 -0400

    yapf 0.28.0
    Closes #42233.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/yapf.rb
+++ Formula/yapf.rb
@@ -3,8 +3,8 @@ class Yapf < Formula
 
   desc "Formatter for python code"
   homepage "https://github.com/google/yapf"
-  url "https://files.pythonhosted.org/packages/0c/ad/1dd7e729e9d707c602267ed9a6ca9b771a507862f85456bf18f5fff8f0d1/yapf-0.27.0.tar.gz"
-  sha256 "34f6f80c446dcb2c44bd644c4037a2024b6645e293a4c9c4521983dd0bb247a1"
+  url "https://files.pythonhosted.org/packages/89/41/7f7c884531730c0cb471764e1ddf50f59d25bb2ab258ede633264344e9cb/yapf-0.28.0.tar.gz"
+  sha256 "6f94b6a176a7c114cfa6bad86d40f259bbe0f10cf2fa7f2f4b3596fc5802a41b"
 
   bottle do
     cellar :any_skip_relocation
