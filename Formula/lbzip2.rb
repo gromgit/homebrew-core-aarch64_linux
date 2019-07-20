e90fc02fe39c23079ffecaa186ca360e9b64f732
Viktor Szakats <commit@vszakats.net>
2019-07-20 11:57:02 +0000

    lbzip2: fix broken urls

--- Formula/lbzip2.rb
+++ Formula/lbzip2.rb
@@ -1,7 +1,8 @@
 class Lbzip2 < Formula
   desc "Parallel bzip2 utility"
   homepage "https://github.com/kjn/lbzip2"
-  url "http://archive.lbzip2.org/lbzip2-2.5.tar.bz2"
+  url "https://web.archive.org/web/20170304050514/archive.lbzip2.org/lbzip2-2.5.tar.bz2"
+  mirror "https://fossies.org/linux/privat/lbzip2-2.5.tar.bz2"
   sha256 "eec4ff08376090494fa3710649b73e5412c3687b4b9b758c93f73aa7be27555b"
 
   bottle do
