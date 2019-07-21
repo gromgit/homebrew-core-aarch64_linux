f943e22a876f38fc9544f5b0538ff09651708918
Igor Kapkov <igasgeek@me.com>
2019-07-21 10:10:47 +1000

    gibo 2.2.3
    Closes #42243.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/gibo.rb
+++ Formula/gibo.rb
@@ -1,8 +1,8 @@
 class Gibo < Formula
   desc "Access GitHub's .gitignore boilerplates"
   homepage "https://github.com/simonwhitaker/gibo"
-  url "https://github.com/simonwhitaker/gibo/archive/2.2.2.tar.gz"
-  sha256 "1ce2497b0d8a5ad165d7428c4e87e40072581f3ba6ff56b3fceeb53c52630442"
+  url "https://github.com/simonwhitaker/gibo/archive/2.2.3.tar.gz"
+  sha256 "022132cc0d98c7c4a8002c34940cca0c56fd993c1391df4e43f7e3b7517cd63a"
 
   bottle :unneeded
 
