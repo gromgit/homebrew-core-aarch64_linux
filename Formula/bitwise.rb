a7548a3dd4a4736b8588c339673b0c0f24139914
Max Riveiro <kavu13@gmail.com>
2019-07-21 21:57:58 +0300

    bitwise 0.33 (New Formula)
    Closes #42257.
    
    Signed-off-by: Sean Molenaar <smillerdev@me.com>

---
+++ Formula/bitwise.rb
@@ -0,0 +1,23 @@
+class Bitwise < Formula
+  desc "Terminal based bit manipulator in ncurses"
+  homepage "https://github.com/mellowcandle/bitwise"
+  url "https://github.com/mellowcandle/bitwise/releases/download/v0.33/bitwise-v0.33.tar.gz"
+  sha256 "598a623d9befaf1e724d78406a93140c1fc05ba9526f11bee945d236a92572c8"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "readline"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+
+    system "make", "install"
+  end
+
+  test do
+    assert_match "0 0 1 0 1 0 0 1", shell_output("#{bin}/bitwise --no-color '0x29A >> 4'")
+  end
+end
