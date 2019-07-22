071453ca90acc6185a09e3265370e7decedc541a
EricFromCanada <enk3@outlook.com>
2019-07-22 17:59:19 -0400

    wireshark 3.0.3
    Closes #42306.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/wireshark.rb
+++ Formula/wireshark.rb
@@ -1,9 +1,9 @@
 class Wireshark < Formula
   desc "Graphical network analyzer and capture tool"
   homepage "https://www.wireshark.org"
-  url "https://www.wireshark.org/download/src/all-versions/wireshark-3.0.2.tar.xz"
-  mirror "https://1.eu.dl.wireshark.org/src/all-versions/wireshark-3.0.2.tar.xz"
-  sha256 "7e1ec5664a0dba4493d8729f9922378bdd05040fb159c2b03b42111efda2e53b"
+  url "https://www.wireshark.org/download/src/all-versions/wireshark-3.0.3.tar.xz"
+  mirror "https://1.eu.dl.wireshark.org/src/all-versions/wireshark-3.0.3.tar.xz"
+  sha256 "af92729c781d870110885c31ebcbe4c4224ed51bb580d00c896fe9746994211c"
   head "https://code.wireshark.org/review/wireshark", :using => :git
 
   bottle do
