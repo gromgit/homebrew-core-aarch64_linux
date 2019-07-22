557fcc7c2ba86277af930dc1a2824fd66fbcd958
Caleb Xu <calebcenter@live.com>
2019-07-22 17:23:14 -0400

    swaks 20181104.0
    Closes #42302.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/swaks.rb
+++ Formula/swaks.rb
@@ -1,8 +1,8 @@
 class Swaks < Formula
   desc "SMTP command-line test tool"
   homepage "https://www.jetmore.org/john/code/swaks/"
-  url "https://www.jetmore.org/john/code/swaks/files/swaks-20170101.0.tar.gz"
-  sha256 "84e62dd0de4a56d5daebe25afd16835bd8d3c7f39caa5e6bc7d86a056925915e"
+  url "https://www.jetmore.org/john/code/swaks/files/swaks-20181104.0.tar.gz"
+  sha256 "023f7f8818ebcd638618327809cf1939c5ffcaf6c3d2572ef56179d68f683e58"
 
   bottle :unneeded
 
