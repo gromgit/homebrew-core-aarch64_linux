12ebcbebb57de0d75676182ff8fd8abab9dcc023
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-22 12:39:09 +0000

    sf-pwgen: update 1.5 bottle.

--- Formula/sf-pwgen.rb
+++ Formula/sf-pwgen.rb
@@ -7,11 +7,9 @@ class SfPwgen < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ddd6e11e839c49e2fafb3ed4c3a28b773bc1580a47a441c9ffb025b3bfa9dbd3" => :mojave
-    sha256 "05ac7530759e9f2cf3a0e94a5b1f4fea83a1710de8010fa9b3d9b60167018038" => :high_sierra
-    sha256 "ff59281df8d8c0e341233cdffafe67d0dc78101afb90a8597b0a83605ef578a3" => :sierra
-    sha256 "2c0e0523569aa25fe254012d3b86ae0bdc587c0f17c4a62d8d12917b7fa44fbf" => :el_capitan
-    sha256 "2c6d133b3c9b079dc8c81407107a3c1fb4d5cb3d654afa7acef6f23b9f9df9a6" => :yosemite
+    sha256 "50e87a417ac3d9b5be7318c7e2983db1a1f90759fab02a898f9cd257b15ac6e2" => :mojave
+    sha256 "2ebd137c58bd8d20a50251e159b6074e65009a265aa351cf6eb0afd39d59edc1" => :high_sierra
+    sha256 "01cf1ff26d304c0cbb0072130ba2476ddeebd8933040092b937ced1ede06c2a2" => :sierra
   end
 
   def install
