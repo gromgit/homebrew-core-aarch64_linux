a7f3781788ecfc1450ef4129acbf9cd332e8e6e6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-22 13:54:36 +0000

    dhall-bash: add 1.0.21 bottle.

--- Formula/dhall-bash.rb
+++ Formula/dhall-bash.rb
@@ -9,6 +9,13 @@ class DhallBash < Formula
   sha256 "2801d68cba9682a493b7ff54e3773aa262c59db7e37174fcd7a6faa033073b1b"
   head "https://github.com/dhall-lang/dhall-haskell.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f4d548e7aed6ed60d1a3ae5c0813ec8bfc9504a28b9bc5b4778136fb9804e2ae" => :mojave
+    sha256 "6b29a05b3846fe870658c1bef2d52af49deff1d91f8bb5bfb311ebf9a6ccd7ac" => :high_sierra
+    sha256 "eb836ac98acf3dd6b0efd428c7d1006627cc4139f9245b0643f71ff2cff11795" => :sierra
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
 
