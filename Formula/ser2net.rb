d1d0547f94bfc3c07dd41613ffe1d6fb431a1d47
Caleb Xu <calebcenter@live.com>
2019-07-22 07:43:52 -0400

    ser2net 3.5.1
    Closes #42287.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/ser2net.rb
+++ Formula/ser2net.rb
@@ -1,8 +1,8 @@
 class Ser2net < Formula
   desc "Allow network connections to serial ports"
   homepage "https://ser2net.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/ser2net/ser2net/ser2net-3.5.tar.gz"
-  sha256 "ba9e1d60a89fd7ed075553b4a2074352902203f7fbd9b65b15048c05f0e3f3be"
+  url "https://downloads.sourceforge.net/project/ser2net/ser2net/ser2net-3.5.1.tar.gz"
+  sha256 "02f5dd0abbef5a17b80836b0de1ef0588e257106fb5e269b86822bfd001dc862"
 
   bottle do
     sha256 "36da05b58e05873eec72b04da6bb5e719d26814d8820927bc8684ac680f67587" => :mojave
@@ -13,13 +13,6 @@ class Ser2net < Formula
   depends_on :macos => :sierra # needs clock_gettime
 
   def install
-    # values.h doesn't exist on macOS
-    # https://github.com/cminyard/ser2net/pull/4
-    inreplace "readconfig.c", "#include <values.h>", ""
-
-    # Fix etc location
-    inreplace ["ser2net.c", "ser2net.8"], "/etc/ser2net.conf", "#{etc}/ser2net.conf"
-
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}"
