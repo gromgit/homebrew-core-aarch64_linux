e9961cf93f7d2e61c884a086dead912a9ac55695
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-22 20:07:09 +0000

    x11vnc: update 0.9.16 bottle.

--- Formula/x11vnc.rb
+++ Formula/x11vnc.rb
@@ -6,11 +6,9 @@ class X11vnc < Formula
 
   bottle do
     cellar :any
-    sha256 "388752257323e1376251e32dea385830afd3a11215b3661dc928111442cb2b5c" => :mojave
-    sha256 "3ed573fd3d02c34f407858af3f6e01386d1af2fc9fee16258eea94a4b2c19137" => :high_sierra
-    sha256 "3f840749807f57b4248c9ae202a214209c4bf4c4e7386d47da83b424153abe2a" => :sierra
-    sha256 "f4a33301592ef159be8f999fce086875bf88afd6ad2b48d6709c2f32d4ab3be1" => :el_capitan
-    sha256 "38e07e6c3a26cf1e8d60f1a4e7061da400afb9bb2803f0dd79566c5a3bfd0d22" => :yosemite
+    sha256 "cd3d5d0047a8fb2e7b66ac94baf08c2da16aa8e135b8180acacce2d1bf366e58" => :mojave
+    sha256 "2660aa48f9545eef71c5a42f9985720629d0391eaef37155264ec4c71cf13b29" => :high_sierra
+    sha256 "4e974a6cbc6bd9c03e90ed2f991a40c4589489ccbd01bd20552bf0a66773f924" => :sierra
   end
 
   depends_on "autoconf" => :build
