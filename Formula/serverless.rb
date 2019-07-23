8141ecd12f4eb551637f12e7a308982cbcbf9f3c
Rui Chen <rchen@meetup.com>
2019-07-23 14:28:26 -0400

    serverless 1.48.3
    Closes #42346.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/serverless.rb
+++ Formula/serverless.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Serverless < Formula
   desc "Build applications with serverless architectures"
   homepage "https://serverless.com"
-  url "https://github.com/serverless/serverless/archive/v1.48.2.tar.gz"
-  sha256 "6d12d48a659ebbb6e67b05364cf24c726803cd3e48ed602ca7ccf987f952a032"
+  url "https://github.com/serverless/serverless/archive/v1.48.3.tar.gz"
+  sha256 "c7c4917331186b9b1a19b4db1879e14013b90317540dbf8ebddf4eeab976c5ad"
 
   bottle do
     cellar :any_skip_relocation
@@ -29,6 +29,8 @@ class Serverless < Formula
         stage: dev
         region: eu-west-1
     EOS
+
+    system("#{bin}/serverless config credentials --provider aws --key aa --secret xx")
     output = shell_output("#{bin}/serverless package")
     assert_match "Serverless: Packaging service...", output
   end
