a96087e21b438dba6346f8f678cbd661883127be
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-23 18:03:24 +0000

    grpc-swift: add 0.9.1 bottle.

--- Formula/grpc-swift.rb
+++ Formula/grpc-swift.rb
@@ -5,6 +5,12 @@ class GrpcSwift < Formula
   sha256 "6b3a2ed13c805c6b8f339f558f2a1372bdcf84c079c104a6d0b54fd7650b8fbf"
   head "https://github.com/grpc/grpc-swift.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f8d66abf01b8b8573b58718a8999ab905619a2b677aa24050dab78e1cd1323de" => :mojave
+    sha256 "a8abd068afd441640955a8399ebb07c63228716e6ecd7198258f9e4b92a22e4f" => :high_sierra
+  end
+
   depends_on :xcode => ["10.0", :build]
   depends_on "protobuf"
   depends_on "swift-protobuf"
