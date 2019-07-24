b0bb8563f6676dfcc4016d16c0eadc32e0941830
Rui Chen <rchen@meetup.com>
2019-07-23 23:30:46 -0400

    lerna 3.16.4 (new formula)
    Closes #42377.
    
    Signed-off-by: Sean Molenaar <smillerdev@me.com>

---
+++ Formula/lerna.rb
@@ -0,0 +1,22 @@
+require "language/node"
+
+class Lerna < Formula
+  desc "Tool for managing JavaScript projects with multiple packages"
+  homepage "https://lerna.js.org"
+  url "https://registry.npmjs.org/lerna/-/lerna-3.16.4.tgz"
+  sha256 "dcaee23bbed63e3c278a934394a2cbef3ff896d12cc10ffae67f38960ce7d979"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/lerna --version")
+
+    output = shell_output("#{bin}/lerna init --independent 2>&1")
+    assert_match "lerna success Initialized Lerna files", output
+  end
+end
