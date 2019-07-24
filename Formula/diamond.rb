7f91bde06ea24194a8d158f6097d1bda7f08209c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-24 07:06:14 +0000

    diamond: update 0.9.25 bottle.

--- Formula/diamond.rb
+++ Formula/diamond.rb
@@ -6,9 +6,9 @@ class Diamond < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d142fdeb7bcaf6fe11a63bea7027d7e71a16165243ec1a4e1bc319941ffea21f" => :mojave
-    sha256 "2ea545335e21704468d960aae7b9d5702e7a0bcb7540cc7ca6bb872ecce4bb50" => :high_sierra
-    sha256 "a7eb2dad7dece443eeb54b53084bc7c715f221067dce51d7e238214dc402b2d2" => :sierra
+    sha256 "d67e3246f8a06a2791446e584bbc732d489d87b37f08c093de1b936beef49593" => :mojave
+    sha256 "7f390e12983dceccc640ef4c2121da20b7baf89a920bfd9dec56bf7a39f418c9" => :high_sierra
+    sha256 "3e7b49eab32d6594ca27b54a9034ea07f3b95bfa65b26396a7978d2468fea809" => :sierra
   end
 
   depends_on "cmake" => :build
