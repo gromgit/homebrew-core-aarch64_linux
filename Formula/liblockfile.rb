8b80db71252bccd569a2fad280a316c49bc0438d
Caleb Xu <calebcenter@live.com>
2019-07-24 08:03:15 -0400

    liblockfile 1.16
    Closes #42386.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/liblockfile.rb
+++ Formula/liblockfile.rb
@@ -1,8 +1,8 @@
 class Liblockfile < Formula
   desc "Library providing functions to lock standard mailboxes"
   homepage "https://tracker.debian.org/pkg/liblockfile"
-  url "https://deb.debian.org/debian/pool/main/libl/liblockfile/liblockfile_1.14.orig.tar.gz"
-  sha256 "ab40d4a3e8cbc204f7e87fea637a4e4ddf9a1149aaa0a723a4267febd0b1d060"
+  url "https://deb.debian.org/debian/pool/main/libl/liblockfile/liblockfile_1.16.orig.tar.gz"
+  sha256 "cf6e3828ced3fcc7e5ed36b7dabdf9e0e3ba55a973dab8ed212fb86afc901c69"
 
   bottle do
     sha256 "26b5675b6797bb7d652bedd7795bc8b141e4bc91f21fdd0e143f24cc2f76ef0d" => :mojave
