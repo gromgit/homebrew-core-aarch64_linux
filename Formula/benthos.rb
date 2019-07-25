825224b59ffdc294bad612c16323f348051a6d81
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-25 19:21:28 +0000

    benthos: add 2.9.3 bottle.

--- Formula/benthos.rb
+++ Formula/benthos.rb
@@ -4,6 +4,13 @@ class Benthos < Formula
   url "https://github.com/Jeffail/benthos/archive/v2.9.3.tar.gz"
   sha256 "40c8e53081ce6ea115dad574769f74b0cd7de467ec4f2212747d867bb10e96bc"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4f44ce90e385d63ccdec1e4b1961c873a60723208ea900b8cd073bcab14f2b0b" => :mojave
+    sha256 "1ad68acfcc2bb7a136e6572aeb5f64887cfd3b3f3b2de4e13d75e3247fc542b0" => :high_sierra
+    sha256 "759d7a91908dee5a907f8db37561c2f0abeb4ca1db98c5ae081dfa54775705b2" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
