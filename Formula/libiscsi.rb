f14ad95b9c9e3fd9102e07f76547c4b0f72f767b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-25 02:03:37 +0000

    libiscsi: update 1.19.0 bottle.

--- Formula/libiscsi.rb
+++ Formula/libiscsi.rb
@@ -7,11 +7,9 @@ class Libiscsi < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "8fa9581efc6326e9b980813067cc40b46cd288109f50720260a58e1530604d69" => :mojave
-    sha256 "b7d473398d94e269df98015f2187a791f61eaffcd1cc2dd4f043056e57d1820f" => :high_sierra
-    sha256 "be04eca8be4587455ea7e5f7f696a0d473102093da3ef32c969ddc5f7e3263e7" => :sierra
-    sha256 "7436d0d7cd21c2620f5144cc9137f8dcf8288a31310ec9e5fe951d868b2a889a" => :el_capitan
+    sha256 "473988c2ba81d9d9cf6eb21f2f3d41ade13e76131a2c2aabdade9983c79f99ed" => :mojave
+    sha256 "c05b614ecbacf4f957777c33144924322147b40b898fbb1acf91b72663e35203" => :high_sierra
+    sha256 "832760665cad678de3079365edc72bc21d946dd03ecff9304220b9972a29dd8c" => :sierra
   end
 
   depends_on "autoconf" => :build
