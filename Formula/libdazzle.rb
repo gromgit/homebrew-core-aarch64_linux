652cb58d4d9acba1b1e5373505b94ba595a238c4
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-07-25 07:30:31 +0100

    libdazzle 3.32.3
    Closes #42420.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/libdazzle.rb
+++ Formula/libdazzle.rb
@@ -1,9 +1,8 @@
 class Libdazzle < Formula
   desc "GNOME companion library to GObject and Gtk+"
   homepage "https://gitlab.gnome.org/GNOME/libdazzle"
-  url "https://download.gnome.org/sources/libdazzle/3.32/libdazzle-3.32.2.tar.xz"
-  sha256 "413f8dfb8706760e0c649e2994bd10524ac0736601dd03ad2036293bed3bf141"
-  revision 2
+  url "https://download.gnome.org/sources/libdazzle/3.32/libdazzle-3.32.3.tar.xz"
+  sha256 "6c8d9b1514b5f6422107596f4145b89b8f2a99abef6383e086dfcd28c28667e8"
 
   bottle do
     cellar :any
