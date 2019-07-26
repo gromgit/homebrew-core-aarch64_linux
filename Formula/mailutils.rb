30b190e2f58e1db3e1f5f19a32e2c1dfca46acbc
Caleb Xu <calebcenter@live.com>
2019-07-26 18:30:30 -0400

    mailutils 3.7
    Closes #42504.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/mailutils.rb
+++ Formula/mailutils.rb
@@ -1,9 +1,9 @@
 class Mailutils < Formula
   desc "Swiss Army knife of email handling"
   homepage "https://mailutils.org/"
-  url "https://ftp.gnu.org/gnu/mailutils/mailutils-3.6.tar.gz"
-  mirror "https://ftpmirror.gnu.org/mailutils/mailutils-3.6.tar.gz"
-  sha256 "a0dc30361195594f0bf08a2a2b4906222a36e3ec8936056abe808184ec513ef1"
+  url "https://ftp.gnu.org/gnu/mailutils/mailutils-3.7.tar.gz"
+  mirror "https://ftpmirror.gnu.org/mailutils/mailutils-3.7.tar.gz"
+  sha256 "e95920d23b5ded11479d2aded795e7f8f94e02c88e05198e9093f23b45eb04fa"
 
   bottle do
     sha256 "d1e191fc899b56957fe5b81ead95466cb8332025226ca8c98e23171f45eb6cb0" => :mojave
@@ -16,9 +16,24 @@ class Mailutils < Formula
   depends_on "libtool"
   depends_on "readline"
 
+  # Patch to fix build error:
+  #
+  #   duplicate symbol _status in:
+  #       .libs/dotmail.o
+  #       .libs/message.o
+  #   ld: 2 duplicate symbols for architecture x86_64
+  #
+  # Patch has been accepted upstream, remove on next release
+  patch do
+    url "https://git.savannah.gnu.org/cgit/mailutils.git/patch/?id=b696daa86b51a38841e4c39bce0a46eaac2f1db4"
+    sha256 "919c89d05ae88c33ff715b14fd47733960eb8f633835f3ca6f1867995a51e5a5"
+  end
+
   def install
     system "./configure", "--disable-mh",
                           "--prefix=#{prefix}",
+                          "--without-fribidi",
+                          "--without-gdbm",
                           "--without-guile",
                           "--without-tokyocabinet"
     system "make", "PYTHON_LIBS=-undefined dynamic_lookup", "install"
