d4616b8ca0cb4de990cf2f930243291f2c23fbcb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-26 02:27:51 +0000

    planck: update 2.24.0 bottle.
    Closes #42466.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>
    
    Closes #42465.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>
    
    Closes #42459.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/planck.rb
+++ Formula/planck.rb
@@ -7,9 +7,9 @@ class Planck < Formula
 
   bottle do
     cellar :any
-    sha256 "09cb6e88959cae462136493836a0ebf496a9fc50b2f478b281b3f1fe4767fdf0" => :mojave
-    sha256 "b2eac1a03c217e6276bf5a654203976baf03d2467d51f453180dbb00223e0a13" => :high_sierra
-    sha256 "1a3048a8e8630034be48bfd4ffb5fd5c71bb2cebc9dd2b54ad6e1d79c7fc95ff" => :sierra
+    sha256 "ebc05c0fca4a5c7c709a5cfc0c74fc204879cee084725e0817c8e9fdbcbb37f4" => :mojave
+    sha256 "b1592c991ae76dafbad080bdebd73607c15fd104b6c01094dc685719d3dd71ca" => :high_sierra
+    sha256 "143c12e2d4fab26e682ef5c62fa72074a37f8ba8e0f6b0c177e3aa35083c9fcc" => :sierra
   end
 
   depends_on "clojure" => :build
