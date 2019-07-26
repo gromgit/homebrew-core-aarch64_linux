fb151d935f145cfc3f98723bf82c9744c31943cb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-26 12:55:23 +0000

    desktop-file-utils: update 0.24 bottle.

--- Formula/desktop-file-utils.rb
+++ Formula/desktop-file-utils.rb
@@ -5,9 +5,9 @@ class DesktopFileUtils < Formula
   sha256 "a1de5da60cbdbe91e5c9c10ac9afee6c3deb019e0cee5fdb9a99dddc245f83d9"
 
   bottle do
-    sha256 "c38944b2998cb74506a7cb10e621305db5b37d74d90aa7db51db146ccf378ca3" => :mojave
-    sha256 "f3666dfcad0fda5049ef9b9a7ac85429b0fe5f4c81b12526694c18980b9a3708" => :high_sierra
-    sha256 "67af40d090e09bfe3ce0f83d3f0cb910bd3762851131cc55ef9e434e85a11b11" => :sierra
+    sha256 "2e57d92d8b0385a97a55d6aa32b191a144b329c1b73f85f7465f7c2ab0f2c40f" => :mojave
+    sha256 "12281d3b3e388bb2f9149ec6b229d44b8b1e65a01e5ecfbde482c7ebb9cec6b9" => :high_sierra
+    sha256 "16405a7ca997e90c3448d86f291b8d8793cf413ccd95090f64e05bfbb10cb7e9" => :sierra
   end
 
   depends_on "pkg-config" => :build
