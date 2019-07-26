3b8d92411f7926cac2a36c3480916f1f5d6b7e32
Caleb Xu <calebcenter@live.com>
2019-07-26 00:27:27 -0400

    cfr-decompiler 0.146
    Closes #42471.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/cfr-decompiler.rb
+++ Formula/cfr-decompiler.rb
@@ -1,8 +1,8 @@
 class CfrDecompiler < Formula
   desc "Yet Another Java Decompiler"
   homepage "https://www.benf.org/other/cfr/"
-  url "https://www.benf.org/other/cfr/cfr-0.144.jar"
-  sha256 "29a5d4fd4c70fb9d24f1013725a54a7ee2a61dc672ab2b948602c91cffd90df4"
+  url "https://www.benf.org/other/cfr/cfr-0.146.jar"
+  sha256 "6cc41ece70cb463e0a830fe9d68c22836d9eef5c5f7da1d6e8002c08f5b0f59a"
 
   bottle :unneeded
 
