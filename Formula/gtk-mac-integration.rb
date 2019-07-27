9a3975ddb04c982991ce859118772ad28267aebc
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-07-27 21:14:49 +0100

    gtk-mac-integration: fix test

--- Formula/gtk-mac-integration.rb
+++ Formula/gtk-mac-integration.rb
@@ -63,6 +63,7 @@ class GtkMacIntegration < Formula
     gettext = Formula["gettext"]
     glib = Formula["glib"]
     gtkx = Formula["gtk+"]
+    harfbuzz = Formula["harfbuzz"]
     libpng = Formula["libpng"]
     pango = Formula["pango"]
     pixman = Formula["pixman"]
@@ -77,6 +78,7 @@ class GtkMacIntegration < Formula
       -I#{glib.opt_lib}/glib-2.0/include
       -I#{gtkx.opt_include}/gtk-2.0
       -I#{gtkx.opt_lib}/gtk-2.0/include
+      -I#{harfbuzz.opt_include}/harfbuzz
       -I#{include}/gtkmacintegration
       -I#{libpng.opt_include}/libpng16
       -I#{pango.opt_include}/pango-1.0
