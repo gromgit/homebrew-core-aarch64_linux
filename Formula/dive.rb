6aa0a9b9164cae97b247d505d0c3f1d24daa42b2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-27 20:56:03 +0000

    dive: add 0.7.2 bottle.

--- Formula/dive.rb
+++ Formula/dive.rb
@@ -5,6 +5,13 @@ class Dive < Formula
     :tag      => "v0.7.2",
     :revision => "09296c0214c4cc7477fe53bc79c54805899c6d19"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "47366221b4f7e6ebdc7c89f6dd620e9615154aef174d4e1e55fd93007c991dea" => :mojave
+    sha256 "f2a31f3886bb3ebb8fd834f5712882b3fedcb2fb16e590b0be2b13eda3f3294b" => :high_sierra
+    sha256 "f1fa1a7e1c7082e513977ea1d9974aaa0263fa3abe3b8db5156de71e2287aafb" => :sierra
+  end
+
   depends_on "go" => :build
   depends_on "docker"
 
