4f5a5add4dce289de40df08396419fb2263a4b45
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-07-27 21:28:29 +0100

    vte: fix test

--- Formula/vte.rb
+++ Formula/vte.rb
@@ -53,6 +53,7 @@ class Vte < Formula
     gettext = Formula["gettext"]
     glib = Formula["glib"]
     gtkx = Formula["gtk+"]
+    harfbuzz = Formula["harfbuzz"]
     libpng = Formula["libpng"]
     pango = Formula["pango"]
     pixman = Formula["pixman"]
@@ -68,6 +69,7 @@ class Vte < Formula
       -I#{glib.opt_lib}/glib-2.0/include
       -I#{gtkx.opt_include}/gtk-2.0
       -I#{gtkx.opt_lib}/gtk-2.0/include
+      -I#{harfbuzz.opt_include}/harfbuzz
       -I#{include}/vte-0.0
       -I#{libpng.opt_include}/libpng16
       -I#{pango.opt_include}/pango-1.0
