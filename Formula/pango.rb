27ef882a1aa9837f0924bb4e35e55aa867330e8b
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-07-27 17:54:41 +0100

    pango 1.44.0

--- Formula/pango.rb
+++ Formula/pango.rb
@@ -1,9 +1,8 @@
 class Pango < Formula
   desc "Framework for layout and rendering of i18n text"
   homepage "https://www.pango.org/"
-  url "https://download.gnome.org/sources/pango/1.42/pango-1.42.4.tar.xz"
-  sha256 "1d2b74cd63e8bd41961f2f8d952355aa0f9be6002b52c8aa7699d9f5da597c9d"
-  revision 2
+  url "https://download.gnome.org/sources/pango/1.44/pango-1.44.0.tar.xz"
+  sha256 "004fffebb2ab4f89b375f4720c54b285d569526969ba791dfa20757a7f2f1d1b"
 
   bottle do
     sha256 "00b769ae7c76db06f9828398023c60597b11f33410b9f5b7c3f321b34fb7e0a9" => :mojave
@@ -13,14 +12,11 @@ class Pango < Formula
 
   head do
     url "https://gitlab.gnome.org/GNOME/pango.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "gtk-doc" => :build
-    depends_on "libtool" => :build
   end
 
   depends_on "gobject-introspection" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "cairo"
   depends_on "fontconfig"
@@ -28,18 +24,19 @@ class Pango < Formula
   depends_on "glib"
   depends_on "harfbuzz"
 
-  def install
-    system "./autogen.sh" if build.head?
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}",
-                          "--with-html-dir=#{share}/doc",
-                          "--enable-introspection=yes",
-                          "--enable-static",
-                          "--without-xft"
+  # submitted upstream as https://gitlab.gnome.org/GNOME/pango/merge_requests/105
+  patch :DATA
 
-    system "make"
-    system "make", "install"
+  def install
+    mkdir "build" do
+      system "meson", "--prefix=#{prefix}",
+                      "-Ddefault_library=both",
+                      "-Dintrospection=true",
+                      "-Duse_fontconfig=true",
+                      ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
+    end
   end
 
   test do
@@ -62,6 +59,7 @@ class Pango < Formula
     freetype = Formula["freetype"]
     gettext = Formula["gettext"]
     glib = Formula["glib"]
+    harfbuzz = Formula["harfbuzz"]
     libpng = Formula["libpng"]
     pixman = Formula["pixman"]
     flags = %W[
@@ -71,6 +69,7 @@ class Pango < Formula
       -I#{gettext.opt_include}
       -I#{glib.opt_include}/glib-2.0
       -I#{glib.opt_lib}/glib-2.0/include
+      -I#{harfbuzz.opt_include}/harfbuzz
       -I#{include}/pango-1.0
       -I#{libpng.opt_include}/libpng16
       -I#{pixman.opt_include}/pixman-1
@@ -90,3 +89,19 @@ class Pango < Formula
     system "./test"
   end
 end
+
+__END__
+diff --git a/meson.build b/meson.build
+index 8c41381c..8680e56f 100644
+--- a/meson.build
++++ b/meson.build
+@@ -42,7 +42,8 @@ pango_conf.set('PANGO_VERSION_MICRO', pango_micro_version)
+ # Maintain version scheme with libtool
+ pango_soversion = 0
+ pango_libversion = '@0@.@1@.@2@'.format(pango_soversion, (pango_binary_age - pango_interface_age), pango_interface_age)
+-pango_osxversion = pango_binary_age + 1
++osx_current = pango_binary_age - pango_interface_age + 1
++pango_osxversion = [osx_current, '@0@.@1@.0'.format(osx_current, pango_interface_age)]
+
+ cc = meson.get_compiler('c')
+ host_system = host_machine.system()
