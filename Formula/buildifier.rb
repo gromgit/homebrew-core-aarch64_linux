77eaa306f5cfca77aca8484e1020f6ad70875630
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-27 04:40:49 +0000

    buildifier: update 0.28.0 bottle.

--- Formula/buildifier.rb
+++ Formula/buildifier.rb
@@ -7,9 +7,9 @@ class Buildifier < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "87c05f3dec0b45f6ae609c9646aed2973476f5922c74e4a14de2bb3da2433fc5" => :mojave
-    sha256 "87c05f3dec0b45f6ae609c9646aed2973476f5922c74e4a14de2bb3da2433fc5" => :high_sierra
-    sha256 "58b0a2e0c2396b7557434065c52b911cf2f59bdc5756c1d6ec8a1537cec9002b" => :sierra
+    sha256 "d426e7e57b757a076c9cc6a34da79cb4e9756dec3c7fbed3a1905387c54c8576" => :mojave
+    sha256 "d426e7e57b757a076c9cc6a34da79cb4e9756dec3c7fbed3a1905387c54c8576" => :high_sierra
+    sha256 "cb8a814474d68e6450ba0d6d531438feb127cf3dff21022f8b116ceed657a9cb" => :sierra
   end
 
   depends_on "bazel" => :build
