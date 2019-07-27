8cfae117ba469f1ab5982df550a616445c13db61
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-07-27 21:08:16 +0100

    gdl: fix test

--- Formula/gdl.rb
+++ Formula/gdl.rb
@@ -42,6 +42,7 @@ class Gdl < Formula
     gettext = Formula["gettext"]
     glib = Formula["glib"]
     gtkx3 = Formula["gtk+3"]
+    harfbuzz = Formula["harfbuzz"]
     libepoxy = Formula["libepoxy"]
     libpng = Formula["libpng"]
     pango = Formula["pango"]
@@ -57,6 +58,7 @@ class Gdl < Formula
       -I#{glib.opt_include}/glib-2.0
       -I#{glib.opt_lib}/glib-2.0/include
       -I#{gtkx3.opt_include}/gtk-3.0
+      -I#{harfbuzz.opt_include}/harfbuzz
       -I#{include}/libgdl-3.0
       -I#{libepoxy.opt_include}
       -I#{libpng.opt_include}/libpng16
