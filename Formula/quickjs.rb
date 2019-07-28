ff5e8823d17fe575f400f3889c4a937ca90417c8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-28 21:19:04 +0000

    quickjs: update 2019-07-28 bottle.

--- Formula/quickjs.rb
+++ Formula/quickjs.rb
@@ -5,9 +5,9 @@ class Quickjs < Formula
   sha256 "361090ac7d6e30b532b5bf03addfb8017c802a97c15e0843960cef901a10b250"
 
   bottle do
-    sha256 "291f80952057e91b0e8a9bb449bc9e947e81586979fab12ab64144d9619c18d5" => :mojave
-    sha256 "f64371e9eaf20c2c0cead535c58019bb0c3c647a01cbbe25d8bb9f392630a1ab" => :high_sierra
-    sha256 "a151d816d4799c254fd87ccc8f3576f169bad84298333a98a48d1e429f69a51b" => :sierra
+    sha256 "92b69162ea0ae1838e364799cefc06e118761870f016f1e62df9b3e346057c81" => :mojave
+    sha256 "1ce5a48ad0423dbc53b09d20bae7c57d2dc8a2eee1a30a709185c05b24a1bd69" => :high_sierra
+    sha256 "b843b7a6d945b5295a802ed2e9e0dd5cedfe24947148a428e3f5b5059284f066" => :sierra
   end
 
   def install
