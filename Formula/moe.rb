134fc7fd3a0be716b2de3bdd4109ef1384b902a4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-28 13:44:14 +0000

    moe: update 1.10 bottle.

--- Formula/moe.rb
+++ Formula/moe.rb
@@ -6,11 +6,9 @@ class Moe < Formula
   sha256 "8cfd44ab5623ed4185ee53962b879fd9bdd18eab47bf5dd9bdb8271f1bf7d53b"
 
   bottle do
-    rebuild 1
-    sha256 "b1b91b2a30d506381f31c96f63e75bc78e32bec6051dd2e62d78fa19775975b8" => :mojave
-    sha256 "8795bf38abe1ee929d3573e25bd54bb73220d4d2b30d0f2af4b6242ae5f862f7" => :high_sierra
-    sha256 "c5015c950c74f73093aacac9e9f3b42f741b5bc57e864006c2a62f3c6739c538" => :sierra
-    sha256 "4b7e08c089bcf431efba6be217df5a30e7ade7fa2813ae53152e5671958abb69" => :el_capitan
+    sha256 "934ee30ec5f7f95c74183e5faf6ccc7ac36c426747476a5a0fb9628a6169de04" => :mojave
+    sha256 "fdfffe18871a25a5f0a8cf86ac8682f2cc6623dea335575d39f1dd529ee2ae46" => :high_sierra
+    sha256 "f83a8e961f1a7d295741a6abfe7434580761fa485e32498327ffb0e09322fa1e" => :sierra
   end
 
   def install
