80a2dfc9f91eb20cae72eeb8c008d55b018dff15
Rui Chen <chenrui333@gmail.com>
2019-07-30 15:49:56 -0400

    auditbeat 6.8.2
    Closes #42604.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/auditbeat.rb
+++ Formula/auditbeat.rb
@@ -2,8 +2,8 @@ class Auditbeat < Formula
   desc "Lightweight Shipper for Audit Data"
   homepage "https://www.elastic.co/products/beats/auditbeat"
   url "https://github.com/elastic/beats.git",
-      :tag      => "v6.8.1",
-      :revision => "6e16f47450373f04d6a60db1d23c5b13b37f7431"
+      :tag      => "v6.8.2",
+      :revision => "0ffbeab5a52fa93586e4178becf1252e6a837028"
   head "https://github.com/elastic/beats.git"
 
   bottle do
