974c8ed6cc3fba1e2e74f9d0d1106e4f27c775ff
Caleb Xu <calebcenter@live.com>
2019-07-31 16:12:09 -0400

    imagemagick@6 6.9.10-58
    Closes #42623.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/imagemagick@6.rb
+++ Formula/imagemagick@6.rb
@@ -4,9 +4,9 @@ class ImagemagickAT6 < Formula
   # Please always keep the Homebrew mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://dl.bintray.com/homebrew/mirror/imagemagick%406--6.9.10-56.tar.xz"
-  mirror "https://www.imagemagick.org/download/ImageMagick-6.9.10-56.tar.xz"
-  sha256 "d62bd1c0197581ee29b7e408cd09ceb0546dde6707bd739cd5d267dedf11d91e"
+  url "https://dl.bintray.com/homebrew/mirror/imagemagick%406--6.9.10-58.tar.xz"
+  mirror "https://www.imagemagick.org/download/ImageMagick-6.9.10-58.tar.xz"
+  sha256 "0e1aa29fbf08a8d3eb9d429c609ea657f31b29300fb9a814388f3f4c11202dc3"
   head "https://github.com/imagemagick/imagemagick6.git"
 
   bottle do
