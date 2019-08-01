1dda1e105c3cd6ab77a39f8aaff0c22f8521d7ee
Rui Chen <chenrui333@gmail.com>
2019-08-01 15:40:07 -0400

    tika 1.22
    Closes #42684.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/tika.rb
+++ Formula/tika.rb
@@ -1,16 +1,16 @@
 class Tika < Formula
   desc "Content analysis toolkit"
   homepage "https://tika.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=tika/tika-app-1.21.jar"
-  sha256 "0d5f8174942450ab0eaf9f07b286b8ac2c3f3734e69e5fb72fd5be5be8b4e433"
+  url "https://www.apache.org/dyn/closer.cgi?path=tika/tika-app-1.22.jar"
+  sha256 "d7219709abc547136fa5fca17632a85fe1cd36dc08cb4031957e3c9a836543e2"
 
   bottle :unneeded
 
   depends_on :java => "1.7+"
 
   resource "server" do
-    url "https://www.apache.org/dyn/closer.cgi?path=tika/tika-server-1.21.jar"
-    sha256 "27b2d50470f690c20fab960aa1927973a0db40972e2899f60113e7d2631d349f"
+    url "https://www.apache.org/dyn/closer.cgi?path=tika/tika-server-1.22.jar"
+    sha256 "9913c69e8481edd848f27a7fd912300acd6f0b29044f43fe705a959119e2f44b"
   end
 
   def install
