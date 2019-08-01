ed6b6576ce9ed71b51bf148e9ecde3488252a768
Rui Chen <chenrui333@gmail.com>
2019-08-01 15:40:38 -0400

    newman 4.5.3
    Closes #42685.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/newman.rb
+++ Formula/newman.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Newman < Formula
   desc "Command-line collection runner for Postman"
   homepage "https://www.getpostman.com"
-  url "https://registry.npmjs.org/newman/-/newman-4.5.1.tgz"
-  sha256 "c84563ace0733dc1f62e9e24fe4ae1455332811c17009ff2096201ceb2354273"
+  url "https://registry.npmjs.org/newman/-/newman-4.5.3.tgz"
+  sha256 "e982c7ca6eeb53b9ae6d2922c09a5202f82ebbda228ff2133b2cb74fe722d5c8"
 
   bottle do
     cellar :any_skip_relocation
