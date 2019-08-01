e3b8e5207442abeb8986ed52fead1b85318a7b61
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-01 11:52:00 +0000

    fastjar: update 0.98 bottle.

--- Formula/fastjar.rb
+++ Formula/fastjar.rb
@@ -6,12 +6,9 @@ class Fastjar < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "bd0faa9a7294fe6dcee4e48d6fa987f46b3c713390594251244a280ee20a71b5" => :mojave
-    sha256 "0884c36bf9d1ea07880caa682fbf64076ae86eb732fb151a5ee42e02a76ff272" => :high_sierra
-    sha256 "5d03ecf7d89b4c9cd3ca25735692b77f55ae7df83bdb4073e013f5361256c689" => :sierra
-    sha256 "996937a030b443cee74e1de1945e3199022fc27514cf9925c332ed5d5804c80a" => :el_capitan
-    sha256 "07dd91fef374251b87b1a2987089234a3da225b79313afa4d8a7f502d1a51aae" => :yosemite
-    sha256 "4e1d46c61723a1babbba0841b9dbde5c33388e107202f6ca292adc23ab7149a3" => :mavericks
+    sha256 "2dba61ec801db3d83692b9c9dd26eab247cb4bb6a6d6afc27f059bb6ba6052e5" => :mojave
+    sha256 "87b2c870895191b309b595481f73346e763b87c661e64ef35b821e54395d5cc1" => :high_sierra
+    sha256 "35230e788987e3a3c63d126af24c634bcbf58c0a320223d61f0eae69f6cbcc00" => :sierra
   end
 
   def install
