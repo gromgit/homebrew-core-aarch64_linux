734529755a2a4ab1c6d0044fb35efbad943ab8f6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-01 12:19:37 +0000

    vifm: update 0.10.1 bottle.

--- Formula/vifm.rb
+++ Formula/vifm.rb
@@ -5,9 +5,9 @@ class Vifm < Formula
   sha256 "99006f56db05e1bdfb7983e8d5c31c9ac93bf2ac9e0409a577c8ca660fecd03b"
 
   bottle do
-    sha256 "ecd4ded0553705dff6ce635bfd424671e9ce8e4c7aebb279fc4040763041e98b" => :mojave
-    sha256 "26afd08c530568c64968da378e4913e169fc478d427ebc70f1aba43dc89cc39b" => :high_sierra
-    sha256 "ebeea80fbbce5f75d6541c1ee2b1bad9d29076a84ead9b6b9d019b259d7c3566" => :sierra
+    sha256 "0495acdf812f89f23c7fafcccef47589249477c5c38dd0e47049c3421edc712a" => :mojave
+    sha256 "8371dea589590796e0f1c6b7b5ce263e18b9a1619551d5967deefd5035e512e0" => :high_sierra
+    sha256 "ded145b46f48c79712b12e7f8d14ab8b219778fcd6d0b436d0c156dd0b15129d" => :sierra
   end
 
   def install
