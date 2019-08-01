dac1f22f2aeb40949219cc52c54d1e18048c48a2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-01 12:28:57 +0000

    vulkan-headers: update 1.1.117 bottle.

--- Formula/vulkan-headers.rb
+++ Formula/vulkan-headers.rb
@@ -6,9 +6,9 @@ class VulkanHeaders < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "659b081c02ab41a7aa86f34af849c9201fc180131c18fa0d5dcf955ae4de035d" => :mojave
-    sha256 "659b081c02ab41a7aa86f34af849c9201fc180131c18fa0d5dcf955ae4de035d" => :high_sierra
-    sha256 "3a5206e0bece256e38512cee4070ba0fe1cc307812d5fcf852773be52dd983c0" => :sierra
+    sha256 "9e9816dfc20a5981323c37357614083a636584d69fe4a39a8c95f8e0bd064040" => :mojave
+    sha256 "9e9816dfc20a5981323c37357614083a636584d69fe4a39a8c95f8e0bd064040" => :high_sierra
+    sha256 "c53dbf2e014b68cf69d108c7d0e2b6adefb64147bebc205665d8c76a68b00542" => :sierra
   end
 
   depends_on "cmake" => :build
