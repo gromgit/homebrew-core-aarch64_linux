5cc73a973c79d6d5761b84bb1b92677c69f57c79
Roger A. Light <roger@atchoo.org>
2019-08-01 21:28:21 +0100

    mosquitto 1.6.4
    Closes #42692.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/mosquitto.rb
+++ Formula/mosquitto.rb
@@ -1,8 +1,8 @@
 class Mosquitto < Formula
   desc "Message broker implementing the MQTT protocol"
   homepage "https://mosquitto.org/"
-  url "https://mosquitto.org/files/source/mosquitto-1.6.3.tar.gz"
-  sha256 "9ef5cc75f4fe31d7bf50654ddf4728ad9e1ae2e5609a4b42ecbbcb4a209ed17e"
+  url "https://mosquitto.org/files/source/mosquitto-1.6.4.tar.gz"
+  sha256 "a3d5822c249f6a6e13311b1b09eff6807ea01608a5a77934e1769842e9d146ef"
 
   bottle do
     cellar :any
