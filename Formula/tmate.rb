fa5c2b8264a04aceeb5cb253f4e60fd9378565ef
Caleb Xu <calebcenter@live.com>
2019-08-02 11:20:20 -0400

    tmate 2.3.0

--- Formula/tmate.rb
+++ Formula/tmate.rb
@@ -1,9 +1,8 @@
 class Tmate < Formula
   desc "Instant terminal sharing"
   homepage "https://tmate.io/"
-  url "https://github.com/tmate-io/tmate/archive/2.2.1.tar.gz"
-  sha256 "d9c2ac59f42e65aac5f500f0548ea8056fd79c9c5285e5af324d833e2a84c305"
-  revision 4
+  url "https://github.com/tmate-io/tmate/archive/2.3.0.tar.gz"
+  sha256 "f837514edbc19180d06b27713628466e68aba91654d748d6e65bfad308f5b00a"
   head "https://github.com/tmate-io/tmate.git"
 
   bottle do
