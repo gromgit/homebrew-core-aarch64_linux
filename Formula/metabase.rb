c9f348993918281b1feab396e4652ef36e7952b0
Caleb Xu <calebcenter@live.com>
2019-08-02 01:19:07 -0400

    metabase 0.32.10
    Closes #42708.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/metabase.rb
+++ Formula/metabase.rb
@@ -1,8 +1,8 @@
 class Metabase < Formula
   desc "Business intelligence report server"
   homepage "https://www.metabase.com/"
-  url "https://downloads.metabase.com/v0.32.9/metabase.jar"
-  sha256 "80fb606f5a97913e7daa7fcef26a0fcc388f486ab324ed80adb15e51c35a3e22"
+  url "https://downloads.metabase.com/v0.32.10/metabase.jar"
+  sha256 "360b97a4d9c948388e054d2e1fe5f309f527bf174073bc3a20f9a6a837e7fc37"
 
   head do
     url "https://github.com/metabase/metabase.git"
