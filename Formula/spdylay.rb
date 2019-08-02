3051f84498f675f03402fef683a9df06f657337d
Caleb Xu <calebcenter@live.com>
2019-08-02 11:05:58 -0400

    spdylay: bump revision for libevent update

--- Formula/spdylay.rb
+++ Formula/spdylay.rb
@@ -3,7 +3,7 @@ class Spdylay < Formula
   homepage "https://github.com/tatsuhiro-t/spdylay"
   url "https://github.com/tatsuhiro-t/spdylay/archive/v1.4.0.tar.gz"
   sha256 "31ed26253943b9d898b936945a1c68c48c3e0974b146cef7382320a97d8f0fa0"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
