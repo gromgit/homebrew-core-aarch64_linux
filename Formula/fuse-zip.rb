f8299330e34cbe0a34d013810531ccb1e3717856
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-02 04:46:44 +0000

    fuse-zip: update 0.6.1 bottle.

--- Formula/fuse-zip.rb
+++ Formula/fuse-zip.rb
@@ -7,9 +7,9 @@ class FuseZip < Formula
 
   bottle do
     cellar :any
-    sha256 "2323a2a375f62f388cd23c117c69d8d5583f004f9b12bcb41b5951c28f06643b" => :mojave
-    sha256 "9f3f6df480954b829b58f755c8bae180923c8118a94e6b9a28650d7b2c31af69" => :high_sierra
-    sha256 "f6edb889c97544c4f145f1e682e97fb4dace8b97baa60a21df6708c7f1c6ca12" => :sierra
+    sha256 "86b8df314e9dabf3e5fe0df1d9d647204799696a9f2f0d9199f94a6536fcaf01" => :mojave
+    sha256 "58e531f3bc48fa5d1da367a57dda87fc04538d9f7c8447d88b1d8cc04ab7e74f" => :high_sierra
+    sha256 "88b1d9e1c807232718036fbf619df6ddf7e6626ab064db4599d48142410458df" => :sierra
   end
 
   depends_on "pkg-config" => :build
