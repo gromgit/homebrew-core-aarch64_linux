50b71432bcf2385f7cba0157a57064bcd5e0f464
Caleb Xu <calebcenter@live.com>
2019-08-02 00:25:51 -0400

    checkbashisms 2.19.6
    Closes #42705.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/checkbashisms.rb
+++ Formula/checkbashisms.rb
@@ -1,8 +1,8 @@
 class Checkbashisms < Formula
   desc "Checks for bashisms in shell scripts"
   homepage "https://launchpad.net/ubuntu/+source/devscripts/"
-  url "https://deb.debian.org/debian/pool/main/d/devscripts/devscripts_2.19.5.tar.xz"
-  sha256 "3b79ca7c7225c544bc5bf4d4b28519686d9413fc630cb844e2f94c919e4cedc4"
+  url "https://deb.debian.org/debian/pool/main/d/devscripts/devscripts_2.19.6.tar.xz"
+  sha256 "3196092bd274db9aa1e34e68a0c94c6edb49a481dded1656fd9785831bfdcec4"
 
   bottle :unneeded
 
