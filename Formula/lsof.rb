092381b0f572bebc43a7d10dfd95787eeb33d5b2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-02 04:53:35 +0000

    lsof: update 4.93.2 bottle.

--- Formula/lsof.rb
+++ Formula/lsof.rb
@@ -6,10 +6,9 @@ class Lsof < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "830be0b158cbb5ff2f6bf019a517e6c89ff013760a6297fe8e285e66322b8e1a" => :mojave
-    sha256 "276399b5b42f5a9dc167515add55c3bb2e545faec1299516b06793b1581a9311" => :high_sierra
-    sha256 "9d57eea962770c2f3c5b56d26d9fdb9b0bbdde559dfc92d0c9db3dcaae33cc43" => :sierra
-    sha256 "59c8c1a9455e3f10c8d6326bab49ab33a85c3cfa702627aa4ba2dd9600cc7d72" => :el_capitan
+    sha256 "3532e9650fc5b836d5584cd8733f83218229d170859bf2c85e62b4abad08d356" => :mojave
+    sha256 "8ebe4f68ada3d1c1984bbfb660437984f5fc9d61c93b3da8024bfd0d797a2172" => :high_sierra
+    sha256 "e07f28fd45b1eae5231393b45360920da4ad4386a83ccb6a256fc7ea26509f59" => :sierra
   end
 
   keg_only :provided_by_macos
