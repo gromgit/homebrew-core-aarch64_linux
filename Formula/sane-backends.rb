d212d605ef30c5e978ceaec403b5325179239748
Caleb Xu <calebcenter@live.com>
2019-08-01 23:44:01 -0400

    sane-backends 1.0.28
    Closes #42703.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/sane-backends.rb
+++ Formula/sane-backends.rb
@@ -1,11 +1,9 @@
 class SaneBackends < Formula
   desc "Backends for scanner access"
   homepage "http://www.sane-project.org/"
-  url "https://deb.debian.org/debian/pool/main/s/sane-backends/sane-backends_1.0.27.orig.tar.gz"
-  mirror "https://fossies.org/linux/misc/sane-backends-1.0.27.tar.gz"
-  sha256 "293747bf37275c424ebb2c833f8588601a60b2f9653945d5a3194875355e36c9"
-  revision 5
-  head "https://salsa.debian.org/debian/sane-backends.git"
+  url "https://gitlab.com/sane-project/backends/uploads/9e718daff347826f4cfe21126c8d5091/sane-backends-1.0.28.tar.gz"
+  sha256 "31260f3f72d82ac1661c62c5a4468410b89fb2b4a811dabbfcc0350c1346de03"
+  head "https://gitlab.com/sane-project/backends.git"
 
   bottle do
     sha256 "19a5dd6aab043b2552e4ddb785c4f41c184019a7854e5bf28054ee809839a81f" => :mojave
@@ -23,18 +21,16 @@ class SaneBackends < Formula
   depends_on "openssl"
 
   def install
+    # malloc lives in malloc/malloc.h instead of just malloc.h on macOS.
+    # Merge request opened upstream: https://gitlab.com/sane-project/backends/merge_requests/90
+    inreplace "backend/ricoh2_buffer.c", "#include <malloc.h>", "#include <malloc/malloc.h>"
+
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--localstatedir=#{var}",
                           "--without-gphoto2",
                           "--enable-local-backends",
                           "--with-usb=yes"
-
-    # Remove for > 1.0.27
-    # Workaround for bug in Makefile.am described here:
-    # https://lists.alioth.debian.org/pipermail/sane-devel/2017-August/035576.html
-    # It's already fixed in commit 519ff57.
-    system "make"
     system "make", "install"
   end
 
