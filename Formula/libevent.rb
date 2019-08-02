675cb08b914c5f3115e04f401e61e792e89c0860
Caleb Xu <calebcenter@live.com>
2019-08-02 08:05:36 -0400

    libevent 2.1.11

--- Formula/libevent.rb
+++ Formula/libevent.rb
@@ -1,8 +1,8 @@
 class Libevent < Formula
   desc "Asynchronous event library"
   homepage "https://libevent.org/"
-  url "https://github.com/libevent/libevent/archive/release-2.1.10-stable.tar.gz"
-  sha256 "52c9db0bc5b148f146192aa517db0762b2a5b3060ccc63b2c470982ec72b9a79"
+  url "https://github.com/libevent/libevent/archive/release-2.1.11-stable.tar.gz"
+  sha256 "229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e"
 
   bottle do
     cellar :any
