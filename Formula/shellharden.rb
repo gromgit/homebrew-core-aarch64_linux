fd74c952baeb6863acdb0e10b6e69929685422c2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-04 15:49:19 +0000

    shellharden: update 4.1.1 bottle.

--- Formula/shellharden.rb
+++ Formula/shellharden.rb
@@ -5,10 +5,10 @@ class Shellharden < Formula
   sha256 "7d7ac3443f35eb74abfc78fa67db2947d60b7d0782f225f55d6eefafcf294c7c"
 
   bottle do
-    sha256 "23a4338547c6cb9b3e4e8d454cb8e1420c5a38f0440b9dde0f95384656ef87ca" => :mojave
-    sha256 "1dc1515f934b43e17b4faeb17cda61a22a28866e625d863c7372eda6a2e111d3" => :high_sierra
-    sha256 "2fdb7e3d8fdeab4089143e5d11f1b5b379f25b11623af5497cd54e829ccd1b85" => :sierra
-    sha256 "d1b2430ab2de01134b5a0b4435fb7280bed7f140e662d7e2ccd4764a5be6e737" => :el_capitan
+    cellar :any_skip_relocation
+    sha256 "edca629536f1b38bfb473dd98b2259726dba51fc448ea4e59e7a15359bc99fc3" => :mojave
+    sha256 "41e43c80d63fe7a33d3d9f06741db902c0ab6fc26ed9bc5037f30295d43c8360" => :high_sierra
+    sha256 "78a7c2a75348c1746c0d658b6e1070a4277934d534137642caf84871ac467596" => :sierra
   end
 
   depends_on "rust" => :build
