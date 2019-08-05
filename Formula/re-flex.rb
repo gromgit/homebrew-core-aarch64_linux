b78cc189d2f1690534ae2feca1751873285ef667
Caleb Xu <calebcenter@live.com>
2019-08-05 17:26:22 -0400

    re-flex 1.3.2
    Closes #42825.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/re-flex.rb
+++ Formula/re-flex.rb
@@ -1,8 +1,8 @@
 class ReFlex < Formula
   desc "The regex-centric, fast and flexible scanner generator for C++"
   homepage "https://www.genivia.com/doc/reflex/html"
-  url "https://github.com/Genivia/RE-flex/archive/v1.3.0.tar.gz"
-  sha256 "b3ccf23cfb2e167927258c4ab58ee3799c4cf5892b8696232f9c6e73a6c235b9"
+  url "https://github.com/Genivia/RE-flex/archive/v1.3.2.tar.gz"
+  sha256 "aaad0c9b7c7907567c3ccf4fd0abcc9a8e5f8d1caa653557c36997443ce3189f"
 
   bottle do
     cellar :any_skip_relocation
@@ -10,8 +10,6 @@ class ReFlex < Formula
     sha256 "70006a21a1d1f2192be7792a251a49669bd805235641b1c20e7ef16556b227a9" => :high_sierra
     sha256 "eccc6b41c77c6951b489df54456cfef3c52f50efab0213820e8e8e8637d3bd5e" => :sierra
   end
-
-  depends_on "boost"
 
   def install
     system "./configure", "--disable-dependency-tracking",
