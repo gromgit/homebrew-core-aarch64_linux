f4fb20de8b2b0ddd6d78460e2761afac53f389e5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-05 16:12:20 +0000

    gtksourceviewmm: update 2.10.3_5 bottle.

--- Formula/gtksourceviewmm.rb
+++ Formula/gtksourceviewmm.rb
@@ -7,9 +7,10 @@ class Gtksourceviewmm < Formula
 
   bottle do
     cellar :any
-    sha256 "ba0e44c276ecd0bf7770d6a48d67f9f40d445217f3136b14b7864bb8bf93f7ef" => :mojave
-    sha256 "6c241900e5eb52e186de31869c898504f923c8199ad1c63becc2b76e6a083b42" => :high_sierra
-    sha256 "3b6f901da7ee95a6f4b66d5bc2ec14ad53f96b303b4c73b80b23e2cb6e0e6c8f" => :sierra
+    rebuild 1
+    sha256 "c82cd52d2db768eb50d2b452c995f717047effb700a063fce8d7a4ad9ea29ac9" => :mojave
+    sha256 "95271d7bb254090c70e8b81638be9346d19ddab4b6c11195596d6e107d97db37" => :high_sierra
+    sha256 "935a6ee355739c40b24dc93f8db22e19e6e94af0e85bdc481f16c7db08239291" => :sierra
   end
 
   depends_on "pkg-config" => :build
