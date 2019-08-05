f1538d78b9bdc9bdab62e687658ecda637e09e62
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-05 17:35:07 +0000

    scrcpy: update 1.10 bottle.

--- Formula/scrcpy.rb
+++ Formula/scrcpy.rb
@@ -5,9 +5,9 @@ class Scrcpy < Formula
   sha256 "71bd3b01f26233d73b91c0953ce004bd3195bcfd0c83b76c269094fb06e5ffa5"
 
   bottle do
-    sha256 "6b3c73d844f2e03884494b5ef35ae61e954d61deaabee3dc2c6890ea59f2c8d9" => :mojave
-    sha256 "3ede1d5379b523fa49bafded248216359461d5f469f31f487317dd6b975fe2d4" => :high_sierra
-    sha256 "894321121f744ff2e39557ebd913a6ba208f6f61eeb3578e8e61854091b63992" => :sierra
+    sha256 "3424b91b362905cb2140fde701a4a89107d9ea65919d5b9b04f22b136400fe5c" => :mojave
+    sha256 "5a82192ee21b66b11f726db20857f386ba75ece9fae6796363ff02f604a53dd6" => :high_sierra
+    sha256 "ddf29ffdf4e935f86de8a2ee5ff2c434a1472414624f48cf71aefc62557e0b03" => :sierra
   end
 
   depends_on "meson" => :build
