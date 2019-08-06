e4f25c9e5f95ea335fbb2e674f0f541f92e59247
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-06 21:15:51 +0000

    lerna: add 3.16.4 bottle.

--- Formula/lerna.rb
+++ Formula/lerna.rb
@@ -6,6 +6,13 @@ class Lerna < Formula
   url "https://registry.npmjs.org/lerna/-/lerna-3.16.4.tgz"
   sha256 "dcaee23bbed63e3c278a934394a2cbef3ff896d12cc10ffae67f38960ce7d979"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ad4d3587344dadc5a41894350cd78ef87e32b6eeaf15e19fa259230acdb48029" => :mojave
+    sha256 "ebebca0bb59b1f6fb0c051cb900ed84490fd382f1ebe83fabc11164084959298" => :high_sierra
+    sha256 "e5fb1d244c72686e2b4744723f4b955def887ca069449a71935d0ebbbfbe4256" => :sierra
+  end
+
   depends_on "node"
 
   def install
