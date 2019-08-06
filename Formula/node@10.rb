406c466713d7d5eed3571314d6336b31d5584a8d
Rui Chen <rchen@meetup.com>
2019-08-06 18:51:34 -0400

    node@10 10.16.2
    Closes #42877.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/node@10.rb
+++ Formula/node@10.rb
@@ -1,8 +1,8 @@
 class NodeAT10 < Formula
   desc "Platform built on V8 to build network applications"
   homepage "https://nodejs.org/"
-  url "https://nodejs.org/dist/v10.16.1/node-v10.16.1.tar.gz"
-  sha256 "98c92edcfced73b572917d01a53aa9deefec85d8a2fe96c46fe10ee1d0a7763d"
+  url "https://nodejs.org/dist/v10.16.2/node-v10.16.2.tar.gz"
+  sha256 "5936ef12ab3f0ce5fbb6751c1bb41f626b6058f414a297b3d8c5eb418a27e8fe"
 
   bottle do
     cellar :any
