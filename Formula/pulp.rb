109c322a6461572063e8b77b00148092a9c85787
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-06 22:36:59 +0000

    pulp: add 13.0.0 bottle.

--- Formula/pulp.rb
+++ Formula/pulp.rb
@@ -6,6 +6,13 @@ class Pulp < Formula
   url "https://registry.npmjs.org/pulp/-/pulp-13.0.0.tgz"
   sha256 "91b5e517afffc8f53ed6dc608eef908bd92843ce73976acf57db6eca897b5ba0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "31f201012bb920b2716f47a48bbf6a762e88c7ba5587e1bcf206b0420c12324a" => :mojave
+    sha256 "14503036a125c1bda8fb419d7b35ce7a9de7314b74893f6726c4cd726f611b0a" => :high_sierra
+    sha256 "f565ef1a1cd9235139f52d968b09c1d5dc17dddf015efe7d9b68cf9f644b0135" => :sierra
+  end
+
   depends_on "bower"
   depends_on "node"
   depends_on "purescript"
