fc26035c2605e9ff7fb80f4c012c5a5fe5d1c271
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-08 03:44:58 +0000

    libbitcoin-consensus: update 3.6.0 bottle.

--- Formula/libbitcoin-consensus.rb
+++ Formula/libbitcoin-consensus.rb
@@ -6,10 +6,9 @@ class LibbitcoinConsensus < Formula
 
   bottle do
     cellar :any
-    sha256 "3e84a8b81167e6cbf98cbba5fcd6f40741758704e9acec422b7399604449b74b" => :mojave
-    sha256 "36e607d57dcf9347cedede744be5461c8c9e866047d4b31942987110153c9bfe" => :high_sierra
-    sha256 "0ed7771f106cd05c2413b9da72013f168077dc8f6a21d5a01806c715d038e680" => :sierra
-    sha256 "1306b0c9f124e71aeaa22825c0d0f13c49d747ad762ea1fc6a8ea0853d47c4c7" => :el_capitan
+    sha256 "46a04baae04ffd5ac737a43003396ca2cce533afa18a47b038938fb1a3c782d5" => :mojave
+    sha256 "a96ea9a7a7761ec95d98c513ec7acfcbc5638e86b7d483204446be711cbdff2c" => :high_sierra
+    sha256 "d1c377534d2bec37652a62790e44e443cc3a72ef85b822679e07939fe2a51982" => :sierra
   end
 
   depends_on "autoconf" => :build
