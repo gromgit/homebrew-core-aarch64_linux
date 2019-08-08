8618309beb3d742942ed7c124ec841a5b40bbb8b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-08 18:59:31 +0000

    topgit: update 0.19.12 bottle.

--- Formula/topgit.rb
+++ Formula/topgit.rb
@@ -6,12 +6,9 @@ class Topgit < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a33a1cd27a30392535a263631bf395b818abeb887a059ff1bb58bdf45ad4580b" => :mojave
-    sha256 "bf554bc1d3a6afc87245deebbff89577c8e86e664681333956124da696b54629" => :high_sierra
-    sha256 "5c1167b5d2f75b7b09d4b43ece472e2ae541d50ef2202afe08c14dbd8368ba06" => :sierra
-    sha256 "a70a472becf5cdf957519831ff3365a7113b35c4d962f4159d23d89f7e5acc47" => :el_capitan
-    sha256 "6ace8c6f3df714648de5cfc30ccb2066e589d9038fbf67796eeb4b76c7426989" => :yosemite
-    sha256 "d682d50b1fda7c88b519153806d17886a46ef70840fc00d37a42e5b942ed50e7" => :mavericks
+    sha256 "30c348bcfbdcfc5fe3a91b0bb8889841a5e492f2fed7626577cda1523d815dc2" => :mojave
+    sha256 "30c348bcfbdcfc5fe3a91b0bb8889841a5e492f2fed7626577cda1523d815dc2" => :high_sierra
+    sha256 "ec7f9140e122265f34c03469803cf7eb932006d240ab158cb9ee5a27f53b3b38" => :sierra
   end
 
   def install
