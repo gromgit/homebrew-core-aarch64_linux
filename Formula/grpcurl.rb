e2b2ab06f9942240523eb1dbfff8069bc0832406
Rui Chen <rchen@meetup.com>
2019-08-09 17:13:56 -0400

    grpcurl 1.3.2
    Closes #42982.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/grpcurl.rb
+++ Formula/grpcurl.rb
@@ -1,8 +1,8 @@
 class Grpcurl < Formula
   desc "Like cURL, but for gRPC"
   homepage "https://github.com/fullstorydev/grpcurl"
-  url "https://github.com/fullstorydev/grpcurl/archive/v1.3.1.tar.gz"
-  sha256 "b856057b7e68e4867ebce18058205554c6317a822eee347a40757d312754311c"
+  url "https://github.com/fullstorydev/grpcurl/archive/v1.3.2.tar.gz"
+  sha256 "3b0b9efc187d86658d241642325fa0dbc3e408ec33ff375d6bd4adf373fa1a9a"
 
   bottle do
     cellar :any_skip_relocation
