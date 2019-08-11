ede9f13b20e04d2fe06645ec3ff1aa3b96b8d6d2
Rui Chen <chenrui333@gmail.com>
2019-08-11 00:39:41 -0400

    bitwise 0.40
    Closes #43008.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/bitwise.rb
+++ Formula/bitwise.rb
@@ -1,8 +1,8 @@
 class Bitwise < Formula
   desc "Terminal based bit manipulator in ncurses"
   homepage "https://github.com/mellowcandle/bitwise"
-  url "https://github.com/mellowcandle/bitwise/releases/download/v0.33/bitwise-v0.33.tar.gz"
-  sha256 "598a623d9befaf1e724d78406a93140c1fc05ba9526f11bee945d236a92572c8"
+  url "https://github.com/mellowcandle/bitwise/releases/download/v0.40/bitwise-v0.40.tar.gz"
+  sha256 "d74585153d8ae605fd9bb23d2a4b57ed5df283f902d376d0d31ade95423afa03"
 
   bottle do
     cellar :any
