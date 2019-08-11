0c154f8fdb39de2b2eac5dbd6d3104403436e9aa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-11 04:47:53 +0000

    bitwise: update 0.40 bottle.

--- Formula/bitwise.rb
+++ Formula/bitwise.rb
@@ -6,9 +6,9 @@ class Bitwise < Formula
 
   bottle do
     cellar :any
-    sha256 "ca1fe56e701f249f78cf606a418257c2e5ef39f7de2c6a4b8426b3dfc5a7ef5e" => :mojave
-    sha256 "24c4effd91b4f596576ebb8f92966195233f353fd5dd2daf4f774b8e7eeccf50" => :high_sierra
-    sha256 "787986ce6174a4a564ef8b63c2535a84c674fd0f6bcd2863f00eb3e9019d3ee2" => :sierra
+    sha256 "c78b42bffab47e12d4c98d97c47e14e9809e08fce3921cef84ae7f038fb1159a" => :mojave
+    sha256 "0a3bd971b0473b8f863158df2f7176eb90b1da390840e43433f660bd62e006da" => :high_sierra
+    sha256 "5b63274d741baf311a5e7409d42e5e63854ac5e5852c12e5ee08ba98572393d1" => :sierra
   end
 
   depends_on "autoconf" => :build
