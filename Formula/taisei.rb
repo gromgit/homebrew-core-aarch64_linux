bc39c50332c7c8ac6a8fd69f93dc52ee9143ed5c
EricFromCanada <enk3@outlook.com>
2019-08-12 11:18:06 -0400

    taisei 1.3
    Closes #43047.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/taisei.rb
+++ Formula/taisei.rb
@@ -1,10 +1,9 @@
 class Taisei < Formula
-  desc "Clone of Touhou Project shoot-em-up games"
+  desc "Clone of Tōhō Project shoot-em-up games"
   homepage "https://taisei-project.org/"
   url "https://github.com/taisei-project/taisei.git",
-      :tag      => "v1.2",
-      :revision => "46fb0f894ad269528ac7fda533c7994eddd9b758"
-  revision 1
+      :tag      => "v1.3",
+      :revision => "f8ef67224f47e85f4095e32736dc21e0d46ae5b7"
 
   bottle do
     sha256 "5a63d77e6ba51f8907f1d6a08ca6fc3c94b7f4317afd746c91f0c8011d767c20" => :mojave
@@ -20,18 +19,12 @@ class Taisei < Formula
   depends_on "libzip" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
+  depends_on "openssl@1.1" => :build
   depends_on "pkg-config" => :build
   depends_on "python" => :build
   depends_on "sdl2" => :build
   depends_on "sdl2_mixer" => :build
   depends_on "sdl2_ttf" => :build
-
-  # Fixes a bug in the .app bundle build script.
-  # # Will be in the next release.
-  patch do
-    url "https://github.com/taisei-project/taisei/commit/68b0d4f5c6f2015704e1ed1b4098be1c4336db74.patch?full_index=1"
-    sha256 "cb1f79826e632a61daa271cb59d0a80ab77dea876d384c381ab66d5eb9b9bd27"
-  end
 
   def install
     mkdir "build" do
@@ -47,6 +40,6 @@ class Taisei < Formula
 
   test do
     output = shell_output("#{prefix}/Taisei.app/Contents/MacOS/Taisei -h", 1)
-    assert_match "Touhou clone", output
+    assert_match "Tōhō Project", output
   end
 end
