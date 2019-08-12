d0c1fd3ce45b7bf95f5818fba8a36ab84304906f
Vasyl Khomko <vasyl@crowdin.com>
2019-08-12 12:34:11 +0300

    crowdin 2.0.30
    Closes #43038.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/crowdin.rb
+++ Formula/crowdin.rb
@@ -1,8 +1,8 @@
 class Crowdin < Formula
   desc "Command-line tool that allows to manage your resources with crowdin.com"
   homepage "https://support.crowdin.com/cli-tool/"
-  url "https://downloads.crowdin.com/cli/v2/crowdin-cli-2.0.29.zip"
-  sha256 "5fa69facda79a5c9f70018c86de5467edd2fcaf0154da92d59d4f63003cc7af0"
+  url "https://downloads.crowdin.com/cli/v2/crowdin-cli-2.0.30.zip"
+  sha256 "7066ce25339107c00b82918ef0c3b8d4913e3dedbafa677b2b50f12c45f6d4f8"
 
   bottle :unneeded
 
@@ -17,7 +17,7 @@ class Crowdin < Formula
     generate_output = shell_output("#{bin}/crowdin generate").chomp
     assert_predicate testpath/"crowdin.yml", :exist?
     assert_match /^Generates Crowdin CLI configuration skeleton .*crowdin\.yml\'- OK$/, generate_output
-    lint_output = shell_output("#{bin}/crowdin lint").split("\n")
+    lint_output = shell_output("#{bin}/crowdin lint", 1).split("\n")
     lint_output.each do |line|
       assert_match /^Project [^ ]+ is empty$/, line
     end
