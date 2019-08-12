f407ea0277d01bc83affd7a3331ad51aa43c13d8
Viktor Szakats <commit@vszakats.net>
2019-08-12 14:44:35 +0000

    unittest: secure/fix url(s)

--- Formula/unittest.rb
+++ Formula/unittest.rb
@@ -1,7 +1,7 @@
 class Unittest < Formula
   desc "C++ Unit Test Framework"
-  homepage "http://unittest.red-bean.com/"
-  url "http://unittest.red-bean.com/tar/unittest-0.50-62.tar.gz"
+  homepage "https://unittest.red-bean.com/"
+  url "https://unittest.red-bean.com/tar/unittest-0.50-62.tar.gz"
   sha256 "9586ef0149b6376da9b5f95a992c7ad1546254381808cddad1f03768974b165f"
 
   bottle do
