6b3285f9e7dcdc07f5a56f2605d78fef0f88e9ac
Nick Golder <nrg@nirgo.net>
2019-08-13 07:48:00 -0600

    git-annex 7.20190730
    Closes #43068.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/git-annex.rb
+++ Formula/git-annex.rb
@@ -5,8 +5,8 @@ class GitAnnex < Formula
 
   desc "Manage files with git without checking in file contents"
   homepage "https://git-annex.branchable.com/"
-  url "https://hackage.haskell.org/package/git-annex-7.20190322/git-annex-7.20190322.tar.gz"
-  sha256 "ddda8ce4c23d76ab77610a0116ab68a9adb2e9a7087db99577e6b7743f1a9ffa"
+  url "https://hackage.haskell.org/package/git-annex-7.20190730/git-annex-7.20190730.tar.gz"
+  sha256 "4feafd05ccd815da2d9cc8da1e0e05ae5b8b0b8af96bcf6b7b2fe1539b50be9a"
   head "git://git-annex.branchable.com/"
 
   bottle do
@@ -17,7 +17,7 @@ class GitAnnex < Formula
   end
 
   depends_on "cabal-install" => :build
-  depends_on "ghc@8.2" => :build
+  depends_on "ghc" => :build
   depends_on "pkg-config" => :build
   depends_on "gsasl"
   depends_on "libmagic"
@@ -29,8 +29,8 @@ class GitAnnex < Formula
     # This is already resolved in aws 0.20 but we can't move to 0.20 until
     # esqueleto 2.6.0 ships. See https://github.com/bitemyapp/esqueleto/issues/88
     # The network 2.7.0.1 issue has been fixed upstream but needs a new release.
-    install_cabal_package "--constraint", "http-conduit<2.3",
-                          "--constraint", "network<2.7.0.1",
+    install_cabal_package "--constraint", "http-conduit>=2.3",
+                          "--constraint", "network>=2.6.3.0",
                           :using => ["alex", "happy", "c2hs"],
                           :flags => ["s3", "webapp"]
     bin.install_symlink "git-annex" => "git-annex-shell"
@@ -70,7 +70,7 @@ class GitAnnex < Formula
     system "git", "annex", "init"
     (testpath/"Hello.txt").write "Hello!"
     assert !File.symlink?("Hello.txt")
-    assert_match "add Hello.txt ok", shell_output("git annex add .")
+    assert_match /^add Hello.txt.*ok.*\(recording state in git\.\.\.\)/m, shell_output("git annex add .")
     system "git", "commit", "-a", "-m", "Initial Commit"
     assert File.symlink?("Hello.txt")
 
