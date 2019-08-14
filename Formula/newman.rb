d69416557529003cd9eba577185ceec26d6749f9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-14 17:12:49 +0000

    newman: update 4.5.4 bottle.

--- Formula/newman.rb
+++ Formula/newman.rb
@@ -8,9 +8,9 @@ class Newman < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b70f201ca959626adc794615ee104c7e9ba40500ed312b49f0259d6d24eac325" => :mojave
-    sha256 "8927b26ecc8b246e93a239c61317827809f13cc43505304d40c7b44fe76ee355" => :high_sierra
-    sha256 "43fa03a48d0bef489f5822271329cef211331f4b003b001ef9702e108008b5fd" => :sierra
+    sha256 "a6ab1400604b2f0e2480b49adcdea3da94709731b4f82bfff62eae8eb3cf2bf4" => :mojave
+    sha256 "b8da4046f59e43bd457151a00ce03f015f3536debdade304021d708f7ae735a7" => :high_sierra
+    sha256 "6de5e0fc7453bf3ce93134c704900bfb954c777cc0f4d547623afe8b99ad0042" => :sierra
   end
 
   depends_on "node"
