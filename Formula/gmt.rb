0d95e362533b4ff3198f6f1adb108b604ac611c2
Caleb Xu <calebcenter@live.com>
2019-08-14 07:36:21 -0400

    gmt: bump revision for openblas update

--- Formula/gmt.rb
+++ Formula/gmt.rb
@@ -5,7 +5,7 @@ class Gmt < Formula
   mirror "https://mirrors.ustc.edu.cn/gmt/gmt-5.4.5-src.tar.xz"
   mirror "https://fossies.org/linux/misc/GMT/gmt-5.4.5-src.tar.xz"
   sha256 "225629c7869e204d5f9f1a384c4ada43e243f83e1ed28bdca4f7c2896bf39ef6"
-  revision 4
+  revision 5
 
   bottle do
     sha256 "e4378f5c77d0809935ba901fadcf0ac449cf68213d91345a8837a58c7d3a4c0f" => :mojave
