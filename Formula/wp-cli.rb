c5b56e4260accf6e4d357890a13a070bc672a249
Alain Schlesser <alain.schlesser@gmail.com>
2019-08-14 02:29:15 +0200

    wp-cli 2.3.0
    Closes #43082.
    
    Signed-off-by: Sean Molenaar <smillerdev@me.com>

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -1,8 +1,8 @@
 class WpCli < Formula
   desc "Command-line interface for WordPress"
   homepage "https://wp-cli.org/"
-  url "https://github.com/wp-cli/wp-cli/releases/download/v2.2.0/wp-cli-2.2.0.phar"
-  sha256 "71ee8a2449f17bf8a6fd385872cbdc3cc263107f7767a3b55f6c6e2ef5920368"
+  url "https://github.com/wp-cli/wp-cli/releases/download/v2.3.0/wp-cli-2.3.0.phar"
+  sha256 "3c5edf945846f650846b973a22645823c7f5e00b0e393f921397d26ee11f3770"
 
   bottle :unneeded
 
