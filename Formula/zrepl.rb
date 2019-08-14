4b40b7cc84bfa5e5ff7d9882aac40d73f0b1a3b7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-14 07:14:25 +0000

    zrepl: add 0.1.1 bottle.

--- Formula/zrepl.rb
+++ Formula/zrepl.rb
@@ -5,6 +5,13 @@ class Zrepl < Formula
   sha256 "0c16554e4527d14a390d78cf95bce759da425019a83ec63acfed5b4c50d68c9c"
   head "https://github.com/zrepl/zrepl.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9cec0ea416a2a394452fac82be895e8931c8e882465518e237a2b847e3e168d7" => :mojave
+    sha256 "2fd02af7892953417e8a4e4322c40ea2e1e2ac0181719d5afeaa88df62a91029" => :high_sierra
+    sha256 "05827e523e9e3c6e25579abc41d7367cd877694d6f4a90174dd4f801a775f9e3" => :sierra
+  end
+
   depends_on "dep" => :build
   depends_on "go" => :build
 
