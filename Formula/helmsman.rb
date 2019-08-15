4434717c84c13c62ae12fc48e40fce63e51d26cd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-15 12:55:35 +0000

    helmsman: update 1.12.0 bottle.

--- Formula/helmsman.rb
+++ Formula/helmsman.rb
@@ -7,9 +7,9 @@ class Helmsman < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "84542045562e7ca2298f811f38c9b86d038816720ca3c2a551da03f5fee3e3b0" => :mojave
-    sha256 "05fd133dec4b9d900d9d5d8254d0fbc2803c11a61b818a427c7ee140bb0eb7cc" => :high_sierra
-    sha256 "e044eecff4c7624bdc9623fa4d35ca15b6907da688c5c6b6f6f62318d3b3aef7" => :sierra
+    sha256 "c48c7f7263f4888c70fd01050b09c1353fc3ec9ff94d890a1b91201ff7fd2dd7" => :mojave
+    sha256 "12c21c1446a0c7664d7e184463d1507880a6118d89019fa0d3523e3dc52aece1" => :high_sierra
+    sha256 "0712878d28acc0efc4ef596801cc5a378712fd22007bf4d6592cef9687573e3c" => :sierra
   end
 
   depends_on "dep" => :build
