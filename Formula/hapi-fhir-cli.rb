0eebf1682e052dd6d1974f311e8f180546f8d535
John Grimes <John.Grimes@csiro.au>
2019-08-15 10:29:50 +1000

    hapi-fhir-cli 4.0.0
    Closes #43120.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/hapi-fhir-cli.rb
+++ Formula/hapi-fhir-cli.rb
@@ -1,15 +1,15 @@
 class HapiFhirCli < Formula
   desc "Command-line interface for the HAPI FHIR library"
   homepage "https://hapifhir.io/doc_cli.html"
-  url "https://github.com/jamesagnew/hapi-fhir/releases/download/v3.8.0/hapi-fhir-3.8.0-cli.zip"
-  sha256 "0446005f23b8b8319d20474d309e3b7f423cf02b88d4c59dc517a0aaf9535943"
+  url "https://github.com/jamesagnew/hapi-fhir/releases/download/v4.0.0/hapi-fhir-4.0.0-cli.zip"
+  sha256 "fdba98e9ddc295030f9c14bf083fa764a2706adf3f88788d7a021c77c318ca55"
 
   bottle :unneeded
 
   depends_on :java => "1.8+"
 
   resource "test_resource" do
-    url "https://github.com/jamesagnew/hapi-fhir/raw/v3.8.0/hapi-fhir-structures-dstu3/src/test/resources/specimen-example.json"
+    url "https://github.com/jamesagnew/hapi-fhir/raw/v4.0.0/hapi-fhir-structures-dstu3/src/test/resources/specimen-example.json"
     sha256 "4eacf47eccec800ffd2ca23b704c70d71bc840aeb755912ffb8596562a0a0f5e"
   end
 
