f6bdb88baca7c9d139db65a1da77f97097428ae6
Vasyl Khomko <vasyl@crowdin.com>
2019-08-15 17:13:29 +0300

    crowdin 2.0.31
    Closes #43188.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/crowdin.rb
+++ Formula/crowdin.rb
@@ -1,8 +1,8 @@
 class Crowdin < Formula
   desc "Command-line tool that allows to manage your resources with crowdin.com"
   homepage "https://support.crowdin.com/cli-tool/"
-  url "https://downloads.crowdin.com/cli/v2/crowdin-cli-2.0.30.zip"
-  sha256 "7066ce25339107c00b82918ef0c3b8d4913e3dedbafa677b2b50f12c45f6d4f8"
+  url "https://downloads.crowdin.com/cli/v2/crowdin-cli-2.0.31.zip"
+  sha256 "93defe16706783e92cbe3b32e528e495ddffa9e2a68471c3b70a2eb6c487e245"
 
   bottle :unneeded
 
