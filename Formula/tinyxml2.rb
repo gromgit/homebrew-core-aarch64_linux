caba5a689c8dda0f765cfc620f72eb3870e4f41c
Caleb Xu <calebcenter@live.com>
2019-08-15 08:59:47 -0400

    tinyxml2 7.1.0
    Closes #43148.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/tinyxml2.rb
+++ Formula/tinyxml2.rb
@@ -1,8 +1,8 @@
 class Tinyxml2 < Formula
   desc "Improved tinyxml (in memory efficiency and size)"
   homepage "http://grinninglizard.com/tinyxml2"
-  url "https://github.com/leethomason/tinyxml2/archive/7.0.1.tar.gz"
-  sha256 "a381729e32b6c2916a23544c04f342682d38b3f6e6c0cad3c25e900c3a7ef1a6"
+  url "https://github.com/leethomason/tinyxml2/archive/7.1.0.tar.gz"
+  sha256 "68ebd396a4220d5a9b5a621c6e9c66349c5cfdf5efaea3f16e3bb92e45f4e2a3"
   head "https://github.com/leethomason/tinyxml2.git"
 
   bottle do
