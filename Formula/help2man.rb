9ac3e47d761417be860e77bedaca49b2916a6faf
Caleb Xu <calebcenter@live.com>
2019-08-16 10:03:15 -0400

    help2man 1.47.11
    Closes #43197.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/help2man.rb
+++ Formula/help2man.rb
@@ -1,9 +1,9 @@
 class Help2man < Formula
   desc "Automatically generate simple man pages"
   homepage "https://www.gnu.org/software/help2man/"
-  url "https://ftp.gnu.org/gnu/help2man/help2man-1.47.10.tar.xz"
-  mirror "https://ftpmirror.gnu.org/help2man/help2man-1.47.10.tar.xz"
-  sha256 "f371cbfd63f879065422b58fa6b81e21870cd791ef6e11d4528608204aa4dcfb"
+  url "https://ftp.gnu.org/gnu/help2man/help2man-1.47.11.tar.xz"
+  mirror "https://ftpmirror.gnu.org/help2man/help2man-1.47.11.tar.xz"
+  sha256 "5985b257f86304c8791842c0c807a37541d0d6807ee973000cf8a3fe6ad47b88"
 
   bottle do
     cellar :any_skip_relocation
