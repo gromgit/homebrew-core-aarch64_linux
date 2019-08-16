af81fd1ca51d859cb0ddcb641580d55e344b260d
Caleb Xu <calebcenter@live.com>
2019-08-16 09:27:42 -0400

    ckan 1.26.4
    Closes #43193.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/ckan.rb
+++ Formula/ckan.rb
@@ -1,8 +1,8 @@
 class Ckan < Formula
   desc "The Comprehensive Kerbal Archive Network"
   homepage "https://github.com/KSP-CKAN/CKAN/"
-  url "https://github.com/KSP-CKAN/CKAN/releases/download/v1.26.2/ckan.exe"
-  sha256 "a0b4c43640d91ef5a0c3171906152636de75bf8497a1dc85d116a97c2c416185"
+  url "https://github.com/KSP-CKAN/CKAN/releases/download/v1.26.4/ckan.exe"
+  sha256 "18e294ff3f0301a95d467d7ee62349c2a8c5fc38d8fa6a9fa3d37e8cc6c8bfd2"
 
   bottle :unneeded
 
