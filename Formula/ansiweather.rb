1ab077c19794db2c0e0fdbcedc650b30fccc56bf
Caleb Xu <calebcenter@live.com>
2019-08-16 12:47:11 -0400

    ansiweather 1.14.0
    Closes #43212.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/ansiweather.rb
+++ Formula/ansiweather.rb
@@ -1,8 +1,8 @@
 class Ansiweather < Formula
   desc "Weather in your terminal, with ANSI colors and Unicode symbols"
   homepage "https://github.com/fcambus/ansiweather"
-  url "https://github.com/fcambus/ansiweather/archive/1.13.0.tar.gz"
-  sha256 "2f0fc8722e45f02042a96d903e87196ad08b237ae693895df9a805c27637dac9"
+  url "https://github.com/fcambus/ansiweather/archive/1.14.0.tar.gz"
+  sha256 "8b7d456c657e3240b14821b3819f2d2279d9a914e397058ee25b98b4783f9ad3"
   head "https://github.com/fcambus/ansiweather.git"
 
   bottle :unneeded
