cbea6978358c29b0d33d5cb542cb5a6f02793afa
Caleb Xu <calebcenter@live.com>
2019-08-17 19:02:45 -0400

    wcslib 6.4

--- Formula/wcslib.rb
+++ Formula/wcslib.rb
@@ -1,8 +1,8 @@
 class Wcslib < Formula
   desc "Library and utilities for the FITS World Coordinate System"
   homepage "https://www.atnf.csiro.au/people/mcalabre/WCS/"
-  url "https://www.atnf.csiro.au/pub/software/wcslib/wcslib-6.3.tar.bz2"
-  sha256 "4c37f07d64d51abaf30093238fdd426a321ffaf5a9575f7fd81e155d973822ea"
+  url "https://www.atnf.csiro.au/pub/software/wcslib/wcslib-6.4.tar.bz2"
+  sha256 "13c11ff70a7725563ec5fa52707a9965fce186a1766db193d08c9766ea107000"
 
   bottle do
     cellar :any
