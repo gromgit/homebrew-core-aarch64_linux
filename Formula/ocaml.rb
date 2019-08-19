94588d5c7b66d6505d888b36d212ea87689ee456
Andrew J. Hesford <ajh@sideband.org>
2019-08-19 15:00:08 -0400

    ocaml 4.08.1

--- Formula/ocaml.rb
+++ Formula/ocaml.rb
@@ -14,8 +14,8 @@
 class Ocaml < Formula
   desc "General purpose programming language in the ML family"
   homepage "https://ocaml.org/"
-  url "https://caml.inria.fr/pub/distrib/ocaml-4.07/ocaml-4.07.1.tar.xz"
-  sha256 "dfe48b1da31da9c82d77612582fae74c80e8d1ac650e1c24f5ac9059e48307b8"
+  url "https://caml.inria.fr/pub/distrib/ocaml-4.08/ocaml-4.08.1.tar.xz"
+  sha256 "cd4f180453ffd7cc6028bb18954b3d7c3f715af13157df2f7c68bdfa07655ea3"
   head "https://github.com/ocaml/ocaml.git", :branch => "trunk"
 
   bottle do
@@ -36,17 +36,15 @@ class Ocaml < Formula
     ENV.deparallelize # Builds are not parallel-safe, esp. with many cores
 
     # the ./configure in this package is NOT a GNU autoconf script!
-    args = [
-      "-prefix",
-      HOMEBREW_PREFIX.to_s,
-      "-with-debug-runtime",
-      "-mandir",
-      man.to_s,
-      "-no-graph",
+    args = %W[
+      --prefix=#{HOMEBREW_PREFIX}
+      --enable-debug-runtime
+      --mandir=#{man}
+      --disable-graph-lib
     ]
     system "./configure", *args
     system "make", "world.opt"
-    system "make", "install", "PREFIX=#{prefix}"
+    system "make", "prefix=#{prefix}", "install"
   end
 
   test do
