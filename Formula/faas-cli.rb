4ce3ff903caf2be1a874238bdf7e2852355a5c3b
Rui Chen <rchen@meetup.com>
2019-08-19 13:17:08 -0400

    faas-cli 0.9.2
    Closes #43274.
    
    Signed-off-by: Jan Viljanen <527069+javian@users.noreply.github.com>

--- Formula/faas-cli.rb
+++ Formula/faas-cli.rb
@@ -2,8 +2,8 @@ class FaasCli < Formula
   desc "CLI for templating and/or deploying FaaS functions"
   homepage "https://docs.get-faas.com/"
   url "https://github.com/openfaas/faas-cli.git",
-      :tag      => "0.9.1",
-      :revision => "bf075ea3a0c83e751e3060da8c5867cc5b84637a"
+      :tag      => "0.9.2",
+      :revision => "893972afffa5ea6672faa6e11743ae46322ec58c"
 
   bottle do
     cellar :any_skip_relocation
