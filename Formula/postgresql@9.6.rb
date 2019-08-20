1f2cc235d884cd78926b0ff2a9ee6a5cff2e2953
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-20 17:14:54 +0000

    postgresql@9.6: update 9.6.15 bottle.

--- Formula/postgresql@9.6.rb
+++ Formula/postgresql@9.6.rb
@@ -5,9 +5,9 @@ class PostgresqlAT96 < Formula
   sha256 "3cd9fe9af247167f863030842c1a57f58bdf3e5d50a94997d34a802b6032170a"
 
   bottle do
-    sha256 "03423061a65ade325b95e176896f55ffb59ab56498095a356c6120796be7790e" => :mojave
-    sha256 "75b6a65e4e35346e28feb8595e84ad89d1809326bd34ddebddd83d34f6fc7c2e" => :high_sierra
-    sha256 "98e5a9e5054991bc2809535f2cc95ffa783be36981e876078047c1107c902fd9" => :sierra
+    sha256 "4f2b8966cf72586643fca65ac20a820936c37e945df05374cbe2e14b4019998c" => :mojave
+    sha256 "e12f8db9a5d6a133f868781d3fee6a37414ee4fcb1c1131d2e3c60e11275dd3b" => :high_sierra
+    sha256 "a209edc33c358d177052f096826bddd90d3d637509ce1415c63a29d1fd134456" => :sierra
   end
 
   keg_only :versioned_formula
