dfb11fd62be7b6deea596b420aa797fdc75917c8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-20 00:46:22 +0000

    libiodbc: update 3.52.13 bottle.

--- Formula/libiodbc.rb
+++ Formula/libiodbc.rb
@@ -6,12 +6,9 @@ class Libiodbc < Formula
 
   bottle do
     cellar :any
-    sha256 "004fa7bfa6e2abb79b0ad623344aeede05932c18d65820b1c1c84069f6368fe0" => :mojave
-    sha256 "8255e22ddcb97db352f37a8be775eb40d120b6a89b724bd461e5d76bbc746c18" => :high_sierra
-    sha256 "197ddbad1eec2fc783faf97622dd53cc29c600b0c725fb96b6252dc94dabd731" => :sierra
-    sha256 "85570401135c9fa3f6325ae4ce098180128491c4472155f85ad5b7c4c6473d9e" => :el_capitan
-    sha256 "cbcd0d50f16a1faa596466ba6678529550d631f770f84657da877f87e17b0424" => :yosemite
-    sha256 "47fecc486608df1edc094742a3afbd33c7159c8957429528e11a6fb6f551ebc4" => :mavericks
+    sha256 "1472bb0987705537158b7c3196d27d01ba02d6c0fdcca733f3cf8d53eca29c5d" => :mojave
+    sha256 "77a4fb5fa3036a831e05e2a83585ac2fcdcdf4cf83baa72f28cfb2f8a659ba13" => :high_sierra
+    sha256 "abc07f2fe98ed04c4dc5bd5cada2ea68fb9be56337ed442393609f0a22ec21e8" => :sierra
   end
 
   depends_on "autoconf" => :build
