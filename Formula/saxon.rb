19d5f37b80336e5db54dff5da5c6af466c7bfa3e
Caleb Xu <calebcenter@live.com>
2019-08-20 08:56:55 -0400

    saxon 9.9.1.4
    Closes #43305.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/saxon.rb
+++ Formula/saxon.rb
@@ -1,9 +1,9 @@
 class Saxon < Formula
   desc "XSLT and XQuery processor"
   homepage "https://saxon.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/saxon/Saxon-HE/9.9/SaxonHE9-9-1-3J.zip"
-  version "9.9.1.3"
-  sha256 "9947367c805678d8140c14bfc0eec0515e8ef162801c489cbdf3f4c60087a613"
+  url "https://downloads.sourceforge.net/project/saxon/Saxon-HE/9.9/SaxonHE9-9-1-4J.zip"
+  version "9.9.1.4"
+  sha256 "5c0b4ad1f81958ebe18cc40641518671c39be36f25a1a10ace73b69c1f2eb671"
 
   bottle :unneeded
 
