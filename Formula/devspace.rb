2f2f74529467ff43890fd855b7f78d0280fe48fa
Rui Chen <rchen@meetup.com>
2019-08-21 12:00:09 -0400

    devspace 3.5.17
    Closes #43343.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/devspace.rb
+++ Formula/devspace.rb
@@ -2,8 +2,8 @@ class Devspace < Formula
   desc "CLI helps develop/deploy/debug apps with Docker and k8s"
   homepage "https://devspace.cloud/docs"
   url "https://github.com/devspace-cloud/devspace.git",
-    :tag      => "v3.5.16",
-    :revision => "517310f18a03aaa247e8b6635db585503d98cae8"
+    :tag      => "v3.5.17",
+    :revision => "9a2a679d8e0de1940fbea59801e59023a745df32"
 
   bottle do
     cellar :any_skip_relocation
