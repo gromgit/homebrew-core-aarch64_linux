b26d60b976f7bc83641cef1d09f634bce88157b2
lixiaojun <lixiaojun629@163.com>
2019-08-22 16:01:51 +0800

    ucloud 0.1.23
    Closes #43359.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/ucloud.rb
+++ Formula/ucloud.rb
@@ -1,8 +1,8 @@
 class Ucloud < Formula
   desc "The official tool to managment your ucloud services"
   homepage "https://www.ucloud.cn"
-  url "https://github.com/ucloud/ucloud-cli/archive/0.1.22.tar.gz"
-  sha256 "4618222c3cfffb991ec1c4dbb2aca1518ac4be035ca22ba268b027e550022e9f"
+  url "https://github.com/ucloud/ucloud-cli/archive/0.1.23.tar.gz"
+  sha256 "a7d4ee43320e31514b17d6deb8768da2c2a6c84dc741c20082bc98683b3fde9a"
 
   bottle do
     cellar :any_skip_relocation
