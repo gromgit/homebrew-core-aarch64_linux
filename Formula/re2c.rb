816d2bac9630c69b7643a29473d4b93d5755b35e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-23 19:52:40 +0000

    re2c: update 1.2.1 bottle.

--- Formula/re2c.rb
+++ Formula/re2c.rb
@@ -5,10 +5,9 @@ class Re2c < Formula
   sha256 "1a4cd706b5b966aeffd78e3cf8b24239470ded30551e813610f9cd1a4e01b817"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "cf04c6f2c4631614ddca8391e0d443428b92bff5858d0a87150eb26366e9a431" => :mojave
-    sha256 "58a4fa0867037c92f9c9c29159f64a4435cdd2af9f76112213fc67ce47f94d5d" => :high_sierra
-    sha256 "71a1e1aa49f524d87452641a05f29f117d86d6f87bac19a5bdfed96b3ca8aedf" => :sierra
+    sha256 "2d9f14907212e68580afa38eae450c8c0a9157169a1fd00e6faf27f872134433" => :mojave
+    sha256 "3a39e5869772c6774b53c22e59d5e4fd195349962aedcf65731cd6eb0cb54e9d" => :high_sierra
+    sha256 "20d4035575168abd66a65bfda9e93efa4d2aae6ef6597f1bd66ffa3028151dff" => :sierra
   end
 
   def install
