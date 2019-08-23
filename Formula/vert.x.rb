dbf834cf43b434c62aa9a3a01f8a645a98096d3c
Clement Escoffier <clement.escoffier@gmail.com>
2019-08-23 03:43:16 +0200

    vert.x 3.8.1 (#43371)
    Update the vert.x formula to 3.8.1.

--- Formula/vert.x.rb
+++ Formula/vert.x.rb
@@ -1,8 +1,8 @@
 class VertX < Formula
   desc "Toolkit for building reactive applications on the JVM"
   homepage "https://vertx.io/"
-  url "https://bintray.com/vertx/downloads/download_file?file_path=vert.x-3.8.0-full.tar.gz"
-  sha256 "660dc32533eb3d37771be2f70497b2346a5d174abb782c17f334cbc36adcdcc9"
+  url "https://bintray.com/vertx/downloads/download_file?file_path=vert.x-3.8.1-full.tar.gz"
+  sha256 "940448e11d145791ae7c31a95992471790ab73dddfe5f79e3ed01ae58c7a2da3"
 
   bottle :unneeded
   depends_on :java => "1.8+"
