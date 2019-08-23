c7e2135bd0c161e02e52e06c64e0396f9351e2a6
Rui Chen <chenrui333@gmail.com>
2019-08-23 09:24:55 -0400

    tomcat@8 8.5.45
    Closes #43407.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/tomcat@8.rb
+++ Formula/tomcat@8.rb
@@ -1,8 +1,8 @@
 class TomcatAT8 < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-8/v8.5.43/bin/apache-tomcat-8.5.43.tar.gz"
-  sha256 "5352ab0cc4b6e71caf9161da8c4316e9cd8691bc9f5ac3e8d8b3d259ca2bae03"
+  url "https://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-8/v8.5.45/bin/apache-tomcat-8.5.45.tar.gz"
+  sha256 "d9b58d12979243fba01bbbbb33c140c8593940c005ec9acef1b2f54ce9b3d0fc"
 
   bottle :unneeded
 
