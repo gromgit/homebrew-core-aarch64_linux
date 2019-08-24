7cb48220334ffa04518ad23288c235f00a260b44
Michael Rolli <michael@rollis.ch>
2019-08-24 11:12:31 +0200

    packer-completion 1.4.3
    Closes #43431.
    
    Signed-off-by: Chongyu Zhu <i@lembacon.com>

--- Formula/packer-completion.rb
+++ Formula/packer-completion.rb
@@ -1,8 +1,8 @@
 class PackerCompletion < Formula
   desc "Bash completion for Packer"
   homepage "https://github.com/mrolli/packer-bash-completion"
-  url "https://github.com/mrolli/packer-bash-completion/archive/1.0.0.tar.gz"
-  sha256 "20ebfacd3f3a60f8dbd09e25b97d3b6e5049cbdf00a2d607fe79eaaef39e1eea"
+  url "https://github.com/mrolli/packer-bash-completion/archive/1.4.3.tar.gz"
+  sha256 "af7b3b49b29ffdb05b519dad2d83066f3d166dd8e29abd406ca0f3d480901df4"
   head "https://github.com/mrolli/packer-bash-completion.git"
 
   bottle :unneeded
