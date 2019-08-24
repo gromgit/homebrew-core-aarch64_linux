8c790136036c3b93c08a009ca8116334d2497bf2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-24 05:11:41 +0000

    cpu_features: add 0.4.1 bottle.

--- Formula/cpu_features.rb
+++ Formula/cpu_features.rb
@@ -4,6 +4,13 @@ class CpuFeatures < Formula
   url "https://github.com/google/cpu_features/archive/v0.4.1.tar.gz"
   sha256 "b7b6b27f759410f73f15f935ae646f30c9c6742731dc354416399677bd418156"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b2736429ca74d23173266aab1d3ee6fea58ee963e0042391d803b26d322838d7" => :mojave
+    sha256 "0d8119f02b609f5cfcac19177bd669668686c549bb138fbe2a0aa391df7dc838" => :high_sierra
+    sha256 "d703ec98a4f4913ac50c872475e7c37e6c439326db607297fa14cd438e75f919" => :sierra
+  end
+
   depends_on "cmake" => :build
 
   def install
