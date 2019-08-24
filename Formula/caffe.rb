cef8f62d94a8ae2695b0dd0310da6c1313287d10
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-24 05:50:07 +0000

    caffe: update 1.0_14 bottle.

--- Formula/caffe.rb
+++ Formula/caffe.rb
@@ -6,9 +6,9 @@ class Caffe < Formula
   revision 14
 
   bottle do
-    sha256 "a31068f71a8b250436e2d16d0f62e742b26d81e1eab9630da819a86471d18de1" => :mojave
-    sha256 "3d28b5321e7e8e5bad633994f2e568c80ec69863456935bd6b14b1f43faafb03" => :high_sierra
-    sha256 "c791d7efe37be781ee2aeaa478fb1d9a17e3e3685cc7834ed004c0ee848a1be0" => :sierra
+    sha256 "e42c7b0dd7165199c299008551f1224353982421bb9fc3ef2a006c380907a6de" => :mojave
+    sha256 "5cb7b806c64970fe3013ba3e9c7c10ff0667bf5d2b90da03bde23e5d42461c69" => :high_sierra
+    sha256 "3186e66f6bead0992c8fbe39d4177c14b4e4328c2bb2cc22fc4d9e5113e27063" => :sierra
   end
 
   depends_on "cmake" => :build
