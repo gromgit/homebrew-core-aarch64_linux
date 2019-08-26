a3722143e26d38f52a1680e68be1989cd451e4ff
Viktor Szakats <commit@vszakats.net>
2019-08-26 06:40:12 +0000

    gti: secure url(s)

--- Formula/gti.rb
+++ Formula/gti.rb
@@ -1,6 +1,6 @@
 class Gti < Formula
   desc "ASCII-art displaying typo-corrector for commands"
-  homepage "http://r-wos.org/hacks/gti"
+  homepage "https://r-wos.org/hacks/gti"
   url "https://github.com/rwos/gti/archive/v1.6.1.tar.gz"
   sha256 "6dd5511b92b64df115b358c064e7701b350b343f30711232a8d74c6274c962a5"
   head "https://github.com/rwos/gti.git"
