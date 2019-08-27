687c403a774419f5120bb9196f021b937f5809f1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-27 15:43:33 +0000

    pokerstove: add 1.0 bottle.

--- Formula/pokerstove.rb
+++ Formula/pokerstove.rb
@@ -4,6 +4,13 @@ class Pokerstove < Formula
   url "https://github.com/andrewprock/pokerstove/archive/v1.0.tar.gz"
   sha256 "68503e7fc5a5b2bac451c0591309eacecba738d787874d5421c81f59fde2bc74"
 
+  bottle do
+    cellar :any
+    sha256 "60f469d6ec2b39eac5d801c7968f56b21ddd0464ebc074964f6dacb4cd151b74" => :mojave
+    sha256 "49b7d8bcd4c54b42dbd311f94dcfb71ff04d3a1e273ecda213baaa562ad9b02b" => :high_sierra
+    sha256 "c78b16c6cfd9981df7ba66dd367f3b25181350d317e0ec00704a3b837383a8e8" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "boost"
 
