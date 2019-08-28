cefc586c9cfc03724c445bf70e8aea69413f2fdf
FX Coudert <fxcoudert@gmail.com>
2019-08-29 01:01:11 +0200

    http_load: move to OpenSSL 1.1

--- Formula/http_load.rb
+++ Formula/http_load.rb
@@ -4,7 +4,7 @@ class HttpLoad < Formula
   url "https://www.acme.com/software/http_load/http_load-09Mar2016.tar.gz"
   version "20160309"
   sha256 "5a7b00688680e3fca8726dc836fd3f94f403fde831c71d73d9a1537f215b4587"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
@@ -15,7 +15,7 @@ class HttpLoad < Formula
     sha256 "f4702e82a17b0c972164f2bc8ba985edccf0f3dc840627d37d5307d9b914ba25" => :yosemite
   end
 
-  depends_on "openssl"
+  depends_on "openssl@1.1"
 
   def install
     bin.mkpath
@@ -26,7 +26,7 @@ class HttpLoad < Formula
       LIBDIR=#{lib}
       MANDIR=#{man1}
       CC=#{ENV.cc}
-      SSL_TREE=#{Formula["openssl"].opt_prefix}
+      SSL_TREE=#{Formula["openssl@1.1"].opt_prefix}
     ]
 
     inreplace "Makefile", "#SSL_", "SSL_"
