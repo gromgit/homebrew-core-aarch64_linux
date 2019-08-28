84d41c55af723989ccfd61161fc014c69f62da5c
Matteo Merli <mmerli@apache.org>
2019-08-28 11:26:50 -0700

    libpulsar 2.4.0
    Closes #43555.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libpulsar.rb
+++ Formula/libpulsar.rb
@@ -1,9 +1,8 @@
 class Libpulsar < Formula
   desc "Apache Pulsar C++ library"
   homepage "https://pulsar.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=pulsar/pulsar-2.3.2/apache-pulsar-2.3.2-src.tar.gz"
-  sha256 "18f3681982f206c8f9ad98b5ce0cfaeae24628473d86feb9a3711682050bbbbc"
-  revision 2
+  url "https://www.apache.org/dyn/closer.cgi?path=pulsar/pulsar-2.4.0/apache-pulsar-2.4.0-src.tar.gz"
+  sha256 "b4666cade20f7e7c01b9050813a3975d4e0fba36f0ab058e39e41b30e029dc05"
 
   bottle do
     cellar :any
@@ -18,12 +17,6 @@ class Libpulsar < Formula
   depends_on "openssl"
   depends_on "protobuf"
   depends_on "zstd"
-
-  # patch for boost 1.70
-  patch do
-    url "https://github.com/apache/pulsar/commit/07845c5b463b35824f7b4bcab526e90e53489cdb.diff?full_index=1"
-    sha256 "62467d4ff27485caf6784ce3a6756ba9e62b5ad5070fb29637435faf12c39afe"
-  end
 
   def install
     cd "pulsar-client-cpp" do
