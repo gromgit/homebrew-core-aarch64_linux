1ccd5c2e49ffa84d27339628c20584a4f88989c7
FX Coudert <fxcoudert@gmail.com>
2019-08-29 00:57:34 +0200

    freeradius-server: move to OpenSSL 1.1

--- Formula/freeradius-server.rb
+++ Formula/freeradius-server.rb
@@ -3,6 +3,7 @@ class FreeradiusServer < Formula
   homepage "https://freeradius.org/"
   url "https://github.com/FreeRADIUS/freeradius-server/archive/release_3_0_19.tar.gz"
   sha256 "34c50ac47a683b13eae1a02f2d0263c0bd51a83f01b99c02c5fe25df07a1ee77"
+  revision 1
   head "https://github.com/FreeRADIUS/freeradius-server.git"
 
   bottle do
@@ -11,7 +12,7 @@ class FreeradiusServer < Formula
     sha256 "da36b7c93a9e2c4b3fc58fa319fe47287c5e0eb2d6dfa702d9f95fc6424f9bff" => :sierra
   end
 
-  depends_on "openssl"
+  depends_on "openssl@1.1"
   depends_on "talloc"
   uses_from_macos "perl"
 
@@ -22,8 +23,8 @@ class FreeradiusServer < Formula
       --prefix=#{prefix}
       --sbindir=#{bin}
       --localstatedir=#{var}
-      --with-openssl-includes=#{Formula["openssl"].opt_include}
-      --with-openssl-libraries=#{Formula["openssl"].opt_lib}
+      --with-openssl-includes=#{Formula["openssl@1.1"].opt_include}
+      --with-openssl-libraries=#{Formula["openssl@1.1"].opt_lib}
       --with-talloc-lib-dir=#{Formula["talloc"].opt_lib}
       --with-talloc-include-dir=#{Formula["talloc"].opt_include}
     ]
