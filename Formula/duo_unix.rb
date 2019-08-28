5da740ab89fd906e4a5f540e7db5d4f2bac15b1e
FX Coudert <fxcoudert@gmail.com>
2019-08-28 20:01:00 +0200

    duo_unix: move to OpenSSL 1.1

--- Formula/duo_unix.rb
+++ Formula/duo_unix.rb
@@ -3,6 +3,7 @@ class DuoUnix < Formula
   homepage "https://www.duosecurity.com/docs/duounix"
   url "https://github.com/duosecurity/duo_unix/archive/duo_unix-1.11.2.tar.gz"
   sha256 "e1ec2f43036ba639743d631f308419c9a88618a93d4038bf40a9cdeef89ca6db"
+  revision 1
 
   bottle do
     sha256 "006db27f7e6d2370e6a5e318c5c5eb0105c1dd5092c0fe397b0c9196d7298432" => :mojave
@@ -13,7 +14,7 @@ class DuoUnix < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on "openssl"
+  depends_on "openssl@1.1"
 
   def install
     system "./bootstrap"
@@ -22,7 +23,7 @@ class DuoUnix < Formula
                           "--prefix=#{prefix}",
                           "--sysconfdir=#{etc}",
                           "--includedir=#{include}/duo",
-                          "--with-openssl=#{Formula["openssl"].opt_prefix}",
+                          "--with-openssl=#{Formula["openssl@1.1"].opt_prefix}",
                           "--with-pam=#{lib}/pam/"
     system "make", "install"
   end
