f1a012701720ee1a154d7d80d96a7c50a7ed085c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-28 21:54:10 +0000

    btpd: update 0.16_2 bottle.

--- Formula/btpd.rb
+++ Formula/btpd.rb
@@ -7,12 +7,9 @@ class Btpd < Formula
 
   bottle do
     cellar :any
-    sha256 "6957aac8c38ccf8faa140f961de205bcd1bf7e7ee3dd68ae545dd0b94d16da19" => :mojave
-    sha256 "81d4cb1f1407e4619120e6551258888fc7266ab1d98968095fbbef76e80d1f42" => :high_sierra
-    sha256 "c951cbaadc8a4a04dbe95fe28fdb6f2ad760352c58fba5a36309ed3fdff69e16" => :sierra
-    sha256 "4dce615bca726cf8ea3adfd0ab5e18f4f1e3d95d1a4d98ce38d6f894b1206a25" => :el_capitan
-    sha256 "382f8e3ec6e514f5a5116b562c82e0f3b2ae786b625ba08ab222fe7db9a4bcff" => :yosemite
-    sha256 "62a5bcf9db33b7b543053ce0a7d6ce4ed1fdfc43c9fca2500adc289c8bf34dc8" => :mavericks
+    sha256 "0b479e7b812055a0ebbbae40c63624258044d74cb11a2d698392792a5b543e4d" => :mojave
+    sha256 "35042eba57182babbaff9f4a2eb1cbe891ebd82d2427a14926fd3617475da363" => :high_sierra
+    sha256 "6951afbf4af1e9d0df95f5d9260ef04eeb7e558cd2d58c3a429a99ad93c2dddc" => :sierra
   end
 
   depends_on "openssl@1.1"
