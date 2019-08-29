e0c84fe895a09f6339ca6e43cc6a83226924a01a
Igor Kapkov <igasgeek@me.com>
2019-08-29 14:36:41 +1000

    u-boot-tools: move to OpenSSL 1.1
    Closes #43579.
    
    Signed-off-by: Igor Kapkov <igasgeek@me.com>

--- Formula/u-boot-tools.rb
+++ Formula/u-boot-tools.rb
@@ -3,6 +3,7 @@ class UBootTools < Formula
   homepage "https://www.denx.de/wiki/U-Boot/"
   url "https://ftp.denx.de/pub/u-boot/u-boot-2019.07.tar.bz2"
   sha256 "bff4fa77e8da17521c030ca4c5b947a056c1b1be4d3e6ee8637020b8d50251d0"
+  revision 1
 
   bottle do
     cellar :any
@@ -11,7 +12,7 @@ class UBootTools < Formula
     sha256 "700555f219bb29d8b01676e0c7c77d3603acdc4f8384d3ac4f322fbc621104dd" => :sierra
   end
 
-  depends_on "openssl"
+  depends_on "openssl@1.1"
 
   def install
     system "make", "sandbox_defconfig"
