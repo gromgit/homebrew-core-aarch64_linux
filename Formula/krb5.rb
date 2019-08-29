03e92c149ad5547db72ae6a2887263d22ca34895
FX Coudert <fxcoudert@gmail.com>
2019-08-29 10:27:59 +0200

    krb5: move to OpenSSL 1.1

--- Formula/krb5.rb
+++ Formula/krb5.rb
@@ -3,6 +3,7 @@ class Krb5 < Formula
   homepage "https://web.mit.edu/kerberos/"
   url "https://kerberos.org/dist/krb5/1.17/krb5-1.17.tar.gz"
   sha256 "5a6e2284a53de5702d3dc2be3b9339c963f9b5397d3fbbc53beb249380a781f5"
+  revision 1
 
   bottle do
     sha256 "a73183fac51592371795af726b991893db251dbacbcad5e9b0c2f1eb41dd1f62" => :mojave
@@ -12,16 +13,15 @@ class Krb5 < Formula
 
   keg_only :provided_by_macos
 
-  depends_on "openssl"
+  depends_on "openssl@1.1"
 
   def install
     cd "src" do
-      system "./configure",
-        "--disable-debug",
-        "--disable-dependency-tracking",
-        "--disable-silent-rules",
-        "--prefix=#{prefix}",
-        "--without-system-verto"
+      system "./configure", "--disable-debug",
+                            "--disable-dependency-tracking",
+                            "--disable-silent-rules",
+                            "--prefix=#{prefix}",
+                            "--without-system-verto"
       system "make"
       system "make", "install"
     end
