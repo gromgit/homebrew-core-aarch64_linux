9d1cddfd128c2c84afe57356b6e08d293834c4b8
Igor Kapkov <igasgeek@me.com>
2019-08-29 14:19:53 +1000

    ircd-hybrid: move to OpenSSL 1.1
    Closes #43578.
    
    Signed-off-by: Igor Kapkov <igasgeek@me.com>

--- Formula/ircd-hybrid.rb
+++ Formula/ircd-hybrid.rb
@@ -3,6 +3,7 @@ class IrcdHybrid < Formula
   homepage "http://www.ircd-hybrid.org/"
   url "https://downloads.sourceforge.net/project/ircd-hybrid/ircd-hybrid/ircd-hybrid-8.2.24/ircd-hybrid-8.2.24.tgz"
   sha256 "eaa42d8bf10c0e619e3bda96f35d31bb20715305a85a1386cfbc6b8761fed50e"
+  revision 1
 
   bottle do
     sha256 "019444c3b32e17ec5dab9f9a7697b2cceda280d2af447b4f56271082e98a2438" => :mojave
@@ -11,7 +12,7 @@ class IrcdHybrid < Formula
     sha256 "cb2919d989ee6ae1fa2ed37b4cdc24ff32d10512d436ec29d47218b75ab3f3a3" => :el_capitan
   end
 
-  depends_on "openssl"
+  depends_on "openssl@1.1"
 
   conflicts_with "ircd-irc2", :because => "both install an `ircd` binary"
 
@@ -25,7 +26,7 @@ class IrcdHybrid < Formula
                           "--prefix=#{prefix}",
                           "--localstatedir=#{var}",
                           "--sysconfdir=#{etc}",
-                          "--enable-openssl=#{Formula["openssl"].opt_prefix}"
+                          "--enable-openssl=#{Formula["openssl@1.1"].opt_prefix}"
     system "make", "install"
     etc.install "doc/reference.conf" => "ircd.conf"
   end
