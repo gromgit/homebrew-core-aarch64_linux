8bcf9dd780ce7bb691c14ccf2d3b6eb84e6d2771
FX Coudert <fxcoudert@gmail.com>
2019-08-29 10:27:59 +0200

    lftp: move to OpenSSL 1.1

--- Formula/lftp.rb
+++ Formula/lftp.rb
@@ -3,7 +3,7 @@ class Lftp < Formula
   homepage "https://lftp.yar.ru/"
   url "https://lftp.yar.ru/ftp/lftp-4.8.4.tar.xz"
   sha256 "4ebc271e9e5cea84a683375a0f7e91086e5dac90c5d51bb3f169f75386107a62"
-  revision 1
+  revision 2
 
   bottle do
     rebuild 1
@@ -12,13 +12,13 @@ class Lftp < Formula
   end
 
   depends_on "libidn"
-  depends_on "openssl"
+  depends_on "openssl@1.1"
   depends_on "readline"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
-                          "--with-openssl=#{Formula["openssl"].opt_prefix}",
+                          "--with-openssl=#{Formula["openssl@1.1"].opt_prefix}",
                           "--with-readline=#{Formula["readline"].opt_prefix}",
                           "--with-libidn=#{Formula["libidn"].opt_prefix}"
     system "make", "install"
