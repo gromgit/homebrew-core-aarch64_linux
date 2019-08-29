e3a868d89618ed8a1accd4dd4543349ecc8fca0d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-29 04:20:28 +0000

    w3m: update 0.5.3_6 bottle.

--- Formula/w3m.rb
+++ Formula/w3m.rb
@@ -19,9 +19,10 @@ class W3m < Formula
   end
 
   bottle do
-    sha256 "571d0562f50fb42eab8fc7efd03e7d3be1418ffdcbe378d2501ba3f53593e650" => :mojave
-    sha256 "74830829e5e3de7ac58b7486a2a8371ba2ea340d657d46693fc16e9db8c7ee34" => :high_sierra
-    sha256 "2d71608f1489de2eea58f1246d54f9cda56ddb94b7f1cd6652af3527823d64de" => :sierra
+    rebuild 1
+    sha256 "c2a4f7208e98f575eadaff6af3dc9a93305008b93d2f069c53d687ba61b85d64" => :mojave
+    sha256 "bc46bb9b70d7149058d2c757aa0b8ea68c7c6836faee26da0b697d81cca0927d" => :high_sierra
+    sha256 "809a34cb2c14b98827cfe9f18008b0ebc545e359c5f8c1279e71948ac336bdd1" => :sierra
   end
 
   depends_on "pkg-config" => :build
