6f5b01090954362c067644ca5b7a34c3ef651fa7
FX Coudert <fxcoudert@gmail.com>
2019-08-29 14:20:34 +0200

    pev: move to OpenSSL 1.1

--- Formula/pev.rb
+++ Formula/pev.rb
@@ -3,7 +3,15 @@ class Pev < Formula
   homepage "https://pev.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/pev/pev-0.80/pev-0.80.tar.gz"
   sha256 "f68c8596f16d221d9a742812f6f728bcc739be90957bc1b00fbaa5943ffc5cfa"
+  revision 1
   head "https://github.com/merces/pev.git"
+
+  # Upstream fix for compatibility with OpenSSL 1.1
+  # https://github.com/merces/pev/pull/104
+  patch do
+    url "https://github.com/merces/pev/commit/53eb494b.diff?full_index=1"
+    sha256 "66eeadd149716597dfa4ecb6dc9c021cb6ab910aa4002b7730c822dd5c8ea654"
+  end
 
   bottle do
     sha256 "dbc6f431e9f70bbbf582e07bcbd9cd852413d57adbd213573ec2b4c0ca3c19bb" => :mojave
@@ -13,7 +21,7 @@ class Pev < Formula
     sha256 "1013086fe324f7b6147181762541d89dcf4837bd29bd89756d7df23c26fad1f2" => :yosemite
   end
 
-  depends_on "openssl"
+  depends_on "openssl@1.1"
   depends_on "pcre"
 
   def install
