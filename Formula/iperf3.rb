be7e7432210437357095d9e84ee0daeaf6671639
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-29 11:23:50 +0000

    iperf3: update 3.7_1 bottle.

--- Formula/iperf3.rb
+++ Formula/iperf3.rb
@@ -7,9 +7,9 @@ class Iperf3 < Formula
 
   bottle do
     cellar :any
-    sha256 "c6f1a298b199949fc2642cd6eb5860a5d9be4433d1cfbe88ec6734a97fdbfaf6" => :mojave
-    sha256 "86cfc8e385961af48f2432773e5a5574d0aae07b7db14904b98c1f9c8d4f5063" => :high_sierra
-    sha256 "c0e071e3241558fe1e96ee08a90d65af8429f5aa077454b4956a1183228a8271" => :sierra
+    sha256 "8ac59ee41772cc626b683fd52b5647bfcf947d3fc8f5bc3672b2e37a7b05d71e" => :mojave
+    sha256 "3f5bd19f55d0635ce7ff229d404076513c374a30865b24b43b9094aae0453c25" => :high_sierra
+    sha256 "f50d4b377603451b62e60b8cb25e0bcb2cc808ba02f3474af1e922d22ec2ddc3" => :sierra
   end
 
   head do
