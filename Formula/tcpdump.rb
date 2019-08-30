9f7e5c81308b4a4466de919dfba5bb1f962216e1
FX Coudert <fxcoudert@gmail.com>
2019-08-30 11:15:42 +0200

    tcpdump: move to OpenSSL 1.1

--- Formula/tcpdump.rb
+++ Formula/tcpdump.rb
@@ -3,7 +3,7 @@ class Tcpdump < Formula
   homepage "https://www.tcpdump.org/"
   url "https://www.tcpdump.org/release/tcpdump-4.9.2.tar.gz"
   sha256 "798b3536a29832ce0cbb07fafb1ce5097c95e308a6f592d14052e1ef1505fe79"
-  revision 1
+  revision 2
   head "https://github.com/the-tcpdump-group/tcpdump.git"
 
   bottle do
@@ -14,7 +14,7 @@ class Tcpdump < Formula
   end
 
   depends_on "libpcap"
-  depends_on "openssl"
+  depends_on "openssl@1.1"
 
   def install
     system "./configure", "--prefix=#{prefix}",
