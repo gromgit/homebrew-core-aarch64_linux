9291766e52e82384eefb3387a7987c4e458ce3a8
FX Coudert <fxcoudert@gmail.com>
2019-08-30 11:15:42 +0200

    tiny-fugue: move to OpenSSL 1.1

--- Formula/tiny-fugue.rb
+++ Formula/tiny-fugue.rb
@@ -4,6 +4,7 @@ class TinyFugue < Formula
   url "https://downloads.sourceforge.net/project/tinyfugue/tinyfugue/5.0%20beta%208/tf-50b8.tar.gz"
   version "5.0b8"
   sha256 "3750a114cf947b1e3d71cecbe258cb830c39f3186c369e368d4662de9c50d989"
+  revision 1
 
   bottle do
     rebuild 1
@@ -16,7 +17,7 @@ class TinyFugue < Formula
   end
 
   depends_on "libnet"
-  depends_on "openssl"
+  depends_on "openssl@1.1"
   depends_on "pcre"
 
   conflicts_with "tee-clc", :because => "both install a `tf` binary"
