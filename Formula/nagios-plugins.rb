558a78b98b64ad1b65b34d0b9342ea6261ad6500
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-30 03:58:21 +0000

    nagios-plugins: update 2.2.1_1 bottle.

--- Formula/nagios-plugins.rb
+++ Formula/nagios-plugins.rb
@@ -6,11 +6,10 @@ class NagiosPlugins < Formula
   revision 1
 
   bottle do
-    sha256 "27cfda8fe9e205ff63eda487821e13d93b19900838b4ad0ebd73a86fc8c7224b" => :mojave
-    sha256 "d8f55e381e65df1be3113923dc351fac227ae17b6334a9d2c939cf346434eca9" => :high_sierra
-    sha256 "f88a0ce6fd30f875cc9654f3989b0728d2bd230e09bec994cdb8c2461a7f2166" => :sierra
-    sha256 "d9920741a2e4322d7c9fd55a87f3d7bf56f4abfa3a49f0fc9adcf408f891775a" => :el_capitan
-    sha256 "1fcf4d4934fe7f7793fb78d13f17f948d46a19600e02881b22695f736f327e65" => :yosemite
+    cellar :any
+    sha256 "bd7d15750d00a4b75d6e8c0b2fe75a5e508c00ab832c206e4cf017d53c793c68" => :mojave
+    sha256 "14e25d0a27df813595c6060767fb9e24545efe6065f6c15e2b27d3f5e704c449" => :high_sierra
+    sha256 "4119699bb703aea5a3a9369300b209587209c4d8675d22997a16ed75089e141b" => :sierra
   end
 
   depends_on "openssl@1.1"
