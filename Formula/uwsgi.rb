1faacd42b75cd9fb8ba2fee50659f767b9031479
Igor Kapkov <igasgeek@me.com>
2019-08-30 19:24:15 +1000

    uwsgi: move to OpenSSL 1.1
    Closes #43649.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/uwsgi.rb
+++ Formula/uwsgi.rb
@@ -1,6 +1,7 @@
 class Uwsgi < Formula
   desc "Full stack for building hosting services"
   homepage "https://uwsgi-docs.readthedocs.org/en/latest/"
+  revision 1
   head "https://github.com/unbit/uwsgi.git"
 
   stable do
@@ -22,7 +23,7 @@ class Uwsgi < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on "openssl"
+  depends_on "openssl@1.1"
   depends_on "pcre"
   depends_on "python@2"
   depends_on "yajl"
@@ -42,7 +43,7 @@ class Uwsgi < Formula
     end
 
     ENV.append %w[CFLAGS LDFLAGS], "-arch #{MacOS.preferred_arch}"
-    openssl = Formula["openssl"]
+    openssl = Formula["openssl@1.1"]
     ENV.prepend "CFLAGS", "-I#{openssl.opt_include}"
     ENV.prepend "LDFLAGS", "-L#{openssl.opt_lib}"
 
