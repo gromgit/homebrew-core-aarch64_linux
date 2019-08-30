c93b3e3478eefaa93b9e2deb44e56fe6aa63090d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-30 14:21:16 +0000

    htpdate: update 1.2.2 bottle.

--- Formula/htpdate.rb
+++ Formula/htpdate.rb
@@ -6,8 +6,8 @@ class Htpdate < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d47cbbcdf265b8997e5d496851e5d30d010723950616c09cc1881830967a34cb" => :mojave
-    sha256 "6c0525a59ea4312ade6e748dc4dab10798a918b1b978f0234637a95688f734f7" => :high_sierra
+    sha256 "4da5825b9f51a83c7de24d289719f0d341b79685a7e1580f2de867e53941934a" => :mojave
+    sha256 "437b8823d451f79f1ad8e2420387a3f50c3dc5919ef19717d41c437a88b77247" => :high_sierra
   end
 
   depends_on :macos => :high_sierra # needs <sys/timex.h>
