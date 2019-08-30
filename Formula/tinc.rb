69b29daaf77edb4db4974e460b2440e2af7cc7c7
FX Coudert <fxcoudert@gmail.com>
2019-08-30 11:15:42 +0200

    tinc: move to OpenSSL 1.1

--- Formula/tinc.rb
+++ Formula/tinc.rb
@@ -3,6 +3,7 @@ class Tinc < Formula
   homepage "https://www.tinc-vpn.org/"
   url "https://tinc-vpn.org/packages/tinc-1.0.35.tar.gz"
   sha256 "18c83b147cc3e2133a7ac2543eeb014d52070de01c7474287d3ccecc9b16895e"
+  revision 1
 
   bottle do
     sha256 "98c73cd30d30b74363b2093621006361da5a8738e040d9c6bf329016d7f18c3f" => :mojave
@@ -11,11 +12,11 @@ class Tinc < Formula
   end
 
   depends_on "lzo"
-  depends_on "openssl"
+  depends_on "openssl@1.1"
 
   def install
     system "./configure", "--prefix=#{prefix}", "--sysconfdir=#{etc}",
-                          "--with-openssl=#{Formula["openssl"].opt_prefix}"
+                          "--with-openssl=#{Formula["openssl@1.1"].opt_prefix}"
     system "make", "install"
   end
 
