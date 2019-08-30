0affca0b4f816da639e0c61d19b5c42988ce46e9
Sunayana Sharma <sunayana1310@gmail.com>
2019-08-30 12:31:34 -0700

    repo 1.13.5.1 (#43674)

--- Formula/repo.rb
+++ Formula/repo.rb
@@ -2,8 +2,8 @@ class Repo < Formula
   desc "Repository tool for Android development"
   homepage "https://source.android.com/source/developing.html"
   url "https://gerrit.googlesource.com/git-repo.git",
-      :tag      => "v1.13.4",
-      :revision => "745be2ede1e67421275afc00c04d996d9d6908ee"
+      :tag      => "v1.13.5.1",
+      :revision => "46702eddc7b5f38391e121e47f0ab4dbd6fbdf58"
   version_scheme 1
 
   bottle :unneeded
