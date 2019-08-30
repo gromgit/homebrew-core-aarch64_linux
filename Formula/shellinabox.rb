65b542b2d34cf66bf28fb234c19ca166aedcc495
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-30 10:26:23 +0000

    shellinabox: update 2.20 bottle.

--- Formula/shellinabox.rb
+++ Formula/shellinabox.rb
@@ -6,11 +6,9 @@ class Shellinabox < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "50777e72e0f35004b980a433631bebc7904c971bf509349d80537d788167f8d9" => :mojave
-    sha256 "4a7624626d440f813a04b80d11d04f5529ebab4c2e2079587505abc1df01ad83" => :high_sierra
-    sha256 "ad56ade520d138d05387117756a57cb9cc4c4ba1b3ca1c2adc7dec5c8d8b1cb8" => :sierra
-    sha256 "9ea1e1799cc9496ee7e391e65b1c69e693ff95bb5479c6be28138fc0f9fb9d7c" => :el_capitan
-    sha256 "2cfd35e4f9b2d28d9ba72a318d7ca48f5b6bed7c7a655a6b0437254bab268186" => :yosemite
+    rebuild 1
+    sha256 "0fb5d5bf2e9370f9826ad3326c6c5116837d062346e5526b81ad6bf34b465b3f" => :mojave
+    sha256 "6258d7d019bb81204ba0159c3b483bace0137502cc9a5a9dff9f2a5bcf0b4409" => :sierra
   end
 
   depends_on "autoconf" => :build
