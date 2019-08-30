f491ba879b3c360565051ad68a2b10fd1c3ee20c
www-data <www-data@bepsvpt.me>
2019-08-31 03:00:10 +0800

    kubectx 0.7.0
    Closes #43677.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/kubectx.rb
+++ Formula/kubectx.rb
@@ -1,8 +1,8 @@
 class Kubectx < Formula
   desc "Tool that can switch between kubectl contexts easily and create aliases"
   homepage "https://github.com/ahmetb/kubectx"
-  url "https://github.com/ahmetb/kubectx/archive/v0.6.3.tar.gz"
-  sha256 "f39eb7bc448f4444f4e3bfd1bb1258187615d51f1c4fa60d826b794b9ca70983"
+  url "https://github.com/ahmetb/kubectx/archive/v0.7.0.tar.gz"
+  sha256 "b7e13a890a3543b819b8a68a0b9802dde0bedc7609b609b54475ec2c7612d26c"
   head "https://github.com/ahmetb/kubectx.git"
 
   bottle :unneeded
