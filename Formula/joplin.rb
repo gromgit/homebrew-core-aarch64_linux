22ddeacf23fa9cc70fa89cfde23fe0e88f02eef0
Caleb Xu <calebcenter@live.com>
2019-08-29 23:19:10 -0400

    joplin: remove python2 build dependency
    Closes #43629.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/joplin.rb
+++ Formula/joplin.rb
@@ -5,6 +5,7 @@ class Joplin < Formula
   homepage "https://joplin.cozic.net/"
   url "https://registry.npmjs.org/joplin/-/joplin-1.0.141.tgz"
   sha256 "2f7c841a19a45466f9e84325e6c718b0116b450095aa6058da303321c7c225f1"
+  revision 1
 
   bottle do
     sha256 "d919343dfc489383ede02badc16b5bbb6a73cda4c1fd4a7a66a3b6e47bf0fd71" => :mojave
@@ -12,7 +13,6 @@ class Joplin < Formula
     sha256 "d0f509be6170917f2173a4dd34624d98be18845f1e70c9ae97845b6abaf9540d" => :sierra
   end
 
-  depends_on "python@2" => :build
   depends_on "node"
 
   def install
