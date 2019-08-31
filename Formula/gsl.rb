e6b080a7a2f49c7c5dd03103ba7c63d6be89f7e4
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-08-31 09:35:56 +0100

    gsl 2.6

--- Formula/gsl.rb
+++ Formula/gsl.rb
@@ -1,9 +1,9 @@
 class Gsl < Formula
   desc "Numerical library for C and C++"
   homepage "https://www.gnu.org/software/gsl/"
-  url "https://ftp.gnu.org/gnu/gsl/gsl-2.5.tar.gz"
-  mirror "https://ftpmirror.gnu.org/gsl/gsl-2.5.tar.gz"
-  sha256 "0460ad7c2542caaddc6729762952d345374784100223995eb14d614861f2258d"
+  url "https://ftp.gnu.org/gnu/gsl/gsl-2.6.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gsl/gsl-2.6.tar.gz"
+  sha256 "b782339fc7a38fe17689cb39966c4d821236c28018b6593ddb6fd59ee40786a8"
 
   bottle do
     cellar :any
@@ -14,6 +14,7 @@ class Gsl < Formula
   end
 
   def install
+    ENV.deparallelize
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make" # A GNU tool which doesn't support just make install! Shameful!
     system "make", "install"
