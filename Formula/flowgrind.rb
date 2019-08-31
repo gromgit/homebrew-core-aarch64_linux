1e5f2635a38ad1d8b60fa41889563beb5ead909e
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-08-31 14:23:40 +0100

    flowgrind: revision bump for gsl

--- Formula/flowgrind.rb
+++ Formula/flowgrind.rb
@@ -3,6 +3,7 @@ class Flowgrind < Formula
   homepage "https://launchpad.net/flowgrind"
   url "https://launchpad.net/flowgrind/trunk/flowgrind-0.8.0/+download/flowgrind-0.8.0.tar.bz2"
   sha256 "2e8b58fc919bb1dae8f79535e21931336355b4831d8b5bf75cf43eacd1921d04"
+  revision 1
 
   bottle do
     cellar :any
