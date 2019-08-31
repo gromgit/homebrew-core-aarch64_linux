226cf23958827c274f7ac205c260b65f4dfff4d3
FX Coudert <fxcoudert@gmail.com>
2019-08-31 19:39:51 +0200

    pyenv: move to OpenSSL 1.1

--- Formula/pyenv.rb
+++ Formula/pyenv.rb
@@ -3,6 +3,7 @@ class Pyenv < Formula
   homepage "https://github.com/pyenv/pyenv"
   url "https://github.com/pyenv/pyenv/archive/v1.2.13.tar.gz"
   sha256 "ebf9899f70cb04a6a6bf9835c37d9d7e4ed7dadb22dd8123b19d6d790a13fffe"
+  revision 1
   version_scheme 1
   head "https://github.com/pyenv/pyenv.git"
 
@@ -14,7 +15,7 @@ class Pyenv < Formula
   end
 
   depends_on "autoconf"
-  depends_on "openssl"
+  depends_on "openssl@1.1"
   depends_on "pkg-config"
   depends_on "readline"
 
