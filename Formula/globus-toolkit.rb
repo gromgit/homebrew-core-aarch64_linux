1be2f5c8b5fd13bff1113943807542952da33f23
FX Coudert <fxcoudert@gmail.com>
2019-08-31 15:14:48 +0200

    globus-toolkit: move to OpenSSL 1.1

--- Formula/globus-toolkit.rb
+++ Formula/globus-toolkit.rb
@@ -4,6 +4,7 @@ class GlobusToolkit < Formula
   # Note: Stable distributions have an even minor version number (e.g. 5.0.3)
   url "https://downloads.globus.org/toolkit/gt6/stable/installers/src/globus_toolkit-6.0.1531931206.tar.gz"
   sha256 "ef7b127174016627e1e161a99a95a4558b1c47fc0d368c4c3e84320924f14081"
+  revision 1
 
   bottle do
     sha256 "c9640e4f0b1829702b05fa33971fdc7dca2aa433fd1808c3ebc378b420ad3e45" => :mojave
@@ -14,7 +15,7 @@ class GlobusToolkit < Formula
 
   depends_on "pkg-config" => :build
   depends_on "libtool"
-  depends_on "openssl"
+  depends_on "openssl@1.1"
   uses_from_macos "zlib"
 
   def install
