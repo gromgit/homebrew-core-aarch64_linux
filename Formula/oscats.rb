9d3253be0493f576c3fffa3d82759b3fd2843b50
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-08-31 14:25:00 +0100

    oscats: revision bump for gsl

--- Formula/oscats.rb
+++ Formula/oscats.rb
@@ -3,7 +3,7 @@ class Oscats < Formula
   homepage "https://code.google.com/archive/p/oscats/"
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/oscats/oscats-0.6.tar.gz"
   sha256 "2f7c88cdab6a2106085f7a3e5b1073c74f7d633728c76bd73efba5dc5657a604"
-  revision 3
+  revision 4
 
   bottle do
     cellar :any
