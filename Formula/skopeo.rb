46b65dc907b7831a3acc3778ce769c776b4f2303
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-31 21:06:50 +0000

    skopeo: update 0.1.38_1 bottle.

--- Formula/skopeo.rb
+++ Formula/skopeo.rb
@@ -6,10 +6,9 @@ class Skopeo < Formula
   revision 1
 
   bottle do
-    cellar :any
-    sha256 "440a3c2544571dca44df029d903c75ef92e9cdfe31d67b36f721d52934596a42" => :mojave
-    sha256 "ff2ddfc7084e6ef15c973b4ac0e63ce3b4f9ae61effa62a061e470126f3bdd78" => :high_sierra
-    sha256 "5e96313a0b8729fd42de2914eaa1ef2cc6777b93c4ac5173356d82e903539380" => :sierra
+    sha256 "968f179e2ffacc33a4442e212d1dd789be35a250490f80fb862cd9777559134c" => :mojave
+    sha256 "9b8acfee75274b2c7ee46183e893cbd67c6dfecdad7391094d8a7c326101c413" => :high_sierra
+    sha256 "86d4a6c35cf115708e8027af5732ce5feba864112f97498b49ec354fe98b38f0" => :sierra
   end
 
   depends_on "go" => :build
