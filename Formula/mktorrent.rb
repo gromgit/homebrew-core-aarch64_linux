2274bdf308f7aa7bff9112a3ca7413d5bf838f7b
adiabatic <adiabatic@users.noreply.github.com>
2019-09-01 01:07:06 -0700

    mktorrent: update homepage (#43735)

--- Formula/mktorrent.rb
+++ Formula/mktorrent.rb
@@ -1,6 +1,6 @@
 class Mktorrent < Formula
   desc "Create BitTorrent metainfo files"
-  homepage "https://mktorrent.sourceforge.io/"
+  homepage "https://github.com/Rudde/mktorrent/wiki"
   url "https://github.com/Rudde/mktorrent/archive/v1.1.tar.gz"
   sha256 "d0f47500192605d01b5a2569c605e51ed319f557d24cfcbcb23a26d51d6138c9"
   revision 1
