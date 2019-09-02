e77e4350fd5149eebe2cfe20f4b36e0009e9ea4a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-02 14:19:35 +0000

    pwntools: update 3.12.2_1 bottle.

--- Formula/pwntools.rb
+++ Formula/pwntools.rb
@@ -9,9 +9,9 @@ class Pwntools < Formula
 
   bottle do
     cellar :any
-    sha256 "d234861d32454d58b82d6c3d8ea20b35baf24538bed05d9d0778894d39bd6405" => :mojave
-    sha256 "eac26ad4ddb18db83e848852e09a4559959a1f5ddbc00bb8d6711d8842173ae2" => :high_sierra
-    sha256 "91d82535a5683cc31032453f0a7214b1c2ff8580be30286bea2344709b99a8e0" => :sierra
+    sha256 "258e2df2d025801d2173307b68639b968f0293d3d5f9acb8c7a884e5cd639768" => :mojave
+    sha256 "3a6fa84fbdd9683af6b5800fd4fa14e8369419f3a8abc9b5e8bd23bde447c3a5" => :high_sierra
+    sha256 "60d76ec99a2646e6ea82e226aff196f45af7d209c36ed848cc29dea52b5e1c54" => :sierra
   end
 
   depends_on "openssl@1.1"
