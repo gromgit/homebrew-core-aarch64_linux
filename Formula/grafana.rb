623afb917e6eda738db4e8c716cf6175223ace94
Rui Chen <chenrui333@gmail.com>
2019-09-02 12:37:50 -0400

    grafana 6.3.5
    Closes #43779.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/grafana.rb
+++ Formula/grafana.rb
@@ -1,8 +1,8 @@
 class Grafana < Formula
   desc "Gorgeous metric visualizations and dashboards for timeseries databases"
   homepage "https://grafana.com"
-  url "https://github.com/grafana/grafana/archive/v6.3.4.tar.gz"
-  sha256 "12defd0674a9656e0146f7414fc74b12ff3934b4b6f44e3e7f1f430d88bf357e"
+  url "https://github.com/grafana/grafana/archive/v6.3.5.tar.gz"
+  sha256 "193974115bf868272dbff08e53fdc0c97af3a07a24758390178ce53e5c9e179b"
   head "https://github.com/grafana/grafana.git"
 
   bottle do
