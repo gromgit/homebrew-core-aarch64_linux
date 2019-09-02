1a23def8098c455881575d97e5eab1878a961e93
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-02 09:30:30 +0000

    libbitcoin-network: update 3.6.0_1 bottle.

--- Formula/libbitcoin-network.rb
+++ Formula/libbitcoin-network.rb
@@ -7,9 +7,9 @@ class LibbitcoinNetwork < Formula
 
   bottle do
     cellar :any
-    sha256 "c1c9981dacee0b9f10bfde83cf55f5a301b2a310ce1f4f0ca7d737f41cab9980" => :mojave
-    sha256 "c8ac3448d551ec0b92a20f3a4d5ed2393cec041e2e4d2fca56ff2e3c4ce8b2f6" => :high_sierra
-    sha256 "2184821f86e8c811bd4546b7a6829448f580d533472bc85e7cf2b2d304236392" => :sierra
+    sha256 "8e4f39b82eb02eaba960d02dd78783e70dbe190566e3b978d4000c95f3b211d8" => :mojave
+    sha256 "4f641ea3e82b0ec336172a3cfb87f4cc5750390f57fcd73fd15c85cbf6a5356f" => :high_sierra
+    sha256 "4619c4f9515111e430c671fd580fdf5c8547a75b82d1ea88ae6c2e250446c242" => :sierra
   end
 
   depends_on "autoconf" => :build
