f2750c568f55199dc03114d806c53f2f6139ba4e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-02 09:30:31 +0000

    libbitcoin-blockchain: update 3.6.0_1 bottle.

--- Formula/libbitcoin-blockchain.rb
+++ Formula/libbitcoin-blockchain.rb
@@ -7,9 +7,9 @@ class LibbitcoinBlockchain < Formula
 
   bottle do
     cellar :any
-    sha256 "381c63f70509cd74addd5981dd6f5ef48cc3fa0dcbe3153b205f408f196376d5" => :mojave
-    sha256 "ebd3efe29001f1c4e74877ab0267d8ebdc43e3222c6bc8df2e10b489b886c141" => :high_sierra
-    sha256 "cf2845efcd1676bd16ac497537915296bd6d3e7bfa6b32cf970b32d0a0ccf69d" => :sierra
+    sha256 "ef3b880069ae513b2cc91e702f7e28920acd9f24b0264049138037d692a83b6a" => :mojave
+    sha256 "0ab52ee6e7c204c4948468e551b00042cf4a3118ddc7f6eb20f71999b3be9861" => :high_sierra
+    sha256 "a399e9b0540b8a41176d852f06222f6b6e831def5618fe4cbd2555c8f2708b32" => :sierra
   end
 
   depends_on "autoconf" => :build
