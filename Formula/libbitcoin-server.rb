de49b2e0c0659acc31009d5852071976d9206d2b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-02 09:30:31 +0000

    libbitcoin-server: update 3.6.0_1 bottle.

--- Formula/libbitcoin-server.rb
+++ Formula/libbitcoin-server.rb
@@ -6,9 +6,9 @@ class LibbitcoinServer < Formula
   revision 1
 
   bottle do
-    sha256 "6aeaa77667d55c81618fae9f326036a8e748141cb542c8896d8030e9b9fd1a9e" => :mojave
-    sha256 "0bc088ef96cbf8e0278a0eea4936ffda9ce94ad0190d72c4da34e8a4d578f35a" => :high_sierra
-    sha256 "b14ca3951d8b5cd6d7285302c04932ed31d92e6eb822abfdc3300d8a2e72d4b0" => :sierra
+    sha256 "4c9b616832fa7083b809ef912a5c147a298ed5b1e9cc3dcb6080f881c39ad4f2" => :mojave
+    sha256 "5df6c725d671fcc589104afddd0cc7f20368ed12593a98978193acf3f190410f" => :high_sierra
+    sha256 "07e5cb2e67e3cf0248eeaa6efc17cac7c6bf1ed4639c3c50b24d854321e79f0a" => :sierra
   end
 
   depends_on "autoconf" => :build
