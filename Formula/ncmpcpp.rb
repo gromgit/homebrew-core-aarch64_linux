7142bc00efea1fe64968c733cacd213b5e3dc7a8
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-09-01 23:13:01 -0400

    ncmpcpp: revision for boost 1.71.0

--- Formula/ncmpcpp.rb
+++ Formula/ncmpcpp.rb
@@ -3,7 +3,7 @@ class Ncmpcpp < Formula
   homepage "https://rybczak.net/ncmpcpp/"
   url "https://rybczak.net/ncmpcpp/stable/ncmpcpp-0.8.2.tar.bz2"
   sha256 "650ba3e8089624b7ad9e4cc19bc1ac6028edb7523cc111fa1686ea44c0921554"
-  revision 6
+  revision 7
 
   bottle do
     cellar :any
