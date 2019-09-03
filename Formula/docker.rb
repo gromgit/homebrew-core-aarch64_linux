0719fa02770b7aa1cc8e3dc4bab89de980cadbc8
Michihito Shigemura <i.am.shige@gmail.com>
2019-09-04 05:21:11 +0900

    docker 19.03.2
    Closes #43828.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -2,8 +2,8 @@ class Docker < Formula
   desc "Pack, ship and run any application as a lightweight container"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/docker-ce.git",
-      :tag      => "v19.03.1",
-      :revision => "74b1e89e8ac68948be88fe0aa1e2767ae28659fe"
+      :tag      => "v19.03.2",
+      :revision => "6a30dfca03664a0b6bf0646a7d389ee7d0318e6e"
 
   bottle do
     cellar :any_skip_relocation
