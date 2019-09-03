84355ec497cded5def23e60f8c15634f10c144ee
Matt Bond <matt+git@codefire.io>
2019-09-03 16:17:47 -0400

    git-revise 0.4.2 (new formula)
    Closes #43830.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/git-revise.rb
@@ -0,0 +1,29 @@
+class GitRevise < Formula
+  include Language::Python::Virtualenv
+
+  desc "Rebase alternative for easy & efficient in-memory rebases and fixups"
+  homepage "https://github.com/mystor/git-revise"
+  url "https://github.com/mystor/git-revise/archive/0.4.2.tar.gz"
+  sha256 "217b1bb9a3b34d0c7e7544b516a9eed5bc061b9bafd3a97bc7edfd83ea54c0d6"
+  head "https://github.com/mystor/git-revise.git"
+
+  depends_on "python"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    (testpath/".gitconfig").write <<~EOS
+      [user]
+        name = J. Random Tester
+        email = test@example.com
+    EOS
+    system "git", "init"
+    (testpath/"test").write "foo"
+    system "git", "add", "test"
+    system "git", "commit", "--message", "a bad message"
+    system "git", "revise", "--message", "a good message", "HEAD"
+    assert_match "a good message", shell_output("git log --format=%B -n 1 HEAD")
+  end
+end
