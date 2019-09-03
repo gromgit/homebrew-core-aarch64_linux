80a7892e41ad3ac625aa560bb9e0c2e81b551c88
Caleb Xu <calebcenter@live.com>
2019-09-03 19:22:08 -0400

    avro-tools 1.9.1 (#43815)

--- Formula/avro-tools.rb
+++ Formula/avro-tools.rb
@@ -1,8 +1,8 @@
 class AvroTools < Formula
   desc "Avro command-line tools and utilities"
   homepage "https://avro.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=avro/avro-1.9.0/java/avro-tools-1.9.0.jar"
-  sha256 "09e2556753c0c0a6983b43a198f1bdd0487d33095bd6f4e563851e6f6e53901e"
+  url "https://www.apache.org/dyn/closer.cgi?path=avro/avro-1.9.1/java/avro-tools-1.9.1.jar"
+  sha256 "940b0865b2824fa4b85247f6f5211ace480abd13f9d87b4828ee233233713c43"
 
   bottle :unneeded
 
