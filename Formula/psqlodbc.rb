45ed37786303416cd7bd89b05b7bb043a26b2d71
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-03 08:13:13 +0000

    psqlodbc: update 10.03.0000_1 bottle.

--- Formula/psqlodbc.rb
+++ Formula/psqlodbc.rb
@@ -7,10 +7,9 @@ class Psqlodbc < Formula
 
   bottle do
     cellar :any
-    sha256 "260c8444a2a9b34d77bd08b523ff5e570006aca67527a9afdc3ae5f84c31eb7d" => :mojave
-    sha256 "e24fc6b6c219c8ee08a1448fe8fb8fe7e70cadae99d197c21838515dc96751de" => :high_sierra
-    sha256 "420b5d885afc1839b1ec764e07d28803cfe88680c233dd7833d22cd382df8c40" => :sierra
-    sha256 "846df80d05b7692bb2cf7d3fd13b9be0e720e02dabfd32c14a560385e0592895" => :el_capitan
+    sha256 "49fa960ad8d53bc332464cca31c41168b252b486da1f3aee9afe8a8937df572c" => :mojave
+    sha256 "3c06991bd75a012f7087a11940597221092f780efbf686e88770c996df05e278" => :high_sierra
+    sha256 "b9bbfb50bdc7b77fe47a6b1eebd80adaf2ae8fc2b1f5be527aeb531ddfc585fe" => :sierra
   end
 
   head do
