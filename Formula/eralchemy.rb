7e0150637fa4757933f2cb0432ddf2fe5140a550
FX Coudert <fxcoudert@gmail.com>
2019-09-04 17:02:55 +0200

    eralchemy: move to OpenSSL 1.1

--- Formula/eralchemy.rb
+++ Formula/eralchemy.rb
@@ -5,7 +5,7 @@ class Eralchemy < Formula
   homepage "https://github.com/Alexis-benoist/eralchemy"
   url "https://files.pythonhosted.org/packages/87/40/07b58c29406ad9cc8747e567e3e37dd74c0a8756130ad8fd3a4d71c796e3/ERAlchemy-1.2.10.tar.gz"
   sha256 "be992624878278195c3240b90523acb35d97453f1a350c44b4311d4333940f0d"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
@@ -16,7 +16,7 @@ class Eralchemy < Formula
 
   depends_on "pkg-config" => :build
   depends_on "graphviz"
-  depends_on "openssl"
+  depends_on "openssl@1.1"
   depends_on "python"
 
   resource "pygraphviz" do
