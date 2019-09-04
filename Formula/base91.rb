e8227bb0a91b94a19ad8e9528dd819b8e897c983
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-04 03:12:47 +0000

    base91: add 0.6.0 bottle.

--- Formula/base91.rb
+++ Formula/base91.rb
@@ -4,6 +4,13 @@ class Base91 < Formula
   url "https://downloads.sourceforge.net/base91/base91-0.6.0.tar.gz"
   sha256 "02cfae7322c1f865ca6ce8f2e0bb8d38c8513e76aed67bf1c94eab1343c6c651"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "fca64b5013c75658646a7d758365a624aa5f3a89488573222f2bbb867b04cc49" => :mojave
+    sha256 "3b9c972390a56bc2ea0be9943558018cc271802369b5b36ff0fa10391aaf1f57" => :high_sierra
+    sha256 "7d43d307ad7fb92e10b21696e4f3d5880979f12b465db614f7ecaf9e4c9d4904" => :sierra
+  end
+
   def install
     system "make"
     bin.install "base91"
