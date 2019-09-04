d114608eafd82f218912f3ae5ba3ffc7aa7b795b
FX Coudert <fxcoudert@gmail.com>
2019-09-04 17:03:06 +0200

    xrootd: move to OpenSSL 1.1

--- Formula/xrootd.rb
+++ Formula/xrootd.rb
@@ -3,6 +3,7 @@ class Xrootd < Formula
   homepage "http://xrootd.org"
   url "http://xrootd.org/download/v4.10.0/xrootd-4.10.0.tar.gz"
   sha256 "f07f85e27d72e9e8ff124173c7b53619aed8fcd36f9d6234c33f8f7fd511995b"
+  revision 1
   head "https://github.com/xrootd/xrootd.git"
 
   bottle do
@@ -13,12 +14,12 @@ class Xrootd < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "openssl"
+  depends_on "openssl@1.1"
   depends_on "readline"
 
   def install
     mkdir "build" do
-      system "cmake", "..", *std_cmake_args
+      system "cmake", "..", *std_cmake_args, "-DENABLE_PYTHON=OFF"
       system "make", "install"
     end
   end
