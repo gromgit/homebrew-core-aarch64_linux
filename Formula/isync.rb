0ff11dfedf26af868a57a88d1ac7edeb940424ae
FX Coudert <fxcoudert@gmail.com>
2019-09-04 17:02:57 +0200

    isync: move to OpenSSL 1.1 and patch

--- Formula/isync.rb
+++ Formula/isync.rb
@@ -3,6 +3,8 @@ class Isync < Formula
   homepage "https://isync.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/isync/isync/1.3.1/isync-1.3.1.tar.gz"
   sha256 "68cb4643d58152097f01c9b3abead7d7d4c9563183d72f3c2a31d22bc168f0ea"
+  revision 1
+  head "https://git.code.sf.net/p/isync/isync.git"
 
   bottle do
     cellar :any
@@ -11,18 +13,22 @@ class Isync < Formula
     sha256 "e07239359fe1f1f6b62a1c86d4f38b56e0715195b1e881a292903be17895bf6d" => :sierra
   end
 
-  head do
-    url "https://git.code.sf.net/p/isync/isync.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-  end
-
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "berkeley-db"
-  depends_on "openssl"
+  depends_on "openssl@1.1"
+
+  # Patch to fix detection of OpenSSL 1.1
+  # https://sourceforge.net/p/isync/bugs/51/
+  patch :DATA
 
   def install
-    system "./autogen.sh" if build.head?
+    # Regenerated for HEAD, and because of our patch
+    if build.head?
+      system "./autogen.sh"
+    else
+      system "autoreconf", "-fiv"
+    end
 
     args = %W[
       --disable-dependency-tracking
@@ -72,3 +78,16 @@ class Isync < Formula
     system bin/"mbsync-get-cert", "duckduckgo.com:443"
   end
 end
+__END__
+diff -pur isync-1.3.1/configure.ac isync-1.3.1-fixed/configure.ac
+--- isync-1.3.1/configure.ac	2019-05-28 15:44:13.000000000 +0200
++++ isync-1.3.1-fixed/configure.ac	2019-09-07 15:39:55.000000000 +0200
+@@ -94,7 +94,7 @@ if test "x$ob_cv_with_ssl" != xno; then
+     sav_LDFLAGS=$LDFLAGS
+     LDFLAGS="$LDFLAGS $SSL_LDFLAGS"
+     AC_CHECK_LIB(dl, dlopen, [LIBDL=-ldl])
+-    AC_CHECK_LIB(crypto, CRYPTO_lock, [LIBCRYPTO=-lcrypto])
++    AC_CHECK_LIB(crypto, HMAC_Update, [LIBCRYPTO=-lcrypto])
+     AC_CHECK_LIB(ssl, SSL_connect,
+                  [SSL_LIBS="-lssl $LIBCRYPTO $LIBDL" have_ssl_paths=yes])
+     LDFLAGS=$sav_LDFLAGS
