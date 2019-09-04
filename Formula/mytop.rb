ae8dd3c84b080abaa159405aa7be500a1e6f9eef
FX Coudert <fxcoudert@gmail.com>
2019-09-04 17:03:02 +0200

    mytop: move to OpenSSL 1.1

--- Formula/mytop.rb
+++ Formula/mytop.rb
@@ -4,7 +4,7 @@ class Mytop < Formula
   url "http://www.mysqlfanboy.com/mytop-3/mytop-1.9.1.tar.gz"
   mirror "https://deb.debian.org/debian/pool/main/m/mytop/mytop_1.9.1.orig.tar.gz"
   sha256 "179d79459d0013ab9cea2040a41c49a79822162d6e64a7a85f84cdc44828145e"
-  revision 6
+  revision 7
 
   bottle do
     cellar :any
@@ -15,7 +15,7 @@ class Mytop < Formula
   end
 
   depends_on "mysql-client"
-  depends_on "openssl"
+  depends_on "openssl@1.1"
 
   conflicts_with "mariadb", :because => "both install `mytop` binaries"
 
