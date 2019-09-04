cefebfb6d273b99a956720b946df00199211f281
FX Coudert <fxcoudert@gmail.com>
2019-09-04 22:10:39 +0200

    dnsperf: revision for openssl migration

--- Formula/dnsperf.rb
+++ Formula/dnsperf.rb
@@ -3,6 +3,7 @@ class Dnsperf < Formula
   homepage "https://www.dns-oarc.net/tools/dnsperf"
   url "https://www.dns-oarc.net/files/dnsperf/dnsperf-2.3.1.tar.gz"
   sha256 "ffefdc4610943c645b181d26843842d4890721d4da09ebb19aa7c8a5b7effd8a"
+  revision 1
 
   bottle do
     cellar :any
