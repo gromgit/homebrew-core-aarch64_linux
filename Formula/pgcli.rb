0b7e693a3ea7b5997b8e08c01a084a3b53bbc1e4
FX Coudert <fxcoudert@gmail.com>
2019-09-04 17:03:03 +0200

    pgcli: move to OpenSSL 1.1

--- Formula/pgcli.rb
+++ Formula/pgcli.rb
@@ -5,6 +5,7 @@ class Pgcli < Formula
   homepage "https://pgcli.com/"
   url "https://files.pythonhosted.org/packages/5a/2a/9821c4a30d79fafdefeff7b071639b0968e1d54710430f2da16f8124fc37/pgcli-2.1.1.tar.gz"
   sha256 "84e0817a29c3cacaaaf85351422bdb90f230f2550e4af558c25536fd235ab6ca"
+  revision 1
 
   bottle do
     cellar :any
@@ -15,7 +16,7 @@ class Pgcli < Formula
   end
 
   depends_on "libpq"
-  depends_on "openssl"
+  depends_on "openssl@1.1"
   depends_on "python"
 
   resource "backports.csv" do
