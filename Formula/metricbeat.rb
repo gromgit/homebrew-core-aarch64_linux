bbdb68234215cdcc9532dce4d3b427c569d63b85
FX Coudert <fxcoudert@gmail.com>
2019-09-04 18:20:54 +0200

    metricbeat: does not support Python 3
    Closes #43861.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/metricbeat.rb
+++ Formula/metricbeat.rb
@@ -14,7 +14,7 @@ class Metricbeat < Formula
   end
 
   depends_on "go" => :build
-  depends_on "python@2" => :build
+  depends_on "python@2" => :build # does not support Python 3
 
   resource "virtualenv" do
     url "https://files.pythonhosted.org/packages/8b/f4/360aa656ddb0f4168aeaa1057d8784b95d1ce12f34332c1cf52420b6db4e/virtualenv-16.3.0.tar.gz"
