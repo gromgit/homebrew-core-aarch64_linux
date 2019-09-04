3f277d53e5e93aa7842db3f240b3e2ea506b4047
FX Coudert <fxcoudert@gmail.com>
2019-09-04 17:03:05 +0200

    tor: move to OpenSSL 1.1

--- Formula/tor.rb
+++ Formula/tor.rb
@@ -4,6 +4,7 @@ class Tor < Formula
   url "https://www.torproject.org/dist/tor-0.4.1.5.tar.gz"
   mirror "https://www.torservers.net/mirrors/torproject.org/dist/tor-0.4.1.5.tar.gz"
   sha256 "a864e0b605fb933fcc167bf242eed4233949e8a1bf23ac8e0381b106cd920425"
+  revision 1
 
   bottle do
     sha256 "a147055612425b991e5ad92b7a5ed37ccfb48db668507ccabd78abcc9d160877" => :mojave
@@ -14,7 +15,7 @@ class Tor < Formula
   depends_on "pkg-config" => :build
   depends_on "libevent"
   depends_on "libscrypt"
-  depends_on "openssl"
+  depends_on "openssl@1.1"
 
   def install
     args = %W[
@@ -23,7 +24,7 @@ class Tor < Formula
       --prefix=#{prefix}
       --sysconfdir=#{etc}
       --localstatedir=#{var}
-      --with-openssl-dir=#{Formula["openssl"].opt_prefix}
+      --with-openssl-dir=#{Formula["openssl@1.1"].opt_prefix}
     ]
 
     system "./configure", *args
