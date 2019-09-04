ecf5f3aaa2750498fe149717a0b249ded1361bdf
FX Coudert <fxcoudert@gmail.com>
2019-09-04 17:02:55 +0200

    curl-openssl: move to OpenSSL 1.1

--- Formula/curl-openssl.rb
+++ Formula/curl-openssl.rb
@@ -3,6 +3,7 @@ class CurlOpenssl < Formula
   homepage "https://curl.haxx.se/"
   url "https://curl.haxx.se/download/curl-7.65.3.tar.bz2"
   sha256 "0a855e83be482d7bc9ea00e05bdb1551a44966076762f9650959179c89fce509"
+  revision 1
 
   bottle do
     sha256 "1fd4e12d9b493c08e289ef85df77da1836a3bfda993307873453538854f50c25" => :mojave
@@ -28,7 +29,7 @@ class CurlOpenssl < Formula
   depends_on "libssh2"
   depends_on "nghttp2"
   depends_on "openldap"
-  depends_on "openssl"
+  depends_on "openssl@1.1"
   depends_on "rtmpdump"
 
   def install
@@ -47,7 +48,7 @@ class CurlOpenssl < Formula
       --with-libmetalink
       --with-librtmp
       --with-libssh2
-      --with-ssl=#{Formula["openssl"].opt_prefix}
+      --with-ssl=#{Formula["openssl@1.1"].opt_prefix}
     ]
 
     system "./configure", *args
