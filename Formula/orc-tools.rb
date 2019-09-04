2620588265983a326c6798445b8021dae59d9c34
Rui Chen <rchen@meetup.com>
2019-09-04 09:41:35 -0400

    orc-tools 1.6.0
    Closes #43851.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/orc-tools.rb
+++ Formula/orc-tools.rb
@@ -1,8 +1,8 @@
 class OrcTools < Formula
   desc "ORC java command-line tools and utilities"
   homepage "https://orc.apache.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/apache/orc/orc-tools/1.5.6/orc-tools-1.5.6-uber.jar"
-  sha256 "c429c94e9550ca113c45315083453b2e2ceb83eca3596695b68b8cafc8248c56"
+  url "https://search.maven.org/remotecontent?filepath=org/apache/orc/orc-tools/1.6.0/orc-tools-1.6.0-uber.jar"
+  sha256 "573a61901d520649cf92797f20088f37aa12fa29adb8a34b89dc10263ddda62d"
 
   bottle :unneeded
 
