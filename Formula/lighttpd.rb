94e363eded65372baf829360bc23e8d225fa5731
FX Coudert <fxcoudert@gmail.com>
2019-09-04 17:02:59 +0200

    lighttpd: move to OpenSSL 1.1

--- Formula/lighttpd.rb
+++ Formula/lighttpd.rb
@@ -3,6 +3,7 @@ class Lighttpd < Formula
   homepage "https://www.lighttpd.net/"
   url "https://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.54.tar.xz"
   sha256 "cf14cce2254a96d8fcb6d3181e1a3c29a8f832531c3e86ff6f2524ecda9a8721"
+  revision 1
 
   bottle do
     sha256 "63c0882928954711b5d5acfa09812f061e9ecb3fc9b1b484d39711d48941483e" => :mojave
@@ -15,7 +16,7 @@ class Lighttpd < Formula
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "openldap"
-  depends_on "openssl"
+  depends_on "openssl@1.1"
   depends_on "pcre"
 
   # default max. file descriptors; this option will be ignored if the server is not started as root
