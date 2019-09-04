f9d3a56828da2ba187c6d79646f01ffdd93a334a
FX Coudert <fxcoudert@gmail.com>
2019-09-04 17:02:59 +0200

    libzdb: move to OpenSSL 1.1

--- Formula/libzdb.rb
+++ Formula/libzdb.rb
@@ -3,6 +3,7 @@ class Libzdb < Formula
   homepage "https://tildeslash.com/libzdb/"
   url "https://tildeslash.com/libzdb/dist/libzdb-3.2.tar.gz"
   sha256 "005ddf4b29c6db622e16303298c2f914dfd82590111cea7cfd09b4acf46cf4f2"
+  revision 1
 
   bottle do
     cellar :any
@@ -12,7 +13,7 @@ class Libzdb < Formula
 
   depends_on :macos => :high_sierra # C++ 17 is required
   depends_on "mysql-client"
-  depends_on "openssl"
+  depends_on "openssl@1.1"
   depends_on "postgresql"
   depends_on "sqlite"
 
