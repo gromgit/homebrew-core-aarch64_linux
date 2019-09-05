5e5bce55aa7202c1bdcce8f945a0a856e7e59ee2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-05 23:11:56 +0000

    boost-bcp: update 1.71.0 bottle.

--- Formula/boost-bcp.rb
+++ Formula/boost-bcp.rb
@@ -7,9 +7,9 @@ class BoostBcp < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e8bc92d0fd7a062612c2e19ea8993b30ba583fb92a4bc45467092dca5e93bca4" => :mojave
-    sha256 "dba120d947a05ceed5a225c23a2a67dd97c4fbffcf45e45c19bd47cad0730a2a" => :high_sierra
-    sha256 "d2f3b79542144ebc71b239084a5bab68d6aef7637d7545fa8e70d14eea5e98ee" => :sierra
+    sha256 "5573988c2215550bebe8c27b521f5b1a0942d66da9f151f3d9770d9b7a8638e2" => :mojave
+    sha256 "c2df115f346bdf5702951d9ad0107f9cbfd424dfb643266c5cf38582098c71a0" => :high_sierra
+    sha256 "46fb8269a86b79eb2d1426432cd5bbdefeac9e7d5414c1a161d912a403708403" => :sierra
   end
 
   depends_on "boost-build" => :build
