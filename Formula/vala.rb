11059702221e4d71955e0bd120bffee54ce4db48
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-05 17:34:20 +0000

    vala: update 0.46.0 bottle.

--- Formula/vala.rb
+++ Formula/vala.rb
@@ -5,9 +5,9 @@ class Vala < Formula
   sha256 "a10a3b9d60bdfe3349afd3778cd4518b1eb517b59134a70ab5ac845432220c17"
 
   bottle do
-    sha256 "5e9813b3a4b8099d839e16675bd6b7d145790ea08eb3eb0225395a59102d4dd9" => :mojave
-    sha256 "90377134310f96133cf21f59d30c3f9ed285ec4269d98f2ff27c49b233e37a0a" => :high_sierra
-    sha256 "59b417266c2fda8ae96fe9c150a91f53657de905ba43c114e7418a859b495a42" => :sierra
+    sha256 "dad27d9adb3a07b4e442bae8356a6bd51fbc1ee81853ddc96d30ca168e294ae2" => :mojave
+    sha256 "25e9352121aaacc6ec671b855dd7a445b0be1feeea82b13fa01d3d751bfa0eef" => :high_sierra
+    sha256 "550a8685b78ef3477d8d87b575ee24b89e3a120a6631ab9be17765e5235bc86c" => :sierra
   end
 
   depends_on "gettext"
