6450881e0aa3888578b29161dd7135f1f6777cb0
FX Coudert <fxcoudert@gmail.com>
2019-09-05 10:28:30 +0200

    vim@7.4: revision for openssl

--- Formula/vim@7.4.rb
+++ Formula/vim@7.4.rb
@@ -3,7 +3,7 @@ class VimAT74 < Formula
   homepage "https://www.vim.org/"
   url "https://github.com/vim/vim/archive/v7.4.2367.tar.gz"
   sha256 "a9ae4031ccd73cc60e771e8bf9b3c8b7f10f63a67efce7f61cd694cd8d7cda5c"
-  revision 23
+  revision 24
 
   bottle do
     sha256 "c2406501c95ff378f8c1d207bd6b44e66338dbe1ece72ba0ae0b3d08cb750488" => :mojave
