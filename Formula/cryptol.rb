c2d9694e3ff2e1e58cd295494bdb1c8588fb0e6a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-05 04:50:46 +0000

    cryptol: update 2.8.0 bottle.

--- Formula/cryptol.rb
+++ Formula/cryptol.rb
@@ -11,9 +11,9 @@ class Cryptol < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c47c79277b44e3155acf83f2e9bbceeeaac0ffdf1faa511bf2468cf73b2ca441" => :mojave
-    sha256 "106a4ca34518a4a19695b96b5966c2016b8616a6c17e2d6d1413a4549840ca69" => :high_sierra
-    sha256 "b5966fba039bc47a91f7dd0aaea238dffae0e6417e34dbc8fa5ac517211b4487" => :sierra
+    sha256 "0aca3e2c29be5d4533e6114f0e7fd774173358f92d0f4e73903d0d536fb54160" => :mojave
+    sha256 "04d3178e67b8836a720d82dc3b88d1c69366aae6f0e7abdb9a1155b7dc31c28c" => :high_sierra
+    sha256 "3c3ffec1e47196b6c1767086fb9cc62e792546476cad5c6b92896eb45db13744" => :sierra
   end
 
   depends_on "cabal-install" => :build
