a17d0462da465a70e53152e518113de1cd944683
Thierry Moisan <thierry.moisan@gmail.com>
2019-09-04 22:40:30 -0400

    flyway 6.0.1
    Closes #43884.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/5.2.4/flyway-commandline-5.2.4.tar.gz"
-  sha256 "ac42a414e316d7592c7035cd7fa14050ed02c77972196367ab8f11b9a9352a9a"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/6.0.1/flyway-commandline-6.0.1.tar.gz"
+  sha256 "30661fa5e0809b97d88ecab870a06a0c6869b340fa4f2a34f0fa3ed5375c66fd"
 
   bottle :unneeded
 
