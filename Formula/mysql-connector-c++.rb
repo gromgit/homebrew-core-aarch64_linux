bb336107af5b28498ac1a70310a8ef48ddf25cef
FX Coudert <fxcoudert@gmail.com>
2019-09-06 12:23:13 +0200

    mysql-connector-c++ 8.0.17
    Closes #43938.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/mysql-connector-c++.rb
+++ Formula/mysql-connector-c++.rb
@@ -1,9 +1,8 @@
 class MysqlConnectorCxx < Formula
   desc "MySQL database connector for C++ applications"
   homepage "https://dev.mysql.com/downloads/connector/cpp/"
-  url "https://cdn.mysql.com/Downloads/Connector-C++/mysql-connector-c++-1.1.9.tar.gz"
-  sha256 "3e31847a69a4e5c113b7c483731317ec4533858e3195d3a85026a0e2f509d2e4"
-  revision 2
+  url "https://dev.mysql.com/get/Downloads/Connector-C++/mysql-connector-c++-8.0.17-src.tar.gz"
+  sha256 "69de69f373a7c5ddf291c15c52af83d634e0b900cb1204eddb3834836afe7dbe"
 
   bottle do
     cellar :any
@@ -19,24 +18,29 @@ class MysqlConnectorCxx < Formula
   depends_on "openssl"
 
   def install
-    system "cmake", ".", *std_cmake_args
+    system "cmake", ".", *std_cmake_args,
+                    "-DINSTALL_LIB_DIR=lib"
     system "make", "install"
   end
 
   test do
     (testpath/"test.cpp").write <<~EOS
-      #include <cppconn/driver.h>
-      int main(void) {
-        try {
-          sql::Driver *driver = get_driver_instance();
-        } catch (sql::SQLException &e) {
-          return 1;
-        }
+      #include <iostream>
+      #include <mysqlx/xdevapi.h>
+      int main(void)
+      try {
+        ::mysqlx::Session sess("mysqlx://root@127.0.0.1");
+        return 1;
+      }
+      catch (const mysqlx::Error &err)
+      {
+        ::std::cout <<"ERROR: " << err << ::std::endl;
         return 0;
       }
     EOS
-    system ENV.cxx, "test.cpp", "-I#{Formula["mysql-client"].opt_include}",
-                    "-L#{lib}", "-lmysqlcppconn", "-o", "test"
-    system "./test"
+    system ENV.cxx, "test.cpp", "-std=c++11", "-I#{include}",
+                    "-L#{lib}", "-lmysqlcppconn8", "-o", "test"
+    output = shell_output("./test")
+    assert_match "Connection refused", output
   end
 end
