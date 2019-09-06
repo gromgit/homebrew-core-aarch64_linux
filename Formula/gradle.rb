ff9eb9e79f6ee4c5636e85ab67cb551e3a430221
Gary Hale <ghhale@computer.org>
2019-09-06 06:37:33 -0400

    gradle 5.6.2 (#43905)

--- Formula/gradle.rb
+++ Formula/gradle.rb
@@ -1,8 +1,8 @@
 class Gradle < Formula
   desc "Open-source build automation tool based on the Groovy and Kotlin DSL"
   homepage "https://www.gradle.org/"
-  url "https://services.gradle.org/distributions/gradle-5.6.1-all.zip"
-  sha256 "f6ea7f48e2823ca7ff8481044b892b24112f5c2c3547d4f423fb9e684c39f710"
+  url "https://services.gradle.org/distributions/gradle-5.6.2-all.zip"
+  sha256 "027fdd265d277bae65a0d349b6b8da02135b0b8e14ba891e26281fa877fe37a2"
 
   bottle :unneeded
 
