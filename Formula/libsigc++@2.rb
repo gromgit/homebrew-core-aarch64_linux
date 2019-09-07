25194991a90fd4f39a0a4c451529156c73c313a0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-07 16:47:35 +0000

    libsigc++@2: add 2.10.2 bottle.

--- Formula/libsigc++@2.rb
+++ Formula/libsigc++@2.rb
@@ -4,6 +4,13 @@ class LibsigcxxAT2 < Formula
   url "https://download.gnome.org/sources/libsigc++/2.10/libsigc++-2.10.2.tar.xz"
   sha256 "b1ca0253379596f9c19f070c83d362b12dfd39c0a3ea1dd813e8e21c1a097a98"
 
+  bottle do
+    cellar :any
+    sha256 "cfb591da46f83d5242f860533bb509e4966b32f45d96abd1aebe60ccce536de6" => :mojave
+    sha256 "81130d69dd341e31f941f9d457745bbefb4eb59427a81c014e5cf6c1c8b861ad" => :high_sierra
+    sha256 "d9c71c15b7d4d244aa74aa319a14f0bba110ca97160abe035a461409b2b1630b" => :sierra
+  end
+
   def install
     ENV.cxx11
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
