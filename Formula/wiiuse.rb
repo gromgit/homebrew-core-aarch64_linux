e9b3b7b4a51cd7f03c8bdda4385bf95126f3939d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-07 11:29:49 +0000

    wiiuse: add 0.15.4 bottle.

--- Formula/wiiuse.rb
+++ Formula/wiiuse.rb
@@ -4,6 +4,13 @@ class Wiiuse < Formula
   url "https://github.com/wiiuse/wiiuse/archive/0.15.4.tar.gz"
   sha256 "45be974acc418b8c8e248d960f3c0da143a513f6404a9c5cc5aa0072934b0cc4"
 
+  bottle do
+    cellar :any
+    sha256 "81f9c831744a302586fa3158fe29127aaeeac73e0d5c074425abda85ecf2f00c" => :mojave
+    sha256 "1a6064dd68d05c22b3078371569eec25311ff222f795c2eb2dc71848089b3230" => :high_sierra
+    sha256 "c14e2c7d0a576284818879a756b0da9dd0bf4d9c789d301b69afe3a0f7c9bc17" => :sierra
+  end
+
   depends_on "cmake" => :build
 
   def install
