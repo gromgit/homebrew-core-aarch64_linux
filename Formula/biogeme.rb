71f970717bf6ee9ad141823533f5e4b8c065e39f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-07 16:47:33 +0000

    biogeme: update 2.6a_5 bottle.

--- Formula/biogeme.rb
+++ Formula/biogeme.rb
@@ -7,10 +7,9 @@ class Biogeme < Formula
 
   bottle do
     cellar :any
-    sha256 "058c5338d43d7a1b8a174583c6ab6dfe91fff41f476a6bd1a7561ec7758416db" => :mojave
-    sha256 "9e0f2c7228e239a0476147fb818ba1bb635443ee6f4d776fe7879dfa2faa5936" => :high_sierra
-    sha256 "aa71ee8900b00619fe1ac171c0196b3a4f56c4e1c1d7cecfd2230b7e289bf141" => :sierra
-    sha256 "cceacdb78b3866310b0dbe2cf59148ab9b0d2c398f2f5f13bea6a218ef69cc46" => :el_capitan
+    sha256 "669b97da46ec4e508169b764b6c801682f9282702ec6f17d32f9e4b7426cf8dc" => :mojave
+    sha256 "b90e3f0d203a5ad33d2ad1f70e12503a93784bb4a97d78b284c0d4c746666ea5" => :high_sierra
+    sha256 "cad38740685b800f07bece9dd13238b900427155697582fc689bd3eee42e8c38" => :sierra
   end
 
   depends_on "gtkmm3"
