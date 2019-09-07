df443c76fc1e4db6f1e09e5989c0217c5cbf3e4d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-07 16:47:35 +0000

    graph-tool: update 2.29_1 bottle.

--- Formula/graph-tool.rb
+++ Formula/graph-tool.rb
@@ -8,7 +8,7 @@ class GraphTool < Formula
   revision 1
 
   bottle do
-    sha256 "0d76deae45f5596c67e72c05963870e2d17d72cefdd2c71338a0f9688a018ee7" => :mojave
+    sha256 "1f996b31dfb36bd8cb6a0e8162df27820fdebadd76f8d6650e6b09f5f61576e1" => :mojave
   end
 
   depends_on "pkg-config" => :build
