1791897d0f682ef8a4b219c1e13e6743cefa38c1
Rui Chen <chenrui333@gmail.com>
2019-09-06 23:25:53 -0400

    helmfile 0.83.0
    Closes #43971.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/helmfile.rb
+++ Formula/helmfile.rb
@@ -1,8 +1,8 @@
 class Helmfile < Formula
   desc "Deploy Kubernetes Helm Charts"
   homepage "https://github.com/roboll/helmfile"
-  url "https://github.com/roboll/helmfile/archive/v0.82.0.tar.gz"
-  sha256 "68a69b7a3ed999c2699d672fb0a152f18703298a5edc982d338590c140c2d64a"
+  url "https://github.com/roboll/helmfile/archive/v0.83.0.tar.gz"
+  sha256 "859202104e1a5cb01c69a971c0b131764154139a224bc4082fd7e789cf05ec11"
 
   bottle do
     cellar :any_skip_relocation
@@ -13,13 +13,6 @@ class Helmfile < Formula
 
   depends_on "go" => :build
   depends_on "kubernetes-helm"
-
-  # See https://github.com/roboll/helmfile/issues/834
-  # Remove this patch in the next version.
-  patch do
-    url "https://github.com/roboll/helmfile/commit/1823bb1e5b159dfd8198704722d63ec6db40b4ee.patch?full_index=1"
-    sha256 "4d37b12b9565d45c2c885277b54cf8c799263dbbd8d2d165f13da095dc38dfe6"
-  end
 
   def install
     ENV["GOPATH"] = buildpath
