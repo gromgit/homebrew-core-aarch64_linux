b6c218e9c2289a15f9765dc3aaf2cc7c8e429e47
Caleb Xu <calebcenter@live.com>
2019-09-07 01:41:07 -0400

    libogg 1.3.4
    Closes #43986.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libogg.rb
+++ Formula/libogg.rb
@@ -1,8 +1,17 @@
 class Libogg < Formula
   desc "Ogg Bitstream Library"
   homepage "https://www.xiph.org/ogg/"
-  url "https://downloads.xiph.org/releases/ogg/libogg-1.3.3.tar.gz"
-  sha256 "c2e8a485110b97550f453226ec644ebac6cb29d1caef2902c007edab4308d985"
+
+  stable do
+    url "https://downloads.xiph.org/releases/ogg/libogg-1.3.4.tar.gz"
+    sha256 "fe5670640bd49e828d64d2879c31cb4dde9758681bb664f9bdbf159a01b0c76e"
+
+    # os_types: fix unsigned typedefs for MacOS. This is already merged upstream; remove on next version
+    patch do
+      url "https://github.com/xiph/ogg/commit/c8fca6b4a02d695b1ceea39b330d4406001c03ed.patch?full_index=1"
+      sha256 "0f4d289aecb3d5f7329d51f1a72ab10c04c336b25481a40d6d841120721be485"
+    end
+  end
 
   bottle do
     cellar :any
