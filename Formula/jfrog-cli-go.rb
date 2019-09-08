9ebf9d16da62f6993d6863391fce05f61facafb4
Igor Kapkov <igasgeek@me.com>
2019-09-08 14:39:06 +1000

    jfrog-cli-go: new url
    Closes #44008.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/jfrog-cli-go.rb
+++ Formula/jfrog-cli-go.rb
@@ -1,7 +1,7 @@
 class JfrogCliGo < Formula
   desc "Command-line interface for Jfrog Artifactory and Bintray"
   homepage "https://github.com/jfrog/jfrog-cli"
-  url "https://github.com/JFrog/jfrog-cli-go/archive/1.28.0.tar.gz"
+  url "https://github.com/jfrog/jfrog-cli/archive/1.28.0.tar.gz"
   sha256 "b0018994ad68fb7e6ed3938f291b3babfb3d0e975c4c5b18b9e097b8d97b84e3"
 
   bottle do
@@ -16,9 +16,10 @@ class JfrogCliGo < Formula
   def install
     ENV["GO111MODULE"] = "on"
     ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/jfrog/jfrog-cli-go").install Dir["*"]
 
-    cd "src/github.com/jfrog/jfrog-cli-go" do
+    src = buildpath/"src/github.com/jfrog/jfrog-cli-go"
+    src.install buildpath.children
+    src.cd do
       system "go", "run", "./python/addresources.go"
       system "go", "build", "-o", bin/"jfrog", "-ldflags", "-s -w -extldflags '-static'"
       prefix.install_metafiles
