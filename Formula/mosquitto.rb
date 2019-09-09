5944d81390bd996f4d05f9d8989c0c5a10a55e35
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-09 05:15:06 +0000

    mosquitto: update 1.6.4_2 bottle.

--- Formula/mosquitto.rb
+++ Formula/mosquitto.rb
@@ -7,9 +7,9 @@ class Mosquitto < Formula
 
   bottle do
     cellar :any
-    sha256 "af872a69b14e4afb695e0a1b3b5cbab1c173745786f8ca50b96c97fa69a3bdf3" => :mojave
-    sha256 "b6aa2771c20eb913832555dac11f82ba2708cd6d635726c763a759e4db05db6d" => :high_sierra
-    sha256 "44f0b813a9e12316a893c933e5c86dc30433a89b580f781c89f4f0db35adfd4a" => :sierra
+    sha256 "a9fe32d552244cb10c2fa09302d5f5e3b749a93dac2012ea8729cc5d5b9a77ef" => :mojave
+    sha256 "11a11f22a00c2c940c017af3be16b02c8bb007c25aa05b6ec6a08c8f9e1398c6" => :high_sierra
+    sha256 "9ad9ae666ce4f6a5409b6c002ec0b17640a6e3985f3718d5fd8fd40a330d7d71" => :sierra
   end
 
   depends_on "cmake" => :build
