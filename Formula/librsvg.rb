8a0115abab5779f25b434f8fea76634f136d9796
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-09 21:42:13 +0000

    librsvg: update 2.46.0 bottle.

--- Formula/librsvg.rb
+++ Formula/librsvg.rb
@@ -5,9 +5,9 @@ class Librsvg < Formula
   sha256 "96c81e52cb81450f3b2e915e6409fd7d1e3c01e4661974b3a98c09a7c45743d1"
 
   bottle do
-    sha256 "5a1e396d53f515d9316f23ad00596465afc6c61e9fa172adc356bd922cad7d81" => :mojave
-    sha256 "ad1b4d381be8601021f8705882f928235631469896c0b37ac2bb61befce3162c" => :high_sierra
-    sha256 "b54edacf2719d7f72bc2b9acf9b9ac9b0d03a6332d9a1a81d5a06545af4ac531" => :sierra
+    sha256 "32f1166d60d8433139f6ac9f27f481e3198d3a0c1c9bd63c152393b1d2a6b6ab" => :mojave
+    sha256 "6428d46ad241fbc3b1481e4d0d903f1a1e65035dbcdd5df17f7c4c40d7693fc5" => :high_sierra
+    sha256 "75d7cef5779c1bd14e7a603111957c836bee21cf24e83e5d5b9ad9cd9ae91bed" => :sierra
   end
 
   depends_on "gobject-introspection" => :build
