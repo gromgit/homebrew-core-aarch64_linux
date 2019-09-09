18975f5aa3e53b92059e74c5dd988e73da2edc76
Alexander M. Turek <me@derrabus.de>
2019-09-09 17:10:33 +0200

    php-cs-fixer 2.15.3
    Closes #44059.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/php-cs-fixer.rb
+++ Formula/php-cs-fixer.rb
@@ -1,9 +1,9 @@
 class PhpCsFixer < Formula
   desc "Tool to automatically fix PHP coding standards issues"
   homepage "https://cs.sensiolabs.org/"
-  url "https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v2.15.1/php-cs-fixer.phar"
-  version "2.15.1"
-  sha256 "8c56b58fd3733f08c160fe60b18536f904f93088954c518b7c0bc96de26c7861"
+  url "https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v2.15.3/php-cs-fixer.phar"
+  version "2.15.3"
+  sha256 "8c94ede44b66560c3867ed10ce42ec8e4eb8ce0bf7511a0802ad9963864f6c41"
 
   bottle :unneeded
 
