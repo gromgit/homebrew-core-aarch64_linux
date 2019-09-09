5c40fbccf542a95084582f5c3049be3ff186b260
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-09 05:15:05 +0000

    fabric: update 2.5.0_1 bottle.

--- Formula/fabric.rb
+++ Formula/fabric.rb
@@ -10,9 +10,9 @@ class Fabric < Formula
 
   bottle do
     cellar :any
-    sha256 "54a3ab2e1f45a4f7db5245b01bb7b2e17fa207793d57cffecd8035a7a24c6434" => :mojave
-    sha256 "e64c8ee94a51e10e1b4dff7c837a09a05ae0ef0470d9cd67314ebd2d991677c0" => :high_sierra
-    sha256 "07cf0556c6e5b8593ab9839afaee77794aac3353770201aae9cefa219c759155" => :sierra
+    sha256 "0bc0320b14d683b44e4b5e1c1dc044be63cd322956c73fe52a42ad5828f9d434" => :mojave
+    sha256 "92cc6c5ea7b59c3b62e8264ee542c1508d69bbf483446151f169a1808fa0c182" => :high_sierra
+    sha256 "8e141cc1941ce0cc0f44b05a5cf4faa72d34b8cc151411dfa26c4c653bf21a2d" => :sierra
   end
 
   depends_on "openssl@1.1"
