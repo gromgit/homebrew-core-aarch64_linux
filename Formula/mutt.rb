562340395d0a876356cf7e5be4502023335004f5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-09 19:05:57 +0000

    mutt: update 1.12.1_2 bottle.

--- Formula/mutt.rb
+++ Formula/mutt.rb
@@ -15,9 +15,10 @@ class Mutt < Formula
   revision 2
 
   bottle do
-    sha256 "60e75a481a2b25d50d0c404a14233e79579ee61e72cb14769c0a021dabf81428" => :mojave
-    sha256 "7d51106017603dec20a15e483b8aca629b3d4d34e3aa92094842c2b8f8ef83d2" => :high_sierra
-    sha256 "ae8f34c0378d4e1f40f31bbe841f4ee89fe26774305ad1c4a42d090b8b9f11ec" => :sierra
+    rebuild 1
+    sha256 "ae1e8d0cf5154dd010ce6465cb192fdf2819b6c30fba0cca635973e4a932e64d" => :mojave
+    sha256 "26a4ae02aa0cb91ab2af02ff27f17f35240480c5bc5e128ad64af94d73ef95a0" => :high_sierra
+    sha256 "1bf61c1c880829d92e5824262ef0db664ab07e6c117cfcf02b9a6dee4a92e5f3" => :sierra
   end
 
   head do
