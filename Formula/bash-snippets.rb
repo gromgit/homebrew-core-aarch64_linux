6d6ad58b729882f2010493ce47273df2a53f86ce
Thierry Moisan <thierry.moisan@gmail.com>
2019-09-08 20:53:41 -0400

    bash-snippets 1.23.0
    Closes #44031.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/bash-snippets.rb
+++ Formula/bash-snippets.rb
@@ -1,8 +1,8 @@
 class BashSnippets < Formula
   desc "Collection of small bash scripts for heavy terminal users"
   homepage "https://github.com/alexanderepstein/Bash-Snippets"
-  url "https://github.com/alexanderepstein/Bash-Snippets/archive/v1.22.1.tar.gz"
-  sha256 "72a4d11ddd2e8e1c48fcee816cd43a34e34348102474c6b76c9cb31d1e95c8eb"
+  url "https://github.com/alexanderepstein/Bash-Snippets/archive/v1.23.0.tar.gz"
+  sha256 "59b784e714ba34a847b6a6844ae1703f46db6f0a804c3e5f2de994bbe8ebe146"
 
   bottle :unneeded
 
@@ -14,9 +14,9 @@ class BashSnippets < Formula
 
   test do
     output = shell_output("#{bin}/weather Paramus").lines.first.chomp
-    assert_equal "Weather report: Paramus, United States of America", output
+    assert_equal "Weather report: Paramus", output
     output = shell_output("#{bin}/qrify This is a test")
     assert_match "████ ▄▄▄▄▄ █▀ █▀▄█ ▄▄▄▄▄ ████", output
-    assert_match "AAPL stock info", shell_output("#{bin}/stocks Apple")
+    assert_match "Apple Inc. -- Stock Information", shell_output("#{bin}/stocks Apple")
   end
 end
