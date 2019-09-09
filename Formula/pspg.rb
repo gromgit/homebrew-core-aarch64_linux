a73ae65dd654e53cd6d25d05e931b397b468f4d8
Igor Kapkov <igasgeek@me.com>
2019-09-10 08:18:38 +1000

    pspg 2.0.1
    Closes #44083.
    
    Signed-off-by: Igor Kapkov <igasgeek@me.com>

--- Formula/pspg.rb
+++ Formula/pspg.rb
@@ -1,8 +1,8 @@
 class Pspg < Formula
   desc "Unix pager optimized for psql"
   homepage "https://github.com/okbob/pspg"
-  url "https://github.com/okbob/pspg/archive/1.9.0.tar.gz"
-  sha256 "fad790b404170e828673a86f9a657b2191df63102ce37b248e57a8cc7f169285"
+  url "https://github.com/okbob/pspg/archive/2.0.1.tar.gz"
+  sha256 "9dc539b4acde5a11adc90a2017dd9a26fd32636fc1109c49dfdf4283e224144b"
   head "https://github.com/okbob/pspg.git"
 
   bottle do
