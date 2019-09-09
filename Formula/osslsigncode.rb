a9b942b2ca44991eeffbd30beb7342de6051b347
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-09 14:57:17 +0000

    osslsigncode: update 2.0 bottle.

--- Formula/osslsigncode.rb
+++ Formula/osslsigncode.rb
@@ -6,10 +6,9 @@ class Osslsigncode < Formula
 
   bottle do
     cellar :any
-    rebuild 2
-    sha256 "ce350145e5e0d0b3dc19042ccca189ec124519a4c5b5cd67319d7f1d517e1a1e" => :mojave
-    sha256 "987a7f92ca9d86ecc687e6cdb7758a1c87db8d15e0307c9f6ad3c392ccc63f4b" => :high_sierra
-    sha256 "1fcfa6be97026cd27145c79289c10c19ab123bd50ff682ed57236475368dd5cb" => :sierra
+    sha256 "372930dce3e97d7ff42ceb60c21996b98bc05eeab16c4badf7d224ae9c3bc3b2" => :mojave
+    sha256 "77be2ee5af3ae642658118eac2fad889b48995fcfb183ff0602462467ba0cb22" => :high_sierra
+    sha256 "a430c3f1cdb8e9be02aa18c89483309431e77ae4daf3b8288e1680fe60e129c8" => :sierra
   end
 
   depends_on "autoconf" => :build
