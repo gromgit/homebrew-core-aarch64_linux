36ea2d94d66e673e1a7e8b025f1245683677cbf4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-09 20:09:31 +0000

    baobab: update 3.34.0 bottle.

--- Formula/baobab.rb
+++ Formula/baobab.rb
@@ -5,9 +5,9 @@ class Baobab < Formula
   sha256 "46ebd9466da6a68c340653e9095f1e905b6fac79305879a9e644634f7da98607"
 
   bottle do
-    sha256 "720156b89e707645562c7d4bbe6f630dd85a371588a69dca181356d21d445500" => :mojave
-    sha256 "754b24a65120fc1aa7d289fc357ed45c7305e802d5d0d71962bd8fde886b3d67" => :high_sierra
-    sha256 "881bb64db834d11db421bd419195c4bda0b750094f24cbd4dbdb469684af7d88" => :sierra
+    sha256 "5d067135c6485d32ab4bf69cf70471e2f3e97b7ebe085ca530437bcdaad6e646" => :mojave
+    sha256 "ecf65805b45f5e0b7bc1f88248544c1bec9fa97809af03d7f04756d2e4882f3f" => :high_sierra
+    sha256 "c2cc7e5bc165be0e0bf96ec9e63deefc204a75ce345a3f8b723577647f35dfec" => :sierra
   end
 
   depends_on "itstool" => :build
