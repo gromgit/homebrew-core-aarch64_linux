caccf2dcf709cffae28413e547839345028750b3
Alexander Bayandin <a.bayandin@gmail.com>
2019-09-10 15:53:57 +0100

    libtensorflow 2.0.0

--- Formula/libtensorflow.rb
+++ Formula/libtensorflow.rb
@@ -3,8 +3,8 @@ class Libtensorflow < Formula
 
   desc "C interface for Google's OS library for Machine Intelligence"
   homepage "https://www.tensorflow.org/"
-  url "https://github.com/tensorflow/tensorflow/archive/v1.14.0.tar.gz"
-  sha256 "aa2a6a1daafa3af66807cfe0bc77bfe1144a9a53df9a96bab52e3e575b3047ed"
+  url "https://github.com/tensorflow/tensorflow/archive/v2.0.0.tar.gz"
+  sha256 "49b5f0495cd681cbcb5296a4476853d4aea19a43bdd9f179c928a977308a0617"
 
   bottle do
     cellar :any
@@ -17,21 +17,6 @@ class Libtensorflow < Formula
   depends_on :java => ["1.8", :build]
   depends_on "python" => :build
 
-  # Allow libtensorflow to be built on bazel 0.28.0 (currently the latest available version)
-  # this patch a consolidation of the following patches
-  # - https://github.com/tensorflow/tensorflow/commit/c33055c44226499e99270ed05265ae3b960210c9 (from 0.25.2 to 0.25.3)
-  # - https://github.com/tensorflow/tensorflow/commit/040546fdefc5a111496dcd7be97b7d65594f9119 (from 0.25.3 to 0.26.0)
-  # - https://github.com/tensorflow/tensorflow/commit/c3260eb5ab384e4d03e7e8c840712380492b26d9 (from 0.26.0 to 0.26.1)
-  # - Bump bazel from 0.26.1 to 0.28.0
-  patch :DATA
-
-  # Upgrade protobuf to 3.8.0
-  # The custom commit contains a fix to make protobuf.bzl compatible with Bazel 0.26 or later version.
-  patch do
-    url "https://github.com/tensorflow/tensorflow/commit/508f76b1d9925304cedd56d51480ec380636cb82.diff?full_index=1"
-    sha256 "89f09f266ee56ee583cfffb8b4ce9333f181f497f7e04a672a68a8b611d21270"
-  end
-
   def install
     venv_root = "#{buildpath}/venv"
     virtualenv_create(venv_root, "python3")
@@ -41,6 +26,7 @@ class Libtensorflow < Formula
 
     ENV["PYTHON_BIN_PATH"] = "#{venv_root}/bin/python"
     ENV["CC_OPT_FLAGS"] = "-march=native"
+    ENV["TF_IGNORE_MAX_BAZEL_VERSION"] = "1"
     ENV["TF_NEED_JEMALLOC"] = "1"
     ENV["TF_NEED_GCP"] = "0"
     ENV["TF_NEED_HDFS"] = "0"
@@ -58,11 +44,19 @@ class Libtensorflow < Formula
     ENV["TF_NEED_ROCM"] = "0"
     ENV["TF_DOWNLOAD_CLANG"] = "0"
     ENV["TF_SET_ANDROID_WORKSPACE"] = "0"
+    ENV["TF_CONFIGURE_IOS"] = "0"
     system "./configure"
 
     system "bazel", "build", "--jobs", ENV.make_jobs, "--compilation_mode=opt", "--copt=-march=native", "tensorflow:libtensorflow.so"
     lib.install Dir["bazel-bin/tensorflow/*.so*", "bazel-bin/tensorflow/*.dylib*"]
-    (include/"tensorflow/c").install ["tensorflow/c/c_api.h", "tensorflow/c/c_api_experimental.h", "tensorflow/c/tf_attrtype.h"]
+    (include/"tensorflow/c").install %w[
+      tensorflow/c/c_api.h
+      tensorflow/c/c_api_experimental.h
+      tensorflow/c/tf_attrtype.h
+      tensorflow/c/tf_datatype.h
+      tensorflow/c/tf_status.h
+      tensorflow/c/tf_tensor.h
+    ]
 
     (lib/"pkgconfig/tensorflow.pc").write <<~EOS
       Name: tensorflow
@@ -85,18 +79,3 @@ class Libtensorflow < Formula
     assert_equal version, shell_output("./test_tf")
   end
 end
-
-__END__
-diff --git a/configure.py b/configure.py
-index 43af22de88..7b0d99f04d 100644
---- a/configure.py
-+++ b/configure.py
-@@ -1383,7 +1383,7 @@ def main():
-   # environment variables.
-   environ_cp = dict(os.environ)
-
--  current_bazel_version = check_bazel_version('0.24.1', '0.25.2')
-+  current_bazel_version = check_bazel_version('0.24.1', '0.28.0')
-   _TF_CURRENT_BAZEL_VERSION = convert_version_to_int(current_bazel_version)
-
-   reset_tf_configure_bazelrc()
