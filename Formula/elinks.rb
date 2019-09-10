253b0e302df0fc1c3e8cab165d7cc1d5655f4464
FX Coudert <fxcoudert@gmail.com>
2019-09-10 14:58:10 +0200

    elinks: patch for OpenSSL 1.1

--- Formula/elinks.rb
+++ Formula/elinks.rb
@@ -3,7 +3,7 @@ class Elinks < Formula
   homepage "http://elinks.or.cz/"
   url "http://elinks.or.cz/download/elinks-0.11.7.tar.bz2"
   sha256 "456db6f704c591b1298b0cd80105f459ff8a1fc07a0ec1156a36c4da6f898979"
-  revision 2
+  revision 3
 
   bottle do
     rebuild 3
@@ -20,13 +20,26 @@ class Elinks < Formula
     depends_on "libtool" => :build
   end
 
-  depends_on "openssl" # no OpenSSL 1.1 support
+  depends_on "openssl@1.1"
+
+  # Two patches for compatibility with OpenSSL 1.1, from FreeBSD:
+  # https://www.freshports.org/www/elinks/
+  patch :p0 do
+    url "https://reviews.freebsd.org/file/data/mhvwibmet2v2udg4zxej/PHID-FILE-t5ovwhj2ffidzafayfj7/patch-src_network_ssl_socket.c"
+    sha256 "a4f199f6ce48989743d585b80a47bc6e0ff7a4fa8113d120e2732a3ffa4f58cc"
+  end
+
+  patch :p0 do
+    url "https://reviews.freebsd.org/file/data/gnlzo63jiq6hqudtanmv/PHID-FILE-3mj6cioca2hw4bpk76i5/patch-src_network_ssl_ssl.c"
+    sha256 "45c140d5db26fc0d98f4d715f5f355e56c12f8009a8dd9bf20b05812a886c348"
+  end
 
   def install
     ENV.deparallelize
     ENV.delete("LD")
     system "./autogen.sh" if build.head?
-    system "./configure", "--prefix=#{prefix}", "--without-spidermonkey",
+    system "./configure", "--prefix=#{prefix}",
+                          "--without-spidermonkey",
                           "--enable-256-colors"
     system "make", "install"
   end
