4323d282d2141ec19eb6e6d59757d5d722d0eda6
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-09-10 13:07:24 +0100

    gobject-introspection 1.62.0
    Closes #44100.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/gobject-introspection.rb
+++ Formula/gobject-introspection.rb
@@ -1,9 +1,8 @@
 class GobjectIntrospection < Formula
   desc "Generate introspection data for GObject libraries"
   homepage "https://wiki.gnome.org/Projects/GObjectIntrospection"
-  url "https://download.gnome.org/sources/gobject-introspection/1.60/gobject-introspection-1.60.2.tar.xz"
-  sha256 "ffdfe2368fb2e34a547898b01aac0520d52d8627fdeb1c306559bcb503ab5e9c"
-  revision 1
+  url "https://download.gnome.org/sources/gobject-introspection/1.62/gobject-introspection-1.62.0.tar.xz"
+  sha256 "b1ee7ed257fdbc008702bdff0ff3e78a660e7e602efa8f211dc89b9d1e7d90a2"
 
   bottle do
     sha256 "aebf18135464982195e0f14d8a5fd76dd03ef91fa45e3648500576228aec97fc" => :mojave
@@ -11,6 +10,8 @@ class GobjectIntrospection < Formula
     sha256 "dcd2ffc04732cd3e60e8f1de914838492d1f37d07d8bb26201fd0c25b634cbc3" => :sierra
   end
 
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "cairo"
   depends_on "glib"
   depends_on "libffi"
@@ -22,18 +23,26 @@ class GobjectIntrospection < Formula
         :revision => "499ac89f8a9ad17d250e907f74912159ea216416"
   end
 
+  # submitted upstream as https://gitlab.gnome.org/GNOME/gobject-introspection/merge_requests/177
+  patch :DATA
+
   def install
     ENV["GI_SCANNER_DISABLE_CACHE"] = "true"
     inreplace "giscanner/transformer.py", "/usr/share", "#{HOMEBREW_PREFIX}/share"
-    inreplace "configure" do |s|
-      s.change_make_var! "GOBJECT_INTROSPECTION_LIBDIR", "#{HOMEBREW_PREFIX}/lib"
-    end
+    inreplace "meson.build",
+      "config.set_quoted('GOBJECT_INTROSPECTION_LIBDIR', join_paths(get_option('prefix'), get_option('libdir')))",
+      "config.set_quoted('GOBJECT_INTROSPECTION_LIBDIR', '#{HOMEBREW_PREFIX}/lib')"
 
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--with-python=#{Formula["python"].opt_bin}/python3"
-    system "make"
-    system "make", "install"
+    args = %W[
+      --prefix=#{prefix}
+      -Dpython=#{Formula["python"].opt_bin}/python3
+    ]
+
+    mkdir "build" do
+      system "meson", *args, ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
+    end
   end
 
   test do
@@ -43,3 +52,34 @@ class GobjectIntrospection < Formula
     assert_predicate testpath/"Tut-0.1.typelib", :exist?
   end
 end
+
+__END__
+diff --git a/girepository/meson.build b/girepository/meson.build
+index 0183153e..204659fe 100644
+--- a/girepository/meson.build
++++ b/girepository/meson.build
+@@ -163,6 +163,15 @@ if cc.get_id() != 'msvc'
+   ])
+ endif
+
++lib_version = '1.0.0'
++lib_version_arr = lib_version.split('.')
++lib_version_major = lib_version_arr[0].to_int()
++lib_version_minor = lib_version_arr[1].to_int()
++lib_version_micro = lib_version_arr[2].to_int()
++
++osx_current = lib_version_major + 1
++lib_osx_version = [osx_current, '@0@.@1@'.format(osx_current, lib_version_minor)]
++
+ girepo_lib = shared_library('girepository-1.0',
+   sources: girepo_sources,
+   include_directories : configinc,
+@@ -170,7 +179,8 @@ girepo_lib = shared_library('girepository-1.0',
+           custom_c_args,
+   dependencies: [glib_dep, gobject_dep, gmodule_dep,
+                  gio_dep, girepo_internals_dep],
+-  version: '1.0.0',
++  version: lib_version,
++  darwin_versions: lib_osx_version,
+   install: true,
+ )
