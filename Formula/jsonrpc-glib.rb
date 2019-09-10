20e81aa5dd31e19b2c2f1d0e46c07e0a1e5e8d93
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-09-10 06:22:04 +0100

    jsonrpc-glib 3.34.0
    Closes #44089.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/jsonrpc-glib.rb
+++ Formula/jsonrpc-glib.rb
@@ -1,8 +1,8 @@
 class JsonrpcGlib < Formula
   desc "GNOME library to communicate with JSON-RPC based peers"
   homepage "https://gitlab.gnome.org/GNOME/jsonrpc-glib"
-  url "https://download.gnome.org/sources/jsonrpc-glib/3.32/jsonrpc-glib-3.32.0.tar.xz"
-  sha256 "bc60aa36c8bdc9c701ad490508445633a9f3973ae0bd5bdd0633d5f6ffeea6eb"
+  url "https://download.gnome.org/sources/jsonrpc-glib/3.34/jsonrpc-glib-3.34.0.tar.xz"
+  sha256 "d1ceb24b503e49e7bfe6e44630c03abc65f2d047a68271eb62e332b13be90548"
 
   bottle do
     sha256 "04cca91e37582e34e6c67edc5c6582899a137ba5b36c5b9be1ac7878746bd748" => :mojave
@@ -18,9 +18,6 @@ class JsonrpcGlib < Formula
   depends_on "vala" => :build
   depends_on "glib"
   depends_on "json-glib"
-
-  # submitted upstream as https://gitlab.gnome.org/GNOME/jsonrpc-glib/merge_requests/5
-  patch :DATA
 
   def install
     mkdir "build" do
@@ -69,39 +66,3 @@ class JsonrpcGlib < Formula
     system "./test"
   end
 end
-__END__
-diff --git a/meson.build b/meson.build
-index e949308..cb98d63 100644
---- a/meson.build
-+++ b/meson.build
-@@ -26,6 +26,8 @@ current = jsonrpc_glib_version_minor * 100 + jsonrpc_glib_version_micro - jsonrp
- revision = jsonrpc_glib_interface_age
- libversion = '@0@.@1@.@2@'.format(soversion, current, revision)
-
-+darwin_versions = [current + 1, '@0@.@1@'.format(current + 1, revision)]
-+
- config_h = configuration_data()
- config_h.set_quoted('GETTEXT_PACKAGE', 'libjsonrpc_glib')
- config_h.set_quoted('LOCALEDIR', join_paths(get_option('prefix'), get_option('localedir')))
-diff --git a/src/meson.build b/src/meson.build
-index 3366e96..83fe506 100644
---- a/src/meson.build
-+++ b/src/meson.build
-@@ -52,11 +52,12 @@ libjsonrpc_glib = library(
-   'jsonrpc-glib-' + apiversion,
-   libjsonrpc_glib_sources,
-
--        c_args: hidden_visibility_args + release_args,
--  dependencies: libjsonrpc_glib_deps,
--     soversion: soversion,
--       version: libversion,
--       install: true,
-+         c_args: hidden_visibility_args + release_args,
-+   dependencies: libjsonrpc_glib_deps,
-+      soversion: soversion,
-+        version: libversion,
-+darwin_versions: darwin_versions,
-+        install: true,
- )
-
- libjsonrpc_glib_dep = declare_dependency(
