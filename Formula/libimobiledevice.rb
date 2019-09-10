a27f4a543ce63ecb92417419f064d0b903b113b7
FX Coudert <fxcoudert@gmail.com>
2019-09-10 14:42:50 +0200

    libimobiledevice: patch for OpenSSL 1.1

--- Formula/libimobiledevice.rb
+++ Formula/libimobiledevice.rb
@@ -1,9 +1,24 @@
 class Libimobiledevice < Formula
   desc "Library to communicate with iOS devices natively"
   homepage "https://www.libimobiledevice.org/"
-  url "https://www.libimobiledevice.org/downloads/libimobiledevice-1.2.0.tar.bz2"
-  sha256 "786b0de0875053bf61b5531a86ae8119e320edab724fc62fe2150cc931f11037"
-  revision 3
+  revision 4
+
+  stable do
+    url "https://www.libimobiledevice.org/downloads/libimobiledevice-1.2.0.tar.bz2"
+    sha256 "786b0de0875053bf61b5531a86ae8119e320edab724fc62fe2150cc931f11037"
+
+    # Upstream commit for OpenSSL 1.1 compatibility
+    patch do
+      url "https://github.com/libimobiledevice/libimobiledevice/commit/13bf235c.diff?full_index=1"
+      sha256 "be4cc20d11551e04ae51bfd797f154f47110c68b4363bdc4c1fe2b9f1c0667d5"
+    end
+
+    # Upstream commit for OpenSSL 1.1 compatibility
+    patch do
+      url "https://github.com/libimobiledevice/libimobiledevice/commit/02a0e03e.diff?full_index=1"
+      sha256 "76a5eae502424fe7f59d64d526bf1d2d8736879ad03055c16f92cc5e0d6b8579"
+    end
+  end
 
   bottle do
     cellar :any
@@ -24,7 +39,7 @@ class Libimobiledevice < Formula
   depends_on "pkg-config" => :build
   depends_on "libplist"
   depends_on "libtasn1"
-  depends_on "openssl" # no OpenSSL 1.1 support
+  depends_on "openssl@1.1"
   depends_on "usbmuxd"
 
   def install
