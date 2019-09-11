5a9a4df82d2b12227f68456f1844e523b40f641c
csardothien <45049244+csardothien@users.noreply.github.com>
2019-09-11 14:30:27 -0400

    wireguard-go 0.0.20190908
    Closes #44149.
    
    Signed-off-by: Caleb Xu <calebcenter@live.com>

--- Formula/wireguard-go.rb
+++ Formula/wireguard-go.rb
@@ -1,8 +1,8 @@
 class WireguardGo < Formula
   desc "Userspace Go implementation of WireGuard"
   homepage "https://www.wireguard.com/"
-  url "https://git.zx2c4.com/wireguard-go/snapshot/wireguard-go-0.0.20190805.tar.xz"
-  sha256 "2f56d2c4ebc3e50d581e92c046ebf3c884c703c4d5b3b484e45f55e50c0034da"
+  url "https://git.zx2c4.com/wireguard-go/snapshot/wireguard-go-0.0.20190908.tar.xz"
+  sha256 "3c4cc802a521736d01d24bfb4fe29a5e74da07d69637ec7bcdee074decf62c6a"
   head "https://git.zx2c4.com/wireguard-go", :using => :git
 
   bottle do
