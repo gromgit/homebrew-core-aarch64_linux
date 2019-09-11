ad96822e73a349c00c3a5af510d037279a6bf314
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-11 03:20:38 +0000

    testdisk: update 7.1 bottle.

--- Formula/testdisk.rb
+++ Formula/testdisk.rb
@@ -6,13 +6,9 @@ class Testdisk < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "2c605aa973b201cd63a19efa53a7748a629b860b48f452db6c2ea61856d6aea3" => :mojave
-    sha256 "e2643fdb09f3d33b18fb181590fe5fc58f9117a8a02791eb55ba5cce998b6cb0" => :high_sierra
-    sha256 "30784b33a74eaa138c16ddc7cbe56bc542f19759a87cfdea37084691ba5788b4" => :sierra
-    sha256 "979d1f6ba12aeee68300a657a78a234874707068232934d7f91597621a60253e" => :el_capitan
-    sha256 "13f6481decb5ad3f40f0617351dd9c78a02c3c0694a82cb048adde6ba897703f" => :yosemite
-    sha256 "d3e8a600a135807b630a4d649c052dc6065270910bd96f6b1f27265251787331" => :mavericks
+    sha256 "1e77fbc276d986fcf378901b2ba0d5957f17b569e512980017ecd09926505a4a" => :mojave
+    sha256 "8cd43adea2ddf632e5c9305609cf377b47fcf5836805075d06dd3ccd2142ccc6" => :high_sierra
+    sha256 "752a686f8fa7717cbbdef064eefd80503eccdddfc587bd48fd24256e23332470" => :sierra
   end
 
   def install
