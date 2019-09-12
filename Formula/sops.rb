b2bb9407b20cd77590ea210a72f068e050912653
Rui Chen <rchen@meetup.com>
2019-09-12 17:24:36 -0400

    sops 3.4.0
    Closes #44191.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/sops.rb
+++ Formula/sops.rb
@@ -1,8 +1,8 @@
 class Sops < Formula
   desc "Editor of encrypted files"
   homepage "https://github.com/mozilla/sops"
-  url "https://github.com/mozilla/sops/archive/3.3.1.tar.gz"
-  sha256 "9f6a4a369378cef9e8e7cdd8ba00b3105b0b6875404c3b9f482f804b2f355a77"
+  url "https://github.com/mozilla/sops/archive/3.4.0.tar.gz"
+  sha256 "65f680ada424094dcdb80b44e3c11c86235618ef1ab10f5f632fcda954a06363"
   head "https://github.com/mozilla/sops.git"
 
   bottle do
@@ -12,14 +12,19 @@ class Sops < Formula
     sha256 "8345f9f6375d27756ad9a8b6adab4c249bdc223b863e20258f869da79f90ea69" => :sierra
   end
 
-  depends_on "go" => :build
+  depends_on "go@1.12" => :build
 
   def install
     ENV["GOPATH"] = buildpath
     ENV["GOBIN"] = bin
-    (buildpath/"src/go.mozilla.org").mkpath
-    ln_s buildpath, "src/go.mozilla.org/sops"
-    system "make", "install"
+
+    dir = buildpath/"src/github.com/mozilla/sops"
+    dir.install buildpath.children
+
+    cd dir do
+      system "make", "install"
+      prefix.install_metafiles
+    end
   end
 
   test do
