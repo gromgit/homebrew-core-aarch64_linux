019cae9269ec33104d6f976d602efc9b176bcc26
Misty De Meo <mistydemeo@github.com>
2019-09-12 13:10:19 +1000

    memcached: remove mysql-cluster reference

--- Formula/memcached.rb
+++ Formula/memcached.rb
@@ -13,8 +13,6 @@ class Memcached < Formula
 
   depends_on "libevent"
 
-  conflicts_with "mysql-cluster", :because => "both install `bin/memcached`"
-
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-coverage"
     system "make", "install"
