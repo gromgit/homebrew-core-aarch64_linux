05daf8bb2ae4aca92a598f7a78ced7efa1eed4a3
Viktor Szakats <commit@vszakats.net>
2019-09-12 11:06:34 +0000

    stone: secure url(s)

--- Formula/stone.rb
+++ Formula/stone.rb
@@ -1,7 +1,7 @@
 class Stone < Formula
   desc "TCP/IP packet repeater in the application layer"
-  homepage "http://www.gcd.org/sengoku/stone/"
-  url "http://www.gcd.org/sengoku/stone/stone-2.3e.tar.gz"
+  homepage "https://www.gcd.org/sengoku/stone/"
+  url "https://www.gcd.org/sengoku/stone/stone-2.3e.tar.gz"
   sha256 "b2b664ee6771847672e078e7870e56b886be70d9ff3d7b20d0b3d26ee950c670"
 
   bottle do
