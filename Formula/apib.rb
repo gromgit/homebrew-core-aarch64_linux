acfee58e0248b3e75daa10b3f09c65e8c83d9000
Sean Molenaar <smillerdev@me.com>
2019-09-13 08:47:53 +0200

    apib: revision bump
    Closes #44144.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/apib.rb
+++ Formula/apib.rb
@@ -3,7 +3,7 @@ class Apib < Formula
   homepage "https://github.com/apigee/apib"
   url "https://github.com/apigee/apib/archive/APIB_1_0.tar.gz"
   sha256 "1592e55c01f2f9bc8085b39f09c49cd7b786b6fb6d02441ca665eef262e7b87e"
-  revision 2
+  revision 3
   head "https://github.com/apigee/apib.git"
 
   bottle do
@@ -17,19 +17,19 @@ class Apib < Formula
   depends_on "openssl@1.1"
 
   def install
-    # Upstream hardcodes finding apr in /usr/include. When CLT is not present
-    # we need to fix this so our apr requirement works.
+    # Fix detection of libcrypto for OpenSSL 1.1
+    inreplace "configure", "CRYPTO_num_locks", "EVP_sha1"
+
+    # Upstream hardcodes finding apr in /usr/include
     # https://github.com/apigee/apib/issues/11
-    unless MacOS::CLT.installed?
-      inreplace "configure" do |s|
-        s.gsub! "/usr/include/apr-1.0", "#{Formula["apr"].opt_libexec}/include/apr-1"
-        s.gsub! "/usr/include/apr-1", "#{Formula["apr"].opt_libexec}/include/apr-1"
-      end
-      ENV.append "LDFLAGS", "-L#{Formula["apr-util"].opt_libexec}/lib"
-      ENV.append "LDFLAGS", "-L#{Formula["apr"].opt_libexec}/lib"
-      ENV.append "CFLAGS", "-I#{Formula["apr"].opt_libexec}/include/apr-1"
-      ENV.append "CFLAGS", "-I#{Formula["apr-util"].opt_libexec}/include/apr-1"
+    inreplace "configure" do |s|
+      s.gsub! "/usr/include/apr-1.0", "#{Formula["apr"].opt_libexec}/include/apr-1"
+      s.gsub! "/usr/include/apr-1", "#{Formula["apr"].opt_libexec}/include/apr-1"
     end
+    ENV.append "LDFLAGS", "-L#{Formula["apr-util"].opt_libexec}/lib"
+    ENV.append "LDFLAGS", "-L#{Formula["apr"].opt_libexec}/lib"
+    ENV.append "CFLAGS", "-I#{Formula["apr"].opt_libexec}/include/apr-1"
+    ENV.append "CFLAGS", "-I#{Formula["apr-util"].opt_libexec}/include/apr-1"
 
     system "./configure", "--prefix=#{prefix}"
     system "make"
