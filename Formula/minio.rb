86068e1a65beaebc03f5a9814f90089ad2e26e09
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-13 01:02:36 +0000

    minio: update 20190911195316 bottle.

--- Formula/minio.rb
+++ Formula/minio.rb
@@ -8,9 +8,9 @@ class Minio < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "fff2da8cec5959ecd7c3a5e3c8b796f1c60539f7ad0ac0dd87b7e631fa9ca604" => :mojave
-    sha256 "4087e5bd650645204c8f303f2e5845079436ad2f4fe2eff135198c144f21c9cb" => :high_sierra
-    sha256 "18ca5c1f8c4705ab82c3c57f08ebf1aa17b6c5eea7b8cf91acae7e611221b3b6" => :sierra
+    sha256 "42fe72e69068a8f277c16349af6b1003c65667ea99f50e4353630f95de02aab4" => :mojave
+    sha256 "0df63c0b406751bfb8b31dfdb0ddafca5bb0406fafebd91bacf046c2179308a6" => :high_sierra
+    sha256 "9b6bd802ee2dc72a9d2071e6755786fb4155991ec46cb05f16f993caf7faef2f" => :sierra
   end
 
   depends_on "go" => :build
