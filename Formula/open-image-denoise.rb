4d8d010ff21c0f4898204e4176ed327266c9eed7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-14 09:07:34 +0000

    open-image-denoise: add 1.0.0 bottle.

--- Formula/open-image-denoise.rb
+++ Formula/open-image-denoise.rb
@@ -4,6 +4,12 @@ class OpenImageDenoise < Formula
   url "https://github.com/OpenImageDenoise/oidn/releases/download/v1.0.0/oidn-1.0.0.src.tar.gz"
   sha256 "e3cb903c9def3a1485191da1a6d252055d907ed1b9a007b3bda1d3b522cd324e"
 
+  bottle do
+    cellar :any
+    sha256 "b63fc5dea6c3114322d85193c709a5d5313fc53eacc359b818ffd3808f0ce2ed" => :mojave
+    sha256 "2cf532b344578f9e6350816858e6fdbd5fd13bfd992fbbbaeab120e2ecbd42fd" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   # clang: error: unknown argument: '-fopenmp-simd'
   # https://github.com/OpenImageDenoise/oidn/issues/35
