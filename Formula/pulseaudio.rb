f826e12886a187d5e9d5ec45a0c7388b6220ae77
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-14 11:34:20 +0000

    pulseaudio: update 13.0 bottle.

--- Formula/pulseaudio.rb
+++ Formula/pulseaudio.rb
@@ -5,9 +5,9 @@ class Pulseaudio < Formula
   sha256 "961b23ca1acfd28f2bc87414c27bb40e12436efcf2158d29721b1e89f3f28057"
 
   bottle do
-    sha256 "0ab6cea67c0968cf93aa6f14817af6ded1b95ec4e9ebc93ed2fc483e27ec10e8" => :mojave
-    sha256 "406e04df6b778ee1859d82210043875551f5678ec09a24b3fd9668bb533d72ab" => :high_sierra
-    sha256 "5074b72e0c37236a3489a8dae38a1c53f74fa0534ac4e12d752f7195970cdb29" => :sierra
+    sha256 "819cb5b8dd86715db2285f647b1742611dd2a802447aea637f05adae33a6056b" => :mojave
+    sha256 "a5c5442b2118b9e3e3b2cbd8a8a700a121e9264b11c7096a3b3c42ce780a7a0b" => :high_sierra
+    sha256 "bc42617a58074e5631eae20559f6a043ee87c8dcfd9944ac45b47467a3cdca66" => :sierra
   end
 
   head do
