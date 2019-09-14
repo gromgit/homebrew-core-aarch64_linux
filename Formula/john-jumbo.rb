8c037a0c5293b6a9c34122bb4a9224a0ad734059
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-14 07:41:14 +0000

    john-jumbo: update 1.9.0 bottle.

--- Formula/john-jumbo.rb
+++ Formula/john-jumbo.rb
@@ -6,14 +6,9 @@ class JohnJumbo < Formula
   sha256 "f5d123f82983c53d8cc598e174394b074be7a77756f5fb5ed8515918c81e7f3b"
 
   bottle do
-    cellar :any
-    rebuild 6
-    sha256 "18a76d1f8abe542239444a3931d1d08f5f745cbb9a202b1d5fd2e1290b30fa4d" => :mojave
-    sha256 "6612d2001d80361ec70a19a8c54254a76fe1919a9cc72918032304e714144530" => :high_sierra
-    sha256 "6a2460e120f697e09a1ed62a948e5ecd767fc1b24a3f331aa4d59833205e48a4" => :sierra
-    sha256 "a87bf02d882413393f3f3759ab0fa6a171438609d101c7c9bc7772fe69e2ab47" => :el_capitan
-    sha256 "cf9c82f416a4eb3aad7d4202b21105988d346be8d8df262ea4ca18e683475d32" => :yosemite
-    sha256 "b36f66b0469b5c6cde95f780671db5b32e4e4dd7c16c4e7e591043bfdef2b65c" => :mavericks
+    sha256 "bebd822ea6b92a99e9376f972b24d47f870f123f87d173446f1f1614a766c1d5" => :mojave
+    sha256 "b927a6a9536228b744e7bb45b8248624bf334ebcbefb337ee7e5450a87954bb0" => :high_sierra
+    sha256 "73de9893b76f924973e03c3cb3f89030da701918965601aea20537a17efc1b15" => :sierra
   end
 
   depends_on "pkg-config" => :build
