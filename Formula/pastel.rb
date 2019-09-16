7bdaf133beffc284c9537eb8607334c03a39540c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-16 08:21:59 +0000

    pastel: add 0.5.3 bottle.

--- Formula/pastel.rb
+++ Formula/pastel.rb
@@ -4,6 +4,13 @@ class Pastel < Formula
   url "https://github.com/sharkdp/pastel/archive/v0.5.3.tar.gz"
   sha256 "0850e37b92b8d2f5a396d5b9114a21a64dce5c6cce736039151c800f35abaa1e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "cd35a70df7798441446d2494d45be08844bfc261d3235d8b9a4da976d372fd1b" => :mojave
+    sha256 "6c22ebd7ef0c661c1c616ed366cf128983ade51947248116ef18ba0a7a58f586" => :high_sierra
+    sha256 "8f63ddedd1873d13bf383e9e574c18ebaa0a29e6f4adfa75ea2ed7a2b23bdada" => :sierra
+  end
+
   depends_on "rust" => :build
 
   def install
