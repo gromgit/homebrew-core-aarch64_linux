c01e654a2e29eec20922c3f3e3688af93e50abd8
Harold Putman <h.putman@ieee.org>
2019-09-16 15:24:20 -0400

    vault-cli 3.4.0 (#44331)

--- Formula/vault-cli.rb
+++ Formula/vault-cli.rb
@@ -1,8 +1,8 @@
 class VaultCli < Formula
   desc "Subversion-like utility to work with Jackrabbit FileVault"
   homepage "https://jackrabbit.apache.org/filevault/index.html"
-  url "https://search.maven.org/remotecontent?filepath=org/apache/jackrabbit/vault/vault-cli/3.2.8/vault-cli-3.2.8-bin.tar.gz"
-  sha256 "dfb19ee7dbf86183336775865cd610c353f3d141c243cca722ce0b4bfef98abc"
+  url "https://search.maven.org/remotecontent?filepath=org/apache/jackrabbit/vault/vault-cli/3.4.0/vault-cli-3.4.0-bin.tar.gz"
+  sha256 "e500ff02a95461a07442c817c8a975ca7294848f694dfc1c015011c19613f64d"
   head "https://github.com/apache/jackrabbit-filevault.git"
 
   bottle :unneeded
