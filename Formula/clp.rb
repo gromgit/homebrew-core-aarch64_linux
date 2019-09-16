8eeb8f5a3d55e7f0e2bc820b3407f3209cf90c1a
Jamie Snape <jamie.snape@kitware.com>
2019-09-16 12:38:02 -0400

    clp 1.17.3 (new formula)
    Closes #40725.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/clp.rb
@@ -0,0 +1,54 @@
+class Clp < Formula
+  desc "Linear programming solver"
+  homepage "https://github.com/coin-or/Clp"
+  url "https://github.com/coin-or/Clp/archive/releases/1.17.3.tar.gz"
+  sha256 "25f0692fe1daa492e7801770af6991506ae9a8c34a4cae358d017400a02dfcf8"
+
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "coinutils"
+  depends_on "openblas"
+
+  resource "coin-or-tools-data-sample-p0033-mps" do
+    url "https://raw.githubusercontent.com/coin-or-tools/Data-Sample/releases/1.2.11/p0033.mps"
+    sha256 "8ccff819023237c79ef32e238a5da9348725ce9a4425d48888baf3a0b3b42628"
+  end
+
+  def install
+    # Work around https://github.com/coin-or/Clp/issues/109:
+    # Error 1: "mkdir: #{include}/clp/coin: File exists."
+    mkdir include/"clp/coin"
+
+    args = [
+      "--datadir=#{pkgshare}",
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--disable-silent-rules",
+      "--includedir=#{include}/clp",
+      "--prefix=#{prefix}",
+      "--with-blas-incdir=#{Formula["openblas"].opt_include}",
+      "--with-blas-lib=-L#{Formula["openblas"].opt_lib} -lopenblas",
+      "--with-lapack-incdir=#{Formula["openblas"].opt_include}",
+      "--with-lapack-lib=-L#{Formula["openblas"].opt_lib} -lopenblas",
+    ]
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    resource("coin-or-tools-data-sample-p0033-mps").stage testpath
+    system bin/"clp", "-import", testpath/"p0033.mps", "-primals"
+    (testpath/"test.cpp").write <<~EOS
+      #include <ClpSimplex.hpp>
+      int main() {
+        ClpSimplex model;
+        int status = model.readMps("#{testpath}/p0033.mps", true);
+        if (status != 0) { return status; }
+        status = model.primal();
+        return status;
+      }
+    EOS
+    pkg_config_flags = `pkg-config --cflags --libs clp`.chomp.split
+    system ENV.cxx, "test.cpp", *pkg_config_flags
+    system "./a.out"
+  end
+end
