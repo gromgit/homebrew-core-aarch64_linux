b75bb8c42a13e444ecdd91bfb67caffdc9a36c68
kshakir <kshakir@broadinstitute.org>
2019-09-16 18:57:06 -0400

    cromwell 46 (#44334)

--- Formula/cromwell.rb
+++ Formula/cromwell.rb
@@ -1,8 +1,8 @@
 class Cromwell < Formula
   desc "Workflow Execution Engine using Workflow Description Language"
   homepage "https://github.com/broadinstitute/cromwell"
-  url "https://github.com/broadinstitute/cromwell/releases/download/45//cromwell-45.jar"
-  sha256 "aeb4962c93689e1e3d6d1a2271d4be5a2d8b2d954ec3cbd05298c7d5e126fbbf"
+  url "https://github.com/broadinstitute/cromwell/releases/download/46//cromwell-46.jar"
+  sha256 "3c616b1d37f90e3c864b227621af4d9f108258244cba30f71b7cfba42ac35c83"
 
   head do
     url "https://github.com/broadinstitute/cromwell.git"
@@ -14,8 +14,8 @@ class Cromwell < Formula
   depends_on :java => "1.8+"
 
   resource "womtool" do
-    url "https://github.com/broadinstitute/cromwell/releases/download/45//womtool-45.jar"
-    sha256 "3e30066f848bf365ffe81eeb9ed17686f71fb8c529c6743000fbec852442ea0f"
+    url "https://github.com/broadinstitute/cromwell/releases/download/46//womtool-46.jar"
+    sha256 "d15759ea076929505005624c5866b8208518c24b5f6e75c603c611fb813db1ca"
   end
 
   def install
