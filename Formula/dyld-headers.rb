efee252de363a2d850a2bafe3e2fa1733ce6bc23
Caleb Xu <calebcenter@live.com>
2019-09-16 14:54:39 -0400

    dyld-headers 655.1.1
    Closes #44332.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/dyld-headers.rb
+++ Formula/dyld-headers.rb
@@ -1,8 +1,8 @@
 class DyldHeaders < Formula
   desc "Header files for the dynamic linker"
   homepage "https://opensource.apple.com/"
-  url "https://opensource.apple.com/tarballs/dyld/dyld-635.2.tar.gz"
-  sha256 "e891f256bf0cb570b7bc881a67de94944f973b97a37af5e3deeae7437d27dd37"
+  url "https://opensource.apple.com/tarballs/dyld/dyld-655.1.1.tar.gz"
+  sha256 "8ca6e3cf0263d3f69dfa65e0846e2bed051b0cff92e796352ad178e7e4c92f1d"
 
   bottle :unneeded
 
