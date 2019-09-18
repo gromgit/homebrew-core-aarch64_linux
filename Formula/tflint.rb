1812545551b951057cde6b3a94289707867d750e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-18 23:42:45 +0000

    tflint: update 0.11.2 bottle.

--- Formula/tflint.rb
+++ Formula/tflint.rb
@@ -8,9 +8,9 @@ class Tflint < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ceb2dfc4b77a86616be7babfddc013f3bdeae9b6fd60f4f318680c7fd133d8b4" => :mojave
-    sha256 "bca39eb983b366f3c16f6ce08e9cee1b39e584fed8338fef93eefbcb1565d3ea" => :high_sierra
-    sha256 "b20c9e9b857dd39b616e072b0ccadbc97ee0cbe4d657104bd9d145d9f543c51d" => :sierra
+    sha256 "44676ccfd2eab7b62e949d9244a0b40ed45df4cb1864ce4feb801a4076a7b69f" => :mojave
+    sha256 "cffc8f1cfd97aef7142313b20a0e7fc0dedec87973f384c35edb426e24a46ba8" => :high_sierra
+    sha256 "7c0c391c1397327633c9dcd96323cfe32e2572f9df8b2c82988b0a63ffa0af47" => :sierra
   end
 
   depends_on "go" => :build
