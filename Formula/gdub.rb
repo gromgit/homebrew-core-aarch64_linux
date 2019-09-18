71ca1d95630454ab5b898b751c029d726b9483a2
Tarek Belkahia <tarek.belkahia@gmail.com>
2019-09-18 03:22:15 +0200

    gdub 0.2.0 (#44354)

--- Formula/gdub.rb
+++ Formula/gdub.rb
@@ -1,8 +1,8 @@
 class Gdub < Formula
   desc "Gradlew/gradle wrapper"
   homepage "https://www.gdub.rocks/"
-  url "https://github.com/dougborg/gdub/archive/v0.1.0.tar.gz"
-  sha256 "ddf2572cc67b8df3293b1707720c6ef09d6caf73227fa869a73b16239df959c3"
+  url "https://github.com/dougborg/gdub/archive/v0.2.0.tar.gz"
+  sha256 "aa3da76752b597e60094a67971f35dfe20377390d21b3ae3b45b7b7040e9a268"
 
   bottle :unneeded
 
