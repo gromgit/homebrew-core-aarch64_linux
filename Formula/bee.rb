c3020ba2cdc9d4efe9a9dbdc0918bc7bbbea7e4d
chenrui <rchen@meetup.com>
2019-09-18 22:11:39 -0400

    bee 1.72 (#44404)

--- Formula/bee.rb
+++ Formula/bee.rb
@@ -1,8 +1,8 @@
 class Bee < Formula
   desc "Tool for managing database changes"
   homepage "https://github.com/bluesoft/bee"
-  url "https://github.com/bluesoft/bee/releases/download/1.71/bee-1.71.zip"
-  sha256 "d6b6bede523447101fe9f8d2b73c6c7e2c980f80aee60152ffdf784b430f92f3"
+  url "https://github.com/bluesoft/bee/releases/download/1.72/bee-1.72.zip"
+  sha256 "52824718a00ac9310fd08d605df6e8499ccc31ea7cf38912c40cf5e39098d630"
 
   bottle :unneeded
 
