ce7ba32b79b8ff20e29eb65465645b0cadcf7c3b
Jérémy CHABERNAUD <djerfy@gmail.com>
2019-09-20 19:27:32 +0200

    traefik@1 1.7.17 (new formula)
    Closes #44478.
    
    Signed-off-by: Rui Chen <rchen@meetup.com>

---
+++ Formula/traefik@1.rb
@@ -0,0 +1,93 @@
+class TraefikAT1 < Formula
+  desc "Modern reverse proxy (v1.7)"
+  homepage "https://traefik.io/"
+  url "https://github.com/containous/traefik/releases/download/v1.7.17/traefik-v1.7.17.src.tar.gz"
+  version "1.7.17"
+  sha256 "e47a984e41aba3b6f9c4950fbf35614a82e1594b4a956465d1ff469bd4b6dd2c"
+
+  keg_only :versioned_formula
+
+  depends_on "go" => :build
+  depends_on "go-bindata" => :build
+  depends_on "node" => :build
+  depends_on "yarn" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/containous/traefik").install buildpath.children
+
+    cd "src/github.com/containous/traefik" do
+      cd "webui" do
+        system "yarn", "upgrade"
+        system "yarn", "install"
+        system "yarn", "run", "build"
+      end
+      system "go", "generate"
+      system "go", "build", "-o", bin/"traefik", "./cmd/traefik"
+      prefix.install_metafiles
+    end
+  end
+
+  plist_options :manual => "traefik"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>KeepAlive</key>
+          <false/>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/traefik</string>
+            <string>--configfile=#{etc/"traefik/traefik.toml"}</string>
+          </array>
+          <key>EnvironmentVariables</key>
+          <dict>
+          </dict>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>WorkingDirectory</key>
+          <string>#{var}</string>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/traefik.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/traefik.log</string>
+        </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    require "socket"
+
+    web_server = TCPServer.new(0)
+    http_server = TCPServer.new(0)
+    web_port = web_server.addr[1]
+    http_port = http_server.addr[1]
+    web_server.close
+    http_server.close
+
+    (testpath/"traefik.toml").write <<~EOS
+      [web]
+      address = ":#{web_port}"
+
+      [entryPoints.http]
+      address = ":#{http_port}"
+    EOS
+
+    begin
+      pid = fork do
+        exec bin/"traefik", "--configfile=#{testpath}/traefik.toml"
+      end
+      sleep 5
+      cmd = "curl -sIm3 -XGET http://localhost:#{web_port}/dashboard/"
+      assert_match /200 OK/m, shell_output(cmd)
+    ensure
+      Process.kill("HUP", pid)
+    end
+  end
+end
