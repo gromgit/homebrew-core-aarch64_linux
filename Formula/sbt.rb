b6583235531cd39f748d1c8294e4d1eb60c83f9d
eugene yokota <eed3si9n@gmail.com>
2019-09-20 22:58:30 -0400

    sbt 1.3.2 (#44464)

--- Formula/sbt.rb
+++ Formula/sbt.rb
@@ -1,9 +1,9 @@
 class Sbt < Formula
   desc "Build tool for Scala projects"
   homepage "https://www.scala-sbt.org/"
-  url "https://github.com/sbt/sbt/releases/download/v1.3.1/sbt-1.3.1.tgz"
-  mirror "https://sbt-downloads.cdnedge.bluemix.net/releases/v1.3.1/sbt-1.3.1.tgz"
-  sha256 "331f23ef5a06ed90993cb2ea9b6971269d8e938762557b75db16b14df4609056"
+  url "https://github.com/sbt/sbt/releases/download/v1.3.2/sbt-1.3.2.tgz"
+  mirror "https://sbt-downloads.cdnedge.bluemix.net/releases/v1.3.2/sbt-1.3.2.tgz"
+  sha256 "ed8cef399129895ad0d757eea812b3f95830a36fa838f8ede1c6cdc2294f326f"
 
   bottle :unneeded
 
