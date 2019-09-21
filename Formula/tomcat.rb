fe1e2c55557820b76244310813001df2c574852a
mystygage <mystygage@users.noreply.github.com>
2019-09-21 05:49:44 +0200

    tomcat 9.0.26 (#44445)

--- Formula/tomcat.rb
+++ Formula/tomcat.rb
@@ -1,8 +1,8 @@
 class Tomcat < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-9/v9.0.24/bin/apache-tomcat-9.0.24.tar.gz"
-  sha256 "22064138e25f7ab899802804775259a156c06770535b8ce93856beba13dfcf6d"
+  url "https://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-9/v9.0.26/bin/apache-tomcat-9.0.26.tar.gz"
+  sha256 "b5430890d3b986d6b7ec6a6ef611f9451cbfa933b0a1a3dd48e2cd1f46a63381"
 
   bottle :unneeded
 
