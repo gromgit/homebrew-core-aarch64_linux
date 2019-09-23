8bfbd0065b35472e944d65d0d1240b7a3063bd21
Anthony Ramine <nox@nox.paris>
2019-09-23 09:49:43 +0200

    sofia-sip: Bump revision for glib changes
    Closes #44446.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- Formula/sofia-sip.rb
+++ Formula/sofia-sip.rb
@@ -3,7 +3,7 @@ class SofiaSip < Formula
   homepage "https://sofia-sip.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/sofia-sip/sofia-sip/1.12.11/sofia-sip-1.12.11.tar.gz"
   sha256 "2b01bc2e1826e00d1f7f57d29a2854b15fd5fe24695e47a14a735d195dd37c81"
-  revision 3
+  revision 4
 
   bottle do
     cellar :any
