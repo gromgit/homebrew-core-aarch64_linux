06767db82daff311a4af11c7a260fc26f5b2355b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-23 15:44:35 +0000

    ripgrep-all: add 0.9.3 bottle.

--- Formula/ripgrep-all.rb
+++ Formula/ripgrep-all.rb
@@ -5,6 +5,13 @@ class RipgrepAll < Formula
   sha256 "06259e7c1734a9246c2d113bf5e914f4d418e53c201efc697bfc041a713fbef3"
   head "https://github.com/phiresky/ripgrep-all.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2e12ab8408a5477ade1faae0d4ff52feba1d62fa83c468d2013e3f4e3381f098" => :mojave
+    sha256 "0647dbf099bec9e929e6dad6988a129116b784623c8da8caa1ebd7f284b62912" => :high_sierra
+    sha256 "a705783f96e33d80f13fd67f53c4b11be3cce0ec1049cdadb53814941ac3035c" => :sierra
+  end
+
   depends_on "rust" => :build
   depends_on "ripgrep"
 
