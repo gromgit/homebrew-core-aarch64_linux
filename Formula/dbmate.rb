0b6e4860f5217034dc4b93402bd40a6076ef1020
Adrian Macneil <adrian@getcruise.com>
2019-09-24 22:14:59 -0700

    dbmate 1.7.0
    Closes #44563.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/dbmate.rb
+++ Formula/dbmate.rb
@@ -1,8 +1,8 @@
 class Dbmate < Formula
   desc "Lightweight, framework-agnostic database migration tool"
   homepage "https://github.com/amacneil/dbmate"
-  url "https://github.com/amacneil/dbmate/archive/v1.6.0.tar.gz"
-  sha256 "b73c6edf57a125b9d5bb51d2eac0067dae6f8049d7ff9aa891b2acaa2882f4d5"
+  url "https://github.com/amacneil/dbmate/archive/v1.7.0.tar.gz"
+  sha256 "042ede72743afa8a044f56908105760472dcfdca67880c2622a3cd75ae11ee3d"
   head "https://github.com/amacneil/dbmate.git"
 
   bottle do
