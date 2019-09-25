a0863b27ef4e3a3ffc1df320cfa979d7f2af3a3a
Igor Kapkov <igasgeek@me.com>
2019-09-25 17:00:40 +1000

    jfrog-cli-go: remove GO111MODULE as it is default

--- Formula/jfrog-cli-go.rb
+++ Formula/jfrog-cli-go.rb
@@ -14,7 +14,6 @@ class JfrogCliGo < Formula
   depends_on "go" => :build
 
   def install
-    ENV["GO111MODULE"] = "on"
     ENV["GOPATH"] = buildpath
 
     src = buildpath/"src/github.com/jfrog/jfrog-cli-go"
