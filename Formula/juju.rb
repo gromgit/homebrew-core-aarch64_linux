ac03cac94ab99019fb962898cd0986a1dc9c6682
Achilleas Anagnostopoulos <achilleas.imap.mbox@gmail.com>
2019-09-26 16:22:16 +0100

    Juju 2.6.9
    Closes #44613.
    
    Signed-off-by: Rui Chen <rchen@meetup.com>

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -1,8 +1,8 @@
 class Juju < Formula
   desc "DevOps management tool"
   homepage "https://jujucharms.com/"
-  url "https://launchpad.net/juju/2.6/2.6.8/+download/juju-core_2.6.8.tar.gz"
-  sha256 "0c5a6ccae09e602621c3958a2c56c0bfa7676060f2169008577b809552af4568"
+  url "https://launchpad.net/juju/2.6/2.6.9/+download/juju-core_2.6.9.tar.gz"
+  sha256 "85c428a6c558432d24b9d29011f62fc2ed46961933cb3cbb5aba85cb9768a00c"
 
   bottle do
     cellar :any_skip_relocation
