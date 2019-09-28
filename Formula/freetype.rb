86382901f48fcf0d4fa573197ab00fab0c8e3277
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 14:44:58 +0000

    freetype: update 2.10.1 bottle.

--- Formula/freetype.rb
+++ Formula/freetype.rb
@@ -7,6 +7,7 @@ class Freetype < Formula
 
   bottle do
     cellar :any
+    sha256 "ddd686141a969caec11ea248324e3736f6db50a54673187be103dde39cb01ebf" => :catalina
     sha256 "2f5716f987df6f45a9d66e5f9af935bbb4202fe0b9850b6b0660fd6555ba1be4" => :mojave
     sha256 "c367032a79a287a004e29023329c74e19887b80e05c495eec44182165ecd79e0" => :high_sierra
     sha256 "282171a01965ae9dfd2d7955459a7285ca3a85f16504133e5fa46c72f682c14f" => :sierra
