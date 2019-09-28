54156d73cc7251c1118a00291e3d935342730c25
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 13:26:15 +0000

    libtool: update 2.4.6_1 bottle.

--- Formula/libtool.rb
+++ Formula/libtool.rb
@@ -8,6 +8,7 @@ class Libtool < Formula
 
   bottle do
     cellar :any
+    sha256 "38212486e78db33048438cffe38b6914f13553e7bb8c7d3d2fbecb18a6481d3c" => :catalina
     sha256 "c92ab35c3706c255a36b733aa7a475159da9cf375c275d230fd6a7802a94e4dc" => :mojave
     sha256 "ebb50367eb2336ee317841587e24690de124fb2c3e4d346405e9b41c4e6120ae" => :high_sierra
     sha256 "78a1f6c6644eae01eb5c204ef705f7e48721a0fe8ece492c10c84791061885db" => :sierra
