896d1018c7a4906f2c3fa1386aaf283497db60a2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 13:49:39 +0000

    icu4c: update 64.2 bottle.

--- Formula/icu4c.rb
+++ Formula/icu4c.rb
@@ -8,6 +8,7 @@ class Icu4c < Formula
 
   bottle do
     cellar :any
+    sha256 "e9ae7bb5a76b48e82f56bc744eaaa1e9bdb5ca49ea6b5a2e4d52f57ad331f063" => :catalina
     sha256 "e858556120acc0c2d52b8fb572b677856724cc28d24a7e1b2762d458a2977c8e" => :mojave
     sha256 "02afdd6a472c31351e46a3b2a38c1c77354f0cc9199c3dbd7e06bc171b3638a2" => :high_sierra
     sha256 "4270a558e2709b7cee1be047a8e890ee15bdb379825fd6dc81fdba74cd438c01" => :sierra
