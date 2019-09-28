4412f6d2d6a0e4b2ece311d5854a852bee0a60c2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 16:22:22 +0000

    pixman: update 0.38.4 bottle.

--- Formula/pixman.rb
+++ Formula/pixman.rb
@@ -6,6 +6,7 @@ class Pixman < Formula
 
   bottle do
     cellar :any
+    sha256 "3b56eb903b40382b7f52a82b811d07e80d316d855963aeff3b977d8225df26a4" => :catalina
     sha256 "3990b771ee29451c8a9bcb6cb077205ae08adc0d5af2faebf29197d13c36a51a" => :mojave
     sha256 "d383ddee57685391ea55033e6fccdca0352a898cbc4c75be40d7b5dc7c312916" => :high_sierra
     sha256 "005ab5564c93b757494692b1f0e52d50414058ba4674b4e3e4d9fdd30f8ee8f2" => :sierra
