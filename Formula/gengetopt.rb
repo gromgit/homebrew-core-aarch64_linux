20f71fdc1d70e9fcbf20eff3bfb45a6daa85199b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 19:09:18 +0000

    gengetopt: update 2.23 bottle.

--- Formula/gengetopt.rb
+++ Formula/gengetopt.rb
@@ -7,6 +7,7 @@ class Gengetopt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7134042a80bb314db08216b1de2d293b5925ab729ba87649fdab4dc6298256f4" => :catalina
     sha256 "2ae5eeef439a6abc4d1f65965e1bafa9ac5ad0620cb4ef5e9444a4b2dbef1872" => :mojave
     sha256 "00f2578e7697c01d060a422e1be0ce8f4c6d23b365967ff7b5501d5cd6306dd1" => :high_sierra
     sha256 "57acd0ca20988a1b4f0f16383edb985549597b8a5266316e3a314b7775bab3c0" => :sierra
