0a597ff02ff0abff24dfeac0f626b7a298e65c5a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 21:35:29 +0000

    openexr: update 2.3.0 bottle.

--- Formula/openexr.rb
+++ Formula/openexr.rb
@@ -6,6 +6,7 @@ class Openexr < Formula
 
   bottle do
     cellar :any
+    sha256 "253c51ec98e9bd038d47d8482ea15e85183cc8dd718e5cf41facdfe9555f3717" => :catalina
     sha256 "111658d105dc894ea67e1fdc98597c73c1bf1fa64ac52f1287372ded53a1cdab" => :mojave
     sha256 "5673dfdc40bfc3f3495ec2fcff9ea5d5b65b244940f5bf686f79b838b97fdf3d" => :high_sierra
     sha256 "1418b449d38fbe7f37071ab6bf8f383d53d3540d41597be97eefdcc08749b7e3" => :sierra
