d49a314d7cb35b2166f764f8305704b4cf3cfab5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 14:33:02 +0000

    pcre2: update 10.33 bottle.

--- Formula/pcre2.rb
+++ Formula/pcre2.rb
@@ -7,6 +7,7 @@ class Pcre2 < Formula
 
   bottle do
     cellar :any
+    sha256 "7b92993a7ad0487cabc4395e3633d8294896fa9ffa9e46507d9a7ef25a213ab8" => :catalina
     sha256 "fb30c3eebba4483a10378bc8df52f96761fed4a1ac5572ad1bf4afbf2f8638c5" => :mojave
     sha256 "26b34cf7a846d49cdbdefa853227d7d4b02d6bd97c59dacdb0fb9dcf155444b5" => :high_sierra
     sha256 "a712fa6195fd968bae380e54709f46f66a94c69549a77f0836dae43ed95e11dc" => :sierra
