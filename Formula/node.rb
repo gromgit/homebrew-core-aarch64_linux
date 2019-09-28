3f5a4b3fc51176b17d89d16efbb50fd8b3bdd311
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 19:55:29 +0000

    node: update 12.10.0 bottle.

--- Formula/node.rb
+++ Formula/node.rb
@@ -7,6 +7,7 @@ class Node < Formula
 
   bottle do
     cellar :any
+    sha256 "e681e3e9cf08ed07da671a9ddb2c04e9f9cca0751080c19b6446b713a36855f1" => :catalina
     sha256 "e230187b694d98b4419de13f66b56a13d0330226b6125d341c2170b364714de7" => :mojave
     sha256 "e4ad2465c13b8ecd669f4d9e34ff4ca62aa2a2b3ac3b4f79d181c3becc13b470" => :high_sierra
     sha256 "c310d9d4924f813c3b4ff2d2d5c5ed10e8b11c7a5e08a176a007fba0061b21cf" => :sierra
