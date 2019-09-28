1eb598bac6dbd3e9234e50d7ab208074f5fcaac8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 22:21:54 +0000

    hugo: update 0.58.3 bottle.

--- Formula/hugo.rb
+++ Formula/hugo.rb
@@ -7,6 +7,7 @@ class Hugo < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9763a67a17ef5ae11c1a79ca97e5898c8d7999d74487432091b4f7251b3c6350" => :catalina
     sha256 "9fdca915591f7b84426a8c259979d955266dffd8fa89c705114c14ffd871443b" => :mojave
     sha256 "8fa4d09414c4d746d14d518b862bfd9adc9d6cc3de6e35588097c7d2678249d0" => :high_sierra
     sha256 "c32035708c6d60fce971eee46857f0b29b62abb4d24028be68878fa1cf6cc082" => :sierra
