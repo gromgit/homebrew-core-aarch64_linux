aa736af7103ccbf05f0e7ec4ab2e6b155f6d59c2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 14:01:28 +0000

    gdbm: update 1.18.1 bottle.

--- Formula/gdbm.rb
+++ Formula/gdbm.rb
@@ -8,6 +8,7 @@ class Gdbm < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "b20854a82cf1285c3be2c0890e45fa532bf2f5a9d9465694439c4a4c4310e528" => :catalina
     sha256 "2168d58856917ca996d12dedaa930643529c66046103fe55018afc51f2bc1fcb" => :mojave
     sha256 "ac688d571f9c00e09670440d67d2869a34dab0fb897ba0b183ed84fceffdbc9c" => :high_sierra
     sha256 "89d6db4fbffbe2184b4531faaebf0432a4b01e1ed92678ce6bd2f95c69dc9803" => :sierra
