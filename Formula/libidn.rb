6ad16fcb0c4a4e2172400331229cf7907b14f87f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 18:04:18 +0000

    libidn: update 1.35 bottle.

--- Formula/libidn.rb
+++ Formula/libidn.rb
@@ -7,6 +7,7 @@ class Libidn < Formula
 
   bottle do
     cellar :any
+    sha256 "a720f31e3d82a3e537ae2fbaf88bdfcf537b3452615a170c72714c111df4a661" => :catalina
     sha256 "d3741facdecc039b53d64392b6f8f4377a01d38bd0ce388db1d4d3606c7d5da9" => :mojave
     sha256 "94e508f0b66a0c37c7a05dad6fff49542439a05cf913980caff4142e58ae998f" => :high_sierra
     sha256 "ddbdf99759b5bf8840afdf4b50de61b3770fab70d26e08c0cd54c8df54c7da7d" => :sierra
