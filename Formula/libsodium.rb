61c16908894142db5252614aca462eaf8eaa002d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 17:57:39 +0000

    libsodium: update 1.0.18 bottle.

--- Formula/libsodium.rb
+++ Formula/libsodium.rb
@@ -7,6 +7,7 @@ class Libsodium < Formula
 
   bottle do
     cellar :any
+    sha256 "1cc1552646fee1e528a06e00e23fc57b7d76fb39b8287bacc716400a906d57f7" => :catalina
     sha256 "f0c5a9db15c8798edfa438b6911c6e354591f22ae3ed53696bf81c0a18afb0be" => :mojave
     sha256 "2b7cb72aaf03b3e6b4ae4e03133001abd134c8ebf008bbb788115df5df42cf97" => :high_sierra
     sha256 "e54c6a8913c83ef3201e15a4d68bed3ff329dd773d70aaf6cf7771ea93abe6a6" => :sierra
