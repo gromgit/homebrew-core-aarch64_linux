f0e6e91f7a9e0f4070d6420b739da1ab9bc6d9ee
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 22:30:05 +0000

    nspr: update 4.22 bottle.

--- Formula/nspr.rb
+++ Formula/nspr.rb
@@ -6,6 +6,7 @@ class Nspr < Formula
 
   bottle do
     cellar :any
+    sha256 "bf4d5e07f501ef6ac4b5c5fd64451ffc2110ff9256b6fd76614a7e6310e464fe" => :catalina
     sha256 "44b8d6ae65760da94e14b9bb742378c81b72821abd6ac4bb0be88c218234210a" => :mojave
     sha256 "5a466d443e42a227e6c6d972725558068a2121ee69a2807af993837281c0bcd1" => :high_sierra
     sha256 "f8e389b885ebef82929c0b6afc8b0ffbccbebb98f7267d81cad3e0d6d177f8c0" => :sierra
