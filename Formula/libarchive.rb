8182cefd4b9b7de36622d1b7eef0f86a1dbd62b5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 23:12:40 +0000

    libarchive: update 3.4.0 bottle.

--- Formula/libarchive.rb
+++ Formula/libarchive.rb
@@ -6,6 +6,7 @@ class Libarchive < Formula
 
   bottle do
     cellar :any
+    sha256 "d3eb488c913723c85258a026d294af420fd6d502837ec340e0878ec5b7703c9a" => :catalina
     sha256 "83f56d2bc1d70ab3b8fcf6bf32780368c0dbfdf6c4e16d324f39a5cbac4ca4c4" => :mojave
     sha256 "62bb5281530d97f33a502a865d6178fe4d66d94323e468d8711d24cf834c7dd7" => :high_sierra
     sha256 "4e41f900e82a342b90721e09831397684e98222213d02e78694574758f9ce393" => :sierra
