88a5debd77ddfca7c8154e99567acebb7f333b62
Igor Kapkov <igasgeek@me.com>
2019-09-28 11:37:06 +1000

    wskdeploy 1.0.0
    Closes #44646.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/wskdeploy.rb
+++ Formula/wskdeploy.rb
@@ -1,9 +1,8 @@
 class Wskdeploy < Formula
   desc "Apache OpenWhisk project deployment utility"
   homepage "https://openwhisk.apache.org/"
-  url "https://github.com/apache/incubator-openwhisk-wskdeploy/archive/0.10.0-incubating.tar.gz"
-  version "0.10.0-incubating"
-  sha256 "c5787a66e24c9fcbe45d8193fdacbb3263056784d8a46695cf05093cd4bf6641"
+  url "https://github.com/apache/openwhisk-wskdeploy/archive/1.0.0.tar.gz"
+  sha256 "74c02e8118a123cfad113dc75d5e7d256b18fb80ad9e27b2b95eb74b8677e483"
 
   bottle do
     cellar :any_skip_relocation
@@ -17,8 +16,8 @@ class Wskdeploy < Formula
 
   def install
     ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/apache/incubator-openwhisk-wskdeploy").install buildpath.children
-    cd "src/github.com/apache/incubator-openwhisk-wskdeploy" do
+    (buildpath/"src/github.com/apache/openwhisk-wskdeploy").install buildpath.children
+    cd "src/github.com/apache/openwhisk-wskdeploy" do
       system "godep", "restore"
       system "go", "build", "-o", bin/"wskdeploy",
                    "-ldflags", "-X main.Version=#{version}"
