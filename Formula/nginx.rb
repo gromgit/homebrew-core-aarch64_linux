1d5b09e044b2c9028cd403918daea0a9e1991260
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 21:48:29 +0000

    nginx: update 1.17.3_1 bottle.

--- Formula/nginx.rb
+++ Formula/nginx.rb
@@ -9,6 +9,7 @@ class Nginx < Formula
   head "https://hg.nginx.org/nginx/", :using => :hg
 
   bottle do
+    sha256 "484511b8e595f7dba53248439c320208fc4b885ba0baf78e05cbb7f89e80b755" => :catalina
     sha256 "11b43209912c3f75918d07a36c9e676ad042707a2f2948115edc3242daf0b28d" => :mojave
     sha256 "fb7d0e09b9fa42615cb53f03c2ddaad90946a79e9bf184d48f430b7f9ce1513e" => :high_sierra
     sha256 "61f78dccbe4df891bf0697ac906e6fe779ca2b3a61ab70b28412439c419d91a7" => :sierra
