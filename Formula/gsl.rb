8bff06d6bee75dfa2a2412e75e518bd480187412
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 23:39:35 +0000

    gsl: update 2.6 bottle.

--- Formula/gsl.rb
+++ Formula/gsl.rb
@@ -7,6 +7,7 @@ class Gsl < Formula
 
   bottle do
     cellar :any
+    sha256 "5972e8669b2560124278b43788a002e3ff22f024c761750a1a33b41d2002f292" => :catalina
     sha256 "6c88a066c85f76c93a20f6e3256fb9022d6e7db828d184be5b42fd0b322ca7b8" => :mojave
     sha256 "8213b1a73d038e499223ccae6d04afe6eb2d9455e327d9558351cf47a0431b84" => :high_sierra
     sha256 "8515f26e5a06a99097e87dc9b88ee79787b95448ab67f09b449aee4b0d67bdba" => :sierra
