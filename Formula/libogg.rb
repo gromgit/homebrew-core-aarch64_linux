8953990f4e78dc112ba246b6b078e99a43aaae5b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 14:29:35 +0000

    libogg: update 1.3.4 bottle.

--- Formula/libogg.rb
+++ Formula/libogg.rb
@@ -15,6 +15,7 @@ class Libogg < Formula
 
   bottle do
     cellar :any
+    sha256 "0a03b8a7307aeac70762fd4ee9837fff4ed523c34063a6aec52c5cf34c54695f" => :catalina
     sha256 "57278622c3550ef4cfe1a14aa885daacaac50e3571d4e8f2d6a6e5e9416b8e88" => :mojave
     sha256 "7883d41539869a4db165825e6f37c74dbd082fe0e839992aae8c2f9b09d55bea" => :high_sierra
     sha256 "ba70b9105ecf35ebc6df91306eb62708c7557c2aafdb1799fd7e7424b133c237" => :sierra
