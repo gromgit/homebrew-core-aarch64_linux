c8fe6fe214b558f11e23d140183aa016c18dd301
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 21:14:29 +0000

    htop: update 2.2.0_1 bottle.

--- Formula/htop.rb
+++ Formula/htop.rb
@@ -7,6 +7,7 @@ class Htop < Formula
 
   bottle do
     cellar :any
+    sha256 "c06ff60960f64f5c8395f53d7419cbcce2a22ee87f0cb0138352c8a88111d21c" => :catalina
     sha256 "77aa302765353b4085dcad52356d3264183e06310dda8d5bac64642299ea2902" => :mojave
     sha256 "0ebfb655b91566ba31f8effc94d642a43305ff95bdc9b30b46fadc132e2ced0c" => :high_sierra
     sha256 "ed93b86f011de155c5d261b8c9cc9cb81fd0017667bf3ebe26ee090716bcd650" => :sierra
