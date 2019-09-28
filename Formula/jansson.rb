ccb1f3437621da85e1dfa83a3f1b15010ccb28c7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 18:07:04 +0000

    jansson: update 2.12 bottle.

--- Formula/jansson.rb
+++ Formula/jansson.rb
@@ -6,6 +6,7 @@ class Jansson < Formula
 
   bottle do
     cellar :any
+    sha256 "3f11cd288c2a9ae85cbeb4cd778fc3da1b55a1eb1962c9487705dd01c352d252" => :catalina
     sha256 "79437c250f1b9fff4eab1a15385bbc38e5a29856b0efe3e5b0d68356d24d1f7b" => :mojave
     sha256 "aeb69f2744314df891be52ee4ff2fdb95e8991715a24f74858535063639f3491" => :high_sierra
     sha256 "ffafd42341f4a86ab91bf46e56a2cc4436d840998a9d053bff38467f8b6f4a1b" => :sierra
