7ca921b0c7013bbb5cf7bb75d143c9845859b98d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 18:12:03 +0000

    lz4: update 1.9.2 bottle.

--- Formula/lz4.rb
+++ Formula/lz4.rb
@@ -7,6 +7,7 @@ class Lz4 < Formula
 
   bottle do
     cellar :any
+    sha256 "7de6165d86c7a7ae01d254a5d0ea0a6d5876f90cffb63a2570942d46cca6373a" => :catalina
     sha256 "67ca428e60e2c2f6e524dd3de42629c1a616d28b2c743b66bf4cbdcc3b28ea46" => :mojave
     sha256 "7f60879b81a3a9ee52b3e9b87ed265c4934058b841e8f5320044f826b4660a92" => :high_sierra
     sha256 "00d3610cf09b0fcde34928890f5dac870ebcaffacd6eb51eaea05b754753e462" => :sierra
