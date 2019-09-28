c41047a2c64803699eeaaa931f4d8f46f155e28b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 22:04:24 +0000

    argon2: update 20190702_1 bottle.

--- Formula/argon2.rb
+++ Formula/argon2.rb
@@ -8,6 +8,7 @@ class Argon2 < Formula
 
   bottle do
     cellar :any
+    sha256 "f8e550c8597728bb9edc5a548497fd7b1219203932cd0f93ecc97a4fbf0bdad8" => :catalina
     sha256 "a76192a41826619fc399e7f6de5e6cb1c8a5fbe6bea4f2c1554daa830fa0e296" => :mojave
     sha256 "830016982e60870f50b3f6fc9a215d8cc4bda6061595f4883f7c11ab19ecba39" => :high_sierra
     sha256 "21889ac6ed40c792f1b372b5aa0d6b3be1be86577a4c1b06b08569124d2d0da2" => :sierra
