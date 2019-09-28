8d6998aefcf2f35c4a0384c84ca48be90fb0a998
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-28 23:53:57 +0000

    json-c: update 0.13.1 bottle.

--- Formula/json-c.rb
+++ Formula/json-c.rb
@@ -7,6 +7,7 @@ class JsonC < Formula
 
   bottle do
     cellar :any
+    sha256 "40a0c602bbc055eabd9a8be69fbf43f7e5b83a7b3b346581574f20e0b97e558a" => :catalina
     sha256 "9282e683104df6ee6cf495821de87a9badab080dfdcace1e631e518328e302d9" => :mojave
     sha256 "4f51e5ad713e467d1df189c8b594c7e9ae279ed4fc2b0a8d1b328a3c258135d7" => :high_sierra
     sha256 "279d88326f2f6aff9faf0c593ae4173c058e07ba0523f73107dfcbef3b54bd45" => :sierra
