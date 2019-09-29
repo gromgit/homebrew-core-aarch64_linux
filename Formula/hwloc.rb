3a70f9addd0c633eed12011c170a5b626ad65eaf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-29 11:58:01 +0000

    hwloc: update 2.0.4 bottle.

--- Formula/hwloc.rb
+++ Formula/hwloc.rb
@@ -6,6 +6,7 @@ class Hwloc < Formula
 
   bottle do
     cellar :any
+    sha256 "d3688db66e47f68ff32d639d62d4cf745995ddd2a3e596d4e5b3ce3603f80916" => :catalina
     sha256 "471cf1870f1719d87422e9a66488bb98b9e9bcb6af3c8482ccf2c8d3dad66997" => :mojave
     sha256 "f57cbfaa45150def690a473468909743ea38dda08166977bde552b3c3fe5704e" => :high_sierra
     sha256 "0b13e2d51c3644d0dc8febe826d1ab845f15bc5f32f0bb08f7f1b7c95dfbd9ef" => :sierra
