8b9b7f0b78612e820b2967a9499d369308703d69
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-29 09:40:37 +0000

    dialog: update 1.3-20190808 bottle.

--- Formula/dialog.rb
+++ Formula/dialog.rb
@@ -6,6 +6,7 @@ class Dialog < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2921441c6c6db1e8b7baa97469541245228c20790fb5b026dfe53d8b6e3c0c9f" => :catalina
     sha256 "47d5c4ac5913ff36ec7b8d4aa2d4fa2206ba4408dbdf5a24d7faac324e4264f7" => :mojave
     sha256 "d47c369a985ab8761cbdb3ceda72ed9d059557b206252f36b31830c1dab8f462" => :high_sierra
     sha256 "20fc5cc9a9ea20a873d3143cd629ebff9cac22fd14fef6f6f46c6bb0febeadf9" => :sierra
