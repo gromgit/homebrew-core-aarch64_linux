379493eb6ace930fe9c5f8a7c9d80f581a3af8da
chenrui <chenrui333@gmail.com>
2019-09-29 16:07:20 -0400

    wtf 20190925 (#44668)

--- Formula/wtf.rb
+++ Formula/wtf.rb
@@ -1,8 +1,8 @@
 class Wtf < Formula
   desc "Translate common Internet acronyms"
   homepage "https://sourceforge.net/projects/bsdwtf/"
-  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20190909.tar.gz"
-  sha256 "811bb5474b832ba433f209a707859ccbfb30966d51e9690e9c4074832ea09c89"
+  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20190925.tar.gz"
+  sha256 "4848dbb110848b0266cf302196cc4c29dfd5c402ce830838773647d2cd7a1ae4"
 
   bottle :unneeded
 
