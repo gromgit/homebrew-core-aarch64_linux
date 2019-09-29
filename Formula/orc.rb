ae5190c989b74a0932d9e150c9451753bf6a4423
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-29 12:51:01 +0000

    orc: update 0.4.29 bottle.

--- Formula/orc.rb
+++ Formula/orc.rb
@@ -6,6 +6,7 @@ class Orc < Formula
 
   bottle do
     cellar :any
+    sha256 "9509aa50e56e4cb34e5852fe69a1137cf231ef1b0f3412e0f6af7cb6af69c97a" => :catalina
     sha256 "d2f7fdd730740d23c0a3da22b3fac412b68974a0e6c4cc1f5b55abf8f075d374" => :mojave
     sha256 "a04f6a05f82c5dc8ba526992d294fccb9d3e1206fd21e9e870f04f226f88f86e" => :high_sierra
     sha256 "9a84e0cc0f7268c805ab3440302e40f5b4c44f48f47b284e23c75f1e7014c206" => :sierra
