2f0c90ffae04496cf203b17c60ae3a01cabaa680
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-29 12:39:44 +0000

    utf8proc: update 2.4.0 bottle.

--- Formula/utf8proc.rb
+++ Formula/utf8proc.rb
@@ -6,6 +6,7 @@ class Utf8proc < Formula
 
   bottle do
     cellar :any
+    sha256 "1f231714b571b71bb44282b94c0ab19c3ad8e7f4c57719bc9f963578011dfe1c" => :catalina
     sha256 "5cbd1c95723915eda5c5bb0437f49c3aeb50d768cfec87e9ff09507912244589" => :mojave
     sha256 "b1bbbd93be674c8304888ff5843a63433a5e3b7ccd7d8c8d9ceb450ce71a9c88" => :high_sierra
     sha256 "447fe55565ddc9e411a78939b829d2a175f608ea0497eae069c86befef471d34" => :sierra
