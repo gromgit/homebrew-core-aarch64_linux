9c81547859b044f8de8afb1b031da84afaab551c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-29 12:48:51 +0000

    cabextract: update 1.9.1 bottle.

--- Formula/cabextract.rb
+++ Formula/cabextract.rb
@@ -6,6 +6,7 @@ class Cabextract < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d60179c028ac5fb69580f2f01cd9f59c1d1544c8f6d84a230a7dd3587f3c27e0" => :catalina
     sha256 "cd27b939a0191d4dfff8ae13300b260b5ae01c563a21613718160012a982d5e8" => :mojave
     sha256 "c77caa7c32b4320f9e887abeea99261345e83f03e2c321ec9e99ddd9c75f5d98" => :high_sierra
     sha256 "c531546af69afda3101f07b509eb143cdaef00f4fdcbdd420e60287508a87e5e" => :sierra
