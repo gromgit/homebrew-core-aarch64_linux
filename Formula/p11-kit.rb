b1ef22e89dbfb5fd27724d930ae7b8da98208b2a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-29 09:10:57 +0000

    p11-kit: update 0.23.17 bottle.

--- Formula/p11-kit.rb
+++ Formula/p11-kit.rb
@@ -5,6 +5,7 @@ class P11Kit < Formula
   sha256 "5447b25d66c05f86cce5bc8856f7a074be84c186730e32c74069ca03386d7c1e"
 
   bottle do
+    sha256 "82ca19bc7ca3be640c16f9ba1329f563f093f6ee6fb3700d75786836aa8d94fc" => :catalina
     sha256 "6eef561b7f7159a96ae5d161fe0177e84fa0f929bbb89e799039cb1162d11387" => :mojave
     sha256 "f4035d5739c5bb31e6db0d523ee9b44e18a35f22bb73bf49fc2f1dd8035fac3f" => :high_sierra
     sha256 "de9ed5f549f2cb54e6228868a7724be6a1395ad5027ca753bb00068828c7a592" => :sierra
