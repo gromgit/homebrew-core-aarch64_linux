d72c6fb31b1a5f8f6e8d1939ad9f73770e20411d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-29 13:15:21 +0000

    fltk: update 1.3.5 bottle.

--- Formula/fltk.rb
+++ Formula/fltk.rb
@@ -6,6 +6,7 @@ class Fltk < Formula
   sha256 "8729b2a055f38c1636ba20f749de0853384c1d3e9d1a6b8d4d1305143e115702"
 
   bottle do
+    sha256 "d0ff3728a8da506e399b094b0e2a94ffef5a32805308d73fd2fb5fd0e402c88b" => :catalina
     sha256 "3ea6ccc2fec9151f3ed0f20761794b9fe0477d168dbc4e83ba88b3f3d16c530b" => :mojave
     sha256 "6edac0b91f19783376ec95c84819405a6f029d7d2bf8ac636d421682fc064e34" => :high_sierra
     sha256 "e2bd28a348c8fbf948f2400d3df29ba786a2ca9cc3f87b3727477fb49ebf57f0" => :sierra
