0b0835c1dcc1d57c9042b3710a15c8377fdc112c
Igor Kapkov <igasgeek@me.com>
2019-09-30 08:30:20 +1000

    folly 2019.09.23.00
    Closes #44703.
    
    Signed-off-by: Igor Kapkov <igasgeek@me.com>

--- Formula/folly.rb
+++ Formula/folly.rb
@@ -1,8 +1,8 @@
 class Folly < Formula
   desc "Collection of reusable C++ library artifacts developed at Facebook"
   homepage "https://github.com/facebook/folly"
-  url "https://github.com/facebook/folly/archive/v2019.09.16.00.tar.gz"
-  sha256 "a212b49713e8efcfa3116cf2f702af313e755156f757459dd13ecf20608015aa"
+  url "https://github.com/facebook/folly/archive/v2019.09.23.00.tar.gz"
+  sha256 "ad4f066bafa255d2953a1939c2c8a6fc803d68a3aa9fdb83f949e2b0839779b3"
   head "https://github.com/facebook/folly.git"
 
   bottle do
