30c51fe32b3671abfe19082fb9cd5234673a2870
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-29 12:53:49 +0000

    lzlib: update 1.11 bottle.

--- Formula/lzlib.rb
+++ Formula/lzlib.rb
@@ -6,6 +6,7 @@ class Lzlib < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8d43e434cb121e5fd9c1be9adfc0ff6c8afa8f51c786a5b855117eb6f3e9e2dd" => :catalina
     sha256 "3c28dea3721b03227d660c7c453673d3cb40f00f41e2e4ba3f163a7171926da0" => :mojave
     sha256 "a49b8dfcf257d31c46841a733f1925020dd49493554d049a479467e602e3e796" => :high_sierra
     sha256 "3c6df94a873fc2268478e10c23d1aa631c6b29e1afff38de63e2839ad0f1968c" => :sierra
