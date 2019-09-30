8bea1e3c20044071b95f31a3319f7d95f00ceb79
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-30 18:31:35 +0000

    py2cairo: update 1.18.1 bottle.

--- Formula/py2cairo.rb
+++ Formula/py2cairo.rb
@@ -6,6 +6,7 @@ class Py2cairo < Formula
 
   bottle do
     cellar :any
+    sha256 "db4127cd316e919e5c49be339e6ea0ddb20ae3eacf21969b4e48bd60b9c6622f" => :catalina
     sha256 "03ad3e2f9143464e5168ce2f225e0b064b070737301216a67658457c0ca4f456" => :mojave
     sha256 "a50b0c485a57c92b994de627796d6daf7c98b6aa5c35d89b67ee5bef5e26e458" => :high_sierra
     sha256 "f095cacdd07d60a81a01ca1468df6cd544f2684050d051eaecd7808adbe3928e" => :sierra
