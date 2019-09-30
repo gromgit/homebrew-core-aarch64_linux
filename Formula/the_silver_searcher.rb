7b4006a726d610b6a8571b51c616a5a7647170c7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-30 23:32:09 +0000

    the_silver_searcher: update 2.2.0 bottle.

--- Formula/the_silver_searcher.rb
+++ Formula/the_silver_searcher.rb
@@ -7,6 +7,7 @@ class TheSilverSearcher < Formula
 
   bottle do
     cellar :any
+    sha256 "6fd80fdd0896dae09c01d3c9785ddd658bb5f2f229e7d011d3fbdde887bc35d0" => :catalina
     sha256 "e57f89664f48c131dfb462dc4be2f5265867d827f82efb1c3841ba71d9156ed0" => :mojave
     sha256 "0bf5394d8ab5f61b8fbb593249f556f13b358d16eb0d3c97215be3da0476e94b" => :high_sierra
     sha256 "2365e24e5d0b1bef64b35c6a8f9e4f61d1f38eafe38c06d6e0acefc6a9a955db" => :sierra
