b4ca1716fe1d0462fd0115d234f144e4a36cc220
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-30 20:22:02 +0000

    gst-plugins-base: update 1.16.1 bottle.

--- Formula/gst-plugins-base.rb
+++ Formula/gst-plugins-base.rb
@@ -5,6 +5,7 @@ class GstPluginsBase < Formula
   sha256 "5c3cc489933d0597087c9bc6ba251c93693d64554bcc563539a084fa2d5fcb2b"
 
   bottle do
+    sha256 "54ab689dd69b17d4dc78f98b8e36b44d65df8310500f9461feab5522504ebce8" => :catalina
     sha256 "4acaaf10a1174a9b4339653683a17601dbaa7be69e379680fc51650e64c3b19b" => :mojave
     sha256 "1a0f81a6161550b42e7bbd349eee7accb5b5e489f6b24756ff7d2847bd8eaafc" => :high_sierra
     sha256 "3adaa7060d255bf9198db9c72894d605eca6f15507e1b725c8f53637cb93a702" => :sierra
