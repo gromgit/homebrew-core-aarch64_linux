727cd552669b15716a63f22109288de848de6c38
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-30 21:05:18 +0000

    libshout: update 2.4.3 bottle.

--- Formula/libshout.rb
+++ Formula/libshout.rb
@@ -6,6 +6,7 @@ class Libshout < Formula
 
   bottle do
     cellar :any
+    sha256 "c0a0c87887e331d2f4b174d956e974bae44d427f282024f6a7b36910da7dbe05" => :catalina
     sha256 "14144ef978dbceb4b858ebeb667be83721ba4b3742fd0e0743ab76a9b2f99095" => :mojave
     sha256 "f9fef02da35d628c601f24726fa2d748f350692d4bf4c196d28bd08b0f4786eb" => :high_sierra
     sha256 "1d246580781a0bf5240363da62a476499a16948935f1b0693d05660ccd5aa9de" => :sierra
