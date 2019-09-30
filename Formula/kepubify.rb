44b25ae86e66c4537d50b395163957396fba1869
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-30 06:45:29 +0000

    kepubify: add 2.3.3 bottle.

--- Formula/kepubify.rb
+++ Formula/kepubify.rb
@@ -5,6 +5,13 @@ class Kepubify < Formula
   sha256 "57bbd5c5f24eab4d5eb5ab2dd9fb7b534afbfaf78d0c07fbf8c570b2fe5fae0a"
   head "https://github.com/geek1011/kepubify.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4b6c99b8a4bd18131c01a2cfd4cc855140f28bb828f586c98d98b18bf686b1a7" => :catalina
+    sha256 "4ac2a6391a16528f35127f88a52476086d8b62833be8afe2234fb99d23c151ad" => :mojave
+    sha256 "0856de98e48b3ceedc567399cf08871f51ecb4df9e091e4e9aebb6437ff9c98a" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
