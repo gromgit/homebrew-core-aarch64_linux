2614c8a9882ca672dbd77ac5296126568ab77e5b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-30 06:58:31 +0000

    sops: update 3.4.0 bottle.

--- Formula/sops.rb
+++ Formula/sops.rb
@@ -7,9 +7,9 @@ class Sops < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "65df7ca6d13386edc9c99f3c95c2e7b18cf586fcd228f6644f974c5ca508b52e" => :mojave
-    sha256 "a465ee3a048e1ef83497325dc76370d96404f6789178a4efbd19215630fd58dd" => :high_sierra
-    sha256 "8345f9f6375d27756ad9a8b6adab4c249bdc223b863e20258f869da79f90ea69" => :sierra
+    sha256 "184c2b79a276faa3efb6ec26c8ebb98d1e24b0cdd8bade41136943d787d6ee9c" => :catalina
+    sha256 "ab29594d5eb30f6470adb2c9937b9288f024a233d961dbbb965e03f58355dff6" => :mojave
+    sha256 "3f4baab5e9e5e53bec5f76eb1c7786db53c6e83f6dce1c20aabbd5bb180f486f" => :high_sierra
   end
 
   depends_on "go@1.12" => :build
