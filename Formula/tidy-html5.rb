8842938570a8f839c5d794ab4e0b7b1457f853ee
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-30 12:50:57 +0000

    tidy-html5: update 5.6.0 bottle.

--- Formula/tidy-html5.rb
+++ Formula/tidy-html5.rb
@@ -7,6 +7,7 @@ class TidyHtml5 < Formula
 
   bottle do
     cellar :any
+    sha256 "fb2134180fbdb92cc10f3fad33c769073adceb7796e465db7dbc3778f7d3547a" => :catalina
     sha256 "bd3ca7dc82a913c8576716cbcc957260251132f6dd7b8c526c9ef0c4674faf0f" => :mojave
     sha256 "af9633f1578980fe3d4351c3d71b4b83cc79f814d87310e4b7d05830c53c9621" => :high_sierra
     sha256 "6c8f843d25d6964b18d4c2fa15aaf2606b36decbbe65c31b38a7982e499a9d28" => :sierra
