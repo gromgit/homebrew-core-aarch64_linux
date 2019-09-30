a167183ae28d7717b0b3e308d751f54d98a0c4a3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-30 10:48:22 +0000

    nettle: update 3.4.1 bottle.

--- Formula/nettle.rb
+++ Formula/nettle.rb
@@ -7,6 +7,7 @@ class Nettle < Formula
 
   bottle do
     cellar :any
+    sha256 "338da826ad5127a98f1658c01f2bd128aa9383b3581c9beebc0f0ebc88b0d089" => :catalina
     sha256 "9e7f78a4cc96ca57f75ca1d37cc12c11655b7e0aa7109da4becd0408a1e2ed6b" => :mojave
     sha256 "4327e8e4c4760653113f0bc4a7b0bada37b2d820f6c3ba759832e59ed553cb9a" => :high_sierra
     sha256 "4624e3b0964d695408cf45330bab8cda2536002834f96202f7a37007407123fd" => :sierra
