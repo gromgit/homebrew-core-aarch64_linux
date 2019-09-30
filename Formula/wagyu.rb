a913d418e290a37953d06fe31e9ef4b2e14f4b51
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-30 07:37:06 +0000

    wagyu: add 0.6.1 bottle.

--- Formula/wagyu.rb
+++ Formula/wagyu.rb
@@ -5,6 +5,13 @@ class Wagyu < Formula
   sha256 "2458b3d49653acd5df5f3161205301646527eca9f6ee3d84c7871afa275bad9f"
   head "https://github.com/ArgusHQ/wagyu.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a3a5aa09189771137686bdf191e8c4d92605046e72f85921cfb9ae25ded7f0d0" => :catalina
+    sha256 "63ce808a32a7cf7f73e4fc37d3665c06ddcfa920720d0a0175528e93b86d9992" => :mojave
+    sha256 "82c1f93d5905a7614d75f45314f584aebe7181050bb3476483a6c954e0ba3997" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
