03f829faeceb4810b3e787d4fa81b1819e8451ae
chenrui <chenrui333@gmail.com>
2019-09-29 22:56:39 -0400

    cargo-completion 0.39.0 (#44679)

--- Formula/cargo-completion.rb
+++ Formula/cargo-completion.rb
@@ -1,8 +1,8 @@
 class CargoCompletion < Formula
   desc "Bash and Zsh completion for Cargo"
   homepage "https://github.com/rust-lang/cargo"
-  url "https://github.com/rust-lang/cargo/archive/0.38.0.tar.gz"
-  sha256 "c0a81ef51196cde811ff425213bc5fc19c4ef02d6fe32725249103ed5d85db96"
+  url "https://github.com/rust-lang/cargo/archive/0.39.0.tar.gz"
+  sha256 "1fade63d5c09eab6fa234a1fdf64693ccf1b63398a316ad27edeb1f76ee4a57e"
   version_scheme 1
   head "https://github.com/rust-lang/cargo.git"
 
