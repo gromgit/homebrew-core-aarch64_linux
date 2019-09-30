dd02a43124200c7c95f4a7c1b77ff109e0860f35
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-30 14:04:13 +0000

    opam: update 2.0.5 bottle.

--- Formula/opam.rb
+++ Formula/opam.rb
@@ -7,6 +7,7 @@ class Opam < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2eaa945aa00834374c7170da065b5d2c15a5df25130bb9b336fb17097f7000cd" => :catalina
     sha256 "73d4d1936a7862127e77efa45a07af5e4b49ed00227e9455671ca2a87a32875c" => :mojave
     sha256 "8799c5a87853dba2a74a3cbc58a7ecb39197ade8372726d044f617ca6bf55a33" => :high_sierra
     sha256 "2e81e914a8259d6f97e808d4def682154d45545efc6efc67c2c344dc5e97e1d8" => :sierra
