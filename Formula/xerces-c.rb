2494fe0350219ec14e36f7baaeb41471d8236051
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-30 23:17:43 +0000

    xerces-c: update 3.2.2 bottle.

--- Formula/xerces-c.rb
+++ Formula/xerces-c.rb
@@ -7,6 +7,7 @@ class XercesC < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "8bcc9b20b0b3df89ec53900e0b3b09ea1bbc3159b4cffc4b8ef4f62413621924" => :catalina
     sha256 "fab62b22422c24b0218cae42f7f81ad736db316d9bde4218272cdf7b174c313f" => :mojave
     sha256 "e62fba2c06fd03edf0491b54f753d10c4ca9e73e97c24389b749e655f9199b50" => :high_sierra
     sha256 "8390cdf10fcc8b65a1f295eacf8b3fec34776d18219b8a8ce565592ee3b03372" => :sierra
