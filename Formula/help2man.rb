27e50eecc0fcab3e643a6f36b25627875f2402c3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-01 08:34:06 +0000

    help2man: update 1.47.11 bottle.

--- Formula/help2man.rb
+++ Formula/help2man.rb
@@ -7,6 +7,7 @@ class Help2man < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "de8418f205b74a0194d44f061cbdecaceb9353ee8991651c75380d8d2394077f" => :catalina
     sha256 "0aecf41b06ca3914f21e370060652a4209560c13acde2b1bb9ffd03b1990f54a" => :mojave
     sha256 "0aecf41b06ca3914f21e370060652a4209560c13acde2b1bb9ffd03b1990f54a" => :high_sierra
     sha256 "3b0ce6bc3ddc6fcc6290132fac3da4c083a24dcaffad4323db1e4e8d99d6062d" => :sierra
