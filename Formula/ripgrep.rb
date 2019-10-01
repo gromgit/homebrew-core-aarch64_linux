34473e19122aadbfcbef68823828c4db586297c7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-01 09:50:11 +0000

    ripgrep: update 11.0.2 bottle.

--- Formula/ripgrep.rb
+++ Formula/ripgrep.rb
@@ -7,6 +7,7 @@ class Ripgrep < Formula
 
   bottle do
     cellar :any
+    sha256 "780e74bc09f13e5ac6a38894cf46e6b09e3ea0e1d5b24bb8f516f7c395363edf" => :catalina
     sha256 "8a9673593698833a3e79407423c549499c2096c206f2309baf3a6a1fa58bc83c" => :mojave
     sha256 "e0f43c540165eb7c181328c9e18a28f9fb65ca6e178a2d898915760c212bfcd6" => :high_sierra
     sha256 "3bb6e27a3adaf94129c18491a552899a354ce36846ec2a849ca5ad40e848ad62" => :sierra
