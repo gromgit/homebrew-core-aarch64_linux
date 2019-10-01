f00727ae45d703fabdd8af2d1d963d14d0fcfa1c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-01 12:44:42 +0000

    libxmlsec1: update 1.2.28_1 bottle.

--- Formula/libxmlsec1.rb
+++ Formula/libxmlsec1.rb
@@ -7,6 +7,7 @@ class Libxmlsec1 < Formula
 
   bottle do
     cellar :any
+    sha256 "fb781945c6de7dab1e51502d8aee4c909f37fa6e862a42a4fe58bec56e350c07" => :catalina
     sha256 "c9cc625bf2b00cdaa9ed8d59e873dfe0928931b6e62392a877d5a437757b3c5c" => :mojave
     sha256 "7ebacb8f7c89a0c8828cf721ba7385b3f0d7561549daaa0941b9a3a15186bc57" => :high_sierra
     sha256 "cec2b227a01fe451566ce854f94e187ec8f7e045a0ca570bc927bb58d3407a72" => :sierra
