415d0d7e37fdfa06ee2e784de70d8cfb3f1dbaeb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-01 07:58:13 +0000

    gst-plugins-ugly: update 1.16.1 bottle.

--- Formula/gst-plugins-ugly.rb
+++ Formula/gst-plugins-ugly.rb
@@ -5,6 +5,7 @@ class GstPluginsUgly < Formula
   sha256 "4bf913b2ca5195ac3b53b5e3ade2dc7c45d2258507552ddc850c5fa425968a1d"
 
   bottle do
+    sha256 "88a6413d10e0ad2a2a1b36372ba3831b902ec78ada06c2f763e0cf7eab2d5a11" => :catalina
     sha256 "013482b8c2b286e9f97b61d6aed9a64f954f158d6b93f8cb7e054974a83b9b8a" => :mojave
     sha256 "28c1bf0e8f8c83eec65d49ce829bb37c821c48f76c287aa6f3f19daba5e077eb" => :high_sierra
     sha256 "35ad1217120377f824ba83b318e1bac892ff42529c514ae0eed3badd43a3c5e1" => :sierra
