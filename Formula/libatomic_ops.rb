ecbc05c90f70d8d7926a5dc5bbbf1b5d9347fb27
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-01 09:23:36 +0000

    libatomic_ops: update 7.6.10 bottle.

--- Formula/libatomic_ops.rb
+++ Formula/libatomic_ops.rb
@@ -6,6 +6,7 @@ class LibatomicOps < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d102cc71b5959eac9f8ecbc5029801ea2a544ad5b6602c586e5d5d33c67ebd55" => :catalina
     sha256 "b509d8669c5336775b0462c3e1464419d083bccc0bf43f8dab8fa3eb0ac44405" => :mojave
     sha256 "2e3053c22101fd8baf693ca404f258763b15e77372b5293e999f8bbc8b032522" => :high_sierra
     sha256 "d94aa0351ce2de312d0b31a64d48059473a170d9704968378cf121ad097d7d9b" => :sierra
