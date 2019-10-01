b532d0f7b49c8909803e420097305ba842f0d178
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-01 07:48:56 +0000

    gitlab-runner: update 12.3.0 bottle.

--- Formula/gitlab-runner.rb
+++ Formula/gitlab-runner.rb
@@ -8,6 +8,7 @@ class GitlabRunner < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f9c96585fe7da9df4d02d22dc659eea2c9b24c6a42130f616d89d7342bdf5ffc" => :catalina
     sha256 "c7d325e3468921ce8728ac3654856c0470b21d3bb52c367ff67ea9a5d1435184" => :mojave
     sha256 "484bea49e93b110bb41905f5fd29aac441d451a7b3af5593bc3e48eed9bcf181" => :high_sierra
     sha256 "d8845eb8570eafbc6c6dcbf01c4b8b6f4b3142a32498da3925c498673dbac655" => :sierra
