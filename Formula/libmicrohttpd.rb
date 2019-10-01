f524892ab40b918a9ea81c97fbe258c2f7c9f7c2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-01 20:21:50 +0000

    libmicrohttpd: update 0.9.63 bottle.

--- Formula/libmicrohttpd.rb
+++ Formula/libmicrohttpd.rb
@@ -7,6 +7,7 @@ class Libmicrohttpd < Formula
 
   bottle do
     cellar :any
+    sha256 "0d9d914ee9491289a10f751dce3e57efc02544cd520ec647d939a1b58baa60ef" => :catalina
     sha256 "065c03c15e7cddac444a335ae9a4a581b9cb10f9ea4edbafab86cb9ec083ce49" => :mojave
     sha256 "2b022a903526ca0d939b8660fd1d9ca82877c29734b177bb865631a6fbf0e375" => :high_sierra
     sha256 "60a0909d2d0f9784060686b71c58afae980601141d19d20d62c78c451467245c" => :sierra
