54e9311c86e88194b4f5a90fbfe2c0e590664e20
Dick Cavender <dcavender@pivotal.io>
2019-10-01 15:25:44 -0700

    apache-geode 1.10.0 (#44788)

--- Formula/apache-geode.rb
+++ Formula/apache-geode.rb
@@ -1,14 +1,14 @@
 class ApacheGeode < Formula
   desc "In-memory Data Grid for fast transactional data processing"
   homepage "https://geode.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=geode/1.9.1/apache-geode-1.9.1.tgz"
-  mirror "https://archive.apache.org/dist/geode/1.9.1/apache-geode-1.9.1.tgz"
-  sha256 "4243c1361fc7dec3f77801ec164b43c2fcda8526972516033e1dec5b0b2d544d"
+  url "https://www.apache.org/dyn/closer.cgi?path=geode/1.10.0/apache-geode-1.10.0.tgz"
+  mirror "https://archive.apache.org/dist/geode/1.10.0/apache-geode-1.10.0.tgz"
+  mirror "https://www.apache.org/dist/geode/1.10.0/apache-geode-1.10.0.tgz"
+  sha256 "d13a7e91d11ce14cc7ad3c024b25e541343653b8ad2a88d461a59fec97d59655"
 
   bottle :unneeded
 
-  # Geode should not be compiled on Java 9+ yet (see https://issues.apache.org/jira/browse/GEODE-3)
-  depends_on :java => "1.8"
+  depends_on :java => "1.8+"
 
   def install
     rm_f "bin/gfsh.bat"
