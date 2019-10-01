cc0c028868fe083db25ec4098ee50760a253b9f6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-01 13:00:26 +0000

    links: update 2.20.1 bottle.

--- Formula/links.rb
+++ Formula/links.rb
@@ -6,6 +6,7 @@ class Links < Formula
 
   bottle do
     cellar :any
+    sha256 "02e54327ff6086906bc9d1e499298f52ae5032b2e68f625541a9151cf23dcee5" => :catalina
     sha256 "66ed3179fd7e8e439d3c19b5e11a9903a16ac994baef6aa3b6595e697ebce14d" => :mojave
     sha256 "89b10181d05992aead0afb46a818c9fa3c1218b215146efe864574c877c84545" => :high_sierra
     sha256 "aa2902faaaff12df4013e05c7cd85ff09be788378729091fc0cf140a18c060c2" => :sierra
