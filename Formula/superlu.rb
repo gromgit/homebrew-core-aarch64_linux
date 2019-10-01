fea757affc0c5cc7ce39939a6d3238d0c1db3c1e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-01 14:26:30 +0000

    superlu: update 5.2.1_4 bottle.

--- Formula/superlu.rb
+++ Formula/superlu.rb
@@ -7,6 +7,7 @@ class Superlu < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c8cf07d7ce9841af6f5ff93f3bab779c385e0c6f84ac1c5a49d6c16ac2275120" => :catalina
     sha256 "d47a98b1d94b041aa93835c10e024f2e3bb4f6535f1dd5c142343e5cf395e785" => :mojave
     sha256 "5e02b75c1053a83ae4d07e3450d1cff929b825e2296327cbae038ace4d077e3a" => :high_sierra
     sha256 "f2038e0b4edb755631cc4f9b42dc362996d8161fa9aad306a412c7e8ff39d9f8" => :sierra
