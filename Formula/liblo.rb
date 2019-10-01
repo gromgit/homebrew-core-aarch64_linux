6fa24e89cfb58b67c6541ce8f3785d159712947f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-01 20:08:56 +0000

    liblo: update 0.29_1 bottle.

--- Formula/liblo.rb
+++ Formula/liblo.rb
@@ -7,6 +7,7 @@ class Liblo < Formula
 
   bottle do
     cellar :any
+    sha256 "f87426829c595b95559f0efba6fae738f6fbc3995cb4223a9946d18509c7c89a" => :catalina
     sha256 "bec4aa8cf37050f2b35123d1ca894801e5ae75d95034e7ee2d3b365922180332" => :mojave
     sha256 "e69e5f3405e2c55b25864a62dcdd3d454c9e460fe54d87447a1821973f9a6e67" => :high_sierra
     sha256 "c39875452da500b334cb532d903c284b178cb3f9dd15e0201228dfbe78511985" => :sierra
