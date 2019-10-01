59e58de4a06cb872edfd086f0ae8f64f4ed10325
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-01 13:59:45 +0000

    fftw: update 3.3.8_1 bottle.

--- Formula/fftw.rb
+++ Formula/fftw.rb
@@ -7,6 +7,7 @@ class Fftw < Formula
 
   bottle do
     cellar :any
+    sha256 "271f7a5febd92b8757948c86fa24546aca9025087406056b7cf4036ca97b29e4" => :catalina
     sha256 "fafc0d1b43619cf3ac63946a4380782747903378dfb4f7b21387c40131ad2d59" => :mojave
     sha256 "da4329aec211bdc19e9404b35318517d8a3d029dde6e0e28dac646330a7554c4" => :high_sierra
     sha256 "eb140060084d40bc484f8e7048b516b7afe92902c6da04f9e283bfa83f271551" => :sierra
