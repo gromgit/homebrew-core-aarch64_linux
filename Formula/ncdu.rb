5484cc3e829f0483001bc0210931e0aace6b6eee
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-01 07:33:51 +0000

    ncdu: update 1.14.1 bottle.

--- Formula/ncdu.rb
+++ Formula/ncdu.rb
@@ -6,6 +6,7 @@ class Ncdu < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "757cbcc5ed617cbe01b66d04b4db07fa6ef568077f6f66eb46cbf0c0cabb3d7c" => :catalina
     sha256 "4affa4dad2978f22cb284a3efed3b63408e0e4d4d77ac851266df15386a02c30" => :mojave
     sha256 "9fcb90b8776c442b94d387f679cf8dfceb6c7f043f33212aa77ecc72db6a8ff8" => :high_sierra
     sha256 "874b76e2ff36b231358596581ed2e8536682bf17e65e5762ae5d5aa20e511a3a" => :sierra
