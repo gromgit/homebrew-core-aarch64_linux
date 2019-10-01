7ba9dac1ee7aabf16f2a3f2d8b11cbe5ad2c701b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-01 09:03:26 +0000

    enchant: update 2.2.7 bottle.

--- Formula/enchant.rb
+++ Formula/enchant.rb
@@ -5,6 +5,7 @@ class Enchant < Formula
   sha256 "1b22976135812b35cb5b8d21a53ad11d5e7c1426c93f51e7a314a2a42cab3a09"
 
   bottle do
+    sha256 "d5eedc2985d47ad2c68fc02920ce159ffed812dbe21a0798a0c19b9c764b73e0" => :catalina
     sha256 "31b8df7f3a7c9000a9172a08e95563e4c0aa5eff9166d5908637adea3f3853c4" => :mojave
     sha256 "5175504ba38adc84dc8b0eb756270de9509c0702aca2eafc399a237bba67cb0b" => :high_sierra
     sha256 "cbdd497839d06d13c0457ae4ce827bcdbc85e976902598b39fae14821cc1a3b9" => :sierra
