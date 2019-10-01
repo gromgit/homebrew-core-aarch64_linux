0a17b8e50963de12e8ab3de22e53fccddbe8a226
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-01 02:14:40 +0000

    kubernetes-helm: update 2.14.3 bottle.

--- Formula/kubernetes-helm.rb
+++ Formula/kubernetes-helm.rb
@@ -8,6 +8,7 @@ class KubernetesHelm < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4745dd0bc615ecb4ed1ab0cf0a2bedc91e8fc6d468d9bdf253f741e2ab4d06ab" => :catalina
     sha256 "35424b638f34e451a572959e2548c97a18589a97e997053f4c25e39511e17ac8" => :mojave
     sha256 "b0dabf5935ba18efd9c247f75b8b50efcdb34e6f2d74bc8ccb781bc78ca612c3" => :high_sierra
     sha256 "4e43a2e5ca35248314a84d9e791de028a9634b7060ecb6188192f59e7010faec" => :sierra
