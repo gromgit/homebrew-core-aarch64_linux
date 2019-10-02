9494977d03f10b1d8473dca71171dd47593b6717
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-02 06:04:58 +0000

    dafny: add 2.3.0 bottle.

--- Formula/dafny.rb
+++ Formula/dafny.rb
@@ -4,6 +4,13 @@ class Dafny < Formula
   url "https://github.com/dafny-lang/dafny/archive/v2.3.0.tar.gz"
   sha256 "ea7ae310282c922772a46a9a85e2b4213043283038b74d012047b5294687d168"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "73faff043056b7e45842d6c528a530d4ce4f5072f92ed7612cdd458d64de2985" => :catalina
+    sha256 "c0a0e194956400b852678cddfd8f9135ccdb8fd1949dc24611d0a37ec9b5640a" => :mojave
+    sha256 "2bf8cf46f3233236273c8cfdec90e7f8168fc160342ac36a0c6bc8f22f57559b" => :high_sierra
+  end
+
   depends_on "mono-libgdiplus" => :build
   depends_on "nuget" => :build
   depends_on "mono"
