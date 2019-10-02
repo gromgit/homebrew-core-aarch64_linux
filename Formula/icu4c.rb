a806a621ed3722fb580a58000fb274a2f2d86a6d
Thierry Moisan <thierry.moisan@gmail.com>
2019-10-02 13:07:31 -0400

    icu4c: update homepage and url (#44812)

--- Formula/icu4c.rb
+++ Formula/icu4c.rb
@@ -1,8 +1,7 @@
 class Icu4c < Formula
   desc "C/C++ and Java libraries for Unicode and globalization"
-  homepage "https://ssl.icu-project.org/"
-  url "https://ssl.icu-project.org/files/icu4c/64.2/icu4c-64_2-src.tgz"
-  mirror "https://github.com/unicode-org/icu/releases/download/release-64-2/icu4c-64_2-src.tgz"
+  homepage "http://site.icu-project.org/home"
+  url "https://github.com/unicode-org/icu/releases/download/release-64-2/icu4c-64_2-src.tgz"
   version "64.2"
   sha256 "627d5d8478e6d96fc8c90fed4851239079a561a6a8b9e48b0892f24e82d31d6c"
 
