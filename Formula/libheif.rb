bc144932eb8e4384b7fc4a2da1ce29e2c9163be3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-02 06:48:22 +0000

    libheif: update 1.5.1 bottle.

--- Formula/libheif.rb
+++ Formula/libheif.rb
@@ -6,6 +6,7 @@ class Libheif < Formula
 
   bottle do
     cellar :any
+    sha256 "28833a4c3ca9806930944f3f714ea95ec7d5c2e8e9b7120cf32d212aa350c772" => :catalina
     sha256 "09b000d29523544e6ebbaabce1550b6e51575d3b61522621e01efd7a8fab1622" => :mojave
     sha256 "2d1831b3668450da2d30befc0caa18916cdd429a1c9f22557ec03e3e584549db" => :high_sierra
     sha256 "9bee18eba422e4c23bf6260321d9a1680a7ba795e9d983e5c628ac0bf1d108e0" => :sierra
