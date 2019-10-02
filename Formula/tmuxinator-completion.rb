761ca40491bb6a31594574b5e9837927c3e40512
Igor Kapkov <igasgeek@me.com>
2019-10-03 05:14:14 +1000

    tmuxinator-completion 1.1.2 (#44749)

--- Formula/tmuxinator-completion.rb
+++ Formula/tmuxinator-completion.rb
@@ -1,8 +1,8 @@
 class TmuxinatorCompletion < Formula
   desc "Shell completion for Tmuxinator"
   homepage "https://github.com/tmuxinator/tmuxinator"
-  url "https://github.com/tmuxinator/tmuxinator/archive/v1.1.1.tar.gz"
-  sha256 "cc293578bca43ba5cf0d60c1355c6aa1da9d923a0acc274a47ceab03812a6ef4"
+  url "https://github.com/tmuxinator/tmuxinator/archive/v1.1.2.tar.gz"
+  sha256 "455e0b92f6b54d9ed8a132eb624c004ffa6fd219d1d0f710af90d3068e10ce07"
   head "https://github.com/tmuxinator/tmuxinator.git"
 
   bottle :unneeded
