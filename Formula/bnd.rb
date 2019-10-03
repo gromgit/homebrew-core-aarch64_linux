b39cd6820d14706609070fbb544b9796257ed448
BJ Hargrave <bj@bjhargrave.com>
2019-10-03 15:18:36 -0400

    bnd 4.3.0 (#44782)

--- Formula/bnd.rb
+++ Formula/bnd.rb
@@ -1,8 +1,8 @@
 class Bnd < Formula
   desc "The Swiss Army Knife for OSGi bundles"
   homepage "https://bnd.bndtools.org/"
-  url "https://search.maven.org/remotecontent?filepath=biz/aQute/bnd/biz.aQute.bnd/4.2.0/biz.aQute.bnd-4.2.0.jar"
-  sha256 "c3ae2bca2b11183630c9cafa1ac4fe247ad510b0352574b17333f1afc1bbf3a0"
+  url "https://search.maven.org/remotecontent?filepath=biz/aQute/bnd/biz.aQute.bnd/4.3.0/biz.aQute.bnd-4.3.0.jar"
+  sha256 "5c1d0fc0e1d46e37398581068690c13e1498abed42343aab27b615479ad06026"
 
   bottle :unneeded
 
@@ -24,6 +24,7 @@ class Bnd < Formula
         <resource>
           <capability namespace="osgi.identity">
             <attribute name="osgi.identity" value="#{test_bsn}"/>
+            <attribute name="type" value="osgi.bundle"/>
             <attribute name="version" type="Version" value="#{test_version}"/>
           </capability>
           <capability namespace="osgi.content">
@@ -35,7 +36,7 @@ class Bnd < Formula
     EOS
 
     (testpath/"launch.bndrun").write <<~EOS
-      -standalone: index.xml
+      -standalone: ${.}/index.xml
       -runrequires: osgi.identity;filter:='(osgi.identity=#{test_bsn})'
     EOS
 
@@ -43,7 +44,6 @@ class Bnd < Formula
     EOS
 
     output = shell_output("#{bin}/bnd resolve resolve -b launch.bndrun")
-    assert_match /launch.bndrun\s+ok/, output
-    assert_match /#{test_bsn};version='\[#{test_version},#{test_version_next}\)/, output
+    assert_match /BUNDLES\s+#{test_bsn};version='\[#{test_version},#{test_version_next}\)'/, output
   end
 end
