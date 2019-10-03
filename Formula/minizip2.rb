06579be55d8685ba0b25005b511e8e8b9da470b1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-03 13:18:23 +0000

    minizip2: update 2.9.0 bottle.

--- Formula/minizip2.rb
+++ Formula/minizip2.rb
@@ -6,9 +6,9 @@ class Minizip2 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c3e526e7c1d3a135d3b2ed02b4f41ed2aa07adde0ca7b32a1ddcc87e9f9f5ecd" => :mojave
-    sha256 "937fee9e2b22cd01cc36f042f3e5ae11ace7df1f40ceec7af3d77053c87a0fe1" => :high_sierra
-    sha256 "650410384cd941f2b0326e3d9ae0123b17dfca30bf2224a07f2a387bd65ddc29" => :sierra
+    sha256 "8da088dfc8380733a9ec95e35ac80bcc6c7142fc0c6de075a792cce3e288240b" => :catalina
+    sha256 "fc53e475c3b6d8e78f7e8e6e9c746f52d13549ca851d0c4efc28aea9767742fc" => :mojave
+    sha256 "3d67c6a0cdb7290c69b193cb341397f7cd5d5706068db6bd37f10070c79b97b4" => :high_sierra
   end
 
   depends_on "cmake" => :build
