6e66874c1f98ed375a7e6b1828a9b2e42d5f7e3d
Rui Chen <rchen@meetup.com>
2019-10-03 13:05:59 -0400

    mosh: revision for protobuf

--- Formula/mosh.rb
+++ Formula/mosh.rb
@@ -3,7 +3,7 @@ class Mosh < Formula
   homepage "https://mosh.org"
   url "https://mosh.org/mosh-1.3.2.tar.gz"
   sha256 "da600573dfa827d88ce114e0fed30210689381bbdcff543c931e4d6a2e851216"
-  revision 8
+  revision 9
 
   bottle do
     cellar :any
