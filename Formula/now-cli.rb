3e8aa801c23cfddceeaa099a2c726bb5e7e0f4a6
Rui Chen <rchen@meetup.com>
2019-10-03 14:09:19 -0400

    now-cli 16.3.1
    Closes #44863.
    
    Signed-off-by: Rui Chen <rchen@meetup.com>

--- Formula/now-cli.rb
+++ Formula/now-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class NowCli < Formula
   desc "The command-line interface for Now"
   homepage "https://zeit.co/now"
-  url "https://registry.npmjs.org/now/-/now-16.3.0.tgz"
-  sha256 "cbf8f26aea8653c044405d47cb6f5a33be112361a5371730a5aa6662f6f8f6dc"
+  url "https://registry.npmjs.org/now/-/now-16.3.1.tgz"
+  sha256 "5871a4ddd57c91adab7976cc7ac8a434b117ca5f98c9c601cbeccd2a80efa190"
 
   bottle do
     cellar :any_skip_relocation
