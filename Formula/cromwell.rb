4e45cd57497f5600fda134af15965e5532182978
Viktor Szakats <commit@vszakats.net>
2019-10-03 15:48:43 +0000

    cromwell: remove duplicate dir separators

--- Formula/cromwell.rb
+++ Formula/cromwell.rb
@@ -1,7 +1,7 @@
 class Cromwell < Formula
   desc "Workflow Execution Engine using Workflow Description Language"
   homepage "https://github.com/broadinstitute/cromwell"
-  url "https://github.com/broadinstitute/cromwell/releases/download/46.1//cromwell-46.1.jar"
+  url "https://github.com/broadinstitute/cromwell/releases/download/46.1/cromwell-46.1.jar"
   sha256 "6a5d3c495484937af7d6ac0629126c16a3062152b4a8e2b91f344b64508a26c4"
 
   head do
@@ -14,7 +14,7 @@ class Cromwell < Formula
   depends_on :java => "1.8+"
 
   resource "womtool" do
-    url "https://github.com/broadinstitute/cromwell/releases/download/46.1//womtool-46.1.jar"
+    url "https://github.com/broadinstitute/cromwell/releases/download/46.1/womtool-46.1.jar"
     sha256 "0d9a05ce1823721718e0623f89e446d11b746aac2929cad4c58bfbaf2f43651d"
   end
 
