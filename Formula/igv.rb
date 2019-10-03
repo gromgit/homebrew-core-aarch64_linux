1693a45dccace54390681ce95a40830256a38c7e
Shaun Jackman <sjackman@gmail.com>
2019-10-02 19:36:49 -0700

    igv: Does not require Java 8 (#44616)
    * igv: Does not require Java 8
    
    Install igv.args.
    
    * igv: Install igvtools

--- Formula/igv.rb
+++ Formula/igv.rb
@@ -3,18 +3,23 @@ class Igv < Formula
   homepage "https://www.broadinstitute.org/software/igv"
   url "https://data.broadinstitute.org/igv/projects/downloads/2.5/IGV_2.5.3.zip"
   sha256 "d33f6e20aaf5158770c6154953b8b3df89ced0f9416b399502864d3ccd44f22a"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.8"
+  depends_on :java
 
   def install
-    inreplace "igv.sh", /^prefix=.*/, "prefix=#{libexec}"
-    libexec.install "igv.sh", "lib"
-    (bin/"igv").write_env_script libexec/"igv.sh", Language::Java.java_home_env("1.8")
+    inreplace ["igv.sh", "igvtools"], /^prefix=.*/, "prefix=#{libexec}"
+    inreplace "igvtools", "@igv.args", '@"${prefix}/igv.args"'
+    bin.install "igv.sh" => "igv"
+    bin.install "igvtools"
+    libexec.install "igv.args", "lib"
+    bin.env_script_all_files libexec, Language::Java.java_home_env
   end
 
   test do
+    assert_match "Usage:", shell_output("#{bin}/igvtools")
     assert_match "org/broad/igv/ui/IGV.class", shell_output("jar tf #{libexec}/lib/igv.jar")
     # Fails on Jenkins with Unhandled exception: java.awt.HeadlessException
     unless ENV["CI"]
