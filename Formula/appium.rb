ce9694655384a7c7495537017daf336abfefd18c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-03 01:26:59 +0000

    appium: add 1.15.0 bottle.

--- Formula/appium.rb
+++ Formula/appium.rb
@@ -7,6 +7,13 @@ class Appium < Formula
   sha256 "49c0971bf9638c400ec70c53eb70071b1193f5b7dc0266928aa665e7ecf18ea2"
   head "https://github.com/appium/appium.git"
 
+  bottle do
+    cellar :any
+    sha256 "bbb43424e565ef0d3c2d4129031485637a8b06091524384bbed2713e2f2769bd" => :catalina
+    sha256 "668ea4b2f3a9a56457886d572b77793cc50c51ae098c8eda7c39620a20dca88c" => :mojave
+    sha256 "34c4a1d57f07f08fdfa0b80e2ed3f71f9f037f74cf0f5b50e573a10ea7e7fe95" => :high_sierra
+  end
+
   depends_on "node"
 
   def install
