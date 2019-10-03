84a8684324a3dcd05b9b71be03bae781829ebe51
Issy Long <me@issyl0.co.uk>
2019-10-03 23:36:32 +0100

    newsboat: uses_from_macos "curl" and "libxslt"
    - Over in [Homebrew/linuxbrew-core PR
      15920](https://github.com/Homebrew/linuxbrew-core/pull/15920) we found
      that the latest version of newsboat wouldn't build without these.
    - Rather than adding `depends_on` lines, follow prior art upstream
      (here) and add `uses_from_macos`.
    
    Closes #44877.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/newsboat.rb
+++ Formula/newsboat.rb
@@ -18,7 +18,9 @@ class Newsboat < Formula
   depends_on "gettext"
   depends_on "json-c"
   depends_on "libstfl"
+  uses_from_macos "curl"
   uses_from_macos "libxml2"
+  uses_from_macos "libxslt"
 
   def install
     gettext = Formula["gettext"]
