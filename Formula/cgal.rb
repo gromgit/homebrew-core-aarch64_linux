614eb10b664b78df9745efabb1d4f7bfe8c9337a
Igor Kapkov <igasgeek@me.com>
2019-10-04 09:37:29 +1000

    cgal 4.14.1
    Closes #44882.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/cgal.rb
+++ Formula/cgal.rb
@@ -1,9 +1,8 @@
 class Cgal < Formula
   desc "Computational Geometry Algorithm Library"
   homepage "https://www.cgal.org/"
-  url "https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.14/CGAL-4.14.tar.xz"
-  sha256 "59464b1eaee892f2223ba570a7642892c999e29524ab102a6efd7c29c94a29f7"
-  revision 1
+  url "https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.14.1/CGAL-4.14.1.tar.xz"
+  sha256 "d4ec2528b88a7c3a07b0b86db96c216822f85b951bf4bc7f9d1f26bf6c369afe"
 
   bottle do
     cellar :any
