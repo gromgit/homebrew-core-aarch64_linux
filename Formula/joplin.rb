9179d6a5fb5def069ccff48c7ecafc3c8ebdbad8
Igor Kapkov <igasgeek@me.com>
2019-10-04 10:07:38 +1000

    joplin 1.0.149
    Closes #44885.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/joplin.rb
+++ Formula/joplin.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Joplin < Formula
   desc "Note taking and to-do application with synchronisation capabilities"
   homepage "https://joplin.cozic.net/"
-  url "https://registry.npmjs.org/joplin/-/joplin-1.0.146.tgz"
-  sha256 "d7dd4572c07f2c843cc939e48d2ee0c91fef8cfb005448e6c1c9611ba443b48b"
+  url "https://registry.npmjs.org/joplin/-/joplin-1.0.149.tgz"
+  sha256 "26b1c4b80db58148e934852016687ced1c617f1841afc9b1e5eea00e10e20c8e"
 
   bottle do
     sha256 "0ac59071813496b98820e98afeb5a4b0019dce1f15e7dc82a1977ffb6fd0d295" => :mojave
