0470ff898ab1f64297dd16510b9532021a645ef6
Kim <kim_rioux@hotmail.com>
2019-10-04 09:35:26 -0400

    texinfo 6.7
    Closes #44901.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/texinfo.rb
+++ Formula/texinfo.rb
@@ -1,9 +1,9 @@
 class Texinfo < Formula
   desc "Official documentation format of the GNU project"
   homepage "https://www.gnu.org/software/texinfo/"
-  url "https://ftp.gnu.org/gnu/texinfo/texinfo-6.6.tar.xz"
-  mirror "https://ftpmirror.gnu.org/texinfo/texinfo-6.6.tar.xz"
-  sha256 "9bb9ca00da53f26a7e5725eee49689cd4a1e18d25d5b061ac8b2053018d93d66"
+  url "https://ftp.gnu.org/gnu/texinfo/texinfo-6.7.tar.xz"
+  mirror "https://ftpmirror.gnu.org/texinfo/texinfo-6.7.tar.xz"
+  sha256 "988403c1542d15ad044600b909997ba3079b10e03224c61188117f3676b02caa"
 
   bottle do
     sha256 "3b73960b196abf2328e7efdd49c2b327ba883ca2661d63b96c5d2928200e2c69" => :catalina
@@ -11,6 +11,8 @@ class Texinfo < Formula
     sha256 "0181ad9121222a096857230faa80ed6b706a03eec49a5b9359694c0b8587db73" => :high_sierra
     sha256 "00f00f4d5ab3a6ba46d90264df0c8e0117cb14e30398ca574b0b53b95226e687" => :sierra
   end
+
+  depends_on "gettext" if MacOS.version <= :high_sierra
 
   keg_only :provided_by_macos, <<~EOS
     software that uses TeX, such as lilypond and octave, require a newer
