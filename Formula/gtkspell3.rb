52c5f7f4364768368fc5871a7babf1532aff274b
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-10-04 20:47:43 +0100

    gtkspell3: revision bump for gtk+3

--- Formula/gtkspell3.rb
+++ Formula/gtkspell3.rb
@@ -3,7 +3,7 @@ class Gtkspell3 < Formula
   homepage "https://gtkspell.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/gtkspell/3.0.10/gtkspell3-3.0.10.tar.xz"
   sha256 "b040f63836b347eb344f5542443dc254621805072f7141d49c067ecb5a375732"
-  revision 1
+  revision 2
 
   bottle do
     rebuild 1
