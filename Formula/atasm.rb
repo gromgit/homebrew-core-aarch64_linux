528c896c5274cefac47390cea1d76e9cb58b3976
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-04 20:26:16 +0000

    atasm: add 1.07d bottle.

--- Formula/atasm.rb
+++ Formula/atasm.rb
@@ -5,6 +5,13 @@ class Atasm < Formula
   version "1.07d"
   sha256 "24a165506346029fbe05ed99b22900ae50f91f5a8c5d38ebad6a92a5c53f3d99"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8f142806b05036e541ef3fec3009d481423f451cbcd99e6be68ae5095cfa205e" => :catalina
+    sha256 "7a2437b5a0adf8047fc75a20fb669d2d80b15d261eab0ec0ad5c7d74b9123a2b" => :mojave
+    sha256 "b9eb26201949590ab8fce80ee3feabe7f0be2f611e7c60b6b456c8d78480680c" => :high_sierra
+  end
+
   def install
     cd "src" do
       system "make", "prog"
