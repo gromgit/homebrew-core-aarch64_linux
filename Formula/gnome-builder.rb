272ae488703723585fac9a721a888421ba4d06b7
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-10-04 20:43:13 +0100

    gnome-builder: revision bump for gtk+3

--- Formula/gnome-builder.rb
+++ Formula/gnome-builder.rb
@@ -3,7 +3,7 @@ class GnomeBuilder < Formula
   homepage "https://wiki.gnome.org/Apps/Builder"
   url "https://download.gnome.org/sources/gnome-builder/3.30/gnome-builder-3.30.3.tar.xz"
   sha256 "9998f3d41d9526fdbf274cae712fafe7b79d0b9d1dd5739c6c2141e5e5550686"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "b4a7f7f5bd1d17500b3a9981d6068dab4c680f483248fb8ea78285dcedefb694" => :mojave
