b28851de03ebd9f4d922fe944c262a05dfaa41a5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-04 09:05:31 +0000

    kyma-cli: add 1.5.0 bottle.

--- Formula/kyma-cli.rb
+++ Formula/kyma-cli.rb
@@ -6,6 +6,13 @@ class KymaCli < Formula
       :revision => "6c8148df282e0bd23a377e9e5eb250c33179a1ca"
   head "https://github.com/kyma-project/cli.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e4f0222bc6915b6a4c73f6565bbc560bec671bd40b420d7fe0934c64e5734e16" => :catalina
+    sha256 "5a8705b6de2acbaa0a6ba66f0af112c6982c9618d006c5a3012298e691e58043" => :mojave
+    sha256 "b3a2fb78d3f063057712823f1bcaef51cdf38606b8b83cb506508e8134017c31" => :high_sierra
+  end
+
   depends_on "dep" => :build
   depends_on "go" => :build
 
