a758c386166c9c4deae90d22795059907dce5723
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-10-04 20:49:23 +0100

    klavaro: revision bump for gtk+3

--- Formula/klavaro.rb
+++ Formula/klavaro.rb
@@ -3,6 +3,7 @@ class Klavaro < Formula
   homepage "https://klavaro.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/klavaro/klavaro-3.09.tar.bz2"
   sha256 "9983e501563a4d05e429700a2bd5bb078ac43b2f0d4014864e3cac42e0a1f589"
+  revision 1
 
   bottle do
     sha256 "2a9619dd4f1c5bfe4bfb3506acd5f1ccd88a9db52961e633e37bba1bc53e4cec" => :mojave
