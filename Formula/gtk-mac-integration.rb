0591aea265018024fe2b81ea521195fa0d1151b8
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-10-05 06:38:18 +0100

    gtk-mac-integration: revision bump for gtk+3

--- Formula/gtk-mac-integration.rb
+++ Formula/gtk-mac-integration.rb
@@ -3,7 +3,7 @@ class GtkMacIntegration < Formula
   homepage "https://wiki.gnome.org/Projects/GTK+/OSX/Integration"
   url "https://download.gnome.org/sources/gtk-mac-integration/2.1/gtk-mac-integration-2.1.3.tar.xz"
   sha256 "d5f72302daad1f517932194d72967a32e72ed8177cfa38aaf64f0a80564ce454"
-  revision 1
+  revision 2
 
   bottle do
     rebuild 1
