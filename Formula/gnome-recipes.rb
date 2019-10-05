26a3b88e5edd6b444ff887a104b25e228a4ba24d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-05 21:19:53 +0000

    gnome-recipes: update 2.0.2_10 bottle.

--- Formula/gnome-recipes.rb
+++ Formula/gnome-recipes.rb
@@ -6,9 +6,9 @@ class GnomeRecipes < Formula
   revision 10
 
   bottle do
-    sha256 "b7b78f4f6f8ee51d7b9f38a3bf7a048acb82ed3750fc05ff0fc668916883a8dc" => :mojave
-    sha256 "7caabe6388d94713d3d8ecb4dd4ed32950b6a22e3c9d4e1d93612a5170cd1844" => :high_sierra
-    sha256 "e0f8a05324747a9ee4b0d6941b9a9c20323aaa29f77412a54aaa0564e51b9896" => :sierra
+    sha256 "1bd7f5d9f6adb0f4855ff1ae2d1d6a024f1d635f064dc86407a4088a8109f749" => :catalina
+    sha256 "1f49413fbb54652d11ea480a68b45261941cc1827b30f660941da423d07391f7" => :mojave
+    sha256 "b6cab9caf039f7b99591e2c0f15c5bc90b6e62eb0bf9ad4adf66b69bf8359ace" => :high_sierra
   end
 
   depends_on "itstool" => :build
