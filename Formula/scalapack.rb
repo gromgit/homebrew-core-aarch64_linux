94aaffb5e88f273db233cb938f33fa72cf3938c5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 17:56:24 +0000

    scalapack: update 2.0.2_16 bottle.

--- Formula/scalapack.rb
+++ Formula/scalapack.rb
@@ -7,6 +7,7 @@ class Scalapack < Formula
 
   bottle do
     cellar :any
+    sha256 "2bd0eb0eaa9b0fc85a98fa7785dcd243503961f2711b6dd86cc93d80f84bbe3d" => :catalina
     sha256 "0d0975114692d302afb2caa38f3e12cc64b37fdad13ce7b41cbbdc6002567d26" => :mojave
     sha256 "773a7fc4d19e9a9329637d8849bf21b93423b790bb5f0fbe90166ff2d8c19ad2" => :high_sierra
     sha256 "b52679f06f9f2de153139426ccd949ad5cf6d65814f82a8e2a16dc7bfcf480f6" => :sierra
