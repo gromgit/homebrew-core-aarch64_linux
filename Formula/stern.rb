b445f963cb437d7fdca73129a1c17f908659ab44
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 20:48:18 +0000

    stern: update 1.11.0 bottle.

--- Formula/stern.rb
+++ Formula/stern.rb
@@ -8,6 +8,7 @@ class Stern < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "886c99c34ee64ae8ea2292822b268324b573449fb3d621b874c0746d071a5082" => :catalina
     sha256 "f862bab034a7d05b995e762e7feae5d840db9099faba8465e4e20daa548f02f7" => :mojave
     sha256 "fd68b5ceb1ef3a1cbdb51653530f0ac0625e59adb55b02c1b7dad2268db7e4b3" => :high_sierra
     sha256 "01a7f817326d1172201d36df18cbee1ceca864d83e9cc528e301018b1510872f" => :sierra
