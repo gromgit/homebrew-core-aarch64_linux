0130ca128daf7b2bd55a8625151aa3fe36d2ba70
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 20:35:48 +0000

    e2fsprogs: update 1.44.5_1 bottle.

--- Formula/e2fsprogs.rb
+++ Formula/e2fsprogs.rb
@@ -7,6 +7,7 @@ class E2fsprogs < Formula
   head "https://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git"
 
   bottle do
+    sha256 "a2b0a389e56c197c9a68c3a0598933534c6e0da0b15023dec490cbe3889d49c7" => :catalina
     sha256 "8bfa7f8116eabc2422696487b228493ddcc2991699a3158de30d6dba6c10ef73" => :mojave
     sha256 "f0850aaef4670664e086d9a30568f5a50b5da21bf27d03b8e26478d44b2436f7" => :high_sierra
     sha256 "70bfe479fd8e5769780ad9b408739653c8b4ef94d4752c9b65ebe134c2b03eb4" => :sierra
