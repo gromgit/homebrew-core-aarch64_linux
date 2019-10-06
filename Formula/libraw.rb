de1f33c0626c54e5e1b0d2d204c309d24dbedd89
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 18:08:04 +0000

    libraw: update 0.19.5 bottle.

--- Formula/libraw.rb
+++ Formula/libraw.rb
@@ -6,6 +6,7 @@ class Libraw < Formula
 
   bottle do
     cellar :any
+    sha256 "0a6a6a01ee33d8a8ca2d97eba728958078173b51a3513c1ea4220c448869fb6a" => :catalina
     sha256 "7c962099ebd8959acee2921993e26120928604ff6809a1f7a372b95a00d2c130" => :mojave
     sha256 "f2e39a3e5811e1798fe84bc3122b9450eed0dfd861e586aaf9a76f13451942dd" => :high_sierra
     sha256 "684fb608b113365f690fad65636d45ae2f46469b10930363e45193ea9b4286ed" => :sierra
