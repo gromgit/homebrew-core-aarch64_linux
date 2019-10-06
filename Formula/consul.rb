0b84a287af8f709fdad1c927a14c859fe079265c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 19:24:23 +0000

    consul: update 1.6.1 bottle.

--- Formula/consul.rb
+++ Formula/consul.rb
@@ -9,6 +9,7 @@ class Consul < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "68b0d5d33d3121017ec1007b3aa6a59ca9ae1e0e3239ec798b0866f4a8f75e7b" => :catalina
     sha256 "bc99db5d92d406b6f1f94af1360ca8f9a1d04cbf68e195490f7e5bc8a1833c13" => :mojave
     sha256 "ef5a6a6ff48cc04b35d5b7e7ebb43a5cf0cb5abad7fb3d9d3eef1582ea1c0e49" => :high_sierra
     sha256 "0aba9ab485ca5777c07b678c93a5fffe76020fe5b7dfb45bd11b83794443f9af" => :sierra
