0f373e7344d28dc016457d2685bbc10ddbfe0af2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 17:24:42 +0000

    open-babel: update 2.4.1_1 bottle.

--- Formula/open-babel.rb
+++ Formula/open-babel.rb
@@ -8,6 +8,7 @@ class OpenBabel < Formula
   head "https://github.com/openbabel/openbabel.git"
 
   bottle do
+    sha256 "4ab9e67beff60ffa0f90c8f35b3fc729ecbf44387deca6d42a57feb6b399d6ca" => :catalina
     sha256 "61c3b83067ef6ecff739ad64af1f7dee20c66459838f2584b5ace1077261cbef" => :mojave
     sha256 "0383df12c965d3e2b0087812186992b32ed6d02fc7bcfef397b071db2b96e568" => :high_sierra
     sha256 "fac962fa1127ac9476e5dd1c44ba593c6184bb49444eac28804e45609243e8f9" => :sierra
