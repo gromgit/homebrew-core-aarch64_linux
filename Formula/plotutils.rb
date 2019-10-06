a589f1ec64ff3383779c3eff2679040a994b287c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 23:20:44 +0000

    plotutils: update 2.6_1 bottle.

--- Formula/plotutils.rb
+++ Formula/plotutils.rb
@@ -9,6 +9,7 @@ class Plotutils < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "edab5b91771162c1783dc69482834de6a2ca0fd077ea83b79d1934a365f7276d" => :catalina
     sha256 "96a618ea8123f08d676b0db38c1c3b93dc8f707c742e97442b74650c2dd8e4c5" => :mojave
     sha256 "00796c7f6aa36203eb0fd919ef4f096c6016d3c5973b2032328c95c87b354d92" => :high_sierra
     sha256 "b63f4f051452f8fd9b5ddb50f9d574122c2277c9778e1a56c3f2d59e55c3da73" => :sierra
