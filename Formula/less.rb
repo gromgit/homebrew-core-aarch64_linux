43108b8664c2a295cc14ab9077903d219029ded1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 22:00:23 +0000

    less: update 530_1 bottle.

--- Formula/less.rb
+++ Formula/less.rb
@@ -8,6 +8,7 @@ class Less < Formula
 
   bottle do
     cellar :any
+    sha256 "e9c850a28846a39541fd76e5159071527e8db94258a809da968cf69e42277d48" => :catalina
     sha256 "f005662a0d661c28540163078807f4b518a6e6a2e8c86a5a0a0993eb6c4c4ad5" => :mojave
     sha256 "f9896f9b0e0fb82dcbafd312a93d05061a2aa6f451592b30ca833dbdfb2b38c0" => :high_sierra
     sha256 "a123eef24eeb7839ed0b7a3b0d53d17402c72576554fecaad81743e550b49107" => :sierra
