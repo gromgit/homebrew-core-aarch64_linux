f902c4ab2d311cedf57e4cd046d7adb72051ac68
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 20:35:48 +0000

    gifsicle: update 1.92 bottle.

--- Formula/gifsicle.rb
+++ Formula/gifsicle.rb
@@ -6,6 +6,7 @@ class Gifsicle < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5c39ab6736846e30082db190167bc498e78d03e523f642057eadc29f854c71b1" => :catalina
     sha256 "52be1cd49246909777199147dcbbeb6f490580558615138ae063b6149cfbe53c" => :mojave
     sha256 "a7cfb607906023c5bdb56a49f8a75ce0b3e2c76a971266bb530d04ed29be74f9" => :high_sierra
     sha256 "746d071f268950c6af18704590b981f5f965d35e2adf6c202aa3df0f13e943e9" => :sierra
