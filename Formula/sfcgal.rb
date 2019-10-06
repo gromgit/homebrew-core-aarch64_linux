a2791afa40f0afe4dc691f686d05d6968ba2f581
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 17:14:52 +0000

    sfcgal: update 1.3.7_1 bottle.

--- Formula/sfcgal.rb
+++ Formula/sfcgal.rb
@@ -6,6 +6,7 @@ class Sfcgal < Formula
   revision 1
 
   bottle do
+    sha256 "ed1fe60ab20f1c5dafa487779a06becd1b5ca7dd8aaac3c59dcb8460a439ba38" => :catalina
     sha256 "1fcacf1736448f1490a29abf188bb00499bef52f7dfe6d11adb7bb9f7d6b1730" => :mojave
     sha256 "be74e088dc8b81771e8ece9a79e3ba93f8338e764bb70b173ea045037d4b1790" => :high_sierra
     sha256 "49f796b256dd748214d8b2d8a954daa3b1f01c6ae3bd96bc1dff2a3364e57c63" => :sierra
