63b51ff2c9946d8065537ad53263a95a87c2bfad
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 22:42:40 +0000

    tcpdump: update 4.9.2_2 bottle.

--- Formula/tcpdump.rb
+++ Formula/tcpdump.rb
@@ -8,6 +8,7 @@ class Tcpdump < Formula
 
   bottle do
     cellar :any
+    sha256 "25b9dd6c81432d2713c8a8642e1d4ac869b7b549b6cc9d435f82693d2d26a58c" => :catalina
     sha256 "3c598caf02f9ca31d2892d9586a35c70400ca2a7895758b72e4672e83b646457" => :mojave
     sha256 "2fc5873438a618ba6140b37ba4e41f7f318b95e0608f17620104146843d99fbf" => :high_sierra
     sha256 "7b57d5f603ed82fb98c58198660596c7418af6b567c964d6873d23eb6629140b" => :sierra
