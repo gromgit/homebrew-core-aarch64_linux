e7c602ccd26aaa74d5d6c96cc90f93daf2b30e51
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 21:24:03 +0000

    tinyxml2: update 7.1.0 bottle.

--- Formula/tinyxml2.rb
+++ Formula/tinyxml2.rb
@@ -7,6 +7,7 @@ class Tinyxml2 < Formula
 
   bottle do
     cellar :any
+    sha256 "2326b039acf4a2550a21fab854fc9a03c0583cac2837feab19f3caf03975f09d" => :catalina
     sha256 "93e79de7e9acda8014942e9fb1fccc8be218af0799e0465752e967adc05d74bb" => :mojave
     sha256 "b33ac4fad0b064be28130659f32ba3f44cbd16a8a5610d5fdb33026ce7eb5bf5" => :high_sierra
     sha256 "3519517f7b877558d0e8ff5784970fe30c6c4d2e06b437ef3e1c972f2890056c" => :sierra
