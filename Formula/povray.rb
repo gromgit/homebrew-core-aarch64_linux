68112b3c16690a8d0ef3859e7e8179e11d6102f3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 17:00:52 +0000

    povray: update 3.7.0.8_1 bottle.

--- Formula/povray.rb
+++ Formula/povray.rb
@@ -7,6 +7,7 @@ class Povray < Formula
 
   bottle do
     rebuild 1
+    sha256 "a882f103b0ad016cbafa13cc1fd028046b337590feff3e6188bb574f1e328488" => :catalina
     sha256 "eae4cf975215cdfdeadb665c53061c6ed2b4f9fa95121e7145222409b0e44c56" => :mojave
     sha256 "4472bb00380eb26d3045dd5e67effa4f75934936263129009f9a80bbf5290633" => :high_sierra
     sha256 "f21cb29c30c8367aa14f6a4485bf03377f23e30b2e7178be466d12bb84be26a9" => :sierra
