f3fc59bfec34e003b6cb6d4cda84f7094358878e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 13:54:26 +0000

    gdb: update 8.3 bottle.

--- Formula/gdb.rb
+++ Formula/gdb.rb
@@ -7,6 +7,7 @@ class Gdb < Formula
   head "https://sourceware.org/git/binutils-gdb.git"
 
   bottle do
+    sha256 "fbc1eb60dc46eb6517ea530049496336e1a4d3c9b3bdf8816665cac528fd99a4" => :catalina
     sha256 "16da5f61ca304740defde7f8a772d5fb5f5c48ac658984ef186d1c77f53b5d6a" => :mojave
     sha256 "2721c3a733fba77d623f84c33cce6a1cca46c6a020649269f4431de402704fa1" => :high_sierra
     sha256 "b2343fca9963d198248c98ee069211c28e04135effcc2e7ed0900fec7c7d95a3" => :sierra
