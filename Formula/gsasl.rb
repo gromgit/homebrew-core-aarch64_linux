e222ee78d22b51c69d13cff515729281e5f24e53
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 18:49:10 +0000

    gsasl: update 1.8.0 bottle.

--- Formula/gsasl.rb
+++ Formula/gsasl.rb
@@ -8,6 +8,7 @@ class Gsasl < Formula
   bottle do
     cellar :any
     rebuild 3
+    sha256 "d60d223245fcca817c96ac40755fc41a17289ded2c9cf06d8cbc38457b5739ef" => :catalina
     sha256 "d52080e846621b8b297fb8f100c077a859545b933ec527573a02addddca0b40d" => :mojave
     sha256 "9e6544e25c68eff0e352ba84355a9a6ea201b9b533cbb9420eee3bdfd1531eb3" => :high_sierra
     sha256 "22c6d1dd63deb2e26dd8e3b2b51b30735ada08b6972222eed3d1868fddf1ed3c" => :sierra
