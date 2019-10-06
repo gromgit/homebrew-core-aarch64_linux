72d3dec92c4db1bf1575c97572360f0cf60abc8d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 18:13:12 +0000

    libdvdcss: update 1.4.2 bottle.

--- Formula/libdvdcss.rb
+++ Formula/libdvdcss.rb
@@ -6,6 +6,7 @@ class Libdvdcss < Formula
 
   bottle do
     cellar :any
+    sha256 "352a2c343c04e65ee38fe154c797a29cc9cca509212e2296e9cd54e3e824ce29" => :catalina
     sha256 "645422cdd6facba8137146fd12df0538b27432a72bc79c5ca8c2667ab9fc70bc" => :mojave
     sha256 "4029db91ed7536435bd29db6b67f55509be13e70b6170337edec72daad8992c4" => :high_sierra
     sha256 "907d51957c4674ddeb27b458dcf5f1f4b382219bda893fc8908147acc1c2b1ea" => :sierra
