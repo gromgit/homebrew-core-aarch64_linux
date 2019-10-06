6e9806d58f236f35e951cdf7fba6679de42aa659
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 17:39:54 +0000

    glm: update 0.9.9.5 bottle.

--- Formula/glm.rb
+++ Formula/glm.rb
@@ -7,6 +7,7 @@ class Glm < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "94bc3a8dc77a81377802bf9cb21ae9d394847a85f172eb4675f09d34131f544c" => :catalina
     sha256 "12ad0bff26df5739227bb1f2865c064b46a1e21faa07e77f19c2be4e2abd8182" => :mojave
     sha256 "534081b8d1624708d52a12d75a31b47372c1711c56057b658c0dbf5fa67306c0" => :high_sierra
     sha256 "b7185a79a24187a43750f740ac07686ef144182eb8006ca0deadbbf4dc037584" => :sierra
