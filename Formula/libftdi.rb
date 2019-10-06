479356ac00791d29af0611650dc9e6562e3a01f1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 22:31:20 +0000

    libftdi: update 1.4_1 bottle.

--- Formula/libftdi.rb
+++ Formula/libftdi.rb
@@ -7,6 +7,7 @@ class Libftdi < Formula
 
   bottle do
     cellar :any
+    sha256 "37e829f5dfd5bb27398ed4c2744b6ce35f8338b0e4994d4981b3e6a915b80872" => :catalina
     sha256 "1ab6dc2e9827ee83b319996ab3e90d10b4dbeb8b474ef06649832b00b857223a" => :mojave
     sha256 "111b0b0e9798795eebe44154610bfe022b288c2461a63d8c7f1656c148eba568" => :high_sierra
     sha256 "f4e880f83165a30696f49be0915ff22dbd1b27f1c04a903f9bcec49b9985c4c4" => :sierra
