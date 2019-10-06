c2d02a663655c4a9d0bf0805d824080f87f98a35
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 19:31:50 +0000

    aces_container: update 1.0.2 bottle.

--- Formula/aces_container.rb
+++ Formula/aces_container.rb
@@ -6,6 +6,7 @@ class AcesContainer < Formula
 
   bottle do
     cellar :any
+    sha256 "26cfdcca70d0fb62834376bfbda89af0acd57f0173a4a3c9bd0f77adf748c8b9" => :catalina
     sha256 "5f2c4a76a3a1082e3d969143455c8a47b8d05d7251424b7e8d821b4e73f46a9e" => :mojave
     sha256 "4297afa069f1cd305e93038ed43260b3643f0bd27f39e33355061fc111fb7f6f" => :high_sierra
     sha256 "6b276491297d4052538bd2fd22d5129389f27d90a98f831987236a5b90511b98" => :sierra
