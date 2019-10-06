d21b0f3846e4ffa00e2f730dff9fadd9fd2a6b17
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 21:41:08 +0000

    stow: update 2.3.1 bottle.

--- Formula/stow.rb
+++ Formula/stow.rb
@@ -7,6 +7,7 @@ class Stow < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c99a90dc5e3db8ebcb017df044723fb4e6cce7fb94aa24cf46c8d2c0665bf9a0" => :catalina
     sha256 "409987564f7779d6a1db75f64e54c4713ecd9b9e006abac931f8e8d645bdac92" => :mojave
     sha256 "409987564f7779d6a1db75f64e54c4713ecd9b9e006abac931f8e8d645bdac92" => :high_sierra
     sha256 "cbc7a61940a343aff46fdb6190dc26a359d26c9c468c05b1dbde2484a066ceb6" => :sierra
