60d3ab5dbff00339b5e06a228f9ef2a93c6155a0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 20:48:19 +0000

    pwgen: update 2.08 bottle.

--- Formula/pwgen.rb
+++ Formula/pwgen.rb
@@ -6,6 +6,7 @@ class Pwgen < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "725911d1fd71b259acb7b907c09ef86a03545afe95e161856130992fc0789ffc" => :catalina
     sha256 "2f35a2d575e16a2ab0497cabfc927a7b40aba68edba574889bf9bbdf03572c12" => :mojave
     sha256 "185f2f56eb03da60277520734452204ec2e0059cbc1f0af5d0fec1e7fa837658" => :high_sierra
     sha256 "01a0709f74923e7b86d680f03d3ec056d3175cb7e54be176a26d5bfae890fd21" => :sierra
