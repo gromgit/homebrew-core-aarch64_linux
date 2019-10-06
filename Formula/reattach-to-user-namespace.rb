1cf37aebf3505d5fa90ec3b1552d91db871016d4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 19:12:43 +0000

    reattach-to-user-namespace: update 2.8 bottle.

--- Formula/reattach-to-user-namespace.rb
+++ Formula/reattach-to-user-namespace.rb
@@ -7,6 +7,7 @@ class ReattachToUserNamespace < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "165edabd70a4acbdee2f3e09e9a37e9ca23b2d485ebe12d62511cef9c45fd757" => :catalina
     sha256 "388e45300bf6cf3888a490878c9a03b97c06a52fe316a18d099ab97bddbed295" => :mojave
     sha256 "adc59ad02ccf302e9c71c08d10a4eb95d5d210dbfebe9a00b41648b094c96f09" => :high_sierra
     sha256 "1ec537d4be63bbcdaf3644506d0b272051fd2617db84c111152a33d8d0184339" => :sierra
