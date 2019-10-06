9980c0a23a6f142a7216f8335fb732492529ae76
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 23:43:44 +0000

    ctop: update 0.7.2 bottle.

--- Formula/ctop.rb
+++ Formula/ctop.rb
@@ -8,6 +8,7 @@ class Ctop < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "ab60a890e27e6eeffb6a077af370f06f0881d0fd75e571439e52e37c3ee00061" => :catalina
     sha256 "2e2f235137e94a3480bf54a0ef951665756736e38db7e0d61bee478e03d550ce" => :mojave
     sha256 "6d86410f2860a8d05d15b4cb0ae22a940de3968d75209493b3988d0c456b0b34" => :high_sierra
     sha256 "143755c7fee144254c6d3fa401a607aae977820acf199f954ff8cdfabf336235" => :sierra
