4c72124f88075cfddb97043d520ac97f9abf0036
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 16:33:32 +0000

    unzip: update 6.0_6 bottle.

--- Formula/unzip.rb
+++ Formula/unzip.rb
@@ -8,6 +8,7 @@ class Unzip < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "178cea56554b0e6b74856203340554c7615c6ed2e122059c78370e35c896f0ce" => :catalina
     sha256 "f0b95f2d5c664f45686f3aa318384906014ed28939da28020d12138f025aaeb6" => :mojave
     sha256 "6dd7d0862f5a8b954dd94b3c91378209e0086eec7c5be367af0d8c330bc099da" => :high_sierra
     sha256 "f4d59c04a44f93a30a23ec403784c73f9c06db9b72f3277679f66b1870a94331" => :sierra
