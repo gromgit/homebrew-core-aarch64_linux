f3d2b7d6660413b53fffa21040e7d281c9a2753a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 19:07:03 +0000

    serd: update 0.30.0 bottle.

--- Formula/serd.rb
+++ Formula/serd.rb
@@ -6,6 +6,7 @@ class Serd < Formula
 
   bottle do
     cellar :any
+    sha256 "14de362fe3bb578ece49d17a7c1e75d477b7cdc64b4a99d2975c05a9afad1ea4" => :catalina
     sha256 "782e370f3ee5718fa706744b5a2c7185c9d1e1216c0b6c11c34ef10c489af8ad" => :mojave
     sha256 "34a847ef82d478c6f60ad532e43ae8601758242be46aed85485d2cf2e6c20b6d" => :high_sierra
     sha256 "6daae3f4fe65b0fae94f309d8538a40bdca5b207d2e464b47badcacd5408bb7a" => :sierra
