8998d5b2182e0d526d7730bb0c1b56af6a0684ac
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 16:34:12 +0000

    git-crypt: update 0.6.0_1 bottle.

--- Formula/git-crypt.rb
+++ Formula/git-crypt.rb
@@ -7,6 +7,7 @@ class GitCrypt < Formula
 
   bottle do
     cellar :any
+    sha256 "f38bb645c3eff62cfb43802199370d85e4785fcf10c063e4d7453e032788bcba" => :catalina
     sha256 "89d2058a4dd5afc565696707c8e93621fd644f9ab303fe378727ae999783d156" => :mojave
     sha256 "0d2cf3c93ab2ca4059163f8da8a3ab845b566b13debf5e1b43a734dc86138a18" => :high_sierra
     sha256 "6b2c2773e5c327282d461f5d49600928ae97d432e5f4d8b7acfcaaa6e6d1ef68" => :sierra
