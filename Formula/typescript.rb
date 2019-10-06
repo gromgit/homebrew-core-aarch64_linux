4e42c8a9918e753f92bae143af00bb731a06cd7f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 23:37:49 +0000

    typescript: update 3.6.3 bottle.

--- Formula/typescript.rb
+++ Formula/typescript.rb
@@ -9,6 +9,7 @@ class Typescript < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a636fdbcfbacd7546c0d5b5767f98e10ac06036c3fe5dc3934018ea86aa9168b" => :catalina
     sha256 "3bd0896b1c6a423c2f502932599931c7c2f9348bcb27856fa932ab56c0028be4" => :mojave
     sha256 "1049bf48f6cc02358a3a4ec2e1187f30ed7d8d5bc4a53b388661f7f205027df1" => :high_sierra
     sha256 "bffc050131abccb5dedd986667e4dcc263e736699c03f953afb52f1ec83174d5" => :sierra
