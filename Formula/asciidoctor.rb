7cfa271299392ce29202bac6f60653453e43b32a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 19:31:51 +0000

    asciidoctor: update 2.0.10 bottle.

--- Formula/asciidoctor.rb
+++ Formula/asciidoctor.rb
@@ -8,6 +8,7 @@ class Asciidoctor < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "db8e26deff7e7a32c6b08620ff4d7a167f080cf0ac13ed21581faf0c50887247" => :catalina
     sha256 "3a76289cb376285952c46a17ebc44c24f5c56bc6cc5219507321d9745963f23b" => :mojave
     sha256 "36b22c4bba82748fbd212c2bc0c4726aff9850addeea7b7df2ac707b0728034c" => :high_sierra
     sha256 "48f283b4f49a361a4a1aedc433996345edb49e8de0d039e9c35cadf0830e33b4" => :sierra
