140b81e9d2d816e7387ff5852d85ac8543f1f45d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-07 09:58:06 +0000

    aspcud: update 1.9.4_1 bottle.

--- Formula/aspcud.rb
+++ Formula/aspcud.rb
@@ -6,6 +6,7 @@ class Aspcud < Formula
   revision 1
 
   bottle do
+    sha256 "0196b40592cf41eee37dded4519dbf4fea99df6f1eee4a93922a6dfde7e9eedd" => :catalina
     sha256 "663b82ae45395f7235e1ee0acb7dfc8821efb2a799569c65275ecac90d96570d" => :mojave
     sha256 "07f0e44c6cf608f20da7a37744d7559c1f7b77fc3151bcd37ea0af9fbd39cde7" => :high_sierra
     sha256 "c3c886728b9713da9ec4837b7faf19832219636743654f5b94dbe83b09c83bae" => :sierra
