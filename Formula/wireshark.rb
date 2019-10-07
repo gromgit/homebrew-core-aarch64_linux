abb933cfba68ca6adf0ad17c4a3b9c8262923037
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-07 10:45:48 +0000

    wireshark: update 3.0.5 bottle.

--- Formula/wireshark.rb
+++ Formula/wireshark.rb
@@ -7,6 +7,7 @@ class Wireshark < Formula
   head "https://code.wireshark.org/review/wireshark", :using => :git
 
   bottle do
+    sha256 "0dc5cb8d614d3bcc388b51b05a6b85bdef8195e5dd39477239a79a82736670d9" => :catalina
     sha256 "7c538b398788ec39b394f3973073323f76434653880bfedb48e5f3767026853d" => :mojave
     sha256 "44f9bc010e0d11fea64ab47ef40128a08b4b186ffd77a60048f0b3d4de4ac171" => :high_sierra
     sha256 "e7723a63da53ec4441807123f167e63dd8df02e79cca2a5d3340c34d8758368b" => :sierra
