d95c512dfbe9a6b9466088ac6c9ca741dba2b18c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-07 00:17:19 +0000

    smartmontools: update 7.0 bottle.

--- Formula/smartmontools.rb
+++ Formula/smartmontools.rb
@@ -5,6 +5,7 @@ class Smartmontools < Formula
   sha256 "e5e1ac2786bc87fdbd6f92d0ee751b799fbb3e1a09c0a6a379f9eb64b3e8f61c"
 
   bottle do
+    sha256 "83418daec52373443bbf5c30ea6a808fbd36ec7ed31e708512e1adf7cc3204a1" => :catalina
     sha256 "eeb9c234d61a85547f6e5f169966f5afd5cb5c31a9f17d758910bdd855d711c4" => :mojave
     sha256 "e77726b986b056d7b58112e965c399861d41a568193c45b9862d7ef5f04ed479" => :high_sierra
     sha256 "74c52c197fbfef3715fe6ed1dc787756cb7185932958005f738c2ce142579a48" => :sierra
