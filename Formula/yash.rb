8dfe44e9ded24ecb98eca54361b00a956f74e3df
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-07 17:02:04 +0000

    yash: update 2.49 bottle.

--- Formula/yash.rb
+++ Formula/yash.rb
@@ -6,9 +6,9 @@ class Yash < Formula
   sha256 "66eaf11d6c749165a7503801691759ae151e4eae00785875e121db2e9c219c72"
 
   bottle do
-    sha256 "78fbbc947f0cbe7484acc442be951dd47941358220bc4abf75f2e466b746efde" => :mojave
-    sha256 "ff5e85274a16fedb468c62ce09866ca475f6c651f0fd139b46847e07f4c120a6" => :high_sierra
-    sha256 "29e2875a5ea150b0517006280265ed2a11da8cb9c050b1b1c34777f2a90b3e22" => :sierra
+    sha256 "bdea912700abd0af3cfadf0b53498389bf8712fbcc05a4e4dff1facc49a05238" => :catalina
+    sha256 "006b68f9a1cfa870cdc2e77832ca43612cab41007217830b6448a2d60851f4ca" => :mojave
+    sha256 "a6bea4d4ebf343937daa87f97189d868368b8626d14d75fd1d07adc470e07850" => :high_sierra
   end
 
   depends_on "gettext"
