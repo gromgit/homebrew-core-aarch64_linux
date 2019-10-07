a8d2ed2fc4e1477661eb90d1bf8a6188cd3633b5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-07 00:43:29 +0000

    net-snmp: update 5.8_1 bottle.

--- Formula/net-snmp.rb
+++ Formula/net-snmp.rb
@@ -6,6 +6,7 @@ class NetSnmp < Formula
   revision 1
 
   bottle do
+    sha256 "365760ffedb84e252289adc26a17365b62de654b3cff95952b6eff2ff90df7db" => :catalina
     sha256 "efd1bcbd0e99fc29571de33c64fd1494db705a114778800d8181a27424a24421" => :mojave
     sha256 "f5774ae4c5cc7f5a7fe5eb9eaa60f35842af5f6c2c184444428cc2e412f040fb" => :high_sierra
     sha256 "1b1ea4f4456b6fc36c501398852b2e9979791387f38993129382b75782176f97" => :sierra
