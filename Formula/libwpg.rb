d7bdcc6e12850e07160101787ceb5160fdfc5baf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-07 09:57:21 +0000

    libwpg: update 0.3.3 bottle.

--- Formula/libwpg.rb
+++ Formula/libwpg.rb
@@ -6,6 +6,7 @@ class Libwpg < Formula
 
   bottle do
     cellar :any
+    sha256 "d12ae12e729a2d2e327f07fe927e02dd15151a987b7cab0a19ca94ee15f8cfde" => :catalina
     sha256 "162171b22e6df4f4f4169634fc6872d40bea9a17a9c49e01dd737e9d74b1d445" => :mojave
     sha256 "dd0c4dc2a9369d7d6b97f930dd63e6f4ddd9d12d0372c12e13d2a22cf6a0cd06" => :high_sierra
     sha256 "cf9ab0d990b3fccb101312999f6d0ea5980990edd279ae994cf3c7f9c33a7d55" => :sierra
