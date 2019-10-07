d229a4c66511fba963eb3814f7f11fc6e5bdb533
Andy Li <andy@onthewings.net>
2019-10-07 10:14:04 +0800

    graphviz 2.42.2
    Closes #44956.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/graphviz.rb
+++ Formula/graphviz.rb
@@ -1,12 +1,10 @@
 class Graphviz < Formula
   desc "Graph visualization software from AT&T and Bell Labs"
   homepage "https://www.graphviz.org/"
-  # versioned URLs are missing upstream as of 16 Dec 2017
-  url "https://www.mirrorservice.org/sites/distfiles.macports.org/graphviz/graphviz-2.40.1.tar.gz"
-  mirror "https://fossies.org/linux/misc/graphviz-2.40.1.tar.gz"
-  sha256 "ca5218fade0204d59947126c38439f432853543b0818d9d728c589dfe7f3a421"
-  revision 1
+  url "https://gitlab.com/graphviz/graphviz/-/archive/2.42.2/graphviz-2.42.2.tar.gz"
+  sha256 "b92a92bb16755b11875be9203a6216e5b827eb1d6cf8dda6824380457cd18c55"
   version_scheme 1
+  head "https://gitlab.com/graphviz/graphviz.git"
 
   bottle do
     sha256 "9cce10295f139ee106efb2ea147a2ddfe96f3e01019881d4378d1a5f67086ae5" => :catalina
@@ -15,14 +13,9 @@ class Graphviz < Formula
     sha256 "3336446bf3ad335583744a88549b19a0bae2fd427270863476c2590a575ff021" => :sierra
   end
 
-  head do
-    url "https://gitlab.com/graphviz/graphviz.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
-
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "gd"
   depends_on "gts"
@@ -30,15 +23,6 @@ class Graphviz < Formula
   depends_on "libtool"
 
   def install
-    # Only needed when using superenv, which causes qfrexp and qldexp to be
-    # falsely detected as available. The problem is triggered by
-    #   args << "-#{ENV["HOMEBREW_OPTIMIZATION_LEVEL"]}"
-    # during argument refurbishment of cflags.
-    # https://github.com/Homebrew/brew/blob/ab060c9/Library/Homebrew/shims/super/cc#L241
-    # https://github.com/Homebrew/legacy-homebrew/issues/14566
-    # Alternative fixes include using stdenv or using "xcrun make"
-    inreplace "lib/sfio/features/sfio", "lib qfrexp\nlib qldexp\n", "" unless build.head?
-
     args = %W[
       --disable-debug
       --disable-dependency-tracking
@@ -52,11 +36,7 @@ class Graphviz < Formula
       --with-gts
     ]
 
-    if build.head?
-      system "./autogen.sh", *args
-    else
-      system "./configure", *args
-    end
+    system "./autogen.sh", *args
     system "make", "install"
 
     (bin/"gvmap.sh").unlink
