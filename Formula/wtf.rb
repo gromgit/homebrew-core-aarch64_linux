09d387ed1f32485d1407f62db73beb3baa065b69
chenrui <rchen@meetup.com>
2019-10-07 13:54:29 -0400

    wtf 20191006 (#44969)

--- Formula/wtf.rb
+++ Formula/wtf.rb
@@ -1,8 +1,8 @@
 class Wtf < Formula
   desc "Translate common Internet acronyms"
   homepage "https://sourceforge.net/projects/bsdwtf/"
-  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20191002.tar.gz"
-  sha256 "9923b4214e52b93ca26cc3f078fb0fdcaab2ef838fac52702c766696ce7ccd40"
+  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20191006.tar.gz"
+  sha256 "0ba0bdcd6569ce444e9280a2e680b3c0cba461bbb6eeeb5f6f4ee31acc95e43c"
 
   bottle :unneeded
 
