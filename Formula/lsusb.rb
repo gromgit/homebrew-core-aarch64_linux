7e2e6f1d42a2504bdd6c28b011c0d28f37e96286
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-07 00:41:09 +0000

    lsusb: update 1.0 bottle.

--- Formula/lsusb.rb
+++ Formula/lsusb.rb
@@ -6,6 +6,7 @@ class Lsusb < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7ab2cb027f186840ea1c96e47b4d48a8dfc42d91847d79bdd3faa6677ef603ca" => :catalina
     sha256 "4f2f4f45cb6df2d5262bb823e02f750e7e5b4f117dca8a41fc6956435a277cb9" => :mojave
     sha256 "e696db36d09169064b3e97852d07464125e5bc6e400cb2a4cc186e6aa606574a" => :high_sierra
     sha256 "e696db36d09169064b3e97852d07464125e5bc6e400cb2a4cc186e6aa606574a" => :sierra
