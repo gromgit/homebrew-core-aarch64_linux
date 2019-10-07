a6e08d877a4d898683ee4133e196dbbdd4a98046
Igor Kapkov <igasgeek@me.com>
2019-10-07 18:48:48 +1100

    vulkan-headers 1.1.124
    Closes #44962.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/vulkan-headers.rb
+++ Formula/vulkan-headers.rb
@@ -1,8 +1,8 @@
 class VulkanHeaders < Formula
   desc "Vulkan Header files and API registry"
   homepage "https://github.com/KhronosGroup/Vulkan-Headers"
-  url "https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.1.123.tar.gz"
-  sha256 "aa58ddcf972f05d26bdfa50ac689022b77baf101317258ac146393ef47741581"
+  url "https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.1.124.tar.gz"
+  sha256 "ef33146bfc92a4889de8e9cc4b135740b5651b2250b0e8483ea745ce5894a04b"
 
   bottle do
     cellar :any_skip_relocation
