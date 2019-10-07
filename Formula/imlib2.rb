f6d9c1d34afc84f9183b1e198776ab65fe566d5f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-07 11:51:07 +0000

    imlib2: update 1.5.1 bottle.

--- Formula/imlib2.rb
+++ Formula/imlib2.rb
@@ -6,6 +6,7 @@ class Imlib2 < Formula
 
   bottle do
     rebuild 1
+    sha256 "13ce2f21891d83e1c8087f33bdf80a1f703975a21a8ca0927a4385321647813b" => :catalina
     sha256 "f2bb2e5ca54865edf5808245b54b0b636669e496757989bd4d6a19dc098b152b" => :mojave
     sha256 "0349423dda370df2ec72d02ff5d818f0f7c34197b9d6661212ae03c14b6dfbb0" => :high_sierra
     sha256 "1bebf0a2a3e58f6766e32fe6cbafa8f478788eb9aaed64204fa113204981a3f5" => :sierra
