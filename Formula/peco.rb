b78f8852ce6690bcde11c20d529f2bddc9483bb5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-07 00:17:19 +0000

    peco: update 0.5.3 bottle.

--- Formula/peco.rb
+++ Formula/peco.rb
@@ -7,6 +7,7 @@ class Peco < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "cc754e10e1b8c80ce328659814c4450f5b5fcfe8947dc1972631877b52ad3b19" => :catalina
     sha256 "d2a3d2950d81cf76b42950eba381f3359e0fa8c2b95829f8eb771013ff4cc4a1" => :mojave
     sha256 "653707beb0b04d448f7d4184213575df8a6ce138e42abff2861a98a8f1aa60b6" => :high_sierra
     sha256 "e4a10c067d24b7b790c9e522f9405cb5437d2b11837a3291ca2fd2ba00cfc253" => :sierra
