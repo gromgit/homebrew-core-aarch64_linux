aff05952497389097820525c9bf8c7e141aee9b2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-07 00:48:05 +0000

    sshuttle: update 0.78.5 bottle.

--- Formula/sshuttle.rb
+++ Formula/sshuttle.rb
@@ -10,6 +10,7 @@ class Sshuttle < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "107aecff3f7504b0eab9d370aca884bc13f24df4ff6b55a2b8e4d8664ba985a2" => :catalina
     sha256 "dff7259d647fd392e53de03660ece7be96452882938f387c199938f8054f4c2b" => :mojave
     sha256 "28f3d7fc8858779cb6020908302fb185810d574e8023e935b8410d0660973d49" => :high_sierra
     sha256 "04e8f60acb5131f58a0fbe66d7c8d847c4d423db3edb2671ff1319fc499a2c5f" => :sierra
