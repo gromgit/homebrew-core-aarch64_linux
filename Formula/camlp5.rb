d941094776c9ad518fee668db823db4ce3a3d11a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-07 00:17:19 +0000

    camlp5: update 7.10 bottle.

--- Formula/camlp5.rb
+++ Formula/camlp5.rb
@@ -7,6 +7,7 @@ class Camlp5 < Formula
   head "https://gforge.inria.fr/anonscm/git/camlp5/camlp5.git"
 
   bottle do
+    sha256 "6c8c1d3f0239f92b455a1cb801e45ba6b4cb7f9021a48f36ccade3f0a9c3a679" => :catalina
     sha256 "f20e753e899bf8d6fedf51284a5ffeabfffc3da01151d07ef6f040888ffd159a" => :mojave
     sha256 "c6d3725bb7e79d77c00b1f430abad3f8542563eab79429eaa7f0420138a38d94" => :high_sierra
     sha256 "6246f8aa046ae45095f20ad3c839b317d284c7c4a27b6d572b8bb3256d9a5c19" => :sierra
