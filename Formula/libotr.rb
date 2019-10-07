9eddfa32fcb2781bf998ee90ab0abe70f73ed7c4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-07 09:57:21 +0000

    libotr: update 4.1.1 bottle.

--- Formula/libotr.rb
+++ Formula/libotr.rb
@@ -6,6 +6,7 @@ class Libotr < Formula
 
   bottle do
     cellar :any
+    sha256 "b841026a4752756107affe9f6016da14ea5a9a0a48b33ccd461eced5cd89b64a" => :catalina
     sha256 "90da0033157a7771cf7239d038f36e0d613616f1918a168fa763f3e2eafc0106" => :mojave
     sha256 "0b340441feba4b325c3ff5c26a9e79b16294461f6f681ae42a2a5d45966e7391" => :high_sierra
     sha256 "9f0b214278e4cdf81a1a0c083f1aa45ba64430b449121c4d0596357952dcc93d" => :sierra
