9e263a15c701bdda8a52ffe1806e881fcf732fa3
Caleb Xu <calebcenter@live.com>
2019-10-07 21:47:17 -0400

    zsh-completions 0.31.0 (#44482)

--- Formula/zsh-completions.rb
+++ Formula/zsh-completions.rb
@@ -1,8 +1,8 @@
 class ZshCompletions < Formula
   desc "Additional completion definitions for zsh"
   homepage "https://github.com/zsh-users/zsh-completions"
-  url "https://github.com/zsh-users/zsh-completions/archive/0.30.0.tar.gz"
-  sha256 "981c386fa01f9bcb0c8a66b0db16af794ab10169d12e2242f83715967b737621"
+  url "https://github.com/zsh-users/zsh-completions/archive/0.31.0.tar.gz"
+  sha256 "6c1cccf6fd8c7d875145e357e3a46680b74013b6fc7b98724801126c072fc024"
   head "https://github.com/zsh-users/zsh-completions.git"
 
   bottle :unneeded
