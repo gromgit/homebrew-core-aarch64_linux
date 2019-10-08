f72a69f196053222578d871c9bed830dfb2f5e37
Michael Chiu <chiumichel@gmail.com>
2019-10-07 23:55:09 -0400

    fmt 6.0.0
    Closes #44999.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/fmt.rb
+++ Formula/fmt.rb
@@ -1,8 +1,8 @@
 class Fmt < Formula
   desc "Open-source formatting library for C++"
   homepage "https://fmtlib.github.io/"
-  url "https://github.com/fmtlib/fmt/archive/5.3.0.tar.gz"
-  sha256 "defa24a9af4c622a7134076602070b45721a43c51598c8456ec6f2c4dbb51c89"
+  url "https://github.com/fmtlib/fmt/archive/6.0.0.tar.gz"
+  sha256 "f1907a58d5e86e6c382e51441d92ad9e23aea63827ba47fd647eacc0d3a16c78"
 
   bottle do
     cellar :any_skip_relocation
