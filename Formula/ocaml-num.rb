c6602c5c34968d1d76db31e475af50511ab70a7f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-09 11:58:48 +0000

    ocaml-num: update 1.2_1 bottle.

--- Formula/ocaml-num.rb
+++ Formula/ocaml-num.rb
@@ -7,6 +7,7 @@ class OcamlNum < Formula
 
   bottle do
     cellar :any
+    sha256 "30c8c4e2925aec9055c637286a678b8b2b8e35660deaf410dcf50089238c0246" => :catalina
     sha256 "0181124dda2a3a5c35cbf1155712526b99bbc402df14b24cc532b9a6ef8b06e9" => :mojave
     sha256 "bcaa0380dc03f7095cd4b00656ae3b4e1cbe66c8519cbbe7eab63efa76e31503" => :high_sierra
     sha256 "819a3ceaa82022ec9b8019610d906d4520752810dcb688595c23e8cf25051b57" => :sierra
