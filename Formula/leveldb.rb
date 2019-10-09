05e7d573484009334f08e3545363e594961936ee
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-09 11:32:43 +0000

    leveldb: update 1.22 bottle.

--- Formula/leveldb.rb
+++ Formula/leveldb.rb
@@ -6,6 +6,7 @@ class Leveldb < Formula
 
   bottle do
     cellar :any
+    sha256 "908fb99544bbc0906134bc9677fbd91c6948324c4de6cd1315fc7e5e6f6634cc" => :catalina
     sha256 "22e4a129bedd5030525f749a5b5ec978bf6da0a9b0625fe829da482a5ab85755" => :mojave
     sha256 "b1cf697cad28caac418d2e0ef49bc90863f389402185d3cc0f1f7079516d02c2" => :high_sierra
     sha256 "810dbeba5e3f7d72d4772b9eff4d9022a1240c0abc6235afbd343c199741e6f7" => :sierra
