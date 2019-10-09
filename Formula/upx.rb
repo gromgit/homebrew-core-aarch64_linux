22d5bd20b8df1c276fffe5e1c4a71f36d99e83af
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-09 11:54:12 +0000

    upx: update 3.95_1 bottle.

--- Formula/upx.rb
+++ Formula/upx.rb
@@ -44,6 +44,7 @@ class Upx < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4e49be10264340e0c964f9595bedf797df5b18c4489a32265d049bdc371d07ea" => :catalina
     sha256 "29c7ae82e1fda0f801c388d874d02d9371ca5c7842ae9a8355a16eb2daa8035f" => :mojave
     sha256 "bd7b838097f139055c99a692f9e74f62582d020203d649a190d74c9e9ccab584" => :high_sierra
     sha256 "209bff5e1c4622c2e8f19ef8855e006ad5fb2fde93f937717b39c9aebc4de07e" => :sierra
