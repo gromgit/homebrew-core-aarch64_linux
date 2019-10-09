d6d7b62a06cb1ef4e4de877c1731b61e09b547e4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-09 11:52:12 +0000

    exiv2: update 0.27.2 bottle.

--- Formula/exiv2.rb
+++ Formula/exiv2.rb
@@ -7,6 +7,7 @@ class Exiv2 < Formula
 
   bottle do
     cellar :any
+    sha256 "fdadbb93ae659a651b2e7b899c3c2bf8910b8f0891661903a04a4c81a66ff534" => :catalina
     sha256 "3b78b8fbffcc6d62685bc4a9a0a51855f5ccf6fe7fabc866f0970e1a12ced0b4" => :mojave
     sha256 "8a4e65d47307247b11127c00cdad18626425eafb271faaeb1c076beb57298e12" => :high_sierra
     sha256 "fe386bc9bfe7270655a6b3163f8e33a6fc6e6f36512e6ac6e6a49a1650a6a485" => :sierra
