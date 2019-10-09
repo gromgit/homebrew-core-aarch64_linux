4c479394d4aae48509d726e6cb92a13ccef0fd2b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-09 12:23:09 +0000

    i386-elf-binutils: update 2.32 bottle.

--- Formula/i386-elf-binutils.rb
+++ Formula/i386-elf-binutils.rb
@@ -6,6 +6,7 @@ class I386ElfBinutils < Formula
   sha256 "9b0d97b3d30df184d302bced12f976aa1e5fbf4b0be696cdebc6cca30411a46e"
 
   bottle do
+    sha256 "3a3d88a8b8d33ae13854cae4ccb7120b277862ae42e863acee9926744294ed0c" => :catalina
     sha256 "deef284fbb0b50aeafdaf9312c56bed4f3ca9571374f4174ab6f877bcfdab3e0" => :mojave
     sha256 "f382f13d45f7d71ca9471d78aae6d03af5f1622ede5346cb27014cf36abf7b29" => :high_sierra
     sha256 "992fa92fb007f81e25469a479730b560d84d06c6eedaac1b989b929ee6609c57" => :sierra
