40a54f080f8b62fceac8a63c87b45fc8157712e8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-09 11:58:48 +0000

    miniupnpc: update 2.1 bottle.

--- Formula/miniupnpc.rb
+++ Formula/miniupnpc.rb
@@ -6,6 +6,7 @@ class Miniupnpc < Formula
 
   bottle do
     cellar :any
+    sha256 "de583006c4f55d9d085ac9bb94123bc55e769aba1e7f483fcb51f7a1db490c84" => :catalina
     sha256 "7207c8a442fc73842141aba18994652a9dcea64813b0e5ad86901a8e9ae026f2" => :mojave
     sha256 "266556f31f0430f41a1d64b3fb96daea2a4804b1a85c4486c5e39de0f2808d35" => :high_sierra
     sha256 "ed39714d275ffb083e29c72b3a5d9142c0a4081fb8c8479950f71bbddbe5d196" => :sierra
