4635ff3021c7af6b44e77d7b90f365cf0b216022
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-09 11:19:31 +0000

    libgphoto2: update 2.5.23 bottle.

--- Formula/libgphoto2.rb
+++ Formula/libgphoto2.rb
@@ -5,6 +5,7 @@ class Libgphoto2 < Formula
   sha256 "d8af23364aa40fd8607f7e073df74e7ace05582f4ba13f1724d12d3c97e8852d"
 
   bottle do
+    sha256 "d5e55b07bcb538aed536da66d79c6366a58e0e106b5835846cf5d8bd06aa02b7" => :catalina
     sha256 "b758f4caa384721d7593bec1314dd9e05fa05f69a74e7edd702dc3e9fabb4377" => :mojave
     sha256 "efb624c0e1495dce73b72b24868a5fab80836aa162b37951c2ad04eba093b5c5" => :high_sierra
     sha256 "21dc9f114533994fbeedd7893a8c40b2ec42f2a46bd14d2c40d463e397e9c69b" => :sierra
