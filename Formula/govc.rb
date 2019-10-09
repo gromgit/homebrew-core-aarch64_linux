ea4d29c1ac22319bd8237d879b1b1152ea2a55c3
dutch <chris@lamberson.online>
2019-10-09 11:04:11 -0700

    govc 0.21.0 (new formula)
    Closes #45056.
    
    Signed-off-by: Rui Chen <rchen@meetup.com>

---
+++ Formula/govc.rb
@@ -0,0 +1,16 @@
+class Govc < Formula
+  desc "Command-line tool for VMware vSphere"
+  homepage "https://github.com/vmware/govmomi/tree/master/govc"
+  url "https://github.com/vmware/govmomi/archive/v0.21.0.tar.gz"
+  sha256 "3690bc16e6193ee0d142e8abc1941ad38f841494dd460277722ba3873e0793dc"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", "#{bin}/#{name}", "./#{name}"
+  end
+
+  test do
+    assert_match "GOVC_URL=foo", shell_output("#{bin}/#{name} env -u=foo")
+  end
+end
