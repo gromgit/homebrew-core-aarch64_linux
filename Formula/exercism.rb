75fa93d7ed30bfd74e02084a6e2574c9ef7b80fd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-09 10:57:08 +0000

    exercism: update 3.0.12 bottle.

--- Formula/exercism.rb
+++ Formula/exercism.rb
@@ -7,6 +7,7 @@ class Exercism < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a6f79c60ab990d249f47af44310ecb93840c8af784aca73aeb413fdbd24b58ea" => :catalina
     sha256 "832b5e24b5310320c075c083e149ea59d59f1b5916bfb3d47093053a26930491" => :mojave
     sha256 "fdbe96b882426e313866bedb826b606842a9448ad9884e23b171bc97f2205803" => :high_sierra
     sha256 "983714f0e0e358b0fed5f300214cfdd8404edd6817b0f5bb0fc756cb61cb4bf4" => :sierra
