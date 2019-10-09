f1bdcda4d53e7c8c4bb5db663e8754cfd14bf994
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-09 19:44:55 +0000

    git-delta: add 0.0.12 bottle.

--- Formula/git-delta.rb
+++ Formula/git-delta.rb
@@ -6,6 +6,13 @@ class GitDelta < Formula
     :revison => "2ba73e32d57075bb8b34181752f977fcbadfe5f5"
   head "https://github.com/dandavison/delta.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7f8c588c4f4cfad057029fd9d8848b86bb87fb3cefa4547293e7f8a21cde6601" => :catalina
+    sha256 "e65c3f7f684ea10f13f4738af0e9cfde5d35f8600e2d49015e6dea5b2b398389" => :mojave
+    sha256 "4404c76204184cdcd89224c86e96b268b7a81979b307f54b57f28338899aadbf" => :high_sierra
+  end
+
   depends_on "rust" => :build
   uses_from_macos "llvm"
 
