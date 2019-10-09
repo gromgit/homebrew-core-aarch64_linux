bc75af7be83eb5521d24735e465bdba4d761acbb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-09 10:56:41 +0000

    libmatroska: update 1.5.2 bottle.

--- Formula/libmatroska.rb
+++ Formula/libmatroska.rb
@@ -7,6 +7,7 @@ class Libmatroska < Formula
 
   bottle do
     cellar :any
+    sha256 "52d5d700ad22e0486fc93dc0115609425e2e6874bf6259946c7c441b1ef03612" => :catalina
     sha256 "ed8d3ac8b50484c2db4dafc46a7bab0d59751ac4bb99d1f5c11d8e18f71024d5" => :mojave
     sha256 "4645660af5cf1737a3bc91815e6e82a466fbf82dd4f8f7cf483139871aa1f513" => :high_sierra
     sha256 "90faa3f9bbab4b0bba4777c5dfb89b00000dd49b8da104631a4eb2b76b9e45e9" => :sierra
