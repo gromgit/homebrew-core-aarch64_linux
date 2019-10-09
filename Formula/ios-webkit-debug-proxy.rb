4270b5d654621e91f94352d89eec6775ae6d95d4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-09 11:13:58 +0000

    ios-webkit-debug-proxy: update 1.8.5_1 bottle.

--- Formula/ios-webkit-debug-proxy.rb
+++ Formula/ios-webkit-debug-proxy.rb
@@ -8,6 +8,7 @@ class IosWebkitDebugProxy < Formula
 
   bottle do
     cellar :any
+    sha256 "f9eb8f0af0d41e8b35bc6d6f192ee92470ad4050767720849a4fa42768ac3f9b" => :catalina
     sha256 "1aadb8bdcb8d9f3026f20fdbae563d01d889964a94a46995ebbdf70446f3d4a7" => :mojave
     sha256 "b2ebcf8d4009ec190ca13f9fdf6dfc212b3db6cd2fe36cdc16b89054f25d1afa" => :high_sierra
     sha256 "a41fac7294b52cb9742d8cb04ed36a182b0bfabd41342529862b9896a3538a55" => :sierra
