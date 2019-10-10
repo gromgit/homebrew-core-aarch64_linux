0b14ced8503453d63acfa12ac81624cb34a6eab8
Baptiste Fontaine <b@ptistefontaine.fr>
2019-10-10 21:03:33 +0200

    mysqltuner 1.7.17
    Closes #45124.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/mysqltuner.rb
+++ Formula/mysqltuner.rb
@@ -1,8 +1,8 @@
 class Mysqltuner < Formula
   desc "Increase performance and stability of a MySQL installation"
   homepage "https://raw.github.com/major/MySQLTuner-perl/master/mysqltuner.pl"
-  url "https://github.com/major/MySQLTuner-perl/archive/1.7.15.tar.gz"
-  sha256 "1486bd7baaab06a2cffd0783ae17bd5a0d7240c08b920c82185097ccda69fcc8"
+  url "https://github.com/major/MySQLTuner-perl/archive/1.7.17.tar.gz"
+  sha256 "c82f29aa017360ab8888808393bd06e79af6fa447a62908a3b3dddea92b768b5"
   head "https://github.com/major/MySQLTuner-perl.git"
 
   bottle :unneeded
