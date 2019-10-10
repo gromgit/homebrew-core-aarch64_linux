a5ca2b05a78f43cd3fbf924f18608b72f7901867
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-10 18:55:25 +0000

    isync: update 1.3.1_1 bottle.

--- Formula/isync.rb
+++ Formula/isync.rb
@@ -8,6 +8,7 @@ class Isync < Formula
 
   bottle do
     cellar :any
+    sha256 "d4ea3fd276458ae669596cc955beee0d2cb38ab217fc51c5c6c2acb6c73de260" => :catalina
     sha256 "7863e1861cc119853fadc35ff6afe7f13bf1e420f22b70e77d0bb32997943329" => :mojave
     sha256 "2da1bd2fef7c6eb9af331a0536e02df8dd0bc9b0fc42eb534ec8499a87f8c197" => :high_sierra
     sha256 "a19f503aa9490146a19a4197e8e0190cffad685c7fdba0582544c44ee96f1fe5" => :sierra
