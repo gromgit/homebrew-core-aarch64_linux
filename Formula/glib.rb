ae054fb6b37e332b6764b9f12da43c51635e01fc
Kleis Auke Wolthuizen <github@kleisauke.nl>
2019-10-10 22:02:41 +0200

    glib: fix failing inreplace
    It seems that Meson 0.52.0 (#45089) generates the pkg-config file in a slightly different way.
    
    Closes #45179.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/glib.rb
+++ Formula/glib.rb
@@ -56,7 +56,7 @@ class Glib < Formula
     # have a pkgconfig file, so we add gettext lib and include paths here.
     gettext = Formula["gettext"].opt_prefix
     inreplace lib+"pkgconfig/glib-2.0.pc" do |s|
-      s.gsub! "Libs: -lintl -L${libdir} -lglib-2.0",
+      s.gsub! "Libs: -L${libdir} -lglib-2.0 -lintl",
               "Libs: -L${libdir} -lglib-2.0 -L#{gettext}/lib -lintl"
       s.gsub! "Cflags: -I${includedir}/glib-2.0 -I${libdir}/glib-2.0/include",
               "Cflags: -I${includedir}/glib-2.0 -I${libdir}/glib-2.0/include -I#{gettext}/include"
