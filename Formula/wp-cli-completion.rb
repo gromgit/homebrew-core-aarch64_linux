7663d7b516888bb952c0ae8af2151471d4eccc0c
Baptiste Fontaine <b@ptistefontaine.fr>
2019-10-10 21:49:06 +0200

    wp-cli-completion 2.3.0
    Closes #45159.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/wp-cli-completion.rb
+++ Formula/wp-cli-completion.rb
@@ -1,8 +1,8 @@
 class WpCliCompletion < Formula
   desc "Bash completion for Wpcli"
   homepage "https://github.com/wp-cli/wp-cli"
-  url "https://github.com/wp-cli/wp-cli/archive/v2.2.0.tar.gz"
-  sha256 "c40b7427532b7c42623a23e3537c6440463b0bc5294943bbaa1805cd88c73083"
+  url "https://github.com/wp-cli/wp-cli/archive/v2.3.0.tar.gz"
+  sha256 "316096eec59654f1ab473ef53c7a0172b0d286be0c6238332b7ab731069c30c1"
   head "https://github.com/wp-cli/wp-cli.git"
 
   bottle :unneeded
