a44585cbb2be364373e5343cbe697f860f84f7f4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-10 19:39:27 +0000

    tokei: update 10.0.1 bottle.

--- Formula/tokei.rb
+++ Formula/tokei.rb
@@ -6,6 +6,7 @@ class Tokei < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f5b3afdcdab0f7c07d78f824463f27ddddaa318ceec020db851d317ce48cc672" => :catalina
     sha256 "f103533c65977fe05d4f6c3c3bb4a743203f4ed2ee1db6b1e333f778afbb198d" => :mojave
     sha256 "6483b07776d509d91e57e619dcba915a334f016f144beb5018029ed394e5ad3b" => :high_sierra
     sha256 "9168e0097002c31e08b892625072dc86e1820a59d51a0c7a4c4f4aaaf1e2a4c0" => :sierra
