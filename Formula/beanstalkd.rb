af568d089fc24b891a883f4ab69fc58859ae42a8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-10 16:21:15 +0000

    beanstalkd: update 1.11 bottle.

--- Formula/beanstalkd.rb
+++ Formula/beanstalkd.rb
@@ -6,6 +6,7 @@ class Beanstalkd < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "addabe0f9d9b6e0acdaaa389f3fa74c790d18e74541776854bd0d81864dbea3c" => :catalina
     sha256 "25b0295808e5a2656048353cbdb1951f1a2adcec9c0670a8e2f2f3def0a390c7" => :mojave
     sha256 "bb0e40b2cae04860948bb8496c4255185c19bb5b184256623168d9b9731a82ab" => :high_sierra
     sha256 "26ac90328390f5ba74741810df546a6faa99d37f8d124ce1225789794d3568ac" => :sierra
