feb769f67f554217150df81fa410410380ce1cbf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-10 15:22:57 +0000

    pdfgrep: update 2.1.2 bottle.

--- Formula/pdfgrep.rb
+++ Formula/pdfgrep.rb
@@ -6,6 +6,7 @@ class Pdfgrep < Formula
 
   bottle do
     cellar :any
+    sha256 "575608fb99410f9271656ed1bf051456318cd7bece7ae654d122db930ddbd7b3" => :catalina
     sha256 "4e6828ef5db24086dae00e10c9c18671352303c6e79a2148f62bd9104678ea08" => :mojave
     sha256 "95ffadce5ed5baa82a48c71e1bb8915d080c9e9d4a14e63982945eb543e58b10" => :high_sierra
     sha256 "b004e7801489c6cb0361c5032278d11cafd4ace151a02ee97214c79dba0f89be" => :sierra
