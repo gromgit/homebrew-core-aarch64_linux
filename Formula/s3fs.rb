3a299e9a4223c7130616a853e79bf5c7d70582e0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-10 16:21:15 +0000

    s3fs: update 1.85 bottle.

--- Formula/s3fs.rb
+++ Formula/s3fs.rb
@@ -7,6 +7,7 @@ class S3fs < Formula
 
   bottle do
     cellar :any
+    sha256 "d1f6ed9a6ba336c7d7650a27df5aedc8307596bfa909f764f44ee2a57c56aa97" => :catalina
     sha256 "08a49f1747589a73c47e1d33009ab1ec9f915e8198bfce4471e9deb3ab22b3ec" => :mojave
     sha256 "c9f8045bd8cf246306103400cfe191e92a41d52baccd7ab9596dd80cdaa30237" => :high_sierra
     sha256 "37952aa8b45492b8fcd4b3c468b5c72ceec9407b1126a79c1cbf4f0a9f0e878b" => :sierra
