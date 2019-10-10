552fa162de12243655404e8855525a44e4eb6cc5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-10 15:22:58 +0000

    subnetcalc: update 2.4.14 bottle.

--- Formula/subnetcalc.rb
+++ Formula/subnetcalc.rb
@@ -7,6 +7,7 @@ class Subnetcalc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5a8ee90e354ce39bd02d8ad7baa4ef0575d26c1a78cc16264c61e3da6a6da5ca" => :catalina
     sha256 "cb1f6dd2854ca7d6049d8c7336ba4ac5537b3927057753f2ca36a58f8bee842f" => :mojave
     sha256 "ff67b614cd174896b3ad818df3b98aedd8741bb7d9345c12fec9946f1c3ad022" => :high_sierra
     sha256 "43aa559cbf9c1253bc95a0fda1a9036b2c7b334b38a0bfae95091f1502f61a35" => :sierra
