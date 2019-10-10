1e95a69c873c71eec491e9e06f9cbdeb8023e524
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-10 18:00:42 +0000

    pbzip2: update 1.1.13 bottle.

--- Formula/pbzip2.rb
+++ Formula/pbzip2.rb
@@ -6,6 +6,7 @@ class Pbzip2 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "57c1c1065cd29ee37187b87705adfb73b84d114fc46408d4690024f3a29ac837" => :catalina
     sha256 "5594212d69f619f7fa59cfec23ce2c6eefa0f8c69d5e77cdd84cf9e2478d0d51" => :mojave
     sha256 "d72e618d7301937ab6a392e1ef3d9ed1f8d5380cd6516ea17b4e4bde11eea9a7" => :high_sierra
     sha256 "c15b9c38b5302286033e54ff4be006c3b31ccb179f96641e44f1126958527d7e" => :sierra
