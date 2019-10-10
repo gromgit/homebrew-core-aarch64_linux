6b0deaa735d6e1a37e0caca6c6860eefb3b60779
Rui Chen <rchen@meetup.com>
2019-10-09 20:14:46 -0400

    duo_unix 1.11.3
    Closes #45072.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/duo_unix.rb
+++ Formula/duo_unix.rb
@@ -1,9 +1,8 @@
 class DuoUnix < Formula
   desc "Two-factor authentication for SSH"
   homepage "https://www.duosecurity.com/docs/duounix"
-  url "https://github.com/duosecurity/duo_unix/archive/duo_unix-1.11.2.tar.gz"
-  sha256 "e1ec2f43036ba639743d631f308419c9a88618a93d4038bf40a9cdeef89ca6db"
-  revision 1
+  url "https://github.com/duosecurity/duo_unix/archive/duo_unix-1.11.3.tar.gz"
+  sha256 "beabba9fc286fea651941548aa830920f465ca6b754adb6fbc050ad420d3ae75"
 
   bottle do
     sha256 "210721657888ee878f11a03497a9e5d8823410c4d0099b9aa33d03af4cce3b6e" => :mojave
