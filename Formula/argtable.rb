a0d4a2d95798bc15b1234739be63dd9dfa7ad288
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-10 19:39:27 +0000

    argtable: update 2.13 bottle.

--- Formula/argtable.rb
+++ Formula/argtable.rb
@@ -7,6 +7,7 @@ class Argtable < Formula
 
   bottle do
     cellar :any
+    sha256 "29bfa5bfd7e897512347ecf664c3e3a9bbe7ec585115c09167ca8b6c312be9d6" => :catalina
     sha256 "61ec2ac4b9e65f7965931dfd983848fae06130686c4f800eb9341f96a6f6d398" => :mojave
     sha256 "e68b3df66d638a024c3b57b069bcdebfbdabb230a9c851de886321c2b3df7099" => :high_sierra
     sha256 "9485d1e045ed40c0145eb867f9d24425ccedd53b4f0cb0ec949139b0c99507c7" => :sierra
