55ba232ab66ccee0b1606ade2b77868b083e8e17
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-10 07:08:28 +0000

    screenfetch: update 3.9.0 bottle.

--- Formula/screenfetch.rb
+++ Formula/screenfetch.rb
@@ -7,11 +7,9 @@ class Screenfetch < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3e69c6119f9b9b4133817ce2d06de71317866c0b4aa08773aec038abef2d31ef" => :mojave
-    sha256 "8e0571c592a9ae4b20d7e88e12dcbde9b722856721c3227d3334800cfd8445f5" => :high_sierra
-    sha256 "62441999c591325de600f4cbd5d46e4157c443cfb514a3b1dfce8764c911785d" => :sierra
-    sha256 "62441999c591325de600f4cbd5d46e4157c443cfb514a3b1dfce8764c911785d" => :el_capitan
-    sha256 "62441999c591325de600f4cbd5d46e4157c443cfb514a3b1dfce8764c911785d" => :yosemite
+    sha256 "4ebc2dbe3075fdb801971050cf9f5970142309c4473e72fa78c4f18de5db2933" => :catalina
+    sha256 "4ebc2dbe3075fdb801971050cf9f5970142309c4473e72fa78c4f18de5db2933" => :mojave
+    sha256 "4ebc2dbe3075fdb801971050cf9f5970142309c4473e72fa78c4f18de5db2933" => :high_sierra
   end
 
   def install
