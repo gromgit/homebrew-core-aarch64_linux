e8850eef0ac810312350c811705c704619e2f2cd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-10 22:23:39 +0000

    pngcrush: update 1.8.13 bottle.

--- Formula/pngcrush.rb
+++ Formula/pngcrush.rb
@@ -6,6 +6,7 @@ class Pngcrush < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f6b31e35011fd69dc4ee678e4529fd5a76ee7be8faba88bb7c9cb0b7cbfafacb" => :catalina
     sha256 "904e958b1198e2931ab233981764b1ec66b26da793445c0fa10182588b5369a7" => :mojave
     sha256 "db13f642eae1815e00e1a80d363228e0311d85ca510e9c9de94dba8483fa2d87" => :high_sierra
     sha256 "f648ad0c664699f67bba8ba791358e8b294d0c1d975f026aa67fc1635badbc73" => :sierra
