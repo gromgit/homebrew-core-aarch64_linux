f26cb5b7138d40c82092ed005ccb5bde95af79ee
Romain Batby <romainb@theodo.fr>
2019-10-10 13:32:27 +0200

    flyway 6.0.4 (#45029)

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/6.0.2/flyway-commandline-6.0.2.tar.gz"
-  sha256 "976e68dad31dd216f002a972ed7cdd77e6b25857fc5122ef0f30446af158e32a"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/6.0.4/flyway-commandline-6.0.4.tar.gz"
+  sha256 "47129b3b6ed0b6b77940bfba281f6f9ed83e90e5184f0801084ee03d98d91258"
 
   bottle :unneeded
 
