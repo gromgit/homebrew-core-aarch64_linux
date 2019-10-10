bd165966dd7efcb99737eef6f162dfea46b92bd2
Rui Chen <rchen@meetup.com>
2019-10-10 13:54:35 -0400

    fork-cleaner 1.5.0
    fix
    
    Closes #45109.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/fork-cleaner.rb
+++ Formula/fork-cleaner.rb
@@ -1,8 +1,8 @@
 class ForkCleaner < Formula
   desc "Cleans up old and inactive forks on your GitHub account"
   homepage "https://github.com/caarlos0/fork-cleaner"
-  url "https://github.com/caarlos0/fork-cleaner/archive/v1.4.3.tar.gz"
-  sha256 "5a71b3d454ac030522ea5fd8f78b90432d4a9a299c2b923388e7f9ee223795d8"
+  url "https://github.com/caarlos0/fork-cleaner/archive/v1.5.0.tar.gz"
+  sha256 "208d881aed374c97e6abb13880ef1df67c1a3a1da8577d8588cf1e55f4911961"
 
   bottle do
     cellar :any_skip_relocation
@@ -11,15 +11,15 @@ class ForkCleaner < Formula
     sha256 "c4f6ff2fb271638993210665fa49a44a6bb57f40116f87e7f1872029dc10000f" => :sierra
   end
 
-  depends_on "dep" => :build
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
+
     dir = buildpath/"src/github.com/caarlos0/fork-cleaner"
     dir.install buildpath.children
+
     cd dir do
-      system "dep", "ensure", "-vendor-only"
       system "make"
       bin.install "fork-cleaner"
       prefix.install_metafiles
