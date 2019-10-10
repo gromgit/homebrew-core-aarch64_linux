27abb3049e4d7669c0c71869d6df152d1f79401c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-10 21:31:22 +0000

    httping: update 2.5_2 bottle.

--- Formula/httping.rb
+++ Formula/httping.rb
@@ -8,6 +8,7 @@ class Httping < Formula
 
   bottle do
     cellar :any
+    sha256 "9432f93eec676aad685be06819da5649ec071f6542302d077ccf5d0623b9b567" => :catalina
     sha256 "2314efd3b919b759290b7ead8dea99c50b11860f7aadb8fd4f9c7e0e7cc92e5e" => :mojave
     sha256 "8df0f98d479c72a20ca2b353a06c9c1bf071cceed53774c737f41caf27238fc1" => :high_sierra
     sha256 "9d0b6368e6fa4e2b4fb618c7ba3893a5b3b47471b366305026ee75b44d6ce91e" => :sierra
