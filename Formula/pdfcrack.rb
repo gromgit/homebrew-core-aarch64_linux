8d22f3a7f29da1a5efb46b777f101a43bd3f3d06
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-10 17:40:27 +0000

    pdfcrack: update 0.18 bottle.

--- Formula/pdfcrack.rb
+++ Formula/pdfcrack.rb
@@ -6,6 +6,7 @@ class Pdfcrack < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a70ee2e224ba6650265a93fa0cdef209baf3721f76c4f00a0c8edf497426d588" => :catalina
     sha256 "02521002ff10a9cd937464143ca29065288c3a3e6f1f27c0ef4e663af5ce8a73" => :mojave
     sha256 "516cf147a20751bf6c76871fcb471fbab770949b18e3f417bf97ba3a4ce44207" => :high_sierra
     sha256 "0eab853d75f86a085fcf3f93681d795dab857012701fe85ddbe598b9e12dbfbd" => :sierra
