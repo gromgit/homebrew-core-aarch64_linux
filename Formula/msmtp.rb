7387c085812df41238e8d1efca91a2c40d55d466
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-10 17:44:54 +0000

    msmtp: update 1.8.5 bottle.

--- Formula/msmtp.rb
+++ Formula/msmtp.rb
@@ -5,10 +5,10 @@ class Msmtp < Formula
   sha256 "1613daced9c47b8c028224fc076799c2a4d72923e242be4e9e5c984cbbbb9f39"
 
   bottle do
-    cellar :any
-    sha256 "b565cc6011abc3d4491ce6e35362dd97297bcce3d6694cba7bc20bcc756d6738" => :mojave
-    sha256 "cd32f619d3b85794bc62b2374ca2ee9874014c2d31b609f34af437311ad2f028" => :high_sierra
-    sha256 "a2008bd10e0465b0a165810000c18bf1a8a25d3de154ed2fbe0206b70ce5fb1f" => :sierra
+    rebuild 1
+    sha256 "99970d911f8d68c08d77a191880f18f20b79a525881925cb236eebd5ecdf3917" => :catalina
+    sha256 "7fb3d22477c6b338159bf8fa5b21c764e7add5e941b03fbbe293c08d3413b13e" => :mojave
+    sha256 "56d46b248e6e5636ddea99f7651ba0b8fe20b557a82ddf2610c7800b5f2e371e" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
