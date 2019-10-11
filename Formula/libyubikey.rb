a3c321f070d3e486cb2a982627810760cd85c0ad
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 00:39:35 +0000

    libyubikey: update 1.13 bottle.

--- Formula/libyubikey.rb
+++ Formula/libyubikey.rb
@@ -6,6 +6,7 @@ class Libyubikey < Formula
 
   bottle do
     cellar :any
+    sha256 "b6fccb68ae85837533ea4680063cc64f207f2d6926c4eafaf23e81f0b790fc55" => :catalina
     sha256 "f5f99ad5056fe1d8bfa69a389983ac9ae0f5e65c60d984de4fb9591b6b19daba" => :mojave
     sha256 "8440f766e153b537a092f55a07990c0fd28e0b244407bf6824d21fedb3d97f32" => :high_sierra
     sha256 "23f550d2f6e2cd6310756e3625c17868e206c90029e241fbc915a408f4761263" => :sierra
