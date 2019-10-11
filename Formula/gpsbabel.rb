fc115e8139a622cbf687c5a0cdda64efe464c0d7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 15:25:36 +0000

    gpsbabel: update 1.6.0 bottle.

--- Formula/gpsbabel.rb
+++ Formula/gpsbabel.rb
@@ -5,6 +5,7 @@ class Gpsbabel < Formula
   sha256 "ad56796f725dcdb7f52d9a9509d4922f11198c382fe10fc2d6c9efa8159f2090"
 
   bottle do
+    sha256 "685947e84880f27a16a442c10456990f9ede1efa8adf723fa217ba7ac5123ff7" => :catalina
     sha256 "0d5fa17f760e4ff0ebf88bf4b461c1fba6498278edd57ab77caee7576f5c4609" => :mojave
     sha256 "e147b5217a57fdf32a8073f53718e6423f227e967f9d495cb3a0bc38b5e2ad3a" => :high_sierra
     sha256 "e982a298816049c9094762699799f238cfc8d7804cf5d72f6816ebd0e8aa414e" => :sierra
