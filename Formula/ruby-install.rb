90d7c07015a573cdccf0ca09050afb75da3c0828
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 16:59:20 +0000

    ruby-install: update 0.7.0 bottle.

--- Formula/ruby-install.rb
+++ Formula/ruby-install.rb
@@ -7,6 +7,7 @@ class RubyInstall < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8141c3baa93dedadbdeba6d02a0d00ad13493ba052d013807a40ac048617c6da" => :catalina
     sha256 "df0319ebf05c4b0700f16b0bd3150f53d3034a4b664d8d10c3f190af266cdbe4" => :mojave
     sha256 "0e88bbe463d29a1e1cc0a91816023346a860a8a08c3daff660c24b313d2b4511" => :high_sierra
     sha256 "0e88bbe463d29a1e1cc0a91816023346a860a8a08c3daff660c24b313d2b4511" => :sierra
