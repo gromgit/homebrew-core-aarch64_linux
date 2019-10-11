7dc21b1b704a4a0a5b9d24eb959061a01ac99a45
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 22:22:07 +0000

    hiredis: update 0.14.0 bottle.

--- Formula/hiredis.rb
+++ Formula/hiredis.rb
@@ -7,6 +7,7 @@ class Hiredis < Formula
 
   bottle do
     cellar :any
+    sha256 "e19f4ea25971bd484ecbcf1bfd6a1556cf99dff7619f4312cef0fb1caf5b656a" => :catalina
     sha256 "b4ad73060273cc312df323beb4657426ed554b6fa436381e68c5b37fc64dd471" => :mojave
     sha256 "f4ac0116806f6a754175e1ceadca0ddafaf1d70fdd5bc0af0604cb82483040e3" => :high_sierra
     sha256 "df1316285bbf8325a8e9dcc2f8cd472905354cba2b1562cf0dd75e268b596f5c" => :sierra
