09dad7a4b6599edbcbce921cd0574829d922fad1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 01:16:22 +0000

    xpa: update 2.1.19 bottle.

--- Formula/xpa.rb
+++ Formula/xpa.rb
@@ -6,6 +6,7 @@ class Xpa < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "bb91619a16950257bd0d38e4a31efb591d28f49d7f8abaa258f2320b85f66839" => :catalina
     sha256 "5472c2f6a21a42fef5af1719fcc7948277950e740c6dbabf0046218748c63387" => :mojave
     sha256 "c0dc2dc017c17f0add29646c011602af1ce4ac208cf03b80674f2fa480f89ac9" => :high_sierra
     sha256 "40cf0e2dc27c94926ab77537d28efad269499956a08b8a7792c05958adfe77b0" => :sierra
