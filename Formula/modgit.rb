e55ac220e617052f67b21d8088d9f85f51b9fd60
Baptiste Fontaine <b@ptistefontaine.fr>
2019-10-11 12:47:18 +0200

    modgit 1.1.0 (#45174)

--- Formula/modgit.rb
+++ Formula/modgit.rb
@@ -1,8 +1,8 @@
 class Modgit < Formula
   desc "Tool for git repo deploy with filters. Used for magento development"
   homepage "https://github.com/jreinke/modgit"
-  url "https://github.com/jreinke/modgit/archive/v1.0.1.tar.gz"
-  sha256 "3df1ccddaacf386a32be0fe5b6333438f17876bc6a4851d9bc22a68a73ae314d"
+  url "https://github.com/jreinke/modgit/archive/v1.1.0.tar.gz"
+  sha256 "9d279c370eee29f54017ca20cf543efda87534bd6a584e7c0f489bbf931dccb8"
 
   bottle :unneeded
 
