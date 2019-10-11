9a9cca66bd9441413d333749208eeeca2866b5f0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 09:34:37 +0000

    roll: update 2.5.0 bottle.

--- Formula/roll.rb
+++ Formula/roll.rb
@@ -6,6 +6,7 @@ class Roll < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8cda95b1da0131870732c0baf08750b50d492c42918ae14adaf7c1b4553a8419" => :catalina
     sha256 "c1c2251d5ac3b6a03065877f6c211f326909c7710f85441adf0d9987815b6c35" => :mojave
     sha256 "0fe1da205f9d37a29489ac8b437cefcd915041c47ddf95cae577369b46d8d7ea" => :high_sierra
     sha256 "f9c7a833dad6d95fe4ee83fccf0890f49dee962e9965a624cf5d483d86c72eab" => :sierra
