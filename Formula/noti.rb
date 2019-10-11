3fb0b06ec98fc333b01d3a3148fbd0763ae398d1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 13:43:35 +0000

    noti: update 3.3.0 bottle.

--- Formula/noti.rb
+++ Formula/noti.rb
@@ -7,6 +7,7 @@ class Noti < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "55373807cc24bc00f1e26855c43cb2d00ea1bfa993c4f61a4368210990a1d5b0" => :catalina
     sha256 "483119c4bf2865ddc52208f33eeb3e8db9e4ff0e5901baf0e441d31492b2a5ff" => :mojave
     sha256 "44d31ac34449acb98d99c65403c6515f4f66f88cb0f0e9f4bed41f31b47107d3" => :high_sierra
     sha256 "2684f0630a200aac31d9ef08f757b431a956534d40776d80599b1a02d80d6ec8" => :sierra
