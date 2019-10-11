560f9ae7b8b2f3caa94c1c5a5755c5fa98f76636
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 19:20:05 +0000

    openh264: update 2.0.0 bottle.

--- Formula/openh264.rb
+++ Formula/openh264.rb
@@ -7,6 +7,7 @@ class Openh264 < Formula
 
   bottle do
     cellar :any
+    sha256 "b9bf4acbd1f71511cfdd34b9a1f50cc4d67078b5cbe88ba831e4cb4c5c9a4005" => :catalina
     sha256 "0a110d3d44c184d8019f5442296a005d5d9d2415c11df117a6a8e526f514039f" => :mojave
     sha256 "dc02b0c4aa5f4b777e982e71ab244e583de8dc811b99c342806325ef87ee533a" => :high_sierra
     sha256 "95b82a01da74048615c89b8d99598536d84f3704edcd132d70fca60c2900b572" => :sierra
