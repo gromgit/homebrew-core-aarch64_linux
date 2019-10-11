dd5aa7973c0bb1499d160d44a464b132dfa0bf72
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 19:20:05 +0000

    potrace: update 1.15 bottle.

--- Formula/potrace.rb
+++ Formula/potrace.rb
@@ -6,6 +6,7 @@ class Potrace < Formula
 
   bottle do
     cellar :any
+    sha256 "464057e611c3715cad4698b2acfd3a1cd809dd3fceb172b3003ccbb9c16309c0" => :catalina
     sha256 "3d25cc97a832eb016be11e133d0196308b0b3dd21b2f62302215ba566ee77399" => :mojave
     sha256 "f98e3daaed06c6296d446c53087b82a7e100eb3407d41075e706e66327c26f95" => :high_sierra
     sha256 "b71fa6c507b43df9e48dc0259e5dfb803a13c9728ffd6978b9798d03da363e0d" => :sierra
