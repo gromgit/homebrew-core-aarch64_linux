a79ee980ca493798c04be59430c3a5899ae38101
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 01:06:03 +0000

    lsd: update 0.16.0 bottle.

--- Formula/lsd.rb
+++ Formula/lsd.rb
@@ -6,6 +6,7 @@ class Lsd < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4107ab0f07cb55f9a27393f31917deae3c144c82a106ee62672398e8ab405688" => :catalina
     sha256 "9cf1be00bfe0f3f7d0142dd1d1f4118be58415f7a8444ff194a5f919a3dd03c9" => :mojave
     sha256 "1017f694172dec6120c2d49277268ee21f417a363c55a880541c1119bd82fc4e" => :high_sierra
     sha256 "93be9faa9b2a58a509f7d53a78ce1950bc3a770a13cffdc0980a329b97f14a5f" => :sierra
