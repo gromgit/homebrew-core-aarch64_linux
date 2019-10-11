c030d34fe628f5d7c46516a45ea519a77a483c88
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 12:08:09 +0000

    mpfi: update 1.5.3 bottle.

--- Formula/mpfi.rb
+++ Formula/mpfi.rb
@@ -7,6 +7,7 @@ class Mpfi < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "950fb479ad3748345f0410a7ce02d70527d9757d0c20ea1ed73d8f3f4e1c512c" => :catalina
     sha256 "55d8819c0310e6b8bc66742f7ab5881b9b552a9c60eaf940595ed08e8a320a56" => :mojave
     sha256 "d4464bdbbb2861861fa92e471f75e1b658e7c5f5814028a6c57f74c76092b013" => :high_sierra
     sha256 "50d3b78c1ef6837198a0320dbbe0852ad524f83bc2e12460bfbdc188bd1da76a" => :sierra
