d387cfd50ac292ef3c1901523d089f797622171b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 15:51:45 +0000

    pacvim: update 1.1.1 bottle.

--- Formula/pacvim.rb
+++ Formula/pacvim.rb
@@ -6,12 +6,11 @@ class Pacvim < Formula
   head "https://github.com/jmoon018/PacVim.git"
 
   bottle do
-    sha256 "adb164c1decfda7414827e3c7932f6b49c055ef093618dfc6f9381f118df2e73" => :mojave
-    sha256 "ce8a3d33ea044109ca9d32e486a8373e732dfea439bc68169335a260c5947659" => :high_sierra
-    sha256 "7cdbfca5fe0209e68f3f79e7949a6e60c96435c3a8da68e59036142d54a706dc" => :sierra
-    sha256 "91b0806e76c36ddb814f6a54cac2722b21784f9be1604aee45e7091099172f8a" => :el_capitan
-    sha256 "36b138084ac97f4eb28bc6b7068e19dc28cc478459fc59de12e8e849820ac65d" => :yosemite
-    sha256 "1f5e908420b40f2af868727b7065a51ebc39525c241b983f7f9b9449b95fdc1c" => :mavericks
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 "85bd0087ecc54716772881e46ce00553ee037eb2ea200d34d5db28709092369f" => :catalina
+    sha256 "e2ecd6cc1337adb4c9e760c50a83ae04a8cb86495d3c1ea167bfa5930d7a16a0" => :mojave
+    sha256 "b8ef8cdba34802db97fba770e013393973e908e11486b87a4f5189f139e468dc" => :high_sierra
   end
 
   def install
