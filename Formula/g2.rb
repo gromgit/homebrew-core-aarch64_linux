a96dc69d905adb69e0f01a1882e9fe554a8bb952
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 07:55:17 +0000

    g2: update 1.1 bottle.

--- Formula/g2.rb
+++ Formula/g2.rb
@@ -7,6 +7,7 @@ class G2 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e9350d1a2278bc954807b784dd6f34044ba69bcbeef3c91410f4bac9affdc0ca" => :catalina
     sha256 "64107e7e395a373205f8e4953082f90e86736b27c01198836ea3915a40362dc5" => :mojave
     sha256 "172bb101cd40ab61ea7a6ce85798556da5bbb980ae050023e8e8ff9f4d2e2c52" => :high_sierra
     sha256 "6bd5de5c1e1335c1be168bf5eec800c8ac5d0b4d16534a7e686d9c4e8d396417" => :sierra
