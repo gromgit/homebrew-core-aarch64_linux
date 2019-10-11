6fbc47ec92563ff2a46ed717f57edfd2241f41f2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 14:29:55 +0000

    two-lame: update 0.3.13 bottle.

--- Formula/two-lame.rb
+++ Formula/two-lame.rb
@@ -9,6 +9,7 @@ class TwoLame < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "98a0ad3cf35a49fa67c88f0e0fc5cd129abcc2ee1083576d1a181340cecbed1d" => :catalina
     sha256 "3e1206ffe2663c75cc8def2832a0b36374bd1c548f234e178462219536abf539" => :mojave
     sha256 "34e26fc01f8c195e0b410bf3756cb283d86417f1b1d4ec0ac799441976601f78" => :high_sierra
     sha256 "f1138207ebf9a6e1a95ebd553b3f7a8c91ba7546d812c313fa1f3beac8d593c9" => :sierra
