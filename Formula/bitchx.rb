ecb3294978881438ef607fdf4a3d53535a629174
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 16:35:48 +0000

    bitchx: update 1.2.1_1 bottle.

--- Formula/bitchx.rb
+++ Formula/bitchx.rb
@@ -6,6 +6,7 @@ class Bitchx < Formula
   revision 1
 
   bottle do
+    sha256 "9e24f64d188be8be36054aad67ead05bffd2f1b5a7c6bef6bc9f98f7ea92fb87" => :catalina
     sha256 "52939d589b5697402b6b5c658ab065651ac1943e8c7c7c9798aca5f76790be00" => :mojave
     sha256 "0a021e6d01b7f7d4ee9d048459ab7367b48da791896b2edeb96e270b196ff202" => :high_sierra
     sha256 "0c9e7fcf39a8fb0c80f867495cf1d6776fbe4aec6010a1986edbca820ed7a6f0" => :sierra
