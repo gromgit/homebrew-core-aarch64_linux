87903132c0ab8026812a2200b9cf4942f8fe4fa5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 16:20:49 +0000

    rmlint: update 2.9.0 bottle.

--- Formula/rmlint.rb
+++ Formula/rmlint.rb
@@ -6,6 +6,7 @@ class Rmlint < Formula
 
   bottle do
     cellar :any
+    sha256 "ed346f576ec02c98e54c337e0370bb3b238abc0ccb90c84cd7d302ddd0d3713d" => :catalina
     sha256 "155767f2b4bc9d03e965f56e2dd6b9b8bd2cde55fb6fb553ce05a00fad35bd2a" => :mojave
     sha256 "9a05ea7f81263a7e2c0c6f2770b80ca942c92887906741dc3b8faba1d7a41d10" => :high_sierra
     sha256 "3c0e9d07bd06e4e9785dc54ccee7a7d0cdd2a95b2fe6a7ff0e12f66e1bbc0a6d" => :sierra
