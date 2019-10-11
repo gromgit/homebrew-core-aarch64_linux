cb0e40a0acc622bf68d1f19b176547674190e777
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 22:07:42 +0000

    ios-sim: update 8.0.2 bottle.

--- Formula/ios-sim.rb
+++ Formula/ios-sim.rb
@@ -9,6 +9,7 @@ class IosSim < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6d584211ecf3afe536ca2cf5f0fc14a46a7303b9ab664bdcc138f84731c392ae" => :catalina
     sha256 "57eba6f175bb19ea53f53142345d6a729ff2494bf5e986182e19cd3228c13683" => :mojave
     sha256 "fb1aef7e85f401660584629d09499e3c58b788b11313dbf68fe0840ee372e20e" => :high_sierra
     sha256 "03387aef5b0f1f52d398971fd6189324f52f73b21bd4d1c378d516a50a329dce" => :sierra
