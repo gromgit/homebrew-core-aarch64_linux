f0a93674e4d0307c59c19f4c559ac283327a40de
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 16:35:49 +0000

    mozjpeg: update 3.3.1_1 bottle.

--- Formula/mozjpeg.rb
+++ Formula/mozjpeg.rb
@@ -7,6 +7,7 @@ class Mozjpeg < Formula
 
   bottle do
     cellar :any
+    sha256 "ef7d5b06f03e05a4babab7b464bb83475a9a3fedbaa78e1ceedf23f051828e4c" => :catalina
     sha256 "8f84ae70b36cba861c17bc0b7f3befa2e3d30fc807cadd70ed7af0521de325cb" => :mojave
     sha256 "36faaf1c7c6baaeaed402dc9239b594222e8bce4c8b7b8468d13e7d084c06582" => :high_sierra
     sha256 "1108870dce024757e600488eb3122994675ca81b4ed20fefd32a622d3ff6a2e8" => :sierra
