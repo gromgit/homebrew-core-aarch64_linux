7b9f01e485e01912556915d913b1234f2d584966
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 06:50:59 +0000

    bsdconv: update 11.5 bottle.

--- Formula/bsdconv.rb
+++ Formula/bsdconv.rb
@@ -6,6 +6,7 @@ class Bsdconv < Formula
   head "https://github.com/buganini/bsdconv.git"
 
   bottle do
+    sha256 "ea7dbfdeede5b0144d00533798f4b76a4af216b12c37a5e5ecaeaac030eab507" => :catalina
     sha256 "c4133e2b55be3a74a241941b4ee3bf4a1a9cacbbe0e5a41827b1229698d79215" => :mojave
     sha256 "ddbd2053b0cff3e0d5c635b370138d5e4de8f5353621dbcabe616f014f3362c1" => :high_sierra
     sha256 "9afd5dd285b98bf656bbd0e29fa3594128a312e525e079e4348b1a706e16b16a" => :sierra
