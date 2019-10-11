ef5a020c8d1dc1dc7e753b156a473f1ceea9dba9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 05:36:24 +0000

    gifski: update 0.9.2 bottle.

--- Formula/gifski.rb
+++ Formula/gifski.rb
@@ -6,10 +6,9 @@ class Gifski < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "099235da89897480463d7728d0c2a9bf88413ce7212dc68badb6d417f70412e4" => :catalina
-    sha256 "c259af696f34802bde2f7376506cf2b2ff1f0ed8781cd8f9b723c2066324c8da" => :mojave
-    sha256 "d4bc8326504cf4703b7bc548c3534cac6e3f2d8741f8f9c6355206522984b8f7" => :high_sierra
-    sha256 "7dd6250d1cbb19abd7b281f345979ead819f7db5f208cdb2ef70254c2cc9445e" => :sierra
+    sha256 "0d8357f6ae4d8649a6cf39cd22958cd977c804a5dad275db0ca610de5b1f85b6" => :catalina
+    sha256 "f79804792744ae8dfffd5a64164bf54141fff6a9014a5eeff321b209ed3ccae4" => :mojave
+    sha256 "7922b291a3e8d0d962de2344c794fdc14f084774099ab1a3c082f8ddf9d2d2f8" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
