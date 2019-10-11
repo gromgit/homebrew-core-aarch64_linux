ab19cee54306a8a3c4e3448528700d3daa278581
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 22:50:26 +0000

    libzt: update 1.3.1 bottle.

--- Formula/libzt.rb
+++ Formula/libzt.rb
@@ -8,6 +8,7 @@ class Libzt < Formula
 
   bottle do
     cellar :any
+    sha256 "81e6e2709780349eb9d3c654e16a2dfa9b5f8f0e96e508c6ddc4d9e001df435d" => :catalina
     sha256 "1e677336f94b2d33bfc8050c571bd65b5091b2072ef17730831136948a8fb528" => :mojave
     sha256 "065371f5dc43376107afc6aa6e055ffd8c3d3bf22e034152205be5dcc30e527b" => :high_sierra
     sha256 "8fdc6bb393c91cc1807a1ac4e5758369b41716bdda353de87d7914806a28a64f" => :sierra
