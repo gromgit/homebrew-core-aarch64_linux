83558443fc4a3101891dac75f5024c215ebd210a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 07:41:44 +0000

    switch-lan-play: update 0.1.0 bottle.

--- Formula/switch-lan-play.rb
+++ Formula/switch-lan-play.rb
@@ -7,6 +7,7 @@ class SwitchLanPlay < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a04c4d8d196c9777c5cf03fab67cb5dc94b5f2890396ed38559ff1147182fab6" => :catalina
     sha256 "be0f9d8661c0c8ce72abcc42dcd67a2e34bcd23fdd3cee01bf2a7f60690d2146" => :mojave
     sha256 "aed551064a4fca5c96d1059020d0cde04c49f72d5b90ee29280137e8f26f93a7" => :high_sierra
     sha256 "fa6f336a8bd322bfb4bfe9bf47f0705fdf37658719eb3c03252e5532ed71c554" => :sierra
