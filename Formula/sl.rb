33a69130ea6476b692abdc7b7472407dae44ee99
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 16:26:55 +0000

    sl: update 5.02 bottle.

--- Formula/sl.rb
+++ Formula/sl.rb
@@ -7,6 +7,7 @@ class Sl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "31b8e67d984635b74aec3a5b47b6145789ed9c09d065751cac862eec1386502d" => :catalina
     sha256 "e489648bcc7eff8f065855dcc891eb55f3793a5ff464d96726e313a1bc74d00f" => :mojave
     sha256 "627b0b5f8027f876466d03038da7dd0d75804cccc3bbcf45f0fe9c91199be3c3" => :high_sierra
     sha256 "afd30cb3a99d238a8ac52810834244d5f47fc2ff597db9ad61012bd2014395b9" => :sierra
