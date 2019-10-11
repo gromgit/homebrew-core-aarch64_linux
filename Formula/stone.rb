d7c3e437e39d695d207cd138816f56820b4fa187
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 09:45:57 +0000

    stone: update 2.3e bottle.

--- Formula/stone.rb
+++ Formula/stone.rb
@@ -6,6 +6,7 @@ class Stone < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8b37777addb031114cadd5d09ca216ffaff9df2316073077c79c072d9debc761" => :catalina
     sha256 "0d2db17e57c53f2be5b1b8feea072923bddd3c86efe37c9a8db6296087ee5687" => :mojave
     sha256 "579a9dee4b6fb57f0f1313a656250b00a428bdff7c2401431bb96d0ef0496c25" => :high_sierra
     sha256 "540bd64b2264bfe03d88ad620a7138a1d96742d6810eec301fd7e5ea63970ee9" => :sierra
