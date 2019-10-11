07380f4eda8618255d3990ad993d2994b5bd8c21
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 21:56:45 +0000

    libzzip: update 0.13.69 bottle.

--- Formula/libzzip.rb
+++ Formula/libzzip.rb
@@ -7,6 +7,7 @@ class Libzzip < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "aab2e4b218f21888c92b055fff7d801ea4511b4d133517cf4d5696bea0fb54e7" => :catalina
     sha256 "2e293f90e2ebee0734ff9bb6a23cdcd562383d87e801de996f57296aef3a15b4" => :mojave
     sha256 "7ae8222e9b3f3d56639d19de2666eb1dffb6399d5985a64f52a24cdbe3763b58" => :high_sierra
     sha256 "72c6927e722159e240f313b0bbc5dfd7648b340fd7a9c732d99e9eeaac6d4945" => :sierra
