cc083e53d45beda9114a39cf371ba1e4f75937e6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 06:31:08 +0000

    testdisk: update 7.1 bottle.

--- Formula/testdisk.rb
+++ Formula/testdisk.rb
@@ -6,6 +6,7 @@ class Testdisk < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "66c4088c77794a244fd5b38fa39216eb8d6a09b9e4efd5e68a249e9b5df65606" => :catalina
     sha256 "1e77fbc276d986fcf378901b2ba0d5957f17b569e512980017ecd09926505a4a" => :mojave
     sha256 "8cd43adea2ddf632e5c9305609cf377b47fcf5836805075d06dd3ccd2142ccc6" => :high_sierra
     sha256 "752a686f8fa7717cbbdef064eefd80503eccdddfc587bd48fd24256e23332470" => :sierra
