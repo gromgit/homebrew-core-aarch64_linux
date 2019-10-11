97952f1454bcee39a9913cfb9d481331076f1284
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 15:50:18 +0000

    tnftp: update 20070806 bottle.

--- Formula/tnftp.rb
+++ Formula/tnftp.rb
@@ -7,6 +7,7 @@ class Tnftp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d64ad33eea88bd1b7f061c0eec784d259183d60376d0e5e4dbf3d07aef49ff74" => :catalina
     sha256 "286b8f6bfb0217e88c81305e28eb32aaa78c3d0528518372191c8364c080f351" => :mojave
     sha256 "d10b32070661a883375a361016f73c3be47f9702be5fa902ca491d3f12ed8022" => :high_sierra
     sha256 "8aca7a23ac918f7a69b13df67452420fb711e320cc57743cefd15134516da1ab" => :sierra
