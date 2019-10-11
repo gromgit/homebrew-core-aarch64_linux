deea3f72c8fe8ac564c9a3be3a578778302dbe24
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 08:11:31 +0000

    iprint: update 1.3-9 bottle.

--- Formula/iprint.rb
+++ Formula/iprint.rb
@@ -8,6 +8,7 @@ class Iprint < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "e5fba1fa985ad96aac02d36f50e0985c14248655fd810c15c053e1ff7d5a1981" => :catalina
     sha256 "8b1752455e0ff26b804070e3eb710493342fc2b2897a132a26433f4cabf5ec17" => :mojave
     sha256 "c71f0b21d59a21fdc1e86e0a2016f79d862e838eb0fb7c92c50ed56e8aa1a163" => :high_sierra
     sha256 "3fc40e5d2ee26c7b8709bf61e651ec3506561b98fcbf6ca52b8d353dd4be356d" => :sierra
