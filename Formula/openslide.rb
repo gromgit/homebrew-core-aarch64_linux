97eb046ba94a0e09d17c533841970c5e3351886e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 00:45:30 +0000

    openslide: update 3.4.1_5 bottle.

--- Formula/openslide.rb
+++ Formula/openslide.rb
@@ -7,6 +7,7 @@ class Openslide < Formula
 
   bottle do
     cellar :any
+    sha256 "6b59207518417bb5a45a716d6c26a01ed8d9977af51992b00d0479a7d9a4ffae" => :catalina
     sha256 "d90e3ee5514064389cea2bdf1d4369cc2be4e1d965ac9d56f47e0c6e22f310af" => :mojave
     sha256 "1d70f22fad80e061bcfa5d4955d522d37bd077c51cad4697579a104759233ad2" => :high_sierra
     sha256 "de34071d033c87c731be7954d7c0ced87ddf086100c29fea07410d68621b9929" => :sierra
