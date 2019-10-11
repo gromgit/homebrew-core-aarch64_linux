30a82f2739a360661e7f7438b89a1486794ce73e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 01:22:19 +0000

    libwandevent: update 3.0.2 bottle.

--- Formula/libwandevent.rb
+++ Formula/libwandevent.rb
@@ -6,6 +6,7 @@ class Libwandevent < Formula
 
   bottle do
     cellar :any
+    sha256 "f175ecabb18921593ddd08bbd0b2aaa5e0a24c85d2964be230cd3a1f0ede22ee" => :catalina
     sha256 "1e1db4ade4de58ab9ca1f0545d91537b935b65e062d99737c288dd059a17da8e" => :mojave
     sha256 "7593e96a9e76e4b67fa3851a3491c8d7cbd458ad53ff65b3a34b64e2f697e75b" => :high_sierra
     sha256 "e4b00ade9387b8fdccf72bbe9edd0e334c69f23597f85dd1e6da02088703c286" => :sierra
