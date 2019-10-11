6265967789ce31031d032e027d0060a3d54067e1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 22:50:23 +0000

    libmpd: update 11.8.17_1 bottle.

--- Formula/libmpd.rb
+++ Formula/libmpd.rb
@@ -7,6 +7,7 @@ class Libmpd < Formula
 
   bottle do
     cellar :any
+    sha256 "a89b23f581da1a00a6c9cd077c854bb6b7f1c818664630cec1ed8f0b6f543a32" => :catalina
     sha256 "9a7f7829ec1d79442d3dade12c338b42a0f248b35aa25475b512f0b70171d8db" => :mojave
     sha256 "2d8f1fae6ecc3ab4b440531ae13a2db5bc82282a89f2670a986cc6136da16068" => :high_sierra
     sha256 "8518a3880db71a27a414e8e2ae020afec29afbb777694389cd57d983ec1904a5" => :sierra
