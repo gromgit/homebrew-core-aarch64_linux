71bffe0b82d35988a7689a240282b4b2300e30ef
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 21:56:45 +0000

    hashpump: update 1.2.0_3 bottle.

--- Formula/hashpump.rb
+++ Formula/hashpump.rb
@@ -7,6 +7,7 @@ class Hashpump < Formula
 
   bottle do
     cellar :any
+    sha256 "5137114b347ef2c2cd42ce982c0b8d6fa261d6b7105b4735a6e8e8c5c6154448" => :catalina
     sha256 "ddd00b7f11649d0ad36bdc9bfa724daee15e135687a1d71c5f043aa758b15399" => :mojave
     sha256 "dd22c32a8a2c4ade6b45e573b61d09d91f56c60a74c7fd9265fb75e8dc60be5f" => :high_sierra
     sha256 "91dd089a608636170ad9ba63f5095f254773d510e0eaf48735aa35c3cf6d9bf2" => :sierra
