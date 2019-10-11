9c79d03c1af1e3a275042045b10e8f9fed1c8c2f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 14:22:30 +0000

    ncftp: update 3.2.6 bottle.

--- Formula/ncftp.rb
+++ Formula/ncftp.rb
@@ -6,6 +6,7 @@ class Ncftp < Formula
   sha256 "129e5954850290da98af012559e6743de193de0012e972ff939df9b604f81c23"
 
   bottle do
+    sha256 "4082ca1bf2427d1780e0ebcf96b1d90d78630544a318ef94808ba003bfb49f47" => :catalina
     sha256 "3f7f108352b84b36c6d2174e4cc71c9b5b3632ac79b2aa8293205ea322541ba0" => :mojave
     sha256 "bd53fba3c13ba333f8e22ce0adc67a9ee3fa0d95e571f9833f4928d0adb0ee30" => :high_sierra
     sha256 "25caf7d9c7ac3c1642d3d205fcedfbea05878798033c0ff82cc3b5fbab4674d5" => :sierra
