4f18f87bec5a4180c8afe7ab16f7a46ea3e2f55c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 16:08:48 +0000

    protoc-gen-go: update 1.3.2 bottle.

--- Formula/protoc-gen-go.rb
+++ Formula/protoc-gen-go.rb
@@ -7,6 +7,7 @@ class ProtocGenGo < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "777f058de704ae1cc116c537ac00b868401818ecfcbf505993fcfc325853dd40" => :catalina
     sha256 "203a532a500411acd36873816c788b439fbd2077b4253eb1975a6da7045ec08c" => :mojave
     sha256 "48f9810dde13eda31b003eda0b2c409572fde48ef33ffd0fcb9e8979416d4c69" => :high_sierra
     sha256 "c66cc2bd02f7dd004d3880b495b598d578382caf583fea04029c39e0773e0f0b" => :sierra
