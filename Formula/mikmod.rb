ef876bb72e964fe982caaebfccf21edca392bb32
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 08:47:31 +0000

    mikmod: update 3.2.8 bottle.

--- Formula/mikmod.rb
+++ Formula/mikmod.rb
@@ -5,6 +5,7 @@ class Mikmod < Formula
   sha256 "dbb01bc36797ce25ffcab2b3bf625537b85b42534344e1808236ca612fbaa4cc"
 
   bottle do
+    sha256 "6d6794da1daf749a56cf55738f796fe5b6a7b337456730b21a5efba2fab60f38" => :catalina
     sha256 "6812f223d67d763208eaf21ab6e1ebfaf50e349852cb6820010010ed0524b2f2" => :mojave
     sha256 "5907f92b40ddc0ba15cddd60269a9f9a8e9fcf6295a099df4145818536431427" => :high_sierra
     sha256 "a9586a9306006e8fd451aecb6c3259fc57cb0bb328a2b0ce8c064e5518f943bc" => :sierra
