a7cff32c0f18d0025d1be6aaaafea811b635ff43
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 11:19:28 +0000

    mm-common: update 0.9.12 bottle.

--- Formula/mm-common.rb
+++ Formula/mm-common.rb
@@ -6,6 +6,7 @@ class MmCommon < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5894c91c768f38b87d9d69bf3afb5bc1e8675e07de0b4afda506865f5c7cd35a" => :catalina
     sha256 "cffdd1590271cec32a56b7b0a54f92e1b657e5b26ec7c62c8c18b8b49364f72d" => :mojave
     sha256 "42c9654bebbc472d90bc31d14e0832d55367d8d86d6750ab546a129a48de342b" => :high_sierra
     sha256 "42c9654bebbc472d90bc31d14e0832d55367d8d86d6750ab546a129a48de342b" => :sierra
