0dc723a9ce22ab467fe3c356c967598672305b25
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 14:09:40 +0000

    ode: update 0.16 bottle.

--- Formula/ode.rb
+++ Formula/ode.rb
@@ -7,6 +7,7 @@ class Ode < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e5b71e7f775b1d5d17e80cda8d174e72cd8b0b99c75bc266f198abd8a42bb134" => :catalina
     sha256 "ea4f04cfd28205dfa4215c96342046a9b89e7c13731159aecd5788a791d07ec2" => :mojave
     sha256 "eb5e88678caee617bee029a5d90ec2e2344b1d4ff1ac28d4953549e9dd4f6dcf" => :high_sierra
     sha256 "8a5ab1cb46e5454379dcbc090d012b42cdc0b3dba9b81475af7fda8abc7b93a0" => :sierra
