0de0c5d9823bdd5f19b65a007dcbe482696d5828
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 21:42:09 +0000

    docutils: update 0.15 bottle.

--- Formula/docutils.rb
+++ Formula/docutils.rb
@@ -8,6 +8,7 @@ class Docutils < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3230d98f9912c462a2b6dd1aa64494fa26037eec4c1975a75970361de6243e79" => :catalina
     sha256 "c655dd3f311370b4c6683236b6ead52d800397e050584124a54709effa556746" => :mojave
     sha256 "7fc8102f9f46d9f3bc4debe405fa6b533809aa73da074d0379652971a30fbd93" => :high_sierra
     sha256 "6393cc30ff4cef96e4309fc912267fd9649396fc31a471b15af6363eb1cedf95" => :sierra
