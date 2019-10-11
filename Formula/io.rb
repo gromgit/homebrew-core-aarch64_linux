cae743845ab8c2a2a354641dea61be4195061c8b
Jason Staten <code@jxs.me>
2019-10-11 13:52:36 -0600

    io 2017.09.06 revision 1
    Updated to the new community io repository at
    https://github.com/IoLanguage/io
    
    Also, fixed building `--HEAD` by conditionally doing the `inreplace`
    only when not head. `add_subdirectory` was removed from the
    CMakeLists.txt on master.
    
    Closes #45209.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/io.rb
+++ Formula/io.rb
@@ -1,18 +1,10 @@
 class Io < Formula
   desc "Small prototype-based programming language"
   homepage "http://iolanguage.com/"
-  url "https://github.com/stevedekorte/io/archive/2017.09.06.tar.gz"
+  url "https://github.com/IoLanguage/io/archive/2017.09.06.tar.gz"
   sha256 "9ac5cd94bbca65c989cd254be58a3a716f4e4f16480f0dc81070457aa353c217"
-  head "https://github.com/stevedekorte/io.git"
-
-  bottle do
-    sha256 "1dc9783722f9ea256f7f0dcb57032dee40692a78a31707eca025b52c8857772c" => :catalina
-    sha256 "9e628fa0879d7d2e370ae5275393d1d52b578f6f10d3f005faf9d0360caf8851" => :mojave
-    sha256 "686d5d23790b53c27765d49da0a74ec96ee949353b31646a0a92ee931270a23d" => :high_sierra
-    sha256 "2d0e05344917ad3a1d322f2860030013315ceb7e8ae962cf6070d1ee8cc395d4" => :sierra
-    sha256 "3a5a0e9a1ec0ce7f4bc6bcfc5fb8c782f0b1ba0451251aaab51a796452b59e67" => :el_capitan
-    sha256 "16d31a7062e2c7ebab815bcd48b03aab9597a6c40071cb407e2bc6dec91fef0b" => :yosemite
-  end
+  revision 1
+  head "https://github.com/IoLanguage/io.git"
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
@@ -24,9 +16,11 @@ class Io < Formula
     # FSF GCC needs this to build the ObjC bridge
     ENV.append_to_cflags "-fobjc-exceptions"
 
-    # Turn off all add-ons in main cmake file
-    inreplace "CMakeLists.txt", "add_subdirectory(addons)",
-                                "#add_subdirectory(addons)"
+    unless build.head?
+      # Turn off all add-ons in main cmake file
+      inreplace "CMakeLists.txt", "add_subdirectory(addons)",
+                                  "#add_subdirectory(addons)"
+    end
 
     mkdir "buildroot" do
       system "cmake", "..", "-DCMAKE_DISABLE_FIND_PACKAGE_ODE=ON",
