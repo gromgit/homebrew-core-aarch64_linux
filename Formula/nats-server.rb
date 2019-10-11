26396b7831e81c26b0dffde296af909d04c8eb70
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 22:07:42 +0000

    nats-server: update 2.1.0 bottle.

--- Formula/nats-server.rb
+++ Formula/nats-server.rb
@@ -7,6 +7,7 @@ class NatsServer < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c10d4e8addeb1dd4ff708bfb7571d6fb205aa6babc71c362cb504204f75f43ba" => :catalina
     sha256 "4e2fe1b1837049177c882d32f791de938f96efc5714801b035a8f2788bf2fe89" => :mojave
     sha256 "a1b2d7408b282d44c4ab12563b4e44163553aac7b36ef44458312ed9465e45fc" => :high_sierra
     sha256 "1f61848ed5de9b75450e9d4d3607ee0bdf690785ad954b2dfb139f964477034d" => :sierra
