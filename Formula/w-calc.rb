1c8aee2bcb6c1e47af48e8ff2f8bf3587ee7bfd1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 11:38:38 +0000

    w-calc: update 2.5_1 bottle.

--- Formula/w-calc.rb
+++ Formula/w-calc.rb
@@ -7,6 +7,7 @@ class WCalc < Formula
 
   bottle do
     cellar :any
+    sha256 "dfde02c6213c6eeeecaeae55d7ecaa7620ab5c86f346f9242c82899802901b8b" => :catalina
     sha256 "955d80417eea9747844f52b13d91005f207a869e04f49a4a8f1e1db7e8acfa74" => :mojave
     sha256 "be1800e5bb6cbf1e8087a0310ba648ec80f5013081d8db1145011c2c826b3c0c" => :high_sierra
     sha256 "f934e56de20012d05890525117377efd717ee9d1f09feada9cb41068791065ba" => :sierra
