919edcbf42a6a49af30af0699b84e0cb086e6217
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 00:39:34 +0000

    ykpers: update 1.20.0 bottle.

--- Formula/ykpers.rb
+++ Formula/ykpers.rb
@@ -6,6 +6,7 @@ class Ykpers < Formula
 
   bottle do
     cellar :any
+    sha256 "0b11199f0ee0d96c456d700043073eaea499a2e379ce21ce3fd13fe826f3a667" => :catalina
     sha256 "5a77e8978ad9cf3ed1d596d5ec209cf94a4eb1f13ec7b7b228847c6d6241e662" => :mojave
     sha256 "5907731c71ecd35c52caebbda5b1cd2bca595a06d2a1571c3ffeadb745e33054" => :high_sierra
     sha256 "1f2863cf8132643dc9c3572b649792430d3fc932a5e6d49709a7076b10718d8c" => :sierra
