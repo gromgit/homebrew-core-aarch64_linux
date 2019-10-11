49ec13c7327ba8e0b9e8cff65102c142ae6f32ad
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 16:36:35 +0000

    leiningen: update 2.9.1 bottle.

--- Formula/leiningen.rb
+++ Formula/leiningen.rb
@@ -7,6 +7,7 @@ class Leiningen < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "480cf69d0f2f9d43879b0768018ef323426d0cca145e00984d76890c2baaa919" => :catalina
     sha256 "4b353034cd7bf4825ed9c5a340a20beed7382a8369f9ecd9f7da233e9b36a03a" => :mojave
     sha256 "4b353034cd7bf4825ed9c5a340a20beed7382a8369f9ecd9f7da233e9b36a03a" => :high_sierra
     sha256 "f7ebcf91cfac411472d2dfdee71f008bc2ad3d7289b342a98db0916e74b7f615" => :sierra
