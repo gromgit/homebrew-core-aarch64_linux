29da3d2c06dbd5a3b0c92b01521117a6ce05d82e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 22:54:34 +0000

    handbrake: update 1.2.2 bottle.

--- Formula/handbrake.rb
+++ Formula/handbrake.rb
@@ -7,6 +7,7 @@ class Handbrake < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d355b648d84c7438e4abc411e491c3a49a0558f24fa16f8f9fa48822c75bc242" => :catalina
     sha256 "62a2eb518d2058921f467312c37214a6b9cabd0e5d7e0efe95015a5dd38c9425" => :mojave
     sha256 "c5b58b1d1cd02b23975833e737ec09568e9f36e622e7137aa6c0939ef764b03e" => :high_sierra
     sha256 "0f92e1d55b7d537313e3c2b01e7a780c58956f044a3f3968f84a13ae00050dd9" => :sierra
