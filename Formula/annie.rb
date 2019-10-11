ef762f724ce45afb0007f7d3b96a8f10ceeb3b58
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 21:56:45 +0000

    annie: update 0.9.5 bottle.

--- Formula/annie.rb
+++ Formula/annie.rb
@@ -6,6 +6,7 @@ class Annie < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "06ff55a2834ad01a262b66f181a8c99800157ec0f465f80e0cc3518be7ebd1da" => :catalina
     sha256 "e5176c32457d725bf14ccb0d36e570996db4571428cc21a8f562bf938c86e17e" => :mojave
     sha256 "d532d4c449e12039bbe0d4f539f96b8a0b7d77c5bc2615c620b627ffafa265bf" => :high_sierra
     sha256 "baa97a5223e66e67786dbd786df0c75467393fdff9df0594e57a084097d9e7b6" => :sierra
