fa973a8fece7c3dfb60e92551e6c9611f1bfc8f2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 16:08:48 +0000

    k6: update 0.25.1 bottle.

--- Formula/k6.rb
+++ Formula/k6.rb
@@ -7,6 +7,7 @@ class K6 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e6a4f10a33ce1f4c5860643afcd1a3a5a1c23bb4f481c59d02bbd4f7a41836de" => :catalina
     sha256 "a06505e1d2e7cc083acc511d6f1d9d710186561d9ae8a7dbf884b3a62f649ae4" => :mojave
     sha256 "47718cefff5ce85d27e09914590b26c13f4a37d26096f971e0cff059cd74166e" => :high_sierra
     sha256 "d88317a09fddc7343bfedc774decd5b679284298bcf37dac0f3372e76e429c38" => :sierra
