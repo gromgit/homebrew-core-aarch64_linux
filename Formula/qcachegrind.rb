08138b98bdd22c70b8ad0535a510c140d9f38b4d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 14:09:40 +0000

    qcachegrind: update 18.04.3_1 bottle.

--- Formula/qcachegrind.rb
+++ Formula/qcachegrind.rb
@@ -7,6 +7,7 @@ class Qcachegrind < Formula
 
   bottle do
     cellar :any
+    sha256 "2e2d8e8cbce2e6e2fdaab39c0f1087a143a678c3b2b387dee3b912d8ef2cde2e" => :catalina
     sha256 "e53f3805eed322c207ea13e6d8a5917f16329b423adf11864ad6b4778110c07a" => :mojave
     sha256 "7a0dd392028ecb443bf84284dc671c1697781c5099c253dc526cd02d5530e697" => :high_sierra
     sha256 "f8ec63325129315fd760eccdc59ef14ac096f9f5ddb49e52a495c67e6980b835" => :sierra
