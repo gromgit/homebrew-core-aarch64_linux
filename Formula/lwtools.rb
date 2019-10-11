fbbb72f0c7444baf6b2a0e890a8c4592fadb607c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 01:22:20 +0000

    lwtools: update 4.17 bottle.

--- Formula/lwtools.rb
+++ Formula/lwtools.rb
@@ -6,6 +6,7 @@ class Lwtools < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1ce602bff92ea48cca7732b218e999350d62a3f76d7c69c3e73573da2139d662" => :catalina
     sha256 "3cfae9c3afc0a2fa0d8fdf48c88cb504056f35702f45a4afe5070ceb408d4919" => :mojave
     sha256 "8c0c67d34661986431d9fdb2fe5b6315a0da6b4ec9b4eac139868bfa1ee18069" => :high_sierra
     sha256 "d4f5b062ba3fbd7c7d3115c6f6451fdaa4daf331e0e7f0641580df19dc3c65e4" => :sierra
