09e14af137039ff40578aedb84a598906375aede
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 14:22:30 +0000

    awk: update 20180827 bottle.

--- Formula/awk.rb
+++ Formula/awk.rb
@@ -7,6 +7,7 @@ class Awk < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2920fef8c3a7f5c3e45480b002968a860b0fbe36408cd0c0f1edb94a9b3c67b5" => :catalina
     sha256 "da17e7e893d2a2fb4ab267fb9ead8785ef9417dead77d6c84204d2151330bf47" => :mojave
     sha256 "3e7c18b44cd1f1783a28c34edbc2215a2b975021ec42ccaa0f792243d3cb320b" => :high_sierra
     sha256 "2c55499ad7ed357a30d643430dd00d426fd3cfa2f5705c772f5a3dd8c8cd020c" => :sierra
