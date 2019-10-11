78d7f9a8bd20ba3135e92725331b0317d165bfaa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 22:22:08 +0000

    snapcraft: update 3.8 bottle.

--- Formula/snapcraft.rb
+++ Formula/snapcraft.rb
@@ -8,6 +8,7 @@ class Snapcraft < Formula
 
   bottle do
     cellar :any
+    sha256 "509a7389ba800be5788942ba8d17c6e15e6ae9e2700dd166688b004d3307cd14" => :catalina
     sha256 "0ff03cbd17caa708024170c44e47fe280dac4b947c3738e47c6dd96e90321d43" => :mojave
     sha256 "b5b5a5aa345198d048625e01ec94929e79152a041171ee2d45153837df1488d7" => :high_sierra
     sha256 "17953b952330538bac77d8cde3db6df2b4f508044df3d04d6dacff6497280c51" => :sierra
