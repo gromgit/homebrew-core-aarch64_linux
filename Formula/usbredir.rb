a3c10c0f4348ce946165692e3d858489dd9c3608
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 13:17:45 +0000

    usbredir: update 0.8.0 bottle.

--- Formula/usbredir.rb
+++ Formula/usbredir.rb
@@ -6,6 +6,7 @@ class Usbredir < Formula
 
   bottle do
     cellar :any
+    sha256 "c7182aed390cc4cf96e9a99a728129367714b954062b7f92471a6e3864aed244" => :catalina
     sha256 "579f1db366d50c027cfd6ea92149878b358d86bb6a9d491320e5f7fd62dfd2e8" => :mojave
     sha256 "0d83ca33451b2c382dcf4b70be515549db139b0960712dc7f213e993ba7973d7" => :high_sierra
     sha256 "7feac9566048e308877ef3f3d1b93660433dc8f1611e3daf031eaa4dd90c7238" => :sierra
