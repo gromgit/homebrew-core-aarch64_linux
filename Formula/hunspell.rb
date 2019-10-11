749576d845d309e36f3732a2d3cd12c66895d867
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 14:22:30 +0000

    hunspell: update 1.7.0_2 bottle.

--- Formula/hunspell.rb
+++ Formula/hunspell.rb
@@ -7,6 +7,7 @@ class Hunspell < Formula
 
   bottle do
     cellar :any
+    sha256 "3b7a02908774fa50dafee913969963ce223a09de100b62269fdb0fd71cd09115" => :catalina
     sha256 "30927ed74597ba96c52ec0c1e9380aaaadee2adadf2e17414e1b494bfd8066b3" => :mojave
     sha256 "4ada0a39e041b9e6676b4cd68e95c6523725043088f3555d1cac1216c8f91944" => :high_sierra
     sha256 "65b1d0fc54a0de1fc7e8520aaee4dbe192a2f441757002c7b305b0fb93e3e341" => :sierra
