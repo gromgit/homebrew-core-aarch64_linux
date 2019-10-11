bea4a4783312d4e4a2c4827a7197a883ee05615b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 06:49:43 +0000

    codec2: update 0.8.1 bottle.

--- Formula/codec2.rb
+++ Formula/codec2.rb
@@ -7,6 +7,7 @@ class Codec2 < Formula
 
   bottle do
     cellar :any
+    sha256 "3316417a3e0244dcdc81466af56be6d323169f38c3146075e9314da92c60c938" => :catalina
     sha256 "92031b75a027390385864b1c2a4bde522da712162b7c6f8187a1b2adf74f8504" => :mojave
     sha256 "37a6ae2407ae97ae632078020e89163e9b58d3613207bcf534401f6660128108" => :high_sierra
     sha256 "d90f5373ac39385b8fffee0605afe2e27c195f44ef211f98d7b5d89c7200508d" => :sierra
