f55755b021175d91982d83ebae82e4d093b1fccf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 13:46:31 +0000

    ott: update 0.29 bottle.

--- Formula/ott.rb
+++ Formula/ott.rb
@@ -7,11 +7,9 @@ class Ott < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "2e9d2682f185656de583c42df9b99b3a23e1d167f7cfb366d97d74e9d728c9f9" => :catalina
-    sha256 "a64451a61a2d6cb0d34e327459bdd789233d8acc2fd89673b931220a7780b869" => :mojave
-    sha256 "d590c5c215ca73f47baceb8c851836c1e9ea0d78fcb6f253c02568c6db56a65d" => :high_sierra
-    sha256 "d2132bd134e79d0e73a4b4ea6e23363f9b14ade49b550fa65b5744dc41c35fd9" => :sierra
-    sha256 "12177801e7faf22a9d875d6c0e222fd7eff9fcab5755f5a6266223296ec01ef4" => :el_capitan
+    sha256 "61309aafe923d5ff35914516b48adb21ab3d2aa95a317b8d827cde1166e2252e" => :catalina
+    sha256 "28c5c4755f6e7c34e6ef86009fa839b694ac00136fb822132e38fa06a5361be3" => :mojave
+    sha256 "ed14bf477139c689eaa93ec2d0fb3c6cc73940a04d194621819f417d18bae033" => :high_sierra
   end
 
   depends_on "ocaml" => :build
