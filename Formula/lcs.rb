7f65c9e5a480346a5949ed19594269a057a8e8de
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 22:47:10 +0000

    lcs: update 4.07.4b bottle.

--- Formula/lcs.rb
+++ Formula/lcs.rb
@@ -6,6 +6,7 @@ class Lcs < Formula
   head "https://svn.code.sf.net/p/lcsgame/code/trunk"
 
   bottle do
+    sha256 "65391613e5fd3ea3d8e5f7a2d5586105e3408bb09cddb77aebcfd4bcb7e3396a" => :catalina
     sha256 "91469578607a9f4d1ae989ee523f2b5dd97c976d32d9a822769129df828163e5" => :mojave
     sha256 "5b640b7b87dfe6603670addce1b6af77b0cd7ebbda10c445fddc6d365960e761" => :high_sierra
     sha256 "a8fa614ec5adc3ee2d7417a024bf5e9c78e9f8d4e043e0b916dc5a99f1bb1d9c" => :sierra
