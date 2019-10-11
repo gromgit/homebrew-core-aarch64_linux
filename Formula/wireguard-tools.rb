638f33ba6475990bc3c37604d7703db5c8dec762
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 16:41:53 +0000

    wireguard-tools: update 0.0.20190905 bottle.

--- Formula/wireguard-tools.rb
+++ Formula/wireguard-tools.rb
@@ -9,6 +9,7 @@ class WireguardTools < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "65e63b009640b8f72f526602d426492d7e9e76d4608b4d2dec2ccd9e8ee341ab" => :catalina
     sha256 "c165ed343667bd331ba792ce5fb869c31196940ab97f61527413d5ae2b8849f8" => :mojave
     sha256 "4c20b4c5c986e7d1972f12be22f2051d4f817cfd5f0e789a923de1497952e9cc" => :high_sierra
     sha256 "135dd7d027656bd2f8e2d64f7f5c03df708d4aad2351f2cc6597a2bbcfbc070d" => :sierra
