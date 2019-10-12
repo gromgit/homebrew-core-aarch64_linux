0339ae5c2e1d08da94818bab34fa5f9ef886a2e1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-12 08:14:09 +0000

    zebra: update 2.1.4_2 bottle.

--- Formula/zebra.rb
+++ Formula/zebra.rb
@@ -6,6 +6,7 @@ class Zebra < Formula
   revision 2
 
   bottle do
+    sha256 "e67fb475fd3468f8622460b1e57cccb265017cab4173a080334a015e36c0621b" => :catalina
     sha256 "dd784ac02afbfdbda3786a0a76007f03b6e2d49f62881240f0511a5cde31d4c8" => :mojave
     sha256 "2828bc307310857dc7aaf504ac5d4be5404cae0cadd10825197d671a41ab1bb3" => :high_sierra
     sha256 "d0bba81ae80e339d30ea0e0741e8547a0c3ae21ee190b6f0ff6171fa8fd0fbc4" => :sierra
