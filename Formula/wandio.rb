73a23ad0f42125aff4931cd6eb9b0fa81e9f6d5a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-12 19:38:02 +0000

    wandio: update 4.2.2 bottle.

--- Formula/wandio.rb
+++ Formula/wandio.rb
@@ -6,6 +6,7 @@ class Wandio < Formula
 
   bottle do
     cellar :any
+    sha256 "dc91ef0f03bfc027c7921a132c92ef7b9e768307050742b6ff9543998a786f6b" => :catalina
     sha256 "02cce1059503c0fa2051661ab8075e62c279aeb8a761d408ef2dd4ab7545a6b6" => :mojave
     sha256 "0dbb3e5ddd4fc6de440174d2667d8453f3f4136d6dba0d3f6cc58f15537ff9d7" => :high_sierra
     sha256 "98922801f17e30f8aa0685ba4fd2b4a21b46ab53ad1a71f7f39f28fa7cbc2186" => :sierra
