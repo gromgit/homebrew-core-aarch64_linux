4408e0135e3a146e34cc96c8053e14b4aca412ec
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-12 08:09:39 +0000

    jmxtrans: update 270 bottle.

--- Formula/jmxtrans.rb
+++ Formula/jmxtrans.rb
@@ -7,6 +7,7 @@ class Jmxtrans < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4bbf264bf2d65ba622358f879cce60433636d7fd91c1f7c0e04d5869e42886d7" => :catalina
     sha256 "8ba9f5d54de8350421d72dfbbaa821e89d8707e5177a7156ce55f87e89befcfd" => :mojave
     sha256 "a443805c63b0b51d4b13dfa2cc92b0d3f04ec2bcca3cffedb27ccd1e8d124fad" => :high_sierra
     sha256 "3b9479ac46dc39a6f0a69bf43c5469abb31f33de6f51159ba6754a52d4127d7c" => :sierra
