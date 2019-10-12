91409c5f2fb00a63f93e0a830d8f2d732cd04029
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-12 09:01:13 +0000

    libtorrent-rasterbar: update 1.2.1_1 bottle.

--- Formula/libtorrent-rasterbar.rb
+++ Formula/libtorrent-rasterbar.rb
@@ -7,6 +7,7 @@ class LibtorrentRasterbar < Formula
 
   bottle do
     cellar :any
+    sha256 "8b244067c5402516ed9430fb06fb007bd8895de3b7fb8c48e88af1fce992849a" => :catalina
     sha256 "f93dc11cc55befe32f43c6ade5dbc6eb5b65e9f70442b80d26e948306e1964f8" => :mojave
     sha256 "bb13af8b8c7495fb1fb3c07a38c690687a78af3164d26681ca3f81511f0c3277" => :high_sierra
     sha256 "842c4c7e9a5e50ceb7bdc74149933e8ae79c41ca2e0c6e867b4450046221dc92" => :sierra
