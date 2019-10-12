516ea9c4e37f343f4ed8add7595ae96c41f4e162
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-12 09:24:44 +0000

    peg-markdown: update 0.4.14_1 bottle.

--- Formula/peg-markdown.rb
+++ Formula/peg-markdown.rb
@@ -8,6 +8,7 @@ class PegMarkdown < Formula
 
   bottle do
     cellar :any
+    sha256 "08910e3fdd97183865c2839a4e14839826101e6dfa48120aebc60fbe838f0689" => :catalina
     sha256 "a60087175a8f3c5242e9183eeddb433e6bdbe68409cae0a7c61d66da4622b150" => :mojave
     sha256 "207764b26b253904cf61e9e13eb32e81a51d61d548b7dafd366da5a5394a5f08" => :high_sierra
     sha256 "2d75448f008aa176b624ecb02bc6e3f7492ea8953a99f84fcdacc6b301b39412" => :sierra
