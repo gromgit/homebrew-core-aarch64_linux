103a5c327ed188487aba7db98a7f9dec96d8e46f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-12 00:15:22 +0000

    befunge93: update 2.25 bottle.

--- Formula/befunge93.rb
+++ Formula/befunge93.rb
@@ -8,6 +8,7 @@ class Befunge93 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "61b8cd95c36c1bc4729663a3023416a3fecea6a0d47d21e8ef37edf032b8a6b4" => :catalina
     sha256 "f77d79e0bc06317441221cf22696f3be81f75221531f63bcb71d819954e2884b" => :mojave
     sha256 "3e82d14825ded095e92ebec797f682a65c7ada55ec178e069d0093eb29da8e4a" => :high_sierra
     sha256 "302f0c69592719782676dc2873a9a9faef3f72b6ec7bb225d60e74ca1cc0f640" => :sierra
