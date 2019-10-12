4364941fb1ab741d1bcf50cdf09159f7b23822ba
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-12 16:43:24 +0000

    libresample: update 0.1.3 bottle.

--- Formula/libresample.rb
+++ Formula/libresample.rb
@@ -7,6 +7,7 @@ class Libresample < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "779b21b26d28a7318e67e0444b74ee5782715b523c1f79ba9bdff41c334cd312" => :catalina
     sha256 "7973809674c5ca9dceaf822abaf482c2a8126928140fa056168644b1196005c2" => :mojave
     sha256 "42b971ed75ad6ba1bd6879c2b7cb5fb416706ed184291d12983e46ab6c90a20c" => :high_sierra
     sha256 "b94dc206fa507bcdceb49534740c5c0dff0868a9d9333e4acd8922f22b10c912" => :sierra
