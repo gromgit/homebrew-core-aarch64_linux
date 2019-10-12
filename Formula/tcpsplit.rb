f0089fd83afc7eb8aeb7218f42d7200f3dca7ec0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-12 11:11:45 +0000

    tcpsplit: update 0.2 bottle.

--- Formula/tcpsplit.rb
+++ Formula/tcpsplit.rb
@@ -6,6 +6,7 @@ class Tcpsplit < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ab3131cd8829f943cc4142dc616adfa696ff9d0af5dc21f94408d114f59434cd" => :catalina
     sha256 "b3a7f083a50a33edf1799fc16b6d52db71eee85bd69bad9d1d3d42e6de5cfa6f" => :mojave
     sha256 "0b603f1737a000ec2452bd3ac48df7c4e04d6cfb15fc48dabca96bd23137f40a" => :high_sierra
     sha256 "2e9d12ee609d30075f141527c3804ce78a8c312e5b72ce6eb655ed08521faf45" => :sierra
