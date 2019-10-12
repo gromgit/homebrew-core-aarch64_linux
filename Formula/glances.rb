3eadaa92b2d268582e9d530432d35bb0d9e1117e
Rui Chen <chenrui333@gmail.com>
2019-10-12 11:12:45 -0400

    glances 3.1.3
    Closes #45228.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/glances.rb
+++ Formula/glances.rb
@@ -1,8 +1,8 @@
 class Glances < Formula
   desc "Alternative to top/htop"
   homepage "https://nicolargo.github.io/glances/"
-  url "https://github.com/nicolargo/glances/archive/v3.1.2.tar.gz"
-  sha256 "adda21ceaff7a04b18b0f71f2dbfcc1647bd0aa888d12f61f1e03f4f60216a12"
+  url "https://github.com/nicolargo/glances/archive/v3.1.3.tar.gz"
+  sha256 "e3e8f9362b82c74427522e82501b47696945251035b35282f9ee4bc533996220"
 
   bottle do
     cellar :any_skip_relocation
