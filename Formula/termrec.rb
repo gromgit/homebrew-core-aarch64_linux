66fb48456fbf6aaf633aa5ed34fbe822e5ac6e70
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-12 11:39:26 +0000

    termrec: update 0.18 bottle.

--- Formula/termrec.rb
+++ Formula/termrec.rb
@@ -7,6 +7,7 @@ class Termrec < Formula
 
   bottle do
     cellar :any
+    sha256 "4c6ec1ae65b46494728ca14fdd9112474b8030d6d5d5b6abcd312b4ae6dff5ad" => :catalina
     sha256 "062bb1a65c337321569546e845048d3fb5912113bca87080530b72cd262fc8ff" => :mojave
     sha256 "61ffceeab4daf5feeeada49c1057ddafc8c5efb0c1561d7b8e1303462cba490b" => :high_sierra
     sha256 "b51ae0744fb22e3807841f852452c422f9117d53c83503b8b89ea7936ba700e7" => :sierra
