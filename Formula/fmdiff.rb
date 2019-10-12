37909f303c3cf6ede666500a71461844e193689e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-12 08:09:40 +0000

    fmdiff: update 20150915 bottle.

--- Formula/fmdiff.rb
+++ Formula/fmdiff.rb
@@ -8,6 +8,7 @@ class Fmdiff < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "4dc5d4eed6916cab57ab1350c7623014c1f2136b69dcb5593c9e344b86328f6c" => :catalina
     sha256 "4c5fb2bb25510c7269a40ef77d55a3d7e52377db2a012d60c3003a9512616314" => :mojave
     sha256 "03bf7b7acda053f3b96de99591fb97cef678297941eab37f52802a3c2734afdd" => :high_sierra
     sha256 "59d9c9d8a8759531a2f715619cfb2bce404fc7378235cf416ea5a426eb8d967f" => :sierra
