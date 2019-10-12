6370c7440affe84c4ba97d28e95a2f9220d79f8a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-12 08:36:14 +0000

    nu-smv: update 2.6.0 bottle.

--- Formula/nu-smv.rb
+++ Formula/nu-smv.rb
@@ -6,6 +6,7 @@ class NuSmv < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "90dad1b30d80ee7ddba984d6ad2536fff08896e79cf1a26a083a5e9990fc3c43" => :catalina
     sha256 "c2cc207758d6f315db1116e0e162be72edc0356312c460cd3359dca8c7de597e" => :mojave
     sha256 "f2e93143e60b64244fd25958a88480acee332fd4109a6bd356719dc6259efc36" => :high_sierra
     sha256 "64f825eac53c6c16c9b3db4b505d37a6de9f1f3471863b39081b5a98d517fb3e" => :sierra
