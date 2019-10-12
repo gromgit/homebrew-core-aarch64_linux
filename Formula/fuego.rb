9de24496c94313bce1dd0e0ee4ed5b39eca0becd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-12 08:09:41 +0000

    fuego: update 1.1.SVN_2 bottle.

--- Formula/fuego.rb
+++ Formula/fuego.rb
@@ -7,6 +7,7 @@ class Fuego < Formula
   head "https://svn.code.sf.net/p/fuego/code/trunk"
 
   bottle do
+    sha256 "3fcaf6036c24d8771fb2bb55a5e697bbb811aa03c37bae887982b96be70a422f" => :catalina
     sha256 "2e8c65ddbcbb76158ab22805982c75940ed1a6eddc033cc157a03bee1364d502" => :mojave
     sha256 "7efef5865934cb21cce5a12c7adf39d3c74a86990067220d456e53db69f8861f" => :high_sierra
     sha256 "828c076fbcd288d4cc2348323497983f78aceb8bd1b607403b13e35fa209a86f" => :sierra
