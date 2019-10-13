afac171d488bddb259af235fdebdcc316515564b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 23:57:30 +0000

    agedu: update 20190630 bottle.

--- Formula/agedu.rb
+++ Formula/agedu.rb
@@ -8,6 +8,7 @@ class Agedu < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "cca96d256d3ec4eb7ccaa2e5062cf575bd3065d7068a9d6bd7e3508a7e87048d" => :catalina
     sha256 "3cec13de1853d543fd8bd42f578f1037e5005040638af2143bf38b3dd22a0060" => :mojave
     sha256 "5e6ba4f9a0e740e8091def5ffa43c8b88f841f0554fb721d92b43bff517500e0" => :high_sierra
     sha256 "bac281c68b7b62f7241f49567c29828c97e51990f54da8c208dfa206465716b4" => :sierra
