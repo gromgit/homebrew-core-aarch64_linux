d6bf9f65d19b29c089258ae4f6dfa2dc3a26e3d9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 23:57:30 +0000

    afsctool: update 1.6.4_2 bottle.

--- Formula/afsctool.rb
+++ Formula/afsctool.rb
@@ -9,6 +9,7 @@ class Afsctool < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "f418e15be4bafdcb1a85e14c3148c8d4af1b300bd6ed3e4a30eca3725459ac48" => :catalina
     sha256 "15c264a828ed98a42cc5ac68869c16b8306f73effe108e50bb1f731574311c51" => :mojave
     sha256 "72e92414d524b82ec1d8381ad50f55bd330f1109a5e10bca4235300fee557caf" => :high_sierra
     sha256 "96437b04a2974c215979550d3d70b4c8e3f609e76954ca41059c6f246da452ee" => :sierra
