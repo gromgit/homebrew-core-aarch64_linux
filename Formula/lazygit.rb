0f465497893ecf43237ea2930c75226905857717
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 20:56:47 +0000

    lazygit: update 0.8.2 bottle.

--- Formula/lazygit.rb
+++ Formula/lazygit.rb
@@ -6,6 +6,7 @@ class Lazygit < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f351832c8c29ead471e9c762e468021976002e15b0e980712bef7f5e04c6f2a2" => :catalina
     sha256 "04317d3625563735d60f8e7d7871693da99867ef463b09072209a377c32e14ac" => :mojave
     sha256 "f6c93c2b35b269a83811cf10bf8a58ad5af108806777a0c6e489329621432721" => :high_sierra
     sha256 "31308b34df92ff5ec10195e117729ff475fabce00fbd0ed64c01d68198b221d9" => :sierra
