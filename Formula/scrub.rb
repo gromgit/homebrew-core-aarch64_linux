1a2dccdb71e97717f5a2ca2f3590ff4a920cfd51
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 15:28:45 +0000

    scrub: update 2.6.1 bottle.

--- Formula/scrub.rb
+++ Formula/scrub.rb
@@ -8,6 +8,7 @@ class Scrub < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "01146146976c9be7bf2b74b018e7b98a158407f7318ffe604bee4603270e6f4f" => :catalina
     sha256 "9343d2cc328739d3315f319eeb6704cbd8e98e8105065ff194fcb51456114c4e" => :mojave
     sha256 "c9e96dce0a6f2d7c3b32d481aae3a3aa2c0f42cd3c53b10e2fd60c6479ebf128" => :high_sierra
     sha256 "703ee9b222437bf008ceaa25ab802ace51f207bcba8503f88037896aee2fde40" => :sierra
