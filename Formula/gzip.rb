69691fcaa9ece8598985c24c3a5ee8eb65ded3ce
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 19:54:16 +0000

    gzip: update 1.10 bottle.

--- Formula/gzip.rb
+++ Formula/gzip.rb
@@ -7,6 +7,7 @@ class Gzip < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "903289308ce89ae70f3f8116738834d2c18fed8c248c1da82fde69cdca2b34a9" => :catalina
     sha256 "9391f27b1cf04c20abb2320ba55e1f8ac186db22b7c07bf51ebccfab073f85dd" => :mojave
     sha256 "d639fe5a95eb7c0e12aa1577ca9b230cdbbd31b0ef51794d57415f9a9fa68f08" => :high_sierra
     sha256 "bfd50566283402c72d15cd87670a3fd8ea122ebbaf583bc8cfafc552340ecf70" => :sierra
