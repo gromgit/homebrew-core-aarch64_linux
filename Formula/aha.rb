cdb6059ddeb6891bfd6e985860d4a545060c037c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 23:59:14 +0000

    aha: update 0.5 bottle.

--- Formula/aha.rb
+++ Formula/aha.rb
@@ -7,6 +7,7 @@ class Aha < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6492a70a68055bd7034820fc1a8dd951797ebde44515ffcd9d4416da732eeeb6" => :catalina
     sha256 "b22a64305a6e2b05dfbc534169d4536d34dcb5ed81be53dbf57f7dd640a6b9bf" => :mojave
     sha256 "be1915be3aebd7fdccc3e0d694351b29c9e5af4093492154dbf46ddb5d506a2b" => :high_sierra
     sha256 "2625fabfb62878a3123448e5dbe56e18efbd0d90f1b4dcf3dc1619274ecb7c08" => :sierra
