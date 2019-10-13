fdbe8e47a93f08b948e111fab988c20b6170a6d8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 20:06:29 +0000

    docker-machine-driver-hyperkit: update 1.0.0 bottle.

--- Formula/docker-machine-driver-hyperkit.rb
+++ Formula/docker-machine-driver-hyperkit.rb
@@ -7,6 +7,7 @@ class DockerMachineDriverHyperkit < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "970f9a0f226f1dde7d60e0878a05cef43b503e79f669e2f69fa6e2fd48cfb7f5" => :catalina
     sha256 "1b3ba8ce6ae05b27463ef2b8ebfbdeec911a0b6f1ba20188279b79dac81b4754" => :mojave
     sha256 "41aecb9ebaf6d8b45780cef4acd16a3b40b4e6be0020d1aae8a68d4d314adeda" => :high_sierra
     sha256 "4cdd1e0ed1b3d36dc19b31ad22d1f03578221504ce4c731ba30c0179f2c1ee00" => :sierra
