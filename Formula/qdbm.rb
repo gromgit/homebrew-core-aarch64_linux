ee858b31faefecfe1d39f857977a6a5271d03e0d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 15:28:44 +0000

    qdbm: update 1.8.78 bottle.

--- Formula/qdbm.rb
+++ Formula/qdbm.rb
@@ -7,6 +7,7 @@ class Qdbm < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "0a0ba32270742fbd821ba60bbc6452e6b6b6a476d72e719bdb33fdf535e316f0" => :catalina
     sha256 "4861035c21a7fcd02efca60c922d06a45f3078eaffa374784a533932f9efa806" => :mojave
     sha256 "4ec4e60b16efb21fd7835c182fcf5d8f43c4af4329dd8afb07b4900bc1b17f60" => :high_sierra
     sha256 "547ecf82252706d276c8359448b7f4e738264999028b06cd3738af34ba58276c" => :sierra
