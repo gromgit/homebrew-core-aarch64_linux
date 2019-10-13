8da988fc5021164cc84f6cd93e16286d599adf44
Igor Kapkov <igasgeek@me.com>
2019-10-13 19:39:43 +1100

    docfx 2.46 (#45245)

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -1,8 +1,8 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/releases/download/v2.45.1/docfx.zip"
-  sha256 "4aff305a573bf73f64df82516e94a1bd4e5ed7b42197b8b3c74bf077e383cb48"
+  url "https://github.com/dotnet/docfx/releases/download/v2.46/docfx.zip"
+  sha256 "b94b7f8fb94061a73da82126b55eb1e98355650194ea0514df1841a7553f3c15"
 
   bottle :unneeded
 
