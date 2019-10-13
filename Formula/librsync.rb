e05851375717739f30f1c2115c6d951e91b928eb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 04:45:03 +0000

    librsync: update 2.2.0 bottle.

--- Formula/librsync.rb
+++ Formula/librsync.rb
@@ -5,10 +5,9 @@ class Librsync < Formula
   sha256 "9eac82c09c4404ba4edadd25dbbcc14008e553c9b2fb492ff9ee05cad9fa012c"
 
   bottle do
-    sha256 "b582a3cff68f50a4d42fcb4aa4bb9401514715ba02814945a7af0c95d56b6d24" => :catalina
-    sha256 "addc0756351610330977decc0ae1b31c6f2928e527faef5d38230f623b88dc07" => :mojave
-    sha256 "9e813729589ad923be1fd040cd54a5c5083a824c05f09f7f8a77fc529a9516ce" => :high_sierra
-    sha256 "a4523e8193af9a30986f706d22d53b937d3ffc9c1bfa5fda05d54654a616a0ef" => :sierra
+    sha256 "b9b3afb3ec1694152e3c0bd32044d3a6fedae7143837004ce3ebb6dd4350c4a5" => :catalina
+    sha256 "18e5e0e833f9ce29038c02bf88f08e05bb319d0e52b9a28da2f7dac5931076d6" => :mojave
+    sha256 "4e58b9ce861bd356f6626e7e8d2b0e6b80d4c0f54387c51adab6561a41c873a6" => :high_sierra
   end
 
   depends_on "cmake" => :build
