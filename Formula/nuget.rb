91bcae1acc61d672d81e5c220e9a65ff9ac781c1
hn3000 <harald@niesche.de>
2019-10-13 23:06:20 +0200

    nuget 5.3.0 (#45292)

--- Formula/nuget.rb
+++ Formula/nuget.rb
@@ -1,15 +1,15 @@
 class Nuget < Formula
   desc "Package manager for Microsoft development platform including .NET"
   homepage "https://www.nuget.org/"
-  url "https://dist.nuget.org/win-x86-commandline/v5.2.0/nuget.exe"
-  sha256 "2865714c6588ef0770b4a04bdd20dac59bcb56756d001c040664c9966f1b835a"
+  url "https://dist.nuget.org/win-x86-commandline/v5.3.0/nuget.exe" # make sure libexec.install below matches case
+  sha256 "96a1dc5ad572716b2ab345a6830365cf0103661ec906b0e78fd04a064a7bb730"
 
   bottle :unneeded
 
   depends_on "mono"
 
   def install
-    libexec.install "NuGet.exe" => "nuget.exe"
+    libexec.install "nuget.exe" => "nuget.exe"
     (bin/"nuget").write <<~EOS
       #!/bin/bash
       mono #{libexec}/nuget.exe "$@"
@@ -17,6 +17,6 @@ class Nuget < Formula
   end
 
   test do
-    assert_match "NuGet.Protocol.Core.v3", shell_output("#{bin}/nuget list NuGet.Protocol.Core.v3")
+    assert_match "NuGet.Protocol.Core.v3", shell_output("#{bin}/nuget list packageid:NuGet.Protocol.Core.v3")
   end
 end
