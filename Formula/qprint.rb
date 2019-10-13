fc4dc99d7de4b60949c6302960e977228f16fde5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 15:28:44 +0000

    qprint: update 1.1 bottle.

--- Formula/qprint.rb
+++ Formula/qprint.rb
@@ -6,6 +6,7 @@ class Qprint < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "081c0663cccb890326323fce7ac57b8bb020d3505eaf0d19f1824dd63c304de2" => :catalina
     sha256 "0915aa3e8b8481b717c4c84b0eda595821ecc99c9ffdcd0aa3e4952a3de9ae87" => :mojave
     sha256 "57950dba66674d62c84076374427f6c3de6d8cda81448c50b579c11b1b1959e4" => :high_sierra
     sha256 "f26387daf3d025dd45843784dd90fb3bf77609bdf0eb870f1b66782c89571950" => :sierra
