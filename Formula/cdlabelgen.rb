44f493ee24a85751235618cf2317a830089b91b4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 23:47:18 +0000

    cdlabelgen: update 4.3.0 bottle.

--- Formula/cdlabelgen.rb
+++ Formula/cdlabelgen.rb
@@ -6,6 +6,7 @@ class Cdlabelgen < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5facce52a8f22279160a388513b2a9406f427f3ab231e119fbc0b074dc7028f9" => :catalina
     sha256 "5162ba8c34a6aeef369b5004d1608fa2b8de5a33350f1d7629f08eed8b18d5d9" => :mojave
     sha256 "ece93bae3d8b9e6e5c37b347849836dc970183efcea603d7d3b6f8f0dbaebd4a" => :high_sierra
     sha256 "a874e660972a4ac722e56e13749a17f3c76c5fa61691f44d70afb13c88e4e65f" => :sierra
