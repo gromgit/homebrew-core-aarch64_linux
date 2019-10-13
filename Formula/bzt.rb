a4734fa87313f17bccc3e99b0790248f50297d40
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 22:49:17 +0000

    bzt: update 1.13.8 bottle.

--- Formula/bzt.rb
+++ Formula/bzt.rb
@@ -9,6 +9,7 @@ class Bzt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0d4e3017fff798da70748c4eb0a5697404cf0400daf052002b5bfcd1e4dbc2c1" => :catalina
     sha256 "7b63d8e8b278839e3eb772a645b84d599b7f298e07858c91763aed50b34fa250" => :mojave
     sha256 "a9dc15a0a3e2b8489d6e925c2806f75688885654abacd7c8e2eb30f0e6a00330" => :high_sierra
     sha256 "853caaf1abc51a7cd4dc92decd21f4a8bb56511571ce8fce8fdec105071a7ce7" => :sierra
