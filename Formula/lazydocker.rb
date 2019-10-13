feefadf5107b389919fba1c08d88f9d0c0c20529
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 20:56:47 +0000

    lazydocker: update 0.7.4 bottle.

--- Formula/lazydocker.rb
+++ Formula/lazydocker.rb
@@ -8,6 +8,7 @@ class Lazydocker < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "ed7a928d2a93a05441b28560c2d0bf6a8ac58278313966c286e2d5d8703dd04c" => :catalina
     sha256 "7e6904339596de03b73b9a7e9fd4fcb99bc3e17e7188de4305d0367af09094aa" => :mojave
     sha256 "41f35cdbf18a012b7160ec10164ce66d4aedba2d9601f34d3cf0e93afa569528" => :high_sierra
     sha256 "cc0ea7892e0615a0e6d8b02573334cb3759cafd9abf81c2cb55fbddb68979f44" => :sierra
