16fc44ca776caf553a17b6e4a4ef7cfb2fc44467
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 22:06:40 +0000

    boxes: update 1.3 bottle.

--- Formula/boxes.rb
+++ Formula/boxes.rb
@@ -6,6 +6,7 @@ class Boxes < Formula
   head "https://github.com/ascii-boxes/boxes.git"
 
   bottle do
+    sha256 "48a3b6d9b8c23a3ab1f366f6f085361cc8db8cd341dfe9474665d87c4c23bbf8" => :catalina
     sha256 "84b135ad528536233546dbf8d36e0be4a21a89050910e45a4f8e2796c99b7c3f" => :mojave
     sha256 "ca1c4e0e76f03ee4a60789f30093d2eee3794ff54b989da1a8a3ae555228f081" => :high_sierra
     sha256 "d31462128d1f55cd3014ae942b4620f1ec4d06e72e8a47cae5ef56afcf65e791" => :sierra
