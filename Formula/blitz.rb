9fef1ee0f59a86fdb66cf9ff72e39ba3c53aec0d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 21:37:50 +0000

    blitz: update 0.10 bottle.

--- Formula/blitz.rb
+++ Formula/blitz.rb
@@ -7,6 +7,7 @@ class Blitz < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "f8eac429778e92f80e83d9f013e4b5ebc162e458a77af592cfe4f2de820b1bfa" => :catalina
     sha256 "1d88c433d1c5dc863d670358624cc3d733f042cdb052848c673d5eafa8a6dc33" => :mojave
     sha256 "1fbcdb2453e10ef03721f965050244519743a6161dfc581bda663597ecf44595" => :high_sierra
     sha256 "b676b24071752779faadf53d71b53b0c632b8ba62d1cd7c1f90d40ee5b13a85b" => :sierra
