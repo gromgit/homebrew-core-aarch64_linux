1df52b9762a71d81687bc8aae4701dc4e7d15494
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 19:24:16 +0000

    vips: update 8.8.3 bottle.

--- Formula/vips.rb
+++ Formula/vips.rb
@@ -5,6 +5,7 @@ class Vips < Formula
   sha256 "c5e4dd5a5c6a777c129037d19ca606769b3f1d405fcc9c8eeda906a61491f790"
 
   bottle do
+    sha256 "2b2ef7ae88cfc411e860386a705a9580ac16b6ede3bdc058e77085e02f06e27b" => :catalina
     sha256 "7f823bc23484c3c00efc9dd3b6c2e152305639a66f6d9a39ae58c86fc123c1c3" => :mojave
     sha256 "107f0b0417f8676a829cf3c0923331d917696d6aed196f45f4de26823ed0be05" => :high_sierra
     sha256 "2d09af95e01b212b19dd00342ad2fb9ce8aa9956160a1a4dea7e7d7a860869ce" => :sierra
