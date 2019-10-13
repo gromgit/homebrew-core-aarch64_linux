1956c54057e95c37a979f2846fa6d98f7a7a9d33
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 23:47:18 +0000

    cdogs-sdl: update 0.6.9 bottle.

--- Formula/cdogs-sdl.rb
+++ Formula/cdogs-sdl.rb
@@ -6,6 +6,7 @@ class CdogsSdl < Formula
   head "https://github.com/cxong/cdogs-sdl.git"
 
   bottle do
+    sha256 "6fb64eabb3777469eae01c7a106ae23a476207acc464dce439ee0df32113f559" => :catalina
     sha256 "0d9364354ce369be43114c059c8ae55fdc9a9f77534463f919cb3fc491f63c79" => :mojave
     sha256 "fa1acdcace9940b7e633d21f42159ff72aa55095d995f8331fba40b4aa85f31e" => :high_sierra
     sha256 "811a7269fa69ba5fde0a15486e019c91f0d6b81afc644d7ed782f1d7e144917e" => :sierra
