fbc43ae3871aa747727805f3fe236f1d37cbd7f5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 21:33:18 +0000

    bibutils: update 6.7 bottle.

--- Formula/bibutils.rb
+++ Formula/bibutils.rb
@@ -6,6 +6,7 @@ class Bibutils < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c9468806b3c51e20497707ffbd6b5d65880fb7016d9b8fe0f34efca96a34739f" => :catalina
     sha256 "77c7e24e44776095769e2103d7bc37ebbd314e91c6c86a9a4e24ad42f15b16dd" => :mojave
     sha256 "cf925ae310688ae93b2f93a4bb23deb16f06fa8f28f20b67aee17162eaa484c1" => :high_sierra
     sha256 "97a7c39fa603ac3ebdbde752b0b34d55bc724df248c8ecc8293e9cf47a791823" => :sierra
