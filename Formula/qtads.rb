60e3ab5b00133c2d2a66b0bfc60b5621df2d063a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 14:24:04 +0000

    qtads: update 2.1.7_1 bottle.

--- Formula/qtads.rb
+++ Formula/qtads.rb
@@ -34,6 +34,7 @@ class Qtads < Formula
 
   bottle do
     cellar :any
+    sha256 "ddc00587ac0d9f3ebcd6f0bac9e8a4207f9ae930a6646e4f3ce60d186abdc832" => :catalina
     sha256 "3158fb6eb3d97f548c908983348e221ee190835bda5ce70704747117ecf7611d" => :mojave
     sha256 "ef218d294d01133003c6e52fc32f9482726d6f237b3b5b90add019960ffe9eb2" => :high_sierra
     sha256 "51fff5c39b8c234bb72b9a3865f7a067fb2dab902316c7943261ba66ed98ab19" => :sierra
