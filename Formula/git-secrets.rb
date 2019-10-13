47edccc7373b98891d9b5bc8d376cc31efaff8f0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 19:33:21 +0000

    git-secrets: update 1.3.0 bottle.

--- Formula/git-secrets.rb
+++ Formula/git-secrets.rb
@@ -7,6 +7,7 @@ class GitSecrets < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "92ae3c8447754b1f5d23b5e7e3a601fca133f4b691b51b5004ffe17f4d763622" => :catalina
     sha256 "d77761ee552d2963788f2bcab6c695d1b52f9d0c1d68dad65230901c750e63aa" => :mojave
     sha256 "d77761ee552d2963788f2bcab6c695d1b52f9d0c1d68dad65230901c750e63aa" => :high_sierra
     sha256 "fc2745b24be00e6b8e4b82d6768632810823ffff3f80ad99ca9943b31d003003" => :sierra
