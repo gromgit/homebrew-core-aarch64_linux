7dae10f6fa96e73e7a040757fd0997ad399c5850
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 19:47:02 +0000

    octant: update 0.7.0 bottle.

--- Formula/octant.rb
+++ Formula/octant.rb
@@ -8,6 +8,7 @@ class Octant < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "adff5d7e62c5d40c887adf3ba788dd5424d85fd09618f439230d50d1f28cfbc4" => :catalina
     sha256 "7c3f8a3f5d2f8888368bf6f24c0b28c4d516ed005c3c5aba3c485fc72eda1414" => :mojave
     sha256 "b60233f74ae10e0b5bdb3f7003676db10e2d8daf177ddd0245389521001379a6" => :high_sierra
     sha256 "bcab9204ffb24a70f5a9d838c1494e932d641c281b5ba6c13a87ab4aca90ebd3" => :sierra
