7f8b435c5a19ba94fdd2177d28acf6c5b283b516
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-13 20:32:47 +0000

    arping: update 2.20 bottle.

--- Formula/arping.rb
+++ Formula/arping.rb
@@ -6,6 +6,7 @@ class Arping < Formula
 
   bottle do
     cellar :any
+    sha256 "b89ee98a8f209031e51c3289ae5afb518491b1ead9d12980dfd5fd58fa88fa92" => :catalina
     sha256 "15e267fd6609e686236668578452b0d2f3fc7771b24fb9ef1486fe9752f44645" => :mojave
     sha256 "f9d6ddf4906c516e80b25f7a28e8d00f917af69653dc9ecfcc36a0f4bd0b2f99" => :high_sierra
     sha256 "3170e9a0f53d76b1ae18ae15fa5945578044debb5798c1955bb8d9437b83088e" => :sierra
