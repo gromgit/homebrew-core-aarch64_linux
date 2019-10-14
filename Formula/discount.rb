d823409242c6cd1c7ea86a47a449d1a3330de6ea
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 09:33:16 +0000

    discount: update 2.2.4 bottle.

--- Formula/discount.rb
+++ Formula/discount.rb
@@ -7,6 +7,7 @@ class Discount < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "8f5a27a62f9a96cce7872780be458cf1cdee98333e4ff9ed4acd9772fd6bcd5f" => :catalina
     sha256 "717446676d0861c18c2cc4f7eb968eb8abec3c91bb623df176d1735d71ed58c3" => :mojave
     sha256 "fdcd1162af5608087a3520333760fb68a467866cf02ee059a876b38c0e684d5b" => :high_sierra
     sha256 "4798e5e4fb0dbcbcfe92babf8c0a9eb7d0739197a636f1daa34fd52715bba05d" => :sierra
