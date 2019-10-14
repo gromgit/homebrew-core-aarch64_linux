3cf9daf2d21ed520287f84903410e1ce7f0fa12b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 19:41:28 +0000

    librasterlite: update 1.1g_6 bottle.

--- Formula/librasterlite.rb
+++ Formula/librasterlite.rb
@@ -7,6 +7,7 @@ class Librasterlite < Formula
 
   bottle do
     cellar :any
+    sha256 "a66bb073375cf60241540cedba00415ac85fde9b64106f76a5a052a1da508661" => :catalina
     sha256 "e50438fe42116fd2206c46ad813bd65ea732513f2993293d5c8211b38c7642e0" => :mojave
     sha256 "b622c859952b981aea4443d46fc24948a653ed4faaf83184347af82699f2f74e" => :high_sierra
     sha256 "7605cd6756f69c93737ae300e946b81e559d8c5c697bcdfae1f9c1b80b84f657" => :sierra
