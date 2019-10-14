aca02e5bd2ad91ab747c9a8841a99865e406c83f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 10:34:40 +0000

    dssim: update 2.10.0 bottle.

--- Formula/dssim.rb
+++ Formula/dssim.rb
@@ -6,6 +6,7 @@ class Dssim < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d8eb6a36277a6dc7943aa09d07360e1b6f020074b8f0e98a67673d6542f77973" => :catalina
     sha256 "c03454b7582f988de44af524a342e016b3bf63937496450365c758809e79a224" => :mojave
     sha256 "f1284c6d8db31ce548232d0e7c0d4204249555ee5a9c92958b2c8bea4deee832" => :high_sierra
     sha256 "61c6019667ce76ccc145381eea2131ee3119366b1d54d2878da908eca8f85339" => :sierra
