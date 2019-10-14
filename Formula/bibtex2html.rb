535c344d09eda0b413a552d956d04ea2ac654ba2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 07:25:04 +0000

    bibtex2html: update 1.99 bottle.

--- Formula/bibtex2html.rb
+++ Formula/bibtex2html.rb
@@ -7,6 +7,7 @@ class Bibtex2html < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "63c9c1f00863d64097c681fce1ee380de9120de9a4ede1a534b6bc0452e94f31" => :catalina
     sha256 "25f6eb3f580bf87ffe7453a3d98fdd8040ac9e2845fcf1985ed5805f3eff98b8" => :mojave
     sha256 "ae155ef0c5f2d07f2559d0b4d661b1b0bf5f96546053f623dec074aee9415b71" => :high_sierra
     sha256 "269cbbac8dbae6a4e20e06df609d27fe60cc62ea176dd495eabeb2f9518fa736" => :sierra
