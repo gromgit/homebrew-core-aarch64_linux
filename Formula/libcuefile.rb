6daae5cf753875639945a23b85104237186ad27d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 21:09:17 +0000

    libcuefile: update r475 bottle.

--- Formula/libcuefile.rb
+++ Formula/libcuefile.rb
@@ -8,6 +8,7 @@ class Libcuefile < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "3069cf9b0261d8cedee8979348227f5c77a5c6dcb8942f9fbea20b3e3f190374" => :catalina
     sha256 "1e64fe68ce178b904ac44a7a2c017a030c6f0ff87fb18b7c943c8c766f23d186" => :mojave
     sha256 "a0b9b31c26ac9dc2704e71834259c0f9d0a12dce4ad4bbcdaae64fea5004ceae" => :high_sierra
     sha256 "66ec2d9281a5459326a1b2d220b9f68fa241a6b9f8370324377af6751d60b7fd" => :sierra
