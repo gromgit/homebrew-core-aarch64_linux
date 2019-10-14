38d47c8b4e6e45450be4f893ce5dbb8d4e27dd7d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 22:38:21 +0000

    shmcat: update 1.9 bottle.

--- Formula/shmcat.rb
+++ Formula/shmcat.rb
@@ -6,6 +6,7 @@ class Shmcat < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f86090c36d839092913667dcfc924f76c71d318a03434a1e608b3960b1df7807" => :catalina
     sha256 "e052a4f6b21407c032c1ee5a79fe9b1c08e78b7980c1cd3d6bfbfa8ffe639a58" => :mojave
     sha256 "ff73e6df8b663b4f382098ce75a9ec4634d4658c5378b3ad122de135e30d44ab" => :high_sierra
     sha256 "5ee7bcafe69d653421e29b56cf2e48a55874dc1e092e817a83cb446cda4acf01" => :sierra
