768f2bc31a5a33f1ba52189ed9b47369c517a917
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 19:51:09 +0000

    libpst: update 0.6.72_3 bottle.

--- Formula/libpst.rb
+++ Formula/libpst.rb
@@ -7,6 +7,7 @@ class Libpst < Formula
 
   bottle do
     cellar :any
+    sha256 "deebf5c542e32c4db5a6409743c2e20d55b3151e96a999962990c6d8c9c7ce39" => :catalina
     sha256 "713575b82c8c6121fb24b6e81f3db9c97269ce36b7437bf627005aca52adbd0c" => :mojave
     sha256 "53d20866aae36d6c27f70b87ca5ebdc95fce3812c0f7867ace75195851cb9255" => :high_sierra
     sha256 "ba6f9f3cc335802c9dd31f7098c71c06adcc014ac4de0f821cd823956a6839fc" => :sierra
