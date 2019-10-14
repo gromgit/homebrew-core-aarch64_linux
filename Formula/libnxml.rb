dec63f8ec75da103a84454bfa012d021e3159d01
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 19:14:42 +0000

    libnxml: update 0.18.3 bottle.

--- Formula/libnxml.rb
+++ Formula/libnxml.rb
@@ -7,6 +7,7 @@ class Libnxml < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "af92d830dbb7a103cd5a512c03c1cf2777742ea72c998ecbf1fc80912679cb47" => :catalina
     sha256 "61e076a06cab737a7410a8a2adf9c29c3d32e44467caaef25d54c7be63093bd6" => :mojave
     sha256 "a6b51b3ed4d09a603b7d232040b7e53fb26013a16ea9b4b86f415c45200faf43" => :high_sierra
     sha256 "ddeb6f19f803f29eb44f498ed687dd76a5bdeb0b6416c67759e1690ab9fa4f14" => :sierra
