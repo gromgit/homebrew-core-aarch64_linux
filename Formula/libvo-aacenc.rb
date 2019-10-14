f5be2ade7647f208538da84b5cf0fbcc33fcc2b5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 20:34:59 +0000

    libvo-aacenc: update 0.1.3 bottle.

--- Formula/libvo-aacenc.rb
+++ Formula/libvo-aacenc.rb
@@ -6,6 +6,7 @@ class LibvoAacenc < Formula
 
   bottle do
     cellar :any
+    sha256 "144e0c345d0567a74aba09cfec49fba8f409e2db5abcee96b598127cc5d722ad" => :catalina
     sha256 "099ac7d191241c476ab4ba7375dd05e2d965adc6a7638cc616a99a243cbd077b" => :mojave
     sha256 "761ecbbaaa2a944d077040692fc62fe2e929ec788ca7e23b3fb25e6ee1b88d3a" => :high_sierra
     sha256 "9430e86c9f25aa9fcccf0a19cc6125c9397c23b311b993b1adf83cbe330cd9b4" => :sierra
