545edaf7b9771f56efd4ff62b01c990460678b24
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 08:17:06 +0000

    x11vnc: update 0.9.16 bottle.

--- Formula/x11vnc.rb
+++ Formula/x11vnc.rb
@@ -6,6 +6,7 @@ class X11vnc < Formula
 
   bottle do
     cellar :any
+    sha256 "66ddb190e2e2a183ba662d4c7ac2de508b6ebe3c3c827078a5eec5b550477e5e" => :catalina
     sha256 "cd3d5d0047a8fb2e7b66ac94baf08c2da16aa8e135b8180acacce2d1bf366e58" => :mojave
     sha256 "2660aa48f9545eef71c5a42f9985720629d0391eaef37155264ec4c71cf13b29" => :high_sierra
     sha256 "4e974a6cbc6bd9c03e90ed2f991a40c4589489ccbd01bd20552bf0a66773f924" => :sierra
