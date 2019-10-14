f3e8b3d5132777e103fb8de17423010acc204cf9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 21:21:39 +0000

    libdvbpsi: update 1.3.3 bottle.

--- Formula/libdvbpsi.rb
+++ Formula/libdvbpsi.rb
@@ -6,6 +6,7 @@ class Libdvbpsi < Formula
 
   bottle do
     cellar :any
+    sha256 "b6e6f300bbc36fabf785f74abb083c5cfc3f91fdd51ee7bd058cc579e709c78d" => :catalina
     sha256 "26298540d01f52628385c83cac4b6666543af4cc059fa7ad5b3a8bd458955628" => :mojave
     sha256 "c6d79686bf05346bc473cc148b68901d99ac447a85542ff68d089c71eda1bc87" => :high_sierra
     sha256 "8bb1f1fff61674756153e8aec744d5d3c726da0c4ecd4bd291cae732e8264af3" => :sierra
