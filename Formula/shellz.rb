850cc5f42d38171ab878a47df41394e16d51849f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 22:38:21 +0000

    shellz: update 1.5.0 bottle.

--- Formula/shellz.rb
+++ Formula/shellz.rb
@@ -6,6 +6,7 @@ class Shellz < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1c1eabfee3228f25f75b4838f3d0a8a49e84c87eb2926e78cdf05dff094aa0e8" => :catalina
     sha256 "aa5043471c26fba80ba9db128f5ff3e8b60051bd76a8d26c3ad114b59b24c8b3" => :mojave
     sha256 "83b7e5e52243efe75e302853574243667a8e9cf9899d480c12c27886e77a9788" => :high_sierra
     sha256 "b659a90bd79e516d71679e68d36a35038937f23ee9d1de1dfee313fd11b0169e" => :sierra
