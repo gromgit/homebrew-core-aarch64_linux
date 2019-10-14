2c4eac5d4def7b6088fe1201b5a953b2b6fe3071
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 13:23:01 +0000

    fetch-crl: update 3.0.20 bottle.

--- Formula/fetch-crl.rb
+++ Formula/fetch-crl.rb
@@ -6,6 +6,7 @@ class FetchCrl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5a6f936f6411b77ae9a337573b8515cd812fc5a1531f8f23e5b81f67cf6746a6" => :catalina
     sha256 "b29839aecb3c2baccaea03af66504fe993fa590dda06d608b40ffb64697d0125" => :mojave
     sha256 "d267b22108f0174da30e56d8d1346b9d581c0ab98a48885c1c5e668368c47598" => :high_sierra
     sha256 "d267b22108f0174da30e56d8d1346b9d581c0ab98a48885c1c5e668368c47598" => :sierra
