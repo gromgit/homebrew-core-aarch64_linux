7b89d6009a2a613fbb5ba21485648b4899d4e81e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 19:57:38 +0000

    libsixel: update 1.8.2 bottle.

--- Formula/libsixel.rb
+++ Formula/libsixel.rb
@@ -6,6 +6,7 @@ class Libsixel < Formula
 
   bottle do
     cellar :any
+    sha256 "91a56eaeeb48be9347ed3ef0618d0c89c1e966b2ae88be40c6b40d70c6b5504b" => :catalina
     sha256 "d4cb5ca4c127d45f7727dc0c8c4e527f537484f437922ed9fac74c5f628543f1" => :mojave
     sha256 "0e6e7c7fea1d75cd9fbbb3ef1e67f9c2d9daeff592808d2392a8e517f555536b" => :high_sierra
     sha256 "0847829121008f75c987d932ef91866d4d201fabe6533953a476f17be2379714" => :sierra
