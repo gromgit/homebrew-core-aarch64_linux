dddf7254a01ea78b223204684c3576bfc3a69fbf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 14:36:51 +0000

    frobtads: update 1.2.4 bottle.

--- Formula/frobtads.rb
+++ Formula/frobtads.rb
@@ -5,6 +5,7 @@ class Frobtads < Formula
   sha256 "705be5849293844f499a85280e793941b0eacb362b90d49d85ae8308e4c5b63c"
 
   bottle do
+    sha256 "7c35ab7ec92f7d4c50fee6265aef35c83766dd0999612fa3de926bf61966cd2c" => :catalina
     sha256 "1f930caa2b88fb90d0cc1938397be4e66e8b43835773ddbedff9c891fae12e59" => :mojave
     sha256 "af5706f2616c0be86e6cfbed57ba560fa2bbdcb8b59c769c0c3e800552d51829" => :high_sierra
     sha256 "d3c660cd331b2a35ef36f55210e50e05e98d06fe3e5d606205ba63d226625f2b" => :sierra
