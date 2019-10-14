474558e56e8ae776b05df2209adc7930d99ddc63
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 22:09:13 +0000

    scs: update 2.1.1 bottle.

--- Formula/scs.rb
+++ Formula/scs.rb
@@ -6,6 +6,7 @@ class Scs < Formula
 
   bottle do
     cellar :any
+    sha256 "75459dcf470d2685ee0d27ce63e4bf39367a6e8ab12478379cf77cf0455dcab7" => :catalina
     sha256 "790a8b317c31bed14703a624c2c10ac21d6b5a79e49a9602cf17d4fcfda0206a" => :mojave
     sha256 "972212da980f70df69900f0af8115022cf12eb1dbad0c74a2c65b83b9c22daba" => :high_sierra
     sha256 "9c4f346e1a76e4951c6eb12324e40f08a3b52671872401091d01e456d715455f" => :sierra
