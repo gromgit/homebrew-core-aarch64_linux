fda936f723c0cf60659a15a2d004d4c7f093f719
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 17:37:52 +0000

    libfixbuf: update 2.3.1_1 bottle.

--- Formula/libfixbuf.rb
+++ Formula/libfixbuf.rb
@@ -6,6 +6,7 @@ class Libfixbuf < Formula
   revision 1
 
   bottle do
+    sha256 "835aeb4089c8add19f4c9c56be373559f15c7c5134bc555988cab4244c9f9ca4" => :catalina
     sha256 "72d1509d9b0bea98eb09e2fd2aaaff75a2a8bddbef035ee5c244578ccf247832" => :mojave
     sha256 "a7bd2c731284fd783a6b1cc74efc0d7b3e3668b6b02afc33ce307aba8a2b3845" => :high_sierra
     sha256 "cd6ba9552c12bfb738591876977a2470c6132c3fa649d542f9a9a064ffa64488" => :sierra
