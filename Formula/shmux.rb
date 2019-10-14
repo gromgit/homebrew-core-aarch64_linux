d55273c63d12e2c3c9d08b6284df54a890b2052a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 22:38:20 +0000

    shmux: update 1.0.3 bottle.

--- Formula/shmux.rb
+++ Formula/shmux.rb
@@ -6,6 +6,7 @@ class Shmux < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6781e9876911d4d44080b069dd3295c86520699ae24b3385980d51a53bc4d2f3" => :catalina
     sha256 "e433bd14622d3f77a35042649d0d73e888b164ab4f04431864fb68c9ec64b62c" => :mojave
     sha256 "bc38ad3a6feddd116edd9d3ab00ac18bc6663d08b9d111414975bdd1543d1b79" => :high_sierra
     sha256 "13f8831248e646784dd3cefd82707c45966ea05528e0c836156dea98b9c8c870" => :sierra
