7fba1e8b2552764a74783f0376b5c87b15569d29
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 17:34:18 +0000

    libestr: update 0.1.11 bottle.

--- Formula/libestr.rb
+++ Formula/libestr.rb
@@ -6,6 +6,7 @@ class Libestr < Formula
 
   bottle do
     cellar :any
+    sha256 "f539c76e3acdd0a93def55a0e82ecf45c53de65dc6dc18fd123efe815d8a65cd" => :catalina
     sha256 "543dcd541a69d52d5d1d21d51d0cf57c1617cc177f743c2dfea8ea3d548b93e8" => :mojave
     sha256 "7f17c5dbb6534afe6b37ae1d1f994d3387cd8527d6aaa768604837ac681eee59" => :high_sierra
     sha256 "5ff130cf6aa42842636dd90b7a8e7e60adbb289682bd915c98937b032c38fc54" => :sierra
