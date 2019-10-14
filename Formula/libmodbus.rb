6435d6754b9059024979e82a69b7072264b43a59
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 18:54:33 +0000

    libmodbus: update 3.1.6 bottle.

--- Formula/libmodbus.rb
+++ Formula/libmodbus.rb
@@ -6,6 +6,7 @@ class Libmodbus < Formula
 
   bottle do
     cellar :any
+    sha256 "dd69c46175e27a8a79938c12dbdcbdb67d3d1b74a0c4ad067a394eaa7a5869ca" => :catalina
     sha256 "0dd32373b5988d09317728357780b3cdc54cad290afcb51b390f33a50fa653e0" => :mojave
     sha256 "d821c77d787cf51c96811c37374d5cc42c7948636c84d7b7b6251a7e0afa29a0" => :high_sierra
     sha256 "462dd4e48e87bab1e48ef64f3f3a907392994a9f8c0753c8b739ca5207882b32" => :sierra
