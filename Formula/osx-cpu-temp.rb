716ce0e9ac89ca5d489c7849604f74fc75b071b8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 08:24:44 +0000

    osx-cpu-temp: update 1.1.0 bottle.

--- Formula/osx-cpu-temp.rb
+++ Formula/osx-cpu-temp.rb
@@ -6,6 +6,7 @@ class OsxCpuTemp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e1df41402ed817941f591a5cc094fe4491b092de8d5177dd363eccecff811bec" => :catalina
     sha256 "c0301d2c47c23bc8ed0042fbaf447e82ca8dbbf10b1939d9a4f684961a24d0d2" => :mojave
     sha256 "2255aa28242ce07a62fc0eabaf146592fb70745e641cfc775a21f99841cec625" => :high_sierra
     sha256 "d68a47b126eaee8f75d281785322877055187f89540eb2744b9cd4da15ca6a69" => :sierra
