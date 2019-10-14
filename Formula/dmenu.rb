84a349e7d30f1de36f32a0d95b8817b8da7dc72f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 09:44:18 +0000

    dmenu: update 4.9 bottle.

--- Formula/dmenu.rb
+++ Formula/dmenu.rb
@@ -7,6 +7,7 @@ class Dmenu < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6d5bd66279c7595ce553efc719503205af487e300623fce212e1155db370f6b5" => :catalina
     sha256 "44e6f96de8f8dd18389c17b99f65a9632cb134b183512ddbd05a542279005f84" => :mojave
     sha256 "297b8b591ee33d1a4f8100de2de275ca15268a906c77c1b4123d0787deb2cab4" => :high_sierra
     sha256 "2fe7512953f6e5099a4a624d8ebc6a3e83bda0753eafa7bb7f2942db90d21e62" => :sierra
