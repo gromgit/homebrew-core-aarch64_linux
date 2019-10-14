3369808c6680f3fb4eb80fb4080a82ef72ac9452
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 16:24:52 +0000

    ficy: update 1.0.21 bottle.

--- Formula/ficy.rb
+++ Formula/ficy.rb
@@ -7,6 +7,7 @@ class Ficy < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "eae06b7876eea9fcaa55315522e96b3346cdd4316fd4c329a200fd681d382efd" => :catalina
     sha256 "fe5ec58f592b05a79f1a1f867ceedf29b342c10659bb9167cef924ddc9260d72" => :mojave
     sha256 "a47243a3eddfb8b8aa8ddb337de2ec09b80385bcdf6922a858a50d4a5d79cc47" => :high_sierra
     sha256 "c8e04a4eb2cf74a46ed02c14c18bb13b06dcdc8703f5913744e904492efe64d8" => :sierra
