cf6c262c6b992d182d0ce582847898a28b51eab1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 15:25:49 +0000

    exult: update 1.4.9rc1+r7520 bottle.

--- Formula/exult.rb
+++ Formula/exult.rb
@@ -7,6 +7,7 @@ class Exult < Formula
 
   bottle do
     rebuild 1
+    sha256 "4a0df0a28c993f5e52c51de40e0b4194e73ea1869ca1277dda10e8793258c3c6" => :catalina
     sha256 "43967db7a4ff32b78f7478c920eeaf1c730a11952462d9b5bcc2d5b8ee27b932" => :mojave
     sha256 "642d16cef7ecf374ff50e10b32497f2744468010ee452e3e5819cc698215f8dc" => :high_sierra
     sha256 "01c7906864324d3ffe1ce9a11ba7bb60093c379e07d15aab2822e0bdd4789cc3" => :sierra
