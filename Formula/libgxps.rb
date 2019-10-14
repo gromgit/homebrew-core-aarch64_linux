dba019fead6f62aa557cd4da0f4a9b0c8f22bf61
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 18:02:53 +0000

    libgxps: update 0.3.1 bottle.

--- Formula/libgxps.rb
+++ Formula/libgxps.rb
@@ -5,6 +5,7 @@ class Libgxps < Formula
   sha256 "1a939fc8fcea9471b7eca46b1ac90cff89a30d26f65c7c9a375a4bf91223fa94"
 
   bottle do
+    sha256 "74f4662c40830e4697255a05447171637c749ce28a6aa7262ab29cbfca32d642" => :catalina
     sha256 "f2a0b5d1e9d85ad84ab041381c3c0bdd8f6f98d2be8586cdf48958e4a85aab27" => :mojave
     sha256 "ef0b670a2dcbbbc2e42e6dd9ada96353fad1d039c42d8a41ef5271b8370bdd25" => :high_sierra
     sha256 "949980a659fa2c301992af86c54a91e47a9721e372348bb1e5b49891d55b5285" => :sierra
