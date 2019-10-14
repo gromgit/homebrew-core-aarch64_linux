1efd5d09c49f4c6fbefef31cf77f699c94691a26
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 09:30:45 +0000

    di: update 4.47.1 bottle.

--- Formula/di.rb
+++ Formula/di.rb
@@ -6,6 +6,7 @@ class Di < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "199334727c2f4c35df0637885a99f8fb06a7d5b438af2444d7903dc135e0bd34" => :catalina
     sha256 "ac66afaf91adbe32a27b6f9f2642abe2f72f2be4e89cd135c0b24bf5d29363e5" => :mojave
     sha256 "d56258c488485a6d10789cd051687d94dd60974fce12d575af0766254588392d" => :high_sierra
     sha256 "4d351a2e919590ff239fad5871ce20001cf6246af03fac69a3f721bf89140a8d" => :sierra
