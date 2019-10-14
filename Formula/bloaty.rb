b8bf9ac71f7fadeb53d21440290273b5d12ecac7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 07:26:58 +0000

    bloaty: update 1.0 bottle.

--- Formula/bloaty.rb
+++ Formula/bloaty.rb
@@ -6,6 +6,7 @@ class Bloaty < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "437f08da84822e2912af5db09437afb2f214df97443dd10d66c289e933fce1b1" => :catalina
     sha256 "e0eaa91cfad61274a41ffb94f335cf61ad8217e9d47eff8bb77a390d7bcdb165" => :mojave
     sha256 "1c06ab5eb36a968ba9755028760f1dfc4958273325e5d243d9562cb571912fa6" => :high_sierra
     sha256 "aa8aa6c63e5cd626ce78146912c8abf79d2ed110d3ed0501482a88f421dce4b5" => :sierra
