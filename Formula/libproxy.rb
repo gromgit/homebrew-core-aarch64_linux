9e48cb576ecbb7b024fb38ab6149bd2123adccc8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 19:37:43 +0000

    libproxy: update 0.4.15_1 bottle.

--- Formula/libproxy.rb
+++ Formula/libproxy.rb
@@ -7,6 +7,7 @@ class Libproxy < Formula
   head "https://github.com/libproxy/libproxy.git"
 
   bottle do
+    sha256 "85c0981635696e9160b6f69ed4396609a4542ad478fdd3ac6b5b43b3c25bee07" => :catalina
     sha256 "2bd92529540425a786f17b2b2cc10423394c53a6120bbfa7a8d1df29b0617818" => :mojave
     sha256 "1da068be3ea931eda7ac2f58c8db57d9169d299bc5d57c70b8a455decd351931" => :high_sierra
     sha256 "e848c71f0cdd15d30a2f8003188883f4cf395034447d8b8db4909db5d51904ea" => :sierra
