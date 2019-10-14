2956baee924a6fca6ab31a5267540fc7ffc94837
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 11:02:38 +0000

    datetime-fortran: update 1.6.1 bottle.

--- Formula/datetime-fortran.rb
+++ Formula/datetime-fortran.rb
@@ -6,6 +6,7 @@ class DatetimeFortran < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "590d12115f8204c0d8de5300ea910bcfc30801645edb7b7fd3502202c85e933e" => :catalina
     sha256 "160cac6100d26f5e09387daac49d16c40aedd38a44dc5aa0da950a62fc4f601b" => :mojave
     sha256 "2b3663b87374f2aff2bbe1ddc6436a2e387cb0f158149fcc11f46b73aa530e89" => :high_sierra
     sha256 "16073e773f18e30155af5805975b085fbd9c715cf0dcfe006cd1091de97c2cc5" => :sierra
