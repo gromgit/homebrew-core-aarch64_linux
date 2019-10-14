23edb6e4dc41dce5ca89fd96b1570c82c5ade215
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 19:03:16 +0000

    libnids: update 1.24_1 bottle.

--- Formula/libnids.rb
+++ Formula/libnids.rb
@@ -7,6 +7,7 @@ class Libnids < Formula
 
   bottle do
     cellar :any
+    sha256 "3439fed4035b6ed2eb7cc491514ba2ac108fdf57fec9fb18e14e0efb1b375913" => :catalina
     sha256 "d42b4d49f5b6e4b465cbed83337adcc218d32c99c754b89b249d8f2c675a02f9" => :mojave
     sha256 "35f886a96ce255259d1cef139939cf760f3fc3b3b35664f9a59e9a8bdf27f072" => :high_sierra
     sha256 "b0ab2ca7fe6339f9ce06b6916e6205792a62b123e3887f7e372a9ed3a6ce85ed" => :sierra
