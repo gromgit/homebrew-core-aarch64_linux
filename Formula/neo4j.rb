9096f82a112202fbffb925b42e7072262047dc90
Chris Farrell <7016191+seajaysec@users.noreply.github.com>
2019-10-14 18:33:18 -0400

    neo4j 3.5.11
    Updated target URL and sha256sum. Verified working locally on macOS Catalina.
    
    Closes #45338.
    
    Signed-off-by: Rui Chen <rchen@meetup.com>

--- Formula/neo4j.rb
+++ Formula/neo4j.rb
@@ -1,8 +1,8 @@
 class Neo4j < Formula
   desc "Robust (fully ACID) transactional property graph database"
   homepage "https://neo4j.com/"
-  url "https://neo4j.com/artifact.php?name=neo4j-community-3.5.9-unix.tar.gz"
-  sha256 "cf0e6c6e9733cda11922a4a060e53269ac05b6e55cb7817c55621e005928f6cf"
+  url "https://neo4j.com/artifact.php?name=neo4j-community-3.5.11-unix.tar.gz"
+  sha256 "4dd4f2b6c32e216b42ab8d2235f10c4d992d567d36927df93d2d9fb1763e6376"
 
   bottle :unneeded
 
