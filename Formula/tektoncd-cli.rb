7d4a89794ea68b5b4a6e89e3a0624ef25fbef40c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 20:21:05 +0000

    tektoncd-cli: add 0.4.0 bottle.

--- Formula/tektoncd-cli.rb
+++ Formula/tektoncd-cli.rb
@@ -5,6 +5,13 @@ class TektoncdCli < Formula
 
   sha256 "5fec9adb2c17e004eab757a83d52dacc41b58a08d16fc5f1d6382c2cd474a0d2"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "97205827393ba9f2c9de2b3a002c3e18474f7570352d07442212c57d85b560f8" => :catalina
+    sha256 "8e4a430598fbb4d68fd60c3147fef4cb582b23a7842670556cb9710e8f417008" => :mojave
+    sha256 "65bb6ba4faaa0e99fa924360f837787f29e5e1b53c13fae02308c15e59379863" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
