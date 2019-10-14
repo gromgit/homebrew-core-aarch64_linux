8bee88c2d0010adc014fd1d5f36f2acf5771b98c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 23:25:15 +0000

    slowhttptest: update 1.7_1 bottle.

--- Formula/slowhttptest.rb
+++ Formula/slowhttptest.rb
@@ -8,6 +8,7 @@ class Slowhttptest < Formula
 
   bottle do
     cellar :any
+    sha256 "87ffd6f13e4643eba8434b08287bbb370fea6a6f38383c579c92cb11a3b8e689" => :catalina
     sha256 "b9cc1a26d74eed668f3f337e0bd5a639e7017c5b9b50ec90c68290f56efe946d" => :mojave
     sha256 "e99ac598ee7bce8c956cac5db78bbde906266debee14f9586e72f933e8a471a0" => :high_sierra
     sha256 "8c1228a2dfd57a36124947ddde4981493c52ca5f94b48649afacc7c94b9f32bb" => :sierra
