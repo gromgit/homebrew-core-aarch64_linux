8c9cd4ddb222b6d5e773f1f493d4807bcbd00735
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 11:33:55 +0000

    eccodes: update 2.13.1 bottle.

--- Formula/eccodes.rb
+++ Formula/eccodes.rb
@@ -5,6 +5,7 @@ class Eccodes < Formula
   sha256 "158021539a85ca597075f4a534117721ea51f5ddea44b5ec27519d553c980f8d"
 
   bottle do
+    sha256 "d9f55032952d2ec77fc71fa1e10a2fc0c8eb61035f13cf2964f91191fe510c30" => :catalina
     sha256 "e2176627399b50a2ff590e4bc4eb46992eac5535e5bc3614d6c6e52a8fbe781c" => :mojave
     sha256 "80af243cbb5b7c42529ef8d4efa783f57b8d9e8f4dad7367842715c922aec4a5" => :high_sierra
     sha256 "2a16c2a52726f0164f2c70f9ba56e0ebd8ed5738e0990ec085f07e65a08af7ea" => :sierra
