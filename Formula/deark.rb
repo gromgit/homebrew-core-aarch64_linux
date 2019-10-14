476c3c2b87502abd741296eb4acc526afe6123e2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 09:00:36 +0000

    deark: update 1.5.2 bottle.

--- Formula/deark.rb
+++ Formula/deark.rb
@@ -8,6 +8,7 @@ class Deark < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c26268ffce7476c73cb93e7fa4683f4efb864913199588851860a1cf4abc392b" => :catalina
     sha256 "16bd12bfa6d0ca8f192a1b7a8febf411cae588b1887d18ca2c48dcbe46357fe4" => :mojave
     sha256 "f7fbcf71cf7ce9f6e4f402e8a22b3a11bf213790fb82c2fb1865da9aac78671d" => :high_sierra
     sha256 "41adf7c83aeae1c945d3007e860993e9f8845f6a1f7c239b86327958b9ed470a" => :sierra
