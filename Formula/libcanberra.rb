72cf9574773055112e1b97a3a081b0eb78384834
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 17:18:57 +0000

    libcanberra: update 0.30 bottle.

--- Formula/libcanberra.rb
+++ Formula/libcanberra.rb
@@ -5,12 +5,11 @@ class Libcanberra < Formula
   sha256 "c2b671e67e0c288a69fc33dc1b6f1b534d07882c2aceed37004bf48c601afa72"
 
   bottle do
-    sha256 "63be18f829bb64854eff3c78b7fc75a0bda59dcffa510f86f776734b178a8cc0" => :mojave
-    sha256 "c30a642b46b4da40690fd0c49b77d9ca2cea2c95d8e1f2e39f92d655b9b324c6" => :high_sierra
-    sha256 "066b33288fa7e3741ef45fc2d59578b4f1cf8141b5e6f9847635555a3436ef00" => :sierra
-    sha256 "dc9f8e76c81bb21afedd4deb9b9d9bbf3382f012192e5796658845e2083ca018" => :el_capitan
-    sha256 "3d820f8c9747e8658c226483267536c76b3dfdf44a9e14ea1327cb5cf0385ba8" => :yosemite
-    sha256 "10c6f3c931a349848b6436d10893e9b1bc5051658f125f6b67ddc1f9319396a0" => :mavericks
+    cellar :any
+    rebuild 1
+    sha256 "34ff83c6dc8af0afc1f1988ebde1ccb4c17d4604fa6d36567daedef43da3047d" => :catalina
+    sha256 "3d32a254ac069ef41b785f6950e3eea625de6faaf99d2402236b451f8c765b05" => :mojave
+    sha256 "561aa9aba4e6b5f191b74d3dd1c96de9951e3dc5b696d93abaeaa301aa117bae" => :high_sierra
   end
 
   head do
