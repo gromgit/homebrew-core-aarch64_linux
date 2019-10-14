a71be698de6488b51e906c8d58529194b06ad2cf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 14:16:31 +0000

    foma: update 0.9.18 bottle.

--- Formula/foma.rb
+++ Formula/foma.rb
@@ -6,6 +6,7 @@ class Foma < Formula
 
   bottle do
     cellar :any
+    sha256 "cac241ad5d3039dc6debbeeb2fbd635f5078a11030f1f170dd3841633efb0ddc" => :catalina
     sha256 "a4d787025fe8d3c90cddf4255bd93156a77075e5fc7b11f1849ef01867b0d861" => :mojave
     sha256 "852b24482bd36775a871647d905411d0a4d69d5dead9d116890be4118f6388e1" => :high_sierra
     sha256 "b47b293d11dafa179d2c7a384336d2affc19e574aaecfe206026fa17de112328" => :sierra
