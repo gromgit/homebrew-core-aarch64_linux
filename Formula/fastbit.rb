d7802d0b17876595f2caab605babed2cb17c425d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 15:41:35 +0000

    fastbit: update 2.0.3 bottle.

--- Formula/fastbit.rb
+++ Formula/fastbit.rb
@@ -6,6 +6,7 @@ class Fastbit < Formula
 
   bottle do
     cellar :any
+    sha256 "2c72f2aa131632844a265fb3cef214701ab04618e77eadceeff70107a064155a" => :catalina
     sha256 "fa55894d8decb82e8731d1af1541aa17569cf34691c1b23272525e5cd6ea66d3" => :mojave
     sha256 "10b30f7face5fec9926fcc84b1acda1e11edbaf6438ddde8d819a431766adc98" => :high_sierra
     sha256 "688a35ebf6323a6323181db1c8fd048c00031b8c3d89f49b2d81586576723541" => :sierra
