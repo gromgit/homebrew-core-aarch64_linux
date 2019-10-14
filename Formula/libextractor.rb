a2d5789caf8946b8a218a4911b0957d0f7b7afa9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 17:34:18 +0000

    libextractor: update 1.9 bottle.

--- Formula/libextractor.rb
+++ Formula/libextractor.rb
@@ -6,6 +6,7 @@ class Libextractor < Formula
   sha256 "f08f257d26c5e9b503f068d6753c8e55cb76f47f73a81da6ed2bba3de3fee2ff"
 
   bottle do
+    sha256 "05f3281584bd5a64cee246033510bdd0e04674785576db58759c1c5ddb8acc6d" => :catalina
     sha256 "7e0ed720f6c76d148affdccfdffd70285114e03063ef10711ab207ba52d753f7" => :mojave
     sha256 "3feba61ae58271eb373e0f215e5e857e1709b7ae6cfeb222d7be8bf302b8a219" => :high_sierra
     sha256 "5ae5f5a6335e1bc2f2c9d5a9d6206cadd99de6da11594eb87e52ca148bedd1b0" => :sierra
