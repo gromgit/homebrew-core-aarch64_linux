d93cf5c3085af5f2231407f81ed0348903edc722
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 11:47:40 +0000

    entityx: update 1.3.0 bottle.

--- Formula/entityx.rb
+++ Formula/entityx.rb
@@ -6,6 +6,7 @@ class Entityx < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8e0e5b8ed56eaca89dadc59a78ede051c1f6eded8b7a9996fe33393e4d14bd0e" => :catalina
     sha256 "5d2b3d80d9be39b08b61003fe0f8c30bf8aec792636b78e475fbbbb55d3e01a7" => :mojave
     sha256 "b015609cd7e4ad7154e846a34e91627a605983ab3e3f1767df5ccf7e46cc9d10" => :high_sierra
     sha256 "d0ecde656ac88f1f312d69894a32330827cd52ac64a7e20d1357a0a9bbe8d596" => :sierra
