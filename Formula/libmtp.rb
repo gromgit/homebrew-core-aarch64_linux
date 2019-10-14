0c77dad26f1c159fc06bb5575d87b4df3764067a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 19:00:58 +0000

    libmtp: update 1.1.16 bottle.

--- Formula/libmtp.rb
+++ Formula/libmtp.rb
@@ -6,6 +6,7 @@ class Libmtp < Formula
 
   bottle do
     cellar :any
+    sha256 "edba32848630d4e88dfe923ff910b9b606c3a06499cb931893a74fa65b045e27" => :catalina
     sha256 "787180c3be62277d66b70c5d362e98ad1906648ddd6be83714f243eb2eaf4347" => :mojave
     sha256 "db8b67e377fc8297a135bf63df9945b7101627bc2d10d79c5054d20a3b9b6856" => :high_sierra
     sha256 "daeed59855f2f12403175cee945ebd11b4de41494af53618f70de779ef483939" => :sierra
