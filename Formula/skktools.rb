227968eaafe0da669e8d58c542a17e7ee6d23826
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 23:22:11 +0000

    skktools: update 1.3.4_1 bottle.

--- Formula/skktools.rb
+++ Formula/skktools.rb
@@ -7,6 +7,7 @@ class Skktools < Formula
 
   bottle do
     cellar :any
+    sha256 "c10f7baf6bc379ed47fc82b5bf1dd2ae8cbc53c358f5c838e87d8440ae8c5ebd" => :catalina
     sha256 "e3e3033a8f0d05939c9ebe2ab73cff7377351a36b8276337c920026c27d0a127" => :mojave
     sha256 "65826868e8d1ca3d310ebc14e2aadc281b09751220e3310643b4e2aa4ab9ee68" => :high_sierra
     sha256 "b190a2b730f1604ac76725e99c5fd9927ed5a7cbba905291f017b77fee1e3c85" => :sierra
