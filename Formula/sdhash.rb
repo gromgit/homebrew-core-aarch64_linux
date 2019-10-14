b544d25a327f6b16bb5f0f0896c697fb69df3c15
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 22:20:20 +0000

    sdhash: update 3.1_2 bottle.

--- Formula/sdhash.rb
+++ Formula/sdhash.rb
@@ -7,6 +7,7 @@ class Sdhash < Formula
 
   bottle do
     cellar :any
+    sha256 "2b9a824cc177fe984f61eb756447e77f947b993317c6c5909fc5e56f52d7ab5f" => :catalina
     sha256 "ff3ebba92126bd9ac537d4c6b7b0e818ca1318eb41fe410d9cbbbf7efde68ac2" => :mojave
     sha256 "81064cc409fb71b361a4be539ccf8d014dc5adf0186d666d00025d2109ff5168" => :high_sierra
     sha256 "3d019e14266847dcfa7fa27f69ffa4aea25cc78a2ff62c1883a2a8c74fa02116" => :sierra
