e679eef1fca752c0ad82b1cc268f585c4b048962
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 14:17:46 +0000

    fossil: update 2.9_1 bottle.

--- Formula/fossil.rb
+++ Formula/fossil.rb
@@ -8,6 +8,7 @@ class Fossil < Formula
 
   bottle do
     cellar :any
+    sha256 "9477508505b77e23d3e20387141b1dd98ea384b768283bec8f3b4ae161220e6a" => :catalina
     sha256 "c876f19f2fe8bd1d92360437baee0e3887a7598497328f408b5ad84d5ffe6696" => :mojave
     sha256 "8b3b9b01b25196cef89e25e95b0eda93976a43fc3f525f77cc4353e492142091" => :high_sierra
     sha256 "52de169a2cc7dcf4eff56e9553184f1790f98ee1bd21b367aeb83d1c637c862e" => :sierra
