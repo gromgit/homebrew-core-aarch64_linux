b15e3f57b4dca3d60b103810e729e8439d25dd24
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 11:46:30 +0000

    emp: update 0.13.0 bottle.

--- Formula/emp.rb
+++ Formula/emp.rb
@@ -6,6 +6,7 @@ class Emp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8c4bca6eca037bbef2b1a65d1974b43b36c81274e20597a76e87703ec477ee1a" => :catalina
     sha256 "33eafe903efc393c0964ac05ab684508b98e72a4ee2f26272ee16eee159cd514" => :mojave
     sha256 "d96c6b3f2ee49480ddc0dac10484284e7620dce5499482bdaf12c26f42f93a13" => :high_sierra
     sha256 "2a45cd98d7345ff1872137576f97a028729ff4c0d62994d1ce6d573e3835e9db" => :sierra
