7046d2f1f8392e0987a7e93e98f4b33ea96d10fd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 08:13:04 +0000

    cd-discid: update 1.4_2 bottle.

--- Formula/cd-discid.rb
+++ Formula/cd-discid.rb
@@ -16,6 +16,7 @@ class CdDiscid < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0a9f85136e9727175a4d861f759236d62cf24f19170e27bfd9bf8aeddbc4c8b3" => :catalina
     sha256 "158d91563b2e79574c0a336f775b49033d85ce3b290f122dae853dea45841f5b" => :mojave
     sha256 "26b88be0312f960484625161d94adf9a44aa88ef5817ba28b61af520a6e17e03" => :high_sierra
     sha256 "6b0d9c55a1adfce8a2c6e9eabd00c37118a05b60678564e7a9695d876bca117b" => :sierra
