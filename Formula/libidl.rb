885a5184255a1cf907f4ef3fff2462c0903e5eaa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 18:19:53 +0000

    libidl: update 0.8.14_1 bottle.

--- Formula/libidl.rb
+++ Formula/libidl.rb
@@ -7,6 +7,7 @@ class Libidl < Formula
 
   bottle do
     cellar :any
+    sha256 "fc384a7b4357147c85196b681bd1a96a70e2a7e194c38b6e8afbef5bafc21efb" => :catalina
     sha256 "6221a3b0ea37b55c26bc1f83c84ce3e027a8925b92d63055a51fe3a7d6bdff19" => :mojave
     sha256 "9b07bec68567266f1bc065b05afdb9b034c0c70548145d7cdd963b5958c8da30" => :high_sierra
     sha256 "ecabcc1a9cd229a135557f0f8bc32a38d03d399ff6816b0fc897cc4bcf72cd1c" => :sierra
