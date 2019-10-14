af5c6ddd267542c7b6c64af38b587d26beabe407
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 12:02:50 +0000

    envv: update 1.7 bottle.

--- Formula/envv.rb
+++ Formula/envv.rb
@@ -6,6 +6,7 @@ class Envv < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "54b7b425a3db83134fc9038b8672bd84a943413f5386d9cef92711eeaaade467" => :catalina
     sha256 "59acc1f13ed58898376a14ffcb23766f62ff7c0446eebb3ee8aa1f8162f0994c" => :mojave
     sha256 "35e2781067a3f5429c36546a20faca9d4762882bf3908122efc58c8b752968e9" => :high_sierra
     sha256 "cc30a2317f78124c609d6313a33cea58c9d428a95903966da4cb42051630ef97" => :sierra
