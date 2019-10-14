9c2c627969efad14d2dee286df436b0c15e74ced
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 09:17:42 +0000

    deheader: update 1.6 bottle.

--- Formula/deheader.rb
+++ Formula/deheader.rb
@@ -8,6 +8,7 @@ class Deheader < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "84bb08cf8c454272c7bfa95f530dc8f8bc15af43748631704dc7321f2989226c" => :catalina
     sha256 "cef55345cf5a32b30aa7d9a320b3143eb05b5f8329a8abbfad2a247622ab233f" => :mojave
     sha256 "cf2689471e033bb6e55cd948679eef30c9abecb17f022fc6cb914fbe0ca85c6d" => :high_sierra
     sha256 "2b70a9eb18042a3e93ab8fc1bf018c417d8b41f9b8efe6d818d45aed6922cf52" => :sierra
