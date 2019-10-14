94a9aa872198be24946bb7034f7936a87216b3f0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 16:24:52 +0000

    fizsh: update 1.0.9 bottle.

--- Formula/fizsh.rb
+++ Formula/fizsh.rb
@@ -7,6 +7,7 @@ class Fizsh < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c1cb674d5a7219ea4c41f3becd0997cf55dfd26a06a17d739f14a3d59da7df75" => :catalina
     sha256 "02457429b5257b916207bc7f46acd5577f8e01183437ef03b594991ba3e69466" => :mojave
     sha256 "7916e571aaf891561a5a6be1ef9708e63ee17ecb41fe60b75129c765d3dad1cb" => :high_sierra
     sha256 "334ceff9d649e87f87be9d3f561ee38221f8c87712a3b506b145191dc51fc4bd" => :sierra
