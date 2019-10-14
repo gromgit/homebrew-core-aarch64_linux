95eb889a6c1e6ccda6b6407d3513d341b5657b23
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 13:23:01 +0000

    fex: update 2.0.0 bottle.

--- Formula/fex.rb
+++ Formula/fex.rb
@@ -6,6 +6,7 @@ class Fex < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ddc12a1eefc9238e48ba57f00694e6b01cdce2ef41bcf34ddfd405696d3a7a65" => :catalina
     sha256 "c3daa86f0f51e3bd7be8cd890a46bd2a50e6a0fb728f664ce1847edb6b5f7f0c" => :mojave
     sha256 "1b293789f75a67d36037f9d80641814119c8f4534a78dbf321744276a41f2c15" => :high_sierra
     sha256 "e42328824017f1432ace562fdd70061e504c5524d2702f3d4b470c40b1bf105e" => :sierra
