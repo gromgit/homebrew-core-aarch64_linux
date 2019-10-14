985dfa92212437a9a323ff037797de1a177676b7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 17:37:53 +0000

    libfreefare: update 0.4.0_2 bottle.

--- Formula/libfreefare.rb
+++ Formula/libfreefare.rb
@@ -7,6 +7,7 @@ class Libfreefare < Formula
 
   bottle do
     cellar :any
+    sha256 "e9432f6847992d848ab9ff220e5f7309672933a857987b1f49d5533c0c998af5" => :catalina
     sha256 "cb3dedfc77ded9e44680a7c7f68ab9fc20d7cb2042bde58494968f011354eb74" => :mojave
     sha256 "d3d40b88ac6049ea3eb2f3afc96dee6608425bd46526b5e15f264c9b9c8fba99" => :high_sierra
     sha256 "4f75161d80be443a364a7d594dd9c627d6c253edbb856241dff1f9b73af3fec3" => :sierra
