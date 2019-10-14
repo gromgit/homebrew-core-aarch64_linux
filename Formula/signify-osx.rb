a39f1053651f5cfb6efeb9997ea0ca44915af93a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 23:04:20 +0000

    signify-osx: update 1.4 bottle.

--- Formula/signify-osx.rb
+++ Formula/signify-osx.rb
@@ -7,6 +7,7 @@ class SignifyOsx < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "74a8c2fa3d258ad59a5ab7302411a194903ea5295fbf5ecd95a43c2ac28677f4" => :catalina
     sha256 "842a6fb535ce56db38ca545fd229f184850e34211c7817879f707f71fe6b31d0" => :mojave
     sha256 "cdb1896e5e480edfb6ad7f179d9a2b217cda774039fcf5922bc3eba9b6d3d1bb" => :high_sierra
     sha256 "fdac23b07368d6c8ebad06c2b8451f0c8228f71f5c65b48d672cfd581b222509" => :sierra
