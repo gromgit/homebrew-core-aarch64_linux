f995f7364e1413fceece5e2ca61a79b4b2cf1b6b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 20:01:14 +0000

    libsquish: update 1.15 bottle.

--- Formula/libsquish.rb
+++ Formula/libsquish.rb
@@ -6,6 +6,7 @@ class Libsquish < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a3f9fc5e20792dd4ced369a1be063a01c873afc399c8c73bb8800db1777ce6fb" => :catalina
     sha256 "d3a42b4342fab6548ec4e2467a571631edf0891d1c4c51a6573b14afda5b0972" => :mojave
     sha256 "734574ea8d63c2d52f291c1d3c96e18ee7fb6f404b5039245a31625fcb6277df" => :high_sierra
     sha256 "ebfa2b4d94a71334548800ceb00803ed1ed1e91226f6892048f376d73ee7ef74" => :sierra
