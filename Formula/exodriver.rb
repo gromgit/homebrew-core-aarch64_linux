8dfbe2947d534b5c43cee53710d9ace463d55036
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 12:24:43 +0000

    exodriver: update 2.5.3 bottle.

--- Formula/exodriver.rb
+++ Formula/exodriver.rb
@@ -8,6 +8,7 @@ class Exodriver < Formula
   bottle do
     cellar :any
     rebuild 2
+    sha256 "d0b269dc59c28dd739a3596d300de53b195efbc0a897fc15993526fe8cf9416a" => :catalina
     sha256 "2fae34af7c35f396fd6cc26143da484be3ca4bf51a900a76650096bbe40adb5c" => :mojave
     sha256 "2be616189c54c4d1046b8d8fbddfb6366a64149f37927ed692413c154cbdae96" => :high_sierra
     sha256 "7d02fce0526573c60aebe7a30e0b3b60d114cc95f4e59f0575b508b65409f187" => :sierra
