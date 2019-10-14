419c39f50123d658012053b213ea8083beed4205
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 19:03:16 +0000

    libmxml: update 3.1 bottle.

--- Formula/libmxml.rb
+++ Formula/libmxml.rb
@@ -7,6 +7,7 @@ class Libmxml < Formula
 
   bottle do
     cellar :any
+    sha256 "e386b406006d568647f83d92268685f47c3ce72995ea843b6fb456947ef669ef" => :catalina
     sha256 "f8e186285e66c760f033ab4205cfa5d05a48d3b5ac2a668c0f3cd4572c0fd151" => :mojave
     sha256 "bf35de7007c525ef4e179ec3e89df8656b9a206f9390df068585361d90cbd3b6" => :high_sierra
     sha256 "044434b96bcf9a3097e28c4e85fa5e1e558f2b2dc62c7e8eba6363c664924b68" => :sierra
