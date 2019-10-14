c00d74dcca26d9cc7d9dea6ca6ff53cd387a54fb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 23:22:10 +0000

    sipp: update 3.6.0 bottle.

--- Formula/sipp.rb
+++ Formula/sipp.rb
@@ -6,6 +6,7 @@ class Sipp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6e475cbb06a46ae80606b12897928b601b8280fc16e1467c395687a2a93317b1" => :catalina
     sha256 "23616329b56c55eb022b055d31dadfdc79fafe5fc224cd342106a1a59afa46dd" => :mojave
     sha256 "822893f07f76031fcd3c0410e2d682dc743c9ac1a9b438361dd1a6a9b6a767b8" => :high_sierra
     sha256 "79f72467ed25cb8c6db260a9fa3e8f130058424b980d9daa07f79fce77159296" => :sierra
