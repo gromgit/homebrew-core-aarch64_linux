3858e04b12bb0fd7fb28c7c55fc304373f8395bd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 12:13:20 +0000

    etl: update 1.2.2 bottle.

--- Formula/etl.rb
+++ Formula/etl.rb
@@ -6,6 +6,7 @@ class Etl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1ff4de15ba9b82ef2afe44be648f1c42031d7bc4e9e1538e3e1951cfa353ecaa" => :catalina
     sha256 "b775dfeb3634c3b4ff3828239250394328b5c971e472cb775a0590d94bcdc6f8" => :mojave
     sha256 "b775dfeb3634c3b4ff3828239250394328b5c971e472cb775a0590d94bcdc6f8" => :high_sierra
     sha256 "507d4f4b35d0e075869446600e36e0f9f382014e99bf16a07d77f2c256cbc594" => :sierra
