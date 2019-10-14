1bb3a260dc1048130e1c9e0361956f44aedce358
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 09:19:12 +0000

    dfc: update 3.1.1_1 bottle.

--- Formula/dfc.rb
+++ Formula/dfc.rb
@@ -7,6 +7,7 @@ class Dfc < Formula
   head "https://github.com/Rolinh/dfc.git"
 
   bottle do
+    sha256 "315767ea4838836254830a63f2b10c34faae0ae1f0757c7e6212832da409dc15" => :catalina
     sha256 "1a313424cdf9c4eecd2f9c343f8218da48bec1cf1da3585038e0b0d7742d5247" => :mojave
     sha256 "6729cbd05c951477c251e240afc01f6a1cc4ab04441f653194388a6dcf048d13" => :high_sierra
     sha256 "158a1dc96381a8c13a38aa6682120c5f6985ee2a71bf511eba5b99c32d6ab9e4" => :sierra
