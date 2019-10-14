1c9bac00f7182681ee182bcd378ba66d3bf8e89f
Thierno BARRY <thierno.barry@polyconseil.fr>
2019-10-14 14:32:34 +0200

    terragrunt 0.20.4
    Closes #45313.
    
    Signed-off-by: Rui Chen <rchen@meetup.com>

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -2,8 +2,8 @@ class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state"
   homepage "https://github.com/gruntwork-io/terragrunt"
   url "https://github.com/gruntwork-io/terragrunt.git",
-    :tag      => "v0.20.3",
-    :revision => "54f09ca195ad97e8f21a66369cfdf8413862d720"
+    :tag      => "v0.20.4",
+    :revision => "0dcbc14aca6ed85b46c518bc16585651a3db4d6e"
 
   bottle do
     cellar :any_skip_relocation
