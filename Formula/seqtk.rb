84048e48928c32a3526c34501a546e9956347294
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 22:20:19 +0000

    seqtk: update 1.3 bottle.

--- Formula/seqtk.rb
+++ Formula/seqtk.rb
@@ -6,6 +6,7 @@ class Seqtk < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5abbf374f3dab69b198b98a3126f521b64baf01ac5ed69b99be91ffd97f891f8" => :catalina
     sha256 "b695a43103700d7d0d4a07d50d8effec280f7d7a781ff518a42dec2bef44801e" => :mojave
     sha256 "4f377caf93e5d334e739375a5dcf06782f1d85516988a26df3f8f53d172b1e6f" => :high_sierra
     sha256 "fd3ecced5ba8f5a9eab13f8f2184f6a69d08b58c1ef53ad6e74bb45cab9324f4" => :sierra
