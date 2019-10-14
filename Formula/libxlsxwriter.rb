f41c6b86f7801d1c04501fd97e4327b08afcde3e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 20:36:57 +0000

    libxlsxwriter: update 0.8.7 bottle.

--- Formula/libxlsxwriter.rb
+++ Formula/libxlsxwriter.rb
@@ -6,6 +6,7 @@ class Libxlsxwriter < Formula
 
   bottle do
     cellar :any
+    sha256 "a0108b3fd598b490b4705dc3087ce97f02f1635b57604e90571acbd86e0ec9a9" => :catalina
     sha256 "032f628bfb43e3fa5108db3feadd521a51b8c4494bf3e3235d7840648fee1334" => :mojave
     sha256 "c63a03bfdb9284391d5081300d037aea88e9cec0e888e014998dd121d6343013" => :high_sierra
     sha256 "212ed6216f90977a3cd0bb173a51f6061263db2a3391a3c3fcf0d09df80ed176" => :sierra
