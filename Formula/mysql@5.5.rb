bba83fcb844c82c291aa80fb919cec2f77b8d959
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 00:46:11 +0000

    mysql@5.5: update 5.5.62 bottle.

--- Formula/mysql@5.5.rb
+++ Formula/mysql@5.5.rb
@@ -6,6 +6,7 @@ class MysqlAT55 < Formula
 
   bottle do
     rebuild 2
+    sha256 "9769598f3cd623c3b6a0c63f42e31c19c1ff843fd94d056aacc8478563b2ae85" => :catalina
     sha256 "1b4f8a3d3faa62bebccfa7b0bc19cf33eb5aae8b32af06acbd2ac20aa715d32f" => :mojave
     sha256 "cb28f6a3ef4d6eb84b5e6825a6764b383400e0061eca694d77336d1952333e68" => :high_sierra
     sha256 "46331ab927515e6897c67c1219f4e00a3b61b15c4adbc21898fe23dea2c51cfc" => :sierra
