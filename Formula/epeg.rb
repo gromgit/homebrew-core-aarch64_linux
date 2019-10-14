1cbceb4cf01bc51ccdf6121f673e4b21a190f526
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 12:02:50 +0000

    epeg: update 0.9.2_1 bottle.

--- Formula/epeg.rb
+++ Formula/epeg.rb
@@ -8,6 +8,7 @@ class Epeg < Formula
 
   bottle do
     cellar :any
+    sha256 "1ba1e2980210accf42548e0eac13ad41469bd282a273e2a1d177dbb16e39e140" => :catalina
     sha256 "05651d71e9f7fbb8f65b4f15e8037392bde8062ec8419535eaf4d213cacea3de" => :mojave
     sha256 "8ca494e4c2131e0b9c9e02199a26998f7f14e47cf00da9fbe7a5e75891d5fb94" => :high_sierra
     sha256 "a7d1777cff7684385a5a7d9c524a26e6f6509c80a638fadc99b6db84b96b1636" => :sierra
