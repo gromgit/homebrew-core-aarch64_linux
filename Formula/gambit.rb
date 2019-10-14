2ad869d843c01f7c16933d1ed25baadcad81f701
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 20:53:29 +0000

    gambit: update 15.1.1 bottle.

--- Formula/gambit.rb
+++ Formula/gambit.rb
@@ -6,6 +6,7 @@ class Gambit < Formula
 
   bottle do
     cellar :any
+    sha256 "85c73c5372aa6bba15f651f9b0b5b3e5eaa790c83ba19159de8a0e8bca0e60fc" => :catalina
     sha256 "697370ba59d7b4a8e7f35e5bbfc3535ed6fef6d0a80a39e9e0583e7496865fbf" => :mojave
     sha256 "8419c4b938c3ee82df324d4e4fbc0de03302a973ebeb82b917c90be04fa10569" => :high_sierra
     sha256 "1d0324bc5c27cfe5660be48a8d2e7c9d01ac342e5d3ee1ee6d89f30b9c149afa" => :sierra
