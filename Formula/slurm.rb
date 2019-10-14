3e3bbd6450a692cc3be43f3a8010988a948603c1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 23:49:01 +0000

    slurm: update 0.4.3 bottle.

--- Formula/slurm.rb
+++ Formula/slurm.rb
@@ -6,6 +6,7 @@ class Slurm < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7c177b599a6612e1b6e7f68dfe44dc7f23b71788548eab184b33d03d5a5d8da8" => :catalina
     sha256 "1877e60b9696aca27dce408c18113108ae08611914a120c9fc52a59db9eec99a" => :mojave
     sha256 "97f41cff81bbc7ee1d0f9599e7b697d97834343a7a867497b5920246f836a327" => :high_sierra
     sha256 "03f2d26fda7d44d9853f4e24ca0cd28b7096ec174ea6de731234bdb7d7742f88" => :sierra
