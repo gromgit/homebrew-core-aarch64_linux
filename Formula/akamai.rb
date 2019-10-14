87e45a5dbf40db5fb1c1342d9c98a6848620bf2c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 00:13:00 +0000

    akamai: update 1.1.4 bottle.

--- Formula/akamai.rb
+++ Formula/akamai.rb
@@ -6,6 +6,7 @@ class Akamai < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "65d232aa3ff2f8a3af7a8f3d74c80539ce493bc45e2e0763e5509ad3e3ba399b" => :catalina
     sha256 "e9850383e5d94f1e8c5c1b813cdb735a81aa0e5d073e23d797cebc6e895fe96d" => :mojave
     sha256 "cf2b12e909cf2b622e2c2913dc5b20d86a45a6fa3f5b7de551dd30cde142d3df" => :high_sierra
     sha256 "a79cf0cbf309d832059bfa4a62be000ac39eb12220491ad3d93e7ca7086fcf4c" => :sierra
