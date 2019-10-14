9b346a40703fda095e83d4bb2d6aa0da7aaca9e1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 14:53:00 +0000

    fuzzy-find: update 0.6.0 bottle.

--- Formula/fuzzy-find.rb
+++ Formula/fuzzy-find.rb
@@ -8,6 +8,7 @@ class FuzzyFind < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4e58e0ac23df5dbd26787238c0160716db8eb673b4a62625a9edcb4ceaf38eac" => :catalina
     sha256 "1a7bddb6228630cd27bd08863e41eaa01211dae1e5d409dd4ea9777c1599057d" => :mojave
     sha256 "b3f47d3bfd49f76960e5979fd4ef898c848e73f7f8da758b71c7eefe3f585fe0" => :high_sierra
     sha256 "feefa3913b9b1df2d8b283fdd55abd1de9ee924633d8e157142cce4980572ffb" => :sierra
