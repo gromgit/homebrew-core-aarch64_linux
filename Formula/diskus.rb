3471210dc4f1a6caf206a9cf16084158a7c2ae31
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 11:07:54 +0000

    diskus: update 0.6.0 bottle.

--- Formula/diskus.rb
+++ Formula/diskus.rb
@@ -6,6 +6,7 @@ class Diskus < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0240a6a42c4c18fdcf0e578cb84c0dc8f532320df6a2f223891e5cada82cd477" => :catalina
     sha256 "09f3ecb398c323353ea7d32d54691504eeacca1b01c494cc232bbf33a2040c0c" => :mojave
     sha256 "ed4bce4e6350c6968696a27faa2271185739a546c1e402d76a8fcae50fbf4ea4" => :high_sierra
     sha256 "b995363af6e1952e299db83d007de1b03d07f288c650aecec04a34b56a83926b" => :sierra
