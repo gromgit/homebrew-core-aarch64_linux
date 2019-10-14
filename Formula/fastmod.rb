4e4db05d574cde79a2878b1d7d403e614bc86882
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 13:00:01 +0000

    fastmod: update 0.2.6 bottle.

--- Formula/fastmod.rb
+++ Formula/fastmod.rb
@@ -6,6 +6,7 @@ class Fastmod < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2cda181606bdc1998ccc727dbc2f8f6eaaec5a1fb1b09f186056789ecd96b235" => :catalina
     sha256 "0c3aab08129fcffdc2f92046da56402337ea35498bc0ce06a5de94c5e0194deb" => :mojave
     sha256 "f1baa87a1abcee4adbed888b370f0528515ed2db2dd889c98b6a0b1c118fb575" => :high_sierra
     sha256 "854e92c3a2cb41ae6be702aab770ecf295e86c806e6af6f9baa770ba102d4598" => :sierra
