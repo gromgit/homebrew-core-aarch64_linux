6f60b0a2c85eec07b488070ea3ae9257dc32c7e9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 18:02:53 +0000

    libhdhomerun: update 20190621 bottle.

--- Formula/libhdhomerun.rb
+++ Formula/libhdhomerun.rb
@@ -6,6 +6,7 @@ class Libhdhomerun < Formula
 
   bottle do
     cellar :any
+    sha256 "5716a80e73a3d25e11141cb13126a3e36b373fc2a744bb66082fb5b89e629957" => :catalina
     sha256 "9a82dc21efebb0bafb6313e4f29f6aaa3a142347fd5e5e7ba7350433e2531722" => :mojave
     sha256 "f3a120a52ef9e9399395c6a4a91098b73ea8e4a8889e21b4646283011534fea8" => :high_sierra
     sha256 "47d1adc4fcbf5218c074d27a8db604a4f2ab4e5f3d5f14e023497e9cad5b49b4" => :sierra
