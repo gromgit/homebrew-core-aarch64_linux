9efd0d228f5c2fe7b8432b47cb57a85c82f69630
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 09:44:17 +0000

    dmg2img: update 1.6.7_1 bottle.

--- Formula/dmg2img.rb
+++ Formula/dmg2img.rb
@@ -7,6 +7,7 @@ class Dmg2img < Formula
 
   bottle do
     cellar :any
+    sha256 "e16b42ead321d5e0c85a98592154ef13a2206355a13cfe021735653a1dd995be" => :catalina
     sha256 "fb90741dc01f5c7b115c9d5bf142e36a90d7cf0995ecb4a5183150ec6d6161ac" => :mojave
     sha256 "367ab961e50114debc983e5665443ee8fa5a85a2b4fab024753f38df48fb26f1" => :high_sierra
     sha256 "8616423fd5b0109c66a000932b2aa5bf4f3979c5a065617e8ef7dd4ae0ee820b" => :sierra
