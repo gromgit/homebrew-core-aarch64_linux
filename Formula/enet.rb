e67395f078f05be278da48124dc53f65984479fe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 11:46:30 +0000

    enet: update 1.3.14 bottle.

--- Formula/enet.rb
+++ Formula/enet.rb
@@ -6,6 +6,7 @@ class Enet < Formula
 
   bottle do
     cellar :any
+    sha256 "fd907590e904f76351db367069a3d28bec7ab9cb946f66a01c22102e8c9bd951" => :catalina
     sha256 "3813772ea57875407a2f949e2aaa3d9330a25e443efc231bd6aeb6b6af213001" => :mojave
     sha256 "25754aa22b0d3862dc2b0d355452c040022af50a99af5f98b39e46151aca43e1" => :high_sierra
     sha256 "d7a0e4e8189f600db8f657aa46622b20ec5dd67facf06dd370d8185ff4714b27" => :sierra
