d92a9967d545982c19f6d3cc5c0149529800d622
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 22:09:13 +0000

    sd: update 0.6.5 bottle.

--- Formula/sd.rb
+++ Formula/sd.rb
@@ -6,6 +6,7 @@ class Sd < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a2270c4330760007e369fc9da739abacc3744e9a9cc987dc90fc7fc830d36ec8" => :catalina
     sha256 "bae6022d6efa72ce65337baa1be8a1b5d569905c13958be7bab9191239123297" => :mojave
     sha256 "404bbedfd604f51148f33dd9441dd93611def5ddf93c91466f44921507353e4a" => :high_sierra
     sha256 "010317dec38d6fead368fbc2b3d26c9eab2a0ea6d833ccc419484c76989e55e8" => :sierra
