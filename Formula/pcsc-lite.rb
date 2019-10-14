202fc0195fa0476ccc114d2a6b4cbea25b31f5c6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 00:32:00 +0000

    pcsc-lite: update 1.8.25 bottle.

--- Formula/pcsc-lite.rb
+++ Formula/pcsc-lite.rb
@@ -6,6 +6,7 @@ class PcscLite < Formula
 
   bottle do
     cellar :any
+    sha256 "d4fdefb11d6fcfb20ffb8aac8be408d4c1041fd981caabc42a6d482fd980ca62" => :catalina
     sha256 "29ebb59b42af0959efe85ca374d03cd51984b9966c3be2ed51c8ae30098e0ea2" => :mojave
     sha256 "832957657fec785b6d157a6a670da607675bdef8655d82c3a16fc39e305e5e57" => :high_sierra
     sha256 "92fb7438f0467c2f749218cb8b23fa1bf66425fb8f49b20888530fb97094598f" => :sierra
