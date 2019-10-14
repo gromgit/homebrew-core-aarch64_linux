d3e26b83333244871ab0b6e88dcf8ef08dee81ae
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 22:20:19 +0000

    sdcv: update 0.5.2_2 bottle.

--- Formula/sdcv.rb
+++ Formula/sdcv.rb
@@ -8,6 +8,7 @@ class Sdcv < Formula
   head "https://github.com/Dushistov/sdcv.git"
 
   bottle do
+    sha256 "88e35b34c8965ea668da4aeb0e96e90c6e1c8a400e8c119936ebb5180c0afce3" => :catalina
     sha256 "d40dc4d63326ca5268f5b1d75a8a64ecc0081fe6e35d6279ee937774b39cfb33" => :mojave
     sha256 "322ec61f8ae7914f8a8f5da7fac01ef0ee69aa9f9844e1c876168f9fdf54887a" => :high_sierra
     sha256 "e0aa6779b49ae274e5a9da62a12caceaf2149b587d96ce051bd5c7d735c81fa4" => :sierra
