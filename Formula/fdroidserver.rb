7655c3a541dbab893e538aba0ce7ca608703f0c8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 16:10:57 +0000

    fdroidserver: update 1.1.5_1 bottle.

--- Formula/fdroidserver.rb
+++ Formula/fdroidserver.rb
@@ -9,6 +9,7 @@ class Fdroidserver < Formula
 
   bottle do
     cellar :any
+    sha256 "1499c1250def2fbb425b3bf228a2bc0a88944e912e4311fbeb9eb2a0e603bd34" => :catalina
     sha256 "ad45cfa9f6e48be44bc6de975db7bb9fd1b1f7f63d1b003106903f1f6706d4de" => :mojave
     sha256 "8dc42cea3f8b3ac0d5af85a6e8eaffdd175088db58ff1989ab254e132fca6214" => :high_sierra
     sha256 "1c03c755b24210f97b851a676ab58dfc52fd39a47d934f5ba5b47baffd4dacd5" => :sierra
