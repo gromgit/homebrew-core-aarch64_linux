4e9a28dda2c2effe8b7c39c7b60f0710df619b7a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 16:09:57 +0000

    flac123: update 0.0.12 bottle.

--- Formula/flac123.rb
+++ Formula/flac123.rb
@@ -6,6 +6,7 @@ class Flac123 < Formula
 
   bottle do
     cellar :any
+    sha256 "e9d6f0e34bf00197859eb997f353123f67a75d644ed9a3dba400207a83a18d6b" => :catalina
     sha256 "c1aa5158e16136453e09b384480a6aa4faaefc818c14243a0c4b5359cdab2fb4" => :mojave
     sha256 "ac4ee518533f4b043fd380d0ed6e2077ec410c16acdf952b733df533a4750889" => :high_sierra
     sha256 "f62d8e1f08e8cd5d952f02a35ebcdc921a1295035a2b66e843d80aacb8d9843e" => :sierra
