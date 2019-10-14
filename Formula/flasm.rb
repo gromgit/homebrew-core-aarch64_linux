0f5aee26e8200feb91782660f538a1e68415cbfa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 13:43:41 +0000

    flasm: update 1.62 bottle.

--- Formula/flasm.rb
+++ Formula/flasm.rb
@@ -7,6 +7,7 @@ class Flasm < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8b12bb8f245caede2fa69847f452e4d8cbf75d1e52ceaf168272239a318c528f" => :catalina
     sha256 "4f2ad8d2f363f8509f3ec3f97f73773a5b21b893f954da011c431feb3c65aa02" => :mojave
     sha256 "5461503d625b8b25339ac3a518c9079bfc92d4121760c61ef73f69020e2669b2" => :high_sierra
     sha256 "423b77912442f613cec430a8eee149783047f6b1a32d82d4b2920969fb6ca77e" => :sierra
