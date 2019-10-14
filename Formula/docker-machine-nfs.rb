911519a8369cb621bab39b2e324db7fda217c38d
chenrui <rchen@meetup.com>
2019-10-14 11:33:27 -0400

    docker-machine-nfs 0.5.3 (#45319)

--- Formula/docker-machine-nfs.rb
+++ Formula/docker-machine-nfs.rb
@@ -1,8 +1,8 @@
 class DockerMachineNfs < Formula
   desc "Activates NFS on docker-machine"
   homepage "https://github.com/adlogix/docker-machine-nfs"
-  url "https://github.com/adlogix/docker-machine-nfs/archive/0.5.2.tar.gz"
-  sha256 "58ac0384a6db4edd648534af5d11ef3d39d13b4b534b9a7070912504ec3601f6"
+  url "https://github.com/adlogix/docker-machine-nfs/archive/0.5.3.tar.gz"
+  sha256 "ad8fb4e9c3576ca5dd9f4014ff88a41e42c3c65c26c04a94122f79533050e0bb"
 
   bottle :unneeded
 
