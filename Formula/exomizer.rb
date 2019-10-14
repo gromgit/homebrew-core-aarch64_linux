3c7e33da5d1946396a825a782ab66b70d9d4f4cc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 12:24:44 +0000

    exomizer: update 3.0.2 bottle.

--- Formula/exomizer.rb
+++ Formula/exomizer.rb
@@ -6,6 +6,7 @@ class Exomizer < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b298240aec4d77e212b57666ef51ca125127da3ac5185166c6c96495e217358c" => :catalina
     sha256 "653ce86b30882fae1deae16bbf40b50f9b46b7f4f3f51e1952a988121e4fd5dc" => :mojave
     sha256 "46b2f9b190847344ab7a16c94eb4b2bad48009a8f08e5463427af75c9eb67409" => :high_sierra
     sha256 "6c6389b18ce3be2c7ffdb919e79273ecc8e26b9067bd06b29474d37c9e162e83" => :sierra
