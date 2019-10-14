b3482efd52666046b8f2c45c6aa50b5e936e7db1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 09:30:45 +0000

    diceware: update 0.9.6 bottle.

--- Formula/diceware.rb
+++ Formula/diceware.rb
@@ -8,6 +8,7 @@ class Diceware < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0dedd3230de6b269165e1b6bcc790ac1d97bfb01631ad648573ad66e6569e617" => :catalina
     sha256 "0337fa5b3f3b8975a3e98dcf2cc547f0b49362bba602c8ae0ba04c6bcd9775fd" => :mojave
     sha256 "860337c1b1054ebdab64426b10cdcc1b5f508bd327560f78273ccfba348d9e1e" => :high_sierra
     sha256 "7fd4218db14b490a0b2b0f5372c636dced0801829a16278f7751d3d831f79785" => :sierra
