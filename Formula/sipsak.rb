0c7a343b3d52383e898d1ae83c939bff98494506
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 23:22:11 +0000

    sipsak: update 0.9.7_1 bottle.

--- Formula/sipsak.rb
+++ Formula/sipsak.rb
@@ -7,6 +7,7 @@ class Sipsak < Formula
 
   bottle do
     cellar :any
+    sha256 "84d9694858c95f7ce07829e602be3d9727b00602ea69bf8e191f79f399f53597" => :catalina
     sha256 "5da0bb6fc866723e423714b275cbc6c64ac1e57f171cfe112bda6f9779385ae3" => :mojave
     sha256 "6cc0007fe520e225bc0a6b9b646817468acbc1383cb6a4382ae8bf1243ae3e5b" => :high_sierra
     sha256 "0d074a6356f127485211b79741fd7f45e7ed9435e4d708e539088a9e9154daea" => :sierra
