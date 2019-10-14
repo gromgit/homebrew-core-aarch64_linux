5afed5ecc86cc4cbe203616f3990ab4a3ce77010
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 10:55:07 +0000

    dynamips: update 0.2.21 bottle.

--- Formula/dynamips.rb
+++ Formula/dynamips.rb
@@ -6,6 +6,7 @@ class Dynamips < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ecf536589504e42389e91865495faa4eb30ba20adad56bc865c0481e80abefe4" => :catalina
     sha256 "db5398464afdb11af6f26cd4780f6e688bed0f35c9fea8f8308f11991987a037" => :mojave
     sha256 "cb9bf6eebd6a7987976e0e2543a807e1b0f16698a1c71eb64e7da56f320fd425" => :high_sierra
     sha256 "08b44502cd3b052592f11f5b75453fabd51fdcfe1a311405c4b7329a701dc424" => :sierra
