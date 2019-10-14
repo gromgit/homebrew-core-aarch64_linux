88ff701dcbd57e091c0fba7cd46272df7a3d7549
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 10:34:39 +0000

    dsocks: update 1.8 bottle.

--- Formula/dsocks.rb
+++ Formula/dsocks.rb
@@ -7,6 +7,7 @@ class Dsocks < Formula
 
   bottle do
     cellar :any
+    sha256 "d675be1f5c6a31c1fbb20dd8c521a638edca6ecfe13a6bb1f8db84b35a01178d" => :catalina
     sha256 "04977648b6805fb7e82c01064872c9a44356cc2b8499adde514aebe1687bfed8" => :mojave
     sha256 "c6f4212b4e925dc0d29b21f96ab244a8a6842ea44b72f3e48036e69d86ac4c93" => :high_sierra
     sha256 "896675fab1d6bf50e5ab9512041ab49fcf9af65198d93ec85c0f2c0d801df49d" => :sierra
