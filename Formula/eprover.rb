e5b106958e80620b8e6118002377b034831fa915
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 12:04:03 +0000

    eprover: update 2.3 bottle.

--- Formula/eprover.rb
+++ Formula/eprover.rb
@@ -6,6 +6,7 @@ class Eprover < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c281d0aebc16da93a003f2baf76cd3388f8458fba3cc88e38bc7556b54e1cdfd" => :catalina
     sha256 "464244b8f862c83abbf2b5969790af6f80fd26d21cd1b1d8c575b44c6c73c9ec" => :mojave
     sha256 "6f51683fc53c488f2d16fa3b6fab7ed30e7b4a6c99dd42331c29631120891c67" => :high_sierra
     sha256 "d18b5d1b173a6061bd90ecb590ce95ae864c562786376583d57029ce651060e4" => :sierra
