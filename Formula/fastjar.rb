6e34b40c973583318b810f3891a65df039325d3d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 13:00:00 +0000

    fastjar: update 0.98 bottle.

--- Formula/fastjar.rb
+++ Formula/fastjar.rb
@@ -6,6 +6,7 @@ class Fastjar < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ee758c76cb694c96ea30cb9e6ac204f2797c78be36610dcdf36c2a75301b5835" => :catalina
     sha256 "2dba61ec801db3d83692b9c9dd26eab247cb4bb6a6d6afc27f059bb6ba6052e5" => :mojave
     sha256 "87b2c870895191b309b595481f73346e763b87c661e64ef35b821e54395d5cc1" => :high_sierra
     sha256 "35230e788987e3a3c63d126af24c634bcbf58c0a320223d61f0eae69f6cbcc00" => :sierra
