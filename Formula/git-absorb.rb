08cc1e2330bf21ee8e0c0f93f54154d2406ccf38
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 12:00:53 +0000

    git-absorb: update 0.5.0 bottle.

--- Formula/git-absorb.rb
+++ Formula/git-absorb.rb
@@ -6,6 +6,7 @@ class GitAbsorb < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0eb9c509d984ad2345f3d5b3bd8e6fb28fb4590bc937350dbc39990452c9e638" => :catalina
     sha256 "4a8e3e6fa101306c8d99bf95634a7fbe08fcb80d46742161b972e9f5ab1031b5" => :mojave
     sha256 "6723320cdbcef35818dc58ce6e0fcd3be87606ae59fc355d9179e5690bc90540" => :high_sierra
     sha256 "d51c5da6ae0c17b416d0fb2bf1f043c7d0e3cb8535a47dc0afea56ef96a50f1f" => :sierra
