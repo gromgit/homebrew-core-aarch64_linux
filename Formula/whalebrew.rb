92c44b438308501aacae125e1d3caf1bb2430a35
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 20:04:57 +0000

    whalebrew: update 0.2.3 bottle.

--- Formula/whalebrew.rb
+++ Formula/whalebrew.rb
@@ -8,6 +8,7 @@ class Whalebrew < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3243317cffe305c9c21a458793f4319b9784e979cf3225663df3e83b53e1736f" => :catalina
     sha256 "9abfd2e5951f589dde2cc9847b44e77741785a8b9ff5208c958bd8233def74b8" => :mojave
     sha256 "66e059ff26ef8e1a7d15416bf0b3a946716e03e60cf7f1281e572dcd78b1f4d8" => :high_sierra
     sha256 "a6357b16b2285d65c523902fa90180f3b6e77b08196cf6edce3f32b38202ff54" => :sierra
