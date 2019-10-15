05e49d3bb53abf640560a66e3cca15367209efc9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 07:01:24 +0000

    stm32flash: update 0.5 bottle.

--- Formula/stm32flash.rb
+++ Formula/stm32flash.rb
@@ -6,6 +6,7 @@ class Stm32flash < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ad0d22f81963099a648b32697bf1a14ca3ee51cd45f8e73f0f701d5836faecee" => :catalina
     sha256 "14bbc585e618bf4c223e0008503f9007ef3686e3472a1a1f2bfc8122af42793c" => :mojave
     sha256 "2cbbf094a7f2777b674909a5f846bba0cb613a5c2c38e980b67bd769b924e5f5" => :high_sierra
     sha256 "74a92cff8b8099a2b8ee8aa0a2a360639400eb53a24b625c149b052e3f26521e" => :sierra
