01325fd106df87b4a3cfd886f7547d59a84965c0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 16:54:05 +0000

    yank: update 1.2.0 bottle.

--- Formula/yank.rb
+++ Formula/yank.rb
@@ -6,6 +6,7 @@ class Yank < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ef4da54ce9a56a1767b44dd88df6616c147730d74f390d5a661910dddf8785a7" => :catalina
     sha256 "60431f02c576c640597975986ce62f9d157c49f160d7d6e23f917dc321ca8bac" => :mojave
     sha256 "b87461e809f0bebd615d4da69c31509109de8f86d07d280dab07326293cc851f" => :high_sierra
     sha256 "70a5de45249c1656653733fea8d7a92c2496b9ba8e7540eef86b3f805d0e933a" => :sierra
