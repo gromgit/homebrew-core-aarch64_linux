9d4876468b23519c69643f8a1fa25b4f48afce39
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 00:43:02 +0000

    sqlite-analyzer: update 3.29.0 bottle.

--- Formula/sqlite-analyzer.rb
+++ Formula/sqlite-analyzer.rb
@@ -7,6 +7,7 @@ class SqliteAnalyzer < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2ccdf2c56a73f41fed176042dbc9c8adbff0fcedef00fe94f73b6a1fc3269e52" => :catalina
     sha256 "ad8628c564822e587747c49e7d7db2e10011af14077f47094662d29745a2d98b" => :mojave
     sha256 "45710ca091f16bcb148d92f53b1aad0e049d0d49836529add96aadfe5ec53d6a" => :high_sierra
     sha256 "0a7c045d97ffbb2e6f767c4ea55c1fa294cd4f1d7e6ae66ba126739e6cb0e223" => :sierra
