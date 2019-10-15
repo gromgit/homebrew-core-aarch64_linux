ebbd77216ae3fd1064dc629986f74622bbbd4b3f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 15:38:56 +0000

    gvp: update 0.3.0 bottle.

--- Formula/gvp.rb
+++ Formula/gvp.rb
@@ -6,6 +6,7 @@ class Gvp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5ac60db3ccf491e68595a4ae2185ea2acb1e733bbdd4dabe24617e7f19409790" => :catalina
     sha256 "dce646d2b2bbc9cfecbeb99360b0df2d1149b758a40af3ab81138b544a6e3871" => :mojave
     sha256 "c62a176dc8bee30dcd1453a8b3c608dcd059dc133167df74802d515931470f6d" => :high_sierra
     sha256 "2405a1e481ebfafcd4fbfdc2874feacc402b851fafdc69596d1afa120924c157" => :sierra
