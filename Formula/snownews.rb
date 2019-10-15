c350aad85a1a0c8e0dbbab816323437fac75b485
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 08:29:49 +0000

    snownews: update 1.6.10 bottle.

--- Formula/snownews.rb
+++ Formula/snownews.rb
@@ -5,6 +5,7 @@ class Snownews < Formula
   sha256 "8c78067aef75e283df4b3cca1c966587b6654e9e84a3e6e5eb8bdd5829799242"
 
   bottle do
+    sha256 "6829a661dc7dbd01b149a05aff6b81b4ecc22b99f3f06345f029b183bf952c64" => :catalina
     sha256 "db1d20c1468867a2eeb77b521fdfac4e167393797506afd6bbd603fb40c435bf" => :mojave
     sha256 "0e1eed435b2c94a95e35ee89c3b68be6cfa9867d75dc9fbb46b19a66ff6da915" => :high_sierra
     sha256 "6437a67fb63f92f3c4d57b69505cb5146e2d7325da0d6fd73b57ac0f1461c807" => :sierra
