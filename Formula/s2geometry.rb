6874b01b9ac25c9c47bf726b89b40bebc98b7851
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 08:15:13 +0000

    s2geometry: update 0.9.0_1 bottle.

--- Formula/s2geometry.rb
+++ Formula/s2geometry.rb
@@ -7,6 +7,7 @@ class S2geometry < Formula
 
   bottle do
     cellar :any
+    sha256 "69e0912c7a35acdcd926fa997e0c7e37ee1b8908f113b0d5a6ccc80c1bbe020c" => :catalina
     sha256 "5ecc3866aa3ad158fbb42b3d1b545d5c69a1ad5f1d5a574ae902de320d28d073" => :mojave
     sha256 "cfdbc5dd02ab2ddd7561342f6c225c8de8c86b5dcbf321265e33d3296a8b66f1" => :high_sierra
     sha256 "870466f63d2da435da772eff61412ea7a474bd72234f9e14e61deb850223791b" => :sierra
