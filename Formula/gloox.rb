d7c79375b871209b477db43aed99918a23729a54
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 13:11:54 +0000

    gloox: update 1.0.22_1 bottle.

--- Formula/gloox.rb
+++ Formula/gloox.rb
@@ -7,6 +7,7 @@ class Gloox < Formula
 
   bottle do
     cellar :any
+    sha256 "e47784efba1f772b982a2cc8456469c87f5ee42988b294d14b701cfcb9bcd1b6" => :catalina
     sha256 "4d7f5c002dc74b477a069fae22b7361371fa692a378a4186a508a92ecf24e7ad" => :mojave
     sha256 "53776432ab6f6637983b846f853a922fbe71e2d370f58457dc35eab30f3737bc" => :high_sierra
     sha256 "906216cebdc6ae1bf7e5f1a6eae8ae60fd5747f6832288a652f68a480d6dd42d" => :sierra
