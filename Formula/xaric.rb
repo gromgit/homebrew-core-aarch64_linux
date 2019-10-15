f89017354e9b2ec999a3afd8fd15005412af2798
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 17:56:43 +0000

    xaric: update 0.13.7 bottle.

--- Formula/xaric.rb
+++ Formula/xaric.rb
@@ -6,6 +6,7 @@ class Xaric < Formula
 
   bottle do
     rebuild 2
+    sha256 "b916b4f80429db2d487478f6bc9c53de07ea4bd23ada7f0deeb8f2a3fb7f1e1a" => :catalina
     sha256 "31cc50542222e8ceae4b46887441fdd73be022bab481cfa205d2aae558c0d95b" => :mojave
     sha256 "ba6e8b473868db28ea7d83417f1b4a5b4ccc6ed5b126653da6b1a3407d4b54fc" => :high_sierra
     sha256 "7a4074ad4ca9ed3950fb4badb76ae4ec9a7c9228fd65a7a4d7d8b89296af8d3b" => :sierra
