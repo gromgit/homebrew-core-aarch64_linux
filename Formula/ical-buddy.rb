cc5c17ac7e42a13663d46baa352466908e860e51
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 19:57:35 +0000

    ical-buddy: update 1.10.1 bottle.

--- Formula/ical-buddy.rb
+++ Formula/ical-buddy.rb
@@ -7,14 +7,9 @@ class IcalBuddy < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "720a6a3344ce32c2cab7c3d2b686ad8de8d9744b747ac48b275247ed54cb3945" => :catalina
-    sha256 "734aa2e1b61ef6d9ef8800ab5382a8e157a13631cf1eeacbbbb0cc80f1831a76" => :mojave
-    sha256 "fa01f750eeb692d11ae0fa4cb6131e331446ad623bc8fc284b3721cf81621f39" => :high_sierra
-    sha256 "8dc7ef559702a3c489b2905e5cfdd4c9be18decd9557c9034df920f0ef57761e" => :sierra
-    sha256 "1603d15b5b643a25c98baebc7c7e799bf3176a74a139a3f5dfecb474daf9037d" => :el_capitan
-    sha256 "b839dc603deeeaba18efd2c6704e174fc4593fbc4c100c8d655b70f327802407" => :yosemite
-    sha256 "44b1bb23023bf91a055f77232b0f2cdb2ad64dc389224a480e2236b308abf9a7" => :mavericks
+    sha256 "4f621e8b12e2c2e5e7c9fdd97ee973b7d4b14ce58eb5a5f7a9db32243f0f99f1" => :catalina
+    sha256 "4f621e8b12e2c2e5e7c9fdd97ee973b7d4b14ce58eb5a5f7a9db32243f0f99f1" => :mojave
+    sha256 "4f621e8b12e2c2e5e7c9fdd97ee973b7d4b14ce58eb5a5f7a9db32243f0f99f1" => :high_sierra
   end
 
   def install
