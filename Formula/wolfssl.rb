d909dd05ddb62d986dc648c9c8ba439b41004031
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 20:28:02 +0000

    wolfssl: update 4.1.0 bottle.

--- Formula/wolfssl.rb
+++ Formula/wolfssl.rb
@@ -9,6 +9,7 @@ class Wolfssl < Formula
 
   bottle do
     cellar :any
+    sha256 "7a7f8e0e186e4a7d95914442c719415839070d3d71ba8c53c3303d06d557a671" => :catalina
     sha256 "43bb226ebb919ab0f2f890b90bc2802cadc0cdf95b22d6e065d5ba646c0f7f33" => :mojave
     sha256 "d1e20d0c48d9a3ec0b20242960cdb5b5e17bed0fbd023bb5e59b0b46dfbedd68" => :high_sierra
     sha256 "a12383cdd9e68ca5a8a73a158fbaec733c9a7d9de550d4424d2f5723155115b5" => :sierra
