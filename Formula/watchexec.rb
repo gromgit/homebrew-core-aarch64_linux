63c2c93fc78bdcdc7772dd700bd1b37828bc1169
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 19:41:17 +0000

    watchexec: update 1.10.3 bottle.

--- Formula/watchexec.rb
+++ Formula/watchexec.rb
@@ -6,6 +6,7 @@ class Watchexec < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0ac159612e527bef2cc0cabdfd6eeef19263d0faba224371a63f4fdb568d2219" => :catalina
     sha256 "b09275b1c686404ea57e8c0b52c9f2f52b06bc9cbd6b61935e50ca280e902c1c" => :mojave
     sha256 "9bbda6ac49cb30495ec20ed084d08b636a526578a3ec1df8c7e6948c9b7105dc" => :high_sierra
     sha256 "eda530af03c418d4adbd80ae33922d4912df3eae4ecb5695c4692a21d4cc9af5" => :sierra
