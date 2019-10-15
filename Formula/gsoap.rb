ce1cfbd8a260924cb2b07d308e7267bf698e7b69
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 14:45:34 +0000

    gsoap: update 2.8.89_1 bottle.

--- Formula/gsoap.rb
+++ Formula/gsoap.rb
@@ -6,6 +6,7 @@ class Gsoap < Formula
   revision 1
 
   bottle do
+    sha256 "3a1e9b3f413512feaea7436654afef10fe166e57a4c6f9f181250792118d09ec" => :catalina
     sha256 "57b1ed5d8d8b6f38db767f957b75dd05bacb1c168da6f75f2202e9995580bede" => :mojave
     sha256 "4dc18439df3f8f79ecac7eb2438d8937f9f6c5919bdd383b945660c8a398c2c7" => :high_sierra
     sha256 "95d559743a7f807d298908e69517d9c2e11d1c3aa12282712b4b73ac41e12fb7" => :sierra
