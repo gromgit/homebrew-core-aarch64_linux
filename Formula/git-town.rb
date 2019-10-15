fc25111bc200672aa3f28caa33d70b6f821f2158
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 12:31:50 +0000

    git-town: update 7.2.1 bottle.

--- Formula/git-town.rb
+++ Formula/git-town.rb
@@ -6,6 +6,7 @@ class GitTown < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9d756be0c03322770b3ef08f71a356dcf3b7f9effa3f366d8818547a32d6629e" => :catalina
     sha256 "acdbaa473264c569f1d2f2cfe6c6486ff02f6af601e800e0cb0a350ef0c32154" => :mojave
     sha256 "b3be72d926d402afe2ae9b079bd702095729372b7a142d1ade13367a8415cf43" => :high_sierra
     sha256 "423e9c2afbde04439ce9689942b739806b5ae97941d92d9ae4d2f9c7f4295047" => :sierra
