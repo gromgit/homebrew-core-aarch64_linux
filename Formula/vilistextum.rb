589aeeec468603cb5094eb2a3d3d533abe244c2b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 21:30:02 +0000

    vilistextum: update 2.6.9 bottle.

--- Formula/vilistextum.rb
+++ Formula/vilistextum.rb
@@ -6,6 +6,7 @@ class Vilistextum < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "cead55f6cb7e4d66d3f6ca2bf013f0cb653144a0fe79620fdd5735a1e57566a5" => :catalina
     sha256 "c36418e1556b9f5f9c0126811fddca3149137abfed6b36596ec4612c3806a3ec" => :mojave
     sha256 "6005ce3b4c593707dfe7ffbc10ea64f26ce6e441803a9133ab46ba0fbaee422f" => :high_sierra
     sha256 "b8fa6ddde71b9b86128e12bbc343935ca5ec58e15d28da2a1a9972a23df9becd" => :sierra
