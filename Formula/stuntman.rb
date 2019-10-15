7b9195c11551a528698af0617bd30d2f57d81eeb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 07:24:29 +0000

    stuntman: update 1.2.15_1 bottle.

--- Formula/stuntman.rb
+++ Formula/stuntman.rb
@@ -8,6 +8,7 @@ class Stuntman < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "52e9913b0a6208e305f6f5d0e64f6c2888be95f9c35674293d2a7bbde07dd5f1" => :catalina
     sha256 "87375257e1d4c3964587b59ef34a1800e9e9b7e6028a506a22b8cd695f39bf42" => :mojave
     sha256 "b5541fc2478ed4b97cdc5cf97ddbcbc4d76255b0ba9e834666c548612e9b758d" => :high_sierra
     sha256 "51f40332e70148118ca22eb7f393c002188e73fb59c82a44c689430f86b2bf7f" => :sierra
