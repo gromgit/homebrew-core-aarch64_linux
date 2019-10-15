0e12d4b2f1a8a23c6f25cb321cd1bfef7e28bafd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 12:54:40 +0000

    glbinding: update 2.1.4 bottle.

--- Formula/glbinding.rb
+++ Formula/glbinding.rb
@@ -7,6 +7,7 @@ class Glbinding < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "6a371e47b76cd227e12699de3e7a095e620150532789cdac48e1c9b59bee06b6" => :catalina
     sha256 "a44cd2f23650ce664d8f61634c27abce3a00f4b5d9efbb10687759a62ca26895" => :mojave
     sha256 "ad79687ca8b43832ab27d5a459a71c4cb7e2be5b02d5df15c667ad7689fe38d0" => :high_sierra
     sha256 "454bfd4f3f6a983a0614f469388cbe27437350d203c61aed34a8c05fa9bb0710" => :sierra
