0e9924a4cdf2fa8107d5930359997ff51c0ea90e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 18:53:11 +0000

    zssh: update 1.5c bottle.

--- Formula/zssh.rb
+++ Formula/zssh.rb
@@ -6,6 +6,7 @@ class Zssh < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6b9bce24c13dd2e979cdae57892e1b595bfcbd1d342bb81419dda378b8439495" => :catalina
     sha256 "0b1567c1d4aef681ff463f058a884eead039fb0c50a1c03820a03c9f67786b52" => :mojave
     sha256 "9cb26f1bd359977406fae945abd311b2cdc5770570e6350f2ac278bfbe458f5b" => :high_sierra
     sha256 "49e01bb86097999f21f3d96b0f9cd63a975d4fd52f6e286d42ceee16ee996eb7" => :sierra
