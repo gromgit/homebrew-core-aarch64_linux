5746ea98de9a53bc825cdadaa2c660c1e4d9b5da
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 13:33:00 +0000

    gnu-time: update 1.9 bottle.

--- Formula/gnu-time.rb
+++ Formula/gnu-time.rb
@@ -8,6 +8,7 @@ class GnuTime < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "9a1d1160f85f46b3022dc4d978dfafe6b3a02fc97446bc51f8b1ae4580b7c69a" => :catalina
     sha256 "dc007b95e2f9fb0df3380da55d3c9337529b1a4a3cd762972eb88512f567ea1c" => :mojave
     sha256 "ad5d776c38e43f16fad8976770eeaa18e40562c166fa65fdaa12af61981c7b90" => :high_sierra
     sha256 "d51ef948a5a87281175fef771cb28469cbdb3085e3c51ad325d780ff921cc013" => :sierra
