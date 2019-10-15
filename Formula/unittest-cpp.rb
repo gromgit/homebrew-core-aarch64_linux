61019d4937b4fc17cc5a5e46f69c1c0ced057c62
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 22:52:54 +0000

    unittest-cpp: update 2.0.0 bottle.

--- Formula/unittest-cpp.rb
+++ Formula/unittest-cpp.rb
@@ -6,6 +6,7 @@ class UnittestCpp < Formula
 
   bottle do
     cellar :any
+    sha256 "19a4cef9ba95b37528f2a88d280b0f4c77809d7553a8e5747cfd4e41363f2fce" => :catalina
     sha256 "9837dfbba5a3014097d3b406bd48e174a6a788d0c0b3107bd1fabeeb0ce6b89e" => :mojave
     sha256 "206f44c35a82fac519b64b8c4ae6bc397e360d8404e8279a24b906d7729efed2" => :high_sierra
     sha256 "91d028b464f32fcf6edda6b791be2b70d9b770934edd7af7d2b8ff24e9c5eb06" => :sierra
