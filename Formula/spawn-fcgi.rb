1ef3968edc4209576fc44e26a89ad62a0903162a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 00:26:31 +0000

    spawn-fcgi: update 1.6.4 bottle.

--- Formula/spawn-fcgi.rb
+++ Formula/spawn-fcgi.rb
@@ -7,6 +7,7 @@ class SpawnFcgi < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "a0665cd25e441b8f798073125e2f4151588aed54408b17f894e62a353ca73d47" => :catalina
     sha256 "2512789a14b629470c684a4694e7f26fb28a9734b156f0756279bc8f40c2f2bd" => :mojave
     sha256 "31c9d255c30ac65009b0972c7b9fe8a8835f8c305800c1b147471b44113fd285" => :high_sierra
     sha256 "23140d56da75279d033d123b5cc5a7d50018dd08e6c74e3ed118eac5adbac555" => :sierra
