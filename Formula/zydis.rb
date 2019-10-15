756f522cf4dfc2945e52a61e4ca342bdf0ac30ea
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 17:51:09 +0000

    zydis: update 2.0.3 bottle.

--- Formula/zydis.rb
+++ Formula/zydis.rb
@@ -7,6 +7,7 @@ class Zydis < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "00a9d49fa9a542eedd2332d14134c92b6c4022547bcd65a74db2797d43964afa" => :catalina
     sha256 "30e492d8005033bdc38a2fc2b32573ee7f8567484d516cb39f9edd094ec2c9fe" => :mojave
     sha256 "77aebf0f3ab130504dabdc08d54fcb1cc5748eaba01fcea3f7a6558a0e6868a3" => :high_sierra
     sha256 "846e26eb8120db0797a69b8335743aa95d3feef9cacc5e04aa7a2ce21cfa84dc" => :sierra
