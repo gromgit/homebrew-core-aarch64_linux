8a66c8caeb94156c8dfec1c966f7d5a312fa3094
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 13:07:01 +0000

    glslviewer: update 1.5.5 bottle.

--- Formula/glslviewer.rb
+++ Formula/glslviewer.rb
@@ -7,6 +7,7 @@ class Glslviewer < Formula
 
   bottle do
     cellar :any
+    sha256 "81841fc4594a7e06444a0ace80d8ad8c7ea3738325d86e968ba844bc27c8d085" => :catalina
     sha256 "e39a6cd0133d664e812e5148d01a7da705d23253548cb7cd0bae58324f7757b8" => :mojave
     sha256 "79145bf1417520ea637f08bc4b1cc3071f2cef6404ddec54fea7df701860eece" => :high_sierra
     sha256 "c81f27a48b0fbbca9e091d76893d9e9e6064e25c5febce922b800318cb1bf1e8" => :sierra
