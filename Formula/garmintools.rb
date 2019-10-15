f331b0745676e3de8f15d0b32e99ee63f545bf5d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 16:04:15 +0000

    garmintools: update 0.10 bottle.

--- Formula/garmintools.rb
+++ Formula/garmintools.rb
@@ -6,6 +6,7 @@ class Garmintools < Formula
 
   bottle do
     cellar :any
+    sha256 "91c193c86b431bc3541b18ad33cf6793b001fc70293c50289d8fe6d978d50ca5" => :catalina
     sha256 "ee15b7a5ca1312a9ed358f22ce2c36681eedda24ae7b855b079f196e39280101" => :mojave
     sha256 "9ecdb8294089c84a229db39a395bf3f4817f185f30135a6f92711b95705ab869" => :high_sierra
     sha256 "c747a668400406f6625a3832e351a4f27fd1308d8ef840120eba086d3d6adcb4" => :sierra
