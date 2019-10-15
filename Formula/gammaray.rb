7523ad565847cbf2b238f608c8a07e53242e76a3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 16:04:16 +0000

    gammaray: update 2.11.0 bottle.

--- Formula/gammaray.rb
+++ Formula/gammaray.rb
@@ -7,6 +7,7 @@ class Gammaray < Formula
 
   bottle do
     cellar :any
+    sha256 "c6d0ef57b42ec3732e21e2c2971d248cfdcb2e46c97a89fe979055364204bb4e" => :catalina
     sha256 "3efdda21f9a32efef5dc78367e2b0bafae12793fab0e387d426ac3631842c7b4" => :mojave
     sha256 "317874bb7746f5f992984ae9deb89338d852565d6e08b46788d7a2d8e7fcee59" => :high_sierra
     sha256 "ebab58973253ea91221d628922d55a2e60db306170970d770ac53f8c9a50821e" => :sierra
