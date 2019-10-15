21513fd9c35bb148a523b46d5c83c32e09ca481a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 11:11:31 +0000

    genact: update 0.7.0 bottle.

--- Formula/genact.rb
+++ Formula/genact.rb
@@ -6,6 +6,7 @@ class Genact < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1650cc3c9a42146f1e0ea30300d943c29a49a1e25680d80f4e20f2519c1bf00a" => :catalina
     sha256 "fd5ef1de4fea2b1c08785d77eacaf40936764bf843e627977b2ab348f1c190ef" => :mojave
     sha256 "efeb250335f9dc2ef86be2814243554933a99003a2f08aebf70339580917ff1d" => :high_sierra
     sha256 "adeebbec88a70c4e06eece55a38b71e4d7031d85add9cf4069744f4a8b4cd395" => :sierra
