69f94fe14113987ff856546cf983006116944cbe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 22:05:14 +0000

    wakatime-cli: update 10.4.1 bottle.

--- Formula/wakatime-cli.rb
+++ Formula/wakatime-cli.rb
@@ -6,6 +6,7 @@ class WakatimeCli < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3167ac719d8b58384c8b81db49831e06d084b1455bec7198a8b423586742c650" => :catalina
     sha256 "de1250de51e5a98e72a6719ab36ec7ff27d5cfe073bf2977e407dbede12fd92e" => :mojave
     sha256 "21070f5a585ac2b80bcd97c28a6edfcf24a1225bc9fd92b8fbe02de904e66d44" => :high_sierra
     sha256 "21070f5a585ac2b80bcd97c28a6edfcf24a1225bc9fd92b8fbe02de904e66d44" => :sierra
