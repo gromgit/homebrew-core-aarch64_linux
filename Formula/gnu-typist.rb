3d33f9d2af39e0f3839c28eb8d8386d5981823ce
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 13:33:00 +0000

    gnu-typist: update 2.9.5_2 bottle.

--- Formula/gnu-typist.rb
+++ Formula/gnu-typist.rb
@@ -7,6 +7,7 @@ class GnuTypist < Formula
   revision 2
 
   bottle do
+    sha256 "2a824f3fad3871cbf43f15009c23563aa03872597f22e823f9e2551d35fe1e26" => :catalina
     sha256 "9f0fcdd42b9a041408b132882778db2eb479749a7169b82f2caf1f4fd486b599" => :mojave
     sha256 "72503afd4efafe7a8485ea22332819937008263976a6f5f5b42818565d59edbf" => :high_sierra
     sha256 "d32708d6e8a640101ac618ceac23be6b9d1a6a4caa127c5fd12a44b4e57c09e9" => :sierra
