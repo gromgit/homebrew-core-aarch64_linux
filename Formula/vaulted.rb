13f087d454b19e24ddc1a083467525ec8e84d318
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 20:52:36 +0000

    vaulted: update 2.4.1 bottle.

--- Formula/vaulted.rb
+++ Formula/vaulted.rb
@@ -7,6 +7,7 @@ class Vaulted < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9d0e8646d3798280b9fbb7aff4e09e888a97c71864f49301b15c793042f557c8" => :catalina
     sha256 "5b448f1ea890af2ec60633eb6aec8939be2893c7a134b748af5db26fb4926291" => :mojave
     sha256 "6cc74771b207de479c1f45e5385e7b55a46daa256377ec0098746d1e0d3a6441" => :high_sierra
     sha256 "b5a81beb6acdbb14c1d83304c2cf12c21b37a41c93dc48d11b588025f777ed68" => :sierra
