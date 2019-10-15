0fc79c162fab4012d44c0889bde1c8d9eecb3fd3
Vyacheslav Andreykiv <email@andreykiv.com>
2019-10-15 12:10:45 -0700

    ical-buddy 1.10.1
    Closes #45367.
    
    Signed-off-by: Rui Chen <rchen@meetup.com>

--- Formula/ical-buddy.rb
+++ Formula/ical-buddy.rb
@@ -1,14 +1,14 @@
 class IcalBuddy < Formula
   desc "Get events and tasks from the macOS calendar database"
   homepage "https://hasseg.org/icalBuddy/"
-  url "https://github.com/ali-rantakari/icalBuddy/archive/v1.8.10.tar.gz"
-  sha256 "3fb50cffd305ed6ac0ebb479e04ff254074ee5e4b1a1c279bd24c3cc56bcccb0"
-  head "https://github.com/ali-rantakari/icalBuddy.git"
+  url "https://github.com/DavidKaluta/icalBuddy64/releases/download/v1.10.1/icalBuddy-v1.10.1.zip"
+  sha256 "720a6a3344ce32c2cab7c3d2b686ad8de8d9744b747ac48b275247ed54cb3945"
+  head "https://github.com/DavidKaluta/icalBuddy64.git"
 
   bottle do
     cellar :any_skip_relocation
     rebuild 1
-    sha256 "d475683ee500d5967cc779e070b2ed841a16c0ad53ca3abea6d7f3659cccecd8" => :catalina
+    sha256 "720a6a3344ce32c2cab7c3d2b686ad8de8d9744b747ac48b275247ed54cb3945" => :catalina
     sha256 "734aa2e1b61ef6d9ef8800ab5382a8e157a13631cf1eeacbbbb0cc80f1831a76" => :mojave
     sha256 "fa01f750eeb692d11ae0fa4cb6131e331446ad623bc8fc284b3721cf81621f39" => :high_sierra
     sha256 "8dc7ef559702a3c489b2905e5cfdd4c9be18decd9557c9034df920f0ef57761e" => :sierra
