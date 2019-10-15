a9b72a56434b62445167dd0a141def223c449626
Thomas Plaçais <thomas.placais@protonmail.com>
2019-10-15 11:46:55 +0100

    screenfetch: remove bottle (#45349)

--- Formula/screenfetch.rb
+++ Formula/screenfetch.rb
@@ -5,12 +5,7 @@ class Screenfetch < Formula
   sha256 "d6df4ef7763f9761d818c878465d78ef701b71002a50d4f150f65a31cc1bea37"
   head "https://github.com/KittyKatt/screenFetch.git", :shallow => false
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "4ebc2dbe3075fdb801971050cf9f5970142309c4473e72fa78c4f18de5db2933" => :catalina
-    sha256 "4ebc2dbe3075fdb801971050cf9f5970142309c4473e72fa78c4f18de5db2933" => :mojave
-    sha256 "4ebc2dbe3075fdb801971050cf9f5970142309c4473e72fa78c4f18de5db2933" => :high_sierra
-  end
+  bottle :unneeded
 
   def install
     bin.install "screenfetch-dev" => "screenfetch"
