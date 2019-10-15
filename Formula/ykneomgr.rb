6d41bb993d7f1b27a983aa4da7aa7c8399918999
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 17:01:52 +0000

    ykneomgr: update 0.1.8_2 bottle.

--- Formula/ykneomgr.rb
+++ Formula/ykneomgr.rb
@@ -7,6 +7,7 @@ class Ykneomgr < Formula
 
   bottle do
     cellar :any
+    sha256 "8eaee6c402117972a2b3ec4db714594b9e6ddce1bb23c795db3650b576deda38" => :catalina
     sha256 "42dbcc5c2c3f33b3c6cfa6a87be4a37153ef68d361374d2cec410cb406a62bb9" => :mojave
     sha256 "256291cea42d30f95754e2203cec73a7acef9436be3ad0040ce7d966fc9d8470" => :high_sierra
     sha256 "7917d4068d2c68d3309b32ff443622c0540829a9e3cf0053913697c321c74067" => :sierra
