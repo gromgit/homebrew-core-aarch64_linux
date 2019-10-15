597c67fd2ede33cd7b30c1474ca02edbb03c7e08
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 16:14:47 +0000

    gplcver: update 2.12a bottle.

--- Formula/gplcver.rb
+++ Formula/gplcver.rb
@@ -6,6 +6,7 @@ class Gplcver < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e0db2e2d2f4331ecbe4ead3c8f9d4f239c6b9427472ea959dd394544fbbf7b43" => :catalina
     sha256 "fb50587552693b0c0c26ee074c52766c097f90afc6492a0bcf75cc65aaf2f031" => :mojave
     sha256 "2460dcc2da525280cd5b7d2452abe922874291b92f0ba3abd1316da2e5ff40f7" => :high_sierra
     sha256 "a0f14e7d01b7098ed6e770b21df05f03d7506ca0bab3d1f84845ca9ca7d1eb5b" => :sierra
