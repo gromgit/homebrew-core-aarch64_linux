c0de0841f863a14011e44e497015ba0fa73fca55
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 23:54:40 +0000

    heatshrink: update 0.4.1 bottle.

--- Formula/heatshrink.rb
+++ Formula/heatshrink.rb
@@ -6,6 +6,7 @@ class Heatshrink < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5956959544286fc9b6474a0f6df508530431c1632527fa4048091f33f319fab2" => :catalina
     sha256 "504b4b64164343217c6852509b59858494ba38ad9b63e7a9b3bb247290833582" => :mojave
     sha256 "865d11380a3e586a962a5dec0069def43e777f20626bdc5396735d003d90d20b" => :high_sierra
     sha256 "3965350f672040dfec9d2e07ac5f26aa16b324f59d2a762a4faac0930d2de684" => :sierra
