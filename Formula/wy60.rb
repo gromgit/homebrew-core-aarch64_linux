92791f08fc6d5edcc9c1eb268e3da1f8d5eff774
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 20:53:50 +0000

    wy60: update 2.0.9 bottle.

--- Formula/wy60.rb
+++ Formula/wy60.rb
@@ -5,6 +5,7 @@ class Wy60 < Formula
   sha256 "f7379404f0baf38faba48af7b05f9e0df65266ab75071b2ca56195b63fc05ed0"
 
   bottle do
+    sha256 "29247a3617870bdb8364f9ce1b6d167b6029b016683dc5da39816b0a637bf5ef" => :catalina
     sha256 "04e34b5cc12f3c130454dec804ff989e999fe6fb043c66f44976fc710fdce62a" => :mojave
     sha256 "77fb48cc35956863e1f685b41c885337ca770185edffb250cbed8bd8c5a3070b" => :high_sierra
     sha256 "f03706d166cfcc0679e696493bd13df30ad0617a92b602b79e3494ba3b1f46fb" => :sierra
