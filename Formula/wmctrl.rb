3fb93496d09c43dae3501feebb1beef813a55b5f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 20:18:07 +0000

    wmctrl: update 1.07_1 bottle.

--- Formula/wmctrl.rb
+++ Formula/wmctrl.rb
@@ -7,6 +7,7 @@ class Wmctrl < Formula
 
   bottle do
     cellar :any
+    sha256 "7fffdc3399b2af15b638dfb642e44e0c148df088828f307c1f9440e38049cd5a" => :catalina
     sha256 "2223922cda28d81580d85d01fa697284102d10226df76d57660bd92093fd46c5" => :mojave
     sha256 "59b55236fd42a64f6ccc8587a5580a25afd330a137b62e7258568042e8b1b525" => :high_sierra
     sha256 "cbdb379f9b1264847f74c0ea01e0a5412645b442d3c069708bfbe209b845b873" => :sierra
