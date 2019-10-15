3a43f4e0eb621d5720fdfe43aaa24a93da823632
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 17:03:57 +0000

    yapf: update 0.28.0 bottle.

--- Formula/yapf.rb
+++ Formula/yapf.rb
@@ -8,6 +8,7 @@ class Yapf < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c22d7f03bd8c89b8b1d4fc150bc24839c70dc994207d9cda55ec2a722eee4e24" => :catalina
     sha256 "c8de7e9be2a3c5c8a09501199d4beea2accbd5a62471a6def5495899b8facc33" => :mojave
     sha256 "a819475f27f0182bfee9ae5c3a63fb60af1119c78fbea7509954fb1fe035986a" => :high_sierra
     sha256 "c5be10b759aaa579aeafb7b6a0d941aced581d9ab5428198ea588f367c37809b" => :sierra
