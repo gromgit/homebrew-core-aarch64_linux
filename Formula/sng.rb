9b02c4897f36a09638517ca0e17a64c8f4c29d62
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 08:29:49 +0000

    sng: update 1.1.0 bottle.

--- Formula/sng.rb
+++ Formula/sng.rb
@@ -6,6 +6,7 @@ class Sng < Formula
 
   bottle do
     cellar :any
+    sha256 "b37aa0aca0f3ab6b87ebaba92312fe008233a7f03cc015bcd6dd6e29e5291110" => :catalina
     sha256 "f6968419ecf0848134eac6705e33c8fe0ea31696d0aa21a1a530af7767ff2865" => :mojave
     sha256 "b708d5c925acb4986d3cb6af71a2fc25d9ca53b35c3cc7700332513858057786" => :high_sierra
     sha256 "30d9ad9aac3d8aaa67ae524d41bcdfbd92232f053bb0d5ccb3961b811c5b39a3" => :sierra
