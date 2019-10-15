2668af59ce41db48f9bf0ca00db8b6b8650381eb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 00:43:02 +0000

    sqlparse: update 0.2.4_1 bottle.

--- Formula/sqlparse.rb
+++ Formula/sqlparse.rb
@@ -9,6 +9,7 @@ class Sqlparse < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3563df512a09a0719564b8c3a30db9e95a92bd809680f2fac548473ea9cbb748" => :catalina
     sha256 "608b6e92c003594456ab481c7dcdbd876d402fd96c97ceeaba0dcf5853648319" => :mojave
     sha256 "477a0a456ef2dd768bbda3324e9b2fea70b9359402423051f93a5d7e4a6318dd" => :high_sierra
     sha256 "9c77ae1a2ca8a0cfe79197762b9c60be451b827cf72f616542547ec280b3acb3" => :sierra
