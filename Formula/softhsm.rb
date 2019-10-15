c7be8b1c87189ef41beef1911ea256554c8e35e0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 00:07:04 +0000

    softhsm: update 2.5.0_1 bottle.

--- Formula/softhsm.rb
+++ Formula/softhsm.rb
@@ -6,6 +6,7 @@ class Softhsm < Formula
   revision 1
 
   bottle do
+    sha256 "d924a3f96532b0cf983385a97a72fa2c6909f8f8e7b92dd5788a81f71bc20d9c" => :catalina
     sha256 "ba2efed59b5064868a7198dc51aa9be15070bf34ef3c09d8de40c64c7c9bad5e" => :mojave
     sha256 "45fcd165092f402571d77d28ec07e7b16ac4b257b72f87a29174ac614f89336e" => :high_sierra
     sha256 "d42f52da6397f57e84da014d62411d7b4fe7afe69dff63947c53054d6f66de6c" => :sierra
