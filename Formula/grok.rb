57475d6d60bcd9b74a2d145a59b500af95b1eabf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 14:48:24 +0000

    grok: update 0.9.2_2 bottle.

--- Formula/grok.rb
+++ Formula/grok.rb
@@ -8,6 +8,7 @@ class Grok < Formula
 
   bottle do
     cellar :any
+    sha256 "8e3f44420143e731799d52290c9823a42a1833c4bc51906af59d4cd7c284f391" => :catalina
     sha256 "b78cf21dd67826d14d99188e631ff1c431913744d91089c4cefd9b3c9e9d9a46" => :mojave
     sha256 "41889afb55bfcf1d8b41eda76ef2272d29225f4cc4a5690bd409198417d7cf98" => :high_sierra
     sha256 "32dc46849684918dad9ca9005ca43b092de84b16a0837049146948379301b1fa" => :sierra
