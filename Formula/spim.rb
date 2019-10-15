d19548d7f11dd5d125a8c26e4db56fb23aa7ece6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 00:26:31 +0000

    spim: update 9.1.20 bottle.

--- Formula/spim.rb
+++ Formula/spim.rb
@@ -7,6 +7,7 @@ class Spim < Formula
   head "https://svn.code.sf.net/p/spimsimulator/code/"
 
   bottle do
+    sha256 "e4251f5e6241a4f468e7ba4d366e1a3271946e0716ec6440893908eeb12d236d" => :catalina
     sha256 "3d72a5493c97fc542e714595358efdfbbc58a2e84cbaef8ca449b7a943c00302" => :mojave
     sha256 "cde6c0f680acb27b47a9eca2279e72c245360322d5ee910ebc01bb24140285a9" => :high_sierra
     sha256 "29ac5a63bd12dd986b578a31ea0d1a6029940551084c3273d572c68b93f68f38" => :sierra
