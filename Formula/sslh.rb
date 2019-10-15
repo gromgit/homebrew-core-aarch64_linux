88328fe0d5f9badb280dd5619038c7f2b959e950
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 01:04:52 +0000

    sslh: update 1.20 bottle.

--- Formula/sslh.rb
+++ Formula/sslh.rb
@@ -7,6 +7,7 @@ class Sslh < Formula
 
   bottle do
     cellar :any
+    sha256 "be0c707c6a1216bc06da14003f0af6197ac450a2fe4b6b6ce12b3e614b54fbca" => :catalina
     sha256 "cee0f6398a940312dd3198dda55d949955aa1374cd15fb5841c0446c67508e4d" => :mojave
     sha256 "b1d1ea3c654defbd69cc850ecb9b5b97b09e3b8c991faf48cb22d4190e0791b6" => :high_sierra
     sha256 "8a4b0d4715358a714c8f3ef86496e91416bcfe70067f3d71db30f97616b71080" => :sierra
