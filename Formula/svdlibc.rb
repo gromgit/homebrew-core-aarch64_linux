6146e78fc59f5ee437f2967208589fd450a4f2b2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 10:00:08 +0000

    svdlibc: update 1.4 bottle.

--- Formula/svdlibc.rb
+++ Formula/svdlibc.rb
@@ -8,6 +8,7 @@ class Svdlibc < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 3
+    sha256 "3c78491f3d89425e87fb7a1d5ec63845ddf86f5098f817e32f7c1aafd64aa3cf" => :catalina
     sha256 "dc6f967a82e022b8bd193a0f346aa50b17df609de22acda3ead9eac89b1fd103" => :mojave
     sha256 "7b1c2af44513a0280c2922b22dd0107189e9872a6dc2e0476df2b8c47902cf40" => :high_sierra
     sha256 "91a70ced4042615305d39e9d4d88d91b4d971bf8bb40e883b8f7a2682bb2e729" => :sierra
