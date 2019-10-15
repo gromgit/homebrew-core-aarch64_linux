9a0acc1479bb33bb3406b647801597d5553d772d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 11:43:42 +0000

    gcovr: update 4.1 bottle.

--- Formula/gcovr.rb
+++ Formula/gcovr.rb
@@ -7,6 +7,7 @@ class Gcovr < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4c1f761c118737ad2f786b20433f8ffb62344ed0449ba4c57d3316c1e87dace4" => :catalina
     sha256 "375d5736d80f9843b2a77d86b365d123a472325c6c8cd3c68d065bce99d8c9bf" => :mojave
     sha256 "b5b3a5c643c84b547e6c2ae0c9db6cba7d53a8a081e080eb1efefcfd1f95b211" => :high_sierra
     sha256 "7cf8abff45bbea6e268fe4674c5f8ff2be1d4df413abf3068def0f07c2bc0c09" => :sierra
