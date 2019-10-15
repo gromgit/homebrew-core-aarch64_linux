5e49fe5632d0c33b840c78bd893ddee8b3a210dc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 12:39:03 +0000

    github-markdown-toc: update 1.0.0 bottle.

--- Formula/github-markdown-toc.rb
+++ Formula/github-markdown-toc.rb
@@ -6,6 +6,7 @@ class GithubMarkdownToc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "eb8ff2eba1c3a19aede0151b60f1759ba5e9c9056fabd11e4a830024b2155272" => :catalina
     sha256 "0f96d95ab270819393d74cd6daac7cae2b9b996d1ef1f13a575e59e3f355f27c" => :mojave
     sha256 "633c4cbbc1dd8d1194bfbefe0883266f6b61121ba343b83707d77c3cce8d9922" => :high_sierra
     sha256 "c462dd1fdbb04f213f6ad1423b8e659766082760e0fbdfa4451807d97a8ce567" => :sierra
