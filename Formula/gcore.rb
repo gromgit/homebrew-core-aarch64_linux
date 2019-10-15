6a53b0cf0dfd6fe46943bca99c29783008515266
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 10:36:46 +0000

    gcore: update 1.3_1 bottle.

--- Formula/gcore.rb
+++ Formula/gcore.rb
@@ -8,6 +8,7 @@ class Gcore < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3451656da696ce7bb47816bf409dd38f279a0dde973b5d7bc60eaec62edbee9a" => :catalina
     sha256 "86a6941a86863482eb97fd03e40749b83b74b7a93d7c7592db4dab6acb9c859a" => :mojave
     sha256 "b9e7e188bee51975ccfdb8f711101a7637f316be0e3aa6b8f80259f7b884f488" => :high_sierra
     sha256 "5fbccf36d0bd51cc4261859b7faf2cc15fe89244109b64abf83512ea73f3259f" => :sierra
