f4715d87c099c1ae10bbc02e21bf0bb176ae430c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 09:46:06 +0000

    streamripper: update 1.64.6_1 bottle.

--- Formula/streamripper.rb
+++ Formula/streamripper.rb
@@ -7,6 +7,7 @@ class Streamripper < Formula
 
   bottle do
     cellar :any
+    sha256 "bdc01265cc82de8fdd17a432458a22ea22420839daed5d29234efe5c9cf459a2" => :catalina
     sha256 "559e6ce06f450c306178c1e361154f134c3478ad1bc35ca70d0d3f000938043d" => :mojave
     sha256 "9df7827f89ef7f517ccfdb52be976b358ede1ceb2690f8617b4cc52da7c4cf41" => :high_sierra
     sha256 "2ccd049ca0ce6720055a86b726bfb1388b4e3784b2cd597bc7b26fd1e593a60c" => :sierra
