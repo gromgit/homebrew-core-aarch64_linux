081cf9ee355b963d3853c260c0dd8bc93de65ec2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 20:07:52 +0000

    winexe: update 1.00 bottle.

--- Formula/winexe.rb
+++ Formula/winexe.rb
@@ -6,6 +6,7 @@ class Winexe < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4706b05f203ecaf3a56fc453d3c6588fd151d9ce4b8be0f6973725f70379dad3" => :catalina
     sha256 "43444e53e90a4f739a533e4a865952369874d9386460205e501631fa2b3ad2bb" => :mojave
     sha256 "765ad670de08f86b8c9b11ec43493148d1368e6c3ffa5e65d1bca898480996c2" => :high_sierra
     sha256 "e9594f927f9ef58608951175c0bd118b82cf7b25d5b829453195b66f45c2cbc1" => :sierra
