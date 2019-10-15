8c49a893fd160c633660c4135993d0f678a31c75
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 13:59:39 +0000

    goose: update 2.3.0 bottle.

--- Formula/goose.rb
+++ Formula/goose.rb
@@ -6,6 +6,7 @@ class Goose < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0090a7bdb8ffc8831426d49aa138ebd4a608bdc3366cf37e8fd0a441db1e8a53" => :catalina
     sha256 "1143891d26f430acacc6b47128d0eaa520508925dc0f1d594685c997d77b5e38" => :mojave
     sha256 "c98cdc85daea46b8439109211194ede9342ff76c296f830f9225872c28877baa" => :high_sierra
     sha256 "a5c492e34e3351d3efb87b0e5f3e2571741e6efad35f794e3de64f39a8cd464f" => :sierra
