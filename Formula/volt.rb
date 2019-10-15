e042068b4a951bf5815fc80ee5483a09bf128768
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 22:29:50 +0000

    volt: update 0.3.7 bottle.

--- Formula/volt.rb
+++ Formula/volt.rb
@@ -8,6 +8,7 @@ class Volt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "60210297f62f908ef4090a7f69631ad02cb4fe2ce8472e953f67ad91caa9461c" => :catalina
     sha256 "9db9e940c124e8e655cdd84b7d143f526535c588ebd6503acb3960143d08f905" => :mojave
     sha256 "7fd8887efcdc3a9816b2dea510c2e3ba218e0e719390841d3b0b416fde53378e" => :high_sierra
     sha256 "4edc3f1130757ddbf0a7b3c018825f68f2ecb24417f3afc3fd54b532e8c72c46" => :sierra
