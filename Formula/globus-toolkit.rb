f31124fdcbcf1ffeee7ea61d881d0e9909bf3073
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 13:11:54 +0000

    globus-toolkit: update 6.0.1531931206_1 bottle.

--- Formula/globus-toolkit.rb
+++ Formula/globus-toolkit.rb
@@ -7,6 +7,7 @@ class GlobusToolkit < Formula
   revision 1
 
   bottle do
+    sha256 "62862983a40e42adc881068a81f91279780b234d5bdc5a7ed544f0da41381050" => :catalina
     sha256 "05cb734c86b9027af8de43708a890577c057a08585721a50cf8b6a26c653274c" => :mojave
     sha256 "c768db1e3b52930b16d9b82555e8e2515510e946b62c13926c0c0ca10d033712" => :high_sierra
     sha256 "263a91ba8c35690eed22c407b31efec92d641c08405dde4432cb92ee7385ef41" => :sierra
