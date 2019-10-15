0525c4680f4e9c55df1c61b925b8b5f04d3e637f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 15:10:04 +0000

    gst-validate: update 1.16.1 bottle.

--- Formula/gst-validate.rb
+++ Formula/gst-validate.rb
@@ -8,6 +8,7 @@ class GstValidate < Formula
   end
 
   bottle do
+    sha256 "76ac31998b8df5dde6c4be8b84e39622d4350c8871e8ffac6f03ab6ae5572126" => :catalina
     sha256 "adc145563618a2c2e71bc4fb5673431e9fc96a952487c1cc2aec8c70a1890170" => :mojave
     sha256 "f548f74c7824cb1bbc9324f8884a6e257d6c5d4da3cb1bed8291eb4809d10b63" => :high_sierra
     sha256 "bf8c76f74c7c0b447e04efc6d07a068bbf1fc22e34da4b449a3f464336fad706" => :sierra
