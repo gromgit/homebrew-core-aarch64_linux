599813763064d4f52fb6f59bfa3db1d84d630b4e
Mike McQuaid <mike@mikemcquaid.com>
2019-10-15 09:00:37 +0100

    webdis: brew style --fix

--- Formula/webdis.rb
+++ Formula/webdis.rb
@@ -59,16 +59,14 @@ class Webdis < Formula
   end
 
   test do
-    begin
-      server = fork do
-        exec "#{bin}/webdis", "#{etc}/webdis.json"
-      end
-      sleep 0.5
-      # Test that the response is from webdis
-      assert_match(/Server: Webdis/, shell_output("curl --silent -XGET -I http://localhost:7379/PING"))
-    ensure
-      Process.kill "TERM", server
-      Process.wait server
+    server = fork do
+      exec "#{bin}/webdis", "#{etc}/webdis.json"
     end
+    sleep 0.5
+    # Test that the response is from webdis
+    assert_match(/Server: Webdis/, shell_output("curl --silent -XGET -I http://localhost:7379/PING"))
+  ensure
+    Process.kill "TERM", server
+    Process.wait server
   end
 end
