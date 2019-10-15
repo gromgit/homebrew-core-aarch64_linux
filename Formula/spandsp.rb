fccae145d08596a9a4f5dfa2469d10dec43d608c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 00:20:16 +0000

    spandsp: update 0.0.6_1 bottle.

--- Formula/spandsp.rb
+++ Formula/spandsp.rb
@@ -7,6 +7,7 @@ class Spandsp < Formula
 
   bottle do
     cellar :any
+    sha256 "8a34a25e8108c8a5343d00a031d52c1003422a5ad84186ded83fbed819a834e5" => :catalina
     sha256 "d38722be71b9d6b3311ee51cb85bc406b2bc34eaf5741851c3b4ed432aecacf2" => :mojave
     sha256 "0b4bb6795c931452181252754da3db80189e6557c344e743296de823cb711efa" => :high_sierra
     sha256 "c0b8349525680304f99e464d328df804279be85398969974f2b708e5167c89fe" => :sierra
