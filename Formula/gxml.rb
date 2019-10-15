972694fbabf49a216c00dd76e567764ab32ce5d1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 15:38:57 +0000

    gxml: update 0.18.1 bottle.

--- Formula/gxml.rb
+++ Formula/gxml.rb
@@ -5,6 +5,7 @@ class Gxml < Formula
   sha256 "bac5bc82c39423c1dbbfd89235f4a9b03b69cfcd3188905359ce81747b6400ed"
 
   bottle do
+    sha256 "4eb68617f73471be697746b879fe118fb3e116a1e911a2f95541982a77cd4714" => :catalina
     sha256 "b9bb621d776f10dc1c3a9bde25964bd26847bf49cdee49ada1c0407f5fb14dbb" => :mojave
     sha256 "4253e9a1bd9ce221e2287e5d53d39342c65fd06aa63028aa56effae2514854b4" => :high_sierra
     sha256 "47042a94c013db905170cc0c373b8f7000d77e9c75d2d17dbacad6cd658e6b56" => :sierra
