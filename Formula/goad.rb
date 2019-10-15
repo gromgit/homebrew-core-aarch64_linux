25602a88a45df8cd7cafb3c4ca7e295d705c2fb2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 14:00:47 +0000

    goad: update 2.0.4 bottle.

--- Formula/goad.rb
+++ Formula/goad.rb
@@ -8,6 +8,7 @@ class Goad < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "57345918587236f5235fcacc3d6d7f64272a5c1def4e64632fee13b960c027b5" => :catalina
     sha256 "366aad32eaa55e8561244bc31dc7650d829dd1f588801ad5d986a22cbfc80418" => :mojave
     sha256 "3a87755d9b4b48b07a7fecd9be72df574d5866fe39d4dc0c60d6a88da40c5081" => :high_sierra
     sha256 "b10fb1177f1f3548b2a497cb56ce9f10620a06175adec6a1d452b0e136981d1e" => :sierra
