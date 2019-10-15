3425891d168a02b96b7d4ed6e5d869422f70a810
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 22:50:06 +0000

    ultralist: update 0.9.3 bottle.

--- Formula/ultralist.rb
+++ Formula/ultralist.rb
@@ -6,6 +6,7 @@ class Ultralist < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7817cf565464c09bbdae680d0849f59105b5388806ec1c73d0551c697f0c4b3f" => :catalina
     sha256 "7546817405dc0d2e1c87efdef031b3cbdca8d1399fe5fac4eea8cba4c0c1699f" => :mojave
     sha256 "b07adf0017cf56922b16c8529561a8cebbd23605a8c06f9415d7744cdccabf7e" => :high_sierra
     sha256 "e19e911af109a6b04d04f37390a7fb4922ee4e2c571a7a1db5b7a335b13db5b0" => :sierra
