ef1aa16202818d159913d903f912b7cbd701cd2a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 07:45:26 +0000

    surfraw: update 2.3.0 bottle.

--- Formula/surfraw.rb
+++ Formula/surfraw.rb
@@ -6,6 +6,7 @@ class Surfraw < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2a2267217bfdd25ea00b3a08f76c44518e33dac0192a8590e4b3bfa3b5d90073" => :catalina
     sha256 "c9f5fc8020b021799c68cd204d4612f487c44315c15967be78a037576b378920" => :mojave
     sha256 "69920395cbde5fdc2492aa27fc765d4dafe910e26d9d3a05777888425310a0a9" => :high_sierra
     sha256 "69920395cbde5fdc2492aa27fc765d4dafe910e26d9d3a05777888425310a0a9" => :sierra
