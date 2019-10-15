0d164d55bcdbcb10c48f7ec63d4f44bd561e30f8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 22:34:25 +0000

    ubertooth: update 2018-12-R1 bottle.

--- Formula/ubertooth.rb
+++ Formula/ubertooth.rb
@@ -8,6 +8,7 @@ class Ubertooth < Formula
 
   bottle do
     cellar :any
+    sha256 "e08b1229ff32e200e5f6e7d562d83ce26e5dff0e50b4373f1add2a411854ebdf" => :catalina
     sha256 "9e6dcaeeff6974606332371fc0ce861d679bc9d00471f4185a7531b320e581fb" => :mojave
     sha256 "d3891c8cd1e395c8d7acd9f364d6d42d3bcb7d9d1ddd5adea9dfabc7f0aead69" => :high_sierra
     sha256 "b4e68f3183b67bd99d276c0889e6c36ea6a1c99931446bec237d9bf7b4cc5d81" => :sierra
