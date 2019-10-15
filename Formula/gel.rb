1e852a6bf43a25da2491c316bd8b81ec9ef33902
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 11:11:31 +0000

    gel: update 0.3.0 bottle.

--- Formula/gel.rb
+++ Formula/gel.rb
@@ -7,6 +7,7 @@ class Gel < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7452d45bb894918afcf8c4254910fcbcb29b126eb6376fc0ba77ec25ee79dab1" => :catalina
     sha256 "7b08b9ca28185ab4ae1befb9f62d3d3a0d094f72629c0742b7135a521eac3381" => :mojave
     sha256 "7b08b9ca28185ab4ae1befb9f62d3d3a0d094f72629c0742b7135a521eac3381" => :high_sierra
     sha256 "a4a5e3f1b6eb3ea8511adbf12f9b22482c392616bdd37c801be2fd100a1b886f" => :sierra
