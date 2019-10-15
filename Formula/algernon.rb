a1fb027a4b974412c963d2e1752d6d65d8455a5c
Mike McQuaid <mike@mikemcquaid.com>
2019-10-15 09:00:26 +0100

    algernon: brew style --fix

--- Formula/algernon.rb
+++ Formula/algernon.rb
@@ -29,16 +29,14 @@ class Algernon < Formula
   end
 
   test do
-    begin
-      pid = fork do
-        exec "#{bin}/algernon", "-s", "-q", "--httponly", "--boltdb", "tmp.db",
-                                "--addr", ":45678"
-      end
-      sleep 20
-      output = shell_output("curl -sIm3 -o- http://localhost:45678")
-      assert_match /200 OK.*Server: Algernon/m, output
-    ensure
-      Process.kill("HUP", pid)
+    pid = fork do
+      exec "#{bin}/algernon", "-s", "-q", "--httponly", "--boltdb", "tmp.db",
+                              "--addr", ":45678"
     end
+    sleep 20
+    output = shell_output("curl -sIm3 -o- http://localhost:45678")
+    assert_match /200 OK.*Server: Algernon/m, output
+  ensure
+    Process.kill("HUP", pid)
   end
 end
