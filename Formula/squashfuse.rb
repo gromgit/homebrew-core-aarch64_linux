72e2693395b6d5b506b0898b41dfa3c2297eeda0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 00:43:02 +0000

    squashfuse: update 0.1.103 bottle.

--- Formula/squashfuse.rb
+++ Formula/squashfuse.rb
@@ -6,6 +6,7 @@ class Squashfuse < Formula
 
   bottle do
     cellar :any
+    sha256 "7e2e0499c0b9f98beb398319c949d2a1d45de6a3f0b546ef1d55214f68522312" => :catalina
     sha256 "f4cb4305f7773fbf927d51a401453c3cdee1f1d48da2ef33d8fd41d526fa7c0d" => :mojave
     sha256 "ada7e939ff42fcd9fb6b1fb81ab596463d6149ff592f73ca924b5b9dca5ddfc4" => :high_sierra
     sha256 "c1898c81ae091097ae2502ecbdebdd1831db302dd74b814003191007a4d5f018" => :sierra
