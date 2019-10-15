b15214263cb6c1e6b9d61094d138eea4412e5169
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 19:59:37 +0000

    webtorrent-cli: update 3.0.4 bottle.

--- Formula/webtorrent-cli.rb
+++ Formula/webtorrent-cli.rb
@@ -7,6 +7,7 @@ class WebtorrentCli < Formula
   sha256 "5102f46937056db4e1268bc9ce6244435d0b3fab2dc5973911ecf5db4e372b8d"
 
   bottle do
+    sha256 "e2c73414925c70265f2b60fc20aabaa24ad8513fdfa1db350fd52216283c1976" => :catalina
     sha256 "feb8ebedf0ae71e606138a3aade95976433bbf97066a45bbb77b7667b7940ecd" => :mojave
     sha256 "f8ce4a50913b23cf1f8d587d7fc51fbb071a6fb44d34be6b005a47d6cc20f435" => :high_sierra
     sha256 "01b1f9ab124ec3310f09f202d98fdc03a4a8a4fc969a5304da464faadf04bf5e" => :sierra
