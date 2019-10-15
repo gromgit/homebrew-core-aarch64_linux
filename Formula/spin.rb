873f00f551388677807ca6840f51d661726752bc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 00:26:31 +0000

    spin: update 6.5.0 bottle.

--- Formula/spin.rb
+++ Formula/spin.rb
@@ -6,6 +6,7 @@ class Spin < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "aa2c31e9fcd14764f4cd217256ea30caba446a3dadd295adb2dfba4baadc42c4" => :catalina
     sha256 "92ea7a4b20e4d8409df68c717caddc537c0cfb578040f4eb6b48846e64aeef08" => :mojave
     sha256 "ab5249a0e58f7ee677defc10bd187c6ef5ee3ddc5eb5cf0ba4747b222ccb7ed8" => :high_sierra
     sha256 "82e328f8c23068ad8dc2f722cf6872e0dfab0e68eb1b10813f1aaa6d0c667caf" => :sierra
