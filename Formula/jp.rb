22f240aa6d5b7d7752dce613edc91a9d1d9e5e89
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 11:40:51 +0000

    jp: update 1.1.12 bottle.

--- Formula/jp.rb
+++ Formula/jp.rb
@@ -6,6 +6,7 @@ class Jp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ee325c2512d2a069983175999db20d55c8718fd0f0ea000692e6517ac67b32b9" => :catalina
     sha256 "53127a663b20c7c0ac893d991330ca862a6eaa8f235586019e1b8ac33159bcf3" => :mojave
     sha256 "51045489ba9e8790a83a2a366709bd941d3a9e7c190f6c184bcf308b888496b3" => :high_sierra
     sha256 "b75e4ab3a48e2212babba26a4258645ae55eefa50a9ccac463991b05ce4c08d6" => :sierra
