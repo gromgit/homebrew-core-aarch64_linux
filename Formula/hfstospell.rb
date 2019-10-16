1532de9ee8d2be78451115f346b52d97cab328da
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 00:16:05 +0000

    hfstospell: update 0.5.0_6 bottle.

--- Formula/hfstospell.rb
+++ Formula/hfstospell.rb
@@ -7,6 +7,7 @@ class Hfstospell < Formula
 
   bottle do
     cellar :any
+    sha256 "686d0c070747980c0ba5305cb5a75e71d3c0f30d94701829553b6b71a09cca1a" => :catalina
     sha256 "9c6c69212f1e6784eba39c412fee40fc7f67e113f0ce905573d71dd1ea3e1c5e" => :mojave
     sha256 "2f9c344944d7e1ab33854b091421df503a9ae7b6d1ac052b3879616e85173086" => :high_sierra
     sha256 "75b751ea37a89444b7f815d3fb356c8fd8bbe6c60a07161f966bb32719431235" => :sierra
