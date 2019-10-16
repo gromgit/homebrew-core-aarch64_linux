6de572336a251d7a60cb1d931caf83d317d74c67
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 20:49:01 +0000

    tup: update 0.7.8 bottle.

--- Formula/tup.rb
+++ Formula/tup.rb
@@ -7,6 +7,7 @@ class Tup < Formula
 
   bottle do
     cellar :any
+    sha256 "76c67dc80a2317d0c03e98b90007b6ba0ee4350f0bfbfc6b923b21f7f41be587" => :catalina
     sha256 "fe62bd2762c7ff15e628b6c241c8f6acd81d190c03136c6edce2fd76afafe9f0" => :mojave
     sha256 "9a2e688be1a21af9fc0c2e9edb0e2a679eb7553356e59682037a760c6bd90b8d" => :high_sierra
     sha256 "fc8a299e3ed77a756edbcb957343d1cc95594126f23514eea729649a7fcc6071" => :sierra
