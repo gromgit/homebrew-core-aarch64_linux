ccc424507248a78a279b5a10f2cd95aeb3e70b67
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 01:07:19 +0000

    huexpress: update 3.0.4_1 bottle.

--- Formula/huexpress.rb
+++ Formula/huexpress.rb
@@ -9,6 +9,7 @@ class Huexpress < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "a17fe6ae136f94e859edbe7d65fc15466645c1d569fdbf363c841ec85477fa1d" => :catalina
     sha256 "f395a18b662d16c84b18e4594f67516a43e7a692879d68289c08ee652cedc651" => :mojave
     sha256 "53eec3fc474a553bdd70ad1c8dcb63714db5d059839748dcbc80b86380c81d4c" => :high_sierra
     sha256 "31ffed8cb0c04131abefe50aa06aa8154e3cf8e49fa24ebe5f4e6d456f6b3d9b" => :sierra
