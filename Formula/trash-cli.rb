77bd9f7f2781b71a4a2e8d9ee261bfa02fa6433b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 20:09:56 +0000

    trash-cli: update 0.17.1.14_1 bottle.

--- Formula/trash-cli.rb
+++ Formula/trash-cli.rb
@@ -10,6 +10,7 @@ class TrashCli < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6fafad8c264d9c9a12f2346998a39e7407a0fef9713b5aa98541af27be124ca2" => :catalina
     sha256 "63895ceb716e9cd3b45bc9dd676afbb973c49f82cf11445aca89ef3cc1e3da69" => :mojave
     sha256 "c4d324ed98f547cad585e0eaa348c7e8b26e9036c77ab7bbfcc856d76c1ddeb4" => :high_sierra
     sha256 "5cb6346603e9ba432c92d46bc20f8612dad0dfa4e32e1996e2c0cc1aaab87990" => :sierra
