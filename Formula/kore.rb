3567bc629a6eaf5395c08c515246e451ebaebd9f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 17:38:27 +0000

    kore: update 3.3.1_1 bottle.

--- Formula/kore.rb
+++ Formula/kore.rb
@@ -7,6 +7,7 @@ class Kore < Formula
   head "https://github.com/jorisvink/kore.git"
 
   bottle do
+    sha256 "abbcf36f378af2db9914b9f6edf1a4e4808723a9d0421056bd6de645f5393dd2" => :catalina
     sha256 "760a9977cc46ad5f46e5d9b3397af22e0ff1df60e33acc622851d7a8babb2234" => :mojave
     sha256 "0c76cfc29a1c006207b09015550a0f33b5c54a1aaff542c1e8843a6531c083c8" => :high_sierra
     sha256 "719bd8b11d2757fec07aa6558c95c500f2a7b6148a0ebbf2563d1012dc4eece2" => :sierra
