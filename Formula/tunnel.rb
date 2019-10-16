ea6ff680dc321bd183db08a21e07df9e0c525d53
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 20:49:01 +0000

    tunnel: update 0.4.2 bottle.

--- Formula/tunnel.rb
+++ Formula/tunnel.rb
@@ -6,6 +6,7 @@ class Tunnel < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7135b88f2141bf0c7219d6be3017d75780c2ae2840a7b968327ccf3df8c11664" => :catalina
     sha256 "a598e1baf67a2e824b0d0db255c5b59279d7d3f6d05adfe135b382f68c2a3eab" => :mojave
     sha256 "71758e150eef0eb5d826abaa3985332fbee0fd1b8ddb128ea1913c43df7b17de" => :high_sierra
     sha256 "9368538e7827ba06f7dad1aeece19833f721e78883eade09d83dd72bc71f3de1" => :sierra
