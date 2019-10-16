c98a20539761e4ca58767179b35d591cd234db11
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 00:51:22 +0000

    http_load: update 20160309_2 bottle.

--- Formula/http_load.rb
+++ Formula/http_load.rb
@@ -8,6 +8,7 @@ class HttpLoad < Formula
 
   bottle do
     cellar :any
+    sha256 "36fada1e1b8cbe35a9eb1fb2374c175a003d750f0560565c6bfaf6b90a17f748" => :catalina
     sha256 "d0d672723564b758fc3ef0721239e108ec063a395e183db033071200d5d9ee48" => :mojave
     sha256 "22e21275c49121c174024104f9b99c5f55d37e032ff7cae42bba89746c26bd88" => :high_sierra
     sha256 "a949ed2040faf49c7cdb6bf0110dfbbff465641c811e78a035998a4160170a05" => :sierra
