5b96952cc7e855cafcd703b5c6e1434f22144f16
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 22:15:01 +0000

    redir: update 2.2.1-9 bottle.

--- Formula/redir.rb
+++ Formula/redir.rb
@@ -7,6 +7,7 @@ class Redir < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a811c4d0057b6d190a615a1da5cfb6dbb7321310f41da5141397295e31ffe354" => :catalina
     sha256 "5681e33f5a5cb66759b5781989bef550558752c7cd3c1b3e4b590c5441a47082" => :mojave
     sha256 "8a94df616b4af201fe512de86ab7310bed38136397ee53b37d6f0a4a2729282e" => :high_sierra
     sha256 "cb7132731ff0121978a4e72208203d30d4fd91a10731fda2ac474619ab4472cb" => :sierra
