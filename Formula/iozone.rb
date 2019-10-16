459fbe6708110a056bb91f26aac41192256f5ae7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 10:07:55 +0000

    iozone: update 3.487 bottle.

--- Formula/iozone.rb
+++ Formula/iozone.rb
@@ -6,6 +6,7 @@ class Iozone < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "704c25cbbe9ac821b7f4bef8c23b5ac4df5117d15574062f77247769bca8e6ef" => :catalina
     sha256 "ed77f59ddfb45e302b60e80d2f4a842e838af5973df7e6dc7d95a3d27c06d0e8" => :mojave
     sha256 "c0f0f7a10f585f40435d1d0a07322739925c9cae2afeb8c8714fe9e121a41572" => :high_sierra
     sha256 "09eda4f32813be63ba4e05dee24a9cac6805667d022fb8a2ed42ef8b0fafe9dc" => :sierra
