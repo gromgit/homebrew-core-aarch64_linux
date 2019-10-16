594198de22e48ed919fe7ca90918304bc54870de
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 20:58:01 +0000

    twarc: update 1.7.3 bottle.

--- Formula/twarc.rb
+++ Formula/twarc.rb
@@ -8,6 +8,7 @@ class Twarc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9337719eeb53a8e84dbbd3c09af25c32ed24d2fa9934868986b8dcf2feb58e49" => :catalina
     sha256 "3f247f84bdfefa5856eea5a7e8e81f0acd1456bf68ab87fd0f2afb612a940c22" => :mojave
     sha256 "afe2c1fb2379f1d32c6e004823310908850394ed1af33f622afd6165b4914c5e" => :high_sierra
     sha256 "fd651808b565af0bbd66744e1452ce6675c851cc6940b5ce3a4dca894e594fce" => :sierra
