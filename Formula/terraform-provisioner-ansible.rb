c2d0e653c8e22194cfcd0770b5fd979a7677174a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 18:51:24 +0000

    terraform-provisioner-ansible: update 0.0.2 bottle.

--- Formula/terraform-provisioner-ansible.rb
+++ Formula/terraform-provisioner-ansible.rb
@@ -9,6 +9,7 @@ class TerraformProvisionerAnsible < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "664cff934260d219317c5b875a34d7d9e223f7b0ad3d979943a9d33c7bc6928e" => :catalina
     sha256 "47e33e69955c8da7284077ca6ff8b412433a73955f0df91d477ead370e3b99cd" => :mojave
     sha256 "9c5773e4d576ae2f7e28438587b42170a33a6767c13805ed7ef372ad102bc139" => :high_sierra
     sha256 "a56474f71703f7d660a17ed6faf039e98ccf50d32ce82b2c92fcc907f012ad78" => :sierra
