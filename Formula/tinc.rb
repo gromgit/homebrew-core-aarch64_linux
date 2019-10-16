40ff6d9517f2239fff1b8caac1be62da78ef953e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 19:39:36 +0000

    tinc: update 1.0.36 bottle.

--- Formula/tinc.rb
+++ Formula/tinc.rb
@@ -6,6 +6,7 @@ class Tinc < Formula
 
   bottle do
     cellar :any
+    sha256 "fcaaca6b5abf4f30a55149f41871a7c4ec99fe8a9dc87ddb68ea735c03569a66" => :catalina
     sha256 "ba34dc41517f617c4d61d61e2f76cbafd9b75aa5edacc894e5b24e97cfb269f5" => :mojave
     sha256 "a5ec2ae5f1e6252f80f33158bb0a1140e29764ed1f2e8754dcedf50e4fb49290" => :high_sierra
     sha256 "923b15d1dcd7aafbb566f83edc9cced61b13379e857243bbe28b2755270b542d" => :sierra
