0b9366d844b237d3543f0d17b940074a8bc000ed
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 11:40:51 +0000

    jp2a: update 1.0.7 bottle.

--- Formula/jp2a.rb
+++ Formula/jp2a.rb
@@ -6,6 +6,7 @@ class Jp2a < Formula
 
   bottle do
     cellar :any
+    sha256 "1ded015934d8e95c8fa21440973a9f57dfc8b607b46719e0698b61e5daee257b" => :catalina
     sha256 "97f05a0bb8f9ee5b075e21aff16503284592106cc87d384c1b85936128690345" => :mojave
     sha256 "36bd941b1b215c93b6934a085b9cf8fdaad2ba2ce47a8fbe096e53a144932201" => :high_sierra
     sha256 "a45231943df5bffc1589114b20b5b6c9745f909fd1e85db63da40e28bec02709" => :sierra
