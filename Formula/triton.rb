6b48e8e1c1d3075d38f8eeb58bd232ace8d93c2b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 20:31:39 +0000

    triton: update 7.5.0 bottle.

--- Formula/triton.rb
+++ Formula/triton.rb
@@ -7,6 +7,7 @@ class Triton < Formula
   sha256 "b6349e353897391e29a87101b22baca1db115d06d29ae0869b6630d4bbe70926"
 
   bottle do
+    sha256 "6e5e7bcad1e1ac9ae5736d65241ef59f92d80fbfd0473e50a316e02774ceff2b" => :catalina
     sha256 "7257584fdd668f5dd0879a67754a37bb4c270b8b8367c9c6c4f7531aaaa2972a" => :mojave
     sha256 "bc881b355bb3582c34f2a6deeaf44722ae0d2f13c4210242bef4f7bfd7889212" => :high_sierra
     sha256 "c345740c4b8d3a40dbf5d17e563d5072c52f27628fbc80462df1f989e5a846b8" => :sierra
