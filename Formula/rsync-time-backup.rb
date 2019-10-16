8d1e2acb224c646feb611684627da18d21e873f1
Toshiki Murayama <murayama.toshiki@gmail.com>
2019-10-16 21:14:00 +0900

    rsync-time-backup: add head option (#45389)

--- Formula/rsync-time-backup.rb
+++ Formula/rsync-time-backup.rb
@@ -3,6 +3,7 @@ class RsyncTimeBackup < Formula
   homepage "https://github.com/laurent22/rsync-time-backup"
   url "https://github.com/laurent22/rsync-time-backup/archive/v1.1.5.tar.gz"
   sha256 "567f42ddf2c365273252f15580bb64aa3b3a8abb4a375269aea9cf0278510657"
+  head "https://github.com/laurent22/rsync-time-backup.git"
 
   bottle :unneeded
 
