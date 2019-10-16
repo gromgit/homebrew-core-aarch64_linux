297a8b910820babc8e012e8325e9c21ca7d1b2d0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 19:22:35 +0000

    timewarrior: update 1.1.1 bottle.

--- Formula/timewarrior.rb
+++ Formula/timewarrior.rb
@@ -7,6 +7,7 @@ class Timewarrior < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "021d9b46180d9644c6c5096c8b687c01ddc8fdf517e323870116bc02cbcfdeff" => :catalina
     sha256 "48af9bcedd665d7c2541eb3edc9ed14bccf26a1b3861e295b971ed1d8c2cedc6" => :mojave
     sha256 "add032f6bd1e1b67ff81902522473f6c46e232a097d338b711110a8dea7fc622" => :high_sierra
     sha256 "79da22a5383fdd5e22eff38ac9deb005c745e78764e1278909b8488cc770dd0d" => :sierra
