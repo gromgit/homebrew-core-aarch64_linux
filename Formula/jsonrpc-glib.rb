c5d9db246730c9fabb6fd16cda61f0ab7d76def1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 12:03:17 +0000

    jsonrpc-glib: update 3.34.0 bottle.

--- Formula/jsonrpc-glib.rb
+++ Formula/jsonrpc-glib.rb
@@ -6,6 +6,7 @@ class JsonrpcGlib < Formula
 
   bottle do
     cellar :any
+    sha256 "1c302e75c1276b6fac906158727353657f8a6a353ec9a395ecbae99a5025dd18" => :catalina
     sha256 "42081e5890e3bfe1be1d29bc08d2ca9376d9e2493c0a7f1828497e59ffa8cc54" => :mojave
     sha256 "7fc3931e31121ca0f83710193591bc1ffe2fed9c9300aedead32db047b39e940" => :high_sierra
     sha256 "b47f0d67cfe99c39b1d789aabcc4e19384afc7477273687b46e359064f79fa32" => :sierra
