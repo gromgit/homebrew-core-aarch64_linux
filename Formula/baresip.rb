ef4977a3256b24ddbc52d500b931c9ee7461edf1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 07:01:42 +0000

    baresip: update 0.5.10 bottle.

--- Formula/baresip.rb
+++ Formula/baresip.rb
@@ -5,6 +5,7 @@ class Baresip < Formula
   sha256 "393fb010410d3fc6a4879cfda235f0ec98439be5d1dca02c15f3416f7110a7fb"
 
   bottle do
+    sha256 "2738fa4c1740e0c54aad92e122ffd3351a040d58867233a74e7ef93643cf302f" => :catalina
     sha256 "a7d010f0c6ea77cfe38e34a26d163fda8994c2e903988b70d8771e685eeb3c5e" => :mojave
     sha256 "2d21997680aa9d6faba15c4e786afddf9c76f2fc51887475de59d56262fcf785" => :high_sierra
     sha256 "b60def84ebf98e806916f400f602036824b03d3b8b1dd97d44929fe2004ad1ff" => :sierra
