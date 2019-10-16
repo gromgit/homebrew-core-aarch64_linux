89bd5828dc8d0fe46f9c523d5f22af9dc6835094
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 00:11:40 +0000

    hevea: update 2.32 bottle.

--- Formula/hevea.rb
+++ Formula/hevea.rb
@@ -6,6 +6,7 @@ class Hevea < Formula
 
   bottle do
     rebuild 1
+    sha256 "fc03dd618603653bbf60f56d5ac54609f1c684fba7a8a9aaddddb5c84aac82e7" => :catalina
     sha256 "f02097ca2ff65b7ee466ba597bb6d1c0a1fbbf381de3c3922cb31150fba941b7" => :mojave
     sha256 "527209a56a45e2e490978db70e8b2c6343a7e8e283fefeedfc6bb9cfe37843c0" => :high_sierra
     sha256 "b8b5d9cd3c34fe23a704e143b831284beda8601f6f6d037d37695016dc034a55" => :sierra
