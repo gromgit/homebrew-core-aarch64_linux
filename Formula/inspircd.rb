eb9bc7d3b00d20fd7d409d5cbafd8a05a905730c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 10:11:31 +0000

    inspircd: update 2.0.27 bottle.

--- Formula/inspircd.rb
+++ Formula/inspircd.rb
@@ -5,6 +5,7 @@ class Inspircd < Formula
   sha256 "6bc1956bd6a7d2d463c646f1563c99cb507f2f214e51d6ac9c70906ac27aae73"
 
   bottle do
+    sha256 "8d0b4455f893654aa729369b74ffed990fc554fd9b6d4902fdf1b8ee66e44c5e" => :catalina
     sha256 "de7c0627908828d4ec8b317cbf174f043598b38c1916fa64dab5c8b76411ac2d" => :mojave
     sha256 "679569e694341552a1cc4d50240ebbd28f77850f0adfc482d0e3ddc1a7736495" => :high_sierra
     sha256 "45b3a6258e87954cb82f98d97ca87d54f4063a06f5b8cdff645ba0be4bf994bb" => :sierra
