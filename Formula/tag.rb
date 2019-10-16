a7e9febd7b34b4b82c6ac9099d64edb7ca128cf0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 18:00:13 +0000

    tag: update 0.10_1 bottle.

--- Formula/tag.rb
+++ Formula/tag.rb
@@ -8,6 +8,7 @@ class Tag < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e1572ae47d558d60983f7c1cbe9ae42a5c7f2dcb950762ab6c721e81351f5657" => :catalina
     sha256 "ee5dbe68476b6ae900b92486f3dc3c7a9755296c1fee54a75cd64c7d6af66763" => :mojave
     sha256 "5801c9fac7b1a4bad52f02fd8a09b64050ebc52515bd96115153c7049bd4619f" => :high_sierra
     sha256 "5711ce58bd5b224252f1869f84f937c6bca0775bf4c86a6a1168418c1218dc98" => :sierra
