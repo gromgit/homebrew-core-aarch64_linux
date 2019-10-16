23c9134c091b759da352845899618ba9721954b9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 18:19:44 +0000

    tclap: update 1.2.2 bottle.

--- Formula/tclap.rb
+++ Formula/tclap.rb
@@ -6,6 +6,7 @@ class Tclap < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "025227d952333920194194626d4e80eb02439a7de957a6722bd672a0cb2c7631" => :catalina
     sha256 "31d001dc926c1faf748a2dd0ad34fe9f3c3908400eac998802ce36ba78fab794" => :mojave
     sha256 "8e85cfa71594680dc52d5ff18d93cf585fc5990c28316f0b60f42584ff3a2697" => :high_sierra
     sha256 "8e85cfa71594680dc52d5ff18d93cf585fc5990c28316f0b60f42584ff3a2697" => :sierra
