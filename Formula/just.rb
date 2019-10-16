f66854a4d089430c1c13ca7e5c19e9d1860af52e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 12:18:42 +0000

    just: update 0.4.4 bottle.

--- Formula/just.rb
+++ Formula/just.rb
@@ -6,6 +6,7 @@ class Just < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b3933bb34c7fc20b3933ffe0f9fe7c3727866c8476f48ce4cfc62f44f1fff6fb" => :catalina
     sha256 "501d83b4a2d6d5e24f1429320909aca6c05787884a6ad690c55d9e6eb719d957" => :mojave
     sha256 "7df7779bd57aa0c48e7f97e0d617a76591e88dc6d5363b57c187f96f1ac594ab" => :high_sierra
     sha256 "23f3ddf400ef6ce95436539da360e21e3c89a373ad731cfb15a16ff8c5f3b82a" => :sierra
