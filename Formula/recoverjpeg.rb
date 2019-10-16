a777d1e762db73fb13c674a432c8375f6707b0ff
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 22:15:02 +0000

    recoverjpeg: update 2.6.3 bottle.

--- Formula/recoverjpeg.rb
+++ Formula/recoverjpeg.rb
@@ -6,6 +6,7 @@ class Recoverjpeg < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7b9e6cc406462435f7efc7bf6686205f29beba8bc5f481c6bf754a53d37f9411" => :catalina
     sha256 "89bd9fe522bc64508a3c2925edda2a3ae3cfc32e7ff59f7e70eb1069352129b2" => :mojave
     sha256 "ce460e293cc4c4c5bf3650cf9860e7b06654017473c414dc88c1df9d82e8466e" => :high_sierra
     sha256 "453b0d2c88be1b885407e38900ab0303481e9957ed5c160cb8e6456b6a2f81c2" => :sierra
