be13da549bb6ada067ef24c4622a4d5025e45592
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 12:58:03 +0000

    khal: update 0.10.1 bottle.

--- Formula/khal.rb
+++ Formula/khal.rb
@@ -10,6 +10,7 @@ class Khal < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7a5ff88f863f07170a7ac8244c7bbec213690ed09b5b6d47a0d38f757230f790" => :catalina
     sha256 "65c0e242110629b9aba6fc3fb7dcdd0b48c256465cbf8e4bdce6eca10ef791b0" => :mojave
     sha256 "e64129f4743de673ab7ecadf45943cf920e1f263a943e13f4bb2c138cc542855" => :high_sierra
     sha256 "f3fc8e90d8d5e5f452a56954563d2c5e40f17cbda7f1552ad9ad2bd5e8bda30a" => :sierra
