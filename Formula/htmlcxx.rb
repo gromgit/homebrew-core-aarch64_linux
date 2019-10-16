b7ef64324d16ab040022e893185678411bd5ff6e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 00:51:22 +0000

    htmlcxx: update 0.87 bottle.

--- Formula/htmlcxx.rb
+++ Formula/htmlcxx.rb
@@ -5,6 +5,7 @@ class Htmlcxx < Formula
   sha256 "5d38f938cf4df9a298a5346af27195fffabfef9f460fc2a02233cbcfa8fc75c8"
 
   bottle do
+    sha256 "8414d919ae850983832803af525e8b98d3e5aa106c47b05f420d77020c7c99ca" => :catalina
     sha256 "e910595c43c028e25e0e0a44203e3c95b229162ea89678721b4a7f6e22974aca" => :mojave
     sha256 "062a4b1629ab6f28e59ef0ea15c257c8bfd9e3646f3342fbfe14268727be7649" => :high_sierra
     sha256 "4407cb1a50e8d629db9b93bdbbbf2a0892967611f7e579c49c0d084769f8a5ca" => :sierra
