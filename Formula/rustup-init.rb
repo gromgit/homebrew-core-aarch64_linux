4f3af949730967bab01312447c36725dfedd82a5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 17:52:56 +0000

    rustup-init: update 1.20.2 bottle.

--- Formula/rustup-init.rb
+++ Formula/rustup-init.rb
@@ -6,9 +6,9 @@ class RustupInit < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "031768f5038a938da162c65c1b361762b4ebb4ed48f2f45a81aef09db5b64004" => :catalina
-    sha256 "6321dcd0c856fba4a74454ed0226084d99dd58d731f98f01720f497aee25b1a6" => :mojave
-    sha256 "da5882848bf593e7ddda3a80033598dd2fe62edeba0b5be17cf4df4e57aa48e8" => :high_sierra
+    sha256 "729d96e5da37a1973b5447ae27bd02854426b7e082209d1876e8612fe6d96475" => :catalina
+    sha256 "a8b25151b5d1f7e7030659af4dd8f4c2de04179d07ca49cf985b493409db02d0" => :mojave
+    sha256 "56b724bf10782cffb560484bc8d56c5ff6b546f9a672b83685db58c399e4bc51" => :high_sierra
   end
 
   depends_on "rust" => :build
