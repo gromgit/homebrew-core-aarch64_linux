860d8e89323af3145497c32c196bf28ac91ab8f8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 23:19:53 +0000

    rkflashtool: update 6.1 bottle.

--- Formula/rkflashtool.rb
+++ Formula/rkflashtool.rb
@@ -7,6 +7,7 @@ class Rkflashtool < Formula
 
   bottle do
     cellar :any
+    sha256 "19915af37cac5f04cea6d0aa71a533ce6fd2a9aa549820286736b526e4be80cc" => :catalina
     sha256 "192628d1c348870f6ea1bc381aeb87023cc0bd19b2c198ea3c14b63e954db88e" => :mojave
     sha256 "72e99558b94cd4e26a5aeb62209c99e03f8e91059fbfbe83bdfbd755fc83f54f" => :high_sierra
     sha256 "cbeb2509bcd210026250c915a9909e8f056e9e2da1f599d7a611695c334f4966" => :sierra
