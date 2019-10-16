de6f1e39cb6330a8d9828b0505bfbfa5b6acbdcb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 17:38:26 +0000

    ksh: update 93u+ bottle.

--- Formula/ksh.rb
+++ Formula/ksh.rb
@@ -10,6 +10,7 @@ class Ksh < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 3
+    sha256 "f293f9efa89e7b6cb6318ac6406e61de92b05b85f9c1fd17affccb38e941b64c" => :catalina
     sha256 "d6a44587ba5c9ef60ccd4df40bb4e5b4be2a6fbf6c3269d1732ba7376b341c8c" => :mojave
     sha256 "5ca742b83c55b817a0fa09f737d73530c0f73d89618c60a19ec2ea5251ef8cd4" => :high_sierra
     sha256 "37a9f6c57702896fabe762fe90ebe454f04f3523aea311a28d34dc884d53736e" => :sierra
