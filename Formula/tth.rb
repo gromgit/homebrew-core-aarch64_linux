7475233fbc85aac194fc7ad09153174a35218a87
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 20:39:55 +0000

    tth: update 4.12 bottle.

--- Formula/tth.rb
+++ Formula/tth.rb
@@ -6,6 +6,7 @@ class Tth < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b28e90b9d5639deb3023f606eefe1ea6cb8525a58685b1558cf80a3e3e6b3f8c" => :catalina
     sha256 "9a2a248e2b669c30cdb8c085d94be14b3b2fe5092dd76a1f960fa74fb3e554f7" => :mojave
     sha256 "5b06f95fbccf3847cbabee3dec070cae76763acef5b0801e0870359639a20880" => :high_sierra
     sha256 "22b58ab69a94f8e031efa6662179b14a2ba57bf0582174eda559abc0e3cf9701" => :sierra
