77c5fd946ecff68730859fe8b394301910466bfa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 09:50:54 +0000

    inadyn: update 2.5 bottle.

--- Formula/inadyn.rb
+++ Formula/inadyn.rb
@@ -5,6 +5,7 @@ class Inadyn < Formula
   sha256 "f7faf0be4f0b4bfa1acc811189a9ed0a58bc367e48ea31c283920a2ef27cdc40"
 
   bottle do
+    sha256 "3f03070bad5247eab88623753d6c4c3f60c197cc7c629324afbaefaf0d9d8560" => :catalina
     sha256 "6e74812d53caa2ae34d2c571e7e2a0575de36aeec1504c0c840ce7e5cdf7e1b0" => :mojave
     sha256 "870e29d1856c85414d2566d2d5a9d39de1c2aa6846c6c9c3647c29ea4be588b8" => :high_sierra
     sha256 "d2ee8d33a5069f120081ea1c70ecd600bc516e7e0b09658d3196f1df6e40029a" => :sierra
