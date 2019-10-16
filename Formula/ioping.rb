13d174d971fa391450f5c2c24dfa119ee43068d4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 10:11:30 +0000

    ioping: update 1.1 bottle.

--- Formula/ioping.rb
+++ Formula/ioping.rb
@@ -7,6 +7,7 @@ class Ioping < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1225688038ff2ca15f26f6017e34b95da42be3a835a61bd7d7479776475403c1" => :catalina
     sha256 "86ccaf89bf56a5f25f3d91463794d6e26c0dd19a37f72532e6d9c475881b7756" => :mojave
     sha256 "f7ec7717b3a117c9d57ddc62649e5635311ace177db679c57798257c85cfba63" => :high_sierra
     sha256 "f2f45cdd184b2c1e8eaf401123950eb788832a2a2e4234e96e7f31566b63a9bc" => :sierra
