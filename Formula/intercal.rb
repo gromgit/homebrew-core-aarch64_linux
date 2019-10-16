572c23f85ce918959926a552ac8097d24f917d86
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 10:11:31 +0000

    intercal: update 0.31 bottle.

--- Formula/intercal.rb
+++ Formula/intercal.rb
@@ -5,6 +5,7 @@ class Intercal < Formula
   sha256 "93d842b81ecdc82b352beb463fbf688749b0c04445388a999667e1958bba4ffc"
 
   bottle do
+    sha256 "a2c1673fbed3d331e725694196acf9ea4cd6bc6df3b86568af3e67ee90d70b30" => :catalina
     sha256 "d048d5c58fd1fc3b17c44103b3bbddd445a657415c215916587d9eb8e7f9c2da" => :mojave
     sha256 "c0569e08915adc912bdc3fb149d0d3c50e7a2d941fff8b2d951b22fcfaf4539f" => :high_sierra
     sha256 "b00c959878aaead39f9106ef199d7082b4e1a62ef6957f11796a99650678c9b2" => :sierra
