40a6e66fe812acb893b7fed0b0fd9ec8196deaf1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 18:53:28 +0000

    tdlib: update 1.5.0 bottle.

--- Formula/tdlib.rb
+++ Formula/tdlib.rb
@@ -6,6 +6,7 @@ class Tdlib < Formula
 
   bottle do
     cellar :any
+    sha256 "99bba47b0129e9654b18c60386e138c33f71454aa662886c9366a043b7802528" => :catalina
     sha256 "9be0460254a6d1c705c48703fa4acd66ced464e3966d29cde49b82738806ce5b" => :mojave
     sha256 "7d3964bf0288801a360288458d604ab60ed414393d77d674913e93156fa3cb55" => :high_sierra
     sha256 "891c74200c0053cef624e5521b4ec576cd2c133e41433fd16ea42dac0b6672d0" => :sierra
