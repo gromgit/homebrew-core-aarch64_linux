ed5e43daecf69a49f9134275f90daffe46f2553d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 11:40:51 +0000

    jpeg-archive: update 2.2.0 bottle.

--- Formula/jpeg-archive.rb
+++ Formula/jpeg-archive.rb
@@ -6,6 +6,7 @@ class JpegArchive < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a48ad5ec6802a9a1ba2e689b49c2dd4f6e8edd98950c3e3104ff495953d32e34" => :catalina
     sha256 "1fe424d0224c4d76e83aadcdbdbe7b2a4e63873651049ee13c66bc8c55fa916c" => :mojave
     sha256 "2bb41fa08a695f6552524c21bd5e32434f248f8e41bd11574ce8a9d4cf104ea1" => :high_sierra
     sha256 "fe29c7341dffa1ac8d52635ca217e5808c25a98c74748d71c389d951a46fe1cd" => :sierra
