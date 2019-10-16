93770ad39e6e17e0e737000f8b937cff0e08d206
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 21:41:35 +0000

    rc: update 1.7.4 bottle.

--- Formula/rc.rb
+++ Formula/rc.rb
@@ -7,6 +7,7 @@ class Rc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ab871610d857058773a87f70ad995a5e02fdeb1e6fe3d699e2051892ce60af84" => :catalina
     sha256 "f14ceeb0e4315379e2052e39a24fafb529f841428b1a64e3009cfd62769b9e4a" => :mojave
     sha256 "c2ee55c504be78889adc7d0cba962528f995bf222dc77ce5a6b930210851294e" => :high_sierra
     sha256 "627e45477eabd5854e3c5f39af5290befd43d03b385d1b20f0ce4b49636fd2d9" => :sierra
