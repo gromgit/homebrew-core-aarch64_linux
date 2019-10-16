8a2d274c481df514780623f8f986404557dcea32
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 20:39:55 +0000

    ttfautohint: update 1.8.3 bottle.

--- Formula/ttfautohint.rb
+++ Formula/ttfautohint.rb
@@ -6,6 +6,7 @@ class Ttfautohint < Formula
 
   bottle do
     cellar :any
+    sha256 "542ada8a8e7deaa7fc3f14f2fec704b2570bec6baa07396a37ac7b6d280cfab6" => :catalina
     sha256 "04ca530843887602e80fde17d24f4ed8e19d1248bd71c81c925c161770dbdf56" => :mojave
     sha256 "a6573ae816a7555d62308759c2d64f9fb955ba056d856d904a522996ba0a0c83" => :high_sierra
     sha256 "d45d8d85d3ffa162326ea8e2f63778f4fe583c41bc316c15c5a63b3625beb0ff" => :sierra
