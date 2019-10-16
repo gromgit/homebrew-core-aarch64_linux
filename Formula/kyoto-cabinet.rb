52351c83b7ae36e2385e169272f724783126d546
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 17:42:50 +0000

    kyoto-cabinet: update 1.2.77 bottle.

--- Formula/kyoto-cabinet.rb
+++ Formula/kyoto-cabinet.rb
@@ -5,6 +5,7 @@ class KyotoCabinet < Formula
   sha256 "56899329384cc6f0f1f8aa3f1b41001071ca99c1d79225086a7f3575c0209de6"
 
   bottle do
+    sha256 "444a9c86b36b081cc9ad31ef68d8c4e03f0861b9f0603a7fe5c99780cdab3926" => :catalina
     sha256 "ddd2f1b0f1985ad81b04b29dbd54f95c5f7c88f7427b559e9f298a6473a820b0" => :mojave
     sha256 "e4b99c22b5aebf85986e5c172ec61768833708acbb04318335f6641bea1f77ef" => :high_sierra
     sha256 "04ef198a6638dabdee27e881df9b16970eadc724f2f663a01edee7950b38b85a" => :sierra
