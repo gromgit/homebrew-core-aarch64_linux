05c8966a5779543b17fe486bc2dcac46aa7c129d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 10:57:43 +0000

    jdupes: update 1.13.2 bottle.

--- Formula/jdupes.rb
+++ Formula/jdupes.rb
@@ -6,6 +6,7 @@ class Jdupes < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7de156d158f3a57689a6db010bc786e1becc38c594a17114e0e7992e1e541925" => :catalina
     sha256 "5914122e74633a5d22c16102e0ffbf43c6a5d5903ecc75d41b050339af913b96" => :mojave
     sha256 "f2132c6e2a0a1402a0dcc0205efc2ae80fcc1e70508f1910db3113de0d64ce1d" => :high_sierra
     sha256 "95ce9083f5ee6dabdaae7ac9165b51231aa8df5240452f3eaec9c172064082cc" => :sierra
