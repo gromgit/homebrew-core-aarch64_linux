8b31fc243dbdfa6d538e7d9e0bd4fd1c04b99e61
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 11:27:18 +0000

    jnettop: update 0.13.0_2 bottle.

--- Formula/jnettop.rb
+++ Formula/jnettop.rb
@@ -8,6 +8,7 @@ class Jnettop < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "d519f88b031322183f9cfd7303f9d139aecd807037f5d5043997acb2d3324cab" => :catalina
     sha256 "902b1e9e69c982a84e38b09e2f0b15bc84af94028fb32138fd769efffbc6ddbc" => :mojave
     sha256 "944957cbac7c457d3c4ee130e8ac457ebf0f2387c7231fa2b85ead897cc77e8a" => :high_sierra
     sha256 "190334e1019dc9957918164f3fe920607f735edcba3a24296cc634fdd4a70e54" => :sierra
