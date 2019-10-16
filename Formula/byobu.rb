b06706fdd5eee3c04403d27b02b58dc7db21027f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 08:41:58 +0000

    byobu: update 5.129 bottle.

--- Formula/byobu.rb
+++ Formula/byobu.rb
@@ -6,6 +6,7 @@ class Byobu < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3fc6479c9f8ade7976cb70a62eff7aae03cb660f9dc1fc053e1cfe63d8cdf065" => :catalina
     sha256 "6d47abdde37ae912d3ffabf2161284914cd5cd75ec68b2308e14f1e7cc560e1e" => :mojave
     sha256 "6d47abdde37ae912d3ffabf2161284914cd5cd75ec68b2308e14f1e7cc560e1e" => :high_sierra
     sha256 "2071147e4d137922442b615dfe4bc13513b85ba1766aa36c7367aec29097ec2c" => :sierra
