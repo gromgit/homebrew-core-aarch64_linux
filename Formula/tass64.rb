a1f263e9b13bc8fe75dce038db40dc2100c0d70a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 18:19:45 +0000

    tass64: update 1.54.1900 bottle.

--- Formula/tass64.rb
+++ Formula/tass64.rb
@@ -6,6 +6,7 @@ class Tass64 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7c519f6bca815c5ef0726a59b7c6bef323a3ab9c62af8339e52bec41784d402f" => :catalina
     sha256 "a50a7694850bb4da92edf5aa79e509935ae16951f1e362c31ffe22f2c15e9858" => :mojave
     sha256 "db03b63e4f453c52de9b9b22c9596e8f3b45b71c1fcab8a039a71def6186906f" => :high_sierra
     sha256 "ec0c831022ccc4446820bc73076dbb3c1f17c80cdcb3af8c21a62b801f27561b" => :sierra
