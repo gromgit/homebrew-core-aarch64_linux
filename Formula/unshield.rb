12c1764fd6e61dd444695fdcb249d5f20ccf39e7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 06:55:43 +0000

    unshield: update 1.4.3_1 bottle.

--- Formula/unshield.rb
+++ Formula/unshield.rb
@@ -7,6 +7,7 @@ class Unshield < Formula
   head "https://github.com/twogood/unshield.git"
 
   bottle do
+    sha256 "d64e0c93743d7d50858bd5c46c76b8fa79183b5ee4643361202f53378a88cc05" => :catalina
     sha256 "ec5db176e7f9557645cfdb63062802d37a8e516f39f1e53037e37ed398992b3b" => :mojave
     sha256 "c68a5391b55e5101979c69d174160564d88edc7263afa140fd69ce289c6662ed" => :high_sierra
     sha256 "96cc0aa68d191d1bc98d09a48abaa44b58b4e979bfcec3b2abc384c30d56684d" => :sierra
