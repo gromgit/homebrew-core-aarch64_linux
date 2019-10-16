57412f840439106c6e597afac1e600f70f218f4c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 06:33:20 +0000

    haste-client: update 0.2.3_5 bottle.

--- Formula/haste-client.rb
+++ Formula/haste-client.rb
@@ -18,6 +18,7 @@ class HasteClient < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d7b5efc8934cbfb2534db7db7b8418142472f980b8f4165c317ab51cc4f14824" => :catalina
     sha256 "c38551ce841f7a3cd825e1ae20b774836aba13fe6e129c1539eadde9b9e64a02" => :mojave
     sha256 "a97b7aaf38ec730bffa45ffc073ccf4921b4e5714069a21bf63e682a9d21527e" => :high_sierra
     sha256 "746af59be7c010e6e13b67d1f98766c0237061eabca601e5f0cad935e1c648bf" => :sierra
