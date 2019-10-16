fbbd20d2b63ff76994dc85529d49cf312518c57a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 19:52:47 +0000

    angle-grinder: update 0.12.0 bottle.

--- Formula/angle-grinder.rb
+++ Formula/angle-grinder.rb
@@ -6,9 +6,9 @@ class AngleGrinder < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "fb9320f378795c456d775614c7a08e0c2df7c447cf3be700142bbef988f59f41" => :catalina
-    sha256 "e7e27623aad3f772486810f151621f93c9112d58297ede293d7cbde051f4553e" => :mojave
-    sha256 "4071b07d97fab4660b6089e3f51e0a791ebe36170283d410fa91aa0d4cb84105" => :high_sierra
+    sha256 "4a3e617eb5f8bae6828661ef71435709e023bb159fd841186873f43e70efd340" => :catalina
+    sha256 "9dcda46bbacf98eedfa9911fa82e6cc4372f896bdbae20370c9b70aed344a83c" => :mojave
+    sha256 "ac57327f819d4fe13f54366f7927c1765e030d84e2e62d22ad468f8769912f10" => :high_sierra
   end
 
   depends_on "rust" => :build
