259bef9079d5ff220b593d5e3f8dc9767ea31df2
David Christenson <mail@noctem.xyz>
2019-10-15 21:38:20 -0600

    numpy 1.17.2
    Closes #45393.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/numpy.rb
+++ Formula/numpy.rb
@@ -1,8 +1,8 @@
 class Numpy < Formula
   desc "Package for scientific computing with Python"
   homepage "https://www.numpy.org/"
-  url "https://files.pythonhosted.org/packages/cb/79/96df883cd6df0c86cb010e6f4ff790b7a30a45016a9509c94ea72c8695cd/numpy-1.17.1.zip"
-  sha256 "f11331530f0eff69a758d62c2461cd98cdc2eae0147279d8fc86e0464eb7e8ca"
+  url "https://files.pythonhosted.org/packages/ac/36/325b27ef698684c38b1fe2e546e2e7ef9cecd7037bcdb35c87efec4356af/numpy-1.17.2.zip"
+  sha256 "73615d3edc84dd7c4aeb212fa3748fb83217e00d201875a47327f55363cef2df"
   head "https://github.com/numpy/numpy.git"
 
   bottle do
