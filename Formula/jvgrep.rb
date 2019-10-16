696a698a6757f02805ce3ffdec1a3ba164f6c4a1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 12:18:41 +0000

    jvgrep: update 5.8.1 bottle.

--- Formula/jvgrep.rb
+++ Formula/jvgrep.rb
@@ -9,6 +9,7 @@ class Jvgrep < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a58a9b54d4c3166f6b318464990196ba995430d3a225e6a322861b195c70fccc" => :catalina
     sha256 "b3379c1f00e90881fb0e5d4f684cd62208065e0339f13d810480a2d4c7f45455" => :mojave
     sha256 "2bde2644eb0412fb14bc974926756e8cc5da2b7e9eec55af8a16dbae5a5ef9bc" => :high_sierra
     sha256 "c0ad60bf13a602161c77c1d1140ffc22c8c08680fc7ec3d47914b0f8159c5b93" => :sierra
