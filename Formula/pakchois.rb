0afb7b5b4e0224dde059226efec0b9b6ac4d4c37
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 19:38:16 +0000

    pakchois: update 0.4 bottle.

--- Formula/pakchois.rb
+++ Formula/pakchois.rb
@@ -5,12 +5,11 @@ class Pakchois < Formula
   sha256 "d73dc5f235fe98e4d1e8c904f40df1cf8af93204769b97dbb7ef7a4b5b958b9a"
 
   bottle do
-    sha256 "40909ddce4f17e53dd3b6bd68b61c45cfd824ccc486fe2bf0e76be5f55d5681d" => :mojave
-    sha256 "ee7978dad7998e747e468f1b9afaa692304efb2ca857d4c0903945f030841fb7" => :high_sierra
-    sha256 "84a90b245c59676817d4c9100d64d7747068e0d3557fc5c3218d8a83a98f78fe" => :sierra
-    sha256 "b02057a2cc01daa05754c537820b58d7c77b632fc5fdb2a6f6dcec77341fe65b" => :el_capitan
-    sha256 "30a06a914f2025d7d23dff48fa8523be455bf925a3282a8c35f56779fd8bd27a" => :yosemite
-    sha256 "03d6ab9d51bdebf61b3c415908e222467fd31cefc4811200eee9e407c604f7f5" => :mavericks
+    cellar :any
+    rebuild 1
+    sha256 "0312304691fe567185eb8fe854d6ca036f887c39ae4ca5652cc53bd0f837cb44" => :catalina
+    sha256 "5c6f1f39ca7bd268ba0df6f1f0f24a167aef86c77257fd6f8fccee7146b94c0d" => :mojave
+    sha256 "0ff5336b2165a755efeab1185edcde604ddc77f3cc359d5c41a07f3d5d8b9c0f" => :high_sierra
   end
 
   def install
