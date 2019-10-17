30721aea4f2404e560eab2e2c8dbb6a26e38bfd6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 15:30:34 +0000

    objfw: update 0.90.2 bottle.

--- Formula/objfw.rb
+++ Formula/objfw.rb
@@ -5,6 +5,7 @@ class Objfw < Formula
   sha256 "4de24703d45638093a5196eba278a05b3643e8be0ae2eece5c81ba3e2c20bdbb"
 
   bottle do
+    sha256 "07ede29c16dec01d6eb8dd2dca6f79ec993f99298b38a78276f6257ebfb71c6a" => :catalina
     sha256 "abc09195b6abf66d1d638af2999abe712a41cdcbb4bbf8d7ea422443150ae637" => :mojave
     sha256 "33c72d86bb5a56ff4a2c9607707edb31f7af21bf863c8d34d95f6c527d9ee483" => :high_sierra
     sha256 "2369c4233bafe95aeea87f678cc5e0f0b001d36b5aeff6a7b6512f766d77eb5e" => :sierra
