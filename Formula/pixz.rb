e9bdf89a5f7fd3c3c4a8a6bf89f4a8f0a84cfce1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 20:52:16 +0000

    pixz: update 1.0.6_1 bottle.

--- Formula/pixz.rb
+++ Formula/pixz.rb
@@ -8,6 +8,7 @@ class Pixz < Formula
 
   bottle do
     cellar :any
+    sha256 "f8b5da517c4ea08d9dd191c90528fbeeafe696df565dcf7a974c61b3d635b8c5" => :catalina
     sha256 "1fba8335b8dd4b061f961ce2e5d03eafa550a7a92edc8b8a95b6129c55bd6b7b" => :mojave
     sha256 "4c971701c82c6444dec792341b49b8ce38e88b2d9e3d549c12433d2fef37cd0a" => :high_sierra
     sha256 "9d2f908cb1c21882c95f3c3284c9b4c1eea74c2b25d3ba6df9ad47fadbcc0813" => :sierra
