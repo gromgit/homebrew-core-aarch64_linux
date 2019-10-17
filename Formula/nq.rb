97dfe1e35a0cd39af559b03211b562fde0c53ab2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 14:42:44 +0000

    nq: update 0.3.1 bottle.

--- Formula/nq.rb
+++ Formula/nq.rb
@@ -7,6 +7,7 @@ class Nq < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "95011ee6d48728704ee95480374c545d3c2bcea8f4482cecd9b8dbbab9a2407b" => :catalina
     sha256 "b5b3f7b76cc79a5bc6d4a55e4fb3e018b08052dc7faa173300b1ddf2e16e6bee" => :mojave
     sha256 "a6d18f2d7f1fafd661a5d145599969707efe71969ccc6ac34593f3f60c59081a" => :high_sierra
     sha256 "0e8d6557f7713be4c1e5074ea909d36cd12e2e17d85a1c0a1141ac64f06953d3" => :sierra
