c480ba3b394f42b32f083234f48cbaf3b954d8fa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 09:19:20 +0000

    ascii: update 3.18 bottle.

--- Formula/ascii.rb
+++ Formula/ascii.rb
@@ -6,6 +6,7 @@ class Ascii < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2c106e2d3ce3534f09a5ce147f6fc0778e884d06f15e7c272ee99ccabaf947bd" => :catalina
     sha256 "d5f4c8fe4ad1467c1708e49268a42f0d201f8c18ed912cf3de330bdf1f219cc1" => :mojave
     sha256 "858e5bd8f55367349f936f47346a7d4dc2afed7c8f3d9fca16c42071f537f644" => :high_sierra
     sha256 "52fb2a78a1409f4f6db0b59589f773c4427c87a84a7fee1809e5f0a4d50e4d65" => :sierra
