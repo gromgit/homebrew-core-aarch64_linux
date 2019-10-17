84d4a7b99fbba76c47ca82f7aec9256fa19725a8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 12:50:28 +0000

    atari800: update 4.1.0 bottle.

--- Formula/atari800.rb
+++ Formula/atari800.rb
@@ -6,6 +6,7 @@ class Atari800 < Formula
 
   bottle do
     cellar :any
+    sha256 "d0dd5b6efe4714710e45e1d5954f4c29f2f41b778be76f67b965cb026e1a0019" => :catalina
     sha256 "b1d72401236140046396fb54e035816c5cc2ecc0883209cb3a7013b012c0c24f" => :mojave
     sha256 "ea68b68de889ad359bb747f5126f40bf1eb3238abd18d3fcd5c17fe571afbdb9" => :high_sierra
     sha256 "f055eb7567d37deb8b07ceaf7079fb73fca3e7c0ca0e703a5833cf76e1d8f8cb" => :sierra
