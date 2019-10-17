aae50606b9aab35babfdc7d47348409adc43bcd5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 14:24:18 +0000

    nicovideo-dl: update 0.0.20190126 bottle.

--- Formula/nicovideo-dl.rb
+++ Formula/nicovideo-dl.rb
@@ -7,6 +7,7 @@ class NicovideoDl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2c43b33d48c8a63229f5be23d2397bac63d3651f98e1937a19a5d8c910083a6c" => :catalina
     sha256 "21a964141ae24de2635828bde4ac6f46ed09a8b03b5564050e36ab3df0b9bceb" => :mojave
     sha256 "21a964141ae24de2635828bde4ac6f46ed09a8b03b5564050e36ab3df0b9bceb" => :high_sierra
     sha256 "ff5c5d5a91e5384c20cd068a1857c83322e2ab27ac6ee0baf01cfabad44b9dd9" => :sierra
