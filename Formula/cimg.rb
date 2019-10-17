2fc63c20aae6c6ccd309eb9c16a9750aa6c202d0
Caleb Xu <calebcenter@live.com>
2019-10-16 21:20:43 -0400

    cimg 2.7.4
    Closes #45434.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/cimg.rb
+++ Formula/cimg.rb
@@ -1,8 +1,8 @@
 class Cimg < Formula
   desc "C++ toolkit for image processing"
   homepage "http://cimg.eu/"
-  url "http://cimg.eu/files/CImg_2.7.2.zip"
-  sha256 "b8277ca3ff6393fc3e8c1d4addf843982ea9fd068548d31c1c1551c352865b3f"
+  url "http://cimg.eu/files/CImg_2.7.4.zip"
+  sha256 "4b2e795bd1e4f6fe638171748fc4a7ac5b165155c4caa64bba0d61f1115358b3"
 
   bottle :unneeded
 
