c20a8798f713b9a9ad1b5e2e21f524fe031bc782
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 07:01:48 +0000

    angband: update 4.2.0 bottle.

--- Formula/angband.rb
+++ Formula/angband.rb
@@ -6,6 +6,7 @@ class Angband < Formula
   head "https://github.com/angband/angband.git"
 
   bottle do
+    sha256 "3d68682f9905d6f0d829ccebc59f7215c9bdef7e05cde4d88d825d660f22233d" => :catalina
     sha256 "dc6f1a83a2810d52da345f6612064a2b851071a92a0eec5338ee6f1a933b3186" => :mojave
     sha256 "611854a1b7d74e879f596ea2a03522eb5cba323cc7db047a918d2affb60c79fd" => :high_sierra
     sha256 "6330d08684c373c901f25845b838a3ac6c8c6c5ebeae80c7d3f6a63c198af181" => :sierra
