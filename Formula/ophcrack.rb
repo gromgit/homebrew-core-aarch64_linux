0528387b035596e6fa14e74f9ca426e0629714c3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 17:10:49 +0000

    ophcrack: update 3.8.0_1 bottle.

--- Formula/ophcrack.rb
+++ Formula/ophcrack.rb
@@ -8,6 +8,7 @@ class Ophcrack < Formula
 
   bottle do
     cellar :any
+    sha256 "47361d9c18591930ce871fa3c7ab36eaa43003a8a5339238648787cdd748d962" => :catalina
     sha256 "0bdbfbee37e693edff5fc8f71c52f1fb12d6dd07c1e64aa1a20401df0789853a" => :mojave
     sha256 "a1061331c1e9b4a726c818005a3d795ba8c73b29ecd78a3828b5e5eafac18107" => :high_sierra
     sha256 "6229ee0c1e44192fa0d513b7e72e5c72e7fbd29b5ad7f61cd5c5824d76d49105" => :sierra
