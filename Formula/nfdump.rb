4db3ffe0d6c6dad510b133b25eee434dad3095e3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 14:24:18 +0000

    nfdump: update 1.6.18 bottle.

--- Formula/nfdump.rb
+++ Formula/nfdump.rb
@@ -6,6 +6,7 @@ class Nfdump < Formula
 
   bottle do
     cellar :any
+    sha256 "4f9eed36bde70336ac1afe1d8ceaeb0827926c26eba3baced089a0508cd3a033" => :catalina
     sha256 "29c5921e64710a0843b2ca0584213e45295d977650ef2699e3edfbeeab3ab5d5" => :mojave
     sha256 "b79e24cd33227eb6a36db2288b547b825b1b4b754cbfd73d99484b8bd096b857" => :high_sierra
     sha256 "b6b11acd5aa5bf7273fc1e97f046741da8352662cb2aa548e77c809554208dc5" => :sierra
