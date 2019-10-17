45453e314e8d0915ef204c037b6b71492c466e03
Caleb Xu <calebcenter@live.com>
2019-10-17 16:29:21 -0400

    libdvdnav 6.0.1
    Closes #45474.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libdvdnav.rb
+++ Formula/libdvdnav.rb
@@ -1,8 +1,8 @@
 class Libdvdnav < Formula
   desc "DVD navigation library"
   homepage "https://www.videolan.org/developers/libdvdnav.html"
-  url "https://download.videolan.org/pub/videolan/libdvdnav/6.0.0/libdvdnav-6.0.0.tar.bz2"
-  sha256 "f0a2711b08a021759792f8eb14bb82ff8a3c929bf88c33b64ffcddaa27935618"
+  url "https://download.videolan.org/pub/videolan/libdvdnav/6.0.1/libdvdnav-6.0.1.tar.bz2"
+  sha256 "e566a396f1950017088bfd760395b0565db44234195ada5413366c9d23926733"
 
   bottle do
     cellar :any
