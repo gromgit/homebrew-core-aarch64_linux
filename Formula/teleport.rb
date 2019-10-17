0aaab63c9dc96ed9ed12205237a8d1971854ac24
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 23:56:07 +0000

    teleport: update 4.1.2 bottle.

--- Formula/teleport.rb
+++ Formula/teleport.rb
@@ -7,9 +7,9 @@ class Teleport < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5c9192f91f9a18b2aea5f974e7dc615bba3b097b2a1f9b0d1143e43c566ce896" => :catalina
-    sha256 "2bd5b7c4db26b8ee92f75c1bf85d2d1fa61cd0ad79d59bfa8a44e10f2d66c509" => :mojave
-    sha256 "8696db0a47352ca02b8a6974e1e757bd2b35c24586c27e205a502077aca702f1" => :high_sierra
+    sha256 "a74635132bca4322c8ab8bf6621ceee4f3a73b6b4588eb11223894967031a7cf" => :catalina
+    sha256 "f038a722bb70dcc187bcafaeb5b810d0c8173863abda4d67713a7f15cd1f7dca" => :mojave
+    sha256 "e7df0b2ffcab2d8447bbfcb8496a84ba8aa9a4d6821499305e15cdc533a2b790" => :high_sierra
   end
 
   depends_on "go" => :build
