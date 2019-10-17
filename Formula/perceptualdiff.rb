c6ae4533fe3c82034174dcbb938ba969b4aa630a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 19:06:53 +0000

    perceptualdiff: update 1.1.1 bottle.

--- Formula/perceptualdiff.rb
+++ Formula/perceptualdiff.rb
@@ -6,6 +6,7 @@ class Perceptualdiff < Formula
 
   bottle do
     cellar :any
+    sha256 "9edad00fd4470f908e5f9e1eb8c96c364b94c504dab46d1f38a45036871a10a0" => :catalina
     sha256 "1d3d02c27772801105fe9cf3e3ad697bcbeb4db9b260f134bd3e342344455481" => :mojave
     sha256 "683d05fc64186ee518180b56345d446be90ff2c42666c80adb86bc185d20d283" => :high_sierra
     sha256 "eb2da458eda1cebc7872b2621c96e5aa627d9711f8d31fb792cb092d92d060db" => :sierra
