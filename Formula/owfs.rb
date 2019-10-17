9dfaad8f5b6ac2303c4cab16756f81e63965bed9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 17:34:10 +0000

    owfs: update 3.2p3_1 bottle.

--- Formula/owfs.rb
+++ Formula/owfs.rb
@@ -8,6 +8,7 @@ class Owfs < Formula
 
   bottle do
     cellar :any
+    sha256 "2b3d52a12424dddee938a0fe9a4620938b4c01a0989f68f34efb5eadb2098bcb" => :catalina
     sha256 "118ad185bc83ac080c485e1572c5dbf9118c5620a89076e7c2715a45f07684c8" => :mojave
     sha256 "63f73726171fbc413a80a30581a604a68f6371d05d86db9a848d1ddbf5cb7913" => :high_sierra
     sha256 "76c620684afb471f5d348badbc1c7429054b348c8ebe4a8c4f70b02b3ab26374" => :sierra
