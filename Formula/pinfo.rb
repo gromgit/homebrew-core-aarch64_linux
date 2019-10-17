ebc0d668d798dbd21c1bc55b3b9c9599965af20a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 20:27:05 +0000

    pinfo: update 0.6.13_1 bottle.

--- Formula/pinfo.rb
+++ Formula/pinfo.rb
@@ -6,6 +6,7 @@ class Pinfo < Formula
   revision 1
 
   bottle do
+    sha256 "a41b568910292b2119d0f63f53d5015d781b03576a58f08d397535560d407bf5" => :catalina
     sha256 "b81b1202add75d938802681618f5bf95dd245e03ff80f5f0ca67a5ba8b7bfb84" => :mojave
     sha256 "84edf6ec00f570004abc6f3d0335196b513a4a52e589919ca1e70c35b31525cc" => :high_sierra
     sha256 "9b8e3d359081d68626f86cab8b048926b6471f8ca1be8e47ca8625e22da5021f" => :sierra
