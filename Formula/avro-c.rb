978f5a9b86bd89009877c6bd02922f2bb6a8600b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 13:06:57 +0000

    avro-c: update 1.9.1 bottle.

--- Formula/avro-c.rb
+++ Formula/avro-c.rb
@@ -5,6 +5,7 @@ class AvroC < Formula
   sha256 "7df7bc1e13ce7180f0438ed05ab6642b5b2b6df91f30b927b470e25a78e04642"
 
   bottle do
+    sha256 "46eed9b02091bc06e0a1007beed901d6fe8f19366e86e4544103e4f7f43a2e28" => :catalina
     sha256 "35aa07886a5188fc42edf2c4dd473857420f9e2ab8b981c4ebd08fabbbe32455" => :mojave
     sha256 "4ab18f360192adcd9900308820cb06b706d12b997a07b7f2fa6bf1366a91c477" => :high_sierra
     sha256 "0e24c43c6b2d1c356fdb135f0d94afaaf1274c922f7e58abf4c51581925da572" => :sierra
