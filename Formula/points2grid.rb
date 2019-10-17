a12fb50d53a2caf0f7e0228f54e90c0111462f0c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 21:45:10 +0000

    points2grid: update 1.3.1_4 bottle.

--- Formula/points2grid.rb
+++ Formula/points2grid.rb
@@ -7,6 +7,7 @@ class Points2grid < Formula
 
   bottle do
     cellar :any
+    sha256 "55fc987d27a9dd759111258641196dc1d48856c5f4a828fdffedb6ca49b8e63d" => :catalina
     sha256 "2353397675ec9f8fe4f8cf701645e4dceedf01030ee54ff61720521ba6afa046" => :mojave
     sha256 "4d2c91405313abf7fedda20628ce2b11b5bc3f76a42862a40e90d5252ad34e7c" => :high_sierra
     sha256 "58d99c2471ebd555d76e36c1108d352bb4e9b7594459ea3381c5f4a6232bfca3" => :sierra
