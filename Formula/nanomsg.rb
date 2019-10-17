232e38c25dfd94598bc71724f22ad1868f7d58c8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 15:18:56 +0000

    nanomsg: update 1.1.5 bottle.

--- Formula/nanomsg.rb
+++ Formula/nanomsg.rb
@@ -6,6 +6,7 @@ class Nanomsg < Formula
   head "https://github.com/nanomsg/nanomsg.git"
 
   bottle do
+    sha256 "81cd453e3fdf65da66a54fda36c84248a1eb923ac92125fd14bdf68989aeb9b7" => :catalina
     sha256 "95609047c54b0207587db3a5b3cc8985b35fc922fe8785c63d4d2a44a78ff57f" => :mojave
     sha256 "11390e904a94e60865186a846af14565b379ec84942a9bc512ba4e5e3ea7ec85" => :high_sierra
     sha256 "95192ebc59926ff064d7f4cff5ebf9037c7549af61d2f1c23375827c91b88282" => :sierra
