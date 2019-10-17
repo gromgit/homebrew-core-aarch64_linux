0f72290ff112d2c329e0d7ff11862c51d3e4f51b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 13:57:30 +0000

    ndpi: update 2.8 bottle.

--- Formula/ndpi.rb
+++ Formula/ndpi.rb
@@ -7,6 +7,7 @@ class Ndpi < Formula
 
   bottle do
     cellar :any
+    sha256 "cf2ff87a20aa68e4ebc4777c121c963d9b0edb4bcdb3192ef915fd769795f713" => :catalina
     sha256 "dc154d9707bbf0243c811e455307778a237fa04f970c677021ddce99a6dc75ed" => :mojave
     sha256 "ab792cab341a8ba89aae05273d6082d79e758901362ae256e04a3aa9900a4174" => :high_sierra
     sha256 "1cccc0b5ae55ff767aeea7553c5a8ae063b92535ec1f06e575d8b8637b0cbf97" => :sierra
