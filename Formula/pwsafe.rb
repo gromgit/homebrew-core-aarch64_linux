4251af59b27d9bce7bba311acf3fefde49a31e55
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 22:47:43 +0000

    pwsafe: update 0.2.0_4 bottle.

--- Formula/pwsafe.rb
+++ Formula/pwsafe.rb
@@ -7,6 +7,7 @@ class Pwsafe < Formula
 
   bottle do
     cellar :any
+    sha256 "5f952aa85147c86d2f77f9054fe228484820388c3b1e92c39c12432a15ca0f54" => :catalina
     sha256 "94c4b9684c2709c7cbd168609db33271ede431f1f72c348bb508e65a07bf8faa" => :mojave
     sha256 "5d5a277678e752596a342712e46dd2e1ce015d6897ad7f74437509a39f47b5ce" => :high_sierra
     sha256 "e5fd7f0c41f73c0bdf2f455b7ad659d27931afc1e78536e11a0553be0e8cade1" => :sierra
