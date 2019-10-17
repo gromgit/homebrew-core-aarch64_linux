eaab5e2358600104bce72cdbcdec77c89cf45705
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 17:04:54 +0000

    openhmd: update 0.3.0 bottle.

--- Formula/openhmd.rb
+++ Formula/openhmd.rb
@@ -7,6 +7,7 @@ class Openhmd < Formula
 
   bottle do
     cellar :any
+    sha256 "351e8d9e6bfa22b63b035c0f9c0c7e37be52b9e4058c50d7b7ac321eca880e5b" => :catalina
     sha256 "796c1a6f06715aa8a3304cca0083378d5fe2a1006b55da8727938922b5408c8d" => :mojave
     sha256 "1c54727de5836916bca42065d0ed53f0a796d07ec6866408a69213c94b151092" => :high_sierra
     sha256 "97f5dff1e77b6b615544ed6611aa6d8c3395e3c6dc759c4576084d87a4e976ad" => :sierra
