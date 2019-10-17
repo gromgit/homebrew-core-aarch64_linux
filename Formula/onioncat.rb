cf7725ecf26ca6e44b0b97debb743cd5c07fb6ee
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 18:49:25 +0000

    onioncat: update 0.2.2.r578 bottle.

--- Formula/onioncat.rb
+++ Formula/onioncat.rb
@@ -6,10 +6,11 @@ class Onioncat < Formula
   sha256 "69c677e04987bd438495d575b566c358f449ff138b836925fd406cf6d6a400f5"
 
   bottle do
-    sha256 "8d31228fb732238341e83b58991673588818cd27d6be983f65cb02c4292f6d1b" => :mojave
-    sha256 "1062942ec61923ed49c04afd61ccc9bb56166efe0285fe93b406597d23223fa9" => :high_sierra
-    sha256 "539500f5eeed2771a729c9e61196831c3a3796edafcb2c27a38b26ca5674f7a6" => :sierra
-    sha256 "98acc41c8dc5fcefbe5a410266a762ddacbe12323e958a8fe6ca753ec51f33fe" => :el_capitan
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 "8e5072dbf56e675d189d93820e6515b1b2b79f3db41720d148e6b4c4b96f9242" => :catalina
+    sha256 "8e8db91685902f795fae00325d15a0599009cfb9aa7441328a86525a432d58cd" => :mojave
+    sha256 "cfc80dc5e65d788e2433448a3b48d9705f3f1498b85ea8204de760765d371665" => :high_sierra
   end
 
   depends_on "tor"
