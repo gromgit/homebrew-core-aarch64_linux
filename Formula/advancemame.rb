c9fc743a8bab4c43ed33914b7e815e2f38c34ffe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 10:42:50 +0000

    advancemame: update 3.9 bottle.

--- Formula/advancemame.rb
+++ Formula/advancemame.rb
@@ -5,6 +5,7 @@ class Advancemame < Formula
   sha256 "3e4628e1577e70a1dbe104f17b1b746745b8eda80837f53fbf7b091c88be8c2b"
 
   bottle do
+    sha256 "7530ca2e37ac45b53164ae54ab6669f1796ea0af88541a85a93a74c155fb0029" => :catalina
     sha256 "95f2cdff91ff98c3c9f65a0751d7948cefb3829d96e1977b5b8869163eba0790" => :mojave
     sha256 "9c5e0a9f81f43ec02eb951b82b4930639dafcdbacbeadf7bcc5e74f2e2ec7c45" => :high_sierra
     sha256 "6ba2c02db07a9ef7ddf561dbc2cde3904abe0b1b0ab9c9469020fdeced72e011" => :sierra
