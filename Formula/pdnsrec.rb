7fd27155257bf3abcb552898b2df512dfb9b88e0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 19:06:53 +0000

    pdnsrec: update 4.2.0_1 bottle.

--- Formula/pdnsrec.rb
+++ Formula/pdnsrec.rb
@@ -6,6 +6,7 @@ class Pdnsrec < Formula
   revision 1
 
   bottle do
+    sha256 "deeed735fefd99d65289614e3ff8730c71248bdc2218dd1803f3c775df5775b3" => :catalina
     sha256 "700f8316d4b5770dd7764b2a60220a5c7ec239d044280f2436c97432a71d6327" => :mojave
     sha256 "a1cf1c5231fcea9c0a48f928e4479c60627d4823557db01b21c8c9c79be68240" => :high_sierra
     sha256 "531adab0998084f2e66923b2429c5b4c446970d222b6e1b38c71eadcbd5711b3" => :sierra
