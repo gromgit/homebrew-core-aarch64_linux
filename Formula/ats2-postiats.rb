9974ea79764595bea2cc76cdebd3a3df82467215
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 09:49:46 +0000

    ats2-postiats: update 0.3.13 bottle.

--- Formula/ats2-postiats.rb
+++ Formula/ats2-postiats.rb
@@ -6,6 +6,7 @@ class Ats2Postiats < Formula
 
   bottle do
     cellar :any
+    sha256 "4e6f3f3bdc1f4491c1e2dd0955c302f9a375315d924095f5f85a2eafc268381c" => :catalina
     sha256 "751f4582b45bf5b5f3e319f7cd40d3c02ccdca5024e5700062cb14faf3106888" => :mojave
     sha256 "1c0c128bf522b6780e1ff36b2ad959e9ccad36198411dbd5d21395baa412bfde" => :high_sierra
     sha256 "ee94f5c6675016834a7d864d6aa78515589f26a10ad3a650983259f3732e1630" => :sierra
