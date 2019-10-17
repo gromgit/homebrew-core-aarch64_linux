9b9b024e3b51df4cd68f34699ac5b3af3a0fc026
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 14:24:19 +0000

    nickle: update 2.85 bottle.

--- Formula/nickle.rb
+++ Formula/nickle.rb
@@ -5,6 +5,7 @@ class Nickle < Formula
   sha256 "aaa8c96c31e533f238e03b1d91d6a6b5e95bed25a8b89a48d64d67fcfe8c7e89"
 
   bottle do
+    sha256 "04ccc5499b87e41f1d674d4e9be56fc3ddc933384be0960db2c5a9e405eacc38" => :catalina
     sha256 "6294e2a7a132be9cbda76ff8a8529971a0a034a06455060fad7746cb32b0767a" => :mojave
     sha256 "e5ce6aaea5ca0a7f0680d57fc9ff2d74128bda489bd0f1cc89e7c2f40ddc6958" => :high_sierra
     sha256 "8204000310cac65645b48f68855aacd3f8257d11395f9e96b2751c0831c0628f" => :sierra
