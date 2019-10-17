2de2427e9beb2d0e523d2e7c2ae6c3026329b73d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 07:24:37 +0000

    apib: update 1.0_3 bottle.

--- Formula/apib.rb
+++ Formula/apib.rb
@@ -8,6 +8,7 @@ class Apib < Formula
 
   bottle do
     cellar :any
+    sha256 "89a8653925243569be382dc7a7816a836e020973e627ba0a0b3926e3de0ba684" => :catalina
     sha256 "ca59f86634b3b9282496f95b432aa9e0c9924eb189c1ec2965d427edac8bab4e" => :mojave
     sha256 "f2adc68de1b28e305ad7530ec097425bcf75beb70d6dd820f025cabcbeb54585" => :high_sierra
     sha256 "bbe9bc25a8584f163347662675d78b69cdfaac495be5f2fa026dfca112f8d4a4" => :sierra
