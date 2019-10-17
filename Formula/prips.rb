0cc4d7e879aef8ce21807c50e2c608da89851737
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 22:23:24 +0000

    prips: update 1.1.1 bottle.

--- Formula/prips.rb
+++ Formula/prips.rb
@@ -6,6 +6,7 @@ class Prips < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "06f354f3564aa9aa391d56b952fb97056c911f32232f6afeefcb23bce5a8bc0c" => :catalina
     sha256 "771e030cbbf61a7914af375462d24bc2fccb6e60e8959110906e23544aacbb17" => :mojave
     sha256 "ecf0f743bfaffc303c8f520f5f29f10917b63708866fc50553c10f6952c5e06e" => :high_sierra
     sha256 "65a400f8d42e7c38cbc26898dadf3110b0aad7e347ba40585f398d4bcc696d04" => :sierra
