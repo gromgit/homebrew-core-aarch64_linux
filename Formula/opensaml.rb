029229c3c2554d1c9d50e093b06e4de0332cae49
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 18:10:16 +0000

    opensaml: update 3.0.1_1 bottle.

--- Formula/opensaml.rb
+++ Formula/opensaml.rb
@@ -7,6 +7,7 @@ class Opensaml < Formula
 
   bottle do
     cellar :any
+    sha256 "0bfd4a039784847c3b10f2de52af7649f3865cd34332a7bd59f5dc27af605d7e" => :catalina
     sha256 "d40be78eca03a3ea7ef790bef6df3997ad672628cd7d34d13f84354367181a44" => :mojave
     sha256 "5f3ef1f46ab0bdda5319b1806a176ea7a7be99d91f818da112459e479ab942f0" => :high_sierra
     sha256 "93662e4d596b29a1f1aed2db97a7b78ffc34111987ce2d94850aacefe54d3e9b" => :sierra
