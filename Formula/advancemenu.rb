97c79f7d1848bcc7247471f83297a26078898b02
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 08:55:19 +0000

    advancemenu: update 2.9 bottle.

--- Formula/advancemenu.rb
+++ Formula/advancemenu.rb
@@ -5,11 +5,11 @@ class Advancemenu < Formula
   sha256 "c7599da6ff715eb3ad9f7a55973a9aaac2f26740a4e12daf744cf08963d652c1"
 
   bottle do
-    sha256 "b61eed32b7dbf12a05a73e825df47adab2ee86da445808c0e9b4db5597d2ad23" => :mojave
-    sha256 "8c96b4bef369fe6b52d58c9c6d8025a64450fad965af6209c99c7b1edcec58eb" => :high_sierra
-    sha256 "875396a5eb77a17ee1cda679fc782d5fa15072a206331d15cb186e52bf4d5369" => :sierra
-    sha256 "5e4a2e320562c0d92c3d61049f097d0a7ee054b76bacc8dbf81095c2758bed6a" => :el_capitan
-    sha256 "543c8592f5f72c1407fac7e29a9bc298b2d5b15529d669b54338d60209409028" => :yosemite
+    cellar :any
+    rebuild 1
+    sha256 "cddc2110c8f7a94fff740ebb35d9d48fb5b47ccecbf92eedfc5ee28b28bfdd62" => :catalina
+    sha256 "f1f5a182df60bf84339ff741e73dc464b209d87ed50f1b74bc671736eef6e300" => :mojave
+    sha256 "a7e1ea0b085ec97b97bcb8f45ecd4221ad626a530a4d8b6dc1f56a3e85b9cf6a" => :high_sierra
   end
 
   depends_on "sdl"
