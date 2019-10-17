c00e983667cd1c308a8dee46acb1c08dcf3738f6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 20:27:05 +0000

    pipes-sh: update 1.3.0 bottle.

--- Formula/pipes-sh.rb
+++ Formula/pipes-sh.rb
@@ -7,6 +7,7 @@ class PipesSh < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "68d379998c00ca3662db8047c1c6e649491d65d851af264e04ce7cbdb7cbd2e2" => :catalina
     sha256 "b78492e9f13a815dc97200b33c4e228292a4679eb6d048c1094c64aa46504879" => :mojave
     sha256 "2793ad5fb825b4f805a4731c7028cbcb2ca5e9dd904133df0cce7481c5961322" => :high_sierra
     sha256 "2793ad5fb825b4f805a4731c7028cbcb2ca5e9dd904133df0cce7481c5961322" => :sierra
