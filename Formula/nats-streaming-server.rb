4acb24d75ffef9705c544a510326f5221bb34372
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 13:46:39 +0000

    nats-streaming-server: update 0.16.2 bottle.

--- Formula/nats-streaming-server.rb
+++ Formula/nats-streaming-server.rb
@@ -7,6 +7,7 @@ class NatsStreamingServer < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "82e1ea223fbe428cc919989bdab5c8190b721885e518ff58150d09041ef51be8" => :catalina
     sha256 "9f430d70010e052480bde5f073efe69b828da2ca1411aaa2a0f813bf7398bcdd" => :mojave
     sha256 "4acdd612b1d990dd4f91deb2a157d768678d84fe2da844151c9df00df76722a2" => :high_sierra
     sha256 "cccd977ede44ad42685bcd359d7937dc87bb114f1e41d203cdfdd728d15750fe" => :sierra
