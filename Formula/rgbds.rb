15ddc1af22334d29ba34dd41be4efc7c562c8fd1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 06:10:40 +0000

    rgbds: update 0.3.8_1 bottle.

--- Formula/rgbds.rb
+++ Formula/rgbds.rb
@@ -8,6 +8,7 @@ class Rgbds < Formula
 
   bottle do
     cellar :any
+    sha256 "e6c072425e3776b171d8fd9643872aeba97654587730ff6879d30528327b3ee1" => :catalina
     sha256 "df3afb30c0abc0f2da49b139c38a48c17af9161632230160ef2492ed1847ad2d" => :mojave
     sha256 "81bb97ec05af2f438399076f49c8095a82c7345f6eb91058a7de9edbc4d35c36" => :high_sierra
     sha256 "b590e704362afe808cdf7ceab08cc2f4e479c6c14ce7ed0f3c4a9e403fee43f3" => :sierra
