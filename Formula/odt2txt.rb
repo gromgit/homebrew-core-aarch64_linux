3777ad2f6d293fd33a9ec66f1e32a5ad75c9db65
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 16:00:27 +0000

    odt2txt: update 0.5 bottle.

--- Formula/odt2txt.rb
+++ Formula/odt2txt.rb
@@ -7,6 +7,7 @@ class Odt2txt < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "31e17f05898b06469cbc33244f357c61baf059120e96b34d472325e38adfa4d7" => :catalina
     sha256 "eb4ea913c8c1f5108adae12acf43ada9033c3bdd2e6976fcce9726108b47df2b" => :mojave
     sha256 "02dd0957fda7e5845824951a3e98d2ac9a1a623a02709631d26496bbe0353dee" => :high_sierra
     sha256 "88fb433f9e72c6c727f9af5ff017d6bac07f29bc64bfa59f6b53d4ab52f42cb3" => :sierra
