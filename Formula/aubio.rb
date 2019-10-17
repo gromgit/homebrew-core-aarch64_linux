6b903584431230e250ac6cac8352fd76e29bb793
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 09:57:05 +0000

    aubio: update 0.4.9 bottle.

--- Formula/aubio.rb
+++ Formula/aubio.rb
@@ -6,6 +6,7 @@ class Aubio < Formula
 
   bottle do
     cellar :any
+    sha256 "b542f8ab15932bc1b5f51c65ab00f7501d2054752823832fafbc5d2f0d87f0ac" => :catalina
     sha256 "1c011afadd6d9590101b46cb6f3bf530c5ddfb2cef0983bf4fea287ef5f8c265" => :mojave
     sha256 "f4d0585fe52669ce1c8f3b33e64af22219cc8623f27423bc7d9ce8c3f4e2351a" => :high_sierra
     sha256 "11ce710814cb514c434620c24fbb4504a3744747ae06dfe260bcdcfa6b69ef64" => :sierra
