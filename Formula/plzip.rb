1bd63ad27c1afab985197ced96f9d720ac3d36eb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 21:20:33 +0000

    plzip: update 1.8 bottle.

--- Formula/plzip.rb
+++ Formula/plzip.rb
@@ -6,6 +6,7 @@ class Plzip < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9cae6af29d979ef1e9ed1869f8a5013fe188f6c65ca138bed9a5f76ce178c881" => :catalina
     sha256 "3e266c42c66babd4fbdfe82645ab876fc7224846e94b26a39183c57404e17c35" => :mojave
     sha256 "0a5df85c11e9afb266709a907980424cd60f1d1fd3adda71e8b0f9939ddf72a7" => :high_sierra
     sha256 "c26a4b45c09173a4cb8ab2a56d2c5bb9018e16332e637d4d617bfcd75f90c0ad" => :sierra
