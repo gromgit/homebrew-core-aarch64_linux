41b12603d0cb52ff7bac71704a1e9b1f73d15881
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 06:12:01 +0000

    rdfind: update 1.4.1 bottle.

--- Formula/rdfind.rb
+++ Formula/rdfind.rb
@@ -6,6 +6,7 @@ class Rdfind < Formula
 
   bottle do
     cellar :any
+    sha256 "4b0f71d105368d57caf8037100cd79447d9b7e7d8eac3a167c65aa836d04d7ca" => :catalina
     sha256 "a0958b206cea28100f3aa0f08dc3d1be3de2eba641a44b293a44f42f9e43e261" => :mojave
     sha256 "1d504b757728f9f704884de3d347ebd22536e2cb86b4badafaec9aa3e9e915d8" => :high_sierra
     sha256 "894d7aaf7a86a7df034c773c9b62e1a8525cbbebf56870280a886ca0b80d5ae9" => :sierra
