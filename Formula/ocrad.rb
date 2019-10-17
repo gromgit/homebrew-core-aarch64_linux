fa4cc8ef118a4c95346b8b7aeeabe78613bf5caa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 16:00:26 +0000

    ocrad: update 0.27 bottle.

--- Formula/ocrad.rb
+++ Formula/ocrad.rb
@@ -7,6 +7,7 @@ class Ocrad < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6533cd452587714531d20b4aa74ea7fc1e323ff893c8a7c9729655ede1ec9df7" => :catalina
     sha256 "3d1c85bb36faedf5ab12f78e8c3511dcc4164561ba8bc09924b48f6aa3fa0b37" => :mojave
     sha256 "ba9b30eeabc11634502e30fd9a730d5727668550f9708d46fbefc03bcb3917de" => :high_sierra
     sha256 "903ce6530395c0973418020561ddd60da739f3a36e865500776922e18975460b" => :sierra
