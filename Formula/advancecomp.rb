7565a6e207e0f96a15f06a3f4bd1919bf3687500
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 06:54:54 +0000

    advancecomp: update 2.1_1 bottle.

--- Formula/advancecomp.rb
+++ Formula/advancecomp.rb
@@ -7,6 +7,7 @@ class Advancecomp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "fccdb14eb1a620c824c08e748db390d788ad797c30654cb6434205fb16c78784" => :catalina
     sha256 "798de4490c97283280259ffc1dc39159bd0ded85edb47f3212ad5ec9a174289e" => :mojave
     sha256 "fdb2a72157445c33a462388f05580489c427f4f0d2a3d4cdc1b7867ef69e7e53" => :high_sierra
     sha256 "4ef3590e26c5ac96d64dc985b035ec7055f215c84d31dfb09542d958f6ec4e77" => :sierra
