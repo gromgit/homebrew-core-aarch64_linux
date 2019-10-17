c203e686bf3a2fc716a0df90a39808ecac6c4dfa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 13:06:57 +0000

    avro-cpp: update 1.9.1_1 bottle.

--- Formula/avro-cpp.rb
+++ Formula/avro-cpp.rb
@@ -7,6 +7,7 @@ class AvroCpp < Formula
 
   bottle do
     cellar :any
+    sha256 "999bed28804705dacec3f43a036c7702a2fb25e8dd4df0d3f728da90b8491d74" => :catalina
     sha256 "c8914248f52cfdb0b14bdc2a35265b57e63188d90734697f54cbf190838da70f" => :mojave
     sha256 "5b663117e9a813100c712de902a12499e56a0f242425e79e50e964e2239d8b96" => :high_sierra
     sha256 "75e045cca21a51f2ea403edca2dc5f17f1a164969f02b605bdc3e7f733abe64d" => :sierra
