2d6d87906799b6cd3335404c6f88168c6095fbd6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 09:49:44 +0000

    atomicparsley: update 0.9.6_1 bottle.

--- Formula/atomicparsley.rb
+++ Formula/atomicparsley.rb
@@ -8,6 +8,7 @@ class Atomicparsley < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "632b3bc281a6f3bd5b9a913dff1e805c9fb9997f41b4a02db0a1aa34f6faced3" => :catalina
     sha256 "d32a565f675bd0b2c5ebf1b5aee01fb79d9d42b072dedf724b7ee03b2cc242ee" => :mojave
     sha256 "05c4cdc1dfc14fa6f06fdbbcadead5055a9fb53091d014458b86ecb4b22111fe" => :high_sierra
     sha256 "d5f8672d420511ff76fd9ecc4d41c8aee5eecbf4382d7c4bd3fb04400c4617f4" => :sierra
