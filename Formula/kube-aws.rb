d438d76c1b3e663de62f46c539ec6290b10b96ed
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 15:54:42 +0000

    kube-aws: update 0.14.2 bottle.

--- Formula/kube-aws.rb
+++ Formula/kube-aws.rb
@@ -8,10 +8,9 @@ class KubeAws < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "068a3cbb7f097bff550171062d9ed85db12c778c1769d1280ddd2a0a41ab8baf" => :catalina
-    sha256 "18e2da3a952fb04a061d0765cb1f183f7399a6360723ed8e408b56b4ef2069a7" => :mojave
-    sha256 "469566a099c761183a7d40a9c7d47691bc210c30f2cbe93c4b40d791d9cc8d11" => :high_sierra
-    sha256 "61dfb1788f25c19227b55a8019e574fd7bf7e99366b4567f5fa33694c4d5abc4" => :sierra
+    sha256 "f2e08350a7258a9f5679c83aa3635b8646b5aa86c0337eda5e4c023d3af9d40b" => :catalina
+    sha256 "8b402c944c54683c85130bfc0711d12b991d1288d1eff24942fcd812adb3c04d" => :mojave
+    sha256 "3db554acae43e8d8d651bf166853c7b7e23a098f4bb85e9a6dfe80afa9f1299e" => :high_sierra
   end
 
   depends_on "go" => :build
