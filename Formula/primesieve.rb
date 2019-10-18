249d5658b48c72997d6bc583461a575abc93e912
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 00:00:15 +0000

    primesieve: update 7.4 bottle.

--- Formula/primesieve.rb
+++ Formula/primesieve.rb
@@ -6,6 +6,7 @@ class Primesieve < Formula
 
   bottle do
     cellar :any
+    sha256 "fbee45bf6d442c2846d10019a4e0aa90cf54d9ba1e3fd7a068955b25b2156fb0" => :catalina
     sha256 "c932276f2380adb0931435638641dcce166d2ab542ec7254a6fc4257b9b2f259" => :mojave
     sha256 "799b34ea32419dd37598adeaac9b6b779351cfd91a3dba99c122ba3b518e5a04" => :high_sierra
     sha256 "afdbe6ecf6907a23e1e1b20ec5f24293063cb246cb9da2ff903b353e6604ba28" => :sierra
