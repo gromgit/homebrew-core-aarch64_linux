aeb102b9a7995a2c0ab0d3f3267a5dcf6fe32556
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 00:41:15 +0000

    cattle: update 1.2.2_2 bottle.

--- Formula/cattle.rb
+++ Formula/cattle.rb
@@ -6,6 +6,7 @@ class Cattle < Formula
   revision 2
 
   bottle do
+    sha256 "351e447fdd9020945375ad5db0884e1f4f80915d4af4911cfebd37636afd2fe6" => :catalina
     sha256 "0ae9acb42c85b12815f16e0325bdca644a7df6a6d52734e8f713f5d38b20c480" => :mojave
     sha256 "db653a4f8e8463ced42f6b9aaad04f8f09fff9847225054f4833ac06e1a03bfe" => :high_sierra
     sha256 "1d66b604d5ae66b79d83ac63b959ca37df121dd146be43a6af8fb110c6ae196b" => :sierra
