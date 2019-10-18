d7b81d6a26360fbed77ab7380b65c60e62b4f3f9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 17:17:19 +0000

    minbif: update 1.0.5-20150505_3 bottle.

--- Formula/minbif.rb
+++ Formula/minbif.rb
@@ -8,6 +8,7 @@ class Minbif < Formula
 
   bottle do
     cellar :any
+    sha256 "57dc630a96ec93b8168d569cc1ff0152f381ca52c10d2b6b7bffc1d91cbc14a2" => :catalina
     sha256 "479cfbb3b59f2c0c05b0553188ae2497ee313b02e5850172bb7055231def61b8" => :mojave
     sha256 "5b8a0fd609cda94163f95c7d0b6620c143b3ff127178d37a57b76493231c73cc" => :sierra
   end
