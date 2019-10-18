96f0894b8e1554d7e3ea4d3949f926d072880116
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 09:50:33 +0000

    lf: update 13 bottle.

--- Formula/lf.rb
+++ Formula/lf.rb
@@ -6,6 +6,7 @@ class Lf < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "86added59852eebeeea8152ed69feb042f6f4ccc1ca998e3df6ca1629f746fe7" => :catalina
     sha256 "ad4cfc498d617d41c02b9482c0411b8048d7eaa934cfed114861555de742cf7c" => :mojave
     sha256 "e25cfc6246e04b13e90260433cf1fc27a28af17b82ac7d6bb93e09123faa4f61" => :high_sierra
     sha256 "49a61cf93df5beac903c441087755cc81a4062bcd6f4665aa678cbf21e5c286b" => :sierra
