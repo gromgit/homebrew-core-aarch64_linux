532844b5f256f7e62adb0a27064ec67d5e13e03c
Birger J. Nordølum <contact@mindtooth.no>
2019-10-19 01:37:55 +0200

    plantuml 1.2019.11
    Closes #45532.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://downloads.sourceforge.net/project/plantuml/1.2019.10/plantuml.1.2019.10.jar"
-  sha256 "f72b0b9631535141b50ea7e528c1046307f7a1b4f797100f09622c17ef3cad18"
+  url "https://downloads.sourceforge.net/project/plantuml/1.2019.11/plantuml.1.2019.11.jar"
+  sha256 "95a60d5ac3f2b736da74a0d8e9941b521ca4640427b4693adfe515974723344d"
   version_scheme 1
 
   bottle :unneeded
