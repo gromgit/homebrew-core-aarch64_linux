0a398d998efe8a00ec732fd27a3a90e1f1d1234c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 04:09:10 +0000

    curseofwar: update 1.2.0 bottle.

--- Formula/curseofwar.rb
+++ Formula/curseofwar.rb
@@ -7,6 +7,7 @@ class Curseofwar < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "92c8a1650e5e18866211a79499abad640ff8ce8f49b54c50043e7f421d8d8e47" => :catalina
     sha256 "4ce2eaede3f4636c0ddb7a16d65ae6b750c9e20342715841e5cacefd3d61b0b7" => :mojave
     sha256 "b43f143f9fe919e2d88b241865513967054550a852fd62747ed759ac1a53500b" => :high_sierra
     sha256 "8a19536dcb6b4a3f508c800980d6300399d1b9d7af1683df7ea7f4dd502cf238" => :sierra
