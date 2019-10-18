a7ce33ca3ea4fb8546c1a38f341c4df8b77ef642
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 14:23:03 +0000

    mdf2iso: update 0.3.1 bottle.

--- Formula/mdf2iso.rb
+++ Formula/mdf2iso.rb
@@ -7,6 +7,7 @@ class Mdf2iso < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "ac57f5ffc3e1ac884d74b08dddce518e60f878e627cbfccc7dcb4642c5eb0653" => :catalina
     sha256 "444df3ab6a8ee34700f26459e93488d7ac3d3974ea29baa5d83f59d0014f6232" => :mojave
     sha256 "b41429cb8a4191a705b656b627a375cc32aaf8992cb241e30fe6c66c4ab56c9c" => :high_sierra
     sha256 "bc1358412281b1e486d9d1b6d25ae5665b02ac14f93f03603a966bd44ffda1d7" => :sierra
