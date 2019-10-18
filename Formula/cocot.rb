532a1a2e28ba034a95f9e6d593382941d67b82d3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 02:18:44 +0000

    cocot: update 1.2-20171118 bottle.

--- Formula/cocot.rb
+++ Formula/cocot.rb
@@ -7,6 +7,7 @@ class Cocot < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c56c078fce103138a45bd1c715dc3854b9eddf207575fada0e736b866d4f46bb" => :catalina
     sha256 "c5b0aa39597693d037438bd0f411965754539aaf50fbb2cb3b2a61082b6f4178" => :mojave
     sha256 "0070eb38e06043e3c1a4ad1b77205a6ed978ed300e8d0bb407391fecb191b050" => :high_sierra
     sha256 "a91ba93032e33b6a062b82f2df0b9170d5269cf0312d75eb6f16341fca54f9bd" => :sierra
