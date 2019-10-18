3c3ab546d32ff3993a710b811a425e1e7d699a74
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 03:45:50 +0000

    csvq: update 1.11.5 bottle.

--- Formula/csvq.rb
+++ Formula/csvq.rb
@@ -6,6 +6,7 @@ class Csvq < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c1d7e2c55b13599cc26da71e4048b52ac1dcaa78687c687e24d65541e5453d66" => :catalina
     sha256 "7fa8a6884a00705e8dfd33979ca6291e76d82f7b259381da5057d9df8bffb85b" => :mojave
     sha256 "7d1fc7475e9e1982fce98fe8d6f87db807690c7726f7491b8608e749eaefa895" => :high_sierra
     sha256 "128af1146dddfdc4c0dc78ddb4abf819c784b82e753db132a075087c47c63932" => :sierra
