5acb569ad24d3f89e23aa479aeeafa56c57bdf1b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 02:01:30 +0000

    cmus: update 2.8.0_1 bottle.

--- Formula/cmus.rb
+++ Formula/cmus.rb
@@ -8,6 +8,7 @@ class Cmus < Formula
 
   bottle do
     rebuild 1
+    sha256 "d89a46acdec0e5830d41d2d40419d62fe555f34b99f93bf758e749e023e9b294" => :catalina
     sha256 "5597b87c7fcdceec789103df12ae89408e69f98668da522b7ee6a908e622c290" => :mojave
     sha256 "38f6dda244d82bc960b7e1c65e2e0316c2cde6cc61174bad763b7243e6f88ad8" => :high_sierra
     sha256 "42cab3ddb96e7b36b2b8cf67d2384adea5169e2955841c35166aece5afcd9329" => :sierra
