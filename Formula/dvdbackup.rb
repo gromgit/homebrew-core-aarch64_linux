61e6defee002306d54c8eb8d8f1a75408cac239f
Caleb Xu <calebcenter@live.com>
2019-10-18 09:17:59 -0400

    dvdbackup: bump revision for libdvdread update

--- Formula/dvdbackup.rb
+++ Formula/dvdbackup.rb
@@ -3,6 +3,7 @@ class Dvdbackup < Formula
   homepage "https://dvdbackup.sourceforge.io"
   url "https://downloads.sourceforge.net/dvdbackup/dvdbackup-0.4.2.tar.gz"
   sha256 "0a37c31cc6f2d3c146ec57064bda8a06cf5f2ec90455366cb250506bab964550"
+  revision 1
 
   bottle do
     cellar :any
