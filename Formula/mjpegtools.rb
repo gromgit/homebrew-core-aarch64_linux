27345d058ee86de40ee58ac6c148d48e3e35823b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 16:48:11 +0000

    mjpegtools: update 2.1.0_2 bottle.

--- Formula/mjpegtools.rb
+++ Formula/mjpegtools.rb
@@ -7,6 +7,7 @@ class Mjpegtools < Formula
 
   bottle do
     cellar :any
+    sha256 "5628d3b16a0e3172ba49a38b903b5be2fcb2595ce4919e32c41e39a89a250102" => :catalina
     sha256 "c8a22d895e9835274994bdf72b0ca6f3c0df523e5dc8e281ed7d1fd7ae3b41eb" => :mojave
     sha256 "7f47c9df784de38ee02726c1381b42a5924754c7702003ee8fafbae296302638" => :high_sierra
     sha256 "9f5c0eb81540bf70ff8b2352a8ea21117a75c6dbdac58ea8d04d0da47a639cb9" => :sierra
