9a4414f9e90f22b54ebd66f4ac1e63e1e6f54093
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 16:51:15 +0000

    minimodem: update 0.24 bottle.

--- Formula/minimodem.rb
+++ Formula/minimodem.rb
@@ -7,6 +7,7 @@ class Minimodem < Formula
 
   bottle do
     cellar :any
+    sha256 "5f9cd0c17ee17754bfe88c6e275111270e0a0d0cdebb663a0045d6ad49c8b9a8" => :catalina
     sha256 "4c89fe35fbc5478c20d1db50f023c7c89467b7fbd17bd77810a6e8ff63e4b945" => :mojave
     sha256 "091170cbfa058de152f2f1af5f2436963297c01e323e80fdfcd5bcf6d8c9cabd" => :high_sierra
     sha256 "224fc001ea92a1df8133680c6eb9b6d659912d5e8ce84e8c12509a671538d8ae" => :sierra
