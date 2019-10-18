04359a61eeb10ae2f7daa30a0a8d3e19c286ad6e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 19:46:35 +0000

    mysql-connector-c++: update 8.0.17_1 bottle.

--- Formula/mysql-connector-c++.rb
+++ Formula/mysql-connector-c++.rb
@@ -7,6 +7,7 @@ class MysqlConnectorCxx < Formula
 
   bottle do
     cellar :any
+    sha256 "2a77531d227a134ad59877a2ac8901970f364b27d9afeb6d0619a7aebc5cb29d" => :catalina
     sha256 "46b923ef7c8e958719228f76134a6205d3ad597d0f4d85dc6842fb1f2ed14d40" => :mojave
     sha256 "0eb28a81c15aa1b5d860a9655f9b99b8dfb2733b3d3e9d0f1c155e85eb916b0c" => :high_sierra
     sha256 "76973ed4a3a1f6f858696fd919ab60d188e0f91daac9e4899df3bbfabbae4620" => :sierra
