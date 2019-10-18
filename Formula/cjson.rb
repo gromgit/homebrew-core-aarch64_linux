9eff3f045a91e4ab735b39f4ceebfa08155f453a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 00:49:40 +0000

    cjson: update 1.7.12 bottle.

--- Formula/cjson.rb
+++ Formula/cjson.rb
@@ -7,6 +7,7 @@ class Cjson < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "b402e60f6a786780c9739b91845c2f8df2ea60ce8749e42db11cbcc5f86bb52a" => :catalina
     sha256 "3364af8e12165cfd7721e8d64e1a4ee021a2fe2fc64b09726a8624f262d8449f" => :mojave
     sha256 "f8a92b03d0ff1147d604517317366706e93cb398ffa8e208b84b4bd91282231e" => :high_sierra
     sha256 "be61141bcc7cd3706051ead5a2158b7a92f840e0802f745e4c53052a82616d55" => :sierra
