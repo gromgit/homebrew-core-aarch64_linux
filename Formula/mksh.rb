5a934829065fd37764ec837f2b05ea8b8354961c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 16:48:12 +0000

    mksh: update 57 bottle.

--- Formula/mksh.rb
+++ Formula/mksh.rb
@@ -6,6 +6,7 @@ class Mksh < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6ff89051970128cae9f0bb12e1934a52f6d51b6ab77f02b601ff4b708ee40e9a" => :catalina
     sha256 "0028f1d8028129a398937bed7fb3b2f7608932386d116d1b05527ff266c3470c" => :mojave
     sha256 "d666938f31f5c9e5a0dd4b5f0e823ca7d5f2015cc447284ff0204cdc874d70e4" => :high_sierra
     sha256 "1fbdae63a63116e3a321b71d632214ea2419d404db8939f7cb3471a2de35c761" => :sierra
