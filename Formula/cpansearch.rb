f7af3d22056cb1f14ff88e81ba500a36528ad94c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 02:56:34 +0000

    cpansearch: update 0.2_1 bottle.

--- Formula/cpansearch.rb
+++ Formula/cpansearch.rb
@@ -8,6 +8,7 @@ class Cpansearch < Formula
 
   bottle do
     cellar :any
+    sha256 "f5ad7240f2e1d3004c9b80d232192bbc50dcf777bdfe92fa73172e93476f5ef2" => :catalina
     sha256 "5d583c37a54d9d6f96c625faf75b40c53a2ae59b8c9960f51a6f9bc215fa5bae" => :mojave
     sha256 "e8197124d1341e8e5d8348cd322eac2bfa782d885c808b5322a340eb7b91ba8b" => :high_sierra
     sha256 "6b4545b0455642a3b4f3c92ef480e704742cd06fd6ff64d24f9a5edbb3bc33a7" => :sierra
