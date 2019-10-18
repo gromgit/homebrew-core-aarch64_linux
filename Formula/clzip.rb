65eece9d15012b142638638613ce063825ff7db7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 01:55:02 +0000

    clzip: update 1.11 bottle.

--- Formula/clzip.rb
+++ Formula/clzip.rb
@@ -6,6 +6,7 @@ class Clzip < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ae164ce5fd5a020988dc4e177a25cf19f924b282e561cdabf57577c88940ac2b" => :catalina
     sha256 "654a41ff519d4109d38f17c3fb321f130e60c9d72d137674f2dde9ef5cf129be" => :mojave
     sha256 "2a3bf6819a2fdbef49fa7bf1e1cea7ef6c6d090bf8fa787fe7b2a582b2631045" => :high_sierra
     sha256 "26dbdb3a397aa3f62acc15bbbf599a32e5b832564ea6ddc6e15327baac90b5ba" => :sierra
