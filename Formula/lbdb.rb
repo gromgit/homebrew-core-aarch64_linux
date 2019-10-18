a94fe10c2ca3f95e7b77b18299a9dad0f031a2dd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 12:19:04 +0000

    lbdb: update 0.48.1 bottle.

--- Formula/lbdb.rb
+++ Formula/lbdb.rb
@@ -6,6 +6,7 @@ class Lbdb < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "838c923b4c208ace533b34a2eba8b78c348bd3cc89fdbdb36c42a45b7e592f52" => :catalina
     sha256 "a02ea4967e809c9254dac7726c7ee9365e1ba99a8eef68fe6dfd4de34d2fa5af" => :mojave
     sha256 "0e5c126d7cb1c5ed3211ee1758a388597b00f32c3fb22c0420919e3667371ee2" => :high_sierra
     sha256 "d22e5b8b66ab51d8c6d324330178284c9b35e3ef17362724089e1e979e9c05d4" => :sierra
