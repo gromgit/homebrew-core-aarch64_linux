530294707b0637996ff79ebbada64373fbba4b20
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 16:15:52 +0000

    menhir: update 20190626_1 bottle.

--- Formula/menhir.rb
+++ Formula/menhir.rb
@@ -6,6 +6,7 @@ class Menhir < Formula
   revision 1
 
   bottle do
+    sha256 "2d6b3dad7844c63c86217b227f88d12da09abac19d81093cac2f6cbf8306fc8c" => :catalina
     sha256 "d0ba1b0ca38d85177a02dc457c8e873cd4d1909dbc4b198161c80ba97765a1e8" => :mojave
     sha256 "b23ef01c671a973ff8ad1f2f9e1563f0ec2b348edda1d7f3ba716c343ca687b7" => :high_sierra
     sha256 "bbd7c51fbb7f3167328bfc0e7c7a6f28582509b32c7afd067e4acb7115f74112" => :sierra
