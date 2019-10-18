9849246c70e4ccf0bb8cff1b461da737c55e9bba
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 17:17:17 +0000

    minidjvu: update 0.8 bottle.

--- Formula/minidjvu.rb
+++ Formula/minidjvu.rb
@@ -7,6 +7,7 @@ class Minidjvu < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "ac5ddf434a115b421a2fd20645d09c690b559e7c135bfa71687d540f80e9dadb" => :catalina
     sha256 "6bb235aea08165b0a9d359f3813fa3e1760ff283697734761d9663fe1488a0fb" => :mojave
     sha256 "e86d9876389882d5cc6db29798566bc845584280a4fb4f5baf6226313a74dd6d" => :high_sierra
     sha256 "29966954c6c7ff78b48f41a31574369ed58fd9b52cea613891726e8cc444bffe" => :sierra
