eae5866761f3b743727afdd964f237e7b34008d9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 00:49:39 +0000

    cksfv: update 1.3.14 bottle.

--- Formula/cksfv.rb
+++ Formula/cksfv.rb
@@ -6,6 +6,7 @@ class Cksfv < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ee0e6d47bba7ece1e9d3f89db630ba943d2e85834d1b2c7c4307e449f8de517f" => :catalina
     sha256 "fcefe9fe38fb4555760f8d7e651f19aca85115583f896ba602b1396e71547743" => :mojave
     sha256 "095a3a02e99d3f018472202e65231212c818750cd91d42c32a95957b407c1d4b" => :high_sierra
     sha256 "4414da8e35f9d69a0e04e4a1942745f98b5234891a04935627248e2e6954e17d" => :sierra
