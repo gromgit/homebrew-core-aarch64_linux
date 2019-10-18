67c0b78ee51751a316569503c52e521ed5b167b9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 06:41:01 +0000

    cosi: update 0.8.5 bottle.

--- Formula/cosi.rb
+++ Formula/cosi.rb
@@ -8,6 +8,7 @@ class Cosi < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3d5d2579e3a0f54b032908843a6aa6433bfeade0170ddd740f6a5c77825094b1" => :catalina
     sha256 "f368b353b76109a1ef3d4f3859bbac2179bf09bb8ce8a7d349a816d12e9fa298" => :mojave
     sha256 "91a07519054c07be10182099d75adf74d017a74053d14985653e272ec1238fff" => :high_sierra
     sha256 "b387fbd7aa15c4348e56fd1e28adbabee656cd142c979ea556e2d5e8dc56d4b1" => :sierra
