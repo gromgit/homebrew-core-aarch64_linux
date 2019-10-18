f7bf4e2fddc35896a21273a3198d432d92af7ef4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 00:49:39 +0000

    cig: update 0.1.5 bottle.

--- Formula/cig.rb
+++ Formula/cig.rb
@@ -8,6 +8,7 @@ class Cig < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "bb93970229fc7a62a6ca4b0c446ad36f135d2160aa0dce04fa5afbdc072291d3" => :catalina
     sha256 "6ae38e73bed4326d85c7f31498b0a5715d877c7a2e32aad9987ba7726efe240e" => :mojave
     sha256 "9215f225d4b314d1047f6bb4e5c909b82b456d2005fffed8c637ca2d63641791" => :high_sierra
     sha256 "5d4eb1f34f8b185513d59dc9072f1a95555dd222f0f7a0526c89983e1643fef6" => :sierra
