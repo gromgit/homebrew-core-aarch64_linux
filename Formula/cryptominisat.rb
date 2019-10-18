94d7808f74a48fcc3fac1dfb249f62ae03940c5c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 03:48:50 +0000

    cryptominisat: update 5.6.8 bottle.

--- Formula/cryptominisat.rb
+++ Formula/cryptominisat.rb
@@ -5,6 +5,7 @@ class Cryptominisat < Formula
   sha256 "38add382c2257b702bdd4f1edf73544f29efc6e050516b6cacd2d81e35744b55"
 
   bottle do
+    sha256 "2344fee3bb2a80ec14eb320faabade428e32d1bbb9570b2867b54e9f94c5fe69" => :catalina
     sha256 "8c68cbd0307ceddc6a3ba70f488ed4ebb43b9d649cf1df3eb75788b3a5c58e3c" => :mojave
     sha256 "7be2c4e5ace97acdf39f6a4941fd601cb777e1d0c58117f11ab9b2711ba19fae" => :high_sierra
     sha256 "4633506d240ca8298bf26b6f48985c4e04877091abae50f20413f974fbb2bbbd" => :sierra
