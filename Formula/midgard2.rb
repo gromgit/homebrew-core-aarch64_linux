e13e92618f47c966e32648b628d79170712c55c2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 15:48:06 +0000

    midgard2: update 12.09_2 bottle.

--- Formula/midgard2.rb
+++ Formula/midgard2.rb
@@ -6,6 +6,7 @@ class Midgard2 < Formula
   revision 2
 
   bottle do
+    sha256 "c3ed243fb5c433a40f959a357c8c78258a62b2fcc7eb5f3e6d94ca0b9cae3159" => :catalina
     sha256 "d37c0fefe73ad6e8360585d80e26e11f7e1f5735fdf8382f0c3795f95fa93d68" => :mojave
     sha256 "08df9e1d7487d38c8174047aa9d0620bc1f430f23602acba90c2ec9978a3fdd9" => :high_sierra
     sha256 "2aec9cbfb7a432a4ad73157831b9d5f6573ae4b85141410040cb0f053435541a" => :sierra
