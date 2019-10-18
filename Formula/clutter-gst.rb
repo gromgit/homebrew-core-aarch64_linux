2b8e8cc9774a9552ecf130b6721f02143a16077f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 01:33:24 +0000

    clutter-gst: update 3.0.27_1 bottle.

--- Formula/clutter-gst.rb
+++ Formula/clutter-gst.rb
@@ -7,6 +7,7 @@ class ClutterGst < Formula
 
   bottle do
     rebuild 1
+    sha256 "9e5e48bdf08599d63be7a75eabac3221ce4b1799fcf51d857d37336345393c16" => :catalina
     sha256 "3c4dcfd6b9b95d1f0a96e33d23060225c322224e21e4501c8e2b5a6ef32a9ebe" => :mojave
     sha256 "b60c1d84cf2f4e9cf931d10ce759d4b21f08a7a2288dd81cbab78854d3a767a2" => :high_sierra
     sha256 "fb997fb8ac4fcafd52690d64c12dfcd7776630ce717521c7cc0ce7d44ae3b8f7" => :sierra
