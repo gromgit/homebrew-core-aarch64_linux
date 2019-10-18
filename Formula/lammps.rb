d6deb3693e5fdc60da1d951919ae6c89ec460985
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 09:31:15 +0000

    lammps: update 2019-08-07 bottle.

--- Formula/lammps.rb
+++ Formula/lammps.rb
@@ -9,6 +9,7 @@ class Lammps < Formula
   sha256 "4e8342b97441deb76c2fddd8a4fd23beb718cb0910779d98e334026391ed3961"
 
   bottle do
+    sha256 "604056c80bb3b36f0a1644388bff26f2bcdc3e2f2541247e9a7ce941b20e9bcc" => :catalina
     sha256 "e14fbf9e68b16804c9d86f525e92190010c580c479263eaf194ff1e23dd877b1" => :mojave
     sha256 "eebae98d9606af15a5281f07843ea41aeb94974a52f0023e9734aab3db430406" => :high_sierra
     sha256 "cd52cb03b501cb8d65f0dcf8baf3857dc13e2a2206016ef559bf5ff3eba31e1d" => :sierra
