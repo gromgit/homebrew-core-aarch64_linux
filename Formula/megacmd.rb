623ffeee35077471aeada27ee41e44acce934a95
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 16:15:51 +0000

    megacmd: update 0.016 bottle.

--- Formula/megacmd.rb
+++ Formula/megacmd.rb
@@ -7,6 +7,7 @@ class Megacmd < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "80a5775ff5e5fe1e958cc6345a2d53f13e37bf366c7f4c8f3033f218851de813" => :catalina
     sha256 "e7612192a4ac2e79363d8441f7d0f007c319a5aafd8e4f56e805335cbfa72d21" => :mojave
     sha256 "02e75c7bbada7c3b3a230f3395448ec1dfb8598e7053ac6ad6a2b0b2f264d9ad" => :high_sierra
     sha256 "8f60fb2783c1481f086e401e7cbfb2ada3a6e82a35e6d2e1a24643a7c7d8453f" => :sierra
