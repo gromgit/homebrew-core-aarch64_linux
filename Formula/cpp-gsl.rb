ffe8eb9f61e20fd502da2c998a371dea5d3e8c7a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 02:56:33 +0000

    cpp-gsl: update 2.0.0 bottle.

--- Formula/cpp-gsl.rb
+++ Formula/cpp-gsl.rb
@@ -7,6 +7,7 @@ class CppGsl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e981b4b2210a6705349862e2bcc044b0d487ae8723b9be20e90e9b1f58e0db1a" => :catalina
     sha256 "88805bdbc58c3279d13269592c9b0fe8fd6616efe8d7fa71f021f1f7a74f89db" => :mojave
     sha256 "88805bdbc58c3279d13269592c9b0fe8fd6616efe8d7fa71f021f1f7a74f89db" => :high_sierra
     sha256 "2179e8719714e6fbaeb8f0536bc38b6a1d24a39f3ad85b3f738aa7cb6955dbd5" => :sierra
