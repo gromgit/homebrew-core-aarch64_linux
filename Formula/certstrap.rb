4b841a44246c4216e5829680d64ee5445b6eabd1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 00:31:50 +0000

    certstrap: update 1.2.0 bottle.

--- Formula/certstrap.rb
+++ Formula/certstrap.rb
@@ -6,6 +6,7 @@ class Certstrap < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "154b80812c45ee86c241fac030f98ef5fc368021c0dd6fbebde4fd4a9dbf6803" => :catalina
     sha256 "b7eb23b913b4f2bdb3d854a6e22f1b25c71018d369fffeec5a16b8df8eaf1fe8" => :mojave
     sha256 "4dd108d82d046be8576b460d9a2878d593669fa5b11cd015c6aa1f9fc26134f3" => :high_sierra
     sha256 "aa650a24795207c8860ba986e34f2d320a218b8ee414bd73a5390aaf25e6d214" => :sierra
