b65dba12d45355766a980778203737cf3af58bfe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 10:42:47 +0000

    ltc-tools: update 0.7.0 bottle.

--- Formula/ltc-tools.rb
+++ Formula/ltc-tools.rb
@@ -7,6 +7,7 @@ class LtcTools < Formula
 
   bottle do
     cellar :any
+    sha256 "bcd064f64a21f101f6599646306ba65c40ce7ec44fd7b6e2d8f29b4fefeebcc9" => :catalina
     sha256 "ae65212fa593ee7015eb7bfa63b4e7e7691e56a7db0fc1a82a311aef184aae55" => :mojave
     sha256 "15da8efd84adb9d9eb9c7b4450c75e326679b20bed258c8e7011fc6eb2cc9b20" => :high_sierra
     sha256 "51df0ba95565d43955bbdf0cfbc216696b4002f8cc95c80d8f6b387eece034d1" => :sierra
