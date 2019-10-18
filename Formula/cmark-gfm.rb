e3220633a638c23dbaa49598695b271fc5fb5bdc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 01:55:01 +0000

    cmark-gfm: update 0.29.0.gfm.0 bottle.

--- Formula/cmark-gfm.rb
+++ Formula/cmark-gfm.rb
@@ -7,6 +7,7 @@ class CmarkGfm < Formula
 
   bottle do
     cellar :any
+    sha256 "f318b456ed27ff10c495df41c9aed0208761e9fb10b43905d37a39ae30321cf0" => :catalina
     sha256 "82cf0b26777bb839ace2b833e5390c17f5e4da1e91f61c8729f084e3bac4063c" => :mojave
     sha256 "b816bbd9cf54cca8146e55cb9920fe8bb182599d4e0157582d39f6ab3b16d999" => :high_sierra
     sha256 "2f5538c985404d43a128b7ae61ae93c4c4a9c9d277fa14e28cf1f729c0337221" => :sierra
