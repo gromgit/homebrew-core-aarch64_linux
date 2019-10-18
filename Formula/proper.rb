8da7c98d61e929d4a604690b1a9b4beda19c4261
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 06:38:50 +0000

    proper: update 1.3 bottle.

--- Formula/proper.rb
+++ Formula/proper.rb
@@ -6,6 +6,7 @@ class Proper < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "34011621b0cd440a49ee2c13d0bd968b7cf8ed8b0b817fbd6793082de5d4728c" => :catalina
     sha256 "a93bfdd9a7558b9ad0cc013f3aca0cadfe86535948730ab6861c10931cae4b92" => :mojave
     sha256 "f9f6404b3c025d06fb1b6ab0155716fe5f59f337ac6884b92d4ac01d6677b4c7" => :high_sierra
     sha256 "a80e754b0bb2ce17d223034734cd1aa473532ba743cf45cbff89fa154af18220" => :sierra
