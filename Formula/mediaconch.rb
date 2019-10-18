f726c41df48c32114df99ace9e367bc89619cd25
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 20:20:44 +0000

    mediaconch: update 18.03.2_1 bottle.

--- Formula/mediaconch.rb
+++ Formula/mediaconch.rb
@@ -8,6 +8,7 @@ class Mediaconch < Formula
 
   bottle do
     cellar :any
+    sha256 "41a49bbafbffc220f140d8e466f1507757cbe552f8de4ca306217affbf1e6dd5" => :catalina
     sha256 "9d59b85fecc5d5caba622fe57358caab23c8ea904954a137b99e66dd4f7fedec" => :mojave
     sha256 "d59cfb9ac07ffb7eacc4c7970c38676a3909f0966481b99c745735bf87db7b8e" => :high_sierra
     sha256 "fdb3934174a68121357c21d4f0800e8bbbaa6a296f3386ab52e5298fde96a6b6" => :sierra
