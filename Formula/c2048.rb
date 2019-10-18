ac337ead05dea3f73605ab2c372346aad7cde024
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 00:04:46 +0000

    c2048: update 0+20150805 bottle.

--- Formula/c2048.rb
+++ Formula/c2048.rb
@@ -7,6 +7,7 @@ class C2048 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "727165d714b210f559b5f5450d6608bed0e7bfbf87c7a7cd5994259b65865411" => :catalina
     sha256 "dd0cc60f407ccb43f471d7123b9a09fa0b2161ee083638a432ee25795a96ca8f" => :mojave
     sha256 "e5f553baf87fc7ac9f0fa4471d3e9be29328df167700181d9663f61293436888" => :high_sierra
     sha256 "d2f33783cf7cd2ac69eaed113d940aca31e02e5863fcdb40e200e3fe9a4d0623" => :sierra
