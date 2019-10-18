b23ba1f72a33fd95c99eb6c57fa4217e8faa0910
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 15:48:05 +0000

    micropython: update 1.11 bottle.

--- Formula/micropython.rb
+++ Formula/micropython.rb
@@ -7,6 +7,7 @@ class Micropython < Formula
 
   bottle do
     cellar :any
+    sha256 "9c497518754565c12bff1f5ff06158fa7769895f7af52f50d059c4e9049bd6ba" => :catalina
     sha256 "186dd16c2fc9a965c56e5339f571489e99d9ccb29ca46769590fbd40c6c013f3" => :mojave
     sha256 "7934a26348e2fdcc1dd845e31ded1192b7556d0a8b76ba4733c7dacf0c7c755d" => :high_sierra
     sha256 "daabcc35c45501a1b431714f226dd5072ffe6b53b18ff7230904a3040c1c3c4e" => :sierra
