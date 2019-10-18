ee1d0e5564cc1a38f5fa2010f1b4440414504c74
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 00:29:51 +0000

    chkrootkit: update 0.53 bottle.

--- Formula/chkrootkit.rb
+++ Formula/chkrootkit.rb
@@ -7,6 +7,7 @@ class Chkrootkit < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "23a9f903721d19c0b6201163cb937823970c66592f094c673b1de1036da8bef9" => :catalina
     sha256 "286de88eef77a53b9c7fab85ef3cec8b9876cf49a48910cbb591e44c9ca5d631" => :mojave
     sha256 "55ab9957505513fd81d670c54e5ad1834fb72ae9cda7bd7cbc63f98feeccf24a" => :high_sierra
     sha256 "f16966e93433cb877b04be8ea086c8a23905290643099229ffa3d665b2d11994" => :sierra
