0e4128e0ae5dcb5cbabdb3fe0a16ca36d5eb043a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 09:45:07 +0000

    lepton: update 1.2.1 bottle.

--- Formula/lepton.rb
+++ Formula/lepton.rb
@@ -7,6 +7,7 @@ class Lepton < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "61424564a043a4a02ac9beab50a3c57dc454d4c301e536cf17d1457aac82bba9" => :catalina
     sha256 "5b3d88dced1ee91a38ba4a7ede4f21cac5e0ce2d9d5cc3b5f89775c36fbbb177" => :mojave
     sha256 "496840db331a445a5ba66475a58782a26d5a910b9d7ed8fb1aa2e0f9b98c68b9" => :high_sierra
     sha256 "6713be0e881459057b561cbfd6902d167dc9601be856b8715e9abd6ffc02b605" => :sierra
