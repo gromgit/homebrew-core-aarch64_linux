5e8b9e472c410f6da874194b6dffb26736df7cc1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 20:23:17 +0000

    mgba: update 0.7.3 bottle.

--- Formula/mgba.rb
+++ Formula/mgba.rb
@@ -6,6 +6,7 @@ class Mgba < Formula
   head "https://github.com/mgba-emu/mgba.git"
 
   bottle do
+    sha256 "100e6efe060482e007f220a776c45986a48e438e49383a8612ae8c53e6838855" => :catalina
     sha256 "78f1c95c5e21b075394df17f70c1263fc9912a645707eaeb7bfb63ab263ffe0b" => :mojave
     sha256 "7b805b6bd27e90c706b742f6ec0ae3cbbcb24be31426c38e4da41e3d854cf387" => :high_sierra
     sha256 "aa1c834a99df54edc2fca5c5f9aa8e167ea04b8cd3fcdb3b9aa88b13f5a16e96" => :sierra
