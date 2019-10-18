8e9c7b9cbe7285a4d67c1154190a173e7f8e63a1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 21:04:25 +0000

    go: update 1.13.3 bottle.

--- Formula/go.rb
+++ Formula/go.rb
@@ -15,9 +15,9 @@ class Go < Formula
   end
 
   bottle do
-    sha256 "359757bdc1fb20cbc8025685fd7cd91bb45e2277e73c043014c30d2744c72e6f" => :catalina
-    sha256 "77887193657770ff7ae524ddfef468a3063f4f6a6d38d80391c0c50d0d168b09" => :mojave
-    sha256 "4366c42dfa310d00bc749043228e4fb41dd073186ce3d33b426cb69bf40480d5" => :high_sierra
+    sha256 "7dcc9e0ebc1f73596413552aea012585ae86f2db891037b5433f2c06a348d5a9" => :catalina
+    sha256 "e6d507bf402984b0c801286c9ebc5ac0b7627f8c07f6b40b5ba45c6372ccf2aa" => :mojave
+    sha256 "0a3f4481b23c9f21defea8c15829b06c5ec672256778f53f0b8bee10f87fd2b9" => :high_sierra
   end
 
   head do
