fbedec9f584f4cfb822b38505fbf2f1feb0f0ec3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 18:39:34 +0000

    mpck: update 0.21 bottle.

--- Formula/mpck.rb
+++ Formula/mpck.rb
@@ -6,6 +6,7 @@ class Mpck < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "45f8695f2758dd07237c333e8a17aa38f8d0aed4e87e8b5dc7fea7bf4537b0e9" => :catalina
     sha256 "e819ac8ce7eab3b4f83bcdf83cfbb129a9e3cebb36e314dabca646f808ed6257" => :mojave
     sha256 "3ecd47f83f5645cfaf2bfef23b5b9a1b14bb36f2ec146409ca44d9d5f25c3401" => :high_sierra
     sha256 "cd283270b83cf83c3e3a3c393404c1eca16e1620ced195821b97fe5ad6b39236" => :sierra
