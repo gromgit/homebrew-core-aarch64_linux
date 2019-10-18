d157af1bff443641194e1186cbe49c341c04eb35
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 12:19:04 +0000

    lcov: update 1.14 bottle.

--- Formula/lcov.rb
+++ Formula/lcov.rb
@@ -7,6 +7,7 @@ class Lcov < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e41c1e1b7e38279cb0e0e8e638046ffe4172ae18bd43afcb767eb52f28d822fe" => :catalina
     sha256 "2ca04e6c269ebf3587be2e485e0a6be9de81963e0a59a852c3be227bcb43919e" => :mojave
     sha256 "2ca04e6c269ebf3587be2e485e0a6be9de81963e0a59a852c3be227bcb43919e" => :high_sierra
     sha256 "e6d2a1d121c878d81d051de0f9f7fbda37c438a620de9c99d0a0c7540b5f61d1" => :sierra
