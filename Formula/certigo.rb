06f33fe289f10417000f66c2110c122fd670bc6b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 00:31:51 +0000

    certigo: update 1.11.0 bottle.

--- Formula/certigo.rb
+++ Formula/certigo.rb
@@ -7,6 +7,7 @@ class Certigo < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "debd4cd336c0e3e247645810c617555b776dff9b2057e60b7553c4f2ee6064a5" => :catalina
     sha256 "46fa9befb01dd5aa3a923848f1178ed219500400a1d2e2ba644bcb44c6508273" => :mojave
     sha256 "01c67dccacfe76630409daaa8cd8ef53a6680a1c5f6adac6be1cef3adac9a12f" => :high_sierra
     sha256 "55c18b8cf5af060425447fcedac72f9f187efdb17fab4ef37de9577251767797" => :sierra
