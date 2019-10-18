eba73db3bb14697e03bf4cbd07eeec9ae3466433
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 00:04:46 +0000

    c14-cli: update 0.3 bottle.

--- Formula/c14-cli.rb
+++ Formula/c14-cli.rb
@@ -8,6 +8,7 @@ class C14Cli < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "16fc8110913a233923aa9a5e5fae8e28a300e1639a2bc8862785c34d61623229" => :catalina
     sha256 "9547e5c45d10d48de2ff95aabef2cb6d446fbd6f3b706df90d72adf2a363d96b" => :mojave
     sha256 "43107ba495420cf65ab7b3a4f04c1a62de77594ed318e06aba3101213191e694" => :high_sierra
     sha256 "6129daf1900d717da72f13909af71af2bf0d29325f798c02bcf4a68b1bd8edb3" => :sierra
