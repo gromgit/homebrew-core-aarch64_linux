56fa8345e1b0b83c275212c2482730862a6b6cd7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 02:18:42 +0000

    codespell: update 1.16.0 bottle.

--- Formula/codespell.rb
+++ Formula/codespell.rb
@@ -8,6 +8,7 @@ class Codespell < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "adb1bc633bab65e7cd2f44a0b658cd83a3c3c2a00ac5d859c4b341be2aaf3018" => :catalina
     sha256 "577e71571fcefa1ef4edae6d790aa80e978afb21de6f12d232601885b0d30ff9" => :mojave
     sha256 "330c2359a5e5050d555d9f70c29add7d3e7c9554d89ed43607235a896453b461" => :high_sierra
     sha256 "b77254f7aee7303a2edabb0a780a78407e7d8c55a44e6a5c75c9dd799beba12d" => :sierra
