b83cfd347bf14accf5c1283fec5a14cb9abd6e95
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 04:12:15 +0000

    cvsutils: update 0.2.6 bottle.

--- Formula/cvsutils.rb
+++ Formula/cvsutils.rb
@@ -6,6 +6,7 @@ class Cvsutils < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f8637db7bc660a9953b96bca2e68d1ba7c56bd0766e0ef12bc6b0b42d972ae3a" => :catalina
     sha256 "e497ac1ba036fec1ccd8d34b2ec6262f9721ab805d0636f073c5406ef4fbd922" => :mojave
     sha256 "102456ac28b63271b03a5722e8421d6273005c54203f4f818678be065479463b" => :high_sierra
     sha256 "d1f2e13e0df6dbb767a04f7e206114c119f9e6435f227e07e14b4d200e6aba8f" => :sierra
