9101b13ab6ed8a55c0bac43e80f46fd0a4d05018
FX Coudert <fxcoudert@gmail.com>
2019-10-18 23:12:30 +0200

    ykman: update asn1crypto
    Closes #45525.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/ykman.rb
+++ Formula/ykman.rb
@@ -5,7 +5,7 @@ class Ykman < Formula
   homepage "https://developers.yubico.com/yubikey-manager/"
   url "https://developers.yubico.com/yubikey-manager/Releases/yubikey-manager-3.1.0.tar.gz"
   sha256 "0137acd30cffa60421dde8277b3bb0c5d5e2179d36df0d190de6faf7eec76359"
-  revision 1
+  revision 2
   head "https://github.com/Yubico/yubikey-manager.git"
 
   bottle do
@@ -22,8 +22,8 @@ class Ykman < Formula
   depends_on "ykpers"
 
   resource "asn1crypto" do
-    url "https://files.pythonhosted.org/packages/fc/f1/8db7daa71f414ddabfa056c4ef792e1461ff655c2ae2928a2b675bfed6b4/asn1crypto-0.24.0.tar.gz"
-    sha256 "9d5c20441baf0cb60a4ac34cc447c6c189024b6b4c6cd7877034f4965c464e49"
+    url "https://files.pythonhosted.org/packages/c1/a9/86bfedaf41ca590747b4c9075bc470d0b2ec44fb5db5d378bc61447b3b6b/asn1crypto-1.2.0.tar.gz"
+    sha256 "87620880a477123e01177a1f73d0f327210b43a3cdbd714efcd2fa49a8d7b384"
   end
 
   resource "cffi" do
