1f7787a9ae85e3fdbd64c051fb817ed32753f0ef
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 19:05:40 +0000

    mruby: update 2.0.1 bottle.

--- Formula/mruby.rb
+++ Formula/mruby.rb
@@ -7,6 +7,7 @@ class Mruby < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d11cb38cf918e44362f4369f1c3f46414059fc2caf4703b3d32f35d34fa44606" => :catalina
     sha256 "3279a9fe511530c4fe0a56323efcbfd555d2fa981204a0d1595f0365f56cdd59" => :mojave
     sha256 "3e808c9c9a25a98a20a8af26a369bf688db7c9a635e3ae52836e2eda0e42b442" => :high_sierra
     sha256 "5244571e026eefa0158a177b1b6e1c42c91760cb8e3b5ef449277660b31aaec6" => :sierra
