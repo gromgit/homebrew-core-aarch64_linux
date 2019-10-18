1e1370e0fddf2970695ab2e3b12ed5f24eb71819
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 05:43:42 +0000

    open-mpi: update 4.0.2 bottle.

--- Formula/open-mpi.rb
+++ Formula/open-mpi.rb
@@ -5,11 +5,9 @@ class OpenMpi < Formula
   sha256 "900bf751be72eccf06de9d186f7b1c4b5c2fa9fa66458e53b77778dffdfe4057"
 
   bottle do
-    rebuild 1
-    sha256 "a1e1363f461531508564a90ddbf787c4d3e265bc581f1ec9ad4e5ce0034a63f4" => :catalina
-    sha256 "322c39684795f8eaf9e8f342528ecc119aa65566db10963272b975e9c6f31018" => :mojave
-    sha256 "c2a5846928bb13ccf83513f1db65ed79f8499d75cb51da655f8fb4f49ac787bd" => :high_sierra
-    sha256 "ff6a8d0b903ddbd716a33ee4669f80d12bcaa59eb11862614efaf92092c68d41" => :sierra
+    sha256 "ac579ec2df32c7cc06b8eed8763acc19ca60c2efcda202a9ed064f160a82d279" => :catalina
+    sha256 "414093834dd4f424b64131c8af489a4ce2154c104725fff666be34072f5622f0" => :mojave
+    sha256 "096f28e4f8b2bb81277ce390e6c4b207c3c97d63adf12b00d446d91eae096424" => :high_sierra
   end
 
   head do
