330413de7c49a1eefcf22a7bcb4926bfdb3da38d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 03:01:34 +0000

    cproto: update 4.7o bottle.

--- Formula/cproto.rb
+++ Formula/cproto.rb
@@ -7,6 +7,7 @@ class Cproto < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1bd13cfb0cbaf739a6108287a8b9fbe0489370d7306acead182a49ba63f7a562" => :catalina
     sha256 "10ca6eb5bb793309be3dc367b013b97f3ab199cccfc27b0fac2dbdfcb8b73a62" => :mojave
     sha256 "c3cb1dc57b52471d2ce88ee243dbc72bd96984ee23c6508f316fd037f283d144" => :high_sierra
     sha256 "371d43e22636bad41b4a37d7abd06bc42b504b0790a14a6c54f0b5d03b693cf3" => :sierra
