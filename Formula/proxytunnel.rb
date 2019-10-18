ae36a81143e80fd3717c6672112565f6dc3d1f8d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 06:38:50 +0000

    proxytunnel: update 1.9.1_1 bottle.

--- Formula/proxytunnel.rb
+++ Formula/proxytunnel.rb
@@ -7,6 +7,7 @@ class Proxytunnel < Formula
 
   bottle do
     cellar :any
+    sha256 "37f2fa40f5470d1af77b0290d0365677f926e995ddf97fb706c70cb0d362958d" => :catalina
     sha256 "3538bb076024d406670df496f8d40f5c9d17778f20964a0f9e97e35fac37ea8e" => :mojave
     sha256 "7963d82d5defc801687b5ec2b051c97ea2c765fd67ff784374421502159a4c7d" => :high_sierra
     sha256 "ce64fc5b482aed4ca7f1fc0c7bc3e1ebccae84dc50fb2659d6a560dca8cd7435" => :sierra
