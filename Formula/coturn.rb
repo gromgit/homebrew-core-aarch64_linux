cdfdf3584b7d9e0f01b81a3eac72c20e2aaa72d4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 02:56:34 +0000

    coturn: update 4.5.1.1_1 bottle.

--- Formula/coturn.rb
+++ Formula/coturn.rb
@@ -7,6 +7,7 @@ class Coturn < Formula
 
   bottle do
     rebuild 2
+    sha256 "72cdf60dab2ee94a4aa139d1bdbbe566a8c30d6e000f85527bf5a1562fe5f09d" => :catalina
     sha256 "3e61ac696114f38c9d6d361b1915b7a1fdae7bedf82c9bcc6bc9ed9a3dfdb986" => :mojave
     sha256 "3896e563b9c95d367303922cb19675f13e574d2bb99bad23cc098975d709a296" => :high_sierra
     sha256 "a39734b4c588d1d311ca1e6851ba7cefb0b0159c5932ad2a5440b0e82025e8f6" => :sierra
