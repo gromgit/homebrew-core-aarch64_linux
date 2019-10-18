f90d6c9b981df4c59bcfd452779b46b9e15344f9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 00:11:30 +0000

    camellia: update 2.7.0 bottle.

--- Formula/camellia.rb
+++ Formula/camellia.rb
@@ -6,6 +6,7 @@ class Camellia < Formula
 
   bottle do
     cellar :any
+    sha256 "c7d2e77a15331cebfeff928b67bd32ee5b0a9325ac5cbea022b2c6ddbe585ff6" => :catalina
     sha256 "347284dc085d1cd6acad286e8797ba3e001190e7cb04934b1f96d1e67481f302" => :mojave
     sha256 "fc8cb8a0f24226fd1f93b32192f290107d44283196e1edb48458b184597aa729" => :high_sierra
     sha256 "b4783ca8cf782a63d09daa1ff363c2fb4c4ea6dd4e75b8beb29167f536227730" => :sierra
