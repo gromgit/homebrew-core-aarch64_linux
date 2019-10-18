539ca67fa7308a0486b4c6a0f95bc514d73d0fd7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 01:55:01 +0000

    cmigemo: update 20110227 bottle.

--- Formula/cmigemo.rb
+++ Formula/cmigemo.rb
@@ -12,6 +12,7 @@ class Cmigemo < Formula
   end
   bottle do
     cellar :any
+    sha256 "81ea6aecbf5b3dec1ebc423d3503bd134d79f4fbfbb91b291e90c1b5a9fef1a4" => :catalina
     sha256 "28db47c1cedcff4dc6ee2d48bd07a147ae18f400e035e6a583d6b8e6cb36dfa1" => :mojave
     sha256 "a56e9422e30145d388649e9c85bf814adb58688c2c5e374385f4260b8daa049a" => :high_sierra
     sha256 "612544771bde1676044d35e8cb1f64134788580b76c59ced3b651e8996d46b51" => :sierra
