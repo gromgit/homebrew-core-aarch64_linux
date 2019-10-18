9c9b7cfd922c3e83c00e5d121731fc11466425d6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 00:29:53 +0000

    chipmunk: update 7.0.3 bottle.

--- Formula/chipmunk.rb
+++ Formula/chipmunk.rb
@@ -8,6 +8,7 @@ class Chipmunk < Formula
 
   bottle do
     cellar :any
+    sha256 "b71191c2c1e4859cb9d5e77b8684612dec1c191780a0b1d56afc04ada66da036" => :catalina
     sha256 "16292e5518bae60c6990a6f1565e1416f91ffe1c878ab43b58465bb2a24d3d11" => :mojave
     sha256 "5370b9d8db489d6b8944c23fd4906768c84d87e22f054ca3381c7ee527233f4d" => :high_sierra
     sha256 "c92a9c1134a272244ca3936b2c94431df7ed7002a9eec99f6914fe1128adae12" => :sierra
