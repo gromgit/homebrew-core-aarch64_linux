7786777ce39ff6b92d7da0b6e059c3eb620253f4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 18:36:38 +0000

    mp3unicode: update 1.2.1 bottle.

--- Formula/mp3unicode.rb
+++ Formula/mp3unicode.rb
@@ -6,6 +6,7 @@ class Mp3unicode < Formula
 
   bottle do
     cellar :any
+    sha256 "61f39a1605947240874a49624d9aff5aa848c3edcf24017c70f70fc1c7c04e2b" => :catalina
     sha256 "b0b4f5e1d3bcee44c469cd1948f173175b0826569503bad26d027f10a1ebb92e" => :mojave
     sha256 "5d288104d6bf3c0bdce26b509f29b49adba281ebcf1eb713a578298cec4b1305" => :high_sierra
     sha256 "4d8a82928bc851fc314a6c8f57a3897d6f75df65aad84e79b451783d217ebd1d" => :sierra
