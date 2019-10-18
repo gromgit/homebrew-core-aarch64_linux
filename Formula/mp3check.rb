27f6044321587c22be339e9ac06acdd61c0f38b2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 18:19:41 +0000

    mp3check: update 0.8.7 bottle.

--- Formula/mp3check.rb
+++ Formula/mp3check.rb
@@ -6,6 +6,7 @@ class Mp3check < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a98298c030d1ee1a28e2227ed41970fcad21d2af6486c471d045b07010ac232b" => :catalina
     sha256 "e19a17b2360f7a7974fe798cc68a12735155b14c68bb8c0d7a13439dd3fa5a29" => :mojave
     sha256 "99c5e5b8458a0cda5f50d92d858ccbd968f059a3b639130a3378c499331e427e" => :high_sierra
     sha256 "2846b7bd6201b58c40ce9b6193a929c5404fcbe77e97854876e53bba5c9d0d82" => :sierra
