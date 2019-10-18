82961b4e2fe7f1f8d3ba12e8d2ebab6d6c816cec
JounQin <admin@1stg.me>
2019-10-18 10:25:24 +0800

    pnpm 4.0.2 (new formula)
    Closes #45497.
    
    Signed-off-by: Rui Chen <rchen@meetup.com>

---
+++ Formula/pnpm.rb
@@ -0,0 +1,20 @@
+class Pnpm < Formula
+  require "language/node"
+
+  desc "📦🚀 Fast, disk space efficient package manager"
+  homepage "https://pnpm.js.org"
+  url "https://registry.npmjs.org/pnpm/-/pnpm-4.0.2.tgz"
+  sha256 "6f9b5faa2db1c2957e759c853e1e82bfb81fd7d7183858b8e794fb446640a988"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "#{bin}/pnpm", "init", "-y"
+    assert_predicate testpath/"package.json", :exist?, "package.json must exist"
+  end
+end
