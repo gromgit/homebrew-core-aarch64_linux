ddd0adc58b51cf87beede817d8ae4deb8f7f6b8d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 01:55:02 +0000

    cmark: update 0.29.0 bottle.

--- Formula/cmark.rb
+++ Formula/cmark.rb
@@ -6,6 +6,7 @@ class Cmark < Formula
 
   bottle do
     cellar :any
+    sha256 "d7bf29e64f91198441f39325653aa12ab7d4bbeb458a337146039717394cce2e" => :catalina
     sha256 "8687e5aeca18f2c952c5f475569ad90f077d49c3e45477cad0babd68b040a6cb" => :mojave
     sha256 "81e1fe130cae57abb515a8916140b3b2718f4f9ae778fe059d454d51cc24e1b3" => :high_sierra
     sha256 "ffe7ff1b15e9d7283253129feb78db9d9ccd72f39388a33331fbe8d0fb7445a4" => :sierra
