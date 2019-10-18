e2b09fe7a0a0faf6e3e5751d5017f2a110b8e418
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 16:48:10 +0000

    mk-configure: update 0.32.0 bottle.

--- Formula/mk-configure.rb
+++ Formula/mk-configure.rb
@@ -6,6 +6,7 @@ class MkConfigure < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "211fa03580498d11d9086e21fb9908b1c48d606487874aca7dc1876ca9d6c41c" => :catalina
     sha256 "3c6dde9e73c409690bc2e5dee76c0509c8ae0d7fd34bf155e3f63297f9af9e9b" => :mojave
     sha256 "3c6dde9e73c409690bc2e5dee76c0509c8ae0d7fd34bf155e3f63297f9af9e9b" => :high_sierra
     sha256 "f8a80216317e5d7e3ed3c412114b1ee58b2250a2c019bf99ca25686cb7d07547" => :sierra
