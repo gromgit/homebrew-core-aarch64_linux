81a31f7169323fe6c3ae72e72d40dbe4c6d3a4a0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-19 06:47:21 +0000

    source-to-image: update 1.2.0 bottle.

--- Formula/source-to-image.rb
+++ Formula/source-to-image.rb
@@ -8,10 +8,9 @@ class SourceToImage < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "055b98a4f618393b452fbfe1cdc9f991f6431a74a95c2e14f04720fcff7e7f4c" => :catalina
-    sha256 "179ed06aa8cee8c8c7c6b8658eed696359170116c2ac366dadfaf086d533b033" => :mojave
-    sha256 "a315100e844a70c78cf11eae24c70ff663f239fe79274da5b99223fbe6081a87" => :high_sierra
-    sha256 "02ffcf2849f2051b0a7355b626ca06b2b2560ce09f68fff64f6b7a79966c750b" => :sierra
+    sha256 "7865eec2bd488c5b09844e7c13244e9d19912f75f04f13dbda6b0b393077703b" => :catalina
+    sha256 "102e5173cec0a766173a5cafcf95ab35129c2906bfedad3155ab1fe78933ef2a" => :mojave
+    sha256 "33822b820f7febe068dc2dfd1fafd5502c18e0aadc9b4b2e91b9f095c6febbfe" => :high_sierra
   end
 
   depends_on "go" => :build
