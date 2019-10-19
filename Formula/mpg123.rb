343984278ec27fa1ac314b2fbe64d8c2ae35d99b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-19 17:19:12 +0000

    mpg123: update 1.25.12 bottle.

--- Formula/mpg123.rb
+++ Formula/mpg123.rb
@@ -5,10 +5,10 @@ class Mpg123 < Formula
   sha256 "1ffec7c9683dfb86ea9040d6a53d6ea819ecdda215df347f79def08f1fe731d1"
 
   bottle do
-    sha256 "7e26860cdc0e59aca6d177bf1eb2078fd84668741d33f0e3609a5c6e009b868a" => :catalina
-    sha256 "43477cb887e991b1ae0425f25f04028eb59525afb7b4238e9f2c75cf0d0f64fa" => :mojave
-    sha256 "abfccbe7e5e75449a097ca95deae82b04d93062d5dd370ad04cbbef656808201" => :high_sierra
-    sha256 "b640cb47e7116c278b63fdb37d03653317ceb4af83fc3778ae51646bdd786de0" => :sierra
+    rebuild 1
+    sha256 "4e7eb4254949eee9a6da8f931c5752dab58faecc468ece126d42d0cc6f923a98" => :catalina
+    sha256 "80e6dffee72a4f3537f71851c9c74f2a95e5a79a22ac708273c51843418655de" => :mojave
+    sha256 "3ac95fd32b8e60ab3e8980fc968b00d3e910fcc0832b2fd2c4a560b7be1b083a" => :high_sierra
   end
 
   def install
