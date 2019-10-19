ef6c8f71191bd6a5e557af22423b27f51866d0a3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-19 09:10:03 +0000

    buildapp: update 1.5.6_1 bottle.

--- Formula/buildapp.rb
+++ Formula/buildapp.rb
@@ -7,6 +7,7 @@ class Buildapp < Formula
   head "https://github.com/xach/buildapp.git"
 
   bottle do
+    sha256 "7954a9008618ee64fbf2b93b1b2f503e25e979efaf5f10afee916d455ac3e231" => :catalina
     sha256 "86fa0142bbde84222c65edec4f082b99d7fbd67c4767723025fd12c9f60b2db4" => :mojave
     sha256 "aa14506cc1ab8eb712c9b1fdb71af45c9041b327c9ecdf9d48541b652e3da3be" => :high_sierra
     sha256 "15e1be1e7928770abfdb801e67435b9d7c1bc36923bc6d9d5281d9ceab0d9abd" => :sierra
