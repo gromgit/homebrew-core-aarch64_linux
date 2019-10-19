824d15447e522aa53ff30c3bee366398f6e2ad27
Igor Kapkov <igasgeek@me.com>
2019-10-19 17:53:59 +1100

    devspace 4.1.1
    Closes #45548.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/devspace.rb
+++ Formula/devspace.rb
@@ -2,8 +2,8 @@ class Devspace < Formula
   desc "CLI helps develop/deploy/debug apps with Docker and k8s"
   homepage "https://devspace.cloud/docs"
   url "https://github.com/devspace-cloud/devspace.git",
-    :tag      => "v4.1.0",
-    :revision => "2bb491c6e471988a4e198dbd39ca9234fd3ea163"
+    :tag      => "v4.1.1",
+    :revision => "b578da60a565a4d90a252069446dbf9fcd1814bb"
 
   bottle do
     cellar :any_skip_relocation
