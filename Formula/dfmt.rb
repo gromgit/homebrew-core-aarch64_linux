edae40b845d5f153114a1881bd57cbddcc971f0b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-19 08:30:24 +0000

    dfmt: update 0.10.1 bottle.

--- Formula/dfmt.rb
+++ Formula/dfmt.rb
@@ -8,6 +8,7 @@ class Dfmt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "94595dfc8836d0daccf855a41c9e85aef9b123cb5a72a94323abec4bfb5e5155" => :catalina
     sha256 "3f27ea93b418c2a4c163e94f03f7aa504b2116ebc3cffa88eadd30e2075c257d" => :mojave
     sha256 "e4e2fccd219314a3797ae105c8b2a766cd5fda0da0fee2bd492e506d53f48d7a" => :high_sierra
     sha256 "a350f2837fed66d00df684f75f224da6c409ab35c12d6b125631409b1d25d0ab" => :sierra
