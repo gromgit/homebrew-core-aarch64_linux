0890ab7556b96fbf55459a276486c95812242e0b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-19 15:33:17 +0000

    sdlpop: update 1.20 bottle.

--- Formula/sdlpop.rb
+++ Formula/sdlpop.rb
@@ -6,10 +6,9 @@ class Sdlpop < Formula
 
   bottle do
     cellar :any
-    sha256 "686310fe7699a5e291ea7e05006b4210609c564af7fe5dc7e52bf61fae26400a" => :catalina
-    sha256 "b8f410ee151aefc11d905c7ccfff17cee6235988dfd7d2615984fd0ab40a532b" => :mojave
-    sha256 "759d1be2124d3cbd23e1364e950eb2a4fdb713da491119e61da269ca2f5c388d" => :high_sierra
-    sha256 "74f83f14a15a8f612e7669e29a6ea6d45ff3f3610a31184de02bee11cc32a498" => :sierra
+    sha256 "54fd6bcb8f351a98cd38017bb3c2a6df7a607dbd13b32cbd2162ab168a2a7759" => :catalina
+    sha256 "136ad2e3a3dfb37fa6e998ebcada2b35f1a41b8b31b4ac404038388bd2d6902b" => :mojave
+    sha256 "40e3329a4043ea4da4e71ccfbea3c9f710e9fed5d61e1d91a5390f7505f6c5f5" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
