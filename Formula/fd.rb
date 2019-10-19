733bae63d469ca2c8af7b5bfff96336c3b36e7e5
FX Coudert <fxcoudert@gmail.com>
2019-10-19 10:17:32 +0200

    fd: fix build for Catalina
    Closes #45557.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/fd.rb
+++ Formula/fd.rb
@@ -14,6 +14,10 @@ class Fd < Formula
 
   depends_on "rust" => :build
 
+  # Work around issue with rust/jemalloc on Catalina
+  # https://github.com/sharkdp/fd/issues/498
+  patch :DATA
+
   def install
     ENV["SHELL_COMPLETIONS_DIR"] = buildpath
     system "cargo", "install", "--root", prefix, "--path", "."
@@ -29,3 +33,32 @@ class Fd < Formula
     assert_equal "test_file", shell_output("#{bin}/fd test").chomp
   end
 end
+__END__
+diff -pur a/Cargo.toml b/Cargo.toml
+--- a/Cargo.toml	2019-09-15 19:29:15.000000000 +0200
++++ b/Cargo.toml	2019-10-19 10:14:25.000000000 +0200
+@@ -52,9 +52,6 @@ features = ["suggestions", "color", "wra
+ [target.'cfg(all(unix, not(target_os = "redox")))'.dependencies]
+ libc = "0.2"
+
+-[target.'cfg(all(not(windows), not(target_env = "musl")))'.dependencies]
+-jemallocator = "0.3.0"
+-
+ [dev-dependencies]
+ diff = "0.1"
+ tempdir = "0.3"
+diff -pur a/src/main.rs b/src/main.rs
+--- a/src/main.rs	2019-09-15 19:29:15.000000000 +0200
++++ b/src/main.rs	2019-10-19 10:14:39.000000000 +0200
+@@ -35,11 +35,6 @@ use crate::internal::{
+     pattern_has_uppercase_char, transform_args_with_exec, FileTypes,
+ };
+
+-// We use jemalloc for performance reasons, see https://github.com/sharkdp/fd/pull/481
+-#[cfg(all(not(windows), not(target_env = "musl")))]
+-#[global_allocator]
+-static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+-
+ fn main() {
+     let checked_args = transform_args_with_exec(env::args_os());
+     let matches = app::build_app().get_matches_from(checked_args);
