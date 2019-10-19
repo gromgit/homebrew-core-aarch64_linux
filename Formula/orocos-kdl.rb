781f4c87020c2c29d2b3b5d76c5be2c531cc9e68
Viktor Szakats <commit@vszakats.net>
2019-10-19 19:03:55 +0000

    orocos-kdl: secure homepage

--- Formula/orocos-kdl.rb
+++ Formula/orocos-kdl.rb
@@ -1,6 +1,6 @@
 class OrocosKdl < Formula
   desc "Orocos Kinematics and Dynamics C++ library"
-  homepage "http://www.orocos.org/kdl"
+  homepage "https://orocos.org/"
   url "https://github.com/orocos/orocos_kinematics_dynamics/archive/v1.4.0.tar.gz"
   sha256 "05b93e759923684dc07433ccae1e476d158d89b3c2be5079c20062406da7b4dd"
 
