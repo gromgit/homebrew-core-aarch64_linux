d4bf0df8e478d84ea49af18dfffbe8a418812635
Caleb Xu <calebcenter@live.com>
2019-10-19 11:05:30 -0400

    ski 6.13
    Closes #45573.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/ski.rb
+++ Formula/ski.rb
@@ -1,8 +1,8 @@
 class Ski < Formula
   desc "Evade the deadly Yeti on your jet-powered skis"
   homepage "http://catb.org/~esr/ski/"
-  url "http://www.catb.org/~esr/ski/ski-6.12.tar.gz"
-  sha256 "2f34f64868deb0cc773528c68d9829119fac359c44a704695214d87773df5a33"
+  url "http://www.catb.org/~esr/ski/ski-6.13.tar.gz"
+  sha256 "34e95547ecfe7b3791df0a81fef9af422ececf49b1aa1e93bbb1ba067ccdc955"
 
   bottle do
     cellar :any_skip_relocation
