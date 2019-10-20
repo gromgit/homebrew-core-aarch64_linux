450cbb797fe552081c69ed787e2589168e732861
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-20 06:09:04 +0000

    ttyplot: add 1.4 bottle.

--- Formula/ttyplot.rb
+++ Formula/ttyplot.rb
@@ -4,6 +4,13 @@ class Ttyplot < Formula
   url "https://github.com/tenox7/ttyplot/archive/1.4.tar.gz"
   sha256 "11974754981406d19cfa16865b59770faaf3ade8d909d9a0134dc56e00d29bd4"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "760b571d2cc940c6a1ad14655f2e2c0cac4aa64dea8e63d37454528cf969d8b2" => :catalina
+    sha256 "b31388536afde7ef669c334f520e73f95ceef82d0c9f73f5390a65d13d3235ef" => :mojave
+    sha256 "6d01769af5216ac128e8cb9a0a55397959594fa8f20fafcf65780db4b66ac090" => :high_sierra
+  end
+
   uses_from_macos "ncurses"
 
   def install
