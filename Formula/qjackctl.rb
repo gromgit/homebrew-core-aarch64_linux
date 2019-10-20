363e7eaed9a52e3d44d5e869ba17524b0eee9544
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-20 08:55:07 +0000

    qjackctl: update 0.5.3 bottle.

--- Formula/qjackctl.rb
+++ Formula/qjackctl.rb
@@ -6,6 +6,7 @@ class Qjackctl < Formula
   head "https://git.code.sf.net/p/qjackctl/code.git"
 
   bottle do
+    sha256 "4dda3e47401d1ce72a8213bdcd8f63391459e95fb81c83faa3151aa6ee09fa71" => :catalina
     sha256 "e661c47a8bb9be298dabc9d0c656f962b7157c6aa7dcebef18119af8ea07c26b" => :mojave
     sha256 "e23c9278a737692dffba9f15d4ba96ee3545d5054d4dc40b8f298fa2ac88d6d0" => :high_sierra
     sha256 "b5561ee222cd865048a52ea95c98f1a06b05d17ed31500a8f7e8407ef8caace2" => :sierra
