1d8d1f6006415edf589db1f050bbc24dd356ff96
commitay <commitay@users.noreply.github.com>
2019-10-20 16:53:48 +1000

    minikube 1.5.0 (new formula)
    Closes #45603.
    
    Signed-off-by: Rui Chen <rchen@meetup.com>

---
+++ Formula/minikube.rb
@@ -0,0 +1,36 @@
+class Minikube < Formula
+  desc "Run a Kubernetes cluster locally"
+  homepage "https://minikube.sigs.k8s.io/"
+  url "https://github.com/kubernetes/minikube.git",
+      :tag      => "v1.5.0",
+      :revision => "d1151d93385a70c5a03775e166e94067791fe2d9"
+  head "https://github.com/kubernetes/minikube.git"
+
+  depends_on "go" => :build
+  depends_on "go-bindata" => :build
+  depends_on "kubernetes-cli"
+
+  def install
+    system "make"
+    bin.install "out/minikube"
+
+    output = Utils.popen_read("#{bin}/minikube completion bash")
+    (bash_completion/"minikube").write output
+
+    output = Utils.popen_read("#{bin}/minikube completion zsh")
+    (zsh_completion/"_minikube").write output
+  end
+
+  test do
+    output = shell_output("#{bin}/minikube version")
+    assert_match "version: v#{version}", output
+
+    (testpath/".minikube/config/config.json").write <<~EOS
+      {
+        "vm-driver": "virtualbox"
+      }
+    EOS
+    output = shell_output("#{bin}/minikube config view")
+    assert_match "vm-driver: virtualbox", output
+  end
+end
