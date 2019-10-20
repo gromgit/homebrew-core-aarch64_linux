0c495e7524b99ef114cb3040683373e5a3df73c8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-20 17:33:50 +0000

    autogen: update 5.18.16 bottle.

--- Formula/autogen.rb
+++ Formula/autogen.rb
@@ -6,6 +6,7 @@ class Autogen < Formula
   sha256 "f8a13466b48faa3ba99fe17a069e71c9ab006d9b1cfabe699f8c60a47d5bb49a"
 
   bottle do
+    sha256 "e103302688bc9f7e4493c8827133f3341ab701740249196ddbfc2f2cf4fc1246" => :catalina
     sha256 "693b555483cf3f1e67516e45b31ad7718f041c97349ef655e28d55b1918b4e3f" => :mojave
     sha256 "7b79a5aa968c4d95660efb7a30bfb2c747dde2eefb1cf95efdc9fea7847b9151" => :high_sierra
     sha256 "19891a89ee7465e2690d34494e2ee41afee0dfda661a0c1c7407d283438e911e" => :sierra
