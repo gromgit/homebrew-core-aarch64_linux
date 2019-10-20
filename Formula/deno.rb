5576e7db729f17e2e78c5958c55bb3dd3eb64325
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-20 04:44:08 +0000

    deno: update 0.21.0 bottle.

--- Formula/deno.rb
+++ Formula/deno.rb
@@ -7,9 +7,9 @@ class Deno < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "fe1976eaad2a0190e5fad07a5bd11f2a80591b250a5a5bb5ca359f37ab50b9cf" => :catalina
-    sha256 "7761f40d5936d00f95e20cdfa3dc998883db1e16cd17223d2dcd3e3c21ac52cb" => :mojave
-    sha256 "25686f14ed244f5e95c02193e51bbc1c9a3506dcbcedcfb42ed82d8eb3a2d256" => :high_sierra
+    sha256 "756c9df9b5e32392768d7c7e3755d0dd98dbeb14079aef7e1af1c82d20c25b96" => :catalina
+    sha256 "f44bd18decbca28176aee495a58ee58f03181fec2768ec3f73f10968c62a4aad" => :mojave
+    sha256 "0601da71e421b3935f18e15e99adc19ac6c9c10146b10311b8202cd02b12088c" => :high_sierra
   end
 
   depends_on "llvm" => :build if DevelopmentTools.clang_build_version < 1100
