705a312d6b7d39f13007e253407bea95ad41e9b4
FX Coudert <fxcoudert@gmail.com>
2019-10-21 18:23:43 +0200

    udns: fix patch

--- Formula/udns.rb
+++ Formula/udns.rb
@@ -17,7 +17,10 @@ class Udns < Formula
 
   # Build target for dylib. See:
   # https://www.corpit.ru/pipermail/udns/2011q3/000154.html
-  patch :DATA
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/85fa66a9/udns/0.4.patch"
+    sha256 "4c3de5d04f93e7d7a9777b3baf3905707199fce9c08840712ccb2fb5fd6d90f9"
+  end
 
   def install
     system "./configure"
@@ -32,33 +35,3 @@ class Udns < Formula
     man3.install "udns.3"
   end
 end
-
-__END__
---- udns-0.4.orig/Makefile.in	2014-01-23 02:45:31.000000000 -0800
-+++ udns-0.4/Makefile.in	2014-08-16 20:22:00.000000000 -0700
-@@ -42,6 +42,11 @@
- SOLIBV = lib$(NAME).so.$(SOVER)
- SOLIBFL= -L. -l$(NAME)_s
- 
-+DYLIB   = lib$(NAME).dylib
-+DYLIBV = lib$(NAME).$(SOVER).dylib
-+
-+LIBS   = $(LIB) $(SOLIBV) $(DYLIB)
-+
- UTILS   = $(USRCS:.c=)
- UOBJS   = $(USRCS:.c=.o)
- SOUTILS = $(USRCS:.c=_s)
-@@ -71,6 +76,14 @@
- .c.o:
- 	$(CC) $(CFLAGS) $(CDEFS) -c $<
- 
-+dylib: $(DYLIB)
-+
-+$(DYLIBV): $(SOBJS)
-+	$(CC) -dynamiclib $(SOBJS) -o $(DYLIBV)
-+$(DYLIB): $(DYLIBV)
-+	rm -rf $@
-+	ln -s $(DYLIBV) $@
-+
- shared: $(SOLIBV) $(SOUTILS)
- sharedlib: $(SOLIBV)
