9212f63ff39670e5f5ae41bb9afd6abc9d80ecec
FX Coudert <fxcoudert@gmail.com>
2019-10-21 19:30:01 +0200

    cdargs: fix patch

--- Formula/cdargs.rb
+++ Formula/cdargs.rb
@@ -17,7 +17,10 @@ class Cdargs < Formula
 
   # fixes zsh usage using the patch provided at the cdargs homepage
   # (See https://www.skamphausen.de/cgi-bin/ska/CDargs)
-  patch :DATA
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/85fa66a9/cdargs/1.35.patch"
+    sha256 "adb4e73f6c5104432928cd7474a83901fe0f545f1910b51e4e81d67ecef80a96"
+  end
 
   def install
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
@@ -40,31 +43,3 @@ class Cdargs < Formula
     system "#{bin}/cdargs", "--version"
   end
 end
-
-__END__
-diff --git a/contrib/cdargs-bash.sh b/contrib/cdargs-bash.sh
-index 8a197ef..f3da067 100644
---- a/contrib/cdargs-bash.sh
-+++ b/contrib/cdargs-bash.sh
-@@ -11,6 +11,12 @@
- CDARGS_SORT=0   # set to 1 if you want mark to sort the list
- CDARGS_NODUPS=1 # set to 1 if you want mark to delete dups
- 
-+# Support ZSH via its BASH completion emulation
-+if [ -n "$ZSH_VERSION" ]; then
-+	autoload bashcompinit
-+	bashcompinit
-+fi
-+
- # --------------------------------------------- #
- # Run the cdargs program to get the target      #
- # directory to be used in the various context   #
-@@ -166,7 +172,7 @@ function mark ()
-     local tmpfile
- 
-     # first clear any bookmarks with this same alias, if file exists
--    if [[ "$CDARGS_NODUPS" && -e "$HOME/.cdargs" ]]; then
-+    if [ "$CDARGS_NODUPS" ] && [ -e "$HOME/.cdargs" ]; then
-         tmpfile=`echo ${TEMP:-${TMPDIR:-/tmp}} | sed -e "s/\\/$//"`
-         tmpfile=$tmpfile/cdargs.$USER.$$.$RANDOM
-         grep -v "^$1 " "$HOME/.cdargs" > $tmpfile && 'mv' -f $tmpfile "$HOME/.cdargs";
