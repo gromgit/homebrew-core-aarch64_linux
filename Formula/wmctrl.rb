a8be333df4990396ac5dc0d477a244b53be6fff4
FX Coudert <fxcoudert@gmail.com>
2019-10-21 18:19:59 +0200

    wmctrl: fix patch

--- Formula/wmctrl.rb
+++ Formula/wmctrl.rb
@@ -20,7 +20,10 @@ class Wmctrl < Formula
 
   # Fix for 64-bit arch. See:
   # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=362068
-  patch :DATA
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/85fa66a9/wmctrl/1.07.patch"
+    sha256 "8599f75e07cc45ed45384481117b0e0fa6932d1fce1cf2932bf7a7cf884979ee"
+  end
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
@@ -33,44 +36,3 @@ class Wmctrl < Formula
     system "#{bin}/wmctrl", "--version"
   end
 end
-
-__END__
---- wmctrl-1.07.orig/main.c
-+++ wmctrl-1.07/main.c
-@@ -1425,6 +1425,16 @@
-      *
-      * long_length = Specifies the length in 32-bit multiples of the
-      *               data to be retrieved.
-+     *
-+     * NOTE:  see
-+     * https://mail.gnome.org/archives/wm-spec-list/2003-March/msg00067.html
-+     * In particular:
-+     *
-+     * 	When the X window system was ported to 64-bit architectures, a
-+     * rather peculiar design decision was made. 32-bit quantities such
-+     * as Window IDs, atoms, etc, were kept as longs in the client side
-+     * APIs, even when long was changed to 64 bits.
-+     *
-      */
-     if (XGetWindowProperty(disp, win, xa_prop_name, 0, MAX_PROPERTY_VALUE_LEN / 4, False,
-             xa_prop_type, &xa_ret_type, &ret_format,
-@@ -1440,7 +1450,18 @@ static gchar *get_property (Display *disp, Window win, /*{{{*/
-     }
-
-     /* null terminate the result to make string handling easier */
--    tmp_size = (ret_format / 8) * ret_nitems;
-+    switch (ret_format) {
-+        case 8:
-+            tmp_size = sizeof(char) * ret_nitems;
-+            break;
-+        case 16:
-+            tmp_size = sizeof(short) * ret_nitems;
-+            break;
-+        case 32:
-+        default:
-+            tmp_size = sizeof(long) * ret_nitems;
-+            break;
-+    }
-     ret = g_malloc(tmp_size + 1);
-     memcpy(ret, ret_prop, tmp_size);
-     ret[tmp_size] = '\0';
