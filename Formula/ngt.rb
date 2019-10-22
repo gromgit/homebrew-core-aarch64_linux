83a9ebc1059e41053eb23cedf27c0579f1afe4e5
Andrew Kane <andrew@chartkick.com>
2019-10-22 15:23:49 -0700

    ngt 1.7.10 (new formula)
    Closes #45704.
    
    Signed-off-by: Rui Chen <rchen@meetup.com>

---
+++ Formula/ngt.rb
@@ -0,0 +1,23 @@
+class Ngt < Formula
+  desc "Neighborhood graph and tree for indexing high-dimensional data"
+  homepage "https://github.com/yahoojapan/NGT"
+  url "https://github.com/yahoojapan/NGT/archive/v1.7.10.tar.gz"
+  sha256 "c2a7fd25c1bb321f304d605c815261219bfe1ff2bce0a092e4193202a7b63a04"
+
+  depends_on "cmake" => :build
+  depends_on "libomp"
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      system "make", "install"
+    end
+    pkgshare.install "data"
+  end
+
+  test do
+    cp_r (pkgshare/"data"), testpath
+    system "#{bin}/ngt", "-d", "128", "-o", "c", "create", "index", "data/sift-dataset-5k.tsv"
+  end
+end
