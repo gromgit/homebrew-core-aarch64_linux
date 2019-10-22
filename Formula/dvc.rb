6c9825080deb9e3f77e1d48de21fd85a4c029ee7
Ruslan Kuprieiev <ruslan@iterative.ai>
2019-10-23 02:28:49 +0300

    dvc 0.63.4 (new formula)
    Closes #45709.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

---
+++ Formula/dvc.rb
@@ -0,0 +1,34 @@
+class Dvc < Formula
+  include Language::Python::Virtualenv
+
+  desc "Git for data science projects"
+  homepage "https://dvc.org"
+  url "https://github.com/iterative/dvc/archive/0.63.4.tar.gz"
+  sha256 "28b067fbfcddb1dbe76fd63c0862b7bcc6860273baea0fc352332b41e590cb8e"
+
+  depends_on "pkg-config" => :build
+  depends_on "openssl"
+  depends_on "python"
+
+  def install
+    venv = virtualenv_create(libexec, "python3")
+    system libexec/"bin/pip", "install", ".[all]"
+    # NOTE: dvc depends on asciimatics, which depends on Pillow, which
+    # uses liblcms2.2.dylib that causes troubles on mojave. See [1]
+    # and [2] for more info. As a workaround, we need to simply
+    # uninstall Pillow.
+    #
+    # [1] https://github.com/peterbrittain/asciimatics/issues/95
+    # [2] https://github.com/iterative/homebrew-dvc/issues/9
+    system libexec/"bin/pip", "uninstall", "-y", "Pillow"
+    system libexec/"bin/pip", "uninstall", "-y", "dvc"
+    venv.pip_install_and_link buildpath
+
+    bash_completion.install "scripts/completion/dvc.bash" => "dvc"
+    zsh_completion.install "scripts/completion/dvc.zsh"
+  end
+
+  test do
+    system "#{bin}/dvc", "--version"
+  end
+end
