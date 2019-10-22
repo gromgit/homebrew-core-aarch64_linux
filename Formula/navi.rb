e31f1b26f5806684283c8e37845a1685fc51392f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-22 19:57:53 +0000

    navi: add 0.14.2 bottle.

--- Formula/navi.rb
+++ Formula/navi.rb
@@ -4,6 +4,13 @@ class Navi < Formula
   url "https://github.com/denisidoro/navi/archive/v0.14.2.tar.gz"
   sha256 "1e16b8ff440882a2dd8e73bdb55c2b88724b3f0f9844c602ae9fe74f509d0dfb"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7e63e48d419cf40d0e37ad5b877a24fba25bbd0e7da35fd48648b6d039266363" => :catalina
+    sha256 "7e63e48d419cf40d0e37ad5b877a24fba25bbd0e7da35fd48648b6d039266363" => :mojave
+    sha256 "7e63e48d419cf40d0e37ad5b877a24fba25bbd0e7da35fd48648b6d039266363" => :high_sierra
+  end
+
   depends_on "fzf"
 
   def install
