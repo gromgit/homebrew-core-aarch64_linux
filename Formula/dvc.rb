24edb0edb659b57cdbe57f35ea62c9dbfe124609
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-24 05:45:02 +0000

    dvc: add 0.63.4 bottle.

--- Formula/dvc.rb
+++ Formula/dvc.rb
@@ -6,6 +6,12 @@ class Dvc < Formula
   url "https://github.com/iterative/dvc/archive/0.63.4.tar.gz"
   sha256 "28b067fbfcddb1dbe76fd63c0862b7bcc6860273baea0fc352332b41e590cb8e"
 
+  bottle do
+    sha256 "d5b1496b84052b732ed2fb33914ab13c860995e090d81a4b8659555e4b0cbc47" => :catalina
+    sha256 "a000fc47764b04f74c0f3795a9118987651065ceff81be1a7e53a1d707e3403d" => :mojave
+    sha256 "e7df3761c818f4bf63c531f9c2cb2d38b0fcaeaed1a9948a996d10a789e2f65f" => :high_sierra
+  end
+
   depends_on "pkg-config" => :build
   depends_on "openssl"
   depends_on "python"
