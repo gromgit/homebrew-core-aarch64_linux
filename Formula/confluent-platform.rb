1135feef30ba254f692bbeb9afea097b395f3634
Caleb Xu <calebcenter@live.com>
2019-10-25 10:17:01 -0400

    confluent-platform 5.3.1 (#45804)

--- Formula/confluent-platform.rb
+++ Formula/confluent-platform.rb
@@ -1,9 +1,9 @@
 class ConfluentPlatform < Formula
   desc "Developer-optimized distribution of Apache Kafka"
   homepage "https://www.confluent.io/product/confluent-platform/"
-  url "https://packages.confluent.io/archive/5.3/confluent-5.3.0-2.12.tar.gz"
-  version "5.3.0"
-  sha256 "b7e21e09fcbdd5f6695352b0e308ef276acd8d54041440e4f66545370ca69191"
+  url "https://packages.confluent.io/archive/5.3/confluent-5.3.1-2.12.tar.gz"
+  version "5.3.1"
+  sha256 "ed18fa2ec2ef00d34b52c9382dc92c9314cf06d2501ab022a62b5ea7ab1010dd"
 
   bottle :unneeded
 
@@ -19,7 +19,6 @@ class ConfluentPlatform < Formula
   end
 
   test do
-    system "#{bin}/kafka-broker-api-versions", "--version"
-    assert_match "5.3.0", shell_output("#{bin}/kafka-broker-api-versions --version")
+    assert_match version.to_s, shell_output("#{bin}/kafka-broker-api-versions --version")
   end
 end
