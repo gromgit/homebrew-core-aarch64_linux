281256fbde02a478020895da3e13759e78f237e3
Caleb Xu <calebcenter@live.com>
2019-10-25 10:43:18 -0400

    diffoscope 128
    Closes #45812.
    
    Signed-off-by: Rui Chen <rchen@meetup.com>

--- Formula/diffoscope.rb
+++ Formula/diffoscope.rb
@@ -1,8 +1,8 @@
 class Diffoscope < Formula
   desc "In-depth comparison of files, archives, and directories"
   homepage "https://diffoscope.org"
-  url "https://files.pythonhosted.org/packages/7e/19/99beff4108a14cbd097533b92ff0b4210735a2ab27109ef0018ca7153650/diffoscope-126.tar.gz"
-  sha256 "e255d22f2d3f5836e2f2df0eb2df22ffd7c85e18b1af6aef056f6fe153ba3783"
+  url "https://files.pythonhosted.org/packages/89/3f/e99b3a34f7b57b9680582936db229bba361e12b088a20cf82ebd3fafcd8c/diffoscope-128.tar.gz"
+  sha256 "b3baeb222b8ea81765c4b1e3db084da923e0f08f1a4d1207014abb4f0cd29568"
 
   bottle do
     cellar :any_skip_relocation
@@ -17,8 +17,8 @@ class Diffoscope < Formula
   depends_on "python"
 
   resource "libarchive-c" do
-    url "https://files.pythonhosted.org/packages/b9/2c/c975b3410e148dab00d14471784a743268614e21121e50e4e00b13f38370/libarchive-c-2.8.tar.gz"
-    sha256 "06d44d5b9520bdac93048c72b7ed66d11a6626da16d2086f9aad079674d8e061"
+    url "https://files.pythonhosted.org/packages/63/fe/9e6c78db381934e28c7ec3d30d4f209fe24442d17f1bd8c56d13ae185cf6/libarchive-c-2.9.tar.gz"
+    sha256 "9919344cec203f5db6596a29b5bc26b07ba9662925a05e24980b84709232ef60"
   end
 
   resource "progressbar" do
