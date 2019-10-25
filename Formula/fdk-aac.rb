ab87ee13cc0d6005011b15fe5dd46bb207f603e9
Caleb Xu <calebcenter@live.com>
2019-10-25 11:33:48 -0400

    fdk-aac 2.0.1
    Closes #45817.
    
    Signed-off-by: Rui Chen <rchen@meetup.com>

--- Formula/fdk-aac.rb
+++ Formula/fdk-aac.rb
@@ -1,8 +1,8 @@
 class FdkAac < Formula
   desc "Standalone library of the Fraunhofer FDK AAC code from Android"
   homepage "https://sourceforge.net/projects/opencore-amr/"
-  url "https://downloads.sourceforge.net/project/opencore-amr/fdk-aac/fdk-aac-2.0.0.tar.gz"
-  sha256 "f7d6e60f978ff1db952f7d5c3e96751816f5aef238ecf1d876972697b85fd96c"
+  url "https://downloads.sourceforge.net/project/opencore-amr/fdk-aac/fdk-aac-2.0.1.tar.gz"
+  sha256 "840133aa9412153894af03b27b03dde1188772442c316a4ce2a24ed70093f271"
 
   bottle do
     cellar :any
