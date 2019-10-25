040ffb75a6a1ffae71ce09551d56b8ce4e2519a6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-25 13:49:53 +0000

    calcurse: update 4.5.1 bottle.

--- Formula/calcurse.rb
+++ Formula/calcurse.rb
@@ -6,10 +6,9 @@ class Calcurse < Formula
   head "https://git.calcurse.org/calcurse.git"
 
   bottle do
-    sha256 "246b45c9315fe11ab9ef726684282e8fa18929e4c6a4bbb0e48a757754be8f77" => :catalina
-    sha256 "738613d3eef794062e61163897d5d7b66c7420221f386fa9614d44d6389d0dae" => :mojave
-    sha256 "b1334c12ada2d1aab070efa8d61c93cbc4f3e2a6ad1b5ad61513279c128782d4" => :high_sierra
-    sha256 "8da520260dc4b5612d5b9e2a83497e1227a0126195692b48d0842f971df904dd" => :sierra
+    sha256 "8e9e4538d02408549659f6eb7512a8f9fdd9d2c38369967bd5d9aba67519e7d9" => :catalina
+    sha256 "2b88932ea815161ce2a1d9af1d8e56612aea158255e991246b0da92e1fe11365" => :mojave
+    sha256 "9318cecc6785ff6ac34439793e88a904eebafd9c7fd84d543157ffb6a405cf18" => :high_sierra
   end
 
   depends_on "gettext"
