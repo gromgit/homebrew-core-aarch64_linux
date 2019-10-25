6a94558a4aefabc36d030d0a9e2b16b8d7e0ffb3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-25 17:17:57 +0000

    fdk-aac: update 2.0.1 bottle.

--- Formula/fdk-aac.rb
+++ Formula/fdk-aac.rb
@@ -6,10 +6,9 @@ class FdkAac < Formula
 
   bottle do
     cellar :any
-    sha256 "94d7e94ce45859829961e826330356922fac58b5eb91c38f652b5717a32cf92c" => :catalina
-    sha256 "f479a15b109a3cfacdc75ba6bb9564267761912f390cf3b92235f574fd4b69b1" => :mojave
-    sha256 "8f0e553452bdf944e6c13fb653fc603d2b0ef5d6bd259627b02d3c31ecf99c1b" => :high_sierra
-    sha256 "f398fc626dd0645aca5acb73974f7a93eec63a3957f1a0b16fd4de206ecd24e5" => :sierra
+    sha256 "526c83a79b7f208f07e8d04ad5ce47c8104d90a76034e42422d124fba128ba3c" => :catalina
+    sha256 "9353da38e4b43913964f9cdc5fc2a28c4b6c0a19ceef4e2a58db62ba3fdb4d49" => :mojave
+    sha256 "a8812e355f28272c7d8153bcda0e48a58c0bd0a880ed0256a2bc460143d1df78" => :high_sierra
   end
 
   head do
