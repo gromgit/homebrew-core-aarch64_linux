2334a7cc644f5a8a4a5df986088648093b0cf1eb
Juhani Simola <juhani.simola@iki.fi>
2019-10-26 16:22:17 +0300

    hfstospell 0.5.1

--- Formula/hfstospell.rb
+++ Formula/hfstospell.rb
@@ -1,9 +1,8 @@
 class Hfstospell < Formula
   desc "Helsinki Finite-State Technology ospell"
   homepage "https://hfst.github.io/"
-  url "https://github.com/hfst/hfst-ospell/releases/download/v0.5.0/hfstospell-0.5.0.tar.gz"
-  sha256 "0fd2ad367f8a694c60742deaee9fcf1225e4921dd75549ef0aceca671ddfe1cd"
-  revision 6
+  url "https://github.com/hfst/hfst-ospell/releases/download/v0.5.1/hfstospell-0.5.1.tar.gz"
+  sha256 "ccf5f3b06bcdc5636365e753b9f7fad9c11dfe483272061700a905b3d65ac750"
 
   bottle do
     cellar :any
@@ -16,24 +15,12 @@ class Hfstospell < Formula
   depends_on "pkg-config" => :build
   depends_on "icu4c"
   depends_on "libarchive"
-  depends_on "libxml++"
-  uses_from_macos "libxml2"
-
-  # Fix "error: no template named 'auto_ptr' in namespace 'std'"
-  # Upstream PR 20 Jun 2018 "C++14 (C++1y) should be the highest supported standard."
-  # See https://github.com/hfst/hfst-ospell/pull/41
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/674a62d/hfstospell/no-cxx17.diff"
-    sha256 "0a3146e871ac0e3c71248b8671d09f6d8a8a69713b6f4857eab7bdb684709083"
-  end
 
   def install
-    # icu4c 61.1 compatability
-    ENV.append "CPPFLAGS", "-DU_USING_ICU_NAMESPACE=1"
-
     ENV.cxx11
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
+                          "--without-libxmlpp",
                           "--prefix=#{prefix}"
     system "make", "install"
   end
