5b1db9a3475d0061118cab171242e8423a5d101f
Ruben Opdebeeck <ropdebee@vub.be>
2019-10-28 13:25:35 +0100

    mgba: revision for imagemagick

--- Formula/mgba.rb
+++ Formula/mgba.rb
@@ -3,6 +3,7 @@ class Mgba < Formula
   homepage "https://mgba.io/"
   url "https://github.com/mgba-emu/mgba/archive/0.7.3.tar.gz"
   sha256 "6d5e8ab6f87d3d9fa85af2543db838568dbdfcecd6797f8153f1b3a10b4a8bdd"
+  revision 1
   head "https://github.com/mgba-emu/mgba.git"
 
   bottle do
