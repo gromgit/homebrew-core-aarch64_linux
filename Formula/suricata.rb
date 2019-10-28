698318dd558c10be4c3286eb8d7ccc8b58b32094
EricFromCanada <enk3@outlook.com>
2019-10-28 13:29:18 -0400

    suricata 5.0.0
    Closes #45901.
    
    Signed-off-by: Rui Chen <rchen@meetup.com>

--- Formula/suricata.rb
+++ Formula/suricata.rb
@@ -1,9 +1,8 @@
 class Suricata < Formula
   desc "Network IDS, IPS, and security monitoring engine"
   homepage "https://suricata-ids.org/"
-  url "https://www.openinfosecfoundation.org/download/suricata-4.0.5.tar.gz"
-  sha256 "74dacb4359d57fbd3452e384eeeb1dd77b6ae00f02e9994ad5a7b461d5f4c6c2"
-  revision 2
+  url "https://www.openinfosecfoundation.org/download/suricata-5.0.0.tar.gz"
+  sha256 "6a3dcc427196927a5cdefd32c290fa352d6405e9bb6d3fe12c71f47d31d98a63"
 
   bottle do
     sha256 "0dc1eeaa07fa33c2072685ca3cae9ba4b1611598187519bed4fb14446c7f1653" => :catalina
@@ -13,22 +12,29 @@ class Suricata < Formula
   end
 
   depends_on "pkg-config" => :build
+  depends_on "rust" => :build
   depends_on "jansson"
   depends_on "libmagic"
   depends_on "libnet"
   depends_on "libyaml"
+  depends_on "lz4"
   depends_on "nspr"
   depends_on "nss"
   depends_on "pcre"
   depends_on "python"
 
   resource "argparse" do
-    url "https://files.pythonhosted.org/packages/source/a/argparse/argparse-1.4.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/18/dd/e617cfc3f6210ae183374cd9f6a26b20514bbb5a792af97949c5aacddf0f/argparse-1.4.0.tar.gz"
     sha256 "62b089a55be1d8949cd2bc7e0df0bddb9e028faefc8c32038cc84862aefdd6e4"
   end
 
+  resource "PyYAML" do
+    url "https://files.pythonhosted.org/packages/e3/e8/b3212641ee2718d556df0f23f78de8303f068fe29cdaa7a91018849582fe/PyYAML-5.1.2.tar.gz"
+    sha256 "01adf0b6c6f61bd11af6e10ca52b7d4057dd0be0343eb9283c878cf3af56aee4"
+  end
+
   resource "simplejson" do
-    url "https://files.pythonhosted.org/packages/source/s/simplejson/simplejson-3.16.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/e3/24/c35fb1c1c315fc0fffe61ea00d3f88e85469004713dab488dee4f35b0aff/simplejson-3.16.0.tar.gz"
     sha256 "b1f329139ba647a9548aa05fb95d046b4a677643070dc2afc05fa2e975d09ca5"
   end
 
@@ -42,8 +48,8 @@ class Suricata < Formula
     end
 
     jansson = Formula["jansson"]
-    libnet = Formula["libnet"]
     libmagic = Formula["libmagic"]
+    libnet = Formula["libnet"]
 
     args = %W[
       --disable-dependency-tracking
@@ -56,7 +62,8 @@ class Suricata < Formula
       --with-libmagic-includes=#{libmagic.opt_include}
       --with-libmagic-libraries=#{libmagic.opt_lib}
       --with-libnet-includes=#{libnet.opt_include}
-      --with-libnet-libs=#{libnet.opt_lib}
+      --with-libnet-libraries=#{libnet.opt_lib}
+      --enable-ipfw
     ]
 
     system "./configure", *args
