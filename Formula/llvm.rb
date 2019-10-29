f8309b877d61ab9acb31a59393d8150eac546cf2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-29 22:31:24 +0000

    llvm: update 9.0.0 bottle.

--- Formula/llvm.rb
+++ Formula/llvm.rb
@@ -54,10 +54,10 @@ class Llvm < Formula
 
   bottle do
     cellar :any
-    sha256 "16101306dcdea0f1f2c3f926c32e989501e95ba8afe16a4402c79c81241d7558" => :catalina
-    sha256 "ebc1c9a3dc80510c48d8886fd77b74d25a4c16727e5e575557c1e1540b9f4fd7" => :mojave
-    sha256 "05d5806f19f7e382f032f135bf21216f677f3912b3712adf379960673da7a110" => :high_sierra
-    sha256 "2439c1fb7cce8c4eab2960ef02ef3f7046834ef9b44e855c758b5578257dbac1" => :sierra
+    rebuild 1
+    sha256 "2e24b1c53c61457264bb413eed57b5953105f15f674b9c1378c50c8ad62faf0e" => :catalina
+    sha256 "38b3c05f35101dcfb5502a7212aa477fe0add93dc010ceb0f8b3c56bf1e3bfad" => :mojave
+    sha256 "e16c1d5a25f5fe0fc9a10f785d36930862a07daf1115f838f7d2e62bb53ddf90" => :high_sierra
   end
 
   # Clang cannot find system headers if Xcode CLT is not installed
