f7a4f47b942467181294a6abd51bd4f0fc771fee
Rui Chen <rchen@meetup.com>
2019-10-29 17:33:15 -0400

    kubernetes-helm 2.15.2
    Closes #45954.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/kubernetes-helm.rb
+++ Formula/kubernetes-helm.rb
@@ -2,8 +2,8 @@ class KubernetesHelm < Formula
   desc "The Kubernetes package manager"
   homepage "https://helm.sh/"
   url "https://github.com/helm/helm.git",
-      :tag      => "v2.15.1",
-      :revision => "cf1de4f8ba70eded310918a8af3a96bfe8e7683b"
+      :tag      => "v2.15.2",
+      :revision => "8dce272473e5f2a7bf58ce79bb5c3691db54c96b"
   head "https://github.com/helm/helm.git"
 
   bottle do
