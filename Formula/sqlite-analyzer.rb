31550c9562e2cfdf90b8f4ea7eba6d2aa19e179f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-30 21:44:50 +0000

    sqlite-analyzer: update 3.30.1 bottle.

--- Formula/sqlite-analyzer.rb
+++ Formula/sqlite-analyzer.rb
@@ -7,10 +7,9 @@ class SqliteAnalyzer < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "2ccdf2c56a73f41fed176042dbc9c8adbff0fcedef00fe94f73b6a1fc3269e52" => :catalina
-    sha256 "ad8628c564822e587747c49e7d7db2e10011af14077f47094662d29745a2d98b" => :mojave
-    sha256 "45710ca091f16bcb148d92f53b1aad0e049d0d49836529add96aadfe5ec53d6a" => :high_sierra
-    sha256 "0a7c045d97ffbb2e6f767c4ea55c1fa294cd4f1d7e6ae66ba126739e6cb0e223" => :sierra
+    sha256 "45c49ba7c7771948f57f8c3d2113442cc03b68fa35851dde6212db0c7964eb83" => :catalina
+    sha256 "c780dc644a7c5120228009cefbce2eb8fce057ff4922e38e5ffcf977f2f83965" => :mojave
+    sha256 "6ece6a38bf34ae3b1785a8834e75989e39bd5c777e5f0ab7be7cf09d412cfaed" => :high_sierra
   end
 
   def install
