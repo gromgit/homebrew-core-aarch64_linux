7533f185421f63d526f219bdfae2268f33ac4ef7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-30 21:44:49 +0000

    sqlite: update 3.30.1 bottle.

--- Formula/sqlite.rb
+++ Formula/sqlite.rb
@@ -7,10 +7,9 @@ class Sqlite < Formula
 
   bottle do
     cellar :any
-    sha256 "a7ca0667354f342c92cd0da87c80502327a79df859f951203d4e3f49dda4777d" => :catalina
-    sha256 "5f2f8f36a8d13733b0374ac39bdcd32dea10315e7442b9bb9942465487cb7811" => :mojave
-    sha256 "dcdc548263b6a8611d0e3532da5e216399cbd51e04277bb1ec9130fbb1125994" => :high_sierra
-    sha256 "59e5e8d4abca99d7f3162bf8079be9efadebf459b6b24eaaa8d0effba8bb3fd7" => :sierra
+    sha256 "38c39121f7634ec563bb201b483f66cf567dfe61e02624ffb06f620f11158ab1" => :catalina
+    sha256 "5e6fef2d754e0e4009d502c40ad1846ac46937886b5f3fe89378cb838626d95e" => :mojave
+    sha256 "5331999b520ce7e257a0e263ef0a6d0a1d3d40ce9a1d5759a10e0e21dcd001be" => :high_sierra
   end
 
   keg_only :provided_by_macos, "macOS provides an older sqlite3"
