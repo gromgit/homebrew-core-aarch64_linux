2bb4cbd64236cd4a1c34695c75d48cc4329aff90
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-30 07:02:06 +0000

    zita-convolver: update 4.0.3 bottle.

--- Formula/zita-convolver.rb
+++ Formula/zita-convolver.rb
@@ -6,11 +6,9 @@ class ZitaConvolver < Formula
 
   bottle do
     cellar :any
-    sha256 "8bddd7f214f291349d0600da5d6f9062091f2c8c0dd14d59833bce6f070f4e74" => :catalina
-    sha256 "affeedb6852f411c86d7219df7614a1d308e8d46c703cb2dc914188593a79076" => :mojave
-    sha256 "d65f312660d0ca92081a0d217a696a8e03a64f604f6924cd95b9e7a4956979f6" => :high_sierra
-    sha256 "39433124f4b7d8fa9b6eb20f40708ba9ded27322049b09178f3a3ef8e5ce5c1e" => :sierra
-    sha256 "d040044f83b7cd6d2bc2e4d8625a38c08820723744b9a653fc23fc9e3f33e5d2" => :el_capitan
+    sha256 "a616c118732c9f2c3775348e598a972abab7ae67b7cb0f283884cddaa55ce93d" => :catalina
+    sha256 "e9bfda6d2d3119f93ea0d570b9b3516d44513c3eafc206543f8fb055707db8fd" => :mojave
+    sha256 "b8b3326ead45ef0e126488d9c96a181f15888a11b707278c61c2ceeee312b37d" => :high_sierra
   end
 
   depends_on "fftw"
