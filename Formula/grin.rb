cad8f1519c18b3547ad757996ed3a4a4ddc65328
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-30 04:58:59 +0000

    grin: add 2.1.1 bottle.

--- Formula/grin.rb
+++ Formula/grin.rb
@@ -4,6 +4,13 @@ class Grin < Formula
   url "https://github.com/mimblewimble/grin/archive/v2.1.1.tar.gz"
   sha256 "189d45cb08fd19a60d0b1a7e592e527235a7db4da7cb6b0f017f36a643374e18"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5e47755a4c84467215c30717440af6de9e7dde7f303e4096966f9220769eb0dd" => :catalina
+    sha256 "c4f5424a3554bbdc6ee95761a58c18b16cbb8d8a8601d516d12d9ba5439e366a" => :mojave
+    sha256 "86a2b05e7b3a45b695d0556a50c18bcbe0cbd4197cbebc094280053f7a5dd28d" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
