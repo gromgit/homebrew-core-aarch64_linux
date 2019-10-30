9b4f4d4b15cc5cefffc23098a14d62dcddfb5b28
Eric Knibbe <enk3@outlook.com>
2019-10-30 16:43:32 -0400

    zsh-history-substring-search 1.0.2 (#45978)

--- Formula/zsh-history-substring-search.rb
+++ Formula/zsh-history-substring-search.rb
@@ -1,8 +1,8 @@
 class ZshHistorySubstringSearch < Formula
   desc "Zsh port of Fish shell's history search"
   homepage "https://github.com/zsh-users/zsh-history-substring-search"
-  url "https://github.com/zsh-users/zsh-history-substring-search/archive/v1.0.1.tar.gz"
-  sha256 "4de589fe54471f0c3449e74c8297b843ef57ce7d8c19d2cae4171a7d4021d85b"
+  url "https://github.com/zsh-users/zsh-history-substring-search/archive/v1.0.2.tar.gz"
+  sha256 "c1bb21490bd31273fb511b23000fb7caf49c258a79c4b8842f3e1f2ff76fd84c"
 
   bottle :unneeded
 
