8a654bb8adc8f9d096b635064f1ddc4c13baa6d1
EricFromCanada <enk3@outlook.com>
2019-10-30 01:14:05 -0400

    urweb 20190217
    Closes #45970.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/urweb.rb
+++ Formula/urweb.rb
@@ -1,8 +1,8 @@
 class Urweb < Formula
   desc "Ur/Web programming language"
   homepage "http://www.impredicative.com/ur/"
-  url "https://github.com/urweb/urweb/releases/download/20180616/urweb-20180616.tar.gz"
-  sha256 "211793601c3ba958f45b81c30035cf5e3c236650e23bdf4503dc6074fc143e12"
+  url "https://github.com/urweb/urweb/releases/download/20190217/urweb-20190217.tar.gz"
+  sha256 "da24e093369a14ae738dfb08d83fcba083ce07360023f6f55734f0e335e880b2"
 
   bottle do
     rebuild 1
@@ -16,6 +16,7 @@ class Urweb < Formula
   depends_on "libtool" => :build
   depends_on "mlton" => :build
   depends_on "gmp"
+  depends_on "icu4c"
   depends_on "openssl@1.1"
 
   def install
@@ -26,6 +27,8 @@ class Urweb < Formula
       --with-openssl=#{Formula["openssl@1.1"].opt_prefix}
       --prefix=#{prefix}
       SITELISP=$prefix/share/emacs/site-lisp/urweb
+      ICU_INCLUDES=-I#{Formula["icu4c"].opt_include}
+      ICU_LIBS=-L#{Formula["icu4c"].opt_lib}
     ]
 
     system "./configure", *args
