5934b7730dae42bbe46dc6ceebbe6f0c36a84a33
EricFromCanada <enk3@outlook.com>
2019-10-31 14:57:53 -0400

    ffmpeg: revision bump for x265

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -3,7 +3,7 @@ class Ffmpeg < Formula
   homepage "https://ffmpeg.org/"
   url "https://ffmpeg.org/releases/ffmpeg-4.2.1.tar.xz"
   sha256 "cec7c87e9b60d174509e263ac4011b522385fd0775292e1670ecc1180c9bb6d4"
-  revision 1
+  revision 2
   head "https://github.com/FFmpeg/FFmpeg.git"
 
   bottle do
