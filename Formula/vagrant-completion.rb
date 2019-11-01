5e6554d4da93438fa433f355ef1a7a39d35fdc11
Sean Molenaar <SMillerDev@users.noreply.github.com>
2019-11-01 13:42:28 +0100

    vagrant-completion 2.2.6 (#46060)

--- Formula/vagrant-completion.rb
+++ Formula/vagrant-completion.rb
@@ -1,8 +1,8 @@
 class VagrantCompletion < Formula
   desc "Bash completion for Vagrant"
   homepage "https://github.com/hashicorp/vagrant"
-  url "https://github.com/hashicorp/vagrant/archive/v2.2.5.tar.gz"
-  sha256 "0a228f5185b24b72efcc5a3924f86fa9fabab6f7562c3c63c1d9d239aa72a7b1"
+  url "https://github.com/hashicorp/vagrant/archive/v2.2.6.tar.gz"
+  sha256 "1bdafbce594cb17b9b4a7d85ba3dc35093d6693b1717280e062ec64ca2c05a5b"
   head "https://github.com/hashicorp/vagrant.git"
 
   bottle :unneeded
