7a23f7a54d0185daf2c726198d11951895c02aec
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-02 18:12:35 +0000

    oauth2l: add 1.0.1 bottle.

--- Formula/oauth2l.rb
+++ Formula/oauth2l.rb
@@ -5,6 +5,13 @@ class Oauth2l < Formula
   sha256 "8207bb796727019b277f63c709fa4e399f2d40c223633d0df6c74dc1f3c9385f"
   head "https://github.com/google/oauth2l.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "36b8c454a7d8934b7fa5ba65ee4132859ffd07452628c5869ae51beedf010f8c" => :catalina
+    sha256 "7983a17ea03db453152e87d1bf517aff15f4f3bced0bd483405e6e31dd6fd070" => :mojave
+    sha256 "d7d65ee29295d7740068d32b552327a8f3ceed0b3eb378f3d06afcfc03fcf796" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
