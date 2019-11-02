c10eca0c1112131667bcccbb4b1ba563ea1f28fd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-02 03:47:11 +0000

    pssh: update 2.3.1 bottle.

--- Formula/pssh.rb
+++ Formula/pssh.rb
@@ -7,10 +7,10 @@ class Pssh < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "37ba25ad7e68af9c2b5e855fbc6bac62df503b0f2c19a3c768ed753b8bdf9a66" => :catalina
-    sha256 "f629be84f06086697723f5c81bcdddb86fcf97712663062e909b8921c4ab6793" => :mojave
-    sha256 "f6defeaef5356d64010c322358bb1d66769352db4b1d61c1a47f123075cf288c" => :high_sierra
+    rebuild 2
+    sha256 "8f48ad9c3d6c59d77e50a85a940e9698482018140475035b274eee45567d5474" => :catalina
+    sha256 "fd5a9e13b00695332f468814d5bf2c823713cb7f91f423395996f5f65354f8d6" => :mojave
+    sha256 "73f994d5f4b9e8df301351b552108cdc2cf5a99c2899c8f5c929c9111b69187c" => :high_sierra
   end
 
   depends_on "python"
