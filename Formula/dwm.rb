2225d083c69994841a0378ef8e796f273bcb96e0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-02 08:38:05 +0000

    dwm: update 6.2_1 bottle.

--- Formula/dwm.rb
+++ Formula/dwm.rb
@@ -8,6 +8,7 @@ class Dwm < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e418a6853e7a7c270ccf6eb2d80c46986404acbd1b0d5a7fca008f111b6aab42" => :catalina
     sha256 "a749a913684983c05a9998ca73d55bd8f05197fcf6d7573336eacf7e168a5120" => :mojave
     sha256 "0c5e0fd48f4d52ab748d5301add02b7ca39d773c604e108d2eb81705cf80c315" => :high_sierra
     sha256 "20707f7d694eef615c63c98c055db728a6922e68ccf287ca63933715781311e6" => :sierra
