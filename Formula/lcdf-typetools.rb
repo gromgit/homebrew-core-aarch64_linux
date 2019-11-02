4e348daa8e76c1750a20803e716afd9362ce313b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-02 07:41:56 +0000

    lcdf-typetools: update 2.108 bottle.

--- Formula/lcdf-typetools.rb
+++ Formula/lcdf-typetools.rb
@@ -5,6 +5,7 @@ class LcdfTypetools < Formula
   sha256 "fb09bf45d98fa9ab104687e58d6e8a6727c53937e451603662338a490cbbcb26"
 
   bottle do
+    sha256 "e7ce2d4d16d2b79e482cb862231519653e6d3c09cd5e310573b04f804323e1e3" => :catalina
     sha256 "0fd983396dbcf027e560753e6f25797500d085762edcf59a1a2034cd55c24cfd" => :mojave
     sha256 "cdff1c16d03fd920033f85dd2e2180f91791057729fbd26b6f193ac7cd0ce9f4" => :high_sierra
     sha256 "2bfe28f9e869eec676cada56bcf6efe97024e0e1f93b126a7b26ac2a292db2af" => :sierra
