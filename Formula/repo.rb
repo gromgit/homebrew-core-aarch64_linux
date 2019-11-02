07f51bac2cd1c231cb18e6134c4786a3b44f44d2
Thierry Moisan <thierry.moisan@gmail.com>
2019-11-02 16:11:40 -0400

    repo 1.13.7.1 (#46127)

--- Formula/repo.rb
+++ Formula/repo.rb
@@ -2,8 +2,8 @@ class Repo < Formula
   desc "Repository tool for Android development"
   homepage "https://source.android.com/source/developing.html"
   url "https://gerrit.googlesource.com/git-repo.git",
-      :tag      => "v1.13.7",
-      :revision => "e778e57f11f208bd70c51d9cc57090a5cf9e41fa"
+      :tag      => "v1.13.7.1",
+      :revision => "b466854bed4348a210fec5870023c1a44cd830b5"
   version_scheme 1
 
   bottle :unneeded
