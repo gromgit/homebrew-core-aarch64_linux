a9b4ed091dca922d4b2aaad0d36128193fa62254
Rui Chen <rchen@meetup.com>
2019-11-02 21:59:56 -0400

    fastmod: add --locked flag to cargo install
    Closes #46190.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/fastmod.rb
+++ Formula/fastmod.rb
@@ -15,7 +15,7 @@ class Fastmod < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "--root", prefix, "--path", "."
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
 
   test do
