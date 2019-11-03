e9eebda51dee31ba9307a3a8f3a9152d93cdb9ef
Rui Chen <rchen@meetup.com>
2019-11-02 21:11:22 -0400

    rargs: add --locked flag to cargo install
    Closes #46167.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/rargs.rb
+++ Formula/rargs.rb
@@ -15,7 +15,7 @@ class Rargs < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "--root", prefix, "--path", "."
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
 
   test do
