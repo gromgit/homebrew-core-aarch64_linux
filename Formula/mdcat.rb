964c6d43d3fcd52edfd331054cde0eafd9907ee7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-03 02:25:05 +0000

    mdcat: update 0.13.0 bottle.

--- Formula/mdcat.rb
+++ Formula/mdcat.rb
@@ -6,10 +6,10 @@ class Mdcat < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c00019af1facfd5e20ba4c7911119e4afdcab7373f2bfb1f36c59d43bb2e57e6" => :catalina
-    sha256 "03a6092a5f1c29f6d603dcb70d65370df4c766636355c5ba4ad2eae5eb234c37" => :mojave
-    sha256 "8eaebefd87230851d0c79f6b40076225a5a6051c176cd44173b04fa785fe7464" => :high_sierra
-    sha256 "6f49eb3e6378bbcf315a0fece01f6eb67c2ba9166d60a6a2f15afe5efe1dabe9" => :sierra
+    rebuild 1
+    sha256 "9d1828626a16a9be233143eff8d8abe7d2257157b929d62c307a5c3557ea0dd8" => :catalina
+    sha256 "b7a0aa28deba05d9488d9717ba37a1da23ffff794ff7422332069d24f7731b13" => :mojave
+    sha256 "24466bc6fb718df66d2c61b9a53d446e0bace8678b2ebb13fadb7caf4fde4da7" => :high_sierra
   end
 
   depends_on "cmake" => :build
