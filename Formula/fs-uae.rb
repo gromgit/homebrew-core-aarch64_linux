5350a788afd3c0a99d3dff9cacfe7533858fe410
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-03 09:12:06 +0000

    fs-uae: update 3.0.0 bottle.

--- Formula/fs-uae.rb
+++ Formula/fs-uae.rb
@@ -6,6 +6,7 @@ class FsUae < Formula
 
   bottle do
     cellar :any
+    sha256 "99c0efd17141470c96e857a0e00696f2d0e6c901d7fa39a2efe30cf5112203ee" => :catalina
     sha256 "a520a09e6a703765acaf3829b901b86d6f6503ef2887f1c5495ba6f309443a23" => :mojave
     sha256 "ece2223f92dbe078b29dda91e691551d6788f8ea22fa7cc46919b125c4d62fb1" => :high_sierra
     sha256 "f4b193b58774d21e007cd83a564ee8d2efab0f99bb213d91424c56f472ae35ee" => :sierra
