ef173f9ebe3fd0e1f1b7cb1188a9b8642048e220
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-03 02:48:04 +0000

    miniserve: update 0.5.0_2 bottle.

--- Formula/miniserve.rb
+++ Formula/miniserve.rb
@@ -7,10 +7,10 @@ class Miniserve < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "eceff95e8228988815a3946130f3bdb62efc2a65213a9c2866f408755fda7910" => :catalina
-    sha256 "0191163688b41dbe508ecd78334a7c07ca008a56dd593d4388a295721c56bdc9" => :mojave
-    sha256 "8728bf2170cbc7f4c09800166c6017d6132ccabb59a3a7696d656852698acdb5" => :high_sierra
-    sha256 "c5807a45746f591af319ac13b183ba674e98b8f4c8d0e8c9858c0cb88c4192f5" => :sierra
+    rebuild 1
+    sha256 "82eca53aa5ca2e90b82bbce87ab964bae0e83b536ecf164a7f1c39a89571b855" => :catalina
+    sha256 "3b2d47cf800219c31be4767036e8ddfc4dc6bf67793a24f3a7bc83710414cc84" => :mojave
+    sha256 "076ced27fb5a0dccfcf1c6059675369360685ba2f939fd7fd70b775a33dfa863" => :high_sierra
   end
 
   # Miniserve requires a known-good Rust nightly release to use.
