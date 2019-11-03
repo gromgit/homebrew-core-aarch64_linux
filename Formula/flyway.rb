2edd58d9bd40fccb4f23b0fa560b7b580efb7140
Thierry Moisan <thierry.moisan@gmail.com>
2019-11-03 09:40:38 -0500

    flyway 6.0.8
    Closes #46228.
    
    Signed-off-by: Izaak Beekman <zbeekman@gmail.com>

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/6.0.7/flyway-commandline-6.0.7.tar.gz"
-  sha256 "c734793171fc42c475cd8e06cfba6de2196879a85c2fcff1dd341ef8c07d43b6"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/6.0.8/flyway-commandline-6.0.8.tar.gz"
+  sha256 "636d5ab5a3b226cad38b2e0bb56c0063b74a6f029ff887158948cbf962e46a2d"
 
   bottle :unneeded
 
