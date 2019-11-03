115e42f22d2e91b8c1bd7469a1dda8b033c30b87
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-03 02:46:22 +0000

    kyua: update 0.13_1 bottle.

--- Formula/kyua.rb
+++ Formula/kyua.rb
@@ -6,6 +6,7 @@ class Kyua < Formula
   revision 1
 
   bottle do
+    sha256 "181ef5174cbb2f2ab2dd0e011d597a1124bd0e359de33967d89730cd5d708529" => :catalina
     sha256 "a210c64c138b1656093b103ae27d9354972d7c2ec1e7220b0d94a9bc6806522c" => :mojave
     sha256 "ecf3850322d6a575b63519a21f6bfe7eb652c67564f7292f017306d468cbf49d" => :high_sierra
     sha256 "b533f71a13a6b8bdbbc4778515f5701774175879d6171804f43c1da8e12b4217" => :sierra
