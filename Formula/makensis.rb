9a5dd2244ad52ee6dfdbec8503be9677b6853a32
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-03 13:57:37 +0000

    makensis: update 3.04_1 bottle.

--- Formula/makensis.rb
+++ Formula/makensis.rb
@@ -7,6 +7,7 @@ class Makensis < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "71999e64695285eb7c1d6d84505980d6d14fcade0c41fafed821fcc348be70b4" => :catalina
     sha256 "e7cb0cf276e20c96b426188fa69b9a70aff58419747633682be8a957a4c6c166" => :mojave
     sha256 "c4cd3ba5be94d0c9788997dd9d686b7868519ba2c631e215bdc1eac1ecf63ed0" => :high_sierra
     sha256 "8f035781e4e926b8dcd367fbdc3a3a2bdd9b5fd96d268da62e9ac88ada495137" => :sierra
