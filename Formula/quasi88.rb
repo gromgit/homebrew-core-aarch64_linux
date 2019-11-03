535a3a76f80c765bdc87dab29c5a3081ae2ef106
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-03 07:53:49 +0000

    quasi88: update 0.6.4 bottle.

--- Formula/quasi88.rb
+++ Formula/quasi88.rb
@@ -6,6 +6,7 @@ class Quasi88 < Formula
 
   bottle do
     cellar :any
+    sha256 "2a1d1f01c210c06e49f3091dcebb2a30e62e14596e23bc43f349e151e3771d09" => :catalina
     sha256 "8b16ac77e4b8c6481fb7f518d5f7f446ff3b8b39465fa99d7bcbb8b28a3c745f" => :mojave
     sha256 "8199a69a8ecad4247752091f3eeaf5181eaa1dd0e4b2670059e21df807c646c6" => :high_sierra
     sha256 "d9ff4c5657c4179371d60317e0455cbadd59d45d81d0cc71d62d14d681619e95" => :sierra
