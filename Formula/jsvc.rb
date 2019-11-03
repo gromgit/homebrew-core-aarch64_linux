c6519dc37669b896e4a7dc0a7b1787417fa632ad
FX Coudert <fxcoudert@gmail.com>
2019-11-03 13:24:06 +0900

    jsvc 1.2.2
    Closes #46208.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/jsvc.rb
+++ Formula/jsvc.rb
@@ -1,8 +1,8 @@
 class Jsvc < Formula
   desc "Wrapper to launch Java applications as daemons"
   homepage "https://commons.apache.org/daemon/jsvc.html"
-  url "https://www.apache.org/dyn/closer.cgi?path=commons/daemon/source/commons-daemon-1.1.0-native-src.tar.gz"
-  sha256 "11962bc602619fd2eeb840f74a8c63cc1055221f0cc385a1fa906e758d39888d"
+  url "https://www.apache.org/dyn/closer.cgi?path=commons/daemon/source/commons-daemon-1.2.2-src.tar.gz"
+  sha256 "ebd9d50989ee2009cc83f501e6793ad5978672ecea97be5198135a081a8aac71"
 
   bottle do
     cellar :any_skip_relocation
@@ -21,10 +21,17 @@ class Jsvc < Formula
 
     prefix.install %w[NOTICE.txt LICENSE.txt RELEASE-NOTES.txt]
 
-    cd "unix"
-    system "./configure", "--with-java=/System/Library/Frameworks/JavaVM.framework",
-                          "--with-os-type=Headers"
-    system "make"
-    bin.install "jsvc"
+    cd "src/native/unix" do
+      system "./configure", "--with-java=#{ENV["JAVA_HOME"]}"
+      system "make"
+
+      libexec.install "jsvc"
+      (bin/"jsvc").write_env_script libexec/"jsvc", Language::Java.java_home_env("1.8+")
+    end
+  end
+
+  test do
+    output = shell_output("#{bin}/jsvc -help")
+    assert_match "jsvc (Apache Commons Daemon)", output
   end
 end
