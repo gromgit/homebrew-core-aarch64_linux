e8b1b6387c26e32135203e8e03ed9d43bda41882
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-03 09:07:45 +0000

    yq: update 2.4.1 bottle.

--- Formula/yq.rb
+++ Formula/yq.rb
@@ -6,10 +6,9 @@ class Yq < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ebdc4caf081175e62de1abb29bbaa25f82db759ed553d4db49f9144878c2fde6" => :catalina
-    sha256 "04f9e504ff627ee5c24a2051122c68bbb2cabf66d6ea2be4c6d161613e38d2c0" => :mojave
-    sha256 "691172310ea3a78373640dcb21d2d9f8615d337f7314e78627fbda47dd59f47c" => :high_sierra
-    sha256 "8ea51e4fd94aada42606ed05feed0475d87d50d301a2144860d8c85f4220c2d1" => :sierra
+    sha256 "7eef86b614ab9695cb8ba108a659cba5cd504211b7236f8a561c7619aaf9b792" => :catalina
+    sha256 "5bf8a07349fcb306261676187d53d684cd14cfafead1a120a95a6318bb8beda9" => :mojave
+    sha256 "750248b8af5a72505593466f7add52fd6e20a2c2556dd7e28fc756420618680d" => :high_sierra
   end
 
   depends_on "go" => :build
