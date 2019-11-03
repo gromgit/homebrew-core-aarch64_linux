a45b186f5d00b5c3c32aad5001ad07e35af804da
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-03 01:42:36 +0000

    ssh-permit-a38: update 0.2.0_1 bottle.

--- Formula/ssh-permit-a38.rb
+++ Formula/ssh-permit-a38.rb
@@ -7,10 +7,10 @@ class SshPermitA38 < Formula
 
   bottle do
     cellar :any
-    sha256 "4de4d364ee99fdaa45d77d37c26dab88ccd70233172687722e269268c8a411da" => :catalina
-    sha256 "ae34a5a46ef16528db572b93ed733a17f001bdbe23bcd07b6410c958e04d5186" => :mojave
-    sha256 "4de9caa719a5c0a569fbb09fee2df9a0db5df4eaad8046cb05ad8258a641f419" => :high_sierra
-    sha256 "31bfaaee0093524bbc33a809cac2ec4f6ff66d73aea13a9e4a3ae3a8c71b0fcd" => :sierra
+    rebuild 1
+    sha256 "3eefd64fbbe3e4d500a69352091da85ca685a435094facc30e6942d9d5e89a1d" => :catalina
+    sha256 "683ebbe9a6a845802f825f1775e6d861387be41fd520b648275f97a580e92398" => :mojave
+    sha256 "7d82d59932bb6d721a31726efc231d043d54d180995d0119d8f8bf9fc37f3e9b" => :high_sierra
   end
 
   depends_on "cmake" => :build
