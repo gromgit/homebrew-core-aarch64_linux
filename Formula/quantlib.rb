068d6804241a70b3bf0b2651e73a4f286d5cc759
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-03 08:31:23 +0000

    quantlib: update 1.16 bottle.

--- Formula/quantlib.rb
+++ Formula/quantlib.rb
@@ -6,6 +6,7 @@ class Quantlib < Formula
 
   bottle do
     cellar :any
+    sha256 "2c0d5709ac2d0150d9a80e98037e065fb427754c8a5ffc73657b436e1833079e" => :catalina
     sha256 "39702a332dbff419f3d34c12a19ee378a369e8a20400d4a35b16370916c2e22f" => :mojave
     sha256 "c39c206cb0c0b96b6dd218a50020c683095362216c06ad009fea4210a53a0b0c" => :high_sierra
     sha256 "d7589436bfcb0ce268bb25db660500440cb299fd751e32bfbc21485e54b57a98" => :sierra
