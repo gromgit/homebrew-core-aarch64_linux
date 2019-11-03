97ecd80bed2eef2d678d7a4a7276b18f88a3ff7a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-03 14:07:00 +0000

    gtk-gnutella: update 1.1.15 bottle.

--- Formula/gtk-gnutella.rb
+++ Formula/gtk-gnutella.rb
@@ -5,9 +5,9 @@ class GtkGnutella < Formula
   sha256 "2931fab394b6d36c14a6bbc3b0ff584af5550f2eeb03f78bb19743c47767c1b7"
 
   bottle do
-    sha256 "1414404b1a91d09a17d64f12382d4a0ce71a06081322eb27afb05ab9fa58f07e" => :mojave
-    sha256 "42bbb5ab94942025676daee8c9be4b140eb1a324e96330f992bfdf7473db8eec" => :high_sierra
-    sha256 "61f53035fe11324c64b19410dca984bedfea8874dcf931422aa4c6c66b437b0b" => :sierra
+    sha256 "9fb121eec5c3e0f3e8750464148488870056596b59068e5765991c3851e85316" => :catalina
+    sha256 "1ab6b18c2ec8cd720a7024e471ceba5b9c8397c8c589b4eebbb371a2dc390f24" => :mojave
+    sha256 "dc0cab460a17691ee852300b90f014eeaa7871c59859cf92e887aa882b032ed0" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
