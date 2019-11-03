0898f7651759f591d3f7fe8f63197f74611d7ec4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-03 09:33:31 +0000

    gpredict: update 2.2.1_2 bottle.

--- Formula/gpredict.rb
+++ Formula/gpredict.rb
@@ -6,6 +6,7 @@ class Gpredict < Formula
   revision 2
 
   bottle do
+    sha256 "99fff9473dcc5eaa0c58cf0b2bf04f4240e1598aada45565e4dbbf050d2ac7dc" => :catalina
     sha256 "952941a2ecdb5f75805888dfd020acce48c4f1b29a9c2e3ec8742d35fcd9c829" => :mojave
     sha256 "189249444c490bc7984506a3d041de1d057fff671ff774871f549f6b32efa042" => :high_sierra
     sha256 "9a0a4b0e63b3b1f84830f508d60ee3fc5b5fd0b9a5731241873168baa88209cf" => :sierra
