58054c47f74f1af2f341a73afd5ebb1787499324
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-03 07:59:26 +0000

    quilt: update 0.66 bottle.

--- Formula/quilt.rb
+++ Formula/quilt.rb
@@ -7,6 +7,7 @@ class Quilt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b7304ce0125f31f6d38f8645d436de553f765b599e4c66f8f659478963bf33f6" => :catalina
     sha256 "5d7f412108ec8831b8b6bfbc8e41d8b577523ffd66f9d095853a4680ec23b04f" => :mojave
     sha256 "691a01a091194910f0848aea529b331559fb98d44e9821c1ebafba51d2a2d62c" => :high_sierra
     sha256 "2305addd5b8f4b256701b2ec89ec9caffa4699dae48e63f8cac0478545b5d860" => :sierra
