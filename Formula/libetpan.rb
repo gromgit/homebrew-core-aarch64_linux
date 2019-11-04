8702ab89d88f81f19b421eae323c30ba3da0c479
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-04 22:42:10 +0000

    libetpan: update 1.9.4 bottle.

--- Formula/libetpan.rb
+++ Formula/libetpan.rb
@@ -7,10 +7,9 @@ class Libetpan < Formula
 
   bottle do
     cellar :any
-    sha256 "4f9b9ec25e82d190b2f1b488a8309918eb387d92c1112c72affb1d9a2e8bed41" => :catalina
-    sha256 "f9c8629d0d2282ffa40ab63cb18efafc8f1eced93fa34330c23c8a5aa7077e1b" => :mojave
-    sha256 "1c987f8bcdd60768be72e0b724050a6d518f472e6cd10f15a31898415ff2f254" => :high_sierra
-    sha256 "9bc63c0c6302a29e1fe513d179029fe9d70984b043e0940c31073c114fd09199" => :sierra
+    sha256 "2effe5528f31ea1edcdd0baf468bb1ebbfb0061cb8bf131f2636b5db6cc20550" => :catalina
+    sha256 "ba4948b8f0169ee43ba18b0dbea0564bfd5a2c625834f6f5a5c4b9ac1d725334" => :mojave
+    sha256 "6a2f29f42a39d9d3eee7bca1974118fdd8d44a745f61af686aa40c449157b733" => :high_sierra
   end
 
   depends_on :xcode => :build
