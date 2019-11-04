968078d4a52fa80f8c3fe243e7173a183f588b06
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-04 10:45:05 +0000

    calceph: add 3.4.0 bottle.

--- Formula/calceph.rb
+++ Formula/calceph.rb
@@ -4,6 +4,13 @@ class Calceph < Formula
   url "https://www.imcce.fr/content/medias/recherche/equipes/asd/calceph/calceph-3.4.0.tar.gz"
   sha256 "f176f8356d6a3514c60956386260986ee3bc98f5111b1e2bcc83581bbed20f1d"
 
+  bottle do
+    cellar :any
+    sha256 "3729f0d6eeb2fc4fd5ad04357c252fa12537b750679c1f5bc9077cfaa2ee2dba" => :catalina
+    sha256 "32438c3c780e8f5c463273b2a10f396136f5841208f52337996e3cbdca5bdc88" => :mojave
+    sha256 "8c739a44a8ea195b8e352bc6915fa086c77a078d6f98095f3d442d198af98c68" => :high_sierra
+  end
+
   depends_on "gcc" # for gfortran
 
   def install
