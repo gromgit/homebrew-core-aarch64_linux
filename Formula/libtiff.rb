e425ea48f97848cccff28666854e0ebac6678c68
Sam Ford <1584702+samford@users.noreply.github.com>
2019-11-04 09:17:13 -0500

    libtiff 4.1.0
    Closes #46273.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libtiff.rb
+++ Formula/libtiff.rb
@@ -1,10 +1,9 @@
 class Libtiff < Formula
   desc "TIFF library and utilities"
   homepage "https://libtiff.gitlab.io/libtiff/"
-  url "https://download.osgeo.org/libtiff/tiff-4.0.10.tar.gz"
-  mirror "https://fossies.org/linux/misc/tiff-4.0.10.tar.gz"
-  sha256 "2c52d11ccaf767457db0c46795d9c7d1a8d8f76f68b0b800a3dfe45786b996e4"
-  revision 1
+  url "https://download.osgeo.org/libtiff/tiff-4.1.0.tar.gz"
+  mirror "https://fossies.org/linux/misc/tiff-4.1.0.tar.gz"
+  sha256 "5d29f32517dadb6dbcd1255ea5bbc93a2b54b94fbf83653b4d65c7d6775b8634"
 
   bottle do
     cellar :any
@@ -15,15 +14,6 @@ class Libtiff < Formula
   end
 
   depends_on "jpeg"
-
-  # Patches are taken from latest Fedora package, which is currently
-  # libtiff-4.0.10-2.fc30.src.rpm and whose changelog is available at
-  # https://apps.fedoraproject.org/packages/libtiff/changelog/
-
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/d15e00544e7df009b5ad34f3b65351fc249092c0/libtiff/libtiff-CVE-2019-6128.patch"
-    sha256 "dbec51f5bec722905288871e3d8aa3c41059a1ba322c1ac42ddc8d62646abc66"
-  end
 
   def install
     args = %W[
