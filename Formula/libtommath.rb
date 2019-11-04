c0017cb9a98176e27916ce8c22d577946a7818a0
Caleb Xu <calebcenter@live.com>
2019-11-04 17:00:30 -0500

    libtommath 1.2.0
    Closes #46291.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libtommath.rb
+++ Formula/libtommath.rb
@@ -1,8 +1,8 @@
 class Libtommath < Formula
   desc "C library for number theoretic multiple-precision integers"
   homepage "https://www.libtom.net/LibTomMath/"
-  url "https://github.com/libtom/libtommath/releases/download/v1.1.0/ltm-1.1.0.tar.xz"
-  sha256 "90466c88783d1fe9f5c2364a69f5479f10d73ed616011be6196f35f7f1537ead"
+  url "https://github.com/libtom/libtommath/releases/download/v1.2.0/ltm-1.2.0.tar.xz"
+  sha256 "b7c75eecf680219484055fcedd686064409254ae44bc31a96c5032843c0e18b1"
   head "https://github.com/libtom/libtommath.git"
 
   bottle do
@@ -14,6 +14,9 @@ class Libtommath < Formula
 
   def install
     ENV["DESTDIR"] = prefix
+
+    # Work around Xcode 11 clang bug
+    ENV.append_to_cflags "-fno-stack-check" if DevelopmentTools.clang_build_version >= 1010
 
     system "make"
     system "make", "test_standalone"
