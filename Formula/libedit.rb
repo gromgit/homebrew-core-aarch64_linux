f0288e89abec28df21936570c09584bd0e470fcc
Caleb Xu <calebcenter@live.com>
2019-11-04 16:43:02 -0500

    libedit 20191025-3.1
    Closes #46286.
    
    Signed-off-by: Rui Chen <rchen@meetup.com>

--- Formula/libedit.rb
+++ Formula/libedit.rb
@@ -1,9 +1,9 @@
 class Libedit < Formula
   desc "BSD-style licensed readline alternative"
   homepage "https://thrysoee.dk/editline/"
-  url "https://thrysoee.dk/editline/libedit-20190324-3.1.tar.gz"
-  version "20190324-3.1"
-  sha256 "ac8f0f51c1cf65492e4d1e3ed2be360bda41e54633444666422fbf393bba1bae"
+  url "https://thrysoee.dk/editline/libedit-20191025-3.1.tar.gz"
+  version "20191025-3.1"
+  sha256 "6dff036660d478bfaa14e407fc5de26d22da1087118c897b1a3ad2e90cb7bf39"
 
   bottle do
     cellar :any
