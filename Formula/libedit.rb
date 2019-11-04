83064918eff3c0d6072245bf7f08975f636d33cc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-04 22:11:06 +0000

    libedit: update 20191025-3.1 bottle.

--- Formula/libedit.rb
+++ Formula/libedit.rb
@@ -7,10 +7,9 @@ class Libedit < Formula
 
   bottle do
     cellar :any
-    sha256 "46e6433799898fe243615048d7af2edc137a817d02d27a11895e9f4b0981324b" => :catalina
-    sha256 "c445caf1a4f1d47555f00505553b479c1638eb59e63ce7bcbd9cc03fd4b15e14" => :mojave
-    sha256 "0b624516c27f8e3298eff0f2dbfd2e108bf6d428c6d7c057914665b26154b366" => :high_sierra
-    sha256 "8ad985a00377f40928cb2653675da86542678baf2b625eb9861638b4c31878a8" => :sierra
+    sha256 "c7cc54cea12647c6f0fe804e3e85768e3a861d170789bfe53c07cde186dfcdd8" => :catalina
+    sha256 "bb80053d3becbb175049265573c5905641ba2f1fe4db357f7a168d14da489925" => :mojave
+    sha256 "14505d667f2efb073f9b5258e3ab5a7ca8bfd3dcf611c0ed151005fa1221bca8" => :high_sierra
   end
 
   keg_only :provided_by_macos
