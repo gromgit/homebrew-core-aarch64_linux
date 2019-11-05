c64ddfb434a3fa687a3439471c09e309247266c6
chenrui <rchen@meetup.com>
2019-11-05 09:08:00 -0500

    nifi 1.10.0 (#46323)

--- Formula/nifi.rb
+++ Formula/nifi.rb
@@ -1,8 +1,8 @@
 class Nifi < Formula
   desc "Easy to use, powerful, and reliable system to process and distribute data"
   homepage "https://nifi.apache.org"
-  url "https://www.apache.org/dyn/closer.cgi\?path\=/nifi/1.9.2/nifi-1.9.2-bin.tar.gz"
-  sha256 "8d50bd1a0408878ab41a5fcd18d179e5a293e7d0280ddbe5098a4627aaacbff1"
+  url "https://www.apache.org/dyn/closer.cgi?path=/nifi/1.10.0/nifi-1.10.0-bin.tar.gz"
+  sha256 "fd4f0750d18137bb1c21cd0fd5ab8951ccd450e6f673b8988db93ea2ff408288"
 
   bottle :unneeded
 
