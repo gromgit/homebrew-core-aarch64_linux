2a367ae6a42261a3e910c646e3b8dedd7ea8a834
Caleb Xu <calebcenter@live.com>
2019-11-05 16:49:33 -0500

    parallel 20191022
    Closes #46358.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/parallel.rb
+++ Formula/parallel.rb
@@ -1,9 +1,9 @@
 class Parallel < Formula
   desc "Shell command parallelization utility"
   homepage "https://savannah.gnu.org/projects/parallel/"
-  url "https://ftp.gnu.org/gnu/parallel/parallel-20190922.tar.bz2"
-  mirror "https://ftpmirror.gnu.org/parallel/parallel-20190922.tar.bz2"
-  sha256 "282fee2cb40a54ba32a922446365b782c4f461f848432b969de5a17b33193c63"
+  url "https://ftp.gnu.org/gnu/parallel/parallel-20191022.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/parallel/parallel-20191022.tar.bz2"
+  sha256 "641beea4fb9afccb1969ac0fb43ebc458f375ceb6f7e24970a9aced463e909a9"
   head "https://git.savannah.gnu.org/git/parallel.git"
 
   bottle do
