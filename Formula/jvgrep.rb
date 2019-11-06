eea470f6cfd9cb47d121020a05f3047025d67046
Rui Chen <chenrui333@gmail.com>
2019-11-06 07:41:39 -0500

    jvgrep 5.8.2
    fix
    
    Closes #46396.
    
    Signed-off-by: Rui Chen <rchen@meetup.com>

--- Formula/jvgrep.rb
+++ Formula/jvgrep.rb
@@ -1,10 +1,8 @@
-require "language/go"
-
 class Jvgrep < Formula
   desc "Grep for Japanese users of Vim"
   homepage "https://github.com/mattn/jvgrep"
-  url "https://github.com/mattn/jvgrep/archive/v5.8.1.tar.gz"
-  sha256 "128cca9ab2fbf3451560558f990b9bf821981dddaa1d47026bd4e71a3d07f25b"
+  url "https://github.com/mattn/jvgrep/archive/v5.8.2.tar.gz"
+  sha256 "0805e2e663a3d9702e80d12b5e9b54bafbecace08604cbd05e2121da30aaca17"
   head "https://github.com/mattn/jvgrep.git"
 
   bottle do
@@ -17,32 +15,16 @@ class Jvgrep < Formula
 
   depends_on "go" => :build
 
-  go_resource "github.com/mattn/go-colorable" do
-    url "https://github.com/mattn/go-colorable.git",
-        :revision => "efa589957cd060542a26d2dd7832fd6a6c6c3ade"
-  end
-
-  go_resource "github.com/mattn/go-isatty" do
-    url "https://github.com/mattn/go-isatty.git",
-        :revision => "6ca4dbf54d38eea1a992b3c722a76a5d1c4cb25c"
-  end
-
-  go_resource "golang.org/x/net" do
-    url "https://go.googlesource.com/net.git",
-        :revision => "5f9ae10d9af5b1c89ae6904293b14b064d4ada23"
-  end
-
-  go_resource "golang.org/x/text" do
-    url "https://go.googlesource.com/text.git",
-        :revision => "7922cc490dd5a7dbaa7fd5d6196b49db59ac042f"
-  end
-
   def install
     ENV["GOPATH"] = buildpath
-    mkdir_p buildpath/"src/github.com/mattn"
-    ln_s buildpath, buildpath/"src/github.com/mattn/jvgrep"
-    Language::Go.stage_deps resources, buildpath/"src"
-    system "go", "build", "-o", bin/"jvgrep"
+
+    dir = buildpath/"src/github.com/mattn/jvgrep"
+    dir.install buildpath.children
+
+    cd dir do
+      system "go", "build", "-o", bin/"jvgrep"
+      prefix.install_metafiles
+    end
   end
 
   test do
