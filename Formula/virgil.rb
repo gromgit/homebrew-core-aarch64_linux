b885a981da678145efaa4c01186fa7bc2d043233
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-06 11:52:43 +0000

    virgil: update 5.1.6 bottle.

--- Formula/virgil.rb
+++ Formula/virgil.rb
@@ -8,9 +8,9 @@ class Virgil < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "2b2f771af64c7b8827ed627334e5658ebf8a03c7de66cd8454a19edfb5a00f4b" => :catalina
-    sha256 "84f3312865cd188bc4bca33340d8f050ae4cd6bafb5af9ba078b14084cefedee" => :mojave
-    sha256 "f4ef9cd5a3d081ee3edcc38fdb762e6cbc3c0a8cf1b19cd1d5ca873ac649b7cc" => :high_sierra
+    sha256 "6d031ab150d2e143ce3e655ca8adda5fcff86fe784fb650535f5ffeb56ebb640" => :catalina
+    sha256 "81ce76c5dabd29e3d5d06f65359e08caa11dbbf06191e79efd23315e0e90c9d5" => :mojave
+    sha256 "152e84ec92c664b11d005ef2d4a8101546c70d2394f571ef63931f39efdaf38e" => :high_sierra
   end
 
   depends_on "dep" => :build
