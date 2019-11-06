c1439be871d18785369b1f94dd3f5e5704261bbd
Markus Reiter <me@reitermark.us>
2019-11-06 23:34:54 +0100

    apache-ctakes: depend on openjdk
    Closes #46444.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/apache-ctakes.rb
+++ Formula/apache-ctakes.rb
@@ -3,18 +3,19 @@ class ApacheCtakes < Formula
   homepage "https://ctakes.apache.org"
   url "https://apache.osuosl.org/ctakes/ctakes-4.0.0/apache-ctakes-4.0.0-bin.tar.gz"
   sha256 "37ca2b8dfe06465469ed1830fbb84dfc7bcc4295e5387d66e90a76ad2a5cdeaf"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
-    rm_f Dir["bin/*.bat", "bin/*.cmd", "bin/ctakes.profile", "bin/ctakes-ytex",
-             "libexec/*.bat", "libexec/*.cmd"]
+    rm Dir["**/*.{bat,cmd}"] + ["bin/ctakes.profile"]
     libexec.install %w[bin config desc lib resources]
     pkgshare.install_symlink libexec/"resources/org/apache/ctakes/examples"
 
-    bin.write_exec_script Dir["#{libexec}/bin/*"]
+    bin.install Dir["#{libexec}/bin/*.sh"]
+    bin.env_script_all_files libexec/"bin", :JAVA_HOME => Formula["openjdk"].opt_prefix
   end
 
   test do
