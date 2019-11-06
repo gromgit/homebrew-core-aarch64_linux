2e16ab6f6420aa2168831a250a766d41071b064a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-06 08:35:32 +0000

    vim: update 8.1.2250 bottle.

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -7,9 +7,9 @@ class Vim < Formula
   head "https://github.com/vim/vim.git"
 
   bottle do
-    sha256 "a52b6a84f1e3a61747e2b2a93eee922c388c379f10b3546ac7bb1e9f15a0351f" => :catalina
-    sha256 "0ab00a44658bbf34ee1350bf2c11c54480e3955514936db48fbd6e8382da5b3d" => :mojave
-    sha256 "125b769b5b7e667c1f7209108964a90cec9f5f6bbcc07e6dd506d8bb309eaca4" => :high_sierra
+    sha256 "0f1fa589a71190d4b17b9a88983534a016d5208d9ffacf8d6320133262edd72b" => :catalina
+    sha256 "0658a5f418089b7d5939de7e9e980b66548231ca77cd3101d7708a427ac787f8" => :mojave
+    sha256 "1563e40ff9f89d65f1dac93dbbf7944dce70635204eed32833a256184b357e54" => :high_sierra
   end
 
   depends_on "gettext"
