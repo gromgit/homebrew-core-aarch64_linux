da7dd72ec3d00763b1530aeb83ca8a888ff86e31
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-07 05:51:12 +0000

    tweak: add 3.02 bottle.

--- Formula/tweak.rb
+++ Formula/tweak.rb
@@ -4,6 +4,13 @@ class Tweak < Formula
   url "https://www.chiark.greenend.org.uk/~sgtatham/tweak/tweak-3.02.tar.gz"
   sha256 "5b4c19b1bf8734d1623e723644b8da58150b882efa9f23bbe797c3922f295a1a"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a38441e05b3953b324cee772161ebb1ccf12bf2262c476af921fee963fdee413" => :catalina
+    sha256 "82ec40f5ceaee7630a9bba6652c350388176c38908681fe4389a37d2e9605009" => :mojave
+    sha256 "e36456b9e78dafa97c7c972a9c26bc274cc30dff8f50c2a736d2aaca8068dfa8" => :high_sierra
+  end
+
   def install
     system "make"
     system "make", "install", "PREFIX=#{prefix}", "MANDIR=#{man1}"
