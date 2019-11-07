bc6b2f7760d4d30c895215dba443cab765772953
EricFromCanada <enk3@outlook.com>
2019-11-07 10:55:23 -0500

    libusbmuxd: renamed from usbmuxd

--- Formula/libusbmuxd.rb
+++ Formula/libusbmuxd.rb
@@ -1,5 +1,5 @@
-class Usbmuxd < Formula
-  desc "USB multiplexor daemon for iPhone and iPod Touch devices"
+class Libusbmuxd < Formula
+  desc "USB multiplexor library for iOS devices"
   homepage "https://www.libimobiledevice.org/"
   revision 1
 
@@ -12,17 +12,6 @@ class Usbmuxd < Formula
       url "https://github.com/libimobiledevice/libusbmuxd/commit/4397b3376dc4.patch?full_index=1"
       sha256 "b28e17c82dc11320741d33cf68fd78e1baec9e4133f5265b944f167839cbe9bb"
     end
-  end
-
-  bottle do
-    cellar :any
-    sha256 "e3478a1fe9d403496347a8663e686d6499d4510bd4619567f9e9651001822125" => :catalina
-    sha256 "2305d6794314c64d0fb8c3339c2d75a8e01d218715bc871560e4fe9fcad486a8" => :mojave
-    sha256 "253be465f391159e278a0a9625775075503f75b0a404aa64b9f557486de3b82c" => :high_sierra
-    sha256 "37a70014ca7c861639b8dc0f280b66306c082091731dfc3a53d1ef709ab98d5e" => :sierra
-    sha256 "e7227fb7deaefc2990e23d9cfdb3aa4305fc7f31e902560fa46272168c85e151" => :el_capitan
-    sha256 "0aae53db481257e6ce5eed9be080b63b347f2e05d5dfecc55b0936a9ee5ab336" => :yosemite
-    sha256 "e36b16d09c26e83daf359216a9b66fd2515a10d7432fdcc724c7aba5224f19b1" => :mavericks
   end
 
   head do
