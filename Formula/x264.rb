be9505b6549ebd2da50af9a28d0760c7d2c14ba5
Jonathan Chang <jchang641@gmail.com>
2019-11-07 20:36:10 +1100

    x264: disable opportunistic linkages (#46404)

--- Formula/x264.rb
+++ Formula/x264.rb
@@ -29,6 +29,8 @@ class X264 < Formula
     args = %W[
       --prefix=#{prefix}
       --disable-lsmash
+      --disable-swscale
+      --disable-ffms
       --enable-shared
       --enable-static
       --enable-strip
