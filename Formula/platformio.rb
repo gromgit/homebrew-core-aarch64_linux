5c9d16f61cea1dc7a91f1e32b10586f0a15c5f5e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-07 17:22:15 +0000

    platformio: update 4.1.0 bottle.

--- Formula/platformio.rb
+++ Formula/platformio.rb
@@ -8,10 +8,9 @@ class Platformio < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3b10eb609b95f629584a82e660b16bba2c830b58494fbbc2dafa49d65bc22911" => :catalina
-    sha256 "6677f58d58d4b35881e5276f28e6f6cba00cb24023ff3030ced307b160df936f" => :mojave
-    sha256 "2002d274935ccc1a9ea748ec3111157a8021f61352bf8624c201c8c3324fc0a0" => :high_sierra
-    sha256 "a05065a61b9dfb6f8f96c857b5a6c31c3e056b9e0b10c7f060ca6ac2414c1d2e" => :sierra
+    sha256 "f769790bdb0f71864d99da08e650635476a69d56a2e291cca76696d2cfbd3a6c" => :catalina
+    sha256 "d4a008cd54553a7830f8b78029fe44b67179f6fb9676667f34f3dc63b4886413" => :mojave
+    sha256 "1313c81f8906aa6c6646c3175c05089afbcc0260a18ccb957f2ae37d666d9fbe" => :high_sierra
   end
 
   depends_on "python"
