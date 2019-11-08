982f0b56fa81f57591cef54011b80389114eaa4c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-08 23:53:57 +0000

    cf-tool: add 0.8.2 bottle.

--- Formula/cf-tool.rb
+++ Formula/cf-tool.rb
@@ -4,6 +4,13 @@ class CfTool < Formula
   url "https://github.com/xalanq/cf-tool/archive/v0.8.2.tar.gz"
   sha256 "04eeb0ebfd2f15f81940012def95ec964498f7053029aa8b192d99c5a88bb98b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8c28ad65369bed1bc74105242097fca990c3b4ca875cca731eed928f547df1b4" => :catalina
+    sha256 "ea7a5befc573b9c55cb4ce6f5f65b33141737c767548ad98fc50689b8a8c7747" => :mojave
+    sha256 "9e0ab03bf2a895310eea7a6bc73221ffeb490416be1fb3bf9a77eda439e17159" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
