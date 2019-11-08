e8cc710342e5e67cac0cc50314ef579e77eecd83
Bruce Wang <number5@users.noreply.github.com>
2019-11-09 08:27:30 +1100

    tintin 2.01.92
    Update test
    
    Closes #46518.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/tintin.rb
+++ Formula/tintin.rb
@@ -1,8 +1,8 @@
 class Tintin < Formula
   desc "MUD client"
   homepage "https://tintin.sourceforge.io/"
-  url "https://downloads.sourceforge.net/tintin/tintin-2.01.91.tar.gz"
-  sha256 "3596a6d540d58162dc60c318c520cb94ee52fb438b5db8a2fa2513c47fbe6359"
+  url "https://downloads.sourceforge.net/tintin/tintin-2.01.92.tar.gz"
+  sha256 "3386d49810f2dbc162f890f26bb81e3807af34753d7a4ead64cc40e845cba845"
 
   bottle do
     cellar :any
@@ -28,10 +28,6 @@ class Tintin < Formula
   end
 
   test do
-    require "pty"
-    (testpath/"input").write("#end {bye}\n")
-    PTY.spawn(bin/"tt++", "-G", "input") do |r, _w, _pid|
-      assert_match "Goodbye", r.read
-    end
+    shell_output("#{bin}/tt++ -e \"#nop; #info system; #end;\"")
   end
 end
