df8d88781afa8c87dc4de0fb888295d817e376d0
Bo Anderson <mail@boanderson.me>
2019-11-09 20:21:56 +0000

    mysql-client@5.7 5.7.28 (new formula)

---
+++ Formula/mysql-client@5.7.rb
@@ -0,0 +1,43 @@
+class MysqlClientAT57 < Formula
+  desc "Open source relational database management system"
+  homepage "https://dev.mysql.com/doc/refman/5.7/en/"
+  url "https://cdn.mysql.com/Downloads/MySQL-5.7/mysql-boost-5.7.28.tar.gz"
+  sha256 "f16399315212117c08f9bdf8a0d682728b2ce82d691bcfbf25a770f413b6f2da"
+
+  keg_only :versioned_formula
+
+  depends_on "cmake" => :build
+
+  depends_on "openssl@1.1"
+
+  def install
+    # https://bugs.mysql.com/bug.php?id=87348
+    # Fixes: "ADD_SUBDIRECTORY given source
+    # 'storage/ndb' which is not an existing"
+    inreplace "CMakeLists.txt", "ADD_SUBDIRECTORY(storage/ndb)", ""
+
+    # -DINSTALL_* are relative to `CMAKE_INSTALL_PREFIX` (`prefix`)
+    args = %W[
+      -DCOMPILATION_COMMENT=Homebrew
+      -DDEFAULT_CHARSET=utf8
+      -DDEFAULT_COLLATION=utf8_general_ci
+      -DINSTALL_DOCDIR=share/doc/#{name}
+      -DINSTALL_INCLUDEDIR=include/mysql
+      -DINSTALL_INFODIR=share/info
+      -DINSTALL_MANDIR=share/man
+      -DINSTALL_MYSQLSHAREDIR=share/mysql
+      -DWITH_BOOST=boost
+      -DWITH_EDITLINE=system
+      -DWITH_SSL=yes
+      -DWITH_UNIT_TESTS=OFF
+      -DWITHOUT_SERVER=ON
+    ]
+
+    system "cmake", ".", *std_cmake_args, *args
+    system "make", "install"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/mysql --version")
+  end
+end
