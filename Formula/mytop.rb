d29ba6bbafc278fe7bbbabcb36b9eae3a915bc61
Bo Anderson <mail@boanderson.me>
2019-11-09 20:25:19 +0000

    mytop: revision bump for mysql-client

--- Formula/mytop.rb
+++ Formula/mytop.rb
@@ -4,7 +4,7 @@ class Mytop < Formula
   url "http://www.mysqlfanboy.com/mytop-3/mytop-1.9.1.tar.gz"
   mirror "https://deb.debian.org/debian/pool/main/m/mytop/mytop_1.9.1.orig.tar.gz"
   sha256 "179d79459d0013ab9cea2040a41c49a79822162d6e64a7a85f84cdc44828145e"
-  revision 7
+  revision 8
 
   bottle do
     cellar :any
