e44efc7177e9b99a4b539bbfb9e955238e2eb0ea
Caleb Xu <calebcenter@live.com>
2019-11-09 08:58:50 -0500

    sfst 1.4.7e
    fix
    
    Closes #46545.
    
    Signed-off-by: Rui Chen <rchen@meetup.com>

--- Formula/sfst.rb
+++ Formula/sfst.rb
@@ -1,8 +1,8 @@
 class Sfst < Formula
   desc "Toolbox for morphological analysers and other FST-based tools"
   homepage "https://www.cis.uni-muenchen.de/~schmid/tools/SFST/"
-  url "https://www.cis.uni-muenchen.de/~schmid/tools/SFST/data/SFST-1.4.7d.tar.gz"
-  sha256 "5a13c6a45298197216a6299eb6cdf96595d2036572bb518b9e1c1893cb1a6d5f"
+  url "https://www.cis.uni-muenchen.de/~schmid/tools/SFST/data/SFST-1.4.7e.tar.gz"
+  sha256 "9e1bda84db1575ffb3bea56f3d49898661ad663280c5b813467cd17a7d6b76ac"
 
   bottle do
     cellar :any_skip_relocation
