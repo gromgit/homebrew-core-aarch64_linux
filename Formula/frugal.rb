bfba2dea0b92e58516071604c9abe75368bd97b9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-09 02:14:42 +0000

    frugal: update 3.4.11 bottle.

--- Formula/frugal.rb
+++ Formula/frugal.rb
@@ -6,9 +6,9 @@ class Frugal < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a1f6fcd68406616345399ff1d98ce6e0a1d0485b1fefab664eedca0968261303" => :catalina
-    sha256 "83a3e37e7cd5f07393f84a61bc9a15ed6deb6c85e65524a1678c19bf1fe6e0ce" => :mojave
-    sha256 "df212ad1e83ca69002400157f5bb69878bfdf7484af8bfab641c80d3deed22db" => :high_sierra
+    sha256 "35bce17ddf806cf7c9170af302c2940799974ef1ecec2ebc4061f848a631ebd9" => :catalina
+    sha256 "0870926f0726800066d9844ac78dd2c0e110f7366455d4d18b4a2d8da51cb1eb" => :mojave
+    sha256 "3eab07531f450e026dc14b54fffb5e9fca5ee718053cddc29aa713ab027bba2b" => :high_sierra
   end
 
   depends_on "glide" => :build
