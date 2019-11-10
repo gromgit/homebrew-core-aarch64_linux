7e03791159023fcb12e181b7fe3e045237dd4a82
FX Coudert <fxcoudert@gmail.com>
2019-11-10 17:32:04 +0100

    vtk: revision for pyqt
    Closes #46598.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/vtk.rb
+++ Formula/vtk.rb
@@ -3,7 +3,7 @@ class Vtk < Formula
   homepage "https://www.vtk.org/"
   url "https://www.vtk.org/files/release/8.2/VTK-8.2.0.tar.gz"
   sha256 "34c3dc775261be5e45a8049155f7228b6bd668106c72a3c435d95730d17d57bb"
-  revision 3
+  revision 4
   head "https://github.com/Kitware/VTK.git"
 
   bottle do
