cd6385b66197971b364fb37d5ab5b2b276d8a8df
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-10 15:29:41 +0000

    dvc: update 0.67.0 bottle.

--- Formula/dvc.rb
+++ Formula/dvc.rb
@@ -7,9 +7,9 @@ class Dvc < Formula
   sha256 "fa02b646d328a9693e320b428307266789b2fdd24d0d5e47babe8aef6b432ea4"
 
   bottle do
-    sha256 "cdf9146f302a79921c95968e3bb3cc88e3dfe18474fc28f43e3e472ca3c643ed" => :catalina
-    sha256 "00d8faea2f677a87e5fefba4c6ca9da7b6ad3c8b41644c5bdf93cfbb8188c8be" => :mojave
-    sha256 "3174020731aefb8f1435adb22282db8afa72f7e907579731226658d1b3c0198f" => :high_sierra
+    sha256 "f3a6618ac71cde525880edcd1f74c84e6d5a24737e1ea726977b3575ee321ced" => :catalina
+    sha256 "a3988b48e83aa38ea23ac6618880b4216b579c0d482fb61d9891e05f8ade9e49" => :mojave
+    sha256 "c7644e6785311fc8f7bb3af0a666c5ab320cc343ce1160a356bf1773c2323a99" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
