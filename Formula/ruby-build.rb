749cdd00df269d38b4d2cf5e2b5c7ae88806459f
Mislav Marohnić <mislav@github.com>
2019-11-11 15:10:09 +0100

    ruby-build 20191111 (#46619)
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/ruby-build.rb
+++ Formula/ruby-build.rb
@@ -1,8 +1,8 @@
 class RubyBuild < Formula
   desc "Install various Ruby versions and implementations"
   homepage "https://github.com/rbenv/ruby-build"
-  url "https://github.com/rbenv/ruby-build/archive/v20191105.tar.gz"
-  sha256 "65e82df59ae16b359a999256d3441bf38c538891b1e89afa18c42ff4a9f161d3"
+  url "https://github.com/rbenv/ruby-build/archive/v20191111.tar.gz"
+  sha256 "9cbadf9c7cb4cd92b05b1d3e8c59937c524748c5c29b05a5b68ee66f84160899"
   head "https://github.com/rbenv/ruby-build.git"
 
   bottle :unneeded
