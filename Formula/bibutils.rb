8e8ac2ea46fcbf54ff9cb93b0f0740570a109ad0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-11 14:21:07 +0000

    bibutils: update 6.8 bottle.

--- Formula/bibutils.rb
+++ Formula/bibutils.rb
@@ -6,11 +6,9 @@ class Bibutils < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c9468806b3c51e20497707ffbd6b5d65880fb7016d9b8fe0f34efca96a34739f" => :catalina
-    sha256 "77c7e24e44776095769e2103d7bc37ebbd314e91c6c86a9a4e24ad42f15b16dd" => :mojave
-    sha256 "cf925ae310688ae93b2f93a4bb23deb16f06fa8f28f20b67aee17162eaa484c1" => :high_sierra
-    sha256 "97a7c39fa603ac3ebdbde752b0b34d55bc724df248c8ecc8293e9cf47a791823" => :sierra
-    sha256 "b0c558be47dbda2db57b3f13dcf830c37efcef97a5af1899aa3910027c9cdbed" => :el_capitan
+    sha256 "dac93b43ca23378a2479b3940ba06711ee75ec20448b8157f9c86e7793fa903e" => :catalina
+    sha256 "581d652841aeef6824b5dbec7bd4d5d7ee6cb8681125d7588d23ebd28ed311df" => :mojave
+    sha256 "6d10e1fe5b235d6b6027512eed4cb196c5a64db785f5df8dd4535a641d1010d8" => :high_sierra
   end
 
   def install
