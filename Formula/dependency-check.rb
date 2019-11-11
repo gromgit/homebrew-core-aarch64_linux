73359f41d94b064986a8a31ec280fc1b00d58f79
Jeremy Long <jeremy.long@gmail.com>
2019-11-11 08:38:04 -0500

    dependency-check 5.2.3
    Closes #46623.
    
    Signed-off-by: Rui Chen <rchen@meetup.com>

--- Formula/dependency-check.rb
+++ Formula/dependency-check.rb
@@ -1,8 +1,8 @@
 class DependencyCheck < Formula
   desc "OWASP dependency-check"
   homepage "https://www.owasp.org/index.php/OWASP_Dependency_Check"
-  url "https://dl.bintray.com/jeremy-long/owasp/dependency-check-5.2.2-release.zip"
-  sha256 "a0bb0e041db4a22ae0a1db83ff962b960aaf9cd0205d99d64e872361ccfcaa34"
+  url "https://dl.bintray.com/jeremy-long/owasp/dependency-check-5.2.3-release.zip"
+  sha256 "40e2d8514da5930986e2d2f5720a5be9eb12cd49359bc442bcf6e9e63242afd8"
 
   bottle :unneeded
 
