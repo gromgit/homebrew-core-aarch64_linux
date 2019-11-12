2540f807f53d47a64fe39bb8058dc22ca9d966c3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-12 17:17:09 +0000

    starship: update 0.26.3 bottle.

--- Formula/starship.rb
+++ Formula/starship.rb
@@ -7,9 +7,9 @@ class Starship < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "019f4188a7333079222d4badf7eca7e072261cfe315dd86d9a19cc307bf3484e" => :catalina
-    sha256 "1733139a61d2f7b0ff92d9493103d3bd45fad72e847d4c9f233bd3c522ac2f4a" => :mojave
-    sha256 "2af180a8e59d6c8676edd41fa4cf10ee38e0c7d5a1e096d7df8f3c853ba3cf5e" => :high_sierra
+    sha256 "56673443d52a21fe0daedf0e4b8d46c1ea1614c5f485deb91b3bacde5dc8438c" => :catalina
+    sha256 "cceafd0c1f022513307dae1f3215e3ab23df8d642ccb62d93a18f6e50bdf39c8" => :mojave
+    sha256 "0a06146fe49b66f0e49211b0db863c82615d9051101be72477b11ba6b05583b1" => :high_sierra
   end
 
   depends_on "rust" => :build
