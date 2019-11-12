f80fc64a36bf178cb3ab0124853a04053e3c651a
Tim Byrne <sultan@locehilios.com>
2019-11-12 08:09:06 -0600

    yadm 2.0.1
    Closes #46650.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/yadm.rb
+++ Formula/yadm.rb
@@ -1,21 +1,20 @@
 class Yadm < Formula
   desc "Yet Another Dotfiles Manager"
-  homepage "https://thelocehiliosan.github.io/yadm/"
-  url "https://github.com/TheLocehiliosan/yadm/archive/1.12.0.tar.gz"
-  sha256 "c3d612d01e2027d5f457e0f7d120bc67251b716c373d99fe70638bd86edf107f"
+  homepage "https://yadm.io/"
+  url "https://github.com/TheLocehiliosan/yadm/archive/2.0.1.tar.gz"
+  sha256 "935be5ea014a3b5c17a70585815f8f5f254fd0748cd1c649781807c45e5b26fe"
 
   bottle :unneeded
 
   def install
-    bin.install "yadm"
-    man1.install "yadm.1"
+    system "make", "install", "PREFIX=#{prefix}"
     bash_completion.install "completion/yadm.bash_completion"
     zsh_completion.install  "completion/yadm.zsh_completion" => "_yadm"
   end
 
   test do
     system bin/"yadm", "init"
-    assert_predicate testpath/".yadm/repo.git/config", :exist?, "Failed to init repository."
+    assert_predicate testpath/".config/yadm/repo.git/config", :exist?, "Failed to init repository."
     assert_match testpath.to_s, shell_output("#{bin}/yadm gitconfig core.worktree")
 
     # disable auto-alt
