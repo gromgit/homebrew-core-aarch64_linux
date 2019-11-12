ac2958915dd73da31e68528cb1b429b012033e4c
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2019-11-12 18:26:13 +0100

    Liblouis 3.11.0 (new formula)

---
+++ Formula/liblouis.rb
@@ -0,0 +1,48 @@
+class Liblouis < Formula
+  desc "Open-source braille translator and back-translator"
+  homepage "http://liblouis.org"
+
+  stable do
+    url "https://github.com/liblouis/liblouis/releases/download/v3.11.0/liblouis-3.11.0.tar.gz"
+    sha256 "b802aba0bff49636907ca748225e21c56ecf3f3ebc143d582430036d4d9f6259"
+    depends_on "help2man" => :build
+    depends_on "pkg-config" => :build
+    depends_on "python"
+  end
+  head do
+    url "https://github.com/liblouis/liblouis.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "help2man" => :build
+    depends_on "libtool" => :build
+    depends_on "pkg-config" => :build
+    depends_on "python"
+  end
+
+  def install
+    if build.head?
+      system "./autogen.sh"
+    end
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "check"
+    system "make", "install"
+    cd "python" do
+      system "python3", *Language::Python.setup_install_args(prefix)
+    end
+  end
+
+  def post_install
+    mkdir "#{prefix}/tools"
+    mv "#{bin}/lou_maketable", "#{prefix}/tools/", :force => true
+    mv "#{bin}/lou_maketable.d", "#{prefix}/tools/", :force => true
+  end
+
+  test do
+    o, = Open3.capture2(bin/"lou_translate", "unicode.dis,en-us-g2.ctb", :stdin_data=>"42")
+    assert_equal o, "⠼⠙⠃"
+  end
+end
