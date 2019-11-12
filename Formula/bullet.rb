116b14bea212478b54f7dfcac15425ff0052715f
Bo Anderson <mail@boanderson.me>
2019-11-12 14:40:22 +0000

    bullet: patch for Xcode 11
    Closes #46652.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/bullet.rb
+++ Formula/bullet.rb
@@ -1,9 +1,19 @@
 class Bullet < Formula
   desc "Physics SDK"
   homepage "https://bulletphysics.org/"
-  url "https://github.com/bulletphysics/bullet3/archive/2.88.tar.gz"
-  sha256 "21c135775527754fc2929db1db5144e92ad0218ae72840a9f162acb467a7bbf9"
+  revision 1
   head "https://github.com/bulletphysics/bullet3.git"
+
+  stable do
+    url "https://github.com/bulletphysics/bullet3/archive/2.88.tar.gz"
+    sha256 "21c135775527754fc2929db1db5144e92ad0218ae72840a9f162acb467a7bbf9"
+
+    # Fix Xcode 11 compile errors
+    patch do
+      url "https://github.com/bulletphysics/bullet3/commit/7638b7c5a659dceb4e580ae87d4d60b00847ef94.patch?full_index=1"
+      sha256 "2b08ff21dce85562bd6ff24ae6b71ed55de84b24a0a74d1e8323b7a0f783f528"
+    end
+  end
 
   bottle do
     sha256 "441f12cab157a05a69e5a7de426296b096fef0b88d0ebf386ce15503e114814d" => :mojave
