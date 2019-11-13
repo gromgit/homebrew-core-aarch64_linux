0d40511b54a0be18a5a41dec70715dcb1effc947
Jeff Bonhag <jeff@thebonhags.com>
2019-11-12 21:44:13 -0500

    chromaprint: build fpcalc
    Closes #46684.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/chromaprint.rb
+++ Formula/chromaprint.rb
@@ -3,6 +3,7 @@ class Chromaprint < Formula
   homepage "https://acoustid.org/chromaprint"
   url "https://github.com/acoustid/chromaprint/releases/download/v1.4.3/chromaprint-1.4.3.tar.gz"
   sha256 "ea18608b76fb88e0203b7d3e1833fb125ce9bb61efe22c6e169a50c52c457f82"
+  revision 1
 
   bottle do
     cellar :any
@@ -14,9 +15,15 @@ class Chromaprint < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on "ffmpeg"
 
   def install
-    system "cmake", ".", *std_cmake_args
+    system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DBUILD_TOOLS=ON", ".", *std_cmake_args
     system "make", "install"
+  end
+
+  test do
+    out = shell_output("#{bin}/fpcalc -json -format s16le -rate 44100 -channels 2 -length 10 /dev/zero")
+    assert_equal "AQAAO0mUaEkSRZEGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", JSON.parse(out)["fingerprint"]
   end
 end
