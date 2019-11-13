36c0b80a4921eed326e09121bfc00a60dee9b743
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-13 13:15:41 +0000

    virustotal-cli: add 0.7.0 bottle.

--- Formula/virustotal-cli.rb
+++ Formula/virustotal-cli.rb
@@ -4,6 +4,13 @@ class VirustotalCli < Formula
   url "https://github.com/VirusTotal/vt-cli/archive/0.7.0.tar.gz"
   sha256 "39a566be0e2ee1102c0bd9d3ddefa4a0e423c9ffe02962d4a48897a875312c95"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "36fcf039fa5264b8841ba3ba954816031cb1b6ac42aacd26b1e5791b643fae30" => :catalina
+    sha256 "be3fd19af1b5004ccf95ef0996fa387b88e7bfa30ca651dd69b94700fb92dcec" => :mojave
+    sha256 "118eaafec3d20e709091266c786d5121b27e0d1c8d8ded2f5c21c77046b571d5" => :high_sierra
+  end
+
   depends_on "dep" => :build
   depends_on "go" => :build
 
