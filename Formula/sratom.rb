b7c7b9f90529bffef62b1d945d832c1fb14938cb
EricFromCanada <enk3@outlook.com>
2019-11-13 10:50:13 -0500

    sratom 0.6.4
    Closes #46710.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/sratom.rb
+++ Formula/sratom.rb
@@ -1,8 +1,8 @@
 class Sratom < Formula
   desc "Library for serializing LV2 atoms to/from RDF"
   homepage "https://drobilla.net/software/sratom/"
-  url "https://download.drobilla.net/sratom-0.6.2.tar.bz2"
-  sha256 "0a514a55d6b6cb7b5d6f32d1dcb78a1e6e54537fa22fce533e4ef6adf240e853"
+  url "https://download.drobilla.net/sratom-0.6.4.tar.bz2"
+  sha256 "146c8f14b8902ac3c8fa8c2e0a014eb8a38fab60090c5adbfbff3e3b7c5c006e"
 
   bottle do
     cellar :any
@@ -20,5 +20,22 @@ class Sratom < Formula
     system "./waf", "configure", "--prefix=#{prefix}"
     system "./waf"
     system "./waf", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <sratom/sratom.h>
+
+      int main()
+      {
+        return 0;
+      }
+    EOS
+    lv2 = Formula["lv2"].opt_include
+    serd = Formula["serd"].opt_include
+    sord = Formula["sord"].opt_include
+    system ENV.cc, "-I#{lv2}", "-I#{serd}/serd-0", "-I#{sord}/sord-0", "-I#{include}/sratom-0",
+                   "-L#{lib}", "-lsratom-0", "test.c", "-o", "test"
+    system "./test"
   end
 end
