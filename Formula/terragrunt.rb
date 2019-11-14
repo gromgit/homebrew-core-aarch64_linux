30081a601120c875fe4dd0f3c59606dd7f5f4920
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-14 04:08:26 +0000

    terragrunt: update 0.21.6 bottle.

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -7,9 +7,9 @@ class Terragrunt < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ee3fdd0a7dc5034f85ac102176bdc31f198335a07a5208c7ef0589c729a38a0e" => :catalina
-    sha256 "5efb5b254fe215acf133ae182429c98041477d55d0ffb321366de3b3fae47902" => :mojave
-    sha256 "75cc3e08ee14ce2053dd0e4a29c01d2df6bcbd2436cd9e851cbaabb2c38d1188" => :high_sierra
+    sha256 "ec2971d6f1f3c4835263eb3d67dcbd8a52dafbc42a51f831fcd7aaf08124277f" => :catalina
+    sha256 "2e089cb9b5cfda3f9c023f91338fdb4ee2e92d770a147e8b789cdbb4efef1f00" => :mojave
+    sha256 "7c40aeeaac92a84b6fb1104621ca028c427e57112a4fccc55f6e8012d57a2fe2" => :high_sierra
   end
 
   depends_on "dep" => :build
