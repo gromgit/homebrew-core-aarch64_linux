d1cb07647779b9fb7206420d8a3f2387a43ab03a
EricFromCanada <enk3@outlook.com>
2019-11-13 23:46:15 -0500

    screenfetch 3.9.1
    Closes #46736.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/screenfetch.rb
+++ Formula/screenfetch.rb
@@ -1,8 +1,8 @@
 class Screenfetch < Formula
   desc "Generate ASCII art with terminal, shell, and OS info"
   homepage "https://github.com/KittyKatt/screenFetch"
-  url "https://github.com/KittyKatt/screenFetch/archive/v3.9.0.tar.gz"
-  sha256 "d6df4ef7763f9761d818c878465d78ef701b71002a50d4f150f65a31cc1bea37"
+  url "https://github.com/KittyKatt/screenFetch/archive/v3.9.1.tar.gz"
+  sha256 "aa97dcd2a8576ae18de6c16c19744aae1573a3da7541af4b98a91930a30a3178"
   head "https://github.com/KittyKatt/screenFetch.git", :shallow => false
 
   bottle :unneeded
