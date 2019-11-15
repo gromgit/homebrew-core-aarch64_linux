d9c08343684a30b74f6fcece0358e875285e3824
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-15 04:08:43 +0000

    trader: add 7.13 bottle.

--- Formula/trader.rb
+++ Formula/trader.rb
@@ -4,6 +4,12 @@ class Trader < Formula
   url "https://ftp.zap.org.au/pub/trader/unix/trader-7.13.tar.xz"
   sha256 "0d2b51134166b0f436dc6423e2ce378b1df929a9de141c002f3da86af18bb262"
 
+  bottle do
+    sha256 "f0265ff875601e4d5f9ecc1a4a8bdb903f7399b8cd93a137a17b0f9c78f052ec" => :catalina
+    sha256 "656ff27724a844d0059f37977628b5828768ad86a557ae7442365005cbaf535f" => :mojave
+    sha256 "6312d15659ff81b39cc0c03a3aff850ea94e112086d6b39a02287f1bf49ad46b" => :high_sierra
+  end
+
   depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "ncurses" # The system version does not work correctly
