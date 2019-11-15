cb9f162054c2702724b72b33a9ca5ef234cc120d
Joe Testa <jtesta@positronsecurity.com>
2019-11-14 20:07:59 -0500

    ssh-audit 2.1.0 (#46761)

--- Formula/ssh-audit.rb
+++ Formula/ssh-audit.rb
@@ -1,8 +1,8 @@
 class SshAudit < Formula
-  desc "SSH server auditing"
+  desc "SSH server & client auditing"
   homepage "https://github.com/jtesta/ssh-audit"
-  url "https://github.com/jtesta/ssh-audit/releases/download/v2.0.0/ssh-audit-v2.0.0.tar.gz"
-  sha256 "9ae7db82c343fc2d3af20994e750fbe69da8bed7ecde14e3fd8607d23b758c75"
+  url "https://github.com/jtesta/ssh-audit/releases/download/v2.1.0/ssh-audit-2.1.0.tar.gz"
+  sha256 "0c689500b2e2d4496914b5e100705ef8289c21d7b5f0d081470ae25e5b661f63"
   head "https://github.com/jtesta/ssh-audit.git"
 
   bottle :unneeded
