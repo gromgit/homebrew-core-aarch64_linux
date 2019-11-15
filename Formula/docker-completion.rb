00653a98936abdd927c0cc2063376318a8333ed0
chenrui <chenrui333@gmail.com>
2019-11-14 19:54:20 -0500

    docker-completion 19.03.5 (#46763)

--- Formula/docker-completion.rb
+++ Formula/docker-completion.rb
@@ -2,8 +2,8 @@ class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/docker-ce.git",
-      :tag      => "v19.03.4",
-      :revision => "9013bf583a215dc1488d941f9b6f7f11e1ea899f"
+      :tag      => "v19.03.5",
+      :revision => "633a0ea838f10e000b7c6d6eed1623e6e988b5bb"
 
   bottle :unneeded
 
