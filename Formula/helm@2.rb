238537e13db68133f354e5edb6a2f65a3e749509
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-16 06:00:09 +0000

    helm@2: add 2.16.1 bottle.

--- Formula/helm@2.rb
+++ Formula/helm@2.rb
@@ -5,6 +5,13 @@ class HelmAT2 < Formula
       :tag      => "v2.16.1",
       :revision => "bbdfe5e7803a12bbdf97e94cd847859890cf4050"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1450f1bdc13f6ac6f08c3edcefbdf0301a73d6158a8335b3fec68c32bf367d7c" => :catalina
+    sha256 "eedc16bde2576c3ddb4d02213154ad09951e5e65ec2d1a1ec1e451d6fa5108b2" => :mojave
+    sha256 "c9d3fe248f6231c860f7cc0226bea285284f7a8af82ee5d2d88e35c9c407fb0f" => :high_sierra
+  end
+
   keg_only :versioned_formula
 
   depends_on "glide" => :build
