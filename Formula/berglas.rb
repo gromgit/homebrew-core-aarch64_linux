e921e36c82b59dccc9918f7ebf43f8bd159aef27
Rui Chen <chenrui333@gmail.com>
2019-11-17 18:37:18 -0500

    berglas 0.3.0
    Closes #46856.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/berglas.rb
+++ Formula/berglas.rb
@@ -1,8 +1,8 @@
 class Berglas < Formula
   desc "Tool for managing secrets on Google Cloud"
   homepage "https://github.com/GoogleCloudPlatform/berglas"
-  url "https://github.com/GoogleCloudPlatform/berglas/archive/v0.2.1.tar.gz"
-  sha256 "31b25cfe81e72e8af9c6d856f714765be7acf204acf37f0ebae5d801bfe49e4c"
+  url "https://github.com/GoogleCloudPlatform/berglas/archive/v0.3.0.tar.gz"
+  sha256 "4b42a057cf950e001a995e5b9b9dcec51dbfde652a486e1a6bf75a942507fae8"
 
   bottle do
     cellar :any_skip_relocation
@@ -14,18 +14,12 @@ class Berglas < Formula
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/GoogleCloudPlatform/berglas").install buildpath.children
-
-    cd "src/github.com/GoogleCloudPlatform/berglas" do
-      system "go", "build", "-o", bin/"berglas"
-      prefix.install_metafiles
-    end
+    system "go", "build", "-o", bin/"berglas"
   end
 
   test do
     assert_match "#{version}\n", shell_output("#{bin}/berglas --version 2>&1")
-    out = shell_output("#{bin}/berglas list homebrewtest 2>&1", 60)
+    out = shell_output("#{bin}/berglas list homebrewtest 2>&1", 61)
     assert_match "could not find default credentials.", out
   end
 end
