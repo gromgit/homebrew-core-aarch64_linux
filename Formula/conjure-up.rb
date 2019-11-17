464069f02f9f67bdea8a7caf5816d53707e02eb7
FX Coudert <fxcoudert@gmail.com>
2019-11-17 15:37:30 +0100

    conjure-up: remove Catalina bottle

--- Formula/conjure-up.rb
+++ Formula/conjure-up.rb
@@ -8,7 +8,6 @@ class ConjureUp < Formula
 
   bottle do
     cellar :any
-    sha256 "0cc33b2d1044fb6d5b65ce2feb2eb9a3186a3d351bcd51b11f727b89e7386484" => :catalina
     sha256 "8353c30ae1baa70b248c63fbbcab0a958df64eb5c2f4ad7822c344d21528d8b4" => :mojave
     sha256 "311cb690eab903378d5ce9c2a0622d5e4ed98562460d50251a7ecf62aa7d22c6" => :high_sierra
   end
