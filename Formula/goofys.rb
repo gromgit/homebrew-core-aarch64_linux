5c6fece185faeb625abcd0c4178b4fe98f8c7ede
Rui Chen <chenrui333@gmail.com>
2019-11-17 14:19:30 -0500

    goofys 0.23.1
    Closes #46851.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/goofys.rb
+++ Formula/goofys.rb
@@ -1,11 +1,9 @@
-require "language/go"
-
 class Goofys < Formula
   desc "Filey-System interface to Amazon S3"
   homepage "https://github.com/kahing/goofys"
   url "https://github.com/kahing/goofys.git",
-      :tag      => "v0.23.0",
-      :revision => "3d8c910723cded1b62f4adb40bbcd68d7e190745"
+      :tag      => "v0.23.1",
+      :revision => "b720aae5b4845855bb3d06b8ade10585934cd1e2"
   head "https://github.com/kahing/goofys.git"
 
   bottle do
@@ -18,64 +16,12 @@ class Goofys < Formula
   depends_on "go" => :build
   depends_on :osxfuse
 
-  go_resource "github.com/jacobsa/fuse" do
-    url "https://github.com/jacobsa/fuse.git",
-        :revision => "c4e473376f7d5be650b11657ded3afb1cd80ad7c"
-  end
-
-  go_resource "github.com/jinzhu/copier" do
-    url "https://github.com/jinzhu/copier.git",
-        :revision => "db4671f3a9b8df855e993f7c94ec5ef1ffb0a23b"
-  end
-
-  go_resource "github.com/kardianos/osext" do
-    url "https://github.com/kardianos/osext.git",
-        :revision => "ae77be60afb1dcacde03767a8c37337fad28ac14"
-  end
-
-  go_resource "github.com/sevlyar/go-daemon" do
-    url "https://github.com/sevlyar/go-daemon.git",
-        :revision => "e49ef56654f54139c4dc0285f973f74e9649e729"
-  end
-
-  go_resource "github.com/shirou/gopsutil" do
-    url "https://github.com/shirou/gopsutil.git",
-        :revision => "2ae56c34ce208b38309ab1618fc82866a1051811"
-  end
-
-  go_resource "github.com/sirupsen/logrus" do
-    url "https://github.com/sirupsen/logrus.git",
-        :revision => "d682213848ed68c0a260ca37d6dd5ace8423f5ba"
-  end
-
-  go_resource "github.com/urfave/cli" do
-    url "https://github.com/urfave/cli.git",
-        :revision => "75104e932ac2ddb944a6ea19d9f9f26316ff1145"
-  end
-
-  go_resource "golang.org/x/crypto" do
-    url "https://go.googlesource.com/crypto.git",
-        :revision => "0fcca4842a8d74bfddc2c96a073bd2a4d2a7a2e8"
-  end
-
-  go_resource "golang.org/x/net" do
-    url "https://go.googlesource.com/net.git",
-        :revision => "434ec0c7fe3742c984919a691b2018a6e9694425"
-  end
-
-  go_resource "golang.org/x/sys" do
-    url "https://go.googlesource.com/sys.git",
-        :revision => "d38bf781f16e180a1b2ad82697d2f81d7b7ecfac"
-  end
-
   def install
     contents = Dir["*"]
     gopath = buildpath/"gopath"
     (gopath/"src/github.com/kahing/goofys").install contents
 
     ENV["GOPATH"] = gopath
-
-    Language::Go.stage_deps resources, gopath/"src"
 
     cd gopath/"src/github.com/kahing/goofys" do
       commit = Utils.popen_read("git rev-parse HEAD").chomp
