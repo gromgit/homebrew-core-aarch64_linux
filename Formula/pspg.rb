5c0912149128e0f03f036f91b963df216616b422
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-18 16:50:11 +0000

    pspg: update 2.5.3 bottle.

--- Formula/pspg.rb
+++ Formula/pspg.rb
@@ -7,9 +7,10 @@ class Pspg < Formula
 
   bottle do
     cellar :any
-    sha256 "efe13b563bb04d1ea0245904a29eb0ffecef99d520b6b9a07f6ecca09b4a6bf1" => :catalina
-    sha256 "ee50a412369f97e19ea59dba3315e1159bc91c06bb13bc36c79588f13032b19e" => :mojave
-    sha256 "978a2c0600cf23ab33fa128da6870d8df73949f1c57549ee33cde36b8380f196" => :high_sierra
+    rebuild 1
+    sha256 "176811eeb5c2d8b3d41c96b907bfe0bd41c348c3ba0eb2ac0b08cf2593d1a3f5" => :catalina
+    sha256 "65a050cce1994343bd357c63d948738eed7c7989a9e64c13676c4d8f589dbb8d" => :mojave
+    sha256 "fe5cbf052d47393042837cec2db676769df49630b20e33e67003bb72e274ad47" => :high_sierra
   end
 
   depends_on "libpq"
