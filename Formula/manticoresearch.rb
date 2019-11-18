c0b8317e696b4fd1a6e1e60932f3c39a30e022a1
FX Coudert <fxcoudert@gmail.com>
2019-11-18 11:37:10 +0100

    manticoresearch: use OpenSSL 1.1
    Closes #46867.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/manticoresearch.rb
+++ Formula/manticoresearch.rb
@@ -3,6 +3,7 @@ class Manticoresearch < Formula
   homepage "https://www.manticoresearch.com"
   url "https://github.com/manticoresoftware/manticoresearch/releases/download/3.2.0/manticore-3.2.0-191017-e526a01-release.tar.gz"
   sha256 "df6dbcc4df01065fc3cc6328f043b8cef3eb403a28671455cd3c8fc4217e3391"
+  revision 1
   head "https://github.com/manticoresoftware/manticoresearch.git"
 
   bottle do
@@ -16,7 +17,7 @@ class Manticoresearch < Formula
   depends_on "libpq" => :build
   depends_on "mysql" => :build
   depends_on "unixodbc" => :build
-  depends_on "openssl"
+  depends_on "openssl@1.1"
 
   conflicts_with "sphinx",
    :because => "manticore, sphinx install the same binaries."
