597cafcd906c7e2a0802961f2a1278f6a34e218a
Justin Hammond <Justin@dynam.ac>
2019-11-18 19:34:05 +0800

    open-zwave 1.6.962
    Closes #46870.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/open-zwave.rb
+++ Formula/open-zwave.rb
@@ -1,8 +1,8 @@
 class OpenZwave < Formula
   desc "Library that interfaces with selected Z-Wave PC controllers"
   homepage "http://www.openzwave.com"
-  url "https://github.com/OpenZWave/open-zwave/archive/v1.6.tar.gz"
-  sha256 "3b11dffa7608359c8c848451863e0287e17f5f101aeee7c2e89b7dc16f87050b"
+  url "http://old.openzwave.com/downloads/openzwave-1.6.962.tar.gz"
+  sha256 "d8dfc382f7db2a2e530e9971ff7e325a0c46b29954028b050b4ed36fe0e667bf"
 
   bottle do
     sha256 "3558f07a0fc5c7fc44546e1a2cf7b51df88769d3d10e3d72861134ddb418bc07" => :mojave
