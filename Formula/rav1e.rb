720bdfb4100c7deb843e1da9304905c0682e1958
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-19 06:04:55 +0000

    rav1e: add 0.1.0 bottle.

--- Formula/rav1e.rb
+++ Formula/rav1e.rb
@@ -4,6 +4,13 @@ class Rav1e < Formula
   url "https://github.com/xiph/rav1e/archive/0.1.0.tar.gz"
   sha256 "00395087eaba4778d17878924e007716e2f399116b8011bf057fd54cc528a6cb"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1367e1ee122f6c24213d78e7ec5c26545ebc5a901ffb5b129013282e06175bc8" => :catalina
+    sha256 "f2e114c452d5abca2e1ec3635add73036520092f25db8285a5d35b7125483098" => :mojave
+    sha256 "d7677873a6ddb3c9818aadc04324727bd7d402ec930609dbbffff47cb931337a" => :high_sierra
+  end
+
   depends_on "nasm" => :build
   depends_on "rust" => :build
 
