812be5ec487d4ad14cd18199d2aaf4691ac6cda0
Romain Vimont <rom@rom1v.com>
2019-11-20 00:12:36 +0100

    scrcpy 1.11
    Closes #46943.
    
    Signed-off-by: Caleb Xu <calebcenter@live.com>

--- Formula/scrcpy.rb
+++ Formula/scrcpy.rb
@@ -1,8 +1,8 @@
 class Scrcpy < Formula
   desc "Display and control your Android device"
   homepage "https://github.com/Genymobile/scrcpy"
-  url "https://github.com/Genymobile/scrcpy/archive/v1.10.tar.gz"
-  sha256 "71bd3b01f26233d73b91c0953ce004bd3195bcfd0c83b76c269094fb06e5ffa5"
+  url "https://github.com/Genymobile/scrcpy/archive/v1.11.tar.gz"
+  sha256 "86fccd2190a14c7f3024edf922229919803dee774f91c85fdee54029e179f270"
 
   bottle do
     sha256 "f5035dbe7c166ce1cc471328f5a811e67da9bf00002d5ce73bf622c1aed5c449" => :catalina
@@ -18,14 +18,8 @@ class Scrcpy < Formula
   depends_on "sdl2"
 
   resource "prebuilt-server" do
-    url "https://github.com/Genymobile/scrcpy/releases/download/v1.10/scrcpy-server-v1.10.jar"
-    sha256 "cbeb1a4e046f1392c1dc73c3ccffd7f86dec4636b505556ea20929687a119390"
-  end
-
-  # fix build (https://github.com/Genymobile/scrcpy/pull/695)
-  patch do
-    url "https://github.com/Genymobile/scrcpy/commit/c05056343b56be65ae887f8b7ead61a8072622b9.patch?full_index=1"
-    sha256 "dc6019d262884b9d925c7b6c5f9cad56f73f12ecc6291d052764b658e56c0fb4"
+    url "https://github.com/Genymobile/scrcpy/releases/download/v1.11/scrcpy-server-v1.11"
+    sha256 "ff3a454012e91d9185cfe8ca7691cea16c43a7dcc08e92fa47ab9f0ea675abd1"
   end
 
   def install
