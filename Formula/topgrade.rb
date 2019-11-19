d0eb043ce8bf8c374ed5f4e3e9439201acd58fc4
Rui Chen <chenrui333@gmail.com>
2019-11-19 07:41:10 -0500

    topgrade 3.4.0
    Closes #46916.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -1,8 +1,8 @@
 class Topgrade < Formula
   desc "Upgrade all the things"
   homepage "https://github.com/r-darwish/topgrade"
-  url "https://github.com/r-darwish/topgrade/archive/v3.3.0.tar.gz"
-  sha256 "22f8065d3fe715726de400927f845323b66e93ca374fbb3b6276289f83052384"
+  url "https://github.com/r-darwish/topgrade/archive/v3.4.0.tar.gz"
+  sha256 "8dc0357f031e5ffb941f57750293540ec41ac1f0453774d3d2a031ef0e47a8e3"
 
   bottle do
     cellar :any_skip_relocation
