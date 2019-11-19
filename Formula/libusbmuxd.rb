0c7a880933a8b1d64d9aaca40de315b7930e9378
Theodore Dubois <tblodt@icloud.com>
2019-11-18 22:43:24 -0800

    libusbmuxd 2.0.0

--- Formula/libusbmuxd.rb
+++ Formula/libusbmuxd.rb
@@ -4,14 +4,8 @@ class Libusbmuxd < Formula
   revision 1
 
   stable do
-    url "https://www.libimobiledevice.org/downloads/libusbmuxd-1.0.10.tar.bz2"
-    sha256 "1aa21391265d2284ac3ccb7cf278126d10d354878589905b35e8102104fec9f2"
-
-    # Backport of upstream security fix for CVE-2016-5104.
-    patch do
-      url "https://github.com/libimobiledevice/libusbmuxd/commit/4397b3376dc4.patch?full_index=1"
-      sha256 "b28e17c82dc11320741d33cf68fd78e1baec9e4133f5265b944f167839cbe9bb"
-    end
+    url "https://github.com/libimobiledevice/libusbmuxd/archive/2.0.0.tar.gz"
+    sha256 "ecf287b9d5fa28645a6b5ed640b6bd174134227c4fd8fde28d0678df2be0e97a"
   end
 
   bottle do
@@ -22,19 +16,18 @@ class Libusbmuxd < Formula
   end
 
   head do
-    url "https://git.sukimashita.com/libusbmuxd.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
+    url "https://github.com/libimobiledevice/libusbmuxd.git"
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "libplist"
   depends_on "libusb"
 
   def install
-    system "./autogen.sh" if build.head?
+    system "./autogen.sh"
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
