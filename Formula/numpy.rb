ee64f0d9e4b9675b888be2f7a128d8be71750f8f
Alexander Bayandin <a.bayandin@gmail.com>
2019-11-19 12:04:56 +0000

    numpy 1.17.4
    Closes #46914.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/numpy.rb
+++ Formula/numpy.rb
@@ -1,8 +1,8 @@
 class Numpy < Formula
   desc "Package for scientific computing with Python"
   homepage "https://www.numpy.org/"
-  url "https://files.pythonhosted.org/packages/ac/36/325b27ef698684c38b1fe2e546e2e7ef9cecd7037bcdb35c87efec4356af/numpy-1.17.2.zip"
-  sha256 "73615d3edc84dd7c4aeb212fa3748fb83217e00d201875a47327f55363cef2df"
+  url "https://files.pythonhosted.org/packages/ff/59/d3f6d46aa1fd220d020bdd61e76ca51f6548c6ad6d24ddb614f4037cf49d/numpy-1.17.4.zip"
+  sha256 "f58913e9227400f1395c7b800503ebfdb0772f1c33ff8cb4d6451c06cabdf316"
   head "https://github.com/numpy/numpy.git"
 
   bottle do
