641f94a3516d794f079215997ce6cfff800eed33
BJ Hargrave <bj@bjhargrave.com>
2019-11-19 10:11:49 -0500

    bnd 4.3.1
    Closes #46919.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/bnd.rb
+++ Formula/bnd.rb
@@ -1,8 +1,8 @@
 class Bnd < Formula
   desc "The Swiss Army Knife for OSGi bundles"
   homepage "https://bnd.bndtools.org/"
-  url "https://search.maven.org/remotecontent?filepath=biz/aQute/bnd/biz.aQute.bnd/4.3.0/biz.aQute.bnd-4.3.0.jar"
-  sha256 "5c1d0fc0e1d46e37398581068690c13e1498abed42343aab27b615479ad06026"
+  url "https://search.maven.org/remotecontent?filepath=biz/aQute/bnd/biz.aQute.bnd/4.3.1/biz.aQute.bnd-4.3.1.jar"
+  sha256 "b28e916e58dbc0290b0cd75bde4648eb6bf96e5e9b77eda37eb23b7a1cd1d074"
 
   bottle :unneeded
 
