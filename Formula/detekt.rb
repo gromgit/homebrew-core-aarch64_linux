f0728f99aa40e71edead7e481140a353fc460a0e
wasanx25 <wasanx25@gmail.com>
2019-11-20 23:29:36 +0900

    detekt (1.1.1) new formula

---
+++ Formula/detekt.rb
@@ -0,0 +1,17 @@
+class Detekt < Formula
+  desc "Static code analysis for Kotlin"
+  homepage "https://github.com/arturbosch/detekt"
+  url "https://jcenter.bintray.com/io/gitlab/arturbosch/detekt/detekt-cli/1.1.1/detekt-cli-1.1.1-all.jar"
+  sha256 "8b17c25cd0e9c507773eae0afff97e89c4a1c502b1b1b91c8d55a62032326b3a"
+
+  depends_on :java => "1.8+"
+
+  def install
+    libexec.install "detekt-cli-1.1.1-all.jar"
+    bin.write_jar_script libexec/"detekt-cli-1.1.1-all.jar", "detekt"
+  end
+
+  test do
+    system bin/"detekt", "--help"
+  end
+end
