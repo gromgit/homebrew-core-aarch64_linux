fbd4ef7f5705b94e8e9bb2473ea5c144e04f8d21
Rui Chen <chenrui333@gmail.com>
2019-11-20 16:59:35 -0500

    logstash 7.4.2
    Closes #47000.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/logstash.rb
+++ Formula/logstash.rb
@@ -1,8 +1,8 @@
 class Logstash < Formula
   desc "Tool for managing events and logs"
   homepage "https://www.elastic.co/products/logstash"
-  url "https://artifacts.elastic.co/downloads/logstash/logstash-oss-7.4.1.tar.gz"
-  sha256 "0770b6af348a681e218bfdb78360e55ea042dad9ddcea6f9d9b5021f44e5ef5d"
+  url "https://artifacts.elastic.co/downloads/logstash/logstash-oss-7.4.2.tar.gz"
+  sha256 "107ac2fdf7f836a54d4d6416f181ce99859aae30efa216255d5553e543c684da"
   head "https://github.com/elastic/logstash.git"
 
   bottle :unneeded
