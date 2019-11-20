54edb603e0e2837bfb46fc95c652e6c402b570a0
Igor Kapkov <igasgeek@me.com>
2019-11-20 19:45:37 +1100

    picard-tools 2.21.3
    Closes #46960.
    
    Signed-off-by: Igor Kapkov <igasgeek@me.com>

--- Formula/picard-tools.rb
+++ Formula/picard-tools.rb
@@ -1,8 +1,8 @@
 class PicardTools < Formula
   desc "Tools for manipulating HTS data and formats"
   homepage "https://broadinstitute.github.io/picard/"
-  url "https://github.com/broadinstitute/picard/releases/download/2.21.2/picard.jar"
-  sha256 "89be6d0fec5e4bf0a82db0aa17728e5f15d13456dcf9e7f75dd3af9901895700"
+  url "https://github.com/broadinstitute/picard/releases/download/2.21.3/picard.jar"
+  sha256 "dac2d90d2c82912f6ffbb46239ccd13b2ed9d103f2a3368d4181283f97b02f68"
 
   bottle :unneeded
 
