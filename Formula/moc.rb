ccb31c43f25362fe4da2737d88b64341ea5db736
Zhiming Wang <i@zhimingwang.org>
2019-11-22 04:27:54 +0800

    moc: drop outdated caveats (#47029)
    ncurses is now required.

--- Formula/moc.rb
+++ Formula/moc.rb
@@ -69,12 +69,7 @@ class Moc < Formula
   end
 
   def caveats
-    <<~EOS
-      You must start the jack daemon prior to running mocp.
-      If you need wide-character support in the player, for example
-      with Chinese characters, you can install using
-          --with-ncurses
-    EOS
+    "You must start the jack daemon prior to running mocp."
   end
 
   test do
