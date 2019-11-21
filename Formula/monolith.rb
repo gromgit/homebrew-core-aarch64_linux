859a7d080d5c7df6206afbd8755fa3ea8cb699a8
Dawid Dziurla <dawidd0811@gmail.com>
2019-11-21 20:25:13 +0100

    monolith 2.0.16 (new formula)
    Closes #47033.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

---
+++ Formula/monolith.rb
@@ -0,0 +1,18 @@
+class Monolith < Formula
+  desc "CLI tool for saving complete web pages as a single HTML file"
+  homepage "https://github.com/Y2Z/monolith"
+  url "https://github.com/Y2Z/monolith/archive/v2.0.16.tar.gz"
+  sha256 "b2f5cd1d95d4d2ccd06e7f44e88e43ff0f2ec5d73dabe93ca56bf22656b06a0e"
+
+  depends_on "pkg-config" => :build
+  depends_on "rust" => :build
+  depends_on "openssl@1.1"
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    system bin/"monolith", "https://lyrics.github.io/db/p/portishead/dummy/roads"
+  end
+end
