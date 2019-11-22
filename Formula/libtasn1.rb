cde37f0d0ba4fa91ba29392efbaabf506f3e8c56
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-22 01:59:23 +0000

    libtasn1: update 4.15.0 bottle.

--- Formula/libtasn1.rb
+++ Formula/libtasn1.rb
@@ -7,10 +7,9 @@ class Libtasn1 < Formula
 
   bottle do
     cellar :any
-    sha256 "b5d0fcf3df90b67893ee5b4e56d3abfd80fbc52377385c39d6ed29644c588dc2" => :catalina
-    sha256 "54a78a69c7f881f66511b224b11f84448acfa2dd40428e3a8ce77bd3481f2c98" => :mojave
-    sha256 "570ea4d01af3014b275b5bb68cc4bcf7881c305b7cfce8046d81df64422a7c14" => :high_sierra
-    sha256 "e7a9b411a1d658d8d9401a630471fc3736d0f8564b8620dc50c488a5ee52cd83" => :sierra
+    sha256 "f1988371a72d252ecf69f2c39d61a8ad01cb49dd84ea716f00e38a2a74ff5ee0" => :catalina
+    sha256 "2e13d55178c6a4c057dbef42599700d06d7ef4192500b9254ff5cd99d5a07167" => :mojave
+    sha256 "1cf05b98fcb012d92efad23aaf68ba67e9edd7e15813f2b8826878d15b1ba189" => :high_sierra
   end
 
   def install
