5641cd6f23294d1be100f3c67f883c6a295deb6e
Dawid Dziurla <dawidd0811@gmail.com>
2019-11-23 14:07:05 +0100

    navi 0.15.3
    Closes #47097.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/navi.rb
+++ Formula/navi.rb
@@ -1,8 +1,8 @@
 class Navi < Formula
   desc "Interactive cheatsheet tool for the command-line"
   homepage "https://github.com/denisidoro/navi"
-  url "https://github.com/denisidoro/navi/archive/v0.15.2.tar.gz"
-  sha256 "984b978f7d2803b5d2a66866b1df61021610f641b81c4196e23108c1063d9d09"
+  url "https://github.com/denisidoro/navi/archive/v0.15.3.tar.gz"
+  sha256 "4b03693751a45ee078f62749df857f24fcbae7fe7b1c8a746bd11a79900a7e44"
 
   bottle do
     cellar :any_skip_relocation
