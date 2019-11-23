840f2677d0c1360bba9e6698342b63f1a2bd7554
EricFromCanada <enk3@outlook.com>
2019-11-23 13:44:59 -0500

    proftpd 1.3.6b
    Closes #47106.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/proftpd.rb
+++ Formula/proftpd.rb
@@ -1,28 +1,16 @@
 class Proftpd < Formula
   desc "Highly configurable GPL-licensed FTP server software"
   homepage "http://www.proftpd.org/"
-  url "ftp://ftp.proftpd.org/distrib/source/proftpd-1.3.6.tar.gz"
-  mirror "https://fossies.org/linux/misc/proftpd-1.3.6.tar.gz"
-  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/proftpd/proftpd-1.3.6.tar.gz"
-  sha256 "91ef74b143495d5ff97c4d4770c6804072a8c8eb1ad1ecc8cc541b40e152ecaf"
-  revision 1
+  url "ftp://ftp.proftpd.org/distrib/source/proftpd-1.3.6b.tar.gz"
+  mirror "https://fossies.org/linux/misc/proftpd-1.3.6b.tar.gz"
+  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/proftpd/proftpd-1.3.6b.tar.gz"
+  sha256 "fe5baf6c469a2b0b7f0e2611561b6fd5414300e32a76b96adb2ccfe05b5efb60"
 
   bottle do
     sha256 "200fb8d376b7b9df6528444d4544824561ddb0ad1090acce6f851502b28327ed" => :catalina
     sha256 "d8e913133feffb5e1e669991f345ca04e014501833af51cc67527d99a53b72df" => :mojave
     sha256 "654875de19ba33e14985e88c24fbd54a27877966cb26177bfbffd89286a2f2b2" => :high_sierra
     sha256 "5bbb5075bd0e1091feb88f5a57172d2c5ee1e31caa5c5f6e92009f7da1cffcce" => :sierra
-  end
-
-  # Patch CVE-2019-12815. The fix for this CVE did not result in a new release.
-  # This patch has been backported into the 1.3.6 branch. Remove patch on next release.
-  # Additional information:
-  # https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-12815
-  # http://bugs.proftpd.org/show_bug.cgi?id=4372
-  # https://github.com/proftpd/proftpd/pull/816
-  patch do
-    url "https://github.com/proftpd/proftpd/commit/d19dd64161936d70c0a1544bd2c8e90850f4b7ae.patch?full_index=1"
-    sha256 "c4c54a0dec446ee940dc1267e64d502374e0735355005bbfe67c46bdb12d203a"
   end
 
   def install
