49f538fb88f9a777e94fd137d97716b313242259
chenrui <chenrui333@gmail.com>
2019-11-25 17:50:07 -0500

    orc-tools 1.6.2 (#47151)

--- Formula/orc-tools.rb
+++ Formula/orc-tools.rb
@@ -1,8 +1,8 @@
 class OrcTools < Formula
   desc "ORC java command-line tools and utilities"
   homepage "https://orc.apache.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/apache/orc/orc-tools/1.6.1/orc-tools-1.6.1-uber.jar"
-  sha256 "6604f494904acb31a75b56f27fe23f795a5460b20669ea570ba7ac8a8140528a"
+  url "https://search.maven.org/remotecontent?filepath=org/apache/orc/orc-tools/1.6.2/orc-tools-1.6.2-uber.jar"
+  sha256 "1b0aade855761574e073fdb21d6a58f1d8ba945174730a4200f557527f88b4f0"
 
   bottle :unneeded
 
