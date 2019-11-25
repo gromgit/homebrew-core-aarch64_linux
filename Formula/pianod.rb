038347b9ba34b240484bb9ec6c959fdf25520457
EricFromCanada <enk3@outlook.com>
2019-11-25 16:15:29 -0500

    pianod 298
    Closes #47165.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/pianod.rb
+++ Formula/pianod.rb
@@ -1,9 +1,8 @@
 class Pianod < Formula
   desc "Pandora client with multiple control interfaces"
   homepage "https://deviousfish.com/pianod/"
-  url "https://deviousfish.com/Downloads/pianod/pianod-176.tar.gz"
-  sha256 "4f3be12daef1adb3bcbbcf8ec529abf0ac018e03140be9c5b0f1203d6e1b9bf0"
-  revision 1
+  url "http://deviousfish.com/Downloads/pianod2/Devel/pianod2-298.tar.gz"
+  sha256 "0de916d8df19c045a98fd9b63ec25f157d30235c412689c00c3cead99c64ba76"
 
   bottle do
     cellar :any
@@ -14,14 +13,12 @@ class Pianod < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on "faad2"
-  depends_on "gnutls"
   depends_on "json-c"
   depends_on "libao"
   depends_on "libgcrypt"
-  depends_on "mad"
 
   def install
+    ENV["OBJCXXFLAGS"] = "-std=c++11"
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
