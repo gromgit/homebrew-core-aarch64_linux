c2fc25fbbe6500e13fb9226910786842389fd392
Rui Chen <chenrui333@gmail.com>
2019-11-25 09:13:34 -0500

    grafana 6.5.0
    Closes #47150.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/grafana.rb
+++ Formula/grafana.rb
@@ -1,8 +1,8 @@
 class Grafana < Formula
   desc "Gorgeous metric visualizations and dashboards for timeseries databases"
   homepage "https://grafana.com"
-  url "https://github.com/grafana/grafana/archive/v6.4.4.tar.gz"
-  sha256 "fad788bb4f8c4cef776a906603e5277955b6f63e150d34928980892abc5cc651"
+  url "https://github.com/grafana/grafana/archive/v6.5.0.tar.gz"
+  sha256 "40efcb16882d600f45fd13b0b0e1a8ec172048c17cd5cb77912e1a8946159655"
   head "https://github.com/grafana/grafana.git"
 
   bottle do
@@ -12,7 +12,7 @@ class Grafana < Formula
     sha256 "7dcfbda62970e8effcbe83a3246603ae1d98024f1cfeaeeeff82877d69a9aac8" => :high_sierra
   end
 
-  depends_on "go@1.12" => :build
+  depends_on "go" => :build
   depends_on "node@10" => :build
   depends_on "yarn" => :build
 
