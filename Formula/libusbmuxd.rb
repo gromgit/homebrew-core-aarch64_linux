b40d5e3437fb7a92606e898dc2e5eb9b38b89b45
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-26 20:20:22 +0000

    libusbmuxd: update 2.0.1 bottle.

--- Formula/libusbmuxd.rb
+++ Formula/libusbmuxd.rb
@@ -9,9 +9,9 @@ class Libusbmuxd < Formula
 
   bottle do
     cellar :any
-    sha256 "cd86a52e7d94295f6ddb4f61449f349f22e6ebe0dec876904a0bdde78869035b" => :catalina
-    sha256 "c296286ac58e0afbd167f37b7be5ced50c104252d69878e9a54f33268eb54a54" => :mojave
-    sha256 "c37185be694168115ef33c17794a3a00ef3e917ade673f0a6a7f39fb3a9dd5dd" => :high_sierra
+    sha256 "082a3ef816c55324d9c8c51917b155403211284940b10e822d87cac91991ec0b" => :catalina
+    sha256 "d6d7639277e5590b24713cad79e12c7b9dbb373c6ecf76c97a007041b5307fe2" => :mojave
+    sha256 "dd3ef2fb6343f59a05c16a66f3e839a14b660cec0c11d3d40bb00f9505565a2a" => :high_sierra
   end
 
   head do
