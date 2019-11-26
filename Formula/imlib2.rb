e880efae99e715243d75cfb49eb498462ce5c516
Caleb Xu <calebcenter@live.com>
2019-11-25 20:41:46 -0500

    imlib2 1.6.0
    Closes #47184.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/imlib2.rb
+++ Formula/imlib2.rb
@@ -1,9 +1,8 @@
 class Imlib2 < Formula
   desc "Image loading and rendering library"
   homepage "https://sourceforge.net/projects/enlightenment/"
-  url "https://downloads.sourceforge.net/project/enlightenment/imlib2-src/1.5.1/imlib2-1.5.1.tar.bz2"
-  sha256 "fa4e57452b8843f4a70f70fd435c746ae2ace813250f8c65f977db5d7914baae"
-  revision 1
+  url "https://downloads.sourceforge.net/project/enlightenment/imlib2-src/1.6.0/imlib2-1.6.0.tar.bz2"
+  sha256 "cfc440ddfaed5fc85ba2572ad8d87a87cd77a5bffb33ebca882c42cefcd8691d"
 
   bottle do
     sha256 "d4b9918e30a1126f1b9a1b6372cdd2c013e17355e917ec283ea0f792df2328c8" => :catalina
