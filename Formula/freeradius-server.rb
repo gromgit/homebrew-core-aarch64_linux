15c73a76b781c808f7614742aaf91c5cc5393f2f
Caleb Xu <calebcenter@live.com>
2019-11-25 20:09:29 -0500

    freeradius-server 3.0.20
    Closes #47178.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/freeradius-server.rb
+++ Formula/freeradius-server.rb
@@ -1,9 +1,8 @@
 class FreeradiusServer < Formula
   desc "High-performance and highly configurable RADIUS server"
   homepage "https://freeradius.org/"
-  url "https://github.com/FreeRADIUS/freeradius-server/archive/release_3_0_19.tar.gz"
-  sha256 "34c50ac47a683b13eae1a02f2d0263c0bd51a83f01b99c02c5fe25df07a1ee77"
-  revision 1
+  url "https://github.com/FreeRADIUS/freeradius-server/archive/release_3_0_20.tar.gz"
+  sha256 "8177fe550af6685a040884dbe3df28431bdc5a8d3a48a9f4f88bdb49f2d0e90c"
   head "https://github.com/FreeRADIUS/freeradius-server.git"
 
   bottle do
