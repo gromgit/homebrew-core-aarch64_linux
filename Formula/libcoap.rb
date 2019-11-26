2604776cf1c32dcfec68f6cf10158e73234d24d8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-26 05:35:42 +0000

    libcoap: update 4.2.1 bottle.

--- Formula/libcoap.rb
+++ Formula/libcoap.rb
@@ -6,9 +6,9 @@ class Libcoap < Formula
 
   bottle do
     cellar :any
-    sha256 "e43618d83730577d3a647565e919d8b8db6ccd6d947995b001fc75a6e02655f0" => :mojave
-    sha256 "c39515311e41413a6a569dd3b652cac35c15fe21a51140976e7f3294b14193b7" => :high_sierra
-    sha256 "bfe2db4593b4da91a11ccb7e73e4626a68f5d27463cc8db4149e0da7e42372f8" => :sierra
+    sha256 "344f2a098d9f1767d50135fbf4ae3bdf893a079ebf8a54f248811673fa437e39" => :catalina
+    sha256 "012f1efcb1655479c531df4db98eb481d83971751edffb99b4ca8c50592cd27c" => :mojave
+    sha256 "a68df19a4ca87c677173c14b534848592bb35e46a715ca066bcd114f8c735236" => :high_sierra
   end
 
   depends_on "autoconf" => :build
