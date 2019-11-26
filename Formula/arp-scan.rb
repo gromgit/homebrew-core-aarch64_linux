09d541e8b4e48397aca71d6dcaa0193eeddf6be9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-26 03:28:03 +0000

    arp-scan: update 1.9.7 bottle.

--- Formula/arp-scan.rb
+++ Formula/arp-scan.rb
@@ -6,9 +6,9 @@ class ArpScan < Formula
   head "https://github.com/royhills/arp-scan.git"
 
   bottle do
-    sha256 "6b6621b80ffaccbbbd1f9942e7279ac786aaa258003980c6111e27d49ab199d5" => :catalina
-    sha256 "d17e1ad7dac154571c7e48fe2d9aac7b0555c10a60c1184ffc8b273e5b1238a7" => :mojave
-    sha256 "e6dbdf4a4c2cc9f42012c76192bf562858aa8838895cda7e507543497b253003" => :high_sierra
+    sha256 "763b615392ea20ab1900bbc4a21fb0a9a978bbf50d3bbd8d5ff490437defc6f8" => :catalina
+    sha256 "178196ab4312319611ad02c8e086e56fec2217981f9d91d9e7df8cddfeacda4e" => :mojave
+    sha256 "f72f46496eecff4c1a86dbdbf3a295e195310827ef50cdc0b007bd7b6311495d" => :high_sierra
   end
 
   depends_on "autoconf" => :build
