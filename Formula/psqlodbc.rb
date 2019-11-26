27b3a723178603cb80b2f92bbf85913cf87fdfb0
Nick Form <nick.form@oxfordsemantic.tech>
2019-11-26 11:18:03 +0000

    psqlodbc 12.00.0000
    Closes #47212.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/psqlodbc.rb
+++ Formula/psqlodbc.rb
@@ -1,9 +1,8 @@
 class Psqlodbc < Formula
   desc "Official PostgreSQL ODBC driver"
   homepage "https://odbc.postgresql.org"
-  url "https://ftp.postgresql.org/pub/odbc/versions/src/psqlodbc-10.03.0000.tar.gz"
-  sha256 "0d2ff2d10d9347ef6ce83c7ca24f4cb20b4044eeef9638c8ae3bc8107e9e92f8"
-  revision 1
+  url "https://ftp.postgresql.org/pub/odbc/versions/src/psqlodbc-12.00.0000.tar.gz"
+  sha256 "cf4a763737892e85f403ba581ba0d369666b9641c15e35b345552720e8ce24eb"
 
   bottle do
     cellar :any
