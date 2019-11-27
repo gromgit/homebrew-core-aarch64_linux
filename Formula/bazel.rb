a6b7b10a2fcf5ed5d48d713b3446b911240b2fab
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-27 04:25:19 +0000

    bazel: update 1.2.1 bottle.

--- Formula/bazel.rb
+++ Formula/bazel.rb
@@ -6,9 +6,9 @@ class Bazel < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7d70d6c44909ed659835ac0e9d0da100877fc4fbd41ecee91a2ac33518c9e632" => :catalina
-    sha256 "7d70d6c44909ed659835ac0e9d0da100877fc4fbd41ecee91a2ac33518c9e632" => :mojave
-    sha256 "76b460671dd23a477d57f0f1c9e187faaab504f6160baafc4032bb0e59036304" => :high_sierra
+    sha256 "3083bf1b24db4702c29a13a41174343a14aacc023df0e11ab1b17b822cc5a496" => :catalina
+    sha256 "19eca5d57c8ffc7bdf19ebcc1376b40a7466253a48f3209cbb475931e38a5b1d" => :mojave
+    sha256 "7c62fff1f7d36dd22a8c5e56b73f639c873ed91062e871865eb7f7084029987a" => :high_sierra
   end
 
   depends_on "python" => :build
