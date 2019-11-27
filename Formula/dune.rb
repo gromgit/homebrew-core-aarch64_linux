b4b7cce8f7bf06ade11a1b2fab0a3087f62fcc1d
Rudi Grinberg <me@rgrinberg.com>
2019-11-27 14:29:03 +0700

    dune 2.0.0 (new formula)
    Closes #47248.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

---
+++ Formula/dune.rb
@@ -0,0 +1,26 @@
+class Dune < Formula
+  desc "Composable build system for OCaml"
+  homepage "https://dune.build/"
+  url "https://github.com/ocaml/dune/releases/download/2.0.0/dune-2.0.0.tbz"
+  sha256 "9f993b8263775a2236fd4308e2fe2413d3ee925a52858e4d9e18e7f170c4b3f6"
+  head "https://github.com/ocaml/dune.git"
+
+  depends_on "ocaml" => [:build, :test]
+
+  def install
+    system "ocaml", "configure.ml"
+    system "ocaml", "bootstrap.ml"
+    system "./dune.exe", "build", "-p", "dune", "--profile", "dune-bootstrap"
+    bin.install "_build/default/bin/dune.exe"
+    mv bin/"dune.exe", bin/"dune"
+  end
+
+  test do
+    contents = "bar"
+    target_fname = "foo.txt"
+    (testpath/"dune").write("(rule (with-stdout-to #{target_fname} (echo #{contents})))")
+    system bin/"dune", "build", "foo.txt", "--root", "."
+    output = File.read(testpath/"_build/default/#{target_fname}")
+    assert_match contents, output
+  end
+end
