d4559223f0ace903f13beab74658b69b36a91225
Rui Chen <chenrui333@gmail.com>
2019-11-29 16:50:18 -0500

    wiki 1.4.1
    Closes #47338.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/wiki.rb
+++ Formula/wiki.rb
@@ -1,10 +1,8 @@
-require "language/go"
-
 class Wiki < Formula
   desc "Fetch summaries from MediaWiki wikis, like Wikipedia"
   homepage "https://github.com/walle/wiki"
-  url "https://github.com/walle/wiki/archive/1.4.0.tar.gz"
-  sha256 "b9adb27485feba68574e3abf5564577f2fcec1bf2176fc8f80b09b6f8ca6ffff"
+  url "https://github.com/walle/wiki/archive/v1.4.1.tar.gz"
+  sha256 "529c6a58b3b5c5eb3faab07f2bf752155868b912e4f753e432d14040ff4f4262"
 
   bottle do
     cellar :any_skip_relocation
@@ -18,26 +16,13 @@ class Wiki < Formula
 
   depends_on "go" => :build
 
-  go_resource "github.com/mattn/go-colorable" do
-    url "https://github.com/mattn/go-colorable.git",
-        :revision => "40e4aedc8fabf8c23e040057540867186712faa5"
-  end
-
   def install
-    ENV["GOPATH"] = buildpath
-    wikipath = buildpath/"src/github.com/walle/wiki"
-    wikipath.install Dir["{*,.git}"]
-    Language::Go.stage_deps resources, buildpath/"src"
-
-    cd "src/github.com/walle/wiki" do
-      system "go", "build", "-o", "build/wiki", "cmd/wiki/main.go"
-      bin.install "build/wiki"
-      man1.install "_doc/wiki.1"
-      prefix.install_metafiles
-    end
+    system "go", "build", "-o", bin/"wiki", "cmd/wiki/main.go"
   end
 
   test do
+    assert_match version.to_s, shell_output("#{bin}/wiki --version")
+
     assert_match "Read more: https://en.wikipedia.org/wiki/Go", shell_output("#{bin}/wiki golang")
   end
 end
