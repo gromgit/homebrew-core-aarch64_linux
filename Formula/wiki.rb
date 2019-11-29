602cf5a0faff26a70b1c50499d77cf14a10922e0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-29 23:20:00 +0000

    wiki: update 1.4.1 bottle.

--- Formula/wiki.rb
+++ Formula/wiki.rb
@@ -6,12 +6,9 @@ class Wiki < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "97fff58adf33b41ca00e4112ef099a76c333cf2fb501a85ce155a19284b47387" => :catalina
-    sha256 "c2356b595d2c4b9a6ade9f569b0186de56c016121cb28c5e525353ad3b74c7c0" => :mojave
-    sha256 "b659c9bc28a5468e61a7af026f20fe8a9233b852e1165e5d900755db68fa85f6" => :high_sierra
-    sha256 "0f3302cb5063486d6cb1beb1a25c771b50c03f0318f05e7d3520b1a2d05a445b" => :sierra
-    sha256 "6e6d9036b7943ef08cbf92c5aec72b214599aa83bd0a038f4d7a0d19a90a70b0" => :el_capitan
-    sha256 "b7f224cc011a63259a7ef24b2709a4fb3ba053b15f1861a6c3f03d29925251f8" => :yosemite
+    sha256 "ff424f6afbc0d2baab91cee289157d9c90623fa19b7d51574b75df455da76cd6" => :catalina
+    sha256 "316687b381ca23ee0e81eb6e396d2c8c21a5eeaf05a9219ec56dd0024a8d9722" => :mojave
+    sha256 "bd1b52730bbf5bc503d3fece003b069e248261616d9d02767ef019d87659bdd8" => :high_sierra
   end
 
   depends_on "go" => :build
