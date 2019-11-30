dc2b26eb0eb4385e787399c99cb17ac6b52795b2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-30 10:57:48 +0000

    powerline-go: add 1.13.0 bottle.

--- Formula/powerline-go.rb
+++ Formula/powerline-go.rb
@@ -4,6 +4,13 @@ class PowerlineGo < Formula
   url "https://github.com/justjanne/powerline-go/archive/v1.13.0.tar.gz"
   sha256 "0c0d8a2aca578391edc0120f6cbb61f9ef5571190c07a978932348b0489d00ea"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a9b6be73fc466b480d6f4fdeb318dec5ce57e5a9d693a57674ca5aa5cd04a7fb" => :catalina
+    sha256 "cd2f20c1bd8504a5eb449c482b9d34ada5a5d59c1ffb17173e18c7afbebb3be8" => :mojave
+    sha256 "d67e92a591c74edc03a01158672801a8844256c3a078de7c93c317fadcd16d39" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
