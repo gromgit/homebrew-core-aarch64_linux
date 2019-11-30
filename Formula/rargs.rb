5c1b575d5703fae3ab8440b84a76a069de27e812
Rui Chen <chenrui333@gmail.com>
2019-11-30 11:10:34 -0500

    rargs 0.2.3
    Closes #47346.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/rargs.rb
+++ Formula/rargs.rb
@@ -1,8 +1,8 @@
 class Rargs < Formula
   desc "Util like xargs + awk with pattern matching support"
   homepage "https://github.com/lotabout/rargs"
-  url "https://github.com/lotabout/rargs/archive/v0.2.2.tar.gz"
-  sha256 "ac6cf3a31ff5b55f86487fa3d3266edf8f562cc6b548d6e636daf373534388ad"
+  url "https://github.com/lotabout/rargs/archive/v0.2.3.tar.gz"
+  sha256 "b33205cab0d410cd7f25e3bcb4efbfec48de76a8ad9c01ee4c286e407552f982"
 
   bottle do
     cellar :any_skip_relocation
