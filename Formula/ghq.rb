5414e8a3e2fddf29ba858e64e4498fe3a0d2dfc0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-01 20:21:31 +0000

    ghq: update 0.13.0 bottle.

--- Formula/ghq.rb
+++ Formula/ghq.rb
@@ -8,9 +8,9 @@ class Ghq < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c1049d2ebbe1deb69cdb58f7e3ea7d63a36f6128135962cd1da63aadf7d68db7" => :catalina
-    sha256 "78d83748b4a7d89ea17fba47f29d36e554c8efe4b5b85a0f64c90cb2bc1b8fc9" => :mojave
-    sha256 "30c1b6f14b0969a75de43ef6679df89247e8742029028fdb8cc24da1977b553a" => :high_sierra
+    sha256 "57d545df8a37b72bd5fb83ac25b19c1c76251242871764dcbdc19d8c02aeca93" => :catalina
+    sha256 "153f6cfba03d83a63ddaf1fc74a85f54b346bc59de5c915d830120745dc65491" => :mojave
+    sha256 "dade6cea71dcb604626ea931a1decbb156216e396801cd57e3fc808bddabc15e" => :high_sierra
   end
 
   depends_on "go" => :build
