7e54916d06f24d33fd7fdadfb71691ba532f31de
Alexander Bayandin <a.bayandin@gmail.com>
2019-12-02 19:25:59 +0000

    gobject-introspection: revision bump for python@3.8

--- Formula/gobject-introspection.rb
+++ Formula/gobject-introspection.rb
@@ -3,6 +3,7 @@ class GobjectIntrospection < Formula
   homepage "https://wiki.gnome.org/Projects/GObjectIntrospection"
   url "https://download.gnome.org/sources/gobject-introspection/1.64/gobject-introspection-1.64.0.tar.xz"
   sha256 "eac05a63091c81adfdc8ef34820bcc7e7778c5b9e34734d344fc9e69ddf4fc82"
+  revision 1
 
   bottle do
     sha256 "f081afaeb6049797416d98e59cc32b8e71106b810f019070e1d976bdd117a497" => :catalina
@@ -17,7 +18,7 @@ class GobjectIntrospection < Formula
   depends_on "glib"
   depends_on "libffi"
   depends_on "pkg-config"
-  depends_on "python"
+  depends_on "python@3.8"
 
   resource "tutorial" do
     url "https://gist.github.com/7a0023656ccfe309337a.git",
@@ -25,6 +26,8 @@ class GobjectIntrospection < Formula
   end
 
   def install
+    Language::Python.rewrite_python_shebang(Formula["python@3.8"].opt_bin/"python3")
+
     ENV["GI_SCANNER_DISABLE_CACHE"] = "true"
     inreplace "giscanner/transformer.py", "/usr/share", "#{HOMEBREW_PREFIX}/share"
     inreplace "meson.build",
@@ -33,11 +36,12 @@ class GobjectIntrospection < Formula
 
     args = %W[
       --prefix=#{prefix}
-      -Dpython=#{Formula["python"].opt_bin}/python3
+      -Dpython=#{Formula["python@3.8"].opt_bin}/python3
     ]
 
     mkdir "build" do
       system "meson", *args, ".."
+      Language::Python.rewrite_python_shebang(Formula["python@3.8"].opt_bin/"python3")
       system "ninja", "-v"
       system "ninja", "install", "-v"
     end
