1510ed8f7bc1693c599cac5f308467be73b1784e
Alexander Bayandin <a.bayandin@gmail.com>
2019-12-02 19:26:00 +0000

    mpi4py: revision bump for python@3.8

--- Formula/mpi4py.rb
+++ Formula/mpi4py.rb
@@ -3,6 +3,7 @@ class Mpi4py < Formula
   homepage "https://mpi4py.readthedocs.io"
   url "https://bitbucket.org/mpi4py/mpi4py/downloads/mpi4py-3.0.3.tar.gz"
   sha256 "012d716c8b9ed1e513fcc4b18e5af16a8791f51e6d1716baccf988ad355c5a1f"
+  revision 1
 
   bottle do
     cellar :any
@@ -13,28 +14,28 @@ class Mpi4py < Formula
 
   depends_on "cython" => :build
   depends_on "open-mpi"
-  depends_on "python"
+  depends_on "python@3.8"
 
   def install
-    system "#{Formula["python"].opt_bin}/python3",
+    system "#{Formula["python@3.8"].opt_bin}/python3",
            *Language::Python.setup_install_args(libexec)
 
-    system "python3", "setup.py",
+    system Formula["python@3.8"].bin/"python3", "setup.py",
       "build", "--mpicc=mpicc -shared", "--parallel=#{ENV.make_jobs}",
       "install", "--prefix=#{prefix}",
       "--single-version-externally-managed", "--record=installed.txt"
   end
 
   test do
-    system "#{Formula["python"].opt_bin}/python3",
+    system Formula["python@3.8"].opt_bin/"python3",
            "-c", "import mpi4py"
-    system "#{Formula["python"].opt_bin}/python3",
+    system Formula["python@3.8"].opt_bin/"python3",
            "-c", "import mpi4py.MPI"
-    system "#{Formula["python"].opt_bin}/python3",
+    system Formula["python@3.8"].opt_bin/"python3",
            "-c", "import mpi4py.futures"
-    system "mpiexec", "-n", "4", "#{Formula["python"].opt_bin}/python3",
+    system "mpiexec", "-n", "4", Formula["python@3.8"].opt_bin/"python3",
            "-m", "mpi4py.run", "-m", "mpi4py.bench", "helloworld"
-    system "mpiexec", "-n", "4", "#{Formula["python"].opt_bin}/python3",
+    system "mpiexec", "-n", "4", Formula["python@3.8"].opt_bin/"python3",
            "-m", "mpi4py.run", "-m", "mpi4py.bench", "ringtest",
            "-l", "10", "-n", "1024"
   end
