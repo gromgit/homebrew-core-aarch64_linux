93562a36175d6b2685e82860b5b85a10188134ce
Alexander Bayandin <a.bayandin@gmail.com>
2019-12-02 19:25:59 +0000

    gtk-doc: revision bump for python@3.8

--- Formula/gtk-doc.rb
+++ Formula/gtk-doc.rb
@@ -3,6 +3,7 @@ class GtkDoc < Formula
   homepage "https://gitlab.gnome.org/GNOME/gtk-doc"
   url "https://download.gnome.org/sources/gtk-doc/1.32/gtk-doc-1.32.tar.xz"
   sha256 "de0ef034fb17cb21ab0c635ec730d19746bce52984a6706e7bbec6fb5e0b907c"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -18,7 +19,7 @@ class GtkDoc < Formula
   depends_on "docbook-xsl"
   depends_on "gettext"
   depends_on "libxml2"
-  depends_on "python"
+  depends_on "python@3.8"
   depends_on "source-highlight"
 
   uses_from_macos "libxslt"
@@ -29,10 +30,10 @@ class GtkDoc < Formula
   end
 
   def install
-    xy = Language::Python.major_minor_version "python3"
+    xy = Language::Python.major_minor_version Formula["python@3.8"].opt_bin/"python3"
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
     resource("Pygments").stage do
-      system "python3", *Language::Python.setup_install_args(libexec/"vendor")
+      system Formula["python@3.8"].opt_bin/"python3", *Language::Python.setup_install_args(libexec/"vendor")
     end
 
     system "./configure", "--disable-debug",
