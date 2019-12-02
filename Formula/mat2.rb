29c79232782bccbe71dfdcaacc3642c3cdbcc1b7
Alexander Bayandin <a.bayandin@gmail.com>
2019-12-02 19:26:00 +0000

    mat2: revision bump for python@3.8

--- Formula/mat2.rb
+++ Formula/mat2.rb
@@ -3,6 +3,7 @@ class Mat2 < Formula
   homepage "https://0xacab.org/jvoisin/mat2"
   url "https://0xacab.org/jvoisin/mat2/-/archive/0.10.1/mat2-0.10.1.tar.gz"
   sha256 "5ed3d9c945d1475479a42e87879821440c66c2881db157aa480ccdcefc13d202"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -18,7 +19,7 @@ class Mat2 < Formula
   depends_on "poppler"
   depends_on "py3cairo"
   depends_on "pygobject3"
-  depends_on "python"
+  depends_on "python@3.8"
 
   resource "mutagen" do
     url "https://files.pythonhosted.org/packages/30/4c/5ad1a6e1ccbcfaf6462db727989c302d9d721beedd9b09c11e6f0c7065b0/mutagen-1.42.0.tar.gz"
@@ -29,7 +30,7 @@ class Mat2 < Formula
     inreplace "libmat2/exiftool.py", "/usr/bin/exiftool", "#{Formula["exiftool"].opt_bin}/exiftool"
     inreplace "libmat2/video.py", "/usr/bin/ffmpeg", "#{Formula["ffmpeg"].opt_bin}/ffmpeg"
 
-    version = Language::Python.major_minor_version("python3")
+    version = Language::Python.major_minor_version Formula["python@3.8"].bin/"python3"
     pygobject3 = Formula["pygobject3"]
     ENV["PYTHONPATH"] = lib/"python#{version}/site-packages"
     ENV.append_path "PYTHONPATH", pygobject3.opt_lib+"python#{version}/site-packages"
@@ -37,11 +38,11 @@ class Mat2 < Formula
 
     resources.each do |r|
       r.stage do
-        system "python3", *Language::Python.setup_install_args(libexec/"vendor")
+        system Formula["python@3.8"].bin/"python3", *Language::Python.setup_install_args(libexec/"vendor")
       end
     end
 
-    system "python3", *Language::Python.setup_install_args(prefix)
+    system Formula["python@3.8"].bin/"python3", *Language::Python.setup_install_args(prefix)
 
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
