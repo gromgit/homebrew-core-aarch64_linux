b040d1aae5605136c887a9c5083d8c7e0781d6b1
Alexander Bayandin <a.bayandin@gmail.com>
2019-12-02 19:25:58 +0000

    aubio: revision bump for python@3.8

--- Formula/aubio.rb
+++ Formula/aubio.rb
@@ -3,6 +3,7 @@ class Aubio < Formula
   homepage "https://aubio.org/"
   url "https://aubio.org/pub/aubio-0.4.9.tar.bz2"
   sha256 "d48282ae4dab83b3dc94c16cf011bcb63835c1c02b515490e1883049c3d1f3da"
+  revision 1
 
   bottle do
     cellar :any
@@ -15,17 +16,17 @@ class Aubio < Formula
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "numpy"
-  depends_on "python"
+  depends_on "python@3.8"
 
   def install
     # Needed due to issue with recent clang (-fno-fused-madd))
     ENV.refurbish_args
 
-    system "python3", "./waf", "configure", "--prefix=#{prefix}"
-    system "python3", "./waf", "build"
-    system "python3", "./waf", "install"
+    system Formula["python@3.8"].opt_bin/"python3", "./waf", "configure", "--prefix=#{prefix}"
+    system Formula["python@3.8"].opt_bin/"python3", "./waf", "build"
+    system Formula["python@3.8"].opt_bin/"python3", "./waf", "install"
 
-    system "python3", *Language::Python.setup_install_args(prefix)
+    system Formula["python@3.8"].opt_bin/"python3", *Language::Python.setup_install_args(prefix)
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
