9696b41b57fd8b90c2d27642dc825d6f68ca2b74
Alexander Bayandin <a.bayandin@gmail.com>
2019-12-02 19:26:00 +0000

    numpy: revision bump for python@3.8

--- Formula/numpy.rb
+++ Formula/numpy.rb
@@ -3,6 +3,7 @@ class Numpy < Formula
   homepage "https://www.numpy.org/"
   url "https://files.pythonhosted.org/packages/84/1e/ff467ac56bfeaea51d4a2e72d315c1fe440b20192fea7e460f0f248acac8/numpy-1.18.2.zip"
   sha256 "e7894793e6e8540dbeac77c87b489e331947813511108ae097f1715c018b8f3d"
+  revision 1
   head "https://github.com/numpy/numpy.git"
 
   bottle do
@@ -15,7 +16,7 @@ class Numpy < Formula
   depends_on "cython" => :build
   depends_on "gcc" => :build # for gfortran
   depends_on "openblas"
-  depends_on "python"
+  depends_on "python@3.8"
 
   def install
     openblas = Formula["openblas"].opt_prefix
@@ -31,17 +32,17 @@ class Numpy < Formula
 
     Pathname("site.cfg").write config
 
-    version = Language::Python.major_minor_version "python3"
+    version = Language::Python.major_minor_version Formula["python@3.8"].opt_bin/"python3"
     ENV.prepend_create_path "PYTHONPATH", Formula["cython"].opt_libexec/"lib/python#{version}/site-packages"
 
-    system "python3", "setup.py",
+    system Formula["python@3.8"].opt_bin/"python3", "setup.py",
       "build", "--fcompiler=gnu95", "--parallel=#{ENV.make_jobs}",
       "install", "--prefix=#{prefix}",
       "--single-version-externally-managed", "--record=installed.txt"
   end
 
   test do
-    system "python3", "-c", <<~EOS
+    system Formula["python@3.8"].opt_bin/"python3", "-c", <<~EOS
       import numpy as np
       t = np.ones((3,3), int)
       assert t.sum() == 9
