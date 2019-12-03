295132688999dfff5674baafe558f56c94bf08d7
Caleb Xu <calebcenter@live.com>
2019-12-03 15:42:35 -0500

    laszip 3.4.3
    Closes #47458.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/laszip.rb
+++ Formula/laszip.rb
@@ -1,8 +1,8 @@
 class Laszip < Formula
   desc "Lossless LiDAR compression"
   homepage "https://laszip.org/"
-  url "https://github.com/LASzip/LASzip/releases/download/3.4.1/laszip-src-3.4.1.tar.gz"
-  sha256 "5d9b0ffaf8b7319c2fa216da3f3f878bb8f4e5b4b14d2c154d441a351da2be37"
+  url "https://github.com/LASzip/LASzip/releases/download/3.4.3/laszip-src-3.4.3.tar.gz"
+  sha256 "53f546a7f06fc969b38d1d71cceb1862b4fc2c4a0965191a0eee81a57c7b373d"
   head "https://github.com/LASzip/LASzip.git"
 
   bottle do
