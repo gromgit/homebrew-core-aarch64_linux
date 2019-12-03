ab28f8aa54e9421f7f43dbad8ef6338e674063a1
EricFromCanada <enk3@outlook.com>
2019-12-03 11:14:20 -0500

    simutrans 121.0
    Closes #47443.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/simutrans.rb
+++ Formula/simutrans.rb
@@ -1,9 +1,9 @@
 class Simutrans < Formula
   desc "Transport simulator"
   homepage "https://www.simutrans.com/"
-  url "https://downloads.sourceforge.net/project/simutrans/simutrans/120-4-1/simutrans-src-120-4-1.zip"
-  version "120.4.1"
-  sha256 "2cee0d067b3b72fa3a8b4ff31ad2bf5fc77521e7ba8cf9aa10e07e56b7dc877b"
+  url "https://downloads.sourceforge.net/project/simutrans/simutrans/121-0/simutrans-src-121-0.zip"
+  version "121.0"
+  sha256 "69fd95989761a013729106b48135f772f59126398cd93ada072f963c4d1e86b8"
   head "https://github.com/aburch/simutrans.git"
 
   bottle do
@@ -14,23 +14,23 @@ class Simutrans < Formula
   end
 
   depends_on "libpng"
-
   depends_on "sdl2"
 
   resource "pak64" do
-    url "https://downloads.sourceforge.net/project/simutrans/pak64/120-4-1/simupak64-120-4-1.zip"
-    sha256 "fb46cde683ee1c0d10fb18bb2efc767583f1e9e76776a5f93d46a17d699aa69f"
+    url "https://downloads.sourceforge.net/project/simutrans/pak64/121-0/simupak64-121-0.zip"
+    sha256 "acd44278650944cd197ef8d5da7106f3d26c5fd3c3f2586c83a1c286e02c63cc"
   end
 
   resource "text" do
     url "https://simutrans-germany.com/translator/data/tab/language_pack-Base+texts.zip"
-    sha256 "2a3cee3ef7cfbed31236c920502369446100c10c9304a476b2a885b4daae426a"
+    sha256 "4592e14f0e32b044c613d2a51f7783a242ed751be67fdb65c46e136116d76d96"
   end
 
   def install
     args = %w[
       BACKEND=sdl2
       COLOUR_DEPTH=16
+      MULTI_THREAD=1
       OSTYPE=mac
     ]
     args << "AV_FOUNDATION=1" if MacOS.version >= :sierra
