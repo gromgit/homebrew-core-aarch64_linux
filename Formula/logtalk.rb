029a834c4c682cdaefabc091e0d0e353cf323050
Paulo Moura <pmoura@logtalk.org>
2019-12-03 14:15:24 +0000

    logtalk 3.33.0
    Closes #47437.
    
    Signed-off-by: Caleb Xu <calebcenter@live.com>

--- Formula/logtalk.rb
+++ Formula/logtalk.rb
@@ -1,9 +1,9 @@
 class Logtalk < Formula
   desc "Object-oriented logic programming language"
   homepage "https://logtalk.org/"
-  url "https://github.com/LogtalkDotOrg/logtalk3/archive/lgt3320stable.tar.gz"
-  version "3.32.0"
-  sha256 "c9cbbde44e250f31d81b2fb7e160da4f45c8d232b328f3729b9c54d76a693d2b"
+  url "https://github.com/LogtalkDotOrg/logtalk3/archive/lgt3330stable.tar.gz"
+  version "3.33.0"
+  sha256 "9bb7f83410e238416fa8598cc7e1191b45f31c1ca5cb939bc8de0ce226175b49"
 
   bottle do
     cellar :any_skip_relocation
