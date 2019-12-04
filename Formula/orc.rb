df7bdec3bf05a359433ff558e5541ac94c826850
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-04 03:18:15 +0000

    orc: update 0.4.31 bottle.

--- Formula/orc.rb
+++ Formula/orc.rb
@@ -6,10 +6,9 @@ class Orc < Formula
 
   bottle do
     cellar :any
-    sha256 "9509aa50e56e4cb34e5852fe69a1137cf231ef1b0f3412e0f6af7cb6af69c97a" => :catalina
-    sha256 "d2f7fdd730740d23c0a3da22b3fac412b68974a0e6c4cc1f5b55abf8f075d374" => :mojave
-    sha256 "a04f6a05f82c5dc8ba526992d294fccb9d3e1206fd21e9e870f04f226f88f86e" => :high_sierra
-    sha256 "9a84e0cc0f7268c805ab3440302e40f5b4c44f48f47b284e23c75f1e7014c206" => :sierra
+    sha256 "c4a11fbf1e2d645b0bbcbabc467c6f7fe604282833ece90264b063806a1e4909" => :catalina
+    sha256 "b9c58730d763ca611867504ff7005245e95ca435b027d85ff7f7471dc7431b59" => :mojave
+    sha256 "b732bc9e7fa9825222b0dda3dfdbe7f38cf45aeb46c239f432fc646d98079e76" => :high_sierra
   end
 
   depends_on "meson" => :build
