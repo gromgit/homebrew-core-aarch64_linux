58979b0ee858d23bb0ca1151e45da7b7a8322d5a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-04 17:35:06 +0000

    rabbitmq-c: update 0.10.0 bottle.

--- Formula/rabbitmq-c.rb
+++ Formula/rabbitmq-c.rb
@@ -7,10 +7,9 @@ class RabbitmqC < Formula
 
   bottle do
     cellar :any
-    sha256 "704101f05c0bf2ee9b10b6c72918753dde7fa5983e139ba0e2275917af1e3765" => :catalina
-    sha256 "93a530bf4844d0d3c653826359aa77bcb67691a0424b8ca1c6b20bec29733821" => :mojave
-    sha256 "af732c62d886aa4ec4c2e73287d06e2ed422736de17637395c0a44474942fca1" => :high_sierra
-    sha256 "2ed895d32e8a07f12b3583541f9fc8d59935e5aa767797f56711e67cdcfbd889" => :sierra
+    sha256 "6434a9100eeadfcd57d35fd31d1863d75b71ec163a3a1be29076c217712bda55" => :catalina
+    sha256 "5f99c633ece8efad2ef2085955b22d0558d8fc2dedcac67b3ba8b58a2640c2c3" => :mojave
+    sha256 "53d883744a185e5daab18c8bd18fd70fed56dd009cc507356f128663947c2453" => :high_sierra
   end
 
   depends_on "cmake" => :build
