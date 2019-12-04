def2022649a5ae48cd60bedde1305a284edf9534
EricFromCanada <enk3@outlook.com>
2019-12-04 16:19:34 -0500

    dartsim: revision bump
    Closes #47495.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/dartsim.rb
+++ Formula/dartsim.rb
@@ -3,7 +3,7 @@ class Dartsim < Formula
   homepage "https://dartsim.github.io/"
   url "https://github.com/dartsim/dart/archive/v6.9.2.tar.gz"
   sha256 "7d46d23c04d74d3b78331f9fa7deb5ab32fd4b0c03b93548cd84a2d67771d816"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "0f342bef927315c70aa47218abdd5db8f34de241cffd709781e82cfc8bdc85a1" => :catalina
