dcf615330f944be2e7c2bfcd00b2482af0b7f48b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-04 18:26:31 +0000

    kind: update 0.6.1 bottle.

--- Formula/kind.rb
+++ Formula/kind.rb
@@ -7,9 +7,9 @@ class Kind < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "90b44f5c4b4b11526371b974d86c32cf871e7b9d4042fde8e717befa33c3da07" => :catalina
-    sha256 "81100ccfcfda9542f639e8b0ab55761085e539fbfb86c9d459cc22bec6981fd0" => :mojave
-    sha256 "32d4b9e2e0b243668e10c365c18e85b3f01255d7fb06f5ca317eeb7ee3589d06" => :high_sierra
+    sha256 "a27a7b739d4de87af3eaa80784cf17128bb843d392004ac569b26e4d7d1531cb" => :catalina
+    sha256 "0dd0b6740ee5ed21707e85effb654d9d804f3dc3df3d976c54c581e581466ee6" => :mojave
+    sha256 "950c7f642a7763d35411cd31814a3f8f382d2b0f0ae5b83f4602953ae6f84fb9" => :high_sierra
   end
 
   depends_on "go" => :build
