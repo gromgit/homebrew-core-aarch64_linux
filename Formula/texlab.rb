1f5afdebb5ec5dcf3d6a8f44bd3371d27c8cc36b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-05 17:01:30 +0000

    texlab: add 1.8.0 bottle.

--- Formula/texlab.rb
+++ Formula/texlab.rb
@@ -5,6 +5,13 @@ class Texlab < Formula
   sha256 "af644d2555c3852513135e87dc6f9bc8b5ee789a4f1c151f4478d108fa007c49"
   head "https://github.com/latex-lsp/texlab.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b21e7ccb8ba6e2e714543d39e9b1c5dfaacf2fdc0fb5b523900464520762ac76" => :catalina
+    sha256 "8aaf1f88ec5f3c1a7dba5934cdf4ae3aaab5de0cc406039ba7af151ef146ca9a" => :mojave
+    sha256 "8ff6bc889d612a421272f23df4c813b4e1e9411bd88e97bc653a2c06841c87ae" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
