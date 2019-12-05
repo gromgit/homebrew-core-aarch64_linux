d8d4f8e0bd0427c1403c6d3d837f05fb853332c5
Caleb Xu <calebcenter@live.com>
2019-12-05 18:51:51 -0500

    nano 4.6
    Closes #47543.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/nano.rb
+++ Formula/nano.rb
@@ -1,8 +1,8 @@
 class Nano < Formula
   desc "Free (GNU) replacement for the Pico text editor"
   homepage "https://www.nano-editor.org/"
-  url "https://www.nano-editor.org/dist/v4/nano-4.5.tar.gz"
-  sha256 "7abb2c2eff69472520becbde4d9421349639cc1cc47f4756ad36c3a66836e52b"
+  url "https://www.nano-editor.org/dist/v4/nano-4.6.tar.gz"
+  sha256 "81007141b3cb5d06d5b7283845f0b853cdb9c9a5f4feaaf223d5d2bfbb07bfb7"
 
   bottle do
     sha256 "c0de36667c8625b139fb70d88cd2e65b9c35d37458e74106728cd19901d13da1" => :catalina
@@ -13,6 +13,14 @@ class Nano < Formula
   depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "ncurses"
+
+  # Reported upstream at https://savannah.gnu.org/bugs/index.php?57367
+  # Fixed upstream at https://git.savannah.gnu.org/cgit/nano.git/commit/?id=f516cddce749c3bf938271ef3182b9169ac8cbcc
+  # Remove patch in next update.
+  patch do
+    url "https://savannah.gnu.org/file/0001-build-fix-compilation-on-macOS-where-st_mtim-is-unkn.patch?file_id=48016"
+    sha256 "c30e10bb5c5df739c51e7a91c66bf8ebef52709e463c8717f292292daf5aebc9"
+  end
 
   def install
     system "./configure", "--disable-debug",
