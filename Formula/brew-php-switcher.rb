601282a18468ffe782d7397a07679f7dd4d91adb
Andrew Miller <andrew.miller@rakuten.com>
2019-12-05 12:29:10 +0900

    brew-php-switcher 2.2 (#47505)

--- Formula/brew-php-switcher.rb
+++ Formula/brew-php-switcher.rb
@@ -1,8 +1,8 @@
 class BrewPhpSwitcher < Formula
   desc "Switch Apache / Valet / CLI configs between PHP versions"
   homepage "https://github.com/philcook/php-switcher"
-  url "https://github.com/philcook/brew-php-switcher/archive/v2.1.1.tar.gz"
-  sha256 "5a812761d1c0a89f243533a3bad31eb2c6f0b0c9ad93b3ae3307dbd99e890464"
+  url "https://github.com/philcook/brew-php-switcher/archive/v2.2.tar.gz"
+  sha256 "505bc355cd725cacd048e44b60eadb5ad88c31f3c9d311e4b250c274aa78c14c"
   head "https://github.com/philcook/brew-php-switcher.git"
 
   bottle :unneeded
