b8d20f3a57c52906d21f4abf5e4f99791fd0bd55
Mislav Marohnić <mislav@github.com>
2019-12-05 11:32:46 +0100

    ruby-build 20191205
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #47516.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/ruby-build.rb
+++ Formula/ruby-build.rb
@@ -1,8 +1,8 @@
 class RubyBuild < Formula
   desc "Install various Ruby versions and implementations"
   homepage "https://github.com/rbenv/ruby-build"
-  url "https://github.com/rbenv/ruby-build/archive/v20191124.tar.gz"
-  sha256 "aa841de5d107e4b8387cd674834ea4a5f616cd441eb447b23cf566c61e429c48"
+  url "https://github.com/rbenv/ruby-build/archive/v20191205.tar.gz"
+  sha256 "d8ffe806a215b3afacead72e766f293ce380c78a143911b84cdb5f33e20a5284"
   head "https://github.com/rbenv/ruby-build.git"
 
   bottle :unneeded
