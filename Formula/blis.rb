a1045869520ac785e11c6c4c1f791066687a6c10
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-12-06 01:30:35 -0500

    blis 0.6.0 (new formula)
    Closes #47585.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

---
+++ Formula/blis.rb
@@ -0,0 +1,39 @@
+class Blis < Formula
+  desc "BLAS-like Library Instantiation Software Framework"
+  homepage "https://github.com/flame/blis"
+  url "https://github.com/flame/blis/archive/0.6.0.tar.gz"
+  sha256 "ad5765cc3f492d0c663f494850dafc4d72f901c332eb442f404814ff2995e5a9"
+  head "https://github.com/flame/blis.git"
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--enable-cblas", "auto"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <stdlib.h>
+      #include <math.h>
+      #include "blis/blis.h"
+
+      int main(void) {
+        int i;
+        double A[6] = {1.0, 2.0, 1.0, -3.0, 4.0, -1.0};
+        double B[6] = {1.0, 2.0, 1.0, -3.0, 4.0, -1.0};
+        double C[9] = {.5, .5, .5, .5, .5, .5, .5, .5, .5};
+        cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans,
+                    3, 3, 2, 1, A, 3, B, 3, 2, C, 3);
+        for (i = 0; i < 9; i++)
+          printf("%lf ", C[i]);
+        printf("\\n");
+        if (fabs(C[0]-11) > 1.e-5) abort();
+        if (fabs(C[4]-21) > 1.e-5) abort();
+        return 0;
+      }
+    EOS
+    system ENV.cc, "-o", "test", "test.c", "-I#{include}", "-L#{lib}", "-lblis", "-lm"
+    system "./test"
+  end
+end
