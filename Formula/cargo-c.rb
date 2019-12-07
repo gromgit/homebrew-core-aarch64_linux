faa334c9c0f359f1adfed029cbc5f9db732955cc
Rui Chen <chenrui333@gmail.com>
2019-12-07 09:57:39 -0500

    cargo-c 0.4.0
    Closes #47596.
    
    Signed-off-by: Caleb Xu <calebcenter@live.com>

--- Formula/cargo-c.rb
+++ Formula/cargo-c.rb
@@ -1,8 +1,8 @@
 class CargoC < Formula
   desc "Helper program to build and install c-like libraries"
   homepage "https://github.com/lu-zero/cargo-c"
-  url "https://github.com/lu-zero/cargo-c/archive/v0.3.1.tar.gz"
-  sha256 "a8e662539218728372f6b8fbbbd72ff2092ecb71fb4ffef7e6a3969a0734e58f"
+  url "https://github.com/lu-zero/cargo-c/archive/v0.4.0.tar.gz"
+  sha256 "e6712c9327ed46af5fbc19d1b9b35f90cdb4e58c42fdc0facf9b56f3e22b0763"
 
   bottle do
     cellar :any_skip_relocation
