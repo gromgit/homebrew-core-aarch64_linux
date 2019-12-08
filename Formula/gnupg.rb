2eb43f6f549dfecaa1c005e2b897c28d17a76250
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-08 20:05:46 +0000

    gnupg: update 2.2.19 bottle.

--- Formula/gnupg.rb
+++ Formula/gnupg.rb
@@ -5,9 +5,9 @@ class Gnupg < Formula
   sha256 "242554c0e06f3a83c420b052f750b65ead711cc3fddddb5e7274fcdbb4e9dec0"
 
   bottle do
-    sha256 "f917eab1b8680aaec7f565917b168ff5cf085476ccd1a32db5961b27ca98d376" => :catalina
-    sha256 "b65d87ac6cb5dc10de6f4afa7e33ca79c146b5a2a35ad8455ac01eeddc9e82f5" => :mojave
-    sha256 "b3ba15bfd670a472ec9b2ddbf65b3d05c2d9802089afeee8a129a5eb470dcaea" => :high_sierra
+    sha256 "db2ff50cbb6d0c9da3ce2f692c96f776ba7043c8ba7bbbc5d026f1a11a008eba" => :catalina
+    sha256 "5e23d28a490125c271558817155b80d8f3d762afcb9391d47ded71db6b17b306" => :mojave
+    sha256 "f35cd016d9734dc5f4dcd5d43939570f3b61f4aa7b61ade1e0cd970d0916ed16" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
