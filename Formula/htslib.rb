a20d2e27a8a1936110e9bd22d230c0e77210561a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-08 23:29:45 +0000

    htslib: update 1.10 bottle.

--- Formula/htslib.rb
+++ Formula/htslib.rb
@@ -6,11 +6,9 @@ class Htslib < Formula
 
   bottle do
     cellar :any
-    sha256 "b04f9a91003fadfe9515bfbaeb4debdb34bef51713a0145ce5b64d1c44c4e49c" => :catalina
-    sha256 "708e1bf70be4d14c4cc937984cb56cc7f47048a266f62988e630fe874e4bc848" => :mojave
-    sha256 "8200a468ef3bc3fd85523e80ed7d3c99b86ea8b120ea06acbfc8c8f36bd878e8" => :high_sierra
-    sha256 "bdbd67c240d87c70d045067bd29de21eddaa73b13ec7e855776467df6167562b" => :sierra
-    sha256 "fa53384739075cd0bc825a32dfd35b2dcdf363dbf2c6d081dfd515b5cbd5c722" => :el_capitan
+    sha256 "dbacdef5228a255b0c1c3def472eacabebe2785143683f56dce4ae2df12ef601" => :catalina
+    sha256 "7ed8777a2c6aa4bf909d9adb9e41e7b3c9c3f249d6ba6b17ca23b0ef65400b28" => :mojave
+    sha256 "330fb579bea2afe187a93b02efb826349efd8b4fc1ffe59b03c812adfece73f8" => :high_sierra
   end
 
   depends_on "xz"
