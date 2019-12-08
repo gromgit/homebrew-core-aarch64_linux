b82ebf7f81d0e9790e46d14d0786d666664ace12
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-08 23:29:46 +0000

    samtools: update 1.10 bottle.

--- Formula/samtools.rb
+++ Formula/samtools.rb
@@ -6,11 +6,9 @@ class Samtools < Formula
 
   bottle do
     cellar :any
-    sha256 "e11612a5679590d160435dbbcb41e64c89f6f8ae726b45561c40a18b3bb77a35" => :catalina
-    sha256 "9cfd73c0434da559423241827124829a77bcdf3b7b24a86d839896e53ee49582" => :mojave
-    sha256 "c1c3b6b25c052cad33208dc14f1bea183cdf83aa986a66815f9a7a69a1b91d67" => :high_sierra
-    sha256 "e349989a4ee48ed6773017282d32d87cfb08d6ff3fab6649889cd84b08351f1b" => :sierra
-    sha256 "ec950e5ece42592ba47012de10d754dbfc22d29fee50170495506c88e4e1d05b" => :el_capitan
+    sha256 "53c31e536e1f0a98e2dba30eddb61a782bf14b45ebf12e56973788f3427c1bac" => :catalina
+    sha256 "18abf7b1a130579066fa055116589cfc7ca4d20842408d7038470ebd612e8a85" => :mojave
+    sha256 "b9be0c12847087c47e0a1d9776d77795d35f98ed3373963564453de15c40437a" => :high_sierra
   end
 
   depends_on "htslib"
