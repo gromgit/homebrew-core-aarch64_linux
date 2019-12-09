9f1e6ddd2b9bf2e1df1d82c7da9187ff674ddfda
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-09 06:52:49 +0000

    certstrap: update 1.2.0 bottle.

--- Formula/certstrap.rb
+++ Formula/certstrap.rb
@@ -6,10 +6,10 @@ class Certstrap < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "154b80812c45ee86c241fac030f98ef5fc368021c0dd6fbebde4fd4a9dbf6803" => :catalina
-    sha256 "b7eb23b913b4f2bdb3d854a6e22f1b25c71018d369fffeec5a16b8df8eaf1fe8" => :mojave
-    sha256 "4dd108d82d046be8576b460d9a2878d593669fa5b11cd015c6aa1f9fc26134f3" => :high_sierra
-    sha256 "aa650a24795207c8860ba986e34f2d320a218b8ee414bd73a5390aaf25e6d214" => :sierra
+    rebuild 1
+    sha256 "52e68d4bcd2256bb1026aafefc9aef39c0e7945e1f26c06b3e09f3b7e7d9ab14" => :catalina
+    sha256 "8f7fb0f6d8b559ee4d30972a68d5d76117a86c07233abc49237c516f45f07277" => :mojave
+    sha256 "12fdf1f518c3f2944d30f4289813a82aa56580b844fc2cc1ad3383d8675c9882" => :high_sierra
   end
 
   depends_on "go" => :build
