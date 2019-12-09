bba5f7086852f139eef0526bccd4ba61d66e67d5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-09 06:25:57 +0000

    armor: update 0.4.14 bottle.

--- Formula/armor.rb
+++ Formula/armor.rb
@@ -7,10 +7,10 @@ class Armor < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "4139f14219db0d2a3add488cf1a4d6aa078cb0f773a20e47a2d09248af2d3971" => :catalina
-    sha256 "b3fd4ee9a6827f911adfa84ef9ff3ad76a9519232dfcbdde1065af4928030a85" => :mojave
-    sha256 "47d85443ebd54b3805b6a02ee9e2c872f0068ca6782bc343490cc12812b32236" => :high_sierra
-    sha256 "d4f119fed6f576e3173e1229ad2849682026529afa1d88a1a58053406bc27e9f" => :sierra
+    rebuild 1
+    sha256 "d0bbf39148c0dabb28f777b951492814a708dc64610106587b1315fcd6a08559" => :catalina
+    sha256 "538f2c340ec151aa7c22847a61d3c8e1d255d121a2b2a75fe2fe7d22f5067347" => :mojave
+    sha256 "8fc3b2ebb6d8bc978f6dd04c92e2a43573b052e51d69398deb4f5a2b04e0f87d" => :high_sierra
   end
 
   depends_on "go" => :build
