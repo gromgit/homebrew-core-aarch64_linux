83698ad010c57283150c7023983e2c8dd619a31d
EricFromCanada <enk3@outlook.com>
2019-12-10 12:28:54 -0500

    soci 4.0.0
    Closes #47694.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/soci.rb
+++ Formula/soci.rb
@@ -1,11 +1,10 @@
 class Soci < Formula
   desc "Database access library for C++"
   homepage "https://soci.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/soci/soci/soci-3.2.3/soci-3.2.3.zip"
-  sha256 "ab0f82873b0c5620e0e8eb2ff89abad6517571fd63bae4bdcac64dd767ac9a05"
+  url "https://downloads.sourceforge.net/project/soci/soci/soci-4.0.0/soci-4.0.0.zip"
+  sha256 "c7fffa74867182d9559e20c6e8d291936c3bd8cfa8c7d0c13bb2eeb09e0f318b"
 
   bottle do
-    rebuild 1
     sha256 "0fd00b5a595f94e6e62cb7f6976816bca6a747ed3a98b0327f5961548e2606ef" => :catalina
     sha256 "e0898c45669d19255465f990c9ac4ea6f6b42c4c6de7688d5ca137f77080bc55" => :mojave
     sha256 "bf08b482820dd4ce1613b662d573caeeb0e9e78d379d7f21dee9118833867e65" => :high_sierra
@@ -29,5 +28,24 @@ class Soci < Formula
       system "cmake", "..", *args
       system "make", "install"
     end
+  end
+
+  test do
+    (testpath/"test.cxx").write <<~EOS
+      #include "soci/soci.h"
+      #include "soci/empty/soci-empty.h"
+      #include <string>
+
+      using namespace soci;
+      std::string connectString = "";
+      backend_factory const &backEnd = *soci::factory_empty();
+
+      int main(int argc, char* argv[])
+      {
+        soci::session sql(backEnd, connectString);
+      }
+    EOS
+    system ENV.cxx, "-o", "test", "test.cxx", "-std=c++11", "-L#{lib}", "-lsoci_core", "-lsoci_empty"
+    system "./test"
   end
 end
