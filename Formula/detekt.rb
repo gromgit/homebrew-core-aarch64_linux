028c3fc329025826cc438f620c26b32cbe790f69
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-10 03:57:46 +0000

    detekt: add 1.1.1 bottle.

--- Formula/detekt.rb
+++ Formula/detekt.rb
@@ -4,6 +4,13 @@ class Detekt < Formula
   url "https://jcenter.bintray.com/io/gitlab/arturbosch/detekt/detekt-cli/1.1.1/detekt-cli-1.1.1-all.jar"
   sha256 "8b17c25cd0e9c507773eae0afff97e89c4a1c502b1b1b91c8d55a62032326b3a"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e3788d2fa44d85703fac30c55da318c3f772a9c6dc8274c631c69f8b25872f6e" => :catalina
+    sha256 "e3788d2fa44d85703fac30c55da318c3f772a9c6dc8274c631c69f8b25872f6e" => :mojave
+    sha256 "e3788d2fa44d85703fac30c55da318c3f772a9c6dc8274c631c69f8b25872f6e" => :high_sierra
+  end
+
   depends_on :java => "1.8+"
 
   def install
