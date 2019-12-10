8b8f2a50727f465f10a1dcdc0c4b476efa6d368a
EricFromCanada <enk3@outlook.com>
2019-12-10 10:23:00 -0500

    pdftoipe 7.2.13.1

--- Formula/pdftoipe.rb
+++ Formula/pdftoipe.rb
@@ -1,9 +1,8 @@
 class Pdftoipe < Formula
   desc "Reads arbitrary PDF files and generates an XML file readable by Ipe"
   homepage "https://github.com/otfried/ipe-tools"
-  url "https://github.com/otfried/ipe-tools/archive/v7.2.8.1.tar.gz"
-  sha256 "a6174aae72f93b56c6652c2c27d5203e0f867e11a5e1c7d89c4aa3b4bcb67eb3"
-  revision 7
+  url "https://github.com/otfried/ipe-tools/archive/v7.2.13.1.tar.gz"
+  sha256 "c8725d78e43b7d6a04465e8a156bc4c6a78121f291aac74e0b0a10286ef95544"
 
   bottle do
     cellar :any
@@ -28,6 +27,6 @@ class Pdftoipe < Formula
   test do
     cp test_fixtures("test.pdf"), testpath
     system bin/"pdftoipe", "test.pdf"
-    assert_match "Homebrew test.</text>", File.read("test.ipe")
+    assert_match "<ipestyle>", File.read("test.ipe")
   end
 end
