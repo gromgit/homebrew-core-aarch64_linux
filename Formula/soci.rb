f948e82871865ba8f3613ad8bdaa445120b9cea3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-10 20:07:07 +0000

    soci: update 4.0.0 bottle.

--- Formula/soci.rb
+++ Formula/soci.rb
@@ -5,10 +5,9 @@ class Soci < Formula
   sha256 "c7fffa74867182d9559e20c6e8d291936c3bd8cfa8c7d0c13bb2eeb09e0f318b"
 
   bottle do
-    sha256 "0fd00b5a595f94e6e62cb7f6976816bca6a747ed3a98b0327f5961548e2606ef" => :catalina
-    sha256 "e0898c45669d19255465f990c9ac4ea6f6b42c4c6de7688d5ca137f77080bc55" => :mojave
-    sha256 "bf08b482820dd4ce1613b662d573caeeb0e9e78d379d7f21dee9118833867e65" => :high_sierra
-    sha256 "db0a84d0ac41fb65d0bdea2eccc652754bdacca85b8659aec879d85101f2d276" => :sierra
+    sha256 "b25ecdd8f098dc48dc20195cd8852533e47e12fe6cbac8bccb31db99854d9c5b" => :catalina
+    sha256 "0dc4c5223dcefeefbdbc647dc7827adf7fc01fe52a23f3bd325d6cf32624e532" => :mojave
+    sha256 "76d7380ed18a0cac1d883d6d38aea9f7a43b587584f4dceab1c32f56596341cf" => :high_sierra
   end
 
   depends_on "cmake" => :build
