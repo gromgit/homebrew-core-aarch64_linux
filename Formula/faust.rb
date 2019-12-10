b6ce7e7ad363c5f1b8a9df5cde2fb3f857f0da54
Rui Chen <chenrui333@gmail.com>
2019-12-09 20:54:43 -0500

    faust 2.20.2
    Closes #47668.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/faust.rb
+++ Formula/faust.rb
@@ -1,8 +1,8 @@
 class Faust < Formula
   desc "Functional programming language for real time signal processing"
   homepage "https://faust.grame.fr"
-  url "https://github.com/grame-cncm/faust/releases/download/2.15.11/faust-2.15.11.tar.gz"
-  sha256 "660816b7fa44da718868d895a82f18f4a441c347bc39afc20a651124f5711d5c"
+  url "https://github.com/grame-cncm/faust/releases/download/2.20.2/faust-2.20.2.tar.gz"
+  sha256 "bea8675446c5e5ef4ac4ba1fb1d64b3a2af99f5f293be0492ccaf32baf7fcb5c"
 
   bottle do
     cellar :any
