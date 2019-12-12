2301b8b9fce41c28cd721fee96f88748560cdd2c
Julian Oes <julian@oes.ch>
2019-12-12 15:15:50 +0100

    mavsdk 0.22.0 (new formula)
    Closes #47770.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

---
+++ Formula/mavsdk.rb
@@ -0,0 +1,41 @@
+class Mavsdk < Formula
+  desc "API and library for MAVLink compatible systems written in C++11"
+  homepage "https://mavsdk.mavlink.io"
+  url "https://github.com/mavlink/MAVSDK.git",
+    :tag      => "v0.22.0",
+    :revision => "164e5fe719608898b009364dbd870846d5163bfc"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-Bbuild/default",
+                    "-DBUILD_BACKEND=ON",
+                    "-DCMAKE_BUILD_TYPE=Release",
+                    "-DCMAKE_INSTALL_PREFIX=#{prefix}",
+                    "-H."
+    system "cmake", "--build", "build/default", "--target", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <mavsdk/mavsdk.h>
+      #include <mavsdk/plugins/info/info.h>
+      int main() {
+          mavsdk::Mavsdk mavsdk;
+          mavsdk.version();
+          mavsdk::System& system = mavsdk.system();
+          auto info = std::make_shared<mavsdk::Info>(system);
+          return 0;
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", testpath/"test.cpp", "-o", "test",
+                  "-I#{include}/mavsdk",
+                  "-L#{lib}",
+                  "-lmavsdk",
+                  "-lmavsdk_info"
+    system "./test"
+
+    assert_equal "Usage: backend_bin [-h | --help]",
+                 shell_output("#{bin}/mavsdk_server --help").split("\n").first
+  end
+end
