bffac20d0230872272c7c16eefcd0ee648f07c07
Dawid Dziurla <dawidd0811@gmail.com>
2019-12-13 13:13:22 +0100

    okteto 1.6.1 (new formula)
    Closes #47812.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

---
+++ Formula/okteto.rb
@@ -0,0 +1,28 @@
+class Okteto < Formula
+  desc "Build better apps by developing and testing code directly in Kubernetes"
+  homepage "https://okteto.com"
+  url "https://github.com/okteto/okteto/archive/1.6.1.tar.gz"
+  sha256 "10e8ac27ec8d2e8d37d2eb779d71dcda66e1f5751ba84ed9d197ddc1b2fe039b"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/okteto/okteto/pkg/config.VersionString=#{version}"
+    tags = "osusergo netgo static_build"
+    system "go", "build", "-o", "#{bin}/#{name}", "-trimpath", "-ldflags", ldflags, "-tags", tags
+  end
+
+  test do
+    touch "test.rb"
+    system "echo | okteto init --overwrite --file test.yml"
+    expected = <<~EOS
+      name: #{Pathname.getwd.basename}
+      image: okteto/ruby:2
+      command:
+      - bash
+      workdir: /usr/src/app
+    EOS
+    got = File.read("test.yml")
+    assert_equal expected, got
+  end
+end
