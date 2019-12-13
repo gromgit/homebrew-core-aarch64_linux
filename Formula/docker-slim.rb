2dcdd90f5f3639ff1aca4157158050a1ff3b784f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-13 10:39:50 +0000

    docker-slim: add 1.26.1 bottle.

--- Formula/docker-slim.rb
+++ Formula/docker-slim.rb
@@ -4,6 +4,13 @@ class DockerSlim < Formula
   url "https://github.com/docker-slim/docker-slim/archive/1.26.1.tar.gz"
   sha256 "f3decf77b6a75cadd194085892469391cf39f7e54bf83d9ed9080308ec2d603a"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5c3287964ff0aa01af0b14d6474fd3bc087972d29c587402314b267a2c9b2512" => :catalina
+    sha256 "5c3287964ff0aa01af0b14d6474fd3bc087972d29c587402314b267a2c9b2512" => :mojave
+    sha256 "5c3287964ff0aa01af0b14d6474fd3bc087972d29c587402314b267a2c9b2512" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
