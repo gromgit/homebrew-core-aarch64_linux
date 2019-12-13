7e0c67a913973bec2b92dc7a0562cfe961728c40
Zhiming Wang <i@zhimingwang.org>
2019-12-13 13:01:41 +0800

    uni 1.0.0 (new formula)
    Closes #47798.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

---
+++ Formula/uni.rb
@@ -0,0 +1,16 @@
+class Uni < Formula
+  desc "Unicode database query tool for the command-line"
+  homepage "https://github.com/arp242/uni"
+  url "https://github.com/arp242/uni/archive/v1.0.0.tar.gz"
+  sha256 "04edf69aca7b086328fa1a7bd8e06617ba074eaec041eda6bdad7a1ba0a00dee"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", bin/"uni"
+  end
+
+  test do
+    assert_match "CLINKING BEER MUGS", shell_output("#{bin}/uni identify 🍻")
+  end
+end
