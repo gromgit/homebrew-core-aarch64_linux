ebd91591016f32db997b7436849bfa0ff9a78f61
Michka Popoff <michkapopoff@gmail.com>
2019-12-13 22:33:32 +0100

    git-plus 0.3.3
    And move to python 3
    
    Closes #47830.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/git-plus.rb
+++ Formula/git-plus.rb
@@ -1,17 +1,18 @@
 class GitPlus < Formula
   desc "Git utilities: git multi, git relation, git old-branches, git recent"
   homepage "https://github.com/tkrajina/git-plus"
-  url "https://github.com/tkrajina/git-plus/archive/v0.3.1.tar.gz"
-  sha256 "ec650cca2e5d26d89f099e3124fbf927c5ab31a56b8c2191b723c4b6a6a50b21"
+  url "https://github.com/tkrajina/git-plus/archive/v0.3.3.tar.gz"
+  sha256 "42c296bd21f2c0ad5312b39c312718fba1b0a55d5cd167aa385fcdafe0fa1748"
   head "https://github.com/tkrajina/git-plus.git"
 
   bottle :unneeded
 
-  depends_on "python@2" # does not support Python 3
+  depends_on "python"
 
   def install
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
-    system "python", *Language::Python.setup_install_args(libexec)
+    xy = Language::Python.major_minor_version "python3"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
+    system "python3", *Language::Python.setup_install_args(libexec)
 
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
