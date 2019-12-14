87d260cd301ff6079e19aac33e25a19f2fa2910d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-14 07:11:34 +0000

    crc: add 1.3.0 bottle.

--- Formula/crc.rb
+++ Formula/crc.rb
@@ -6,6 +6,13 @@ class Crc < Formula
       :revision => "648b3520011f493a1f420196dffb4709d60a7479"
   head "https://github.com/code-ready/crc.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8b6d162b76f303e9a84556deea99d9e3c350bdfb7255f822f20a73caa6efd3ac" => :catalina
+    sha256 "2e097f5bdcc225c0164939458eceeef0a84c680741164a1f5c8fc5e6a40e1d9a" => :mojave
+    sha256 "d8e8cacdaf9a12267efbb962a460e0a9712ae791f9c1fcb335bbd1650e59223a" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
