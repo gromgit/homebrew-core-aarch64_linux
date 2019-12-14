0e4a8f5dae301772b0225a1632c16d2eb595d7a0
Thomas Klemz <tjklemz@gmail.com>
2019-12-14 13:30:38 -0800

    libffcall 2.2 (new formula)
    Closes #47956.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

---
+++ Formula/libffcall.rb
@@ -0,0 +1,46 @@
+class Libffcall < Formula
+  desc "GNU Foreign Function Interface library"
+  homepage "https://www.gnu.org/software/libffcall/"
+  url "https://ftp.gnu.org/gnu/libffcall/libffcall-2.2.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gnu/libffcall/libffcall-2.2.tar.gz"
+  sha256 "ebfa37f97b6c94fac24ecf3193f9fc829517cf81aee9ac2d191af993d73cb747"
+
+  def install
+    ENV.deparallelize
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"callback.c").write <<~EOS
+      #include <stdio.h>
+      #include <callback.h>
+
+      typedef char (*char_func_t) ();
+
+      void function (void *data, va_alist alist)
+      {
+        va_start_char(alist);
+        va_return_char(alist, *(char *)data);
+      }
+
+      int main() {
+        char *data = "abc";
+        callback_t callback = alloc_callback(&function, data);
+        printf("%s\\n%c\\n",
+          is_callback(callback) ? "true" : "false",
+          ((char_func_t)callback)());
+        free_callback(callback);
+        return 0;
+      }
+    EOS
+    flags = ["-L#{lib}", "-lffcall", "-I#{lib}/libffcall-#{version}/include"]
+    system ENV.cc, "-o", "callback", "callback.c", *(flags + ENV.cflags.to_s.split)
+    output = shell_output("#{testpath}/callback")
+    assert_equal "true\na\n", output
+  end
+end
