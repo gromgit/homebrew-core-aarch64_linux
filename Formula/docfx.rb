13081f40b08cba2eea1795fe4e76292f0235a041
Caleb Xu <calebcenter@live.com>
2019-12-15 17:30:12 -0500

    docfx 2.48
    Closes #47934.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -1,8 +1,8 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/releases/download/v2.47/docfx.zip"
-  sha256 "c52feed2ad8ce31b359335bcfd868c916922e2d81c8aecdb2ea7195b577df8d8"
+  url "https://github.com/dotnet/docfx/releases/download/v2.48/docfx.zip"
+  sha256 "07d6e9cd6374fa635a4577058ac85492d2072447ec7e5887771c6c1648648fab"
 
   bottle :unneeded
 
