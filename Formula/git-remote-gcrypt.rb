f9732163032c31756b63924f2f7fdfaab6dc8ddb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-15 18:12:10 +0000

    git-remote-gcrypt: add 1.2 bottle.

--- Formula/git-remote-gcrypt.rb
+++ Formula/git-remote-gcrypt.rb
@@ -4,6 +4,13 @@ class GitRemoteGcrypt < Formula
   url "https://github.com/spwhitton/git-remote-gcrypt/archive/1.2.tar.gz"
   sha256 "6f00d5891639f8d2c263ca15b14ad02bb5a8dd048e9c778fcc12a38cb26f51c4"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "723f849079d609d6b77d6b1a34065caf46ded851c817dc0466909f02f7d81b4b" => :catalina
+    sha256 "c0d744c949262d994256378e22a45789cbaaead577fc6d4d46fd9a87b87ae903" => :mojave
+    sha256 "91d4ddeb8c3840a2a647f2aca1cf03723f1f459b7ecddaa5f6f391ba9c96d843" => :high_sierra
+  end
+
   depends_on "docutils" => :build
 
   def install
