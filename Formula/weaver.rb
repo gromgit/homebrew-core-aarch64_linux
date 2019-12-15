3687e158b7d43e171bc4053f3bfdf4cd81d50afe
Theo <theo@scribd.com>
2019-12-14 20:16:48 -0800

    Weaver 1.0.0
    Closes #47891.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/weaver.rb
+++ Formula/weaver.rb
@@ -1,8 +1,8 @@
 class Weaver < Formula
   desc "Command-line tool for Weaver"
   homepage "https://github.com/scribd/Weaver"
-  url "https://github.com/scribd/Weaver/archive/0.12.3.tar.gz"
-  sha256 "638a08e996ac1fc5b8d945870a4f8af7548a9c59f8b2164c4f766f049a512033"
+  url "https://github.com/scribd/Weaver/archive/1.0.0.tar.gz"
+  sha256 "976bc968b51e45f8840e527ca56624e7db4a9f9e620dc586e1ab01c41dd90209"
 
   bottle do
     cellar :any_skip_relocation
@@ -10,7 +10,7 @@ class Weaver < Formula
     sha256 "8158b3c36bb668c361660b7d433779faf10f3eb8038528b5f26cda4f4966e238" => :mojave
   end
 
-  depends_on :xcode => ["10.2", :build]
+  depends_on :xcode => ["11.2", :build]
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
@@ -20,6 +20,6 @@ class Weaver < Formula
     # Weaver uses Sourcekitten and thus, has the same sandbox issues.
     # Rewrite test after sandbox issues investigated.
     # https://github.com/Homebrew/homebrew/pull/50211
-    system "#{bin}/weaver", "--version"
+    system "#{bin}/weaver", "version"
   end
 end
