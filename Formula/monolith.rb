e3536eef4ed778dac5541d778374e2f7318ea02c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-16 23:04:25 +0000

    monolith: update 2.1.0 bottle.

--- Formula/monolith.rb
+++ Formula/monolith.rb
@@ -6,9 +6,9 @@ class Monolith < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e999c699c71652b99e8b9815c465979f762f8c73a76331130551ebf92e4f658c" => :catalina
-    sha256 "8a5fb488b300e28d29691bffb530c29d2a45c12dd44c228d24d86d3d7d473b5a" => :mojave
-    sha256 "589877c03351b3dd89555cc3463c0f5e4339732d0ab7fbe8fb8d346826256326" => :high_sierra
+    sha256 "c69cf528cce76bc4f2885d0273ebea9a635355659532dc6c21988b95856d3646" => :catalina
+    sha256 "708795bf4df8c960865e13338bef62e99b485a46a695c109335b6674ee90569f" => :mojave
+    sha256 "297bbf8d263e194da2c6b0503a6d77eabc88de1b53bf3727f940831687c529a8" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
