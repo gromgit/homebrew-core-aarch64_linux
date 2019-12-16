5b3d1f3a3b310afa980fb89f6d923a07db9b6d38
Viktor Mihajlovic <ViktorMihajlovic@Viktors-MacBook-Pro.local>
2019-12-16 12:24:38 +0100

    k6 0.26.0
    Closes #47939.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/k6.rb
+++ Formula/k6.rb
@@ -2,8 +2,8 @@ class K6 < Formula
   desc "Modern load testing tool, using Go and JavaScript"
   homepage "https://k6.io"
   url "https://github.com/loadimpact/k6.git",
-    :tag      => "v0.25.1",
-    :revision => "515452f1a93a271ed8a3785111726c7cfe92232d"
+    :tag      => "v0.26.0",
+    :revision => "aeec9a7fdd73c3278e131926ad31efbd8a5b97d8"
 
   bottle do
     cellar :any_skip_relocation
