98d0e8ad8468decf3d20c0fe02e1f92173225ae2
Rui Chen <chenrui333@gmail.com>
2019-12-16 09:50:31 -0500

    monolith 2.1.0
    Closes #47945.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/monolith.rb
+++ Formula/monolith.rb
@@ -1,8 +1,8 @@
 class Monolith < Formula
   desc "CLI tool for saving complete web pages as a single HTML file"
   homepage "https://github.com/Y2Z/monolith"
-  url "https://github.com/Y2Z/monolith/archive/v2.0.16.tar.gz"
-  sha256 "b2f5cd1d95d4d2ccd06e7f44e88e43ff0f2ec5d73dabe93ca56bf22656b06a0e"
+  url "https://github.com/Y2Z/monolith/archive/v2.1.0.tar.gz"
+  sha256 "966dadf31f174dcc21a73110bc432ed05789cef899ec3aaa035d9e5c2b17258a"
 
   bottle do
     cellar :any_skip_relocation
