c017c355effcef92cad8e0c831e75fa6484a23a4
wataru kikuchi <wasanx25@gmail.com>
2019-12-18 07:37:31 +0900

    detekt 1.2.1 (#47980)

--- Formula/detekt.rb
+++ Formula/detekt.rb
@@ -1,15 +1,10 @@
 class Detekt < Formula
   desc "Static code analysis for Kotlin"
   homepage "https://github.com/arturbosch/detekt"
-  url "https://jcenter.bintray.com/io/gitlab/arturbosch/detekt/detekt-cli/1.1.1/detekt-cli-1.1.1-all.jar"
-  sha256 "8b17c25cd0e9c507773eae0afff97e89c4a1c502b1b1b91c8d55a62032326b3a"
+  url "https://jcenter.bintray.com/io/gitlab/arturbosch/detekt/detekt-cli/1.2.1/detekt-cli-1.2.1-all.jar"
+  sha256 "40de5df866fa0fd12acebb63af75da50d1f4dd3e5c5576f08a9f330b1ba8beee"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "e3788d2fa44d85703fac30c55da318c3f772a9c6dc8274c631c69f8b25872f6e" => :catalina
-    sha256 "e3788d2fa44d85703fac30c55da318c3f772a9c6dc8274c631c69f8b25872f6e" => :mojave
-    sha256 "e3788d2fa44d85703fac30c55da318c3f772a9c6dc8274c631c69f8b25872f6e" => :high_sierra
-  end
+  bottle :unneeded
 
   depends_on :java => "1.8+"
 
