5818528939bcac73d6e0f1fa51ec8f53facfd49c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-17 03:10:42 +0000

    libffcall: add 2.2 bottle.

--- Formula/libffcall.rb
+++ Formula/libffcall.rb
@@ -5,6 +5,13 @@ class Libffcall < Formula
   mirror "https://ftpmirror.gnu.org/gnu/libffcall/libffcall-2.2.tar.gz"
   sha256 "ebfa37f97b6c94fac24ecf3193f9fc829517cf81aee9ac2d191af993d73cb747"
 
+  bottle do
+    cellar :any
+    sha256 "02b522baa2d0f38a85e3a2bee8adb79644e8c834c486737b81a62945b9ec73d4" => :catalina
+    sha256 "e38d2a42a2ad191847e423028580245f4b84829e5c781f6d58ce7da9981da280" => :mojave
+    sha256 "d9f7db3318279d7dac5a162de3a251e96c3810be2e70fe8926d93e63b9849045" => :high_sierra
+  end
+
   def install
     ENV.deparallelize
     system "./configure", "--disable-debug",
