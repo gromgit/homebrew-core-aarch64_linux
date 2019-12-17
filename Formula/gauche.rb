fad244c91390bc4c774e2d11ff7686b375280fe5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-17 14:36:52 +0000

    gauche: update 0.9.9 bottle.

--- Formula/gauche.rb
+++ Formula/gauche.rb
@@ -5,10 +5,9 @@ class Gauche < Formula
   sha256 "4ca9325322a7efadb9680d156eb7b53521321c9ca4955c4cbe738bc2e1d7f7fb"
 
   bottle do
-    sha256 "a1937af6853e5f8f057db286caa8aab4565e89e1693ef8d465b8bf287e9de6cf" => :catalina
-    sha256 "fec8c41780f5f89c39e772acc479816e95b29e7a2d0720a175339c1402d7d6a1" => :mojave
-    sha256 "602e9ee61ab977a245282184c1148d1bbbbd74b8298ece34a55143888299c0db" => :high_sierra
-    sha256 "50f5e28b01a2c73be91a0bd64a5069d0965901544186d41150b94ec736b1eadd" => :sierra
+    sha256 "0d2bc0fa954237af130845e904c6c1680018c52c0fe60ccdcbb25000ed5b5408" => :catalina
+    sha256 "bb0bee61ddd5726151e4569d8ea2c7b5797a82543bb13e45a6fec66a521cdcae" => :mojave
+    sha256 "719f5826572a2aec1383ef5501ee4f92580f8a769205c03e47f9e610fa0b5abd" => :high_sierra
   end
 
   def install
