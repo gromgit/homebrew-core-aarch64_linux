29f554931e18a7c9c72623ce352b8f4dbf4aa927
Rui Chen <chenrui333@gmail.com>
2019-12-18 10:23:45 -0500

    serverless 1.60.0
    Closes #48018.
    
    Signed-off-by: Zach Auten <zachauten@gmail.com>

--- Formula/serverless.rb
+++ Formula/serverless.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Serverless < Formula
   desc "Build applications with serverless architectures"
   homepage "https://serverless.com"
-  url "https://github.com/serverless/serverless/archive/v1.59.3.tar.gz"
-  sha256 "797c9fc837aff558682f890313833e2191148ebd5da1ba0ec3004c79bb0b7e4a"
+  url "https://github.com/serverless/serverless/archive/v1.60.0.tar.gz"
+  sha256 "023f826a77bea33fc65463000a3bd25230576350d99c58c321d475e85f4dbec9"
 
   bottle do
     cellar :any_skip_relocation
