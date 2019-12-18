3fe0975912f746ffd80f5f6483203596a7b0d438
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-18 21:03:14 +0000

    ffuf: add 0.12 bottle.

--- Formula/ffuf.rb
+++ Formula/ffuf.rb
@@ -4,6 +4,13 @@ class Ffuf < Formula
   url "https://github.com/ffuf/ffuf/archive/v0.12.tar.gz"
   sha256 "8a80c043cce5c1125260240d42eb4cff36d45d81668840b6934cb771b050ce45"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1996d2b36b78dba79b0ad59921ee9652884323878c6f6e9d4978c0674a3cd5fb" => :catalina
+    sha256 "c43088744a86b48c2e3f2b11c872209fce85f618062097639b974447ba75d6a6" => :mojave
+    sha256 "73b2149c5c9674a693c95189dfc7418976f621e0c50bda75c78191930b2d30b5" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
