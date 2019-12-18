883f1fa1a9521fa80433d10d8d4cb7184afabacb
shigemk2 <i.am.shige@gmail.com>
2019-12-18 10:24:43 +0900

    ghq 0.14.1
    Closes #47991.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/ghq.rb
+++ Formula/ghq.rb
@@ -2,8 +2,8 @@ class Ghq < Formula
   desc "Remote repository management made easy"
   homepage "https://github.com/motemen/ghq"
   url "https://github.com/motemen/ghq.git",
-      :tag      => "v0.14.0",
-      :revision => "ce6939542ed1176d3accc128fac17b3aebdc76e8"
+      :tag      => "v0.14.1",
+      :revision => "081a0fa15afca604bd3bd9593f0f9da947f91f21"
   head "https://github.com/motemen/ghq.git"
 
   bottle do
