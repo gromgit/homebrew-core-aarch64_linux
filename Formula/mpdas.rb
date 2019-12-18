559403650a62b1e8bca25cf6c2e65807bc355ebf
ixearth <35573806+ixearth@users.noreply.github.com>
2019-12-18 16:11:18 +0000

    mpdas: add service (#47898)

--- Formula/mpdas.rb
+++ Formula/mpdas.rb
@@ -21,6 +21,30 @@ class Mpdas < Formula
     etc.install "mpdasrc.example"
   end
 
+  plist_options :manual => "mpdas"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}</string>
+        <key>ProgramArguments</key>
+        <array>
+            <string>#{opt_bin}/mpdas</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+    </dict>
+    </plist>
+  EOS
+  end
+
   test do
     system bin/"mpdas", "-v"
   end
