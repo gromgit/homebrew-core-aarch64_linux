df87771ca3e95edc54e166764646eb5e31e07c9d
chenrui <chenrui333@gmail.com>
2019-12-18 22:17:00 -0500

    django-completion 2.2.9 (#48016)

--- Formula/django-completion.rb
+++ Formula/django-completion.rb
@@ -1,8 +1,8 @@
 class DjangoCompletion < Formula
   desc "Bash completion for Django"
   homepage "https://github.com/django/django"
-  url "https://github.com/django/django/archive/2.2.8.tar.gz"
-  sha256 "fd83bdc35949303bc349bae8281b7cb02eb57e2e72b3673788bd478a61307c81"
+  url "https://github.com/django/django/archive/2.2.9.tar.gz"
+  sha256 "5825e058cd421f9ee1c09e254d5e124d21230666461e0f2a13407d20fa3d132d"
   head "https://github.com/django/django.git"
 
   bottle :unneeded
