b486c3e59ffaa812d9a0858a0bd937e6d3d75f21
Nick Golder <nrg@nirgo.net>
2019-12-19 09:40:55 -0700

    git-annex 7.20191218
    Closes #48078.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/git-annex.rb
+++ Formula/git-annex.rb
@@ -5,8 +5,8 @@ class GitAnnex < Formula
 
   desc "Manage files with git without checking in file contents"
   homepage "https://git-annex.branchable.com/"
-  url "https://hackage.haskell.org/package/git-annex-7.20191114/git-annex-7.20191114.tar.gz"
-  sha256 "b53a48b6bf26d959af98a57865d461b7c9e4ea35f03a668d1d0ea0355eb1d9a9"
+  url "https://hackage.haskell.org/package/git-annex-7.20191218/git-annex-7.20191218.tar.gz"
+  sha256 "060bb91186130204a29bb5b8c990b280065c3c866c9b3bc240a633d14b11c5b7"
   head "git://git-annex.branchable.com/"
 
   bottle do
