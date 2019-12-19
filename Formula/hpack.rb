0b450b7bae519ad60ded4939a2201a466d840095
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-19 13:30:34 +0000

    hpack: add 0.33.0 bottle.

--- Formula/hpack.rb
+++ Formula/hpack.rb
@@ -9,6 +9,13 @@ class Hpack < Formula
   sha256 "954b02fd01ee3e1bc5fddff7ec625839ee4b64bef51efa02306fbcf33008081e"
   head "https://github.com/sol/hpack.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "07e202936d3d52f9a70975d879115876b667daf26af468500c5117e1c235aedd" => :catalina
+    sha256 "43ce8ac6c6f0b1642b3912c875f3c1f47732994411beae48819aa105bd97a55e" => :mojave
+    sha256 "f4de8c3939a1fbc917740d0694dc729619f706962d7015af9f931d7291c952bd" => :high_sierra
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
 
