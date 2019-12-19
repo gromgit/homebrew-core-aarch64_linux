41fa709f655e3236999d5fbf968af6baa4567516
Josh Holtz <josh@rokkincat.com>
2019-12-19 12:24:27 -0600

    fastlane 2.138.0
    Closes #48081.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/fastlane.rb
+++ Formula/fastlane.rb
@@ -1,8 +1,8 @@
 class Fastlane < Formula
   desc "Easiest way to build and release mobile apps"
   homepage "https://fastlane.tools"
-  url "https://github.com/fastlane/fastlane/archive/2.137.0.tar.gz"
-  sha256 "ff3813d75bbced1030b5d7e3e714e75f358788f74d951be796515e8ec4e7ec79"
+  url "https://github.com/fastlane/fastlane/archive/2.138.0.tar.gz"
+  sha256 "81ee26356de1357b76120aee4d13dd789dd3e358115d830ca2030d4060b0cabf"
   head "https://github.com/fastlane/fastlane.git"
 
   bottle do
