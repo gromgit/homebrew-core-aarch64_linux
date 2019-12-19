105353e1f3483c4a363137a8156d96e950fc84ad
Alexander M. Turek <me@derrabus.de>
2019-12-19 10:01:49 +0100

    deployer 6.7.3 (#48053)

--- Formula/deployer.rb
+++ Formula/deployer.rb
@@ -1,8 +1,8 @@
 class Deployer < Formula
   desc "Deployment tool written in PHP with support for popular frameworks"
   homepage "https://deployer.org/"
-  url "https://deployer.org/releases/v6.6.0/deployer.phar"
-  sha256 "37a5478689477b04c7433d62f97e5727c6a0683c4e08b1959ff0d20c64eab28a"
+  url "https://deployer.org/releases/v6.7.3/deployer.phar"
+  sha256 "cd0dc83247d8cc5e65cde437b20f075c12283e80237d7bfd14f8d8d557fbf515"
 
   bottle :unneeded
 
