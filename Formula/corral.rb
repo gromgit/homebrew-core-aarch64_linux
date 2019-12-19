d9c34a71c653064df003d81aa7b40a509dfcae2f
Sean T. Allen <sean@seantallen.com>
2019-12-19 11:30:37 -0500

    corral 0.3.0 (new formula)
    Closes #48075.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

---
+++ Formula/corral.rb
@@ -0,0 +1,23 @@
+class Corral < Formula
+  desc "Dependency manager for the Pony language"
+  homepage "https://github.com/ponylang/corral"
+  url "https://github.com/ponylang/corral/archive/0.3.0.tar.gz"
+  sha256 "5e2f825dd67a060623bdbd992bcdf2d4a377a6d491f4bd60e8754f60df33e578"
+  head "https://github.com/ponylang/corral.git"
+
+  depends_on "ponyc"
+
+  def install
+    system "make", "prefix=#{prefix}", "install"
+  end
+
+  test do
+    (testpath/"test/main.pony").write <<~EOS
+      actor Main
+        new create(env: Env) =>
+          env.out.print("Hello World!")
+    EOS
+    system "#{bin}/corral", "run", "--", "ponyc", "test"
+    assert_equal "Hello World!", shell_output("./test1").chomp
+  end
+end
