4d12f5bdfabb7aeb584f855d0ec863cfbf61ecc2
Dawid Dziurla <dawidd0811@gmail.com>
2019-12-20 08:41:14 +0100

    run 0.6.5 (new formula)
    Closes #48100.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

---
+++ Formula/run.rb
@@ -0,0 +1,22 @@
+class Run < Formula
+  desc "Easily manage and invoke small scripts and wrappers"
+  homepage "https://github.com/TekWizely/run"
+  url "https://github.com/TekWizely/run/archive/v0.6.5.tar.gz"
+  sha256 "5878541e8e29026bf352f78283424217dbf7322a8282cac13b87aa04644e1b58"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-trimpath", "-ldflags", "-w -s", "-o", bin/name
+  end
+
+  test do
+    text = "Hello Homebrew!"
+    task = "hello"
+    (testpath/"Runfile").write <<~EOS
+      #{task}:
+        echo #{text}
+    EOS
+    assert_equal text, shell_output("#{bin}/#{name} #{task}").chomp
+  end
+end
