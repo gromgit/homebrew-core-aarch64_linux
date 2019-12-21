c84e75b22880221d166ad3badd1c9d8146f627b4
Jarry Shaw <jarryshaw@icloud.com>
2019-12-21 19:57:17 +0100

    node-sass: 1.23.7 (new formula) Originally from homebrew/homebrew-core#47439 but to save myself from all the nightmares, I decided just to simply make a new pull request.
    Closes #48167.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

---
+++ Formula/node-sass.rb
@@ -0,0 +1,31 @@
+class NodeSass < Formula
+  require "language/node"
+
+  desc "JavaScript implementation of a Sass compiler"
+  homepage "https://github.com/sass/dart-sass"
+  url "https://registry.npmjs.org/sass/-/sass-1.23.7.tgz"
+  sha256 "6113e3c3034a8d7701c9ecc73e7c2f53e6e419a47d42a63e26ac4546a878328e"
+
+  depends_on "node"
+
+  # waiting for pull request at #47438
+  # conflicts_with "dart-sass", :because => "both install a `sass` binary"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"test.scss").write <<~EOS
+      div {
+        img {
+          border: 0px;
+        }
+      }
+    EOS
+
+    assert_equal "div img{border:0px}",
+    shell_output("#{bin}/sass --style=compressed test.scss").strip
+  end
+end
