d16c512eb9f450fbb321dad5b440bcb9bfb3425d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-21 21:38:53 +0000

    mavsdk: add 0.22.0 bottle.

--- Formula/mavsdk.rb
+++ Formula/mavsdk.rb
@@ -5,6 +5,13 @@ class Mavsdk < Formula
     :tag      => "v0.22.0",
     :revision => "164e5fe719608898b009364dbd870846d5163bfc"
 
+  bottle do
+    cellar :any
+    sha256 "502efdf39843aa6c52174d55597d0c79ba0869ce08294cad731d4aa721327fef" => :catalina
+    sha256 "13815dcd09df20a9dd8dab702bb1887b45ffc1a3090ad054db3f4e30d107db6d" => :mojave
+    sha256 "7274d048bd1cff800d01e3a4af1b42a7843b9e1798243d304fe57b916adf69cd" => :high_sierra
+  end
+
   depends_on "cmake" => :build
 
   def install
