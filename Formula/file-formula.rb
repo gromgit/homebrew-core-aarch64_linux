01adcda9715a2452a2d46d4ab1f8e866821b5885
Rui Chen <chenrui333@gmail.com>
2019-12-20 21:55:35 -0500

    file-formula 5.38
    Closes #48139.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/file-formula.rb
+++ Formula/file-formula.rb
@@ -2,8 +2,8 @@
 class FileFormula < Formula
   desc "Utility to determine file types"
   homepage "https://www.darwinsys.com/file/"
-  url "https://astron.com/pub/file/file-5.37.tar.gz"
-  sha256 "e9c13967f7dd339a3c241b7710ba093560b9a33013491318e88e6b8b57bae07f"
+  url "https://astron.com/pub/file/file-5.38.tar.gz"
+  sha256 "593c2ffc2ab349c5aea0f55fedfe4d681737b6b62376a9b3ad1e77b2cc19fa34"
   head "https://github.com/file/file.git"
 
   bottle do
