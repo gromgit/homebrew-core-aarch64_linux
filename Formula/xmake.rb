0d236563d337c5dab1c9172f442d422e2899fb61
ruki <waruqi@gmail.com>
2019-12-22 00:05:33 +0800

    xmake 2.2.9
    update xmake v2.2.9
    
    Closes #48164.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/xmake.rb
+++ Formula/xmake.rb
@@ -1,8 +1,8 @@
 class Xmake < Formula
   desc "A cross-platform build utility based on Lua"
   homepage "https://xmake.io/"
-  url "https://github.com/xmake-io/xmake/releases/download/v2.2.8/xmake-v2.2.8.tar.gz"
-  sha256 "fb8ad4ca5133ca0cc303e57c24afaf00fbdaf7a9d59e1a6e7c4337803926bf29"
+  url "https://github.com/xmake-io/xmake/releases/download/v2.2.9/xmake-v2.2.9.tar.gz"
+  sha256 "7d7b4b368808c78cda4bcdd00a140cd8b4cab8f32c7b3c31aa22fdd08dde4940"
   head "https://github.com/xmake-io/xmake.git"
 
   bottle do
