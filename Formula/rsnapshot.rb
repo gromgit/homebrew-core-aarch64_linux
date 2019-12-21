0f8168295a3963525c93e3df51e2306141a8c1ed
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-21 01:37:54 +0000

    rsnapshot: update 1.4.3 bottle.

--- Formula/rsnapshot.rb
+++ Formula/rsnapshot.rb
@@ -7,13 +7,9 @@ class Rsnapshot < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "89274b7befdad090f2fb797392a613059fd06c93553eb8d14fd5a1ead44b5d37" => :catalina
-    sha256 "58aaff911593d284a63426d0ec4f1328910b9a6cb5a6ecb64f4853d0300a3184" => :mojave
-    sha256 "27e99660b73118874b5ba4ba355a0bf9b5db71933138a6b719869da01164a12a" => :high_sierra
-    sha256 "998faa778dcfdef5b362b10aeaffadc97e40f63ec6df72d9bd25029ab82f2550" => :sierra
-    sha256 "21823489c045150e8d8e51addba52b6cd75eedaec93357732db859ba738f59d5" => :el_capitan
-    sha256 "d6374fba65d24f7067197c9a7732f6f629dcb537f0687ab91f4f15d5c55d6cc6" => :yosemite
-    sha256 "66cc127c640855b029881b2cd029197b093d92b8d77c3bb9a61167cbaedfedd8" => :mavericks
+    sha256 "e195b17e2c28a787e6bc183c3f57397256fba91c8d5c490f3c24576033d39a74" => :catalina
+    sha256 "e195b17e2c28a787e6bc183c3f57397256fba91c8d5c490f3c24576033d39a74" => :mojave
+    sha256 "e195b17e2c28a787e6bc183c3f57397256fba91c8d5c490f3c24576033d39a74" => :high_sierra
   end
 
   def install
