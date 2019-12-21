9e9243a869c65cb586afd691f9743e52791bb8c6
Boros Gábor <gabor.brs@gmail.com>
2019-12-21 10:17:12 +0100

    rethinkdb 2.4.0
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/rethinkdb.rb
+++ Formula/rethinkdb.rb
@@ -1,8 +1,8 @@
 class Rethinkdb < Formula
   desc "The open-source database for the realtime web"
   homepage "https://www.rethinkdb.com/"
-  url "https://download.rethinkdb.com/dist/rethinkdb-2.3.6.tgz"
-  sha256 "c42159666910ad01be295a57caf8839ec3a89227d8919be5418e3aa1f0a3dc28"
+  url "https://download.rethinkdb.com/dist/rethinkdb-2.4.0.tgz"
+  sha256 "bfb0708710595c6762f42e25613adec692cf568201cd61da74c254f49fa9ee4c"
   revision 1
 
   bottle do
@@ -14,27 +14,6 @@ class Rethinkdb < Formula
 
   depends_on "boost" => :build
   depends_on "openssl@1.1"
-
-  # Fix error with Xcode 9, patch merged upstream:
-  # https://github.com/rethinkdb/rethinkdb/pull/6450
-  if DevelopmentTools.clang_build_version >= 900
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/fb00ee376a/rethinkdb/xcode9.patch"
-      sha256 "abd50d91a247ee7de988020dd9d405a3d4cd93edb2875b7d5822ba0f513f85a0"
-    end
-  end
-
-  # Upstream commit for OpenSSL 1.1 compatibility
-  patch do
-    url "https://github.com/rethinkdb/rethinkdb/commit/62456155.diff?full_index=1"
-    sha256 "6666074788d7de3295619426350c96b1f2e2e3b2427e76511dc168447034cacd"
-  end
-
-  # Upstream fix for Boost >= 1.69
-  patch do
-    url "https://github.com/rethinkdb/rethinkdb/commit/04785087.diff?full_index=1"
-    sha256 "a26e452ce1f16541a9ba40057af154e594fe89665c2c656994ceab103d2017e9"
-  end
 
   def install
     args = ["--prefix=#{prefix}"]
