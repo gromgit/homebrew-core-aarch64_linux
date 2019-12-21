6c09f8c6e32c48af8da97be6016aedb00c6b1d2c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-21 21:08:13 +0000

    node-sass: add 1.23.7 bottle.

--- Formula/node-sass.rb
+++ Formula/node-sass.rb
@@ -6,6 +6,13 @@ class NodeSass < Formula
   url "https://registry.npmjs.org/sass/-/sass-1.23.7.tgz"
   sha256 "6113e3c3034a8d7701c9ecc73e7c2f53e6e419a47d42a63e26ac4546a878328e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "322346ee7f6719fc0697d8c9e5b5209e0169dde870ec9c7ae71fcadb26410797" => :catalina
+    sha256 "a6536baf69cfe20e40be7109e947b008e64b32534e617a4315bc6404e4520478" => :mojave
+    sha256 "ee3a08eb743459e18456808f98f230e779fd72f62716c881fd35da5131e36302" => :high_sierra
+  end
+
   depends_on "node"
 
   # waiting for pull request at #47438
