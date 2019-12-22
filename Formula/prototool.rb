de74c4935a356b891c91dd3876de66ef8b1e0f08
Caleb Xu <calebcenter@live.com>
2019-12-21 20:10:38 -0500

    prototool: remove gopath
    Closes #48175.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/prototool.rb
+++ Formula/prototool.rb
@@ -14,18 +14,13 @@ class Prototool < Formula
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    dir = buildpath/"src/github.com/uber/prototool"
-    dir.install buildpath.children
-    cd dir do
-      system "make", "brewgen"
-      cd "brew" do
-        bin.install "bin/prototool"
-        bash_completion.install "etc/bash_completion.d/prototool"
-        zsh_completion.install "etc/zsh/site-functions/_prototool"
-        man1.install Dir["share/man/man1/*.1"]
-        prefix.install_metafiles
-      end
+    system "make", "brewgen"
+    cd "brew" do
+      bin.install "bin/prototool"
+      bash_completion.install "etc/bash_completion.d/prototool"
+      zsh_completion.install "etc/zsh/site-functions/_prototool"
+      man1.install Dir["share/man/man1/*.1"]
+      prefix.install_metafiles
     end
   end
 
