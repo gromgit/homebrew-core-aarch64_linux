5caf22262a4e5e32b027c1c84e001ac52026001b
Caleb Xu <calebcenter@live.com>
2019-12-22 00:44:13 -0500

    scons 3.1.2
    Closes #48183.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/scons.rb
+++ Formula/scons.rb
@@ -1,8 +1,8 @@
 class Scons < Formula
   desc "Substitute for classic 'make' tool with autoconf/automake functionality"
   homepage "https://www.scons.org/"
-  url "https://downloads.sourceforge.net/project/scons/scons/3.1.1/scons-3.1.1.tar.gz"
-  sha256 "4cea417fdd7499a36f407923d03b4b7000b0f9e8fd7b31b316b9ce7eba9143a5"
+  url "https://downloads.sourceforge.net/project/scons/scons/3.1.2/scons-3.1.2.tar.gz"
+  sha256 "7801f3f62f654528e272df780be10c0e9337e897650b62ddcee9f39fde13f8fb"
 
   bottle do
     cellar :any_skip_relocation
