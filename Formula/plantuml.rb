10094290c24cf3858320fb87b46206d07e6d58ab
Jesse Tan <j.tan@activevideo.com>
2019-12-23 10:26:19 +0100

    plantuml 1.2019.13
    Closes #48223.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://downloads.sourceforge.net/project/plantuml/1.2019.12/plantuml.1.2019.12.jar"
-  sha256 "e00f054c9953a56b8c58acb41bfeb69f2939789764ecfe3ffcf90d331da30299"
+  url "https://downloads.sourceforge.net/project/plantuml/1.2019.13/plantuml.1.2019.13.jar"
+  sha256 "7fa2c92fb9c57b37d97c01457e02e93e1e90ffbd263264ae3d3a9e1a2d830a65"
   version_scheme 1
 
   bottle :unneeded
