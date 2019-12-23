c8b7444dcbfc4c086423b366dd03f46fb5f47dba
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-23 03:00:55 +0000

    fileicon: add 0.2.2 bottle.

--- Formula/fileicon.rb
+++ Formula/fileicon.rb
@@ -4,6 +4,13 @@ class Fileicon < Formula
   url "https://github.com/mklement0/fileicon/archive/v0.2.2.tar.gz"
   sha256 "1725a6a693b3586617b0fc669719a15c05eb7f8e535daee511a315d9b6fc0eb3"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6b3cfdf0e341c2a39ff84e617279e95f38d315130ed2820d867251ef4c8ba9da" => :catalina
+    sha256 "6b3cfdf0e341c2a39ff84e617279e95f38d315130ed2820d867251ef4c8ba9da" => :mojave
+    sha256 "6b3cfdf0e341c2a39ff84e617279e95f38d315130ed2820d867251ef4c8ba9da" => :high_sierra
+  end
+
   def install
     bin.install "bin/fileicon"
     man1.install "man/fileicon.1"
