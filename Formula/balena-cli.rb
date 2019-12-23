0580871c3e915f9002c8bab3fde794b1a053b2a1
Christian Moritz <chrmoritz@gmail.com>
2019-12-23 16:13:56 +0100

    balena-cli 11.20.2
    Closes #48235.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/balena-cli.rb
+++ Formula/balena-cli.rb
@@ -4,8 +4,8 @@ class BalenaCli < Formula
   desc "The official balena CLI tool"
   homepage "https://www.balena.io/docs/reference/cli/"
   # Frequent upstream releases, do not update more than once a week
-  url "https://registry.npmjs.org/balena-cli/-/balena-cli-11.18.0.tgz"
-  sha256 "39890bd18d6649ce041f477c22b8561027bdac70f0de7885d79709b9cb4875e7"
+  url "https://registry.npmjs.org/balena-cli/-/balena-cli-11.20.2.tgz"
+  sha256 "a78f84c3d343f274d3e51fed1f06656e7cfe557dbe22f9af78871c94f6c1040b"
 
   bottle do
     sha256 "0a5dcaf03c6e45854662631da360f55ca55b16f34341ac4ea1e024f2e0a79f19" => :catalina
@@ -14,6 +14,9 @@ class BalenaCli < Formula
   end
 
   depends_on "node"
+
+  # fixes node 13 compatibility
+  patch :DATA
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
@@ -25,3 +28,56 @@ class BalenaCli < Formula
     assert_match "Logging in to balena-cloud.com", output
   end
 end
+
+__END__
+diff --git a/npm-shrinkwrap.json b/npm-shrinkwrap.json
+index 2101ae4..9d4d743 100644
+--- a/npm-shrinkwrap.json
++++ b/npm-shrinkwrap.json
+@@ -5034,9 +5034,9 @@
+       }
+     },
+     "ext2fs": {
+-      "version": "1.0.29",
+-      "resolved": "https://registry.npmjs.org/ext2fs/-/ext2fs-1.0.29.tgz",
+-      "integrity": "sha512-AoHwqSNx8SLZizLCMs+etrvdethI+jzak5yVmHACV+G0ziiLd19E3OQpC+SMmGOL1V+uRz3om7VxT5itfThYfQ==",
++      "version": "1.0.31",
++      "resolved": "https://registry.npmjs.org/ext2fs/-/ext2fs-1.0.31.tgz",
++      "integrity": "sha512-bfaggH7juFJNxXoY2nPShr8CSFbijKRsPYOwmSSms88I5n1X7+qsVqnMLLiq6VMI9MxHIFvqGfB+cwum0U1uPQ==",
+       "requires": {
+         "async": "^2.6.1",
+         "bindings": "^1.3.0",
+@@ -8290,9 +8290,9 @@
+       }
+     },
+     "lzma-native": {
+-      "version": "4.0.5",
+-      "resolved": "https://registry.npmjs.org/lzma-native/-/lzma-native-4.0.5.tgz",
+-      "integrity": "sha512-pmLMsHQlXQAikqGqapzUOtACPW/gEtt9xhkcrkJnsjWn+I1g7OIbrV2SugL8jinkBCD+QxqAze51VtRsECDcxQ==",
++      "version": "4.0.6",
++      "resolved": "https://registry.npmjs.org/lzma-native/-/lzma-native-4.0.6.tgz",
++      "integrity": "sha512-1kiSs/KAcAuh9vyyd00ATXZFfrg6W8UCBqH1RKlWg/tBP5aQez6HYOY+SihmsZfpy0RVDioW5SLI76dZ3Mq5Rw==",
+       "requires": {
+         "nan": "^2.14.0",
+         "node-pre-gyp": "^0.11.0",
+@@ -12797,7 +12797,7 @@
+         "@types/node": "^6.0.112",
+         "@types/usb": "^1.5.1",
+         "debug": "^3.1.0",
+-        "usb": "github:balena-io/node-usb#v1.3.12"
++        "usb": "1.3.13"
+       },
+       "dependencies": {
+         "@types/node": {
+@@ -17613,8 +17613,9 @@
+       "integrity": "sha1-FQWgOiiaSMvXpDTvuu7FBV9WM6k="
+     },
+     "usb": {
+-      "version": "github:balena-io/node-usb#1fb1bb01d04281432083b96801c6be6d528bd213",
+-      "from": "github:balena-io/node-usb#v1.3.12",
++      "version": "1.3.13",
++      "resolved": "https://registry.npmjs.org/@balena.io/usb/-/usb-1.3.13.tgz",
++      "integrity": "sha512-qG2nGJduDtmgElK6KhcJ/u9M67FlgTSAF8IkcC6CZOTitSLRMh1OqeXtI5B3q7KyFH5773qZYj2g1jlXhpL1dQ==",
+       "requires": {
+         "nan": "2.13.2",
+         "node-pre-gyp": "^0.11.0"
