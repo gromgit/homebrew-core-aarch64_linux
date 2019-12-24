9ce808cda0a1a957ac5076ed66105dfa669da5d7
echopi <eagleking0213@gmail.com>
2019-12-25 01:11:31 +0800

    whistle 2.4.2 (new formula)
    Closes #48268.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

---
+++ Formula/whistle.rb
@@ -0,0 +1,22 @@
+require "language/node"
+
+class Whistle < Formula
+  desc "HTTP, HTTP2, HTTPS, Websocket debugging proxy"
+  homepage "https://github.com/avwo/whistle"
+  url "https://registry.npmjs.org/whistle/-/whistle-2.4.2.tgz"
+  # version "2.4.2"
+  sha256 "bff72cdeba659c66a709c9910c7427ba9bc5ef25a39cc237288f5877ff9a4f62"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"package.json").write('{"name": "test"}')
+    system bin/"whistle", "start"
+    system bin/"whistle", "stop"
+  end
+end
