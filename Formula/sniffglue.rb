dcd99cc57a18b58eac134d244d7db84fca8743b9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-24 01:48:58 +0000

    sniffglue: add 0.10.1 bottle.

--- Formula/sniffglue.rb
+++ Formula/sniffglue.rb
@@ -4,6 +4,13 @@ class Sniffglue < Formula
   url "https://github.com/kpcyrd/sniffglue/archive/v0.10.1.tar.gz"
   sha256 "27b9448a317957eface67fab1a264057b03b614b588031e4f47655a8e369ead7"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7a622b2d3018dda1d59b13bfcc04a81ddcc917855b229d5dea583a3d77676477" => :catalina
+    sha256 "0efbd0283a8ad1aacedd6256972050054c92757ad9b1b80554b4eed55e13217a" => :mojave
+    sha256 "0130451cd4305cbb61a06158b45caf20e7d3f4a869c0ae53d9d6b064421ac3e9" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   resource "testdata" do
