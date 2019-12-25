bde6de7cfcf7074aad8109a3e3221dfe9bcdb0e1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-25 15:34:46 +0000

    wordgrinder: add 0.7.2 bottle.

--- Formula/wordgrinder.rb
+++ Formula/wordgrinder.rb
@@ -5,6 +5,13 @@ class Wordgrinder < Formula
   sha256 "4e1bc659403f98479fe8619655f901c8c03eb87743374548b4d20a41d31d1dff"
   head "https://github.com/davidgiven/wordgrinder.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "05750b2dadc537994d4701005d565782d2afdecfbe1bf1f118f76290255a3946" => :catalina
+    sha256 "572bd029f459c15af9a2d27a471e31594d1d9c2387b6ec66f9a890df1db13e55" => :mojave
+    sha256 "19855507ccd289049c30aad466a66de742b9d58a98247b9f9d223e140a81ff69" => :high_sierra
+  end
+
   depends_on "ninja" => :build
   depends_on "lua"
 
