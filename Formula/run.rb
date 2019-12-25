7766082f76ffae95e30b09a9d8ce5966e6b5b4a9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-25 17:01:32 +0000

    run: add 0.6.5 bottle.

--- Formula/run.rb
+++ Formula/run.rb
@@ -4,6 +4,13 @@ class Run < Formula
   url "https://github.com/TekWizely/run/archive/v0.6.5.tar.gz"
   sha256 "5878541e8e29026bf352f78283424217dbf7322a8282cac13b87aa04644e1b58"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ca616a915230cf04423546409354267e4abc5082f1b8fd27e0fe77e5db4a6a7d" => :catalina
+    sha256 "ca616a915230cf04423546409354267e4abc5082f1b8fd27e0fe77e5db4a6a7d" => :mojave
+    sha256 "ca616a915230cf04423546409354267e4abc5082f1b8fd27e0fe77e5db4a6a7d" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
