3d91fa73c35eb09f8b3865d50e0f21aaf9bd5c83
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-25 16:39:21 +0000

    whistle: add 2.4.2 bottle.

--- Formula/whistle.rb
+++ Formula/whistle.rb
@@ -7,6 +7,13 @@ class Whistle < Formula
   # version "2.4.2"
   sha256 "bff72cdeba659c66a709c9910c7427ba9bc5ef25a39cc237288f5877ff9a4f62"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "33fecf9ed07f69091c10358602db04b4e4ba39eb5b84666b8d26ec485e91c659" => :catalina
+    sha256 "3fa0a4cb94c5a5190949c72a400bf4d5679b969a364204d41bf1643cd018320e" => :mojave
+    sha256 "a74006cdf42883daee4675066ae2d303be393a2a8c3b87ac78e44e4a6b21bea9" => :high_sierra
+  end
+
   depends_on "node"
 
   def install
