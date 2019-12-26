36706335ff304a240479d6b7ed973c52819796c3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-26 16:07:29 +0000

    laszip: update 3.4.3 bottle.

--- Formula/laszip.rb
+++ Formula/laszip.rb
@@ -6,10 +6,9 @@ class Laszip < Formula
   head "https://github.com/LASzip/LASzip.git"
 
   bottle do
-    sha256 "7779ef145383b44244ad5cac6e46e5ef003fe99b4406dcdec9f14aa757511c99" => :catalina
-    sha256 "bb1b85c70f7efee68cac94ed831941fe1420530f36166587a8715ec5507330eb" => :mojave
-    sha256 "a9396e0e7d10b57df846c73fcfe807d354a1aab80f5b0c85fb6a68b420db00aa" => :high_sierra
-    sha256 "7148c262d7dd9e0d9889aebc5fb38f3b12954e74b716fa7b65cb5550350ee196" => :sierra
+    sha256 "df73f3c2c8be13bc0fab13f28cbb22262a24c283f4da85cf6b21c55531516e7f" => :catalina
+    sha256 "3a9bc6d5931145800cb5792740a3cae118d27c4879144f3c74a44c2aee75ce64" => :mojave
+    sha256 "a32459a4896bdc365fae55b70744bb7ae2a05b552e3bb0b0097345e0ea423014" => :high_sierra
   end
 
   depends_on "cmake" => :build
