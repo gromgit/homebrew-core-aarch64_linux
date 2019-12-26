6d52c177a3f14fed7b40bf4fb6436a8ae6c06981
FX Coudert <fxcoudert@gmail.com>
2019-12-26 16:16:15 +0100

    ledger: revision for boost

--- Formula/ledger.rb
+++ Formula/ledger.rb
@@ -3,7 +3,7 @@ class Ledger < Formula
   homepage "https://ledger-cli.org/"
   url "https://github.com/ledger/ledger/archive/v3.1.3.tar.gz"
   sha256 "b248c91d65c7a101b9d6226025f2b4bf3dabe94c0c49ab6d51ce84a22a39622b"
-  revision 3
+  revision 4
   head "https://github.com/ledger/ledger.git"
 
   bottle do
