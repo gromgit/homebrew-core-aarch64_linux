9e2e5a500de93e5339f90ae778494a9b0b757f75
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-26 15:51:11 +0000

    spades: update 3.13.1 bottle.

--- Formula/spades.rb
+++ Formula/spades.rb
@@ -7,10 +7,8 @@ class Spades < Formula
 
   bottle do
     cellar :any
-    sha256 "f79562a627e66647a7013f989e257f3ed33023daf7cbc9f836fecb0356766aa7" => :catalina
-    sha256 "6eff79211afd0a5f2a3194db28a630bfa53cec5b968dc810e65bbaefce55fae4" => :mojave
-    sha256 "ef7d029efa28d81c236a428f40c0780b074827b50e5618cc328b4cfffdc7e579" => :high_sierra
-    sha256 "8418d4226f398f2853500eb3fea5788d58b392202101934a4fba502f7c77efcd" => :sierra
+    sha256 "4761b8cfbaca36fdc4fac08b8122f5519415d86668355224a67c52a5191ae7c5" => :catalina
+    sha256 "f3e29120ab665892ba68d2d7c7522b1fea866a2d405f59547071d8c6c31318c8" => :high_sierra
   end
 
   depends_on "cmake" => :build
