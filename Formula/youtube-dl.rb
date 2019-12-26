446308e51911192199273d9b31e3972b783c0946
David Yantis <404NetworkError@users.noreply.github.com>
2019-12-26 09:36:45 -0500

    youtube-dl 2019.12.25 (#48269)

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -1,8 +1,8 @@
 class YoutubeDl < Formula
   desc "Download YouTube videos from the command-line"
   homepage "https://ytdl-org.github.io/youtube-dl/"
-  url "https://github.com/ytdl-org/youtube-dl/releases/download/2019.11.28/youtube-dl-2019.11.28.tar.gz"
-  sha256 "93dfcb77811cafb5aef090504eb43e8fd3fca0df694676a4ff71023a6ce3b1a7"
+  url "https://github.com/ytdl-org/youtube-dl/releases/download/2019.12.25/youtube-dl-2019.12.25.tar.gz"
+  sha256 "0ea2f15fa68f97ce5a57077a126a85f425cbc996af59e022a4f19fb3d3e6c78d"
 
   head do
     url "https://github.com/ytdl-org/youtube-dl.git"
@@ -22,6 +22,6 @@ class YoutubeDl < Formula
 
   test do
     system "#{bin}/youtube-dl", "--simulate", "https://www.youtube.com/watch?v=he2a4xK8ctk"
-    system "#{bin}/youtube-dl", "--simulate", "--yes-playlist", "https://www.youtube.com/watch?v=iCkYw3cRwLo&list=LLnHXLLNHjNAnDQ50JANLG1g"
+    system "#{bin}/youtube-dl", "--simulate", "--yes-playlist", "https://www.youtube.com/watch?v=7FlzFH9xOF8&list=PLRuS3NbVmMIq4XypJdggVfUVJNRZjbfCR"
   end
 end
