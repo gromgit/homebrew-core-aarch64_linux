af8a22a7cf20336641f458ffcb1ab81a911a052f
EricFromCanada <enk3@outlook.com>
2019-12-27 11:20:03 -0500

    samurai 1.0
    Closes #48321.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/samurai.rb
+++ Formula/samurai.rb
@@ -1,8 +1,8 @@
 class Samurai < Formula
   desc "Ninja-compatible build tool written in C"
   homepage "https://github.com/michaelforney/samurai"
-  url "https://github.com/michaelforney/samurai/releases/download/0.7/samurai-0.7.tar.gz"
-  sha256 "e079e8de3b07ba0f1fffe2dff31c1fcb3be357c523abc6937108635a081a11f0"
+  url "https://github.com/michaelforney/samurai/releases/download/1.0/samurai-1.0.tar.gz"
+  sha256 "55c73da66b5b8af8b6e26e74b55b3a9f06b763547fe6d0a6206dae68274a1438"
   head "https://github.com/michaelforney/samurai.git"
 
   bottle do
