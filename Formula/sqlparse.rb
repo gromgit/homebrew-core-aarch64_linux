a3ea92639d09597f6ed82636e53b5c2daec15bee
Alexander Bayandin <a.bayandin@gmail.com>
2019-12-27 16:44:36 +0000

    sqlparse: revision bump for python@3.8

--- Formula/sqlparse.rb
+++ Formula/sqlparse.rb
@@ -5,7 +5,7 @@ class Sqlparse < Formula
   homepage "https://github.com/andialbrecht/sqlparse"
   url "https://files.pythonhosted.org/packages/79/3c/2ad76ba49f9e3d88d2b58e135b7821d93741856d1fe49970171f73529303/sqlparse-0.2.4.tar.gz"
   sha256 "ce028444cfab83be538752a2ffdb56bc417b7784ff35bb9a3062413717807dec"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any_skip_relocation
@@ -15,7 +15,7 @@ class Sqlparse < Formula
     sha256 "9c77ae1a2ca8a0cfe79197762b9c60be451b827cf72f616542547ec280b3acb3" => :sierra
   end
 
-  depends_on "python"
+  depends_on "python@3.8"
 
   def install
     virtualenv_install_with_resources
