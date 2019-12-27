a8fbaf24469aef676d5457eca3631bb5ba83391d
Camden Narzt <c.narzt@me.com>
2019-12-27 04:12:40 -0700

    ruby-build: add caveat about openssl (#47402)

--- Formula/ruby-build.rb
+++ Formula/ruby-build.rb
@@ -16,6 +16,19 @@ class RubyBuild < Formula
     system "./install.sh"
   end
 
+  def caveats
+    <<~EOS
+      ruby-build installs a non-Homebrew OpenSSL for each Ruby version installed and these are never upgraded.
+
+      To link Rubies to Homebrew's OpenSSL 1.1 (which is upgraded) add the following
+      to your #{shell_profile}:
+        export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+
+      Note: this may interfere with building old versions of Ruby (e.g <2.4) that use
+      OpenSSL <1.1.
+    EOS
+  end
+
   test do
     assert_match "2.0.0", shell_output("#{bin}/ruby-build --definitions")
   end
