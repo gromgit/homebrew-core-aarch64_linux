fd2ce31d3df0695cb2312a1de84709980a15c52e
Alexander Bayandin <a.bayandin@gmail.com>
2019-12-27 16:44:33 +0000

    rst-lint: revision bump for python@3.8

--- Formula/rst-lint.rb
+++ Formula/rst-lint.rb
@@ -5,6 +5,7 @@ class RstLint < Formula
   homepage "https://github.com/twolfson/restructuredtext-lint"
   url "https://github.com/twolfson/restructuredtext-lint/archive/1.3.0.tar.gz"
   sha256 "4bf9d4724f59bc05ebe1cd5192c03d4597ee95c4bbf60bd5644422e1a2558da3"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -14,7 +15,7 @@ class RstLint < Formula
     sha256 "46da54ff6e6f96663327291d0f51380107c377705d9eaccc7461c9156916799b" => :sierra
   end
 
-  depends_on "python"
+  depends_on "python@3.8"
 
   resource "docutils" do
     url "https://files.pythonhosted.org/packages/84/f4/5771e41fdf52aabebbadecc9381d11dea0fa34e4759b4071244fa094804c/docutils-0.14.tar.gz"
