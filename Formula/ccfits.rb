f67be4b9d8a6d72747dd693452ffd614368edb32
chenxu <6235641+cxwx@users.noreply.github.com>
2019-12-27 11:01:51 +0800

    ccfits 2.5 (new formula)
    Closes #48298.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/ccfits.rb
@@ -0,0 +1,31 @@
+class Ccfits < Formula
+  desc "Object oriented interface to the cfitsio library"
+  homepage "https://heasarc.gsfc.nasa.gov/fitsio/CCfits/"
+  url "https://heasarc.gsfc.nasa.gov/fitsio/CCfits/CCfits-2.5.tar.gz"
+  sha256 "938ecd25239e65f519b8d2b50702416edc723de5f0a5387cceea8c4004a44740"
+
+  depends_on "cfitsio"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <CCfits/CCfits>
+      #include <iostream>
+      int main() {
+        CCfits::FITS::setVerboseMode(true);
+        std::cout << "the answer is " << CCfits::VTbyte << std::endl;
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test", "-I#{include}",
+                    "-L#{lib}", "-lCCfits"
+    assert_match /the answer is -11/, shell_output("./test")
+  end
+end
