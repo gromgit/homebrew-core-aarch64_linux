f1d545c20d189426d3c64058f24f03318c0757fd
Alexander Bayandin <a.bayandin@gmail.com>
2019-12-27 16:44:21 +0000

    mongo-orchestration: revision bump for python@3.8

--- Formula/mongo-orchestration.rb
+++ Formula/mongo-orchestration.rb
@@ -5,7 +5,7 @@ class MongoOrchestration < Formula
   homepage "https://github.com/10gen/mongo-orchestration"
   url "https://files.pythonhosted.org/packages/6d/b5/624a02d5f6cbfa8eb3c6554f5387c4739ad283bac7efb27ee99434a4d314/mongo-orchestration-0.6.11.tar.gz"
   sha256 "7a09706d36e94ae767e90646fed98a52b426a4d14b0f4f724b185e292ac8f425"
-  revision 1
+  revision 2
   head "https://github.com/10gen/mongo-orchestration.git"
 
   bottle do
@@ -16,7 +16,7 @@ class MongoOrchestration < Formula
     sha256 "8a463e718a50549325ae910fa944f4fa1761cbc4050d271c16d285b22402685e" => :sierra
   end
 
-  depends_on "python"
+  depends_on "python@3.8"
 
   resource "bottle" do
     url "https://files.pythonhosted.org/packages/bd/99/04dc59ced52a8261ee0f965a8968717a255ea84a36013e527944dbf3468c/bottle-0.12.13.tar.gz"
