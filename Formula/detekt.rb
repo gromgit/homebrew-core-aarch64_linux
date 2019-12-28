ac12581db207b14f72d033fe467fb8076a321c54
wataru kikuchi <wasanx25@gmail.com>
2019-12-28 21:12:07 +0900

    detekt 1.3.0 (#48337)

--- Formula/detekt.rb
+++ Formula/detekt.rb
@@ -1,8 +1,8 @@
 class Detekt < Formula
   desc "Static code analysis for Kotlin"
   homepage "https://github.com/arturbosch/detekt"
-  url "https://jcenter.bintray.com/io/gitlab/arturbosch/detekt/detekt-cli/1.2.1/detekt-cli-1.2.1-all.jar"
-  sha256 "40de5df866fa0fd12acebb63af75da50d1f4dd3e5c5576f08a9f330b1ba8beee"
+  url "https://jcenter.bintray.com/io/gitlab/arturbosch/detekt/detekt-cli/1.3.0/detekt-cli-1.3.0-all.jar"
+  sha256 "610b0211b909d8e79c1b623119e1d94cb833a8e98b32e5a0e991fa2d214992f6"
 
   bottle :unneeded
 
