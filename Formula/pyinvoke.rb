0af7254d4665808b429e7f0babd5b02aef19ff05
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-29 06:18:21 +0000

    pyinvoke: update 1.3.0_1 bottle.

--- Formula/pyinvoke.rb
+++ Formula/pyinvoke.rb
@@ -10,9 +10,9 @@ class Pyinvoke < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f0d3437c7059392b0656e61aa598292f5de95a04dbf3a1b33497ad789cc797d9" => :catalina
-    sha256 "c29ca3258dede2769616b5134911729b102b7de7101d157a4ce34afd73c2d16d" => :mojave
-    sha256 "385bb92f31b81c09dbc329d2f5fcab8fbcfc8608a65cdf924eef23ed9ef38ed0" => :high_sierra
+    sha256 "72fa0ce37ad63f0ad409efe0bfea09e9ff32647cab0916b10a8a143705da0e5f" => :catalina
+    sha256 "5f5d6d6de395b596f9fe65d166a61fcb8d2b24775dde2f9e5b4b44d86c2110e1" => :mojave
+    sha256 "60857ae8855ac13fd79edc46985f825316bee7a5e25590367a45bda77dbc29a9" => :high_sierra
   end
 
   depends_on "python@3.8"
