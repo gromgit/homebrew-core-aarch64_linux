1cd161775c6e60fe2c0dccd5af9cc61e40d3d76f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-30 14:12:01 +0000

    pianod: update 301 bottle.

--- Formula/pianod.rb
+++ Formula/pianod.rb
@@ -5,9 +5,9 @@ class Pianod < Formula
   sha256 "d6fa01d786af65fe3b4e6f4f97fa048db6619b9443e23f655d3ea8ab4766caee"
 
   bottle do
-    sha256 "6c0dee18b03c7dfd0c4d752164b7e86edd84098f3dfccd4e248d4e2085314988" => :catalina
-    sha256 "f5f17916dd35eb4ec45b7ad25ed7b82cd7f8b7dd4cd53846b4fd023e331cd622" => :mojave
-    sha256 "0d3430afe66553f3b3a87798766ff1bccc2972ed2867a363ebf6d1b8a3ae57b1" => :high_sierra
+    sha256 "6698a4353e50aed190386ccdf407af24f6b623691ebabdef7f8a0b7dca77b338" => :catalina
+    sha256 "c0dfa015b5c5546a2fc4788d716c00520282773a93e7d6669f891f2b1126f587" => :mojave
+    sha256 "d7c6ba07d0b46d393c4dc5de718e4dcd3130d6b0f8f2ee5aa5d00efd8ec69e93" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
