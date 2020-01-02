13e054099b2eaa7c57f3796280614eeac5d92dea
Rasika Perera <info.rasika@gmail.com>
2020-01-02 18:01:16 +0530

    ballerina 1.1.0 (#48466)

--- Formula/ballerina.rb
+++ Formula/ballerina.rb
@@ -1,8 +1,8 @@
 class Ballerina < Formula
   desc "A Programming Language for Network Distributed Applications"
-  homepage "https://v1-0.ballerina.io"
-  url "https://product-dist.ballerina.io/downloads/1.0.5/jballerina-1.0.5.zip"
-  sha256 "75254fb25a41fc11e5ee6feef0ed76357e42ed3071656c52dc1501f1b5db8ab4"
+  homepage "https://ballerina.io"
+  url "https://product-dist.ballerina.io/downloads/1.1.0/ballerina-1.1.0.zip"
+  sha256 "801ca6c9d1db44156ad724a9ea10ecab97bb35c641faf3fc35afc97bb68f7855"
 
   bottle :unneeded
 
@@ -14,16 +14,8 @@ class Ballerina < Formula
 
     chmod 0755, "bin/ballerina"
 
-    inreplace ["bin/ballerina"] do |s|
-      s.gsub! /^BALLERINA_HOME=.*$/, "BALLERINA_HOME=#{libexec}"
-      s.gsub! /\r?/, ""
-    end
-
     bin.install "bin/ballerina"
     libexec.install Dir["*"]
-    # Add symlinks for the Language Server
-    prefix.install_symlink libexec/"bre"
-    prefix.install_symlink libexec/"lib"
     bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
   end
 
