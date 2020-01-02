7ac88aefbd6efabe3c2db76db222b11f2eb559a3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-02 20:59:32 +0000

    zim: update 0.72.1 bottle.

--- Formula/zim.rb
+++ Formula/zim.rb
@@ -7,10 +7,9 @@ class Zim < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "eca69f2e3973da487990bf3c2402284bea3aca6e51314f9bc795a2be3cdca34f" => :catalina
-    sha256 "d8d963db3b50abc419b0541d5f707b97edb6c2df1cda7726968ede24eece287f" => :mojave
-    sha256 "d8d963db3b50abc419b0541d5f707b97edb6c2df1cda7726968ede24eece287f" => :high_sierra
-    sha256 "0a87821d6ce7c311fef0fb1de9c74f561a30aa51342318a466edda0be0da0b53" => :sierra
+    sha256 "91f15636c51398dc31f7c43d570a652cc1bddb8fc3571bdfc203d25231aa095d" => :catalina
+    sha256 "91f15636c51398dc31f7c43d570a652cc1bddb8fc3571bdfc203d25231aa095d" => :mojave
+    sha256 "91f15636c51398dc31f7c43d570a652cc1bddb8fc3571bdfc203d25231aa095d" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
