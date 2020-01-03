13e3ead1abf0f23eff164b526aaf243142f60c82
Christian Moritz <chrmoritz@gmail.com>
2020-01-03 17:36:57 +0100

    deno 0.28.1
    Closes #48510.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/deno.rb
+++ Formula/deno.rb
@@ -1,9 +1,9 @@
 class Deno < Formula
   desc "Command-line JavaScript / TypeScript engine"
   homepage "https://deno.land/"
-  url "https://github.com/denoland/deno/releases/download/v0.28.0/deno_src.tar.gz"
-  version "0.28.0"
-  sha256 "dcccd977ccac2ae31dc97bf390c1609afc74fd3f15d5daefdacb67981bab9eb3"
+  url "https://github.com/denoland/deno/releases/download/v0.28.1/deno_src.tar.gz"
+  version "0.28.1"
+  sha256 "6b48749fc14de0262171e6cbf968363a3307e5b0c5aaf6ca55196d6c1a151a25"
 
   bottle do
     cellar :any_skip_relocation
