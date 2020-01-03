d41157e3fbf468d1c2ff92567560e5f75efc20c0
n-ham <nicholas.charles.ham@gmail.com>
2020-01-04 02:11:35 +1100

    nift 2.0
    Closes #48509.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/nift.rb
+++ Formula/nift.rb
@@ -1,8 +1,8 @@
 class Nift < Formula
   desc "Cross-platform open source framework for managing and generating websites"
   homepage "https://nift.cc/"
-  url "https://github.com/nifty-site-manager/nsm/archive/v1.17.tar.gz"
-  sha256 "cdcc3b54d9d98c70a95e14396d09588017fdb2ecfcc821a96778251577b9bafe"
+  url "https://github.com/nifty-site-manager/nsm/archive/v2.0.tar.gz"
+  sha256 "86736893617433e84c1adc1a664ef94ebca53eec77028c3bb5c8b1a10d8a4d1e"
 
   bottle do
     cellar :any_skip_relocation
@@ -18,7 +18,7 @@ class Nift < Formula
 
   test do
     mkdir "empty" do
-      system "#{bin}/nsm", "init"
+      system "#{bin}/nsm", "init-html"
       assert_predicate testpath/"empty/site/index.html", :exist?
     end
   end
