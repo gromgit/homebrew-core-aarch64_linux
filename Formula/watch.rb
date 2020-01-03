f8344450f4ef574589056c66afcda65fc24ef21f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-03 04:03:35 +0000

    watch: update 3.3.16 bottle.

--- Formula/watch.rb
+++ Formula/watch.rb
@@ -7,12 +7,10 @@ class Watch < Formula
   head "https://gitlab.com/procps-ng/procps.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "a0955d44620cfbbd8b191649a4924cd1526f10e786929c036caff0de1258c0c1" => :catalina
-    sha256 "bdb4f8a1feed527be937eb0f470444c93643b3dc72943387fcd7584c2b96baf6" => :mojave
-    sha256 "9fceef6cae551481726f86f9a0e5e79ca2bf27e0f41d0feb0800ab25e9161342" => :high_sierra
-    sha256 "010375a88535763436571b3140bacd5733e8176621d663464a8ae3c57ed7813f" => :sierra
-    sha256 "d89d82028efa1586bd6f6b05fba3f7c15259e6a9fedffa5e36d8a900514b0ecb" => :el_capitan
+    cellar :any
+    sha256 "011e48363fe62c7a4ba0dec746d5964ee25545bde7855a31db0cb411420df190" => :catalina
+    sha256 "d17aba6f80c530e6b91b30e088df60aab3dc84b2f57ca7499928322438b9f0d2" => :mojave
+    sha256 "13d63ebc419965182d98f10b25cc99961679e1ba5ce6118e66b7037eb4e78de8" => :high_sierra
   end
 
   depends_on "autoconf" => :build
