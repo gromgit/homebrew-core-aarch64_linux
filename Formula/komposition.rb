7557e86f658cc3ae4abe627b92919538388c098d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-04 18:21:10 +0000

    komposition: add 0.2.0 bottle.

--- Formula/komposition.rb
+++ Formula/komposition.rb
@@ -9,6 +9,12 @@ class Komposition < Formula
   sha256 "cedb41c68866f8d6a87579f566909fcd32697b03f66c0e2a700a94b6a9263b88"
   head "https://github.com/owickstrom/komposition.git"
 
+  bottle do
+    sha256 "f28f804f8ca5d9c9c23f8fd9d35edb3276e2d397abbbf73fda33b543d5654611" => :catalina
+    sha256 "2f7008e5a901c7c4739104124c0a3faea211f3d7509c0c19f6a28f8de162be08" => :mojave
+    sha256 "75c13e6a1a9f53273974f6445d4d59adc76c16399480f305547e7bddd5237fbd" => :high_sierra
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
   depends_on "pkg-config" => :build
