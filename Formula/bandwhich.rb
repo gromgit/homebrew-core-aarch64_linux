26dba1ebfaefd0b3b26a053abb483f3acaa26d3d
BSKY <git@bsky.moe>
2020-01-06 03:43:58 +0900

    bandwhich 0.7.0 (new formula)
    Closes #48504.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

---
+++ Formula/bandwhich.rb
@@ -0,0 +1,17 @@
+class Bandwhich < Formula
+  desc "Terminal bandwidth utilization tool"
+  homepage "https://github.com/imsnif/bandwhich"
+  url "https://github.com/imsnif/bandwhich/archive/0.7.0.tar.gz"
+  sha256 "6f08b0e1bf07fc8397bf9d4c5e8211368dbbcad4c0cfc2c369066f711a343152"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    output = shell_output "#{bin}/bandwhich --interface bandwhich", 2
+    assert_match output, "Error: Cannot find interface bandwhich"
+  end
+end
