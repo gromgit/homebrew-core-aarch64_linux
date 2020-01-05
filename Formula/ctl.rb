51eff06faf6d566402f43c86ad236772412d326f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-05 23:59:50 +0000

    ctl: update 1.5.2_4 bottle.

--- Formula/ctl.rb
+++ Formula/ctl.rb
@@ -6,10 +6,9 @@ class Ctl < Formula
   revision 4
 
   bottle do
-    sha256 "0ad490aa788bd360a656878b50d31d3a943932072ab51c8b87a01ccfae9f8548" => :catalina
-    sha256 "ab6441016e0027ca393e6c9a92224c5a684a5476cebd9fdb5c48f42dda81e5b2" => :mojave
-    sha256 "fe9ebf7a5d7f115a0ba08ad2d72b21a65315ad213cc8d7cbda02ffba96ef8fde" => :high_sierra
-    sha256 "ab156768e3ae9f46aac1dc14ee0aed58173657ab0cadbb6e85f43bf68a6fd4c6" => :sierra
+    sha256 "e76d1577ae18eff19a8a4b82941ab42d1be88e0cede2e6f53daaf611363a1eee" => :catalina
+    sha256 "42fae5bdcc9ebd6fdcf65048748659a0f5d27169396cf4dc2aef6668263ff8a4" => :mojave
+    sha256 "1a63d9afedf0845adb9daf5b7700c5715217e477b99453dc02c50be0eb9e8565" => :high_sierra
   end
 
   depends_on "cmake" => :build
