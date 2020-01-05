ece9b112b31e033fbe67c9bb9697d441f34b361a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-05 18:57:00 +0000

    bandwhich: add 0.7.0 bottle.

--- Formula/bandwhich.rb
+++ Formula/bandwhich.rb
@@ -4,6 +4,13 @@ class Bandwhich < Formula
   url "https://github.com/imsnif/bandwhich/archive/0.7.0.tar.gz"
   sha256 "6f08b0e1bf07fc8397bf9d4c5e8211368dbbcad4c0cfc2c369066f711a343152"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "de672cda260bbc24b82f72405d3b7dc33b75f966dc46d52f48c5eee48a5376fe" => :catalina
+    sha256 "5bfeb1d3bc8875c315a19a8f824de62beb74f2475be2aa4a6ce3663337a81a68" => :mojave
+    sha256 "1a701120a7f38409071f308e40560ff793e81d69b184a812e76e960de82755df" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
