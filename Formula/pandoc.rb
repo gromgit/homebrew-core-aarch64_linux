77e8c702169f12845ee8e8dd85a3d92cbc8afa08
homebrew-updater <bepsvpt/homebrew-updater>
2020-01-06 14:05:38 +0800

    pandoc 2.9.1.1
    Closes #48570.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/pandoc.rb
+++ Formula/pandoc.rb
@@ -5,8 +5,8 @@ class Pandoc < Formula
 
   desc "Swiss-army knife of markup format conversion"
   homepage "https://pandoc.org/"
-  url "https://hackage.haskell.org/package/pandoc-2.9.1/pandoc-2.9.1.tar.gz"
-  sha256 "0a543566be3468a0283d08cf6f3cda44c70de46b52755d902277273450d78bfb"
+  url "https://hackage.haskell.org/package/pandoc-2.9.1.1/pandoc-2.9.1.1.tar.gz"
+  sha256 "9d21c5efe2074f9b3097a20e0798de9d8b89a86a1ce04a307f476c7b4aa3816d"
   head "https://github.com/jgm/pandoc.git"
 
   bottle do
