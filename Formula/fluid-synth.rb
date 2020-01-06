09b598993bdee78214c26433b86229f7cf5f45fc
Foster McLane <fkmclane@gmail.com>
2020-01-06 12:28:04 -0500

    fluid-synth 2.1.1
    Closes #48863.
    
    Signed-off-by: Izaak Beekman <zbeekman@gmail.com>

--- Formula/fluid-synth.rb
+++ Formula/fluid-synth.rb
@@ -1,8 +1,8 @@
 class FluidSynth < Formula
   desc "Real-time software synthesizer based on the SoundFont 2 specs"
   homepage "http://www.fluidsynth.org"
-  url "https://github.com/FluidSynth/fluidsynth/archive/v2.0.8.tar.gz"
-  sha256 "0c37e72db31d1b35e587b94b7163d68444cffaa9e7fe8a293d79957620bff117"
+  url "https://github.com/FluidSynth/fluidsynth/archive/v2.1.1.tar.gz"
+  sha256 "966d0393591b505d694e51cbf653387007144e9ae0b8705d82ec7d943d31d348"
   head "https://github.com/FluidSynth/fluidsynth.git"
 
   bottle do
@@ -18,6 +18,11 @@ class FluidSynth < Formula
   depends_on "libsndfile"
   depends_on "portaudio"
 
+  resource "example_midi" do
+    url "https://upload.wikimedia.org/wikipedia/commons/6/61/Drum_sample.mid"
+    sha256 "a1259360c48adc81f2c5b822f221044595632bd1a76302db1f9d983c44f45a30"
+  end
+
   def install
     args = std_cmake_args + %w[
       -Denable-framework=OFF
@@ -31,9 +36,15 @@ class FluidSynth < Formula
       system "cmake", "..", *args
       system "make", "install"
     end
+
+    pkgshare.install "sf2"
   end
 
   test do
-    assert_match /#{version}/, shell_output("#{bin}/fluidsynth --version")
+    # Synthesize wav file from example midi
+    resource("example_midi").stage testpath
+    wavout = testpath/"Drum_sample.wav"
+    system bin/"fluidsynth", "-F", wavout, pkgshare/"sf2/VintageDreamsWaves-v2.sf2", testpath/"Drum_sample.mid"
+    assert_predicate wavout, :exist?
   end
 end
