f12a4a334efc0f612191154ea76282c506061dc2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-06 20:15:53 +0000

    rsstail: update 2.1 bottle.

--- Formula/rsstail.rb
+++ Formula/rsstail.rb
@@ -7,10 +7,10 @@ class Rsstail < Formula
 
   bottle do
     cellar :any
-    sha256 "043bb4c59d45bf3d10e8a80c16dd684267c7ee905fd3384d6f197dff9d9ca686" => :mojave
-    sha256 "972ee73523d6f2f90ad8cefea3be3d800ed56ef86f5ab0aa27a9868959e8c4f6" => :high_sierra
-    sha256 "288a7d9668a50fc1db8525be5c1ef3a932e134b095bb6d0b5a785353ca8b6d59" => :sierra
-    sha256 "671b60685eabd26014203b5cd2eff8a94ee940e139c77793b379e1114c23a912" => :el_capitan
+    rebuild 1
+    sha256 "253a99c8187e0dc6fb29049273f08dd4b199c0de1e9171c4a0829f21aaf5c90f" => :catalina
+    sha256 "b6f2a222c1bc903a5d0179331398ced65980798d694d186bd52e0b54239d9dfd" => :mojave
+    sha256 "29b1cd5b6cbfbd66d250586450e2e24e5706da80b03aa1b54834bd0c01e73202" => :high_sierra
   end
 
   depends_on "libmrss"
