457d4bc6ce1509a3e375d4a90524a61760e644de
Rui Chen <chenrui333@gmail.com>
2020-01-06 18:17:32 -0500

    teleport 4.2.1
    Closes #48617.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/teleport.rb
+++ Formula/teleport.rb
@@ -1,8 +1,8 @@
 class Teleport < Formula
   desc "Modern SSH server for teams managing distributed infrastructure"
   homepage "https://gravitational.com/teleport"
-  url "https://github.com/gravitational/teleport/archive/v4.2.0.tar.gz"
-  sha256 "2e4595975ea1751466d1e9382aefd76f7e8eff15c953930227c06602013c5fb6"
+  url "https://github.com/gravitational/teleport/archive/v4.2.1.tar.gz"
+  sha256 "e9c515bd3dd5e85578d8f021b648d9ba008fecf4617a0bdab6190ee896d9cc2d"
   head "https://github.com/gravitational/teleport.git"
 
   bottle do
