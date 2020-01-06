4ab3683e1eaebd1f213fd85e8d33b3c5bd130d99
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-06 13:26:10 +0000

    dtrx: update 7.1 bottle.

--- Formula/dtrx.rb
+++ Formula/dtrx.rb
@@ -7,12 +7,10 @@ class Dtrx < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c88f2dfca39e978e4d49d24e88fcdb479936adcc4773cec913b312a0c6f4dc0a" => :mojave
-    sha256 "c159f3897c3de8b840beb2dba4aba058262b932b06650ae72ef4c86841fcfdbe" => :high_sierra
-    sha256 "812e92d3899f795921dced2d1085dec1258a659595e1a0545e67a03be653b9ca" => :sierra
-    sha256 "5419cb8b5327a5770d1d8a541a11c47f9195885964af3977110a4bd752a63ff7" => :el_capitan
-    sha256 "15359cfa40144872520eba6cdb132a1bfc0534b66cfd87c6efab3d57221ba66f" => :yosemite
-    sha256 "c023c31dfc27c2a198ff310bfa0438fac034209f39e42800ee5d462bb2be3e6b" => :mavericks
+    rebuild 1
+    sha256 "ff3e436e50818837831ef16ca26f7d2760603bdd44d97fffefdc28dcde5fed2b" => :catalina
+    sha256 "a63cfb21c2db4163521db20851b581d739a6707315fbb0f690f470ff697c8965" => :mojave
+    sha256 "a63cfb21c2db4163521db20851b581d739a6707315fbb0f690f470ff697c8965" => :high_sierra
   end
 
   depends_on "p7zip"
