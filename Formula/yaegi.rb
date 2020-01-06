30dc133b36884416dfdba52d810fa15f8f32ba83
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-06 14:06:29 +0000

    yaegi: add 0.7.2 bottle.

--- Formula/yaegi.rb
+++ Formula/yaegi.rb
@@ -5,6 +5,13 @@ class Yaegi < Formula
   sha256 "1f2e63290614e26ffb82ef16af23d0ce5237c10a19a5c1191c22398adb62bc1e"
   head "https://github.com/containous/yaegi.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5517621b05ea0055cfe8796f0f8647c93d6c45b34e0255b4db55dec36e15bac8" => :catalina
+    sha256 "2acdb0bd72e300f83ad712dd7e4ad5be2d12d93ac12a86ef68a065b297e870c0" => :mojave
+    sha256 "e4d8208996f9595298c4602e71c29d4e6862854036cb5b758958adffbe5336ff" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
