eea636bec6205c5654f82707ec4364dc2702a93c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-06 18:47:03 +0000

    b43-fwcutter: update 019 bottle.

--- Formula/b43-fwcutter.rb
+++ Formula/b43-fwcutter.rb
@@ -7,12 +7,10 @@ class B43Fwcutter < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "51b5702dd05e2afc1f8f0e4c10ccf38023610706fae51112a220284bd9976daa" => :mojave
-    sha256 "abbde23073e94982c1f47ea1884abf4b855f8ab0ed8d0e2434de4ca32bfc8dca" => :high_sierra
-    sha256 "f5238356964126642e9dd5c88de5564f221db86d488ca826055629d504a71426" => :sierra
-    sha256 "d24bbb9e1669ef33319b23a37a8ebd4dddaf8d1ce2c9e24abff13d678e3633d4" => :el_capitan
-    sha256 "fed62452f6d8b74976575b0b2fc3f5fac351981ac85768160bb188a6c55ff170" => :yosemite
-    sha256 "b2d662d6f951714738626f19698922875b4f97d149fbc8a79aeac0034f75d594" => :mavericks
+    rebuild 1
+    sha256 "ba4d98184260e69c16c721c905e281f8638a34f8785ce43216093afb55305f22" => :catalina
+    sha256 "0f3c2998b168c9f614f5b28e1e219acada8cdd86018706fe30c46a9b0850d73e" => :mojave
+    sha256 "59c187f34c0f3e3eb99e7da175fcec4bc69cd4baea62e053898c985bef63ecf5" => :high_sierra
   end
 
   def install
