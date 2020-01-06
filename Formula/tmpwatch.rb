f6806903ea021c54f42e16c3044fe95ce2b96e41
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-06 14:50:18 +0000

    tmpwatch: update 2.11 bottle.

--- Formula/tmpwatch.rb
+++ Formula/tmpwatch.rb
@@ -6,12 +6,10 @@ class Tmpwatch < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "727f356600eb58bb27a23ab91a4ffc8904eec726c2e9f85025d810e3a19030eb" => :mojave
-    sha256 "18ca8e16075315e3aca9cd4e2e445358c33d5911a647557ad8971edb232e4cc4" => :high_sierra
-    sha256 "945dbe942b232586517a2fcd81faab1fc20d3936e53ddf9154158dd0b7d3d55c" => :sierra
-    sha256 "d19ca779df5c019f840d0c186822fbab4758bf3635101aa975b4bad35bb2f184" => :el_capitan
-    sha256 "6b7935b74c118e797b2cba298e0f546d4231ac2c5eb165f4a63bc6c2d0a372dc" => :yosemite
-    sha256 "0083a3dd898627e47dabc967328de86df6bf60c2c62210fd9dd4846795de0a8f" => :mavericks
+    rebuild 1
+    sha256 "d2e77af7339bd09c9b7352a1646c79e15db6b43c27537b4af59efbc51e23f114" => :catalina
+    sha256 "4f7b1e540daebe79e1ad64b2e4e4b7214074c05150dcd9de7a5ffe9c12e6b9bb" => :mojave
+    sha256 "24a734b4cf32ce5720e1c089060fa66dcc2d9dde437804a4dc147f81d9cd8512" => :high_sierra
   end
 
   def install
