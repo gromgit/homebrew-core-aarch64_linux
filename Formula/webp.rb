949238270e05ca94fd099c4f2206e069c3616144
Caleb Xu <calebcenter@live.com>
2020-01-07 14:22:25 -0500

    webp 1.1.0
    Closes #48685.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/webp.rb
+++ Formula/webp.rb
@@ -1,8 +1,8 @@
 class Webp < Formula
   desc "Image format providing lossless and lossy compression for web images"
   homepage "https://developers.google.com/speed/webp/"
-  url "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.0.3.tar.gz"
-  sha256 "e20a07865c8697bba00aebccc6f54912d6bc333bb4d604e6b07491c1a226b34f"
+  url "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.1.0.tar.gz"
+  sha256 "98a052268cc4d5ece27f76572a7f50293f439c17a98e67c4ea0c7ed6f50ef043"
 
   bottle do
     cellar :any
