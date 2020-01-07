b52b367ec0df4e901fb1c0e4069cce66986f092c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-07 16:44:48 +0000

    libb64: add 1.2.1 bottle.

--- Formula/libb64.rb
+++ Formula/libb64.rb
@@ -4,6 +4,13 @@ class Libb64 < Formula
   url "https://downloads.sourceforge.net/project/libb64/libb64/libb64/libb64-1.2.1.zip"
   sha256 "20106f0ba95cfd9c35a13c71206643e3fb3e46512df3e2efb2fdbf87116314b2"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ee788cea9a073c456398f86661ea27e092c3d327ea8b0792643ed7d38bd402d0" => :catalina
+    sha256 "7a53661bc0fe2bea875f0a14cc66b6352694f1e797e4506ad2c8ef35dfcc2169" => :mojave
+    sha256 "3b5266e99b78c81ae7ec00c19aa7e9d3fad7f47611fc1332710bd8e03f48da55" => :high_sierra
+  end
+
   def install
     system "make", "all_src"
     include.install "include/b64"
