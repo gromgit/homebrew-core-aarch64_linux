a92577407fa1e3cc389e015ee9f2810f5188cf36
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-07 19:56:58 +0000

    libofx: update 0.9.15 bottle.

--- Formula/libofx.rb
+++ Formula/libofx.rb
@@ -5,10 +5,9 @@ class Libofx < Formula
   sha256 "e95c14e09fc37b331af3ef4ef7bea29eb8564a06982959fbd4bca7e331816144"
 
   bottle do
-    rebuild 1
-    sha256 "95cee3bf5da4fcb5eaeb73a4970632d2786b0f12b3b97a0e919c4d29336d8c08" => :catalina
-    sha256 "f04974342f8b883cb1248e27ff12dfd347d5e6c201852ed0d816ed4852c5be86" => :mojave
-    sha256 "591595de707127624906cafcb8d1271a6d907f4a3cbe2311c93bff53a969937a" => :high_sierra
+    sha256 "aa4c73d9fe09d54bc4fb0a1dde14bd927949f4d0ce100dae987f03df79236958" => :catalina
+    sha256 "9b731e873dee237f2723fd05aa7f88b0e64f29197297c33e9def68112d7c2fc8" => :mojave
+    sha256 "7c561c3c928ad133d1763afe6a9d25b784d236411f00085151740f3505b164b3" => :high_sierra
   end
 
   depends_on "open-sp"
