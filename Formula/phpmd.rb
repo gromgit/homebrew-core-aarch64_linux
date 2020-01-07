1dde8bd392b296ec69c9b1a72ab859e38a09253c
Caleb Xu <calebcenter@live.com>
2020-01-07 16:18:37 -0500

    phpmd 2.8.1
    Closes #48708.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/phpmd.rb
+++ Formula/phpmd.rb
@@ -1,8 +1,8 @@
 class Phpmd < Formula
   desc "PHP Mess Detector"
   homepage "https://phpmd.org"
-  url "https://github.com/phpmd/phpmd/releases/download/2.7.0/phpmd.phar"
-  sha256 "06dca1d8d77eadde31ddb3e60299a1e53363039e94bd691c3e23639f5f4730b0"
+  url "https://github.com/phpmd/phpmd/releases/download/2.8.1/phpmd.phar"
+  sha256 "86b2f14553e23f9bf05e5231b366aab41357a72159c7f0f85c275dca5ce332e6"
 
   bottle :unneeded
 
