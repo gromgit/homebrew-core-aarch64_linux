1ac1dab210dd8d3c9321ddeabeca8152455a6269
Alexander Bayandin <a.bayandin@gmail.com>
2020-01-08 01:50:11 +0000

    aws-okta: remove Catalina bottle
    Closes #48728.

--- Formula/aws-okta.rb
+++ Formula/aws-okta.rb
@@ -7,7 +7,6 @@ class AwsOkta < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
-    sha256 "df6a64ab1b0222ac556838148ee26dc509b4b9e7e562940fe481b52edd97c913" => :catalina
     sha256 "54de6445f3afddf77a59a4078d7381fd614fc80ef6e5fbc63e2d3de777af0517" => :mojave
     sha256 "ff1c50c64e064d16731dd30cf672999148ff98fdae81f85ea8c4c05eebf1a30f" => :high_sierra
   end
