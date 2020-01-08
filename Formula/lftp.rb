78683be9858161ae50ba11ee14c6423c2258d8f6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-08 00:56:50 +0000

    lftp: update 4.9.0 bottle.

--- Formula/lftp.rb
+++ Formula/lftp.rb
@@ -5,9 +5,9 @@ class Lftp < Formula
   sha256 "0b3b659e1969a31827a25861c01ccf71ac6d3f20ee256bdf6999d653e031a24e"
 
   bottle do
-    sha256 "ae15b85b1e7cd7d8f42eacb78f9a231427a4374371c62d5a82ac899b72eeee0e" => :catalina
-    sha256 "6861fecd8432bb8877144bf61df25f052509d1ffd692b9f72e2bb9d86b542750" => :mojave
-    sha256 "b45767d676b23b29eab6b820057820adcac582304ea44ac7926060407c63d072" => :sierra
+    sha256 "fb5f0306c383dd4d19d406529aa15b5468080bbb11b22ef0de4b225cdd981a25" => :catalina
+    sha256 "38f687b927d39fc37f4ff84e35086eeadde61826732b21bf3ec12b091b779fdc" => :mojave
+    sha256 "bbf5aadf64bdf2e7334006c1a04512bd62417e4d74239db44f134fcd2e524d3c" => :high_sierra
   end
 
   depends_on "libidn"
