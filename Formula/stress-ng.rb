a0feb15f1a6cdd95538ba0a002d77b49ed2dda0c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-08 00:44:40 +0000

    stress-ng: update 0.10.15 bottle.

--- Formula/stress-ng.rb
+++ Formula/stress-ng.rb
@@ -6,9 +6,9 @@ class StressNg < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e52795fd0d34109f2eb431b6d84000559ee898c1e56e2e9ba1cbe8cd7711e9b1" => :catalina
-    sha256 "921174484d1907176a15602ed29ee281ff2a60266a5436cca78e02daaeccb80b" => :mojave
-    sha256 "ac5bb01106618d99e6b503ab019c17cd74105a6aba77635369acf123c144c26b" => :high_sierra
+    sha256 "d21d756399b121a80c892c5206df690baf883535ef162b8e96d83b55f046d187" => :catalina
+    sha256 "ecb9034b1dee524a05d8d98fdf1ae855ca2496281157787339ece6eb866afb00" => :mojave
+    sha256 "90701c539a910d95c13dc419e2bcfbdd4ecdf1908cbce9cf41c11deb87923558" => :high_sierra
   end
 
   depends_on :macos => :sierra
