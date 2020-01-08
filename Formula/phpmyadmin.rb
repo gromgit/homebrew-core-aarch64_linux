0690334035167d7c6aadbaf315fc258ba207fcf8
homebrew-updater <bepsvpt/homebrew-updater>
2020-01-08 11:00:14 +0800

    phpmyadmin 5.0.1
    Closes #48747.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/phpmyadmin.rb
+++ Formula/phpmyadmin.rb
@@ -1,8 +1,8 @@
 class Phpmyadmin < Formula
   desc "Web interface for MySQL and MariaDB"
   homepage "https://www.phpmyadmin.net"
-  url "https://files.phpmyadmin.net/phpMyAdmin/5.0.0/phpMyAdmin-5.0.0-all-languages.tar.gz"
-  sha256 "56589e624236778b63cfb221f7752371e4e87192c342a8f2492818ae04e31298"
+  url "https://files.phpmyadmin.net/phpMyAdmin/5.0.1/phpMyAdmin-5.0.1-all-languages.tar.gz"
+  sha256 "1bd82fdcf5375526c8b5124a20968b69981724307d878321ae29499c61bbf48e"
 
   bottle :unneeded
 
