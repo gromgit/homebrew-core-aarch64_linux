9454146e59e497364441941687a4c3d780916f1b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-08 01:34:32 +0000

    openimageio: update 2.1.10 bottle.

--- Formula/openimageio.rb
+++ Formula/openimageio.rb
@@ -7,9 +7,9 @@ class Openimageio < Formula
   head "https://github.com/OpenImageIO/oiio.git"
 
   bottle do
-    sha256 "64357845812ac92f7fb181c6f66ab43ef44d0f00d43c95cc55f57a4ee0a34a6c" => :catalina
-    sha256 "84e594c3b026951721ea29b02d2dfd221c83ac14415763ad23bea0d4a8da8584" => :mojave
-    sha256 "8b439e9b74fd360efab6909ffe6cdab414e6bfc9ae740fc6c3fef1254240e63e" => :high_sierra
+    sha256 "d3cac1c5ddd3e49daffea044b54d8a7ba5100ce21d72564e5b379725f24aee96" => :catalina
+    sha256 "8b78a1ef94bb5b4ae2552500437da9379b5797ee99a8df05007b1880649d6805" => :mojave
+    sha256 "59acecbe8f52b94060f206b994e5bcdcfc631beeb7bec37e5071e9ddd41f5d91" => :high_sierra
   end
 
   depends_on "cmake" => :build
