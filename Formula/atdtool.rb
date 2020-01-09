d1bcbcea1878f3ae3bdae809f752b4863e18236f
Alexander Bayandin <a.bayandin@gmail.com>
2020-01-09 20:36:45 +0000

    atdtool: revision bump for python@3.8

--- Formula/atdtool.rb
+++ Formula/atdtool.rb
@@ -1,9 +1,11 @@
 class Atdtool < Formula
+  include Language::Python::Virtualenv
+
   desc "Command-line interface for After the Deadline language checker"
   homepage "https://github.com/lpenz/atdtool"
-  url "https://github.com/lpenz/atdtool/archive/upstream/1.3.3.tar.gz"
-  sha256 "3e928721388cf6f58b7e663ebc5508f26d180b1c07d5b8119212356c66e57fe8"
-  revision 1
+  url "https://files.pythonhosted.org/packages/83/d1/55150f2dd9afda92e2f0dcb697d6f555f8b1f578f1df4d685371e8b81089/atdtool-1.3.3.tar.gz"
+  sha256 "a83f50e7705c65e7ba5bc339f1a0624151bba9f7cdec7fb1460bb23e9a02dab9"
+  revision 2
 
   bottle do
     cellar :any_skip_relocation
@@ -13,15 +15,10 @@ class Atdtool < Formula
     sha256 "2de45317e5c51f1fcb7365d038e667fcbf48f333af9c49eb0a27ddce2d2b1e57" => :sierra
   end
 
-  depends_on "python"
+  depends_on "python@3.8"
 
   def install
-    xy = Language::Python.major_minor_version "python3"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
-    system "python3", *Language::Python.setup_install_args(libexec)
-    bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
-    prefix.install libexec/"share"
+    virtualenv_install_with_resources
   end
 
   test do
