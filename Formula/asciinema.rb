8fb23ed88f00a4e6340730627095a2dbccdbce4e
Alexander Bayandin <a.bayandin@gmail.com>
2020-01-09 14:35:16 +0000

    asciinema: revision bump for python@3.8

--- Formula/asciinema.rb
+++ Formula/asciinema.rb
@@ -1,8 +1,11 @@
 class Asciinema < Formula
+  include Language::Python::Virtualenv
+
   desc "Record and share terminal sessions"
   homepage "https://asciinema.org"
-  url "https://github.com/asciinema/asciinema/archive/v2.0.2.tar.gz"
-  sha256 "2578a1b5611e5375771ef6582a6533ef8d40cdbed1ba1c87786fd23af625ab68"
+  url "https://files.pythonhosted.org/packages/a7/71/771c859795e02c71c187546f34f7535487b97425bc1dad1e5f6ad2651357/asciinema-2.0.2.tar.gz"
+  sha256 "32f2c1a046564e030708e596f67e0405425d1eca9d5ec83cd917ef8da06bc423"
+  revision 1
   head "https://github.com/asciinema/asciinema.git"
 
   bottle do
@@ -13,15 +16,10 @@ class Asciinema < Formula
     sha256 "29bd540035a1382c6a92e6fb8d47ca909694867c543a456b3f45c5a1ee319aec" => :sierra
   end
 
-  depends_on "python"
+  depends_on "python@3.8"
 
   def install
-    xy = Language::Python.major_minor_version "python3"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
-    system "python3", *Language::Python.setup_install_args(libexec)
-
-    bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    virtualenv_install_with_resources
   end
 
   test do
