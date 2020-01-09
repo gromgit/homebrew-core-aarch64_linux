d4004707368d81bd0ca39e243ad148d11bf08a85
Alexander Bayandin <a.bayandin@gmail.com>
2020-01-09 20:38:57 +0000

    git-plus: revision bump for python@3.8

--- Formula/git-plus.rb
+++ Formula/git-plus.rb
@@ -1,21 +1,17 @@
 class GitPlus < Formula
+  include Language::Python::Virtualenv
+
   desc "Git utilities: git multi, git relation, git old-branches, git recent"
   homepage "https://github.com/tkrajina/git-plus"
-  url "https://github.com/tkrajina/git-plus/archive/v0.3.3.tar.gz"
-  sha256 "42c296bd21f2c0ad5312b39c312718fba1b0a55d5cd167aa385fcdafe0fa1748"
+  url "https://files.pythonhosted.org/packages/72/75/5de42fceb6a7feb50386f29bd2a9d5391c90ba4e74ab78d86c095edd9f21/git-plus-v0.3.3.tar.gz"
+  sha256 "54fa88f82e52863dcf5f2d44c258a22e8d31232473300a4384eba8e2f71df1ea"
+  revision 1
   head "https://github.com/tkrajina/git-plus.git"
 
-  bottle :unneeded
-
-  depends_on "python"
+  depends_on "python@3.8"
 
   def install
-    xy = Language::Python.major_minor_version "python3"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
-    system "python3", *Language::Python.setup_install_args(libexec)
-
-    bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    virtualenv_install_with_resources
   end
 
   test do
