e8728831e357202f4825eab778c7c09efe70b3d5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-10 00:14:50 +0000

    git-plus: add 0.3.3_1 bottle.

--- Formula/git-plus.rb
+++ Formula/git-plus.rb
@@ -8,6 +8,13 @@ class GitPlus < Formula
   revision 1
   head "https://github.com/tkrajina/git-plus.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6bdcfe17d7687fad2e523e104b473744831806b1bf5915d9248a6d36dd6d28e6" => :catalina
+    sha256 "ee281bb04fe93d458c63dba2648861dd30e2003f5bb8871e650959134c6daca7" => :mojave
+    sha256 "72ec0aecb258232a837709d55ae03b4068db623c5f6b214bb2cbedd826c1152e" => :high_sierra
+  end
+
   depends_on "python@3.8"
 
   def install
