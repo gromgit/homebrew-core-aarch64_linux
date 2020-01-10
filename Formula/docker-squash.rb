070858dfa6da45f32c5c7174beb3fa3efd84cbf6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-10 00:14:51 +0000

    docker-squash: update 1.0.8_1 bottle.

--- Formula/docker-squash.rb
+++ Formula/docker-squash.rb
@@ -9,9 +9,9 @@ class DockerSquash < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "919fd1cdcde2e6a161987ca831205daac0b63eff8e6de78cc225e04f75e00e4c" => :catalina
-    sha256 "919fd1cdcde2e6a161987ca831205daac0b63eff8e6de78cc225e04f75e00e4c" => :mojave
-    sha256 "919fd1cdcde2e6a161987ca831205daac0b63eff8e6de78cc225e04f75e00e4c" => :high_sierra
+    sha256 "8ead2027bde16b9a95669e7cf27c4a2bbc62062866d3f22fb689ee265e648e93" => :catalina
+    sha256 "f8fffacef91f2f33bffd9b48a7d3e7441e1b9518ec3b8bff3692c2c96ab048e2" => :mojave
+    sha256 "a7620d50b1b463b766f034a0369e2b78fefc4cad238511b8e7e135638be69a8a" => :high_sierra
   end
 
   depends_on "python@3.8"
