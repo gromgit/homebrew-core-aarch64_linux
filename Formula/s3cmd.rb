b16a726762739ac95c7f8b1ad7925c9b6640adb8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-10 00:14:51 +0000

    s3cmd: update 2.0.2_2 bottle.

--- Formula/s3cmd.rb
+++ Formula/s3cmd.rb
@@ -10,10 +10,9 @@ class S3cmd < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0c9218029b1ab059996b6c2e841fd93d69e4bf08e810b3aff6fa4be59416f17d" => :catalina
-    sha256 "189112abb6e81acbbbd4977a3d573cf090dd7c85fee032642c5a9164ccb03046" => :mojave
-    sha256 "182902e2ea25497262b58393a95b34a0961a1d83dc2702d357c5f3d06d1baecf" => :high_sierra
-    sha256 "182902e2ea25497262b58393a95b34a0961a1d83dc2702d357c5f3d06d1baecf" => :sierra
+    sha256 "9b1bec3d698b4a94ecad38a8a6321923465200724ef58fd7d05fbe3c1204d86b" => :catalina
+    sha256 "20d486647abe96f6ec32069ad52b4a191983073aceb836f9837e352af33f9de1" => :mojave
+    sha256 "876980c1a4073e32dbd58d6c63bf81ad57c5d30fdf9a00445e636e5279df73a2" => :high_sierra
   end
 
   depends_on "python@3.8"
