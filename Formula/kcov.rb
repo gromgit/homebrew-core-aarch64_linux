6760cf57f21ac046b1752459f4ed1cb705331400
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-10 22:01:02 +0000

    kcov: update 37_1 bottle.

--- Formula/kcov.rb
+++ Formula/kcov.rb
@@ -8,9 +8,9 @@ class Kcov < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e3ef70ca7ef17beaf787f9c347571ad52b5113e5c33ad0f242f5c179ea2eecec" => :catalina
-    sha256 "64374d7b480c44d68dbd4deb4f87751581d141f52c79f0380a6be4323d198f06" => :mojave
-    sha256 "e7cfa825d48a187b94a9b22d6fe5b6382feaa7fa505c9d283d3405a1cd9d6936" => :high_sierra
+    sha256 "f5a315633b03a413537445adaa8d7792d8e371c61e14898624846c68710d6fd6" => :catalina
+    sha256 "a229154a02e28f524e8c0e2d7af39dfa1a9f4b9f37717634f8179edb7bd327ff" => :mojave
+    sha256 "53f0444710122b2bb9e15fabad7688a0c4fe1a7e336a23777c2dc124f4c3c81c" => :high_sierra
   end
 
   depends_on "cmake" => :build
