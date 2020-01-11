d201319c0e365c9a7a53a0db234ae3b8fb085cd5
chenrui <chenrui333@gmail.com>
2020-01-11 18:13:52 -0500

    tomee-plume 8.0.0 (#48894)

--- Formula/tomee-plume.rb
+++ Formula/tomee-plume.rb
@@ -1,8 +1,8 @@
 class TomeePlume < Formula
   desc "Apache TomEE Plume"
   homepage "https://tomee.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=tomee/tomee-1.7.4/apache-tomee-1.7.4-plume.tar.gz"
-  sha256 "0f37dbe25c3a68a365f440cfaac01e63158e6ccce943f367203418038b5be402"
+  url "https://archive.apache.org/dist/tomee/tomee-8.0.0/apache-tomee-8.0.0-plume.tar.gz"
+  sha256 "477ef236f0668c3bf8ee22d65588261ac046f0b982d4147accd5bd250250373e"
 
   bottle :unneeded
 
