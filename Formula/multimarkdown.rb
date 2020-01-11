cf773800d21a3f2f4a4804352404ca77c11ad387
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-11 21:38:31 +0000

    multimarkdown: update 6.5.1 bottle.

--- Formula/multimarkdown.rb
+++ Formula/multimarkdown.rb
@@ -7,11 +7,9 @@ class Multimarkdown < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ebc7ddc3bd77a7bf46a94ab08ff99b5e40fe7022ab4a50abc67e3639827aa73b" => :catalina
-    sha256 "67b5b3efbd5508240db39a8e07de73d3afb54b6d7026cfc91bd52c8bab84e4e8" => :mojave
-    sha256 "4b0c84924e451362afe93a6046bfa31bb9ea567930f7d21ad2436241fc44d3d7" => :high_sierra
-    sha256 "3a314a87893e7eea9fc3542d5574d5fdadf96a77763ddef403335a34ee21d35c" => :sierra
-    sha256 "580595562320f4d5b78d0abb2e41f0773c91ef0c0bda6f7560486e3d05c52e13" => :el_capitan
+    sha256 "aa4b46bc273d2afeb082a8a3d81cfdcebbdae947bca417962d2cb5cf0debc42f" => :catalina
+    sha256 "aa4b46bc273d2afeb082a8a3d81cfdcebbdae947bca417962d2cb5cf0debc42f" => :mojave
+    sha256 "1e2a03d1dc7bc2b14f7b051480c20f9692e67bfe05b3313f518fc27f344f858b" => :high_sierra
   end
 
   depends_on "cmake" => :build
