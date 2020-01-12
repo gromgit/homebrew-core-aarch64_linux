230931de5d8e561672f692e7392a2d05c6d2e6e4
Rui Chen <chenrui333@gmail.com>
2020-01-12 12:18:53 -0500

    babel 7.8.0
    Closes #48925.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/babel.rb
+++ Formula/babel.rb
@@ -4,8 +4,8 @@ require "json"
 class Babel < Formula
   desc "Compiler for writing next generation JavaScript"
   homepage "https://babeljs.io/"
-  url "https://registry.npmjs.org/@babel/core/-/core-7.7.7.tgz"
-  sha256 "15f2266ab54addd1c18c88225cbb24d298720f46988bcb8a6ca2a46abdc89fa3"
+  url "https://registry.npmjs.org/@babel/core/-/core-7.8.0.tgz"
+  sha256 "b758856973ad9878d5f5f1c46d4965f5c9d6fff78e01fe332c34a6f1a1eb007e"
 
   bottle do
     sha256 "8cd0b43d3e79e7b59f65c554479ef228496da922ac8fa680421f6fe93f8bbc8d" => :catalina
