06a4d1624f20a8ba504b175d803aa383a0a3591e
willi123yao <willi123yao@gmail.com>
2020-01-13 17:28:26 +0800

    stress-ng 0.10.16
    Closes #48956.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/stress-ng.rb
+++ Formula/stress-ng.rb
@@ -1,8 +1,8 @@
 class StressNg < Formula
   desc "Stress test a computer system in various selectable ways"
   homepage "https://kernel.ubuntu.com/~cking/stress-ng/"
-  url "https://kernel.ubuntu.com/~cking/tarballs/stress-ng/stress-ng-0.10.15.tar.xz"
-  sha256 "1d0ca8a2f287e13c2d36c01e874d16d67bcb368d8d26563324c9aaf0ddb100c1"
+  url "https://kernel.ubuntu.com/~cking/tarballs/stress-ng/stress-ng-0.10.16.tar.xz"
+  sha256 "eabfffcffcf53e67765181280456c204ad00214339cfdb3ee2769d58b4a7e304"
 
   bottle do
     cellar :any_skip_relocation
