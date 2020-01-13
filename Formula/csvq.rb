119bd238a8a357ec14bf50aa5f3fdf2f4e2da7d5
Rui Chen <chenrui333@gmail.com>
2020-01-12 21:38:54 -0500

    csvq 1.12.1
    Closes #48946.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/csvq.rb
+++ Formula/csvq.rb
@@ -1,8 +1,8 @@
 class Csvq < Formula
   desc "SQL-like query language for csv"
   homepage "https://mithrandie.github.io/csvq"
-  url "https://github.com/mithrandie/csvq/archive/v1.11.8.tar.gz"
-  sha256 "f7f1313468cb01c61ba7bb3191fff3a5d1620760ea81313acd4c5ba9ba1ced41"
+  url "https://github.com/mithrandie/csvq/archive/v1.12.1.tar.gz"
+  sha256 "71b8280d1a69d9e53274e2b016509747b464175c51027a1b25d8701a9fdd49a5"
 
   bottle do
     cellar :any_skip_relocation
