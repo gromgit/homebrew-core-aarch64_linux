7b2f6f25af33b8b285efd16a5e2e6e40572c1698
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-13 00:28:44 +0000

    makedepend: update 1.0.6 bottle.

--- Formula/makedepend.rb
+++ Formula/makedepend.rb
@@ -6,14 +6,9 @@ class Makedepend < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "83c50101b3f5165bcd2b62e21f536c9f0375a4b35a554f79b6c1ce7b2d19b68d" => :catalina
-    sha256 "f8fe4a9c0346e86c2a8b675a1818f21024a28c8bd228c2eb2b47c90179705531" => :mojave
-    sha256 "648d18f27ef8ea0067a1d315ac9ffedece1734d950fa1228175ddd6f04c02ecf" => :high_sierra
-    sha256 "9e56751537ccf63d38f7d44c34cdcc565895a774d6f81d844c4900e008399712" => :sierra
-    sha256 "0f13329fdaa980ab3e4440f352a70e99aa3afdcfba0ad9bc60e9bc2e828f1b3b" => :el_capitan
-    sha256 "18186e2c1dbd9ea5b8107f4987318e9a75c87d2195e98238e216d8554c410138" => :yosemite
-    sha256 "afe9b0203383cd9a180c4f247fbf26c2a4bc75a7324963c95f6e9ebc39f1d806" => :mavericks
+    sha256 "afe76789b5f01ccfee8cc0d4ffa308015fb5d8791a1d7ce6b2dc1ee4bf2a020f" => :catalina
+    sha256 "a25fb9fd3ce11f6b98da2c53fad8f046174697087f5f34664999afb9df5f41de" => :mojave
+    sha256 "0f463e197923867ff9387b2ccd1461d4b410e89205bd3896ae98c5d52679c4c8" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
