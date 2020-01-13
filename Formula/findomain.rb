b0a3717db47a7e3b19a1401da0dc1e3c34ed3825
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-13 19:19:38 +0000

    findomain: add 0.9.6 bottle.

--- Formula/findomain.rb
+++ Formula/findomain.rb
@@ -4,6 +4,13 @@ class Findomain < Formula
   url "https://github.com/Edu4rdSHL/findomain/archive/0.9.6.tar.gz"
   sha256 "8d8ae150f4af58a5e65894646d267e30b887adc13ab691473d2f37da589c1849"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9485ab03c88d96be60a6745533e1cbeeb349563301580db253cc83b95ad975c8" => :catalina
+    sha256 "dc3008469ed23ee373aa697f53b31afb94a9e0f0c6635c99d95c6d8813900b13" => :mojave
+    sha256 "947f82536ccf9f22709ed31cf4d3e2fb3d601ecfb1fdd6b1c05700f98ac5db35" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
