2e7ebedf92c03cdf8fcf9dcf3c6f78c0c9543e53
Caleb Xu <calebcenter@live.com>
2020-01-14 09:09:03 -0500

    ack 3.3.1
    Closes #49002.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/ack.rb
+++ Formula/ack.rb
@@ -1,8 +1,8 @@
 class Ack < Formula
   desc "Search tool like grep, but optimized for programmers"
   homepage "https://beyondgrep.com/"
-  url "https://beyondgrep.com/ack-v3.3.0"
-  sha256 "8a1f92a33b1f4fe587e67205275ee22a311f92cf541d549da51bca4d49f41691"
+  url "https://beyondgrep.com/ack-v3.3.1"
+  sha256 "e4fd286d5a56f459147d562660d2f8bc4548f2f1e2d8cf3a5265b70beedbfc87"
 
   head do
     url "https://github.com/beyondgrep/ack3.git", :branch => "dev"
