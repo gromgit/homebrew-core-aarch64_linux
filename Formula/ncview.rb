e4c6e69558d92b35259a0bd3be41f70edf4b1d1a
EricFromCanada <enk3@outlook.com>
2020-01-14 15:25:28 -0500

    ncview: revision bump

--- Formula/ncview.rb
+++ Formula/ncview.rb
@@ -4,6 +4,7 @@ class Ncview < Formula
   url "ftp://cirrus.ucsd.edu/pub/ncview/ncview-2.1.8.tar.gz"
   mirror "https://dl.bintray.com/homebrew/mirror/ncview--2.1.8.tar.gz"
   sha256 "e8badc507b9b774801288d1c2d59eb79ab31b004df4858d0674ed0d87dfc91be"
+  revision 1
 
   bottle do
     sha256 "e4346135421cd4cb576d861c9681c76286843eb58c9c523c1260efb48258d3c9" => :catalina
