be33baf67c69ccb293fe15171be415c8f1f0ccfa
willi123yao <willi123yao@gmail.com>
2020-01-15 22:41:20 +0800

    gnu-sed 4.8
    Closes #49044.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/gnu-sed.rb
+++ Formula/gnu-sed.rb
@@ -1,9 +1,9 @@
 class GnuSed < Formula
   desc "GNU implementation of the famous stream editor"
   homepage "https://www.gnu.org/software/sed/"
-  url "https://ftp.gnu.org/gnu/sed/sed-4.7.tar.xz"
-  mirror "https://ftpmirror.gnu.org/sed/sed-4.7.tar.xz"
-  sha256 "2885768cd0a29ff8d58a6280a270ff161f6a3deb5690b2be6c49f46d4c67bd6a"
+  url "https://ftp.gnu.org/gnu/sed/sed-4.8.tar.xz"
+  mirror "https://ftpmirror.gnu.org/sed/sed-4.8.tar.xz"
+  sha256 "f79b0cfea71b37a8eeec8490db6c5f7ae7719c35587f21edb0617f370eeff633"
 
   bottle do
     cellar :any_skip_relocation
