fb9c00146d00df94d55fd82b9c46dca6e586199d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-16 15:35:19 +0000

    wasmer: add 0.12.0 bottle.

--- Formula/wasmer.rb
+++ Formula/wasmer.rb
@@ -5,6 +5,13 @@ class Wasmer < Formula
   sha256 "cb6c70026f29f09248cf80aa0406ef6a7c7100d2bef1809211efc621967a52c3"
   head "https://github.com/wasmerio/wasmer.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ab4ff5856bfc8fb630de85522d5d8269dbf4685e93a9a6d115693870a1d80d18" => :catalina
+    sha256 "d827f5b4452c63faab931f58979ecb7980d02e8a68951095d84214886b0d32d6" => :mojave
+    sha256 "164d4d9ac8499ed296945121d620f09420bcb2e7f56664d38fab60ab044039e6" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "rust" => :build
   depends_on "wabt" => :build
