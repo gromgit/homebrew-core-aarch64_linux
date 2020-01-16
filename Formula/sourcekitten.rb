705356c3e74980ef67758d1173cee59931d79901
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-16 22:47:24 +0000

    sourcekitten: update 0.29.0 bottle.

--- Formula/sourcekitten.rb
+++ Formula/sourcekitten.rb
@@ -8,8 +8,8 @@ class Sourcekitten < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f549273e9da69397ce4c6331025ad0f45fa6ae787b6f24fe5eaee287585f22d7" => :catalina
-    sha256 "9f9a1f7086aacef4513fde429ff6378eb6bc2db10afac74f6740647e853d6f0f" => :mojave
+    sha256 "d08bed584c9f176c09f460aa77ec0dcbed338b557b3aa35d9913a778b28ceac5" => :catalina
+    sha256 "2afc89c640799f55759d6b72b1e2ba694711ff434a7e883a052263b9e83d7692" => :mojave
   end
 
   depends_on :xcode => ["10.2", :build]
