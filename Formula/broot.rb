3f4bd355e12a92e2df8b4c2d8d4c08ec165c0a78
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-16 05:09:56 +0000

    broot: update 0.11.9 bottle.

--- Formula/broot.rb
+++ Formula/broot.rb
@@ -7,9 +7,9 @@ class Broot < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "31b3ce75ae211ce1fb3e5e7887e9f69a5e144eaa3622ac7265c05563204598b1" => :catalina
-    sha256 "26d160ab0f3e62132d126e0da677ffccbb840b88e4cdcdf2f4f0fa53f9ffa37f" => :mojave
-    sha256 "d7afe598c6942bf988482846720849e55ca31fd2e97e40e100c46929f9feae9c" => :high_sierra
+    sha256 "e6dccab5f81fd14ad040ebe6623a2bf82df86ec2812f24b102a79de364defeb3" => :catalina
+    sha256 "167fb4edc68e17aea721ff0af58c5584fc4094d63d55f20f51ff6e4e56f629f8" => :mojave
+    sha256 "db76e7614619a987874bf7ef3683bf06ddc04dfc463ef651bc63c7306545b8c9" => :high_sierra
   end
 
   depends_on "rust" => :build
