b95878dde38237a19264b700015a4a40c02cb068
Matan Kushner <hello@matchai.dev>
2020-01-16 05:01:58 +0000

    starship 0.33.1
    Closes #49075.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/starship.rb
+++ Formula/starship.rb
@@ -1,8 +1,8 @@
 class Starship < Formula
   desc "The cross-shell prompt for astronauts"
   homepage "https://starship.rs"
-  url "https://github.com/starship/starship/archive/v0.33.0.tar.gz"
-  sha256 "edcd9ab6fb26506cc2745a8e74916cc77964482125e61b166a6cc61c93126231"
+  url "https://github.com/starship/starship/archive/v0.33.1.tar.gz"
+  sha256 "90e1f2e795cd0e694d7a514faeee19f881c35ec46811298adde692d606837d5d"
   head "https://github.com/starship/starship.git"
 
   bottle do
