6d4126b7144d97a4476ed32c74e824f3701f4e24
Glenn Nagel <glenn@mercury-wireless.com>
2020-01-17 12:22:39 -0500

    flyway 6.1.4 (#49123)

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/6.0.8/flyway-commandline-6.0.8.tar.gz"
-  sha256 "636d5ab5a3b226cad38b2e0bb56c0063b74a6f029ff887158948cbf962e46a2d"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/6.1.4/flyway-commandline-6.1.4.tar.gz"
+  sha256 "899d506c1004566810a924389e379ecaa92460928bc223dbc1e604ffc4a387d8"
 
   bottle :unneeded
 
