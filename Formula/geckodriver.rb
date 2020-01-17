4ab4b543015bc11b395ce25bd442f3a3be42df0c
Christian Moritz <chrmoritz@gmail.com>
2020-01-17 16:41:36 +0100

    geckodriver: optimize download
    Closes #49124.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/geckodriver.rb
+++ Formula/geckodriver.rb
@@ -1,11 +1,30 @@
 class Geckodriver < Formula
   desc "WebDriver <-> Marionette proxy"
   homepage "https://github.com/mozilla/geckodriver"
-  # Get the commit id for stable releases from https://github.com/mozilla/geckodriver/releases
-  url "https://hg.mozilla.org/mozilla-central/archive/e9783a644016aa9b317887076618425586730d73.zip/testing/"
-  version "0.26.0"
-  sha256 "89f20b2d492b44b40ee049d0f0d4d91c52758b74dd7b5c7acad89a88ca1f177e"
   head "https://hg.mozilla.org/mozilla-central/", :using => :hg
+
+  stable do
+    # Get the hg_revision for stable releases from https://github.com/mozilla/geckodriver/releases
+    hg_revision = "e9783a644016aa9b317887076618425586730d73"
+    url "https://hg.mozilla.org/mozilla-central/archive/#{hg_revision}.zip/testing/geckodriver/"
+    version "0.26.0"
+    sha256 "c5854000621938de2aac0bdc853da62539e694adcba98b61851adcbb9ce54dd3"
+
+    resource "webdriver" do
+      url "https://hg.mozilla.org/mozilla-central/archive/#{hg_revision}.zip/testing/webdriver/"
+      sha256 "d84d6b84d4b37bb4fadda639026eca63dc61dd289bbeb3961eef1257be49266b"
+    end
+
+    resource "mozbase" do
+      url "https://hg.mozilla.org/mozilla-central/archive/#{hg_revision}.zip/testing/mozbase/rust/"
+      sha256 "a838ae82753aaed38eff52bd2076e47a418858be39c7dc5d833070c6ee2f7beb"
+    end
+
+    resource "Cargo.lock" do
+      url "https://hg.mozilla.org/mozilla-central/raw-file/#{hg_revision}/Cargo.lock"
+      sha256 "107aaf145d4840a389c2d4586660e95e3fa336a42bb9f94524f9a72c89c21d09"
+    end
+  end
 
   bottle do
     cellar :any_skip_relocation
@@ -17,6 +36,17 @@ class Geckodriver < Formula
   depends_on "rust" => :build
 
   def install
+    unless build.head?
+      # we need to do this, because all archives are containing a top level testing directory
+      %w[webdriver mozbase].each do |r|
+        (buildpath/"staging").install resource(r)
+        mv buildpath/"staging"/"testing"/r, buildpath/"testing"
+        rm_rf buildpath/"staging"/"testing"
+      end
+      rm_rf buildpath/"staging"
+      (buildpath/"testing"/"geckodriver").install resource("Cargo.lock")
+    end
+
     cd "testing/geckodriver" do
       system "cargo", "install", "--locked", "--root", prefix, "--path", "."
     end
