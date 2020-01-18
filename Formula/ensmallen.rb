684c58f3a2f4deefeec69e250950e69f98b18d29
Ryan Birmingham <birm@rbirm.us>
2020-01-17 21:47:22 -0500

    ensmallen 2.11.2
    Closes #49049.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/ensmallen.rb
+++ Formula/ensmallen.rb
@@ -1,8 +1,8 @@
 class Ensmallen < Formula
   desc "Flexible C++ library for efficient mathematical optimization"
   homepage "https://ensmallen.org"
-  url "https://ensmallen.org/files/ensmallen-2.11.1.tar.gz"
-  sha256 "c1343f4a61817a396866e497912cc3cd918600c4778869ce8a4f46a5474b980b"
+  url "https://github.com/mlpack/ensmallen/archive/2.11.2.tar.gz"
+  sha256 "314045d7d63997deb0ea36d0046506569aff58fa7dbd54ffaff5f9ba78ff5ff8"
 
   bottle do
     cellar :any_skip_relocation
