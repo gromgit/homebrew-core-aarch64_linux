29519791862322e532356b18e7611f1aa7473cef
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-18 10:39:10 +0000

    libsigc++: update 3.0.2 bottle.

--- Formula/libsigc++.rb
+++ Formula/libsigc++.rb
@@ -6,9 +6,9 @@ class Libsigcxx < Formula
 
   bottle do
     cellar :any
-    sha256 "1b22b26529168f83d74cef10cd9839b774e701e7929d174edcf3b3b4f50c3272" => :catalina
-    sha256 "34d97436f679f9ed9d76a1878d87b29eab692b487cb24aa2b18ba34e6856ab25" => :mojave
-    sha256 "ea4710c4dee791bf3109ed28b2cf1d17deb07811f334fc8ff462aafdcf222fc4" => :high_sierra
+    sha256 "793bb22cf3d7566a5ee346dd110fd2eba97b1ac91c16a9846ffb54af59cc43aa" => :catalina
+    sha256 "02ea0ed1876ad347cdc1a4b0223066f6fb5ced0dd955373834aa2da68404aad7" => :mojave
+    sha256 "ab541bdf6cb5d8c223213796054db86e8896c11fe3a68c79a2a179d85b019751" => :high_sierra
   end
 
   depends_on :macos => :high_sierra # needs C++17
