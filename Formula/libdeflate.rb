2bd61636f425155441a4ca616fbbe47f3bc0bc53
SeekingMeaning <meaningseeking@protonmail.com>
2020-01-19 08:12:37 -0800

    libdeflate 1.5 (new formula)
    Closes #49177.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

---
+++ Formula/libdeflate.rb
@@ -0,0 +1,17 @@
+class Libdeflate < Formula
+  desc "Heavily optimized DEFLATE/zlib/gzip compression and decompression"
+  homepage "https://github.com/ebiggers/libdeflate"
+  url "https://github.com/ebiggers/libdeflate/archive/v1.5.tar.gz"
+  sha256 "45b1b2332f443b705c59d06a49be009827291d2c487b076dc8ec2791eff4c711"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    (testpath/"foo").write "test"
+    system "#{bin}/libdeflate-gzip", "foo"
+    system "#{bin}/libdeflate-gunzip", "-d", "foo.gz"
+    assert_equal "test", shell_output("cat foo")
+  end
+end
