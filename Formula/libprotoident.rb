60b395883652b686653c6e5c8d08ab81e953e7e0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-19 15:45:07 +0000

    libprotoident: update 2.0.13 bottle.

--- Formula/libprotoident.rb
+++ Formula/libprotoident.rb
@@ -6,9 +6,10 @@ class Libprotoident < Formula
 
   bottle do
     cellar :any
-    sha256 "b7e8f79111dad699a0219ce88740ef153fc1152a8090499e07a8cb8b354805d5" => :mojave
-    sha256 "244ad5c441f10b9abe191108943b5d20601bfbb5ac3f245750230f3115472739" => :high_sierra
-    sha256 "8de3bb257a10fd3c492606b643c78e808a2d1248fcf8ebc00525f7d0b50bb66e" => :sierra
+    rebuild 1
+    sha256 "8db46b2b98a50a616d8f1820defead65ffaf5ba5c01b8480858395b64d45f5b6" => :catalina
+    sha256 "1143ffb528f58cf2ae55cf461fad927bf91ef14125dc2b9cbfbcefae908a96d9" => :mojave
+    sha256 "4305f7265ff0343178f0f6019889878c299a1e55a8aaac14c1382445aa0ac600" => :high_sierra
   end
 
   depends_on "libflowmanager"
