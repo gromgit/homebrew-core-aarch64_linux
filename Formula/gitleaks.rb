404d782543d7ae392dc4d15b252d21296bb105fc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-19 22:43:54 +0000

    gitleaks: update 3.2.0 bottle.

--- Formula/gitleaks.rb
+++ Formula/gitleaks.rb
@@ -6,9 +6,9 @@ class Gitleaks < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e09f54b47fc6ba5cf7acac50d8dcf0f02a32c1586a1f14ae1a0882dfd040c6c1" => :catalina
-    sha256 "71dddb77399f32ee00b0cc5dc37e777dbe84aa3506d655281dc148c49d6fdff2" => :mojave
-    sha256 "0ef4358a423555e207abf757db4c991a7e5a1aca1451dd07ee4040c364291d10" => :high_sierra
+    sha256 "1149bb0efb00de9960f036a8cb44df9a70d43f6171e3a1bdcec40054500e2dcb" => :catalina
+    sha256 "b325c01b93d5eb5270e732fb5c1812d3883f4f0e6acbd06537183348a78b8b70" => :mojave
+    sha256 "f063a806f1b52d099691324072df85ed664950327f21b2a156396c9fedc1df0b" => :high_sierra
   end
 
   depends_on "go" => :build
