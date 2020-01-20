00ec503d457f736226896063f382dd1dbb57aff5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-20 15:08:41 +0000

    apollo-cli: add 2.21.3 bottle.

--- Formula/apollo-cli.rb
+++ Formula/apollo-cli.rb
@@ -6,6 +6,13 @@ class ApolloCli < Formula
   url "https://registry.npmjs.org/apollo/-/apollo-2.21.3.tgz"
   sha256 "6b8e4115faaa4fcabd396e91a1317fb035e27244161ac17c398bd2a93f237bf2"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "29c81691e1a97d41af3ecc8f6da099949a812f07e2562b67e46bb1c8be44970e" => :catalina
+    sha256 "c456360ef7925a9727eafda05bf930f4c1b547b1418446ef53cbeae80c230ae7" => :mojave
+    sha256 "d1e694d958d51f02ba19ecc156d33625950031d48aff064ffc8b3b73cd591dc0" => :high_sierra
+  end
+
   depends_on "node"
 
   def install
