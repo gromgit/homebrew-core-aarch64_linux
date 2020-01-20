220c0b147503be7abaadb05543d179c8b19bbb68
Rui Chen <chenrui333@gmail.com>
2020-01-19 21:38:05 -0500

    apollo-cli 2.21.3 (new formula)
    Closes #49191.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

---
+++ Formula/apollo-cli.rb
@@ -0,0 +1,24 @@
+require "language/node"
+
+class ApolloCli < Formula
+  desc "Command-line tool for Apollo GraphQL"
+  homepage "https://apollographql.com"
+  url "https://registry.npmjs.org/apollo/-/apollo-2.21.3.tgz"
+  sha256 "6b8e4115faaa4fcabd396e91a1317fb035e27244161ac17c398bd2a93f237bf2"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match "apollo/#{version}", shell_output("#{bin}/apollo --version")
+
+    assert_match "Missing required flag:", shell_output("#{bin}/apollo codegen:generate 2>&1", 2)
+
+    error_output = shell_output("#{bin}/apollo codegen:generate --target typescript 2>&1", 2)
+    assert_match "Please add either a client or service config", error_output
+  end
+end
