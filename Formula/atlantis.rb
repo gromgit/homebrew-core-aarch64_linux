56eae8ba95b36d2869803aa6d30d0743d5d1bc19
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-20 19:48:07 +0000

    atlantis: update 0.11.0 bottle.

--- Formula/atlantis.rb
+++ Formula/atlantis.rb
@@ -5,9 +5,9 @@ class Atlantis < Formula
   sha256 "35e687cc00c21cf77d73837106830854405320f5f189366635190adb2753729e"
   bottle do
     cellar :any_skip_relocation
-    sha256 "ee357fdb4fd86ae1b82255fa8fa1d55d51bd7fd4a5bafbf36fef04442c0946be" => :catalina
-    sha256 "b4d357f53b25fd4fc503edf532a8f6ab30cc7455cf3953c02d9c96ee21dbdc8b" => :mojave
-    sha256 "8c44712f5de5dee007a5e64049c7079d795e7741cb0c8c7a225bd6cda6add5be" => :high_sierra
+    sha256 "151c59e26e143ce1adc944ae371f62cd2c6fb51f21a9c923c6d42ceb872f88fd" => :catalina
+    sha256 "890decc9e74bc3e8feaf27523bbc24e34b32aff32cc87d53e33a9c4ae8b7ffd8" => :mojave
+    sha256 "328348dfe1414b2768957ebbd92b3de6d7552456904f74b32c702d42e371d35b" => :high_sierra
   end
 
   depends_on "go" => :build
