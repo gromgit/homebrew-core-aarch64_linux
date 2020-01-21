a7b0d6128fb0a30d60ef7a9722f4018170094192
Clément Padovani <676023+clementpadovani@users.noreply.github.com>
2020-01-21 10:20:15 +0100

    gitlab-runner 12.7.0
    Closes #49248.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/gitlab-runner.rb
+++ Formula/gitlab-runner.rb
@@ -2,8 +2,8 @@ class GitlabRunner < Formula
   desc "The official GitLab CI runner written in Go"
   homepage "https://gitlab.com/gitlab-org/gitlab-runner"
   url "https://gitlab.com/gitlab-org/gitlab-runner.git",
-      :tag      => "v12.6.0",
-      :revision => "ac8e767ac77b0be4ad5dc9e6a5566a714512bba3"
+      :tag      => "v12.7.0",
+      :revision => "58272c27cb602196b3df15d866f077da34d8c822"
   head "https://gitlab.com/gitlab-org/gitlab-runner.git"
 
   bottle do
