cc0bceefd6a0b779c58ee3698097125344c3c539
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-21 16:26:15 +0000

    eureka: add 1.6.2 bottle.

--- Formula/eureka.rb
+++ Formula/eureka.rb
@@ -5,6 +5,13 @@ class Eureka < Formula
   sha256 "a8fb41cdf0c8c5a00e5c17fd2cdde71ce8fa1babb2b5d69d68cee7a0df5d1b4b"
   head "https://github.com/simeg/eureka.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ac22c59bd6c9359388c17ca6e4f342ef2c9b114539bc6a8547434fe95f2f430f" => :catalina
+    sha256 "c4e6550eaba8cbfe179fe58f2a0ed621bbc43bf53c28f56f417dc13020405bd5" => :mojave
+    sha256 "2cce6fe9de241d1d78fb1ef7cdc4d1d94e22973da6d0e59bca252d7f039bffd4" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
