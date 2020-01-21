7ba6270e732ab82f39efd5e6b7e0dc8fd65b663d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-21 07:38:34 +0000

    faiss: add 1.6.1 bottle.

--- Formula/faiss.rb
+++ Formula/faiss.rb
@@ -4,6 +4,13 @@ class Faiss < Formula
   url "https://github.com/facebookresearch/faiss/archive/v1.6.1.tar.gz"
   sha256 "827437c9a684fcb88ee21a8fd8f0ecd94f36e2db213f74357d0465c5a7e72ac6"
 
+  bottle do
+    cellar :any
+    sha256 "e8d0ed10a711ac67fd529950a015c1df0c522273de713a6d4cd10acf0821a569" => :catalina
+    sha256 "fedb0fa752508df5c68b99140d4f14be6e5f1f06d1ea6cc3e0b23a86dfc83e3a" => :mojave
+    sha256 "5c5112d0ec9c2c177ed076f4c519e87850e8b59d19decc1e91b9136f3739658c" => :high_sierra
+  end
+
   depends_on "libomp"
 
   def install
