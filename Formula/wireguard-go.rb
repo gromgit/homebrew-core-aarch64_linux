37564ff1f3891b369cb580d400c25f00d6979306
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-22 21:42:02 +0000

    wireguard-go: update 0.0.20200121 bottle.

--- Formula/wireguard-go.rb
+++ Formula/wireguard-go.rb
@@ -7,9 +7,9 @@ class WireguardGo < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "8a7b16f346fc85f4a6f5c3a251c115f8912fc97975e38bb06d89cbc6a86bf5ae" => :catalina
-    sha256 "a83b5669aa33d227ef2a20f2db58fc09ccdbadde417d7cbd4fab69e696a3b86f" => :mojave
-    sha256 "af7d625e4662a343bbbe16c87693331722e8435808e90dcbbe0a2e6df545d2f1" => :high_sierra
+    sha256 "ef82da1cb0b4ed12cdd26181f2ee7d59a585f9dd3905ed626eca31a828e27452" => :catalina
+    sha256 "ecf6348b31bb79b7816d2307959bbf0d25f9418dc2b948b1cd67326a9536bf8a" => :mojave
+    sha256 "e81afd2526eb4e55583ffa9c9a1a6de494fe9d955fcc190f81f8a34e33d89427" => :high_sierra
   end
 
   depends_on "go" => :build
