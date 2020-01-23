bc14c7e5caddb828d4e28bc87183c83e2db2e7ad
404NetworkError <404NetworkError@users.noreply.github.com>
2020-01-23 18:10:47 -0500

    youtube-dl 2020.01.24
    Closes #49332.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -1,8 +1,8 @@
 class YoutubeDl < Formula
   desc "Download YouTube videos from the command-line"
   homepage "https://ytdl-org.github.io/youtube-dl/"
-  url "https://github.com/ytdl-org/youtube-dl/releases/download/2020.01.15/youtube-dl-2020.01.15.tar.gz"
-  sha256 "556711f8dc827910096fd648d7ead051ddadb81b90ae4abdf0393ddf2d62d237"
+  url "https://github.com/ytdl-org/youtube-dl/releases/download/2020.01.24/youtube-dl-2020.01.24.tar.gz"
+  sha256 "0b1dcc96d204984df1ee364e81cddd5645421f6065d2815472258ccdad5c36ff"
 
   head do
     url "https://github.com/ytdl-org/youtube-dl.git"
