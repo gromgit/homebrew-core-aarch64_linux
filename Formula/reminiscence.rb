4f169c6375b1bc8c23ddf374a78d33a0caba8ec9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-23 02:29:27 +0000

    reminiscence: update 0.4.6 bottle.

--- Formula/reminiscence.rb
+++ Formula/reminiscence.rb
@@ -6,9 +6,9 @@ class Reminiscence < Formula
 
   bottle do
     cellar :any
-    sha256 "fb9ac602c0bf9afe43287302a18e9a47d3cc27f2ef894fbfce60a90594e750ad" => :catalina
-    sha256 "165e1694ef3880e68eecb99e1288fc7aa3d31d54cd15240757aa60292c479bda" => :mojave
-    sha256 "b991cb2fbd838085444fe0267b352b9cce450892aa0982e3a5166ce2bfcc0cff" => :high_sierra
+    sha256 "08914eb6ff7af4b482d75d78f69b0d9b8f30df76bef4cd04e88ddc4e457d6b38" => :catalina
+    sha256 "a56001c98f255e684babd5ddbcb28e36ed8699c559730f28fc3939edcdb25a5e" => :mojave
+    sha256 "f6f27f326bb5020cced4c0e1c42f6f4638bfe05092440b51fd8257a1ce9e5288" => :high_sierra
   end
 
   depends_on "autoconf" => :build
