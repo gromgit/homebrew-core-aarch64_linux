2bfb924f6fd765b006d45e6f0b36c35ad53b8b40
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-23 00:05:48 +0000

    pulumi: update 1.9.0 bottle.

--- Formula/pulumi.rb
+++ Formula/pulumi.rb
@@ -7,9 +7,9 @@ class Pulumi < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f54c9c65a8b6a770c66a2b4a2e4b0891516ffb9c60302ee6660226ce9a72fba1" => :catalina
-    sha256 "76c07ab883b423b9420e1dd9c57733fce0add95a2bd1d4e19fb672a889ce2be6" => :mojave
-    sha256 "9d1c41b7ec6d63b8ebf1b9e9dd82c035773bf7bb90b5f67358367cdd5e6c9850" => :high_sierra
+    sha256 "7843d0cb7d08ca62ea08323896eb1549a780c37b96db1dffed6be24023cc2128" => :catalina
+    sha256 "a41a90d58f26f0994fb90186e307bbd0fb9b1087614d5fec97fcdc9a4075cde7" => :mojave
+    sha256 "6a8ee4d7c9de2c9d15f4de071e92e66ba2a7e9c0ca02e2a5c7d9e00600074bbb" => :high_sierra
   end
 
   depends_on "go" => :build
