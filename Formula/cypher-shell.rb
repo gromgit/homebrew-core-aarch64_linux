19c34783295d953138acfb464f7d919bb7762af3
Rui Chen <chenrui333@gmail.com>
2020-01-23 21:25:22 -0500

    cypher-shell 4.0.1
    Closes #49335.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/cypher-shell.rb
+++ Formula/cypher-shell.rb
@@ -1,9 +1,9 @@
 class CypherShell < Formula
   desc "Command-line shell where you can execute Cypher against Neo4j"
   homepage "https://github.com/neo4j/cypher-shell"
-  url "https://github.com/neo4j/cypher-shell/releases/download/4.0.0/cypher-shell.zip"
-  version "4.0.0"
-  sha256 "4c982a9fe4336ca119fa2f1b48f65f47e78ffdc63bf600540e409a66b604d6bf"
+  url "https://github.com/neo4j/cypher-shell/releases/download/4.0.1/cypher-shell.zip"
+  version "4.0.1"
+  sha256 "da73c03d7bc8ad1077a6d1eb7fd3828834f2ab62e0d0e7fd3c17ee95e59200a7"
   version_scheme 1
 
   bottle :unneeded
