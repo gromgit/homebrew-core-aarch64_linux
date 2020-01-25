0c98714dbda07c45241a21577e6a385d0fb50aec
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-25 23:53:44 +0000

    tty-share: add 0.6.2 bottle.

--- Formula/tty-share.rb
+++ Formula/tty-share.rb
@@ -4,6 +4,13 @@ class TtyShare < Formula
   url "https://github.com/elisescu/tty-share/archive/v0.6.2.tar.gz"
   sha256 "4ea8cdc96fa889da414885e8cdbd8fca71569a9146ca4d3caa7cf2411ac141aa"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "adfa3de302df681f3e236d28f69dcb7cd63143582d26b698a034adc5337fe18a" => :catalina
+    sha256 "b9b97a2733c85508451799419dfbc36326c8d9bfe30c26800bc22ddf2bd7b30f" => :mojave
+    sha256 "6820c38434e99f26e0cc247ae89831376c914a6eb1ee0d4b20056a2ca2e7c0e3" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
