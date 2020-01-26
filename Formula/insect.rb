f40af68ffe88a1278590529131a3b12377339f6d
Rui Chen <chenrui333@gmail.com>
2020-01-25 19:23:52 -0500

    insect 5.4.0
    replace bower with psc-package
    
    Closes #49396.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/insect.rb
+++ Formula/insect.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Insect < Formula
   desc "High precision scientific calculator with support for physical units"
   homepage "https://insect.sh/"
-  url "https://registry.npmjs.org/insect/-/insect-5.3.0.tgz"
-  sha256 "770f1c282366bab62fd5cce03416781423d768ee9c88456a11c6159e1e5d776b"
+  url "https://registry.npmjs.org/insect/-/insect-5.4.0.tgz"
+  sha256 "c810e50c473439b04b2b3b7e580e6a2781f1c8747eb4261c262e413d6a145c2c"
 
   bottle do
     cellar :any_skip_relocation
@@ -13,7 +13,7 @@ class Insect < Formula
     sha256 "46444cc696e42c428815638f1ca6b22dfb6c9f92ee53978dfc0b9d4bd3aa6d6a" => :high_sierra
   end
 
-  depends_on "bower" => :build
+  depends_on "psc-package" => :build
   depends_on "pulp" => :build
   depends_on "purescript" => :build
   depends_on "node"
