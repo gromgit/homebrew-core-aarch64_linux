a1df05cc793bc42b818406648111f7dc75822943
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-26 21:10:27 +0000

    alpine: update 2.22 bottle.

--- Formula/alpine.rb
+++ Formula/alpine.rb
@@ -6,9 +6,9 @@ class Alpine < Formula
   head "https://repo.or.cz/alpine.git"
 
   bottle do
-    sha256 "c3df47485dcedfed585bc0dbbb8fcbc2e6eed1494d48cf49a2ee224eba7e659e" => :catalina
-    sha256 "fa4a5f8078a26f5390325855dd98eae3ba60ca8c29cdb19ce6828e51862eef00" => :mojave
-    sha256 "96c62ab0c3a3f297f015f67add86563a365d77d477a4244588ad2e92d6f95e63" => :high_sierra
+    sha256 "d9f2c103746223a50f9bd17cc113e33b058a801a9c69f0cde9613f0120b6dc68" => :catalina
+    sha256 "3061917e8f114d875869a3beb88da6c091a774dd657841052b97d054f10298f9" => :mojave
+    sha256 "8d4a1d7a99103583a2b8e01639e2204b2d6c1c7e028c8d78ce39f82465ab3799" => :high_sierra
   end
 
   depends_on "openssl@1.1"
