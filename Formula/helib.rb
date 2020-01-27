e240db108d7909a74d8dcd370695a26c38b6a950
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-27 00:26:57 +0000

    helib: add 1.0.0 bottle.

--- Formula/helib.rb
+++ Formula/helib.rb
@@ -4,6 +4,13 @@ class Helib < Formula
   url "https://github.com/homenc/HElib/archive/v1.0.0.tar.gz"
   sha256 "5b917a6ba1555be580db4c102a339abe124c284007f0044d637892ec85877214"
 
+  bottle do
+    cellar :any
+    sha256 "ad8926ac577b4adb2ce1cce23769e3a3d31027ed7264c99aa42099e238525fe4" => :catalina
+    sha256 "3ffb80af12a8fd92b03292a375a3ca30c24ae61281b1dd81e5022fe363e39b8f" => :mojave
+    sha256 "c0fed1980b3e977ab565b6bea93d885c3a697881d20ecd812640f3d0a869b6d2" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "ntl"
 
