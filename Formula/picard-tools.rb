5c98fa49c1d72072164ca0f8d39ef2e2fa54db47
Igor Kapkov <igasgeek@me.com>
2020-01-27 10:26:51 +0100

    picard-tools 2.21.7 (#49432)

--- Formula/picard-tools.rb
+++ Formula/picard-tools.rb
@@ -1,8 +1,8 @@
 class PicardTools < Formula
   desc "Tools for manipulating HTS data and formats"
   homepage "https://broadinstitute.github.io/picard/"
-  url "https://github.com/broadinstitute/picard/releases/download/2.21.6/picard.jar"
-  sha256 "ba2c996ee4aa9dfa005d51cbad677a876b9b4d12651e2b268f7bada133fcfeee"
+  url "https://github.com/broadinstitute/picard/releases/download/2.21.7/picard.jar"
+  sha256 "af3833072d3fed76943f6c5ec0ebaa4d2fc8bcc2dbe4bcb01ed7d313fda587c4"
 
   bottle :unneeded
 
