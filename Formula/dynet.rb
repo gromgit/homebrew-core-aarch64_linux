cd87c0bed41f9e72604d14445aeee4e6864e2201
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-27 00:12:32 +0000

    dynet: add 2.1 bottle.

--- Formula/dynet.rb
+++ Formula/dynet.rb
@@ -4,6 +4,13 @@ class Dynet < Formula
   url "https://github.com/clab/dynet/archive/2.1.tar.gz"
   sha256 "72d5c2a0c7dece6b91426f02993f640297f6c5f7de1907d4eb587796e97022bc"
 
+  bottle do
+    cellar :any
+    sha256 "2e0c6026d6a047f6c1885779da5e72f2503568b711fc1f7c0c00322814cb1213" => :catalina
+    sha256 "06482cc7dee2e337482c31e935e9384cbec243a6c407e9e35aeb02f08361e450" => :mojave
+    sha256 "f6603f28e5bf6209808e58ffbe27b7c8841a3bc00be1bc98d9aff6b4f8bf4f37" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "eigen"
 
