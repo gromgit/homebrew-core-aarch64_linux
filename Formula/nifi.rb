4140530b185d0c4c91ce72075da3b75d1cd964d8
Aldrin Piri <aldrin@apache.org>
2020-01-27 20:53:31 -0500

    nifi 1.11.0 (#49452)

--- Formula/nifi.rb
+++ Formula/nifi.rb
@@ -1,8 +1,8 @@
 class Nifi < Formula
   desc "Easy to use, powerful, and reliable system to process and distribute data"
   homepage "https://nifi.apache.org"
-  url "https://www.apache.org/dyn/closer.cgi?path=/nifi/1.10.0/nifi-1.10.0-bin.tar.gz"
-  sha256 "fd4f0750d18137bb1c21cd0fd5ab8951ccd450e6f673b8988db93ea2ff408288"
+  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.11.0/nifi-1.11.0-bin.tar.gz"
+  sha256 "e93c6618347033db85cce2f2542d0b68561987c892fd48e24d6caefb446e823b"
 
   bottle :unneeded
 
