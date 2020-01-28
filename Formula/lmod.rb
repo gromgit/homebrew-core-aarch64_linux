e811ce66bc29592bf01ae1fd4f8413e7e175a036
Igor Kapkov <igasgeek@me.com>
2020-01-28 09:14:32 +0100

    lmod 8.3
    Closes #49471.
    
    Signed-off-by: Igor Kapkov <igasgeek@me.com>

--- Formula/lmod.rb
+++ Formula/lmod.rb
@@ -1,8 +1,8 @@
 class Lmod < Formula
   desc "Lua-based environment modules system to modify PATH variable"
   homepage "https://www.tacc.utexas.edu/research-development/tacc-projects/lmod"
-  url "https://github.com/TACC/Lmod/archive/8.2.10.tar.gz"
-  sha256 "15676d82235faf5c755a747f0e318badb1a5c3ff1552fa8022c67ff083ee9e2f"
+  url "https://github.com/TACC/Lmod/archive/8.3.tar.gz"
+  sha256 "c2c2e9e6b387b011ee617cb009a2199caac8bf200330cb8a065ceedee09e664a"
 
   bottle do
     cellar :any_skip_relocation
