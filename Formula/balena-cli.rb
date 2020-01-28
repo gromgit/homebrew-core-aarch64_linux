4fa481e428b3ec9bf31c585c109e2fcbf3b72874
Rui Chen <chenrui333@gmail.com>
2020-01-27 19:24:22 -0500

    balena-cli 11.23.0
    Closes #49461.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/balena-cli.rb
+++ Formula/balena-cli.rb
@@ -4,8 +4,8 @@ class BalenaCli < Formula
   desc "The official balena CLI tool"
   homepage "https://www.balena.io/docs/reference/cli/"
   # Frequent upstream releases, do not update more than once a week
-  url "https://registry.npmjs.org/balena-cli/-/balena-cli-11.21.7.tgz"
-  sha256 "1409982066cbbea0a823a58db3e031a194f51bfc64652badbfb57703a27762a2"
+  url "https://registry.npmjs.org/balena-cli/-/balena-cli-11.23.0.tgz"
+  sha256 "04b0ffb211a7c9182fd9e33c0d16eb6f8621dcb089659a8289da1933b65d00fe"
 
   bottle do
     sha256 "f3749a1b41aac08c83c4e7977ff68ec2252ea855098ba269b77661b0fa0e1ff8" => :catalina
@@ -47,37 +47,3 @@ index 2101ae4..9d4d743 100644
        "requires": {
          "async": "^2.6.1",
          "bindings": "^1.3.0",
-@@ -8290,9 +8290,9 @@
-       }
-     },
-     "lzma-native": {
--      "version": "4.0.5",
--      "resolved": "https://registry.npmjs.org/lzma-native/-/lzma-native-4.0.5.tgz",
--      "integrity": "sha512-pmLMsHQlXQAikqGqapzUOtACPW/gEtt9xhkcrkJnsjWn+I1g7OIbrV2SugL8jinkBCD+QxqAze51VtRsECDcxQ==",
-+      "version": "4.0.6",
-+      "resolved": "https://registry.npmjs.org/lzma-native/-/lzma-native-4.0.6.tgz",
-+      "integrity": "sha512-1kiSs/KAcAuh9vyyd00ATXZFfrg6W8UCBqH1RKlWg/tBP5aQez6HYOY+SihmsZfpy0RVDioW5SLI76dZ3Mq5Rw==",
-       "requires": {
-         "nan": "^2.14.0",
-         "node-pre-gyp": "^0.11.0",
-@@ -12797,7 +12797,7 @@
-         "@types/node": "^6.0.112",
-         "@types/usb": "^1.5.1",
-         "debug": "^3.1.0",
--        "usb": "github:balena-io/node-usb#v1.3.12"
-+        "usb": "1.3.13"
-       },
-       "dependencies": {
-         "@types/node": {
-@@ -17613,8 +17613,9 @@
-       "integrity": "sha1-FQWgOiiaSMvXpDTvuu7FBV9WM6k="
-     },
-     "usb": {
--      "version": "github:balena-io/node-usb#1fb1bb01d04281432083b96801c6be6d528bd213",
--      "from": "github:balena-io/node-usb#v1.3.12",
-+      "version": "1.3.13",
-+      "resolved": "https://registry.npmjs.org/@balena.io/usb/-/usb-1.3.13.tgz",
-+      "integrity": "sha512-qG2nGJduDtmgElK6KhcJ/u9M67FlgTSAF8IkcC6CZOTitSLRMh1OqeXtI5B3q7KyFH5773qZYj2g1jlXhpL1dQ==",
-       "requires": {
-         "nan": "2.13.2",
-         "node-pre-gyp": "^0.11.0"
