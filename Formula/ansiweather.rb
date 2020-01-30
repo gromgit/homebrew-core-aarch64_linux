e8b3adc9dc718cb45e70a40179467ae8997790d0
Caleb Xu <calebcenter@live.com>
2020-01-30 18:25:25 -0500

    ansiweather 1.16.0
    Closes #49595.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/ansiweather.rb
+++ Formula/ansiweather.rb
@@ -1,8 +1,8 @@
 class Ansiweather < Formula
   desc "Weather in your terminal, with ANSI colors and Unicode symbols"
   homepage "https://github.com/fcambus/ansiweather"
-  url "https://github.com/fcambus/ansiweather/archive/1.15.0.tar.gz"
-  sha256 "6cbfe4b12551a4e0eff2027dd558b73b8bbc315834d87255bc3d53d302f97c59"
+  url "https://github.com/fcambus/ansiweather/archive/1.16.0.tar.gz"
+  sha256 "9ed6c8651413a300cbcfb0d3d353c48fd7a472df896366ff25dd5dfa06fea7f4"
   head "https://github.com/fcambus/ansiweather.git"
 
   bottle :unneeded
