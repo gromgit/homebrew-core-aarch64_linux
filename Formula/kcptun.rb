a17e065311fb600a495010faca8c845e119d4481
Yifan Gao <git@yfgao.com>
2020-01-30 13:15:38 +0800

    kcptun 20200103 (new formula)
    kcptun v20200103 (new formula)
    
    Closes #49566.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

---
+++ Formula/kcptun.rb
@@ -0,0 +1,67 @@
+class Kcptun < Formula
+  desc "Stable & Secure Tunnel based on KCP with N:M multiplexing and FEC"
+  homepage "https://github.com/xtaci/kcptun"
+  url "https://github.com/xtaci/kcptun/archive/v20200103.tar.gz"
+  sha256 "620d4cd3ed1660e71b0fe1041c063e8fb38dc3aa7a6c78f6570e812566f44bb5"
+  head "https://github.com/xtaci/kcptun.git"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-ldflags", "-X main.VERSION=#{version} -s -w",
+      "-o", bin/"kcptun_client", "github.com/xtaci/kcptun/client"
+    system "go", "build", "-ldflags", "-X main.VERSION=#{version} -s -w",
+      "-o", bin/"kcptun_server", "github.com/xtaci/kcptun/server"
+
+    etc.install "examples/local.json" => "kcptun_client.json"
+  end
+
+  plist_options :manual => "#{HOMEBREW_PREFIX}/opt/kcptun/bin/kcptun_client -c #{HOMEBREW_PREFIX}/etc/kcptun_client.json"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/kcptun_client</string>
+          <string>-c</string>
+          <string>#{etc}/kcptun_client.json</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <dict>
+          <key>Crashed</key>
+          <true/>
+          <key>SuccessfulExit</key>
+          <false/>
+        </dict>
+        <key>ProcessType</key>
+        <string>Background</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/kcptun.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/kcptun.log</string>
+      </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    server = fork { exec bin/"kcptun_server", "-t", "1.1.1.1:80" }
+    client = fork { exec bin/"kcptun_client", "-r", "127.0.0.1:29900", "-l", ":12948" }
+    sleep 1
+    begin
+      assert_match "cloudflare", shell_output("curl -vI http://127.0.0.1:12948/")
+    ensure
+      Process.kill 9, server
+      Process.wait server
+      Process.kill 9, client
+      Process.wait client
+    end
+  end
+end
