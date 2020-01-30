570168e4c78234b2a33dec14e61449d86868b7f8
Caleb Xu <calebcenter@live.com>
2020-01-30 18:42:55 -0500

    bingrep 0.8.2
    Closes #49598.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/bingrep.rb
+++ Formula/bingrep.rb
@@ -1,8 +1,8 @@
 class Bingrep < Formula
   desc "Greps through binaries from various OSs and architectures"
   homepage "https://github.com/m4b/bingrep"
-  url "https://github.com/m4b/bingrep/archive/v0.8.1.tar.gz"
-  sha256 "a3d93a3e30f306e5273b95e212007cff5918423d2386233a8625b7f3cf18a0e0"
+  url "https://github.com/m4b/bingrep/archive/v0.8.2.tar.gz"
+  sha256 "5647d78166a2d768b98ae03bd40427f2263b28b81213882d42f638c5b96619e2"
 
   bottle do
     cellar :any_skip_relocation
