89e1775db25a8bb11ffd6f877b52065f036a354a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-30 18:20:58 +0000

    libraqm: add 0.7.0 bottle.

--- Formula/libraqm.rb
+++ Formula/libraqm.rb
@@ -4,6 +4,13 @@ class Libraqm < Formula
   url "https://github.com/HOST-Oman/libraqm/archive//v0.7.0.tar.gz"
   sha256 "21ed67b8d0d2217f3801878f2ceef9b2da24495eeff830552051cef21f95938e"
 
+  bottle do
+    cellar :any
+    sha256 "61857a8c4e5ffada95438c4b98d042c129ebf485b5141fc8cc1bbbb13052d2fc" => :catalina
+    sha256 "b46d537983abc24546765d3d37f7a77330f82371a2c8899ada29e96324fee760" => :mojave
+    sha256 "5599586ba85c892e36cfb5da6eb0bae54d8632b0c569bdcaa71b38b3ecab947f" => :high_sierra
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "gtk-doc" => :build
