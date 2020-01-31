f157e41880e1d35967e2e7c1aa264d6578fd7864
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-31 13:38:12 +0000

    dnscrypt-proxy: update 2.0.39 bottle.

--- Formula/dnscrypt-proxy.rb
+++ Formula/dnscrypt-proxy.rb
@@ -7,9 +7,9 @@ class DnscryptProxy < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "106505b5b78fdee5927aa0eeb17cdc11b6089234628a555fe72f6992c0ae7477" => :catalina
-    sha256 "08920dac7807db05263c0c5e1ed2ffe87cdfbe2f063691be992f2756bbb91836" => :mojave
-    sha256 "43df28407a6561330839b4f909af366432d006a20e9d2b63aca3be7cb29445c7" => :high_sierra
+    sha256 "e86b19432fe18e7024cf7f27a0bbfb35463850e1670e03b05cf24a02be13b482" => :catalina
+    sha256 "0ef2d3a190e0792e37b1dfe29576f849deb2527a3cd801f52a8939b0f562cadc" => :mojave
+    sha256 "5a4c8b384bde18f818f268301592933f15e68bc6e37de2d509be30a748823d3a" => :high_sierra
   end
 
   depends_on "go" => :build
