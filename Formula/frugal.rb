5abd1a00d28c1640f29db12c89df350d39550373
Rui Chen <chenrui333@gmail.com>
2020-01-31 00:18:23 -0500

    frugal 3.7.1
    Closes #49626.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/frugal.rb
+++ Formula/frugal.rb
@@ -1,8 +1,8 @@
 class Frugal < Formula
   desc "Cross language code generator for creating scalable microservices"
   homepage "https://github.com/Workiva/frugal"
-  url "https://github.com/Workiva/frugal/archive/3.7.0.tar.gz"
-  sha256 "ad8e3b58b7b6444b64c07023fe2538162d8eb9b9d2b0f47acfaa252e80fabc3f"
+  url "https://github.com/Workiva/frugal/archive/v3.7.1.tar.gz"
+  sha256 "b3677707ce08caf337ac9965df2f6af3cefd5d94df28f666687c9cc90779524a"
 
   bottle do
     cellar :any_skip_relocation
