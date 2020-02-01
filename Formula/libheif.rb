7f26bc6f192af31126ecb7bc5e69f406c059f541
Igor Kapkov <igasgeek@me.com>
2020-02-01 13:49:34 +0100

    libheif 1.6.2
    Closes #49665.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libheif.rb
+++ Formula/libheif.rb
@@ -1,8 +1,8 @@
 class Libheif < Formula
   desc "ISO/IEC 23008-12:2017 HEIF file format decoder and encoder"
   homepage "https://www.libde265.org/"
-  url "https://github.com/strukturag/libheif/releases/download/v1.6.1/libheif-1.6.1.tar.gz"
-  sha256 "a22611289464da08fa7e580c95ea5e1b1b522b96ee6807de9b3b4605efb621d1"
+  url "https://github.com/strukturag/libheif/releases/download/v1.6.2/libheif-1.6.2.tar.gz"
+  sha256 "bb229e855621deb374f61bee95c4642f60c2a2496bded35df3d3c42cc6d8eefc"
 
   bottle do
     cellar :any
