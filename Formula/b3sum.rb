3c4f294cc7b31c351e303449afba5ff33490f1d8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-01 22:01:14 +0000

    b3sum: update 0.1.3 bottle.

--- Formula/b3sum.rb
+++ Formula/b3sum.rb
@@ -6,9 +6,9 @@ class B3sum < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e76b0fe70a4c1ab17b1f1774131b64c3462a94421e8511eac6a4842d0ee2fec7" => :catalina
-    sha256 "5c483f3a96121031d5f0396aced6a07f663d606fc3ed1b8834edcd6776ffc674" => :mojave
-    sha256 "13549dfba0c2fa956ebc8d263a6de881b8b1ff01d23680013e93d50396037fec" => :high_sierra
+    sha256 "479c9b7a5c7d0af3ed8a405abe275584e44586a87c9d74ba4bdad110afaf2ab3" => :catalina
+    sha256 "42938edd2110cd65cab1451492734fbcae87e30d6cf41884cfe0cace42d66e94" => :mojave
+    sha256 "0dbce5bea8f31c28aae9a4842366bfebce1755f0334c41e51ddf005ba77378d1" => :high_sierra
   end
 
   depends_on "rust" => :build
