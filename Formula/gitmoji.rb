d2983c3daed792f4797bd1936a43671aa662e679
Rui Chen <chenrui333@gmail.com>
2020-02-01 10:08:43 -0500

    gitmoji 3.2.0
    Closes #49676.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/gitmoji.rb
+++ Formula/gitmoji.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Gitmoji < Formula
   desc "Emoji guide for your commit messages"
   homepage "https://gitmoji.carloscuesta.me"
-  url "https://registry.npmjs.org/gitmoji-cli/-/gitmoji-cli-3.1.0.tgz"
-  sha256 "9dc50ec376d52e561faa11a1824585ab5511ec40653573a446ca0d2b47ae7333"
+  url "https://registry.npmjs.org/gitmoji-cli/-/gitmoji-cli-3.2.0.tgz"
+  sha256 "30b996ab58210815a014303cd76ec3276d90c71fa5f872401d7f69ba7f4267b1"
 
   bottle do
     cellar :any_skip_relocation
