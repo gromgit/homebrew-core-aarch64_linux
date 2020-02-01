970dd3061c342caee53f01a4186ad28fd432657c
Santiago Bendavid <sant3001@users.noreply.github.com>
2020-02-01 08:48:34 -0500

    tika 1.23 (#49530)

--- Formula/tika.rb
+++ Formula/tika.rb
@@ -1,16 +1,16 @@
 class Tika < Formula
   desc "Content analysis toolkit"
   homepage "https://tika.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=tika/tika-app-1.22.jar"
-  sha256 "d7219709abc547136fa5fca17632a85fe1cd36dc08cb4031957e3c9a836543e2"
+  url "https://www.apache.org/dyn/closer.cgi?path=tika/tika-app-1.23.jar"
+  sha256 "0c382d300442c3c2b84042e2c5b5cf2287583d4487c9b5c78eec58a625b54ae3"
 
   bottle :unneeded
 
   depends_on :java => "1.7+"
 
   resource "server" do
-    url "https://www.apache.org/dyn/closer.cgi?path=tika/tika-server-1.22.jar"
-    sha256 "9913c69e8481edd848f27a7fd912300acd6f0b29044f43fe705a959119e2f44b"
+    url "https://www.apache.org/dyn/closer.cgi?path=tika/tika-server-1.23.jar"
+    sha256 "9e95c7dada0f8ca81bf1b69192258a82890b645d389659330356fa3427005b6f"
   end
 
   def install
