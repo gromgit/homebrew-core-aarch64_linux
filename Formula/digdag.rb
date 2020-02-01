27276f22b81f86a808497fbe94b8da70b9559133
chenrui <chenrui333@gmail.com>
2020-02-01 08:49:23 -0500

    digdag 0.9.41 (#49573)

--- Formula/digdag.rb
+++ Formula/digdag.rb
@@ -1,21 +1,18 @@
 class Digdag < Formula
   desc "Workload Automation System"
   homepage "https://www.digdag.io/"
-  url "https://dl.digdag.io/digdag-0.9.39.jar"
-  sha256 "dbe3ab5e2d512e39111feaeb8e2859b980a33d7bb3b61456d9f19f4b155ad228"
+  url "https://dl.digdag.io/digdag-0.9.41.jar"
+  sha256 "1dc76896573a0cbb1e36f505bf61e16d7bb4b62a2798406a69deecccde780a40"
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on :java => "1.8"
 
   def install
-    libexec.install "digdag-#{version}.jar" => "digdag.jar"
-
-    # Create a wrapper script to support OS X 10.9.
-    (bin/"digdag").write <<~EOS
-      #!/bin/bash
-      exec /bin/bash "#{libexec}/digdag.jar" "$@"
-    EOS
+    libexec.install "digdag-#{version}.jar"
+    (libexec/"bin").write_jar_script libexec/"digdag-#{version}.jar", "digdag"
+    (libexec/"bin/digdag").chmod 0755
+    (bin/"digdag").write_env_script libexec/"bin/digdag", Language::Java.java_home_env("1.8")
   end
 
   test do
