dd8ed3def4c73aecd03b33ba82666a9d07176d93
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-01 21:48:50 +0000

    binutils: update 2.34 bottle.

--- Formula/binutils.rb
+++ Formula/binutils.rb
@@ -6,9 +6,9 @@ class Binutils < Formula
   sha256 "f00b0e8803dc9bab1e2165bd568528135be734df3fabf8d0161828cd56028952"
 
   bottle do
-    sha256 "c9043b4615a1462646f0af1296fdc4ec70fc654fb7daff77f9c4e73373d1b312" => :catalina
-    sha256 "c97046dc6f519c176addcd4ed37afddc0553e7eebf8b30fbd5a5b64487b9cdc4" => :mojave
-    sha256 "021367441684b194be93d5be015930e1507a6a2d7c7201d3815740a052f0b87a" => :high_sierra
+    sha256 "1e21593a927df65e405f9d3bdc8f86fe83b1236c5c945641a2de775c99327953" => :catalina
+    sha256 "142e380448ac77bc0f7974ff9b9ddae6a90c4ef5f182cac0c2b029baa8460173" => :mojave
+    sha256 "87bda0c909a5bd2043d35b073f2268cac7aed074a89d903973e4909d68dfdf46" => :high_sierra
   end
 
   keg_only :provided_by_macos,
