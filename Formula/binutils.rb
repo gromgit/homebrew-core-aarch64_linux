a8cf405a86b8b2cefe730b6c0fd7e36a2ae65ab0
Alexander Bayandin <a.bayandin@gmail.com>
2020-02-01 21:35:23 +0000

    binutils 2.34
    Closes #49681.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/binutils.rb
+++ Formula/binutils.rb
@@ -1,9 +1,9 @@
 class Binutils < Formula
   desc "GNU binary tools for native development"
   homepage "https://www.gnu.org/software/binutils/binutils.html"
-  url "https://ftp.gnu.org/gnu/binutils/binutils-2.33.1.tar.gz"
-  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.33.1.tar.gz"
-  sha256 "98aba5f673280451a09df3a8d8eddb3aa0c505ac183f1e2f9d00c67aa04c6f7d"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.xz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.34.tar.xz"
+  sha256 "f00b0e8803dc9bab1e2165bd568528135be734df3fabf8d0161828cd56028952"
 
   bottle do
     sha256 "c9043b4615a1462646f0af1296fdc4ec70fc654fb7daff77f9c4e73373d1b312" => :catalina
@@ -11,10 +11,10 @@ class Binutils < Formula
     sha256 "021367441684b194be93d5be015930e1507a6a2d7c7201d3815740a052f0b87a" => :high_sierra
   end
 
-  uses_from_macos "zlib"
-
   keg_only :provided_by_macos,
            "because Apple provides the same tools and binutils is poorly supported on macOS"
+
+  uses_from_macos "zlib"
 
   def install
     system "./configure", "--disable-debug",
