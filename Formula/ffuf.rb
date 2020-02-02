b51b2691955295b918fb54093ae3d602c55e0885
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-02 14:33:53 +0000

    ffuf: update 1.0.1 bottle.

--- Formula/ffuf.rb
+++ Formula/ffuf.rb
@@ -6,9 +6,9 @@ class Ffuf < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a56a5568bdc1d29358ab134b6b15dc446f61f37c73c23c28146361e69dcb008b" => :catalina
-    sha256 "067979a4307641df78dd99eeb8620efbaf0f12e0921f778956f789a26ebbcc4b" => :mojave
-    sha256 "65d2865a483e1d0d0bda9c127927544b9930b32de76b7f23b22bda0e9a71904f" => :high_sierra
+    sha256 "7da6328282eb13a189d20ae14a23333c919f4e4817881f8163955899c11d6c99" => :catalina
+    sha256 "4f0521eff0be5b5df334e94729186706c557e6d9b6daf62cb8c0d11407a0d9b7" => :mojave
+    sha256 "22c580ce9d27759319e00389451da9d5e72b100d5cd90762a5aa45dd8b878edd" => :high_sierra
   end
 
   depends_on "go" => :build
