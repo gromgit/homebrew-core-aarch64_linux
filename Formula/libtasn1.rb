ee0ac3ceec53f5c7225acba9c23c733850e92f6d
Sam Ford <1584702+samford@users.noreply.github.com>
2020-02-02 00:12:49 -0500

    libtasn1 4.16.0
    Closes #49699.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/libtasn1.rb
+++ Formula/libtasn1.rb
@@ -1,9 +1,9 @@
 class Libtasn1 < Formula
   desc "ASN.1 structure parser library"
   homepage "https://www.gnu.org/software/libtasn1/"
-  url "https://ftp.gnu.org/gnu/libtasn1/libtasn1-4.15.0.tar.gz"
-  mirror "https://ftpmirror.gnu.org/libtasn1/libtasn1-4.15.0.tar.gz"
-  sha256 "dd77509fe8f5304deafbca654dc7f0ea57f5841f41ba530cff9a5bf71382739e"
+  url "https://ftp.gnu.org/gnu/libtasn1/libtasn1-4.16.0.tar.gz"
+  mirror "https://ftpmirror.gnu.org/libtasn1/libtasn1-4.16.0.tar.gz"
+  sha256 "0e0fb0903839117cb6e3b56e68222771bebf22ad7fc2295a0ed7d576e8d4329d"
 
   bottle do
     cellar :any
