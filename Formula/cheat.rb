29eb7f37358eaf82f0b8457fd8dedbdaf79e1893
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-02 23:43:10 +0000

    cheat: update 3.5.1 bottle.

--- Formula/cheat.rb
+++ Formula/cheat.rb
@@ -7,9 +7,9 @@ class Cheat < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "1f9e243008e7ee17a67c0639b4381fffbe15e73986885ba21e570c59c0a99b34" => :catalina
-    sha256 "c7457982dc8bc2b5b47c5369696490a9b3537909e0653688b830d9453a0bb5dc" => :mojave
-    sha256 "3eb8a81211128ed5959515edd9662b603a61b621e4f622f01f53b51c6ebcc3a5" => :high_sierra
+    sha256 "5b5659889c08fe22d962c955804c210a508cc5ba4cefa69d81076c81d55c63b9" => :catalina
+    sha256 "1165dcff4c53fdb3adc322101373d2ada3f4eaeced8a133e982377055ed8c57b" => :mojave
+    sha256 "8160f43431907f2d7aed7a9a527b5f4c5760c67eeeb788deb2196396ab297a55" => :high_sierra
   end
 
   depends_on "go" => :build
