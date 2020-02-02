448a8c643cb5c1719b4771b1b27e21857eb439c8
Igor Kapkov <igasgeek@me.com>
2020-02-02 11:50:32 +0100

    libjson-rpc-cpp: revision bump for jsoncpp

--- Formula/libjson-rpc-cpp.rb
+++ Formula/libjson-rpc-cpp.rb
@@ -3,6 +3,7 @@ class LibjsonRpcCpp < Formula
   homepage "https://github.com/cinemast/libjson-rpc-cpp"
   url "https://github.com/cinemast/libjson-rpc-cpp/archive/v1.3.0.tar.gz"
   sha256 "cf132ad9697b034f22ff37d12a1f1c6f2647ec2236701de5e76f6036ab664156"
+  revision 1
   head "https://github.com/cinemast/libjson-rpc-cpp.git"
 
   bottle do
