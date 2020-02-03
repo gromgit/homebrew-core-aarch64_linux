b48fd3f4e3df47e1917cac5040377e97eeed4d2d
Markus Reiter <me@reitermark.us>
2020-02-03 18:12:12 +0100

    vert.x: depend on openjdk

--- Formula/vert.x.rb
+++ Formula/vert.x.rb
@@ -3,14 +3,15 @@ class VertX < Formula
   homepage "https://vertx.io/"
   url "https://bintray.com/vertx/downloads/download_file?file_path=vert.x-3.8.5-full.tar.gz"
   sha256 "ce21b4846f1ad485cdba76cc65690be9ec254c5db12cd57bab2cc8ee73ab9806"
+  revision 1
 
   bottle :unneeded
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %w[bin conf lib]
-    bin.install_symlink "#{libexec}/bin/vertx"
+    (bin/"vertx").write_env_script "#{libexec}/bin/vertx", :JAVA_HOME => "${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
   end
 
   test do
