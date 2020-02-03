7b80adf65fe673c64e594839ce177ba422e6c2d6
Markus Reiter <me@reitermark.us>
2020-02-03 18:12:01 +0100

    vnu: depend on openjdk

--- Formula/vnu.rb
+++ Formula/vnu.rb
@@ -3,15 +3,19 @@ class Vnu < Formula
   homepage "https://validator.github.io/validator/"
   url "https://github.com/validator/validator/releases/download/18.11.5/vnu.jar_18.11.5.zip"
   sha256 "b9785a3ab51671509c8d418cc84298b1389ca2d82107aa0537544090a7c51862"
+  revision 1
   version_scheme 1
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
     libexec.install "vnu.jar"
-    bin.write_jar_script libexec/"vnu.jar", "vnu"
+    (bin/"vnu").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["openjdk"].opt_bin}/java" -jar "#{libexec}/vnu.jar" "$@"
+    EOS
   end
 
   test do
