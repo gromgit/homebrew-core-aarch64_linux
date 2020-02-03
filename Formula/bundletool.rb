9f1bbc28db03a9b33faebf28f03bae94d8202fc3
Markus Reiter <me@reitermark.us>
2020-02-03 17:37:03 +0100

    bundletool: depend on openjdk

--- Formula/bundletool.rb
+++ Formula/bundletool.rb
@@ -3,9 +3,10 @@ class Bundletool < Formula
   homepage "https://github.com/google/bundletool"
   url "https://github.com/google/bundletool/releases/download/0.12.0/bundletool-all-0.12.0.jar"
   sha256 "c143ef768dc7cd3c6ab30dca2edca71f0abad53feaf1feceed74f43043c4c8d1"
+  revision 1
 
   bottle :unneeded
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   resource "bundle" do
     url "https://gist.githubusercontent.com/raw/ca85ede7ac072a44f48c658be55ff0d3/sample.aab"
@@ -14,7 +15,10 @@ class Bundletool < Formula
 
   def install
     libexec.install "bundletool-all-#{version}.jar"
-    bin.write_jar_script libexec/"bundletool-all-#{version}.jar", "bundletool"
+    (bin/"bundletool").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["openjdk"].opt_bin}/java" -jar "#{libexec}/bundletool-all-#{version}.jar" "$@"
+    EOS
   end
 
   test do
