1018ae2476a1450cc213aaee673a9b5e7ea9e68b
Markus Reiter <me@reitermark.us>
2020-02-03 17:22:53 +0100

    asciidoctorj: depend on openjdk

--- Formula/asciidoctorj.rb
+++ Formula/asciidoctorj.rb
@@ -3,18 +3,16 @@ class Asciidoctorj < Formula
   homepage "https://github.com/asciidoctor/asciidoctorj"
   url "https://dl.bintray.com/asciidoctor/maven/org/asciidoctor/asciidoctorj/2.2.0/asciidoctorj-2.2.0-bin.zip"
   sha256 "d5240240a4e39f4f9c56abab2da3e960a4784b7d8e7befa323fc9f64890f0029"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.6+"
+  depends_on "openjdk"
 
   def install
     rm_rf Dir["bin/*.bat"] # Remove Windows files.
     libexec.install Dir["*"]
-
-    executable = libexec/"bin/asciidoctorj"
-    executable.chmod 0555
-    bin.write_exec_script executable
+    (bin/"asciidoctorj").write_env_script libexec/"bin/asciidoctorj", :JAVA_HOME => Formula["openjdk"].opt_prefix
   end
 
   test do
