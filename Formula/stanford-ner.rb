25d97b15224b9c3636c382c82e5216323c66ad43
Markus Reiter <me@reitermark.us>
2020-02-03 18:12:54 +0100

    stanford-ner: depend on openjdk

--- Formula/stanford-ner.rb
+++ Formula/stanford-ner.rb
@@ -4,14 +4,16 @@ class StanfordNer < Formula
   url "https://nlp.stanford.edu/software/stanford-ner-2018-10-16.zip"
   version "3.9.2"
   sha256 "fa737bc6d7ac01de6d13a4628b229b7eabae24c446bc616608b7aad04ed6c65a"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
     libexec.install Dir["*"]
-    bin.write_exec_script Dir["#{libexec}/*.sh"]
+    bin.install Dir["#{libexec}/*.sh"]
+    bin.env_script_all_files libexec, :JAVA_HOME => Formula["openjdk"].opt_prefix
   end
 
   test do
