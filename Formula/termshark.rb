e7795874767aabf7e880c4969e8e9353c22a7801
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-03 05:24:06 +0000

    termshark: update 2.1.1 bottle.

--- Formula/termshark.rb
+++ Formula/termshark.rb
@@ -6,9 +6,9 @@ class Termshark < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7b8b0fe96ff5a914f1de2545f9381465c1a8a6bff877e4c016ed54c6975c9825" => :catalina
-    sha256 "71a09e2a4e0af93d8e0dc4b57596dd66410db6c4846a53ab558920b22df0b451" => :mojave
-    sha256 "1feaeff8adfee9e3722bcb6c5d0da6dd26be45758423910338cafb504542f683" => :high_sierra
+    sha256 "04877a693f8f87f4120847862fa35642b5d862c7b93603b11d330015316efe03" => :catalina
+    sha256 "ec8e1526dd3ba7cb58bbcc510eee873c10fd11f428d12b54950d1dcfa5603a1a" => :mojave
+    sha256 "ef6bfc089550e3ba2afb02510ff7d5d3a8364079970a4f223a8b84801f1c0b93" => :high_sierra
   end
 
   depends_on "go" => :build
