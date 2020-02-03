9eaccb0403dd859a8f5a6ecba130b349a8562232
Markus Reiter <me@reitermark.us>
2020-02-03 18:12:20 +0100

    tomcat: depend on openjdk
    Closes #50450.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/tomcat.rb
+++ Formula/tomcat.rb
@@ -4,10 +4,11 @@ class Tomcat < Formula
   url "https://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-9/v9.0.31/bin/apache-tomcat-9.0.31.tar.gz"
   mirror "https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.31/bin/apache-tomcat-9.0.31.tar.gz"
   sha256 "1b111d890f8c0b3f3eb7f8f8f557a8e163ef6b06bca3e262ba1062808467cb43"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
     # Remove Windows scripts
@@ -16,7 +17,7 @@ class Tomcat < Formula
     # Install files
     prefix.install %w[NOTICE LICENSE RELEASE-NOTES RUNNING.txt]
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/catalina.sh" => "catalina"
+    (bin/"catalina").write_env_script "#{libexec}/bin/catalina.sh", :JAVA_HOME => Formula["openjdk"].opt_prefix
   end
 
   plist_options :manual => "catalina run"
