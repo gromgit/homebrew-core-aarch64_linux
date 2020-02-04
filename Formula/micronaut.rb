5b83d71cbd847b02a2c9511aa8d5a00b56ded08f
Markus Reiter <me@reitermark.us>
2020-02-04 11:26:01 +0100

    micronaut: depend on openjdk

--- Formula/micronaut.rb
+++ Formula/micronaut.rb
@@ -3,15 +3,16 @@ class Micronaut < Formula
   homepage "https://micronaut.io/"
   url "https://github.com/micronaut-projects/micronaut-core/releases/download/v1.3.1/micronaut-1.3.1.zip"
   sha256 "af992cc2ca74f7893c6dde8f43abbc5ea7962ea23f3b1bb1ba77cb5e908fedbf"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %W[bin media cli-#{version}.jar]
-    bin.install_symlink libexec/"bin/mn"
+    (bin/"mn").write_env_script libexec/"bin/mn", :JAVA_HOME => "${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
   end
 
   test do
