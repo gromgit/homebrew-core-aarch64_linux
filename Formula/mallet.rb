c824fcf79dfa3ee71aeb2c803e489360a89a22d0
Markus Reiter <me@reitermark.us>
2020-02-04 19:02:20 +0100

    mallet: depend on openjdk

--- Formula/mallet.rb
+++ Formula/mallet.rb
@@ -3,10 +3,11 @@ class Mallet < Formula
   homepage "http://mallet.cs.umass.edu/"
   url "http://mallet.cs.umass.edu/dist/mallet-2.0.8.tar.gz"
   sha256 "5b2d6fb9bcf600b1836b09881821a6781dd45a7d3032e61d7500d027a5b34faf"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java
+  depends_on "openjdk"
 
   resource "testdata" do
     url "https://raw.githubusercontent.com/mimno/Mallet/master/sample-data/stackexchange/tsv/testing.tsv"
@@ -17,7 +18,7 @@ class Mallet < Formula
     rm Dir["bin/*.{bat,dll,exe}"] # Remove all windows files
     libexec.install Dir["*"]
     bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env)
+    bin.env_script_all_files(libexec/"bin", :JAVA_HOME => Formula["openjdk"].opt_prefix)
   end
 
   test do
