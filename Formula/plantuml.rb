f51759139edadc7e52d33b924c1b26ea058148f1
Markus Reiter <me@reitermark.us>
2020-02-04 18:55:49 +0100

    plantuml: depend on openjdk

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -3,12 +3,13 @@ class Plantuml < Formula
   homepage "https://plantuml.com/"
   url "https://downloads.sourceforge.net/project/plantuml/1.2020.1/plantuml.1.2020.1.jar"
   sha256 "ea25f97c72404f9c8bb9fec57d4fc6a8d711de65605ee3ba75c11483942d4509"
+  revision 1
   version_scheme 1
 
   bottle :unneeded
 
   depends_on "graphviz"
-  depends_on :java
+  depends_on "openjdk"
 
   def install
     jar = "plantuml.jar"
@@ -18,9 +19,9 @@ class Plantuml < Formula
       if [[ "$*" != *"-gui"* ]]; then
         VMARGS="-Djava.awt.headless=true"
       fi
-      GRAPHVIZ_DOT="#{Formula["graphviz"].opt_bin}/dot" exec java $VMARGS -jar #{libexec}/#{jar} "$@"
+      GRAPHVIZ_DOT="#{Formula["graphviz"].opt_bin}/dot" exec "#{Formula["openjdk"].opt_bin}/java" $VMARGS -jar #{libexec}/#{jar} "$@"
     EOS
-    chmod 0555, bin/"plantuml"
+    chmod 0755, bin/"plantuml"
   end
 
   test do
