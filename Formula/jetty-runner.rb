68e94d1dc0815a5464f79e28cf7861b1893e1984
Markus Reiter <me@reitermark.us>
2020-02-04 11:55:27 +0100

    jetty-runner: depend on openjdk

--- Formula/jetty-runner.rb
+++ Formula/jetty-runner.rb
@@ -4,16 +4,20 @@ class JettyRunner < Formula
   url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-runner/9.4.15.v20190215/jetty-runner-9.4.15.v20190215.jar"
   version "9.4.15.v20190215"
   sha256 "4412775d61999b173106d7608ac13b7c8b4066ef85a8ea1279021483166f6596"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
     libexec.install Dir["*"]
 
-    bin.mkpath
-    bin.write_jar_script libexec/"jetty-runner-#{version}.jar", "jetty-runner"
+    (bin/"jetty-runner").write <<~EOS
+      #!/bin/bash
+      export JAVA_HOME="${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
+      exec "${JAVA_HOME}/bin/java" -jar "#{libexec}/jetty-runner-#{version}.jar" "$@"
+    EOS
   end
 
   test do
