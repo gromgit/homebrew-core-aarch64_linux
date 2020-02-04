5e8117950af3c5968e605cdddecfe607b40ac881
Markus Reiter <me@reitermark.us>
2020-02-04 19:04:53 +0100

    solr: depend on openjdk
    Closes #50494.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/solr.rb
+++ Formula/solr.rb
@@ -4,22 +4,24 @@ class Solr < Formula
   url "https://www.apache.org/dyn/closer.cgi?path=lucene/solr/8.4.1/solr-8.4.1.tgz"
   mirror "https://archive.apache.org/dist/lucene/solr/8.4.1/solr-8.4.1.tgz"
   sha256 "ec39e1e024b2e37405149de41e39e875a39bf11a53f506d07d96b47b8d2a4301"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java
+  depends_on "openjdk"
 
   skip_clean "example/logs"
 
   def install
+    # Fix the classpath for the post tool
+    inreplace "bin/post", '"$SOLR_TIP/dist"', "#{libexec}/dist"
+
     bin.install %w[bin/solr bin/post bin/oom_solr.sh]
+    bin.env_script_all_files libexec/"bin", :JAVA_HOME => Formula["openjdk"].opt_prefix
     pkgshare.install "bin/solr.in.sh"
     (var/"lib/solr").install "server/solr/README.txt", "server/solr/solr.xml", "server/solr/zoo.cfg"
     prefix.install %w[contrib dist example server]
     libexec.install Dir["*"]
-
-    # Fix the classpath for the post tool
-    inreplace "#{bin}/post", '"$SOLR_TIP/dist"', "#{libexec}/dist"
 
     # Fix the paths in the sample solrconfig.xml files
     Dir.glob(["#{prefix}/example/**/solrconfig.xml",
