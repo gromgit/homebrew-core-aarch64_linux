19664da1cb8b969a1207f4bba663a88cdfc938da
Markus Reiter <me@reitermark.us>
2020-02-04 11:25:55 +0100

    picard-tools: depend on openjdk

--- Formula/picard-tools.rb
+++ Formula/picard-tools.rb
@@ -3,14 +3,18 @@ class PicardTools < Formula
   homepage "https://broadinstitute.github.io/picard/"
   url "https://github.com/broadinstitute/picard/releases/download/2.21.8/picard.jar"
   sha256 "2ed7568191c95668355cbcfac77e7811b84a5e1a9f4f5e7f4da36ddcbd63aa73"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
     libexec.install "picard.jar"
-    bin.write_jar_script libexec/"picard.jar", "picard"
+    (bin/"picard").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["openjdk"].opt_bin}/java" $JAVA_OPTS -jar "#{libexec}/picard.jar" "$@"
+    EOS
   end
 
   test do
