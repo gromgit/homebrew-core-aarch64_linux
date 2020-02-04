d510557bd4572a9f87cfc7c9c0caf632a3ff1549
Markus Reiter <me@reitermark.us>
2020-02-04 11:56:01 +0100

    detekt: depend on openjdk

--- Formula/detekt.rb
+++ Formula/detekt.rb
@@ -3,14 +3,18 @@ class Detekt < Formula
   homepage "https://github.com/arturbosch/detekt"
   url "https://jcenter.bintray.com/io/gitlab/arturbosch/detekt/detekt-cli/1.3.0/detekt-cli-1.3.0-all.jar"
   sha256 "610b0211b909d8e79c1b623119e1d94cb833a8e98b32e5a0e991fa2d214992f6"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
     libexec.install "detekt-cli-#{version}-all.jar"
-    bin.write_jar_script libexec/"detekt-cli-#{version}-all.jar", "detekt"
+    (bin/"detekt").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["openjdk"].opt_bin}/java" -jar "#{libexec}/detekt-cli-#{version}-all.jar" "$@"
+    EOS
   end
 
   test do
