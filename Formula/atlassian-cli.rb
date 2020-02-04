2accc1b238aed01b2c5a08525ed7ddac9782aa72
Markus Reiter <me@reitermark.us>
2020-02-04 16:19:07 +0100

    atlassian-cli: depend on openjdk

--- Formula/atlassian-cli.rb
+++ Formula/atlassian-cli.rb
@@ -3,23 +3,22 @@ class AtlassianCli < Formula
   homepage "https://bobswift.atlassian.net/wiki/pages/viewpage.action?pageId=1966101"
   url "https://bobswift.atlassian.net/wiki/download/attachments/16285777/atlassian-cli-9.1.1-distribution.zip"
   sha256 "9f972422b3ad425054c76a1ac89434a38fe70fa398414c5136981c7c54bcd8b8"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
-    Dir.glob("*.sh") do |f|
-      cmd = File.basename(f, ".sh")
-      bin.install cmd + ".sh" => cmd
+    inreplace "acli.sh" do |s|
+      s.sub! "`find $directory -name acli-*.jar`", "'#{share}/lib/acli-#{version}.jar'"
+      s.sub! "java", "'#{Formula["openjdk"].opt_bin}/java'"
     end
+    bin.install "acli.sh" => "acli"
     share.install "lib", "license"
   end
 
   test do
-    Dir.glob(bin/"*") do |f|
-      cmd = File.basename(f, ".sh")
-      assert_match "Usage:", shell_output(bin/"#{cmd} --help 2>&1 | head") unless cmd == "atlassian"
-    end
+    assert_match "Welcome to the Atlassian CLI", shell_output("#{bin}/acli --help 2>&1 | head")
   end
 end
