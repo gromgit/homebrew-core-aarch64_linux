618f150c17ffd7c3f908fff9c243c8f00e1b3028
Markus Reiter <me@reitermark.us>
2020-02-04 19:01:14 +0100

    flyway: depend on openjdk

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -3,15 +3,16 @@ class Flyway < Formula
   homepage "https://flywaydb.org/"
   url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/6.2.3/flyway-commandline-6.2.3.tar.gz"
   sha256 "86c743b79820ef5e165ebc81cc87a7fc0a4fe85bc27db00a105790d7ac078596"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java
+  depends_on "openjdk"
 
   def install
     rm Dir["*.cmd"]
     libexec.install Dir["*"]
-    bin.install_symlink Dir["#{libexec}/flyway"]
+    (bin/"flyway").write_env_script libexec/"flyway", :JAVA_HOME => Formula["openjdk"].opt_prefix
   end
 
   test do
