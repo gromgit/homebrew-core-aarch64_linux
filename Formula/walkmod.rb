9afd4c025879c9b189252f564ea1787fbac7513f
Markus Reiter <me@reitermark.us>
2020-02-04 17:22:39 +0100

    walkmod: depend on openjdk

--- Formula/walkmod.rb
+++ Formula/walkmod.rb
@@ -3,16 +3,17 @@ class Walkmod < Formula
   homepage "https://walkmod.com/"
   url "https://bitbucket.org/rpau/walkmod/downloads/walkmod-3.0.0-installer.zip"
   sha256 "7d83564b8b11ce02b5a0924e552a8f006524003a03749e5fe901c937cff3d544"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.7+"
+  depends_on "openjdk"
 
   def install
     # Remove windows files
     rm_f Dir["bin/*.bat"]
     libexec.install Dir["*"]
-    bin.install_symlink libexec+"bin/walkmod"
+    (bin/"walkmod").write_env_script libexec/"bin/walkmod", :JAVA_HOME => Formula["openjdk"].opt_prefix
   end
 
   test do
