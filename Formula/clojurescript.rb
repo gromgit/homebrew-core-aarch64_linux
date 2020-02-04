412964e78954be64bbc63d810838c89bd0fc8cf4
Markus Reiter <me@reitermark.us>
2020-02-04 15:26:08 +0100

    clojurescript: depend on openjdk

--- Formula/clojurescript.rb
+++ Formula/clojurescript.rb
@@ -3,15 +3,20 @@ class Clojurescript < Formula
   homepage "https://github.com/clojure/clojurescript"
   url "https://github.com/clojure/clojurescript/releases/download/r1.10.597/cljs.jar"
   sha256 "f861a9b36b67287ac667a57abe930721bff9f83c19bb07982a05ef09a94208a7"
+  revision 1
   head "https://github.com/clojure/clojurescript.git"
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
     libexec.install "cljs.jar"
-    bin.write_jar_script libexec/"cljs.jar", "cljsc"
+    (bin/"cljsc").write <<~EOS
+      #!/bin/bash
+      export JAVA_HOME="${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
+      exec "${JAVA_HOME}/bin/java" -jar "#{libexec}/cljs.jar" "$@"
+    EOS
   end
 
   def caveats; <<~EOS
