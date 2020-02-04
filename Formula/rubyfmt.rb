357d0a7a50368068efd73301cd57b5308e875508
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-04 14:26:08 +0000

    rubyfmt: add 0.2.0 bottle.

--- Formula/rubyfmt.rb
+++ Formula/rubyfmt.rb
@@ -4,6 +4,13 @@ class Rubyfmt < Formula
   url "https://github.com/penelopezone/rubyfmt/archive/v0.2.0.tar.gz"
   sha256 "68ebc0fd30933b1e27b609cc34c69a3bc886747d11c5b949e460ce01814adaeb"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8d9ed80d496220e02b9df146c41870079116cf798ab90734212d3cdc6080bb8b" => :catalina
+    sha256 "8d9ed80d496220e02b9df146c41870079116cf798ab90734212d3cdc6080bb8b" => :mojave
+    sha256 "8d9ed80d496220e02b9df146c41870079116cf798ab90734212d3cdc6080bb8b" => :high_sierra
+  end
+
   def install
     system "make"
     bin.install "build/rubyfmt.rb" => "rubyfmt"
