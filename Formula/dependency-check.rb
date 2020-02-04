15b455d939bc589b4915bb9338ced727100e765e
Markus Reiter <me@reitermark.us>
2020-02-04 19:50:27 +0100

    dependency-check: depend on openjdk

--- Formula/dependency-check.rb
+++ Formula/dependency-check.rb
@@ -3,10 +3,11 @@ class DependencyCheck < Formula
   homepage "https://www.owasp.org/index.php/OWASP_Dependency_Check"
   url "https://dl.bintray.com/jeremy-long/owasp/dependency-check-5.3.0-release.zip"
   sha256 "1af524b02cb6bb8a03ea57f6ae2880bf8db67479b92a0815b70b03051bf20743"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java
+  depends_on "openjdk"
 
   def install
     rm_f Dir["bin/*.bat"]
@@ -14,8 +15,7 @@ class DependencyCheck < Formula
     chmod 0755, "bin/dependency-check.sh"
     libexec.install Dir["*"]
 
-    mv libexec/"bin/dependency-check.sh", libexec/"bin/dependency-check"
-    bin.install_symlink libexec/"bin/dependency-check"
+    (bin/"dependency-check").write_env_script libexec/"bin/dependency-check.sh", :JAVA_HOME => Formula["openjdk"].opt_prefix
 
     (var/"dependencycheck").mkpath
     libexec.install_symlink var/"dependencycheck" => "data"
@@ -28,7 +28,7 @@ class DependencyCheck < Formula
   end
 
   test do
-    output = shell_output("#{libexec}/bin/dependency-check --version").strip
+    output = shell_output("#{bin}/dependency-check --version").strip
     assert_match "Dependency-Check Core version #{version}", output
 
     (testpath/"temp-props.properties").write <<~EOS
