de7d5eb79871f1d6a90cfdf203cd030ea8440c54
Markus Reiter <me@reitermark.us>
2020-02-04 19:14:13 +0100

    sjk: depend on openjdk

--- Formula/sjk.rb
+++ Formula/sjk.rb
@@ -3,14 +3,19 @@ class Sjk < Formula
   homepage "https://github.com/aragozin/jvm-tools"
   url "https://search.maven.org/remotecontent?filepath=org/gridkit/jvmtool/sjk-plus/0.10.1/sjk-plus-0.10.1.jar"
   sha256 "dafd3b18de282265fe0f806872b23211a1318fc63aaef284fbd6abee5a288368"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java
+  depends_on "openjdk"
 
   def install
     libexec.install "sjk-plus-#{version}.jar"
-    bin.write_jar_script "#{libexec}/sjk-plus-#{version}.jar", "sjk"
+    (bin/"sjk").write <<~EOS
+      #!/bin/bash
+      export JAVA_HOME="${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
+      exec "${JAVA_HOME}/bin/java" -jar "#{libexec}/sjk-plus-#{version}.jar" "$@"
+    EOS
   end
 
   test do
