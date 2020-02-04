5a9b013e6e239357cc1d6830c931629b7ba05a93
Markus Reiter <me@reitermark.us>
2020-02-04 19:02:07 +0100

    igv: depend on openjdk

--- Formula/igv.rb
+++ Formula/igv.rb
@@ -1,26 +1,24 @@
 class Igv < Formula
   desc "Interactive Genomics Viewer"
   homepage "https://www.broadinstitute.org/software/igv"
-  url "https://data.broadinstitute.org/igv/projects/downloads/2.5/IGV_2.5.3.zip"
-  sha256 "d33f6e20aaf5158770c6154953b8b3df89ced0f9416b399502864d3ccd44f22a"
-  revision 1
+  url "https://data.broadinstitute.org/igv/projects/downloads/2.7/IGV_2.7.2.zip"
+  sha256 "cbaeb3d437374ad9d9057eeddaa963c573ddd86e1c2e01c8cad7660263e7cf86"
 
   bottle :unneeded
 
-  depends_on :java
+  depends_on "openjdk"
 
   def install
     inreplace ["igv.sh", "igvtools"], /^prefix=.*/, "prefix=#{libexec}"
-    inreplace "igvtools", "@igv.args", '@"${prefix}/igv.args"'
     bin.install "igv.sh" => "igv"
     bin.install "igvtools"
     libexec.install "igv.args", "lib"
-    bin.env_script_all_files libexec, Language::Java.java_home_env
+    bin.env_script_all_files libexec, :JAVA_HOME => Formula["openjdk"].opt_prefix
   end
 
   test do
     assert_match "Usage:", shell_output("#{bin}/igvtools")
-    assert_match "org/broad/igv/ui/IGV.class", shell_output("jar tf #{libexec}/lib/igv.jar")
+    assert_match "org/broad/igv/ui/IGV.class", shell_output("#{Formula["openjdk"].bin}/jar tf #{libexec}/lib/igv.jar")
     # Fails on Jenkins with Unhandled exception: java.awt.HeadlessException
     unless ENV["CI"]
       (testpath/"script").write "exit"
