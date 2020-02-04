d8a799f48f1796d71a182f67f9fe130f4926d3df
Markus Reiter <me@reitermark.us>
2020-02-04 19:01:29 +0100

    fmpp: depend on openjdk

--- Formula/fmpp.rb
+++ Formula/fmpp.rb
@@ -3,15 +3,19 @@ class Fmpp < Formula
   homepage "https://fmpp.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/fmpp/fmpp/0.9.16/fmpp_0.9.16.tar.gz"
   sha256 "86561e3f3a2ccb436f5f3df88d79a7dad72549a33191901f49d12a38b53759cd"
+  revision 1
   head "https://github.com/freemarker/fmpp.git"
 
   bottle :unneeded
 
-  depends_on :java
+  depends_on "openjdk"
 
   def install
     libexec.install "lib"
-    bin.write_jar_script libexec/"lib/fmpp.jar", "fmpp", "-Dfmpp.home=#{libexec} $FMPP_OPTS $FMPP_ARGS"
+    (bin/"fmpp").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["openjdk"].opt_bin}/java" -Dfmpp.home="#{libexec}" $FMPP_OPTS $FMPP_ARGS -jar "#{libexec}/lib/fmpp.jar" "$@"
+    EOS
   end
 
   test do
