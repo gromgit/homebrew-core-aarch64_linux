c0ad799cf7f82910513fbcf49ed693a2ec93ead1
Markus Reiter <me@reitermark.us>
2020-02-04 10:44:42 +0100

    pmd: depend on openjdk

--- Formula/pmd.rb
+++ Formula/pmd.rb
@@ -3,16 +3,16 @@ class Pmd < Formula
   homepage "https://pmd.github.io"
   url "https://github.com/pmd/pmd/releases/download/pmd_releases/6.21.0/pmd-bin-6.21.0.zip"
   sha256 "c146ce6b6d32590f7f4ef59d11351c47961a82753cf77e9ae2f8047244f933ca"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
     rm Dir["bin/*.bat"]
     libexec.install Dir["*"]
-    (bin/"pmd").write_env_script libexec/"bin/run.sh",
-                                 Language::Java.java_home_env("1.8+")
+    (bin/"pmd").write_env_script libexec/"bin/run.sh", :JAVA_HOME => "${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
   end
 
   def caveats; <<~EOS
