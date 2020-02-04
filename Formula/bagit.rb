2ccdf2d1b25aea33748ab1b8af1b110462fe01e8
Markus Reiter <me@reitermark.us>
2020-02-04 11:31:32 +0100

    bagit: depend on openjdk

--- Formula/bagit.rb
+++ Formula/bagit.rb
@@ -3,10 +3,11 @@ class Bagit < Formula
   homepage "https://github.com/LibraryOfCongress/bagit-java"
   url "https://github.com/LibraryOfCongress/bagit-java/releases/download/v4.12.3/bagit-v4.12.3.zip"
   sha256 "7736d00fafe575ea298788a8d697a2af490dd12f856b43cd57bf579a374199e1"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
     # put logs in var, not in the Cellar
@@ -15,7 +16,8 @@ class Bagit < Formula
 
     libexec.install Dir["*"]
 
-    bin.install_symlink libexec/"bin/bagit"
+    (bin/"bagit").write_env_script libexec/"bin/bagit",
+                                   :JAVA_HOME => Formula["openjdk"].opt_prefix
   end
 
   test do
