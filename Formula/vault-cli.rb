d6e4ad83fb72aedfeb4314a9094ed0010b9fbf8a
Markus Reiter <me@reitermark.us>
2020-02-04 17:23:24 +0100

    vault-cli: depend on openjdk
    Closes #50497.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/vault-cli.rb
+++ Formula/vault-cli.rb
@@ -3,11 +3,12 @@ class VaultCli < Formula
   homepage "https://jackrabbit.apache.org/filevault/index.html"
   url "https://search.maven.org/remotecontent?filepath=org/apache/jackrabbit/vault/vault-cli/3.4.2/vault-cli-3.4.2-bin.tar.gz"
   sha256 "316052e9f6bf22f35f3b4b477d46ce6c6e17ff9de6d6788b82eba61506088f75"
+  revision 1
   head "https://github.com/apache/jackrabbit-filevault.git"
 
   bottle :unneeded
 
-  depends_on :java
+  depends_on "openjdk"
 
   def install
     # Remove windows files
@@ -15,7 +16,7 @@ class VaultCli < Formula
 
     libexec.install Dir["*"]
     bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env)
+    bin.env_script_all_files(libexec/"bin", :JAVA_HOME => Formula["openjdk"].opt_prefix)
   end
 
   test do
