abed4fb196137041f63cc0885471868b8f9b9cbd
Markus Reiter <me@reitermark.us>
2020-02-04 11:06:22 +0100

    mill: depend on openjdk

--- Formula/mill.rb
+++ Formula/mill.rb
@@ -3,15 +3,16 @@ class Mill < Formula
   homepage "https://www.lihaoyi.com/mill/"
   url "https://github.com/lihaoyi/mill/releases/download/0.6.0/0.6.0"
   sha256 "499f73887128a85ec5ff904f7c6e009354ef49148ac0d4307ea03431d7d8be5d"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
     libexec.install Dir["*"].shift => "mill"
     chmod 0555, libexec/"mill"
-    (bin/"mill").write_env_script libexec/"mill", Language::Java.java_home_env("1.8+")
+    (bin/"mill").write_env_script libexec/"mill", :JAVA_HOME => "${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
   end
 
   test do
