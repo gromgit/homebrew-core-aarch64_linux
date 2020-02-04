ffce683ac8432ac447f7a38c6c5a0c05ad9dd973
Markus Reiter <me@reitermark.us>
2020-02-04 11:55:11 +0100

    kumo: depend on openjdk

--- Formula/kumo.rb
+++ Formula/kumo.rb
@@ -3,14 +3,18 @@ class Kumo < Formula
   homepage "https://github.com/kennycason/kumo"
   url "https://search.maven.org/remotecontent?filepath=com/kennycason/kumo-cli/1.17/kumo-cli-1.17.jar"
   sha256 "17f84d6287eeccf361c9eabe6d7449983c13d777afd161105a324c00adcadd0e"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
     libexec.install "kumo-cli-#{version}.jar"
-    bin.write_jar_script libexec/"kumo-cli-#{version}.jar", "kumo"
+    (bin/"kumo").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["openjdk"].opt_bin}/java" -jar "#{libexec}/kumo-cli-#{version}.jar" "$@"
+    EOS
   end
 
   test do
