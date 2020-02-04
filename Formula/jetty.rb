29244f833f06192c36575df003a39056bb98b29d
Markus Reiter <me@reitermark.us>
2020-02-04 11:55:21 +0100

    jetty: depend on openjdk

--- Formula/jetty.rb
+++ Formula/jetty.rb
@@ -4,24 +4,26 @@ class Jetty < Formula
   url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-distribution/9.4.26.v20200117/jetty-distribution-9.4.26.v20200117.tar.gz"
   version "9.4.26.v20200117"
   sha256 "31a157c493687e9b7be7366a5dc4ee7ef9cae1663ea279cd9fcf4070d53ef071"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
     libexec.install Dir["*"]
-    (libexec+"logs").mkpath
+    (libexec/"logs").mkpath
 
     bin.mkpath
     Dir.glob("#{libexec}/bin/*.sh") do |f|
       scriptname = File.basename(f, ".sh")
-      (bin+scriptname).write <<~EOS
+      (bin/scriptname).write <<~EOS
         #!/bin/bash
-        JETTY_HOME=#{libexec}
-        #{f} "$@"
+        export JETTY_HOME='#{libexec}'
+        export JAVA_HOME="${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
+        exec #{f} "$@"
       EOS
-      chmod 0755, bin+scriptname
+      chmod 0755, bin/scriptname
     end
   end
 
