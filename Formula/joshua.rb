a882a0f5cf28757889ece971fe1f93079c3206ed
Markus Reiter <me@reitermark.us>
2020-02-04 19:15:39 +0100

    joshua: depend on openjdk

--- Formula/joshua.rb
+++ Formula/joshua.rb
@@ -3,6 +3,7 @@ class Joshua < Formula
   homepage "https://joshua.incubator.apache.org/"
   url "https://cs.jhu.edu/~post/files/joshua-6.0.5.tgz"
   sha256 "972116a74468389e89da018dd985f1ed1005b92401907881a14bdcc1be8bd98a"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -13,7 +14,7 @@ class Joshua < Formula
     sha256 "343ffcd545e812b27b73807070f778e311d62011351be532de355fb85a8e7ed4" => :sierra
   end
 
-  depends_on :java
+  depends_on "openjdk"
 
   def install
     rm Dir["lib/*.{gr,tar.gz}"]
@@ -21,12 +22,11 @@ class Joshua < Formula
     rm_rf "bin/.gitignore"
 
     libexec.install Dir["*"]
-    bin.install_symlink Dir["#{libexec}/bin/*"]
-    inreplace "#{bin}/joshua-decoder", "JOSHUA\=$(dirname $0)/..", "#JOSHUA\=$(dirname $0)/.."
-    inreplace "#{bin}/decoder", "JOSHUA\=$(dirname $0)/..", "#JOSHUA\=$(dirname $0)/.."
+    bin.install Dir["#{libexec}/bin/*"]
+    bin.env_script_all_files libexec/"bin", :JAVA_HOME => Formula["openjdk"].opt_prefix
   end
 
   test do
-    assert_equal "test_OOV\n", pipe_output("#{libexec}/bin/joshua-decoder -v 0 -output-format %s -mark-oovs", "test")
+    assert_equal "test_OOV\n", pipe_output("#{bin}/joshua-decoder -v 0 -output-format %s -mark-oovs", "test")
   end
 end
