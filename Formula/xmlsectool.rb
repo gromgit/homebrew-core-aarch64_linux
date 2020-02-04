fad06da2ea06188eac164276bfedd63106896557
Markus Reiter <me@reitermark.us>
2020-02-04 17:22:51 +0100

    xmlsectool: depend on openjdk

--- Formula/xmlsectool.rb
+++ Formula/xmlsectool.rb
@@ -3,16 +3,17 @@ class Xmlsectool < Formula
   homepage "https://wiki.shibboleth.net/confluence/display/XSTJ2/xmlsectool+V2+Home"
   url "https://shibboleth.net/downloads/tools/xmlsectool/latest/xmlsectool-2.0.0-bin.zip"
   sha256 "83ea1e977d947dd5f2f5600a3280bc5d372a4913f0dbce1ddf2c0ca1c869ca57"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.7+"
+  depends_on "openjdk"
 
   def install
     prefix.install "doc/LICENSE.txt"
     rm_rf "doc"
     libexec.install Dir["*"]
-    (bin/"xmlsectool").write_env_script "#{libexec}/xmlsectool.sh", Language::Java.java_home_env
+    (bin/"xmlsectool").write_env_script "#{libexec}/xmlsectool.sh", :JAVA_HOME => Formula["openjdk"].opt_prefix
   end
 
   test do
