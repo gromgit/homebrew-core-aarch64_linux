f30c4845ddab7817b2de797a8d8a6f84bfb118d0
Markus Reiter <me@reitermark.us>
2020-02-04 10:45:18 +0100

    scala@2.12: depend on openjdk

--- Formula/scala@2.12.rb
+++ Formula/scala@2.12.rb
@@ -5,19 +5,21 @@ class ScalaAT212 < Formula
   mirror "https://www.scala-lang.org/files/archive/scala-2.12.10.tgz"
   mirror "https://downloads.typesafe.com/scala/2.12.10/scala-2.12.10.tgz"
   sha256 "3b12bda3300fedd91f64fc7f9165fd45c58328b1b760af24ca6ffe92e3b0656a"
+  revision 1
 
   bottle :unneeded
 
   keg_only :versioned_formula
 
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
     rm_f Dir["bin/*.bat"]
     doc.install Dir["doc/*"]
     share.install "man"
     libexec.install "bin", "lib"
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    bin.install Dir["#{libexec}/bin/*"]
+    bin.env_script_all_files libexec/"bin", :JAVA_HOME => "${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
 
     # Set up an IntelliJ compatible symlink farm in 'idea'
     idea = prefix/"idea"
