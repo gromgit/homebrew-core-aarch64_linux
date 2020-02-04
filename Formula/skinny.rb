3b3da9c0d61e174632a5e4744187bbf51138a984
Markus Reiter <me@reitermark.us>
2020-02-04 10:45:25 +0100

    skinny: depend on openjdk

--- Formula/skinny.rb
+++ Formula/skinny.rb
@@ -3,15 +3,17 @@ class Skinny < Formula
   homepage "http://skinny-framework.org/"
   url "https://github.com/skinny-framework/skinny-framework/releases/download/3.0.3/skinny-3.0.3.tar.gz"
   sha256 "6b4af009f7df8ba21cc54b20b2856a40d201906e09fe75fb528b5241cea515d9"
+  revision 1
 
   bottle :unneeded
-  depends_on :java => "1.8+"
+  depends_on "openjdk"
 
   def install
     libexec.install Dir["*"]
     (bin/"skinny").write <<~EOS
       #!/bin/bash
       export PATH=#{bin}:$PATH
+      export JAVA_HOME="${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
       PREFIX="#{libexec}" exec "#{libexec}/skinny" "$@"
     EOS
   end
