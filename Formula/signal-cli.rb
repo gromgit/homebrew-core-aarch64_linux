84cbe779b460fda23fa4dd9b947f36f793c25e97
Markus Reiter <me@reitermark.us>
2020-02-06 00:51:38 +0100

    signal-cli: depend on openjdk

--- Formula/signal-cli.rb
+++ Formula/signal-cli.rb
@@ -3,14 +3,15 @@ class SignalCli < Formula
   homepage "https://github.com/AsamK/signal-cli"
   url "https://github.com/AsamK/signal-cli/releases/download/v0.6.5/signal-cli-0.6.5.tar.gz"
   sha256 "7bff4276e583ea3d0ad518423073142d92706c2498561786fb7cf5a626c25320"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.7+"
+  depends_on "openjdk"
 
   def install
     libexec.install Dir["lib", "bin"]
-    (bin/"signal-cli").write_env_script libexec/"bin/signal-cli", Language::Java.java_home_env("1.7+")
+    (bin/"signal-cli").write_env_script libexec/"bin/signal-cli", :JAVA_HOME => Formula["openjdk"].opt_prefix
   end
 
   test do
