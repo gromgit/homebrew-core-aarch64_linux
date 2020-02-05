2ff678fc6f4f6c40855b5a0057ccc373d2fdcbb6
Markus Reiter <me@reitermark.us>
2020-02-06 00:51:45 +0100

    pig: depend on openjdk
    Closes #50492.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/pig.rb
+++ Formula/pig.rb
@@ -4,16 +4,19 @@ class Pig < Formula
   url "https://www.apache.org/dyn/closer.cgi?path=pig/pig-0.17.0/pig-0.17.0.tar.gz"
   mirror "https://archive.apache.org/dist/pig/pig-0.17.0/pig-0.17.0.tar.gz"
   sha256 "6d613768e9a6435ae8fa758f8eef4bd4f9d7f336a209bba3cd89b843387897f3"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.6+"
+  depends_on "openjdk"
 
   def install
     (libexec/"bin").install "bin/pig"
     libexec.install Dir["pig-#{version}-core-h*.jar"]
     libexec.install "lib"
-    (bin/"pig").write_env_script libexec/"bin/pig", Language::Java.java_home_env("1.6+").merge(:PIG_HOME => libexec)
+    (bin/"pig").write_env_script libexec/"bin/pig",
+                                 :PIG_HOME  => libexec,
+                                 :JAVA_HOME => Formula["openjdk"].opt_prefix
   end
 
   test do
