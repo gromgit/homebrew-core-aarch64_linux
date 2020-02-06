2f5e2b0aff9534e1d643d40a20472dde9d674a10
Markus Reiter <me@reitermark.us>
2020-02-06 10:43:22 +0100

    cfr-decompiler: depend on openjdk

--- Formula/cfr-decompiler.rb
+++ Formula/cfr-decompiler.rb
@@ -3,14 +3,19 @@ class CfrDecompiler < Formula
   homepage "https://www.benf.org/other/cfr/"
   url "https://www.benf.org/other/cfr/cfr-0.148.jar"
   sha256 "1407f91fe7f94ff700cc1c8d546986a3fc2554273f07bae664bd58b0d85fd012"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.6+"
+  depends_on "openjdk"
 
   def install
     libexec.install "cfr-#{version}.jar"
-    bin.write_jar_script libexec/"cfr-#{version}.jar", "cfr-decompiler"
+    (bin/"cfr-decompiler").write <<~EOS
+      #!/bin/bash
+      export JAVA_HOME="${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
+      exec "${JAVA_HOME}/bin/java" -jar "#{libexec}/cfr-#{version}.jar" "$@"
+    EOS
   end
 
   test do
@@ -27,7 +32,7 @@ class CfrDecompiler < Formula
       }
     EOS
     (testpath/"T.java").write fixture
-    system "javac", "T.java"
+    system "#{Formula["openjdk"].bin}/javac", "T.java"
     output = pipe_output("#{bin}/cfr-decompiler T.class")
     assert_match fixture, output
   end
