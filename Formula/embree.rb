f12ffa53c75e526e9d7ebd82bf09511a3717feca
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-06 12:54:55 +0000

    embree: update 3.8.0 bottle.
    Closes #49828.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/embree.rb
+++ Formula/embree.rb
@@ -7,9 +7,9 @@ class Embree < Formula
 
   bottle do
     cellar :any
-    sha256 "e18860a08289bcef0e093c9dda4a4ffbbc123f3f14395cc941cdc69c01bce9ef" => :catalina
-    sha256 "8faa655d40472ea2c3d3fb0cee30629df5654afb2f32c5a7ac81b48907a149f1" => :mojave
-    sha256 "d6553bdd4020aedb7af7f11009bae10de51f4522d19932d1c93abd9505ff9497" => :high_sierra
+    sha256 "eeadbb69a2e0439b9c2d67391dec7ceefc77de75ed0535ec49af93d8ed8c41aa" => :catalina
+    sha256 "fd0ce3dbe8248f382168ce9bccf6d31145de02ee062f333cba5eb6a966f4d532" => :mojave
+    sha256 "5a5641cf4b93a026bc362ba695a5b92788c92ed975222959797c697e20a8be16" => :high_sierra
   end
 
   depends_on "cmake" => :build
