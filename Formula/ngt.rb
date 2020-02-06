82d0cc212ec7fc00a1ae9c96334ab1d9fc75d8eb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-06 12:36:46 +0000

    ngt: update 1.9.0 bottle.

--- Formula/ngt.rb
+++ Formula/ngt.rb
@@ -6,9 +6,9 @@ class Ngt < Formula
 
   bottle do
     cellar :any
-    sha256 "d80e3040720c7f35c43bf9270141e0b10cf7313107af35ae088ddd6ca205174d" => :catalina
-    sha256 "288fdb59156ddf7819f259657d43c860c1a9cfdbe2ac38daa0d7953cff4e6a6b" => :mojave
-    sha256 "fb50c0927a8a06dff04acc1512acf980bc65819eda8b11776b077e242e116ee1" => :high_sierra
+    sha256 "e85832187d9849ecc9c125cd42c1f48c2165358a3bf84f5f67ae1b0fce138eb9" => :catalina
+    sha256 "f03745c7c144e14c0b0caa95b58cd04214b22b4e4bfd986201160d53b27e2d34" => :mojave
+    sha256 "b9c7793735d8a88ef1cfee05353b053b3f48dba0f2d559789e92dc0599fa9752" => :high_sierra
   end
 
   depends_on "cmake" => :build
