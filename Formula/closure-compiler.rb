4c87db002b263ab2e0e4f46974c2b66fa15b71e8
Markus Reiter <me@reitermark.us>
2020-02-06 01:42:44 +0100

    closure-compiler: depend on openjdk

--- Formula/closure-compiler.rb
+++ Formula/closure-compiler.rb
@@ -3,14 +3,18 @@ class ClosureCompiler < Formula
   homepage "https://github.com/google/closure-compiler"
   url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20200204/closure-compiler-v20200204.jar"
   sha256 "2cabfd9d43019d6e76c081a3e2a355d85600576b485123f4a0ee46a85c75ae30"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.7+"
+  depends_on "openjdk"
 
   def install
     libexec.install Dir["*"]
-    bin.write_jar_script libexec.children.first, "closure-compiler"
+    (bin/"closure-compiler").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["openjdk"].opt_bin}/java" -jar "#{libexec.children.first}" "$@"
+    EOS
   end
 
   test do
