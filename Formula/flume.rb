371c33921281bbfe39a5807628743b42581b2bb7
Markus Reiter <me@reitermark.us>
2020-02-06 02:15:32 +0100

    flume: depend on openjdk
    Closes #50491.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/flume.rb
+++ Formula/flume.rb
@@ -4,18 +4,20 @@ class Flume < Formula
   url "https://www.apache.org/dyn/closer.cgi?path=flume/1.9.0/apache-flume-1.9.0-bin.tar.gz"
   mirror "https://archive.apache.org/dist/flume/1.9.0/apache-flume-1.9.0-bin.tar.gz"
   sha256 "0373ed5abfd44dc4ab23d9a02251ffd7e3b32c02d83a03546e97ec15a7b23619"
+  revision 1
 
   bottle :unneeded
 
   depends_on "hadoop"
-  depends_on :java => "1.7+"
+  depends_on "openjdk"
 
   def install
     rm_f Dir["bin/*.cmd", "bin/*.ps1"]
     libexec.install %w[conf docs lib tools]
     bin.install Dir["bin/*"]
-    bin.env_script_all_files(libexec/"bin",
-      Language::Java.java_home_env("1.7+").merge(:FLUME_HOME => libexec))
+    bin.env_script_all_files libexec/"bin",
+                             :JAVA_HOME  => Formula["openjdk"].opt_prefix,
+                             :FLUME_HOME => libexec
   end
 
   test do
