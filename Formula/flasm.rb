de6f9003ea486844125cb6cdf4b27cba18d8858c
Viktor Szakats <commit@vsz.me>
2020-02-06 19:23:33 +0000

    flasm: secure urls

--- Formula/flasm.rb
+++ Formula/flasm.rb
@@ -1,7 +1,7 @@
 class Flasm < Formula
   desc "Flash command-line assembler and disassembler"
-  homepage "http://www.nowrap.de/flasm.html"
-  url "http://www.nowrap.de/download/flasm16src.zip"
+  homepage "https://www.nowrap.de/flasm.html"
+  url "https://www.nowrap.de/download/flasm16src.zip"
   version "1.62"
   sha256 "df1273a506e2479cf95775197f5b7fa94e29fe1e0aae5aa190ed5bbebc4be5c6"
 
