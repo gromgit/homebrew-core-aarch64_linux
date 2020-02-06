6d6ef343e1b564e6824b5837420aceac5852abf4
Markus Reiter <me@reitermark.us>
2020-02-06 02:47:43 +0100

    galen: depend on openjdk

--- Formula/galen.rb
+++ Formula/galen.rb
@@ -3,17 +3,18 @@ class Galen < Formula
   homepage "http://galenframework.com/"
   url "https://github.com/galenframework/galen/releases/download/galen-2.4.4/galen-bin-2.4.4.zip"
   sha256 "b89ed0ccef4e5ea310563ab3220965f72d5fc182e89e6faadf44780f1c43b88d"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.6+"
+  depends_on "openjdk"
 
   def install
     libexec.install "galen.jar"
     (bin/"galen").write <<~EOS
       #!/bin/sh
       set -e
-      java -cp "#{libexec}/galen.jar:lib/*:libs/*" com.galenframework.GalenMain "$@"
+      exec "#{Formula["openjdk"].opt_bin}/java" -cp "#{libexec}/galen.jar:lib/*:libs/*" com.galenframework.GalenMain "$@"
     EOS
   end
 
