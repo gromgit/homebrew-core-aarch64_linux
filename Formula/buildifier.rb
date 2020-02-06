56ba5f79ebf14ddc0b25e368ab4bddf7df98e473
Rui Chen <chenrui333@gmail.com>
2020-02-06 14:44:03 -0500

    buildifier 1.0.0
    Closes #49871.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/buildifier.rb
+++ Formula/buildifier.rb
@@ -2,8 +2,8 @@ class Buildifier < Formula
   desc "Format bazel BUILD files with a standard convention"
   homepage "https://github.com/bazelbuild/buildtools"
   url "https://github.com/bazelbuild/buildtools.git",
-      :tag      => "0.29.0",
-      :revision => "5bcc31df55ec1de770cb52887f2e989e7068301f"
+      :tag      => "1.0.0",
+      :revision => "9e63c6f02e63adaab63e21a3a8feb75f4f7503cb"
 
   bottle do
     cellar :any_skip_relocation
