05775a51e7291fddaf738c68fe316c5bec2165c6
Markus Reiter <me@reitermark.us>
2020-02-06 02:47:16 +0100

    groovysdk: depend on openjdk

--- Formula/groovysdk.rb
+++ Formula/groovysdk.rb
@@ -3,24 +3,24 @@ class Groovysdk < Formula
   homepage "https://www.groovy-lang.org/"
   url "https://dl.bintray.com/groovy/maven/apache-groovy-sdk-3.0.1.zip"
   sha256 "85b763f02e831a609716f4076e160827f1f783112c0361ec7b71e2a65b7f4f83"
+  revision 1
 
   bottle :unneeded
 
-  # Groovy 2.5 requires JDK8+ to build and JDK7 is the minimum version of the JRE that we support.
-  depends_on :java => "1.7+"
+  depends_on "openjdk"
 
   conflicts_with "groovy", :because => "both install the same binaries"
 
   def install
-    ENV["GROOVY_HOME"] = libexec
-
     # We don't need Windows' files.
     rm_f Dir["bin/*.bat"]
 
     prefix.install_metafiles
     bin.install Dir["bin/*"]
     libexec.install "conf", "lib", "src", "doc"
-    bin.env_script_all_files(libexec+"bin", :GROOVY_HOME => ENV["GROOVY_HOME"])
+    bin.env_script_all_files libexec/"bin",
+                             :GROOVY_HOME => libexec,
+                             :JAVA_HOME   => "${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
   end
 
   test do
