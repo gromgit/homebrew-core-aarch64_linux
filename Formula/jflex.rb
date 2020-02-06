e893250597889a2d4bdeb8094456f24e81e073ce
Markus Reiter <me@reitermark.us>
2020-02-06 10:40:55 +0100

    jflex: depend on openjdk

--- Formula/jflex.rb
+++ Formula/jflex.rb
@@ -3,15 +3,20 @@ class Jflex < Formula
   homepage "https://jflex.de/"
   url "https://jflex.de/release/jflex-1.7.0.zip"
   sha256 "833ea6371a4b5ee16023f328fbf7babd41a71e93155cf869c53f3ff670508107"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.7+"
+  depends_on "openjdk"
 
   def install
     pkgshare.install "examples"
     libexec.install "lib/jflex-full-#{version}.jar" => "jflex-#{version}.jar"
-    bin.write_jar_script libexec/"jflex-#{version}.jar", "jflex"
+    (bin/"jflex").write <<~EOS
+      #!/bin/bash
+      export JAVA_HOME="${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
+      exec "${JAVA_HOME}/bin/java" -jar "#{libexec}/jflex-#{version}.jar" "$@"
+    EOS
   end
 
   test do
