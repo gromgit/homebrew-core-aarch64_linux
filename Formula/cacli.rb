252124ee5095b3422a184613e80f5fc005dfd871
Nick <bourdakos1@gmail.com>
2020-02-10 15:47:43 -0500

    cacli 1.2.29 (new formula)
    Closes #49978.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

---
+++ Formula/cacli.rb
@@ -0,0 +1,25 @@
+class Cacli < Formula
+  desc "Train machine learning models from Cloud Annotations"
+  homepage "https://cloud.annotations.ai"
+  url "https://github.com/cloud-annotations/training/archive/v1.2.29.tar.gz"
+  sha256 "bb95ca829a705d2fcb77302415b2a7c8d9ed7e6bd2afa95c97c5b2fd0da45c50"
+
+  depends_on "go" => :build
+
+  def install
+    cd "cacli" do
+      project = "github.com/cloud-annotations/training/cacli"
+      system "go", "build",
+             "-ldflags", "-s -w -X #{project}/version.Version=#{version}",
+             "-o", bin/"cacli"
+    end
+  end
+
+  test do
+    # Attempt to list training runs without credentials and confirm that it
+    # fails as expected.
+    output = shell_output("#{bin}/cacli list 2>&1", 1).strip
+    cleaned = output.gsub(/\e\[([;\d]+)?m/, "") # Remove colors from output.
+    assert_match "FAILED\nNot logged in. Use 'cacli login' to log in.", cleaned
+  end
+end
