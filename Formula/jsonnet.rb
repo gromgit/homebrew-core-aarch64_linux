828b3559605d6cd62fb5238085eb12266ea7019a
Rui Chen <chenrui333@gmail.com>
2020-02-09 20:27:29 -0500

    jsonnet 0.15.0
    Closes #49953.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/jsonnet.rb
+++ Formula/jsonnet.rb
@@ -1,8 +1,8 @@
 class Jsonnet < Formula
   desc "Domain specific configuration language for defining JSON data"
   homepage "https://jsonnet.org/"
-  url "https://github.com/google/jsonnet/archive/v0.14.0.tar.gz"
-  sha256 "7f41cdc4cb366cfc1de3aada926a2f7e640ef025887eb05cdaf8342d6e4e088b"
+  url "https://github.com/google/jsonnet/archive/v0.15.0.tar.gz"
+  sha256 "0b58f2a36a5625c717e717a7e85608730e7bb5bfd8be1765dd6fa23be1f9b9e8"
   head "https://github.com/google/jsonnet.git"
 
   bottle do
