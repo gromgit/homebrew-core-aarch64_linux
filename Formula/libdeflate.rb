b39425f31884c83f0b06c097a882b2773b3dfea8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-11 11:51:18 +0000

    libdeflate: add 1.5 bottle.

--- Formula/libdeflate.rb
+++ Formula/libdeflate.rb
@@ -4,6 +4,13 @@ class Libdeflate < Formula
   url "https://github.com/ebiggers/libdeflate/archive/v1.5.tar.gz"
   sha256 "45b1b2332f443b705c59d06a49be009827291d2c487b076dc8ec2791eff4c711"
 
+  bottle do
+    cellar :any
+    sha256 "d953614355c76a7aa75c6517fc60b4e5d4163fb5582690a3b00a3b9e1a4fd712" => :catalina
+    sha256 "99672ee99b6bc41349c08cf4f047f21f780ad9aa9cf9eb7cca0639da4f79bb48" => :mojave
+    sha256 "249ed4a8ee9ba404dd8cce6efff0365158992aecaab4a2923b9b62ad35f1234a" => :high_sierra
+  end
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
