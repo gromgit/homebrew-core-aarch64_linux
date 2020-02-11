737f6a5476e396cfecec40c7ed0fa7e6a73c7f2a
Milot Mirdita <milot@mirdita.de>
2020-02-12 00:14:01 +0100

    mmseqs2 11-e1a1c

--- Formula/mmseqs2.rb
+++ Formula/mmseqs2.rb
@@ -1,9 +1,10 @@
 class Mmseqs2 < Formula
   desc "Software suite for very fast sequence search and clustering"
   homepage "https://mmseqs.com/"
-  url "https://github.com/soedinglab/MMseqs2/archive/10-6d92c.tar.gz"
-  version "10-6d92c"
-  sha256 "62415e545706adc6e9e6689d34902f405ab5e5c67c8c7562bdd9dd4da2088697"
+  url "https://github.com/soedinglab/MMseqs2/archive/11-e1a1c.tar.gz"
+  version "11-e1a1c"
+  sha256 "ffe1ae300dbe1a0e3d72fc9e947356a4807f07951cb56316f36974d8d5875cbb"
+  head "https://github.com/soedinglab/MMseqs2.git"
 
   bottle do
     cellar :any
@@ -14,21 +15,24 @@ class Mmseqs2 < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "gcc"
-
-  cxxstdlib_check :skip
-
-  fails_with :clang # needs OpenMP support
+  depends_on "libomp"
 
   resource "documentation" do
     url "https://github.com/soedinglab/MMseqs2.wiki.git",
-        :revision => "03da86a5c553d00c8d4484e9fbd8d68ef14e1169"
+        :revision => "c77918c9cebb24075f3c102a73fb1d413017a1a5"
   end
 
   def install
     args = *std_cmake_args << "-DHAVE_TESTS=0" << "-DHAVE_MPI=0"
     args << "-DVERSION_OVERRIDE=#{version}"
     args << "-DHAVE_SSE4_1=1"
+
+    libomp = Formula["libomp"]
+    args << "-DOpenMP_C_FLAGS=\"-Xpreprocessor -fopenmp -I#{libomp.opt_include}\""
+    args << "-DOpenMP_C_LIB_NAMES=omp"
+    args << "-DOpenMP_CXX_FLAGS=\"-Xpreprocessor -fopenmp -I#{libomp.opt_include}\""
+    args << "-DOpenMP_CXX_LIB_NAMES=omp"
+    args << "-DOpenMP_omp_LIBRARY=#{libomp.opt_lib}/libomp.a"
 
     system "cmake", ".", *args
     system "make", "install"
