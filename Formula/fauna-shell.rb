8a5379bf6ff5d3a234993d90ec9fcbbcf0ef2593
Ryan Harris <harrisryan1@gmail.com>
2020-02-12 16:14:34 -0500

    fauna-shell 0.11.0
    Update fauna-shell.rb, v0.11.0
    
    Closes #50075.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/fauna-shell.rb
+++ Formula/fauna-shell.rb
@@ -3,8 +3,8 @@ require "language/node"
 class FaunaShell < Formula
   desc "Interactive shell for FaunaDB"
   homepage "https://fauna.com/"
-  url "https://registry.npmjs.org/fauna-shell/-/fauna-shell-0.10.0.tgz"
-  sha256 "0af05027f5d01d9058db45d24955ba2ab41591237f9b9af62b7c8e34fb7ef055"
+  url "https://registry.npmjs.org/fauna-shell/-/fauna-shell-0.11.0.tgz"
+  sha256 "4f0c9834fc38b68b11ba045ea96a449de20cf026d9de5a8c7259160b708455e4"
 
   bottle do
     cellar :any_skip_relocation
