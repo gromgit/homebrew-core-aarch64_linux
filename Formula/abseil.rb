ecd1696613d9bde2efb9c066cd52cf61f45ee9d3
Andrew Kane <andrew@chartkick.com>
2020-02-12 20:38:49 -0800

    abseil 20190808 (new formula)
    Closes #50130.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

---
+++ Formula/abseil.rb
@@ -0,0 +1,35 @@
+class Abseil < Formula
+  desc "C++ Common Libraries"
+  homepage "https://abseil.io"
+  url "https://github.com/abseil/abseil-cpp/archive/20190808.tar.gz"
+  sha256 "8100085dada279bf3ee00cd064d43b5f55e5d913be0dfe2906f06f8f28d5b37e"
+
+  depends_on "cmake" => :build
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cc").write <<~EOS
+      #include <iostream>
+      #include <string>
+      #include <vector>
+      #include "absl/strings/str_join.h"
+
+      int main() {
+        std::vector<std::string> v = {"foo","bar","baz"};
+        std::string s = absl::StrJoin(v, "-");
+
+        std::cout << "Joined string: " << s << "\\n";
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "-I#{include}", "-L#{lib}", "-labsl_strings",
+                    "test.cc", "-o", "test"
+    assert_equal "Joined string: foo-bar-baz\n", shell_output("#{testpath}/test")
+  end
+end
