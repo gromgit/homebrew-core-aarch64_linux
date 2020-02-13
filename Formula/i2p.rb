156f1da1a079ae6d1bb1b4486fb50fdf39cebc2f
Markus Reiter <me@reitermark.us>
2020-02-13 05:07:58 +0100

    i2p: depend on openjdk (#49917)

--- Formula/i2p.rb
+++ Formula/i2p.rb
@@ -4,19 +4,21 @@ class I2p < Formula
   url "https://download.i2p2.de/releases/0.9.44/i2pinstall_0.9.44.jar"
   mirror "https://launchpad.net/i2p/trunk/0.9.44/+download/i2pinstall_0.9.44.jar"
   sha256 "265729c1d676939bc81f96689b72c5f413083f4bbd849393f7629db05523d238"
+  revision 1
 
   bottle :unneeded
 
-  depends_on :java => "1.7+"
+  depends_on "openjdk"
 
   def install
     (buildpath/"path.conf").write "INSTALL_PATH=#{libexec}"
 
-    system "java", "-jar", "i2pinstall_#{version}.jar", "-options", "path.conf"
+    system "#{Formula["openjdk"].opt_bin}/java", "-jar", "i2pinstall_#{version}.jar", "-options", "path.conf", "-language", "eng"
 
     wrapper_name = "i2psvc-macosx-universal-64"
     libexec.install_symlink libexec/wrapper_name => "i2psvc"
-    bin.write_exec_script Dir["#{libexec}/{eepget,i2prouter}"]
+    (bin/"eepget").write_env_script libexec/"eepget", :JAVA_HOME => Formula["openjdk"].opt_prefix
+    (bin/"i2prouter").write_env_script libexec/"i2prouter", :JAVA_HOME => Formula["openjdk"].opt_prefix
     man1.install Dir["#{libexec}/man/*"]
   end
 
