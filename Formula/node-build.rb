251a8f38a0205ce3e4c4d3cf4936a066a7089390
Jason Karns <jason.karns@gmail.com>
2020-02-13 11:35:26 -0500

    node-build 4.7.2 (#50121)
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/node-build.rb
+++ Formula/node-build.rb
@@ -1,8 +1,8 @@
 class NodeBuild < Formula
   desc "Install NodeJS versions"
   homepage "https://github.com/nodenv/node-build"
-  url "https://github.com/nodenv/node-build/archive/v4.7.1.tar.gz"
-  sha256 "4d9e0498f06ddabb2729e7f90666afcf7f85bcc56865a77e1b288fdf7a62fef7"
+  url "https://github.com/nodenv/node-build/archive/v4.7.2.tar.gz"
+  sha256 "fd7070ae044e4d446a13e56b1a9478e691f908500eadb9128e108d69e156fa66"
   head "https://github.com/nodenv/node-build.git"
 
   bottle :unneeded
