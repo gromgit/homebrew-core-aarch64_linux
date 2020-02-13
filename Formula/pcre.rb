f979704ab9b4d8cfc9491027fe812705b70e77f6
Sam Ford <1584702+samford@users.noreply.github.com>
2020-02-12 19:45:24 -0500

    pcre 8.44
    Closes #50079.
    
    Signed-off-by: Zach Auten <zachauten@gmail.com>

--- Formula/pcre.rb
+++ Formula/pcre.rb
@@ -1,9 +1,9 @@
 class Pcre < Formula
   desc "Perl compatible regular expressions library"
   homepage "https://www.pcre.org/"
-  url "https://ftp.pcre.org/pub/pcre/pcre-8.43.tar.bz2"
-  mirror "https://www.mirrorservice.org/sites/ftp.exim.org/pub/pcre/pcre-8.43.tar.bz2"
-  sha256 "91e762520003013834ac1adb4a938d53b22a216341c061b0cf05603b290faf6b"
+  url "https://ftp.pcre.org/pub/pcre/pcre-8.44.tar.bz2"
+  mirror "https://www.mirrorservice.org/sites/ftp.exim.org/pub/pcre/pcre-8.44.tar.bz2"
+  sha256 "19108658b23b3ec5058edc9f66ac545ea19f9537234be1ec62b714c84399366d"
 
   bottle do
     cellar :any
