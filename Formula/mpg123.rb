cb38e3fac6290d3e80f7ea0f3b04c17dd31657fc
FX Coudert <fxcoudert@gmail.com>
2020-02-14 11:50:39 +0100

    mpg123: remove Xcode < 11.3 hack

--- Formula/mpg123.rb
+++ Formula/mpg123.rb
@@ -11,9 +11,6 @@ class Mpg123 < Formula
   end
 
   def install
-    # Work around Xcode 11 clang bug
-    ENV.append_to_cflags "-fno-stack-check" if DevelopmentTools.clang_build_version >= 1010
-
     args = %W[
       --disable-debug
       --disable-dependency-tracking
