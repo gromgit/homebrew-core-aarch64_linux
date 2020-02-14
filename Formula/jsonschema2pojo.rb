50e4710768e5140f365e5e4aa8644eed5b512013
Zach <zachauten@gmail.com>
2020-02-14 08:25:45 -0500

    jsonschema2pojo 1.0.2 (#50185)

--- Formula/jsonschema2pojo.rb
+++ Formula/jsonschema2pojo.rb
@@ -1,8 +1,8 @@
 class Jsonschema2pojo < Formula
   desc "Generates Java types from JSON Schema (or example JSON)"
   homepage "http://www.jsonschema2pojo.org/"
-  url "https://github.com/joelittlejohn/jsonschema2pojo/releases/download/jsonschema2pojo-1.0.1/jsonschema2pojo-1.0.1.tar.gz"
-  sha256 "2184c4fd3ed3ba4674297765ea55c750346f81838ee9cde9d8d36be023465c0a"
+  url "https://github.com/joelittlejohn/jsonschema2pojo/releases/download/jsonschema2pojo-1.0.2/jsonschema2pojo-1.0.2.tar.gz"
+  sha256 "158e927558e3ae4779dc2b7cd00e2010e43465af5a5aebdb55c889ccacb4d6e3"
 
   bottle :unneeded
 
