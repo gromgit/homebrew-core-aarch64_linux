d866a57050b29d328385b64a30001eab5f52c610
Zach Auten <zachauten@gmail.com>
2020-02-13 23:21:43 -0500

    ucon64 2.2.0
    Co-authored-by: Misty De Meo <mistydemeo@gmail.com>
    
    Closes #50226.
    Closes #50591.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Formula/ucon64.rb
+++ Formula/ucon64.rb
@@ -1,8 +1,8 @@
 class Ucon64 < Formula
   desc "ROM backup tool and emulator's Swiss Army knife program"
   homepage "https://ucon64.sourceforge.io/"
-  url "https://downloads.sourceforge.net/ucon64/ucon64-2.1.0-src.tar.gz"
-  sha256 "c99964060a5337cea811b27c4103e186a14ba1f04b19cff08bac0260271bc872"
+  url "https://downloads.sourceforge.net/ucon64/ucon64-2.2.0-src.tar.gz"
+  sha256 "5727e0be9ee878bba84d204135a7ca25662db6b56fee6895301e50c1bdda70af"
   head "https://svn.code.sf.net/p/ucon64/svn/trunk/ucon64"
 
   bottle do
@@ -16,6 +16,14 @@ class Ucon64 < Formula
   resource "super_bat_puncher_demo" do
     url "http://morphcat.de/superbatpuncher/Super%20Bat%20Puncher%20Demo.zip"
     sha256 "d74cb3ba11a4ef5d0f8d224325958ca1203b0d8bb4a7a79867e412d987f0b846"
+  end
+
+  # Fixes an upstream issue which incorrectly attempts to use a Linux-only
+  # function on macOS. Should be in the next release.
+  # https://sourceforge.net/p/ucon64/svn/2763/
+  patch do
+    url "https://github.com/Homebrew/formula-patches/raw/23a80b586dd35fdde1bf575c57a1b468631e644e/ucon64/sched_setscheduler.diff"
+    sha256 "bb7bf52ec016092bf84b0a5eebb4295394288985993d4ab7a6b69a78c1c3ce77"
   end
 
   def install
@@ -34,7 +42,7 @@ class Ucon64 < Formula
                             "--prefix=#{prefix}"
       system "make"
       bin.install "ucon64"
-      libexec.install "libdiscmage/discmage.so" => "libdiscmage.dylib"
+      libexec.install "libdiscmage/discmage.dylib" => "libdiscmage.dylib"
     end
   end
 
