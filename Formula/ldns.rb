b6ec25788b621d7d7c1581176163a781c2759274
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-14 13:11:08 +0000

    ldns: update 1.7.1_2 bottle.

--- Formula/ldns.rb
+++ Formula/ldns.rb
@@ -7,10 +7,9 @@ class Ldns < Formula
 
   bottle do
     cellar :any
-    sha256 "62204338590c6474bc213a3cfc9d422aca1ef35a763412cbcd8f9d2a279d3807" => :catalina
-    sha256 "4658a9bdae49915184e10be212a19f11504ca731e66ad8a118a6cb80838a555b" => :mojave
-    sha256 "2f2ee1ff4f7c6513b9c59fcee2d36b44e0421d238cbecbd6c6ed8f5801ba1803" => :high_sierra
-    sha256 "e4fe44eec5ae7b987b264cdbb64a02a7e53a25d1203edc44cb820cd7cc93093c" => :sierra
+    sha256 "0446a8f1f3bc55d5d618c5bb2137d45dd3784d69e575ebabacf3dddb0d61055a" => :catalina
+    sha256 "1df2448c33ca744971061cecc782c7fe464b449eb9852c8b6287a59b8f696938" => :mojave
+    sha256 "da38537015dd33fa5ee0b5a5239cfc57a83aa4806a59b8d2111f7a498595d40a" => :high_sierra
   end
 
   depends_on "swig" => :build
