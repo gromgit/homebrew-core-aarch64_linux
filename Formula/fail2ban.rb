1cb22fb172b6b79693b9b9d4009cececb9829999
Zach Auten <zachauten@gmail.com>
2020-02-13 22:31:06 -0500

    fail2ban 0.11.1
    Closes #50163.
    
    Signed-off-by: Zach Auten <zachauten@gmail.com>

--- Formula/fail2ban.rb
+++ Formula/fail2ban.rb
@@ -1,8 +1,8 @@
 class Fail2ban < Formula
   desc "Scan log files and ban IPs showing malicious signs"
   homepage "https://www.fail2ban.org/"
-  url "https://github.com/fail2ban/fail2ban/archive/0.10.5.tar.gz"
-  sha256 "a665df6338cf836edd90d1c87dcef28c8cec1aaae5f72e3f2a45ecda89a7ba5f"
+  url "https://github.com/fail2ban/fail2ban/archive/0.11.1.tar.gz"
+  sha256 "71d2a52b66bb0f87ac3812246bdd3819ec561913cd44afd39130a342f043aa6d"
 
   bottle do
     cellar :any_skip_relocation
