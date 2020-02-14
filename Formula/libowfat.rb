617bbff2f071126ee2ab8ba77760ba190358b390
Zach Auten <zachauten@gmail.com>
2020-02-13 22:52:48 -0500

    libowfat 0.32
    Co-Authored-By: Bo Anderson <mail@boanderson.me>
    
    Closes #50193.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/libowfat.rb
+++ Formula/libowfat.rb
@@ -1,8 +1,8 @@
 class Libowfat < Formula
   desc "Reimplements libdjb"
   homepage "https://www.fefe.de/libowfat/"
-  url "https://www.fefe.de/libowfat/libowfat-0.31.tar.xz"
-  sha256 "d1e4ac1cfccbb7dc51d77d96398e6302d229ba7538158826c84cb4254c7e8a12"
+  url "https://www.fefe.de/libowfat/libowfat-0.32.tar.xz"
+  sha256 "f4b9b3d9922dc25bc93adedf9e9ff8ddbebaf623f14c8e7a5f2301bfef7998c1"
   head ":pserver:cvs:@cvs.fefe.de:/cvs", :using => :cvs
 
   bottle do
@@ -12,6 +12,11 @@ class Libowfat < Formula
     sha256 "43e3968245f33399038ffb25f48618be370cb8242f38ddc36170b76cfd0da3fe" => :high_sierra
     sha256 "4f719fe2a03651ecea7882464e5b8fd1f4f3b1e32a0f75f9e5cd9e66ad32a123" => :sierra
     sha256 "be87e0da446834d6f8f808c434e854ff7c9eb88c3f899fc48a830b36117cac83" => :el_capitan
+  end
+
+  patch do
+    url "https://github.com/mistydemeo/libowfat/commit/278a675a6984e5c202eee9f7e36cda2ae5da658d.patch?full_index=1"
+    sha256 "32eab2348f495f483f7cd34ffd7543bd619f312b7094a4b55be9436af89dd341"
   end
 
   def install
