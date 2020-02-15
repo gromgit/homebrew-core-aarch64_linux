53a97afd7088a719ee214c17b2f3904eee371f8e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-15 03:11:28 +0000

    apollo-cli: update 2.22.1 bottle.

--- Formula/apollo-cli.rb
+++ Formula/apollo-cli.rb
@@ -8,9 +8,9 @@ class ApolloCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "dba1d1209d24431375aca9f109a284e601ec3b11402f756965baeba6b2cd734c" => :catalina
-    sha256 "bfd217e6a00cdaf8e4c52dbe1a8636f0461fc348b7659127fd443878e858f66b" => :mojave
-    sha256 "7e14e9f1dbaae663ae63b61032da2f9091709f70707d7918c6297f4658415bd4" => :high_sierra
+    sha256 "71fa57c89c415a6f6b5ff40b75e4c9dc2ec4310cab51fdd79ede606ca6549254" => :catalina
+    sha256 "35b07fc523297a0e94e29fa46453e4ece999294f3c1f8f083ebc3864cb62439a" => :mojave
+    sha256 "2bf53046cef43c0dcb629260e4e75c471801f5625c39507a7ca15a42e05c2d80" => :high_sierra
   end
 
   depends_on "node"
