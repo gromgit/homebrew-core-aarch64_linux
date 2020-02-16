0b805df471d0c4c2fae8987786054f96c9814573
Luke Lau <luke_lau@icloud.com>
2020-02-16 15:25:52 +0000

    literate-git-0.3.1 (new formula)
    Closes #50306.
    
    Signed-off-by: Izaak Beekman <zbeekman@gmail.com>

---
+++ Formula/literate-git.rb
@@ -0,0 +1,78 @@
+class LiterateGit < Formula
+  include Language::Python::Virtualenv
+
+  desc "Render hierarchical git repositories into HTML"
+  homepage "https://github.com/bennorth/literate-git"
+  url "https://github.com/bennorth/literate-git/archive/v0.3.1.tar.gz"
+  sha256 "f1dec77584236a5ab2bcee9169e16b5d976e83cd53d279512136bdc90b04940a"
+
+  depends_on "libgit2"
+  depends_on "python@3.8"
+
+  resource "cffi" do
+    url "https://files.pythonhosted.org/packages/05/54/3324b0c46340c31b909fcec598696aaec7ddc8c18a63f2db352562d3354c/cffi-1.14.0.tar.gz"
+    sha256 "2d384f4a127a15ba701207f7639d94106693b6cd64173d6c8988e2c25f3ac2b6"
+  end
+
+  resource "docopt" do
+    url "https://files.pythonhosted.org/packages/a2/55/8f8cab2afd404cf578136ef2cc5dfb50baa1761b68c9da1fb1e4eed343c9/docopt-0.6.2.tar.gz"
+    sha256 "49b3a825280bd66b3aa83585ef59c4a8c82f2c8a522dbe754a8bc8d08c85c491"
+  end
+
+  resource "Jinja2" do
+    url "https://files.pythonhosted.org/packages/d8/03/e491f423379ea14bb3a02a5238507f7d446de639b623187bccc111fbecdf/Jinja2-2.11.1.tar.gz"
+    sha256 "93187ffbc7808079673ef52771baa950426fd664d3aad1d0fa3e95644360e250"
+  end
+
+  resource "markdown2" do
+    url "https://files.pythonhosted.org/packages/e3/93/d37055743009d1a492b2670cc215831a388b3d6e4a28b7672fdf0f7854f5/markdown2-2.3.8.tar.gz"
+    sha256 "7ff88e00b396c02c8e1ecd8d176cfa418fb01fe81234dcea77803e7ce4f05dbe"
+  end
+
+  resource "MarkupSafe" do
+    url "https://files.pythonhosted.org/packages/b9/2e/64db92e53b86efccfaea71321f597fa2e1b2bd3853d8ce658568f7a13094/MarkupSafe-1.1.1.tar.gz"
+    sha256 "29872e92839765e546828bb7754a68c418d927cd064fd4708fab9fe9c8bb116b"
+  end
+
+  resource "pycparser" do
+    url "https://files.pythonhosted.org/packages/68/9e/49196946aee219aead1290e00d1e7fdeab8567783e83e1b9ab5585e6206a/pycparser-2.19.tar.gz"
+    sha256 "a988718abfad80b6b157acce7bf130a30876d27603738ac39f140993246b25b3"
+  end
+
+  resource "pygit2" do
+    url "https://files.pythonhosted.org/packages/cd/18/c674c9389aef8c940393e0507f9fe9437efd8e56ff1efa837d3fbfcd1d42/pygit2-1.0.3.tar.gz"
+    sha256 "de6c170dc09878e98424430ed6ba2934c01d811fedbc4cdfd71ec1dcd98487e2"
+  end
+
+  resource "Pygments" do
+    url "https://files.pythonhosted.org/packages/cb/9f/27d4844ac5bf158a33900dbad7985951e2910397998e85712da03ce125f0/Pygments-2.5.2.tar.gz"
+    sha256 "98c8aa5a9f778fcd1026a17361ddaf7330d1b7c62ae97c3bb0ae73e0b9b6b0fe"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "git", "init"
+    (testpath/"foo.txt").write "Hello"
+    system "git", "add", "foo.txt"
+    system "git", "commit", "-m", "foo"
+    system "git", "branch", "one"
+    (testpath/"bar.txt").write "World"
+    system "git", "add", "bar.txt"
+    system "git", "commit", "-m", "bar"
+    system "git", "branch", "two"
+    (testpath/"create_url.py").write <<~EOS
+      class CreateUrl:
+        @staticmethod
+        def result_url(sha1):
+          return ''
+        @staticmethod
+        def source_url(sha1):
+          return ''
+    EOS
+    assert_match "<!DOCTYPE html>",
+      shell_output("git literate-render test one two create_url.CreateUrl")
+  end
+end
