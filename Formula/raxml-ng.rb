80ad2470d765f04d06212059468e10517542a434
Jonathan Chang <jchang641@gmail.com>
2020-02-17 14:05:02 +1100

    raxml-ng 0.9.0 (new formula)
    Closes #50330.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

---
+++ Formula/raxml-ng.rb
@@ -0,0 +1,41 @@
+class RaxmlNg < Formula
+  desc "RAxML Next Generation: faster, easier-to-use and more flexible"
+  homepage "https://sco.h-its.org/exelixis/web/software/raxml/"
+  url "https://github.com/amkozlov/raxml-ng.git",
+    :tag      => "0.9.0",
+    :revision => "0a064e9a40f2e00828662795141659d946440c81"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "cmake" => :build
+  depends_on "libtool" => :build
+  depends_on "gmp"
+  depends_on "open-mpi"
+
+  uses_from_macos "bison" => :build
+  uses_from_macos "flex" => :build
+
+  resource "example" do
+    url "https://sco.h-its.org/exelixis/resource/download/hands-on/dna.phy"
+    sha256 "c2adc42823313831b97af76b3b1503b84573f10d9d0d563be5815cde0effe0c2"
+  end
+
+  def install
+    args = std_cmake_args + ["-DUSE_GMP=ON"]
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+    end
+    mkdir "build_mpi" do
+      ENV["CC"] = "mpicc"
+      ENV["CXX"] = "mpicxx"
+      system "cmake", "..", *args, "-DUSE_MPI=ON", "-DRAXML_BINARY_NAME=raxml-ng-mpi"
+      system "make", "install"
+    end
+  end
+
+  test do
+    testpath.install resource("example")
+    system "#{bin}/raxml-ng", "--msa", "dna.phy", "--start", "--model", "GTR"
+  end
+end
