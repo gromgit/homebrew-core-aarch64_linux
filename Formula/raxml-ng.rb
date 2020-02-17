749e1283a35efeade89a740980b0043a5787e434
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-17 03:37:25 +0000

    raxml-ng: add 0.9.0 bottle.

--- Formula/raxml-ng.rb
+++ Formula/raxml-ng.rb
@@ -5,6 +5,13 @@ class RaxmlNg < Formula
     :tag      => "0.9.0",
     :revision => "0a064e9a40f2e00828662795141659d946440c81"
 
+  bottle do
+    cellar :any
+    sha256 "3deb449ed9ce39343945ed4b003a8f0ed3caaf84cdd240e990f3a1262edaa1da" => :catalina
+    sha256 "5b99cd4e7bbc3b688dc32e73c728fc5ff9c1cd7f567e584523c418285de77cd0" => :mojave
+    sha256 "8e3e83381139bacde52257180a17a9928e6c9c0a833776b245925970570f1937" => :high_sierra
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "cmake" => :build
