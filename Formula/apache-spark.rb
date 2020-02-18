344b1ac08d1f61cb0728d692d0cc19f22121f447
Alexander Bayandin <a.bayandin@gmail.com>
2020-02-18 18:10:19 +0000

    apache-spark: add mirror

--- Formula/apache-spark.rb
+++ Formula/apache-spark.rb
@@ -2,6 +2,7 @@ class ApacheSpark < Formula
   desc "Engine for large-scale data processing"
   homepage "https://spark.apache.org/"
   url "https://www.apache.org/dyn/closer.lua?path=spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz"
+  mirror "https://archive.apache.org/dist/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz"
   version "2.4.5"
   sha256 "020be52524e4df366eb974d41a6e18fcb6efcaba9a51632169e917c74267dd81"
   head "https://github.com/apache/spark.git"
