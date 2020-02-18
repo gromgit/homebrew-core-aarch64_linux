97f950b0fdd30261b53ff6bd507ad15fae22c53d
Galder Zamarreño <galder@zamarreno.com>
2020-02-18 12:44:32 +0100

    openlibm 0.7.0 (new formula)
    Closes #50543.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

---
+++ Formula/openlibm.rb
@@ -0,0 +1,33 @@
+class Openlibm < Formula
+  desc "High quality, portable, open source libm implementation"
+  homepage "https://openlibm.org"
+  url "https://github.com/JuliaMath/openlibm/archive/v0.7.0.tar.gz"
+  sha256 "1699f773198018b55b12631db9c1801fe3ed191e618a1ee1be743f4570ae06a3"
+
+  keg_only :provided_by_macos
+
+  def install
+    lib.mkpath
+    (lib/"pkgconfig").mkpath
+    (include/"openlibm").mkpath
+
+    system "make", "install", "prefix=#{prefix}"
+
+    lib.install Dir["lib/*"].reject { |f| File.directory? f }
+    (lib/"pkgconfig").install Dir["lib/pkgconfig/*"]
+    (include/"openlibm").install Dir["include/openlibm/*"]
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include "openlibm.h"
+      int main (void) {
+        printf("%.1f", cos(acos(0.0)));
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-I#{include}/openlibm",
+           "-o", "test"
+    assert_equal "0.0", shell_output("./test")
+  end
+end
