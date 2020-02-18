01c95ecd3540424d178d45fe1054f64d3bbbf74c
Alexander Bayandin <a.bayandin@gmail.com>
2020-02-18 18:10:22 +0000

    tika: add mirror

--- Formula/tika.rb
+++ Formula/tika.rb
@@ -2,6 +2,7 @@ class Tika < Formula
   desc "Content analysis toolkit"
   homepage "https://tika.apache.org/"
   url "https://www.apache.org/dyn/closer.cgi?path=tika/tika-app-1.23.jar"
+  mirror "https://archive.apache.org/dist/tika/tika-app-1.23.jar"
   sha256 "0c382d300442c3c2b84042e2c5b5cf2287583d4487c9b5c78eec58a625b54ae3"
 
   bottle :unneeded
