9746815f402adb1166b26da990bde8d34f169e4c
EricFromCanada <enk3@outlook.com>
2020-02-18 11:53:34 -0500

    readline 8.0.4

--- Formula/readline.rb
+++ Formula/readline.rb
@@ -3,11 +3,14 @@ class Readline < Formula
   homepage "https://tiswww.case.edu/php/chet/readline/rltop.html"
   url "https://ftp.gnu.org/gnu/readline/readline-8.0.tar.gz"
   mirror "https://ftpmirror.gnu.org/readline/readline-8.0.tar.gz"
-  version "8.0.1"
+  version "8.0.4"
   sha256 "e339f51971478d369f8a053a330a190781acb9864cf4c541060f12078948e461"
 
   %w[
     001 d8e5e98933cf5756f862243c0601cb69d3667bb33f2c7b751fe4e40b2c3fd069
+    002 36b0febff1e560091ae7476026921f31b6d1dd4c918dcb7b741aa2dad1aec8f7
+    003 94ddb2210b71eb5389c7756865d60e343666dfb722c85892f8226b26bb3eeaef
+    004 b1aa3d2a40eee2dea9708229740742e649c32bb8db13535ea78f8ac15377394c
   ].each_slice(2) do |p, checksum|
     patch :p0 do
       url "https://ftp.gnu.org/gnu/readline/readline-8.0-patches/readline80-#{p}"
@@ -33,8 +36,8 @@ class Readline < Formula
   def install
     system "./configure", "--prefix=#{prefix}"
     # There is no termcap.pc in the base system, so we have to comment out
-    # the corresponding Requires.private line otherwise pkg-config will
-    # consider the readline module unusable
+    # the corresponding Requires.private line.
+    # Otherwise, pkg-config will consider the readline module unusable.
     inreplace "readline.pc", /^(Requires.private: .*)$/, "# \\1"
     system "make", "install"
   end
