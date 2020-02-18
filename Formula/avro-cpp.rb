b7274e8835b2a2d902dd963049cdb2132f8e3609
Alexander Bayandin <a.bayandin@gmail.com>
2020-02-18 18:10:20 +0000

    avro-cpp: add mirror

--- Formula/avro-cpp.rb
+++ Formula/avro-cpp.rb
@@ -2,6 +2,7 @@ class AvroCpp < Formula
   desc "Data serialization system"
   homepage "https://avro.apache.org/"
   url "https://www.apache.org/dyn/closer.cgi?path=avro/avro-1.9.2/cpp/avro-cpp-1.9.2.tar.gz"
+  mirror "https://archive.apache.org/dist/avro/avro-1.9.2/cpp/avro-cpp-1.9.2.tar.gz"
   sha256 "bd50f380ed4f77231721ee2347dd0ca3991f6fec5254acdf7e5f105253deba6b"
 
   bottle do
