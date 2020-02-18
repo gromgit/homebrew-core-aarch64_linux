6f2c7920fc20acd98dde01521fd1af914d120f5a
Alexander Bayandin <a.bayandin@gmail.com>
2020-02-18 18:10:20 +0000

    apr-util: add mirror

--- Formula/apr-util.rb
+++ Formula/apr-util.rb
@@ -2,6 +2,7 @@ class AprUtil < Formula
   desc "Companion library to apr, the Apache Portable Runtime library"
   homepage "https://apr.apache.org/"
   url "https://www.apache.org/dyn/closer.cgi?path=apr/apr-util-1.6.1.tar.bz2"
+  mirror "https://archive.apache.org/dist/apr/apr-util-1.6.1.tar.bz2"
   sha256 "d3e12f7b6ad12687572a3a39475545a072608f4ba03a6ce8a3778f607dd0035b"
   revision 3
 
