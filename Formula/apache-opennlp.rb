76e33f869efe01b30fec0779e1c1c9ae1d21350e
Alexander Bayandin <a.bayandin@gmail.com>
2020-02-18 18:10:19 +0000

    apache-opennlp: add mirror

--- Formula/apache-opennlp.rb
+++ Formula/apache-opennlp.rb
@@ -2,6 +2,7 @@ class ApacheOpennlp < Formula
   desc "Machine learning toolkit for processing natural language text"
   homepage "https://opennlp.apache.org/"
   url "https://www.apache.org/dyn/closer.cgi?path=opennlp/opennlp-1.9.2/apache-opennlp-1.9.2-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/opennlp/opennlp-1.9.2/apache-opennlp-1.9.2-bin.tar.gz"
   sha256 "26b55416a6c330e9c91bf9ad31183f3ed3104643b3d74ad2ee6e16b0c0e44f3b"
 
   bottle :unneeded
