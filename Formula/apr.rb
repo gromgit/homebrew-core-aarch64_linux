68d3c0bfd676d6bc2c92d19311b074a27d1f7fed
Alexander Bayandin <a.bayandin@gmail.com>
2020-02-18 18:10:20 +0000

    apr: add mirror

--- Formula/apr.rb
+++ Formula/apr.rb
@@ -2,6 +2,7 @@ class Apr < Formula
   desc "Apache Portable Runtime library"
   homepage "https://apr.apache.org/"
   url "https://www.apache.org/dyn/closer.cgi?path=apr/apr-1.7.0.tar.bz2"
+  mirror "https://archive.apache.org/dist/apr/apr-1.7.0.tar.bz2"
   sha256 "e2e148f0b2e99b8e5c6caa09f6d4fb4dd3e83f744aa72a952f94f5a14436f7ea"
 
   bottle do
