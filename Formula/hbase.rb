d88c021533def7ead35d605332b526273ac9e4e8
Alexander Bayandin <a.bayandin@gmail.com>
2020-02-18 18:10:20 +0000

    hbase: add mirror

--- Formula/hbase.rb
+++ Formula/hbase.rb
@@ -2,6 +2,7 @@ class Hbase < Formula
   desc "Hadoop database: a distributed, scalable, big data store"
   homepage "https://hbase.apache.org"
   url "https://www.apache.org/dyn/closer.cgi?path=hbase/1.3.5/hbase-1.3.5-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/hbase/1.3.5/hbase-1.3.5-bin.tar.gz"
   sha256 "71837369f67c98afd978256ae4012b774fed27dcfbefc30293311e534a376c93"
 
   bottle do
