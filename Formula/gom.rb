3369b5013c71a0dfc7123e792c90c3d7d3ffcc93
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2020-02-19 13:15:33 +0000

    gom 0.4
    Closes #50438.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/gom.rb
+++ Formula/gom.rb
@@ -1,9 +1,8 @@
 class Gom < Formula
   desc "GObject wrapper around SQLite"
   homepage "https://wiki.gnome.org/Projects/Gom"
-  url "https://download.gnome.org/sources/gom/0.3/gom-0.3.3.tar.xz"
-  sha256 "ac57e34b5fe273ed306efaeabb346712c264e341502913044a782cdf8c1036d8"
-  revision 6
+  url "https://download.gnome.org/sources/gom/0.4/gom-0.4.tar.xz"
+  sha256 "68d08006aaa3b58169ce7cf1839498f45686fba8115f09acecb89d77e1018a9d"
 
   bottle do
     sha256 "9ac0fa6a00e863a79a22304e11ce8acdc506cc7115efde0f139400548e4084ba" => :catalina
@@ -13,7 +12,7 @@ class Gom < Formula
   end
 
   depends_on "gobject-introspection" => :build
-  depends_on "meson-internal" => :build
+  depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "python" => :build
@@ -21,20 +20,12 @@ class Gom < Formula
   depends_on "gettext"
   depends_on "glib"
 
-  patch :DATA
-
   def install
-    ENV.refurbish_args
-
     pyver = Language::Python.major_minor_version "python3"
 
-    # prevent sandbox violation
-    inreplace "bindings/python/meson.build",
-              "install_dir: pygobject_override_dir",
-              "install_dir: '#{lib}/python#{pyver}/site-packages'"
-
     mkdir "build" do
-      system "meson", "--prefix=#{prefix}", ".."
+      system "meson", "--prefix=#{prefix}",
+        "-Dpygobject-override-dir=#{lib}/python#{pyver}/site-packages", ".."
       system "ninja"
       system "ninja", "install"
     end
@@ -68,44 +59,3 @@ class Gom < Formula
     system "./test"
   end
 end
-
-__END__
-diff --git a/bindings/python/meson.build b/bindings/python/meson.build
-index feb4a9c..2fda8c1 100644
---- a/bindings/python/meson.build
-+++ b/bindings/python/meson.build
-@@ -1,33 +1 @@
--python3 = import('python3').find_python()
--
--get_overridedir = '''
--import os
--import sysconfig
--
--libdir = sysconfig.get_config_var('LIBDIR')
--
--if not libdir:
--  libdir = '/usr/lib'
--
--try:
--  import gi
--  overridedir = gi._overridesdir
--except ImportError:
--  purelibdir = sysconfig.get_path('purelib')
--  overridedir = os.path.join(purelibdir, 'gi', 'overrides')
--
--if overridedir.startswith(libdir): # Should always be True..
--  overridedir = overridedir[len(libdir) + 1:]
--
--print(overridedir)
--'''
--
--ret = run_command([python3, '-c', get_overridedir])
--
--if ret.returncode() != 0
--  error('Failed to determine pygobject overridedir')
--else
--  pygobject_override_dir = join_paths(get_option('libdir'), ret.stdout().strip())
--endif
--
- install_data('gi/overrides/Gom.py', install_dir: pygobject_override_dir)
-
