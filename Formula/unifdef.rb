67f5e8aeaf8c01573fb8aa207ebda4fe7a6bfc65
EricFromCanada <enk3@outlook.com>
2020-02-19 23:42:46 -0500

    unifdef 2.12
    Closes #50466.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/unifdef.rb
+++ Formula/unifdef.rb
@@ -1,8 +1,8 @@
 class Unifdef < Formula
   desc "Selectively process conditional C preprocessor directives"
   homepage "https://dotat.at/prog/unifdef/"
-  url "https://dotat.at/prog/unifdef/unifdef-2.11.tar.gz"
-  sha256 "e8483c05857a10cf2d5e45b9e8af867d95991fab0f9d3d8984840b810e132d98"
+  url "https://dotat.at/prog/unifdef/unifdef-2.12.tar.gz"
+  sha256 "fba564a24db7b97ebe9329713ac970627b902e5e9e8b14e19e024eb6e278d10b"
   head "https://github.com/fanf2/unifdef.git"
 
   bottle do
