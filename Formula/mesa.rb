d6a093fd0c5bdc690c2d7738456e6645b7cc7190
Yurii Kolesnykov <root@yurikoles.com>
2020-02-20 10:07:27 +0200

    mesa 20.0.0
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>
    
    Closes #50469.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/mesa.rb
+++ Formula/mesa.rb
@@ -2,9 +2,9 @@ class Mesa < Formula
   include Language::Python::Virtualenv
   desc "Graphics Library"
   homepage "https://www.mesa3d.org/"
-  url "https://mesa.freedesktop.org/archive/mesa-19.3.4.tar.xz"
-  mirror "https://www.mesa3d.org/archive/mesa-19.3.4.tar.xz"
-  sha256 "1da467e6ae2799a517e242462331eafd29ae77d9872f3a845df81f7c308e8fe4"
+  url "https://mesa.freedesktop.org/archive/mesa-20.0.0.tar.xz"
+  mirror "https://www.mesa3d.org/archive/mesa-20.0.0.tar.xz"
+  sha256 "bb6db3e54b608d2536d4000b3de7dd3ae115fc114e8acbb5afff4b3bbed04b34"
   head "https://gitlab.freedesktop.org/mesa/mesa.git"
 
   bottle do
