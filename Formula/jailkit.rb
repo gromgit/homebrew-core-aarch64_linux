cc2c8c4be079ad107021b7548f1795ec6f549c67
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-20 09:32:25 +0000

    jailkit: update 2.21 bottle.

--- Formula/jailkit.rb
+++ Formula/jailkit.rb
@@ -5,9 +5,10 @@ class Jailkit < Formula
   sha256 "db3bb090a4fffdef59b5eafd594478d576cacf84306f9929d0dfbed090cf3687"
 
   bottle do
-    sha256 "b0980932e53c18886c0106fd50a6101fc77754fcfb536bf218fd6845f7e91309" => :catalina
-    sha256 "1aae3a9499ec0553b45b48003e43c55e1facd7b56d94cc4ff9cb639496b7fa05" => :mojave
-    sha256 "25e55b2cc8572ad043c97ad2b86e08f93ef1a69c6fb66d1bf1630c4c5092bdfc" => :high_sierra
+    rebuild 1
+    sha256 "1bdfd57d829a6b90286601b2b13660b53cc14fe6443d0d23fa15636171c79439" => :catalina
+    sha256 "43811f3b792ddd4039767cf81882e0c46c2851ba95546c01d7c1a052c3e99f3e" => :mojave
+    sha256 "dd348dd5721fc813bb1556f13b196b684f5ef805f3c03c2bab1e3df4eef41376" => :high_sierra
   end
 
   depends_on "python@3.8"
