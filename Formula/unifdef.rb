f814ef11bbbe20c901f6a09f6ff8291542c3682e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-20 07:02:14 +0000

    unifdef: update 2.12 bottle.

--- Formula/unifdef.rb
+++ Formula/unifdef.rb
@@ -7,13 +7,9 @@ class Unifdef < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "aa00861707fcd190d148f1adbdb2de40c87c8d76fffc14943546b7cfe95e4645" => :catalina
-    sha256 "a4f1251e49839d04791e90a1a6c72d72679ebad399ca8cc184aae7f729ce518e" => :mojave
-    sha256 "a7e42f5a8e2c467e84dbf9edc4f14cad362850e324088844d6b11f1cae88ce9f" => :high_sierra
-    sha256 "be948ffc760273a09943faa7c156e4014534da82c78bc2c3c4f2a4a4416e962e" => :sierra
-    sha256 "a89e5cc9b179fa5135077ad0c27e34cebe13a33dc02adccab4969855ba173357" => :el_capitan
-    sha256 "3277bf0977c385e3cf5ccf3355e11f30b057c58d8539a27d5a23531ce7de9542" => :yosemite
-    sha256 "8533d1ce7b70e51256a3c24d557345b7b7ac0b7e6562de5a0f942c058ae518db" => :mavericks
+    sha256 "ae908aa0c1845ce059576df3922808db790fb0ea91109f89aa930c8da7a68904" => :catalina
+    sha256 "ded3ae5ba762ee493fc5ed45d0e6ed5a8261b4bf9bc2de54880f4a373b1ba075" => :mojave
+    sha256 "74ec90a8fc2467e8d613f2a6347d973dcd6387fdd9734bc66088a64ace7e0d4d" => :high_sierra
   end
 
   keg_only :provided_by_macos, "the unifdef provided by Xcode cannot compile gevent"
