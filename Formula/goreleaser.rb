660501246fa723d5bb0f8dd7cbabab0c9b6f6126
Rui Chen <chenrui333@gmail.com>
2020-02-19 21:14:22 -0500

    goreleaser 0.127.0
    Closes #50462.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/goreleaser.rb
+++ Formula/goreleaser.rb
@@ -2,8 +2,8 @@ class Goreleaser < Formula
   desc "Deliver Go binaries as fast and easily as possible"
   homepage "https://goreleaser.com/"
   url "https://github.com/goreleaser/goreleaser.git",
-      :tag      => "v0.125.0",
-      :revision => "a4c91105b54583ce1450876b493f9700b1d4b712"
+      :tag      => "v0.127.0",
+      :revision => "33d39188fa74ff03bb134b092386c9525da6b53a"
 
   bottle do
     cellar :any_skip_relocation
