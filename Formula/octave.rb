c97aae13dc899b7ca9226a10bf095a9652631236
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-21 00:22:55 +0000

    octave: update 5.2.0_1 bottle.

--- Formula/octave.rb
+++ Formula/octave.rb
@@ -7,9 +7,9 @@ class Octave < Formula
   revision 1
 
   bottle do
-    sha256 "b10ad92fdb35e57c87e00440e1df4674b91ab61a2d55046ab2e3b9d16264edb1" => :catalina
-    sha256 "d5e20c1cdbe7b71e03e24936acb35158ce38ec33a92fa64e6f5ffe5b5f193165" => :mojave
-    sha256 "9f62aad44ae84a39f00bfadc6039e7e17303800a99458cf37bdee3044c4add4d" => :high_sierra
+    sha256 "e9710c9a9d5c84c708017d839baeebe6324c09d07828f4b6c074fcdca302b7c7" => :catalina
+    sha256 "3e99b794c40e48428e3c93258542e4f1e169a90db8e02831ac585475fa09dc60" => :mojave
+    sha256 "04738b5855d6d037e416e2a6a594efcc0567a01b7866c80cb470a3f867f78405" => :high_sierra
   end
 
   head do
