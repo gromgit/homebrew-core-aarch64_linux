ed6c40de2f3dad6059a87992068f22e15f50a73b
Alexander Bayandin <a.bayandin@gmail.com>
2020-02-21 18:16:30 +0000

    apache-forrest: fix url style

--- Formula/apache-forrest.rb
+++ Formula/apache-forrest.rb
@@ -1,7 +1,7 @@
 class ApacheForrest < Formula
   desc "Publishing framework providing multiple output formats"
   homepage "https://forrest.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=forrest/apache-forrest-0.9-sources.tar.gz"
+  url "https://www.apache.org/dyn/closer.lua?path=forrest/apache-forrest-0.9-sources.tar.gz"
   mirror "https://archive.apache.org/dist/forrest/apache-forrest-0.9-sources.tar.gz"
   sha256 "c6ac758db2eb0d4d91bd1733bbbc2dec4fdb33603895c464bcb47a34490fb64d"
   revision 1
@@ -20,7 +20,7 @@ class ApacheForrest < Formula
   depends_on "openjdk"
 
   resource "deps" do
-    url "https://www.apache.org/dyn/closer.cgi?path=forrest/apache-forrest-0.9-dependencies.tar.gz"
+    url "https://www.apache.org/dyn/closer.lua?path=forrest/apache-forrest-0.9-dependencies.tar.gz"
     sha256 "33146b4e64933691d3b779421b35da08062a704618518d561281d3b43917ccf1"
   end
 
