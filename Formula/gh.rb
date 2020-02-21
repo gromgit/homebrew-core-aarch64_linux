fea72f82a11e98e4664e7ab971691f0f72446910
Dawid Dziurla <dawidd0811@gmail.com>
2020-02-21 18:08:53 +0100

    gh: restore
    This reverts commit 838f84f7e4412074caafcb8053116b6729f7850e.
    
    Closes #50541.
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

---
+++ Formula/gh.rb
@@ -0,0 +1,27 @@
+class Gh < Formula
+  desc "GitHub command-line tool"
+  homepage "https://github.com/cli/cli"
+  url "https://github.com/cli/cli/archive/v0.5.7.tar.gz"
+  sha256 "a1185de36b27bb9831a172b70e60f741aeaa774682f3ac4bccb9df45734f8b5b"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -X github.com/cli/cli/command.Version=#{version}
+      -X github.com/cli/cli/command.BuildDate=#{Date.today}
+      -s -w
+    ]
+    system "go", "build", "-trimpath", "-ldflags", ldflags.join(" "), "-o", bin/name, "./cmd/gh"
+
+    (bash_completion/"gh").write `#{bin}/gh completion -s bash`
+    (fish_completion/"gh.fish").write `#{bin}/gh completion -s fish`
+    (zsh_completion/"_gh").write `#{bin}/gh completion -s zsh`
+  end
+
+  test do
+    assert_match "gh version #{version}", shell_output("#{bin}/gh --version")
+    assert_match "Work with GitHub issues.", shell_output("#{bin}/gh issue 2>&1", 1)
+    assert_match "Work with GitHub pull requests.", shell_output("#{bin}/gh pr 2>&1", 1)
+  end
+end
