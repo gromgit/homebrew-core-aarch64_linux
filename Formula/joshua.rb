679593da50245453b8c5ce69f4e3e4ee472a3b26
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-21 00:22:56 +0000

    joshua: update 6.0.5_1 bottle.

--- Formula/joshua.rb
+++ Formula/joshua.rb
@@ -7,11 +7,9 @@ class Joshua < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 2
-    sha256 "724029ff91e387fcf45de137023ef20f132a8a3f6dc6a6627c48f3cff339e0ea" => :catalina
-    sha256 "17fe13d1fe356578c025aa681f16b6d5f929a986a6f102811332a75bbfdf3d64" => :mojave
-    sha256 "17fe13d1fe356578c025aa681f16b6d5f929a986a6f102811332a75bbfdf3d64" => :high_sierra
-    sha256 "343ffcd545e812b27b73807070f778e311d62011351be532de355fb85a8e7ed4" => :sierra
+    sha256 "126f37758cb9f1ace827883911906cab4976bf5f211b200ed0e2f307fae87982" => :catalina
+    sha256 "126f37758cb9f1ace827883911906cab4976bf5f211b200ed0e2f307fae87982" => :mojave
+    sha256 "126f37758cb9f1ace827883911906cab4976bf5f211b200ed0e2f307fae87982" => :high_sierra
   end
 
   depends_on "openjdk"
