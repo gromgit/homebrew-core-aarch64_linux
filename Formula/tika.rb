653e73aa7c468897293ad5cd7fadfdad69dd63dd
Alexander Bayandin <a.bayandin@gmail.com>
2020-02-21 18:16:33 +0000

    tika: fix url style

--- Formula/tika.rb
+++ Formula/tika.rb
@@ -1,7 +1,7 @@
 class Tika < Formula
   desc "Content analysis toolkit"
   homepage "https://tika.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=tika/tika-app-1.23.jar"
+  url "https://www.apache.org/dyn/closer.lua?path=tika/tika-app-1.23.jar"
   mirror "https://archive.apache.org/dist/tika/tika-app-1.23.jar"
   sha256 "0c382d300442c3c2b84042e2c5b5cf2287583d4487c9b5c78eec58a625b54ae3"
   revision 1
@@ -11,7 +11,7 @@ class Tika < Formula
   depends_on "openjdk"
 
   resource "server" do
-    url "https://www.apache.org/dyn/closer.cgi?path=tika/tika-server-1.23.jar"
+    url "https://www.apache.org/dyn/closer.lua?path=tika/tika-server-1.23.jar"
     sha256 "9e95c7dada0f8ca81bf1b69192258a82890b645d389659330356fa3427005b6f"
   end
 
