358565f1bc88ae372bc7bf8aae0ff1a524373d88
Alexander Bayandin <a.bayandin@gmail.com>
2020-02-21 18:16:31 +0000

    flume: fix url style

--- Formula/flume.rb
+++ Formula/flume.rb
@@ -1,7 +1,7 @@
 class Flume < Formula
   desc "Hadoop-based distributed log collection and aggregation"
   homepage "https://flume.apache.org"
-  url "https://www.apache.org/dyn/closer.cgi?path=flume/1.9.0/apache-flume-1.9.0-bin.tar.gz"
+  url "https://www.apache.org/dyn/closer.lua?path=flume/1.9.0/apache-flume-1.9.0-bin.tar.gz"
   mirror "https://archive.apache.org/dist/flume/1.9.0/apache-flume-1.9.0-bin.tar.gz"
   sha256 "0373ed5abfd44dc4ab23d9a02251ffd7e3b32c02d83a03546e97ec15a7b23619"
   revision 1
