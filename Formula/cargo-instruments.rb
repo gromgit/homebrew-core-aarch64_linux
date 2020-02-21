c4cb092382992dea003d38487eb92501079b6a86
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-21 11:57:29 +0000

    cargo-instruments: add 0.3.0 bottle.

--- Formula/cargo-instruments.rb
+++ Formula/cargo-instruments.rb
@@ -4,6 +4,13 @@ class CargoInstruments < Formula
   url "https://github.com/cmyr/cargo-instruments/archive/v0.3.0.tar.gz"
   sha256 "aa511fa2c9c6cdd342c333a7e6d1eb37644c53d2f5c6667510a24c0268b3d659"
 
+  bottle do
+    cellar :any
+    sha256 "6f6f7b4d9d88a5c348d951c3b3d892bc8590e2eca6a3e5da6acd029509864cfe" => :catalina
+    sha256 "bddab3f8c96413ad04f2bfc000f8b3228837d10c70a61924efa21cd723d2153e" => :mojave
+    sha256 "9e2ea11924cc755373b8ac2c0b110b67930552009c0430a6a1d47729235b1a65" => :high_sierra
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@1.1"
 
