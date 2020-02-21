2a3eb04c04d2179c5d8072209568ffaf983a3515
Alexander Bayandin <a.bayandin@gmail.com>
2020-02-21 18:16:32 +0000

    maven@3.2: fix url style

--- Formula/maven@3.2.rb
+++ Formula/maven@3.2.rb
@@ -1,7 +1,7 @@
 class MavenAT32 < Formula
   desc "Java-based project management"
   homepage "https://maven.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz"
+  url "https://www.apache.org/dyn/closer.lua?path=maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz"
   mirror "https://archive.apache.org/dist/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz"
   sha256 "8c190264bdf591ff9f1268dc0ad940a2726f9e958e367716a09b8aaa7e74a755"
   revision 1
