afc8fe5a3dc03d376df9a9b3650e969cb46cd448
Alexander Bayandin <a.bayandin@gmail.com>
2020-02-21 18:16:31 +0000

    ivy: fix url style

--- Formula/ivy.rb
+++ Formula/ivy.rb
@@ -1,7 +1,7 @@
 class Ivy < Formula
   desc "Agile dependency manager"
   homepage "https://ant.apache.org/ivy/"
-  url "https://www.apache.org/dyn/closer.cgi?path=ant/ivy/2.5.0/apache-ivy-2.5.0-bin.tar.gz"
+  url "https://www.apache.org/dyn/closer.lua?path=ant/ivy/2.5.0/apache-ivy-2.5.0-bin.tar.gz"
   mirror "https://archive.apache.org/dist/ant/ivy/2.5.0/apache-ivy-2.5.0-bin.tar.gz"
   sha256 "3855a5769b5dbeafa9fb6a00f130467fd0f89da684a0b33a91e3dc5dae2715c7"
 
