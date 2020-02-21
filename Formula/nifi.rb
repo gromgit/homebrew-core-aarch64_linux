57804367b913b5a9148a65eb056735a57f43358d
Aldrin Piri <aldrinpiri@gmail.com>
2020-02-21 10:38:57 -0500

    Update nifi.rb to restore mirror
    Closes #50533.
    
    Signed-off-by: Izaak Beekman <zbeekman@gmail.com>

--- Formula/nifi.rb
+++ Formula/nifi.rb
@@ -2,6 +2,7 @@ class Nifi < Formula
   desc "Easy to use, powerful, and reliable system to process and distribute data"
   homepage "https://nifi.apache.org"
   url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.11.2/nifi-1.11.2-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/nifi/1.11.2/nifi-1.11.2-bin.tar.gz"
   sha256 "950a58c9aca9c1f3594c21d01e76e63d538471e44d2291ce5bd01989d3a8ee27"
 
   bottle :unneeded
