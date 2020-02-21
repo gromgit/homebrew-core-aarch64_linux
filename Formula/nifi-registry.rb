7536f430e720ce32abf6eaf4b674eae1ffd5bd4a
Alexander Bayandin <a.bayandin@gmail.com>
2020-02-21 18:16:32 +0000

    nifi-registry: fix url style

--- Formula/nifi-registry.rb
+++ Formula/nifi-registry.rb
@@ -1,7 +1,7 @@
 class NifiRegistry < Formula
   desc "Centralized storage & management of NiFi/MiNiFi shared resources"
   homepage "https://nifi.apache.org/registry"
-  url "https://www.apache.org/dyn/closer.lua?path=/nifi/nifi-registry/nifi-registry-0.5.0/nifi-registry-0.5.0-bin.tar.gz"
+  url "https://www.apache.org/dyn/closer.lua?path=nifi/nifi-registry/nifi-registry-0.5.0/nifi-registry-0.5.0-bin.tar.gz"
   mirror "https://archive.apache.org/dist/nifi/nifi-registry/nifi-registry-0.5.0/nifi-registry-0.5.0-bin.tar.gz"
   sha256 "6bf16eb1e73709b2723aaeccd7dddc08ff64ce7f46ef4ed2b0a36e24773b7f64"
 
