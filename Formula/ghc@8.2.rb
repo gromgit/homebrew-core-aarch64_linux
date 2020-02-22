c094fb3abf3c1d96176d36cb6969be4ac946318d
Seeker <meaningseeking@protonmail.com>
2020-02-22 07:32:56 -0800

    ghc@8.2: Fix for catalina & remove make check

--- Formula/ghc@8.2.rb
+++ Formula/ghc@8.2.rb
@@ -7,9 +7,9 @@ class GhcAT82 < Formula
   homepage "https://haskell.org/ghc/"
   url "https://downloads.haskell.org/~ghc/8.2.2/ghc-8.2.2-src.tar.xz"
   sha256 "bb8ec3634aa132d09faa270bbd604b82dfa61f04855655af6f9d14a9eedc05fc"
+  revision 1
 
   bottle do
-    rebuild 1
     sha256 "e8b7dbca371f1c62de38dd06dcd9e90d638acf2e2f6b61b087a0eb043ebbbfd7" => :mojave
     sha256 "b1f1d079df456ce6297160e75766cd8016737bfaefee52cd29eeabddc302d4fd" => :high_sierra
     sha256 "a3a80024bb1535107f78646d0d806efb1a794d132e52aa5a39e47fac24d5c275" => :sierra
@@ -39,6 +39,9 @@ class GhcAT82 < Formula
     sha256 "927ff939f46a0f79aa87e16e56e0a024a288c78259bed874cb15aa96a653566c"
   end
 
+  # Fix for Catalina compatibility https://gitlab.haskell.org/ghc/ghc/issues/17353
+  patch :DATA
+
   def install
     ENV["CC"] = ENV.cc
     ENV["LD"] = "ld"
@@ -52,7 +55,6 @@ class GhcAT82 < Formula
       system "./configure", "--prefix=#{gmp}", "--with-pic", "--disable-shared",
                             "--build=#{Hardware.oldest_cpu}-apple-darwin#{`uname -r`.to_i}"
       system "make"
-      system "make", "check"
       ENV.deparallelize { system "make", "install" }
     end
 
@@ -105,3 +107,25 @@ class GhcAT82 < Formula
     system "#{bin}/runghc", testpath/"hello.hs"
   end
 end
+__END__
+diff -pur a/rts/Linker.c b/rts/Linker.c
+--- a/rts/Linker.c	2019-08-25 21:03:36.000000000 +0900
++++ b/rts/Linker.c	2019-11-05 11:09:06.000000000 +0900
+@@ -192,7 +192,7 @@ int ocTryLoad( ObjectCode* oc );
+  *
+  * MAP_32BIT not available on OpenBSD/amd64
+  */
+-#if defined(x86_64_HOST_ARCH) && defined(MAP_32BIT)
++#if defined(x86_64_HOST_ARCH) && defined(MAP_32BIT) && !defined(__APPLE__)
+ #define TRY_MAP_32BIT MAP_32BIT
+ #else
+ #define TRY_MAP_32BIT 0
+@@ -214,7 +214,7 @@ int ocTryLoad( ObjectCode* oc );
+  */
+ #if !defined(ALWAYS_PIC) && defined(x86_64_HOST_ARCH)
+
+-#if defined(MAP_32BIT)
++#if defined(MAP_32BIT) && !defined(__APPLE__)
+ // Try to use MAP_32BIT
+ #define MMAP_32BIT_BASE_DEFAULT 0
+ #else
