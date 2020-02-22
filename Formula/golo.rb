8b3485ad5f40c491b7fbb5d57ba60861d3d5dc12
Markus Reiter <me@reitermark.us>
2020-02-22 06:33:29 +0100

    golo: depend on openjdk@11

--- Formula/golo.rb
+++ Formula/golo.rb
@@ -3,15 +3,15 @@ class Golo < Formula
   homepage "https://golo-lang.org/"
   url "https://bintray.com/artifact/download/golo-lang/downloads/golo-3.3.0.zip"
   sha256 "a9ff036518aee4280102b2d5b79752d8378857fd9714e5b701999a6c93b89316"
-  revision 1
+  revision 2
   head "https://github.com/eclipse/golo-lang.git"
 
   bottle :unneeded
 
-  depends_on "openjdk@12"
+  depends_on "openjdk@11"
 
   def install
-    ENV["JAVA_HOME"] = Formula["openjdk@12"].opt_prefix
+    ENV["JAVA_HOME"] = Formula["openjdk@11"].opt_prefix
     if build.head?
       system "./gradlew", "installDist"
       libexec.install %w[build/install/golo/bin build/install/golo/docs build/install/golo/lib]
