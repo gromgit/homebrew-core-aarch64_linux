4c66c41061baca0ebf2818b612ad9a6f0ca188e2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-23 12:14:13 +0000

    cassandra-cpp-driver: add 2.15.0 bottle.

--- Formula/cassandra-cpp-driver.rb
+++ Formula/cassandra-cpp-driver.rb
@@ -5,6 +5,13 @@ class CassandraCppDriver < Formula
   sha256 "8cccffc7f3b7a8edc51c5c4344fd6a60d5a7beec27a1a3fd52742003a2675cb9"
   head "https://github.com/datastax/cpp-driver.git"
 
+  bottle do
+    cellar :any
+    sha256 "87a85e45ad3f626199da825b1479f1924627560f40cd9d19a79e72f2f2d27035" => :catalina
+    sha256 "1b557a5511b6b6cfe63e014c926f410a93c19ab40495f9cfa3cd8d2386b90168" => :mojave
+    sha256 "c68d0fefa318bd3409d065bee3ad7b6bb75d86a2f4239e4cebef0f402e8b88e8" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "libuv"
   depends_on "openssl@1.1"
