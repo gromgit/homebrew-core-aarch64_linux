1ab29d5205afcae4f6b3b827aa5f4f6d30088bac
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-23 05:18:22 +0000

    wapm: add 0.4.3 bottle.

--- Formula/wapm.rb
+++ Formula/wapm.rb
@@ -5,6 +5,13 @@ class Wapm < Formula
   sha256 "0a4057217a539a013549fd2bf3913bff28f0ec01d6606ebc278c2057c6e268a2"
   head "https://github.com/wasmerio/wapm-cli.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "15d984bd969f4d207549ae6c51f2ba34d945c4b18f8acf0ba0c001dc54fb9039" => :catalina
+    sha256 "b5be8c6e0592ef6a7133afbb6e9bbb97e62d7eadc36a2e97b505179dd6c35f85" => :mojave
+    sha256 "b0228e63f09a647ebf5942adbfe32e82323eb9c20eb04d333f01ed7f18ffbe28" => :high_sierra
+  end
+
   depends_on "rust" => :build
   depends_on "wasmer" => :test
 
