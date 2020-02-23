3ab8a94eb7a173cd2472d8087f7d52804bd64af7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-23 17:01:33 +0000

    git-fixup: update 1.3.0 bottle.

--- Formula/git-fixup.rb
+++ Formula/git-fixup.rb
@@ -7,10 +7,9 @@ class GitFixup < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "72ff69fa75efeb1f6a5c5384d8084e65b67c1bd0cdf9668d402ab4271ce235f5" => :catalina
-    sha256 "0fc85e047798c8602c8ad69796c6b6ac4d9759852bdba1b189aa0e999dfb53a2" => :mojave
-    sha256 "0fc85e047798c8602c8ad69796c6b6ac4d9759852bdba1b189aa0e999dfb53a2" => :high_sierra
-    sha256 "30b0473a2e18df5f4d91f61d74babf21fd5e8eccf9499d402f61790643400af7" => :sierra
+    sha256 "adb91d6c86a8330a51394a03e20b5d7dc20144ee80426802b1b651766fc2462e" => :catalina
+    sha256 "adb91d6c86a8330a51394a03e20b5d7dc20144ee80426802b1b651766fc2462e" => :mojave
+    sha256 "adb91d6c86a8330a51394a03e20b5d7dc20144ee80426802b1b651766fc2462e" => :high_sierra
   end
 
   def install
