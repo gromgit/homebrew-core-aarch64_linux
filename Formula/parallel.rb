1cfb287d2e49269bbfc5e8de4938be0795e8980e
EricFromCanada <enk3@outlook.com>
2020-02-24 08:51:51 -0500

    parallel 20200222
    Closes #50645.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/parallel.rb
+++ Formula/parallel.rb
@@ -1,9 +1,9 @@
 class Parallel < Formula
   desc "Shell command parallelization utility"
   homepage "https://savannah.gnu.org/projects/parallel/"
-  url "https://ftp.gnu.org/gnu/parallel/parallel-20200122.tar.bz2"
-  mirror "https://ftpmirror.gnu.org/parallel/parallel-20200122.tar.bz2"
-  sha256 "116f1e428da5cdb26cda3ee5f249bdb20c5f96f139c0a284ad142919d6d80c1c"
+  url "https://ftp.gnu.org/gnu/parallel/parallel-20200222.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/parallel/parallel-20200222.tar.bz2"
+  sha256 "269b7236ec0887167ee7bc76357689b5c86e3dfbd5a1b3745d218526a038eb1c"
   head "https://git.savannah.gnu.org/git/parallel.git"
 
   bottle do
