5582b57b5195738c40c700966d8f6c601af712ce
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-24 10:23:49 +0000

    sleuthkit: update 4.8.0 bottle.

--- Formula/sleuthkit.rb
+++ Formula/sleuthkit.rb
@@ -6,10 +6,9 @@ class Sleuthkit < Formula
 
   bottle do
     cellar :any
-    sha256 "7cbf955a3df686e370616256ce35b7f4c6734c32b4446a3c7af3fdb6ec843569" => :catalina
-    sha256 "edd55849d72f35c2f7210d0760b5f21e6204e900a4da977a8c19fa6db3d5cf7b" => :mojave
-    sha256 "1938217697347d823ca2d915c9ab2046843d675b9d07dea38c1c452637a6db1e" => :high_sierra
-    sha256 "c8ce9b0639eb21ea41344bc117df1dda8ac103400be32dd412f79b6d18ebb50f" => :sierra
+    sha256 "a295524b0f1cacd5e67084eec862dae99f1c2787035f2322189982840b37fd73" => :catalina
+    sha256 "02ad0263a10f422f630205b332a5361921f4d33d943fe099afeee98feb5ef1bd" => :mojave
+    sha256 "97657fb6014fa5bde41bbdc79a31ba911a782531cc3b9ac1f01125fe1951d751" => :high_sierra
   end
 
   depends_on "ant" => :build
