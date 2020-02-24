27f37ad0832e3d334e8852708e64985506ff32af
Kazuki Suda <kazuki.suda@gmail.com>
2020-02-24 22:55:13 +0900

    docker-slim: also install docker-slim-sensor
    Signed-off-by: Kazuki Suda <kazuki.suda@gmail.com>
    
    Closes #50648.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/docker-slim.rb
+++ Formula/docker-slim.rb
@@ -3,6 +3,7 @@ class DockerSlim < Formula
   homepage "https://dockersl.im"
   url "https://github.com/docker-slim/docker-slim/archive/1.26.1.tar.gz"
   sha256 "f3decf77b6a75cadd194085892469391cf39f7e54bf83d9ed9080308ec2d603a"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -13,13 +14,22 @@ class DockerSlim < Formula
 
   depends_on "go" => :build
 
+  skip_clean "bin/docker-slim-sensor"
+
   def install
     ENV["CGO_ENABLED"] = "0"
     ldflags = "-s -w -X github.com/docker-slim/docker-slim/pkg/version.appVersionTag=#{version}"
-    system "go", "build", "-trimpath", "-ldflags=#{ldflags}", "-o", bin/name, "./cmd/docker-slim"
+    system "go", "build", "-trimpath", "-ldflags=#{ldflags}", "-o", bin/"docker-slim", "./cmd/docker-slim"
+
+    # docker-slim-sensor is a Linux binary that is used within Docker
+    # containers rather than directly on the macOS host.
+    ENV["GOOS"] = "linux"
+    system "go", "build", "-trimpath", "-ldflags=#{ldflags}", "-o", bin/"docker-slim-sensor", "./cmd/docker-slim-sensor"
+    (bin/"docker-slim-sensor").chmod 0555
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/docker-slim --version")
+    system "test", "-x", bin/"docker-slim-sensor"
   end
 end
