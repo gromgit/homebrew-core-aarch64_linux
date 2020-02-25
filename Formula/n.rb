af47b578d342fde789c62a2776f69066d86c9fb7
Nemanja Mitic <n.mitic@levi9.com>
2020-02-25 10:41:49 +0100

    n 6.3.1
    Closes #50679.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/n.rb
+++ Formula/n.rb
@@ -1,8 +1,8 @@
 class N < Formula
   desc "Node version management"
   homepage "https://github.com/tj/n"
-  url "https://github.com/tj/n/archive/v6.2.0.tar.gz"
-  sha256 "b200b684fd5a7cb352b74108ec53a1d57497b1a97c73479d2ebf8b3474766aea"
+  url "https://github.com/tj/n/archive/v6.3.1.tar.gz"
+  sha256 "cbb30fc25e1942507a2ef116bbb35e7ab6f63772f0126d49b801d0c4d2f38c6b"
   head "https://github.com/tj/n.git"
 
   bottle do
