fa168f3a49edfb0ad5b7f7594eba8aef7577e7aa
Bo Anderson <mail@boanderson.me>
2020-02-26 12:38:29 +0000

    go@1.13 1.13.8 (new formula)
    Closes #50704.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

---
+++ Formula/go@1.13.rb
@@ -0,0 +1,74 @@
+class GoAT113 < Formula
+  desc "Go programming environment (1.13)"
+  homepage "https://golang.org"
+  url "https://dl.google.com/go/go1.13.8.src.tar.gz"
+  mirror "https://fossies.org/linux/misc/go1.13.8.src.tar.gz"
+  sha256 "b13bf04633d4d8cf53226ebeaace8d4d2fd07ae6fa676d0844a688339debec34"
+
+  keg_only :versioned_formula
+
+  depends_on :macos => :el_capitan
+
+  resource "gotools" do
+    url "https://go.googlesource.com/tools.git",
+        :branch => "release-branch.go1.13"
+  end
+
+  # Don't update this unless this version cannot bootstrap the new version.
+  resource "gobootstrap" do
+    url "https://storage.googleapis.com/golang/go1.7.darwin-amd64.tar.gz"
+    sha256 "51d905e0b43b3d0ed41aaf23e19001ab4bc3f96c3ca134b48f7892485fc52961"
+  end
+
+  def install
+    (buildpath/"gobootstrap").install resource("gobootstrap")
+    ENV["GOROOT_BOOTSTRAP"] = buildpath/"gobootstrap"
+
+    cd "src" do
+      ENV["GOROOT_FINAL"] = libexec
+      ENV["GOOS"]         = "darwin"
+      system "./make.bash", "--no-clean"
+    end
+
+    (buildpath/"pkg/obj").rmtree
+    rm_rf "gobootstrap" # Bootstrap not required beyond compile.
+    libexec.install Dir["*"]
+    bin.install_symlink Dir[libexec/"bin/go*"]
+
+    system bin/"go", "install", "-race", "std"
+
+    # Build and install godoc
+    ENV.prepend_path "PATH", bin
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/golang.org/x/tools").install resource("gotools")
+    cd "src/golang.org/x/tools/cmd/godoc/" do
+      system "go", "build"
+      (libexec/"bin").install "godoc"
+    end
+    bin.install_symlink libexec/"bin/godoc"
+  end
+
+  test do
+    (testpath/"hello.go").write <<~EOS
+      package main
+
+      import "fmt"
+
+      func main() {
+          fmt.Println("Hello World")
+      }
+    EOS
+    # Run go fmt check for no errors then run the program.
+    # This is a a bare minimum of go working as it uses fmt, build, and run.
+    system bin/"go", "fmt", "hello.go"
+    assert_equal "Hello World\n", shell_output("#{bin}/go run hello.go")
+
+    # godoc was installed
+    assert_predicate libexec/"bin/godoc", :exist?
+    assert_predicate libexec/"bin/godoc", :executable?
+
+    ENV["GOOS"] = "freebsd"
+    ENV["GOARCH"] = "amd64"
+    system bin/"go", "build", "hello.go"
+  end
+end
