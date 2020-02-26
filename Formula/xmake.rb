74fc5a5be5c0ad95124d47a01261fa9c0b61933d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-26 09:40:56 +0000

    xmake: update 2.3.1 bottle.

--- Formula/xmake.rb
+++ Formula/xmake.rb
@@ -7,9 +7,9 @@ class Xmake < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "060a9e262ac2abb340f7c6329c9bad9dc0926015e863da2b5f6af504884eb0ab" => :catalina
-    sha256 "1de44a55ca9e37aa4e863bb027925d72a9f1975a7131048dd32bebb7d470f9f1" => :mojave
-    sha256 "f5d7779349809fe9f06ffca861a121ad1face3b00bf871079abd336e8fe9fec3" => :high_sierra
+    sha256 "6aa389fe8fcb4df79ea59f531f3c9548559ffb3345ba55e97acd886ac8e4fb1a" => :catalina
+    sha256 "b67ccf857b042dad2f8f7c8de37fa16bec192dfb5b85656875406c3654558e40" => :mojave
+    sha256 "78c6d0dbd13b614c4831bbc62a7a70458367b2994b0e7fa08770448a92786d0c" => :high_sierra
   end
 
   def install
