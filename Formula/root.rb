369b055d98201770d8437d72eff23e6ac00c20b5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-26 22:10:18 +0000

    root: update 6.20.00 bottle.

--- Formula/root.rb
+++ Formula/root.rb
@@ -7,9 +7,9 @@ class Root < Formula
   head "https://github.com/root-project/root.git"
 
   bottle do
-    sha256 "76899519d3608dba01cca9330c3e657689288b044ba809998bce83cc4baca97e" => :catalina
-    sha256 "5ef5db5c7d28ab66e469eca1fecc36f1c97fa71fce96d4cbb5b3fbb30c98e23d" => :mojave
-    sha256 "c1f9523092c84b853aa6e5afd151727662b90e5299f7b74ddf56b9d4bf7c8caa" => :high_sierra
+    sha256 "b3a40092c12f5c30d7721afdaab6796bea633321c29a60a00c73a69fbea7471a" => :catalina
+    sha256 "fdb493e9c6c830a4c84d2c69c71be68eedadd945d00bf1a3aecdfb196f683aba" => :mojave
+    sha256 "3f45125a2b36540181cff1d96a677f2de97f089e4e3b0a90ef37c16de957de32" => :high_sierra
   end
 
   # https://github.com/Homebrew/homebrew-core/issues/30726
