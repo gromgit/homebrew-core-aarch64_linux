c191c555c63c67242c9a0c0880e8f2d1a4bd5905
Mark Severson <markse@instructure.com>
2020-02-25 22:31:16 -0700

    vaulted 3.0.0
    Closes #50695.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/vaulted.rb
+++ Formula/vaulted.rb
@@ -1,8 +1,8 @@
 class Vaulted < Formula
   desc "Allows the secure storage and execution of environments"
   homepage "https://github.com/miquella/vaulted"
-  url "https://github.com/miquella/vaulted/archive/v2.4.1.tar.gz"
-  sha256 "babb2d076476ba477d545da1291918bb73e96668409c9b966c28ad20890c0eb9"
+  url "https://github.com/miquella/vaulted/archive/v3.0.0.tar.gz"
+  sha256 "ea5183f285930ffa4014d54d4ed80ac8f7aa9afd1114e5fce6e65f2e9ed1af0c"
   head "https://github.com/miquella/vaulted.git"
 
   bottle do
