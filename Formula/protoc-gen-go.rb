4ae756821a093c74c33bff1ac5d82eac9f563729
Rui Chen <chenrui333@gmail.com>
2020-02-26 14:43:39 -0500

    protoc-gen-go 1.3.4
    Closes #50738.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/protoc-gen-go.rb
+++ Formula/protoc-gen-go.rb
@@ -1,8 +1,8 @@
 class ProtocGenGo < Formula
   desc "Go support for Google's protocol buffers"
   homepage "https://github.com/golang/protobuf"
-  url "https://github.com/golang/protobuf/archive/v1.3.3.tar.gz"
-  sha256 "b3a1440808d6ed4dc0d194c482de029f6c116b1de7f7a3bf927324d965ff3853"
+  url "https://github.com/golang/protobuf/archive/v1.3.4.tar.gz"
+  sha256 "5e4279eb197ff7271cb06ae97a16f721d0fd6962ff2d2560831309c0900e72c4"
   head "https://github.com/golang/protobuf.git"
 
   bottle do
