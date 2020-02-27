1df3ffd46d2d9a90bbd27058746b191d14107a39
Bo Anderson <mail@boanderson.me>
2020-02-27 12:27:40 +0000

    zero-install 2.15.1
    Closes #50754.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/zero-install.rb
+++ Formula/zero-install.rb
@@ -1,8 +1,10 @@
 class ZeroInstall < Formula
   desc "Zero Install is a decentralised software installation system"
   homepage "https://0install.net/"
-  url "https://downloads.sourceforge.net/project/zero-install/0install/2.13/0install-2.13.tar.bz2"
-  sha256 "10726e05ac262c7c5dd1ae109deddf9aa61a146db8fc75c997dd4efc3a4d35ca"
+  url "https://github.com/0install/0install.git",
+      :tag      => "v2.15.1",
+      :revision => "e67df8585bbe75654a1d5aec60d8e46a4688b5e6"
+  head "https://github.com/0install/0install.git"
 
   bottle do
     cellar :any_skip_relocation
@@ -12,7 +14,6 @@ class ZeroInstall < Formula
     sha256 "0fa494c5e2852f8ebcadadc9c441302145444da6f580efce516263f9c1b33e4f" => :el_capitan
   end
 
-  depends_on "camlp4" => :build
   depends_on "ocaml" => :build
   depends_on "ocamlbuild" => :build
   depends_on "opam" => :build
@@ -20,35 +21,39 @@ class ZeroInstall < Formula
   depends_on "gnupg"
 
   def install
-    ENV["OCAMLPARAM"] = "safe-string=0,_" # OCaml 4.06.0 compat
     ENV.append_path "PATH", Formula["gnupg"].opt_bin
 
-    opamroot = buildpath/"opamroot"
-    ENV["OPAMROOT"] = opamroot
-    ENV["OPAMYES"] = "1"
-    system "opam", "init", "--no-setup"
-    modules = %w[
-      cppo
-      yojson
-      xmlm
-      ounit
-      lwt_react
-      ocurl
-      obus
-      sha
-      cppo_ocamlbuild
-    ]
-    system "opam", "config", "exec", "opam", "install", *modules
-
-    # mkdir: <buildpath>/build: File exists.
-    # https://github.com/0install/0install/issues/87
-    ENV.deparallelize { system "opam", "config", "exec", "make" }
-
-    inreplace "dist/install.sh" do |s|
-      s.gsub! '"/usr/local"', prefix
-      s.gsub! '"${PREFIX}/man"', man
+    # Use correct curl headers
+    if MacOS.version >= :mojave && MacOS::CLT.installed?
+      ENV["HOMEBREW_SDKROOT"] = MacOS::CLT.sdk_path(MacOS.version)
     end
-    system "make", "install"
+
+    Dir.mktmpdir("opamroot") do |opamroot|
+      ENV["OPAMROOT"] = opamroot
+      ENV["OPAMYES"] = "1"
+      ENV["OPAMVERBOSE"] = "1"
+      system "opam", "init", "--no-setup", "--disable-sandboxing"
+      modules = %w[
+        yojson
+        xmlm
+        ounit
+        lwt_react
+        ocurl
+        sha
+        dune
+      ]
+      system "opam", "config", "exec", "opam", "install", *modules
+
+      # mkdir: <buildpath>/build: File exists.
+      # https://github.com/0install/0install/issues/87
+      ENV.deparallelize { system "opam", "config", "exec", "make" }
+
+      inreplace "dist/install.sh" do |s|
+        s.gsub! '"/usr/local"', prefix
+        s.gsub! '"${PREFIX}/man"', man
+      end
+      system "make", "install"
+    end
   end
 
   test do
