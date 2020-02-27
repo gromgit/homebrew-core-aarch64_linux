f7253078fe9e1ff5658d6b1538e5b77da93438dc
Bo Anderson <mail@boanderson.me>
2020-02-27 13:30:12 +0000

    coccinelle 1.0.8
    Closes #50755.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/coccinelle.rb
+++ Formula/coccinelle.rb
@@ -1,9 +1,10 @@
 class Coccinelle < Formula
   desc "Program matching and transformation engine for C code"
   homepage "http://coccinelle.lip6.fr/"
-  url "http://coccinelle.lip6.fr/distrib/coccinelle-1.0.6.tgz"
-  sha256 "8452ed265c209dae99cbb33b67bc7912e72f8bca1e24f33f1a88ba3d7985e909"
-  revision 1
+  url "https://github.com/coccinelle/coccinelle.git",
+      :tag      => "1.0.8",
+      :revision => "d678c34afc0cfb479ad34f2225c57b1b8d3ebeae"
+  head "https://github.com/coccinelle/coccinelle.git"
 
   bottle do
     rebuild 1
@@ -12,28 +13,31 @@ class Coccinelle < Formula
     sha256 "96f2460057f45cc77e03c1980edff643cdc26a2d2cced838567c798d3ee89748" => :el_capitan
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "hevea" => :build
   depends_on "opam" => :build
-  depends_on "camlp4"
   depends_on "ocaml"
 
   def install
-    ENV["OCAMLPARAM"] = "safe-string=0,_" # OCaml 4.06.0 compat
-
-    opamroot = buildpath/"opamroot"
-    ENV["OPAMROOT"] = opamroot
-    ENV["OPAMYES"] = "1"
-    system "opam", "init", "--no-setup"
-    system "opam", "install", "ocamlfind"
-    system "./configure", "--disable-dependency-tracking",
-                          "--enable-release",
-                          "--enable-ocaml",
-                          "--enable-opt",
-                          "--enable-ocaml",
-                          "--with-pdflatex=no",
-                          "--prefix=#{prefix}"
-    system "opam", "config", "exec", "--", "make"
-    system "make", "install"
+    Dir.mktmpdir("opamroot") do |opamroot|
+      ENV["OPAMROOT"] = opamroot
+      ENV["OPAMYES"] = "1"
+      ENV["OPAMVERBOSE"] = "1"
+      system "opam", "init", "--no-setup", "--disable-sandboxing"
+      system "opam", "install", "ocamlfind"
+      system "./autogen"
+      system "opam", "config", "exec", "--", "./configure",
+                            "--disable-dependency-tracking",
+                            "--enable-release",
+                            "--enable-ocaml",
+                            "--enable-opt",
+                            "--with-pdflatex=no",
+                            "--prefix=#{prefix}"
+      ENV.deparallelize
+      system "opam", "config", "exec", "--", "make"
+      system "make", "install"
+    end
 
     pkgshare.install "demos/simple.cocci", "demos/simple.c"
   end
