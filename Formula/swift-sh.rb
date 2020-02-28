4d4270c2682534bbf55eae28334038c147d0b892
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-28 19:08:22 +0000

    swift-sh: add 1.16.1 bottle.

--- Formula/swift-sh.rb
+++ Formula/swift-sh.rb
@@ -5,6 +5,13 @@ class SwiftSh < Formula
   sha256 "fb70b075101594b146d4120b52d6132f8b34d3337d4d55ed289c57e4d7808519"
   head "https://github.com/mxcl/swift-sh.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "73afb17da0cec32d3aa9e4a1777afe0be7fa8a27689a87f626f9821e10e3e412" => :catalina
+    sha256 "75edd9678f6d70c61f1803e10310312b1792ce4e062d5bcbfebf1ec0d8a1b047" => :mojave
+    sha256 "75ef921a5ba911ff0304e213bb17ca641ad2769e8b9566015b803e22078ad348" => :high_sierra
+  end
+
   depends_on :xcode => ["10.0", :build]
 
   def install
