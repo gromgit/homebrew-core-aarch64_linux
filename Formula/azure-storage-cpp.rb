6486b7f32df3872bba095af2be37170522888357
Bo Anderson <mail@boanderson.me>
2020-02-28 04:57:56 +0000

    azure-storage-cpp 7.2.0
    Closes #50789.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/azure-storage-cpp.rb
+++ Formula/azure-storage-cpp.rb
@@ -1,9 +1,8 @@
 class AzureStorageCpp < Formula
   desc "Microsoft Azure Storage Client Library for C++"
   homepage "https://azure.github.io/azure-storage-cpp"
-  url "https://github.com/Azure/azure-storage-cpp/archive/v6.1.0.tar.gz"
-  sha256 "a0b6107372125f756783bf6e5d57d24e2c8330a4941f4c72e8ddcf13c31618ed"
-  revision 2
+  url "https://github.com/Azure/azure-storage-cpp/archive/v7.2.0.tar.gz"
+  sha256 "17e94cf3fe25491c7574eb14355dd518553ca6965b3f22be6e92045383034a56"
 
   bottle do
     cellar :any
@@ -18,13 +17,11 @@ class AzureStorageCpp < Formula
   depends_on "gettext"
   depends_on "openssl@1.1"
 
-  # patch submitted upstream at https://github.com/Azure/azure-storage-cpp/pull/261
-  patch :DATA
-
   def install
     system "cmake", "Microsoft.WindowsAzure.Storage",
                     "-DBUILD_SAMPLES=OFF",
                     "-DBUILD_TESTS=OFF",
+                    "-DBoost_USE_MULTITHREADED=ON",
                     *std_cmake_args
     system "make", "install"
   end
@@ -56,34 +53,3 @@ class AzureStorageCpp < Formula
     system "./test_azurestoragecpp"
   end
 end
-
-__END__
-diff --git a/Microsoft.WindowsAzure.Storage/cmake/Modules/FindUUID.cmake b/Microsoft.WindowsAzure.Storage/cmake/Modules/FindUUID.cmake
-index 9171f8c..a427288 100644
---- a/Microsoft.WindowsAzure.Storage/cmake/Modules/FindUUID.cmake
-+++ b/Microsoft.WindowsAzure.Storage/cmake/Modules/FindUUID.cmake
-@@ -63,6 +63,12 @@ else (UUID_LIBRARIES AND UUID_INCLUDE_DIRS)
-       /usr/freeware/lib64
-   )
-
-+  if (APPLE)
-+    if (NOT UUID_LIBRARY)
-+      set(UUID_LIBRARY  "")
-+    endif (NOT UUID_LIBRARY)
-+  endif (APPLE)
-+
-   find_library(UUID_LIBRARY_DEBUG
-     NAMES
-       uuidd
-@@ -88,9 +94,9 @@ else (UUID_LIBRARIES AND UUID_INCLUDE_DIRS)
-   set(UUID_INCLUDE_DIRS ${UUID_INCLUDE_DIR})
-   set(UUID_LIBRARIES ${UUID_LIBRARY})
-
--  if (UUID_INCLUDE_DIRS AND UUID_LIBRARIES)
-+  if (UUID_INCLUDE_DIRS AND (APPLE OR UUID_LIBRARIES))
-      set(UUID_FOUND TRUE)
--  endif (UUID_INCLUDE_DIRS AND UUID_LIBRARIES)
-+  endif (UUID_INCLUDE_DIRS AND (APPLE OR UUID_LIBRARIES))
-
-   if (UUID_FOUND)
-     if (NOT UUID_FIND_QUIETLY)
