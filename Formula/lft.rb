2c234511dafc87e1102f4fed6c7cce73e1569e48
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-28 17:18:16 +0000

    lft: update 3.80 bottle.

--- Formula/lft.rb
+++ Formula/lft.rb
@@ -7,12 +7,9 @@ class Lft < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a12149950835422ef9b392bb956861292e868ae397c4b1586b17287282b323ac" => :catalina
-    sha256 "68dfe5d46b55f399681245146a4ceee9b47c54b900d914e11c7897fb47c9f421" => :mojave
-    sha256 "42f8215a6ef6acc8cb0ac860c8f671bacfb844c4ba8fa6ce0e305a7367d4ef1a" => :high_sierra
-    sha256 "2d0f6de7ba8bc85736396ff20e8fa2a5d53fedc22a17d71a7858fcdb39afd0b0" => :sierra
-    sha256 "664ce1a522f401dbff33f8e77e1443514e05c98924e1d44cbaee8403ecb3c2e0" => :el_capitan
-    sha256 "65d4dc85d60073e1f90451c7a745dcfe4e85886caac5cbf307291bc7eed21113" => :yosemite
+    sha256 "ed9f4ee60ea875c0e06b873dc3f5a13a7b5f981966a10425418da459fbe3c0d3" => :catalina
+    sha256 "b58a621bacbdeaf1e126368f30dcd7231fb4dc1b8313d922786b7fb535cafb3d" => :mojave
+    sha256 "efc3dea6a94cc6976ba95d1931ade0768a5a8f6ab277a369601954c44f54b213" => :high_sierra
   end
 
   def install
