ebb15f6b98a15873766ae2dedfc61c256d7eb88d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-28 13:25:49 +0000

    shadowenv: update 1.3.1 bottle.

--- Formula/shadowenv.rb
+++ Formula/shadowenv.rb
@@ -6,9 +6,9 @@ class Shadowenv < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "1a016af9b61eb34448862c31aec95703fdd4fff1fcd219cf1674edc7f6cd44d8" => :catalina
-    sha256 "b24c917bb47805a2253f87878da85d2dbfb3bbe34b968cb773953e04efdc6cbc" => :mojave
-    sha256 "47635809fcac1223b736bf2ad9a4a5065595f92bc6a7c240dc16ead2475de188" => :high_sierra
+    sha256 "6eb21077d08aa62b92b162a40c94f2b68c50deb3c8f0a38b68ce225f1d7af91c" => :catalina
+    sha256 "3e93b9b98ba2dcb84dcf19115c5650bd49241260bec2751e6e2843417024025d" => :mojave
+    sha256 "aff6c727929ab3a9de176a341f55997777318d65e0ef2c547baa981e32455e8d" => :high_sierra
   end
 
   depends_on "rust" => :build
