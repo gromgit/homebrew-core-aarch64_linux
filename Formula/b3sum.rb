2a3a76a5f69e848ff37de68fa37bbd09b5f815b9
Dawid Dziurla <dawidd0811@gmail.com>
2020-02-28 14:04:20 +0100

    b3sum 0.2.2
    Closes #50808.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/b3sum.rb
+++ Formula/b3sum.rb
@@ -1,8 +1,8 @@
 class B3sum < Formula
   desc "The BLAKE3 cryptographic hash function"
   homepage "https://github.com/BLAKE3-team/BLAKE3"
-  url "https://github.com/BLAKE3-team/BLAKE3/archive/0.1.3.tar.gz"
-  sha256 "54a0eaa0a8f8fc9114251e7ad1d00d97b1c5cb96da6e648969b6b5b5a9a3bf87"
+  url "https://github.com/BLAKE3-team/BLAKE3/archive/0.2.2.tar.gz"
+  sha256 "79cfb686b940dfb11ddfaf732aa71189d18729ef6ee8f4538a3ebf66c73a0b90"
 
   bottle do
     cellar :any_skip_relocation
@@ -11,9 +11,14 @@ class B3sum < Formula
     sha256 "0dbce5bea8f31c28aae9a4842366bfebce1755f0334c41e51ddf005ba77378d1" => :high_sierra
   end
 
+  depends_on "llvm" => :build if DevelopmentTools.clang_build_version <= 1000
   depends_on "rust" => :build
 
   def install
+    if DevelopmentTools.clang_build_version <= 1000
+      ENV["HOMEBREW_CC"] = "llvm_clang"
+      ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib
+    end
     system "cargo", "install", "--locked", "--root", prefix, "--path", "./b3sum/"
   end
 
