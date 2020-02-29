bd208e4236e726c474a4f7d58f39392b232c7471
Caleb Xu <calebcenter@live.com>
2020-02-28 19:05:13 -0500

    folly 2020.02.24.00
    Closes #50839.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/folly.rb
+++ Formula/folly.rb
@@ -1,8 +1,8 @@
 class Folly < Formula
   desc "Collection of reusable C++ library artifacts developed at Facebook"
   homepage "https://github.com/facebook/folly"
-  url "https://github.com/facebook/folly/archive/v2020.02.10.00.tar.gz"
-  sha256 "d5d9915318d3587b7debdbc7597c7d0001790c6de3ed2a2645236f03edcaa08c"
+  url "https://github.com/facebook/folly/archive/v2020.02.24.00.tar.gz"
+  sha256 "9703c3e2ceab3518fe1795aabfbf3f378ccbbf46fb9ad70084b6aa842e4d86ab"
   head "https://github.com/facebook/folly.git"
 
   bottle do
