34d90a2fc29f3535dabf335f5aa5dcad98fdf704
Caleb Xu <calebcenter@live.com>
2020-02-28 19:59:06 -0500

    rancid 3.11
    Closes #50859.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/rancid.rb
+++ Formula/rancid.rb
@@ -1,9 +1,9 @@
 class Rancid < Formula
   desc "Really Awesome New Cisco confIg Differ"
   homepage "https://www.shrubbery.net/rancid/"
-  url "https://www.shrubbery.net/pub/rancid/rancid-3.10.tar.gz"
-  mirror "https://deb.debian.org/debian/pool/main/r/rancid/rancid_3.10.orig.tar.gz"
-  sha256 "7781af6df112fa45655485d178055b013cdce32523c6737dd593ca645898bafe"
+  url "https://www.shrubbery.net/pub/rancid/rancid-3.11.tar.gz"
+  mirror "https://deb.debian.org/debian/pool/main/r/rancid/rancid_3.11.orig.tar.gz"
+  sha256 "0678a1bad101d48d30308a8df7140eef02698b3a72b5368ff3318bd10394d21a"
 
   bottle do
     cellar :any_skip_relocation
