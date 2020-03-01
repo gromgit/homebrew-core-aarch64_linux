0a51d5d6a747d07cfa7e8fbebe2b618223bc3aed
Suk Min Hwang <shwang53@illinois.edu>
2020-03-01 11:07:55 -0600

    bareos-client 19.2.6
    Co-authored-by: Bo Anderson <mail@boanderson.me>
    
    Closes #50931.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/bareos-client.rb
+++ Formula/bareos-client.rb
@@ -1,9 +1,8 @@
 class BareosClient < Formula
   desc "Client for Bareos (Backup Archiving REcovery Open Sourced)"
   homepage "https://www.bareos.org/"
-  url "https://github.com/bareos/bareos/archive/Release/17.2.7.tar.gz"
-  sha256 "99a5f907e3422532c783ee254dcf5c737d2b1b53522c00924d3e1009289d2fd2"
-  revision 1
+  url "https://github.com/bareos/bareos/archive/Release/19.2.6.tar.gz"
+  sha256 "688505f8bc45b919dfd1c8bdcd448b4bdbe1ea2d1755358a94d702e9aff8482b"
 
   bottle do
     sha256 "7d69e52f214e7e5b8cf30e28ee49344165aea78837a4549b7699f01010b69dce" => :catalina
@@ -12,6 +11,8 @@ class BareosClient < Formula
     sha256 "f4cb5ceaa5c0795aca38e6ced82cff2bee7c3ccfd6b83e7b49f161818e5c0e28" => :sierra
   end
 
+  depends_on "cmake" => :build
+  depends_on "gettext"
   depends_on "openssl@1.1"
   depends_on "readline"
 
@@ -19,27 +20,22 @@ class BareosClient < Formula
     :because => "Both install a `bconsole` executable."
 
   def install
-    system "./configure", "--prefix=#{prefix}",
-                          "--with-working-dir=#{var}/lib/bareos",
-                          "--with-archivedir=#{var}/bareos",
-                          "--with-confdir=#{etc}/bareos",
-                          "--with-configtemplatedir=#{lib}/bareos/defaultconfigs",
-                          "--with-scriptdir=#{lib}/bareos/scripts",
-                          "--with-plugindir=#{lib}/bareos/plugins",
-                          "--with-fd-password=XXX_REPLACE_WITH_CLIENT_PASSWORD_XXX",
-                          "--with-mon-fd-password=XXX_REPLACE_WITH_CLIENT_MONITOR_PASSWORD_XXX",
-                          "--with-basename=XXX_REPLACE_WITH_LOCAL_HOSTNAME_XXX",
-                          "--with-hostname=XXX_REPLACE_WITH_LOCAL_HOSTNAME_XXX",
-                          "--with-python",
-                          "--enable-client-only"
-
-    # The file platforms/osx/Makefile is intended for other environment (not homebrew)
-    # and would break the build process.
-    # Therefore it is removed until this has been fixed upstream,
-    # see https://bugs.bareos.org/view.php?id=900
-    rm "platforms/osx/Makefile"
-
-    system "make", "install"
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args,
+                            "-Dworkingdir=#{var}/lib/bareos",
+                            "-Darchivedir=#{var}/bareos",
+                            "-Dconfdir=#{etc}/bareos",
+                            "-Dconfigtemplatedir=#{lib}/bareos/defaultconfigs",
+                            "-Dscriptdir=#{lib}/bareos/scripts",
+                            "-Dplugindir=#{lib}/bareos/plugins",
+                            "-Dfd-password=XXX_REPLACE_WITH_CLIENT_PASSWORD_XXX",
+                            "-Dmon-fd-password=XXX_REPLACE_WITH_CLIENT_MONITOR_PASSWORD_XXX",
+                            "-Dbasename=XXX_REPLACE_WITH_LOCAL_HOSTNAME_XXX",
+                            "-Dhostname=XXX_REPLACE_WITH_LOCAL_HOSTNAME_XXX",
+                            "-Dclient-only=ON"
+      system "make"
+      system "make", "install"
+    end
   end
 
   def post_install
