42adca048ae3c542df7e13f061da01475340e6c6
Suk Min Hwang <shwang53@illinois.edu>
2020-03-01 11:29:36 -0600

    cf-tool 1.0.0
    Closes #50957.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/cf-tool.rb
+++ Formula/cf-tool.rb
@@ -1,8 +1,8 @@
 class CfTool < Formula
   desc "Command-line tool for Codeforces contests"
   homepage "https://github.com/xalanq/cf-tool"
-  url "https://github.com/xalanq/cf-tool/archive/v0.8.2.tar.gz"
-  sha256 "04eeb0ebfd2f15f81940012def95ec964498f7053029aa8b192d99c5a88bb98b"
+  url "https://github.com/xalanq/cf-tool/archive/v1.0.0.tar.gz"
+  sha256 "6671392df969e7decf9bf6b89a43a93c2bde978e005e99ddb7fd84b0c513df9f"
 
   bottle do
     cellar :any_skip_relocation
