64751700b45602dc21fa907c6b6ff582c4e803dd
Sukmin Hwang <SukMin@wirelessprv-10-192-208-192.near.illinois.edu>
2020-03-01 10:45:02 -0600

    gitlab-gem 4.13.1
    Co-authored-by: Bo Anderson <mail@boanderson.me>
    
    Closes #50914.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/gitlab-gem.rb
+++ Formula/gitlab-gem.rb
@@ -1,8 +1,8 @@
 class GitlabGem < Formula
   desc "Ruby client and CLI for GitLab API"
   homepage "https://github.com/NARKOZ/gitlab"
-  url "https://github.com/NARKOZ/gitlab/archive/v4.11.0.tar.gz"
-  sha256 "c709833c28781b995ea141349d7b67735e27e4ae19ccfecf7d0ba27be50f3d4e"
+  url "https://github.com/NARKOZ/gitlab/archive/v4.13.1.tar.gz"
+  sha256 "45c60993a988d0c81423982f5a2f46c1ccf835796dd9f7975f2f12d503b879b1"
 
   bottle do
     cellar :any_skip_relocation
@@ -14,13 +14,18 @@ class GitlabGem < Formula
   uses_from_macos "ruby"
 
   resource "httparty" do
-    url "https://rubygems.org/gems/httparty-0.16.2.gem"
-    sha256 "fc67e5ba443b5ca822c2babccd3c6ed8bcc75fb67432b99652cb95972d204cff"
+    url "https://rubygems.org/gems/httparty-0.18.0.gem"
+    sha256 "c6f77f117ca6e7aeaf5405d0992edd43f19f4fa5eea79e9a4fdfdb287cfeee6f"
   end
 
-  resource "terminal-table" do
-    url "https://rubygems.org/gems/terminal-table-1.8.0.gem"
-    sha256 "13371f069af18e9baa4e44d404a4ada9301899ce0530c237ac1a96c19f652294"
+  resource "mime-types" do
+    url "https://rubygems.org/gems/mime-types-3.3.1.gem"
+    sha256 "708f737e28ceef48b9a1bc041aa9eec46fa36eb36acb95e6b64a9889131541fe"
+  end
+
+  resource "mime-types-data" do
+    url "https://rubygems.org/gems/mime-types-data-3.2019.1009.gem"
+    sha256 "b09bb0076f4d209d21de5f81569edffdb6e53d43f891e30edfa12433980ba6a3"
   end
 
   resource "multi_xml" do
@@ -28,9 +33,14 @@ class GitlabGem < Formula
     sha256 "d24393cf958adb226db884b976b007914a89c53ad88718e25679d7008823ad52"
   end
 
+  resource "terminal-table" do
+    url "https://rubygems.org/gems/terminal-table-1.8.0.gem"
+    sha256 "13371f069af18e9baa4e44d404a4ada9301899ce0530c237ac1a96c19f652294"
+  end
+
   resource "unicode-display_width" do
-    url "https://rubygems.org/gems/unicode-display_width-1.4.0.gem"
-    sha256 "a72802fd6345c0da220e8088b27f1800924b74d222621a06477757769b5e8000"
+    url "https://rubygems.org/gems/unicode-display_width-1.6.1.gem"
+    sha256 "3d011d8ae44d35ddf1148bdf4de9fb4331dc53b5a39420c6336261823f65f7de"
   end
 
   def install
