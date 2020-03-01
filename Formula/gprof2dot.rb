a75b241e7e3b8791ac34484c05d0c12a98f46fc3
Suk Min Hwang <shwang53@illinois.edu>
2020-03-01 10:56:50 -0600

    gprof2dot 2019.11.30
    Closes #50920.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/gprof2dot.rb
+++ Formula/gprof2dot.rb
@@ -1,9 +1,8 @@
 class Gprof2dot < Formula
   desc "Convert the output from many profilers into a Graphviz dot graph"
   homepage "https://github.com/jrfonseca/gprof2dot"
-  url "https://files.pythonhosted.org/packages/9d/36/f977122502979f3dfb50704979c9ed70e6b620787942b089bf1af15f5aba/gprof2dot-2017.9.19.tar.gz"
-  sha256 "cebc7aa2782fd813ead415ea1fae3409524343485eadc7fb60ef5bd1e810309e"
-  revision 1
+  url "https://files.pythonhosted.org/packages/fd/77/3158821acc45cc26d5815d4250275d03c254572ff53c0388af8df168ce78/gprof2dot-2019.11.30.tar.gz"
+  sha256 "b43fe04ebb3dfe181a612bbfc69e90555b8957022ad6a466f0308ed9c7f22e99"
   head "https://github.com/jrfonseca/gprof2dot.git"
 
   bottle do
