ec652cfcd57cbd9e1818cb5f36028acb7556c500
Bo Anderson <mail@boanderson.me>
2020-03-02 23:59:04 +0000

    git-filter-repo: depend on python@3.8; bottle unneeded
    python3 is only installed on Catalina if the CLT is installed.
    
    See #50550 for similar discussion.
    
    This also fixes an audit failure for the use of the `python3` alias.

--- Formula/git-filter-repo.rb
+++ Formula/git-filter-repo.rb
@@ -4,28 +4,17 @@ class GitFilterRepo < Formula
   url "https://github.com/newren/git-filter-repo/releases/download/v2.25.0/git-filter-repo-2.25.0.tar.xz"
   sha256 "ea8cdb7dca68111e819d141fc4d302b811c1e5362c12de7403882ba9908be29c"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "c47ad881f6c545f5ef1ba71b0c847f5a3575a8ff626b76412362813c0fc503a5" => :catalina
-    sha256 "336c9a9cf76ad1f46f47f74ad4015a905f8690612e2e02072db2300790e89895" => :mojave
-    sha256 "336c9a9cf76ad1f46f47f74ad4015a905f8690612e2e02072db2300790e89895" => :high_sierra
-  end
+  bottle :unneeded
 
   # ignore git dependency audit:
   #  * Don't use git as a dependency (it's always available)
   # But we require Git 2.22.0+
   # https://github.com/Homebrew/homebrew-core/pull/46550#issuecomment-563229479
   depends_on "git"
-
-  # Use any python3 version available
-  # https://github.com/Homebrew/homebrew-core/pull/46550/files#r363751231
-  if MacOS.version >= :catalina
-    uses_from_macos "python3"
-  else
-    depends_on "python3"
-  end
+  depends_on "python@3.8"
 
   def install
+    Language::Python.rewrite_python_shebang(Formula["python@3.8"].opt_bin/"python3")
     bin.install "git-filter-repo"
     man1.install "Documentation/man1/git-filter-repo.1"
   end
