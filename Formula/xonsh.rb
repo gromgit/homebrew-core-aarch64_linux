823ab4774636bf31cd2b20ff2014c013c1b933d5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-02 22:54:29 +0000

    xonsh: update 0.9.14 bottle.

--- Formula/xonsh.rb
+++ Formula/xonsh.rb
@@ -9,9 +9,9 @@ class Xonsh < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "64e1e4f2f3dcf07aacf04b46a898d5df95ee7f66214f02f535749991b910457f" => :catalina
-    sha256 "ffa3b195425cd68d2e45fdecca483068a569acde573007c1780c3ac15693d71e" => :mojave
-    sha256 "68a0826a5d7417e4c20880a8cc6874fee0594a5c9cab69233d5229e4f1cf24fc" => :high_sierra
+    sha256 "6518ee6e314bc7a44270f8dacea2a3b5e5f3b5808ff2c653aded834b8aee1af1" => :catalina
+    sha256 "61fcfd71f7368311e12e98f01c39aaec15a47f9024492a9e5e83950c814ff70f" => :mojave
+    sha256 "dd20b590171005fc7918a789dedb2eb2c5d1f7be734aae26ac64650e0996a079" => :high_sierra
   end
 
   depends_on "python@3.8"
