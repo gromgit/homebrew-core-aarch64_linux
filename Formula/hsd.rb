81680a111dbef60a659cd794be300ab1dd2282d6
Mark Tyneway <mark@purse.io>
2020-03-02 14:47:57 -0800

    hsd 2.1.3 (new formula)
    Closes #51014.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

---
+++ Formula/hsd.rb
@@ -0,0 +1,35 @@
+require "language/node"
+
+class Hsd < Formula
+  desc "Handshake Daemon & Full Node"
+  homepage "https://handshake.org"
+  url "https://github.com/handshake-org/hsd/archive/v2.1.3.tar.gz"
+  sha256 "74d2aecada314d3479ba7dde8c100ddf1e546d4fd4a7bf78ffea4f4f240778dc"
+
+  depends_on "python" => :build
+  depends_on "node@10"
+  depends_on "unbound"
+
+  def install
+    system "#{Formula["node@10"].bin}/npm", "install", *Language::Node.std_npm_install_args(libexec)
+    (bin/"hsd").write_env_script libexec/"bin/hsd", :PATH => "#{Formula["node@10"].opt_bin}:$PATH"
+  end
+
+  test do
+    (testpath/"script.js").write <<~EOS
+      const assert = require('assert');
+      const hsd = require('#{libexec}/lib/node_modules/hsd');
+      assert(hsd);
+
+      const node = new hsd.FullNode({
+        prefix: '#{testpath}/.hsd',
+        memory: false
+      });
+      (async () => {
+        await node.ensure();
+      })();
+    EOS
+    system "#{Formula["node@10"].opt_bin}/node", testpath/"script.js"
+    assert_true File.directory?("#{testpath}/.hsd")
+  end
+end
