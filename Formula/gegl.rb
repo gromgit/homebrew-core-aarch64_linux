051e0b6eb42e70cdab5584f4af5c8b5bf31c2f8c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-03 22:43:06 +0000

    gegl: update 0.4.16_3 bottle.

--- Formula/gegl.rb
+++ Formula/gegl.rb
@@ -6,10 +6,9 @@ class Gegl < Formula
   revision 3
 
   bottle do
-    sha256 "c85c022cbec147dc1c206c6efaaab0ad1cc9db52b90706a3e131b6918eed7f89" => :catalina
-    sha256 "9a94a1bfa65fc2aab0076a2dc5bdd8f78607c735b6043c5ce8d9aad192e9818f" => :mojave
-    sha256 "b12c213159d53df0bf6777fb81d13bb93ba90683da3142bb38d250d7dfc00d05" => :high_sierra
-    sha256 "22fd034b398955586ff07868178a930858d428e1af957c1921260ff679fea672" => :sierra
+    sha256 "d41ef22ba4c1dd583d3b18c305d8d132fa3db3d51da3a83df1824aff4db589ca" => :catalina
+    sha256 "3d0ff52bcb2583817c2c37d052e693e6f3477e18ae3f11d92655d56f19a7c8b9" => :mojave
+    sha256 "0c8cdd38e69be4850198711e93a99d6b524fd04ef1efc577b1f55c687e406834" => :high_sierra
   end
 
   head do
