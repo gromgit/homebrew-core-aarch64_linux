f09d034f06b010577d15394a34715f114c796a26
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-03 15:29:08 +0000

    flvmeta: update 1.2.2 bottle.

--- Formula/flvmeta.rb
+++ Formula/flvmeta.rb
@@ -7,13 +7,9 @@ class Flvmeta < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a0e6ac7e5c8daaf6b707656e97fc95e86b5c73a4f27dda58bb82bc973666892b" => :catalina
-    sha256 "67e0f288fe10e2446cdb9bbbc2991256c69cf9622aba58d6c153b37308744d29" => :mojave
-    sha256 "378420e627f64559bc36e9b80645b6aa4628e09823e408817e05a7cbfb202f2c" => :high_sierra
-    sha256 "2413315dd7ef5bc4e3cde1710d8d54a5b8481c655e3097d7fa3cebd74aae1dfb" => :sierra
-    sha256 "e38ef52c18acdf32d1a61e2604220eeff05b3ca1eb8ca8b215f20371b7b09f37" => :el_capitan
-    sha256 "54e7dc4be603332324c5a0833fa03b982c315696b07321295886d1bc3232448f" => :yosemite
-    sha256 "d50606001ee39c8fed3f928b38652e437fd3b8bb1fa97306114b93088fc98b0b" => :mavericks
+    sha256 "bb16f5006d22ffaebba50c0d9c5cc962cf73dfcf1ca51d1e69735908ef9aa8cd" => :catalina
+    sha256 "176a5edcfbe2da366e27f67590c45870b59ad250cc7f2a51d7a8d0a18f12632b" => :mojave
+    sha256 "2ef376486588157dc4e17914ab8ba62a1689aaf92fe101613f93fd0d05018fee" => :high_sierra
   end
 
   depends_on "cmake" => :build
