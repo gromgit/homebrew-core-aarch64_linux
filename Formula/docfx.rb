cb8ce2253b6a376708bb029a976db79493386154
Suk Min Hwang <47644137+shwang53@users.noreply.github.com>
2020-03-02 18:57:03 -0600

    docfx 2.49 (#50971)

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -1,8 +1,8 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/releases/download/v2.48.1/docfx.zip"
-  sha256 "f9cc6574860b4568be09ed91fc04b024efbd9c100c612e3589a5f0b87d135408"
+  url "https://github.com/dotnet/docfx/releases/download/v2.49/docfx.zip"
+  sha256 "256187a1c6ac0e4fce557e3049a89417010d8c2a09d4dd3cc54c285c4765f2a0"
 
   bottle :unneeded
 
