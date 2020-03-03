af9a87636f8583c0fd7c45f9fa033c6285493ef6
Rui Chen <chenrui333@gmail.com>
2020-03-03 00:37:24 -0500

    balena-cli 11.28.11
    Closes #51021.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/balena-cli.rb
+++ Formula/balena-cli.rb
@@ -4,8 +4,8 @@ class BalenaCli < Formula
   desc "The official balena CLI tool"
   homepage "https://www.balena.io/docs/reference/cli/"
   # Frequent upstream releases, do not update more than once a week
-  url "https://registry.npmjs.org/balena-cli/-/balena-cli-11.28.1.tgz"
-  sha256 "159fb75b19d7a15201e8c8351528c30f7bd98ab5aee759b18e59646386a41a89"
+  url "https://registry.npmjs.org/balena-cli/-/balena-cli-11.28.11.tgz"
+  sha256 "fdfd92badf5dbd02dd85207edb9239be798b1f2972bf5f04d49357c7ed61199a"
 
   bottle do
     sha256 "bc78ccd9defce92e19c97dc783bb555aa8af2173b52badb7b943607757cbfcac" => :catalina
