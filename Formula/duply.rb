39d7aff2eea611fd43848f0dadf081aac53425c0
Suk Min Hwang <47644137+shwang53@users.noreply.github.com>
2020-03-02 18:58:13 -0600

    duply 2.2.2 (#50965)

--- Formula/duply.rb
+++ Formula/duply.rb
@@ -3,8 +3,8 @@ class Duply < Formula
   # Canonical domain: duply.net
   # Historical homepage: https://web.archive.org/web/20131126005707/ftplicity.sourceforge.net
   homepage "https://sourceforge.net/projects/ftplicity/"
-  url "https://downloads.sourceforge.net/project/ftplicity/duply%20%28simple%20duplicity%29/2.2.x/duply_2.2.tgz"
-  sha256 "79e55942ef8d31d2319ed45a2c3e6c70b23797f98f5b072c31afd7cef58ea7ad"
+  url "https://downloads.sourceforge.net/project/ftplicity/duply%20%28simple%20duplicity%29/2.2.x/duply_2.2.2.tgz"
+  sha256 "c72662e8c11b4fae37f4cb47eeb4fcb0d3bcf53538d1522bf2208238de4c306e"
 
   bottle :unneeded
 
