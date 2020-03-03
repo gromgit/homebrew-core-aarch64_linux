2ebe789b37a4d1f7a93748350cd66f7efdce6e4a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-03 04:36:01 +0000

    grpc-swift: update 0.10.0 bottle.

--- Formula/grpc-swift.rb
+++ Formula/grpc-swift.rb
@@ -7,9 +7,9 @@ class GrpcSwift < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "40d3fbbc0193fbc46f029c6c5795ea8bc0faaabe7ea14073a4dc9cb0c98f1c54" => :catalina
-    sha256 "f8d66abf01b8b8573b58718a8999ab905619a2b677aa24050dab78e1cd1323de" => :mojave
-    sha256 "a8abd068afd441640955a8399ebb07c63228716e6ecd7198258f9e4b92a22e4f" => :high_sierra
+    sha256 "e0797863606102081bef338113fe59e92c0dc1e94076b5d2f7d8dc228604ecb9" => :catalina
+    sha256 "41cd40af49f7f7dd68fd65e74f6a6e33cee1bad1bdaa91cb82097b3f88b1f71c" => :mojave
+    sha256 "1b0d72d9130ed12f2cce93a7f7e4be7ad8d36ccc68167d949f4e01f46ee85942" => :high_sierra
   end
 
   depends_on :xcode => ["10.0", :build]
