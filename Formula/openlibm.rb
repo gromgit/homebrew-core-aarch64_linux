0309daa3b0a12e1e98625222738e5f871a040d76
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-03 09:04:02 +0000

    openlibm: add 0.7.0 bottle.

--- Formula/openlibm.rb
+++ Formula/openlibm.rb
@@ -4,6 +4,13 @@ class Openlibm < Formula
   url "https://github.com/JuliaMath/openlibm/archive/v0.7.0.tar.gz"
   sha256 "1699f773198018b55b12631db9c1801fe3ed191e618a1ee1be743f4570ae06a3"
 
+  bottle do
+    cellar :any
+    sha256 "a7d18405ee96e1409a3de89904263280b364ae5e0d046be6a1391031f6ec5bb9" => :catalina
+    sha256 "d7f73575a6b6b7efb4307165f578e172a1563955ed476683b27bbaca6c0cafb6" => :mojave
+    sha256 "4f769ffeda11f2bc17a5cd52a4bcd59723586b9258b789adcdb628fb700cca86" => :high_sierra
+  end
+
   keg_only :provided_by_macos
 
   def install
