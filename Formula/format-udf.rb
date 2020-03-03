7f1ac3df9c44346a3f71282d56f15705f4251681
Suk Min Hwang <47644137+shwang53@users.noreply.github.com>
2020-03-02 22:58:55 -0600

    format-udf 1.8.0 (#50944)

--- Formula/format-udf.rb
+++ Formula/format-udf.rb
@@ -1,8 +1,8 @@
 class FormatUdf < Formula
   desc "Bash script to format a block device to UDF"
   homepage "https://github.com/JElchison/format-udf"
-  url "https://github.com/JElchison/format-udf/archive/1.6.1.tar.gz"
-  sha256 "c87e2690986c5f98991937b31824f8e227ce703b17a7ca0fe63f7e6594ae9ae0"
+  url "https://github.com/JElchison/format-udf/archive/1.8.0.tar.gz"
+  sha256 "52854097db9044d729fbd7cff012f4b554df01c15225ee17ec159c71da174c8d"
 
   bottle :unneeded
 
