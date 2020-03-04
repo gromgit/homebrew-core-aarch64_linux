dae2c4191dace94bc2d5a8e8f92c6f5d9ebf4429
Bo Anderson <mail@boanderson.me>
2020-03-04 22:35:11 +0000

    innoextract 1.8

--- Formula/innoextract.rb
+++ Formula/innoextract.rb
@@ -1,8 +1,8 @@
 class Innoextract < Formula
   desc "Tool to unpack installers created by Inno Setup"
   homepage "https://constexpr.org/innoextract/"
-  url "https://constexpr.org/innoextract/files/innoextract-1.7.tar.gz"
-  sha256 "c1efb732f2bc3a80065c5f51a0d4ea6027aebf528c609d3f336aea2055d2f0a4"
+  url "https://constexpr.org/innoextract/files/innoextract-1.8.tar.gz"
+  sha256 "5e78f6295119eeda08a54dcac75306a1a4a40d0cb812ff3cd405e9862c285269"
   head "https://github.com/dscharrer/innoextract.git"
 
   bottle do
@@ -16,6 +16,12 @@ class Innoextract < Formula
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "xz"
+
+  # Boost 1.70+ compatibility. Remove with next release.
+  patch do
+    url "https://github.com/dscharrer/innoextract/commit/b47f46102bccf1d813ca159230029b0cd820ceff.patch?full_index=1"
+    sha256 "92d321d552a65e16ae6df992a653839fb19de79aa77388c651bf57b3c582d546"
+  end
 
   def install
     system "cmake", ".", *std_cmake_args
