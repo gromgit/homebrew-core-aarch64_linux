ea7395b2d62ab2a6afbc141c0eb2e38384221f83
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-04 00:05:22 +0000

    seal: add 3.4.5 bottle.

--- Formula/seal.rb
+++ Formula/seal.rb
@@ -4,6 +4,13 @@ class Seal < Formula
   url "https://github.com/microsoft/SEAL/archive/v3.4.5.tar.gz"
   sha256 "1badbab7e98a471c0d2a845db0278dd077e2fd1857434f271ef2b82798620f11"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b5c7b9610572b1abce840e9cf88e5232771ea76559fb49776c87f3176f3c8815" => :catalina
+    sha256 "9ab6a02da1750fb8deeefdadcab791017aea85b5921747af01c607c4e44e7eca" => :mojave
+    sha256 "ac9e81e17df37c76f9f0db9218235b614a71d52442c961049f727062ace7c5fa" => :high_sierra
+  end
+
   depends_on "cmake" => [:build, :test]
 
   def install
