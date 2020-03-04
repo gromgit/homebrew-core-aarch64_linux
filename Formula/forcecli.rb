dc547d50ec2c488e53f2df9fecac3e629d388955
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-04 23:53:42 +0000

    forcecli: add 0.27.1 bottle.

--- Formula/forcecli.rb
+++ Formula/forcecli.rb
@@ -5,6 +5,13 @@ class Forcecli < Formula
   sha256 "8009e57788dd7fe528d8e41b8fafc1d209b8d02119f4556b6921f1b665c1e285"
   head "https://github.com/ForceCLI/force.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5269c8ab9fccba75efe3f822abe3d41a9dd59769ed3eab204f875730ad72ee35" => :catalina
+    sha256 "37fb8cbf2d4824cb5e5d6d7a25c2403ca10eeadeb2a71f6c593698748349bb98" => :mojave
+    sha256 "19ee5f0d7a3a08734303fc565ce2a42b10e71eb8aaf7cdaa8735fdaef711ac4f" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
