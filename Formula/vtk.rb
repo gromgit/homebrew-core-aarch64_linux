cad52a8852dd40bd7f5a3a1c2a87cc32303a73e3
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-03-04 17:35:34 +0000

    vtk: revision bump for hdf5

--- Formula/vtk.rb
+++ Formula/vtk.rb
@@ -1,7 +1,7 @@
 class Vtk < Formula
   desc "Toolkit for 3D computer graphics, image processing, and visualization"
   homepage "https://www.vtk.org/"
-  revision 7
+  revision 8
   head "https://github.com/Kitware/VTK.git"
 
   stable do
