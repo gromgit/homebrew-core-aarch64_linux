00eebbc7b0c666217cbc9fd1fcba1ada82f4dda4
Bo Anderson <mail@boanderson.me>
2020-03-04 16:11:30 +0000

    imagemagick@6 6.9.10-97
    Closes #51114.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/imagemagick@6.rb
+++ Formula/imagemagick@6.rb
@@ -4,9 +4,9 @@ class ImagemagickAT6 < Formula
   # Please always keep the Homebrew mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://dl.bintray.com/homebrew/mirror/imagemagick%406-6.9.10-96.tar.xz"
-  mirror "https://www.imagemagick.org/download/releases/ImageMagick-6.9.10-96.tar.xz"
-  sha256 "b84645dbb790a156771bc6a8f1c9de599836814ac82ec6fabd9fba1cd143e9e7"
+  url "https://dl.bintray.com/homebrew/mirror/imagemagick%406-6.9.10-97.tar.xz"
+  mirror "https://www.imagemagick.org/download/releases/ImageMagick-6.9.10-97.tar.xz"
+  sha256 "a7c84103ddf5759eae39fb90fda3803fb32c7739f1cbebcb21d11e052f0ab943"
   head "https://github.com/imagemagick/imagemagick6.git"
 
   bottle do
