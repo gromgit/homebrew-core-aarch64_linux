1ecd447ad5adc6b4e14dea2d6d2d8e7c971afa8d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-05 19:16:46 +0000

    ltl2ba: update 1.2 bottle.

--- Formula/ltl2ba.rb
+++ Formula/ltl2ba.rb
@@ -6,14 +6,9 @@ class Ltl2ba < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "6a19718cf7ed4c57fa938520fc5fa2fc02239cc35b55e21e77c27f3ac4b0e586" => :catalina
-    sha256 "754dd15206872409b2af919b68dfc65a30d6343ba31aead8a8db16416af9f916" => :mojave
-    sha256 "9d1e6e9bca1073e604a7aa1e693c2df5a0636c8e6a5b82f85db491929520ae1b" => :high_sierra
-    sha256 "151f207ca2627b0916371b49918f3b97174f5c2bab56abab0eb11d17d604037d" => :sierra
-    sha256 "45916a60bb32849e1bc3709d019dcf6ac9d140d92e6f6b65bb5ca05de5c63e3b" => :el_capitan
-    sha256 "d4c93b1f70b126540dae9d77e16f7e0b42e58ff1997fce63f51820621693588d" => :yosemite
-    sha256 "75c0b88b4be2658bc02feb02214f833f7b27758381924b3e3a742ee9309579f6" => :mavericks
+    sha256 "453d994d206ec034580146ff6da2661db60c90fd105867fef4be5eb6f7ff062a" => :catalina
+    sha256 "c5a444d20a1811bf071cdc9c95ffbc272f68283978cbdd676b588ffd9c58735b" => :mojave
+    sha256 "e2d8c66cb983cdcca984048d83a3938158f5286d283cf69df40cc2d8b0c70ff4" => :high_sierra
   end
 
   def install
