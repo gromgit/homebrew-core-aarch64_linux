6d52ed8b58363cd1d13792611813df25c3adf7a0
Rui Chen <chenrui333@gmail.com>
2020-03-04 22:47:55 -0500

    kibana 6.8.7
    Closes #51140.
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/kibana.rb
+++ Formula/kibana.rb
@@ -2,8 +2,8 @@ class Kibana < Formula
   desc "Analytics and search dashboard for Elasticsearch"
   homepage "https://www.elastic.co/products/kibana"
   url "https://github.com/elastic/kibana.git",
-      :tag      => "v6.8.6",
-      :revision => "a174acf677e77d280e3cbbbd8ffb6eca6db80846"
+      :tag      => "v6.8.7",
+      :revision => "64089136f7faf8a9ec40a65279a512ffd92b427e"
   head "https://github.com/elastic/kibana.git"
 
   bottle do
@@ -13,27 +13,10 @@ class Kibana < Formula
     sha256 "8bcdc16b14a3f940f54384a42086e1e4af5d3f953734c7760131f275ceb32ee5" => :high_sierra
   end
 
-  resource "node" do
-    url "https://nodejs.org/dist/v10.15.2/node-v10.15.2.tar.xz"
-    sha256 "b8bb2da7cb016e895bc2f70009a420f6b8d519e66548624b6130bbfbd5118c59"
-  end
-
-  resource "yarn" do
-    url "https://yarnpkg.com/downloads/1.21.1/yarn-v1.21.1.tar.gz"
-    sha256 "d1d9f4a0f16f5ed484e814afeb98f39b82d4728c6c8beaafb5abc99c02db6674"
-  end
+  depends_on "yarn" => :build
+  depends_on "node@10"
 
   def install
-    resource("node").stage do
-      # Fixes detecting Apple clang 11. (this is the patch that's applied in the node@10 formula)
-      inreplace "configure.py" do |s|
-        s.gsub! 'cc, r"(^Apple LLVM version) ([0-9]+\.[0-9]+)")', 'cc, r"(^Apple (?:clang|LLVM) version) ([0-9]+\.[0-9]+)")'
-      end
-
-      system "./configure", "--prefix=#{libexec}/node"
-      system "make", "install"
-    end
-
     # remove non open source files
     rm_rf "x-pack"
     inreplace "package.json", /"x-pack":.*/, ""
@@ -44,14 +27,6 @@ class Kibana < Formula
       s.gsub! "new Project(resolve(REPO_ROOT, 'x-pack/test/tsconfig.json'), 'x-pack/test'),", ""
     end
 
-    # trick the build into thinking we've already downloaded the Node.js binary
-    mkdir_p buildpath/".node_binaries/#{resource("node").version}/darwin-x64"
-
-    # run yarn against the bundled node version and not our node formula
-    (buildpath/"yarn").install resource("yarn")
-    (buildpath/".brew_home/.yarnrc").write "build-from-source true\n"
-    ENV.prepend_path "PATH", buildpath/"yarn/bin"
-    ENV.prepend_path "PATH", prefix/"libexec/node/bin"
     system "yarn", "kbn", "bootstrap"
     system "yarn", "build", "--oss", "--release", "--skip-os-packages", "--skip-archives"
 
@@ -59,9 +34,6 @@ class Kibana < Formula
       .glob("build/oss/kibana-#{version}-darwin-x86_64/**")
       .reject { |f| File.fnmatch("build/oss/kibana-#{version}-darwin-x86_64/{node, data, plugins}", f) }
     mv "licenses/APACHE-LICENSE-2.0.txt", "LICENSE.txt" # install OSS license
-
-    inreplace "#{bin}/kibana", %r{/node/bin/node}, "/libexec/node/bin/node"
-    inreplace "#{bin}/kibana-plugin", %r{/node/bin/node}, "/libexec/node/bin/node"
 
     cd prefix do
       inreplace "config/kibana.yml", "/var/run/kibana.pid", var/"run/kibana.pid"
