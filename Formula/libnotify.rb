f15fe7e816f1087554bc8be63df8dedbfe4c3849
Bo Anderson <mail@boanderson.me>
2020-03-05 01:04:03 +0000

    libnotify 0.7.9
    Closes #51132.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/libnotify.rb
+++ Formula/libnotify.rb
@@ -1,9 +1,8 @@
 class Libnotify < Formula
   desc "Library that sends desktop notifications to a notification daemon"
   homepage "https://developer.gnome.org/libnotify"
-  url "https://download.gnome.org/sources/libnotify/0.7/libnotify-0.7.8.tar.xz"
-  sha256 "69209e0b663776a00c7b6c0e560302a8dbf66b2551d55616304f240bba66e18c"
-  revision 1
+  url "https://download.gnome.org/sources/libnotify/0.7/libnotify-0.7.9.tar.xz"
+  sha256 "66c0517ed16df7af258e83208faaf5069727dfd66995c4bbc51c16954d674761"
 
   bottle do
     sha256 "b3d67293db66122b210e6ce6dcac62c2e71dbb881ab87a5e8bac3d4b16b4111f" => :catalina
@@ -12,27 +11,22 @@ class Libnotify < Formula
     sha256 "54dd14c50b6979b7924580431bad1cf61b81940a873fe7390974de9a7cb1cd24" => :sierra
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
+  depends_on "docbook-xsl" => :build
   depends_on "gobject-introspection" => :build
   depends_on "gtk-doc" => :build
-  depends_on "libtool" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "gdk-pixbuf"
 
   def install
-    # the latest build does not include configure so we need to
-    # generate it from configure.ac.
-    # Upstream issue at https://gitlab.gnome.org/GNOME/libnotify/issues/5
-    mkdir_p "m4"
-    system "gtkdocize", "--copy"
-    system "autoreconf", "-i"
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}",
-                          "--disable-tests",
-                          "--enable-introspection"
-    system "make", "install"
+    ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+
+    mkdir "build" do
+      system "meson", "..", "--prefix=#{prefix}", "-Dtests=false"
+      system "ninja"
+      system "ninja", "install"
+    end
   end
 
   test do
