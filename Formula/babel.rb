0548ebc067c8ad8c86cfd0b1ad646a75ceeb51e0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-05 04:52:56 +0000

    babel: update 7.8.7 bottle.

--- Formula/babel.rb
+++ Formula/babel.rb
@@ -8,9 +8,9 @@ class Babel < Formula
   sha256 "e43c2bea6203f99e4964fc9bfac997d7b917fb43e24936db7dc425466c066ce0"
 
   bottle do
-    sha256 "a8cce2b8d6fae4c2ca15f5b43fc341ee709d470b1d2b6ee2c1e9f51b57ebb811" => :catalina
-    sha256 "cbe3831e019e6c42793d76aacd7ef53e6e8e05c1ab57659973c29f42fa16a7bb" => :mojave
-    sha256 "2f0be893881e4262618b5ef50a9c4783862969421c75ea689e3e7321de7bc26a" => :high_sierra
+    sha256 "1a5a985a9ec01bc5b3a917fc840b4abcc43d73099f9e2587f2c18c001519c5e6" => :catalina
+    sha256 "aea88eec45e5f99d1e79cdd24ad9af9d51568417ef3e7c471fe3e8d282e516ef" => :mojave
+    sha256 "48434e27fd11c8a229b662224e4e81925162385e8d1ac8f127347b8657be6bb8" => :high_sierra
   end
 
   depends_on "node"
