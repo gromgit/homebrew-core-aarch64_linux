79b2f6c901d1ba491f5604f83db5e05fff80f765
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-05 01:56:26 +0000

    libnotify: update 0.7.9 bottle.

--- Formula/libnotify.rb
+++ Formula/libnotify.rb
@@ -5,10 +5,10 @@ class Libnotify < Formula
   sha256 "66c0517ed16df7af258e83208faaf5069727dfd66995c4bbc51c16954d674761"
 
   bottle do
-    sha256 "b3d67293db66122b210e6ce6dcac62c2e71dbb881ab87a5e8bac3d4b16b4111f" => :catalina
-    sha256 "49a2299522d7c86e46ac6c6a2e2dbeab7b20c2e18f84b61c9d4370fc38c0511a" => :mojave
-    sha256 "d467b8388a30fe7bd2a327e0422c5316183ac161f45754b8709447cd74c35bb1" => :high_sierra
-    sha256 "54dd14c50b6979b7924580431bad1cf61b81940a873fe7390974de9a7cb1cd24" => :sierra
+    cellar :any
+    sha256 "367a8d51cb565452392b9bc92c753ca641c23f91fc4ff93fb6166b63f2beafda" => :catalina
+    sha256 "e6d5a6a87f885bf421e6a70c9cef1c6aaf89db46a98216af6c06754246a8f896" => :mojave
+    sha256 "0560e601843a3e42a4823904dd5534212efd823292444a9588f1cf99ea8bc8f5" => :high_sierra
   end
 
   depends_on "docbook-xsl" => :build
