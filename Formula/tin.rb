40c2df6f2f4fee6e3d92b5dd16c7ab69aba790ad
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-06 20:59:32 +0000

    tin: update 2.4.4 bottle.

--- Formula/tin.rb
+++ Formula/tin.rb
@@ -5,11 +5,9 @@ class Tin < Formula
   sha256 "9ff12cecf6005be4d150a26403cb736668bcedbc97fe7d6e6846559ea490ff02"
 
   bottle do
-    sha256 "2ca10c400074c9b3ec7fe56bdfd5acb638bfe2d417459971090e85d95118bef6" => :catalina
-    sha256 "e8fb724dc37c9ff8cc7681ca5bac7ee335e8636f57482171ed4f3c31a59bf066" => :mojave
-    sha256 "e9b2afbdc37d3a349dd8341e7ceb1191466b28fc9e636ef15308d6c5b7075ba4" => :high_sierra
-    sha256 "7964b2236af4b8c195271238b66a054293d94ce5bda3f10746f0b8e1d06c9f91" => :sierra
-    sha256 "572e6d081547a2b9fc46afffa994f52ffd5696be884e18858c8c03131a72faec" => :el_capitan
+    sha256 "78c9d679c6669f6e9579c1de58e1769f8d5fdf0de54664aaeba49888811d33a5" => :catalina
+    sha256 "2fb85a878a4930f0d48a35fd3628621d5d0f3009d8283066ddfdc17a950153b8" => :mojave
+    sha256 "89b38a18627bac3b16f2c947350ce3e438793603d04b30fc09bea916f85cbd49" => :high_sierra
   end
 
   depends_on "gettext"
