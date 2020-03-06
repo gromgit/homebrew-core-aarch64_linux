e7454fa1d62a9ec0bf70b13143c1e869e0f2d64d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-06 07:54:13 +0000

    git-trim: add 0.2.3 bottle.

--- Formula/git-trim.rb
+++ Formula/git-trim.rb
@@ -4,6 +4,13 @@ class GitTrim < Formula
   url "https://github.com/foriequal0/git-trim/archive/v0.2.3.tar.gz"
   sha256 "b8c4dd5b50da689432d893f82005cec4f0252761e2c71dfe2dedd24b11168d7a"
 
+  bottle do
+    cellar :any
+    sha256 "3b432c06e89d248aba054cefe6b6472fa524642bed36c95d5a2c4c3c048ddd2b" => :catalina
+    sha256 "6410b2132e6be08bcfcbd12d9f4baf918ebd51bfd1d7c7acc18836522df15eaf" => :mojave
+    sha256 "3ca739e92e5eee4b1420b16d4d4f6d034a1181357f7105c4997bc2a16b552fd9" => :high_sierra
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@1.1"
 
