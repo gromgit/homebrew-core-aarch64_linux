b5c6452e273f298d1e18b52892ff82923b63afa9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-06 18:43:34 +0000

    libgeotiff: update 1.5.1_1 bottle.

--- Formula/libgeotiff.rb
+++ Formula/libgeotiff.rb
@@ -7,10 +7,9 @@ class Libgeotiff < Formula
 
   bottle do
     cellar :any
-    sha256 "f833648eceb8cb0070df9188fab7bb6211323bea2ea9eaa202385d8af02ee705" => :catalina
-    sha256 "5feecdec004c5bc749dbc16c4dda70382b001ad1e64ab7035086cfb425abf231" => :mojave
-    sha256 "f34254f5d27c0b074d1b74dc4c73baeb8c042b4126a0e7283b7e11911e0e0e0c" => :high_sierra
-    sha256 "b8f77860ec5528e75e3c74991ec06885a65dd0fab8d9b153a2742c8696e7e43b" => :sierra
+    sha256 "7a51facc7a04ae6c9e032d5b989612044154a84779a17012e211dfaca2ece6e9" => :catalina
+    sha256 "2e19a2384be65ec690bbabefcf68fc9d33b48d48156de269f13a7ffce10518eb" => :mojave
+    sha256 "2eea9fd6e2b9ac89549ba249628824cffad7119aefebd06dd866c042d2002d0c" => :high_sierra
   end
 
   head do
