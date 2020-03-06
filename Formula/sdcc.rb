6b444012127ebf8c966e990a653b29ddc99bfba7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-06 22:19:52 +0000

    sdcc: update 4.0.0 bottle.

--- Formula/sdcc.rb
+++ Formula/sdcc.rb
@@ -6,10 +6,9 @@ class Sdcc < Formula
   head "https://svn.code.sf.net/p/sdcc/code/trunk/sdcc"
 
   bottle do
-    sha256 "91714d5a605eeff62a35e63aac856be74e35016302fa13dab181bb5422ebe7aa" => :catalina
-    sha256 "a4cfec7484f490988b439426b05310c4ddcd155a16f6d95967b0265e62b82a6c" => :mojave
-    sha256 "79f6440540864bb4fdbe3a30619c3dec35e5daa9ae012c979882e9e95c27d088" => :high_sierra
-    sha256 "129e06c6cab2f160d8eb9da70030443d9ffb783bd346be52a05b1af4b95d22ba" => :sierra
+    sha256 "876e548b2a8c31c2d45d753b59e528c82101d193398d8c158270849fe9703ece" => :catalina
+    sha256 "214547215aa0b7598ecfd80cd291bbc64bd8b2d95c867fca9653e5d0aef042d6" => :mojave
+    sha256 "1f2423cb4c4f66c34b8a68f9c7a967c4256ca438646260dbf50e7f4c0b5f8f59" => :high_sierra
   end
 
   depends_on "autoconf" => :build
