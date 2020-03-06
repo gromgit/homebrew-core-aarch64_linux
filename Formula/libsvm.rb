ba66d523fbed2cd2c0928c1ab30e5a6b77596e51
Bo Anderson <mail@boanderson.me>
2020-03-06 23:13:55 +0000

    libsvm 3.24
    Closes #51230.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/libsvm.rb
+++ Formula/libsvm.rb
@@ -3,9 +3,9 @@ class Libsvm < Formula
   homepage "https://www.csie.ntu.edu.tw/~cjlin/libsvm/"
 
   # Upstream deletes old downloads, so we need to mirror it ourselves
-  url "https://www.csie.ntu.edu.tw/~cjlin/libsvm/libsvm-3.23.tar.gz"
-  mirror "https://dl.bintray.com/homebrew/mirror/libsvm-3.23.tar.gz"
-  sha256 "257aed630dc0a0163e12cb2a80aea9c7dc988e55f28d69c945a38b9433c0ea4a"
+  url "https://www.csie.ntu.edu.tw/~cjlin/libsvm/libsvm-3.24.tar.gz"
+  mirror "https://dl.bintray.com/homebrew/mirror/libsvm-3.24.tar.gz"
+  sha256 "d5881a201a4e6227bf8e2f5de7d6eeaef481c6c2bb9540aeca547737844f8696"
 
   bottle do
     cellar :any
