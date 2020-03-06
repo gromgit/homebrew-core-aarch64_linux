aa6419d72a5490186a3fa91a57905b7c3cf98166
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-06 15:25:33 +0000

    simple-scan: update 3.36.0 bottle.

--- Formula/simple-scan.rb
+++ Formula/simple-scan.rb
@@ -5,9 +5,9 @@ class SimpleScan < Formula
   sha256 "f57d8be3c22f786efe576da04288951664e6f9fa0ec5c79afb5c2c88a11f14a5"
 
   bottle do
-    sha256 "c8335e327170c50817798510f540f76ef5a062dc2c7705a5b99f69b909308319" => :catalina
-    sha256 "7c95278f53cbb13c8128bb812b6bc2bd26eb1042b711034ff600055aa6b90edd" => :mojave
-    sha256 "c9b134c5ae257e94d08a239111b8cb6ad18707e716aad0dd88fda2c800236d8a" => :high_sierra
+    sha256 "18aac59ffddf44639d9be271ddd6fd45a2dbd43b010e42a9b505a5cd228cd370" => :catalina
+    sha256 "318a673d8a28fda2cdfc376fed25efc1a4689d9ba0f61abc7a5d0309c2cfd379" => :mojave
+    sha256 "86a1651a7f08f13e1177ef00a6f0296545dcf71d014c8e85a6a450d808c1fab0" => :high_sierra
   end
 
   depends_on "itstool" => :build
