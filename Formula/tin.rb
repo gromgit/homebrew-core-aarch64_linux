186d1781ee3947effd413ec6be43ee5991ec89cd
Bo Anderson <mail@boanderson.me>
2020-03-06 20:41:31 +0000

    tin 2.4.4
    Closes #51224.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/tin.rb
+++ Formula/tin.rb
@@ -1,8 +1,8 @@
 class Tin < Formula
   desc "Threaded, NNTP-, and spool-based UseNet newsreader"
   homepage "http://www.tin.org"
-  url "http://ftp.cuhk.edu.hk/pub/packages/news/tin/v2.4/tin-2.4.2.tar.gz"
-  sha256 "93839d2fd82175281c57f1a408dfb56bf716cf4f0b259b3e03462dca32391d51"
+  url "https://www.nic.funet.fi/pub/unix/news/tin/v2.4/tin-2.4.4.tar.xz"
+  sha256 "9ff12cecf6005be4d150a26403cb736668bcedbc97fe7d6e6846559ea490ff02"
 
   bottle do
     sha256 "2ca10c400074c9b3ec7fe56bdfd5acb638bfe2d417459971090e85d95118bef6" => :catalina
@@ -11,6 +11,8 @@ class Tin < Formula
     sha256 "7964b2236af4b8c195271238b66a054293d94ce5bda3f10746f0b8e1d06c9f91" => :sierra
     sha256 "572e6d081547a2b9fc46afffa994f52ffd5696be884e18858c8c03131a72faec" => :el_capitan
   end
+
+  depends_on "gettext"
 
   conflicts_with "mutt", :because => "both install mmdf.5 and mbox.5 man pages"
 
