330c94d77744a0b7ed48d1a9f4e5b0d4cd59dace
Bo Anderson <mail@boanderson.me>
2020-03-06 11:43:19 +0000

    freeglut 3.2.1
    Closes #51204.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/freeglut.rb
+++ Formula/freeglut.rb
@@ -1,8 +1,8 @@
 class Freeglut < Formula
   desc "Open-source alternative to the OpenGL Utility Toolkit (GLUT) library"
   homepage "https://freeglut.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/freeglut/freeglut/3.0.0/freeglut-3.0.0.tar.gz"
-  sha256 "2a43be8515b01ea82bcfa17d29ae0d40bd128342f0930cd1f375f1ff999f76a2"
+  url "https://downloads.sourceforge.net/project/freeglut/freeglut/3.2.1/freeglut-3.2.1.tar.gz"
+  sha256 "d4000e02102acaf259998c870e25214739d1f16f67f99cb35e4f46841399da68"
 
   bottle do
     cellar :any
@@ -20,8 +20,8 @@ class Freeglut < Formula
   patch :DATA
 
   def install
-    inreplace "src/x11/fg_main_x11.c", "CLOCK_MONOTONIC", "UNDEFINED_GIBBERISH"
-    system "cmake", "-D", "FREEGLUT_BUILD_DEMOS:BOOL=OFF", "-D", "CMAKE_INSTALL_PREFIX:PATH=#{prefix}", "."
+    inreplace "src/x11/fg_main_x11.c", "CLOCK_MONOTONIC", "UNDEFINED_GIBBERISH" if MacOS.version < :sierra
+    system "cmake", "-DFREEGLUT_BUILD_DEMOS=OFF", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "."
     system "make", "all"
     system "make", "install"
   end
@@ -33,10 +33,10 @@ diff --git a/CMakeLists.txt b/CMakeLists.txt
 index 28f8651..d1f6a86 100644
 --- a/CMakeLists.txt
 +++ b/CMakeLists.txt
-@@ -220,6 +220,16 @@
+@@ -258,6 +258,16 @@
  IF(FREEGLUT_GLES)
-   ADD_DEFINITIONS(-DFREEGLUT_GLES)
-   LIST(APPEND LIBS GLESv2 GLESv1_CM EGL)
+     LIST(APPEND PUBLIC_DEFINITIONS -DFREEGLUT_GLES)
+     LIST(APPEND LIBS GLESv2 GLESv1_CM EGL)
 +ELSEIF(APPLE)
 +  # on OSX FindOpenGL uses framework version of OpenGL, but we need X11 version
 +  FIND_PATH(GLX_INCLUDE_DIR GL/glx.h
