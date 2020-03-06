1b7f1e10598570a4534cffe386e8effedc274e81
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-06 20:49:04 +0000

    zimg: update 2.9.3 bottle.

--- Formula/zimg.rb
+++ Formula/zimg.rb
@@ -7,10 +7,9 @@ class Zimg < Formula
 
   bottle do
     cellar :any
-    sha256 "01b1116ebf7b0c065e19a3ce31c92965c361ba964dcc892f6fdbf69d98d94912" => :catalina
-    sha256 "7e22530b80432466153372f3d05580cf64d23d5f4fad6825887dad65737092b3" => :mojave
-    sha256 "699e5d20544252543c021de24b90aa9daae0f455310e030c485cd29b9eb350c5" => :high_sierra
-    sha256 "467810f773bb00aab981b5ad649751b9fe061a555babf15979f08a76b120f3db" => :sierra
+    sha256 "65da1647208bb9d77aefcf9a2b4413adb1af57fc115f9689dcac02fbc6893fce" => :catalina
+    sha256 "efd508b816f4d22b379645878b2aa415bd51883a404145250b6694a951c4b524" => :mojave
+    sha256 "75a2ab770e1affcee1f185dd42699721bb9d03ca99cf66f417fff106521786a5" => :high_sierra
   end
 
   depends_on "autoconf" => :build
