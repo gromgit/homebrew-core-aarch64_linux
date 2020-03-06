48aea603b7c106c8bb7d00d0bfa17d9b7aab108a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-06 12:01:32 +0000

    freeglut: update 3.2.1 bottle.

--- Formula/freeglut.rb
+++ Formula/freeglut.rb
@@ -6,12 +6,9 @@ class Freeglut < Formula
 
   bottle do
     cellar :any
-    sha256 "76e54ff5c4dce5cbe729e1a4c39c5a2049329197b61ebf04d2987489eac262d9" => :catalina
-    sha256 "5d701c190fea27fad91532d1e66164d6ebeb4f406dc91291986b841c4fe169ef" => :mojave
-    sha256 "587bdcd98c006a1077dbb077a36439c67b14064c9430c610225971f0b1b549ad" => :high_sierra
-    sha256 "ac029acfd40b4c4381dc241e1e0ad94f950cd05ed7c76348e5550a3f848d0152" => :sierra
-    sha256 "e3f9e0796917b54055f737677324560db1ffd849c415a1a40019598383671139" => :el_capitan
-    sha256 "c2816abf98614e8bf4fc1e6fb65a10d637bf1b85659cf7e9e524bee4d46b3ebe" => :yosemite
+    sha256 "48bb8108331861a7f4d1ce70472c53ee8a0f4aec75857f1bfc0ab56a59e53787" => :catalina
+    sha256 "fdc12ba4122ba3128551768b8abddd4815287a5d5b4ffdc6e00008828c43dd43" => :mojave
+    sha256 "c29f4c83e001ee7e6a751769f72b20e5096f30eccf287ca2c572088896e92833" => :high_sierra
   end
 
   depends_on "cmake" => :build
