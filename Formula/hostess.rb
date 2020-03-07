762225a0ac80ebf9233ab9f797682e77f3bc4c8b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-07 19:12:01 +0000

    hostess: update 0.4.1 bottle.

--- Formula/hostess.rb
+++ Formula/hostess.rb
@@ -7,11 +7,9 @@ class Hostess < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d0b8140a1f3d8c97868c218626fbc935ed592abaa6be72dcca368e36b62869db" => :catalina
-    sha256 "d9d5adeeb0551a9bb230b09b8463aa098abf42e2e5df4392ed7ae8236c6b4ce2" => :mojave
-    sha256 "955705aca89353471ac63e07a9254ca4252546c8071eda85a8b95ac6aa0b6331" => :high_sierra
-    sha256 "6a5ca47a7d0047d1595c79008ffc5d9a43ce8ef11cea5b6fc7deeaa239102216" => :sierra
-    sha256 "e77cffa19bdf7feb4973d59c4aa0f1551dc74b8caaf35c8a6330e71e223e2bdf" => :el_capitan
+    sha256 "b2c61fc463c4fd3eb01f39113691c9e125d50e94adca735147983d04727e3266" => :catalina
+    sha256 "6cfdc19ffaa808405e9fde6fa69825cdd9b2cbeaae7a44d67f8bbc2a14d8680d" => :mojave
+    sha256 "dd48ab07035f029439d5fe413304306aa6ef64a0d6dfcba2fd3f3814e15cdd07" => :high_sierra
   end
 
   depends_on "go" => :build
