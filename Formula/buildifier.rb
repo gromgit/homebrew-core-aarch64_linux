bad29fa1aa210a047c3c36367f4a8ab83a58a7dd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-07 13:09:15 +0000

    buildifier: update 2.0.0 bottle.

--- Formula/buildifier.rb
+++ Formula/buildifier.rb
@@ -7,9 +7,9 @@ class Buildifier < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3e43331d6ffa2eac418857c30d53d97e428d08cb6267db09a06d259eb8b01aef" => :catalina
-    sha256 "3e43331d6ffa2eac418857c30d53d97e428d08cb6267db09a06d259eb8b01aef" => :mojave
-    sha256 "3e43331d6ffa2eac418857c30d53d97e428d08cb6267db09a06d259eb8b01aef" => :high_sierra
+    sha256 "62f0325685b520d81ea00e5c59de99b1e9647a121808d60c0d6414fc6847db03" => :catalina
+    sha256 "62f0325685b520d81ea00e5c59de99b1e9647a121808d60c0d6414fc6847db03" => :mojave
+    sha256 "62f0325685b520d81ea00e5c59de99b1e9647a121808d60c0d6414fc6847db03" => :high_sierra
   end
 
   depends_on "bazel" => :build
