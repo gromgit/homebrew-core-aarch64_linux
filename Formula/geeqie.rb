71f4dcec002cdfe44c45b0839d1e2aeeb7a63307
Bo Anderson <mail@boanderson.me>
2020-03-07 00:40:34 +0000

    geeqie 1.5.1
    Closes #51235.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/geeqie.rb
+++ Formula/geeqie.rb
@@ -1,15 +1,8 @@
 class Geeqie < Formula
   desc "Lightweight Gtk+ based image viewer"
   homepage "http://www.geeqie.org/"
-  # URL needs to be an unshallow clone because it needs history to generate
-  # the changelog documentation.
-  # Unfortunately this means that the tarball can't be used to build;
-  # this is documented in the makefile.
-  url "https://github.com/BestImageViewer/geeqie.git",
-      :tag      => "v1.4",
-      :revision => "7c9b41e7c9be8cfc9b4f0a2459c0a1e0e4aaea5b",
-      :shallow  => false
-  revision 3
+  url "http://www.geeqie.org/geeqie-1.5.1.tar.xz"
+  sha256 "4854d5d323c31f8f4068fd73ab2c454ff91e826c4ca4d37b22c246ad14dea10a"
 
   bottle do
     sha256 "ef2ebcd4940ea2d0a9985aa5addc8dfd33b05e43d225dbe945fb887813ee00dd" => :catalina
@@ -36,24 +29,9 @@ class Geeqie < Formula
   depends_on "little-cms2"
   depends_on "pango"
 
-  # Fixes the build on OS X by assigning a value to a variable
-  # before passing to WEXITVALUE.
-  # https://github.com/BestImageViewer/geeqie/pull/589
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/patches/9cacfd49be1db430d7a956132d6521e23fc85f77/geeqie/wexitstatus_fix.diff"
-    sha256 "00bad28d46aafaaed99965a5c054bf04679c100c6f4f13ee82cf83c2782de349"
-  end
-
-  # Fix build with exiv 0.27
-  # https://github.com/BestImageViewer/geeqie/pull/655
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/9226ec07329457300c7af68986661f26f965436b/geeqie/exiv2_fix.diff?full_index=1"
-    sha256 "2627bbb3a338456a8d80eedb260931991e427b4a9c8dee093400632970dd68db"
-  end
-
   def install
     ENV["NOCONFIGURE"] = "yes"
-    system "./autogen.sh"
+    system "./autogen.sh" # Seems to struggle to find GTK headers without this
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--disable-glibtest",
