d1ad329eb95b5d5525e8becd886ea26d3a3e0ea6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-07 23:34:36 +0000

    qp: add 1.0.1 bottle.

--- Formula/qp.rb
+++ Formula/qp.rb
@@ -4,6 +4,13 @@ class Qp < Formula
   url "https://github.com/paybase/qp/archive/1.0.1.tar.gz"
   sha256 "6ef12fd4494262899ee12cc1ac0361ec0dd7b67e29c6ac6899d1df21efc7642b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a65499deed12110ed5a21f3bbd657acdaaf1452dea48877caac93bda55759370" => :catalina
+    sha256 "f119afd6bacbac5af055c398a2dfb5c4f62c8f113bcb9e12dab825800fd8e744" => :mojave
+    sha256 "d9c595a53f82ddd9f086fac02a5f8da34e65d9b0e7564fce02148304704457ed" => :high_sierra
+  end
+
   depends_on "quickjs" => :build
 
   resource "csp-js" do
