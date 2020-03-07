aadea24b916f4fc2ed3eeb6bd3abda9b63b95832
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-03-07 19:32:28 +0000

    gobject-introspection 1.64.0
    Closes #51277.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/gobject-introspection.rb
+++ Formula/gobject-introspection.rb
@@ -1,8 +1,8 @@
 class GobjectIntrospection < Formula
   desc "Generate introspection data for GObject libraries"
   homepage "https://wiki.gnome.org/Projects/GObjectIntrospection"
-  url "https://download.gnome.org/sources/gobject-introspection/1.62/gobject-introspection-1.62.0.tar.xz"
-  sha256 "b1ee7ed257fdbc008702bdff0ff3e78a660e7e602efa8f211dc89b9d1e7d90a2"
+  url "https://download.gnome.org/sources/gobject-introspection/1.64/gobject-introspection-1.64.0.tar.xz"
+  sha256 "eac05a63091c81adfdc8ef34820bcc7e7778c5b9e34734d344fc9e69ddf4fc82"
 
   bottle do
     sha256 "0617abce4097de602c1b3baf28619c963ce977051908ffc7f75067e75300d92c" => :catalina
@@ -11,6 +11,7 @@ class GobjectIntrospection < Formula
     sha256 "18094c0984efb6bfbdd0ada1c7954df1e5041bbcb0191694789dc96976bcdf69" => :sierra
   end
 
+  depends_on "bison" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "cairo"
@@ -23,9 +24,6 @@ class GobjectIntrospection < Formula
     url "https://gist.github.com/7a0023656ccfe309337a.git",
         :revision => "499ac89f8a9ad17d250e907f74912159ea216416"
   end
-
-  # submitted upstream as https://gitlab.gnome.org/GNOME/gobject-introspection/merge_requests/177
-  patch :DATA
 
   def install
     ENV["GI_SCANNER_DISABLE_CACHE"] = "true"
@@ -53,34 +51,3 @@ class GobjectIntrospection < Formula
     assert_predicate testpath/"Tut-0.1.typelib", :exist?
   end
 end
-
-__END__
-diff --git a/girepository/meson.build b/girepository/meson.build
-index 0183153e..204659fe 100644
---- a/girepository/meson.build
-+++ b/girepository/meson.build
-@@ -163,6 +163,15 @@ if cc.get_id() != 'msvc'
-   ])
- endif
-
-+lib_version = '1.0.0'
-+lib_version_arr = lib_version.split('.')
-+lib_version_major = lib_version_arr[0].to_int()
-+lib_version_minor = lib_version_arr[1].to_int()
-+lib_version_micro = lib_version_arr[2].to_int()
-+
-+osx_current = lib_version_major + 1
-+lib_osx_version = [osx_current, '@0@.@1@'.format(osx_current, lib_version_minor)]
-+
- girepo_lib = shared_library('girepository-1.0',
-   sources: girepo_sources,
-   include_directories : configinc,
-@@ -170,7 +179,8 @@ girepo_lib = shared_library('girepository-1.0',
-           custom_c_args,
-   dependencies: [glib_dep, gobject_dep, gmodule_dep,
-                  gio_dep, girepo_internals_dep],
--  version: '1.0.0',
-+  version: lib_version,
-+  darwin_versions: lib_osx_version,
-   install: true,
- )
