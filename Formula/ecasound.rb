3bb0cd5b2adb0d75912eedf3fc767289eeecbf1d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-07 01:28:03 +0000

    ecasound: update 2.9.3 bottle.

--- Formula/ecasound.rb
+++ Formula/ecasound.rb
@@ -5,10 +5,9 @@ class Ecasound < Formula
   sha256 "468bec44566571043c655c808ddeb49ae4f660e49ab0072970589fd5a493f6d4"
 
   bottle do
-    sha256 "1968bbbd93c0a1b0e9d4a69c983993f8449af6dc9b4ec11771509b9448fddaa0" => :catalina
-    sha256 "9bd0c3a15f5efa4ac0a97350ac54ea363ccca4b6d213dc961d8490276a69552a" => :mojave
-    sha256 "e9f0021e07723fc2e5a4d4cb3b5a27cecafd52fefce3255c488183122d0d718d" => :high_sierra
-    sha256 "05131605c2721fe09dca3699c9662d2dec290f8640a6e3daef1d2ac84b9f51d4" => :sierra
+    sha256 "f6fede56fea73bdfd32cebd514448b50dec47542ff7d76342f950a61160a9fff" => :catalina
+    sha256 "38869046308b12e2d722f1bcb5e9a7085ffab93448e0490b161a6d18fc2fbd09" => :mojave
+    sha256 "9dd2864d7b5a66bf3a7fc674b64a11d3cb1abeaf9fc4c65dd8898a2724c1a5a8" => :high_sierra
   end
 
   depends_on "jack"
