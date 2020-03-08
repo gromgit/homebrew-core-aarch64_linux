8933735b8693427133711b2e7ea9a3789bc5fdfb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-08 18:06:32 +0000

    ezstream: update 1.0.1 bottle.

--- Formula/ezstream.rb
+++ Formula/ezstream.rb
@@ -6,14 +6,9 @@ class Ezstream < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "5e90210ab59dc2ff6a0a8a64b0153a59f9f63a37ec32c17d8eef21ba1112f447" => :catalina
-    sha256 "45d77ec44eb517437a2b0bf73b5fd30baafae77f37bc9eb4417956261f345fe4" => :mojave
-    sha256 "8dda7ec4014a1c4f13a3a17f7e243b9e7153c069f5c8cff1a20c7b2518d78be6" => :high_sierra
-    sha256 "eee3cc2ed988d5c0e131d9ba8f0aef0e7bb520311096a9719b914c0a1ca6ffe4" => :sierra
-    sha256 "365c26a87addf50359e65ccd98ce4244b61f7e9015a335ff47bf55a90b35ad19" => :el_capitan
-    sha256 "dfa4b2537b1ce6b0da0c4214ccedca664bdd2e69962fa6579d9c437b1ff94e92" => :yosemite
-    sha256 "04e3a89b8b1e91ce160ec94c981b71d8fb08d7be8fef3da3f1c33b29dc9e3f8c" => :mavericks
+    sha256 "5dbee1cca793b44cd470bd858b3c6f53bd82c48609468fefe11fc79ba495fe56" => :catalina
+    sha256 "fe97ee0a48df55d159cbd9f9cb7c066cc003ff430fb211f83a95df41a2e555e2" => :mojave
+    sha256 "7714b3b155984c561dabce8a39c2668658cce995ee582aaca387fde476b38914" => :high_sierra
   end
 
   depends_on "check" => :build
