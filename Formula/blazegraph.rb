0f7d2d1ae611d1fbcfd527fd8e7cc51d19352b0e
Suk Min Hwang <47644137+shwang53@users.noreply.github.com>
2020-03-08 01:11:06 -0600

    blazegraph 2.1.5 (#50936)
    Update blazegraph.rb
    
    Attempt to pin java version for test

--- Formula/blazegraph.rb
+++ Formula/blazegraph.rb
@@ -1,16 +1,17 @@
 class Blazegraph < Formula
   desc "Graph database supporting RDF data model, Sesame, and Blueprint APIs"
   homepage "https://www.blazegraph.com/"
-  url "https://downloads.sourceforge.net/project/bigdata/bigdata/2.1.4/blazegraph.jar"
-  sha256 "b175d2b4aa9e2f65fcbf4d6f75f8dd12ef75022f82fb94df1fbd0bac5230af2a"
+  url "https://downloads.sourceforge.net/project/bigdata/bigdata/2.1.5/blazegraph.jar"
+  sha256 "fbaeae7e1b3af71f57cfc4da58b9c52a9ae40502d431c76bafa5d5570d737610"
 
   bottle :unneeded
 
-  depends_on :java => "1.7+"
+  # dependnecy can be lifted in the upcoming release, > 2.1.5
+  depends_on :java => "1.8"
 
   def install
     libexec.install "blazegraph.jar"
-    bin.write_jar_script libexec/"blazegraph.jar", "blazegraph"
+    bin.write_jar_script libexec/"blazegraph.jar", "blazegraph", :java_version => "1.8"
   end
 
   plist_options :startup => "true", :manual => "blazegraph start"
@@ -35,6 +36,8 @@ class Blazegraph < Formula
   end
 
   test do
+    ENV.prepend "_JAVA_OPTIONS", "-Djava.io.tmpdir=#{testpath}"
+
     server = fork do
       exec bin/"blazegraph"
     end
