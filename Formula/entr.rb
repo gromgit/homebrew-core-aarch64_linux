3580f40b7b216803c75b7252b2b64850241edf1d
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-07 22:46:27 -0500

    entr: update head (#51286)

--- Formula/entr.rb
+++ Formula/entr.rb
@@ -3,17 +3,13 @@ class Entr < Formula
   homepage "http://entrproject.org/"
   url "http://entrproject.org/code/entr-4.4.tar.gz"
   sha256 "54566c64f360afd43f6a6065bc6d849472337edf2189b1ce34bf15b611f350f4"
+  head "https://github.com/eradman/entr.git"
 
   bottle do
     cellar :any_skip_relocation
     sha256 "98f508565c8dd087b780fda140099fca3afb457ca27fcf3864bb508c87c403cc" => :catalina
     sha256 "d18935ecc0bf78504d6acd00b2adb889389af2586cafc2602e38599f2590183f" => :mojave
     sha256 "25fba36721d2857ca91efc7b82a8cbe15ff0a83f20e9febe57648fc173377629" => :high_sierra
-  end
-
-  head do
-    url "https://bitbucket.org/eradman/entr", :using => :hg
-    depends_on "mercurial" => :build
   end
 
   def install
