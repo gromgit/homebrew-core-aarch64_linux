f6b7fb933923931be01bf259c4dcef8f7cb82025
Bo Anderson <mail@boanderson.me>
2020-03-09 01:19:19 +0000

    inadyn 2.6
    Closes #51342.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/inadyn.rb
+++ Formula/inadyn.rb
@@ -1,8 +1,8 @@
 class Inadyn < Formula
   desc "Dynamic DNS client with IPv4, IPv6, and SSL/TLS support"
   homepage "https://troglobit.com/projects/inadyn/"
-  url "https://github.com/troglobit/inadyn/releases/download/v2.5/inadyn-2.5.tar.gz"
-  sha256 "f7faf0be4f0b4bfa1acc811189a9ed0a58bc367e48ea31c283920a2ef27cdc40"
+  url "https://github.com/troglobit/inadyn/releases/download/v2.6/inadyn-2.6.tar.xz"
+  sha256 "9f4198764abbd2798472b349d8867e86b4692c76ee304f1f9c607f67c9b582a4"
 
   bottle do
     sha256 "3f03070bad5247eab88623753d6c4c3f60c197cc7c629324afbaefaf0d9d8560" => :catalina
@@ -18,12 +18,6 @@ class Inadyn < Formula
   depends_on "confuse"
   depends_on "gnutls"
   depends_on "pkg-config"
-
-  # Fix for Sierra with v2.5, remove in next version
-  patch do
-    url "https://github.com/troglobit/inadyn/commit/57bdcc0321b49ee68397c70140d9895655edb06f.diff?full_index=1"
-    sha256 "6d24c3822e7017a471583f5424421d83e6e426b464ca7521db943ecec580eea5"
-  end
 
   def install
     mkdir_p buildpath/"inadyn/m4"
