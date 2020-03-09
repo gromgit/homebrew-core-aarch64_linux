6a1622de6ab0602d8a454dd7d58da5dba6d85091
Bo Anderson <mail@boanderson.me>
2020-03-09 10:45:32 +0000

    curl-openssl: apply bug fix patches
    Closes #51351.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/curl-openssl.rb
+++ Formula/curl-openssl.rb
@@ -1,8 +1,28 @@
 class CurlOpenssl < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.haxx.se/"
-  url "https://curl.haxx.se/download/curl-7.69.0.tar.bz2"
-  sha256 "668d451108a7316cff040b23c79bc766e7ed84122074e44f662b8982f2e76739"
+  revision 1
+
+  stable do
+    url "https://curl.haxx.se/download/curl-7.69.0.tar.bz2"
+    sha256 "668d451108a7316cff040b23c79bc766e7ed84122074e44f662b8982f2e76739"
+
+    # The below three patches all fix critical bugs. Remove them with curl 7.69.1.
+    patch do
+      url "https://github.com/curl/curl/commit/8aa04e9a24932b830bc5eaf6838dea5a3329341e.patch?full_index=1"
+      sha256 "77595ec475e692bd24832e0e6e98de5d68a43bf7199c632ae0443fcb932791fb"
+    end
+
+    patch do
+      url "https://github.com/curl/curl/commit/e040146f22608fd92c44be2447a6505141a8a867.patch?full_index=1"
+      sha256 "f4267c146592067e84eacb62cdb22e0a35636699a8237470ccaf27d68cb17a86"
+    end
+
+    patch do
+      url "https://github.com/curl/curl/commit/64258bd0aa6ad23195f6be32e6febf7439ab7984.patch?full_index=1"
+      sha256 "afeb69e09b3402926acd40d76f6b28d9790ac1f1e080f4eb3f2500d5aaf46971"
+    end
+  end
 
   bottle do
     sha256 "e2f3d69c79e245ec7f7b3c71d9eaa313e90268f96566f6d02496bca99b6184b3" => :catalina
