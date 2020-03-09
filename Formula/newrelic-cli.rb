00a5542384ac62c3b58d2982280776a3b9051e85
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-09 23:39:41 +0000

    newrelic-cli: add 0.2.3 bottle.

--- Formula/newrelic-cli.rb
+++ Formula/newrelic-cli.rb
@@ -5,6 +5,13 @@ class NewrelicCli < Formula
   sha256 "f09d7e1705731578da79f0d9c204a6875edbd1d78cc6e69bd9f7d00dd9919986"
   head "https://github.com/newrelic/newrelic-cli.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b0acd5cd0bf0b6149972457c53cef90ca7974b4df6f051a559cf67c0df8bdaaa" => :catalina
+    sha256 "e149e6ed90894bd8d74fdfa881861c0e47434cc2c38c29ec1ff1409e6b029ec8" => :mojave
+    sha256 "4c87e062f9dab115690e9a333593ed134e4a1981752683232cb074584c6e5bbe" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
