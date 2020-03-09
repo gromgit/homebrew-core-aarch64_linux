85958a14228b23132709e2e408bdeaaed6ab94e1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-09 22:16:47 +0000

    hdt: add 1.3.3 bottle.

--- Formula/hdt.rb
+++ Formula/hdt.rb
@@ -4,6 +4,13 @@ class Hdt < Formula
   url "http://srvgal80.deri.ie/~pabtor/hdt/hdt-1.3.3.tar.gz"
   sha256 "ac624802909ce04cbc046f00173496fb44768b45157d284d258d394477ae23b9"
 
+  bottle do
+    cellar :any
+    sha256 "66978658e51117e228dea28a0d4264cfe3ce9ed7e4536eb0726d8c1438d4fb59" => :catalina
+    sha256 "333a1baf863f372e94a40474a799fdd7e043bd691817ab5f7467983ce31a21cb" => :mojave
+    sha256 "709ea815a3a24e104b0bd873948d8cbaca317ed235098f1c042ab308f7c3cb6f" => :high_sierra
+  end
+
   depends_on "pkg-config" => :build
   depends_on "serd"
 
