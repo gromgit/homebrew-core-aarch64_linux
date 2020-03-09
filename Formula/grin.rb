f9ff99758649caa485ab881bf86811a2588a8e1d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-09 14:01:23 +0000

    grin: update 3.1.0 bottle.

--- Formula/grin.rb
+++ Formula/grin.rb
@@ -6,9 +6,9 @@ class Grin < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b459b1d029abbe94bb3e65b2c66045c4f37673ccb5ad140b87c20ae648dd3875" => :catalina
-    sha256 "9ebd55d251a8d939569a5ae1c2b882f4a89c52e0f545716f0470c63aa94b6c15" => :mojave
-    sha256 "79e6906b19db7e058014e7f3fe462437c03a15de0d68a974f9e3cb91f8b9deec" => :high_sierra
+    sha256 "883df2b98db4e74b9c7a963d3b9562b4714e83a3601a37668c1bd134443d0557" => :catalina
+    sha256 "981bbb94eb21af293480eef635094063815fb13dc7d672d03b379684817af840" => :mojave
+    sha256 "25038963829236bc0ffa02d8a6ffc4120d2a3e0eccd5248d2bfab969f6c2a0b0" => :high_sierra
   end
 
   depends_on "rust" => :build
