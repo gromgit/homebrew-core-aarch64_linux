79c5603feaa009e68707b52661f2f44b08dd6616
Bo Anderson <mail@boanderson.me>
2020-03-09 00:52:32 +0000

    p11-kit: revision bump for libffi

--- Formula/p11-kit.rb
+++ Formula/p11-kit.rb
@@ -3,6 +3,7 @@ class P11Kit < Formula
   homepage "https://p11-glue.freedesktop.org"
   url "https://github.com/p11-glue/p11-kit/releases/download/0.23.20/p11-kit-0.23.20.tar.xz"
   sha256 "14d86024c3dfd6b967d9bc0b4ec7b2973014fe7423481f4d230a1a63b8aa6104"
+  revision 1
 
   bottle do
     sha256 "7c8fe19508151c0cedbb47c8ac2073357f3a8874e06c24f351c16cfea9aafb57" => :catalina
