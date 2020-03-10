de410b3b3e458eca6ab6d378b1ff016ba34caea9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-10 00:03:01 +0000

    fselect: update 0.6.9 bottle.

--- Formula/fselect.rb
+++ Formula/fselect.rb
@@ -6,9 +6,9 @@ class Fselect < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9ae070104402cd0d37b7ad05aa6ae72bf7413984a0c1de622b57b94d833e7636" => :catalina
-    sha256 "3636cfa901b7867e4874ae130ecf4a991a0343c8a011a1c4d0dbb816604376a7" => :mojave
-    sha256 "ef74e79690e57dbd7b06b112cb62f2ccd06c6f24d17fce28d170c81a9abf34a9" => :high_sierra
+    sha256 "64da9563e3aa17c48574a8ddcedeb3b86d69bb7e9c9bbdd705e37de2e682380a" => :catalina
+    sha256 "72bfe29d9c29b6d3592074b18142289e629b43e017c9c2c7bf3ddac573d4ec0f" => :mojave
+    sha256 "2499034e2890a202bd7aff3a71a0cb5d6d75d55be3852861384db0bb7443cc4f" => :high_sierra
   end
 
   depends_on "rust" => :build
