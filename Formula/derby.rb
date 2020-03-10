a4e3c28606f5bb999bfc7d88b4601ba949b3a5b9
chenrui <chenrui333@gmail.com>
2020-03-10 00:33:57 -0400

    derby 10.15.2.0 (#51416)

--- Formula/derby.rb
+++ Formula/derby.rb
@@ -1,10 +1,9 @@
 class Derby < Formula
   desc "Apache Derby is an embedded relational database running on JVM"
   homepage "https://db.apache.org/derby/"
-  url "https://www.apache.org/dyn/closer.lua?path=db/derby/db-derby-10.15.1.3/db-derby-10.15.1.3-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/db/derby/db-derby-10.15.1.3/db-derby-10.15.1.3-bin.tar.gz"
-  sha256 "eedb0293fea8b7d9cc813371c34935661e42ea8270e72fedd0ffe2a6a29c61ad"
-  revision 1
+  url "https://www.apache.org/dyn/closer.lua?path=db/derby/db-derby-10.15.2.0/db-derby-10.15.2.0-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/db/derby/db-derby-10.15.2.0/db-derby-10.15.2.0-bin.tar.gz"
+  sha256 "ac51246a2d9eef70cecd6562075b30aa9953f622cbd2cd3551bc3d239dc6f02a"
 
   bottle :unneeded
 
