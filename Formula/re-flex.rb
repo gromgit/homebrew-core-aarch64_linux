eeed5464f8fa1d41767b977131669438510c8a8c
Robert van Engelen <ravengelen@gmail.com>
2020-03-10 14:41:58 -0400

    re-flex 1.6.2
    RE-flex version bump to 1.6.2 stable
    
    Closes #51495.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/re-flex.rb
+++ Formula/re-flex.rb
@@ -1,8 +1,8 @@
 class ReFlex < Formula
   desc "The regex-centric, fast and flexible scanner generator for C++"
   homepage "https://www.genivia.com/doc/reflex/html"
-  url "https://github.com/Genivia/RE-flex/archive/v1.4.3.tar.gz"
-  sha256 "8cb9cc9bf40ecc145955fa52570312821bc924d6dba1021ab23af95755c04c3a"
+  url "https://github.com/Genivia/RE-flex/archive/v1.6.2.tar.gz"
+  sha256 "28b253c54add41c6cbb53d3b0f801061edd5d59efaa7732141060d28d57a8611"
 
   bottle do
     cellar :any_skip_relocation
@@ -10,6 +10,9 @@ class ReFlex < Formula
     sha256 "caf15bf38d93db054e1f90a9182802b7df688b3266dccd48ccd7550c7d7d50e7" => :mojave
     sha256 "aeee136f831845272ba2e5b67aee5cbe6e93a6d4b77a9991f5bd0bda88f93e76" => :high_sierra
   end
+
+  depends_on "boost"
+  depends_on "pcre2"
 
   def install
     system "./configure", "--disable-dependency-tracking",
