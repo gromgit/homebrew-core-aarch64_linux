940a99b958c28b4b9a50b578d61bc71943eb0beb
Rui Chen <chenrui333@gmail.com>
2020-03-10 12:24:44 -0400

    faiss 1.6.2
    Closes #51488.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/faiss.rb
+++ Formula/faiss.rb
@@ -1,9 +1,8 @@
 class Faiss < Formula
   desc "Efficient similarity search and clustering of dense vectors"
   homepage "https://github.com/facebookresearch/faiss"
-  url "https://github.com/facebookresearch/faiss/archive/v1.6.1.tar.gz"
-  sha256 "827437c9a684fcb88ee21a8fd8f0ecd94f36e2db213f74357d0465c5a7e72ac6"
-  revision 1
+  url "https://github.com/facebookresearch/faiss/archive/v1.6.2.tar.gz"
+  sha256 "8be8fcb943e94a93fb0796cad02a991432c0d912d8ae946f4beb5a8a9c5d4932"
 
   bottle do
     cellar :any
