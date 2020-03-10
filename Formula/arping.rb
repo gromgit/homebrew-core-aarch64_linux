49f94cf041d192a4488272078da87a917b3cdb23
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-10 03:54:09 +0000

    arping: update 2.21 bottle.

--- Formula/arping.rb
+++ Formula/arping.rb
@@ -6,10 +6,9 @@ class Arping < Formula
 
   bottle do
     cellar :any
-    sha256 "b89ee98a8f209031e51c3289ae5afb518491b1ead9d12980dfd5fd58fa88fa92" => :catalina
-    sha256 "15e267fd6609e686236668578452b0d2f3fc7771b24fb9ef1486fe9752f44645" => :mojave
-    sha256 "f9d6ddf4906c516e80b25f7a28e8d00f917af69653dc9ecfcc36a0f4bd0b2f99" => :high_sierra
-    sha256 "3170e9a0f53d76b1ae18ae15fa5945578044debb5798c1955bb8d9437b83088e" => :sierra
+    sha256 "ef1888a0b7fef648f4f2bb72e62b5c9760f99e200353ab1f73a57b955012ed50" => :catalina
+    sha256 "857799b8c49838abb0fbe7d54b7c87de397233535fb1bfc04a88800e10a0f629" => :mojave
+    sha256 "673a70e73e435f275d8d47c6034cebdffe57908d417abd617fa3c8973b393ce9" => :high_sierra
   end
 
   depends_on "autoconf" => :build
