12480cd960c0796943c91574c0a477bd620d815a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-10 04:06:25 +0000

    vttest: update 20200303 bottle.

--- Formula/vttest.rb
+++ Formula/vttest.rb
@@ -6,10 +6,9 @@ class Vttest < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "91afb7cae0494fc2da73e6e66121e2482d5673b69b2ce3e99792e79b01f8a98b" => :catalina
-    sha256 "a8dbf4d1fa88673fc6aba2c41b13ff917be9669220852bd4a2295d44c60c8f7a" => :mojave
-    sha256 "8d3bd4169069a0e8b87b9715db1d6518acb4e335157f7be25b867a48c13d023f" => :high_sierra
-    sha256 "206744ef6b93ed8a6be0921ebd964b4e8e03e92b057afbcb9dff0806bf6ef747" => :sierra
+    sha256 "e9d893afeefe1997a5419152ce4182bf68f70cdfbc55c3dad5d9b473a06ba906" => :catalina
+    sha256 "237cb61899805abac4a990f714150afa74f4f5a5e7c0d28fd5f01442c275b699" => :mojave
+    sha256 "2c5cc23ef56bcdd29f6168d0759065e4a1eb9738cd263d1bc83500e08c703efa" => :high_sierra
   end
 
   def install
