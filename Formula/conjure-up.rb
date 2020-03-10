5660b369a67c5f02eef23c6d1c577217f41efbe1
Bo Anderson <mail@boanderson.me>
2020-03-10 22:02:16 +0000

    conjure-up: revision bump to fix virtualenv symlinks

--- Formula/conjure-up.rb
+++ Formula/conjure-up.rb
@@ -5,7 +5,7 @@ class ConjureUp < Formula
   homepage "https://conjure-up.io/"
   url "https://github.com/conjure-up/conjure-up/archive/2.6.9.tar.gz"
   sha256 "b5ebba187d27b3474b36acd715df015b198c0e5df8aefb32200ba4f3f3de17f4"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
