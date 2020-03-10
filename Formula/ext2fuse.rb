ecd560df6d2532cf8cdf9e6306968faf1b65dbb3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-10 23:00:17 +0000

    ext2fuse: update 0.8.1_1 bottle.

--- Formula/ext2fuse.rb
+++ Formula/ext2fuse.rb
@@ -7,12 +7,9 @@ class Ext2fuse < Formula
 
   bottle do
     cellar :any
-    sha256 "183585fee33c1752f5c7ee7dbf3174e4c1af7646d3a6ebbd844437d6c50fe3e5" => :catalina
-    sha256 "f3f1a63cf8c5c375e5ebd93a03c33f3bd6df148045171697fbe99a67884b929e" => :mojave
-    sha256 "db5601be89a17e1d0496cf698c6a61adeb6aad617b47d2f93fe35b6176d735a2" => :high_sierra
-    sha256 "22f2c626da7fbf83036debd9d3a564334c7d12919828c90cdb820c69c7911e3f" => :sierra
-    sha256 "fcf23e3ef7bf5afed5f3a009c4e41e4fbe6852faac0dff0cc5c8f35b3b491571" => :el_capitan
-    sha256 "01064d0a21a3a1d60686657df9146194700c879e5b43447c4a6cbc80cc62705b" => :yosemite
+    sha256 "91a5c2561402f51502aa651b44426cd9fd2e8b22962a1d27d8e514ab1828fdb8" => :catalina
+    sha256 "3eb552c79cbee064d7227b55b390beede8469c5c2f18491e031d7f0306f0c2cf" => :mojave
+    sha256 "754488298c713b74d28b91a67b2adb082c290344d1490319e485f420cb402a02" => :high_sierra
   end
 
   depends_on "e2fsprogs"
