704fa97e25879faff244d9b7b1c1d7e893b62fc7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-10 07:42:28 +0000

    libqalculate: update 3.8.0 bottle.

--- Formula/libqalculate.rb
+++ Formula/libqalculate.rb
@@ -5,9 +5,9 @@ class Libqalculate < Formula
   sha256 "d8fcf445677ced76c13db5bd31af96f069f83041a264ac4df0d065af47b2e1ae"
 
   bottle do
-    sha256 "a08afee195e0f848662ce5c555f1d25aed239f3c3f6223d08e4473de168c31c4" => :catalina
-    sha256 "894cfa74226f7da56fba30ac44b8a8e2cc0b62e13c8adcf7518309dcac7741b1" => :mojave
-    sha256 "31b223623a9c64b5e944aace7564c19165716b6f50481668979c7f31a13aaddc" => :high_sierra
+    sha256 "3554e5419aecc4ec6f910c8dd83283b08771aff982694ade05d89116b364499a" => :catalina
+    sha256 "9c13d2e2d86553f4f50f7097b9dba60662d7a3ecb916245650eea25914ecec3d" => :mojave
+    sha256 "810f043cd13cdf704175764baf40b9a5fb5529d15d924fa6ba364efa97f2b2a9" => :high_sierra
   end
 
   depends_on "intltool" => :build
