a4890fbc2deaebf697418a99861184dbe2cf399f
Bo Anderson <mail@boanderson.me>
2020-03-10 02:28:44 +0000

    spice-protocol 0.14.1
    Closes #51398.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/spice-protocol.rb
+++ Formula/spice-protocol.rb
@@ -1,8 +1,8 @@
 class SpiceProtocol < Formula
   desc "Headers for SPICE protocol"
   homepage "https://www.spice-space.org/"
-  url "https://www.spice-space.org/download/releases/spice-protocol-0.14.0.tar.bz2"
-  sha256 "b6a4aa1ca32668790b45a494bbd000e9d05797b391d5a5d4b91adf1118216eac"
+  url "https://www.spice-space.org/download/releases/spice-protocol-0.14.1.tar.bz2"
+  sha256 "79e6da61834b080a143234c1cd4c099a8ead1a64b5039489610b72ab282c132a"
 
   bottle do
     cellar :any_skip_relocation
