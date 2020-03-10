81a0915064f477e2e1c059a2a847337819e699fd
Rui Chen <chenrui333@gmail.com>
2020-03-10 01:29:15 -0400

    git-standup 2.3.2
    Closes #51441.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/git-standup.rb
+++ Formula/git-standup.rb
@@ -1,8 +1,8 @@
 class GitStandup < Formula
   desc "Git extension to generate reports for standup meetings"
   homepage "https://github.com/kamranahmedse/git-standup"
-  url "https://github.com/kamranahmedse/git-standup/archive/2.3.1.tar.gz"
-  sha256 "79c75cb3219f022c55d9df93c1292547745d28ccb62252faf52fe6023a41c60c"
+  url "https://github.com/kamranahmedse/git-standup/archive/2.3.2.tar.gz"
+  sha256 "48d5aaa3c585037c950fa99dd5be8a7e9af959aacacde9fe94143e4e0bfcd6ba"
   head "https://github.com/kamranahmedse/git-standup.git"
 
   bottle do
