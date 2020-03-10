73c77aadbc019384788404565fe06819368f6572
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-10 02:10:32 +0000

    dosbox-x: update 0.83.0 bottle.

--- Formula/dosbox-x.rb
+++ Formula/dosbox-x.rb
@@ -8,9 +8,9 @@ class DosboxX < Formula
 
   bottle do
     cellar :any
-    sha256 "09505657fb2a1d1b701943d6dd762d4a8f07b098c10cc9adbfd7f24d45928c16" => :catalina
-    sha256 "b2c32c4797edd865aea2bb1c4635741fefe9257353e68aaac4ef99c96e45cf99" => :mojave
-    sha256 "5eabafbb606d7d78931e8b39dfae1876b9c5e82897297f1e70c7bbe141dfdfad" => :high_sierra
+    sha256 "50c4f1eef202f3911273db2e03c29dc5181e2c5202791d82c4ce24e0d8b90e00" => :catalina
+    sha256 "dda9ef26dac2c2dc2d5f4b1eef727ffedde93d7b4aaac31fed48c022c4601e5e" => :mojave
+    sha256 "c343baffac58d607f92ea068316f134a82d3849b50b475c6a9fd59caad5eb1c0" => :high_sierra
   end
 
   depends_on "autoconf" => :build
