9ed53ff26f2513f99e6aefc0e5c3b13605a72744
Bo Anderson <mail@boanderson.me>
2020-03-10 22:02:37 +0000

    git-plus: revision bump to fix virtualenv symlinks

--- Formula/git-plus.rb
+++ Formula/git-plus.rb
@@ -5,7 +5,7 @@ class GitPlus < Formula
   homepage "https://github.com/tkrajina/git-plus"
   url "https://files.pythonhosted.org/packages/72/75/5de42fceb6a7feb50386f29bd2a9d5391c90ba4e74ab78d86c095edd9f21/git-plus-v0.3.3.tar.gz"
   sha256 "54fa88f82e52863dcf5f2d44c258a22e8d31232473300a4384eba8e2f71df1ea"
-  revision 1
+  revision 2
   head "https://github.com/tkrajina/git-plus.git"
 
   bottle do
