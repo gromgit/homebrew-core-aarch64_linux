de358880f994f3398c5f053a72f99e183f1f9b26
Rui Chen <chenrui333@gmail.com>
2020-03-10 03:11:16 -0400

    kallisto 0.46.2
    Closes #51469.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/kallisto.rb
+++ Formula/kallisto.rb
@@ -1,9 +1,8 @@
 class Kallisto < Formula
   desc "Quantify abundances of transcripts from RNA-Seq data"
   homepage "https://pachterlab.github.io/kallisto/"
-  url "https://github.com/pachterlab/kallisto/archive/v0.46.1.tar.gz"
-  sha256 "492ef081395e8858fcd9832aceb8b61c79358f00afb45e6709146c0fb51dd231"
-  revision 1
+  url "https://github.com/pachterlab/kallisto/archive/v0.46.2.tar.gz"
+  sha256 "c447ca8ddc40fcbd7d877d7c868bc8b72807aa8823a8a8d659e19bdd515baaf2"
 
   bottle do
     cellar :any
