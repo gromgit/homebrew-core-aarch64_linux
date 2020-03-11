e4a6fca39563ecb077ae24180c69b77a492ac556
chenrui <chenrui333@gmail.com>
2020-03-10 20:23:51 -0400

    micronaut 1.3.3 (#51512)

--- Formula/micronaut.rb
+++ Formula/micronaut.rb
@@ -1,8 +1,8 @@
 class Micronaut < Formula
   desc "Modern JVM-based framework for building modular microservices"
   homepage "https://micronaut.io/"
-  url "https://github.com/micronaut-projects/micronaut-core/releases/download/v1.3.2/micronaut-1.3.2.zip"
-  sha256 "05b61a19b74fa4edc0e29d17211ff39711ed7767944392b8cce04b7f35f7826e"
+  url "https://github.com/micronaut-projects/micronaut-core/releases/download/v1.3.3/micronaut-1.3.3.zip"
+  sha256 "143c044ce11856188a43a95eff82f84f32b2b17548a8fb263a6b0cd5cd518e9c"
 
   bottle :unneeded
 
