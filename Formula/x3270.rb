c5b9a862c7924737e3854e6903680ecf8ffa7c9e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-11 05:15:33 +0000

    x3270: update 3.6ga9 bottle.

--- Formula/x3270.rb
+++ Formula/x3270.rb
@@ -5,10 +5,9 @@ class X3270 < Formula
   sha256 "674689a12d09ed9097d9d62a2422e65bf6d40c06dd217f0d618fb7e3673b7568"
 
   bottle do
-    sha256 "89d86a2b2bd54b97779d62ce7254f229dc7bde386ecea33487dcf3ef62523f6c" => :catalina
-    sha256 "548f8a42e7d672bb6235aa20852e48d9d6a8d09be07017b57f859bcb1e69872a" => :mojave
-    sha256 "99c09d8e8adb73201ce8d3f806a82c0e926deb0a7ca6136086bcbedebce42ae3" => :high_sierra
-    sha256 "02656ba9619eb9b2bec85d17d542ab9e80e9d9b53932d9ec128bb04a3bdfad25" => :sierra
+    sha256 "4a688250d91e04d91aa9c0983f630fe0b682d35769387e4fa490f03f977be7fb" => :catalina
+    sha256 "c5448a7a24aa77b54c9473ea62b4aeac4c87930e79bc7a229dea02427d87c1fe" => :mojave
+    sha256 "962a25eb052b7f3f2a8c01fd1f2d0e90c5e70fd91ac7c63a038a4fd710661557" => :high_sierra
   end
 
   depends_on "readline"
