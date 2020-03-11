150cb1ba1db7afcad774209eb84a97ae6ff3398c
Rui Chen <chenrui333@gmail.com>
2020-03-10 22:37:07 -0400

    sql-translator 1.60
    Closes #51536.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/sql-translator.rb
+++ Formula/sql-translator.rb
@@ -1,8 +1,8 @@
 class SqlTranslator < Formula
   desc "Manipulate structured data definitions (SQL and more)"
   homepage "https://github.com/dbsrgits/sql-translator/"
-  url "https://cpan.metacpan.org/authors/id/I/IL/ILMARI/SQL-Translator-0.11024.tar.gz"
-  sha256 "5bde9d6f67850089ef35a9296d6f53e5ee8e991438366b71477f3f27c1581bb1"
+  url "https://cpan.metacpan.org/authors/id/I/IL/ILMARI/SQL-Translator-1.60.tar.gz"
+  sha256 "6bb0cb32ca25da69df65e5de71f679f3ca90044064526fa336cabd342f220e87"
 
   bottle do
     cellar :any_skip_relocation
@@ -13,15 +13,46 @@ class SqlTranslator < Formula
     sha256 "e90e93b46d07158b9221c55f3a95dc438a8adc0bf965492438a5dc6e66dad22d" => :el_capitan
   end
 
+  uses_from_macos "perl"
+
+  resource "File::ShareDir::Install" do
+    url "https://cpan.metacpan.org/authors/id/E/ET/ETHER/File-ShareDir-Install-0.13.tar.gz"
+    sha256 "45befdf0d95cbefe7c25a1daf293d85f780d6d2576146546e6828aad26e580f9"
+  end
+
+  resource "Package::Variant" do
+    url "https://cpan.metacpan.org/authors/id/M/MS/MSTROUT/Package-Variant-1.003002.tar.gz"
+    sha256 "b2ed849d2f4cdd66467512daa3f143266d6df810c5fae9175b252c57bc1536dc"
+  end
+
+  resource "strictures" do
+    url "https://cpan.metacpan.org/authors/id/H/HA/HAARG/strictures-2.000006.tar.gz"
+    sha256 "09d57974a6d1b2380c802870fed471108f51170da81458e2751859f2714f8d57"
+  end
+
   def install
     ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
-    ENV["PERL_MM_OPT"] = "INSTALL_BASE=#{libexec}"
 
-    system "perl", "Makefile.PL", "--defaultdeps"
+    resources.each do |r|
+      r.stage do
+        system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+        system "make"
+        system "make", "install"
+      end
+    end
+
+    system "perl", "Makefile.PL", "--defaultdeps",
+                                  "INSTALL_BASE=#{libexec}",
+                                  "INSTALLSITESCRIPT=#{bin}",
+                                  "INSTALLSITEMAN1DIR=#{man1}",
+                                  "INSTALLSITEMAN3DIR=#{man3}"
     system "make", "install"
 
-    bin.install Dir["#{libexec}/bin/sqlt*"]
-    man1.install Dir["#{libexec}/man/man1/sqlt*.1"]
+    # Disable dynamic selection of perl which may cause segfault when an
+    # incompatible perl is picked up.
+    # https://github.com/Homebrew/homebrew-core/issues/4936
+    inreplace Dir["#{bin}/*"], "#!/usr/bin/env perl", "#!/usr/bin/perl"
+
     bin.env_script_all_files libexec/"bin", :PERL5LIB => ENV["PERL5LIB"]
   end
 
