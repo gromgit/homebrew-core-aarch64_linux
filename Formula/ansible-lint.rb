6de732beec790ef2bd196377d402b1012251ca6a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-11 09:37:24 +0000

    ansible-lint: update 4.2.0_2 bottle.

--- Formula/ansible-lint.rb
+++ Formula/ansible-lint.rb
@@ -9,9 +9,9 @@ class AnsibleLint < Formula
 
   bottle do
     cellar :any
-    sha256 "4bcb0e12b8186301c42be8fdbbedb50e26a5168324807cbc4223b71257f48472" => :catalina
-    sha256 "262b1e46d5b13ced6641be3eba16f31ce92219caaf89ee4ff0a5cb77fb19f3f6" => :mojave
-    sha256 "78465b1854e034eb68cea65f5b37b62e1b6ec07b2b45e8586c7d25039d35b39b" => :high_sierra
+    sha256 "3426e9ec96b4e938f4f0004d1323db89737f1f526fecee081771cf30e1689dd7" => :catalina
+    sha256 "6558f69553bf09ebdef982f05a57492c9fd14a9a0baaed61151d8d054e652e73" => :mojave
+    sha256 "83a905c6fedaabb38d7a53692d892ea9e2bd429d5a39b1c338dc2e26ea0d352f" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
