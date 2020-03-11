43cb6077867bc063e4da0082fa3e1ad57c80e342
Rui Chen <chenrui333@gmail.com>
2020-03-10 23:15:19 -0400

    txt2tags 3.7
    Closes #51560.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/txt2tags.rb
+++ Formula/txt2tags.rb
@@ -1,20 +1,24 @@
 class Txt2tags < Formula
+  include Language::Python::Virtualenv
+
   desc "Conversion tool to generating several file formats"
   homepage "https://txt2tags.org/"
-  url "https://github.com/txt2tags/txt2tags/archive/2.6.tar.gz"
-  sha256 "c87b911ac5cc97dd2fdb6067601cf4fbd0094a065f6b7a593142fa75e3a924c1"
-  revision 1
+  url "https://files.pythonhosted.org/packages/0e/80/dc4215b549ddbe1d1251bc4cd47ad6f4a65e1f9803815997817ff297d22e/txt2tags-3.7.tar.gz"
+  sha256 "27969387206d12b4e4a0eb13d0d5dd957d71dbb932451b0dceeab5e3dbb6178a"
 
-  bottle :unneeded
+  depends_on "python@3.8"
 
   def install
-    bin.install "txt2tags"
-    man1.install "doc/English/manpage.man" => "txt2tags.1"
+    venv = virtualenv_create(libexec, "python3")
+    system libexec/"bin/pip", "install", "-v", "--no-binary", ":all:",
+                              "--ignore-installed", buildpath
+    system libexec/"bin/pip", "uninstall", "-y", "txt2tags"
+    venv.pip_install_and_link buildpath
   end
 
   test do
     (testpath/"test.txt").write("\n= Title =")
     system bin/"txt2tags", "-t", "html", "--no-headers", "test.txt"
-    assert_match %r{<H1>Title</H1>}, File.read("test.html")
+    assert_match "<h1>Title</h1>", File.read("test.html")
   end
 end
