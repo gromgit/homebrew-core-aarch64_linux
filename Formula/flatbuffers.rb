c440bca48bc3a017642ff14eb23b266baddedde8
Rui Chen <chenrui333@gmail.com>
2020-03-12 19:15:12 -0400

    flatbuffers 1.12.0
    Closes #51665.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/flatbuffers.rb
+++ Formula/flatbuffers.rb
@@ -1,8 +1,8 @@
 class Flatbuffers < Formula
   desc "Serialization library for C++, supporting Java, C#, and Go"
   homepage "https://google.github.io/flatbuffers"
-  url "https://github.com/google/flatbuffers/archive/v1.11.0.tar.gz"
-  sha256 "3f4a286642094f45b1b77228656fbd7ea123964f19502f9ecfd29933fd23a50b"
+  url "https://github.com/google/flatbuffers/archive/v1.12.0.tar.gz"
+  sha256 "62f2223fb9181d1d6338451375628975775f7522185266cd5296571ac152bc45"
   head "https://github.com/google/flatbuffers.git"
 
   bottle do
