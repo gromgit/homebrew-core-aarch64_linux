c38434100b88ff133bb8fa31aae4f9d55085f567
Mike McQuaid <mike@mikemcquaid.com>
2020-03-12 20:15:58 +0000

    camlp5: shorten long lines.

--- Formula/camlp5.rb
+++ Formula/camlp5.rb
@@ -30,6 +30,7 @@ class Camlp5 < Formula
       # ocaml files are in sync with the camlp5 files.  If camlp5 has been
       # compiled with an older version of the ocaml compiler, then an error
       # "interface mismatch" will occur.
-      shell_output("#{bin}/camlp5 #{lib}/ocaml/camlp5/pa_o.cmo #{lib}/ocaml/camlp5/pr_o.cmo #{ocaml.opt_lib}/ocaml/bigarray.cma hi.ml")
+      shell_output("#{bin}/camlp5 #{lib}/ocaml/camlp5/pa_o.cmo #{lib}/ocaml/camlp5/pr_o.cmo " \
+                   "#{ocaml.opt_lib}/ocaml/bigarray.cma hi.ml")
   end
 end
