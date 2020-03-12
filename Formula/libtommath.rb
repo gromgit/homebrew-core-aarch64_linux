6402c7ca904c95e233fa3246daa2e824631ac04c
Bo Anderson <mail@boanderson.me>
2020-03-12 15:17:29 +0000

    libtommath: patch missing mp_set_double on macOS
    Closes #51498.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/libtommath.rb
+++ Formula/libtommath.rb
@@ -3,6 +3,7 @@ class Libtommath < Formula
   homepage "https://www.libtom.net/LibTomMath/"
   url "https://github.com/libtom/libtommath/releases/download/v1.2.0/ltm-1.2.0.tar.xz"
   sha256 "b7c75eecf680219484055fcedd686064409254ae44bc31a96c5032843c0e18b1"
+  revision 1
   head "https://github.com/libtom/libtommath.git"
 
   bottle do
@@ -10,6 +11,16 @@ class Libtommath < Formula
     sha256 "74f8fc0290933dde4d0d8c804707bc4dc421c9f88b09c91f8acb140aafd225a8" => :catalina
     sha256 "e827065d024c0699ca3672e1d806377833d46a7fde999666b097cfb7a31c625a" => :mojave
     sha256 "9affad0e48cc7801adcd196813c5d1271fcb2ba94a50169fa633d4ec7804eb2d" => :high_sierra
+  end
+
+  # Fixes mp_set_double being missing on macOS.
+  # This is needed by some dependents in homebrew-core.
+  # Note: this patch has been merged upstream but we take a backport
+  # from a fork due to file name differences between 1.2.0 and master.
+  # Remove with the next version.
+  patch do
+    url "https://github.com/MoarVM/libtommath/commit/db0d387b808d557bd408a6a253c5bf3a688ef274.patch?full_index=1"
+    sha256 "e5eef1762dd3e92125e36034afa72552d77f066eaa19a0fd03cd4f1a656f9ab0"
   end
 
   def install
