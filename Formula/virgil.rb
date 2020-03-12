16dca75c3365ee250a5a4e1aff07fdfae0a99ef8
Imelstorm <imelstorm.cm@gmail.com>
2020-03-12 18:50:45 +0200

    virgil 5.2.4
    Closes #51162.
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/virgil.rb
+++ Formula/virgil.rb
@@ -2,8 +2,8 @@ class Virgil < Formula
   desc "CLI tool to manage your Virgil account and applications"
   homepage "https://github.com/VirgilSecurity/virgil-cli"
   url "https://github.com/VirgilSecurity/virgil-cli.git",
-     :tag      => "v5.1.7",
-     :revision => "bb86ae1102725e51c85d79bf6babc424be04126d"
+     :tag      => "v5.2.4",
+     :revision => "46ad6eba489482a5eaff5ba89424f822525a6063"
   head "https://github.com/VirgilSecurity/virgil-cli.git"
 
   bottle do
@@ -13,22 +13,15 @@ class Virgil < Formula
     sha256 "ab03a53e7f602341811f1ce6af9ff6b41ccf84ca879f59916fe4f59ff3baca64" => :high_sierra
   end
 
-  depends_on "dep" => :build
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    dir = buildpath/"src/github.com/VirgilSecurity/virgil-cli"
-    dir.install buildpath.children - [buildpath/".brew_home"]
-    cd dir do
-      system "dep", "ensure", "-vendor-only"
-      system "go", "build", "-o", "virgil"
-      bin.install "virgil"
-    end
+    system "make"
+    bin.install "virgil"
   end
 
   test do
-    result = shell_output "#{bin}/virgil pure keygen"
+    result = shell_output "#{bin}/virgil purekit keygen"
     assert_match /SK.1./, result
   end
 end
