aa79a707da882e68969fa96a170df9cd1d355dc8
EricFromCanada <enk3@outlook.com>
2020-03-12 14:07:50 -0400

    luit 20190106
    Closes #51657.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/luit.rb
+++ Formula/luit.rb
@@ -1,8 +1,8 @@
 class Luit < Formula
   desc "Filter run between arbitrary application and UTF-8 terminal emulator"
   homepage "https://invisible-island.net/luit/"
-  url "https://invisible-mirror.net/archives/luit/luit-20180628.tgz"
-  sha256 "7b84f63072589e9b03bb3e99e01ef344bb37793b76ad1cbb7b11f05000d64844"
+  url "https://invisible-mirror.net/archives/luit/luit-20190106.tgz"
+  sha256 "2b900f65ccdc38f8bfc11c6020069d055ba63fce6f90baefe8efc222a5ca3920"
 
   bottle do
     cellar :any_skip_relocation
