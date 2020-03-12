c8a635e9525532e16bb539e98e78ba6aeb4aed7b
Mike McQuaid <mike@mikemcquaid.com>
2020-03-12 20:15:58 +0000

    carrot2: shorten long lines.

--- Formula/carrot2.rb
+++ Formula/carrot2.rb
@@ -45,8 +45,9 @@ class Carrot2 < Formula
     begin
       pid = fork { exec testpath/"bin/carrot2" }
       sleep 5
-      output = shell_output("curl -s -F dcs.c2stream=@#{libexec}/examples/shared/data-mining.xml http://localhost:8080/dcs/rest")
-      assert_match /data mining/m, output
+      assert_match /data mining/m,
+        shell_output("curl -s -F dcs.c2stream=@#{libexec}/examples/shared/data-mining.xml " \
+                     "http://localhost:8080/dcs/rest")
     ensure
       Process.kill "INT", pid
     end
