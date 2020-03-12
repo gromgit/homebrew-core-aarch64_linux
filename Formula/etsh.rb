ca4b183a882b15ebeb2f2e00459e07e6b63a8be9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-12 01:49:58 +0000

    etsh: update 5.4.0 bottle.

--- Formula/etsh.rb
+++ Formula/etsh.rb
@@ -6,10 +6,9 @@ class Etsh < Formula
   version_scheme 1
 
   bottle do
-    sha256 "0fc00861a2a9e210f8ad7a1aad38a50375583299ea6a9e338d9fdd9d8f168e92" => :catalina
-    sha256 "ee5cb69606c9c472cf0ab618e886a0f238a9e044d49cc54d97d4d806e58738c5" => :mojave
-    sha256 "875b9f0c5cd602f8a40b31d2eb7134a3fe045c0d53a5535b4b8ab5ab4cf09806" => :high_sierra
-    sha256 "31219a25086c96ac502d6def1dd108dcf815f62a4a204248a10cbc698b1ab75a" => :sierra
+    sha256 "1bb2f2a1cdb069e4963cba22c6014894a61853644e840341e8fd01f1ca522ea2" => :catalina
+    sha256 "61739a70a6927e119b9f27fe51e24a5bd14f3c5f8cfed1888d1f00682e68c9c8" => :mojave
+    sha256 "dbe3c9f5881aa417660aec6e9469123dde475b33551f7207cb3cb7aaade8c16d" => :high_sierra
   end
 
   conflicts_with "teleport", :because => "both install `tsh` binaries"
