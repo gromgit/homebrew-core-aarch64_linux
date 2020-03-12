d94ffc476980e41a9c40d676230849b035071b28
Rui Chen <chenrui333@gmail.com>
2020-03-12 13:31:43 -0400

    imgproxy 2.11.0
    Closes #51656.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/imgproxy.rb
+++ Formula/imgproxy.rb
@@ -1,9 +1,8 @@
 class Imgproxy < Formula
   desc "Fast and secure server for resizing and converting remote images"
   homepage "https://imgproxy.net"
-  url "https://github.com/imgproxy/imgproxy/archive/v2.10.1.tar.gz"
-  sha256 "3d9458dff70f6ca7dec54996aad7115ffdb0315323814a3a942f773f770b7bc9"
-  revision 1
+  url "https://github.com/imgproxy/imgproxy/archive/v2.11.0.tar.gz"
+  sha256 "5de08bdfb180da030a8a342eed5ed29e031aef6d9ac7f10e5df94cec2ac42a5c"
   head "https://github.com/imgproxy/imgproxy.git"
 
   bottle do
