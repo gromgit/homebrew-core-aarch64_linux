9145a7373ec0d98d9e6b9a2c5cca274de70778f7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-12 23:24:40 +0000

    sdl2: update 2.0.12 bottle.

--- Formula/sdl2.rb
+++ Formula/sdl2.rb
@@ -6,10 +6,9 @@ class Sdl2 < Formula
 
   bottle do
     cellar :any
-    sha256 "dc47470e4dc13c7a0cf48330da2d4938695a5b60bf9d6ef31218f32a803ab48c" => :catalina
-    sha256 "ede7f2495e32fd7f1a1bee57db35542066993f7ad4d24bb4c2078a68b0856dfb" => :mojave
-    sha256 "e8e7c8484eaa791d312f9d77f626faad344f90db74bb93423ed2fc234c5f52a6" => :high_sierra
-    sha256 "67961a420c2a8632822f60f61710b95b1173b8b1c2ae05b5f92e4c9892cdc5b7" => :sierra
+    sha256 "f01b56251f8798f687fc536546a66e18688dc7c1189e83324951e5638ae33657" => :catalina
+    sha256 "9e82713279fc1fb81059f1c1070e60ffa6e3f085adc604aaf889a10222a1a7cb" => :mojave
+    sha256 "40682e8ed7520c51b598833fd0600b7a0a3c4d027d01ec1ea54a1f119a286b52" => :high_sierra
   end
 
   head do
