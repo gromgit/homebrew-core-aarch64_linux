cf101353ab945c4f6567e9a0b55000be27bb35d3
Rui Chen <chenrui333@gmail.com>
2020-03-11 20:43:06 -0400

    wapm-cli 0.5.0
    Closes #51624.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/wapm.rb
+++ Formula/wapm.rb
@@ -1,8 +1,8 @@
 class Wapm < Formula
   desc "WebAssembly Package Manager (CLI)"
   homepage "https://wapm.io/"
-  url "https://github.com/wasmerio/wapm-cli/archive/0.4.3.tar.gz"
-  sha256 "0a4057217a539a013549fd2bf3913bff28f0ec01d6606ebc278c2057c6e268a2"
+  url "https://github.com/wasmerio/wapm-cli/archive/v0.5.0.tar.gz"
+  sha256 "7731d476585105fbb0ac5766661b4b68f1680b7071635654042bdaeef3b66987"
   head "https://github.com/wasmerio/wapm-cli.git"
 
   bottle do
