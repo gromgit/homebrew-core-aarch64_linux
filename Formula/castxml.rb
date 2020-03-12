376c50f047ea0a5e26879debbe8a23140d560bba
Mike McQuaid <mike@mikemcquaid.com>
2020-03-12 20:15:59 +0000

    castxml: shorten long lines.

--- Formula/castxml.rb
+++ Formula/castxml.rb
@@ -29,6 +29,7 @@ class Castxml < Formula
         return 0;
       }
     EOS
-    system "#{bin}/castxml", "-c", "-x", "c++", "--castxml-cc-gnu", "clang++", "--castxml-gccxml", "-o", "test.xml", "test.cpp"
+    system "#{bin}/castxml", "-c", "-x", "c++", "--castxml-cc-gnu", "clang++",
+                             "--castxml-gccxml", "-o", "test.xml", "test.cpp"
   end
 end
