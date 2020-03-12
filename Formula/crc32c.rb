40bcbbc8d81c1f2f18ba6da61e9a04710ca7abac
Mike McQuaid <mike@mikemcquaid.com>
2020-03-12 20:23:04 +0000

    crc32c: shorten long lines.

--- Formula/crc32c.rb
+++ Formula/crc32c.rb
@@ -15,10 +15,14 @@ class Crc32c < Formula
   depends_on "cmake" => :build
 
   def install
-    system "cmake", ".", "-DCRC32C_BUILD_TESTS=0", "-DCRC32C_BUILD_BENCHMARKS=0", "-DCRC32C_USE_GLOG=0", *std_cmake_args
+    system "cmake", ".", "-DCRC32C_BUILD_TESTS=0",
+                          "-DCRC32C_BUILD_BENCHMARKS=0", "-DCRC32C_USE_GLOG=0",
+                         *std_cmake_args
     system "make", "install"
     system "make", "clean"
-    system "cmake", ".", "-DBUILD_SHARED_LIBS=ON", "-DCRC32C_BUILD_TESTS=0", "-DCRC32C_BUILD_BENCHMARKS=0", "-DCRC32C_USE_GLOG=0", *std_cmake_args
+    system "cmake", ".", "-DBUILD_SHARED_LIBS=ON", "-DCRC32C_BUILD_TESTS=0",
+                         "-DCRC32C_BUILD_BENCHMARKS=0", "-DCRC32C_USE_GLOG=0",
+                         *std_cmake_args
     system "make", "install"
   end
 
