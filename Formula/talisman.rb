66344505ea7e9e2227fc53986c15ae6bf0999a84
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-12 14:15:45 +0000

    talisman: update 1.2.0 bottle.

--- Formula/talisman.rb
+++ Formula/talisman.rb
@@ -8,9 +8,9 @@ class Talisman < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c4440913a564ca0f032eaa0c385c13e71f425a41d7aff52506b90118984b8e32" => :catalina
-    sha256 "c2c7e787dcd4cabe6b278ee40e122c2933978aa0518de20f4fc49a4ff59a06c2" => :mojave
-    sha256 "c44fa4bfe8e660829cf927cdb847b8d7024823e3881f14d41174368af48bdf14" => :high_sierra
+    sha256 "701e360c802243d9bdbd15546b02c33ea6fb452a6febde5ce58592e5441f085b" => :catalina
+    sha256 "fd9a61d296fc9170231f9392af0c9ffe5b7ef799ab84b65664ea15e13a77adfa" => :mojave
+    sha256 "708f657afddb95aef4d707ec18735e6d07b1abf2202a6fcaceafdfeca30c6436" => :high_sierra
   end
 
   depends_on "go" => :build
