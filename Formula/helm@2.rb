b300f45df9d3da7cfe80c47f4f9e4e48f00e7e40
Mike McQuaid <mike@mikemcquaid.com>
2020-03-12 20:54:53 +0000

    helm@2: autofix rubocop Style/IfUnlessModifier.

--- Formula/helm@2.rb
+++ Formula/helm@2.rb
@@ -49,6 +49,8 @@ class HelmAT2 < Formula
 
     version_output = shell_output("#{bin}/helm version --client 2>&1")
     assert_match "GitTreeState:\"clean\"", version_output
-    assert_match stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision], version_output if build.stable?
+    if build.stable?
+      assert_match stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision], version_output
+    end
   end
 end
