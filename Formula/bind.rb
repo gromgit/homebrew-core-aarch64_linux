c8447cfa969658acad1730810f9e51c73b0f2881
Mike McQuaid <mike@mikemcquaid.com>
2020-03-12 20:54:49 +0000

    bind: autofix rubocop Style/IfUnlessModifier.

--- Formula/bind.rb
+++ Formula/bind.rb
@@ -41,9 +41,7 @@ class Bind < Formula
     end
 
     # Fix "configure: error: xml2-config returns badness"
-    if MacOS.version == :sierra || MacOS.version == :el_capitan
-      ENV["SDKROOT"] = MacOS.sdk_path
-    end
+    ENV["SDKROOT"] = MacOS.sdk_path if MacOS.version == :sierra || MacOS.version == :el_capitan
 
     system "./configure", "--prefix=#{prefix}",
                           "--with-json-c",
