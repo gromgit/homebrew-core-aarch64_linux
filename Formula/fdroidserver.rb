27297838c832ff41d24596616a835b2d975e7a42
Mike McQuaid <mike@mikemcquaid.com>
2020-03-12 20:54:51 +0000

    fdroidserver: autofix rubocop Style/IfUnlessModifier.

--- Formula/fdroidserver.rb
+++ Formula/fdroidserver.rb
@@ -347,7 +347,9 @@ class Fdroidserver < Formula
 
       # avoid triggering "helpful" distutils code that doesn't recognize Xcode 7 .tbd stubs
       ENV.delete "SDKROOT"
-      ENV.append "CFLAGS", "-I#{MacOS.sdk_path}/System/Library/Frameworks/Tk.framework/Versions/8.5/Headers" unless MacOS::CLT.installed?
+      unless MacOS::CLT.installed?
+        ENV.append "CFLAGS", "-I#{MacOS.sdk_path}/System/Library/Frameworks/Tk.framework/Versions/8.5/Headers"
+      end
       venv.pip_install Pathname.pwd
     end
 
