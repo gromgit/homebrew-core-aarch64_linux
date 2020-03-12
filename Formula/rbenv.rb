63f6b2c28dddeda5aea7c837bb51a44374f672f6
Mike McQuaid <mike@mikemcquaid.com>
2020-03-12 20:55:00 +0000

    rbenv: autofix rubocop Style/IfUnlessModifier.

--- Formula/rbenv.rb
+++ Formula/rbenv.rb
@@ -20,9 +20,7 @@ class Rbenv < Formula
   def install
     inreplace "libexec/rbenv" do |s|
       s.gsub! '"${BASH_SOURCE%/*}"/../libexec', libexec
-      if HOMEBREW_PREFIX.to_s != "/usr/local"
-        s.gsub! ":/usr/local/etc/rbenv.d", ":#{HOMEBREW_PREFIX}/etc/rbenv.d\\0"
-      end
+      s.gsub! ":/usr/local/etc/rbenv.d", ":#{HOMEBREW_PREFIX}/etc/rbenv.d\\0" if HOMEBREW_PREFIX.to_s != "/usr/local"
     end
 
     # Compile optional bash extension.
