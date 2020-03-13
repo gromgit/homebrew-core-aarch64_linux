99d037b6913f36dc33007f0e1cce8fb402c64e55
Mike McQuaid <mike@mikemcquaid.com>
2020-03-13 19:43:28 +0000

    v2ray-plugin: shorten long lines.

--- Formula/v2ray-plugin.rb
+++ Formula/v2ray-plugin.rb
@@ -19,8 +19,12 @@ class V2rayPlugin < Formula
   end
 
   test do
-    server = fork { exec bin/"v2ray-plugin", "-localPort", "54000", "-remoteAddr", "github.com", "-remotePort", "80", "-server" }
-    client = fork { exec bin/"v2ray-plugin", "-localPort", "54001", "-remotePort", "54000" }
+    server = fork do
+      exec bin/"v2ray-plugin", "-localPort", "54000", "-remoteAddr", "github.com", "-remotePort", "80", "-server"
+    end
+    client = fork do
+      exec bin/"v2ray-plugin", "-localPort", "54001", "-remotePort", "54000"
+    end
     sleep 2
     begin
       system "curl", "localhost:54001"
