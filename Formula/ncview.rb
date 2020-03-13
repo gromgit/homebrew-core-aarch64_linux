850ba2943384d92274fa459b5625f19a0e300869
Viktor Szakats <commit@vsz.me>
2020-03-13 14:06:29 +0000

    ncview: fix/improve homepage

--- Formula/ncview.rb
+++ Formula/ncview.rb
@@ -1,6 +1,6 @@
 class Ncview < Formula
   desc "Visual browser for netCDF format files"
-  homepage "https://web.archive.org/web/20190806213507/meteora.ucsd.edu/~pierce/ncview_home_page.html"
+  homepage "https://cirrus.ucsd.edu/ncview/"
   url "ftp://cirrus.ucsd.edu/pub/ncview/ncview-2.1.8.tar.gz"
   mirror "https://dl.bintray.com/homebrew/mirror/ncview--2.1.8.tar.gz"
   sha256 "e8badc507b9b774801288d1c2d59eb79ab31b004df4858d0674ed0d87dfc91be"
