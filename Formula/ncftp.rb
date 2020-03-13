c997542ac10dec6107ba3b40d43c9b72f6526df2
Viktor Szakats <commit@vsz.me>
2020-03-13 14:06:29 +0000

    ncftp: secure url

--- Formula/ncftp.rb
+++ Formula/ncftp.rb
@@ -1,7 +1,7 @@
 class Ncftp < Formula
   desc "FTP client with an advanced user interface"
   homepage "https://www.ncftp.com/"
-  url "ftp://ftp.ncftp.com/ncftp/ncftp-3.2.6-src.tar.gz"
+  url "https://mirrorservice.org/sites/ftp.ncftp.com/ncftp/ncftp-3.2.6-src.tar.gz"
   mirror "https://fossies.org/linux/misc/ncftp-3.2.6-src.tar.gz"
   sha256 "129e5954850290da98af012559e6743de193de0012e972ff939df9b604f81c23"
 
