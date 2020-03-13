dfe0ce2556efaa6a05352ef07f22a75bfbb81ce3
Mike McQuaid <mike@mikemcquaid.com>
2020-03-13 19:43:24 +0000

    sonarqube: shorten long lines.

--- Formula/sonarqube.rb
+++ Formula/sonarqube.rb
@@ -16,7 +16,8 @@ class Sonarqube < Formula
 
     libexec.install Dir["*"]
 
-    (bin/"sonar").write_env_script libexec/"bin/macosx-universal-64/sonar.sh", :JAVA_HOME => Formula["openjdk"].opt_prefix
+    (bin/"sonar").write_env_script libexec/"bin/macosx-universal-64/sonar.sh",
+      :JAVA_HOME => Formula["openjdk"].opt_prefix
   end
 
   plist_options :manual => "sonar console"
