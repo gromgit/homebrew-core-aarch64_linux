20180850f9efdb501d71f47f64508244fc2793c1
Mike McQuaid <mike@mikemcquaid.com>
2020-03-13 19:43:25 +0000

    sqliteodbc: shorten long lines.

--- Formula/sqliteodbc.rb
+++ Formula/sqliteodbc.rb
@@ -23,7 +23,9 @@ class Sqliteodbc < Formula
     ENV["SDKROOT"] = MacOS.sdk_path if MacOS.version == :sierra
 
     lib.mkdir
-    system "./configure", "--prefix=#{prefix}", "--with-odbc=#{Formula["unixodbc"].opt_prefix}", "--with-sqlite3=#{Formula["sqlite"].opt_prefix}"
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-odbc=#{Formula["unixodbc"].opt_prefix}",
+                          "--with-sqlite3=#{Formula["sqlite"].opt_prefix}"
     system "make"
     system "make", "install"
     lib.install_symlink "#{lib}/libsqlite3odbc.dylib" => "libsqlite3odbc.so"
