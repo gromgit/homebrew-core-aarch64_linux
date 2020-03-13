bcdd085805a8bbc28317e7ec0b0c2d214b4102ba
Mike McQuaid <mike@mikemcquaid.com>
2020-03-13 16:49:18 +0000

    nailgun: shorten long lines.

--- Formula/nailgun.rb
+++ Formula/nailgun.rb
@@ -27,7 +27,11 @@ class Nailgun < Formula
     system "make", "install", "CC=#{ENV.cc}", "PREFIX=#{prefix}", "CFLAGS=#{ENV.cflags}"
     require "rexml/document"
     pom_xml = REXML::Document.new(File.new("pom.xml"))
-    jar_version = REXML::XPath.first(pom_xml, "string(/pom:project/pom:version)", "pom" => "http://maven.apache.org/POM/4.0.0")
+    jar_version = REXML::XPath.first(
+      pom_xml,
+      "string(/pom:project/pom:version)",
+      "pom" => "http://maven.apache.org/POM/4.0.0",
+    )
     system "mvn", "clean", "install"
     libexec.install Dir["nailgun-server/target/*.jar"]
     bin.write_jar_script libexec/"nailgun-server-#{jar_version}.jar", "ng-server", "-server"
