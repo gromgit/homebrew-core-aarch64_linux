b9e8079bcebc16e749cb7d4a6ab0e6af293e0c0f
Viktor Szakats <commit@vsz.me>
2020-03-13 10:12:47 +0000

    dmalloc: secure urls

--- Formula/dmalloc.rb
+++ Formula/dmalloc.rb
@@ -1,7 +1,7 @@
 class Dmalloc < Formula
   desc "Debug versions of system memory management routines"
-  homepage "http://dmalloc.com"
-  url "http://dmalloc.com/releases/dmalloc-5.5.2.tgz"
+  homepage "https://dmalloc.com/"
+  url "https://dmalloc.com/releases/dmalloc-5.5.2.tgz"
   sha256 "d3be5c6eec24950cb3bd67dbfbcdf036f1278fae5fd78655ef8cdf9e911e428a"
 
   bottle do
