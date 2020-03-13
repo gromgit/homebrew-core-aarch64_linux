a3ea5d5093fe2467035379dbfebb12962e87e62f
Viktor Szakats <commit@vsz.me>
2020-03-13 10:12:47 +0000

    assimp: secure homepage

--- Formula/assimp.rb
+++ Formula/assimp.rb
@@ -1,6 +1,6 @@
 class Assimp < Formula
   desc "Portable library for importing many well-known 3D model formats"
-  homepage "http://www.assimp.org"
+  homepage "https://www.assimp.org/"
   url "https://github.com/assimp/assimp/archive/v5.0.1.tar.gz"
   sha256 "11310ec1f2ad2cd46b95ba88faca8f7aaa1efe9aa12605c55e3de2b977b3dbfc"
   head "https://github.com/assimp/assimp.git"
