8103ca769e8f368c67bd640330e767e43cbae958
Viktor Szakats <commit@vsz.me>
2020-03-13 10:12:47 +0000

    cgoban: fix urls

--- Formula/cgoban.rb
+++ Formula/cgoban.rb
@@ -1,7 +1,7 @@
 class Cgoban < Formula
   desc "Go-related services"
-  homepage "http://www.igoweb.org/~wms/comp/cgoban/index.html"
-  url "http://www.igoweb.org/~wms/comp/cgoban/cgoban-1.9.12.tar.gz"
+  homepage "https://cgoban1.sourceforge.io/"
+  url "https://web.archive.org/web/20190926163208/www.igoweb.org/~wms/comp/cgoban/cgoban-1.9.12.tar.gz"
   sha256 "b9e8b0d2f793fecbc26803d673de11d8cdc88af9d286a6d49b7523f8b4fa20e1"
 
   bottle do
