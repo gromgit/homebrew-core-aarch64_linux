42cc8e9734d9a488dc774bd7b93e898ac4fd8907
Mike McQuaid <mike@mikemcquaid.com>
2020-03-13 20:59:19 +0000

    python: fix RuboCop Style/ConditionalAssignment.

--- Formula/python.rb
+++ Formula/python.rb
@@ -288,10 +288,10 @@ class Python < Formula
   end
 
   def caveats
-    if prefix.exist?
-      xy = (prefix/"Frameworks/Python.framework/Versions").children.min.basename.to_s
+    xy = if prefix.exist?
+      (prefix/"Frameworks/Python.framework/Versions").children.min.basename.to_s
     else
-      xy = version.to_s.slice(/(3\.\d)/) || "3.7"
+      version.to_s.slice(/(3\.\d)/) || "3.7"
     end
     <<~EOS
       Python has been installed as
