812f895592b2a6992c66b614beb1792c7e273ae9
Mike McQuaid <mike@mikemcquaid.com>
2020-03-13 14:59:27 +0000

    gambit-scheme: shorten long lines.

--- Formula/gambit-scheme.rb
+++ Formula/gambit-scheme.rb
@@ -36,6 +36,7 @@ class GambitScheme < Formula
   end
 
   test do
-    assert_equal "0123456789", shell_output("#{prefix}/current/bin/gsi -e \"(for-each write '(0 1 2 3 4 5 6 7 8 9))\"")
+    assert_equal "0123456789",
+      shell_output("#{prefix}/current/bin/gsi -e \"(for-each write '(0 1 2 3 4 5 6 7 8 9))\"")
   end
 end
