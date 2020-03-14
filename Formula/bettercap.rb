af4e85cdcc8885ce847fa1a6188a767a2a432830
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:44:33 +0000

    bettercap: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/bettercap.rb
+++ Formula/bettercap.rb
@@ -29,10 +29,11 @@ class Bettercap < Formula
     end
   end
 
-  def caveats; <<~EOS
-    bettercap requires root privileges so you will need to run `sudo bettercap`.
-    You should be certain that you trust any software you grant root privileges.
-  EOS
+  def caveats
+    <<~EOS
+      bettercap requires root privileges so you will need to run `sudo bettercap`.
+      You should be certain that you trust any software you grant root privileges.
+    EOS
   end
 
   test do
