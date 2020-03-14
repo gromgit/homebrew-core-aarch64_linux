79688a355dad4ea49bd480c549787fce18c45363
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:57 +0000

    syncthing-inotify: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/syncthing-inotify.rb
+++ Formula/syncthing-inotify.rb
@@ -29,30 +29,31 @@ class SyncthingInotify < Formula
 
   plist_options :manual => "syncthing-inotify"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>KeepAlive</key>
-        <true/>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/syncthing-inotify</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>ProcessType</key>
-        <string>Background</string>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/syncthing-inotify.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/syncthing-inotify.log</string>
-      </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>KeepAlive</key>
+          <true/>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/syncthing-inotify</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>ProcessType</key>
+          <string>Background</string>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/syncthing-inotify.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/syncthing-inotify.log</string>
+        </dict>
+      </plist>
+    EOS
   end
 
   test do
