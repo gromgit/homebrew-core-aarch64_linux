c5e913d383baec1c1099358b3ecf3fc0d712d8c3
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:44:36 +0000

    carrot2: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/carrot2.rb
+++ Formula/carrot2.rb
@@ -16,27 +16,28 @@ class Carrot2 < Formula
 
   plist_options :manual => "carrot2"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
-    "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>AbandonProcessGroup</key>
-        <true/>
-        <key>WorkingDirectory</key>
-        <string>#{opt_libexec}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/carrot2</string>
-        </array>
-      </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
+      "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>AbandonProcessGroup</key>
+          <true/>
+          <key>WorkingDirectory</key>
+          <string>#{opt_libexec}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/carrot2</string>
+          </array>
+        </dict>
+      </plist>
+    EOS
   end
 
   test do
