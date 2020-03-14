49e45b45882c14d70a6b140f3c398d8f0d616fea
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:29 +0000

    nzbget: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/nzbget.rb
+++ Formula/nzbget.rb
@@ -48,27 +48,28 @@ class Nzbget < Formula
 
   plist_options :manual => "nzbget"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/nzbget</string>
-        <string>-s</string>
-        <string>-o</string>
-        <string>OutputMode=Log</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>KeepAlive</key>
-      <true/>
-    </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/nzbget</string>
+          <string>-s</string>
+          <string>-o</string>
+          <string>OutputMode=Log</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+      </dict>
+      </plist>
+    EOS
   end
 
   test do
