4c67362caa9126f6070e9cb9de9600be890469cd
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:44:47 +0000

    dwm: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/dwm.rb
+++ Formula/dwm.rb
@@ -27,16 +27,17 @@ class Dwm < Formula
     system "make", "PREFIX=#{prefix}", "install"
   end
 
-  def caveats; <<~EOS
-    In order to use the Mac OS X command key for dwm commands,
-    change the X11 keyboard modifier map using xmodmap (1).
+  def caveats
+    <<~EOS
+      In order to use the Mac OS X command key for dwm commands,
+      change the X11 keyboard modifier map using xmodmap (1).
 
-    e.g. by running the following command from $HOME/.xinitrc
-    xmodmap -e 'remove Mod2 = Meta_L' -e 'add Mod1 = Meta_L'&
+      e.g. by running the following command from $HOME/.xinitrc
+      xmodmap -e 'remove Mod2 = Meta_L' -e 'add Mod1 = Meta_L'&
 
-    See also https://gist.github.com/311377 for a handful of tips and tricks
-    for running dwm on Mac OS X.
-  EOS
+      See also https://gist.github.com/311377 for a handful of tips and tricks
+      for running dwm on Mac OS X.
+    EOS
   end
 
   test do
