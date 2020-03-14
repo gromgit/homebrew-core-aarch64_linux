87c057fa21d34a38310626c5c1cbd4e8c63d8018
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-14 01:50:31 +0000

    arpoison: update 0.7_1 bottle.

--- Formula/arpoison.rb
+++ Formula/arpoison.rb
@@ -7,14 +7,9 @@ class Arpoison < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "21f8aabff068436f9fd942ef7ec4fac4dda3f675efa2feaf3120283a6dded2f1" => :catalina
-    sha256 "85237acdcba092c948cd45c98ad0e9fdfea757d155e7a464a8379fee5b9420a8" => :mojave
-    sha256 "eb37399841022351ef5551de5a63ecfc93d061d03518d62f01852a1d8e663e4e" => :high_sierra
-    sha256 "2ed60cb186440c24eaf0c8e040acb89ba60cdd730c83f4c6793add25c80a67ca" => :sierra
-    sha256 "33e496f9d1ca384ad23c50a1868fc2682352176d1cf5b37472299a9e36dc7e6c" => :el_capitan
-    sha256 "9d902ac3611dd0422783aecb7d46f39dd0278f65f5cab1aa99490fb527de5e22" => :yosemite
-    sha256 "0737a954fa5f4d6794f7a373b90b5d2b2008a0bf7cdc4e2fd51266485e86b983" => :mavericks
+    sha256 "550588e02ce0eb78b47d2d2f9e8b863c29761667aca72e4ad0c0810b13682d9b" => :catalina
+    sha256 "c97bb55590119dbda338a24e634f9089bb3e43889a810a7bece231d6304b7bcf" => :mojave
+    sha256 "ee2eedf6780546bcf4610984d36a773300c5528122d08b7873b640a51f76ee56" => :high_sierra
   end
 
   depends_on "libnet"
