dc70cc7d7224c3254282b82bc6a76e012dd7eda6
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:52 +0000

    sleepwatcher: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/sleepwatcher.rb
+++ Formula/sleepwatcher.rb
@@ -43,18 +43,19 @@ class Sleepwatcher < Formula
     prefix.install Dir["config/*.plist"]
   end
 
-  def caveats; <<~EOS
-    For SleepWatcher to work, you will need to read the following:
+  def caveats
+    <<~EOS
+      For SleepWatcher to work, you will need to read the following:
 
-      #{prefix}/ReadMe.rtf
+        #{prefix}/ReadMe.rtf
 
-    Ignore information about installing the binary and man page,
-    but read information regarding setup of the launchd files which
-    are installed here:
+      Ignore information about installing the binary and man page,
+      but read information regarding setup of the launchd files which
+      are installed here:
 
-      #{Dir["#{prefix}/*.plist"].join("\n      ")}
+        #{Dir["#{prefix}/*.plist"].join("\n      ")}
 
-    These are the examples provided by the author.
-  EOS
+      These are the examples provided by the author.
+    EOS
   end
 end
