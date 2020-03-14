07d1b7265fdd5734431ea5bfdbcf69fe2bb9c6bf
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:44:52 +0000

    freeswitch: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/freeswitch.rb
+++ Formula/freeswitch.rb
@@ -125,28 +125,29 @@ class Freeswitch < Formula
 
   plist_options :manual => "freeswitch -nc -nonat"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>KeepAlive</key>
-        <true/>
-      <key>Label</key>
-        <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/freeswitch</string>
-          <string>-nc</string>
-          <string>-nonat</string>
-        </array>
-      <key>RunAtLoad</key>
-        <true/>
-      <key>ServiceIPC</key>
-        <true/>
-    </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+          <true/>
+        <key>Label</key>
+          <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/freeswitch</string>
+            <string>-nc</string>
+            <string>-nonat</string>
+          </array>
+        <key>RunAtLoad</key>
+          <true/>
+        <key>ServiceIPC</key>
+          <true/>
+      </dict>
+      </plist>
+    EOS
   end
 
   test do
