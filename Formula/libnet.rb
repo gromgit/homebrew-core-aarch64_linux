d913a2916ede3bf3cbaca551fee25c93435fc287
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-14 01:50:31 +0000

    libnet: update 1.2 bottle.

--- Formula/libnet.rb
+++ Formula/libnet.rb
@@ -6,14 +6,9 @@ class Libnet < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "426becf4736494f39a32fae71916ad8ed2784c9a1f14d2a6b963d8f1c7ae2f9f" => :catalina
-    sha256 "acafac211b84c80292796e6ee62f0ab92ef047a771b0e76ad31b359cbaa7b936" => :mojave
-    sha256 "2adca799087317fa0c93f750239e8be5a746fc0369bd6e7bbb6bc2d79ebe5f5d" => :high_sierra
-    sha256 "2b31af371d3516aae63436e1c12b40f474fd69b1126e6d75bed9d4853fbd4ffc" => :sierra
-    sha256 "26a496e3607f2639592617769522a790259c834f91c05d91721331fe6f1ad0c4" => :el_capitan
-    sha256 "4203e91b8334689591d1dcec4e2f11625b035dbef078dd7f63121dbf3959e69b" => :yosemite
-    sha256 "fd35c44586c926e10d9cb616e2b33594cb553329735ff2fe9130adfa8ccf17da" => :mavericks
+    sha256 "0ecfbf2539a6e051ca8aa5962c0ee7cb57ffd173cf654b0eec8152c1a3fbf133" => :catalina
+    sha256 "cadba638a54f4d5646a3510439ab89317ed23df3c45b12704b78065bb127fbc4" => :mojave
+    sha256 "44e7b11e8f900f9d6f8e0d1a5deed99c46078dd2dbc997937f713ce5a1ac0f38" => :high_sierra
   end
 
   depends_on "doxygen" => :build
