dc9d494824b45fb7c983e97ecb6f847ee94afb84
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:36 +0000

    pgbadger: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/pgbadger.rb
+++ Formula/pgbadger.rb
@@ -21,23 +21,24 @@ class Pgbadger < Formula
     man1.install "usr/local/share/man/man1/pgbadger.1p"
   end
 
-  def caveats; <<~EOS
-    You must configure your PostgreSQL server before using pgBadger.
-    Edit postgresql.conf (in #{var}/postgres if you use Homebrew's
-    PostgreSQL), set the following parameters, and restart PostgreSQL:
+  def caveats
+    <<~EOS
+      You must configure your PostgreSQL server before using pgBadger.
+      Edit postgresql.conf (in #{var}/postgres if you use Homebrew's
+      PostgreSQL), set the following parameters, and restart PostgreSQL:
 
-      log_destination = 'stderr'
-      log_line_prefix = '%t [%p]: [%l-1] user=%u,db=%d '
-      log_statement = 'none'
-      log_duration = off
-      log_min_duration_statement = 0
-      log_checkpoints = on
-      log_connections = on
-      log_disconnections = on
-      log_lock_waits = on
-      log_temp_files = 0
-      lc_messages = 'C'
-  EOS
+        log_destination = 'stderr'
+        log_line_prefix = '%t [%p]: [%l-1] user=%u,db=%d '
+        log_statement = 'none'
+        log_duration = off
+        log_min_duration_statement = 0
+        log_checkpoints = on
+        log_connections = on
+        log_disconnections = on
+        log_lock_waits = on
+        log_temp_files = 0
+        lc_messages = 'C'
+    EOS
   end
 
   test do
