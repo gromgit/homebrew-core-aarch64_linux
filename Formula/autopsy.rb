95eaf3fc15f44ae12c4425c182c806ea2a1e325d
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:44:30 +0000

    autopsy: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/autopsy.rb
+++ Formula/autopsy.rb
@@ -11,39 +11,40 @@ class Autopsy < Formula
   # fixes weird configure script that wouldn't work nicely with homebrew
   patch :DATA
 
-  def autcfg; <<~EOS
-    # Autopsy configuration settings
+  def autcfg
+    <<~EOS
+      # Autopsy configuration settings
 
-    # when set to 1, the server will stop after it receives no
-    # connections for STIMEOUT seconds.
-    $USE_STIMEOUT = 0;
-    $STIMEOUT = 3600;
+      # when set to 1, the server will stop after it receives no
+      # connections for STIMEOUT seconds.
+      $USE_STIMEOUT = 0;
+      $STIMEOUT = 3600;
 
-    # number of seconds that child waits for input from client
-    $CTIMEOUT = 15;
+      # number of seconds that child waits for input from client
+      $CTIMEOUT = 15;
 
-    # set to 1 to save the cookie value in a file (for scripting)
-    $SAVE_COOKIE = 1;
+      # set to 1 to save the cookie value in a file (for scripting)
+      $SAVE_COOKIE = 1;
 
-    $INSTALLDIR = '#{prefix}';
-
-
-    # System Utilities
-    $GREP_EXE = '/usr/bin/grep';
-    $FILE_EXE = '/usr/bin/file';
-    $MD5_EXE = '/sbin/md5';
-    $SHA1_EXE = '/usr/bin/shasum';
+      $INSTALLDIR = '#{prefix}';
 
 
-    # Directories
-    $TSKDIR = '/usr/local/bin/';
+      # System Utilities
+      $GREP_EXE = '/usr/bin/grep';
+      $FILE_EXE = '/usr/bin/file';
+      $MD5_EXE = '/sbin/md5';
+      $SHA1_EXE = '/usr/bin/shasum';
 
-    # Homebrew users can install NSRL database and change this variable later
-    $NSRLDB = '';
 
-    # Evidence locker location
-    $LOCKDIR = '#{var}/lib/autopsy';
-  EOS
+      # Directories
+      $TSKDIR = '/usr/local/bin/';
+
+      # Homebrew users can install NSRL database and change this variable later
+      $NSRLDB = '';
+
+      # Evidence locker location
+      $LOCKDIR = '#{var}/lib/autopsy';
+    EOS
   end
 
   def install
@@ -57,10 +58,11 @@ class Autopsy < Formula
     bin.install "base/autopsy.base" => "autopsy"
   end
 
-  def caveats; <<~EOS
-    By default, the evidence locker is in:
-      #{var}/lib/autopsy
-  EOS
+  def caveats
+    <<~EOS
+      By default, the evidence locker is in:
+        #{var}/lib/autopsy
+    EOS
   end
 end
 
