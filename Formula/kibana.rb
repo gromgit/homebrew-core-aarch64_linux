00b91c174f6dfd33e16e44d0139f4f5ae59eaf44
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:10 +0000

    kibana: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/kibana.rb
+++ Formula/kibana.rb
@@ -48,31 +48,33 @@ class Kibana < Formula
     (prefix/"plugins").mkdir
   end
 
-  def caveats; <<~EOS
-    Config: #{etc}/kibana/
-    If you wish to preserve your plugins upon upgrade, make a copy of
-    #{opt_prefix}/plugins before upgrading, and copy it into the
-    new keg location after upgrading.
-  EOS
+  def caveats
+    <<~EOS
+      Config: #{etc}/kibana/
+      If you wish to preserve your plugins upon upgrade, make a copy of
+      #{opt_prefix}/plugins before upgrading, and copy it into the
+      new keg location after upgrading.
+    EOS
   end
 
   plist_options :manual => "kibana"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN"
-    "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>Program</key>
-        <string>#{opt_bin}/kibana</string>
-        <key>RunAtLoad</key>
-        <true/>
-      </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN"
+      "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>Program</key>
+          <string>#{opt_bin}/kibana</string>
+          <key>RunAtLoad</key>
+          <true/>
+        </dict>
+      </plist>
+    EOS
   end
 
   test do
