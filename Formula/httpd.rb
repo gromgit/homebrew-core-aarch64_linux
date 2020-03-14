06627e06572dfc81871b39acaaa778de9a539602
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:04 +0000

    httpd: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/httpd.rb
+++ Formula/httpd.rb
@@ -124,24 +124,25 @@ class Httpd < Formula
 
   plist_options :manual => "apachectl start"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/httpd</string>
-        <string>-D</string>
-        <string>FOREGROUND</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-    </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/httpd</string>
+          <string>-D</string>
+          <string>FOREGROUND</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+      </dict>
+      </plist>
+    EOS
   end
 
   test do
