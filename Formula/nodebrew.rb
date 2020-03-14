fdcde384ce58fc4bea343703612a69226d59a868
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:27 +0000

    nodebrew: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/nodebrew.rb
+++ Formula/nodebrew.rb
@@ -13,16 +13,17 @@ class Nodebrew < Formula
     zsh_completion.install "completions/zsh/_nodebrew"
   end
 
-  def caveats; <<~EOS
-    You need to manually run setup_dirs to create directories required by nodebrew:
-      #{opt_bin}/nodebrew setup_dirs
+  def caveats
+    <<~EOS
+      You need to manually run setup_dirs to create directories required by nodebrew:
+        #{opt_bin}/nodebrew setup_dirs
 
-    Add path:
-      export PATH=$HOME/.nodebrew/current/bin:$PATH
+      Add path:
+        export PATH=$HOME/.nodebrew/current/bin:$PATH
 
-    To use Homebrew's directories rather than ~/.nodebrew add to your profile:
-      export NODEBREW_ROOT=#{var}/nodebrew
-  EOS
+      To use Homebrew's directories rather than ~/.nodebrew add to your profile:
+        export NODEBREW_ROOT=#{var}/nodebrew
+    EOS
   end
 
   test do
