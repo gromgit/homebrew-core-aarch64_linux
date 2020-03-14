509cac0a536071c052c6b327dc4bcfc380a27268
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:44:40 +0000

    cmigemo: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/cmigemo.rb
+++ Formula/cmigemo.rb
@@ -33,10 +33,11 @@ class Cmigemo < Formula
     system "make", "osx-install"
   end
 
-  def caveats; <<~EOS
-    See also https://github.com/emacs-jp/migemo to use cmigemo with Emacs.
-    You will have to save as migemo.el and put it in your load-path.
-  EOS
+  def caveats
+    <<~EOS
+      See also https://github.com/emacs-jp/migemo to use cmigemo with Emacs.
+      You will have to save as migemo.el and put it in your load-path.
+    EOS
   end
 end
 
