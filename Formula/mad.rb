2e2c1d07d8a4d8e23ac3a3c6626d06c55ed2f63f
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:17 +0000

    mad: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/mad.rb
+++ Formula/mad.rb
@@ -22,19 +22,20 @@ class Mad < Formula
     (lib+"pkgconfig/mad.pc").write pc_file
   end
 
-  def pc_file; <<~EOS
-    prefix=#{opt_prefix}
-    exec_prefix=${prefix}
-    libdir=${exec_prefix}/lib
-    includedir=${prefix}/include
+  def pc_file
+    <<~EOS
+      prefix=#{opt_prefix}
+      exec_prefix=${prefix}
+      libdir=${exec_prefix}/lib
+      includedir=${prefix}/include
 
-    Name: mad
-    Description: MPEG Audio Decoder
-    Version: #{version}
-    Requires:
-    Conflicts:
-    Libs: -L${libdir} -lmad -lm
-    Cflags: -I${includedir}
-  EOS
+      Name: mad
+      Description: MPEG Audio Decoder
+      Version: #{version}
+      Requires:
+      Conflicts:
+      Libs: -L${libdir} -lmad -lm
+      Cflags: -I${includedir}
+    EOS
   end
 end
