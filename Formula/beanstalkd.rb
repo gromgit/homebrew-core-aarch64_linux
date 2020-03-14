29f5219b21211557993429a0ea2b8814ba08c42b
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:44:33 +0000

    beanstalkd: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/beanstalkd.rb
+++ Formula/beanstalkd.rb
@@ -18,32 +18,33 @@ class Beanstalkd < Formula
 
   plist_options :manual => "beanstalkd"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>KeepAlive</key>
-        <true/>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/beanstalkd</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-        <key>WorkingDirectory</key>
-        <string>#{var}</string>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/beanstalkd.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/beanstalkd.log</string>
-      </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>KeepAlive</key>
+          <true/>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/beanstalkd</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>KeepAlive</key>
+          <true/>
+          <key>WorkingDirectory</key>
+          <string>#{var}</string>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/beanstalkd.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/beanstalkd.log</string>
+        </dict>
+      </plist>
+    EOS
   end
 
   test do
