8d47683a01cf4f55b1cdd45e775d53a53004171d
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:44:51 +0000

    flatbuffers: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/flatbuffers.rb
+++ Formula/flatbuffers.rb
@@ -20,48 +20,50 @@ class Flatbuffers < Formula
   end
 
   test do
-    def testfbs; <<~EOS
-      // example IDL file
+    def testfbs
+      <<~EOS
+        // example IDL file
 
-      namespace MyGame.Sample;
+        namespace MyGame.Sample;
 
-      enum Color:byte { Red = 0, Green, Blue = 2 }
+        enum Color:byte { Red = 0, Green, Blue = 2 }
 
-      union Any { Monster }  // add more elements..
+        union Any { Monster }  // add more elements..
 
-        struct Vec3 {
-          x:float;
-          y:float;
-          z:float;
-        }
+          struct Vec3 {
+            x:float;
+            y:float;
+            z:float;
+          }
 
-        table Monster {
-          pos:Vec3;
-          mana:short = 150;
-          hp:short = 100;
-          name:string;
-          friendly:bool = false (deprecated);
-          inventory:[ubyte];
-          color:Color = Blue;
-        }
+          table Monster {
+            pos:Vec3;
+            mana:short = 150;
+            hp:short = 100;
+            name:string;
+            friendly:bool = false (deprecated);
+            inventory:[ubyte];
+            color:Color = Blue;
+          }
 
-      root_type Monster;
+        root_type Monster;
 
-    EOS
+      EOS
     end
     (testpath/"test.fbs").write(testfbs)
 
-    def testjson; <<~EOS
-      {
-        pos: {
-          x: 1,
-          y: 2,
-          z: 3
-        },
-        hp: 80,
-        name: "MyMonster"
-      }
-    EOS
+    def testjson
+      <<~EOS
+        {
+          pos: {
+            x: 1,
+            y: 2,
+            z: 3
+          },
+          hp: 80,
+          name: "MyMonster"
+        }
+      EOS
     end
     (testpath/"test.json").write(testjson)
 
