5ff13a0ca9b68a2c80f368f763d825cc3e3462a5
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:44:46 +0000

    dovecot: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/dovecot.rb
+++ Formula/dovecot.rb
@@ -49,47 +49,49 @@ class Dovecot < Formula
     end
   end
 
-  def caveats; <<~EOS
-    For Dovecot to work, you may need to create a dovecot user
-    and group depending on your configuration file options.
-  EOS
+  def caveats
+    <<~EOS
+      For Dovecot to work, you may need to create a dovecot user
+      and group depending on your configuration file options.
+    EOS
   end
 
   plist_options :startup => true
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>KeepAlive</key>
-        <false/>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_sbin}/dovecot</string>
-          <string>-F</string>
-        </array>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/dovecot/dovecot.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/dovecot/dovecot.log</string>
-        <key>SoftResourceLimits</key>
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
         <dict>
-        <key>NumberOfFiles</key>
-        <integer>1000</integer>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>KeepAlive</key>
+          <false/>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_sbin}/dovecot</string>
+            <string>-F</string>
+          </array>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/dovecot/dovecot.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/dovecot/dovecot.log</string>
+          <key>SoftResourceLimits</key>
+          <dict>
+          <key>NumberOfFiles</key>
+          <integer>1000</integer>
+          </dict>
+          <key>HardResourceLimits</key>
+          <dict>
+          <key>NumberOfFiles</key>
+          <integer>1024</integer>
+          </dict>
         </dict>
-        <key>HardResourceLimits</key>
-        <dict>
-        <key>NumberOfFiles</key>
-        <integer>1024</integer>
-        </dict>
-      </dict>
-    </plist>
-  EOS
+      </plist>
+    EOS
   end
 
   test do
