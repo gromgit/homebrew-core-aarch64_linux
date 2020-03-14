6aa96c9de8551ea61f2ca9b65376a977862caf91
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:17 +0000

    mailhog: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/mailhog.rb
+++ Formula/mailhog.rb
@@ -140,28 +140,29 @@ class Mailhog < Formula
 
   plist_options :manual => "MailHog"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>KeepAlive</key>
-      <true/>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/MailHog</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>StandardErrorPath</key>
-      <string>#{var}/log/mailhog.log</string>
-      <key>StandardOutPath</key>
-      <string>#{var}/log/mailhog.log</string>
-    </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <true/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/MailHog</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/mailhog.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/mailhog.log</string>
+      </dict>
+      </plist>
+    EOS
   end
 
   test do
