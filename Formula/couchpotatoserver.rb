3df827a04aecbb15cd40d655eebf6edd0aa26729
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:44:42 +0000

    couchpotatoserver: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/couchpotatoserver.rb
+++ Formula/couchpotatoserver.rb
@@ -19,35 +19,37 @@ class Couchpotatoserver < Formula
 
   plist_options :manual => "couchpotatoserver"
 
-  def plist; <<~EOS
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>Program</key>
-        <string>#{opt_bin}/couchpotatoserver</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>--quiet</string>
-          <string>--daemon</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>UserName</key>
-        <string>#{`whoami`.chomp}</string>
-      </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>Program</key>
+          <string>#{opt_bin}/couchpotatoserver</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>--quiet</string>
+            <string>--daemon</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>UserName</key>
+          <string>#{`whoami`.chomp}</string>
+        </dict>
+      </plist>
+    EOS
   end
 
-  def startup_script; <<~EOS
-    #!/bin/bash
-    python "#{libexec}/CouchPotato.py"\
-           "--pid_file=#{var}/run/couchpotatoserver.pid"\
-           "--data_dir=#{etc}/couchpotatoserver"\
-           "$@"
-  EOS
+  def startup_script
+    <<~EOS
+      #!/bin/bash
+      python "#{libexec}/CouchPotato.py"\
+             "--pid_file=#{var}/run/couchpotatoserver.pid"\
+             "--data_dir=#{etc}/couchpotatoserver"\
+             "$@"
+    EOS
   end
 
   test do
