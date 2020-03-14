98abb2705918aec7e9b93cfab9424c1cf33d8009
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:54 +0000

    ssdb: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/ssdb.rb
+++ Formula/ssdb.rb
@@ -46,34 +46,35 @@ class Ssdb < Formula
 
   plist_options :manual => "ssdb-server #{HOMEBREW_PREFIX}/etc/ssdb.conf"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>KeepAlive</key>
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
         <dict>
-          <key>SuccessfulExit</key>
-          <false/>
+          <key>KeepAlive</key>
+          <dict>
+            <key>SuccessfulExit</key>
+            <false/>
+          </dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/ssdb-server</string>
+            <string>#{etc}/ssdb.conf</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>WorkingDirectory</key>
+          <string>#{var}</string>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/ssdb.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/ssdb.log</string>
         </dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/ssdb-server</string>
-          <string>#{etc}/ssdb.conf</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>WorkingDirectory</key>
-        <string>#{var}</string>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/ssdb.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/ssdb.log</string>
-      </dict>
-    </plist>
-  EOS
+      </plist>
+    EOS
   end
 
   test do
