9a9758ce22036f2b49d83bd744821e5a34f40dc5
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:52 +0000

    shibboleth-sp: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/shibboleth-sp.rb
+++ Formula/shibboleth-sp.rb
@@ -54,28 +54,29 @@ class ShibbolethSp < Formula
 
   plist_options :startup => true, :manual => "shibd"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_sbin}/shibd</string>
-        <string>-F</string>
-        <string>-f</string>
-        <string>-p</string>
-        <string>#{var}/run/shibboleth/shibd.pid</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>KeepAlive</key>
-      <true/>
-    </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_sbin}/shibd</string>
+          <string>-F</string>
+          <string>-f</string>
+          <string>-p</string>
+          <string>#{var}/run/shibboleth/shibd.pid</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+      </dict>
+      </plist>
+    EOS
   end
 
   test do
