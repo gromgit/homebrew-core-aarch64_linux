35acbdc98e5635e4e0c7a02f2a451d6e372c620c
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:34 +0000

    pdnsd: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/pdnsd.rb
+++ Formula/pdnsd.rb
@@ -25,48 +25,50 @@ class Pdnsd < Formula
     system "make", "install"
   end
 
-  def caveats; <<~EOS
-    This install of "pdnsd" expects config files to be in #{etc}
-    All state files (status and cache) are stored in #{var}/cache/pdnsd.
+  def caveats
+    <<~EOS
+      This install of "pdnsd" expects config files to be in #{etc}
+      All state files (status and cache) are stored in #{var}/cache/pdnsd.
 
-    pdnsd needs to run as root since it listens on privileged ports.
+      pdnsd needs to run as root since it listens on privileged ports.
 
-    Sample config file can be found at #{etc}/pdnsd.conf.sample.
+      Sample config file can be found at #{etc}/pdnsd.conf.sample.
 
-    Note that you must create the config file before starting the service,
-    and change ownership to "root" or pdnsd will refuse to run:
-      sudo chown root #{etc}/pdnsd.conf
+      Note that you must create the config file before starting the service,
+      and change ownership to "root" or pdnsd will refuse to run:
+        sudo chown root #{etc}/pdnsd.conf
 
-    For other related utilities, e.g. pdnsd-ctl, to run, change the ownership
-    to the user (default: nobody) running the service:
-      sudo chown -R nobody #{var}/log/pdnsd.log #{var}/cache/pdnsd
-  EOS
+      For other related utilities, e.g. pdnsd-ctl, to run, change the ownership
+      to the user (default: nobody) running the service:
+        sudo chown -R nobody #{var}/log/pdnsd.log #{var}/cache/pdnsd
+    EOS
   end
 
   plist_options :startup => true, :manual => "sudo pdnsd"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>KeepAlive</key>
-      <true/>
-      <key>Program</key>
-      <string>#{opt_sbin}/pdnsd</string>
-      <key>StandardErrorPath</key>
-      <string>#{var}/log/pdnsd.log</string>
-      <key>StandardOutputPath</key>
-      <string>#{var}/log/pdnsd.log</string>
-      <key>Disabled</key>
-      <false/>
-    </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+        <key>Program</key>
+        <string>#{opt_sbin}/pdnsd</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/pdnsd.log</string>
+        <key>StandardOutputPath</key>
+        <string>#{var}/log/pdnsd.log</string>
+        <key>Disabled</key>
+        <false/>
+      </dict>
+      </plist>
+    EOS
   end
 
   test do
