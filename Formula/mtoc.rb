d0980a0dcb5012e10c1dcf35ed8ca783454ba2e0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-14 23:21:23 +0000

    mtoc: add 927.0.2 bottle.

--- Formula/mtoc.rb
+++ Formula/mtoc.rb
@@ -4,6 +4,13 @@ class Mtoc < Formula
   url "https://opensource.apple.com/tarballs/cctools/cctools-927.0.2.tar.gz"
   sha256 "3d5d35e7120aac187de306019d4b53b3b08dd7ebf48764fcd5c8cefef40b3dbf"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9633efa04b078ac1353ec39db86cdfa98e98024f671ac8a46457c8cd27925bbe" => :catalina
+    sha256 "976565f15269c96da91a36c9f5b7e2497741f44f1ca1ec2c2402fe8d430a072d" => :mojave
+    sha256 "883674b5bfc549ef5ce33fc4ca653cd0d5583319d57beb170763d5b331ed6919" => :high_sierra
+  end
+
   depends_on "llvm" => :build
 
   def install
