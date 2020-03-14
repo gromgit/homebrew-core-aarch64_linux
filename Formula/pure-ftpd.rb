29c9aacbc9faaf734dfb649516c0ed771944b70c
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:43 +0000

    pure-ftpd: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/pure-ftpd.rb
+++ Formula/pure-ftpd.rb
@@ -34,34 +34,35 @@ class PureFtpd < Formula
 
   plist_options :manual => "pure-ftpd"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>KeepAlive</key>
-        <true/>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_sbin}/pure-ftpd</string>
-          <string>--chrooteveryone</string>
-          <string>--createhomedir</string>
-          <string>--allowdotfiles</string>
-          <string>--login=puredb:#{etc}/pureftpd.pdb</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>WorkingDirectory</key>
-        <string>#{var}</string>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/pure-ftpd.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/pure-ftpd.log</string>
-      </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>KeepAlive</key>
+          <true/>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_sbin}/pure-ftpd</string>
+            <string>--chrooteveryone</string>
+            <string>--createhomedir</string>
+            <string>--allowdotfiles</string>
+            <string>--login=puredb:#{etc}/pureftpd.pdb</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>WorkingDirectory</key>
+          <string>#{var}</string>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/pure-ftpd.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/pure-ftpd.log</string>
+        </dict>
+      </plist>
+    EOS
   end
 
   test do
