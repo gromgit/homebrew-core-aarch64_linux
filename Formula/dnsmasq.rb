98f4f718df0b949c62fcc4c2b0b78b4ba1f26a7b
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:44:45 +0000

    dnsmasq: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/dnsmasq.rb
+++ Formula/dnsmasq.rb
@@ -54,29 +54,30 @@ class Dnsmasq < Formula
 
   plist_options :startup => true
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_sbin}/dnsmasq</string>
-          <string>--keep-in-foreground</string>
-          <string>-C</string>
-          <string>#{etc}/dnsmasq.conf</string>
-          <string>-7</string>
-          <string>#{etc}/dnsmasq.d,*.conf</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-      </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_sbin}/dnsmasq</string>
+            <string>--keep-in-foreground</string>
+            <string>-C</string>
+            <string>#{etc}/dnsmasq.conf</string>
+            <string>-7</string>
+            <string>#{etc}/dnsmasq.d,*.conf</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>KeepAlive</key>
+          <true/>
+        </dict>
+      </plist>
+    EOS
   end
 
   test do
