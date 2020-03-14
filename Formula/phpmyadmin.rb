017b52c017cbaf96e7a258e6411062d1d95205d1
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:37 +0000

    phpmyadmin: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/phpmyadmin.rb
+++ Formula/phpmyadmin.rb
@@ -15,24 +15,25 @@ class Phpmyadmin < Formula
     ln_s etc/"phpmyadmin.config.inc.php", pkgshare/"config.inc.php"
   end
 
-  def caveats; <<~EOS
-    To enable phpMyAdmin in Apache, add the following to httpd.conf and
-    restart Apache:
-        Alias /phpmyadmin #{HOMEBREW_PREFIX}/share/phpmyadmin
-        <Directory #{HOMEBREW_PREFIX}/share/phpmyadmin/>
-            Options Indexes FollowSymLinks MultiViews
-            AllowOverride All
-            <IfModule mod_authz_core.c>
-                Require all granted
-            </IfModule>
-            <IfModule !mod_authz_core.c>
-                Order allow,deny
-                Allow from all
-            </IfModule>
-        </Directory>
-    Then open http://localhost/phpmyadmin
-    The configuration file is #{etc}/phpmyadmin.config.inc.php
-  EOS
+  def caveats
+    <<~EOS
+      To enable phpMyAdmin in Apache, add the following to httpd.conf and
+      restart Apache:
+          Alias /phpmyadmin #{HOMEBREW_PREFIX}/share/phpmyadmin
+          <Directory #{HOMEBREW_PREFIX}/share/phpmyadmin/>
+              Options Indexes FollowSymLinks MultiViews
+              AllowOverride All
+              <IfModule mod_authz_core.c>
+                  Require all granted
+              </IfModule>
+              <IfModule !mod_authz_core.c>
+                  Order allow,deny
+                  Allow from all
+              </IfModule>
+          </Directory>
+      Then open http://localhost/phpmyadmin
+      The configuration file is #{etc}/phpmyadmin.config.inc.php
+    EOS
   end
 
   test do
