735d81356d6823ce73adf18f2c49268fa1596fc3
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:41 +0000

    privoxy: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/privoxy.rb
+++ Formula/privoxy.rb
@@ -35,30 +35,31 @@ class Privoxy < Formula
 
   plist_options :manual => "privoxy #{HOMEBREW_PREFIX}/etc/privoxy/config"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>KeepAlive</key>
-      <true/>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>WorkingDirectory</key>
-      <string>#{var}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{sbin}/privoxy</string>
-        <string>--no-daemon</string>
-        <string>#{etc}/privoxy/config</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>StandardErrorPath</key>
-      <string>#{var}/log/privoxy/logfile</string>
-    </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <true/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{sbin}/privoxy</string>
+          <string>--no-daemon</string>
+          <string>#{etc}/privoxy/config</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/privoxy/logfile</string>
+      </dict>
+      </plist>
+    EOS
   end
 
   test do
