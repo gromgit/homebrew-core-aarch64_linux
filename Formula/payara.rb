da19881797574179d6f6ed10b59b2aadf5eeec52
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:33 +0000

    payara: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/payara.rb
+++ Formula/payara.rb
@@ -22,46 +22,48 @@ class Payara < Formula
     bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
   end
 
-  def caveats; <<~EOS
-    You may want to add the following to your .bash_profile:
-      export GLASSFISH_HOME=#{opt_libexec}/glassfish
-      export PATH=${PATH}:${GLASSFISH_HOME}/bin
-  EOS
+  def caveats
+    <<~EOS
+      You may want to add the following to your .bash_profile:
+        export GLASSFISH_HOME=#{opt_libexec}/glassfish
+        export PATH=${PATH}:${GLASSFISH_HOME}/bin
+    EOS
   end
 
   plist_options :manual => "asadmin start-domain --verbose domain1"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>KeepAlive</key>
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
       <dict>
-        <key>Crashed</key>
-        <true/>
-        <key>SuccessfulExit</key>
-        <false/>
-      </dict>
-      <key>WorkingDirectory</key>
-      <string>#{opt_libexec}/glassfish</string>
-      <key>EnvironmentVariables</key>
-      <dict>
-        <key>GLASSFISH_HOME</key>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>KeepAlive</key>
+        <dict>
+          <key>Crashed</key>
+          <true/>
+          <key>SuccessfulExit</key>
+          <false/>
+        </dict>
+        <key>WorkingDirectory</key>
         <string>#{opt_libexec}/glassfish</string>
+        <key>EnvironmentVariables</key>
+        <dict>
+          <key>GLASSFISH_HOME</key>
+          <string>#{opt_libexec}/glassfish</string>
+        </dict>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_libexec}/glassfish/bin/asadmin</string>
+          <string>start-domain</string>
+          <string>--verbose</string>
+          <string>domain1</string>
+        </array>
       </dict>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_libexec}/glassfish/bin/asadmin</string>
-        <string>start-domain</string>
-        <string>--verbose</string>
-        <string>domain1</string>
-      </array>
-    </dict>
-    </plist>
-  EOS
+      </plist>
+    EOS
   end
 
   test do
