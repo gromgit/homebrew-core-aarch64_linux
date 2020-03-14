049561206b8b7e3877bbd333a4bbb262d193723b
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:44:31 +0000

    awscli: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -43,10 +43,11 @@ class Awscli < Formula
     EOS
   end
 
-  def caveats; <<~EOS
-    The "examples" directory has been installed to:
-      #{HOMEBREW_PREFIX}/share/awscli/examples
-  EOS
+  def caveats
+    <<~EOS
+      The "examples" directory has been installed to:
+        #{HOMEBREW_PREFIX}/share/awscli/examples
+    EOS
   end
 
   test do
