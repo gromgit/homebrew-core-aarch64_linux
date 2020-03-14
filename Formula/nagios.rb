b60a887fa0c186135ea0b6452e02a31b1d8d381d
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:25 +0000

    nagios: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/nagios.rb
+++ Formula/nagios.rb
@@ -74,63 +74,65 @@ class Nagios < Formula
     inreplace config, "brew", ENV["USER"]
   end
 
-  def caveats; <<~EOS
-    First we need to create a command dir using superhuman powers:
+  def caveats
+    <<~EOS
+      First we need to create a command dir using superhuman powers:
 
-      mkdir -p #{nagios_var}/rw
-      sudo chgrp _www #{nagios_var}/rw
-      sudo chmod 2775 #{nagios_var}/rw
+        mkdir -p #{nagios_var}/rw
+        sudo chgrp _www #{nagios_var}/rw
+        sudo chmod 2775 #{nagios_var}/rw
 
-    Then install the Nagios web frontend into Apple's build-in Apache:
+      Then install the Nagios web frontend into Apple's build-in Apache:
 
-      1) Turn on Personal Web Sharing.
+        1) Turn on Personal Web Sharing.
 
-      2) Load the cgi and php modules by patching /etc/apache2/httpd.conf:
+        2) Load the cgi and php modules by patching /etc/apache2/httpd.conf:
 
-        -#LoadModule php5_module        libexec/apache2/libphp5.so
-        +LoadModule php5_module        libexec/apache2/libphp5.so
+          -#LoadModule php5_module        libexec/apache2/libphp5.so
+          +LoadModule php5_module        libexec/apache2/libphp5.so
 
-        -#LoadModule cgi_module libexec/apache2/mod_cgi.so
-        +LoadModule cgi_module libexec/apache2/mod_cgi.so
+          -#LoadModule cgi_module libexec/apache2/mod_cgi.so
+          +LoadModule cgi_module libexec/apache2/mod_cgi.so
 
-      3) Symlink the sample config and create your web account:
+        3) Symlink the sample config and create your web account:
 
-        sudo ln -sf #{share}/nagios.conf /etc/apache2/other/
-        htpasswd -cs #{nagios_etc}/htpasswd.users nagiosadmin
-        sudo apachectl restart
+          sudo ln -sf #{share}/nagios.conf /etc/apache2/other/
+          htpasswd -cs #{nagios_etc}/htpasswd.users nagiosadmin
+          sudo apachectl restart
 
-    Log in with your web account (and don't forget to RTFM :-)
+      Log in with your web account (and don't forget to RTFM :-)
 
-      open http://localhost/nagios
+        open http://localhost/nagios
 
-  EOS
+    EOS
   end
 
   plist_options :startup => true, :manual => "nagios #{HOMEBREW_PREFIX}/etc/nagios/nagios.cfg"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>KeepAlive</key>
-      <true/>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/nagios</string>
-        <string>#{nagios_etc}/nagios.cfg</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>StandardErrorPath</key>
-      <string>/dev/null</string>
-      <key>StandardOutPath</key>
-      <string>/dev/null</string>
-    </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <true/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/nagios</string>
+          <string>#{nagios_etc}/nagios.cfg</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>StandardErrorPath</key>
+        <string>/dev/null</string>
+        <key>StandardOutPath</key>
+        <string>/dev/null</string>
+      </dict>
+      </plist>
+    EOS
   end
 
   test do
