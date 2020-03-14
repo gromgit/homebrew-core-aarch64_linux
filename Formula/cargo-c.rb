75dba081bbe6017684eb9588f1af5d064ef545a9
Bo Anderson <mail@boanderson.me>
2020-03-14 03:03:19 +0000

    cargo-c: patch crash issue with v2 Cargo.lock
    Closes #51721.
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/cargo-c.rb
+++ Formula/cargo-c.rb
@@ -3,6 +3,7 @@ class CargoC < Formula
   homepage "https://github.com/lu-zero/cargo-c"
   url "https://github.com/lu-zero/cargo-c/archive/v0.5.1.tar.gz"
   sha256 "e547f3604bb801914d5685c22b54776baf70686b9aeb191396866a6e55391591"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -12,6 +13,13 @@ class CargoC < Formula
   end
 
   depends_on "rust" => [:build, :test]
+
+  # Fixes crash issue with v2 Cargo.lock files - detected while building rav1e.
+  # Remove with the next version.
+  patch do
+    url "https://github.com/lu-zero/cargo-c/commit/0cb09ac3c1950a2c05758b344131ff7b7a38c42d.patch?full_index=1"
+    sha256 "746a6a9e6fc9a3b15afa9167011a2a06b9c70d7c9adbf7bb20b4e376fa0ab667"
+  end
 
   def install
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
