3e5a18cac58ea3f77b8eca2398a44eee9a0cabb7
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:46:06 +0000

    vsftpd: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/vsftpd.rb
+++ Formula/vsftpd.rb
@@ -35,35 +35,37 @@ class Vsftpd < Formula
     man8.install "vsftpd.8"
   end
 
-  def caveats; <<~EOS
-    To use chroot, vsftpd requires root privileges, so you will need to run
-    `sudo vsftpd`.
-    You should be certain that you trust any software you grant root privileges.
+  def caveats
+    <<~EOS
+      To use chroot, vsftpd requires root privileges, so you will need to run
+      `sudo vsftpd`.
+      You should be certain that you trust any software you grant root privileges.
 
-    The vsftpd.conf file must be owned by root or vsftpd will refuse to start:
-      sudo chown root #{HOMEBREW_PREFIX}/etc/vsftpd.conf
-  EOS
+      The vsftpd.conf file must be owned by root or vsftpd will refuse to start:
+        sudo chown root #{HOMEBREW_PREFIX}/etc/vsftpd.conf
+    EOS
   end
 
   plist_options :startup => true, :manual => "sudo vsftpd"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{sbin}/vsftpd</string>
-        <string>#{etc}/vsftpd.conf</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-    </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{sbin}/vsftpd</string>
+          <string>#{etc}/vsftpd.conf</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+      </dict>
+      </plist>
+    EOS
   end
 
   test do
