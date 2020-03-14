284cd20f3bbd1f773c82a3a2b6081facf9295657
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:59 +0000

    tmux: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/tmux.rb
+++ Formula/tmux.rb
@@ -49,10 +49,11 @@ class Tmux < Formula
     bash_completion.install resource("completion")
   end
 
-  def caveats; <<~EOS
-    Example configuration has been installed to:
-      #{opt_pkgshare}
-  EOS
+  def caveats
+    <<~EOS
+      Example configuration has been installed to:
+        #{opt_pkgshare}
+    EOS
   end
 
   test do
