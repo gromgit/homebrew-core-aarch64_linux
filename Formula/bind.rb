1538657005f106e8e6a73193599b476d03cbd695
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:44:33 +0000

    bind: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/bind.rb
+++ Formula/bind.rb
@@ -70,120 +70,124 @@ class Bind < Formula
     end
   end
 
-  def named_conf; <<~EOS
-    //
-    // Include keys file
-    //
-    include "#{etc}/rndc.key";
+  def named_conf
+    <<~EOS
+      //
+      // Include keys file
+      //
+      include "#{etc}/rndc.key";
 
-    // Declares control channels to be used by the rndc utility.
-    //
-    // It is recommended that 127.0.0.1 be the only address used.
-    // This also allows non-privileged users on the local host to manage
-    // your name server.
+      // Declares control channels to be used by the rndc utility.
+      //
+      // It is recommended that 127.0.0.1 be the only address used.
+      // This also allows non-privileged users on the local host to manage
+      // your name server.
 
-    //
-    // Default controls
-    //
-    controls {
-        inet 127.0.0.1 port 54 allow { any; }
-        keys { "rndc-key"; };
-    };
+      //
+      // Default controls
+      //
+      controls {
+          inet 127.0.0.1 port 54 allow { any; }
+          keys { "rndc-key"; };
+      };
 
-    options {
-        directory "#{var}/named";
-        /*
-         * If there is a firewall between you and nameservers you want
-         * to talk to, you might need to uncomment the query-source
-         * directive below.  Previous versions of BIND always asked
-         * questions using port 53, but BIND 8.1 uses an unprivileged
-         * port by default.
-         */
-        // query-source address * port 53;
-    };
-    //
-    // a caching only nameserver config
-    //
-    zone "localhost" IN {
-        type master;
-        file "localhost.zone";
-        allow-update { none; };
-    };
+      options {
+          directory "#{var}/named";
+          /*
+           * If there is a firewall between you and nameservers you want
+           * to talk to, you might need to uncomment the query-source
+           * directive below.  Previous versions of BIND always asked
+           * questions using port 53, but BIND 8.1 uses an unprivileged
+           * port by default.
+           */
+          // query-source address * port 53;
+      };
+      //
+      // a caching only nameserver config
+      //
+      zone "localhost" IN {
+          type master;
+          file "localhost.zone";
+          allow-update { none; };
+      };
 
-    zone "0.0.127.in-addr.arpa" IN {
-        type master;
-        file "named.local";
-        allow-update { none; };
-    };
+      zone "0.0.127.in-addr.arpa" IN {
+          type master;
+          file "named.local";
+          allow-update { none; };
+      };
 
-    logging {
-            category default {
-                    _default_log;
-            };
+      logging {
+              category default {
+                      _default_log;
+              };
 
-            channel _default_log  {
-                    file "#{var}/log/named/named.log";
-                    severity info;
-                    print-time yes;
-            };
-    };
-  EOS
+              channel _default_log  {
+                      file "#{var}/log/named/named.log";
+                      severity info;
+                      print-time yes;
+              };
+      };
+    EOS
   end
 
-  def localhost_zone; <<~EOS
-    $TTL    86400
-    $ORIGIN localhost.
-    @            1D IN SOA    @ root (
-                        42        ; serial (d. adams)
-                        3H        ; refresh
-                        15M        ; retry
-                        1W        ; expiry
-                        1D )        ; minimum
+  def localhost_zone
+    <<~EOS
+      $TTL    86400
+      $ORIGIN localhost.
+      @            1D IN SOA    @ root (
+                          42        ; serial (d. adams)
+                          3H        ; refresh
+                          15M        ; retry
+                          1W        ; expiry
+                          1D )        ; minimum
 
-                1D IN NS    @
-                1D IN A        127.0.0.1
-  EOS
+                  1D IN NS    @
+                  1D IN A        127.0.0.1
+    EOS
   end
 
-  def named_local; <<~EOS
-    $TTL    86400
-    @       IN      SOA     localhost. root.localhost.  (
-                                          1997022700 ; Serial
-                                          28800      ; Refresh
-                                          14400      ; Retry
-                                          3600000    ; Expire
-                                          86400 )    ; Minimum
-                  IN      NS      localhost.
+  def named_local
+    <<~EOS
+      $TTL    86400
+      @       IN      SOA     localhost. root.localhost.  (
+                                            1997022700 ; Serial
+                                            28800      ; Refresh
+                                            14400      ; Retry
+                                            3600000    ; Expire
+                                            86400 )    ; Minimum
+                    IN      NS      localhost.
 
-    1       IN      PTR     localhost.
-  EOS
+      1       IN      PTR     localhost.
+    EOS
   end
 
   plist_options :startup => true
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>EnableTransactions</key>
-      <true/>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_sbin}/named</string>
-        <string>-f</string>
-        <string>-c</string>
-        <string>#{etc}/named.conf</string>
-      </array>
-      <key>ServiceIPC</key>
-      <false/>
-    </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>EnableTransactions</key>
+        <true/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_sbin}/named</string>
+          <string>-f</string>
+          <string>-c</string>
+          <string>#{etc}/named.conf</string>
+        </array>
+        <key>ServiceIPC</key>
+        <false/>
+      </dict>
+      </plist>
+    EOS
   end
 
   test do
