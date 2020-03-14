6bd023b3ac449aa2bc6a2d999eccf5c60e9c2288
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:31 +0000

    opam: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/opam.rb
+++ Formula/opam.rb
@@ -28,12 +28,13 @@ class Opam < Formula
     zsh_completion.install "src/state/shellscripts/complete.zsh" => "_opam"
   end
 
-  def caveats; <<~EOS
-    OPAM uses ~/.opam by default for its package database, so you need to
-    initialize it first by running:
+  def caveats
+    <<~EOS
+      OPAM uses ~/.opam by default for its package database, so you need to
+      initialize it first by running:
 
-    $ opam init
-  EOS
+      $ opam init
+    EOS
   end
 
   test do
