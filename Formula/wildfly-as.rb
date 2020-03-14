79442b40c05d54cda4465770ae25a2f2191afe15
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:46:07 +0000

    wildfly-as: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/wildfly-as.rb
+++ Formula/wildfly-as.rb
@@ -19,46 +19,48 @@ class WildflyAs < Formula
     mkdir_p libexec/"standalone/log"
   end
 
-  def caveats; <<~EOS
-    The home of WildFly Application Server #{version} is:
-      #{opt_libexec}
-    You may want to add the following to your .bash_profile:
-      export JBOSS_HOME=#{opt_libexec}
-      export PATH=${PATH}:${JBOSS_HOME}/bin
-  EOS
+  def caveats
+    <<~EOS
+      The home of WildFly Application Server #{version} is:
+        #{opt_libexec}
+      You may want to add the following to your .bash_profile:
+        export JBOSS_HOME=#{opt_libexec}
+        export PATH=${PATH}:${JBOSS_HOME}/bin
+    EOS
   end
 
   plist_options :manual => "#{HOMEBREW_PREFIX}/opt/wildfly-as/libexec/bin/standalone.sh --server-config=standalone.xml"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>KeepAlive</key>
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
       <dict>
-        <key>SuccessfulExit</key>
-        <false/>
-        <key>Crashed</key>
-        <true/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>KeepAlive</key>
+        <dict>
+          <key>SuccessfulExit</key>
+          <false/>
+          <key>Crashed</key>
+          <true/>
+        </dict>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_libexec}/bin/standalone.sh</string>
+          <string>--server-config=standalone.xml</string>
+        </array>
+        <key>EnvironmentVariables</key>
+        <dict>
+          <key>JBOSS_HOME</key>
+          <string>#{opt_libexec}</string>
+          <key>WILDFLY_HOME</key>
+          <string>#{opt_libexec}</string>
+        </dict>
       </dict>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_libexec}/bin/standalone.sh</string>
-        <string>--server-config=standalone.xml</string>
-      </array>
-      <key>EnvironmentVariables</key>
-      <dict>
-        <key>JBOSS_HOME</key>
-        <string>#{opt_libexec}</string>
-        <key>WILDFLY_HOME</key>
-        <string>#{opt_libexec}</string>
-      </dict>
-    </dict>
-    </plist>
-  EOS
+      </plist>
+    EOS
   end
 
   test do
