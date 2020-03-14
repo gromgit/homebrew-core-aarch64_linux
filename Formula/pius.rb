a39dbd01a696b0e0b25a38aabcb8a5a7291e3da7
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:38 +0000

    pius: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/pius.rb
+++ Formula/pius.rb
@@ -29,11 +29,12 @@ class Pius < Formula
     virtualenv_install_with_resources
   end
 
-  def caveats; <<~EOS
-    The path to gpg is hardcoded in pius as `/usr/bin/env gpg`.
-    You can specify a different path by editing ~/.pius:
-      gpg-path=/path/to/gpg
-  EOS
+  def caveats
+    <<~EOS
+      The path to gpg is hardcoded in pius as `/usr/bin/env gpg`.
+      You can specify a different path by editing ~/.pius:
+        gpg-path=/path/to/gpg
+    EOS
   end
 
   test do
