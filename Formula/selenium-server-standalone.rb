ec86be0c69851b5cd20b52b86dbff9a8ab9ccde3
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:45:51 +0000

    selenium-server-standalone: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/selenium-server-standalone.rb
+++ Formula/selenium-server-standalone.rb
@@ -13,34 +13,35 @@ class SeleniumServerStandalone < Formula
 
   plist_options :manual => "selenium-server -port 4444"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>KeepAlive</key>
-      <false/>
-      <key>ProgramArguments</key>
-      <array>
-        <string>/usr/bin/java</string>
-        <string>-jar</string>
-        <string>#{libexec}/selenium-server-standalone-#{version}.jar</string>
-        <string>-port</string>
-        <string>4444</string>
-      </array>
-      <key>ServiceDescription</key>
-      <string>Selenium Server</string>
-      <key>StandardErrorPath</key>
-      <string>#{var}/log/selenium-error.log</string>
-      <key>StandardOutPath</key>
-      <string>#{var}/log/selenium-output.log</string>
-    </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <false/>
+        <key>ProgramArguments</key>
+        <array>
+          <string>/usr/bin/java</string>
+          <string>-jar</string>
+          <string>#{libexec}/selenium-server-standalone-#{version}.jar</string>
+          <string>-port</string>
+          <string>4444</string>
+        </array>
+        <key>ServiceDescription</key>
+        <string>Selenium Server</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/selenium-error.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/selenium-output.log</string>
+      </dict>
+      </plist>
+    EOS
   end
 
   test do
