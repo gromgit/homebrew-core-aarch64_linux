0aab2ca0d73552998b8a05e6389514f70f2d6235
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:44:39 +0000

    clipper: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/clipper.rb
+++ Formula/clipper.rb
@@ -20,31 +20,32 @@ class Clipper < Formula
 
   plist_options :manual => "clipper"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>KeepAlive</key>
-      <true/>
-      <key>WorkingDirectory</key>
-      <string>#{HOMEBREW_PREFIX}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/clipper</string>
-      </array>
-      <key>EnvironmentVariables</key>
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
       <dict>
-        <key>LANG</key>
-        <string>en_US.UTF-8</string>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/clipper</string>
+        </array>
+        <key>EnvironmentVariables</key>
+        <dict>
+          <key>LANG</key>
+          <string>en_US.UTF-8</string>
+        </dict>
       </dict>
-    </dict>
-    </plist>
-  EOS
+      </plist>
+    EOS
   end
 
   test do
