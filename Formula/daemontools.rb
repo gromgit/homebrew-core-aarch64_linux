ca193a3d04ba11bb4139edd95acf542df8bc9bc7
Mike McQuaid <mike@mikemcquaid.com>
2020-03-14 19:44:43 +0000

    daemontools: fix RuboCop Style/TrailingBodyOnMethodDefinition.

--- Formula/daemontools.rb
+++ Formula/daemontools.rb
@@ -20,33 +20,35 @@ class Daemontools < Formula
     end
   end
 
-  def caveats; <<~EOS
-    You must create the /service directory before starting svscan:
-      sudo mkdir /service
-      sudo brew services start daemontools
-  EOS
+  def caveats
+    <<~EOS
+      You must create the /service directory before starting svscan:
+        sudo mkdir /service
+        sudo brew services start daemontools
+    EOS
   end
 
   plist_options :startup => true
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/svscanboot</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>KeepAlive</key>
-      <true/>
-    </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/svscanboot</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+      </dict>
+      </plist>
+    EOS
   end
 
   test do
