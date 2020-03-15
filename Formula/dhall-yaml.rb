4dfa070aa0eeb6880927429bc296f1b5da0df1ea
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-15 16:48:27 +0000

    dhall-yaml: add 1.0.1 bottle.

--- Formula/dhall-yaml.rb
+++ Formula/dhall-yaml.rb
@@ -9,6 +9,13 @@ class DhallYaml < Formula
   sha256 "4ed4351c1850363607ed025a035f973db5c375b4650da692f69652837935a3de"
   head "https://github.com/dhall-lang/dhall-haskell.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8d7d9ac00313fb6ce4c24ee1eca28cf9d30d3e1c4f67010fb78385665105055e" => :catalina
+    sha256 "f96cc310eb0e8b387def35b3b3736d8c0062ae51ad306f7bf68acaea6849724b" => :mojave
+    sha256 "0f22a381aa9e6093aeac60aa5dc70c692dabc1c91014501fe9294deea410d423" => :high_sierra
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
 
