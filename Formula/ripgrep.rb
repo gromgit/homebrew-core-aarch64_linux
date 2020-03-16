abc55ccf5be10e7819879b0bc67cb13261cb8c65
404NetworkError <404NetworkError@users.noreply.github.com>
2020-03-15 22:36:40 -0400

    ripgrep 12.0.0
    Closes #51762.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/ripgrep.rb
+++ Formula/ripgrep.rb
@@ -1,8 +1,8 @@
 class Ripgrep < Formula
   desc "Search tool like grep and The Silver Searcher"
   homepage "https://github.com/BurntSushi/ripgrep"
-  url "https://github.com/BurntSushi/ripgrep/archive/11.0.2.tar.gz"
-  sha256 "0983861279936ada8bc7a6d5d663d590ad34eb44a44c75c2d6ccd0ab33490055"
+  url "https://github.com/BurntSushi/ripgrep/archive/12.0.0.tar.gz"
+  sha256 "3c84c3a4c80cee961a2b0d00a4e5d1bc9eb58ba587cf69c06d84aff72fedbeff"
   head "https://github.com/BurntSushi/ripgrep.git"
 
   bottle do
