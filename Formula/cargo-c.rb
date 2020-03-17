8bf6998080d979f1b9041531b6064e0854a982de
Rui Chen <chenrui333@gmail.com>
2020-03-17 10:18:36 -0400

    cargo-c 0.5.2
    Update cargo-c.rb
    
    Closes #51810.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/cargo-c.rb
+++ Formula/cargo-c.rb
@@ -1,9 +1,8 @@
 class CargoC < Formula
   desc "Helper program to build and install c-like libraries"
   homepage "https://github.com/lu-zero/cargo-c"
-  url "https://github.com/lu-zero/cargo-c/archive/v0.5.1.tar.gz"
-  sha256 "e547f3604bb801914d5685c22b54776baf70686b9aeb191396866a6e55391591"
-  revision 1
+  url "https://github.com/lu-zero/cargo-c/archive/v0.5.2.tar.gz"
+  sha256 "815fa1d94d2a22a70e556546b60a0f4ce89e920c3cbc3bcbf1961fed3397ebcd"
 
   bottle do
     cellar :any_skip_relocation
@@ -13,13 +12,6 @@ class CargoC < Formula
   end
 
   depends_on "rust" => [:build, :test]
-
-  # Fixes crash issue with v2 Cargo.lock files - detected while building rav1e.
-  # Remove with the next version.
-  patch do
-    url "https://github.com/lu-zero/cargo-c/commit/0cb09ac3c1950a2c05758b344131ff7b7a38c42d.patch?full_index=1"
-    sha256 "746a6a9e6fc9a3b15afa9167011a2a06b9c70d7c9adbf7bb20b4e376fa0ab667"
-  end
 
   def install
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
