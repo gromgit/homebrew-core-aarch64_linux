89fa9c3c928bbe4a5ce6dc11ab0cbf7fdac3b9c6
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-03-17 19:54:37 +0000

    glibmm 2.64.0
    Closes #51827.
    
    Signed-off-by: Zach Auten <zachauten@gmail.com>

--- Formula/glibmm.rb
+++ Formula/glibmm.rb
@@ -1,8 +1,8 @@
 class Glibmm < Formula
   desc "C++ interface to glib"
   homepage "https://www.gtkmm.org/"
-  url "https://download.gnome.org/sources/glibmm/2.62/glibmm-2.62.0.tar.xz"
-  sha256 "36659f13cc73282392d1305858f3bbca46fbd1ce2f078cc9db8b9f79b2e93cfe"
+  url "https://download.gnome.org/sources/glibmm/2.64/glibmm-2.64.0.tar.xz"
+  sha256 "46e83d4a123e41784b3b04a7cdcfcdaf918ba8e7e05b2acb10be0e0c09883c12"
 
   bottle do
     cellar :any
