a882a01cd2b53e0568f00d494b63214a676e2c80
Caleb Xu <calebcenter@live.com>
2020-03-16 22:35:01 -0400

    packetq 1.4.2
    Closes #51789.
    
    Signed-off-by: Zach Auten <zachauten@gmail.com>

--- Formula/packetq.rb
+++ Formula/packetq.rb
@@ -1,8 +1,8 @@
 class Packetq < Formula
   desc "SQL-frontend to PCAP-files"
   homepage "https://www.dns-oarc.net/tools/packetq"
-  url "https://www.dns-oarc.net/files/packetq/packetq-1.4.1.tar.gz"
-  sha256 "de374930dcc36c4b23ef4807ac798016fef6f45d29d3464f993df21e154e57d1"
+  url "https://www.dns-oarc.net/files/packetq/packetq-1.4.2.tar.gz"
+  sha256 "696cfcae6d341b910b86673baa428ffa09d00dd42a70d2d3ac57db13c6977122"
 
   bottle do
     cellar :any_skip_relocation
