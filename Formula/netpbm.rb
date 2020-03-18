fda46bc25fe0b233d5994086da77aa1f763b11df
Sam Ford <1584702+samford@users.noreply.github.com>
2020-03-18 14:46:37 -0400

    netpbm 10.86.10
    Closes #51865.
    
    Signed-off-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Formula/netpbm.rb
+++ Formula/netpbm.rb
@@ -3,8 +3,8 @@ class Netpbm < Formula
   homepage "https://netpbm.sourceforge.io/"
   # Maintainers: Look at https://sourceforge.net/p/netpbm/code/HEAD/tree/
   # for stable versions and matching revisions.
-  url "https://svn.code.sf.net/p/netpbm/code/stable", :revision => 3737
-  version "10.86.09"
+  url "https://svn.code.sf.net/p/netpbm/code/stable", :revision => 3750
+  version "10.86.10"
   version_scheme 1
   head "https://svn.code.sf.net/p/netpbm/code/trunk"
 
