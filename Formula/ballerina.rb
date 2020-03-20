0bd3f08d70ff8b1ba5ddf61f6c3e82cc1bcaaf61
chenrui <chenrui333@gmail.com>
2020-03-20 11:20:35 -0400

    ballerina 1.2.0 (#51928)

--- Formula/ballerina.rb
+++ Formula/ballerina.rb
@@ -1,8 +1,8 @@
 class Ballerina < Formula
   desc "Programming Language for Network Distributed Applications"
   homepage "https://ballerina.io"
-  url "https://product-dist.ballerina.io/downloads/1.1.4/ballerina-1.1.4.zip"
-  sha256 "63986437a86c5ed054923cc816fc3e0425b41be68ea2d5e701acf6c360b7d03c"
+  url "https://product-dist.ballerina.io/downloads/1.2.0/ballerina-1.2.0.zip"
+  sha256 "dc55ad6723f33586cdadff050968a42c269bdcdc6b924ba377ffe82a7390bc7d"
 
   bottle :unneeded
 
