2b977129bdd108f234ef20f2120103d46e4f2a6e
chenrui <chenrui333@gmail.com>
2020-03-20 11:31:03 -0400

    docfx 2.51 (#51933)

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -1,8 +1,8 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/releases/download/v2.50/docfx.zip"
-  sha256 "a291a16316384435a955bd75b9d415da4916d9e1b9717b0f4a43111a9f7aa7e4"
+  url "https://github.com/dotnet/docfx/releases/download/v2.51/docfx.zip"
+  sha256 "feaab69b369c55cd27f9f1a6c31c3279dd309ee61db030d93201c44fcbcd3672"
 
   bottle :unneeded
 
