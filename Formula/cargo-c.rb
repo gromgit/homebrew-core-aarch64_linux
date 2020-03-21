680957759a8ced3b5380462c4e2b76d41094d5f0
Rui Chen <chenrui333@gmail.com>
2020-03-21 13:13:39 -0400

    cargo-c 0.6.0

--- Formula/cargo-c.rb
+++ Formula/cargo-c.rb
@@ -1,8 +1,8 @@
 class CargoC < Formula
   desc "Helper program to build and install c-like libraries"
   homepage "https://github.com/lu-zero/cargo-c"
-  url "https://github.com/lu-zero/cargo-c/archive/v0.5.3.tar.gz"
-  sha256 "3a4860898f68e59a1eb9f19c7f12690b6399ddd916c7bc188c1f01cccdade7a9"
+  url "https://github.com/lu-zero/cargo-c/archive/v0.6.0.tar.gz"
+  sha256 "a5b5adce1618596da61221dda06bb1ae780fdcd5a3c231ad1ba7f89758d02c8f"
 
   bottle do
     cellar :any_skip_relocation
@@ -11,13 +11,15 @@ class CargoC < Formula
     sha256 "11900b65b751d322c77428c5f4508cfa47b5e1a7326a4d91ca3ffc8b84541a90" => :high_sierra
   end
 
-  depends_on "rust" => [:build, :test]
+  depends_on "rust" => :build
 
   def install
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
 
   test do
-    system "cargo", "cinstall", "--version"
+    cargo_error = "could not find `Cargo.toml`"
+    assert_match cargo_error, shell_output("#{bin}/cargo-cinstall cinstall 2>&1", 1)
+    assert_match cargo_error, shell_output("#{bin}/cargo-cbuild cbuild 2>&1", 1)
   end
 end
