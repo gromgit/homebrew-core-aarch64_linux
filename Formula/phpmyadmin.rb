67de074b07c2e6cf6f16edd7c0289a7ddfc3e173
bepsvpt <8221099+bepsvpt@users.noreply.github.com>
2020-03-22 01:03:09 +0800

    phpmyadmin 5.0.2 (#51979)
    Co-authored-by: homebrew-updater <bepsvpt/homebrew-updater>

--- Formula/phpmyadmin.rb
+++ Formula/phpmyadmin.rb
@@ -1,8 +1,8 @@
 class Phpmyadmin < Formula
   desc "Web interface for MySQL and MariaDB"
   homepage "https://www.phpmyadmin.net"
-  url "https://files.phpmyadmin.net/phpMyAdmin/5.0.1/phpMyAdmin-5.0.1-all-languages.tar.gz"
-  sha256 "1bd82fdcf5375526c8b5124a20968b69981724307d878321ae29499c61bbf48e"
+  url "https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz"
+  sha256 "8d5cb67de154262b6e51e6ac6967d0931d28ef39cdc7fbec44011d374eb432ae"
 
   bottle :unneeded
 
