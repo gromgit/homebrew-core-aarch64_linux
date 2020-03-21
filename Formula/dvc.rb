2143ac82fa1b125a7e8d4138dab0b947f4b6f7ac
Rui Chen <chenrui333@gmail.com>
2020-03-20 23:24:48 -0400

    dvc 0.90.2
    Closes #51973.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/dvc.rb
+++ Formula/dvc.rb
@@ -3,8 +3,8 @@ class Dvc < Formula
 
   desc "Git for data science projects"
   homepage "https://dvc.org"
-  url "https://github.com/iterative/dvc/archive/0.90.0.tar.gz"
-  sha256 "8fe146e334f07bc023a2bf3c3afc2245e161c70f83786efe334d67cca781cd9a"
+  url "https://github.com/iterative/dvc/archive/0.90.2.tar.gz"
+  sha256 "387c6436c92990b4dbcb0e8818877a1a519e1dea6bf82c7792e618a9da209ce8"
 
   bottle do
     cellar :any
