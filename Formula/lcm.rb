088cfdefde9456d0208e4ffaac00463e1a5210ac
Jonathan Chang <jchang641@gmail.com>
2020-03-21 11:49:47 +1100

    lcm: needs lua

--- Formula/lcm.rb
+++ Formula/lcm.rb
@@ -3,8 +3,7 @@ class Lcm < Formula
   homepage "https://lcm-proj.github.io/"
   url "https://github.com/lcm-proj/lcm/releases/download/v1.4.0/lcm-1.4.0.zip"
   sha256 "e249d7be0b8da35df8931899c4a332231aedaeb43238741ae66dc9baf4c3d186"
-  revision 3
-
+  revision 4
   head "https://github.com/lcm-proj/lcm.git"
 
   bottle do
@@ -17,6 +16,7 @@ class Lcm < Formula
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "glib"
+  depends_on "lua"
   depends_on "openjdk"
   depends_on "python@3.8"
 
