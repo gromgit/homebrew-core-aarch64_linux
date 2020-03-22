0c140a06cecfd821d23401599ed9d5db9d0f34bc
nbari <nbari@tequila.io>
2020-03-22 08:40:21 +0100

    immortal 0.24.3
    Closes #52015.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/immortal.rb
+++ Formula/immortal.rb
@@ -1,8 +1,8 @@
 class Immortal < Formula
   desc "OS agnostic (*nix) cross-platform supervisor"
   homepage "https://immortal.run/"
-  url "https://github.com/immortal/immortal/archive/0.23.0.tar.gz"
-  sha256 "1edc0f7b81897f1afd3bf5b81b44cb6d10060427a392b87af63c573770be018e"
+  url "https://github.com/immortal/immortal/archive/0.24.3.tar.gz"
+  sha256 "e31d5afb9028fb5047b5a2cc5f96c844f6480d600643a12075550f497e65f5cb"
   head "https://github.com/immortal/immortal.git"
 
   bottle do
@@ -13,21 +13,15 @@ class Immortal < Formula
     sha256 "d9f076f1c3010e57e77bb583ba63855f1db92e8fb0cd432cfefe79ce0d26d9b5" => :sierra
   end
 
-  depends_on "dep" => :build
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/immortal/immortal").install buildpath.children
-    cd "src/github.com/immortal/immortal" do
-      system "dep", "ensure", "-vendor-only"
-      ldflags = "-s -w -X main.version=#{version}"
-      system "go", "build", "-ldflags", ldflags, "-o", "#{bin}/immortal", "cmd/immortal/main.go"
-      system "go", "build", "-ldflags", ldflags, "-o", "#{bin}/immortalctl", "cmd/immortalctl/main.go"
-      system "go", "build", "-ldflags", ldflags, "-o", "#{bin}/immortaldir", "cmd/immortaldir/main.go"
-      man8.install Dir["man/*.8"]
-      prefix.install_metafiles
-    end
+    ldflags = "-s -w -X main.version=#{version}"
+    system "go", "build", "-ldflags", ldflags, "-o", "#{bin}/immortal", "cmd/immortal/main.go"
+    system "go", "build", "-ldflags", ldflags, "-o", "#{bin}/immortalctl", "cmd/immortalctl/main.go"
+    system "go", "build", "-ldflags", ldflags, "-o", "#{bin}/immortaldir", "cmd/immortaldir/main.go"
+    man8.install Dir["man/*.8"]
+    prefix.install_metafiles
   end
 
   test do
