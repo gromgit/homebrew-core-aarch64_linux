8c92df0b3bfc24c2506e72ee57856f54ca741e46
Alexander Ronald Altman <alexanderaltman@me.com>
2020-03-22 20:40:37 -0700

    docbook-xsl: introduce stack usage patch and URL rewrites
    see http://www.linuxfromscratch.org/blfs/view/9.1/pst/docbook-xsl.html
    
    Closes #52041.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/docbook-xsl.rb
+++ Formula/docbook-xsl.rb
@@ -3,6 +3,7 @@ class DocbookXsl < Formula
   homepage "https://github.com/docbook/xslt10-stylesheets"
   url "https://github.com/docbook/xslt10-stylesheets/releases/download/release%2F1.79.2/docbook-xsl-nons-1.79.2.tar.bz2"
   sha256 "ee8b9eca0b7a8f89075832a2da7534bce8c5478fc8fc2676f512d5d87d832102"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -21,6 +22,12 @@ class DocbookXsl < Formula
   resource "doc" do
     url "https://github.com/docbook/xslt10-stylesheets/releases/download/release%2F1.79.2/docbook-xsl-doc-1.79.2.tar.bz2"
     sha256 "9bc38a3015717279a3a0620efb2d4bcace430077241ae2b0da609ba67d8340bc"
+  end
+
+  # see http://www.linuxfromscratch.org/blfs/view/9.1/pst/docbook-xsl.html for this patch
+  patch do
+    url "http://www.linuxfromscratch.org/patches/blfs/9.1/docbook-xsl-nons-1.79.2-stack_fix-1.patch"
+    sha256 "a92c39715c54949ba9369add1809527b8f155b7e2a2b2e30cb4b39ee715f2e30"
   end
 
   def install
@@ -44,17 +51,45 @@ class DocbookXsl < Formula
   end
 
   def post_install
-    ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
-    [prefix/"docbook-xsl/catalog.xml", prefix/"docbook-xsl-ns/catalog.xml"].each do |catalog|
-      system "xmlcatalog", "--noout", "--del", "file://#{catalog}", "#{etc}/xml/catalog"
-      system "xmlcatalog", "--noout", "--add", "nextCatalog", "", "file://#{catalog}", "#{etc}/xml/catalog"
+    etc_catalog = etc/"xml/catalog"
+    ENV["XML_CATALOG_FILES"] = etc_catalog
+
+    [
+      ["xsl",    "xsl-nons"],
+      ["xsl-ns", "xsl"],
+    ].each do |nms|
+      old_name = nms[0]
+      new_name = nms[1]
+      loc = "file://#{opt_prefix}/docbook-#{old_name}"
+
+      # add/replace catalog entries
+      cat_loc = "#{loc}/catalog.xml"
+      system "xmlcatalog", "--noout", "--del", cat_loc, etc_catalog
+      system "xmlcatalog", "--noout", "--add", "nextCatalog", "", cat_loc, etc_catalog
+
+      # add rewrites for the new and old catalog URLs
+      [
+        "https://cdn.docbook.org/release/#{new_name}",
+        "http://docbook.sourceforge.net/release/#{old_name}",
+      ].each do |url_prefix|
+        [version.to_s, "current"].each do |ver|
+          system "xmlcatalog", "--noout", "--del", "#{url_prefix}/#{ver}", etc_catalog
+          ["rewriteSystem", "rewriteURI"].each do |k|
+            system "xmlcatalog", "--noout", "--add", k, "#{url_prefix}/#{ver}", loc, etc_catalog
+          end
+        end
+      end
     end
   end
 
   test do
-    system "xmlcatalog", "#{etc}/xml/catalog", "http://cdn.docbook.org/release/xsl/current/"
-    system "xmlcatalog", "#{etc}/xml/catalog", "http://cdn.docbook.org/release/xsl/#{version}/"
-    system "xmlcatalog", "#{etc}/xml/catalog", "http://cdn.docbook.org/release/xsl-nons/current/"
-    system "xmlcatalog", "#{etc}/xml/catalog", "http://cdn.docbook.org/release/xsl-nons/#{version}/"
+    system "xmlcatalog", "#{etc}/xml/catalog", "https://cdn.docbook.org/release/xsl-nons/current/"
+    system "xmlcatalog", "#{etc}/xml/catalog", "https://cdn.docbook.org/release/xsl-nons/#{version}/"
+    system "xmlcatalog", "#{etc}/xml/catalog", "https://cdn.docbook.org/release/xsl/current/"
+    system "xmlcatalog", "#{etc}/xml/catalog", "https://cdn.docbook.org/release/xsl/#{version}/"
+    system "xmlcatalog", "#{etc}/xml/catalog", "http://docbook.sourceforge.net/release/xsl/current/"
+    system "xmlcatalog", "#{etc}/xml/catalog", "http://docbook.sourceforge.net/release/xsl/#{version}/"
+    system "xmlcatalog", "#{etc}/xml/catalog", "http://docbook.sourceforge.net/release/xsl-ns/current/"
+    system "xmlcatalog", "#{etc}/xml/catalog", "http://docbook.sourceforge.net/release/xsl-ns/#{version}/"
   end
 end
