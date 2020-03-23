e81f13c3917e21fdbc2e905e90c0ef5fd643c80b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-23 12:55:28 +0000

    cargo-c: update 0.6.2 bottle.
    Closes #52040.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/cargo-c.rb
+++ Formula/cargo-c.rb
@@ -6,9 +6,9 @@ class CargoC < Formula
 
   bottle do
     cellar :any
-    sha256 "0764047a3f1f74e5a1e326d22877c3d5405c7dc290cf1cd8f67b73ab99d647a0" => :catalina
-    sha256 "18c66958772ae8a83f089edf9b5a7c7686b5a568fb6239d0bf82b2b009e192a6" => :mojave
-    sha256 "f3da39b2fc89a234bb15871020bdb510de266e54aa6df2d845dc6c6fed7a9d6a" => :high_sierra
+    sha256 "9a3ef103003383aa8647d20df11de8fd10ac94a6b0c2efe518101868d70f28b1" => :catalina
+    sha256 "b752165365f28505a61acf3b9496da1faef023ee709274c14529c0efa2d147db" => :mojave
+    sha256 "a7094727446c2a705e9d4a878fca7080e030dd801ace7ed99e1bd195fcaffa8a" => :high_sierra
   end
 
   depends_on "rust" => :build
