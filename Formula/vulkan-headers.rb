21fe4110531f4bd74ef86b7a16550b309d20b4c3
EricFromCanada <enk3@outlook.com>
2020-03-23 11:18:27 -0400

    vulkan-headers 1.2.135
    Closes #52053.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/vulkan-headers.rb
+++ Formula/vulkan-headers.rb
@@ -1,8 +1,8 @@
 class VulkanHeaders < Formula
   desc "Vulkan Header files and API registry"
   homepage "https://github.com/KhronosGroup/Vulkan-Headers"
-  url "https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.2.134.tar.gz"
-  sha256 "723b33a5d01a1538e66f98214f94fa8401b93b37f0af585ea8ff038234c60ee3"
+  url "https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.2.135.tar.gz"
+  sha256 "cfca993d0ce6f33c669e78ee015fd49c65d3a998cd14e92e900a89380e41ae28"
 
   bottle do
     cellar :any_skip_relocation
