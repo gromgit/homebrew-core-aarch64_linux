a36a0cd03067ea6afae2df2262ac416b7c90ab60
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-26 03:14:08 +0000

    prest: update 0.3.4 bottle.

--- Formula/prest.rb
+++ Formula/prest.rb
@@ -6,6 +6,7 @@ class Prest < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "45be2821fcaa9e3d978d041b91ae6157306ee73f6924b0cb0c7ad7e4eb2b3254" => :catalina
     sha256 "bdffe99b9524f0bf10cb23e6a25c3a8eafab8ede6f8303bcb100a8097db9ba23" => :mojave
     sha256 "6a79ee6fc88b5a450348d977b8adf0d14c80aa5ba0bbd22375d1795cf3d8e070" => :high_sierra
     sha256 "3b6a51b5a9c9ee58510fd70416dc8953f037e3575a255b6171363d2364bce1be" => :sierra
