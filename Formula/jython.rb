b9ca952c25fc9c29568e3dee80438a7afe695e55
Caleb Xu <calebcenter@live.com>
2020-03-27 15:55:28 -0400

    jython 2.7.2
    Closes #52231.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/jython.rb
+++ Formula/jython.rb
@@ -1,13 +1,12 @@
 class Jython < Formula
   desc "Python implementation written in Java (successor to JPython)"
   homepage "https://www.jython.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/python/jython-installer/2.7.1/jython-installer-2.7.1.jar"
-  sha256 "6e58dad0b8565b95c6fb14b4bfbf570523d1c5290244cfb33822789fa53b1d25"
+  url "https://search.maven.org/remotecontent?filepath=org/python/jython-installer/2.7.2/jython-installer-2.7.2.jar"
+  sha256 "36e40609567ce020a1de0aaffe45e0b68571c278c14116f52e58cc652fb71552"
 
   # This isn't accidental; there is actually a compile process here.
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
     sha256 "fca25829eec71179473b89517038a63cbe6a0da68594b7b63846b0e9a8194b14" => :catalina
     sha256 "e64dc854f84ba16de19059102aa5c94d01b9ed2a996e3bafa7df1f6f2c19e3ca" => :mojave
     sha256 "0ff369744b44ef3e03dd405e6c479c93a43770778c4f5f6eaea25e98ca5c7edf" => :high_sierra
