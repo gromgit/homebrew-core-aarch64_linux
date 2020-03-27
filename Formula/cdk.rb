a1128c0b3fecfe986dc7a360e1cb06ce8b70710c
Caleb Xu <calebcenter@live.com>
2020-03-27 15:04:00 -0400

    cdk 5.0.20200228
    Closes #52218.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/cdk.rb
+++ Formula/cdk.rb
@@ -1,9 +1,9 @@
 class Cdk < Formula
   desc "Curses development kit provides predefined curses widget for apps"
   homepage "https://invisible-island.net/cdk/"
-  url "https://invisible-mirror.net/archives/cdk/cdk-5.0-20191231.tgz"
-  version "5.0.20191231"
-  sha256 "66ed08f35fa82c837177a55fa245287e8b81fc55cb8c6c40d26b041088a72b26"
+  url "https://invisible-mirror.net/archives/cdk/cdk-5.0-20200228.tgz"
+  version "5.0.20200228"
+  sha256 "b23b55e2f21b4f1a2d6275e0ee017f4acfd5654f9c318080193ea71b7727b3cc"
 
   bottle do
     cellar :any_skip_relocation
