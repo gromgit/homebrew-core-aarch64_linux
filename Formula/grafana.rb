0488312938b3e5744a82bfd2918ae72bd8e853a8
Caleb Xu <calebcenter@live.com>
2020-03-27 15:24:09 -0400

    grafana 6.7.1
    Closes #52224.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/grafana.rb
+++ Formula/grafana.rb
@@ -1,8 +1,8 @@
 class Grafana < Formula
   desc "Gorgeous metric visualizations and dashboards for timeseries databases"
   homepage "https://grafana.com"
-  url "https://github.com/grafana/grafana/archive/v6.7.0.tar.gz"
-  sha256 "7f4e3f0d42b8188a334e97062c3bf63ff43af273095ba10147b299e3c1c5a7b7"
+  url "https://github.com/grafana/grafana/archive/v6.7.1.tar.gz"
+  sha256 "5750d286273069a195679d5586e810b0ca8cdd08ee07dcdd9b52cfaac8c62b89"
   head "https://github.com/grafana/grafana.git"
 
   bottle do
