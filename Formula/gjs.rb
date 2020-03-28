dd63dd3c2fdf1698a71450241b1bff279d4afdd9
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-03-28 07:34:53 +0000

    gjs 1.64.1
    Closes #52268.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/gjs.rb
+++ Formula/gjs.rb
@@ -1,8 +1,8 @@
 class Gjs < Formula
   desc "JavaScript Bindings for GNOME"
   homepage "https://gitlab.gnome.org/GNOME/gjs/wikis/Home"
-  url "https://download.gnome.org/sources/gjs/1.64/gjs-1.64.0.tar.xz"
-  sha256 "66384cbc6b849ea67ced0ad559d589bb15b5a664bbacc75c9f128416ee8ed66f"
+  url "https://download.gnome.org/sources/gjs/1.64/gjs-1.64.1.tar.xz"
+  sha256 "55af83893e99ba2962eca46f47340a4cf8cba5b1966ab00a0f8b9c3034e9987c"
 
   bottle do
     sha256 "c278f1bc9596525a600c243c8911d087b93cd9d22ffc64327b2af7483aafa78f" => :catalina
