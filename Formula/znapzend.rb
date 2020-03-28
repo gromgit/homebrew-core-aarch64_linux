63d29811350d36b8b9464d6bef4544b31db8b8cd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-28 05:04:43 +0000

    znapzend: update 0.20.0 bottle.

--- Formula/znapzend.rb
+++ Formula/znapzend.rb
@@ -7,11 +7,9 @@ class Znapzend < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5d52960b6314938d41a65d7b441c3a6c8ef211cd1ca5e7388b59af0a92cb61b7" => :catalina
-    sha256 "7962667b10e7f44b35469d4b5ef1a8e4aeaf75310b34e72840edfc283f1aae16" => :mojave
-    sha256 "7fca9b0f4b129afbc30bee164b1a73911e4da9e48c4943b1d6e7e8e77a1cec17" => :high_sierra
-    sha256 "f12ede845017559f77147ceab275fcbb128f3bdec7a1ff0cd72d54029349a419" => :sierra
-    sha256 "d88080d21fd9def227853fc0d08db6bef5a10d0bca74c16c0207f023aabc8d67" => :el_capitan
+    sha256 "37b73cba5b7ed887b1e72175d3c601c08e449ed3bbcaa87668641704477889d1" => :catalina
+    sha256 "9a508c6a3fb15609b3552ce38369b16664f08515f635bd8a3dc92ed79d17d381" => :mojave
+    sha256 "18e1269f3ab2964382c1cc7578fa8785ee7ba1412a1c247861d76accde2a6cc5" => :high_sierra
   end
 
   depends_on "perl" if MacOS.version == :mavericks
