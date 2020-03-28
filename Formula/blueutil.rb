18ad3ed388a1531fede985e430514cd1edb16953
Rui Chen <chenrui333@gmail.com>
2020-03-27 20:55:01 -0400

    blueutil 2.6.0
    Closes #52251.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/blueutil.rb
+++ Formula/blueutil.rb
@@ -1,8 +1,8 @@
 class Blueutil < Formula
   desc "Get/set bluetooth power and discoverable state"
   homepage "https://github.com/toy/blueutil"
-  url "https://github.com/toy/blueutil/archive/v2.5.1.tar.gz"
-  sha256 "a53f82a5b743199600854bebcb79215d30444b0c7d71e0e41213122ecfe64b47"
+  url "https://github.com/toy/blueutil/archive/v2.6.0.tar.gz"
+  sha256 "5ba90cdedd886566e1304813891c0f9f6139db67aaf2829a8294973ee3d0b66c"
   head "https://github.com/toy/blueutil.git"
 
   bottle do
