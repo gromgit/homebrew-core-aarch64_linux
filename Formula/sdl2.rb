8afb48d37f57395fcb2ddf6074b5765593e45c12
Bo Anderson <mail@boanderson.me>
2020-03-28 02:55:14 +0000

    sdl2: apply CMake config patch
    Fixes #52261.

--- Formula/sdl2.rb
+++ Formula/sdl2.rb
@@ -3,6 +3,7 @@ class Sdl2 < Formula
   homepage "https://www.libsdl.org/"
   url "https://libsdl.org/release/SDL2-2.0.12.tar.gz"
   sha256 "349268f695c02efbc9b9148a70b85e58cefbbf704abd3e91be654db7f1e2c863"
+  revision 1
 
   bottle do
     cellar :any
@@ -17,6 +18,13 @@ class Sdl2 < Formula
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
+  end
+
+  # Fix library extension in CMake config file.
+  # https://bugzilla.libsdl.org/show_bug.cgi?id=5039
+  patch do
+    url "https://bugzilla.libsdl.org/attachment.cgi?id=4263"
+    sha256 "07ea066e805f82d85e6472e767ba75d265cb262053901ac9a9e22c5f8ff187a5"
   end
 
   def install
