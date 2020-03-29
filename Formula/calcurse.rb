1b969538f206026e202478fc20c0aca004e1a225
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-29 17:01:10 +0000

    calcurse: update 4.6.0 bottle.
    Closes #52253.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/calcurse.rb
+++ Formula/calcurse.rb
@@ -6,9 +6,9 @@ class Calcurse < Formula
   head "https://git.calcurse.org/calcurse.git"
 
   bottle do
-    sha256 "8e9e4538d02408549659f6eb7512a8f9fdd9d2c38369967bd5d9aba67519e7d9" => :catalina
-    sha256 "2b88932ea815161ce2a1d9af1d8e56612aea158255e991246b0da92e1fe11365" => :mojave
-    sha256 "9318cecc6785ff6ac34439793e88a904eebafd9c7fd84d543157ffb6a405cf18" => :high_sierra
+    sha256 "21cb64443bbffe19f0adb8dfc8f5e49eabed5fc5c0347215bbb7aa3383126719" => :catalina
+    sha256 "0d114c613163c192e9f156750ffa9c3c9a4a303c103a7bc7091c369da151a4c4" => :mojave
+    sha256 "41886cc820da1133c6c5f57fea4f9fbe072a0735e3ef8522a9301736216f4e70" => :high_sierra
   end
 
   depends_on "gettext"
