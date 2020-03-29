2d18ad0b66af7c3e37c401e89b9da2fe89801047
Rui Chen <chenrui333@gmail.com>
2020-03-28 22:50:22 -0400

    euler-py 1.4.0
    euler-py: use python 3.8
    
    Closes #52288.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/euler-py.rb
+++ Formula/euler-py.rb
@@ -1,9 +1,8 @@
 class EulerPy < Formula
   desc "Project Euler command-line tool written in Python"
   homepage "https://github.com/iKevinY/EulerPy"
-  url "https://github.com/iKevinY/EulerPy/archive/v1.3.0.tar.gz"
-  sha256 "ffe2d74b5a0fbde84a96dfd39f1f899fc691e3585bf0d46ada976899038452e1"
-  revision 2
+  url "https://github.com/iKevinY/EulerPy/archive/v1.4.0.tar.gz"
+  sha256 "0d2f633bc3985c8acfd62bc76ff3f19d0bfb2274f7873ec7e40c2caef315e46d"
   head "https://github.com/iKevinY/EulerPy.git"
 
   bottle do
@@ -13,7 +12,7 @@ class EulerPy < Formula
     sha256 "cf75217697b88543c07748ec0f75d1ca39389bd92bf5d5247da0b525c7becaf2" => :high_sierra
   end
 
-  depends_on "python"
+  depends_on "python@3.8"
 
   resource "click" do
     url "https://files.pythonhosted.org/packages/source/c/click/click-4.0.tar.gz"
@@ -21,6 +20,8 @@ class EulerPy < Formula
   end
 
   def install
+    ENV["PYTHON"] = Formula["python@3.8"].opt_bin/"python3"
+
     xy = Language::Python.major_minor_version "python3"
     ENV.prepend_create_path "PYTHONPATH", "#{libexec}/lib/python#{xy}/site-packages"
     resource("click").stage do
