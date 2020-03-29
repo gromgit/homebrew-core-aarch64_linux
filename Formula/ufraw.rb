16a03401842a0c2805d8146d6eee40e93a74d4a0
Bo Anderson <mail@boanderson.me>
2020-03-29 17:23:37 +0100

    ufraw: readd stack check workaround
    This reverts commit da5e7d1a28e0735906a412edac74b8e8e9f0391e.

--- Formula/ufraw.rb
+++ Formula/ufraw.rb
@@ -36,6 +36,10 @@ class Ufraw < Formula
   end
 
   def install
+    # Work around Xcode 11 clang bug
+    # https://bitbucket.org/multicoreware/x265/issues/514/wrong-code-generated-on-macos-1015
+    ENV.append_to_cflags "-fno-stack-check" if DevelopmentTools.clang_build_version >= 1010
+
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--without-gtk",
