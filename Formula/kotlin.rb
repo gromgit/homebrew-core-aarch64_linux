12a5979df8f65f6d5984cf551bb66b7160317fcc
chenrui <chenrui333@gmail.com>
2020-03-29 10:33:43 -0400

    kotlin 1.3.71 (#52297)

--- Formula/kotlin.rb
+++ Formula/kotlin.rb
@@ -1,8 +1,8 @@
 class Kotlin < Formula
   desc "Statically typed programming language for the JVM"
   homepage "https://kotlinlang.org/"
-  url "https://github.com/JetBrains/kotlin/releases/download/v1.3.70/kotlin-compiler-1.3.70.zip"
-  sha256 "709d782ff707a633278bac4c63bab3026b768e717f8aaf62de1036c994bc89c7"
+  url "https://github.com/JetBrains/kotlin/releases/download/v1.3.71/kotlin-compiler-1.3.71.zip"
+  sha256 "7adb77dad99c6d2f7bde9f8bafe4c6244a04587a8e36e62b074d00eda9f8e74a"
 
   bottle :unneeded
 
