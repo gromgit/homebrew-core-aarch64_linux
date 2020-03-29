3dae974fbab98d98a091e1998eed1bc5931a9f15
Bo Anderson <mail@boanderson.me>
2020-03-29 17:22:52 +0100

    mupdf-tools: readd stack check workaround
    This reverts commit 49c33cf9e1a7dd2ee63bf3034934ca68bdc43095.

--- Formula/mupdf-tools.rb
+++ Formula/mupdf-tools.rb
@@ -16,6 +16,9 @@ class MupdfTools < Formula
     :because => "mupdf and mupdf-tools install the same binaries."
 
   def install
+    # Work around Xcode 11 clang bug
+    ENV.append_to_cflags "-fno-stack-check" if DevelopmentTools.clang_build_version >= 1010
+
     system "make", "install",
            "build=release",
            "verbose=yes",
