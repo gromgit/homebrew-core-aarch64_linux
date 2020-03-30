eee9238b4ca0c18a7a2915fd288625caa5c62024
Michka Popoff <michkapopoff@gmail.com>
2020-03-30 13:15:34 +0200

    zim: revision bump for python@3.8

--- Formula/zim.rb
+++ Formula/zim.rb
@@ -3,6 +3,7 @@ class Zim < Formula
   homepage "https://zim-wiki.org/"
   url "https://github.com/jaap-karssenberg/zim-desktop-wiki/archive/0.72.1.tar.gz"
   sha256 "ba02e418b4fb1d7847f96b49ada8c917c881a28bb5fb55dcdca54be7b3fd196a"
+  revision 1
   head "https://github.com/jaap-karssenberg/zim-desktop-wiki.git"
 
   bottle do
@@ -19,7 +20,7 @@ class Zim < Formula
   depends_on "gtk+3"
   depends_on "gtksourceview3"
   depends_on "pygobject3"
-  depends_on "python"
+  depends_on "python@3.8"
 
   resource "pyxdg" do
     url "https://files.pythonhosted.org/packages/47/6e/311d5f22e2b76381719b5d0c6e9dc39cd33999adae67db71d7279a6d70f4/pyxdg-0.26.tar.gz"
@@ -27,13 +28,14 @@ class Zim < Formula
   end
 
   def install
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python3.7/site-packages"
+    python_version = Language::Python.major_minor_version Formula["python@3.8"].opt_bin/"python3"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{python_version}/site-packages"
     resource("pyxdg").stage do
-      system "python", *Language::Python.setup_install_args(libexec/"vendor")
+      system Formula["python@3.8"].opt_bin/"python3", *Language::Python.setup_install_args(libexec/"vendor")
     end
     ENV["XDG_DATA_DIRS"] = libexec/"share"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python3.7/site-packages"
-    system "python3", "./setup.py", "install", "--prefix=#{libexec}"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{python_version}/site-packages"
+    system Formula["python@3.8"].opt_bin/"python3", "./setup.py", "install", "--prefix=#{libexec}"
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files libexec/"bin",
       :PYTHONPATH    => ENV["PYTHONPATH"],
