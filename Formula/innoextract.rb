84457ad6708b334a8616ccd46549c9530ffbc75a
Konstantin Mochalov <incredible.angst@gmail.com>
2020-04-01 23:37:03 +0300

    innoextract: don't patch when using HEAD version (#52351)

--- Formula/innoextract.rb
+++ Formula/innoextract.rb
@@ -1,9 +1,18 @@
 class Innoextract < Formula
   desc "Tool to unpack installers created by Inno Setup"
   homepage "https://constexpr.org/innoextract/"
-  url "https://constexpr.org/innoextract/files/innoextract-1.8.tar.gz"
-  sha256 "5e78f6295119eeda08a54dcac75306a1a4a40d0cb812ff3cd405e9862c285269"
   head "https://github.com/dscharrer/innoextract.git"
+  stable do
+    url "https://constexpr.org/innoextract/files/innoextract-1.8.tar.gz"
+    sha256 "5e78f6295119eeda08a54dcac75306a1a4a40d0cb812ff3cd405e9862c285269"
+
+    # Boost 1.70+ compatibility. Remove with next release. b47f46 is
+    # already in master.
+    patch do
+      url "https://github.com/dscharrer/innoextract/commit/b47f46102bccf1d813ca159230029b0cd820ceff.patch?full_index=1"
+      sha256 "92d321d552a65e16ae6df992a653839fb19de79aa77388c651bf57b3c582d546"
+    end
+  end
 
   bottle do
     cellar :any
@@ -15,12 +24,6 @@ class Innoextract < Formula
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "xz"
-
-  # Boost 1.70+ compatibility. Remove with next release.
-  patch do
-    url "https://github.com/dscharrer/innoextract/commit/b47f46102bccf1d813ca159230029b0cd820ceff.patch?full_index=1"
-    sha256 "92d321d552a65e16ae6df992a653839fb19de79aa77388c651bf57b3c582d546"
-  end
 
   def install
     system "cmake", ".", *std_cmake_args
