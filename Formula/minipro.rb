ca239a1bd80d8ba85d95c39fad278e01c0440e22
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-01 03:04:46 +0000

    minipro: add 0.4 bottle.

--- Formula/minipro.rb
+++ Formula/minipro.rb
@@ -4,6 +4,13 @@ class Minipro < Formula
   url "https://gitlab.com/DavidGriffith/minipro/-/archive/0.4/minipro-0.4.tar.gz"
   sha256 "05e0090eab33a236992f5864f3485924fb5dfad95d8f16916a17296999c094cc"
 
+  bottle do
+    cellar :any
+    sha256 "eca0b4a6206491f7d429f75032bf16312078ac7bd36fffc8b97dc19b9999dbc4" => :catalina
+    sha256 "a636a9d69994308ecc1340ed93285171e04c5c25f3ad7c970d31d11e7c6123cd" => :mojave
+    sha256 "0969900d68207aab7fc9edca1d49f8816d2bf48a754af904ffa57466463b9c28" => :high_sierra
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libusb"
   depends_on "srecord"
