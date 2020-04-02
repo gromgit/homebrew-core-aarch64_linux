c1450b41d39bc8ea9cf2d4675e2b23bf54a3d00b
Bo Anderson <mail@boanderson.me>
2020-04-02 14:37:49 +0000

    atk: update 2.36.0 bottle.

--- Formula/atk.rb
+++ Formula/atk.rb
@@ -6,10 +6,9 @@ class Atk < Formula
 
   bottle do
     cellar :any
-    sha256 "01909cda5426d86c09f354fae84bdf29fdc42428b42d07eb799a49452cac7909" => :catalina
-    sha256 "481a81e57b58fd84251bd10a364433c5558802084f2dc4e459515b27703c6abb" => :mojave
-    sha256 "f80df2351f0b557484f7eb7c3b6dbd34e73dfdedd07a8cf0f1fd56be155f615f" => :high_sierra
-    sha256 "ec44e1cc0f0c110579b3e2a339bff88a9455f187cadd4cac3eec420cf2347ffe" => :sierra
+    sha256 "1065293046ab2984940dfa0b9c9e724439838e63f685c932d508ccd74bcf921b" => :catalina
+    sha256 "68c7b621339c03964036877987db69806f663612ba275e68554a97d218a2b5b4" => :mojave
+    sha256 "fa8f525bfeacab676f795bac37f622fc100e63c9e9661fbd6ddd3e1725ebd097" => :high_sierra
   end
 
   depends_on "gobject-introspection" => :build
