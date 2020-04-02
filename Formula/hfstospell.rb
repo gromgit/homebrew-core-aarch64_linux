9ec2ed74a0a006f38ab65f34de38d68bb2a7e053
Pavel Omelchenko <p.Omelchenko@gmail.com>
2020-04-03 01:00:16 +0300

    hfstospell: revision for icu4c

--- Formula/hfstospell.rb
+++ Formula/hfstospell.rb
@@ -3,6 +3,7 @@ class Hfstospell < Formula
   homepage "https://hfst.github.io/"
   url "https://github.com/hfst/hfst-ospell/releases/download/v0.5.1/hfstospell-0.5.1.tar.gz"
   sha256 "ccf5f3b06bcdc5636365e753b9f7fad9c11dfe483272061700a905b3d65ac750"
+  revision 1
 
   bottle do
     cellar :any
