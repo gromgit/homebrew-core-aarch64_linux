c78114de1252ac63590b06c1f2325e576a5d5226
Pavel Omelchenko <p.Omelchenko@gmail.com>
2020-04-03 00:58:01 +0300

    icu4c 66.1

--- Formula/icu4c.rb
+++ Formula/icu4c.rb
@@ -1,9 +1,9 @@
 class Icu4c < Formula
   desc "C/C++ and Java libraries for Unicode and globalization"
   homepage "http://site.icu-project.org/home"
-  url "https://github.com/unicode-org/icu/releases/download/release-64-2/icu4c-64_2-src.tgz"
-  version "64.2"
-  sha256 "627d5d8478e6d96fc8c90fed4851239079a561a6a8b9e48b0892f24e82d31d6c"
+  url "https://github.com/unicode-org/icu/releases/download/release-66-1/icu4c-66_1-src.tgz"
+  version "66.1"
+  sha256 "52a3f2209ab95559c1cf0a14f24338001f389615bf00e2585ef3dbc43ecf0a2e"
 
   bottle do
     cellar :any
