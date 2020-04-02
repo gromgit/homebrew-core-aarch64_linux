017fd67c9d9ebf04859b477593d1901f9c501ab0
Pavel Omelchenko <p.Omelchenko@gmail.com>
2020-04-03 01:03:07 +0300

    pazpar2: revision for icu4c

--- Formula/pazpar2.rb
+++ Formula/pazpar2.rb
@@ -3,6 +3,7 @@ class Pazpar2 < Formula
   homepage "https://www.indexdata.com/pazpar2"
   url "http://ftp.indexdata.dk/pub/pazpar2/pazpar2-1.14.0.tar.gz"
   sha256 "3b0012450c66d6932009ac0decb72436690cc939af33e2ad96c0fec85863d13d"
+  revision 1
 
   bottle do
     cellar :any
