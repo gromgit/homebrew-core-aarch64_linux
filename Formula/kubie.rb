7fd70ec052f028015c22e36ab18c19b7729b2ad5
Cassiano Leal <git@c10l.cc>
2020-04-03 15:52:51 +0100

    Add kubie 0.9.1 (new formula)
    `brew tests` fails but they are the same failures present in `master`.
    
    All the other tests in the "How To Open a Homebrew Pull Request" document pass.
    
    Please let me know if there are any changes needed in this formula.
    
    Closes #52502.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: chenrui <chenrui333@gmail.com>

---
+++ Formula/kubie.rb
@@ -0,0 +1,43 @@
+class Kubie < Formula
+  desc "Much more powerful alternative to kubectx and kubens"
+  homepage "https://blog.sbstp.ca/introducing-kubie/"
+  url "https://github.com/sbstp/kubie/archive/v0.9.1.tar.gz"
+  sha256 "8f7926b1bcd0421247231353b6969c36498add3b88b55103349df3da763a5f42"
+  license "Zlib"
+
+  depends_on "rust" => :build
+  depends_on "kubernetes-cli" => :test
+
+  def install
+    system "cargo", "install", *std_cargo_args
+    bash_completion.install "./completion/kubie.bash"
+  end
+
+  test do
+    mkdir_p testpath/".kube"
+    (testpath/".kube/kubie-test.yaml").write <<~EOS
+      apiVersion: v1
+      clusters:
+      - cluster:
+          server: http://0.0.0.0/
+        name: kubie-test-cluster
+      contexts:
+      - context:
+          cluster: kubie-test-cluster
+          user: kubie-test-user
+          namespace: kubie-test-namespace
+        name: kubie-test
+      current-context: baz
+      kind: Config
+      preferences: {}
+      users:
+      - user:
+        name: kubie-test-user
+    EOS
+
+    assert_match "kubie #{version}", shell_output("#{bin}/kubie --version")
+
+    assert_match "The connection to the server 0.0.0.0 was refused - did you specify the right host or port?",
+      shell_output("#{bin}/kubie exec kubie-test kubie-test-namespace kubectl get pod 2>&1")
+  end
+end
