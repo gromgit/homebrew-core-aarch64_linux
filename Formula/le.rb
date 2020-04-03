4dd7b010ff83154e5f8802848d9aebddda438a4a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-03 05:42:04 +0000

    le: update 1.16.7 bottle.

--- Formula/le.rb
+++ Formula/le.rb
@@ -5,11 +5,9 @@ class Le < Formula
   sha256 "1cbe081eba31e693363c9b8a8464af107e4babfd2354a09a17dc315b3605af41"
 
   bottle do
-    sha256 "8f923693a632a447888ac16d766e2318aed6af1400663de3a1ad9f27da850109" => :mojave
-    sha256 "614516096f3fa5bb72f68fa747e611401098239d72c7e31274b1fdf41a4d0c59" => :high_sierra
-    sha256 "bb670b11e7bf2813cf89cf35ad7e04b67c330cef6c1c07ab5b65fbe1879af67a" => :sierra
-    sha256 "285e9bf7c3debedb6286ca34971d939c673a35d4553817fdc48dc915e649ba0c" => :el_capitan
-    sha256 "82e83a7c1f1a030429b013d4fd138327003605193e683e092924e80b5e02bf8e" => :yosemite
+    sha256 "704e7762fb13634aa7b2fe4cc271747894d8ffcf5028abd0d27497bceb6bc378" => :catalina
+    sha256 "aa1144661f13ab5fbe4eb132415da66785ab1b903c8d517df03f40826d08632f" => :mojave
+    sha256 "b6fad9458d040f9a47a0d3ff003ab5f77cdb9508a5b653c3cddc201cfb5310e2" => :high_sierra
   end
 
   def install
