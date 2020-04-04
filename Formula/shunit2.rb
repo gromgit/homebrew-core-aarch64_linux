8c671866ffedbf61392a359afbf51a94d9775cc1
Caleb Xu <calebcenter@live.com>
2020-04-04 07:10:51 -0400

    shunit2 2.1.8 (#52534)

--- Formula/shunit2.rb
+++ Formula/shunit2.rb
@@ -1,8 +1,8 @@
 class Shunit2 < Formula
   desc "xUnit unit testing framework for Bourne-based shell scripts"
   homepage "https://github.com/kward/shunit2"
-  url "https://github.com/kward/shunit2/archive/v2.1.7.tar.gz"
-  sha256 "41a103aa984bfdeea3fec426edb0b3dda19153a5ce448d4b4bbe52a85c4d7fee"
+  url "https://github.com/kward/shunit2/archive/v2.1.8.tar.gz"
+  sha256 "b2fed28ba7282e4878640395284e43f08a029a6c27632df73267c8043c71b60c"
 
   bottle :unneeded
 
