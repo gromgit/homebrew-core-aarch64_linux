a73e39d29f1c7b4a016b9daf3e58dbaee122b7c0
Bo Anderson <mail@boanderson.me>
2020-04-04 11:18:13 +0000

    cjson: update 1.7.13 bottle.

--- Formula/cjson.rb
+++ Formula/cjson.rb
@@ -6,11 +6,9 @@ class Cjson < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "b402e60f6a786780c9739b91845c2f8df2ea60ce8749e42db11cbcc5f86bb52a" => :catalina
-    sha256 "3364af8e12165cfd7721e8d64e1a4ee021a2fe2fc64b09726a8624f262d8449f" => :mojave
-    sha256 "f8a92b03d0ff1147d604517317366706e93cb398ffa8e208b84b4bd91282231e" => :high_sierra
-    sha256 "be61141bcc7cd3706051ead5a2158b7a92f840e0802f745e4c53052a82616d55" => :sierra
+    sha256 "082bbc54aa1bf574900bc7ee017435d01888fd854953c6de1a6d62124fcd5306" => :catalina
+    sha256 "71a46c6fb3f06ed82cbb3009b5daed3b1083c6e7240760230ba900cb80267912" => :mojave
+    sha256 "76bde53a90ac3e1d62ae20283170392948bfce48046c6204e1114c1624dfa4aa" => :high_sierra
   end
 
   depends_on "cmake" => :build
