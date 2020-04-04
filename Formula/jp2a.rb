9722731ba8ac20ff59c8f057590198c73008f205
Illya Moskvin <imoskvin@artic.edu>
2020-04-04 16:19:12 -0500

    jp2a 1.0.6 (downgrade)
    Closes #52582.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/jp2a.rb
+++ Formula/jp2a.rb
@@ -1,8 +1,14 @@
 class Jp2a < Formula
   desc "Convert JPG images to ASCII"
   homepage "https://csl.name/jp2a/"
-  url "https://github.com/cslarsen/jp2a/archive/v1.0.7.tar.gz"
-  sha256 "e509d8bbf9434afde5c342568b21d11831a61d9942ca8cb1633d4295b7bc5059"
+  # Do not change source from SourceForge to GitHub until this issue is resolved:
+  # https://github.com/cslarsen/jp2a/issues/8
+  # Currently, GitHub only has jp2a v1.0.7, which is broken as described above.
+  # jp2a v1.0.6 is stable, but it is only available on SourceForge, not GitHub.
+  url "https://downloads.sourceforge.net/project/jp2a/jp2a/1.0.6/jp2a-1.0.6.tar.gz"
+  sha256 "0930ac8a9545c8a8a65dd30ff80b1ae0d3b603f2ef83b04226da0475c7ccce1c"
+  revision 1
+  version_scheme 1
 
   bottle do
     cellar :any
@@ -12,13 +18,12 @@ class Jp2a < Formula
     sha256 "a45231943df5bffc1589114b20b5b6c9745f909fd1e85db63da40e28bec02709" => :sierra
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
   depends_on "jpeg"
 
   def install
-    system "autoreconf", "-ivf"
-    system "./configure", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make", "install"
   end
