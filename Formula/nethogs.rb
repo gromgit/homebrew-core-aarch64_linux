ba18eb938954b9c1b6bcd87789cc41197ea2fb22
Bo Anderson <mail@boanderson.me>
2020-04-04 11:06:44 +0000

    nethogs: update 0.8.6 bottle.

--- Formula/nethogs.rb
+++ Formula/nethogs.rb
@@ -6,13 +6,9 @@ class Nethogs < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "cc7d9af4eebb27c162e280dd551742ed0346cc807e45980e2ce6de8b1f8b26ff" => :catalina
-    sha256 "a43516ede7e2046c39f1984e4afaee53a3705a81d78491827e33a6a3f631bed8" => :mojave
-    sha256 "b70632b33aeb7f23c93cbd27cdb36d49382f3619684c32b60c76129a5d64195e" => :high_sierra
-    sha256 "25731b08cad9b8f5a420172bab21d6986d49ee7044cbdd3a7717c6b10ee8da8d" => :sierra
-    sha256 "f133cdfd28bd88778241a607fda39d17419d8aaf24fd6de49d8a086e112fc7bf" => :el_capitan
-    sha256 "aa00775f5f8add09c031b13d3ec2575b082da47aa573e869288b28cad879f431" => :yosemite
-    sha256 "3652cadd0558c01c522e586a483b03b95badf98028ab3b7f9a227f8bae275def" => :mavericks
+    sha256 "6f6124fdf95847d5a49d15733e1bebb0b7060995f7ad672863fcd89bb985ef1d" => :catalina
+    sha256 "e61f5ad80657ad381414c992a391d454689f6845965387c7b803f115b5fd72b4" => :mojave
+    sha256 "f994c61fe07025f7c18de9a15be44c3de107e12c19cde6e3cd53a892cc61b7b4" => :high_sierra
   end
 
   uses_from_macos "libpcap"
