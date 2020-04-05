e95acb4f6045f7a57e012bba9d0d8e16ad22f1b6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-05 10:39:58 +0000

    pytouhou: update 634_7 bottle.

--- Formula/pytouhou.rb
+++ Formula/pytouhou.rb
@@ -9,6 +9,7 @@ class Pytouhou < Formula
   bottle do
     cellar :any
     sha256 "e0470e99f10a318e152eeb46e7e555271234e1ee256d26d9115d947bd810beca" => :catalina
+    sha256 "038adcb10d7aa824288b6a67c85cdd7b950d6f3ec5bfefc9a2a37f55c6c96026" => :mojave
     sha256 "5222b26c38e279f14622baf64a1d2dfab9decf6920533b353eed61b9d9f6b411" => :high_sierra
   end
 
