9a04bbe395a3a5dc28d3de486f81dc0f8221804e
Alexander Bayandin <a.bayandin@gmail.com>
2020-04-05 15:27:23 +0100

    inlets: use free_port instead of TCPServer.new(0)
    Co-authored-by: Sean Molenaar <smillerdev@me.com>

--- Formula/inlets.rb
+++ Formula/inlets.rb
@@ -35,47 +35,33 @@ class Inlets < Formula
     Process.wait(pid)
   end
 
-  MOCK_RESPONSE = "INLETS OK".freeze
-  SECRET_TOKEN = "itsasecret-sssshhhhh".freeze
-
   test do
-    upstream_server = TCPServer.new(0)
-    upstream_port = upstream_server.addr[1]
-    remote_server = TCPServer.new(0)
-    remote_port = remote_server.addr[1]
-    upstream_server.close
-    remote_server.close
-
-    puts "Starting mock server on: localhost:#{upstream_port}"
+    upstream_port = free_port
+    remote_port = free_port
+    MOCK_RESPONSE = "INLETS OK".freeze
+    SECRET_TOKEN = "itsasecret-sssshhhhh".freeze
 
     (testpath/"mock_upstream_server.rb").write <<~EOS
       require 'socket'
-
       server = TCPServer.new('localhost', #{upstream_port})
-
       loop do
         socket = server.accept
         request = socket.gets
         STDERR.puts request
-
         response = "OK\\n"
         shutdown = false
-
         if request.include? "inlets-test"
           response = "#{MOCK_RESPONSE}\\n"
           shutdown = true
         end
-
         socket.print "HTTP/1.1 200 OK\\r\\n" +
                     "Host: localhost:#{upstream_port}\\r\\n" +
                     "Content-Type: text/plain\\r\\n" +
                     "Content-Length: \#\{response.bytesize\}\\r\\n" +
                     "Connection: close\\r\\n"
-
         socket.print "\\r\\n"
         socket.print response
         socket.close
-
         if shutdown
           puts "Exiting test server"
           exit 0
@@ -88,9 +74,6 @@ class Inlets < Formula
     end
 
     begin
-      require "uri"
-      require "net/http"
-
       stable_resource = stable.instance_variable_get(:@resource)
       commit = stable_resource.instance_variable_get(:@specs)[:revision]
 
@@ -103,28 +86,19 @@ class Inlets < Formula
       # This test involves establishing a client-server inlets tunnel on the
       # remote_port, running a mock server on the upstream_port and then
       # testing that we can hit the mock server upstream_port via the tunnel remote_port
-      puts "Waiting for mock server"
-      sleep 3
+      sleep 3 # Waiting for mock server
       server_pid = fork do
-        puts "Starting inlets server with port #{remote_port}"
         exec "#{bin}/inlets server --port #{remote_port} --token #{SECRET_TOKEN}"
       end
 
       client_pid = fork do
-        puts "Starting inlets client with remote localhost:#{remote_port}, " \
-             "upstream localhost:#{upstream_port}, token: #{SECRET_TOKEN}"
+        # Starting inlets client
         exec "#{bin}/inlets client --remote localhost:#{remote_port} " \
              "--upstream localhost:#{upstream_port} --token #{SECRET_TOKEN}"
       end
 
-      puts "Waiting for inlets websocket tunnel"
-      sleep 3
-
-      uri = URI("http://localhost:#{remote_port}/inlets-test")
-      puts "Querying upstream endpoint via inlets remote: #{uri}"
-      response = Net::HTTP.get_response(uri)
-      assert_match MOCK_RESPONSE, response.body
-      assert_equal response.code, "200"
+      sleep 3 # Waiting for inlets websocket tunnel
+      assert_match MOCK_RESPONSE, shell_output("curl -s http://localhost:#{remote_port}/inlets-test")
     ensure
       cleanup("Mock Server", mock_upstream_server_pid)
       cleanup("Inlets Server", server_pid)
