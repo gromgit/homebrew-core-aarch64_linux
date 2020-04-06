0cb6c05d77d3f9129b08df3901c9ecb058b9a1c9
Bo Anderson <mail@boanderson.me>
2020-04-06 02:25:23 +0100

    vtk: revision bump for netcdf
    Closes #52604.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/vtk.rb
+++ Formula/vtk.rb
@@ -1,7 +1,7 @@
 class Vtk < Formula
   desc "Toolkit for 3D computer graphics, image processing, and visualization"
   homepage "https://www.vtk.org/"
-  revision 9
+  revision 10
   head "https://github.com/Kitware/VTK.git"
 
   stable do
