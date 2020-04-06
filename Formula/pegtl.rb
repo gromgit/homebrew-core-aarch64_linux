d3acae65d331a228b87c027137e22112913088e2
Caleb Xu <calebcenter@live.com>
2020-04-06 01:22:13 -0400

    pegtl 2.8.2
    Closes #52614.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/pegtl.rb
+++ Formula/pegtl.rb
@@ -1,8 +1,8 @@
 class Pegtl < Formula
   desc "Parsing Expression Grammar Template Library"
   homepage "https://github.com/taocpp/PEGTL"
-  url "https://github.com/taocpp/PEGTL/archive/2.8.1.tar.gz"
-  sha256 "d23f81a60896cdd8c2ce0d1b31737fe0e1137915ba3fdb4f366ae1a997a1bce6"
+  url "https://github.com/taocpp/PEGTL/archive/2.8.2.tar.gz"
+  sha256 "1d7db20502125a75fbb583c7020038d4faeb6812de504147b23b3db4ef329ca3"
 
   bottle :unneeded
 
