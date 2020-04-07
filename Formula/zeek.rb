5ab9766e70776ef1702f8d40c8ef5159252c31be
Mike McQuaid <mike@mikemcquaid.com>
2020-04-07 14:14:07 +0100

    zeek: remove invalid uses_from_macos.

--- Formula/zeek.rb
+++ Formula/zeek.rb
@@ -21,7 +21,6 @@ class Zeek < Formula
 
   uses_from_macos "flex"
   uses_from_macos "libpcap"
-  uses_from_macos "python@2" # See https://github.com/zeek/zeek/issues/706
 
   def install
     mkdir "build" do
