3f32d19e96928a37b4050368d19f2c254246c5fe
Bo Anderson <mail@boanderson.me>
2020-04-07 11:52:13 +0000

    ejabberd: update 20.03 bottle.

--- Formula/ejabberd.rb
+++ Formula/ejabberd.rb
@@ -6,9 +6,9 @@ class Ejabberd < Formula
 
   bottle do
     cellar :any
-    sha256 "aa28c86c26a48f1e5bf1138add19cdbb8b6b07a5fab9e3d35beda6969aa9b020" => :catalina
-    sha256 "d5dd3cd458519ee5f531c5000e308112f5ad1366fc7c250260db5c3628393c8f" => :mojave
-    sha256 "68e0a18b7a0cb4a3a9952f1698f91cd071ebca3660f8c62c42b3b81750be7501" => :high_sierra
+    sha256 "9518b32a672d6a756a29594892982c175a54c32aee9b5253c20e57dcf7760c44" => :catalina
+    sha256 "74e8c73f8032241b193fb8e4b54a5164efb0aca5fb357857392feeee05d758f6" => :mojave
+    sha256 "9b6bf1b79ee1d24bf099b93173d4b4c461be63834cd79185be1e3bd3884bd9e7" => :high_sierra
   end
 
   head do
