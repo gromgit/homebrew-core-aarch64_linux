0fab12e5e753f8817c64ea70430617e95dd54b16
Mike McQuaid <mike@mikemcquaid.com>
2020-04-07 14:13:57 +0100

    auditbeat: remove invalid uses_from_macos.

--- Formula/auditbeat.rb
+++ Formula/auditbeat.rb
@@ -15,9 +15,6 @@ class Auditbeat < Formula
 
   depends_on "go" => :build
 
-  # https://github.com/elastic/beats/pull/14798
-  uses_from_macos "python@2" => :build # does not support Python 3
-
   # Newer virtualenvs are not compatible with Python 2.7.10 on high sierra, use an old version
   resource "virtualenv" do
     url "https://files.pythonhosted.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz"
