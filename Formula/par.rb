43904ca23dc31d2881e47e8ce2017de35fce025b
Bo Anderson <mail@boanderson.me>
2020-04-07 16:20:21 +0000

    par: update 1.53.0 bottle.

--- Formula/par.rb
+++ Formula/par.rb
@@ -6,13 +6,9 @@ class Par < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 2
-    sha256 "9862bdce3a73b66ac07515ddc63190f9ac112a022b04d8caae3a78dbb18cc0d2" => :catalina
-    sha256 "5f35670c248a421d3b8d4605ea689d3d40f2a9a902d91a3ad8b5d6802564d4cf" => :mojave
-    sha256 "a73f538602df2f35f6d10b8a50fb893a26b407e5e5bc2e2065c9c2b9bcdce668" => :high_sierra
-    sha256 "efa3ba3bdd3b34ad8e5089b8cd5562d8b8cf4a5e5488e54e43dfb45760a1b4fa" => :sierra
-    sha256 "3683d5918dc91fcd073fc8e35af2fca416b3756aff8479ff549598bcd2500e8b" => :el_capitan
-    sha256 "cb1042ef12ead6645653775571ebe84798b707194922030563ff4056687954e3" => :yosemite
+    sha256 "457e5ff8ba94268a745fc954f84cbbaab7ac7d3a239ca602107a85a2e5d146a8" => :catalina
+    sha256 "ef5da7a3e359ba4c72ad4f11c2f1fb18adea19c6c51409d0fc7400ec60ef2422" => :mojave
+    sha256 "344dd1109a03e8c6017c2ca26a17c9f07c700c743b89b42786efce956bac70e1" => :high_sierra
   end
 
   conflicts_with "rancid", :because => "both install `par` binaries"
