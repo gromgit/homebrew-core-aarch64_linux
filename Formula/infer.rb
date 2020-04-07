23852d48bea9c66f998fda82394b7fa976abf327
Mike McQuaid <mike@mikemcquaid.com>
2020-04-07 14:14:02 +0100

    infer: remove invalid uses_from_macos.

--- Formula/infer.rb
+++ Formula/infer.rb
@@ -31,7 +31,6 @@ class Infer < Formula
   uses_from_macos "m4" => :build
   uses_from_macos "unzip" => :build
   uses_from_macos "ncurses"
-  uses_from_macos "python@2" # python@2 dependency will be removed in https://github.com/facebook/infer/issues/934
   uses_from_macos "xz"
   uses_from_macos "zlib"
 
