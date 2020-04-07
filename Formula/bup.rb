b2e312b10c9e3d72e8cf164826df307022ade506
Mike McQuaid <mike@mikemcquaid.com>
2020-04-07 14:13:59 +0100

    bup: remove invalid uses_from_macos.

--- Formula/bup.rb
+++ Formula/bup.rb
@@ -14,7 +14,6 @@ class Bup < Formula
   end
 
   depends_on "pandoc" => :build
-  uses_from_macos "python@2"
 
   resource "certifi" do
     url "https://files.pythonhosted.org/packages/41/bf/9d214a5af07debc6acf7f3f257265618f1db242a3f8e49a9b516f24523a6/certifi-2019.11.28.tar.gz"
