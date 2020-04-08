caf5e5b9f3bcbfcbc1d14b0f597837f74ec95737
Bo Anderson <mail@boanderson.me>
2020-04-08 21:00:24 +0100

    bumpversion 1.0.0
    Closes #52745.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/bumpversion.rb
+++ Formula/bumpversion.rb
@@ -5,8 +5,8 @@ class Bumpversion < Formula
   homepage "https://pypi.python.org/pypi/bumpversion"
   # maintained fork for the project
   # Ongoing maintenance discussion for the project, https://github.com/c4urself/bump2version/issues/86
-  url "https://github.com/c4urself/bump2version/archive/v0.5.11.tar.gz"
-  sha256 "f06c943b320033b3aa07958c99920474a54f1d0d76b12299fa67d59cdb17ab00"
+  url "https://github.com/c4urself/bump2version/archive/v1.0.0.tar.gz"
+  sha256 "06a7cb0fb7155b9283c4d10180e477f658754595b4dedb249f1e143e899d0e6c"
 
   bottle do
     cellar :any_skip_relocation
@@ -16,13 +16,14 @@ class Bumpversion < Formula
     sha256 "5b40851a4fa852ef9b58c379aeba9ace20f6c3132c1caad41ce43307e79b68d9" => :high_sierra
   end
 
-  depends_on "python"
+  depends_on "python@3.8"
 
   def install
     virtualenv_install_with_resources
   end
 
   test do
+    ENV["COLUMNS"] = "80"
     assert_includes shell_output("script -q /dev/null #{bin}/bumpversion --help"), "bumpversion: v#{version}"
     version_file = testpath/"VERSION"
     version_file.write "0.0.0"
