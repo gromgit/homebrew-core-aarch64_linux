190a3001a07d2d1c88fbf86c72fe01d037ca4844
Michka Popoff <michkapopoff@gmail.com>
2020-04-08 20:01:38 +0200

    unicorn: depend on macos

--- Formula/unicorn.rb
+++ Formula/unicorn.rb
@@ -15,6 +15,8 @@ class Unicorn < Formula
   end
 
   depends_on "pkg-config" => :build
+  depends_on :macos # Due to Python 2 (Might work with Python 3 with next release (1.0.2)
+  # See https://github.com/Homebrew/linuxbrew-core/pull/19728
 
   def install
     ENV["PREFIX"] = prefix
