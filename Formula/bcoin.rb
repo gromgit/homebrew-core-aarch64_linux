456ff1e849c98412c84634e1fbffe24141297af0
Bo Anderson <mail@boanderson.me>
2020-04-08 10:39:12 +0100

    bcoin: add 2.0.0 bottle.

--- Formula/bcoin.rb
+++ Formula/bcoin.rb
@@ -6,6 +6,12 @@ class Bcoin < Formula
   url "https://github.com/bcoin-org/bcoin/archive/v2.0.0.tar.gz"
   sha256 "ff99b735d3b23624455d72eada0de7f7ba4207f4a4cc0cab963ca2036de254a3"
 
+  bottle do
+    sha256 "4213df6b695d8fef6991a6d8a911b23599b8539487d5a6f1897d95845a281efc" => :catalina
+    sha256 "c7f4e5ff082cc63eb342c1f6ec5f88480abc12bddcdd5d310c1514496904ea62" => :mojave
+    sha256 "3054378525f593ac70a13a139ef5be6f343c7c0e22cedffee2b98b44020e0cc6" => :high_sierra
+  end
+
   depends_on "python@3.8" => :build
   depends_on "node"
 
