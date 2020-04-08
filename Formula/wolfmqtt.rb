a738cefcda976003749663edbdcb18d0dddef7bd
Adrian Fedoreanu <adrian.fedoreanu@gmail.com>
2020-04-08 12:16:15 +0200

    wolfmqtt 1.4 (new formula)
    Closes #52710.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

---
+++ Formula/wolfmqtt.rb
@@ -0,0 +1,46 @@
+class Wolfmqtt < Formula
+  desc "Small, fast, portable MQTT client C implementation"
+  homepage "https://github.com/wolfSSL/wolfMQTT"
+  url "https://github.com/wolfSSL/wolfMQTT.git",
+      :tag      => "v1.4",
+      :revision => "af3d2926773b2c97f5e3a86ea2562e339a91b747"
+  head "https://github.com/wolfSSL/wolfMQTT.git"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "wolfssl"
+
+  def install
+    args = %W[
+      --disable-silent-rules
+      --disable-dependency-tracking
+      --infodir=#{info}
+      --mandir=#{man}
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --enable-nonblock
+      --enable-mt
+      --enable-mqtt5
+      --enable-propcb
+      --enable-sn
+    ]
+
+    system "./autogen.sh"
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOT
+      #include <wolfmqtt/mqtt_client.h>
+      int main() {
+        MqttClient mqttClient;
+        return 0;
+      }
+    EOT
+    system ENV.cc, "test.cpp", "-L#{lib}", "-lwolfmqtt", "-o", "test"
+    system "./test"
+  end
+end
