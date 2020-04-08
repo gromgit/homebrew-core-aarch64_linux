c8b04644b42a35f9159b8992e020ba8b88dc231c
Michka Popoff <michkapopoff@gmail.com>
2020-04-08 19:13:33 +0200

    mesos: depend on macos

--- Formula/mesos.rb
+++ Formula/mesos.rb
@@ -15,6 +15,7 @@ class Mesos < Formula
   depends_on "maven" => :build
   depends_on "apr-util"
   depends_on :java => "1.8"
+  depends_on :macos # Due to Python 2
   depends_on "subversion"
 
   conflicts_with "nanopb-generator", :because => "they depend on an incompatible version of protobuf"
