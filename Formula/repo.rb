cb7c6bfc4d5d1583191e959ba73af9919683cd97
Pete Woods <pete.woods@circleci.com>
2020-04-08 12:44:50 +0100

    repo 2.5 (#52711)

--- Formula/repo.rb
+++ Formula/repo.rb
@@ -2,8 +2,8 @@ class Repo < Formula
   desc "Repository tool for Android development"
   homepage "https://source.android.com/source/developing.html"
   url "https://github.com/GerritCodeReview/git-repo.git",
-      :tag      => "v2.4.1",
-      :revision => "d957ec6a834e333a3812546911f786b0c20b808f"
+      :tag      => "v2.5",
+      :revision => "3e5b269fc6e0b0691e571ca7f818a43f10ca2cbe"
   version_scheme 1
 
   bottle :unneeded
