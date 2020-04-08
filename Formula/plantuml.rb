6881170fe4f4e2f9705f099f0b0620adf780652b
Víctor Orozco <tuxtor@shekalug.org>
2020-04-07 20:05:22 -0600

    plantuml 1.2020.6 (#52687)

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://downloads.sourceforge.net/project/plantuml/1.2020.5/plantuml.1.2020.5.jar"
-  sha256 "0d4e4fd5e8a114958440138c5793f96f664600da22a91ee18070e304955cc1b2"
+  url "https://downloads.sourceforge.net/project/plantuml/1.2020.6/plantuml.1.2020.6.jar"
+  sha256 "081c9d7b268fab99475e1163e6d96dd3d2496d2356abf06c4b942a2dca80584f"
   version_scheme 1
 
   bottle :unneeded
