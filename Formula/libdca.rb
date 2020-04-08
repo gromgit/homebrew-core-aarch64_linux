aae7baba1056d184a917617b67692abb76000e6b
EricFromCanada <enk3@outlook.com>
2020-04-08 10:29:28 -0400

    libdca 0.0.7
    Closes #52720.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/libdca.rb
+++ Formula/libdca.rb
@@ -1,8 +1,8 @@
 class Libdca < Formula
   desc "Library for decoding DTS Coherent Acoustics streams"
   homepage "https://www.videolan.org/developers/libdca.html"
-  url "https://download.videolan.org/pub/videolan/libdca/0.0.6/libdca-0.0.6.tar.bz2"
-  sha256 "98f98a9aa000a26b927c6facd15d18dcf664238adfc5db24f533c5932cdb1f40"
+  url "https://download.videolan.org/pub/videolan/libdca/0.0.7/libdca-0.0.7.tar.bz2"
+  sha256 "3a0b13815f582c661d2388ffcabc2f1ea82f471783c400f765f2ec6c81065f6a"
 
   bottle do
     cellar :any
@@ -13,11 +13,16 @@ class Libdca < Formula
     sha256 "641d1810fd6ca84d49824335403d79a8834611d1dd615d93300989040135ed1d" => :el_capitan
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
   def install
     # Fixes "duplicate symbol ___sputc" error when building with clang
     # https://github.com/Homebrew/homebrew/issues/31456
     ENV.append_to_cflags "-std=gnu89"
 
+    system "autoreconf", "-fiv"
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make"
