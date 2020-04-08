452b737f6a5365c8f70a0425c020413ccac97957
EricFromCanada <enk3@outlook.com>
2020-04-08 13:25:32 -0400

    caffe: revision bump

--- Formula/caffe.rb
+++ Formula/caffe.rb
@@ -3,7 +3,7 @@ class Caffe < Formula
   homepage "https://caffe.berkeleyvision.org/"
   url "https://github.com/BVLC/caffe/archive/1.0.tar.gz"
   sha256 "71d3c9eb8a183150f965a465824d01fe82826c22505f7aa314f700ace03fa77f"
-  revision 18
+  revision 19
 
   bottle do
     rebuild 1
