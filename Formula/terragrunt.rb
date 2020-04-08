3a46c09c6605fc80ce16b7a4102d3cfab53b0897
Rui Chen <chenrui333@gmail.com>
2020-04-08 00:00:10 -0400

    terragrunt 0.23.7
    Closes #52696.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -2,8 +2,8 @@ class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state"
   homepage "https://github.com/gruntwork-io/terragrunt"
   url "https://github.com/gruntwork-io/terragrunt.git",
-    :tag      => "v0.23.6",
-    :revision => "bfe773fe06dc6e386a7dc0df14e27efe57c1a5fc"
+    :tag      => "v0.23.7",
+    :revision => "042ec65d977c9c757781b6c172d4b732b7485a07"
 
   bottle do
     cellar :any_skip_relocation
