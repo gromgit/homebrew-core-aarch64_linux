4520fcfa96bf291a4ed93ecb6a4bb2b4bd0a4ea7
Bo Anderson <mail@boanderson.me>
2020-04-09 14:09:32 +0000

    ripgrep-all: update 0.9.5 bottle.

--- Formula/ripgrep-all.rb
+++ Formula/ripgrep-all.rb
@@ -7,10 +7,9 @@ class RipgrepAll < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "687b7bf509f088bdb9cd0058e6346d7aa66fdddd35fe9f1095085ffc59040b69" => :catalina
-    sha256 "8b0b3bb348853c51a99826d326d3bdd1ff52bab521fd6e37deaaaf0004ff77e5" => :mojave
-    sha256 "ef28f16cb59908b17a4e07a39ca3804173204f2ccf333262bab3988866d5c741" => :high_sierra
+    sha256 "fa8041bd45eba089f3cf80bcf83c75a02bdfb4d1f1fe195d86d0d444438d06a1" => :catalina
+    sha256 "6c5f1e7b0f4cdf65750e65800e9030144180f6cf605292104fe67c775f65b82b" => :mojave
+    sha256 "fb87b9ac0643c4530ce5528469b9502a564fa36f29cdc37f92d90b8030494ae2" => :high_sierra
   end
 
   depends_on "rust" => :build
