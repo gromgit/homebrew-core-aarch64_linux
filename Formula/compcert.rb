ec5d12cd54488a109e61d43a3462fcde210fc70b
Tej Chajed <tchajed@mit.edu>
2020-04-09 11:22:11 -0500

    compcert 3.7
    Closes #52788.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/compcert.rb
+++ Formula/compcert.rb
@@ -1,8 +1,8 @@
 class Compcert < Formula
   desc "Formally verified C compiler"
   homepage "http://compcert.inria.fr"
-  url "https://github.com/AbsInt/CompCert/archive/v3.6.tar.gz"
-  sha256 "7a77839f6b990ab632ba14feccf4f17da189f0e3b95d6ce2ef0986e4caebc575"
+  url "https://github.com/AbsInt/CompCert/archive/v3.7.tar.gz"
+  sha256 "ceee1b2ed6c2576cb66eb7a0f2669dcf85e65c0fc68385f0781b0ca4edb87eb0"
 
   bottle do
     cellar :any_skip_relocation
@@ -15,12 +15,6 @@ class Compcert < Formula
   depends_on "coq" => :build
   depends_on "menhir" => :build
   depends_on "ocaml" => :build
-
-  # coq 8.11 compatibility. Remove with the next release.
-  patch do
-    url "https://github.com/AbsInt/CompCert/commit/b7374d225af55ecc6f5d6aa8f3684bfae99ff465.patch?full_index=1"
-    sha256 "a873dcbf7855935e4b5087883ef07b3fb0c17b6e62a1a0c266a009e0bbd3c794"
-  end
 
   def install
     # We pass -ignore-coq-version, otherwise every new version of coq
