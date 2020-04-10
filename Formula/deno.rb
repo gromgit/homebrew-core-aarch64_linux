634023505bb6efaacd144adcc949c7cc4d0469ac
Michka Popoff <michkapopoff@gmail.com>
2020-04-10 07:43:39 +0200

    deno: Fix dependency order

--- Formula/deno.rb
+++ Formula/deno.rb
@@ -14,9 +14,8 @@ class Deno < Formula
   depends_on "llvm" => :build
   depends_on "ninja" => :build
   depends_on "rust" => :build
-  depends_on :macos # Due to Python 2 (see https://github.com/denoland/deno/issues/2893)
-
   depends_on :xcode => ["10.0", :build] # required by v8 7.9+
+  depends_on :macos # Due to Python 2 (see https://github.com/denoland/deno/issues/2893)
 
   uses_from_macos "xz"
 
