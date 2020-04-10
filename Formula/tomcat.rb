5e0f4180f5d9738037130bf4973f2cf3613acc38
mystygage <mystygage@users.noreply.github.com>
2020-04-10 13:14:45 +0200

    tomcat 9.0.34 (#52785)

--- Formula/tomcat.rb
+++ Formula/tomcat.rb
@@ -1,9 +1,9 @@
 class Tomcat < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-9/v9.0.33/bin/apache-tomcat-9.0.33.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.33/bin/apache-tomcat-9.0.33.tar.gz"
-  sha256 "d5cd9463492f4552229295a9a8c00615748f85e9de36434847d495e95b0ef796"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-9/v9.0.34/bin/apache-tomcat-9.0.34.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.34/bin/apache-tomcat-9.0.34.tar.gz"
+  sha256 "321de5b18a48ec09d2963d9faba4bfeafc7dd2203d80a2ef7e7a20b159e2120a"
 
   bottle :unneeded
 
