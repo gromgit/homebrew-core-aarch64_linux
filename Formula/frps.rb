2d15ac09bb83c1881f0d830ec5cc86b31194e176
Rui Chen <chenrui333@gmail.com>
2020-04-11 18:36:39 -0400

    frps: update 0.32.1 bottle.

--- Formula/frps.rb
+++ Formula/frps.rb
@@ -7,9 +7,9 @@ class Frps < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "71ad026828e9e35c9a54564bd191bce3f7e9cbc17cf40e556064a2fd91eb2d68" => :catalina
-    sha256 "bc9302bbc263cc71ec8354b3172a18eecb5a648207c45b9a123343e431e52596" => :mojave
-    sha256 "89fb402e20a3afe2d0cd14aedc927d96a2493cbd3f82c5df0fccbe3948c83ba9" => :high_sierra
+    sha256 "fd32c875fb000ba39f0f8f7ddcd1dfb491386f3213d42612377cb973073aea66" => :catalina
+    sha256 "265cf6a6b5a0b6eb064d420dc767e000755cabcd74160eb54087168aec8542e5" => :mojave
+    sha256 "4fe4f1a8c06f5c1fdc1ddf3d4b650609a285b8da9ee621755e7e40c797dc8559" => :high_sierra
   end
 
   depends_on "go" => :build
