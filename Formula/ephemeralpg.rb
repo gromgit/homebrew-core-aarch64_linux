9d59c87b3ed3c8af8ac47e19ac8f2230f1bace0e
Rui Chen <chenrui333@gmail.com>
2020-04-11 14:51:26 -0400

    ephemeralpg 3.0
    Closes #52892.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/ephemeralpg.rb
+++ Formula/ephemeralpg.rb
@@ -1,12 +1,11 @@
 class Ephemeralpg < Formula
   desc "Run tests on an isolated, temporary Postgres database"
   homepage "https://eradman.com/ephemeralpg/"
-  url "https://eradman.com/ephemeralpg/code/ephemeralpg-2.9.tar.gz"
-  sha256 "09314fe7d7ba2c26fb02864b9ddc92a538bc53674200363e77bb53a2fc1c17be"
+  url "https://eradman.com/ephemeralpg/code/ephemeralpg-3.0.tar.gz"
+  sha256 "70ef314e31c5547f353ea7b2787faafa07adc32dcfaea6f4f1475512c23b0fc8"
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
     sha256 "bd7129fa7c2635627e6c78b0dd96e9fe7f51fcb58fcaaefc1a8f549275275173" => :catalina
     sha256 "fbd15290717e762090d699ea2ca7ed649124b4fbcb702bc5873ab629dd00f0af" => :mojave
     sha256 "875913b4e09b30348a651db65afa6049ed659aec192f32b8564f465fcaf5bfaf" => :high_sierra
@@ -15,6 +14,7 @@ class Ephemeralpg < Formula
   depends_on "postgresql"
 
   def install
+    system "make"
     system "make", "PREFIX=#{prefix}", "MANPREFIX=#{man}", "install"
   end
 
