eb9f4f901e5e50a179caf17d41aabad969fcd34d
Rui Chen <chenrui333@gmail.com>
2020-04-11 14:00:17 -0400

    devspace 4.11.1
    Closes #52888.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/devspace.rb
+++ Formula/devspace.rb
@@ -2,8 +2,8 @@ class Devspace < Formula
   desc "CLI helps develop/deploy/debug apps with Docker and k8s"
   homepage "https://devspace.cloud/docs"
   url "https://github.com/devspace-cloud/devspace.git",
-    :tag      => "v4.11.0",
-    :revision => "b9a1e6a541df4b82f3ca9c88ae3698e85f98cc56"
+    :tag      => "v4.11.1",
+    :revision => "f169107381a6d4c2553efc921b45035945fbbbd6"
 
   bottle do
     cellar :any_skip_relocation
