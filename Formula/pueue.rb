d658ad7e1f1d9de8a439219c1f31935406d34565
Bo Anderson <mail@boanderson.me>
2020-04-11 12:07:05 +0000

    pueue: update 0.3.1 bottle.

--- Formula/pueue.rb
+++ Formula/pueue.rb
@@ -7,9 +7,9 @@ class Pueue < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f54b9f001c331bbbed5aac58dc45f3fadefb4ccc1d14af47304ccf4b503311e2" => :catalina
-    sha256 "9034ce6cb9f8b39da4d5f0849beebfe364725abb50ed097a72421f0386f50270" => :mojave
-    sha256 "6a71f14b2ffca4d8c32ae8c6262917533b4ef82669dde783eadbbc7cc4df7cd8" => :high_sierra
+    sha256 "6a1f7c804aa734c011fd27cb2817355db79b7f1d4351db92943c6d07bb069241" => :catalina
+    sha256 "f6118e9d6c0338b19c2ea5adbae4ccc9bb7cdd024aa2a04bd397392dc3f53dfa" => :mojave
+    sha256 "22c7a167fadd66cd1a1f09e539d334e5dc86e8539cb4d4d1f4a2d7e77e9f16d5" => :high_sierra
   end
 
   depends_on "rust" => :build
