f15e4821c701580b7afe3b854c3d6f530ddae86f
Dawid Dziurla <dawidd0811@gmail.com>
2020-04-11 14:23:28 +0000

    wireshark: update 3.2.3 bottle.

--- Formula/wireshark.rb
+++ Formula/wireshark.rb
@@ -7,9 +7,9 @@ class Wireshark < Formula
   head "https://code.wireshark.org/review/wireshark", :using => :git
 
   bottle do
-    sha256 "c0509d8a6fe03675bea1a9f3d2f8db62790000a02e2363202ff384d646feb4ab" => :catalina
-    sha256 "e55d0fe730ec7c96ccfa16d3c1b2bc5ba5638e496543c8b0fb332bfef5f39c85" => :mojave
-    sha256 "cddabe5267aa896a8c6b22adad6f6f9b6754bfcc0b9a4fee25ebfedd801275a2" => :high_sierra
+    sha256 "c4763d2c0b61b6c1e0aa0f471273538f85d0b312ae2a3f6b722604a930b3fd15" => :catalina
+    sha256 "4b78b7a908debf1bf98312acd737a13acd997a136aacbca128587b913400711a" => :mojave
+    sha256 "abb3009117170952fc2866d7aae359b6d35a568d6afc89eb247a45ef82876f2c" => :high_sierra
   end
 
   depends_on "cmake" => :build
