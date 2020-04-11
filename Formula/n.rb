afc44f4d406d82bc7d176c0640440391436be044
Rui Chen <chenrui333@gmail.com>
2020-04-11 13:18:03 -0400

    n 6.5.1
    Closes #52881.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/n.rb
+++ Formula/n.rb
@@ -1,8 +1,8 @@
 class N < Formula
   desc "Node version management"
   homepage "https://github.com/tj/n"
-  url "https://github.com/tj/n/archive/v6.5.0.tar.gz"
-  sha256 "7034a3563eba5d1093884f1699a0587ac3e82782590ba0875205177ce0cf426e"
+  url "https://github.com/tj/n/archive/v6.5.1.tar.gz"
+  sha256 "5833f15893b9951a9ed59487e87b6c181d96b83a525846255872c4f92f0d25dd"
   head "https://github.com/tj/n.git"
 
   bottle do
