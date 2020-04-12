6f9b848ce58924f3cad488c40429caa390400b81
Rui Chen <chenrui333@gmail.com>
2020-04-11 22:44:42 -0400

    pulp 14.0.0
    Closes #52957.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/pulp.rb
+++ Formula/pulp.rb
@@ -3,12 +3,11 @@ require "language/node"
 class Pulp < Formula
   desc "Build tool for PureScript projects"
   homepage "https://github.com/bodil/pulp"
-  url "https://registry.npmjs.org/pulp/-/pulp-13.0.0.tgz"
-  sha256 "91b5e517afffc8f53ed6dc608eef908bd92843ce73976acf57db6eca897b5ba0"
+  url "https://registry.npmjs.org/pulp/-/pulp-14.0.0.tgz"
+  sha256 "ab4d1a669b7ff7d4cd6c4473251a516f310473fcded3c4ee04547865957c1e67"
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
     sha256 "bec16642f812372fa5260f59250a72c13ef363c01ab6aaba1eb5997e27d387df" => :catalina
     sha256 "d5380db101369a8a71a6e64badd0a30419907a4bba22b0400e8add077dcdaa7b" => :mojave
     sha256 "e3fc10ded86c2d8ec89efb373ad8af6994ff800904a7808a73b994c42a273dce" => :high_sierra
