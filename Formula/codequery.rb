a98f10a5d5cd5f4679f8224ae2305885fbabe42f
Issy Long <me@issyl0.co.uk>
2020-04-12 11:48:27 +0000

    codequery: update 0.22.0 bottle.

--- Formula/codequery.rb
+++ Formula/codequery.rb
@@ -6,11 +6,9 @@ class Codequery < Formula
 
   bottle do
     cellar :any
-    sha256 "e42335ea67b706cc84cd96ed7c23a127f1a58e7b17a362f5556f8a61d64e2523" => :catalina
-    sha256 "1765b7bf67fd6eaef6b4464bfe06a0ae467165acca5dd17e78d371165a5db91a" => :mojave
-    sha256 "fe67cc5adc84bb2558ad4e67c5b02bfba8b80f3a18bc036801e4c1e1d81c96eb" => :high_sierra
-    sha256 "21a673797bdc3ff9c7453930499e7e29e7f1320eb4dbddec67f7127dee34a989" => :sierra
-    sha256 "15e18f6bb87bd489b33f4322af5d6351e28ec805aef291d6c0df50d80fb7958d" => :el_capitan
+    sha256 "45bcd6dfe541e18dcec19044b05bd1f50e56f48ef46b91013e9279301176e1cc" => :catalina
+    sha256 "1e5fcb01cf9e8ae18b0673322b682153b029ca59ca9c50dd2685be360c61772f" => :mojave
+    sha256 "40193a3269ec436ddcb36a6aa1d70851c6cd7cd905fa92e1ee10d5e44387702e" => :high_sierra
   end
 
   depends_on "cmake" => :build
