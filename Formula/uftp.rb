63236531d6da93f008796948576f43472bc65bec
Rui Chen <chenrui333@gmail.com>
2020-04-12 00:00:06 -0400

    uftp 4.10.2
    Update uftp.rb
    
    Closes #52981.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/uftp.rb
+++ Formula/uftp.rb
@@ -1,12 +1,11 @@
 class Uftp < Formula
   desc "Secure, reliable, efficient multicast file transfer program"
   homepage "https://uftp-multicast.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/uftp-multicast/source-tar/uftp-4.10.1.tar.gz"
-  sha256 "903353febf1b88f701198d956a60cec3d2be3cc251561a678061761aec6622f6"
+  url "https://downloads.sourceforge.net/project/uftp-multicast/source-tar/uftp-4.10.2.tar.gz"
+  sha256 "ecab6ab07fe0ebaf7bfe35d99fe2da28ede3ddc6f21f825d3b259cf171258505"
 
   bottle do
     cellar :any
-    rebuild 1
     sha256 "f0459c3a5f56b16a2b636a1f37cff5220f08265400b18a2f3bea5640856ae608" => :catalina
     sha256 "23bf3d26128baf28b1204c3b574eeb1c108607b0dc2a3b7d1725f3289ef5b8cf" => :mojave
     sha256 "bc8d7ee7eeee6fa2f586681ca68d892bb504bbeef1d25f156a8039fdb33862d9" => :high_sierra
