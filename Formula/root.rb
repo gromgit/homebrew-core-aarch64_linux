1381450128c908725e647fedc521fbbddba5d49d
Rui Chen <chenrui333@gmail.com>
2020-04-12 00:54:28 -0400

    root: revision bump for gl2ps
    Closes #52906.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/root.rb
+++ Formula/root.rb
@@ -4,6 +4,7 @@ class Root < Formula
   url "https://root.cern.ch/download/root_v6.20.04.source.tar.gz"
   version "6.20.04"
   sha256 "1f8c76ccdb550e64e6ddb092b4a7e9d0a10655ef80044828cba12d5e7c874472"
+  revision 1
   head "https://github.com/root-project/root.git"
 
   bottle do
