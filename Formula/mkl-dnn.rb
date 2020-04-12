04d67ae2a14d94962615d7b28de29c070ca9aa49
Bo Anderson <mail@boanderson.me>
2020-04-12 02:00:33 +0000

    mkl-dnn: update 1.3 bottle.

--- Formula/mkl-dnn.rb
+++ Formula/mkl-dnn.rb
@@ -6,9 +6,9 @@ class MklDnn < Formula
 
   bottle do
     cellar :any
-    sha256 "8365520f2ee8245beacd14c63e8c8cf66d90f502a1ec2fe15771810de1e0fe36" => :catalina
-    sha256 "6e46c0a7840a076ff108f2d6befc99fa4cb6fbe2b9b44b1592f68966fedaa85a" => :mojave
-    sha256 "2ed24a849cfeb4b0a2c5d16376ac1bce5847eb4fd61e32bc18faca487ea32f23" => :high_sierra
+    sha256 "8ec4f729539f1e8b6ca35277013d0b5fe85039fdc743177d425f8a1e2f001905" => :catalina
+    sha256 "675a0cbbcb2f62cfb2f45f4bf4f33101e0177a89b2f7527611dba342a40b8e13" => :mojave
+    sha256 "cfa114bbcc5fd3c64f9ce1c9e60112ea098643a21f0ff2af8b530ec084c3241b" => :high_sierra
   end
 
   depends_on "cmake" => :build
