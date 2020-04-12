6c3ad7a9337c4a9d59a730503d5f4e12301aa1e7
Rui Chen <chenrui333@gmail.com>
2020-04-11 23:46:09 -0400

    swift-sh 1.17.1
    Closes #52977.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/swift-sh.rb
+++ Formula/swift-sh.rb
@@ -1,8 +1,8 @@
 class SwiftSh < Formula
   desc "Scripting with easy zero-conf dependency imports"
   homepage "https://github.com/mxcl/swift-sh"
-  url "https://github.com/mxcl/swift-sh/archive/1.17.0.tar.gz"
-  sha256 "6e48fc5697bb19826ecd29ae01167299a5ab1fcece0453fba9318e51020f936d"
+  url "https://github.com/mxcl/swift-sh/archive/1.17.1.tar.gz"
+  sha256 "90c2d8ca708922fdb36b26d81dac768183ff65f0cd00698937fb89435c53dcfb"
   head "https://github.com/mxcl/swift-sh.git"
 
   bottle do
