29772300c5d28f6dd8e88520e499ffa422a8d4c8
Bo Anderson <mail@boanderson.me>
2020-04-12 21:57:21 +0100

    ospray 2.1.0
    Closes #53057.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/ospray.rb
+++ Formula/ospray.rb
@@ -1,8 +1,8 @@
 class Ospray < Formula
   desc "Ray-tracing-based rendering engine for high-fidelity visualization"
   homepage "https://www.ospray.org/"
-  url "https://github.com/ospray/ospray/archive/v1.8.5.tar.gz"
-  sha256 "6d85e103280aa4c8d0032a2cc3082f08a6021a79d22cf4a8e38b09f152f35f53"
+  url "https://github.com/ospray/ospray/archive/v2.1.0.tar.gz"
+  sha256 "ab38106beac3868cb39ef35f5564d51277641ecd982afc45e463aada6a124502"
   head "https://github.com/ospray/ospray.git"
 
   bottle do
@@ -16,7 +16,29 @@ class Ospray < Formula
   depends_on :macos => :mojave # Needs embree bottle built with SSE4.2.
   depends_on "tbb"
 
+  resource "ospcommon" do
+    url "https://github.com/ospray/ospcommon/archive/v1.3.0.tar.gz"
+    sha256 "23f17fd930e63af9dd0f76ea3505e5d11f91a138b8e8bdec50efd51162544042"
+  end
+
+  resource "openvkl" do
+    url "https://github.com/openvkl/openvkl/archive/v0.9.0.tar.gz"
+    sha256 "06aa82c8c3ff68ab93fb8240f4881a1bc238b3de681812e71145f8a1629d3fee"
+  end
+
   def install
+    resources.each do |r|
+      r.stage do
+        mkdir "build" do
+          system "cmake", "..", *std_cmake_args,
+                                "-DBUILD_EXAMPLES=OFF",
+                                "-DBUILD_TESTING=OFF"
+          system "make"
+          system "make", "install"
+        end
+      end
+    end
+
     args = std_cmake_args + %W[
       -DCMAKE_INSTALL_NAME_DIR=#{opt_lib}
       -DCMAKE_INSTALL_RPATH=#{opt_lib}
