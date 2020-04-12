e7bbde6a5d3b3f288cde8b33e9fee3708de18189
Issy Long <me@issyl0.co.uk>
2020-04-12 20:23:47 +0000

    olsrd: update 0.9.8 bottle.

--- Formula/olsrd.rb
+++ Formula/olsrd.rb
@@ -8,13 +8,9 @@ class Olsrd < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ebde0dd319053bf4a5fff8d508b77e64648c99744784f294f488c1c75afda008" => :catalina
-    sha256 "4f10d4e0d33d101369a9b216adcdc445cd8bce3ee6a1c09548578688c7dc8ac9" => :mojave
-    sha256 "27524e71f30ba6e64fa5184058b6c73c4dfbfda348b2c2817258db758fcdeb99" => :high_sierra
-    sha256 "1131b24ca42d043af8eb8a338337150c8ad9f10d9d71968e119779c3eae1bc93" => :sierra
-    sha256 "88c836acf65237195c3b0d74a7fde0813c2008ab79c216ba8b36e789e58192ab" => :el_capitan
-    sha256 "c6c165c6ae75a58c33995a7820f83604758ed37c9fb1c4d1557cad4c68b7f752" => :yosemite
-    sha256 "842c328edcde3ccbffcc8dfddae63f802c716fb18aa63aea4fe620bbed5d8562" => :mavericks
+    sha256 "95e531e19da3a6e11bf48851691e411d3fb27acf7dc18ccf5bed5c32aa3df4ff" => :catalina
+    sha256 "5ba1b0c584a2efe1d518be4032432818fca8bbccd3078e23ef7bbb3a9359a73e" => :mojave
+    sha256 "70402085753c70fb12f3e0f249bf109ac77e0a22d7be890ac6484d7ffce8501f" => :high_sierra
   end
 
   depends_on "coreutils" => :build # needs GNU cp
