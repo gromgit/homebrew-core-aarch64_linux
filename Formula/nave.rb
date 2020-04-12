c00b2f73bad50c311860bf181674387664cc4b5b
chenrui <chenrui333@gmail.com>
2020-04-12 15:52:46 -0400

    nave 3.2.1 (#53040)

--- Formula/nave.rb
+++ Formula/nave.rb
@@ -1,8 +1,8 @@
 class Nave < Formula
   desc "Virtual environments for Node.js"
   homepage "https://github.com/isaacs/nave"
-  url "https://github.com/isaacs/nave/archive/v2.4.2.tar.gz"
-  sha256 "31cca9a97c263936200989890d4ac08acb948118349d5356a7ac61d31a5ca4a0"
+  url "https://github.com/isaacs/nave/archive/v3.2.1.tar.gz"
+  sha256 "6ad13ce65659960fdb6133529c3fd365c04cb7c7208c60c2a6e0245494908977"
   head "https://github.com/isaacs/nave.git"
 
   bottle :unneeded
