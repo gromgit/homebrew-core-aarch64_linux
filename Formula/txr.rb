3f4aeedf04fab8ac12779d69fdc205831cc0af47
Rui Chen <chenrui333@gmail.com>
2020-04-12 00:50:16 -0400

    txr: update 234 bottle.

--- Formula/txr.rb
+++ Formula/txr.rb
@@ -7,9 +7,9 @@ class Txr < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ae02c2579cd93bb188aaec1cb801c894245098761358b5bfb4d40b49a4505f77" => :catalina
-    sha256 "facfed24452bdb1e07344788fc839d9c9acc553a666f1849b653aaf4eae06fc2" => :mojave
-    sha256 "31f4399f13d8ca162c10db32c81f17f3ea0a876c6e72351566c5be7c18ac9530" => :high_sierra
+    sha256 "75e3da843c36098dffaa3bd6f5da3aa4c16245a8a3a88a896f39a690077c04aa" => :catalina
+    sha256 "7d2d43005b9c738b372e19fa5f57727c97317635e96521932093d0ebd7e66edf" => :mojave
+    sha256 "48bcb460f3e6f13c75b9afd1edac3f1e5085f5905772018061ebf8a1f321827d" => :high_sierra
   end
 
   uses_from_macos "bison" => :build
