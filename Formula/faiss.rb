22e9599391134322931809b35491951b26640aae
Issy Long <me@issyl0.co.uk>
2020-04-12 15:46:16 +0000

    faiss: update 1.6.3 bottle.

--- Formula/faiss.rb
+++ Formula/faiss.rb
@@ -6,9 +6,9 @@ class Faiss < Formula
 
   bottle do
     cellar :any
-    sha256 "2ed35e49a096403ccf38e721a9d76aee9690ecf307bf9279fe56e158933d22bc" => :catalina
-    sha256 "6e4c9e7c66c700cbce22296fb258ec6e779118acef74741e1bfcadab389634a7" => :mojave
-    sha256 "2ca185b2dc508c2d8b7a3f44779a603c199db50000da6aea6ac66c802f3867cc" => :high_sierra
+    sha256 "457e410d8e5b009bf12cb1b5881485f03461646ef18ff8afb69dbbc7113519b4" => :catalina
+    sha256 "b3eb242ff373017f8d7ba621fde32d745a6d7d6c5c7ca5de888b7f8087e94776" => :mojave
+    sha256 "03b95260a4fdd6cceaa69bb4e7168939aadf2b608f998079f7511aec6171f2d1" => :high_sierra
   end
 
   depends_on "libomp"
