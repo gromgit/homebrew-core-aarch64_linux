e8c78279f87fdfb874b492d1938619547eaf7b5d
Rui Chen <chenrui333@gmail.com>
2020-04-11 22:59:33 -0400

    redpen 1.10.4
    redpen: Fix test
    
    - The file under test, plus the output of the `redpen` command or maybe the
      verbosity of this command, changed in this release.
    
    update release artifact
    
    Closes #52961.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/redpen.rb
+++ Formula/redpen.rb
@@ -1,9 +1,8 @@
 class Redpen < Formula
   desc "Proofreading tool to help writers of technical documentation"
   homepage "https://redpen.cc/"
-  url "https://github.com/redpen-cc/redpen/releases/download/redpen-1.10.3/redpen-1.10.3.tar.gz"
-  sha256 "0a7543e3961428ce68eb47c964e8988ec2f585db6e32ded582eb6626a98ffdd2"
-  revision 1
+  url "https://github.com/redpen-cc/redpen/releases/download/redpen-1.10.4/redpen-1.10.4.tar.gz"
+  sha256 "6c3dc4a6a45828f9cc833ca7253fdb036179036631248288251cb9ac4520c39d"
 
   bottle :unneeded
 
@@ -21,7 +20,7 @@ class Redpen < Formula
   test do
     path = "#{libexec}/sample-doc/en/sampledoc-en.txt"
     output = "#{bin}/redpen -l 20 -c #{libexec}/conf/redpen-conf-en.xml #{path}"
-    match = /^sampledoc-en.txt:1: ValidationError[SymbolWithSpace]*/
-    assert_match match, shell_output(output).split("\n").select { |line| line.start_with?("sampledoc-en.txt") }[0]
+    match = /sampledoc-en.txt:1: ValidationError[SentenceLength]*/
+    assert_match match, shell_output(output).split("\n").select { |line| line.include?("sampledoc-en.txt") }[0]
   end
 end
