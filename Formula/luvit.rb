8d60365526d2ddc5e12ed6cd8da9bd9cb56068f6
Rui Chen <chenrui333@gmail.com>
2020-04-12 17:39:00 -0400

    luvit 2.17.0
    Closes #53059.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/luvit.rb
+++ Formula/luvit.rb
@@ -1,14 +1,12 @@
 class Luvit < Formula
   desc "Asynchronous I/O for Lua"
   homepage "https://luvit.io"
-  url "https://github.com/luvit/luvit/archive/2.16.0.tar.gz"
-  sha256 "3cbd5136da6dba4ccfaee86357255c39b5fafa5fffa62d7d793514fa4dca1a79"
-  revision 1
+  url "https://github.com/luvit/luvit/archive/2.17.0.tar.gz"
+  sha256 "80657aa752322560fcde780212b6807b626b45d65aca3f3dae254e5c4fb0ee78"
   head "https://github.com/luvit/luvit.git"
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
     sha256 "b057dc9f3afbcc37bd7ed40c9a283928f340af080cf75146d428976f61747535" => :catalina
     sha256 "b057dc9f3afbcc37bd7ed40c9a283928f340af080cf75146d428976f61747535" => :mojave
     sha256 "b057dc9f3afbcc37bd7ed40c9a283928f340af080cf75146d428976f61747535" => :high_sierra
