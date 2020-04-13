577bc4f3ac6301a3b6b634bd4bac9c5367a667e5
Thierry Moisan <thierry.moisan@gmail.com>
2020-04-13 13:34:18 +0000

    mint: update 0.14.2 bottle.

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -6,8 +6,8 @@ class Mint < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c904d521d18720a27a6eccdddd7747f2a680b7e9e87c309ebfc15c13b05e3def" => :catalina
-    sha256 "449bdbdd101b36b678348588a372b9af7118bdbaa8ac73b826e4f86b706009fd" => :mojave
+    sha256 "cc7a17ee517fe97f12852c71252896c9ca30dfe8236f816f2990b2d0553ff5b3" => :catalina
+    sha256 "f70e96558839243a845c67f15defbd8718ad5a9e094ae1c1b998ac9faf4399b0" => :mojave
   end
 
   depends_on :xcode => ["10.2", :build]
