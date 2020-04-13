37f984cdefaaa349b9ee7116dbda577fde480642
Issy Long <me@issyl0.co.uk>
2020-04-13 17:24:14 +0000

    lf: update 14 bottle.

--- Formula/lf.rb
+++ Formula/lf.rb
@@ -6,10 +6,9 @@ class Lf < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "86added59852eebeeea8152ed69feb042f6f4ccc1ca998e3df6ca1629f746fe7" => :catalina
-    sha256 "ad4cfc498d617d41c02b9482c0411b8048d7eaa934cfed114861555de742cf7c" => :mojave
-    sha256 "e25cfc6246e04b13e90260433cf1fc27a28af17b82ac7d6bb93e09123faa4f61" => :high_sierra
-    sha256 "49a61cf93df5beac903c441087755cc81a4062bcd6f4665aa678cbf21e5c286b" => :sierra
+    sha256 "a1bdb6f01aa7b75dd2384b6d4b7952503cddb596dfa70b4ab2ea4763bd8cc81c" => :catalina
+    sha256 "749e71359f1e380c6ff0eacad07edfc249c4a47ca5243d3dcf528eea6ed3600f" => :mojave
+    sha256 "dc2b5d311734cb673eeb301d1733a324f1fa9f0594b796b13896af15370af49e" => :high_sierra
   end
 
   depends_on "go" => :build
