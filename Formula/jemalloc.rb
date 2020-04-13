dcd02dc0f1ef72376efec0fd6d2557859f790e67
Mike McQuaid <mike@mikemcquaid.com>
2020-04-13 14:47:36 +0100

    jemalloc: fix brew style.

--- Formula/jemalloc.rb
+++ Formula/jemalloc.rb
@@ -1,5 +1,5 @@
 class Jemalloc < Formula
-  desc "malloc implementation emphasizing fragmentation avoidance"
+  desc "Implementation of malloc emphasizing fragmentation avoidance"
   homepage "http://jemalloc.net/"
   url "https://github.com/jemalloc/jemalloc/releases/download/5.2.1/jemalloc-5.2.1.tar.bz2"
   sha256 "34330e5ce276099e2e8950d9335db5a875689a4c6a56751ef3b1d8c537f887f6"
