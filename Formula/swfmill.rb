6aa64fe6dddfd3ca415091a11a01e910493d42bc
Mike McQuaid <mike@mikemcquaid.com>
2020-04-13 14:47:37 +0100

    swfmill: fix brew style.

--- Formula/swfmill.rb
+++ Formula/swfmill.rb
@@ -1,5 +1,5 @@
 class Swfmill < Formula
-  desc "xml2swf and swf2xml processor"
+  desc "Processor of xml2swf and swf2xml"
   homepage "https://swfmill.org"
   url "https://www.swfmill.org/releases/swfmill-0.3.6.tar.gz"
   sha256 "db24f63963957faec02bb14b8b61cdaf7096774f8cfdeb9d3573e2e19231548e"
