056ce187f31ce89a7b59e34c3e64e5ad25669cbf
Mike McQuaid <mike@mikemcquaid.com>
2020-04-13 14:47:37 +0100

    shunit2: fix brew style.

--- Formula/shunit2.rb
+++ Formula/shunit2.rb
@@ -1,5 +1,5 @@
 class Shunit2 < Formula
-  desc "xUnit unit testing framework for Bourne-based shell scripts"
+  desc "Unit testing framework for Bourne-based shell scripts"
   homepage "https://github.com/kward/shunit2"
   url "https://github.com/kward/shunit2/archive/v2.1.8.tar.gz"
   sha256 "b2fed28ba7282e4878640395284e43f08a029a6c27632df73267c8043c71b60c"
