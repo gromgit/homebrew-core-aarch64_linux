8cdee7e758bde06249ac06c2142254748a49c2a1
chenrui <chenrui333@gmail.com>
2020-04-13 00:50:00 +0000

    rgxg: update 0.1.2 bottle.

--- Formula/rgxg.rb
+++ Formula/rgxg.rb
@@ -6,13 +6,9 @@ class Rgxg < Formula
 
   bottle do
     cellar :any
-    sha256 "2d6e655c81a471deca35be0df8843f92f3e192f17a77a55cfcecfd0a0a2f3408" => :catalina
-    sha256 "6104ec3b5902403a86044b75d4f97f522450f42021dead1445c5a08d0b09c35a" => :mojave
-    sha256 "5a3dcbe7906757077109d8a6b2c6cce5389de96175392ee38daeb657da2e5e5a" => :high_sierra
-    sha256 "3b15445df62f8f57c4447a85cd719251f8820596548913e76d9738124d08f763" => :sierra
-    sha256 "e84c6dfcb4195ef84bd4c5373bfa6029ea4d8600f94b58bc97fbee7334bf3f74" => :el_capitan
-    sha256 "68a4566c40db42aa298862e8b2cc02dc16dfcb9f373454e48be31e0899dc6a64" => :yosemite
-    sha256 "6fc96e2fbf981e67c6374928430dbfd20691d59f6b5dd7b1e255284edeeb6fc5" => :mavericks
+    sha256 "4a07550d93bedfa3b2ac3cb77a8484951321697ca9384d2c2a0301ea261aa954" => :catalina
+    sha256 "b410fe9ea150e0fb52326e4f7ce6642f946098b0713c5741c64699de3f55f762" => :mojave
+    sha256 "286318be76fc55c094da739c44176d5babd814df1e4f0462711aea283db042f5" => :high_sierra
   end
 
   def install
