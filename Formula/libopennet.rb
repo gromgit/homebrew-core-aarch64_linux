c32b8246951a83a5aa90a2f65fdb0f100b15a4d4
Mike McQuaid <mike@mikemcquaid.com>
2020-04-13 14:47:36 +0100

    libopennet: fix brew style.

--- Formula/libopennet.rb
+++ Formula/libopennet.rb
@@ -1,5 +1,5 @@
 class Libopennet < Formula
-  desc "open_net(), similar to open()"
+  desc "Provides open_net() (similar to open())"
   homepage "https://www.rkeene.org/oss/libopennet"
   url "https://www.rkeene.org/files/oss/libopennet/libopennet-0.9.9.tar.gz"
   sha256 "d1350abe17ac507ffb50d360c5bf8290e97c6843f569a1d740f9c1d369200096"
