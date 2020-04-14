37ccfff702dc42295e987a3ecec63cd1f787510f
David Alexander <opensource@thelonelyghost.com>
2020-04-14 11:54:17 -0400

    k3d: default to latest k3s
    Reverts a hack from f59d6da9293 to use modern version of k3s, now we can
    just refer to the `latest` tag on docker hub, which is the default in
    the application source code.
    
    Signed-off-by: David Alexander <opensource@thelonelyghost.com>
    
    Closes #53142.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/k3d.rb
+++ Formula/k3d.rb
@@ -3,6 +3,7 @@ class K3d < Formula
   homepage "https://github.com/rancher/k3d"
   url "https://github.com/rancher/k3d/archive/v1.7.0.tar.gz"
   sha256 "e741809eb27f707c0f22c19a41ebbd6be7c20ec275285bb12bbf437a675aafb7"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -16,8 +17,7 @@ class K3d < Formula
   def install
     system "go", "build",
            "-mod", "vendor",
-           "-ldflags", "-s -w -X github.com/rancher/k3d/version.Version=v#{version} " \
-                       "-X github.com/rancher/k3d/version.K3sVersion=v1.0.1",
+           "-ldflags", "-s -w -X github.com/rancher/k3d/version.Version=v#{version}",
            "-trimpath", "-o", bin/"k3d"
     prefix.install_metafiles
   end
