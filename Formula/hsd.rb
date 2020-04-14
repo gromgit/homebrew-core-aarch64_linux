692edb32571662e6b8fe763be5b9e12bac198fa1
Mark Tyneway <mark.tyneway@gmail.com>
2020-04-14 13:46:22 -0700

    hsd: update formula deps, link cli tool
    Closes #53160.
    
    Signed-off-by: Jonathan Chang <jonathan.chang@monash.edu>

--- Formula/hsd.rb
+++ Formula/hsd.rb
@@ -12,13 +12,15 @@ class Hsd < Formula
     sha256 "3efaa9f3ab569d428064329dbceff2426c65ffaa939a905a872a8b154c1f6a9f" => :high_sierra
   end
 
-  depends_on "python" => :build
+  depends_on "python@3.8" => :build
   depends_on "node@10"
   depends_on "unbound"
 
   def install
     system "#{Formula["node@10"].bin}/npm", "install", *Language::Node.std_npm_install_args(libexec)
     (bin/"hsd").write_env_script libexec/"bin/hsd", :PATH => "#{Formula["node@10"].opt_bin}:$PATH"
+    bin.install_symlink libexec/"bin/hsd-cli"
+    bin.install_symlink libexec/"bin/hsw-cli"
   end
 
   test do
