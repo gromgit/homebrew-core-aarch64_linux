f7147917ad1a20eeecb813f31bbff9e5ca874fb4
Bo Anderson <mail@boanderson.me>
2020-04-15 01:14:48 +0100

    imagemagick@6: fix reference to pkg-config
    Closes #53162.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/imagemagick@6.rb
+++ Formula/imagemagick@6.rb
@@ -7,6 +7,7 @@ class ImagemagickAT6 < Formula
   url "https://dl.bintray.com/homebrew/mirror/imagemagick%406-6.9.11-6.tar.xz"
   mirror "https://www.imagemagick.org/download/releases/ImageMagick-6.9.11-6.tar.xz"
   sha256 "fc7405ad33e50f97c7961b0ca548e9fbf4bf7cdb68e36b0266a9eead0244ecef"
+  revision 1
   head "https://github.com/imagemagick/imagemagick6.git"
 
   bottle do
@@ -32,6 +33,9 @@ class ImagemagickAT6 < Formula
   skip_clean :la
 
   def install
+    # Avoid references to shim
+    inreplace Dir["**/*-config.in"], "@PKG_CONFIG@", Formula["pkg-config"].opt_bin/"pkg-config"
+
     args = %W[
       --disable-osx-universal-binary
       --prefix=#{prefix}
