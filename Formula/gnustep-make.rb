4d5696fa3c5d7044da2a1ea3d9dc44fb1dffcd00
Bo Anderson <mail@boanderson.me>
2020-04-15 02:34:18 +0100

    gnustep-make 2.8.0
    Closes #53164.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/gnustep-make.rb
+++ Formula/gnustep-make.rb
@@ -1,8 +1,8 @@
 class GnustepMake < Formula
   desc "Basic GNUstep Makefiles"
   homepage "http://gnustep.org"
-  url "http://ftpmain.gnustep.org/pub/gnustep/core/gnustep-make-2.7.0.tar.gz"
-  sha256 "90a01cbfb68aafe01c4cc4123121ebd2da0e1e2076795b5682f0833fddf311ce"
+  url "http://ftpmain.gnustep.org/pub/gnustep/core/gnustep-make-2.8.0.tar.gz"
+  sha256 "9fce2942dd945c103df37d668dd5fff650b23351b25a650428f6f59133f5ca5d"
 
   bottle do
     cellar :any_skip_relocation
