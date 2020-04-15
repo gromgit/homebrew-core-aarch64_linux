e676652e197db11b659cd9fb7741fc6ede97754e
Dawid Dziurla <dawidd0811@gmail.com>
2020-04-15 11:48:44 +0000

    geph2: add 0.20.2 bottle.

--- Formula/geph2.rb
+++ Formula/geph2.rb
@@ -4,6 +4,13 @@ class Geph2 < Formula
   url "https://github.com/geph-official/geph2/archive/v0.20.2.tar.gz"
   sha256 "fe5bef5ed04ab6f2e254e7e142ce93d57f5f1c157651db2c29aaf3cf13abf647"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "11a121a53802b0197c326309ae592106bf0c8ade75c33de968d93aff42a6c9f4" => :catalina
+    sha256 "11a121a53802b0197c326309ae592106bf0c8ade75c33de968d93aff42a6c9f4" => :mojave
+    sha256 "11a121a53802b0197c326309ae592106bf0c8ade75c33de968d93aff42a6c9f4" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
