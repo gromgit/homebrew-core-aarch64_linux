bfd4c2add191b096d8b1a676db132647dd218c64
Bo Anderson <mail@boanderson.me>
2020-04-15 16:01:07 +0100

    ruby: fix MJIT_CC using shim

--- Formula/ruby.rb
+++ Formula/ruby.rb
@@ -3,6 +3,7 @@ class Ruby < Formula
   homepage "https://www.ruby-lang.org/"
   url "https://cache.ruby-lang.org/pub/ruby/2.7/ruby-2.7.1.tar.xz"
   sha256 "b224f9844646cc92765df8288a46838511c1cec5b550d8874bd4686a904fcee7"
+  revision 1
 
   bottle do
     sha256 "7f5ed2afb15b25f9616b617ecca2ee376eb67cf6c105790df22221b7be9c1ef9" => :catalina
@@ -57,6 +58,9 @@ class Ruby < Formula
       --without-gmp
     ]
     args << "--disable-dtrace" unless MacOS::CLT.installed?
+
+    # Correct MJIT_CC to not use superenv shim
+    args << "MJIT_CC=/usr/bin/clang"
 
     system "./configure", *args
 
