514917fbc98bdb2209fe7a3bee78b675f0d37468
Bo Anderson <mail@boanderson.me>
2020-04-15 16:00:20 +0100

    perl@5.18: fix sed reference
    Closes #53186.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/perl@5.18.rb
+++ Formula/perl@5.18.rb
@@ -3,6 +3,7 @@ class PerlAT518 < Formula
   homepage "https://www.perl.org/"
   url "https://www.cpan.org/src/5.0/perl-5.18.4.tar.gz"
   sha256 "01a4e11a9a34616396c4a77b3cef51f76a297e1a2c2c490ae6138bf0351eb29f"
+  revision 1
 
   bottle do
     sha256 "bdfac28ec4bf1ea1859b23564e0e4fd80ffe5ec5f7b54efc21cf8f1c47b396b8" => :high_sierra
@@ -13,14 +14,17 @@ class PerlAT518 < Formula
   keg_only :versioned_formula
 
   def install
-    args = [
-      "-des",
-      "-Dprefix=#{prefix}",
-      "-Dman1dir=#{man1}",
-      "-Dman3dir=#{man3}",
-      "-Duseshrplib",
-      "-Duselargefiles",
-      "-Dusethreads",
+    ENV.deparallelize if MacOS.version >= :catalina
+
+    args = %W[
+      -des
+      -Dprefix=#{prefix}
+      -Dman1dir=#{man1}
+      -Dman3dir=#{man3}
+      -Duseshrplib
+      -Duselargefiles
+      -Dusethreads
+      -Dsed=/usr/bin/sed
     ]
 
     system "./Configure", *args
