92852a8ea8c9bf67a2a4dc2cb1ff4b37c55fea04
Bo Anderson <mail@boanderson.me>
2020-04-15 10:09:46 +0000

    gnustep-make: update 2.8.0 bottle.

--- Formula/gnustep-make.rb
+++ Formula/gnustep-make.rb
@@ -6,12 +6,9 @@ class GnustepMake < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "02291b90795cb7d5c797883f993e6b3877b0e8bd4be7ca0e2a8f7143838ff705" => :catalina
-    sha256 "ec339c4c3d597509b68e07ff092582075e5d60d07f4dbc8ca208915b3b6b1451" => :mojave
-    sha256 "2f9b5760d28e77b0ffc8f51f9adc639ea69f97f3105da2c21cae90d08132b53e" => :high_sierra
-    sha256 "7a2905ce98897c3f74f4802b66f27105c095f078074a874335f2286bec1957db" => :sierra
-    sha256 "a7fc1782bf06e382a4f240f648e55d09a2294202b7f2a7bedf506823acb67fa4" => :el_capitan
-    sha256 "5e005875a2b2b9edd967edcedf14ee4e927312d7e97878ea14d509e10e25186b" => :yosemite
+    sha256 "0112f9b5cc350a2e8efc7eff2ea1b3e0b13e62877ce02592eac34052b33de00f" => :catalina
+    sha256 "4025644721c7902db42e5f63a1d8980056b809bdab7237289b92381e82492cca" => :mojave
+    sha256 "ff2edab383602c3449f074284f992567b0d072a3a442be898be21da0d484d3c3" => :high_sierra
   end
 
   def install
