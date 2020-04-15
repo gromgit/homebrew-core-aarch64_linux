0a493a96f7a5f6602c611b6c4dfb96d2cd185bce
chenrui <chenrui333@gmail.com>
2020-04-15 04:40:14 +0000

    go-jsonnet: add 0.15.0 bottle.

--- Formula/go-jsonnet.rb
+++ Formula/go-jsonnet.rb
@@ -5,6 +5,13 @@ class GoJsonnet < Formula
   sha256 "cddc3462151de4c5c65183b613895575db06297edd12a63e740f09e5e0ed98dd"
   head "https://github.com/google/go-jsonnet.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bb17871662de6fcd37242ec9a60782b50840ed5bcfdef15d2051e7d85f58d64e" => :catalina
+    sha256 "c1a8a6dd1935906504fcd72b60cd6291efaea0be467002192a6fbbf9dc0c2f5d" => :mojave
+    sha256 "79e3dd913f59a061ae00bc98dd119791641395b730ebc233b26b583deb4a186a" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   conflicts_with "jsonnet", :because => "both install binaries with the same name"
