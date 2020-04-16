7d3cfd9041115acb0b4c89e84c4688c2b278d55a
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-04-16 08:21:06 +0000

    tengo: add 2.1.2 bottle.

--- Formula/tengo.rb
+++ Formula/tengo.rb
@@ -4,6 +4,13 @@ class Tengo < Formula
   url "https://github.com/d5/tengo/archive/v2.1.2.tar.gz"
   sha256 "6940faef1e77cda77d791295a49d1034f23023445a63cff8391622acfb10a09a"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f3cd1bd6316ad686042587f752726a5a45cc81e4de633c92b7dcb3371726623f" => :catalina
+    sha256 "f3cd1bd6316ad686042587f752726a5a45cc81e4de633c92b7dcb3371726623f" => :mojave
+    sha256 "f3cd1bd6316ad686042587f752726a5a45cc81e4de633c92b7dcb3371726623f" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
