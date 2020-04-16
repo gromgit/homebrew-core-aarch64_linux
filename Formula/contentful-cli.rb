bbb55ebe02f76082dde002caea34f47d582a901f
Bo Anderson <mail@boanderson.me>
2020-04-16 13:32:00 +0000

    contentful-cli: update 1.3.15 bottle.

--- Formula/contentful-cli.rb
+++ Formula/contentful-cli.rb
@@ -9,9 +9,9 @@ class ContentfulCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3bc5fbecf8a23abd47d4de2ce794a7da867c28955896759067a37d7c6579112c" => :catalina
-    sha256 "78f33dc21052c8c6fcb53c928a27945f3d646c618bd2cc94ab51111da19c99b2" => :mojave
-    sha256 "3d64bd5c37604dd0100231d77b5f46d937819875c42f8f1611469f5387be11e7" => :high_sierra
+    sha256 "aa9de3ba601ae69eb6a90737ac0cff203fc56027ac24fcbcfa0551bf18d1c305" => :catalina
+    sha256 "ad15018573a44d0b10f10b86b01d6f1fd1772b7c1550a5548f390e141557bc78" => :mojave
+    sha256 "38a3a7a84d1bcaf05adeb21a6ece84ecbfd3733b43d977f6aeb75b472503d25f" => :high_sierra
   end
 
   depends_on "node"
