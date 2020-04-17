e49a6bbce0492193ef39fa8299ac827cbed777d4
Caleb Xu <calebcenter@live.com>
2020-04-17 12:39:57 -0400

    closure-compiler 20200406 (#53279)

--- Formula/closure-compiler.rb
+++ Formula/closure-compiler.rb
@@ -1,8 +1,8 @@
 class ClosureCompiler < Formula
   desc "JavaScript optimizing compiler"
   homepage "https://github.com/google/closure-compiler"
-  url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20200315/closure-compiler-v20200315.jar"
-  sha256 "7cbce403e1c17949f86c9838ef2b16e18c2f9c33be8dfc59257655f11c392d86"
+  url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20200406/closure-compiler-v20200406.jar"
+  sha256 "dd72360757ecf326c124a44d5f9ec093ffdffbf9888d69f201b83a606e0a3d05"
 
   bottle :unneeded
 
