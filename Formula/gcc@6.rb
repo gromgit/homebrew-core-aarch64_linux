d22f8d2eb4aa6fe698628445009574cefc498a47
Bo Anderson <mail@boanderson.me>
2020-04-17 02:05:13 +0000

    gcc@6: update 6.5.0_5 bottle.

--- Formula/gcc@6.rb
+++ Formula/gcc@6.rb
@@ -7,9 +7,9 @@ class GccAT6 < Formula
   revision 5
 
   bottle do
-    sha256 "7c319573ebe9a6234ef5f0672fee30a9a5d6f7a6d31db0ef7621fdd7587ef713" => :catalina
-    sha256 "7d28d93f7ea639423da60681f6259c0a69a926fc2e008fbd0a5a41ded2172bcb" => :mojave
-    sha256 "9a65db1f7d36cfe7be6e9a3c08d3ec7a3d5c6b0d03b3dc4328cb1d319e6c5f58" => :high_sierra
+    sha256 "59e24c6441d4ebd6e6462406d07a4722f765b81aa96b0c81117376cf342641b0" => :catalina
+    sha256 "2280bb37e05ca20d3d797a4b75695ea6d57196c2f4b4003d43ba191883558073" => :mojave
+    sha256 "e3ab3cd2f05c00351825fb0a6bf2b4e57e959400d8f7931d47b9c81a3e5b6ad3" => :high_sierra
   end
 
   # The bottles are built on systems with the CLT installed, and do not work
