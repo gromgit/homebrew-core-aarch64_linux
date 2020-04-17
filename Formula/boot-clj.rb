b0e9082a93b8bba1a962f4de8503e309f222bac8
Bo Anderson <mail@boanderson.me>
2020-04-17 15:44:19 +0100

    boot-clj: pass BOOT_JVM_OPTIONS (#53273)
    Fixes #53209.

--- Formula/boot-clj.rb
+++ Formula/boot-clj.rb
@@ -3,7 +3,7 @@ class BootClj < Formula
   homepage "https://boot-clj.com/"
   url "https://github.com/boot-clj/boot/releases/download/2.8.3/boot.jar"
   sha256 "31f001988f580456b55a9462d95a8bf8b439956906c8aca65d3656206aa42ec7"
-  revision 1
+  revision 2
 
   bottle :unneeded
 
@@ -14,7 +14,8 @@ class BootClj < Formula
     (bin/"boot").write <<~EOS
       #!/bin/bash
       export JAVA_HOME="${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
-      exec "${JAVA_HOME}/bin/java" -Dboot.app.path="#{bin}/boot" -jar "#{libexec}/boot.jar" "$@"
+      declare -a "options=($BOOT_JVM_OPTIONS)"
+      exec "${JAVA_HOME}/bin/java" "${options[@]}" -Dboot.app.path="#{bin}/boot" -jar "#{libexec}/boot.jar" "$@"
     EOS
   end
 
