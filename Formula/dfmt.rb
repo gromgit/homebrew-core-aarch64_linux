ce26ea65c1a139a4a9580ebdd787990987434725
Rui Chen <chenrui333@gmail.com>
2020-04-18 14:18:26 -0400

    dfmt 0.12.0
    Closes #53333.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/dfmt.rb
+++ Formula/dfmt.rb
@@ -2,13 +2,12 @@ class Dfmt < Formula
   desc "Formatter for D source code"
   homepage "https://github.com/dlang-community/dfmt"
   url "https://github.com/dlang-community/dfmt.git",
-      :tag      => "v0.11.0",
-      :revision => "a4d4d94a50a19b903408825055427c44996811a0"
+      :tag      => "v0.12.0",
+      :revision => "09caf255e537ea832a87c9aeb7ec4ed38d751300"
   head "https://github.com/dlang-community/dfmt.git", :branch => "v0.x.x", :shallow => false
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
     sha256 "201eb36e0b6461b52ec774f665665660eba0f757a2af342ab76849bcb4252c9d" => :catalina
     sha256 "707ba50ad974af797a50ed7470a8b53254b6882bf32d338861987df190fdddd9" => :mojave
     sha256 "247777bbba22b6934b2be5223cc234d46b42f97777781818b6cd8b2508dcc52b" => :high_sierra
