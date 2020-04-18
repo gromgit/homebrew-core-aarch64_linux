ea2bb99b2b527dff762e168aad62d84242901dbd
Odysseas Georgoudis <odygrd@gmail.com>
2020-04-18 17:12:14 +0100

    quill 1.2.0
    Closes #53304.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

---
+++ Formula/quill.rb
@@ -0,0 +1,38 @@
+class Quill < Formula
+  desc "C++14 Asynchronous Low Latency Logging Library"
+  homepage "https://github.com/odygrd/quill"
+  url "https://github.com/odygrd/quill/archive/v1.2.1.tar.gz"
+  sha256 "14b3926e3b529e3e843dd80d72d34e18db81e9f9a9f7cb400c4737ed276fbfcd"
+  head "https://github.com/odygrd/quill.git"
+
+  depends_on "cmake" => :build
+
+  def install
+    ENV.cxx11
+
+    mkdir "quill-build" do
+      args = std_cmake_args
+      args << "-Dpkg_config_libdir=#{lib}" << ".."
+      system "cmake", *args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include "quill/Quill.h"
+      int main()
+      {
+        quill::start();
+        quill::Handler* file_handler = quill::file_handler("#{testpath}/basic-log.txt", "w");
+        quill::Logger* logger = quill::create_logger("logger_bar", file_handler);
+        LOG_INFO(logger, "Test");
+      }
+    EOS
+
+    system ENV.cxx, "-std=c++14", "test.cpp", "-I#{include}", "-L#{lib}", "-lquill", "-o", "test"
+    system "./test"
+    assert_predicate testpath/"basic-log.txt", :exist?
+    assert_match "Test", (testpath/"basic-log.txt").read
+  end
+end
