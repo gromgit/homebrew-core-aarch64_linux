b6ad62d2bab0ae56e839c5efad788e2caadbdd80
Kelly Brazil <kellyjonbrazil@gmail.com>
2020-04-17 17:44:47 -0700

    jc 1.10.6 (new formula)
    jc 1.10.6 (new formula)
    
    add ruamel.yaml.clib requirement. working install
    
    fix test
    
    resolve conflicts
    
    jc 1.10.6 (new formula)
    
    Squashed commits:
    [f865b4a023] updated to new version of jc that does not include homebrew/shim references in tests
    
    Closes #53285.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/jc.rb
@@ -0,0 +1,44 @@
+class Jc < Formula
+  include Language::Python::Virtualenv
+
+  desc "Serializes the output of command-line tools to structured JSON output"
+  homepage "https://github.com/kellyjonbrazil/jc"
+  url "https://files.pythonhosted.org/packages/17/7f/5820ca206b8ce79142c5385ff5a4262757a0f568d96beee45945dfbd83db/jc-1.10.6.tar.gz"
+  sha256 "b4995a9c2b1e5ba15152d5253089535440ca1c715a0e53267fb61997278f5422"
+
+  depends_on "python@3.8"
+
+  resource "ifconfig-parser" do
+    url "https://files.pythonhosted.org/packages/19/58/887683fe9baa51f307e1fe58d8beea5beb714a210b2ff6e6430a1430bb34/ifconfig-parser-0.0.5.tar.gz"
+    sha256 "6c512f12da0d7f314bb16f883e2d7d89d53198e88f6272a2465330cef117c58a"
+  end
+
+  resource "ruamel.yaml" do
+    url "https://files.pythonhosted.org/packages/16/8b/54a26c1031595e5edd0e616028b922d78d8ffba8bc775f0a4faeada846cc/ruamel.yaml-0.16.10.tar.gz"
+    sha256 "099c644a778bf72ffa00524f78dd0b6476bca94a1da344130f4bf3381ce5b954"
+  end
+
+  resource "ruamel.yaml.clib" do
+    url "https://files.pythonhosted.org/packages/92/28/612085de3fae9f82d62d80255d9f4cf05b1b341db1e180adcf28c1bf748d/ruamel.yaml.clib-0.2.0.tar.gz"
+    sha256 "b66832ea8077d9b3f6e311c4a53d06273db5dc2db6e8a908550f3c14d67e718c"
+  end
+
+  resource "xmltodict" do
+    url "https://files.pythonhosted.org/packages/58/40/0d783e14112e064127063fbf5d1fe1351723e5dfe9d6daad346a305f6c49/xmltodict-0.12.0.tar.gz"
+    sha256 "50d8c638ed7ecb88d90561beedbf720c9b4e851a9fa6c47ebd64e99d166d8a21"
+  end
+
+  resource "Pygments" do
+    url "https://files.pythonhosted.org/packages/6e/4d/4d2fe93a35dfba417311a4ff627489a947b01dc0cc377a3673c00cf7e4b2/Pygments-2.6.1.tar.gz"
+    sha256 "647344a061c249a3b74e230c739f434d7ea4d8b1d5f3721bc0f3558049b38f44"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_equal "[{\"header1\": \"data1\", \"header2\": \"data2\"}]\n", \
+                  pipe_output("#{bin}/jc --csv", "header1, header2\n data1, data2")
+  end
+end
