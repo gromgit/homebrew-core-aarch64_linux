c58ad8b077aab967abc0a856ae2ed0eb13d24532
chenrui <chenrui333@gmail.com>
2020-04-19 17:51:12 -0400

    testssl 3.0.1 (#53414)

--- Formula/testssl.rb
+++ Formula/testssl.rb
@@ -1,8 +1,8 @@
 class Testssl < Formula
   desc "Tool which checks for the support of TLS/SSL ciphers and flaws"
   homepage "https://testssl.sh/"
-  url "https://github.com/drwetter/testssl.sh/archive/3.0.tar.gz"
-  sha256 "ab3c9a000f0f6703e4fc94821e06f531de6d2799322bf534188ebf766365a9c1"
+  url "https://github.com/drwetter/testssl.sh/archive/3.0.1.tar.gz"
+  sha256 "f38f25336cf59ecdf0ae3826982a6580ea53b056fb96d1430cf97645a8b1b560"
   head "https://github.com/drwetter/testssl.sh.git", :branch => "3.1dev"
 
   bottle :unneeded
