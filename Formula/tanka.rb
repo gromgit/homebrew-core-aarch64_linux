03e8944b95665478ee74e72fdaf04248b471b1d3
sh0rez <me@shorez.de>
2020-04-20 00:15:28 +0200

    tanka v0.9.2 (new formula)
    - test: execise tk binary using show
    - feat: add commit hash
    
    Closes #53421.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

---
+++ Formula/tanka.rb
@@ -0,0 +1,21 @@
+class Tanka < Formula
+  desc "Flexible, reusable and concise configuration for Kubernetes using Jsonnet"
+  homepage "https://tanka.dev"
+  url "https://github.com/grafana/tanka.git",
+    :tag     => "v0.9.2",
+    :revsion => "178f4bef263581a8cdf298a11365d412e11b6351"
+  head "https://github.com/grafana/tanka.git"
+
+  depends_on "go" => :build
+  depends_on "kubernetes-cli"
+
+  def install
+    system "make", "static"
+    bin.install "tk"
+  end
+
+  test do
+    system "git", "clone", "https://github.com/sh0rez/grafana.libsonnet"
+    system "#{bin}/tk", "show", "--dangerous-allow-redirect", "grafana.libsonnet/environments/default"
+  end
+end
