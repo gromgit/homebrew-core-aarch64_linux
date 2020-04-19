a5c1284d2ca8caa78083d2e499a18a6431ba5ef7
chenrui <chenrui333@gmail.com>
2020-04-19 00:25:22 -0400

    embulk: add note for 0.10.x release (#52996)

--- Formula/embulk.rb
+++ Formula/embulk.rb
@@ -1,6 +1,9 @@
 class Embulk < Formula
   desc "Data transfer between various databases, file formats and services"
   homepage "https://www.embulk.org/"
+  # https://www.embulk.org/articles/2020/03/13/embulk-v0.10.html
+  # v0.10.* is a "development" series, not for your production use.
+  # In your production, keep using v0.9.*.
   url "https://bintray.com/artifact/download/embulk/maven/embulk-0.9.23.jar"
   sha256 "153977fad482bf52100dd96f47e897c87b48de4fb13bccd6b3101475d3a5ebb9"
 
