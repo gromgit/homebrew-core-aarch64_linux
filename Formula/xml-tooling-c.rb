5536cfb1e833e0cfe667018ae79f6719978399cf
Rui Chen <chenrui333@gmail.com>
2020-04-19 17:45:05 -0400

    xml-tooling-c: use from osx curl
    Closes #53417.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/xml-tooling-c.rb
+++ Formula/xml-tooling-c.rb
@@ -20,34 +20,12 @@ class XmlToolingC < Formula
   depends_on "xerces-c"
   depends_on "xml-security-c"
 
-  resource "curl" do
-    url "https://curl.haxx.se/download/curl-7.65.1.tar.bz2"
-    mirror "https://curl.askapache.com/download/curl-7.65.1.tar.bz2"
-    sha256 "cbd36df60c49e461011b4f3064cff1184bdc9969a55e9608bf5cadec4686e3f7"
-  end
+  uses_from_macos "curl"
 
   def install
     ENV.cxx11
 
     ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["openssl@1.1"].opt_lib}/pkgconfig"
-
-    resource("curl").stage do
-      system "./configure", "--disable-debug",
-                            "--disable-dependency-tracking",
-                            "--disable-silent-rules",
-                            "--prefix=#{libexec}/curl",
-                            "--with-ssl=#{Formula["openssl@1.1"].opt_prefix}",
-                            "--with-ca-bundle=#{etc}/openssl/cert.pem",
-                            "--with-ca-path=#{etc}/openssl/certs",
-                            "--without-libssh2",
-                            "--without-libmetalink",
-                            "--without-gssapi",
-                            "--without-librtmp",
-                            "--disable-ares"
-      system "make", "install"
-    end
-
-    ENV.prepend_path "PKG_CONFIG_PATH", libexec/"curl/lib/pkgconfig"
 
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
