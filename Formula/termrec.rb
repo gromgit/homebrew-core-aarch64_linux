25b6ab77a0d4b49dfb2178c7db5a2b12bd4ee676
chenrui <chenrui333@gmail.com>
2020-04-19 21:52:38 +0000

    termrec: update 0.19 bottle.

--- Formula/termrec.rb
+++ Formula/termrec.rb
@@ -7,10 +7,9 @@ class Termrec < Formula
 
   bottle do
     cellar :any
-    sha256 "4c6ec1ae65b46494728ca14fdd9112474b8030d6d5d5b6abcd312b4ae6dff5ad" => :catalina
-    sha256 "062bb1a65c337321569546e845048d3fb5912113bca87080530b72cd262fc8ff" => :mojave
-    sha256 "61ffceeab4daf5feeeada49c1057ddafc8c5efb0c1561d7b8e1303462cba490b" => :high_sierra
-    sha256 "b51ae0744fb22e3807841f852452c422f9117d53c83503b8b89ea7936ba700e7" => :sierra
+    sha256 "1d93149ec34c0bf531da76b0137390ed1f05bf2e35e806f1fe875fe6648c4c2b" => :catalina
+    sha256 "e3f9f241763a05de367da2ee91727674e18a126a99480a750b901a21bdad0ffb" => :mojave
+    sha256 "d6cb43ed14ec0531824bd4eb55ddc625b5711c28b274ce78eb815501e5f3ebf2" => :high_sierra
   end
 
   depends_on "autoconf" => :build
