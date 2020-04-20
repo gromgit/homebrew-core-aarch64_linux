e3213dd27183793ba9038bd1efd15d5857d101ef
Bo Anderson <mail@boanderson.me>
2020-04-20 12:03:02 +0000

    robotfindskitten: update 2.8284271.702 bottle.

--- Formula/robotfindskitten.rb
+++ Formula/robotfindskitten.rb
@@ -5,11 +5,9 @@ class Robotfindskitten < Formula
   sha256 "020172e4f4630f7c4f62c03b6ffe2eeeba5637b60374d3e6952ae5816a9f99af"
 
   bottle do
-    cellar :any_skip_relocation
-    rebuild 1
-    sha256 "f18e0b6bddba41feb96ea70b94bc24b9d66e991471baed046f7c337b42f1af93" => :catalina
-    sha256 "61ed19fc71545c4dda1757de56dfb02ac8f6099e723f3755f4014207202a80e7" => :mojave
-    sha256 "458ca9d932ef2ca25d6a511fc3f9712a17968ea805f270080e7477cd3ac9fb0d" => :high_sierra
+    sha256 "439c4c752b9606e645719ca46388b0143a0f3feb6ff248b4fd0d370f3192d506" => :catalina
+    sha256 "4bb958df30f8b4d8a46b24f8126404bfb545040c0cafaed7fb14c8965d46c2fc" => :mojave
+    sha256 "667bd9a9abd587202ccd8519ace649638e3eeeb859021c2b9f2014ec55f69ea9" => :high_sierra
   end
 
   def install
