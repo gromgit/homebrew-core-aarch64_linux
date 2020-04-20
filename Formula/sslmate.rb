11e3814b04c72880abe641b81df7531ab13261e7
Bo Anderson <mail@boanderson.me>
2020-04-20 12:04:50 +0000

    sslmate: update 1.7.1 bottle.

--- Formula/sslmate.rb
+++ Formula/sslmate.rb
@@ -10,10 +10,9 @@ class Sslmate < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "675270c9a7dd6ed26c528153b3bb96d85928b4f9bf0eac45aa66a431b251e6fe" => :catalina
-    sha256 "7c8c3adbd6de365695d82dea6c27182c2032ebd0a09d08d4a5e20cce91abc029" => :mojave
-    sha256 "fd6edadfa6af0d2a2bb7390ac37b588a3c1970678e0bfcb306958902e4aea4e5" => :high_sierra
-    sha256 "fd6edadfa6af0d2a2bb7390ac37b588a3c1970678e0bfcb306958902e4aea4e5" => :sierra
+    sha256 "39df7966fbc2d8ddf054fca99456e3f5837b78c849bacc25fd40a0ae9e48bb60" => :catalina
+    sha256 "39df7966fbc2d8ddf054fca99456e3f5837b78c849bacc25fd40a0ae9e48bb60" => :mojave
+    sha256 "39df7966fbc2d8ddf054fca99456e3f5837b78c849bacc25fd40a0ae9e48bb60" => :high_sierra
   end
 
   depends_on "python@3.8"
