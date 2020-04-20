15581744b1d6788b21890183a56427b6e44f3d94
Bo Anderson <mail@boanderson.me>
2020-04-20 21:18:15 +0100

    funcoeszz 18.3 (#53440)

--- Formula/funcoeszz.rb
+++ Formula/funcoeszz.rb
@@ -1,8 +1,8 @@
 class Funcoeszz < Formula
   desc "Dozens of command-line mini-applications (Portuguese)"
   homepage "https://funcoeszz.net/"
-  url "https://funcoeszz.net/download/funcoeszz-15.5.sh"
-  sha256 "13e6347018b43c54f5032d4700d45255f7e26e1e8f1eacb25e324d4fbe07e15d"
+  url "https://funcoeszz.net/download/funcoeszz-18.3.sh"
+  sha256 "c1348fbaf79e7ac97568785674edee602077c3a747d3a1bfa4cf63af343c4352"
 
   bottle :unneeded
 
