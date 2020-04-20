0161e94384892680c6662823bd46f7fc581247f0
Bo Anderson <mail@boanderson.me>
2020-04-20 17:08:38 +0100

    wumpus 1.7
    Closes #53461.
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/wumpus.rb
+++ Formula/wumpus.rb
@@ -1,8 +1,8 @@
 class Wumpus < Formula
   desc "Exact clone of the ancient BASIC Hunt the Wumpus game"
   homepage "http://www.catb.org/~esr/wumpus/"
-  url "http://www.catb.org/~esr/wumpus/wumpus-1.6.tar.gz"
-  sha256 "965e49b3e53f44023994b42b3aa568ad79d3a2287bb0a07460b601500c9ae16d"
+  url "http://www.catb.org/~esr/wumpus/wumpus-1.7.tar.gz"
+  sha256 "892678a66d6d1fe2a7ede517df2694682b882797a546ac5c0568cc60b659f702"
 
   bottle do
     cellar :any_skip_relocation
@@ -12,17 +12,6 @@ class Wumpus < Formula
     sha256 "fa693e01a17d31e9e5a5b51f27ea18b69e30f5613615c95ddb89c7dbc4bbe75d" => :sierra
     sha256 "3b6751d8a871e44c033e4a6d83009bb8b499952385d8c1743be3d26bfe37b312" => :el_capitan
     sha256 "d292048f1869507766d60e6931bffbf402f99d8fe36711ed6d9dbb060407a4a6" => :yosemite
-  end
-
-  # Patches to allow `make install` to specify a prefix; both patches
-  # can be removed in the next release
-  patch do
-    url "https://gitlab.com/esr/wumpus/commit/ea272d4786a55dbaa493d016324b7a05b4f165b9.diff"
-    sha256 "7b95a5e12447b69d0958cf00bb6413a42612ffea47cdf483c6225a1980f97fb0"
-  end
-  patch do
-    url "https://gitlab.com/esr/wumpus/commit/99022db86e54c3338d6a670f219a0845fd531530.diff"
-    sha256 "143cba3992b27addb8e25e245624902b935b29cc5465cbe947dd5ae573dafdca"
   end
 
   def install
