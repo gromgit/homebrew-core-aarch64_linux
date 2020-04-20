06e994a5b657ab9bda3b4aaa0a2bb24965ef070c
Bo Anderson <mail@boanderson.me>
2020-04-20 11:56:37 +0000

    rcm: add 1.3.3 bottle.

--- Formula/rcm.rb
+++ Formula/rcm.rb
@@ -4,6 +4,13 @@ class Rcm < Formula
   url "https://thoughtbot.github.io/rcm/dist/rcm-1.3.3.tar.gz"
   sha256 "935524456f2291afa36ef815e68f1ab4a37a4ed6f0f144b7de7fb270733e13af"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3fdc7266d9faa6015ef560254f036cc355445ee12861cb17a1c0bf5cc218b368" => :catalina
+    sha256 "3fdc7266d9faa6015ef560254f036cc355445ee12861cb17a1c0bf5cc218b368" => :mojave
+    sha256 "3fdc7266d9faa6015ef560254f036cc355445ee12861cb17a1c0bf5cc218b368" => :high_sierra
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
