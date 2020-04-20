0fa7c4fb71f3cd752c77a61d935f466ebcf89bca
Bo Anderson <mail@boanderson.me>
2020-04-20 11:54:10 +0000

    bootloadhid: add 2012-12-08 bottle.

--- Formula/bootloadhid.rb
+++ Formula/bootloadhid.rb
@@ -5,6 +5,13 @@ class Bootloadhid < Formula
   version "2012-12-08"
   sha256 "154e7e38629a3a2eec2df666edfa1ee2f2e9a57018f17d9f0f8f064cc20d8754"
 
+  bottle do
+    cellar :any
+    sha256 "aa0bc95a39610d6b5951d064d781d85b898ca2ebf230acbc60aa2f4e1f51e573" => :catalina
+    sha256 "36032498ab37f82f538d6aa037dac2b2f1c90f552ab5403f3e87c184bc47e75b" => :mojave
+    sha256 "59d545d65c052c2a62f171d4b6e92098a2725cb7c44997051e96863e30d26a03" => :high_sierra
+  end
+
   depends_on "libusb-compat"
 
   def install
