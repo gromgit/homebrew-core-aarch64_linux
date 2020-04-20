93f6f16145564286a0c8670ab2424c5dd4a53443
Bo Anderson <mail@boanderson.me>
2020-04-20 12:07:02 +0000

    benthos: update 3.12.0 bottle.

--- Formula/benthos.rb
+++ Formula/benthos.rb
@@ -6,9 +6,9 @@ class Benthos < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d03c075aaa4466af2392b2c9f49855654eedc6381d4b3ba404e4a8de0776d12a" => :catalina
-    sha256 "1a529c326b86c075f3a95e9dc5e3cd0c4960c736235270f01c708f3a2fffa89b" => :mojave
-    sha256 "dc05481effbf7e4b05f3cfda297fb35cb6f2ad2d7ef78208f36ce34c05747435" => :high_sierra
+    sha256 "69305f44327522481627d9b201197ada57e11dd14915c014f910aa53571c2a1d" => :catalina
+    sha256 "9f1e8ac7a384240aa89f9d2fc214fc055f7c80c32f6f471bd94e87291fd156e5" => :mojave
+    sha256 "fdc8610ce357e8bbfa31614db80f61acdbedae708b5bf5b7fb31f7aab0f796b8" => :high_sierra
   end
 
   depends_on "go" => :build
