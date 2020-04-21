3055d059ccb58a8bea066652338f2a46c1e81502
Bo Anderson <mail@boanderson.me>
2020-04-21 12:58:56 +0100

    conan 1.24.1
    Closes #53479.
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/conan.rb
+++ Formula/conan.rb
@@ -3,9 +3,8 @@ class Conan < Formula
 
   desc "Distributed, open source, package manager for C/C++"
   homepage "https://github.com/conan-io/conan"
-  url "https://github.com/conan-io/conan/archive/1.24.0.tar.gz"
-  sha256 "fe7fc766d3ff4997a700d141485ba7dd2768cf78eb710fed413a6ccea8b7f9a1"
-  revision 1
+  url "https://github.com/conan-io/conan/archive/1.24.1.tar.gz"
+  sha256 "10d8e005a16f584cb744d42452036f723f33b8d77ab72fe18a2cf0030f238e90"
   head "https://github.com/conan-io/conan.git"
 
   bottle do
@@ -23,7 +22,7 @@ class Conan < Formula
   def install
     venv = virtualenv_create(libexec, "python3")
     system libexec/"bin/pip", "install", "-v", "--no-binary", ":all:",
-                              "--ignore-installed", "PyYAML==3.13", buildpath
+                              "--ignore-installed", buildpath
     system libexec/"bin/pip", "uninstall", "-y", name
     venv.pip_install_and_link buildpath
   end
