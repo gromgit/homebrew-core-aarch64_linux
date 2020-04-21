b8cdb6f2e46cfe933851106e92a4f1bd3f19d464
Alexander Bayandin <a.bayandin@gmail.com>
2020-04-21 14:17:12 +0100

    quill: add 1.2.1 bottle.

--- Formula/quill.rb
+++ Formula/quill.rb
@@ -5,6 +5,13 @@ class Quill < Formula
   sha256 "14b3926e3b529e3e843dd80d72d34e18db81e9f9a9f7cb400c4737ed276fbfcd"
   head "https://github.com/odygrd/quill.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "de0ab9b92b048b414b79fdbba6bcb8e145e3594b523ac057cb016123ed11f6c5" => :catalina
+    sha256 "6fa26085c6f26ecb18b03f7d37cfeab346ad721d567d27ce08a6d5a8a95d31b1" => :mojave
+    sha256 "e94c9f715720651aae036503f40954c05b48470f21820de6d9459858da361e73" => :high_sierra
+  end
+
   depends_on "cmake" => :build
 
   def install
