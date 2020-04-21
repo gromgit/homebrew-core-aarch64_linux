59fc18327c62499cdb983f90042c142eb71cf784
Bo Anderson <mail@boanderson.me>
2020-04-21 18:01:40 +0100

    clozure-cl 1.12
    Closes #53493.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/clozure-cl.rb
+++ Formula/clozure-cl.rb
@@ -1,8 +1,8 @@
 class ClozureCl < Formula
   desc "Common Lisp implementation with a long history"
   homepage "https://ccl.clozure.com"
-  url "https://github.com/Clozure/ccl/archive/v1.11.6.tar.gz"
-  sha256 "6a496d35e05dc3e6e7637884552b1f14c82296525546f28337b222e4c3d7d50b"
+  url "https://github.com/Clozure/ccl/archive/v1.12.tar.gz"
+  sha256 "774a06b4fb6dc4b51dfb26da8e1cc809c605e7706c12180805d1be6f2885bd52"
   head "https://github.com/Clozure/ccl.git"
 
   bottle do
@@ -15,8 +15,8 @@ class ClozureCl < Formula
   depends_on :xcode => :build
 
   resource "bootstrap" do
-    url "https://github.com/Clozure/ccl/releases/download/v1.11.5/ccl-1.11.5-darwinx86.tar.gz"
-    sha256 "5adbea3d8b4a2e29af30d141f781c6613844f468c0ccfa11bae908c3e9641939"
+    url "https://github.com/Clozure/ccl/releases/download/v1.12/darwinx86.tar.gz"
+    sha256 "9434fb5ebc01fc923625ad56726fdd217009e2d3c107cfa3c5435cb7692ba7ca"
   end
 
   def install
@@ -25,7 +25,16 @@ class ClozureCl < Formula
     buildpath.install tmpdir/"dx86cl64.image"
     buildpath.install tmpdir/"darwin-x86-headers64"
     cd "lisp-kernel/darwinx8664" do
-      system "make"
+      args = []
+
+      if DevelopmentTools.clang_build_version == 1100 && MacOS::CLT.installed?
+        # Xcode 11.0-11.3 assembler is broken. Try the CLT in case it is older.
+        # https://github.com/Clozure/ccl/issues/271
+        # Note: ccl NEEDS the system assembler - it is not compatible with Clang's
+        args << "AS=/Library/Developer/CommandLineTools/usr/bin/as"
+      end
+
+      system "make", *args
     end
 
     ENV["CCL_DEFAULT_DIRECTORY"] = buildpath
