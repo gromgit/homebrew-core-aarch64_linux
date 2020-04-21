cb2f980703db2da4f2586541fa8b454803b220fc
Alexander Bayandin <a.bayandin@gmail.com>
2020-04-21 11:28:08 +0000

    timewarrior: update 1.3.0 bottle.

--- Formula/timewarrior.rb
+++ Formula/timewarrior.rb
@@ -7,9 +7,9 @@ class Timewarrior < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "8bf48d94c71fabbd3ded0432500495c330374693e8c4960f1dc5f0a90dd6c3b0" => :catalina
-    sha256 "708b85bb339ff690ab4cc6b58bb4b97f90494c5f117980c2bc9290887c3d76e6" => :mojave
-    sha256 "18ee4385567276d5e73e63bc197954473a7cb1aaccbdf52325b1118c719c5914" => :high_sierra
+    sha256 "b31a1b3ffe25ae8e006e172a228a6c02b749fc0a121957b076a2ed0125ad434e" => :catalina
+    sha256 "b09c7933fd20dae4a309a6cf278b63ff6317944e2298a3dd290acbb62dc87fb6" => :mojave
+    sha256 "6a4ec380d1c382da7e67bed6072ebe20365a72a1f098e62e25d4840488e5718c" => :high_sierra
   end
 
   depends_on "cmake" => :build
