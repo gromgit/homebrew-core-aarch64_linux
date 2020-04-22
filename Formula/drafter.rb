946222e03f3fd6cc7e74376e8e4cc7a61b4585e9
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-04-22 09:01:43 +0000

    drafter: update 5.0.0 bottle.

--- Formula/drafter.rb
+++ Formula/drafter.rb
@@ -7,9 +7,9 @@ class Drafter < Formula
 
   bottle do
     cellar :any
-    sha256 "4cbb74b2c3398833b374c27fe5490386239ff6717436a1737def8b20d9e74ae9" => :catalina
-    sha256 "333b8f9b68f883f9aa75b96ec2547b5149bcf6e963f03f06d38c262e180ff503" => :mojave
-    sha256 "36c0c14b26b458caa49e41e20d6005a5fbbdac577d3ae80b1ed417ab5625975e" => :high_sierra
+    sha256 "29fa18ff148f6ebf454ed383181384bfb9aff1520e64072dfb386445bf8e52a3" => :catalina
+    sha256 "2a56e75e39f7b46eba355ae6163b645e161c4e458a4f127c37a948377143ac3e" => :mojave
+    sha256 "125fb907888693fd3d638a79d185483f44112f5bb64f098626aa17f00b25513d" => :high_sierra
   end
 
   depends_on "cmake" => :build
