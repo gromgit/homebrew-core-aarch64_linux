6d39a71935c6d359f45b07228b87cb038918f33d
Jesse Duffield <jessedduffield@gmail.com>
2020-04-22 01:27:16 +0000

    lazygit 0.20.2
    Closes #53512.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/lazygit.rb
+++ Formula/lazygit.rb
@@ -1,8 +1,8 @@
 class Lazygit < Formula
   desc "Simple terminal UI for git commands"
   homepage "https://github.com/jesseduffield/lazygit/"
-  url "https://github.com/jesseduffield/lazygit/archive/v0.20.tar.gz"
-  sha256 "36689f98b0bbb9e227cf80daa802739c77e5f8a2cc2690fdcffb3c8598a4aa95"
+  url "https://github.com/jesseduffield/lazygit/archive/v0.20.2.tar.gz"
+  sha256 "ee99f77ae67129764835e6d85afd71a4f91c61a06952a891fe8fb46d8d5425e4"
 
   bottle do
     cellar :any_skip_relocation
