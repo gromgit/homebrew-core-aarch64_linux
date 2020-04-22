cd6c90907bee54c409c64415163aa11ca078cf78
Alexander Bayandin <a.bayandin@gmail.com>
2020-04-22 13:16:40 +0000

    entr: update 4.5 bottle.

--- Formula/entr.rb
+++ Formula/entr.rb
@@ -7,9 +7,9 @@ class Entr < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "98f508565c8dd087b780fda140099fca3afb457ca27fcf3864bb508c87c403cc" => :catalina
-    sha256 "d18935ecc0bf78504d6acd00b2adb889389af2586cafc2602e38599f2590183f" => :mojave
-    sha256 "25fba36721d2857ca91efc7b82a8cbe15ff0a83f20e9febe57648fc173377629" => :high_sierra
+    sha256 "2b4d9fac8d479903227ae181d1d1d7e403d7a6ebef7848be4365ad80e8e7e380" => :catalina
+    sha256 "b83f3bd4f30ebcd65d22516f04ad7aec3260c75151488cc6b3ae15829872d3ec" => :mojave
+    sha256 "a08481646a0439de7643b26c41d100adc099ba573aabb8ca45723ce9fbfa97e2" => :high_sierra
   end
 
   def install
