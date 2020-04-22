ebe87b3be7694e0c3906e916c1e366ae98ef146a
Alexander Bayandin <a.bayandin@gmail.com>
2020-04-22 10:16:00 +0000

    gitless: update 0.8.8_4 bottle.

--- Formula/gitless.rb
+++ Formula/gitless.rb
@@ -9,9 +9,9 @@ class Gitless < Formula
 
   bottle do
     cellar :any
-    sha256 "b220d9334addd71c08152ed7b2975bc44293885f3ff81938d37eae728abe9a64" => :catalina
-    sha256 "713f9b5f089a318e6d3aecabd94ad4145293e6371c4e4874729cab02173b82a5" => :mojave
-    sha256 "7ffe8a5480d4f844273c678ab3f28b0da7928cca0015a1d0c9a61f0bfabf95e0" => :high_sierra
+    sha256 "c0ffed9f5228aecb14175929167812114176ac2df5b9583161c4f3c0f7785da0" => :catalina
+    sha256 "f72efc4323f531adcb1a38c8d864a4bd464b4a7d67400623479f05d37a4332cd" => :mojave
+    sha256 "aa526af3aaa79be5c7d86d4265589194e331766ba138e5b74894ad8037224c5c" => :high_sierra
   end
 
   depends_on "libgit2"
