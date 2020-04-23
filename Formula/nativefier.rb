b70452edb1595a40fc9524c46e7053072e9538a4
Bo Anderson <mail@boanderson.me>
2020-04-23 20:46:03 +0100

    nativefier 8.0.7
    Closes #53586.
    
    Signed-off-by: Zach Auten <zachauten@gmail.com>

--- Formula/nativefier.rb
+++ Formula/nativefier.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Nativefier < Formula
   desc "Wrap web apps natively"
   homepage "https://github.com/jiahaog/nativefier"
-  url "https://registry.npmjs.org/nativefier/-/nativefier-8.0.6.tgz"
-  sha256 "24b24ee476ac470f9ba290e66213f1b739be1c5aa8e9f2ecb8cb62824a40e40d"
+  url "https://registry.npmjs.org/nativefier/-/nativefier-8.0.7.tgz"
+  sha256 "14390eca2632a6bec498f9946a7cfb52abf052ab20e395b70edd054255fbd349"
 
   bottle do
     cellar :any_skip_relocation
