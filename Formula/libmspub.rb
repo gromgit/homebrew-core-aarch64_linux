e5a9faff7e28e8c39b66710abc896e1865e25f8e
Bo Anderson <mail@boanderson.me>
2020-04-23 14:37:03 +0000

    libmspub: update 0.1.4_5 bottle.

--- Formula/libmspub.rb
+++ Formula/libmspub.rb
@@ -7,10 +7,9 @@ class Libmspub < Formula
 
   bottle do
     cellar :any
-    sha256 "aee93415583bfd6d1b12ccbb1f18b7bad97579ab9046cfff2d303e1bc7336abf" => :catalina
-    sha256 "93f7e02d005fb22101eab93659f200ed8376130c9a4bad0c24b0cdde51b92f18" => :mojave
-    sha256 "43347c63d9fb4a6746929b2127453241cdad9b13a9fd00ddf82af418d6a175b2" => :high_sierra
-    sha256 "ebf70558240b3cc3c49994e865108d7d29796c45d1129e844ef1c99d8ae510a7" => :sierra
+    sha256 "02bb91ef4042b5f4d4ba3ce610aba4508eeef8963ad2711301679db5d48c1f93" => :catalina
+    sha256 "5c44634f544cffa8c080c5c3bffc27c31ef128c33bebe071f7944f2edb78834c" => :mojave
+    sha256 "f957b921fe2ebb07dde55b1707023fc9667e02dfdd10385b889d7044ebe5df32" => :high_sierra
   end
 
   depends_on "boost" => :build
