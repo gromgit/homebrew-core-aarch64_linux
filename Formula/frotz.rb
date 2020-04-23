2540fb4f7240633b14aa7203a3e129dd247ca3b4
Dan Fabulich <dan.fabulich@redfin.com>
2020-04-22 18:45:43 -0700

    frotz: enable sound and SDL support
    Co-authored-by: Jonathan Chang <jonathan.chang@monash.edu>
    
    Closes #53551.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/frotz.rb
+++ Formula/frotz.rb
@@ -3,6 +3,7 @@ class Frotz < Formula
   homepage "https://661.org/proj/if/frotz/"
   url "https://gitlab.com/DavidGriffith/frotz/-/archive/2.51/frotz-2.51.tar.gz"
   sha256 "7916f17061e845e4fa5047c841306c4be2614e9c941753f9739c5d39c7e9f05b"
+  revision 1
   head "https://gitlab.com/DavidGriffith/frotz.git"
 
   bottle do
@@ -11,19 +12,31 @@ class Frotz < Formula
     sha256 "c529875e350b14e50ff06777fca8b5455fe18f20a3ab9efc51929f5358a1d501" => :high_sierra
   end
 
-  def install
-    inreplace "Makefile" do |s|
-      s.change_make_var! "PREFIX", prefix
-      s.change_make_var! "MANDIR", man
-      s.change_make_var! "SYSCONFDIR", etc
-      s.change_make_var! "SOUND_TYPE", "none"
-    end
+  depends_on "pkg-config" => :build
+  depends_on "freetype"
+  depends_on "jpeg"
+  depends_on "libao"
+  depends_on "libmodplug"
+  depends_on "libpng"
+  depends_on "libsamplerate"
+  depends_on "libsndfile"
+  depends_on "libvorbis"
+  depends_on "sdl2"
+  depends_on "sdl2_mixer"
 
-    system "make", "frotz"
-    system "make", "install"
+  uses_from_macos "ncurses"
+  uses_from_macos "zlib"
+
+  def install
+    args = %W[PREFIX=#{prefix} MANDIR=#{man} SYSCONFDIR=#{etc}]
+    system "make", "all", *args
+    ENV.deparallelize # install has race condition
+    system "make", "install_all", *args
   end
 
   test do
     assert_match "FROTZ", shell_output("#{bin}/frotz --version").strip
+    assert_match "FROTZ", shell_output("#{bin}/sfrotz --version").strip
+    assert_match "FROTZ", shell_output("#{bin}/dfrotz --version").strip
   end
 end
