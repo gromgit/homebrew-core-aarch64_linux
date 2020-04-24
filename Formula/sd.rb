9e577f3fe4d205d455863a39e1c2220d5cbf49f3
Zach Auten <zachauten@gmail.com>
2020-04-23 20:17:28 -0400

    sd: update 0.7.4 bottle.

--- Formula/sd.rb
+++ Formula/sd.rb
@@ -6,9 +6,9 @@ class Sd < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b9cdd449bcfe0956157d9de33e4435edc3b3ee57316378aa3f7303cc5436d411" => :catalina
-    sha256 "05a0730808a22ab931779e1669894031295d92eff4ec66af39cd2e6be616d484" => :mojave
-    sha256 "4b1d873551f2934b59570b5a97c5458366751d7930eaceb356781e518f5f254b" => :high_sierra
+    sha256 "5bde1490283fa9844eb9af5ac0d8f2f3d744923c39a5b7c9bfd959b186a7571a" => :catalina
+    sha256 "8525800846c415c336621b5d9d284b01ab0d42603c4f9922d0c332d4d4bc0573" => :mojave
+    sha256 "e02cebee44a63afba05378546b86c58be59252dbd6acb99dd4a34aabeb19d861" => :high_sierra
   end
 
   depends_on "rust" => :build
