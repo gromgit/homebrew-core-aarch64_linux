30090db55fb2b842bf957bc06285bbfb0afe7392
Bo Anderson <mail@boanderson.me>
2020-04-24 21:14:02 +0100

    nift: update 2.3.5 bottle.

--- Formula/nift.rb
+++ Formula/nift.rb
@@ -6,9 +6,9 @@ class Nift < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e7e7aa6af70b62b847d620080276b4b835c2b70f8ffed91a2af6a2dd7f01a62c" => :catalina
-    sha256 "07614528c96c910169477165a83dd3d8fb5db747df3918897c5eb9377347e16d" => :mojave
-    sha256 "8a4fca1c254e90bfb5ff499e5c7d333a79049a29e00b09140219246e458f735a" => :high_sierra
+    sha256 "59c78ce930e5fd55149cd8ad8fe6b2ea57364d6ae038db4e08d4242e49847ff5" => :catalina
+    sha256 "54054a65d96046bde9d901a9f140e96752f0a3c7f14e73e05e90e40cc906283a" => :mojave
+    sha256 "1d314159a7133ab707da24005fb2a9edb81bc05d5b8b9649c8a16909b0fd0ac0" => :high_sierra
   end
 
   def install
