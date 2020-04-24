bcee84697e57892aaa7bb0b6d994d7669f683723
Zach Auten <zachauten@gmail.com>
2020-04-23 20:24:59 -0400

    libdap: update 3.20.6 bottle.

--- Formula/libdap.rb
+++ Formula/libdap.rb
@@ -5,9 +5,9 @@ class Libdap < Formula
   sha256 "35cc7f952d72de4936103e8a95c67ac8f9b855c9211fae73ad065331515cc54a"
 
   bottle do
-    sha256 "41ca77910281671985b9e698d1f6be7fd4943e0b32bf889e39f16ccbb36ab6bc" => :catalina
-    sha256 "6aaeaf8fd4959e3cddbb1486d4f1787a6ad8870ad314850311a2c1acedb5382c" => :mojave
-    sha256 "7891d895c5a30518351651f6963a6996287c185703487ea6f1ad71de51eb383e" => :high_sierra
+    sha256 "6a0bbd25fd0b5e873d34a46045c6ba72161007b9937d7957790bfc16bf5b05c3" => :catalina
+    sha256 "ce373bf6fbe4f5b28825fcf243633ae7a807d35b1627e985cc231bc722010793" => :mojave
+    sha256 "fbaa33ce89105a9cab0a7d8b22755524a93f39fb2a8e6d3c5a0459c2ded3bdf5" => :high_sierra
   end
 
   head do
