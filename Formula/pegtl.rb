b1be776ebe9809b4f35f22049ec02654c7d97e9b
Bo Anderson <mail@boanderson.me>
2020-04-24 17:11:48 +0000

    pegtl: add 2.8.3 bottle.

--- Formula/pegtl.rb
+++ Formula/pegtl.rb
@@ -4,6 +4,13 @@ class Pegtl < Formula
   url "https://github.com/taocpp/PEGTL/archive/2.8.3.tar.gz"
   sha256 "88b8e4ded6ea1f3f2223cc3e37072e2db1e123b90d36c309816341ae9d966723"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "10ad60dbc5a6c4054579b2e7529dfb053395cfd2c9bc5ddb5c2bf6b939b8f9cd" => :catalina
+    sha256 "10ad60dbc5a6c4054579b2e7529dfb053395cfd2c9bc5ddb5c2bf6b939b8f9cd" => :mojave
+    sha256 "10ad60dbc5a6c4054579b2e7529dfb053395cfd2c9bc5ddb5c2bf6b939b8f9cd" => :high_sierra
+  end
+
   depends_on "cmake" => :build
 
   def install
