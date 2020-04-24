c3635eeeb0cf9f21b7ce7a60b326fb1260e07e02
Bo Anderson <mail@boanderson.me>
2020-04-24 02:47:27 +0100

    inxi 3.1.00 (#53579)

--- Formula/inxi.rb
+++ Formula/inxi.rb
@@ -1,20 +1,19 @@
 class Inxi < Formula
   desc "Full featured CLI system information tool"
   homepage "https://smxi.org/docs/inxi.htm"
-  url "https://github.com/smxi/inxi/archive/3.0.38-1.tar.gz"
-  version "3.0.38"
-  sha256 "5adbbe8145e970de1d516a404554e887806d31382de81d290e71fe270183c28a"
-
+  url "https://github.com/smxi/inxi/archive/3.1.00-1.tar.gz"
+  version "3.1.00"
+  sha256 "ee6675489467cbfb90756db9aaf55da4bc5f7506cc8166d3a42ba28963e810f1"
   head "https://github.com/smxi/inxi.git"
 
   bottle :unneeded
 
   def install
-    bin.install("inxi")
-    man1.install("inxi.1")
+    bin.install "inxi"
+    man1.install "inxi.1"
 
     ["LICENSE.txt", "README.txt", "inxi.changelog"].each do |file|
-      prefix.install(file)
+      prefix.install file
     end
   end
 
