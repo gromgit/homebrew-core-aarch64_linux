0664c164b941202f15650f7ae4b22719e6f01459
Zach Auten <zachauten@gmail.com>
2020-04-23 20:24:41 -0400

    msmtp: update 1.8.10 bottle.

--- Formula/msmtp.rb
+++ Formula/msmtp.rb
@@ -5,9 +5,9 @@ class Msmtp < Formula
   sha256 "caba7f39d19df7a31782fe7336dd640c61ea33b92f987bd5423bca9683482f10"
 
   bottle do
-    sha256 "e742b036265028d00689e3476db274f47596ee6be299cb3f2f3f046d23d93741" => :catalina
-    sha256 "902355fbdaa19cb16474403ed09babfdbf3072792ec525d43a9ba6fe719c761d" => :mojave
-    sha256 "eab65ceebd178110b7857df4b6611b4ed8d7be0e030dbb3bd43a92502170d5a7" => :high_sierra
+    sha256 "ae5634512b5c69936c1755b182d8eafccca28005d28f4f7d8b41d074f64da66f" => :catalina
+    sha256 "813d8e19745ad1642d69203d1d977ab124bf32e612220834a04ddd5086219308" => :mojave
+    sha256 "d1d3b8b75826e109a521575b4b09d6690d34f73f488b8cca1c34f4c33c97b582" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
