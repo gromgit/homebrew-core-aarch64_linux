79b4c37a871cd64f3d737260ec22c51e424431cc
Bo Anderson <mail@boanderson.me>
2020-04-24 16:53:08 +0000

    capnp: update 0.8.0 bottle.

--- Formula/capnp.rb
+++ Formula/capnp.rb
@@ -7,11 +7,9 @@ class Capnp < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b1516e75ca0fa771b0a8e0e8a0405784b9f4d8311413645be79a636e826e2a9e" => :catalina
-    sha256 "86b0a4f0bbc70a1bc04cbdd1b2312974e2acd56e606a2865e1dfbc48f07c2d1e" => :mojave
-    sha256 "a4720c5dc1b0866536b4649a87e16149d29cdaa730ced45acd7e557918dc5285" => :high_sierra
-    sha256 "56c4c541de5388071f53d582a12b7d0672c476ecf15130122d527bde2af4f358" => :sierra
-    sha256 "c828367f66d7b83289de33b8b3d47cc32dcc1b8da555469bdf886f2a7febdf2b" => :el_capitan
+    sha256 "741c2079361cdb5881a60684190bc4aa98ff9cc6f8d29aa46880e809ac1b06c3" => :catalina
+    sha256 "f389012b8211b70af4fa7d2eed8db8ad399ef2bdc98e286fb57a4b1beb93dfe4" => :mojave
+    sha256 "9c3beb8d8db3b372e4d2fd07d99a553fde6ff53824c6cfec82c3db41e212bc5b" => :high_sierra
   end
 
   depends_on "cmake" => :build
