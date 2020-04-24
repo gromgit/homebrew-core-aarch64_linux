07dd5080fac213b89c43b47fbf1641573a3a1ffb
Bo Anderson <mail@boanderson.me>
2020-04-24 12:53:07 +0100

    pegtl 2.8.3
    Closes #53629.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/pegtl.rb
+++ Formula/pegtl.rb
@@ -1,13 +1,18 @@
 class Pegtl < Formula
   desc "Parsing Expression Grammar Template Library"
   homepage "https://github.com/taocpp/PEGTL"
-  url "https://github.com/taocpp/PEGTL/archive/2.8.2.tar.gz"
-  sha256 "1d7db20502125a75fbb583c7020038d4faeb6812de504147b23b3db4ef329ca3"
+  url "https://github.com/taocpp/PEGTL/archive/2.8.3.tar.gz"
+  sha256 "88b8e4ded6ea1f3f2223cc3e37072e2db1e123b90d36c309816341ae9d966723"
 
-  bottle :unneeded
+  depends_on "cmake" => :build
 
   def install
-    prefix.install "include"
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args,
+                            "-DPEGTL_BUILD_TESTS=OFF",
+                            "-DPEGTL_BUILD_EXAMPLES=OFF"
+      system "make", "install"
+    end
     rm "src/example/pegtl/CMakeLists.txt"
     (pkgshare/"examples").install (buildpath/"src/example/pegtl").children
   end
