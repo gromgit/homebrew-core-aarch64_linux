8bff08f9433dced82a0af8c4e29dccaa94c21ab6
Bo Anderson <mail@boanderson.me>
2020-04-24 13:05:03 +0100

    tika 1.24.1
    Closes #53632.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/tika.rb
+++ Formula/tika.rb
@@ -1,17 +1,18 @@
 class Tika < Formula
   desc "Content analysis toolkit"
   homepage "https://tika.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tika/tika-app-1.24.jar"
-  mirror "https://archive.apache.org/dist/tika/tika-app-1.24.jar"
-  sha256 "540dd9395d313e82cfb6ce9ba4f001220dcb962aea587f8ca220ac5be39db530"
+  url "https://www.apache.org/dyn/closer.lua?path=tika/tika-app-1.24.1.jar"
+  mirror "https://archive.apache.org/dist/tika/tika-app-1.24.1.jar"
+  sha256 "e56d2e38be4755c78b511f316bda2a55af5c3b3b36e7e5536d3584c71239b187"
 
   bottle :unneeded
 
   depends_on "openjdk"
 
   resource "server" do
-    url "https://www.apache.org/dyn/closer.lua?path=tika/tika-server-1.24.jar"
-    sha256 "45f52079536386e5344e75ae77e7c70d92e28bd6a3229758df695b56d9200085"
+    url "https://www.apache.org/dyn/closer.lua?path=tika/tika-server-1.24.1.jar"
+    mirror "https://archive.apache.org/dist/tika/tika-server-1.24.1.jar"
+    sha256 "466ae64b3f6fa52fe08bfa2b0339671e69988e84fd8bb0a359d345ff0ae024a3"
   end
 
   def install
