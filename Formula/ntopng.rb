e78607e4d7da120c8ce49a7272938a859927d366
EricFromCanada <enk3@outlook.com>
2020-04-24 19:36:33 -0400

    ntopng: revision for json-c

--- Formula/ntopng.rb
+++ Formula/ntopng.rb
@@ -2,6 +2,7 @@ class Ntopng < Formula
   desc "Next generation version of the original ntop"
   homepage "https://www.ntop.org/products/traffic-analysis/ntop/"
   license "GPL-3.0"
+  revision 1
 
   stable do
     url "https://github.com/ntop/ntopng/archive/4.0.tar.gz"
