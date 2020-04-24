829b8e5a28c342e6782178462e91e39f88d1fc37
Bo Anderson <mail@boanderson.me>
2020-04-24 18:58:34 +0100

    bash 5.0.17
    Closes #53646.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/bash.rb
+++ Formula/bash.rb
@@ -9,7 +9,7 @@ class Bash < Formula
     mirror "https://mirrors.kernel.org/gnu/bash/bash-5.0.tar.gz"
     mirror "https://mirrors.ocf.berkeley.edu/gnu/bash/bash-5.0.tar.gz"
     sha256 "b4a80f2ac66170b2913efbfb9f2594f1f76c7b1afd11f799e22035d63077fb4d"
-    version "5.0.16"
+    version "5.0.17"
 
     %w[
       001 f2fe9e1f0faddf14ab9bfa88d450a75e5d028fedafad23b88716bd657c737289
@@ -28,6 +28,7 @@ class Bash < Formula
       014 5d6eee6514ee6e22a87bba8d22be0a8621a0ae119246f1c5a9a35db1f72af589
       015 a517df2dda93b26d5cbf00effefea93e3a4ccd6652f152f4109170544ebfa05e
       016 ffd1d7a54a99fa7f5b1825e4f7e95d8c8876bc2ca151f150e751d429c650b06d
+      017 4cf3b9fafb8a66d411dd5fc9120032533a4012df1dc6ee024c7833373e2ddc31
     ].each_slice(2) do |p, checksum|
       patch :p0 do
         url "https://ftp.gnu.org/gnu/bash/bash-5.0-patches/bash50-#{p}"
