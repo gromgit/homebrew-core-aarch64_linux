0bb74cd58af38fbc2fffcf04cd82c3ae17e98c8a
Bo Anderson <mail@boanderson.me>
2020-04-24 17:05:17 +0000

    uchardet: update 0.0.7 bottle.

--- Formula/uchardet.rb
+++ Formula/uchardet.rb
@@ -7,13 +7,9 @@ class Uchardet < Formula
 
   bottle do
     cellar :any
-    sha256 "83fd5ae3ce1597de3b2847f979fbc92fc5b95489c48637efcb3afab5620d2426" => :catalina
-    sha256 "d276b3607abe2ed5e2922cda3572d09192c60f13bf18d6d84cee24c12d700bcf" => :mojave
-    sha256 "c0416fb559e8c10f1cca21a0b0162d462cc02019d8abbce3712f64261b1ce8fe" => :high_sierra
-    sha256 "dce2d199e163858a10f27f9d94d232b8df5d38507098b629356ee5154d4f182c" => :sierra
-    sha256 "998232b6d034c090680202ca6d48a9af4924f091f3b597e4aa318f87fdf29bb8" => :el_capitan
-    sha256 "ab930a4e2c217362dc7e05940cc6449d024f18c5014847ff9428facef02316c7" => :yosemite
-    sha256 "c02f20920ac97596ab425b057275372a77c80c7d523191f2e5ab78c636d6827f" => :mavericks
+    sha256 "34bd4791834a762da961de136a3cad253800a98deea2d1dededf8efb465f215d" => :catalina
+    sha256 "a254139ee777de77d5907dfeb47d4306f2e5e5f0b3775edfedcbf1fcf217fdbe" => :mojave
+    sha256 "a89237dd88e4190826ce6fb4a8a625711636db7d6c4ba10d5ddd56d37347e868" => :high_sierra
   end
 
   depends_on "cmake" => :build
