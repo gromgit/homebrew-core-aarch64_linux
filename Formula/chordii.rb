966e40e40f9e15c9f04b98559c3ec9d6403e4e59
Bo Anderson <mail@boanderson.me>
2020-04-24 19:03:09 +0100

    chordii 4.5.3b
    Closes #53647.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/chordii.rb
+++ Formula/chordii.rb
@@ -1,8 +1,8 @@
 class Chordii < Formula
   desc "Text file to music sheet converter"
   homepage "https://www.vromans.org/johan/projects/Chordii/"
-  url "https://downloads.sourceforge.net/project/chordii/chordii/4.5/chordii-4.5.3.tar.gz"
-  sha256 "140d24a8bc8c298e8db1b9ca04cf02890951aa048a656adb7ee7212c42ce8d06"
+  url "https://downloads.sourceforge.net/project/chordii/chordii/4.5/chordii-4.5.3b.tar.gz"
+  sha256 "edb19be9de456366e592a75a5ce1c0a75352a55d5b4e5f282c953c7e7f2d87b5"
 
   bottle do
     cellar :any_skip_relocation
