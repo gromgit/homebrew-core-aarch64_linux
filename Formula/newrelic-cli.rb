a01a02a4c81a9b2fdc082a748cfac8703872c54e
Jonathan Thurman <jthurman@newrelic.com>
2020-04-24 11:50:12 -0700

    newrelic-cli 0.8.3
    Closes #53651.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/newrelic-cli.rb
+++ Formula/newrelic-cli.rb
@@ -1,8 +1,8 @@
 class NewrelicCli < Formula
   desc "The New Relic Command-line Interface"
   homepage "https://github.com/newrelic/newrelic-cli"
-  url "https://github.com/newrelic/newrelic-cli/archive/v0.6.2.tar.gz"
-  sha256 "603bf5b23e8f986085596b0728503eaa46d41cb31d1c3ac9f1988ac7d48219ec"
+  url "https://github.com/newrelic/newrelic-cli/archive/v0.8.3.tar.gz"
+  sha256 "d0e22d9f88180051b6cb0caf7cdab9faaf7fca28a96c8ce0a320d0a7c340071d"
   head "https://github.com/newrelic/newrelic-cli.git"
 
   bottle do
