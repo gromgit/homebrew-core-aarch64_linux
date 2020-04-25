d076e84ce2271d20c8334bd36e1554403c14ecbf
Bo Anderson <mail@boanderson.me>
2020-04-25 16:46:37 +0000

    httpflow: update 0.0.8 bottle.

--- Formula/httpflow.rb
+++ Formula/httpflow.rb
@@ -7,12 +7,9 @@ class Httpflow < Formula
 
   bottle do
     cellar :any
-    sha256 "6e2dae55b26343cf1e384ab67e22d70c19162f09d28c96f02dcad2b96a70de17" => :catalina
-    sha256 "602b0986019d2d669e91ccd4d43411b6c8ad52ae44feaaac6f19f57c5267a7f9" => :mojave
-    sha256 "81bacec65a65a4f97be8035f1d21407ddada8bf3e8ab2dea8163f42162af6d65" => :high_sierra
-    sha256 "02f297152a11c184e4b16e3881d416f9cd388ecab8a4979a02bf0bbf6f2c4f56" => :sierra
-    sha256 "aae82cdbdfbda4766e7ac39ac7cf0d588ae05379b6de60fe9a62521eaf097f61" => :el_capitan
-    sha256 "f1f6b80b4d0ae25da4bc0b626ce3da653ad48f55c4d9e82c3e60ec51ff6ddea4" => :yosemite
+    sha256 "b9e4d694b806f3a4201ac691b680a9cbd21a465de018ba28aa944e9ca869d848" => :catalina
+    sha256 "7284170097a075c1831a5ae8cf3ad128f4a3352392d57b65ce5b375f2324449b" => :mojave
+    sha256 "4d369eb896c76cd1895e6f363779499387bae50053c36445adafbd23ba5f722a" => :high_sierra
   end
 
   depends_on "pcre"
