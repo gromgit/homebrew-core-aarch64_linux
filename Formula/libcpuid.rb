25ebb4fe155c7b3fabcbbcaa81226e228be54708
Caleb Xu <calebcenter@live.com>
2020-04-25 02:58:53 -0400

    libcpuid 0.4.1 (new formula)
    Closes #53662.
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

---
+++ Formula/libcpuid.rb
@@ -0,0 +1,27 @@
+class Libcpuid < Formula
+  desc "Small C library for x86 CPU detection and feature extraction"
+  homepage "https://github.com/anrieff/libcpuid"
+  url "https://github.com/anrieff/libcpuid/archive/v0.4.1.tar.gz"
+  sha256 "263b370d154d55e3b7246e069600b045d27c512456f051e9ce3d999318b58b61"
+  head "https://github.com/anrieff/libcpuid.git"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
+  def install
+    system "autoreconf", "-ivf"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"cpuid_tool"
+    assert_predicate testpath/"raw.txt", :exist?
+    assert_predicate testpath/"report.txt", :exist?
+    assert_match "CPUID is present", File.read(testpath/"report.txt")
+  end
+end
