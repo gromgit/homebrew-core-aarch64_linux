5311af8657b0bb141c2b74af5242ac55ef46d885
Bo Anderson <mail@boanderson.me>
2020-04-25 22:39:42 +0100

    hive: fix runtime error with latest hadoop (#53675)

--- Formula/hive.rb
+++ Formula/hive.rb
@@ -4,15 +4,22 @@ class Hive < Formula
   url "https://www.apache.org/dyn/closer.lua?path=hive/hive-3.1.2/apache-hive-3.1.2-bin.tar.gz"
   mirror "https://archive.apache.org/dist/hive/hive-3.1.2/apache-hive-3.1.2-bin.tar.gz"
   sha256 "d75dcf36908b4e7b9b0ec9aec57a46a6628b97b276c233cb2c2f1a3e89b13462"
+  revision 1
 
   bottle :unneeded
 
   depends_on "hadoop"
-  depends_on :java => "1.7+"
+  depends_on :java => "1.8"
 
   def install
     rm_f Dir["bin/*.cmd", "bin/ext/*.cmd", "bin/ext/util/*.cmd"]
     libexec.install %w[bin conf examples hcatalog lib scripts]
+
+    # Hadoop currently supplies a newer version
+    # and two versions on the classpath causes problems
+    rm libexec/"lib/guava-19.0.jar"
+    guava = (Formula["hadoop"].opt_libexec/"share/hadoop/common/lib").glob("guava-*-jre.jar")
+    ln_s guava.first, libexec/"lib"
 
     Pathname.glob("#{libexec}/bin/*") do |file|
       next if file.directory?
