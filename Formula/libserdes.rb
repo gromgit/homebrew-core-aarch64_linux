18e5eb6b760b5f73c0cb40b7a1cb017b6d2d12c1
Rui Chen <chenrui333@gmail.com>
2020-04-25 11:50:10 -0400

    libserdes 5.5.0
    Closes #53705.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/libserdes.rb
+++ Formula/libserdes.rb
@@ -2,7 +2,7 @@ class Libserdes < Formula
   desc "Schema ser/deserializer lib for Avro + Confluent Schema Registry"
   homepage "https://github.com/confluentinc/libserdes"
   url "https://github.com/confluentinc/libserdes.git",
-    :tag      => "v5.4.1",
+    :tag      => "v5.5.0",
     :revision => "b259d15f68dce65591700b0ccccb73311db1de3d"
   head "https://github.com/confluentinc/libserdes.git"
 
