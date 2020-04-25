b19a03fd4864512b2dd4fa16d66e653482bbe8c1
Rui Chen <chenrui333@gmail.com>
2020-04-25 12:16:17 -0400

    netlify-cli 2.48.0
    Closes #53712.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/netlify-cli.rb
+++ Formula/netlify-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class NetlifyCli < Formula
   desc "Netlify command-line tool"
   homepage "https://www.netlify.com/docs/cli"
-  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-2.47.0.tgz"
-  sha256 "a11988f6a67c20df053f7c996de493de921e6f0320bde7046536a5af69e3d276"
+  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-2.48.0.tgz"
+  sha256 "3fcc90051023aa9b322901c4fb0099f521df8ef9502b8659bdd29f76116952a6"
   head "https://github.com/netlify/cli.git"
 
   bottle do
