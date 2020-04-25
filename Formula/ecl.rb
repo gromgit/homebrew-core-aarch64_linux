df0eab133188be61aecc6a57b06fd921f81d1015
Bo Anderson <mail@boanderson.me>
2020-04-25 11:12:10 +0100

    ecl 20.4.24
    Closes #53664.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/ecl.rb
+++ Formula/ecl.rb
@@ -1,9 +1,8 @@
 class Ecl < Formula
   desc "Embeddable Common Lisp"
   homepage "https://common-lisp.net/project/ecl/"
-  url "https://common-lisp.net/project/ecl/static/files/release/ecl-16.1.3.tgz"
-  sha256 "76a585c616e8fa83a6b7209325a309da5bc0ca68e0658f396f49955638111254"
-  revision 4
+  url "https://common-lisp.net/project/ecl/static/files/release/ecl-20.4.24.tgz"
+  sha256 "670838edf258a936b522fdb620da336de7e575aa0d27e34841727252726d0f07"
   head "https://gitlab.com/embeddable-common-lisp/ecl.git"
 
   bottle do
@@ -12,6 +11,7 @@ class Ecl < Formula
     sha256 "315810b954020ffde49b5386e789d49640f4a3018fac98e8f2f76aab8b3b0258" => :high_sierra
   end
 
+  depends_on "texinfo" => :build # Apple's is too old
   depends_on "bdw-gc"
   depends_on "gmp"
   depends_on "libffi"
