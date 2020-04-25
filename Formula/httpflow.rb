9ebc231c37af974bb631c86d9915d71ce39f9f7b
Rui Chen <chenrui333@gmail.com>
2020-04-25 11:32:27 -0400

    httpflow 0.0.8
    Closes #53700.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/httpflow.rb
+++ Formula/httpflow.rb
@@ -1,8 +1,8 @@
 class Httpflow < Formula
   desc "Packet capture and analysis utility similar to tcpdump for HTTP"
   homepage "https://github.com/six-ddc/httpflow"
-  url "https://github.com/six-ddc/httpflow/archive/0.0.7.tar.gz"
-  sha256 "9bcc7bc00427a4b01c5a5178113780f2321fbe2f81857c915a420df3a6b41de2"
+  url "https://github.com/six-ddc/httpflow/archive/0.0.8.tar.gz"
+  sha256 "e2d4f233460a5a52cf64fb15adc574f521a85d0898f7d6306d3ad8ddd56a1493"
   head "https://github.com/six-ddc/httpflow.git"
 
   bottle do
