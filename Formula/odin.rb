334e30f7f7130e353879d6c752ecb3c88f86522c
Alexander Bayandin <a.bayandin@gmail.com>
2020-04-25 14:27:19 +0000

    odin: add 0.12.0 bottle.

--- Formula/odin.rb
+++ Formula/odin.rb
@@ -5,6 +5,13 @@ class Odin < Formula
   sha256 "8356124c8cc7e08ac39872e5bb10593a412e67f81df621124097facd9b2b26cc"
   head "https://github.com/odin-lang/Odin.git"
 
+  bottle do
+    cellar :any
+    sha256 "742f1adfd7a9ff880ad2fb80759e3a239530f6774bcf77ca5b5f650e88b3ee31" => :catalina
+    sha256 "80b0b0d73769b90551a5e759d82b41e16af6993540936dc2e94363e46ac1b0c1" => :mojave
+    sha256 "579b027ea67fcb8d0095f2a49e96a6660c903474bb145f930406caea8cc15ae7" => :high_sierra
+  end
+
   depends_on "llvm"
 
   uses_from_macos "libiconv"
