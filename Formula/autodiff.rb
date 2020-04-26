fb0aaf57c700de80eb3283983dd4b4367caa5b46
Bo Anderson <mail@boanderson.me>
2020-04-26 23:01:37 +0000

    autodiff: add 0.5.10 bottle.

--- Formula/autodiff.rb
+++ Formula/autodiff.rb
@@ -5,6 +5,13 @@ class Autodiff < Formula
   sha256 "d0e62994b7984014b2944d13f6ce9a75fc6b681d2d81f9051ec44410912dc5d7"
   head "https://github.com/autodiff/autodiff.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0be7da5b5c8cb88296037137126dc4530f23559509b6a93d33485db8ae74446c" => :catalina
+    sha256 "a8a8c129b51be0a90a7f683b52010d42a56439c869beeac44ed77e16a8d406b0" => :mojave
+    sha256 "464905e7e857c916c85a3c60757ae1ab21834e0187161185043fd52c29fa7fb7" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "eigen"
 
