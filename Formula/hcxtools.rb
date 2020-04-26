68fb3b52bb8ce059fc5d1e2fbb6b0dd596b19754
Bo Anderson <mail@boanderson.me>
2020-04-26 21:58:26 +0000

    hcxtools: add 6.0.2 bottle.

--- Formula/hcxtools.rb
+++ Formula/hcxtools.rb
@@ -5,6 +5,13 @@ class Hcxtools < Formula
   sha256 "963029bf00d3a98f2caa934d5765a91e29f5d1b0e0036a4c53328b5cf9c3c003"
   head "https://github.com/ZerBea/hcxtools.git"
 
+  bottle do
+    cellar :any
+    sha256 "2cc7082a49f45dcdbd368044a16cc569ec208fbd4efbf5a419e3adac9cd8c0ca" => :catalina
+    sha256 "1d5c0d72c0780e8cde71e56b841ea6494f053f71f5dad5092f526868c5ae230c" => :mojave
+    sha256 "d53de9bf0fdeb662804bf9bcdfdbd3d6fca89980e3454664acc7106212164fc1" => :high_sierra
+  end
+
   depends_on "openssl@1.1"
 
   def install
