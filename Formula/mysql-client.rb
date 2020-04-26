115ed714f7154fd84faf17654a6063428b5c83ac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-04-26 17:06:01 +0000

    mysql-client: update 8.0.19 bottle.

--- Formula/mysql-client.rb
+++ Formula/mysql-client.rb
@@ -5,9 +5,9 @@ class MysqlClient < Formula
   sha256 "3622d2a53236ed9ca62de0616a7e80fd477a9a3f862ba09d503da188f53ca523"
 
   bottle do
-    sha256 "6aeb496213d40f45bc44123703753012ea889468b7f37101a42772da237ab4ad" => :catalina
-    sha256 "9729c80a49ac7b808bbc26a93130c3ea16797da3824ed9450df53974958de066" => :mojave
-    sha256 "98ae3bb78d4044053cba6acc3cd3e4737cc71df2cd2afb68e0cbccfd27488de7" => :high_sierra
+    sha256 "43a72c20ae7b146f6dc7d01b9f63bef605972c26ecf2970de841f4ecd02c4b9e" => :catalina
+    sha256 "c8d2434da809237c64cd3dd4139a2bd148b6773fca040c001cc7af468354e761" => :mojave
+    sha256 "cb1fdc14848bdeaf8c3e4ebb1550cabd794771e7f7918be1716e08b43e8889b9" => :high_sierra
   end
 
   keg_only "it conflicts with mysql (which contains client libraries)"
