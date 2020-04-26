46911ee4020214a22481a9ce4c5f6fe5a5cead61
Pavel Zhovner <pavel@zhovner.com>
2020-04-26 19:24:15 +0300

    hcxtools 6.0.2 (new formula)
    Closes #53793.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

---
+++ Formula/hcxtools.rb
@@ -0,0 +1,33 @@
+class Hcxtools < Formula
+  desc "Utils for conversion of cap/pcap/pcapng WiFi dump files"
+  homepage "https://github.com/ZerBea/hcxtools"
+  url "https://github.com/ZerBea/hcxtools/archive/6.0.2.tar.gz"
+  sha256 "963029bf00d3a98f2caa934d5765a91e29f5d1b0e0036a4c53328b5cf9c3c003"
+  head "https://github.com/ZerBea/hcxtools.git"
+
+  depends_on "openssl@1.1"
+
+  def install
+    bin.mkpath
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    # Create file with 22000 hash line
+    testhash = testpath/"test.22000"
+    (testpath/"test.22000").write <<~EOS
+      WPA*01*4d4fe7aac3a2cecab195321ceb99a7d0*fc690c158264*f4747f87f9f4*686173686361742d6573736964***
+    EOS
+
+    # Convert hash to .cap file
+    testcap = testpath/"test.cap"
+    system "#{bin}/hcxhash2cap", "--pmkid-eapol=#{testhash}", "-c", testpath/"test.cap"
+
+    # Convert .cap file back to hash file
+    newhash = testpath/"new.22000"
+    system "#{bin}/hcxpcapngtool", "-o", newhash, testcap
+
+    # Diff old and new hash file to check if they are identical
+    system "diff", newhash, testhash
+  end
+end
