1bb855851895998f7039cd55584b268b1d220c08
Bo Anderson <mail@boanderson.me>
2020-04-26 11:01:43 +0100

    bats-core 1.2.0
    Closes #53758.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bats-core.rb
+++ Formula/bats-core.rb
@@ -1,10 +1,12 @@
 class BatsCore < Formula
   desc "Bash Automated Testing System"
   homepage "https://github.com/bats-core/bats-core"
-  url "https://github.com/bats-core/bats-core/archive/v1.1.0.tar.gz"
-  sha256 "855d8b8bed466bc505e61123d12885500ef6fcdb317ace1b668087364717ea82"
+  url "https://github.com/bats-core/bats-core/archive/v1.2.0.tar.gz"
+  sha256 "c74ea5517d9381c044e8e87427de0113be6ad591213d489aeaa28146daf1c723"
 
   bottle :unneeded
+
+  depends_on "coreutils"
 
   conflicts_with "bats", :because => "both install `bats` executables"
 
