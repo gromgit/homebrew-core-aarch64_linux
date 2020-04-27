1079ff5df8621baa6ff97344394c404a7c129ef6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-04-27 06:08:48 +0000

    khal: update 0.10.1_1 bottle.

--- Formula/khal.rb
+++ Formula/khal.rb
@@ -11,10 +11,9 @@ class Khal < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7a5ff88f863f07170a7ac8244c7bbec213690ed09b5b6d47a0d38f757230f790" => :catalina
-    sha256 "65c0e242110629b9aba6fc3fb7dcdd0b48c256465cbf8e4bdce6eca10ef791b0" => :mojave
-    sha256 "e64129f4743de673ab7ecadf45943cf920e1f263a943e13f4bb2c138cc542855" => :high_sierra
-    sha256 "f3fc8e90d8d5e5f452a56954563d2c5e40f17cbda7f1552ad9ad2bd5e8bda30a" => :sierra
+    sha256 "6d310786c31a7cdb4529ec2a28b094de5f458961ecd3b4edaa16049474910fa5" => :catalina
+    sha256 "8c5773a8e532deee750356a65b1714a02ae5c974b4159263e873f0f70efda0ba" => :mojave
+    sha256 "954d7aee32da8561dc770023c0bec4ac914eed5fb31d4fcbf22208cba426bf85" => :high_sierra
   end
 
   depends_on "python@3.8"
