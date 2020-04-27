5c315cb438407c4aee7b30fe50359023c81b135a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-04-27 06:08:46 +0000

    honcho: update 1.0.1_2 bottle.

--- Formula/honcho.rb
+++ Formula/honcho.rb
@@ -7,10 +7,9 @@ class Honcho < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "37a860eebf914c1fd376d28d1199172c78c303b3386388e573f1824e50644045" => :catalina
-    sha256 "8bc883894e8b6f07ab526e862500f40b7c47101a0f0ee66540cc5451b00b807e" => :mojave
-    sha256 "41492a0296ea50b9b93f83e11d2b50bf7bc2a1361cf1da82bf61023f04f1e782" => :high_sierra
-    sha256 "41492a0296ea50b9b93f83e11d2b50bf7bc2a1361cf1da82bf61023f04f1e782" => :sierra
+    sha256 "986c98221b9bb025b0c8fa8c1f4ca150ee1853488f6060b603c54aa7e02c8be1" => :catalina
+    sha256 "986c98221b9bb025b0c8fa8c1f4ca150ee1853488f6060b603c54aa7e02c8be1" => :mojave
+    sha256 "986c98221b9bb025b0c8fa8c1f4ca150ee1853488f6060b603c54aa7e02c8be1" => :high_sierra
   end
 
   depends_on "python@3.8"
