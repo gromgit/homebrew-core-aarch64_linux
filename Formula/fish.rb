ba4ce271878442ebbcbd0ba1dac35b01cdff3848
David Adam <zanchey@ucc.gu.uwa.edu.au>
2020-04-27 22:28:46 +0800

    fish 3.1.1
    Closes #53834.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/fish.rb
+++ Formula/fish.rb
@@ -1,9 +1,8 @@
 class Fish < Formula
   desc "User-friendly command-line shell for UNIX-like operating systems"
   homepage "https://fishshell.com"
-  url "https://github.com/fish-shell/fish-shell/releases/download/3.1.0/fish-3.1.0.tar.gz"
-  sha256 "e5db1e6839685c56f172e1000c138e290add4aa521f187df4cd79d4eab294368"
-  revision 1
+  url "https://github.com/fish-shell/fish-shell/releases/download/3.1.1/fish-3.1.1.tar.gz"
+  sha256 "07dc78eea3bc4cbd490b2f2a2e19e5771ac9e3b6b1a75893039ad8b34d6122b8"
 
   bottle do
     cellar :any
@@ -22,16 +21,6 @@ class Fish < Formula
   depends_on "pcre2"
 
   uses_from_macos "ncurses"
-
-  # Fixes severe performance issues with one of the default prompt
-  # integrations. This has already been applied upstream and will
-  # be in the next release.
-  unless build.head?
-    patch do
-      url "https://github.com/Homebrew/formula-patches/raw/8743c955ae8809f692c92ef6b4bc78595bf98f50/fish/disable_svn_prompt.patch"
-      sha256 "953dfc21f45575022d8f47c8654da1908682de1711712a60d4220e3a4c8133b9"
-    end
-  end
 
   def install
     # In Homebrew's 'superenv' sed's path will be incompatible, so
