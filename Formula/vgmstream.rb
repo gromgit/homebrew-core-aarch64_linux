4f39da8cb39232c7fe6bc65869603507132cd90d
Bo Anderson <mail@boanderson.me>
2020-04-28 12:40:14 +0000

    vgmstream: update r1050-2908-g14dc8566 bottle.

--- Formula/vgmstream.rb
+++ Formula/vgmstream.rb
@@ -8,10 +8,9 @@ class Vgmstream < Formula
 
   bottle do
     cellar :any
-    sha256 "e999d79d61402e4f297b49d0b634d01bc5ff10ff27f2bb61c9859dacca104ef9" => :catalina
-    sha256 "d180afb14e4343b8820c189ea1e857157d81973683923c06e9f58dcfbeacc247" => :mojave
-    sha256 "d4a4e8c4652075288e44abfee2ffc56690a1f84b14e126a0dc0746123bb337ee" => :high_sierra
-    sha256 "c743fe265464bd875289a5ae0db662654587f2a9e29ef1b8851bc5312d951041" => :sierra
+    sha256 "27cb2df8b0a88d31507695a9b8711bbeca66ac9a09f8e34de399a45b7f2c216a" => :catalina
+    sha256 "8a52d5fdc81f87c709c0f41c90b02cba2788f7429402a327fcf343d331346018" => :mojave
+    sha256 "513761e04c72ecf6300222bd06d662b7ab0ae74b56f37cc586dcd514e5f2ffc3" => :high_sierra
   end
 
   depends_on "libao"
