78a31176483135f42289fbe194a592c5f8d3ffc2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-04-28 05:05:30 +0000

    cups: update 2.3.3 bottle.

--- Formula/cups.rb
+++ Formula/cups.rb
@@ -5,9 +5,9 @@ class Cups < Formula
   sha256 "261fd948bce8647b6d5cb2a1784f0c24cc52b5c4e827b71d726020bcc502f3ee"
 
   bottle do
-    sha256 "028330028dca9194605bdf6ec807b413adae82b1491bb69cbee64d31fc04a6f3" => :catalina
-    sha256 "4fd5c5705dfb551e9fd5091f63a69d985bae05bc0b29fb253c9877138b254e7b" => :mojave
-    sha256 "932ce69ebe900f3e307939ffb475a1d2d3f46d079b3c6b5238385051bdc110a1" => :high_sierra
+    sha256 "1bed38f7aa9ee2c76fed5393e308cf7de5dd6d443debb0455ee7739e010b0e20" => :catalina
+    sha256 "19ea09cef986e1c66f1736409f4e7917914100bb68525d8118e705500fbf28e0" => :mojave
+    sha256 "0d3328bddece686cfa54e63b61f68c68fed1b1f8553cd45ed194d8a80e56f9c9" => :high_sierra
   end
 
   keg_only :provided_by_macos
