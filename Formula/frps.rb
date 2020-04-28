2e9e426b5353e0ab50d9479765f89bc431e2a71c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-04-28 05:05:30 +0000

    frps: update 0.33.0 bottle.

--- Formula/frps.rb
+++ Formula/frps.rb
@@ -7,9 +7,9 @@ class Frps < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "fd32c875fb000ba39f0f8f7ddcd1dfb491386f3213d42612377cb973073aea66" => :catalina
-    sha256 "265cf6a6b5a0b6eb064d420dc767e000755cabcd74160eb54087168aec8542e5" => :mojave
-    sha256 "4fe4f1a8c06f5c1fdc1ddf3d4b650609a285b8da9ee621755e7e40c797dc8559" => :high_sierra
+    sha256 "b1f2f28f18b53ca6041b7e3b55156a26c131fe1c163a1a559ffa158836ab75af" => :catalina
+    sha256 "dfc09d5fb82cdb397f955ef6bc2ba9f2d7fa7b5f00cf2aae5ed7bd427f3f7fa6" => :mojave
+    sha256 "de264b527c16414a7ae39b8334c953b8d9464c27b8b1c4f1ffc8300f35d3e0af" => :high_sierra
   end
 
   depends_on "go" => :build
