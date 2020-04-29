3e5b97065de472e40f1ce1afaf78a2fd2df4a1e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-04-29 15:05:59 +0000

    fonttools: update 4.9.0 bottle.

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -9,9 +9,9 @@ class Fonttools < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d4ca3c0eee1bc93498a7ee79f252b3480f6e609b514076049ff4840eea4b10f7" => :catalina
-    sha256 "00b4e22d111ec55c0e316ac1ff4f45e98dcdf7a344cdbfc57ebadeff26f31310" => :mojave
-    sha256 "a5c01ccf14a250209d02bbbefa49c0be21ac7b8a9e85f70b8258a1716a44ceb4" => :high_sierra
+    sha256 "b7f128c3911235f8e914ee83661f7d04c8fc152ec7590dea8bc1ada9581cc02d" => :catalina
+    sha256 "1ba05c6a7d061805ac74621a1c186e723a3dd3c532cc2a5e18cd04445eb83163" => :mojave
+    sha256 "a455bd2ca61d13347c18197f10b8425ea87912b56485097774ce6ad37f51b036" => :high_sierra
   end
 
   depends_on "python@3.8"
