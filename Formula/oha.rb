42ae6a96a1b57ee149f9d6e4c548c8410cf04c56
chenrui <chenrui333@gmail.com>
2020-04-29 04:00:16 +0000

    oha: add 0.2.3 bottle.

--- Formula/oha.rb
+++ Formula/oha.rb
@@ -4,6 +4,13 @@ class Oha < Formula
   url "https://github.com/hatoo/oha/archive/v0.2.3.tar.gz"
   sha256 "491c8a3e4931be904eabd298e9ba9193508f976095b06dea1e179bb8071ee8bd"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "85a0f4985360b43b39463a14095092fb59e7301c9f6e9c2c2a3016f59e88ebe0" => :catalina
+    sha256 "7d75f4966c217216899834adc162cd4820c457ba211402c2f97100c1040d368f" => :mojave
+    sha256 "3c06fe21b4b14862896a1648d05145275a79cb62ce7a4716b634262c85aefcc2" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
