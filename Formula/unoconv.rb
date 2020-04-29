c8ecb2be253a19f25e83d7039f75ab555112e9bd
Caleb Xu <calebcenter@live.com>
2020-04-29 17:20:34 -0400

    unoconv: use python@3.8

--- Formula/unoconv.rb
+++ Formula/unoconv.rb
@@ -3,6 +3,7 @@ class Unoconv < Formula
   homepage "http://dag.wiee.rs/home-made/unoconv/"
   url "https://files.pythonhosted.org/packages/ab/40/b4cab1140087f3f07b2f6d7cb9ca1c14b9bdbb525d2d83a3b29c924fe9ae/unoconv-0.9.0.tar.gz"
   sha256 "308ebfd98e67d898834876348b27caf41470cd853fbe2681cc7dacd8fd5e6031"
+  revision 1
   head "https://github.com/dagwieers/unoconv.git"
 
   bottle do
@@ -12,7 +13,7 @@ class Unoconv < Formula
     sha256 "080b156c88fd409534a862fb82fb98813f5aa342c17b10897c30186b6014ad15" => :high_sierra
   end
 
-  depends_on "python"
+  depends_on "python@3.8"
 
   def install
     system "make", "install", "prefix=#{prefix}"
