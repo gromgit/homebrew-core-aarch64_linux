61b801be27aa25a149a9fc0352f375647fef5dc0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-04-29 04:05:42 +0000

    cgoban: update 1.9.14 bottle.

--- Formula/cgoban.rb
+++ Formula/cgoban.rb
@@ -6,12 +6,9 @@ class Cgoban < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "95ba2672c251a7bb23614aee4711c84ceca8453679549847083fb5e025645f04" => :catalina
-    sha256 "9784461d9756059fa03d3239c3497ee3087fcaa67bcf235bdec2bab543560fae" => :mojave
-    sha256 "14efcd85d7d9f5a15fe0693eb8ebd4b1ee8b49fb7604681be91c14964af0cee3" => :high_sierra
-    sha256 "4f88f760ce464806c607e9a29da5f701cc6d395f27110cd866579fdf8737931a" => :sierra
-    sha256 "f6f9b32dddefb3474dca91d55c0aecdacec7e3b7ccbb2cb8c9b151b41e16f4d0" => :el_capitan
-    sha256 "65b5a814e1b4e3c115c1fb873d8a740ee4c0b39eff33331336d9bfac073e1a27" => :yosemite
+    sha256 "6b8613fe9566746c60a0ec45830b5c687d54cbaa6107179906248fa6094e856d" => :catalina
+    sha256 "fd7177595494fb367982e080af14fb7a249d6651a73a1b33f63394f9546fc837" => :mojave
+    sha256 "38bb87533d7c54253a2836af87c326651dda87c046f3803189308b155651e825" => :high_sierra
   end
 
   depends_on :x11
