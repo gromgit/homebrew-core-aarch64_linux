484216b8022927fff161606848df1a39bc850789
Rui Chen <chenrui333@gmail.com>
2020-04-29 00:41:06 -0400

    rswift 5.2.2
    Update tarball file
    
    Closes #53955.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rswift.rb
+++ Formula/rswift.rb
@@ -1,14 +1,12 @@
 class Rswift < Formula
   desc "Get strong typed, autocompleted resources like images, fonts and segues"
   homepage "https://github.com/mac-cain13/R.swift"
-  url "https://github.com/mac-cain13/R.swift.git",
-      :tag      => "v5.2.0",
-      :revision => "ec748b51d016e42016c93f8df8acb5e8f3635ccc"
+  url "https://github.com/mac-cain13/R.swift/releases/download/v5.2.2/rswift-v5.2.2-source.tar.gz"
+  sha256 "b27c51e9c69adcc61ed2f8a476fa5f2d97be5996d4f1e59a0e1b754a58980e26"
   head "https://github.com/mac-cain13/R.swift.git"
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
     sha256 "45db5b001da5af79ee047da3496a89e430f458ec83a57d68c0da696c4b8b2e29" => :catalina
     sha256 "479d11f836c09faf090901c3326da1d5cd1d0f545be691e6c3bddb47d931eda9" => :mojave
   end
@@ -22,5 +20,6 @@ class Rswift < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/rswift --version")
+    assert_match "[R.swift] Failed to write out", shell_output("#{bin}/rswift generate #{testpath} 2>1&")
   end
 end
