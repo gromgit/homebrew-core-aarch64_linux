7783ff981aa019b47e63fbe1391d60999c9f50ae
Rui Chen <chenrui333@gmail.com>
2020-04-29 09:32:32 -0400

    okteto 1.8.5
    Closes #53975.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/okteto.rb
+++ Formula/okteto.rb
@@ -1,8 +1,8 @@
 class Okteto < Formula
   desc "Build better apps by developing and testing code directly in Kubernetes"
   homepage "https://okteto.com"
-  url "https://github.com/okteto/okteto/archive/1.8.4.tar.gz"
-  sha256 "fc3629699355e7ab66661d5c77d34eb8b75ba8cb1ed8beaad74a5ab88a211ec6"
+  url "https://github.com/okteto/okteto/archive/1.8.5.tar.gz"
+  sha256 "bc7b5b14fe7880f82993d680e244285ea99c4db36a31f7d1b0ce110b91a84a70"
 
   bottle do
     cellar :any_skip_relocation
