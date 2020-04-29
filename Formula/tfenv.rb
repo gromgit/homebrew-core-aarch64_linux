6428d7ac24694e22f84da9c8dd4b716568b4717e
Rui Chen <chenrui333@gmail.com>
2020-04-29 00:50:55 -0400

    tfenv 2.0.0
    Closes #53959.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tfenv.rb
+++ Formula/tfenv.rb
@@ -1,8 +1,8 @@
 class Tfenv < Formula
   desc "Terraform version manager inspired by rbenv"
   homepage "https://github.com/tfutils/tfenv"
-  url "https://github.com/tfutils/tfenv/archive/v1.0.2.tar.gz"
-  sha256 "5ee33ee80fa850dd7c007d65a0a18bfb35ff2edfaef1e35d92d836941836f42f"
+  url "https://github.com/tfutils/tfenv/archive/v2.0.0.tar.gz"
+  sha256 "de3dcf13768cb078e94d68ca85071b8d6e44104394336d952255ca558b854b0b"
   head "https://github.com/tfutils/tfenv.git"
 
   bottle :unneeded
@@ -11,9 +11,15 @@ class Tfenv < Formula
 
   conflicts_with "terraform", :because => "tfenv symlinks terraform binaries"
 
+  # fix bash 3.x compatibility
+  # removed in the next release
+  patch do
+    url "https://github.com/tfutils/tfenv/pull/181.patch?full_index=1"
+    sha256 "b1365be51a8310a44b330f9b008dabcdfe2d16b0349f38988e7a24bcef6cae09"
+  end
+
   def install
-    prefix.install ["bin", "libexec", "share"]
-    prefix.install "lib" if build.head?
+    prefix.install %w[bin lib libexec share]
   end
 
   test do
