8327df06e7cdde355bde801974b65e42f940aecc
Sean Molenaar <smillerdev@me.com>
2020-04-29 15:08:32 +0200

    kops use go and update checksum

--- Formula/kops.rb
+++ Formula/kops.rb
@@ -2,7 +2,7 @@ class Kops < Formula
   desc "Production Grade K8s Installation, Upgrades, and Management"
   homepage "https://github.com/kubernetes/kops"
   url "https://github.com/kubernetes/kops/archive/1.16.1.tar.gz"
-  sha256 "720054de39fbb270397558a2eb47a8f4cbbfcacb9f792857c77d2cb32f56f764"
+  sha256 "16e2a04e5c55c5bf587c42ce575c9bc571530aa11f81cf30611a84d96cbd7924"
   head "https://github.com/kubernetes/kops.git"
 
   bottle do
@@ -12,7 +12,7 @@ class Kops < Formula
     sha256 "6c61ebad20e3ea6711072c5a257c0afbf8d70e864e93357f1a02b7a2da1b7a45" => :high_sierra
   end
 
-  depends_on "go@1.12" => :build
+  depends_on "go" => :build
   depends_on "kubernetes-cli"
 
   def install
