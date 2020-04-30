7f0b685f65a3a1c4c29e97aacf79b829088ab631
EricFromCanada <enk3@outlook.com>
2020-04-30 09:51:50 -0400

    glslang 8.13.3743
    Closes #54040.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/glslang.rb
+++ Formula/glslang.rb
@@ -1,8 +1,8 @@
 class Glslang < Formula
   desc "OpenGL and OpenGL ES reference compiler for shading languages"
   homepage "https://www.khronos.org/opengles/sdk/tools/Reference-Compiler/"
-  url "https://github.com/KhronosGroup/glslang/archive/8.13.3559.tar.gz"
-  sha256 "c58fdcf7e00943ba10f9ae565b2725ec9d5be7dab7c8e82cac72fcaa83c652ca"
+  url "https://github.com/KhronosGroup/glslang/archive/8.13.3743.tar.gz"
+  sha256 "639ebec56f1a7402f2fa094469a5ddea1eceecfaf2e9efe361376a0f73a7ee2f"
   head "https://github.com/KhronosGroup/glslang.git"
 
   bottle do
