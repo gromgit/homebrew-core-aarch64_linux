40c227a5885561519827edecffd959f6316163d8
chenrui <chenrui333@gmail.com>
2020-04-30 19:03:46 +0000

    kumactl: add 0.4.0 bottle.

--- Formula/kumactl.rb
+++ Formula/kumactl.rb
@@ -5,6 +5,13 @@ class Kumactl < Formula
       :tag      => "0.4.0",
       :revision => "a37aaadf84e97fa9e5135c31353b51ba6a2245f3"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "eff8edfec8b9f41c9488bf4bf34518af27b5272ccf25935e94e14716fb09e055" => :catalina
+    sha256 "2fa66e54b56d29ed3d2ded1d486e5107bb03b95f1b819a366e8ee0207d8a7388" => :mojave
+    sha256 "e0022bc88bbfd96c5897e99f4f73866f6c3580a99e0ade811d3aeb3f177312fa" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
