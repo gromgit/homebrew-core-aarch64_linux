fce3b4efaf54a297038bbe8cf645458b3eb53719
Derk Schooltink <derk.schooltink@sqills.com>
2020-04-30 08:59:28 +0200

    aws-cdk 1.36.1
    Closes #54017.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/aws-cdk.rb
+++ Formula/aws-cdk.rb
@@ -3,8 +3,8 @@ require "language/node"
 class AwsCdk < Formula
   desc "AWS Cloud Development Kit - framework for defining AWS infra as code"
   homepage "https://github.com/aws/aws-cdk"
-  url "https://registry.npmjs.org/aws-cdk/-/aws-cdk-1.36.0.tgz"
-  sha256 "907b7e2e35831d900da2dd07391963ab322cee2d83e73eb284ef91b9cf9bba62"
+  url "https://registry.npmjs.org/aws-cdk/-/aws-cdk-1.36.1.tgz"
+  sha256 "ac94bf120c4a09c37fd1851a0a58a2bb0d21bdc5e90ddc8bf4d904185f0bb31f"
 
   bottle do
     cellar :any_skip_relocation
