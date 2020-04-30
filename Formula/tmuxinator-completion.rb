86bfa7e27a3cf212260cd406ae1b36904b3f286f
Bo Anderson <mail@boanderson.me>
2020-04-30 14:54:12 +0100

    tmuxinator-completion 2.0.0 (#54032)

--- Formula/tmuxinator-completion.rb
+++ Formula/tmuxinator-completion.rb
@@ -1,8 +1,8 @@
 class TmuxinatorCompletion < Formula
   desc "Shell completion for Tmuxinator"
   homepage "https://github.com/tmuxinator/tmuxinator"
-  url "https://github.com/tmuxinator/tmuxinator/archive/v1.1.5.tar.gz"
-  sha256 "5b445dcb62556a439d76e191fd869e7c5fa79a787c6957eda8549fa343db2eb3"
+  url "https://github.com/tmuxinator/tmuxinator/archive/v2.0.0.tar.gz"
+  sha256 "83e50381fbdb224dbc214249c34af7ede912445bfc4eff20fdb88a8052404e09"
   head "https://github.com/tmuxinator/tmuxinator.git"
 
   bottle :unneeded
