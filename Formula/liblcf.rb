f37e1b2473de5f3bcb644693cfb485104d8a756d
Bo Anderson <mail@boanderson.me>
2020-04-30 15:25:57 +0100

    liblcf: revision bump for icu4c

--- Formula/liblcf.rb
+++ Formula/liblcf.rb
@@ -3,6 +3,7 @@ class Liblcf < Formula
   homepage "https://easyrpg.org/"
   url "https://easyrpg.org/downloads/player/0.6.2/liblcf-0.6.2.tar.xz"
   sha256 "c48b4f29ee0c115339a6886fc435b54f17799c97ae134432201e994b1d3e0d34"
+  revision 1
   head "https://github.com/EasyRPG/liblcf.git"
 
   bottle do
