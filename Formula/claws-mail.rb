7e0349ba5b46e528d047ecc5488718d46b72aadb
Dawid Dziurla <dawidd0811@gmail.com>
2020-04-30 13:52:33 +0000

    claws-mail: add 3.17.5 bottle.

--- Formula/claws-mail.rb
+++ Formula/claws-mail.rb
@@ -4,6 +4,12 @@ class ClawsMail < Formula
   url "https://www.claws-mail.org/releases/claws-mail-3.17.5.tar.gz"
   sha256 "f79e4c7f089d08e6b2b3323a90b6f46991a154ce168ba4a829d593b7a5768c7b"
 
+  bottle do
+    sha256 "21715bb6c0a1311197209fc443741d7c75051dc83a7f03ed18a876cd8df95e94" => :catalina
+    sha256 "c45802e7cd2c2d09c41a08e44ebaa9ebc7b8209a41a4e7c7308dde6fc960bfc6" => :mojave
+    sha256 "0ae3a6b02360f9a4dfefbdc5e5e0325230436992ce16ba669c99ca6928ab5557" => :high_sierra
+  end
+
   depends_on "pkg-config" => :build
   depends_on "cairo"
   depends_on "glib"
