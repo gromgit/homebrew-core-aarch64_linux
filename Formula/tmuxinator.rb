fb4a8cbf60bed734143511546e62692b954d2fd8
Bo Anderson <mail@boanderson.me>
2020-04-30 13:14:19 +0100

    tmuxinator 2.0.0
    Closes #54031.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/tmuxinator.rb
+++ Formula/tmuxinator.rb
@@ -1,8 +1,8 @@
 class Tmuxinator < Formula
   desc "Manage complex tmux sessions easily"
   homepage "https://github.com/tmuxinator/tmuxinator"
-  url "https://github.com/tmuxinator/tmuxinator/archive/v1.1.5.tar.gz"
-  sha256 "5b445dcb62556a439d76e191fd869e7c5fa79a787c6957eda8549fa343db2eb3"
+  url "https://github.com/tmuxinator/tmuxinator/archive/v2.0.0.tar.gz"
+  sha256 "83e50381fbdb224dbc214249c34af7ede912445bfc4eff20fdb88a8052404e09"
   head "https://github.com/tmuxinator/tmuxinator.git"
 
   bottle do
