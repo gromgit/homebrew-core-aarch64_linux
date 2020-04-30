afc1f6021165db985006a9473c6323ee163489ed
Bo Anderson <mail@boanderson.me>
2020-04-30 14:18:07 +0000

    ctemplate: update 2.4 bottle.

--- Formula/ctemplate.rb
+++ Formula/ctemplate.rb
@@ -7,13 +7,9 @@ class Ctemplate < Formula
 
   bottle do
     cellar :any
-    sha256 "5182660efd56f221c6fa002ec0d6f9e56de53c7518a6dadd2d634e298f1e6bea" => :catalina
-    sha256 "0f0dd5a902d31e3ff1e6868b425e4aeb9ec9d094337fea48f85cad7b7e0d747f" => :mojave
-    sha256 "6cddb070bbc9b662305513c142119cb83fac09d1c1579b925f6f2bbddb0773fe" => :high_sierra
-    sha256 "f2bbb674557034e487ad218f871145c9f27b02b908e10b2fd15f457c960191e0" => :sierra
-    sha256 "3cb0314574a76b022e365c293f50eff604def9a9171b45c4e3af1c56f5310927" => :el_capitan
-    sha256 "f405ef2681e4e0aff1a034226733e4466d6f916ae540fa7a3a52e3d94f529f26" => :yosemite
-    sha256 "335fb8f9b6ac20aeb09efba90dcdba941b7c0cef2571dcb50fb2040f515386c7" => :mavericks
+    sha256 "de55fe59cb142f6837d75f81c32a1430bd1df1cd3e612f2ccaa60baf4443ac4b" => :catalina
+    sha256 "614ac53920b9d21fcd936bf83580168ba9bebc15df7e87cf99135ab20bf1744d" => :mojave
+    sha256 "563a9a2d8c62edbd1496b4e360a625e3b484856d35edd945b364586f65c0e4e8" => :high_sierra
   end
 
   depends_on "autoconf" => :build
