f31c2428ff5185cdce3dcb26f67e334357021a2f
Bo Anderson <mail@boanderson.me>
2020-05-01 21:39:51 +0100

    weechat: migrate to python@3.8
    Closes #54088.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/weechat.rb
+++ Formula/weechat.rb
@@ -3,7 +3,7 @@ class Weechat < Formula
   homepage "https://www.weechat.org"
   url "https://weechat.org/files/src/weechat-2.8.tar.xz"
   sha256 "553ea295edad3b03cf88e6029c21e7bde32ff1cc026d35386ba9da3e56a6018c"
-  revision 1
+  revision 2
   head "https://github.com/weechat/weechat.git"
 
   bottle do
@@ -23,7 +23,7 @@ class Weechat < Formula
   depends_on "lua"
   depends_on "ncurses"
   depends_on "perl"
-  depends_on "python"
+  depends_on "python@3.8"
   depends_on "ruby"
 
   uses_from_macos "curl"
@@ -38,9 +38,8 @@ class Weechat < Formula
       -DENABLE_PHP=OFF
     ]
 
-    if MacOS.version >= :mojave && MacOS::CLT.installed?
-      ENV["SDKROOT"] = ENV["HOMEBREW_SDKROOT"] = MacOS::CLT.sdk_path(MacOS.version)
-    end
+    # Fix system gem on Mojave
+    ENV["SDKROOT"] = ENV["HOMEBREW_SDKROOT"]
 
     mkdir "build" do
       system "cmake", "..", *args
