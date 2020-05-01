c50d7f340a73401ecc5c87b942d8bdba7d16cd9b
Michka Popoff <michkapopoff@gmail.com>
2020-05-01 23:35:46 +0200

    gnome-recipes: use standard meson args

--- Formula/gnome-recipes.rb
+++ Formula/gnome-recipes.rb
@@ -50,7 +50,7 @@ class GnomeRecipes < Formula
     ENV["DESTDIR"] = ""
     ENV.delete "PYTHONPATH"
     mkdir "build" do
-      system "meson", "--prefix=#{prefix}", ".."
+      system "meson", *std_meson_args, ".."
       system "ninja"
       system "ninja", "install"
     end
