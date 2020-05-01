3b1c14e6024f5c4c5103c4baae045f4e21282681
Bo Anderson <mail@boanderson.me>
2020-05-01 12:03:25 +0100

    acpica 20200430
    Closes #54074.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/acpica.rb
+++ Formula/acpica.rb
@@ -1,8 +1,8 @@
 class Acpica < Formula
   desc "OS-independent implementation of the ACPI specification"
   homepage "https://www.acpica.org/"
-  url "https://acpica.org/sites/acpica/files/acpica-unix-20200326.tar.gz"
-  sha256 "99f9f394783de68e4f340135c72c8566d5aa7f8dc2bfdbebe35b161fe3d722ff"
+  url "https://acpica.org/sites/acpica/files/acpica-unix-20200430.tar.gz"
+  sha256 "fb807f1dec31664f972af37d213abf72987afe33abf68c83051e298da35d297c"
   head "https://github.com/acpica/acpica.git"
 
   bottle do
