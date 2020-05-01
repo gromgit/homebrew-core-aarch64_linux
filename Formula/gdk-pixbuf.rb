03d97df99ea688b25aabdd5457fc6a1f3275e645
Michka Popoff <michkapopoff@gmail.com>
2020-05-01 23:32:12 +0200

    gdk-pixbuf: use standard meson args

--- Formula/gdk-pixbuf.rb
+++ Formula/gdk-pixbuf.rb
@@ -37,8 +37,7 @@ class GdkPixbuf < Formula
               "-DGDK_PIXBUF_LIBDIR=\"@0@\"'.format(gdk_pixbuf_libdir)",
               "-DGDK_PIXBUF_LIBDIR=\"@0@\"'.format('#{HOMEBREW_PREFIX}/lib')"
 
-    args = %W[
-      --prefix=#{prefix}
+    args = std_meson_args + %w[
       -Dx11=false
       -Ddocs=false
       -Dgir=true
