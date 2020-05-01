702c65ca9f9ac5c441cc98ac798850642686bfb5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-01 01:06:11 +0000

    scamper: update 20191102b bottle.

--- Formula/scamper.rb
+++ Formula/scamper.rb
@@ -6,9 +6,9 @@ class Scamper < Formula
 
   bottle do
     cellar :any
-    sha256 "1a0f5c1946b62fb7fd92ee13f8313c8cc1aab2b167ed379a58480e6f6a033df5" => :catalina
-    sha256 "c42efb0765d212d5df33142b3923c0e04d49e0f58891f15530796838c6ff9e17" => :mojave
-    sha256 "8887037431a396ab36e404588554c8729e96b9da785e4216817832f2c805242d" => :high_sierra
+    sha256 "ce4b06740377720808c70eedad0c3da7e99a5686ec34f68cb578b4644a78b14b" => :catalina
+    sha256 "54d78388a155989fbb95dce2301f5ff72bb7bbc53ad3e067382c06600d63fc0b" => :mojave
+    sha256 "0133b481958220fe1db704531da243d22a50b2ddb01a30513410037bc44cf613" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
