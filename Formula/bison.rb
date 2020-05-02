0de5c9e14ec3059ae45e523b5df6bc26f900cb27
Thierry Moisan <thierry.moisan@gmail.com>
2020-05-01 21:49:18 -0400

    bison 3.5.91
    Closes #54104.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>

--- Formula/bison.rb
+++ Formula/bison.rb
@@ -1,9 +1,9 @@
 class Bison < Formula
   desc "Parser generator"
   homepage "https://www.gnu.org/software/bison/"
-  url "https://ftp.gnu.org/gnu/bison/bison-3.5.4.tar.xz"
-  mirror "https://ftpmirror.gnu.org/bison/bison-3.5.4.tar.xz"
-  sha256 "4c17e99881978fa32c05933c5262457fa5b2b611668454f8dc2a695cd6b3720c"
+  url "https://ftp.gnu.org/gnu/bison/bison-3.5.91.tar.xz"
+  mirror "https://ftpmirror.gnu.org/bison/bison-3.5.91.tar.xz"
+  sha256 "9145f59d184c5703a3857c074c1a570c9606be0e9471dd6548c9192e80ce30da"
 
   bottle do
     sha256 "1da0de53472ab236628591350fbe698813346b1d4872b8eeb8d64cfb9bdcfb30" => :catalina
