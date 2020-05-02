d8caa0ff7a35366ce2722b046a585371216e0bf1
Bo Anderson <mail@boanderson.me>
2020-05-02 16:41:18 +0100

    libsass 3.6.4
    Closes #54145.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libsass.rb
+++ Formula/libsass.rb
@@ -2,8 +2,8 @@ class Libsass < Formula
   desc "C implementation of a Sass compiler"
   homepage "https://github.com/sass/libsass"
   url "https://github.com/sass/libsass.git",
-      :tag      => "3.6.3",
-      :revision => "e1c16e09b4a953757a15149deaaf28a3fd81dc97"
+      :tag      => "3.6.4",
+      :revision => "8d312a1c91bb7dd22883ebdfc829003f75a82396"
   head "https://github.com/sass/libsass.git"
 
   bottle do
