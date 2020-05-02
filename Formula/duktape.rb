3d9232453e023fc29a0e36d3a4d9ebb039fc0790
chenrui <chenrui333@gmail.com>
2020-05-02 20:12:15 +0000

    duktape: add 2.5.0 bottle.

--- Formula/duktape.rb
+++ Formula/duktape.rb
@@ -4,6 +4,13 @@ class Duktape < Formula
   url "https://duktape.org/duktape-2.5.0.tar.xz"
   sha256 "83d411560a1cd36ea132bd81d8d9885efe9285c6bc6685c4b71e69a0c4329616"
 
+  bottle do
+    cellar :any
+    sha256 "db21b00be3c76ede599684bedbee17f4821dbb64933cefae7cfbea551929d405" => :catalina
+    sha256 "77188e6a3c739a0f81f531223f99aa7490fa82f5c7b33ca59d8f153caf770910" => :mojave
+    sha256 "90408a129a8233ac1250325ed8e1fd68e1ff98f3f0d983d14f4185f596961ef4" => :high_sierra
+  end
+
   def install
     inreplace "Makefile.sharedlibrary" do |s|
       s.gsub! %r{\/usr\/local}, prefix
