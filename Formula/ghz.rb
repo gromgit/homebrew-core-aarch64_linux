1a3b8fa18ac70ebf905eeb12661501dfabb1473b
Bojan <dbojan@gmail.com>
2020-05-03 16:28:18 -0300

    ghz 0.52.0 (new formula)
    ghz 0.52.0 (new formula) add additional test and improve install command
    
    Closes #54219.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

---
+++ Formula/ghz.rb
@@ -0,0 +1,30 @@
+class Ghz < Formula
+  desc "Simple gRPC benchmarking and load testing tool"
+  homepage "https://ghz.sh"
+  url "https://github.com/bojand/ghz/archive/v0.52.0.tar.gz"
+  sha256 "6f7e4e0cd81ad00c2c4e17c93257810e2ea8897f60713352c9d9583231e2739a"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build",
+      "-ldflags", "-s -w -X main.version=#{version}",
+      *std_go_args,
+      "cmd/ghz/main.go"
+    prefix.install_metafiles
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ghz -v 2>&1")
+    (testpath/"config.toml").write <<~EOS
+      proto = "greeter.proto"
+      call = "helloworld.Greeter.SayHello"
+      host = "0.0.0.0:50051"
+      insecure = true
+      [data]
+      name = "Bob"
+    EOS
+    assert_match "open greeter.proto: no such file or directory",
+      shell_output("#{bin}/ghz --config config.toml 2>&1", 1)
+  end
+end
