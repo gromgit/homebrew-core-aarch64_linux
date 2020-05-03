705a8bccef89a4c4b90086466f85edc122b10caa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-03 17:05:36 +0000

    pdftk-java: update 3.1.1 bottle.

--- Formula/pdftk-java.rb
+++ Formula/pdftk-java.rb
@@ -7,9 +7,9 @@ class PdftkJava < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9f2bad4b0697701837747055309581a2f722140e619f8211e8af3b60399311d0" => :catalina
-    sha256 "86540c3aea1b817a1423e3c7b8c1af0e1c01d2579f4f8bf22aaaa7fe2da5602b" => :mojave
-    sha256 "05fc4aff36077b66e3bc768c7dddb309e45d976f3a996a75a9191c8a88f8c0f6" => :high_sierra
+    sha256 "ae6790bdcf8eb9d44defd8cf082f09a97aed55f9ddfbf250b655d50e88985eb2" => :catalina
+    sha256 "e4bbfd10d2c57095fcdb0d85217a2fe28ac14dff4d74c2b857751df56b050689" => :mojave
+    sha256 "c9c8962a32595706de8a7f57fa490dd92b6ad49aa7b09aca23f3dce4bafadd79" => :high_sierra
   end
 
   depends_on "gradle" => :build
