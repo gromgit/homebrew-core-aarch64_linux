0f1e22825281d50023a5ba38d91d98bb33d546fe
Bo Anderson <mail@boanderson.me>
2020-05-03 13:06:24 +0100

    pre-commit: migrate to python@3.8
    Closes #54188.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pre-commit.rb
+++ Formula/pre-commit.rb
@@ -5,6 +5,7 @@ class PreCommit < Formula
   homepage "https://pre-commit.com/"
   url "https://github.com/pre-commit/pre-commit/archive/v2.3.0.tar.gz"
   sha256 "cbe534c57d0e0e17b0a739d71bb0d33a3bc295e65d6d125860bcfc7053abd645"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -13,22 +14,74 @@ class PreCommit < Formula
     sha256 "89872237307d4e37792531ded63106ffed9cc5c35cb0ca5250bdc66bd461b9d7" => :high_sierra
   end
 
-  # To avoid breaking existing git hooks when we update Python,
-  # we should never depend on a versioned Python formula and
-  # always use the "default".
-  depends_on "python"
+  depends_on "libyaml"
+  depends_on "python@3.8"
+
+  resource "appdirs" do
+    url "https://files.pythonhosted.org/packages/48/69/d87c60746b393309ca30761f8e2b49473d43450b150cb08f3c6df5c11be5/appdirs-1.4.3.tar.gz"
+    sha256 "9e5896d1372858f8dd3344faf4e5014d21849c756c8d5701f78f8a103b372d92"
+  end
+
+  resource "cfgv" do
+    url "https://files.pythonhosted.org/packages/88/e8/159f95e454d6c35d2fc238ca056497672b8d93c3dab568a9b13e7a602331/cfgv-3.1.0.tar.gz"
+    sha256 "c8e8f552ffcc6194f4e18dd4f68d9aef0c0d58ae7e7be8c82bee3c5e9edfa513"
+  end
+
+  resource "distlib" do
+    url "https://files.pythonhosted.org/packages/7d/29/694a3a4d7c0e1aef76092e9167fbe372e0f7da055f5dcf4e1313ec21d96a/distlib-0.3.0.zip"
+    sha256 "2e166e231a26b36d6dfe35a48c4464346620f8645ed0ace01ee31822b288de21"
+  end
+
+  resource "filelock" do
+    url "https://files.pythonhosted.org/packages/14/ec/6ee2168387ce0154632f856d5cc5592328e9cf93127c5c9aeca92c8c16cb/filelock-3.0.12.tar.gz"
+    sha256 "18d82244ee114f543149c66a6e0c14e9c4f8a1044b5cdaadd0f82159d6a6ff59"
+  end
+
+  resource "identify" do
+    url "https://files.pythonhosted.org/packages/13/1f/b3b54c63b24aea84bc70c4fbab80f61f93384517bb867831e643402d0bbf/identify-1.4.15.tar.gz"
+    sha256 "23c18d97bb50e05be1a54917ee45cc61d57cb96aedc06aabb2b02331edf0dbf0"
+  end
+
+  resource "nodeenv" do
+    url "https://files.pythonhosted.org/packages/00/6e/ed417bd1ed417ab3feada52d0c89ab0ed87d150f91590badf84273e047c9/nodeenv-1.3.3.tar.gz"
+    sha256 "ad8259494cf1c9034539f6cced78a1da4840a4b157e23640bc4a0c0546b0cb7a"
+  end
+
+  resource "PyYAML" do
+    url "https://files.pythonhosted.org/packages/64/c2/b80047c7ac2478f9501676c988a5411ed5572f35d1beff9cae07d321512c/PyYAML-5.3.1.tar.gz"
+    sha256 "b8eac752c5e14d3eca0e6dd9199cd627518cb5ec06add0de9d32baeee6fe645d"
+  end
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/21/9f/b251f7f8a76dec1d6651be194dfba8fb8d7781d10ab3987190de8391d08e/six-1.14.0.tar.gz"
+    sha256 "236bdbdce46e6e6a3d61a337c0f8b763ca1e8717c03b369e87a7ec7ce1319c0a"
+  end
+
+  resource "toml" do
+    url "https://files.pythonhosted.org/packages/b9/19/5cbd78eac8b1783671c40e34bb0fa83133a06d340a38b55c645076d40094/toml-0.10.0.tar.gz"
+    sha256 "229f81c57791a41d65e399fc06bf0848bab550a9dfd5ed66df18ce5f05e73d5c"
+  end
+
+  resource "virtualenv" do
+    url "https://files.pythonhosted.org/packages/54/a2/bbfd87f524096f26b373a3428e56bdcda6661ba38314f1d864a71a6e70ce/virtualenv-20.0.18.tar.gz"
+    sha256 "ac53ade75ca189bc97b6c1d9ec0f1a50efe33cbf178ae09452dcd9fd309013c1"
+  end
 
   def install
-    # Make sure we are actually using Homebrew's Python
+    # Point hook shebang to virtualenv Python.
+    # The global one also works - but may be keg-only.
+    # A full path can also move around if we use versioned formulae.
+    # Git hooks should only have to be installed once and never need changing.
     inreplace "pre_commit/commands/install_uninstall.py",
               "f'#!/usr/bin/env {py}'",
-              "'#!#{Formula["python"].opt_bin}/python3'"
+              "'#!#{opt_libexec}/bin/python3'"
 
-    venv = virtualenv_create(libexec, "python3")
-    system libexec/"bin/pip", "install", "-v", "--no-binary", ":all:",
-                              "--ignore-installed", buildpath
-    system libexec/"bin/pip", "uninstall", "-y", "pre-commit"
-    venv.pip_install_and_link buildpath
+    # Avoid Cellar path reference, which is only good for one version.
+    inreplace "pre_commit/commands/install_uninstall.py",
+              "'INSTALL_PYTHON': sys.executable",
+              "'INSTALL_PYTHON': '#{opt_libexec}/bin/python3'"
+
+    virtualenv_install_with_resources
   end
 
   # Avoid relative paths
