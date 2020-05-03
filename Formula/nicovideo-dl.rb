4affb9521eac65016e91d83fa3e3ad4282ab8a99
Bo Anderson <mail@boanderson.me>
2020-05-03 16:57:37 +0100

    nicovideo-dl: migrate to python@3.8
    Closes #54204.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nicovideo-dl.rb
+++ Formula/nicovideo-dl.rb
@@ -1,21 +1,19 @@
 class NicovideoDl < Formula
+  include Language::Python::Shebang
+
   desc "Command-line program to download videos from www.nicovideo.jp"
   homepage "https://osdn.net/projects/nicovideo-dl/"
   # Canonical: https://osdn.net/dl/nicovideo-dl/nicovideo-dl-0.0.20190126.tar.gz
   url "https://dotsrc.dl.osdn.net/osdn/nicovideo-dl/70568/nicovideo-dl-0.0.20190126.tar.gz"
   sha256 "886980d154953bc5ff5d44758f352ce34d814566a83ceb0b412b8d2d51f52197"
+  revision 1
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "2c43b33d48c8a63229f5be23d2397bac63d3651f98e1937a19a5d8c910083a6c" => :catalina
-    sha256 "21a964141ae24de2635828bde4ac6f46ed09a8b03b5564050e36ab3df0b9bceb" => :mojave
-    sha256 "21a964141ae24de2635828bde4ac6f46ed09a8b03b5564050e36ab3df0b9bceb" => :high_sierra
-    sha256 "ff5c5d5a91e5384c20cd068a1857c83322e2ab27ac6ee0baf01cfabad44b9dd9" => :sierra
-  end
+  bottle :unneeded
 
-  depends_on "python"
+  depends_on "python@3.8"
 
   def install
+    rewrite_shebang detected_python_shebang, "nicovideo-dl"
     bin.install "nicovideo-dl"
   end
 
