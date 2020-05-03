dfdc86a7f3a54960b88f90fb199556b050bb54bb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-03 10:05:54 +0000

    passpie: update 1.6.1_1 bottle.

--- Formula/passpie.rb
+++ Formula/passpie.rb
@@ -9,11 +9,10 @@ class Passpie < Formula
   head "https://github.com/marcwebbie/passpie.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "b2f672d9946464659d5bb0f76684a2acb46386fa5f8dde617dae26af46e0c308" => :catalina
-    sha256 "8d0bea708df6b337834af0eff5e387a2f134001233171210f854a48b4b53bbbe" => :mojave
-    sha256 "864bb80f52775b4941847c929922169fafebce6642ee7301f198e74acd6b0817" => :high_sierra
-    sha256 "619b05a27170c7fd8881e1922a7836625175b984ecc1a2cdadb5103bff7a8b58" => :sierra
+    cellar :any
+    sha256 "29a24482b5c955a6d14b7a285d6937c04ab89a53f110c8343221d2ccef2cb508" => :catalina
+    sha256 "9f524fdab59188aab2b53fe7c3e5084ecdc27149dd742abffdfb13af074ba0ee" => :mojave
+    sha256 "acac2254266a3c741c15e28403482e67517d447dc4a4c0411934ec93ab902945" => :high_sierra
   end
 
   depends_on "gnupg"
