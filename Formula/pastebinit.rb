b0c964e493e5e6d78143002ae889c82b0e05137f
Michka Popoff <michkapopoff@gmail.com>
2020-05-03 14:35:13 +0200

    pastebinit: migrate to python@3.8
    Closes #54192.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pastebinit.rb
+++ Formula/pastebinit.rb
@@ -3,7 +3,7 @@ class Pastebinit < Formula
   homepage "https://launchpad.net/pastebinit"
   url "https://launchpad.net/pastebinit/trunk/1.5/+download/pastebinit-1.5.tar.gz"
   sha256 "0d931dddb3744ed38aa2d319dd2d8a2f38a391011ff99db68ce7c83ab8f5b62f"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any_skip_relocation
@@ -14,11 +14,17 @@ class Pastebinit < Formula
   end
 
   depends_on "docbook2x" => :build
-  depends_on "python"
+  depends_on "python@3.8"
+
+  # Remove for next release
+  patch do
+    url "https://github.com/lubuntu-team/pastebinit/commit/ab05aa431a6bf76b28586ad97c98069b8de5e46a.patch?full_index=1"
+    sha256 "1abd0ec274cf0952a371e6738fcd3ece67bb9a4dd52f997296cd107f035f5690"
+  end
 
   def install
     inreplace "pastebinit" do |s|
-      s.gsub! "/usr/bin/python3", Formula["python"].opt_bin/"python3"
+      s.gsub! "/usr/bin/python3", Formula["python@3.8"].opt_bin/"python3"
       s.gsub! "/usr/local/etc/pastebin.d", etc/"pastebin.d"
     end
 
