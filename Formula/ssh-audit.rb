19b33a7e47b26888794538254515eaf8f5f55fe6
Bo Anderson <mail@boanderson.me>
2020-05-03 18:13:42 +0100

    ssh-audit: migrate to python@3.8
    Closes #54213.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ssh-audit.rb
+++ Formula/ssh-audit.rb
@@ -1,15 +1,19 @@
 class SshAudit < Formula
+  include Language::Python::Shebang
+
   desc "SSH server & client auditing"
   homepage "https://github.com/jtesta/ssh-audit"
   url "https://github.com/jtesta/ssh-audit/releases/download/v2.2.0/ssh-audit-2.2.0.tar.gz"
   sha256 "a8f4f01122234bd84c01440bfd3b7a6026c50c1a06f3044846a8503c94f94cfb"
+  revision 1
   head "https://github.com/jtesta/ssh-audit.git"
 
   bottle :unneeded
 
-  depends_on "python"
+  depends_on "python@3.8"
 
   def install
+    rewrite_shebang detected_python_shebang, "ssh-audit.py"
     bin.install "ssh-audit.py" => "ssh-audit"
   end
 
