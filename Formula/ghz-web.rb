5e724d1b890e6c0a1baf15936f44e87a56b69327
Bojan <dbojan@gmail.com>
2020-05-03 16:30:43 -0300

    ghz-web 0.52.0 (new formula)

---
+++ Formula/ghz-web.rb
@@ -0,0 +1,30 @@
+class GhzWeb < Formula
+  desc "Web interface for ghz"
+  homepage "https://ghz.sh"
+  url "https://github.com/bojand/ghz/archive/v0.52.0.tar.gz"
+  sha256 "6f7e4e0cd81ad00c2c4e17c93257810e2ea8897f60713352c9d9583231e2739a"
+
+  depends_on "go" => :build
+  depends_on :xcode => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "1"
+    system "go", "build",
+      "-ldflags", "-s -w -X main.version=#{version}",
+      *std_go_args,
+      "cmd/ghz-web/main.go"
+    prefix.install_metafiles
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ghz-web -v 2>&1")
+    port = free_port
+    ENV["GHZ_SERVER_PORT"] = port.to_s
+    fork do
+      exec "#{bin}/ghz-web"
+    end
+    sleep 1
+    cmd = "curl -sIm3 -XGET http://localhost:#{port}/"
+    assert_match /200 OK/m, shell_output(cmd)
+  end
+end
