96cbf402dfd02028a9c9d9903c08d7a4cd02e86b
lapfelix <felix@lapal.me>
2020-05-02 22:57:17 -0400

    bluetoothconnector 2.0.0
    Closes #54183.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bluetoothconnector.rb
+++ Formula/bluetoothconnector.rb
@@ -1,8 +1,9 @@
 class Bluetoothconnector < Formula
   desc "Connect and disconnect Bluetooth devices"
   homepage "https://github.com/lapfelix/BluetoothConnector"
-  url "https://github.com/lapfelix/BluetoothConnector/archive/1.3.0.tar.gz"
-  sha256 "66b94a154e25c867bbdad50e643980a475a63af935900b539e7c48ed0fb1edd7"
+  url "https://github.com/lapfelix/BluetoothConnector/archive/2.0.0.tar.gz"
+  sha256 "41474f185fd40602fb197e79df5cd4783ff57b92c1dfe2b8e2c4661af038ed9b"
+  head "https://github.com/lapfelix/BluetoothConnector.git"
 
   bottle do
     cellar :any_skip_relocation
@@ -10,18 +11,15 @@ class Bluetoothconnector < Formula
     sha256 "b49cd1726285c91d42935e0052814940a9c33a99df54ea1da2eab4c5b7411c71" => :high_sierra
   end
 
-  depends_on :xcode => ["10.0", :build]
+  depends_on :xcode => ["11.0", :build]
 
   def install
-    system "swift", "build", "--disable-sandbox", "-c", "release", "-Xswiftc", "-static-stdlib"
+    system "swift", "build", "--disable-sandbox", "-c", "release", "--static-swift-stdlib"
     bin.install ".build/release/BluetoothConnector"
   end
 
   test do
-    output = shell_output("#{bin}/BluetoothConnector")
-    assert_match "Usage: BluetoothConnector", output
-    assert_match "Get the MAC address from the list below", output
-
+    shell_output("#{bin}/BluetoothConnector", 64)
     output_fail = shell_output("#{bin}/BluetoothConnector --connect 00-00-00-00-00-00", 252)
     assert_equal "Not paired to device\n", output_fail
   end
