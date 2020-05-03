11d0f8d68e0118ca680e8109412470ae58e94e84
Bo Anderson <mail@boanderson.me>
2020-05-04 00:58:20 +0100

    simgrid: migrate to python@3.8
    Closes #54231.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/simgrid.rb
+++ Formula/simgrid.rb
@@ -1,8 +1,11 @@
 class Simgrid < Formula
+  include Language::Python::Shebang
+
   desc "Studies behavior of large-scale distributed systems"
   homepage "https://simgrid.org/"
   url "https://framagit.org/simgrid/simgrid/uploads/0365f13697fb26eae8c20fc234c5af0e/SimGrid-3.25.tar.gz"
   sha256 "0b5dcdde64f1246f3daa7673eb1b5bd87663c0a37a2c5dcd43f976885c6d0b46"
+  revision 1
 
   bottle do
     sha256 "ad770f08175c732a0d41fb8ce9cd0c5522d3276b8f062560cfe2b638529481da" => :catalina
@@ -15,16 +18,21 @@ class Simgrid < Formula
   depends_on "boost"
   depends_on "graphviz"
   depends_on "pcre"
-  depends_on "python"
+  depends_on "python@3.8"
 
   def install
+    # Avoid superenv shim references
+    inreplace "src/smpi/smpicc.in", "@CMAKE_C_COMPILER@", "/usr/bin/clang"
+    inreplace "src/smpi/smpicxx.in", "@CMAKE_CXX_COMPILER@", "/usr/bin/clang++"
+
     system "cmake", ".",
                     "-Denable_debug=on",
                     "-Denable_compile_optimizations=off",
-                    "-DCMAKE_C_COMPILER=clang",
-                    "-DCMAKE_CXX_COMPILER=clang++",
+                    "-Denable_fortran=off",
                     *std_cmake_args
     system "make", "install"
+
+    bin.find { |f| rewrite_shebang detected_python_shebang, f }
   end
 
   test do
