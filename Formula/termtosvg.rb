20e73dbecb921a5f6539f66aac2c39eab883a8c2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-03 09:05:41 +0000

    termtosvg: update 1.1.0_1 bottle.

--- Formula/termtosvg.rb
+++ Formula/termtosvg.rb
@@ -9,9 +9,9 @@ class Termtosvg < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "98353f1a6df1675441bd9fbd7f7015905b654cdce008582072d6b7e8c8fdde42" => :catalina
-    sha256 "94ec2cd77e154ba2314aeff68ce68ad7614c9a5c8be8bcf0830bc02b3f5b37a5" => :mojave
-    sha256 "24b342f95d1700df9485dba70ef456a7f8bb64391a3b58502ca4c651e330f494" => :high_sierra
+    sha256 "f882539d594efc11647207ad81eed3c1a2f365c6c9a3b7be1401dffbab6ea98d" => :catalina
+    sha256 "860ca46763f9cfeb387a806e380d6a680fc39f13d31795ebed68e7c2df087aaa" => :mojave
+    sha256 "6cb7b9688575469970762f3ecdee882a30051a4b6d42f41af0e6aed159d97b0b" => :high_sierra
   end
 
   depends_on "python@3.8"
