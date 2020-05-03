2b522022ab47bbf600f879df3d8eab8caca46925
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-03 09:06:11 +0000

    theharvester: update 3.1_1 bottle.

--- Formula/theharvester.rb
+++ Formula/theharvester.rb
@@ -9,10 +9,10 @@ class Theharvester < Formula
   head "https://github.com/laramies/theHarvester.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "274398dbaf8d24f8b52d6aaa436081486d95839b3a7040ed40f8afe37d1184b4" => :catalina
-    sha256 "a12701a839ff69050ab270b1e3b18758aa6d107cc30a6cc7b1c4ce9d0a2d8dc0" => :mojave
-    sha256 "fa44985ba8c6761d27b5c26582dfc79e14ede1c55d9e320d843095b419acdece" => :high_sierra
+    cellar :any
+    sha256 "46856f81e9b3e915c37f652886bd17f886fbc7d3c4fb25095a43a04b5b2a916c" => :catalina
+    sha256 "ef8331534e766d52d242fe874e331baa3e563e366a8f5a2bae777024b38310fb" => :mojave
+    sha256 "628cf264b4109d51f768dde76b59409a02d1d6167ce1843d30d9f0ef82016a65" => :high_sierra
   end
 
   depends_on "libyaml"
