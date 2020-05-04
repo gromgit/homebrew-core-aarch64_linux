696389bccdc51b7c88eb1ee9f3a0bdff7302b745
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-04 16:05:52 +0000

    hlint: update 3.0.4 bottle.

--- Formula/hlint.rb
+++ Formula/hlint.rb
@@ -10,9 +10,9 @@ class Hlint < Formula
   head "https://github.com/ndmitchell/hlint.git"
 
   bottle do
-    sha256 "1a1c76cae91a3899b216f734895e3cec07be0b733662f94a9a0f4bdfa31c112a" => :catalina
-    sha256 "86389812a5ea73471e7c5897073ec9800a60fdb615282292ee2eee2b047fac8a" => :mojave
-    sha256 "98d4a5aeb962d0872a69738cb20c17986703e153b2ab261119ba164ce2210132" => :high_sierra
+    sha256 "555e51f0fc3741d2fbd6282ba120fa7ff755bc5d83703c85504eb1f8ff3ee170" => :catalina
+    sha256 "312be3c601bb7e7e3f4104983af1c3ec58a6c653d01d6ab8c1bf167fc71cdd01" => :mojave
+    sha256 "4d313e73741877fe103c0bdf1105405d40bebb76da1c1b14d83eea346488d305" => :high_sierra
   end
 
   depends_on "cabal-install" => :build
