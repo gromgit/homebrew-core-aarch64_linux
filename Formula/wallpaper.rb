237fca4d294730f621c95e3bdd7e001537f0309e
Dario Vladovic <d.vladimyr@gmail.com>
2020-05-04 17:54:37 +0200

    wallpaper 2.1.0
    Closes #54254.
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/wallpaper.rb
+++ Formula/wallpaper.rb
@@ -1,8 +1,8 @@
 class Wallpaper < Formula
   desc "Manage the desktop wallpaper"
   homepage "https://github.com/sindresorhus/macos-wallpaper"
-  url "https://github.com/sindresorhus/macos-wallpaper/archive/v2.0.0.tar.gz"
-  sha256 "49ab6121dcc78d17aae3219ceeeb1846792855179f11021192e5c42e500b166c"
+  url "https://github.com/sindresorhus/macos-wallpaper/archive/v2.1.0.tar.gz"
+  sha256 "4925247524535c0cc128dcc4d87f5538a5ce3b5d3a3c211127fd646ee00252b6"
   head "https://github.com/sindresorhus/macos-wallpaper.git"
 
   bottle do
@@ -11,11 +11,11 @@ class Wallpaper < Formula
     sha256 "bdf9b70a15c4060ba202631c2a3e803ff37369d2d3994d913b67dd840c124f1e" => :high_sierra
   end
 
-  depends_on :xcode => ["10.0", :build]
+  depends_on :xcode => ["11.4", :build]
   depends_on :macos => :sierra
 
   def install
-    system "swift", "build", "-c", "release", "-Xswiftc", "-static-stdlib", "--disable-sandbox"
+    system "swift", "build", "-c", "release", "--static-swift-stdlib", "--disable-sandbox"
     bin.install ".build/release/wallpaper"
   end
 
