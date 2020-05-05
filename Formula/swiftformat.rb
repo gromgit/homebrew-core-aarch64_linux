0d90e8aa71cb13316a483d5df4e6b157260c7244
Issy Long <me@issyl0.co.uk>
2020-05-05 22:39:45 +0000

    swiftformat: update 0.44.9 bottle.

--- Formula/swiftformat.rb
+++ Formula/swiftformat.rb
@@ -7,9 +7,9 @@ class Swiftformat < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9cbd2c21d4579c8c4ffc9df7b44593f7622d13f8517a3fed047c5e5f3f2560c6" => :catalina
-    sha256 "a8f8b6fa977804c2d13f0cbc9ed223ddaa88e3c0684e91519e005784c8418b36" => :mojave
-    sha256 "fd43d192d194a8bb02308714b691e6a7101f839889f749dff4a1facf8172b951" => :high_sierra
+    sha256 "20066e7f5becea0099f05dc510634d5193799c4bb5e0e25d6fc6be318e812964" => :catalina
+    sha256 "ff9622c427d88dadc5b0f6348d47044e85ba45e55449ca506687b3773172e2d1" => :mojave
+    sha256 "2dab824409448a41181ae6e41bd6e6a32f9fe373e104b87891ad4327993a870f" => :high_sierra
   end
 
   depends_on :xcode => ["10.1", :build]
