1e092bd1e23da1818cbe1fe7164988b15876be19
Rui Chen <chenrui333@gmail.com>
2020-05-05 14:06:41 -0400

    rancher-cli 2.4.3
    Closes #54295.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rancher-cli.rb
+++ Formula/rancher-cli.rb
@@ -1,8 +1,8 @@
 class RancherCli < Formula
   desc "The Rancher CLI is a unified tool to manage your Rancher server"
   homepage "https://github.com/rancher/cli"
-  url "https://github.com/rancher/cli/archive/v2.4.0.tar.gz"
-  sha256 "666e8cc507a88f5b98da1d86ca13d9cb85ebd14e979297c06e882ce3e99ddf1d"
+  url "https://github.com/rancher/cli/archive/v2.4.3.tar.gz"
+  sha256 "9e35b653f472aead86fbbd37bde6f3f311fa4f808d4230f091e18fcd4f7fd9ba"
 
   bottle do
     cellar :any_skip_relocation
