e88ec097820e9770d2989d356793c5904554d2ed
Mike McQuaid <mike@mikemcquaid.com>
2020-05-06 11:40:26 +0100

    openssl@1.1: update openssl reference.

--- Formula/openssl@1.1.rb
+++ Formula/openssl@1.1.rb
@@ -87,7 +87,7 @@ class OpensslAT11 < Formula
 
   test do
     # Make sure the necessary .cnf file exists, otherwise OpenSSL gets moody.
-    assert_predicate HOMEBREW_PREFIX/"etc/openssl@1.1/openssl.cnf", :exist?,
+    assert_predicate pkgetc/"openssl.cnf", :exist?,
             "OpenSSL requires the .cnf file for some functionality"
 
     # Check OpenSSL itself functions as expected.
