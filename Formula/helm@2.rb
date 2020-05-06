81d2c7756a86073b263886ecec8f8391bec308e9
Jonathan Meyers <jonathan@cybrary.it>
2020-05-06 11:48:34 +0800

    helm@2 2.16.7
    Closes #54308.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/helm@2.rb
+++ Formula/helm@2.rb
@@ -2,8 +2,8 @@ class HelmAT2 < Formula
   desc "The Kubernetes package manager"
   homepage "https://helm.sh/"
   url "https://github.com/helm/helm.git",
-      :tag      => "v2.16.6",
-      :revision => "dd2e5695da88625b190e6b22e9542550ab503a47"
+      :tag      => "v2.16.7",
+      :revision => "5f2584fd3d35552c4af26036f0c464191287986b"
 
   bottle do
     cellar :any_skip_relocation
