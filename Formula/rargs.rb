24d981fe322b73b3c852fb91ea2f7cbe78b0ed3d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-06 15:06:13 +0000

    rargs: update 0.3.0 bottle.

--- Formula/rargs.rb
+++ Formula/rargs.rb
@@ -6,9 +6,9 @@ class Rargs < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "89fe0e1d60e88b0ce966d31962adbe352af2bc1b6a45e66a8fc3783f80d90edc" => :catalina
-    sha256 "2fb0c1a1c572c148027cc48bc8d66cc3a02ba0325648eee9c2b1dd20242cb083" => :mojave
-    sha256 "e98e7b13fa8b875de0fe15ca7dbb6a8e2e5e4d0b38b75eab62342b8decc38f16" => :high_sierra
+    sha256 "37d5a3c2a5608eb4a10df0814a1334b88602a7200fdf99db60113f7aea598489" => :catalina
+    sha256 "1c24f60f8b91301cd167b0040e2c9ec7895fe818eeb21f13d40fca94e6f4f08b" => :mojave
+    sha256 "9cea3ec1abc342281b94649496e0d28275eead691238a2d03e47c2621afc9801" => :high_sierra
   end
 
   depends_on "rust" => :build
