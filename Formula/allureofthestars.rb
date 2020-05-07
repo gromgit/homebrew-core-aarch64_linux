eabdf946ecb0801cde4b609071be60ba9fe522f0
Alexander Bayandin <a.bayandin@gmail.com>
2020-05-07 18:22:02 +0100

    allureofthestars: use ghc@8.8
    Co-Authored-By: Bo Anderson <mail@boanderson.me>
    Co-Authored-By: fishtreesugar <fishtreesugar@gmail.com>
    Co-Authored-By: Luke Lau <luke_lau@icloud.com>

--- Formula/allureofthestars.rb
+++ Formula/allureofthestars.rb
@@ -17,7 +17,7 @@ class Allureofthestars < Formula
   end
 
   depends_on "cabal-install" => :build
-  depends_on "ghc" => :build
+  depends_on "ghc@8.8" => :build
   depends_on "pkg-config" => :build
   depends_on "sdl2_ttf"
 
