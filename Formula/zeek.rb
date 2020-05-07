66d88f2ae2310f77ef18287e818a78436378f9b9
Caleb Xu <calebcenter@live.com>
2020-05-07 02:52:17 -0400

    zeek 3.1.3
    Closes #54357.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/zeek.rb
+++ Formula/zeek.rb
@@ -2,8 +2,8 @@ class Zeek < Formula
   desc "Network security monitor"
   homepage "https://www.zeek.org"
   url "https://github.com/zeek/zeek.git",
-      :tag      => "v3.1.2",
-      :revision => "df348b789002454cd0386757f3783f04398e5ae6"
+      :tag      => "v3.1.3",
+      :revision => "4f695cae3ba720981ab7e38c16d4ab1adf4f01f5"
   head "https://github.com/zeek/zeek.git"
 
   bottle do
