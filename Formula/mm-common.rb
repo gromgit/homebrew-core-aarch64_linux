89db74f48f6525e9711acb0f2c1be9cb8f70a486
Caleb Xu <calebcenter@live.com>
2020-05-08 04:02:52 -0400

    mm-common: migrate to python@3.8
    Closes #54388.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mm-common.rb
+++ Formula/mm-common.rb
@@ -3,6 +3,7 @@ class MmCommon < Formula
   homepage "https://www.gtkmm.org/"
   url "https://download.gnome.org/sources/mm-common/1.0/mm-common-1.0.0.tar.xz"
   sha256 "b97d9b041e5952486cab620b44ab09f6013a478f43b6699ae899b8a4da189cd4"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -13,7 +14,7 @@ class MmCommon < Formula
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
-  depends_on "python"
+  depends_on "python@3.8"
 
   def install
     mkdir "build" do
@@ -21,5 +22,16 @@ class MmCommon < Formula
       system "ninja"
       system "ninja", "install"
     end
+  end
+
+  test do
+    mkdir testpath/"test"
+    touch testpath/"test/a"
+
+    system bin/"mm-common-prepare", "-c", testpath/"test/a"
+    assert_predicate testpath/"test/compile-binding.am", :exist?
+    assert_predicate testpath/"test/dist-changelog.am", :exist?
+    assert_predicate testpath/"test/doc-reference.am", :exist?
+    assert_predicate testpath/"test/generate-binding.am", :exist?
   end
 end
