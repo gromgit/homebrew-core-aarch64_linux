38b6d6154aedeec2dd10692f2ff24931a7a6fbdc
Caleb Xu <calebcenter@live.com>
2020-05-08 03:30:35 -0400

    xa 2.3.11
    Closes #54386.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/xa.rb
+++ Formula/xa.rb
@@ -1,8 +1,8 @@
 class Xa < Formula
   desc "6502 cross assembler"
   homepage "https://www.floodgap.com/retrotech/xa/"
-  url "https://www.floodgap.com/retrotech/xa/dists/xa-2.3.10.tar.gz"
-  sha256 "867b5b26b6524be8bcfbad8820ab3efe422b3e0cc9775dcb743284778868ba78"
+  url "https://www.floodgap.com/retrotech/xa/dists/xa-2.3.11.tar.gz"
+  sha256 "32f2164c99e305218e992970856dd8e2309b5cb6ac4758d7b2afe3bfebc9012d"
 
   bottle do
     cellar :any_skip_relocation
