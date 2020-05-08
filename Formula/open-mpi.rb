a7667084f426ab7f45a506d814db05d1f31e5c81
FX Coudert <fxcoudert@gmail.com>
2020-05-09 01:03:37 +0200

    open-mpi: revision for gcc

--- Formula/open-mpi.rb
+++ Formula/open-mpi.rb
@@ -3,6 +3,7 @@ class OpenMpi < Formula
   homepage "https://www.open-mpi.org/"
   url "https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.4.tar.bz2"
   sha256 "47e24eb2223fe5d24438658958a313b6b7a55bb281563542e1afc9dec4a31ac4"
+  revision 1
 
   bottle do
     sha256 "b45ead03ae109edf9e21641c95936971bc26e73043f714bdafc8424ca48f80ab" => :catalina
