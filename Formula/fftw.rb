9f7b46c7afc3d792104505be751821d6b2a8c7ec
FX Coudert <fxcoudert@gmail.com>
2020-05-09 01:03:36 +0200

    fftw: revision for gcc

--- Formula/fftw.rb
+++ Formula/fftw.rb
@@ -3,7 +3,7 @@ class Fftw < Formula
   homepage "http://www.fftw.org"
   url "http://fftw.org/fftw-3.3.8.tar.gz"
   sha256 "6113262f6e92c5bd474f2875fa1b01054c4ad5040f6b0da7c03c98821d9ae303"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
