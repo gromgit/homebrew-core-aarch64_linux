90f7f4960c35c0b1d91a87ae3e6032c271f9bd8c
Rui Chen <chenrui333@gmail.com>
2020-05-09 14:33:49 -0400

    pngpaste 0.2.3
    Closes #54481.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/pngpaste.rb
+++ Formula/pngpaste.rb
@@ -1,8 +1,8 @@
 class Pngpaste < Formula
   desc "Paste PNG into files"
   homepage "https://github.com/jcsalterego/pngpaste"
-  url "https://github.com/jcsalterego/pngpaste/archive/0.2.2.tar.gz"
-  sha256 "f7566b4eba94916df5723cdcef8e325ee7151c530eec025e996d0e784293362c"
+  url "https://github.com/jcsalterego/pngpaste/archive/0.2.3.tar.gz"
+  sha256 "6221201cb05191855f0d2707ce7f4055f6e1330de8efc09d386be2a6629f543b"
 
   bottle do
     cellar :any_skip_relocation
