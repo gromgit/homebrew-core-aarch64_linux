2d5e061a44bfac91043d869cf23bd9bde7e0fd78
chenrui <chenrui333@gmail.com>
2020-05-09 18:12:29 +0000

    libfabric: update 1.10.1 bottle.

--- Formula/libfabric.rb
+++ Formula/libfabric.rb
@@ -7,9 +7,9 @@ class Libfabric < Formula
 
   bottle do
     cellar :any
-    sha256 "8d2368705fbf1afd50ea10508d987396576fd4b3d43b036c36cb272c3370ffa6" => :catalina
-    sha256 "014f3a1e5aaedfdd997bd4c57324144052c549b5441b70cac0f45c408b7a5177" => :mojave
-    sha256 "e594d5b93f0f2ce539995834049d8d6698114432382ec3d9d93f795c43427615" => :high_sierra
+    sha256 "abf3b135a763ce34e818435e3b3b071f4c9aeea48d62bf22ffdae8432a55103e" => :catalina
+    sha256 "fdeab4a76a5ee87946a2d76b9e4a8f3b8960c8d3d2fee0d1058a5363ed3f38b8" => :mojave
+    sha256 "06e8fca360c7be76f060f23569defe285175f8d4dbe889bce0e2fe2d552b1a25" => :high_sierra
   end
 
   depends_on "autoconf" => :build
