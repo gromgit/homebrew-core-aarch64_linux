c2770b1bf7048d2955a427f1771af5da91025650
chenrui <chenrui333@gmail.com>
2020-05-09 11:58:15 -0400

    clojurescript 1.10.758 (#54448)

--- Formula/clojurescript.rb
+++ Formula/clojurescript.rb
@@ -1,8 +1,8 @@
 class Clojurescript < Formula
   desc "Clojure to JS compiler"
   homepage "https://github.com/clojure/clojurescript"
-  url "https://github.com/clojure/clojurescript/releases/download/r1.10.742/cljs.jar"
-  sha256 "4c003d7ccc4d6e49bd7a6b15dd7d0f1499df995715733def881374d8de0a70b9"
+  url "https://github.com/clojure/clojurescript/releases/download/r1.10.758/cljs.jar"
+  sha256 "dcc98e103d281d4eab21ca94fba11728e9f587c3aa09c8ffc3b96cff210adcce"
   head "https://github.com/clojure/clojurescript.git"
 
   bottle :unneeded
