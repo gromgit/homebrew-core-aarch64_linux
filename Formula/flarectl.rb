2aa886e2ce5d8be292583478fa66bf23f049f408
chenrui <chenrui333@gmail.com>
2020-05-09 05:05:03 +0000

    flarectl: add 0.11.7 bottle.

--- Formula/flarectl.rb
+++ Formula/flarectl.rb
@@ -5,6 +5,13 @@ class Flarectl < Formula
   sha256 "d35d07833cb61d290750882c083c16a77860eb4615c5e14d3315f21a200f4662"
   head "https://github.com/cloudflare/cloudflare-go.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "263f404a08fc223ebb4c540ef788674760072c92fb6297f20572681ba8306a31" => :catalina
+    sha256 "72823f606afafa9c37ba7499dca4c1167e50411f46a51d60f97574e894bc0894" => :mojave
+    sha256 "3e41c3f80638f768b61f0d9447851ca3231cd2a1ae93d7afa5f205ca64c41db3" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
