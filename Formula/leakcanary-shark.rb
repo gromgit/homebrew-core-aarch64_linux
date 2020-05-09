bc324cfdd0ecbda769db621bc5d727c5c108dfe6
ckesc <iamckesc@gmail.com>
2020-05-09 08:16:31 +0300

    leakcanary-shark 2.2 (new formula) (#53224)
    * Add LeakCanary's Shark CLI
    
    * Depend on openjdk
    
    * Test via real heap dump
    
    * Fix lint issues

---
+++ Formula/leakcanary-shark.rb
@@ -0,0 +1,30 @@
+class LeakcanaryShark < Formula
+  desc "CLI Java memory leak explorer for LeakCanary"
+  homepage "https://square.github.io/leakcanary/shark/"
+  url "https://github.com/square/leakcanary/releases/download/v2.2/shark-cli-2.2.zip"
+  sha256 "4d8aaeb68e2783217267109f5d0c7bcafc78607b54b248e3f3214c07777fd278"
+
+  bottle :unneeded
+
+  depends_on "openjdk"
+
+  resource "sample_hprof" do
+    url "https://github.com/square/leakcanary/raw/v2.2/shark-android/src/test/resources/leak_asynctask_m.hprof"
+    sha256 "7575158108b701e0f7233bc208decc243e173c75357bf0be9231a1dcb5b212ab"
+  end
+
+  def install
+    # Remove Windows scripts
+    rm_rf Dir["bin/*.bat"]
+
+    libexec.install Dir["*"]
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    resource("sample_hprof").stage do
+      assert_match "1 APPLICATION LEAKS",
+                   shell_output("#{bin}/shark-cli --hprof ./leak_asynctask_m.hprof analyze").strip
+    end
+  end
+end
