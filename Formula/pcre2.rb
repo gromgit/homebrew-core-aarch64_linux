c0406d10cdb078485999120d05f91705a527187d
chenrui <chenrui333@gmail.com>
2020-05-10 19:45:57 +0000

    pcre2: update 10.35 bottle.

--- Formula/pcre2.rb
+++ Formula/pcre2.rb
@@ -7,9 +7,9 @@ class Pcre2 < Formula
 
   bottle do
     cellar :any
-    sha256 "af3bf030a455daf0a560f8d9e433f7a803d7150933895699f311d661c33b9729" => :catalina
-    sha256 "9bc0815c6c4c584ef16e93e5ecf37aa786303d88f9321274a29b4f60876d583f" => :mojave
-    sha256 "494acfae91394df3c48b2bdb13d46b5a540e401c54be9f7c10234e6001fba678" => :high_sierra
+    sha256 "6a1e59a5db23d684f92d2bf695601d1b466f3e9d5407f704ba4679d885d13cef" => :catalina
+    sha256 "d7be9b0193654484e40bc30dd330711cc1e72fa9bf29f854dd50458f6a827d1b" => :mojave
+    sha256 "69c4fb400d19d1910df33376974b274362ed715ab7d67ee480b5211156174784" => :high_sierra
   end
 
   uses_from_macos "bzip2"
