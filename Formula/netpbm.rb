44c04e64f9e1b323f882e59328b610f93083d9d3
Rui Chen <chenrui333@gmail.com>
2020-05-10 10:36:34 -0400

    netpbm 10.86.13
    Closes #54516.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/netpbm.rb
+++ Formula/netpbm.rb
@@ -3,8 +3,8 @@ class Netpbm < Formula
   homepage "https://netpbm.sourceforge.io/"
   # Maintainers: Look at https://sourceforge.net/p/netpbm/code/HEAD/tree/
   # for stable versions and matching revisions.
-  url "https://svn.code.sf.net/p/netpbm/code/stable", :revision => 3779
-  version "10.86.12"
+  url "https://svn.code.sf.net/p/netpbm/code/stable", :revision => 3806
+  version "10.86.13"
   version_scheme 1
   head "https://svn.code.sf.net/p/netpbm/code/trunk"
 
