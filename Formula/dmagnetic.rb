492e14feae18394872721b24e79a2b9a21cfe684
chenrui <chenrui333@gmail.com>
2020-05-10 00:22:06 +0000

    dmagnetic: add 0.22 bottle.

--- Formula/dmagnetic.rb
+++ Formula/dmagnetic.rb
@@ -4,6 +4,13 @@ class Dmagnetic < Formula
   url "https://www.dettus.net/dMagnetic/dMagnetic_0.22.tar.bz2"
   sha256 "53905c8a1a69c5c69a7afcac5d0368edc6c214fc603f3d76313e4f33faeb0431"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e852de4f215f98be5c5a8b661af2b1169f41d0ca96f0060f4a45a3cf14d853ab" => :catalina
+    sha256 "3ded9902642fbce1800f3516fce3766ed39762791453d6b12d945af1095f5cfe" => :mojave
+    sha256 "86336f5f6d27927b9915dc73eded254ca529c06d7cdd75f95a94115762d4a162" => :high_sierra
+  end
+
   def install
     system "make", "PREFIX=#{prefix}", "install"
     (share/"games/dMagnetic").install "testcode/minitest.mag", "testcode/minitest.gfx"
