6c46baab635b89c959c87fb24cd6f9e21391d9ea
Monson Shao <holymonson@gmail.com>
2020-05-11 22:35:11 +0800

    z.lua 1.8.4 (new formula)
    Closes #54579.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Seeker <meaningseeking@protonmail.com>

---
+++ Formula/z.lua.rb
@@ -0,0 +1,20 @@
+class ZLua < Formula
+  desc "New cd command that helps you navigate faster by learning your habits"
+  homepage "https://github.com/skywind3000/z.lua"
+  url "https://github.com/skywind3000/z.lua/archive/1.8.4.tar.gz"
+  sha256 "ae26580b5e0a3b3d82a932804e0d2cd9ead44cbb49edb3360d3c00d4ae12f06b"
+  head "https://github.com/skywind3000/z.lua.git"
+
+  bottle :unneeded
+
+  depends_on "lua"
+
+  def install
+    pkgshare.install "z.lua", "z.lua.plugin.zsh", "init.fish"
+    doc.install "README.md", "LICENSE"
+  end
+
+  test do
+    system "lua", "#{opt_pkgshare}/z.lua", "."
+  end
+end
