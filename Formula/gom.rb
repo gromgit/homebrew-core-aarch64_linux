10f3679cfc77b4b1ae01cd0baf2d703be6285c10
Alexander Bayandin <a.bayandin@gmail.com>
2020-05-11 11:34:38 +0100

    gom: revision bump for python@3.8

--- Formula/gom.rb
+++ Formula/gom.rb
@@ -3,6 +3,7 @@ class Gom < Formula
   homepage "https://wiki.gnome.org/Projects/Gom"
   url "https://download.gnome.org/sources/gom/0.4/gom-0.4.tar.xz"
   sha256 "68d08006aaa3b58169ce7cf1839498f45686fba8115f09acecb89d77e1018a9d"
+  revision 1
 
   bottle do
     cellar :any
@@ -15,17 +16,16 @@ class Gom < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "python" => :build
+  depends_on "python@3.8" => :build
   depends_on "gdk-pixbuf"
   depends_on "gettext"
   depends_on "glib"
 
   def install
-    pyver = Language::Python.major_minor_version "python3"
+    pyver = Language::Python.major_minor_version Formula["python@3.8"].opt_bin/"python3"
 
     mkdir "build" do
-      system "meson", *std_meson_args,
-        "-Dpygobject-override-dir=#{lib}/python#{pyver}/site-packages", ".."
+      system "meson", *std_meson_args, "-Dpygobject-override-dir=#{lib}/python#{pyver}/site-packages", ".."
       system "ninja"
       system "ninja", "install"
     end
