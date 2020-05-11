f916ff9041bf675a4f0648c76b7b4d914f3c94d6
pyed <pyed@fastmail.com>
2020-05-11 18:09:06 +0300

    libtorrent-rakshasa 0.13.8 (new formula)
    Closes #54572.
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

---
+++ Formula/libtorrent-rakshasa.rb
@@ -0,0 +1,38 @@
+class LibtorrentRakshasa < Formula
+  desc "BitTorrent library with a focus on high performance"
+  homepage "https://github.com/rakshasa/libtorrent"
+  url "https://github.com/rakshasa/libtorrent/archive/v0.13.8.tar.gz"
+  sha256 "0f6c2e7ffd3a1723ab47fdac785ec40f85c0a5b5a42c1d002272205b988be722"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "openssl@1.1"
+
+  conflicts_with "libtorrent-rasterbar",
+    :because => "they both use the same libname"
+
+  def install
+    args = ["--prefix=#{prefix}", "--disable-debug",
+            "--disable-dependency-tracking"]
+
+    system "sh", "autogen.sh"
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <string>
+      #include <torrent/torrent.h>
+      int main(int argc, char* argv[])
+      {
+        return strcmp(torrent::version(), argv[1]);
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-o", "test", "-L#{lib}", "-ltorrent"
+    system "./test", version.to_s
+  end
+end
