6dece745b7e55aa30920d90f467a6d03fed329b8
chenrui <chenrui333@gmail.com>
2020-05-10 21:56:17 -0400

    vfuse 2.2.6 (#54541)
    patch the version

--- Formula/vfuse.rb
+++ Formula/vfuse.rb
@@ -1,12 +1,17 @@
 class Vfuse < Formula
   desc "Convert bootable DMG images for use in VMware Fusion"
   homepage "https://github.com/chilcote/vfuse"
-  url "https://github.com/chilcote/vfuse/archive/2.2.5.tar.gz"
-  sha256 "040997cacacf54d61e98cc7268c1c393c04b51fda3bf80fe3d4191acee9c2bb9"
+  url "https://github.com/chilcote/vfuse/archive/2.2.6.tar.gz"
+  sha256 "fbf5f8a1c664b03c7513a70aa05c3fc501a7ebdb53f128f1f05c24395871a314"
 
   bottle :unneeded
 
   def install
+    # Fix upstream artifact packaging issue
+    # remove in the next release
+    inreplace "Makefile", "2.2.5", "2.2.6"
+    inreplace "pkgroot/usr/local/vfuse/bin/vfuse", "2.2.5", "2.2.6"
+
     bin.install Dir["pkgroot/usr/local/vfuse/bin/*"]
   end
 
