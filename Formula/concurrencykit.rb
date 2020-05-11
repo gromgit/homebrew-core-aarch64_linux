7935931328910e72b38cf9b2bd4ed9947c42bb22
chenrui <chenrui333@gmail.com>
2020-05-11 15:05:28 +0000

    concurrencykit: update 0.7.0 bottle.

--- Formula/concurrencykit.rb
+++ Formula/concurrencykit.rb
@@ -10,12 +10,9 @@ class Concurrencykit < Formula
 
   bottle do
     cellar :any
-    sha256 "5f9f70680563a29f6575b2dd64e6d6b9f84d31d207a9c96af2d5e38ae69a289f" => :catalina
-    sha256 "d219f60638ce9501978e8494b64eef8861685f78c9e3eeefa295043a05ba75a2" => :mojave
-    sha256 "4bb00e2cc25ebe7e103ca8923c3376e86b3b7b360fc73beb8078d15af1239571" => :high_sierra
-    sha256 "1597c3fde162ccc3c8c729003da472f3f414509b18a2e64a1fade268ee8798e0" => :sierra
-    sha256 "897667302b03467c291ff141082b21ec2f31fc82ef5940f791196a14cec24909" => :el_capitan
-    sha256 "914d6e5afd3412f8892770f73233e1cca915b2a2315c811fc6a8d6fa5ab811ce" => :yosemite
+    sha256 "12788eda54c82375102f2f33e28f533151b4d3f6fa9042ea7c7aa03fb25c3e3a" => :catalina
+    sha256 "357062d84adf8f0c8d1a92a7fda6fb9278b6264edf061935d595342e0c334aed" => :mojave
+    sha256 "c8def7655e173d45ff8ec94bc78750507235fa9eb8b79be639bf250698d59761" => :high_sierra
   end
 
   def install
