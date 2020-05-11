d99244c5f21e25366669a46d91a465cf465acf66
Sean Molenaar <smillerdev@me.com>
2020-05-11 20:13:22 +0200

    gnutls: revision bump

--- Formula/gnutls.rb
+++ Formula/gnutls.rb
@@ -4,7 +4,7 @@ class Gnutls < Formula
   url "https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/gnutls-3.6.13.tar.xz"
   mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gnutls/v3.6/gnutls-3.6.13.tar.xz"
   sha256 "32041df447d9f4644570cf573c9f60358e865637d69b7e59d1159b7240b52f38"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "d1556519600c746ffda9937319134d9209d5e0a573be192b58ff81215add4e16" => :catalina
