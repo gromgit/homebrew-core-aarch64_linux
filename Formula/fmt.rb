e92a811fe9f281272fcd764d045d7ba67a1cadfc
EricFromCanada <enk3@outlook.com>
2020-05-11 14:27:39 -0400

    fmt 6.2.1
    Closes #54586.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fmt.rb
+++ Formula/fmt.rb
@@ -1,8 +1,8 @@
 class Fmt < Formula
   desc "Open-source formatting library for C++"
   homepage "https://fmtlib.github.io/"
-  url "https://github.com/fmtlib/fmt/archive/6.2.0.tar.gz"
-  sha256 "fe6e4ff397e01c379fc4532519339c93da47404b9f6674184a458a9967a76575"
+  url "https://github.com/fmtlib/fmt/archive/6.2.1.tar.gz"
+  sha256 "5edf8b0f32135ad5fafb3064de26d063571e95e8ae46829c2f4f4b52696bbff0"
 
   bottle do
     cellar :any_skip_relocation
