06cc6851a88a1f21cc0f776b11b699a80ba18dda
Leopoldo Pla <lpla@dlsi.ua.es>
2020-05-12 21:48:51 +0200

    sdl: only patch stable
    Update sdl.rb
    
    Moved sdl1.2 macOS patches to stable compilation
    
    Update sdl.rb
    
    Trying to fix some CI structure errors
    
    Update sdl.rb
    
    Closes #54630.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sdl.rb
+++ Formula/sdl.rb
@@ -1,9 +1,32 @@
 class Sdl < Formula
   desc "Low-level access to audio, keyboard, mouse, joystick and graphics"
   homepage "https://www.libsdl.org/"
-  url "https://www.libsdl.org/release/SDL-1.2.15.tar.gz"
-  sha256 "d6d316a793e5e348155f0dd93b979798933fb98aa1edebcc108829d6474aad00"
   revision 1
+
+  stable do
+    url "https://www.libsdl.org/release/SDL-1.2.15.tar.gz"
+    sha256 "d6d316a793e5e348155f0dd93b979798933fb98aa1edebcc108829d6474aad00"
+    # Fix for a bug preventing SDL from building at all on OSX 10.9 Mavericks
+    # Related ticket: https://bugzilla.libsdl.org/show_bug.cgi?id=2085
+    patch do
+      url "https://bugzilla-attachments.libsdl.org/attachment.cgi?id=1320"
+      sha256 "ba0bf2dd8b3f7605db761be11ee97a686c8516a809821a4bc79be738473ddbf5"
+    end
+
+    # Fix compilation error on 10.6 introduced by the above patch
+    patch do
+      url "https://bugzilla-attachments.libsdl.org/attachment.cgi?id=1324"
+      sha256 "ee7eccb51cefff15c6bf8313a7cc7a3f347dc8e9fdba7a3c3bd73f958070b3eb"
+    end
+
+    # Fix mouse cursor transparency on 10.13, https://bugzilla.libsdl.org/show_bug.cgi?id=4076
+    if MacOS.version == :high_sierra
+      patch do
+        url "https://bugzilla-attachments.libsdl.org/attachment.cgi?id=3721"
+        sha256 "954875a277d9246bcc444b4e067e75c29b7d3f3d2ace5318a6aab7d7a502f740"
+      end
+    end
+  end
 
   bottle do
     cellar :any
@@ -19,27 +42,6 @@ class Sdl < Formula
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
-  end
-
-  # Fix for a bug preventing SDL from building at all on OSX 10.9 Mavericks
-  # Related ticket: https://bugzilla.libsdl.org/show_bug.cgi?id=2085
-  patch do
-    url "https://bugzilla-attachments.libsdl.org/attachment.cgi?id=1320"
-    sha256 "ba0bf2dd8b3f7605db761be11ee97a686c8516a809821a4bc79be738473ddbf5"
-  end
-
-  # Fix compilation error on 10.6 introduced by the above patch
-  patch do
-    url "https://bugzilla-attachments.libsdl.org/attachment.cgi?id=1324"
-    sha256 "ee7eccb51cefff15c6bf8313a7cc7a3f347dc8e9fdba7a3c3bd73f958070b3eb"
-  end
-
-  # Fix mouse cursor transparency on 10.13, https://bugzilla.libsdl.org/show_bug.cgi?id=4076
-  if MacOS.version == :high_sierra
-    patch do
-      url "https://bugzilla-attachments.libsdl.org/attachment.cgi?id=3721"
-      sha256 "954875a277d9246bcc444b4e067e75c29b7d3f3d2ace5318a6aab7d7a502f740"
-    end
   end
 
   def install
