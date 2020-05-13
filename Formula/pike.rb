cc8eef51a2930a167c85f775b62ad7bf03614761
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-13 07:07:33 +0000

    pike: update 8.0.702_1 bottle.

--- Formula/pike.rb
+++ Formula/pike.rb
@@ -7,10 +7,9 @@ class Pike < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "f299725be9679d3be3abcb580dc81c6512ae66e1a1a3b2034e329212899cfcff" => :catalina
-    sha256 "f588f3ad79e59c90e7339fba213f67102b7040bb42fad2437ec4d3094e636295" => :mojave
-    sha256 "5d67e09c9bb49b77a1a670591d6d2c1f8f039efe4b2b18c50e0f8941c072e976" => :high_sierra
+    sha256 "ae20ba3c7fd69c026892555798559bd2da90d53dc3cf07eb5d7423af505082d5" => :catalina
+    sha256 "ff1e2f11d0beec51cc41d9eb566a80cbf53b51933158c6083054e3b91dfa251c" => :mojave
+    sha256 "10eb373d72d3c178dc1f560a7e1c35f8b6dc51412d7eb4f4cf4f751109d4fd9d" => :high_sierra
   end
 
   depends_on "gmp"
