d65473d6f6a8066b9bbc9941b926c6525e7a05df
EricFromCanada <enk3@outlook.com>
2020-05-13 09:50:04 -0400

    vips: revision bump
    Closes #54404.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vips.rb
+++ Formula/vips.rb
@@ -3,6 +3,7 @@ class Vips < Formula
   homepage "https://github.com/libvips/libvips"
   url "https://github.com/libvips/libvips/releases/download/v8.9.2/vips-8.9.2.tar.gz"
   sha256 "ae8491b1156cd2eb9cbbaa2fd6caa1dc9ed3ded0b70443d28cd7fea798ab2a27"
+  revision 1
 
   bottle do
     sha256 "f2443f5efc30f35a5dee6412043489bbffdca0a8c304880d9eca05a1b384f38a" => :catalina
