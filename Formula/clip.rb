9a61ef0523fbc39b07db3e3ee700b22e792374d9
Andrew Kane <andrew@chartkick.com>
2020-05-14 06:33:17 -0700

    clip 0.7 (new formula)
    Closes #54612.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/clip.rb
@@ -0,0 +1,28 @@
+class Clip < Formula
+  desc "Create high-quality charts from the command-line"
+  homepage "https://clip-lang.org/"
+  url "https://github.com/asmuth/clip/archive/v0.7.tar.gz"
+  sha256 "f38f455cf3e9201614ac71d8a871e4ff94a6e4cf461fd5bf81bdf457ba2e6b3e"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "cairo"
+  depends_on "fmt"
+  depends_on "freetype"
+  depends_on "fribidi"
+  depends_on "harfbuzz"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    system "make", "install"
+    pkgshare.install "test"
+  end
+
+  test do
+    cp_r pkgshare/"test", testpath
+    system "#{bin}/clip", "--export", "chart.svg",
+           "test/examples/charts_basic_areachart.clp"
+    assert_predicate testpath/"chart.svg", :exist?
+  end
+end
