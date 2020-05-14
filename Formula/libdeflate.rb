a8093cbbec33fa43c4c36d86a9b3e59076dc0637
EricFromCanada <enk3@outlook.com>
2020-05-14 09:29:27 -0400

    libdeflate 1.6
    Closes #54687.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libdeflate.rb
+++ Formula/libdeflate.rb
@@ -1,8 +1,8 @@
 class Libdeflate < Formula
   desc "Heavily optimized DEFLATE/zlib/gzip compression and decompression"
   homepage "https://github.com/ebiggers/libdeflate"
-  url "https://github.com/ebiggers/libdeflate/archive/v1.5.tar.gz"
-  sha256 "45b1b2332f443b705c59d06a49be009827291d2c487b076dc8ec2791eff4c711"
+  url "https://github.com/ebiggers/libdeflate/archive/v1.6.tar.gz"
+  sha256 "60748f3f7b22dae846bc489b22a4f1b75eab052bf403dd8e16c8279f16f5171e"
 
   bottle do
     cellar :any
