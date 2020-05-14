0d534326152d2f57ebdf518ea87492efc9cd0517
Rui Chen <chenrui333@gmail.com>
2020-05-14 00:09:43 -0400

    erlang@22 22.3.4 (new formula)

---
+++ Formula/erlang@22.rb
@@ -0,0 +1,77 @@
+class ErlangAT22 < Formula
+  desc "Programming language for highly scalable real-time systems"
+  homepage "https://www.erlang.org/"
+  # Download tarball from GitHub; it is served faster than the official tarball.
+  url "https://github.com/erlang/otp/archive/OTP-22.3.4.tar.gz"
+  sha256 "79657e07aee0cc174f89c1bd7d8d251295f64144cced6ea72b98777ec6a6660d"
+
+  keg_only :versioned_formula
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "openssl@1.1"
+  depends_on "wxmac" # for GUI apps like observer
+
+  uses_from_macos "m4" => :build
+
+  resource "man" do
+    url "https://www.erlang.org/download/otp_doc_man_22.3.tar.gz"
+    mirror "https://fossies.org/linux/misc/otp_doc_man_22.3.tar.gz"
+    sha256 "43b6d62d9595e1dc51946d55c9528c706c5ae753876b9bf29303b7d11a7ccb16"
+  end
+
+  resource "html" do
+    url "https://www.erlang.org/download/otp_doc_html_22.3.tar.gz"
+    mirror "https://fossies.org/linux/misc/otp_doc_html_22.3.tar.gz"
+    sha256 "9b01c61f2898235e7f6643c66215d6419f8706c8fdd7c3e0123e68960a388c34"
+  end
+
+  def install
+    # Unset these so that building wx, kernel, compiler and
+    # other modules doesn't fail with an unintelligable error.
+    %w[LIBS FLAGS AFLAGS ZFLAGS].each { |k| ENV.delete("ERL_#{k}") }
+
+    # Do this if building from a checkout to generate configure
+    system "./otp_build", "autoconf" if File.exist? "otp_build"
+
+    args = %W[
+      --disable-debug
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --enable-dynamic-ssl-lib
+      --enable-hipe
+      --enable-sctp
+      --enable-shared-zlib
+      --enable-smp-support
+      --enable-threads
+      --enable-wx
+      --with-ssl=#{Formula["openssl@1.1"].opt_prefix}
+      --without-javac
+      --enable-darwin-64bit
+    ]
+
+    args << "--enable-kernel-poll" if MacOS.version > :el_capitan
+    args << "--with-dynamic-trace=dtrace" if MacOS::CLT.installed?
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+
+    (lib/"erlang").install resource("man").files("man")
+    doc.install resource("html")
+  end
+
+  def caveats
+    <<~EOS
+      Man pages can be found in:
+        #{opt_lib}/erlang/man
+
+      Access them with `erl -man`, or add this directory to MANPATH.
+    EOS
+  end
+
+  test do
+    system "#{bin}/erl", "-noshell", "-eval", "crypto:start().", "-s", "init", "stop"
+  end
+end
