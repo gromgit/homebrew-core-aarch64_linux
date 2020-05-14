38680743e3593598424a63841c4dcfdbfa6435fb
Rui Chen <chenrui333@gmail.com>
2020-05-14 18:36:30 -0400

    ghz: add 0.52.0 bottle.

--- Formula/ghz.rb
+++ Formula/ghz.rb
@@ -4,6 +4,13 @@ class Ghz < Formula
   url "https://github.com/bojand/ghz/archive/v0.52.0.tar.gz"
   sha256 "6f7e4e0cd81ad00c2c4e17c93257810e2ea8897f60713352c9d9583231e2739a"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "37baf017141bf81d6607443c034e465b48c4694ff81d59220dd0bad6f0732f13" => :catalina
+    sha256 "fd94085e4d3db4f73e72928cf24ec51085d60f71f54d93800e5cb56fa3bf7405" => :mojave
+    sha256 "5f6f8cc187926b95fcc1ab071d3ca6c8e37af3981b6d3afae9d895b9b1ed0422" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
