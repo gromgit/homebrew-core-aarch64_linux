4509c5fb724ebdca9545ebd06a149d50684dca35
Caleb Xu <calebcenter@live.com>
2020-05-14 01:21:14 -0400

    repo 2.7
    Closes #54676.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/repo.rb
+++ Formula/repo.rb
@@ -2,8 +2,8 @@ class Repo < Formula
   desc "Repository tool for Android development"
   homepage "https://source.android.com/source/developing.html"
   url "https://github.com/GerritCodeReview/git-repo.git",
-      :tag      => "v2.6",
-      :revision => "2fe84e17b923f29139dc6056756ab30078864c18"
+      :tag      => "v2.7",
+      :revision => "dbfbcb14c162ef8233a5a13a462ba86f5b99921a"
   version_scheme 1
 
   bottle :unneeded
