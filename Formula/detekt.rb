2ad65fe3bf22d011865ed84acd556dafc4da5fd2
wasanx25 <wasanx25@gmail.com>
2020-05-14 23:53:48 +0900

    detekt 1.9.0
    Closes #54696.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/detekt.rb
+++ Formula/detekt.rb
@@ -1,8 +1,8 @@
 class Detekt < Formula
   desc "Static code analysis for Kotlin"
   homepage "https://github.com/arturbosch/detekt"
-  url "https://jcenter.bintray.com/io/gitlab/arturbosch/detekt/detekt-cli/1.8.0/detekt-cli-1.8.0-all.jar"
-  sha256 "f287c7a2db2277efa8b6c5246e55ab52dc92e13ef5397ee2651fac42547ffaad"
+  url "https://jcenter.bintray.com/io/gitlab/arturbosch/detekt/detekt-cli/1.9.0/detekt-cli-1.9.0-all.jar"
+  sha256 "7bfea4f7aecb0db4caf613768b531274555fa4f315ebaba04e7649d454d40978"
 
   bottle :unneeded
 
