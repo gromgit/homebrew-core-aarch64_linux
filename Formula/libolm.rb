d87c2e0e3af9da942219af8f1c3706d21822f610
Aaron Raimist <aaron@raim.ist>
2020-05-15 14:46:50 -0500

    libolm 3.1.4 (new formula)
    Signed-off-by: Aaron Raimist <aaron@raim.ist>
    
    Closes #54742.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

---
+++ Formula/libolm.rb
@@ -0,0 +1,37 @@
+class Libolm < Formula
+  desc "Implementation of the Double Ratchet cryptographic ratchet"
+  homepage "https://gitlab.matrix.org/matrix-org/olm"
+  url "https://gitlab.matrix.org/matrix-org/olm/-/archive/3.1.4/olm-3.1.4.tar.gz"
+  sha256 "1ca9926ce71d778fb7352d1ee77513194db8c7f49c0d69d38ac49ec3bafcea38"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", "-Bbuild", "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+    system "cmake", "--build", "build", "--target", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <iostream>
+      #include <vector>
+
+      #include "olm/olm.h"
+
+      using std::cout;
+
+      int main() {
+        void * utility_buffer = malloc(::olm_utility_size());
+        ::OlmUtility * utility = ::olm_utility(utility_buffer);
+
+        uint8_t output[43];
+        ::olm_sha256(utility, "Hello, World", 12, output, 43);
+        cout << output;
+        return 0;
+      }
+    EOS
+
+    system ENV.cc, "test.cpp", "-L#{lib}", "-lolm", "-lstdc++", "-o", "test"
+    assert_equal "A2daxT/5zRU1zMffzfosRYxSGDcfQY3BNvLRmsH76KU", shell_output("./test").strip
+  end
+end
