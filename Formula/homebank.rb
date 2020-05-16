8cfcf5dcbd5afe2df74f0b851a4b1e88f3e0f252
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-16 15:19:37 +0000

    homebank: update 5.4.2 bottle.

--- Formula/homebank.rb
+++ Formula/homebank.rb
@@ -5,9 +5,9 @@ class Homebank < Formula
   sha256 "c6ce84b421e7199ae545ea8b8d981f347af4d67b4cd5912b6789cf1450db722e"
 
   bottle do
-    sha256 "19428dfc7ea700df634f4200cfac4d5dad97e000e6ba82a5803bb6a369875e0a" => :catalina
-    sha256 "6a4b542966723577d7ceeffb20d05bdfa853ffcf93862b7716a0d7cd8ec8e604" => :mojave
-    sha256 "18ce681d781685ca73e785040d4750b447b4ed3c44f7f087c7d4f8525089d9c7" => :high_sierra
+    sha256 "aa749fac81a7659dc5749076e2f6cdf585a06c2b8629ff65cbaa1527aeace397" => :catalina
+    sha256 "d23f83f4aa1a034f41964d65681deb90966712f51bd635b19f2b461b5e0c7327" => :mojave
+    sha256 "417184676c620e0f2ec3f2ea4cf41a834bf018f7478cb98c0b17e374d27f8f44" => :high_sierra
   end
 
   depends_on "intltool" => :build
