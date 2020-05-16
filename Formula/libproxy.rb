0a8a7aa5254ee6a150f5bf2246f19a2085e94bb4
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-05-16 06:50:52 +0000

    libproxy: update 0.4.15_2 bottle.

--- Formula/libproxy.rb
+++ Formula/libproxy.rb
@@ -7,10 +7,9 @@ class Libproxy < Formula
   head "https://github.com/libproxy/libproxy.git"
 
   bottle do
-    sha256 "85c0981635696e9160b6f69ed4396609a4542ad478fdd3ac6b5b43b3c25bee07" => :catalina
-    sha256 "2bd92529540425a786f17b2b2cc10423394c53a6120bbfa7a8d1df29b0617818" => :mojave
-    sha256 "1da068be3ea931eda7ac2f58c8db57d9169d299bc5d57c70b8a455decd351931" => :high_sierra
-    sha256 "e848c71f0cdd15d30a2f8003188883f4cf395034447d8b8db4909db5d51904ea" => :sierra
+    sha256 "fbb6b461e2abfbd8f3c117c64410827fac0759cefab76cbccd4051f9d5b98d9c" => :catalina
+    sha256 "74b3f2231eaaaf6ca8cbcb7868b0cb71a62ed4228b1c6fb81ce1b9548819cdb6" => :mojave
+    sha256 "bf36cc90d464f46a70aca6407df2ea7c7b1b325d29346de3813298016cd0c324" => :high_sierra
   end
 
   depends_on "cmake" => :build
