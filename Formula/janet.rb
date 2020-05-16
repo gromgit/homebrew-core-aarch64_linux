07ea63276f745e1f67239706c64ada74a228732a
Rui Chen <chenrui333@gmail.com>
2020-05-16 14:56:04 -0400

    janet 1.9.1
    remove patch
    
    Closes #54803.
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/janet.rb
+++ Formula/janet.rb
@@ -1,8 +1,8 @@
 class Janet < Formula
   desc "Dynamic language and bytecode vm"
   homepage "https://janet-lang.org"
-  url "https://github.com/janet-lang/janet/archive/v1.9.0.tar.gz"
-  sha256 "30ca113843e597127912719a03a7da5f93fd1cf321245d8dbbcbae64d307ad5c"
+  url "https://github.com/janet-lang/janet/archive/v1.9.1.tar.gz"
+  sha256 "02724d6074a0d6fa53a548e8bdaaf49999f082e30b277c73444900f739a53062"
   head "https://github.com/janet-lang/janet.git"
 
   bottle do
@@ -14,12 +14,6 @@ class Janet < Formula
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
-
-  # should be removed in the next release
-  patch do
-    url "https://github.com/janet-lang/janet/commit/7275370ae563e8bfa7e907c9931bb1e0c88686d3.patch?full_index=1"
-    sha256 "fcd581e38edfc89aef8c4164a7d36da0b55ce6965f85f01bca0ff92e1bf9019e"
-  end
 
   def install
     system "meson", "setup", "build", "--buildtype=release", *std_meson_args
