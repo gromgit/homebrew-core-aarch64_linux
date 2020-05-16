8037f20de4b509c0f93277545619a1d2c8edc4b3
Issy Long <me@issyl0.co.uk>
2020-05-16 18:32:31 +0000

    asuka: add 0.8.0 bottle.

--- Formula/asuka.rb
+++ Formula/asuka.rb
@@ -4,6 +4,13 @@ class Asuka < Formula
   url "https://git.sr.ht/~julienxx/asuka/archive/0.8.0.tar.gz"
   sha256 "c06dc528b8588be4922a7b4357f8e9701b1646db0828ccfcad3a5be178d31582"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e822cdbdf7c6eeaefabde6c69853ed5acdab7b959d44feff9e5c6eb44ea4b75c" => :catalina
+    sha256 "95691f54ea6f8e5e218ec152b0d9685a4a8f4d59075207a1f75958333a174410" => :mojave
+    sha256 "422e2efe84c94e78b3f1ab4bf9928d594c83e09b8ad1e32aabf04b17c2b8bb8a" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   uses_from_macos "ncurses"
