944cbe147b1c5c831d80337e616be38fc91fe171
Dustin Rodrigues <dust.rod@gmail.com>
2020-05-17 09:05:06 -0400

    urh 2.8.8
    Closes #54853.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/urh.rb
+++ Formula/urh.rb
@@ -1,8 +1,8 @@
 class Urh < Formula
   desc "Universal Radio Hacker"
   homepage "https://github.com/jopohl/urh"
-  url "https://files.pythonhosted.org/packages/7a/4b/d343f4cd7409d61cf69fdf9c450fc1b048c24258e248747670f653dba9c7/urh-2.8.7.tar.gz"
-  sha256 "def9be4f4464875549e4d49bf3c7fa4bdc496c1b34e275aa62128b401f8efca6"
+  url "https://files.pythonhosted.org/packages/ef/9c/b023e69e2de551fe2250d2e374a62e41719d6bb491f96c6bc9c689fb1d28/urh-2.8.8.tar.gz"
+  sha256 "8c05637f7090958a03b3c67cb824252fa8a3677622f0c9e60aac91f0d0af07ca"
   head "https://github.com/jopohl/urh.git"
 
   bottle do
@@ -16,7 +16,6 @@ class Urh < Formula
   depends_on "numpy"
   depends_on "pyqt"
   depends_on "python@3.8"
-  depends_on "zeromq"
 
   resource "Cython" do
     url "https://files.pythonhosted.org/packages/99/36/a3dc962cc6d08749aa4b9d85af08b6e354d09c5468a3e0edc610f44c856b/Cython-0.29.17.tar.gz"
@@ -26,11 +25,6 @@ class Urh < Formula
   resource "psutil" do
     url "https://files.pythonhosted.org/packages/c4/b8/3512f0e93e0db23a71d82485ba256071ebef99b227351f0f5540f744af41/psutil-5.7.0.tar.gz"
     sha256 "685ec16ca14d079455892f25bd124df26ff9137664af445563c1bd36629b5e0e"
-  end
-
-  resource "pyzmq" do
-    url "https://files.pythonhosted.org/packages/16/4c/762c2c3063c4d45baf4a49acea7a4f561f7b78a45cd04b58d63f4c5f6b8d/pyzmq-19.0.0.tar.gz"
-    sha256 "5e1f65e576ab07aed83f444e201d86deb01cd27dcf3f37c727bc8729246a60a8"
   end
 
   def install
