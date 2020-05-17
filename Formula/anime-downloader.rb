6a9643f7afae715b3c016e64e51cb881f78db5a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-17 16:06:21 +0000

    anime-downloader: update 4.3.0 bottle.

--- Formula/anime-downloader.rb
+++ Formula/anime-downloader.rb
@@ -9,9 +9,9 @@ class AnimeDownloader < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "499dd80af5f48604bd1dcc4d6368f7b779a81a547094505dd67ab42d0b583c5d" => :catalina
-    sha256 "f512fd4707d4d70894ee25141fda56279f0d999b7c695afba6b94c40bc6eb8c8" => :mojave
-    sha256 "e20fa7e4604b8c9d11a2dd6bf5311683eba99b68c1e827ed5e5d28dc143a1c56" => :high_sierra
+    sha256 "afabaec3f49758f2d6c802a3f6cc732d359bc1aa4f96102d5bbb5db7cb28851f" => :catalina
+    sha256 "7c10ffe30e521687890945769fe95f9d3f08262e8d9a3055072a63b749d9cb26" => :mojave
+    sha256 "842d8ee3c6fc388e9bfb18f9d05f3c07bdeb9aa6b77a218fb05b0803036cd942" => :high_sierra
   end
 
   depends_on "aria2"
