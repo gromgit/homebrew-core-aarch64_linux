b56832aa4a1c759cad9c6da891692e3bc443d781
Issy Long <me@issyl0.co.uk>
2020-05-17 20:44:42 +0000

    rtorrent: add 0.9.8 bottle.

--- Formula/rtorrent.rb
+++ Formula/rtorrent.rb
@@ -4,6 +4,13 @@ class Rtorrent < Formula
   url "https://github.com/rakshasa/rtorrent/releases/download/v0.9.8/rtorrent-0.9.8.tar.gz"
   sha256 "9edf0304bf142215d3bc85a0771446b6a72d0ad8218efbe184b41e4c9c7542af"
 
+  bottle do
+    cellar :any
+    sha256 "2040210e9850a74760c06cd96dab5e7dfbec58187e768127e657216f51f30e87" => :catalina
+    sha256 "1e9e0d6926724f70776d19c39377c422e17cfa61729043853b48159e2df779c0" => :mojave
+    sha256 "079eb5eee6d8844045cf37f88f153d0be37731caa53768a50799acbd13a2ef6b" => :high_sierra
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
