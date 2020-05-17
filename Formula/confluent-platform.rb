7f89ab534604150102f4f265947afe73c49cc9bd
abicky <takeshi.arabiki@gmail.com>
2020-05-17 20:05:23 +0900

    confluent-platform: Support the execuatble "confluent"
    This commit resolves the following error:
    
    > confluentinc/cli crit executable /usr/local/bin/../libexec/cli/
    > darwin_amd64/confluent does not exist.  Make sure this script
    > is up-to-date and resides at bin/confluent in the Confluent
    > Platform release directory.
    
    The executable "confluent" tries to execute the executable
    "${BASH_SOURCE%/*}/../libexec/cli/${OS}_${ARCH}/${BINARY}" in it,
    so all files should be installed to libexec like other Java
    projects.

--- Formula/confluent-platform.rb
+++ Formula/confluent-platform.rb
@@ -12,13 +12,23 @@ class ConfluentPlatform < Formula
   conflicts_with "kafka", :because => "kafka also ships with identically named Kafka related executables"
 
   def install
-    prefix.install "bin"
-    rm_rf "#{bin}/windows"
-    prefix.install "etc"
-    prefix.install "share"
+    libexec.install %w[bin etc libexec share]
+    rm_rf libexec/"bin/windows"
+
+    bin.write_exec_script Dir["#{libexec}/bin/*"]
   end
 
   test do
+    require "open3"
+
     assert_match version.to_s, shell_output("#{bin}/kafka-broker-api-versions --version")
+
+    # The executable "confluent" tries to create .confluent under the home directory
+    # without considering the envrionment variable "HOME", so the execution will fail
+    # due to sandbox-exec.
+    # The message "unable to load config" means that the execution will succeed
+    # if the user has write permission.
+    err, = Open3.capture2e("#{bin}/confluent")
+    assert_match /\Aunable to load config/, err
   end
 end
