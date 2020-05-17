be55163b850be9a68facc4de963e82fe1fe6af7e
chenrui <chenrui333@gmail.com>
2020-05-17 16:17:44 +0000

    libhttpserver: update 0.18.0 bottle.

--- Formula/libhttpserver.rb
+++ Formula/libhttpserver.rb
@@ -7,10 +7,9 @@ class Libhttpserver < Formula
 
   bottle do
     cellar :any
-    sha256 "e0166077c271995554782fdbf558be98893e2ef64aadbcec200398b81db87f3d" => :catalina
-    sha256 "086dcf919fa2afba8883e7d6c26bdd9823d8741b93ffe3864226534f71a218d9" => :mojave
-    sha256 "948c65b78b36e0baf3682fb94459c33ae5283b811b836b7f4abdf27a94aa8859" => :high_sierra
-    sha256 "59d96bc3f9f33c84c0700318deed666528a9ee60da8a55d45fd3450185ecfed0" => :sierra
+    sha256 "95189595f6b252fb49de7d388416fca1c7107347d2e72401cc2f59dff9bb61ab" => :catalina
+    sha256 "a7ba4e925c9e5597721523f528f16e76c427a54f3986ffa213f3c66c62e60a83" => :mojave
+    sha256 "190a74ca1999a1e42877971ee39b1f79f2ef5742ced8db46c2115d4fa5729d9c" => :high_sierra
   end
 
   depends_on "autoconf" => :build
