a64b46988dafaf45eab3a508aae854158f4f3097
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-17 00:09:35 +0000

    singular: update 4.1.3p1 bottle.

--- Formula/singular.rb
+++ Formula/singular.rb
@@ -6,9 +6,9 @@ class Singular < Formula
   sha256 "5c527d8ed244a4ae74cb98b718deac58c09b0bd75d6ed7fc1ded1ec865ae02b8"
 
   bottle do
-    sha256 "0a6accd0f1c272885fd7a680c345b32001d74b59f2a8b59c4922c76ce2db0e6b" => :catalina
-    sha256 "d36ad45835c2a4dc3bf019bbe6c6a046c5808cf0ed4af621de80f4d4816c46fa" => :mojave
-    sha256 "81d7c0c02e3cc3e9870a6b8ae898f20dafd9f50fdecad129909c8e4bdc984dbf" => :high_sierra
+    sha256 "cfae6f7caed42457b53162e4b8bb1ad2bb6fed62eeaf0ebe010a6078ed648553" => :catalina
+    sha256 "a1ef3230103e83b912220afbd87d055174c0261857068498cb3ea85598523ea8" => :mojave
+    sha256 "bbc3ef698536d7f5d3beb0be0daf667d71e6257b6a290b6ac3b1a6008b2ab6c7" => :high_sierra
   end
 
   head do
