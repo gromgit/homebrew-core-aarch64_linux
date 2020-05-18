79574c1f7a119be8edb813a48b864caab023327c
Dustin Rodrigues <dust.rod@gmail.com>
2020-05-18 10:42:19 -0400

    libelf: fix url (#54887)

--- Formula/libelf.rb
+++ Formula/libelf.rb
@@ -1,7 +1,7 @@
 class Libelf < Formula
   desc "ELF object file access library"
   homepage "https://web.archive.org/web/20181111033959/www.mr511.de/software/english.html"
-  url "https://web.archive.org/web/20181111033959/www.mr511.de/software/libelf-0.8.13.tar.gz"
+  url "https://dl.bintray.com/homebrew/mirror/libelf-0.8.13.tar.gz" # using this url since original source is offline ( https://github.com/Homebrew/homebrew-core/pull/54887 )
   sha256 "591a9b4ec81c1f2042a97aa60564e0cb79d041c52faa7416acb38bc95bd2c76d"
   revision 1
 
