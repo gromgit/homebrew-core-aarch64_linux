3926ce8e8713e6bec1c3e7dc1fcf651adb881d72
Jonathan Chang <jonathan.chang@monash.edu>
2020-05-18 10:43:49 +0000

    spotify-tui: add 0.19.0 bottle.

--- Formula/spotify-tui.rb
+++ Formula/spotify-tui.rb
@@ -5,6 +5,13 @@ class SpotifyTui < Formula
   sha256 "eb2c8e8cb5f7778755589eebab91118be6eaa33a2289b058007af11666bffb84"
   head "https://github.com/Rigellute/spotify-tui.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "358ad4bae54d211d8be22d5ca3f46a4b15ab4a99b88c46016c73a4522cf2d5ca" => :catalina
+    sha256 "7bddf8a63a41404024495aa504a7a08829998b377a1f7e28814d4acb98df00a7" => :mojave
+    sha256 "ea964c3ee56fa603461204e613c96c3733bf1c283199da1d5ecfcbcfef7bfdcf" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
