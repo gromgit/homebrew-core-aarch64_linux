c68646aa988a9c2dd8de8b908e34440cdfc5a3d6
Steve Ayers <sayers@uber.com>
2020-05-19 14:27:38 -0400

    prototool 1.10.0
    Closes #54984.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/prototool.rb
+++ Formula/prototool.rb
@@ -1,12 +1,11 @@
 class Prototool < Formula
   desc "Your Swiss Army Knife for Protocol Buffers"
   homepage "https://github.com/uber/prototool"
-  url "https://github.com/uber/prototool/archive/v1.9.0.tar.gz"
-  sha256 "5f549c2c0c36f938b7d38d1fdec1deeb891ea10d534ee0e6a56ee7f9f746e89c"
+  url "https://github.com/uber/prototool/archive/v1.10.0.tar.gz"
+  sha256 "5b516418f41f7283a405bf4a8feb2c7034d9f3d8c292b2caaebcd218581d2de4"
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
     sha256 "212ef8fee4b629c635cecdd9d62dd444a96d4e24047f420d6069b0738fc2ab16" => :catalina
     sha256 "52f5d535cefcc43527411992d98de7a80eb923815f249d1e744f62c87b25ce22" => :mojave
     sha256 "fb2169f2abe3585e18aaf7fd375eb3278d5a46b8e76a4092d8674981197f3e4c" => :high_sierra
