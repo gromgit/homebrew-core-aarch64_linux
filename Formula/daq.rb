87457d640fd65d58dda66a58bb59f84f397be918
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-19 15:06:25 +0000

    daq: update 2.0.7 bottle.

--- Formula/daq.rb
+++ Formula/daq.rb
@@ -7,13 +7,9 @@ class Daq < Formula
 
   bottle do
     cellar :any
-    sha256 "734f5ff5e9df559b6a807b88b6987376ed12b570fc06f59b60ff4ce71663d23e" => :catalina
-    sha256 "0cc2e4509d68cc4c8b59e0e398003d7f57d50d8c85e4f4c66fc943b215da4075" => :mojave
-    sha256 "d01c68e8ece0df01a1132b9591dad43a84381e601848915972fdbe9497ecada2" => :high_sierra
-    sha256 "f0be58035bc6f4764567cf186673035818e6025d027695795f959fdfc88c7806" => :sierra
-    sha256 "9c2720bd46954e9f2631801d8f8283974436a82827f01c9e954e319f0b9f7e88" => :el_capitan
-    sha256 "02d198f42f56471feaf127824230d7ea752490b3c7f5a34f8b50ff0a85062f01" => :yosemite
-    sha256 "8ce4fbbbb9f6189f6ee51d3223a81ebc7ea76069353bd284822989d6ccc364a5" => :mavericks
+    sha256 "3b1f25eab6e2c04f4b5e609a1d3e72c3eb55eb12d4a7acb61f43ae815bd10347" => :catalina
+    sha256 "8d57a1f8536259612d6ce312b54a96e8d0fd5527000593d11765baf095d1fd2d" => :mojave
+    sha256 "861fbfd197f0cef898687b427cfa259d6dbf15b2eace0036477910177b8c4c16" => :high_sierra
   end
 
   depends_on "autoconf" => :build
