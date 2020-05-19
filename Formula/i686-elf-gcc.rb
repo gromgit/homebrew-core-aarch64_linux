76d6ad819ce20acc16ed1edea114844b167e14bd
Roman Bolshakov <roolebo@gmail.com>
2020-05-19 05:50:41 +0300

    i686-elf-gcc 9.3.0 (new formula)
    The formula provides GNU C/C++ compilers for generic ELF target on Intel
    32-bit CPUs. Canonical target name is i686-pc-elf.
    
    It's different from x86_64-elf-gcc. Certain operations like division of
    signed and unsigned long are not native for i686 and provided by libgcc:
      https://gcc.gnu.org/onlinedocs/gccint/Integer-library-routines.html
    
    E.g. libgcc in x86_64-elf-gcc does not have the functions:
      __umoddi3
      __udivdi3
    
    Therefore an attempt to compile code with -m32 that uses division of
    unsigned long will fail on x86_64-elf-gcc.
    
    Closes #54946.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/i686-elf-gcc.rb
@@ -0,0 +1,48 @@
+class I686ElfGcc < Formula
+  desc "The GNU compiler collection for i686-elf"
+  homepage "https://gcc.gnu.org"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-9.3.0/gcc-9.3.0.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gcc/gcc-9.3.0/gcc-9.3.0.tar.xz"
+  sha256 "71e197867611f6054aa1119b13a0c0abac12834765fe2d81f35ac57f84f742d1"
+
+  depends_on "gmp"
+  depends_on "i686-elf-binutils"
+  depends_on "libmpc"
+  depends_on "mpfr"
+
+  def install
+    mkdir "i686-elf-gcc-build" do
+      system "../configure", "--target=i686-elf",
+                             "--prefix=#{prefix}",
+                             "--infodir=#{info}/i686-elf-gcc",
+                             "--disable-nls",
+                             "--without-isl",
+                             "--without-headers",
+                             "--with-as=#{Formula["i686-elf-binutils"].bin}/i686-elf-as",
+                             "--with-ld=#{Formula["i686-elf-binutils"].bin}/i686-elf-ld",
+                             "--enable-languages=c,c++",
+                             "SED=/usr/bin/sed"
+      system "make", "all-gcc"
+      system "make", "install-gcc"
+      system "make", "all-target-libgcc"
+      system "make", "install-target-libgcc"
+
+      # FSF-related man pages may conflict with native gcc
+      (share/"man/man7").rmtree
+    end
+  end
+
+  test do
+    (testpath/"test-c.c").write <<~EOS
+      int main(void)
+      {
+        int i=0;
+        while(i<10) i++;
+        return i;
+      }
+    EOS
+    system "#{bin}/i686-elf-gcc", "-c", "-o", "test-c.o", "test-c.c"
+    assert_match "file format elf32-i386",
+      shell_output("#{Formula["i686-elf-binutils"].bin}/i686-elf-objdump -a test-c.o")
+  end
+end
