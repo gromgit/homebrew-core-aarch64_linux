a537e63247b2fa50b46572ae396494d98c6ed8f9
chenrui <chenrui333@gmail.com>
2020-05-18 22:51:40 -0400

    tomcat@7 7.0.104 (#54936)

--- Formula/tomcat@7.rb
+++ Formula/tomcat@7.rb
@@ -1,9 +1,9 @@
 class TomcatAT7 < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-7/v7.0.103/bin/apache-tomcat-7.0.103.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-7/v7.0.103/bin/apache-tomcat-7.0.103.tar.gz"
-  sha256 "121dcefa2312ec77cd1ef27b085f4b3c913cde7ee67470d582154d845fd94754"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-7/v7.0.104/bin/apache-tomcat-7.0.104.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-7/v7.0.104/bin/apache-tomcat-7.0.104.tar.gz"
+  sha256 "0cbc72a05f62978e0b78ce4acf1bb7aa720bfbbabc8a11e11930d73285aea73e"
 
   bottle :unneeded
 
