34f111cf80d4df387e64ffe7f22be7c853166d23
Rui Chen <chenrui333@gmail.com>
2020-05-19 14:26:14 -0400

    fd 8.1.0
    Closes #54982.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/fd.rb
+++ Formula/fd.rb
@@ -1,8 +1,8 @@
 class Fd < Formula
   desc "Simple, fast and user-friendly alternative to find"
   homepage "https://github.com/sharkdp/fd"
-  url "https://github.com/sharkdp/fd/archive/v8.0.0.tar.gz"
-  sha256 "fba93204c10266317e0981914c630b08e12cd322c75ff2a2e504ff1dce17d557"
+  url "https://github.com/sharkdp/fd/archive/v8.1.0.tar.gz"
+  sha256 "a58f0d74533a6e79a955c961c7228c53abfa3ca56051713be2395e56ac7212ce"
   head "https://github.com/sharkdp/fd.git"
 
   bottle do
@@ -13,10 +13,6 @@ class Fd < Formula
   end
 
   depends_on "rust" => :build
-
-  # Work around issue with rust/jemalloc on Catalina
-  # https://github.com/sharkdp/fd/issues/498
-  patch :DATA
 
   def install
     ENV["SHELL_COMPLETIONS_DIR"] = buildpath
@@ -33,32 +29,3 @@ class Fd < Formula
     assert_equal "test_file", shell_output("#{bin}/fd test").chomp
   end
 end
-__END__
-diff -pur a/Cargo.toml b/Cargo.toml
---- a/Cargo.toml	2019-09-15 19:29:15.000000000 +0200
-+++ b/Cargo.toml	2019-10-19 10:14:25.000000000 +0200
-@@ -52,9 +52,6 @@ features = ["suggestions", "color", "wra
- [target.'cfg(all(unix, not(target_os = "redox")))'.dependencies]
- libc = "0.2"
-
--[target.'cfg(all(not(windows), not(target_env = "musl")))'.dependencies]
--jemallocator = "0.3.0"
--
- [dev-dependencies]
- diff = "0.1"
- tempdir = "0.3"
-diff -pur a/src/main.rs b/src/main.rs
---- a/src/main.rs
-+++ b/src/main.rs
-@@ -29,11 +29,6 @@ use crate::filter::{SizeFilter, TimeFilter};
- use crate::options::Options;
- use crate::regex_helper::pattern_has_uppercase_char;
- 
--// We use jemalloc for performance reasons, see https://github.com/sharkdp/fd/pull/481
--#[cfg(all(not(windows), not(target_env = "musl")))]
--#[global_allocator]
--static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
--
- fn run() -> Result<ExitCode> {
-     let matches = app::build_app().get_matches_from(env::args_os());
-
