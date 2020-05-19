cdf3e2ab5945eddfddbf3aaf3e443932d4190305
Caleb Xu <calebcenter@live.com>
2020-05-19 02:42:20 -0400

    sane-backends 1.0.30
    Closes #54955.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sane-backends.rb
+++ Formula/sane-backends.rb
@@ -1,10 +1,19 @@
 class SaneBackends < Formula
   desc "Backends for scanner access"
   homepage "http://www.sane-project.org/"
-  url "https://gitlab.com/sane-project/backends/uploads/9e718daff347826f4cfe21126c8d5091/sane-backends-1.0.28.tar.gz"
-  sha256 "31260f3f72d82ac1661c62c5a4468410b89fb2b4a811dabbfcc0350c1346de03"
-  revision 1
   head "https://gitlab.com/sane-project/backends.git"
+
+  stable do
+    url "https://gitlab.com/sane-project/backends/uploads/c3dd60c9e054b5dee1e7b01a7edc98b0/sane-backends-1.0.30.tar.gz"
+    sha256 "3f5d96a9c47f6124a46bb577c776bbc4896dd17b9203d8bfbc7fe8cbbcf279a3"
+
+    # Fixes build error "error: use of undeclared identifier 'HOST_NAME_MAX'"
+    # Commit is already included upstream in versions > 1.0.30
+    patch do
+      url "https://gitlab.com/sane-project/backends/-/commit/011d0f9bacab126fb2ae09d29abdd6eb88f1333d.diff"
+      sha256 "2fb2366d8e53397237f4beda1f51000b0ae5beb2683387fb2a779d3a43ef1c9d"
+    end
+  end
 
   bottle do
     sha256 "07216e961a590f3d9a00b43cae10b132ed5008309a410db93d68aa4dca824888" => :catalina
@@ -21,11 +30,16 @@ class SaneBackends < Formula
   depends_on "net-snmp"
   depends_on "openssl@1.1"
 
-  def install
-    # malloc lives in malloc/malloc.h instead of just malloc.h on macOS.
-    # Merge request opened upstream: https://gitlab.com/sane-project/backends/merge_requests/90
-    inreplace "backend/ricoh2_buffer.c", "#include <malloc.h>", "#include <malloc/malloc.h>"
+  if build.head?
+    depends_on "autoconf" => :build
+    depends_on "autoconf-archive" => :build
+    depends_on "automake" => :build
+    depends_on "gettext" => :build
+    depends_on "libtool" => :build
+  end
 
+  def install
+    system "./autogen.sh" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--localstatedir=#{var}",
