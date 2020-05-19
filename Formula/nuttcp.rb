c11640e51e80ea7a2bc4048bbc030b81e79ea24f
Rui Chen <chenrui333@gmail.com>
2020-05-18 21:39:10 -0400

    nuttcp 8.2.2
    add rebuild 1 back
    
    Closes #54933.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nuttcp.rb
+++ Formula/nuttcp.rb
@@ -1,8 +1,8 @@
 class Nuttcp < Formula
   desc "Network performance measurement tool"
   homepage "https://www.nuttcp.net/nuttcp"
-  url "https://www.nuttcp.net/nuttcp/nuttcp-8.1.4.tar.bz2"
-  sha256 "737f702ec931ec12fcf54e66c4c1d5af72bd3631439ffa724ed2ac40ab2de78d"
+  url "https://www.nuttcp.net/nuttcp/nuttcp-8.2.2.tar.bz2"
+  sha256 "7ead7a89e7aaa059d20e34042c58a198c2981cad729550d1388ddfc9036d3983"
 
   bottle do
     cellar :any_skip_relocation
