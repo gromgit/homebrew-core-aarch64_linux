96c1a121e1d1607fdcfd9c6711b840587990b6fa
Caleb Xu <calebcenter@live.com>
2020-05-18 23:45:48 -0400

    libpqxx@6: create versioned formula

---
+++ Formula/libpqxx@6.rb
@@ -0,0 +1,37 @@
+class LibpqxxAT6 < Formula
+  desc "C++ connector for PostgreSQL"
+  homepage "https://pqxx.org/development/libpqxx/"
+  url "https://github.com/jtv/libpqxx/archive/6.4.6.tar.gz"
+  sha256 "2280621eee7ec675ed6751abac2273f0ac41395e99be96e06584a54a0cc3985a"
+
+  keg_only :versioned_formula
+
+  deprecate!
+
+  depends_on "pkg-config" => :build
+  depends_on "python@3.8" => :build
+  depends_on "xmlto" => :build
+  depends_on "libpq"
+
+  def install
+    ENV.prepend_path "PATH", Formula["python@3.8"].opt_libexec/"bin"
+    ENV["PG_CONFIG"] = Formula["libpq"].opt_bin/"pg_config"
+
+    system "./configure", "--prefix=#{prefix}", "--enable-shared"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <pqxx/pqxx>
+      int main(int argc, char** argv) {
+        pqxx::connection con;
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "test.cpp", "-L#{lib}", "-lpqxx",
+           "-I#{include}", "-o", "test"
+    # Running ./test will fail because there is no runnning postgresql server
+    # system "./test"
+  end
+end
