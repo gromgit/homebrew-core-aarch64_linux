2e28c8c843d83e198f81e595bf4dd6595578b5a6
Rui Chen <chenrui333@gmail.com>
2020-05-20 10:59:16 -0400

    wtf 20200518
    Closes #55015.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/wtf.rb
+++ Formula/wtf.rb
@@ -1,8 +1,8 @@
 class Wtf < Formula
   desc "Translate common Internet acronyms"
   homepage "https://sourceforge.net/projects/bsdwtf/"
-  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20200505.tar.gz"
-  sha256 "8bd0421b412bfba012b1d6189ab991c5d1511cd93e17fe18da326ba7b7f53ace"
+  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20200518.tar.gz"
+  sha256 "c86b84332875e48dad2ab18fced62fdf6f3d9cf3fcd10ff3468d6e88f5385c22"
 
   bottle :unneeded
 
