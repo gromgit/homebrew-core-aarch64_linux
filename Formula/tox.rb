64ca2b6ee7b85983dbf2f5f38cac13ff50191c50
Dustin Rodrigues <dust.rod@gmail.com>
2020-05-20 10:40:11 -0400

    tox 3.15.1
    Closes #55011.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tox.rb
+++ Formula/tox.rb
@@ -3,8 +3,8 @@ class Tox < Formula
 
   desc "Generic Python virtualenv management and test command-line tool"
   homepage "https://tox.readthedocs.org/"
-  url "https://files.pythonhosted.org/packages/f1/bc/a3b7fcacf763b23c047cf25e52708855bda873e48022a69a8e74a0387aeb/tox-3.15.0.tar.gz"
-  sha256 "af09c19478e8fc7ce7555b3d802ddf601b82684b874812c5857f774b8aee1b67"
+  url "https://files.pythonhosted.org/packages/31/20/3daf87b70ce61deecdf7ab5092ae03a29cd8c87ecb04080d3717f1578a88/tox-3.15.1.tar.gz"
+  sha256 "8c9ad9b48659d291c5bc78bcabaa4d680d627687154b812fa52baedaa94f9f83"
 
   bottle do
     cellar :any_skip_relocation
