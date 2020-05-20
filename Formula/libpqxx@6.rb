9bee1779271c4213c25b1908734b9deffc02de61
Bo Anderson <mail@boanderson.me>
2020-05-20 01:20:00 +0000

    libpqxx@6: add 6.4.6 bottle.

--- Formula/libpqxx@6.rb
+++ Formula/libpqxx@6.rb
@@ -4,6 +4,13 @@ class LibpqxxAT6 < Formula
   url "https://github.com/jtv/libpqxx/archive/6.4.6.tar.gz"
   sha256 "2280621eee7ec675ed6751abac2273f0ac41395e99be96e06584a54a0cc3985a"
 
+  bottle do
+    cellar :any
+    sha256 "9e895c25d4b2bcab931ae5095e7710ee71005faba3607fb4b94e87bbb0df1617" => :catalina
+    sha256 "9bbc89817da8c93f4fcad89e9ed43545da579733db8d77dd313e1b0fc375adca" => :mojave
+    sha256 "040431f687f56ae15c0a9109b2c74d1f4a7ab643ef64ee9f376a76d1c4837207" => :high_sierra
+  end
+
   keg_only :versioned_formula
 
   deprecate!
