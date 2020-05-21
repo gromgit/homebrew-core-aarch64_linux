52623dc5d990f67a9597800ab4c26d64b4462df4
Caleb Xu <calebcenter@live.com>
2020-05-21 02:14:10 -0400

    gocloc 0.3.3 (new formula)
    Closes #55058.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

---
+++ Formula/gocloc.rb
@@ -0,0 +1,26 @@
+class Gocloc < Formula
+  desc "Little fast LoC counter"
+  homepage "https://github.com/hhatto/gocloc"
+  url "https://github.com/hhatto/gocloc/archive/v0.3.3.tar.gz"
+  sha256 "308461beea124991c1558d8278e3a7cc0c8411c5730d444ebbd54187edeb688d"
+  head "https://github.com/hhatto/gocloc.git"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "./cmd/gocloc"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      int main(void) {
+        return 0;
+      }
+    EOS
+
+    assert_equal "{\"languages\":[{\"name\":\"C\",\"files\":1,\"code\":4,\"comment\":0," \
+                 "\"blank\":0}],\"total\":{\"files\":1,\"code\":4,\"comment\":0,\"blank\":0}}",
+                 shell_output("#{bin}/gocloc --output-type=json .")
+  end
+end
