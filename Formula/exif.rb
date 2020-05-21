0c440c55fe8f04d84554b3b4ac8c7ca52e96b5b2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-21 23:05:30 +0000

    exif: update 0.6.22 bottle.

--- Formula/exif.rb
+++ Formula/exif.rb
@@ -6,13 +6,9 @@ class Exif < Formula
 
   bottle do
     cellar :any
-    sha256 "6f63742dc5354ff861a3a9097ef4fd6a4f4a8e7bc13258799017cd605cfc397a" => :catalina
-    sha256 "f53bbbb67c2ed09c34e3afeedaf464675c0f9b5de5aa449422d23fc3c426b072" => :mojave
-    sha256 "73abe70edbf8eaac5773627b225afe24461340c163849356ca8a730ac89f0f30" => :high_sierra
-    sha256 "ae8645e99466b553e7198556a2d331759c9f173956dc0c10f584903030c78876" => :sierra
-    sha256 "607827dc887cae86a2bedc7892e4d061990cb04814fb0ac2fd5d0cd1fdefcb30" => :el_capitan
-    sha256 "ebf412bb43ad3695f01f8ed1e5851e7c02218589fc7cbb175c9a3f1914894e37" => :yosemite
-    sha256 "e24a0bdfee07a46a49620c29726748e73a4eca0937c59c1f5efeeb51ea9f6971" => :mavericks
+    sha256 "3d4f3d7c86e7c112f9164970cb5e283a96d82235c1633f15de6683b04ec7df87" => :catalina
+    sha256 "a600fdec30f561aaf97184c57ef77697cb617dd19795cb89201f9851646e9fad" => :mojave
+    sha256 "f8978e60a9eedc21fe0da30fa0a6bf900635635a5b1fa827473881b25c12d542" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
