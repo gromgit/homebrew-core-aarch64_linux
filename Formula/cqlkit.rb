9aa2d50e74349459e7e9aeab124c2ecb4537fc58
Study Hsueh <ph.study@gmail.com>
2020-05-21 12:32:16 +0800

    cqlkit 0.3.2
    Closes #55053.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cqlkit.rb
+++ Formula/cqlkit.rb
@@ -1,8 +1,8 @@
 class Cqlkit < Formula
   desc "CLI tool to export Cassandra query as CSV and JSON format"
   homepage "https://github.com/tenmax/cqlkit"
-  url "https://github.com/tenmax/cqlkit/releases/download/v0.3.1/cqlkit-0.3.1.zip"
-  sha256 "f1d0cdbfa0780ee66c3b6e42d9eef1dd6b9c925918090ea2d4a547cb80dca4e8"
+  url "https://github.com/tenmax/cqlkit/releases/download/v0.3.2/cqlkit-0.3.2.zip"
+  sha256 "37e573d2afe7ef7a1b820716923b94ba9eb8ace5d141fb34a018733f6f8de1ab"
 
   bottle :unneeded
 
