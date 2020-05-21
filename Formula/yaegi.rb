57a75a24f25ec52fb36bfead34166f3fa307ff44
chenrui <chenrui333@gmail.com>
2020-05-21 04:10:05 +0000

    yaegi: update 0.8.6 bottle.

--- Formula/yaegi.rb
+++ Formula/yaegi.rb
@@ -7,9 +7,9 @@ class Yaegi < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c328cd77044c3b8479c39304a2f4ff90a41223a462d1410744dd3df6044a7000" => :catalina
-    sha256 "43b7c84825eaa3bc7133f9fd9fe7728be417bcc717333f7f8d547c70fe687aca" => :mojave
-    sha256 "24ea156583c8da3be1178ef19069bf6b6424390799d35eb5c0113c3d32620e10" => :high_sierra
+    sha256 "d16015dc1139316b1bf9864cafd7d7bdc132accb600f043f0ab3f81f02ee53f0" => :catalina
+    sha256 "7ce961873f891f3e233dbee8d0a6734aec19a3499eb3927c0bed15118303a560" => :mojave
+    sha256 "f1f27ef2699107f57f9562e207a802bfb46bc1d9f4af4b9b39a1480141b14dde" => :high_sierra
   end
 
   depends_on "go" => :build
