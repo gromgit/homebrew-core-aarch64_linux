658dfb679fb1d6479eacdb151bd70c239a43e965
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-21 21:05:59 +0000

    nss: update 3.52.1 bottle.

--- Formula/nss.rb
+++ Formula/nss.rb
@@ -6,9 +6,9 @@ class Nss < Formula
 
   bottle do
     cellar :any
-    sha256 "79ae9615f454e499df9dbbf738082f8517e80f9834c1db166260e76ff26aa76e" => :catalina
-    sha256 "8dcf8da1de0e61a07b31753739b8a4b2e78d9ff7fe4589a9084f48ca3c6de685" => :mojave
-    sha256 "7b6499923488a485722f14b9218202a92bc300920487f99720db7b5c835e762b" => :high_sierra
+    sha256 "a3a0873699f617cf1a3095ae9b63b4d009832e771d809e02c1d54b8cea4914c1" => :catalina
+    sha256 "4d1b636b0c0df9089029d19adef6c07dd04a9f8269747870408e48061600f13e" => :mojave
+    sha256 "13c68cc01a86c1b62d588a25affa001efc37e01d863acb5a67f60971a9937beb" => :high_sierra
   end
 
   depends_on "nspr"
