c653db4391776119d357dca4897e45f0df19e6f3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-21 22:05:57 +0000

    sentencepiece: update 0.1.91 bottle.

--- Formula/sentencepiece.rb
+++ Formula/sentencepiece.rb
@@ -5,9 +5,9 @@ class Sentencepiece < Formula
   sha256 "acbc7ea12713cd2a8d64892f8d2033c7fd2bb4faecab39452496120ace9a4b1b"
 
   bottle do
-    sha256 "a76efbe9aa72abd0a119faf3b3694e77102284b22ec567a2d18472ec52d7c402" => :catalina
-    sha256 "f7244e947ec84c223e441c07482e8805cc9e590ce6f2dc93a4c1c5049c7a4a4f" => :mojave
-    sha256 "78bb55baa1320006ffe370dba1c6c90b08aa567ae20627b409627d179c128e38" => :high_sierra
+    sha256 "d65c679bb760be25ab088947503541a62d4fe3bf73d182bc618702fc29463d45" => :catalina
+    sha256 "64278463f601a298edca2d3c334b8b4e9afefcc6cb3c6d31bf37b09f38f709fe" => :mojave
+    sha256 "a935105f263603af33cb30190e3a324758914b72e82dae1ab31908ee0848aaef" => :high_sierra
   end
 
   depends_on "cmake" => :build
