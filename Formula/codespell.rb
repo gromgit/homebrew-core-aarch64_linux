4a504385f5d003270ed5ff1e1f9a673a88ca2625
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-22 10:06:16 +0000

    codespell: update 1.17.1 bottle.

--- Formula/codespell.rb
+++ Formula/codespell.rb
@@ -8,9 +8,9 @@ class Codespell < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "99881ae6b8146c6d2f1920ebe38326ec956fb6f373e0a24e0edb752aba542b39" => :catalina
-    sha256 "5a03b3a7c8ee14881fa1ea4b67704d0669f67947ea4dce63e559563105d9d811" => :mojave
-    sha256 "adf3d2cb26a64b62a1cd57a380330bf5328aa49a8591668445e212d970c820d0" => :high_sierra
+    sha256 "fb3fb87c3d707656b5c796fd40a13b6e7d0170f5cc4db6361751074b1f089bcf" => :catalina
+    sha256 "f44c96916092e661dfa53499d3570b98bba5fbcf964751f55c775e0aee68b37c" => :mojave
+    sha256 "752254907866753d1941f39193d67cb2fbaa54f294d6d0f4a1f11cd8a8247aae" => :high_sierra
   end
 
   depends_on "python@3.8"
