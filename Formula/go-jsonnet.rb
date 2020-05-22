ca19fec4a5ea7e041fce54a4f0b6310642399d7b
Igor <igorwwwwwwwwwwwwwwwwwwww@users.noreply.github.com>
2020-05-22 20:20:04 +0200

    go-jsonnet 0.16.0
    Closes #55128.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/go-jsonnet.rb
+++ Formula/go-jsonnet.rb
@@ -1,8 +1,8 @@
 class GoJsonnet < Formula
   desc "Go implemention of configuration language for defining JSON data"
   homepage "https://jsonnet.org/"
-  url "https://github.com/google/go-jsonnet/archive/v0.15.0.tar.gz"
-  sha256 "cddc3462151de4c5c65183b613895575db06297edd12a63e740f09e5e0ed98dd"
+  url "https://github.com/google/go-jsonnet/archive/v0.16.0.tar.gz"
+  sha256 "8ca930c892d34a119c1970431d159000321fe323734f06a1253bd78fc3625b84"
   head "https://github.com/google/go-jsonnet.git"
 
   bottle do
@@ -18,11 +18,7 @@ class GoJsonnet < Formula
 
   def install
     system "go", "build", "-o", bin/"jsonnet", "./cmd/jsonnet"
-
-    if build.head?
-      # jsonnetfmt will only be added with the next release
-      system "go", "build", "-o", bin/"jsonnetfmt", "./cmd/jsonnetfmt"
-    end
+    system "go", "build", "-o", bin/"jsonnetfmt", "./cmd/jsonnetfmt"
   end
 
   test do
