84e746a2246cc06ee08696eecf61f30f11cb76f7
chenrui <chenrui333@gmail.com>
2020-05-21 21:30:19 -0400

    fuseki 3.15.0 (#55097)

--- Formula/fuseki.rb
+++ Formula/fuseki.rb
@@ -1,9 +1,9 @@
 class Fuseki < Formula
   desc "SPARQL server"
   homepage "https://jena.apache.org/documentation/fuseki2/"
-  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-fuseki-3.14.0.tar.gz"
-  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-fuseki-3.14.0.tar.gz"
-  sha256 "11cdfc60c515281efeb392394d750f1847ee77a2b0a4728ab006f4faafee916a"
+  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-fuseki-3.15.0.tar.gz"
+  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-fuseki-3.15.0.tar.gz"
+  sha256 "221f29084fecd34000b31db510887a436a112b3d2e9df9a33c836b7b4832fdfc"
 
   bottle :unneeded
 
