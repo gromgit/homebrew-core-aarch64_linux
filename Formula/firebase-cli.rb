8cfc0ff17ed335b61e5285bddf69c451feb46e42
Rui Chen <chenrui333@gmail.com>
2020-05-21 21:15:55 -0400

    firebase-cli 8.4.0
    Closes #55096.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class FirebaseCli < Formula
   desc "Firebase command-line tools"
   homepage "https://firebase.google.com/docs/cli/"
-  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-8.3.0.tgz"
-  sha256 "0a2c9bd10b8c2842dbd297e423e4f5b46592129660cee32572183b3db918b743"
+  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-8.4.0.tgz"
+  sha256 "6d6e3bc1307e1948ba35f0bd69ed380eb4510a80e3b608b776097f8d26a14a92"
   head "https://github.com/firebase/firebase-tools.git"
 
   bottle do
