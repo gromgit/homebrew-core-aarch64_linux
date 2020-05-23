4ee85d8f15b9463795293636e59d2b6f48ca7cb9
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-05-23 19:06:01 +0100

    gexiv2 0.12.1
    Closes #55155.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gexiv2.rb
+++ Formula/gexiv2.rb
@@ -1,9 +1,8 @@
 class Gexiv2 < Formula
   desc "GObject wrapper around the Exiv2 photo metadata library"
   homepage "https://wiki.gnome.org/Projects/gexiv2"
-  url "https://download.gnome.org/sources/gexiv2/0.12/gexiv2-0.12.0.tar.xz"
-  sha256 "58f539b0386f36300b76f3afea3a508de4914b27e78f58ee4d142486a42f926a"
-  revision 2
+  url "https://download.gnome.org/sources/gexiv2/0.12/gexiv2-0.12.1.tar.xz"
+  sha256 "8aeafd59653ea88f6b78cb03780ee9fd61a2f993070c5f0d0976bed93ac2bd77"
 
   bottle do
     cellar :any
@@ -20,9 +19,6 @@ class Gexiv2 < Formula
   depends_on "vala" => :build
   depends_on "exiv2"
   depends_on "glib"
-
-  # submitted upstream as https://gitlab.gnome.org/GNOME/gexiv2/merge_requests/10
-  patch :DATA
 
   def install
     pyver = Language::Python.major_minor_version Formula["python@3.8"].opt_bin/"python3"
@@ -54,45 +50,3 @@ class Gexiv2 < Formula
     system "./test"
   end
 end
-
-__END__
-diff --git a/gexiv2/meson.build b/gexiv2/meson.build
-index 196b298..12abf92 100644
---- a/gexiv2/meson.build
-+++ b/gexiv2/meson.build
-@@ -2,6 +2,13 @@ pkg = import('pkgconfig')
-
- as_version = meson.project_version().split('.')
-
-+libversion = '2.0.0'
-+libversion_arr = libversion.split('.')
-+darwin_version_major = libversion_arr[0].to_int()
-+darwin_version_minor = libversion_arr[1].to_int()
-+darwin_version_micro = libversion_arr[2].to_int()
-+darwin_versions = [darwin_version_major + darwin_version_minor + 1, '@0@.@1@'.format(darwin_version_major + darwin_version_minor + 1, darwin_version_micro)]
-+
- gexiv2_include_dir = join_paths(get_option('includedir'), 'gexiv2')
-
- config = configuration_data()
-@@ -53,7 +60,8 @@ gexiv2 = library('gexiv2',
-                  [version_header] +
-                  enum_sources,
-                  include_directories : include_directories('..'),
--                 version: '2.0.0',
-+                 version: libversion,
-+                 darwin_versions: darwin_versions,
-                  dependencies : [gobject, exiv2, gio],
-                  install : true)
-
-diff --git a/meson.build b/meson.build
-index 601afc1..b84255f 100644
---- a/meson.build
-+++ b/meson.build
-@@ -2,6 +2,7 @@ project(
-     'gexiv2',
-     ['c', 'cpp'],
-     version : '0.12.0',
-+    meson_version : '>=0.48',
-     default_options : [
-         'cpp_std=c++11'
-     ]
