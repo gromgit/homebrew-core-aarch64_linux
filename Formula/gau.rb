e5939a469b3c12aff6b55e3a4ab3c0941a3fc59a
Issy Long <me@issyl0.co.uk>
2020-05-23 08:13:47 +0000

    gau: add 1.0.2 bottle.

--- Formula/gau.rb
+++ Formula/gau.rb
@@ -4,6 +4,13 @@ class Gau < Formula
   url "https://github.com/lc/gau/archive/v1.0.2.tar.gz"
   sha256 "058a0818b3b1465af9878566c6cb01e2b2835903b5630f63afb0645c07b28663"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8fbfdf238dd7408adf09c159338c48e67ee9030f80f129a3b20683a960ab4531" => :catalina
+    sha256 "d24c1d256af7be9370ec0d12d5deddabde47885d8d2b387ce5bfb6eb0a36e05c" => :mojave
+    sha256 "67ece09c592ad7cdab9ec296e8fbff86fb23410a149aa9bb8f7a295d55661ab7" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
