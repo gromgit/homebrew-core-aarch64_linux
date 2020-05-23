09077c082504096a5fa93284c4549e8154bd575a
Yurii Kolesnykov <root@yurikoles.com>
2020-05-24 00:15:51 +0300

    kde-threadweaver 5.71.0 (new formula)
    Closes #55175.
    
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>
    Signed-off-by: Caleb Xu <calebcenter@live.com>

---
+++ Formula/kde-threadweaver.rb
@@ -0,0 +1,34 @@
+class KdeThreadweaver < Formula
+  desc "Helper for multithreaded programming"
+  homepage "https://api.kde.org/frameworks/threadweaver/html/index.html"
+  url "https://download.kde.org/stable/frameworks/5.71/threadweaver-5.71.0.tar.xz"
+  sha256 "039e73d70f38af38a63235cfb554111ee0d58a6ac168bff0745f0d029c5c528d"
+  head "https://invent.kde.org/frameworks/threadweaver.git"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "doxygen" => :build
+  depends_on "graphviz" => :build
+  depends_on "kde-extra-cmake-modules" => [:build, :test]
+  depends_on "qt"
+
+  def install
+    args = std_cmake_args
+    args << "-DBUILD_TESTING=OFF"
+    args << "-DBUILD_QCH=ON"
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+    end
+
+    pkgshare.install "examples"
+  end
+
+  test do
+    qt5_arg = "-DQt5Core_DIR=#{Formula["qt"].opt_prefix/"lib/cmake/Qt5Core"}"
+    system "cmake", (pkgshare/"examples/HelloWorld"), *std_cmake_args, qt5_arg
+    system "make"
+
+    assert_equal "Hello World!", shell_output("./ThreadWeaver_HelloWorld 2>&1").strip
+  end
+end
