48e82b10cdbc453e73fdeaa21dd88877e11e62b0
Caleb Xu <calebcenter@live.com>
2020-05-24 03:11:01 -0400

    fastd 19
    Closes #55205.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fastd.rb
+++ Formula/fastd.rb
@@ -1,20 +1,9 @@
 class Fastd < Formula
   desc "Fast and Secure Tunnelling Daemon"
-  homepage "https://projects.universe-factory.net/projects/fastd"
-  revision 4
-  head "https://git.universe-factory.net/fastd/", :using => :git
-
-  stable do
-    url "https://projects.universe-factory.net/attachments/download/86/fastd-18.tar.xz"
-    sha256 "714ff09d7bd75f79783f744f6f8c5af2fe456c8cf876feaa704c205a73e043c9"
-
-    # https://projects.universe-factory.net/issues/239
-    # https://projects.universe-factory.net/projects/fastd/repository/revisions/2fa2187
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/c2c6049/fastd/patch-xcode8-clock_gettime.diff"
-      sha256 "dfa07eccf01a84a6e5eacc82c47c2cd5ba216e1f5032b41d2cef32e0b205ba9c"
-    end
-  end
+  homepage "https://github.com/NeoRaider/fastd"
+  url "https://github.com/NeoRaider/fastd/releases/download/v19/fastd-19.tar.xz"
+  sha256 "6054608e2103b634c9d19ecd1ae058d4ec694747047130719db180578729783a"
+  head "https://github.com/NeoRaider/fastd.git"
 
   bottle do
     cellar :any
@@ -33,14 +22,8 @@ class Fastd < Formula
   depends_on "libuecc"
 
   def install
-    args = std_cmake_args
-    args << "-DENABLE_LTO=ON"
-
-    # https://projects.universe-factory.net/issues/251
-    args << "-DWITH_CIPHER_AES128_CTR_NACL=OFF"
-
-    mkdir "fastd-build" do
-      system "cmake", "..", *args
+    mkdir "build" do
+      system "cmake", "..", "-DENABLE_LTO=ON", *std_cmake_args
       system "make"
       system "make", "install"
     end
