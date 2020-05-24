23c360ed730761092a5c459779334703304045d7
Camden Narzt <c.narzt@me.com>
2020-05-24 16:40:13 -0600

    improve coturn formula
    Closes #55234.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

--- Formula/coturn.rb
+++ Formula/coturn.rb
@@ -3,6 +3,7 @@ class Coturn < Formula
   homepage "https://github.com/coturn/coturn"
   url "http://turnserver.open-sys.org/downloads/v4.5.1.2/turnserver-4.5.1.2.tar.gz"
   sha256 "5f34ded2c2871c74d498712c3fbfce8a418f8ea8e6508adc07cfe5e152881455"
+  revision 1
 
   bottle do
     sha256 "49feaa7d1896622d9716abd64dd4e70b7719671ab0e6f69be4fe50b607b4b135" => :catalina
@@ -10,16 +11,60 @@ class Coturn < Formula
     sha256 "5cb6a86a0beca4eb08b8d660095db3d146f66a904b54b7d5de00e6c55f1d1652" => :high_sierra
   end
 
+  depends_on "hiredis"
   depends_on "libevent"
+  depends_on "libpq"
+  depends_on "openssl@1.1"
 
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--mandir=#{man}",
+                          "--localstatedir=#{var}",
+                          "--includedir=#{include}",
+                          "--libdir=#{lib}",
+                          "--docdir=#{doc}",
                           "--prefix=#{prefix}"
 
     system "make", "install"
+
+    man.mkpath
+    man1.install Dir["man/man1/*"]
+  end
+
+  plist_options :manual => "turnserver -c #{HOMEBREW_PREFIX}/etc/turnserver.conf --userdb=#{HOMEBREW_PREFIX}/opt/coturn/var/db/turndb --daemon"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>KeepAlive</key>
+          <dict>
+            <key>SuccessfulExit</key>
+            <false/>
+          </dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/turnserver</string>
+            <string>-c</string>
+            <string>#{etc}/turnserver.conf</string>
+          </array>
+          <key>WorkingDirectory</key>
+          <string>#{HOMEBREW_PREFIX}</string>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/coturn.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/coturn.log</string>
+        </dict>
+      </plist>
+    EOS
   end
 
   test do
