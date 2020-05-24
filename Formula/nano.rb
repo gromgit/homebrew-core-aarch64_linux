a88a877fb51494f6b053358549bb4c6ab6e264e6
Caleb Xu <calebcenter@live.com>
2020-05-23 21:15:04 -0400

    nano 4.9.3
    Closes #55188.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/nano.rb
+++ Formula/nano.rb
@@ -1,8 +1,8 @@
 class Nano < Formula
   desc "Free (GNU) replacement for the Pico text editor"
   homepage "https://www.nano-editor.org/"
-  url "https://www.nano-editor.org/dist/v4/nano-4.9.2.tar.xz"
-  sha256 "d8a25eea942ecee2d57b8e037eb4b28f030f818b78773b8fcb994ed5835d2ef6"
+  url "https://www.nano-editor.org/dist/v4/nano-4.9.3.tar.xz"
+  sha256 "6e3438f033a0ed07d3d74c30d0803cbda3d2366ba1601b7bbf9b16ac371f51b4"
 
   bottle do
     sha256 "0ddfd95328a4d8be2677a8f42a01528d719af912b92df30f6004749586c235df" => :catalina
