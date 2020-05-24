f641eb658b5b68675c7313a66a3c3bd7142ff555
Caleb Xu <calebcenter@live.com>
2020-05-23 23:42:05 -0400

    sngrep 1.4.7
    Closes #55198.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sngrep.rb
+++ Formula/sngrep.rb
@@ -1,9 +1,8 @@
 class Sngrep < Formula
   desc "Command-line tool for displaying SIP calls message flows"
   homepage "https://github.com/irontec/sngrep"
-  url "https://github.com/irontec/sngrep/archive/v1.4.6.tar.gz"
-  sha256 "638d6557dc68db401b07d73b2e7f8276800281f021fe0c942992566d6b59a48a"
-  revision 1
+  url "https://github.com/irontec/sngrep/archive/v1.4.7.tar.gz"
+  sha256 "3c96aadd8b8c6b7adb5552d6fb4234f2000f15109b0f6c5feb632c81306cc49b"
 
   bottle do
     sha256 "780b2ae6e08a8041ddf7d6698d699745f29fb2eb642556de96dfcd2989ef27fe" => :catalina
