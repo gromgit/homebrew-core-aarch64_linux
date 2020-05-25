046ac79e2058dcc962615938381fc26f1b61eabf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-25 09:06:37 +0000

    mariadb-connector-c: update 3.1.8 bottle.

--- Formula/mariadb-connector-c.rb
+++ Formula/mariadb-connector-c.rb
@@ -5,9 +5,9 @@ class MariadbConnectorC < Formula
   sha256 "431434d3926f4bcce2e5c97240609983f60d7ff50df5a72083934759bb863f7b"
 
   bottle do
-    sha256 "b45cc1f8d53b6d24fdbf588d7120de36f9bcd315860dab27768c9a752f6ad333" => :catalina
-    sha256 "4ae4e9fd1fd1d054393d641576ce4bf122937d79aa52cb0e185fe94d02345752" => :mojave
-    sha256 "93981e72f4b5054c5bdb74706bcf7795da5011c0267773c7789b381a1ada0132" => :high_sierra
+    sha256 "9b46c115a5e52eda9c098ddb5f426119af1b2fe706c936a23af9dcd8396e5a2d" => :catalina
+    sha256 "b1677f7aab7d36908ec07abe4427fb1d04929a7d59a5b08de103a5ae573dffea" => :mojave
+    sha256 "c681d5b8bf178d5e1e4bd6ea08ed819fe1128845dc290eda2776e7c06731731f" => :high_sierra
   end
 
   depends_on "cmake" => :build
