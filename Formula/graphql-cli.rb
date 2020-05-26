8fd7e00e89f8a95f43dc2922d69e79f5ce759ae5
chenrui <chenrui333@gmail.com>
2020-05-26 17:09:51 +0000

    graphql-cli: add 3.0.14 bottle.

--- Formula/graphql-cli.rb
+++ Formula/graphql-cli.rb
@@ -6,6 +6,13 @@ class GraphqlCli < Formula
   url "https://registry.npmjs.org/graphql-cli/-/graphql-cli-3.0.14.tgz"
   sha256 "bf56bbe425795198bf6fcc488f24fe10779327749671b1a331d4137684d8a72d"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "62783d3c7488a1e1a228fd2e9567d9ff936ad96f16be56c541c486ea625e843a" => :catalina
+    sha256 "15a20fa31664af3c398c15eb8d8f29e2a07f00ae3e81315a541dec3ee7c112c4" => :mojave
+    sha256 "e48b7e202b04240f33743ebfa5171056e9de9cd6d668f2f50e92b324af46f751" => :high_sierra
+  end
+
   depends_on "node"
 
   uses_from_macos "expect" => :test
