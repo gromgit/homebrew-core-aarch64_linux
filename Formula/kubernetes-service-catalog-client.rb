b3ea3c27410d76371ef41182c20db94c89f5d0c4
Rui Chen <chenrui333@gmail.com>
2020-05-26 13:18:09 -0400

    kubernetes-service-catalog-client 0.3.0
    - remove gopath
    - update to use tarball/sha256
    - update test
    
    Closes #55316.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/kubernetes-service-catalog-client.rb
+++ Formula/kubernetes-service-catalog-client.rb
@@ -1,9 +1,8 @@
 class KubernetesServiceCatalogClient < Formula
   desc "Consume Services in k8s using the OSB API"
   homepage "https://svc-cat.io/"
-  url "https://github.com/kubernetes-sigs/service-catalog.git",
-      :tag      => "v0.2.3",
-      :revision => "62201e94f74962ff94dba0664e5e9e757c58188a"
+  url "https://github.com/kubernetes-sigs/service-catalog/archive/v0.3.0.tar.gz"
+  sha256 "3f79365cbe9c63513ddf3ad836177f0bd9ecee5b36cad015a3e9353e47cc874e"
 
   bottle do
     cellar :any_skip_relocation
@@ -15,25 +14,19 @@ class KubernetesServiceCatalogClient < Formula
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
     ENV["NO_DOCKER"] = "1"
 
-    dir = buildpath/"src/github.com/kubernetes-sigs/service-catalog"
-    dir.install buildpath.children
-
-    cd dir do
-      ldflags = %W[
-        -s -w -X
-        github.com/kubernetes-sigs/service-catalog/pkg.VERSION=v#{version}
-      ]
-      system "go", "build", "-ldflags", ldflags.join(" "), "-o",
-             bin/"svcat", "./cmd/svcat"
-      prefix.install_metafiles
-    end
+    ldflags = %W[
+      -s -w
+      -X github.com/kubernetes-sigs/service-catalog/pkg.VERSION=v#{version}
+    ]
+    system "go", "build", "-ldflags", ldflags.join(" "), "-o",
+            bin/"svcat", "./cmd/svcat"
+    prefix.install_metafiles
   end
 
   test do
-    version_output = shell_output("#{bin}/svcat version --client 2>&1")
-    assert_match "Client Version: v#{version}", version_output
+    version_output = shell_output("#{bin}/svcat version --client 2>&1", 1)
+    assert_match "Error: could not get Kubernetes config for context", version_output
   end
 end
