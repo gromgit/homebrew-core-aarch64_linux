fe03617d4e2aa292822d9425d3862b5915ee07bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-26 06:07:22 +0000

    mutt: update 1.14.2 bottle.

--- Formula/mutt.rb
+++ Formula/mutt.rb
@@ -14,9 +14,9 @@ class Mutt < Formula
   sha256 "e22f700e8c57fbc41a642bfeeeed1adca06e6187887c39614a5c38d381a6ac31"
 
   bottle do
-    sha256 "d6c0ed6a7626e2a6b614c7aec7133ca40e7cd0afba8d3d5b834647f418e2087b" => :catalina
-    sha256 "048bfc084fcb45485eea83a82baab50487f7652b4bfe25c94698971f0fbd536d" => :mojave
-    sha256 "5ad3d9737873b99721bc1d50a17f7d90deaee31586c684c527e3ab74b12b245d" => :high_sierra
+    sha256 "9f0640f8bf8ade96892ea1e7ce2a0d6d8232bc83cb5bfd3895b21a72c70b74fd" => :catalina
+    sha256 "69d90a277a0d08226c1564381dd139737b59acab1deba1f1e14d85485522761a" => :mojave
+    sha256 "bace0e9cf94594612677f93f33244f05a9fba0046cdf0d8460a2657b5538047a" => :high_sierra
   end
 
   head do
