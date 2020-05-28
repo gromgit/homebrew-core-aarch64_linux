28e02be7000d72c3c149d13c873a1961bda5ab3d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-28 15:07:56 +0000

    gnu-getopt: update 2.35.2 bottle.

--- Formula/gnu-getopt.rb
+++ Formula/gnu-getopt.rb
@@ -6,9 +6,9 @@ class GnuGetopt < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "057a54871793ac9c455872ea65f5dc4a4391d842a0b887f7761c17aa62f3e9b2" => :catalina
-    sha256 "af7e6ff9fce7d0286ca4cb0c0ce01d385ba3ea4cfc6735f97165272f97f18a81" => :mojave
-    sha256 "3114f75086a792b8a40259056a95875f77fefae73e43d8fd39d7818436f6b8bf" => :high_sierra
+    sha256 "4eda655b18149121dd39d19928b64b63f7310469ad1d64a232996b61afb5016c" => :catalina
+    sha256 "ff402c05420ff975374689ce0f185c8d9f9578f3e9113e935cf3030f0743c31c" => :mojave
+    sha256 "a9c00a4e5483a0636b9a8dc6def5cc73c53f7b6ef16a476af5d470d4d6e1e5c4" => :high_sierra
   end
 
   keg_only :provided_by_macos
