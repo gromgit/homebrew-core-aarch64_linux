55017e79471e4ee8915108d51f20ab721038d3e2
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-05-28 10:17:57 +0100

    gobject-introspection: fix MethodDeprecatedError
    Closes #55383.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gobject-introspection.rb
+++ Formula/gobject-introspection.rb
@@ -1,9 +1,11 @@
 class GobjectIntrospection < Formula
+  include Language::Python::Shebang
+
   desc "Generate introspection data for GObject libraries"
   homepage "https://wiki.gnome.org/Projects/GObjectIntrospection"
   url "https://download.gnome.org/sources/gobject-introspection/1.64/gobject-introspection-1.64.1.tar.xz"
   sha256 "80beae6728c134521926affff9b2e97125749b38d38744dc901f4010ee3e7fa7"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "243f6d51f2d9b91ba55a8011edd4a6033e2e60332176cab17c6697fedc5b32ab" => :catalina
@@ -26,8 +28,6 @@ class GobjectIntrospection < Formula
   end
 
   def install
-    Language::Python.rewrite_python_shebang(Formula["python@3.8"].opt_bin/"python3")
-
     ENV["GI_SCANNER_DISABLE_CACHE"] = "true"
     inreplace "giscanner/transformer.py", "/usr/share", "#{HOMEBREW_PREFIX}/share"
     inreplace "meson.build",
@@ -37,9 +37,9 @@ class GobjectIntrospection < Formula
     mkdir "build" do
       system "meson", *std_meson_args,
         "-Dpython=#{Formula["python@3.8"].opt_bin}/python3", ".."
-      Language::Python.rewrite_python_shebang(Formula["python@3.8"].opt_bin/"python3")
       system "ninja", "-v"
       system "ninja", "install", "-v"
+      bin.find { |f| rewrite_shebang detected_python_shebang, f }
     end
   end
 
