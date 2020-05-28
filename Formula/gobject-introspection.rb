02744b7107c5c0be46612c08b08ee7b8904ef348
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-05-28 12:07:48 +0000

    gobject-introspection: update 1.64.1_2 bottle.

--- Formula/gobject-introspection.rb
+++ Formula/gobject-introspection.rb
@@ -8,9 +8,9 @@ class GobjectIntrospection < Formula
   revision 2
 
   bottle do
-    sha256 "243f6d51f2d9b91ba55a8011edd4a6033e2e60332176cab17c6697fedc5b32ab" => :catalina
-    sha256 "848dae21878e2178b8b93730b84633e41fd4fe3163cb51d1fb0c33dbc46511d4" => :mojave
-    sha256 "2f102d682211e523586cf519fe6d3be3a874f28942245df0f6ee3841756ea328" => :high_sierra
+    sha256 "4715d7cb549501fa75101266f6c49fe656666cb071a6e8f111b08cb3b74a2b15" => :catalina
+    sha256 "e6f0eacacdda25019941d42dd600c478f22de8c57a8975b2e26c4a5cfb8f367c" => :mojave
+    sha256 "b54f973af9b413e20a7b6485315da882f73e623c61f3573325a434d47e5b011a" => :high_sierra
   end
 
   depends_on "bison" => :build
