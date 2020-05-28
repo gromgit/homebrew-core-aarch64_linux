b65e4a4d1aeac629b65304fb37f8709478df68bc
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-05-28 12:06:38 +0000

    wasm-pack: add 0.9.1 bottle.

--- Formula/wasm-pack.rb
+++ Formula/wasm-pack.rb
@@ -5,6 +5,13 @@ class WasmPack < Formula
   sha256 "56930d1f15bbcc85771e9b8e5c1b7bb6734b2c410c353ecd11eae828e35d5fb0"
   head "https://github.com/rustwasm/wasm-pack.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "349d47601c15c1639f9a2c303544addd05b6178a0ae9c3dff15ce53f74d2dfc6" => :catalina
+    sha256 "3fa2dd4e71144a07b14323486a31ef12065d31b90e30c30394f2833b205781db" => :mojave
+    sha256 "f6bc091f507521b82e308a1f1aabdaa1e6afdae80e3beaa0c097bf5702e55c1e" => :high_sierra
+  end
+
   depends_on "rust" => :build
   depends_on "rustup-init"
 
