8f6870802564a102e85f8b350957c7c7b54d1d0e
Alexander Bayandin <a.bayandin@gmail.com>
2020-05-28 13:34:29 +0100

    jfrog-cli 1.36.0
    Closes #55384.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jfrog-cli.rb
+++ Formula/jfrog-cli.rb
@@ -1,15 +1,8 @@
-class JfrogCliGo < Formula
+class JfrogCli < Formula
   desc "Command-line interface for Jfrog Artifactory and Bintray"
   homepage "https://github.com/jfrog/jfrog-cli"
-  url "https://github.com/JFrog/jfrog-cli-go/archive/1.35.5.tar.gz"
-  sha256 "5b3fcb87a7c4b8992ac96c2fc6564231bbfcae2ea4ec5c4aab05d67ead9e0d06"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "bf56d27960763ea258c58253ebd9c1bf219b5bdd7f7c2dea5ff4a8ad7323ed2c" => :catalina
-    sha256 "55d888068330d84f243449c6b8d57ce1e56ffff7ca64593a633b3f2aa2606d52" => :mojave
-    sha256 "7cc5f1dab2fb8dd5fd70e6b68291f7c79e1da3f9f829d3c8434e6c94a94595a4" => :high_sierra
-  end
+  url "https://github.com/jfrog/jfrog-cli/archive/1.36.0.tar.gz"
+  sha256 "31c98a57518fef4c3f23459d9844a4025fd8d8e1bdf98a39237342e7fb31d7b1"
 
   depends_on "go" => :build
 
