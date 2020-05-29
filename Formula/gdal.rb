e19e30db2f2553b6d1e7ce1b7b61beb6a85f7e3c
Ed Porras <ep@digressed.net>
2020-05-29 13:56:39 +0200

    gdal: revision for poppler 0.89.0

--- Formula/gdal.rb
+++ Formula/gdal.rb
@@ -3,7 +3,7 @@ class Gdal < Formula
   homepage "https://www.gdal.org/"
   url "https://download.osgeo.org/gdal/2.4.4/gdal-2.4.4.tar.xz"
   sha256 "a383bd3cf555d6e1169666b01b5b3025b2722ed39e834f1b65090f604405dcd8"
-  revision 6
+  revision 7
 
   bottle do
     sha256 "c95b8b7a57439c00ed619c017b4a40a095f8fde774abe3114415698d38dff053" => :catalina
