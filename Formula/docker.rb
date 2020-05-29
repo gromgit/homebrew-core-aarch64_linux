b4fa98fb4b7bb5042d54ed0f90f76f92b8baf170
shigemk2 <i.am.shige@gmail.com>
2020-05-29 14:32:16 +0900

    docker 19.03.10
    Closes #55415.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -2,8 +2,8 @@ class Docker < Formula
   desc "Pack, ship and run any application as a lightweight container"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/docker-ce.git",
-      :tag      => "v19.03.9",
-      :revision => "9d988398e765e4b97f27f93c4d04b272ac64c9c7"
+      :tag      => "v19.03.10",
+      :revision => "9424aeaee927a5ddd19ee10702159cd91cc3f48e"
 
   bottle do
     cellar :any_skip_relocation
