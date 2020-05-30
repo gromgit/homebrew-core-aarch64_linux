2da52f44a6c69ce6234c6b37a82519b098c773b2
Rui Chen <rui@meetup.com>
2020-05-30 00:51:34 -0400

    gatsby-cli: avoid shims ref
    Closes #55456.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gatsby-cli.rb
+++ Formula/gatsby-cli.rb
@@ -18,6 +18,9 @@ class GatsbyCli < Formula
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    # Avoid references to Homebrew shims
+    rm_f "#{libexec}/lib/node_modules/gatsby-cli/node_modules/websocket/builderror.log"
   end
 
   test do
