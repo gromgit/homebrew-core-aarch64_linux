9b507d9f91c2ea0160ef5897ebd91c62d242ce88
chenrui <chenrui333@gmail.com>
2020-05-30 10:10:43 -0400

    solr@7.7 7.7.3 (#55469)

--- Formula/solr@7.7.rb
+++ Formula/solr@7.7.rb
@@ -1,10 +1,9 @@
 class SolrAT77 < Formula
   desc "Enterprise search platform from the Apache Lucene project"
   homepage "https://lucene.apache.org/solr/"
-  url "https://www.apache.org/dyn/closer.lua?path=lucene/solr/7.7.2/solr-7.7.2.tgz"
-  mirror "https://archive.apache.org/dist/lucene/solr/7.7.2/solr-7.7.2.tgz"
-  sha256 "eb8ee4038f25364328355de3338e46961093e39166c9bcc28b5915ae491320df"
-  revision 2
+  url "https://www.apache.org/dyn/closer.lua?path=lucene/solr/7.7.3/solr-7.7.3.tgz"
+  mirror "https://archive.apache.org/dist/lucene/solr/7.7.3/solr-7.7.3.tgz"
+  sha256 "3ec67fa430afa5b5eb43bb1cd4a659e56ee9f8541e0116d6080c0d783870baee"
 
   bottle :unneeded
 
