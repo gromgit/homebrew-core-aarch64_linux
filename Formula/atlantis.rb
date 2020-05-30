a6ced45cfe9d2a95f917a2e63015b8fa9e68dc98
Rui Chen <rui@meetup.com>
2020-05-30 01:13:40 -0400

    atlantis 0.13.0
    Closes #55474.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/atlantis.rb
+++ Formula/atlantis.rb
@@ -1,8 +1,8 @@
 class Atlantis < Formula
   desc "Terraform Pull Request Automation tool"
   homepage "https://www.runatlantis.io/"
-  url "https://github.com/runatlantis/atlantis/archive/v0.12.0.tar.gz"
-  sha256 "df7c17fef4b37347fc22e1c5bf3d2b29ad1b219e70b6bff111e38d8ee01102e4"
+  url "https://github.com/runatlantis/atlantis/archive/v0.13.0.tar.gz"
+  sha256 "7e06f1e4d9410611763f2a9da3d45ecba6921be77a8f5d1ebba722047cfa081f"
 
   bottle do
     cellar :any_skip_relocation
