3030feb00779ba6fdbb900df2680670c884240cc
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-05-30 11:38:59 +0100

    evince 3.36.2
    This also marks the transition to meson as buildsystem.
    
    Closes #55486.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/evince.rb
+++ Formula/evince.rb
@@ -1,8 +1,8 @@
 class Evince < Formula
   desc "GNOME document viewer"
   homepage "https://wiki.gnome.org/Apps/Evince"
-  url "https://download.gnome.org/sources/evince/3.36/evince-3.36.1.tar.xz"
-  sha256 "8cb8bfd358a02d8edba51114548e98154321e0895bf379a5bb336a458d594bd7"
+  url "https://download.gnome.org/sources/evince/3.36/evince-3.36.2.tar.xz"
+  sha256 "34c3ce22fda0d08d61e2a3aee0d1e1117879d5fea6ff9acf67f5b0632359b608"
 
   bottle do
     sha256 "37631b73c322e2301e5bb851b7845843244e5aedd62e7eb69734d568fce42e9a" => :catalina
@@ -10,10 +10,10 @@ class Evince < Formula
     sha256 "76b38830fcf331cab2f471b69a8c7a4602c4267193fd6bce7885a238abb36c3a" => :high_sierra
   end
 
-  depends_on "appstream-glib" => :build
   depends_on "gobject-introspection" => :build
-  depends_on "intltool" => :build
   depends_on "itstool" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "adwaita-icon-theme"
   depends_on "djvulibre"
@@ -21,43 +21,36 @@ class Evince < Formula
   depends_on "gtk+3"
   depends_on "hicolor-icon-theme"
   depends_on "libarchive"
+  depends_on "libgxps"
   depends_on "libsecret"
   depends_on "libspectre"
-  depends_on "libxml2"
   depends_on "poppler"
   depends_on "python@3.8"
 
+  # Patch submitted upstream at https://gitlab.gnome.org/GNOME/evince/-/merge_requests/245
+  patch :DATA
+
   def install
-    ENV["GETTEXTDATADIR"] = "#{Formula["appstream-glib"].opt_share}/gettext"
+    ENV["DESTDIR"] = "/"
 
-    # Fix build failure "ar: illegal option -- D"
-    # Reported 15 Sep 2017 https://bugzilla.gnome.org/show_bug.cgi?id=787709
-    inreplace "configure", "AR_FLAGS=crD", "AR_FLAGS=r"
+    args = %w[
+      --buildtype=release
+      -Dnautilus=false
+      -Ddjvu=enabled
+      -Dgxps=enabled
+      -Dcomics=enabled
+      -Dgtk_doc=false
+      -Dintrospection=true
+      -Dbrowser_plugin=false
+      -Dgspell=enabled
+      -Ddbus=false
+    ]
 
-    # Add MacOS mime-types to the list of supported comic book archive mime-types
-    # Submitted upstream at https://gitlab.gnome.org/GNOME/evince/merge_requests/157
-    inreplace "configure", "COMICS_MIME_TYPES=\"",
-      "COMICS_MIME_TYPES=\"application/x-rar;application/zip;application/x-cb7;" \
-                          "application/x-7z-comperssed;application/x-tar;"
-
-    # forces use of gtk3-update-icon-cache instead of gtk-update-icon-cache. No bugreport should
-    # be filed for this since it only occurs because Homebrew renames gtk+3's gtk-update-icon-cache
-    # to gtk3-update-icon-cache in order to avoid a collision between gtk+ and gtk+3.
-    inreplace "data/Makefile.in", "gtk-update-icon-cache", "gtk3-update-icon-cache"
-
-    xy = Language::Python.major_minor_version Formula["python@3.8"].opt_bin/"python3"
-    ENV.append_path "PYTHONPATH", "#{Formula["libxml2"].opt_lib}/python#{xy}/site-packages"
-
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}",
-                          "--disable-nautilus",
-                          "--disable-schemas-compile",
-                          "--enable-introspection",
-                          "--enable-djvu",
-                          "--disable-browser-plugin"
-    system "make", "install"
+    mkdir "build" do
+      system "meson", *std_meson_args, *args, ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
+    end
   end
 
   def post_install
@@ -69,3 +62,117 @@ class Evince < Formula
     assert_match version.to_s, shell_output("#{bin}/evince --version")
   end
 end
+
+__END__
+diff --git a/backend/comics/meson.build b/backend/comics/meson.build
+index 66b73a9..f1a80ca 100644
+--- a/backend/comics/meson.build
++++ b/backend/comics/meson.build
+@@ -20,6 +20,7 @@ shared_module(
+   link_depends: backends_symbol_map,
+   install: true,
+   install_dir: ev_backendsdir,
++  name_suffix: name_suffix,
+ )
+
+ test_name = 'test-ev-archive'
+diff --git a/backend/djvu/meson.build b/backend/djvu/meson.build
+index 604450c..b532897 100644
+--- a/backend/djvu/meson.build
++++ b/backend/djvu/meson.build
+@@ -14,4 +14,5 @@ shared_module(
+   link_depends: backends_symbol_map,
+   install: true,
+   install_dir: ev_backendsdir,
++  name_suffix: name_suffix,
+ )
+diff --git a/backend/dvi/meson.build b/backend/dvi/meson.build
+index 9f65936..c3bea84 100644
+--- a/backend/dvi/meson.build
++++ b/backend/dvi/meson.build
+@@ -24,4 +24,5 @@ shared_module(
+   link_depends: backends_symbol_map,
+   install: true,
+   install_dir: ev_backendsdir,
++  name_suffix: name_suffix,
+ )
+diff --git a/backend/pdf/meson.build b/backend/pdf/meson.build
+index b1f3be4..f89fce2 100644
+--- a/backend/pdf/meson.build
++++ b/backend/pdf/meson.build
+@@ -15,4 +15,5 @@ shared_module(
+   link_depends: backends_symbol_map,
+   install: true,
+   install_dir: ev_backendsdir,
++  name_suffix: name_suffix,
+ )
+diff --git a/backend/ps/meson.build b/backend/ps/meson.build
+index 3569bd1..109cb52 100644
+--- a/backend/ps/meson.build
++++ b/backend/ps/meson.build
+@@ -8,4 +8,5 @@ shared_module(
+   link_depends: backends_symbol_map,
+   install: true,
+   install_dir: ev_backendsdir,
++  name_suffix: name_suffix,
+ )
+diff --git a/backend/tiff/meson.build b/backend/tiff/meson.build
+index b33cc1d..bb14a1d 100644
+--- a/backend/tiff/meson.build
++++ b/backend/tiff/meson.build
+@@ -13,4 +13,5 @@ shared_module(
+   link_depends: backends_symbol_map,
+   install: true,
+   install_dir: ev_backendsdir,
++  name_suffix: name_suffix,
+ )
+diff --git a/backend/xps/meson.build b/backend/xps/meson.build
+index 6306599..361ac7b 100644
+--- a/backend/xps/meson.build
++++ b/backend/xps/meson.build
+@@ -8,4 +8,5 @@ shared_module(
+   link_depends: backends_symbol_map,
+   install: true,
+   install_dir: ev_backendsdir,
++  name_suffix: name_suffix,
+ )
+diff --git a/browser-plugin/meson.build b/browser-plugin/meson.build
+index af1dc69..84d0311 100644
+--- a/browser-plugin/meson.build
++++ b/browser-plugin/meson.build
+@@ -52,4 +52,5 @@ shared_module(
+   cpp_args: cppflags,
+   install: true,
+   install_dir: browser_plugin_dir,
++  name_suffix: name_suffix,
+ )
+diff --git a/meson.build b/meson.build
+index 50f778d..b530758 100644
+--- a/meson.build
++++ b/meson.build
+@@ -429,6 +429,13 @@ endif
+ mime_types_conf = configuration_data()
+ mime_types_conf.set('EVINCE_MIME_TYPES', ';'.join(evince_mime_types))
+
++# GLib on macOS expects so as shared_module suffix, while meson uses dylib by default
++if host_machine.system() == 'darwin'
++  name_suffix = 'so'
++else
++  name_suffix = []
++endif
++
+ subdir('cut-n-paste')
+ subdir('libdocument')
+ subdir('backend')
+diff --git a/properties/meson.build b/properties/meson.build
+index cd64dde..50e65be 100644
+--- a/properties/meson.build
++++ b/properties/meson.build
+@@ -25,5 +25,6 @@ if enable_nautilus
+     link_with: libevproperties,
+     install: true,
+     install_dir: nautilus_extension_dir,
++    name_suffix: name_suffix,
+   )
+ endif
+
