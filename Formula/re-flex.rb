5450ec09923d87ac553b8f730fda5f1bb025f71e
chenrui <chenrui333@gmail.com>
2020-05-30 19:49:09 +0000

    re-flex: update 2.0.1 bottle.

--- Formula/re-flex.rb
+++ Formula/re-flex.rb
@@ -6,9 +6,9 @@ class ReFlex < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c0660554b8262f432ea63aa1d1b9e59edbc5eec33c3009ecaae49b81a381037d" => :catalina
-    sha256 "0787591f3afb9fa58fc78f20ae42281687b54d4d06e8c74f2dcf499123e4470a" => :mojave
-    sha256 "a6d97617c06828a8b0a30c57b8b9693ed13a656be192485a783e6efb8690e86d" => :high_sierra
+    sha256 "a071e974001152622adff0f712d5b4dab9f8e8d8a9b01f5b19dee9b138d5710c" => :catalina
+    sha256 "99a40921e321271478e36b7db91f9023f5f9a6364af391878b6b1640447191b2" => :mojave
+    sha256 "8c4c45dcd7566c848053bdf013cb5426ffd2801d0972ed6c231097ef85a9ce08" => :high_sierra
   end
 
   depends_on "pcre2"
