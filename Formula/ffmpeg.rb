eec00be03e68e51b29adf9fba88e0f020b31fdf9
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-31 10:22:33 -0400

    ffmpeg: revision for x265

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -3,6 +3,7 @@ class Ffmpeg < Formula
   homepage "https://ffmpeg.org/"
   url "https://ffmpeg.org/releases/ffmpeg-4.2.3.tar.xz"
   sha256 "9df6c90aed1337634c1fb026fb01c154c29c82a64ea71291ff2da9aacb9aad31"
+  revision 1
   head "https://github.com/FFmpeg/FFmpeg.git"
 
   bottle do
