71202b6807e4bf2542decbf55ccbad7c25e6facb
Gary Ewan Park <gep13@gep13.co.uk>
2020-05-31 22:31:22 +0100

    cake 0.38.1 (#55509)
    * cake 0.38.1
    
    * remove cake.rb.orig
    
    Co-authored-by: chenrui <rui@meetup.com>

--- Formula/cake.rb
+++ Formula/cake.rb
@@ -1,8 +1,11 @@
 class Cake < Formula
+  # As discussed with chenrui333 in this PR: https://github.com/Homebrew/homebrew-core/pull/55500#issuecomment-636390974
+  # Cake uses the pre-release setting on all releases.  This will change
+  # once we ship version 1.0.0, which is likely going to be our next release.
   desc "Cross platform build automation system with a C# DSL"
   homepage "https://cakebuild.net/"
-  url "https://github.com/cake-build/cake/releases/download/v0.38.0/Cake-bin-net461-v0.38.0.zip"
-  sha256 "37623e099f1aa335dc5fad777e61e1e81aa6c60e86a66b7d44d7bf7ecc2ff124"
+  url "https://github.com/cake-build/cake/releases/download/v0.38.1/Cake-bin-net461-v0.38.1.zip"
+  sha256 "d5432d0fb478eb43df8fe8410b7aa6fcd330ed70912bf78dcc305fe362751615"
 
   bottle :unneeded
 
