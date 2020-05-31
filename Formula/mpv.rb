4cd218f3568db9af22fef687ba1f155c8195ab75
Sam Ford <1584702+samford@users.noreply.github.com>
2020-05-31 10:25:10 -0400

    mpv: revision for x265

--- Formula/mpv.rb
+++ Formula/mpv.rb
@@ -3,7 +3,7 @@ class Mpv < Formula
   homepage "https://mpv.io"
   url "https://github.com/mpv-player/mpv/archive/v0.32.0.tar.gz"
   sha256 "9163f64832226d22e24bbc4874ebd6ac02372cd717bef15c28a0aa858c5fe592"
-  revision 2
+  revision 3
   head "https://github.com/mpv-player/mpv.git"
 
   bottle do
