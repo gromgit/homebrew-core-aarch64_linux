6c19cacd601e748117a58b609e6712349821149c
chenrui <chenrui333@gmail.com>
2020-05-31 19:51:41 -0400

    homeshick 2.0.0 (#55573)

--- Formula/homeshick.rb
+++ Formula/homeshick.rb
@@ -1,8 +1,8 @@
 class Homeshick < Formula
   desc "Git dotfiles synchronizer written in bash"
   homepage "https://github.com/andsens/homeshick"
-  url "https://github.com/andsens/homeshick/archive/v1.1.0.tar.gz"
-  sha256 "1894675f4f2ab002a97574445c03c7c7336c996c01023b7fc5e6668dce7d5a3c"
+  url "https://github.com/andsens/homeshick/archive/v2.0.0.tar.gz"
+  sha256 "14a538bfc2e7cb6bfd35c984cdedbf3d3293413a70cc67f685dbfbd33ce64fdd"
   head "https://github.com/andsens/homeshick.git"
 
   bottle :unneeded
