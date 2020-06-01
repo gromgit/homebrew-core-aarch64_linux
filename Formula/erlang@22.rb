1b2a2eb192b9a83c9fecf0e5e04a5a302846cbc8
chenrui <chenrui333@gmail.com>
2020-06-01 04:05:17 +0000

    erlang@22: update 22.3.4.1 bottle.

--- Formula/erlang@22.rb
+++ Formula/erlang@22.rb
@@ -7,9 +7,9 @@ class ErlangAT22 < Formula
 
   bottle do
     cellar :any
-    sha256 "2a479a0747b354992d65e6abf4d6bc7b776b460fb9e69b975ec1e5bacc8d21a0" => :catalina
-    sha256 "f3e6720c3ca38669232e46f2ffc9abaea1626ef0286af8fc1fe6cfe34ab3efd3" => :mojave
-    sha256 "c3a070c5f750e1cde904346475ce91dddab621e2ca21e522893b32dd720cef23" => :high_sierra
+    sha256 "db3e6d6f06cd804682b311aab2bdd91eb2e078b7c5b6b7f0e8252bba18edd673" => :catalina
+    sha256 "5d3bfe372a4fb0427b56aaaf27eef585d960b7852bd935819f867d74805ea982" => :mojave
+    sha256 "63d1888b4f764e16bc9d788ae62e0c9efab0db0b2dd39ca75cb498577dd54ebf" => :high_sierra
   end
 
   keg_only :versioned_formula
