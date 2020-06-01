4e7e4235174b4e22e766b2839e26b5ac3634099f
Sam Ford <1584702+samford@users.noreply.github.com>
2020-06-01 01:12:20 +0000

    minidlna: update 1.2.1_3 bottle.

--- Formula/minidlna.rb
+++ Formula/minidlna.rb
@@ -7,9 +7,9 @@ class Minidlna < Formula
 
   bottle do
     cellar :any
-    sha256 "0a82c3c230a67f0271ccd965714a311f91cca8245f1ac99fe3da224a77767f17" => :catalina
-    sha256 "c0dd14e634b64fffddd900a81db969fe74b26a3a01638f4959852c1b9cf544c9" => :mojave
-    sha256 "120b4962ca10c9463ebe1c1af3df56ce2b31def22e9e97a8327c879a7ae4c5c0" => :high_sierra
+    sha256 "200ede8d7a76a0ddf22978ec19f464e7716ae1e33c0c01b17877de7fcf0a0ea9" => :catalina
+    sha256 "c8b56b111f9625c1baaf66e9f06f6e7df6b039e1ebb188995edb16c2e264830c" => :mojave
+    sha256 "09e2127980deb0e8ed824e72bfa575f110b737ed4fa653ef0edb629faa815369" => :high_sierra
   end
 
   head do
