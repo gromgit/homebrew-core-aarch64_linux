94c907ef4a275dc34cbed66a7cb0ab05ccba7fad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-01 08:06:35 +0000

    v2ray-plugin: update 1.3.1 bottle.

--- Formula/v2ray-plugin.rb
+++ Formula/v2ray-plugin.rb
@@ -7,9 +7,9 @@ class V2rayPlugin < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ee3169521266e5bf5140bec4a3bc1ad573d965bb46c23fff22697bc257d9dfb7" => :catalina
-    sha256 "3c74254d3ea548d60bb3f7287ef8109a4cce77434edba3d61ddffa8b43f107a5" => :mojave
-    sha256 "a4f3f9651cba61a169de22cb0e933675baacd7089d8db69f91acf92e3f9e5fab" => :high_sierra
+    sha256 "891f541e150a393ff20caa78eb79ef12f60929fb9e5b35826e2e639c46a61dc2" => :catalina
+    sha256 "cb8ff7b812aa561f9e23935461968ba1c26cbe393c599aab4e1753b37702748b" => :mojave
+    sha256 "f11b330c3dc9c445b757188057c93ce94de89f03f4adfa1a8c6405f5ba66b400" => :high_sierra
   end
 
   depends_on "go" => :build
