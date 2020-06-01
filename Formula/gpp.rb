4d9c9c0cc8899d0c493138f004801f692cfe47d2
Rui Chen <rui@meetup.com>
2020-06-01 10:26:23 -0400

    gpp 2.26
    Closes #55618.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gpp.rb
+++ Formula/gpp.rb
@@ -1,8 +1,8 @@
 class Gpp < Formula
   desc "General-purpose preprocessor with customizable syntax"
   homepage "https://logological.org/gpp"
-  url "https://files.nothingisreal.com/software/gpp/gpp-2.25.tar.bz2"
-  sha256 "16ba9329208f587f96172f951ad3d24a81afea6a5b7836fe87955726eacdd19f"
+  url "https://files.nothingisreal.com/software/gpp/gpp-2.26.tar.bz2"
+  sha256 "4176aa5e37be1c72cb8a90a371ecb2d3388c772814a34debe0ff581f2e1dccb3"
 
   bottle do
     cellar :any_skip_relocation
