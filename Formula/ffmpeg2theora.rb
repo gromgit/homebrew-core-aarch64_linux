6e29e5bf52b1092830d790de56d918baeafe47e3
Sam Ford <1584702+samford@users.noreply.github.com>
2020-06-01 01:12:20 +0000

    ffmpeg2theora: update 0.30_6 bottle.

--- Formula/ffmpeg2theora.rb
+++ Formula/ffmpeg2theora.rb
@@ -8,9 +8,9 @@ class Ffmpeg2theora < Formula
 
   bottle do
     cellar :any
-    sha256 "00e261c0c5ef700021b5ddf40fd16cd897a40fc4fd045714a0c54e0068c279d8" => :catalina
-    sha256 "c4930fca649508ff83fc15a2e1f51edf59871f1e3be10fb1385298a0bc493c27" => :mojave
-    sha256 "11f4f614943e58ec46835b7434a35dcd90b46654849fc7c626f8d781b05f4fcf" => :high_sierra
+    sha256 "8c9c5de4f7abb7aff89f0d29a5234e549dc9a3ab8a58f7dbc79ef84e290cf3c9" => :catalina
+    sha256 "955b11ab8815ad86a02fdd7f7454880108a191d8b406c499b5d5314f4498656c" => :mojave
+    sha256 "f91d9c881ea674c6b6b02f83a6ef53c6e30554c7b2828a9e706eb38cce33220f" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
