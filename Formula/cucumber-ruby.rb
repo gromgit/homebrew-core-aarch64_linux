83bcb48920e58ca67167515f49ebcaadfdb6b269
Sean Molenaar <smillerdev@me.com>
2020-06-02 15:29:03 +0200

    cucumber-ruby 4.0.0
    Closes #55653.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cucumber-ruby.rb
+++ Formula/cucumber-ruby.rb
@@ -1,8 +1,8 @@
 class CucumberRuby < Formula
   desc "Cucumber for Ruby"
   homepage "https://cucumber.io"
-  url "https://github.com/cucumber/cucumber-ruby/archive/v3.1.2.tar.gz"
-  sha256 "b0193098fdedd9ffbf8dc35dfc53113fb55a4f8a991b93ee0b44c1bfc9fcff45"
+  url "https://github.com/cucumber/cucumber-ruby/archive/v4.0.0.tar.gz"
+  sha256 "de1eb7b99d0e03e851e8d7d1aacb9939590dc7f5d2d2d91180917e6f20c22ebe"
 
   bottle do
     cellar :any_skip_relocation
@@ -11,7 +11,10 @@ class CucumberRuby < Formula
     sha256 "290a326dfcdae9a4cb61765761814ba070729a8d0076e4110e6bc536905b6976" => :high_sierra
   end
 
-  uses_from_macos "ruby"
+  depends_on "pkg-config" => :build
+
+  uses_from_macos "libffi", :since => :catalina
+  uses_from_macos "ruby", :since => :catalina
 
   def install
     ENV["GEM_HOME"] = libexec
