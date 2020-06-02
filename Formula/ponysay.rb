c364adefb3c10d9423b314f7b96bf2e4cea32bb1
LS <latifsulistyo.me@gmail.com>
2020-06-02 14:43:05 +0700

    ponysay: add head and build dependencies
    Closes #55652.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ponysay.rb
+++ Formula/ponysay.rb
@@ -1,9 +1,19 @@
 class Ponysay < Formula
   desc "Cowsay but with ponies"
   homepage "https://github.com/erkin/ponysay/"
-  url "https://github.com/erkin/ponysay/archive/3.0.3.tar.gz"
-  sha256 "c382d7f299fa63667d1a4469e1ffbf10b6813dcd29e861de6be55e56dc52b28a"
-  revision 4
+  revision 5
+  head "https://github.com/erkin/ponysay.git"
+
+  stable do
+    url "https://github.com/erkin/ponysay/archive/3.0.3.tar.gz"
+    sha256 "c382d7f299fa63667d1a4469e1ffbf10b6813dcd29e861de6be55e56dc52b28a"
+
+    # upstream commit 16 Nov 2019, `fix: do not compare literal with "is not"`
+    patch do
+      url "https://github.com/erkin/ponysay/commit/69c23e3a.diff?full_index=1"
+      sha256 "4343703851dee3ea09f153f57c4dbd1731e5eeab582d3316fbbf938f36100542"
+    end
+  end
 
   bottle do
     cellar :any_skip_relocation
@@ -12,8 +22,11 @@ class Ponysay < Formula
     sha256 "a502ed3340bc2c7591788c8747c8175e0ac7902bfbfdf73454aef09d39a0db16" => :high_sierra
   end
 
+  depends_on "gzip" => :build
   depends_on "coreutils"
   depends_on "python@3.8"
+
+  uses_from_macos "texinfo" => :build
 
   def install
     system "./setup.py",
@@ -26,6 +39,8 @@ class Ponysay < Formula
   end
 
   test do
-    system "#{bin}/ponysay", "-A"
+    output = shell_output("#{bin}/ponysay test")
+    assert_match "test", output
+    assert_match "____", output
   end
 end
