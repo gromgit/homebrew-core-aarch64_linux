ed5ff49ae462497103c19468ab68a4c85131048c
Caleb Xu <calebcenter@live.com>
2020-06-02 12:01:34 -0400

    inxi 3.1.01
    Closes #55674.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/inxi.rb
+++ Formula/inxi.rb
@@ -1,9 +1,9 @@
 class Inxi < Formula
   desc "Full featured CLI system information tool"
   homepage "https://smxi.org/docs/inxi.htm"
-  url "https://github.com/smxi/inxi/archive/3.1.00-1.tar.gz"
-  version "3.1.00"
-  sha256 "ee6675489467cbfb90756db9aaf55da4bc5f7506cc8166d3a42ba28963e810f1"
+  url "https://github.com/smxi/inxi/archive/3.1.01-1.tar.gz"
+  version "3.1.01"
+  sha256 "0b46e0dd3e5391c7005d08fa52365037f27bd8d7d0a56a6900d18e11720b1c7c"
   head "https://github.com/smxi/inxi.git"
 
   bottle :unneeded
