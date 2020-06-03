ac0c4943d49fe0b7b37aec6e7b461da53c9ddb7a
Mike McQuaid <mike@mikemcquaid.com>
2020-06-03 18:02:30 +0100

    zsh-lovers: remove redundant version.

--- Formula/zsh-lovers.rb
+++ Formula/zsh-lovers.rb
@@ -2,7 +2,6 @@ class ZshLovers < Formula
   desc "Tips, tricks, and examples for zsh"
   homepage "https://grml.org/zsh/#zshlovers"
   url "https://deb.grml.org/pool/main/z/zsh-lovers/zsh-lovers_0.9.1_all.deb"
-  version "0.9.1"
   sha256 "011b7931a555c77e98aa9cdd16b3c4670c0e0e3b5355e5fd60188885a6678de8"
 
   bottle :unneeded
