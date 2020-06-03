bbc1e9abd8dac4a2af8f24d76b5acb4ca24db5b7
Dawid Dziurla <dawidd0811@gmail.com>
2020-06-03 10:22:09 +0000

    bombadillo: add 2.3.1 bottle.

--- Formula/bombadillo.rb
+++ Formula/bombadillo.rb
@@ -4,6 +4,13 @@ class Bombadillo < Formula
   url "https://tildegit.org/sloum/bombadillo/archive/2.3.1.tar.gz"
   sha256 "e8076493e924bd5860d3e17884b0675ea514eea75e7b4e96da1c79ab9805731f"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e28036e78119313b05947c16b65c5d512a97b96c1a096470c2c38877bd396954" => :catalina
+    sha256 "dc47ed396c8a8c988d82b0a87f5c5c368d604f535ae5cc932ad941c22376073f" => :mojave
+    sha256 "e45a0bec0801f7719539f0ccda6ce7cfc76dccd346d6a09bb65a04f2c2238cd9" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
