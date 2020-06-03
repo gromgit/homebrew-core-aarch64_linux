d4edd94d39bc69b6729da7a8fb107c63113d44a0
Bastian Hofmann <bashofmann@gmail.com>
2020-06-03 11:29:10 +0200

    rke: update homepage link to latest version (#55729)
    Signed-off-by: Bastian Hofmann <bashofmann@gmail.com>

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -1,6 +1,6 @@
 class Rke < Formula
   desc "Rancher Kubernetes Engine, a Kubernetes installer that works everywhere"
-  homepage "https://rancher.com/docs/rke/v0.1.x/en/"
+  homepage "https://rancher.com/docs/rke/latest/en/"
   url "https://github.com/rancher/rke.git",
       :tag      => "v1.1.2",
       :revision => "cd9d26dc51b50ae7be47a9b7972fc09c128aa405"
