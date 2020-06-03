25c655f0153a67b94ace8f16ff52472ea596e7b8
Mark Moll <mmoll@rice.edu>
2020-06-02 17:54:47 -0700

    ompl 1.5.0

--- Formula/ompl.rb
+++ Formula/ompl.rb
@@ -1,8 +1,9 @@
 class Ompl < Formula
   desc "Open Motion Planning Library consists of many motion planning algorithms"
   homepage "https://ompl.kavrakilab.org/"
-  url "https://bitbucket.org/ompl/ompl/downloads/ompl-1.4.2-Source.tar.gz"
-  sha256 "03d5a661061928ced63bb945055579061665634ef217559b1f47fef842e1fa85"
+  url "https://github.com/ompl/ompl/archive/1.5.0.tar.gz"
+  sha256 "a7df8611b7823ef44c731f02897571adfc23551e85c6618d926e7720004267a5"
+  head "https://github.com/ompl/ompl.git"
 
   bottle do
     rebuild 1
@@ -12,18 +13,25 @@ class Ompl < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
   depends_on "boost"
   depends_on "eigen"
+  depends_on "flann"
+  depends_on "ode"
 
-  # fix for Boost 1.71
-  patch do
-    url "https://github.com/ompl/ompl/commit/962961fb.diff?full_index=1"
-    sha256 "56adf06b5bdc12823c04e96e764618c48a912fd01422138f15cc578f2464177a"
-  end
-
+    
   def install
     ENV.cxx11
-    system "cmake", ".", *std_cmake_args
+    args = std_cmake_args + %W[
+      -DOMPL_REGISTRATION=OFF
+      -DOMPL_BUILD_DEMOS=OFF
+      -DOMPL_BUILD_TESTS=OFF
+      -DOMPL_BUILD_PYBINDINGS=OFF
+      -DOMPL_BUILD_PYTESTS=OFF
+      -DCMAKE_DISABLE_FIND_PACKAGE_spot=ON
+      -DCMAKE_DISABLE_FIND_PACKAGE_Triangle=ON
+    ]
+    system "cmake", ".", *args
     system "make", "install"
   end
 
