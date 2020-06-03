d3798b850377b7352353c0bbaa2c4bc8d638cdc2
Sha Sha Chu <shasha@pinterest.com>
2020-06-02 23:26:22 -0700

    ktlint 0.37.0
    Closes #55726.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ktlint.rb
+++ Formula/ktlint.rb
@@ -1,8 +1,8 @@
 class Ktlint < Formula
   desc "Anti-bikeshedding Kotlin linter with built-in formatter"
   homepage "https://ktlint.github.io/"
-  url "https://github.com/pinterest/ktlint/releases/download/0.36.0/ktlint"
-  sha256 "a656342cfce5c1fa14f13353b84b1505581af246638eb970c919fb053e695d5e"
+  url "https://github.com/pinterest/ktlint/releases/download/0.37.0/ktlint"
+  sha256 "ee4a95021bc4c248169fec0633264fa2b8dc1520a62772d04b32879f19dc50fc"
 
   bottle :unneeded
 
