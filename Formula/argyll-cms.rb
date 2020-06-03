58b36b53b9f7aaeba796fd7d752f04ed0529b1a8
Mike McQuaid <mike@mikemcquaid.com>
2020-06-03 18:02:27 +0100

    argyll-cms: remove redundant version.

--- Formula/argyll-cms.rb
+++ Formula/argyll-cms.rb
@@ -2,7 +2,6 @@ class ArgyllCms < Formula
   desc "ICC compatible color management system"
   homepage "https://www.argyllcms.com/"
   url "https://www.argyllcms.com/Argyll_V2.1.2_src.zip"
-  version "2.1.2"
   sha256 "be378ca836b17b8684db05e9feaab138d711835ef00a04a76ac0ceacd386a3e3"
 
   bottle do
