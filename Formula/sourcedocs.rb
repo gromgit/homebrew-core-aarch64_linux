15ca34d808f318ed61a40cf9f13391d9641f9c4e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-04 21:06:24 +0000

    sourcedocs: update 1.2.0 bottle.

--- Formula/sourcedocs.rb
+++ Formula/sourcedocs.rb
@@ -6,8 +6,8 @@ class Sourcedocs < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "709fe9bcd8d9bf1cb5285618983eb6934807eb1c58d893d0576e7889815d9a03" => :catalina
-    sha256 "ae52c1dcdd808cb5da7bf34f0b293c25056df18308ed95754b0f9cef0a4fabfc" => :mojave
+    sha256 "32bc59646532b362651b126946cbf051a90b2b1da5d07a614d50fc0b0bc1efd1" => :catalina
+    sha256 "3bfe25d253eecef289c822012efddc14d9bd331eba0f3f54fe88e7d0cf2aa924" => :mojave
   end
 
   depends_on :xcode => ["10.3", :build, :test]
