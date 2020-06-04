b040ecacce31bcc796933eb3a007feff1ad3c62f
Danny Moesch <danny.moesch@icloud.com>
2020-06-04 14:35:41 +0100

    vivid 0.6.0 (new formula)
    Closes #61447.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

---
+++ Formula/vivid.rb
@@ -0,0 +1,17 @@
+class Vivid < Formula
+  desc "Generator for LS_COLORS with support for multiple color themes"
+  homepage "https://github.com/sharkdp/vivid"
+  url "https://github.com/sharkdp/vivid/archive/v0.6.0.tar.gz"
+  sha256 "c8640f524aef1cd4dc15286bdc6189894ad067ea79bf8c40b9ca8d9d752d161f"
+  license any_of: ["MIT", "Apache-2.0"]
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_includes shell_output("#{bin}/vivid preview molokai"), "archives.images: \e[4;38;2;249;38;114m*.bin\e[0m\n"
+  end
+end
