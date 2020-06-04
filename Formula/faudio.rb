824ab59df5c38a5512779d1ae2d91bdf6adadcd3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-04 08:06:38 +0000

    faudio: update 20.06 bottle.

--- Formula/faudio.rb
+++ Formula/faudio.rb
@@ -7,9 +7,9 @@ class Faudio < Formula
 
   bottle do
     cellar :any
-    sha256 "4bee9d3801955f6b6d345b5dced23643122a80ec071e47288f2f52a5c4254877" => :catalina
-    sha256 "e1d7d623732d27007d918936d219ca2ed9feb852300ed55df200c90b6ed47081" => :mojave
-    sha256 "383efd54c5c1e61e13fab424d20f95dba7d4464862187e830f63978f9b7391a5" => :high_sierra
+    sha256 "51fca38eae3ad1285799ebfc907f5f60223e378cce7b7a8065fe3d37d1c73566" => :catalina
+    sha256 "1baffef5100bef44d3611750287f19f0948f4e1dde0a4826e47e86f345c4b946" => :mojave
+    sha256 "a3fe52fc2ea8dc1c247f81daa37557496436e40b01227fe5162d15d48161dfe5" => :high_sierra
   end
 
   depends_on "cmake" => :build
