6a10aeddfcdd831db4cb34f8d4104ee9f1533d4b
Michka Popoff <michkapopoff@gmail.com>
2020-06-05 21:06:11 +0200

    postgresql@9.4: depend on util-linux on linux

--- Formula/postgresql@9.4.rb
+++ Formula/postgresql@9.4.rb
@@ -21,6 +21,10 @@ class PostgresqlAT94 < Formula
   uses_from_macos "libxslt"
   uses_from_macos "perl"
 
+  on_linux do
+    depends_on "util-linux"
+  end
+
   def install
     # Fix "configure: error: readline library not found"
     ENV["SDKROOT"] = MacOS.sdk_path if MacOS.version == :sierra || MacOS.version == :el_capitan
