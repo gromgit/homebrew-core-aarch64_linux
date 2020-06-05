d923749971b2464c2d156ee9861508d2a8ae0a20
Michka Popoff <michkapopoff@gmail.com>
2020-06-05 21:52:42 +0200

    statik: depend on pkg-config for linux

--- Formula/statik.rb
+++ Formula/statik.rb
@@ -19,6 +19,10 @@ class Statik < Formula
 
   uses_from_macos "libffi"
 
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
+
   conflicts_with "go-statik", :because => "both install `statik` binaries"
 
   def install
