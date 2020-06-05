5dd5fc90c50e4f8a50f24ef7bd7d2e22dd2418a7
Michka Popoff <michkapopoff@gmail.com>
2020-06-05 21:43:18 +0200

    openjdk: depend on pkg-config for linux

--- Formula/openjdk.rb
+++ Formula/openjdk.rb
@@ -17,6 +17,10 @@ class Openjdk < Formula
 
   depends_on "autoconf" => :build
 
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
+
   resource "boot-jdk" do
     url "https://download.java.net/java/GA/jdk12.0.2/e482c34c86bd4bf8b56c0b35558996b9/10/GPL/openjdk-12.0.2_osx-x64_bin.tar.gz"
     sha256 "675a739ab89b28a8db89510f87cb2ec3206ec6662fb4b4996264c16c72cdd2a1"
