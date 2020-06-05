c5dd41eec30c77288871fd886f7d592cd4b68209
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-05 22:06:36 +0000

    choose-gui: update 1.1 bottle.

--- Formula/choose-gui.rb
+++ Formula/choose-gui.rb
@@ -6,12 +6,9 @@ class ChooseGui < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "918bd486105a1b5b4c7e25c521f80fcaacb0ae82689b9c0a4692736166611f3e" => :catalina
-    sha256 "a4c32058d0658cbaa5562974eb2d98149f6adde6b95b91bfb464e8e8e12ff267" => :mojave
-    sha256 "bcd4c1c75dad408adfa451351a719ad126e2dadb26a49a4316d7d170ed0c6702" => :high_sierra
-    sha256 "5968418e6ee9717884d32f98e840cf02549165a9efc8f42e32549e3ae800c4cd" => :sierra
-    sha256 "100c28baee98417c8ce7041956c4a62fe9126b6b36a0f3e186b33500b6b1f2fb" => :el_capitan
-    sha256 "fd41325edc44dde3a61c52e310b0faa35314ce9c331b04673f55b6ebd5da28ba" => :yosemite
+    sha256 "56e072eddbb83e48e719cd4e66b25a3c1b72883d8837310f86b7c12798f9617c" => :catalina
+    sha256 "ae372dc62e19e3617efa2cabfab18e55ea2734c2b38dc9960ae3de9ab8935269" => :mojave
+    sha256 "3cd12daccad8f8381c3b37f2eb59a48ff2671f18e4cbc539daa1cb6ee606568c" => :high_sierra
   end
 
   depends_on :xcode => :build
