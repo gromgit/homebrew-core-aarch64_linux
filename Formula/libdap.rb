3f7ea3b2948f13a98386eb583242d4be9552f402
Michka Popoff <michkapopoff@gmail.com>
2020-06-05 21:03:17 +0200

    libdap: depend on util-linux on linux

--- Formula/libdap.rb
+++ Formula/libdap.rb
@@ -26,6 +26,10 @@ class Libdap < Formula
   uses_from_macos "flex" => :build
   uses_from_macos "curl"
 
+  on_linux do
+    depends_on "util-linux"
+  end
+
   def install
     args = %W[
       --prefix=#{prefix}
