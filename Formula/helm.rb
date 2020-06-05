6ff7241760da53a582f20c9364613143f03a2423
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-05 21:06:26 +0000

    helm: update 3.2.2 bottle.

--- Formula/helm.rb
+++ Formula/helm.rb
@@ -8,9 +8,9 @@ class Helm < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "10b68227cc61d6ba7511268904118c240a3a5e2c9e289a5fe05b3cac44a8b4ef" => :catalina
-    sha256 "3ca91e66102ea11a510c7ae9cf4bdee1ed28bcdfedc6b765de2dee9bad1fcd28" => :mojave
-    sha256 "7123a1745847d8f4cf68df8a9c48970026329ae222c9180a04dd945e29db3f37" => :high_sierra
+    sha256 "021167b314b89312f605a67c6c2e6b9be3af5b638fc7f30bcdbfdbf47e21e104" => :catalina
+    sha256 "122c7290f343257897e759560130538f42cabb1e62e211bc4b8e9c5a364aa223" => :mojave
+    sha256 "94246a13ae516c00c90b7ffe653b175a01729659e7ce19970ff534419707b258" => :high_sierra
   end
 
   depends_on "go@1.13" => :build
