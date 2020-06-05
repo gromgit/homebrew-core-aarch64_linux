c13b4404c85af799977a331eaee734735ea4d5db
Michka Popoff <michkapopoff@gmail.com>
2020-06-05 21:56:00 +0200

    tealdeer: depend on pkg-config for linux

--- Formula/tealdeer.rb
+++ Formula/tealdeer.rb
@@ -14,6 +14,10 @@ class Tealdeer < Formula
 
   depends_on "rust" => :build
 
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
+
   conflicts_with "tldr", :because => "both install `tldr` binaries"
 
   def install
