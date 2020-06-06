b441124680fe390edeaf26ea8321528f3747efd4
Alexander Bayandin <a.bayandin@gmail.com>
2020-06-06 07:46:50 +0000

    devd: update 0.9 bottle.

--- Formula/devd.rb
+++ Formula/devd.rb
@@ -7,10 +7,10 @@ class Devd < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d394040c10c42a1e4ac22af9fff69a8fa93e26cdbc3c9d21cec0c1bedb0bfc12" => :catalina
-    sha256 "14f2c29b4bd17afe84568daa6800e9500438c28b02b7bc83f2a76252bb79099b" => :mojave
-    sha256 "64faee99fcec04c2d603610f169900a1379bd5961be352745078c38dfc5cdb17" => :high_sierra
-    sha256 "3023d0d25dbca5e3790fbf42c6f093d8241970700bbb4d6ca7b516c23ff25d77" => :sierra
+    rebuild 1
+    sha256 "26a21f49c1abdafb1be22d48bc4186c18c0ff53620cc60a35ac778ed22a46c5a" => :catalina
+    sha256 "21b10d2ef5cdb6a11cb0fa64da6cb4ef5049d5bc9bc411ad4192127224709f2e" => :mojave
+    sha256 "2cc541a0a844b83e60b89f79e1f296d391737fb635f1bd43ff5dd022a07dc67b" => :high_sierra
   end
 
   depends_on "go" => :build
