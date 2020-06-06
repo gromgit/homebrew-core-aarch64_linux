a74ad2f414355e84c3df837415427ba7ae5859f0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-06 01:07:42 +0000

    contentful-cli: update 1.4.6 bottle.

--- Formula/contentful-cli.rb
+++ Formula/contentful-cli.rb
@@ -9,9 +9,9 @@ class ContentfulCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f852a9df7816978818bb9adf1738635b57d53b5f4863d32d746f6edf4410c69b" => :catalina
-    sha256 "ff8059e2047db6d3c85b1871f89c738e563742b2a732a5234590438654cd582c" => :mojave
-    sha256 "593c9834ec4ff8fd6e798be9b3ac9b13bb096a148722d2e8c0d81d84caec1671" => :high_sierra
+    sha256 "fe5aa6295d872aa146ca0b8e11e5cfe579e2d0459381256976db182c8e9b40fe" => :catalina
+    sha256 "4e8ac82aaa6496e06a9cfbd5a95c11328b8bab2f6d54d700a40ddafe499e6989" => :mojave
+    sha256 "c18561168e9409c346b0f12156c79e056a2aea7b70913ed76ce950a16cb0a2e5" => :high_sierra
   end
 
   depends_on "node"
