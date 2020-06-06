dd0512e386d0d698bcb8c6202e4a0c31abcf6d8a
chenrui <chenrui333@gmail.com>
2020-06-06 16:32:52 -0400

    picard-tools 2.22.9 (#55896)

--- Formula/picard-tools.rb
+++ Formula/picard-tools.rb
@@ -1,8 +1,8 @@
 class PicardTools < Formula
   desc "Tools for manipulating HTS data and formats"
   homepage "https://broadinstitute.github.io/picard/"
-  url "https://github.com/broadinstitute/picard/releases/download/2.22.8/picard.jar"
-  sha256 "b447ef8a38c88fd72b96fea8cebdf0c3cba567df43f987fa1437e4e3a83c683c"
+  url "https://github.com/broadinstitute/picard/releases/download/2.22.9/picard.jar"
+  sha256 "a3d8830f68443a745cca9e1bab9adbc1c435fe3639709b118f5a89858a12a362"
 
   bottle :unneeded
 
