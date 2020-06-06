9368fe1dbeb9d751ec63c45e334a730ecd593ca2
Caleb Xu <calebcenter@live.com>
2020-06-05 20:49:41 -0400

    nomad 0.11.3
    Closes #55867.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nomad.rb
+++ Formula/nomad.rb
@@ -1,8 +1,8 @@
 class Nomad < Formula
   desc "Distributed, Highly Available, Datacenter-Aware Scheduler"
   homepage "https://www.nomadproject.io"
-  url "https://github.com/hashicorp/nomad/archive/v0.11.2.tar.gz"
-  sha256 "d0da412d23dfeba313a9824e9cc56c4aeb6180156d540d5a13313c510f6d549b"
+  url "https://github.com/hashicorp/nomad/archive/v0.11.3.tar.gz"
+  sha256 "4ac797fd87a9e24a99e45a5dae91dd1064ab90a9da180ee2d0914a6ded4d3272"
   head "https://github.com/hashicorp/nomad.git"
 
   bottle do
