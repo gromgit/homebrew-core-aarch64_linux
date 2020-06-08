0d6679c96dc81b36e2912d5d42aec7a3d0d4ceab
nandahkrishna <nanda.harishankar@gmail.com>
2020-06-09 05:24:59 +0530

    pass: update head url

--- Formula/pass.rb
+++ Formula/pass.rb
@@ -3,7 +3,7 @@ class Pass < Formula
   homepage "https://www.passwordstore.org/"
   url "https://git.zx2c4.com/password-store/snapshot/password-store-1.7.3.tar.xz"
   sha256 "2b6c65846ebace9a15a118503dcd31b6440949a30d3b5291dfb5b1615b99a3f4"
-  head "https://git.zx2c4.com/password-store", :using => :git
+  head "https://git.zx2c4.com/password-store.git"
 
   bottle do
     cellar :any_skip_relocation
