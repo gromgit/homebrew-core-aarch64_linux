3f301ca07136779d4387b8d0b0db84fb96bab547
Dustin Rodrigues <dust.rod@gmail.com>
2020-06-08 12:48:22 -0400

    eg-examples 1.2.0
    Closes #55938.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/eg-examples.rb
+++ Formula/eg-examples.rb
@@ -3,9 +3,8 @@ class EgExamples < Formula
 
   desc "Useful examples at the command-line"
   homepage "https://github.com/srsudar/eg"
-  url "https://files.pythonhosted.org/packages/a6/93/38075713a7968a9e8484e894f604f99a68e443e0f9db0ed48063b1241969/eg-1.1.1.tar.gz"
-  sha256 "3faa5fb453d8ba113975a1f31e37ace94867539ba9d46a40af4cea90028a04e4"
-  revision 2
+  url "https://files.pythonhosted.org/packages/59/06/7281154a4cd961f56302d9cac300cc8fc965b16d04ce797ed622a2f53586/eg-1.2.0.tar.gz"
+  sha256 "ac9827824c2c9aee0fd0a54ca57440021a0a87868b503b215c2c273a035bef59"
 
   bottle do
     cellar :any_skip_relocation
