103a5bd4fd00db4ff6e7c989f18c29e18f9de09a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-09 20:06:44 +0000

    cargo-c: update 0.6.7 bottle.

--- Formula/cargo-c.rb
+++ Formula/cargo-c.rb
@@ -6,9 +6,9 @@ class CargoC < Formula
 
   bottle do
     cellar :any
-    sha256 "b677282c113016d5a2292870ff316daa99dcee19adbbbfd466340f88a66e0794" => :catalina
-    sha256 "2e781da01fa6295a8cb8ad4240bccdc0148af0dfd712475d26e373a19493c758" => :mojave
-    sha256 "89570707a2c183c68c78b146e4d77d1c58ca323be2455b3a31f5e60624b06138" => :high_sierra
+    sha256 "9fd28e5611437a0f5d436c11befb65f501a8b51bac65e50e52405b5c0dafa65f" => :catalina
+    sha256 "c36758d5dc3a14d0839b9f5c97f9242804091a85ffd06fe79e9618e227edc345" => :mojave
+    sha256 "5a6cf9b1d532128211a0095d9c9641e6982540dd7493ed65a85769e3975cfe64" => :high_sierra
   end
 
   depends_on "rust" => :build
