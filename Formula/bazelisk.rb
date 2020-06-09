2d58168f64b8b56d906af02a879deb72d51afd1a
Caleb Xu <calebcenter@live.com>
2020-06-09 09:03:57 -0400

    bazelisk 1.5.0
    Closes #55966.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bazelisk.rb
+++ Formula/bazelisk.rb
@@ -2,8 +2,8 @@ class Bazelisk < Formula
   desc "User-friendly launcher for Bazel"
   homepage "https://github.com/bazelbuild/bazelisk/"
   url "https://github.com/bazelbuild/bazelisk.git",
-      :tag      => "v1.4.0",
-      :revision => "1e4c5df59ebf03feb6ec01fc489751f1db62b89e"
+      :tag      => "v1.5.0",
+      :revision => "9f7127db3c137e32a21484f9345bbc0da8301135"
   head "https://github.com/bazelbuild/bazelisk.git"
 
   bottle do
