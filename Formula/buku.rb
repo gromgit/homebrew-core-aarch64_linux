ffb05e27a9fb3a8905312b28340f4123f40a5be1
Michka Popoff <michkapopoff@gmail.com>
2020-06-09 23:17:47 +0200

    buku: depend on pkg-config for linux

--- Formula/buku.rb
+++ Formula/buku.rb
@@ -20,6 +20,10 @@ class Buku < Formula
   uses_from_macos "expect" => :test
   uses_from_macos "libffi"
 
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
+
   resource "beautifulsoup4" do
     url "https://files.pythonhosted.org/packages/3b/e4/7cfc641f11e0eef60123912611a5c9ee7d4638da7325878b695b9ae4bb6f/beautifulsoup4-4.9.0.tar.gz"
     sha256 "594ca51a10d2b3443cbac41214e12dbb2a1cd57e1a7344659849e2e20ba6a8d8"
