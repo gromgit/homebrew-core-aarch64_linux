275037b0d314eb67c7aba2130bd6c1b210accf41
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-06-09 09:02:22 +0000

    libcouchbase@2: add 2.10.6 bottle.

--- Formula/libcouchbase@2.rb
+++ Formula/libcouchbase@2.rb
@@ -3,6 +3,12 @@ class LibcouchbaseAT2 < Formula
   homepage "https://docs.couchbase.com/c-sdk/2.10/start-using-sdk.html"
   url "https://packages.couchbase.com/clients/c/libcouchbase-2.10.6.tar.gz"
   sha256 "341d7af60d637c81a4de623168746b699f49a75358aa33d9b1b99dd0a63a7cf3"
+  bottle do
+    sha256 "b2069cc7be17f5bf8508b8262d5dee0b1948758f8d65eaab3ded9baad902100d" => :catalina
+    sha256 "70cdc91856c6c9c24e0e34ba59afffdb42fbe6c95cb0e3c2ff09f22d266b4f6d" => :mojave
+    sha256 "7dc8d8f3258f267952e2e0bf87004d650a88a7bf1ed64bbaf56934d42271ad56" => :high_sierra
+  end
+
   keg_only :versioned_formula
 
   depends_on "cmake" => :build
