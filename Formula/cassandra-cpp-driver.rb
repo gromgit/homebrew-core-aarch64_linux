22385af61dc065ebf1a82ced9fa0004bb44b8240
Michka Popoff <michkapopoff@gmail.com>
2020-06-09 23:19:02 +0200

    cassandra-cpp-driver: depend on pkg-config for linux

--- Formula/cassandra-cpp-driver.rb
+++ Formula/cassandra-cpp-driver.rb
@@ -18,6 +18,10 @@ class CassandraCppDriver < Formula
 
   uses_from_macos "zlib"
 
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
+
   def install
     Dir.mkdir "build"
     Dir.chdir "build" do
