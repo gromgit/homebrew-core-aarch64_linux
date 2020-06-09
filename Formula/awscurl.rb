5a96ba199bad387036381424e906da3c92b6819a
Michka Popoff <michkapopoff@gmail.com>
2020-06-09 23:17:15 +0200

    awscurl: depend on pkg-config for linux

--- Formula/awscurl.rb
+++ Formula/awscurl.rb
@@ -19,6 +19,10 @@ class Awscurl < Formula
 
   uses_from_macos "libffi"
 
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
+
   resource "certifi" do
     url "https://files.pythonhosted.org/packages/41/bf/9d214a5af07debc6acf7f3f257265618f1db242a3f8e49a9b516f24523a6/certifi-2019.11.28.tar.gz"
     sha256 "25b64c7da4cd7479594d035c08c2d809eb4aab3a26e5a990ea98cc450c320f1f"
