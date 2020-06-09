7833ba4dccb8bdf16d6e467fc1efd2cef87dc184
Caleb Xu <calebcenter@live.com>
2020-06-09 09:37:50 -0400

    micronaut 1.3.6
    Closes #55978.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/micronaut.rb
+++ Formula/micronaut.rb
@@ -1,8 +1,8 @@
 class Micronaut < Formula
   desc "Modern JVM-based framework for building modular microservices"
   homepage "https://micronaut.io/"
-  url "https://github.com/micronaut-projects/micronaut-core/releases/download/v1.3.5/micronaut-1.3.5.zip"
-  sha256 "456ae4f241772465bfafa4e589fb572a761476186f182ec365fcfb0f486b235e"
+  url "https://github.com/micronaut-projects/micronaut-core/releases/download/v1.3.6/micronaut-1.3.6.zip"
+  sha256 "271a5af14f87e78ec8c72cfd09d04c50c96826532a5496bd9651a2775815693f"
 
   bottle :unneeded
 
