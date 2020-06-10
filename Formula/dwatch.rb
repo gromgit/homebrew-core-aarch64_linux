7d602c19f73473a3bf6f88ced7b16bddb94bb060
Dustin Rodrigues <dust.rod@gmail.com>
2020-06-10 02:39:29 -0400

    use https url for dwatch
    Closes #56033.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dwatch.rb
+++ Formula/dwatch.rb
@@ -1,7 +1,7 @@
 class Dwatch < Formula
   desc "Watch programs and perform actions based on a configuration file"
-  homepage "http://siag.nu/dwatch/"
-  url "http://siag.nu/pub/dwatch/dwatch-0.1.1.tar.gz"
+  homepage "https://siag.nu/dwatch/"
+  url "https://siag.nu/pub/dwatch/dwatch-0.1.1.tar.gz"
   sha256 "ba093d11414e629b4d4c18c84cc90e4eb079a3ba4cfba8afe5026b96bf25d007"
 
   bottle do
