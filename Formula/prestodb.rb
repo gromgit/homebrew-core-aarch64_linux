4350480ebec7f5f08a4cf59141da2d3cf191af61
Dipti Borkar <1181877+dborkar@users.noreply.github.com>
2020-06-10 01:27:16 -0700

    prestodb 0.235.1
    Closes #56039.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/prestodb.rb
+++ Formula/prestodb.rb
@@ -1,8 +1,8 @@
 class Prestodb < Formula
   desc "Distributed SQL query engine for big data"
   homepage "https://prestodb.io"
-  url "https://search.maven.org/remotecontent?filepath=com/facebook/presto/presto-server/0.230/presto-server-0.230.tar.gz"
-  sha256 "dfa98c7d709e2c2c81e400f746cfec28665ba2005faaa8d87ed38d82b5550503"
+  url "https://search.maven.org/remotecontent?filepath=com/facebook/presto/presto-server/0.235.1/presto-server-0.235.1.tar.gz"
+  sha256 "862871609b14eb5259530de04f1a1ed69e3bb3b172490f2a1585f8f31fd9453e"
   revision 1
 
   bottle :unneeded
@@ -12,8 +12,8 @@ class Prestodb < Formula
   conflicts_with "prestosql", :because => "both install `presto` and `presto-server` binaries"
 
   resource "presto-cli" do
-    url "https://search.maven.org/remotecontent?filepath=com/facebook/presto/presto-cli/0.230/presto-cli-0.230-executable.jar"
-    sha256 "b37cff444f79f5a11998ea51cc5b2ae082c51c5ebd9fff26ded5f6550412ce88"
+    url "https://search.maven.org/remotecontent?filepath=com/facebook/presto/presto-cli/0.235.1/presto-cli-0.235.1-executable.jar"
+    sha256 "22deb730fd593eef83f3204d1b9a925eded2a6fb8a06e88b11a5e1e3d82855a8"
   end
 
   def install
@@ -34,6 +34,7 @@ class Prestodb < Formula
       -XX:+ExplicitGCInvokesConcurrent
       -XX:+HeapDumpOnOutOfMemoryError
       -XX:+ExitOnOutOfMemoryError
+      -Djdk.attach.allowAttachSelf=true
     EOS
 
     (libexec/"etc/config.properties").write <<~EOS
