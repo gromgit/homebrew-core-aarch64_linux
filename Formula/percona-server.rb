8ca1539fdf3e625b92f6909f924cb736ed9be35c
Mike McQuaid <mike@mikemcquaid.com>
2020-06-10 10:24:15 +0100

    percona-server: remove Mavericks code.

--- Formula/percona-server.rb
+++ Formula/percona-server.rb
@@ -16,11 +16,6 @@ class PerconaServer < Formula
   end
 
   depends_on "cmake" => :build
-
-  # https://github.com/Homebrew/homebrew-core/issues/1475
-  # Needs at least Clang 3.3, which shipped alongside Lion.
-  # Note: MySQL themselves don't support anything below Sierra.
-  depends_on :macos => :yosemite
   depends_on "openssl@1.1"
 
   conflicts_with "mariadb", "mysql",
