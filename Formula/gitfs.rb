ff2410413e83b508a11c5418f39c5c12d8575091
Michka Popoff <michkapopoff@gmail.com>
2020-06-10 07:34:13 +0200

    gitfs: depend on pkg-config for linux

--- Formula/gitfs.rb
+++ Formula/gitfs.rb
@@ -21,6 +21,10 @@ class Gitfs < Formula
 
   uses_from_macos "libffi"
 
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
+
   resource "atomiclong" do
     url "https://files.pythonhosted.org/packages/86/8c/70aea8215c6ab990f2d91e7ec171787a41b7fbc83df32a067ba5d7f3324f/atomiclong-0.1.1.tar.gz"
     sha256 "cb1378c4cd676d6f243641c50e277504abf45f70f1ea76e446efcdbb69624bbe"
