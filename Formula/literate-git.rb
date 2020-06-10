11924ef970afc2c4fc0b7a29aeb2d8b55e9f2a5e
Michka Popoff <michkapopoff@gmail.com>
2020-06-10 07:38:13 +0200

    literate-git: depend on pkg-config for linux

--- Formula/literate-git.rb
+++ Formula/literate-git.rb
@@ -19,6 +19,10 @@ class LiterateGit < Formula
 
   uses_from_macos "libffi"
 
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
+
   resource "cached-property" do
     url "https://files.pythonhosted.org/packages/57/8e/0698e10350a57d46b3bcfe8eff1d4181642fd1724073336079cb13c5cf7f/cached-property-1.5.1.tar.gz"
     sha256 "9217a59f14a5682da7c4b8829deadbfc194ac22e9908ccf7c8820234e80a1504"
