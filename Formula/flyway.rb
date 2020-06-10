d1ba4f985ae7ac048a97b27fac6d07d21f6febda
Caleb Xu <calebcenter@live.com>
2020-06-10 18:46:35 -0400

    flyway 6.4.4 (#56062)

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/6.4.3/flyway-commandline-6.4.3.tar.gz"
-  sha256 "ebf0877487bfe97b44492885bd9daf7230b877559ec7011c8efec40d07c3eb15"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/6.4.4/flyway-commandline-6.4.4.tar.gz"
+  sha256 "d5b3f6b7c6fda311cf41b769283cecf6a1e42de17f67adc300634b62309eec9b"
 
   bottle :unneeded
 
