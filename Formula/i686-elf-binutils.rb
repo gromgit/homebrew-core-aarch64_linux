a84d7170323973893cdf6266112a36111f336bf9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-10 12:07:36 +0000

    i686-elf-binutils: add 2.34 bottle.

--- Formula/i686-elf-binutils.rb
+++ Formula/i686-elf-binutils.rb
@@ -5,6 +5,12 @@ class I686ElfBinutils < Formula
   mirror "https://ftpmirror.gnu.org/binutils/binutils-2.34.tar.gz"
   sha256 "53537d334820be13eeb8acb326d01c7c81418772d626715c7ae927a7d401cab3"
 
+  bottle do
+    sha256 "c2d082bc08ccb4ce1c870a3e670670eb756f0da433c9a29246277d42814263c1" => :catalina
+    sha256 "eb5d102d5ed4cff2710d084d80a3ecba884d39364b7616df9944788090e517e7" => :mojave
+    sha256 "7b6014e9338df52a85c747915bafe09af2abba4d9688ac19dc3de73958e4bcde" => :high_sierra
+  end
+
   def install
     system "./configure", "--target=i686-elf",
                           "--prefix=#{prefix}",
