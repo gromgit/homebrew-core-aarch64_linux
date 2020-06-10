8f5966606795c95239d19cea2a2584ccf2dde3b5
Dong Wang <wd@wdicc.com>
2020-06-10 16:50:38 +0800

    assh 2.9.3
    Closes #56040.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/assh.rb
+++ Formula/assh.rb
@@ -1,8 +1,8 @@
 class Assh < Formula
   desc "Advanced SSH config - Regex, aliases, gateways, includes and dynamic hosts"
   homepage "https://manfred.life/assh"
-  url "https://github.com/moul/assh/archive/v2.9.1.tar.gz"
-  sha256 "fed8876c574061c239a1d159d9c7197e8bda94f6610f6e29e682d8b6dde60852"
+  url "https://github.com/moul/advanced-ssh-config/archive/v2.9.3.tar.gz"
+  sha256 "d700a23a97b579aa48ee8ecc6d30878acc0840d368c7a0089c7c20f6fa028c71"
   head "https://github.com/moul/assh.git"
 
   bottle do
