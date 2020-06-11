1d3e8d62a7b461af7ea062a9e63ab045cbb43991
Rylan Polster <rslpolster@gmail.com>
2020-06-11 10:14:12 -0400

    natalie: update build arguments
    Closes #56162.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/natalie.rb
+++ Formula/natalie.rb
@@ -4,6 +4,7 @@ class Natalie < Formula
   url "https://github.com/krzyzanowskim/Natalie/archive/0.7.0.tar.gz"
   sha256 "f7959915595495ce922b2b6987368118fa28ba7d13ac3961fd513ec8dfdb21c8"
   head "https://github.com/krzyzanowskim/Natalie.git"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -14,14 +15,13 @@ class Natalie < Formula
   depends_on :xcode => ["9.4", :build]
 
   def install
-    system "swift", "build", "--disable-sandbox", "-c", "release", "-Xswiftc",
-           "-static-stdlib"
+    system "swift", "build", "--disable-sandbox", "-c", "release", "--static-swift-stdlib"
     bin.install ".build/release/natalie"
     share.install "NatalieExample"
   end
 
   test do
-    generated_code = Utils.popen_read("#{bin}/natalie #{share}/NatalieExample")
+    generated_code = shell_output("#{bin}/natalie #{share}/NatalieExample")
     assert generated_code.lines.count > 1, "Natalie failed to generate code!"
   end
 end
