236de067832df1daa57b3a0bb022389c04f09f3a
Dustin Rodrigues <dust.rod@gmail.com>
2020-06-11 00:40:56 -0400

    libcouchbase 3.0.2
    Closes #56110.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libcouchbase.rb
+++ Formula/libcouchbase.rb
@@ -1,8 +1,8 @@
 class Libcouchbase < Formula
   desc "C library for Couchbase"
   homepage "https://docs.couchbase.com/c-sdk/3.0/hello-world/start-using-sdk.html"
-  url "https://packages.couchbase.com/clients/c/libcouchbase-3.0.1.tar.gz"
-  sha256 "488cd906450c797b014910f5b9239cf1e14c7e0f94d3b7dc5f9cfb76ce1085b1"
+  url "https://packages.couchbase.com/clients/c/libcouchbase-3.0.2.tar.gz"
+  sha256 "901c1490c2f5627b969fe88eb0216576ba22bb6928844ee4283e8e0c6acee721"
   head "https://github.com/couchbase/libcouchbase.git"
 
   bottle do
