9f661759de1f81cf80a5158c94ead920b68360d0
Dustin Rodrigues <dust.rod@gmail.com>
2020-06-11 01:23:32 -0400

    nave 3.2.2
    Closes #56128.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nave.rb
+++ Formula/nave.rb
@@ -1,8 +1,8 @@
 class Nave < Formula
   desc "Virtual environments for Node.js"
   homepage "https://github.com/isaacs/nave"
-  url "https://github.com/isaacs/nave/archive/v3.2.1.tar.gz"
-  sha256 "6ad13ce65659960fdb6133529c3fd365c04cb7c7208c60c2a6e0245494908977"
+  url "https://github.com/isaacs/nave/archive/v3.2.2.tar.gz"
+  sha256 "a8eb92bb47f6d00326b710f086aea23ae76ceadd277f79256263f524d3540ed1"
   head "https://github.com/isaacs/nave.git"
 
   bottle :unneeded
