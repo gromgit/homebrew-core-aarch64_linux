70b980f71cc67c6829dc5f9406505ed1de00f02b
Michka Popoff <michkapopoff@gmail.com>
2020-06-11 07:43:23 +0200

    angle-grinder: add linux-only dependencies

--- Formula/angle-grinder.rb
+++ Formula/angle-grinder.rb
@@ -14,6 +14,11 @@ class AngleGrinder < Formula
 
   depends_on "rust" => :build
 
+  on_linux do
+    depends_on "pkg-config" => :build
+    depends_on "openssl@1.1"
+  end
+
   def install
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
