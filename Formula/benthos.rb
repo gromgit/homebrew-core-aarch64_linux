40f93b467b10d2682fe02cc444fff77878c97411
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-11 10:06:02 +0000

    benthos: update 3.17.0 bottle.

--- Formula/benthos.rb
+++ Formula/benthos.rb
@@ -6,10 +6,9 @@ class Benthos < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "e7fa31dfcf36a0accafc722b69af29ec4fbfd854dd0b08f0ddf5084141c6b20b" => :catalina
-    sha256 "75f3bfc071bc025627bf4e911fec5e29d37684b336e0f08befab68d26c00ed81" => :mojave
-    sha256 "2f8378c466ae7e7f136c27fe63a1bf59743ed319327384f8ee9e644445cc6092" => :high_sierra
+    sha256 "b01d4117e4133e0d0a3f3a491d6d27775b0d459d041c03ca32be9e8ea401a2fd" => :catalina
+    sha256 "7da85e02185751e006b4bb6ee6e5f6771519ea98e98c33893e1d8690737255a7" => :mojave
+    sha256 "5ac803ca081885e5a3d920a2e95ac0607081141250d8ccded589602c3ca7823c" => :high_sierra
   end
 
   depends_on "go" => :build
