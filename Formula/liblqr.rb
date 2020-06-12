5ce1facadfd2ecd19218a0fdb654d504cdc29b8a
Sam Ford <1584702+samford@users.noreply.github.com>
2020-06-12 21:51:17 +0000

    liblqr: update 0.4.2_1 bottle.

--- Formula/liblqr.rb
+++ Formula/liblqr.rb
@@ -8,10 +8,10 @@ class Liblqr < Formula
 
   bottle do
     cellar :any
-    sha256 "71b84dda3f24cec54647d2ddceb5ca6fdaa181817034ef5996751ab893dce740" => :catalina
-    sha256 "00910cec48716bb94fb5279eaf41a72b484cba518529f33c3fc3da27a249f72e" => :mojave
-    sha256 "200c63486701a6b120c947d950bd69a65de42728c3585b275a3b7c37cf7358f8" => :high_sierra
-    sha256 "9be258b912074344d3c1a1f328c505432fddf8a84dc9d3fbd641616748100a93" => :sierra
+    rebuild 1
+    sha256 "18803ed552ae07c1998c87ba6c4ebaee1ec5eaab843c2cfa2cc3775f0b55da23" => :catalina
+    sha256 "83054ddb4fffb94ea12f609a90082220a451bfdc793284d104f1fdeaf4aa8fd6" => :mojave
+    sha256 "43e9b4f518364d436b53c89b1ac42e2cfdcafc47fad1ba711bd6456122e47d62" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
