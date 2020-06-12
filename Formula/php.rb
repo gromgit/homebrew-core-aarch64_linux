c193ce65dd80246ab111118eabfb7ec24a16a204
Dawid Dziurla <dawidd0811@gmail.com>
2020-06-12 06:45:32 +0000

    php: update 7.4.7 bottle.

--- Formula/php.rb
+++ Formula/php.rb
@@ -7,9 +7,9 @@ class Php < Formula
   sha256 "53558f8f24cd8ab6fa0ea252ca8198e2650160649681ce5230c1df1dc2b52faf"
 
   bottle do
-    sha256 "f9bda8ea1bf0108091487a5595b02cc66370198296f7a78d033ccbc7c9f9958c" => :catalina
-    sha256 "a5c17a3e4a186cece0e483a2e4cf8d6ac41826decfe826f01688f5d53e170d36" => :mojave
-    sha256 "75345ed154fd07c14153e9285db4ac65e7be511392bce7708d1da7179267d881" => :high_sierra
+    sha256 "ccf94b39c146eb766a2f3bd250008fa13649814968c3dbd17a9801fd0d70a87e" => :catalina
+    sha256 "eda05d8ddbcf4d611b64836bc7902ee1e2dad3e49eeb494ad3493f5c1589fcf9" => :mojave
+    sha256 "f1409ba91d2446d9eabac8933ac102c64f6f5491ffea8c822ee0daff8572bd1c" => :high_sierra
   end
 
   head do
