c98072e259888ba9892f60332dcd34bf5888d8f5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-12 20:06:40 +0000

    devdash: update 0.4.0 bottle.

--- Formula/devdash.rb
+++ Formula/devdash.rb
@@ -6,9 +6,9 @@ class Devdash < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "bc3e56b0b9cefdb3e2871cd27ca5499d61ec1e2d182a9390e3303d2d6aee3a62" => :catalina
-    sha256 "565f301ac09f5f55cade8db7d70bf559a8e1c422b7c88b2859e418e08e760914" => :mojave
-    sha256 "72e269d84c8a1e0d973fcf892a343a1f3b11db63b69788581997cb62e91588f8" => :high_sierra
+    sha256 "86085a11dbf43d1edafd9e9fd7e47443ec11ab692b9b48a80c5ffc58a2f1b39a" => :catalina
+    sha256 "a15bcbbc6085b0420bb9bc863acfffbd4a8fd05956dc66dd163caa8ddf731fc3" => :mojave
+    sha256 "ef75c99c9d174815f7fb244f8b63866e0b27e22d5c35c9e37190da3293c7678b" => :high_sierra
   end
 
   depends_on "go" => :build
