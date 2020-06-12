2ae9f93ed3022a6a486ae9b48950a616e790ce0d
Caleb Xu <calebcenter@live.com>
2020-06-12 09:29:27 -0400

    helib 1.0.1
    Closes #56206.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/helib.rb
+++ Formula/helib.rb
@@ -1,8 +1,8 @@
 class Helib < Formula
   desc "Implementation of homomorphic encryption"
   homepage "https://github.com/homenc/HElib"
-  url "https://github.com/homenc/HElib/archive/v1.0.0.tar.gz"
-  sha256 "5b917a6ba1555be580db4c102a339abe124c284007f0044d637892ec85877214"
+  url "https://github.com/homenc/HElib/archive/v1.0.1.tar.gz"
+  sha256 "428b61075cfe313071bb59ccd07736dae07244a065c2001662c9c3d3988ab349"
 
   bottle do
     cellar :any
@@ -23,11 +23,12 @@ class Helib < Formula
   end
 
   test do
-    cp (pkgshare/"examples/BGV_general_example/BGV_general_example.cpp"), testpath/"test.cpp"
+    cp pkgshare/"examples/BGV_database_lookup/BGV_database_lookup.cpp", testpath/"test.cpp"
+    mkdir "build"
     system ENV.cxx, "-std=c++14", "-L#{lib}", "-L#{Formula["ntl"].opt_lib}",
-                    "-lhelib", "-lntl", "test.cpp", "-o", "test"
-    # 2*(n^2) from 0 to 23
-    expected = "0 2 8 18 32 50 72 98 128 162 200 242 288 338 392 450 512 578 648 722 800 882 968 1058"
-    assert_match expected, shell_output("./test")
+                    "-lhelib", "-lntl", "test.cpp", "-o", "build/BGV_database_lookup"
+
+    cp pkgshare/"examples/BGV_database_lookup/runtest.sh", testpath/"runtest.sh"
+    system "./runtest.sh"
   end
 end
