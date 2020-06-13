d2e3da4f438c30eba9bed3b06dc0c944dec8b8a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-13 00:13:04 +0000

    tup: update 0.7.9 bottle.

--- Formula/tup.rb
+++ Formula/tup.rb
@@ -7,10 +7,9 @@ class Tup < Formula
 
   bottle do
     cellar :any
-    sha256 "76c67dc80a2317d0c03e98b90007b6ba0ee4350f0bfbfc6b923b21f7f41be587" => :catalina
-    sha256 "fe62bd2762c7ff15e628b6c241c8f6acd81d190c03136c6edce2fd76afafe9f0" => :mojave
-    sha256 "9a2e688be1a21af9fc0c2e9edb0e2a679eb7553356e59682037a760c6bd90b8d" => :high_sierra
-    sha256 "fc8a299e3ed77a756edbcb957343d1cc95594126f23514eea729649a7fcc6071" => :sierra
+    sha256 "6a6730ccbf131493bfd3f35b4e38f50a60cbefb122794ab603b6ad2e7fba2f28" => :catalina
+    sha256 "d1d2207224fb78fd4f1dcdbfacf2b62b10538ebacccd33356c5e93fcedad030e" => :mojave
+    sha256 "9e45e8c40a8576611ba6fa53a55c9004b31f41af75c9130ba909012ec19fef45" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
