120dbe560716b45b10f49367fd59e020bec67fee
Yurii Kolesnykov <root@yurikoles.com>
2020-06-14 15:24:41 +0300

    kde-ki18n 5.71.0 (new formula)
    Closes #56279.
    
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>
    Signed-off-by: Issy Long <me@issyl0.co.uk>

---
+++ Formula/kde-ki18n.rb
@@ -0,0 +1,54 @@
+class KdeKi18n < Formula
+  desc "KDE Gettext-based UI text internationalization"
+  homepage "https://api.kde.org/frameworks/ki18n/html/index.html"
+  url "https://download.kde.org/stable/frameworks/5.71/ki18n-5.71.0.tar.xz"
+  sha256 "f2fc8c40c10576da8b74070b7dc8e752fdd04204cb2bfe522f37a0458fbaf881"
+  head "https://invent.kde.org/frameworks/ki18n.git"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "doxygen" => :build
+  depends_on "graphviz" => :build
+  depends_on "kde-extra-cmake-modules" => [:build, :test]
+  depends_on "gettext"
+  depends_on "qt"
+
+  def install
+    args = std_cmake_args
+    args << "-DBUILD_TESTING=OFF"
+    args << "-DBUILD_QCH=ON"
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+    end
+
+    pkgshare.install "autotests"
+    (pkgshare/"cmake").install "cmake/FindLibIntl.cmake"
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<~EOS
+      cmake_minimum_required(VERSION 3.5)
+      include(FeatureSummary)
+      find_package(ECM 5.71.0 NO_MODULE)
+      set_package_properties(ECM PROPERTIES TYPE REQUIRED)
+      set(CMAKE_MODULE_PATH ${ECM_MODULE_PATH} "#{pkgshare}/cmake")
+      find_package(Qt5 5.12.0 REQUIRED Core)
+      find_package(KF5I18n REQUIRED)
+      INCLUDE(CheckCXXSourceCompiles)
+      find_package(LibIntl)
+      set_package_properties(LibIntl PROPERTIES TYPE REQUIRED)
+      add_subdirectory(autotests)
+    EOS
+
+    cp_r (pkgshare/"autotests"), testpath
+
+    args = std_cmake_args
+    args << "-DQt5_DIR=#{Formula["qt"].opt_prefix/"lib/cmake/Qt5"}"
+    args << "-DLibIntl_INCLUDE_DIRS=#{Formula["gettext"].include}"
+    args << "-DLibIntl_LIBRARIES=#{Formula["gettext"].lib/"libintl.dylib"}"
+
+    system "cmake", testpath.to_s, *args
+    system "make"
+  end
+end
