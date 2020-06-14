7a86fed0b607c806cfdd718e62f3fa7b66b30839
Dawid Dziurla <dawidd0811@gmail.com>
2020-06-14 13:31:10 +0000

    liboqs: add 0.3.0 bottle.

--- Formula/liboqs.rb
+++ Formula/liboqs.rb
@@ -4,6 +4,13 @@ class Liboqs < Formula
   url "https://github.com/open-quantum-safe/liboqs/archive/0.3.0.tar.gz"
   sha256 "b447133039a8fb8df3cc90e22db1453cd67987834d495ac6898ac6a63df7c14b"
 
+  bottle do
+    cellar :any
+    sha256 "f08fb2aefced27d1cc26680e50a1f0df0d87182f923135cd57900a5e3fc0beb8" => :catalina
+    sha256 "6a58d68654065e151ca045a81eb0c1e3e786696a84453b6c75aea4b2d449f66e" => :mojave
+    sha256 "47d203b377f2702041634aabe030ba786f466afb78b83bbe27a79b3aabcd9d49" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
   depends_on "ninja" => :build
