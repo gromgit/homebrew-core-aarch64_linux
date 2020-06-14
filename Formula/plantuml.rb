402507bfc1e418c7ec3c842f3b5d7342594c6880
Caleb Xu <calebcenter@live.com>
2020-06-14 05:57:34 -0400

    plantuml 1.2020.13 (#56274)

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://downloads.sourceforge.net/project/plantuml/1.2020.12/plantuml.1.2020.12.jar"
-  sha256 "526af2f4b8cf293a532e7a4c328b14877a63aeb82a2b08af456f232e27380230"
+  url "https://downloads.sourceforge.net/project/plantuml/1.2020.13/plantuml.1.2020.13.jar"
+  sha256 "f09b90d5b72f3fd85082c8445fcbf5f3b6a29dc24bb9ab6d157aaacca83780a5"
   version_scheme 1
 
   bottle :unneeded
