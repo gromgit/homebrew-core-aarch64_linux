b987f02893802f380177ebfc590429ea3edce50e
Luflosi <luflosi@luflosi.de>
2020-06-16 01:26:55 +0200

    youtube-dl 2020.06.16.1
    Closes #56366.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -1,8 +1,8 @@
 class YoutubeDl < Formula
   desc "Download YouTube videos from the command-line"
   homepage "https://ytdl-org.github.io/youtube-dl/"
-  url "https://github.com/ytdl-org/youtube-dl/releases/download/2020.06.16/youtube-dl-2020.06.16.tar.gz"
-  sha256 "a99642ca873e4c2d2b356c94f961e7ec28170fbd96a2415f3a7704be2f8af8b9"
+  url "https://github.com/ytdl-org/youtube-dl/releases/download/2020.06.16.1/youtube-dl-2020.06.16.1.tar.gz"
+  sha256 "fd06c016ce2a8b82a3cee245acc397433ddb54a276bdf8d79bcaddbe194000e0"
 
   head do
     url "https://github.com/ytdl-org/youtube-dl.git"
