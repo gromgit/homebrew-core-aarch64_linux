1a7c9f126fc4599bf95084d1418171cdd2425da8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-15 16:06:50 +0000

    aws-google-auth: update 0.0.36 bottle.

--- Formula/aws-google-auth.rb
+++ Formula/aws-google-auth.rb
@@ -9,9 +9,9 @@ class AwsGoogleAuth < Formula
 
   bottle do
     cellar :any
-    sha256 "249b2968b5c6c21a5ae511fe47087f38fad3fa40ce617628ebc75ad43d6d0c8c" => :catalina
-    sha256 "d739c4fab55b1c76316c33262788217cdc50dac00207d279aa99f0bd652f1146" => :mojave
-    sha256 "2a34df761955d6f550ee8cdc6fb77eb103633362f7baec5bd5c92cf77dbc9f35" => :high_sierra
+    sha256 "1d6890e0d3fd5e514d91db206c47b798ee5c5411a25cf2cd2eda8cf4abfc4806" => :catalina
+    sha256 "a1da781b800a1a533e2b6afd3443f77b04a952da872320732c16b1bab858a4a5" => :mojave
+    sha256 "371f6acaf6ce534b1a7d6569ea5fb02d05a0da5818b6f0e7097edb91ebdc5a66" => :high_sierra
   end
 
   depends_on "freetype"
