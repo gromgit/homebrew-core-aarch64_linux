fe2dc6edc08ef9ba7644f62587ab318fdf070662
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-15 08:06:54 +0000

    cglm: update 0.7.6 bottle.

--- Formula/cglm.rb
+++ Formula/cglm.rb
@@ -6,9 +6,9 @@ class Cglm < Formula
 
   bottle do
     cellar :any
-    sha256 "6a5d8dfd8e51b2bd8f3fc1040a5343bd72feb3f33dd27007c2bfc9ec6cf5e23c" => :catalina
-    sha256 "3062af7484b8fe4e5e44068c0157824542584c66f2c79628553a8010fd261592" => :mojave
-    sha256 "33d1294407b153435704cd5d942d155fdc36c799306c483a0b8a738a5798343f" => :high_sierra
+    sha256 "71acb4380ab467ed76133c76d7b971a9914c0ba07b41ad51ee7e5b74334b6861" => :catalina
+    sha256 "253894ff185a4f8499253543a8cd246dc8958f65746f3319f1e311c0f81feabc" => :mojave
+    sha256 "683def15df6b8bf55a4954a3427291b559204975b5cdc35d74a18170d4e7db58" => :high_sierra
   end
 
   depends_on "autoconf" => :build
