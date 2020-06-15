ddde0e4a79e52747a669f58937fc1fd70786b14f
Caleb Xu <calebcenter@live.com>
2020-06-15 19:33:54 -0400

    libusbmuxd 2.0.2
    Closes #56361.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

--- Formula/libusbmuxd.rb
+++ Formula/libusbmuxd.rb
@@ -1,21 +1,15 @@
 class Libusbmuxd < Formula
   desc "USB multiplexor library for iOS devices"
   homepage "https://www.libimobiledevice.org/"
-
-  stable do
-    url "https://github.com/libimobiledevice/libusbmuxd/archive/2.0.1.tar.gz"
-    sha256 "f93faf3b3a73e283646f4d62b3421aeccf58142266b0eb22b2b13dd4b2362eb8"
-  end
+  url "https://github.com/libimobiledevice/libusbmuxd/archive/2.0.2.tar.gz"
+  sha256 "8ae3e1d9340177f8f3a785be276435869363de79f491d05d8a84a59efc8a8fdc"
+  head "https://github.com/libimobiledevice/libusbmuxd.git"
 
   bottle do
     cellar :any
     sha256 "082a3ef816c55324d9c8c51917b155403211284940b10e822d87cac91991ec0b" => :catalina
     sha256 "d6d7639277e5590b24713cad79e12c7b9dbb373c6ecf76c97a007041b5307fe2" => :mojave
     sha256 "dd3ef2fb6343f59a05c16a66f3e839a14b660cec0c11d3d40bb00f9505565a2a" => :high_sierra
-  end
-
-  head do
-    url "https://github.com/libimobiledevice/libusbmuxd.git"
   end
 
   depends_on "autoconf" => :build
@@ -34,6 +28,13 @@ class Libusbmuxd < Formula
   end
 
   test do
-    system bin/"iproxy"
+    source = free_port
+    dest = free_port
+    fork do
+      exec bin/"iproxy", "#{source}:#{dest}"
+    end
+
+    sleep(2)
+    system "nc", "-z", "localhost", source
   end
 end
