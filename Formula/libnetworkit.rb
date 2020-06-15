67fb894f80f2847ba8325327944f67b1701a105e
Sean Molenaar <smillerdev@me.com>
2020-06-15 11:38:23 +0200

    libnetworkit: add 7.0 bottle.

--- Formula/libnetworkit.rb
+++ Formula/libnetworkit.rb
@@ -4,6 +4,13 @@ class Libnetworkit < Formula
   url "https://github.com/networkit/networkit/archive/7.0.tar.gz"
   sha256 "4faf16c5fae3e14d3c1b6f30e25c6e093dcf6a3dbf021235f3161ac2a527f682"
 
+  bottle do
+    cellar :any
+    sha256 "86915bb4086ab367364d03ae45b1228aa0b788facda6daf18e0a0adaff9b140e" => :catalina
+    sha256 "9b9d3a3e7aa1a6f948a474b14aa251813d96e703eddf2bbc65e1c815e83be564" => :mojave
+    sha256 "2b2ae31876287265a67d737efa586d66e839e9fbf648354c53a3222a0c679ae1" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "libomp"
   depends_on "tlx"
