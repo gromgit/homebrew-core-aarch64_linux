74e2157a6861e0af4e0a1dc078d43aee74f0b1a5
Rui Chen <rui@meetup.com>
2020-06-15 00:00:15 -0400

    file-formula 5.39
    Closes #56307.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/file-formula.rb
+++ Formula/file-formula.rb
@@ -2,8 +2,8 @@
 class FileFormula < Formula
   desc "Utility to determine file types"
   homepage "https://www.darwinsys.com/file/"
-  url "https://astron.com/pub/file/file-5.38.tar.gz"
-  sha256 "593c2ffc2ab349c5aea0f55fedfe4d681737b6b62376a9b3ad1e77b2cc19fa34"
+  url "https://astron.com/pub/file/file-5.39.tar.gz"
+  sha256 "f05d286a76d9556243d0cb05814929c2ecf3a5ba07963f8f70bfaaa70517fad1"
   head "https://github.com/file/file.git"
 
   bottle do
