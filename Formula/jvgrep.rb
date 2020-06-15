ba5d619cfc6659050714dedce0eb2950edd897fb
Rui Chen <rui@meetup.com>
2020-06-14 23:52:56 -0400

    jvgrep 5.8.8
    Closes #56303.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/jvgrep.rb
+++ Formula/jvgrep.rb
@@ -1,8 +1,8 @@
 class Jvgrep < Formula
   desc "Grep for Japanese users of Vim"
   homepage "https://github.com/mattn/jvgrep"
-  url "https://github.com/mattn/jvgrep/archive/v5.8.5.tar.gz"
-  sha256 "82470a9852b5cb2f093bcdbcb7c66f3d2e1ab7a8f3680e90d6944a0990a11f3d"
+  url "https://github.com/mattn/jvgrep/archive/v5.8.8.tar.gz"
+  sha256 "dc3b5f77189bf8f91d7c8f48e3908dcf4dfea9fd12cd23e71deb54e3ea64d724"
   head "https://github.com/mattn/jvgrep.git"
 
   bottle do
@@ -15,8 +15,7 @@ class Jvgrep < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-ldflags", "-s -w", "-trimpath", "-o", bin/"jvgrep"
-    prefix.install_metafiles
+    system "go", "build", *std_go_args
   end
 
   test do
