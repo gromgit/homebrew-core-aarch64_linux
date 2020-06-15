b7065c3a2e3f3f5e6732cbb91a363884be008127
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-15 04:06:18 +0000

    yq: update 3.3.2 bottle.

--- Formula/yq.rb
+++ Formula/yq.rb
@@ -6,9 +6,9 @@ class Yq < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "8f91edb58ba2f7ccf2a4f008d372a25cb3a76e3bda28b98d1cbc0a52f3ba6fb7" => :catalina
-    sha256 "9e7bff9525666a35b5f6297ad8646e111241ad9429736ddc7270f33b4a02c594" => :mojave
-    sha256 "31273bf05b627315103b77951fa5fbd1a8750fea10c7355bab3d7aaf45fd8aef" => :high_sierra
+    sha256 "1cd476d249fd7a11034e3e8581a099cc8dfff09cc1ff40c79b819a5562399137" => :catalina
+    sha256 "7b33668ea6670cc65b4ef92d640c0066d9fc0c54f7bea4136cd326d8b59d37e8" => :mojave
+    sha256 "d59fe8d26fa97d6fc26bfff73315d90d361e456bdecf1fdfc10084c3cadedd30" => :high_sierra
   end
 
   depends_on "go" => :build
