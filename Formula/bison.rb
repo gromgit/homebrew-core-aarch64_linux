20a3ea5c17ed9ceeb6440ce5f67012a6c718f85e
Caleb Xu <calebcenter@live.com>
2020-06-16 00:33:43 -0400

    bison 3.6.4
    Closes #56370.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bison.rb
+++ Formula/bison.rb
@@ -2,9 +2,9 @@ class Bison < Formula
   desc "Parser generator"
   homepage "https://www.gnu.org/software/bison/"
   # X.Y.9Z are beta releases that sometimes get accidentally uploaded to the release FTP
-  url "https://ftp.gnu.org/gnu/bison/bison-3.6.3.tar.xz"
-  mirror "https://ftpmirror.gnu.org/bison/bison-3.6.3.tar.xz"
-  sha256 "06db793651de9dd5f0a85a6fe4bdbca413c0806bf2432377523da96ca0b4b73d"
+  url "https://ftp.gnu.org/gnu/bison/bison-3.6.4.tar.xz"
+  mirror "https://ftpmirror.gnu.org/bison/bison-3.6.4.tar.xz"
+  sha256 "8b13473b31ca7fcf65e5e8a74224368ffd5df19275602a9c9567ba393f18577d"
   version_scheme 1
 
   bottle do
