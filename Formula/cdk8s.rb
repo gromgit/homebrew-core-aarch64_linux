ac164d98c4f2520b5d6e5889329a1948bd7ecd1e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-16 03:06:20 +0000

    cdk8s: update 0.25.0 bottle.

--- Formula/cdk8s.rb
+++ Formula/cdk8s.rb
@@ -8,9 +8,9 @@ class Cdk8s < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "efaf53ffab538afe3de5e9adf1cea580395de2907fff98b8691b2e3e9bfc60bb" => :catalina
-    sha256 "eb445573a9ee9d1b72603f88a69811719f563420791d5176b20d0e3a64413dfd" => :mojave
-    sha256 "c801c3ba74d97db9b7bcb2b312cc6cb261f46ebacf2520bc317efea3afbc0c72" => :high_sierra
+    sha256 "bbe959a51ea974ef1cb5ba1b5e63269c14905b2b9ca6975b17a4c9370c6287c0" => :catalina
+    sha256 "40e4cdd6c1ed58d815b6685f7df0a57a9156209bd4460d604af16cdbdc2bdae3" => :mojave
+    sha256 "c5638765608a604e0a7974a145bc49f665f210afaf50588ec5924708e26e3286" => :high_sierra
   end
 
   depends_on "node"
