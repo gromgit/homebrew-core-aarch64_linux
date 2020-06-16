49bd8eed6bf9d053ba8db4fc2ec59266405dff94
Issy Long <me@issyl0.co.uk>
2020-06-16 17:25:30 +0000

    inko: add 0.6.0 bottle.

--- Formula/inko.rb
+++ Formula/inko.rb
@@ -5,6 +5,13 @@ class Inko < Formula
   sha256 "3560023128675db5f76698e546e7f3fce70f45816735fb3fa71d103ae383fc61"
   head "https://gitlab.com/inko-lang/inko.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f3191304d3390550d8c47e063ca84e204d571f398c6034fda2395487f8aa81ea" => :catalina
+    sha256 "be14d156158816f1389f2b18e04f06f8a6bc9310c9ff6286e1045d9a98f27f18" => :mojave
+    sha256 "751dd81d09fcbd7aff9fe9580997d04f8c96ea45c064a18b94455c7508a3ba7e" => :high_sierra
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "coreutils" => :build
