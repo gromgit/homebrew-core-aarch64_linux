dbc233fdd0887fa51a185b594338ac7dc16f9b8b
miccal <miccal.matthews@gmail.com>
2020-06-16 12:49:20 +0800

    ilmbase 2.5.2
    Closes #56371.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ilmbase.rb
+++ Formula/ilmbase.rb
@@ -1,8 +1,8 @@
 class Ilmbase < Formula
   desc "OpenEXR ILM Base libraries (high dynamic-range image file format)"
   homepage "https://www.openexr.com/"
-  url "https://github.com/openexr/openexr/archive/v2.5.1.tar.gz"
-  sha256 "11f806bf256453e39fc33bd1cf1fa576a54f144cedcdd3e6935a177e5a89d02e"
+  url "https://github.com/openexr/openexr/archive/v2.5.2.tar.gz"
+  sha256 "5da8dff448d0c4a529e52c97daf238a461d01cd233944f75095668d6d7528761"
 
   bottle do
     sha256 "4748d38413303488484f98d5fbb94bb90a3f2e6a7fc9830fe5ce12463ef6812b" => :catalina
