e7b958bb7d8b12ad72e22472d517e70f4b3eada8
Yurii Kolesnykov <root@yurikoles.com>
2020-06-16 17:32:07 +0300

    kde-kdoctools 5.71.0 (new formula)
    Closes #56594.
    
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>
    Signed-off-by: Issy Long <me@issyl0.co.uk>

---
+++ Formula/kde-kdoctools.rb
@@ -0,0 +1,90 @@
+class KdeKdoctools < Formula
+  desc "Create documentation from DocBook"
+  homepage "https://api.kde.org/frameworks/kdoctools/html/index.html"
+  url "https://download.kde.org/stable/frameworks/5.71/kdoctools-5.71.0.tar.xz"
+  sha256 "1e2fcaa97a014e82f68c0c36591ce84568ead7abd59b66e534789103e162cd09"
+  head "https://invent.kde.org/frameworks/kdoctools.git"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "docbook-xsl" => [:build, :test]
+  depends_on "doxygen" => :build
+  depends_on "gettext" => :build
+  depends_on "kde-extra-cmake-modules" => [:build, :test]
+  depends_on "kde-ki18n" => :build
+
+  depends_on "kde-karchive"
+
+  uses_from_macos "libxml2"
+  uses_from_macos "libxslt"
+  uses_from_macos "perl"
+
+  resource "URI::Escape" do
+    url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/URI-1.76.tar.gz"
+    sha256 "b2c98e1d50d6f572483ee538a6f4ccc8d9185f91f0073fd8af7390898254413e"
+  end
+
+  def install
+    ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+    ENV.prepend_path "PERL5LIB", libexec/"lib"
+
+    resource("URI::Escape").stage do
+      system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+      system "make", "install"
+    end
+
+    args = std_cmake_args
+    args << "-DBUILD_TESTING=OFF"
+    args << "-DBUILD_QCH=ON"
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+    end
+
+    pkgshare.install ["cmake", "autotests", "tests"]
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<~EOS
+      cmake_minimum_required(VERSION 3.5)
+      include(FeatureSummary)
+      find_package(ECM 5.71.0 NO_MODULE)
+      set_package_properties(ECM PROPERTIES TYPE REQUIRED)
+      set(CMAKE_MODULE_PATH ${ECM_MODULE_PATH} "#{pkgshare}/cmake")
+      find_package(Qt5 5.12.0 REQUIRED Core)
+      find_package(KF5DocTools REQUIRED)
+
+      find_package(LibXslt)
+      set_package_properties(LibXslt PROPERTIES TYPE REQUIRED)
+
+      find_package(LibXml2)
+      set_package_properties(LibXml2 PROPERTIES TYPE REQUIRED)
+
+      if (NOT LIBXML2_XMLLINT_EXECUTABLE)
+         message(FATAL_ERROR "xmllint is required to process DocBook XML")
+      endif()
+
+      find_package(DocBookXML4 "4.5")
+      set_package_properties(DocBookXML4 PROPERTIES TYPE REQUIRED)
+
+      find_package(DocBookXSL)
+      set_package_properties(DocBookXSL PROPERTIES TYPE REQUIRED)
+
+      remove_definitions(-DQT_NO_CAST_FROM_ASCII)
+      add_definitions(-DQT_NO_FOREACH)
+      
+      add_subdirectory(autotests)
+      add_subdirectory(tests/create-from-current-dir-test)
+      add_subdirectory(tests/kdoctools_install-test)
+    EOS
+
+    cp_r (pkgshare/"autotests"), testpath
+    cp_r (pkgshare/"tests"), testpath
+
+    args = std_cmake_args
+    args << "-DQt5_DIR=#{Formula["qt"].opt_prefix/"lib/cmake/Qt5"}"
+
+    system "cmake", testpath.to_s, *args
+    system "make"
+  end
+end
