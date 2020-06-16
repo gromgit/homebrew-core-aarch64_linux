d6b7e8a2e56ec5bb1eb076991ec34f75ec1a07ed
Yurii Kolesnykov <root@yurikoles.com>
2020-06-16 20:29:55 +0300

    kde-karchive 5.71.0 (new formula)
    Closes #56407.
    
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/kde-karchive.rb
@@ -0,0 +1,46 @@
+class KdeKarchive < Formula
+  desc "Reading, creating, and manipulating file archives"
+  homepage "https://api.kde.org/frameworks/karchive/html/index.html"
+  url "https://download.kde.org/stable/frameworks/5.71/karchive-5.71.0.tar.xz"
+  sha256 "cc81e856365dec2bcf3ec78aa01d42347ca390a2311ea12050f309dfbdb09624"
+  head "https://invent.kde.org/frameworks/karchive.git"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "doxygen" => :build
+  depends_on "graphviz" => :build
+  depends_on "kde-extra-cmake-modules" => [:build, :test]
+
+  depends_on "qt"
+  depends_on "xz"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "zlib"
+
+  def install
+    args = std_cmake_args
+    args << "-DBUILD_TESTING=OFF"
+    args << "-DBUILD_QCH=ON"
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+    end
+
+    pkgshare.install "examples"
+  end
+
+  test do
+    args = std_cmake_args
+    args << "-DQt5Core_DIR=#{Formula["qt"].opt_prefix/"lib/cmake/Qt5Core"}"
+
+    %w[bzip2gzip
+       helloworld
+       tarlocalfiles
+       unzipper].each do |test_name|
+      mkdir test_name.to_s do
+        system "cmake", (pkgshare/"examples/#{test_name}"), *args
+        system "make"
+      end
+    end
+  end
+end
