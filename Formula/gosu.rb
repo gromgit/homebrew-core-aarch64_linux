119db0121dad83028b8693c82aac3647c6f777e8
Mike McQuaid <mike@mikemcquaid.com>
2020-06-17 08:45:24 +0100

    gosu: use Utils.safe_popen_read.
    Co-authored-by: Rylan Polster <rslpolster@gmail.com>

--- Formula/gosu.rb
+++ Formula/gosu.rb
@@ -19,7 +19,7 @@ class Gosu < Formula
 
   def install
     cmd = Language::Java.java_home_cmd("1.8")
-    ENV["JAVA_HOME"] = Utils.popen_read(cmd).chomp
+    ENV["JAVA_HOME"] = Utils.safe_popen_read(cmd).chomp
 
     system "mvn", "package"
     libexec.install Dir["gosu/target/gosu-#{version}-full/gosu-#{version}/*"]
