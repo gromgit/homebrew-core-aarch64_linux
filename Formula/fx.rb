9f3c0507ea9754523a7650ac8fd0fec7e943c1c1
Anton Medvedev <anton@medv.io>
2020-06-17 18:38:00 +0300

    fx 19.0.1
    Closes #56453.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fx.rb
+++ Formula/fx.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Fx < Formula
   desc "Command-line JSON processing tool"
   homepage "https://github.com/antonmedv/fx"
-  url "https://registry.npmjs.org/fx/-/fx-18.0.1.tgz"
-  sha256 "d7cba3cf63743600c2a246b3f32178432fe1a2ba5a2ab77675c16eda342a3dd5"
+  url "https://registry.npmjs.org/fx/-/fx-19.0.1.tgz"
+  sha256 "3f71183462975f5b3a7fde39971384fe519ae55b256a6b6bae83ad03c359db3f"
 
   bottle do
     cellar :any_skip_relocation
