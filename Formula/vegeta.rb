d64df10b778775c200363f1b17e2b93b260aeb5c
Mike McQuaid <mike@mikemcquaid.com>
2020-06-17 08:45:33 +0100

    vegeta: use Utils.safe_popen_read.
    Co-authored-by: Rylan Polster <rslpolster@gmail.com>

--- Formula/vegeta.rb
+++ Formula/vegeta.rb
@@ -14,7 +14,7 @@ class Vegeta < Formula
   depends_on "go" => :build
 
   def install
-    build_time = Utils.popen_read("date -u +'%Y-%m-%dT%H:%M:%SZ' 2> /dev/null").chomp
+    build_time = Utils.safe_popen_read("date -u +'%Y-%m-%dT%H:%M:%SZ' 2> /dev/null").chomp
 
     ldflags = %W[
       -s -w
