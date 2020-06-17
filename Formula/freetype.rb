2fee576ace5962f477cb95ad56b04b6208415de7
miccal <miccal.matthews@gmail.com>
2020-06-18 03:59:48 +0800

    freetype 2.10.2

--- Formula/freetype.rb
+++ Formula/freetype.rb
@@ -1,9 +1,9 @@
 class Freetype < Formula
   desc "Software library to render fonts"
   homepage "https://www.freetype.org/"
-  url "https://downloads.sourceforge.net/project/freetype/freetype2/2.10.1/freetype-2.10.1.tar.xz"
-  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.10.1.tar.xz"
-  sha256 "16dbfa488a21fe827dc27eaf708f42f7aa3bb997d745d31a19781628c36ba26f"
+  url "https://downloads.sourceforge.net/project/freetype/freetype2/2.10.2/freetype-2.10.2.tar.xz"
+  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.10.2.tar.xz"
+  sha256 "1543d61025d2e6312e0a1c563652555f17378a204a61e99928c9fcef030a2d8b"
 
   bottle do
     cellar :any
