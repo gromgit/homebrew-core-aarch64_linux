e16d33df6c8458d966e63fc97501d770f727ec58
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-17 14:06:11 +0000

    allureofthestars: update 0.9.5.0 bottle.

--- Formula/allureofthestars.rb
+++ Formula/allureofthestars.rb
@@ -10,10 +10,10 @@ class Allureofthestars < Formula
   head "https://github.com/AllureOfTheStars/Allure.git"
 
   bottle do
-    rebuild 2
-    sha256 "e8e53b6165ef8de7a92048a5768cbbb17d0901a0c4f3fbb34eb57d79eaa8fe4f" => :catalina
-    sha256 "89d961dd52842e4ec8e44d40fe0f5385845792637f836108dabee5cadc91fe9d" => :mojave
-    sha256 "3f8b9d0a2e203c896ae4a0a1a340f5e794f5ff6d26203195669de31ecd116c53" => :high_sierra
+    rebuild 3
+    sha256 "cdcc579293d895e65bdfd907c2ab4d66db89e0389f78df9acaf1ea556ea47c63" => :catalina
+    sha256 "4b18f47a9ade6d260030488503b5bb3021ae523cf3b54960c8092495f0ffd47c" => :mojave
+    sha256 "2a056d85e8a4794158435ca324f7bc81d8dcb098770ec1d3d288dfcc77553c47" => :high_sierra
   end
 
   depends_on "cabal-install" => :build
