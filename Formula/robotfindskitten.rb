f40461607e05cb1014b3742d7752774c8347c801
Ryan Finnie <ryan@finnie.org>
2020-06-17 17:53:13 -0700

    robotfindskitten: Add Git head, always autoreconf
    - Adds upstream robotfindskitten Git head
    - Upstream repository does not include auto* scripts, so autoreconf
      must be run to build --HEAD.  This requires autoconf/automake/libtool.
      Technically this is only needed for --HEAD and could be worked around
      for official releases, but autoreconf is recommended build procedure
      anyway, as shipped scripts can lag behind auto* features.
    
    Closes #56467.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/robotfindskitten.rb
+++ Formula/robotfindskitten.rb
@@ -3,6 +3,8 @@ class Robotfindskitten < Formula
   homepage "http://robotfindskitten.org/"
   url "https://downloads.sourceforge.net/project/rfk/robotfindskitten-POSIX/ship_it_anyway/robotfindskitten-2.8284271.702.tar.gz"
   sha256 "020172e4f4630f7c4f62c03b6ffe2eeeba5637b60374d3e6952ae5816a9f99af"
+  revision 1
+  head "https://github.com/robotfindskitten/robotfindskitten.git", :branch => "main"
 
   bottle do
     sha256 "439c4c752b9606e645719ca46388b0143a0f3feb6ff248b4fd0d370f3192d506" => :catalina
@@ -10,7 +12,12 @@ class Robotfindskitten < Formula
     sha256 "667bd9a9abd587202ccd8519ace649638e3eeeb859021c2b9f2014ec55f69ea9" => :high_sierra
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
   def install
+    system "autoreconf", "-ivf"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install", "execgamesdir=#{bin}"
