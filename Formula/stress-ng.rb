cf9bc443ab2e188483a9b782b52b99eedcf9a7d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-18 06:06:45 +0000

    stress-ng: update 0.11.14 bottle.

--- Formula/stress-ng.rb
+++ Formula/stress-ng.rb
@@ -6,9 +6,9 @@ class StressNg < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ec9c912a17f21cfb6f060f228fc834cbfa47536725f2a6d01b3e4c85696c7bcf" => :catalina
-    sha256 "085fc372c87a3aeb8374cc9cca894eb37675c3e3d0a2a93e7e4b6f1a20531935" => :mojave
-    sha256 "5b3b204b15f649f47273aadc697915ab91b90751043f52adeeb78974700a4083" => :high_sierra
+    sha256 "a770ad23d8ea44676d36920a511707fc8a2696d978a8911370e1ef04ffdd7c25" => :catalina
+    sha256 "880650bca21eefa4a8e903b9b1fc0ffc3454449ab5927e6993a430e950efafba" => :mojave
+    sha256 "77f19860f13cc93aef0bc28471a402bf491e745b89285d8949eebf07d3498e85" => :high_sierra
   end
 
   depends_on :macos => :sierra
