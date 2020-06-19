ca65d0415531303ac725f805e2fc4f4c08f9fce7
Caleb Xu <calebcenter@live.com>
2020-06-19 12:18:00 -0400

    skopeo 1.1.0
    Closes #56556.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/skopeo.rb
+++ Formula/skopeo.rb
@@ -1,8 +1,8 @@
 class Skopeo < Formula
   desc "Work with remote images registries"
   homepage "https://github.com/containers/skopeo"
-  url "https://github.com/containers/skopeo/archive/v1.0.0.tar.gz"
-  sha256 "df5f38ee72e2fede508d1fd272a48773b86eb6bc6cc4b7b856a99669d22fa5df"
+  url "https://github.com/containers/skopeo/archive/v1.1.0.tar.gz"
+  sha256 "3bd3acc614514fc2261b89dc0ea85ac7a4dba64fb7dcef8676ae61849e8faeb4"
 
   bottle do
     rebuild 1
@@ -19,38 +19,32 @@ class Skopeo < Formula
   end
 
   def install
-    ENV["GOPATH"] = buildpath
     ENV["CGO_ENABLED"] = "1"
     ENV.append "CGO_FLAGS", ENV.cppflags
     ENV.append "CGO_FLAGS", Utils.safe_popen_read("#{Formula["gpgme"].bin}/gpgme-config --cflags")
 
-    (buildpath/"src/github.com/containers/skopeo").install buildpath.children
-    cd buildpath/"src/github.com/containers/skopeo" do
-      buildtags = [
-        "containers_image_ostree_stub",
-        Utils.safe_popen_read("hack/btrfs_tag.sh").chomp,
-        Utils.safe_popen_read("hack/btrfs_installed_tag.sh").chomp,
-        Utils.safe_popen_read("hack/libdm_tag.sh").chomp,
-      ].uniq.join(" ")
+    buildtags = [
+      "containers_image_ostree_stub",
+      Utils.safe_popen_read("hack/btrfs_tag.sh").chomp,
+      Utils.safe_popen_read("hack/btrfs_installed_tag.sh").chomp,
+      Utils.safe_popen_read("hack/libdm_tag.sh").chomp,
+    ].uniq.join(" ")
 
-      ldflags = [
-        "-X main.gitCommit=",
-        "-X github.com/containers/image/v5/docker.systemRegistriesDirPath=#{etc/"containers/registries.d"}",
-        "-X github.com/containers/image/v5/internal/tmpdir.unixTempDirForBigFiles=/var/tmp",
-        "-X github.com/containers/image/v5/signature.systemDefaultPolicyPath=#{etc/"containers/policy.json"}",
-        "-X github.com/containers/image/v5/pkg/sysregistriesv2.systemRegistriesConfPath=" \
-                                              "#{etc/"containers/registries.conf"}",
-      ].join(" ")
+    ldflags = [
+      "-X main.gitCommit=",
+      "-X github.com/containers/image/v5/docker.systemRegistriesDirPath=#{etc/"containers/registries.d"}",
+      "-X github.com/containers/image/v5/internal/tmpdir.unixTempDirForBigFiles=/var/tmp",
+      "-X github.com/containers/image/v5/signature.systemDefaultPolicyPath=#{etc/"containers/policy.json"}",
+      "-X github.com/containers/image/v5/pkg/sysregistriesv2.systemRegistriesConfPath=" \
+                                            "#{etc/"containers/registries.conf"}",
+    ].join(" ")
 
-      system "go", "build", "-v", "-x", "-tags", buildtags, "-ldflags", ldflags, "-o", bin/"skopeo", "./cmd/skopeo"
+    system "go", "build", "-tags", buildtags, "-ldflags", ldflags, *std_go_args, "./cmd/skopeo"
 
-      (etc/"containers").install "default-policy.json" => "policy.json"
-      (etc/"containers/registries.d").install "default.yaml"
+    (etc/"containers").install "default-policy.json" => "policy.json"
+    (etc/"containers/registries.d").install "default.yaml"
 
-      bash_completion.install "completions/bash/skopeo"
-
-      prefix.install_metafiles
-    end
+    bash_completion.install "completions/bash/skopeo"
   end
 
   test do
