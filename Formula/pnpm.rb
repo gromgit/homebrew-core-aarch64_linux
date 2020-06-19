8ec48ed8fb13198dc4b4e8af4e0e218b5dc87de4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-19 08:06:41 +0000

    pnpm: update 5.2.2 bottle.

--- Formula/pnpm.rb
+++ Formula/pnpm.rb
@@ -8,9 +8,9 @@ class Pnpm < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "999a796a26560ad38e72a59c83d3d3d4f0afd1ec4b71a6d7ca020cf5bc9b7283" => :catalina
-    sha256 "510d3aa7049540318b768799bcb0a421ab0aa974566d2e281d99a37f82c48c40" => :mojave
-    sha256 "d5ac6d80981a9e291b9a845d50406438cb702419d52eee194054b6615704e0b8" => :high_sierra
+    sha256 "b6c70ab47dce57d05ec1d38ca3de8a9cc058059259f8191e8c5c5fc1ff9b4cef" => :catalina
+    sha256 "2cd1fe79381fc1c5e25213f84fea2275e4eb48b2a66a3b7f324f71bccfac78b5" => :mojave
+    sha256 "1d9226ef0c646f325453ebd6ba6941d13efb5e19d9b55f148e1dc09e481faf01" => :high_sierra
   end
 
   depends_on "node"
