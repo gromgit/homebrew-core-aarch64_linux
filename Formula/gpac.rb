f45912656b3c5f63a46eb0b1b6af4fc0ed3bf9d5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-21 19:06:07 +0000

    gpac: update 1.0.0 bottle.

--- Formula/gpac.rb
+++ Formula/gpac.rb
@@ -15,10 +15,9 @@ class Gpac < Formula
 
   bottle do
     cellar :any
-    sha256 "4fc891e2da6330b285fa451d8ef381a584ce6db88775b236dfea9be15ca5ec94" => :catalina
-    sha256 "e524e50f08bcb03f3c8af5313f517bd51382a0cf830aa632499834e1f396514e" => :mojave
-    sha256 "3cc7442693e7173b95ae2b7cd5ed8eb4e89a7e5859284d1f12f21a181e8d2bdf" => :high_sierra
-    sha256 "0f2a6e4e1a24d9754b71062cbe86c871db8a98915933a819bfee8e0e68baf4c7" => :sierra
+    sha256 "615c346a9a86fd6fcdc1591bf6c5a634ae408e0ba39e11ec1b9e011488e278e3" => :catalina
+    sha256 "167c5e24935a3f4b118e0d931f1d3ed2a4101a39b5de85910ec7384ee13f95b1" => :mojave
+    sha256 "420874d6129c931e0dd7051bb2a91fd492f40456ade36259de611546f409f539" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
