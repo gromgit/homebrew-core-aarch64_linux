107f0d543760c9baf261b1f423e6f7cadf7da206
Rui Chen <rui@meetup.com>
2020-06-20 23:55:24 -0400

    mavsdk 0.28.0
    Closes #56620.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mavsdk.rb
+++ Formula/mavsdk.rb
@@ -2,8 +2,8 @@ class Mavsdk < Formula
   desc "API and library for MAVLink compatible systems written in C++11"
   homepage "https://mavsdk.mavlink.io"
   url "https://github.com/mavlink/MAVSDK.git",
-    :tag      => "v0.27.0",
-    :revision => "10b1968d108042cd3ead1613a51eb5441fb9c755"
+    :tag      => "v0.28.0",
+    :revision => "a9b2416293aad8dee1b6ad0f9d39f7072b1e66b4"
 
   bottle do
     cellar :any
