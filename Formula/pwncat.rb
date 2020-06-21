51fecc5faae2ce74685053628f8a8de851aa68dc
chenrui <chenrui333@gmail.com>
2020-06-21 20:22:29 +0000

    pwncat: add 0.1.0 bottle.

--- Formula/pwncat.rb
+++ Formula/pwncat.rb
@@ -6,6 +6,13 @@ class Pwncat < Formula
   url "https://files.pythonhosted.org/packages/31/8a/efc45ecc5e91afc76de9c56f89de99af01d575529fead6ee24331a3fddf2/pwncat-0.1.0.tar.gz"
   sha256 "4f711c3d0f22650e20ad1429a7f0c9116b930be04435e7f690746ca0e1c5cd69"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "effc23c8094a031f9bc1964adf5f5ab050500b86726c948f81fe67d35f5e700d" => :catalina
+    sha256 "c140bcf0cca3b1b2176e762dbbafb1c02f831c032741ece1dda4f95fa0ada397" => :mojave
+    sha256 "cc5dcb70ed1762f24f7d0511b74b2f119403f2bf8bc463aec844b8701682e2f2" => :high_sierra
+  end
+
   depends_on "python@3.8"
 
   def install
