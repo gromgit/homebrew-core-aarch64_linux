aa98ce83c2c65d204e064a22c6fd6c8f942e3ac4
Caleb Xu <calebcenter@live.com>
2020-06-22 15:52:17 -0400

    epubcheck 4.2.3
    Closes #56702.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/epubcheck.rb
+++ Formula/epubcheck.rb
@@ -1,8 +1,8 @@
 class Epubcheck < Formula
   desc "Validate EPUB files, version 2.0 and later"
   homepage "https://github.com/w3c/epubcheck"
-  url "https://github.com/w3c/epubcheck/releases/download/v4.2.2/epubcheck-4.2.2.zip"
-  sha256 "029ee9cda237a42e0d4c1a2b8771a440a4a6326449db843510b16a3c61045463"
+  url "https://github.com/w3c/epubcheck/releases/download/v4.2.3/epubcheck-4.2.3.zip"
+  sha256 "03437a2cd268009b33f1e62cf4021186eb2581ea0ae47f5f6b83561ead515d7f"
 
   bottle :unneeded
 
