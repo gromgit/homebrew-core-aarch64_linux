647ff165db4c8bcf52b3ccb9f6e114538f0ed385
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-22 12:07:51 +0000

    onednn: add 1.5 bottle.

--- Formula/onednn.rb
+++ Formula/onednn.rb
@@ -5,6 +5,13 @@ class Onednn < Formula
   sha256 "2aacc00129418185e0bc1269d3ef82f93f08de2c336932989c0c360279129edb"
   head "https://github.com/oneapi-src/onednn.git"
 
+  bottle do
+    cellar :any
+    sha256 "6c0397633ae7e439d40026bff8593cdf90385b3f08e2a5286c9dbb1684e16097" => :catalina
+    sha256 "179cc20a24a1181e27ecacbdc2ea43f8ebf4575ab798dca288fa32f842c3fbaf" => :mojave
+    sha256 "6a22a61f6c15c14487513dc882c1f762036131a15cf4b2b743bb1590e3f207f3" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
 
