dbc40ab823f8dfb9fbe1969057b9767629739d1c
Alexander Bayandin <a.bayandin@gmail.com>
2020-06-23 23:38:22 +0100

    jpeg-turbo 2.0.5
    Closes #56789.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jpeg-turbo.rb
+++ Formula/jpeg-turbo.rb
@@ -1,8 +1,8 @@
 class JpegTurbo < Formula
   desc "JPEG image codec that aids compression and decompression"
   homepage "https://www.libjpeg-turbo.org/"
-  url "https://downloads.sourceforge.net/project/libjpeg-turbo/2.0.4/libjpeg-turbo-2.0.4.tar.gz"
-  sha256 "33dd8547efd5543639e890efbf2ef52d5a21df81faf41bb940657af916a23406"
+  url "https://downloads.sourceforge.net/project/libjpeg-turbo/2.0.5/libjpeg-turbo-2.0.5.tar.gz"
+  sha256 "16f8f6f2715b3a38ab562a84357c793dd56ae9899ce130563c72cd93d8357b5d"
   head "https://github.com/libjpeg-turbo/libjpeg-turbo.git"
 
   bottle do
