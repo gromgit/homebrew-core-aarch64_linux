2ee63db19d65064f72faba6edc1356b61d9a2c2c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-23 06:07:32 +0000

    grpc: update 1.30.0 bottle.

--- Formula/grpc.rb
+++ Formula/grpc.rb
@@ -8,9 +8,9 @@ class Grpc < Formula
   head "https://github.com/grpc/grpc.git"
 
   bottle do
-    sha256 "c96ba02fd205cb9dfa8dfc90d2b76c7e78fd361b6c0cc627d824926c62d4f7e5" => :catalina
-    sha256 "38b16768396deae0813d4eb3d903cf227023776583b5cc95fe46e0150eb59707" => :mojave
-    sha256 "3b27b761aa04fa300d42b223a32fbd53b72466a2c1f27d87650c2c3780725d58" => :high_sierra
+    sha256 "8f3212ca72a09d11787864d33503116d0527478565f36f666b68b10f6e286173" => :catalina
+    sha256 "eb609a63758d89d676d8e41e6f2b4a1d9eef0c5e2aeeffa85dc883728c9c79ae" => :mojave
+    sha256 "990d0b3fa57618f5b19d5ea719bbe1817f8fc17e51e05a8709b097e4acdaa18e" => :high_sierra
   end
 
   depends_on "autoconf" => :build
