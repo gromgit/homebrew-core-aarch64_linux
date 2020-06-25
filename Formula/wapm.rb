151c6cfb49719c79d691c2048ac39e8fe8c88a84
Dario Vladovic <d.vladimyr@gmail.com>
2020-06-25 16:37:35 +0200

    wapm: use std_cargo_args

--- Formula/wapm.rb
+++ Formula/wapm.rb
@@ -16,7 +16,7 @@ class Wapm < Formula
   depends_on "wasmer" => :test
 
   def install
-    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
