8a1895c1f5a95b61c7bfa6ba29b53f09b130f8f4
Dario Vladovic <d.vladimyr@gmail.com>
2020-06-25 16:37:28 +0200

    miniserve: use std_cargo_args

--- Formula/miniserve.rb
+++ Formula/miniserve.rb
@@ -16,7 +16,7 @@ class Miniserve < Formula
   depends_on "rust"
 
   def install
-    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
