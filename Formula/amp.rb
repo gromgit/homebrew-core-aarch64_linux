8089b58f61f0f0639966d0b20eb2aa46d210644f
Dario Vladovic <d.vladimyr@gmail.com>
2020-06-25 16:37:18 +0200

    amp: use std_cargo_args

--- Formula/amp.rb
+++ Formula/amp.rb
@@ -18,7 +18,7 @@ class Amp < Formula
   uses_from_macos "libiconv"
 
   def install
-    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
