87f7e7d1b0527033acd381545ba5974ea002e86c
Sam Ford <1584702+samford@users.noreply.github.com>
2020-06-25 16:06:25 -0400

    gradio: update deprecate date format

--- Formula/gradio.rb
+++ Formula/gradio.rb
@@ -10,7 +10,7 @@ class Gradio < Formula
     sha256 "5c3f745ad431c61ef3d19b4a2a03d6f24eb99dd47768178e6d1810edba4f12fa" => :high_sierra
   end
 
-  deprecate! :date => "November 16, 2019"
+  deprecate! :date => "2019-11-16"
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
