f1589e0636041e406e55c621028f1f696346ee26
Dawid Dziurla <dawidd0811@gmail.com>
2020-06-25 08:03:59 +0000

    exempi: update 2.5.2 bottle.

--- Formula/exempi.rb
+++ Formula/exempi.rb
@@ -6,10 +6,9 @@ class Exempi < Formula
 
   bottle do
     cellar :any
-    sha256 "56a9fb56625cb579bac0bd809b934c8147e081dd90fe474669ba650dfc489d8f" => :catalina
-    sha256 "7ae283f3e160cf837544567cd015c8c2e7d68499584405bc1509916639f72c21" => :mojave
-    sha256 "b7f6fc2e81ca747e6ed8c899bc8353cbba0006959834bf7bd495eb482fd75658" => :high_sierra
-    sha256 "0b9773e6206ec4be656de18f2cb8e13e32dc892745867f048f9524d87381efab" => :sierra
+    sha256 "3ef58fd5cbd177ac785cfab9b58f813ce24320a507243d9d7b6c940fd463564f" => :catalina
+    sha256 "189bb3c57e78845c33678353cb877ad7cdedd665087c0a4525397f32088abc39" => :mojave
+    sha256 "0843f9bc589fd3c9ed0f5dfd724ba60eea4832410a0b6ff831bdb22c6563eafd" => :high_sierra
   end
 
   depends_on "boost"
