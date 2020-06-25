766205644a4726d6c30f53fd4ff3c78bf59ec888
Dario Vladovic <d.vladimyr@gmail.com>
2020-06-25 16:37:23 +0200

    ffsend: use std_cargo_args

--- Formula/ffsend.rb
+++ Formula/ffsend.rb
@@ -19,7 +19,7 @@ class Ffsend < Formula
   end
 
   def install
-    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    system "cargo", "install", *std_cargo_args
 
     bash_completion.install "contrib/completions/ffsend.bash"
     fish_completion.install "contrib/completions/ffsend.fish"
