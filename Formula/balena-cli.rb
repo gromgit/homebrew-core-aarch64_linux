413d9896c436113ad25dc96c41f105b43c9ded33
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-25 13:06:30 +0000

    balena-cli: update 12.1.12 bottle.

--- Formula/balena-cli.rb
+++ Formula/balena-cli.rb
@@ -8,9 +8,9 @@ class BalenaCli < Formula
   sha256 "adb9adb36ee9b21b2915eac4f7ad7cd6a32077840cd6e528fa2d9d8d7f908394"
 
   bottle do
-    sha256 "b50d78486a57fc1467329cf54dbba718ab5c5ab0acd353d3609a47e544d4e07f" => :catalina
-    sha256 "88524ff1c4fad49e50d82ccd3899c65dfda8a317155e75752ec6c0ed4921bd22" => :mojave
-    sha256 "db902fa0ed768901121858705dd3b1ce49ea6f8b606a49de2bf9fa86cdad2566" => :high_sierra
+    sha256 "121dd87e69e4831a7d50d12d2f80a7d504628be025d1e80fe2ed6e4c646ae706" => :catalina
+    sha256 "12c0054c6cea128affc15d3907a7ef9cc2cfb9110bbf4226077758d98a579483" => :mojave
+    sha256 "3d6b5858fcdc6682fab2348822322a731da14f64e9a6c01828d39158924d7238" => :high_sierra
   end
 
   depends_on "node"
