651a4312a64cdc09c89fd46b31a12a4dcbf5b21c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-25 04:07:28 +0000

    ngircd: update 26 bottle.

--- Formula/ngircd.rb
+++ Formula/ngircd.rb
@@ -6,9 +6,10 @@ class Ngircd < Formula
   sha256 "128441256c489f67a63c6d8459b97f0106959526ccd70b513eba2508dfbac651"
 
   bottle do
-    sha256 "ab560382241371099b6dd976be87d317c806ae26373d326e0bb3e3dd3fad0458" => :catalina
-    sha256 "b0cd1030229d21b7c8f7d100e26f83c4c201741f796d6df48c8ba628d2c69b41" => :mojave
-    sha256 "6c3ccfd94aa2ad7a24354ada84b40f34ee1891a3130cd0987f2e7139f0187b84" => :high_sierra
+    rebuild 1
+    sha256 "129bf331b86f1d54735cf773a36b402323af0667afc1e2a1da4554899d05e8e3" => :catalina
+    sha256 "7fdce95437555b6de33aa1b0c9d3e3441405b5d3c9ec2f728a7b28f35983402d" => :mojave
+    sha256 "b58cacea0c009487e46b48d7220623001e49ad31722704ee805fd7029313f5d6" => :high_sierra
   end
 
   depends_on "libident"
