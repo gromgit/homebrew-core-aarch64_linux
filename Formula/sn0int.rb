447fa18b2ed7c67ffe89970e4760e99451100646
Dario Vladovic <d.vladimyr@gmail.com>
2020-06-25 16:37:32 +0200

    sn0int: use std_cargo_args

--- Formula/sn0int.rb
+++ Formula/sn0int.rb
@@ -19,7 +19,7 @@ class Sn0int < Formula
   uses_from_macos "sqlite"
 
   def install
-    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    system "cargo", "install", *std_cargo_args
 
     system "#{bin}/sn0int completions bash > sn0int.bash"
     system "#{bin}/sn0int completions fish > sn0int.fish"
