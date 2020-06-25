887a3f15ed7528c52844d8f2cf9ed1cb798b900a
Dario Vladovic <d.vladimyr@gmail.com>
2020-06-25 16:37:21 +0200

    click: use std_cargo_args

--- Formula/click.rb
+++ Formula/click.rb
@@ -17,7 +17,7 @@ class Click < Formula
   uses_from_macos "expect" => :test
 
   def install
-    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
