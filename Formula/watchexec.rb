3356668a24bdd1cc6f27f346b192cb12df4b20a1
Dario Vladovic <d.vladimyr@gmail.com>
2020-06-25 16:37:35 +0200

    watchexec: use std_cargo_args

--- Formula/watchexec.rb
+++ Formula/watchexec.rb
@@ -14,7 +14,7 @@ class Watchexec < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    system "cargo", "install", *std_cargo_args
     man1.install "doc/watchexec.1"
   end
 
