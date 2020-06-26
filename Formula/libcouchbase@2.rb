a64b93f6b97600451b9e99adb661ade38f810943
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-26 04:08:04 +0000

    libcouchbase@2: update 2.10.7 bottle.

--- Formula/libcouchbase@2.rb
+++ Formula/libcouchbase@2.rb
@@ -4,9 +4,9 @@ class LibcouchbaseAT2 < Formula
   url "https://packages.couchbase.com/clients/c/libcouchbase-2.10.7.tar.gz"
   sha256 "bbee97ef2394f275b8cd4b9b940010566a2d34d4754ba3532a7fca3d94029b75"
   bottle do
-    sha256 "b2069cc7be17f5bf8508b8262d5dee0b1948758f8d65eaab3ded9baad902100d" => :catalina
-    sha256 "70cdc91856c6c9c24e0e34ba59afffdb42fbe6c95cb0e3c2ff09f22d266b4f6d" => :mojave
-    sha256 "7dc8d8f3258f267952e2e0bf87004d650a88a7bf1ed64bbaf56934d42271ad56" => :high_sierra
+    sha256 "d3065af67b6c27379c674fadeca9a3a37eda0323d701769c34ab4aa5d5e1a345" => :catalina
+    sha256 "b0fc35ac0a09d79476957fef601928fce6e9bc1abee8ec3522d59a389754fa39" => :mojave
+    sha256 "4efb6194205ecbd2149ccda7631ca9f5257bcf8bc8ea00e89d50fbb7f7113da2" => :high_sierra
   end
 
   keg_only :versioned_formula
