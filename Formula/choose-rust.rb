d816073697a68e539f1fd68d6fb11f44edb3347d
Issy Long <me@issyl0.co.uk>
2020-06-26 09:50:33 +0000

    choose-rust: add 1.2.0 bottle.

--- Formula/choose-rust.rb
+++ Formula/choose-rust.rb
@@ -4,6 +4,13 @@ class ChooseRust < Formula
   url "https://github.com/theryangeary/choose/archive/v1.2.0.tar.gz"
   sha256 "5711a301e8cc7a5f257773feef992ce8da714a9ee167f234943203ee36c923ad"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8fa22384529ff492abd9859f57b7b771e0f96f911ef0a56845154c8eff73fc4d" => :catalina
+    sha256 "8753e0f24b129a4417ca550665517946aeec068d94dfe1cc129685bc8f74aafc" => :mojave
+    sha256 "fa2a955907368022bd282626b6043d1ed6647d357eff05cdd634ae809ea33b9a" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   conflicts_with "choose", :because => "both install a `choose` binary"
