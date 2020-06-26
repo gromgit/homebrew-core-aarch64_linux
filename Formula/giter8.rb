ec7a17fb16b9847d78d10a0213fe19e5450dc5ae
eugene yokota <eed3si9n@gmail.com>
2020-06-26 18:46:41 -0400

    giter8 0.13.1 (#56942)

--- Formula/giter8.rb
+++ Formula/giter8.rb
@@ -1,8 +1,8 @@
 class Giter8 < Formula
   desc "Generate files and directories from templates in a git repo"
   homepage "https://github.com/foundweekends/giter8"
-  url "https://search.maven.org/remotecontent?filepath=org/foundweekends/giter8/giter8-bootstrap_2.12/0.12.0/giter8-bootstrap_2.12-0.12.0.sh"
-  sha256 "77cf4326076ab0c159b8370bb3c6980fd11b32180dc7a0559625bd07ba5aa393"
+  url "https://search.maven.org/remotecontent?filepath=org/foundweekends/giter8/giter8-bootstrap_2.12/0.13.1/giter8-bootstrap_2.12-0.13.1.sh"
+  sha256 "f3465f6b69a68c4cc67086446e6f583af3a243b2541e64dc32122a94999d29e0"
 
   bottle :unneeded
 
