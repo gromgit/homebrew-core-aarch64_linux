b1b2e201ec23d897753a5c447769cf0745506200
Sam Ford <1584702+samford@users.noreply.github.com>
2020-06-26 00:35:19 -0400

    node_exporter: add head (#56909)

--- Formula/node_exporter.rb
+++ Formula/node_exporter.rb
@@ -3,6 +3,7 @@ class NodeExporter < Formula
   homepage "https://prometheus.io/"
   url "https://github.com/prometheus/node_exporter/archive/v1.0.1.tar.gz"
   sha256 "a841bf3e236376840be9e1d8e6c4a38196be6f3957b0982d1c7970a5e416b0ad"
+  head "https://github.com/prometheus/node_exporter.git"
 
   bottle do
     cellar :any_skip_relocation
