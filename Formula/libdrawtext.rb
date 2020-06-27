02b4149c5ccdd93111bd7905ebdb8450813f0434
chenrui <chenrui333@gmail.com>
2020-06-27 21:41:34 +0000

    libdrawtext: update 0.5 bottle.

--- Formula/libdrawtext.rb
+++ Formula/libdrawtext.rb
@@ -7,13 +7,9 @@ class Libdrawtext < Formula
 
   bottle do
     cellar :any
-    sha256 "71027e757bc74f40a619b63c38fdd2aec7c7d96a2c9bc3f3b507e958338d3cb4" => :catalina
-    sha256 "62b2abf2b3daeb3832174f342b63d9b684f9c5314305dcd53aedc20c64be9cf3" => :mojave
-    sha256 "56701e24e6d2d89dfab1e6857ee450394ca155409e659d87578874e5dcb09fdc" => :high_sierra
-    sha256 "b964ecf876b0e7118dbc8f6b39f0295f3f93244db5109d512258f0f036975e9b" => :sierra
-    sha256 "f0ea7bf5a4ddaa71eabcf015be0c774a707eb44c0bab20dbb87633f3fbe11941" => :el_capitan
-    sha256 "9aa0cb7f932e819bf07d4da05a5e134bb0dbce0aa07057a78af14ae03ef2423f" => :yosemite
-    sha256 "e734a70c82a43accad6adc21b747accce048efb521f04f03767ddaa16ffc53e4" => :mavericks
+    sha256 "e6aea4db0e3298e04dfbb215bcedcc4302d76965f82b4dfc921636dbc24ff939" => :catalina
+    sha256 "0c63e8d53c61bcda8452501c584b2f06919054f4e447fa9ce8c929b0bee50d24" => :mojave
+    sha256 "a4169631c0ac82995409931836ea16664618d37650337e994bfea7121a386791" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
