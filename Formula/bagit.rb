6cbf440babbfb171bf0d41f120ee387874b42083
Rui Chen <rui@meetup.com>
2020-06-27 15:06:29 -0400

    bagit: move to python version
    update tests
    
    fix syntax
    
    reset version
    
    Closes #57043.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bagit.rb
+++ Formula/bagit.rb
@@ -1,26 +1,26 @@
 class Bagit < Formula
+  include Language::Python::Virtualenv
+
   desc "Library for creation, manipulation, and validation of bags"
-  homepage "https://github.com/LibraryOfCongress/bagit-java"
-  url "https://github.com/LibraryOfCongress/bagit-java/releases/download/v4.12.3/bagit-v4.12.3.zip"
-  sha256 "7736d00fafe575ea298788a8d697a2af490dd12f856b43cd57bf579a374199e1"
-  revision 1
+  homepage "https://libraryofcongress.github.io/bagit-python/"
+  url "https://files.pythonhosted.org/packages/ee/11/7a7fa81c0d43fb4d449d418eba57fc6c77959754c5c2259a215152810555/bagit-1.7.0.tar.gz"
+  sha256 "f248a3dad06fd3e5d329217baace6ade79d106579696b13e2c0bbc583101ded4"
+  version_scheme 1
+  head "https://github.com/LibraryOfCongress/bagit-python.git"
 
-  bottle :unneeded
-
-  depends_on "openjdk"
+  depends_on "python@3.8"
 
   def install
-    # put logs in var, not in the Cellar
-    (var/"log/bagit").mkpath
-    inreplace "conf/log4j.properties", "${app.home}/logs", "#{var}/log/bagit"
-
-    libexec.install Dir["*"]
-
-    (bin/"bagit").write_env_script libexec/"bin/bagit",
-                                   :JAVA_HOME => Formula["openjdk"].opt_prefix
+    virtualenv_install_with_resources
   end
 
   test do
-    system bin/"bagit"
+    system "#{bin}/bagit.py", "--source-organization", "Library of Congress", testpath.to_s
+    assert_predicate testpath/"bag-info.txt", :exist?
+    assert_predicate testpath/"bagit.txt", :exist?
+    assert_match "Bag-Software-Agent: bagit.py", File.read("bag-info.txt")
+    assert_match "BagIt-Version: 0.97", File.read("bagit.txt")
+
+    assert_match version.to_s, shell_output("#{bin}/bagit.py --version")
   end
 end
