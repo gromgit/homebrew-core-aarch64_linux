33b32318705d11b955dba5892b9dd0cd5c7b0841
Rui Chen <rui@meetup.com>
2020-06-27 17:03:43 -0400

    libdrawtext 0.5
    fix test
    
    Closes #56986.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/libdrawtext.rb
+++ Formula/libdrawtext.rb
@@ -1,9 +1,8 @@
 class Libdrawtext < Formula
   desc "Library for anti-aliased text rendering in OpenGL"
   homepage "http://nuclear.mutantstargoat.com/sw/libdrawtext/"
-  url "https://github.com/jtsiomb/libdrawtext/archive/release_0.4.tar.gz"
-  sha256 "e9460eb489e0ef6d1496afed2dae2e41c94005c85737ff53a8c09d51b6f93074"
-  revision 1
+  url "https://github.com/jtsiomb/libdrawtext/archive/v0.5.tar.gz"
+  sha256 "7eea99dbf9c86698b5b00ad7f0675b9327098112bf5c11f1bad0635077eae8a9"
   head "https://github.com/jtsiomb/libdrawtext.git"
 
   bottle do
@@ -20,6 +19,11 @@ class Libdrawtext < Formula
   depends_on "pkg-config" => :build
   depends_on "freetype"
 
+  patch do
+    url "https://github.com/jtsiomb/libdrawtext/commit/543cfc67beb76e2c25df0a329b5d38eff9d36c71.diff?full_index=1"
+    sha256 "a2d7ad77e7f1a4590ca85754de2f9961c921c34723f6c86cdd23395cc3566be0"
+  end
+
   def install
     system "./configure", "--disable-dbg", "--enable-opt", "--prefix=#{prefix}"
     system "make", "install"
@@ -33,6 +37,6 @@ class Libdrawtext < Formula
     cp "/System/Library/Fonts/LastResort.#{ext}", testpath
     system bin/"font2glyphmap", "LastResort.#{ext}"
     bytes = File.read("LastResort_s12.glyphmap").bytes.to_a[0..12]
-    assert_equal [80, 54, 10, 53, 49, 50, 32, 50, 53, 54, 10, 35, 32], bytes
+    assert_equal [80, 53, 10, 53, 49, 50, 32, 53, 49, 50, 10, 35, 32], bytes
   end
 end
