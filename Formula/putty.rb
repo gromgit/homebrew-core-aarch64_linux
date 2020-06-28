d37b507f01d2ce85b52e5dbfa11567f009cccacd
Dustin Rodrigues <dust.rod@gmail.com>
2020-06-28 15:12:57 -0400

    putty 0.74
    Closes #57047.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/putty.rb
+++ Formula/putty.rb
@@ -1,8 +1,8 @@
 class Putty < Formula
   desc "Implementation of Telnet and SSH"
   homepage "https://www.chiark.greenend.org.uk/~sgtatham/putty/"
-  url "https://the.earth.li/~sgtatham/putty/0.73/putty-0.73.tar.gz"
-  sha256 "3db0b5403fb41aecd3aa506611366650d927650b6eb3d839ad4dcc782519df1c"
+  url "https://the.earth.li/~sgtatham/putty/0.74/putty-0.74.tar.gz"
+  sha256 "ddd5d388e51dd9e6e294005b30037f6ae802239a44c9dc9808c779e6d11b847d"
 
   bottle do
     cellar :any_skip_relocation
