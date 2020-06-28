e2e3ca639ad485666af7e61d79d28030d84af53f
chenrui <chenrui333@gmail.com>
2020-06-27 20:36:03 -0400

    neo4j 4.1.0 (#56992)

--- Formula/neo4j.rb
+++ Formula/neo4j.rb
@@ -1,8 +1,8 @@
 class Neo4j < Formula
   desc "Robust (fully ACID) transactional property graph database"
   homepage "https://neo4j.com/"
-  url "https://neo4j.com/artifact.php?name=neo4j-community-4.0.6-unix.tar.gz"
-  sha256 "47a7edfe0f673517c7c21b33b5f124377cbfa1763081475ba1013caa6ad2340e"
+  url "https://neo4j.com/artifact.php?name=neo4j-community-4.1.0-unix.tar.gz"
+  sha256 "b598edeb3401e5ec40fb7bc3370307addfcaa21565f731016c9c7f8e70af659a"
 
   bottle :unneeded
 
@@ -11,6 +11,8 @@ class Neo4j < Formula
   # https://github.com/neo4j/neo4j/issues/11728#issuecomment-387038804
   # https://github.com/neo4j/neo4j-browser/issues/671#issuecomment-346224754
   # https://github.com/Homebrew/homebrew-core/issues/31090
+  # As of v4.1.0, neo4j still needs to build with java 1.8
+  # https://github.com/neo4j/neo4j/issues/12516
   depends_on :java => "1.8"
 
   def install
