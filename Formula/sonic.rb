c317a7f20600c4579b80ec2bd0a2a498f9ef1aa6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-28 02:08:20 +0000

    sonic: update 1.3.0 bottle.

--- Formula/sonic.rb
+++ Formula/sonic.rb
@@ -6,9 +6,9 @@ class Sonic < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "084a2a6ae18232ddcfdbe2a5a28dee53feff5876f1b5c70513cab74956f5fda0" => :catalina
-    sha256 "c844ce9bf1a10f0483ef03e8a29d853d200ddf605829418c7246832701695322" => :mojave
-    sha256 "72b44c6fd48e1fd331b028a42bb66c030c28f71c09cf9662a987cd021731669a" => :high_sierra
+    sha256 "d22157b31f471d3b5a74018cef0fbcb5c5b6cf4f2a59df7b3cfe315090d3d3b4" => :catalina
+    sha256 "5e68bc4761ff25830382fe068ef89a38c71762f98958cf1b7f3dc1db8dc7cc26" => :mojave
+    sha256 "184bf1ac4972c580d1a648f48a4aa6f01fecdc1aeefb2cd0bb6789232fc2ba22" => :high_sierra
   end
 
   depends_on "rust" => :build
