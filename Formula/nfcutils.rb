9c7ee37ff1f9012d05e976bec03fcf696602e906
Rui Chen <rui@meetup.com>
2020-06-28 15:04:30 -0400

    nfcutils: revision for libnfc
    Closes #57035.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/nfcutils.rb
+++ Formula/nfcutils.rb
@@ -3,6 +3,7 @@ class Nfcutils < Formula
   homepage "https://github.com/nfc-tools/nfcutils"
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/nfc-tools/nfcutils-0.3.2.tar.gz"
   sha256 "dea258774bd08c8b7ff65e9bed2a449b24ed8736326b1bb83610248e697c7f1b"
+  revision 1
 
   bottle do
     cellar :any
