29c1c18ea2bd82a1a53fbb1453f91bb05828738a
Rui Chen <chenrui333@gmail.com>
2020-06-28 18:52:48 -0400

    nasm 2.15.01
    Closes #57054.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/nasm.rb
+++ Formula/nasm.rb
@@ -1,8 +1,8 @@
 class Nasm < Formula
   desc "Netwide Assembler (NASM) is an 80x86 assembler"
   homepage "https://www.nasm.us/"
-  url "https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.xz"
-  sha256 "e24ade3e928f7253aa8c14aa44726d1edf3f98643f87c9d72ec1df44b26be8f5"
+  url "https://www.nasm.us/pub/nasm/releasebuilds/2.15.01/nasm-2.15.01.tar.xz"
+  sha256 "28a50f80d2f4023e444b113e9ddc57fcec2b2f295a07ce158cf3f18740375831"
 
   bottle do
     cellar :any_skip_relocation
