12ca85f5cb18ee46e1dfd2a4fc4845d0fb6d83ea
Rui Chen <rui@meetup.com>
2020-06-28 10:50:26 -0400

    muparser: update homepage
    Closes #56991.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/muparser.rb
+++ Formula/muparser.rb
@@ -1,6 +1,6 @@
 class Muparser < Formula
   desc "C++ math expression parser library"
-  homepage "https://beltoforion.de/article.php?a=muparser"
+  homepage "https://beltoforion.de/en/muparser/"
   url "https://github.com/beltoforion/muparser/archive/v2.3.2.tar.gz"
   sha256 "b35fc84e3667d432e3414c8667d5764dfa450ed24a99eeef7ee3f6647d44f301"
   head "https://github.com/beltoforion/muparser.git"
