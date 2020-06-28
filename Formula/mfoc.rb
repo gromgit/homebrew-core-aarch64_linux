81bfccb5f4dc7d83de3192100e6d410efc132506
chenrui <chenrui333@gmail.com>
2020-06-28 21:39:11 +0000

    mfoc: update 0.10.7_1 bottle.

--- Formula/mfoc.rb
+++ Formula/mfoc.rb
@@ -7,13 +7,9 @@ class Mfoc < Formula
 
   bottle do
     cellar :any
-    sha256 "ae0c48b0627ab98e77eb92b35be2fc049cb7fdf39da9cec82c03342c09d8d852" => :catalina
-    sha256 "5a4fe631caec6c2070b408158048c9d5976da9aa0fc8f1e60bd43dc64616db39" => :mojave
-    sha256 "854390fb4717577ae0cfa33cffc50c0d5d568f23b28401aee6c81661b12dede4" => :high_sierra
-    sha256 "be07709faf874fb9b9457950201a71b5f683f7971f2a3a7d61de630f78a2e69c" => :sierra
-    sha256 "d895fdd47221e48e45f6858a1dc1c39c79ae743419b93ec4dfce84baa80af0a9" => :el_capitan
-    sha256 "931fb4f22e9b02e40b4f1f28210df4a994a12efbd746bd7644d99a02837dbfda" => :yosemite
-    sha256 "ecbf3f20c620ad022ab253ed68bc0005ee05c1fcc9eb178d6cc33bdf34f4515a" => :mavericks
+    sha256 "14c431c29b0b0e746d1533606ab13097a84b853c13d4399672027cf9256dad32" => :catalina
+    sha256 "ff9f6c43ef70b8ae6fee40c43cf5f0acd6f72acd5507874e75d82703aeed5fc3" => :mojave
+    sha256 "83a0236f5971e007e67e620730d458f8dcdcb7ff7770cc97c07407a771dbf69a" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
