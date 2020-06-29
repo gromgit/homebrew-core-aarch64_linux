ab1b592011f5391e9b92a55f9e72c423d81ff45f
chenrui <chenrui333@gmail.com>
2020-06-29 21:17:03 +0000

    sfcgal: update 1.3.8 bottle.

--- Formula/sfcgal.rb
+++ Formula/sfcgal.rb
@@ -5,9 +5,9 @@ class Sfcgal < Formula
   sha256 "5154bfc67a5e99d95cb653d70d2b9d9293d3deb3c8f18b938a33d68fec488a6d"
 
   bottle do
-    sha256 "fefaaab69ba9fc4664303b558c01a9bcc584fbe39b0d34e2373d384c4a371e2e" => :catalina
-    sha256 "e69cc15c8b93ddd06a9c65acd55afc338d520939d8781cbe97d8238548eee380" => :mojave
-    sha256 "58cd7e79ae765402b421fc1f4a6b7fd3b9d9e4a54bfe06fb5687b6b886c25bb4" => :high_sierra
+    sha256 "f78f5ccb5b6feb0e7530770d3411149e4335ead8a2bcd08e275cbe7748b3c396" => :catalina
+    sha256 "39a00ba2c7681850a601053adcfd8f920e2037aee5ebe9abf0f9a1b89ac674a1" => :mojave
+    sha256 "86c543cb703b1d7885045354917e7da1a1fa3e52ca6749359f64ea736af74aa0" => :high_sierra
   end
 
   depends_on "cmake" => :build
