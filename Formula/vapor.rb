e9f1adebe082fbe147565b191ecc66f55e41d561
chenrui <chenrui333@gmail.com>
2020-06-29 23:01:42 +0000

    vapor: add 18.0.0 bottle.

--- Formula/vapor.rb
+++ Formula/vapor.rb
@@ -5,6 +5,11 @@ class Vapor < Formula
   sha256 "1d1b72d92dfb4e417dbbfe8486149f64a92cbfcf86db5209686fdd926b43c74c"
   head "https://github.com/vapor/toolbox.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d7762239037cff59e33be03863b68cccc19580db9414b1f4bdb012f16df0d2fb" => :catalina
+  end
+
   depends_on :xcode => "11.4"
 
   def install
