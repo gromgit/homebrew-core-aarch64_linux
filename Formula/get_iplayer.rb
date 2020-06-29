83ad99ea5da45088613d83d05db03e1769534a20
Rui Chen <rui@meetup.com>
2020-06-29 09:58:56 -0400

    get_iplayer 3.26
    Closes #57074.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/get_iplayer.rb
+++ Formula/get_iplayer.rb
@@ -1,8 +1,8 @@
 class GetIplayer < Formula
   desc "Utility for downloading TV and radio programmes from BBC iPlayer"
   homepage "https://github.com/get-iplayer/get_iplayer"
-  url "https://github.com/get-iplayer/get_iplayer/archive/v3.25.tar.gz"
-  sha256 "2ae278a812494d6289224e6d347c8761ec815ad84f4daf41fd0a4f62832b7d63"
+  url "https://github.com/get-iplayer/get_iplayer/archive/v3.26.tar.gz"
+  sha256 "2ec105ccb47910d7f3ff7124ecaa839a8778837c12881358e627a132ba027b06"
   head "https://github.com/get-iplayer/get_iplayer.git", :branch => "develop"
 
   bottle do
