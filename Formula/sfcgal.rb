b2640d98c77708301593e60f13d9a96d7bc555df
Rui Chen <rui@meetup.com>
2020-06-29 12:23:48 -0400

    sfcgal 1.3.8
    remove patch
    
    Closes #57092.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/sfcgal.rb
+++ Formula/sfcgal.rb
@@ -1,9 +1,8 @@
 class Sfcgal < Formula
   desc "C++ wrapper library around CGAL"
   homepage "http://sfcgal.org/"
-  url "https://github.com/Oslandia/SFCGAL/archive/v1.3.7.tar.gz"
-  sha256 "30ea1af26cb2f572c628aae08dd1953d80a69d15e1cac225390904d91fce031b"
-  revision 2
+  url "https://github.com/Oslandia/SFCGAL/archive/v1.3.8.tar.gz"
+  sha256 "5154bfc67a5e99d95cb653d70d2b9d9293d3deb3c8f18b938a33d68fec488a6d"
 
   bottle do
     sha256 "fefaaab69ba9fc4664303b558c01a9bcc584fbe39b0d34e2373d384c4a371e2e" => :catalina
@@ -16,12 +15,6 @@ class Sfcgal < Formula
   depends_on "cgal"
   depends_on "gmp"
   depends_on "mpfr"
-
-  # Patch for CGAL-5.0. To be removed next release. See https://github.com/Oslandia/SFCGAL/pull/197 for fix upstream
-  patch do
-    url "https://github.com/Oslandia/SFCGAL/compare/v1.3.7...sloriot:remove_auto_ptr.patch"
-    sha256 "4cc975509368df986ff634ddcf605ad6469aa01bb68659ae21d171ed2a0f5f66"
-  end
 
   def install
     system "cmake", ".", *std_cmake_args
