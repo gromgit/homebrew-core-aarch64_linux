a384abdc9940c8aa0df8997bac40ed49e940b869
Rui Chen <rui@meetup.com>
2020-06-29 15:26:33 -0400

    ccal: update to use http urls
    Closes #57101.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ccal.rb
+++ Formula/ccal.rb
@@ -1,7 +1,8 @@
 class Ccal < Formula
   desc "Create Chinese calendars for print or browsing"
-  homepage "https://ccal.chinesebay.com/ccal"
-  url "https://ccal.chinesebay.com/ccal/ccal-2.5.3.tar.gz"
+  # no https urls
+  homepage "http://ccal.chinesebay.com/ccal/ccal.htm"
+  url "http://ccal.chinesebay.com/ccal/ccal-2.5.3.tar.gz"
   sha256 "3d4cbdc9f905ce02ab484041fbbf7f0b7a319ae6a350c6c16d636e1a5a50df96"
 
   bottle do
