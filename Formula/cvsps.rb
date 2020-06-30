4cbfce58ef6c5bb255873fff3ee74329960c3218
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-06-30 10:06:57 +0000

    cvsps: update 3.13 bottle.

--- Formula/cvsps.rb
+++ Formula/cvsps.rb
@@ -6,13 +6,10 @@ class Cvsps < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "bb047775f259d1a0aa7cb47bbc810eb9bfe73706bafa9b9330fdf4e4d57be153" => :catalina
-    sha256 "661a264420cedda6c6940ef81e88e91ca8d433c4c48cb79a5bb3f3d60c541974" => :mojave
-    sha256 "964d2f695e15f6377f18482820f69f1efcaf305e9ea45b4bd3b6da1ad55238b8" => :high_sierra
-    sha256 "fafd244bbd5ea71ef8940ec6d1d4319b6d2036c68dbafd923ed3aa1126e92c0a" => :sierra
-    sha256 "166312c7b7c2aacd431120ddee3f8ca798809ee955c626cd545688c8fec5e460" => :el_capitan
-    sha256 "fb968620dcdb28706f395b439fa5b2de09a5de2fec23fbb20a929234872d3978" => :yosemite
-    sha256 "72af55d9573f7de107155e0a5e2160a2ef45d729e3970be27e60ecacc4baebbf" => :mavericks
+    rebuild 1
+    sha256 "98ee59741e5e48ef4ca18f5b0b08cfc8eab19d8e96cebcb75e989334add7411f" => :catalina
+    sha256 "8901d9d03137e3ebc2dfa52eeec1b6fb5278aa21fcd2075302ea9c9e20ff1db5" => :mojave
+    sha256 "d67b00e52b9688d1249d996eaf94a728691f0b171b1708e83bba07508939d376" => :high_sierra
   end
 
   # http://www.catb.org/~esr/cvsps/
