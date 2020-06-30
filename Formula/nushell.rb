00100be570b8253aaf2a114c2970b75e40626c7d
Rui Chen <rui@meetup.com>
2020-06-30 19:34:17 -0400

    nushell 0.16.0
    make the test more robust
    
    fix brew style
    
    Closes #57174.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nushell.rb
+++ Formula/nushell.rb
@@ -1,8 +1,8 @@
 class Nushell < Formula
   desc "Modern shell for the GitHub era"
   homepage "https://www.nushell.sh"
-  url "https://github.com/nushell/nushell/archive/0.15.0.tar.gz"
-  sha256 "f19e580b645ff047e3b7cb2e0823654e020cd5c62b22e601caf6be579204dc2a"
+  url "https://github.com/nushell/nushell/archive/0.16.0.tar.gz"
+  sha256 "ba596706bfdb58d10f1185ae0f9cd0988ab77a9972daefba5c93306f6ed314d6"
   head "https://github.com/nushell/nushell.git"
 
   bottle do
@@ -26,7 +26,7 @@ class Nushell < Formula
   end
 
   test do
-    assert_equal pipe_output("#{bin}/nu", 'echo \'{"foo":1, "bar":2}\' | from json | get bar | echo $it'),
-    "Welcome to Nushell #{version} (type 'help' for more info)\n~ \n❯ 2~ \n❯ "
+    assert_match "homebrew_test",
+      pipe_output("#{bin}/nu", 'echo \'{"foo":1, "bar" : "homebrew_test"}\' | from json | get bar')
   end
 end
