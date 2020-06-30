7e9262eb3233fa7f214b56e33fcaf5b017db3658
Mike McQuaid <mike@mikemcquaid.com>
2020-06-30 09:16:41 +0100

    wallpaper: fix components order.

--- Formula/wallpaper.rb
+++ Formula/wallpaper.rb
@@ -2,8 +2,8 @@ class Wallpaper < Formula
   desc "Manage the desktop wallpaper"
   homepage "https://github.com/sindresorhus/macos-wallpaper"
   url "https://github.com/sindresorhus/macos-wallpaper/archive/v2.1.0.tar.gz"
-  revision 1
   sha256 "4925247524535c0cc128dcc4d87f5538a5ce3b5d3a3c211127fd646ee00252b6"
+  revision 1
   head "https://github.com/sindresorhus/macos-wallpaper.git"
 
   bottle do
