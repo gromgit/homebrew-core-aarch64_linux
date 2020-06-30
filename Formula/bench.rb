1552641d4d579e14d920ebbd066b4035554cde35
Mike McQuaid <mike@mikemcquaid.com>
2020-06-30 09:16:39 +0100

    bench: fix components order.

--- Formula/bench.rb
+++ Formula/bench.rb
@@ -1,18 +1,8 @@
 class Bench < Formula
   desc "Command-line benchmark tool"
   homepage "https://github.com/Gabriel439/bench"
-  head "https://github.com/Gabriel439/bench.git"
   revision 1
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "b1eccbf77a04e4de1a59a0eed5c0f6e2d8b6b191736ee9ad4fdea9a173010651" => :catalina
-    sha256 "493de8888b6fe1745a887cda10a421448a08943496124b1cb49cc02453002638" => :mojave
-    sha256 "cd0e9ae0bc13d3db0330ae839689d9b2d129bc0bf0c1b7165033968a9e6a0f22" => :high_sierra
-  end
-
-  depends_on "cabal-install" => :build
-  depends_on "ghc@8.8" => :build
+  head "https://github.com/Gabriel439/bench.git"
 
   stable do
     url "https://hackage.haskell.org/package/bench-1.0.12/bench-1.0.12.tar.gz"
@@ -24,6 +14,16 @@ class Bench < Formula
       sha256 "fac63cd1ddb0af3bda78900df3ac5a4e6b6d2bb8a3d4d94c2f55d3f21dc681d1"
     end
   end
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b1eccbf77a04e4de1a59a0eed5c0f6e2d8b6b191736ee9ad4fdea9a173010651" => :catalina
+    sha256 "493de8888b6fe1745a887cda10a421448a08943496124b1cb49cc02453002638" => :mojave
+    sha256 "cd0e9ae0bc13d3db0330ae839689d9b2d129bc0bf0c1b7165033968a9e6a0f22" => :high_sierra
+  end
+
+  depends_on "cabal-install" => :build
+  depends_on "ghc@8.8" => :build
 
   def install
     system "cabal", "v2-update"
