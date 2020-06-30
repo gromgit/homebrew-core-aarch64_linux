ccde64b2eb86b647004a9dd947b17d943effefdd
Mike McQuaid <mike@mikemcquaid.com>
2020-06-30 09:16:39 +0100

    cgrep: fix components order.

--- Formula/cgrep.rb
+++ Formula/cgrep.rb
@@ -3,8 +3,8 @@ class Cgrep < Formula
   homepage "https://github.com/awgn/cgrep"
   url "https://github.com/awgn/cgrep/archive/v6.6.32.tar.gz"
   sha256 "c45d680a2a00ef9524fc921e4c10fc7e68f02e57f4d6f1e640b7638a2f49c198"
-  head "https://github.com/awgn/cgrep.git"
   revision 1
+  head "https://github.com/awgn/cgrep.git"
 
   bottle do
     cellar :any
