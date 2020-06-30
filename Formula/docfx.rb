8a1d8c93ad8863fe9dc690c6bb6c33f5f9b56f9e
Rui Chen <rui@meetup.com>
2020-06-30 11:12:21 -0400

    docfx 2.56
    Closes #57153.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -1,8 +1,8 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/releases/download/v2.55/docfx.zip"
-  sha256 "900cfe0d7ecee14b312e1faaa7a7663bc1868d5086bd4c88b5c8be8d9df5d34d"
+  url "https://github.com/dotnet/docfx/releases/download/v2.56/docfx.zip"
+  sha256 "8d20b192e59348de705a584b3dec664dd927fd0d1b560b7ab3c2625e489ce474"
 
   bottle :unneeded
 
