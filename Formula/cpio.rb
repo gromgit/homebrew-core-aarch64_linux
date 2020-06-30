b351f58c7ed78bfcc29a461e01ca507de5eb8779
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-06-30 21:07:45 +0000

    cpio: add 2.13 bottle.

--- Formula/cpio.rb
+++ Formula/cpio.rb
@@ -5,6 +5,13 @@ class Cpio < Formula
   mirror "https://ftpmirror.gnu.org/cpio/cpio-2.13.tar.bz2"
   sha256 "eab5bdc5ae1df285c59f2a4f140a98fc33678a0bf61bdba67d9436ae26b46f6d"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7648c6e9a9cc7c7834674ec839f8811134a109e9419fefc165107e1489fffefb" => :catalina
+    sha256 "d8e90cbadcf1856c4c3291ba0d2798984a8c6069464d87be92ef3b9db622118e" => :mojave
+    sha256 "cfd09e4738ac54a935262baf38b546770942e90b6327c3b5342d2604aa37e733" => :high_sierra
+  end
+
   keg_only :shadowed_by_macos, "macOS provides cpio"
 
   def install
