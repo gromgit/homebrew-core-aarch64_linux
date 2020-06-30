876b975927aac89d510577d9287b114100f82e6a
Rui Chen <rui@meetup.com>
2020-06-30 11:09:37 -0400

    awscli@1 1.18.90
    Closes #57148.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/awscli@1.rb
+++ Formula/awscli@1.rb
@@ -4,8 +4,8 @@ class AwscliAT1 < Formula
   desc "Official Amazon AWS command-line interface"
   homepage "https://aws.amazon.com/cli/"
   # awscli should only be updated every 10 releases on multiples of 10
-  url "https://github.com/aws/aws-cli/archive/1.18.80.tar.gz"
-  sha256 "317115eed4d1fa8e1da0a3f9b0a31a050fceacb5d0f097b85bb0ccc48c889140"
+  url "https://github.com/aws/aws-cli/archive/1.18.90.tar.gz"
+  sha256 "590a52374079c459d6a67ab4cf94db6fdbc0311baf1f5a5688f460bc73ca0853"
 
   bottle do
     cellar :any_skip_relocation
