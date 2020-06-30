b5fabb7c3729696d526a0887955ea58a0995565e
Mike McQuaid <mike@mikemcquaid.com>
2020-06-30 09:16:39 +0100

    dhall-json: fix components order.

--- Formula/dhall-json.rb
+++ Formula/dhall-json.rb
@@ -3,8 +3,8 @@ class DhallJson < Formula
   homepage "https://github.com/dhall-lang/dhall-haskell/tree/master/dhall-json"
   url "https://hackage.haskell.org/package/dhall-json-1.6.4/dhall-json-1.6.4.tar.gz"
   sha256 "3f0162949d53e10c23ee11ddfe92a808dc5b18c5a40289015c65baec0450266f"
-  head "https://github.com/dhall-lang/dhall-haskell.git"
   revision 1
+  head "https://github.com/dhall-lang/dhall-haskell.git"
 
   bottle do
     cellar :any_skip_relocation
