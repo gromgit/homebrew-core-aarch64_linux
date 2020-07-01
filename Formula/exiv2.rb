b064a745122312952f4cef034313888d55ed882a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-01 19:06:46 +0000

    exiv2: update 0.27.3 bottle.

--- Formula/exiv2.rb
+++ Formula/exiv2.rb
@@ -7,10 +7,9 @@ class Exiv2 < Formula
 
   bottle do
     cellar :any
-    sha256 "fdadbb93ae659a651b2e7b899c3c2bf8910b8f0891661903a04a4c81a66ff534" => :catalina
-    sha256 "3b78b8fbffcc6d62685bc4a9a0a51855f5ccf6fe7fabc866f0970e1a12ced0b4" => :mojave
-    sha256 "8a4e65d47307247b11127c00cdad18626425eafb271faaeb1c076beb57298e12" => :high_sierra
-    sha256 "fe386bc9bfe7270655a6b3163f8e33a6fc6e6f36512e6ac6e6a49a1650a6a485" => :sierra
+    sha256 "607f8322cba23a92185541c3b8ee245e7ff339becda5364e1ea6c2168015375c" => :catalina
+    sha256 "f4ed492ccb45b869000b2cc514ae507422624f6413057ee158ea80b772e182fb" => :mojave
+    sha256 "cd1d11df6b535b1ccfb3458cef28a7662c1e2b7213382e8292abbe00526c7b52" => :high_sierra
   end
 
   depends_on "cmake" => :build
