604841f73a684c6ef85150e0ec7d6301bbb8ecde
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-01 12:09:44 +0000

    gsmartcontrol: update 1.1.3_5 bottle.

--- Formula/gsmartcontrol.rb
+++ Formula/gsmartcontrol.rb
@@ -6,9 +6,9 @@ class Gsmartcontrol < Formula
   revision 5
 
   bottle do
-    sha256 "556220452f97694f3645f359d2dec863134dfcfb4295143fc96752c26a5fcb17" => :catalina
-    sha256 "69c420e26e619dd25f064703cf27058dee4a55df67240576b599444041baebc8" => :mojave
-    sha256 "5a1c7102e0fd7cfb20d2dc895ce82a24117fe8137f47ec357fa5df6ec14d1ee0" => :high_sierra
+    sha256 "f013a063bc125ddf0eb174d8893edc7eea8433aac4ba910b0ed3a11337171fd4" => :catalina
+    sha256 "f07ac44ce8d76f469f2bd02da3476f41151416dacb64569a03cf936d8b50697f" => :mojave
+    sha256 "67fa6b9c4b3f203916bb66ca3b5b67c2171cbd2d0fd26886746408e09243577f" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
