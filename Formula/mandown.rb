ab766335cc6df901e88aaf3bac8221ce4087bbbf
chenrui <chenrui333@gmail.com>
2020-07-01 23:09:11 +0000

    mandown: add 1.0.1 bottle.

--- Formula/mandown.rb
+++ Formula/mandown.rb
@@ -4,6 +4,13 @@ class Mandown < Formula
   url "https://github.com/Titor8115/mandown/archive/v1.0.1.tar.gz"
   sha256 "b014a44b27f921c12505ba4d8dba15487ca2b442764da645cd6d0fd607ef068c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "09ad2e54a3b54c9687580b4499f4c5247dfd2e18fb64230b3c255fbc7df1c5be" => :catalina
+    sha256 "9186b868866dd17f080343297e145161f3fe6303701a12bd0a47f8ef246f6630" => :mojave
+    sha256 "acf617ed0300f38b429ed05504c47bb9e403441316d335ae83bf28c18baa63a6" => :high_sierra
+  end
+
   uses_from_macos "libxml2"
   uses_from_macos "ncurses"
 
