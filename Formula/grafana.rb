f03305f5aa688f9ac9dfaf5d0ae6fde9a8d5c38a
Bastian Hofmann <bashofmann@gmail.com>
2020-07-01 11:43:18 +0200

    grafana 7.0.5
    Closes #57203.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/grafana.rb
+++ Formula/grafana.rb
@@ -1,8 +1,8 @@
 class Grafana < Formula
   desc "Gorgeous metric visualizations and dashboards for timeseries databases"
   homepage "https://grafana.com"
-  url "https://github.com/grafana/grafana/archive/v7.0.4.tar.gz"
-  sha256 "21801e74c2181d5c37dd3639f270379a5053ea200f59a53c3cfcd1a85e6f89db"
+  url "https://github.com/grafana/grafana/archive/v7.0.5.tar.gz"
+  sha256 "0c088e0018255628396889ed1459164ce5dc0e5539cb3ffb48aa729f8505b6fe"
   head "https://github.com/grafana/grafana.git"
 
   bottle do
