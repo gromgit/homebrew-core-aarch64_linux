460c4a2cca4edb99ff0cd6807581dc45d117c55c
Alfie Pates <alfie@alfiepates.me>
2020-07-02 00:18:42 +0100

    nebula: add plist
    Closes #57258.
    
    Signed-off-by: Rui Chen <rui@meetup.com>

--- Formula/nebula.rb
+++ Formula/nebula.rb
@@ -21,6 +21,38 @@ class Nebula < Formula
     prefix.install_metafiles
   end
 
+  plist_options :startup => true
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/nebula</string>
+          <string>-config</string>
+          <string>#{etc}/nebula/config.yml</string>
+        </array>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/nebula.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/nebula.log</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <dict>
+          <key>NetworkState</key>
+          <true/>
+        </dict>
+      </dict>
+      </plist>
+    EOS
+  end
+
   test do
     system "#{bin}/nebula-cert", "ca", "-name", "testorg"
     system "#{bin}/nebula-cert", "sign", "-name", "host", "-ip", "192.168.100.1/24"
