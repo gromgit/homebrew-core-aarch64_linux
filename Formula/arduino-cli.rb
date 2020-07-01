f25cfb56fa5300600a7d9cfcdf664699cf29d467
Dawid Dziurla <dawidd0811@gmail.com>
2020-07-01 07:47:37 +0000

    arduino-cli: update 0.11.0_1 bottle.

--- Formula/arduino-cli.rb
+++ Formula/arduino-cli.rb
@@ -9,9 +9,9 @@ class ArduinoCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5a8049eed8d57096c6fa53c9e2555f686a3f29b622de099433c85b2b24787637" => :catalina
-    sha256 "c215a2743e597c19c1252ea977f5bc0936964380544d69b0079473ba6a09d95b" => :mojave
-    sha256 "48ab865e380a27c8abc1e661969e1382d44be37bcec31409c5c0546d5e617637" => :high_sierra
+    sha256 "3dd10065c03827d1cc0a508daaf2787c154eda4bfc5378d671b17d16672bd293" => :catalina
+    sha256 "4d176c41402ad7a3ceb8ad20ef4a621675c20bdb080b543193efddad07e7fc26" => :mojave
+    sha256 "d885f46abbb996e64a47d8566677263bca7ca2cacfe78d8bd825d5a4bba02f38" => :high_sierra
   end
 
   depends_on "go" => :build
