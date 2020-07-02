36721ec4ed0515fc550ec4a8328c8875a93d8875
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:03 -0400

    truncate: add license

--- Formula/truncate.rb
+++ Formula/truncate.rb
@@ -3,6 +3,7 @@ class Truncate < Formula
   homepage "https://www.vanheusden.com/truncate/"
   url "https://github.com/flok99/truncate/archive/0.9.tar.gz"
   sha256 "a959d50cf01a67ed1038fc7814be3c9a74b26071315349bac65e02ca23891507"
+  license "AGPL-3.0"
   head "https://github.com/flok99/truncate.git"
 
   bottle do
