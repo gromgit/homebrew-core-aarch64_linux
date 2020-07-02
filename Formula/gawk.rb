f0b56d27229ccd3f98392b0110805fc5e83eb41c
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:23 -0400

    gawk: add license

--- Formula/gawk.rb
+++ Formula/gawk.rb
@@ -4,6 +4,7 @@ class Gawk < Formula
   url "https://ftp.gnu.org/gnu/gawk/gawk-5.1.0.tar.xz"
   mirror "https://ftpmirror.gnu.org/gawk/gawk-5.1.0.tar.xz"
   sha256 "cf5fea4ac5665fd5171af4716baab2effc76306a9572988d5ba1078f196382bd"
+  license "GPL-3.0"
 
   bottle do
     sha256 "581b48f781104f0c3233edc30c47628f4eec8c2f1f2e191151f367ce26ec538a" => :catalina
