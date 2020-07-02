216ea6a21fe3d96d136dc0bdedef1005b6f90efa
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:20 -0400

    pyenv-virtualenvwrapper: add license

--- Formula/pyenv-virtualenvwrapper.rb
+++ Formula/pyenv-virtualenvwrapper.rb
@@ -3,6 +3,7 @@ class PyenvVirtualenvwrapper < Formula
   homepage "https://github.com/pyenv/pyenv-virtualenvwrapper"
   url "https://github.com/pyenv/pyenv-virtualenvwrapper/archive/v20140609.tar.gz"
   sha256 "c1c812c4954394c58628952654ba745c4fb814d045adc076f7fb9e310bed03bf"
+  license "MIT"
   head "https://github.com/pyenv/pyenv-virtualenvwrapper.git"
 
   bottle :unneeded
