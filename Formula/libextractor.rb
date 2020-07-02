e74c88378a0b2e40aa46892ffd6130f5a9277abb
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:18 -0400

    libextractor: add license

--- Formula/libextractor.rb
+++ Formula/libextractor.rb
@@ -4,6 +4,7 @@ class Libextractor < Formula
   url "https://ftp.gnu.org/gnu/libextractor/libextractor-1.10.tar.gz"
   mirror "https://ftpmirror.gnu.org/libextractor/libextractor-1.10.tar.gz"
   sha256 "9eed11b5ddc7c929ba112c50de8cfaa379f1d99a0c8e064101775837cf432357"
+  license "GPL-3.0"
 
   bottle do
     sha256 "1f9781fe4c690eca0d719016cd4f23bd94890ae69cc30c4c1caa47d919286483" => :catalina
