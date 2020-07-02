d07615a1de67919a15cc6e4653eed7ffa1576a65
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:51 -0400

    sysbench: add license

--- Formula/sysbench.rb
+++ Formula/sysbench.rb
@@ -3,6 +3,7 @@ class Sysbench < Formula
   homepage "https://github.com/akopytov/sysbench"
   url "https://github.com/akopytov/sysbench/archive/1.0.20.tar.gz"
   sha256 "e8ee79b1f399b2d167e6a90de52ccc90e52408f7ade1b9b7135727efe181347f"
+  license "GPL-2.0"
 
   bottle do
     cellar :any
