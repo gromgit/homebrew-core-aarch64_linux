2833207ad22607bd62da09ba5c507144e0a172dc
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:05 -0400

    ucl: add license

--- Formula/ucl.rb
+++ Formula/ucl.rb
@@ -3,6 +3,7 @@ class Ucl < Formula
   homepage "https://www.oberhumer.com/opensource/ucl/"
   url "https://www.oberhumer.com/opensource/ucl/download/ucl-1.03.tar.gz"
   sha256 "b865299ffd45d73412293369c9754b07637680e5c826915f097577cd27350348"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
