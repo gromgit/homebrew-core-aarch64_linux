b368e14e6f0a310898fdf152ec683c57400f2980
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:29 -0400

    libtommath: add license

--- Formula/libtommath.rb
+++ Formula/libtommath.rb
@@ -3,6 +3,7 @@ class Libtommath < Formula
   homepage "https://www.libtom.net/LibTomMath/"
   url "https://github.com/libtom/libtommath/releases/download/v1.2.0/ltm-1.2.0.tar.xz"
   sha256 "b7c75eecf680219484055fcedd686064409254ae44bc31a96c5032843c0e18b1"
+  license "Unlicense"
   revision 1
   head "https://github.com/libtom/libtommath.git"
 
