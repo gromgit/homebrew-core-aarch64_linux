e1593b5ab75af65d4ebb4d97a0ed69b1a6ac0461
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:36 -0400

    cgvg: add license

--- Formula/cgvg.rb
+++ Formula/cgvg.rb
@@ -3,6 +3,7 @@ class Cgvg < Formula
   homepage "http://www.uzix.org/cgvg.html"
   url "http://www.uzix.org/cgvg/cgvg-1.6.3.tar.gz"
   sha256 "d879f541abcc988841a8d86f0c0781ded6e70498a63c9befdd52baf4649a12f3"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
