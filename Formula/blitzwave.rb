9eb4706d6e1e64549546278998e302ae6fc5cfcf
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:26 -0400

    blitzwave: add license

--- Formula/blitzwave.rb
+++ Formula/blitzwave.rb
@@ -3,6 +3,7 @@ class Blitzwave < Formula
   homepage "https://oschulz.github.io/blitzwave"
   url "https://github.com/oschulz/blitzwave/archive/v0.8.0.tar.gz"
   sha256 "edb0b708a0587e77b8e0aa3387b44f4e838855c17e896a8277bb80fbe79b9a63"
+  license "GPL-2.0"
 
   bottle do
     cellar :any
