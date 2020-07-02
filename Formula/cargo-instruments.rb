7c212908bf918c119e485686ea1f657c061c3a0e
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:32 -0400

    cargo-instruments: add license

--- Formula/cargo-instruments.rb
+++ Formula/cargo-instruments.rb
@@ -3,6 +3,7 @@ class CargoInstruments < Formula
   homepage "https://github.com/cmyr/cargo-instruments"
   url "https://github.com/cmyr/cargo-instruments/archive/v0.3.1.tar.gz"
   sha256 "55a71300045a689b9a416a4367b0c1f6823c5bc837156e774bfd52eba105726f"
+  license "MIT"
 
   bottle do
     cellar :any
