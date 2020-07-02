2ec5eb3a8f10828dfcfa1d3af87c7edaebe4b305
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:07 -0400

    usbredir: add license

--- Formula/usbredir.rb
+++ Formula/usbredir.rb
@@ -3,6 +3,7 @@ class Usbredir < Formula
   homepage "https://www.spice-space.org"
   url "https://www.spice-space.org/download/usbredir/usbredir-0.8.0.tar.bz2"
   sha256 "87bc9c5a81c982517a1bec70dc8d22e15ae197847643d58f20c0ced3c38c5e00"
+  license "GPL-2.0"
 
   bottle do
     cellar :any
