df400f85a3292bc519bbe0a5fadebf330aa18922
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:12 -0400

    pinfo: add license

--- Formula/pinfo.rb
+++ Formula/pinfo.rb
@@ -3,6 +3,7 @@ class Pinfo < Formula
   homepage "https://packages.debian.org/sid/pinfo"
   url "https://github.com/baszoetekouw/pinfo/archive/v0.6.13.tar.gz"
   sha256 "9dc5e848a7a86cb665a885bc5f0fdf6d09ad60e814d75e78019ae3accb42c217"
+  license "GPL-2.0"
   revision 1
 
   bottle do
