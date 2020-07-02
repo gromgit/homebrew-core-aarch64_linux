81540c0d72dab9123f38c0701b5b6cbbff3682fb
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:52 -0400

    htmldoc: add license

--- Formula/htmldoc.rb
+++ Formula/htmldoc.rb
@@ -3,6 +3,7 @@ class Htmldoc < Formula
   homepage "https://www.msweet.org/htmldoc/"
   url "https://github.com/michaelrsweet/htmldoc/archive/v1.9.9.tar.gz"
   sha256 "59c699f7ae9f6e9a291e28870f93a824cf5ecf7b3ea6634773cc5a7f9ccd1000"
+  license "GPL-2.0"
   head "https://github.com/michaelrsweet/htmldoc.git"
 
   bottle do
