4300984d9bd6cac22d5ffc742e6127496f0d9d5e
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:17 -0400

    libdvdcss: add license

--- Formula/libdvdcss.rb
+++ Formula/libdvdcss.rb
@@ -3,6 +3,7 @@ class Libdvdcss < Formula
   homepage "https://www.videolan.org/developers/libdvdcss.html"
   url "https://download.videolan.org/pub/videolan/libdvdcss/1.4.2/libdvdcss-1.4.2.tar.bz2"
   sha256 "78c2ed77ec9c0d8fbed7bf7d3abc82068b8864be494cfad165821377ff3f2575"
+  license "GPL-2.0"
 
   bottle do
     cellar :any
