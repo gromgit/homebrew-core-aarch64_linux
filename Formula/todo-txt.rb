47f22eb406b7015baa5505849291dad73dbf002b
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:00 -0400

    todo-txt: add license

--- Formula/todo-txt.rb
+++ Formula/todo-txt.rb
@@ -3,6 +3,7 @@ class TodoTxt < Formula
   homepage "http://todotxt.org/"
   url "https://github.com/todotxt/todo.txt-cli/releases/download/v2.11.0/todo.txt_cli-2.11.0.tar.gz"
   sha256 "5fe723bea3e3a1e697484cba92b46e90f13150ee1a444c4a9408c2cd28ea5255"
+  license "GPL-3.0"
   head "https://github.com/todotxt/todo.txt-cli.git"
 
   bottle :unneeded
