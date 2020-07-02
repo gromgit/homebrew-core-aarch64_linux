b9301f23321498dcc2e4a1c162545853e7532e13
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:14 -0400

    polipo: add license

--- Formula/polipo.rb
+++ Formula/polipo.rb
@@ -3,6 +3,7 @@ class Polipo < Formula
   homepage "https://www.irif.univ-paris-diderot.fr/~jch/software/polipo/"
   url "https://www.irif.univ-paris-diderot.fr/~jch/software/files/polipo/polipo-1.1.1.tar.gz"
   sha256 "a259750793ab79c491d05fcee5a917faf7d9030fb5d15e05b3704e9c9e4ee015"
+  license "MIT"
   head "https://github.com/jech/polipo.git"
 
   bottle do
