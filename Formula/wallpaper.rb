34c287e12b61b2d439486cd0328249b89653bd9a
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:12 -0400

    wallpaper: add license

--- Formula/wallpaper.rb
+++ Formula/wallpaper.rb
@@ -3,6 +3,7 @@ class Wallpaper < Formula
   homepage "https://github.com/sindresorhus/macos-wallpaper"
   url "https://github.com/sindresorhus/macos-wallpaper/archive/v2.1.0.tar.gz"
   sha256 "4925247524535c0cc128dcc4d87f5538a5ce3b5d3a3c211127fd646ee00252b6"
+  license "MIT"
   revision 1
   head "https://github.com/sindresorhus/macos-wallpaper.git"
 
