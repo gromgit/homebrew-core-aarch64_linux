06e6bf600a8ebb86b0c84282bfe46073991e4fdd
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:31 -0400

    libxdg-basedir: add license

--- Formula/libxdg-basedir.rb
+++ Formula/libxdg-basedir.rb
@@ -3,6 +3,7 @@ class LibxdgBasedir < Formula
   homepage "https://github.com/devnev/libxdg-basedir"
   url "https://github.com/devnev/libxdg-basedir/archive/libxdg-basedir-1.2.0.tar.gz"
   sha256 "1c2b0032a539033313b5be2e48ddd0ae94c84faf21d93956d53562eef4614868"
+  license "MIT"
 
   bottle do
     cellar :any
