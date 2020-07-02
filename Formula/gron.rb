325202487b87259703e07de17a8d73ad0747d851
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:44 -0400

    gron: add license

--- Formula/gron.rb
+++ Formula/gron.rb
@@ -5,6 +5,7 @@ class Gron < Formula
   homepage "https://github.com/tomnomnom/gron"
   url "https://github.com/tomnomnom/gron/archive/v0.6.0.tar.gz"
   sha256 "fe75b1b4922b591723f48cb9cd2c31cb60bb3ab9f8d0398df75a08b781d8591c"
+  license "MIT"
   head "https://github.com/tomnomnom/gron.git"
 
   bottle do
