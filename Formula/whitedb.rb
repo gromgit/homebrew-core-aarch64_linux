9f4bbf5d4212fc42cdc283a28c1077d21afc9a5c
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:15 -0400

    whitedb: add license

--- Formula/whitedb.rb
+++ Formula/whitedb.rb
@@ -3,6 +3,7 @@ class Whitedb < Formula
   homepage "https://www.whitedb.org/"
   url "https://www.whitedb.org/whitedb-0.7.3.tar.gz"
   sha256 "10c4ccd754ed2d53dbdef9ec16c88c732aa73d923fc0ee114e7e3a78a812849d"
+  license "GPL-3.0"
 
   bottle do
     cellar :any
