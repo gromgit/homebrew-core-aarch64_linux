f9e5ced5b6be9a4eec6cb614db08d58473c29bf5
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:47 -0400

    statik: add license

--- Formula/statik.rb
+++ Formula/statik.rb
@@ -5,6 +5,7 @@ class Statik < Formula
   homepage "https://getstatik.com"
   url "https://github.com/thanethomson/statik/archive/v0.23.0.tar.gz"
   sha256 "6159066f486811e5773da318d6e8d1b1dd4c99ac140f1a3c660ef1c1f5e7124f"
+  license "MIT"
   revision 1
   head "https://github.com/thanethomson/statik.git"
 
