94cc609855b87d51b0888cecd883754f43ee628f
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:25 -0400

    bison: add license

--- Formula/bison.rb
+++ Formula/bison.rb
@@ -5,6 +5,7 @@ class Bison < Formula
   url "https://ftp.gnu.org/gnu/bison/bison-3.6.4.tar.xz"
   mirror "https://ftpmirror.gnu.org/bison/bison-3.6.4.tar.xz"
   sha256 "8b13473b31ca7fcf65e5e8a74224368ffd5df19275602a9c9567ba393f18577d"
+  license "GPL-3.0"
   version_scheme 1
 
   bottle do
