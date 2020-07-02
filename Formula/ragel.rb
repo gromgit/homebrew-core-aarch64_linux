1ed414a6a138ef9f6f5cf7cbcd3202f8c3daaf89
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:23 -0400

    ragel: add license

--- Formula/ragel.rb
+++ Formula/ragel.rb
@@ -3,6 +3,7 @@ class Ragel < Formula
   homepage "https://www.colm.net/open-source/ragel/"
   url "https://www.colm.net/files/ragel/ragel-6.10.tar.gz"
   sha256 "5f156edb65d20b856d638dd9ee2dfb43285914d9aa2b6ec779dac0270cd56c3f"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
