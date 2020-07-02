7a2b77fe6f3d4a0357db60af3688df506a745b83
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:37 -0400

    shellshare: add license

--- Formula/shellshare.rb
+++ Formula/shellshare.rb
@@ -3,6 +3,7 @@ class Shellshare < Formula
   homepage "https://shellshare.net"
   url "https://github.com/vitorbaptista/shellshare/archive/v1.0.4.tar.gz"
   sha256 "55bfb1521f312b9b9b3e7bc10c9ab8e0c198278e6372187556a8d310ee6b602a"
+  license "Apache-2.0"
 
   bottle :unneeded
 
