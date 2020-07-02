ce9a15fd8393e65d95e885b9c59e6629d5790dff
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:01 -0400

    open-tyrian: add license

--- Formula/open-tyrian.rb
+++ Formula/open-tyrian.rb
@@ -3,6 +3,7 @@ class OpenTyrian < Formula
   homepage "https://github.com/opentyrian/opentyrian"
   url "https://www.camanis.net/opentyrian/releases/opentyrian-2.1.20130907-src.tar.gz"
   sha256 "f54b6b3cedcefa187c9f605d6164aae29ec46a731a6df30d351af4c008dee45f"
+  license "GPL-2.0"
   head "https://github.com/opentyrian/opentyrian.git"
 
   bottle do
