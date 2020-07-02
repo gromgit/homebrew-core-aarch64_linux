30905b42de21d32b226d1a06acddcf9c17ab91ed
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:11 -0400

    exif: add license

--- Formula/exif.rb
+++ Formula/exif.rb
@@ -3,6 +3,7 @@ class Exif < Formula
   homepage "https://libexif.sourceforge.io/"
   url "https://github.com/libexif/exif/releases/download/exif-0_6_22-release/exif-0.6.22.tar.xz"
   sha256 "0fe268736e0ca0538d4af941022761a438854a64c8024a4175e57bf0418117b9"
+  license "LGPL-2.1"
 
   bottle do
     cellar :any
