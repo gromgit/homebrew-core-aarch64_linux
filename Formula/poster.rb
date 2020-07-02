2a49151f4375cfe74e575993aa486c71b05a5b59
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:15 -0400

    poster: add license

--- Formula/poster.rb
+++ Formula/poster.rb
@@ -3,6 +3,7 @@ class Poster < Formula
   homepage "https://schrfr.github.io/poster/"
   url "https://github.com/schrfr/poster/archive/1.0.0.tar.gz"
   sha256 "1df49dfd4e50ffd66e0b6e279b454a76329a36280e0dc73b08e5b5dcd5cff451"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
