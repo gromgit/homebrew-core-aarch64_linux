9b9615be8f7077d91bde78aac625ed38dfb7a466
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:19 -0400

    pup: add license

--- Formula/pup.rb
+++ Formula/pup.rb
@@ -3,6 +3,7 @@ class Pup < Formula
   homepage "https://github.com/EricChiang/pup"
   url "https://github.com/ericchiang/pup/archive/v0.4.0.tar.gz"
   sha256 "0d546ab78588e07e1601007772d83795495aa329b19bd1c3cde589ddb1c538b0"
+  license "MIT"
   head "https://github.com/EricChiang/pup.git"
 
   bottle do
