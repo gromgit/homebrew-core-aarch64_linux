6d867143675578751dd4c1b5bc0ecfc4740a76ec
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:51 -0400

    cxxtest: add license

--- Formula/cxxtest.rb
+++ Formula/cxxtest.rb
@@ -6,6 +6,7 @@ class Cxxtest < Formula
   url "https://github.com/CxxTest/cxxtest/releases/download/4.4/cxxtest-4.4.tar.gz"
   mirror "https://deb.debian.org/debian/pool/main/c/cxxtest/cxxtest_4.4.orig.tar.gz"
   sha256 "1c154fef91c65dbf1cd4519af7ade70a61d85a923b6e0c0b007dc7f4895cf7d8"
+  license "LGPL-3.0"
   revision 1
 
   bottle do
