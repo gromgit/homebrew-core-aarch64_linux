fd07bd81106f466a005165d844cf89987307e9b7
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:58 -0400

    nuvie: add license

--- Formula/nuvie.rb
+++ Formula/nuvie.rb
@@ -3,6 +3,7 @@ class Nuvie < Formula
   homepage "https://nuvie.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/nuvie/Nuvie/0.5/nuvie-0.5.tgz"
   sha256 "ff026f6d569d006d9fe954f44fdf0c2276dbf129b0fc5c0d4ef8dce01f0fc257"
+  license "GPL-2.0"
 
   bottle do
     cellar :any
