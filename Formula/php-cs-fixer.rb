95994098863938cc66bdf9f8ece30317fafb818e
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:11 -0400

    php-cs-fixer: add license

--- Formula/php-cs-fixer.rb
+++ Formula/php-cs-fixer.rb
@@ -3,6 +3,7 @@ class PhpCsFixer < Formula
   homepage "https://cs.symfony.com/"
   url "https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v2.16.4/php-cs-fixer.phar"
   sha256 "286faddc6b4b21734adbee732d663b2e87779466a2c5b39096d020d6afdf2d17"
+  license "MIT"
 
   bottle :unneeded
 
