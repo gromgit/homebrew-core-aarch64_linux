7eba907814385a61cd2f592b94995feb62ff1e0f
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:11 -0400

    leaps: add license

--- Formula/leaps.rb
+++ Formula/leaps.rb
@@ -4,6 +4,7 @@ class Leaps < Formula
   url "https://github.com/Jeffail/leaps.git",
       :tag      => "v0.9.0",
       :revision => "89d8ab9e9130238e56a0df283edbcd1115ec9225"
+  license "MIT"
 
   bottle do
     cellar :any_skip_relocation
