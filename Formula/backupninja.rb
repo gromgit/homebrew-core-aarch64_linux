1dc7490878f1fe72902748874e8ab2f5119bfbfe
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:19 -0400

    backupninja: add license

--- Formula/backupninja.rb
+++ Formula/backupninja.rb
@@ -4,6 +4,7 @@ class Backupninja < Formula
   url "https://sourcearchive.raspbian.org/main/b/backupninja/backupninja_1.1.0.orig.tar.gz"
   mirror "https://debian.ethz.ch/ubuntu/ubuntu/pool/universe/b/backupninja/backupninja_1.1.0.orig.tar.gz"
   sha256 "abe444d0c7520ede7847b9497da4b1253a49579f59293b043c47b1dd9833280a"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
