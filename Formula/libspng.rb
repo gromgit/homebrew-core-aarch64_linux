323c076287f77feb78ac63bde16b51ac84b94ac1
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:27 -0400

    libspng: add license

--- Formula/libspng.rb
+++ Formula/libspng.rb
@@ -3,6 +3,7 @@ class Libspng < Formula
   homepage "https://libspng.org/"
   url "https://gitlab.com/randy408/libspng/uploads/3d980bac86c51368f40af2f1ac79a057/libspng-0.5.0.tar.xz"
   sha256 "220a653802559943ae43fd48f03ba6ff3935a5243766d9ee5ff905240d4399a7"
+  license "BSD-2-Clause"
 
   bottle do
     cellar :any
