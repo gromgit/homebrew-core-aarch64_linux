d783dbd2442c00ed0e663f28b2dace1f7478371c
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:19 -0400

    fprobe: add license

--- Formula/fprobe.rb
+++ Formula/fprobe.rb
@@ -3,6 +3,7 @@ class Fprobe < Formula
   homepage "https://sourceforge.net/projects/fprobe/"
   url "https://downloads.sourceforge.net/project/fprobe/fprobe/1.1/fprobe-1.1.tar.bz2"
   sha256 "3a1cedf5e7b0d36c648aa90914fa71a158c6743ecf74a38f4850afbac57d22a0"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
