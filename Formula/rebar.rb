0dff38a041854dfe273a9838af6d6ee1b0714cd4
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:26 -0400

    rebar: add license

--- Formula/rebar.rb
+++ Formula/rebar.rb
@@ -3,6 +3,7 @@ class Rebar < Formula
   homepage "https://github.com/rebar/rebar"
   url "https://github.com/rebar/rebar/archive/2.6.4.tar.gz"
   sha256 "577246bafa2eb2b2c3f1d0c157408650446884555bf87901508ce71d5cc0bd07"
+  license "Apache-2.0"
   head "https://github.com/rebar/rebar.git"
 
   bottle do
