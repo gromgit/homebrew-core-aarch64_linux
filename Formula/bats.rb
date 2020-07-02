f85ccb81d3bd012768e51e552fa24b026c9dee17
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:22 -0400

    bats: add license

--- Formula/bats.rb
+++ Formula/bats.rb
@@ -3,6 +3,7 @@ class Bats < Formula
   homepage "https://github.com/sstephenson/bats"
   url "https://github.com/sstephenson/bats/archive/v0.4.0.tar.gz"
   sha256 "480d8d64f1681eee78d1002527f3f06e1ac01e173b761bc73d0cf33f4dc1d8d7"
+  license "MIT"
   head "https://github.com/sstephenson/bats.git"
 
   bottle :unneeded
