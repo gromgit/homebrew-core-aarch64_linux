ebaf4354bd5ae1f7bfcef62edb305e85a5495f32
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:36 -0400

    serd: add license

--- Formula/serd.rb
+++ Formula/serd.rb
@@ -3,6 +3,7 @@ class Serd < Formula
   homepage "https://drobilla.net/software/serd/"
   url "https://download.drobilla.net/serd-0.30.4.tar.bz2"
   sha256 "0c95616a6587bee5e728e026190f4acd5ab6e2400e8890d5c2a93031eab01999"
+  license "ISC"
 
   bottle do
     cellar :any
