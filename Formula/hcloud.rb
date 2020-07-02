8206990eb5a7f1e78bb423366a404eea4423e0ec
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:48 -0400

    hcloud: add license

--- Formula/hcloud.rb
+++ Formula/hcloud.rb
@@ -3,6 +3,7 @@ class Hcloud < Formula
   homepage "https://github.com/hetznercloud/cli"
   url "https://github.com/hetznercloud/cli/archive/v1.17.0.tar.gz"
   sha256 "72710ebf4ce5598791d4e295dc749b7996433084dcd7523ced97912d321bdc4f"
+  license "MIT"
 
   bottle do
     cellar :any_skip_relocation
