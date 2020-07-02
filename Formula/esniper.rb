a5a73c05a464df84a2a422025330b3d9de0d0501
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:10 -0400

    esniper: add license

--- Formula/esniper.rb
+++ Formula/esniper.rb
@@ -4,6 +4,7 @@ class Esniper < Formula
   url "https://downloads.sourceforge.net/project/esniper/esniper/2.35.0/esniper-2-35-0.tgz"
   version "2.35.0"
   sha256 "a93d4533e31640554f2e430ac76b43e73a50ed6d721511066020712ac8923c12"
+  license "BSD-2-Clause"
 
   bottle do
     cellar :any_skip_relocation
