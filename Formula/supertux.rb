d2a13d6ed6869d357e1bae9c93544c585fdd71fb
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:49 -0400

    supertux: add license

--- Formula/supertux.rb
+++ Formula/supertux.rb
@@ -3,6 +3,7 @@ class Supertux < Formula
   homepage "https://www.supertux.org/"
   url "https://github.com/SuperTux/supertux/releases/download/v0.5.1/SuperTux-v0.5.1-Source.tar.gz"
   sha256 "c9dc3b42991ce5c5d0d0cb94e44c4ec2373ad09029940f0e92331e7e9ada0ac5"
+  license "GPL-3.0"
   revision 3
   head "https://github.com/SuperTux/supertux.git"
 
