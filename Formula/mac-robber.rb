895c5771a64eac3a8c158e0c0f66ab0fbf8de616
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:36 -0400

    mac-robber: add license

--- Formula/mac-robber.rb
+++ Formula/mac-robber.rb
@@ -3,6 +3,7 @@ class MacRobber < Formula
   homepage "https://www.sleuthkit.org/mac-robber/"
   url "https://downloads.sourceforge.net/project/mac-robber/mac-robber/1.02/mac-robber-1.02.tar.gz"
   sha256 "5895d332ec8d87e15f21441c61545b7f68830a2ee2c967d381773bd08504806d"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
