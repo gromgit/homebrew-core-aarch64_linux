e8b645a5010f4cd720f8c9ea4b7fd9c4e421b6e4
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:23 -0400

    ykpers: add license

--- Formula/ykpers.rb
+++ Formula/ykpers.rb
@@ -3,6 +3,7 @@ class Ykpers < Formula
   homepage "https://developers.yubico.com/yubikey-personalization/"
   url "https://developers.yubico.com/yubikey-personalization/Releases/ykpers-1.20.0.tar.gz"
   sha256 "0ec84d0ea862f45a7d85a1a3afe5e60b8da42df211bb7d27a50f486e31a79b93"
+  license "BSD-2-Clause"
 
   bottle do
     cellar :any
