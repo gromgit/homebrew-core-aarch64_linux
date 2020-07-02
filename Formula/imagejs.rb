d61c21928f042a6d71d7e2e7f6119cf18de483e4
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:56 -0400

    imagejs: add license

--- Formula/imagejs.rb
+++ Formula/imagejs.rb
@@ -3,6 +3,7 @@ class Imagejs < Formula
   homepage "https://jklmnn.de/imagejs/"
   url "https://github.com/jklmnn/imagejs/archive/0.7.2.tar.gz"
   sha256 "ba75c7ea549c4afbcb2a516565ba0b762b5fc38a03a48e5b94bec78bac7dab07"
+  license "GPL-3.0"
   head "https://github.com/jklmnn/imagejs.git"
 
   bottle do
