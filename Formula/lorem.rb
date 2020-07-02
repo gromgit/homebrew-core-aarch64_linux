eae93dc60f967b73600574cf6a8adb1789fd14de
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:34 -0400

    lorem: add license

--- Formula/lorem.rb
+++ Formula/lorem.rb
@@ -3,6 +3,7 @@ class Lorem < Formula
   homepage "https://github.com/per9000/lorem"
   url "https://github.com/per9000/lorem/archive/v0.8.0.tar.gz"
   sha256 "3eec439d616a044e61a6733730b1fc009972466f869dae358991f95abd57e8b3"
+  license "GPL-3.0"
   head "https://github.com/per9000/lorem.git"
 
   bottle :unneeded
