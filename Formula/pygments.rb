d349541a82dd819ff244211b9fe9c92fe4392442
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:20 -0400

    pygments: add license

--- Formula/pygments.rb
+++ Formula/pygments.rb
@@ -5,6 +5,7 @@ class Pygments < Formula
   homepage "https://pygments.org/"
   url "https://files.pythonhosted.org/packages/6e/4d/4d2fe93a35dfba417311a4ff627489a947b01dc0cc377a3673c00cf7e4b2/Pygments-2.6.1.tar.gz"
   sha256 "647344a061c249a3b74e230c739f434d7ea4d8b1d5f3721bc0f3558049b38f44"
+  license "BSD-2-Clause"
 
   head "https://github.com/pygments/pygments.git"
 
