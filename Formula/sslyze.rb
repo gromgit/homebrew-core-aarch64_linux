2d3d1c9a799a504ae3fbe629cbd8c75c1973c741
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:47 -0400

    sslyze: add license

--- Formula/sslyze.rb
+++ Formula/sslyze.rb
@@ -3,6 +3,7 @@ class Sslyze < Formula
 
   desc "SSL scanner"
   homepage "https://github.com/nabla-c0d3/sslyze"
+  license "AGPL-3.0"
 
   stable do
     url "https://github.com/nabla-c0d3/sslyze/archive/3.0.8.tar.gz"
