c7b2885825ac44ed5a516fa270d06a7e717513f3
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:01 -0400

    jmxtrans: add license

--- Formula/jmxtrans.rb
+++ Formula/jmxtrans.rb
@@ -3,6 +3,7 @@ class Jmxtrans < Formula
   homepage "https://github.com/jmxtrans/jmxtrans"
   url "https://github.com/jmxtrans/jmxtrans/archive/jmxtrans-parent-271.tar.gz"
   sha256 "4aee400641eaeee7f33e1253043b1e644f8a9ec18f95ddc911ff8d35e2ca6530"
+  license "MIT"
   version_scheme 1
 
   bottle do
