dc00f5a64f8a01f7d3e528db3df7f58891c1982c
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:58 -0400

    ditaa: add license

--- Formula/ditaa.rb
+++ Formula/ditaa.rb
@@ -3,6 +3,7 @@ class Ditaa < Formula
   homepage "https://ditaa.sourceforge.io/"
   url "https://github.com/stathissideris/ditaa/releases/download/v0.11.0/ditaa-0.11.0-standalone.jar"
   sha256 "9418aa63ff6d89c5d2318396f59836e120e75bea7a5930c4d34aa10fe7a196a9"
+  license "LGPL-3.0"
   revision 1
 
   bottle :unneeded
