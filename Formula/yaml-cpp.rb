738de92958531e4a1093a25164bb8942b2fa98aa
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:21 -0400

    yaml-cpp: add license

--- Formula/yaml-cpp.rb
+++ Formula/yaml-cpp.rb
@@ -3,6 +3,7 @@ class YamlCpp < Formula
   homepage "https://github.com/jbeder/yaml-cpp"
   url "https://github.com/jbeder/yaml-cpp/archive/yaml-cpp-0.6.3.tar.gz"
   sha256 "77ea1b90b3718aa0c324207cb29418f5bced2354c2e483a9523d98c3460af1ed"
+  license "MIT"
   revision 1
 
   bottle do
