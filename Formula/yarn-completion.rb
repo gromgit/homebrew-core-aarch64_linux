e76bad35d184f44abebb04516b574ccf3eba46bb
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:22 -0400

    yarn-completion: add license

--- Formula/yarn-completion.rb
+++ Formula/yarn-completion.rb
@@ -3,6 +3,7 @@ class YarnCompletion < Formula
   homepage "https://github.com/dsifford/yarn-completion"
   url "https://github.com/dsifford/yarn-completion/archive/v0.8.0.tar.gz"
   sha256 "f359dbfd4d9dd28e231e81da16f308e73662fd586546672569a0eac994f2bcd6"
+  license "MIT"
 
   bottle :unneeded
 
