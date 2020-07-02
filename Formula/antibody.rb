68f2985744154b2323c3209034932f7af63aba9e
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:11 -0400

    antibody: add license

--- Formula/antibody.rb
+++ Formula/antibody.rb
@@ -3,6 +3,7 @@ class Antibody < Formula
   homepage "https://getantibody.github.io/"
   url "https://github.com/getantibody/antibody/archive/v6.0.1.tar.gz"
   sha256 "dad02a91cbf5715209ca2958dfeb29127f674a00615f80254efc87c33930dbe0"
+  license "MIT"
 
   bottle do
     cellar :any_skip_relocation
