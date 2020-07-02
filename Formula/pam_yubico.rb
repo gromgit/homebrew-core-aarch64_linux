b2d81cb3f5dd64d31cb950fb616fe56bcdc702ae
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:06 -0400

    pam_yubico: add license

--- Formula/pam_yubico.rb
+++ Formula/pam_yubico.rb
@@ -3,6 +3,7 @@ class PamYubico < Formula
   homepage "https://developers.yubico.com/yubico-pam/"
   url "https://developers.yubico.com/yubico-pam/Releases/pam_yubico-2.26.tar.gz"
   sha256 "2de96495963fefd72b98243952ca5d5ec513e702c596e54bc667ef6b5e252966"
+  license "BSD-2-Clause"
 
   bottle do
     cellar :any
