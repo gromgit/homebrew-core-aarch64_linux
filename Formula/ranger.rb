71e364046707a270feb62ab040687f2e4397428a
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:24 -0400

    ranger: add license

--- Formula/ranger.rb
+++ Formula/ranger.rb
@@ -3,6 +3,7 @@ class Ranger < Formula
   homepage "https://ranger.github.io"
   url "https://ranger.github.io/ranger-1.9.3.tar.gz"
   sha256 "ce088a04c91c25263a9675dc5c43514b7ec1b38c8ea43d9a9d00923ff6cdd251"
+  license "GPL-3.0"
   head "https://github.com/ranger/ranger.git"
 
   bottle :unneeded
