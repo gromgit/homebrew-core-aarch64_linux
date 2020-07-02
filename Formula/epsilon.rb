38b2e636630b5140f2edb3ce0757c505cbd22722
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:09 -0400

    epsilon: add license

--- Formula/epsilon.rb
+++ Formula/epsilon.rb
@@ -3,6 +3,7 @@ class Epsilon < Formula
   homepage "https://epsilon-project.sourceforge.io"
   url "https://downloads.sourceforge.net/project/epsilon-project/epsilon/0.9.2/epsilon-0.9.2.tar.gz"
   sha256 "5421a15969d4d7af0ac0a11d519ba8d1d2147dc28d8c062bf0c52f3a0d4c54c4"
+  license "GPL-3.0"
 
   bottle do
     cellar :any
