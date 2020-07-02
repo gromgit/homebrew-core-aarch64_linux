b3ab7b0573d61b2d8da4e3ec6e9462ff877e0704
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:11 -0400

    exa: add license

--- Formula/exa.rb
+++ Formula/exa.rb
@@ -3,6 +3,7 @@ class Exa < Formula
   homepage "https://the.exa.website"
   url "https://github.com/ogham/exa/archive/v0.9.0.tar.gz"
   sha256 "96e743ffac0512a278de9ca3277183536ee8b691a46ff200ec27e28108fef783"
+  license "MIT"
   head "https://github.com/ogham/exa.git"
 
   bottle do
