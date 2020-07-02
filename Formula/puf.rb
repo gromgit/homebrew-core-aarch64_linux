ed9bdb43bcac4c2bc9667a72e034852d5c16ed06
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:18 -0400

    puf: add license

--- Formula/puf.rb
+++ Formula/puf.rb
@@ -3,6 +3,7 @@ class Puf < Formula
   homepage "https://puf.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/puf/puf/1.0.0/puf-1.0.0.tar.gz"
   sha256 "3f1602057dc47debeb54effc2db9eadcffae266834389bdbf5ab14fc611eeaf0"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
