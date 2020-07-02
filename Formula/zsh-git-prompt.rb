8f7f608fbb8889efd2e282390814f442d3201e2b
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:25 -0400

    zsh-git-prompt: add license

--- Formula/zsh-git-prompt.rb
+++ Formula/zsh-git-prompt.rb
@@ -3,6 +3,7 @@ class ZshGitPrompt < Formula
   homepage "https://github.com/olivierverdier/zsh-git-prompt"
   url "https://github.com/olivierverdier/zsh-git-prompt/archive/v0.5.tar.gz"
   sha256 "87e5a908369f402e975426ffd61a8800f1c04c0a293f1d4015a6fb1f4408e77d"
+  license "MIT"
 
   bottle :unneeded
 
