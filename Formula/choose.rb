3f5bd23e6bac7a8f9f1f50d1fa681f2f1b29d780
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:38 -0400

    choose: add license

--- Formula/choose.rb
+++ Formula/choose.rb
@@ -3,6 +3,7 @@ class Choose < Formula
   homepage "https://github.com/geier/choose"
   url "https://github.com/geier/choose/archive/v0.1.0.tar.gz"
   sha256 "d09a679920480e66bff36c76dd4d33e8ad739a53eace505d01051c114a829633"
+  license "MIT"
   revision 2
   head "https://github.com/geier/choose.git"
 
