e44ecd3579cedb772a68bc4aeab4709d1aace45d
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:13 -0400

    watch-sim: add license

--- Formula/watch-sim.rb
+++ Formula/watch-sim.rb
@@ -3,6 +3,7 @@ class WatchSim < Formula
   homepage "https://github.com/alloy/watch-sim"
   url "https://github.com/alloy/watch-sim/archive/1.0.0.tar.gz"
   sha256 "138616472e980276999fee47072a24501ea53ce3f7095a3de940e683341b7cba"
+  license "MIT"
   head "https://github.com/alloy/watch-sim.git"
 
   bottle do
