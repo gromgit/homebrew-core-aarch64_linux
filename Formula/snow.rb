4536abf962d912ccda093c4e948df11058628f10
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:42 -0400

    snow: add license

--- Formula/snow.rb
+++ Formula/snow.rb
@@ -5,6 +5,7 @@ class Snow < Formula
   # Consistently returns "HTTP/1.1 406 Not Acceptable".
   url "https://dl.bintray.com/homebrew/mirror/snow-20130616.tar.gz"
   sha256 "c0b71aa74ed628d121f81b1cd4ae07c2842c41cfbdf639b50291fc527c213865"
+  license "Apache-2.0"
 
   bottle do
     cellar :any_skip_relocation
