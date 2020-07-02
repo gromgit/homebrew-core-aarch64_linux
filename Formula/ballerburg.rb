82862016cc2c8cb421691c3b0f886a36001437d2
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:20 -0400

    ballerburg: add license

--- Formula/ballerburg.rb
+++ Formula/ballerburg.rb
@@ -3,6 +3,7 @@ class Ballerburg < Formula
   homepage "https://baller.tuxfamily.org/"
   url "https://download.tuxfamily.org/baller/ballerburg-1.2.0.tar.gz"
   sha256 "0625f4b213c1180f2cb2179ef2bc6ce35c7e99db2b27306a8690c389ceac6300"
+  license "GPL-3.0"
   head "https://git.tuxfamily.org/baller/baller.git"
 
   bottle do
