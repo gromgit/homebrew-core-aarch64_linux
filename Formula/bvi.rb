34599a7b41dfb4c3e5fc63cf92025d9bdb099a85
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:30 -0400

    bvi: add license

--- Formula/bvi.rb
+++ Formula/bvi.rb
@@ -3,6 +3,7 @@ class Bvi < Formula
   homepage "https://bvi.sourceforge.io"
   url "https://downloads.sourceforge.net/bvi/bvi-1.4.1.src.tar.gz"
   sha256 "3035255ca79e0464567d255baa5544f7794e2b7eb791dcc60cc339cf1aa01e28"
+  license "GPL-3.0"
 
   bottle do
     sha256 "83cfa7a1fe8848d8eab7f01da94a32e75eb7c57221854a3f3f06a05417975977" => :catalina
