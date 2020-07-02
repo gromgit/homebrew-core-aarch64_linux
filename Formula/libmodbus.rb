87b0ef31e4e0ef67d5cb98cf5ccda9c7f90fb303
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:22 -0400

    libmodbus: add license

--- Formula/libmodbus.rb
+++ Formula/libmodbus.rb
@@ -3,6 +3,7 @@ class Libmodbus < Formula
   homepage "https://libmodbus.org/"
   url "https://libmodbus.org/releases/libmodbus-3.1.6.tar.gz"
   sha256 "d7d9fa94a16edb094e5fdf5d87ae17a0dc3f3e3d687fead81835d9572cf87c16"
+  license "LGPL-2.1"
 
   bottle do
     cellar :any
