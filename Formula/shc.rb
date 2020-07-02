3571eb1fda0cae41b842e934a018f9b585a27afc
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:37 -0400

    shc: add license

--- Formula/shc.rb
+++ Formula/shc.rb
@@ -3,6 +3,7 @@ class Shc < Formula
   homepage "https://neurobin.github.io/shc"
   url "https://github.com/neurobin/shc/archive/4.0.3.tar.gz"
   sha256 "7d7fa6a9f5f53d607ab851d739ae3d3b99ca86e2cb1425a6cab9299f673aee16"
+  license "GPL-3.0"
   head "https://github.com/neurobin/shc.git"
 
   bottle do
