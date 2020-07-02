2841b403aff253d5ad91f273cb5d414919047653
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:50 -0400

    svgcleaner: add license

--- Formula/svgcleaner.rb
+++ Formula/svgcleaner.rb
@@ -3,6 +3,7 @@ class Svgcleaner < Formula
   homepage "https://github.com/RazrFalcon/svgcleaner"
   url "https://github.com/RazrFalcon/svgcleaner/archive/v0.9.5.tar.gz"
   sha256 "dcf8dbc8939699e2e82141cb86688b6cd09da8cae5e18232ef14085c2366290c"
+  license "GPL-2.0"
   head "https://github.com/RazrFalcon/svgcleaner.git"
 
   bottle do
