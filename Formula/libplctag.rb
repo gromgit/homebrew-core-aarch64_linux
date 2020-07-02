1ca88903ed8e598e77758b692ed0f2f7affaadbe
Rui Chen <rui@meetup.com>
2020-07-02 01:04:43 -0400

    libplctag 2.1.9
    Closes #57279.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libplctag.rb
+++ Formula/libplctag.rb
@@ -1,8 +1,8 @@
 class Libplctag < Formula
   desc "Portable and simple API for accessing AB PLC data over Ethernet"
   homepage "https://github.com/kyle-github/libplctag"
-  url "https://github.com/kyle-github/libplctag/archive/v2.1.6.tar.gz"
-  sha256 "b4d9d965080f795d98894c6fb615d8469ebdec8fc17dec503b5e546445bfaa03"
+  url "https://github.com/kyle-github/libplctag/archive/v2.1.9.tar.gz"
+  sha256 "39705326f4680bec20c0013858956b5a8dd558a2b4d81725f7849d59a99aeac7"
 
   bottle do
     cellar :any
