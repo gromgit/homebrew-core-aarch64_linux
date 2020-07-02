fd39a4fdd7f910cbb9a4bb68e5ea712c4cf3fa2d
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:05 -0400

    kobalt: add license

--- Formula/kobalt.rb
+++ Formula/kobalt.rb
@@ -3,6 +3,7 @@ class Kobalt < Formula
   homepage "https://beust.com/kobalt/"
   url "https://github.com/cbeust/kobalt/releases/download/1.0.129/kobalt-1.0.129.zip"
   sha256 "0b4db9992f18f3b04503aa09dbb690bb12761552b392e24d49b04cc77b8bff68"
+  license "Apache-2.0"
 
   bottle :unneeded
 
