635578df5c2513644c2928621b28de57abc58542
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:04 -0400

    tunnel: add license

--- Formula/tunnel.rb
+++ Formula/tunnel.rb
@@ -3,6 +3,7 @@ class Tunnel < Formula
   homepage "https://tunnel.labstack.com/docs"
   url "https://github.com/labstack/tunnel-client/archive/v0.5.15.tar.gz"
   sha256 "7a57451416b76dbf220e69c7dd3e4c33dc84758a41cdb9337a464338565e3e6e"
+  license "MIT"
 
   bottle do
     cellar :any_skip_relocation
