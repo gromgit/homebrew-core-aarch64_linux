f575363919fc8a88dbc90a4420a379b5ceaf2266
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:53 -0400

    neofetch: add license

--- Formula/neofetch.rb
+++ Formula/neofetch.rb
@@ -3,6 +3,7 @@ class Neofetch < Formula
   homepage "https://github.com/dylanaraps/neofetch"
   url "https://github.com/dylanaraps/neofetch/archive/7.0.0.tar.gz"
   sha256 "8c6bd217cf6d34fc1f3dcbb0e8b1137655bc13fbb21165273dbb2a7bce0d3130"
+  license "MIT"
   head "https://github.com/dylanaraps/neofetch.git"
 
   bottle do
