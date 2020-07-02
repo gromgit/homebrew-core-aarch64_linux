a305254355b995f381407b9ed594189ae25a8ee4
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:20 -0400

    fsh: add license

--- Formula/fsh.rb
+++ Formula/fsh.rb
@@ -3,6 +3,7 @@ class Fsh < Formula
   homepage "https://www.lysator.liu.se/fsh/"
   url "https://www.lysator.liu.se/fsh/fsh-1.2.tar.gz"
   sha256 "9600882648966272c264cf3f1c41c11c91e704f473af43d8d4e0ac5850298826"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
