5ea7bea877db908c9d110eaeb46e5eacd2c9ed83
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:15 -0400

    libchamplain: add license

--- Formula/libchamplain.rb
+++ Formula/libchamplain.rb
@@ -3,6 +3,7 @@ class Libchamplain < Formula
   homepage "https://wiki.gnome.org/Projects/libchamplain"
   url "https://download.gnome.org/sources/libchamplain/0.12/libchamplain-0.12.20.tar.xz"
   sha256 "0232b4bfcd130a1c5bda7b6aec266bf2d06e701e8093df1886f1e26bc1ba3066"
+  license "LGPL-2.1"
   revision 1
 
   bottle do
