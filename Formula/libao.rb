07ebd4c81e3bf27e64c73411cd1f6a1ae61e9194
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:13 -0400

    libao: add license

--- Formula/libao.rb
+++ Formula/libao.rb
@@ -3,6 +3,7 @@ class Libao < Formula
   homepage "https://www.xiph.org/ao/"
   url "https://github.com/xiph/libao/archive/1.2.2.tar.gz"
   sha256 "df8a6d0e238feeccb26a783e778716fb41a801536fe7b6fce068e313c0e2bf4d"
+  license "GPL-2.0"
   head "https://gitlab.xiph.org/xiph/libao.git"
 
   bottle do
