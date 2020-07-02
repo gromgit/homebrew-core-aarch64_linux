bf6a29ccce267415355a86083e99a2a57c663818
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:48 -0400

    hello: add license

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -3,6 +3,7 @@ class Hello < Formula
   homepage "https://www.gnu.org/software/hello/"
   url "https://ftp.gnu.org/gnu/hello/hello-2.10.tar.gz"
   sha256 "31e066137a962676e89f69d1b65382de95a7ef7d914b8cb956f41ea72e0f516b"
+  license "GPL-3.0"
 
   bottle do
     cellar :any_skip_relocation
