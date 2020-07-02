fa4f3663326a671890d275e5f865560180c53a17
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:57 -0400

    distcc: add license

--- Formula/distcc.rb
+++ Formula/distcc.rb
@@ -3,6 +3,7 @@ class Distcc < Formula
   homepage "https://github.com/distcc/distcc/"
   url "https://github.com/distcc/distcc/releases/download/v3.3.3/distcc-3.3.3.tar.gz"
   sha256 "bead25471d5a53ecfdf8f065a6fe48901c14d5008956c318c700e56bc87bf0bc"
+  license "GPL-2.0"
   revision 1
   head "https://github.com/distcc/distcc.git"
 
