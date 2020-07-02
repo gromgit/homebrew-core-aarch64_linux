97e3c3624eb7d411314cc6121aa645d527c108be
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:59 -0400

    iproute2mac: add license

--- Formula/iproute2mac.rb
+++ Formula/iproute2mac.rb
@@ -3,6 +3,7 @@ class Iproute2mac < Formula
   homepage "https://github.com/brona/iproute2mac"
   url "https://github.com/brona/iproute2mac/releases/download/v1.2.3/iproute2mac-1.2.3.tar.gz"
   sha256 "95ef8d4b0e32e4d3e3d975afa11d7aa0797d59b0f2c21b73a4e26d357bd6f93f"
+  license "MIT"
 
   bottle :unneeded
 
