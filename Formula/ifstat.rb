f8b40f9f1a64a26b4be1c654d4d57974f4640ee9
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:55 -0400

    ifstat: add license

--- Formula/ifstat.rb
+++ Formula/ifstat.rb
@@ -3,6 +3,7 @@ class Ifstat < Formula
   homepage "http://gael.roualland.free.fr/ifstat/"
   url "http://gael.roualland.free.fr/ifstat/ifstat-1.1.tar.gz"
   sha256 "8599063b7c398f9cfef7a9ec699659b25b1c14d2bc0f535aed05ce32b7d9f507"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
