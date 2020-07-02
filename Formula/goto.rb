fa985f1cc61a50d60fbaa0f84615c7b68641a3c9
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:40 -0400

    goto: add license

--- Formula/goto.rb
+++ Formula/goto.rb
@@ -3,6 +3,7 @@ class Goto < Formula
   homepage "https://github.com/iridakos/goto"
   url "https://github.com/iridakos/goto/archive/v2.0.0.tar.gz"
   sha256 "460fe3994455501b50b2f771f999ace77ade295122e90e959084047dbfb1f0dc"
+  license "MIT"
 
   bottle :unneeded
 
