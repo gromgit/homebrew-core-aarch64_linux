2dd6ed3cd5b143ee5af8fa4a6881cf16d969e750
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:11 -0400

    leakcanary-shark: add license

--- Formula/leakcanary-shark.rb
+++ Formula/leakcanary-shark.rb
@@ -3,6 +3,7 @@ class LeakcanaryShark < Formula
   homepage "https://square.github.io/leakcanary/shark/"
   url "https://github.com/square/leakcanary/releases/download/v2.4/shark-cli-2.4.zip"
   sha256 "5f9854868873ac6c63da5903082fe4bc3e08a8e46fe5de1335a2122d8e827a9f"
+  license "Apache-2.0"
 
   bottle :unneeded
 
