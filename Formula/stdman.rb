ae96102810d507e8f2328fae96f17af23e216350
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:47 -0400

    stdman: add license

--- Formula/stdman.rb
+++ Formula/stdman.rb
@@ -3,6 +3,7 @@ class Stdman < Formula
   homepage "https://github.com/jeaye/stdman"
   url "https://github.com/jeaye/stdman/archive/2018.03.11.tar.gz"
   sha256 "d29e6b34cb5ba9905360cee6adcdf8c49e7f11272521bf2e10b42917486840e8"
+  license "MIT"
   version_scheme 1
   head "https://github.com/jeaye/stdman.git"
 
