eec9692a7cc1dade6cbfdcf35015a1a751411eab
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:46 -0400

    ssh-audit: add license

--- Formula/ssh-audit.rb
+++ Formula/ssh-audit.rb
@@ -5,6 +5,7 @@ class SshAudit < Formula
   homepage "https://github.com/jtesta/ssh-audit"
   url "https://github.com/jtesta/ssh-audit/releases/download/v2.2.0/ssh-audit-2.2.0.tar.gz"
   sha256 "a8f4f01122234bd84c01440bfd3b7a6026c50c1a06f3044846a8503c94f94cfb"
+  license "MIT"
   revision 1
   head "https://github.com/jtesta/ssh-audit.git"
 
