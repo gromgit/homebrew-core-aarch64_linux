b624563e71b2c52f71f490fcd1700fc7c5a4fb73
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:59 -0400

    oggz: add license

--- Formula/oggz.rb
+++ Formula/oggz.rb
@@ -3,6 +3,7 @@ class Oggz < Formula
   homepage "https://www.xiph.org/oggz/"
   url "https://downloads.xiph.org/releases/liboggz/liboggz-1.1.1.tar.gz"
   sha256 "6bafadb1e0a9ae4ac83304f38621a5621b8e8e32927889e65a98706d213d415a"
+  license "BSD-3-Clause"
 
   bottle do
     cellar :any
