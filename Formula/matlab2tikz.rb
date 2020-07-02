9a3ed94f523857fd6c82f3b6ee46623fb2e20ef3
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:38 -0400

    matlab2tikz: add license

--- Formula/matlab2tikz.rb
+++ Formula/matlab2tikz.rb
@@ -3,6 +3,7 @@ class Matlab2tikz < Formula
   homepage "https://github.com/matlab2tikz/matlab2tikz"
   url "https://github.com/matlab2tikz/matlab2tikz/archive/v1.1.0.tar.gz"
   sha256 "4e6fe80ebe4c8729650eb00679f97398c2696fd9399c17f9c5b60a1a6cf23a19"
+  license "BSD-2-Clause"
   head "https://github.com/matlab2tikz/matlab2tikz.git"
 
   bottle :unneeded
