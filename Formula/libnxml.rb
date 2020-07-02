03772e1b664c28a88fb8a4175619c29858c0802b
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:23 -0400

    libnxml: add license

--- Formula/libnxml.rb
+++ Formula/libnxml.rb
@@ -3,6 +3,7 @@ class Libnxml < Formula
   homepage "https://www.autistici.org/bakunin/libnxml/"
   url "https://www.autistici.org/bakunin/libnxml/libnxml-0.18.3.tar.gz"
   sha256 "0f9460e3ba16b347001caf6843f0050f5482e36ebcb307f709259fd6575aa547"
+  license "LGPL-2.1"
 
   bottle do
     cellar :any
