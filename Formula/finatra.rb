dd8e2600b40710f012abf037ab89f44691a82baa
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:15 -0400

    finatra: add license

--- Formula/finatra.rb
+++ Formula/finatra.rb
@@ -3,6 +3,7 @@ class Finatra < Formula
   homepage "http://finatra.info/"
   url "https://github.com/twitter/finatra/archive/1.5.3.tar.gz"
   sha256 "aa4fab5ccdc012da9edf4650addf54b6ba64eb7e6a5e88d8c76e68e4d89216de"
+  license "Apache-2.0"
 
   bottle :unneeded
 
