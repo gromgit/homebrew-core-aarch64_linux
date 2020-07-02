3d89419a23f5534f7005c96de41c291050f228f0
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:50 -0400

    swagger-codegen: add license

--- Formula/swagger-codegen.rb
+++ Formula/swagger-codegen.rb
@@ -3,6 +3,7 @@ class SwaggerCodegen < Formula
   homepage "https://swagger.io/swagger-codegen/"
   url "https://github.com/swagger-api/swagger-codegen/archive/v3.0.20.tar.gz"
   sha256 "82de5438e233631026b945d2f25bd0a30c36d496daf42b696de27aa40dcd5dc1"
+  license "Apache-2.0"
   head "https://github.com/swagger-api/swagger-codegen.git"
 
   bottle do
