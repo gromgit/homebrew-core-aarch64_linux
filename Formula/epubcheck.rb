c70d356603528101258551014f4257336d50c7fb
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:09 -0400

    epubcheck: add license

--- Formula/epubcheck.rb
+++ Formula/epubcheck.rb
@@ -3,6 +3,7 @@ class Epubcheck < Formula
   homepage "https://github.com/w3c/epubcheck"
   url "https://github.com/w3c/epubcheck/releases/download/v4.2.4/epubcheck-4.2.4.zip"
   sha256 "11550a96f0695cb475026664f0015133e88d8a58f9f8b31180515f8e7ae2b7d2"
+  license "BSD-3-Clause"
 
   bottle :unneeded
 
