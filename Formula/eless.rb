fe51aefd92624f5cee09a3b851841f9799cfa463
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:07 -0400

    eless: add license

--- Formula/eless.rb
+++ Formula/eless.rb
@@ -3,6 +3,7 @@ class Eless < Formula
   homepage "https://eless.scripter.co/"
   url "https://github.com/kaushalmodi/eless/archive/v0.6.tar.gz"
   sha256 "a691a56da6d92f279e46c10d72d3ef6e4951f0e30092ca394622b6e94aae551b"
+  license "MIT"
 
   bottle :unneeded
 
