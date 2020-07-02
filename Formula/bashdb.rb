d0946b8c896a8efe0423328bbd4dc6cc1f933b17
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:21 -0400

    bashdb: add license

--- Formula/bashdb.rb
+++ Formula/bashdb.rb
@@ -4,6 +4,7 @@ class Bashdb < Formula
   url "https://downloads.sourceforge.net/project/bashdb/bashdb/5.0-1.1.2/bashdb-5.0-1.1.2.tar.bz2"
   version "5.0-1.1.2"
   sha256 "30176d2ad28c5b00b2e2d21c5ea1aef8fbaf40a8f9d9f723c67c60531f3b7330"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
