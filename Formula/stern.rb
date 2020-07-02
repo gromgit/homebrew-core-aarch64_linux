47641aa1f2fab352de3f2ba0a51b3600c5cc9ec8
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:47 -0400

    stern: add license

--- Formula/stern.rb
+++ Formula/stern.rb
@@ -3,6 +3,7 @@ class Stern < Formula
   homepage "https://github.com/wercker/stern"
   url "https://github.com/wercker/stern/archive/1.11.0.tar.gz"
   sha256 "d6f47d3a6f47680d3e4afebc8b01a14f0affcd8fb625132af14bb77843f0333f"
+  license "Apache-2.0"
   head "https://github.com/wercker/stern.git",
     :shallow => false
 
