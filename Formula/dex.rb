34381ad9bb54417d7296266b563820a9b4a13e0a
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:55 -0400

    dex: add license

--- Formula/dex.rb
+++ Formula/dex.rb
@@ -3,6 +3,7 @@ class Dex < Formula
   homepage "https://github.com/tihirvon/dex"
   url "https://github.com/tihirvon/dex/archive/v1.0.tar.gz"
   sha256 "4468b53debe8da6391186dccb78288a8a77798cb4c0a00fab9a7cdc711cd2123"
+  license "GPL-2.0"
   head "https://github.com/tihirvon/dex.git"
 
   bottle do
