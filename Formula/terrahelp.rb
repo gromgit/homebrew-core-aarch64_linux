a05b8c51a3a3dbed7c50fd36e6fa733f0768eb97
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:56 -0400

    terrahelp: add license

--- Formula/terrahelp.rb
+++ Formula/terrahelp.rb
@@ -3,6 +3,7 @@ class Terrahelp < Formula
   homepage "https://github.com/opencredo/terrahelp"
   url "https://github.com/opencredo/terrahelp/archive/v0.7.4.tar.gz"
   sha256 "2d70b6471bfb4b9c8ff3bb12050ecedca8d39830fa221bf8c319a1b6144ee6e5"
+  license "Apache-2.0"
   head "https://github.com/opencredo/terrahelp.git"
 
   bottle do
