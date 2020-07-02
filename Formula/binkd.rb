4481721b09295c39ed35992a93875e311e2071bf
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:24 -0400

    binkd: add license

--- Formula/binkd.rb
+++ Formula/binkd.rb
@@ -3,6 +3,7 @@ class Binkd < Formula
   homepage "http://binkd.grumbler.org/"
   url "https://happy.kiev.ua/pub/fidosoft/mailer/binkd/binkd-1.0.4.tar.gz"
   sha256 "917e45c379bbd1a140d1fe43179a591f1b2ec4004b236d6e0c4680be8f1a0dc0"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
