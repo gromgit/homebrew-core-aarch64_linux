53eacded59c9f48e1004b537e3b5b7a939e9d55d
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:13 -0400

    argus: add license

--- Formula/argus.rb
+++ Formula/argus.rb
@@ -3,6 +3,7 @@ class Argus < Formula
   homepage "https://qosient.com/argus/"
   url "https://qosient.com/argus/src/argus-3.0.8.2.tar.gz"
   sha256 "ca4e3bd5b9d4a8ff7c01cc96d1bffd46dbd6321237ec94c52f8badd51032eeff"
+  license "GPL-3.0"
 
   bottle do
     cellar :any_skip_relocation
