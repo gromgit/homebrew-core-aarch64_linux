8dc90012da5ace186de635fad566ebd630fd3718
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:13 -0400

    libagg: add license

--- Formula/libagg.rb
+++ Formula/libagg.rb
@@ -4,6 +4,7 @@ class Libagg < Formula
   # Canonical URL inaccessible: https://antigrain.com/agg-2.5.tar.gz
   url "https://ftp.osuosl.org/pub/blfs/8.0/a/agg-2.5.tar.gz"
   sha256 "ab1edc54cc32ba51a62ff120d501eecd55fceeedf869b9354e7e13812289911f"
+  license "GPL-2.0"
 
   bottle do
     cellar :any
