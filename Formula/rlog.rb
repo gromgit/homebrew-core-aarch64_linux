d8491123114a4ea0ed358b5333b0e836a2755043
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:29 -0400

    rlog: add license

--- Formula/rlog.rb
+++ Formula/rlog.rb
@@ -3,6 +3,7 @@ class Rlog < Formula
   homepage "https://www.arg0.net/rlog"
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/rlog/rlog-1.4.tar.gz"
   sha256 "a938eeedeb4d56f1343dc5561bc09ae70b24e8f70d07a6f8d4b6eed32e783f79"
+  license "LGPL-2.1"
 
   bottle do
     cellar :any
