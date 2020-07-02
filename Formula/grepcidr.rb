9a1f9016752e09b34924a0dbdb19b43d015270ed
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:43 -0400

    grepcidr: add license

--- Formula/grepcidr.rb
+++ Formula/grepcidr.rb
@@ -3,6 +3,7 @@ class Grepcidr < Formula
   homepage "http://www.pc-tools.net/unix/grepcidr/"
   url "http://www.pc-tools.net/files/unix/grepcidr-2.0.tar.gz"
   sha256 "61886a377dabf98797145c31f6ba95e6837b6786e70c932324b7d6176d50f7fb"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
