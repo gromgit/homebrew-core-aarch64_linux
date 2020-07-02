757a3808044da26d82dbc24d3fa0913e8187a0f3
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:53 -0400

    tcptunnel: add license

--- Formula/tcptunnel.rb
+++ Formula/tcptunnel.rb
@@ -3,6 +3,7 @@ class Tcptunnel < Formula
   homepage "http://www.vakuumverpackt.de/tcptunnel/"
   url "https://github.com/vakuum/tcptunnel/archive/v0.8.tar.gz"
   sha256 "1926e2636d26570035a5a0292c8d7766c4a9af939881121660df0d0d4513ade4"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
