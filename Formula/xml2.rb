891463a4d2563df84a7f63001894345e43e5a804
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:19 -0400

    xml2: add license

--- Formula/xml2.rb
+++ Formula/xml2.rb
@@ -3,6 +3,7 @@ class Xml2 < Formula
   homepage "https://web.archive.org/web/20160730094113/www.ofb.net/~egnor/xml2/"
   url "https://web.archive.org/web/20160427221603/download.ofb.net/gale/xml2-0.5.tar.gz"
   sha256 "e3203a5d3e5d4c634374e229acdbbe03fea41e8ccdef6a594a3ea50a50d29705"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
