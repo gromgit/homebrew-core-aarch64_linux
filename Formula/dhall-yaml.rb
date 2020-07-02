10086a017931fa05e0803553f7219ca2a0c2ba22
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:56 -0400

    dhall-yaml: add license

--- Formula/dhall-yaml.rb
+++ Formula/dhall-yaml.rb
@@ -3,6 +3,7 @@ class DhallYaml < Formula
   homepage "https://github.com/dhall-lang/dhall-haskell/tree/master/dhall-yaml"
   url "https://hackage.haskell.org/package/dhall-yaml-1.1.0/dhall-yaml-1.1.0.tar.gz"
   sha256 "72919ce641af46b6f2e8884ad358f545b58023682f65b2b3d1d1499974fc9c1a"
+  license "BSD-3-Clause"
   revision 1
   head "https://github.com/dhall-lang/dhall-haskell.git"
 
