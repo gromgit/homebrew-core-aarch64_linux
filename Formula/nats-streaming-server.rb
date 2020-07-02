36528bf1460ff35f6ee39fdccb5d548701983424
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:52 -0400

    nats-streaming-server: add license

--- Formula/nats-streaming-server.rb
+++ Formula/nats-streaming-server.rb
@@ -3,6 +3,7 @@ class NatsStreamingServer < Formula
   homepage "https://nats.io"
   url "https://github.com/nats-io/nats-streaming-server/archive/v0.18.0.tar.gz"
   sha256 "b1127d5317612178b0ba19468066ff3f59212a407fcd88401ffb6c6df9b68d1a"
+  license "Apache-2.0"
   head "https://github.com/nats-io/nats-streaming-server.git"
 
   bottle do
