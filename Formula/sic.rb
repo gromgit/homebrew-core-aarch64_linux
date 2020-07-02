846006c2e86640a7449d62de55651b19b119e55d
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:38 -0400

    sic: add license

--- Formula/sic.rb
+++ Formula/sic.rb
@@ -3,6 +3,7 @@ class Sic < Formula
   homepage "https://tools.suckless.org/sic/"
   url "https://dl.suckless.org/tools/sic-1.2.tar.gz"
   sha256 "ac07f905995e13ba2c43912d7a035fbbe78a628d7ba1c256f4ca1372fb565185"
+  license "MIT"
   head "https://git.suckless.org/sic", :using => :git
 
   bottle do
