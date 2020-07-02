566f5aece21f5dbde0f01c619785a67884447c26
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:35 -0400

    luarocks: add license

--- Formula/luarocks.rb
+++ Formula/luarocks.rb
@@ -3,6 +3,7 @@ class Luarocks < Formula
   homepage "https://luarocks.org/"
   url "https://luarocks.org/releases/luarocks-3.3.1.tar.gz"
   sha256 "eb20cd9814df05535d9aae98da532217c590fc07d48d90ca237e2a7cdcf284fe"
+  license "MIT"
   head "https://github.com/luarocks/luarocks.git"
 
   bottle do
