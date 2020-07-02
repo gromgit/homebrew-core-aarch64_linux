b55225e6bef8270d17e3114725206aab46c238a1
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:01 -0400

    jmxterm: add license

--- Formula/jmxterm.rb
+++ Formula/jmxterm.rb
@@ -3,6 +3,7 @@ class Jmxterm < Formula
   homepage "https://docs.cyclopsgroup.org/jmxterm"
   url "https://github.com/jiaqi/jmxterm/releases/download/v1.0.1/jmxterm-1.0.1-uber.jar"
   sha256 "76e0dae56b410c77724b561897e9073f088bd0b6158d668147d466debac6e9b0"
+  license "Apache-2.0"
 
   bottle :unneeded
 
