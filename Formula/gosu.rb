14ddbd4132ec1e0d59a089f9ee7fd92e166fbf8f
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:40 -0400

    gosu: add license

--- Formula/gosu.rb
+++ Formula/gosu.rb
@@ -3,6 +3,7 @@ class Gosu < Formula
   homepage "https://gosu-lang.github.io/"
   url "https://github.com/gosu-lang/gosu-lang/archive/v1.14.18.tar.gz"
   sha256 "b6bf6967ee127b727b4be56aaa02bc807340d4663e00dafd9fd8a6c1a5cd6958"
+  license "Apache-2.0"
   head "https://github.com/gosu-lang/gosu-lang.git"
 
   bottle do
