a07db20eef5b4cdd17486a081330ab24a2206edf
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:03 -0400

    jsonnet: add license

--- Formula/jsonnet.rb
+++ Formula/jsonnet.rb
@@ -3,6 +3,7 @@ class Jsonnet < Formula
   homepage "https://jsonnet.org/"
   url "https://github.com/google/jsonnet/archive/v0.16.0.tar.gz"
   sha256 "fa1a4047942797b7c4ed39718a20d63d1b98725fb5cf563efbc1ecca3375426f"
+  license "Apache-2.0"
   head "https://github.com/google/jsonnet.git"
 
   bottle do
