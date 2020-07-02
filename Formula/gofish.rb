5c03614e9ff620972c9cea1e384d0b210402d609
chenrui <chenrui333@gmail.com>
2020-07-02 16:54:35 +0000

    gofish: add 0.12.2 bottle.

--- Formula/gofish.rb
+++ Formula/gofish.rb
@@ -4,6 +4,13 @@ class Gofish < Formula
   url "https://github.com/fishworks/gofish.git",
       :tag      => "v0.12.2",
       :revision => "7ffd9b5ecf427284c425eb86830a53e427eda5f9"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bac35a400b653cf733b089e18eff0b99695c99d08f5dd1a0e199328b360de376" => :catalina
+    sha256 "8c9b0f7f5ba7d628cb52aa0b1ab01d1712fd206dce43ad5ac91296b8a3c51ca8" => :mojave
+    sha256 "60a95d0ee72b99643765e8f1f29d4551007d904f644a447df2ee1c8706c1b072" => :high_sierra
+  end
+
   license "Apache-2.0"
 
   depends_on "go" => :build
