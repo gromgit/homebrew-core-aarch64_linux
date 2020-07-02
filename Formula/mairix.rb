62454edba96a78eab5f75f37605876f2f7db70a6
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:37 -0400

    mairix: add license

--- Formula/mairix.rb
+++ Formula/mairix.rb
@@ -3,6 +3,7 @@ class Mairix < Formula
   homepage "http://www.rpcurnow.force9.co.uk/mairix/"
   url "https://downloads.sourceforge.net/project/mairix/mairix/0.24/mairix-0.24.tar.gz"
   sha256 "a0702e079c768b6fbe25687ebcbabe7965eb493d269a105998c7c1c2caef4a57"
+  license "GPL-2.0"
   head "https://github.com/rc0/mairix.git"
 
   bottle do
