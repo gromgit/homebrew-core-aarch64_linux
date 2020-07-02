fca6c8cd425db009a73f05278b291bd1f7c89f4a
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:22 -0400

    libnet: add license

--- Formula/libnet.rb
+++ Formula/libnet.rb
@@ -3,6 +3,7 @@ class Libnet < Formula
   homepage "https://github.com/sam-github/libnet"
   url "https://github.com/libnet/libnet/releases/download/v1.2/libnet-1.2.tar.gz"
   sha256 "caa4868157d9e5f32e9c7eac9461efeff30cb28357f7f6bf07e73933fb4edaa7"
+  license "BSD-2-Clause"
 
   bottle do
     cellar :any
