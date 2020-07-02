658b26c3d8738e934c8d34d72eb49688425b1114
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:38 -0400

    shmux: add license

--- Formula/shmux.rb
+++ Formula/shmux.rb
@@ -3,6 +3,7 @@ class Shmux < Formula
   homepage "https://github.com/shmux/shmux"
   url "https://github.com/shmux/shmux/archive/v1.0.3.tar.gz"
   sha256 "c9f8863e2550e23e633cf5fc7a9c4c52d287059f424ef78aba6ecd98390fb9ab"
+  license "BSD-3-Clause"
 
   bottle do
     cellar :any_skip_relocation
