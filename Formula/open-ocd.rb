38fa51790a0790a538e5546ddb58791f4c81d954
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:01 -0400

    open-ocd: add license

--- Formula/open-ocd.rb
+++ Formula/open-ocd.rb
@@ -3,6 +3,7 @@ class OpenOcd < Formula
   homepage "http://openocd.org/"
   url "https://downloads.sourceforge.net/project/openocd/openocd/0.10.0/openocd-0.10.0.tar.bz2"
   sha256 "7312e7d680752ac088b8b8f2b5ba3ff0d30e0a78139531847be4b75c101316ae"
+  license "GPL-2.0"
 
   bottle do
     rebuild 2
