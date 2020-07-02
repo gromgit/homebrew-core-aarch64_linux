0b4230997ab3f07955f4ef94b7da8c4d72cd591a
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:52 -0400

    tag: add license

--- Formula/tag.rb
+++ Formula/tag.rb
@@ -3,6 +3,7 @@ class Tag < Formula
   homepage "https://github.com/jdberry/tag/"
   url "https://github.com/jdberry/tag/archive/v0.10.tar.gz"
   sha256 "5ab057d3e3f0dbb5c3be3970ffd90f69af4cb6201c18c1cbaa23ef367e5b071e"
+  license "MIT"
   revision 1
   head "https://github.com/jdberry/tag.git"
 
