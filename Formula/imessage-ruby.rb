d422f6b67d27ee86bacd184ce1c101768be056a7
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:56 -0400

    imessage-ruby: add license

--- Formula/imessage-ruby.rb
+++ Formula/imessage-ruby.rb
@@ -3,6 +3,7 @@ class ImessageRuby < Formula
   homepage "https://github.com/linjunpop/imessage"
   url "https://github.com/linjunpop/imessage/archive/v0.3.1.tar.gz"
   sha256 "74ccd560dec09dcf0de28cd04fc4d512812c3348fc5618cbb73b6b36c43e14ef"
+  license "MIT"
   head "https://github.com/linjunpop/imessage.git"
 
   bottle do
