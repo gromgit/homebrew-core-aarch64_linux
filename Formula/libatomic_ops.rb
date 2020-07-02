d2f04e7d2397051699018ef2d8eae6f9b7c70c35
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:13 -0400

    libatomic_ops: add license

--- Formula/libatomic_ops.rb
+++ Formula/libatomic_ops.rb
@@ -3,6 +3,7 @@ class LibatomicOps < Formula
   homepage "https://github.com/ivmai/libatomic_ops/"
   url "https://github.com/ivmai/libatomic_ops/releases/download/v7.6.10/libatomic_ops-7.6.10.tar.gz"
   sha256 "587edf60817f56daf1e1ab38a4b3c729b8e846ff67b4f62a6157183708f099af"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
