1017cde0452de9a36ac41997cc3b7dbe2b7074c1
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:13 -0400

    libassuan: add license

--- Formula/libassuan.rb
+++ Formula/libassuan.rb
@@ -4,6 +4,7 @@ class Libassuan < Formula
   url "https://gnupg.org/ftp/gcrypt/libassuan/libassuan-2.5.3.tar.bz2"
   mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/libassuan/libassuan-2.5.3.tar.bz2"
   sha256 "91bcb0403866b4e7c4bc1cc52ed4c364a9b5414b3994f718c70303f7f765e702"
+  license "GPL-3.0"
 
   bottle do
     cellar :any
