8cbe7abb5661dbbbfc37f039ab25db186580b001
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:42 -0400

    gradle-completion: add license

--- Formula/gradle-completion.rb
+++ Formula/gradle-completion.rb
@@ -3,6 +3,7 @@ class GradleCompletion < Formula
   homepage "https://gradle.org/"
   url "https://github.com/gradle/gradle-completion/archive/v1.4.1.tar.gz"
   sha256 "5d77f0c739fe983cfa86078a615f43be9be0e3ce05a3a7b70cb813a1ebd1ceef"
+  license "MIT"
   head "https://github.com/gradle/gradle-completion.git"
 
   bottle :unneeded
