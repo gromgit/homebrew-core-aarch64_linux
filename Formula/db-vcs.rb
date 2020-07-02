9ebb41ce8674492a47c80a0ff124d6e2d64a6dc9
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:53 -0400

    db-vcs: add license

--- Formula/db-vcs.rb
+++ Formula/db-vcs.rb
@@ -3,6 +3,7 @@ class DbVcs < Formula
   homepage "https://github.com/infostreams/db"
   url "https://github.com/infostreams/db/archive/1.1.tar.gz"
   sha256 "90f07c13c388896ba02032544820f8ff3a23e6f9dc1e320a1a653dd77e032ee7"
+  license "MIT"
 
   bottle :unneeded
 
