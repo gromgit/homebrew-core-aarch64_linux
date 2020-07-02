67462c9e5381171b14c8575a5ce3d251679a2e6f
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:47 -0400

    hackrf: add license

--- Formula/hackrf.rb
+++ Formula/hackrf.rb
@@ -3,6 +3,7 @@ class Hackrf < Formula
   homepage "https://github.com/mossmann/hackrf"
   url "https://github.com/mossmann/hackrf/archive/v2018.01.1.tar.gz"
   sha256 "84dbb5536d3aa5bd6b25d50df78d591e6c3431d752de051a17f4cb87b7963ec3"
+  license "GPL-2.0"
   head "https://github.com/mossmann/hackrf.git"
 
   bottle do
