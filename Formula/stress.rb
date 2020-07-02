aa9479e27ab70f725faa8911dda9269c6b81b3d5
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:49 -0400

    stress: add license

--- Formula/stress.rb
+++ Formula/stress.rb
@@ -4,6 +4,7 @@ class Stress < Formula
   url "https://people.seas.harvard.edu/~apw/stress/stress-1.0.4.tar.gz"
   mirror "https://deb.debian.org/debian/pool/main/s/stress/stress_1.0.4.orig.tar.gz"
   sha256 "057e4fc2a7706411e1014bf172e4f94b63a12f18412378fca8684ca92408825b"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
