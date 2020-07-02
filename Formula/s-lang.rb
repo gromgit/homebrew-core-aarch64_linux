293df2f52e2956952ce91a3862759193d1929e92
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:32 -0400

    s-lang: add license

--- Formula/s-lang.rb
+++ Formula/s-lang.rb
@@ -4,6 +4,7 @@ class SLang < Formula
   url "https://www.jedsoft.org/releases/slang/slang-2.3.2.tar.bz2"
   mirror "https://src.fedoraproject.org/repo/pkgs/slang/slang-2.3.2.tar.bz2/sha512/35cdfe8af66dac62ee89cca60fa87ddbd02cae63b30d5c0e3786e77b1893c45697ace4ac7e82d9832b8a9ac342560bc35997674846c5022341481013e76f74b5/slang-2.3.2.tar.bz2"
   sha256 "fc9e3b0fc4f67c3c1f6d43c90c16a5c42d117b8e28457c5b46831b8b5d3ae31a"
+  license "GPL-2.0"
 
   bottle do
     sha256 "4753499f91b8d6ad4f17865ba850eb5f170aff6460441655c75838b759b2ff9d" => :catalina
