74eecc3bcf44f275f08021d5782a0d164c3a1c10
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:21 -0400

    bash-completion@2: add license

--- Formula/bash-completion@2.rb
+++ Formula/bash-completion@2.rb
@@ -3,6 +3,7 @@ class BashCompletionAT2 < Formula
   homepage "https://github.com/scop/bash-completion"
   url "https://github.com/scop/bash-completion/releases/download/2.10/bash-completion-2.10.tar.xz"
   sha256 "123c17998e34b937ce57bb1b111cd817bc369309e9a8047c0bcf06ead4a3ec92"
+  license "GPL-2.0"
   revision 1
 
   bottle do
