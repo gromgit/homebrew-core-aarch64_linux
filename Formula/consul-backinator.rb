b8b5229b41efb20411795f72bb5ddc6a831aae52
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:45 -0400

    consul-backinator: add license

--- Formula/consul-backinator.rb
+++ Formula/consul-backinator.rb
@@ -3,6 +3,7 @@ class ConsulBackinator < Formula
   homepage "https://github.com/myENA/consul-backinator"
   url "https://github.com/myENA/consul-backinator/archive/v1.6.5.tar.gz"
   sha256 "e464d597a3a28c6376e0d602c9484a465476db13684585bd52c6b5d81b07019d"
+  license "MPL-2.0"
   head "https://github.com/myENA/consul-backinator.git"
 
   bottle do
