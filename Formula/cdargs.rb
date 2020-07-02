c321b3f1ab3c83b9d3928fe6482369ef927aa746
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:34 -0400

    cdargs: add license

--- Formula/cdargs.rb
+++ Formula/cdargs.rb
@@ -3,6 +3,7 @@ class Cdargs < Formula
   homepage "https://github.com/cbxbiker61/cdargs"
   url "https://github.com/cbxbiker61/cdargs/archive/2.1.tar.gz"
   sha256 "062515c3fbd28c68f9fa54ff6a44b81cf647469592444af0872b5ecd7444df7d"
+  license "GPL-2.0"
   head "https://github.com/cbxbiker61/cdargs.git"
 
   bottle do
