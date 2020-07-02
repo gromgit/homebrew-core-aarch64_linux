f019ebb940e59d881882427f0b4a47949e6065b8
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:36 -0400

    cgoban: add license

--- Formula/cgoban.rb
+++ Formula/cgoban.rb
@@ -3,6 +3,7 @@ class Cgoban < Formula
   homepage "https://cgoban1.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/cgoban1/cgoban1/1.9.14/cgoban-1.9.14.tar.gz"
   sha256 "3b8a6fc0e989bf977fcd9a65a367aa18e34c6e25800e78dd8f0063fa549c9b62"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
