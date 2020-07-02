dc0772e3306fc3cf4d44cc9ae134d0c9fa468779
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:25 -0400

    binwalk: add license

--- Formula/binwalk.rb
+++ Formula/binwalk.rb
@@ -5,6 +5,7 @@ class Binwalk < Formula
   homepage "https://github.com/ReFirmLabs/binwalk"
   url "https://github.com/ReFirmLabs/binwalk/archive/v2.2.0.tar.gz"
   sha256 "f5495f0e4c5575023d593f7c087c367675df6aeb7f4d9a2966e49763924daa27"
+  license "MIT"
   revision 2
   head "https://github.com/ReFirmLabs/binwalk.git"
 
