cbe805f7f15ee3a50301cfb4aa0d2320d8507fd2
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:30 -0400

    rofs-filtered: add license

--- Formula/rofs-filtered.rb
+++ Formula/rofs-filtered.rb
@@ -3,6 +3,7 @@ class RofsFiltered < Formula
   homepage "https://github.com/gburca/rofs-filtered/"
   url "https://github.com/gburca/rofs-filtered/archive/rel-1.7.tar.gz"
   sha256 "d66066dfd0274a2fb7b71dd929445377dd23100b9fa43e3888dbe3fc7e8228e8"
+  license "GPL-2.0"
 
   bottle do
     cellar :any
