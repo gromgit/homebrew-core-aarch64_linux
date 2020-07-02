6b60c2e7443978beef4f14e70b82ea752f942f59
kommtzeitkonrad <42938101+kommtzeitkonrad@users.noreply.github.com>
2020-07-02 10:18:38 +0200

    mtoc 949.0.1
    Credits: @vit9696, @dakanji & @acidanthera research team
    
    See also: https://github.com/acidanthera/ocbuild/blob/master/mtocbuild.sh
    
    Closes #57285.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mtoc.rb
+++ Formula/mtoc.rb
@@ -1,8 +1,8 @@
 class Mtoc < Formula
   desc "Mach-O to PE/COFF binary converter"
-  homepage "https://opensource.apple.com/source/cctools/cctools-927.0.2/"
-  url "https://opensource.apple.com/tarballs/cctools/cctools-927.0.2.tar.gz"
-  sha256 "3d5d35e7120aac187de306019d4b53b3b08dd7ebf48764fcd5c8cefef40b3dbf"
+  homepage "https://opensource.apple.com/source/cctools/cctools-949.0.1/"
+  url "https://opensource.apple.com/tarballs/cctools/cctools-949.0.1.tar.gz"
+  sha256 "830485ac7c563cd55331f643952caab2f0690dfbd01e92eb432c45098b28a5d0"
 
   bottle do
     cellar :any_skip_relocation
@@ -13,12 +13,15 @@ class Mtoc < Formula
 
   depends_on "llvm" => :build
 
-  def install
-    # Conflicts with _structs.h in macOS 10.13 - 10.15 SDK
-    File.delete "include/mach/i386/_structs.h"
+  patch do
+    url "https://raw.githubusercontent.com/acidanthera/ocbuild/d3e57820ce85bc2ed4ce20cc25819e763c17c114/patches/mtoc-permissions.patch"
+    sha256 "0d20ee119368e30913936dfee51055a1055b96dde835f277099cb7bcd4a34daf"
+  end
 
-    system "make", "-C", "libstuff", "EFITOOLS=efitools"
+  def install
+    system "make", "LTO=", "EFITOOLS=efitools", "-C", "libstuff"
     system "make", "-C", "efitools"
+    system "strip", "-x", "efitools/mtoc.NEW"
 
     bin.install "efitools/mtoc.NEW" => "mtoc"
     man1.install "man/mtoc.1"
