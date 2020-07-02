247a3ef79900bac84fbb8bbed25ba5ae5599be7c
Bo Anderson <mail@boanderson.me>
2020-07-02 17:31:14 +0100

    r: separate test system call args (#57299)

--- Formula/r.rb
+++ Formula/r.rb
@@ -92,7 +92,7 @@ class R < Formula
     assert_equal "[1] 2", shell_output("#{bin}/Rscript -e 'print(1+1)'").chomp
     assert_equal ".dylib", shell_output("#{bin}/R CMD config DYLIB_EXT").chomp
 
-    system bin/"Rscript -e \'install.packages(\"gss\", \".\", \"https://cloud.r-project.org\")\'"
+    system bin/"Rscript", "-e", "install.packages('gss', '.', 'https://cloud.r-project.org')"
     assert_predicate testpath/"gss/libs/gss.so", :exist?,
                      "Failed to install gss package"
   end
