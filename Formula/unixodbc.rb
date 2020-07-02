b1d6bb2d2ca3ffd30ee216e5693098ec0e4ba923
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:06 -0400

    unixodbc: add license

--- Formula/unixodbc.rb
+++ Formula/unixodbc.rb
@@ -3,6 +3,7 @@ class Unixodbc < Formula
   homepage "http://www.unixodbc.org/"
   url "http://www.unixodbc.org/unixODBC-2.3.7.tar.gz"
   sha256 "45f169ba1f454a72b8fcbb82abd832630a3bf93baa84731cf2949f449e1e3e77"
+  license "LGPL-2.1"
 
   bottle do
     rebuild 1
