add730776984b45844255319916e8d705dad4eb2
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:35 -0400

    gnu-chess: add license

--- Formula/gnu-chess.rb
+++ Formula/gnu-chess.rb
@@ -4,6 +4,7 @@ class GnuChess < Formula
   url "https://ftp.gnu.org/gnu/chess/gnuchess-6.2.7.tar.gz"
   mirror "https://ftpmirror.gnu.org/chess/gnuchess-6.2.7.tar.gz"
   sha256 "e536675a61abe82e61b919f6b786755441d9fcd4c21e1c82fb9e5340dd229846"
+  license "GPL-3.0"
 
   bottle do
     sha256 "85423112485c7dbe474c99c93008b8a7a7a8c9a9737bbda3e372fde8674cbef1" => :catalina
