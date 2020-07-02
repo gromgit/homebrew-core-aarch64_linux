2d7fd431bbf8c700d2f0d869764a54e87e39cd29
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:47 -0400

    mpir: add license

--- Formula/mpir.rb
+++ Formula/mpir.rb
@@ -3,6 +3,7 @@ class Mpir < Formula
   homepage "http://mpir.org/"
   url "http://mpir.org/mpir-3.0.0.tar.bz2"
   sha256 "52f63459cf3f9478859de29e00357f004050ead70b45913f2c2269d9708675bb"
+  license "GPL-3.0"
 
   bottle do
     cellar :any
