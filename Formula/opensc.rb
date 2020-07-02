926236bfd0b97594ca1c9d0729a81f5d0e22927f
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:02 -0400

    opensc: add license

--- Formula/opensc.rb
+++ Formula/opensc.rb
@@ -3,6 +3,7 @@ class Opensc < Formula
   homepage "https://github.com/OpenSC/OpenSC/wiki"
   url "https://github.com/OpenSC/OpenSC/releases/download/0.20.0/opensc-0.20.0.tar.gz"
   sha256 "bbf4b4f4a44463645c90a525e820a8059b2f742a53b7b944f941de3c97ba4863"
+  license "LGPL-2.1"
   head "https://github.com/OpenSC/OpenSC.git"
 
   bottle do
