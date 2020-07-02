c683e2b67b311e2201acbe23d07d1f9c4e6d40e0
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:57 -0400

    texinfo: add license

--- Formula/texinfo.rb
+++ Formula/texinfo.rb
@@ -4,6 +4,7 @@ class Texinfo < Formula
   url "https://ftp.gnu.org/gnu/texinfo/texinfo-6.7.tar.xz"
   mirror "https://ftpmirror.gnu.org/texinfo/texinfo-6.7.tar.xz"
   sha256 "988403c1542d15ad044600b909997ba3079b10e03224c61188117f3676b02caa"
+  license "GPL-3.0"
 
   bottle do
     sha256 "0686381d97b0448c10d11eaba59722c029d17c8423c17ad524b76ec086790f44" => :catalina
