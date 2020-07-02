4185a0d60885c893254300e822b6a2cd9b51e8ab
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:27 -0400

    git-absorb: add license

--- Formula/git-absorb.rb
+++ Formula/git-absorb.rb
@@ -3,6 +3,7 @@ class GitAbsorb < Formula
   homepage "https://github.com/tummychow/git-absorb"
   url "https://github.com/tummychow/git-absorb/archive/0.6.3.tar.gz"
   sha256 "d62ba36150d1113ea9216b8dc8f1f749c97a468cb41b2d1cd5c019158915ca70"
+  license "BSD-3-Clause"
 
   bottle do
     cellar :any_skip_relocation
