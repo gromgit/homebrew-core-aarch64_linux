2fd111c5a5d5e1e154c881eb1efb2a2a711f9846
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:59 -0400

    tmux-mem-cpu-load: add license

--- Formula/tmux-mem-cpu-load.rb
+++ Formula/tmux-mem-cpu-load.rb
@@ -3,6 +3,7 @@ class TmuxMemCpuLoad < Formula
   homepage "https://github.com/thewtex/tmux-mem-cpu-load"
   url "https://github.com/thewtex/tmux-mem-cpu-load/archive/v3.4.0.tar.gz"
   sha256 "a773994e160812a964abc7fc4e8ec16b7d9833edb0a66e5c67f287c7c5949ecb"
+  license "Apache-2.0"
   head "https://github.com/thewtex/tmux-mem-cpu-load.git"
 
   bottle do
