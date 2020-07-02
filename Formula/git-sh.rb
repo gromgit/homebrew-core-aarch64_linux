a8f78b45d1700258995121e3ce257a0d344a36fc
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:30 -0400

    git-sh: add license

--- Formula/git-sh.rb
+++ Formula/git-sh.rb
@@ -3,6 +3,7 @@ class GitSh < Formula
   homepage "https://github.com/rtomayko/git-sh"
   url "https://github.com/rtomayko/git-sh/archive/1.3.tar.gz"
   sha256 "461848dfa52ea6dd6cd0a374c52404b632204dc637cde17c0532529107d52358"
+  license "GPL-2.0"
   head "https://github.com/rtomayko/git-sh.git"
 
   bottle do
