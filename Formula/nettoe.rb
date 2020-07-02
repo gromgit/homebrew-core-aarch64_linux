c8dd3ec0250a91a8c7226a0d9f2a975029195af0
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:54 -0400

    nettoe: add license

--- Formula/nettoe.rb
+++ Formula/nettoe.rb
@@ -3,6 +3,7 @@ class Nettoe < Formula
   homepage "https://nettoe.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/nettoe/nettoe/1.5.1/nettoe-1.5.1.tar.gz"
   sha256 "dbc2c08e7e0f7e60236954ee19a165a350ab3e0bcbbe085ecd687f39253881cb"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
