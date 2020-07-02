88654a4abad515220e850616f0d3274d43c1a064
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:29 -0400

    libunistring: add license

--- Formula/libunistring.rb
+++ Formula/libunistring.rb
@@ -4,6 +4,7 @@ class Libunistring < Formula
   url "https://ftp.gnu.org/gnu/libunistring/libunistring-0.9.10.tar.xz"
   mirror "https://ftpmirror.gnu.org/libunistring/libunistring-0.9.10.tar.xz"
   sha256 "eb8fb2c3e4b6e2d336608377050892b54c3c983b646c561836550863003c05d7"
+  license "GPL-3.0"
 
   bottle do
     cellar :any
