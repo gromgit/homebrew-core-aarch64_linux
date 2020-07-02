ee65959513055af91c12f3fd8545afc0736fd32b
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:16 -0400

    aurora: add license

--- Formula/aurora.rb
+++ Formula/aurora.rb
@@ -3,6 +3,7 @@ class Aurora < Formula
   homepage "https://xuri.me/aurora"
   url "https://github.com/xuri/aurora/archive/2.2.tar.gz"
   sha256 "90ac08b7c960aa24ee0c8e60759e398ef205f5b48c2293dd81d9c2f17b24ca42"
+  license "MIT"
 
   bottle do
     cellar :any_skip_relocation
