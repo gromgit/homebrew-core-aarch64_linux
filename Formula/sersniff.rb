07f1703ea189c4e111e2cab258e144e767cadf3f
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:36 -0400

    sersniff: add license

--- Formula/sersniff.rb
+++ Formula/sersniff.rb
@@ -3,6 +3,7 @@ class Sersniff < Formula
   homepage "https://www.earth.li/projectpurple/progs/sersniff.html"
   url "https://www.earth.li/projectpurple/files/sersniff-0.0.5.tar.gz"
   sha256 "8aa93f3b81030bcc6ff3935a48c1fd58baab8f964b1d5e24f0aaecbd78347209"
+  license "GPL-2.0"
   head "https://the.earth.li/git/sersniff.git"
 
   bottle do
