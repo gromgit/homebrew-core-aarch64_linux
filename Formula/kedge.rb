47492b5a64597bc0f69c3ffc761eb10e928c242a
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:04 -0400

    kedge: add license

--- Formula/kedge.rb
+++ Formula/kedge.rb
@@ -3,6 +3,7 @@ class Kedge < Formula
   homepage "https://github.com/kedgeproject/kedge"
   url "https://github.com/kedgeproject/kedge/archive/v0.12.0.tar.gz"
   sha256 "3c01880ba9233fe5b0715527ba32f0c59b25b73284de8cfb49914666a158487b"
+  license "Apache-2.0"
 
   bottle do
     cellar :any_skip_relocation
