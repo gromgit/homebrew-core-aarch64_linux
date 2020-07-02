a6b0d5f9aa50ed0a938a6ece33f2084dc84eaffe
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:32 -0400

    gitter-cli: add license

--- Formula/gitter-cli.rb
+++ Formula/gitter-cli.rb
@@ -5,6 +5,7 @@ class GitterCli < Formula
   homepage "https://github.com/RodrigoEspinosa/gitter-cli"
   url "https://github.com/RodrigoEspinosa/gitter-cli/archive/v0.8.5.tar.gz"
   sha256 "c4e335620fc1be50569f3b7543c28ba2c6121b1c7e6d041464b29a31b3d84c25"
+  license "MIT"
 
   bottle do
     cellar :any_skip_relocation
