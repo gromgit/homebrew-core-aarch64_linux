8c394121a10a59f526ba2435fb0c58f11bd4eb23
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:53 -0400

    nedit: add license

--- Formula/nedit.rb
+++ Formula/nedit.rb
@@ -3,6 +3,7 @@ class Nedit < Formula
   homepage "https://sourceforge.net/projects/nedit/"
   url "https://downloads.sourceforge.net/project/nedit/nedit-source/nedit-5.7-src.tar.gz"
   sha256 "add9ac79ff973528ad36c86858238bac4f59896c27dbf285cbe6a4d425fca17a"
+  license "GPL-2.0"
 
   bottle do
     cellar :any
