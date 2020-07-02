c8d2647d3a11d04d92e4f68982cfc8865470de23
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:15 -0400

    libdaemon: add license

--- Formula/libdaemon.rb
+++ Formula/libdaemon.rb
@@ -3,6 +3,7 @@ class Libdaemon < Formula
   homepage "http://0pointer.de/lennart/projects/libdaemon/"
   url "http://0pointer.de/lennart/projects/libdaemon/libdaemon-0.14.tar.gz"
   sha256 "fd23eb5f6f986dcc7e708307355ba3289abe03cc381fc47a80bca4a50aa6b834"
+  license "LGPL-2.1"
 
   bottle do
     cellar :any
