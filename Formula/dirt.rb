683780b96daac614b7de8870f47195caf18cf927
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:57 -0400

    dirt: add license

--- Formula/dirt.rb
+++ Formula/dirt.rb
@@ -3,6 +3,7 @@ class Dirt < Formula
   homepage "https://github.com/tidalcycles/Dirt"
   url "https://github.com/tidalcycles/Dirt/archive/1.1.tar.gz"
   sha256 "bb1ae52311813d0ea3089bf3837592b885562518b4b44967ce88a24bc10802b6"
+  license "GPL-3.0"
   revision 1
   head "https://github.com/tidalcycles/Dirt.git"
 
