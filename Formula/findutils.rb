6e93df772599171c1d6052b5b7266fa64145f141
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:15 -0400

    findutils: add license

--- Formula/findutils.rb
+++ Formula/findutils.rb
@@ -4,6 +4,7 @@ class Findutils < Formula
   url "https://ftp.gnu.org/gnu/findutils/findutils-4.7.0.tar.xz"
   mirror "https://ftpmirror.gnu.org/findutils/findutils-4.7.0.tar.xz"
   sha256 "c5fefbdf9858f7e4feb86f036e1247a54c79fc2d8e4b7064d5aaa1f47dfa789a"
+  license "GPL-3.0"
 
   bottle do
     cellar :any_skip_relocation
