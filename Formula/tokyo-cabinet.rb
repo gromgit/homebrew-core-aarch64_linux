096225498cb99e545c65cd893f6f8cc8cecfccd1
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:00 -0400

    tokyo-cabinet: add license

--- Formula/tokyo-cabinet.rb
+++ Formula/tokyo-cabinet.rb
@@ -4,6 +4,7 @@ class TokyoCabinet < Formula
   url "https://fallabs.com/tokyocabinet/tokyocabinet-1.4.48.tar.gz"
   mirror "https://deb.debian.org/debian/pool/main/t/tokyocabinet/tokyocabinet_1.4.48.orig.tar.gz"
   sha256 "a003f47c39a91e22d76bc4fe68b9b3de0f38851b160bbb1ca07a4f6441de1f90"
+  license "LGPL-2.1"
 
   bottle do
     rebuild 1
