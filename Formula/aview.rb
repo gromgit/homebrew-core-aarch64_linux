0830145593429e06caf854efc6ba523f824f5624
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:17 -0400

    aview: add license

--- Formula/aview.rb
+++ Formula/aview.rb
@@ -3,6 +3,7 @@ class Aview < Formula
   homepage "https://aa-project.sourceforge.io/"
   url "https://downloads.sourceforge.net/aa-project/aview-1.3.0rc1.tar.gz"
   sha256 "42d61c4194e8b9b69a881fdde698c83cb27d7eda59e08b300e73aaa34474ec99"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
