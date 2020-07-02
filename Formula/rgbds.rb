f6d1fce9f70b36739cba63e9aa21d2e93423b686
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:28 -0400

    rgbds: add license

--- Formula/rgbds.rb
+++ Formula/rgbds.rb
@@ -3,6 +3,7 @@ class Rgbds < Formula
   homepage "https://github.com/rednex/rgbds"
   url "https://github.com/rednex/rgbds/archive/v0.4.0.tar.gz"
   sha256 "18be4a8ec79e43a6343fa128c6790dae33a229e0ed10e3dcccbbdc0b0c363933"
+  license "MIT"
   head "https://github.com/rednex/rgbds.git"
 
   bottle do
