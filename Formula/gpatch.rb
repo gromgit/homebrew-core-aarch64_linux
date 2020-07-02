cd560b5cc2dac4d8e0f649e167c8e24e606b0b01
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:41 -0400

    gpatch: add license

--- Formula/gpatch.rb
+++ Formula/gpatch.rb
@@ -4,6 +4,7 @@ class Gpatch < Formula
   url "https://ftp.gnu.org/gnu/patch/patch-2.7.6.tar.xz"
   mirror "https://ftpmirror.gnu.org/patch/patch-2.7.6.tar.xz"
   sha256 "ac610bda97abe0d9f6b7c963255a11dcb196c25e337c61f94e4778d632f1d8fd"
+  license "GPL-3.0"
 
   bottle do
     cellar :any_skip_relocation
