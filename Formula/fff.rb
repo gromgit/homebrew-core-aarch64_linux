7df6445e9d0c6daea8a5183c675c9bcd0f44f092
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:14 -0400

    fff: add license

--- Formula/fff.rb
+++ Formula/fff.rb
@@ -3,6 +3,7 @@ class Fff < Formula
   homepage "https://github.com/dylanaraps/fff"
   url "https://github.com/dylanaraps/fff/archive/2.1.tar.gz"
   sha256 "776870d11c022fa40468d5d582831c0ab5beced573489097deaaf5dd690e7eab"
+  license "MIT"
 
   bottle :unneeded
 
