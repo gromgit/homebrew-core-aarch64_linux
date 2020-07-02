d55a8ae6abd39f83af5cc4622226ecf4909575b1
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:31 -0400

    libyubikey: add license

--- Formula/libyubikey.rb
+++ Formula/libyubikey.rb
@@ -3,6 +3,7 @@ class Libyubikey < Formula
   homepage "https://yubico.github.io/yubico-c/"
   url "https://developers.yubico.com/yubico-c/Releases/libyubikey-1.13.tar.gz"
   sha256 "04edd0eb09cb665a05d808c58e1985f25bb7c5254d2849f36a0658ffc51c3401"
+  license "BSD-2-Clause"
 
   bottle do
     cellar :any
