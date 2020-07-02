12197f5d96adbee1cc5cfdc6bc7952c492c0de45
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:00 -0400

    docker-machine-nfs: add license

--- Formula/docker-machine-nfs.rb
+++ Formula/docker-machine-nfs.rb
@@ -3,6 +3,7 @@ class DockerMachineNfs < Formula
   homepage "https://github.com/adlogix/docker-machine-nfs"
   url "https://github.com/adlogix/docker-machine-nfs/archive/0.5.3.tar.gz"
   sha256 "ad8fb4e9c3576ca5dd9f4014ff88a41e42c3c65c26c04a94122f79533050e0bb"
+  license "MIT"
 
   bottle :unneeded
 
