08c27b4bb7459b86d4d1563ed2054a3134aef1aa
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:15 -0400

    ficy: add license

--- Formula/ficy.rb
+++ Formula/ficy.rb
@@ -3,6 +3,7 @@ class Ficy < Formula
   homepage "https://www.thregr.org/~wavexx/software/fIcy/"
   url "https://www.thregr.org/~wavexx/software/fIcy/releases/fIcy-1.0.21.tar.gz"
   sha256 "8564b16d3a52fa6dc286b02bfcc19e4acdc148c30f1750ca144e2ea47c84fd81"
+  license "LGPL-2.1"
   head "https://github.com/wavexx/fIcy.git"
 
   bottle do
