6b3241ff2740e97843455a9bfaf680554c57f85c
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:24 -0400

    zero-install: add license

--- Formula/zero-install.rb
+++ Formula/zero-install.rb
@@ -4,6 +4,7 @@ class ZeroInstall < Formula
   url "https://github.com/0install/0install.git",
       :tag      => "v2.17",
       :revision => "4a837bd638d93905b96d073c28c644894f8d4a0b"
+  license "LGPL-2.1"
   head "https://github.com/0install/0install.git"
 
   bottle do
