04de5cafe38f62174fbe5ca864c4c39602673c8b
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:01 -0400

    tracebox: add license

--- Formula/tracebox.rb
+++ Formula/tracebox.rb
@@ -4,6 +4,7 @@ class Tracebox < Formula
   url "https://github.com/tracebox/tracebox.git",
       :tag      => "v0.4.4",
       :revision => "4fc12b2e330e52d340ecd64b3a33dbc34c160390"
+  license "GPL-2.0"
   revision 1
   head "https://github.com/tracebox/tracebox.git"
 
