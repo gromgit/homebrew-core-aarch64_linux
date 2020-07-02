450ac8f3567c4c723cf63c7a05cd70ea8159303b
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:04 -0400

    dtrx: add license

--- Formula/dtrx.rb
+++ Formula/dtrx.rb
@@ -4,6 +4,7 @@ class Dtrx < Formula
   url "https://brettcsmith.org/2007/dtrx/dtrx-7.1.tar.gz"
   mirror "https://fossies.org/linux/privat/dtrx-7.1.tar.gz"
   sha256 "1c9afe48e9d9d4a1caa4c9b0c50593c6fe427942716ce717d81bae7f8425ce97"
+  license "GPL-3.0"
 
   bottle do
     cellar :any_skip_relocation
