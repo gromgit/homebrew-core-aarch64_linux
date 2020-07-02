84027bdc8c18515674574d4532e3e16a9cdd4e97
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:18 -0400

    ford: add license

--- Formula/ford.rb
+++ Formula/ford.rb
@@ -5,6 +5,7 @@ class Ford < Formula
   homepage "https://github.com/cmacmackin/ford/"
   url "https://github.com/cmacmackin/ford/archive/v6.0.0.tar.gz"
   sha256 "45fd53c7e5263fea2e751c436de6a1513d250647e98e32668b9965677974309e"
+  license "GPL-3.0"
   revision 2
   head "https://github.com/cmacmackin/ford.git"
 
