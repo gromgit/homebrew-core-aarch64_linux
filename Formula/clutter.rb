9bb421dadf04fb13d2035124c370ccbab403e8a0
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:42 -0400

    clutter: add license

--- Formula/clutter.rb
+++ Formula/clutter.rb
@@ -3,6 +3,7 @@ class Clutter < Formula
   homepage "https://wiki.gnome.org/Projects/Clutter"
   url "https://download.gnome.org/sources/clutter/1.26/clutter-1.26.4.tar.xz"
   sha256 "8b48fac159843f556d0a6be3dbfc6b083fc6d9c58a20a49a6b4919ab4263c4e6"
+  license "LGPL-2.1"
 
   bottle do
     sha256 "ccec39ce9c941de753798e466b8cfc2a69612319d8b5a422f6e4bde49db305b1" => :catalina
