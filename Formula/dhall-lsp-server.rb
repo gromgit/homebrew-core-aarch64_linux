307e56cf45866f2db6565ff0c1f3cd483b217f6d
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:56 -0400

    dhall-lsp-server: add license

--- Formula/dhall-lsp-server.rb
+++ Formula/dhall-lsp-server.rb
@@ -3,6 +3,7 @@ class DhallLspServer < Formula
   homepage "https://github.com/dhall-lang/dhall-haskell/tree/master/dhall-lsp-server"
   url "https://hackage.haskell.org/package/dhall-lsp-server-1.0.7/dhall-lsp-server-1.0.7.tar.gz"
   sha256 "81b85964ef2865b76fdfa494e241090da3dbd00fac3b2f1b530ee9e35354de22"
+  license "BSD-3-Clause"
   revision 1
   head "https://github.com/dhall-lang/dhall-haskell.git"
 
