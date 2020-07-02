4c1a45751125f4f3575c6ff1b46404daa35afdf6
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:17 -0400

    prodigal: add license

--- Formula/prodigal.rb
+++ Formula/prodigal.rb
@@ -3,6 +3,7 @@ class Prodigal < Formula
   homepage "https://github.com/hyattpd/Prodigal"
   url "https://github.com/hyattpd/Prodigal/archive/v2.6.3.tar.gz"
   sha256 "89094ad4bff5a8a8732d899f31cec350f5a4c27bcbdd12663f87c9d1f0ec599f"
+  license "GPL-3.0"
 
   bottle do
     cellar :any_skip_relocation
