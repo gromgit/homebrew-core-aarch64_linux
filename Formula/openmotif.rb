c724db3700a28ca150aef129a513dbaac5ea4cfa
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:02 -0400

    openmotif: add license

--- Formula/openmotif.rb
+++ Formula/openmotif.rb
@@ -3,6 +3,7 @@ class Openmotif < Formula
   homepage "https://motif.ics.com/motif"
   url "https://downloads.sourceforge.net/project/motif/Motif%202.3.8%20Source%20Code/motif-2.3.8.tar.gz"
   sha256 "859b723666eeac7df018209d66045c9853b50b4218cecadb794e2359619ebce7"
+  license "LGPL-2.1"
 
   bottle do
     sha256 "a997ddf37cc71329a09ca6616cbf0ef63bbe1a477a65a94781fdb72d8ec15822" => :catalina
