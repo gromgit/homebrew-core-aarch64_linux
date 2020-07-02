afc86149196d1475585675edc9775f0ff89a3cc9
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:38 -0400

    shivavg: add license

--- Formula/shivavg.rb
+++ Formula/shivavg.rb
@@ -3,6 +3,7 @@ class Shivavg < Formula
   homepage "https://sourceforge.net/projects/shivavg/"
   url "https://downloads.sourceforge.net/project/shivavg/ShivaVG/0.2.1/ShivaVG-0.2.1.zip"
   sha256 "9735079392829f7aaf79e02ed84dd74f5c443c39c02ff461cfdd19cfc4ae89c4"
+  license "LGPL-2.1"
 
   bottle do
     cellar :any
