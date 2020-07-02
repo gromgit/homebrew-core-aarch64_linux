add8f36e67ac9b7213c60eef18388af1f7e7ee1b
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:36 -0400

    chakra: add license

--- Formula/chakra.rb
+++ Formula/chakra.rb
@@ -3,6 +3,7 @@ class Chakra < Formula
   homepage "https://github.com/Microsoft/ChakraCore"
   url "https://github.com/microsoft/ChakraCore/archive/v1.11.20.tar.gz"
   sha256 "2d7b70db1ab01cd8c3e519639f560b7787abafe4bc41dfb862095a3573f76f0d"
+  license "MIT"
 
   bottle do
     cellar :any
