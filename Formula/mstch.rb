58bdd3589194682c85a7d2a79e655513a4ff8064
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:49 -0400

    mstch: add license

--- Formula/mstch.rb
+++ Formula/mstch.rb
@@ -3,6 +3,7 @@ class Mstch < Formula
   homepage "https://github.com/no1msd/mstch"
   url "https://github.com/no1msd/mstch/archive/1.0.2.tar.gz"
   sha256 "811ed61400d4e9d4f9ae0f7679a2ffd590f0b3c06b16f2798e1f89ab917cba6c"
+  license "MIT"
 
   bottle do
     cellar :any_skip_relocation
