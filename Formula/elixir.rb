cf5fd53e86271b5c0338e58c0ff1dcd0432c3f1d
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:07 -0400

    elixir: add license

--- Formula/elixir.rb
+++ Formula/elixir.rb
@@ -3,6 +3,7 @@ class Elixir < Formula
   homepage "https://elixir-lang.org/"
   url "https://github.com/elixir-lang/elixir/archive/v1.10.4.tar.gz"
   sha256 "8518c78f43fe36315dbe0d623823c2c1b7a025c114f3f4adbb48e04ef63f1d9f"
+  license "Apache-2.0"
   head "https://github.com/elixir-lang/elixir.git"
 
   bottle do
