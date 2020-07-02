2716519507d3a2e6ed79189a70343f7aa6be026c
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:12 -0400

    fabric: add license

--- Formula/fabric.rb
+++ Formula/fabric.rb
@@ -5,6 +5,7 @@ class Fabric < Formula
   homepage "https://www.fabfile.org/"
   url "https://github.com/fabric/fabric/archive/2.5.0.tar.gz"
   sha256 "4b3f4c79bcc372de575e7756b50c89373ce1acb8565902a5e406b5e78c41389c"
+  license "BSD-2-Clause"
   revision 4
   head "https://github.com/fabric/fabric.git"
 
