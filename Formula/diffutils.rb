980b931bdb471c96946e727e7bc59ffc3bcb97db
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:57 -0400

    diffutils: add license

--- Formula/diffutils.rb
+++ Formula/diffutils.rb
@@ -4,6 +4,7 @@ class Diffutils < Formula
   url "https://ftp.gnu.org/gnu/diffutils/diffutils-3.7.tar.xz"
   mirror "https://ftpmirror.gnu.org/diffutils/diffutils-3.7.tar.xz"
   sha256 "b3a7a6221c3dc916085f0d205abf6b8e1ba443d4dd965118da364a1dc1cb3a26"
+  license "GPL-3.0"
 
   bottle do
     cellar :any_skip_relocation
