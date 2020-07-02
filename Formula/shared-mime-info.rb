58ab353d50b7dfad6f5bb700cf615abd557d8bb4
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:37 -0400

    shared-mime-info: add license

--- Formula/shared-mime-info.rb
+++ Formula/shared-mime-info.rb
@@ -3,6 +3,7 @@ class SharedMimeInfo < Formula
   homepage "https://wiki.freedesktop.org/www/Software/shared-mime-info"
   url "https://gitlab.freedesktop.org/xdg/shared-mime-info/uploads/0440063a2e6823a4b1a6fb2f2af8350f/shared-mime-info-2.0.tar.xz"
   sha256 "23c1cb7919f31cf97aeb5225548f75705f706aa5cc7d1c4c503364bcc8681e06"
+  license "GPL-2.0"
 
   bottle do
     cellar :any
