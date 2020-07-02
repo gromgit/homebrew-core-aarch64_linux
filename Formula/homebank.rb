98ce1cf40da4bc80f633fede48d55829bb0989fd
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:50 -0400

    homebank: add license

--- Formula/homebank.rb
+++ Formula/homebank.rb
@@ -3,6 +3,7 @@ class Homebank < Formula
   homepage "http://homebank.free.fr"
   url "http://homebank.free.fr/public/homebank-5.4.2.tar.gz"
   sha256 "c6ce84b421e7199ae545ea8b8d981f347af4d67b4cd5912b6789cf1450db722e"
+  license "GPL-2.0"
   revision 1
 
   bottle do
