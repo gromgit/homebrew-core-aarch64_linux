f60cb3acab05254b44131840f7d414c87a6cd863
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:23 -0400

    yosys: add license

--- Formula/yosys.rb
+++ Formula/yosys.rb
@@ -3,6 +3,7 @@ class Yosys < Formula
   homepage "http://www.clifford.at/yosys/"
   url "https://github.com/YosysHQ/yosys/archive/yosys-0.9.tar.gz"
   sha256 "f2e31371f9cf1b36cb4f57b23fd6eb849adc7d935dcf49f3c905aa5136382c2f"
+  license "ISC"
   revision 2
   head "https://github.com/YosysHQ/yosys.git"
 
