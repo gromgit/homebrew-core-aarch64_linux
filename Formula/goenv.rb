789ca7be25d7051484e43f3d4db0001d0073a496
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:38 -0400

    goenv: add license

--- Formula/goenv.rb
+++ Formula/goenv.rb
@@ -3,6 +3,7 @@ class Goenv < Formula
   homepage "https://github.com/syndbg/goenv"
   url "https://github.com/syndbg/goenv/archive/1.23.3.tar.gz"
   sha256 "1559f2907ee0339328466fe93f3c9637b7674917db81754412c7f842749e3201"
+  license "MIT"
   version_scheme 1
   head "https://github.com/syndbg/goenv.git"
 
