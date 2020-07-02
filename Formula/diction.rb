665f471cfe885bea94e134282c39c6dba7370fa9
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:56 -0400

    diction: add license

--- Formula/diction.rb
+++ Formula/diction.rb
@@ -4,6 +4,7 @@ class Diction < Formula
   url "https://ftp.gnu.org/gnu/diction/diction-1.11.tar.gz"
   mirror "https://ftpmirror.gnu.org/diction/diction-1.11.tar.gz"
   sha256 "35c2f1bf8ddf0d5fa9f737ffc8e55230736e5d850ff40b57fdf5ef1d7aa024f6"
+  license "GPL-3.0"
 
   bottle do
     sha256 "ff26ae017482eaef3a07b4c6522e65a84b2ec03b6afaffa20e0138a244edd5e2" => :catalina
