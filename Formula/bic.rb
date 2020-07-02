549d3f1e8caa448302082cfca61f5aa2579ee261
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:24 -0400

    bic: add license

--- Formula/bic.rb
+++ Formula/bic.rb
@@ -3,6 +3,7 @@ class Bic < Formula
   homepage "https://github.com/hexagonal-sun/bic"
   url "https://github.com/hexagonal-sun/bic/releases/download/v1.0.0/bic-v1.0.0.tar.gz"
   sha256 "553324e39d87df59930d093a264c14176d5e3aaa24cd8bff276531fb94775100"
+  license "GPL-2.0"
   bottle do
     cellar :any
     sha256 "41d1871d125642f8437b5bb7b74f205b0eee956be0ad46b7677680b76764c0cb" => :catalina
