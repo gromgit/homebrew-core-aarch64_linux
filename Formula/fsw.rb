afeb6919dec820169ff9e50881afc803422fd0b6
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:20 -0400

    fsw: add license

--- Formula/fsw.rb
+++ Formula/fsw.rb
@@ -3,6 +3,7 @@ class Fsw < Formula
   homepage "https://emcrisostomo.github.io/fsw/"
   url "https://github.com/emcrisostomo/fsw/releases/download/1.3.9/fsw-1.3.9.tar.gz"
   sha256 "9222f76f99ef9841dc937a8f23b529f635ad70b0f004b9dd4afb35c1b0d8f0ff"
+  license "GPL-3.0"
 
   bottle do
     cellar :any_skip_relocation
