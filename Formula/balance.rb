66b3b601382a68917163604a9947aec1cdcfc95a
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:20 -0400

    balance: add license

--- Formula/balance.rb
+++ Formula/balance.rb
@@ -3,6 +3,7 @@ class Balance < Formula
   homepage "https://www.inlab.net/balance/"
   url "https://www.inlab.net/wp-content/uploads/2018/05/balance-3.57.tar.gz"
   sha256 "b355f98932a9f4c9786cb61012e8bdf913c79044434b7d9621e2fa08370afbe1"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
