a5c4689039b5fca0b514baba7b32a7222e0e1e3b
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:52 -0400

    httpdiff: add license

--- Formula/httpdiff.rb
+++ Formula/httpdiff.rb
@@ -3,6 +3,7 @@ class Httpdiff < Formula
   homepage "https://github.com/jgrahamc/httpdiff"
   url "https://github.com/jgrahamc/httpdiff/archive/v1.0.0.tar.gz"
   sha256 "b2d3ed4c8a31c0b060c61bd504cff3b67cd23f0da8bde00acd1bfba018830f7f"
+  license "GPL-2.0"
   head "https://github.com/jgrahamc/httpdiff.git"
 
   bottle do
