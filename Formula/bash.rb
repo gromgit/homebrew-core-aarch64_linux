2f213cbe65f86d386b63edd592e96db2aa81eef4
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:21 -0400

    bash: add license

--- Formula/bash.rb
+++ Formula/bash.rb
@@ -1,6 +1,7 @@
 class Bash < Formula
   desc "Bourne-Again SHell, a UNIX command interpreter"
   homepage "https://www.gnu.org/software/bash/"
+  license "GPL-3.0"
   head "https://git.savannah.gnu.org/git/bash.git"
 
   stable do
