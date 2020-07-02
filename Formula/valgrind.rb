b2e890b1d882816f310d1156e98e0bdbadf18658
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:08 -0400

    valgrind: add license

--- Formula/valgrind.rb
+++ Formula/valgrind.rb
@@ -1,6 +1,7 @@
 class Valgrind < Formula
   desc "Dynamic analysis tools (memory, debug, profiling)"
   homepage "https://www.valgrind.org/"
+  license "GPL-2.0"
 
   stable do
     url "https://sourceware.org/pub/valgrind/valgrind-3.16.1.tar.bz2"
