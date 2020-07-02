6104ca4ef5a6bc47ce989422b1b6e78b1a4c147f
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:57 -0400

    distribution: add license

--- Formula/distribution.rb
+++ Formula/distribution.rb
@@ -3,6 +3,7 @@ class Distribution < Formula
   homepage "https://github.com/philovivero/distribution"
   url "https://github.com/philovivero/distribution/archive/1.3.tar.gz"
   sha256 "d7f2c9beeee15986d24d8068eb132c0a63c0bd9ace932e724cb38c1e6e54f95d"
+  license "GPL-2.0"
   head "https://github.com/philovivero/distribution.git"
 
   bottle :unneeded
