cfdd558a65db8d5839005575f0cf032460383ece
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:47 -0400

    mpgtx: add license

--- Formula/mpgtx.rb
+++ Formula/mpgtx.rb
@@ -3,6 +3,7 @@ class Mpgtx < Formula
   homepage "https://mpgtx.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/mpgtx/mpgtx/1.3.1/mpgtx-1.3.1.tar.gz"
   sha256 "8815e73e98b862f12ba1ef5eaaf49407cf211c1f668c5ee325bf04af27f8e377"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
