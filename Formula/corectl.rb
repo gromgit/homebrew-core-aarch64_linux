55a53474b7a17409c8de30be0fbf19db16c02322
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:46 -0400

    corectl: add license

--- Formula/corectl.rb
+++ Formula/corectl.rb
@@ -3,6 +3,7 @@ class Corectl < Formula
   homepage "https://github.com/TheNewNormal/corectl"
   url "https://github.com/TheNewNormal/corectl/archive/v0.7.18.tar.gz"
   sha256 "9bdf7bc8c6a7bd861e2b723c0566d0a093ed5d5caf370a065a1708132b4ab98a"
+  license "Apache-2.0"
   revision 2
   head "https://github.com/TheNewNormal/corectl.git", :branch => "golang"
 
