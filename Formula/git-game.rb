c24269279da2d2be8a0d2653cf19ac8f9d355c79
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:28 -0400

    git-game: add license

--- Formula/git-game.rb
+++ Formula/git-game.rb
@@ -3,6 +3,7 @@ class GitGame < Formula
   homepage "https://github.com/jsomers/git-game"
   url "https://github.com/jsomers/git-game/archive/1.2.tar.gz"
   sha256 "d893b2c813388754c16d867cf37726cd7e73c9ccd316735aac43bf1cd3ab1412"
+  license "MIT"
 
   bottle :unneeded
 
