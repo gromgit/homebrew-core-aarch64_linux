691ee3f4205e72c8354c8c259caef5ec4e8b8ac7
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:21 -0400

    fwup: add license

--- Formula/fwup.rb
+++ Formula/fwup.rb
@@ -3,6 +3,7 @@ class Fwup < Formula
   homepage "https://github.com/fhunleth/fwup"
   url "https://github.com/fhunleth/fwup/releases/download/v1.8.0/fwup-1.8.0.tar.gz"
   sha256 "92912386d3ae11d68bc9793242403133d08b01fea3f5eaf481b8bb1aac18c9c6"
+  license "Apache-2.0"
 
   bottle do
     cellar :any
