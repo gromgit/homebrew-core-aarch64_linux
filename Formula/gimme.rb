e337524616b5affb78f99e5c30cb0c7d8f35dbe4
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:27 -0400

    gimme: add license

--- Formula/gimme.rb
+++ Formula/gimme.rb
@@ -3,6 +3,7 @@ class Gimme < Formula
   homepage "https://github.com/travis-ci/gimme"
   url "https://github.com/travis-ci/gimme/archive/v1.5.4.tar.gz"
   sha256 "71036f892b3cae08f29be6fd4c69fb20d9b003ec80a24221d73e995e12ab0fe0"
+  license "MIT"
 
   bottle :unneeded
 
