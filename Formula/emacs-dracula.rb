623ff6fbe1e9bc3614da30a3c8892d012f14241e
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:07 -0400

    emacs-dracula: add license

--- Formula/emacs-dracula.rb
+++ Formula/emacs-dracula.rb
@@ -3,6 +3,7 @@ class EmacsDracula < Formula
   homepage "https://github.com/dracula/emacs"
   url "https://github.com/dracula/emacs/archive/v1.7.0.tar.gz"
   sha256 "dbbcc968cf8187a8ada9f040137ba03dc0e51b285e96e128d26cea05cf470330"
+  license "MIT"
   head "https://github.com/dracula/emacs.git"
 
   bottle :unneeded
