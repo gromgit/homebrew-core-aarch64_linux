56ecfc5224f0449fb0eadccc6ad09c42c7a17ff9
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:20 -0400

    pyenv: add license

--- Formula/pyenv.rb
+++ Formula/pyenv.rb
@@ -3,6 +3,7 @@ class Pyenv < Formula
   homepage "https://github.com/pyenv/pyenv"
   url "https://github.com/pyenv/pyenv/archive/v1.2.19.tar.gz"
   sha256 "e93466735ac9c34d68b7d5d71f32c16a2b4b1a6a1adffb85acc4126a3398b9d0"
+  license "MIT"
   version_scheme 1
   head "https://github.com/pyenv/pyenv.git"
 
