1f4c718174bace21ba018fce99a18ff6bfdd6727
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:26 -0400

    gifify: add license

--- Formula/gifify.rb
+++ Formula/gifify.rb
@@ -3,6 +3,7 @@ class Gifify < Formula
   homepage "https://github.com/jclem/gifify"
   url "https://github.com/jclem/gifify/archive/v4.0.tar.gz"
   sha256 "4cb967e8d0ba897bc91a60006e34299687f388dd47e05fd534f2eff8379fe479"
+  license "MIT"
   head "https://github.com/jclem/gifify.git"
 
   bottle :unneeded
