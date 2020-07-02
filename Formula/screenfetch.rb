1b51dd605ddcd5d3f9847fe95e09586e2b2e31b6
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:34 -0400

    screenfetch: add license

--- Formula/screenfetch.rb
+++ Formula/screenfetch.rb
@@ -3,6 +3,7 @@ class Screenfetch < Formula
   homepage "https://github.com/KittyKatt/screenFetch"
   url "https://github.com/KittyKatt/screenFetch/archive/v3.9.1.tar.gz"
   sha256 "aa97dcd2a8576ae18de6c16c19744aae1573a3da7541af4b98a91930a30a3178"
+  license "GPL-3.0"
   head "https://github.com/KittyKatt/screenFetch.git", :shallow => false
 
   bottle :unneeded
