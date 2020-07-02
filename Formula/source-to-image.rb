595710ec5266cabd18776313e016dddc82c67d91
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:43 -0400

    source-to-image: add license

--- Formula/source-to-image.rb
+++ Formula/source-to-image.rb
@@ -4,6 +4,7 @@ class SourceToImage < Formula
   url "https://github.com/openshift/source-to-image.git",
       :tag      => "v1.3.0",
       :revision => "eed2850f2187435ef5d83487e05bf3dc18622ceb"
+  license "Apache-2.0"
   revision 1
   head "https://github.com/openshift/source-to-image.git"
 
