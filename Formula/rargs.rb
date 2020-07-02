4e50d47c7b3ab5e22ee5754226828c31b737294b
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:24 -0400

    rargs: add license

--- Formula/rargs.rb
+++ Formula/rargs.rb
@@ -3,6 +3,7 @@ class Rargs < Formula
   homepage "https://github.com/lotabout/rargs"
   url "https://github.com/lotabout/rargs/archive/v0.3.0.tar.gz"
   sha256 "22d9aa4368a0f9d1fd82391439d3aabf4ddfb24ad674a680d6407c9e22969da3"
+  license "MIT"
 
   bottle do
     cellar :any_skip_relocation
