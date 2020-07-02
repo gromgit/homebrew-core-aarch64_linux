5b7616fb821adcc739e9b127b8d6e587b038c336
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:52 -0400

    t1lib: add license

--- Formula/t1lib.rb
+++ Formula/t1lib.rb
@@ -4,6 +4,7 @@ class T1lib < Formula
   url "https://www.ibiblio.org/pub/linux/libs/graphics/t1lib-5.1.2.tar.gz"
   mirror "https://fossies.org/linux/misc/old/t1lib-5.1.2.tar.gz"
   sha256 "821328b5054f7890a0d0cd2f52825270705df3641dbd476d58d17e56ed957b59"
+  license "GPL-2.0"
 
   bottle do
     rebuild 2
