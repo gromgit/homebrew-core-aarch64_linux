7d7b5b25c8be65a2f0028ab5b2726482b416be1a
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:33 -0400

    scalaenv: add license

--- Formula/scalaenv.rb
+++ Formula/scalaenv.rb
@@ -3,6 +3,7 @@ class Scalaenv < Formula
   homepage "https://github.com/scalaenv/scalaenv"
   url "https://github.com/scalaenv/scalaenv/archive/version/0.1.4.tar.gz"
   sha256 "b791aaf1e0be7e26f455b4d659fba272b5017bc702d5bb5b3a1dc7d039cd456c"
+  license "MIT"
   head "https://github.com/scalaenv/scalaenv.git"
 
   bottle :unneeded
