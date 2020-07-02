9b4bfc3106664da1554caa5863eea484db3fa494
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:23 -0400

    gdbm: add license

--- Formula/gdbm.rb
+++ Formula/gdbm.rb
@@ -4,6 +4,7 @@ class Gdbm < Formula
   url "https://ftp.gnu.org/gnu/gdbm/gdbm-1.18.1.tar.gz"
   mirror "https://ftpmirror.gnu.org/gdbm/gdbm-1.18.1.tar.gz"
   sha256 "86e613527e5dba544e73208f42b78b7c022d4fa5a6d5498bf18c8d6f745b91dc"
+  license "GPL-3.0"
 
   bottle do
     cellar :any
