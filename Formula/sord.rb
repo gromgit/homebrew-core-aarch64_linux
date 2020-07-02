9ca070458262fe670fabd6025307e5d4cf18a8c1
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:42 -0400

    sord: add license

--- Formula/sord.rb
+++ Formula/sord.rb
@@ -3,6 +3,7 @@ class Sord < Formula
   homepage "https://drobilla.net/software/sord/"
   url "https://download.drobilla.net/sord-0.16.4.tar.bz2"
   sha256 "b15998f4e7ad958201346009477d6696e90ee5d3e9aff25e7e9be074372690d7"
+  license "ISC"
 
   bottle do
     cellar :any
