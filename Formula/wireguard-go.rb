62141e173022306d9c4b4a6124f1d1c4814b7010
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:16 -0400

    wireguard-go: add license

--- Formula/wireguard-go.rb
+++ Formula/wireguard-go.rb
@@ -3,6 +3,7 @@ class WireguardGo < Formula
   homepage "https://www.wireguard.com/"
   url "https://git.zx2c4.com/wireguard-go/snapshot/wireguard-go-0.0.20200320.tar.xz"
   sha256 "c8262da949043976d092859843d3c0cdffe225ec6f1398ba119858b6c1b3552f"
+  license "MIT"
   head "https://git.zx2c4.com/wireguard-go", :using => :git
 
   bottle do
