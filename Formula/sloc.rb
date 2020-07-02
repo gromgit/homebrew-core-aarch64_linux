c191b22f6dcaa37cb99c50c8078855e90a28333f
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:40 -0400

    sloc: add license

--- Formula/sloc.rb
+++ Formula/sloc.rb
@@ -5,6 +5,7 @@ class Sloc < Formula
   homepage "https://github.com/flosse/sloc#readme"
   url "https://registry.npmjs.org/sloc/-/sloc-0.2.1.tgz"
   sha256 "fb56f1763b7dadfd0566f819665efc0725ba8dfbec13c75da3839edf309596e6"
+  license "MIT"
 
   bottle do
     cellar :any_skip_relocation
