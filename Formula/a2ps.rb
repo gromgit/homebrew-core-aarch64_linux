32f9a729da9b9da96fff07ea9fc01b118ee08f1b
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:06 -0400

    a2ps: add license

--- Formula/a2ps.rb
+++ Formula/a2ps.rb
@@ -4,6 +4,7 @@ class A2ps < Formula
   url "https://ftp.gnu.org/gnu/a2ps/a2ps-4.14.tar.gz"
   mirror "https://ftpmirror.gnu.org/a2ps/a2ps-4.14.tar.gz"
   sha256 "f3ae8d3d4564a41b6e2a21f237d2f2b104f48108591e8b83497500182a3ab3a4"
+  license "GPL-3.0"
 
   bottle do
     rebuild 3
