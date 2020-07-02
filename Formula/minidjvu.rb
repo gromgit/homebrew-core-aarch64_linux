dd659125ef899c3bdae8eecf7f409a458cc6fe09
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:42 -0400

    minidjvu: add license

--- Formula/minidjvu.rb
+++ Formula/minidjvu.rb
@@ -3,6 +3,7 @@ class Minidjvu < Formula
   homepage "https://minidjvu.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/minidjvu/minidjvu/0.8/minidjvu-0.8.tar.gz"
   sha256 "e9c892e0272ee4e560eaa2dbd16b40719b9797a1fa2749efeb6622f388dfb74a"
+  license "GPL-2.0"
 
   bottle do
     cellar :any
