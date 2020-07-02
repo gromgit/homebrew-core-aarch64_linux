d67014bf6bd459ba6bc81c6ecba81bac723870ad
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:29 -0400

    brew-gem: add license

--- Formula/brew-gem.rb
+++ Formula/brew-gem.rb
@@ -3,6 +3,7 @@ class BrewGem < Formula
   homepage "https://github.com/sportngin/brew-gem"
   url "https://github.com/sportngin/brew-gem/archive/v1.0.0.tar.gz"
   sha256 "bc321722746e82243be50bc999d02f3d0754d40e7e40552ed2d61103191c82cf"
+  license "MIT"
   head "https://github.com/sportngin/brew-gem.git"
 
   bottle :unneeded
