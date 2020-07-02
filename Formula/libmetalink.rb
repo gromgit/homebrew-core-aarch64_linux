cc68c46daf53bdc94c39ba18fc392d93b52fd293
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:21 -0400

    libmetalink: add license

--- Formula/libmetalink.rb
+++ Formula/libmetalink.rb
@@ -3,6 +3,7 @@ class Libmetalink < Formula
   homepage "https://launchpad.net/libmetalink/"
   url "https://launchpad.net/libmetalink/trunk/libmetalink-0.1.3/+download/libmetalink-0.1.3.tar.xz"
   sha256 "86312620c5b64c694b91f9cc355eabbd358fa92195b3e99517504076bf9fe33a"
+  license "MIT"
 
   bottle do
     cellar :any
