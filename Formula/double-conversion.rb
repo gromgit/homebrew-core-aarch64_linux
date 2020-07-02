bb4e2e135986c9df64a64da488066ca731a65c2b
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:02 -0400

    double-conversion: add license

--- Formula/double-conversion.rb
+++ Formula/double-conversion.rb
@@ -3,6 +3,7 @@ class DoubleConversion < Formula
   homepage "https://github.com/google/double-conversion"
   url "https://github.com/google/double-conversion/archive/v3.1.5.tar.gz"
   sha256 "a63ecb93182134ba4293fd5f22d6e08ca417caafa244afaa751cbfddf6415b13"
+  license "BSD-3-Clause"
   head "https://github.com/google/double-conversion.git"
 
   bottle do
