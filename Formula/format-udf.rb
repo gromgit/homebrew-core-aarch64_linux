b4bc704ec09d9c90324ab9e0b63be271e1805ecf
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:18 -0400

    format-udf: add license

--- Formula/format-udf.rb
+++ Formula/format-udf.rb
@@ -3,6 +3,7 @@ class FormatUdf < Formula
   homepage "https://github.com/JElchison/format-udf"
   url "https://github.com/JElchison/format-udf/archive/1.8.0.tar.gz"
   sha256 "52854097db9044d729fbd7cff012f4b554df01c15225ee17ec159c71da174c8d"
+  license "GPL-2.0"
 
   bottle :unneeded
 
