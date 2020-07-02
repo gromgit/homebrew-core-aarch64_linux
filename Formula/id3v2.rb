62c2d2a1592dade2d1d2511721c14ca53323d4ae
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:55 -0400

    id3v2: add license

--- Formula/id3v2.rb
+++ Formula/id3v2.rb
@@ -3,6 +3,7 @@ class Id3v2 < Formula
   homepage "https://id3v2.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/id3v2/id3v2/0.1.12/id3v2-0.1.12.tar.gz"
   sha256 "8105fad3189dbb0e4cb381862b4fa18744233c3bbe6def6f81ff64f5101722bf"
+  license "LGPL-2.1"
 
   bottle do
     cellar :any
