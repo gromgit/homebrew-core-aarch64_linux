04a7864b6115577d7774b0d1bd3bc1220104a48d
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:53 -0400

    tccutil: add license

--- Formula/tccutil.rb
+++ Formula/tccutil.rb
@@ -3,6 +3,7 @@ class Tccutil < Formula
   homepage "https://github.com/jacobsalmela/tccutil"
   url "https://github.com/jacobsalmela/tccutil/archive/v1.2.7.tar.gz"
   sha256 "4584962a059584ba0810cee7912f9333ba6259da22bea7e6b3206bcf934d5cf1"
+  license "GPL-2.0"
   head "https://github.com/jacobsalmela/tccutil.git"
 
   bottle :unneeded
