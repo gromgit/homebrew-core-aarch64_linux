dd305723463ca2a6de369a74a83a87861bb26ab8
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:27 -0400

    git-appraise: add license

--- Formula/git-appraise.rb
+++ Formula/git-appraise.rb
@@ -3,6 +3,7 @@ class GitAppraise < Formula
   homepage "https://github.com/google/git-appraise"
   url "https://github.com/google/git-appraise/archive/v0.6.tar.gz"
   sha256 "5c674ee7f022cbc36c5889053382dde80b8e80f76f6fac0ba0445ed5313a36f1"
+  license "Apache-2.0"
   head "https://github.com/google/git-appraise.git"
 
   bottle do
