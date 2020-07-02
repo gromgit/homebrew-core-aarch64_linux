9df3714260b41821745fb75eb7c52a42e330d22c
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:28 -0400

    boot-clj: add license

--- Formula/boot-clj.rb
+++ Formula/boot-clj.rb
@@ -3,6 +3,7 @@ class BootClj < Formula
   homepage "https://boot-clj.com/"
   url "https://github.com/boot-clj/boot/releases/download/2.8.3/boot.jar"
   sha256 "31f001988f580456b55a9462d95a8bf8b439956906c8aca65d3656206aa42ec7"
+  license "EPL-1.0"
   revision 2
 
   bottle :unneeded
