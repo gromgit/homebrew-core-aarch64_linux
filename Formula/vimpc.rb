d9ad09b41113741f19bd44b1447db13718c4eed9
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:10 -0400

    vimpc: add license

--- Formula/vimpc.rb
+++ Formula/vimpc.rb
@@ -3,6 +3,7 @@ class Vimpc < Formula
   homepage "https://sourceforge.net/projects/vimpc/"
   url "https://github.com/boysetsfrog/vimpc/archive/v0.09.2.tar.gz"
   sha256 "caa772f984e35b1c2fbe0349bc9068fc00c17bcfcc0c596f818fa894cac035ce"
+  license "GPL-3.0"
   head "https://github.com/boysetsfrog/vimpc.git"
 
   bottle do
