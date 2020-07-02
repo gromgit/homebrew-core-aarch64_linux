c7c530d7924b2f3709fd086859068324e4cf78d9
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:54 -0400

    density: add license

--- Formula/density.rb
+++ Formula/density.rb
@@ -3,6 +3,7 @@ class Density < Formula
   homepage "https://github.com/centaurean/density"
   url "https://github.com/centaurean/density/archive/density-0.14.2.tar.gz"
   sha256 "0b130ea9da3ddaad78810a621a758b47c4135d91d5b5fd22d60bbaf04bc147da"
+  license "BSD-3-Clause"
 
   bottle do
     cellar :any
