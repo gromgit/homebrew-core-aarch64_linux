2ca98a2d54ea33df6f1e3332399876f07e3451c0
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:35 -0400

    lzo: add license

--- Formula/lzo.rb
+++ Formula/lzo.rb
@@ -3,6 +3,7 @@ class Lzo < Formula
   homepage "https://www.oberhumer.com/opensource/lzo/"
   url "https://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz"
   sha256 "c0f892943208266f9b6543b3ae308fab6284c5c90e627931446fb49b4221a072"
+  license "GPL-2.0"
 
   bottle do
     cellar :any
