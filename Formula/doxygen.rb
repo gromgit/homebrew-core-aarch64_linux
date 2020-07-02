b3c884bdc253557c7de0f9aa7bc47760f65228cb
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:02 -0400

    doxygen: add license

--- Formula/doxygen.rb
+++ Formula/doxygen.rb
@@ -4,6 +4,7 @@ class Doxygen < Formula
   url "https://doxygen.nl/files/doxygen-1.8.18.src.tar.gz"
   mirror "https://downloads.sourceforge.net/project/doxygen/rel-1.8.18/doxygen-1.8.18.src.tar.gz"
   sha256 "18173d9edc46d2d116c1f92a95d683ec76b6b4b45b817ac4f245bb1073d00656"
+  license "GPL-2.0"
   head "https://github.com/doxygen/doxygen.git"
 
   bottle do
