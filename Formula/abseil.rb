75db8577f9738d1e9dd0ced7bf1448d793554c6a
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:06 -0400

    abseil: add license

--- Formula/abseil.rb
+++ Formula/abseil.rb
@@ -3,6 +3,7 @@ class Abseil < Formula
   homepage "https://abseil.io"
   url "https://github.com/abseil/abseil-cpp/archive/20200225.2.tar.gz"
   sha256 "f41868f7a938605c92936230081175d1eae87f6ea2c248f41077c8f88316f111"
+  license "Apache-2.0"
 
   bottle do
     cellar :any_skip_relocation
