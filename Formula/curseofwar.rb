e4bcd949ee8b57d28c28e3ec75543e6618414813
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:51 -0400

    curseofwar: add license

--- Formula/curseofwar.rb
+++ Formula/curseofwar.rb
@@ -3,6 +3,7 @@ class Curseofwar < Formula
   homepage "https://a-nikolaev.github.io/curseofwar/"
   url "https://github.com/a-nikolaev/curseofwar/archive/v1.2.0.tar.gz"
   sha256 "91b7781e26341faa6b6999b6baf6e74ef532fa94303ab6a2bf9ff6d614a3f670"
+  license "GPL-3.0"
   head "https://github.com/a-nikolaev/curseofwar.git"
 
   bottle do
