eb270e2c92cf6c14837ce25f88eb475807378d17
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:25 -0400

    rbtools: add license

--- Formula/rbtools.rb
+++ Formula/rbtools.rb
@@ -5,6 +5,7 @@ class Rbtools < Formula
   homepage "https://www.reviewboard.org/downloads/rbtools/"
   url "https://files.pythonhosted.org/packages/04/98/10d5f67470c48e000cf9c95fb748f6a302eb01c0cb006d3ec37ff2f1e7c1/RBTools-1.0.3.tar.gz"
   sha256 "ff4cea3ad7b2d1b1666b811021cf5047f1fbe9417428fb5133a40ede81e3e83c"
+  license "MIT"
   head "https://github.com/reviewboard/rbtools.git"
 
   bottle do
