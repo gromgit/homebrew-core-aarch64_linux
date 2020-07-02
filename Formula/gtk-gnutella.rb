b013b83d33fa147812e23495a89c57e2d5fa6ba8
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:45 -0400

    gtk-gnutella: add license

--- Formula/gtk-gnutella.rb
+++ Formula/gtk-gnutella.rb
@@ -3,6 +3,7 @@ class GtkGnutella < Formula
   homepage "https://gtk-gnutella.sourceforge.io"
   url "https://downloads.sourceforge.net/project/gtk-gnutella/gtk-gnutella/1.1.15/gtk-gnutella-1.1.15.tar.xz"
   sha256 "2931fab394b6d36c14a6bbc3b0ff584af5550f2eeb03f78bb19743c47767c1b7"
+  license "GPL-2.0"
 
   bottle do
     sha256 "9fb121eec5c3e0f3e8750464148488870056596b59068e5765991c3851e85316" => :catalina
