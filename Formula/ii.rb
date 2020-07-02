092c408e19c2c095b6ced081bd7cb1828467a0da
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:55 -0400

    ii: add license

--- Formula/ii.rb
+++ Formula/ii.rb
@@ -3,6 +3,7 @@ class Ii < Formula
   homepage "https://tools.suckless.org/ii/"
   url "https://dl.suckless.org/tools/ii-1.8.tar.gz"
   sha256 "b9d9e1eae25e63071960e921af8b217ab1abe64210bd290994aca178a8dc68d2"
+  license "MIT"
   head "https://git.suckless.org/ii", :using => :git
 
   bottle do
