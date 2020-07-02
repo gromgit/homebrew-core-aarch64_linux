11b8692e6cd235d939f6adc0fa2965ee84df2dab
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:42 -0400

    gpsim: add license

--- Formula/gpsim.rb
+++ Formula/gpsim.rb
@@ -3,6 +3,7 @@ class Gpsim < Formula
   homepage "https://gpsim.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/gpsim/gpsim/0.31.0/gpsim-0.31.0.tar.gz"
   sha256 "110ee6be3a5d02b32803a91e480cbfc9d423ef72e0830703fc0bc97b9569923f"
+  license "GPL-2.0"
   head "https://svn.code.sf.net/p/gpsim/code/trunk"
 
   bottle do
