a9610e0ac55665e35cf9f918fcff2b25ed28bf3a
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:41 -0400

    gpredict: add license

--- Formula/gpredict.rb
+++ Formula/gpredict.rb
@@ -3,6 +3,7 @@ class Gpredict < Formula
   homepage "http://gpredict.oz9aec.net/"
   url "https://github.com/csete/gpredict/releases/download/v2.2.1/gpredict-2.2.1.tar.bz2"
   sha256 "e759c4bae0b17b202a7c0f8281ff016f819b502780d3e77b46fe8767e7498e43"
+  license "GPL-2.0"
   revision 2
 
   bottle do
