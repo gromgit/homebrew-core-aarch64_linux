e13accf2ed1e7b67ead40c2c4f46cf6c679fe195
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:05 -0400

    dwarf: add license

--- Formula/dwarf.rb
+++ Formula/dwarf.rb
@@ -3,6 +3,7 @@ class Dwarf < Formula
   homepage "https://github.com/elboza/dwarf-ng/"
   url "https://github.com/elboza/dwarf-ng/archive/dwarf-0.4.0.tar.gz"
   sha256 "a64656f53ded5166041ae25cc4b1ad9ab5046a5c4d4c05b727447e73c0d83da0"
+  license "GPL-2.0"
   revision 1
 
   bottle do
