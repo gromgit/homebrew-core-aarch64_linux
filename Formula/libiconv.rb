4b0d35560340ec87a5cf338dee5bef7ed7ab1aaf
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:20 -0400

    libiconv: add license

--- Formula/libiconv.rb
+++ Formula/libiconv.rb
@@ -4,6 +4,7 @@ class Libiconv < Formula
   url "https://ftp.gnu.org/gnu/libiconv/libiconv-1.16.tar.gz"
   mirror "https://ftpmirror.gnu.org/libiconv/libiconv-1.16.tar.gz"
   sha256 "e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04"
+  license "GPL-3.0"
 
   bottle do
     cellar :any
