47c1f143b9bb436a0e2070f9fd3436a528fc7e25
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:25 -0400

    bitlbee: add license

--- Formula/bitlbee.rb
+++ Formula/bitlbee.rb
@@ -1,6 +1,7 @@
 class Bitlbee < Formula
   desc "IRC to other chat networks gateway"
   homepage "https://www.bitlbee.org/"
+  license "GPL-2.0"
   head "https://github.com/bitlbee/bitlbee.git"
 
   stable do
