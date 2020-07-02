839c86f32fd16c725be92c55f20b9b44d915388e
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:08 -0400

    enigma: add license

--- Formula/enigma.rb
+++ Formula/enigma.rb
@@ -3,6 +3,7 @@ class Enigma < Formula
   homepage "https://www.nongnu.org/enigma/"
   url "https://downloads.sourceforge.net/project/enigma-game/Release%201.21/enigma-1.21.tar.gz"
   sha256 "d872cf067d8eb560d3bb1cb17245814bc56ac3953ae1f12e2229c8eb6f82ce01"
+  license "GPL-2.0"
   revision 4
 
   bottle do
