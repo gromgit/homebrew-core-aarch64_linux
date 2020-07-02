955df2b8549bfa8f5e4bf0076768ad8ce9352fe5
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:52 -0400

    tailor: add license

--- Formula/tailor.rb
+++ Formula/tailor.rb
@@ -3,6 +3,7 @@ class Tailor < Formula
   homepage "https://tailor.sh"
   url "https://github.com/sleekbyte/tailor/releases/download/v0.12.0/tailor-0.12.0.tar"
   sha256 "ec3810b27e9a35ecdf3a21987f17cad86918240d773172264e9abbb1a7efc415"
+  license "MIT"
   revision 1
 
   bottle :unneeded
