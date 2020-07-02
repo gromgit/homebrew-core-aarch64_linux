2db31c6c0cd8e758c5bc41e2066403b36a240e6a
William Ma <whoiswillma@gmail.com>
2020-07-02 11:47:41 -0400

    clipper: add license

--- Formula/clipper.rb
+++ Formula/clipper.rb
@@ -3,6 +3,7 @@ class Clipper < Formula
   homepage "https://wincent.com/products/clipper"
   url "https://github.com/wincent/clipper/archive/2.0.0.tar.gz"
   sha256 "9c9fa0b198d11513777d40c88e2529b2f2f84d7045a500be5946976a5cdcfe83"
+  license "BSD-2-Clause"
 
   bottle do
     cellar :any_skip_relocation
