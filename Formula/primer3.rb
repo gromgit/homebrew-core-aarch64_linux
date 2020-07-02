abbfa713d475f74cf5693aeb440c404f9e2dbf38
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:16 -0400

    primer3: add license

--- Formula/primer3.rb
+++ Formula/primer3.rb
@@ -3,6 +3,7 @@ class Primer3 < Formula
   homepage "https://primer3.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/primer3/primer3/2.4.0/primer3-2.4.0.tar.gz"
   sha256 "6d537640c86e2b4656ae77f75b6ad4478fd0ca43985a56cce531fb9fc0431c47"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
