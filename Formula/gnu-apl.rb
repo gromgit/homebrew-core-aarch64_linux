4922973f8c755a7a757633d71f1ccb4d7b1193d7
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:35 -0400

    gnu-apl: add license

--- Formula/gnu-apl.rb
+++ Formula/gnu-apl.rb
@@ -4,6 +4,7 @@ class GnuApl < Formula
   url "https://ftp.gnu.org/gnu/apl/apl-1.8.tar.gz"
   mirror "https://ftpmirror.gnu.org/apl/apl-1.8.tar.gz"
   sha256 "144f4c858a0d430ce8f28be90a35920dd8e0951e56976cb80b55053fa0d8bbcb"
+  license "GPL-3.0"
 
   bottle do
     sha256 "2a7717b6b60567eade10b30f473771f563ebd6a009c91e0522eab6497516e892" => :catalina
