26186a5f1ea42e2fce718a8f63066b12e6f7a9e9
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:06 -0400

    pandoc-citeproc: add license

--- Formula/pandoc-citeproc.rb
+++ Formula/pandoc-citeproc.rb
@@ -7,6 +7,7 @@ class PandocCiteproc < Formula
   homepage "https://github.com/jgm/pandoc-citeproc"
   url "https://hackage.haskell.org/package/pandoc-citeproc-0.17.0.1/pandoc-citeproc-0.17.0.1.tar.gz"
   sha256 "f3e5ce3d1d21c27178f2fc69580750e3ce97fc5f962f2d01f7b6aa2e090c2342"
+  license "BSD-3-Clause"
   head "https://github.com/jgm/pandoc-citeproc.git"
 
   bottle do
