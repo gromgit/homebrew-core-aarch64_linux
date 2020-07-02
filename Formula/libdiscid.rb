373f95403d77eda50d8e1df876e835a46f6c3573
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:16 -0400

    libdiscid: add license

--- Formula/libdiscid.rb
+++ Formula/libdiscid.rb
@@ -3,6 +3,7 @@ class Libdiscid < Formula
   homepage "https://musicbrainz.org/doc/libdiscid"
   url "http://ftp.musicbrainz.org/pub/musicbrainz/libdiscid/libdiscid-0.6.2.tar.gz"
   sha256 "f9e443ac4c0dd4819c2841fcc82169a46fb9a626352cdb9c7f65dd3624cd31b9"
+  license "LGPL-2.1"
 
   bottle do
     cellar :any
