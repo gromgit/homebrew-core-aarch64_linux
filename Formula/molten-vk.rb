0edb00746d36cffeb3474cfaa77e3d9905be3d20
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:45 -0400

    molten-vk: add license

--- Formula/molten-vk.rb
+++ Formula/molten-vk.rb
@@ -3,6 +3,7 @@ class MoltenVk < Formula
   homepage "https://github.com/KhronosGroup/MoltenVK"
   url "https://github.com/KhronosGroup/MoltenVK/archive/v1.0.41.tar.gz"
   sha256 "a11208f3bc2eb5cd6cfebbc0bc09ac2af8ecbe5cc3e57cde817bc8bc96d2cc33"
+  license "Apache-2.0"
   revision 1
 
   bottle do
