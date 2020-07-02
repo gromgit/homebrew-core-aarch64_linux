eff2e7b508a818512929b7e40fd0c2f554b2d4bc
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:15 -0400

    ffuf: add license

--- Formula/ffuf.rb
+++ Formula/ffuf.rb
@@ -3,6 +3,7 @@ class Ffuf < Formula
   homepage "https://github.com/ffuf/ffuf"
   url "https://github.com/ffuf/ffuf/archive/v1.0.2.tar.gz"
   sha256 "018e3aa92d27846eaf55b49a451b2517db1ad65d5d696116ade1fe8bda4535ba"
+  license "MIT"
 
   bottle do
     cellar :any_skip_relocation
