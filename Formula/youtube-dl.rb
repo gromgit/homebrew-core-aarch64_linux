6d8f830f0f127760c4ee930c654294d7c70fff80
William Ma <whoiswillma@gmail.com>
2020-07-02 11:51:23 -0400

    youtube-dl: add license

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -3,6 +3,7 @@ class YoutubeDl < Formula
   homepage "https://ytdl-org.github.io/youtube-dl/"
   url "https://github.com/ytdl-org/youtube-dl/releases/download/2020.06.16.1/youtube-dl-2020.06.16.1.tar.gz"
   sha256 "fd06c016ce2a8b82a3cee245acc397433ddb54a276bdf8d79bcaddbe194000e0"
+  license "Unlicense"
 
   head do
     url "https://github.com/ytdl-org/youtube-dl.git"
