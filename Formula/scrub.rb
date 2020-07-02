0f87462f6bd4ccdf90272a2d318ca7f8d5f39760
William Ma <whoiswillma@gmail.com>
2020-07-02 11:50:35 -0400

    scrub: add license

--- Formula/scrub.rb
+++ Formula/scrub.rb
@@ -4,6 +4,7 @@ class Scrub < Formula
   url "https://github.com/chaos/scrub/releases/download/2.6.1/scrub-2.6.1.tar.gz"
   mirror "https://deb.debian.org/debian/pool/main/s/scrub/scrub_2.6.1.orig.tar.gz"
   sha256 "43d98d3795bc2de7920efe81ef2c5de4e9ed1f903c35c939a7d65adc416d6cb8"
+  license "GPL-2.0"
 
   bottle do
     cellar :any_skip_relocation
