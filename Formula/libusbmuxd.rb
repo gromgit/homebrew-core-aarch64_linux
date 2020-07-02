39e7ab4e5855b864601eaab64569cee73eb11936
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:29 -0400

    libusbmuxd: add license

--- Formula/libusbmuxd.rb
+++ Formula/libusbmuxd.rb
@@ -3,6 +3,7 @@ class Libusbmuxd < Formula
   homepage "https://www.libimobiledevice.org/"
   url "https://github.com/libimobiledevice/libusbmuxd/archive/2.0.2.tar.gz"
   sha256 "8ae3e1d9340177f8f3a785be276435869363de79f491d05d8a84a59efc8a8fdc"
+  license "LGPL-2.1"
   head "https://github.com/libimobiledevice/libusbmuxd.git"
 
   bottle do
