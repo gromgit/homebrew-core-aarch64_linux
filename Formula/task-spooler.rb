104b2711ea7f66d896b51f0da9de7af81cffdf02
EricFromCanada <enk3@outlook.com>
2020-07-03 15:59:05 -0400

    task-spooler: fix conflicts_with reason

--- Formula/task-spooler.rb
+++ Formula/task-spooler.rb
@@ -13,7 +13,7 @@ class TaskSpooler < Formula
     sha256 "9403d0c240bad09d576288d6b5ed94057dad03ceb30a4893a935c13f9e58af7f" => :el_capitan
   end
 
-  conflicts_with "moreutils", :because => "both install a `ts` executable."
+  conflicts_with "moreutils", :because => "both install a `ts` executable"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
