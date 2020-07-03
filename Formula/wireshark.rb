1f6aabe74134dfcf6c14e5961f40c1cfa270505d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-03 18:07:30 +0000

    wireshark: update 3.2.5 bottle.

--- Formula/wireshark.rb
+++ Formula/wireshark.rb
@@ -7,9 +7,9 @@ class Wireshark < Formula
   head "https://code.wireshark.org/review/wireshark", :using => :git
 
   bottle do
-    sha256 "92ee317ff4290c3c93a6bbfda5de62095da7cca4f0cc881914632fbbdfc3528e" => :catalina
-    sha256 "3efa164bfe9b96f82c54334fc9b665a781fa4bdacd14626f4c8433521e0105f8" => :mojave
-    sha256 "b8c334c790c9ba8923d5ebccdc0199a120cb07f265dd70aad6e1faf6360233cc" => :high_sierra
+    sha256 "27ac1313478b321ece59273e73ff0ef3dbfc8f3bb5e804f84d1647e9c3493f02" => :catalina
+    sha256 "6f7222fa6ba170a6f51cdde7b4167aa25fff8bd5f507b0741844f30225e0c959" => :mojave
+    sha256 "2201d32c491a0b8e0d1116e58120695826fb4b1782129ec62bcb8d67b222c6f5" => :high_sierra
   end
 
   depends_on "cmake" => :build
