22a6085106b14d202e1591e5c6f74c0d237772c4
EricFromCanada <enk3@outlook.com>
2020-07-03 15:59:05 -0400

    virtuoso: fix conflicts_with reason

--- Formula/virtuoso.rb
+++ Formula/virtuoso.rb
@@ -26,7 +26,7 @@ class Virtuoso < Formula
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
 
-  conflicts_with "unixodbc", :because => "Both install `isql` binaries."
+  conflicts_with "unixodbc", :because => "both install `isql` binaries"
 
   skip_clean :la
 
