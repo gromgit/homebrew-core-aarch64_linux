f349a940076409cf37ef3b2918ff8b5b01f1a4f1
EricFromCanada <enk3@outlook.com>
2020-07-03 15:59:04 -0400

    rarian: fix conflicts_with reason

--- Formula/rarian.rb
+++ Formula/rarian.rb
@@ -15,7 +15,7 @@ class Rarian < Formula
   end
 
   conflicts_with "scrollkeeper",
-    :because => "rarian and scrollkeeper install the same binaries."
+    :because => "rarian and scrollkeeper install the same binaries"
 
   def install
     system "./configure", "--disable-dependency-tracking",
