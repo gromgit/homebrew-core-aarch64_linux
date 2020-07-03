a8172703ace605576def07f80db332938460b587
EricFromCanada <enk3@outlook.com>
2020-07-03 15:59:03 -0400

    mariadb: fix conflicts_with reason

--- Formula/mariadb.rb
+++ Formula/mariadb.rb
@@ -23,8 +23,7 @@ class Mariadb < Formula
   conflicts_with "mysql", "percona-server",
     :because => "mariadb, mysql, and percona install the same binaries"
   conflicts_with "mytop", :because => "both install `mytop` binaries"
-  conflicts_with "mariadb-connector-c",
-    :because => "both install plugins"
+  conflicts_with "mariadb-connector-c", :because => "both install `mariadb_config`"
 
   def install
     # Set basedir and ldata so that mysql_install_db can find the server
