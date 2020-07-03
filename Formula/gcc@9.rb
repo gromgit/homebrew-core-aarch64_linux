c389d6c0079b2493469e63375f10aeb01f4c8882
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-07-03 09:35:45 +0000

    gcc@9: add 9.3.0 bottle.

--- Formula/gcc@9.rb
+++ Formula/gcc@9.rb
@@ -5,6 +5,12 @@ class GccAT9 < Formula
   mirror "https://ftpmirror.gnu.org/gcc/gcc-9.3.0/gcc-9.3.0.tar.xz"
   sha256 "71e197867611f6054aa1119b13a0c0abac12834765fe2d81f35ac57f84f742d1"
 
+  bottle do
+    sha256 "68aa5249f09a70b9c46bd403a46ae42b64f6ea6b3a2af00603852ecaf77c72ce" => :catalina
+    sha256 "445cf4a6a4f8f3da61c7e1e6aceaf6fe919a08c475126b2b1e159eae829617a4" => :mojave
+    sha256 "682244d252f68de9513ed43f45e3e9f80bcd582e58df1d4aaa16197f3fc88742" => :high_sierra
+  end
+
   # The bottles are built on systems with the CLT installed, and do not work
   # out of the box on Xcode-only systems due to an incorrect sysroot.
   pour_bottle? do
