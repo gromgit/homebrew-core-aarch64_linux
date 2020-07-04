1423cefe10fddeae92c9a40ed421227265edf2cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-04 11:07:02 +0000

    atomicparsley: update 20200701.154658.b0d6223 bottle.

--- Formula/atomicparsley.rb
+++ Formula/atomicparsley.rb
@@ -9,11 +9,9 @@ class Atomicparsley < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "632b3bc281a6f3bd5b9a913dff1e805c9fb9997f41b4a02db0a1aa34f6faced3" => :catalina
-    sha256 "d32a565f675bd0b2c5ebf1b5aee01fb79d9d42b072dedf724b7ee03b2cc242ee" => :mojave
-    sha256 "05c4cdc1dfc14fa6f06fdbbcadead5055a9fb53091d014458b86ecb4b22111fe" => :high_sierra
-    sha256 "d5f8672d420511ff76fd9ecc4d41c8aee5eecbf4382d7c4bd3fb04400c4617f4" => :sierra
-    sha256 "c0a7964ced998b2db7150f95b9329e138f28f0768be50d531fd4d82754e0ebde" => :el_capitan
+    sha256 "6bc22e04f5d2863e73010606d823eb0768d637165d190d3889db3780bbbb724c" => :catalina
+    sha256 "204e206047f48cdffef4fa91f81dbce6db370f002dd883000798d91f2916c391" => :mojave
+    sha256 "ce2509fe2cc72c18b6b82c9df5e802e2503f61ebf841833618a974ac21fc92c3" => :high_sierra
   end
 
   depends_on "cmake" => :build
