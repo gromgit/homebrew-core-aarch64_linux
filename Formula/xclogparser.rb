334d3508d48f753142f470743e0a44c062866601
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-04 03:06:45 +0000

    xclogparser: update 0.2.16 bottle.

--- Formula/xclogparser.rb
+++ Formula/xclogparser.rb
@@ -6,8 +6,8 @@ class Xclogparser < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c647d71fc8c047e200b596c5ebf7bff339526f185aba415e07383f16a57a6414" => :catalina
-    sha256 "9cdb1b675865de4e44956add153f1661dcf59c325b4bcb3840ce501922c63bb4" => :mojave
+    sha256 "ff3a52de3458d323f7594c8c6cda5e37790f02b5cf41f030ebb0dc34b4e96ffe" => :catalina
+    sha256 "0384c8142d4f4b708a5738a04917caea51070f1b132cc90b7748790f4c507ed7" => :mojave
   end
 
   depends_on :xcode => "11.0"
