80ef9ac4e64b2352c076a6f745151721ed51acd9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-04 11:07:11 +0000

    simple-scan: update 3.36.3_2 bottle.

--- Formula/simple-scan.rb
+++ Formula/simple-scan.rb
@@ -6,9 +6,9 @@ class SimpleScan < Formula
   revision 2
 
   bottle do
-    sha256 "b979d465d9efd3d607ce46103958255ab7b65cc191f7f6d6bda205fc94bc09f6" => :catalina
-    sha256 "3d6befb85ce58b752ef00196eb6d2a593d33bc063fb66f0580dddced28692d92" => :mojave
-    sha256 "f7dca4b3f325cadd784d0f61b751eb6eb781e00e5810be5b1f5a226dc55dd4a9" => :high_sierra
+    sha256 "aeef2e94520f6d1cc563a1d5f74792ad85ddacfce1888f9441a0c020ef6b99b2" => :catalina
+    sha256 "273399430d9cec668be18c74b27204483c3135b740b56923dc1b26ebee889038" => :mojave
+    sha256 "7fb0684ace98012471a2bb86afeb176723f0fc505c0b30bc3dc39feff0cd5356" => :high_sierra
   end
 
   depends_on "itstool" => :build
