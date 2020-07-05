fe4bda287e60f1b0f30c724c406af83fa56a48f2
Dongdong Tian <seisman.info@gmail.com>
2020-07-04 23:39:05 -0400

    gmt 6.1.0
    Remove "revision 0"
    
    Co-authored-by: Dawid Dziurla <dawidd0811@gmail.com>
    
    Keep only two mirrors
    
    Closes #57470.
    
    Signed-off-by: Rui Chen <rui@meetup.com>

--- Formula/gmt.rb
+++ Formula/gmt.rb
@@ -1,11 +1,10 @@
 class Gmt < Formula
   desc "Tools for manipulating and plotting geographic and Cartesian data"
   homepage "https://www.generic-mapping-tools.org/"
-  url "https://github.com/GenericMappingTools/gmt/releases/download/6.0.0/gmt-6.0.0-src.tar.xz"
-  mirror "https://mirrors.ustc.edu.cn/gmt/gmt-6.0.0-src.tar.xz"
-  mirror "https://fossies.org/linux/misc/GMT/gmt-6.0.0-src.tar.xz"
-  sha256 "8b91af18775a90968cdf369b659c289ded5b6cb2719c8c58294499ba2799b650"
-  revision 7
+  url "https://github.com/GenericMappingTools/gmt/releases/download/6.1.0/gmt-6.1.0-src.tar.xz"
+  mirror "ftp://ftp.soest.hawaii.edu/gmt/gmt-6.1.0-src.tar.xz"
+  mirror "https://mirrors.ustc.edu.cn/gmt/gmt-6.1.0-src.tar.xz"
+  sha256 "ad02780153c53a1116ae0cc7945b6f533f066af44c30d7f95ff138cfede1867c"
   head "https://github.com/GenericMappingTools/gmt.git"
 
   bottle do
@@ -22,38 +21,16 @@ class Gmt < Formula
 
   resource "gshhg" do
     url "https://github.com/GenericMappingTools/gshhg-gmt/releases/download/2.3.7/gshhg-gmt-2.3.7.tar.gz"
+    mirror "ftp://ftp.soest.hawaii.edu/gmt/gshhg-gmt-2.3.7.tar.gz"
     mirror "https://mirrors.ustc.edu.cn/gmt/gshhg-gmt-2.3.7.tar.gz"
-    mirror "https://fossies.org/linux/misc/GMT/gshhg-gmt-2.3.7.tar.gz"
     sha256 "9bb1a956fca0718c083bef842e625797535a00ce81f175df08b042c2a92cfe7f"
   end
 
   resource "dcw" do
     url "https://github.com/GenericMappingTools/dcw-gmt/releases/download/1.1.4/dcw-gmt-1.1.4.tar.gz"
+    mirror "ftp://ftp.soest.hawaii.edu/gmt/dcw-gmt-1.1.4.tar.gz"
     mirror "https://mirrors.ustc.edu.cn/gmt/dcw-gmt-1.1.4.tar.gz"
-    mirror "https://fossies.org/linux/misc/GMT/dcw-gmt-1.1.4.tar.gz"
     sha256 "8d47402abcd7f54a0f711365cd022e4eaea7da324edac83611ca035ea443aad3"
-  end
-
-  unless build.head?
-    # The following two patches fix a problem in detecting locally installed
-    # html pages (https://github.com/GenericMappingTools/gmt/issues/1960).
-    # They must be removed when GMT 6.0.1 is released.
-    patch do
-      url "https://github.com/GenericMappingTools/gmt/commit/b65dc6eb.diff?full_index=1"
-      sha256 "459dee38eef6b0c960a3d3a992ac715e82693c4c2cd34afd99965faf07cce2f8"
-    end
-
-    patch do
-      url "https://github.com/GenericMappingTools/gmt/commit/daf64655.diff?full_index=1"
-      sha256 "f2b5b0d5c4d6f568f453365f857c09429f2e29f9e72b220ca17a81128db75d37"
-    end
-
-    # netcdf 4.7.4 compatibility
-    # Remove with GMT 6.0.1.
-    patch do
-      url "https://github.com/GenericMappingTools/gmt/commit/53800c1f8206e9988dff88a71915cda7e7bff6e3.patch?full_index=1"
-      sha256 "8f9c072a394467853f8a44aa8af436ae9b1db7c7305985cb8018bb061cd5188c"
-    end
   end
 
   def install
