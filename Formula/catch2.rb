e65b726335ea20e88c87af75c3d4f3769d0b83ed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-05 16:07:23 +0000

    catch2: update 2.12.4 bottle.

--- Formula/catch2.rb
+++ Formula/catch2.rb
@@ -6,9 +6,9 @@ class Catch2 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "28cba45445941657d8e793a3eda2f29a5d5c4d2424c90a044dab7131638ff288" => :catalina
-    sha256 "28cba45445941657d8e793a3eda2f29a5d5c4d2424c90a044dab7131638ff288" => :mojave
-    sha256 "28cba45445941657d8e793a3eda2f29a5d5c4d2424c90a044dab7131638ff288" => :high_sierra
+    sha256 "7405839bc33f35be20565a1653bc112789d0ff0ba3daeff5e9846e1f80fc76f9" => :catalina
+    sha256 "7405839bc33f35be20565a1653bc112789d0ff0ba3daeff5e9846e1f80fc76f9" => :mojave
+    sha256 "6b886c94c3f56a9bd999eebf6140c7a62729fc9e8e4a110495f3e06d640abcf6" => :high_sierra
   end
 
   depends_on "cmake" => :build
