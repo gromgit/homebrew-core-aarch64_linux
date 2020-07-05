ce87e7c30fd49c2bb9839c8d24d2039ccdc18a17
miccal <miccal.matthews@gmail.com>
2020-07-06 03:38:43 +0800

    libevent 2.1.12
    libevent 2.1.12
    
    libevent 2.1.12
    
    libevent 2.1.12
    
    libevent 2.1.12
    
    libevent 2.1.12
    
    libevent 2.1.12
    
    Closes #57506.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/libevent.rb
+++ Formula/libevent.rb
@@ -1,9 +1,8 @@
 class Libevent < Formula
   desc "Asynchronous event library"
   homepage "https://libevent.org/"
-  url "https://github.com/libevent/libevent/archive/release-2.1.11-stable.tar.gz"
-  sha256 "229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e"
-  revision 1
+  url "https://github.com/libevent/libevent/archive/release-2.1.12-stable.tar.gz"
+  sha256 "7180a979aaa7000e1264da484f712d403fcf7679b1e9212c4e3d09f5c93efc24"
 
   bottle do
     cellar :any
@@ -15,22 +14,17 @@ class Libevent < Formula
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "doxygen" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "openssl@1.1"
 
   def install
-    inreplace "Doxyfile", /GENERATE_MAN\s*=\s*NO/, "GENERATE_MAN = YES"
     system "./autogen.sh"
     system "./configure", "--disable-dependency-tracking",
                           "--disable-debug-mode",
                           "--prefix=#{prefix}"
     system "make"
     system "make", "install"
-    system "make", "doxygen"
-    man3.install Dir["doxygen/man/man3/*.3"]
-    doc.install Dir["doxygen/html/*"]
   end
 
   test do
