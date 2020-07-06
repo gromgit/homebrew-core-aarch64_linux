f156f59bcdee810e014e8145d66f388010d418c2
Claudia <claui@users.noreply.github.com>
2020-07-06 20:53:06 +0200

    libimobiledevice: revert sha256 to previous value
    After @Bo98’s suggestion to use GitHub’s source release instead of
    GitHub’s source tarball, I forgot to revert the hash.
    
    This partially reverts commit 86a7bc16c1491f554d07fda79dc798b5804b981e.

--- Formula/libimobiledevice.rb
+++ Formula/libimobiledevice.rb
@@ -2,7 +2,7 @@ class Libimobiledevice < Formula
   desc "Library to communicate with iOS devices natively"
   homepage "https://www.libimobiledevice.org/"
   url "https://github.com/libimobiledevice/libimobiledevice/releases/download/1.3.0/libimobiledevice-1.3.0.tar.bz2"
-  sha256 "5143eaf34011a22dd1951f10495a7568e77a2e862fb9f4dbae9bab2f784f926e"
+  sha256 "53f2640c6365cd9f302a6248f531822dc94a6cced3f17128d4479a77bd75b0f6"
 
   bottle do
     cellar :any
