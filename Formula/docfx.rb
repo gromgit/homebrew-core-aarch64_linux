67fb923153aac74421b8d0f24c7b17cb5ca9265e
Caleb Xu <calebcenter@live.com>
2020-07-06 01:17:52 -0400

    docfx 2.56.1
    Closes #57532.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -1,8 +1,8 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/releases/download/v2.56/docfx.zip"
-  sha256 "8d20b192e59348de705a584b3dec664dd927fd0d1b560b7ab3c2625e489ce474"
+  url "https://github.com/dotnet/docfx/releases/download/v2.56.1/docfx.zip"
+  sha256 "b2170a20f90b80085b0f2579a81d070d742b8be3954478ddd14486a0ed288458"
 
   bottle :unneeded
 
