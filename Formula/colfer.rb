4a66bec7b99c3b1493c93108d4d4d23d419eb51b
Dawid Dziurla <dawidd0811@gmail.com>
2020-07-06 19:18:49 +0000

    colfer: add 1.8.1 bottle.

--- Formula/colfer.rb
+++ Formula/colfer.rb
@@ -5,6 +5,13 @@ class Colfer < Formula
   sha256 "5d184c8a311543f26c957fff6cad3908b9b0a4d31e454bb7f254b300d004dca7"
   license "CC0-1.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "dfdb2743960de62ee18ab35a7ead3d2d8de4207cc6ffa11ff0d8ebf393a591e8" => :catalina
+    sha256 "dfdb2743960de62ee18ab35a7ead3d2d8de4207cc6ffa11ff0d8ebf393a591e8" => :mojave
+    sha256 "dfdb2743960de62ee18ab35a7ead3d2d8de4207cc6ffa11ff0d8ebf393a591e8" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
