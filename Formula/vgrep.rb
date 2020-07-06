cfb5b5eda2f8b02f765c620cb5afe109e2375e8e
Dawid Dziurla <dawidd0811@gmail.com>
2020-07-06 19:21:10 +0000

    vgrep: add 2.3.3 bottle.

--- Formula/vgrep.rb
+++ Formula/vgrep.rb
@@ -5,6 +5,13 @@ class Vgrep < Formula
   sha256 "062145687d4c33f66b35be15633ff60cd24fd467bf2791f1a8c3ffb069935aa4"
   license "GPL-3.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5006d83eb22993f4cedcfce32b20b59e74526431bc44ea129b11f112f181c9a9" => :catalina
+    sha256 "d9fe404e03ea5f5a7cd2709d0d064e4f41fae3f65d66263d242847af25040613" => :mojave
+    sha256 "ab0e2c15aa3814e4e6f24c7d86a901fda915e6aa9ccbc7bbaf9d79df73ef91a4" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
