201abebf67ad430766ab4e00a3ed9c803d34ed3b
Sam Tay <sam.chong.tay@gmail.com>
2020-07-05 18:42:17 -0700

    so 0.3.6 (new formula)
    Closes #57520.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/so.rb
@@ -0,0 +1,39 @@
+class So < Formula
+  desc "Terminal interface for StackOverflow"
+  homepage "https://github.com/samtay/so"
+  url "https://github.com/samtay/so/archive/v0.3.6.tar.gz"
+  sha256 "6d2f541bb00dceb9163faf4cc44ff1bd39e07b46c35d6532e24b47d7ad6d47da"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  on_linux do
+    depends_on "pkg-config" => :build
+    depends_on "openssl@1.1"
+  end
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # try a query
+    opts = "--search-engine stackexchange --limit 1 --lucky"
+    query = "how do I exit Vim"
+    env_vars = "LC_CTYPE=en_US.UTF-8 LANG=en_US.UTF-8 TERM=xterm"
+    input, _, wait_thr = Open3.popen2 "script -q /dev/null"
+    input.puts "stty rows 80 cols 130"
+    input.puts "env #{env_vars} #{bin}/so #{opts} #{query} 2>&1 > output"
+    sleep 3
+
+    # quit
+    input.puts "q"
+    sleep 2
+    input.close
+
+    # make sure it's the correct answer
+    assert_match /:wq/, File.read("output")
+  ensure
+    Process.kill("TERM", wait_thr.pid)
+  end
+end
