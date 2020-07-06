b441e040d9a21da1d05c6f3eba6dda3740cd6c28
Alexander Zigelski <ali@crunchtime.dev>
2020-07-06 16:58:30 +0200

    bats-core 1.2.1
    Closes #57556.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bats-core.rb
+++ Formula/bats-core.rb
@@ -1,8 +1,8 @@
 class BatsCore < Formula
   desc "Bash Automated Testing System"
   homepage "https://github.com/bats-core/bats-core"
-  url "https://github.com/bats-core/bats-core/archive/v1.2.0.tar.gz"
-  sha256 "c74ea5517d9381c044e8e87427de0113be6ad591213d489aeaa28146daf1c723"
+  url "https://github.com/bats-core/bats-core/archive/v1.2.1.tar.gz"
+  sha256 "91c49b1fe6f0656c46491929ed728f8dfa9a96df0cce294963e8c6082bff87a2"
 
   bottle :unneeded
 
