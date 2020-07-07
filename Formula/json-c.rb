cb3ff5a39750c216d31e60bce5889b0c5d45fe4a
Rui Chen <rui@meetup.com>
2020-07-07 01:01:31 -0400

    json-c: update 0.14 bottle.

--- Formula/json-c.rb
+++ Formula/json-c.rb
@@ -8,11 +8,9 @@ class JsonC < Formula
 
   bottle do
     cellar :any
-    sha256 "40a0c602bbc055eabd9a8be69fbf43f7e5b83a7b3b346581574f20e0b97e558a" => :catalina
-    sha256 "9282e683104df6ee6cf495821de87a9badab080dfdcace1e631e518328e302d9" => :mojave
-    sha256 "4f51e5ad713e467d1df189c8b594c7e9ae279ed4fc2b0a8d1b328a3c258135d7" => :high_sierra
-    sha256 "279d88326f2f6aff9faf0c593ae4173c058e07ba0523f73107dfcbef3b54bd45" => :sierra
-    sha256 "724bffe043ecc73611fb4e7b2fcefbe35cb8b3a64aabf5cec92d43938b8e02d3" => :el_capitan
+    sha256 "50a8093fa52bf5a78391eaaede3565bd94769739bcb638ead50ef8ce5f36bd03" => :catalina
+    sha256 "8555eff999bab67ad77f403902e2a4410fa5d55f53d808218d677d08f7fcc9a1" => :mojave
+    sha256 "0d34251aac5183c121629d4a2506697d91cb56aac3ca247002594b6ac4054d96" => :high_sierra
   end
 
   depends_on "cmake" => :build
