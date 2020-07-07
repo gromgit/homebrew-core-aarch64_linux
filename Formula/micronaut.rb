6e02f7414adf96ad5b79e4d12e8bcf3751c33dc0
Sean Molenaar <smillerdev@me.com>
2020-07-07 17:17:47 +0200

    micronaut 2.0.0
    Closes #57244.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/micronaut.rb
+++ Formula/micronaut.rb
@@ -1,15 +1,21 @@
 class Micronaut < Formula
   desc "Modern JVM-based framework for building modular microservices"
   homepage "https://micronaut.io/"
-  url "https://github.com/micronaut-projects/micronaut-starter/archive/v2.0.0.RC1.tar.gz"
-  sha256 "91dc71b200c598a91b95a20792dcc3f3cb160b8e169b581f6b98689dc713acad"
+  url "https://github.com/micronaut-projects/micronaut-starter/archive/v2.0.0.tar.gz"
+  sha256 "5e30fc1878c9d7da9046745a5f71c611fc9bd1b765e32dd5e597a6fae1739f6c"
   license "Apache-2.0"
 
   depends_on "gradle" => :build
   depends_on "openjdk"
 
   def install
-    system "gradle", "build"
+    system "gradle", "micronaut-cli:assemble", "-x", "test"
+
+    mkdir_p libexec/"bin"
+    mv "starter-cli/build/exploded/bin/mn", libexec/"bin/mn"
+    mv "starter-cli/build/exploded/lib", libexec/"lib"
+
+    bash_completion.install "starter-cli/build/exploded/bin/mn_completion"
     (bin/"mn").write_env_script libexec/"bin/mn", Language::Java.overridable_java_home_env
   end
 
