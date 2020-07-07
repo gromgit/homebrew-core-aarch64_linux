a3da5e33ffaee6519ec9e50255c5adbc33799686
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-07 01:07:57 +0000

    folly: update 2020.07.06.00 bottle.

--- Formula/folly.rb
+++ Formula/folly.rb
@@ -8,9 +8,9 @@ class Folly < Formula
 
   bottle do
     cellar :any
-    sha256 "07e680ce12aaeceee4b5f360f359c80f92a30f0e944c54ed212b6bef0fd6a60c" => :catalina
-    sha256 "9316034f2811d377fe9d06e4613eba868392e07291487015b4712ae5b3e960b4" => :mojave
-    sha256 "841ea0d4865dc9ee8e3b43acebb42ff944783f557f4904d6fac900e1a70aaa47" => :high_sierra
+    sha256 "91e661372fe77bfe11ff2b032b4bd191a079f585cdbde09ce19b7a8d94c7c454" => :catalina
+    sha256 "07ae07a367c6ef8eeaa8e487d671ec18ad30f431de4133de75817b23679f01a2" => :mojave
+    sha256 "d0462b6b52590327f066f34f2cf11aa2c0f6d1f25b14510267180db43b7a9b25" => :high_sierra
   end
 
   depends_on "cmake" => :build
