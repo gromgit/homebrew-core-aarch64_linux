d5c8dc63d2583e516a0537b2f27692de26d99868
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-08 09:07:32 +0000

    ory-hydra: add 1.5.2 bottle.

--- Formula/ory-hydra.rb
+++ Formula/ory-hydra.rb
@@ -5,6 +5,13 @@ class OryHydra < Formula
   sha256 "50f7c09b1d6186ff35eed717c75f77884d038120f8096f5f0456526e49976a94"
   license "Apache-2.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5bc6be9f9dbd75db15304fc8e6e2eda6c398d75831285afe42986022781c9ab7" => :catalina
+    sha256 "1dd0d349b402e1051ed828e1be9051af5e6cd816517c891d429d21bb576c1675" => :mojave
+    sha256 "aae2b39200e3a5debd9840e6d0845f8268df4ab867ce2f6396620c5c4272b70d" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   conflicts_with "hydra", :because => "both install `hydra` binaries"
