09d04b1c4b47553f256a2f6c74b7f1b06c734b12
Seeker <meaningseeking@protonmail.com>
2020-07-08 09:12:46 -0700

    flarectl 0.12.1
    Closes #57672.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flarectl.rb
+++ Formula/flarectl.rb
@@ -1,8 +1,8 @@
 class Flarectl < Formula
   desc "CLI application for interacting with a Cloudflare account"
   homepage "https://github.com/cloudflare/cloudflare-go/tree/master/cmd/flarectl"
-  url "https://github.com/cloudflare/cloudflare-go/archive/v0.12.0.tar.gz"
-  sha256 "66b2768674b248ea46c6775c5f8f55916459583937606727677a4c7690348f07"
+  url "https://github.com/cloudflare/cloudflare-go/archive/v0.12.1.tar.gz"
+  sha256 "86bb23d7c7fe1abe5d93a05aafb7be5083d199a0533efc4bf8182db5d18afc3a"
   license "BSD-3-Clause"
   head "https://github.com/cloudflare/cloudflare-go.git"
 
