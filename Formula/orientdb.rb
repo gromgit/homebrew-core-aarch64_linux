ac590272f533130f9d7faa0051e5a8a056d824b5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-08 22:07:48 +0000

    orientdb: update 3.1.1 bottle.

--- Formula/orientdb.rb
+++ Formula/orientdb.rb
@@ -6,9 +6,9 @@ class Orientdb < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9855e1f9406b889701a242cbf7a5fc9dcc6d93e1fe7d5d043af591661997d037" => :catalina
-    sha256 "9855e1f9406b889701a242cbf7a5fc9dcc6d93e1fe7d5d043af591661997d037" => :mojave
-    sha256 "9855e1f9406b889701a242cbf7a5fc9dcc6d93e1fe7d5d043af591661997d037" => :high_sierra
+    sha256 "4dd8ac529ac0d88ebe0924e4c67ab6ec12e516286a503997061586e1c4a19164" => :catalina
+    sha256 "4dd8ac529ac0d88ebe0924e4c67ab6ec12e516286a503997061586e1c4a19164" => :mojave
+    sha256 "4dd8ac529ac0d88ebe0924e4c67ab6ec12e516286a503997061586e1c4a19164" => :high_sierra
   end
 
   depends_on "maven" => :build
