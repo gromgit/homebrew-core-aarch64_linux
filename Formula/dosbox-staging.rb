fa4535a02123d94b85536957da92e9be8e567745
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-07-09 07:33:09 +0000

    dosbox-staging: add 0.75.0 bottle.

--- Formula/dosbox-staging.rb
+++ Formula/dosbox-staging.rb
@@ -6,6 +6,13 @@ class DosboxStaging < Formula
   license "GPL-2.0"
   head "https://github.com/dosbox-staging/dosbox-staging.git"
 
+  bottle do
+    cellar :any
+    sha256 "9a09801ea7b4699eff48acfe05adc51015ef4d0c5baf192d0e52ea1a153c785a" => :catalina
+    sha256 "cbb2dcd04d78c41f098c99e72ec52f466ed73ef38b54b2286fe790aabf02a3b0" => :mojave
+    sha256 "a32107574d134e7b8cdeda5717307656a086929b2291ea647042beb26e771d0e" => :high_sierra
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
