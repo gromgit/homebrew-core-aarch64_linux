70b04f4aeda6fd2c8af123d792de6964bbdd4206
Adrian Ho <aho.rover-air61@03s.net>
2020-07-09 11:17:36 +0800

    batik: Fix app jar script creation
    The original formula created scripts for a hardcoded list of JARs. This update scripts against the JARs that are actually installed.
    
    Closes #57707.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/batik.rb
+++ Formula/batik.rb
@@ -4,14 +4,15 @@ class Batik < Formula
   url "https://www.apache.org/dyn/closer.lua?path=xmlgraphics/batik/binaries/batik-bin-1.13.tar.gz"
   mirror "https://archive.apache.org/dist/xmlgraphics/batik/binaries/batik-bin-1.13.tar.gz"
   sha256 "7c565899e4377a72edee216ffdf168d7b6928d5e1a8cdf477dfa1abd2c48589b"
+  revision 1
 
   bottle :unneeded
 
   def install
     libexec.install "lib", Dir["*.jar"]
-    bin.write_jar_script libexec/"batik-rasterizer-#{version}.jar", "batik-rasterizer"
-    bin.write_jar_script libexec/"batik-#{version}.jar", "batik"
-    bin.write_jar_script libexec/"batik-ttf2svg-#{version}.jar", "batik-ttf2svg"
+    Dir[libexec/"*.jar"].each do |f|
+      bin.write_jar_script f, File.basename(f, "-#{version}.jar")
+    end
   end
 
   test do
