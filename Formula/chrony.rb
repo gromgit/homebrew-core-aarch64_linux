aca851b5a70cba1ae0a09fd4312b5af7f9864680
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-07-09 11:54:09 +0000

    chrony: add 3.5 bottle.

--- Formula/chrony.rb
+++ Formula/chrony.rb
@@ -5,6 +5,13 @@ class Chrony < Formula
   sha256 "4e02795b1260a4ec51e6ace84149036305cc9fc340e65edb9f8452aa611339b5"
   license "GPL-2.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c9dcac360bb8b69012e963df7dcf34fa50b944bd419ce4d9c28d729cc6787ce4" => :catalina
+    sha256 "946bc4e955230a22006204b3bea76ae4f5b3bd186f94fad91e3fc4d5c10b7a02" => :mojave
+    sha256 "856b8ccc137b877687e7054b28e160b0ecd7b5b94a76b6d4a6978258dbf619e4" => :high_sierra
+  end
+
   depends_on "nettle"
 
   uses_from_macos "libedit"
