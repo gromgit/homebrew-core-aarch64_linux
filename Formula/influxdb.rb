c9c0740564999b1ce63f3d1eb8495e29c47abc79
Seeker <meaningseeking@protonmail.com>
2020-07-09 14:47:56 -0700

    influxdb 1.8.1
    Closes #57752.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/influxdb.rb
+++ Formula/influxdb.rb
@@ -2,8 +2,8 @@ class Influxdb < Formula
   desc "Time series, events, and metrics database"
   homepage "https://influxdata.com/time-series-platform/influxdb/"
   url "https://github.com/influxdata/influxdb.git",
-      :tag      => "v1.8.0",
-      :revision => "781490de48220d7695a05c29e5a36f550a4568f5"
+      :tag      => "v1.8.1",
+      :revision => "af0237819ab9c5997c1c0144862dc762b9d8fc25"
   license "MIT"
   head "https://github.com/influxdata/influxdb.git"
 
