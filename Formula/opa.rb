4a02be4fd749f4f73e73e1ddb98845cb405bc72d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-09 19:06:58 +0000

    opa: update 0.21.1 bottle.

--- Formula/opa.rb
+++ Formula/opa.rb
@@ -8,9 +8,9 @@ class Opa < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ad97bf77534ea5b72012148ec96e08700b1762b89f1222738e5c51f43c74f5d8" => :catalina
-    sha256 "8ef806fc00001368ac78a50df08024c0b52d76438b1f7963ab9d71e02c045b4f" => :mojave
-    sha256 "271f1584b341382fd5f9b5f601707d4fcb0efe96b66d82bf958221f091a287a9" => :high_sierra
+    sha256 "75c60b2042d8c6336450dcd439e743ef0602cf270e5db396f21bb1674f6d8138" => :catalina
+    sha256 "aab236e6b10b5a6cd1099cccb670c9320908bacaeb3a5500903d06fc5b9b6b1a" => :mojave
+    sha256 "53d3b23e6d966b4d0f4e8d742fadcbdbf6705d74e6d891567e9b00c9d1370aaf" => :high_sierra
   end
 
   depends_on "go" => :build
