5f4a588417d411e1af35103f4308680e6817b128
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-09 04:07:25 +0000

    ser2net: update 4.2.0 bottle.

--- Formula/ser2net.rb
+++ Formula/ser2net.rb
@@ -7,9 +7,9 @@ class Ser2net < Formula
 
   bottle do
     cellar :any
-    sha256 "c0133e4f6e044e683024ebf11fc81ceb6b38f19da808dea1b6aaa7219cfdf735" => :catalina
-    sha256 "575504b003d96ed94cc054507cebdb89d8db23a381ff325e308c87fb49187079" => :mojave
-    sha256 "3de3c5f9099400e4d4411c39ec33f959279c3d0c9ef03603f6b0128826070bcd" => :high_sierra
+    sha256 "1f7cee8eefe4625bd2c75f48f3b1386dd61379b6b81b21b57c1792b0a246a8f1" => :catalina
+    sha256 "f8a9cc43919eeae9ebbbfc91a5b463f2f95fe0f312724cc1555038a4ef423f93" => :mojave
+    sha256 "32df43b370d1da50357d2890c65f57c228a9352272b2ca462728183801db917f" => :high_sierra
   end
 
   depends_on "libyaml"
