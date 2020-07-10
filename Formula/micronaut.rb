fc064ce7e7a79899ac05b4e507ab44a7542319ce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-10 22:07:11 +0000

    micronaut: add 2.0.0 bottle.

--- Formula/micronaut.rb
+++ Formula/micronaut.rb
@@ -5,6 +5,13 @@ class Micronaut < Formula
   sha256 "5e30fc1878c9d7da9046745a5f71c611fc9bd1b765e32dd5e597a6fae1739f6c"
   license "Apache-2.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "77f97e9fe5a4923355cd9dad206ba0918ac7797d9895aa6627aff1453526d7ea" => :catalina
+    sha256 "44f688913894d10acbf14c11e7378b553b7a4305081b2889284c91ba515fd724" => :mojave
+    sha256 "9f328cf27f0c71fd9d1351e50c3ad80598fa86b060f5d0707d971e17eb3070d0" => :high_sierra
+  end
+
   depends_on "gradle" => :build
   depends_on "openjdk"
 
