0e80d2153708aaa182e3842c67f3988ba8e9ccac
Dustin Rodrigues <dust.rod@gmail.com>
2020-07-10 19:08:08 -0400

    re-flex 2.1.2
    Closes #57820.
    
    Signed-off-by: Claudia Pellegrino <1239874+claui@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/re-flex.rb
+++ Formula/re-flex.rb
@@ -1,8 +1,8 @@
 class ReFlex < Formula
   desc "The regex-centric, fast and flexible scanner generator for C++"
   homepage "https://www.genivia.com/doc/reflex/html"
-  url "https://github.com/Genivia/RE-flex/archive/v2.1.1.tar.gz"
-  sha256 "75994065b7a8aeec0c41f86afe7d33b571a73f124a001ad9a4bfe500641a9670"
+  url "https://github.com/Genivia/RE-flex/archive/v2.1.2.tar.gz"
+  sha256 "d5affd1b2368b5a14377089416b7357a16e8f4ff6738ed30c06018a3715fc270"
   license "BSD-3-Clause"
 
   bottle do
