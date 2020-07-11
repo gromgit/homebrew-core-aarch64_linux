7f2e83a85d7f1b88e72b23cf8f4578f7706039c0
Jonathan Chang <jchang641@gmail.com>
2020-07-11 22:07:42 +1000

    osi: add 0.108.6 bottle.

--- Formula/osi.rb
+++ Formula/osi.rb
@@ -5,6 +5,13 @@ class Osi < Formula
   sha256 "984a5886825e2da9bf44d8a665f4b92812f0700e451c12baf9883eaa2315fad5"
   license "EPL-1.0"
 
+  bottle do
+    cellar :any
+    sha256 "448319c96791abd303e976711176d74260d575b528c5c1229e1439fb85c295b9" => :catalina
+    sha256 "a21200a175b4d0a2208be0e34fb2dd64965133be04b30db7150cf55fe46093e4" => :mojave
+    sha256 "4fb8d7a49968da18a979df560dde2c8cae711c37ad722af8a0c20c3a7980134d" => :high_sierra
+  end
+
   depends_on "pkg-config" => :build
   depends_on "coinutils"
 
