45846c2a50ea21d3b04930f2b6b03b36badd2221
chenrui <chenrui333@gmail.com>
2020-07-11 14:08:53 +0000

    git-hooks-go: add 1.3.0 bottle.

--- Formula/git-hooks-go.rb
+++ Formula/git-hooks-go.rb
@@ -6,6 +6,13 @@ class GitHooksGo < Formula
   license "MIT"
   head "https://github.com/git-hooks/git-hooks.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "367a0e8d166a30749447132c88d5bf1c43fc8ab7c4316c91a990c13d4e887c32" => :catalina
+    sha256 "b61330cf67d4b8a572bb2f7a22434a00bb74d85bb93254ff6a60e8d3c8f12877" => :mojave
+    sha256 "95786772c28deeaaa6c979f93174e1f49bd6dd8370e8927861f6b950dd5b3910" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   conflicts_with "git-hooks", :because => "both install `git-hooks` binaries"
