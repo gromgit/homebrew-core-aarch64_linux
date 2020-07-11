eea195e40c9e521d048fc41bc6cdc29acd81144a
Seeker <meaningseeking@protonmail.com>
2020-07-10 22:48:17 -0700

    dosbox-x 0.83.3
    Closes #57829.
    
    Signed-off-by: Claudia Pellegrino <1239874+claui@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dosbox-x.rb
+++ Formula/dosbox-x.rb
@@ -1,8 +1,8 @@
 class DosboxX < Formula
   desc "DOSBox with accurate emulation and wide testing"
   homepage "https://dosbox-x.com/"
-  url "https://github.com/joncampbell123/dosbox-x/archive/dosbox-x-v0.83.2.tar.gz"
-  sha256 "87cfdf515b6c5a71afb3f5b1960d5cd323a0cce5643094713db3cf2ccb82b4bc"
+  url "https://github.com/joncampbell123/dosbox-x/archive/dosbox-x-v0.83.3.tar.gz"
+  sha256 "48f005949ada1ace8ad8c00bb27fad17d566e5bcdbec8be6078e44f8ad04759a"
   license "GPL-2.0"
   version_scheme 1
   head "https://github.com/joncampbell123/dosbox-x.git"
@@ -19,6 +19,13 @@ class DosboxX < Formula
   depends_on "pkg-config" => :build
   depends_on "fluid-synth"
   depends_on :macos => :high_sierra # needs futimens
+
+  # Remove with upstream release > 0.83.3
+  # https://github.com/joncampbell123/dosbox-x/pull/1715
+  patch do
+    url "https://github.com/joncampbell123/dosbox-x/commit/a8be3fcda5f91d8cff9f792b366cc05ad75eaef0.patch?full_index=1"
+    sha256 "39c96ae37b58a1410b0dc9cdc8b9c9bb8c55792395b2b049bebf3cb4c8838d20"
+  end
 
   def install
     ENV.cxx11
