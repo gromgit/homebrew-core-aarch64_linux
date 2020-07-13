78ed8709eb17b2f32673bc6beca35a3c92b11df3
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-07-13 08:44:44 +0000

    arrayfire: add 3.7.2 bottle.

--- Formula/arrayfire.rb
+++ Formula/arrayfire.rb
@@ -5,6 +5,13 @@ class Arrayfire < Formula
   sha256 "3f28c6c079fc0adffe2f7f62fb09f96a5e2143f6a73a3597c1b2022fbfdee30a"
   license "BSD-3-Clause"
 
+  bottle do
+    cellar :any
+    sha256 "ee4a83613f9c2515a0b59a19bf83913c6f8f81646e5e0d024d2850508cc6925c" => :catalina
+    sha256 "20ffb2185bdca09c2797186c17c7e89146b367c9c5c95f0602dfc02e4f14bb16" => :mojave
+    sha256 "e0b0931974ffa870ec3e48f712aa0eaaaead6880214ba158f94215f70a2f2241" => :high_sierra
+  end
+
   depends_on "boost" => :build
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
