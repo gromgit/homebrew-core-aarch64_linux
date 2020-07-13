4273639c15f2a4a22717c209b06dab8a18c9f217
Seeker <meaningseeking@protonmail.com>
2020-07-12 21:57:22 -0700

    cubejs-cli 0.19.48 (new formula)
    Closes #57887.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/cubejs-cli.rb
@@ -0,0 +1,22 @@
+require "language/node"
+
+class CubejsCli < Formula
+  desc "Cube.js command-line interface"
+  homepage "https://cube.dev/"
+  url "https://registry.npmjs.org/cubejs-cli/-/cubejs-cli-0.19.48.tgz"
+  sha256 "94ccb156e453cbdbab0dca83ce0870cb41ac35d5b5d971846c565bc3a7b25eca"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/cubejs --version")
+    system "cubejs", "create", "hello-world", "-d", "postgres"
+    assert_predicate testpath/"hello-world/schema/Orders.js", :exist?
+  end
+end
