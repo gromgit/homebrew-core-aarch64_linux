1015fc907103f5dd4518b2b6fd263dc22145f8d9
Seeker <meaningseeking@protonmail.com>
2020-07-13 10:20:56 -0700

    bazel 3.4.0
    Closes #57916.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bazel.rb
+++ Formula/bazel.rb
@@ -1,8 +1,8 @@
 class Bazel < Formula
   desc "Google's own build tool"
   homepage "https://bazel.build/"
-  url "https://github.com/bazelbuild/bazel/releases/download/3.3.1/bazel-3.3.1-dist.zip"
-  sha256 "e0f1f43c65c4e0a38522b37e81f6129d8a1f7cd3d8884847be306544a7492747"
+  url "https://github.com/bazelbuild/bazel/releases/download/3.4.0/bazel-3.4.0-dist.zip"
+  sha256 "7583abf8905ba9dd5394294e815e8873635ac4e5067e63392e8a33b397e450d8"
   license "Apache-2.0"
 
   bottle do
