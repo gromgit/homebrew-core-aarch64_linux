8b3ad40f64c1b40f6e98e04f4ebc7603a66d024d
Seeker <meaningseeking@protonmail.com>
2020-07-13 11:48:50 -0700

    marked 1.1.0 (new formula)
    Closes #57923.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

---
+++ Formula/marked.rb
@@ -0,0 +1,20 @@
+require "language/node"
+
+class Marked < Formula
+  desc "Markdown parser and compiler built for speed"
+  homepage "https://marked.js.org/"
+  url "https://registry.npmjs.org/marked/-/marked-1.1.0.tgz"
+  sha256 "6817b49595f95b8cebe5ecd6e7ea1c3d57c334458653285f391edd9c0352a9f7"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_equal "<p>hello <em>world</em></p>", pipe_output("#{bin}/marked", "hello *world*").strip
+  end
+end
