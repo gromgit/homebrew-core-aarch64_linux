96877fd07138828a5ffe36bd504d98e89ebc209a
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-07-13 11:46:30 +0000

    cubejs-cli: add 0.19.48 bottle.

--- Formula/cubejs-cli.rb
+++ Formula/cubejs-cli.rb
@@ -7,6 +7,13 @@ class CubejsCli < Formula
   sha256 "94ccb156e453cbdbab0dca83ce0870cb41ac35d5b5d971846c565bc3a7b25eca"
   license "Apache-2.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "28956e5b0af37c95adea0dea763486bd616824bc279a4dc285c0b9eedea29c60" => :catalina
+    sha256 "024a14d3785b779ddef050f5f1fa8b1deddce9717647957f89d62c49042022aa" => :mojave
+    sha256 "bf23e7ee8d73238b3d6c2edd3a7cfecbe3e5b32426748debf12f36741269edaf" => :high_sierra
+  end
+
   depends_on "node"
 
   def install
