7978b49c8cba439ee669cc8a2fed8cf6bd8a8399
Josh Holtz <josh@rokkincat.com>
2020-07-13 22:24:31 -0500

    fastlane 2.152.0
    Closes #57939.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fastlane.rb
+++ Formula/fastlane.rb
@@ -1,8 +1,8 @@
 class Fastlane < Formula
   desc "Easiest way to build and release mobile apps"
   homepage "https://fastlane.tools"
-  url "https://github.com/fastlane/fastlane/archive/2.151.2.tar.gz"
-  sha256 "476b03ba4245c8ab2b2589fdc2ac92d8437b9fe8fa4f70f42a549063035eae8c"
+  url "https://github.com/fastlane/fastlane/archive/2.152.0.tar.gz"
+  sha256 "cf35921559f924bfee538eb14bbd04811e54e07b692042a3699dede76e1e57db"
   license "MIT"
   head "https://github.com/fastlane/fastlane.git"
 
