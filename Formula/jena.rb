11253478a7a8f2b7472494e86fc01f308b53570a
Seeker <meaningseeking@protonmail.com>
2020-07-13 20:29:12 -0700

    jena 3.16.0
    Closes #57940.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jena.rb
+++ Formula/jena.rb
@@ -1,9 +1,9 @@
 class Jena < Formula
   desc "Framework for building semantic web and linked data apps"
   homepage "https://jena.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-3.15.0.tar.gz"
-  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-3.15.0.tar.gz"
-  sha256 "6e50f7426d7d607db93ded55e8f6d3b928fedf5cfc80a4b56be646bb45f42a9d"
+  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-3.16.0.tar.gz"
+  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-3.16.0.tar.gz"
+  sha256 "6ef65ab3e24948f6f8fa97281a936276d74a8732fb4a14c624c1aa9fa93adb30"
 
   bottle :unneeded
 
