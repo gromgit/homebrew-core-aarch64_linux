68ad8f165755e9dce0c96949f13907eb35f7f1fa
Dustin Rodrigues <dust.rod@gmail.com>
2020-07-13 20:06:37 -0400

    rbenv-vars: add license

--- Formula/rbenv-vars.rb
+++ Formula/rbenv-vars.rb
@@ -3,6 +3,7 @@ class RbenvVars < Formula
   homepage "https://github.com/sstephenson/rbenv-vars"
   url "https://github.com/sstephenson/rbenv-vars/archive/v1.2.0.tar.gz"
   sha256 "9e6a5726aad13d739456d887a43c220ba9198e672b32536d41e884c0a54b4ddb"
+  license "MIT"
   revision 1
   head "https://github.com/sstephenson/rbenv-vars.git"
 
