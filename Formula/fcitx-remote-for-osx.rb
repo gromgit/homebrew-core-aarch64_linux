95d14bb864300aa20b49e31313ba088cc57969db
Dustin Rodrigues <dust.rod@gmail.com>
2020-07-13 20:03:55 -0400

    fcitx-remote-for-osx: add license

--- Formula/fcitx-remote-for-osx.rb
+++ Formula/fcitx-remote-for-osx.rb
@@ -3,6 +3,7 @@ class FcitxRemoteForOsx < Formula
   homepage "https://github.com/CodeFalling/fcitx-remote-for-osx"
   url "https://github.com/CodeFalling/fcitx-remote-for-osx/archive/0.3.0.tar.gz"
   sha256 "b4490a6a0db3c28ce3ddbe89dd038f5ab404744539adc5520eab1a1a39819de6"
+  license "GPL-3.0"
 
   bottle do
     cellar :any_skip_relocation
