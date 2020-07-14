1c2b4a5822e333ba9a528345f0ab189af7f66659
Dustin Rodrigues <dust.rod@gmail.com>
2020-07-13 20:06:26 -0400

    protobuf-swift: add license

--- Formula/protobuf-swift.rb
+++ Formula/protobuf-swift.rb
@@ -3,6 +3,7 @@ class ProtobufSwift < Formula
   homepage "https://github.com/alexeyxo/protobuf-swift"
   url "https://github.com/alexeyxo/protobuf-swift/archive/4.0.6.tar.gz"
   sha256 "598d9e459b4ac74bfbcf22857c7e8fda8f5219c10caac0aa18aea7d8710cce22"
+  license "Apache-2.0"
   revision 2
 
   bottle do
