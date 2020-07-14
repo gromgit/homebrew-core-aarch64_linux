8496e7a9688f4edb8d331020cbb6150224e78f6e
Dustin Rodrigues <dust.rod@gmail.com>
2020-07-13 20:04:08 -0400

    gdub: add license

--- Formula/gdub.rb
+++ Formula/gdub.rb
@@ -3,6 +3,7 @@ class Gdub < Formula
   homepage "https://www.gdub.rocks/"
   url "https://github.com/dougborg/gdub/archive/v0.2.0.tar.gz"
   sha256 "aa3da76752b597e60094a67971f35dfe20377390d21b3ae3b45b7b7040e9a268"
+  license "MIT"
 
   bottle :unneeded
 
