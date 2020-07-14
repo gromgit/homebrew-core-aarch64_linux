d111c0277354fe7dd26a5806b155f4f172c7541f
Dustin Rodrigues <dust.rod@gmail.com>
2020-07-13 20:05:33 -0400

    miruo: add license

--- Formula/miruo.rb
+++ Formula/miruo.rb
@@ -4,6 +4,7 @@ class Miruo < Formula
   url "https://github.com/KLab/miruo/archive/0.9.6b.tar.gz"
   version "0.9.6b"
   sha256 "0b31a5bde5b0e92a245611a8e671cec3d330686316691daeb1de76360d2fa5f1"
+  license "GPL-3.0"
 
   bottle do
     cellar :any_skip_relocation
