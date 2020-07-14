52b95c0dfa7069d497122e78f05d2844d2fe2a43
Dustin Rodrigues <dust.rod@gmail.com>
2020-07-13 20:07:23 -0400

    unoconv: add license

--- Formula/unoconv.rb
+++ Formula/unoconv.rb
@@ -3,6 +3,7 @@ class Unoconv < Formula
   homepage "http://dag.wiee.rs/home-made/unoconv/"
   url "https://files.pythonhosted.org/packages/ab/40/b4cab1140087f3f07b2f6d7cb9ca1c14b9bdbb525d2d83a3b29c924fe9ae/unoconv-0.9.0.tar.gz"
   sha256 "308ebfd98e67d898834876348b27caf41470cd853fbe2681cc7dacd8fd5e6031"
+  license "GPL-2.0"
   revision 1
   head "https://github.com/dagwieers/unoconv.git"
 
