7fba9196bf26c6fdbde7235126d314de8a434e22
Dustin Rodrigues <dust.rod@gmail.com>
2020-07-13 20:05:09 -0400

    libimobiledevice: add license

--- Formula/libimobiledevice.rb
+++ Formula/libimobiledevice.rb
@@ -3,6 +3,7 @@ class Libimobiledevice < Formula
   homepage "https://www.libimobiledevice.org/"
   url "https://github.com/libimobiledevice/libimobiledevice/releases/download/1.3.0/libimobiledevice-1.3.0.tar.bz2"
   sha256 "53f2640c6365cd9f302a6248f531822dc94a6cced3f17128d4479a77bd75b0f6"
+  license "LGPL-2.1"
 
   bottle do
     cellar :any
