089f727376666199d30e6850cc9dc3569dfb4648
Dustin Rodrigues <dust.rod@gmail.com>
2020-07-13 20:06:58 -0400

    sjk: add license

--- Formula/sjk.rb
+++ Formula/sjk.rb
@@ -3,6 +3,7 @@ class Sjk < Formula
   homepage "https://github.com/aragozin/jvm-tools"
   url "https://search.maven.org/remotecontent?filepath=org/gridkit/jvmtool/sjk-plus/0.16/sjk-plus-0.16.jar"
   sha256 "306c40fbf76b9cb9a09b394116b8a2614b0d9acb3725e845d2a754686a6d3d49"
+  license "Apache-2.0"
 
   bottle :unneeded
 
