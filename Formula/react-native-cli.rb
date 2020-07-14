f705ca0ffe189e4745822ed0fa0846a91d609d42
Dustin Rodrigues <dust.rod@gmail.com>
2020-07-14 10:03:26 -0400

    react-native-cli: add license

--- Formula/react-native-cli.rb
+++ Formula/react-native-cli.rb
@@ -5,6 +5,7 @@ class ReactNativeCli < Formula
   homepage "https://facebook.github.io/react-native/"
   url "https://registry.npmjs.org/react-native-cli/-/react-native-cli-2.0.1.tgz"
   sha256 "f1039232c86c29fa0b0c85ad2bfe0ff455c3d3cd9af9d9ddb8e9c560231a8322"
+  license "BSD-3-Clause"
 
   bottle do
     cellar :any_skip_relocation
