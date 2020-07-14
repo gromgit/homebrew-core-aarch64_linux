4764db72062e5cb3d6ef4a015ffa987644dc21fb
Dustin Rodrigues <dust.rod@gmail.com>
2020-07-13 20:03:48 -0400

    eject: add license

--- Formula/eject.rb
+++ Formula/eject.rb
@@ -3,6 +3,7 @@ class Eject < Formula
   homepage "https://github.com/Raizlabs/Eject"
   url "https://github.com/Raizlabs/Eject/archive/0.1.27.tar.gz"
   sha256 "b4aa8d281074074632422e9e8583d50024f1b2712566fae7950e73f751f75791"
+  license "MIT"
 
   bottle do
     cellar :any
