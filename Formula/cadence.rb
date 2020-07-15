44c524cb4cdecee669b076a92d1502327fcfd16a
Seeker <meaningseeking@protonmail.com>
2020-07-14 18:41:03 -0700

    cadence 0.6.0 (new formula)
    Closes #57981.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

---
+++ Formula/cadence.rb
@@ -0,0 +1,23 @@
+class Cadence < Formula
+  desc "Resource-oriented smart contract programming language"
+  homepage "https://github.com/onflow/cadence"
+  url "https://github.com/onflow/cadence/archive/v0.6.0.tar.gz"
+  sha256 "2e18cb91308794065a0242ca4c15d7584da9e6a08f78072c970f51db3a470412"
+  license "Apache-2.0"
+  head "https://github.com/onflow/cadence.git"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "./runtime/cmd/main"
+  end
+
+  test do
+    (testpath/"hello.cdc").write <<~EOS
+      pub fun main() {
+        log("Hello, world!")
+      }
+    EOS
+    assert_match "Hello, world!", shell_output("#{bin}/cadence hello.cdc")
+  end
+end
