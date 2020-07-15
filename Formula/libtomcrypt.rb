ffb59d6b7f95c1b8713bfcab4a8545eaeb67aa6b
Dustin Rodrigues <dust.rod@gmail.com>
2020-07-14 23:36:46 -0400

    libtomcrypt: add license (#57985)

--- Formula/libtomcrypt.rb
+++ Formula/libtomcrypt.rb
@@ -3,6 +3,7 @@ class Libtomcrypt < Formula
   homepage "https://www.libtom.net/"
   url "https://github.com/libtom/libtomcrypt/archive/v1.18.2.tar.gz"
   sha256 "d870fad1e31cb787c85161a8894abb9d7283c2a654a9d3d4c6d45a1eba59952c"
+  license "Unlicense"
 
   bottle do
     cellar :any_skip_relocation
