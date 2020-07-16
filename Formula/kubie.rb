d95e0c893c1e60716e2f5a31bbb57b18877af84f
chenrui <chenrui333@gmail.com>
2020-07-16 20:23:36 +0000

    kubie: add 0.9.1 bottle.

--- Formula/kubie.rb
+++ Formula/kubie.rb
@@ -5,6 +5,13 @@ class Kubie < Formula
   sha256 "8f7926b1bcd0421247231353b6969c36498add3b88b55103349df3da763a5f42"
   license "Zlib"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "955b6ae1436406d41e9ef9ffe6df4ac571d8259becf59b51c71d938367c826a4" => :catalina
+    sha256 "3f000617f7175dbc8df85d68b8720863fec9c1f2bae2a49d5d636feb856a3a0c" => :mojave
+    sha256 "43e175dfd87c5f3b1dad37d2588b4063d8e1f57c16d11d84ed5d901dd948f94f" => :high_sierra
+  end
+
   depends_on "rust" => :build
   depends_on "kubernetes-cli" => :test
 
