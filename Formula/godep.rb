e5e564de23e0cfcb2cdfe9219567bf108561932a
Bo Anderson <mail@boanderson.me>
2020-07-16 03:24:31 +0100

    godep: update 80_35 bottle.

--- Formula/godep.rb
+++ Formula/godep.rb
@@ -8,9 +8,9 @@ class Godep < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b75f52e4b6ff4f29c6ee086b14e973595408edcba02027acf47206a6b1cdf748" => :catalina
-    sha256 "65fac648f744fe78ac382e7dda5cdc1774be69d83c77a0c07eb34493cef8fe48" => :mojave
-    sha256 "d8125aec91095023ff641181d442dd10c303b7e0b618c5d1a93c471202ccb087" => :high_sierra
+    sha256 "68307bb76b9fc3caad85bf857edd1c3eb243133c6f1dd6fa8410fe3031e3513a" => :catalina
+    sha256 "66795c1166983b401a9fd93f54a67d0f61e26d435e02fe4086ee9b8ee24ef8ba" => :mojave
+    sha256 "b06ff82b8a44be0f6e2d67950d54f763e6af52d8fd15d99bce96b5d5a4f63740" => :high_sierra
   end
 
   depends_on "go"
