d3531edb57180129f2e3658c5df46c5aa0ef7ffb
chenrui <chenrui333@gmail.com>
2020-07-16 15:51:35 +0000

    subfinder: add 2.3.6 bottle.

--- Formula/subfinder.rb
+++ Formula/subfinder.rb
@@ -6,6 +6,13 @@ class Subfinder < Formula
   license "MIT"
   head "https://github.com/projectdiscovery/subfinder.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "09dc0347f7b7cc454903abcea0e962289d4dd11c5a4bb067c8c454855d9db25d" => :catalina
+    sha256 "0820be0f2658e42671c63d223c780c61eb49bbf246455d382d89de7c615b0bea" => :mojave
+    sha256 "b18f4004c01b54e5c0f0c07ec5efbfe581cdef10e8fe72ffe8968284579e5052" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
