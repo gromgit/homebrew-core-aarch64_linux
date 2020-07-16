025ecdf12d94378aa7f02fb352d984aff6b5f383
SeekingMeaning <meaningseeking@protonmail.com>
2020-07-15 19:01:07 -0700

    ckan 1.28.0 (#58027)

--- Formula/ckan.rb
+++ Formula/ckan.rb
@@ -1,8 +1,8 @@
 class Ckan < Formula
   desc "The Comprehensive Kerbal Archive Network"
   homepage "https://github.com/KSP-CKAN/CKAN/"
-  url "https://github.com/KSP-CKAN/CKAN/releases/download/v1.27.2/ckan.exe"
-  sha256 "57d487f8a81ed225eacc46b37a53cb3a8ff44057d6557b68e132b6669697d1af"
+  url "https://github.com/KSP-CKAN/CKAN/releases/download/v1.28.0/ckan.exe"
+  sha256 "2486d98e06c247a61a94e583c202b8a4c74a439de3ea271b769d041aa78d21cd"
 
   bottle :unneeded
 
