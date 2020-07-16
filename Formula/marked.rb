f7da36693b2f9be20a6390e7d5cf2a73fce95838
chenrui <chenrui333@gmail.com>
2020-07-16 04:14:23 +0000

    marked: add 1.1.0 bottle.

--- Formula/marked.rb
+++ Formula/marked.rb
@@ -7,6 +7,13 @@ class Marked < Formula
   sha256 "6817b49595f95b8cebe5ecd6e7ea1c3d57c334458653285f391edd9c0352a9f7"
   license "MIT"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9202c583c6447cf7d8d9f460cba10c66840dcb1adca730fad1b4acadff81fb17" => :catalina
+    sha256 "696faee9ba51edb9ed10f85fac1473c4db82969674768c02a94e434d6275374a" => :mojave
+    sha256 "7f311a0a5ec8ca62308b4e8ad3403313621156d0079c3aff04c56670097a6236" => :high_sierra
+  end
+
   depends_on "node"
 
   def install
