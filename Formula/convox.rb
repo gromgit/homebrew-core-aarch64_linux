406e64e7654b2dce03e37d4e71bd79a0e0d18f80
Seeker <meaningseeking@protonmail.com>
2020-07-15 20:42:29 -0700

    convox 3.0.31
    Closes #58049.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -1,8 +1,8 @@
 class Convox < Formula
   desc "Command-line interface for the Convox PaaS"
   homepage "https://convox.com/"
-  url "https://github.com/convox/convox/archive/3.0.29.tar.gz"
-  sha256 "31c74af451d68c14a0a927f96afb81dbe937d4971e3974484578109dc14a9f81"
+  url "https://github.com/convox/convox/archive/3.0.31.tar.gz"
+  sha256 "804da820f9cd078426ea29b0b8d95f3ff563913451b2f9ceb908b32ceaeaf885"
   license "Apache-2.0"
   version_scheme 1
 
