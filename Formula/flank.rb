4bf57385aff172c214e82c5046b449389e8d0b24
SeekingMeaning <meaningseeking@protonmail.com>
2020-07-15 20:21:58 -0700

    flank 20.07.0 (#58029)

--- Formula/flank.rb
+++ Formula/flank.rb
@@ -1,8 +1,8 @@
 class Flank < Formula
   desc "Massively parallel Android and iOS test runner for Firebase Test Lab"
   homepage "https://github.com/Flank/flank"
-  url "https://github.com/Flank/flank/releases/download/v20.06.2/flank.jar"
-  sha256 "2aa7e5e5fc83396fa95a2f41341a39d07ee289289e67bdfea874fd5e521777b9"
+  url "https://github.com/Flank/flank/releases/download/v20.07.0/flank.jar"
+  sha256 "d3a99fdaaa3000b4346201a7fed18afc2c8c5309c5ffa442a647a96a08cea21d"
   license "Apache-2.0"
 
   bottle :unneeded
