4123dac50ed55ff2666294eade9a496b616b34a3
chenrui <chenrui333@gmail.com>
2020-07-16 15:54:00 +0000

    cadence: add 0.6.0 bottle.

--- Formula/cadence.rb
+++ Formula/cadence.rb
@@ -6,6 +6,13 @@ class Cadence < Formula
   license "Apache-2.0"
   head "https://github.com/onflow/cadence.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c7fe58786e0356835ecb38655cb743c7f62707c9e9b810293a5d34e5bf3a79dc" => :catalina
+    sha256 "c7fe58786e0356835ecb38655cb743c7f62707c9e9b810293a5d34e5bf3a79dc" => :mojave
+    sha256 "c7fe58786e0356835ecb38655cb743c7f62707c9e9b810293a5d34e5bf3a79dc" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
