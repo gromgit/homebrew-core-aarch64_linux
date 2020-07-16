e206aa9b09606721427c3e2438fecf1cffcb0345
Sam Ford <1584702+samford@users.noreply.github.com>
2020-07-16 03:53:41 +0000

    recoverjpeg: update 2.6.3 bottle.

--- Formula/recoverjpeg.rb
+++ Formula/recoverjpeg.rb
@@ -7,10 +7,10 @@ class Recoverjpeg < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7b9e6cc406462435f7efc7bf6686205f29beba8bc5f481c6bf754a53d37f9411" => :catalina
-    sha256 "89bd9fe522bc64508a3c2925edda2a3ae3cfc32e7ff59f7e70eb1069352129b2" => :mojave
-    sha256 "ce460e293cc4c4c5bf3650cf9860e7b06654017473c414dc88c1df9d82e8466e" => :high_sierra
-    sha256 "453b0d2c88be1b885407e38900ab0303481e9957ed5c160cb8e6456b6a2f81c2" => :sierra
+    rebuild 1
+    sha256 "87b3d9adf8b59d91350b7e655a78b68525caaaad0a614c5b7e1b6097d29cf6d9" => :catalina
+    sha256 "0f424efc21d5e07c2cdce7a870e28ee1aea42ac8f65f12eb5a845895c49ed958" => :mojave
+    sha256 "5366edde2383098f7ee4ac866d0d2ff528efbf63af934dd469c3b8e6739678ed" => :high_sierra
   end
 
   def install
