9a6e940a3df05329ada571f77d08b41d37d492db
Mislav Marohnić <mislav@github.com>
2020-07-16 20:27:21 +0200

    gh 0.11.0
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #58094.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>

--- Formula/gh.rb
+++ Formula/gh.rb
@@ -1,8 +1,8 @@
 class Gh < Formula
   desc "GitHub command-line tool"
   homepage "https://github.com/cli/cli"
-  url "https://github.com/cli/cli/archive/v0.10.1.tar.gz"
-  sha256 "5265f9594cca6c4c2f0d573c810f4ddb16e3baed0c18b18a353529506fc41297"
+  url "https://github.com/cli/cli/archive/v0.11.0.tar.gz"
+  sha256 "e2137491f269df179ded7066f641ae7879a40870be53170a0388310e904158b0"
   license "MIT"
 
   bottle do
@@ -20,7 +20,7 @@ class Gh < Formula
       -X github.com/cli/cli/command.BuildDate=#{Date.today}
       -s -w
     ]
-    system "make", "bin/gh", "manpages", "LDFLAGS=#{ldflags.join(" ")}"
+    system "make", "bin/gh", "manpages", "GO_LDFLAGS=#{ldflags.join(" ")}"
     bin.install "bin/gh"
     man1.install Dir["share/man/man1/gh*.1"]
     (bash_completion/"gh").write `#{bin}/gh completion -s bash`
