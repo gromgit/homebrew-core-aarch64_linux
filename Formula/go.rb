8c9d7544bef93d770989a8ec57db04c93a4c126b
Bo Anderson <mail@boanderson.me>
2020-07-16 03:24:30 +0100

    go: update 1.14.5 bottle.

--- Formula/go.rb
+++ Formula/go.rb
@@ -16,9 +16,9 @@ class Go < Formula
   end
 
   bottle do
-    sha256 "935214ab166d8da148af812614545a25778e4ee11c2ea5cfd332bff72ec4046e" => :catalina
-    sha256 "f1ef93181b0c4c97617b74d787ced8e18ceca12ee52d9559a8db0f21eb84fa43" => :mojave
-    sha256 "4b5e79d15f4bb768bc0cefe54a670a525f21257f2532697df76d44c8cc0e8a7a" => :high_sierra
+    sha256 "bbecee76043eeb6d34f76565faaa856a9bbfb2dd052d63dcd5c6326059ab7898" => :catalina
+    sha256 "d653d2c8faf8b8c0e2152082e7d9cbfbae0d46c1c66f04d58517234d93bbe39e" => :mojave
+    sha256 "edd810c7103a4080220cc929820f1cdd56c8dc0c11cab53f5738d37532750e24" => :high_sierra
   end
 
   head do
