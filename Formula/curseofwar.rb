f149cd3cfb0b85610b5180c14b2bc0caeefaa9e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-17 08:08:12 +0000

    curseofwar: update 1.3.0 bottle.

--- Formula/curseofwar.rb
+++ Formula/curseofwar.rb
@@ -8,12 +8,9 @@ class Curseofwar < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "92c8a1650e5e18866211a79499abad640ff8ce8f49b54c50043e7f421d8d8e47" => :catalina
-    sha256 "4ce2eaede3f4636c0ddb7a16d65ae6b750c9e20342715841e5cacefd3d61b0b7" => :mojave
-    sha256 "b43f143f9fe919e2d88b241865513967054550a852fd62747ed759ac1a53500b" => :high_sierra
-    sha256 "8a19536dcb6b4a3f508c800980d6300399d1b9d7af1683df7ea7f4dd502cf238" => :sierra
-    sha256 "014a448d16cef523e97f898c952a74b08b379569f22d951189e90c9ee5056c11" => :el_capitan
-    sha256 "6b1368e3d4875b19c9817c413037edbeaf49120519032b725207079b86913c8a" => :yosemite
+    sha256 "ddd5726a8951c2ec18c9f26bbed80d2d22baeef02eb6e1f313d4591f0db7064b" => :catalina
+    sha256 "5847323530aec077f4a17d4c4eb78ee0f90499940dbce3608aba6d4f39e3719e" => :mojave
+    sha256 "b2a0646e145b7ef8f502b6f544d106c05c90974c0f8972285a5dfa753305eece" => :high_sierra
   end
 
   def install
