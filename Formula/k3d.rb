042dcab8f8227248b67e4d97a59a14ac43019eb0
Thorsten Klein <iwilltry42@gmail.com>
2020-07-17 19:27:42 +0200

    k3d 3.0.0
    Closes #58163.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/k3d.rb
+++ Formula/k3d.rb
@@ -1,10 +1,9 @@
 class K3d < Formula
   desc "Little helper to run Rancher Lab's k3s in Docker"
   homepage "https://github.com/rancher/k3d"
-  url "https://github.com/rancher/k3d/archive/v1.7.0.tar.gz"
-  sha256 "e741809eb27f707c0f22c19a41ebbd6be7c20ec275285bb12bbf437a675aafb7"
+  url "https://github.com/rancher/k3d/archive/v3.0.0.tar.gz"
+  sha256 "939fae09600ae7edb5e92ecab5c25ac2adec5be432c8a7ee34a14e01a0245b11"
   license "MIT"
-  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -18,18 +17,19 @@ class K3d < Formula
   def install
     system "go", "build",
            "-mod", "vendor",
-           "-ldflags", "-s -w -X github.com/rancher/k3d/version.Version=v#{version}",
+           "-ldflags", "-s -w -X github.com/rancher/k3d/v3/version.Version=v#{version}"\
+           " -X github.com/rancher/k3d/v3/version.K3sVersion=latest",
            "-trimpath", "-o", bin/"k3d"
     prefix.install_metafiles
   end
 
   test do
-    assert_match "k3d version v#{version}", shell_output("#{bin}/k3d --version")
-    code = if File.exist?("/var/run/docker.sock")
-      0
-    else
-      1
-    end
-    assert_match "Checking docker...", shell_output("#{bin}/k3d check-tools 2>&1", code)
+    assert_match "k3d version v#{version}\nk3s version latest (default)", shell_output("#{bin}/k3d --version")
+    # Either docker is not present or it is, where the command will fail in the first case.
+    # In any case I wouldn't expect a cluster with name 6d6de430dbd8080d690758a4b5d57c86 to be present
+    # (which is the md5sum of 'homebrew-failing-test')
+    output = shell_output("#{bin}/k3d cluster get 6d6de430dbd8080d690758a4b5d57c86 2>&1", 1).split("\n").pop
+    assert_match output,
+      "\x1B\[31mFATA\x1B\[0m\[0000\]\ No\ nodes\ found\ for\ cluster\ '6d6de430dbd8080d690758a4b5d57c86'\ "
   end
 end
