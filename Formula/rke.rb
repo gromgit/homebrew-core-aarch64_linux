f7f21444c6d448906ae1364905ce1b4f6d63b502
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-17 08:08:05 +0000

    rke: update 1.1.4 bottle.

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -8,9 +8,9 @@ class Rke < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9e32789bb442855f6012aa4b96a3d67ee315cdf0b8b851ceaa59a955f372e9ba" => :catalina
-    sha256 "267e5bee1bc54be1c0f41336dac4b59f309fcf6cce836e0308c6c775eb4530eb" => :mojave
-    sha256 "f98a5dbb85122f5dc8f0e33a377359b323b36447f9976eed94da0e0998f7a057" => :high_sierra
+    sha256 "d6bc20bbddf5f0a031a25f5928cbec0a3181c6c2cd100f819de4f0b511033b8c" => :catalina
+    sha256 "c6629fd7706fecb4c0309fdd36d9c1b04dd08f6767659c3f05095321e36df4ac" => :mojave
+    sha256 "c12e24a66c2f6048d06d0428865b7e2f872c7ff7769fe163b9091d6ada1e1e62" => :high_sierra
   end
 
   depends_on "go" => :build
