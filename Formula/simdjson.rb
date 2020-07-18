3f77fa50f9b1e564edb131783686785ebb5169d9
Caleb Xu <calebcenter@live.com>
2020-07-18 00:18:24 -0400

    simdjson 0.4.7 (new formula)
    Closes #58172.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

---
+++ Formula/simdjson.rb
@@ -0,0 +1,36 @@
+class Simdjson < Formula
+  desc "SIMD-accelerated C++ JSON parser"
+  homepage "https://simdjson.org"
+  url "https://github.com/simdjson/simdjson/archive/v0.4.7.tar.gz"
+  sha256 "44c20aa1000a8ed67ed6d541048aa7e5ea26a3bcb833a7232310414fc46b3ef9"
+  license "Apache-2.0"
+  head "https://github.com/simdjson/simdjson.git"
+
+  depends_on "cmake" => :build
+
+  def install
+    args = std_cmake_args + ["-DSIMDJSON_JUST_LIBRARY=ON"]
+    system "cmake", ".", *args
+    system "make", "install"
+    system "make", "clean"
+    system "cmake", ".", *args, "-DSIMDJSON_BUILD_STATIC=ON"
+    system "make"
+    lib.install "src/libsimdjson.a"
+  end
+
+  test do
+    (testpath/"test.json").write "{\"name\":\"Homebrew\",\"isNull\":null}"
+    (testpath/"test.cpp").write <<~EOS
+      #include <simdjson.h>
+      int main(void) {
+        simdjson::dom::parser parser;
+        simdjson::dom::element json = parser.load("test.json");
+        std::cout << json["name"] << std::endl;
+      }
+    EOS
+
+    system ENV.cxx, "test.cpp", "-std=c++11",
+           "-I#{include}", "-L#{lib}", "-lsimdjson", "-o", "test"
+    assert_equal "\"Homebrew\"\n", shell_output("./test")
+  end
+end
