f0cc3ed2388922ec19087193ecce68f6118e7fab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-18 19:07:12 +0000

    diffoscope: update 152 bottle.

--- Formula/diffoscope.rb
+++ Formula/diffoscope.rb
@@ -9,9 +9,9 @@ class Diffoscope < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "23942df2f56b437775f410b81e4d564f04da23346cb45e34178b52548d300b89" => :catalina
-    sha256 "9c71e1e73327a2a3c9b6eefe8d6dad352c7f1c5d700722f73aa269a46bbe867a" => :mojave
-    sha256 "47edb1a23384d0d25b9c541e9da84787f14516a43c8b6dafb74478a57ca803ee" => :high_sierra
+    sha256 "5e5235d4aec8575aebd2df7e89810b8a0ffb99cd83285df43e2f0ee0c780d87f" => :catalina
+    sha256 "85534cf70b5531a8576f55ad0569e7b117eb2295fed8593fc195184949eb5475" => :mojave
+    sha256 "cb47986cb75b6d9bf7853d401ffa87179630bb3b15b8847d3da9fa3a5a3bc7cd" => :high_sierra
   end
 
   depends_on "gnu-tar"
