bd6f3610af4609023950d4f109c5022a4efaab16
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-18 22:06:28 +0000

    midnight-commander: update 4.8.25 bottle.

--- Formula/midnight-commander.rb
+++ Formula/midnight-commander.rb
@@ -5,9 +5,9 @@ class MidnightCommander < Formula
   sha256 "ffc19617f20ebb23330acd3998b7fd559a042d172fa55746d53d246697b2548a"
 
   bottle do
-    sha256 "c6adcb70e949c89ba12ba91fffb89ad00c55e8c3a063ae6d01954a02a84512f2" => :catalina
-    sha256 "2e3e95bd852f0edd7069b09ff24e897e94bb495f3b852230f7cc3400acfc2d9a" => :mojave
-    sha256 "2e888d8d8cec7a0c881d0df09f5662080494f89e1871fbcccf30e9a0cc18aa1b" => :high_sierra
+    sha256 "2c36f252c47b8ecff2fa4afb4191a963af7c3d30a8aeb267a40f967873a01643" => :catalina
+    sha256 "224d6aa6577e51952833ee65888bb99eacb89508dc9ac2f82a0e679b4635d7e3" => :mojave
+    sha256 "79c2208b2097941cf3a792f47ad1f280ddbc3add7bd631084484163b7ba14ae9" => :high_sierra
   end
 
   head do
