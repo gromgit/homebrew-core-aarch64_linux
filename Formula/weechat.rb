f5918111017b9f3d51b1186a7c85319b015c43f0
Rui Chen <rui@meetup.com>
2020-07-18 15:06:15 -0400

    weechat 2.9
    Closes #58194.
    
    Signed-off-by: Seeker <meaningseeking@protonmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/weechat.rb
+++ Formula/weechat.rb
@@ -1,10 +1,9 @@
 class Weechat < Formula
   desc "Extensible IRC client"
   homepage "https://www.weechat.org"
-  url "https://weechat.org/files/src/weechat-2.8.tar.xz"
-  sha256 "553ea295edad3b03cf88e6029c21e7bde32ff1cc026d35386ba9da3e56a6018c"
+  url "https://weechat.org/files/src/weechat-2.9.tar.xz"
+  sha256 "eab406c385c3a10d0107ddc3aac6596ae8c59af99e9158c6d769e90ec9adfa0e"
   license "GPL-3.0"
-  revision 5
   head "https://github.com/weechat/weechat.git"
 
   bottle do
