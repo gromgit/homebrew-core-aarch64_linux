3635145b479e8168b3a6b6f93c6662e862364fd3
Dustin Rodrigues <dust.rod@gmail.com>
2020-07-18 12:26:45 -0400

    stormssh: update pythonhosted urls

--- Formula/stormssh.rb
+++ Formula/stormssh.rb
@@ -30,7 +30,7 @@ class Stormssh < Formula
   end
 
   resource "pycrypto" do
-    url "https://files.pythonhosted.org/packages/source/p/pycrypto/pycrypto-2.6.1.tar.gz"
+    url "https://files.pythonhosted.org/packages/60/db/645aa9af249f059cc3a368b118de33889219e0362141e75d4eaf6f80f163/pycrypto-2.6.1.tar.gz"
     sha256 "f2ce1e989b272cfcb677616763e0a2e7ec659effa67a88aa92b3a65528f60a3c"
   end
 
@@ -45,7 +45,7 @@ class Stormssh < Formula
   end
 
   resource "termcolor" do
-    url "https://files.pythonhosted.org/packages/source/t/termcolor/termcolor-1.1.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/8a/48/a76be51647d0eb9f10e2a4511bf3ffb8cc1e6b14e9e4fab46173aa79f981/termcolor-1.1.0.tar.gz"
     sha256 "1d6d69ce66211143803fbc56652b41d73b4a400a2891d7bf7a1cdf4c02de613b"
   end
 
