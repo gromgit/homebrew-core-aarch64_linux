c59ef82b09c0d9142ccd8ab7604e17aeb55008b8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-18 19:07:13 +0000

    nasm: update 2.15.03 bottle.

--- Formula/nasm.rb
+++ Formula/nasm.rb
@@ -6,9 +6,9 @@ class Nasm < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "8bd0bdbfb3471d37857357458dc599650465b53d1661459900f61ef60effe4a1" => :catalina
-    sha256 "833f4b88e5d32727234e7934d77eebafa661b868bc19dc320fa032ab72e8419a" => :mojave
-    sha256 "457a1648b0b8447bd693068e10c366dcda1a71dc4ce0f27838520b32db653d01" => :high_sierra
+    sha256 "cbfd79dae0adf49b161549e937be1a32ed8b2672995afcd08abb4683dc4114d5" => :catalina
+    sha256 "3056dc42c3cbdf9364cce58f6f1f374b6c2c8d4e0fd79ee7a8bd7a62723f040a" => :mojave
+    sha256 "f4cf9a2a41ee8270a952dc15e70ee198dec7bbf10c151053c6ef4556b32b2b7e" => :high_sierra
   end
 
   head do
