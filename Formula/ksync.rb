47cd0f02532788e89836223141817988fc5f2e6b
Issy Long <me@issyl0.co.uk>
2020-07-19 19:28:13 +0000

    ksync: add 0.4.5 bottle.

--- Formula/ksync.rb
+++ Formula/ksync.rb
@@ -7,6 +7,13 @@ class Ksync < Formula
   sha256 "bd47c5b5418bfc6d2f08abe855a52d4189f8bf380747cdd4e5c1a3ca0a87498a"
   license "Apache-2.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1973e9cc12928e1680bc63c948404a81ae0eb83a083ecfcc593c0f9ba6799f2b" => :catalina
+    sha256 "253963ea2fe87fd45fb16ea131273878368964637140de4b0eeb9b2150e290c7" => :mojave
+    sha256 "b86ba3886a09fa0a5d78d86cf46bdd2dbcf524ef99cd4342e6c176698e0b5d00" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
