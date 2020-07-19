4cd2ee1f934846da5ccb1af797b0484a2a9a14d2
Rui Chen <rui@meetup.com>
2020-07-19 15:56:52 -0400

    fontconfig: add license

--- Formula/fontconfig.rb
+++ Formula/fontconfig.rb
@@ -3,6 +3,7 @@ class Fontconfig < Formula
   homepage "https://wiki.freedesktop.org/www/Software/fontconfig/"
   url "https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.13.1.tar.bz2"
   sha256 "f655dd2a986d7aa97e052261b36aa67b0a64989496361eca8d604e6414006741"
+  license "MIT"
 
   bottle do
     sha256 "64ff208b28613dfe2a65b9d74fd9b0129f3ca7e423db78329144cdaf51b36f70" => :catalina
