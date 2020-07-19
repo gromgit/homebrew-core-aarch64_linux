0337e74e9f36005231ebf407562652b3647ff89e
Rui Chen <rui@meetup.com>
2020-07-19 10:49:15 -0400

    mesos: add license

--- Formula/mesos.rb
+++ Formula/mesos.rb
@@ -4,6 +4,7 @@ class Mesos < Formula
   url "https://www.apache.org/dyn/closer.lua?path=mesos/1.8.1/mesos-1.8.1.tar.gz"
   mirror "https://archive.apache.org/dist/mesos/1.8.1/mesos-1.8.1.tar.gz"
   sha256 "583f2ad0de36c3e3ce08609a6df1a3ef1145e84f453b3d56fd8332767c3a84e7"
+  license "Apache-2.0"
   revision 1
 
   bottle do
