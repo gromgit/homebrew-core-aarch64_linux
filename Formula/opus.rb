958b39c23ec8dc2e9048def6734b2329da307509
Rui Chen <rui@meetup.com>
2020-07-19 17:39:28 -0400

    opus: add license

--- Formula/opus.rb
+++ Formula/opus.rb
@@ -3,6 +3,7 @@ class Opus < Formula
   homepage "https://www.opus-codec.org/"
   url "https://archive.mozilla.org/pub/opus/opus-1.3.1.tar.gz"
   sha256 "65b58e1e25b2a114157014736a3d9dfeaad8d41be1c8179866f144a2fb44ff9d"
+  license "BSD-3-Clause"
 
   bottle do
     cellar :any
