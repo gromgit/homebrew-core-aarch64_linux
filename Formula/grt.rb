cad0619ff718d4eca0d8e6d629a42544b59880c8
Rui Chen <rui@meetup.com>
2020-07-19 17:22:40 -0400

    grt: add license

--- Formula/grt.rb
+++ Formula/grt.rb
@@ -3,6 +3,7 @@ class Grt < Formula
   homepage "https://nickgillian.com/grt/"
   url "https://github.com/nickgillian/grt/archive/v0.2.4.tar.gz"
   sha256 "55bcabe7a58916461dc4341758eff2a45bd5b236c263dfe6e58c176c1a7e1ac4"
+  license "MIT"
 
   bottle do
     cellar :any
