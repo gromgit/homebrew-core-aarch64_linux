9bfb3efcd49ad91c9c465ca1416620a0fa1084c4
Rui Chen <rui@meetup.com>
2020-07-19 10:49:20 -0400

    storm: add license

--- Formula/storm.rb
+++ Formula/storm.rb
@@ -4,6 +4,7 @@ class Storm < Formula
   url "https://www.apache.org/dyn/closer.lua?path=storm/apache-storm-2.2.0/apache-storm-2.2.0.tar.gz"
   mirror "https://archive.apache.org/dist/storm/apache-storm-2.2.0/apache-storm-2.2.0.tar.gz"
   sha256 "f621163f349a8e85130bc3d2fbb34e3b08f9c039ccac5474f3724e47a3a38675"
+  license "Apache-2.0"
 
   bottle :unneeded
 
