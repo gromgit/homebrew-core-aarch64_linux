ed0d5c9535a2a2acc8e32b3d9e115e431d70e8f7
Rui Chen <rui@meetup.com>
2020-07-18 23:08:41 -0400

    cereal: add license

--- Formula/cereal.rb
+++ Formula/cereal.rb
@@ -3,6 +3,7 @@ class Cereal < Formula
   homepage "https://uscilab.github.io/cereal/"
   url "https://github.com/USCiLab/cereal/archive/v1.3.0.tar.gz"
   sha256 "329ea3e3130b026c03a4acc50e168e7daff4e6e661bc6a7dfec0d77b570851d5"
+  license "BSD-3-Clause"
   head "https://github.com/USCiLab/cereal.git", :branch => "develop"
 
   bottle do
