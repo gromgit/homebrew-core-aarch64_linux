078fc051d99c8a8501c4344c3dd212ef061a6d66
Rui Chen <rui@meetup.com>
2020-07-19 17:22:30 -0400

    geoip: add license

--- Formula/geoip.rb
+++ Formula/geoip.rb
@@ -3,6 +3,7 @@ class Geoip < Formula
   homepage "https://github.com/maxmind/geoip-api-c"
   url "https://github.com/maxmind/geoip-api-c/releases/download/v1.6.12/GeoIP-1.6.12.tar.gz"
   sha256 "1dfb748003c5e4b7fd56ba8c4cd786633d5d6f409547584f6910398389636f80"
+  license "LGPL-2.1"
   head "https://github.com/maxmind/geoip-api-c.git"
 
   bottle do
