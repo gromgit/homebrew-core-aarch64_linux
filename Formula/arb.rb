9cb4c191a6103ec7830bad50691bdba07ec58471
Caleb Xu <calebcenter@live.com>
2020-07-19 15:37:22 -0400

    arb 2.18.1 (new formula)
    Closes #58241.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/arb.rb
@@ -0,0 +1,52 @@
+class Arb < Formula
+  desc "C library for arbitrary-precision interval arithmetic"
+  homepage "http://arblib.org"
+  url "https://github.com/fredrik-johansson/arb/archive/2.18.1.tar.gz"
+  sha256 "9c5c6128c2e7bdc6e7e8d212f2b301068b87b956e1a238fe3b8d69d10175ceec"
+  license "LGPL-2.1"
+  head "https://github.com/fredrik-johansson/arb.git"
+
+  depends_on "cmake" => :build
+  depends_on "flint"
+  depends_on "gmp"
+  depends_on "mpfr"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <arb.h>
+
+      int main()
+      {
+          slong prec;
+          arb_t x, y;
+          arb_init(x); arb_init(y);
+
+          for (prec = 64; ; prec *= 2)
+          {
+              arb_const_pi(x, prec);
+              arb_set_si(y, -10000);
+              arb_exp(y, y, prec);
+              arb_add(x, x, y, prec);
+              arb_sin(y, x, prec);
+              if (arb_rel_accuracy_bits(y) >= 53) {
+                  arb_printn(y, 15, 0); printf("\\n");
+                  break;
+              }
+          }
+
+          arb_clear(x); arb_clear(y);
+          flint_cleanup();
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-I#{include}", "-I#{Formula["flint"].opt_include}",
+           "-L#{lib}", "-L#{Formula["flint"].opt_lib}",
+           "-larb", "-lflint", "-o", "test"
+    assert_match %r{\[-?\d+\.\d+e-\d+ \+/- \d+\.\d+e-\d+\]}, shell_output("./test")
+  end
+end
