f7ecbd268bfc042806e0e4fa83e9e6e6748a329e
Rui Chen <rui@meetup.com>
2020-07-18 21:28:58 -0400

    bgpdump: add license

--- Formula/bgpdump.rb
+++ Formula/bgpdump.rb
@@ -3,6 +3,7 @@ class Bgpdump < Formula
   homepage "https://github.com/RIPE-NCC/bgpdump/wiki"
   url "https://github.com/RIPE-NCC/bgpdump/archive/v1.6.2.tar.gz"
   sha256 "415692c173a84c48b1e927a6423a4f8fd3e6359bc3008c06b7702fe143a76223"
+  license "GPL-2.0"
 
   bottle do
     cellar :any
