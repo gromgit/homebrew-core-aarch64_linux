1ca3accc645634086c52d3d842daa6cb31551a4c
Rui Chen <rui@meetup.com>
2020-07-18 23:08:43 -0400

    cliclick: add license

--- Formula/cliclick.rb
+++ Formula/cliclick.rb
@@ -3,6 +3,7 @@ class Cliclick < Formula
   homepage "https://www.bluem.net/jump/cliclick/"
   url "https://github.com/BlueM/cliclick/archive/4.0.1.tar.gz"
   sha256 "78f9584524b2cb4710a964f4d8e23667479f1dab37240d1ffbc8dfa9841ff1c2"
+  license "BSD-3-Clause"
   head "https://github.com/BlueM/cliclick.git"
 
   bottle do
