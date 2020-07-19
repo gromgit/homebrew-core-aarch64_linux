7fe6e2f8dce656d77c6d267585e21ce8e0edd579
Rui Chen <rui@meetup.com>
2020-07-18 21:29:01 -0400

    bulk_extractor: add license

--- Formula/bulk_extractor.rb
+++ Formula/bulk_extractor.rb
@@ -3,6 +3,7 @@ class BulkExtractor < Formula
   homepage "https://github.com/simsong/bulk_extractor/wiki"
   url "https://digitalcorpora.org/downloads/bulk_extractor/bulk_extractor-1.5.5.tar.gz"
   sha256 "297a57808c12b81b8e0d82222cf57245ad988804ab467eb0a70cf8669594e8ed"
+  license "MIT"
   revision 3
 
   bottle do
