e6db7ee57589e254400d75fea824522243e88203
Rui Chen <rui@meetup.com>
2020-07-18 23:08:44 -0400

    coccinelle: add license

--- Formula/coccinelle.rb
+++ Formula/coccinelle.rb
@@ -4,6 +4,7 @@ class Coccinelle < Formula
   url "https://github.com/coccinelle/coccinelle.git",
       :tag      => "1.0.8",
       :revision => "d678c34afc0cfb479ad34f2225c57b1b8d3ebeae"
+  license "GPL-2.0"
   head "https://github.com/coccinelle/coccinelle.git"
 
   bottle do
