1f839e4c964dd32703f8aaa062cd987d4ba51b62
Rui Chen <rui@meetup.com>
2020-07-19 17:22:33 -0400

    git-gerrit: add license

--- Formula/git-gerrit.rb
+++ Formula/git-gerrit.rb
@@ -3,6 +3,7 @@ class GitGerrit < Formula
   homepage "https://github.com/fbzhong/git-gerrit"
   url "https://github.com/fbzhong/git-gerrit/archive/v0.3.0.tar.gz"
   sha256 "433185315db3367fef82a7332c335c1c5e0b05dabf8d4fbeff9ecf6cc7e422eb"
+  license "BSD-3-Clause"
   head "https://github.com/fbzhong/git-gerrit.git"
 
   bottle :unneeded
