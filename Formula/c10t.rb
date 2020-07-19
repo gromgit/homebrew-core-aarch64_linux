962c28df3cd24e7f92b10a2602809caa3e864c63
Rui Chen <rui@meetup.com>
2020-07-18 23:08:39 -0400

    c10t: add license

--- Formula/c10t.rb
+++ Formula/c10t.rb
@@ -3,6 +3,7 @@ class C10t < Formula
   homepage "https://github.com/udoprog/c10t"
   url "https://github.com/udoprog/c10t/archive/1.7.tar.gz"
   sha256 "0e5779d517105bfdd14944c849a395e1a8670bedba5bdab281a0165c3eb077dc"
+  license "BSD-3-Clause"
   revision 1
 
   bottle do
