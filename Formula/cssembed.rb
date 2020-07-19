bae3e473966c1bf42cda9228838b5eac14e8c1ae
Rui Chen <rui@meetup.com>
2020-07-18 23:08:47 -0400

    cssembed: add license

--- Formula/cssembed.rb
+++ Formula/cssembed.rb
@@ -3,6 +3,7 @@ class Cssembed < Formula
   homepage "https://www.nczonline.net/blog/2009/11/03/automatic-data-uri-embedding-in-css-files/"
   url "https://github.com/downloads/nzakas/cssembed/cssembed-0.4.5.jar"
   sha256 "8955016c0d32de8755d9ee33d365d1ad658a596aba48537a810ce62f3d32a1af"
+  license "MIT"
 
   bottle :unneeded
 
