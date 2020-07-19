f4f26d262e411d6fd56b74152a051c8792fe866d
Rui Chen <rui@meetup.com>
2020-07-19 14:18:54 -0400

    diff-pdf: add license

--- Formula/diff-pdf.rb
+++ Formula/diff-pdf.rb
@@ -3,6 +3,7 @@ class DiffPdf < Formula
   homepage "https://vslavik.github.io/diff-pdf/"
   url "https://github.com/vslavik/diff-pdf/releases/download/v0.4.1/diff-pdf-0.4.1.tar.gz"
   sha256 "0eb81af6b06593488acdc5924a199f74fe3df6ecf2a0f1be208823c021682686"
+  license "GPL-2.0"
   revision 4
 
   bottle do
