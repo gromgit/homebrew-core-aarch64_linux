72a7b43ce529e509572e0ad1296489a676ba31e3
Rui Chen <rui@meetup.com>
2020-07-19 17:22:36 -0400

    glui: add license

--- Formula/glui.rb
+++ Formula/glui.rb
@@ -3,6 +3,7 @@ class Glui < Formula
   homepage "https://glui.sourceforge.io/"
   url "https://github.com/libglui/glui/archive/2.37.tar.gz"
   sha256 "f7f6983f7410fe8dfaa032b2b7b1aac2232ec6a400a142b73f680683dad795f8"
+  license "Zlib"
 
   bottle do
     cellar :any_skip_relocation
