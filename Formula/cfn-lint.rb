c620e7668bd247f766f3d34a219b11b8f64de8c2
Rui Chen <rui@meetup.com>
2020-07-18 23:08:41 -0400

    cfn-lint: add license

--- Formula/cfn-lint.rb
+++ Formula/cfn-lint.rb
@@ -5,6 +5,7 @@ class CfnLint < Formula
   homepage "https://github.com/aws-cloudformation/cfn-python-lint/"
   url "https://files.pythonhosted.org/packages/5f/84/b1b9b6fb643819a7cb43b3ea0a1155a4682e3e916f4018f149314b01a595/cfn-lint-0.34.0.tar.gz"
   sha256 "443e7c1a57a8909050b51873cbf53628e253b6a2ab00fba9d25f89e75f13f1b6"
+  license "MIT"
 
   bottle do
     cellar :any_skip_relocation
