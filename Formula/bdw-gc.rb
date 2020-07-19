3c80341eeba303e1351e1b19fc6ecbbd8c1d1fd9
Rui Chen <rui@meetup.com>
2020-07-18 21:28:57 -0400

    bdw-gc: add license

--- Formula/bdw-gc.rb
+++ Formula/bdw-gc.rb
@@ -3,6 +3,7 @@ class BdwGc < Formula
   homepage "https://www.hboehm.info/gc/"
   url "https://github.com/ivmai/bdwgc/releases/download/v8.0.4/gc-8.0.4.tar.gz"
   sha256 "436a0ddc67b1ac0b0405b61a9675bca9e075c8156f4debd1d06f3a56c7cd289d"
+  license "MIT"
 
   bottle do
     cellar :any
