7fe49f209e2f1e0aca72daa27f1c63a05356d3df
Rui Chen <rui@meetup.com>
2020-07-18 21:28:59 -0400

    bmon: add license

--- Formula/bmon.rb
+++ Formula/bmon.rb
@@ -3,6 +3,7 @@ class Bmon < Formula
   homepage "https://github.com/tgraf/bmon"
   url "https://github.com/tgraf/bmon/releases/download/v4.0/bmon-4.0.tar.gz"
   sha256 "02fdc312b8ceeb5786b28bf905f54328f414040ff42f45c83007f24b76cc9f7a"
+  license "BSD-2-Clause"
   revision 2
 
   bottle do
