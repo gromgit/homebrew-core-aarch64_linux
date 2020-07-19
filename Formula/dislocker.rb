dac14841b93c1b4bec8d62b9a67581592b75b4d9
Rui Chen <rui@meetup.com>
2020-07-19 14:18:54 -0400

    dislocker: add license

--- Formula/dislocker.rb
+++ Formula/dislocker.rb
@@ -3,6 +3,7 @@ class Dislocker < Formula
   homepage "https://github.com/Aorimn/dislocker"
   url "https://github.com/Aorimn/dislocker/archive/v0.7.1.tar.gz"
   sha256 "742fb5c1b3ff540368ced54c29eae8b488ae5a5fcaca092947e17c2d358a6762"
+  license "GPL-2.0"
   revision 4
 
   bottle do
