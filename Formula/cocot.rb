7387b6691ba3e00cc464b1d812f45c281a33d19a
Rui Chen <rui@meetup.com>
2020-07-18 23:08:45 -0400

    cocot: add license

--- Formula/cocot.rb
+++ Formula/cocot.rb
@@ -3,6 +3,7 @@ class Cocot < Formula
   homepage "https://vmi.jp/software/cygwin/cocot.html"
   url "https://github.com/vmi/cocot/archive/cocot-1.2-20171118.tar.gz"
   sha256 "b718630ce3ddf79624d7dcb625fc5a17944cbff0b76574d321fb80c61bb91e4c"
+  license "BSD-3-Clause"
   head "https://github.com/vmi/cocot.git"
 
   bottle do
