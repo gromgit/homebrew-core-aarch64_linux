2b1faa39501a9e8b94b09d53245419002d436cd6
Rui Chen <rui@meetup.com>
2020-07-18 20:25:01 -0400

    audacious: add license

--- Formula/audacious.rb
+++ Formula/audacious.rb
@@ -1,6 +1,7 @@
 class Audacious < Formula
   desc "Free and advanced audio player based on GTK+"
   homepage "https://audacious-media-player.org/"
+  license "BSD-2-Clause"
 
   stable do
     url "https://distfiles.audacious-media-player.org/audacious-4.0.5.tar.bz2"
