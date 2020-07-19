4ad64fee0f25d97987af26312fb7f5f35dc61838
Rui Chen <rui@meetup.com>
2020-07-19 17:22:34 -0400

    git-open: add license

--- Formula/git-open.rb
+++ Formula/git-open.rb
@@ -3,6 +3,7 @@ class GitOpen < Formula
   homepage "https://github.com/jeffreyiacono/git-open"
   url "https://github.com/jeffreyiacono/git-open/archive/v1.3.tar.gz"
   sha256 "a1217e9b0a76382a96afd33ecbacad723528ec1116381c22a17cc7458de23701"
+  license "MIT"
 
   bottle :unneeded
 
