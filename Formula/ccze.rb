eff2b9df26ad8818d0d7789283480b44a20085ba
Rui Chen <rui@meetup.com>
2020-07-18 23:08:41 -0400

    ccze: add license

--- Formula/ccze.rb
+++ Formula/ccze.rb
@@ -3,6 +3,7 @@ class Ccze < Formula
   homepage "https://packages.debian.org/wheezy/ccze"
   url "https://deb.debian.org/debian/pool/main/c/ccze/ccze_0.2.1.orig.tar.gz"
   sha256 "8263a11183fd356a033b6572958d5a6bb56bfd2dba801ed0bff276cfae528aa3"
+  license "GPL-2.0"
   revision 1
 
   bottle do
