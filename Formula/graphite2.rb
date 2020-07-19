2d53dd3b4892ea18fd4772501fdb4c772eeed596
Rui Chen <rui@meetup.com>
2020-07-19 17:22:39 -0400

    graphite2: add license

--- Formula/graphite2.rb
+++ Formula/graphite2.rb
@@ -3,6 +3,7 @@ class Graphite2 < Formula
   homepage "https://graphite.sil.org/"
   url "https://github.com/silnrsi/graphite/releases/download/1.3.14/graphite2-1.3.14.tgz"
   sha256 "f99d1c13aa5fa296898a181dff9b82fb25f6cc0933dbaa7a475d8109bd54209d"
+  license "LGPL-2.1"
   head "https://github.com/silnrsi/graphite.git"
 
   bottle do
