5ef83c887a29ec218d17cc59336ef3cbb25b4048
Rui Chen <rui@meetup.com>
2020-07-19 14:44:49 -0400

    expat: add license

--- Formula/expat.rb
+++ Formula/expat.rb
@@ -3,6 +3,7 @@ class Expat < Formula
   homepage "https://libexpat.github.io/"
   url "https://github.com/libexpat/libexpat/releases/download/R_2_2_9/expat-2.2.9.tar.xz"
   sha256 "1ea6965b15c2106b6bbe883397271c80dfa0331cdf821b2c319591b55eadc0a4"
+  license "MIT"
 
   bottle do
     cellar :any
