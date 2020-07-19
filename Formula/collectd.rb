784aef4e67bc4594d1fa7a1219246a1554062f7d
Rui Chen <rui@meetup.com>
2020-07-18 23:08:45 -0400

    collectd: add license

--- Formula/collectd.rb
+++ Formula/collectd.rb
@@ -3,6 +3,7 @@ class Collectd < Formula
   homepage "https://collectd.org/"
   url "https://collectd.org/files/collectd-5.11.0.tar.bz2"
   sha256 "37b10a806e34aa8570c1cafa6006c604796fae13cc2e1b3e630d33dcba9e5db2"
+  license "MIT"
 
   bottle do
     sha256 "6b1fc316e7cab7aa33cfb8034a4e7485798ef2ceb477ceee88c212af480a1d3d" => :catalina
