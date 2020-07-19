4c45c9ec5f6dc2a2467c6e0e9e433ed3833d3201
Seeker <meaningseeking@protonmail.com>
2020-07-19 09:31:53 -0700

    servus: add license

--- Formula/servus.rb
+++ Formula/servus.rb
@@ -4,6 +4,7 @@ class Servus < Formula
   url "https://github.com/HBPVIS/Servus.git",
       :tag      => "1.5.2",
       :revision => "170bd93dbdd6c0dd80cf4dfc5926590cc5cef5ab"
+  license "LGPL-3.0"
 
   bottle do
     cellar :any
