bff8a00f8e667fda146b50ba26861a7951286f45
Rui Chen <rui@meetup.com>
2020-07-19 10:49:23 -0400

    tomcat@8: add license

--- Formula/tomcat@8.rb
+++ Formula/tomcat@8.rb
@@ -4,6 +4,7 @@ class TomcatAT8 < Formula
   url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-8/v8.5.57/bin/apache-tomcat-8.5.57.tar.gz"
   mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.57/bin/apache-tomcat-8.5.57.tar.gz"
   sha256 "2615839daf1899cd705f9b82f2df6bd21adb29a93a05afdea0a6192ce54025c4"
+  license "Apache-2.0"
 
   bottle :unneeded
 
