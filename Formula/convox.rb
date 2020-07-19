6c9cbd811354f2514b68faa43ec06034d4d20fde
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-19 21:08:14 +0000

    convox: update 3.0.32 bottle.

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -8,9 +8,9 @@ class Convox < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3f83a13ef234ec28f484464aeade3f9ca514dc2fae0dab83f9963dd5c98f7221" => :catalina
-    sha256 "9bf1b6e64b2866c964a32db058b9c3bd53e305bb005f7c96cec4f969449f7ed8" => :mojave
-    sha256 "51861918f11c3a7f4312aea64f299ed50f2c726a5708c6c26ece14b8b66fc704" => :high_sierra
+    sha256 "7fdff1247bd3c2ba86fe617232d0ba2fa185d8830f3a3a77055466aa1dd9b64a" => :catalina
+    sha256 "035eb53a9487add8bfd323a19644d999c9b190a5e1477461136627a22e7b04b7" => :mojave
+    sha256 "81d9558502513a868efab1c25edb7202846b04d9181b7f2f5079e57b56ecbe0c" => :high_sierra
   end
 
   depends_on "go" => :build
