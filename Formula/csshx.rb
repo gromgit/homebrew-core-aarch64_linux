9f60a2a2b907833b118fe4c928f71633c9e952d6
Rui Chen <rui@meetup.com>
2020-07-18 23:08:47 -0400

    csshx: add license

--- Formula/csshx.rb
+++ Formula/csshx.rb
@@ -4,6 +4,8 @@ class Csshx < Formula
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/csshx/csshX-0.74.tgz"
   mirror "https://distfiles.macports.org/csshX/csshX-0.74.tgz"
   sha256 "eaa9e52727c8b28dedc87398ed33ffa2340d6d0f3ea9d261749c715cb7a0e9c8"
+  # same terms as Perl
+  license "GPL-1.0"
   head "https://github.com/brockgr/csshx.git"
 
   bottle :unneeded
