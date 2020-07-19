0f00a9f8246d0c0857dd62a30adf31281bb4a76f
Rui Chen <rui@meetup.com>
2020-07-18 23:08:45 -0400

    cpanminus: add license

--- Formula/cpanminus.rb
+++ Formula/cpanminus.rb
@@ -3,6 +3,8 @@ class Cpanminus < Formula
   homepage "https://github.com/miyagawa/cpanminus"
   url "https://github.com/miyagawa/cpanminus/archive/1.9019.tar.gz"
   sha256 "d0a37547a3c4b6dbd3806e194cd6cf4632158ebed44d740ac023e0739538fb46"
+  # dual licensed same as perl (GPL-1.0 or Artistic-1.0)
+  license "GPL-1.0"
   head "https://github.com/miyagawa/cpanminus.git"
 
   bottle :unneeded
