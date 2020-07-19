f204b6b30d3981259a764a29dca1a4b403524c9d
Rui Chen <rui@meetup.com>
2020-07-18 23:08:47 -0400

    cryptopp: add license

--- Formula/cryptopp.rb
+++ Formula/cryptopp.rb
@@ -3,6 +3,7 @@ class Cryptopp < Formula
   homepage "https://www.cryptopp.com/"
   url "https://github.com/weidai11/cryptopp/archive/CRYPTOPP_8_1_0.tar.gz"
   sha256 "8a4e4773a39b0c07d7cea1b8be7a3f7a9d126bd3ac9a9f072f82d3a53a474a87"
+  license "BSD-3-Clause"
 
   # https://cryptopp.com/wiki/Config.h#Options_and_Defines
   bottle :disable, "Library and clients must be built on the same microarchitecture"
