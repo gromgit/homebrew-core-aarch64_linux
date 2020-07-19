a2c9e457ab8cd85bf5972a112879f5db35ebf336
Rui Chen <rui@meetup.com>
2020-07-19 10:49:03 -0400

    batik: add license

--- Formula/batik.rb
+++ Formula/batik.rb
@@ -4,6 +4,7 @@ class Batik < Formula
   url "https://www.apache.org/dyn/closer.lua?path=xmlgraphics/batik/binaries/batik-bin-1.13.tar.gz"
   mirror "https://archive.apache.org/dist/xmlgraphics/batik/binaries/batik-bin-1.13.tar.gz"
   sha256 "7c565899e4377a72edee216ffdf168d7b6928d5e1a8cdf477dfa1abd2c48589b"
+  license "Apache-2.0"
   revision 1
 
   bottle :unneeded
