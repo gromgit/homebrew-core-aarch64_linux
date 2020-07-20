9fc0eafe0be346c322eeb89fed033f7b076d2cb3
miccal <miccal.matthews@gmail.com>
2020-07-20 20:46:22 +0800

    pixman: add license

--- Formula/pixman.rb
+++ Formula/pixman.rb
@@ -3,6 +3,7 @@ class Pixman < Formula
   homepage "https://cairographics.org/"
   url "https://cairographics.org/releases/pixman-0.40.0.tar.gz"
   sha256 "6d200dec3740d9ec4ec8d1180e25779c00bc749f94278c8b9021f5534db223fc"
+  license "LGPL-2.1"
 
   bottle do
     cellar :any
