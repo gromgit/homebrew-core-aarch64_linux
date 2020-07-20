1dd0311c8dbbf3f1a6d984818fc3928a9081feac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-20 08:09:28 +0000

    wxmaxima: update 20.07.0 bottle.

--- Formula/wxmaxima.rb
+++ Formula/wxmaxima.rb
@@ -6,9 +6,9 @@ class Wxmaxima < Formula
   head "https://github.com/wxMaxima-developers/wxmaxima.git"
 
   bottle do
-    sha256 "9e9bdb5eacc61fdad7945970db68f8d04bdf96172643301c918ad2cb028dfede" => :catalina
-    sha256 "db544d8b5bb33915a44bf93382c182d56935e2a88fab17ff4d631095630c9e0f" => :mojave
-    sha256 "0942521551716a682c363ad83f9bc0ce90cf53dd74864d31a1dfd7eab8dd680e" => :high_sierra
+    sha256 "a313284d285cb130b1567536d4004d96e29da2a364f69f066360870bab89574e" => :catalina
+    sha256 "1f7c0913f429666b64caa646f4e425c41fa451d2f7d859ec72fe89961c2081e5" => :mojave
+    sha256 "cd4d134f65f80c72bf5191612b6555ea540b0a4b697cd20c5e36d605470db731" => :high_sierra
   end
 
   depends_on "cmake" => :build
