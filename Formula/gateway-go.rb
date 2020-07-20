485043b7c0f8b9c896baf6e1e0a25a704771f11f
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-07-20 11:55:03 +0000

    gateway-go: add 0.1.79 bottle.

--- Formula/gateway-go.rb
+++ Formula/gateway-go.rb
@@ -6,6 +6,13 @@ class GatewayGo < Formula
       :revision => "e974826206ceff6c7d8fed3e8e290411b0437fd4"
   license "MIT"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "59911d2de9cb1d9016b9a8479babe1809898e89824c5889f0e83b80b4276c369" => :catalina
+    sha256 "a2e85fa1ceff2b9361f716c177135f6520c5e21b4f51f7f3c04ad18cbb8ee5cf" => :mojave
+    sha256 "698be75a61421c6c205b1d1901fb680e477477448bef0365db0e593cec6426dc" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
