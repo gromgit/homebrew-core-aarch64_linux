1218f8fb6401259716ca46cba19f27b13b874ca4
Seeker <meaningseeking@protonmail.com>
2020-07-20 09:24:04 -0700

    polyml 5.8.1
    Closes #58275.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/polyml.rb
+++ Formula/polyml.rb
@@ -1,8 +1,8 @@
 class Polyml < Formula
   desc "Standard ML implementation"
   homepage "https://www.polyml.org/"
-  url "https://github.com/polyml/polyml/archive/v5.8.tar.gz"
-  sha256 "6bcc2c5af91f361ef9e0bb28f39ce20171b0beae73b4db3674df6fc793cec8bf"
+  url "https://github.com/polyml/polyml/archive/v5.8.1.tar.gz"
+  sha256 "fa0507b44470b4e07a061ef6a8896efe42710d8436e15721d743572ad2f7753b"
   license "LGPL-2.1"
   head "https://github.com/polyml/polyml.git"
 
@@ -13,17 +13,21 @@ class Polyml < Formula
     sha256 "d69da52fe77cd77d079de8ba2b389ced34800cebf55202d023c6800d584a5212" => :high_sierra
   end
 
-  # Patch for Xcode 11
-  # https://github.com/polyml/polyml/pull/119
-  patch do
-    url "https://github.com/polyml/polyml/commit/44efa473.diff?full_index=1"
-    sha256 "0835165da3f0b540c13e06d79dfdc4bcbcc4cde17207ea2e02978582552ee4d0"
-  end
-
   def install
     system "./configure", "--disable-dependency-tracking", "--disable-debug",
                           "--prefix=#{prefix}"
     system "make"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"hello.ml").write <<~EOS
+      let
+        fun concatWithSpace(a,b) = a ^ " " ^ b
+      in
+        TextIO.print(concatWithSpace("Hello", "World"))
+      end
+    EOS
+    assert_match "Hello World", shell_output("#{bin}/poly --script hello.ml")
   end
 end
