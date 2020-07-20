1e1b348ad288a6efd311d2b83e0a351b8d73f51d
miccal <miccal.matthews@gmail.com>
2020-07-20 20:48:21 +0800

    libsvg-cairo: add license

--- Formula/libsvg-cairo.rb
+++ Formula/libsvg-cairo.rb
@@ -3,6 +3,7 @@ class LibsvgCairo < Formula
   homepage "https://cairographics.org/"
   url "https://cairographics.org/snapshots/libsvg-cairo-0.1.6.tar.gz"
   sha256 "a380be6a78ec2938100ce904363815a94068fca372c666b8cc82aa8711a0215c"
+  license "LGPL-2.1"
   revision 2
 
   bottle do
