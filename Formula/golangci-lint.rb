e8c8a230a40dd1721634bf4f02b18b4a739f291b
Oleg Butuzov <butuzov@made.ua>
2020-07-20 14:01:24 +0300

    golangci-lint 1.29.0 (new formula)
    Closes #58329.
    
    Signed-off-by: Dawid Dziurla <9713907+dawidd6@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@meetup.com>

---
+++ Formula/golangci-lint.rb
@@ -0,0 +1,62 @@
+class GolangciLint < Formula
+  desc "Fast linters runner for Go"
+  homepage "https://golangci.com"
+  url "https://github.com/golangci/golangci-lint.git",
+    :tag      => "v1.29.0",
+    :revision => "6a689074bf17fd4ae1db779a74dba821a162b6c8"
+  license "GPL-3.0"
+
+  depends_on "go"
+
+  def install
+    commit = Utils.safe_popen_read("git", "rev-parse", "--short=7", "HEAD").chomp
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.commit=#{commit}
+      -X main.date=#{Time.now.utc.rfc3339}
+    ].join(" ")
+
+    system "go", "build", *std_go_args, "-ldflags", ldflags, "./cmd/golangci-lint"
+
+    output = Utils.safe_popen_read({ "SHELL" => "bash" }, "#{bin}/golangci-lint", "completion", "bash")
+    (bash_completion/"golangci-lint").write output
+
+    output = Utils.safe_popen_read({ "SHELL" => "zsh" }, "#{bin}/golangci-lint", "completion", "zsh")
+    (zsh_completion/"golangci-lint").write output
+  end
+
+  test do
+    str_version = shell_output("#{bin}/golangci-lint --version")
+    assert_match "golangci-lint has version #{version} built from", str_version
+
+    str_help = shell_output("#{bin}/golangci-lint --help")
+    str_default = shell_output("#{bin}/golangci-lint")
+    assert_equal str_default, str_help
+    assert_match "Usage:", str_help
+    assert_match "Available Commands:", str_help
+
+    (testpath/"try.go").write <<~EOS
+      package try
+
+      func add(nums ...int) (res int) {
+        for _, n := range nums {
+          res += n
+        }
+        return
+      }
+    EOS
+
+    args = %w[
+      --color=never
+      --disable-all
+      --issues-exit-code=0
+      --print-issued-lines=false
+      --enable=deadcode
+    ].join(" ")
+
+    ok_test = shell_output("#{bin}/golangci-lint run #{args} #{testpath}/try.go")
+    expected_message = "try.go:3:6: `add` is unused (deadcode)"
+    assert_match expected_message, ok_test
+  end
+end
