fabc793ee5415dac42029f9346bd5d884b0d6aa4
Alexander Zigelski <ali@crunchtime.dev>
2020-07-20 09:12:36 +0200

    argo 2.9.3 (new formula)
    Closes #58259.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: chenrui <chenrui333@gmail.com>

---
+++ Formula/argo.rb
@@ -0,0 +1,30 @@
+class Argo < Formula
+  desc "Get stuff done with container-native workflows for Kubernetes"
+  homepage "https://argoproj.io"
+  url "https://github.com/argoproj/argo.git",
+      :tag      => "v2.9.3",
+      :revision => "9407e19b3a1c61ad4043e382484fd0b6b15574f2"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+  depends_on "node" => :build
+  depends_on "yarn" => :build
+
+  def install
+    # this needs to be remove to prevent multiple 'operation not permitted' errors
+    inreplace "Makefile", "CGO_ENABLED=0", ""
+    system "make", "dist/argo"
+    bin.install "dist/argo"
+  end
+
+  test do
+    assert_match "argo is the command line interface to Argo",
+      shell_output("#{bin}/argo --help")
+
+    # argo consumes the Kubernetes configuration with the `--kubeconfig` flag
+    # Since it is an empty file we expect it to be invalid
+    touch testpath/"kubeconfig"
+    assert_match "invalid configuration",
+      shell_output("#{bin}/argo lint --kubeconfig ./kubeconfig 2>&1", 1)
+  end
+end
