94ec4ae786d5d9f83ce4c077f975fce8308179c6
miccal <miccal.matthews@gmail.com>
2020-07-21 15:18:36 +0800

    pcre2: add license

--- Formula/pcre2.rb
+++ Formula/pcre2.rb
@@ -3,6 +3,7 @@ class Pcre2 < Formula
   homepage "https://www.pcre.org/"
   url "https://ftp.pcre.org/pub/pcre/pcre2-10.35.tar.bz2"
   sha256 "9ccba8e02b0ce78046cdfb52e5c177f0f445e421059e43becca4359c669d4613"
+  license "BSD-3-Clause"
   head "svn://vcs.exim.org/pcre2/code/trunk"
 
   bottle do
