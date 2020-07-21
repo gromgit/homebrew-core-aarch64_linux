cf8810cafc6a12fba4569f0ae2399e4e21a2ef96
Seeker <meaningseeking@protonmail.com>
2020-07-20 20:15:18 -0700

    gl2ps: add license

--- Formula/gl2ps.rb
+++ Formula/gl2ps.rb
@@ -3,6 +3,7 @@ class Gl2ps < Formula
   homepage "https://www.geuz.org/gl2ps/"
   url "https://geuz.org/gl2ps/src/gl2ps-1.4.2.tgz"
   sha256 "8d1c00c1018f96b4b97655482e57dcb0ce42ae2f1d349cd6d4191e7848d9ffe9"
+  license "GL2PS"
 
   bottle do
     cellar :any
