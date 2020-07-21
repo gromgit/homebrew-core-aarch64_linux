2b4b74aafeb98989e6dd56b71f14c127d520a109
Andrew Kane <andrew@chartkick.com>
2020-07-21 02:32:21 -0700

    promtail 1.5.0 (new formula)
    Closes #58303.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: Seeker <meaningseeking@protonmail.com>

---
+++ Formula/promtail.rb
@@ -0,0 +1,32 @@
+class Promtail < Formula
+  desc "Log agent for Loki"
+  homepage "https://grafana.com/loki"
+  url "https://github.com/grafana/loki/archive/v1.5.0.tar.gz"
+  sha256 "bd32bb96db1f8d90fa8c7f5473fbff4048364b8b8a0c9fdcd21155f6a062689d"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    cd "cmd/promtail" do
+      system "go", "build", *std_go_args
+      etc.install "promtail-local-config.yaml"
+    end
+  end
+
+  test do
+    port = free_port
+
+    cp etc/"promtail-local-config.yaml", testpath
+    inreplace "promtail-local-config.yaml" do |s|
+      s.gsub! "9080", port.to_s
+      s.gsub! /__path__: .+$/, "__path__: #{testpath}"
+    end
+
+    fork { exec bin/"promtail", "-config.file=promtail-local-config.yaml" }
+    sleep 3
+
+    output = shell_output("curl -s localhost:#{port}/metrics")
+    assert_match "log_messages_total", output
+  end
+end
