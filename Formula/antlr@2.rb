6010e5db043625686ed6c7f93208623043500329
Seeker <meaningseeking@protonmail.com>
2020-07-20 20:13:09 -0700

    antlr@2: add license

--- Formula/antlr@2.rb
+++ Formula/antlr@2.rb
@@ -3,6 +3,7 @@ class AntlrAT2 < Formula
   homepage "https://www.antlr2.org/"
   url "https://www.antlr2.org/download/antlr-2.7.7.tar.gz"
   sha256 "853aeb021aef7586bda29e74a6b03006bcb565a755c86b66032d8ec31b67dbb9"
+  license "ANTLR-PD"
   revision 3
 
   bottle do
