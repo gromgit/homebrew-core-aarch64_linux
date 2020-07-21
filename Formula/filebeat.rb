65791ca01a9916cb5b4e3371d1e9ca568b6166c5
Michka Popoff <michkapopoff@gmail.com>
2020-07-21 23:35:29 +0200

    filebeat: add linux-only dependency and fix build

--- Formula/filebeat.rb
+++ Formula/filebeat.rb
@@ -19,6 +19,8 @@ class Filebeat < Formula
   depends_on "go" => :build
   depends_on "python@3.8" => :build
 
+  uses_from_macos "rsync" => :build
+
   resource "virtualenv" do
     url "https://files.pythonhosted.org/packages/b1/72/2d70c5a1de409ceb3a27ff2ec007ecdd5cc52239e7c74990e32af57affe9/virtualenv-15.2.0.tar.gz"
     sha256 "1d7e241b431e7afce47e77f8843a276f652699d1fa4f93b9d8ce0076fd7b0b54"
