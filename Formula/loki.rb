59db765453c7660ef533e017a3719d31ea6e5f36
Andrew Kane <andrew@chartkick.com>
2020-07-21 02:22:37 -0700

    loki 1.5.0 (new formula)
    Closes #58300.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: Seeker <meaningseeking@protonmail.com>

---
+++ Formula/loki.rb
@@ -0,0 +1,66 @@
+class Loki < Formula
+  desc "Horizontally-scalable, highly-available log aggregation system"
+  homepage "https://grafana.com/loki"
+  url "https://github.com/grafana/loki/archive/v1.5.0.tar.gz"
+  sha256 "bd32bb96db1f8d90fa8c7f5473fbff4048364b8b8a0c9fdcd21155f6a062689d"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    cd "cmd/loki" do
+      system "go", "build", *std_go_args
+      inreplace "loki-local-config.yaml", "/tmp", var
+      etc.install "loki-local-config.yaml"
+    end
+  end
+
+  plist_options :manual => "loki -config.file=#{HOMEBREW_PREFIX}/etc/loki-local-config.yaml"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>KeepAlive</key>
+          <dict>
+            <key>SuccessfulExit</key>
+            <false/>
+          </dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/loki</string>
+            <string>-config.file=#{etc}/loki-local-config.yaml</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>WorkingDirectory</key>
+          <string>#{var}</string>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/loki.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/loki.log</string>
+        </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    port = free_port
+
+    cp etc/"loki-local-config.yaml", testpath
+    inreplace "loki-local-config.yaml" do |s|
+      s.gsub! "3100", port.to_s
+      s.gsub! var, testpath
+    end
+
+    fork { exec bin/"loki", "-config.file=loki-local-config.yaml" }
+    sleep 3
+
+    output = shell_output("curl -s localhost:#{port}/metrics")
+    assert_match "log_messages_total", output
+  end
+end
