a6f2109077ad6791c8e6a3c7de52df378b2401ab
chenrui <chenrui333@gmail.com>
2020-07-21 23:30:43 +0000

    fennel: add 0.4.2 bottle.

--- Formula/fennel.rb
+++ Formula/fennel.rb
@@ -5,6 +5,13 @@ class Fennel < Formula
   sha256 "390e28fb341681fc0a9237c6aea55d1afb4bffb422fbdd6619f83589407e2bc0"
   license "MIT"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9577b157dd309dbc8f975898e24124246043a22e812abf5c8763e88872b2dcf1" => :catalina
+    sha256 "9577b157dd309dbc8f975898e24124246043a22e812abf5c8763e88872b2dcf1" => :mojave
+    sha256 "9577b157dd309dbc8f975898e24124246043a22e812abf5c8763e88872b2dcf1" => :high_sierra
+  end
+
   depends_on "lua"
 
   def install
