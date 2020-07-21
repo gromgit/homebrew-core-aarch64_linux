1ebe69dcfa43ff5c8a550535112ed68c80717212
chenrui <chenrui333@gmail.com>
2020-07-21 23:29:14 +0000

    act: add 0.2.10 bottle.

--- Formula/act.rb
+++ Formula/act.rb
@@ -5,6 +5,13 @@ class Act < Formula
   sha256 "1e4d371cfa6aec7e350927e55c3ac8413dbf6e23edbb811ebe2d587dbf136928"
   license "MIT"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "056167aa7eec2e5229a0f50b43d3bcd4845fadef990cd29d65a13b316449dc06" => :catalina
+    sha256 "1e6ab1ae827952962a92b1bcd82643b950edd446b87f6f991bcd6ad333de0852" => :mojave
+    sha256 "c8ac56be5db0a72aa5eac9c8f8937c5c52386e45d0f364ad9c63a7cba04db39d" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
