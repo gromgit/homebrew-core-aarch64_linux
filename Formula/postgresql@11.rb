77c40af331c7334f5c61df2a839ad59be5e2a6f4
Seeker <meaningseeking@protonmail.com>
2020-07-20 21:23:30 -0700

    postgresql@11: add license

--- Formula/postgresql@11.rb
+++ Formula/postgresql@11.rb
@@ -3,6 +3,7 @@ class PostgresqlAT11 < Formula
   homepage "https://www.postgresql.org/"
   url "https://ftp.postgresql.org/pub/source/v11.8/postgresql-11.8.tar.bz2"
   sha256 "eaf2f4329ccc349c89e950761b81daf8c99bb8966abcab5665ccd6ee95c77ae2"
+  license "PostgreSQL"
   revision 3
 
   bottle do
