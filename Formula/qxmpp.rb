71ec3a1c8acf58949ed0fbf4ce3f6afacfebdbe8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-22 04:08:05 +0000

    qxmpp: update 1.3.1 bottle.

--- Formula/qxmpp.rb
+++ Formula/qxmpp.rb
@@ -7,9 +7,9 @@ class Qxmpp < Formula
 
   bottle do
     cellar :any
-    sha256 "4c3019ce06ecc7202748b289ecbf1ac13f880f336de7d55f1e4ca717e6255d3e" => :catalina
-    sha256 "7adb72c5f0920f8642cdbc768bee46e317594d261611c9ad91515c847afe9dee" => :mojave
-    sha256 "3c656c837d070fb9cf7d6de56056636876440aa6d00bcae8f8dda8945f9581f2" => :high_sierra
+    sha256 "ae5ada2da192e552193487318f63c28bc5a1ce71705ac97ff2f2cdaebf1ace20" => :catalina
+    sha256 "ace2c4096387d98f5a27d28d73b0ed2453a61a5a9b4bd7ff1c55b105f0373b38" => :mojave
+    sha256 "a8342df624addc888a2d409e01a087e7974ff78b2091df7f6dc94949bb55abee" => :high_sierra
   end
 
   depends_on "cmake" => :build
