cca4b894abe3c37938857ef76024c8b2b20de85b
miccal <miccal.matthews@gmail.com>
2020-07-22 10:53:58 +0800

    libtool: add license

--- Formula/libtool.rb
+++ Formula/libtool.rb
@@ -4,6 +4,7 @@ class Libtool < Formula
   url "https://ftp.gnu.org/gnu/libtool/libtool-2.4.6.tar.xz"
   mirror "https://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.xz"
   sha256 "7c87a8c2c8c0fc9cd5019e402bed4292462d00a718a7cd5f11218153bf28b26f"
+  license "GPL-2.0"
   revision 2
 
   bottle do
