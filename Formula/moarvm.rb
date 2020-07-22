ee7f0584c4ce8f2c888c76dbc0e372dd4815ce14
Caleb Xu <calebcenter@live.com>
2020-07-22 00:42:47 -0400

    moarvm 2020.07

--- Formula/moarvm.rb
+++ Formula/moarvm.rb
@@ -1,8 +1,8 @@
 class Moarvm < Formula
   desc "Virtual machine for NQP and Rakudo Perl 6"
   homepage "https://moarvm.org"
-  url "https://github.com/MoarVM/MoarVM/releases/download/2020.06/MoarVM-2020.06.tar.gz"
-  sha256 "db9759f16f76cb6d3b1f4baff9616c5ddb99fbb47ea62f54a5cc251b6ea99dc2"
+  url "https://github.com/MoarVM/MoarVM/releases/download/2020.07/MoarVM-2020.07.tar.gz"
+  sha256 "da48887a1be0e397586b3470c2627d04c64d1e0caf9305ee7dc8db8db739f7cf"
 
   bottle do
     sha256 "c72de5db670e00640c9d275c44d0450d5aba135941736db72f556db2a58986fc" => :catalina
@@ -17,7 +17,7 @@ class Moarvm < Formula
 
   conflicts_with "rakudo-star", :because => "rakudo-star currently ships with moarvm included"
 
-  resource("nqp") do
+  resource "nqp" do
     url "https://github.com/perl6/nqp/releases/download/2020.05/nqp-2020.05.tar.gz"
     sha256 "291b92d9db968a691195adb1c9533edc1076d12d6617d6d931e40595e906b577"
   end
