a5e20188047253364033a812b5c98e5df361ea01
Alex Couture-Beil <alex@earthly.dev>
2020-07-22 22:45:48 +0000

    earthly 0.2.0

--- Formula/earthly.rb
+++ Formula/earthly.rb
@@ -1,8 +1,8 @@
 class Earthly < Formula
   desc "Build automation tool for the container era"
   homepage "https://docs.earthly.dev/"
-  url "https://github.com/earthly/earthly/archive/v0.1.3.tar.gz"
-  sha256 "bfbfddc52eb39dd6ea389dc5c8a86386e72005ea17d04af6b36ca5400492e328"
+  url "https://github.com/earthly/earthly/archive/v0.2.0.tar.gz"
+  sha256 "2c20e117d91caa5ee68349d9509fca89bec15e05b7173eded3d9bf8eac6a41d2"
   license "MPL-2.0"
   head "https://github.com/earthly/earthly.git"
 
