aa2615cdc4cde479d3c65e1f6721cb6ee799bf36
miccal <miccal.matthews@gmail.com>
2020-07-22 10:58:52 +0800

    p11-kit: add license

--- Formula/p11-kit.rb
+++ Formula/p11-kit.rb
@@ -3,6 +3,7 @@ class P11Kit < Formula
   homepage "https://p11-glue.freedesktop.org"
   url "https://github.com/p11-glue/p11-kit/releases/download/0.23.20/p11-kit-0.23.20.tar.xz"
   sha256 "14d86024c3dfd6b967d9bc0b4ec7b2973014fe7423481f4d230a1a63b8aa6104"
+  license "BSD-3-Clause"
   revision 1
 
   bottle do
