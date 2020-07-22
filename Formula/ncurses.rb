0b4c5f6d0de1b6ffcacd597aef76a250301f02a8
miccal <miccal.matthews@gmail.com>
2020-07-22 10:56:05 +0800

    ncurses: add license

--- Formula/ncurses.rb
+++ Formula/ncurses.rb
@@ -4,6 +4,7 @@ class Ncurses < Formula
   url "https://ftp.gnu.org/gnu/ncurses/ncurses-6.2.tar.gz"
   mirror "https://ftpmirror.gnu.org/ncurses/ncurses-6.2.tar.gz"
   sha256 "30306e0c76e0f9f1f0de987cf1c82a5c21e1ce6568b9227f7da5b71cbea86c9d"
+  license "MIT"
 
   bottle do
     sha256 "eae51ad3391edafe3d6c649ba44f607ee1464b4b5d9ee48770e9817ee5f0ccdd" => :catalina
