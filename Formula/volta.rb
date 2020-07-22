54f344b08b97b245617988149be7a78f105a5ea3
Caleb Xu <calebcenter@live.com>
2020-07-22 01:25:04 -0400

    volta 0.8.4
    Closes #58373.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/volta.rb
+++ Formula/volta.rb
@@ -2,8 +2,8 @@ class Volta < Formula
   desc "JavaScript toolchain manager for reproducible environments"
   homepage "https://volta.sh"
   url "https://github.com/volta-cli/volta.git",
-    :revision => "42caba1cc64b0e2946b9dc2db8b570fc55aef298",
-    :tag      => "v0.8.3"
+      :tag      => "v0.8.4",
+      :revision => "d72e13e6465d05f8f0b852d8d791ef4bdfd14d4c"
 
   bottle do
     cellar :any_skip_relocation
