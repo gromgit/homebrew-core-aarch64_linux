df60325162ae56cc95cb54799145a1c8f51749fe
miccal <miccal.matthews@gmail.com>
2020-07-22 11:00:49 +0800

    pinentry: add license

--- Formula/pinentry.rb
+++ Formula/pinentry.rb
@@ -4,6 +4,7 @@ class Pinentry < Formula
   url "https://www.gnupg.org/ftp/gcrypt/pinentry/pinentry-1.1.0.tar.bz2"
   mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/pinentry/pinentry-1.1.0.tar.bz2"
   sha256 "68076686fa724a290ea49cdf0d1c0c1500907d1b759a3bcbfbec0293e8f56570"
+  license "GPL-2.0"
   revision 1
 
   bottle do
