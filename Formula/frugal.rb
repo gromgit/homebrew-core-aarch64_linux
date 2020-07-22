3c23a4e15f44581f0c73289400c814c1bc51e55e
Rui Chen <rui@meetup.com>
2020-07-22 14:40:50 -0400

    frugal 3.9.7
    Closes #58410.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/frugal.rb
+++ Formula/frugal.rb
@@ -1,8 +1,8 @@
 class Frugal < Formula
   desc "Cross language code generator for creating scalable microservices"
   homepage "https://github.com/Workiva/frugal"
-  url "https://github.com/Workiva/frugal/archive/v3.9.6.tar.gz"
-  sha256 "579aa376d6e9f8c406a8f8ade0635d8eae807337a12c6189633225763e221cea"
+  url "https://github.com/Workiva/frugal/archive/v3.9.7.tar.gz"
+  sha256 "24fcc61c9416eb87d92c8cbbcd80b4ed7f9ba2f6b34df09925de8bd96041a3e6"
   license "Apache-2.0"
 
   bottle do
