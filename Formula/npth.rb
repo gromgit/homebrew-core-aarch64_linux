ffdc91aae1d3c49af4b973c6e0713c0224bd4a36
miccal <miccal.matthews@gmail.com>
2020-07-22 10:57:47 +0800

    npth: add license

--- Formula/npth.rb
+++ Formula/npth.rb
@@ -4,6 +4,7 @@ class Npth < Formula
   url "https://gnupg.org/ftp/gcrypt/npth/npth-1.6.tar.bz2"
   mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/npth/npth-1.6.tar.bz2"
   sha256 "1393abd9adcf0762d34798dc34fdcf4d0d22a8410721e76f1e3afcd1daa4e2d1"
+  license "LGPL-2.1"
 
   bottle do
     cellar :any
