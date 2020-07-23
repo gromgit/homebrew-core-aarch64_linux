57facb2dcc84d373de5a34279e0a14329bbf06c3
Dustin Rodrigues <dust.rod@gmail.com>
2020-07-23 00:54:14 -0400

    scrcpy: rely on --buildtype=release in std_meson_args

--- Formula/scrcpy.rb
+++ Formula/scrcpy.rb
@@ -30,7 +30,6 @@ class Scrcpy < Formula
 
     mkdir "build" do
       system "meson", *std_meson_args,
-                      "--buildtype=release",
                       "-Dprebuilt_server=#{buildpath}/prebuilt-server.jar",
                       ".."
 
