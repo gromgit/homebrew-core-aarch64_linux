64c4c10962410e20303606cf397e974cce821831
Seeker <meaningseeking@protonmail.com>
2020-07-23 20:22:41 +0000

    ldpl: add 4.4 bottle.

--- Formula/ldpl.rb
+++ Formula/ldpl.rb
@@ -5,6 +5,13 @@ class Ldpl < Formula
   sha256 "c34fb7d67d45050c7198f83ec9bb0b7790f78df8c6d99506c37141ccd2ac9ff1"
   license "Apache-2.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7e5cd92ebf4f0babb34d7af78189e7915731fad5fac39e66d63ecbbce86a72d0" => :catalina
+    sha256 "b9a0fdeb6134828ef4f60d81339185c5ac5a86123d6301035cbfb3b45c1a91ed" => :mojave
+    sha256 "01f2a987ba4b74d1b50374c7a9a616703a2a8ad479aaad8b80ed8e936af91d80" => :high_sierra
+  end
+
   def install
     cd "src" do
       system "make"
