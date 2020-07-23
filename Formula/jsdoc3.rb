bf451532368ceab5867de2d438cd48b811795e57
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-23 04:08:13 +0000

    jsdoc3: update 3.6.5 bottle.

--- Formula/jsdoc3.rb
+++ Formula/jsdoc3.rb
@@ -10,9 +10,9 @@ class Jsdoc3 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "8f873274000b4e3d0959b91bfc67e7019861171d980194e65bdeec2295f6699c" => :catalina
-    sha256 "55423238074c5cef313aee63947353a5f785af4fba129ab26c21d49654c81b43" => :mojave
-    sha256 "babfa75687211cb6aa6e7d9466bed814eb43b25ef0f5c6ada5352e5179a7eae2" => :high_sierra
+    sha256 "f27ce46b59636cf0b06c0857e59c5cc7de89f59524e1597c0efd05d48d02299a" => :catalina
+    sha256 "d33bd4efc75166f484d496cfc58d1273bb6f42a73f220841efdaca1e54be37ce" => :mojave
+    sha256 "47d6b79de54abbd92259da7b2101125158a07949195e760ab385fd088c086379" => :high_sierra
   end
 
   depends_on "node"
