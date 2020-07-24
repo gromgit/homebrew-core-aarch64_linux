c8a03fe8329c0e568028e0b952fa75973476fcbb
chenrui <chenrui333@gmail.com>
2020-07-23 22:16:33 -0400

    geoserver 2.17.2 (#58317)

--- Formula/geoserver.rb
+++ Formula/geoserver.rb
@@ -1,8 +1,8 @@
 class Geoserver < Formula
   desc "Java server to share and edit geospatial data"
   homepage "http://geoserver.org/"
-  url "https://downloads.sourceforge.net/project/geoserver/GeoServer/2.17.1/geoserver-2.17.1-bin.zip"
-  sha256 "9ee69f896fb1e5f8b3d7adaff910ffccb70fcae803ccf974bc436bed8950704f"
+  url "https://downloads.sourceforge.net/project/geoserver/GeoServer/2.17.2/geoserver-2.17.2-bin.zip"
+  sha256 "2469c4f8735dea0604dabe975c37663bbaf9adc569cc888ce24b9e987dc06400"
 
   bottle :unneeded
 
