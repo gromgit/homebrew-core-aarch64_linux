f259c36ed2ef7fc259dd4a92dab1c3d2347fe1bf
Issy Long <me@issyl0.co.uk>
2020-07-25 21:46:58 +0000

    janet: update 1.11.1 bottle.

--- Formula/janet.rb
+++ Formula/janet.rb
@@ -8,9 +8,9 @@ class Janet < Formula
 
   bottle do
     cellar :any
-    sha256 "0fc6ab9e55ee6cc6cb1fe4f17f4c10d010e99faa79bf24628e05b63031b5ba93" => :catalina
-    sha256 "991ec3cf8e7e8d7ef2a0391816820b8e41dd67f76eb656a79b7adbc3dc1fe055" => :mojave
-    sha256 "7e0ee918f6df0c842fd09a41fb7a25e4f75dec32520f68e226f11c7e447c099b" => :high_sierra
+    sha256 "864da331114336a348b2ff3ef3c5aecd726f13844df8cb6bc69085ba0daa6394" => :catalina
+    sha256 "6446f5e32bffe1349d9a452efe126ac1cd407c28fb33ddef8ced27d138d9b79d" => :mojave
+    sha256 "6c387d9057c19cda172241c0cea0bea364c357ac2c1bfaa18718feb25170f4ac" => :high_sierra
   end
 
   depends_on "meson" => :build
