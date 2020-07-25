6f9858f1b432f0641fffd4294409ba33625763fb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-25 07:09:16 +0000

    ios-deploy: update 1.11.0 bottle.

--- Formula/ios-deploy.rb
+++ Formula/ios-deploy.rb
@@ -8,10 +8,9 @@ class IosDeploy < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "1b6bb2f1d4f8267797575e5fb2d42fcc4aef8798605b7ccd6a0b77362d37203d" => :catalina
-    sha256 "14e96bd659a3d47bed7d2e893f9912e5201c04fea54ce56df3642a5037ff621a" => :mojave
-    sha256 "f810b5465871c66bc706f06edef3fc1570b1c9e92c806d75742c4998f9c27ee6" => :high_sierra
+    sha256 "46c878ef32dd500901a6d12859f467d5bf55851ff5b26d508d140016a3c9e834" => :catalina
+    sha256 "b35fa700f91ecd1738ede8fb2a149c839f57841e831f8ac0971e42d4ae32d741" => :mojave
+    sha256 "19485d858ef8be95dff22feeb01e8367792caa8919cc8289d0893bd49640b3aa" => :high_sierra
   end
 
   depends_on :xcode => :build
