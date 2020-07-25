9b6d47b10429c189dd878fa7c08977176ab3c1f6
chenrui <chenrui333@gmail.com>
2020-07-25 01:32:40 +0000

    wgcf: add 1.0.6 bottle.

--- Formula/wgcf.rb
+++ Formula/wgcf.rb
@@ -6,6 +6,13 @@ class Wgcf < Formula
   license "MIT"
   head "https://github.com/ViRb3/wgcf.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "98aedcc0ea89206a598ad085bdda918ee87e7fd4d0953ea61bb11ee413425907" => :catalina
+    sha256 "76b8267b99558a8d59c633dce247b6c59fca66346fbd754c6ed13635efbfbbf2" => :mojave
+    sha256 "e8f1364e5ab689ccd79a48157f1913ae29d206f9ced7cececeb56e0ddd8177bd" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
