0f2273ca0002de5b78a9500f04e82b1cb21801a2
Rui Chen <rui@meetup.com>
2020-07-26 16:39:16 -0400

    httpry: add license

--- Formula/httpry.rb
+++ Formula/httpry.rb
@@ -3,6 +3,7 @@ class Httpry < Formula
   homepage "https://github.com/jbittel/httpry"
   url "https://github.com/jbittel/httpry/archive/httpry-0.1.8.tar.gz"
   sha256 "b3bcbec3fc6b72342022e940de184729d9cdecb30aa754a2c994073447468cf0"
+  license "GPL-2.0"
   head "https://github.com/jbittel/httpry.git"
 
   bottle do
