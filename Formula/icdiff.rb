93a520d1aada7ae3664e76d01af5f7b6d6ecb1e4
Rui Chen <rui@meetup.com>
2020-07-26 17:12:36 -0400

    icdiff: add license

--- Formula/icdiff.rb
+++ Formula/icdiff.rb
@@ -3,6 +3,7 @@ class Icdiff < Formula
   homepage "https://github.com/jeffkaufman/icdiff"
   url "https://github.com/jeffkaufman/icdiff/archive/release-1.9.5.tar.gz"
   sha256 "f2e3df30e93df92224538ef3c62a73891af92de4caf94e8117582835e1040fc7"
+  license "PSF-2.0"
   head "https://github.com/jeffkaufman/icdiff.git"
 
   bottle :unneeded
