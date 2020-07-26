9e769a7b21258aab6094c00fada9a59ac15b915e
Sean Molenaar <SMillerDev@users.noreply.github.com>
2020-07-26 03:37:55 +0200

    opencv@2 deprecate (#58461)
    * opencv@2 deprecate
    
    * Update opencv@2.rb
    
    Co-authored-by: chenrui <rui@meetup.com>

--- Formula/opencv@2.rb
+++ Formula/opencv@2.rb
@@ -11,6 +11,9 @@ class OpencvAT2 < Formula
     sha256 "b90a2e7e26ef9d18a2f87a954a786a6bc983047fbcae2280b662df66e254e76c" => :high_sierra
   end
 
+  # https://www.slideshare.net/EugeneKhvedchenya/opencv-30-latest-news-and-the-roadmap
+  deprecate! :date => "2015-02-01"
+
   keg_only :versioned_formula
 
   depends_on "cmake" => :build
