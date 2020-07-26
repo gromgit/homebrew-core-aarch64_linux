431ae12299b683ee4b2a6ebf2f83b7f7fac23bd1
Brona <brona@robenek.me>
2020-07-26 21:03:17 +0200

    iproute2mac 1.3.0
    Upgrading iproute2mac and adding Python 3 dependency as Python 2 is no logner supported by iproute2mac.
    
    Closes #58654.
    
    Signed-off-by: Seeker <meaningseeking@protonmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/iproute2mac.rb
+++ Formula/iproute2mac.rb
@@ -1,14 +1,19 @@
 class Iproute2mac < Formula
+  include Language::Python::Shebang
+
   desc "CLI wrapper for basic network utilities on macOS - ip command"
   homepage "https://github.com/brona/iproute2mac"
-  url "https://github.com/brona/iproute2mac/releases/download/v1.2.3/iproute2mac-1.2.3.tar.gz"
-  sha256 "95ef8d4b0e32e4d3e3d975afa11d7aa0797d59b0f2c21b73a4e26d357bd6f93f"
+  url "https://github.com/brona/iproute2mac/releases/download/v1.3.0/iproute2mac-1.3.0.tar.gz"
+  sha256 "3fefce6b0f5e166355fdb04934cbdd906211b64e5adb6a385469696dc51233b7"
   license "MIT"
 
   bottle :unneeded
 
+  depends_on "python@3.8"
+
   def install
     bin.install "src/ip.py" => "ip"
+    rewrite_shebang detected_python_shebang, bin/"ip"
   end
 
   test do
