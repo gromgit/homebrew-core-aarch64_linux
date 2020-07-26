3b199e247c6ea30490285faa92c6f2c15932cd46
Rui Chen <rui@meetup.com>
2020-07-25 20:58:35 -0400

    bde 3.59.0.1
    Closes #58627.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bde.rb
+++ Formula/bde.rb
@@ -1,8 +1,8 @@
 class Bde < Formula
   desc "Basic Development Environment: foundational C++ libraries used at Bloomberg"
   homepage "https://github.com/bloomberg/bde"
-  url "https://github.com/bloomberg/bde/archive/3.58.0.1.tar.gz"
-  sha256 "d4ce069bc8e170b983738350ad6cafd76ed23c205580604e82f9f738277b8001"
+  url "https://github.com/bloomberg/bde/archive/3.59.0.1.tar.gz"
+  sha256 "ab8c41f7b7ebdbd3e0949a4a4521f34f7b08e1e8626b84158b5ad895d1abf689"
   license "Apache-2.0"
 
   bottle do
