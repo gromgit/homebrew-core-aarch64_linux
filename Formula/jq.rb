81e08b0d9ae47a9968fabde84378faab1f67e557
Rui Chen <rui@meetup.com>
2020-07-26 17:41:15 -0400

    jq: add license

--- Formula/jq.rb
+++ Formula/jq.rb
@@ -3,6 +3,7 @@ class Jq < Formula
   homepage "https://stedolan.github.io/jq/"
   url "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-1.6.tar.gz"
   sha256 "5de8c8e29aaa3fb9cc6b47bb27299f271354ebb72514e3accadc7d38b5bbaa72"
+  license "MIT"
 
   bottle do
     cellar :any
