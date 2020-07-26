d21725b2394918fbfe40b36fb40d7cfd922c9ee5
Rui Chen <rui@meetup.com>
2020-07-26 10:49:39 -0400

    simple-mtpfs 0.4.0
    fix style issue
    
    Closes #58651.
    
    Signed-off-by: Seeker <meaningseeking@protonmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/simple-mtpfs.rb
+++ Formula/simple-mtpfs.rb
@@ -1,8 +1,8 @@
 class SimpleMtpfs < Formula
   desc "Simple MTP fuse filesystem driver"
   homepage "https://github.com/phatina/simple-mtpfs"
-  url "https://github.com/phatina/simple-mtpfs/archive/simple-mtpfs-0.3.0.tar.gz"
-  sha256 "5556cae4414254b071d79ce656cce866b42fd7ba40ce480abfc3ba4e357cd491"
+  url "https://github.com/phatina/simple-mtpfs/archive/v0.4.0.tar.gz"
+  sha256 "1d011df3fa09ad0a5c09d48d84c03e6cddf86390af9eb4e0c178193f32f0e2fc"
   license "GPL-2.0"
 
   bottle do
@@ -16,17 +16,17 @@ class SimpleMtpfs < Formula
   end
 
   depends_on "autoconf" => :build
+  depends_on "autoconf-archive" => :build # required for AX_CXX_COMPILE_STDCXX_17
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
   depends_on "libmtp"
   depends_on :osxfuse
 
   def install
-    ENV.cxx11
-
     system "./autogen.sh"
-    system "./configure", "--prefix=#{prefix}", "CPPFLAGS=-I/usr/local/include/osxfuse",
-    "LDFLAGS=-L/usr/local/include/osxfuse"
+    system "./configure", "--prefix=#{prefix}",
+      "CPPFLAGS=-I/usr/local/include/osxfuse -I/usr/local/include/osxfuse/fuse",
+      "LDFLAGS=-L/usr/local/include/osxfuse"
     system "make"
     system "make", "install"
   end
