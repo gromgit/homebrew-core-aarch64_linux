6f791098012483525a708b3b6fb47a597d1fab08
Rui Chen <rui@meetup.com>
2020-07-26 17:12:42 -0400

    ipsumdump: add license

--- Formula/ipsumdump.rb
+++ Formula/ipsumdump.rb
@@ -3,6 +3,7 @@ class Ipsumdump < Formula
   homepage "https://read.seas.harvard.edu/~kohler/ipsumdump/"
   url "https://read.seas.harvard.edu/~kohler/ipsumdump/ipsumdump-1.86.tar.gz"
   sha256 "e114cd01b04238b42cd1d0dc6cfb8086a6b0a50672a866f3d0d1888d565e3b9c"
+  license "MIT"
   head "https://github.com/kohler/ipsumdump.git"
 
   bottle do
