4081be1bc003f53745450a38a29eca39e5a3a785
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-26 16:09:11 +0000

    hmmer: update 3.3.1 bottle.

--- Formula/hmmer.rb
+++ Formula/hmmer.rb
@@ -6,9 +6,9 @@ class Hmmer < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6cafc97dbddaa66d9e353f161ce402786d887aea8a88f52be3323ffc4cf5733b" => :catalina
-    sha256 "544c93b2fc808d5176ac562689afb59785f24ef29f3198b8ca24adf2c5fab422" => :mojave
-    sha256 "aac6c2a8264ae6192bff7dd02c57cfe5000e830a0991c9e5be8c0fc27b0daded" => :high_sierra
+    sha256 "438b4330b30b95486261d55bf3073bd2032842f3cd4acf64d295324440db7b6b" => :catalina
+    sha256 "cf633a2db613b4c5e95632d125a737fe978bec225ec3df48fb4c356dc41ed385" => :mojave
+    sha256 "c86672e08a73cd19f2cf3f3f02f9907d85c9ab9a7a716aa1861845f001902497" => :high_sierra
   end
 
   def install
