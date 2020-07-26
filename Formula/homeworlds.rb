8cf961a3f0c5a59a567d1fc9e0b103a0c3c30add
Rui Chen <rui@meetup.com>
2020-07-26 16:39:15 -0400

    homeworlds: add license

--- Formula/homeworlds.rb
+++ Formula/homeworlds.rb
@@ -4,6 +4,7 @@ class Homeworlds < Formula
   url "https://github.com/Quuxplusone/Homeworlds.git",
     :revision => "917cd7e7e6d0a5cdfcc56cd69b41e3e80b671cde"
   version "20141022"
+  license "BSD-2-Clause"
 
   bottle do
     cellar :any
