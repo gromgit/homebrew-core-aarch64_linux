3c321a438d133fbf7a22899e3e221fc09e37beb2
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:57 +0100

    vert.x: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/vert.x.rb
+++ Formula/vert.x.rb
@@ -12,7 +12,7 @@ class VertX < Formula
     rm_f Dir["bin/*.bat"]
     libexec.install %w[bin conf lib]
     (bin/"vertx").write_env_script "#{libexec}/bin/vertx",
-      :JAVA_HOME => "${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
+      JAVA_HOME: "${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
   end
 
   test do
