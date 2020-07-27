98434eede8f63dfa9489fab9992f39725ab8162b
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:14:26 +0100

    chapel: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/chapel.rb
+++ Formula/chapel.rb
@@ -31,7 +31,7 @@ class Chapel < Formula
 
     # Install chpl and other binaries (e.g. chpldoc) into bin/ as exec scripts.
     bin.install Dir[libexec/"bin/darwin-x86_64/*"]
-    bin.env_script_all_files libexec/"bin/darwin-x86_64/", :CHPL_HOME => libexec
+    bin.env_script_all_files libexec/"bin/darwin-x86_64/", CHPL_HOME: libexec
     man1.install_symlink Dir["#{libexec}/man/man1/*.1"]
   end
 
