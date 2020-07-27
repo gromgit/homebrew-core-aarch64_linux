230d66d27f188b4a741ffac2dd0a58faf51659da
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:16:31 +0100

    keepassc: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/keepassc.rb
+++ Formula/keepassc.rb
@@ -37,7 +37,7 @@ class Keepassc < Formula
     man1.install Dir["*.1"]
 
     bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec+"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    bin.env_script_all_files(libexec+"bin", PYTHONPATH: ENV["PYTHONPATH"])
   end
 
   test do
