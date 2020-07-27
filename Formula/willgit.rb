c2c6d0cd5ff0b969057c9db0e2f106fb1b5a6d82
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:08 +0100

    willgit: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/willgit.rb
+++ Formula/willgit.rb
@@ -9,7 +9,7 @@ class Willgit < Formula
   bottle :unneeded
 
   conflicts_with "git-utils",
-    :because => "both install a `git-rank-contributors` script"
+    because: "both install a `git-rank-contributors` script"
 
   def install
     prefix.install "bin"
