7197e2ffd08a89447813973b3e124e17fc6ea432
Seeker <meaningseeking@protonmail.com>
2020-07-26 19:31:08 -0700

    jinx 1.1.8 (new formula)
    Closes #58679.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: Seeker <meaningseeking@protonmail.com>

---
+++ Formula/jinx.rb
@@ -0,0 +1,51 @@
+class Jinx < Formula
+  desc "Embeddable scripting language for real-time applications"
+  homepage "https://github.com/JamesBoer/Jinx"
+  url "https://github.com/JamesBoer/Jinx/archive/v1.1.8.tar.gz"
+  sha256 "9388305d55e61cfb41eae1b50b0144668d39eb512d40af83efdfbccc1ab8ad1a"
+  license "MIT"
+  head "https://github.com/JamesBoer/Jinx.git"
+
+  depends_on "cmake" => :build
+
+  def install
+    # disable building tests
+    inreplace "CMakeLists.txt", "if(NOT jinx_is_subproject)", "if(FALSE)"
+
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      lib.install "libJinx.a"
+    end
+
+    include.install Dir["Source/*.h"]
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include "Jinx.h"
+
+      int main() {
+        // Create the Jinx runtime object
+        auto runtime = Jinx::CreateRuntime();
+
+        // Text containing our Jinx script
+        const char * scriptText =
+        u8R"(
+
+        -- Use the core library
+        import core
+
+        -- Write to the debug output
+        write line "Hello, world!"
+
+        )";
+
+        // Create and execute a script object
+        auto script = runtime->ExecuteScript(scriptText);
+      }
+    EOS
+    system ENV.cxx, "-std=c++17", "test.cpp", "-I#{include}", "-L#{lib}", "-lJinx", "-o", "test"
+    assert_match "Hello, world!", shell_output("./test")
+  end
+end
