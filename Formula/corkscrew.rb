34d7ea6cc1b7b7619497db3da54cb681c83c2d85
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:14:38 +0100

    corkscrew: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/corkscrew.rb
+++ Formula/corkscrew.rb
@@ -32,7 +32,7 @@ class Corkscrew < Formula
     require "webrick/httpproxy"
 
     pid = fork do
-      proxy = WEBrick::HTTPProxyServer.new :Port => 8080
+      proxy = WEBrick::HTTPProxyServer.new Port: 8080
       proxy.start
     end
 
