2be9e77a435c4c6791caf57b170c572216449dde
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:12 +0100

    x265: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/x265.rb
+++ Formula/x265.rb
@@ -4,7 +4,7 @@ class X265 < Formula
   url "https://bitbucket.org/multicoreware/x265/downloads/x265_3.4.tar.gz"
   sha256 "c2047f23a6b729e5c70280d23223cb61b57bfe4ad4e8f1471eeee2a61d148672"
   license "GPL-2.0"
-  head "https://bitbucket.org/multicoreware/x265", :using => :hg
+  head "https://bitbucket.org/multicoreware/x265", using: :hg
 
   bottle do
     cellar :any
