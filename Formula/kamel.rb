1808370d5a28115fe3a8a6b55214491d304c0268
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:16:29 +0100

    kamel: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/kamel.rb
+++ Formula/kamel.rb
@@ -3,8 +3,8 @@ class Kamel < Formula
   homepage "https://camel.apache.org/"
 
   url "https://github.com/apache/camel-k.git",
-    :tag      => "v1.1.0",
-    :revision => "29d581a49eacb4d191ff665a559fdd569c6deef7"
+    tag:      "v1.1.0",
+    revision: "29d581a49eacb4d191ff665a559fdd569c6deef7"
   license "Apache-2.0"
   head "https://github.com/apache/camel-k.git"
 
