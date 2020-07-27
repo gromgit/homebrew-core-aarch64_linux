141c8b49b73332eb970f7ee3f88d0fa43ece4f7a
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:14:57 +0100

    docker-completion: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/docker-completion.rb
+++ Formula/docker-completion.rb
@@ -2,14 +2,14 @@ class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/docker-ce.git",
-      :tag      => "v19.03.12",
-      :revision => "48a66213fe1747e8873f849862ff3fb981899fc6"
+      tag:      "v19.03.12",
+      revision: "48a66213fe1747e8873f849862ff3fb981899fc6"
   license "Apache-2.0"
 
   bottle :unneeded
 
   conflicts_with "docker",
-    :because => "docker already includes these completion scripts"
+    because: "docker already includes these completion scripts"
 
   def install
     bash_completion.install "components/cli/contrib/completion/bash/docker"
