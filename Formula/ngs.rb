d679ed56f8b27caf3a26140fb864c8e30fed498f
Seeker <meaningseeking@protonmail.com>
2020-07-27 16:06:00 +0000

    ngs: add 0.2.8 bottle.

--- Formula/ngs.rb
+++ Formula/ngs.rb
@@ -6,6 +6,13 @@ class Ngs < Formula
   license "GPL-3.0"
   head "https://github.com/ngs-lang/ngs.git"
 
+  bottle do
+    cellar :any
+    sha256 "39660e7b8a4a88b41ab30ec43a832feb487c1813e57f713a3100af05fa8a762e" => :catalina
+    sha256 "a4a4fd7ddcfc2d51851a733c117dd3e6076e2ee9b8334fa852c4890a547fe0d4" => :mojave
+    sha256 "8a9b038543f6a2fb7f7cd065942c75e76ae971cde8ca448c2aff0bbef9fa59f1" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "pandoc" => :build
   depends_on "pkg-config" => :build
