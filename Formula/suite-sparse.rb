f9d1118daca575713866c105ac827ecd8bd7001e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:20 +0100

    suite-sparse: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/suite-sparse.rb
+++ Formula/suite-sparse.rb
@@ -17,7 +17,7 @@ class SuiteSparse < Formula
 
   uses_from_macos "m4"
 
-  conflicts_with "mongoose", :because => "suite-sparse vendors libmongoose.dylib"
+  conflicts_with "mongoose", because: "suite-sparse vendors libmongoose.dylib"
 
   def install
     mkdir "GraphBLAS/build" do
