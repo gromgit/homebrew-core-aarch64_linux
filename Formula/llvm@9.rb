2a35d9d02b9b5cd7266dc27c121c9d68675ddf0c
Rui Chen <rui@meetup.com>
2020-07-26 20:30:54 -0400

    llvm@9: add license

--- Formula/llvm@9.rb
+++ Formula/llvm@9.rb
@@ -3,6 +3,8 @@ class LlvmAT9 < Formula
   homepage "https://llvm.org/"
   url "https://github.com/llvm/llvm-project/releases/download/llvmorg-9.0.1/llvm-9.0.1.src.tar.xz"
   sha256 "00a1ee1f389f81e9979f3a640a01c431b3021de0d42278f6508391a2f0b81c9a"
+  # The LLVM Project is under the Apache License v2.0 with LLVM Exceptions
+  license "Apache-2.0"
   revision 2
 
   bottle do
