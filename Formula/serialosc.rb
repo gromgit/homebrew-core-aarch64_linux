e602f6bb57ba21370c246f152b0a79ef05572506
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:18:57 +0100

    serialosc: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/serialosc.rb
+++ Formula/serialosc.rb
@@ -2,8 +2,8 @@ class Serialosc < Formula
   desc "Opensound control server for monome devices"
   homepage "https://monome.org/docs/osc/"
   url "https://github.com/monome/serialosc.git",
-      :tag      => "v1.4.1",
-      :revision => "4fec6f11276dd302faf9ca8e0a8e126f273cf954"
+      tag:      "v1.4.1",
+      revision: "4fec6f11276dd302faf9ca8e0a8e126f273cf954"
   license "ISC"
   head "https://github.com/monome/serialosc.git"
 
