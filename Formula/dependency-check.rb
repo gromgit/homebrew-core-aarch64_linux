ba0c1254addd3c082286ef08e68342c2da05c368
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:14:51 +0100

    dependency-check: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/dependency-check.rb
+++ Formula/dependency-check.rb
@@ -15,7 +15,7 @@ class DependencyCheck < Formula
     libexec.install Dir["*"]
 
     (bin/"dependency-check").write_env_script libexec/"bin/dependency-check.sh",
-      :JAVA_HOME => Formula["openjdk"].opt_prefix
+      JAVA_HOME: Formula["openjdk"].opt_prefix
 
     (var/"dependencycheck").mkpath
     libexec.install_symlink var/"dependencycheck" => "data"
