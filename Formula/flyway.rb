95714779e7c88be6aeef02e9e4eebf6066cab037
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:18 +0100

    flyway: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -11,7 +11,7 @@ class Flyway < Formula
   def install
     rm Dir["*.cmd"]
     libexec.install Dir["*"]
-    (bin/"flyway").write_env_script libexec/"flyway", :JAVA_HOME => Formula["openjdk"].opt_prefix
+    (bin/"flyway").write_env_script libexec/"flyway", JAVA_HOME: Formula["openjdk"].opt_prefix
   end
 
   test do
