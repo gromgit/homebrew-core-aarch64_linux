96544f60a9eeb7328877f1a937647c1814f87a8d
Rui Chen <rui@meetup.com>
2020-07-26 20:30:53 -0400

    llvm: add license

--- Formula/llvm.rb
+++ Formula/llvm.rb
@@ -1,6 +1,8 @@
 class Llvm < Formula
   desc "Next-gen compiler infrastructure"
   homepage "https://llvm.org/"
+  # The LLVM Project is under the Apache License v2.0 with LLVM Exceptions
+  license "Apache-2.0"
   revision 3
   head "https://github.com/llvm/llvm-project.git"
 
