718b27607d0a14e2d6ddb2d45c236a14edc5e222
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:25 +0100

    switchaudio-osx: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/switchaudio-osx.rb
+++ Formula/switchaudio-osx.rb
@@ -15,7 +15,7 @@ class SwitchaudioOsx < Formula
     sha256 "515b762164648d739ae36f8c5013d250d84af1264bf3ee366ed35adae2f44208" => :el_capitan
   end
 
-  depends_on :xcode => :build
+  depends_on xcode: :build
 
   def install
     xcodebuild "-project", "AudioSwitcher.xcodeproj",
