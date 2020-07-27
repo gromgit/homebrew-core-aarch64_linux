3d52788795c6b92b8159ab0b330d12a0922b04a5
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:17:25 +0100

    mongrel2: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/mongrel2.rb
+++ Formula/mongrel2.rb
@@ -2,7 +2,7 @@ class Mongrel2 < Formula
   desc "Application, language, and network architecture agnostic web server"
   homepage "https://mongrel2.org/"
   license "BSD-3-Clause"
-  head "https://github.com/mongrel2/mongrel2.git", :branch => "develop"
+  head "https://github.com/mongrel2/mongrel2.git", branch: "develop"
 
   stable do
     url "https://github.com/mongrel2/mongrel2/releases/download/v1.11.0/mongrel2-v1.11.0.tar.bz2"
