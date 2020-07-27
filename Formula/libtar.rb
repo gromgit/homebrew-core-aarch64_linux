7da2bc08d1fd0b2adca950903b0e7f2c33fd765b
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:16:52 +0100

    libtar: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/libtar.rb
+++ Formula/libtar.rb
@@ -2,8 +2,8 @@ class Libtar < Formula
   desc "C library for manipulating POSIX tar files"
   homepage "https://repo.or.cz/libtar.git"
   url "https://repo.or.cz/libtar.git",
-      :tag      => "v1.2.20",
-      :revision => "0907a9034eaf2a57e8e4a9439f793f3f05d446cd"
+      tag:      "v1.2.20",
+      revision: "0907a9034eaf2a57e8e4a9439f793f3f05d446cd"
 
   bottle do
     cellar :any
