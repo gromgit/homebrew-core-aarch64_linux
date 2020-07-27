312f186ebd31bf1ce3218c152f111e4949ab2379
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:18:10 +0100

    petsc-complex: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/petsc-complex.rb
+++ Formula/petsc-complex.rb
@@ -19,7 +19,7 @@ class PetscComplex < Formula
   depends_on "scalapack"
   depends_on "suite-sparse"
 
-  conflicts_with "petsc", :because => "petsc must be installed with either real or complex support, not both"
+  conflicts_with "petsc", because: "petsc must be installed with either real or complex support, not both"
 
   def install
     system "./configure", "--prefix=#{prefix}",
