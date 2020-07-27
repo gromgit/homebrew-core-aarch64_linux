9583e228b52814d40d1966dab32734819a30bb95
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:04 +0100

    dshb: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/dshb.rb
+++ Formula/dshb.rb
@@ -13,7 +13,7 @@ class Dshb < Formula
     sha256 "e0aa0f64ac02e9244fc59773a966d1ac5755dc4a4c91c0dbcd92633c4330f14b" => :el_capitan
   end
 
-  depends_on :xcode => ["8.0", :build]
+  depends_on xcode: ["8.0", :build]
 
   def install
     system "make", "release"
