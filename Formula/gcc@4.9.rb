7a438ca3bfdbea800be73fcce368749ad0eaf699
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:24 +0100

    gcc@4.9: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/gcc@4.9.rb
+++ Formula/gcc@4.9.rb
@@ -21,7 +21,7 @@ class GccAT49 < Formula
     satisfy { MacOS::CLT.installed? }
   end
 
-  depends_on :maximum_macos => [:high_sierra, :build]
+  depends_on maximum_macos: [:high_sierra, :build]
 
   # GCC bootstraps itself, so it is OK to have an incompatible C++ stdlib
   cxxstdlib_check :skip
