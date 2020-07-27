528ff581be178810f23313e7009c1381900d2661
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:16:46 +0100

    libextractor: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/libextractor.rb
+++ Formula/libextractor.rb
@@ -15,8 +15,8 @@ class Libextractor < Formula
   depends_on "pkg-config" => :build
   depends_on "libtool"
 
-  conflicts_with "csound", :because => "both install `extract` binaries"
-  conflicts_with "pkcrack", :because => "both install `extract` binaries"
+  conflicts_with "csound", because: "both install `extract` binaries"
+  conflicts_with "pkcrack", because: "both install `extract` binaries"
 
   def install
     ENV.deparallelize
