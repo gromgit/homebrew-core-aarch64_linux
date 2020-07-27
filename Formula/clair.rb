b40af9777939ce14c885a2ff129d4f99a6a2c8f0
Seeker <meaningseeking@protonmail.com>
2020-07-27 06:55:11 +0000

    clair: add 2.1.4 bottle.

--- Formula/clair.rb
+++ Formula/clair.rb
@@ -5,6 +5,13 @@ class Clair < Formula
   sha256 "444e109091ddc49e00277e38ddf9456a53243ab70f2560ab927f4d35b53555f4"
   license "Apache-2.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d310d5c7a3596a17612fd5b56d6c321129ecfae40d4fa7dc5032056c863b4dc3" => :catalina
+    sha256 "8b48f7520edfa1b74124b848de69802f127d476968a7c5471bd6174a43fc9899" => :mojave
+    sha256 "6ea27e3eed1bbf53401a81d55d138e1f808a9cfb52ca857316b1c371258b9c34" => :high_sierra
+  end
+
   depends_on "go" => :build
   depends_on "rpm"
   depends_on "xz"
