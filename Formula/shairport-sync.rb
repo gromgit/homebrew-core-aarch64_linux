785f11894b62f2f90a599e95b372176e8fbad06e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:18:58 +0100

    shairport-sync: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/shairport-sync.rb
+++ Formula/shairport-sync.rb
@@ -3,7 +3,7 @@ class ShairportSync < Formula
   homepage "https://github.com/mikebrady/shairport-sync"
   url "https://github.com/mikebrady/shairport-sync/archive/3.3.6.tar.gz"
   sha256 "a8382affd25c473fa38ead5690148c6c3902098f359f9c881eefe139e1f49f49"
-  head "https://github.com/mikebrady/shairport-sync.git", :branch => "development"
+  head "https://github.com/mikebrady/shairport-sync.git", branch: "development"
 
   bottle do
     sha256 "3c2a973fbbe200704e35737226fd4e7ed6edad9f389253602fe5d14c50bb171e" => :catalina
