6420737ec88646cbf7caedfc5eb4ac20bf4e0c84
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:16:45 +0100

    libbluray: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/libbluray.rb
+++ Formula/libbluray.rb
@@ -21,7 +21,7 @@ class Libbluray < Formula
   end
 
   depends_on "ant" => :build
-  depends_on :java => ["1.8", :build]
+  depends_on java: ["1.8", :build]
   depends_on "pkg-config" => :build
   depends_on "fontconfig"
   depends_on "freetype"
