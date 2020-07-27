4e64854313eb30b8e279c8efc0265b706e65319b
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:17:35 +0100

    mysql-client: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/mysql-client.rb
+++ Formula/mysql-client.rb
@@ -14,7 +14,7 @@ class MysqlClient < Formula
 
   depends_on "cmake" => :build
   # GCC is not supported either, so exclude for El Capitan.
-  depends_on :macos => :sierra if DevelopmentTools.clang_build_version < 900
+  depends_on macos: :sierra if DevelopmentTools.clang_build_version < 900
   depends_on "openssl@1.1"
 
   uses_from_macos "libedit"
