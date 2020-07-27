4001cd6084da06b521cac328fdc6c6daf847b024
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:53 +0100

    vamp-plugin-sdk: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/vamp-plugin-sdk.rb
+++ Formula/vamp-plugin-sdk.rb
@@ -3,7 +3,7 @@ class VampPluginSdk < Formula
   homepage "https://www.vamp-plugins.org/"
   url "https://code.soundsoftware.ac.uk/attachments/download/2691/vamp-plugin-sdk-2.10.0.tar.gz"
   sha256 "aeaf3762a44b148cebb10cde82f577317ffc9df2720e5445c3df85f3739ff75f"
-  head "https://code.soundsoftware.ac.uk/hg/vamp-plugin-sdk", :using => :hg
+  head "https://code.soundsoftware.ac.uk/hg/vamp-plugin-sdk", using: :hg
 
   bottle do
     cellar :any
