a6b342bf88303619e62ae8da4ce6686a78fe5f1e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:15 +0100

    standardese: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/standardese.rb
+++ Formula/standardese.rb
@@ -2,8 +2,8 @@ class Standardese < Formula
   desc "Next-gen documentation generator for C++"
   homepage "https://standardese.github.io"
   url "https://github.com/standardese/standardese.git",
-      :tag      => "v0.4.0",
-      :revision => "dc1e327df74c83ceb476ff3e74920cba021c7cc9"
+      tag:      "v0.4.0",
+      revision: "dc1e327df74c83ceb476ff3e74920cba021c7cc9"
   license "MIT"
   head "https://github.com/standardese/standardese.git"
 
