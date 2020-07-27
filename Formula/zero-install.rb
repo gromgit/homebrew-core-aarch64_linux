daccf20105b633ddf19cf7c350fe71ee5701f23e
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:19 +0100

    zero-install: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/zero-install.rb
+++ Formula/zero-install.rb
@@ -2,8 +2,8 @@ class ZeroInstall < Formula
   desc "Zero Install is a decentralised software installation system"
   homepage "https://0install.net/"
   url "https://github.com/0install/0install.git",
-      :tag      => "v2.17",
-      :revision => "4a837bd638d93905b96d073c28c644894f8d4a0b"
+      tag:      "v2.17",
+      revision: "4a837bd638d93905b96d073c28c644894f8d4a0b"
   license "LGPL-2.1"
   head "https://github.com/0install/0install.git"
 
