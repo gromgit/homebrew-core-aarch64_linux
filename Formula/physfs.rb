a5c9d1639590a322cc32b4f02256356557d3ac45
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:18:12 +0100

    physfs: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/physfs.rb
+++ Formula/physfs.rb
@@ -3,7 +3,7 @@ class Physfs < Formula
   homepage "https://icculus.org/physfs/"
   url "https://icculus.org/physfs/downloads/physfs-3.0.2.tar.bz2"
   sha256 "304df76206d633df5360e738b138c94e82ccf086e50ba84f456d3f8432f9f863"
-  head "https://hg.icculus.org/icculus/physfs/", :using => :hg
+  head "https://hg.icculus.org/icculus/physfs/", using: :hg
 
   bottle do
     cellar :any
