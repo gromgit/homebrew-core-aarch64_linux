d41c6f2c141819399730359c6909e49aa14c15eb
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:51 +0100

    grails: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/grails.rb
+++ Formula/grails.rb
@@ -13,7 +13,7 @@ class Grails < Formula
     rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
     libexec.install Dir["*"]
     bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files libexec/"bin", :JAVA_HOME => "${JAVA_HOME:-#{Formula["openjdk@11"].opt_prefix}}"
+    bin.env_script_all_files libexec/"bin", JAVA_HOME: "${JAVA_HOME:-#{Formula["openjdk@11"].opt_prefix}}"
   end
 
   def caveats
