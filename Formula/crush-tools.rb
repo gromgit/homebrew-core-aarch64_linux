f0f59b7e64c65dfebce49f6fddb30f5a6161863c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:14:43 +0100

    crush-tools: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/crush-tools.rb
+++ Formula/crush-tools.rb
@@ -25,8 +25,8 @@ class CrushTools < Formula
 
   depends_on "pcre"
 
-  conflicts_with "aggregate", :because => "both install an `aggregate` binary"
-  conflicts_with "num-utils", :because => "both install an `range` binary"
+  conflicts_with "aggregate", because: "both install an `aggregate` binary"
+  conflicts_with "num-utils", because: "both install an `range` binary"
 
   def install
     system "./bootstrap" if build.head?
