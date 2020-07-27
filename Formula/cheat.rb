2a886f96ee3ddf629e53e5e121bab8c06c466995
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:14:26 +0100

    cheat: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/cheat.rb
+++ Formula/cheat.rb
@@ -14,7 +14,7 @@ class Cheat < Formula
 
   depends_on "go" => :build
 
-  conflicts_with "bash-snippets", :because => "both install a `cheat` executable"
+  conflicts_with "bash-snippets", because: "both install a `cheat` executable"
 
   def install
     system "go", "build", "-mod", "vendor", "-o", bin/"cheat", "./cmd/cheat"
