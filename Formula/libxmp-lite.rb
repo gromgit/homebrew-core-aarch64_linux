939d14c113d19cd30143d65a9bb4593796f8d2e3
Rui Chen <rui@meetup.com>
2020-07-26 20:30:52 -0400

    libxmp-lite: add license

--- Formula/libxmp-lite.rb
+++ Formula/libxmp-lite.rb
@@ -3,6 +3,7 @@ class LibxmpLite < Formula
   homepage "https://xmp.sourceforge.io"
   url "https://downloads.sourceforge.net/project/xmp/libxmp/4.4.1/libxmp-lite-4.4.1.tar.gz"
   sha256 "bce9cbdaa19234e08e62660c19ed9a190134262066e7f8c323ea8ad2ac20dc39"
+  license "MIT"
 
   bottle do
     cellar :any
