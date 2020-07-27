634951c9ce25ce7ea2fffec50eafe8b265a1bf07
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:20:05 +0100

    watch: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/watch.rb
+++ Formula/watch.rb
@@ -2,8 +2,8 @@ class Watch < Formula
   desc "Executes a program periodically, showing output fullscreen"
   homepage "https://gitlab.com/procps-ng/procps"
   url "https://gitlab.com/procps-ng/procps.git",
-      :tag      => "v3.3.16",
-      :revision => "59c88e18f29000ceaf7e5f98181b07be443cf12f"
+      tag:      "v3.3.16",
+      revision: "59c88e18f29000ceaf7e5f98181b07be443cf12f"
   license "GPL-2.0"
   head "https://gitlab.com/procps-ng/procps.git"
 
