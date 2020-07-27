2827a68ca12ebeff8f8c50bf06e78fb41d8f467b
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:17:14 +0100

    metabase: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/metabase.rb
+++ Formula/metabase.rb
@@ -14,7 +14,7 @@ class Metabase < Formula
 
   bottle :unneeded
 
-  depends_on :java => "1.8"
+  depends_on java: "1.8"
 
   def install
     if build.head?
@@ -31,7 +31,7 @@ class Metabase < Formula
     EOS
   end
 
-  plist_options :startup => true, :manual => "metabase"
+  plist_options startup: true, manual: "metabase"
 
   def plist
     <<~EOS
