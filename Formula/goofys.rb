78f626a83dfbe5d60eeee0637d252713d29d1939
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:47 +0100

    goofys: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/goofys.rb
+++ Formula/goofys.rb
@@ -2,8 +2,8 @@ class Goofys < Formula
   desc "Filey-System interface to Amazon S3"
   homepage "https://github.com/kahing/goofys"
   url "https://github.com/kahing/goofys.git",
-      :tag      => "v0.24.0",
-      :revision => "45b8d78375af1b24604439d2e60c567654bcdf88"
+      tag:      "v0.24.0",
+      revision: "45b8d78375af1b24604439d2e60c567654bcdf88"
   license "Apache-2.0"
   head "https://github.com/kahing/goofys.git"
 
