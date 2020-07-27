3407e2d02b2228097a938a46c1e5e3de52026ccf
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:23 +0100

    g3log: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/g3log.rb
+++ Formula/g3log.rb
@@ -13,7 +13,7 @@ class G3log < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on :macos => :el_capitan # needs thread-local storage
+  depends_on macos: :el_capitan # needs thread-local storage
 
   def install
     system "cmake", ".", *std_cmake_args
