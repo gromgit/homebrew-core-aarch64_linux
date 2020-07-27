8bc3e83a4940b50e69c107eed894a912ea05eb86
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:53 +0100

    v8@3.15: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/v8@3.15.rb
+++ Formula/v8@3.15.rb
@@ -19,7 +19,7 @@ class V8AT315 < Formula
 
   resource "gyp" do
     url "https://chromium.googlesource.com/external/gyp.git",
-        :revision => "f7bc250ccc4d619a1cf238db87e5979f89ff36d7"
+        revision: "f7bc250ccc4d619a1cf238db87e5979f89ff36d7"
   end
 
   def install
