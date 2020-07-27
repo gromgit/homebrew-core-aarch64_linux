f716c533e2df275eff3bcac5ec71949d05dd0232
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:25 +0100

    gdal: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/gdal.rb
+++ Formula/gdal.rb
@@ -48,7 +48,7 @@ class Gdal < Formula
   depends_on "xz" # get liblzma compression algorithm library from XZutils
   depends_on "zstd"
 
-  conflicts_with "cpl", :because => "both install cpl_error.h"
+  conflicts_with "cpl", because: "both install cpl_error.h"
 
   def install
     args = [
