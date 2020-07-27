5420f8ac95348dc265c4347e47b095a4538762e7
Rui Chen <rui@meetup.com>
2020-07-26 22:11:33 -0400

    memcached: add license

--- Formula/memcached.rb
+++ Formula/memcached.rb
@@ -3,6 +3,7 @@ class Memcached < Formula
   homepage "https://memcached.org/"
   url "https://www.memcached.org/files/memcached-1.6.6.tar.gz"
   sha256 "908f0eecfa559129c9e44edc46f02e73afe8faca355b4efc5c86d902fc3e32f7"
+  license "BSD-3-Clause"
   head "https://github.com/memcached/memcached.git"
 
   bottle do
