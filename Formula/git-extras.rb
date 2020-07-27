76f637024be46b7b2a93deef78378ee7bad6adbb
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:33 +0100

    git-extras: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/git-extras.rb
+++ Formula/git-extras.rb
@@ -15,7 +15,7 @@ class GitExtras < Formula
   end
 
   conflicts_with "git-utils",
-    :because => "both install a `git-pull-request` script"
+    because: "both install a `git-pull-request` script"
 
   def install
     system "make", "PREFIX=#{prefix}", "INSTALL_VIA=brew", "install"
