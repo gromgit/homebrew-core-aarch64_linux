c5e3129aa9a9fa02aac009d93975b11774005346
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:17:05 +0100

    mallet: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/mallet.rb
+++ Formula/mallet.rb
@@ -18,7 +18,7 @@ class Mallet < Formula
     rm Dir["bin/*.{bat,dll,exe}"] # Remove all windows files
     libexec.install Dir["*"]
     bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files(libexec/"bin", :JAVA_HOME => Formula["openjdk"].opt_prefix)
+    bin.env_script_all_files(libexec/"bin", JAVA_HOME: Formula["openjdk"].opt_prefix)
   end
 
   test do
