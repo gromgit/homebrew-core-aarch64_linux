542e2e3f34e28770134e30781be04bb357387fe8
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:17:31 +0100

    mr: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/mr.rb
+++ Formula/mr.rb
@@ -2,8 +2,8 @@ class Mr < Formula
   desc "Multiple Repository management tool"
   homepage "https://myrepos.branchable.com/"
   url "git://myrepos.branchable.com/",
-      :tag      => "1.20180726",
-      :revision => "0ad7a17bb455de1fec3b2375c7aac72ab2a22ac4"
+      tag:      "1.20180726",
+      revision: "0ad7a17bb455de1fec3b2375c7aac72ab2a22ac4"
 
   bottle do
     cellar :any_skip_relocation
