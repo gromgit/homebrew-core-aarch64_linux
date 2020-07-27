41bf81cfffa0b823adf46965a55cabdfd01ae260
Rui Chen <rui@meetup.com>
2020-07-26 20:30:55 -0400

    luabind: add license

--- Formula/luabind.rb
+++ Formula/luabind.rb
@@ -3,6 +3,7 @@ class Luabind < Formula
   homepage "https://github.com/luabind/luabind"
   url "https://downloads.sourceforge.net/project/luabind/luabind/0.9.1/luabind-0.9.1.tar.gz"
   sha256 "80de5e04918678dd8e6dac3b22a34b3247f74bf744c719bae21faaa49649aaae"
+  license "MIT"
   revision 2
 
   bottle do
