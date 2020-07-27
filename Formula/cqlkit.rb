1813ffcd1938459e3c6ec98b8a90d8217eef9ccf
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:14:42 +0100

    cqlkit: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/cqlkit.rb
+++ Formula/cqlkit.rb
@@ -13,7 +13,7 @@ class Cqlkit < Formula
     libexec.install %w[bin lib]
     rm_f Dir["#{libexec}/bin/*.bat"]
     bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files libexec/"bin", :JAVA_HOME => "${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
+    bin.env_script_all_files libexec/"bin", JAVA_HOME: "${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
   end
 
   test do
