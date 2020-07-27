a2e0c15a796d0ba2cea691f200e438075a52fc53
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:16:23 +0100

    jing-trang: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/jing-trang.rb
+++ Formula/jing-trang.rb
@@ -2,8 +2,8 @@ class JingTrang < Formula
   desc "Schema validation and conversion based on RELAX NG"
   homepage "http://www.thaiopensource.com/relaxng/"
   url "https://github.com/relaxng/jing-trang.git",
-      :tag      => "V20181222",
-      :revision => "a3ec4cd650f48ec00189578f314fbe94893cd92d"
+      tag:      "V20181222",
+      revision: "a3ec4cd650f48ec00189578f314fbe94893cd92d"
   license "BSD-3-Clause"
 
   bottle do
@@ -15,7 +15,7 @@ class JingTrang < Formula
   end
 
   depends_on "ant" => :build
-  depends_on :java => "1.8"
+  depends_on java: "1.8"
 
   def install
     system "./ant", "jar"
