9497123c189d9793db93181f809d34bab3b49127
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:18:00 +0100

    optipng: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/optipng.rb
+++ Formula/optipng.rb
@@ -3,7 +3,7 @@ class Optipng < Formula
   homepage "https://optipng.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/optipng/OptiPNG/optipng-0.7.7/optipng-0.7.7.tar.gz"
   sha256 "4f32f233cef870b3f95d3ad6428bfe4224ef34908f1b42b0badf858216654452"
-  head "http://hg.code.sf.net/p/optipng/mercurial", :using => :hg
+  head "http://hg.code.sf.net/p/optipng/mercurial", using: :hg
 
   bottle do
     cellar :any_skip_relocation
