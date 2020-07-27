db27066a95bafb2077a99ab6db98b7c5e4da0cdd
Rui Chen <rui@meetup.com>
2020-07-26 20:30:46 -0400

    libmowgli: add license

--- Formula/libmowgli.rb
+++ Formula/libmowgli.rb
@@ -3,6 +3,7 @@ class Libmowgli < Formula
   homepage "https://github.com/atheme/libmowgli-2"
   url "https://github.com/atheme/libmowgli-2/archive/v2.1.3.tar.gz"
   sha256 "b7faab2fb9f46366a52b51443054a2ed4ecdd04774c65754bf807c5e9bdda477"
+  license "ISC"
   revision 1
   head "https://github.com/atheme/libmowgli-2.git"
 
