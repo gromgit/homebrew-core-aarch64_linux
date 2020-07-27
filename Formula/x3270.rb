79c3eabf0a1ee342eb71e0436f6efa49a9e9774f
chimurai <655241+chimurai@users.noreply.github.com>
2020-07-27 20:49:14 +0000

    x3270 4.0ga10
    Closes #58711.
    
    Signed-off-by: Sam Ford <1584702+samford@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/x3270.rb
+++ Formula/x3270.rb
@@ -1,8 +1,9 @@
 class X3270 < Formula
   desc "IBM 3270 terminal emulator for the X Window System and Windows"
   homepage "http://x3270.bgp.nu/"
-  url "http://x3270.bgp.nu/download/04.00/suite3270-4.0ga9-src.tgz"
-  sha256 "32dd89393bc9987e0b1fc397a48509ba99a372f19ba21f4d9444034a5f26a24e"
+  url "http://x3270.bgp.nu/download/04.00/suite3270-4.0ga10-src.tgz"
+  sha256 "05db34a7508a0d61c95a43563472e61d0f7fa12ce0d5f3ed3e5f88e966d8d98c"
+  license "BSD-3-Clause"
 
   bottle do
     sha256 "0a97545f8a95a71631e033a52b15a99c36ba620d5e79a62fb8f4e947ed48b827" => :catalina
