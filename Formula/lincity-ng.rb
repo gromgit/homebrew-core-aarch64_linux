886752ae3204b53647af66585db4a9186de18c77
Rui Chen <rui@meetup.com>
2020-07-26 20:30:52 -0400

    lincity-ng: add license

--- Formula/lincity-ng.rb
+++ Formula/lincity-ng.rb
@@ -3,6 +3,7 @@ class LincityNg < Formula
   homepage "https://github.com/lincity-ng/lincity-ng/"
   url "https://github.com/lincity-ng/lincity-ng/archive/lincity-ng-2.0.tar.gz"
   sha256 "e05a2c1e1d682fbf289caecd0ea46ca84b0db9de43c7f1b5add08f0fdbf1456b"
+  license "GPL-2.0"
   revision 2
   head "https://github.com/lincity-ng/lincity-ng.git"
 
