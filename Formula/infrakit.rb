37165a2170821468334bff3c330e0ec946ed29b8
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:16:13 +0100

    infrakit: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/infrakit.rb
+++ Formula/infrakit.rb
@@ -2,8 +2,8 @@ class Infrakit < Formula
   desc "Toolkit for creating and managing declarative infrastructure"
   homepage "https://github.com/docker/deploykit"
   url "https://github.com/docker/deploykit.git",
-      :tag      => "v0.5",
-      :revision => "3d2670e484176ce474d4b3d171994ceea7054c02"
+      tag:      "v0.5",
+      revision: "3d2670e484176ce474d4b3d171994ceea7054c02"
   license "Apache-2.0"
 
   bottle do
