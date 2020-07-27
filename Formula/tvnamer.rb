50fc100b64cef835677cbdea90afca06406c4139
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:19:45 +0100

    tvnamer: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/tvnamer.rb
+++ Formula/tvnamer.rb
@@ -62,7 +62,7 @@ class Tvnamer < Formula
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
     system "python3", *Language::Python.setup_install_args(libexec)
     bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
   end
 
   test do
