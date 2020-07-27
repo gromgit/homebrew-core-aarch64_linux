32b510a5706f99b710bc3473c91aa178a3b7c9e9
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:14:18 +0100

    carina: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/carina.rb
+++ Formula/carina.rb
@@ -2,8 +2,8 @@ class Carina < Formula
   desc "Command-line client for Carina"
   homepage "https://github.com/getcarina/carina"
   url "https://github.com/getcarina/carina.git",
-        :tag      => "v2.1.3",
-        :revision => "2b3ec267e298e095d7c2f81a2d82dc50a720e81c"
+        tag:      "v2.1.3",
+        revision: "2b3ec267e298e095d7c2f81a2d82dc50a720e81c"
   license "Apache-2.0"
   head "https://github.com/getcarina/carina.git"
 
