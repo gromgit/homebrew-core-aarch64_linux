fc834b70b8b091789af2534a384e11bea17cfa02
Rui Chen <rui@meetup.com>
2020-07-26 22:11:30 -0400

    macvim: add license

--- Formula/macvim.rb
+++ Formula/macvim.rb
@@ -5,6 +5,7 @@ class Macvim < Formula
   url "https://github.com/macvim-dev/macvim/archive/snapshot-163.tar.gz"
   version "8.2-163"
   sha256 "3af72f22b25cf4f94b1b6e27a74d74bdefa8ed1529fe6edec59ae2756b3ca209"
+  license "Vim"
   revision 2
   head "https://github.com/macvim-dev/macvim.git"
 
