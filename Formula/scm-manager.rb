7426217ef78601289225b6c8191b232896fb032c
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:18:53 +0100

    scm-manager: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/scm-manager.rb
+++ Formula/scm-manager.rb
@@ -13,7 +13,7 @@ class ScmManager < Formula
     sha256 "42e177bd72cba3b27750308aeeaf8afa0ec8cc553b8a9acf1a02a6d5a698ce14" => :el_capitan
   end
 
-  depends_on :java => "1.8"
+  depends_on java: "1.8"
 
   resource "client" do
     url "https://maven.scm-manager.org/nexus/content/repositories/releases/sonia/scm/clients/scm-cli-client/1.59/scm-cli-client-1.59-jar-with-dependencies.jar"
@@ -46,7 +46,7 @@ class ScmManager < Formula
     chmod 0755, scm_cli_client
   end
 
-  plist_options :manual => "scm-server start"
+  plist_options manual: "scm-server start"
 
   def plist
     <<~EOS
