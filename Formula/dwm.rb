670cc03a2d7a6cc63fc9dab532c3b692f77dea37
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:15:04 +0100

    dwm: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/dwm.rb
+++ Formula/dwm.rb
@@ -4,7 +4,7 @@ class Dwm < Formula
   url "https://dl.suckless.org/dwm/dwm-6.2.tar.gz"
   sha256 "97902e2e007aaeaa3c6e3bed1f81785b817b7413947f1db1d3b62b8da4cd110e"
   revision 1
-  head "https://git.suckless.org/dwm", :using => :git
+  head "https://git.suckless.org/dwm", using: :git
 
   bottle do
     cellar :any_skip_relocation
