fa87877fff860fea87b9366858ef3879dd935519
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:17:07 +0100

    mariadb@10.2: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/mariadb@10.2.rb
+++ Formula/mariadb@10.2.rb
@@ -122,7 +122,7 @@ class MariadbAT102 < Formula
     EOS
   end
 
-  plist_options :manual => "#{HOMEBREW_PREFIX}/opt/mariadb@10.2/bin/mysql.server start"
+  plist_options manual: "#{HOMEBREW_PREFIX}/opt/mariadb@10.2/bin/mysql.server start"
 
   def plist
     <<~EOS
