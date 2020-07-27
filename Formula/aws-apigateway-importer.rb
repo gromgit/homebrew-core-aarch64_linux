35503ab3f37f7a0a8e3c617ab9bbd3b9ac9b61ea
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:14:01 +0100

    aws-apigateway-importer: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/aws-apigateway-importer.rb
+++ Formula/aws-apigateway-importer.rb
@@ -17,7 +17,7 @@ class AwsApigatewayImporter < Formula
   end
 
   depends_on "maven" => :build
-  depends_on :java => "1.8"
+  depends_on java: "1.8"
 
   # Pin aws-sdk-java-core for JSONObject compatibility
   patch do
