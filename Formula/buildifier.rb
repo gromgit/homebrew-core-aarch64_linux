b251aa108711e08196d77c374cba52b6caa45d45
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:14:13 +0100

    buildifier: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/buildifier.rb
+++ Formula/buildifier.rb
@@ -2,8 +2,8 @@ class Buildifier < Formula
   desc "Format bazel BUILD files with a standard convention"
   homepage "https://github.com/bazelbuild/buildtools"
   url "https://github.com/bazelbuild/buildtools.git",
-      :tag      => "3.4.0",
-      :revision => "b1667ff58f714d13c2bba6823d6c52214705508f"
+      tag:      "3.4.0",
+      revision: "b1667ff58f714d13c2bba6823d6c52214705508f"
   license "Apache-2.0"
 
   bottle do
