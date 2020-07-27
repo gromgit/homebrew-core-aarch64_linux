4a2da0cca2b6e7312af5b28000cc429a9112c668
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 10:18:34 +0100

    rakudo-star: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/rakudo-star.rb
+++ Formula/rakudo-star.rb
@@ -17,7 +17,7 @@ class RakudoStar < Formula
   depends_on "libffi"
   depends_on "pcre"
 
-  conflicts_with "moarvm", "nqp", :because => "rakudo-star currently ships with moarvm and nqp included"
+  conflicts_with "moarvm", "nqp", because: "rakudo-star currently ships with moarvm and nqp included"
   conflicts_with "parrot"
   conflicts_with "rakudo"
 
