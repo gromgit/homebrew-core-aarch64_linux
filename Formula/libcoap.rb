23768678f04ea471b2f5e1921e8b6e83b4111026
Rui Chen <rui@meetup.com>
2020-07-26 20:30:41 -0400

    libcoap: add license

--- Formula/libcoap.rb
+++ Formula/libcoap.rb
@@ -3,6 +3,7 @@ class Libcoap < Formula
   homepage "https://github.com/obgm/libcoap"
   url "https://github.com/obgm/libcoap/archive/v4.2.1.tar.gz"
   sha256 "29a0394a265d3febee41e5e2dc03d34292a0aede37f5f80334e529ac0dab2321"
+  license "BSD-2-Clause"
 
   bottle do
     cellar :any
