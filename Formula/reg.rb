b50a6345209cdeb1000dbf0ea9832445a5740272
Seeker <meaningseeking@protonmail.com>
2020-07-28 06:47:35 +0000

    reg: add 0.16.1 bottle.

--- Formula/reg.rb
+++ Formula/reg.rb
@@ -5,6 +5,13 @@ class Reg < Formula
   sha256 "b65787bff71bff21f21adc933799e70aa9b868d19b1e64f8fd24ebdc19058430"
   license "MIT"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "566141035e7c94c92a4422addea68ea86431916055d14bfe5e20de79c3a6451c" => :catalina
+    sha256 "fc74e858cf6aa00783292b40d24ddbe0597d53c0e2f04c66dbbb0f103cbb50ec" => :mojave
+    sha256 "6c834ffc790787be203c01f7d153971f34d4c75f70245058717e4a13f0afcf79" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
