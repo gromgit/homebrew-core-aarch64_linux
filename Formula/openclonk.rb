a1f284c9a031e7daf1db637a0734c40ee510883b
Rui Chen <rui@meetup.com>
2020-07-28 09:28:06 -0400

    openclonk: add license

--- Formula/openclonk.rb
+++ Formula/openclonk.rb
@@ -3,6 +3,7 @@ class Openclonk < Formula
   homepage "https://www.openclonk.org/"
   url "https://www.openclonk.org/builds/release/7.0/openclonk-7.0-src.tar.bz2"
   sha256 "bc1a231d72774a7aa8819e54e1f79be27a21b579fb057609398f2aa5700b0732"
+  license "ISC"
   revision 2
   head "https://github.com/openclonk/openclonk", using: :git
 
