5509e6840356a2f665507e78d1c7b32dbc66b1b8
Rui Chen <rui@meetup.com>
2020-07-28 09:28:03 -0400

    octave: add license

--- Formula/octave.rb
+++ Formula/octave.rb
@@ -4,6 +4,7 @@ class Octave < Formula
   url "https://ftp.gnu.org/gnu/octave/octave-5.2.0.tar.xz"
   mirror "https://ftpmirror.gnu.org/octave/octave-5.2.0.tar.xz"
   sha256 "2757b5cc1854c9326d6c99d2900c7cec2909ac7ed500212d170d0df592bfd26b"
+  license "GPL-3.0"
   revision 9
 
   bottle do
