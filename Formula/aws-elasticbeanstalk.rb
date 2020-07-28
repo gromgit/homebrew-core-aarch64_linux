82c727a8781e3643292c2da8fd5202a3b69413ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-07-28 03:09:00 +0000

    aws-elasticbeanstalk: update 3.18.2 bottle.

--- Formula/aws-elasticbeanstalk.rb
+++ Formula/aws-elasticbeanstalk.rb
@@ -9,9 +9,9 @@ class AwsElasticbeanstalk < Formula
 
   bottle do
     cellar :any
-    sha256 "d14253acd05c343f517faa6d1f24a7ca4fa6290dd4ab28fd5a67769508dbd1fc" => :catalina
-    sha256 "52baaf0d0f9d339358d8b6241ae5b0f0b0d7f9b5bd870c986eeee9c2badddabd" => :mojave
-    sha256 "42cb725eb85274070418f47eb1300f010e79222da68f010840ec6b6831ce2517" => :high_sierra
+    sha256 "462a44e381a47a5726d384f2196317ac9ee55b062ba9baefbd6399cbdca728cb" => :catalina
+    sha256 "fb7fd3985498aad3f583dfbac8de442472ded847a9c9af8a33c2f2ec9f199b0d" => :mojave
+    sha256 "0645bf2f1c0824ad2e4dca435a9e4d44d8a0647d116072bf9db62dfefd5b5d38" => :high_sierra
   end
 
   depends_on "python@3.8"
