0688c0595886d42919ba383b1eaa0ec4405f0253
Dawid Dziurla <dawidd0811@gmail.com>
2020-07-28 12:38:34 +0000

    jsonnet-bundler: add 0.4.0 bottle.

--- Formula/jsonnet-bundler.rb
+++ Formula/jsonnet-bundler.rb
@@ -6,6 +6,13 @@ class JsonnetBundler < Formula
     revision: "447344d5a038562d320a3f0dca052611ade29280"
   license "Apache-2.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1d90eef17450bddbe12e3bbc65d55d2b324a34f2cb36a028b32060e416e262c1" => :catalina
+    sha256 "a42bfdfd2d149e8667ff0849de992e91160e78aaed6f72f2624f9a191c83fe75" => :mojave
+    sha256 "e058cee6482accf6cc38bb4c404148464e4e071cecdb7191e1fbc6315e53f851" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
