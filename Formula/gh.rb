06b6233c8c886e9dc22e6f5f67ce9a3f1da363e2
Mislav Marohnić <mislav@github.com>
2020-07-28 16:27:54 +0200

    gh 0.11.1
    Closes #58734.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/gh.rb
+++ Formula/gh.rb
@@ -1,8 +1,9 @@
 class Gh < Formula
   desc "GitHub command-line tool"
   homepage "https://github.com/cli/cli"
-  url "https://github.com/cli/cli/archive/v0.11.0.tar.gz"
-  sha256 "e2137491f269df179ded7066f641ae7879a40870be53170a0388310e904158b0"
+  url "https://github.com/cli/cli.git",
+    tag:      "v0.11.1",
+    revision: "58bd549de5db008e9dd92cb6673b9ed85449d778"
   license "MIT"
 
   bottle do
@@ -15,12 +16,7 @@ class Gh < Formula
   depends_on "go" => :build
 
   def install
-    ldflags = %W[
-      -X github.com/cli/cli/command.Version=#{version}
-      -X github.com/cli/cli/command.BuildDate=#{Date.today}
-      -s -w
-    ]
-    system "make", "bin/gh", "manpages", "GO_LDFLAGS=#{ldflags.join(" ")}"
+    system "make", "bin/gh", "manpages"
     bin.install "bin/gh"
     man1.install Dir["share/man/man1/gh*.1"]
     (bash_completion/"gh").write `#{bin}/gh completion -s bash`
