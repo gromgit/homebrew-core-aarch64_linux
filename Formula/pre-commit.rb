9f63c82cc66fba11372ec3dcb7b799e13ca369b2
Seeker <meaningseeking@protonmail.com>
2020-07-28 09:01:04 -0700

    pre-commit: skip redundant cd to testpath

--- Formula/pre-commit.rb
+++ Formula/pre-commit.rb
@@ -97,25 +97,23 @@ class PreCommit < Formula
   end
 
   test do
-    testpath.cd do
-      system "git", "init"
-      (testpath/".pre-commit-config.yaml").write <<~EOS
-        -   repo: https://github.com/pre-commit/pre-commit-hooks
-            sha: v0.9.1
-            hooks:
-            -   id: trailing-whitespace
-      EOS
-      system bin/"pre-commit", "install"
-      (testpath/"f").write "hi\n"
-      system "git", "add", "f"
+    system "git", "init"
+    (testpath/".pre-commit-config.yaml").write <<~EOS
+      -   repo: https://github.com/pre-commit/pre-commit-hooks
+          sha: v0.9.1
+          hooks:
+          -   id: trailing-whitespace
+    EOS
+    system bin/"pre-commit", "install"
+    (testpath/"f").write "hi\n"
+    system "git", "add", "f"
 
-      ENV["GIT_AUTHOR_NAME"] = "test user"
-      ENV["GIT_AUTHOR_EMAIL"] = "test@example.com"
-      ENV["GIT_COMMITTER_NAME"] = "test user"
-      ENV["GIT_COMMITTER_EMAIL"] = "test@example.com"
-      git_exe = which("git")
-      ENV["PATH"] = "/usr/bin:/bin"
-      system git_exe, "commit", "-m", "test"
-    end
+    ENV["GIT_AUTHOR_NAME"] = "test user"
+    ENV["GIT_AUTHOR_EMAIL"] = "test@example.com"
+    ENV["GIT_COMMITTER_NAME"] = "test user"
+    ENV["GIT_COMMITTER_EMAIL"] = "test@example.com"
+    git_exe = which("git")
+    ENV["PATH"] = "/usr/bin:/bin"
+    system git_exe, "commit", "-m", "test"
   end
 end
