394f6c3c94791f26e2a0de559e244337faa7ccad
Dustin Rodrigues <dust.rod@gmail.com>
2020-07-28 00:33:00 -0400

    fcitx-remote-for-osx: fix urls

--- Formula/fcitx-remote-for-osx.rb
+++ Formula/fcitx-remote-for-osx.rb
@@ -1,7 +1,7 @@
 class FcitxRemoteForOsx < Formula
   desc "Handle input method in command-line"
-  homepage "https://github.com/CodeFalling/fcitx-remote-for-osx"
-  url "https://github.com/CodeFalling/fcitx-remote-for-osx/archive/0.3.0.tar.gz"
+  homepage "https://github.com/xcodebuild/fcitx-remote-for-osx"
+  url "https://github.com/xcodebuild/fcitx-remote-for-osx/archive/0.3.0.tar.gz"
   sha256 "b4490a6a0db3c28ce3ddbe89dd038f5ab404744539adc5520eab1a1a39819de6"
   license "GPL-3.0"
 
