62e56f07ad80e4e71f3c72e3786fb166729ce671
Rui Chen <rui@meetup.com>
2020-07-29 00:42:12 -0400

    protobuf@3.7: add license

--- Formula/protobuf@3.7.rb
+++ Formula/protobuf@3.7.rb
@@ -4,6 +4,7 @@ class ProtobufAT37 < Formula
   url "https://github.com/protocolbuffers/protobuf.git",
       tag:      "v3.7.1",
       revision: "6973c3a5041636c1d8dc5f7f6c8c1f3c15bc63d6"
+  license "BSD-3-Clause"
   revision 1
 
   bottle do
