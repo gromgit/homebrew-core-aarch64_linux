4a74ba26ac0b4dd8bc7cde4b69f8f08787e0785a
Rui Chen <rui@meetup.com>
2020-07-29 00:42:07 -0400

    partio: add license

--- Formula/partio.rb
+++ Formula/partio.rb
@@ -3,6 +3,7 @@ class Partio < Formula
   homepage "https://github.com/wdas/partio"
   url "https://github.com/wdas/partio/archive/v1.10.1.tar.gz"
   sha256 "49f0d61bcca4ac3900dc68fdf11fa325cec6fab6cedde37c5a2ce365b1b46112"
+  license "BSD-3-Clause"
 
   bottle do
     cellar :any
