65ebbb991e2043a657f9cb7f505efc3a96733b9d
Jonathan Chang <jchang641@gmail.com>
2020-07-29 22:13:22 +1000

    cucumber-cpp: update 0.5_6 bottle.

--- Formula/cucumber-cpp.rb
+++ Formula/cucumber-cpp.rb
@@ -8,9 +8,9 @@ class CucumberCpp < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "647928a1ca3316d5d7c46f1022b616f88c21fa9fab3ca147e8944a92377e2d67" => :catalina
-    sha256 "1913b19a2c3c876ae0ec6cf46bed0ea144cb97ab025b769472d9371c267e3764" => :mojave
-    sha256 "8d325d95cdd4a3dabcee3f9fe3184585dc5ddbae623033d157847a4ceca2bd45" => :high_sierra
+    sha256 "f63868e84ffc9bf22d0b25d5f0e8d48fd3153b7d0862adfcf8edd3afc852ed79" => :catalina
+    sha256 "a0826d69b4ad3b0109166578892bb9c17458813136684e841da790bd85b96c71" => :mojave
+    sha256 "2507d7b18568db8856e01e21ad211739a14f217a8c4b4df3cc871c122d3c0c74" => :high_sierra
   end
 
   depends_on "cmake" => :build
