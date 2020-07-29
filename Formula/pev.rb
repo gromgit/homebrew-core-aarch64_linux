c153aa6128a6937d257f73383a08a58221cf5941
Rui Chen <rui@meetup.com>
2020-07-29 00:42:08 -0400

    pev: add license

--- Formula/pev.rb
+++ Formula/pev.rb
@@ -3,6 +3,7 @@ class Pev < Formula
   homepage "https://pev.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/pev/pev-0.80/pev-0.80.tar.gz"
   sha256 "f68c8596f16d221d9a742812f6f728bcc739be90957bc1b00fbaa5943ffc5cfa"
+  license "GPL-2.0"
   revision 1
   head "https://github.com/merces/pev.git"
 
