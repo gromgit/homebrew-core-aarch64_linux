6180644f726f43eceb5dddfb8bee10ebf3c8de72
Dawid Dziurla <dawidd0811@gmail.com>
2020-07-29 21:47:26 +0000

    gh: update 0.11.1 bottle.

--- Formula/gh.rb
+++ Formula/gh.rb
@@ -8,9 +8,9 @@ class Gh < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ee4b879f06935d40035318d4189d5c057e53c38c53e5d4e28ec6b458bd98ce22" => :catalina
-    sha256 "85cc073f44cbb43bd87e171fa505a770822e4655b5c92c2424d7aeac73fae727" => :mojave
-    sha256 "34074c761bd6200fd5d5a61e23434836e487febc0bee3336c93e8a1c4864715a" => :high_sierra
+    sha256 "8c407d1a98c582f12b7830c854b1c29e706a4215b5d5fae94cdf85b1929a9dfe" => :catalina
+    sha256 "d758ddf6c6e240b09cb2133a35b4c05cf78c722d1e54f40622485628a6d544ac" => :mojave
+    sha256 "9f65a309f159a2bed8f7b4d737aa01b4b4f7037cf79f8b112db4f04ac38a3666" => :high_sierra
   end
 
   depends_on "go" => :build
