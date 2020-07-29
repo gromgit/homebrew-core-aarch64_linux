19c8fb0b90ced2cf4ad24ec07d431fcd3c65e5ca
Michka Popoff <michkapopoff@gmail.com>
2020-07-29 23:00:11 +0200

    flawfinder: depend on Python@3.8
    Closes #58820.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flawfinder.rb
+++ Formula/flawfinder.rb
@@ -1,9 +1,12 @@
 class Flawfinder < Formula
+  include Language::Python::Shebang
+
   desc "Examines code and reports possible security weaknesses"
   homepage "https://www.dwheeler.com/flawfinder/"
   url "https://www.dwheeler.com/flawfinder/flawfinder-2.0.11.tar.gz"
   sha256 "9b4929fca5c6703880d95f201e470b7f19262ff63e991b3ac4ea3257f712f5ec"
   license "GPL-2.0"
+  revision 1
   head "https://github.com/david-a-wheeler/flawfinder.git"
 
   bottle do
@@ -14,12 +17,15 @@ class Flawfinder < Formula
     sha256 "db9c5f0ba0292c17e40eafb1b26f8ec45aa56265797a6af6b23242c5f81f538e" => :high_sierra
   end
 
+  depends_on "python@3.8"
+
   resource "flaws" do
     url "https://www.dwheeler.com/flawfinder/test.c"
     sha256 "4a9687a091b87eed864d3e35a864146a85a3467eb2ae0800a72e330496f0aec3"
   end
 
   def install
+    rewrite_shebang detected_python_shebang, "flawfinder"
     system "make", "prefix=#{prefix}", "install"
   end
 
