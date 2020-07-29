703d088aa48e57e66526e74dd8c0748829b08160
Caleb Xu <calebcenter@live.com>
2020-07-28 22:50:21 -0400

    logstash 7.8.1
    Closes #58775.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/logstash.rb
+++ Formula/logstash.rb
@@ -1,8 +1,8 @@
 class Logstash < Formula
   desc "Tool for managing events and logs"
   homepage "https://www.elastic.co/products/logstash"
-  url "https://artifacts.elastic.co/downloads/logstash/logstash-oss-7.8.0.tar.gz"
-  sha256 "48b59d2b20f1b0bd21cbf3aa719ff51541a16166bca0d8298bb762e23a129d42"
+  url "https://artifacts.elastic.co/downloads/logstash/logstash-oss-7.8.1.tar.gz"
+  sha256 "4d8039c24f3d62f94902b218dbaacc8b058061ecc14cfd4f86d68674572cd679"
   license "Apache-2.0"
   head "https://github.com/elastic/logstash.git"
 
