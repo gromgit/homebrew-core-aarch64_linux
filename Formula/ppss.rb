90f8ecb12444746912eca70d9e2837501f32889c
Rui Chen <rui@meetup.com>
2020-07-29 00:42:11 -0400

    ppss: add license

--- Formula/ppss.rb
+++ Formula/ppss.rb
@@ -3,6 +3,7 @@ class Ppss < Formula
   homepage "https://github.com/louwrentius/PPSS"
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/ppss/ppss-2.97.tgz"
   sha256 "25d819a97d8ca04a27907be4bfcc3151712837ea12a671f1a3c9e58bc025360f"
+  license "GPL-3.0"
 
   bottle :unneeded
 
