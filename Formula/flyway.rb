bb5e32459b1a3414d75e9b3451651e4eda74a6a4
Caleb Xu <calebcenter@live.com>
2020-07-29 10:56:42 -0400

    flyway 6.5.3
    Closes #58807.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/6.5.2/flyway-commandline-6.5.2.tar.gz"
-  sha256 "13bada6d84ab4bf40b000198109ec2f182a0ee476fc862f3971d8323d08595e2"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/6.5.3/flyway-commandline-6.5.3.tar.gz"
+  sha256 "4c07f2b9e9e2e0e01b0d5d3c78981ef6db5167863e7ba50c9de9c476de4baabf"
 
   bottle :unneeded
 
