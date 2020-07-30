722ad963f742c42eda9370e8482533a8d5217565
Caleb Xu <calebcenter@live.com>
2020-07-30 15:56:53 -0400

    aliyun-cli 3.0.55
    Closes #58861.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/aliyun-cli.rb
+++ Formula/aliyun-cli.rb
@@ -1,8 +1,8 @@
 class AliyunCli < Formula
   desc "Universal Command-Line Interface for Alibaba Cloud"
   homepage "https://github.com/aliyun/aliyun-cli"
-  url "https://github.com/aliyun/aliyun-cli/archive/v3.0.54.tar.gz"
-  sha256 "bd1c51cb8b43e84b51ce3014250943d6ac0e106571dcc703405f53c9fbd59409"
+  url "https://github.com/aliyun/aliyun-cli/archive/v3.0.55.tar.gz"
+  sha256 "9e0ff0f8c928f8f731785b889294862368c900f8c5e669acc18aabbb438effa3"
   license "Apache-2.0"
 
   bottle do
