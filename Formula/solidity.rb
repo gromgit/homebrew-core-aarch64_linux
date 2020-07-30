2007655bf9abcc2e8cb54270cb808c5615f2efac
Seeker <meaningseeking@protonmail.com>
2020-07-30 14:48:29 +0000

    solidity: add 0.6.12 bottle.

--- Formula/solidity.rb
+++ Formula/solidity.rb
@@ -5,6 +5,12 @@ class Solidity < Formula
   sha256 "214bd37867d59c0f2f22dbaf10fd8eea2a58c9055c853c5016d26ad7091d5776"
   license "GPL-3.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "fdc25fe91c43bdfe39cf0c49f7729bf5968bcf8bda5613fed5c0c7a49d3019d7" => :catalina
+    sha256 "feec8ba23058fe088ac1cfcebadc0151a519382921d5b65514193e8421bef61b" => :mojave
+  end
+
   depends_on "cmake" => :build
   depends_on xcode: ["11.0", :build]
   depends_on "boost"
