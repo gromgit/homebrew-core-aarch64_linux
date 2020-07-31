90c21be6a50099a657b06e80c691ecc47d8cd58f
Caleb Xu <calebcenter@live.com>
2020-07-31 00:59:48 -0400

    sjk: use write_jar_script

--- Formula/sjk.rb
+++ Formula/sjk.rb
@@ -4,6 +4,7 @@ class Sjk < Formula
   url "https://search.maven.org/remotecontent?filepath=org/gridkit/jvmtool/sjk-plus/0.16/sjk-plus-0.16.jar"
   sha256 "306c40fbf76b9cb9a09b394116b8a2614b0d9acb3725e845d2a754686a6d3d49"
   license "Apache-2.0"
+  revision 1
 
   bottle :unneeded
 
@@ -11,11 +12,7 @@ class Sjk < Formula
 
   def install
     libexec.install "sjk-plus-#{version}.jar"
-    (bin/"sjk").write <<~EOS
-      #!/bin/bash
-      export JAVA_HOME="${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
-      exec "${JAVA_HOME}/bin/java" -jar "#{libexec}/sjk-plus-#{version}.jar" "$@"
-    EOS
+    bin.write_jar_script libexec/"sjk-plus-#{version}.jar", "sjk"
   end
 
   test do
