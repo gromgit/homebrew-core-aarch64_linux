eb521c7b1cdca2d04cb441b9a8c78c9003b23e14
Caleb Xu <calebcenter@live.com>
2020-07-31 00:51:38 -0400

    jflex: use write_jar_script

--- Formula/jflex.rb
+++ Formula/jflex.rb
@@ -3,6 +3,7 @@ class Jflex < Formula
   homepage "https://jflex.de/"
   url "https://jflex.de/release/jflex-1.8.2.tar.gz"
   sha256 "a1e0d25e341d01de6b93ec32b45562905e69d06598113934b74f76b1be7927ab"
+  revision 1
 
   bottle :unneeded
 
@@ -11,11 +12,7 @@ class Jflex < Formula
   def install
     pkgshare.install "examples"
     libexec.install "lib/jflex-full-#{version}.jar" => "jflex-#{version}.jar"
-    (bin/"jflex").write <<~EOS
-      #!/bin/bash
-      export JAVA_HOME="${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
-      exec "${JAVA_HOME}/bin/java" -jar "#{libexec}/jflex-#{version}.jar" "$@"
-    EOS
+    bin.write_jar_script libexec/"jflex-#{version}.jar", "jflex"
   end
 
   test do
