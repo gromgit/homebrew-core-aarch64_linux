e3624430a611ff1f91f8b96a45e6847211616765
Dawid Dziurla <dawidd0811@gmail.com>
2020-07-31 13:29:06 +0200

    apidoc 0.24.0 (new formula)
    Closes #58906.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

---
+++ Formula/apidoc.rb
@@ -0,0 +1,43 @@
+require "language/node"
+
+class Apidoc < Formula
+  desc "RESTful web API Documentation Generator"
+  homepage "https://apidocjs.com"
+  url "https://github.com/apidoc/apidoc/archive/0.24.0.tar.gz"
+  sha256 "b63797091e9ccd0cc2d65129bb38e54957131067e37029ae137d73eb2e3520d6"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"api.go").write <<~EOS
+      /**
+       * @api {get} /user/:id Request User information
+       * @apiVersion #{version}
+       * @apiName GetUser
+       * @apiGroup User
+       *
+       * @apiParam {Number} id User's unique ID.
+       *
+       * @apiSuccess {String} firstname Firstname of the User.
+       * @apiSuccess {String} lastname  Lastname of the User.
+       */
+    EOS
+    (testpath/"apidoc.json").write <<~EOS
+      {
+        "name": "example",
+        "version": "#{version}",
+        "description": "A basic apiDoc example"
+      }
+    EOS
+    system bin/"apidoc", "-o", "out"
+    api_data_json = (testpath/"out/api_data.json").read
+    api_data = JSON.parse api_data_json
+    assert_equal api_data.first["version"], version
+  end
+end
