beb8a5967bb438067d47cc9b2b02374a4dbe26b5
Caleb Xu <calebcenter@live.com>
2020-07-31 01:11:10 -0400

    jsonschema2pojo: use write_jar_script

--- Formula/jsonschema2pojo.rb
+++ Formula/jsonschema2pojo.rb
@@ -4,7 +4,7 @@ class Jsonschema2pojo < Formula
   url "https://github.com/joelittlejohn/jsonschema2pojo/releases/download/jsonschema2pojo-1.0.2/jsonschema2pojo-1.0.2.tar.gz"
   sha256 "158e927558e3ae4779dc2b7cd00e2010e43465af5a5aebdb55c889ccacb4d6e3"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   bottle :unneeded
 
@@ -12,10 +12,7 @@ class Jsonschema2pojo < Formula
 
   def install
     libexec.install "jsonschema2pojo-#{version}-javadoc.jar", "lib"
-    (bin/"jsonschema2pojo").write <<~EOS
-      #!/bin/bash
-      exec "#{Formula["openjdk"].opt_bin}/java" -jar "#{libexec}/lib/jsonschema2pojo-cli-#{version}.jar" "$@"
-    EOS
+    bin.write_jar_script libexec/"lib/jsonschema2pojo-cli-#{version}.jar", "jsonschema2pojo"
   end
 
   test do
