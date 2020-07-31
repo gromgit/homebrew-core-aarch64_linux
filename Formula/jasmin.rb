774ced2da50651ea54ed5f8b81280d9578aae51e
Caleb Xu <calebcenter@live.com>
2020-07-31 00:45:34 -0400

    jasmin: use write_jar_script

--- Formula/jasmin.rb
+++ Formula/jasmin.rb
@@ -3,7 +3,7 @@ class Jasmin < Formula
   homepage "https://jasmin.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/jasmin/jasmin/jasmin-2.4/jasmin-2.4.zip"
   sha256 "eaa10c68cec68206fd102e9ec7113739eccd790108a1b95a6e8c3e93f20e449d"
-  revision 1
+  revision 2
 
   bottle :unneeded
 
@@ -15,11 +15,7 @@ class Jasmin < Formula
 
     libexec.install Dir["*.jar"]
     prefix.install %w[Readme.txt license-ant.txt license-jasmin.txt]
-    (bin/"jasmin").write <<~EOS
-      #!/bin/bash
-      export JAVA_HOME="${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
-      exec "${JAVA_HOME}/bin/java" -jar "#{libexec}/jasmin.jar" "$@"
-    EOS
+    bin.write_jar_script libexec/"jasmin.jar", "jasmin"
   end
 
   test do
