b1f3f1cda986ce65de32f64e96123632c8fe3238
Rui Chen <rui@meetup.com>
2020-07-31 00:10:16 -0400

    bazelisk: move to go build and bazelisk zsh completion
    fix style issue
    
    Closes #55403.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/bazelisk.rb
+++ Formula/bazelisk.rb
@@ -5,6 +5,7 @@ class Bazelisk < Formula
       tag:      "v1.5.0",
       revision: "9f7127db3c137e32a21484f9345bbc0da8301135"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/bazelbuild/bazelisk.git"
 
   bottle do
@@ -14,18 +15,31 @@ class Bazelisk < Formula
     sha256 "9c189bca9ddac7acaab8da42f451a8707320c52b4b233ab9b42cbfefc94310d9" => :high_sierra
   end
 
-  depends_on "bazel" => :build
+  depends_on "go" => :build
+
+  conflicts_with "bazel", because: "Bazelisk replaces the bazel binary"
+
+  resource "bazel_zsh_completion" do
+    url "https://raw.githubusercontent.com/bazelbuild/bazel/3.4.1/scripts/zsh_completion/_bazel"
+    sha256 "4094dc84add2f23823bc341186adf6b8487fbd5d4164bd52d98891c41511eba4"
+  end
 
   def install
-    rm_f ".bazelversion" # Homebrew uses the latest bazel
-    system "bazel", "build", "--config=release", "//:bazelisk-darwin"
+    ENV["GOOS"] = "darwin"
+    ENV["GOARCH"] = "amd64"
 
-    bin.install "bazel-bin/bazelisk-darwin_amd64" => "bazelisk"
+    system "go", "build", *std_go_args, "-ldflags", "-X main.BazeliskVersion=#{version}"
+
+    bin.install_symlink "bazelisk" => "bazel"
+
+    resource("bazel_zsh_completion").stage do
+      zsh_completion.install "_bazel"
+    end
   end
 
   test do
     ENV["USE_BAZEL_VERSION"] = Formula["bazel"].version
-    assert_match /v#{version}/, shell_output("#{bin}/bazelisk version")
+    assert_match /Bazelisk version: #{version}/, shell_output("#{bin}/bazelisk version")
 
     # This is an older than current version, so that we can test that bazelisk
     # will target an explicit version we specify. This version shouldn't need to
