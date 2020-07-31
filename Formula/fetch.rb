ca69c13ab6669f59a02490e2410f995184593988
Lucas Bleme <andreybleme1@gmail.com>
2020-07-31 19:39:57 -0300

    fetch 0.3.10 (new formula)
    Updated Fetch version
    
    Fixed Formula test
    
    fetch 0.3.9 (new formula)
    
    Updated Fetch version
    
    Fixed Formula test
    
    Fixed Formula test
    
    fetch 0.3.9 (new formula)
    
    Updated Fetch version
    
    add head
    
    some style update
    
    Closes #59049.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

---
+++ Formula/fetch.rb
@@ -0,0 +1,21 @@
+class Fetch < Formula
+  desc "Download assets from a commit, branch, or tag of GitHub repositories"
+  homepage "https://www.gruntwork.io/"
+  url "https://github.com/gruntwork-io/fetch/archive/v0.3.10.tar.gz"
+  sha256 "488102201f69c875489f5d8751f3e2ed14a508849174705b5574925ee3a9dadb"
+  license "MIT"
+  head "https://github.com/gruntwork-io/fetch.git"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-ldflags", "-X main.VERSION=v#{version}", *std_go_args
+  end
+
+  test do
+    repo_url = "https://github.com/gruntwork-io/fetch"
+
+    assert_match "Downloading release asset SHA256SUMS to SHA256SUMS",
+      shell_output("#{bin}/fetch --repo=\"#{repo_url}\" --tag=\"v0.3.10\" --release-asset=\"SHA256SUMS\" . 2>1&")
+  end
+end
