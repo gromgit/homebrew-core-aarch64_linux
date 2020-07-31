8832e026d21a89a16ac807c1deeddb093bfc1ba4
Caleb Xu <calebcenter@live.com>
2020-07-30 22:00:22 -0400

    aspectj: depend on openjdk
    Closes #58890.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/aspectj.rb
+++ Formula/aspectj.rb
@@ -3,16 +3,17 @@ class Aspectj < Formula
   homepage "https://eclipse.org/aspectj/"
   url "https://www.eclipse.org/downloads/download.php?r=1&file=/tools/aspectj/aspectj-1.9.6.jar"
   sha256 "afec62c03fe154adeecf9cd599ce033fff258d1d373a82511e5df54f79ab03e2"
+  revision 1
 
   bottle :unneeded
 
-  depends_on java: "1.8"
+  depends_on "openjdk"
 
   def install
     mkdir_p "#{libexec}/#{name}"
-    system "java", "-jar", "aspectj-#{version}.jar", "-to", "#{libexec}/#{name}"
+    system "#{Formula["openjdk"].bin}/java", "-jar", "aspectj-#{version}.jar", "-to", "#{libexec}/#{name}"
     bin.install Dir["#{libexec}/#{name}/bin/*"]
-    bin.env_script_all_files(libexec/"#{name}/bin", Language::Java.java_home_env("1.8"))
+    bin.env_script_all_files libexec/"#{name}/bin", Language::Java.overridable_java_home_env
     chmod 0555, Dir["#{libexec}/#{name}/bin/*"] # avoid 0777
   end
 
