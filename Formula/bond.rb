1e214c2396abaa959f90333d97f31138708e84b6
Dawid Dziurla <dawidd0811@gmail.com>
2020-07-31 11:47:21 +0000

    bond: add 9.0.1 bottle.

--- Formula/bond.rb
+++ Formula/bond.rb
@@ -5,6 +5,13 @@ class Bond < Formula
   sha256 "df56b97961ae1c6cf8399170051d977cda6ba30be45a0a50fb7bdea8c65fb076"
   license "MIT"
 
+  bottle do
+    cellar :any
+    sha256 "5c7c89d4a830486361cc7176fa633f6583f640f2f77c7cba1208278297eca82d" => :catalina
+    sha256 "53abaa89e4119a886c857e778a28d316a58e367b82bd84fcb4343bfe4034e18c" => :mojave
+    sha256 "b213875a163f78af9d6e50c87cffa2cee5a9a17bf37f7a391dd9ac5ef29c8e5a" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "haskell-stack" => :build
   depends_on "boost"
