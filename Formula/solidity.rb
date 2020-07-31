c3c3255dc14c2473a560b3761e2e5f8ff2c47fe4
Seeker <meaningseeking@protonmail.com>
2020-07-30 19:29:57 -0700

    solidity 0.7.0
    Closes #58892.
    
    Co-authored-by: Jonathan Chang <jonathan.chang@monash.edu>
    Signed-off-by: Jonathan Chang <jonathan.chang@monash.edu>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/solidity.rb
+++ Formula/solidity.rb
@@ -1,8 +1,8 @@
 class Solidity < Formula
   desc "Contract-oriented programming language"
   homepage "https://solidity.readthedocs.io"
-  url "https://github.com/ethereum/solidity/releases/download/v0.6.12/solidity_0.6.12.tar.gz"
-  sha256 "214bd37867d59c0f2f22dbaf10fd8eea2a58c9055c853c5016d26ad7091d5776"
+  url "https://github.com/ethereum/solidity/releases/download/v0.7.0/solidity_0.7.0.tar.gz"
+  sha256 "86e782a88eaaf4aa98f4e1e915f46b5bc5f596ea86c784fb911dc6e4c04309bf"
   license "GPL-3.0"
 
   bottle do
@@ -25,7 +25,7 @@ class Solidity < Formula
   test do
     (testpath/"hello.sol").write <<~EOS
       // SPDX-License-Identifier: GPL-3.0
-      pragma solidity ^0.6.0;
+      pragma solidity ^0.7.0;
       contract HelloWorld {
         function helloWorld() external pure returns (string memory) {
           return "Hello, World!";
