54f586eda920496c0e64def69663f0f345e99915
Caleb Xu <calebcenter@live.com>
2020-07-31 13:49:52 -0400

    broot 0.19.4
    Closes #58938.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/broot.rb
+++ Formula/broot.rb
@@ -1,8 +1,8 @@
 class Broot < Formula
   desc "New way to see and navigate directory trees"
   homepage "https://dystroy.org/broot/"
-  url "https://github.com/Canop/broot/archive/v0.19.3.tar.gz"
-  sha256 "8aa62073ba4c68d7df3e5a83271fc7c2ffa3c24d041005e69a28b3b8ef7d7bdf"
+  url "https://github.com/Canop/broot/archive/v0.19.4.tar.gz"
+  sha256 "839f9a2808e35ae78b184310e4c6940908dca2dd117b02c50bf55e5322efd3e1"
   license "MIT"
   head "https://github.com/Canop/broot.git"
 
