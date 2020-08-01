0624be7b2afc206c68e212f57b58651a5f603828
miccal <miccal.matthews@gmail.com>
2020-08-01 20:19:01 +0800

    imagemagick 7.0.10-25
    Closes #58980.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -1,9 +1,9 @@
 class Imagemagick < Formula
   desc "Tools and libraries to manipulate images in many formats"
   homepage "https://www.imagemagick.org/"
-  url "https://dl.bintray.com/homebrew/mirror/ImageMagick-7.0.10-24.tar.xz"
-  mirror "https://www.imagemagick.org/download/releases/ImageMagick-7.0.10-24.tar.xz"
-  sha256 "8f8d4715ac995aaae14dc1567608a05833764c13cd0c16cbac2a3a8e229e7f43"
+  url "https://dl.bintray.com/homebrew/mirror/ImageMagick-7.0.10-25.tar.xz"
+  mirror "https://www.imagemagick.org/download/releases/ImageMagick-7.0.10-25.tar.xz"
+  sha256 "0a125992e63b2a7f13fb966718304fe5f8142192c275d55b497a3bfe1f554ae6"
   license "ImageMagick"
   head "https://github.com/ImageMagick/ImageMagick.git"
 
