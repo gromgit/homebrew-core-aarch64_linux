c290a36f263ec3f4368f94fd48aaa4756682c74e
Jonathan Chang <jchang641@gmail.com>
2020-08-01 12:56:55 +1000

    pandocomatic 0.2.6 (new formula)
    Closes #58970.
    
    Signed-off-by: Seeker <meaningseeking@protonmail.com>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

---
+++ Formula/pandocomatic.rb
@@ -0,0 +1,32 @@
+class Pandocomatic < Formula
+  desc "Automate the use of pandoc"
+  homepage "https://heerdebeer.org/Software/markdown/pandocomatic/"
+  url "https://github.com/htdebeer/pandocomatic/archive/0.2.6.tar.gz"
+  sha256 "902d1c366e85c14b5a3bc0fd5247b22fde985858f8f985e368e3eb7c03324e23"
+  license "GPL-3.0"
+
+  depends_on "pandoc"
+  uses_from_macos "ruby", since: :catalina
+
+  def install
+    ENV["GEM_HOME"] = libexec
+    system "gem", "build", "#{name}.gemspec"
+    system "gem", "install", "#{name}-#{version}.gem"
+    bin.install libexec/"bin/#{name}"
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+  end
+
+  test do
+    (testpath/"test.md").write <<~EOS
+      # Homebrew
+
+      A package manager for humans. Cats should take a look at Tigerbrew.
+    EOS
+    expected_html = <<~EOS
+      <h1 id="homebrew">Homebrew</h1>
+      <p>A package manager for humans. Cats should take a look at Tigerbrew.</p>
+    EOS
+    system "#{bin}/pandocomatic", "-i", "test.md", "-o", "test.html"
+    assert_equal expected_html, (testpath/"test.html").read
+  end
+end
