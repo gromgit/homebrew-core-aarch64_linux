fb880931834a89dd6a66d33fbab6eedee7ee011e
Seeker <meaningseeking@protonmail.com>
2020-07-31 23:57:52 -0700

    archey: deprecate and move patch to stable block (#58971)

--- Formula/archey.rb
+++ Formula/archey.rb
@@ -1,21 +1,24 @@
 class Archey < Formula
   desc "Graphical system information display for macOS"
   homepage "https://obihann.github.io/archey-osx/"
-  url "https://github.com/obihann/archey-osx/archive/1.6.0.tar.gz"
-  sha256 "0f0ffcf8c5f07610b98f0351dcb38bb8419001f40906d5dc4bfd28ef12dbd0f8"
   license "GPL-2.0"
   revision 1
   head "https://github.com/obihann/archey-osx.git"
 
-  bottle :unneeded
+  stable do
+    url "https://github.com/obihann/archey-osx/archive/1.6.0.tar.gz"
+    sha256 "0f0ffcf8c5f07610b98f0351dcb38bb8419001f40906d5dc4bfd28ef12dbd0f8"
 
-  # Fix double percent sign in battery output, remove in next release
-  unless build.head?
+    # Fix double percent sign in battery output
     patch do
       url "https://github.com/obihann/archey-osx/commit/cd125547d0936f066b64616553269bf647348e53.diff?full_index=1"
       sha256 "c03b80e4d5aa114b81ac04bfa77c46055fe01764ae877a8a061f3d43c9de8a72"
     end
   end
+
+  bottle :unneeded
+
+  deprecate! date: "2017-04-28"
 
   def install
     bin.install "bin/archey"
