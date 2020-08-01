cf7ad5522119f3df7a7eb16d85d10c0b329c4838
Roopak Venkatakrishnan <roopak.v@gmail.com>
2020-08-01 02:38:17 +0100

    godep: revision bump for go

--- Formula/godep.rb
+++ Formula/godep.rb
@@ -4,7 +4,7 @@ class Godep < Formula
   url "https://github.com/tools/godep/archive/v80.tar.gz"
   sha256 "029adc1a0ce5c63cd40b56660664e73456648e5c031ba6c214ba1e1e9fc86cf6"
   license "BSD-3-Clause"
-  revision 35
+  revision 36
   head "https://github.com/tools/godep.git"
 
   bottle do
@@ -13,6 +13,8 @@ class Godep < Formula
     sha256 "66795c1166983b401a9fd93f54a67d0f61e26d435e02fe4086ee9b8ee24ef8ba" => :mojave
     sha256 "b06ff82b8a44be0f6e2d67950d54f763e6af52d8fd15d99bce96b5d5a4f63740" => :high_sierra
   end
+
+  deprecate! date: "2018-01-26"
 
   depends_on "go"
 
