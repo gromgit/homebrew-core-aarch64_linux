956caef0d2747dfdec1fd891e7bd9d8209e1d9f0
Rui Chen <rui@meetup.com>
2020-08-02 14:49:41 -0400

    trash: add license

--- Formula/trash.rb
+++ Formula/trash.rb
@@ -3,6 +3,7 @@ class Trash < Formula
   homepage "https://hasseg.org/trash/"
   url "https://github.com/ali-rantakari/trash/archive/v0.9.2.tar.gz"
   sha256 "e8739c93d710ac4da721e16878e7693019d3a2ad7d8acd817f41426601610083"
+  license "MIT"
   head "https://github.com/ali-rantakari/trash.git"
 
   bottle do
