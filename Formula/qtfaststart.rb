3c7f30e0e77a3b94a750c37225a2b6463b8790a3
Rui Chen <rui@meetup.com>
2020-08-02 12:18:34 -0400

    qtfaststart: add license

--- Formula/qtfaststart.rb
+++ Formula/qtfaststart.rb
@@ -3,6 +3,7 @@ class Qtfaststart < Formula
   homepage "https://libav.org/"
   url "https://libav.org/releases/libav-12.3.tar.gz"
   sha256 "115b659022dd387f662e26fbc5bc0cc14ec18daa100003ffd34f4da0479b272e"
+  license "LGPL-2.1"
 
   bottle do
     cellar :any_skip_relocation
