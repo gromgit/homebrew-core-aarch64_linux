cb837f4d55e3f81381e9ff574836362c38d947a5
Rui Chen <rui@meetup.com>
2020-08-02 14:49:37 -0400

    taskd: add license

--- Formula/taskd.rb
+++ Formula/taskd.rb
@@ -3,6 +3,7 @@ class Taskd < Formula
   homepage "https://taskwarrior.org/docs/taskserver/setup.html"
   url "https://taskwarrior.org/download/taskd-1.1.0.tar.gz"
   sha256 "7b8488e687971ae56729ff4e2e5209ff8806cf8cd57718bfd7e521be130621b4"
+  license "MIT"
   revision 1
   head "https://github.com/GothenburgBitFactory/taskserver.git"
 
