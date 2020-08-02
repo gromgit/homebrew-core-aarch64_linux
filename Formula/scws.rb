57e32cb457c8e95d062ff65d6a8a2675ef05d5cb
Rui Chen <rui@meetup.com>
2020-08-02 14:04:01 -0400

    scws: add license

--- Formula/scws.rb
+++ Formula/scws.rb
@@ -3,6 +3,7 @@ class Scws < Formula
   homepage "https://github.com/hightman/scws"
   url "http://www.xunsearch.com/scws/down/scws-1.2.3.tar.bz2"
   sha256 "60d50ac3dc42cff3c0b16cb1cfee47d8cb8c8baa142a58bc62854477b81f1af5"
+  license "BSD-3-Clause"
 
   bottle do
     cellar :any
