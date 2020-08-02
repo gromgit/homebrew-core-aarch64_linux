175f5c3f4ae8760c177afb5c7a9d25a128070d48
Rui Chen <rui@meetup.com>
2020-08-02 14:49:41 -0400

    transmission-cli: add license

--- Formula/transmission-cli.rb
+++ Formula/transmission-cli.rb
@@ -3,6 +3,8 @@ class TransmissionCli < Formula
   homepage "https://www.transmissionbt.com/"
   url "https://github.com/transmission/transmission-releases/raw/d5ccf14/transmission-3.00.tar.xz"
   sha256 "9144652fe742f7f7dd6657716e378da60b751aaeda8bef8344b3eefc4db255f2"
+  # license ["GPL-2.0", "GPL-3.0"] - pending https://github.com/Homebrew/brew/pull/7953
+  license "GPL-2.0"
 
   bottle do
     sha256 "576f0f5017a86da149292b6da4fde251ad7a77bd9a88e82639ed4fc586cb08e7" => :catalina
