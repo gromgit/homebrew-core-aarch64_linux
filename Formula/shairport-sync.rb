2ce4891c2e04b831054ad829e0bd3450122dbeba
Rui Chen <rui@meetup.com>
2020-08-02 14:04:02 -0400

    shairport-sync: add license

--- Formula/shairport-sync.rb
+++ Formula/shairport-sync.rb
@@ -3,6 +3,7 @@ class ShairportSync < Formula
   homepage "https://github.com/mikebrady/shairport-sync"
   url "https://github.com/mikebrady/shairport-sync/archive/3.3.6.tar.gz"
   sha256 "a8382affd25c473fa38ead5690148c6c3902098f359f9c881eefe139e1f49f49"
+  license "MIT"
   head "https://github.com/mikebrady/shairport-sync.git", branch: "development"
 
   bottle do
