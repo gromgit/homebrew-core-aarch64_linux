4fd1ecc786555834a8bddd5efaab3fefa97a5506
Rui Chen <rui@meetup.com>
2020-08-02 14:04:05 -0400

    sonar-scanner: add license

--- Formula/sonar-scanner.rb
+++ Formula/sonar-scanner.rb
@@ -3,6 +3,7 @@ class SonarScanner < Formula
   homepage "https://docs.sonarqube.org/display/SCAN/Analyzing+with+SonarQube+Scanner"
   url "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.4.0.2170.zip"
   sha256 "b24c19e948f152a4ea8dc43d037253234add639503d42194f6175869693ff058"
+  license "LGPL-3.0"
   head "https://github.com/SonarSource/sonar-scanner-cli.git"
 
   bottle :unneeded
