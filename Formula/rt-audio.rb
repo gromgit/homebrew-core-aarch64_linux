82e934eaeb273f863211a62cb447f4b28bc2cf23
Rui Chen <rui@meetup.com>
2020-08-02 12:18:38 -0400

    rt-audio: add license

--- Formula/rt-audio.rb
+++ Formula/rt-audio.rb
@@ -3,6 +3,7 @@ class RtAudio < Formula
   homepage "https://www.music.mcgill.ca/~gary/rtaudio/"
   url "https://www.music.mcgill.ca/~gary/rtaudio/release/rtaudio-5.1.0.tar.gz"
   sha256 "ff138b2b6ed2b700b04b406be718df213052d4c952190280cf4e2fab4b61fe09"
+  license "MIT"
   head "https://github.com/thestk/rtaudio.git"
 
   bottle do
