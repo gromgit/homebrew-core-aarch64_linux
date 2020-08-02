f85d6ccc1132689b16b92bde311f4a8ab4f705f6
Rui Chen <rui@meetup.com>
2020-08-02 12:18:37 -0400

    rmate: add license

--- Formula/rmate.rb
+++ Formula/rmate.rb
@@ -3,6 +3,7 @@ class Rmate < Formula
   homepage "https://github.com/textmate/rmate"
   url "https://github.com/textmate/rmate/archive/v1.5.8.tar.gz"
   sha256 "40be07ae251bfa47b408eb56395dd2385d8e9ea220a19efd5145593cd8cbd89c"
+  license "MIT"
   head "https://github.com/textmate/rmate.git"
 
   bottle :unneeded
