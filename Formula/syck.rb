08fad9e027fc87b5f3f98b05b6c70967e0e0b160
Rui Chen <rui@meetup.com>
2020-08-02 14:04:08 -0400

    syck: add license

--- Formula/syck.rb
+++ Formula/syck.rb
@@ -3,6 +3,8 @@ class Syck < Formula
   homepage "https://github.com/indeyets/syck"
   url "https://github.s3.amazonaws.com/downloads/indeyets/syck/syck-0.70.tar.gz"
   sha256 "4c94c472ee8314e0d76eb2cca84f6029dc8fc58bfbc47748d50dcb289fda094e"
+  # it is dual licensed, but the other license is not listed in SPDX
+  license "MIT"
 
   bottle do
     cellar :any_skip_relocation
