7d195b6c2062d1fd2706a3ef675f1e63e8fc94d0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-02 21:10:50 +0000

    libmatroska: update 1.6.2 bottle.

--- Formula/libmatroska.rb
+++ Formula/libmatroska.rb
@@ -8,9 +8,9 @@ class Libmatroska < Formula
 
   bottle do
     cellar :any
-    sha256 "6a2ae9df8279bb33af3195baad983b9984b9d15e4102e1a8324f365c704aaa71" => :catalina
-    sha256 "15ec15e52dfbab751edc3717e9486e8d1d86c27b9c11d191d7b4386da9719558" => :mojave
-    sha256 "3951b56f6f095b6f4fd2fef10b73368294ed53755295bfc0349cb461d87014bc" => :high_sierra
+    sha256 "78373c5516fdadee736e360c5e94a80ca3e3092ab9ca44fd88f31c2a08f8fc5a" => :catalina
+    sha256 "a1c46ddc10694208aae53738cd9927674e076b805180149a1104b4a04bdc19b0" => :mojave
+    sha256 "74faf2d3e6539e847538cfbd9f7a86abacb7272d83cfa1d36094f9295f66727f" => :high_sierra
   end
 
   depends_on "cmake" => :build
