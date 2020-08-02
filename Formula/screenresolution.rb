8063198c635d69c3a2d7429fb89fe9d9b2489f1d
Rui Chen <rui@meetup.com>
2020-08-02 14:04:01 -0400

    screenresolution: add license

--- Formula/screenresolution.rb
+++ Formula/screenresolution.rb
@@ -3,6 +3,7 @@ class Screenresolution < Formula
   homepage "https://github.com/jhford/screenresolution"
   url "https://github.com/jhford/screenresolution/archive/v1.6.tar.gz"
   sha256 "d3761663eaf585b014391a30a77c9494a6404e78e8a4863383e12c59b0f539eb"
+  license "GPL-2.0"
   head "https://github.com/jhford/screenresolution.git"
 
   bottle do
