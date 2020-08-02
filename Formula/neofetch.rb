8f57750b45c0fe40d91015d9ef219206c888be85
Dawid Dziurla <dawidd0811@gmail.com>
2020-08-02 19:12:47 +0000

    neofetch 7.1.0
    Closes #59031.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/neofetch.rb
+++ Formula/neofetch.rb
@@ -1,8 +1,8 @@
 class Neofetch < Formula
   desc "Fast, highly customisable system info script"
   homepage "https://github.com/dylanaraps/neofetch"
-  url "https://github.com/dylanaraps/neofetch/archive/7.0.0.tar.gz"
-  sha256 "8c6bd217cf6d34fc1f3dcbb0e8b1137655bc13fbb21165273dbb2a7bce0d3130"
+  url "https://github.com/dylanaraps/neofetch/archive/7.1.0.tar.gz"
+  sha256 "58a95e6b714e41efc804eca389a223309169b2def35e57fa934482a6b47c27e7"
   license "MIT"
   head "https://github.com/dylanaraps/neofetch.git"
 
