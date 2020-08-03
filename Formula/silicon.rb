9a2655cf569817b92ab2994c6f8bf485f83a58b2
Dario Vladovic <d.vladimyr@gmail.com>
2020-08-03 21:35:23 +0200

    silicon 0.3.2 (new formula)
    Closes #59091.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

---
+++ Formula/silicon.rb
@@ -0,0 +1,33 @@
+class Silicon < Formula
+  desc "Create beautiful image of your source code"
+  homepage "https://github.com/Aloxaf/silicon/"
+  url "https://github.com/Aloxaf/silicon/archive/v0.3.2.tar.gz"
+  sha256 "510e8d6a5cf856f5060088ea6d5d22a510b6adeea2ea5b71d4d186e417115061"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    (testpath/"test.rs").write <<~EOF
+      fn factorial(n: u64) -> u64 {
+          match n {
+              0 => 1,
+              _ => n * factorial(n - 1),
+          }
+      }
+
+      fn main() {
+          println!("10! = {}", factorial(10));
+      }
+    EOF
+
+    system bin/"silicon", "-o", "output.png", "test.rs"
+    assert_predicate testpath/"output.png", :exist?
+    expected_size = [894, 630]
+    assert_equal expected_size, IO.read("output.png")[0x10..0x18].unpack("NN")
+  end
+end
