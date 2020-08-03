e9a411a0e619ab6d984639858e7c333b4d937903
Rui Chen <rui@meetup.com>
2020-08-03 13:46:30 -0400

    ats2-postiats 0.4.1
    Closes #59087.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ats2-postiats.rb
+++ Formula/ats2-postiats.rb
@@ -1,8 +1,8 @@
 class Ats2Postiats < Formula
   desc "Programming language with formal specification features"
   homepage "http://www.ats-lang.org/"
-  url "https://downloads.sourceforge.net/project/ats2-lang/ats2-lang/ats2-postiats-0.4.0/ATS2-Postiats-0.4.0.tgz"
-  sha256 "a749b62d429eda45ec304075f1743e1a2638c4772d37b579839d7797470869c0"
+  url "https://downloads.sourceforge.net/project/ats2-lang/ats2-lang/ats2-postiats-0.4.1/ATS2-Postiats-0.4.1.tgz"
+  sha256 "03f4bf7205f76732624c32a4bf0b346ddace23fdb5a1cde25c378d33043aaa3c"
   license "GPL-3.0"
 
   bottle do
