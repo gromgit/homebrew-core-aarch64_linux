db1bb5d1776cf82072a4eaeb8eedcc823858b552
Sarisia <sarisiaice@gmail.com>
2020-08-05 19:30:41 +0900

    yuicompressor: add openjdk as dependency (#59182)
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>

--- Formula/yuicompressor.rb
+++ Formula/yuicompressor.rb
@@ -3,8 +3,12 @@ class Yuicompressor < Formula
   homepage "https://yui.github.io/yuicompressor/"
   url "https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.zip"
   sha256 "3243fd79cb68cc61a5278a8ff67a0ad6a2d825c36464594b66900ad8426a6a6e"
+  license "BSD-3-Clause"
+  revision 1
 
   bottle :unneeded
+
+  depends_on "openjdk"
 
   def install
     libexec.install "yuicompressor-#{version}.jar"
