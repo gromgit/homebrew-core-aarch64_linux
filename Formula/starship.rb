9c9eed6f0e5dc0640855984a3cd5cfebe1f154b0
darklore <zodiac.brave.story@gmail.com>
2020-08-05 13:35:07 +0900

    starship: add shell completions
    Closes #59180.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/starship.rb
+++ Formula/starship.rb
@@ -4,6 +4,7 @@ class Starship < Formula
   url "https://github.com/starship/starship/archive/v0.44.0.tar.gz"
   sha256 "b002fa0e2b34ad59330a543461a51648751db4ae8d439d58065a3b9656772fe3"
   license "ISC"
+  revision 1
   head "https://github.com/starship/starship.git"
 
   bottle do
@@ -19,6 +20,15 @@ class Starship < Formula
 
   def install
     system "cargo", "install", *std_cargo_args
+
+    bash_output = Utils.safe_popen_read("#{bin}/starship", "completions", "bash")
+    (bash_completion/"starship").write bash_output
+
+    zsh_output = Utils.safe_popen_read("#{bin}/starship", "completions", "zsh")
+    (zsh_completion/"_starship").write zsh_output
+
+    fish_output = Utils.safe_popen_read("#{bin}/starship", "completions", "fish")
+    (fish_completion/"starship.fish").write fish_output
   end
 
   test do
