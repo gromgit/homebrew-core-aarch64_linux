4a7b85d7e37d5f7ad262a0c55cbc5d21a90cc255
Dawid Dziurla <dawidd0811@gmail.com>
2020-08-05 07:37:02 +0000

    fetch: add 0.3.10 bottle.

--- Formula/fetch.rb
+++ Formula/fetch.rb
@@ -6,6 +6,13 @@ class Fetch < Formula
   license "MIT"
   head "https://github.com/gruntwork-io/fetch.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c0e1d50848e6e2f46a796a642857fc43082b14c0e6d99107426265eed17a518f" => :catalina
+    sha256 "e359908f3c023cfb0179a1829641792109beb9815ffbf75e0abb85562f3ef66a" => :mojave
+    sha256 "2589550aef609838abb725f0f96589c889884d9c3ad64da3c235cac040549ebd" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
