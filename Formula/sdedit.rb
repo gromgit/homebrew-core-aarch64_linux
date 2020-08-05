3b207a99b3163ebc042eabfbabf90cca7a65f881
Caleb Xu <calebcenter@live.com>
2020-08-05 09:26:03 -0400

    sdedit: use write_jar_script

--- Formula/sdedit.rb
+++ Formula/sdedit.rb
@@ -3,6 +3,7 @@ class Sdedit < Formula
   homepage "https://sdedit.sourceforge.io"
   url "https://downloads.sourceforge.net/project/sdedit/sdedit/4.2/sdedit-4.2.1.jar"
   sha256 "270af857e6d2823ce0c18dee47e1e78ef7bc90c7e8afeda36114d364e0f4441c"
+  revision 1
 
   bottle :unneeded
 
@@ -10,10 +11,7 @@ class Sdedit < Formula
 
   def install
     libexec.install "sdedit-#{version}.jar"
-    (bin/"sdedit").write <<~EOS
-      #!/bin/bash
-      exec "#{Formula["openjdk"].opt_bin}/java" -jar "#{libexec}/sdedit-#{version}.jar" "$@"
-    EOS
+    bin.write_jar_script libexec/"sdedit-#{version}.jar", "sdedit"
   end
 
   test do
@@ -23,5 +21,6 @@ class Sdedit < Formula
       user:Actor
     EOS
     system bin/"sdedit", "-t", "pdf", "-o", testpath/"test.pdf", testpath/"test.sd"
+    assert_predicate testpath/"test.pdf", :exist?
   end
 end
