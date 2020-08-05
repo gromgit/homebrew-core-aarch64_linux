da0f355b4feeb37283e0ad93813703ed2f5a1838
Caleb Xu <calebcenter@live.com>
2020-08-05 09:46:28 -0400

    git-credential-manager: use write_jar_script

--- Formula/git-credential-manager.rb
+++ Formula/git-credential-manager.rb
@@ -4,7 +4,7 @@ class GitCredentialManager < Formula
   url "https://github.com/Microsoft/Git-Credential-Manager-for-Mac-and-Linux/releases/download/git-credential-manager-2.0.4/git-credential-manager-2.0.4.jar"
   sha256 "fb8536aac9b00cdf6bdeb0dd152bb1306d88cd3fdb7a958ac9a144bf4017cad7"
   license "MIT"
-  revision 1
+  revision 2
 
   bottle :unneeded
 
@@ -12,10 +12,7 @@ class GitCredentialManager < Formula
 
   def install
     libexec.install "git-credential-manager-#{version}.jar"
-    (bin/"git-credential-manager").write <<~EOS
-      #!/bin/bash
-      exec "#{Formula["openjdk"].opt_bin}/java" -jar "#{libexec}/git-credential-manager-#{version}.jar" "$@"
-    EOS
+    bin.write_jar_script libexec/"git-credential-manager-#{version}.jar", "git-credential-manager"
   end
 
   test do
