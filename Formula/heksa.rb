ac23493c6482e7064beb11e6a59ca4821f7d190f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-05 04:12:11 +0000

    heksa: update 1.13.0 bottle.

--- Formula/heksa.rb
+++ Formula/heksa.rb
@@ -9,9 +9,9 @@ class Heksa < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "346f10b8cfc84fe37619e925372c27b7787beecd3d240a3f381fdca629da117e" => :catalina
-    sha256 "481e0b181ff88978df7804ff8f657821b7802a9c2fe3937bb949b723e55a4369" => :mojave
-    sha256 "cf17ed8f9f50e1c4c1864b8847e456bf6a93a89bb9bcb76416393effbcf4d441" => :high_sierra
+    sha256 "771946a21e72b9d5eea465ca9fd213615b0eba272820691905522bea993ce6a3" => :catalina
+    sha256 "7592f466b87e68f8e4f4762bc0ed6cf14ac92a127ef6fdd0cfbe3d4fdb550b05" => :mojave
+    sha256 "14394cbe0fe767fd205e96ec62b27e7223ad48231f61d30271b5516bc99e652a" => :high_sierra
   end
 
   depends_on "go" => :build
