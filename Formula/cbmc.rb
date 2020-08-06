76335e51ddc032ccc98ad7537371d0df14947989
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-06 21:12:15 +0000

    cbmc: update 5.12.5 bottle.

--- Formula/cbmc.rb
+++ Formula/cbmc.rb
@@ -9,9 +9,9 @@ class Cbmc < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ae760e8d4ebef48795153e008b9f973280fb9012ee0f187a92ba1ebd5f7176e0" => :catalina
-    sha256 "fdb290684027c7732887d5735d133d91787488feebff3febfa5ea52c0e1956c0" => :mojave
-    sha256 "3d35aded8e36a10a7a400b8084c7d76c9557f4a6a8327adb09c06ecd02209370" => :high_sierra
+    sha256 "2168ecf10a53b43349471559dc48d4b5ff57f260d64b1b1b74a400d45e9af881" => :catalina
+    sha256 "3b85cdef01873900c6fe20976dbf0479cecdc17ef07f839585577865efbb3672" => :mojave
+    sha256 "769326912492922c9dd6dbd6573511e8ad281806fd6eb450ff8b863898030b22" => :high_sierra
   end
 
   depends_on "cmake" => :build
