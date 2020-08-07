afb957233611612cb74dc89e84ed96d84c58a04d
Michal Kuratczyk <mkuratczyk@gmail.com>
2020-08-07 10:29:54 +0200

    rabbitmq 3.8.6 (#59154)

--- Formula/rabbitmq.rb
+++ Formula/rabbitmq.rb
@@ -1,12 +1,13 @@
 class Rabbitmq < Formula
   desc "Messaging broker"
   homepage "https://www.rabbitmq.com"
-  url "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.3/rabbitmq-server-generic-unix-3.8.3.tar.xz"
-  sha256 "5a2739ed1dba77f88316b4c63393d8037fc4acf51881ba922470453e891875b6"
+  url "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.6/rabbitmq-server-generic-unix-3.8.6.tar.xz"
+  sha256 "51f8013881d7965134e8fbba2c23c93d64f9b3279e4eedbcccfb493a5146eac4"
   license "MPL-2.0"
 
   bottle :unneeded
 
+  depends_on "python@3.8" => :build
   depends_on "erlang"
 
   uses_from_macos "unzip" => :build
@@ -23,7 +24,6 @@ class Rabbitmq < Formula
     erlang = Formula["erlang"]
     inreplace sbin/"rabbitmq-defaults" do |s|
       s.gsub! "SYS_PREFIX=${RABBITMQ_HOME}", "SYS_PREFIX=#{HOMEBREW_PREFIX}"
-      s.gsub! /^ERL_DIR=$/, "ERL_DIR=#{erlang.opt_bin}/"
       s.gsub! "CLEAN_BOOT_FILE=start_clean", "CLEAN_BOOT_FILE=#{erlang.opt_lib/"erlang/bin/start_clean"}"
       s.gsub! "SASL_BOOT_FILE=start_sasl", "SASL_BOOT_FILE=#{erlang.opt_lib/"erlang/bin/start_clean"}"
     end
@@ -104,8 +104,8 @@ class Rabbitmq < Formula
 
   test do
     ENV["RABBITMQ_MNESIA_BASE"] = testpath/"var/lib/rabbitmq/mnesia"
-    system sbin/"rabbitmq-server", "-detached"
-    sleep 5
+    pid = fork { exec sbin/"rabbitmq-server" }
+    system sbin/"rabbitmq-diagnostics", "wait", "--pid", pid
     system sbin/"rabbitmqctl", "status"
     system sbin/"rabbitmqctl", "stop"
   end
