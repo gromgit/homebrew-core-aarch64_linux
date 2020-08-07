8f75969097b21087dc8fae4877848e126b7e7c73
chenrui <chenrui333@gmail.com>
2020-08-07 06:50:56 -0400

    flank 20.08.0 (#59238)

--- Formula/flank.rb
+++ Formula/flank.rb
@@ -1,8 +1,8 @@
 class Flank < Formula
   desc "Massively parallel Android and iOS test runner for Firebase Test Lab"
   homepage "https://github.com/Flank/flank"
-  url "https://github.com/Flank/flank/releases/download/v20.07.0/flank.jar"
-  sha256 "d3a99fdaaa3000b4346201a7fed18afc2c8c5309c5ffa442a647a96a08cea21d"
+  url "https://github.com/Flank/flank/releases/download/v20.08.0/flank.jar"
+  sha256 "9ddeea5406e51daaec6e3903e20f0ddd4f518c0e26c1853ddfc6f880fc0cf693"
   license "Apache-2.0"
 
   bottle :unneeded
@@ -26,8 +26,15 @@ class Flank < Formula
           locale: en
           orientation: portrait
     EOS
+
+    expected = <<~EOS
+      version: local_snapshot
+      revision: e1637b70b2962c6ab34e7656a8def6c0067e7936
+
+      Valid yml file
+    EOS
+
     output = shell_output("#{bin}/flank android doctor")
-    assert_match "version: v#{version}", output
-    assert_match "Valid yml file", output
+    assert_equal expected, output
   end
 end
