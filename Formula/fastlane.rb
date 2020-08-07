926140637379688c4ed4a43573f221708e65f3d0
Alexander Zigelski <7140800+crunchtime-ali@users.noreply.github.com>
2020-08-07 06:07:03 +0200

    fastlane 2.155.0 (#59250)
    Co-authored-by: Alexander Zigelski <crunchtime-ali@users.noreply.github.com>

--- Formula/fastlane.rb
+++ Formula/fastlane.rb
@@ -1,8 +1,8 @@
 class Fastlane < Formula
   desc "Easiest way to build and release mobile apps"
   homepage "https://fastlane.tools"
-  url "https://github.com/fastlane/fastlane/archive/2.154.0.tar.gz"
-  sha256 "6ca498892e233d47c052984e318135802869189f35ff63cced0ad0eb3b5ea304"
+  url "https://github.com/fastlane/fastlane/archive/2.155.0.tar.gz"
+  sha256 "e362bc7ffe9d88b1d4fc6799fba19f29cad338e99ffa22591296c6f6f211f593"
   license "MIT"
   head "https://github.com/fastlane/fastlane.git"
 
