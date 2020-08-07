22c2d635438a1ce9a7bcf80ce6379f89a3b06a1f
Rui Chen <rui@meetup.com>
2020-08-07 12:48:06 -0400

    bazelisk 1.6.0
    Closes #59298.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bazelisk.rb
+++ Formula/bazelisk.rb
@@ -2,10 +2,9 @@ class Bazelisk < Formula
   desc "User-friendly launcher for Bazel"
   homepage "https://github.com/bazelbuild/bazelisk/"
   url "https://github.com/bazelbuild/bazelisk.git",
-      tag:      "v1.5.0",
-      revision: "9f7127db3c137e32a21484f9345bbc0da8301135"
+      tag:      "v1.6.0",
+      revision: "ec601329a3c0ccdecb3040a0b40ed3e7695ee8d0"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/bazelbuild/bazelisk.git"
 
   bottle do
