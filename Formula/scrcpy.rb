7db7e47e75afe8e3beaf09b53ffec1fda3b1d909
Rui Chen <rui@meetup.com>
2020-08-07 09:54:28 -0400

    scrcpy 1.15.1
    Closes #59296.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/scrcpy.rb
+++ Formula/scrcpy.rb
@@ -1,10 +1,9 @@
 class Scrcpy < Formula
   desc "Display and control your Android device"
   homepage "https://github.com/Genymobile/scrcpy"
-  url "https://github.com/Genymobile/scrcpy/archive/v1.14.tar.gz"
-  sha256 "45353b6e6cf1efbc8aadf2a51103242460562f6a55c6772037f492716b3fca07"
+  url "https://github.com/Genymobile/scrcpy/archive/v1.15.1.tar.gz"
+  sha256 "1d66dccf14f77e085853453d91a06fad40d1ab5cf2997f00079502edeac9d575"
   license "Apache-2.0"
-  revision 1
 
   bottle do
     sha256 "cd68e22dbb71217ef2b2cfcb36c1805ee9c5e9502e0b8ec9401d12b620b5237f" => :catalina
@@ -19,8 +18,8 @@ class Scrcpy < Formula
   depends_on "sdl2"
 
   resource "prebuilt-server" do
-    url "https://github.com/Genymobile/scrcpy/releases/download/v1.14/scrcpy-server-v1.14"
-    sha256 "1d1b18a2b80e956771fd63b99b414d2d028713a8f12ddfa5a369709ad4295620"
+    url "https://github.com/Genymobile/scrcpy/releases/download/v1.15.1/scrcpy-server-v1.15.1"
+    sha256 "fe06bd6a30da8c89860bf5e16eecce2b5054d4644c84289670ce00ca5d1637c3"
   end
 
   def install
