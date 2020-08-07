67538deda9a6ffbd3dab44cf72313a39e0294791
Dustin Rodrigues <dust.rod@gmail.com>
2020-08-07 12:57:04 +0000

    sheldon: add 0.5.3 bottle.

--- Formula/sheldon.rb
+++ Formula/sheldon.rb
@@ -7,6 +7,13 @@ class Sheldon < Formula
   license "Apache-2.0"
   head "https://github.com/rossmacarthur/sheldon.git"
 
+  bottle do
+    cellar :any
+    sha256 "d1a4347ae1da63cdd709d88b458cb33ef37860f9aba634c574d66a12d56cfa77" => :catalina
+    sha256 "a92939b2ee92320e8905ec7c71a68e109c1e46424dd7d7620645e4ea7037557d" => :mojave
+    sha256 "0890f9bcbce9e3ae8c657ea9b811404e34bc20e6416ab64d626c1f3baf97d2ac" => :high_sierra
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@1.1"
 
