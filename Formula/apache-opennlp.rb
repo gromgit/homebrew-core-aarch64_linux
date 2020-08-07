67cf9f1cf86b601cf0c5c32784a9c028810bc7aa
chenrui <chenrui333@gmail.com>
2020-08-07 08:58:11 -0400

    apache-opennlp 1.9.3 (#59159)
    * apache-opennlp 1.9.3
    
    * apache-opennlp: depends_on openjdk

--- Formula/apache-opennlp.rb
+++ Formula/apache-opennlp.rb
@@ -1,16 +1,18 @@
 class ApacheOpennlp < Formula
   desc "Machine learning toolkit for processing natural language text"
   homepage "https://opennlp.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=opennlp/opennlp-1.9.2/apache-opennlp-1.9.2-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/opennlp/opennlp-1.9.2/apache-opennlp-1.9.2-bin.tar.gz"
-  sha256 "26b55416a6c330e9c91bf9ad31183f3ed3104643b3d74ad2ee6e16b0c0e44f3b"
+  url "https://www.apache.org/dyn/closer.lua?path=opennlp/opennlp-1.9.3/apache-opennlp-1.9.3-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/opennlp/opennlp-1.9.3/apache-opennlp-1.9.3-bin.tar.gz"
+  sha256 "935eb148e3b5c5d60f80fe27d9b9de5640d385bbe2c59b046ab669375ccc4350"
   license "Apache-2.0"
 
   bottle :unneeded
 
+  depends_on "openjdk"
+
   def install
     libexec.install Dir["*"]
-    bin.write_exec_script libexec/"bin/opennlp"
+    (bin/"opennlp").write_env_script libexec/"bin/opennlp", JAVA_HOME: Formula["openjdk"].opt_prefix
   end
 
   test do
