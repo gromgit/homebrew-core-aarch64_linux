f31f4844fe0a0357983c59ded71a7b9daa316fbc
Seeker <meaningseeking@protonmail.com>
2020-08-08 18:33:59 +0000

    osm: add 0.1.0 bottle.

--- Formula/osm.rb
+++ Formula/osm.rb
@@ -5,6 +5,13 @@ class Osm < Formula
   sha256 "2074040e13b78022b5cf041c7c0102eb19c16c5924273c46ac376561e2ac0c65"
   license "MIT"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c33f82bb8363354a301b64ffa8508e127ca793940576c7eb45b88bf7169cf13f" => :catalina
+    sha256 "8597c2048dffd49f295c3166cbb5340175ba14abc2cca66e507b600acfb75d57" => :mojave
+    sha256 "0c3fbe6574723c2281a794671468eab84fa6b33ec21a1a27bda1d23ae9375335" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
