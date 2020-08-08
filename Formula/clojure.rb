cf97f1d1bf0bebd2c85e3e4935abef18c942bf5d
chenrui <chenrui333@gmail.com>
2020-08-07 20:15:20 -0400

    clojure 1.10.1.636 (#59312)

--- Formula/clojure.rb
+++ Formula/clojure.rb
@@ -1,8 +1,8 @@
 class Clojure < Formula
   desc "The Clojure Programming Language"
   homepage "https://clojure.org"
-  url "https://download.clojure.org/install/clojure-tools-1.10.1.619.tar.gz"
-  sha256 "9b2b361d2c7166b29ffdaf00114e10db574446742eb733e7fd16d4a2ba3f45ed"
+  url "https://download.clojure.org/install/clojure-tools-1.10.1.636.tar.gz"
+  sha256 "f3a962954a6e45b0f35e2f61c8c10e93dbff469766a57d3cbeb667cb0ea7b2ea"
 
   bottle :unneeded
 
