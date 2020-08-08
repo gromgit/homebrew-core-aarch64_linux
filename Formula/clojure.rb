bc2a778d4ba7355e5a3fea1a6db39c09a2b3dca8
chenrui <chenrui333@gmail.com>
2020-08-07 21:43:20 -0400

    clojure: add license (#59313)

--- Formula/clojure.rb
+++ Formula/clojure.rb
@@ -3,6 +3,7 @@ class Clojure < Formula
   homepage "https://clojure.org"
   url "https://download.clojure.org/install/clojure-tools-1.10.1.636.tar.gz"
   sha256 "f3a962954a6e45b0f35e2f61c8c10e93dbff469766a57d3cbeb667cb0ea7b2ea"
+  license "EPL-1.0"
 
   bottle :unneeded
 
