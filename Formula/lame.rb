50be1b4855242c742cfd16e9fe616c1c06a03461
Sam Ford <1584702+samford@users.noreply.github.com>
2020-08-08 08:57:25 -0400

    lame: update stable url format, add license (#59319)

--- Formula/lame.rb
+++ Formula/lame.rb
@@ -1,8 +1,9 @@
 class Lame < Formula
   desc "High quality MPEG Audio Layer III (MP3) encoder"
   homepage "https://lame.sourceforge.io/"
-  url "https://downloads.sourceforge.net/sourceforge/lame/lame-3.100.tar.gz"
+  url "https://downloads.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz"
   sha256 "ddfe36cab873794038ae2c1210557ad34857a4b6bdc515785d1da9e175b1da1e"
+  license "LGPL-2.0"
 
   bottle do
     cellar :any
