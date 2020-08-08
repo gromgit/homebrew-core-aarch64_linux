b00d461893c7925c7fd6b56f1ddddd8485cbb80c
Dustin Rodrigues <dust.rod@gmail.com>
2020-08-08 01:57:35 +0000

    jfrog-cli: update 1.38.3 bottle.

--- Formula/jfrog-cli.rb
+++ Formula/jfrog-cli.rb
@@ -7,9 +7,9 @@ class JfrogCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b0f1fe16927b98e52471328ead28971d2492395dcee462fd5d18acee896d0f37" => :catalina
-    sha256 "4dda90a876d29f9871d0f7b1b09dee1043e9542ecbebc73eb3a301fcc2279012" => :mojave
-    sha256 "1c84763c61b89ad0ebc1f5005da32e8fb43091d6f6653d82d6193828deff97e2" => :high_sierra
+    sha256 "6d4296723a05a4b6c569b0d509c1eee76a57ca725039c077657ff10745f72b64" => :catalina
+    sha256 "06facd617d87a615d7442d40419326f183ef47f3348eb283134df744f5003b0e" => :mojave
+    sha256 "abc6da254c7cb7ae9d95b64f43438b88654c4f3e9ff7271a6f498f526900c1cf" => :high_sierra
   end
 
   depends_on "go" => :build
