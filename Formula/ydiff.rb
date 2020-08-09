f9d44440552b135d42206169878bc7418010d6cf
willi123yao <willi123yao@gmail.com>
2020-08-10 04:12:47 +0800

    ydiff 1.2 (#59356)

--- Formula/ydiff.rb
+++ Formula/ydiff.rb
@@ -1,14 +1,18 @@
 class Ydiff < Formula
   desc "View colored diff with side by side and auto pager support"
   homepage "https://github.com/ymattw/ydiff"
-  url "https://github.com/ymattw/ydiff/archive/1.1.tar.gz"
-  sha256 "4c749204d1368557e74392845de22155201484edb2fdb5f97730cd6282c2dcca"
+  url "https://github.com/ymattw/ydiff/archive/1.2.tar.gz"
+  sha256 "0a0acf326b1471b257f51d63136f3534a41c0f9a405a1bbbd410457cebfdd6a1"
 
   bottle :unneeded
 
+  depends_on "python@3.8"
+
   def install
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
-    system "python", *Language::Python.setup_install_args(libexec)
+    xy = Language::Python.major_minor_version "python3"
+    ENV["PYTHONPATH"] = libexec/"lib/python#{xy}/site-packages"
+
+    system "python3", *Language::Python.setup_install_args(libexec)
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
   end
