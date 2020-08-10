ed5d6fb1529144123553fda47b5ae21af7bb49db
Rui Chen <rui@meetup.com>
2020-08-10 09:20:24 -0400

    homesick-completion: deprecate
    Closes #59409.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/homesick-completion.rb
+++ Formula/homesick-completion.rb
@@ -3,8 +3,12 @@ class HomesickCompletion < Formula
   homepage "https://github.com/JoeNyland/homesick-completion"
   url "https://github.com/JoeNyland/homesick-completion/archive/v1.0.0.tar.gz"
   sha256 "f9953d92dc5c0d9770d502a68651795c78f5d7bd6078cd747e77ebc602c43609"
+  license "MIT"
 
   bottle :unneeded
+
+  # upstream repo archived
+  deprecate!
 
   def install
     bash_completion.install "homesick"
