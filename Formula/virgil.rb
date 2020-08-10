c310d1d381d8d700438c4c4c20d2016cd105adc6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-10 03:12:45 +0000

    virgil: update 5.2.6 bottle.

--- Formula/virgil.rb
+++ Formula/virgil.rb
@@ -9,9 +9,9 @@ class Virgil < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "660c5390a198f3dd6848374d907281cd8026c7aff5a6ec7cae85267b07b941ae" => :catalina
-    sha256 "4cb7ef9ff96d2dc1c848563f421c1a24c22435431bc7467946a945a819e3638e" => :mojave
-    sha256 "2b79544d53288dc228a59c5c66bc7ea4dd58f23987a5a27bb893a191a3370559" => :high_sierra
+    sha256 "74ac4f690aa0580d8d1b9d4f5e2ebf345bc93a6769a3385c1978df1c223a6c2d" => :catalina
+    sha256 "5735c0bddb932732eb75032380d408a92f614520b985faee55ed2d00daf5d4a7" => :mojave
+    sha256 "31143675cbccc8e2eff06e482a65a720cd659aedd455a646679aec996be1836d" => :high_sierra
   end
 
   depends_on "go" => :build
