809fd9fa984ebe7b3dc3624da76a8724c61eff6c
bepsvpt <8221099+bepsvpt@users.noreply.github.com>
2020-08-10 19:23:28 +0800

    phpunit 9.3.5 (#59395)
    Co-authored-by: homebrew-updater <bepsvpt/homebrew-updater>

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -1,8 +1,8 @@
 class Phpunit < Formula
   desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-9.3.3.phar"
-  sha256 "23bbcb24ac958466eaa05931940367bed9d3428fb773c79f18b3736e363f1cd7"
+  url "https://phar.phpunit.de/phpunit-9.3.5.phar"
+  sha256 "9822ede9e430a71b8f83b2dd185f519b66140fdce6eef9d791afa8a1ee9679f9"
   license "BSD-3-Clause"
 
   bottle :unneeded
