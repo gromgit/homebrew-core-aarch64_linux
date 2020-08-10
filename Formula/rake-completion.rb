ef5bf20d7dc43dafb31180de4f4aa0ef0a3431f2
chenrui <chenrui333@gmail.com>
2020-08-09 20:52:54 -0400

    rake-completion 1.0.1 (#59387)
    * rake-completion 1.0.1
    
    * rake-completion: add license

--- Formula/rake-completion.rb
+++ Formula/rake-completion.rb
@@ -1,8 +1,9 @@
 class RakeCompletion < Formula
   desc "Bash completion for Rake"
   homepage "https://github.com/JoeNyland/rake-completion"
-  url "https://github.com/JoeNyland/rake-completion/archive/v1.0.0.tar.gz"
-  sha256 "2d619d0d1d8052994011209c62f926b9e41d45e9268da4b9858fa45911b04cd1"
+  url "https://github.com/JoeNyland/rake-completion/archive/v1.0.1.tar.gz"
+  sha256 "085801e62cb240311d77885778a603f649b3fd5d85ee279691d1d00bc060bba6"
+  license "MIT"
 
   bottle :unneeded
 
