66275899dbdc9f685ae49fbc41b3e90964fd7d3b
Pete Woods <pete.woods@circleci.com>
2020-08-10 12:15:23 +0100

    ugrep 2.5.0 (new formula)
    Closes #59398.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

---
+++ Formula/ugrep.rb
@@ -0,0 +1,30 @@
+class Ugrep < Formula
+  desc "Ultra fast grep with query UI, fuzzy search, archive search, and more"
+  homepage "https://github.com/Genivia/ugrep"
+  url "https://github.com/Genivia/ugrep/archive/v2.5.0.tar.gz"
+  sha256 "98cc23f27ec4ebba14d36f21b5625e0def4aab3e65afecc609e676938002b110"
+  license "BSD-3-Clause"
+
+  depends_on "pcre2"
+  depends_on "xz"
+
+  def install
+    ENV.O2
+    ENV.deparallelize
+    ENV.delete("CFLAGS")
+    ENV.delete("CXXFLAGS")
+    system "./configure", "--enable-color",
+                          "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"Hello.txt").write("Hello World!")
+    assert_match /Hello World!/, shell_output("#{bin}/ug 'Hello' '#{testpath}'").strip
+    assert_match /Hello World!/, shell_output("#{bin}/ugrep 'World' '#{testpath}'").strip
+  end
+end
