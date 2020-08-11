d35316428259a61658401f58d45cb71154f28053
Caleb Xu <calebcenter@live.com>
2020-08-11 00:23:20 -0400

    gostatic 2.21 (new formula)
    Closes #59453.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

---
+++ Formula/gostatic.rb
@@ -0,0 +1,40 @@
+class Gostatic < Formula
+  desc "Fast static site generator"
+  homepage "https://github.com/piranha/gostatic"
+  url "https://github.com/piranha/gostatic/archive/2.21.tar.gz"
+  sha256 "5749c03d7854cb6479ad765993cb032ad04675d18f92c1f55566b1bb2146f6a2"
+  license "ISC"
+  head "https://github.com/piranha/gostatic.git"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args
+  end
+
+  test do
+    (testpath/"config").write <<~EOS
+      TEMPLATES = site.tmpl
+      SOURCE = src
+      OUTPUT = out
+      TITLE = Hello from Homebrew
+
+      index.md:
+      \tconfig
+      \text .html
+      \tmarkdown
+      \ttemplate site
+    EOS
+
+    (testpath/"site.tmpl").write <<~EOS
+      {{ define "site" }}
+      <html><head><title>{{ .Title }}</title></head><body>{{ .Content }}</body></html>
+      {{ end }}
+    EOS
+
+    (testpath/"src/index.md").write "Hello world!"
+
+    system bin/"gostatic", testpath/"config"
+    assert_predicate testpath/"out/index.html", :exist?
+  end
+end
