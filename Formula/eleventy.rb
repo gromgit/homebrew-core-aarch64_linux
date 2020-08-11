012b2309e0e47a0a9e00285db449c125ebe8c82f
Caleb Xu <calebcenter@live.com>
2020-08-11 01:32:48 -0400

    eleventy 0.11.0 (new formula)
    Closes #59454.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

---
+++ Formula/eleventy.rb
@@ -0,0 +1,24 @@
+require "language/node"
+
+class Eleventy < Formula
+  desc "Simpler static site generator"
+  homepage "https://www.11ty.dev"
+  url "https://registry.npmjs.org/@11ty/eleventy/-/eleventy-0.11.0.tgz"
+  sha256 "1430be9f422802b1833b116822b9526ee58e59b627d67842df5cf2d8ef84f65e"
+  license "MIT"
+  head "https://github.com/11ty/eleventy.git"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"README.md").write "# Hello from Homebrew\nThis is a test."
+    system bin/"eleventy"
+    assert_equal "<h1>Hello from Homebrew</h1>\n<p>This is a test.</p>\n",
+                 (testpath/"_site/README/index.html").read
+  end
+end
