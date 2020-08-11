3a70e321c3f062da956f2289331990c9d0ec4660
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-11 04:13:06 +0000

    gitmoji: update 3.2.7 bottle.

--- Formula/gitmoji.rb
+++ Formula/gitmoji.rb
@@ -9,9 +9,9 @@ class Gitmoji < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "625e829cad6317c55ba225ad7429bdc44b5f2f0e8542913150fdbcf995f72cda" => :catalina
-    sha256 "0a9e423e8d354c7a1b5dac4cc8aee6455c54081bc5a6f81fdf1d987afc0a1dd1" => :mojave
-    sha256 "61f3bfbad5458f75bbaf554eeca5d6840a0aaa1e27702ef210a6ec3aed81013f" => :high_sierra
+    sha256 "ec16f010f6639a64c7e116d7edc896a53b3a83c9fb767107d13826346ea7046d" => :catalina
+    sha256 "b1419dd32fe24575174a1f2995d9a64355c0f99d30923e96507b428e2285c432" => :mojave
+    sha256 "950fdc21af1b817b6126ec0a2aec7ba7b05d8a8051971602747caf13a5e35702" => :high_sierra
   end
 
   depends_on "node"
