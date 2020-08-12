8ffbde8040ca3b72a26383a28800d73a2965502d
Rui Chen <rui@meetup.com>
2020-08-12 09:24:01 -0400

    mdcat 0.21.0
    Closes #59525.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/mdcat.rb
+++ Formula/mdcat.rb
@@ -1,8 +1,8 @@
 class Mdcat < Formula
   desc "Show markdown documents on text terminals"
   homepage "https://github.com/lunaryorn/mdcat"
-  url "https://github.com/lunaryorn/mdcat/archive/mdcat-0.20.0.tar.gz"
-  sha256 "c62e3fbe7461410b72b3fd27dcaa543aa0bec99cbfd78c87aa8fe441972e79ff"
+  url "https://github.com/lunaryorn/mdcat/archive/mdcat-0.21.0.tar.gz"
+  sha256 "6093ac86cffce76752d2fc7e60b147514b2f037fd193b0bfad7e3d700a9c4610"
   license "MPL-2.0"
 
   bottle do
