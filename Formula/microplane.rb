2485b0600becc2c853319be9e026e5c747ab2fdc
chenrui <chenrui333@gmail.com>
2020-08-12 00:58:53 +0000

    microplane: add 0.0.21 bottle.

--- Formula/microplane.rb
+++ Formula/microplane.rb
@@ -6,6 +6,13 @@ class Microplane < Formula
   license "Apache-2.0"
   head "https://github.com/Clever/microplane.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d68bf03327811a42c6187c44aa036fe95063109580b0cd0a2850bf6dedff2a3d" => :catalina
+    sha256 "868395416f7071c4f225930c1c1bd5c0a8544f45891c578c1801b580cb4e606e" => :mojave
+    sha256 "078f2d72287107c94382ec3f985f58ccc80fc8620750ac44718938da36e62634" => :high_sierra
+  end
+
   depends_on "dep" => :build
   depends_on "go" => :build
 
