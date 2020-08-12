aa514ab69dc7e18848e4a89c39d221e3de21fbf6
Andrew Kane <andrew@chartkick.com>
2020-08-11 19:10:19 -0700

    cbc 2.10.5 (new formula)
    Closes #59456.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

---
+++ Formula/cbc.rb
@@ -0,0 +1,46 @@
+class Cbc < Formula
+  desc "Mixed integer linear programming solver"
+  homepage "https://github.com/coin-or/Cbc"
+  url "https://github.com/coin-or/Cbc/archive/releases/2.10.5.tar.gz"
+  sha256 "cc44c1950ff4615e7791d7e03ea34318ca001d3cac6dc3f7f5ee392459ce6719"
+  # update to EPL-2.0 on next release
+  license "EPL-1.0"
+
+  depends_on "pkg-config" => :build
+  depends_on "cgl"
+  depends_on "clp"
+  depends_on "coinutils"
+  depends_on "osi"
+
+  def install
+    # Work around - same as clp formula
+    # Error 1: "mkdir: #{include}/cbc/coin: File exists."
+    mkdir include/"cbc/coin"
+
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--includedir=#{include}/cbc"
+    system "make"
+    system "make", "install"
+    pkgshare.install "Cbc/examples"
+  end
+
+  test do
+    cp_r pkgshare/"examples/.", testpath
+    system ENV.cxx, "-std=c++11", "sudoku.cpp",
+                    "-L#{lib}", "-lCbc",
+                    "-L#{Formula["cgl"].opt_lib}", "-lCgl",
+                    "-L#{Formula["clp"].opt_lib}", "-lClp", "-lOsiClp",
+                    "-L#{Formula["coinutils"].opt_lib}", "-lCoinUtils",
+                    "-L#{Formula["osi"].opt_lib}", "-lOsi",
+                    "-I#{include}/cbc/coin",
+                    "-I#{Formula["cgl"].opt_include}/cgl/coin",
+                    "-I#{Formula["clp"].opt_include}/clp/coin",
+                    "-I#{Formula["coinutils"].opt_include}/coinutils/coin",
+                    "-I#{Formula["osi"].opt_include}/osi/coin",
+                    "-o", "sudoku"
+    assert_match "solution is valid", shell_output("./sudoku")
+  end
+end
