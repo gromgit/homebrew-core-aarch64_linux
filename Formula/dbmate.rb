a5e134ec7350518359407255a183e47eca33ebfd
dbmate-publish <55776497+dbmate-publish@users.noreply.github.com>
2020-08-13 08:32:49 -0700

    dbmate 1.10.0
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #59598.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dbmate.rb
+++ Formula/dbmate.rb
@@ -1,8 +1,8 @@
 class Dbmate < Formula
   desc "Lightweight, framework-agnostic database migration tool"
   homepage "https://github.com/amacneil/dbmate"
-  url "https://github.com/amacneil/dbmate/archive/v1.9.1.tar.gz"
-  sha256 "3314d44099fc5e25c48467c8499dd797206337395fad48712f19fdebc6f9300e"
+  url "https://github.com/amacneil/dbmate/archive/v1.10.0.tar.gz"
+  sha256 "bf00360c9eb12151c157cc227a21f6688d8ea89de571b618a018b081637297a3"
   license "MIT"
   head "https://github.com/amacneil/dbmate.git"
 
