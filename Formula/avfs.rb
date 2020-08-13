0bdcf6c15f28f668628231049ab9e32cdf96e0e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-13 12:16:37 +0000

    avfs: update 1.1.3 bottle.

--- Formula/avfs.rb
+++ Formula/avfs.rb
@@ -5,9 +5,9 @@ class Avfs < Formula
   sha256 "4f4ec1e8c0d5da94949e3dab7500ee29fa3e0dda723daf8e7d60e5f3ce4450df"
 
   bottle do
-    sha256 "b384f9e4d2e44eb776ea56e5daa54e08caf7ed735c17717f1a81c5ded06375bc" => :catalina
-    sha256 "a20fc2155f0b4392a8d417e2bd960fd8c579ec8666946e3c127be0306288e847" => :mojave
-    sha256 "8ae40f6a5695d4b2e1e66db5201a524ddf2bfbfa97a45d9e72d8adb89090de7b" => :high_sierra
+    sha256 "6f496a30b6bd1c8eba1005e4bc0da26b53353effab3f447cf8d43a669ad7a6b5" => :catalina
+    sha256 "1e75ce4753a0d9a9af12e4a718537a9e2398fd535413b72505dd126a33610fe6" => :mojave
+    sha256 "690fbe0161f0c5ce4ec737e67624b54bfcd7825efa8b554e1773691365dcd6ed" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
