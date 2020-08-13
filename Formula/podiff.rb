2914087323e517d883eaa1aa1e9f559bff9180e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-13 08:14:01 +0000

    podiff: update 1.2 bottle.

--- Formula/podiff.rb
+++ Formula/podiff.rb
@@ -7,13 +7,9 @@ class Podiff < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "34c82b7a2caa7d65bea883d10bf20dc29346a04c381c9e7bf4a38473916d079f" => :catalina
-    sha256 "2a9b5cb91d5f6b36ea965f2d7bd85fd47c5719f3a9d9079cba7e5d2f6cd667c3" => :mojave
-    sha256 "c862b6ec4c259c495879fd91dcb143e9768b2cbb12ae5e07506d63350e36cdbb" => :high_sierra
-    sha256 "be964cc5259d37f0f4676d5e3a0fac77b42e4f3e7641c7579d47182e4f036e83" => :sierra
-    sha256 "24f385afff1c9074d995cd1374c005e8770a80ef11b06058f6024eee67a9de69" => :el_capitan
-    sha256 "21cc248bf36ce685061e234f57c6fc5cbe0d207b201e7a8e485ee7b71d3d21c6" => :yosemite
-    sha256 "c3a3b66344303ad473d8c6808eb9e2a2124bb0a801536870b739beba4de3d5ae" => :mavericks
+    sha256 "cdda50f296e87f84f828d09777f90217c98ca4578a00b09307df9dcd830424c2" => :catalina
+    sha256 "20e29ef344ca1da47dff379a12290150de1540338d49d00043a2093f3a22a6fa" => :mojave
+    sha256 "71b8f6e4b7935a26b50e32805036593d4fd20e24d4de73023a423a6889e72752" => :high_sierra
   end
 
   def install
