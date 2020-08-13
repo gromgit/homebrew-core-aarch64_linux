37070554db9fd19ea46593508ed2d63953e5b8eb
Caleb Xu <calebcenter@live.com>
2020-08-13 13:58:54 -0400

    staticcheck 2020.1.5 (new formula)
    Closes #59608.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: chenrui <chenrui333@gmail.com>

---
+++ Formula/staticcheck.rb
@@ -0,0 +1,30 @@
+class Staticcheck < Formula
+  desc "State of the art linter for the Go programming language"
+  homepage "https://staticcheck.io/"
+  url "https://github.com/dominikh/go-tools/archive/2020.1.5.tar.gz"
+  sha256 "aa57272dd96a8d93af74c504c9a8f9949712e1983512830a0b82f3afbd96b4ac"
+  license "MIT"
+  head "https://github.com/dominikh/go-tools.git"
+
+  depends_on "go"
+
+  def install
+    system "go", "build", *std_go_args, "./cmd/staticcheck"
+  end
+
+  test do
+    (testpath/"test.go").write <<~EOS
+      package main
+
+      import "fmt"
+
+      func main() {
+        var x uint
+        x = 1
+        fmt.Println(x)
+      }
+    EOS
+    json_output = JSON.parse(shell_output("#{bin}/staticcheck -f json test.go", 1))
+    assert_equal json_output["code"], "S1021"
+  end
+end
