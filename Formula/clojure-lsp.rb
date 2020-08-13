00f94997a05e7634b2eb70b74a893032bada8ae7
Rui Chen <rui@meetup.com>
2020-08-13 08:26:20 -0400

    clojure-lsp 20200812T191554
    Closes #59564.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/clojure-lsp.rb
+++ Formula/clojure-lsp.rb
@@ -3,9 +3,9 @@ class ClojureLsp < Formula
   homepage "https://github.com/snoe/clojure-lsp"
   # Switch to use git tag/revision as needed by `lein-git-version`
   url "https://github.com/snoe/clojure-lsp.git",
-    tag:      "release-20200806T150857",
-    revision: "04bf409514634c2e537dca13c3d83ddadcdbd4f2"
-  version "20200806T150857"
+    tag:      "release-20200812T191554",
+    revision: "49271b758d40dcaf8a090fabb9eac0fd8a336a3e"
+  version "20200812T191554"
   license "MIT"
   head "https://github.com/snoe/clojure-lsp.git"
 
