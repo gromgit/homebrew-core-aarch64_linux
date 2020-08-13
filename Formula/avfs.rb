48569c6495145383debcd1fe6c6cabc8475a877b
Alexander Zigelski <ali@crunchtime.dev>
2020-08-13 13:33:40 +0200

    avfs 1.1.3
    Closes #59562.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/avfs.rb
+++ Formula/avfs.rb
@@ -1,8 +1,8 @@
 class Avfs < Formula
   desc "Virtual file system that facilitates looking inside archives"
   homepage "https://avf.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/avf/avfs/1.1.2/avfs-1.1.2.tar.bz2"
-  sha256 "b69339d3602221bc381fd7528e6dda4b23522f825781638be3fa7d9b8837ab0c"
+  url "https://downloads.sourceforge.net/project/avf/avfs/1.1.3/avfs-1.1.3.tar.bz2"
+  sha256 "4f4ec1e8c0d5da94949e3dab7500ee29fa3e0dda723daf8e7d60e5f3ce4450df"
 
   bottle do
     sha256 "b384f9e4d2e44eb776ea56e5daa54e08caf7ed735c17717f1a81c5ded06375bc" => :catalina
@@ -27,9 +27,6 @@ class Avfs < Formula
       --with-ssl=#{Formula["openssl@1.1"].opt_prefix}
     ]
 
-    # This fix is already present in upstream but so far unreleased
-    # https://sourceforge.net/p/avf/git/ci/ea83e0559f65ab89e81b4105e38b39a40cf9900e
-    inreplace "modules/urar.c", "#include <unistd.h>", "#include <unistd.h>\n#include <limits.h>"
     system "./configure", *args
     system "make", "install"
   end
