5925febd058f99f2160fd375574180484b83e73d
Rui Chen <rui@meetup.com>
2020-08-13 19:33:41 -0400

    dcd 0.13.1

--- Formula/dcd.rb
+++ Formula/dcd.rb
@@ -2,8 +2,8 @@ class Dcd < Formula
   desc "Auto-complete program for the D programming language"
   homepage "https://github.com/dlang-community/DCD"
   url "https://github.com/dlang-community/DCD.git",
-      tag:      "v0.13.0",
-      revision: "808460a678d9c993d393c36e7eb06601a157efcf"
+      tag:      "v0.13.1",
+      revision: "92db0486968f8ee59c16099c21b133dc4e9d91cb"
   license "GPL-3.0"
   head "https://github.com/dlang-community/dcd.git", shallow: false
 
