23c119223ee1193e248c3cd975029be3ea452993
Gábor Boros <gabor.brs@gmail.com>
2020-08-14 07:09:59 +0200

    rethinkdb 2.4.1
    * rethinkdb 2.4.1
    * add patch PR ref
    * rethinkdb: use commit patch
    
    Closes #67782.
    
    Co-authored-by: Jonathan Chang <jonathan.chang@monash.edu>
    Signed-off-by: Rui Chen <rui@meetup.com>
    Co-authored-by: Rui Chen <rui@meetup.com>
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/rethinkdb.rb
+++ Formula/rethinkdb.rb
@@ -1,8 +1,8 @@
 class Rethinkdb < Formula
   desc "Open-source database for the realtime web"
   homepage "https://rethinkdb.com/"
-  url "https://download.rethinkdb.com/repository/raw/dist/rethinkdb-2.4.0.tgz"
-  sha256 "bfb0708710595c6762f42e25613adec692cf568201cd61da74c254f49fa9ee4c"
+  url "https://download.rethinkdb.com/repository/raw/dist/rethinkdb-2.4.1.tgz"
+  sha256 "5f1786c94797a0f8973597796e22545849dc214805cf1962ef76969e0b7d495b"
   license "Apache-2.0"
   head "https://github.com/rethinkdb/rethinkdb.git", branch: "next"
 
@@ -26,13 +26,18 @@ class Rethinkdb < Formula
 
   uses_from_macos "curl"
 
-  def install
-    args = ["--prefix=#{prefix}"]
-    args += ["--allow-fetch"] if build.head?
+  # patch submitted to upstream, https://github.com/rethinkdb/rethinkdb/pull/6934
+  # remove in next release
+  patch do
+    url "https://github.com/chenrui333/rethinkdb/commit/d7d22b4.patch?full_index=1"
+    sha256 "45d387c3360cf6265e27485c273c37224b2ef0000ebd4fdd361dcecc4d2e9791"
+  end
 
+  def install
     # rethinkdb requires that protobuf be linked against libc++
     # but brew's protobuf is sometimes linked against libstdc++
-    args += ["--fetch", "protobuf"]
+    args = %W[--prefix=#{prefix} --fetch protobuf]
+    args << "--allow-fetch" if build.head?
 
     system "./configure", *args
     system "make"
