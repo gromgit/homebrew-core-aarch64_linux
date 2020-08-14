491ad1520d448ba295761ddfe59c740d4672bb6b
Dawid Dziurla <dawidd0811@gmail.com>
2020-08-14 13:17:29 +0200

    deheader: rewrite python shebang
    https://github.com/Homebrew/linuxbrew-core/pull/20954
    
    Closes #59649.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/deheader.rb
+++ Formula/deheader.rb
@@ -1,9 +1,12 @@
 class Deheader < Formula
+  include Language::Python::Shebang
+
   desc "Analyze C/C++ files for unnecessary headers"
   homepage "http://www.catb.org/~esr/deheader"
   url "http://www.catb.org/~esr/deheader/deheader-1.7.tar.gz"
   sha256 "6856e4fa3efa664a0444b81c2e1f0209103be3b058455625c79abe65cf8db70d"
   license "BSD-2-Clause"
+  revision 1
   head "https://gitlab.com/esr/deheader.git"
 
   bottle do
@@ -14,6 +17,7 @@ class Deheader < Formula
   end
 
   depends_on "xmlto" => :build
+  depends_on "python@3.8"
 
   on_linux do
     depends_on "libarchive" => :build
@@ -25,6 +29,8 @@ class Deheader < Formula
     system "make"
     bin.install "deheader"
     man1.install "deheader.1"
+
+    rewrite_shebang detected_python_shebang, bin/"deheader"
   end
 
   test do
