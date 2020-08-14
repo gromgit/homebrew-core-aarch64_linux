9113aa03c91a50b4fc704dbc696926a595ce6f61
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-14 19:12:28 +0000

    mariadb@10.2: update 10.2.33 bottle.

--- Formula/mariadb@10.2.rb
+++ Formula/mariadb@10.2.rb
@@ -6,9 +6,9 @@ class MariadbAT102 < Formula
   license "GPL-2.0-only"
 
   bottle do
-    sha256 "7cd378e6fe322f9456a119be681cabe358b934019446402a7d7a3ebdf28ea353" => :catalina
-    sha256 "2768d5d4101ac669ca3a8e5b0dc2fd5d06d630cc4e50afad0c2ad89ee920bdd0" => :mojave
-    sha256 "c592f1f9da3f224916a046ccadd8d8adfa7f25ad17809114a9128a7395bcc924" => :high_sierra
+    sha256 "3188883efa988994dfa76c8a0bf9a06da65d77008bbf9f07691e06687f144305" => :catalina
+    sha256 "c0ab2f2acb1e7bd6c996633d39b01f29d4f87d34f9dea53f51421ea1c44c755e" => :mojave
+    sha256 "8a1ed1a5abb9052396acc769cac3393a277b23c42f22e18ec169e9cffe52b40d" => :high_sierra
   end
 
   keg_only :versioned_formula
