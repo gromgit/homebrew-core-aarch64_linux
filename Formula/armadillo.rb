3d530e276249c3a9ad820a43a3abc25a4ba0df96
Matteo Seclì <secli.matteo@gmail.com>
2020-08-14 11:31:11 +0200

    Swap dependencies according to brew audit --strict

--- Formula/armadillo.rb
+++ Formula/armadillo.rb
@@ -14,10 +14,10 @@ class Armadillo < Formula
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-  depends_on "openblas"
   depends_on "arpack"
-  depends_on "superlu"
+  depends_on "openblas"
   depends_on "hdf5"
+  depends_on "superlu"
   depends_on "szip"
 
   def install
