d136dc49e8b1cf43eeff885183cd08553f2b1cf7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-15 14:13:35 +0000

    sheldon: update 0.5.4 bottle.

--- Formula/sheldon.rb
+++ Formula/sheldon.rb
@@ -8,9 +8,9 @@ class Sheldon < Formula
 
   bottle do
     cellar :any
-    sha256 "d1a4347ae1da63cdd709d88b458cb33ef37860f9aba634c574d66a12d56cfa77" => :catalina
-    sha256 "a92939b2ee92320e8905ec7c71a68e109c1e46424dd7d7620645e4ea7037557d" => :mojave
-    sha256 "0890f9bcbce9e3ae8c657ea9b811404e34bc20e6416ab64d626c1f3baf97d2ac" => :high_sierra
+    sha256 "c0b02a54e4d4aa0fc5d444d25dc4931284e195805f589072a7c67527d58f3553" => :catalina
+    sha256 "5b87b182244565b087e5d781f9adc3955002a4306b4e87c57a202c0d75a5ca3a" => :mojave
+    sha256 "3f1fdfdfac34a711fd99a9db7d6bec8375bdc7633b16433551ab23834ce6c5ae" => :high_sierra
   end
 
   depends_on "rust" => :build
