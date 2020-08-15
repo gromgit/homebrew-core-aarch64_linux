a91548554f287c56c80a7dc7abf36c1fb08f6ae8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-15 22:12:10 +0000

    logcli: update 1.6.0 bottle.

--- Formula/logcli.rb
+++ Formula/logcli.rb
@@ -7,9 +7,9 @@ class Logcli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e54c343b3d2f59c89074932d84d860a63c2f554cea4d318788168e92ed359d4d" => :catalina
-    sha256 "895cd722c06b619fc35dc8cf34b43f0c500ebbb35ed658e53faf20b7f5f85ad3" => :mojave
-    sha256 "f82e423546fe2428c2491410f6497a6b132390ef54e5f1cdc92be6fa748a2f62" => :high_sierra
+    sha256 "af69580a9106b8b08ee73b65aaa21e159004337a07f976c9c1cf027c6b45d502" => :catalina
+    sha256 "2a50707940dff101b465d2f5bf4a926ea506081a80ba711681ca399cf1ba3cca" => :mojave
+    sha256 "759365beafe8a2996eaeaea081a1020b545a2ed28b8a74e017b4d63823b30193" => :high_sierra
   end
 
   depends_on "go" => :build
