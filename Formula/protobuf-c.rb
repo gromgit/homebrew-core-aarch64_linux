309c6b972ee9d450fd51634edfdc22524afb881c
Austin Shalit <austinshalit@gmail.com>
2020-08-14 23:33:42 -0700

    protobuf-c: revision bump

--- Formula/protobuf-c.rb
+++ Formula/protobuf-c.rb
@@ -4,7 +4,7 @@ class ProtobufC < Formula
   url "https://github.com/protobuf-c/protobuf-c/releases/download/v1.3.3/protobuf-c-1.3.3.tar.gz"
   sha256 "22956606ef50c60de1fabc13a78fbc50830a0447d780467d3c519f84ad527e78"
   license "BSD-2-Clause"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
