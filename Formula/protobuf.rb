cdfffc7afe630b8e15edb1da62726ba768bca0dc
Austin Shalit <austinshalit@gmail.com>
2020-08-14 18:01:32 -0700

    protobuf 3.13.0

--- Formula/protobuf.rb
+++ Formula/protobuf.rb
@@ -1,8 +1,8 @@
 class Protobuf < Formula
   desc "Protocol buffers (Google's data interchange format)"
   homepage "https://github.com/protocolbuffers/protobuf/"
-  url "https://github.com/protocolbuffers/protobuf/releases/download/v3.12.4/protobuf-all-3.12.4.tar.gz"
-  sha256 "e7bf41873d1a87c05c2b0a6197f4445c6ea3469ce0165ff14de2df8b34262530"
+  url "https://github.com/protocolbuffers/protobuf/releases/download/v3.13.0/protobuf-all-3.13.0.tar.gz"
+  sha256 "465fd9367992a9b9c4fba34a549773735da200903678b81b25f367982e8df376"
   license "BSD-3-Clause"
 
   bottle do
