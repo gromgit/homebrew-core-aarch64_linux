92745cea471752c543cbbab5f397d0f0f890c425
Ryosuke IWANAGA <me@riywo.com>
2020-08-14 23:41:30 -0700

    anyenv 1.1.2
    Closes #59685.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/anyenv.rb
+++ Formula/anyenv.rb
@@ -1,8 +1,8 @@
 class Anyenv < Formula
   desc "All in one for **env"
   homepage "https://anyenv.github.io/"
-  url "https://github.com/anyenv/anyenv/archive/v1.1.1.tar.gz"
-  sha256 "d7f386f74d5fd40ded51b4e83f05490493fc6b63d22eb3d502836020548b0137"
+  url "https://github.com/anyenv/anyenv/archive/v1.1.2.tar.gz"
+  sha256 "414dd42b262cc0ddb8ce77f2f2971943b55a045286a85232372e42d60b16389f"
   license "MIT"
 
   bottle do
