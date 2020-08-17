ea328e83e22f7eaf488917301b900f937a479dba
Rui Chen <rui@meetup.com>
2020-08-17 01:58:44 -0400

    rgf 3.9.0
    Closes #59759.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rgf.rb
+++ Formula/rgf.rb
@@ -1,8 +1,8 @@
 class Rgf < Formula
   desc "Regularized Greedy Forest library"
   homepage "https://github.com/RGF-team/rgf"
-  url "https://github.com/RGF-team/rgf/archive/3.8.0.tar.gz"
-  sha256 "f21dce9b877bf802a5699ce58b1b87346337d3be838ddfe78b18cfec45813af7"
+  url "https://github.com/RGF-team/rgf/archive/3.9.0.tar.gz"
+  sha256 "78ccd04dfcb610094393e8463e01e6933d9ff15ed08024e6088daf7ab0e94a97"
   license "MIT"
 
   bottle do
