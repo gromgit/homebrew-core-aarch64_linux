5ef574dc14311a3dea0f97b308ddd2ce92799772
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-17 08:14:24 +0000

    promtail: update 1.6.0 bottle.

--- Formula/promtail.rb
+++ Formula/promtail.rb
@@ -7,9 +7,9 @@ class Promtail < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "70470c14896c1ed892a274541431b88938532a80982559c698f6e1a932017edf" => :catalina
-    sha256 "47495ee7998dc8f9e6ccd2c8d601153d9e7f4d2ac70afadb44835bbef11ba5f5" => :mojave
-    sha256 "62fa78cc81525e0141bc8dc3d1c3d0f0fead0e9ab0722ccb6566405d9a0abe82" => :high_sierra
+    sha256 "80899b66557883d07214162e1430ef1a1351b24f0acff1989f8deae263ca5177" => :catalina
+    sha256 "fa62cef67ad8deb5ece3ef8c767d5ad66054c4effc797a9877b5270a7c62d3b5" => :mojave
+    sha256 "3b9e7f19626dd7df92d3a3de3c0adb3bbaf1bc4f1039e036fa8cabecde16d8e4" => :high_sierra
   end
 
   depends_on "go" => :build
