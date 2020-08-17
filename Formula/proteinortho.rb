ef7ec4727824791547e21f9bfdf67de3d72cb66d
chenrui <rui@meetup.com>
2020-08-17 02:15:53 -0400

    proteinortho: update license
    Closes #59758.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/proteinortho.rb
+++ Formula/proteinortho.rb
@@ -3,7 +3,7 @@ class Proteinortho < Formula
   homepage "https://gitlab.com/paulklemm_PHD/proteinortho"
   url "https://gitlab.com/paulklemm_PHD/proteinortho/-/archive/v6.0.21/proteinortho-v6.0.21.tar.gz"
   sha256 "fa7177da0f3fe42eb59f6287c52e05568b4f4cb9c4e2b1640ac87af6c9080420"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
 
   bottle do
     cellar :any
