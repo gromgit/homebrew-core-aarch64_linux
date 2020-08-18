92c722d4ec62dc7bbf69a2a55a930427397f788a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-18 12:17:02 +0000

    siril: update 0.99.4 bottle.

--- Formula/siril.rb
+++ Formula/siril.rb
@@ -7,9 +7,9 @@ class Siril < Formula
   head "https://gitlab.com/free-astro/siril.git"
 
   bottle do
-    sha256 "67b6fadc0395a67e3f6d9c62e5aa799943c88667fb314ecb7c7bf2fc5cf4df56" => :catalina
-    sha256 "e648058025e964763cec9bdac798cf2f22de519509369484e3ef9c3187c5fcca" => :mojave
-    sha256 "642afa82a18456cf54339bbb402d25805f88628c64e02f32eb35dbde4f98990f" => :high_sierra
+    sha256 "42ec041e02ee5d09131c256f96f202d325c06878bc142841699d9cf7a98f125b" => :catalina
+    sha256 "00e7b66ced0dc8e4ae619713e4ed2cc075061b9137485a2b33a7ab733d3b2d82" => :mojave
+    sha256 "21fbaa766a7ddd7d70a549e77a5ee04e1ae2fdcd83d2b6fae21926f01fde15e4" => :high_sierra
   end
 
   depends_on "autoconf" => :build
