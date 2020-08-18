029480be74b5bd6b4e3eececc475581d80614974
chenrui <rui@meetup.com>
2020-08-17 22:05:40 -0400

    tccutil: update license

--- Formula/tccutil.rb
+++ Formula/tccutil.rb
@@ -3,7 +3,7 @@ class Tccutil < Formula
   homepage "https://github.com/jacobsalmela/tccutil"
   url "https://github.com/jacobsalmela/tccutil/archive/v1.2.9.tar.gz"
   sha256 "8f1212c80f0df0ddcb76f921ae43a3e5c1e65044f68569b52cbef2c02e558157"
-  license "GPL-2.0"
+  license "GPL-2.0-or-later"
   head "https://github.com/jacobsalmela/tccutil.git"
 
   bottle :unneeded
