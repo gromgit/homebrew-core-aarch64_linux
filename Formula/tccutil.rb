8a1efeb6b342b50bd7c7f4aab9355e75f593f456
Jacob Salmela <me@jacobsalmela.com>
2020-08-17 20:43:08 -0500

    tccutil 1.2.9

--- Formula/tccutil.rb
+++ Formula/tccutil.rb
@@ -1,8 +1,8 @@
 class Tccutil < Formula
   desc "Utility to modify the macOS Accessibility Database (TCC.db)"
   homepage "https://github.com/jacobsalmela/tccutil"
-  url "https://github.com/jacobsalmela/tccutil/archive/v1.2.7.tar.gz"
-  sha256 "4584962a059584ba0810cee7912f9333ba6259da22bea7e6b3206bcf934d5cf1"
+  url "https://github.com/jacobsalmela/tccutil/archive/v1.2.9.tar.gz"
+  sha256 "8f1212c80f0df0ddcb76f921ae43a3e5c1e65044f68569b52cbef2c02e558157"
   license "GPL-2.0"
   head "https://github.com/jacobsalmela/tccutil.git"
 
