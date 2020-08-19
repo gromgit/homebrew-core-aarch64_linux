48c3419b2ce13d2927627d78eee634d0c2003c86
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-19 05:13:14 +0000

    ory-hydra: update 1.7.0 bottle.

--- Formula/ory-hydra.rb
+++ Formula/ory-hydra.rb
@@ -7,9 +7,9 @@ class OryHydra < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f9e1c364d45b83b7013ae48efb6d785c44a7286b755443926a4de3d16132d1a4" => :catalina
-    sha256 "a96797c6f94d2ae341d23866b76c7e09b390dcaf61aa8064806e193b7645aeac" => :mojave
-    sha256 "04bf760fdd26360a93226340bb9a681534924a9d84b46e9298f601793ac44a21" => :high_sierra
+    sha256 "2f3a82862e3669005719c9c7b69e0c83b3e2523027bc5f64ff00d498c5649c53" => :catalina
+    sha256 "fcfd384f726c51960f0103467e81f39e69ccb2ce434dd68134deb48e200ce50e" => :mojave
+    sha256 "8e4cda62383e2cf4bff91cdc8a76fd25cc9bfc4880fd729b18c9a18e008b35a1" => :high_sierra
   end
 
   depends_on "go" => :build
