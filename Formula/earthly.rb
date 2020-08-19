746bb60dad6d1a2cc8328770b7ec324f996acda1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-19 21:59:46 +0000

    earthly: update 0.3.1 bottle.

--- Formula/earthly.rb
+++ Formula/earthly.rb
@@ -8,9 +8,9 @@ class Earthly < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5929dc77833f8c3df5401ac67fb3cd6f519b9f24152d6c8e52d8675919826934" => :catalina
-    sha256 "b6d2e459b9d8d12033899bb16acb33b5db19119f8bb198983ecb051bb367ab2a" => :mojave
-    sha256 "5d6412cc573aebf07cf98b90d755964747d4868135f2ca721761d30e7133d53d" => :high_sierra
+    sha256 "da9d4f4c382ea6380c4c5ee82f9b7b42efd9be7f6e7c019eea5bfd97f7a63479" => :catalina
+    sha256 "709ec0a0a31a4b0c10f571bc384fe3c75b49ca50d19eba28061156307746bbd0" => :mojave
+    sha256 "ca13dba89290ac229a849a4c3f6acb6f598e69471260893fddd9e32dae89aeb5" => :high_sierra
   end
 
   depends_on "go" => :build
