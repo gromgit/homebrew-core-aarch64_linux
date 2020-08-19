60f4bf844a41e164a666f642d42298a24960ed4c
Caleb Xu <calebcenter@live.com>
2020-08-19 01:24:55 -0400

    nifi 1.12.0 (#59868)

--- Formula/nifi.rb
+++ Formula/nifi.rb
@@ -1,9 +1,9 @@
 class Nifi < Formula
   desc "Easy to use, powerful, and reliable system to process and distribute data"
   homepage "https://nifi.apache.org"
-  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.11.4/nifi-1.11.4-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/nifi/1.11.4/nifi-1.11.4-bin.tar.gz"
-  sha256 "5bb68014f818f74b475bcd774ce8c446fc20368ec3062c5ef86e4af9b2ba9aef"
+  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.12.0/nifi-1.12.0-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/nifi/1.12.0/nifi-1.12.0-bin.tar.gz"
+  sha256 "e2ddbc1b57d88e1e154c40d2c533b42ff37275bed1d71a392eb2fa111511651b"
   license "Apache-2.0"
 
   bottle :unneeded
