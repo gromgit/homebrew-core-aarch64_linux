c9d9c24f831840101ee85f094f3a10a3f6da840a
Dustin Rodrigues <dust.rod@gmail.com>
2020-08-19 19:42:25 -0400

    appium 1.18.1
    Closes #59903.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>

--- Formula/appium.rb
+++ Formula/appium.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Appium < Formula
   desc "Automation for Apps"
   homepage "https://appium.io/"
-  url "https://registry.npmjs.org/appium/-/appium-1.18.0.tgz"
-  sha256 "7c2ace86435147fe3d961199d5bac542ed159aefa8ab1d5b58c7b88cd273ed3c"
+  url "https://registry.npmjs.org/appium/-/appium-1.18.1.tgz"
+  sha256 "938783100df8be224cb85b5dbdcebeb91d164f003d4cdd2aa9c801480bd027b4"
   license "Apache-2.0"
   head "https://github.com/appium/appium.git"
 
