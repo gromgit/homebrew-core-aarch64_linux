49e110e8e3106184e19f5e089ab11e3ad5740839
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-19 02:14:24 +0000

    terraform-ls: update 0.6.1 bottle.

--- Formula/terraform-ls.rb
+++ Formula/terraform-ls.rb
@@ -8,9 +8,9 @@ class TerraformLs < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b3aba9b27a2293982a203924ed47c0cf58ca1f1d242fb57dfbc1b2ef21cb304a" => :catalina
-    sha256 "5fb933f7dba0e21910f7ff37f0d7bca35e286e2f09b974a49c68804c7358fdd1" => :mojave
-    sha256 "797540b968c723b223b50ff51c6e4204e842833094e85c4664d188840244d350" => :high_sierra
+    sha256 "585a5b91ccd9e1ab9238c06cf5f3a49526d8be2df05ebf58df3948afdf9b3c14" => :catalina
+    sha256 "913dba8ec8eeb9db4933520083ab1b27c49f332dc48744b089c52d2a89960c7e" => :mojave
+    sha256 "87b86cbf117c4aa3cb11be9f4be025171e510127b40bed5d6db91fa3b6f1a7f4" => :high_sierra
   end
 
   depends_on "go" => :build
