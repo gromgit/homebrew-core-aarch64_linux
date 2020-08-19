2cb96a9d6239ccfef0514352372b279e229ad183
Roger Que <git@alerante.net>
2020-08-19 14:15:42 -0700

    castget 2.0.1 (new formula)
    Closes #59901.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

---
+++ Formula/castget.rb
@@ -0,0 +1,44 @@
+class Castget < Formula
+  desc "Command-line podcast and RSS enclosure downloader"
+  homepage "https://castget.johndal.com/"
+  url "http://savannah.nongnu.org/download/castget/castget-2.0.1.tar.bz2"
+  sha256 "438b5f7ec7e31a45ed3756630fe447f42015acda53ec09202f48628726b5e875"
+  license "LGPL-2.1-only"
+
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "id3lib"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.rss").write <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <rss version="2.0">
+        <channel>
+          <title>Test podcast</title>
+          <description>Test podcast</description>
+          <link>http://www.podcast.test/</link>
+          <item>
+            <title>Test item</title>
+            <enclosure url="#{test_fixtures("test.mp3")}" type="audio/mpeg" />
+          </item>
+        </channel>
+      </rss>
+    EOS
+
+    (testpath/"castgetrc").write <<~EOS
+      [test]
+      url=file://#{testpath/"test.rss"}
+      spool=#{testpath}
+    EOS
+
+    system "#{bin}/castget", "-C", testpath/"castgetrc"
+    assert_predicate testpath/"test.mp3", :exist?
+  end
+end
