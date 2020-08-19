debc0378be636928bac60634d438a46ddea20737
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-19 04:14:35 +0000

    ircd-hybrid: update 8.2.32 bottle.

--- Formula/ircd-hybrid.rb
+++ Formula/ircd-hybrid.rb
@@ -6,9 +6,9 @@ class IrcdHybrid < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 "547457f864424055dca2cea8503012e2b9847aebbc1ee21fda02100d357cef3e" => :catalina
-    sha256 "abce429d77cb8c9949cd25f1c3560642eadf264906d05340779acea9e92c35dd" => :mojave
-    sha256 "444b6c2610188e971e583200ed8f243ce1609682690cd8e1775b9b98321f2f48" => :high_sierra
+    sha256 "4141097277b2a5e1c28c4d6ac1bf0b01743c7e34fb371aca7267e480db8ec830" => :catalina
+    sha256 "0a4639339b71b7b385a5f7a5bf0755d1b055a093db29ace3421d768f1aa6c4d9" => :mojave
+    sha256 "9d334046c230f6df1c75a7e5313477bb79fe6cc35f53f9be441216c577ca3d22" => :high_sierra
   end
 
   depends_on "openssl@1.1"
