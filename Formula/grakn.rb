bd27f5ba26941a8b496b180befbfcb216ab4d547
Rui Chen <rui@meetup.com>
2020-08-20 18:50:27 -0400

    grakn 1.8.1

--- Formula/grakn.rb
+++ Formula/grakn.rb
@@ -1,8 +1,8 @@
 class Grakn < Formula
   desc "The distributed hyper-relational database for knowledge engineering"
   homepage "https://grakn.ai"
-  url "https://github.com/graknlabs/grakn/releases/download/1.8.0/grakn-core-all-mac-1.8.0.zip"
-  sha256 "5d9e4e7067e53be2868d8a9a56f9e64fe2dee2b46039a303655d32911cbbdf56"
+  url "https://github.com/graknlabs/grakn/releases/download/1.8.1/grakn-core-all-mac-1.8.1.zip"
+  sha256 "24d0f66d682250c00a400f8ace24162dc1b246078b28da355374667a68631264"
   license "AGPL-3.0"
 
   bottle :unneeded
