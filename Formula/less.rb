60937b9fc23d61840c3a5a0b6ed6d041529e5f7c
Waldir Pimenta <waldyrious@gmail.com>
2020-08-20 23:19:20 +0100

    less: add license
    Sources:
    
    - https://github.com/gwsw/less/blob/master/COPYING
    
    - https://github.com/gwsw/less/blob/master/LICENSE
    
    - https://github.com/gwsw/less/blob/master/README.VER:
    
      > This program is free software.
      > You may redistribute it and/or modify it under the terms of either:
      > 1. The GNU General Public License, as published by the Free Software
      >    Foundation; either version 3, or (at your option) any later version.
      >    A copy of this license is in the file COPYING.
      > or
      > 2. The Less License, in the file LICENSE.
    
    Note about the Less License:
    
      The less project is dual-licensed, but the Less License lacks a SPDX identifier.
      It matches the BSD-2-Clause nearly exactly, but changes the second clause from:
    
      > Redistributions in binary form must reproduce the above copyright notice,
      > this list of conditions and the following disclaimer
      > in the documentation and/or other materials provided with the distribution.
    
      to simply:
    
      > Redistributions in binary form must reproduce the above copyright notice
      > in the documentation and/or other materials provided with the distribution.
    
      Notice how "this list of conditions and the following disclaimer"
      was removed from the clause, so the legal obligations are slightly different
    
      If eventually the Less License gets an SPDX identifier,
      we might change the formula's license expression to include it,
      but at this point we are unable to represent it accurately,
      so only the GPL is mentioned (which is allowed in a dual-licensing context).

--- Formula/less.rb
+++ Formula/less.rb
@@ -3,6 +3,7 @@ class Less < Formula
   homepage "http://www.greenwoodsoftware.com/less/index.html"
   url "http://www.greenwoodsoftware.com/less/less-551.tar.gz"
   sha256 "ff165275859381a63f19135a8f1f6c5a194d53ec3187f94121ecd8ef0795fe3d"
+  license "GPL-3.0-or-later"
 
   livecheck do
     url :homepage
