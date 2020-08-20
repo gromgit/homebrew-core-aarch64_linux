72eb2c03b1c34478d79fe864dc5b7bb987fb8a2b
Bo Anderson <mail@boanderson.me>
2020-08-20 14:21:48 +0100

    usb.ids: fix component order

--- Formula/usb.ids.rb
+++ Formula/usb.ids.rb
@@ -3,14 +3,14 @@ class UsbIds < Formula
   homepage "http://www.linux-usb.org/usb-ids.html"
   url "https://deb.debian.org/debian/pool/main/u/usb.ids/usb.ids_2020.06.22.orig.tar.xz"
   sha256 "d55befb3b8bdb5db799fb8894c4e27ef909b2975c062fa6437297902213456a7"
+  license ["GPL-2.0-or-later", "BSD-3-Clause"]
+
   bottle do
     cellar :any_skip_relocation
     sha256 "86ee1383749511bfa93b03e04b34e99f405abfbe791df50be85f992a303965c1" => :catalina
     sha256 "9acaeabecc451f483976baeee5967a6123d4945b3227942b15ecec8bb390e44f" => :mojave
     sha256 "cb6f7ce696f58356e9ab5679ac4dba62f66d597dd62eacdb64e94e94c10e5be8" => :high_sierra
   end
-
-  license ["GPL-2.0-or-later", "BSD-3-Clause"]
 
   def install
     (share/"misc").install "usb.ids"
