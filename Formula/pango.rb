a48bdcd05f85c0edcaee6b31497e575e0ae5efb3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-20 20:15:13 +0000

    pango: update 1.46.1 bottle.

--- Formula/pango.rb
+++ Formula/pango.rb
@@ -8,9 +8,9 @@ class Pango < Formula
 
   bottle do
     cellar :any
-    sha256 "56e6c57459d6c984905aa61a7d585b6e48c52e787a365bfb52e962337a96e1f0" => :catalina
-    sha256 "ba5b72bea65bbf6b81aa010b7090c644c9a0ecf4e76720829bd1adf510de3d26" => :mojave
-    sha256 "b32c45c7c62c11153070ff91c226430a4b3341fced4d8b1ef4db687366ce4bc4" => :high_sierra
+    sha256 "ddf6c6e74e58909336517fdad64ff0f32212627b1fae8d9bb53439953d29279a" => :catalina
+    sha256 "cbf05eae84754034359bfed76853667eaf1b53bfaa92f2da67c416299e17e6c7" => :mojave
+    sha256 "de0f7b7a6c735a240feb38c00c66afc99316e1d82432b677f4de3fef076cbb89" => :high_sierra
   end
 
   depends_on "gobject-introspection" => :build
