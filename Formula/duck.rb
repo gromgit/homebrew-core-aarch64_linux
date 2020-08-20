206e70ca3edd8352ac67505c0c29afd4932d8cb3
Jenkins CI <noreply@cyberduck.io>
2020-08-20 11:36:08 +0200

    duck 7.5.2
    Closes #59916.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/duck.rb
+++ Formula/duck.rb
@@ -2,9 +2,9 @@ class Duck < Formula
   desc "Command-line interface for Cyberduck (a multi-protocol file transfer tool)"
   homepage "https://duck.sh/"
   # check the changelog for the latest stable version: https://cyberduck.io/changelog/
-  url "https://dist.duck.sh/duck-src-7.4.1.33065.tar.gz"
-  sha256 "b535188cf37946c350f090aa493d5240ee1ef2b077d673613cd5655c337ef912"
-  license "GPL-3.0"
+  url "https://dist.duck.sh/duck-src-7.5.2.33336.tar.gz"
+  sha256 "f084aca8b58069ccfeb62752f47daeff80f54991478189b274c06c7cfe0fa338"
+  license "GPL-3.0-only"
   head "https://svn.cyberduck.io/trunk/"
 
   bottle do
