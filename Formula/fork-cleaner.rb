2d6abe0722336a3cdeae70a3b4f0f5cecd47bf20
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-20 19:12:45 +0000

    fork-cleaner: update 1.8.0 bottle.

--- Formula/fork-cleaner.rb
+++ Formula/fork-cleaner.rb
@@ -7,9 +7,9 @@ class ForkCleaner < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3764b433f450ce49be8d8e002c5ec457fd75850a300b9e0868ced294b78fca42" => :catalina
-    sha256 "6f1cd16063670084e17628102f5dced6d0d4fb15d2a8ae80c00e531a35377135" => :mojave
-    sha256 "51d4939c1f7139ecf9eee2783ca15a5e40142fbeee8f85b426c96e9fc424df03" => :high_sierra
+    sha256 "7a963849e6b00d64efe10a5193d91d596611fa4e8a4bb9ee49c8000dbe8985db" => :catalina
+    sha256 "5d17b463da2d368f7ba3276a81a0edd0f398fea58edfd865bd5f4795b0379deb" => :mojave
+    sha256 "af54b2458a786b5d8ad0ebf398b4b5e1d0b335dd496e0d5157e10d17f62dc86a" => :high_sierra
   end
 
   depends_on "go" => :build
