bda8cc323324869a200c390721e978343e02dbe0
Alexander Bayandin <a.bayandin@gmail.com>
2020-08-20 19:32:53 +0000

    apache-arrow-glib: update 1.0.0_1 bottle.

--- Formula/apache-arrow-glib.rb
+++ Formula/apache-arrow-glib.rb
@@ -9,9 +9,9 @@ class ApacheArrowGlib < Formula
   head "https://github.com/apache/arrow.git"
 
   bottle do
-    sha256 "91b2ef2ce4bd4eea4e424fb5f0762ecac997d5152a9b1c9ae5ab76b0e743d152" => :catalina
-    sha256 "9f2299d887246872c3bee18ceccfd623f575a569975d0cd30f527964641f4a53" => :mojave
-    sha256 "40ff7f66e31c0f34c24cf05664d2f85888c66006e5cc03fd1bc239a437ca68fe" => :high_sierra
+    sha256 "9b665624077b7eab70266b768c2e1a16039e85580d994045a6a87b5582fa126f" => :catalina
+    sha256 "a2701485e70e016fd2560d8736cf3724ffc367c4345832e1fbd61103afdaf001" => :mojave
+    sha256 "139ec7427d44de82a538125f9d686f689f2d01a4b5afeecb785e80888c678141" => :high_sierra
   end
 
   depends_on "gobject-introspection" => :build
