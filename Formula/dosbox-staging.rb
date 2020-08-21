24af29899ac2a3bf9c903afede7c70d1334c6342
Rui Chen <rui@meetup.com>
2020-08-21 14:16:12 -0400

    dosbox-staging 0.75.1

--- Formula/dosbox-staging.rb
+++ Formula/dosbox-staging.rb
@@ -1,8 +1,8 @@
 class DosboxStaging < Formula
   desc "Modernized DOSBox soft-fork"
   homepage "https://dosbox-staging.github.io/"
-  url "https://github.com/dosbox-staging/dosbox-staging/archive/v0.75.0.tar.gz"
-  sha256 "6279690a05b9cc134484b8c7d11e9c1cb53b50bdb9bf32bdf683bd66770b6658"
+  url "https://github.com/dosbox-staging/dosbox-staging/archive/v0.75.1.tar.gz"
+  sha256 "9155cc7220e612817670fbe51f30c69e560573f1daad075037194f2731a538a8"
   license "GPL-2.0"
   head "https://github.com/dosbox-staging/dosbox-staging.git"
 
