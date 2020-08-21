86eccde9f666e522de77f0eb4d41ea4853647907
Mike McQuaid <mike@mikemcquaid.com>
2020-08-21 09:48:10 +0100

    sip: use new license syntax.

--- Formula/sip.rb
+++ Formula/sip.rb
@@ -3,7 +3,7 @@ class Sip < Formula
   homepage "https://www.riverbankcomputing.com/software/sip/intro"
   url "https://www.riverbankcomputing.com/static/Downloads/sip/4.19.24/sip-4.19.24.tar.gz"
   sha256 "edcd3790bb01938191eef0f6117de0bf56d1136626c0ddb678f3a558d62e41e5"
-  license ["GPL-2.0-only", "GPL-3.0-only"]
+  license any_of: ["GPL-2.0-only", "GPL-3.0-only"]
   head "https://www.riverbankcomputing.com/hg/sip", using: :hg
 
   bottle do
