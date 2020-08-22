44ddad6f0aa41a92089e27c2725ca2fc3db36967
Caleb Xu <calebcenter@live.com>
2020-08-22 00:51:53 -0400

    parallel-hashmap 1.32 (new formula)
    Closes #60001.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

---
+++ Formula/parallel-hashmap.rb
@@ -0,0 +1,42 @@
+class ParallelHashmap < Formula
+  desc "Family of header-only, fast, memory-friendly C++ hashmap and btree containers"
+  homepage "https://greg7mdp.github.io/parallel-hashmap/"
+  url "https://github.com/greg7mdp/parallel-hashmap/archive/1.32.tar.gz"
+  sha256 "50cc7abc08f78c6396a33a334e5bc0b3ade121af8604690dae13a1bad47cf07c"
+  license "Apache-2.0"
+  head "https://github.com/greg7mdp/parallel-hashmap.git"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <iostream>
+      #include <string>
+      #include <parallel_hashmap/phmap.h>
+
+      using phmap::flat_hash_map;
+
+      int main() {
+          flat_hash_map<std::string, std::string> examples =
+          {
+              {"foo", "a"},
+              {"bar", "b"}
+          };
+
+          for (const auto& n : examples)
+              std::cout << n.first << ":" << n.second << std::endl;
+
+          examples["baz"] = "c";
+          std::cout << "baz:" << examples["baz"] << std::endl;
+          return 0;
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test", "-I#{include}"
+    assert_equal "foo:a\nbar:b\nbaz:c\n", shell_output("./test")
+  end
+end
