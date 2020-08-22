dc981f4dd2685c9503ef0b1df1b77e650d1536be
Rui Chen <rui@meetup.com>
2020-08-22 17:10:08 -0400

    darcs 2.16.2

--- Formula/darcs.rb
+++ Formula/darcs.rb
@@ -1,8 +1,8 @@
 class Darcs < Formula
   desc "Distributed version control system that tracks changes, via Haskell"
   homepage "http://darcs.net/"
-  url "https://hackage.haskell.org/package/darcs-2.14.5/darcs-2.14.5.tar.gz"
-  sha256 "2f4e82f9e0a4748a63ef26753f04d960cc4afe4a0549fb9129b05c938b382dd2"
+  url "https://hackage.haskell.org/package/darcs-2.16.2/darcs-2.16.2.tar.gz"
+  sha256 "161752851d910fb8d2168cb49b3b9bbe0571e0b622cd1d36d1ea174aa25255db"
   license "GPL-2.0"
 
   bottle do
