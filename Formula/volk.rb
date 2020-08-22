94a7675a34b9e076aa1bfaf208645e88c18228e6
Jonathan Chang <jchang641@gmail.com>
2020-08-22 15:21:24 +1000

    volk 2.3.0 (new formula)

---
+++ Formula/volk.rb
@@ -0,0 +1,49 @@
+class Volk < Formula
+  include Language::Python::Virtualenv
+
+  desc "Vector Optimized Library of Kernels"
+  homepage "https://libvolk.org"
+  url "https://github.com/gnuradio/volk/releases/download/v2.3.0/volk-2.3.0.tar.xz"
+  sha256 "40645886d713ed23295d7fb3e69734b5173a22259886b1a4abdad229a44123b9"
+  license "GPL-3.0-or-later"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "boost"
+  depends_on "orc"
+  depends_on "python@3.8"
+
+  resource "Mako" do
+    url "https://files.pythonhosted.org/packages/72/89/402d2b4589e120ca76a6aed8fee906a0f5ae204b50e455edd36eda6e778d/Mako-1.1.3.tar.gz"
+    sha256 "8195c8c1400ceb53496064314c6736719c6f25e7479cd24c77be3d9361cddc27"
+  end
+
+  def install
+    # Set up Mako
+    venv_root = libexec/"venv"
+    xy = Language::Python.major_minor_version "python3"
+    ENV.prepend_create_path "PYTHONPATH", "#{venv_root}/lib/python#{xy}/site-packages"
+    venv = virtualenv_create(venv_root, "python3")
+    venv.pip_install resource("Mako")
+
+    # Avoid references to the Homebrew shims directory
+    inreplace "lib/CMakeLists.txt" do |s|
+      s.gsub! "${CMAKE_C_COMPILER}", ENV.cc
+      s.gsub! "${CMAKE_CXX_COMPILER}", ENV.cxx
+    end
+
+    args = %W[-DPYTHON_EXECUTABLE=#{venv_root}/bin/python -DENABLE_TESTING=OFF]
+    system "cmake", ".", *std_cmake_args, *args
+    system "make", "install"
+
+    # Set up volk_modtool paths
+    site_packages = lib/"python#{xy}/site-packages"
+    pth_contents = "import site; site.addsitedir('#{site_packages}')\n"
+    (venv_root/"lib/python#{xy}/site-packages/homebrew-volk.pth").write pth_contents
+  end
+
+  test do
+    system "volk_modtool", "--help"
+    system "volk_profile", "--iter", "10"
+  end
+end
