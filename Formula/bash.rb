85ba74b88f2374708abd6ddc737c9105850560c9
neeldug <5161147+neeldug@users.noreply.github.com>
2020-08-22 03:23:53 +0100

    bash: update license (#59984)
    Change license to GPL-3.0-or-later

--- Formula/bash.rb
+++ Formula/bash.rb
@@ -1,7 +1,7 @@
 class Bash < Formula
   desc "Bourne-Again SHell, a UNIX command interpreter"
   homepage "https://www.gnu.org/software/bash/"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
   head "https://git.savannah.gnu.org/git/bash.git"
 
   stable do
