37b5298276b191fe5d902bac759e6f4b21e0d847
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-23 20:15:12 +0000

    subfinder: update 2.4.2 bottle.

--- Formula/subfinder.rb
+++ Formula/subfinder.rb
@@ -8,9 +8,9 @@ class Subfinder < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5be0e1ba5341b28e664ec92fd76a072a9c1cdc0cda604d7a09b9d1539ee21bcb" => :catalina
-    sha256 "fb5b981f50794b69e0d30c5dc479ddbe431f08c2a0e4f2381b06159b6b490d60" => :mojave
-    sha256 "47ae3e2e7f2645f16c963a43ec1bf57f3bf9afa25ebffb11fe6b85f540d52a72" => :high_sierra
+    sha256 "24783d440b8af36b7cae85d60ea382fe9455f5730b2859a766b22d8e093d9c33" => :catalina
+    sha256 "d0cea688610125a9f8c9c119ce1c22b2be0cbefc0a03f8160abf64fc4a556409" => :mojave
+    sha256 "6e7d36487ff9e2bdfa84cf033abd48f45eae34e51c78ef4a8825d0944d02e9c0" => :high_sierra
   end
 
   depends_on "go" => :build
