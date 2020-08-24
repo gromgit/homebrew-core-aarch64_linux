df6f72154975c461e89e2662493550ff67152162
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-24 19:13:08 +0000

    squid: update 4.13 bottle.

--- Formula/squid.rb
+++ Formula/squid.rb
@@ -6,9 +6,9 @@ class Squid < Formula
   license "GPL-2.0"
 
   bottle do
-    sha256 "9910eea9598125a80e3282a09781dda3bb73b5edc9b3a155e2e41361b672c9b3" => :catalina
-    sha256 "afb188d816e0ca55a082d5dfc79cb48c0dbd879be1835bf8f774e4949cd71817" => :mojave
-    sha256 "65900a2c2ba2f9fcef8b294ec2dc26c76157e972796458fffd20b07c69e64150" => :high_sierra
+    sha256 "9993d7d8c41a778163e0bdcd13a882125269784fce9969b3f6a4c723daa8b750" => :catalina
+    sha256 "cab7176b8938750b08ec079ff5bd2b37408dace0a1823f61fc2e913d08afe377" => :mojave
+    sha256 "5fc9145a26ff1555e94e5c0e124cdacd5e9b06747240da161ddb1782731b5f71" => :high_sierra
   end
 
   head do
