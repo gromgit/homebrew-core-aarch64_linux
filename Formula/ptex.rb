b48b286b65f4635a189b88ed2e0db1a2d50029bc
Viktor Szakats <commit@vsz.me>
2020-08-24 21:59:05 +0000

    ptex: secure homepage

--- Formula/ptex.rb
+++ Formula/ptex.rb
@@ -1,6 +1,6 @@
 class Ptex < Formula
   desc "Texture mapping system"
-  homepage "http://ptex.us"
+  homepage "https://ptex.us/"
   url "https://github.com/wdas/ptex.git",
       tag:      "v2.3.2",
       revision: "1b8bc985a71143317ae9e4969fa08e164da7c2e5"
