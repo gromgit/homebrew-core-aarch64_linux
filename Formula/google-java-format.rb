2b1f5dd0058eb9f2a89f0c748f654d5d0cb52e06
Rui Chen <rui@meetup.com>
2020-08-24 14:50:02 -0400

    google-java-format 1.9
    depends_on openjdk
    
    Closes #60122.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/google-java-format.rb
+++ Formula/google-java-format.rb
@@ -1,25 +1,17 @@
 class GoogleJavaFormat < Formula
   desc "Reformats Java source code to comply with Google Java Style"
   homepage "https://github.com/google/google-java-format"
-  url "https://github.com/google/google-java-format/archive/google-java-format-1.8.tar.gz"
-  sha256 "7ae8449441a15fc76e58d0cd69628a5e5135f01a5dea5184738c9b5ba57b525e"
+  url "https://github.com/google/google-java-format/releases/download/google-java-format-1.9/google-java-format-1.9-all-deps.jar"
+  sha256 "1d98720a5984de85a822aa32a378eeacd4d17480d31cba6e730caae313466b97"
   license "Apache-2.0"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "40458975314ee330d5563f305c08538a3774b3f816d43075e9509fe16dc121a5" => :catalina
-    sha256 "264c72a1955375d7aecb428be58dd60c1710a1d9ef3ba242bc9a17bd185547c9" => :mojave
-    sha256 "83fc6d1ca07eb27c3c9124020aa6e366815585c1e6e2d0daf90076a11844e179" => :high_sierra
-  end
+  bottle :unneeded
 
-  depends_on "maven" => :build
+  depends_on "openjdk"
 
   def install
-    system "mvn", "versions:set", "-DnewVersion=#{version}"
-    system "mvn", "install", "-DskipTests=true", "-Dmaven.javadoc.skip=true", "-B"
-    libexec.install "core/target/google-java-format-#{version}-all-deps.jar"
-
-    bin.write_jar_script libexec/"google-java-format-#{version}-all-deps.jar", "google-java-format"
+    libexec.install "google-java-format-#{version}-all-deps.jar" => "google-java-format.jar"
+    bin.write_jar_script libexec / "google-java-format.jar", "google-java-format"
   end
 
   test do
