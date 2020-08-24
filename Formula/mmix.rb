cc4bef5e29d7fe6e093bda7608014506dfa458c5
Viktor Szakats <commit@vsz.me>
2020-08-24 21:59:05 +0000

    mmix: secure urls

--- Formula/mmix.rb
+++ Formula/mmix.rb
@@ -1,7 +1,7 @@
 class Mmix < Formula
   desc "64-bit RISC architecture designed by Donald Knuth"
-  homepage "http://mmix.cs.hm.edu/"
-  url "http://mmix.cs.hm.edu/src/mmix-20160804.tgz"
+  homepage "https://mmix.cs.hm.edu/"
+  url "https://mmix.cs.hm.edu/src/mmix-20160804.tgz"
   sha256 "fad8e64fddf2d75cbcd5080616b47e11a2d292a428cdb0c12e579be680ecdee9"
 
   bottle do
