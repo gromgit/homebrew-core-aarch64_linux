9f6e4e202b1a33865f746789957d9bbd73f66c51
Caleb Xu <calebcenter@live.com>
2020-08-24 09:16:09 -0400

    spotbugs: use Language::Java.overridable_java_home_env

--- Formula/spotbugs.rb
+++ Formula/spotbugs.rb
@@ -3,7 +3,7 @@ class Spotbugs < Formula
   homepage "https://spotbugs.github.io/"
   url "https://repo.maven.apache.org/maven2/com/github/spotbugs/spotbugs/4.1.2/spotbugs-4.1.2.tgz"
   sha256 "575dd1aa4b25b0a5f9766c35ee6ca1d40a3bec8ad02f9f7bb3037f467c60c76f"
-  license "LGPL-2.1"
+  license "LGPL-2.1-or-later"
 
   head do
     url "https://github.com/spotbugs/spotbugs.git"
@@ -25,8 +25,7 @@ class Spotbugs < Formula
       libexec.install Dir["*"]
       chmod 0755, "#{libexec}/bin/spotbugs"
     end
-    (bin/"spotbugs").write_env_script "#{libexec}/bin/spotbugs",
-      JAVA_HOME: "${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
+    (bin/"spotbugs").write_env_script "#{libexec}/bin/spotbugs", Language::Java.overridable_java_home_env
   end
 
   test do
