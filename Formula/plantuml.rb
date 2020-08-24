388c981211fb140ec65af94d43d8b88a7be2027b
chenrui <rui@meetup.com>
2020-08-24 00:41:32 -0400

    plantuml: add license
    Closes #60085.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -3,6 +3,7 @@ class Plantuml < Formula
   homepage "https://plantuml.com/"
   url "https://downloads.sourceforge.net/project/plantuml/1.2020.16/plantuml.1.2020.16.jar"
   sha256 "f772e3d1a5410cc56374b7ec931e5a337c0475babccc443362532cb05ea42d4d"
+  license "GPL-3.0-or-later"
   version_scheme 1
 
   bottle :unneeded
