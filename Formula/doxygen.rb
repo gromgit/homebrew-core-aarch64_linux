8163ed27e66b9996d956b3d609bb1d5ec2f18168
Daniel Vollmer <code@maven.de>
2020-08-25 10:36:36 +0200

    Update license to GPL-2.0-only
    Closes #60145.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/doxygen.rb
+++ Formula/doxygen.rb
@@ -4,7 +4,7 @@ class Doxygen < Formula
   url "https://doxygen.nl/files/doxygen-1.8.20.src.tar.gz"
   mirror "https://downloads.sourceforge.net/project/doxygen/rel-1.8.20/doxygen-1.8.20.src.tar.gz"
   sha256 "e0db6979286fd7ccd3a99af9f97397f2bae50532e4ecb312aa18862f8401ddec"
-  license "GPL-2.0"
+  license "GPL-2.0-only"
   head "https://github.com/doxygen/doxygen.git"
 
   bottle do
