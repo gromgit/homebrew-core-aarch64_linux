fcd3fa2790ec24390826bcc98354b8c34f9c7231
Miguel Mota <miguelmota2@gmail.com>
2020-08-24 21:51:03 -0700

    cointop 1.5.4
    Closes #60140.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cointop.rb
+++ Formula/cointop.rb
@@ -1,8 +1,8 @@
 class Cointop < Formula
   desc "Interactive terminal based UI application for tracking cryptocurrencies"
   homepage "https://cointop.sh"
-  url "https://github.com/miguelmota/cointop/archive/v1.5.3.tar.gz"
-  sha256 "da7c975a19b71cb0c62afd69565ce98eddbb54d3b875e277e0fefe32456b106e"
+  url "https://github.com/miguelmota/cointop/archive/v1.5.4.tar.gz"
+  sha256 "9d6d11e71c3c754a654b191a6813a41427c717966999ce335f0f155358a2292b"
   license "Apache-2.0"
 
   bottle do
