953d419537b0b337f2ffaa5a612f545bdb1fd377
chenrui <chenrui333@gmail.com>
2020-08-26 01:03:54 +0000

    cdktf: add 0.0.16 bottle.

--- Formula/cdktf.rb
+++ Formula/cdktf.rb
@@ -7,6 +7,13 @@ class Cdktf < Formula
   sha256 "3b6eff02a32783ef6bccda828a04fa9996e8ac0164ab423260c3bc6e50abcb2f"
   license "MPL-2.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d5c7287cff8afdf54bdf4726611f5525c67a4b763f46f9bbc2a5865be7ccdf0c" => :catalina
+    sha256 "dad70dab191cbf12a6e7bfd22b6e7b835675810131b98d10397279f863ae7ff7" => :mojave
+    sha256 "f4542d9fc8ccd4731ff65a71a496be739f4102578c9a13b68925bd065ef6d070" => :high_sierra
+  end
+
   depends_on "node"
   depends_on "terraform"
 
