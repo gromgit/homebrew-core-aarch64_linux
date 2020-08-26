d65d0a1485ba4fd2f21ded0490397d978cc8551b
chenrui <chenrui333@gmail.com>
2020-08-25 23:13:20 -0400

    liquibase 4.0.0 (#60187)

--- Formula/liquibase.rb
+++ Formula/liquibase.rb
@@ -1,8 +1,8 @@
 class Liquibase < Formula
   desc "Library for database change tracking"
   homepage "https://www.liquibase.org/"
-  url "https://github.com/liquibase/liquibase/releases/download/v3.10.1/liquibase-3.10.1.tar.gz"
-  sha256 "e54dc5d639fa404ebcc72de647c2009748fa0685566a46abaed9c2a9b88e40e0"
+  url "https://github.com/liquibase/liquibase/releases/download/v4.0.0/liquibase-4.0.0.tar.gz"
+  sha256 "b51e852d81f19ed2146d8bdf55d755616772ce0defef66074de4f0b33dde971b"
   license "Apache-2.0"
 
   bottle :unneeded
