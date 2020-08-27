5368d7a514dc682988f9d6150eb737e2985ab45c
Dustin Rodrigues <dust.rod@gmail.com>
2020-08-27 08:30:02 -0400

    grep: update license (#60249)

--- Formula/grep.rb
+++ Formula/grep.rb
@@ -4,7 +4,7 @@ class Grep < Formula
   url "https://ftp.gnu.org/gnu/grep/grep-3.4.tar.xz"
   mirror "https://ftpmirror.gnu.org/grep/grep-3.4.tar.xz"
   sha256 "58e6751c41a7c25bfc6e9363a41786cff3ba5709cf11d5ad903cf7cce31cc3fb"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
 
   bottle do
     cellar :any
