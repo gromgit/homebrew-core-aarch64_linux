5c85fdfe8362a12c1b8bd0bf10a17f571803bb91
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-27 02:14:44 +0000

    nano: update 5.2 bottle.

--- Formula/nano.rb
+++ Formula/nano.rb
@@ -6,9 +6,9 @@ class Nano < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 "7ec2ac2d56019cbc9c41db0e2bb30db192d2c286545b3e6ae6257a6c07e491e7" => :catalina
-    sha256 "e67eb4b70a17baa9430b3811c0a69d6246a652553d83e2c80b37bda154d6cdfb" => :mojave
-    sha256 "6f70b0d956076fff6bc2daff41dfde0b843f1932b289a457303adf7b2362107b" => :high_sierra
+    sha256 "54e6c2c5d89db89e86a198214c88eafa4b8bccb42d9219e71e54be83feaecff2" => :catalina
+    sha256 "f576828dc01dc9d9edacc0d4dffa268b09fec85ada7375e6dc1e905d9dc95e6a" => :mojave
+    sha256 "501092d0cd9e3e27af362473b236076c5ea127b3800c3184c747a5921f962f46" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
