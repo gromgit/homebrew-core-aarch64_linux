4e333d3310c96c444a59e4453efe8100b4de1469
Dawid Dziurla <dawidd0811@gmail.com>
2020-08-27 00:47:53 +0000

    helmfile 0.125.8
    Closes #60215.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/helmfile.rb
+++ Formula/helmfile.rb
@@ -1,8 +1,8 @@
 class Helmfile < Formula
   desc "Deploy Kubernetes Helm Charts"
   homepage "https://github.com/roboll/helmfile"
-  url "https://github.com/roboll/helmfile/archive/v0.125.7.tar.gz"
-  sha256 "86f663742f830955186316979aa9debf203102bb6c0d6c7345c79237eb6edcbe"
+  url "https://github.com/roboll/helmfile/archive/v0.125.8.tar.gz"
+  sha256 "cd0da88b4be3aeee9f2d4f5613ac946a985bf7c627c78f5360b9e3b01686443e"
   license "MIT"
 
   bottle do
