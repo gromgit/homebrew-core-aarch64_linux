46a57f2c068163a779d069c6a63d3e847e08e1c3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-27 16:14:47 +0000

    genact: update 0.10.0 bottle.

--- Formula/genact.rb
+++ Formula/genact.rb
@@ -7,9 +7,9 @@ class Genact < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "814b886307623c9a1eded2f6e774f06d6827dd491b23dc3cc3dd1e839adb48be" => :catalina
-    sha256 "867c8a47b87062e03a14b0e88503333a73050be8b1ef7db41ff0eed124a1d03c" => :mojave
-    sha256 "194eda0bcb43dcbad2214c1f0736c53c7bf5a7e2281ec025c019aae0f9accae9" => :high_sierra
+    sha256 "cda834ea05c4403ebf7555ff32233141eda5d6e1c05cf88c2c601e88d72e27f9" => :catalina
+    sha256 "a8e61aa9d3683aaedd59154030f154a8bfbfcc8fceec399a1b51b03ae37091d3" => :mojave
+    sha256 "d52830ec3b3744074df6193d3c714f9572472969bc610236813441ea257a8de6" => :high_sierra
   end
 
   depends_on "rust" => :build
