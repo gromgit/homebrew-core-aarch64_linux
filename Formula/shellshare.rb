b587d92db2486254b05ed1e19330082e372bb885
Rui Chen <rui@meetup.com>
2020-08-26 23:03:11 -0400

    shellshare 1.1.0
    Closes #60239.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/shellshare.rb
+++ Formula/shellshare.rb
@@ -1,8 +1,8 @@
 class Shellshare < Formula
   desc "Live Terminal Broadcast"
   homepage "https://shellshare.net"
-  url "https://github.com/vitorbaptista/shellshare/archive/v1.0.4.tar.gz"
-  sha256 "55bfb1521f312b9b9b3e7bc10c9ab8e0c198278e6372187556a8d310ee6b602a"
+  url "https://github.com/vitorbaptista/shellshare/archive/v1.1.0.tar.gz"
+  sha256 "0a102c863f60402ab48908563edde38450add0ae02454360fa94065824a61907"
   license "Apache-2.0"
 
   bottle :unneeded
