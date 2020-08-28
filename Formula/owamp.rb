6c34c787409a491773549fc46f93f4a1f50a5d81
Viktor Szakats <commit@vsz.me>
2020-08-28 21:38:29 +0000

    owamp: secure url

--- Formula/owamp.rb
+++ Formula/owamp.rb
@@ -6,7 +6,7 @@ class Owamp < Formula
   license "Apache-2.0"
 
   livecheck do
-    url "http://software.internet2.edu/sources/owamp/"
+    url "https://software.internet2.edu/sources/owamp/"
     regex(/href=.*?owamp[._-]v?(\d+(?:\.\d+)+(?:-\d+)?)\.t/i)
   end
 
