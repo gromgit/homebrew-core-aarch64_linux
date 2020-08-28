458944593b95ea6432b27bb05486cb5a08383d1d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-28 02:15:28 +0000

    newrelic-cli: update 0.13.0 bottle.

--- Formula/newrelic-cli.rb
+++ Formula/newrelic-cli.rb
@@ -8,9 +8,9 @@ class NewrelicCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b9c69ab98f5c912d6452b40731340910b8ffd0312fe29d2cf8477de00360aeae" => :catalina
-    sha256 "6763c28db1f6338be252b6254810bf48185af9a60a0e8f72faaa0c190cb0ccf9" => :mojave
-    sha256 "08ec8c9e2cb7c1b9f42ee6ee838107fd315e83526208bcfbc94fd789a06ca491" => :high_sierra
+    sha256 "9cd1a4c8aae5e17cb24b1b472e5f66d0cc69accd6f2d5eac25b119a2cd5f868f" => :catalina
+    sha256 "ba8ca70e463f23d17ca103f4c445c4eaa0cb5284fda28c018c1cb2fb3a06a328" => :mojave
+    sha256 "310d4ce06193437c11cf0fc01ac3191c2061267dfc162964a34c633daeee5a50" => :high_sierra
   end
 
   depends_on "go" => :build
