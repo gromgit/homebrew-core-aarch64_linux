42e228c63b7901533819726b62ed9c8bfe144c10
Viktor Szakats <commit@vsz.me>
2020-08-28 21:38:28 +0000

    minizip: secure url

--- Formula/minizip.rb
+++ Formula/minizip.rb
@@ -5,7 +5,7 @@ class Minizip < Formula
   sha256 "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1"
 
   livecheck do
-    url "http://zlib.net"
+    url "https://zlib.net/"
     regex(/href=.*?zlib[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
