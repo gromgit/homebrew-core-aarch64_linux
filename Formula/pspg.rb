8da8798dc292a9013721c851f5ca9f72205ceaff
Caleb Xu <calebcenter@live.com>
2020-08-28 03:20:34 -0400

    pspg 3.1.3
    Closes #60312.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pspg.rb
+++ Formula/pspg.rb
@@ -1,8 +1,8 @@
 class Pspg < Formula
   desc "Unix pager optimized for psql"
   homepage "https://github.com/okbob/pspg"
-  url "https://github.com/okbob/pspg/archive/3.1.2.tar.gz"
-  sha256 "f88fe56b08ec00ec4894aa370f90677817af85581154642dd155466e9d64a465"
+  url "https://github.com/okbob/pspg/archive/3.1.3.tar.gz"
+  sha256 "2f4d1660a150c988c3b1d126a24fe8cb0f76471a829806dacb3ed85fe0e45c8d"
   license "BSD-2-Clause"
   head "https://github.com/okbob/pspg.git"
 
