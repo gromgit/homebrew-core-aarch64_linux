57ec8a6ea6550eaa7069adde2b1c19a014157972
Rylan Polster <rslpolster@gmail.com>
2020-08-28 16:43:21 -0400

    semtag: add disable! reason

--- Formula/semtag.rb
+++ Formula/semtag.rb
@@ -6,9 +6,8 @@ class Semtag < Formula
 
   bottle :unneeded
 
-  # Does not have any license
   # https://github.com/pnikosis/semtag/issues/15
-  disable!
+  disable! because: "has no license"
 
   def install
     bin.install "semtag"
