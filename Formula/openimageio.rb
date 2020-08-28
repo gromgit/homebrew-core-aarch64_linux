a2bbc3f8b5c73bdc6e76a36471e546ae8984b8c6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-08-28 10:14:18 +0000

    openimageio: update 2.1.18_1 bottle.

--- Formula/openimageio.rb
+++ Formula/openimageio.rb
@@ -9,9 +9,9 @@ class Openimageio < Formula
   head "https://github.com/OpenImageIO/oiio.git"
 
   bottle do
-    sha256 "d3467bc4fb634acd02576a8f43546ae2c898b85969a091751953b71e85f47d79" => :catalina
-    sha256 "d5ddb5faf23546680135f830ea2f170215cf1929df911a510555566db69fb243" => :mojave
-    sha256 "61aed9ec9cfc49177f67fbee3987630c50ed5eb4559bc22925458fffe84e9702" => :high_sierra
+    sha256 "38683156e032d0dd6314399ebcc1ad4b59135e6b221f39fef74fd027fc040990" => :catalina
+    sha256 "e2b1b3d5ca05a0a8cd87675a046e2b9ff25f3096b8c1fd736a1b5431dd6c1be8" => :mojave
+    sha256 "e709bacc08751c67528daab022bd76ba32fdb20a061e4a8d509bc75997af13f7" => :high_sierra
   end
 
   depends_on "cmake" => :build
