d0efbeb1553a303d410d58434cad6d17cf582c97
Viktor Szakats <commit@vsz.me>
2020-08-28 21:38:29 +0000

    ragel: secure url

--- Formula/ragel.rb
+++ Formula/ragel.rb
@@ -8,7 +8,7 @@ class Ragel < Formula
   # TODO: Return to using `url :homepage` once the SSL certificate verification
   # issue is resolved on the upstream server.
   livecheck do
-    url "http://www.colm.net/open-source/ragel/"
+    url "https://www.colm.net/open-source/ragel/"
     regex(/Stable.*?href=.*?ragel[._-]v?(\d+(?:\.\d+)+)\.t/im)
   end
 
