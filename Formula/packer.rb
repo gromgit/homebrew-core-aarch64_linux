81706655d9e1a458aaaff2f5728d7743b61f56f7
Rui Chen <rui@meetup.com>
2020-08-29 14:54:51 -0400

    packer 1.6.2
    Closes #60364.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/packer.rb
+++ Formula/packer.rb
@@ -2,8 +2,8 @@ class Packer < Formula
   desc "Tool for creating identical machine images for multiple platforms"
   homepage "https://packer.io"
   url "https://github.com/hashicorp/packer.git",
-      tag:      "v1.6.1",
-      revision: "4c32875c3f8daf1366bf64dd0a8da190ad6f2f06"
+      tag:      "v1.6.2",
+      revision: "e3ac7de9655b3de4b0b0e0f7563fcfc17d6f5150"
   license "MPL-2.0"
   head "https://github.com/hashicorp/packer.git"
 
