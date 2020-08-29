44e3ef83db1adcb482f9f8137161649144ffa355
Rylan Polster <rslpolster@gmail.com>
2020-08-28 20:15:18 -0400

    postgresql@9.4: add deprecate! reason

--- Formula/postgresql@9.4.rb
+++ Formula/postgresql@9.4.rb
@@ -14,7 +14,7 @@ class PostgresqlAT94 < Formula
 
   keg_only :versioned_formula
 
-  deprecate! date: "2020-02-13"
+  deprecate! date: "2020-02-13", because: "is a versioned formula"
 
   depends_on "openssl@1.1"
   depends_on "readline"
