556712b1c8af2cdc50eb3e365994f875fdc0c767
chenrui <rui@meetup.com>
2020-08-29 14:07:02 -0400

    checkstyle: update license
    Closes #60344.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/checkstyle.rb
+++ Formula/checkstyle.rb
@@ -3,7 +3,7 @@ class Checkstyle < Formula
   homepage "https://checkstyle.sourceforge.io/"
   url "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.36/checkstyle-8.36-all.jar"
   sha256 "108a045d6aab90636fc6498023ad6dc39face7347ebf56066b7af4a9c0180fb9"
-  license "LGPL-2.1"
+  license "LGPL-2.1-or-later"
 
   bottle :unneeded
 
