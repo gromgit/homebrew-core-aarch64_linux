0fa9d962e7276639d82776bbc38b5228dbef7c24
Rui Chen <rui@meetup.com>
2020-08-30 08:12:30 -0400

    wtf 20200829
    Closes #60382.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wtf.rb
+++ Formula/wtf.rb
@@ -1,8 +1,8 @@
 class Wtf < Formula
   desc "Translate common Internet acronyms"
   homepage "https://sourceforge.net/projects/bsdwtf/"
-  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20200822.tar.gz"
-  sha256 "0d8fb35e44ec28382e97019164c27af7db12fe1823a108ec9bd21944f4260ce0"
+  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20200829.tar.gz"
+  sha256 "7e2c361a1341115314060d992b53f8847c79159930748b77ca05e0a8e67beed5"
   license :public_domain
 
   livecheck do
