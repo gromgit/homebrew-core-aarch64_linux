fd610bc65aca49992e8037a2a65c3cea190886e0
Markus Reiter <me@reitermark.us>
2020-08-31 02:53:47 +0200

    joe: remove name from description

--- Formula/joe.rb
+++ Formula/joe.rb
@@ -1,5 +1,5 @@
 class Joe < Formula
-  desc "Joe's Own Editor (JOE)"
+  desc "Full featured terminal-based screen editor"
   homepage "https://joe-editor.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/joe-editor/JOE%20sources/joe-4.6/joe-4.6.tar.gz"
   sha256 "495a0a61f26404070fe8a719d80406dc7f337623788e445b92a9f6de512ab9de"
