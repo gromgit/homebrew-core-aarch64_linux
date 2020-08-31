4f6800b90f3479720f29881b1d630b1341e76f0a
Rui Chen <rui@meetup.com>
2020-08-31 10:53:13 -0400

    gravity 0.7.9
    Closes #60427.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gravity.rb
+++ Formula/gravity.rb
@@ -1,8 +1,8 @@
 class Gravity < Formula
   desc "The Gravity Programming Language"
   homepage "https://marcobambini.github.io/gravity/"
-  url "https://github.com/marcobambini/gravity/archive/0.7.8.tar.gz"
-  sha256 "d6589e1cf55eb3b46e205342cb8c01d524da66b98b6fabe0785c711ec314969d"
+  url "https://github.com/marcobambini/gravity/archive/0.7.9.tar.gz"
+  sha256 "6fb79176b991420fed4d19dc6e831d4cd8b0b5a591257a32b1eb64fe03530dfa"
   license "MIT"
 
   bottle do
