f12654056034f37465ae7afb21c230cf71b588d0
Rylan Polster <rslpolster@gmail.com>
2020-08-31 15:27:39 -0400

    apollo: change deprecate! reason to symbol

--- Formula/apollo.rb
+++ Formula/apollo.rb
@@ -30,7 +30,7 @@ class Apollo < Formula
   end
 
   # https://github.com/apache/activemq-apollo/commit/049d68bf3f94cdf62ded5426d3cad4ef3e3c56ca
-  deprecate! date: "2019-03-11", because: "is deprecated upstream"
+  deprecate! date: "2019-03-11", because: :deprecated_upstream
 
   def install
     prefix.install_metafiles
