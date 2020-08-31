f694844ca1744d7467f830bc8aafccd21521da48
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-08-31 10:20:50 +0000

    sponge: add 0.63 bottle.

--- Formula/sponge.rb
+++ Formula/sponge.rb
@@ -5,6 +5,13 @@ class Sponge < Formula
   sha256 "4fc86d56a8a276a0cec71cdabda5ccca50c7a44a2a1ccd888476741d1ce6831d"
   license "GPL-2.0-only"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b77ba5818bf53f3e864592820af2277c00b8e371b1ffb478b33edc78f9343993" => :catalina
+    sha256 "0d651d015389d46adb0d91d4dba2fab77ad75a53af1979ce6e7f254e6dd7b941" => :mojave
+    sha256 "bfbb4be2239ab1e181d8e671e940c42d55784facb6ca9734e1d8569ede665c00" => :high_sierra
+  end
+
   def install
     system "make", "sponge"
     bin.install "sponge"
