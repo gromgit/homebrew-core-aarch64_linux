7187164060f47b59939f024205d3010d304e65f5
Rylan Polster <rslpolster@gmail.com>
2020-08-31 15:07:38 -0400

    sonar-completion: change disable! reason to symbol

--- Formula/sonar-completion.rb
+++ Formula/sonar-completion.rb
@@ -9,7 +9,7 @@ class SonarCompletion < Formula
   bottle :unneeded
 
   # https://github.com/a1dutch/sonarqube-bash-completion/issues/1
-  disable! because: "has no license"
+  disable! because: :no_license
 
   def install
     bash_completion.install "etc/bash_completion.d/sonar"
