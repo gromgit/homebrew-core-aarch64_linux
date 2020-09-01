a91ada91e9b47e733df6355f0d7f27150e286b2c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-01 15:15:30 +0000

    gleam: update 0.11.2 bottle.

--- Formula/gleam.rb
+++ Formula/gleam.rb
@@ -7,9 +7,9 @@ class Gleam < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6ee102b22907f603eccf80a6c602c0148a9d126d90700b8fa5b616c653e5db20" => :catalina
-    sha256 "c9601cf7e9c5787188a67c7c3cb2ea6ed83348dc40c74c00e8bcaa23dea6dfd5" => :mojave
-    sha256 "ef7836c4a212db613622287a290026faeb02b184e331974cb59f0aa9282f4176" => :high_sierra
+    sha256 "6341cb125abf33b142c33104b89d6ad680ae7e2fb9cc15e20984a19466d2de6b" => :catalina
+    sha256 "ccfb7ad2cc98c358ce6f2ac4797e3d2cb60683c36420419d57a257d63f5fe907" => :mojave
+    sha256 "f8a8090c533509c5449fe24ee49da9cb4da2f18cda095cd48a487c9861803e42" => :high_sierra
   end
 
   depends_on "rust" => :build
