413cc3a9c88dc02534f8e1796b7b5783c312a727
Dawid Dziurla <dawidd0811@gmail.com>
2020-09-01 20:03:25 +0000

    whistle 2.5.19
    Closes #60503.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/whistle.rb
+++ Formula/whistle.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Whistle < Formula
   desc "HTTP, HTTP2, HTTPS, Websocket debugging proxy"
   homepage "https://github.com/avwo/whistle"
-  url "https://registry.npmjs.org/whistle/-/whistle-2.5.18.tgz"
-  sha256 "8b719b9e64f6d3cfc948cb38b99dfdaacbfc3fedcfa6008ecfec0d2d0d7e0dff"
+  url "https://registry.npmjs.org/whistle/-/whistle-2.5.19.tgz"
+  sha256 "a8e4e92732467347cf365fe9954b2b2afd9dd795ffb356f9ff234e8cf68acd9e"
   license "MIT"
 
   livecheck do
