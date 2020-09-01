028679a5e183eb4544369b00a77e1be837259780
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-01 02:16:19 +0000

    htop: update 3.0.0 bottle.

--- Formula/htop.rb
+++ Formula/htop.rb
@@ -13,10 +13,9 @@ class Htop < Formula
 
   bottle do
     cellar :any
-    sha256 "c06ff60960f64f5c8395f53d7419cbcce2a22ee87f0cb0138352c8a88111d21c" => :catalina
-    sha256 "77aa302765353b4085dcad52356d3264183e06310dda8d5bac64642299ea2902" => :mojave
-    sha256 "0ebfb655b91566ba31f8effc94d642a43305ff95bdc9b30b46fadc132e2ced0c" => :high_sierra
-    sha256 "ed93b86f011de155c5d261b8c9cc9cb81fd0017667bf3ebe26ee090716bcd650" => :sierra
+    sha256 "f53ae5c8d846b7f11ef6431f65b1a056a31d2703c1b2bc691330cd618dea5a3a" => :catalina
+    sha256 "7007e92a3c9b4f818c9caa53ac27b941b3b6079d5eff62f84837d5092d04ea83" => :mojave
+    sha256 "db64171160ccd3abce6be5fad7547aa254fa0b46b6447b37a08a3a31c5e1f66a" => :high_sierra
   end
 
   depends_on "autoconf" => :build
