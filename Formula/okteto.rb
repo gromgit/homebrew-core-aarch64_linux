bd02c1429c0286997388929bebc1aff3f0f40628
Rui Chen <rui@meetup.com>
2020-09-01 10:58:12 -0400

    okteto 1.8.18
    Closes #60485.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/okteto.rb
+++ Formula/okteto.rb
@@ -1,8 +1,8 @@
 class Okteto < Formula
   desc "Build better apps by developing and testing code directly in Kubernetes"
   homepage "https://okteto.com"
-  url "https://github.com/okteto/okteto/archive/1.8.17.tar.gz"
-  sha256 "ef21e6209658096e0ba3946e9977d82e1bdb2ed5519382b4dfea21804e8290a0"
+  url "https://github.com/okteto/okteto/archive/1.8.18.tar.gz"
+  sha256 "a628d6c3fd8cef2b97b148f77bd3e697ac32be269fe4fe507394763e2cbf79a2"
   license "Apache-2.0"
 
   bottle do
