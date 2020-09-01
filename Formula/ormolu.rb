fbe81faf89d8aab32333d7991c0f0324a926b558
Troels Henriksen <athas@sigkill.dk>
2020-09-01 11:17:27 +0200

    ormolu 0.1.2.0 (new formula)
    Closes #60470.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: chenrui <chenrui333@gmail.com>

---
+++ Formula/ormolu.rb
@@ -0,0 +1,54 @@
+class Ormolu < Formula
+  desc "Formatter for Haskell source code"
+  homepage "https://github.com/tweag/ormolu"
+  url "https://github.com/tweag/ormolu/archive/0.1.2.0.tar.gz"
+  sha256 "ef7164d1d32f59e2a3317d82978fb7b66b496dd2c9ed618d224dc9b6ec15123c"
+  license "BSD-3-Clause"
+  head "https://github.com/tweag/ormolu.git"
+
+  depends_on "cabal-install" => :build
+  depends_on "ghc" => :build
+
+  def install
+    system "cabal", "v2-update"
+    system "cabal", "v2-install", *std_cabal_v2_args
+  end
+
+  test do
+    (testpath/"test.hs").write <<~EOS
+      foo =
+        f1
+        p1
+        p2 p3
+
+      foo' =
+        f2 p1
+        p2
+        p3
+
+      foo'' =
+        f3 p1 p2
+        p3
+    EOS
+    expected = <<~EOS
+      foo =
+        f1
+          p1
+          p2
+          p3
+
+      foo' =
+        f2
+          p1
+          p2
+          p3
+
+      foo'' =
+        f3
+          p1
+          p2
+          p3
+    EOS
+    assert_equal expected, shell_output("#{bin}/ormolu test.hs")
+  end
+end
