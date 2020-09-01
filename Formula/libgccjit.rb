99e5d3335b2477a02d32645e797d638b5a7d0d34
Jonathan Chang <jchang641@gmail.com>
2020-09-01 16:51:49 +1000

    libgccjit 10.2.0 (new formula)
    Closes #60338.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: Jonathan Chang <me@jonathanchang.org>

---
+++ Formula/libgccjit.rb
@@ -0,0 +1,146 @@
+class Libgccjit < Formula
+  desc "JIT library for the GNU compiler collection"
+  homepage "https://gcc.gnu.org/"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz"
+  sha256 "b8dd4368bb9c7f0b98188317ee0254dd8cc99d1e3a18d0ff146c855fe16c1d8c"
+  license "GPL-3.0-or-later" => {
+    with: "GCC-exception-3.1",
+  }
+  head "https://gcc.gnu.org/git/gcc.git"
+
+  livecheck do
+    url :stable
+    regex(%r{href=.*?gcc[._-]v?(\d+(?:\.\d+)+)(?:/?["' >]|\.t)}i)
+  end
+
+  # The bottles are built on systems with the CLT installed, and do not work
+  # out of the box on Xcode-only systems due to an incorrect sysroot.
+  pour_bottle? do
+    reason "The bottle needs the Xcode CLT to be installed."
+    satisfy { MacOS::CLT.installed? }
+  end
+
+  depends_on "gcc" => :test
+  depends_on "gmp"
+  depends_on "isl"
+  depends_on "libmpc"
+  depends_on "mpfr"
+
+  uses_from_macos "zlib"
+
+  # GCC bootstraps itself, so it is OK to have an incompatible C++ stdlib
+  cxxstdlib_check :skip
+
+  def install
+    # GCC will suffer build errors if forced to use a particular linker.
+    ENV.delete "LD"
+
+    osmajor = `uname -r`.split(".").first
+    pkgversion = "Homebrew GCC #{pkg_version} #{build.used_options*" "}".strip
+
+    args = %W[
+      --build=x86_64-apple-darwin#{osmajor}
+      --prefix=#{prefix}
+      --libdir=#{lib}/gcc/#{version.major}
+      --disable-nls
+      --enable-checking=release
+      --with-gmp=#{Formula["gmp"].opt_prefix}
+      --with-mpfr=#{Formula["mpfr"].opt_prefix}
+      --with-mpc=#{Formula["libmpc"].opt_prefix}
+      --with-isl=#{Formula["isl"].opt_prefix}
+      --with-system-zlib
+      --with-pkgversion=#{pkgversion}
+      --with-bugurl=https://github.com/Homebrew/homebrew-core/issues
+    ]
+
+    # Xcode 10 dropped 32-bit support
+    args << "--disable-multilib" if DevelopmentTools.clang_build_version >= 1000
+
+    # System headers may not be in /usr/include
+    sdk = MacOS.sdk_path_if_needed
+    if sdk
+      args << "--with-native-system-header-dir=/usr/include"
+      args << "--with-sysroot=#{sdk}"
+    end
+
+    # Avoid reference to sed shim
+    args << "SED=/usr/bin/sed"
+
+    # Use -headerpad_max_install_names in the build,
+    # otherwise updated load commands won't fit in the Mach-O header.
+    # This is needed because `gcc` avoids the superenv shim.
+    make_args = ["BOOT_LDFLAGS=-Wl,-headerpad_max_install_names"]
+
+    # Building jit needs --enable-host-shared, which slows down the compiler.
+    mkdir "build-jit" do
+      system "../configure", *args, "--enable-languages=jit", "--enable-host-shared"
+      system "make", *make_args
+      system "make", "install"
+    end
+
+    # We only install the relevant libgccjit files from libexec and delete the rest.
+    Dir["#{prefix}/**/*"].each do |f|
+      rm_rf f unless File.directory?(f) || File.basename(f).to_s.start_with?("libgccjit")
+    end
+  end
+
+  test do
+    (testpath/"test-libgccjit.c").write <<~EOS
+      #include <libgccjit.h>
+      #include <stdlib.h>
+      #include <stdio.h>
+
+      static void create_code (gcc_jit_context *ctxt) {
+          gcc_jit_type *void_type = gcc_jit_context_get_type (ctxt, GCC_JIT_TYPE_VOID);
+          gcc_jit_type *const_char_ptr_type = gcc_jit_context_get_type (ctxt, GCC_JIT_TYPE_CONST_CHAR_PTR);
+          gcc_jit_param *param_name = gcc_jit_context_new_param (ctxt, NULL, const_char_ptr_type, "name");
+          gcc_jit_function *func = gcc_jit_context_new_function (ctxt, NULL, GCC_JIT_FUNCTION_EXPORTED,
+                  void_type, "greet", 1, &param_name, 0);
+          gcc_jit_param *param_format = gcc_jit_context_new_param (ctxt, NULL, const_char_ptr_type, "format");
+          gcc_jit_function *printf_func = gcc_jit_context_new_function (ctxt, NULL, GCC_JIT_FUNCTION_IMPORTED,
+                  gcc_jit_context_get_type (ctxt, GCC_JIT_TYPE_INT), "printf", 1, &param_format, 1);
+          gcc_jit_rvalue *args[2];
+          args[0] = gcc_jit_context_new_string_literal (ctxt, "hello %s");
+          args[1] = gcc_jit_param_as_rvalue (param_name);
+          gcc_jit_block *block = gcc_jit_function_new_block (func, NULL);
+          gcc_jit_block_add_eval (block, NULL, gcc_jit_context_new_call (ctxt, NULL, printf_func, 2, args));
+          gcc_jit_block_end_with_void_return (block, NULL);
+      }
+
+      int main (int argc, char **argv) {
+          gcc_jit_context *ctxt;
+          gcc_jit_result *result;
+          ctxt = gcc_jit_context_acquire ();
+          if (!ctxt) {
+              fprintf (stderr, "NULL ctxt");
+              exit (1);
+          }
+          gcc_jit_context_set_bool_option (ctxt, GCC_JIT_BOOL_OPTION_DUMP_GENERATED_CODE, 0);
+          create_code (ctxt);
+          result = gcc_jit_context_compile (ctxt);
+          if (!result) {
+              fprintf (stderr, "NULL result");
+              exit (1);
+          }
+          typedef void (*fn_type) (const char *);
+          fn_type greet = (fn_type)gcc_jit_result_get_code (result, "greet");
+          if (!greet) {
+              fprintf (stderr, "NULL greet");
+              exit (1);
+          }
+          greet ("world");
+          fflush (stdout);
+          gcc_jit_context_release (ctxt);
+          gcc_jit_result_release (result);
+          return 0;
+      }
+    EOS
+
+    gcc_major_ver = Formula["gcc"].any_installed_version.major
+    gcc = Formula["gcc"].opt_bin/"gcc-#{gcc_major_ver}"
+    libs = "#{HOMEBREW_PREFIX}/lib/gcc/#{gcc_major_ver}"
+
+    system gcc.to_s, "-I#{include}", "test-libgccjit.c", "-o", "test", "-L#{libs}", "-lgccjit"
+    assert_equal "hello world", shell_output("./test")
+  end
+end
