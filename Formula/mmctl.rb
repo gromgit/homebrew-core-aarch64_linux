6e3b2626c635a7156e057934e9f4f226db070b94
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-01 21:14:25 +0000

    mmctl: update 5.28.0 bottle.

--- Formula/mmctl.rb
+++ Formula/mmctl.rb
@@ -9,9 +9,9 @@ class Mmctl < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c6487e68fdd7c307affcefb22559d490f91d872c42c9603126abbba7abbd64df" => :catalina
-    sha256 "f657c4416f8cebc8f3491f422d64637181b20fab332bf43bb15771fc78e4a77d" => :mojave
-    sha256 "81083c3540155a6f3413b83f24ec46eab80081a9e1f84b9d9243b0419e66217e" => :high_sierra
+    sha256 "5945fc578f0941531179e91d0762450623543aa6cb1802dbf7cee8ab407fcc6b" => :catalina
+    sha256 "3dbd31758106e27c730d792f7146eef5cade53015acfc87870472462a1c055ad" => :mojave
+    sha256 "84374da7458bb215178268a99c268a0901bb603797aa7c86db7b4ce235468ef2" => :high_sierra
   end
 
   depends_on "go" => :build
