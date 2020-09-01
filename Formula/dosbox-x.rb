4fb925940a29c9d9e3055ed0c236f2a7c37c11a6
Rui Chen <rui@meetup.com>
2020-09-01 12:21:11 -0400

    dosbox-x 0.83.5

--- Formula/dosbox-x.rb
+++ Formula/dosbox-x.rb
@@ -1,8 +1,8 @@
 class DosboxX < Formula
   desc "DOSBox with accurate emulation and wide testing"
   homepage "https://dosbox-x.com/"
-  url "https://github.com/joncampbell123/dosbox-x/archive/dosbox-x-v0.83.4.tar.gz"
-  sha256 "0fcd9bf17c0bdb2fc5264b1e92a6731758988179e38c727f0fc64002efe19165"
+  url "https://github.com/joncampbell123/dosbox-x/archive/dosbox-x-v0.83.5.tar.gz"
+  sha256 "debcd2370c260d1e90a3b42dccfa90406b2a51332d3e2dd7b50d92af5287370d"
   license "GPL-2.0"
   version_scheme 1
   head "https://github.com/joncampbell123/dosbox-x.git"
