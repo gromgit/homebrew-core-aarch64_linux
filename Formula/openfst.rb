de384d21e7ecb23fb5b2a9a513b6519c498e1579
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-09-02 07:52:34 +0000

    openfst: add 1.7.9 bottle.

--- Formula/openfst.rb
+++ Formula/openfst.rb
@@ -5,6 +5,13 @@ class Openfst < Formula
   sha256 "9319aeb31d1e2950ae25449884e255cc2bc9dfaf987f601590763e61a10fbdde"
   license "Apache-2.0"
 
+  bottle do
+    cellar :any
+    sha256 "b32fb6cb0eb43a7d8775d8bfc760c49471586eeb33797f3d44a8b53cd45dc792" => :catalina
+    sha256 "7e5a450f383ddfeddcb7ee8d240e7db576fcc32a25c199d6a35eba40fea920d9" => :mojave
+    sha256 "0635e790f390be0a97c78a434e723280339fe0f0d86ee55c4a34339840f160a7" => :high_sierra
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
