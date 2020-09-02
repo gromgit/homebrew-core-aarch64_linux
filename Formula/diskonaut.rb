07632300c81e810b1832498b2c7cad135ef8fb6c
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-09-02 08:01:18 +0000

    diskonaut: add 0.9.0 bottle.

--- Formula/diskonaut.rb
+++ Formula/diskonaut.rb
@@ -5,6 +5,13 @@ class Diskonaut < Formula
   sha256 "93564a195a62796f95eacc154ad10df8f1050c8a0b2e2e22a0612228c930c1f5"
   license "MIT"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1274eb6a11cf81c7f4c0c51ed89e3c04f284db8c253122b34e9787967f199320" => :catalina
+    sha256 "f71266b35c309b22923eae70bd3e0d575da35a70dae1ba598ca3f13e3962ce01" => :mojave
+    sha256 "07872d42646d2e7ac73958d5c8b2da3aeaf9f87553436728ae73a0bf2fb7fbc6" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
