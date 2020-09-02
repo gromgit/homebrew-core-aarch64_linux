b352ac37071d35884451bc8529f4d35d2f02a052
miccal <miccal.matthews@gmail.com>
2020-09-02 19:03:57 +0800

    pdftoipe: revision bump for poppler
    Closes #60513.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pdftoipe.rb
+++ Formula/pdftoipe.rb
@@ -3,8 +3,8 @@ class Pdftoipe < Formula
   homepage "https://github.com/otfried/ipe-tools"
   url "https://github.com/otfried/ipe-tools/archive/v7.2.13.1.tar.gz"
   sha256 "c8725d78e43b7d6a04465e8a156bc4c6a78121f291aac74e0b0a10286ef95544"
-  license "GPL-2.0"
-  revision 7
+  license "GPL-2.0-or-later"
+  revision 8
 
   bottle do
     cellar :any
