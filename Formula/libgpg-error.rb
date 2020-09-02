86aa2e0c6de65a34baeda45d749d683e0c1bd89b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-02 00:37:10 +0000

    libgpg-error: update 1.39 bottle.

--- Formula/libgpg-error.rb
+++ Formula/libgpg-error.rb
@@ -11,9 +11,9 @@ class LibgpgError < Formula
   end
 
   bottle do
-    sha256 "e75e41ea083a1d480fd60a0a45e5ab838ad032f961ea5fb0cf8aafff070e3925" => :catalina
-    sha256 "60867a965e4ea8dc1e119adfecaa2bcfe63300bb7bb3e7af635ec921bc64f599" => :mojave
-    sha256 "6d00f7cb42d8e9b75d6c2ab9fa5b691b7844b14ddc268a67d60d4301f568d6f5" => :high_sierra
+    sha256 "192fb153fd3471c6491a444de03aac3b65479675ffa3d830330106676f309a3f" => :catalina
+    sha256 "1f6be0800675342ed83ab22fc85a3335d14514bbb7d718a8082147fdf6594249" => :mojave
+    sha256 "4a973d0829eea76e5e792afd22411f730c88676c6dda7060e56e111a8fd8351c" => :high_sierra
   end
 
   def install
