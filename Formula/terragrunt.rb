6498c8b698842dee06a76f1914327d4cdfe2e3ed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-02 16:15:40 +0000

    terragrunt: update 0.23.40 bottle.

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -7,9 +7,9 @@ class Terragrunt < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "520dd7b8cda37781ebb360bf1d4e5d2f4d821a6cd5ce7aabca372a838473d69e" => :catalina
-    sha256 "8cb7d2ffd5b10620a4450c0690ff8bdabe0e9db4112ec47d5375b6752bd36008" => :mojave
-    sha256 "191bd23da2b461c36d1e976ed03b24a1c46495f14451b8f427b3c6364a927914" => :high_sierra
+    sha256 "bd794fa1a52c47d59dcbb641355f854f103a5ac1bd237bf8e00968e10bb4dbd4" => :catalina
+    sha256 "6629a3152b6cb712a8fd6d4d99401f8aff1173e7a93e26383d91ec21e9bead53" => :mojave
+    sha256 "9f065d6f65666df5f1d6d67fb5d91e0751452fa087aede6d7b2f59f8864c4c4a" => :high_sierra
   end
 
   depends_on "go" => :build
