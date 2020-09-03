2a18906d407bfc11f0e1696d7909812c8ddca13c
Caleb Xu <calebcenter@live.com>
2020-09-03 00:19:12 -0400

    easyengine 4.1.4 (#60386)
    remove version test

--- Formula/easyengine.rb
+++ Formula/easyengine.rb
@@ -1,8 +1,8 @@
 class Easyengine < Formula
   desc "Command-line control panel to manage WordPress sites"
   homepage "https://easyengine.io/"
-  url "https://github.com/EasyEngine/easyengine/releases/download/v4.1.3/easyengine.phar"
-  sha256 "d6e6509ef61ee1a30b2856cf9d775a195dd5cc2670a9ebd85846ea29f6a36bf0"
+  url "https://github.com/EasyEngine/easyengine/releases/download/v4.1.4/easyengine.phar"
+  sha256 "c544d866e95ab227ee4f47f7f2eebcfb368e2f319ebab9d54aee7ca90303b539"
   license "MIT"
 
   bottle :unneeded
@@ -18,9 +18,6 @@ class Easyengine < Formula
     system bin/"ee config set locale hi_IN"
     output = shell_output("#{bin}/ee config get locale")
     assert_match "hi_IN", output
-
-    output = shell_output("#{bin}/ee cli version")
-    assert_match "EE #{version}", output
 
     output = shell_output("#{bin}/ee cli info")
     assert_match "Darwin", output
