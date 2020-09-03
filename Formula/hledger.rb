e888767b36f265621ee694c585b61e0a28b9bda8
Rui Chen <rui@meetup.com>
2020-09-02 21:56:31 -0400

    hledger 1.19
    update resolver
    
    Closes #60579.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hledger.rb
+++ Formula/hledger.rb
@@ -1,8 +1,8 @@
 class Hledger < Formula
   desc "Easy plain text accounting with command-line, terminal and web UIs"
   homepage "https://hledger.org/"
-  url "https://hackage.haskell.org/package/hledger-1.18.1/hledger-1.18.1.tar.gz"
-  sha256 "0c88c9a1896a6c431854c76229f0fe8d9bc59b6560829a4af1b8fcbad85486fa"
+  url "https://hackage.haskell.org/package/hledger-1.19/hledger-1.19.tar.gz"
+  sha256 "a1ab3b4035e37f7f55988f6b64f51543f5562d4767fa3f43ec021e46ef6d7b4d"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -23,16 +23,16 @@ class Hledger < Formula
   uses_from_macos "zlib"
 
   resource "hledger-lib" do
-    url "https://hackage.haskell.org/package/hledger-lib-1.18.1/hledger-lib-1.18.1.tar.gz"
-    sha256 "44c265b655a8ad37111821d58486bdbe7e8f0e285d3ecb94b46d132b0219cd99"
+    url "https://hackage.haskell.org/package/hledger-lib-1.19/hledger-lib-1.19.tar.gz"
+    sha256 "41e660efe94b23aacd06552eaea0231fe16c25200c1c2d350b971813fb2f4f2b"
   end
   resource "hledger-ui" do
-    url "https://hackage.haskell.org/package/hledger-ui-1.18.1/hledger-ui-1.18.1.tar.gz"
-    sha256 "c4f487ea97ac83ac5f2efd772f24e70b47339ba6ebdb3fbfb4f693e046faee3d"
+    url "https://hackage.haskell.org/package/hledger-ui-1.19/hledger-ui-1.19.tar.gz"
+    sha256 "5a4b35a6fdf44d0e4b0e233fb02ccbd8c0c699c52d9c83c65b6c9885c43c26f6"
   end
   resource "hledger-web" do
-    url "https://hackage.haskell.org/package/hledger-web-1.18.1/hledger-web-1.18.1.tar.gz"
-    sha256 "3e2ca18e5a5d0c76c19208ede84b3931844254382c9f9a48a2fd1c8da3ef20e8"
+    url "https://hackage.haskell.org/package/hledger-web-1.19/hledger-web-1.19.tar.gz"
+    sha256 "1e15ff4efa853a1f677f4ccf8fd623b9711d39b51c9ffcb97b4a553dc463aada"
   end
 
   def install
@@ -41,10 +41,10 @@ class Hledger < Formula
     (buildpath/"../hledger-web").install resource("hledger-web")
     cd ".." do
       system "stack", "update"
-      system "stack", "init", "--resolver=lts-15.16"
+      system "stack", "init", "--resolver=lts-16.12"
       system "stack", "install", "--system-ghc", "--no-install-ghc", "--local-bin-path=#{bin}"
 
-      man1.install "hledger-1.18.1/hledger.1"
+      man1.install "hledger-1.19/hledger.1"
       man1.install "hledger-ui/hledger-ui.1"
       man1.install "hledger-web/hledger-web.1"
       man5.install "hledger-lib/hledger_csv.5"
@@ -52,7 +52,7 @@ class Hledger < Formula
       man5.install "hledger-lib/hledger_timeclock.5"
       man5.install "hledger-lib/hledger_timedot.5"
 
-      info.install "hledger-1.18.1/hledger.info"
+      info.install "hledger-1.19/hledger.info"
       info.install "hledger-lib/hledger_csv.info"
       info.install "hledger-lib/hledger_journal.info"
       info.install "hledger-lib/hledger_timeclock.info"
