6e41e875786c8492c552660f1b6aed4971d592fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-03 00:38:17 +0000

    lego: update 4.0.0 bottle.

--- Formula/lego.rb
+++ Formula/lego.rb
@@ -8,9 +8,9 @@ class Lego < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "07a313017b535766bc6be9911528c70acba3175d4fab125dec63b6442486cd3d" => :catalina
-    sha256 "e0f7035f3948882f3ec7f94721baf408f93cb25e96728a166f83b87ef02c75ca" => :mojave
-    sha256 "04f0d2bece9478bb406d27848555903069148c72a86f1185497e3a683ac79bb8" => :high_sierra
+    sha256 "8752def187b7dadb167a45282f6dce99e716bba5116ce795f52d1a442db7e07b" => :catalina
+    sha256 "0731aacff85f51f12d5626470430a3f75b1051b2c1dc1c955c60c17328310a88" => :mojave
+    sha256 "18d1f2c2fcf2d71d0f1b9a0f5364e695145c42c7617e9bb365b1fb405979a974" => :high_sierra
   end
 
   depends_on "go" => :build
