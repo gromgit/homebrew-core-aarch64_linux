02b450ad7074ee0bc1bdbfbad69fdf70994ac36f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-03 06:17:37 +0000

    kore: update 4.0.0 bottle.

--- Formula/kore.rb
+++ Formula/kore.rb
@@ -7,10 +7,9 @@ class Kore < Formula
   head "https://github.com/jorisvink/kore.git"
 
   bottle do
-    sha256 "abbcf36f378af2db9914b9f6edf1a4e4808723a9d0421056bd6de645f5393dd2" => :catalina
-    sha256 "760a9977cc46ad5f46e5d9b3397af22e0ff1df60e33acc622851d7a8babb2234" => :mojave
-    sha256 "0c76cfc29a1c006207b09015550a0f33b5c54a1aaff542c1e8843a6531c083c8" => :high_sierra
-    sha256 "719bd8b11d2757fec07aa6558c95c500f2a7b6148a0ebbf2563d1012dc4eece2" => :sierra
+    sha256 "aa62a711583a594d57933cbd958b0d4485b5ca4f971804e53e468c8ec09e5109" => :catalina
+    sha256 "ee74e5ce481203e66ba7f9227b3f59a2f7bf33814a80d584e9213175e753240b" => :mojave
+    sha256 "2e9bd366c8be331cf17b1819abc697d7f1fd8b88ec17bf80ad07d5999313b545" => :high_sierra
   end
 
   depends_on macos: :sierra # needs clock_gettime
