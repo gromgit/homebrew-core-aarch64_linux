5dc83be5fbbc4170570abab99398509b0f8b72c1
Sean Molenaar <smillerdev@me.com>
2020-09-04 11:31:04 +0200

    unixodbc 2.3.8

--- Formula/unixodbc.rb
+++ Formula/unixodbc.rb
@@ -1,8 +1,8 @@
 class Unixodbc < Formula
   desc "ODBC 3 connectivity for UNIX"
   homepage "http://www.unixodbc.org/"
-  url "http://www.unixodbc.org/unixODBC-2.3.7.tar.gz"
-  sha256 "45f169ba1f454a72b8fcbb82abd832630a3bf93baa84731cf2949f449e1e3e77"
+  url "http://www.unixodbc.org/unixODBC-2.3.8.tar.gz"
+  sha256 "85372b9cd2cdfea3983c3958ab11ca1513ea091a263d82105e5da043379e48a5"
   license "LGPL-2.1"
 
   livecheck do
