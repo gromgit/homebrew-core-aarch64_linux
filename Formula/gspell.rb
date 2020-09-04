7e2102fdbc32ff86a34348fb7e76f1cc447631c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-04 18:15:19 +0000

    gspell: update 1.8.4 bottle.

--- Formula/gspell.rb
+++ Formula/gspell.rb
@@ -10,9 +10,9 @@ class Gspell < Formula
   end
 
   bottle do
-    sha256 "491b662bd382e322b2041a255551dcd1a3ba6dbaa5a2fc825dc239241ac7a64a" => :catalina
-    sha256 "d471894ee5c85f31be7a2a9c5a122382a3619852f4b9a36f1d2778e669ad4cb0" => :mojave
-    sha256 "3b167053c54d962f2eed54e7bff70b2ce4b0a21ccb22be19a1091318e85bcbfc" => :high_sierra
+    sha256 "b7165cc3def086000bc01e62724c5ae303200ca3eff64f681ead49f80313db20" => :catalina
+    sha256 "eb478bbf00c69404e64f21db962abdcd237d146a326d2779aaeae08f6414b8a2" => :mojave
+    sha256 "24dd633197c7ea490125a506db6379f7b134efdd1f7426bb6d9c0830c1b09f98" => :high_sierra
   end
 
   depends_on "autoconf" => :build
