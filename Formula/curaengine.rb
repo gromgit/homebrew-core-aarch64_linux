1e1e55e918f76f569cfef3e2ab69fd44b72fe452
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-05 20:16:15 +0000

    curaengine: update 4.7.1 bottle.

--- Formula/curaengine.rb
+++ Formula/curaengine.rb
@@ -19,9 +19,9 @@ class Curaengine < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9228dc8d4d2882c9c9446b254f2662730a966bfdae889d21160ba6d8f22f0828" => :catalina
-    sha256 "f09a5563330546fcd4eea14e824df26f7bbca435e87330e11198742305d8a240" => :mojave
-    sha256 "610e9ee7b9e8a914dbf07139daad3d5360f810531e45bd02860ec1dd92929ee0" => :high_sierra
+    sha256 "b1b33c714035d7400481caec6af52fe0cd9cceb2ca9f6d528bfd12393e16ff73" => :catalina
+    sha256 "7463c7161e156ae11f125994dc7eae5b95e2b6a2a7df3f3582e835cc2eddfaa2" => :mojave
+    sha256 "da76f6f441244dfd0543b07a07fa02a84f04a494a189148d3e3784789136f861" => :high_sierra
   end
 
   depends_on "cmake" => :build
