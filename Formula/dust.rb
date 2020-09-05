af257d89d2c5379e817a1ec446d3ab819ecd72f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-05 09:26:02 +0000

    dust: update 0.5.3 bottle.

--- Formula/dust.rb
+++ Formula/dust.rb
@@ -13,9 +13,9 @@ class Dust < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0f66a74fbd7def9b4bf0bdb77f2179caa0b61b18ad22cd401e4ff85c5a7d6bb4" => :catalina
-    sha256 "eefa1e6de716f8b880dc1ef6bb137a5169bcfc3a4b1ba41a7f54b20904cbaf27" => :mojave
-    sha256 "98ae783e45795c50a21d2dbbaef947bd000b194a2039f4a31cb309964b44bfb8" => :high_sierra
+    sha256 "693714f8f0d1207b8e54357f924e3a1242dd54d93f21de76b669217812065711" => :catalina
+    sha256 "4c5c1cda666e8d405cf39ae79b3327dd8581555c18417b0834c2d334ad4568ed" => :mojave
+    sha256 "52750a57ee2e58c7b95661b61cb0d855ba653946f6f21881ed041f2ce2cea747" => :high_sierra
   end
 
   depends_on "rust" => :build
