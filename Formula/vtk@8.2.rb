7f2129e9ecc5a8465d8f159b5d056ba7a7f3bf0f
Dustin Rodrigues <dust.rod@gmail.com>
2020-09-07 03:04:29 -0400

    vtk@8.2: add versioned formula

---
+++ Formula/vtk@8.2.rb
@@ -0,0 +1,118 @@
+class VtkAT82 < Formula
+  desc "Toolkit for 3D computer graphics, image processing, and visualization"
+  homepage "https://www.vtk.org/"
+  url "https://www.vtk.org/files/release/8.2/VTK-8.2.0.tar.gz"
+  sha256 "34c3dc775261be5e45a8049155f7228b6bd668106c72a3c435d95730d17d57bb"
+  license "BSD-3-Clause"
+
+  bottle do
+    sha256 "8eb01d3ba874398a6a8d1a920df4bfd0ae759aa1c608e141b8136b72c57c357b" => :catalina
+    sha256 "e39960ed536681ea158fbe2d10da875e8fb52a1354b37c97a631d4d84b4216bf" => :mojave
+    sha256 "335502476de20a9780fc2bcc3257cce3aebffcc3f7fdc57e322ac94e7e0c8f3b" => :high_sierra
+  end
+
+  keg_only :versioned_formula
+
+  deprecate! date: "2020-05-14", because: :versioned_formula
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "fontconfig"
+  depends_on "hdf5"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "netcdf"
+  depends_on "pyqt"
+  depends_on "python@3.8"
+  depends_on "qt"
+
+  # Fix compile issues on Mojave and later
+  patch do
+    url "https://gitlab.kitware.com/vtk/vtk/commit/ca3b5a50d945b6e65f0e764b3138cad17bd7eb8d.diff"
+    sha256 "b9f7a3ebf3c29f3cad4327eb15844ac0ee849755b148b60fef006314de8e822e"
+  end
+
+  # Python 3.8 compatibility
+  patch do
+    url "https://gitlab.kitware.com/vtk/vtk/commit/257b9d7b18d5f3db3fe099dc18f230e23f7dfbab.diff"
+    sha256 "572c06a4ba279a133bfdcf0190fec2eff5f330fa85ad6a2a0b0f6dfdea01ca69"
+  end
+
+  # Qt 5.15 support
+  patch do
+    url "https://gitlab.kitware.com/vtk/vtk/-/commit/797f28697d5ba50c1fa2bc5596af626a3c277826.diff"
+    sha256 "cb3b3a0e6978889a9cb95be35f3d4a6928397d3b843ab72ecaaf96554c6d4fc7"
+  end
+
+  def install
+    # Do not record compiler path because it references the shim directory
+    inreplace "Common/Core/vtkConfigure.h.in", "@CMAKE_CXX_COMPILER@", "clang++"
+
+    pyver = Language::Python.major_minor_version "python3"
+    args = std_cmake_args + %W[
+      -DBUILD_SHARED_LIBS=ON
+      -DBUILD_TESTING=OFF
+      -DCMAKE_INSTALL_NAME_DIR:STRING=#{lib}
+      -DCMAKE_INSTALL_RPATH:STRING=#{lib}
+      -DModule_vtkInfovisBoost=ON
+      -DModule_vtkInfovisBoostGraphAlgorithms=ON
+      -DModule_vtkRenderingFreeTypeFontConfig=ON
+      -DVTK_REQUIRED_OBJCXX_FLAGS=''
+      -DVTK_USE_COCOA=ON
+      -DVTK_USE_SYSTEM_EXPAT=ON
+      -DVTK_USE_SYSTEM_HDF5=ON
+      -DVTK_USE_SYSTEM_JPEG=ON
+      -DVTK_USE_SYSTEM_LIBXML2=ON
+      -DVTK_USE_SYSTEM_NETCDF=ON
+      -DVTK_USE_SYSTEM_PNG=ON
+      -DVTK_USE_SYSTEM_TIFF=ON
+      -DVTK_USE_SYSTEM_ZLIB=ON
+      -DVTK_WRAP_PYTHON=ON
+      -DVTK_PYTHON_VERSION=3
+      -DPYTHON_EXECUTABLE=#{Formula["python@3.8"].opt_bin}/python3
+      -DVTK_INSTALL_PYTHON_MODULE_DIR=#{lib}/python#{pyver}/site-packages
+      -DVTK_QT_VERSION:STRING=5
+      -DVTK_Group_Qt=ON
+      -DVTK_WRAP_PYTHON_SIP=ON
+      -DSIP_PYQT_DIR='#{Formula["pyqt5"].opt_share}/sip'
+    ]
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make"
+      system "make", "install"
+    end
+
+    # Avoid hard-coding HDF5's Cellar path
+    inreplace Dir["#{lib}/cmake/**/vtkhdf5.cmake"].first,
+              Formula["hdf5"].prefix.realpath,
+              Formula["hdf5"].opt_prefix
+    # get rid of bad include paths on 10.14+
+    if MacOS.version >= :mojave
+      inreplace Dir["#{lib}/cmake/vtk-*/Modules/vtklibxml2.cmake"], %r{;/Library/Developer/CommandLineTools[^"]*}, ""
+      inreplace Dir["#{lib}/cmake/vtk-*/Modules/vtkexpat.cmake"], %r{;/Library/Developer/CommandLineTools[^"]*}, ""
+      inreplace Dir["#{lib}/cmake/vtk-*/Modules/vtkzlib.cmake"], %r{;/Library/Developer/CommandLineTools[^"]*}, ""
+      inreplace Dir["#{lib}/cmake/vtk-*/Modules/vtkpng.cmake"], %r{;/Library/Developer/CommandLineTools[^"]*}, ""
+    end
+  end
+
+  test do
+    vtk_include = Dir[opt_include/"vtk-*"].first
+    major, minor = vtk_include.match(/.*-(.*)$/)[1].split(".")
+
+    (testpath/"version.cpp").write <<~EOS
+      #include <vtkVersion.h>
+      #include <assert.h>
+      int main(int, char *[]) {
+        assert (vtkVersion::GetVTKMajorVersion()==#{major});
+        assert (vtkVersion::GetVTKMinorVersion()==#{minor});
+        return EXIT_SUCCESS;
+      }
+    EOS
+
+    system ENV.cxx, "-std=c++11", "version.cpp", "-I#{vtk_include}"
+    system "./a.out"
+    system "#{bin}/vtkpython", "-c", "exit()"
+  end
+end
