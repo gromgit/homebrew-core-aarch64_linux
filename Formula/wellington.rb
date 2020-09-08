bdf8c2c1ff75214d7008f506a2c79bc0e5fddeab
Drew Wells <drew.wells00@gmail.com>
2020-09-07 22:00:01 -0500

    wellington 1.0.5
    remove gopath
    
    Closes #60776.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wellington.rb
+++ Formula/wellington.rb
@@ -1,10 +1,8 @@
-require "language/go"
-
 class Wellington < Formula
   desc "Project-focused tool to manage Sass and spriting"
   homepage "https://getwt.io/"
-  url "https://github.com/wellington/wellington/archive/v1.0.4.tar.gz"
-  sha256 "ef92d6c2b11fe36f66b88612e7a9cfff3ea6f81f29f4c21481d358f474a191d6"
+  url "https://github.com/wellington/wellington/archive/v1.0.5.tar.gz"
+  sha256 "e2379722849cdd8e5f094849290aacba4b789d4d65c733dec859565c728e7205"
   license "Apache-2.0"
   head "https://github.com/wellington/wellington.git"
 
@@ -17,26 +15,11 @@ class Wellington < Formula
   end
 
   depends_on "go" => :build
-  depends_on "pkg-config" => :build
-
-  go_resource "golang.org/x/net" do
-    url "https://github.com/golang/net.git",
-        revision: "f09c4662a0bd6bd8943ac7b4931e185df9471da4"
-  end
 
   def install
-    ENV["GOPATH"] = buildpath
-
-    dir = buildpath/"src/github.com/wellington/wellington"
-    dir.install buildpath.children
-    Language::Go.stage_deps resources, buildpath/"src"
-
-    cd dir do
-      system "go", "build", "-ldflags",
-             "-X github.com/wellington/wellington/version.Version=#{version}",
-             "-o", bin/"wt", "wt/main.go"
-      prefix.install_metafiles
-    end
+    system "go", "build", "-ldflags",
+            "-X github.com/wellington/wellington/version.Version=#{version}",
+            "-o", bin/"wt", "wt/main.go"
   end
 
   test do
