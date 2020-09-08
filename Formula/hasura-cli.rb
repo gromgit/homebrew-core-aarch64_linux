20f2853a808abb13e5edd07d34a4eb58e0789cd7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-08 15:16:07 +0000

    hasura-cli: update 1.3.2 bottle.

--- Formula/hasura-cli.rb
+++ Formula/hasura-cli.rb
@@ -7,9 +7,9 @@ class HasuraCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "4b1b4aa16d5f355ee318189b13076d71c84ce18c3dba4d790de9a5555c1fa4c0" => :catalina
-    sha256 "552166554a2ba329ffed87238619971d8d68c1ae5279ff1d94c85e776d044e3f" => :mojave
-    sha256 "dbadb855600d3722c7ba1b88c612f7f2c435a20d6139b0e8849dca8b920dcc1c" => :high_sierra
+    sha256 "85e902904e462772a3f45e236178195a61a71daf53ca2dd488e8aafbd5374be7" => :catalina
+    sha256 "204859c85e1cbe73eeffb0a02641f23d607ead9aa4a90337c9c6e64a1b219c3e" => :mojave
+    sha256 "0b2eac67491041cf2ff84dc7a1b5668ee47c8aab366edc8fda3f943347f396ee" => :high_sierra
   end
 
   depends_on "go" => :build
