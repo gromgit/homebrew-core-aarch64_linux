ca2d301c2a1d299f280efba43fb17e2398d50a57
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-09-08 17:09:22 +0100

    gst-python 1.18.0

--- Formula/gst-python.rb
+++ Formula/gst-python.rb
@@ -1,9 +1,9 @@
 class GstPython < Formula
   desc "Python overrides for gobject-introspection-based pygst bindings"
   homepage "https://gstreamer.freedesktop.org/modules/gst-python.html"
-  url "https://gstreamer.freedesktop.org/src/gst-python/gst-python-1.16.2.tar.xz"
-  sha256 "208df3148d73d9f416d016564737585d8ea763d91201732d44b5fe688c6288a8"
-  revision 1
+  url "https://gstreamer.freedesktop.org/src/gst-python/gst-python-1.18.0.tar.xz"
+  sha256 "76bfe8b85a9c4a6ddfb81874f2635fd0da38c3f39d9d2a0b175213218516dd45"
+  license "LGPL-2.1-or-later"
 
   livecheck do
     url "https://gstreamer.freedesktop.org/src/gst-python/"
@@ -17,20 +17,24 @@ class GstPython < Formula
     sha256 "4f97f255287bcefc62d520f4c29111c3ec1012a582cc5251c00779c29b8a4a02" => :high_sierra
   end
 
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "gst-plugins-base"
   depends_on "pygobject3"
   depends_on "python@3.8"
 
+  # See https://gitlab.freedesktop.org/gstreamer/gst-python/-/merge_requests/41
+  patch do
+    url "https://gitlab.freedesktop.org/gstreamer/gst-python/-/commit/3e752ede7ed6261681ef3831bc3dbb594f189e76.patch"
+    sha256 "734291a1468dff21c61598a36ffa84776d33c113313f5a8c420829ea67f55e78"
+  end
+
   def install
-    python_version = Language::Python.major_minor_version Formula["python@3.8"].opt_bin/"python3"
-    # pygi-overrides-dir switch ensures files don't break out of sandbox.
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}",
-                          "--with-pygi-overrides-dir=#{lib}/python#{python_version}/site-packages/gi/overrides",
-                          "PYTHON=#{Formula["python@3.8"].opt_bin}/python3",
-                          "LDFLAGS=-undefined dynamic_lookup"
-    system "make", "install"
+    mkdir "build" do
+      system "meson", *std_meson_args, ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
+    end
   end
 
   test do
