8ef4468d67253c3ddc4cb807aba404f48907a14b
Seeker <meaningseeking@protonmail.com>
2020-09-09 00:38:22 -0700

    aalib: add license

--- Formula/aalib.rb
+++ Formula/aalib.rb
@@ -3,6 +3,7 @@ class Aalib < Formula
   homepage "https://aa-project.sourceforge.io/aalib/"
   url "https://downloads.sourceforge.net/project/aa-project/aa-lib/1.4rc5/aalib-1.4rc5.tar.gz"
   sha256 "fbddda9230cf6ee2a4f5706b4b11e2190ae45f5eda1f0409dc4f99b35e0a70ee"
+  license "GPL-2.0-or-later"
   revision 1
 
   # The latest version in the formula is a release candidate, so we have to
