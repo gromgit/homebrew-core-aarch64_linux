f57f327f5c1e03fc2de02cfe235d8d84fb1ef419
Harel Ben-Attia <harelba@gmail.com>
2020-09-09 23:05:27 +0300

    q 2.0.16
    Closes #60861.
    
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/q.rb
+++ Formula/q.rb
@@ -1,16 +1,25 @@
 class Q < Formula
-  desc "Treat text as a database"
-  homepage "https://github.com/harelba/q"
-  url "https://github.com/harelba/q/archive/2.0.11.tar.gz"
-  sha256 "f0b88809d0b1f35ba7a8df5d2d86af12d10cea9e64eb554d3b7f8a5fe886d875"
-  license "GPL-3.0"
+  include Language::Python::Virtualenv
+
+  desc "Run SQL directly on CSV or TSV files"
+  homepage "https://harelba.github.io/q/"
+  url "https://github.com/harelba/q/archive/2.0.16.tar.gz"
+  sha256 "3376c7bcea4b2511e4bcbb4f3b0a04d76a4fdf28f3345755f6d468edaa2fd936"
+  license "GPL-3.0-only"
   head "https://github.com/harelba/q.git"
 
-  bottle :unneeded
+  depends_on "python@3.8"
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/6b/34/415834bfdafca3c5f451532e8a8d9ba89a21c9743a0c59fbd0205c7f9426/six-1.15.0.tar.gz"
+    sha256 "30639c035cdb23534cd4aa2dd52c3bf48f06e5f4a941509c8bafd8ce11080259"
+  end
 
   def install
-    bin.install Dir["bin/*"] - ["bin/q.bat"]
-    bin.install_symlink bin/"q.py" => "q"
+    # broken symlink, fixed in next version
+    rm_f "bin/qtextasdata.py"
+
+    virtualenv_install_with_resources
   end
 
   test do
