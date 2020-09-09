93e8a25d2904cb016184e40a12ec52a301407acc
Rui Chen <rui@meetup.com>
2020-09-09 10:00:40 -0400

    openfortivpn 1.15.0

--- Formula/openfortivpn.rb
+++ Formula/openfortivpn.rb
@@ -1,10 +1,9 @@
 class Openfortivpn < Formula
   desc "Open Fortinet client for PPP+SSL VPN tunnel services"
   homepage "https://github.com/adrienverge/openfortivpn"
-  url "https://github.com/adrienverge/openfortivpn/archive/v1.14.1.tar.gz"
-  sha256 "bc62fc6ecaaa6c6f8f2510e14a067a0cb9762158d9460c04555990bba44b50ca"
+  url "https://github.com/adrienverge/openfortivpn/archive/v1.15.0.tar.gz"
+  sha256 "5279dbd3da779b818d201bbd9243cff9421a7e790fd2190438610a03c88641f8"
   license "GPL-3.0"
-  revision 1
 
   bottle do
     sha256 "0c04566ef63f3cc1cb5062c3a18b78f6fed368dd6d449670e3a2fedf2d6f3806" => :catalina
