2ea6a075fa0e60874330bb5624f74f1afef5e8ce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-10 08:15:25 +0000

    unixodbc: update 2.3.9 bottle.

--- Formula/unixodbc.rb
+++ Formula/unixodbc.rb
@@ -11,9 +11,9 @@ class Unixodbc < Formula
   end
 
   bottle do
-    sha256 "b0df0122140e5b40cf874b0334c6a7bfc1cd5acc79ceffde21f9b95c56c27cc6" => :catalina
-    sha256 "7cd0f37108f83ec9079cd22592e4afc528d726bba5dafb7a93cc8a62ffcd7961" => :mojave
-    sha256 "6d0f38787107264f9e55563625b18d8c5a744da061925c37ee013c538001f875" => :high_sierra
+    sha256 "b312633496b3b92a61751508d0c35b7053a1cf202aedae79d2609cf6dfdede27" => :catalina
+    sha256 "f52d9ff5a13e7e78560cead35ca4a3d17e4582e791319c6c15d47ac8ac6f63d4" => :mojave
+    sha256 "f7bbaf85f41df090d7ea6c8103543ec2890164ef43c4c2bdb7cef13c0993585d" => :high_sierra
   end
 
   depends_on "libtool"
