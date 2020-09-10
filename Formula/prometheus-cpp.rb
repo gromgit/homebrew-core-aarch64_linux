699e3ab9fad361ecad7c33331b7775fa69af1464
Arne Luenser <arne.luenser@ppro.com>
2020-09-10 11:53:50 +0200

    prometheus-cpp 0.9.0 (new formula)
    Closes #60912.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/prometheus-cpp.rb
@@ -0,0 +1,31 @@
+class PrometheusCpp < Formula
+  desc "Prometheus Client Library for Modern C++"
+  homepage "https://github.com/jupp0r/prometheus-cpp"
+  url "https://github.com/jupp0r/prometheus-cpp.git",
+    tag:      "v0.9.0",
+    revision: "023c93e4e504d0954b01cdde13bd293865880388",
+    shallow:  false
+  license "MIT"
+  head "https://github.com/jupp0r/prometheus-cpp.git"
+
+  depends_on "cmake" => :build
+  uses_from_macos "curl"
+  uses_from_macos "zlib"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <prometheus/Registry.h>
+      int main() {
+        prometheus::Registry reg;
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "test.cpp", "-I#{include}", "-L#{lib}", "-lprometheus-cpp-core", "-o", "test"
+    system "./test"
+  end
+end
