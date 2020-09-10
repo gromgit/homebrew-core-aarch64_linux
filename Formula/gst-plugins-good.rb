74cd981c5de1c5d0d583835eb9a63c4090d86ff0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-10 12:19:28 +0000

    gst-plugins-good: update 1.18.0 bottle.

--- Formula/gst-plugins-good.rb
+++ Formula/gst-plugins-good.rb
@@ -12,9 +12,9 @@ class GstPluginsGood < Formula
   end
 
   bottle do
-    sha256 "4ea6d0ea1dcc6c20afbd0976006a84d57067b83926aa74bd28a0d21b4afa1aa9" => :catalina
-    sha256 "5bab7c207014dad9b950247148d930251fba89458f97527b9fb076a1a5a843ca" => :mojave
-    sha256 "0af59390678c89f8a5c396afcd63e5b1d95d84dad217c61860db6adc3f8c2539" => :high_sierra
+    sha256 "640ea34d1d6a1677ef0d686c94a94c7f11fc51a3cc559f259a93a67829df104e" => :catalina
+    sha256 "947085d8a2e20821863142835ed01c2194b5fc5f5215d1bce2a5305da600d177" => :mojave
+    sha256 "a4ce36c20f484ae760bad584863b37d24d685f7418e459f9ce2e407d9ca3f458" => :high_sierra
   end
 
   depends_on "meson" => :build
