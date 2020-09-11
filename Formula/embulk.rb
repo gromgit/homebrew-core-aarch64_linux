96eb0af14b18df44ef7d909ce1e027acba92b6d0
chenrui <chenrui333@gmail.com>
2020-09-10 22:01:25 -0400

    embulk 0.10.15 (#60935)

--- Formula/embulk.rb
+++ Formula/embulk.rb
@@ -4,10 +4,9 @@ class Embulk < Formula
   # https://www.embulk.org/articles/2020/03/13/embulk-v0.10.html
   # v0.10.* is a "development" series, not for your production use.
   # In your production, keep using v0.9.*.
-  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.9.23.jar"
-  sha256 "153977fad482bf52100dd96f47e897c87b48de4fb13bccd6b3101475d3a5ebb9"
+  url "https://github.com/embulk/embulk/releases/download/v0.10.15/embulk-0.10.15.jar"
+  sha256 "4f14f446eafc121297ffa08b17af86eef253289cec71cfe66b6f67059ceeb4e7"
   license "Apache-2.0"
-  revision 1
 
   livecheck do
     url "https://github.com/embulk/embulk.git"
