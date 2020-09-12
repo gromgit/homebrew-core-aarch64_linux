4e004d114cc66673924ce6ee37b997b885288309
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-12 14:15:14 +0000

    google-benchmark: update 1.5.2 bottle.

--- Formula/google-benchmark.rb
+++ Formula/google-benchmark.rb
@@ -8,9 +8,9 @@ class GoogleBenchmark < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d81d77c351b590b574869d88e22203aa3728410bc1d775f10bec672bf947e0af" => :catalina
-    sha256 "ec80102ba3d26ce496a543a5270b4958c25cac3fc72c23f1daceff49bfafc467" => :mojave
-    sha256 "407dfc8c6995f34fc9b6fd7da02747d538326462402e744e2318a2db5e3e86ed" => :high_sierra
+    sha256 "432e4f98bad2a73d0f47279714d5028dfad2283f939eb745794b47272bf90f2e" => :catalina
+    sha256 "e303f9f7f9ce196aa338a18767605162d27612514cd134e7b143be0b85ffe66c" => :mojave
+    sha256 "8275a82eeb23188a166f67ace983ee9968f247840d20fb0119a5f1c0f5067c7a" => :high_sierra
   end
 
   depends_on "cmake" => :build
