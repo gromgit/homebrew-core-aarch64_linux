80fd9bd21c019bd9753e7992bbfe55da257520b3
Rui Chen <rui@meetup.com>
2020-09-12 16:08:22 -0400

    proteinortho 6.0.23
    use c++11
    
    Closes #61067.
    
    Signed-off-by: Claudia Pellegrino <1239874+claui@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/proteinortho.rb
+++ Formula/proteinortho.rb
@@ -1,8 +1,8 @@
 class Proteinortho < Formula
   desc "Detecting orthologous genes within different species"
   homepage "https://gitlab.com/paulklemm_PHD/proteinortho"
-  url "https://gitlab.com/paulklemm_PHD/proteinortho/-/archive/v6.0.22/proteinortho-v6.0.22.tar.gz"
-  sha256 "9a30ae580a360f2898019d9651ab8d6522bfc1abeceb32d17ed1454ba807193d"
+  url "https://gitlab.com/paulklemm_PHD/proteinortho/-/archive/v6.0.23/proteinortho-v6.0.23.tar.gz"
+  sha256 "e2a93fee6bcbc422e0bbeb98150bf3287c536007397d2bdc4859fc955abb3dc2"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -16,6 +16,8 @@ class Proteinortho < Formula
   depends_on "openblas"
 
   def install
+    ENV.cxx11
+
     bin.mkpath
     system "make", "install", "PREFIX=#{bin}"
     doc.install "manual.html"
