b5c271a06b72d38b96154656df4ac235f6b36d7f
chenrui <rui@meetup.com>
2020-09-12 12:50:55 -0400

    knot-resolver: add license
    update license
    
    Closes #61046.
    
    Signed-off-by: Rui Chen <rui@meetup.com>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/knot-resolver.rb
+++ Formula/knot-resolver.rb
@@ -3,6 +3,7 @@ class KnotResolver < Formula
   homepage "https://www.knot-resolver.cz"
   url "https://secure.nic.cz/files/knot-resolver/knot-resolver-5.1.3.tar.xz"
   sha256 "20cd829027e39a9f7d993894e3640e886825b492d9ac1a744ac5616cc101458b"
+  license all_of: ["CC0-1.0", "GPL-3.0-or-later", "LGPL-2.1-or-later", "MIT"]
   head "https://gitlab.labs.nic.cz/knot/knot-resolver.git"
 
   livecheck do
