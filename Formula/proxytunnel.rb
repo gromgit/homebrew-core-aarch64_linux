e812c08205c3c56bb6fe503cd9435dd047bb8815
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-12 17:13:45 +0000

    proxytunnel: update 1.10.20200907 bottle.

--- Formula/proxytunnel.rb
+++ Formula/proxytunnel.rb
@@ -7,9 +7,9 @@ class Proxytunnel < Formula
 
   bottle do
     cellar :any
-    sha256 "f356990b424dc670a68c753c072e1a008d772bf7e27025f34b343c4a63a590d9" => :catalina
-    sha256 "045219106e5c4b06627cf57fd21b27c8a24d11c5375df3c028e9b23d719e4b0f" => :mojave
-    sha256 "98cf5abe9bbb285c92ef1c0e504968707fa5106e2783fed2dbfc64fb2a5dafd4" => :high_sierra
+    sha256 "18a5771a429cb73cc2f8de55c44ba2ba061238157458533302b5d609478039c7" => :catalina
+    sha256 "257d06dda3279ca934dead8774ac3800bf97404af51d5fbfda7fba4ec99ce36c" => :mojave
+    sha256 "46362f2b3cd8118b315e5eaae4c0fd5e391a2c741399f33c772cac3992221846" => :high_sierra
   end
 
   depends_on "asciidoc" => :build
