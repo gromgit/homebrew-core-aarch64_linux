bf94976c3eff6e62305e642319ca7c535b6d94e9
Rui Chen <rui@meetup.com>
2020-09-12 10:00:48 -0400

    jasper 2.0.20
    add "-DJAS_ENABLE_AUTOMATIC_DEPENDENCIES=false"
    
    fix code style issue
    
    Closes #61029.
    
    Signed-off-by: Rui Chen <rui@meetup.com>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jasper.rb
+++ Formula/jasper.rb
@@ -1,8 +1,8 @@
 class Jasper < Formula
   desc "Library for manipulating JPEG-2000 images"
   homepage "https://www.ece.uvic.ca/~frodo/jasper/"
-  url "https://github.com/mdadams/jasper/archive/version-2.0.19.tar.gz"
-  sha256 "b9d16162a088617ada36450f2374d72165377cb64b33ed197c200bcfb73ec76c"
+  url "https://github.com/mdadams/jasper/archive/version-2.0.20.tar.gz"
+  sha256 "d55843ce52afa9bfe90f30118329578501040f30d48a027459a68a962695e506"
   license "JasPer-2.0"
 
   bottle do
@@ -19,12 +19,19 @@ class Jasper < Formula
       # Make sure macOS's GLUT.framework is used, not XQuartz or freeglut
       # Reported to CMake upstream 4 Apr 2016 https://gitlab.kitware.com/cmake/cmake/issues/16045
       glut_lib = "#{MacOS.sdk_path}/System/Library/Frameworks/GLUT.framework"
-      system "cmake", "..", "-DGLUT_glut_LIBRARY=#{glut_lib}", *std_cmake_args
+
+      system "cmake", "..",
+        "-DGLUT_glut_LIBRARY=#{glut_lib}",
+        "-DJAS_ENABLE_AUTOMATIC_DEPENDENCIES=false",
+        *std_cmake_args
       system "make"
-      system "make", "test"
       system "make", "install"
       system "make", "clean"
-      system "cmake", "..", "-DGLUT_glut_LIBRARY=#{glut_lib}", "-DJAS_ENABLE_SHARED=OFF", *std_cmake_args
+
+      system "cmake", "..",
+        "-DGLUT_glut_LIBRARY=#{glut_lib}",
+        "-DJAS_ENABLE_SHARED=OFF",
+        *std_cmake_args
       system "make"
       lib.install "src/libjasper/libjasper.a"
     end
