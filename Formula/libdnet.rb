acd77a8ed919da9632094c6688a154e4f54a10fb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-13 16:15:37 +0000

    libdnet: update 1.14 bottle.

--- Formula/libdnet.rb
+++ Formula/libdnet.rb
@@ -12,11 +12,9 @@ class Libdnet < Formula
 
   bottle do
     cellar :any
-    rebuild 4
-    sha256 "f0f94480e8f973b31b39dfbb7f594c678f02c7176559badb1d31d644deb0a34a" => :catalina
-    sha256 "540ccb96d3647a0d71f563e06d08e410d14b7d09c23f6348bc91fd22251e5ff2" => :mojave
-    sha256 "ece250e6792f542e5546ac5e8e5144fe07c76ce3ddb94216181e85092d530e81" => :high_sierra
-    sha256 "bb550ef762ca5d65f87b65575758557afcf8e6b93855be32638cab265540ba6b" => :sierra
+    sha256 "f8c9ace5eb112c484f50da7624df13c551b14114ece91a155ce2394b30e264b7" => :catalina
+    sha256 "1e967ac6c5b9c70f72efba9082844c755dba2d62054a4c4dfbd5629da3cb0b76" => :mojave
+    sha256 "fd53de5c1830dcdb52ecba97cf0c9c6afccf44037e6df6f64ef1d163d6c6adff" => :high_sierra
   end
 
   depends_on "autoconf" => :build
