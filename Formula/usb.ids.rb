5fe64f0d0a534bb5db7087c5823ade748ba31100
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-13 01:21:01 +0000

    usb.ids: update 2020.08.26 bottle.

--- Formula/usb.ids.rb
+++ Formula/usb.ids.rb
@@ -7,9 +7,9 @@ class UsbIds < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "86ee1383749511bfa93b03e04b34e99f405abfbe791df50be85f992a303965c1" => :catalina
-    sha256 "9acaeabecc451f483976baeee5967a6123d4945b3227942b15ecec8bb390e44f" => :mojave
-    sha256 "cb6f7ce696f58356e9ab5679ac4dba62f66d597dd62eacdb64e94e94c10e5be8" => :high_sierra
+    sha256 "2994769226c7815ef5eee9ba27f729005fd993341dfbca50f413139ef411ac5c" => :catalina
+    sha256 "8b29c5873a395b8bdff9219dcfafb13d05d7428c8f4d050cb776d332dd7aef1f" => :mojave
+    sha256 "18a048550eae20c48c7af4cc0b93f1da748cae52417e364b1aedc154c27613d5" => :high_sierra
   end
 
   def install
