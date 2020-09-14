eea0fa73e5ac41d083e83520100982cef4046e6b
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-09-14 07:46:36 +0000

    prometheus-cpp: add 0.9.0 bottle.

--- Formula/prometheus-cpp.rb
+++ Formula/prometheus-cpp.rb
@@ -8,6 +8,13 @@ class PrometheusCpp < Formula
   license "MIT"
   head "https://github.com/jupp0r/prometheus-cpp.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8a7554ae3fc4618f5b405aa365f2c48192304406a5aba72611110cb909e1669f" => :catalina
+    sha256 "c843326804e9e84640a31254e793e15b7956aa69be769d8bd0d6888bf7933a0f" => :mojave
+    sha256 "3b9b31ccd9421f6777605f7c0b1921b0dca02a3be8c143b5468e32ccf7b36536" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   uses_from_macos "curl"
   uses_from_macos "zlib"
