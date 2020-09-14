648d949634a9b374907b1422a26c21c8ea17e86e
contentful <support@contentful.com>
2020-09-13 19:04:55 -0700

    contentful-cli 1.4.31
    Closes #61100.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/contentful-cli.rb
+++ Formula/contentful-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class ContentfulCli < Formula
   desc "Contentful command-line tools"
   homepage "https://github.com/contentful/contentful-cli"
-  url "https://registry.npmjs.org/contentful-cli/-/contentful-cli-1.4.30.tgz"
-  sha256 "f6d1ae178f74b302b603263b59adec0056f681a40ef5a7df663e9884170e9e2a"
+  url "https://registry.npmjs.org/contentful-cli/-/contentful-cli-1.4.31.tgz"
+  sha256 "36d8c9af3bb648c94c13db7dbc9daf57dfd87718d4196875cbd0f761726ead0e"
   license "MIT"
   head "https://github.com/contentful/contentful-cli.git"
 
@@ -30,6 +30,6 @@ class ContentfulCli < Formula
     output = shell_output("#{bin}/contentful space list 2>&1", 1)
     assert_match "🚨  Error: You have to be logged in to do this.", output
     assert_match "You can log in via contentful login", output
-    assert_match "Or provide a managementToken via --management-Token argument", output
+    assert_match "Or provide a management token via --management-token argument", output
   end
 end
