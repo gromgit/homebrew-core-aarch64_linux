08a2eb7ed8cc138b3533232e51d331690e31afb7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-14 08:30:15 +0000

    dssim: update 2.11.3 bottle.

--- Formula/dssim.rb
+++ Formula/dssim.rb
@@ -7,10 +7,9 @@ class Dssim < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d8eb6a36277a6dc7943aa09d07360e1b6f020074b8f0e98a67673d6542f77973" => :catalina
-    sha256 "c03454b7582f988de44af524a342e016b3bf63937496450365c758809e79a224" => :mojave
-    sha256 "f1284c6d8db31ce548232d0e7c0d4204249555ee5a9c92958b2c8bea4deee832" => :high_sierra
-    sha256 "61c6019667ce76ccc145381eea2131ee3119366b1d54d2878da908eca8f85339" => :sierra
+    sha256 "a45bab9dbd7de721418a18394c0b70dc66fcd44050c14a04b87e97687aa316a1" => :catalina
+    sha256 "2c7b025773e8f419bd0edab3815e243708b47f60ad62fa4595fa5726c1ab77d0" => :mojave
+    sha256 "288735020c3dca238550306be4e1ca80636539c2b779211889f9f485e7b8b610" => :high_sierra
   end
 
   depends_on "nasm" => :build
