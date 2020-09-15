63f7abc7bd8550b2a045e44ca560c17f596edd0e
Seeker <meaningseeking@protonmail.com>
2020-09-15 12:03:36 -0700

    uhd 4.0.0.0

--- Formula/uhd.rb
+++ Formula/uhd.rb
@@ -1,10 +1,9 @@
 class Uhd < Formula
   desc "Hardware driver for all USRP devices"
   homepage "https://files.ettus.com/manual/"
-  url "https://github.com/EttusResearch/uhd/archive/v3.15.0.0.tar.gz"
-  sha256 "eed4a77d75faafff56be78985950039f8d9d1eb9fcbd58b8862e481dd49825cd"
+  url "https://github.com/EttusResearch/uhd/archive/v4.0.0.0.tar.gz"
+  sha256 "4f3513c43edf0178391ed5755266864532716b8b503bcfb9a983ae6256c51b14"
   license all_of: ["GPL-3.0-or-later", "LGPL-3.0-or-later", "MIT", "BSD-3-Clause", "Apache-2.0"]
-  revision 2
   head "https://github.com/EttusResearch/uhd.git"
 
   livecheck do
@@ -26,24 +25,11 @@ class Uhd < Formula
   depends_on "python@3.9"
 
   resource "Mako" do
-    url "https://files.pythonhosted.org/packages/b0/3c/8dcd6883d009f7cae0f3157fb53e9afb05a0d3d33b3db1268ec2e6f4a56b/Mako-1.1.0.tar.gz"
-    sha256 "a36919599a9b7dc5d86a7a8988f23a9a3a3d083070023bab23d64f7f1d1e0a4b"
-  end
-
-  # Fix build with Boost 1.73 and later
-  # https://github.com/EttusResearch/uhd/issues/347
-  # Remove in next version
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/b9055b4b/uhd/boost-1.73.diff"
-    sha256 "0bf797655424d66f4ceae2feabbd00831bb7b6fa8e3e55e6ed56c7e26dd1a4cd"
+    url "https://files.pythonhosted.org/packages/72/89/402d2b4589e120ca76a6aed8fee906a0f5ae204b50e455edd36eda6e778d/Mako-1.1.3.tar.gz"
+    sha256 "8195c8c1400ceb53496064314c6736719c6f25e7479cd24c77be3d9361cddc27"
   end
 
   def install
-    # https://github.com/EttusResearch/uhd/issues/313
-    inreplace "host/lib/transport/nirio/lvbitx/process-lvbitx.py",
-              "autogen_src_path = os.path.relpath(options.output_src_path)",
-              "autogen_src_path = os.path.realpath(options.output_src_path)"
-
     xy = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
 
