e006a81b8de8e77b3d578cdf2a51c6549d86c9aa
Seeker <meaningseeking@protonmail.com>
2020-09-15 11:58:33 -0700

    dbdeployer 1.54.0
    Closes #61166.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dbdeployer.rb
+++ Formula/dbdeployer.rb
@@ -1,8 +1,8 @@
 class Dbdeployer < Formula
   desc "Tool to deploy sandboxed MySQL database servers"
   homepage "https://github.com/datacharmer/dbdeployer"
-  url "https://github.com/datacharmer/dbdeployer/archive/v1.53.3.tar.gz"
-  sha256 "f86b5684c9c997a1fa092467b872d591605a1b2acd96012aea428eb02691e05d"
+  url "https://github.com/datacharmer/dbdeployer/archive/v1.54.0.tar.gz"
+  sha256 "9ea2ac21c58e53dd221d1502a283857a6b9e2af1076f0778e30774d018043680"
   license "Apache-2.0"
   head "https://github.com/datacharmer/dbdeployer.git"
 
