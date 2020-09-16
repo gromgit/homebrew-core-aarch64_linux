0c0bd548476a314c70016de7650942eb91698472
chenrui <chenrui333@gmail.com>
2020-09-16 23:01:23 +0000

    kubernetes-cli: update 1.19.2 bottle.

--- Formula/kubernetes-cli.rb
+++ Formula/kubernetes-cli.rb
@@ -14,9 +14,9 @@ class KubernetesCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0038cd9ee8b1f266df2e1842da768faac6a28ae73336a55631a0738ddda64014" => :catalina
-    sha256 "09fc4442ff78ea737f343e25c22fa9dbd30b4e84fe2f407e1b6d9ba82b41dae4" => :mojave
-    sha256 "92c1c37bd26196b8eea55418ee31f418ebe0a41bc4ad024198ad175050eef818" => :high_sierra
+    sha256 "914a5582fdccdbc429e438b700f7130a6df821c24a454177c3bac9d66a7bad9e" => :catalina
+    sha256 "3ec1cae14b29188e6d756c3dd6a372d3043c8b3ce84e4275ff3a2ccdd5171148" => :mojave
+    sha256 "bc49147fe564917ab13ca30dfc687e020e8483647238fe738244d5b5c752b92f" => :high_sierra
   end
 
   depends_on "go" => :build
