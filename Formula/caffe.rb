075981727688550d9762e9b1a6cea51cbf31118c
Seeker <meaningseeking@protonmail.com>
2020-09-16 14:52:07 -0700

    caffe: use .patch instead of .diff

--- Formula/caffe.rb
+++ Formula/caffe.rb
@@ -37,8 +37,8 @@ class Caffe < Formula
   # Fix compilation with OpenCV 4
   # https://github.com/BVLC/caffe/issues/6652
   patch do
-    url "https://github.com/BVLC/caffe/commit/0a04cc2ccd37ba36843c18fea2d5cbae6e7dd2b5.diff?full_index=1"
-    sha256 "6a6368d715284fabfa96660b6d24d1f4f419f3e6cdddab9a7293954fee4ec2bc"
+    url "https://github.com/BVLC/caffe/commit/0a04cc2ccd37ba36843c18fea2d5cbae6e7dd2b5.patch?full_index=1"
+    sha256 "f79349200c46fc1228ab1e1c135a389a6d0c709024ab98700017f5f66b373b39"
   end
 
   def install
