2708f9a1e8c95aa26c794d1e55bc16d48b9d05e2
Caleb Xu <calebcenter@live.com>
2020-09-17 19:23:26 -0400

    grakn 1.8.3 (#61275)

--- Formula/grakn.rb
+++ Formula/grakn.rb
@@ -1,8 +1,8 @@
 class Grakn < Formula
   desc "The distributed hyper-relational database for knowledge engineering"
   homepage "https://grakn.ai"
-  url "https://github.com/graknlabs/grakn/releases/download/1.8.2/grakn-core-all-mac-1.8.2.zip"
-  sha256 "6e3c450e5d787f38b86697be48c99a4ce4489dd00fdb095b3a78286a7dc88fc2"
+  url "https://github.com/graknlabs/grakn/releases/download/1.8.3/grakn-core-all-mac-1.8.3.zip"
+  sha256 "e8d2a96c4b6144113ebd71781041168fe8dae2b115602b25aebe16960931434b"
   license "AGPL-3.0-or-later"
 
   bottle :unneeded
