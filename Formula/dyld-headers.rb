28b98422d67ed35969a73cd269faabe93aa5523b
Caleb Xu <calebcenter@live.com>
2020-09-17 19:03:03 -0400

    dyld-headers 750.6
    Closes #61273.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dyld-headers.rb
+++ Formula/dyld-headers.rb
@@ -1,8 +1,8 @@
 class DyldHeaders < Formula
   desc "Header files for the dynamic linker"
   homepage "https://opensource.apple.com/"
-  url "https://opensource.apple.com/tarballs/dyld/dyld-733.6.tar.gz"
-  sha256 "cabbea38a188a3b3c57e3f5ecba2d0124aa5c1edc4676d814ac635410bf1c538"
+  url "https://opensource.apple.com/tarballs/dyld/dyld-750.6.tar.gz"
+  sha256 "4fd378cf30718e0746c91b145b90ddfcaaa4c0bf01158d0461a4e092d7219222"
 
   livecheck do
     url "https://opensource.apple.com/tarballs/dyld/"
