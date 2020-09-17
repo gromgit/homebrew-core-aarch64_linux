5aa5290572f28108118a7f87aea62024819444eb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-17 22:15:52 +0000

    flow: update 0.134.0 bottle.

--- Formula/flow.rb
+++ Formula/flow.rb
@@ -8,9 +8,9 @@ class Flow < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "33676e1f00b63da0d5cca3db2304f8fb0913a39c86c788696986629e611b7cc9" => :catalina
-    sha256 "e94d74b743ea057b376e3da652f61bd524031ceddc056b54e0a102aff7ec3d4c" => :mojave
-    sha256 "723601f92a5463dd556cffcd871ee35dcaffad3ab29f49f69788a09e09cce805" => :high_sierra
+    sha256 "e177fd3b78aebf1dadcc7efd4fe3eadf9ab383dde41dfe3a8ba63803ffaf8dbc" => :catalina
+    sha256 "db4b378cae2ab2bf5ecbade9dd9a1205829e74daefce922a7694c5e734dee892" => :mojave
+    sha256 "58d04c305431a1448c18fc3d3954c2988849ce224cdc0e76e332d027369b8533" => :high_sierra
   end
 
   depends_on "ocaml" => :build
