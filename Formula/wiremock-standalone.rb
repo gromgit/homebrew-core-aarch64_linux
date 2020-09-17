e61cffcbd9d59e0421b5d84a414a4ebf69197f79
Seeker <meaningseeking@protonmail.com>
2020-09-17 00:47:35 -0700

    wiremock-standalone 2.27.2 (#61211)

--- Formula/wiremock-standalone.rb
+++ Formula/wiremock-standalone.rb
@@ -1,8 +1,8 @@
 class WiremockStandalone < Formula
   desc "Simulator for HTTP-based APIs"
   homepage "http://wiremock.org/docs/running-standalone/"
-  url "https://search.maven.org/remotecontent?filepath=com/github/tomakehurst/wiremock-standalone/2.27.1/wiremock-standalone-2.27.1.jar"
-  sha256 "e98c07e202eb3f8b470b7d0ae2703c880736ac0de870bba31b231df9b9db7833"
+  url "https://search.maven.org/remotecontent?filepath=com/github/tomakehurst/wiremock-standalone/2.27.2/wiremock-standalone-2.27.2.jar"
+  sha256 "00b14f9d6399a9bba96139f84354f981d9fd1c3f4cf6938d36391f292f76382b"
   license "Apache-2.0"
   head "https://github.com/tomakehurst/wiremock.git"
 
