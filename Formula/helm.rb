625b3a382337ec1f268486fac7a50933611efaf3
chenrui <chenrui333@gmail.com>
2020-09-17 20:33:43 +0000

    helm 3.3.2
    Closes #61269.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/helm.rb
+++ Formula/helm.rb
@@ -2,8 +2,8 @@ class Helm < Formula
   desc "The Kubernetes package manager"
   homepage "https://helm.sh/"
   url "https://github.com/helm/helm.git",
-      tag:      "v3.3.1",
-      revision: "249e5215cde0c3fa72e27eb7a30e8d55c9696144"
+      tag:      "v3.3.2",
+      revision: "e5077257b6ca106d1f65652b4ca994736d221ab1"
   license "Apache-2.0"
   head "https://github.com/helm/helm.git"
 
