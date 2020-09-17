9304e1551fc1619862fd19b2b48ee2dde7f23758
Thierry Moisan <thierry.moisan@gmail.com>
2020-09-16 23:23:04 -0400

    flyway 6.5.6
    Closes #61250.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/6.5.5/flyway-commandline-6.5.5.tar.gz"
-  sha256 "b6a5809ae91cc8acf9c11312967d732937d9ce3810584f729b54b354f3a3803c"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/6.5.6/flyway-commandline-6.5.6.tar.gz"
+  sha256 "7adc487a0f7007809b46ef6d65cedff504b2e27b5e7d588d0764403b584830b1"
   license "Apache-2.0"
 
   livecheck do
