a86392974f7859e09db69746ebfd2cdc5ee81ba7
Alexander Bayandin <a.bayandin@gmail.com>
2020-09-17 14:07:10 +0100

    micronaut: use openjdk

--- Formula/micronaut.rb
+++ Formula/micronaut.rb
@@ -4,6 +4,7 @@ class Micronaut < Formula
   url "https://github.com/micronaut-projects/micronaut-starter/archive/v2.1.2.tar.gz"
   sha256 "a124ff3b6b7bccad95d1322452c5770bcfdc484b866dd95100e9cf9648807ccf"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     url "https://github.com/micronaut-projects/micronaut-starter/releases/latest"
@@ -18,7 +19,7 @@ class Micronaut < Formula
   end
 
   depends_on "gradle" => :build
-  depends_on "openjdk@11" # Will be switched to openjdk in https://github.com/Homebrew/homebrew-core/pull/61226
+  depends_on "openjdk"
 
   def install
     system "gradle", "micronaut-cli:assemble", "-x", "test"
@@ -28,7 +29,7 @@ class Micronaut < Formula
     mv "starter-cli/build/exploded/lib", libexec/"lib"
 
     bash_completion.install "starter-cli/build/exploded/bin/mn_completion"
-    (bin/"mn").write_env_script libexec/"bin/mn", Language::Java.overridable_java_home_env("11")
+    (bin/"mn").write_env_script libexec/"bin/mn", Language::Java.overridable_java_home_env
   end
 
   test do
