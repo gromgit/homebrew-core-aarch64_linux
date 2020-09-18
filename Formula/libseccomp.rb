d6866a9f3bfe965d6435b23b2f481e358739cc93
Seeker <meaningseeking@protonmail.com>
2020-09-17 18:33:53 -0700

    libseccomp 2.5.0 (#61203)

--- Formula/libseccomp.rb
+++ Formula/libseccomp.rb
@@ -1,12 +1,13 @@
 class Libseccomp < Formula
   desc "Interface to the Linux Kernel's syscall filtering mechanism"
   homepage "https://github.com/seccomp/libseccomp"
-  url "https://github.com/seccomp/libseccomp/releases/download/v2.4.2/libseccomp-2.4.2.tar.gz"
-  sha256 "b54f27b53884caacc932e75e6b44304ac83586e2abe7a83eca6daecc5440585b"
+  url "https://github.com/seccomp/libseccomp/releases/download/v2.5.0/libseccomp-2.5.0.tar.gz"
+  sha256 "1ffa7038d2720ad191919816db3479295a4bcca1ec14e02f672539f4983014f3"
   license "LGPL-2.1-only"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+  depends_on "gperf" => :build
   depends_on "libtool" => :build
   depends_on :linux
 
