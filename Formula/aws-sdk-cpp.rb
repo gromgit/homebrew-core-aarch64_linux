854cdc9d618c0cf74dffcd818f057790d7b634eb
Caleb Xu <calebcenter@live.com>
2020-09-18 03:31:10 -0400

    aws-sdk-cpp 1.8.50
    Closes #61300.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/aws-sdk-cpp.rb
+++ Formula/aws-sdk-cpp.rb
@@ -2,8 +2,8 @@ class AwsSdkCpp < Formula
   desc "AWS SDK for C++"
   homepage "https://github.com/aws/aws-sdk-cpp"
   # aws-sdk-cpp should only be updated every 10 releases on multiples of 10
-  url "https://github.com/aws/aws-sdk-cpp/archive/1.8.40.tar.gz"
-  sha256 "1e95f2812d19d77ba361301a5a277b91baa4782aa797311b738c8b9fa1f5e252"
+  url "https://github.com/aws/aws-sdk-cpp/archive/1.8.50.tar.gz"
+  sha256 "86ab613924e4039f99bf9b7a76eb498c1157aef3c454726eb93231bddefc3e80"
   license "Apache-2.0"
   head "https://github.com/aws/aws-sdk-cpp.git"
 
