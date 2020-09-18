1f8084580dac61918b2b28b739c1e11766b3cb2a
Markus Reiter <me@reitermark.us>
2020-09-18 17:52:18 +0200

    ckan: remove article from description

--- Formula/ckan.rb
+++ Formula/ckan.rb
@@ -1,5 +1,5 @@
 class Ckan < Formula
-  desc "The Comprehensive Kerbal Archive Network"
+  desc "Comprehensive Kerbal Archive Network"
   homepage "https://github.com/KSP-CKAN/CKAN/"
   url "https://github.com/KSP-CKAN/CKAN/releases/download/v1.28.0/ckan.exe"
   sha256 "2486d98e06c247a61a94e583c202b8a4c74a439de3ea271b769d041aa78d21cd"
