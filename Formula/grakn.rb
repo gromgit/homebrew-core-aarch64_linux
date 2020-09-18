7b1e438436626443e76dfae28b9a3d5559cf5739
Markus Reiter <me@reitermark.us>
2020-09-18 17:52:19 +0200

    grakn: remove article from description

--- Formula/grakn.rb
+++ Formula/grakn.rb
@@ -1,5 +1,5 @@
 class Grakn < Formula
-  desc "The distributed hyper-relational database for knowledge engineering"
+  desc "Distributed hyper-relational database for knowledge engineering"
   homepage "https://grakn.ai"
   url "https://github.com/graknlabs/grakn/releases/download/1.8.3/grakn-core-all-mac-1.8.3.zip"
   sha256 "e8d2a96c4b6144113ebd71781041168fe8dae2b115602b25aebe16960931434b"
