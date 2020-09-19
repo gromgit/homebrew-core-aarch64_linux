5ba72d30632adc8bddde2411e841f10bfcd72822
chenrui <rui@meetup.com>
2020-09-19 15:13:40 -0400

    jbig2dec: update license
    Closes #61381.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jbig2dec.rb
+++ Formula/jbig2dec.rb
@@ -3,7 +3,7 @@ class Jbig2dec < Formula
   homepage "https://jbig2dec.com/"
   url "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs9531/jbig2dec-0.19.tar.gz"
   sha256 "279476695b38f04939aa59d041be56f6bade3422003a406a85e9792c27118a37"
-  license "AGPL-3.0"
+  license "AGPL-3.0-or-later"
 
   livecheck do
     url "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/latest"
