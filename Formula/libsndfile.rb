a6dac7f40f2c272ad2a447c4f0b7c95d62226e13
jnozsc <470215+jnozsc@users.noreply.github.com>
2020-09-20 10:26:30 -0700

    libsndfile 1.0.30
    Closes #61448.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libsndfile.rb
+++ Formula/libsndfile.rb
@@ -1,12 +1,9 @@
 class Libsndfile < Formula
   desc "C library for files containing sampled sound"
   homepage "http://www.mega-nerd.com/libsndfile/"
-  # Change back to stable when upstream releases > 1.0.28
-  # Fixes several security vulnerabilities
-  url "http://www.mega-nerd.com/libsndfile/files/1.0.29pre2/libsndfile-1.0.29pre2.tar.bz2"
-  sha256 "ffe2d6bff622bc66e6f96059ada79cfcdc43b3e8bc9cc4f45dbc567dccbfae75"
-  license "LGPL-2.1"
-  revision 1
+  url "https://github.com/erikd/libsndfile/releases/download/v1.0.30/libsndfile-1.0.30.tar.bz2"
+  sha256 "ec898634766595438142c76cf3bdd46b77305d4a295dd16b29d024122d7a4b3f"
+  license "LGPL-2.1-or-later"
 
   livecheck do
     url :homepage
