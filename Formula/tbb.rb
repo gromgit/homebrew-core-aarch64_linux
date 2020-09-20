c1afd5da5d5e536b0845bfd21776382a1d4a91cd
Rui Chen <rui@meetup.com>
2020-09-19 21:15:02 -0400

    tbb 2020_U3
    Closes #61416.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tbb.rb
+++ Formula/tbb.rb
@@ -1,9 +1,9 @@
 class Tbb < Formula
   desc "Rich and complete approach to parallelism in C++"
   homepage "https://www.threadingbuildingblocks.org/"
-  url "https://github.com/intel/tbb/archive/v2020.2.tar.gz"
-  version "2020_U2"
-  sha256 "4804320e1e6cbe3a5421997b52199e3c1a3829b2ecb6489641da4b8e32faf500"
+  url "https://github.com/intel/tbb/archive/v2020.3.tar.gz"
+  version "2020_U3"
+  sha256 "ebc4f6aa47972daed1f7bf71d100ae5bf6931c2e3144cf299c8cc7d041dca2f3"
   license "Apache-2.0"
 
   bottle do
