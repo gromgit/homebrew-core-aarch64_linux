3296f0573915475c293b5f62b6c71b76fc394523
Rui Chen <rui@meetup.com>
2020-09-19 21:39:21 -0400

    tomcat@8 8.5.58
    Closes #61419.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tomcat@8.rb
+++ Formula/tomcat@8.rb
@@ -1,9 +1,9 @@
 class TomcatAT8 < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-8/v8.5.57/bin/apache-tomcat-8.5.57.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.57/bin/apache-tomcat-8.5.57.tar.gz"
-  sha256 "2615839daf1899cd705f9b82f2df6bd21adb29a93a05afdea0a6192ce54025c4"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-8/v8.5.58/bin/apache-tomcat-8.5.58.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.58/bin/apache-tomcat-8.5.58.tar.gz"
+  sha256 "af53606de49ab01287127bbc366bebcc596a5cf3119b4817467142dbf18ac053"
   license "Apache-2.0"
 
   livecheck do
