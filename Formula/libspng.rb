4a2a6bda6f5ce82f9087325725e9b27006fd10af
Rui Chen <rui@meetup.com>
2020-09-20 01:35:39 -0400

    libspng 0.6.1
    Closes #61429.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libspng.rb
+++ Formula/libspng.rb
@@ -1,8 +1,8 @@
 class Libspng < Formula
   desc "C library for reading and writing PNG format files"
   homepage "https://libspng.org/"
-  url "https://github.com/randy408/libspng/archive/v0.6.0.tar.gz"
-  sha256 "81fea8d8a2e0c8aa51769605ad0e49a682e88697c6b5b60105f5c3806efaa3a3"
+  url "https://github.com/randy408/libspng/archive/v0.6.1.tar.gz"
+  sha256 "336856bea0216fe0ddc6cc584be5823cfd3a142e9d90d8e1635d2d2a5241f584"
   license "BSD-2-Clause"
 
   bottle do
@@ -30,10 +30,7 @@ class Libspng < Formula
     cp pkgshare/"example.c", testpath/"example.c"
     system ENV.cc, "example.c", "-L#{lib}", "-I#{include}", "-lspng", "-o", "example"
 
-    # example.c returns 73 ("no text chunk") for the test.png fixture
-    # this will probably be changed in v0.7,
-    # see: https://github.com/randy408/libspng/issues/109
-    output = shell_output("./example #{fixture}", 73)
+    output = shell_output("./example #{fixture}")
     assert_match "width: 8\nheight: 8\nbit depth: 1\ncolor type: 3 - indexed color\n" \
                  "compression method: 0\nfilter method: 0\ninterlace method: 0", output
   end
