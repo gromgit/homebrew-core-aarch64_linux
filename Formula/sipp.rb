bcec742df3e58725bc94d2d18376e1d0ad3846c3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-20 02:16:36 +0000

    sipp: update 3.6.1 bottle.

--- Formula/sipp.rb
+++ Formula/sipp.rb
@@ -7,10 +7,9 @@ class Sipp < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6e475cbb06a46ae80606b12897928b601b8280fc16e1467c395687a2a93317b1" => :catalina
-    sha256 "23616329b56c55eb022b055d31dadfdc79fafe5fc224cd342106a1a59afa46dd" => :mojave
-    sha256 "822893f07f76031fcd3c0410e2d682dc743c9ac1a9b438361dd1a6a9b6a767b8" => :high_sierra
-    sha256 "79f72467ed25cb8c6db260a9fa3e8f130058424b980d9daa07f79fce77159296" => :sierra
+    sha256 "b5363f025eb4ab69bd4a5d255396115d16ec5740a7de7cc810cffaed2b6e8b7a" => :catalina
+    sha256 "43d25e09e529be8802eca6fcc67ea3a9b50927acf71ecc3095a074b8f693afb5" => :mojave
+    sha256 "41183e24e41ad9d05017652d0e1ed94e3aeb74883daa165210eafc7f6e034ab7" => :high_sierra
   end
 
   depends_on "cmake" => :build
