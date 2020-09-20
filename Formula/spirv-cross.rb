a5dbe67a9e7e2224170c3c69330f7c599e9ac153
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-20 03:15:17 +0000

    spirv-cross: update 2020-09-17 bottle.

--- Formula/spirv-cross.rb
+++ Formula/spirv-cross.rb
@@ -8,9 +8,9 @@ class SpirvCross < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e23ad5f26f55793c665977deecea85f7d1ac92a9f80bef9c86eaf8195709007a" => :catalina
-    sha256 "33675bde9b1f2e5861d6c5b37f027ac9be2f9a1783d2efdc84f7756c483b085e" => :mojave
-    sha256 "51295b9f28b4bff7807d938c741c7a0c5a807d22808dd84df97457bbcde9f621" => :high_sierra
+    sha256 "6c7ebf3dcc65a7392247b7643f6f36461720ef8399c52bd05aaf79ed53af2123" => :catalina
+    sha256 "4da93b781991248cd8af56b41b84dc09a18f587a41e5a52b1f65f75a0be78afb" => :mojave
+    sha256 "971cd943944647d47c1a6def86faed5f011236915d4085a7b8b5317610f32be4" => :high_sierra
   end
 
   depends_on "cmake" => :build
