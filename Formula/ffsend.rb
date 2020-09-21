adf91f8d0fc4fd1fcbeb8436db9a910610ab2e3c
chenrui <chenrui333@gmail.com>
2020-09-21 12:36:50 +0000

    ffsend 0.2.66
    Closes #61481.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ffsend.rb
+++ Formula/ffsend.rb
@@ -1,8 +1,8 @@
 class Ffsend < Formula
   desc "Fully featured Firefox Send client"
   homepage "https://gitlab.com/timvisee/ffsend"
-  url "https://github.com/timvisee/ffsend/archive/v0.2.65.tar.gz"
-  sha256 "a07f1c45888df7ed061d2bb8c035a0b158b9d3c84d533900043407108ce953d6"
+  url "https://github.com/timvisee/ffsend/archive/v0.2.66.tar.gz"
+  sha256 "e769509bda8bf8b7f2d7dc17bd21f5af0398e2bd076721034a23f6c0ea2537b2"
   license "GPL-3.0"
 
   bottle do
