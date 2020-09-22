9a83701b3dbe2f9e3a6fae38769824f5c7463d4a
chenrui <chenrui333@gmail.com>
2020-09-22 22:36:18 +0000

    webify: add 1.5.0 bottle.

--- Formula/webify.rb
+++ Formula/webify.rb
@@ -6,6 +6,13 @@ class Webify < Formula
   license "MIT"
   head "https://github.com/beefsack/webify.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7b6543358b1c92e8e8cc71584ed52802a039c9327edc839dcc75216fbd23558c" => :catalina
+    sha256 "8a58b27bcb9d6f9cd611b8f7dfb6192f617854cfcaf8638b388f6dd88ec40f70" => :mojave
+    sha256 "9701f9952fb05880c48c5ca26d14807cf324c2210d4b45d0fb5408243d8d76cc" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
