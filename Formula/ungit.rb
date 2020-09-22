b3b7e8394458e0d4097f4fef79b5256e48ecc94c
Dawid Dziurla <dawidd0811@gmail.com>
2020-09-22 12:33:51 +0000

    ungit 1.5.11
    Closes #61524.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ungit.rb
+++ Formula/ungit.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Ungit < Formula
   desc "Easiest way to use Git. On any platform. Anywhere"
   homepage "https://github.com/FredrikNoren/ungit"
-  url "https://registry.npmjs.org/ungit/-/ungit-1.5.10.tgz"
-  sha256 "766824a8322b2220a42ad64e9bfae80bba2631de1a5b5ad031bea351f32400c7"
+  url "https://registry.npmjs.org/ungit/-/ungit-1.5.11.tgz"
+  sha256 "1f63fc4e6697396c61a01917474409c9a51c4ee120e4bb8bec2e4a177bc3f1e9"
   license "MIT"
 
   livecheck do
