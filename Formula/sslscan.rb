538906349fc7c495512634f98be2254d0fb90060
Rui Chen <rui@meetup.com>
2020-09-21 22:40:46 -0400

    sslscan 2.0.1

--- Formula/sslscan.rb
+++ Formula/sslscan.rb
@@ -1,8 +1,8 @@
 class Sslscan < Formula
   desc "Test SSL/TLS enabled services to discover supported cipher suites"
   homepage "https://github.com/rbsec/sslscan"
-  url "https://github.com/rbsec/sslscan/archive/2.0.0.tar.gz"
-  sha256 "f582c4b1c9ff6cadde4a3130a3f721866faf6048f5b1cddd1f696dc5a6fb7921"
+  url "https://github.com/rbsec/sslscan/archive/2.0.1.tar.gz"
+  sha256 "89208e59a8946f44e6d4e7c7d371ff3fdeb8db1f4c5324e1b42f4abd05ff28cc"
   license "GPL-3.0"
   head "https://github.com/rbsec/sslscan.git"
 
