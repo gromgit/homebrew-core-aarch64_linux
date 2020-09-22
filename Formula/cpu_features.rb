848798cc98cef5a323bf88f46b2393bdf4345f82
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-22 15:16:26 +0000

    cpu_features: update 0.5.0 bottle.

--- Formula/cpu_features.rb
+++ Formula/cpu_features.rb
@@ -7,10 +7,9 @@ class CpuFeatures < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "cf7f7d47468e2c0cf321c22c86adb2c734920559b1a63553b1688ed0e4a39b73" => :catalina
-    sha256 "b2736429ca74d23173266aab1d3ee6fea58ee963e0042391d803b26d322838d7" => :mojave
-    sha256 "0d8119f02b609f5cfcac19177bd669668686c549bb138fbe2a0aa391df7dc838" => :high_sierra
-    sha256 "d703ec98a4f4913ac50c872475e7c37e6c439326db607297fa14cd438e75f919" => :sierra
+    sha256 "d59ba9266e5071e5539fa644c3c3480f5fc4b1b1a2d535d8e82301c6de9a9e87" => :catalina
+    sha256 "9a8b3073f6fee8c19d073bd828aa8e7289f6360bc077551a1401ca92a79ebb39" => :mojave
+    sha256 "839ea1c38972d31f18e5a372972c6977398ffc6ab9872c0ed347a33f5a3e9c9c" => :high_sierra
   end
 
   depends_on "cmake" => :build
