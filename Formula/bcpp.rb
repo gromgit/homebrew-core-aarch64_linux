9511a196b4f71ad6169bf68fdc28f638167eb7c6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-23 21:15:41 +0000

    bcpp: update 20200922 bottle.

--- Formula/bcpp.rb
+++ Formula/bcpp.rb
@@ -13,11 +13,9 @@ class Bcpp < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "cc81540bae3c797979fdd5246a97479b685683ed4ec611f5e9a1f691147356af" => :catalina
-    sha256 "e27cfc6f4e0417a5ad0531f9b2dbf2630bc79d06404573c2a9a799717aa5dd4f" => :mojave
-    sha256 "25399ade6485e4272d264611a32db839905b56a3607f04ad9d66a5571469aa03" => :high_sierra
-    sha256 "339190468a41d319c161ab16933012517f68d11e5162d57b117bdc0220a51db0" => :sierra
-    sha256 "62ea91b7f94761da062c0f78757f48299bdd614ffba1cbc9e3e44794b305901e" => :el_capitan
+    sha256 "e7c25aa0608c77248f3414b49ebfa7808673b4afea1925e57323c9b1384d8c03" => :catalina
+    sha256 "75cfd0b4bf87b30217e8d36c29d083fd961425583e322b5fdfdf16f189009048" => :mojave
+    sha256 "7bd443a2afe3a8bccdc39c0744de2e7b3f9c6d45ff11030c9757e434d4767b85" => :high_sierra
   end
 
   def install
