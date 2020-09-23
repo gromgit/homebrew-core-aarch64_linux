6d07f1511405de67ddc8e409f844dd614b78b91e
Rui Chen <rui@meetup.com>
2020-09-22 22:00:05 -0400

    fff 2.2
    Closes #61553.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fff.rb
+++ Formula/fff.rb
@@ -1,8 +1,8 @@
 class Fff < Formula
   desc "Simple file manager written in bash"
   homepage "https://github.com/dylanaraps/fff"
-  url "https://github.com/dylanaraps/fff/archive/2.1.tar.gz"
-  sha256 "776870d11c022fa40468d5d582831c0ab5beced573489097deaaf5dd690e7eab"
+  url "https://github.com/dylanaraps/fff/archive/2.2.tar.gz"
+  sha256 "45f6e1091986c892ea45e1ac82f2d7f5417cfb343dc569d2625b5980e6bcfb62"
   license "MIT"
 
   bottle :unneeded
