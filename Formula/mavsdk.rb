e9b06cdda345d68341bc14eb77fe4aee4aefa9a2
Jonas Vautherin <jonas.vautherin@gmail.com>
2020-09-24 18:20:15 +0200

    mavsdk 0.31.0
    Closes #61612.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mavsdk.rb
+++ Formula/mavsdk.rb
@@ -2,8 +2,8 @@ class Mavsdk < Formula
   desc "API and library for MAVLink compatible systems written in C++11"
   homepage "https://mavsdk.mavlink.io"
   url "https://github.com/mavlink/MAVSDK.git",
-    tag:      "v0.30.1",
-    revision: "f2ff5146a4944e19086e536d947132bb072dfdf0"
+    tag:      "v0.31.0",
+    revision: "6dd94a16249ae9dd5252c532da6a60cde5194cb7"
   license "BSD-3-Clause"
 
   livecheck do
