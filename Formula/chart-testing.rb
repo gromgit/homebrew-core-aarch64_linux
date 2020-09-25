b85f728e614c81ff9ad4763f887a2dc961e5ec26
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-25 14:16:24 +0000

    chart-testing: update 3.1.0 bottle.

--- Formula/chart-testing.rb
+++ Formula/chart-testing.rb
@@ -8,10 +8,9 @@ class ChartTesting < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "b94f0fab8f35f89ce25ec4c8610af7310639899b1534b0907a4f96fb13c0c016" => :catalina
-    sha256 "58bfd0469d42422dc48b74cda82effc8f1baec442201a77ec2b8753c9810d5ae" => :mojave
-    sha256 "97661974290727c2832803f74bcf0277d01192adc2b65432714b0c376e01bb99" => :high_sierra
+    sha256 "2da98bb66be3b8bf89f22715b41ac5d34c575edf9dba3c03029f06088b312c59" => :catalina
+    sha256 "0cabedffe7f3b96dedaf3ffc12ebe3f669e66bb026a6a2dd503d338c54ff41e0" => :mojave
+    sha256 "c29b17cde85ef7b98ab5a590b43fa8cbabd8d7bba2115e46a9c0630c4ea9203f" => :high_sierra
   end
 
   depends_on "go" => :build
