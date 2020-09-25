4e94219f474acbabe850423714639ee5fca51e11
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-25 12:37:03 +0000

    libheif: update 1.9.1 bottle.

--- Formula/libheif.rb
+++ Formula/libheif.rb
@@ -7,9 +7,9 @@ class Libheif < Formula
 
   bottle do
     cellar :any
-    sha256 "9c0478e654c5c7da8dd014f9ac33ce0a11e47e5387e29f93d4f479a31c88c537" => :catalina
-    sha256 "482935bb3d124f89b03a3127d7fe2f5efa194b0ceb956751307c2aa7584d678f" => :mojave
-    sha256 "8989630a939edf69650c13089b33f6cdfb56346684a41d549fad39ea324d54c8" => :high_sierra
+    sha256 "4e6759dcfcedf91ca277b8c338f25f39884b2daacf997f14c9a82e8914cf9a9f" => :catalina
+    sha256 "39b1616553af121c49bafcbaf95a19fbdd6dc7ebcecbdfbe3c25dbbc097d2266" => :mojave
+    sha256 "18febe31fe9dfa094c745e7e3a45082bbf8c43d928891f7a1856a64ae35a89c6" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
