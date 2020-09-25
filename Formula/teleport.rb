582f011e2e795c8d13ca89840cdfc0332cc8191e
Sean Molenaar <smillerdev@me.com>
2020-09-25 11:39:34 +0200

    teleport 4.3.6
    Closes #61633.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/teleport.rb
+++ Formula/teleport.rb
@@ -1,8 +1,8 @@
 class Teleport < Formula
   desc "Modern SSH server for teams managing distributed infrastructure"
   homepage "https://gravitational.com/teleport"
-  url "https://github.com/gravitational/teleport/archive/v4.3.5.tar.gz"
-  sha256 "7a4081117e4a13bfbea0d1cc64aef6e8103c360551523b2abef0971712beb5a9"
+  url "https://github.com/gravitational/teleport/archive/v4.3.6.tar.gz"
+  sha256 "8ccbf622389306c131e7609185f5b53d17a6b8fc87755265e63ae389101e006b"
   license "Apache-2.0"
   head "https://github.com/gravitational/teleport.git"
 
