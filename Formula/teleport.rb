136a43943a8bb8dc47cba8f25bf0e73e29b3ffe1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-09-25 14:16:43 +0000

    teleport: update 4.3.6 bottle.

--- Formula/teleport.rb
+++ Formula/teleport.rb
@@ -13,9 +13,9 @@ class Teleport < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "25f58c4d14b1276df4b957b51edebb9cfae56083f6084cc831c29ea5e79720e6" => :catalina
-    sha256 "0b5c412839bf5f27fd70a752a2a28ffd1408dc30a5dd489e2aaf1ad7275d538c" => :mojave
-    sha256 "3c5f65eb7952139861314f3a7a571b66d03defb883e963d15746d78d5926b723" => :high_sierra
+    sha256 "9a861b12e388b76bb74df5f03caebc2c91165087a797caf766032d4c222fee42" => :catalina
+    sha256 "415016179a1286830109b6e70880903ca1d4373508609b3f9e84776b59266685" => :mojave
+    sha256 "b13d8b17a148ce9190dcd74d5acccb23deb8977635701d358520fd502b7c7c11" => :high_sierra
   end
 
   depends_on "go" => :build
