0b4cbb3c78a07baa7886d93f060e1976dbf2d0b5
Michka Popoff <michkapopoff@gmail.com>
2020-09-26 19:27:48 +0200

    libpthread-stubs 0.4 (new formula)

---
+++ Formula/libpthread-stubs.rb
@@ -0,0 +1,19 @@
+class LibpthreadStubs < Formula
+  desc "X.Org: pthread-stubs.pc"
+  homepage "https://www.x.org/"
+  url "https://xcb.freedesktop.org/dist/libpthread-stubs-0.4.tar.bz2"
+  sha256 "e4d05911a3165d3b18321cc067fdd2f023f06436e391c6a28dff618a78d2e733"
+  license "MIT"
+
+  depends_on "pkg-config"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "pkg-config", "--exists", "pthread-stubs"
+    assert_equal 0, $CHILD_STATUS.exitstatus
+  end
+end
