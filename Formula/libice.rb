cd183d1292e31bea642f956bf0c4e4f9c9d05e5a
Michka Popoff <michkapopoff@gmail.com>
2020-09-26 19:27:34 +0200

    libice 1.0.10 (new formula)

---
+++ Formula/libice.rb
@@ -0,0 +1,41 @@
+class Libice < Formula
+  desc "X.Org: Inter-Client Exchange Library"
+  homepage "https://www.x.org/"
+  url "https://www.x.org/archive/individual/lib/libICE-1.0.10.tar.bz2"
+  sha256 "6f86dce12cf4bcaf5c37dddd8b1b64ed2ddf1ef7b218f22b9942595fb747c348"
+  license "MIT"
+
+  depends_on "pkg-config" => :build
+  depends_on "xtrans" => :build
+  depends_on "xorgproto"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --enable-docs=no
+      --enable-specs=no
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include "X11/Xlib.h"
+      #include "X11/ICE/ICEutil.h"
+
+      int main(int argc, char* argv[]) {
+        IceAuthFileEntry entry;
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c"
+    assert_equal 0, $CHILD_STATUS.exitstatus
+  end
+end
