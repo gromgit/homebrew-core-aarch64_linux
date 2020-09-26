317edc9a306f645fd4f7122ba39f758290751aaf
Michka Popoff <michkapopoff@gmail.com>
2020-09-26 19:28:18 +0200

    libx11 1.6.9 (new formula)

---
+++ Formula/libx11.rb
@@ -0,0 +1,71 @@
+class Libx11 < Formula
+  desc "X.Org: Core X11 protocol client library"
+  homepage "https://www.x.org/"
+  url "https://www.x.org/archive/individual/lib/libX11-1.6.9.tar.bz2"
+  sha256 "9cc7e8d000d6193fa5af580d50d689380b8287052270f5bb26a5fb6b58b2bed1"
+  license "MIT"
+
+  depends_on "pkg-config" => :build
+  depends_on "util-macros" => :build
+  depends_on "xtrans" => :build
+  depends_on "libxcb"
+  depends_on "xorgproto"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --enable-unix-transport
+      --enable-tcp-transport
+      --enable-ipv6
+      --enable-local-transport
+      --enable-loadable-i18n
+      --enable-xthreads
+      --enable-specs=no
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <X11/Xlib.h>
+      #include <stdio.h>
+      int main() {
+        Display* disp = XOpenDisplay(NULL);
+        if (disp == NULL)
+        {
+          fprintf(stderr, "Unable to connect to display\\n");
+          return 7;
+        }
+
+        int screen_num = DefaultScreen(disp);
+        unsigned long background = BlackPixel(disp, screen_num);
+        unsigned long border = WhitePixel(disp, screen_num);
+        int width = 60, height = 40;
+        Window win = XCreateSimpleWindow(disp, DefaultRootWindow(disp), 0, 0, width, height, 2, border, background);
+        XSelectInput(disp, win, ButtonPressMask|StructureNotifyMask);
+        XMapWindow(disp, win); // display blank window
+
+        XGCValues values;
+        values.foreground = WhitePixel(disp, screen_num);
+        values.line_width = 1;
+        values.line_style = LineSolid;
+        GC pen = XCreateGC(disp, win, GCForeground|GCLineWidth|GCLineStyle, &values);
+        // draw two diagonal lines
+        XDrawLine(disp, win, pen, 0, 0, width, height);
+        XDrawLine(disp, win, pen, width, 0, 0, height);
+
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-o", "test", "-I#{include}", "-L#{lib}", "-lx11"
+    system "./test"
+    assert_equal 0, $CHILD_STATUS.exitstatus
+  end
+end
