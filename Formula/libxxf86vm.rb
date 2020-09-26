ef61feb25d1566ac8279845db564eb26eb4cd423
Michka Popoff <michkapopoff@gmail.com>
2020-09-26 19:34:39 +0200

    libxxf86vm 1.1.4 (new formula)

---
+++ Formula/libxxf86vm.rb
@@ -0,0 +1,40 @@
+class Libxxf86vm < Formula
+  desc "X.Org: XFree86-VidMode X extension"
+  homepage "https://www.x.org/"
+  url "https://www.x.org/archive/individual/lib/libXxf86vm-1.1.4.tar.bz2"
+  sha256 "afee27f93c5f31c0ad582852c0fb36d50e4de7cd585fcf655e278a633d85cd57"
+  license "MIT"
+
+  depends_on "pkg-config" => :build
+  depends_on "libx11"
+  depends_on "libxext"
+  depends_on "xorgproto"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include "X11/Xlib.h"
+      #include "X11/extensions/xf86vmode.h"
+
+      int main(int argc, char* argv[]) {
+        XF86VidModeModeInfo mode;
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c"
+    assert_equal 0, $CHILD_STATUS.exitstatus
+  end
+end
