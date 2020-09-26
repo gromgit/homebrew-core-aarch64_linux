8340bab05c5799ae91d84d497674f920cf683d6d
Rui Chen <chenrui333@gmail.com>
2020-09-26 11:59:32 -0400

    balena-cli 12.23.0
    Closes #61664.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/balena-cli.rb
+++ Formula/balena-cli.rb
@@ -4,8 +4,8 @@ class BalenaCli < Formula
   desc "Command-line tool for interacting with the balenaCloud and balena API"
   homepage "https://www.balena.io/docs/reference/cli/"
   # balena-cli should only be updated every 10 releases on multiples of 10
-  url "https://registry.npmjs.org/balena-cli/-/balena-cli-12.20.0.tgz"
-  sha256 "5cc5462071e8ede88f42b1838f22e386c69666987c20b83d10c1df2dea4de8cc"
+  url "https://registry.npmjs.org/balena-cli/-/balena-cli-12.23.0.tgz"
+  sha256 "c90e4a66701debb81c24d8d6442d19de072710c100ef6e96c9454d8245b2ece2"
   license "Apache-2.0"
 
   livecheck do
