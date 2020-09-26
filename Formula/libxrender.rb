f8da582001eba3cada61e6ddb8b87bd126619e2d
Michka Popoff <michkapopoff@gmail.com>
2020-09-26 19:32:37 +0200

    libxrender 0.9.10 (new formula)

---
+++ Formula/libxrender.rb
@@ -0,0 +1,39 @@
+class Libxrender < Formula
+  desc "X.Org: Library for the Render Extension to the X11 protocol"
+  homepage "https://www.x.org/"
+  url "https://www.x.org/archive/individual/lib/libXrender-0.9.10.tar.bz2"
+  sha256 "c06d5979f86e64cabbde57c223938db0b939dff49fdb5a793a1d3d0396650949"
+  license "MIT"
+
+  depends_on "pkg-config" => :build
+  depends_on "libx11"
+  depends_on "xorgproto"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include "X11/Xlib.h"
+      #include "X11/extensions/Xrender.h"
+
+      int main(int argc, char* argv[]) {
+        XRenderColor color;
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c"
+    assert_equal 0, $CHILD_STATUS.exitstatus
+  end
+end
