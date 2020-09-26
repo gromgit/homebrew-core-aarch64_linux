d0f726f928f0e14075e05368c4f05c8ef2f89365
Michka Popoff <michkapopoff@gmail.com>
2020-09-26 19:30:31 +0200

    libxext 1.3.4 (new formula)

---
+++ Formula/libxext.rb
@@ -0,0 +1,39 @@
+class Libxext < Formula
+  desc "X.Org: Library for common extensions to the X11 protocol"
+  homepage "https://www.x.org/"
+  url "https://www.x.org/archive/individual/lib/libXext-1.3.4.tar.bz2"
+  sha256 "59ad6fcce98deaecc14d39a672cf218ca37aba617c9a0f691cac3bcd28edf82b"
+  license "MIT"
+
+  depends_on "pkg-config" => :build
+  depends_on "libx11"
+  depends_on "xorgproto"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --enable-specs=no
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include "X11/extensions/shape.h"
+
+      int main(int argc, char* argv[]) {
+        XShapeEvent event;
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c"
+    assert_equal 0, $CHILD_STATUS.exitstatus
+  end
+end
