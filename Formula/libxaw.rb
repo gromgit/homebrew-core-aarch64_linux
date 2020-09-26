b183ce8350939a86cb3ebc088c9d771404d6a1f4
Michka Popoff <michkapopoff@gmail.com>
2020-09-26 19:28:43 +0200

    libxaw 1.0.13 (new formula)

---
+++ Formula/libxaw.rb
@@ -0,0 +1,42 @@
+class Libxaw < Formula
+  desc "X.Org: X Athena Widget Set"
+  homepage "https://www.x.org/"
+  url "https://www.x.org/archive/individual/lib/libXaw-1.0.13.tar.bz2"
+  sha256 "8ef8067312571292ccc2bbe94c41109dcf022ea5a4ec71656a83d8cce9edb0cd"
+  license "MIT"
+
+  depends_on "pkg-config" => :build
+  depends_on "libx11"
+  depends_on "libxext"
+  depends_on "libxmu"
+  depends_on "libxpm"
+  depends_on "libxt"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --enable-specs=no
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include "X11/Xaw/Text.h"
+
+      int main(int argc, char* argv[]) {
+        XawTextScrollMode mode;
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c"
+    assert_equal 0, $CHILD_STATUS.exitstatus
+  end
+end
