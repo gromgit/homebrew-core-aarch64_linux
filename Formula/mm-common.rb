334f92d399ca21a846aa08009659b2ad9c0ec6be
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-09-26 06:40:27 +0100

    mm-common 1.0.2
    Closes #61657.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/mm-common.rb
+++ Formula/mm-common.rb
@@ -1,9 +1,9 @@
 class MmCommon < Formula
   desc "Build utilities for C++ interfaces of GTK+ and GNOME packages"
   homepage "https://www.gtkmm.org/"
-  url "https://download.gnome.org/sources/mm-common/1.0/mm-common-1.0.1.tar.xz"
-  sha256 "28a2d775afbf05c5c957b24b220ed6e9b2023179b98ecde471d89e9754ea5ac9"
-  license "GPL-2.0"
+  url "https://download.gnome.org/sources/mm-common/1.0/mm-common-1.0.2.tar.xz"
+  sha256 "a2a99f3fa943cf662f189163ed39a2cfc19a428d906dd4f92b387d3659d1641d"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url :stable
