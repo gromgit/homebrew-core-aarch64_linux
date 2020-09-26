fc5eec660f810a181264b4fc9c641f70c68b7d6b
Michka Popoff <michkapopoff@gmail.com>
2020-09-26 19:35:41 +0200

    xtrans 1.4.0 (new formula)
    Closes #57995.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

---
+++ Formula/xtrans.rb
@@ -0,0 +1,40 @@
+class Xtrans < Formula
+  desc "X.Org: X Network Transport layer shared code"
+  homepage "https://www.x.org/"
+  url "https://www.x.org/archive/individual/lib/xtrans-1.4.0.tar.bz2"
+  sha256 "377c4491593c417946efcd2c7600d1e62639f7a8bbca391887e2c4679807d773"
+  license "MIT"
+
+  depends_on "pkg-config" => :build
+  depends_on "util-macros" => :build
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --enable-docs=no
+    ]
+
+    # Fedora systems do not provide sys/stropts.h
+    inreplace "Xtranslcl.c", "# include <sys/stropts.h>", "# include <sys/ioctl.h>"
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include "X11/Xtrans/Xtrans.h"
+
+      int main(int argc, char* argv[]) {
+        Xtransaddr addr;
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c"
+    assert_equal 0, $CHILD_STATUS.exitstatus
+  end
+end
