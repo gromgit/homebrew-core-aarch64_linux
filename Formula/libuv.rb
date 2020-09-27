61d9cbd31506ccd05465906928b5497d9f25d900
Caleb Xu <calebcenter@live.com>
2020-09-27 00:43:43 -0400

    libuv 1.40.0
    Closes #61687.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/libuv.rb
+++ Formula/libuv.rb
@@ -1,8 +1,8 @@
 class Libuv < Formula
   desc "Multi-platform support library with a focus on asynchronous I/O"
   homepage "https://github.com/libuv/libuv"
-  url "https://github.com/libuv/libuv/archive/v1.39.0.tar.gz"
-  sha256 "dc7b21f1bb7ef19f4b42c5ea058afabe51132d165da18812b70fb319659ba629"
+  url "https://github.com/libuv/libuv/archive/v1.40.0.tar.gz"
+  sha256 "70fe1c9ba4f2c509e8166c0ca2351000237da573bb6c82092339207a9715ba6b"
   license "MIT"
   head "https://github.com/libuv/libuv.git", branch: "v1.x"
 
