d9bce60886c8d042eced47532653cd733d4da8bc
Caleb Xu <calebcenter@live.com>
2020-09-27 00:31:57 -0400

    nvm 0.36.0
    Closes #61681.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nvm.rb
+++ Formula/nvm.rb
@@ -1,8 +1,8 @@
 class Nvm < Formula
   desc "Manage multiple Node.js versions"
   homepage "https://github.com/nvm-sh/nvm"
-  url "https://github.com/creationix/nvm/archive/v0.35.3.tar.gz"
-  sha256 "a88c8c1e920ca24c09a2f9f0733afa9d6ccf03fe068e9ffba488416d9710d4fb"
+  url "https://github.com/nvm-sh/nvm/archive/v0.36.0.tar.gz"
+  sha256 "4e9de23fedde992af9547bedb2bf55b74fa765e930170e65907e85d6b48ae069"
   license "MIT"
   head "https://github.com/nvm-sh/nvm.git"
 
