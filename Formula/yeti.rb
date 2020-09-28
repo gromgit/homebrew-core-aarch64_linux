c8a41a4142d0e8f3ec7e1ba6130fdad139a62acb
Dawid Dziurla <dawidd0811@gmail.com>
2020-09-28 08:18:59 +0000

    yeti: update 1.0 bottle.

--- Formula/yeti.rb
+++ Formula/yeti.rb
@@ -8,13 +8,9 @@ class Yeti < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5bb8d8a2c4ba29c5e4cab58c9c0ba9ad53ee19d48f1845885b49d8cf3f2e83f0" => :catalina
-    sha256 "5945caa7c11f1424c971fbd1df5c0e904526dc69b35897bab7c34fcd1a2895e9" => :mojave
-    sha256 "b9d0083c8220cb02f5b994998149904613bceb589b7fb265bb9964e5af7dbb18" => :high_sierra
-    sha256 "5298c83ec842d28b26bf4d1cec151e2ae674509c27778de0aeacaf365711e7b9" => :sierra
-    sha256 "268781d28a766896e5ef40dc5c364b8a40617e6c28a3c96b693eba473c56e660" => :el_capitan
-    sha256 "a1ef95834d3631f7d04e18fd90786b0c8b0f4e492abe6f665a64d0d31ee768c5" => :yosemite
-    sha256 "9dd1a7662b45c454c47075ee029c554bf2655423376bd373cde6e15e716e7677" => :mavericks
+    sha256 "4c352083ddfbf202777d4f8d52895ac2bcb556fd5cc2fc1f820a48bb773b823f" => :catalina
+    sha256 "9dffa6798409e7d40acf301547dc8508547331922eaa1b7365a0b04e020ae90f" => :mojave
+    sha256 "1c49573337d0ca872a060038e3c7e5496d02b025e442c062314d98a786ab708a" => :high_sierra
   end
 
   depends_on "ant" => :build
