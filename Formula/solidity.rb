70be7bff12e273f3bed377fba9aa19449e1a6425
chriseth <chris@ethereum.org>
2020-09-28 21:54:00 +0200

    solidity 0.7.2
    Created with `brew bump-formula-pr`.
    
    Closes #61736.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>

--- Formula/solidity.rb
+++ Formula/solidity.rb
@@ -1,8 +1,8 @@
 class Solidity < Formula
   desc "Contract-oriented programming language"
   homepage "https://solidity.readthedocs.io"
-  url "https://github.com/ethereum/solidity/releases/download/v0.7.1/solidity_0.7.1.tar.gz"
-  sha256 "c69205d902ac8dd66f5aeaa78a08e5411cdb26a0b9479fcf44f394b7008b484c"
+  url "https://github.com/ethereum/solidity/releases/download/v0.7.2/solidity_0.7.2.tar.gz"
+  sha256 "71628cc4ad19500e5f532819b4c8bc241040032c7ba9a635ee51a5c8fcf480c8"
   license all_of: ["GPL-3.0-or-later", "MIT", "BSD-3-Clause", "Apache-2.0", "CC0-1.0"]
 
   bottle do
