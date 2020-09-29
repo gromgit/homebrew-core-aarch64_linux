f3121b4c75c4c5e9e2a976e14aad0162f0044d9d
Zeke Gabrielse <ezekg@yahoo.com>
2020-09-29 15:12:44 -0500

    git-hound 1.0.0 (new formula)
    Closes #61764.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/git-hound.rb
@@ -0,0 +1,49 @@
+class GitHound < Formula
+  desc "Git plugin that prevents sensitive data from being committed"
+  homepage "https://github.com/ezekg/git-hound"
+  url "https://github.com/ezekg/git-hound/archive/1.0.0.tar.gz"
+  sha256 "32f79f470c790db068a23fd68e9763b3bedc84309a281b4c99b941d4f33f5763"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "-ldflags", "-X main.version=#{version}"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/git-hound -v")
+
+    (testpath/".githound.yml").write <<~EOS
+      warn:
+        - '(?i)user(name)?\W*[:=,]\W*.+$'
+      fail:
+        - '(?i)pass(word)?\W*[:=,]\W*.+$'
+      skip:
+        - 'skip-test.txt'
+    EOS
+
+    (testpath/"failure-test.txt").write <<~EOS
+      password="hunter2"
+    EOS
+
+    (testpath/"warn-test.txt").write <<~EOS
+      username="AzureDiamond"
+    EOS
+
+    (testpath/"skip-test.txt").write <<~EOS
+      password="password123"
+    EOS
+
+    (testpath/"pass-test.txt").write <<~EOS
+      foo="bar"
+    EOS
+
+    diff_cmd = "git diff /dev/null"
+
+    assert_match "failure", shell_output("#{diff_cmd} #{testpath}/failure-test.txt | #{bin}/git-hound sniff", 1)
+    assert_match "warning", shell_output("#{diff_cmd} #{testpath}/warn-test.txt | #{bin}/git-hound sniff")
+    assert_match "", shell_output("#{diff_cmd} #{testpath}/skip-test.txt | #{bin}/git-hound sniff")
+    assert_match "", shell_output("#{diff_cmd} #{testpath}/pass-test.txt | #{bin}/git-hound sniff")
+  end
+end
