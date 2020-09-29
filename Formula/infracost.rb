1d846984af4e57c6c7a601a41c189b50010bc448
Alistair Scott <ali.scott@gmail.com>
2020-09-29 21:10:26 +0100

    infracost: fix version and test (#4)
    Closes #61761.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/infracost.rb
+++ Formula/infracost.rb
@@ -17,63 +17,13 @@ class Infracost < Formula
   depends_on "terraform" => :test
 
   def install
-    system "go", "build", *std_go_args, "./cmd/infracost"
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = "-X github.com/infracost/infracost/pkg/version.Version=v#{version}"
+    system "go", "build", *std_go_args, "-ldflags", ldflags, "./cmd/infracost"
   end
 
   test do
-    (testpath/"ami.tf").write <<~EOS
-      provider "aws" {
-        region                      = "us-east-1"
-        s3_force_path_style         = true
-        skip_credentials_validation = true
-        skip_metadata_api_check     = true
-        skip_requesting_account_id  = true
-        access_key                  = "mock_access_key"
-        secret_key                  = "mock_secret_key"
-      }
-
-      resource "aws_instance" "web_app" {
-        ami           = "fake1"
-        instance_type = "t3.micro"
-
-        root_block_device {
-          volume_size = 15
-        }
-      }
-    EOS
-
-    expected = <<~EOS
-      [{
-        "name": "aws_instance.web_app",
-        "hourlyCost": "0.01245479452054795",
-        "monthlyCost": "9.0920000000000035",
-        "costComponents": [{
-          "name": "Compute (on-demand, t3.micro)",
-          "unit": "hours",
-          "hourlyQuantity": "1",
-          "monthlyQuantity": "730",
-          "price": "0.0104",
-          "hourlyCost": "0.0104",
-          "monthlyCost": "7.592"
-        }],
-        "subresources": [{
-          "name": "root_block_device",
-          "hourlyCost": "0.00205479452054795",
-          "monthlyCost": "1.5000000000000035",
-          "costComponents": [{
-            "name": "Storage",
-            "unit": "GB-months",
-            "hourlyQuantity": "0.0205479452054795",
-            "monthlyQuantity": "15",
-            "price": "0.1",
-            "hourlyCost": "0.00205479452054795",
-            "monthlyCost": "1.5"
-          }]
-        }]
-      }]
-    EOS
-
-    output = shell_output("#{bin}/infracost --no-color --tfdir . --output json")
-    assert_equal JSON.parse(expected), JSON.parse(output)
+    output = shell_output("#{bin}/infracost --no-color", 1)
+    assert_equal output.lines.first, "No INFRACOST_API_KEY environment variable is set.\n"
   end
 end
