b783e76d0123545f8e28ba9c102ce87d20ab3e04
Dawid Dziurla <dawidd0811@gmail.com>
2020-09-30 22:16:11 +0000

    cloudformation-guard: add 0.7.0 bottle.

--- Formula/cloudformation-guard.rb
+++ Formula/cloudformation-guard.rb
@@ -5,6 +5,13 @@ class CloudformationGuard < Formula
   sha256 "f285c76ca32ca671380f254d0ce01dc6e5e3d91ef7d6e4c7fc2d6abeb7520e79"
   license "Apache-2.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5d1af2c3d59f5a87feb0d86d51538c4a300c8c37fdce88faa668d63e42b67492" => :catalina
+    sha256 "5a3af034fb1c5c4729a2c1043e032d156531ade94a56f88349df32e43f340be0" => :mojave
+    sha256 "1d97514ed0c474825c6c0b334fa546f8dfbab107031d435b4325e523b46e5de9" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
