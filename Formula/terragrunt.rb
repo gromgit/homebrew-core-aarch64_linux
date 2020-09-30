39f1f341da8a65b5b856b74733c2b2b65d7cd2a3
chenrui <chenrui333@gmail.com>
2020-09-30 18:53:36 +0000

    terragrunt 0.25.2
    Closes #61784.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -1,8 +1,8 @@
 class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state"
   homepage "https://github.com/gruntwork-io/terragrunt"
-  url "https://github.com/gruntwork-io/terragrunt/archive/v0.25.1.tar.gz"
-  sha256 "6543d9b1a3cd0686176d5b89d5ec0e53dde9defc38dbe4670ae451c942ac8308"
+  url "https://github.com/gruntwork-io/terragrunt/archive/v0.25.2.tar.gz"
+  sha256 "f3d2008ba843e3ae604e067cdda59f24639e3d986abc5e1ecd6f064c0e4776d2"
   license "MIT"
 
   bottle do
