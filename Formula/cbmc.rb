42f96d8ebca3208dee0c0d874509ba69242dc3cb
Alexander Bayandin <a.bayandin@gmail.com>
2020-10-01 00:06:24 +0100

    cbmc 5.14.3
    Closes #61793.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cbmc.rb
+++ Formula/cbmc.rb
@@ -2,8 +2,8 @@ class Cbmc < Formula
   desc "C Bounded Model Checker"
   homepage "https://www.cprover.org/cbmc/"
   url "https://github.com/diffblue/cbmc.git",
-      tag:      "cbmc-5.14.1",
-      revision: "c21ede2ef8191facee70260f97ffe4ae5d954143"
+      tag:      "cbmc-5.14.3",
+      revision: "d70285a9fcbea2febd823e75ac72fcd04c535ca1"
   license "BSD-4-Clause"
 
   bottle do
@@ -18,12 +18,9 @@ class Cbmc < Formula
   depends_on "openjdk" => :build
 
   def install
-    system "git", "submodule", "update", "--init"
-
-    # Build CBMC
-    system "cmake", "-S.", "-Bbuild", *std_cmake_args
-    system "cmake", "--build", "build"
-    cd "build" do
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "cmake", "--build", "."
       system "make", "install"
     end
   end
