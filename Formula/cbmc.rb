cc33d5d4ed8be05035f71a36a5eea8172792045e
runner <runner@Mac-1601561318927.local>
2020-10-01 14:25:18 +0000

    cbmc 5.15.0
    Closes #61823.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cbmc.rb
+++ Formula/cbmc.rb
@@ -2,8 +2,8 @@ class Cbmc < Formula
   desc "C Bounded Model Checker"
   homepage "https://www.cprover.org/cbmc/"
   url "https://github.com/diffblue/cbmc.git",
-      tag:      "cbmc-5.14.3",
-      revision: "d70285a9fcbea2febd823e75ac72fcd04c535ca1"
+      tag:      "5.15.0",
+      revision: "94fb414c0b2dde60e000cdd687c12e4590f9a51f"
   license "BSD-4-Clause"
 
   bottle do
