9e21708ce356bb2a70846dc5401a7a4a08611d21
Pierre-Yves Ricau <py@squareup.com>
2020-10-01 11:01:02 -0700

    leakcanary-shark 2.5
    Closes #61833.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/leakcanary-shark.rb
+++ Formula/leakcanary-shark.rb
@@ -1,8 +1,8 @@
 class LeakcanaryShark < Formula
   desc "CLI Java memory leak explorer for LeakCanary"
   homepage "https://square.github.io/leakcanary/shark/"
-  url "https://github.com/square/leakcanary/releases/download/v2.4/shark-cli-2.4.zip"
-  sha256 "5f9854868873ac6c63da5903082fe4bc3e08a8e46fe5de1335a2122d8e827a9f"
+  url "https://github.com/square/leakcanary/releases/download/v2.5/shark-cli-2.5.zip"
+  sha256 "23154247a6020143ccbb143d426f61fffbd9b316ed50acfe671264d27836201c"
   license "Apache-2.0"
 
   bottle :unneeded
