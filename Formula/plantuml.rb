4ac305dddc618efba505263be79b748120d5c0ff
Rui Chen <chenrui333@gmail.com>
2020-10-02 01:09:47 -0400

    plantuml 1.2020.18
    Closes #61864.
    
    Signed-off-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://downloads.sourceforge.net/project/plantuml/1.2020.17/plantuml.1.2020.17.jar"
-  sha256 "e7bccf7d75f2a64f39f58c1628652d19c2d9a7e369d9a48c892641018004207f"
+  url "https://downloads.sourceforge.net/project/plantuml/1.2020.18/plantuml.1.2020.18.jar"
+  sha256 "a6e2b64a9207acb14f6f587f6e264fbe5e0fffa8cf645eadfe640abc9c9ac1ca"
   license "GPL-3.0-or-later"
   version_scheme 1
 
