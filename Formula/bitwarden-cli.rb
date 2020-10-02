fbb422fd894f53c4ce3e6c85a4b90d9f2d948d8a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-02 12:29:29 +0000

    bitwarden-cli: update 1.12.1 bottle.

--- Formula/bitwarden-cli.rb
+++ Formula/bitwarden-cli.rb
@@ -13,9 +13,9 @@ class BitwardenCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b4326ba5082b398b5fd6b11f8837f111756e02cfef54f6af587452dfbe5456f0" => :catalina
-    sha256 "d606f871bf2ccfe4f749fbfc796603fb8973df47444443605ae0feba7c7482b3" => :mojave
-    sha256 "175bb66acd1a979ba17c3d38f32edf995f5ed34a8c7f1188206ddfca0ed6c856" => :high_sierra
+    sha256 "05083e62dc125662f5a19765dc1a8721dfcbd161f77b7a75a0196e43a89f8f46" => :catalina
+    sha256 "ecf7d13078fb366613cdfb8bde52610b03fecced474bc52ee2d30ad0faeb7461" => :mojave
+    sha256 "b4dc63d64afaa3d36a50f6c46acc1b4131111c3999de99bbbc115319e33e7448" => :high_sierra
   end
 
   depends_on "node"
