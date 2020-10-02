c8826a66b976ad2a5ca94741353e0bf80f2fb51a
Rui Chen <chenrui333@gmail.com>
2020-10-02 15:07:42 -0400

    wskdeploy 1.1.0
    update url
    
    drop gopath
    
    Closes #61957.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wskdeploy.rb
+++ Formula/wskdeploy.rb
@@ -1,8 +1,8 @@
 class Wskdeploy < Formula
   desc "Apache OpenWhisk project deployment utility"
   homepage "https://openwhisk.apache.org/"
-  url "https://github.com/apache/openwhisk-wskdeploy/archive/1.0.0.tar.gz"
-  sha256 "74c02e8118a123cfad113dc75d5e7d256b18fb80ad9e27b2b95eb74b8677e483"
+  url "https://github.com/apache/openwhisk-wskdeploy/archive/1.1.0.tar.gz"
+  sha256 "caf1f147c363a7324ce0c5d9851794c5671f56712888004c0db644de8f2a2169"
   license "Apache-2.0"
 
   bottle do
@@ -13,17 +13,9 @@ class Wskdeploy < Formula
   end
 
   depends_on "go" => :build
-  depends_on "godep" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/apache/openwhisk-wskdeploy").install buildpath.children
-    cd "src/github.com/apache/openwhisk-wskdeploy" do
-      system "godep", "restore"
-      system "go", "build", "-o", bin/"wskdeploy",
-                   "-ldflags", "-X main.Version=#{version}"
-      prefix.install_metafiles
-    end
+    system "go", "build", *std_go_args, "-ldflags", "-X main.Version=#{version}"
   end
 
   test do
