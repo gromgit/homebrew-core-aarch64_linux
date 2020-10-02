da44151a95fa66f9f28bbd45f09facb3a209647b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-02 12:28:58 +0000

    kubeaudit: update 0.11.5 bottle.

--- Formula/kubeaudit.rb
+++ Formula/kubeaudit.rb
@@ -8,9 +8,9 @@ class Kubeaudit < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d65b5f9006a160152b0f6544744c450fcb727ad864452b9c54793e720fc8baaf" => :catalina
-    sha256 "5267e37f84caca5aa62d30dd488d5722cb421be8d626dd81e8f5eff150873015" => :mojave
-    sha256 "1756610bb72615d4c5077ec1e798e7222fcb7c883e2a00f5abdbb3ba79eed1c9" => :high_sierra
+    sha256 "394668c7ec1a212bf48765d5b4a434189968d493c23a8c04254b65b6058749cd" => :catalina
+    sha256 "c6d2fdb6945d9b6ed28d7e3a81fb2f3bc6c1f80243e6578b15cf169aea25dec3" => :mojave
+    sha256 "0c9ab243680baa0926eb9c3d3e554730e436907f8fa2c457c6f8abca055a48e3" => :high_sierra
   end
 
   depends_on "go" => :build
