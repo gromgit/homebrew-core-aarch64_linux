2832a35b8e6cf36c893bb20e68f67693ef6510a0
Rui Chen <chenrui333@gmail.com>
2020-10-02 01:29:24 -0400

    cfn-lint 0.37.0
    update deps
    
    Closes #61874.
    
    Signed-off-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cfn-lint.rb
+++ Formula/cfn-lint.rb
@@ -3,8 +3,8 @@ class CfnLint < Formula
 
   desc "Validate CloudFormation templates against the CloudFormation spec"
   homepage "https://github.com/aws-cloudformation/cfn-python-lint/"
-  url "https://files.pythonhosted.org/packages/33/22/973a311874a44c993abbbc4bee376aebd89c861a59e86c927720b79b396f/cfn-lint-0.36.1.tar.gz"
-  sha256 "101bf9024aa11fb5098d1b00c885b01cfefd5cf51e334c05519595873863c026"
+  url "https://files.pythonhosted.org/packages/29/f5/93257b28a1b0fc1164a557b46313d8d99ca11a81f6789ea51cafb7a28c20/cfn-lint-0.37.0.tar.gz"
+  sha256 "5178595b7ccbbafb8b2b6fe9d3bc6215007387145ddf29812ecee70bb8500801"
   license "MIT"
 
   livecheck do
@@ -31,13 +31,13 @@ class CfnLint < Formula
   end
 
   resource "boto3" do
-    url "https://files.pythonhosted.org/packages/80/83/00c4d3f512d63771456dfe8666e9214703235a66fe353f624da6c458c45f/boto3-1.15.2.tar.gz"
-    sha256 "7e22c4e21533d7c532e0136610b9cc45e3f81e57b738ff434a1f3d0a3b16e05e"
+    url "https://files.pythonhosted.org/packages/68/57/7961b290c2aee2dac088bf240537bfb4e3ec41c55456aed5a16c0b9aab4f/boto3-1.15.10.tar.gz"
+    sha256 "04ce4665dee58c89b0eee784bbb480fb4346a7baeb1d5611f676ae21fd3f553a"
   end
 
   resource "botocore" do
-    url "https://files.pythonhosted.org/packages/a1/92/5336c9b5beac8bded4c3f6ee408e88c193d3545e3a9a901d8918a533688c/botocore-1.18.2.tar.gz"
-    sha256 "7fcfc20921de61bd015d363226476c6f51f38846c2a93f67ee7c70b66f54e223"
+    url "https://files.pythonhosted.org/packages/3c/d9/01e6e2b36cb366d00621b970a028dde369a1403fdcf3d9f3cb81bbe7f951/botocore-1.18.10.tar.gz"
+    sha256 "33083dc87ba7b4dae8ecd4b019896a5e5cb2626808f52450b6a79def8a54023b"
   end
 
   resource "decorator" do
