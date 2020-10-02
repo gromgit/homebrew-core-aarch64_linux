f10dbe5018181b76545f14779e80c1dd34dbe332
Caleb Xu <calebcenter@live.com>
2020-10-02 02:34:20 -0400

    ansifilter 2.17

--- Formula/ansifilter.rb
+++ Formula/ansifilter.rb
@@ -1,8 +1,8 @@
 class Ansifilter < Formula
   desc "Strip or convert ANSI codes into HTML, (La)Tex, RTF, or BBCode"
   homepage "http://www.andre-simon.de/doku/ansifilter/ansifilter.html"
-  url "http://www.andre-simon.de/zip/ansifilter-2.16.tar.bz2"
-  sha256 "7fcd2fa3520bce2bd3834c299f533cbfb43a29a095c83e8fea372a383dfbbaf2"
+  url "http://www.andre-simon.de/zip/ansifilter-2.17.tar.bz2"
+  sha256 "5985beb39c960a3cb5b0e70d6a121687043dc8458e5783777ff250303cccc42f"
   license "GPL-3.0"
 
   livecheck do
