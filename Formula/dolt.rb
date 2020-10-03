bee15bf4ceea3653d0ed7deff4b44ac507c2c5e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-03 01:26:13 +0000

    dolt: update 0.20.2 bottle.

--- Formula/dolt.rb
+++ Formula/dolt.rb
@@ -12,9 +12,9 @@ class Dolt < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "71cae90e3fc61f1907ece7df33086c5071af334a031b53a72d91766399edae70" => :catalina
-    sha256 "1de6745eab46bd21c0d697a69d17634b2b181bbc24d03216d6928232a6e20260" => :mojave
-    sha256 "f8561ab3c958b226e087fe4752a4ddcb04122f35b3ff53e6df8e2a42b809a494" => :high_sierra
+    sha256 "e9165578769716455c70bfd884298869106a7e9d2e3e1050fadd5e328bacf888" => :catalina
+    sha256 "de9df4d21f63cb7e44f59fc399b6c34d63fea249a6465c25f867c02500b92ef7" => :mojave
+    sha256 "e212e2142fd2a5b97fdfdebf1c251e40025634526d89fbffe5f7afa0aa0c2c61" => :high_sierra
   end
 
   depends_on "go" => :build
