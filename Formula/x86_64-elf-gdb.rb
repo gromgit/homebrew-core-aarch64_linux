93065293aac0ea198e465f1ac256d86626004765
Jonathan Chang <me@jonathanchang.org>
2020-10-03 05:56:52 +0000

    x86_64-elf-gdb: add 9.2 bottle.

--- Formula/x86_64-elf-gdb.rb
+++ Formula/x86_64-elf-gdb.rb
@@ -5,6 +5,12 @@ class X8664ElfGdb < Formula
   sha256 "360cd7ae79b776988e89d8f9a01c985d0b1fa21c767a4295e5f88cb49175c555"
   license "GPL-3.0-or-later"
 
+  bottle do
+    sha256 "dfe4806259f346a687fdd33e04c0380a3e29042359d0035649bc921197482472" => :catalina
+    sha256 "e791fd5b2c8bdf7c64574615dc0e5aeea5022b5f18beb2a92586ef777b999578" => :mojave
+    sha256 "08a7d238e78d7cda369afbf7e00e5e778a6d089c7c28713da8eb81692525c849" => :high_sierra
+  end
+
   depends_on "python@3.8"
   depends_on "xz"
 
