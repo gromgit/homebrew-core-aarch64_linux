e1c60bef029902e6b4174e59896d6b072253c721
Jaan Janesmae <git@janesmae.com>
2020-10-03 04:28:56 +0300

    jetty: add license
    Update Formula/jetty.rb
    
    Co-authored-by: Rylan Polster <rslpolster@gmail.com>

--- Formula/jetty.rb
+++ Formula/jetty.rb
@@ -4,6 +4,7 @@ class Jetty < Formula
   url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-distribution/9.4.32.v20200930/jetty-distribution-9.4.32.v20200930.tar.gz"
   version "9.4.32.v20200930"
   sha256 "64e5fe203347a4b4f5b5a828f9c241b2f13f154b56babccd533513991fd156c2"
+  license any_of: ["Apache-2.0", "EPL-1.0"]
 
   livecheck do
     url "https://www.eclipse.org/jetty/download.html"
