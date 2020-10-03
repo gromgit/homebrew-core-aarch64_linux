d7d8324f0c48640a6b9acf70e4f3442a6ccce7fb
Jaan Janesmae <git@janesmae.com>
2020-10-03 04:32:03 +0300

    jetty-runner: add license
    Add license reference to jetty-runner
    
    Update jetty-runner.rb
    
    Jetty is released under a dual license
    
    Closes #61994.
    
    Signed-off-by: Jonathan Chang <me@jonathanchang.org>

--- Formula/jetty-runner.rb
+++ Formula/jetty-runner.rb
@@ -4,6 +4,7 @@ class JettyRunner < Formula
   url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-runner/9.4.32.v20200930/jetty-runner-9.4.32.v20200930.jar"
   version "9.4.32.v20200930"
   sha256 "a1ec09f941f6baf6974d7dfb16bbbdf994a021b00fcbee69135eda482e464531"
+  license any_of: ["Apache-2.0", "EPL-1.0"]
 
   livecheck do
     url "https://www.eclipse.org/jetty/download.html"
