8585e1d8c0d6013b9fdfa40c51c30dd3beaf744c
Rui Chen <chenrui333@gmail.com>
2020-10-02 22:15:59 -0400

    rom-tools 0.225
    Closes #62005.
    
    Signed-off-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rom-tools.rb
+++ Formula/rom-tools.rb
@@ -1,9 +1,9 @@
 class RomTools < Formula
   desc "Tools for Multiple Arcade Machine Emulator"
   homepage "https://mamedev.org/"
-  url "https://github.com/mamedev/mame/archive/mame0224.tar.gz"
-  version "0.224"
-  sha256 "3518e71ec20fbeac8ebe93f8ec856078b8288e19f0d7cb38959d4bde30cd2810"
+  url "https://github.com/mamedev/mame/archive/mame0225.tar.gz"
+  version "0.225"
+  sha256 "ca4d5a429d72b30fd2bdf60350e490c7de4ac64b1e0dafcf38450f8ba84a1a95"
   license "GPL-2.0-or-later"
   head "https://github.com/mamedev/mame.git"
 
