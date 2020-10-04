e3ea994687fdd6aa1e9d3749de802ec6eefd2ecb
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-10-04 19:12:29 +0000

    libxdmcp: add 1.1.3 bottle.

--- Formula/libxdmcp.rb
+++ Formula/libxdmcp.rb
@@ -5,6 +5,13 @@ class Libxdmcp < Formula
   sha256 "20523b44aaa513e17c009e873ad7bbc301507a3224c232610ce2e099011c6529"
   license "MIT"
 
+  bottle do
+    cellar :any
+    sha256 "123c77fba2179591f3c1588808f33d231e9e04d8a91c99f6684d2c7eb64626b0" => :catalina
+    sha256 "1684eb0ed9e92430971293f58347b9b6de899998bf03be9a19e21f69db65b53f" => :mojave
+    sha256 "bff6a7f6da6c59277ca41503e66d6b778b6f89c2d1fc9047fde56ae028e3cda4" => :high_sierra
+  end
+
   depends_on "pkg-config" => :build
   depends_on "xorgproto"
 
