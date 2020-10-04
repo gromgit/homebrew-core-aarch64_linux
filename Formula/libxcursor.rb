67b4910d27cb02a0bb0d27fd1518f6c2b4691de9
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-10-04 19:12:29 +0000

    libxcursor: add 1.2.0 bottle.

--- Formula/libxcursor.rb
+++ Formula/libxcursor.rb
@@ -5,6 +5,13 @@ class Libxcursor < Formula
   sha256 "3ad3e9f8251094af6fe8cb4afcf63e28df504d46bfa5a5529db74a505d628782"
   license "MIT"
 
+  bottle do
+    cellar :any
+    sha256 "c8c48a2d78f7a5d23c1a1322ceac267d1d50ae8b7f1ad93c7a8b0e510afdf39e" => :catalina
+    sha256 "fe9c7b74829d92a0330caa49aeaf03d52213f2d8671fe8a4840b2700fa1cb863" => :mojave
+    sha256 "ac83b3e1c320f65c0dd7d66ef03abb3a5163ee8cb4b8932e14f008ae4cce5126" => :high_sierra
+  end
+
   depends_on "pkg-config" => :build
   depends_on "util-macros" => :build
   depends_on "libx11"
