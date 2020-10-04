854508d21ae6bc213ea26d4df4a01b36db135111
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-10-04 19:12:28 +0000

    libxvmc: add 1.0.12 bottle.

--- Formula/libxvmc.rb
+++ Formula/libxvmc.rb
@@ -5,6 +5,13 @@ class Libxvmc < Formula
   sha256 "6b3da7977b3f7eaf4f0ac6470ab1e562298d82c4e79077765787963ab7966dcd"
   license "MIT"
 
+  bottle do
+    cellar :any
+    sha256 "25bf67d5ac709c8a1551f38dc7da22bc5a2007b1a0d56589e96f172d35248f7e" => :catalina
+    sha256 "aa97b7fa2fa9c9a6df4cdd7bd939b2febfe870dd3feed76214d9f41789d50ab8" => :mojave
+    sha256 "b00b2e0ba733b3cd0ad8979cce06353e13a9474c5be32e53a467b8308b78f99a" => :high_sierra
+  end
+
   depends_on "pkg-config" => :build
   depends_on "util-macros" => :build
   depends_on "xorgproto" => :build
