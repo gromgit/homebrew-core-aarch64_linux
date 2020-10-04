ae5df5d4d79552f1e505cc489f188b7338ba29de
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-10-04 12:06:15 +0100

    vips: fix build with gobject-introspection 1.66.x
    Closes #61827.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/vips.rb
+++ Formula/vips.rb
@@ -4,6 +4,7 @@ class Vips < Formula
   url "https://github.com/libvips/libvips/releases/download/v8.10.1/vips-8.10.1.tar.gz"
   sha256 "e089bb4f73e1dce866ae53e25604ea4f94535488a45bb4f633032e1d2f4e2dda"
   license "LGPL-2.1-or-later"
+  revision 1
 
   bottle do
     sha256 "d2a0d29e9b37b9f396d9f21d5483ac433d83de92335a87651e20dc0368fb26f4" => :catalina
@@ -43,6 +44,11 @@ class Vips < Formula
     depends_on "gobject-introspection"
   end
 
+  # Reported upstream at https://github.com/libvips/libvips/issues/1836
+  # This patch is a reworked version of https://github.com/libvips/libvips/commit/e3181e05799fa1e51e1a4f8fc6884e3f9c3de765
+  # To be removed when the next update is released
+  patch :DATA
+
   def install
     # mozjpeg needs to appear before libjpeg, otherwise it's not used
     ENV.prepend_path "PKG_CONFIG_PATH", Formula["mozjpeg"].opt_lib/"pkgconfig"
@@ -71,3 +77,44 @@ class Vips < Formula
     assert_equal "", shell_output(cmd)
   end
 end
+
+__END__
+diff --git a/libvips/iofuncs/error.c b/libvips/iofuncs/error.c
+index 165623843..746742cb3 100644
+--- a/libvips/iofuncs/error.c
++++ b/libvips/iofuncs/error.c
+@@ -71,7 +71,7 @@
+ #endif /*OS_WIN32*/
+
+ /**
+- * SECTION: error
++ * SECTION: errors
+  * @short_description: error messages and error handling
+  * @stability: Stable
+  * @include: vips/vips.h
+diff --git a/libvips/iofuncs/rect.c b/libvips/iofuncs/rect.c
+index 79ae69f76..9038efb9b 100644
+--- a/libvips/iofuncs/rect.c
++++ b/libvips/iofuncs/rect.c
+@@ -45,7 +45,7 @@
+ #include <vips/vips.h>
+
+ /**
+- * SECTION: rect
++ * SECTION: rectangle
+  * @short_description: the VIPS rectangle class
+  * @stability: Stable
+  * @see_also: <link linkend="VipsRegion">region</link>
+diff --git a/libvips/resample/interpolate.c b/libvips/resample/interpolate.c
+index 5ec50b742..d295ad9fa 100644
+--- a/libvips/resample/interpolate.c
++++ b/libvips/resample/interpolate.c
+@@ -63,7 +63,7 @@
+ #include <vips/internal.h>
+
+ /**
+- * SECTION: interpolate
++ * SECTION: interpolator
+  * @short_description: various interpolators: nearest, bilinear, and
+  * some non-linear
+  * @stability: Stable
