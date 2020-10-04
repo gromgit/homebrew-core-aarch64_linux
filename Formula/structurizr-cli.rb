0ddc610efdfdedc676805e87e63c7b3fa214eee2
Gilberto Taccari <gilberto.taccari@gmail.com>
2020-10-04 14:58:09 +0200

    structurizr-cli 1.4.4 (new formula)
    Closes #62064.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/structurizr-cli.rb
@@ -0,0 +1,25 @@
+class StructurizrCli < Formula
+  desc "Command-line utility for Structurizr"
+  homepage "https://structurizr.com"
+  url "https://github.com/structurizr/cli/releases/download/v1.4.4/structurizr-cli-1.4.4.zip"
+  sha256 "ac173013c397fd2180a2b8134e29f8dfd3e0884418252433bcc9982e15b03909"
+  license "Apache-2.0"
+
+  bottle :unneeded
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "structurizr-cli-#{version}.jar"
+    bin.write_jar_script libexec/"structurizr-cli-#{version}.jar", "structurizr-cli"
+  end
+
+  test do
+    expected_output = <<~EOS.strip
+      Structurizr CLI v#{version}
+      Usage: structurizr push|pull|export [options]
+    EOS
+    result = pipe_output("#{bin}/structurizr-cli").strip
+    assert_equal result, expected_output
+  end
+end
