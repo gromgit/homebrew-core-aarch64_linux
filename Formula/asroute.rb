899c95cbe78d1364021de9f42d08b070c6700085
Dawid Dziurla <dawidd0811@gmail.com>
2020-10-04 08:54:31 +0000

    asroute: add 0.1.0 bottle.

--- Formula/asroute.rb
+++ Formula/asroute.rb
@@ -6,6 +6,13 @@ class Asroute < Formula
   license "MIT"
   head "https://github.com/stevenpack/asroute.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "873d80ef73a84598637218e467a0887477e113a9f26d8dd1f1a4b6a4571b11b8" => :catalina
+    sha256 "6dbd83956cb0d73b74fd8fa6706206c7d9701eeb6a44f0e6eebcaebd9b96fbc2" => :mojave
+    sha256 "77fd60fff4aa4abf7e9fd7bb7e14961b3eaab1aae0f074a318d874ecd869d32b" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
