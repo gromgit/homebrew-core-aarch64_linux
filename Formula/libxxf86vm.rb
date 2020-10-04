f58b5b562c1b9a3891b23bf99760f4e1d8ef1db2
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-10-04 19:12:28 +0000

    libxxf86vm: add 1.1.4 bottle.

--- Formula/libxxf86vm.rb
+++ Formula/libxxf86vm.rb
@@ -5,6 +5,13 @@ class Libxxf86vm < Formula
   sha256 "afee27f93c5f31c0ad582852c0fb36d50e4de7cd585fcf655e278a633d85cd57"
   license "MIT"
 
+  bottle do
+    cellar :any
+    sha256 "0a4d6a9d0b98bd8b8cd2aa2025b6ee80a19ffca34744fed599ef0e754d9e810b" => :catalina
+    sha256 "c89b765023eb0b6820cbf776d6af714363dc6ebd1ea000e1e53a99fa79be4a0d" => :mojave
+    sha256 "37cf2b440d8d4e7cda6aa70d070cd45427cebc61781b6c1b3aec153bf82d638d" => :high_sierra
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libx11"
   depends_on "libxext"
