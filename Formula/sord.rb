3a2d29f222b7110df86a1d71e3cf3bf8ce48846f
Jeff Gerlach <jgerlach@nodecentric.com>
2020-10-05 11:11:50 -0600

    sord 0.16.6
    Closes #62139.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sord.rb
+++ Formula/sord.rb
@@ -1,8 +1,8 @@
 class Sord < Formula
   desc "C library for storing RDF data in memory"
   homepage "https://drobilla.net/software/sord/"
-  url "https://download.drobilla.net/sord-0.16.4.tar.bz2"
-  sha256 "b15998f4e7ad958201346009477d6696e90ee5d3e9aff25e7e9be074372690d7"
+  url "https://download.drobilla.net/sord-0.16.6.tar.bz2"
+  sha256 "667af27a76085336d3865d087c1bb31a9d5f85b3359bcb27112dc43659229d44"
   license "ISC"
 
   livecheck do
