23e104ad2e4a9c57f2d3ff96abda7e772934d908
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-10-05 11:34:49 +0000

    trunk: add 0.6.0 bottle.

--- Formula/trunk.rb
+++ Formula/trunk.rb
@@ -6,6 +6,13 @@ class Trunk < Formula
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/thedodd/trunk.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e30a89f19b89e892613e044bc11b7297c4a4e319cacb5f9738dc26a0f4f6c9a7" => :catalina
+    sha256 "4d60c3565e55e1b72914f32a9a7fd546da25e852ba9fe1fadbcecd9c1b58c313" => :mojave
+    sha256 "1951420ea7503d12523b1497deea5655737c7b1b95ca0b70696fec6457b715df" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
