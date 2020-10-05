a98104b262acacfed2e32f29a17a477c9c77c8a7
Max Eisner <4730112+max-ae@users.noreply.github.com>
2020-10-05 10:08:41 +0200

    bat: added zsh completion
    Closes #62102.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bat.rb
+++ Formula/bat.rb
@@ -24,6 +24,7 @@ class Bat < Formula
     assets_dir = Dir["target/release/build/bat-*/out/assets"].first
     man1.install "#{assets_dir}/manual/bat.1"
     fish_completion.install "#{assets_dir}/completions/bat.fish"
+    zsh_completion.install "#{assets_dir}/completions/bat.zsh" => "_bat"
   end
 
   test do
