dad37571063fd2d76a76a7adb48eac932a9b9d75
Samit Shaikh <shaikhsamit@live.com>
2020-10-05 17:41:02 +0800

    lorem: add license
    Closes #62108.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lorem.rb
+++ Formula/lorem.rb
@@ -3,7 +3,7 @@ class Lorem < Formula
   homepage "https://github.com/per9000/lorem"
   url "https://github.com/per9000/lorem/archive/v0.8.0.tar.gz"
   sha256 "3eec439d616a044e61a6733730b1fc009972466f869dae358991f95abd57e8b3"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
   head "https://github.com/per9000/lorem.git"
 
   bottle :unneeded
