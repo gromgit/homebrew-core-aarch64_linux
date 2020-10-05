5e3232f14c0b09a9ed16d7a13af4e84c68c90530
Aaron van Geffen <aaron@aaronweb.net>
2020-10-05 23:58:00 +0200

    nlohmann-json: include json_fwd.hpp
    Closes #62150.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nlohmann-json.rb
+++ Formula/nlohmann-json.rb
@@ -4,6 +4,7 @@ class NlohmannJson < Formula
   url "https://github.com/nlohmann/json/archive/v3.9.1.tar.gz"
   sha256 "4cf0df69731494668bdd6460ed8cb269b68de9c19ad8c27abc24cd72605b2d5b"
   license "MIT"
+  revision 1
   head "https://github.com/nlohmann/json.git", branch: "develop"
 
   bottle do
@@ -17,7 +18,7 @@ class NlohmannJson < Formula
 
   def install
     mkdir "build" do
-      system "cmake", "..", "-DJSON_BuildTests=OFF", *std_cmake_args
+      system "cmake", "..", "-DJSON_BuildTests=OFF", "-DJSON_MultipleHeaders=ON", *std_cmake_args
       system "make", "install"
     end
   end
