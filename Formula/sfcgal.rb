8913720dc5c4cb7e4cdfcf6b606eba16f0acb4f1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-06 10:18:22 +0000

    sfcgal: update 1.3.9 bottle.

--- Formula/sfcgal.rb
+++ Formula/sfcgal.rb
@@ -6,9 +6,9 @@ class Sfcgal < Formula
   license "LGPL-2.0+"
 
   bottle do
-    sha256 "f78f5ccb5b6feb0e7530770d3411149e4335ead8a2bcd08e275cbe7748b3c396" => :catalina
-    sha256 "39a00ba2c7681850a601053adcfd8f920e2037aee5ebe9abf0f9a1b89ac674a1" => :mojave
-    sha256 "86c543cb703b1d7885045354917e7da1a1fa3e52ca6749359f64ea736af74aa0" => :high_sierra
+    sha256 "8cc583e706bc2755d61c362afbdd0cc627c6126113d4d6c5c0cc1f1bb62e8dfa" => :catalina
+    sha256 "4137e3e9b3058a0d05a1ef9a357f0b22d4e0cf57ea6c525b3c4d7e3741a6ba86" => :mojave
+    sha256 "cddd030fa1e55f74ee70068d2ac5b6c748def91794a82cad87a3025078cb2f78" => :high_sierra
   end
 
   depends_on "cmake" => :build
