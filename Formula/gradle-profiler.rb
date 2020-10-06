13252df4007f8a012bb5fe83d3605c0ddeec2af3
chenrui <chenrui333@gmail.com>
2020-10-06 16:15:27 +0000

    gradle-profiler 0.15.0

--- Formula/gradle-profiler.rb
+++ Formula/gradle-profiler.rb
@@ -1,8 +1,8 @@
 class GradleProfiler < Formula
   desc "Profiling and benchmarking tool for Gradle builds"
   homepage "https://github.com/gradle/gradle-profiler/"
-  url "https://repo.gradle.org/gradle/ext-releases-local/org/gradle/profiler/gradle-profiler/0.14.0/gradle-profiler-0.14.0.zip"
-  sha256 "0a47504895eecff7cb89fd79efeff3c406f4ccb3d0af49a6749eecb1178e1c1a"
+  url "https://repo.gradle.org/gradle/ext-releases-local/org/gradle/profiler/gradle-profiler/0.15.0/gradle-profiler-0.15.0.zip"
+  sha256 "0cd279754502f796ea4bc684decd201cd8eea67566b3a6ef8b55f84cc4021d34"
   license "Apache-2.0"
 
   bottle :unneeded
