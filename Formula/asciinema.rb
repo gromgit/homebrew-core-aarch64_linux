aea8769d3a6c8b315f9afe56968da1ad80561507
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-06 21:55:34 +0200

    asciinema: migrate to python@3.9
    Closes #62210.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/asciinema.rb
+++ Formula/asciinema.rb
@@ -6,7 +6,7 @@ class Asciinema < Formula
   url "https://files.pythonhosted.org/packages/a7/71/771c859795e02c71c187546f34f7535487b97425bc1dad1e5f6ad2651357/asciinema-2.0.2.tar.gz"
   sha256 "32f2c1a046564e030708e596f67e0405425d1eca9d5ec83cd917ef8da06bc423"
   license "GPL-3.0"
-  revision 2
+  revision 3
   head "https://github.com/asciinema/asciinema.git"
 
   livecheck do
@@ -21,7 +21,7 @@ class Asciinema < Formula
     sha256 "1a941e2c0594a7c0a07c66f02c411e121ed2b3e869f3f015c6e69cb4fd92daba" => :high_sierra
   end
 
-  depends_on "python@3.8"
+  depends_on "python@3.9"
 
   def install
     virtualenv_install_with_resources
