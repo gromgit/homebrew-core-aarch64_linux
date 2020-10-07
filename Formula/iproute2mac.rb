35241ef74565ae326775613b055d4a8f9489abee
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-07 14:04:37 +0200

    iproute2mac: migrate to python@3.9
    Closes #62335.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/iproute2mac.rb
+++ Formula/iproute2mac.rb
@@ -6,10 +6,11 @@ class Iproute2mac < Formula
   url "https://github.com/brona/iproute2mac/releases/download/v1.3.0/iproute2mac-1.3.0.tar.gz"
   sha256 "3fefce6b0f5e166355fdb04934cbdd906211b64e5adb6a385469696dc51233b7"
   license "MIT"
+  revision 1
 
   bottle :unneeded
 
-  depends_on "python@3.8"
+  depends_on "python@3.9"
 
   def install
     bin.install "src/ip.py" => "ip"
