bcb673938f17f1d832654bcf4f832147411ab238
FX Coudert <fxcoudert@gmail.com>
2020-10-07 20:01:54 +0000

    googler: update 4.2_1 bottle.

--- Formula/googler.rb
+++ Formula/googler.rb
@@ -11,9 +11,9 @@ class Googler < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0689e822b6428b12c88c4d8a54775562e360a60e298cbea02e4bbfc42f12ffc9" => :catalina
-    sha256 "0689e822b6428b12c88c4d8a54775562e360a60e298cbea02e4bbfc42f12ffc9" => :mojave
-    sha256 "0689e822b6428b12c88c4d8a54775562e360a60e298cbea02e4bbfc42f12ffc9" => :high_sierra
+    sha256 "ab7dc8c32567630c5eb8348dd980024a0e627c39770f465f8f4bc5b7f2333d2f" => :catalina
+    sha256 "2dbb353381599066e977437e053a751f0ee7d09de78c1837040d1eb38f29ed33" => :mojave
+    sha256 "adb6f377a2dc1af03609a799edb3c94b81602c179f76ed0ea1837a37a17a25c9" => :high_sierra
   end
 
   depends_on "python@3.9"
