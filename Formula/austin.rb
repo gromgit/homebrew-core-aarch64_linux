2aab17262ea43c332adffa5e1091f79b5566fcd8
FX Coudert <fxcoudert@gmail.com>
2020-10-07 19:48:25 +0000

    austin: update 1.0.1_1 bottle.

--- Formula/austin.rb
+++ Formula/austin.rb
@@ -9,9 +9,9 @@ class Austin < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d54d25854d57ba21937ee4cf57bf9948f084e38afb19723150c787e2742697c2" => :catalina
-    sha256 "7e87587953588d5f50dbdaa09d731653f1fa354913da4bcc43a34b505cfde2fa" => :mojave
-    sha256 "700f3399a63c70da0b0ee0a3be9d64a886ea503a7ea9192062998a63d8a7f23b" => :high_sierra
+    sha256 "cbf21f93a97f0f3e56c63b76f610a6294395c3a37d760b2da643f66f20c85b11" => :catalina
+    sha256 "0387ff648979fee932f28c6c6def6806eb579520b93f0e4e24e8f0385a231131" => :mojave
+    sha256 "25572af19bbac744c2603efa8e8b82ba220e34a3866d4907b7823ac31857beae" => :high_sierra
   end
 
   depends_on "autoconf" => :build
