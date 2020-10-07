c3960051e3c19fd4db43b270b01c91a748fe764f
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-07 14:42:36 +0200

    x86_64-elf-gdb: migrate to python@3.9

--- Formula/x86_64-elf-gdb.rb
+++ Formula/x86_64-elf-gdb.rb
@@ -4,6 +4,7 @@ class X8664ElfGdb < Formula
   url "https://ftp.gnu.org/gnu/gdb/gdb-9.2.tar.xz"
   sha256 "360cd7ae79b776988e89d8f9a01c985d0b1fa21c767a4295e5f88cb49175c555"
   license "GPL-3.0-or-later"
+  revision 1
 
   bottle do
     sha256 "dfe4806259f346a687fdd33e04c0380a3e29042359d0035649bc921197482472" => :catalina
@@ -11,10 +12,18 @@ class X8664ElfGdb < Formula
     sha256 "08a7d238e78d7cda369afbf7e00e5e778a6d089c7c28713da8eb81692525c849" => :high_sierra
   end
 
-  depends_on "python@3.8"
+  depends_on "python@3.9"
   depends_on "xz"
 
   conflicts_with "gdb", because: "both install include/gdb, share/gdb and share/info"
+  conflicts_with "i386-elf-gdb", because: "both install include/gdb, share/gdb and share/info"
+
+  # Fix for Python 3.9, remove in next version
+  # https://sourceware.org/pipermail/gdb-patches/2020-May/169110.html
+  patch do
+    url "https://github.com/Homebrew/formula-patches/raw/88f56f8f/gdb/python39.diff"
+    sha256 "19e989104f54c09a30f06aac87e31706f109784d3e0fdc7ff0fd1bcfd261ebee"
+  end
 
   def install
     args = %W[
@@ -23,7 +32,7 @@ class X8664ElfGdb < Formula
       --disable-debug
       --disable-dependency-tracking
       --with-lzma
-      --with-python=#{Formula["python@3.8"].opt_bin}/python3
+      --with-python=#{Formula["python@3.9"].opt_bin}/python3
       --disable-binutils
     ]
 
