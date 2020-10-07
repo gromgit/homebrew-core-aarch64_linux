e0957c6283538ce91569025f996bbc1d36cea99f
Sam Ford <1584702+samford@users.noreply.github.com>
2020-10-07 04:01:34 -0400

    helm@2: add deprecation date, restore livecheck (#62200)

--- Formula/helm@2.rb
+++ Formula/helm@2.rb
@@ -6,6 +6,13 @@ class HelmAT2 < Formula
       revision: "47f0b88409e71fd9ca272abc7cd762a56a1c613e"
   license "Apache-2.0"
 
+  # NOTE: Remove this livecheck block after deprecation takes effect, so we
+  # don't unnecessarily check for updates.
+  livecheck do
+    url :stable
+    regex(/^v?(2(?:\.\d+)+)$/i)
+  end
+
   bottle do
     cellar :any_skip_relocation
     sha256 "f842166eae515ed30c9f91c0ad27994640332b889fb490c2fe2f4ef818501dac" => :catalina
@@ -15,8 +22,8 @@ class HelmAT2 < Formula
 
   keg_only :versioned_formula
 
-  # https://github.com/helm/helm/issues/2466#issuecomment-691177445
-  deprecate! because: :unsupported
+  # See: https://helm.sh/blog/helm-v2-deprecation-timeline/
+  deprecate! date: "2020-11-13", because: :deprecated_upstream
 
   depends_on "glide" => :build
   depends_on "go" => :build
