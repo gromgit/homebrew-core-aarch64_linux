50d10c7dfcfd0c4c4361a26fff252374cbc7862f
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-07 14:38:39 +0200

    sqlparse: migrate to python@3.9
    Closes #62420.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sqlparse.rb
+++ Formula/sqlparse.rb
@@ -6,6 +6,7 @@ class Sqlparse < Formula
   url "https://files.pythonhosted.org/packages/67/4b/253b6902c1526885af6d361ca8c6b1400292e649f0e9c95ee0d2e8ec8681/sqlparse-0.3.1.tar.gz"
   sha256 "e162203737712307dfe78860cc56c8da8a852ab2ee33750e33aeadf38d12c548"
   license "BSD-3-Clause"
+  revision 1
 
   livecheck do
     url :stable
@@ -18,7 +19,7 @@ class Sqlparse < Formula
     sha256 "c3e7934f912db3b38d16bd6adb86a496568040bcbd66f646822679b233d3d193" => :high_sierra
   end
 
-  depends_on "python@3.8"
+  depends_on "python@3.9"
 
   def install
     virtualenv_install_with_resources
