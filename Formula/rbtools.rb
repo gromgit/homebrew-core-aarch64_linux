1f4f3afb459c998765625385f7b9fb6fb0b3a499
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-07 14:36:52 +0200

    rbtools: migrate to python@3.9
    Closes #62401.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/rbtools.rb
+++ Formula/rbtools.rb
@@ -6,6 +6,7 @@ class Rbtools < Formula
   url "https://files.pythonhosted.org/packages/04/98/10d5f67470c48e000cf9c95fb748f6a302eb01c0cb006d3ec37ff2f1e7c1/RBTools-1.0.3.tar.gz"
   sha256 "ff4cea3ad7b2d1b1666b811021cf5047f1fbe9417428fb5133a40ede81e3e83c"
   license "MIT"
+  revision 1
   head "https://github.com/reviewboard/rbtools.git"
 
   livecheck do
@@ -19,7 +20,7 @@ class Rbtools < Formula
     sha256 "6aa5f77cc2368f5e442635294a6878d1e3d738a0ac27083ba3856b255c0b3ba2" => :high_sierra
   end
 
-  depends_on "python@3.8"
+  depends_on "python@3.9"
 
   resource "colorama" do
     url "https://files.pythonhosted.org/packages/76/53/e785891dce0e2f2b9f4b4ff5bc6062a53332ed28833c7afede841f46a5db/colorama-0.4.1.tar.gz"
