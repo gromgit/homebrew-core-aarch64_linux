8402b5590296f70236e7b08f235c118c7117c168
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-07 14:38:45 +0200

    ssh-audit: migrate to python@3.9

--- Formula/ssh-audit.rb
+++ Formula/ssh-audit.rb
@@ -6,11 +6,12 @@ class SshAudit < Formula
   url "https://github.com/jtesta/ssh-audit/releases/download/v2.3.0/ssh-audit-2.3.0.tar.gz"
   sha256 "776547591e7b69a2a8dcd1eaaac5d38321f2cb4a5de5f8e5a3e135b33236e812"
   license "MIT"
+  revision 1
   head "https://github.com/jtesta/ssh-audit.git"
 
   bottle :unneeded
 
-  depends_on "python@3.8"
+  depends_on "python@3.9"
 
   def install
     rewrite_shebang detected_python_shebang, "ssh-audit.py"
