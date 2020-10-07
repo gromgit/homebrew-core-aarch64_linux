f7acb3c1b6f3ed2926a916bb2775ba7022106f70
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-07 14:39:50 +0200

    tarsnapper: migrate to python@3.9
    Closes #62433.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tarsnapper.rb
+++ Formula/tarsnapper.rb
@@ -6,7 +6,7 @@ class Tarsnapper < Formula
   url "https://github.com/miracle2k/tarsnapper/archive/0.4.tar.gz"
   sha256 "94ac22c3ed72e6321596f7d229b34fd21b59a00035162c5b22f2a1ee64dc6d01"
   license "BSD-2-Clause"
-  revision 2
+  revision 3
 
   bottle do
     cellar :any
@@ -16,7 +16,7 @@ class Tarsnapper < Formula
   end
 
   depends_on "libyaml"
-  depends_on "python@3.8"
+  depends_on "python@3.9"
   depends_on "tarsnap"
 
   resource "argparse" do
