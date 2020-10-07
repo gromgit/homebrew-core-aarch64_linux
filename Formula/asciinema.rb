5ecd89710913a15275c72e35689f6c2877f94313
FX Coudert <fxcoudert@gmail.com>
2020-10-07 19:34:51 +0000

    asciinema: update 2.0.2_3 bottle.

--- Formula/asciinema.rb
+++ Formula/asciinema.rb
@@ -15,10 +15,9 @@ class Asciinema < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "4ac59de631594cea60621b45d85214e39a90a0ba8ddf4eeec5cba34bd6145711" => :catalina
-    sha256 "b395128b1d0825a1b0ab81c4238d0acf38f6edc2df2a1bcfcaf6658ba191900d" => :mojave
-    sha256 "1a941e2c0594a7c0a07c66f02c411e121ed2b3e869f3f015c6e69cb4fd92daba" => :high_sierra
+    sha256 "caa76523c644cf5916cde300c407d89282509a0291cfa9c4812d888c26ce7f77" => :catalina
+    sha256 "b1a1aff9cf3f46328f0df80773a85898db70453fff4785e26e7ee4d6f12ef408" => :mojave
+    sha256 "4ce78d8edeeb635ad7cf2f5edd88770baaa72d623ecc471fb0e8f5e12efb0e04" => :high_sierra
   end
 
   depends_on "python@3.9"
