50ea499931eca8b4e952721561b02619dc39f40c
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-07 14:36:46 +0200

    rabbitmq: migrate to python@3.9
    Closes #62400.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/rabbitmq.rb
+++ Formula/rabbitmq.rb
@@ -4,6 +4,7 @@ class Rabbitmq < Formula
   url "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.9/rabbitmq-server-generic-unix-3.8.9.tar.xz"
   sha256 "fe1f1ef9b1bd8362421d689ec9b73cb33c8aaf96acf990df6549e3c0275b7aa0"
   license "MPL-2.0"
+  revision 1
 
   livecheck do
     url :stable
@@ -12,7 +13,7 @@ class Rabbitmq < Formula
 
   bottle :unneeded
 
-  depends_on "python@3.8" => :build
+  depends_on "python@3.9" => :build
   depends_on "erlang"
 
   uses_from_macos "unzip" => :build
