db872a03ff104c2f70ed9da1a1b67d73f91a8f7c
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-07 14:33:32 +0200

    nicovideo-dl: migrate to python@3.9
    Closes #62366.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/nicovideo-dl.rb
+++ Formula/nicovideo-dl.rb
@@ -6,11 +6,11 @@ class NicovideoDl < Formula
   # Canonical: https://osdn.net/dl/nicovideo-dl/nicovideo-dl-0.0.20190126.tar.gz
   url "https://dotsrc.dl.osdn.net/osdn/nicovideo-dl/70568/nicovideo-dl-0.0.20190126.tar.gz"
   sha256 "886980d154953bc5ff5d44758f352ce34d814566a83ceb0b412b8d2d51f52197"
-  revision 1
+  revision 2
 
   bottle :unneeded
 
-  depends_on "python@3.8"
+  depends_on "python@3.9"
 
   def install
     rewrite_shebang detected_python_shebang, "nicovideo-dl"
