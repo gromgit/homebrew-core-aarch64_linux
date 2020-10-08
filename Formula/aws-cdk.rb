780989bd4e8a1e27907e443c35174df0737edc5c
Alexander Zigelski <crunchtime-ali@users.noreply.github.com>
2020-10-08 00:42:58 +0000

    aws-cdk 1.67.0
    Closes #62505.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/aws-cdk.rb
+++ Formula/aws-cdk.rb
@@ -3,8 +3,8 @@ require "language/node"
 class AwsCdk < Formula
   desc "AWS Cloud Development Kit - framework for defining AWS infra as code"
   homepage "https://github.com/aws/aws-cdk"
-  url "https://registry.npmjs.org/aws-cdk/-/aws-cdk-1.66.0.tgz"
-  sha256 "bb408ffa439521ddf683d954cba3d62e2aa6005c601d2167490476364de52f57"
+  url "https://registry.npmjs.org/aws-cdk/-/aws-cdk-1.67.0.tgz"
+  sha256 "0a1254af0de2de5ce7cdaaf78a35a30421ca8427c0320acd39e90254f9f5dbb7"
   license "Apache-2.0"
 
   livecheck do
