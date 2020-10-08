f639418e1e78601eac8ec102dbf362ed5b4ddc5e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-08 05:14:43 +0000

    trash-cli: update 0.17.1.14_4 bottle.

--- Formula/trash-cli.rb
+++ Formula/trash-cli.rb
@@ -11,10 +11,9 @@ class TrashCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "abef6ba69850e6dfa74ed7ed5d5a21b9c93aa45822fa7561b298e70889a6ca7f" => :catalina
-    sha256 "3e1575beadbf5223f948d050b792671755874ad645c852c3ce2f5c3495a5cd21" => :mojave
-    sha256 "14a43467042890dd35c6ae2f832a5a91c5d8b4d7fa0b731c7f9eeb9dee2d8a15" => :high_sierra
+    sha256 "dc13679d7b02e8ff8deb976e1866fa715cdd71a11afb4c749ed7f72ca9dd4121" => :catalina
+    sha256 "7c50f6d779aef1d69d7620433ec0bb90a5031960c0e701ff11bcb8a9737af72c" => :mojave
+    sha256 "4731567ca54e4fb86125dd7d37bce5d079fdcbd0830151c68f2c01854eff1512" => :high_sierra
   end
 
   depends_on "python@3.9"
