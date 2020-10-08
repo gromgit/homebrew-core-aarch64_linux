49f646c181451e66ba247075b635d33152a43dbb
Shohei Ueda <30958501+peaceiris@users.noreply.github.com>
2020-10-09 06:07:44 +0900

    hugo 0.76.3
    Closes #62550.
    
    Signed-off-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hugo.rb
+++ Formula/hugo.rb
@@ -1,8 +1,8 @@
 class Hugo < Formula
   desc "Configurable static site generator"
   homepage "https://gohugo.io/"
-  url "https://github.com/gohugoio/hugo/archive/v0.76.2.tar.gz"
-  sha256 "03a32d3c7bff0264bf5e2cb995bff6250c9b6a05a7172ffe98c4f923226f7181"
+  url "https://github.com/gohugoio/hugo/archive/v0.76.3.tar.gz"
+  sha256 "7429f0705999aea0d468d890358c1c13bae3f5ae2a372468615b1424d20ace8f"
   license "Apache-2.0"
   head "https://github.com/gohugoio/hugo.git"
 
