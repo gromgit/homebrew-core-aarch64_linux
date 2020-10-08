c8eb692b50bb072781ff9208bba6652e9c7df361
miccal <miccal.matthews@gmail.com>
2020-10-08 15:24:18 +0800

    unbound 1.12.0
    Closes #62523.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/unbound.rb
+++ Formula/unbound.rb
@@ -1,8 +1,8 @@
 class Unbound < Formula
   desc "Validating, recursive, caching DNS resolver"
   homepage "https://www.unbound.net"
-  url "https://nlnetlabs.nl/downloads/unbound/unbound-1.11.0.tar.gz"
-  sha256 "9f2f0798f76eb8f30feaeda7e442ceed479bc54db0e3ac19c052d68685e51ef7"
+  url "https://nlnetlabs.nl/downloads/unbound/unbound-1.12.0.tar.gz"
+  sha256 "5b9253a97812f24419bf2e6b3ad28c69287261cf8c8fa79e3e9f6d3bf7ef5835"
   license "BSD-3-Clause"
   head "https://github.com/NLnetLabs/unbound.git"
 
