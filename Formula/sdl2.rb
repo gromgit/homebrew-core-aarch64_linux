76dfcb98ca316fc0318d1145240035f7814a960e
sidneys.github.io <sidneys.github.io@outlook.com>
2020-10-08 18:05:04 +0200

    sdl2: fix build for head (#62173)

--- Formula/sdl2.rb
+++ Formula/sdl2.rb
@@ -1,9 +1,19 @@
 class Sdl2 < Formula
   desc "Low-level access to audio, keyboard, mouse, joystick, and graphics"
   homepage "https://www.libsdl.org/"
-  url "https://libsdl.org/release/SDL2-2.0.12.tar.gz"
-  sha256 "349268f695c02efbc9b9148a70b85e58cefbbf704abd3e91be654db7f1e2c863"
   revision 1
+
+  stable do
+    url "https://libsdl.org/release/SDL2-2.0.12.tar.gz"
+    sha256 "349268f695c02efbc9b9148a70b85e58cefbbf704abd3e91be654db7f1e2c863"
+
+    # Fix library extension in CMake config file.
+    # https://bugzilla.libsdl.org/show_bug.cgi?id=5039
+    patch do
+      url "https://bugzilla.libsdl.org/attachment.cgi?id=4263"
+      sha256 "07ea066e805f82d85e6472e767ba75d265cb262053901ac9a9e22c5f8ff187a5"
+    end
+  end
 
   livecheck do
     url "https://www.libsdl.org/download-2.0.php"
@@ -27,13 +37,6 @@ class Sdl2 < Formula
 
   on_linux do
     depends_on "pkg-config" => :build
-  end
-
-  # Fix library extension in CMake config file.
-  # https://bugzilla.libsdl.org/show_bug.cgi?id=5039
-  patch do
-    url "https://bugzilla.libsdl.org/attachment.cgi?id=4263"
-    sha256 "07ea066e805f82d85e6472e767ba75d265cb262053901ac9a9e22c5f8ff187a5"
   end
 
   def install
