73e0da2b26f89573fd5349a0d0cab71a260344df
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-08 00:38:13 +0000

    streamlink: update 1.6.0_1 bottle.

--- Formula/streamlink.rb
+++ Formula/streamlink.rb
@@ -10,9 +10,9 @@ class Streamlink < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "dec6621b9130d3b9150f09c7e1356f020ba7292bb490850ac40865c9621a7e1b" => :catalina
-    sha256 "e849f2501e7e14d5e4ee454412cf195baada45a91ef6593da920d433c74a1069" => :mojave
-    sha256 "f256ce18d4d0d3eceb969922ee48607ecfbaf6a81c0d4f7cd9414812cf2106b4" => :high_sierra
+    sha256 "f04bb2d21f00858c07173934b6fb455cff79c01ceed45258dc723454f655cfc4" => :catalina
+    sha256 "246e12f4e073864035c96cbe195d3690528401de04a0bff334c7356f6bf81a03" => :mojave
+    sha256 "c115f03313785a4f891d86007f52c01c34b8bb2d998b379c3684bfc34db54066" => :high_sierra
   end
 
   depends_on "python@3.9"
