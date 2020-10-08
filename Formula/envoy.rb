6bf57860543fc2766e19b784408242fbd638af1d
Andrew Kane <andrew@chartkick.com>
2020-10-08 10:46:13 -0700

    envoy 1.16.0 (new formula)
    Closes #61858.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/envoy.rb
@@ -0,0 +1,45 @@
+class Envoy < Formula
+  desc "Cloud-native high-performance edge/middle/service proxy"
+  homepage "https://www.envoyproxy.io"
+  url "https://github.com/envoyproxy/envoy.git",
+      tag:      "v1.16.0",
+      revision: "8fb3cb86082b17144a80402f5367ae65f06083bd"
+  license "Apache-2.0"
+
+  depends_on "automake" => :build
+  depends_on "bazelisk" => :build
+  depends_on "cmake" => :build
+  depends_on "coreutils" => :build
+  depends_on "go" => :build
+  depends_on "libtool" => :build
+  depends_on "ninja" => :build
+
+  def install
+    args = %w[
+      --curses=no
+      --show_task_finish
+      --verbose_failures
+      --action_env=PATH=/usr/local/bin:/opt/local/bin:/usr/bin:/bin
+      --test_output=all
+    ]
+    system Formula["bazelisk"].opt_bin/"bazelisk", "build", *args, "//source/exe:envoy-static"
+    bin.install "bazel-bin/source/exe/envoy-static" => "envoy"
+    pkgshare.install "configs", "examples"
+  end
+
+  test do
+    port = free_port
+
+    cp pkgshare/"configs/google_com_proxy.v2.yaml", testpath/"envoy.yaml"
+    inreplace "envoy.yaml" do |s|
+      s.gsub! "port_value: 9901", "port_value: #{port}"
+      s.gsub! "port_value: 10000", "port_value: #{free_port}"
+    end
+
+    fork do
+      exec bin/"envoy", "-c", "envoy.yaml"
+    end
+    sleep 10
+    assert_match "HEALTHY", shell_output("curl -s 127.0.0.1:#{port}/clusters?format=json")
+  end
+end
