9cbbe347b696523a6c0382643c56a8e19349760d
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2020-10-08 10:00:34 -0700

    aardvark_shell_utils: update license

--- Formula/aardvark_shell_utils.rb
+++ Formula/aardvark_shell_utils.rb
@@ -3,7 +3,7 @@ class AardvarkShellUtils < Formula
   homepage "http://www.laffeycomputer.com/shellutils.html"
   url "https://web.archive.org/web/20170106105512/downloads.laffeycomputer.com/current_builds/shellutils/aardvark_shell_utils-1.0.tar.gz"
   sha256 "aa2b83d9eea416aa31dd1ce9b04054be1a504e60e46426225543476c0ebc3f67"
-  license "GPL-2.0"
+  license "GPL-2.0-or-later"
 
   # This regex is multiline since there's a line break between `href=` and the
   # attribute value on the homepage.
