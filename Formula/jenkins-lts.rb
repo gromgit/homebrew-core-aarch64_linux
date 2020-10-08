7920ad6d1d9769fa319b32bcb4e8e7b1b29765ea
Kent Sutherland <git@ksuther.com>
2020-10-07 22:06:40 -0400

    jenkins-lts 2.249.2
    Closes #62508.
    
    Signed-off-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jenkins-lts.rb
+++ Formula/jenkins-lts.rb
@@ -1,8 +1,8 @@
 class JenkinsLts < Formula
   desc "Extendable open-source CI server"
   homepage "https://jenkins.io/index.html#stable"
-  url "http://mirrors.jenkins.io/war-stable/2.249.1/jenkins.war"
-  sha256 "a38ca485b3e2bae6ec69a34b6b34b674f4dd4baf04f20f8dce2d838d08ed5bd6"
+  url "http://mirrors.jenkins.io/war-stable/2.249.2/jenkins.war"
+  sha256 "1bb06cc42cef730e6326faba146f6c40e77df381ec4b281287f6993423f9a522"
   license "MIT"
 
   livecheck do
