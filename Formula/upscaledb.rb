584183b6bd5710207df0d35a8e6d837a86c10567
Alexander Bayandin <a.bayandin@gmail.com>
2020-10-08 16:51:43 +0100

    upscaledb 2.2.1
    Closes #62541.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/upscaledb.rb
+++ Formula/upscaledb.rb
@@ -2,18 +2,16 @@ class Upscaledb < Formula
   desc "Database for embedded devices"
   homepage "https://upscaledb.com/"
   license "Apache-2.0"
-  revision 14
+  head "https://github.com/cruppstahl/upscaledb.git"
 
   stable do
-    url "http://files.upscaledb.com/dl/upscaledb-2.2.0.tar.gz"
-    mirror "https://dl.bintray.com/homebrew/mirror/upscaledb-2.2.0.tar.gz"
-    sha256 "7d0d1ace47847a0f95a9138637fcaaf78b897ef682053e405e2c0865ecfd253e"
+    url "https://github.com/cruppstahl/upscaledb.git",
+      tag:      "release-2.2.1",
+      revision: "60d39fc19888fbc5d8b713d30373095a41bf9ced"
 
-    # Remove for > 2.2.2
-    # Upstream commit from 12 Feb 2018 "Fix compilation with Boost 1.66 (#110)"
     patch do
-      url "https://github.com/cruppstahl/upscaledb/commit/01156f9a8.patch?full_index=1"
-      sha256 "e65b9f2b624b7cdad00c3c1444721cadd615688556d8f0bb389d15f5f5f4f430"
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/31fa2b66ae637e8f1dc2864af869baa34604f8fe/upscaledb/2.2.1.diff"
+      sha256 "fc99845f15e87c8ba30598cfdd15f0f010efa45421462548ee56c8ae26a12ee5"
     end
   end
 
@@ -29,53 +27,25 @@ class Upscaledb < Formula
     sha256 "003ac2a786a136acf66f8de4a25a0d72950c21dbb4e4b70afad486bba01319d7" => :high_sierra
   end
 
-  head do
-    url "https://github.com/cruppstahl/upscaledb.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
-
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "boost"
   depends_on "gnutls"
   depends_on "openjdk"
   depends_on "openssl@1.1"
-  depends_on "protobuf"
-
-  resource "libuv" do
-    url "https://github.com/libuv/libuv/archive/v0.10.37.tar.gz"
-    sha256 "4c12bed4936dc16a20117adfc5bc18889fa73be8b6b083993862628469a1e931"
-  end
-
-  # Patch for compatibility with OpenSSL 1.1
-  # https://github.com/cruppstahl/upscaledb/issues/124
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/a7095c61/upscaledb/openssl-1.1.diff"
-    sha256 "c388613c88e856ee64be2b4a72b64a1b998f1f8b835122579e2049e9f01a0f58"
-  end
 
   def install
-    # Fix collision with isset() in <sys/params.h>
-    # See https://github.com/Homebrew/homebrew-core/pull/4145
-    inreplace "./src/5upscaledb/upscaledb.cc",
-      "#  include \"2protobuf/protocol.h\"",
-      "#  include \"2protobuf/protocol.h\"\n#define isset(f, b)       (((f) & (b)) == (b))"
+    ENV.cxx11
 
-    system "./bootstrap.sh" if build.head?
+    # Avoid references to Homebrew shims
+    ENV["SED"] = "sed"
 
-    resource("libuv").stage do
-      system "make", "libuv.dylib", "SO_LDFLAGS=-Wl,-install_name,#{libexec}/libuv/lib/libuv.dylib"
-      (libexec/"libuv/lib").install "libuv.dylib"
-      (libexec/"libuv").install "include"
-    end
-
-    ENV.prepend "LDFLAGS", "-L#{libexec}/libuv/lib"
-    ENV.prepend "CFLAGS", "-I#{libexec}/libuv/include"
-    ENV.prepend "CPPFLAGS", "-I#{libexec}/libuv/include"
+    system "./bootstrap.sh"
 
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
+                          "--disable-remote", # upscaledb is not compatible with latest protobuf
                           "--prefix=#{prefix}",
                           "JDK=#{Formula["openjdk"].opt_prefix}"
     system "make", "install"
