deaed668e3e5bf054139219ca053296e7bba34a0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-09 19:15:48 +0000

    pyenv: update 1.2.21 bottle.

--- Formula/pyenv.rb
+++ Formula/pyenv.rb
@@ -14,9 +14,9 @@ class Pyenv < Formula
 
   bottle do
     cellar :any
-    sha256 "74e2f3a7cf6513948cbac05f3af8f3f2ae7df825dfc21a91759963afbb398420" => :catalina
-    sha256 "ac4f0bdd6bba22dd590ce2b2e86ee51300c3bb708288bc26e6294e1035a38a03" => :mojave
-    sha256 "608b5e27568abc7c326c0bb066cf6d188c829362dd0dff718a04a44e9082f82a" => :high_sierra
+    sha256 "2f4c78fd8cce10de8e9fb48a43c7cd51003e7a87c3cba9e4c3942d72f331df58" => :catalina
+    sha256 "a3f8395adbc0644940fa03f41807fac246b997d10ae990f549e016699a2b7e51" => :mojave
+    sha256 "d4a573a9116ee1f474e4a77517af5c9dfaefe5ebb9cf7f26d2cc9ac872fe1155" => :high_sierra
   end
 
   depends_on "autoconf"
