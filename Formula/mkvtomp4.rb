ba0dbf0b730113ca6cf925d641e2bd8bed330335
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-09 14:54:23 +0200

    mkvtomp4: migrate to python@3.9

--- Formula/mkvtomp4.rb
+++ Formula/mkvtomp4.rb
@@ -6,7 +6,7 @@ class Mkvtomp4 < Formula
   url "https://github.com/gavinbeatty/mkvtomp4/archive/mkvtomp4-v2.0.tar.gz"
   sha256 "0f9aa1754d4b33a6a83c0a71a373836d4872b3288dae9cfb6168c35f09887f2d"
   license "MIT"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any_skip_relocation
@@ -17,7 +17,7 @@ class Mkvtomp4 < Formula
   depends_on "ffmpeg"
   depends_on "gpac"
   depends_on "mkvtoolnix"
-  depends_on "python@3.8"
+  depends_on "python@3.9"
 
   def install
     virtualenv_install_with_resources
