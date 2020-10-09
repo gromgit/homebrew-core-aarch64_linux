60fbb51fd586a8fe8da4307fdc6f827b610ff4f8
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-09 14:54:11 +0200

    arangodb: migrate to python@3.9

--- Formula/arangodb.rb
+++ Formula/arangodb.rb
@@ -4,6 +4,7 @@ class Arangodb < Formula
   url "https://download.arangodb.com/Source/ArangoDB-3.7.2-1.tar.gz"
   sha256 "de79c04b6b15d9b7a415f8cbfd6682e000414725be1c25966aeab19dfbb49195"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/arangodb/arangodb.git", branch: "devel"
 
   bottle do
@@ -14,7 +15,7 @@ class Arangodb < Formula
   depends_on "ccache" => :build
   depends_on "cmake" => :build
   depends_on "go@1.13" => :build
-  depends_on "python@3.8" => :build
+  depends_on "python@3.9" => :build
   depends_on macos: :mojave
   depends_on "openssl@1.1"
 
