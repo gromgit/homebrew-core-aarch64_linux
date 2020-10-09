78c35c2300a7617867e96a0da2eaf3a63886abeb
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-09 14:54:18 +0200

    i386-elf-gdb: migrate to python@3.9

--- Formula/i386-elf-gdb.rb
+++ Formula/i386-elf-gdb.rb
@@ -5,6 +5,7 @@ class I386ElfGdb < Formula
   mirror "https://ftpmirror.gnu.org/gdb/gdb-9.2.tar.xz"
   sha256 "360cd7ae79b776988e89d8f9a01c985d0b1fa21c767a4295e5f88cb49175c555"
   license "GPL-2.0"
+  revision 1
   head "https://sourceware.org/git/binutils-gdb.git"
 
   livecheck do
@@ -17,10 +18,18 @@ class I386ElfGdb < Formula
     sha256 "8dad06d6eea6ec145763819d982916d590edd45b40e4c91e328bba76f0aac0bf" => :high_sierra
   end
 
-  depends_on "python@3.8"
+  depends_on "python@3.9"
   depends_on "xz" # required for lzma support
 
   conflicts_with "gdb", because: "both install include/gdb, share/gdb and share/info"
+  conflicts_with "x86_64-elf-gdb", because: "both install include/gdb, share/gdb and share/info"
+
+  # Fix for Python 3.9, remove in next version
+  # https://sourceware.org/pipermail/gdb-patches/2020-May/169110.html
+  patch do
+    url "https://github.com/Homebrew/formula-patches/raw/88f56f8f/gdb/python39.diff"
+    sha256 "19e989104f54c09a30f06aac87e31706f109784d3e0fdc7ff0fd1bcfd261ebee"
+  end
 
   def install
     args = %W[
@@ -29,7 +38,7 @@ class I386ElfGdb < Formula
       --disable-debug
       --disable-dependency-tracking
       --with-lzma
-      --with-python=#{Formula["python@3.8"].opt_bin}/python3
+      --with-python=#{Formula["python@3.9"].opt_bin}/python3
       --disable-binutils
     ]
 
