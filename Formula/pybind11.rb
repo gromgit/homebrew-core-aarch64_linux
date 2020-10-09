2361b6749edd13291a9666bf91ad1c63e704e5db
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-09 14:54:26 +0200

    pybind11: migrate to python@3.9

--- Formula/pybind11.rb
+++ Formula/pybind11.rb
@@ -4,6 +4,7 @@ class Pybind11 < Formula
   url "https://github.com/pybind/pybind11/archive/v2.5.0.tar.gz"
   sha256 "97504db65640570f32d3fdf701c25a340c8643037c3b69aec469c10c93dc8504"
   license "BSD-3-Clause"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -13,7 +14,7 @@ class Pybind11 < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "python@3.8"
+  depends_on "python@3.9"
 
   def install
     system "cmake", ".", "-DPYBIND11_TEST=OFF", *std_cmake_args
@@ -40,8 +41,8 @@ class Pybind11 < Formula
       example.add(1,2)
     EOS
 
-    python_flags = `#{Formula["python@3.8"].opt_bin}/python3-config --cflags --ldflags --embed`.split(" ")
+    python_flags = `#{Formula["python@3.9"].opt_bin}/python3-config --cflags --ldflags --embed`.split(" ")
     system ENV.cxx, "-O3", "-shared", "-std=c++11", *python_flags, "example.cpp", "-o", "example.so"
-    system Formula["python@3.8"].opt_bin/"python3", "example.py"
+    system Formula["python@3.9"].opt_bin/"python3", "example.py"
   end
 end
