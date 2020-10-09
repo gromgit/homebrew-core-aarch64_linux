a67d6f69f1ccac87051b0bda9dccfec129c9066f
Thomas Berdy <thomas.berdy@outlook.com>
2020-10-09 17:32:55 +0200

    pyenv 1.2.21
    Closes #62563.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pyenv.rb
+++ Formula/pyenv.rb
@@ -1,8 +1,8 @@
 class Pyenv < Formula
   desc "Python version management"
   homepage "https://github.com/pyenv/pyenv"
-  url "https://github.com/pyenv/pyenv/archive/v1.2.20.tar.gz"
-  sha256 "5ecd10d3ec502ce9b7d8109fbe8cb2e4a8af8b73ce5c216b8e268452724a65f3"
+  url "https://github.com/pyenv/pyenv/archive/v1.2.21.tar.gz"
+  sha256 "a36f11a9e35bb3354e820fe2e9d258208624703506878fc6b7466d646b59d782"
   license "MIT"
   version_scheme 1
   head "https://github.com/pyenv/pyenv.git"
