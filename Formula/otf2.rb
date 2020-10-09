5d31e389fc70d45eca14aeb71c4ef64c11c45bca
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-09 14:54:25 +0200

    otf2: migrate to python@3.9

--- Formula/otf2.rb
+++ Formula/otf2.rb
@@ -3,7 +3,7 @@ class Otf2 < Formula
   homepage "https://www.vi-hps.org/projects/score-p/"
   url "https://www.vi-hps.org/cms/upload/packages/otf2/otf2-2.2.tar.gz"
   sha256 "d0519af93839dc778eddca2ce1447b1ee23002c41e60beac41ea7fe43117172d"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "4c532e5cb9864669bc29b6ad5007669c92a6abb3e069d7904ab3b102169ec2bc" => :catalina
@@ -14,7 +14,7 @@ class Otf2 < Formula
   depends_on "sphinx-doc" => :build
   depends_on "gcc"
   depends_on "open-mpi"
-  depends_on "python@3.8"
+  depends_on "python@3.9"
 
   resource "future" do
     url "https://files.pythonhosted.org/packages/45/0b/38b06fd9b92dc2b68d58b75f900e97884c45bedd2ff83203d933cf5851c9/future-0.18.2.tar.gz"
@@ -22,7 +22,7 @@ class Otf2 < Formula
   end
 
   def install
-    python3 = Formula["python@3.8"].opt_bin/"python3"
+    python3 = Formula["python@3.9"].opt_bin/"python3"
     xy = Language::Python.major_minor_version python3
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
 
