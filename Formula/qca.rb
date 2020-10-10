0bdc5112096bfd149e953b62419a9f9b5d2520f5
Rui Chen <rui@meetup.com>
2020-10-10 17:01:41 -0400

    qca: revision for botan
    Closes #62634.
    
    Signed-off-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/qca.rb
+++ Formula/qca.rb
@@ -2,7 +2,7 @@ class Qca < Formula
   desc "Qt Cryptographic Architecture (QCA)"
   homepage "https://userbase.kde.org/QCA"
   license "LGPL-2.1"
-  revision 1
+  revision 2
   head "https://invent.kde.org/libraries/qca.git"
 
   stable do
