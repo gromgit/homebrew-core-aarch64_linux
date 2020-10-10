3cf4942f06d8096ca134f3efb5f7284fde688e4e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-10 10:17:04 +0000

    eslint: update 7.11.0 bottle.

--- Formula/eslint.rb
+++ Formula/eslint.rb
@@ -13,9 +13,9 @@ class Eslint < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "37cdc4e0d0a9a0e62e1f0573c3fa429f40d8b9fe1e17d33751974e37bd7f191e" => :catalina
-    sha256 "f4f00dc48a3e2846f5e136a1d44c0fe0ad2d8684cc6a03eb4b589ded0bfd9671" => :mojave
-    sha256 "965706f668855aaf4cf797b248d3a2522f559be02bd472f2bc280255c0d9c046" => :high_sierra
+    sha256 "5e5c17351f44053f30e59029510abb3f834eb10b406cd4b9f6625ce535490270" => :catalina
+    sha256 "c459dda58ac19bf26a1298a28de6cf34a0f05de290d0c181e5fd16bdcae86caf" => :mojave
+    sha256 "2c9bdf660e74a6318353bc883a622abaf86e6589b8c141462704fff36b75bd3d" => :high_sierra
   end
 
   depends_on "node"
