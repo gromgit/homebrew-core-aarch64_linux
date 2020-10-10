e02911279db26979bac9a81f415d3c004ecd03ae
Rui Chen <rui@meetup.com>
2020-10-10 19:55:36 -0400

    reattach-to-user-namespace 2.9
    Closes #62679.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/reattach-to-user-namespace.rb
+++ Formula/reattach-to-user-namespace.rb
@@ -1,8 +1,8 @@
 class ReattachToUserNamespace < Formula
   desc "Reattach process (e.g., tmux) to background"
   homepage "https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard"
-  url "https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard/archive/v2.8.tar.gz"
-  sha256 "8b1b2785f2be19cc29083e7782270e6dcca67a66c66f11f785f4b26c446bbd77"
+  url "https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard/archive/v2.9.tar.gz"
+  sha256 "e4df00ead6b267a027a4ea35032bcfa114d91e709b1986ec0cbaee6825cec436"
   license "BSD-2-Clause"
   head "https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard.git"
 
