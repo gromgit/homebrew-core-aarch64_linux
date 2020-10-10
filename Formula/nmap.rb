85f328918406d106756e6a8753b550a5a083004e
Rui Chen <rui@meetup.com>
2020-10-10 17:36:20 -0400

    nmap 7.91

--- Formula/nmap.rb
+++ Formula/nmap.rb
@@ -1,8 +1,8 @@
 class Nmap < Formula
   desc "Port scanning utility for large networks"
   homepage "https://nmap.org/"
-  url "https://nmap.org/dist/nmap-7.90.tar.bz2"
-  sha256 "5557c3458275e8c43e1d0cfa5dad4e71dd39e091e2029a293891ad54098a40e8"
+  url "https://nmap.org/dist/nmap-7.91.tar.bz2"
+  sha256 "18cc4b5070511c51eb243cdd2b0b30ff9b2c4dc4544c6312f75ce3a67a593300"
   head "https://svn.nmap.org/nmap/"
 
   livecheck do
