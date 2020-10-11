51508b11508f0dcfad7c6bd959538ca573c47079
Dawid Dziurla <dawidd0811@gmail.com>
2020-10-11 13:35:35 +0200

    protoc-gen-go-grpc 1.0.0 (new formula)
    Closes #62685.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/protoc-gen-go-grpc.rb
@@ -0,0 +1,40 @@
+class ProtocGenGoGrpc < Formula
+  desc "Protoc plugin that generates code for gRPC-Go clients"
+  homepage "https://github.com/grpc/grpc-go"
+  url "https://github.com/grpc/grpc-go/archive/cmd/protoc-gen-go-grpc/v1.0.0.tar.gz"
+  sha256 "488bb2b4b282b7d4adda32a6534268b2622990d59e2bdadd6f28603638f07116"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(%r{cmd/protoc-gen-go-grpc/v?(\d+(?:\.\d+)+)}i)
+  end
+
+  depends_on "go" => :build
+  depends_on "protobuf"
+
+  def install
+    cd "cmd/protoc-gen-go-grpc" do
+      system "go", "build", *std_go_args
+    end
+  end
+
+  test do
+    (testpath/"service.proto").write <<~EOS
+      syntax = "proto3";
+
+      option go_package = ".;proto";
+
+      service Greeter {
+        rpc Hello(HelloRequest) returns (HelloResponse);
+      }
+
+      message HelloRequest {}
+      message HelloResponse {}
+    EOS
+
+    system "protoc", "--plugin=#{bin}/protoc-gen-go-grpc", "--go-grpc_out=.", "service.proto"
+
+    assert_predicate testpath/"service_grpc.pb.go", :exist?
+  end
+end
