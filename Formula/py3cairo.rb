0b03a9c68ab826a95095870e750612aea92a9404
Rui Chen <rui@meetup.com>
2020-10-11 11:42:02 -0400

    py3cairo 1.20.0

--- Formula/py3cairo.rb
+++ Formula/py3cairo.rb
@@ -1,10 +1,9 @@
 class Py3cairo < Formula
   desc "Python 3 bindings for the Cairo graphics library"
   homepage "https://cairographics.org/pycairo/"
-  url "https://github.com/pygobject/pycairo/releases/download/v1.19.1/pycairo-1.19.1.tar.gz"
-  sha256 "2c143183280feb67f5beb4e543fd49990c28e7df427301ede04fc550d3562e84"
+  url "https://github.com/pygobject/pycairo/releases/download/v1.20.0/pycairo-1.20.0.tar.gz"
+  sha256 "5695a10cb7f9ae0d01f665b56602a845b0a8cb17e2123bfece10c2e58552468c"
   license "LGPL-2.1"
-  revision 1
 
   bottle do
     cellar :any
