f0581f81b46937e9b2df4b36ffe3ea0eb9bfd01f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-11 09:17:22 +0000

    qca: update 2.3.1_2 bottle.

--- Formula/qca.rb
+++ Formula/qca.rb
@@ -24,9 +24,9 @@ class Qca < Formula
 
   bottle do
     cellar :any
-    sha256 "2a841d8b078e42054b374449de243111a63a150b90381bd080349952f8335599" => :catalina
-    sha256 "c44e0329bf19868a6b06da56b370e411ee7d5dff8c0532e1a40a0720dc54f30e" => :mojave
-    sha256 "7c5bb96e14843f75764c83eec5a6fde61e5c53cdab6e9ac5f02fce94acff75c6" => :high_sierra
+    sha256 "beffb3f0ec64707df65d6debe01378970a64a09bd1f3e62a75135fceb05fad2a" => :catalina
+    sha256 "1698ced59723dd813b0d844557701ff944e2392877bdf6ef3c5dbf6d2dbbc60b" => :mojave
+    sha256 "adf5f64082417a58d3472fbd8a57b1b2f9b37cb5a9410cf1db86b8fa44868cf5" => :high_sierra
   end
 
   depends_on "cmake" => :build
