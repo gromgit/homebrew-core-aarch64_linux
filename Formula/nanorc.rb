c7f909f967bdb2a7bc6d5dee32df91203c509616
chenrui <chenrui333@gmail.com>
2020-10-11 05:00:16 -0400

    nanorc 2020.10.10 (#62669)

--- Formula/nanorc.rb
+++ Formula/nanorc.rb
@@ -1,8 +1,8 @@
 class Nanorc < Formula
   desc "Improved Nano Syntax Highlighting Files"
   homepage "https://github.com/scopatz/nanorc"
-  url "https://github.com/scopatz/nanorc/releases/download/2020.1.25/nanorc-2020.1.25.tar.gz"
-  sha256 "a7466a712315391559b010c224de0dc814e7fb4227853f66692ce9c4347ece7e"
+  url "https://github.com/scopatz/nanorc/releases/download/2020.10.10/nanorc-2020.10.10.tar.gz"
+  sha256 "cd674e9eb230e4ba306b418c22d1891d93a3d2ffdf22234748d3398da50dfe64"
   license "GPL-3.0"
   head "https://github.com/scopatz/nanorc.git"
 
