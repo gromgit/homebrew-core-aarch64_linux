922a928c749ce3ae9b7d2e4616079cb97d916985
Jesse Duffield <jessedduffield@gmail.com>
2020-10-11 22:13:50 +0000

    lazygit 0.23.2
    Closes #62720.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lazygit.rb
+++ Formula/lazygit.rb
@@ -1,8 +1,8 @@
 class Lazygit < Formula
   desc "Simple terminal UI for git commands"
   homepage "https://github.com/jesseduffield/lazygit/"
-  url "https://github.com/jesseduffield/lazygit/archive/v0.23.1.tar.gz"
-  sha256 "754a7751f609305fdbfc6630ec18cfd22f4b7b8e77ed9084e9e11a789a5788de"
+  url "https://github.com/jesseduffield/lazygit/archive/v0.23.2.tar.gz"
+  sha256 "a541756a28d00ffc1fc9ba385c06b91fcc51df815a2919f7072873c969b2034c"
   license "MIT"
 
   bottle do
