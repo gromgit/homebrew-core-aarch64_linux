a9a0b96a254ba74f50b7d535171c65422be3da2d
chenrui <chenrui333@gmail.com>
2020-10-12 07:45:15 -0400

    calabash 1.2.5-100 (#62716)

--- Formula/calabash.rb
+++ Formula/calabash.rb
@@ -1,8 +1,8 @@
 class Calabash < Formula
   desc "XProc (XML Pipeline Language) implementation"
   homepage "https://xmlcalabash.com/"
-  url "https://github.com/ndw/xmlcalabash1/releases/download/1.2.1-99/xmlcalabash-1.2.1-99.zip"
-  sha256 "7dde0a5bd5dc2471d866efa3e4b5a4c456aa1acabe8e1b441abfd4b1c51f4279"
+  url "https://github.com/ndw/xmlcalabash1/releases/download/1.2.5-100/xmlcalabash-1.2.5-100.zip"
+  sha256 "1ce954e9563d9acc7a6dbcfaccbc3ec82e1cb938fdf816d5c5e57fa73b566fc4"
   license "GPL-2.0"
 
   bottle :unneeded
