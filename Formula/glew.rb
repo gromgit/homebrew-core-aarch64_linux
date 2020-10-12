00989b2d6fe4a1e107d5cdbc7c85b5fb20129adb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-12 08:30:18 +0000

    glew: update 2.2.0 bottle.

--- Formula/glew.rb
+++ Formula/glew.rb
@@ -12,10 +12,9 @@ class Glew < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "3181853e5ec2d8e0b24842c06b2882fce2d3ff89d83f4647bfee755005e165ca" => :catalina
-    sha256 "04ad309f33a1355e3e29251cf60ad24058b53443352c74d30624ec470b0428a1" => :mojave
-    sha256 "1b5e8d521625cfabd6e429e4111d74cd68fdc4efbde826a6b5bbee1a7261e801" => :high_sierra
+    sha256 "5f130b7557c1753c3880fc2eb16363de05a9d5a7d032294e8f8e744583df467f" => :catalina
+    sha256 "dc1e74289200e3c1db6792f085f1216529b491fc463bc6205bcd40807a4dba31" => :mojave
+    sha256 "1e2d9d489808104dfa3a4dab5662e200e1020b40b869bac45b6b84b8490cd936" => :high_sierra
   end
 
   depends_on "cmake" => :build
