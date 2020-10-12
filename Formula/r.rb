ad3bccd99c143147cccc3d5035c64ee3be4e59e7
Jonathan Chang <me@jonathanchang.org>
2020-10-12 10:18:25 +0000

    r: update 4.0.3 bottle.

--- Formula/r.rb
+++ Formula/r.rb
@@ -11,9 +11,9 @@ class R < Formula
   end
 
   bottle do
-    sha256 "4d2073c7d93e1117cc35fcc375f6eb112f16c311cc4400d703a805d57f0de71a" => :catalina
-    sha256 "e337fd3411cd1dffc9be88c1e116d87acac55021b3a5c70af0feba1b1a2c7259" => :mojave
-    sha256 "9040c02a85ac2c37007f066cb3c388c41ea5f4e9e7cfde864f249999b0ce4026" => :high_sierra
+    sha256 "4050483a8ea4ff9fbe1531f5e9cbe98fb1c7d8091b42184e40a5bcc678733cf3" => :catalina
+    sha256 "59f6e7ec476e78f4dbd99e01100cd6e937d97a3ecd818a00ad8c60dcb8ee7bea" => :mojave
+    sha256 "a95f0100c9c16a0a45edd271dca3d70b20a4bc9599774510eb1544b901e3d89a" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
