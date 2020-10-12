5cbcdba311315fcd3bf8d5139d9f140d8a3f57a0
Thierry Moisan <thierry.moisan@gmail.com>
2020-10-12 14:06:25 -0400

    vips 8.10.2
    Closes #62764.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vips.rb
+++ Formula/vips.rb
@@ -1,10 +1,9 @@
 class Vips < Formula
   desc "Image processing library"
   homepage "https://github.com/libvips/libvips"
-  url "https://github.com/libvips/libvips/releases/download/v8.10.1/vips-8.10.1.tar.gz"
-  sha256 "e089bb4f73e1dce866ae53e25604ea4f94535488a45bb4f633032e1d2f4e2dda"
+  url "https://github.com/libvips/libvips/releases/download/v8.10.2/vips-8.10.2.tar.gz"
+  sha256 "c1d0d9cb54d75cd4f66dce787fbcac99f834f6621fbf47bce9e02ef65b4ab02a"
   license "LGPL-2.1-or-later"
-  revision 1
 
   bottle do
     sha256 "0ff53f9fae72ead39ff7177c9b1bf8722bfd87a358ec00a939ab7d7c5404376b" => :catalina
@@ -44,11 +43,6 @@ class Vips < Formula
     depends_on "gobject-introspection"
   end
 
-  # Reported upstream at https://github.com/libvips/libvips/issues/1836
-  # This patch is a reworked version of https://github.com/libvips/libvips/commit/e3181e05799fa1e51e1a4f8fc6884e3f9c3de765
-  # To be removed when the next update is released
-  patch :DATA
-
   def install
     # mozjpeg needs to appear before libjpeg, otherwise it's not used
     ENV.prepend_path "PKG_CONFIG_PATH", Formula["mozjpeg"].opt_lib/"pkgconfig"
@@ -77,44 +71,3 @@ class Vips < Formula
     assert_equal "", shell_output(cmd)
   end
 end
-
-__END__
-diff --git a/libvips/iofuncs/error.c b/libvips/iofuncs/error.c
-index 165623843..746742cb3 100644
---- a/libvips/iofuncs/error.c
-+++ b/libvips/iofuncs/error.c
-@@ -71,7 +71,7 @@
- #endif /*OS_WIN32*/
-
- /**
-- * SECTION: error
-+ * SECTION: errors
-  * @short_description: error messages and error handling
-  * @stability: Stable
-  * @include: vips/vips.h
-diff --git a/libvips/iofuncs/rect.c b/libvips/iofuncs/rect.c
-index 79ae69f76..9038efb9b 100644
---- a/libvips/iofuncs/rect.c
-+++ b/libvips/iofuncs/rect.c
-@@ -45,7 +45,7 @@
- #include <vips/vips.h>
-
- /**
-- * SECTION: rect
-+ * SECTION: rectangle
-  * @short_description: the VIPS rectangle class
-  * @stability: Stable
-  * @see_also: <link linkend="VipsRegion">region</link>
-diff --git a/libvips/resample/interpolate.c b/libvips/resample/interpolate.c
-index 5ec50b742..d295ad9fa 100644
---- a/libvips/resample/interpolate.c
-+++ b/libvips/resample/interpolate.c
-@@ -63,7 +63,7 @@
- #include <vips/internal.h>
-
- /**
-- * SECTION: interpolate
-+ * SECTION: interpolator
-  * @short_description: various interpolators: nearest, bilinear, and
-  * some non-linear
-  * @stability: Stable
