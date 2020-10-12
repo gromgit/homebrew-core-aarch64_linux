51862604f663a6eaea84ee7fde1b17e986ef23a6
chenrui <chenrui333@gmail.com>
2020-10-12 16:31:39 +0000

    infracost 0.6.4
    fix version
    
    Closes #62760.
    
    Signed-off-by: Rui Chen <rui@meetup.com>
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/infracost.rb
+++ Formula/infracost.rb
@@ -1,8 +1,8 @@
 class Infracost < Formula
   desc "Cost estimates for Terraform"
   homepage "https://www.infracost.io/docs/"
-  url "https://github.com/infracost/infracost/archive/v0.6.3.tar.gz"
-  sha256 "e5cf6bfc09475c69de32a17b448ff7cae593768335620a2e7f0032610a63ed08"
+  url "https://github.com/infracost/infracost/archive/v0.6.4.tar.gz"
+  sha256 "e8ff7bd05b6116c569b2c0d5fe4283a55d12b773d17be548af148a13b999b11c"
   license "Apache-2.0"
   head "https://github.com/infracost/infracost.git"
 
@@ -18,11 +18,13 @@ class Infracost < Formula
 
   def install
     ENV["CGO_ENABLED"] = "0"
-    ldflags = "-X github.com/infracost/infracost/pkg/version.Version=v#{version}"
+    ldflags = "-X github.com/infracost/infracost/internal/version.Version=v#{version}"
     system "go", "build", *std_go_args, "-ldflags", ldflags, "./cmd/infracost"
   end
 
   test do
+    assert_match "v#{version}", shell_output("#{bin}/infracost --help 2>&1")
+
     output = shell_output("#{bin}/infracost --no-color 2>&1", 1)
     assert_match "No INFRACOST_API_KEY environment variable is set.", output
   end
