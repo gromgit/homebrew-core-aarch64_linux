00ff7c0baa099b83a62d81255e8574d6677a4a9a
FX Coudert <fxcoudert@gmail.com>
2020-10-12 11:47:35 +0000

    halide: update 10.0.0_1 bottle.

--- Formula/halide.rb
+++ Formula/halide.rb
@@ -8,9 +8,9 @@ class Halide < Formula
 
   bottle do
     cellar :any
-    sha256 "93c86ba497d66ddd04bb5fc29ed122f9388f6911fcbc5d5b9972869a9cdd1bc6" => :catalina
-    sha256 "d9db4f376d1aec822e9302540670c7853b8163caf0b824afb6beb33d53549740" => :mojave
-    sha256 "34f379eb801990b3c43011befab88deb8507df0d8d9c4b015e5c84c04394f64e" => :high_sierra
+    sha256 "997ed23a3fcb238899272fab8a0f9c2948477fb53bdc1bf2382a31418bc51571" => :catalina
+    sha256 "281f9219faf56e6e4b01e44bde6d9e8a4cd8bb0283e14aa6f2f78f6db4301dec" => :mojave
+    sha256 "5aaa4023bfff9d9f1f18ee1efae442ff7e0c21c84acc220555504c42705d9c7c" => :high_sierra
   end
 
   depends_on "cmake" => :build
