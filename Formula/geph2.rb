b0ac018839ac6ad3f62bd04c508270fc51dfded2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-12 07:33:47 +0000

    geph2: update 0.22.3 bottle.

--- Formula/geph2.rb
+++ Formula/geph2.rb
@@ -7,10 +7,9 @@ class Geph2 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "6a596d4f5c73bdaf166874f9ad4c8721d8ccbbe39da45541b00293a4b55675d1" => :catalina
-    sha256 "6a596d4f5c73bdaf166874f9ad4c8721d8ccbbe39da45541b00293a4b55675d1" => :mojave
-    sha256 "6a596d4f5c73bdaf166874f9ad4c8721d8ccbbe39da45541b00293a4b55675d1" => :high_sierra
+    sha256 "3bb066d5037011005e96467b055349395684098af46b9e8bbafa6d6ca23f28f3" => :catalina
+    sha256 "8be3fa40789dc634416626db3aaf4c7457da63178ee04005edd2d5b807c92553" => :mojave
+    sha256 "2632e8c16a9df74e0278db220961e15e9ff3bb421a15db944c8aa801e7125626" => :high_sierra
   end
 
   depends_on "go" => :build
