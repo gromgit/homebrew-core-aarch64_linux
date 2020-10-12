063a40ab2fd3213b730e89fedbb8a487912b33c3
Caleb Xu <calebcenter@live.com>
2020-10-12 12:30:19 -0400

    swaks 20201010.0 (#62752)

--- Formula/swaks.rb
+++ Formula/swaks.rb
@@ -1,8 +1,8 @@
 class Swaks < Formula
   desc "SMTP command-line test tool"
   homepage "https://www.jetmore.org/john/code/swaks/"
-  url "https://www.jetmore.org/john/code/swaks/files/swaks-20190914.0.tar.gz"
-  sha256 "5733a51a5c3f74f62274c17dc825f177c22ed52703c97c3b23a5354d7ec15c89"
+  url "https://www.jetmore.org/john/code/swaks/files/swaks-20201010.0.tar.gz"
+  sha256 "f6b1423e16ea4001f2b5666043921da020770de5e4cf3a2bc14315643a9be462"
 
   livecheck do
     url "https://www.jetmore.org/john/code/swaks/versions.html"
