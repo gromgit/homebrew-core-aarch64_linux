c082e53bc8cea294edc3c8a7f1e1d790202805fa
Jakub Konka <kubkon@jakubkonka.com>
2020-10-13 16:55:42 +0200

    ccls revision bump
    Signed-off-by: Jakub Konka <kubkon@jakubkonka.com>

--- Formula/ccls.rb
+++ Formula/ccls.rb
@@ -4,6 +4,7 @@ class Ccls < Formula
   url "https://github.com/MaskRay/ccls/archive/0.20190823.6.tar.gz"
   sha256 "83dd45120e9674319f91e4379013831e124c0858e050bbc3521e3f8aebe5c95b"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/MaskRay/ccls.git"
 
   bottle do
