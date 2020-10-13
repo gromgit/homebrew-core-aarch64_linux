4ac6f345ce42ec5712e7be7e2030c4a5f284f410
chenrui <chenrui333@gmail.com>
2020-10-13 16:38:36 +0000

    cargo-c 0.6.14
    Closes #62810.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cargo-c.rb
+++ Formula/cargo-c.rb
@@ -1,8 +1,8 @@
 class CargoC < Formula
   desc "Helper program to build and install c-like libraries"
   homepage "https://github.com/lu-zero/cargo-c"
-  url "https://github.com/lu-zero/cargo-c/archive/v0.6.13.tar.gz"
-  sha256 "aa6b23c7ae19ec93e6edeb59be46056faccb915a02d1784a0c29ac7a3c6a8aea"
+  url "https://github.com/lu-zero/cargo-c/archive/v0.6.14.tar.gz"
+  sha256 "71b7f6c1c729e2e3c98d16c5d72c8eb634ae480283fdfb6b7ecd9a56071c58b7"
   license "MIT"
 
   bottle do
