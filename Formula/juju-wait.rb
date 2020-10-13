c7d8193973b4247e824efbeb0796fb1bf7242799
James Harr <james.harr@gmail.com>
2020-10-13 16:00:59 -0500

    juju-wait: migrate to python@3.9
    Closes #62821.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/juju-wait.rb
+++ Formula/juju-wait.rb
@@ -6,6 +6,7 @@ class JujuWait < Formula
   url "https://files.pythonhosted.org/packages/0c/2b/f4bd0138f941e4ba321298663de3f1c8d9368b75671b17aa1b8d41a154dc/juju-wait-2.8.4.tar.gz"
   sha256 "9e84739056e371ab41ee59086313bf357684bc97aae8308716c8fe3f19df99be"
   license "GPL-3.0-only"
+  revision 1
 
   livecheck do
     url :stable
@@ -20,7 +21,7 @@ class JujuWait < Formula
 
   depends_on "juju"
   depends_on "libyaml"
-  depends_on "python@3.8"
+  depends_on "python@3.9"
 
   resource "PyYAML" do
     url "https://files.pythonhosted.org/packages/64/c2/b80047c7ac2478f9501676c988a5411ed5572f35d1beff9cae07d321512c/PyYAML-5.3.1.tar.gz"
