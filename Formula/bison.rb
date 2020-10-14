e3e1efc1a55957c2a477fc328f9663b67b8eb8a2
Caleb Xu <calebcenter@live.com>
2020-10-14 03:00:27 -0400

    bison 3.7.3
    Closes #62851.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bison.rb
+++ Formula/bison.rb
@@ -2,9 +2,9 @@ class Bison < Formula
   desc "Parser generator"
   homepage "https://www.gnu.org/software/bison/"
   # X.Y.9Z are beta releases that sometimes get accidentally uploaded to the release FTP
-  url "https://ftp.gnu.org/gnu/bison/bison-3.7.2.tar.xz"
-  mirror "https://ftpmirror.gnu.org/bison/bison-3.7.2.tar.xz"
-  sha256 "7948d193104d979c0fb0294a1854c73c89d72ae41acfc081826142578a78a91b"
+  url "https://ftp.gnu.org/gnu/bison/bison-3.7.3.tar.xz"
+  mirror "https://ftpmirror.gnu.org/bison/bison-3.7.3.tar.xz"
+  sha256 "88d9e36856b004c0887a12ba00ea3c47db388519629483dd8c3fce9694d4da6f"
   license "GPL-3.0-or-later"
   version_scheme 1
 
