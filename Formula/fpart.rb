c865d306b57f6e77838b80bec3e3b8a6dfe1b69b
Dawid Dziurla <dawidd0811@gmail.com>
2020-10-14 13:42:51 +0000

    fpart: add 1.2.0 bottle.

--- Formula/fpart.rb
+++ Formula/fpart.rb
@@ -5,6 +5,13 @@ class Fpart < Formula
   sha256 "ced59d68236d960473b5d9481f3d0c083f10a7be806c33125cc490ef2c1705f8"
   license "BSD-2-Clause"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "86e086c356acb32a5c69d8f27426b73a766c7eb6afa685f06f9a23449aa14983" => :catalina
+    sha256 "3530748c1b13e7c12ab59cb31d97bf0a86d01dda91676225d76bf0b209066bab" => :mojave
+    sha256 "2dda0b4cbd994c5017392eb43c870a85e7e567a0d13cba179a2786762067e7c2" => :high_sierra
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
 
