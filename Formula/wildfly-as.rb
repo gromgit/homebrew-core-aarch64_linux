bdae8037aca3b66fd3431b8c279017d371a55f04
chenrui <chenrui333@gmail.com>
2020-10-14 00:34:29 +0000

    wildfly-as 21.0.0
    * wildfly-as 21.0.0
    * wildfly: add license
    
    Closes #62843.
    
    Co-authored-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wildfly-as.rb
+++ Formula/wildfly-as.rb
@@ -1,8 +1,9 @@
 class WildflyAs < Formula
   desc "Managed application runtime for building applications"
   homepage "https://wildfly.org/"
-  url "https://download.jboss.org/wildfly/20.0.1.Final/wildfly-20.0.1.Final.tar.gz"
-  sha256 "63ced690c05149f444e8d0418c1d76ab82941d1e3763ef4c49b0c43de5f95ae7"
+  url "https://download.jboss.org/wildfly/21.0.0.Final/wildfly-21.0.0.Final.tar.gz"
+  sha256 "5454dc750c06fd052457d32f5a7ba352b2fb11602fd79f4f1789046d32da0d9f"
+  license "LGPL-2.1-or-later"
 
   livecheck do
     url "https://wildfly.org/downloads/"
