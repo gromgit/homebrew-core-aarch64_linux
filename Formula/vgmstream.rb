ab74209c8bb8afa04614389c5eea2ea779f73342
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-15 08:20:22 +0000

    vgmstream: update 405509 bottle.

--- Formula/vgmstream.rb
+++ Formula/vgmstream.rb
@@ -9,9 +9,9 @@ class Vgmstream < Formula
 
   bottle do
     cellar :any
-    sha256 "235ded9d960b2a99ceb17630c5cc0aa352b2afbadc90a01439a7ff481f7062bd" => :catalina
-    sha256 "584faaf779e7adcd1b8d2243641b80f277314588372f9bd8a137e9c4110b3a9e" => :mojave
-    sha256 "c70ca3ae85b5138dbb8d97cadaf459e77633e0b022c2f8c210be05cd2881544b" => :high_sierra
+    sha256 "74b726aca9e8a085ff1d5f4afe6f892f3620d2cb229c86276451fcb25e993a11" => :catalina
+    sha256 "b233a6c088526b989be281b44ff467aec792ff8cfc69afed7329f60f82b51eb5" => :mojave
+    sha256 "ffa46eab45d850eaa18f0f0e280a9e387d0332b89935e4a6cdc5f1e5455fd228" => :high_sierra
   end
 
   depends_on "cmake" => :build
