29d9b15abd8386ee67b35af6af478f126ac2e195
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-10-15 07:16:31 +0000

    cvs-fast-export: add 1.55 bottle.

--- Formula/cvs-fast-export.rb
+++ Formula/cvs-fast-export.rb
@@ -5,6 +5,13 @@ class CvsFastExport < Formula
   sha256 "af58e16667c6e02e8431ff666effe306d3b30086fab483170257890da1afc21b"
   license "GPL-2.0-or-later"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "57328518c731d726a7c7f4d1af902638c042711e79f73e34002d71f08a70d432" => :catalina
+    sha256 "70e1bc966678021164929473ca45b68b3fc3a1daa94646ab75c1e24d409ab4d6" => :mojave
+    sha256 "b95ad7499c774198531e33bbfc0dc6872af239d17e40a9e9fbf7102de8fdb067" => :high_sierra
+  end
+
   depends_on "asciidoc" => :build
   depends_on "docbook-xsl" => :build
   depends_on "cvs" => :test
