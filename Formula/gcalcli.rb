4777fe3eca13485319a0b0420ab30d54a7fc9451
Sebastian Rodriguez <331558+sebasrp@users.noreply.github.com>
2020-10-16 00:24:59 +0800

    use latest version of python

--- Formula/gcalcli.rb
+++ Formula/gcalcli.rb
@@ -12,7 +12,7 @@ class Gcalcli < Formula
     url :stable
   end
 
-  depends_on "python@3.8"
+  depends_on "python@3.9"
 
   resource "cachetools" do
     url "https://files.pythonhosted.org/packages/fc/c8/0b52cf3132b4b85c9e83faa3e4d375575afeb3a1710c40b2b2cd2a3e5635/cachetools-4.1.1.tar.gz"
