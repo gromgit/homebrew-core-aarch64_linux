1ed7bdf1701f0618ce440a2f792de59f9c8bba8e
Thierry Moisan <thierry.moisan@gmail.com>
2020-10-15 09:19:17 -0400

    languagetool: add license

--- Formula/languagetool.rb
+++ Formula/languagetool.rb
@@ -3,6 +3,7 @@ class Languagetool < Formula
   homepage "https://www.languagetool.org/"
   url "https://languagetool.org/download/LanguageTool-5.1.3.oxt"
   sha256 "3c3288a4a651553b3f9e762a027fab75a38b35050f522cbf8fce2554977dc7f0"
+  license "LGPL-2.1-or-later"
 
   livecheck do
     url "https://github.com/languagetool-org/languagetool.git"
