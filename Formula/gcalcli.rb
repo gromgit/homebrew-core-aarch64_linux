7a5ac5b19d5e1a0eaa4f49b2db303de556df0d20
Sebastian Rodriguez <331558+sebasrp@users.noreply.github.com>
2020-10-16 00:56:03 +0800

    fix findings in the brew audit and use pipe_output instead of capture

--- Formula/gcalcli.rb
+++ Formula/gcalcli.rb
@@ -129,13 +129,13 @@ class Gcalcli < Formula
   end
 
   test do
-    expected_output = """
-Go to the following link in your browser:
-
-    https://accounts.google.com/o/oauth2/v2/auth?client_id=foo&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar&access_type=offline&response_type=code
-
-Enter verification code: """
-    stdout_str, status = Open3.capture2("#{libexec}/bin/gcalcli --client-id=foo --client-secret=bar --noauth_local_webserver list", :stdin_data=>"foo")
-    assert_equal expected_output, stdout_str
+    expected_output = "\nGo to the following link in your browser:\n\n"\
+    "    https://accounts.google.com/o/oauth2/v2/auth?client_id=foo&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob"\
+    "&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar&access_type=offline&response_type=code\n\n"\
+    "Enter verification code: "
+    output = pipe_output(
+      "#{libexec}/bin/gcalcli --client-id=foo --client-secret=bar --noauth_local_webserver list", "foo"
+    )
+    assert_equal expected_output, output
   end
 end
