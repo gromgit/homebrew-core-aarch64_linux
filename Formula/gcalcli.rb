c52d580c2ba436b95630e7aec44b0e437b0c3cf8
Sebastian Rodriguez <331558+sebasrp@users.noreply.github.com>
2020-10-16 09:20:18 +0800

    fix line break of command

--- Formula/gcalcli.rb
+++ Formula/gcalcli.rb
@@ -133,9 +133,7 @@ class Gcalcli < Formula
     "    https://accounts.google.com/o/oauth2/v2/auth?client_id=foo&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob"\
     "&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar&access_type=offline&response_type=code\n\n"\
     "Enter verification code: "
-    output = pipe_output(
-      "#{bin}/gcalcli --client-id=foo --client-secret=bar --noauth_local_webserver list", "foo"
-    )
+    output = pipe_output("#{bin}/gcalcli --client-id=foo --client-secret=bar --noauth_local_webserver list", "foo")
     assert_equal expected_output, output
   end
 end
