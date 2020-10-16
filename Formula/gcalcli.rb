ae37bd10b9e80f60eecf608f23ca37c450ca9508
Sebastian Rodriguez <331558+sebasrp@users.noreply.github.com>
2020-10-16 16:34:17 +0800

    simplify test to make more robust
    Closes #62935.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/gcalcli.rb
+++ Formula/gcalcli.rb
@@ -129,11 +129,7 @@ class Gcalcli < Formula
   end
 
   test do
-    expected_output = "\nGo to the following link in your browser:\n\n"\
-    "    https://accounts.google.com/o/oauth2/v2/auth?client_id=foo&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob"\
-    "&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar&access_type=offline&response_type=code\n\n"\
-    "Enter verification code: "
     output = pipe_output("#{bin}/gcalcli --client-id=foo --client-secret=bar --noauth_local_webserver list", "foo")
-    assert_equal expected_output, output
+    assert_match "Go to the following link in your browser:", output
   end
 end
