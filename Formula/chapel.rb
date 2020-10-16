8a5d08fe5b95d5e4e9f59bc6d96ea2bfaee73999
chenrui <chenrui333@gmail.com>
2020-10-16 05:13:57 +0000

    chapel: update 1.23.0 bottle.

--- Formula/chapel.rb
+++ Formula/chapel.rb
@@ -6,9 +6,9 @@ class Chapel < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 "aa7c3e7f089ac71f88c51eb06d0551d78661cb4fadaa86d9807a908c45650df8" => :catalina
-    sha256 "e722faf3c5f799150134f179d81733830b9b838812bdec77d350e0f752f71a5e" => :mojave
-    sha256 "9ba754a9f0788efe6ff78a6218773a915078ef798c2a9c72defa12ff18374fd1" => :high_sierra
+    sha256 "5aa5b6a7e03ed702530959c1983e8989be5f1ba4494af6d9da044e38ef62bfcb" => :catalina
+    sha256 "123c5824b82621b4984e1afbf4f01f5c20af8cbc03e7b1266af396f8eaa80f79" => :mojave
+    sha256 "101b6a940e07d3e86ec809b3f7737950e0ef12c61daf43ef92c00e95047333c5" => :high_sierra
   end
 
   def install
