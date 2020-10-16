0fae3f6616d37608d3bf7d91404632b3f6de0397
BJ Hargrave <bj@bjhargrave.com>
2020-10-16 01:06:09 -0400

    bnd 5.2.0 (#62947)

--- Formula/bnd.rb
+++ Formula/bnd.rb
@@ -1,8 +1,8 @@
 class Bnd < Formula
   desc "Swiss Army Knife for OSGi bundles"
   homepage "https://bnd.bndtools.org/"
-  url "https://search.maven.org/remotecontent?filepath=biz/aQute/bnd/biz.aQute.bnd/5.1.2/biz.aQute.bnd-5.1.2.jar"
-  sha256 "dc94fa5b87c604d43205674af4a8e0af8c524ed29f92c59a401572174b99e273"
+  url "https://search.maven.org/remotecontent?filepath=biz/aQute/bnd/biz.aQute.bnd/5.2.0/biz.aQute.bnd-5.2.0.jar"
+  sha256 "9f84bac4a9c7477dad779033da141090caf054e796df739090746a79b5077dfe"
 
   bottle :unneeded
 
