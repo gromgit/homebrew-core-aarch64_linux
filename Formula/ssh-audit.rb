b13a33dee4f91042f83f501fc77cf3fd4c568a12
Joe Testa <jtesta@positronsecurity.com>
2020-10-16 01:09:13 -0400

    ssh-audit: add man page (#62955)
    * Added man page for ssh-audit.
    
    * Bumped revision field.

--- Formula/ssh-audit.rb
+++ Formula/ssh-audit.rb
@@ -6,7 +6,7 @@ class SshAudit < Formula
   url "https://github.com/jtesta/ssh-audit/releases/download/v2.3.0/ssh-audit-2.3.0.tar.gz"
   sha256 "776547591e7b69a2a8dcd1eaaac5d38321f2cb4a5de5f8e5a3e135b33236e812"
   license "MIT"
-  revision 1
+  revision 2
   head "https://github.com/jtesta/ssh-audit.git"
 
   bottle :unneeded
@@ -16,6 +16,7 @@ class SshAudit < Formula
   def install
     rewrite_shebang detected_python_shebang, "ssh-audit.py"
     bin.install "ssh-audit.py" => "ssh-audit"
+    man1.install "ssh-audit.1"
   end
 
   test do
