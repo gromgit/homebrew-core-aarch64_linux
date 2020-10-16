8c0ec4f309bcc97901ebe89e17d3f625c86d7c9c
chenrui <chenrui333@gmail.com>
2020-10-16 01:19:12 -0400

    bundletool 1.3.0 (#62938)

--- Formula/bundletool.rb
+++ Formula/bundletool.rb
@@ -1,8 +1,8 @@
 class Bundletool < Formula
   desc "Command-line tool to manipulate Android App Bundles"
   homepage "https://github.com/google/bundletool"
-  url "https://github.com/google/bundletool/releases/download/1.2.0/bundletool-all-1.2.0.jar"
-  sha256 "25ef168c6f9a14acd3113d4cffb3efde7ed8c94eeac50e4203be7a71cf1ca837"
+  url "https://github.com/google/bundletool/releases/download/1.3.0/bundletool-all-1.3.0.jar"
+  sha256 "b2ad86d5ee298e9b64dd5955243fee584124434eedbe3c6401607b5ebb4e2dc5"
   license "Apache-2.0"
 
   bottle :unneeded
