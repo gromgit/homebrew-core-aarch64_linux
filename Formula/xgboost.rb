dbc9612b4d36e5a674786fe3cb3da7fa67a2342b
Rui Chen <rui@meetup.com>
2020-10-17 17:14:39 -0400

    xgboost 1.2.1
    update deps
    
    Closes #63048.
    
    Signed-off-by: Rui Chen <rui@meetup.com>
    Signed-off-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xgboost.rb
+++ Formula/xgboost.rb
@@ -2,8 +2,8 @@ class Xgboost < Formula
   desc "Scalable, Portable and Distributed Gradient Boosting Library"
   homepage "https://xgboost.ai/"
   url "https://github.com/dmlc/xgboost.git",
-      tag:      "v1.2.0",
-      revision: "738786680b65b6f7716e9db2ae60161c6121926f"
+      tag:      "v1.2.1",
+      revision: "bcb15a980f611ac0b7540ddcb55091d0ab9655aa"
   license "Apache-2.0"
 
   bottle do
@@ -17,13 +17,13 @@ class Xgboost < Formula
   depends_on "libomp"
 
   resource "numpy" do
-    url "https://files.pythonhosted.org/packages/2c/2f/7b4d0b639a42636362827e611cfeba67975ec875ae036dd846d459d52652/numpy-1.19.1.zip"
-    sha256 "b8456987b637232602ceb4d663cb34106f7eb780e247d51a260b84760fd8f491"
+    url "https://files.pythonhosted.org/packages/bf/e8/15aea783ea72e2d4e51e3ec365e8dc4a1a32c9e5eb3a6d695b0d58e67cdd/numpy-1.19.2.zip"
+    sha256 "0d310730e1e793527065ad7dde736197b705d0e4c9999775f212b03c44a8484c"
   end
 
   resource "scipy" do
-    url "https://files.pythonhosted.org/packages/53/10/776750d57ade26522478a92a2e14035868624a6a62f4157b0cc5abd4a980/scipy-1.5.2.tar.gz"
-    sha256 "066c513d90eb3fd7567a9e150828d39111ebd88d3e924cdfc9f8ce19ab6f90c9"
+    url "https://files.pythonhosted.org/packages/93/63/4a566494594a13697c5d5d8a754d6e329d018ddf881520775e0229fa29ef/scipy-1.5.3.tar.gz"
+    sha256 "ddae76784574cc4c172f3d5edd7308be16078dd3b977e8746860c76c195fa707"
   end
 
   def install
