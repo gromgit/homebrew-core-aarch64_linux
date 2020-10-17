e4ba6ade71cb4402e8c3999375a3e9969a6f8322
Rui Chen <rui@meetup.com>
2020-10-17 16:49:54 -0400

    mrboom 5.0
    Signed-off-by: Rui Chen <rui@meetup.com>

--- Formula/mrboom.rb
+++ Formula/mrboom.rb
@@ -1,8 +1,8 @@
 class Mrboom < Formula
   desc "Eight player Bomberman clone"
   homepage "http://mrboom.mumblecore.org/"
-  url "https://github.com/Javanaise/mrboom-libretro/archive/4.9.tar.gz"
-  sha256 "062cf1f91364d2d6ea717e92304ca163cfba5d14b30bb440ee118d1b8e10328d"
+  url "https://github.com/Javanaise/mrboom-libretro/archive/5.0.tar.gz"
+  sha256 "71af5a47ca5a8972598fe388b653120315bd2e7e05e99a3ee68637c2f74688fc"
   license "MIT"
 
   bottle do
@@ -17,12 +17,6 @@ class Mrboom < Formula
   depends_on "minizip"
   depends_on "sdl2"
   depends_on "sdl2_mixer"
-
-  # fix Makefile issue, remove in next release
-  patch do
-    url "https://github.com/Javanaise/mrboom-libretro/commit/c777f1059c9a4b3fcefe6e2a19cfe9f81a13740b.diff?full_index=1"
-    sha256 "19f469ccde5f1a9bc45fa440fd4cbfd294947f17b191f299822db17de66a5a23"
-  end
 
   def install
     system "make", "mrboom", "LIBSDL2=1"
