991d83a188e5abe10a15c1d000e6f7f57119174f
FX Coudert <fxcoudert@gmail.com>
2020-10-18 22:50:34 +0000

    sk: update 0.9.0 bottle.

--- Formula/sk.rb
+++ Formula/sk.rb
@@ -8,9 +8,9 @@ class Sk < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5292b952619fdff305f8124e8ff7342487c2b8904582d21d7d36ee70552e55cb" => :catalina
-    sha256 "88cd830c34f998238c0bc89e435fa114fd89081bada5e6c04baed79d5f97f2c9" => :mojave
-    sha256 "cdf01b18bb6d20bbe3063246520e09caf0591490212e1ced07e996027a87dc32" => :high_sierra
+    sha256 "efada87a283b9ae7d664abd02c6cc7d426ebf139c96962655e4dbc90188b0e00" => :catalina
+    sha256 "8275445e131d4806b073b857cde306d19eb7e6b1b37c7cf17d8d81b57b5698e8" => :mojave
+    sha256 "d6ec21c17486089cf527ab051ddecff0401b8773cd9582bc02e0fcbdd4896538" => :high_sierra
   end
 
   depends_on "rust" => :build
