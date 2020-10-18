14ba39331732e6cc74e8c8e61817c80ccb8175a1
chenrui <chenrui333@gmail.com>
2020-10-18 05:35:58 +0000

    go: update 1.15.3 bottle.

--- Formula/go.rb
+++ Formula/go.rb
@@ -21,9 +21,9 @@ class Go < Formula
   end
 
   bottle do
-    sha256 "dc31fb80b6f6d7d8db13db55d2994fc77d37ffeffcd56c755fbc2674a038227c" => :catalina
-    sha256 "a85ee24aaee867a45aff503a0f70da50310390374f1a2e9cf793834f9ca474e6" => :mojave
-    sha256 "abc48dd817025641accdcfac9c018db8f95d4bbeb347e4ca006a48d97f6ad82d" => :high_sierra
+    sha256 "6bd0119a31c1ae26831105490a67b97bed5d0fd2c6ad140cdf4c84aa7a315af2" => :catalina
+    sha256 "6251ba0acffd00c58adb27bdeeb68213ec7723da54d83581272fa19f82a21137" => :mojave
+    sha256 "48df8ba509f14e4f28236fc74217233d62ab352a2f4db673c5081c163d92a35b" => :high_sierra
   end
 
   head do
