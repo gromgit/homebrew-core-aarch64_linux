fe0f7273da895469414096480ddc7231d5de4c70
Dustin Rodrigues <dust.rod@gmail.com>
2020-10-18 10:32:10 -0400

    swaks: add license (#63067)

--- Formula/swaks.rb
+++ Formula/swaks.rb
@@ -3,6 +3,7 @@ class Swaks < Formula
   homepage "https://www.jetmore.org/john/code/swaks/"
   url "https://www.jetmore.org/john/code/swaks/files/swaks-20201014.0.tar.gz"
   sha256 "fb0a3b7d487a15b124ba6690f7b01a56617f1af2aa54233fd69013982de95a30"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url "https://www.jetmore.org/john/code/swaks/versions.html"
