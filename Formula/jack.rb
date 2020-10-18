03460bec8a6dae0d5876639e932a289124c5c2d7
Padraic Fanning <fanninpm@miamioh.edu>
2020-10-18 15:35:52 -0400

    jack 1.9.16
    jack2 1.9.16
    
    Add python 3.9 as a build dependency
    
    Make python 3.9 a general dependency
    
    Add pkg-config as a build dependency
    
    Add autoconf as a build dependency
    
    Add automake as a build dependency
    
    Add libtool as a build dependency
    
    Add other dependencies
    
    Reconfigure python usage w/r/t waf
    
    Reorder dependencies
    
    Add dylib compatibility patches
    
    Use full_index parameter in patch links
    
    Add compatibility version sanity check
    
    Make sanity check more rigorous
    
    Remove dependency on macos
    
    Remove unnecessary patches and sanity check
    
    Re-add python@3.9 as a build dependency
    
    Strengthen python integration
    
    Set LDFLAGS to make 10.13 integration tests pass
    
    Change version string to dynamic version string
    
    Revert "Change version string to dynamic version string"
    
    This reverts commit 1aa6930cdf236d71791e4a9cc61bd4e7a019d362.
    
    Revert earlier reversion
    
    This reverts commit 0e523888b2ca449539a0c783d540eb7581ce283a.
    
    Remove extraneous  commands
    
    Remove extraneous  block
    
    Revert "Remove extraneous  block"
    
    This reverts commit d4a8fba5c4d5fdc8d23382206d9b43f00fc162dc.
    
    Delete extraneous ensure block
    
    Change IO.popen instances to 'fork do' blocks
    
    Add marshal to try to extract forked output
    
    Remove marshal from test
    
    Change wait to kill
    
    Revert midi_sink fork to IO.popen
    
    This allows the test to take the standard output of the midi_sink_cmd.
    
    Refactor the test to reduce variable usage
    
    Remove comment at top of formula
    
    Change "system" to "exec" to start dummy
    
    Change "system" to "exec" to start midi source
    
    Closes #63082.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jack.rb
+++ Formula/jack.rb
@@ -1,20 +1,9 @@
-# This now builds a version of JACKv1 which matches the current API
-# for JACKv2. JACKv2 is not buildable on a number of macOS
-# distributions, and the JACK team instead suggests installation of
-# JACKOSX, a pre-built binary form for which the source is not available.
-# If you require JACKv2, you should use that. Otherwise, this formula should
-# operate fine.
-# Please see https://github.com/Homebrew/homebrew/pull/22043 for more info
 class Jack < Formula
   desc "Audio Connection Kit"
   homepage "https://jackaudio.org/"
-  # pull from git tag to get submodules
-  url "https://github.com/jackaudio/jack1.git",
-      tag:      "0.125.0",
-      revision: "f5e00e485e7aa4c5baa20355b27e3b84a6912790"
-  license "GPL-2.0"
-  revision 5
-  head "https://github.com/jackaudio/jack1.git"
+  url "https://github.com/jackaudio/jack2/archive/v1.9.16.tar.gz"
+  sha256 "e176d04de94dcaa3f9d32ca1825091e1b938783a78c84e7466abd06af7637d37"
+  license "GPL-2.0-or-later"
 
   bottle do
     sha256 "f06b40271bfe65dc21417b614fa485dbe86d0f1567c52edf95d6fb1cd01323bc" => :big_sur
@@ -27,33 +16,22 @@ class Jack < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
+  depends_on "aften"
   depends_on "berkeley-db"
   depends_on "libsamplerate"
   depends_on "libsndfile"
-
-  on_linux do
-    depends_on "util-linux"
-  end
+  depends_on "python@3.9"
 
   def install
-    sdk = MacOS.sdk_path_if_needed ? MacOS.sdk_path : ""
-
-    # Makefile hardcodes Carbon header location
-    inreplace Dir["drivers/coreaudio/Makefile.{am,in}"],
-      "/System/Library/Frameworks/Carbon.framework/Headers/Carbon.h",
-      "#{sdk}/System/Library/Frameworks/Carbon.framework/Headers/Carbon.h"
-
-    # https://github.com/jackaudio/jack1/issues/81
-    inreplace "configure.ac", "-mmacosx-version-min=10.4",
-                              "-mmacosx-version-min=#{MacOS.version}"
-
-    system "./autogen.sh"
-    ENV["LINKFLAGS"] = ENV.ldflags
-    system "./configure", "--prefix=#{prefix}"
-    system "make", "install"
+    # See https://github.com/jackaudio/jack2/issues/640#issuecomment-723022578
+    ENV.append "LDFLAGS", "-Wl,-compatibility_version,1" if MacOS.version <= :high_sierra
+    ENV.append "LDFLAGS", "-Wl,-current_version,#{version}" if MacOS.version <= :high_sierra
+    system Formula["python@3.9"].opt_bin/"python3", "./waf", "configure", "--prefix=#{prefix}"
+    system Formula["python@3.9"].opt_bin/"python3", "./waf", "build"
+    system Formula["python@3.9"].opt_bin/"python3", "./waf", "install"
   end
 
-  plist_options manual: "jackd -d coreaudio"
+  plist_options manual: "jackd -X coremidi -d coreaudio"
 
   def plist
     <<~EOS
@@ -73,6 +51,8 @@ class Jack < Formula
         <key>ProgramArguments</key>
         <array>
           <string>#{opt_bin}/jackd</string>
+          <string>-X</string>
+          <string>coremidi</string>
           <string>-d</string>
           <string>coreaudio</string>
         </array>
@@ -86,6 +66,23 @@ class Jack < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/jackd --version")
+    source_name = "test_source"
+    sink_name = "test_sink"
+    fork do
+      exec "#{bin}/jackd", "-X", "coremidi", "-d", "dummy"
+    end
+    system "#{bin}/jack_wait", "--wait", "--timeout", "10"
+    fork do
+      exec "#{bin}/jack_midiseq", source_name, "16000", "0", "60", "8000"
+    end
+    midi_sink = IO.popen "#{bin}/jack_midi_dump #{sink_name}"
+    sleep 1
+    system "#{bin}/jack_connect #{source_name}:out #{sink_name}:input"
+    sleep 1
+    Process.kill "TERM", midi_sink.pid
+
+    midi_dump = midi_sink.read
+    assert_match "90 3c 40", midi_dump
+    assert_match "80 3c 40", midi_dump
   end
 end
