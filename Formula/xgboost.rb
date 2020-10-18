cdac87f4ce865092b53efca98b68988fe5bf7b65
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-18 11:17:04 +0000

    xgboost: update 1.2.1 bottle.

--- Formula/xgboost.rb
+++ Formula/xgboost.rb
@@ -8,9 +8,9 @@ class Xgboost < Formula
 
   bottle do
     cellar :any
-    sha256 "c8d19350dfc4643d4336a58c88a405edc804c1b0513c83b50cb5a72a89c8dc32" => :catalina
-    sha256 "be8665b0eb41df7319746c723d438241ccca5b28847524193ceb6a37a5708a77" => :mojave
-    sha256 "2d0116ba64bcfdc82f00a3da16bd243a59ddd970fb661e095891451668533f6e" => :high_sierra
+    sha256 "6458d02e2907e74a9cc7b4d6ae22d132a7dc83f37a13e79a87f409567d46bd68" => :catalina
+    sha256 "bd6bdd967e100e38130b834f93b01a82c3cfa1ea6581533ffa0d897d611656c1" => :mojave
+    sha256 "5ea7f8b48ded860e69166b8f7f5adedc34c7007fab4154f5d802ee126d09bc3f" => :high_sierra
   end
 
   depends_on "cmake" => :build
