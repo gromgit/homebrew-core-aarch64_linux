01bcfe09e63691b4aeb14bde41ee7ec020fbd2ee
Alex Tomkins <tomkins@darkzone.net>
2020-10-19 16:54:17 +0100

    sfcgal: fix sha256
    v1.3.9 was re-released: https://gitlab.com/Oslandia/SFCGAL/-/issues/236
    
    Closes #63110.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sfcgal.rb
+++ Formula/sfcgal.rb
@@ -2,8 +2,9 @@ class Sfcgal < Formula
   desc "C++ wrapper library around CGAL"
   homepage "http://sfcgal.org/"
   url "https://gitlab.com/Oslandia/SFCGAL/-/archive/v1.3.9/SFCGAL-v1.3.9.tar.gz"
-  sha256 "4f8ecfaa0d9ac5d8d7969f767ce3abadc7cc64059a11481e4bc497f04d5f6598"
+  sha256 "2451cb6df24853c7e59173eec0068e3263ab625fcf61add4624f8bf8366ae4e3"
   license "LGPL-2.0+"
+  revision 1
 
   bottle do
     sha256 "8cc583e706bc2755d61c362afbdd0cc627c6126113d4d6c5c0cc1f1bb62e8dfa" => :catalina
