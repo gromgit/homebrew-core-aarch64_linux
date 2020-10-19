3e42d0d65ff608435a5cae5fd07d0f39462fa264
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-19 23:17:16 +0000

    vttest: update 20200920 bottle.

--- Formula/vttest.rb
+++ Formula/vttest.rb
@@ -7,9 +7,9 @@ class Vttest < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "4ed2226713142cbe3358b35cc20b1b5f566e8a337593db770d395c47ce807d08" => :catalina
-    sha256 "150b0841791012479c9afda2f44511595d38685e7c4720a4a29c9298409a72be" => :mojave
-    sha256 "5bcfa979eae920e1e2e87da085ebd6060ee449fee35f464db1bfb86d4528c486" => :high_sierra
+    sha256 "18550bfedc0f4b511f56c564509b911b2a62a26b24e6b9d95093e44300be1ece" => :catalina
+    sha256 "e3bda0ee8aa89fe4197dddb7fcddaf510424b929667a029a4c05a67824e0a46b" => :mojave
+    sha256 "80644da812e6ee1c267ef9007ae5d1fb65538e732f18c1dc3851860a4e0495ad" => :high_sierra
   end
 
   def install
