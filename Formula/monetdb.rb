12f5b660811587c1705d005aa48f07b94aa3cf74
Sjoerd Mullender <sjoerd@acm.org>
2020-10-19 11:37:35 +0200

    monetdb 11.39.5
    Change call to sed into inreplace, add python3.9 build dependency.
    Be explicit in what we want to include + declare dependencies.
    
    Closes #63094.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/monetdb.rb
+++ Formula/monetdb.rb
@@ -1,8 +1,10 @@
 class Monetdb < Formula
   desc "Column-store database"
   homepage "https://www.monetdb.org/"
-  url "https://www.monetdb.org/downloads/sources/Jun2020-SP1/MonetDB-11.37.11.tar.xz"
-  sha256 "3cadb3ea42aa6205678dd532756f5e9b6f47650f18463db83f16c8dc55a4325c"
+  url "https://www.monetdb.org/downloads/sources/Oct2020/MonetDB-11.39.5.tar.xz"
+  sha256 "49654be66305cffa9c59959a6c17897e1f2cb4dbaecde66be76837cc7f01fd75"
+  license "MPL-2.0"
+  head "https://dev.monetdb.org/hg/MonetDB", using: :hg
 
   bottle do
     sha256 "21b3726ec5e83c3ebd0089c0ffba9b612bf282569e46b4b8006d5c1d7da04ca5" => :catalina
@@ -10,35 +12,48 @@ class Monetdb < Formula
     sha256 "b2b72d85df319b66f74bbfefd3d47ca81ce0a76cd085344a0f9732ece2004451" => :high_sierra
   end
 
-  head do
-    url "https://dev.monetdb.org/hg/MonetDB", using: :hg
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "bison" => :build
-    depends_on "gettext" => :build
-    depends_on "libtool" => :build
-  end
-
-  depends_on "libatomic_ops" => :build
+  depends_on "bison" => :build  # macOS bison is too old
+  depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "python@3.9" => :build
+  depends_on "lz4"
   depends_on "openssl@1.1"
   depends_on "pcre"
   depends_on "readline" # Compilation fails with libedit
+  depends_on "xz"
 
   def install
-    ENV["M4DIRS"] = "#{Formula["gettext"].opt_share}/aclocal" if build.head?
-    system "./bootstrap" if build.head?
-
-    system "./configure", "--prefix=#{prefix}",
-                          "--enable-assert=no",
-                          "--enable-debug=no",
-                          "--enable-optimize=yes",
-                          "--enable-testing=no",
-                          "--with-readline=#{Formula["readline"].opt_prefix}",
-                          "--disable-rintegration"
-    system "make"
-    system "make", "install"
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args,
+                      "-DRELEASE_VERSION=ON",
+                      "-DCMAKE_BUILD_TYPE=Release",
+                      "-DASSERT=OFF",
+                      "-DSTRICT=OFF",
+                      "-DTESTING=OFF",
+                      "-DFITS=OFF",
+                      "-DGEOM=OFF",
+                      "-DNETCDF=OFF",
+                      "-DODBC=OFF",
+                      "-DPY3INTEGRATION=OFF",
+                      "-DRINTEGRATION=OFF",
+                      "-DSHP=OFF",
+                      "-DWITH_BZ2=ON",
+                      "-DWITH_CMOCKA=OFF",
+                      "-DWITH_CURL=ON",
+                      "-DWITH_LZ4=ON",
+                      "-DWITH_LZMA=ON",
+                      "-DWITH_PCRE=ON",
+                      "-DWITH_PROJ=OFF",
+                      "-DWITH_SNAPPY=OFF",
+                      "-DWITH_XML2=ON",
+                      "-DWITH_ZLIB=ON",
+                      "-DOPENSSL_ROOT_DIR=#{Formula["openssl@1.1"].opt_prefix}",
+                      "-DREADLINE_ROOT=#{Formula["readline"].opt_prefix}"
+      # remove reference to shims directory from compilation/linking info
+      inreplace "tools/mserver/monet_version.c", %r{"/[^ ]*/}, "\""
+      system "cmake", "--build", "."
+      system "cmake", "--build", ".", "--target", "install"
+    end
   end
 
   test do
