4ff44ec76ecc0335c80e38e3ae80575bcff89206
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-10-19 12:37:57 +0000

    or-tools: add 8.0 bottle.

--- Formula/or-tools.rb
+++ Formula/or-tools.rb
@@ -6,6 +6,13 @@ class OrTools < Formula
   license "Apache-2.0"
   head "https://github.com/google/or-tools.git"
 
+  bottle do
+    cellar :any
+    sha256 "7a119a20186b34d80b7a36e3cd18f2e7bc6561cd70e52ea05231422a38fdcd1f" => :catalina
+    sha256 "61e99ba40756cdc5d4fd5cbff6efc8ed3bf82ea187396895f0e310946105591a" => :mojave
+    sha256 "f5b9f71179930075b58dcc26f0ebe69a4ae34e7f528347f685116690cc23089d" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "abseil"
