06a4f3454422eb53ed635081aba065cfcc6aa599
Petr Pučil <petr.pucil@seznam.cz>
2020-10-20 20:48:26 +0200

    kaitai-struct-compiler 0.9 (#63145)
    * kaitai-struct-compiler 0.9
    
    * kaitai-struct-compiler 0.9: Fix tests
    
    * kaitai-struct-compiler: Add 'license' field

--- Formula/kaitai-struct-compiler.rb
+++ Formula/kaitai-struct-compiler.rb
@@ -1,9 +1,9 @@
 class KaitaiStructCompiler < Formula
   desc "Compiler for generating binary data parsers"
   homepage "https://kaitai.io/"
-  url "https://bintray.com/artifact/download/kaitai-io/universal/0.8/kaitai-struct-compiler-0.8.zip"
-  sha256 "545fc10e134db2901cad8817be1b440fca6f2bad8b92b2948ebe0647f3ffa2c9"
-  revision 2
+  url "https://bintray.com/artifact/download/kaitai-io/universal/0.9/kaitai-struct-compiler-0.9.zip"
+  sha256 "3038243334fb65bbb264f33b82986facfe1fbad2de1978766899855b40212215"
+  license "GPL-3.0-or-later"
 
   bottle :unneeded
 
@@ -26,6 +26,6 @@ class KaitaiStructCompiler < Formula
           type: u4
     EOS
     system bin/"kaitai-struct-compiler", "Test.ksy", "-t", "java", "--outdir", testpath
-    assert_predicate testpath/"src/Test.java", :exist?
+    assert_predicate testpath/"Test.java", :exist?
   end
 end
