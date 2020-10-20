d1da2dcfaad362786f79ae7c4df76bc16338c980
chenrui <chenrui333@gmail.com>
2020-10-20 12:16:56 +0000

    detekt 1.14.2
    Closes #63156.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/detekt.rb
+++ Formula/detekt.rb
@@ -1,8 +1,8 @@
 class Detekt < Formula
   desc "Static code analysis for Kotlin"
   homepage "https://github.com/detekt/detekt"
-  url "https://jcenter.bintray.com/io/gitlab/arturbosch/detekt/detekt-cli/1.14.1/detekt-cli-1.14.1-all.jar"
-  sha256 "ad520ce281ee14604b343cf0625035e309cf4098b04cb05f30d28dc1cc8765a6"
+  url "https://jcenter.bintray.com/io/gitlab/arturbosch/detekt/detekt-cli/1.14.2/detekt-cli-1.14.2-all.jar"
+  sha256 "04d3e959061c546af3cea9e08794621c03a6caf157f6847a31b4d13d55df961a"
   license "Apache-2.0"
 
   livecheck do
