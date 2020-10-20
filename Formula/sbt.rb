4c7fc365bcabdf1b385ef9c64254d4b90ca08d43
chenrui <chenrui333@gmail.com>
2020-10-20 00:38:30 +0000

    sbt 1.4.1

--- Formula/sbt.rb
+++ Formula/sbt.rb
@@ -1,9 +1,9 @@
 class Sbt < Formula
   desc "Build tool for Scala projects"
   homepage "https://www.scala-sbt.org/"
-  url "https://github.com/sbt/sbt/releases/download/v1.4.0/sbt-1.4.0.tgz"
-  mirror "https://sbt-downloads.cdnedge.bluemix.net/releases/v1.4.0/sbt-1.4.0.tgz"
-  sha256 "b4775b470920e03de7a5d81121b4dc741c00513f041e65dbb981052ec6d1eed5"
+  url "https://github.com/sbt/sbt/releases/download/v1.4.1/sbt-1.4.1.tgz"
+  mirror "https://sbt-downloads.cdnedge.bluemix.net/releases/v1.4.1/sbt-1.4.1.tgz"
+  sha256 "5cf648f18ee9573cd26970999ae4e76ac034721a671bb45e7311c6d1375f9d33"
   license "Apache-2.0"
 
   bottle :unneeded
