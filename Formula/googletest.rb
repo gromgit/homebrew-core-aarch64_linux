d73e7eb31bb2ac809d5482e33123cbdefffc592c
chenrui <chenrui333@gmail.com>
2020-10-20 01:16:27 +0000

    googletest: add 1.10.0 bottle.

--- Formula/googletest.rb
+++ Formula/googletest.rb
@@ -5,6 +5,13 @@ class Googletest < Formula
   sha256 "9dc9157a9a1551ec7a7e43daea9a694a0bb5fb8bec81235d8a1e6ef64c716dcb"
   license "BSD-3-Clause"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ebe4172d84fd8d29282e61c810e1f99b3b6b78ac0369381b617755cd070371c6" => :catalina
+    sha256 "7eface4df1365cd951d402e244a28984a2f656afb2e3fee31cf046f1a8aefaff" => :mojave
+    sha256 "595a13da3151557305ddf8fc479fcab3cd6041c2ee9832fe18a9f717046116b1" => :high_sierra
+  end
+
   depends_on "cmake" => :build
 
   def install
