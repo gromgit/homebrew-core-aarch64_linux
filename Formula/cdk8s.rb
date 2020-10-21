158f1bc18db06ab8fe4b44ebb981c21b25c4d68d
Dustin Rodrigues <dust.rod@gmail.com>
2020-10-21 07:24:30 -0400

    cdk8s 0.33.0
    Closes #63212.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cdk8s.rb
+++ Formula/cdk8s.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Cdk8s < Formula
   desc "Define k8s native apps and abstractions using object-oriented programming"
   homepage "https://github.com/awslabs/cdk8s"
-  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-0.32.0.tgz"
-  sha256 "88f78e0d015e5ab5bbf1c0254946b31d59c274a5fc02513204a695812b9d57fe"
+  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-0.33.0.tgz"
+  sha256 "e18eeb79e8764fb4da4c92fb0a14d4d44e06b0c571d3e719cb19ede1b36d6572"
   license "Apache-2.0"
 
   livecheck do
