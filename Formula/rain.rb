ecc0d4533e5a7cb629bc533641a45fcb6be62c1f
Dawid Dziurla <dawidd0811@gmail.com>
2020-10-21 17:45:12 +0200

    Apply suggestions from code review

--- Formula/rain.rb
+++ Formula/rain.rb
@@ -8,8 +8,7 @@ class Rain < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build"
-    bin.install "rain" => "rain"
+    system "go", "build", *std_go_args
   end
 
   test do
