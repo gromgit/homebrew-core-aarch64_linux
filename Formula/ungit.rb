44ea9381bb8e692eadead3b0f55a0b78dafb0b71
Dustin Rodrigues <dust.rod@gmail.com>
2020-10-21 07:25:33 -0400

    ungit 1.5.12
    Closes #63214.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ungit.rb
+++ Formula/ungit.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Ungit < Formula
   desc "Easiest way to use Git. On any platform. Anywhere"
   homepage "https://github.com/FredrikNoren/ungit"
-  url "https://registry.npmjs.org/ungit/-/ungit-1.5.11.tgz"
-  sha256 "1f63fc4e6697396c61a01917474409c9a51c4ee120e4bb8bec2e4a177bc3f1e9"
+  url "https://registry.npmjs.org/ungit/-/ungit-1.5.12.tgz"
+  sha256 "8a52eec719818d520d2e774cf032126b7a5edfc2bd80c777202100bb2049c4d8"
   license "MIT"
 
   livecheck do
