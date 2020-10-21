508e3af5a8cdbaa9d15d7e08bc67bbbac8589b84
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-21 07:19:33 +0000

    sparse: update 0.6.3 bottle.

--- Formula/sparse.rb
+++ Formula/sparse.rb
@@ -6,9 +6,9 @@ class Sparse < Formula
   head "https://git.kernel.org/pub/scm/devel/sparse/sparse.git"
 
   bottle do
-    sha256 "7b68cfced4d0ab9aa951900794eae5610ff9f6641b57a3fc6cd8a6d61b69f3f7" => :catalina
-    sha256 "6f9eb074c79700342aa36a6359079e8d853e5a6b9d6203459b62118b747e1210" => :mojave
-    sha256 "b74ccd0fe82e07b7d0552954634e1e7a8395ceaae09b8405d8460ce3b828e75d" => :high_sierra
+    sha256 "1fb15ba5444a7c67d9f45215fc02e948583c28d8078546b3e0f766df50a1a859" => :catalina
+    sha256 "ac0ade8d4c0f98cadcca728fdd3c0694e3d3a4432c57edf0725542a91d750128" => :mojave
+    sha256 "7dcabb27270d98a6ad13d6b2c6b8c1bf3f0a9fa001a9737db841f1cd604dbeec" => :high_sierra
   end
 
   depends_on "gcc" if DevelopmentTools.clang_build_version < 1100
