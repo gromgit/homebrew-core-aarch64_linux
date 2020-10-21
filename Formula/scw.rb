7362564802b45b3402ab2cd552af6386cd1a1a57
chenrui <chenrui333@gmail.com>
2020-10-21 03:54:54 +0000

    scw: update 2.2.1 bottle.

--- Formula/scw.rb
+++ Formula/scw.rb
@@ -7,9 +7,9 @@ class Scw < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c11b7fc563ff4f88728f913865037eb60eb8fd9a6e4de2cd72967b3c6d32c4d2" => :catalina
-    sha256 "9729fc288de05cfb1ad122619ab247a497e6d53f32202430ad5e47b25d015570" => :mojave
-    sha256 "4dcbfe7942ee684dbc6c8779c7e424f7d6a25a3b0bfc1e4902a5d4f755a05ecb" => :high_sierra
+    sha256 "6eeba561b3bdbe6dff8ff4f49c5bf33d04e2156fb00c6cc5d50c8241ff93476b" => :catalina
+    sha256 "7cbf5837c44e54e63cece810fbb94d3a4ff41dc1a93fd6c9332fd624e43bd17d" => :mojave
+    sha256 "9137a7a6ca1766eeec847760ce8c93087cd8858e8b9bb2465ef5856de80b4b81" => :high_sierra
   end
 
   depends_on "go" => :build
