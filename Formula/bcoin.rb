a617f2bdb912b069ade38da300ca282458180fdf
Diogo Azevedo <diogoazevedos@gmail.com>
2020-10-21 09:23:03 +0200

    bcoin: revision for node
    Closes #63170.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/bcoin.rb
+++ Formula/bcoin.rb
@@ -6,7 +6,7 @@ class Bcoin < Formula
   url "https://github.com/bcoin-org/bcoin/archive/v2.1.2.tar.gz"
   sha256 "b4c63598ee1efc17e4622ef88c1dff972692da1157e8daf7da5ea8abc3d234df"
   license "MIT"
-  revision 1
+  revision 2
   head "https://github.com/bcoin-org/bcoin.git"
 
   bottle do
@@ -19,6 +19,8 @@ class Bcoin < Formula
   depends_on "node"
 
   def install
+    # These dirs must exists before npm install.
+    mkdir_p libexec/"lib"
     system "#{Formula["node"].libexec}/bin/npm", "install", *Language::Node.std_npm_install_args(libexec)
     (bin/"bcoin").write_env_script libexec/"bin/bcoin", PATH: "#{Formula["node"].opt_bin}:$PATH"
   end
