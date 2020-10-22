2c124a4ad1f0c7ddcdce706d0395d78229efc4f3
Francois Marier <francois@fmarier.org>
2020-10-22 07:40:55 -0700

    safe-rm: rebuild
    Update git repository
    
    The repository has moved to Launchpad: https://code.launchpad.net/safe-rm
    
    Closes #63301.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

--- Formula/safe-rm.rb
+++ Formula/safe-rm.rb
@@ -4,7 +4,7 @@ class SafeRm < Formula
   url "https://launchpad.net/safe-rm/trunk/0.13/+download/safe-rm-0.13.tar.gz"
   sha256 "f0f792faa076fbf3dbffbb4bc8918fd6bf699775417fef3f5f408f0c69200c12"
   license "GPL-3.0-or-later"
-  head "https://repo.or.cz/safe-rm.git"
+  head "https://git.launchpad.net/safe-rm"
 
   livecheck do
     url :stable
