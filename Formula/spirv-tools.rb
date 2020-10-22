c4da0a9a5ce6e47b4c806f4c8c527f0c0f911232
Jonathan Chang <me@jonathanchang.org>
2020-10-22 23:21:01 +1100

    spirv-tools: update 2020.3_1 bottle.

--- Formula/spirv-tools.rb
+++ Formula/spirv-tools.rb
@@ -8,9 +8,9 @@ class SpirvTools < Formula
 
   bottle do
     cellar :any
-    sha256 "eb2e8acb244d040f0bbc586c037650a13672c32b5894fbe54be1531eb649ee35" => :catalina
-    sha256 "5874ff4a3b024d83c6b203e75087717951b571352bf1a25a59aeb4e58e5194fc" => :mojave
-    sha256 "f9f5ffdf2de7a2650b251aad002e94b27d16818172f2a168ca51cda8b80bf74a" => :high_sierra
+    sha256 "b150e16e5d4d40f29dfd739bf297cebfa232d3ae267fb7965d4151a8ace43ec4" => :catalina
+    sha256 "f5ddf173300822bf7f7f0b42bafdf1d242b1de1b63a565d02e0084f7b4107d36" => :mojave
+    sha256 "4bcbda12ffa24d5bcc19271cc318a035130d4f168145397704d0ef98fe1adda5" => :high_sierra
   end
 
   depends_on "cmake" => :build
