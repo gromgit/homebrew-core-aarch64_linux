3fe9fe41da782f354827ff0562b5869bc576b91a
Jonathan Chang <me@jonathanchang.org>
2020-10-22 23:20:53 +1100

    gpgme: update 1.14.0_1 bottle.

--- Formula/gpgme.rb
+++ Formula/gpgme.rb
@@ -12,9 +12,9 @@ class Gpgme < Formula
 
   bottle do
     cellar :any
-    sha256 "9adeee9e826faf7e4955cf5e7da6a3c17b6c31a8c96b27eb486201db01fae024" => :catalina
-    sha256 "8d5d6da3d5161f149e2b1b724b058f46c98c444af366a11165fb3cb2afd7cdf6" => :mojave
-    sha256 "370890e7494a7be13e88493520715c48d2d45217daf0419594c1e78d06c6d8ac" => :high_sierra
+    sha256 "fa8c359b9ca00fc0269f26243da8a1032f6a75a01196942b3fbf5ab0933bca23" => :catalina
+    sha256 "95e303f1b3f7201c9d684d84a5fa849dfa5942dc623a810f9f383f3331d6bb25" => :mojave
+    sha256 "3fa5a848421bcf9f36106f49d9fde42bd59999cfbfd57c9d2b6fbd0da095ba1b" => :high_sierra
   end
 
   depends_on "python@3.9" => [:build, :test]
