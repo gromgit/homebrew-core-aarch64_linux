bc08fb246a2a067c379425f40598fbdaae7f322b
Jonathan Chang <me@jonathanchang.org>
2020-10-22 23:21:01 +1100

    itstool: update 2.0.6_2 bottle.

--- Formula/itstool.rb
+++ Formula/itstool.rb
@@ -9,9 +9,9 @@ class Itstool < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "73238debce4f4c1e9922427a22312161d0400d70f22b642137689b4431ba0a20" => :catalina
-    sha256 "73238debce4f4c1e9922427a22312161d0400d70f22b642137689b4431ba0a20" => :mojave
-    sha256 "73238debce4f4c1e9922427a22312161d0400d70f22b642137689b4431ba0a20" => :high_sierra
+    sha256 "f860a74756beaab039bffa02a4c8b8258f1a54a692532f4a1e57d0b4431c7ab9" => :catalina
+    sha256 "d3b26ca21d37e4e0eb6e7318571a69aa021034bc69936749e8891213c16465c9" => :mojave
+    sha256 "1ee274a6df78727bfcba1221ea16b5c2fa55819c66e2de9168c7915fd3238508" => :high_sierra
   end
 
   depends_on "autoconf" => :build
