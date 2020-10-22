9daec3d10fc92431774614219a497370d6aedf4c
Rui Chen <chenrui333@gmail.com>
2020-10-22 01:27:28 -0400

    tfsec 0.31.0
    Closes #63279.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tfsec.rb
+++ Formula/tfsec.rb
@@ -1,8 +1,8 @@
 class Tfsec < Formula
   desc "Static analysis powered security scanner for your terraform code"
   homepage "https://github.com/tfsec/tfsec"
-  url "https://github.com/tfsec/tfsec/archive/v0.30.1.tar.gz"
-  sha256 "95c0b856c95185315cdad4b8442b65cd8178664ed36d776813230d2fb15c43ac"
+  url "https://github.com/tfsec/tfsec/archive/v0.31.0.tar.gz"
+  sha256 "4cf8194d1aabf7207f9873ef10bd7e9b271eaeed503a0d71c03719cd5126dbe1"
   license "MIT"
 
   bottle do
