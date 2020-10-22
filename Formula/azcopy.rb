deaf0fa50c61fb61579da4a68f5361c46bf1a7df
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-22 00:50:27 +0000

    azcopy: update 10.6.1 bottle.

--- Formula/azcopy.rb
+++ Formula/azcopy.rb
@@ -7,9 +7,9 @@ class Azcopy < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "fafe2581acf992a8fddddcfa5a6ba85e9294964d8e8793567851f4434561c274" => :catalina
-    sha256 "415e3931ff54ec4fb6b2dd5ff1225459b93e730d17a6ac2e943b89c8d1a1de12" => :mojave
-    sha256 "3bea18c2192e8697b2490fd24477a65a681278aae3141b89d5fa03d4d13e76fa" => :high_sierra
+    sha256 "18a2bd84e8497296777f093f9651c7ccfe981ed9c7ebc2d626b0c5da512c2466" => :catalina
+    sha256 "4732f8449fc1c58f70b8436d09190445ab6574c033f83e97b5984674481c531a" => :mojave
+    sha256 "7104cf9b26940cf776c9b28c2a79fbe146d85edf462a2eae31181954afe18e74" => :high_sierra
   end
 
   depends_on "go" => :build
