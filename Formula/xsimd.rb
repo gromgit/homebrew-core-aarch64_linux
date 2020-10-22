b2149ed512f81703fdc4b0359def7c7ba628a43e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-22 08:20:54 +0000

    xsimd: update 7.4.9 bottle.

--- Formula/xsimd.rb
+++ Formula/xsimd.rb
@@ -7,9 +7,9 @@ class Xsimd < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a8c6a9f46585989372faf4b18029b2f69a1660e743ba8ee126a51686656dfc56" => :catalina
-    sha256 "a8c6a9f46585989372faf4b18029b2f69a1660e743ba8ee126a51686656dfc56" => :mojave
-    sha256 "a8c6a9f46585989372faf4b18029b2f69a1660e743ba8ee126a51686656dfc56" => :high_sierra
+    sha256 "683112b939a02c0a8bd76f8e9a2e623d50a24bc4775b9e8dcfa1a6f750ad904b" => :catalina
+    sha256 "3144366e8952b3c158eaec803eac46ad3dcf55ca7b5ba5dea3a3418922ef5f4e" => :mojave
+    sha256 "355cbbb72b037b406eb9ca4600e446958368bd61b8cedb7abeea81f08b5c5c7b" => :high_sierra
   end
 
   depends_on "cmake" => :build
