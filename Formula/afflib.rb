70c3fce4530279cbd25194b72815b44bab92f91d
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-23 16:02:52 +0200

    afflib: migrate to python@3.9
    Closes #63357.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/afflib.rb
+++ Formula/afflib.rb
@@ -3,6 +3,7 @@ class Afflib < Formula
   homepage "https://github.com/sshock/AFFLIBv3"
   url "https://github.com/sshock/AFFLIBv3/archive/v3.7.19.tar.gz"
   sha256 "d358b07153dd08df3f35376bab0202c6103808686bab5e8486c78a18b24e2665"
+  revision 1
 
   bottle do
     cellar :any
@@ -16,13 +17,19 @@ class Afflib < Formula
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "openssl@1.1"
-  depends_on "python@3.8"
+  depends_on "python@3.9"
 
   uses_from_macos "curl"
   uses_from_macos "expat"
 
+  # Fix for Python 3.9, remove in next version
+  patch do
+    url "https://github.com/sshock/AFFLIBv3/commit/aeb444da.patch?full_index=1"
+    sha256 "90cbb0b55a6e273df986b306d20e0cfb77a263cb85e272e01f1b0d8ee8bd37a0"
+  end
+
   def install
-    ENV["PYTHON"] = Formula["python@3.8"].opt_bin/"python3"
+    ENV["PYTHON"] = Formula["python@3.9"].opt_bin/"python3"
 
     args = %w[
       --enable-s3
