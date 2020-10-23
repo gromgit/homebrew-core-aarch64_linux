cd7206eb39c104983b71e516f0bc344a3e1e0ce0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-23 16:25:17 +0000

    rain: update 0.10.2 bottle.

--- Formula/rain.rb
+++ Formula/rain.rb
@@ -7,9 +7,9 @@ class Rain < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f3972a324af4785ea1faced508ac6cf4051fce1e3590d3e384030a5502f20108" => :catalina
-    sha256 "23ceceeaefb5684331c69143a968459aec9a665a11ff7c9b2bd25370b1fa7c05" => :mojave
-    sha256 "41fc5a0444c0ec63e2b70ee2705f673c1cbb8a62da07d0b57fe60c776e7977f7" => :high_sierra
+    sha256 "56023b2b4652e0354075acfb10548bff506fcd296e9414473abca8829116b049" => :catalina
+    sha256 "98ab12c9dea3d6601231c35be6e2e24f0cca7351d6c86760307d56f9d5767434" => :mojave
+    sha256 "d7abe54bc7455e0ca5547ccd09afded852f4ce30721aebbd28136919cbe885f5" => :high_sierra
   end
 
   depends_on "go" => :build
