3575435c3699ee7dd07c6c38d7270545a993e87d
Dheepak Krishnamurthy <me@kdheepak.com>
2020-10-23 11:15:19 -0600

    taskwarrior-tui 0.8.0 (new formula)
    Closes #63373.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>

---
+++ Formula/taskwarrior-tui.rb
@@ -0,0 +1,26 @@
+class TaskwarriorTui < Formula
+  desc "Terminal user interface for taskwarrior"
+  homepage "https://github.com/kdheepak/taskwarrior-tui"
+  url "https://github.com/kdheepak/taskwarrior-tui/archive/v0.8.0.tar.gz"
+  sha256 "15a2195dde970f369b5b1e346a087c72581142525765baeffe8d54b95824bae0"
+  license "MIT"
+  head "https://github.com/kdheepak/taskwarrior-tui.git"
+
+  livecheck do
+    url "https://github.com/kdheepak/taskwarrior-tui/releases/latest"
+    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
+  end
+
+  depends_on "rust" => :build
+  depends_on "task"
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/taskwarrior-tui --version")
+    assert_match "The argument '--config <FILE>' requires a value but none was supplied",
+      shell_output("#{bin}/taskwarrior-tui --config 2>&1", 1)
+  end
+end
