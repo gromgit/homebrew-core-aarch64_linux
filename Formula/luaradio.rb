597dd7c47af7a04ae24567a3e80dc35a1758022b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-23 01:42:25 +0000

    luaradio: update 0.8.0 bottle.

--- Formula/luaradio.rb
+++ Formula/luaradio.rb
@@ -8,9 +8,9 @@ class Luaradio < Formula
 
   bottle do
     cellar :any
-    sha256 "df3f0b9ba19651e37c5b7c8e6bbe04658f852bd909fcebc14d9c08c9926e1061" => :catalina
-    sha256 "909850451f26146b3c9e65129177afd31a715e463223c2713b414d345929376d" => :mojave
-    sha256 "6d16f13182248aac79fcda6cbc11284ddbfa0e660cb9ba38a4b5e76262113e26" => :high_sierra
+    sha256 "1370415899d4c7c41b0bfd5ac4792ea3447736acf8727cbbe492b4214b2cc3c1" => :catalina
+    sha256 "0968a0cd591fd3e3f7af909df152d5065b8b711a4a7a024c2ac13aceea9aac01" => :mojave
+    sha256 "190f3227e451c188e7588e4bdd9f9b1883c97516da52386b2f7019a20858270e" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
