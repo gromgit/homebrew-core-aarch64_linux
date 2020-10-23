84b442b95b6b346d0ade387525d0b4af0e946b0e
Russi, Matthew <matthew.russi@etrade.com>
2020-10-23 16:14:11 -0400

    cntlm: Add license and test
    Closes #63378.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cntlm.rb
+++ Formula/cntlm.rb
@@ -3,6 +3,7 @@ class Cntlm < Formula
   homepage "https://cntlm.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/cntlm/cntlm/cntlm%200.92.3/cntlm-0.92.3.tar.bz2"
   sha256 "7b603d6200ab0b26034e9e200fab949cc0a8e5fdd4df2c80b8fc5b1c37e7b930"
+  license "GPL-2.0-only"
 
   livecheck do
     url :stable
@@ -57,5 +58,26 @@ class Cntlm < Formula
         </dict>
       </plist>
     EOS
+  end
+
+  test do
+    assert_match "version #{version}", shell_output("#{bin}/cntlm -h 2>&1", 1)
+
+    bind_port = free_port
+    (testpath/"cntlm.conf").write <<~EOS
+      # Cntlm Authentication Proxy Configuration
+      Username	testuser
+      Domain		corp-uk
+      Password	password
+      Proxy		localhost:#{free_port}
+      NoProxy		localhost, 127.0.0.*, 10.*, 192.168.*
+      Listen		#{bind_port}
+    EOS
+
+    fork do
+      exec "#{bin}/cntlm -c #{testpath}/cntlm.conf -v"
+    end
+    sleep 2
+    assert_match /502 Parent proxy unreacheable/, shell_output("curl -s localhost:#{bind_port}")
   end
 end
