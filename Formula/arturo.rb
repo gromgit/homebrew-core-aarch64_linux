6cba92a24ccf63e64dff57529c88c6e9a81fcfc0
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-10-23 06:50:30 +0000

    arturo: add 0.9.4.6 bottle.

--- Formula/arturo.rb
+++ Formula/arturo.rb
@@ -5,6 +5,13 @@ class Arturo < Formula
   sha256 "90dfd8870ab0bfadd14cd611a33353a1d4b66b25e0c56d75cab895989be25c98"
   license "MIT"
 
+  bottle do
+    cellar :any
+    sha256 "b4a5f610f82c3a10ddb4a03936690c9bec560d16218e5b952e8690d7853adbfd" => :catalina
+    sha256 "1cbeced6fff8823b2405bb7d75360236f5686e8edcddb1113e5f98146b2138dc" => :mojave
+    sha256 "56f91a92ff90cf0282128601c23d486fac24ce218d5ce0a034d20b652c4f2932" => :high_sierra
+  end
+
   depends_on "nim" => :build
   depends_on "gmp"
 
