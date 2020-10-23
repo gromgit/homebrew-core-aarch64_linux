4b175e5074ba0bf4a68d04c5a77a8d9ac70388f7
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-10-23 11:36:01 +0200

    s3cmd: migrate to python@3.9
    Closes #63174.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/s3cmd.rb
+++ Formula/s3cmd.rb
@@ -6,6 +6,7 @@ class S3cmd < Formula
   url "https://files.pythonhosted.org/packages/c7/eb/5143fe1884af2303cb7b23f453e5c9f337af46c2281581fc40ab5322dee4/s3cmd-2.1.0.tar.gz"
   sha256 "966b0a494a916fc3b4324de38f089c86c70ee90e8e1cae6d59102103a4c0cc03"
   license "GPL-2.0"
+  revision 1
   head "https://github.com/s3tools/s3cmd.git"
 
   livecheck do
@@ -19,7 +20,7 @@ class S3cmd < Formula
     sha256 "4d2c498462eba573c43413ec16f4681a8e604b67ff6739351900e500e2be5923" => :high_sierra
   end
 
-  depends_on "python@3.8"
+  depends_on "python@3.9"
 
   resource "python-dateutil" do
     url "https://files.pythonhosted.org/packages/be/ed/5bbc91f03fa4c839c4c7360375da77f9659af5f7086b7a7bdda65771c8e0/python-dateutil-2.8.1.tar.gz"
