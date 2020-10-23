2f9a18d223a39d15da839d00173e04519864cbdc
chenrui <chenrui333@gmail.com>
2020-10-23 12:37:32 +0000

    phpstan 0.12.51
    Closes #63353.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/phpstan.rb
+++ Formula/phpstan.rb
@@ -1,8 +1,8 @@
 class Phpstan < Formula
   desc "PHP Static Analysis Tool"
   homepage "https://github.com/phpstan/phpstan"
-  url "https://github.com/phpstan/phpstan/releases/download/0.12.50/phpstan.phar"
-  sha256 "83367805ac14512f3734276da6ff3417a9d8c6d8f0309b5b8856f3ed14a42a4f"
+  url "https://github.com/phpstan/phpstan/releases/download/0.12.51/phpstan.phar"
+  sha256 "e49eb38f1605baaa95b39040b89136a608f58b103cf482e96f90980c1809de5d"
   license "MIT"
 
   bottle :unneeded
