31bf5ad51a57471ccafc03935a6905d2ff2d6ff5
chenrui <chenrui333@gmail.com>
2020-10-24 12:15:13 +0000

    pmd 6.29.0
    Closes #63409.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pmd.rb
+++ Formula/pmd.rb
@@ -1,8 +1,8 @@
 class Pmd < Formula
   desc "Source code analyzer for Java, JavaScript, and more"
   homepage "https://pmd.github.io"
-  url "https://github.com/pmd/pmd/releases/download/pmd_releases/6.28.0/pmd-bin-6.28.0.zip"
-  sha256 "9a19365f2e107ae801b39be04c5c03cdca2d352c450faac639a6dd95b5c3ab0c"
+  url "https://github.com/pmd/pmd/releases/download/pmd_releases/6.29.0/pmd-bin-6.29.0.zip"
+  sha256 "78ef4cff4b2e8160b8c7fbbdbdecdf2b782e2f2fa8d2aa613c734299dc5d3122"
   license "BSD-4-Clause"
 
   bottle :unneeded
