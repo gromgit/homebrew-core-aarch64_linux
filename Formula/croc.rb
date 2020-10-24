47acb8b0099ced2db2e52e5df7f0d4ffc7a1d994
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-24 05:17:58 +0000

    croc: update 8.6.5 bottle.

--- Formula/croc.rb
+++ Formula/croc.rb
@@ -8,9 +8,9 @@ class Croc < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "82c31455d6d6010ba7a6012f01adc014075307089f6cd589fbef63b7fe0e435f" => :catalina
-    sha256 "8b406f8c1e6be1779a99fbabbb1f539a32703eb7b806e7244ae3ef477c206435" => :mojave
-    sha256 "75a7061af51e4db4851e069ac2003cd49103eb28399caa942e1fe085f61a6813" => :high_sierra
+    sha256 "98249aa16eaf16722abd3a0cc6c98c4499d26b644909ee8879e01a3fac878ffa" => :catalina
+    sha256 "71fec2667e693fbdc000d8ac4bc99f1b1898101d963c84247f23add13c1af13e" => :mojave
+    sha256 "c294ca113397098cf77867dfbd042fc69ebc7fa52c418b08cb04049a730b00fb" => :high_sierra
   end
 
   depends_on "go" => :build
