708834a7c78ca33a3f291c207fc0ffdce4e3e79b
Eric Veenendaal <eveenendaal@thorne.com>
2020-10-24 08:53:59 -0500

    node@14 14.15.0 (new formula)
    * Creating formula for Node14
    * Switching to metadata from last V14 commit
    * Adding keg_only
    * Removing HEAD
    * Typo
    * Removing "bottle do"
    * Update Formula/node@14.rb
    * Update node@14.rb
      Making the post_install look like the rest of the versioned node formula
    * Update node@14.rb
      Matching the other versioned node installs even more.
    * Fixing whitespace
    * Adding python to install
    * Apply suggestions from code review
    * Updating to v14.15.0
    
    Closes #63410.
    
    Co-authored-by: Jason Karns <jason.karns@gmail.com>
    Co-authored-by: Diogo Azevedo <diogoazevedos@gmail.com>
    Co-authored-by: Eric Veenendaal <eric@veenendaal.co>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/node@14.rb
@@ -0,0 +1,56 @@
+class NodeAT14 < Formula
+  desc "Platform built on V8 to build network applications"
+  homepage "https://nodejs.org/"
+  url "https://nodejs.org/dist/v14.15.0/node-v14.15.0.tar.gz"
+  sha256 "5f9529e0a42985d5a81620635ab2541e3553daede42903d1b3176f808d46411c"
+  license "MIT"
+
+  livecheck do
+    url "https://nodejs.org/dist/"
+    regex(%r{href=["']?v?(14(?:\.\d+)+)/?["' >]}i)
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "pkg-config" => :build
+  depends_on "python@3.9" => :build
+  depends_on "icu4c"
+
+  def install
+    # make sure subprocesses spawned by make are using our Python 3
+    ENV["PYTHON"] = Formula["python@3.9"].opt_bin/"python3"
+
+    system "python3", "configure.py", "--prefix=#{prefix}", "--with-intl=system-icu"
+    system "make", "install"
+  end
+
+  def post_install
+    (lib/"node_modules/npm/npmrc").atomic_write("prefix = #{HOMEBREW_PREFIX}\n")
+  end
+
+  test do
+    path = testpath/"test.js"
+    path.write "console.log('hello');"
+
+    output = shell_output("#{bin}/node #{path}").strip
+    assert_equal "hello", output
+    output = shell_output("#{bin}/node -e 'console.log(new Intl.NumberFormat(\"en-EN\").format(1234.56))'").strip
+    assert_equal "1,234.56", output
+
+    output = shell_output("#{bin}/node -e 'console.log(new Intl.NumberFormat(\"de-DE\").format(1234.56))'").strip
+    assert_equal "1.234,56", output
+
+    # make sure npm can find node
+    ENV.prepend_path "PATH", opt_bin
+    ENV.delete "NVM_NODEJS_ORG_MIRROR"
+    assert_equal which("node"), opt_bin/"node"
+    assert_predicate bin/"npm", :exist?, "npm must exist"
+    assert_predicate bin/"npm", :executable?, "npm must be executable"
+    npm_args = ["-ddd", "--cache=#{HOMEBREW_CACHE}/npm_cache", "--build-from-source"]
+    system "#{bin}/npm", *npm_args, "install", "npm@latest"
+    system "#{bin}/npm", *npm_args, "install", "bufferutil"
+    assert_predicate bin/"npx", :exist?, "npx must exist"
+    assert_predicate bin/"npx", :executable?, "npx must be executable"
+    assert_match "< hello >", shell_output("#{bin}/npx cowsay hello")
+  end
+end
