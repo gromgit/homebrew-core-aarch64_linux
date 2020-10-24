2cec45bbc169d63847e2745c69d3b2751da7ab15
Rui Chen <rui@meetup.com>
2020-10-24 19:20:47 -0400

    youtube-dl: add 2020.9.20 bottle.

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -7,6 +7,13 @@ class YoutubeDl < Formula
   sha256 "67fb9bfa30f5b8f06227c478a8a6ed04af1f97ad4e81dd7e2ce518df3e275391"
   license "Unlicense"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6553843d76f95feeb9a488b2a14cb8374b313f02bd72b18d75cbbd2270fe11ee" => :catalina
+    sha256 "8a6f47c7bf197a9cb40e08d130ce6ac9530d2c77f4ecd92f838e19abe23f2c5a" => :mojave
+    sha256 "7b797964bed458c4271ad917373155a9be574afc174bfdda14b73a77fb16cbc5" => :high_sierra
+  end
+
   depends_on "python@3.9"
 
   def install
