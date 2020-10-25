1b8210b96a8cc662ce804f09e3696220ac000358
Gal Schlezinger <gal@spitfire.co.il>
2020-10-25 10:21:17 +0200

    fnm 1.22.2 (new formula)
    * fnm v1.22.1 (new formula)
    * fnm 1.22.2 (new formula)
    * Use `Utils.safe_popen_read` instead of backticks
    * Fix audit
    * Fix wrong spacing
    * Remove caveats
    * Remove unused method
    
    Closes #63459.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/fnm.rb
@@ -0,0 +1,30 @@
+class Fnm < Formula
+  desc "Fast and simple Node.js version manager"
+  homepage "https://fnm.vercel.app"
+  url "https://github.com/Schniz/fnm/archive/v1.22.2.tar.gz"
+  sha256 "49013376359a1e0d690359b10c09604df90ab30602e8400acf0a97d241707344"
+  license "GPL-3.0-only"
+  head "https://github.com/Schniz/fnm.git"
+
+  livecheck do
+    url "https://github.com/Schniz/fnm/releases/latest"
+    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
+  end
+
+  depends_on "rust" => :build
+
+  uses_from_macos "zlib"
+
+  def install
+    system "cargo", "install", *std_cargo_args
+
+    (bash_completion/"fnm").write Utils.safe_popen_read("#{bin}/fnm", "completions", "--shell=bash")
+    (fish_completion/"fnm.fish").write Utils.safe_popen_read("#{bin}/fnm", "completions", "--shell=fish")
+    (zsh_completion/"_fnm").write Utils.safe_popen_read("#{bin}/fnm", "completions", "--shell=zsh")
+  end
+
+  test do
+    system("#{bin}/fnm", "install", "12.0.0")
+    assert_match "v12.0.0", shell_output("#{bin}/fnm exec --using=12.0.0 -- node --version")
+  end
+end
