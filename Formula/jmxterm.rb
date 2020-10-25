25cda35f4a56df2ddf17263a293ef16ee0e7ed66
mattrussi <mattrussi@users.noreply.github.com>
2020-10-25 01:01:58 +0000

    jmxterm: migrate to openjdk@8 (#63394)
    Co-authored-by: Russi, Matthew <matthew.russi@etrade.com>

--- Formula/jmxterm.rb
+++ Formula/jmxterm.rb
@@ -4,10 +4,11 @@ class Jmxterm < Formula
   url "https://github.com/jiaqi/jmxterm/releases/download/v1.0.2/jmxterm-1.0.2-uber.jar"
   sha256 "3a8a7cf99d89a3f46fcf3bcfe9bb4838d7778a10730e0983a258edc765fede5c"
   license "Apache-2.0"
+  revision 1
 
   bottle :unneeded
 
-  depends_on java: "1.8"
+  depends_on "openjdk@8"
 
   def install
     libexec.install "jmxterm-#{version}-uber.jar"
