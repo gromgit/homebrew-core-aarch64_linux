c3f9b20738958c3f88ef165b5020c4743302311a
chenrui <chenrui333@gmail.com>
2020-10-25 22:13:44 -0400

    sjk 0.17 (#63475)

--- Formula/sjk.rb
+++ Formula/sjk.rb
@@ -1,10 +1,9 @@
 class Sjk < Formula
   desc "Swiss Java Knife"
   homepage "https://github.com/aragozin/jvm-tools"
-  url "https://search.maven.org/remotecontent?filepath=org/gridkit/jvmtool/sjk-plus/0.16/sjk-plus-0.16.jar"
-  sha256 "306c40fbf76b9cb9a09b394116b8a2614b0d9acb3725e845d2a754686a6d3d49"
+  url "https://search.maven.org/remotecontent?filepath=org/gridkit/jvmtool/sjk-plus/0.17/sjk-plus-0.17.jar"
+  sha256 "4f3d1b8b5c1c85b7fdcec1ebc880bac6d81da5d924fd445ce7e221e6728705d7"
   license "Apache-2.0"
-  revision 1
 
   bottle :unneeded
 
