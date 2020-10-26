285c3248793e4c219c5d1bee414e63493f10975b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-26 17:19:11 +0000

    pypy3: update 7.3.2 bottle.

--- Formula/pypy3.rb
+++ Formula/pypy3.rb
@@ -12,10 +12,9 @@ class Pypy3 < Formula
   end
 
   bottle do
-    cellar :any
-    sha256 "384b960848c433008154103f8cdf57c77b8ee805115818cae4cb502e5485f043" => :catalina
-    sha256 "de50ccf32775b38c4d1e2324fe7b9c0e23856fbe34eaf6ebd7b23e7d6d8f6459" => :mojave
-    sha256 "ea4031fc8f85001fea131cc880108416f0d9bbac400655a7d8337ad8cb8f1547" => :high_sierra
+    sha256 "0abe67d4da37ae77c85e8883bcb01ed6b5b8061ad54d4ef9f659b50d9e7a8246" => :catalina
+    sha256 "7b80681acba5b237e6a164e3f91dd1d968ebf5b35b60c503d19e4ad3d903147e" => :mojave
+    sha256 "022d70ee06728680081e825e1d6737fab2d74d0b51fcc758361a01258423df34" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
