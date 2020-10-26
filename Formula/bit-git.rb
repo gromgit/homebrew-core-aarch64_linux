6e04344287d58b7d892ea2de436f849353bfcdea
Alexander Zigelski <ali@crunchtime.dev>
2020-10-26 20:12:15 +0100

    bit-git 0.8.1 (new formula)
    Add bit-git formula
    
    Closes #63508.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/bit-git.rb
@@ -0,0 +1,30 @@
+class BitGit < Formula
+  desc "Bit is a modern Git CLI"
+  homepage "https://github.com/chriswalz/bit"
+  url "https://github.com/chriswalz/bit/archive/v0.8.1.tar.gz"
+  sha256 "80f19e249356f6adc46071bbf2a01f139c0af9997bc3e323eb62952863d7cf8b"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  conflicts_with "bit", because: "both install `bit` binaries"
+
+  def install
+    system "go", "build", *std_go_args
+    bin.install_symlink "bit-git" => "bit"
+  end
+
+  test do
+    system "git", "init", testpath/"test-repository"
+
+    cd testpath/"test-repository" do
+      (testpath/"test-repository/test.txt").write <<~EOS
+        Hello Homebrew!
+      EOS
+      system bin/"bit", "add", "test.txt"
+
+      output = shell_output("#{bin}/bit status").chomp
+      assert_equal "new file:   test.txt", output.lines.last.strip
+    end
+  end
+end
