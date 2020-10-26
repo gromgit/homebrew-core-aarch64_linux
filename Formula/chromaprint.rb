bbbf97b0413dd995f31718be50b9b65c1d5cb785
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-26 13:20:00 +0000

    chromaprint: update 1.5.0_4 bottle.

--- Formula/chromaprint.rb
+++ Formula/chromaprint.rb
@@ -8,9 +8,9 @@ class Chromaprint < Formula
 
   bottle do
     cellar :any
-    sha256 "8693666d997e651672c56e619fb9a024d7f839748625cf481028600010f37ab3" => :catalina
-    sha256 "eebfcab3bf0e6534a89b8a69c2cfa3d89b4c4f6114351e8cc3a368c4220d31b5" => :mojave
-    sha256 "f47fb0b6d7cb0f7b6f974a8ec2f85c9c1afe1cbb553a87dbf0c47742f5db7e72" => :high_sierra
+    sha256 "7cfc1a6b0cf27c036ea1d7c7f96c4794424b69253814ae65b168828356506695" => :catalina
+    sha256 "73f8a42a88353c9edb9b9b2d009ce1a1a476bee8c8e282fe1308542b2edc7477" => :mojave
+    sha256 "e462f49fa6588091c870536678af79bce347f76ddf32de7540d636bb5e5f4efb" => :high_sierra
   end
 
   depends_on "cmake" => :build
