6ad08dca03ccdba580630a3a5ce678e679e685a3
FX Coudert <fxcoudert@gmail.com>
2020-10-26 12:55:39 +0000

    freeling: update 4.2_1 bottle.

--- Formula/freeling.rb
+++ Formula/freeling.rb
@@ -7,10 +7,9 @@ class Freeling < Formula
   revision 1
 
   bottle do
-    rebuild 1
-    sha256 "680ce0ad080cd5e9b0415650464c198b9206ed1833791895322e5f0df4b781d5" => :catalina
-    sha256 "120ce824973a4dec44ca66988d80b067dd47bee0ec4ee8bd052d8a5ed45840aa" => :mojave
-    sha256 "11db1280b733420f74d65c273bb06884f93da15f91b954c5bb38bcca2ac45869" => :high_sierra
+    sha256 "d1423f6b6ea4ee29d218bd99e3a97f398dbd800f245ca1a14c84aede92346e31" => :catalina
+    sha256 "9d5712675f4656cb7e210aa48ed0628aae6db36acf47ec4d0581ef07e670a494" => :mojave
+    sha256 "cbe44e3b559e1df5cbcfcd7b94d243d913e714d3b215a0187dbd740c93b6c357" => :high_sierra
   end
 
   depends_on "cmake" => :build
