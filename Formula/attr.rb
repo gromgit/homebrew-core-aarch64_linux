f74db5bff70195e366368bc2d4b350c122f25dc0
Michka Popoff <michkapopoff@gmail.com>
2020-10-27 23:09:56 +0100

    attr (new formula)

---
+++ Formula/attr.rb
@@ -0,0 +1,27 @@
+class Attr < Formula
+  desc "Manipulate filesystem extended attributes"
+  homepage "https://savannah.nongnu.org/projects/attr"
+  url "https://mirror.csclub.uwaterloo.ca/nongnu/attr/attr-2.4.48.tar.gz"
+  sha256 "5ead72b358ec709ed00bbf7a9eaef1654baad937c001c044fe8b74c57f5324e7"
+  license "GPL-2.0-or-later"
+
+  depends_on "gettext" => :build
+  depends_on :linux
+
+  def install
+    system "./configure",
+           "--disable-debug",
+           "--disable-dependency-tracking",
+           "--disable-silent-rules",
+           "--prefix=#{prefix}",
+           "--with-libintl-prefix=#{Formula["gettext"].opt_prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.txt").write("Hello World!\n")
+    system bin/"attr", "-s", "name", "test.txt"
+    assert_match 'Attribute "name" has a 0 byte value for test.txt',
+                 shell_output(bin/"attr -l test.txt")
+  end
+end
