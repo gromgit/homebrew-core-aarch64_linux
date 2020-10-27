fa67d082ed064a1de6e05d0b75cca878909adc1f
Ivan Malopinsky <imsky@users.noreply.github.com>
2020-10-27 19:44:54 -0400

    git-fresh 1.13.0
    Closes #63563.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/git-fresh.rb
+++ Formula/git-fresh.rb
@@ -1,8 +1,8 @@
 class GitFresh < Formula
   desc "Utility to keep git repos fresh"
   homepage "https://github.com/imsky/git-fresh"
-  url "https://github.com/imsky/git-fresh/archive/v1.12.1.tar.gz"
-  sha256 "48be2629113ba77a2a6d8fd478b7186f7237669e9495e768b9d3133704c49dde"
+  url "https://github.com/imsky/git-fresh/archive/v1.13.0.tar.gz"
+  sha256 "7043aaf2bf66dade7d06ebcf96e5d368c4910c002b7b00962bd2bd24490ce2dc"
   license "MIT"
 
   bottle :unneeded
