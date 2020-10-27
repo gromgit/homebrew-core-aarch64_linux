a2d2d480aa6697a63e107d82f04ab2a627a122d5
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-10-27 09:30:10 +0000

    bit-git: add 0.8.1 bottle.

--- Formula/bit-git.rb
+++ Formula/bit-git.rb
@@ -5,6 +5,13 @@ class BitGit < Formula
   sha256 "80f19e249356f6adc46071bbf2a01f139c0af9997bc3e323eb62952863d7cf8b"
   license "Apache-2.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "85b8409f9012bfda02a17d27605063b4aa8eabe2a38522ae56325b8f4c423f15" => :catalina
+    sha256 "d4c25d968475239933582206f9733164286f60b1c1d6a6229da15c954080077b" => :mojave
+    sha256 "494b39c9402126019107c13b8e31a61b7354be7d42b58bb91e45574bb7c9d213" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   conflicts_with "bit", because: "both install `bit` binaries"
