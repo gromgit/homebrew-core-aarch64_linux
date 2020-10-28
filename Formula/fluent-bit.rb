0d52381197be7fa8877a95918952d531f8542c6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-28 08:22:10 +0000

    fluent-bit: update 1.6.2 bottle.

--- Formula/fluent-bit.rb
+++ Formula/fluent-bit.rb
@@ -13,9 +13,9 @@ class FluentBit < Formula
 
   bottle do
     cellar :any
-    sha256 "40ccb5eef5c8435d7dfb9fca2e0a7290b6452022ab16a93d4561872011993e6e" => :catalina
-    sha256 "a0ec424da02c4a768d40528de416b63f03453ed4981729846f955db095cafe11" => :mojave
-    sha256 "e0c8be899457517500a3424abb73f9ec8e9d2ad6b25efd7d6fb691055bea1005" => :high_sierra
+    sha256 "a0db82c901511a03b48ae2ceb5711f626965bb3a296550658e722e257d296f60" => :catalina
+    sha256 "d586847eca6eebc9241f5310b0df3a4442086af3afbde5071621673525b36480" => :mojave
+    sha256 "52f91f4fdbe28417726518ba4470f68552dc92a2a4b55dd665d840f12b53e9f7" => :high_sierra
   end
 
   depends_on "bison" => :build
