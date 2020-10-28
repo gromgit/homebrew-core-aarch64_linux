aaf469ccf0396c850b29101e9169d5873a2af038
Viktor Szakats <commit@vsz.me>
2020-10-28 00:50:01 +0000

    unoconv: fix/update urls

--- Formula/unoconv.rb
+++ Formula/unoconv.rb
@@ -1,11 +1,11 @@
 class Unoconv < Formula
   desc "Convert between any document format supported by OpenOffice"
-  homepage "http://dag.wiee.rs/home-made/unoconv/"
+  homepage "https://github.com/unoconv/unoconv"
   url "https://files.pythonhosted.org/packages/ab/40/b4cab1140087f3f07b2f6d7cb9ca1c14b9bdbb525d2d83a3b29c924fe9ae/unoconv-0.9.0.tar.gz"
   sha256 "308ebfd98e67d898834876348b27caf41470cd853fbe2681cc7dacd8fd5e6031"
   license "GPL-2.0"
   revision 2
-  head "https://github.com/dagwieers/unoconv.git"
+  head "https://github.com/unoconv/unoconv.git"
 
   livecheck do
     url :stable
