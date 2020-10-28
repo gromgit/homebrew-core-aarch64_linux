c00be9ea58c175f381d00f96b9353604cc5490c2
Viktor Szakats <commit@vsz.me>
2020-10-28 00:49:59 +0000

    fceux: secure homepage

--- Formula/fceux.rb
+++ Formula/fceux.rb
@@ -1,6 +1,6 @@
 class Fceux < Formula
   desc "All-in-one NES/Famicom Emulator"
-  homepage "http://fceux.com"
+  homepage "https://fceux.com/"
   url "https://downloads.sourceforge.net/project/fceultra/Source%20Code/2.2.3%20src/fceux-2.2.3.src.tar.gz"
   sha256 "4be6dda9a347f941809a3c4a90d21815b502384adfdd596adaa7b2daf088823e"
   revision 4
