00d32bc4ed7b96f50db304b63faa2eb1b35664cd
circleci <ops+deploy-bot@circleci.com>
2020-10-28 11:53:01 +0000

    circleci 0.1.11393
    Closes #63598.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/circleci.rb
+++ Formula/circleci.rb
@@ -3,8 +3,8 @@ class Circleci < Formula
   homepage "https://circleci.com/docs/2.0/local-cli/"
   # Updates should be pushed no more frequently than once per week.
   url "https://github.com/CircleCI-Public/circleci-cli.git",
-      tag:      "v0.1.11146",
-      revision: "275febaff705bc4e095a594c221bd00ef553398d"
+      tag:      "v0.1.11393",
+      revision: "c9b8b8763f0a9b2cc21da0531ab6d95057646945"
   license "MIT"
 
   bottle do
