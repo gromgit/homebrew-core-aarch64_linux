edfb85e39a55b30af5c31061bf1ead073d0ae5d6
Alexander Bayandin <a.bayandin@gmail.com>
2020-10-28 10:11:48 +0000

    sofia-sip 1.13.2
    Closes #63593.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sofia-sip.rb
+++ Formula/sofia-sip.rb
@@ -1,9 +1,9 @@
 class SofiaSip < Formula
   desc "SIP User-Agent library"
   homepage "https://sofia-sip.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/sofia-sip/sofia-sip/1.12.11/sofia-sip-1.12.11.tar.gz"
-  sha256 "2b01bc2e1826e00d1f7f57d29a2854b15fd5fe24695e47a14a735d195dd37c81"
-  revision 4
+  url "https://github.com/freeswitch/sofia-sip/archive/v1.13.2.tar.gz"
+  sha256 "b9eca9688ce4b28e062daf0933c3bf661fb607e7afafa71bda3e8f07eb88df44"
+  license "LGPL-2.1-or-later"
 
   livecheck do
     url :stable
@@ -17,13 +17,17 @@ class SofiaSip < Formula
     sha256 "95a892ab2ae71eb09d5aa22c6e30a2336376d34321c54032b6d03106a96dc631" => :sierra
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "gettext"
   depends_on "glib"
   depends_on "openssl@1.1"
 
   def install
+    system "./bootstrap.sh"
     system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make", "install"
   end
