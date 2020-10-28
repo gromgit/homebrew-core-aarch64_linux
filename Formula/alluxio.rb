c62111a68fe391376e8612faec70d96d38ec0ffa
Bin Fan <fanbin103@gmail.com>
2020-10-27 23:19:07 -0700

    alluxio 2.4.0
    Closes #63589.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/alluxio.rb
+++ Formula/alluxio.rb
@@ -1,8 +1,8 @@
 class Alluxio < Formula
   desc "Open Source Memory Speed Virtual Distributed Storage"
   homepage "https://www.alluxio.io/"
-  url "https://downloads.alluxio.io/downloads/files/1.8.2/alluxio-1.8.2-bin.tar.gz"
-  sha256 "e927f80aabf80ac0b47d4491a4320058bcd15f554fccec1375e8f6dcf243ebb4"
+  url "https://downloads.alluxio.io/downloads/files/2.4.0/alluxio-2.4.0-bin.tar.gz"
+  sha256 "6f664325d79bf3dd5682749b476eef1f99b0557c473f245903300eea2e5459d9"
 
   livecheck do
     url "https://downloads.alluxio.io/downloads/files/"
