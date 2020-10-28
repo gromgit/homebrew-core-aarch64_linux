f03740b78f285abfc9c829e3fa19dd7847b9f324
Caleb Xu <calebcenter@live.com>
2020-10-28 13:25:08 -0400

    fcct 0.7.0
    Closes #63609.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fcct.rb
+++ Formula/fcct.rb
@@ -1,8 +1,8 @@
 class Fcct < Formula
   desc "Fedora CoreOS Config Transpiler"
   homepage "https://github.com/coreos/fcct"
-  url "https://github.com/coreos/fcct/archive/v0.6.0.tar.gz"
-  sha256 "2cd630f37fcd549e931df9a01f94f0f783e06463701b9f8b5ab9b293515f9915"
+  url "https://github.com/coreos/fcct/archive/v0.7.0.tar.gz"
+  sha256 "da06603f7e42e9de9cd01b8404f2ad2315a4a3062e905a9d1871e33e07c2ecc3"
   license "Apache-2.0"
   head "https://github.com/coreos/fcct.git"
 
