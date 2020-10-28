3a0fbf84f171e4272a3e9516c203bac480db5e52
Caleb Xu <calebcenter@live.com>
2020-10-28 13:33:46 -0400

    hr 1.3

--- Formula/hr.rb
+++ Formula/hr.rb
@@ -1,8 +1,8 @@
 class Hr < Formula
   desc "<hr />, for your terminal window"
   homepage "https://github.com/LuRsT/hr"
-  url "https://github.com/LuRsT/hr/archive/1.2.tar.gz"
-  sha256 "8f611b3f25e10fac1e67cf8b30fea4c1c02db7ab8c55d39402fe08caecb68a1a"
+  url "https://github.com/LuRsT/hr/archive/1.3.tar.gz"
+  sha256 "258ff3121369d17c5c70fa18e466ac01cdb4cf890c605f7a5e706d5b1a3afebf"
   license "MIT"
   head "https://github.com/LuRsT/hr.git"
 
