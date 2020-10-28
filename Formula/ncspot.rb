a8d542fdb51ee3a0ca0146c394ef71c73f8f42a5
Jonathan Chang <me@jonathanchang.org>
2020-10-28 03:05:24 +0000

    ncspot: add 0.2.4 bottle.

--- Formula/ncspot.rb
+++ Formula/ncspot.rb
@@ -5,6 +5,13 @@ class Ncspot < Formula
   sha256 "faf789ddc83db718874fbd71838996574b52008bf3192171c435f533bc769916"
   license "BSD-2-Clause"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "168060ab5814e9c3f8fb5234112fee4466d7bcfc58c49254d60765c790128bbf" => :catalina
+    sha256 "ab6ccbc785b6a573400378e8f7480296c90fc177aa5f86d45945fded290a00fa" => :mojave
+    sha256 "334e1437787e641ce4f80dbc06fbb4368bb87a34e72aa7d4740acf4d8c016a62" => :high_sierra
+  end
+
   depends_on "python3" => :build
   depends_on "rust" => :build
 
