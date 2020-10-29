453f1c3af6bef074bb1c18da8113205a97a6607a
Rui Chen <rui@meetup.com>
2020-10-29 00:53:11 -0400

    libslax: add livecheck
    Signed-off-by: Rui Chen <rui@meetup.com>

--- Formula/libslax.rb
+++ Formula/libslax.rb
@@ -5,6 +5,11 @@ class Libslax < Formula
   sha256 "4da6fb9886e50d75478d5ecc6868c90dae9d30ba7fc6e6d154fc92e6a48d9a95"
   license "BSD-3-Clause"
 
+  livecheck do
+    url "https://github.com/Juniper/libslax/releases"
+    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
+  end
+
   bottle do
     sha256 "8b4506f10c72d75425ad849f17918a6574c349ebdf29ab740ad323811d1a4d02" => :catalina
     sha256 "5e024a22f8a47c0a11724d7543cd50141e8246b3669155cd734854ee74ec9d71" => :mojave
