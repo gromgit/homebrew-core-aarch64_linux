5d09089f2e51208a1199cf2e81b49f4137ccc7fc
Rui Chen <rui@meetup.com>
2020-10-29 00:23:16 -0400

    cubejs-cli 0.23.2
    Closes #63644.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cubejs-cli.rb
+++ Formula/cubejs-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class CubejsCli < Formula
   desc "Cube.js command-line interface"
   homepage "https://cube.dev/"
-  url "https://registry.npmjs.org/cubejs-cli/-/cubejs-cli-0.22.2.tgz"
-  sha256 "88e8a404aafa50dbfed288273b09d6e28afc7486d25820e427423e3cfee3d994"
+  url "https://registry.npmjs.org/cubejs-cli/-/cubejs-cli-0.23.2.tgz"
+  sha256 "dce5b8c0e275e3c176a57085c7c24b8ab355ad4962dfba6f4ef709b2e9d8d2b5"
   license "Apache-2.0"
 
   livecheck do
