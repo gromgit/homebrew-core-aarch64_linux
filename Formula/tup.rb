37eb885b31c3b4c199a73c4ae4bfae5e903b56c6
Mike Shal <marfey@gmail.com>
2020-10-29 16:02:29 -0700

    tup 0.7.10
    * tup 0.7.10
    * tup: update license
    
    Closes #63706.
    
    Co-authored-by: chenrui <rui@meetup.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tup.rb
+++ Formula/tup.rb
@@ -1,9 +1,9 @@
 class Tup < Formula
   desc "File-based build system"
   homepage "http://gittup.org/tup/"
-  url "https://github.com/gittup/tup/archive/v0.7.9.tar.gz"
-  sha256 "9b0951afaa749186eb55d88860405b2f6b3d88632d1b5df1ec4a0bf973d8d326"
-  license "GPL-2.0"
+  url "https://github.com/gittup/tup/archive/v0.7.10.tar.gz"
+  sha256 "c80946bc772ae4a5170855e907c866dae5040620e81ee1a590223bdbdf65f0f8"
+  license "GPL-2.0-only"
   head "https://github.com/gittup/tup.git"
 
   bottle do
