a6f7243019364a621659635ab5aae63813d50b99
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-29 00:52:58 +0000

    geoipupdate: update 4.5.0 bottle.

--- Formula/geoipupdate.rb
+++ Formula/geoipupdate.rb
@@ -7,9 +7,9 @@ class Geoipupdate < Formula
   head "https://github.com/maxmind/geoipupdate.git"
 
   bottle do
-    sha256 "50a8fe494f4e3f6d961323200ef496f0efa5bffa4624a18a518b46ffe3a7f3e7" => :catalina
-    sha256 "dec6091fbc16a87bfdd752b95ba4ddc3d864e6c1c4255d1f7ead79d8f2656443" => :mojave
-    sha256 "c1f12ee079226b233cc137d7024c1c84ceea92b13e1b2a154a7de2b859fec42b" => :high_sierra
+    sha256 "97e31b482981f2027c4223e8392b6e4d1bb6f5845abc4a92fd2f31fe60092bad" => :catalina
+    sha256 "94cf0c34c2e48c8976efd32281b8b899ffb984f9828ab7e5fdfd92dcd8122d1b" => :mojave
+    sha256 "371eed40eee1c2e15a20c9b9c942cd55f5a67b9875bfb57fc6ff905af8e19300" => :high_sierra
   end
 
   depends_on "go" => :build
