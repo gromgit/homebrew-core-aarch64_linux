2bed189a4103877ed5b9704462ee0171f4848c81
Caleb Xu <calebcenter@live.com>
2020-10-29 12:42:30 -0400

    nginx 1.19.4

--- Formula/nginx.rb
+++ Formula/nginx.rb
@@ -3,8 +3,8 @@ class Nginx < Formula
   homepage "https://nginx.org/"
   # Use "mainline" releases only (odd minor version number), not "stable"
   # See https://www.nginx.com/blog/nginx-1-12-1-13-released/ for why
-  url "https://nginx.org/download/nginx-1.19.3.tar.gz"
-  sha256 "91e5b74fa17879d2463294e93ad8f6ffc066696ae32ad0478ffe15ba0e9e8df0"
+  url "https://nginx.org/download/nginx-1.19.4.tar.gz"
+  sha256 "61df546927905a0d624f9396bb7a8bc7ca7fd26522ce9714d56a78b73284000e"
   license "BSD-2-Clause"
   head "https://hg.nginx.org/nginx/", using: :hg
 
