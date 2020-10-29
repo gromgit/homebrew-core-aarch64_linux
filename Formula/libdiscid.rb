13fc72aca14439b3d90f0060e708d83c795c5b07
Viktor Szakats <commit@vsz.me>
2020-10-29 13:10:26 +0000

    libdiscid: add secure mirror, secure livecheck

--- Formula/libdiscid.rb
+++ Formula/libdiscid.rb
@@ -2,11 +2,12 @@ class Libdiscid < Formula
   desc "C library for creating MusicBrainz and freedb disc IDs"
   homepage "https://musicbrainz.org/doc/libdiscid"
   url "http://ftp.musicbrainz.org/pub/musicbrainz/libdiscid/libdiscid-0.6.2.tar.gz"
+  mirror "https://ftp.osuosl.org/pub/musicbrainz/libdiscid/libdiscid-0.6.2.tar.gz"
   sha256 "f9e443ac4c0dd4819c2841fcc82169a46fb9a626352cdb9c7f65dd3624cd31b9"
   license "LGPL-2.1"
 
   livecheck do
-    url "http://ftp.musicbrainz.org/pub/musicbrainz/libdiscid/"
+    url "https://ftp.osuosl.org/pub/musicbrainz/libdiscid/"
     regex(/href=.*?libdiscid[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
