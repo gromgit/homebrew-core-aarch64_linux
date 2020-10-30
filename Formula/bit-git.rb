85fa195d2eceeffda01b280b5459296110772ecc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-30 09:12:22 +0000

    bit-git: update 0.9.1 bottle.

--- Formula/bit-git.rb
+++ Formula/bit-git.rb
@@ -7,9 +7,9 @@ class BitGit < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6cafe748e647aeaf41c2d4a3191726bc2bd23b891c18f86a2b625e5da81b4be9" => :catalina
-    sha256 "a7a2220f9f0fd3929db87df0ed480b80414ee530dc6253330e2d04788b529a65" => :mojave
-    sha256 "e572c0defed08460d80427b828a626816dcbd475a4bb76cd1ba41b77c1f99728" => :high_sierra
+    sha256 "9d8d5ce469d71500e41a71203320f54f19ff8dede36a369da3a7b34802c36bf0" => :catalina
+    sha256 "e7eee70da0e4644227b175415d75264fbe86109f7f161831aaf1b99217d2a34b" => :mojave
+    sha256 "1fb7f8988ec1c6918262b04297779b4655dee27fb0abac1f7b99567dfee9eb2c" => :high_sierra
   end
 
   depends_on "go" => :build
