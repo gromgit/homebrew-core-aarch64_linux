44e5388b96501e782e039e2dd23661f31b8bae50
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-10-30 11:10:34 +0000

    fetchmail: update 6.4.13 bottle.

--- Formula/fetchmail.rb
+++ Formula/fetchmail.rb
@@ -11,9 +11,9 @@ class Fetchmail < Formula
 
   bottle do
     cellar :any
-    sha256 "ddfe9fb2f887e126a342d328f15e1e605dc4e3a010de8ecd2f8c87c6ec9f41c6" => :catalina
-    sha256 "acf99e4e026b233c26d45d0b6eb902aaf2fd94fef8ad88681dabb14531bd9391" => :mojave
-    sha256 "c934824791e311a5a18f442a41822338d6a1ef5446d4bc52bbf9b7319991a1b2" => :high_sierra
+    sha256 "4d8de02963568efa7ee1d6981ba07a65a3a71dae716b6c39d50194041412b654" => :catalina
+    sha256 "ec6784b3cfb19a9b71b5ad52b9a27d35cef4f059ca8bf0a00ae2efe824450de3" => :mojave
+    sha256 "6c6899e760722945cf40667a1e6300180c5623da67cb024d21242cfd1e9c2543" => :high_sierra
   end
 
   depends_on "openssl@1.1"
