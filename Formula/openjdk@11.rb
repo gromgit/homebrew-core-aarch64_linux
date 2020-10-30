86b568fca9281fa7241f0a55e16d09c422dae8fb
Michka Popoff <michkapopoff@gmail.com>
2020-10-30 09:52:36 +0100

    openjdk@11: add linux-only resource

--- Formula/openjdk@11.rb
+++ Formula/openjdk@11.rb
@@ -22,8 +22,14 @@ class OpenjdkAT11 < Formula
   end
 
   resource "boot-jdk" do
-    url "https://download.java.net/java/GA/jdk10/10.0.2/19aef61b38124481863b1413dce1855f/13/openjdk-10.0.2_osx-x64_bin.tar.gz"
-    sha256 "77ea7675ee29b85aa7df138014790f91047bfdafbc997cb41a1030a0417356d7"
+    on_macos do
+      url "https://download.java.net/java/GA/jdk10/10.0.2/19aef61b38124481863b1413dce1855f/13/openjdk-10.0.2_osx-x64_bin.tar.gz"
+      sha256 "77ea7675ee29b85aa7df138014790f91047bfdafbc997cb41a1030a0417356d7"
+    end
+    on_linux do
+      url "https://download.java.net/java/GA/jdk10/10.0.2/19aef61b38124481863b1413dce1855f/13/openjdk-10.0.2_linux-x64_bin.tar.gz"
+      sha256 "f3b26abc9990a0b8929781310e14a339a7542adfd6596afb842fa0dd7e3848b2"
+    end
   end
 
   # Fix build on Xcode 12
