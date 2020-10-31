b43cf6bf38247461ab86973612b95eca9f90a83d
Robert Blair <robertleeblairjr@outlook.com>
2020-10-30 21:05:10 -0500

    wireshark 3.4.0
    * wireshark 3.4.0
    * Update wireshark.rb
      Update missing license.
    
    Closes #63770.
    
    Co-authored-by: Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wireshark.rb
+++ Formula/wireshark.rb
@@ -1,9 +1,10 @@
 class Wireshark < Formula
   desc "Graphical network analyzer and capture tool"
   homepage "https://www.wireshark.org"
-  url "https://www.wireshark.org/download/src/all-versions/wireshark-3.2.7.tar.xz"
-  mirror "https://1.eu.dl.wireshark.org/src/all-versions/wireshark-3.2.7.tar.xz"
-  sha256 "be832fb86d9c455c5be8b225a755cdc77cb0e92356bdfc1fe4b000d93f7d70da"
+  url "https://www.wireshark.org/download/src/all-versions/wireshark-3.4.0.tar.xz"
+  mirror "https://1.na.dl.wireshark.org/src/wireshark-3.4.0.tar.xz"
+  sha256 "67e4ebbd9153fc589fd67dc21b93176674c73adc3d5a43934c3ac69d8594a8ae"
+  license "GPL-2.0-or-later"
   head "https://code.wireshark.org/review/wireshark", using: :git
 
   livecheck do
