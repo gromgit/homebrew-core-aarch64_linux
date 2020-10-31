5034f1d2c7c0a9cb68fea65347265174b79935f2
Dario Vladovic <d.vladimyr@gmail.com>
2020-10-31 21:37:19 +0100

    shfmt 3.2.0
    Closes #63812.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/shfmt.rb
+++ Formula/shfmt.rb
@@ -1,8 +1,8 @@
 class Shfmt < Formula
   desc "Autoformat shell script source code"
   homepage "https://github.com/mvdan/sh"
-  url "https://github.com/mvdan/sh/archive/v3.1.2.tar.gz"
-  sha256 "133fcdb4645ee0c2893319b1ce5b83c88b8576c1e3936b1fa14b967df1501ee5"
+  url "https://github.com/mvdan/sh/archive/v3.2.0.tar.gz"
+  sha256 "6755f587fcb6f037f819b96a322b0273d0ab6ecb5911c005b9aae74292c4a819"
   license "BSD-3-Clause"
   head "https://github.com/mvdan/sh.git"
 
@@ -14,6 +14,7 @@ class Shfmt < Formula
   end
 
   depends_on "go" => :build
+  depends_on "scdoc" => :build
 
   def install
     ENV["CGO_ENABLED"] = "0"
@@ -22,6 +23,8 @@ class Shfmt < Formula
     system "go", "build", "-a", "-tags", "production brew", "-ldflags",
                           "-w -s -extldflags '-static' -X main.version=#{version}",
                           "-o", "#{bin}/shfmt", "./cmd/shfmt"
+    man1.mkpath
+    system "scdoc < ./cmd/shfmt/shfmt.1.scd > #{man1}/shfmt.1"
   end
 
   test do
