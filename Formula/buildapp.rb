4cc2e72759dc6ff60c0e70c3347c85ed361ef999
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-01 04:17:05 +0000

    buildapp: update 1.5.6_2 bottle.

--- Formula/buildapp.rb
+++ Formula/buildapp.rb
@@ -8,11 +8,9 @@ class Buildapp < Formula
   head "https://github.com/xach/buildapp.git"
 
   bottle do
-    sha256 "7954a9008618ee64fbf2b93b1b2f503e25e979efaf5f10afee916d455ac3e231" => :catalina
-    sha256 "86fa0142bbde84222c65edec4f082b99d7fbd67c4767723025fd12c9f60b2db4" => :mojave
-    sha256 "aa14506cc1ab8eb712c9b1fdb71af45c9041b327c9ecdf9d48541b652e3da3be" => :high_sierra
-    sha256 "15e1be1e7928770abfdb801e67435b9d7c1bc36923bc6d9d5281d9ceab0d9abd" => :sierra
-    sha256 "49d62ce2b8564d5456725d1a7cedafb38690d9bcdf5e7b6f279805665ed65833" => :el_capitan
+    sha256 "a0177161f65808315986f1eaab3305ffcf9ca03fff434687f6354d0711e7c9ef" => :catalina
+    sha256 "d4721d62fb334d49ac7730a4110826efeac3b0b8db25671a20ce20258ff303a3" => :mojave
+    sha256 "87758d9045461b6b9cf23dd9b75e328a8b817a0e12703a696b93dd574396cd72" => :high_sierra
   end
 
   depends_on "sbcl"
