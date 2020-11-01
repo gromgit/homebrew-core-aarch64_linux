73bcb035c02e03e371c77008bdfce1cc99b39747
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-11-01 17:18:07 +0100

    pkg-config: add license

--- Formula/pkg-config.rb
+++ Formula/pkg-config.rb
@@ -4,6 +4,7 @@ class PkgConfig < Formula
   url "https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.2.tar.gz"
   mirror "https://dl.bintray.com/homebrew/mirror/pkg-config-0.29.2.tar.gz"
   sha256 "6fc69c01688c9458a57eb9a1664c9aba372ccda420a02bf4429fe610e7e7d591"
+  license "GPL-2.0-or-later"
   revision 3
 
   livecheck do
