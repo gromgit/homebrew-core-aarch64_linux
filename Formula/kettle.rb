352f01daf89995c8968d1077e84127bc90f6fe7e
Rui Chen <rui@meetup.com>
2020-11-02 11:21:37 -0500

    kettle 9.1.0.0-324
    * kettle 9.1.0.0-324
    * kettle: update homepage
    
    Closes #63962.
    
    Co-authored-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kettle.rb
+++ Formula/kettle.rb
@@ -1,8 +1,8 @@
 class Kettle < Formula
   desc "Pentaho Data Integration software"
-  homepage "https://community.hitachivantara.com/docs/DOC-1009931-downloads"
-  url "https://downloads.sourceforge.net/project/pentaho/Pentaho%209.0/client-tools/pdi-ce-9.0.0.0-423.zip"
-  sha256 "05adf26c8b51fb14d1ea75e73579a7718d881228f502a4be5d1ce3a27d5c7997"
+  homepage "https://www.hitachivantara.com/en-us/products/data-management-analytics.html"
+  url "https://downloads.sourceforge.net/project/pentaho/Pentaho%209.1/client-tools/pdi-ce-9.1.0.0-324.zip"
+  sha256 "ffbcb7bba736af765bbb14ccb0a5f2ae239e75b5aebf0ecfee924a5738d2c530"
 
   livecheck do
     url :stable
