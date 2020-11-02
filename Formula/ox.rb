a73d450786c2dc24580ff6ce90c06a4b41e34f76
chenrui <chenrui333@gmail.com>
2020-11-02 14:30:31 +0000

    ox: add 0.2.5 bottle.

--- Formula/ox.rb
+++ Formula/ox.rb
@@ -6,6 +6,13 @@ class Ox < Formula
   license "GPL-2.0-only"
   head "https://github.com/curlpipe/ox.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7677242429502708970da5a5a4665cfa0b999581e6ab793ce8c4304d2488c6d7" => :catalina
+    sha256 "fc76d843172825e04a7912b0aeace96c7e913c2b851f08cb646b4bc3f1add3aa" => :mojave
+    sha256 "3008a1719ef3675fc9fb9c12de08dc7e1193877084f6a83db6a1ec87ac453da7" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
