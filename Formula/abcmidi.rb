15e62124e573415823ce9c9eb5b025526cae8555
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-02 06:14:16 +0000

    abcmidi: update 2020.11.01 bottle.

--- Formula/abcmidi.rb
+++ Formula/abcmidi.rb
@@ -12,9 +12,9 @@ class Abcmidi < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "2b013148c153f0dee49557daad99ecd5b70771c077739a129b23625f81fb7ee0" => :catalina
-    sha256 "916f79557e947f91d39b9aeeefa1bdd034532da46d817174d197cdd07b8fbb52" => :mojave
-    sha256 "184b1d4290470a4756acf100df93c50435e5b55837c9456ce587f614ac981ae3" => :high_sierra
+    sha256 "65561d7449e8ddbb172a5def4a5ff56e74bf39b2dc7e60caf610ae3061bdcfd4" => :catalina
+    sha256 "66de62a0862dd7ed33fad053e84aa4291bbb8537341a6cfb6c3c0b86681af40a" => :mojave
+    sha256 "9b0110ff1521e9b2aea3b467680572129788c9fcea5dc392eec4fde29c26dd93" => :high_sierra
   end
 
   def install
