dbf7ce573c8a351bbb86e13e8f38d6eda57770a5
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-11-02 21:24:15 +0000

    double-conversion: update 3.1.5_1 bottle.

--- Formula/double-conversion.rb
+++ Formula/double-conversion.rb
@@ -8,11 +8,10 @@ class DoubleConversion < Formula
   head "https://github.com/google/double-conversion.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "f016bf145173af7242ce615b5764e123972e5c0c469908ea1e3c58793e7b829e" => :catalina
-    sha256 "faa661750aeda3faf356d445d3d293fa52021c93a08fea35fd6666251b44203b" => :mojave
-    sha256 "c948a1b31bc508f9218b6373e5ac3cc92838aa033e15f777aa046675921c3369" => :high_sierra
-    sha256 "6fad17756240370dffc053a66fdfff4f17b02669c9456546a591349c3ea0e959" => :sierra
+    cellar :any
+    sha256 "20b93e20891d48912ffbfbdf3ef470f7305684df2381ef93056a11cedd95c65f" => :catalina
+    sha256 "ec700c89a4f1794170b4466f5a0a100b6eafee7cb0a794e55ea53de18114a1d3" => :mojave
+    sha256 "9b54153b09683b8fa40160588792385e04f6be56ba355c5a530a2209b9f0526d" => :high_sierra
   end
 
   depends_on "cmake" => :build
