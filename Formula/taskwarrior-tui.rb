697ee1f02d2894269671be5b6d83c19f5ea41540
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-03 21:11:17 +0000

    taskwarrior-tui: update 0.9.1 bottle.

--- Formula/taskwarrior-tui.rb
+++ Formula/taskwarrior-tui.rb
@@ -13,9 +13,9 @@ class TaskwarriorTui < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "99d5dc70534dc556b5ee04f65325c1fe1e6edf8e06b050403c77dc12bf24acd3" => :catalina
-    sha256 "1ed0f1608949bb581da1ecfdd6dec512b70206e3a4593cddff72542bee66d1ef" => :mojave
-    sha256 "2de2c50dffa8368ecda125f685559f685dc10765ca9054083dba732e22bdfe46" => :high_sierra
+    sha256 "f4e90c97e487d9f4e2ff4eb348b992d91ab5dd596b0c4f0a8694d9fb6300dda9" => :catalina
+    sha256 "e9220b3c5e471c626a8863d7c1e9847c181953fe27ea1f6749f6fec6c1fd9d80" => :mojave
+    sha256 "dd4678c8654e6c26b2ebb813e73233446d69e07b97c150fd928f7ad43e96bbc1" => :high_sierra
   end
 
   depends_on "rust" => :build
