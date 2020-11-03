da4ff5d292b0aa87537a96ec4d8c07cc6b2ac9ae
Rui Chen <rui@meetup.com>
2020-11-03 16:39:03 -0500

    stellar-core 15.0.0
    Closes #64061.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/stellar-core.rb
+++ Formula/stellar-core.rb
@@ -2,8 +2,8 @@ class StellarCore < Formula
   desc "Backbone of the Stellar (XLM) network"
   homepage "https://www.stellar.org/"
   url "https://github.com/stellar/stellar-core.git",
-      tag:      "v14.1.1",
-      revision: "5b2989d334f578b00ac9b5996888257f5922a8a4"
+      tag:      "v15.0.0",
+      revision: "8d6e6d74cfae65943211bdcc5ba083976d525a04"
   license "Apache-2.0"
   head "https://github.com/stellar/stellar-core.git"
 
