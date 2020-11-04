f8c50a382156306b36d413eb5f8f98d464ebc9e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-04 07:14:31 +0000

    libvirt: update 6.9.0 bottle.

--- Formula/libvirt.rb
+++ Formula/libvirt.rb
@@ -11,9 +11,9 @@ class Libvirt < Formula
   end
 
   bottle do
-    sha256 "f803b6967dc9261f561d4deba6ff0950f7e56c9b0febcb2e3c6f7c67b02325a4" => :catalina
-    sha256 "c37e01074f22e646bcffe850e33ff1295a490295aab8860aad80cf5ac09f05e6" => :mojave
-    sha256 "401fc69e2a5daf2f6b47a8cc76791bf80824f5b748ca239b0ef8f61fa27e1c0a" => :high_sierra
+    sha256 "29af84b6c94f23f1b0a5bdccd594637a6152f8f1206b4b20825e814f76223bf8" => :catalina
+    sha256 "ba463dcbd3549764adc8de27bf2c291974fdc497e9cd89e94104f7effda1e9ec" => :mojave
+    sha256 "9b00d9ffca80b31f488d1d5ba10e7088d04f81507dd5056dd19c8295d6378f9a" => :high_sierra
   end
 
   head do
