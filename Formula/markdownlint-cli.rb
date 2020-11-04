42d7903f0bdab796c2d6fd15eca3aa5a63b5d067
Dustin Rodrigues <dust.rod@gmail.com>
2020-11-04 17:21:01 -0500

    markdownlint-cli 0.24.0 (new formula)
    Closes #64124.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>

---
+++ Formula/markdownlint-cli.rb
@@ -0,0 +1,35 @@
+require "language/node"
+
+class MarkdownlintCli < Formula
+  desc "CLI for Node.js style checker and lint tool for Markdown files"
+  homepage "https://github.com/igorshubovych/markdownlint-cli"
+  url "https://registry.npmjs.org/markdownlint-cli/-/markdownlint-cli-0.24.0.tgz"
+  sha256 "9ac4da108412d826be23549bc1fbddf5f3ad62e4e6b239b283ee4ad439605875"
+  license "MIT"
+
+  livecheck do
+    url :stable
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"test-bad.md").write <<~EOS
+      # Header 1
+      body
+    EOS
+    (testpath/"test-good.md").write <<~EOS
+      # Header 1
+
+      body
+    EOS
+    assert_match "MD022/blanks-around-headings/blanks-around-headers",
+                 shell_output("#{bin}/markdownlint #{testpath}/test-bad.md  2>&1", 1)
+    assert_empty shell_output("#{bin}/markdownlint #{testpath}/test-good.md")
+  end
+end
