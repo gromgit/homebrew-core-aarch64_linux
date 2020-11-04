497315300c49e5fead4e50704bdf7c50418f7f40
curoky <cccuroky@gmail.com>
2020-11-04 22:35:59 +0800

    hexo 5.3.0 (new formula)
    * hexo 5.2.0 (new formula)
    * hexo: upgrade to 5.3.0
    * hexo: update test
    * hexo: update desc
    
    Closes #69167.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: rui <rui@chenrui.dev>

---
+++ Formula/hexo.rb
@@ -0,0 +1,31 @@
+require "language/node"
+
+class Hexo < Formula
+  desc "Fast, simple & powerful blog framework"
+  homepage "https://hexo.io/"
+  url "https://registry.npmjs.org/hexo/-/hexo-5.3.0.tgz"
+  sha256 "fe9f025d1f0d51e27a9a66cc691748c730f126b50bcc0c99bee239a582fd1ed8"
+  license "MIT"
+  head "https://github.com/hexojs/hexo.git"
+
+  livecheck do
+    url :stable
+  end
+
+  depends_on "node"
+
+  def install
+    mkdir_p libexec/"lib"
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    output = shell_output("#{bin}/hexo --help")
+    assert_match "Usage: hexo <command>", output.strip
+
+    output = shell_output("#{bin}/hexo init blog --no-install")
+    assert_match "Cloning hexo-starter", output.strip
+    assert_predicate testpath/"blog/_config.yml", :exist?
+  end
+end
