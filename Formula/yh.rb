87eb9a38e3d9acd1d834df245730e2ee88a39e04
Dawid Dziurla <dawidd0811@gmail.com>
2020-11-04 10:51:39 +0000

    yh: add 0.4.0 bottle.

--- Formula/yh.rb
+++ Formula/yh.rb
@@ -6,6 +6,13 @@ class Yh < Formula
   license "Apache-2.0"
   head "https://github.com/andreazorzetto/yh.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1a2425d399a63df18758dfabf9d50da2559fb489c32bfb4462d7437f64fc0817" => :catalina
+    sha256 "69f1ab9c740906f04924c780cb512ea26fa0c51bdf66be85c71c4cbaa9dc6ca1" => :mojave
+    sha256 "184eb9a41954f7a3d11f3065dfab42085a724c617ec635681e05784eeebe6329" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
