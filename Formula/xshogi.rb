89e29cb7b30f400249e0ff829b5c2c3c48e3a113
Issy Long <me@issyl0.co.uk>
2020-11-05 21:05:28 +0000

    xshogi: Migrate to depend on brewed X11
    - Issue reference: #64166.
    
    ----
    
    Before:
    
    ➜ brew linkage xshogi
    System libraries:
      /opt/X11/lib/libX11.6.dylib
      /opt/X11/lib/libXaw.7.dylib
      /opt/X11/lib/libXext.6.dylib
      /opt/X11/lib/libXmu.6.dylib
      /opt/X11/lib/libXt.6.dylib
      /usr/lib/libSystem.B.dylib
    
    After:
    
    ➜ brew linkage xshogi
    System libraries:
      /usr/lib/libSystem.B.dylib
    Homebrew libraries:
      /usr/local/opt/libx11/lib/libX11.6.dylib (libx11)
      /usr/local/opt/libxaw/lib/libXaw.7.dylib (libxaw)
      /usr/local/opt/libxext/lib/libXext.6.dylib (libxext)
      /usr/local/opt/libxmu/lib/libXmu.6.dylib (libxmu)
      /usr/local/opt/libxt/lib/libXt.6.dylib (libxt)
    
    Closes #64200.
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/xshogi.rb
+++ Formula/xshogi.rb
@@ -5,6 +5,7 @@ class Xshogi < Formula
   mirror "https://ftpmirror.gnu.org/gnushogi/xshogi-1.4.2.tar.gz"
   sha256 "2e2f1145e3317143615a764411178f538bd54945646b14fc2264aaeaa105dab6"
   license "GPL-3.0-or-later"
+  revision 1
 
   livecheck do
     url :stable
@@ -21,7 +22,11 @@ class Xshogi < Formula
   end
 
   depends_on "gnu-shogi"
-  depends_on :x11
+  depends_on "libx11"
+  depends_on "libxaw"
+  depends_on "libxext"
+  depends_on "libxmu"
+  depends_on "libxt"
 
   def install
     system "./configure", "--prefix=#{prefix}",
