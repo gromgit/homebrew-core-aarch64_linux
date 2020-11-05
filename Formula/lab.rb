439b6aeed9b3ec7a1edeadfe8badc9696d60ac71
Dario Vladovic <d.vladimyr@gmail.com>
2020-11-06 00:40:16 +0100

    lab 0.17.2 (new formula)
    Closes #64208.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>

---
+++ Formula/lab.rb
@@ -0,0 +1,38 @@
+class Lab < Formula
+  desc "Git wrapper for GitLab"
+  homepage "https://zaquestion.github.io/lab"
+  url "https://github.com/zaquestion/lab/archive/v0.17.2.tar.gz"
+  sha256 "467cb35793c4129e7da68e4c63ef5ee96e9ca43f933c88758e90850f0d6c77b9"
+  license "CC0-1.0"
+  head "https://github.com/zaquestion/lab.git"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-X main.version=#{version} -s -w"
+    system "go", "build", *std_go_args, "-ldflags=#{ldflags}"
+    output = Utils.safe_popen_read("#{bin}/lab", "completion", "bash")
+    (bash_completion/"lab").write output
+    output = Utils.safe_popen_read("#{bin}/lab", "completion", "zsh")
+    (zsh_completion/"_lab").write output
+    output = Utils.safe_popen_read("#{bin}/lab", "completion", "fish")
+    (fish_completion/"lab.fish").write output
+  end
+
+  test do
+    ENV["LAB_CORE_USER"] = "test_user"
+    ENV["LAB_CORE_HOST"] = "https://gitlab.com"
+    ENV["LAB_CORE_TOKEN"] = "dummy"
+
+    ENV["GIT_AUTHOR_NAME"] = "test user"
+    ENV["GIT_AUTHOR_EMAIL"] = "test@example.com"
+    ENV["GIT_COMMITTER_NAME"] = "test user"
+    ENV["GIT_COMMITTER_EMAIL"] = "test@example.com"
+
+    system "git", "init"
+    %w[haunted house].each { |f| touch testpath/f }
+    system "git", "add", "haunted", "house"
+    system "git", "commit", "-a", "-m", "Initial Commit"
+    assert_equal "haunted\nhouse", shell_output("#{bin}/lab ls-files").strip
+  end
+end
