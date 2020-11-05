4948bc66457eaf4f6c4975970ac6964a7faec5c9
Rui Chen <rui@meetup.com>
2020-11-05 15:49:11 -0500

    mpv: revision for x264
    Signed-off-by: Rui Chen <rui@meetup.com>

--- Formula/mpv.rb
+++ Formula/mpv.rb
@@ -4,7 +4,7 @@ class Mpv < Formula
   url "https://github.com/mpv-player/mpv/archive/v0.32.0.tar.gz"
   sha256 "9163f64832226d22e24bbc4874ebd6ac02372cd717bef15c28a0aa858c5fe592"
   license :cannot_represent
-  revision 7
+  revision 8
   head "https://github.com/mpv-player/mpv.git"
 
   bottle do
