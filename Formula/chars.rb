c9dd32e3e497afe917ac30f91f19f3d371812982
Dustin Rodrigues <dust.rod@gmail.com>
2020-11-06 18:50:44 +0000

    chars: add 0.5.0 bottle.

--- Formula/chars.rb
+++ Formula/chars.rb
@@ -6,6 +6,13 @@ class Chars < Formula
   license "MIT"
   head "https://github.com/antifuchs/chars.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4d56b107586689744485ede26f55a0916fe09bfa2e9ec27a250c9da3764f5e42" => :catalina
+    sha256 "d4bd08669fb838fb8773e4131fd71b972662299ac3c03ff6d83802c6e3d14efc" => :mojave
+    sha256 "2dbec73c5adcc494e0932921f015e69b7b7bd8b1d1b29f7ea08bc56da029c9a0" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
