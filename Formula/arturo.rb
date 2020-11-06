0a7a40cc49976d75e3bfa710bcf43a875bd78ebc
Rui Chen <rui@meetup.com>
2020-11-06 17:12:44 -0500

    arturo 0.9.5
    * arturo 0.9.5
    * arturo: depends on mysql
    * fix syntax
    
    Closes #64264.
    
    Co-authored-by: chenrui <rui@meetup.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/arturo.rb
+++ Formula/arturo.rb
@@ -1,8 +1,8 @@
 class Arturo < Formula
   desc "Simple, modern and portable programming language for efficient scripting"
   homepage "http://arturo-lang.io"
-  url "https://github.com/arturo-lang/arturo/archive/v0.9.4.6.tar.gz"
-  sha256 "90dfd8870ab0bfadd14cd611a33353a1d4b66b25e0c56d75cab895989be25c98"
+  url "https://github.com/arturo-lang/arturo/archive/v0.9.5.tar.gz"
+  sha256 "fcf8064d4ae49fee0eeca6fd55a9ba30abefd9111c3102c6d682f56993694fc1"
   license "MIT"
 
   bottle do
@@ -14,6 +14,7 @@ class Arturo < Formula
 
   depends_on "nim" => :build
   depends_on "gmp"
+  depends_on "mysql"
 
   def install
     system "./build.sh"
