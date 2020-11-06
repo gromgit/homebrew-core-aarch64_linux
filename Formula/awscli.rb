ff5b631fe7bec2d08386284c1d03bb54dfea220e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-06 05:13:16 +0000

    awscli: update 2.0.62 bottle.

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -9,9 +9,9 @@ class Awscli < Formula
   head "https://github.com/aws/aws-cli.git", branch: "v2"
 
   bottle do
-    sha256 "ce5cbc07072c9e4b328b76491570810d95fcf7eaa008bf5b8902828cadd7736a" => :catalina
-    sha256 "bd29b0ab2c51b00fab1fbe8bbeab8aac52c4c80e74c7905399f53414b8ef1745" => :mojave
-    sha256 "6c0a5c57dfe052ee0e0334255e7d9043a251419aa85d227287296fce1d4c5d4b" => :high_sierra
+    sha256 "b0d31caddb17ab21e6684520653457ff9775e9e32f9900f6b648bd02bcc8233f" => :catalina
+    sha256 "f46e9579bf320868345c6e71540db8ba824a01ea9cac0a48443bbaebe91e22b5" => :mojave
+    sha256 "a8790b4a1ab6cd70258f22393d9b7b7c5f6eb7532fcc5b4abb6271cda69672fa" => :high_sierra
   end
 
   depends_on "python@3.9"
