37b43bfde416f3e11f1975c99bb58c0f5d0df06f
Seeker <meaningseeking@protonmail.com>
2020-11-06 08:51:20 -0800

    strace 5.8 (new formula)

---
+++ Formula/strace.rb
@@ -0,0 +1,32 @@
+class Strace < Formula
+  desc "Diagnostic, instructional, and debugging tool for the Linux kernel"
+  homepage "https://strace.io/"
+  url "https://github.com/strace/strace/releases/download/v5.8/strace-5.8.tar.xz"
+  sha256 "df4a669f7fff9cc302784085bd4b72fab216a426a3f72c892b28a537b71e7aa9"
+  license "LGPL-2.1-or-later"
+
+  head do
+    url "https://github.com/strace/strace.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
+  depends_on :linux
+  depends_on "linux-headers"
+
+  def install
+    system "./bootstrap" if build.head?
+    system "./configure",
+      "--disable-dependency-tracking",
+      "--disable-silent-rules",
+      "--prefix=#{prefix}",
+      "--enable-mpers=no" # FIX: configure: error: Cannot enable m32 personality support
+    system "make", "install"
+  end
+
+  test do
+    out = `"strace" "true" 2>&1` # strace the true command, redirect stderr to output
+    assert_match "execve(", out
+    assert_match "+++ exited with 0 +++", out
+  end
+end
