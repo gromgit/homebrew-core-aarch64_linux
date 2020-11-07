14772cbd1648281594692a4b83112caff6c96647
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-07 05:13:09 +0000

    sword: update 1.9.0 bottle.

--- Formula/sword.rb
+++ Formula/sword.rb
@@ -11,11 +11,9 @@ class Sword < Formula
   end
 
   bottle do
-    sha256 "3c4fd6972c16470479e43a43ef097a053ed6543b6e3d2cdfb1c15ff7416dbae1" => :catalina
-    sha256 "ab228fd2df3f45e696e50224872a2fd80d24fcebc92f2b6ba5ff6e36d8534bc6" => :mojave
-    sha256 "3279c77477c21d1636f573202df976bd37fbaca39ed7e3b310158dad4e961641" => :high_sierra
-    sha256 "794afe687eb7933fd3aeaee7e480224295614fc0138b8d89b7d9b81be55239a1" => :sierra
-    sha256 "032c83b3302b78c198d1e346258a1d09c542a1361e1b0f000f82306d8c82acb4" => :el_capitan
+    sha256 "606a413399112a486c2b4095a76f83b03ed6f08efc0b00074e68dcdbad6c8aff" => :catalina
+    sha256 "4090521dc9b44fcfba1d9fdebd3ea2c708ca4f6046c278fea3fc568e3a8bc61c" => :mojave
+    sha256 "6bb12d6e5bee18ef39752f365fdb2cfb9ea6347b51db9ee36f8d4a70684dcaa1" => :high_sierra
   end
 
   def install
