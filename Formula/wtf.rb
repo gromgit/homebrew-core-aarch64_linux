adfb52804b7351c7aa93e4b813153dae7fa0d287
chenrui <chenrui333@gmail.com>
2020-11-06 19:11:38 -0500

    wtf 20201105 (#64268)

--- Formula/wtf.rb
+++ Formula/wtf.rb
@@ -1,8 +1,8 @@
 class Wtf < Formula
   desc "Translate common Internet acronyms"
   homepage "https://sourceforge.net/projects/bsdwtf/"
-  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20201018.tar.gz"
-  sha256 "befc1416385e9c928adb526f623ddb6a9207e5e10eeda4911fe5efedd2300574"
+  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20201105.tar.gz"
+  sha256 "d931b7ffa1b2f52f55f778c3de640def0a17a94898670e1de3201d9155cc0a50"
   license :public_domain
 
   livecheck do
