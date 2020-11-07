ae1233fb6c9a7746ff83b9ab6b5178cc5a25ef7d
Seeker <meaningseeking@protonmail.com>
2020-11-07 09:24:55 -0800

    xterm 361 (new formula)
    Closes #64303.
    
    Signed-off-by: Jonathan Chang <me@jonathanchang.org>

---
+++ Formula/xterm.rb
@@ -0,0 +1,35 @@
+class Xterm < Formula
+  desc "Terminal emulator for the X Window System"
+  homepage "https://invisible-island.net/xterm/"
+  url "https://invisible-mirror.net/archives/xterm/xterm-361.tgz"
+  mirror "https://deb.debian.org/debian/pool/main/x/xterm/xterm_361.orig.tar.gz"
+  sha256 "85610f20d5e47205cc1b6876f7a4da28d6ae051bd8eac0b932e92c37a73a623f"
+  license :cannot_represent
+
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "libice"
+  depends_on "libx11"
+  depends_on "libxaw"
+  depends_on "libxext"
+  depends_on "libxft"
+  depends_on "libxinerama"
+  depends_on "libxmu"
+  depends_on "libxpm"
+  depends_on "libxt"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    %w[koi8rxterm resize uxterm xterm].each do |exe|
+      assert_predicate bin/exe, :exist?
+      assert_predicate bin/exe, :executable?
+    end
+  end
+end
