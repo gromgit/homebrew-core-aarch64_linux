f9d8ff93b71000c3489773d0e489ad2c1b0def0b
chenrui <chenrui333@gmail.com>
2020-11-06 19:23:42 -0500

    nvm 0.37.0 (#64263)

--- Formula/nvm.rb
+++ Formula/nvm.rb
@@ -1,8 +1,8 @@
 class Nvm < Formula
   desc "Manage multiple Node.js versions"
   homepage "https://github.com/nvm-sh/nvm"
-  url "https://github.com/nvm-sh/nvm/archive/v0.36.0.tar.gz"
-  sha256 "4e9de23fedde992af9547bedb2bf55b74fa765e930170e65907e85d6b48ae069"
+  url "https://github.com/creationix/nvm/archive/v0.37.0.tar.gz"
+  sha256 "c22fb788a64ed48f6f44ebd7ba6c796471a1413eee085adfb7493b553ae05a09"
   license "MIT"
   head "https://github.com/nvm-sh/nvm.git"
 
