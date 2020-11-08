3acb1660770a81d33c207530de3c9a1a93a19e43
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-08 06:15:27 +0000

    futhark: update 0.18.2 bottle.

--- Formula/futhark.rb
+++ Formula/futhark.rb
@@ -8,9 +8,9 @@ class Futhark < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f118ffa2a1850d2cc4b913b730d643028b524bf7e0680a142c0150f04da70ba6" => :catalina
-    sha256 "26298e76327b8504f57fc2b0762f1b0680519184881678161b299fd904f4b168" => :mojave
-    sha256 "9272b36bd8044cb7521c17bc777a50734223a50f5460a33b96fcf997777863d7" => :high_sierra
+    sha256 "334d72f32e477ff87ed21faa8d21e065ac3cb228c572a1b80a394201634b76f4" => :catalina
+    sha256 "c4714889d9c830626b6d3a559ed2a8dad02c052f3f866f69ae876960e366f100" => :mojave
+    sha256 "75765bfb4335e08df2a03896eb1cb588df6e9e4b9dd5ac6f03286554396b8e9c" => :high_sierra
   end
 
   depends_on "cabal-install" => :build
