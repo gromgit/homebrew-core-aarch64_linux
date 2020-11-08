6a47e49bb51de3f029b523a2a42143c4c0055ae0
Adrian Ho <aho+rover-air61@03s.net>
2020-11-08 22:48:57 +0800

    openmotif: migrate to brewed X11
    Also fixed license. Supports #64166.

--- Formula/openmotif.rb
+++ Formula/openmotif.rb
@@ -3,7 +3,8 @@ class Openmotif < Formula
   homepage "https://motif.ics.com/motif"
   url "https://downloads.sourceforge.net/project/motif/Motif%202.3.8%20Source%20Code/motif-2.3.8.tar.gz"
   sha256 "859b723666eeac7df018209d66045c9853b50b4218cecadb794e2359619ebce7"
-  license "LGPL-2.1"
+  license "LGPL-2.1-or-later"
+  revision 1
 
   livecheck do
     url :stable
@@ -21,8 +22,16 @@ class Openmotif < Formula
   depends_on "fontconfig"
   depends_on "freetype"
   depends_on "jpeg"
+  depends_on "libice"
   depends_on "libpng"
-  depends_on :x11
+  depends_on "libsm"
+  depends_on "libx11"
+  depends_on "libxext"
+  depends_on "libxft"
+  depends_on "libxmu"
+  depends_on "libxp"
+  depends_on "libxt"
+  depends_on "xbitmaps"
 
   conflicts_with "lesstif",
     because: "both Lesstif and Openmotif are complete replacements for each other"
