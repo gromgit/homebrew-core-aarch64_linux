db867d02977594ce83a772136f5ffd6f5ed7ac81
chenrui <chenrui333@gmail.com>
2020-11-08 16:11:16 +0000

    openal-soft: update 1.21.0 bottle.

--- Formula/openal-soft.rb
+++ Formula/openal-soft.rb
@@ -8,9 +8,9 @@ class OpenalSoft < Formula
 
   bottle do
     cellar :any
-    sha256 "b62337bb563eaaacec777b6861de4b6ba1d33ad8e19418b4b5a0cdfb37473fcf" => :catalina
-    sha256 "f6448e18550e62cd283bebe24e98f6253c5c18758aeb7c44f3816f1840082b25" => :mojave
-    sha256 "26dbb8819cd991e4a901f3f90f0cc2339c9e4220330329dcf7caa37b14d048d7" => :high_sierra
+    sha256 "2f288bddf5b23b868e7ee2773877eeeab70dce4bc3ba7e95fd106753be7e361d" => :catalina
+    sha256 "a6fac3b7778cba045106631a61b7f9cf58c189cc27ca210983b3f7c73c48301e" => :mojave
+    sha256 "c1f4cf0e42e75b583ff7a78dad6850b6ed8874bb6aeb7734b8116366a5b6697e" => :high_sierra
   end
 
   keg_only :shadowed_by_macos, "macOS provides OpenAL.framework"
