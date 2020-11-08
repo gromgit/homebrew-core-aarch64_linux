79aacbde14ad3f6a09a990a7ad15a5259798022e
Dylan Garrett <dylan.garrett@gmail.com>
2020-11-08 15:01:25 -0800

    overdrive 2.1.1 (new formula)
    * overdrive 2.1.1 (new formula)
    * Fix dependencies
    * Improve test and fix audit
    * Fix command naming, per original install instructions
    * Switch to libressl, which is actually correct
    
    Closes #64392.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/overdrive.rb
@@ -0,0 +1,24 @@
+class Overdrive < Formula
+  desc "Bash script to download mp3s from the OverDrive audiobook service"
+  homepage "https://github.com/chbrown/overdrive"
+  url "https://github.com/chbrown/overdrive/archive/2.1.1.tar.gz"
+  sha256 "74ec42df2c5dda56bfe04c0f8b831d21fd1511c0ef2839dd2bd84d1fda2b8b6b"
+  license "MIT"
+  head "https://github.com/chbrown/overdrive.git"
+
+  bottle :unneeded
+
+  depends_on "tidy-html5"
+  uses_from_macos "curl"
+  uses_from_macos "libressl"
+  uses_from_macos "libxml2"
+
+  def install
+    bin.install "overdrive.sh" => "overdrive"
+  end
+
+  test do
+    # A full run would require an authentic file, which can only be used once
+    assert_match "warning: failed to load", shell_output("#{bin}/overdrive download fake_file.odm 2>&1", 1)
+  end
+end
