293d877116e661a78e5c1d85ac972bcc2d1cf14e
chenrui <chenrui333@gmail.com>
2020-11-08 15:06:37 -0500

    intercal: update livecheck
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Formula/intercal.rb
+++ Formula/intercal.rb
@@ -5,6 +5,18 @@ class Intercal < Formula
   sha256 "93d842b81ecdc82b352beb463fbf688749b0c04445388a999667e1958bba4ffc"
   license "GPL-2.0"
 
+  # The latest version tags in the Git repository are `0.31` (2019-06-12) and
+  # `0.30` (2015-04-02) but there are older versions like `1.27` (2010-08-25)
+  # and `1.26` (2010-08-25). These two older 1.x releases are wrongly treated
+  # as newer but the GitLab project doesn't do releases, so we can only
+  # reference the tags. We work around this by restricting matching to 0.x
+  # releases for now. If the major version reaches 1.x in the future, this
+  # check will also need to be updated.
+  livecheck do
+    url :head
+    regex(/^v?(0(?:\.\d+)+)$/i)
+  end
+
   bottle do
     sha256 "a2c1673fbed3d331e725694196acf9ea4cd6bc6df3b86568af3e67ee90d70b30" => :catalina
     sha256 "d048d5c58fd1fc3b17c44103b3bbddd445a657415c215916587d9eb8e7f9c2da" => :mojave
