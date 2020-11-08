f3c8639d9f2c96baa3329c4b96caf3f21c7ffe07
FX Coudert <fxcoudert@gmail.com>
2020-11-08 14:55:22 +0100

    xterm: move bottle block (#64349)

--- Formula/xterm.rb
+++ Formula/xterm.rb
@@ -4,13 +4,13 @@ class Xterm < Formula
   url "https://invisible-mirror.net/archives/xterm/xterm-361.tgz"
   mirror "https://deb.debian.org/debian/pool/main/x/xterm/xterm_361.orig.tar.gz"
   sha256 "85610f20d5e47205cc1b6876f7a4da28d6ae051bd8eac0b932e92c37a73a623f"
+  license :cannot_represent
+
   bottle do
     sha256 "88e30a37f2bab99f4de4dc13a09714cd267fde631015260e07736315a769d21a" => :catalina
     sha256 "21a7daa15dabd4b41968010342bbc2ea4a4e97075e42c7a51fe582c2d4331480" => :mojave
     sha256 "7991cdc6a1bca3713f71b76d1751aade257f50f4ca6274adc5126e36d8dc2978" => :high_sierra
   end
-
-  license :cannot_represent
 
   depends_on "fontconfig"
   depends_on "freetype"
