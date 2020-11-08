ebf1b4d80b0f713d0251ffcae6c11a5f5779cdce
Sam Ford <1584702+samford@users.noreply.github.com>
2020-11-08 10:40:46 -0500

    djvu2pdf: add livecheck

--- Formula/djvu2pdf.rb
+++ Formula/djvu2pdf.rb
@@ -4,6 +4,11 @@ class Djvu2pdf < Formula
   url "https://0x2a.at/site/projects/djvu2pdf/djvu2pdf-0.9.2.tar.gz"
   sha256 "afe86237bf4412934d828dfb5d20fe9b584d584ef65b012a893ec853c1e84a6c"
 
+  livecheck do
+    url :homepage
+    regex(/href=.*?djvu2pdf[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
   bottle :unneeded
 
   depends_on "djvulibre"
