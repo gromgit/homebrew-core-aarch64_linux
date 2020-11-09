b6856d4563c0c5d376aca820e24420697cc25130
Alexander Zigelski <ali@crunchtime.dev>
2020-11-09 09:53:05 +0100

    kube-linter 0.1.2 (new formula)
    Closes #64408.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/kube-linter.rb
@@ -0,0 +1,45 @@
+class KubeLinter < Formula
+  desc "Static analysis tool for Kubernetes YAML files and Helm charts"
+  homepage "https://github.com/stackrox/kube-linter"
+  url "https://github.com/stackrox/kube-linter.git",
+    tag:      "0.1.2",
+    revision: "a6df7881ca012d5ea4d88e2e8a73cbe4c382fc13"
+  license "Apache-2.0"
+  head "https://github.com/stackrox/kube-linter.git"
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "build"
+    bin.install ".gobin/kube-linter"
+  end
+
+  test do
+    (testpath/"pod.yaml").write <<~EOS
+      apiVersion: v1
+      kind: Pod
+      metadata:
+        name: homebrew-demo
+      spec:
+        securityContext:
+          runAsUser: 1000
+          runAsGroup: 3000
+          fsGroup: 2000
+        containers:
+        - name: homebrew-test
+          image: busybox
+          resources:
+            limits:
+              memory: "128Mi"
+              cpu: "500m"
+            requests:
+              memory: "64Mi"
+              cpu: "250m"
+          securityContext:
+            readOnlyRootFilesystem: true
+    EOS
+
+    # Lint pod.yaml for default errors
+    assert_match "No lint errors found!", shell_output("#{bin}/kube-linter lint pod.yaml 2>&1").chomp
+  end
+end
