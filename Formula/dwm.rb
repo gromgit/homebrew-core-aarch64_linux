ed6057bd06e48ba10590b525a7050c7a8e244026
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-09 00:36:31 +0000

    dwm: update 6.2_2 bottle.

--- Formula/dwm.rb
+++ Formula/dwm.rb
@@ -8,11 +8,10 @@ class Dwm < Formula
   head "https://git.suckless.org/dwm", using: :git
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "e418a6853e7a7c270ccf6eb2d80c46986404acbd1b0d5a7fca008f111b6aab42" => :catalina
-    sha256 "a749a913684983c05a9998ca73d55bd8f05197fcf6d7573336eacf7e168a5120" => :mojave
-    sha256 "0c5e0fd48f4d52ab748d5301add02b7ca39d773c604e108d2eb81705cf80c315" => :high_sierra
-    sha256 "20707f7d694eef615c63c98c055db728a6922e68ccf287ca63933715781311e6" => :sierra
+    cellar :any
+    sha256 "d872be09d1f5c11c9fb4d34002cc5f4622fbc259691800e1742354573b9effb0" => :catalina
+    sha256 "e4ec85368754c0594847dad5272770a36e69876ed433fdd390d73a7d05c43263" => :mojave
+    sha256 "b22ec01678edc39f1b82837087bb69ac311bce937eb10cb096fc8b1002f97701" => :high_sierra
   end
 
   depends_on "dmenu"
