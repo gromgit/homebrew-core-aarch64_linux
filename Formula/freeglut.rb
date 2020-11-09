e96d9d0a7d6b5bb1d14566a7045d541aa19703f3
Adrian Ho <aho+rover-air61@03s.net>
2020-11-09 19:13:00 +0800

    freeglut: migrate to brewed X11
    Also added license and test.
    
    Closes #64419.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/freeglut.rb
+++ Formula/freeglut.rb
@@ -3,6 +3,8 @@ class Freeglut < Formula
   homepage "https://freeglut.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/freeglut/freeglut/3.2.1/freeglut-3.2.1.tar.gz"
   sha256 "d4000e02102acaf259998c870e25214739d1f16f67f99cb35e4f46841399da68"
+  license "MIT"
+  revision 1
 
   livecheck do
     url :stable
@@ -16,38 +18,33 @@ class Freeglut < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on :x11
+  depends_on "pkg-config" => :test
+  depends_on "libx11"
+  depends_on "libxi"
+  depends_on "libxrandr"
+  depends_on "libxxf86vm"
+  depends_on "mesa"
 
-  patch :DATA
+  resource "init_error_func.c" do
+    url "https://raw.githubusercontent.com/dcnieho/FreeGLUT/c63102d06d09f8a9d4044fd107fbda2034bb30c6/freeglut/freeglut/progs/demos/init_error_func/init_error_func.c"
+    sha256 "74ff9c3f722043fc617807f19d3052440073b1cb5308626c1cefd6798a284613"
+  end
 
   def install
-    inreplace "src/x11/fg_main_x11.c", "CLOCK_MONOTONIC", "UNDEFINED_GIBBERISH" if MacOS.version < :sierra
-    system "cmake", *std_cmake_args, "-DFREEGLUT_BUILD_DEMOS=OFF", "."
+    args = %W[
+      -DFREEGLUT_BUILD_DEMOS=OFF
+      -DOPENGL_INCLUDE_DIR=#{Formula["mesa"].include}
+      -DOPENGL_gl_LIBRARY=#{Formula["mesa"].lib}/#{shared_library("libGL")}
+    ]
+    system "cmake", *std_cmake_args, *args, "."
     system "make", "all"
     system "make", "install"
   end
+
+  test do
+    resource("init_error_func.c").stage(testpath)
+    flags = shell_output("pkg-config --cflags --libs glut gl xext x11").chomp.split
+    system ENV.cc, "init_error_func.c", "-o", "init_error_func", *flags
+    assert_match "Entering user defined error handler", shell_output("./init_error_func 2>&1", 1)
+  end
 end
-
-__END__
-
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 28f8651..d1f6a86 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -258,6 +258,16 @@
- IF(FREEGLUT_GLES)
-     LIST(APPEND PUBLIC_DEFINITIONS -DFREEGLUT_GLES)
-     LIST(APPEND LIBS GLESv2 GLESv1_CM EGL)
-+ELSEIF(APPLE)
-+  # on OSX FindOpenGL uses framework version of OpenGL, but we need X11 version
-+  FIND_PATH(GLX_INCLUDE_DIR GL/glx.h
-+            PATHS /opt/X11/include /usr/X11/include /usr/X11R6/include)
-+  FIND_LIBRARY(OPENGL_gl_LIBRARY GL
-+               PATHS /opt/X11/lib /usr/X11/lib /usr/X11R6/lib)
-+  FIND_LIBRARY(OPENGL_glu_LIBRARY GLU
-+               PATHS /opt/X11/lib /usr/X11/lib /usr/X11R6/lib)
-+  LIST(APPEND LIBS ${OPENGL_gl_LIBRARY})
-+  INCLUDE_DIRECTORIES(${GLX_INCLUDE_DIR})
- ELSE()
-   FIND_PACKAGE(OpenGL REQUIRED)
-   LIST(APPEND LIBS ${OPENGL_gl_LIBRARY})
