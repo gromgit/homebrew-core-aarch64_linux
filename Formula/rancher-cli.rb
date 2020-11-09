06a62ad32ed4362467476d3561f0ab11001ce275
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-09 22:17:07 +0000

    rancher-cli: update 2.4.8 bottle.

--- Formula/rancher-cli.rb
+++ Formula/rancher-cli.rb
@@ -13,9 +13,9 @@ class RancherCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9dcae93136d9f79a102ab1d7acd3da9d7ea51302150dfb63aecbc39a8232825e" => :catalina
-    sha256 "cea838a7d939f24f60f98df61d58206385f944269bfebefac16cc046cf0537e2" => :mojave
-    sha256 "c44ed170e389fe323966e95d25eff6adefdf3e44f3dff3656ec8e5511155bf2c" => :high_sierra
+    sha256 "60e0aaf8dfdd598e252e517dad981638fee13ee09ed84a17db1f73fcc0127c77" => :catalina
+    sha256 "0f2db3a62b98d2284041580c842c5962aca726c59bfab511303b179ecde27470" => :mojave
+    sha256 "89587feedd470f4af2091f29369ab4a87c8b62e2d295ce50286faaee3dcc69f9" => :high_sierra
   end
 
   depends_on "go" => :build
