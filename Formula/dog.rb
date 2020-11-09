bfb9e9c2470a8c8a6f3151e5bb08a18eda65bd91
Dawid Dziurla <dawidd0811@gmail.com>
2020-11-09 08:18:35 +0000

    dog: add 0.1.0 bottle.

--- Formula/dog.rb
+++ Formula/dog.rb
@@ -6,6 +6,13 @@ class Dog < Formula
   license "EUPL-1.2"
   head "https://github.com/ogham/dog.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "21f901c6abb536f0a7bc00d97df6e561aec1acff1759a96a5f69bd51eac1da98" => :catalina
+    sha256 "095b6dbb23b96504a6f0e0a292a7157c78e900825e92f4c140cc29643a294e29" => :mojave
+    sha256 "fef7ea4d925e230ba9e75cb051a888263c2bc10fcf26a96899cea2cc8891a302" => :high_sierra
+  end
+
   depends_on "just" => :build
   depends_on "pandoc" => :build
   depends_on "rust" => :build
