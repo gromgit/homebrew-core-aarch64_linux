ec6d559e1dca1ea6f1c4c42e19da90f0d4103ea7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-10 04:12:50 +0000

    azure-cli: update 2.14.2 bottle.

--- Formula/azure-cli.rb
+++ Formula/azure-cli.rb
@@ -16,9 +16,9 @@ class AzureCli < Formula
 
   bottle do
     cellar :any
-    sha256 "cae69be45f788153abf82424f57aaa8cc5251bb7fbfba8d8eb023024cfd40380" => :catalina
-    sha256 "7745cd117223f05c28a658c3cf237b2bb6539651e19920a249ffa54e3a771505" => :mojave
-    sha256 "9115af3b0d39641284b081e1296426f22dddfbfeacc7fea9e1afda6d6b64198b" => :high_sierra
+    sha256 "dbaeee14b295050ec1d2d0274ce6511ed4927f21c5f0d5d4d7a44d07843bc816" => :catalina
+    sha256 "ce4a31d89d1d31ff527dd98f6d9920656e4a10b7b7a7b13b8e471ef852cb9810" => :mojave
+    sha256 "3c2149d7609015e5efb647ab8c2beec990d4cb6f6588b2c9b5f0054342cb44fb" => :high_sierra
   end
 
   depends_on "openssl@1.1"
