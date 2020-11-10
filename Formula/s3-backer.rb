7522b8c89578b00621a15ce88f1962c140cffb04
Jonathan Chang <me@jonathanchang.org>
2020-11-10 23:52:43 +1100

    s3-backer: deprecate due to FUSE Requirement
    https://github.com/Homebrew/homebrew-core/pull/64491

--- Formula/s3-backer.rb
+++ Formula/s3-backer.rb
@@ -5,6 +5,8 @@ class S3Backer < Formula
   sha256 "deea48205347b24d1298fa16bf3252d9348d0fe81dde9cb20f40071b8de60519"
   license "GPL-2.0-or-later"
 
+  deprecate! because: "requires FUSE"
+
   livecheck do
     url "https://build.opensuse.org/package/view_file/openSUSE:Factory/s3backer/s3backer.spec"
     regex(/Version:\s+v?(\d+(?:\.\d+)+)/i)
