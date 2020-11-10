014e97614434a4778ba43fcfc4f296b54e3c18a0
Rui Chen <rui@meetup.com>
2020-11-10 16:32:42 -0500

    gmt@5: revision for gdal

--- Formula/gmt@5.rb
+++ Formula/gmt@5.rb
@@ -6,7 +6,7 @@ class GmtAT5 < Formula
   mirror "https://fossies.org/linux/misc/GMT/gmt-5.4.5-src.tar.gz"
   sha256 "225629c7869e204d5f9f1a384c4ada43e243f83e1ed28bdca4f7c2896bf39ef6"
   license "LGPL-3.0"
-  revision 6
+  revision 7
 
   bottle do
     sha256 "ccbfdc5293e264dfa68d6bd0bc6bbe84cda41df4b0dae485f44921a918762ff0" => :catalina
