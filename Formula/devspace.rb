a35bbb2d83693fb54fdc7e00eb4e003bafc456fe
chenrui <chenrui333@gmail.com>
2020-11-10 16:32:37 +0000

    devspace 5.2.2
    Closes #64496.
    
    Signed-off-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/devspace.rb
+++ Formula/devspace.rb
@@ -2,8 +2,8 @@ class Devspace < Formula
   desc "CLI helps develop/deploy/debug apps with Docker and k8s"
   homepage "https://devspace.cloud/docs"
   url "https://github.com/devspace-cloud/devspace.git",
-    tag:      "v5.2.1",
-    revision: "e069ac5d031e7c3ef2b9ac3dd8730c78e0145767"
+    tag:      "v5.2.2",
+    revision: "2b69cf7eee778c892129a7b8233ab7ecd6125690"
   license "Apache-2.0"
 
   bottle do
