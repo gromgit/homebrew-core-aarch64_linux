dba8e50ca414bb6431250cfd78c3a11c2bfe5ceb
Sam Ford <1584702+samford@users.noreply.github.com>
2020-11-11 18:14:06 -0500

    Remove livecheck blocks using Google Code Archive

--- Formula/pssh.rb
+++ Formula/pssh.rb
@@ -7,11 +7,6 @@ class Pssh < Formula
   license "BSD-3-Clause"
   revision 3
 
-  livecheck do
-    url "https://www.googleapis.com/download/storage/v1/b/google-code-archive/o/v2%2Fcode.google.com%2Fparallel-ssh%2Fdownloads-page-1.json?&alt=media"
-    regex(/pssh[._-]v?(\d+(?:\.\d+)+)\.t/i)
-  end
-
   bottle do
     cellar :any_skip_relocation
     rebuild 1
