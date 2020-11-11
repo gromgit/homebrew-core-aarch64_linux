7f7af002b04017cb482faab07cf26170314dc004
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-11 17:12:56 +0000

    k6: update 0.29.0 bottle.

--- Formula/k6.rb
+++ Formula/k6.rb
@@ -7,9 +7,9 @@ class K6 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3b7ab6f424658d10e9c08b8981e8bd13661191ad7582a8a8dfa6dc4d4de2377b" => :catalina
-    sha256 "f1ba8bcdf1f06986d8a69dda23771ccd5c10f07a5aedfa94b72c52793ec71b49" => :mojave
-    sha256 "fbcaf23570c3d77669a7d630333a94bba56b82464183661c5ee20b506d979efd" => :high_sierra
+    sha256 "b7bd5708af3593bb66e252e35d37aad25c8f2b86686ed53f67d438cb0da4adc9" => :catalina
+    sha256 "0eb5aa720d0a1bc5b306e7c7e4d767a346b7073f14e018bc866a5e2540cd9423" => :mojave
+    sha256 "037428689ad82115999e98f4f8967f615339e9c9f3e9211d0935ac63a73fbf20" => :high_sierra
   end
 
   depends_on "go" => :build
