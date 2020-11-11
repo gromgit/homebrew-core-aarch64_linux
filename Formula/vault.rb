0b2993f5659b06e5800c566ff383e7417bd0146f
chenrui <chenrui333@gmail.com>
2020-11-11 16:28:05 +0000

    vault 1.6.0
    Closes #64580.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -5,8 +5,8 @@ class Vault < Formula
   desc "Secures, stores, and tightly controls access to secrets"
   homepage "https://vaultproject.io/"
   url "https://github.com/hashicorp/vault.git",
-      tag:      "v1.5.5",
-      revision: "f5d1ddb3750e7c28e25036e1ef26a4c02379fc01"
+      tag:      "v1.6.0",
+      revision: "7ce0bd9691998e0443bc77e98b1e2a4ab1e965d4"
   license "MPL-2.0"
   head "https://github.com/hashicorp/vault.git"
 
