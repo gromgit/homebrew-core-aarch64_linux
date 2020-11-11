e6924a0b1e51dc464335ec800563d4c4ce153726
Sam Ford <1584702+samford@users.noreply.github.com>
2020-11-11 16:27:52 -0500

    psutils: add livecheck

--- Formula/psutils.rb
+++ Formula/psutils.rb
@@ -7,6 +7,17 @@ class Psutils < Formula
   sha256 "3853eb79584ba8fbe27a815425b65a9f7f15b258e0d43a05a856bdb75d588ae4"
   license "psutils"
 
+  # This regex is open-ended (i.e., it doesn't contain a trailing delimiter like
+  # `\.t`), since the homepage only links to an unversioned archive file
+  # (`psutils.tar.gz`) or a versioned archive file with additional trailing text
+  # (`psutils-p17-a4-nt.zip`). Relying on the trailing text of the versioned
+  # archive file remaining the same could make this check liable to break, so
+  # we'll simply leave it looser until/unless it causes a problem.
+  livecheck do
+    url :homepage
+    regex(/href=.*?psutils[._-](p\d+)/i)
+  end
+
   bottle do
     cellar :any_skip_relocation
     rebuild 1
