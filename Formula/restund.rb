540bd1619430270b57902643e98ece49f609dde7
Sam Ford <1584702+samford@users.noreply.github.com>
2020-11-11 14:21:28 -0500

    restund: update livecheck url

--- Formula/restund.rb
+++ Formula/restund.rb
@@ -5,7 +5,7 @@ class Restund < Formula
   sha256 "3170441dc882352ab0275556b6fc889b38b14203d936071b5fa12f39a5c86d47"
 
   livecheck do
-    url "https://web.archive.org/web/20200425105956/www.creytiv.com/pub/"
+    url "https://sources.openwrt.org/"
     regex(/href=.*?restund[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
