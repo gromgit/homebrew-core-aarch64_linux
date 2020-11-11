4c79975055e7b15d2c3b14dce2df636a973ad619
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-11 16:19:14 +0000

    rpg: update 0.3.0 bottle.

--- Formula/rpg.rb
+++ Formula/rpg.rb
@@ -10,13 +10,10 @@ class Rpg < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "826bacff2dddeeeb41cdf328b7702fa415d049b9d1d55b2f93e7f1084ebcb3e0" => :catalina
-    sha256 "995850411f4a47126309c849e2da32c76abaddf40317515d7af5b8d2e2d6d680" => :mojave
-    sha256 "27b65a242fd862d805460bc06c91d7f164935dd14f8ded5cfa62924a1b973a42" => :high_sierra
-    sha256 "ddb4f887b798101d95300f0b0c22a0760d06158c09796e9bfb65869f28b69434" => :sierra
-    sha256 "be6550c915e361dcc0e020048f9a5f4ea37f1c5890a526282a3d70c610594981" => :el_capitan
-    sha256 "5c1af29955697dcd46ff58fd70f9aca986b977f3cc17f638822c81289f180df2" => :yosemite
-    sha256 "2ebf1a744c3c072c107883f565c04154b3e530c93868bb438bb90a1be35a4cb7" => :mavericks
+    rebuild 1
+    sha256 "d1d4031e3e641523f759f3c2dc69fed0cffdfa537639c1f9686ddc7763b93df9" => :catalina
+    sha256 "fab3d032e629a4d20add14e9693919a074286990a16eb6fa8772180fc60730ee" => :mojave
+    sha256 "f1c7e5d997a1f0ceb1cca6b1067408912ff8e14522fb411530649f0689f9d042" => :high_sierra
   end
 
   def install
