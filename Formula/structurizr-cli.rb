d76d088d012cc8cc50d73ac7b3f43e6c387e1651
Gilberto Taccari <gilbertotcc@users.noreply.github.com>
2020-11-12 23:23:55 +0100

    structurizr-cli 1.5.0 (#64669)

--- Formula/structurizr-cli.rb
+++ Formula/structurizr-cli.rb
@@ -1,8 +1,8 @@
 class StructurizrCli < Formula
   desc "Command-line utility for Structurizr"
   homepage "https://structurizr.com"
-  url "https://github.com/structurizr/cli/releases/download/v1.4.5/structurizr-cli-1.4.5.zip"
-  sha256 "67135390e1c14286300d6d62985ebf4739b01f3a03da7dfe129f3f25f4c7eb15"
+  url "https://github.com/structurizr/cli/releases/download/v1.5.0/structurizr-cli-1.5.0.zip"
+  sha256 "3f40e9f61c0753b2911d169011deb2c889f0e3945289163b9ff60243ec1e83cf"
   license "Apache-2.0"
 
   bottle :unneeded
