a18314e0a9a037d1a27bf390adec915508e53707
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-12 03:13:00 +0000

    wmctrl: update 1.07_2 bottle.

--- Formula/wmctrl.rb
+++ Formula/wmctrl.rb
@@ -13,10 +13,9 @@ class Wmctrl < Formula
 
   bottle do
     cellar :any
-    sha256 "7fffdc3399b2af15b638dfb642e44e0c148df088828f307c1f9440e38049cd5a" => :catalina
-    sha256 "2223922cda28d81580d85d01fa697284102d10226df76d57660bd92093fd46c5" => :mojave
-    sha256 "59b55236fd42a64f6ccc8587a5580a25afd330a137b62e7258568042e8b1b525" => :high_sierra
-    sha256 "cbdb379f9b1264847f74c0ea01e0a5412645b442d3c069708bfbe209b845b873" => :sierra
+    sha256 "d585a38070e3343da1be66819f7d3f840140acee8dde1d3912542d682466ee48" => :catalina
+    sha256 "49f4d10d0e8d8b4cfa2e5ba4240f5c623f01b66d4e466eace255c1496c627da5" => :mojave
+    sha256 "10200373a514341920fd453d769c07040eae2ba01a691c418d10b6a1d44ec70b" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
