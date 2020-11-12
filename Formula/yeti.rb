7b1b4cb4b389f958290be1d9020f0dd11b25675e
Jonathan Chang <me@jonathanchang.org>
2020-11-13 01:29:29 +1100

    yeti: deprecate java requirement
    These no longer work properly on Big Sur and will be
    deprecated in a future version of Homebrew.
    
    https://github.com/Homebrew/homebrew-core/pull/64662#issuecomment-731116763

--- Formula/yeti.rb
+++ Formula/yeti.rb
@@ -4,6 +4,7 @@ class Yeti < Formula
   url "https://github.com/mth/yeti/archive/v1.0.tar.gz"
   sha256 "f1451a7c58cecaee41c46e886eb714a81e0dfe5557c10568421dcbd33ab9357c"
   license "BSD-3-Clause"
+  revision 1
   head "https://github.com/mth/yeti.git"
 
   bottle do
@@ -12,6 +13,8 @@ class Yeti < Formula
     sha256 "9dffa6798409e7d40acf301547dc8508547331922eaa1b7365a0b04e020ae90f" => :mojave
     sha256 "1c49573337d0ca872a060038e3c7e5496d02b025e442c062314d98a786ab708a" => :high_sierra
   end
+
+  deprecate! because: :does_not_build
 
   depends_on "ant" => :build
   depends_on java: "1.8"
