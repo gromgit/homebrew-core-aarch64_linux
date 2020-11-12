182f60678c43c7084a25bb958b89aa47aa85d34d
Jonathan Chang <me@jonathanchang.org>
2020-11-13 01:29:26 +1100

    groovyserv: deprecate :java Requirements
    These no longer work properly on Big Sur and will be
    deprecated in a future version of Homebrew.
    
    https://github.com/Homebrew/homebrew-core/pull/64662#issuecomment-731116763

--- Formula/groovyserv.rb
+++ Formula/groovyserv.rb
@@ -17,6 +17,8 @@ class Groovyserv < Formula
     sha256 "51aef6e15608021ae127aaa93e2aa39bfaf52cfea688b45841d315b6a04b55aa" => :el_capitan
   end
 
+  deprecate! because: :does_not_build
+
   depends_on "go" => :build
   depends_on "groovy"
   depends_on java: "1.8"
