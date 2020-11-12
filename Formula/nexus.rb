932471e6e5176a3c1d2375c0b12975563faa9425
Sean Molenaar <smillerdev@me.com>
2020-11-12 14:17:55 +0100

    nexus 3.28.1
    Closes #64657.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

--- Formula/nexus.rb
+++ Formula/nexus.rb
@@ -1,9 +1,8 @@
 class Nexus < Formula
   desc "Repository manager for binary software components"
   homepage "https://www.sonatype.org/"
-  url "https://sonatype-download.global.ssl.fastly.net/repository/downloads-prod-group/oss/nexus-2.14.19-01-bundle.tar.gz"
-  version "2.14.19-01"
-  sha256 "5bf321e40b219756441fbdb9fa0d270493a25640af67076111ccd151ab007b7e"
+  url "https://github.com/sonatype/nexus-public/archive/release-3.28.1-01.tar.gz"
+  sha256 "2b17b3fee3fd81b299bf7d5d818d248a1c82554580aa51210c78f89f2a5b2c37"
   license "EPL-1.0"
 
   livecheck do
@@ -11,16 +10,34 @@ class Nexus < Formula
     regex(/href=.*?nexus[._-]v?(\d+(?:\.\d+)+(?:-\d+)?)(?:-bundle)?\.t/i)
   end
 
-  bottle :unneeded
+  depends_on "maven" => :build
+  depends_on "openjdk@8"
+
+  uses_from_macos "unzip" => :build
 
   def install
-    rm_f Dir["bin/*.bat"]
-    # Put the sonatype-work directory in the var directory, to persist across version updates
-    inreplace "nexus-#{version}/conf/nexus.properties",
-      "nexus-work=${bundleBasedir}/../sonatype-work/nexus",
-      "nexus-work=#{var}/nexus"
-    libexec.install Dir["nexus-#{version}/*"]
-    bin.install_symlink libexec/"bin/nexus"
+    ENV["JAVA_HOME"] = Formula["openjdk@8"].opt_prefix
+    system "mvn", "install", "-DskipTests"
+    system "unzip", "-o", "-d", "target", "assemblies/nexus-base-template/target/nexus-base-template-#{version}.zip"
+
+    rm_f Dir["target/nexus-base-template-#{version}/bin/*.bat"]
+    rm_f "target/nexus-base-template-#{version}/bin/contrib"
+    libexec.install Dir["target/nexus-base-template-#{version}/*"]
+
+    env = {
+      JAVA_HOME:  Formula["openjdk@8"].opt_prefix,
+      KARAF_DATA: "${NEXUS_KARAF_DATA:-#{var}/nexus}",
+      KARAF_LOG:  "#{var}/log/nexus",
+      KARAF_ETC:  "#{etc}/nexus",
+    }
+
+    (bin/"nexus").write_env_script libexec/"bin/nexus", env
+  end
+
+  def post_install
+    mkdir_p "#{var}/log/nexus" unless (var/"log/nexus").exist?
+    mkdir_p "#{var}/nexus" unless (var/"nexus").exist?
+    mkdir "#{etc}/nexus" unless (etc/"nexus").exist?
   end
 
   plist_options manual: "nexus start"
@@ -46,7 +63,12 @@ class Nexus < Formula
   end
 
   test do
-    output = `#{bin}/nexus status`
-    assert_match "Nexus OSS is", output
+    mkdir "data"
+    fork do
+      ENV["NEXUS_KARAF_DATA"] = testpath/"data"
+      exec "#{bin}/nexus", "server"
+    end
+    sleep 60
+    assert_match "<title>Nexus Repository Manager</title>", shell_output("curl --silent --fail http://localhost:8081")
   end
 end
