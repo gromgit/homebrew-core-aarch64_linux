37f069e8815e8ec90a7f433473a829ee320a230d
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-11-12 08:32:53 +0000

    flux: add 0.94.0 bottle.

--- Formula/flux.rb
+++ Formula/flux.rb
@@ -12,6 +12,13 @@ class Flux < Formula
     regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
   end
 
+  bottle do
+    cellar :any
+    sha256 "b1b981c9c6d0189d3c4fc93795077dfb04ee8349b33b19929f76c79993f373c0" => :catalina
+    sha256 "bad9cd01d1a588e2a8de1ddbdb85b152d98645232781f3bfcf718c6b2282f48d" => :mojave
+    sha256 "94d271d3a87ae8086197e5945488be7b8684a3fefc57a3ead34e26253b05fede" => :high_sierra
+  end
+
   depends_on "go" => :build
   depends_on "rust" => :build
 
