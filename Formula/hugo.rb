33d55cf5b934fe6f1584310960252c14e4674446
chenrui <chenrui333@gmail.com>
2020-11-13 10:21:15 +0000

    hugo 0.78.2
    Closes #64712.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hugo.rb
+++ Formula/hugo.rb
@@ -1,8 +1,8 @@
 class Hugo < Formula
   desc "Configurable static site generator"
   homepage "https://gohugo.io/"
-  url "https://github.com/gohugoio/hugo/archive/v0.78.1.tar.gz"
-  sha256 "607502a8678fc7134f3df1ce2978f31e407ac0cb1d6b61e46cdcc9883ce12af8"
+  url "https://github.com/gohugoio/hugo/archive/v0.78.2.tar.gz"
+  sha256 "bbfacf3bf379911bc348e6300912838fd07a75ad6de6d41082c54226e0055029"
   license "Apache-2.0"
   head "https://github.com/gohugoio/hugo.git"
 
