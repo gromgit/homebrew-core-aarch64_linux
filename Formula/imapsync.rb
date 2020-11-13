27dbb3995e9f4f2e13a0ece1b6bc1082fd20e387
Rylan Polster <rslpolster@gmail.com>
2020-11-13 18:05:39 -0500

    imapsync: fix note comment style

--- Formula/imapsync.rb
+++ Formula/imapsync.rb
@@ -2,7 +2,7 @@ class Imapsync < Formula
   desc "Migrate or backup IMAP mail accounts"
   homepage "https://imapsync.lamiral.info/"
   url "https://imapsync.lamiral.info/dist2/imapsync-1.977.tgz"
-  # Note the mirror will return 404 until the version becomes outdated.
+  # NOTE: The mirror will return 404 until the version becomes outdated.
   sha256 "1ce601150568a6b13a5d8730bee07fdc05b35f3f4e35775f1b471ba221940c2a"
   license "NLPL"
   head "https://github.com/imapsync/imapsync.git"
