ca6c4c51a9ac802595160f1f3b4ab4eb4f033399
Jonathan Chang <me@jonathanchang.org>
2020-11-14 10:35:36 +0000

    gdbm: update 1.18.1_1 bottle.

--- Formula/gdbm.rb
+++ Formula/gdbm.rb
@@ -13,6 +13,7 @@ class Gdbm < Formula
 
   bottle do
     cellar :any
+    sha256 "36b492f1b0910367dd394cbdcffe1606f64ab41ec6701210becfb591a8557dee" => :big_sur
     sha256 "f7b5ab7363961fa6defcb66b4ffdf5365264fcb97d35bc413e754f173a3b1912" => :catalina
     sha256 "0f65874bcd50d31aaf8b2e6c8ef414cb65a8d8b9eb6d1fa4ef179c6e0a94983c" => :mojave
     sha256 "4a644af2fcc2781c3a161209deff7b62d760058bc1bac7c4f91a5ce5738f0798" => :high_sierra
