dcc4f55557fb01a942e3290ef90bc61906195be9
Jonathan Chang <me@jonathanchang.org>
2020-11-14 17:13:45 +0000

    shadowenv: update 2.0.3 bottle.

--- Formula/shadowenv.rb
+++ Formula/shadowenv.rb
@@ -7,6 +7,7 @@ class Shadowenv < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "43ef903f8c706b1aaa787a28a101bb488f80fecfcf5207d56a55205464cd3ca3" => :big_sur
     sha256 "a8e73028069e37003f2e019230e48947913b164ebb9cdb50ea088b6398fc352c" => :catalina
     sha256 "5022c65cf81ba99f8be31fdd879201e566f203f357e5ca88d14daaa0d9a83325" => :mojave
     sha256 "fe0a543dc0a0ce7adba2eebc9f8f224aa55a4f63b49a4474d576d6467d604065" => :high_sierra
