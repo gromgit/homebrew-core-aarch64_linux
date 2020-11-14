3c193a1bc2cff9df4fbda20dc86aea22ffedb2a4
FX Coudert <fxcoudert@gmail.com>
2020-11-14 18:37:43 +0000

    graphene: update 1.10.2 bottle.

--- Formula/graphene.rb
+++ Formula/graphene.rb
@@ -10,6 +10,7 @@ class Graphene < Formula
 
   bottle do
     cellar :any
+    sha256 "0e6af428f089a813408d7edc7a5196372a1ee8dd3ed13f7edbf4acfb1f62407a" => :big_sur
     sha256 "d8519d2811ee796969121cd0b087fb7a5e96c2952c69bb2dfe206f3efc299e31" => :catalina
     sha256 "0e7e238034cfcd390b8bd0e49bd6ba3c23dcdbff7c9a1cea5e626b34a3381acb" => :mojave
     sha256 "7bcd5ab83e1509ddeec6b710ba2a69c99bb92438344694423b1c886346fd44c2" => :high_sierra
