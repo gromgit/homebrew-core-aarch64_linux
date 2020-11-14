2d4031ffb31b9bbac016e1248e27858078f7b986
Jonathan Chang <me@jonathanchang.org>
2020-11-14 15:39:52 +0000

    dotenv-linter: update 2.2.1 bottle.

--- Formula/dotenv-linter.rb
+++ Formula/dotenv-linter.rb
@@ -8,6 +8,7 @@ class DotenvLinter < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ced7a6252dfccabeef5bebe57d826ff2d47ad169fe675769194ff8a670a74261" => :big_sur
     sha256 "ea3cd8fbb12d6c5aaa962d20a07f6a3aabb418cf6142a9fff057fecbf63c02cd" => :catalina
     sha256 "6dc5965ef3f36811e133c28636dec3b06eed99782fbfc980e83d775722a979e0" => :mojave
     sha256 "8bdecac347a74490c4972a31a4c713cfb438e5531b5dc626fea42d495ad932c3" => :high_sierra
