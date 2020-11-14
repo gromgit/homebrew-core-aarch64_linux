850fe2d91353836d5be90f6dbd0f91993af5b26d
FX Coudert <fxcoudert@gmail.com>
2020-11-14 20:33:39 +0000

    pkcs11-helper: update 1.26.0 bottle.

--- Formula/pkcs11-helper.rb
+++ Formula/pkcs11-helper.rb
@@ -14,6 +14,7 @@ class Pkcs11Helper < Formula
 
   bottle do
     cellar :any
+    sha256 "b602c8e201fd5f6a76315f8e05f83b8a3677b7a77cefc48d0643af4b5da0fab8" => :big_sur
     sha256 "dc52806291729e3481bb2a90cabb9df77f21c30351ed2cc67213caaefed8c667" => :catalina
     sha256 "f2783434ebb595a4e9a9b570c05dcf030a7539d1008e988427e4cd36281b6917" => :mojave
     sha256 "6796ffb18fc1f6c22b800caf32e0e4698c1f82e10529ed6535b634afabf237fe" => :high_sierra
