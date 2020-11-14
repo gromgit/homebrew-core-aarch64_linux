41959bdd710b8805e25e8965a8c9343b20a9797f
Jonathan Chang <me@jonathanchang.org>
2020-11-14 15:55:43 +0000

    fselect: update 0.7.1 bottle.

--- Formula/fselect.rb
+++ Formula/fselect.rb
@@ -7,6 +7,7 @@ class Fselect < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a59d8f537da54a5b7830e48eb42b1911973820a5a86555d76e4a9760d79f9684" => :big_sur
     sha256 "7da995ae54157a3ecdaa8d5799a1a6b2356c6e8cf3ba2291ba29502c1d422053" => :catalina
     sha256 "ec6594a93ea9d8034833603fd78ba3dfbc2b6095018ea8f314b383c566f73534" => :mojave
     sha256 "d9fd1eb3acdda1c204adc4c7c2424ec0cc53e9ce0b516a92506ca04fe2ab21cc" => :high_sierra
