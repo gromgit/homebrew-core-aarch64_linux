6583d53c2071f8ac0d4253e1da5ab07fb9acdbc8
Jonathan Chang <me@jonathanchang.org>
2020-11-14 16:40:12 +0000

    mdbook: update 0.4.4 bottle.

--- Formula/mdbook.rb
+++ Formula/mdbook.rb
@@ -8,6 +8,7 @@ class Mdbook < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2a6d98eb7767042009250b84490dea3582bd7a811dc2374d616ce602c826eafb" => :big_sur
     sha256 "59595db021b9ae3e8a2137f7c9deb740508cf110a3822f49715721d571870b1d" => :catalina
     sha256 "12bd9ff40b7421497209163efb1e802dd3ca08e5b84fe5f5c7446eae0366d291" => :mojave
     sha256 "0671b254178a018b1d5ce8e2d1a880886904ff93caebe3d0867a3a5423e35f6f" => :high_sierra
