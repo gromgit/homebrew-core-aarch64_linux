07f12324292fa898decfcbe58f3056d4ab4b57cd
Jonathan Chang <me@jonathanchang.org>
2020-11-14 11:43:16 +0000

    cairo: update 1.16.0_3 bottle.

--- Formula/cairo.rb
+++ Formula/cairo.rb
@@ -12,6 +12,7 @@ class Cairo < Formula
   end
 
   bottle do
+    sha256 "42b20f0db683c4fe7a0d5ffbb605388e67d21abfba3c6578443f74d9839f80bb" => :big_sur
     sha256 "6a23a68837269a8410a54950fdc8883feda091f221118370f1bfd3adbf5ee89c" => :catalina
     sha256 "0984045234fb22fa3e54a337137e9e43a1bf997f5d77692ed02249dfdee2b1bf" => :mojave
     sha256 "5c383ad4625fb1bd15e44e99fba1201490fa478b26178abaca5abb0fdb51510e" => :high_sierra
