4350c302f46fcaf4f16a2b4123218ec86e6dd045
FX Coudert <fxcoudert@gmail.com>
2020-11-14 18:28:57 +0000

    libusb-compat: update 0.1.5_1 bottle.

--- Formula/libusb-compat.rb
+++ Formula/libusb-compat.rb
@@ -12,6 +12,7 @@ class LibusbCompat < Formula
 
   bottle do
     cellar :any
+    sha256 "75c7921c73433f1481dfc4aeed5e63dac1a75d67ed13409ed1c2ec053864dadc" => :big_sur
     sha256 "e1f03f77caed5418c50c3c9659e6c56f2363eddfd6cc1aac3f8dcac9451771d2" => :catalina
     sha256 "11fe66aff70c0177a186c946624f91417565c43bbdc9e7c51725e26ea0c868c5" => :mojave
     sha256 "fccc08c6c3ff2bf93d2aa8e7cc18f30c1fb95fbca044ecaa42d45f7c73a8facf" => :high_sierra
