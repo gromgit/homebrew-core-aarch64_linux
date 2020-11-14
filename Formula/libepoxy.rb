6774b13f5d35418c0cc00d4909fb2170881d5b4b
FX Coudert <fxcoudert@gmail.com>
2020-11-14 14:17:45 +0000

    libepoxy: update 1.5.4_1 bottle.

--- Formula/libepoxy.rb
+++ Formula/libepoxy.rb
@@ -15,6 +15,7 @@ class Libepoxy < Formula
 
   bottle do
     cellar :any
+    sha256 "73fb58d2b0cf8a1283a0ff7216cb669f306b95f15721a61142831a5fa31094e5" => :big_sur
     sha256 "9f58a2eab6aafcc95ade6893bde8d878ab422284353e22c11d04c3a6f3a1e7cb" => :catalina
     sha256 "e42a0410e6f94fa419f785c5b0901eea1506242b1729f97b672f25b463ce3d4e" => :mojave
     sha256 "95cbc3ce1fc94931e0259f9e55a25d9dcacacd70713ae3e59cba28f3d7ff2a3a" => :high_sierra
