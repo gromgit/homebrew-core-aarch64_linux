9227838d16d08f2df9c02906bd0020ac65ee2f05
FX Coudert <fxcoudert@gmail.com>
2020-11-14 14:33:35 +0000

    direnv: update 2.23.1 bottle.

--- Formula/direnv.rb
+++ Formula/direnv.rb
@@ -8,6 +8,7 @@ class Direnv < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6e43a0eeaa3240d5ad0a8dc4de6e1869752d8224c149c1cab54fba6aca9aa3fe" => :big_sur
     sha256 "4cc10d8fad64eb6d5f9a0eee257c77c7d812a0a6ea6d53439e001bf023ac1bf1" => :catalina
     sha256 "1307aa1a9a14aa8e4b04abc444d2d36c199c4ff8466d2ef3fba115b61e12ef21" => :mojave
     sha256 "d7cc71241d1e4656598051536dfb30f0d2ef18bf9134e3e899ff3ada3888cd6a" => :high_sierra
