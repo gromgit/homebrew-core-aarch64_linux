e157f47323c25644e95d59d5fc775cf17cc6e694
FX Coudert <fxcoudert@gmail.com>
2020-11-14 15:08:35 +0000

    binutils: update 2.35.1 bottle.

--- Formula/binutils.rb
+++ Formula/binutils.rb
@@ -11,6 +11,7 @@ class Binutils < Formula
   end
 
   bottle do
+    sha256 "939b10a1c2ffd8c0404827adcb53d8581e3187b418a3f460c7b8ca80083c0265" => :big_sur
     sha256 "f5b1ef7f5209ccceee53625f240fe85ffa42661d6d7a1058d879d544c3d2076b" => :catalina
     sha256 "12d25883cb8fd258343baba320ad465da3ce929c988b9950545e8b4d1a403a7d" => :mojave
     sha256 "56e4eba418acac34302d7b46bc04dbf3c64ddffa2a2830cd5f6ad12911e33294" => :high_sierra
