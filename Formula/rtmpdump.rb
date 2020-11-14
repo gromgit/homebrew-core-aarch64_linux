88e3e9907ef95a713595dd07645a81085e00d6f1
Jonathan Chang <me@jonathanchang.org>
2020-11-14 11:24:44 +0000

    rtmpdump: update 2.4+20151223_1 bottle.

--- Formula/rtmpdump.rb
+++ Formula/rtmpdump.rb
@@ -15,6 +15,7 @@ class Rtmpdump < Formula
 
   bottle do
     cellar :any
+    sha256 "b9e42bf8023a8634a741402f7f902bbd0083e663b2e0d36d3e70dec657f1dd07" => :big_sur
     sha256 "f39d714005d28ed61728832877433a68dd256796bc225bac68b505b2c1d97ef4" => :catalina
     sha256 "97cf25d61d474c2115f6448940f924324d630b60776396398662b1368b4544da" => :mojave
     sha256 "7e95dc18fc03a6c1f19385e1507448f23e2e570c9b3ad60bd3fbc05c65295fb8" => :high_sierra
