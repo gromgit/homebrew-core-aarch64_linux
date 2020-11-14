9a17e03c8de43040379f79169bd14c2c87f8b99d
FX Coudert <fxcoudert@gmail.com>
2020-11-14 19:04:49 +0000

    orc: update 0.4.32 bottle.

--- Formula/orc.rb
+++ Formula/orc.rb
@@ -11,6 +11,7 @@ class Orc < Formula
 
   bottle do
     cellar :any
+    sha256 "7c5d9d6caf789828006ff7d604eb81a247c8d9ff6004c524978452a252dd8162" => :big_sur
     sha256 "df8ed6c8e2d13c9425174f38a6ff60d291cc3febababa670ebe74b21fe56ceab" => :catalina
     sha256 "8b11d82f1c1e51fe41d4b7cf1a905f72bdb977ce2acc0eafcf2ba6b89f06d58c" => :mojave
     sha256 "d347537787016fa19c7653a9912e0c11d134f10e96a75bc0f5bb28e221e012d8" => :high_sierra
