d2bff0a9e0d1449054ec8ad314d2e1fe6e2c0a65
Jonathan Chang <me@jonathanchang.org>
2020-11-14 15:33:18 +0000

    broot: update 1.0.5 bottle.

--- Formula/broot.rb
+++ Formula/broot.rb
@@ -8,6 +8,7 @@ class Broot < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "423fd83b04200bd91ac0558c48bf34d543333ed0b19da76215ce77edaea34923" => :big_sur
     sha256 "e0e104254d8f4e6245ccec6c87552a3b3960c6c5f81db0253195bea168636658" => :catalina
     sha256 "e7af6b7a1cfaeefe827c486669f1ee6339c54e239423b06debe9d55bbd212104" => :mojave
     sha256 "18d1f77fa3178531237c063daddde839281c2eabcc007ccea0c13ec7bc948e6b" => :high_sierra
