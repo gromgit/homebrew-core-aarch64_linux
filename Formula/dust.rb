27a28d60b6ec31e519d809dd252c2ba8f3bd4b26
Jonathan Chang <me@jonathanchang.org>
2020-11-14 15:43:38 +0000

    dust: update 0.5.3 bottle.

--- Formula/dust.rb
+++ Formula/dust.rb
@@ -13,6 +13,7 @@ class Dust < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "bfcdb4a3fe8417d4bb88d1b0945b002e818271a8b1f5e7ac3a9502763c0e4157" => :big_sur
     sha256 "693714f8f0d1207b8e54357f924e3a1242dd54d93f21de76b669217812065711" => :catalina
     sha256 "4c5c1cda666e8d405cf39ae79b3327dd8581555c18417b0834c2d334ad4568ed" => :mojave
     sha256 "52750a57ee2e58c7b95661b61cb0d855ba653946f6f21881ed041f2ce2cea747" => :high_sierra
