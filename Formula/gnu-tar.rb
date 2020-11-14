2cfd69f8a3d6ba440ab57fefc6536cb7067485b6
Jonathan Chang <me@jonathanchang.org>
2020-11-14 10:43:15 +0000

    gnu-tar: update 1.32 bottle.

--- Formula/gnu-tar.rb
+++ Formula/gnu-tar.rb
@@ -13,6 +13,7 @@ class GnuTar < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "952e5c4dd04ddab0c2678edf97f24c02f6dfde9f5d031a98fc269064197a0872" => :big_sur
     sha256 "158cb67ea9e02435d671013b4d0d7369822758d9f7ff400ce2512a03f2f7f4e4" => :catalina
     sha256 "1034894e78bb22b0fcf0c8114666d4dc3eb82345a5ca83797ca3bda367d998ac" => :mojave
     sha256 "3771cead286229786d9d92a7697bc6e0de576ec9cae1f881017884ceb3e24f17" => :high_sierra
