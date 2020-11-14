28c0d3a0879672225fcfe4b13cea2e90e190809c
FX Coudert <fxcoudert@gmail.com>
2020-11-14 12:36:32 +0000

    wget: update 1.20.3_2 bottle.

--- Formula/wget.rb
+++ Formula/wget.rb
@@ -11,6 +11,7 @@ class Wget < Formula
   end
 
   bottle do
+    sha256 "c965fd423db73afdcce5ccde8af2783b5659ec2287bf02ae6a982fd6dcbd6292" => :big_sur
     sha256 "ef65c759c5097a36323fa9c77756468649e8d1980a3a4e05695c05e39568967c" => :catalina
     sha256 "28f4090610946a4eb207df102d841de23ced0d06ba31cb79e040d883906dcd4f" => :mojave
     sha256 "91dd0caca9bd3f38c439d5a7b6f68440c4274945615fae035ff0a369264b8a2f" => :high_sierra
