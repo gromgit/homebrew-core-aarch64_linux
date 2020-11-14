eaa20619f5ca61c58edfc82f9bae24f596aebb07
Jonathan Chang <me@jonathanchang.org>
2020-11-14 16:24:20 +0000

    websocat: update 1.6.0 bottle.

--- Formula/websocat.rb
+++ Formula/websocat.rb
@@ -12,6 +12,7 @@ class Websocat < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0eb4c8f2100c0d41bd382ff9e4c8bdee1df19e5dbb60053dce90a24339466fec" => :big_sur
     sha256 "b9ec3480735006f0603df6a0f4ba123f3bf807f8b2c731070975263088b6cc8f" => :catalina
     sha256 "cfb99f76c48c5ef48ee89118012a75a8b78c1f3602db084407e3fd4e7f0922eb" => :mojave
     sha256 "c865fed700599d7619e91ecb815b2672b9a1d9da6b4289383cd5455d82d79743" => :high_sierra
