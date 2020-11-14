a1e2f9abc36d7d738e6fd20c1c50ad6cd5720294
Jonathan Chang <me@jonathanchang.org>
2020-11-14 09:52:25 +0000

    nethack: update 3.6.6 bottle.

--- Formula/nethack.rb
+++ Formula/nethack.rb
@@ -15,6 +15,7 @@ class Nethack < Formula
   end
 
   bottle do
+    sha256 "d4db0fc0aa73dd80ef2e32a521db2f67d597606217713dd5644adec265a7cab5" => :big_sur
     sha256 "69418bfcba43b656118140a7e50992772567c4c2ab4827ce0af343892a149945" => :catalina
     sha256 "4d186d190dcab9cc719a3868aa73a6c311407f8c1510e1d3bfd185a8070177bc" => :mojave
     sha256 "6b6b5eb3571c69d31ac0c88f42acae3cea5f42ec513bafd03960db8c9f994177" => :high_sierra
