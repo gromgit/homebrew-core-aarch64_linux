0fad15832cdf970cca68f331b127d6e82651f2f4
FX Coudert <fxcoudert@gmail.com>
2020-11-14 23:41:05 +0000

    fswatch: update 1.15.0 bottle.

--- Formula/fswatch.rb
+++ Formula/fswatch.rb
@@ -7,6 +7,7 @@ class Fswatch < Formula
 
   bottle do
     cellar :any
+    sha256 "f0e4988d417dc53f21f03a82358900a31be9f2962b067bddc49c9d786189d5e4" => :big_sur
     sha256 "77233b7d6c11644f14682862d613ed37a5eda86ba1ec5a6ea3c18b75ccafe906" => :catalina
     sha256 "2602ae4b5b43cb63ec6a249bdf8b1e81b124ae18392f01a2a8ebf0faec1ac5ff" => :mojave
     sha256 "057894d713ee24ea5e64c0db014e1593cee8eb253bbb962c68a6426a25f39c9e" => :high_sierra
