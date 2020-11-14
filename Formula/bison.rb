609454ba79376980c67dc22833f04fee7122ab7e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-14 19:10:38 +0000

    bison: update 3.7.4 bottle.

--- Formula/bison.rb
+++ Formula/bison.rb
@@ -13,10 +13,9 @@ class Bison < Formula
   end
 
   bottle do
-    sha256 "526bbc72b3a70297ffba6cc557cec656aedccd32416ab508b05591c27914c9bb" => :big_sur
-    sha256 "f5bfd5e40ae8b0501e398e2ea29f95e8b80bdbabab2daedcb40c19cf5c4e3491" => :catalina
-    sha256 "8a99fb499af4af8e49d93e8eae8b48e26a4c30400a9d7cc66e70ea0621b47e7e" => :mojave
-    sha256 "20c775058c2bc15b2f7ccf27cf3b2817a98ea13cf656fa69f1877af2ea62cf04" => :high_sierra
+    sha256 "6252edf4d591cf1de3e94e9e08c5986205593da971d0b381a411d272c95cf30f" => :catalina
+    sha256 "50a80771867419e621404f94ff89cab629fd1aff69bdb68a473fd41a233d00a2" => :mojave
+    sha256 "b0015c5773c569e7de673bc2159f0c86d50422bc4fcf06de66f6bac1fe1f06c5" => :high_sierra
   end
 
   keg_only :provided_by_macos
