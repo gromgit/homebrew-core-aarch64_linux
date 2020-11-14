5e35487388aaca1422de3bae76897668389c1ae4
Jonathan Chang <me@jonathanchang.org>
2020-11-14 10:36:54 +0000

    sqlite: update 3.33.0 bottle.

--- Formula/sqlite.rb
+++ Formula/sqlite.rb
@@ -13,6 +13,7 @@ class Sqlite < Formula
 
   bottle do
     cellar :any
+    sha256 "9f22ccb1b9d30d2026421eb1922607e268b6adbbd062e8d21a9ff82b81a5a934" => :big_sur
     sha256 "78566572373e0010a52729c1b41f7503f4e86acc67e7a37bafe091a6ddc36147" => :catalina
     sha256 "0d4a34731923310528f3ca79418eb7149cf12eda3b3043e5ba11b040ca5f602f" => :mojave
     sha256 "9fd2f150b96a7ca378f84e09a2715c7a87c1b95a3a3a241a25f57d22ab0be781" => :high_sierra
