1a36a39304add3b44407f4174ab2ff518e0d9b60
Jonathan Chang <me@jonathanchang.org>
2020-11-14 16:36:27 +0000

    mask: update 0.9.0 bottle.

--- Formula/mask.rb
+++ Formula/mask.rb
@@ -7,6 +7,7 @@ class Mask < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "46561cab979648d082e4c2c591b0b8db89d5bffaa440b41460177fd2716ccef3" => :big_sur
     sha256 "0f36dcc50a822da9a3facf9878157cb43bbf2c5ba44a56bdd87759d0ebe5d534" => :catalina
     sha256 "b8a0c2921b7641e36a8ce92523ba5a89aa6558b1e9089095eee102a734360bf3" => :mojave
     sha256 "5445d94b7efe4a65c8d9c5332e951aace2b9bc88564a3e4f714c38d53a4b4961" => :high_sierra
