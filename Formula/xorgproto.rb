4c1c4e91c653b8f4c1b814429fa3a3a724b20669
FX Coudert <fxcoudert@gmail.com>
2020-11-14 17:34:08 +0000

    xorgproto: update 2020.1 bottle.

--- Formula/xorgproto.rb
+++ Formula/xorgproto.rb
@@ -7,6 +7,7 @@ class Xorgproto < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "544326e121b491df07548620fd34df91c127b041e5f2f633f12e01738ea349a7" => :big_sur
     sha256 "b50060e1137a990a6cb6c873c4f2bf8383af62285fdb229445d2d160f84d0736" => :catalina
     sha256 "0809cb3a8941d8e3994a9f940e181b465fb67db49e8dc2e434fbd53d10f04e3c" => :mojave
     sha256 "11537fb587e7d0e539a60850f62327ded423b635969e846865dbdded481d24c8" => :high_sierra
