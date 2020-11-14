9f54478d16cb97095eaade086cebc29ef738ba95
Jonathan Chang <me@jonathanchang.org>
2020-11-14 11:28:48 +0000

    sdl2: update 2.0.12_1 bottle.

--- Formula/sdl2.rb
+++ Formula/sdl2.rb
@@ -24,6 +24,7 @@ class Sdl2 < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "7ee842d81f00d7cc3943854e99535d177b0b0bc4edb84aa957e6980975d281d5" => :big_sur
     sha256 "4a074d422e597b6f68c61cff5ee7d212264f3392b1c40a185a01cf180fe34516" => :catalina
     sha256 "7d757169bb95da1477e01a4704e8ad204fccfb1cabb3292cee3449885e14e6b8" => :mojave
     sha256 "5c7aa312b1a5d7fb8fe3fcd2112a8a74250bb84954024794333b465acafbee4f" => :high_sierra
