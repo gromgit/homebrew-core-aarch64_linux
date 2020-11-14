2fbc826a0ccbdd91572840d9fbdc3429854185cd
Jonathan Chang <me@jonathanchang.org>
2020-11-14 15:41:45 +0000

    dssim: update 2.11.3 bottle.

--- Formula/dssim.rb
+++ Formula/dssim.rb
@@ -7,6 +7,7 @@ class Dssim < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b92c04687905f6d436e15e34705106a969c931c49416c0be5fada4676f68c2de" => :big_sur
     sha256 "a45bab9dbd7de721418a18394c0b70dc66fcd44050c14a04b87e97687aa316a1" => :catalina
     sha256 "2c7b025773e8f419bd0edab3815e243708b47f60ad62fa4595fa5726c1ab77d0" => :mojave
     sha256 "288735020c3dca238550306be4e1ca80636539c2b779211889f9f485e7b8b610" => :high_sierra
