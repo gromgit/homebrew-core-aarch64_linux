7052448693a44ca17c3466636c043645289b63f5
FX Coudert <fxcoudert@gmail.com>
2020-11-14 20:20:26 +0000

    blueutil: update 2.6.0 bottle.

--- Formula/blueutil.rb
+++ Formula/blueutil.rb
@@ -8,6 +8,7 @@ class Blueutil < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f6a34cffc707320bff30528653b12df4d0d6b708c6462865923b3ea52e4b9e5c" => :big_sur
     sha256 "9e82b1bcb5a3737fb75474f4f1e793f1954d6cc2ebf8cde4c538efc046998dbf" => :catalina
     sha256 "151d723172aaf7acd9b23cf2a0b41f0fe5100357f511fea170cf34bc05093bb5" => :mojave
     sha256 "afceca3182b5a43540b44c2b61bb0d510888f0744e924468ee3208d8ab612a4a" => :high_sierra
