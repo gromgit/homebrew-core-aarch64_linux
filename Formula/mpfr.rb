db578808688437aad39ac50a99e84a0910e20dfa
FX Coudert <fxcoudert@gmail.com>
2020-11-14 12:01:21 +0000

    mpfr: update 4.1.0 bottle.

--- Formula/mpfr.rb
+++ Formula/mpfr.rb
@@ -12,6 +12,7 @@ class Mpfr < Formula
 
   bottle do
     cellar :any
+    sha256 "1e8eb0326f62d3461d420d98af6fc088daca481cae89fd77a75b420d2e76d776" => :big_sur
     sha256 "5fcf57834f58c18761c6c7b0eb961eb7f9fc54325b5361bf3a17c4dee6ebc08a" => :catalina
     sha256 "93c0d2ca093819f125300002cd34c1d1b4dfb7a1403729205861bec21388ff12" => :mojave
     sha256 "77581a1df66fb1ef55ffb19777d08b0b60fbc3d2d7ad491a8aceb3a6a4bf7ffd" => :high_sierra
