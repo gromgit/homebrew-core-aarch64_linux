45e77bc32992a37469f68ea1360f4c9b7d4a7b57
FX Coudert <fxcoudert@gmail.com>
2020-11-14 14:14:36 +0000

    snappy: update 1.1.8 bottle.

--- Formula/snappy.rb
+++ Formula/snappy.rb
@@ -8,6 +8,7 @@ class Snappy < Formula
 
   bottle do
     cellar :any
+    sha256 "b3ebf378db9b345341fc4695d1c6834433bd729a471d60b4f34306823312c464" => :big_sur
     sha256 "b15a258346dc93bd5c6900a405ccb2e9e02ebfeb5b16607b340cc6a5a021eba3" => :catalina
     sha256 "e996c3b0dfac02c8cdd06d849db47e853800389ff7d18fa66526d7d51d305589" => :mojave
     sha256 "77276307037cc20bf44c86fef60b1745c1d8f84d6f963332535b34868f5fc2b4" => :high_sierra
