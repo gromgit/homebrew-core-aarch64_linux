df60c7609aefffbf3d1d475f6de79f407c99a8fe
FX Coudert <fxcoudert@gmail.com>
2020-11-14 18:58:45 +0000

    libxdmcp: update 1.1.3 bottle.

--- Formula/libxdmcp.rb
+++ Formula/libxdmcp.rb
@@ -7,6 +7,7 @@ class Libxdmcp < Formula
 
   bottle do
     cellar :any
+    sha256 "87be4ae9085ab662369dfefff8a1c0b2fd24142ecfb905e8dea4efd09e1ecae1" => :big_sur
     sha256 "123c77fba2179591f3c1588808f33d231e9e04d8a91c99f6684d2c7eb64626b0" => :catalina
     sha256 "1684eb0ed9e92430971293f58347b9b6de899998bf03be9a19e21f69db65b53f" => :mojave
     sha256 "bff6a7f6da6c59277ca41503e66d6b778b6f89c2d1fc9047fde56ae028e3cda4" => :high_sierra
