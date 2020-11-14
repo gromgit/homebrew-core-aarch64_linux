9393ebcf422d865fe29c55be44ecfd2a97de9f0d
FX Coudert <fxcoudert@gmail.com>
2020-11-14 12:20:43 +0000

    libuv: update 1.40.0 bottle.

--- Formula/libuv.rb
+++ Formula/libuv.rb
@@ -8,6 +8,7 @@ class Libuv < Formula
 
   bottle do
     cellar :any
+    sha256 "de936683bd073e5e564222cdd2dd954a422c03298591f77a720aab9183d1d62c" => :big_sur
     sha256 "2f7836743d77fcf9ccebd6b6d00b28b38c6490639db3cf802eac039916db0647" => :catalina
     sha256 "ed9d2d1bcea3599185ec85ea3f270148971153d4e61d3e181558a768cbfffd4f" => :mojave
     sha256 "d574eb3deffdca605ab70a0bbfd9687c3795c98db2d9224eadb39b2be77aa125" => :high_sierra
