2ad5bfaec8d50cfe0420cc30a66122e006d760a2
Jonathan Chang <me@jonathanchang.org>
2020-11-14 16:43:44 +0000

    miniserve: update 0.10.3 bottle.

--- Formula/miniserve.rb
+++ Formula/miniserve.rb
@@ -7,6 +7,7 @@ class Miniserve < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "360d6a2c155008807a600f6918d102c18e4189fd22e1b3e951df61b1a26af785" => :big_sur
     sha256 "5aecd4dd080d29cbdc19c9364d0f258dab2c0b949d91f7e1476c04815b1efe04" => :catalina
     sha256 "a1ad84a40334855440f7a05815a6824c6ee300fadaa6324be156057b2ee04d60" => :mojave
     sha256 "3c004b4741d5b70faa4d77dc560b7416027a5dc5af4fcca54baaa559946571a5" => :high_sierra
