bf7cba91c8251f5fae00e98917138cba8a5afbc9
FX Coudert <fxcoudert@gmail.com>
2020-11-14 13:35:39 +0000

    dos2unix: update 7.4.2 bottle.

--- Formula/dos2unix.rb
+++ Formula/dos2unix.rb
@@ -13,6 +13,7 @@ class Dos2unix < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7a4b0e40724b8721ff76a36cb74fd6c9816ce0e9b518c3abf0e2ec5bab5b0cce" => :big_sur
     sha256 "eb14b6db2fbf8fa0f52b69aa33101e1eb04bf3d4ef5dae7234754046e4cd54a6" => :catalina
     sha256 "7a23d283d81cad13690d788c61117cbfe091282503077ad56bde83f026dd5097" => :mojave
     sha256 "98858938fe95e2453056d80c03c35396913a5c6902b2df00b618c884a4b51521" => :high_sierra
