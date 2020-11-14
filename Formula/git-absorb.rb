b14ffdc74b862683fc28f22e58145e864e0c2fb1
Jonathan Chang <me@jonathanchang.org>
2020-11-14 15:55:12 +0000

    git-absorb: update 0.6.6 bottle.

--- Formula/git-absorb.rb
+++ Formula/git-absorb.rb
@@ -7,6 +7,7 @@ class GitAbsorb < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1a960e64840655e62b5bfdbdf5b235650b6d6655c32eaac05f9afb1b472893ef" => :big_sur
     sha256 "663a57962ac9400e4b35164b82358025b1304c5097ff8841043a8885ba8881ea" => :catalina
     sha256 "afa2140c2f7e4f26c4027c3871a1f6b9f1522b9ef64b59cec358383247ae8263" => :mojave
     sha256 "42f677cb8adf051c212bc04ada5a0527289a0491d58d1b0ea71dbe4a04115c39" => :high_sierra
