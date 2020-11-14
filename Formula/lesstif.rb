4cf1c49bd518abb95ae79199cc4d999826dd5971
Jonathan Chang <me@jonathanchang.org>
2020-11-14 05:16:31 +0000

    lesstif: update 0.95.2_1 bottle.

--- Formula/lesstif.rb
+++ Formula/lesstif.rb
@@ -13,10 +13,9 @@ class Lesstif < Formula
   end
 
   bottle do
-    rebuild 2
-    sha256 "c38e2d072aeade356f5bab6e974fbbeb910259c1fe9d2ed8c352f7b67ca5bd0c" => :catalina
-    sha256 "a7b26f9ad83bbca9c88de347bc96c616eee9c6d5f0a68caca19b4cffa1347def" => :mojave
-    sha256 "bb253ab1835a89928a7c9edb904416b75876cedd50f62647e2d6044fcb55f4f4" => :high_sierra
+    sha256 "78f251801b6befbfc5823a668c45babcec2f24a0de4befd089f1034e02dcbf46" => :catalina
+    sha256 "f522a309507b2f9c2aad4aea7a8bbb6cc7d845e922d6d49cd3ca81bccad7f5f5" => :mojave
+    sha256 "6bc0a2511a83a9a15bc27a2385aa7fd944836eb4e685ee7878e590be7680e713" => :high_sierra
   end
 
   depends_on "freetype"
