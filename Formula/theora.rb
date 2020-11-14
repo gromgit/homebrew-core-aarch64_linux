af980d9bbb68c5ed18f4e136fa0330c1a13dd47a
Jonathan Chang <me@jonathanchang.org>
2020-11-14 11:34:49 +0000

    theora: update 1.1.1 bottle.

--- Formula/theora.rb
+++ Formula/theora.rb
@@ -12,6 +12,7 @@ class Theora < Formula
   bottle do
     cellar :any
     rebuild 3
+    sha256 "4ebd656de045374d8c31d772e88945c5093b17f73b708a994a01938566b2b98e" => :big_sur
     sha256 "69f9b7922ddae2c007ad5329d53067838e2208051f3a54926f8cb46a7753b1a3" => :catalina
     sha256 "243d34cb232ae0f7b45d7e2973c247ae68a57d8a4c50a2ee9e2bc7aeeabe5c78" => :mojave
     sha256 "4b5021649d047cbd556387ca6a8bd535cd8f9129be0a48f2d21bde8fb957a3b1" => :high_sierra
