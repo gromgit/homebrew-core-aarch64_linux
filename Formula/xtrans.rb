2d7c83b69af8bbde9a427a6d50455ce5693fc717
FX Coudert <fxcoudert@gmail.com>
2020-11-14 18:58:37 +0000

    xtrans: update 1.4.0 bottle.

--- Formula/xtrans.rb
+++ Formula/xtrans.rb
@@ -7,6 +7,7 @@ class Xtrans < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "784cdda022187276b0428069a1c57b7f598d4748d2bb824bd483a809fb94ae06" => :big_sur
     sha256 "74e4e5cf12976f0b9ef865052f6b40b6d3bb17fad1f6298f7cb54792aec3cb8e" => :catalina
     sha256 "a84a48c11a607fa66fa70119c46b6a590ee0b744ff600c22c2887a6bdedf73bf" => :mojave
     sha256 "7bd1e64df9191e69567a8fe7f82e97e6c2aac7a39f3f3ad96661b3369978c861" => :high_sierra
