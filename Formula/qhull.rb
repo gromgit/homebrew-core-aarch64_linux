35fdde59e68653e749c214cf1782021246c914aa
FX Coudert <fxcoudert@gmail.com>
2020-11-14 15:04:10 +0000

    qhull: update 2020.1 bottle.

--- Formula/qhull.rb
+++ Formula/qhull.rb
@@ -14,6 +14,7 @@ class Qhull < Formula
 
   bottle do
     cellar :any
+    sha256 "4184d2e81f587b29b20f1b116862c2311dbcb46c37e0067bb9a670cf30dedbf2" => :big_sur
     sha256 "45f8b6efc0a77e4fd613ef527d6c55545908f860106d4355bd753ad07a934bd1" => :catalina
     sha256 "61a2fab7f2854401bdffe54b889a1e2b9d90f6a11e52aba80688875b8f7d08a5" => :mojave
     sha256 "36e0e4a621e0c89746584562634768ccb93e71fc500230133620db030b1cc05a" => :high_sierra
