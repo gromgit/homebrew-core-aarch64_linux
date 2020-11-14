376b57953cdb14e0dea3ef5a0a79edf2eeed6874
Jonathan Chang <me@jonathanchang.org>
2020-11-14 15:17:47 +0000

    asroute: update 0.1.0 bottle.

--- Formula/asroute.rb
+++ Formula/asroute.rb
@@ -8,6 +8,7 @@ class Asroute < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "dded44d666d600e065f0a49e0c26eaa56597f1235ff937703c8dff298f665453" => :big_sur
     sha256 "873d80ef73a84598637218e467a0887477e113a9f26d8dd1f1a4b6a4571b11b8" => :catalina
     sha256 "6dbd83956cb0d73b74fd8fa6706206c7d9701eeb6a44f0e6eebcaebd9b96fbc2" => :mojave
     sha256 "77fd60fff4aa4abf7e9fd7bb7e14961b3eaab1aae0f074a318d874ecd869d32b" => :high_sierra
