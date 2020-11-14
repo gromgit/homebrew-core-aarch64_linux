28517a77cd58f6ad74c26aaf7cbf17e9d65f648c
Jonathan Chang <me@jonathanchang.org>
2020-11-14 11:39:31 +0000

    libvpx: update 1.9.0 bottle.

--- Formula/libvpx.rb
+++ Formula/libvpx.rb
@@ -8,6 +8,7 @@ class Libvpx < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9fb33bbcc029287ea3b368a3242d7ae215951a63e3809c71268380f2a41a763b" => :big_sur
     sha256 "19a684e5b0a2109b40a6c412517ad8639200c76d8bd527e98fc24d9589bb1c4e" => :catalina
     sha256 "c813698920d7e5144ae45f9922477cc30ed4f7ee81463977b01d50af43e0be19" => :mojave
     sha256 "13e231eb9c8158e84df24a58c8b96f3f57e9202ad680b4be3bbaf7e67f40aaac" => :high_sierra
