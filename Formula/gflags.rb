5eeac6c7a1f240a6988c943daf1d1213e2adf54c
FX Coudert <fxcoudert@gmail.com>
2020-11-14 14:19:06 +0000

    gflags: update 2.2.2 bottle.

--- Formula/gflags.rb
+++ Formula/gflags.rb
@@ -8,6 +8,7 @@ class Gflags < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "013d34b7e3e9ef0b1ebae5c0bad9661cf1462a4fddec2e31c27dbacb5e8697b9" => :big_sur
     sha256 "ebc7b6a9b5c14419f01a763f8b5d178525231d0fb4f5a4768673745a893f3b0b" => :catalina
     sha256 "e3176e449321b1e2070a9fabc796e6820f2f0f1f4db1c3916f58e6cdd52e510e" => :mojave
     sha256 "4beffa84f47bdfd9a1a90d9e591d9af4616db464d63046018ef0c58936d58366" => :high_sierra
