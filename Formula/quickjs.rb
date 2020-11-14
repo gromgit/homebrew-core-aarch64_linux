56339c741a8e2c451bf832dc97e540d67523a646
FX Coudert <fxcoudert@gmail.com>
2020-11-14 21:48:16 +0000

    quickjs: update 2020-11-08 bottle.

--- Formula/quickjs.rb
+++ Formula/quickjs.rb
@@ -6,6 +6,7 @@ class Quickjs < Formula
   license "MIT"
 
   bottle do
+    sha256 "207638559af40db48e5656b490f27aede6882af7dc2cd8721c25d907d54e1fb3" => :big_sur
     sha256 "b88f53813926176757beb784b812e17e53522de98e078ae97c385d349a13818c" => :catalina
     sha256 "93f6dc2c5dfd6ac8250b5595a242a5d2892a6a09b4f5999c029ce5bf0e1bb951" => :mojave
     sha256 "dfdee7a1285ce8648695c388f8fce766e2113bcd6b11ebd5ba4f21baf988c0fd" => :high_sierra
