2207940cf32f5476ceb55292ec102215ca0e3e91
Jonathan Chang <me@jonathanchang.org>
2020-11-14 11:10:16 +0000

    libass: update 0.15.0 bottle.

--- Formula/libass.rb
+++ Formula/libass.rb
@@ -7,6 +7,7 @@ class Libass < Formula
 
   bottle do
     cellar :any
+    sha256 "e95df755d6236cb7a56140c4bc12faad1d87023d23412b9f245bbda60073bf00" => :big_sur
     sha256 "427b18a8c9c8c5331553c0e814bf4e4c6f965cc53715d89a0ad3ba66b8e231c4" => :catalina
     sha256 "64f2a67f35510fe088f3e6e18075d5e08e93081d958fcee6b65ee29ab3b730ad" => :mojave
     sha256 "881db49f437027abdae60f4c849097b720216bcfa197589aea373b5f3451f9ef" => :high_sierra
