be790dce41ebda13356ca4c1eb8c5db966dc0d9d
Jonathan Chang <me@jonathanchang.org>
2020-11-14 16:17:17 +0000

    tokei: update 12.0.4 bottle.

--- Formula/tokei.rb
+++ Formula/tokei.rb
@@ -12,6 +12,7 @@ class Tokei < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "64f4a21669538cfd4ce4f7cce8f6caa21db7be05057efa4fb369f45157d71904" => :big_sur
     sha256 "c5864a5bcefee2ab0819cd07449839ab9189d04aa8ac3302c0bab85536b6c508" => :catalina
     sha256 "c40027988a8081bcd64f3fb5b4905d2efbba7d55231b0084d38578c268ea924a" => :mojave
     sha256 "3c2cb79394bdc792fb1880842077dabfc7b038f1126e29e66d4acba5349b0253" => :high_sierra
