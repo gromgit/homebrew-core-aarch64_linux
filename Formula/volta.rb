914a9cf82df8e1083bf7d1c43208bc2697605a5c
Jonathan Chang <me@jonathanchang.org>
2020-11-14 16:22:16 +0000

    volta: update 0.9.2 bottle.

--- Formula/volta.rb
+++ Formula/volta.rb
@@ -8,6 +8,7 @@ class Volta < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2a6e425ea344015228a4d9ce10792a8b754f1b747a65be2f9bd07ff819b2c924" => :big_sur
     sha256 "4ad0a1987c96c59b41a89c97b81b11a437176308e44672391a6776c4f5e805ca" => :catalina
     sha256 "a21224ba822d847eaa199e8d4da8dff0d2dd91e8a514f16092d0ed16aba83ada" => :mojave
     sha256 "261320ab49c1877a2f7fe0e1d6be111697bf2c52a8e5ee39256fee6875cecbdd" => :high_sierra
