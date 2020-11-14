e602b333e951d90caf0ac1420d75b5730c2d544e
Jonathan Chang <me@jonathanchang.org>
2020-11-14 16:37:09 +1100

    xcb-util 0.4.0 (new formula)

---
+++ Formula/xcb-util.rb
@@ -0,0 +1,28 @@
+class XcbUtil < Formula
+  desc "Additional extensions to the XCB library"
+  homepage "https://xcb.freedesktop.org"
+  url "https://xcb.freedesktop.org/dist/xcb-util-0.4.0.tar.bz2"
+  sha256 "46e49469cb3b594af1d33176cd7565def2be3fa8be4371d62271fabb5eae50e9"
+  license "MIT"
+
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "libxcb"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    assert_match "-I#{include}", shell_output("pkg-config --cflags xcb-util").chomp
+  end
+end
