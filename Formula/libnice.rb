f60030777e424c3cb01d9fa933bfe1924cd7d716
FX Coudert <fxcoudert@gmail.com>
2020-11-14 20:45:11 +0000

    libnice: update 0.1.18 bottle.

--- Formula/libnice.rb
+++ Formula/libnice.rb
@@ -13,6 +13,7 @@ class Libnice < Formula
 
   bottle do
     cellar :any
+    sha256 "f1f999f7e8b5eaed86f80237a0d588cb32d0b9f26592b2ee7186a240f6111984" => :big_sur
     sha256 "e50743980ae10fe31ab6f758031d02d174e52992a54c66899b0f6a044eb11ce1" => :catalina
     sha256 "027bb7ef74af48925a0ec5e0cf493881d519a47f354671214885ae6047d8300d" => :mojave
     sha256 "77cc78ba9ed8fd769bbc441c51998cbf2cfaa9533b1750bb0f50a2015c122fa8" => :high_sierra
