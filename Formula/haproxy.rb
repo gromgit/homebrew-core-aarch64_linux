a93d5714726776fb848fdd59e14439dc0ccba94a
chenrui <chenrui333@gmail.com>
2020-11-14 10:06:14 +0000

    haproxy 2.3.1
    Closes #64783.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/haproxy.rb
+++ Formula/haproxy.rb
@@ -1,8 +1,8 @@
 class Haproxy < Formula
   desc "Reliable, high performance TCP/HTTP load balancer"
   homepage "https://www.haproxy.org/"
-  url "https://www.haproxy.org/download/2.3/src/haproxy-2.3.0.tar.gz"
-  sha256 "08badc59e037883f788f2c8f3358fa3e351aeaee826fd8a719f1e6252aff78fc"
+  url "https://www.haproxy.org/download/2.3/src/haproxy-2.3.1.tar.gz"
+  sha256 "8d3bf1252a5b60b21e9885c8d0d6d89e932d320c2977a6522aed6df81eefca4b"
 
   livecheck do
     url :homepage
