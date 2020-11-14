11a7a955042340c3f754bb8343bd3ca46697d027
Jonathan Chang <me@jonathanchang.org>
2020-11-14 16:14:04 +0000

    tre-command: update 0.3.3 bottle.

--- Formula/tre-command.rb
+++ Formula/tre-command.rb
@@ -8,6 +8,7 @@ class TreCommand < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "980b7e58d8f8f34d6e5b5e8edc99f1f336173c25f4f0310e8922e098ef5758e4" => :big_sur
     sha256 "bef95c814d73de29b1c9cc2e868191b86e9e1d1326c0a7c590dbbb45f159e060" => :catalina
     sha256 "56ffe7b2461747687c0caededfa6b3fd2094c1773744f3ea7660d6c6ce56f0c5" => :mojave
     sha256 "3c7d84087c637c80d13c29938d218ac442b841d1265552a6c383b28ad145204e" => :high_sierra
