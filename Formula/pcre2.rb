c46ea5a13d53272b6f8ca127e8f3951ad7bf47f5
FX Coudert <fxcoudert@gmail.com>
2020-11-14 12:33:04 +0000

    pcre2: update 10.35 bottle.

--- Formula/pcre2.rb
+++ Formula/pcre2.rb
@@ -13,6 +13,7 @@ class Pcre2 < Formula
 
   bottle do
     cellar :any
+    sha256 "89748e9a1bab61ba11a63e63fa47e060bc45173d4188fbcc2cf0414ee03f1642" => :big_sur
     sha256 "6a1e59a5db23d684f92d2bf695601d1b466f3e9d5407f704ba4679d885d13cef" => :catalina
     sha256 "d7be9b0193654484e40bc30dd330711cc1e72fa9bf29f854dd50458f6a827d1b" => :mojave
     sha256 "69c4fb400d19d1910df33376974b274362ed715ab7d67ee480b5211156174784" => :high_sierra
