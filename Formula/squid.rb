f66dcd12381b3802dc1e8b3f5df8a5ecde322e98
FX Coudert <fxcoudert@gmail.com>
2020-11-14 21:29:12 +0000

    squid: update 4.13 bottle.

--- Formula/squid.rb
+++ Formula/squid.rb
@@ -11,6 +11,7 @@ class Squid < Formula
   end
 
   bottle do
+    sha256 "bcfe5fb98976abceb99c38e1a56be7d8ce008898dfc99e4ae7dccb9c188c5d2d" => :big_sur
     sha256 "9993d7d8c41a778163e0bdcd13a882125269784fce9969b3f6a4c723daa8b750" => :catalina
     sha256 "cab7176b8938750b08ec079ff5bd2b37408dace0a1823f61fc2e913d08afe377" => :mojave
     sha256 "5fc9145a26ff1555e94e5c0e124cdacd5e9b06747240da161ddb1782731b5f71" => :high_sierra
