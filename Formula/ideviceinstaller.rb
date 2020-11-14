084a2a059f0919add6dd185b54f618b3b05ff77f
FX Coudert <fxcoudert@gmail.com>
2020-11-14 19:57:28 +0000

    ideviceinstaller: update 1.1.1 bottle.

--- Formula/ideviceinstaller.rb
+++ Formula/ideviceinstaller.rb
@@ -7,6 +7,7 @@ class Ideviceinstaller < Formula
 
   bottle do
     cellar :any
+    sha256 "6d98523b90770662e350311c375f1157ac0c708769ce2145036aeed451e26621" => :big_sur
     sha256 "6ee12db78e8c224c0eb0cf88eb4f43242eb1ba672eb006636273b99b75b02a87" => :catalina
     sha256 "6ed5e4f7ace33fd5f4d1b4c6b9f0fd519836080e170b981e63942087698351c6" => :mojave
     sha256 "0dfe944eaa47cad87ad22f70dbbcefdb6b27bbeb83ca1f7a229827c03054c07c" => :high_sierra
