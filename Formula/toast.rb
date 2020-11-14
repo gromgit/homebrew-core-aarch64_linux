11179836a8d9d622df0339a7c69aa7356e49d5a1
Jonathan Chang <me@jonathanchang.org>
2020-11-14 16:13:20 +0000

    toast: update 0.32.0 bottle.

--- Formula/toast.rb
+++ Formula/toast.rb
@@ -7,6 +7,7 @@ class Toast < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9cbc0d4f8a2aba9a0f62ba9ae5938dfd8d487159c7b3818460e612d9b57c6dc2" => :big_sur
     sha256 "7d64f44aeeffee13008cc6fb8f8aafc4277f1fa032b8da12e5e484f44db10cee" => :catalina
     sha256 "248c78453c196fc4b821c9e9b5a94d12e38795c80a94c863aaccb22a0c109278" => :mojave
     sha256 "5da5b46d7f8b6eb4902696cdaaddfde84d81e3d4267f2242fd5691ef699778b5" => :high_sierra
