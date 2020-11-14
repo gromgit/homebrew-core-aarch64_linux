999fdc4c149bcffcc00c7b81c22a55f640296c17
FX Coudert <fxcoudert@gmail.com>
2020-11-14 20:03:01 +0000

    packer: update 1.6.5 bottle.

--- Formula/packer.rb
+++ Formula/packer.rb
@@ -14,6 +14,7 @@ class Packer < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3a7145af46f765f9c29f3d9019f174f34ecdf0f38ff7c343cbe247721e7c78cd" => :big_sur
     sha256 "b112017e8bf0c69756c17849b1a5b17a4820f705d4a88cfeaa584e95e7a6a7de" => :catalina
     sha256 "4be1290b8d4e4e3fdf34b75535b4136e24355a8a165ccdf08d5fa58e801ac1b8" => :mojave
     sha256 "1205b8daec00af365f36d44b6a9343cb9ba0a2e8e5534520199f0f3f656c9dbc" => :high_sierra
