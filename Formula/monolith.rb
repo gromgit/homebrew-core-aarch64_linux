adbc0195e13e690f3f2065023389030b7d88fef8
Jonathan Chang <me@jonathanchang.org>
2020-11-14 16:43:31 +0000

    monolith: update 2.3.1 bottle.

--- Formula/monolith.rb
+++ Formula/monolith.rb
@@ -7,6 +7,7 @@ class Monolith < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2f9188c982346fa01eae772a38a94d85c5f2caa5c8c9ead9c180d2062b80a31a" => :big_sur
     sha256 "d3dc3a9d7415b6599f7b2d8405b54f5af8aaff550d0536cef47817cd4cc134fb" => :catalina
     sha256 "5244b390e2a4465b4d9e882353fbf0b7ecb01a03011f4268a8fa4b066bcda42b" => :mojave
     sha256 "1f6f354e4b427036593fa4c7a93f8b1d551455f8a423f0e9f73550a230978b35" => :high_sierra
