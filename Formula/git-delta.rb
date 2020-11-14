5066c79b1c6f9f338529602835d58e4d904c16e2
Jonathan Chang <me@jonathanchang.org>
2020-11-14 16:02:14 +0000

    git-delta: update 0.4.4 bottle.

--- Formula/git-delta.rb
+++ Formula/git-delta.rb
@@ -8,6 +8,7 @@ class GitDelta < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9ca2f88662822e52195b10e52f16f0261a3faebddf4ec30496997fab3ebc699c" => :big_sur
     sha256 "3561fccb1ff28c7021d5cc5ed549f3be47dfd4127787510aac38192670077f17" => :catalina
     sha256 "6ece760a0d39fbee0b2d40237c3a0e81c1d67df655502072dc06077676ac43ea" => :mojave
     sha256 "a5767a8e9621884bb26cfb4205ffb9b2e2c0ff53536d79981ee2e22ff823a474" => :high_sierra
