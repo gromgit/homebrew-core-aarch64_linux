5d5d0dbdc2a2f3712ceddaba47e78b6f76733a93
Jonathan Chang <me@jonathanchang.org>
2020-11-14 16:30:40 +0000

    loc: update 0.4.1 bottle.

--- Formula/loc.rb
+++ Formula/loc.rb
@@ -8,6 +8,7 @@ class Loc < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "499912987d82682d5bfebd638d9a116c0d55f71284bf543067abb086bc9c55a3" => :big_sur
     sha256 "2a8ac9341661cefa1221418aa2cb5cdd5207108ade6803ab5af34ca01d0aef13" => :catalina
     sha256 "008db46fed420d7ec698d46e059a4913368af4d8f0b2f4f8502a39ee392b830d" => :mojave
     sha256 "f4241a70db520e24c587649bf7b8db0f743afaf00b01ebee5934bee7e88ae42e" => :high_sierra
