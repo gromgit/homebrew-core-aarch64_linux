ed09f42bf5ff30909d42dcc7c2cbbd38320c6974
FX Coudert <fxcoudert@gmail.com>
2020-11-14 20:20:26 +0000

    parallel: update 20201022 bottle.

--- Formula/parallel.rb
+++ Formula/parallel.rb
@@ -15,6 +15,7 @@ class Parallel < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1bc1e75b1ae0b9214b828cd1834cea067c0ce5460ffd6886bf591d3eaa77fa75" => :big_sur
     sha256 "ff1a330be51f19aaac564e19993cb0577da67c83f6218b02f94ea3d9a110ceb3" => :catalina
     sha256 "11d90c8fbbc68580b6e0aa738b29b79eaa5fb515ab5c217e55552bc62b242d5c" => :mojave
     sha256 "fa0ef5033ed82ff4639af5d4ea53ca8036f3e28d9c4aad280b645a273bda78f1" => :high_sierra
