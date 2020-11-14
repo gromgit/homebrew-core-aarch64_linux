d76c7997cc69627305c0c399af6e7bdb46fa94fa
FX Coudert <fxcoudert@gmail.com>
2020-11-14 13:16:39 +0000

    texinfo: update 6.7 bottle.

--- Formula/texinfo.rb
+++ Formula/texinfo.rb
@@ -11,6 +11,7 @@ class Texinfo < Formula
   end
 
   bottle do
+    sha256 "7332c7a1665fc64484fff626116928630d4ee21983f1e221bda360a4d590a396" => :big_sur
     sha256 "0686381d97b0448c10d11eaba59722c029d17c8423c17ad524b76ec086790f44" => :catalina
     sha256 "419fccc89f850de008e954984c65eea9b7f82940178f7ee439e42c2c892a2e52" => :mojave
     sha256 "a634a1bd15d3d7735e4934fcf26bfa295ce17108912ae7451d2761c6d578de6a" => :high_sierra
