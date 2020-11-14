b75bf6d07994e80c7f34fef3b11f0815960613d3
FX Coudert <fxcoudert@gmail.com>
2020-11-14 17:35:18 +0000

    libcbor: update 0.8.0 bottle.

--- Formula/libcbor.rb
+++ Formula/libcbor.rb
@@ -7,6 +7,7 @@ class Libcbor < Formula
 
   bottle do
     cellar :any
+    sha256 "3bbc50d56f1418e3acd54c56b28322366b1f1ca306fd3f6fd21755f2848abaab" => :big_sur
     sha256 "2860cc48fef2c42aaa50ae25aa90a683e22b81c3905a923e008871679aced20a" => :catalina
     sha256 "105d0a4b3b1a2556603e5e5619a3805183af2041ef06a85784660982c803b97e" => :mojave
     sha256 "5b20e9f902ca71c4f9a1c411c1e65eedb25f9c395d1e3ff691a0d3e7451cd6fd" => :high_sierra
