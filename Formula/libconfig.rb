a3e3645921d3f897cd7ea40fd4654be50b91e6b2
FX Coudert <fxcoudert@gmail.com>
2020-11-14 18:05:37 +0000

    libconfig: update 1.7.2 bottle.

--- Formula/libconfig.rb
+++ Formula/libconfig.rb
@@ -9,6 +9,7 @@ class Libconfig < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "3b66cbc5fae338f422386f6a2eecd650a64391da8d2f7fba259af614729844da" => :big_sur
     sha256 "5133affbfe2df2eccf05017748542e521e70a8db8763c8d8e39e00aec78fe3f8" => :catalina
     sha256 "b1c005fc0d3a811efcef915d8e84d9cc2828d6c35c5649f71fab3c714b2ae1ea" => :mojave
     sha256 "5762b7106a3e4ecc470193cd8abcfd40de090c456d42b413e545402246d73f69" => :high_sierra
