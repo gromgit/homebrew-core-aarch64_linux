e36743dcfb7f8b2e266237d5f1fc40b80692dc5b
FX Coudert <fxcoudert@gmail.com>
2020-11-14 21:42:19 +0000

    libsm: update 1.2.3 bottle.

--- Formula/libsm.rb
+++ Formula/libsm.rb
@@ -7,6 +7,7 @@ class Libsm < Formula
 
   bottle do
     cellar :any
+    sha256 "6b764ce643e30d5d152eede7592e544fbe1baf4ced75a92589d5e6242dfa55cc" => :big_sur
     sha256 "0cfe06bc49f376e5f770e378097ecf7e261db7d4b3c51740ddfcb86df36815af" => :catalina
     sha256 "35cca1d4348481da2d35f1c91882e9b32604480a15b679efed3209f74ff8d78b" => :mojave
     sha256 "927db02bae25120237de025a8219899d9ed69d8f4669c6662e170a4e0ce9eee2" => :high_sierra
