85854c854ed1d824c9e0411ac39e8c3d12401972
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-11-14 17:19:46 +0100

    faad2: update homepage

--- Formula/faad2.rb
+++ Formula/faad2.rb
@@ -1,6 +1,6 @@
 class Faad2 < Formula
   desc "ISO AAC audio decoder"
-  homepage "https://www.audiocoding.com/faad2.html"
+  homepage "https://sourceforge.net/projects/faac/"
   url "https://downloads.sourceforge.net/project/faac/faad2-src/faad2-2.8.0/faad2-2.8.8.tar.gz"
   sha256 "985c3fadb9789d2815e50f4ff714511c79c2710ac27a4aaaf5c0c2662141426d"
 
