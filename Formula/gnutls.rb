566d53cdbe83cb7d6ba5caaea05d04572f973a04
Jonathan Chang <me@jonathanchang.org>
2020-11-14 11:08:39 +0000

    gnutls: update 3.6.15 bottle.

--- Formula/gnutls.rb
+++ Formula/gnutls.rb
@@ -13,6 +13,7 @@ class Gnutls < Formula
   end
 
   bottle do
+    sha256 "6f523e8ce74c567d17a4a5b69794e897074a016b895a5d8ef7122ac006b770fc" => :big_sur
     sha256 "513407ec28ac63623dbc05ac6880d59cf7c082827687dfda7d0f065232151878" => :catalina
     sha256 "cd25205fbf27599b4186f8549324a50f045fa680b8c02a98230dcf910dff0941" => :mojave
     sha256 "5a1c108c598159c9d3dc203bed684cf70ca5dae5ee875166b35420fd2415a61e" => :high_sierra
