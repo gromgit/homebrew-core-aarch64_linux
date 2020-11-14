558099f3e2a62e0896a2b4df47801a16d8797b7b
Rui Chen <chenrui333@gmail.com>
2020-11-13 20:22:44 -0500

    libdeflate 1.7
    * libdeflate 1.7
    * remove patch
    
    Closes #64756.
    
    Co-authored-by: chenrui <rui@meetup.com>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libdeflate.rb
+++ Formula/libdeflate.rb
@@ -1,23 +1,15 @@
 class Libdeflate < Formula
   desc "Heavily optimized DEFLATE/zlib/gzip compression and decompression"
   homepage "https://github.com/ebiggers/libdeflate"
-  url "https://github.com/ebiggers/libdeflate/archive/v1.6.tar.gz"
-  sha256 "60748f3f7b22dae846bc489b22a4f1b75eab052bf403dd8e16c8279f16f5171e"
+  url "https://github.com/ebiggers/libdeflate/archive/v1.7.tar.gz"
+  sha256 "a5e6a0a9ab69f40f0f59332106532ca76918977a974e7004977a9498e3f11350"
   license "MIT"
-  revision 1
 
   bottle do
     cellar :any
     sha256 "cda21372c1a5a131c1bff0f56db0bcede77fc33b7d7993a2d10c942a687a12fa" => :catalina
     sha256 "73e0789c105bca4c823f90d4e299fa92033a3420efbde58f173cd09a469ad3a2" => :mojave
     sha256 "841ca895ade3760d2ded53aa4734a2919ca1f74cdf8acfb8cc63c9f3aa4d1165" => :high_sierra
-  end
-
-  # Install shared lib symlink as dylib on macOS
-  # https://github.com/ebiggers/libdeflate/pull/74
-  patch do
-    url "https://github.com/ebiggers/libdeflate/commit/061282f1c1e22cf9372835ca163bbe1819b892b9.patch?full_index=1"
-    sha256 "ed1ccd205f3d070aa2a50755e6c27a530fc8273de09e6d9a4a3ea79d529ccdbe"
   end
 
   def install
