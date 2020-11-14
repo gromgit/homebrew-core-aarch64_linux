d268c340ba15b87854bf2bd9f8b72448fc5207dc
FX Coudert <fxcoudert@gmail.com>
2020-11-14 19:51:25 +0000

    scrcpy: update 1.16_2 bottle.

--- Formula/scrcpy.rb
+++ Formula/scrcpy.rb
@@ -7,6 +7,7 @@ class Scrcpy < Formula
   revision 2
 
   bottle do
+    sha256 "c9658d00be68ecb6e0a4375a94d241f0f9499bf1495cf5713c61a5b6dd238de4" => :big_sur
     sha256 "e6201c5231f70f2a2ea894e5103fc3de4ac6ecd3646898642346dea3973b5943" => :catalina
     sha256 "b0d097de2bbec45803d193a4e7f49245ba97e30ef3a23b4f68a8dc38756847e9" => :mojave
     sha256 "67b0df2cfc95417ce5f300229fb14f84e20003f409bda70e76a8e75fa51c9df5" => :high_sierra
