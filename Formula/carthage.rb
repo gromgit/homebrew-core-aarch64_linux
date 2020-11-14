c522e13b689cbc74b4261cbf24d2b151cd63ee60
FX Coudert <fxcoudert@gmail.com>
2020-11-14 14:26:29 +0000

    carthage: update 0.36.0 bottle.

--- Formula/carthage.rb
+++ Formula/carthage.rb
@@ -10,6 +10,7 @@ class Carthage < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "39fe319f00259c08fcc6d0ca66c0b2240aa9f1b0f2d00f2a6b61d7c7d367b4f8" => :big_sur
     sha256 "2ea99e7a332fc6a76a1ca43b087f56534e2c658e7155c0751ffa6407c5e0a528" => :catalina
     sha256 "334d9982e33f71850b1f467a170ab5c7c050f49a3c9f2429d7db4ac1450d975b" => :mojave
     sha256 "962d60c36c3bd904cdd7b1ae0c7199b3de617b84ba7cfee7fdc91e46a413ccb9" => :high_sierra
