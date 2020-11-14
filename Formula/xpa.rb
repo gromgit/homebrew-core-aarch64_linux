b03595ac92adb54d0987b5644448dd60a4138c7d
Jonathan Chang <me@jonathanchang.org>
2020-11-14 04:46:07 +0000

    xpa: update 2.1.20 bottle.

--- Formula/xpa.rb
+++ Formula/xpa.rb
@@ -7,9 +7,10 @@ class Xpa < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "754e739f0bc2964210fa4b794ed69f46374cd77b9b0dde8e7afba7196f724ada" => :catalina
-    sha256 "660294618a8430fed54dad12001ee432ad057e7a9bd61615c356b3cd5359b1ac" => :mojave
-    sha256 "29a855b77adcb642355fd4ffc78787507e75fac460f1f53994f7ba8f2324c1ac" => :high_sierra
+    rebuild 1
+    sha256 "223dc44eba3ff66b59c26e53e9d0ab14c63d57e2f76786bae9fdb7a2be5bbdac" => :catalina
+    sha256 "6ba46da9e3de8719db32f1f577fb6943be03a58f8c6472ef7f9b398d0fea9743" => :mojave
+    sha256 "686a14717f6ae1b2af6230c3568622f9a480f8884cfee0924ffbecbee8b33db9" => :high_sierra
   end
 
   depends_on "libxt" => :build
