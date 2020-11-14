4131e15a3f281701b48a9accd6c00792ff1dfa51
Rui Chen <chenrui333@gmail.com>
2020-11-13 20:26:16 -0500

    mednafen 1.26.1

--- Formula/mednafen.rb
+++ Formula/mednafen.rb
@@ -1,8 +1,8 @@
 class Mednafen < Formula
   desc "Multi-system emulator"
   homepage "https://mednafen.github.io/"
-  url "https://mednafen.github.io/releases/files/mednafen-1.24.3.tar.xz"
-  sha256 "3dea853f784364557fa59e9ba11a17eb2674fc0fb93205f33bdbdaba1da3f70f"
+  url "https://mednafen.github.io/releases/files/mednafen-1.26.1.tar.xz"
+  sha256 "842907c25c4292c9ba497c9cb9229c7d10e04e22cb4740d154ab690e6587fdf4"
   license "GPL-2.0"
 
   livecheck do
