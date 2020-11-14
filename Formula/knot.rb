0cc5eff644766924fd8f1cb239d3345d3e964509
Rui Chen <chenrui333@gmail.com>
2020-11-13 20:19:36 -0500

    knot 3.0.2
    Closes #64753.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/knot.rb
+++ Formula/knot.rb
@@ -1,8 +1,8 @@
 class Knot < Formula
   desc "High-performance authoritative-only DNS server"
   homepage "https://www.knot-dns.cz/"
-  url "https://secure.nic.cz/files/knot-dns/knot-3.0.1.tar.xz"
-  sha256 "97af6724b04308f691392c80d75564ff8b246871f2f59c4f03cede3c4dd401bb"
+  url "https://secure.nic.cz/files/knot-dns/knot-3.0.2.tar.xz"
+  sha256 "f813a5e53263ef51d0415508e1f7d33cfbb75a139ccb10a344ae5a91689933fb"
   license all_of: ["GPL-3.0-or-later", "0BSD", "BSD-3-Clause", "LGPL-2.0-or-later", "MIT"]
 
   livecheck do
