351d79f38e8af4e72ec1bdba411c4f7d607eda32
FX Coudert <fxcoudert@gmail.com>
2020-11-14 20:46:45 +0000

    pngquant: update 2.13.0 bottle.

--- Formula/pngquant.rb
+++ Formula/pngquant.rb
@@ -13,6 +13,7 @@ class Pngquant < Formula
 
   bottle do
     cellar :any
+    sha256 "ac9a63953788be2e15e1215b32f3280dcaa4beaebeae7f239247616d9186f7d8" => :big_sur
     sha256 "cad350e78adc1912e1895b2f1c4abaf27bd14db902bad179424580934b9e1a05" => :catalina
     sha256 "60f111e8252d2480df50d6fc77e2938c50480dc03e00207a75033b882cbeb740" => :mojave
     sha256 "eb2b662bda1612dece961a7809ea5336997a00354fe204a001b4191b5b658fed" => :high_sierra
