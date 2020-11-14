fd1f75f399384faa19b4951a404f716507b0808c
FX Coudert <fxcoudert@gmail.com>
2020-11-14 14:54:21 +0000

    libwebsockets: update 4.0.21 bottle.

--- Formula/libwebsockets.rb
+++ Formula/libwebsockets.rb
@@ -12,6 +12,7 @@ class Libwebsockets < Formula
   end
 
   bottle do
+    sha256 "5edd30eeb43f99680834d0cae61fde57077b51bfe6e811be304e8a8c1fd11617" => :big_sur
     sha256 "00debd0d684c9f178d4e43aadf3b3fb3ee45ff32084cea0b026b13bb007e2bd3" => :catalina
     sha256 "011f64fbfe3adf3efda20aa89deaeef93c9959e701496752b84ba190a0a6fede" => :mojave
     sha256 "8c98c0f71a20ee3a8066f0bbb8e28104a8e0e8a65dbdbb93c8311f7ca7f4b2cf" => :high_sierra
