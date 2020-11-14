492177fcf35b48526cce63ba0e47036621c2e042
Jonathan Chang <me@jonathanchang.org>
2020-11-14 16:58:25 +0000

    procs: update 0.10.7 bottle.

--- Formula/procs.rb
+++ Formula/procs.rb
@@ -7,6 +7,7 @@ class Procs < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a930c262828b2bfef0a16166d2b310cc958af0fee2f97cd61d5b0f53b31debed" => :big_sur
     sha256 "d99de02a8ca920d0354b9dd25c7f11932cce9c8f7b8852e9a90c1d87a53782e7" => :catalina
     sha256 "f1d77baf604e57b25e5abea150ededf06acef7c9efdc552af2c1c2ff307d8b18" => :mojave
     sha256 "a0836dc38f18299c3f42d6485684d3868913b1bc00ba2d762cdf2a922aadc1aa" => :high_sierra
