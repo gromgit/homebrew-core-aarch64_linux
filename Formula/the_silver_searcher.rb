e4bf8406d24857e69dfd48a3fa27513155897235
FX Coudert <fxcoudert@gmail.com>
2020-11-14 23:03:09 +0000

    the_silver_searcher: update 2.2.0 bottle.

--- Formula/the_silver_searcher.rb
+++ Formula/the_silver_searcher.rb
@@ -8,6 +8,7 @@ class TheSilverSearcher < Formula
 
   bottle do
     cellar :any
+    sha256 "e0fe6360a649e3a9722d72d258a65a4ec449e76e82166c9d0fc48530e73e952e" => :big_sur
     sha256 "6fd80fdd0896dae09c01d3c9785ddd658bb5f2f229e7d011d3fbdde887bc35d0" => :catalina
     sha256 "e57f89664f48c131dfb462dc4be2f5265867d827f82efb1c3841ba71d9156ed0" => :mojave
     sha256 "0bf5394d8ab5f61b8fbb593249f556f13b358d16eb0d3c97215be3da0476e94b" => :high_sierra
