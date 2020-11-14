2f106a4ec27249d7dfaaa51ba1259165ba2507be
FX Coudert <fxcoudert@gmail.com>
2020-11-14 12:26:54 +0000

    libsodium: update 1.0.18_1 bottle.

--- Formula/libsodium.rb
+++ Formula/libsodium.rb
@@ -13,6 +13,7 @@ class Libsodium < Formula
 
   bottle do
     cellar :any
+    sha256 "5afc5678e30a174c1e46f1e905124f2619e6d9815ac776836090c0bff85631d6" => :big_sur
     sha256 "db372521cd0b1861a5b578bee22426f3a1f4f7cb3c382be1f842da4715dc65bd" => :catalina
     sha256 "55245bfcf6654b0914d3f7459b99a08c54ef2560587bf583a1c1aff4cfc81f28" => :mojave
     sha256 "fc972755eb60f4221d7b32e58fc0f94e99b913fefefc84c4c76dc4bca1c5c445" => :high_sierra
