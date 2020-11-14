53f393649d1112fb08d0317024fc2c4f28032acf
Jonathan Chang <me@jonathanchang.org>
2020-11-14 16:46:25 +0000

    ox: update 0.2.5 bottle.

--- Formula/ox.rb
+++ Formula/ox.rb
@@ -8,6 +8,7 @@ class Ox < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d7488d81ea3d4717ea749f5c9fdc252077f2a99ccc40fbec98e9e12b20a16f74" => :big_sur
     sha256 "7677242429502708970da5a5a4665cfa0b999581e6ab793ce8c4304d2488c6d7" => :catalina
     sha256 "fc76d843172825e04a7912b0aeace96c7e913c2b851f08cb646b4bc3f1add3aa" => :mojave
     sha256 "3008a1719ef3675fc9fb9c12de08dc7e1193877084f6a83db6a1ec87ac453da7" => :high_sierra
