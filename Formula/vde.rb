aa9e02b1d6a8f405143a6e2b90d4bbddaac454e2
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-11-14 16:03:47 +0100

    vde: disable python
    Closes #64809.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/vde.rb
+++ Formula/vde.rb
@@ -4,6 +4,7 @@ class Vde < Formula
   url "https://downloads.sourceforge.net/project/vde/vde2/2.3.2/vde2-2.3.2.tar.gz"
   sha256 "22df546a63dac88320d35d61b7833bbbcbef13529ad009c7ce3c5cb32250af93"
   license "GPL-2.0"
+  revision 1
 
   livecheck do
     url :stable
@@ -21,7 +22,7 @@ class Vde < Formula
   end
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "--disable-python"
     # 2.3.1 built in parallel but 2.3.2 does not. See:
     # https://sourceforge.net/p/vde/bugs/54/
     ENV.deparallelize
