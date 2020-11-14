dd046fdbdf0ea709ecce49247d020f1d3590754b
Jonathan Chang <me@jonathanchang.org>
2020-11-14 10:39:12 +0000

    sl: update 5.02 bottle.

--- Formula/sl.rb
+++ Formula/sl.rb
@@ -8,6 +8,7 @@ class Sl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0300afadf35bb67efe622add3f7a928bf123dd855e37376e278052b4787e65d4" => :big_sur
     sha256 "31b8e67d984635b74aec3a5b47b6145789ed9c09d065751cac862eec1386502d" => :catalina
     sha256 "e489648bcc7eff8f065855dcc891eb55f3793a5ff464d96726e313a1bc74d00f" => :mojave
     sha256 "627b0b5f8027f876466d03038da7dd0d75804cccc3bbcf45f0fe9c91199be3c3" => :high_sierra
