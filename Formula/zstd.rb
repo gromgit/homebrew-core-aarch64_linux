fd16f6e4d33d210b263bf37c26b0656c0099959d
FX Coudert <fxcoudert@gmail.com>
2020-11-14 14:09:16 +0000

    zstd: update 1.4.5 bottle.

--- Formula/zstd.rb
+++ Formula/zstd.rb
@@ -7,6 +7,7 @@ class Zstd < Formula
 
   bottle do
     cellar :any
+    sha256 "4143146aa691738893d19f6dc7de4ae983dbbf7023717094df8ff2d36cb839b5" => :big_sur
     sha256 "2375c206a934090c4ba53362d038e4e191d8dd09eec734e8e72106089aa24e9d" => :catalina
     sha256 "86b04bfd318315486d772b29d30b361e734a74269ae48805eeb3eae1d562b984" => :mojave
     sha256 "61de5a45183f4d029c66024d645ad44b0a625d58f9f583b47af42346a7c90fe5" => :high_sierra
