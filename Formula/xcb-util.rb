5c8f7f1b923fb3d581f813b05d23947f83b70802
Jonathan Chang <me@jonathanchang.org>
2020-11-14 06:31:17 +0000

    xcb-util: add 0.4.0 bottle.

--- Formula/xcb-util.rb
+++ Formula/xcb-util.rb
@@ -5,6 +5,13 @@ class XcbUtil < Formula
   sha256 "46e49469cb3b594af1d33176cd7565def2be3fa8be4371d62271fabb5eae50e9"
   license "MIT"
 
+  bottle do
+    cellar :any
+    sha256 "c161b6f0372d40ace1238507365c18a52581b798262c856099cd86eabc38c625" => :catalina
+    sha256 "0979f730b01775f3dcb33c093132ec25a49912b99e679e774bae0e995fc3f73c" => :mojave
+    sha256 "16578b76b505e33f0ccb428a947e475520d78f4dd7a56504ff9e0af9870793cc" => :high_sierra
+  end
+
   depends_on "pkg-config" => [:build, :test]
   depends_on "libxcb"
 
