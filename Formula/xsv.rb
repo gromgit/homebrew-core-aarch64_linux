e8693edc43e31686819037bc788eff6cf3b559af
Jonathan Chang <me@jonathanchang.org>
2020-11-14 16:26:32 +0000

    xsv: update 0.13.0 bottle.

--- Formula/xsv.rb
+++ Formula/xsv.rb
@@ -8,6 +8,7 @@ class Xsv < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "4dad76acfa42962ebd2a1b372e2f0cfdaedda68024849662e275525dd2c26a64" => :big_sur
     sha256 "b077e2fd6f968d6a3fa5c22a145e134bcead5cbe50ff20c78495937cbee7ea1d" => :catalina
     sha256 "ade2c436c1acf011ef7807b01a2783f6c31c0cacf066e51db904adefd99d4a14" => :mojave
     sha256 "557ef267b773911347df0d1960040f2d28e20c3680637b8af5bd89edc77bcce2" => :high_sierra
