884947ff40a062c6d2b53857402eaec9b6c9e940
FX Coudert <fxcoudert@gmail.com>
2020-11-14 12:36:00 +0000

    gnu-getopt: update 2.36 bottle.

--- Formula/gnu-getopt.rb
+++ Formula/gnu-getopt.rb
@@ -7,6 +7,7 @@ class GnuGetopt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2e24553a1a029da4d37771a5c79c9548110ee0eb98470283e68c3c47e1188a68" => :big_sur
     sha256 "729d02eac3c092be7dec5d8d3c0f0fc813e729a94a955f1bbc56d416fc410ef1" => :catalina
     sha256 "080e1a67f6efa6ab711e300366969ae63d4b3b533bde7f5347b35c2337e20e2e" => :mojave
     sha256 "4e9566ba6ef6f6bf8f3a261ae1802f621198122e227512c65ddd9bf5d2415ab4" => :high_sierra
