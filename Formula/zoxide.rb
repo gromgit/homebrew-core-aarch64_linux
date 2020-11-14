3b3a7f037050d6f5fc09070f880f0a6690cbc3fa
Jonathan Chang <me@jonathanchang.org>
2020-11-14 16:30:11 +0000

    zoxide: update 0.5.0 bottle.

--- Formula/zoxide.rb
+++ Formula/zoxide.rb
@@ -7,6 +7,7 @@ class Zoxide < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2d39de2508929c51d9d5e8e188a1752fa72b71fc2119e1cd9edae56cfc63fbfb" => :big_sur
     sha256 "0752c35a480b33fe21e6b336ebfd192b913817d649de9baf1e7d1b69e84f3a83" => :catalina
     sha256 "e9070115cff363740372b028958e590405663813526d77cfbf40b3b0abb9b9d0" => :mojave
     sha256 "9c85724a7f8272fa6fb29cd894ec78bcf4034178d88fd37cd332dec0ce34de30" => :high_sierra
