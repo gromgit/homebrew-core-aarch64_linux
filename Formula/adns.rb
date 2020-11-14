6f37193caadf99648d9990b81e5f8f1b4d96a8a5
FX Coudert <fxcoudert@gmail.com>
2020-11-14 12:53:12 +0000

    adns: update 1.6.0 bottle.

--- Formula/adns.rb
+++ Formula/adns.rb
@@ -13,6 +13,7 @@ class Adns < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8801de53f606e09e09a2bf27a17d0724cc54557328b859ed337315104eadbcc3" => :big_sur
     sha256 "d9cc50eec8ac243148a121049c236cba06af4a0b1156ab397d0a2850aa79c137" => :catalina
     sha256 "7cf73e25044783cd93ecd28e2e8bfb84f0b2fff3343acf39dff3c5fe68d1c5be" => :mojave
     sha256 "6cbe64a32b077c9abd61337c51c4e17a2286f9bee04b33f24a5dd762125798d1" => :high_sierra
