a012f6003c0f1c0e21a4b1ad3871c9723067dca0
FX Coudert <fxcoudert@gmail.com>
2020-11-14 13:55:48 +0000

    libspiro: update 20200505 bottle.

--- Formula/libspiro.rb
+++ Formula/libspiro.rb
@@ -8,6 +8,7 @@ class Libspiro < Formula
 
   bottle do
     cellar :any
+    sha256 "76ef3ec2cffe248bceafb680e741fc062d1c3b115d5b105632678d34eeb62f20" => :big_sur
     sha256 "238761be2cd640f6c3f59f0461ce7f5b73dc71c9613236e180bc55f4231e167b" => :catalina
     sha256 "fb1b2e548eddc684a5b615cedabc2c6403e0c87409fdb419369ca40e2b70aa52" => :mojave
     sha256 "b7155fe14b9909a06a9044ca2d3275f5b2cbcd9aac23583b3819ba53898ff120" => :high_sierra
