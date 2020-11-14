e6b36dffa4db8c0ef994cb5047c28cacd304f1e7
FX Coudert <fxcoudert@gmail.com>
2020-11-14 13:09:45 +0000

    watchman: update 4.9.0_5 bottle.

--- Formula/watchman.rb
+++ Formula/watchman.rb
@@ -24,6 +24,7 @@ class Watchman < Formula
   end
 
   bottle do
+    sha256 "f03c91e17cd7595f98106ee4a27f28433ecc2fd6dde8cc1b7e279bd60b730051" => :big_sur
     sha256 "30ed7115aa2a2534f5255508915f827c2e6f3100fcd7842415db64e31eabac30" => :catalina
     sha256 "135eb0a8f098417a8e4d67bf8d732a19bad1932eee085497877e93982e91074f" => :mojave
     sha256 "e872c3aae64c3b78197de9f12e272bebd5d20c316a120916f59a5f1cd2fac039" => :high_sierra
