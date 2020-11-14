701f03afa52312d1ec0b2f22c42d8f5c4c51baeb
FX Coudert <fxcoudert@gmail.com>
2020-11-14 13:44:46 +0000

    proj: update 7.2.0 bottle.

--- Formula/proj.rb
+++ Formula/proj.rb
@@ -6,6 +6,7 @@ class Proj < Formula
   license "MIT"
 
   bottle do
+    sha256 "495e52db81783a1a043c21c22b56feea8c28f3eee8e7bc0f8023e1ab83247f07" => :big_sur
     sha256 "29d73567f5e9bef026df4f1d52578d4220cf14ff3b855d1e3ac3754514071722" => :catalina
     sha256 "c531a12ccc387d6e05f159ce6dd8dca7e01da4ffeff590728cb0861c9838f7aa" => :mojave
     sha256 "4fa482aa21e501e70b3954d633d45ba2dd9fd3e17af02811646d283e8f9db144" => :high_sierra
