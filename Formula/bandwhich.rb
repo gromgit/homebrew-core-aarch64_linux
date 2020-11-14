95fba6f7316e867994bf56837cf765a50d105360
Jonathan Chang <me@jonathanchang.org>
2020-11-14 15:28:33 +0000

    bandwhich: update 0.20.0 bottle.

--- Formula/bandwhich.rb
+++ Formula/bandwhich.rb
@@ -7,6 +7,7 @@ class Bandwhich < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f140095cd6eb79ad6d06396a3c3b1bd8d9c5072639e77e54de24b45f0e82ab26" => :big_sur
     sha256 "424d3eff3b11609ad4645c028b3806babf18d9457749486fceff2522e2dd703d" => :catalina
     sha256 "99d4980e850a91edc9e12749150151a0803aa0f2591a790f7236bc7031d1f8da" => :mojave
     sha256 "5ca8f58d406af543ec3833c190472cbefaa8fb614cd5f42cfc42392e3139283c" => :high_sierra
