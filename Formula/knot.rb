5121ed32b4a115c378d1fcbd70f40fd04834283e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-14 17:12:45 +0000

    knot: update 3.0.2 bottle.

--- Formula/knot.rb
+++ Formula/knot.rb
@@ -11,9 +11,9 @@ class Knot < Formula
   end
 
   bottle do
-    sha256 "776b7135bb54a11baf79fc7cd0570bcb2125927cdf85e228f4d2c196e5737a24" => :catalina
-    sha256 "7ecd4fbc0bc7629fca8204b2c8c9f2c23b4a196f24c5b3ea77ed7d0e313a2d82" => :mojave
-    sha256 "21625608fe98fcd2b01f3eb5dcca96127a8b714ba437d3dbb8fcb62fedab838c" => :high_sierra
+    sha256 "c9f2f232383377a91e2a953d68610040d380042ed8a879e09b0a15479a4ff1a4" => :catalina
+    sha256 "9b8c1cbdad41548cae6102015737945393d00370efee92e6628b5841ac224592" => :mojave
+    sha256 "23fabf70fd2770b19eb1c8666706dbae48f311f247dd6833450f15cea4f125cb" => :high_sierra
   end
 
   head do
