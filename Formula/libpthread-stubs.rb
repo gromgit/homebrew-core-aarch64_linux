693e2264a00f9cc1169150746fb76cb31e82ff55
FX Coudert <fxcoudert@gmail.com>
2020-11-14 14:28:50 +0000

    libpthread-stubs: update 0.4 bottle.

--- Formula/libpthread-stubs.rb
+++ Formula/libpthread-stubs.rb
@@ -7,6 +7,7 @@ class LibpthreadStubs < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "959dcecc35db3faf40e18ac1fe4350b034a552a32c2b2d5a9e9c8570b8626a90" => :big_sur
     sha256 "4d69c165836f4d19d4afd152f02340ea1f6e3f218faf950dddca39e10553c80d" => :catalina
     sha256 "1baaf595e397cbfa6ba2379e1080d39de10115c39be797476cf89ff33b85938f" => :mojave
     sha256 "52d34ffb143025031355dcc9e9d4c64fe5cd46f020f2c3861fdc26d1f9c759d6" => :high_sierra
