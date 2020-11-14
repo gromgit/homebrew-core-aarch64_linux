513303fc619ee01bac20bc96ea2d83122abf9b73
FX Coudert <fxcoudert@gmail.com>
2020-11-14 15:06:33 +0000

    glibmm: update 2.64.2 bottle.

--- Formula/glibmm.rb
+++ Formula/glibmm.rb
@@ -11,6 +11,7 @@ class Glibmm < Formula
 
   bottle do
     cellar :any
+    sha256 "aaff9671885e0337aeeee7bf5c3b18a87c88fd79323bbce01f79c648a0531c87" => :big_sur
     sha256 "8b39f15570f8ec9281554ec8db93e4011ad2e13a1248047c18c7f8570a548d53" => :catalina
     sha256 "316a5f0f84491a62cf1c48a12cd4f8d9b7f7de9aa8092f72256f5114aa8730d3" => :mojave
     sha256 "7d224a2283e08715a1f7f286fcdc3e1c5cc277101bb3e2cc4bce488ec776cc02" => :high_sierra
