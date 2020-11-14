40341fe20b935e186c5ba2999c010b453d5f6aca
Jonathan Chang <me@jonathanchang.org>
2020-11-14 16:50:53 +0000

    oxipng: update 4.0.0 bottle.

--- Formula/oxipng.rb
+++ Formula/oxipng.rb
@@ -7,6 +7,7 @@ class Oxipng < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "779e480284ae1f84378ebe55d3fefb8094eab04b7685a2ec71ce5ee04f78ccf7" => :big_sur
     sha256 "731dc0bb62382fcc1ed327a9991af54c8b9e5a3744390eb0f33a820a2d76fba5" => :catalina
     sha256 "e7a6e4f1073c53479339da86dc958fd1cf7337892f7a6ff471437d9a03f9d4d2" => :mojave
     sha256 "78c733696edd12b4c76459399d792d0723bf469d1add45449b26d19877843be9" => :high_sierra
