906356a24c87f20e08b062120215471032252672
Jonathan Chang <me@jonathanchang.org>
2020-11-14 15:28:38 +0000

    bingrep: update 0.8.2 bottle.

--- Formula/bingrep.rb
+++ Formula/bingrep.rb
@@ -7,6 +7,7 @@ class Bingrep < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "18a5d9c56b49961303a17cb37da53d43ff0c5ebe80730f3a8fcc08b9f6405b7b" => :big_sur
     sha256 "38cb293ea71d8d11e422838e378cb67b09334590ed501e45b9a0f6da7d70f3ac" => :catalina
     sha256 "cef323546a1e6978ca5a67f9f18333819e318bbe136d9ba210c1fbd89f4af82f" => :mojave
     sha256 "d63ae62eff912723629b9d991fb77771f700ee306cf3b3cc40a934e3f2f13dd1" => :high_sierra
