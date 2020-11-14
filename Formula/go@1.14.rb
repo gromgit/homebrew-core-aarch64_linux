511c6c04b18cf43e887d1943e3d0e40b3ddae249
Jonathan Chang <me@jonathanchang.org>
2020-11-14 11:58:03 +0000

    go@1.14: update 1.14.12 bottle.

--- Formula/go@1.14.rb
+++ Formula/go@1.14.rb
@@ -12,6 +12,7 @@ class GoAT114 < Formula
   end
 
   bottle do
+    sha256 "40e5d0db81c6ac340fc21dd33698cf7474aada9711e046f8d90da3201a50babd" => :big_sur
     sha256 "d38b78e34a60be87ba8d3cf3fb7e8fe0ed73de7ccb66ad912913633349b6ced9" => :catalina
     sha256 "a4ada63752ff72dcce99582ad24423ccb08107431304f00493e7355643c8f3d5" => :mojave
     sha256 "49e3e33f162f8161b04413981df3ec613490872d055b55de6b0274afc2d0ae43" => :high_sierra
