503d507206339c3befdc2178316c841ea188fb53
Jonathan Chang <me@jonathanchang.org>
2020-11-15 11:03:18 +0000

    libstrophe: update 0.10.0 bottle.

--- Formula/libstrophe.rb
+++ Formula/libstrophe.rb
@@ -8,6 +8,7 @@ class Libstrophe < Formula
 
   bottle do
     cellar :any
+    sha256 "b8a23414b9196004c337bfd76a29649477014499e86269219e61dc82805b65af" => :big_sur
     sha256 "a9d230d388d880b06d5e17d14e0943499d7b2eaccb33cd7921e63f16891f1d3b" => :catalina
     sha256 "e29fab2eb0fda7024475d2ecd57476b33eee51c4123041b9e447b0a1f462fc34" => :mojave
     sha256 "a1739c44c90b762a2e94e7e97a48ba5d3e6c99bfd1d1fe3aacb4f0aee571a919" => :high_sierra
