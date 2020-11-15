850417c95a6bc044200174bc2d882839935333a1
FX Coudert <fxcoudert@gmail.com>
2020-11-15 16:21:02 +0000

    ngt: update 1.12.1 bottle.

--- Formula/ngt.rb
+++ Formula/ngt.rb
@@ -12,6 +12,7 @@ class Ngt < Formula
 
   bottle do
     cellar :any
+    sha256 "0c0390236b196ef49cd82ab692a1689281db1d06b0632878e534e02126544798" => :big_sur
     sha256 "630a651c1764f527a39d9b43ffe4d6ade5b0004596aad4038d55612f10a21c24" => :catalina
     sha256 "ce88dcbfc71c78b2e3155868a6df503e71771ae488547be9113213f09a35c049" => :mojave
     sha256 "daff8e6efb83eee79a205962f1e2346de46e53f63df7ee4183cfe9dd31f19130" => :high_sierra
