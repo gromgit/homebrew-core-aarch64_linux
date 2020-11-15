dda7c36fd7ada1a47dbfc710d5ff2bcfe5d2e74f
FX Coudert <fxcoudert@gmail.com>
2020-11-15 16:28:11 +0000

    dhcpdump: update 1.8 bottle.

--- Formula/dhcpdump.rb
+++ Formula/dhcpdump.rb
@@ -7,6 +7,7 @@ class Dhcpdump < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c6661ad77e8aa1a0355ca8c93454e9a5cba9936790b2fdbf97ada7c7e094af1a" => :big_sur
     sha256 "508365f542d3fc6574525a2b2ccacc4467b40fd53b9885e8c4e91e9e2b172f21" => :catalina
     sha256 "5c0e4a3120148871209e5943dd42c5fc81ad6b8d0e78e0964d2dc46829ac5030" => :mojave
     sha256 "d49aaa82bf92fd7adeb0edb91812d4c48aa20fb0af2f30f9b4238d032dbb99ce" => :high_sierra
