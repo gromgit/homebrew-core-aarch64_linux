6bcf30486a34316fbde08033626c85bdf8a473aa
Jonathan Chang <me@jonathanchang.org>
2020-11-15 11:12:59 +0000

    libvisio: update 0.1.7_2 bottle.

--- Formula/libvisio.rb
+++ Formula/libvisio.rb
@@ -12,6 +12,7 @@ class Libvisio < Formula
 
   bottle do
     cellar :any
+    sha256 "a284e8c456caa89555af24f961f0679423add081798fff96dbce645516d208ea" => :big_sur
     sha256 "a32916487b500b927b22b2e3cbd0f3ec5de65a56dd5306472ee6dc09080c7a38" => :catalina
     sha256 "2866289c378dcf276526de732f830dff28d1dbb12eb690209140e987b6bf2f10" => :mojave
     sha256 "793fa03c334b4faea7b94c2b4e2992b40070358a94914272a119c411060baabd" => :high_sierra
