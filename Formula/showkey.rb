d277cba228fe849280873ab4ad1f27db181fb37c
FX Coudert <fxcoudert@gmail.com>
2020-11-15 16:23:08 +0000

    showkey: update 1.8 bottle.

--- Formula/showkey.rb
+++ Formula/showkey.rb
@@ -13,6 +13,7 @@ class Showkey < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "712671c9ed8023a91e5f170d40db86fed18d70703981e1397f63d1bf40f0010f" => :big_sur
     sha256 "7565c213c9d1e4d0859164eaa92ec1e6f0cdf4d9e28f8e3a4f7a156971292de1" => :catalina
     sha256 "7bb7683a2f338db50eaed3bb2079308f32d30a43ce0ac37d16b7a9ae98235678" => :mojave
     sha256 "2eb4e4da78137fc93f5558e9448744e7dfdef298e6259f562093f956656c86a6" => :high_sierra
