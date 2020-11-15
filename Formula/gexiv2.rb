fa69c78119862f7b7836a09cc1c2969fd60caa94
FX Coudert <fxcoudert@gmail.com>
2020-11-15 13:47:27 +0000

    gexiv2: update 0.12.1_1 bottle.

--- Formula/gexiv2.rb
+++ Formula/gexiv2.rb
@@ -12,6 +12,7 @@ class Gexiv2 < Formula
 
   bottle do
     cellar :any
+    sha256 "c70dc1804031fb8c387dc3eff59274de4fdd85152df44f42001c630302080ea7" => :big_sur
     sha256 "9ebb451be639c6e3557c4113dc999ab3a0ef6c0f9f2ab508a6eb5197da40e2c7" => :catalina
     sha256 "87d16bcad50a98b318106735fb10ed2652d8cab8768f2e9a5fb8690690d656d5" => :mojave
     sha256 "6fdb45c5dec3259a2f178fdd3baee874d3b6db477ab2067d89635632900742a8" => :high_sierra
