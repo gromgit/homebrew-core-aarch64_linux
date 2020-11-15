761412e1af952499bdb48dfff2dde265580ea3c2
FX Coudert <fxcoudert@gmail.com>
2020-11-15 06:19:56 +0000

    libcouchbase@2: update 2.10.7 bottle.

--- Formula/libcouchbase@2.rb
+++ Formula/libcouchbase@2.rb
@@ -5,6 +5,7 @@ class LibcouchbaseAT2 < Formula
   sha256 "bbee97ef2394f275b8cd4b9b940010566a2d34d4754ba3532a7fca3d94029b75"
   license "Apache-2.0"
   bottle do
+    sha256 "301e8a60feeae14d7edce86f2a414d0097553e837b07b5a414f1b871b33519c4" => :big_sur
     sha256 "d3065af67b6c27379c674fadeca9a3a37eda0323d701769c34ab4aa5d5e1a345" => :catalina
     sha256 "b0fc35ac0a09d79476957fef601928fce6e9bc1abee8ec3522d59a389754fa39" => :mojave
     sha256 "4efb6194205ecbd2149ccda7631ca9f5257bcf8bc8ea00e89d50fbb7f7113da2" => :high_sierra
