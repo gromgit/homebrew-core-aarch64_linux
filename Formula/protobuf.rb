9b320bdb0fd10ea962b4cc439e7c5f942f68101e
Jeroen Ooms <jeroenooms@gmail.com>
2020-11-15 01:45:57 +0100

    protobuf 3.14.0

--- Formula/protobuf.rb
+++ Formula/protobuf.rb
@@ -1,10 +1,9 @@
 class Protobuf < Formula
   desc "Protocol buffers (Google's data interchange format)"
   homepage "https://github.com/protocolbuffers/protobuf/"
-  url "https://github.com/protocolbuffers/protobuf/releases/download/v3.13.0/protobuf-all-3.13.0.tar.gz"
-  sha256 "465fd9367992a9b9c4fba34a549773735da200903678b81b25f367982e8df376"
+  url "https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/protobuf-all-3.14.0.tar.gz"
+  sha256 "6dd0f6b20094910fbb7f1f7908688df01af2d4f6c5c21331b9f636048674aebf"
   license "BSD-3-Clause"
-  revision 1
 
   livecheck do
     url "https://github.com/protocolbuffers/protobuf/releases/latest"
