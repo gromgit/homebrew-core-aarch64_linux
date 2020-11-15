96f6470718a2c2aeb7d2755ea06d886990081b24
Jonathan Chang <me@jonathanchang.org>
2020-11-15 09:01:12 +0000

    arduino-cli: update 0.13.0 bottle.

--- Formula/arduino-cli.rb
+++ Formula/arduino-cli.rb
@@ -14,6 +14,7 @@ class ArduinoCli < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f5bf7e8be260422b006a669efd8440d7aa07e7c8e00d79988c14bb8edc0ee28d" => :big_sur
     sha256 "b5927718821674c171e8853f97c83331216bea92257168e1bd4e830fe88d5e58" => :catalina
     sha256 "4522d66a123be600d11f7c7b76dbc327f139cc060564c920fcf7c1d888a07ede" => :mojave
     sha256 "fc4945e6f995a7367d807edcb31708a2d5d7d82cfec22b26b1187133db40b8a6" => :high_sierra
