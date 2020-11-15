3ac93ce721133ccd4b0d1094aac2e083315d03ce
FX Coudert <fxcoudert@gmail.com>
2020-11-15 01:19:18 +0000

    istioctl: update 1.7.4 bottle.

--- Formula/istioctl.rb
+++ Formula/istioctl.rb
@@ -9,6 +9,7 @@ class Istioctl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d47871fc967f98fa67a3b05187438c0695376102cdfc0ff6fca22fd9215d6574" => :big_sur
     sha256 "ad04447c6efaa1d6737cca6e30786284a4a67e5c23d3f1651fe72d6d0e073687" => :catalina
     sha256 "24788010e30202c5da8c2172f8e56a193c6c75912b76f280d4037ad07d52a412" => :mojave
     sha256 "9e8356bca3cd45c820422efd52ed7961dab34d64a8ab10a56f4bf5ca38e1ff5e" => :high_sierra
