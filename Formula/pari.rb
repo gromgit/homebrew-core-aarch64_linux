855b180b535e5075987e67cb5dd71ae2a05eed1c
FX Coudert <fxcoudert@gmail.com>
2020-11-15 03:42:50 +0000

    pari: update 2.13.0 bottle.

--- Formula/pari.rb
+++ Formula/pari.rb
@@ -11,6 +11,7 @@ class Pari < Formula
   end
 
   bottle do
+    sha256 "7ace3e8f2f47e1c88afb201ba7ac04d3b8256087a35ef54d87fc8c7b360bbe07" => :big_sur
     sha256 "1394ad170419cc2002f364bdf3ec579823ca259ec4e082f225fcff3d52b1bde9" => :catalina
     sha256 "ab722c114214113c867f878cf7016d67f2d0dea2cc6e1526e8924fc5b5fb21cd" => :mojave
     sha256 "c0e5508356c5cc200b5085c4e46a3bb233cf02e6146ebe4eeaf1abc5b0af071b" => :high_sierra
