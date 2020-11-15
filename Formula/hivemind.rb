9f0297ed1e1673b47d6d6c3426ccb30c82f3d70e
FX Coudert <fxcoudert@gmail.com>
2020-11-15 14:03:00 +0000

    hivemind: update 1.0.6 bottle.

--- Formula/hivemind.rb
+++ Formula/hivemind.rb
@@ -8,6 +8,7 @@ class Hivemind < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "792e06f04a8f0742bb4f0467c24d42a92cce9c403827ebc2055a0a891c5c07f8" => :big_sur
     sha256 "ed80238e557ca71f115b2b3cd5031bfac16a3d02d8b37b386b629d89dd26af48" => :catalina
     sha256 "8e6f70ca5e0c8eb1e42d47bee207ec5333b453660d808103a36d53c51a7fb59a" => :mojave
     sha256 "7a89018774693681975cea22dcdebe35df043507476d1318f195e6d194978693" => :high_sierra
