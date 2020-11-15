395a1922c331ad3a4b4ee83b4bf6f0904f92f261
Jonathan Chang <me@jonathanchang.org>
2020-11-15 09:02:26 +0000

    assh: update 2.10.0 bottle.

--- Formula/assh.rb
+++ Formula/assh.rb
@@ -8,6 +8,7 @@ class Assh < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6762c33fb1336c69c2291490db6a48c14117885e75dbc7567cf70bb4b1ba4fb4" => :big_sur
     sha256 "e12956db15a33133390f2317143315710f6692aad3e5b36fafa3934e43ab3241" => :catalina
     sha256 "2507b79a656698c02c1fb678ebe280ba44326a9f8ec17d4fd355a3843b60253b" => :mojave
     sha256 "359270a9985af3faba8cfe637027d2a4d44ed3b0ce129bd5eb22354eb000bf45" => :high_sierra
