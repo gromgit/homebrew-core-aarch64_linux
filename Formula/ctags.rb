2aef7b9e282466d9e89f67d0de342719c587bbee
FX Coudert <fxcoudert@gmail.com>
2020-11-15 00:58:07 +0000

    ctags: update 5.8_1 bottle.

--- Formula/ctags.rb
+++ Formula/ctags.rb
@@ -23,6 +23,7 @@ class Ctags < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "d874d12982113ff0cc79501241b6c5f0caa16ae73821be66a49e246c257ccee5" => :big_sur
     sha256 "0f9bebdadd76a7ec818b904d6266eae183e869bf6f83302d836b93fc50a03714" => :catalina
     sha256 "da05bcfc8536c7e627dbea17e67997b45388706ba9bb84e521682c0358cf18b5" => :mojave
     sha256 "169b9d458f2db04d609c86c36e9d9dd4ee2474b7c472a1a11c766454e4bba1a4" => :high_sierra
