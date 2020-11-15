30f0ec3453fc2341dda3d7d6acebfc81f32612bb
Jonathan Chang <me@jonathanchang.org>
2020-11-15 09:22:20 +0000

    charm: update 2.5.1 bottle.

--- Formula/charm.rb
+++ Formula/charm.rb
@@ -7,6 +7,7 @@ class Charm < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c3c809d7c9488ae876f1549f825284dac9e500070ee62d9b351ff28c84e1620c" => :big_sur
     sha256 "b2a4c9b953f6a54c51a171d819cfddc0655caf857ec72c3ec5136493f9296b24" => :catalina
     sha256 "58e3a054f6b6ff73184e1f2909ee58117bd023bc28e448af2d6fe2e82b192735" => :mojave
     sha256 "389d0723e4bcc34bc7747e66f7f721a36cf1ab63623ec23982d68ca2623e3f29" => :high_sierra
