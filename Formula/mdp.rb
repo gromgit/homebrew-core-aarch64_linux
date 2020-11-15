b0a46bce04cb5933d2ec384687c974dac41829f3
FX Coudert <fxcoudert@gmail.com>
2020-11-15 14:24:46 +0000

    mdp: update 1.0.15 bottle.

--- Formula/mdp.rb
+++ Formula/mdp.rb
@@ -8,6 +8,7 @@ class Mdp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "422a1aa5fdbc6c1286036480a306c5842876a26982d78809b3ad84c448971018" => :big_sur
     sha256 "4d4430aea06ed48c1284b8a6b064d2e69a8a37cafb27de9ad5c65ce08c4681c4" => :catalina
     sha256 "606ffc22ff72a524f46ae683466098409e2db33cb5de58dad6ea179a9390cdbc" => :mojave
     sha256 "0bfa062ad64e8da4fa6d1df9be7e8a52da7799f87fc012b80847ac58adf81830" => :high_sierra
