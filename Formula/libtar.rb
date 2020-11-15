573ffb67c81b6b7a88076a5c7e25274fbea66bb8
FX Coudert <fxcoudert@gmail.com>
2020-11-15 06:38:04 +0000

    libtar: update 1.2.20 bottle.

--- Formula/libtar.rb
+++ Formula/libtar.rb
@@ -8,6 +8,7 @@ class Libtar < Formula
   bottle do
     cellar :any
     rebuild 2
+    sha256 "7424cf8229c7aea825592a76227da3355f32a43b9fbc5e140a0cf1eb07d05c8e" => :big_sur
     sha256 "35617f312e3c6fb1e473a5d20a559dcbd1815544bdd99c95419ac7e6e8abf9f6" => :catalina
     sha256 "070d9355e6d03dcb64ea33ecf7e3b99972e0b3ca5fc8e60e89616f0a061ee0e5" => :mojave
     sha256 "a263cfaa1499f0c82902009964df0a310e7841ddff29409c67ede0a79157c31e" => :high_sierra
