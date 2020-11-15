004cc1f1dc04ed9300fbe020e10ffcafd0eaa8fd
FX Coudert <fxcoudert@gmail.com>
2020-11-15 13:48:45 +0000

    fetch: update 0.3.11 bottle.

--- Formula/fetch.rb
+++ Formula/fetch.rb
@@ -8,6 +8,7 @@ class Fetch < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6a106ac8e98524efb62d5094a3b06f6b0883ef95f1affff388ae8656f84fcfa7" => :big_sur
     sha256 "4ff8bb89d5ce13392ed8f96d7da747eaacb842fa5829d799aa469c15197e8caf" => :catalina
     sha256 "e2c9c3b2aa915880cda1e940763d6adc09bca9de097003a06c2b900b24616ef8" => :mojave
     sha256 "47a73e9563c765b488948574e9ef935fb3a2f8db8f2c98b4997829d877a233ca" => :high_sierra
