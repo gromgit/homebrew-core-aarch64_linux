f47de5451a1274ed88cabac3a9e741a975d6efdc
FX Coudert <fxcoudert@gmail.com>
2020-11-15 18:01:24 +0000

    getxbook: update 1.2_1 bottle.

--- Formula/getxbook.rb
+++ Formula/getxbook.rb
@@ -7,6 +7,7 @@ class Getxbook < Formula
 
   bottle do
     cellar :any
+    sha256 "86d7a091869ff032ca24ddd7979ed545ca17775ad5c99fac92c706857c31eeb0" => :big_sur
     sha256 "a99745b1db4509b84e84c0306bdf4439086670e608e7ff7e4d89e5318547391b" => :catalina
     sha256 "68f7f76c607807315d1e8305830eaa94d04e1c87f1ea1382fb00bd7ec74f2886" => :mojave
     sha256 "dce13d6e7d9f5f0eb79064858dca9dc3d62b274ba1f07b2ceaca3bf06e4effed" => :high_sierra
