5389c399e9f79d42d976c6b5ef4efc677ea973c0
FX Coudert <fxcoudert@gmail.com>
2020-11-15 07:26:08 +0000

    potrace: update 1.16 bottle.

--- Formula/potrace.rb
+++ Formula/potrace.rb
@@ -13,6 +13,7 @@ class Potrace < Formula
 
   bottle do
     cellar :any
+    sha256 "3b5294deed86179a4e496236fb882eb0b7ad3c020741e2a1398861b545062712" => :big_sur
     sha256 "c3f357a8bd6460384400acd00dab0d8571ad0b1543a81e5b9d5ff49d1ece4fa1" => :catalina
     sha256 "3ad69cce4edecea6e5170b766201845b703a98bbac3c5272ef6a045f828643e2" => :mojave
     sha256 "56d821a4d3579bedf64ebf5357fc04f214cb2efbea7ddb681b202e684e71d97e" => :high_sierra
