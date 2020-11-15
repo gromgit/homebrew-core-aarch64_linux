bf53f939e9fc99c6262b0e1b1e09a26f5f30ae61
FX Coudert <fxcoudert@gmail.com>
2020-11-15 13:55:29 +0000

    govc: update 0.23.1 bottle.

--- Formula/govc.rb
+++ Formula/govc.rb
@@ -7,6 +7,7 @@ class Govc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "55998445bf49aaf86766301bd3dda7e966cdb0d5692d1167746968e0eb93d58e" => :big_sur
     sha256 "f87e7522bf5a4d2dfadd33586f362c362373de714f46e08f3417ea228fbbf8ec" => :catalina
     sha256 "9381ff648dcb11aa393221f3051685a32fec1e08e1e820dac5f6b4c9add33015" => :mojave
     sha256 "b1004db501274c309e2c1b61c692f38e438ca1742bb59397ce332059259ce813" => :high_sierra
