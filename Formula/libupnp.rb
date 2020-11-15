c3fa4c863a03186ef72b08dc04dd2d91f0b35d8d
FX Coudert <fxcoudert@gmail.com>
2020-11-15 06:41:42 +0000

    libupnp: update 1.14.0 bottle.

--- Formula/libupnp.rb
+++ Formula/libupnp.rb
@@ -12,6 +12,7 @@ class Libupnp < Formula
 
   bottle do
     cellar :any
+    sha256 "13f2cab18c8b12b11802c2bd8dd0cf62b15c44e8f3ad008f2acc679d112adb11" => :big_sur
     sha256 "f687168c37ba09bb0ee06b2d0d373c6de2f8d5511c563191788e45282fd104af" => :catalina
     sha256 "b7e3d1d7fc0a4011e093f040f71578f549e06b7702d9ec29c9595fb11fa206f6" => :mojave
     sha256 "70340bed620fd9c537881d58a6b0f720689fbf0ebf9dbfa9acfdb846190e9c69" => :high_sierra
