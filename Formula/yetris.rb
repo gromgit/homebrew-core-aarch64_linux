02a9276d1a9fb5e7214e85b19bd7858bb1b60615
FX Coudert <fxcoudert@gmail.com>
2020-11-15 19:56:31 +0000

    yetris: update 2.3.0 bottle.

--- Formula/yetris.rb
+++ Formula/yetris.rb
@@ -7,6 +7,7 @@ class Yetris < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d0d9c0ddd6f7f825024cb4e96978ad43919eb77a216e8788943f1c8d7bfa80bb" => :big_sur
     sha256 "a43b346adc20fc7d4f84ec1300e839bb4e615ab40ccf8e1a591f099092ad6078" => :catalina
     sha256 "ace31e89cefd33d38a65864d7343baad6dbda23aee0ba2a10f6b19480b9708e0" => :mojave
     sha256 "21537f5957c5ce90281195e6d962363920bda756a6c965ca107c329ec712f126" => :high_sierra
