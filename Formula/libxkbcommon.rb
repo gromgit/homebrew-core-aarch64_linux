2beca258e67f927dc9e303f06236812d12c31a42
Jonathan Chang <me@jonathanchang.org>
2020-11-15 11:20:09 +0000

    libxkbcommon: update 1.0.1_1 bottle.

--- Formula/libxkbcommon.rb
+++ Formula/libxkbcommon.rb
@@ -8,6 +8,7 @@ class Libxkbcommon < Formula
   head "https://github.com/xkbcommon/libxkbcommon.git"
 
   bottle do
+    sha256 "eba7e1822c399b6a0e71e91b6a17248817f0a8f205cb6498068c971980546769" => :big_sur
     sha256 "bf2346f444e3c8dcf1b34a08abeb5142eb07019a14c1513638df8e4b60c7acef" => :catalina
     sha256 "57e4659ca526ee0b579a35f4eb7ab4a97d56631ec17f5320cd662d3379c92521" => :mojave
     sha256 "01e3b905b6e11da0fd02674ae5e659659a2019bc4a99c8d874ceb2dc3daa7610" => :high_sierra
