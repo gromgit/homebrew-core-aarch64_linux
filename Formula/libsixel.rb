4889e931451412191c9d191e59247d12f688a696
Jonathan Chang <me@jonathanchang.org>
2020-11-15 10:34:33 +0000

    libsixel: update 1.8.6 bottle.

--- Formula/libsixel.rb
+++ Formula/libsixel.rb
@@ -6,6 +6,7 @@ class Libsixel < Formula
 
   bottle do
     cellar :any
+    sha256 "b2963fe42a38cea1521ac653c7009f278bd4ed1931ea567380b860d62edef0b7" => :big_sur
     sha256 "520fa6d77af3c6cc84fb84b1a5b8797bb6e44396b70ad7654eb3362d2174d0ab" => :catalina
     sha256 "716d90122f113bd1c6b2ad7e872a476923981b4c26830c94ca68724437e860b1" => :mojave
     sha256 "9e061ce67b22c8ad8760bccc7e954ee46852285bc078087712538e102ce8215c" => :high_sierra
