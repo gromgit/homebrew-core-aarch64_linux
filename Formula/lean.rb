b10898fc520ca06c4f4651600207fa95d9b30232
FX Coudert <fxcoudert@gmail.com>
2020-11-15 03:02:15 +0000

    lean: update 3.18.4 bottle.

--- Formula/lean.rb
+++ Formula/lean.rb
@@ -15,6 +15,7 @@ class Lean < Formula
 
   bottle do
     cellar :any
+    sha256 "1961d97c68235a0c2d349323b171f31acd073b23809f007e5874d019b6fc7baa" => :big_sur
     sha256 "60bf9be856f7c4158d18717387bd9a74e1e8036fb4ecdb285b3d410e82fec907" => :catalina
     sha256 "a1cee5b0d39bc214d16ad925c4f5fb70c4edba84da64d4cc0f288233f4b032c8" => :mojave
     sha256 "4efbb7149a7a7f8b85a1c1e8e48e561757b0cf3849b55d31795d30d372ac7a08" => :high_sierra
