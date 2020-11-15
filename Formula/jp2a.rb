fe92e658866325a3f891530220a44a2d18313593
FX Coudert <fxcoudert@gmail.com>
2020-11-15 03:29:24 +0000

    jp2a: update 1.0.6_1 bottle.

--- Formula/jp2a.rb
+++ Formula/jp2a.rb
@@ -18,6 +18,7 @@ class Jp2a < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "8ab042d2d660fbc55e4a055e0618d85fbbd32029f703d35c59f3d12c0b000936" => :big_sur
     sha256 "a9aa7c8893c63ad5621788af3813ed9758e09e0c79b9ba3a8262d5c17b2272f9" => :catalina
     sha256 "4e62b310889a384daf9058267ac0b1bdc73d2cb408f05b9e3d3072be52355bfd" => :mojave
     sha256 "8400fccf2a4459fe37ce0f3856459127f4f66ff002c356f36942462c0c9c3809" => :high_sierra
