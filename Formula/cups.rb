62d151b44652277936933c41875d5696744af7a4
Jonathan Chang <me@jonathanchang.org>
2020-11-15 08:34:32 +0000

    cups: update 2.3.3 bottle.

--- Formula/cups.rb
+++ Formula/cups.rb
@@ -6,6 +6,7 @@ class Cups < Formula
   license "Apache-2.0"
 
   bottle do
+    sha256 "00bdcf0cb9e681d4e10bc7ec87eb57ccc9c63a5b76816a8545b77e454a8a0021" => :big_sur
     sha256 "1bed38f7aa9ee2c76fed5393e308cf7de5dd6d443debb0455ee7739e010b0e20" => :catalina
     sha256 "19ea09cef986e1c66f1736409f4e7917914100bb68525d8118e705500fbf28e0" => :mojave
     sha256 "0d3328bddece686cfa54e63b61f68c68fed1b1f8553cd45ed194d8a80e56f9c9" => :high_sierra
