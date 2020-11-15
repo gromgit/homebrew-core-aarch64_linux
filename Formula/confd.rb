b810266783c626a41b56fd141f7f66474d681803
Jonathan Chang <me@jonathanchang.org>
2020-11-15 09:27:57 +0000

    confd: update 0.16.0 bottle.

--- Formula/confd.rb
+++ Formula/confd.rb
@@ -8,6 +8,7 @@ class Confd < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8c337c7afdcf9d7bf7662f94d24fa326990e69344f6c23700ba2a5c0c540592e" => :big_sur
     sha256 "34d59b3c47493cd00685c62997ac0385f52f90a5d99adb9ed5c98576c6c02452" => :catalina
     sha256 "6c83fe2e7e744917d241e8fd51d76b83838ac08dcab31c2663c7b2c7703140cc" => :mojave
     sha256 "8605d52c611da0530d31178fbb9805592113d70b3d496d21a34696ff499aac70" => :high_sierra
