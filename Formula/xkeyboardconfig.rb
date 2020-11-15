17e2b51014eaa777d35b25b0d83c7b860693492b
Jonathan Chang <me@jonathanchang.org>
2020-11-15 08:24:00 +0000

    xkeyboardconfig: update 2.28 bottle.

--- Formula/xkeyboardconfig.rb
+++ Formula/xkeyboardconfig.rb
@@ -8,6 +8,7 @@ class Xkeyboardconfig < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "914f4d2b2f99366532b6966a45f58da6be6bd391e1f162d1aca512341aab6298" => :big_sur
     sha256 "2bc4ef4bb2f03b72f8ceb8c4fb12048c8dfda6c4db24f7fa2cf6b19f4ba54b17" => :catalina
     sha256 "347f0195f728cbff5e9e2330248d1d195e8fb198134572bd66d1f45fb4449c10" => :mojave
     sha256 "8350bb43f9ae136e0d3a811230d351ce741c63ba2ba1e9901d63b009cf6f588e" => :high_sierra
