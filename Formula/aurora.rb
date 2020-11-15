19c7bd36cae579cacf78b0a47497f98a7531153c
Jonathan Chang <me@jonathanchang.org>
2020-11-15 09:03:41 +0000

    aurora: update 2.2 bottle.

--- Formula/aurora.rb
+++ Formula/aurora.rb
@@ -8,6 +8,7 @@ class Aurora < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "714b7116c80107b6ffb0f5b8abba41ae5aa88708fe688e61144ca3a636b7fc4f" => :big_sur
     sha256 "f3b45006b5b5c6f15166d11d1a740fb14f3b22c1d64b3b64397ed2958e9c882d" => :catalina
     sha256 "21abebb582fbac2ebb400328b455c890206f78ae0910f75ded8019bfc6a40c1f" => :mojave
     sha256 "e3e9b06b4b9053afb4b75b48d90555d00fcc8404309d8b2b2b336538810746cb" => :high_sierra
