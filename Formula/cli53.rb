62f33ad20e5330a9d056d9e2465da6320cef8328
FX Coudert <fxcoudert@gmail.com>
2020-11-15 05:57:13 +0000

    cli53: update 0.8.17 bottle.

--- Formula/cli53.rb
+++ Formula/cli53.rb
@@ -7,6 +7,7 @@ class Cli53 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0cb72c1182f1f323ce607fe9e016c3cf9352db57e18384da6b356164aab751cf" => :big_sur
     sha256 "65f9f13eb5c2f53afcb697f9699f8dd6b9df5f400908a2d3423879f8ca895942" => :catalina
     sha256 "73ec9efeaf423dd32530bd4a547b11522b54dd5666942735268bb7690479ff09" => :mojave
     sha256 "190e2e02b890cf099ca52d1468b224d16c82840a3a534db83a2aed5553b07bfc" => :high_sierra
