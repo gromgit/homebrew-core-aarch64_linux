33d8bb66d18a638f062b6c9c36075a2267d8f7b0
FX Coudert <fxcoudert@gmail.com>
2020-11-15 14:04:14 +0000

    t1utils: update 1.42 bottle.

--- Formula/t1utils.rb
+++ Formula/t1utils.rb
@@ -7,6 +7,7 @@ class T1utils < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f00f838c4ebef97926c3a2cd9940d2105029839a25543f9861903ec1a71939e1" => :big_sur
     sha256 "dfaaef0c838273e5c4cee7d6d2eb515e91c77c3226913b4c4486ca0086c2e6bc" => :catalina
     sha256 "1b511df389dee041c0cdadae94e38e987ea978024730d687b8642623cb054e09" => :mojave
     sha256 "c17de51c95690f3133933cd508873e21734a8e4f8ed80ec6546ab3c7fb82edd2" => :high_sierra
