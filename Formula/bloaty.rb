c3e2f64bc6c1b12c4a1ef3370f8daeec3a99d0b0
Jonathan Chang <me@jonathanchang.org>
2020-11-15 08:47:02 +0000

    bloaty: update 1.1_4 bottle.

--- Formula/bloaty.rb
+++ Formula/bloaty.rb
@@ -8,6 +8,7 @@ class Bloaty < Formula
 
   bottle do
     cellar :any
+    sha256 "87290bf7e990ad1a82e6dd3ccd8ff2c671849818e3295b8d4154172d44f39d18" => :big_sur
     sha256 "749928462562883be2116fb4a7065c94148ad046163a39a51fce4e227e9eac30" => :catalina
     sha256 "6e277b2abe8cedf0b4b3afecc32fbcb85bc3516d7b014f1409184d1929339589" => :mojave
     sha256 "a133017f62791ec01190fc5e734e9e1e150bbcaebe65ad50491b9d2d6631a010" => :high_sierra
