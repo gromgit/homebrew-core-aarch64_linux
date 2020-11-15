f8396de263edf7a6fee9801481b26d468e581b62
Jonathan Chang <me@jonathanchang.org>
2020-11-15 10:26:41 +0000

    liboqs: update 0.4.0 bottle.

--- Formula/liboqs.rb
+++ Formula/liboqs.rb
@@ -12,6 +12,7 @@ class Liboqs < Formula
 
   bottle do
     cellar :any
+    sha256 "37ccef44b4ea6d76bbd98343ef266dda8d3f6b7c103f981a4194ecd841685017" => :big_sur
     sha256 "ffd8b834836ed6b28606c173766f99d168c57b322153cbea5100bbbc27e1073d" => :catalina
     sha256 "1253594d96910c9bea3566d75461de4e497097dd24e94fcddb232dac3d2bdbfd" => :mojave
     sha256 "c0fb642f6934a0413fbd2e4a32a02f8cd5e7b7491f3f6d6423ab4856cef8f5df" => :high_sierra
