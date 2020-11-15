a85bd8f5b338f3306c9b1ce56613a2572aa370bb
FX Coudert <fxcoudert@gmail.com>
2020-11-15 19:59:46 +0000

    uni: update 1.1.1 bottle.

--- Formula/uni.rb
+++ Formula/uni.rb
@@ -7,6 +7,7 @@ class Uni < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4441cc2d95b43746b9e39646b14417387d439253d5313d291ddf52002650c837" => :big_sur
     sha256 "d6a995e94c8bc6f9b74a1eb370b5aa348a7377e9170453820400da83641cef66" => :catalina
     sha256 "1a2f04a3dd21f6d2c2ea16d3683f936e1d8310dd4b2d503aa8a60d67b6f24367" => :mojave
     sha256 "e4b8e98523d14eb9cba3991946deb0cea86c09acfb65d175c04d4233ec32eceb" => :high_sierra
