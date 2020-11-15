c5c4dec128d5c7d1f39b3e8daf3722b9dbba4b24
Jonathan Chang <me@jonathanchang.org>
2020-11-15 08:44:37 +0000

    osmfilter: update 0.9 bottle.

--- Formula/osmfilter.rb
+++ Formula/osmfilter.rb
@@ -9,6 +9,7 @@ class Osmfilter < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5647d8f3a704bd126e2b5f24237febb50989798b425147baf1d1ce1a08fbdaaa" => :big_sur
     sha256 "5e2b755a970b7432fb076d787cb1777df18861832d0e4d45132fd84e4d7aea20" => :catalina
     sha256 "470532603de299b9073f5511b8be798558d430f86ba4f37b330a497ec9fdae48" => :mojave
     sha256 "b2e2d4190462b0b0e473da4a50ab5e25da007aca21db898d2d359e9e9eb2cde7" => :high_sierra
