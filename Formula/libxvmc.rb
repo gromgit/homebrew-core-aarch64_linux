883d9e6bc1092977059c4074e567c2f42b5523f6
Jonathan Chang <me@jonathanchang.org>
2020-11-15 09:40:26 +0000

    libxvmc: update 1.0.12 bottle.

--- Formula/libxvmc.rb
+++ Formula/libxvmc.rb
@@ -7,6 +7,7 @@ class Libxvmc < Formula
 
   bottle do
     cellar :any
+    sha256 "e6921d091fcd38b2ba85587864b3a4c2e2834fd74e985d82cfffd971c6d06ffd" => :big_sur
     sha256 "25bf67d5ac709c8a1551f38dc7da22bc5a2007b1a0d56589e96f172d35248f7e" => :catalina
     sha256 "aa97b7fa2fa9c9a6df4cdd7bd939b2febfe870dd3feed76214d9f41789d50ab8" => :mojave
     sha256 "b00b2e0ba733b3cd0ad8979cce06353e13a9474c5be32e53a467b8308b78f99a" => :high_sierra
