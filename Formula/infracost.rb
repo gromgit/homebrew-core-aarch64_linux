004ef68982415ce1436eec295ea5d73f4d6aa85c
FX Coudert <fxcoudert@gmail.com>
2020-11-15 06:02:33 +0000

    infracost: update 0.7.2 bottle.

--- Formula/infracost.rb
+++ Formula/infracost.rb
@@ -8,6 +8,7 @@ class Infracost < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "79c781572ae9e13d9aa137e34ed40173c306b326d2108b4f546085d8aed6c654" => :big_sur
     sha256 "45ac661839f2c8f39576289b1ddf2e821f722e97a000415cc46c2160d0de2668" => :catalina
     sha256 "1b3375697e30cc0f0ad46e7732e038b9541aa08e5cae4a7e3a14e3e1cdbc8609" => :mojave
     sha256 "19aed52b30358d70d438f20308fc4ea0140db45070c428b0c77f874e00f6ed23" => :high_sierra
