cf5cbe57b0744d1f41e7ed39b9db84917183ce5a
FX Coudert <fxcoudert@gmail.com>
2020-11-15 17:31:20 +0000

    lsdvd: update 0.17_3 bottle.

--- Formula/lsdvd.rb
+++ Formula/lsdvd.rb
@@ -11,6 +11,7 @@ class Lsdvd < Formula
 
   bottle do
     cellar :any
+    sha256 "78c4f868f8b534b59db6820fe0f303d42c0f908c49be6e5a5896da76441d0e0d" => :big_sur
     sha256 "63e52d4264e765ca6095153de37b08dee02d59676ae98b2f0c36b29c0fdae48e" => :catalina
     sha256 "1489bcdd29d86d725cbf76a68ff77335729c676fcf1155555da799d7096ee933" => :mojave
     sha256 "d5a945ffb9e6172df4625d0ed5b3e5c6aecd2dfc6c9ab5b2a83d88267027edc8" => :high_sierra
