103bd6a921b8bd9f885f00bc9cb387074e53d225
FX Coudert <fxcoudert@gmail.com>
2020-11-15 07:09:01 +0000

    stockfish: update 12 bottle.

--- Formula/stockfish.rb
+++ Formula/stockfish.rb
@@ -8,6 +8,7 @@ class Stockfish < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "33e7b97bde0b94856ef6e14087f2c1e40d8e17f5a4878123bedeacfd6329fdd3" => :big_sur
     sha256 "04832aaf954ad2c9f50909fc8d03958e445a9e784d4c43b04d5cdcbd8232b4dd" => :catalina
     sha256 "a2fd4c1073710d99d49a34e4a77399915e13829dab269774beb71f48e087461f" => :mojave
     sha256 "9c65cfd27113438abd3823e5172b58816b9081f8b41123e32da8c72d590121e2" => :high_sierra
