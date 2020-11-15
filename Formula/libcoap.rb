8d42e6dfd83fa6855dd0b6cf397bb1f54ed06360
FX Coudert <fxcoudert@gmail.com>
2020-11-15 02:44:06 +0000

    libcoap: update 4.2.1 bottle.

--- Formula/libcoap.rb
+++ Formula/libcoap.rb
@@ -7,6 +7,7 @@ class Libcoap < Formula
 
   bottle do
     cellar :any
+    sha256 "f57bd53021361886f6928bbb0156dd642bd39dbdd459e5a69951560e4cc47050" => :big_sur
     sha256 "344f2a098d9f1767d50135fbf4ae3bdf893a079ebf8a54f248811673fa437e39" => :catalina
     sha256 "012f1efcb1655479c531df4db98eb481d83971751edffb99b4ca8c50592cd27c" => :mojave
     sha256 "a68df19a4ca87c677173c14b534848592bb35e46a715ca066bcd114f8c735236" => :high_sierra
