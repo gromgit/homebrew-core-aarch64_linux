cced5f509948c5db41a29c4ce3d1cd80f5d58ed7
FX Coudert <fxcoudert@gmail.com>
2020-11-15 03:43:19 +0000

    jdupes: update 1.19.0 bottle.

--- Formula/jdupes.rb
+++ Formula/jdupes.rb
@@ -12,6 +12,7 @@ class Jdupes < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9588e410df4f34d12d68ee4605ca3e2dc3a35b5a7112dcb05352de07951b2bf2" => :big_sur
     sha256 "46fec950c90e6d2306884dc961d508368e744b6b3b516a1c935930c694f5dd4b" => :catalina
     sha256 "d1c88242da6c924d310427620cdef06190b33f9a60c9633f1439df7caddc8f05" => :mojave
     sha256 "654d11b34e057cff3150e264d2dbf10d8d2114a9529cb20ab6c76865fd6caf7e" => :high_sierra
