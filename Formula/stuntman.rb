04546ce39d43243b6e594c500a24ff6f0b85071f
FX Coudert <fxcoudert@gmail.com>
2020-11-15 14:54:59 +0000

    stuntman: update 1.2.16 bottle.

--- Formula/stuntman.rb
+++ Formula/stuntman.rb
@@ -13,6 +13,7 @@ class Stuntman < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "74b6ba4ad33c987b2b3e8a70f4c4e3c383cfa1955a0eb499b02edbf4ed7c8a45" => :big_sur
     sha256 "e337d1ad8978b0bb926bca46992575b686145f9e8eb43dbc990e4efe08539722" => :catalina
     sha256 "d1b2a91211d57f057081fba43d0ed6ae3b05c40114b1e77f0cd3c0189f7ad07c" => :mojave
     sha256 "a7055d814d7645e408d92ffeba5ff5c1215302bdf4411bbf02e8d49ff40115a6" => :high_sierra
