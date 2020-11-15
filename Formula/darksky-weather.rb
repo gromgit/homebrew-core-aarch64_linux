61927d611f64b4438e5619466c6d5695e9f1d935
Jonathan Chang <me@jonathanchang.org>
2020-11-15 09:31:23 +0000

    darksky-weather: update 0.15.7 bottle.

--- Formula/darksky-weather.rb
+++ Formula/darksky-weather.rb
@@ -7,6 +7,7 @@ class DarkskyWeather < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ed2ee9baca087ab4fd02ee792514135a7a2d1d685e3fa565567918c1d85a94aa" => :big_sur
     sha256 "64aac1bb9c9f6fc856fdb6e818b11d715addb17c6f35480abf0a337c1dcaa311" => :catalina
     sha256 "ae72f00275774f08c66fb8f90697545b97701c4fb819416fb6215779ec775cab" => :mojave
     sha256 "b621c14d94f6e0e1c350e0bc78c0269bed889b065d811acb8da39d137de5ac4f" => :high_sierra
