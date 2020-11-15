127025d61782eee0a68645c84856db6757159eed
FX Coudert <fxcoudert@gmail.com>
2020-11-15 00:31:42 +0000

    libxxf86vm: update 1.1.4 bottle.

--- Formula/libxxf86vm.rb
+++ Formula/libxxf86vm.rb
@@ -7,6 +7,7 @@ class Libxxf86vm < Formula
 
   bottle do
     cellar :any
+    sha256 "3443b4465d3839bcee9bbf60a1028b0acfa45877f9b8bfd2654ca42ad1087d46" => :big_sur
     sha256 "0a4d6a9d0b98bd8b8cd2aa2025b6ee80a19ffca34744fed599ef0e754d9e810b" => :catalina
     sha256 "c89b765023eb0b6820cbf776d6af714363dc6ebd1ea000e1e53a99fa79be4a0d" => :mojave
     sha256 "37cf2b440d8d4e7cda6aa70d070cd45427cebc61781b6c1b3aec153bf82d638d" => :high_sierra
