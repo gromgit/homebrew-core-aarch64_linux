a9c6329722c9b04966fe31ad1372ab05e8c88ee6
FX Coudert <fxcoudert@gmail.com>
2020-11-15 11:29:39 +0000

    libgsf: update 1.14.47 bottle.

--- Formula/libgsf.rb
+++ Formula/libgsf.rb
@@ -10,6 +10,7 @@ class Libgsf < Formula
   end
 
   bottle do
+    sha256 "190c702ad48598787b9fc72a019cd8f2701d1ae28ed41e3c111acd19b223d139" => :big_sur
     sha256 "82a2fce8b091d204302919c7541a0ad21a28a24fe8ad8a5d4ae7f50f3f190349" => :catalina
     sha256 "a051f21e80044277fafb60264c915cb79ce5f64059e9737f7a15795bc79ad3c8" => :mojave
     sha256 "c2cb9985014c2c727abe935f113ab827e5a7af1e4376c27261897381fc87a2ba" => :high_sierra
