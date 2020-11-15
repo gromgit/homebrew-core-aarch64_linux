6ee43182566b4e9985cec72cbffc598176fa9a6e
FX Coudert <fxcoudert@gmail.com>
2020-11-15 17:17:55 +0000

    fasttext: update 0.9.2 bottle.

--- Formula/fasttext.rb
+++ Formula/fasttext.rb
@@ -8,6 +8,7 @@ class Fasttext < Formula
 
   bottle do
     cellar :any
+    sha256 "3869650705430f8b682416be4e7c0a01c243a2f9517c6668027c6e9576f1e9c6" => :big_sur
     sha256 "ec085551ced1f55b863a65aa60ad8f31d796002702b7effaaaafbf1490df867f" => :catalina
     sha256 "79f08167fb55b478829434be84d919c08c888563e0abbdeb66bc19cd3e82457f" => :mojave
     sha256 "4602a32c2a373ed97de8fd36bf1e998299682d45e465af39026a32a3a06fe574" => :high_sierra
