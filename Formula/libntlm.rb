e5f318d38cf45c152d37b5a77f769c979902b021
Jonathan Chang <me@jonathanchang.org>
2020-11-15 10:17:54 +0000

    libntlm: update 1.6 bottle.

--- Formula/libntlm.rb
+++ Formula/libntlm.rb
@@ -12,6 +12,7 @@ class Libntlm < Formula
 
   bottle do
     cellar :any
+    sha256 "c8662388829c4cacb52f1c948b8ae2ba19ad8c28ef53cda651b0e5cf8c0529fe" => :big_sur
     sha256 "7e34bd216191b40a86075d825c98c929d4f61842be989b605caba169ac68c999" => :catalina
     sha256 "a7de6d5c400b83a6f5e18423d396321aa45fb1a12dd1577df04389a7379e743a" => :mojave
     sha256 "e9b9b29b0f54e3349be1fad6f281d7ed0b972deaab07a0febe2ab75a73028ea5" => :high_sierra
