855ccf3279d6eca60265b3fc5aeec5c4c3349eeb
FX Coudert <fxcoudert@gmail.com>
2020-11-15 05:25:06 +0000

    tidyp: update 1.04 bottle.

--- Formula/tidyp.rb
+++ Formula/tidyp.rb
@@ -10,6 +10,7 @@ class Tidyp < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "7698a00e976bc8e04d0cefa0450713f73c658e694400fc107b161aa1184b2d72" => :big_sur
     sha256 "e9529c4cb157eb48b5d3a4cde35c4a8f1994496290a5a308b7feacd4b5bc58bf" => :catalina
     sha256 "b48c3587cde0cbc77ff07e9cb6849dcd3a985d8360ab5e8e7a7d5f0691e5d68b" => :mojave
     sha256 "267b4c383278baa37d4bab8e10aba1ee73d2eba642332414fad77d262b602099" => :high_sierra
