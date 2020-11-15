3fca2ffdc5131936da1b130b2e3da48c30852438
Jonathan Chang <me@jonathanchang.org>
2020-11-15 08:57:51 +0000

    anycable-go: update 1.0.2 bottle.

--- Formula/anycable-go.rb
+++ Formula/anycable-go.rb
@@ -13,6 +13,7 @@ class AnycableGo < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f41de1cc84bba5012e16fd2373b0f6db6781d5196506c6933a2e3eed7ff068c3" => :big_sur
     sha256 "dd4aad7525d8582e96f3d782b1f541bd12653b393a39b04d34b83401f007dff8" => :catalina
     sha256 "4584067882cb44c1af66c66e5730f040d6194ede81c6436d2622e3a2ddfae682" => :mojave
     sha256 "4c88aa03c10d9f6d45374d7fddcc4d6763790528567c332432f476a4eb05c314" => :high_sierra
