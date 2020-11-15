f3a964c2045cf810ceaf7dea40ceb0cbc3feb7d4
Jonathan Chang <me@jonathanchang.org>
2020-11-15 09:10:30 +0000

    berglas: update 0.5.3_1 bottle.

--- Formula/berglas.rb
+++ Formula/berglas.rb
@@ -8,6 +8,7 @@ class Berglas < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "911eb1ca9babcea29286f8ee235a6f785a34dea9d43ac44684663a487eb3d775" => :big_sur
     sha256 "2c7a65e0e04b84106700c9228739c4e1f8e7939b66f5770819db0c044ed3b60f" => :catalina
     sha256 "3ba9898e75191e458e9da01dcbe835aefd82b167e7a2af2ab02a1fdfbbd7f8f8" => :mojave
     sha256 "63db59c2436d167b2c6315ed23ad9e3e2d814d37b3e0460d4ac53c7608f99dbf" => :high_sierra
