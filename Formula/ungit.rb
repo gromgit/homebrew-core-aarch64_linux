0ac26d1a63d522c83bf7a331800a79a82531f244
FX Coudert <fxcoudert@gmail.com>
2020-11-15 05:24:52 +0000

    ungit: update 1.5.13 bottle.

--- Formula/ungit.rb
+++ Formula/ungit.rb
@@ -13,6 +13,7 @@ class Ungit < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3ced4848318d45a0cda134de736985c0c5f951537e0b799eea21ec4e65948166" => :big_sur
     sha256 "4532b8cdcebb312719952234b74c131e33300764a32904c5e92d3ffac89064a4" => :catalina
     sha256 "48ed6d0ba522cf36e0e6ec25cc03fba0305d52f9200911242387061cbffe5a56" => :mojave
     sha256 "16fc839b21c419de4b446bb7df046b407fd70125fc3202af5c1ce31f34cac643" => :high_sierra
