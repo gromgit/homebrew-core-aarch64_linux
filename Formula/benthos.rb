a5fd302e90d21c3debbf648804dbe688aa823606
Jonathan Chang <me@jonathanchang.org>
2020-11-15 09:11:03 +0000

    benthos: update 3.32.0 bottle.

--- Formula/benthos.rb
+++ Formula/benthos.rb
@@ -7,6 +7,7 @@ class Benthos < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8d86bcc5e3a209c916777dbdce85e727bf5d715fad2769785dc21f1e088c26b2" => :big_sur
     sha256 "86fb876c245c0d4ce5dbdb5fb449f9089c4cfce455c772198738711b86ba5b9a" => :catalina
     sha256 "287ff527915c06f474b8920aaf6939485cff0c62e3265d487d5740c68bf26cd1" => :mojave
     sha256 "33e9a367c8de7195896ebad2db7fd791d45cd8de105fa6ff1395c5dd3739392f" => :high_sierra
