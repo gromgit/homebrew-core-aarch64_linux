aee6926d20c846e74785749cc05722edd7b20376
FX Coudert <fxcoudert@gmail.com>
2020-11-15 15:20:59 +0000

    typespeed: update 0.6.5 bottle.

--- Formula/typespeed.rb
+++ Formula/typespeed.rb
@@ -10,6 +10,7 @@ class Typespeed < Formula
   end
 
   bottle do
+    sha256 "a1624c4d927fda389aceec074f743e73bd10417059764d480141a88fab23bb82" => :big_sur
     sha256 "cff9da11f7441f1ff4db7cbfa57f0711ff0bbe08a80ee7067021c619bc01cb06" => :catalina
     sha256 "49c54c15fa8204ca5ae373f0a1995c01b7b6e24de0ab0af7d8081e9f3b229258" => :mojave
     sha256 "70fe987eeaabcf8e94996d56a478c1aac14781f2475337476ff2dc87543bb602" => :high_sierra
