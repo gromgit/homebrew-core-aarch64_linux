6a9449c41309e6f313af3f53ba29eaee73b6eb5b
FX Coudert <fxcoudert@gmail.com>
2020-11-15 16:57:04 +0000

    mscgen: update 0.20_3 bottle.

--- Formula/mscgen.rb
+++ Formula/mscgen.rb
@@ -8,6 +8,7 @@ class Mscgen < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "662b9da17d8c911e9d24be48def9a222e7068386c0b482eca48248d127467e14" => :big_sur
     sha256 "315656cf5f9d72907591b4c8a91e635e6aa9b2116cadefe9fdd76db4cff7ae87" => :catalina
     sha256 "1f194eb67147772b362ae5446b2e369b35ee9ffa935c8e22d37cdb4c1364349b" => :mojave
     sha256 "0f125ab1fbaf04c670f252f05358771f1663b3fc59857bcfd855bbb52e01f88b" => :high_sierra
