15d8ed09796e1974257df9fb2dffc8284c7ffc6e
Jonathan Chang <me@jonathanchang.org>
2020-11-15 10:51:08 +0000

    libhandy: update 1.0.2 bottle.

--- Formula/libhandy.rb
+++ Formula/libhandy.rb
@@ -6,6 +6,7 @@ class Libhandy < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
+    sha256 "9eeeb58cdefdb836005b138a289f98f56da15811c0f0b780f40fbed77d203aa7" => :big_sur
     sha256 "3087f66709a5278ba808feb87e6567bfea461896b3691b30f9d07f5f91b93658" => :catalina
     sha256 "7c844577d6f7b7093ae9f2c0569140d8392e6ad9790274a643226290d2e201d0" => :mojave
     sha256 "aff98ff6a6b9d071a635cbe294c675b4f7fd2c155f737bafa0d787501714e847" => :high_sierra
