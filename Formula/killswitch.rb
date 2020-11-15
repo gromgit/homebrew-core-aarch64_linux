df8adafc0f27b5fc277964b76ebe293d027113ba
FX Coudert <fxcoudert@gmail.com>
2020-11-15 17:04:19 +0000

    killswitch: update 0.7.2 bottle.

--- Formula/killswitch.rb
+++ Formula/killswitch.rb
@@ -7,6 +7,7 @@ class Killswitch < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8d3e4381fb6137e38fd394941e40f1c0fab743c7b4bfef3c2ecb43679b6ab03b" => :big_sur
     sha256 "3e00a8591a897509a48c65d76e529c6f4ef6fc910ebb762c8e5e7f54e2e03a43" => :catalina
     sha256 "4cdbf573342205befe4e908ae318125be61850d2346c5ca649cdd867067eab63" => :mojave
     sha256 "82a98dbef512e928dfcee02d0c7c50889856ce88740645ec1af0fcac7edfab12" => :high_sierra
