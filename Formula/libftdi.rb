5230a13aa81cfd4d7c585add6e02f5fd7657ad4c
FX Coudert <fxcoudert@gmail.com>
2020-11-15 21:14:05 +0000

    libftdi: update 1.5 bottle.

--- Formula/libftdi.rb
+++ Formula/libftdi.rb
@@ -11,6 +11,7 @@ class Libftdi < Formula
 
   bottle do
     cellar :any
+    sha256 "17e8dfd27de8a962633117a3ec780dbbe416206bd66b315ead3a5e5ed5caee27" => :big_sur
     sha256 "2ac29fc67dacd7c6e2c73e93114019d0df07aaeac7678c74402289d91d128d00" => :catalina
     sha256 "e267d6e573aad2f1372f5731bf2be30177d5b4feb6c30b0ac96b8933f545983a" => :mojave
     sha256 "5610431987b6b03db32ebed2c24b5007ffad77343cee35bfd23ed93470539846" => :high_sierra
