25005b1eda826fabbd8d01d44e5da4a065200495
Jonathan Chang <me@jonathanchang.org>
2020-11-15 09:31:47 +0000

    croc: update 8.6.5 bottle.

--- Formula/croc.rb
+++ Formula/croc.rb
@@ -8,6 +8,7 @@ class Croc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "099c5500a3d772c70da94b03540348191c342e7470e3406296a2873fc8b957e3" => :big_sur
     sha256 "98249aa16eaf16722abd3a0cc6c98c4499d26b644909ee8879e01a3fac878ffa" => :catalina
     sha256 "71fec2667e693fbdc000d8ac4bc99f1b1898101d963c84247f23add13c1af13e" => :mojave
     sha256 "c294ca113397098cf77867dfbd042fc69ebc7fa52c418b08cb04049a730b00fb" => :high_sierra
