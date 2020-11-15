48d04c2fdbf841b1e613e02401c77924b9c70814
FX Coudert <fxcoudert@gmail.com>
2020-11-15 20:18:52 +0000

    carrot2: update 4.0.4 bottle.

--- Formula/carrot2.rb
+++ Formula/carrot2.rb
@@ -9,6 +9,7 @@ class Carrot2 < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "390e44722413183c996b9e5f777fd0e4aa3ba41e743c1271925aeb1a0563f447" => :big_sur
     sha256 "7dd4787aef0833a6147df58a89d9c3183ac96cddc724f6ae04ac39bb7b5e95f2" => :catalina
     sha256 "4499a0e7e0bc182c03b01aafb70e189c7fc6dbb04308ba92ba2dc2ffa8904202" => :mojave
     sha256 "952d43fdc6efa2a79d7cd206561f6392ef65f6f53699f584d26e5b9e8b7f8dde" => :high_sierra
