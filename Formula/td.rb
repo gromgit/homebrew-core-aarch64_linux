b321b39b80ab9cb7b374425f38385b19d79ecc2a
FX Coudert <fxcoudert@gmail.com>
2020-11-15 02:03:13 +0000

    td: update 1.4.0 bottle.

--- Formula/td.rb
+++ Formula/td.rb
@@ -7,6 +7,7 @@ class Td < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "42d31fcb00186c05db503b8bd8468a66ff61bb66d14ca286a07c158cd1a23511" => :big_sur
     sha256 "e2fdce36bbab98d388fcc14c448ba87cb4cb77e10a5e20f083128c8cfa2ad367" => :catalina
     sha256 "b77b89e4f9b100d834c786d0e60c17aa80a51ed452ffab032589837c11c00714" => :mojave
     sha256 "91a8beaacb3c67dff0dd12a717c10868df7874d9668a043eb658be4eb180390a" => :high_sierra
