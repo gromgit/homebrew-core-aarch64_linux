b2178c6832c96e3742c0710de65a8ab66d318b38
FX Coudert <fxcoudert@gmail.com>
2020-11-15 15:04:45 +0000

    kamel: update 1.2.0 bottle.

--- Formula/kamel.rb
+++ Formula/kamel.rb
@@ -16,6 +16,7 @@ class Kamel < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "609a4700b9787fbdd4b296d4b2b31b413a5c4afca1ce03ce4f57c1b58ed5c55f" => :big_sur
     sha256 "574dbbd1475b4d45421dd5e6d2705612eaf011c12f24fe0899e47490b51e4399" => :catalina
     sha256 "43663df873f8d797f4a460a2aee38abe8e4fd221111aaa0b53fda75e28f1f4e4" => :mojave
     sha256 "8d546456d8fe9f2435caee683b44c458c61e3ce86957514025ebe90495660edc" => :high_sierra
