278a8c3cbe805353f7f98c862ecf492051839d6f
FX Coudert <fxcoudert@gmail.com>
2020-11-15 06:32:49 +0000

    antibody: update 6.1.1 bottle.

--- Formula/antibody.rb
+++ Formula/antibody.rb
@@ -7,6 +7,7 @@ class Antibody < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "68b409c42eeab15437a9c64a55e13f69c37f6e085bcff794bb1f9a8ca6419e98" => :big_sur
     sha256 "572351da6247daf6bf29afbdcc8ff10c4fe47e9e413c2ae0df0dd249e855599d" => :catalina
     sha256 "c33467a9d42a9c767bd2d3382937e9f1dcf9bce2cb45fe3de6adb736ae2d6e89" => :mojave
     sha256 "7af2bd8779f129597713ebd6155d493616f4ed4b2344cac9db84191b01f3110c" => :high_sierra
