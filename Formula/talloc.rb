a7838aebc6281d947b4797bb05de598a8d4c074d
FX Coudert <fxcoudert@gmail.com>
2020-11-15 12:34:10 +0000

    talloc: update 2.3.1_2 bottle.

--- Formula/talloc.rb
+++ Formula/talloc.rb
@@ -12,6 +12,7 @@ class Talloc < Formula
 
   bottle do
     cellar :any
+    sha256 "1ec1b32b844e7c4b8c00d67f400b9140a1d79637924e4cf198b306672c0130fa" => :big_sur
     sha256 "f9b77cca02b56ffa431f45d49d090c21f1a762a47149d6e92d57884772dd0875" => :catalina
     sha256 "e0a573fbcce5380f8a31be002f14651f8f9e0cf43b254fe50100ae9134ff319a" => :mojave
     sha256 "c4c4ebb7d3e92b05abe832134c3f6745e8279a0585963e5a9967471b6c3a753a" => :high_sierra
