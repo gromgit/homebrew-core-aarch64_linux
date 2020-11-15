29a38d13657a2e47a1fb2b51a106da6dafa591dc
FX Coudert <fxcoudert@gmail.com>
2020-11-15 03:06:29 +0000

    unshield: update 1.4.3_1 bottle.

--- Formula/unshield.rb
+++ Formula/unshield.rb
@@ -8,6 +8,7 @@ class Unshield < Formula
   head "https://github.com/twogood/unshield.git"
 
   bottle do
+    sha256 "fe9c9710de87f7734c84e3454ba88a2c35b859fb8b8dfae56968c3c0bb4e76fc" => :big_sur
     sha256 "d64e0c93743d7d50858bd5c46c76b8fa79183b5ee4643361202f53378a88cc05" => :catalina
     sha256 "ec5db176e7f9557645cfdb63062802d37a8e516f39f1e53037e37ed398992b3b" => :mojave
     sha256 "c68a5391b55e5101979c69d174160564d88edc7263afa140fd69ce289c6662ed" => :high_sierra
