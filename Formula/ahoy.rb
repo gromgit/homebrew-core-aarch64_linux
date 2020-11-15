4effc44913399f8a2098b9e76c8f5bae18478c6a
Jonathan Chang <me@jonathanchang.org>
2020-11-15 08:52:32 +0000

    ahoy: update 2.0.0 bottle.

--- Formula/ahoy.rb
+++ Formula/ahoy.rb
@@ -7,6 +7,7 @@ class Ahoy < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4d796f66f413571680fa23532f3f1384670daa4cff4d91e5b05ff3b0ca478462" => :big_sur
     sha256 "fb8e03826f9109edc8bb5b1e0c7c9d8054d76e364bca13e0afdf7a23b022a817" => :catalina
     sha256 "eabaf2c0faa64d878f3fd552823b9d5103e0755ba5f3120628e605964fc93257" => :mojave
     sha256 "93db889b646270f7a92d32f649c9e256e4e90cfa006a04c614334f28557ce7ca" => :high_sierra
