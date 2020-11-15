f5b4673860beca70be2afee897a8bbb3767be35f
FX Coudert <fxcoudert@gmail.com>
2020-11-15 14:33:30 +0000

    clhep: update 2.4.4.0 bottle.

--- Formula/clhep.rb
+++ Formula/clhep.rb
@@ -12,6 +12,7 @@ class Clhep < Formula
 
   bottle do
     cellar :any
+    sha256 "8d74340c61103f296ca7669e6640f9e5d31cc738594c677985e783e72e3227e0" => :big_sur
     sha256 "029450b4260d87bffc05d876e62a5fb51b285757c8e589debc609728e71c0da2" => :catalina
     sha256 "abf5e7c7c0490a2f82eaf5654670db7e5b0f38df0e9d739d28d233343e54f129" => :mojave
     sha256 "6a643d6bc0031270e6a85f4cb2f7e66e18e22c9925988f191384ec3dea90f1e7" => :high_sierra
