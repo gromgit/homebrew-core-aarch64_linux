42ec0ec86782baae67ed3b189629545e7a9fc974
Jonathan Chang <me@jonathanchang.org>
2020-11-15 10:30:40 +0000

    libproxy: update 0.4.15_3 bottle.

--- Formula/libproxy.rb
+++ Formula/libproxy.rb
@@ -8,6 +8,7 @@ class Libproxy < Formula
   head "https://github.com/libproxy/libproxy.git"
 
   bottle do
+    sha256 "8ce22907b3d2e06edc6f1d244322ecb1362da13efb946079314765e34459bc36" => :big_sur
     sha256 "f8d85ff96d4da5414b766d3515c837a7c836bbf6f1d491f2c151a8f13a4a684d" => :catalina
     sha256 "17d3a321a78e6eb8b5d9fdd2c5a9abc02867cab29b51f30015d4a191030479e2" => :mojave
     sha256 "9097c3a2158d8b6dc2a4c6413cead843a81c1125e03a787a7edc21a8e3866f6f" => :high_sierra
