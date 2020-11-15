e5755172979a1a2a59de0ec2af17dbbfff48da77
Jonathan Chang <me@jonathanchang.org>
2020-11-15 10:19:34 +0000

    libolm: update 3.2.1 bottle.

--- Formula/libolm.rb
+++ Formula/libolm.rb
@@ -7,6 +7,7 @@ class Libolm < Formula
 
   bottle do
     cellar :any
+    sha256 "0634af436ccc158ea8ba3e15a53df9b3677b88db950691aaf1174189a925c8a5" => :big_sur
     sha256 "850ca4e75b42221ea5ec4ed0ffb845b9f9d032f711b6ceb600d85813e81d50a3" => :catalina
     sha256 "09cad39ec7953a8f3d0c9848673490624153107f1fce18db4a489147fa1170bf" => :mojave
     sha256 "efe5613af63cc93f582b0e6f6dbe81ec9863d49dc347bf354e202ed8c9ed3ed6" => :high_sierra
