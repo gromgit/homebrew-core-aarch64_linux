674151dba2cdaf057a1f7e171236af85a642fdd7
FX Coudert <fxcoudert@gmail.com>
2020-11-15 06:00:25 +0000

    swig@3: update 3.0.12 bottle.

--- Formula/swig@3.rb
+++ Formula/swig@3.rb
@@ -5,6 +5,7 @@ class SwigAT3 < Formula
   sha256 "7cf9f447ae7ed1c51722efc45e7f14418d15d7a1e143ac9f09a668999f4fc94d"
 
   bottle do
+    sha256 "d0d9561bf105072f30a93f3914d05d58422f13c578de51f760e3822405dd5c36" => :big_sur
     sha256 "f50becfc883397db62530bab927dcf4b5a4db5f0bcbb2839d5ac795fb924c586" => :catalina
     sha256 "28e5c0a5e8aac0c0d5f58e4dd69c590f57d3a450d92aa35b18aee037ab7d8b60" => :mojave
     sha256 "730bd728981cc1534664ef35d08d0b285e79756c286913d868af6afa43f60f4d" => :high_sierra
