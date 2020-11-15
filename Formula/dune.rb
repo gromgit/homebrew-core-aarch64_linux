8e8290b58b4d45805d66af82b0ae03bb2899ade9
FX Coudert <fxcoudert@gmail.com>
2020-11-15 15:02:23 +0000

    dune: update 2.7.1 bottle.

--- Formula/dune.rb
+++ Formula/dune.rb
@@ -8,6 +8,7 @@ class Dune < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7f5a035846320482950c3f5763a0eca689de8c9b22c1c3a09583baf7cc6f8cf9" => :big_sur
     sha256 "a915f93d1b30931e577598deb935ca2326c523836b6fa0795db01bfd391911fe" => :catalina
     sha256 "4c35344cfb436b2238bf46846b382a4777a424da0b9ff1b769ed37c0f3f77520" => :mojave
     sha256 "b93f2bfaf0038552e9b7fba63aaf7ddb0cc53d464411f83d019b7044d83c49b0" => :high_sierra
