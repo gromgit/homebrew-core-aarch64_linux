f4b3ad821bc1a2ed3561223a96716d61881980f0
FX Coudert <fxcoudert@gmail.com>
2020-11-15 13:42:02 +0000

    gpsd: update 3.21 bottle.

--- Formula/gpsd.rb
+++ Formula/gpsd.rb
@@ -12,6 +12,7 @@ class Gpsd < Formula
 
   bottle do
     cellar :any
+    sha256 "a68031d9cbef4b9cfbdfed566b8d0e556ae8188cedd6f505ca65314588bbc7f4" => :big_sur
     sha256 "e41f44df2cf96b33b2f62e65ff2ef9154d872bc8fac88b3bdaeb503246d77c2b" => :catalina
     sha256 "caafc4aea3632fdbe8df1ce265c025430a816d2ad7c26f973c254887ec6a2a8f" => :mojave
     sha256 "bc0775e450c0129fd71a4abd163a7645ac9b3e1698009b2735fafeb838e09e79" => :high_sierra
