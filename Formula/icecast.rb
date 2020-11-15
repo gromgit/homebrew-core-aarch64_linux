600f2829bf159f576fa453c08a8249ed1d79e072
FX Coudert <fxcoudert@gmail.com>
2020-11-15 03:22:05 +0000

    icecast: update 2.4.4_1 bottle.

--- Formula/icecast.rb
+++ Formula/icecast.rb
@@ -12,6 +12,7 @@ class Icecast < Formula
 
   bottle do
     cellar :any
+    sha256 "170c2fefda083f993451d4a6ccd6349ab6742ed3581c9610730cf88ae7083fb1" => :big_sur
     sha256 "824f7d295c28fbdb17da3015b4e4d6ca76be536f6bf81e98d5312dd7b9a095cd" => :catalina
     sha256 "3fb3b8c1e995a9c39a56ecd91a42cc0187f3bb2541c1abb4d0b7fc922da9cb95" => :mojave
     sha256 "a498fdc056b3afbb14b3138586f5dca3b0c1bae523c909c0b9383d5c5f4b02ca" => :high_sierra
