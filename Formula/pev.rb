e8ef8cc6dcd972b37acba4d754faad79c15de0bc
FX Coudert <fxcoudert@gmail.com>
2020-11-15 16:58:27 +0000

    pev: update 0.80_1 bottle.

--- Formula/pev.rb
+++ Formula/pev.rb
@@ -12,6 +12,7 @@ class Pev < Formula
   end
 
   bottle do
+    sha256 "57ebf53a0c9647eca2f604ef68354c207f7baf3929affa40b5e1147771dd7245" => :big_sur
     sha256 "594049ef545f762b9f6d3cad098fa23971c0b84a3623799004c83e62a7303779" => :catalina
     sha256 "e4d191b795eebb97ee0bb6a3122bf45f1c2f05c7b192381e712d96d71cd4ffb0" => :mojave
     sha256 "70c993e146e9d78b9d8d129f06c4a67071f110d286d87fccf1132a7022833a1b" => :high_sierra
