9eaea12a67d4ed63bed0d31e0b9dba35b1694150
FX Coudert <fxcoudert@gmail.com>
2020-11-15 02:59:40 +0000

    mmctl: update 5.29.0 bottle.

--- Formula/mmctl.rb
+++ Formula/mmctl.rb
@@ -9,6 +9,7 @@ class Mmctl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "987612c5e599cc1a91afabc2629f258bc2f6703382e9f49a4a2f841fb2361154" => :big_sur
     sha256 "e8286b3dca1fc493717c788e4b4c826966ad54bb09b46d2138cc0f1086a0797c" => :catalina
     sha256 "865ba6d1a37ce690118b408c001310b31ee20f7d86e0c0cf5c2862f819512714" => :mojave
     sha256 "bb55ff1ce984bab1079145f84fb1cd5c37ffe29538451f582ee9eaba9571569d" => :high_sierra
