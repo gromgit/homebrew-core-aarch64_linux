efeb7bc98cac8ebad6b6ec5e1b73294bcb4e86fa
FX Coudert <fxcoudert@gmail.com>
2020-11-15 20:58:12 +0000

    tlx: update 0.5.20200222 bottle.

--- Formula/tlx.rb
+++ Formula/tlx.rb
@@ -7,6 +7,7 @@ class Tlx < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "bd7aca4d147c132cd31909e0e99f6eb6192d63ba98805d1ae5da6b7ea04826e1" => :big_sur
     sha256 "c27858a2595d4fe9444821160e85aa6924fcc7194e13baadd5fda0b79252b9a1" => :catalina
     sha256 "5038cd9dff7968390f0e4208059c02a667fb9c3308ce88f444bd57ef60bd8895" => :mojave
     sha256 "9a81855db3041742ac4e6ae96c3bc8bc9f15e0dc30436afbcbbf36bace3ef633" => :high_sierra
