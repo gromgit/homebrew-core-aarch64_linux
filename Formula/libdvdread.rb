d1f1faa44f1160b367db6bd89cba4f3df37dbc7b
FX Coudert <fxcoudert@gmail.com>
2020-11-15 05:27:02 +0000

    libdvdread: update 6.1.1 bottle.

--- Formula/libdvdread.rb
+++ Formula/libdvdread.rb
@@ -7,6 +7,7 @@ class Libdvdread < Formula
 
   bottle do
     cellar :any
+    sha256 "732ca22426ca97c3b96a975a3ee25ad12b74de870f58b506f36c7e9b7e91d328" => :big_sur
     sha256 "83bebe58015f6f34973afa003934f183e7ac9202f5e579cfd12536f9ceac1719" => :catalina
     sha256 "7405838fee2b93209c2bd0834db89c2a2334a94f7d368feb87599da1b08062f6" => :mojave
     sha256 "c881a8c1c872d922f45bf8a692b9d79b5f6ade1a2f4a48d470d05491bc017436" => :high_sierra
