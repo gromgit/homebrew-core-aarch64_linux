5c60ba66305092654f0e3c28921117971106aed4
FX Coudert <fxcoudert@gmail.com>
2020-11-15 07:40:42 +0000

    zenity: update 3.32.0_1 bottle.

--- Formula/zenity.rb
+++ Formula/zenity.rb
@@ -10,6 +10,7 @@ class Zenity < Formula
   end
 
   bottle do
+    sha256 "bf8f705cbdadd17fca250b94df00f7a26c345c7476234a312819add6d0534e3d" => :big_sur
     sha256 "6bbf833a5a7d71346a5391fae436c2c46f530f0f5b9ac5d57330601b3456db49" => :catalina
     sha256 "cef54fcd5601eb5dd3b563d1a09a6cd83654a2fa46e4a83a3d3c6e6a356fe29a" => :mojave
     sha256 "36cf68d4838890e8d9122109464548a4630da0b06dcf6d4f0976ccf58b99dde2" => :high_sierra
