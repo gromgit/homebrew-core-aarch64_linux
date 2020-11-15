22ecfb402a099e53f1c0972af4572fa2e957ba95
FX Coudert <fxcoudert@gmail.com>
2020-11-15 20:28:17 +0000

    spim: update 9.1.22 bottle.

--- Formula/spim.rb
+++ Formula/spim.rb
@@ -7,6 +7,7 @@ class Spim < Formula
   head "https://svn.code.sf.net/p/spimsimulator/code/"
 
   bottle do
+    sha256 "25c74ff403edf01eda62cb8b6ce569fd2c5e84319ca5e6189ab6a3109d09ba83" => :big_sur
     sha256 "553aee29312b5b491d20c139652f87d8bd1547abd078285c5c80a13e02a868ff" => :catalina
     sha256 "429ed6272e9255d16227b58bbc405c58d19ecb360540d2d228a91029b62506ab" => :mojave
     sha256 "dfb4e24f378665fee30af8a3c362b1bc13e83b33196b66b4102c400fcee99b2e" => :high_sierra
