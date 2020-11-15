a59039b5fcb0290d15d7f3dd605e6d1a3dec01c7
FX Coudert <fxcoudert@gmail.com>
2020-11-15 00:29:59 +0000

    libxi: update 1.7.10 bottle.

--- Formula/libxi.rb
+++ Formula/libxi.rb
@@ -7,6 +7,7 @@ class Libxi < Formula
 
   bottle do
     cellar :any
+    sha256 "ef121c80170a695096e8505424820959c31a757659d282507b3933ab109a6a96" => :big_sur
     sha256 "a0fddd9dcb30077a6a2736540bf2d1cee8bf44fb8291d8b6ad1207086bf6f63e" => :catalina
     sha256 "c6745a3e9274dd78e9c4372ebca168ca1ae83600fd5c5ac68a59cc4dc60f3d6a" => :mojave
     sha256 "641cd78fbe4d389ce51c071deb60b62da482aa623e49bf0fe1b765b67d1380ac" => :high_sierra
