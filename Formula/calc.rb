234da62ae77d0707756f913e98e8e95c7fee239e
Michka Popoff <michkapopoff@gmail.com>
2020-11-15 23:43:35 +0100

    calc: set sdk inc dir only on mac

--- Formula/calc.rb
+++ Formula/calc.rb
@@ -3,6 +3,7 @@ class Calc < Formula
   homepage "http://www.isthe.com/chongo/tech/comp/calc/"
   url "https://downloads.sourceforge.net/project/calc/calc/2.12.7.1/calc-2.12.7.1.tar.bz2"
   sha256 "eb1dc5dd680019e30264109167e20539fe9ac869049d8b1639781a51d1dea84c"
+  license "LGPL-2.1-or-later"
   revision 1
 
   livecheck do
@@ -25,17 +26,21 @@ class Calc < Formula
     ENV["EXTRA_CFLAGS"] = ENV.cflags
     ENV["EXTRA_LDFLAGS"] = ENV.ldflags
 
-    readline = Formula["readline"]
+    args = [
+      "BINDIR=#{bin}",
+      "LIBDIR=#{lib}",
+      "MANDIR=#{man1}",
+      "CALC_INCDIR=#{include}/calc",
+      "CALC_SHAREDIR=#{pkgshare}",
+      "USE_READLINE=-DUSE_READLINE",
+      "READLINE_LIB=-L#{Formula["readline"].opt_lib} -lreadline",
+      "READLINE_EXTRAS=-lhistory -lncurses",
+    ]
+    on_macos do
+      args << "INCDIR=#{MacOS.sdk_path}/usr/include"
+    end
+    system "make", "install", *args
 
-    system "make", "install", "INCDIR=#{MacOS.sdk_path}/usr/include",
-                              "BINDIR=#{bin}",
-                              "LIBDIR=#{lib}",
-                              "MANDIR=#{man1}",
-                              "CALC_INCDIR=#{include}/calc",
-                              "CALC_SHAREDIR=#{pkgshare}",
-                              "USE_READLINE=-DUSE_READLINE",
-                              "READLINE_LIB=-L#{readline.opt_lib} -lreadline",
-                              "READLINE_EXTRAS=-lhistory -lncurses"
     libexec.install "#{bin}/cscript"
   end
 
