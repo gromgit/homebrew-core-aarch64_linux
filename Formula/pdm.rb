5d09a6b6ef0d764723f445c94ef75cb56a12caaf
FX Coudert <fxcoudert@gmail.com>
2020-11-15 15:18:33 +0000

    pdm: update 0.10.2 bottle.

--- Formula/pdm.rb
+++ Formula/pdm.rb
@@ -10,6 +10,7 @@ class Pdm < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "fb2f69c797f02f04a28d911266ef5c956224de49ad6002bd33068b8fe1400ee8" => :big_sur
     sha256 "8bfb03d58e43a247c1be73e99f4c1167661c98ec30f33262184ad4e2fdcb33cf" => :catalina
     sha256 "44daa5245e235b777f195b64364827aa698ccef2e55abd2ec6cb6b6371808868" => :mojave
     sha256 "dcb87c9d91eeeae80e2e53f132e065561471137ea4a650fda0c5d4e64a0fa0c6" => :high_sierra
