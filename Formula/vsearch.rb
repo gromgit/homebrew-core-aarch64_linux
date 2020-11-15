a26a572c58bcb10dee63b1cc67b762c4c6f2d12e
FX Coudert <fxcoudert@gmail.com>
2020-11-15 16:38:19 +0000

    vsearch: update 2.15.1 bottle.

--- Formula/vsearch.rb
+++ Formula/vsearch.rb
@@ -7,6 +7,7 @@ class Vsearch < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f93eaf8d2c94693f7eaf185790604b49e2dae03a7a7569c031e5a73a2f1fdd15" => :big_sur
     sha256 "25c75ec8b1a1dc243b11145ba0cddcfb74d910aa682a20b0faa7d297f12de945" => :catalina
     sha256 "54adb18eb8744a4c6cd3c827c5088962e6587e4bc2becc1c296ce52304d62be2" => :mojave
     sha256 "c308c67a02b669b546cb179df8d3ec7b90089ccfc40fb4bdc11ea7e91994b55f" => :high_sierra
