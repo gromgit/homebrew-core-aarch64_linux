d0ce4a086402f34483f2cf462b32ff2765405275
FX Coudert <fxcoudert@gmail.com>
2020-11-15 06:01:10 +0000

    keystone: update 0.9.2 bottle.

--- Formula/keystone.rb
+++ Formula/keystone.rb
@@ -8,6 +8,7 @@ class Keystone < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2274b6cf90bf0b1b49d3aa6e0a527911e71438903c228bfd37644d58ac3ad22d" => :big_sur
     sha256 "84cdef2aa8a5697ce2fc62e6ae1316f2dcca6fcd0f92d2bba68b399af9c48440" => :catalina
     sha256 "814feeee85e111a21fdd287fbed3fca3e1cd86be396dcba7612c3e1aec7dd4d3" => :mojave
     sha256 "77740af9b9e48baaf0a7d1dc4d74b883c1babbaab6a7e9bf65a00035b59c546d" => :high_sierra
