f233b5e0a429b1b296a61d1c8d33bb2c60cf19c5
FX Coudert <fxcoudert@gmail.com>
2020-11-15 17:01:54 +0000

    cproto: update 4.7q bottle.

--- Formula/cproto.rb
+++ Formula/cproto.rb
@@ -12,6 +12,7 @@ class Cproto < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "74853b756108f1bc8cea4d4f64affa9cbb8b5cd1d95c249c84657e87454830b7" => :big_sur
     sha256 "05936e555dc7f1cbe36cebf67e20de0f7c0404db6cbf1ee23f69aa4a858281db" => :catalina
     sha256 "5123cb4d20d57211cd5348087194fcbaff4f79b0e45317e2d82f82c3c54b42da" => :mojave
     sha256 "ecd7b1cb65d67008fe7b904a24d60df256d4d3d88cf7fff6647adf98db5a411c" => :high_sierra
