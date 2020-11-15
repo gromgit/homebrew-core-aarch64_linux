134525c030a7e9604c3ab951d116c8b008912539
FX Coudert <fxcoudert@gmail.com>
2020-11-15 05:21:51 +0000

    timewarrior: update 1.4.2 bottle.

--- Formula/timewarrior.rb
+++ Formula/timewarrior.rb
@@ -8,6 +8,7 @@ class Timewarrior < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "40d7ce6f5802ca1a755f6a722ebcabc68b574632007d95ef109ad99c2e7a4902" => :big_sur
     sha256 "4c872f73c14a7219179b3f468d5ceb0739b79ace42e69126f9b3549eac9cba94" => :catalina
     sha256 "336d8bbaf618d17901774358a58772579318a405e7020c63583538283b1f2165" => :mojave
     sha256 "c15692ae447c6364eb7c74665e3b9d8acb01be7f31eb0f4ebaf92d7c7dc3f874" => :high_sierra
