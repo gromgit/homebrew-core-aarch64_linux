7f4274989dd67b6c5825926f68356d8cc304a5f8
FX Coudert <fxcoudert@gmail.com>
2020-11-15 05:46:29 +0000

    cdk8s: update 0.33.0 bottle.

--- Formula/cdk8s.rb
+++ Formula/cdk8s.rb
@@ -13,6 +13,7 @@ class Cdk8s < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5a974f74c2510849e093b0286aeb9d48bcadeb67caecb60108396b32314a0dfa" => :big_sur
     sha256 "6a183f35fa611a82475eeb6ce8814bfcd3b06ef0fa26f19830a6e69ca9328426" => :catalina
     sha256 "094a4e184bf640dced7744a154140b6ec87d2c1c62d381ead9d695ad1dc05c39" => :mojave
     sha256 "8fd5ae70cb00cf12f8d9ad75eef53e99f9b3ced3fa0987bbcc88c9fda81313d2" => :high_sierra
