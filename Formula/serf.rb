44b0b25224d3bb3eb61d4027950d41250db5bffd
FX Coudert <fxcoudert@gmail.com>
2020-11-15 03:10:18 +0000

    serf: update 0.9.5 bottle.

--- Formula/serf.rb
+++ Formula/serf.rb
@@ -9,6 +9,7 @@ class Serf < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5ae109c95ab044f550a3e20702eb165d014d618198a7fbf280f9731cc02f9e1b" => :big_sur
     sha256 "9fa6ba9a73d743e404c84088e64ef860dcf3f8e0bc0fbeb74a39437440c0dc72" => :catalina
     sha256 "2bcfffa14b7a86099e6dc3574f1dcece4d125b72f32d8fad6be943a63380da75" => :mojave
     sha256 "78b0abfda4b9f41da7df720f79e93346ad524450e801c3528706232d012cbadc" => :high_sierra
