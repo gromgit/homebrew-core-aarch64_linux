5f686e3b7b59cb6e9eb4111bddcd53ec6780b41f
FX Coudert <fxcoudert@gmail.com>
2020-11-15 16:56:36 +0000

    gau: update 1.0.7 bottle.

--- Formula/gau.rb
+++ Formula/gau.rb
@@ -7,6 +7,7 @@ class Gau < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0ae2d716079e4c4d418126c7396446aafc714340368b4fb2ebbd29f63cb2ff30" => :big_sur
     sha256 "314c4c8636714511a7d09c14a68bf4e60d5096a3e8121863365f35843891ef57" => :catalina
     sha256 "2ab669a1b9e3321a5c9be26765e62625fb835cf253b7bd3fb702e32036f2c7d8" => :mojave
     sha256 "1030ebef25ac3961a59c9794a16697d894c084d14933e7df7201d4c99313705b" => :high_sierra
