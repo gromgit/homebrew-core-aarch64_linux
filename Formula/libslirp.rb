7fc1d5f8218722ec134846a24699c567da570207
Jonathan Chang <me@jonathanchang.org>
2020-11-15 11:02:34 +0000

    libslirp: update 4.3.1 bottle.

--- Formula/libslirp.rb
+++ Formula/libslirp.rb
@@ -7,6 +7,7 @@ class Libslirp < Formula
 
   bottle do
     cellar :any
+    sha256 "51817f70add83d1fb74dd233d683aa88e3d0da1e9f8df5c724347e946c2bad6d" => :big_sur
     sha256 "1b810179f0b4978a6b06f15156b8f4ecddd7b7b8408e61129a11913cf9ac4145" => :catalina
     sha256 "d708a39f70d01586c4d9e6fcccd832f638b07325424ce451418bcaee68688669" => :mojave
     sha256 "a7c30a50420febf21f5969719f1d7d9a0abee04a79586c951799212cb952cb9f" => :high_sierra
