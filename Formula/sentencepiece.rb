2f8cf633ad2278d71a4d830ba41672c6de1ab8b9
FX Coudert <fxcoudert@gmail.com>
2020-11-15 16:37:01 +0000

    sentencepiece: update 0.1.94 bottle.

--- Formula/sentencepiece.rb
+++ Formula/sentencepiece.rb
@@ -12,6 +12,7 @@ class Sentencepiece < Formula
   end
 
   bottle do
+    sha256 "f75c30a42e7ac2e931a8a4b19a17aa3640eef3c2e65b99fa86fb2f6af34f9131" => :big_sur
     sha256 "34eaf54eed2f033c154575c04b517742dea72d9fdba2d80515348acb9afb8793" => :catalina
     sha256 "367c727302ab46993a8604b21f4276d051bb06e17dd91b27252639abee05d84b" => :mojave
     sha256 "81a320ad4d6d5c92493f471735bc50fc38ca580d97dd4c710928e743f48d3841" => :high_sierra
