9d8c06495667c72b6af9ae5823b46e01ae7fbaea
FX Coudert <fxcoudert@gmail.com>
2020-11-15 13:29:26 +0000

    aha: update 0.5.1 bottle.

--- Formula/aha.rb
+++ Formula/aha.rb
@@ -8,6 +8,7 @@ class Aha < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "68281908da802d600716d979b84a47109fabd6770da0cc9a1a689b609b2024b8" => :big_sur
     sha256 "bcd5f7ea0e30795e05719351823769f9a7ac434e57bf09cb738eeef50c0f0f85" => :catalina
     sha256 "b8def8fe2809928ffbf3ae5746f1157bacfef12e720d0eef798b4d77902d8f4f" => :mojave
     sha256 "9de609b23501a93b6fc39422bc51f4b79c31eba3c39272a06f2710aa7e2d6a3f" => :high_sierra
