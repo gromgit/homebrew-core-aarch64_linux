662aab220c530f43723020cc8a2bfe3b0d0366e3
FX Coudert <fxcoudert@gmail.com>
2020-11-15 06:42:08 +0000

    osm2pgsql: update 1.3.0 bottle.

--- Formula/osm2pgsql.rb
+++ Formula/osm2pgsql.rb
@@ -7,6 +7,7 @@ class Osm2pgsql < Formula
   head "https://github.com/openstreetmap/osm2pgsql.git"
 
   bottle do
+    sha256 "b9c7c27b5c1903c04ccbf6f55404369d94475a3f461fc86a7fd3585c054cad6c" => :big_sur
     sha256 "324bdc79fb0fabceb5f605982b13fca817eecd0017736e77481246be2f793015" => :catalina
     sha256 "cacdde87d5fc923df8cd9f6d366d3840facdeea335741b2c82a68d526d59f2e6" => :mojave
     sha256 "9d7c59f529112c64bee0083a28fdc5791a43390b4761ccabf50578abaa6a0da3" => :high_sierra
