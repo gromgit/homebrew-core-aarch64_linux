b17ecdd9cb0bb3536ff546bf4f8aade5f3c44636
FX Coudert <fxcoudert@gmail.com>
2020-11-15 11:32:18 +0000

    terminal-notifier: update 2.0.0 bottle.

--- Formula/terminal-notifier.rb
+++ Formula/terminal-notifier.rb
@@ -8,6 +8,7 @@ class TerminalNotifier < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "91f14694ebce08887492aa75138753cd9ff74977868927b15b52559728280055" => :big_sur
     sha256 "78eff95b7436480521ee68a8581ff2df0c615adefccd279486f2491f1b1c0a4b" => :catalina
     sha256 "9671c602326357b7397248bfb0cf062bc47f19add15b615e512f58545c387c31" => :mojave
     sha256 "f112656234f4100d23cc1a41b96f92a09974360a822c2ec0fb6f9970862c1a22" => :high_sierra
