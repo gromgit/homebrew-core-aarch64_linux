5bb52052fcbb09b198cd06ce05151aabea81e1d6
Jonathan Chang <me@jonathanchang.org>
2020-11-15 08:24:11 +0000

    libkeccak: update 1.2 bottle.

--- Formula/libkeccak.rb
+++ Formula/libkeccak.rb
@@ -7,6 +7,7 @@ class Libkeccak < Formula
 
   bottle do
     cellar :any
+    sha256 "0eb4bb61482f4985919fa6a5a1907090eb1a7c3ac507e384f112fce9c7afc146" => :big_sur
     sha256 "a6c562298b9f840a535e67c4a1af2cd2cee7a64b148867dffbd5c43abe449fac" => :catalina
     sha256 "62bef7ab66b3080369802009fce876879f6a756fc6f2734de0e33a514fa8bf93" => :mojave
     sha256 "802c42ae842ebed113639b7e647da9eb09d8fddde91b9d2938652849f2543a06" => :high_sierra
