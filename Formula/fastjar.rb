7c453d17e7d8ef7d47ff13644a576d0f646122ad
FX Coudert <fxcoudert@gmail.com>
2020-11-15 18:00:29 +0000

    fastjar: update 0.98 bottle.

--- Formula/fastjar.rb
+++ Formula/fastjar.rb
@@ -12,6 +12,7 @@ class Fastjar < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1cbf953373b3d48accd872aa603fa66fb3b96e1ec33d4a17dfe6da2f97ad5cbb" => :big_sur
     sha256 "ee758c76cb694c96ea30cb9e6ac204f2797c78be36610dcdf36c2a75301b5835" => :catalina
     sha256 "2dba61ec801db3d83692b9c9dd26eab247cb4bb6a6d6afc27f059bb6ba6052e5" => :mojave
     sha256 "87b2c870895191b309b595481f73346e763b87c661e64ef35b821e54395d5cc1" => :high_sierra
