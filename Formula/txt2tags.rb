5e819ebb3b55f3d54dbbeb758314319680603022
FX Coudert <fxcoudert@gmail.com>
2020-11-15 17:11:32 +0000

    txt2tags: update 3.7_2 bottle.

--- Formula/txt2tags.rb
+++ Formula/txt2tags.rb
@@ -13,6 +13,7 @@ class Txt2tags < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d6c3f9115b0b82e14d0dd9cf8647c67004c1432e8c4469c8450950bbe29a3a6b" => :big_sur
     sha256 "6d2516f5a7e3ac9dd0b87f14aa21689c98fc85cd04133d61feb57f7b67ca815e" => :catalina
     sha256 "91d15a499c27c63bcd94c13a348cba21412a3dd2d67630033d0704a4399ca769" => :mojave
     sha256 "2e6484c9e6eacb833e0f1eeac952fa88bf0582c33517d3cba587d8540c0748f2" => :high_sierra
