45eaeb94c2b72b66930c059c365c562c930c144e
FX Coudert <fxcoudert@gmail.com>
2020-11-15 07:01:44 +0000

    libmtp: update 1.1.18 bottle.

--- Formula/libmtp.rb
+++ Formula/libmtp.rb
@@ -11,6 +11,7 @@ class Libmtp < Formula
 
   bottle do
     cellar :any
+    sha256 "5ebeb1696d5c7af72cb4a14f905dbde2cd871334ea392e7e8ff0305159c09aa1" => :big_sur
     sha256 "9b305e731b8d8608b688bb5c8bb98192d4879eb944fd4b08c09daadf367b68fc" => :catalina
     sha256 "e4c497e80277170743a4ff8ddde06687a01f3afb053088b921b8399796f630ae" => :mojave
     sha256 "704cd1e718e42dc9284ca020a11c1788d8a222cb8a4ca939d6b289cd17cf86ad" => :high_sierra
