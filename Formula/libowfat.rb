c10e41170fd1b905495b4bd675d14c87f489795d
Jonathan Chang <me@jonathanchang.org>
2020-11-15 10:23:58 +0000

    libowfat: update 0.32 bottle.

--- Formula/libowfat.rb
+++ Formula/libowfat.rb
@@ -13,6 +13,7 @@ class Libowfat < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1a08648fa8307771ae1d5c45da0ddefdfdc20d58b89091a614df74371eebbc59" => :big_sur
     sha256 "2424abb2cccd7f41582ea49ccbee60dbecc436c843d9531c0e7c68c35b9330a4" => :catalina
     sha256 "08041ad3f0edd4b20e6ed1f6c768414aa7241940a14386c1dffd04caa5ef70ca" => :mojave
     sha256 "4740574a0e5184f8b371b1a7571304810b4fb29a92d60cf54979387dab3448c5" => :high_sierra
