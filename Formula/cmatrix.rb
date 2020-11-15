4fa251c9a2aa7cc42baa92d0167c2c217b2f54ea
Jonathan Chang <me@jonathanchang.org>
2020-11-15 08:07:28 +0000

    cmatrix: update 2.0 bottle.

--- Formula/cmatrix.rb
+++ Formula/cmatrix.rb
@@ -7,6 +7,7 @@ class Cmatrix < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "745b7d4d2da66a0c4d159909cb16b0b6a29647a96ac6ae74fcf3b993fd730e6c" => :big_sur
     sha256 "fcc9c366c560e89ee2b4f61d1bdece14379a2c598719fd2eef784564bf9ed677" => :catalina
     sha256 "1b3d3155c87c8f2b788fe18c3e72f04af4e79c609030e3447c3e17f9d3870c7b" => :mojave
     sha256 "2c3b0ce73a2f89ece3783885e44aba5f584268768283e650d8b6552bc00f058d" => :high_sierra
