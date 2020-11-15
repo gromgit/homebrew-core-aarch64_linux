ee5250200085aadc11569dfd9f2570f385c2f587
FX Coudert <fxcoudert@gmail.com>
2020-11-15 06:55:05 +0000

    fio: update 3.19 bottle.

--- Formula/fio.rb
+++ Formula/fio.rb
@@ -7,6 +7,7 @@ class Fio < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c665a3ba50222002863a421fd34d3a7449b63e7e89dff20e3237a63467f2778c" => :big_sur
     sha256 "252dd7cba1c767568b9ecb13fbbd891e1ffe47f590ed126cfea8214ff20333f5" => :catalina
     sha256 "2b4b3372f9ad040eb974ba38ecdde11c08b0328fae71d785e5d0b88c77ecffc3" => :mojave
     sha256 "89e47c70a1cca2e1acf29b97720da6b968348ea93a5e417fdca7ad86d670114d" => :high_sierra
