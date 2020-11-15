8f61d05b8749665d87602d81b8e5ddc432c3006e
FX Coudert <fxcoudert@gmail.com>
2020-11-15 20:15:07 +0000

    whalebrew: update 0.2.5 bottle.

--- Formula/whalebrew.rb
+++ Formula/whalebrew.rb
@@ -9,6 +9,7 @@ class Whalebrew < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5a5c9c6a3132942741bc69ed57081a38570f783f316a5b922d20ee6c16efa4a2" => :big_sur
     sha256 "13a6c59ba4c19570fcccef4b187755a4dfcc26038faba5168a3eb33644ade588" => :catalina
     sha256 "0aeae5e296d00a1b00a53b6a5b9c9aea39731826ea882e3adb51e51bd3fed653" => :mojave
     sha256 "14b39018ed3d1fa076250354d86e3f9296de24bd5a57144cd265666cbe29a6ee" => :high_sierra
