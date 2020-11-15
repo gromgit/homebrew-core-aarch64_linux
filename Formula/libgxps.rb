8e66b411afbb269389359af0204bb767fe5847f0
Jonathan Chang <me@jonathanchang.org>
2020-11-15 10:49:05 +0000

    libgxps: update 0.3.1_1 bottle.

--- Formula/libgxps.rb
+++ Formula/libgxps.rb
@@ -13,6 +13,7 @@ class Libgxps < Formula
 
   bottle do
     cellar :any
+    sha256 "2b3f7d48efd67aae795cbe831c3f6016ef3935927bc277e605525a9a29d38e45" => :big_sur
     sha256 "dd6c63cf7f8af07a9ea8bbe4ee902d55a834652f4100780affab11dd38a3deb0" => :catalina
     sha256 "a71f1a595fe620805393786fe14dedc8fe3fb6f75a812536ba5acc00e9ec9c07" => :mojave
     sha256 "ed21a1e2b30b473883f54fa09c7a1707eb6ae2a78946ecbb1d1d11f5f340154a" => :high_sierra
