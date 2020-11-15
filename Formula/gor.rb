659ae80de6e23947a3e09ed76dc7d8398bd87a93
FX Coudert <fxcoudert@gmail.com>
2020-11-15 16:08:17 +0000

    gor: update 1.2.0 bottle.

--- Formula/gor.rb
+++ Formula/gor.rb
@@ -9,6 +9,7 @@ class Gor < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b02c8cacea6257c1faf97f6c3a686f09fc1519e54cbd80713b19c95b9c03ced2" => :big_sur
     sha256 "0a2c7715c47fa3fb9ba70494b1bf20a4216cabb09d909702f86d810a07c58f17" => :catalina
     sha256 "347fab444ceaee3d2dae0f23cedcd924267bbbba95a099ad5602ba3051fd5c1f" => :mojave
     sha256 "0e07f5bf90d57b9bd1b0ccb961e6ee240fd8346a923c45d075b66cd7c4714a63" => :high_sierra
