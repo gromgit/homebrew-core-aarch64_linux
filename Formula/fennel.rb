e0c7a1b938d2d6dedbb671eee95ced91df108714
Jonathan Chang <me@jonathanchang.org>
2020-11-15 08:48:20 +0000

    fennel: update 0.7.0 bottle.

--- Formula/fennel.rb
+++ Formula/fennel.rb
@@ -7,6 +7,7 @@ class Fennel < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e4d1d9aed1b9b8b463660e6725ccafb24a3ae4a5de5d19bb0e67f046f3996d25" => :big_sur
     sha256 "eeceef32216dce9edeb2f213d9f6433f0a0f40784146a15c2a2ca29dec94baa4" => :catalina
     sha256 "33178e611d635926983ae9c254601414fb855a967eda93e1d257b2d0bf4859cc" => :mojave
     sha256 "07287f0807062132269ac48897285118c2aa50c404088cb6cef159571a848822" => :high_sierra
