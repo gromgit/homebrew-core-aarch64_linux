17940c9bb9195817d46fdd6f293224a7dcdc1fb5
Jonathan Chang <me@jonathanchang.org>
2020-11-15 09:43:45 +0000

    pdf2image: update 0.53_1 bottle.

--- Formula/pdf2image.rb
+++ Formula/pdf2image.rb
@@ -7,6 +7,7 @@ class Pdf2image < Formula
   revision 1
 
   bottle do
+    sha256 "51717dc099723d65d1aeedd18be5886fdb228dca2ceb190cde744a63e1b51bbf" => :big_sur
     sha256 "9550e644b89b03d2e78880145d64446d1fe07ef575c2fd4109a932ef2d5258b0" => :catalina
     sha256 "00f2f25eb5580dc2a4bbab2ac2913fea732967098cd373e52b6f5317098d8936" => :mojave
     sha256 "622e0f3caa2eeffe59384682a196fd42b381e638d67ddb399e39342e08fee1b1" => :high_sierra
