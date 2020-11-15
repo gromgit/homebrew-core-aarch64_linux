874ca19a6c673d466c5a12a8636ca565e3fe61c8
Jonathan Chang <me@jonathanchang.org>
2020-11-15 10:00:41 +0000

    libdill: update 2.14 bottle.

--- Formula/libdill.rb
+++ Formula/libdill.rb
@@ -7,6 +7,7 @@ class Libdill < Formula
 
   bottle do
     cellar :any
+    sha256 "d0bc288a3ce54ab333f48ae08d127aa78300f6ee4921f0a9b59ca7f685649a93" => :big_sur
     sha256 "124f2fd7aa4ba68e528bd2700637511943ae55ec9c2b2c9dcdb3bff1f2e34909" => :catalina
     sha256 "7ebbbe85ab5989b48664688c9fdc833b4bbc7846ea94f3f73c34ef620026b878" => :mojave
     sha256 "062d2f9f6fdeb5588036d3e06752ecfd95b5f4e7b6008b727208fc0a2e7f50a6" => :high_sierra
