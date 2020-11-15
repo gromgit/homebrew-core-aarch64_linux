9daae6126b8157da68b2df921fe31d5bd014a9ee
FX Coudert <fxcoudert@gmail.com>
2020-11-15 16:01:57 +0000

    textql: update 2.0.3 bottle.

--- Formula/textql.rb
+++ Formula/textql.rb
@@ -8,6 +8,7 @@ class Textql < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "84bb8f0f712da5618c74b46f86afffa5da7ade72a68e508e3037d590206f28f6" => :big_sur
     sha256 "d33d111039e957631d3a77cd35413707b47e684638a2571e3719a17c0173b55d" => :catalina
     sha256 "b6d4fd5ee0a2d1758651f91c35e6bd40a832f0d997ec2a120268bfde03a48cfb" => :mojave
     sha256 "38cbf8cacc0dd7e29831c8c7fe9f0437473c164bee549defb8744d6ca3e53fcb" => :high_sierra
