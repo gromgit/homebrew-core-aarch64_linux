9d70076d3e6defc93b40ac8b62870e0d0b7e1d24
FX Coudert <fxcoudert@gmail.com>
2020-11-15 06:35:09 +0000

    xgboost: update 1.2.1 bottle.

--- Formula/xgboost.rb
+++ Formula/xgboost.rb
@@ -8,6 +8,7 @@ class Xgboost < Formula
 
   bottle do
     cellar :any
+    sha256 "51634738fabc05059dadc938b602bbd5c627e135eed4466c543f59b03c69ad86" => :big_sur
     sha256 "6458d02e2907e74a9cc7b4d6ae22d132a7dc83f37a13e79a87f409567d46bd68" => :catalina
     sha256 "bd6bdd967e100e38130b834f93b01a82c3cfa1ea6581533ffa0d897d611656c1" => :mojave
     sha256 "5ea7f8b48ded860e69166b8f7f5adedc34c7007fab4154f5d802ee126d09bc3f" => :high_sierra
