421c8ecbee7892637c9d2542efb34f571be95d25
Jonathan Chang <me@jonathanchang.org>
2020-11-15 09:12:08 +0000

    bombadillo: update 2.3.1 bottle.

--- Formula/bombadillo.rb
+++ Formula/bombadillo.rb
@@ -8,6 +8,7 @@ class Bombadillo < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "f17f909fa5202ac56fa978fdff15ad2d30bfd5d7386ae223fa63a5b1857b7c5a" => :big_sur
     sha256 "d4cb984fc96583b1bf71dc482125b54d6dac9b46c47c6b0134183cc501cfb8b8" => :catalina
     sha256 "a2ae947fb7d64598be63f1e54ea66cc5325fd135efed9c16f1fc2220f362a056" => :mojave
     sha256 "685062637b7a2d279bcd9b002edd87e16cff4ce400bcb3c739899e95a652c809" => :high_sierra
