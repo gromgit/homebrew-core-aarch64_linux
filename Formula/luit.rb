f9708becda2f440b63ac31347f4d4d601b325da5
FX Coudert <fxcoudert@gmail.com>
2020-11-15 13:07:09 +0000

    luit: update 20190106 bottle.

--- Formula/luit.rb
+++ Formula/luit.rb
@@ -6,6 +6,7 @@ class Luit < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "94866891c1da82fe283a81563f768a43d495c59db3fad393a717b7c5343868b6" => :big_sur
     sha256 "3aafc250abb3e80b97eb19c4cd33760526086ce59c6c4609a75504e34a5b25bf" => :catalina
     sha256 "11e27c20be2a427608f074fdc2d42d7647dd78671cf8242f436441a15fcbc001" => :mojave
     sha256 "83411e2dbc25e9b93bff916334d2dbf4c0534deb526ac2369e878eb6bcd0cbcc" => :high_sierra
