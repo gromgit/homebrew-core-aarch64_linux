4974b69bfde3678123cb08734ccaa056d1157e1f
FX Coudert <fxcoudert@gmail.com>
2020-11-15 01:28:30 +0000

    siege: update 4.0.7 bottle.

--- Formula/siege.rb
+++ Formula/siege.rb
@@ -11,6 +11,7 @@ class Siege < Formula
   end
 
   bottle do
+    sha256 "d9319969845b82b053d675fa125cfce2241c498be7966fca5790af1410d7050e" => :big_sur
     sha256 "ccc545449c2a0bacb0054078faee630f23e4728d1bf137cba9c5d8aa82e02495" => :catalina
     sha256 "0704038a2995eec4ffacbb3554230b2825ebb21c3c9c612baae24999a620e183" => :mojave
     sha256 "d2793b5aee87ffe4c04fca2c807477c3bcc8c5979888d26db995e0e58bece246" => :high_sierra
