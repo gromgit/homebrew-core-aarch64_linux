a411ad5eebb7677be406b5d93556476585495a14
FX Coudert <fxcoudert@gmail.com>
2020-11-15 13:59:58 +0000

    dateutils: update 0.4.7 bottle.

--- Formula/dateutils.rb
+++ Formula/dateutils.rb
@@ -6,6 +6,7 @@ class Dateutils < Formula
   license "BSD-3-Clause"
 
   bottle do
+    sha256 "8ec20a61569fda99fc4979e7c1113546e03cb8f4aeae7177899acb72bdb7ab26" => :big_sur
     sha256 "25d5db665c0591e56c4ec698656a5519f20417473e3cb763299d804bc735a9a5" => :catalina
     sha256 "3124ddab0439b64bcfc95057ee52c7c902e684898a2d96832732819682fba75c" => :mojave
     sha256 "310bbe8e2d4d039d065fb9bc5cd33ced2fc45f970057f21a205ff0004d6921e0" => :high_sierra
