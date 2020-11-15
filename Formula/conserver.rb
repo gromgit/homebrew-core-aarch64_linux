c60fcc0af513081609f91a09d04f2379e96d3fa1
FX Coudert <fxcoudert@gmail.com>
2020-11-15 16:16:45 +0000

    conserver: update 8.2.6 bottle.

--- Formula/conserver.rb
+++ Formula/conserver.rb
@@ -12,6 +12,7 @@ class Conserver < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "35fc01a42164ba7af8f73497fbd25c0f4193580e2ca93d0e33683a0d0c1d0ffa" => :big_sur
     sha256 "eaa811a62521fd8f6f33af9837d03f42dedb218021d64390b8e9ece105e928c1" => :catalina
     sha256 "03290cfe6fffbbb28d16b526a9667bc321cf048f32c947fbc9c20787bd67ed7b" => :mojave
     sha256 "71080dd0b8f5cf10c4c2e9aa935d48cf6f458f7bd926c59a7087108129a83ac7" => :high_sierra
