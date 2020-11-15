8d7632083c3ba471f77386e3fa3e6f96722d8800
Jonathan Chang <me@jonathanchang.org>
2020-11-15 10:57:09 +0000

    liblcf: update 0.6.2_1 bottle.

--- Formula/liblcf.rb
+++ Formula/liblcf.rb
@@ -9,6 +9,7 @@ class Liblcf < Formula
 
   bottle do
     cellar :any
+    sha256 "c9188f359be4dda83cd9fd6759cfbc2398a48e1c50a6b46e1678cba4056d6011" => :big_sur
     sha256 "1765ea5fd438e35c255104fcf3670f4fb20b44b0d88dfc5579deeb95fd0567a1" => :catalina
     sha256 "626aeacfaba89b8d5bb57945c4f82c33758e7a78de2b2d87261e6e83e874a7bc" => :mojave
     sha256 "b508d5fd894f0962328296f0d292eb3cd88e48d42d20e75934b7b9abad62c53a" => :high_sierra
