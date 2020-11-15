af2f89d6a4b93c088f48f9df473e5f55a9d597cb
FX Coudert <fxcoudert@gmail.com>
2020-11-15 16:48:13 +0000

    ngircd: update 26 bottle.

--- Formula/ngircd.rb
+++ Formula/ngircd.rb
@@ -7,6 +7,7 @@ class Ngircd < Formula
 
   bottle do
     rebuild 1
+    sha256 "4e4bd4f8a09df60785d81aade4d04db63db930a7203b2065b10b754e08cb17e3" => :big_sur
     sha256 "129bf331b86f1d54735cf773a36b402323af0667afc1e2a1da4554899d05e8e3" => :catalina
     sha256 "7fdce95437555b6de33aa1b0c9d3e3441405b5d3c9ec2f728a7b28f35983402d" => :mojave
     sha256 "b58cacea0c009487e46b48d7220623001e49ad31722704ee805fd7029313f5d6" => :high_sierra
