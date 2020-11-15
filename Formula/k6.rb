6f98abe0caf402f540914b3d811b44bfcc9d2ba5
FX Coudert <fxcoudert@gmail.com>
2020-11-15 01:25:33 +0000

    k6: update 0.29.0 bottle.

--- Formula/k6.rb
+++ Formula/k6.rb
@@ -7,6 +7,7 @@ class K6 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "18408a1b36d5bd61dbf10fc55e83efaa33b358e51fc5d2a91879c9049c5d0f10" => :big_sur
     sha256 "b7bd5708af3593bb66e252e35d37aad25c8f2b86686ed53f67d438cb0da4adc9" => :catalina
     sha256 "0eb5aa720d0a1bc5b306e7c7e4d767a346b7073f14e018bc866a5e2540cd9423" => :mojave
     sha256 "037428689ad82115999e98f4f8967f615339e9c9f3e9211d0935ac63a73fbf20" => :high_sierra
