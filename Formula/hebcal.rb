e8b50df219d954a522a25c043dfb2582e84a85a4
FX Coudert <fxcoudert@gmail.com>
2020-11-15 16:07:32 +0000

    hebcal: update 4.22 bottle.

--- Formula/hebcal.rb
+++ Formula/hebcal.rb
@@ -7,6 +7,7 @@ class Hebcal < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f6e790acc45c8c23ef75170752861cef1c2797759ea0221422f3bdaba05298b0" => :big_sur
     sha256 "d6bcc551ab5a7f4da895483e648186db77ed80a8d6f446a79bc62fa0e9bba8ab" => :catalina
     sha256 "be4e928b3b2e02e80797bc2526625b91f368b234d1e8f8e1801eb9bd24be86da" => :mojave
     sha256 "1b6d05812ade070433b47ebfb332db7859c23eb60636dcc70f65cca6ad04fa04" => :high_sierra
