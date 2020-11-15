2b7257ed1694dc6635b9a16ee5602dd787242d62
Jonathan Chang <me@jonathanchang.org>
2020-11-15 10:13:40 +0000

    libgaiagraphics: update 0.5_7 bottle.

--- Formula/libgaiagraphics.rb
+++ Formula/libgaiagraphics.rb
@@ -7,6 +7,7 @@ class Libgaiagraphics < Formula
 
   bottle do
     cellar :any
+    sha256 "71019ebb245fbf75794ffc377be75d4a9731a7cc842a458d630ef7fb9d824741" => :big_sur
     sha256 "05b3806c31a6e084eeeec2e44c83b8fb728cd0de4cc22dae14888ff52e290cca" => :catalina
     sha256 "bfaf50e26b9312c1ef7d9b62677e92099339d14393ce855b870fe9288503c5df" => :mojave
     sha256 "20a230ae5fccd2d5114e8ab7a128dd57834104461e5a7cbc2f7c7e63075214d9" => :high_sierra
