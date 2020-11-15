4bca50172bdcdc8dc28074d622259e7f175702b0
FX Coudert <fxcoudert@gmail.com>
2020-11-15 17:56:11 +0000

    wandio: update 4.2.3 bottle.

--- Formula/wandio.rb
+++ Formula/wandio.rb
@@ -12,6 +12,7 @@ class Wandio < Formula
 
   bottle do
     cellar :any
+    sha256 "d22ac83e77d52b6ea039e2fe4a8b888a17fb9ee91a933d0149fac081ae0b783c" => :big_sur
     sha256 "028d07a97370b37fc28a2f2045bf3e4a9241d9c49f5eea2e635960ced7b6453c" => :catalina
     sha256 "a16a370f4bd6d2acd415f305fb99b2bfba1b86f666c68877d01bd90ddcb7522b" => :mojave
     sha256 "29602aec2851811108e97397e6310f091f5e5fe0844f9cfcd6657193d9c53ff4" => :high_sierra
