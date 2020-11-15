3cc6286d3f9d10aecbbd071ee28f08034538d40a
FX Coudert <fxcoudert@gmail.com>
2020-11-15 06:56:38 +0000

    ccat: update 1.1.0 bottle.

--- Formula/ccat.rb
+++ Formula/ccat.rb
@@ -7,6 +7,7 @@ class Ccat < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "56555b8a3744a0af29b6bddcab2587457bb8622f78484b38fbbaceab88ea3f5b" => :big_sur
     sha256 "aec38270a3b41a57fe6d05df08eea67042f2b65a2a5de30b2452afefd81a6d9d" => :catalina
     sha256 "0170dc610f0561cd562a2614f5bb0139cad5d37133a4181318a0edc08b3182c9" => :mojave
     sha256 "895c26dc74369ef72990fd79447e654f5266dda9c662d3bed2926caab7180678" => :high_sierra
