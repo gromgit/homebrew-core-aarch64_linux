8de3ca313a5c5a72a0d36a06b122f6f6ec7654b0
FX Coudert <fxcoudert@gmail.com>
2020-11-15 13:41:01 +0000

    datamash: update 1.7 bottle.

--- Formula/datamash.rb
+++ Formula/datamash.rb
@@ -12,6 +12,7 @@ class Datamash < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "50590b93f6f3a25e3e2724ddad696e6ed8a168f840fafe887be423f5020ce86c" => :big_sur
     sha256 "f592c4bda737ef924fb4c1642fb381db54c9ce246eb51d03a145dd28a8391406" => :catalina
     sha256 "6533f0decc607d6e3ce1ad1fdb7f5b30f99bbbcbacbba1bcd880486eef648189" => :mojave
     sha256 "b6100d066c3cf9d91b2bd4a8d8bcdc5fa453c6eb6a28d7cacb06659baa358e46" => :high_sierra
