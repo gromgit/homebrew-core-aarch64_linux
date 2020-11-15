016fa77a46b2963c106d3c45dc092dd562ea2de2
FX Coudert <fxcoudert@gmail.com>
2020-11-15 17:23:59 +0000

    nsd: update 4.3.3 bottle.

--- Formula/nsd.rb
+++ Formula/nsd.rb
@@ -14,6 +14,7 @@ class Nsd < Formula
   end
 
   bottle do
+    sha256 "b3eeb6e4a4145b3384982aa2330690834373f547235a659404eefd974163b199" => :big_sur
     sha256 "d2d0731a5a4a03fff114250debcc1ff5fa6fe15faf77351afda9a2ccc6cdfbd0" => :catalina
     sha256 "1f0661c9656ab0d0821a9eeb5b990c9eb6a088654e5406318577084296ab8119" => :mojave
     sha256 "189a5b486bbfdcc0571e89f67d5f4f11474ce1fe2fb9e5ca720ced3662aba054" => :high_sierra
