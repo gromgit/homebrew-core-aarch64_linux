3eadf79db00241bbc35d5f04da01382b3dcfdbad
FX Coudert <fxcoudert@gmail.com>
2020-11-15 07:00:51 +0000

    isync: update 1.3.3 bottle.

--- Formula/isync.rb
+++ Formula/isync.rb
@@ -12,6 +12,7 @@ class Isync < Formula
 
   bottle do
     cellar :any
+    sha256 "278f8ee2e2d3cd15cff66ab586f458b2103896573e2772522d97629c46768e37" => :big_sur
     sha256 "e246ae47ad32253be95b4344e3a8c7ef2b586364944080d98beb7d9543ba7c9e" => :catalina
     sha256 "af52bcdb59df55a0aa754d5fd3b77ef14107365d9d1ef39cf20115e0d49d6221" => :mojave
     sha256 "aaea9bd9e79853387aef7c312616d9c1446af6661b889539500e5640070679f4" => :high_sierra
