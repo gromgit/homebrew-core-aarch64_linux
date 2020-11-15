5d90e6a4094f75d9a45aac910679e1bf7454fd80
FX Coudert <fxcoudert@gmail.com>
2020-11-15 16:28:18 +0000

    tnef: update 1.4.18 bottle.

--- Formula/tnef.rb
+++ Formula/tnef.rb
@@ -7,6 +7,7 @@ class Tnef < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "37ce4eac19eaa6e7111d7ef7897595ac71ebb58f6d7da32dc309bb02bc5a90b4" => :big_sur
     sha256 "ff92eb820b2efae9e87e42491a590601f400160f27ea2804b176b02b1648be66" => :catalina
     sha256 "2700f31ebcc1e2ba9219d6b6ac040846eba21ccc25baca4fea8b7d630b6673d2" => :mojave
     sha256 "842ba6bbe666302bd39c1cf7d29caa7d5180c20757b8dfe91b99d3fe1d3da841" => :high_sierra
