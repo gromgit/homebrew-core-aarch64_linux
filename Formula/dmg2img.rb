fd608ea49abe566a34104c471245a5646a75d869
FX Coudert <fxcoudert@gmail.com>
2020-11-15 04:19:57 +0000

    dmg2img: update 1.6.7_1 bottle.

--- Formula/dmg2img.rb
+++ Formula/dmg2img.rb
@@ -12,6 +12,7 @@ class Dmg2img < Formula
 
   bottle do
     cellar :any
+    sha256 "e1df6e7db928dd7e5d865968a527e310a7d1cad6f68c5a72c3bd717b75cef325" => :big_sur
     sha256 "e16b42ead321d5e0c85a98592154ef13a2206355a13cfe021735653a1dd995be" => :catalina
     sha256 "fb90741dc01f5c7b115c9d5bf142e36a90d7cf0995ecb4a5183150ec6d6161ac" => :mojave
     sha256 "367ab961e50114debc983e5665443ee8fa5a85a2b4fab024753f38df48fb26f1" => :high_sierra
