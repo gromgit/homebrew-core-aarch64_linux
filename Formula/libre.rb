325fab02330ebfe7166b46ef4508d8b1118abde4
Jonathan Chang <me@jonathanchang.org>
2020-11-15 10:32:08 +0000

    libre: update 0.6.1 bottle.

--- Formula/libre.rb
+++ Formula/libre.rb
@@ -6,6 +6,7 @@ class Libre < Formula
 
   bottle do
     cellar :any
+    sha256 "73bb1c1c6ebe022f294cc73e672154579e4d8e634f109883bcbb68818e1f3374" => :big_sur
     sha256 "0ca7e76631b5f30d72b4bc4248e894d00f05cfb785c98856d82cd5cc13e591f9" => :catalina
     sha256 "5d43d79ef2406e40c858463189ca8a40f0b13ede8a7090b56ba0fd1ef942dabc" => :mojave
     sha256 "32787ca36540a0c7c330560076e25726bcca0f08a7b77014d3837bd9c7ca1840" => :high_sierra
