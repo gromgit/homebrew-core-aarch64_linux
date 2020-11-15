1ac818194b3fc3e8f56b146e20c03f2e64b5f603
FX Coudert <fxcoudert@gmail.com>
2020-11-15 14:56:55 +0000

    ipv6calc: update 2.2.0 bottle.

--- Formula/ipv6calc.rb
+++ Formula/ipv6calc.rb
@@ -7,6 +7,7 @@ class Ipv6calc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "df471847ca4cad96821c296f0b2fcb30622c0284498cef72f8bd6486d183fe52" => :big_sur
     sha256 "f38b9f04ac49b301c599a497b2c4cbc562bc2fcfb73db76ba787fa10d185be76" => :catalina
     sha256 "df70a9f7bb60eb8c65ed1b7ef6d850f3c2d57321e9be3e814c2480edac3c89df" => :mojave
     sha256 "8e1168b64a282ce94ded17806bd16142920411f9515f17cd5bb7ed1eb272635c" => :high_sierra
