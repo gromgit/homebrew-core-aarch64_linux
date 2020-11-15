5745fe61491b6042dc6e0d3c861ee6410d90b7b2
Jonathan Chang <me@jonathanchang.org>
2020-11-15 09:23:32 +0000

    cointop: update 1.5.4 bottle.

--- Formula/cointop.rb
+++ Formula/cointop.rb
@@ -7,6 +7,7 @@ class Cointop < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b5816d00059223189e26efd3db01da8ed68bf6487ee7f6a2c8e7d234e8a20562" => :big_sur
     sha256 "8b3ab6e0ce9af52ddd7c07c47b1c97067fcf1cdefb2663b21fe10d0db20bb467" => :catalina
     sha256 "bbaf1d1da14a998b7f786b5135e09b2178b790dcf4efacb77c72f0c0c29aee2f" => :mojave
     sha256 "6b8901459eebd705f512c122812815afed289265e3ae406d1e8054aac4261e70" => :high_sierra
