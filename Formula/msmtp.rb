c3391c0c12952e08af946fe90e0b7b93739ff4b1
FX Coudert <fxcoudert@gmail.com>
2020-11-15 07:29:20 +0000

    msmtp: update 1.8.13 bottle.

--- Formula/msmtp.rb
+++ Formula/msmtp.rb
@@ -11,6 +11,7 @@ class Msmtp < Formula
   end
 
   bottle do
+    sha256 "00e59fccc50af2f7293dffc2ff25c3a174f69b69e7bfbce37128bed939e03cf8" => :big_sur
     sha256 "64ace78142e59a05032fc65e683c1a36d2c28e457be1f22718519ca37c1e90c2" => :catalina
     sha256 "6e529ebbebd56ef337ab4099d39d17adf2792d5d3c827d348fd1c23ac04fa7fe" => :mojave
     sha256 "63abd352182e42ae21a6c143db533446e1bc9c8fc6cfcf4c60a69f630558a189" => :high_sierra
