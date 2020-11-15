0b5218eb6d709e1249d50414ab850798a12b68d3
FX Coudert <fxcoudert@gmail.com>
2020-11-15 05:53:51 +0000

    libedit: update 20191231-3.1 bottle.

--- Formula/libedit.rb
+++ Formula/libedit.rb
@@ -12,6 +12,7 @@ class Libedit < Formula
 
   bottle do
     cellar :any
+    sha256 "7add4831a5be1d9829064a690c36fb47a9a3b75e8a59acf266f4fc4f2a3ad4f6" => :big_sur
     sha256 "faa58f2e587c5b982af44765f7a034a27837fc1e94816e094ace3f408ab4a7bf" => :catalina
     sha256 "a707377be9d5fef881cdbb77ad3b562c9d5f54befb97a10d0b7158e4db87ef86" => :mojave
     sha256 "06e087927f024a9030947216be3aaa46f97fc9dcc1b70959f60240b86bd8f574" => :high_sierra
