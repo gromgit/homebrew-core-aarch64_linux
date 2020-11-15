b73a7ab9efdc1c2c730cb1e315f40553277a94e5
Jonathan Chang <me@jonathanchang.org>
2020-11-15 08:21:20 +0000

    telnet: update 63 bottle.

--- Formula/telnet.rb
+++ Formula/telnet.rb
@@ -12,6 +12,7 @@ class Telnet < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e6fb7de53e703755a72e227752f81023c2935567d935af638959e986da910b3e" => :big_sur
     sha256 "7435a9fd2515158762a85197a4ad7141e430383e185e002da169dbbb638c952f" => :catalina
     sha256 "d5009f496dc6cf0c13b936996f98b91b0f12733ea9462843b56a39fc53b20fe0" => :mojave
     sha256 "af38f3c6dd4ff5eda2248671958e66595b39e74cdeecca52af4efb495bc659a7" => :high_sierra
