d2cf1c9dc43b209a5d4c72c14cedc81018e3bbe2
Jonathan Chang <me@jonathanchang.org>
2020-11-15 10:08:47 +0000

    libffcall: update 2.2 bottle.

--- Formula/libffcall.rb
+++ Formula/libffcall.rb
@@ -12,6 +12,7 @@ class Libffcall < Formula
 
   bottle do
     cellar :any
+    sha256 "d55d915d1eaa2f0f9fc1d417ab8e7b425bf0bd9b20eec50c1fbf3179c68a0d5a" => :big_sur
     sha256 "02b522baa2d0f38a85e3a2bee8adb79644e8c834c486737b81a62945b9ec73d4" => :catalina
     sha256 "e38d2a42a2ad191847e423028580245f4b84829e5c781f6d58ce7da9981da280" => :mojave
     sha256 "d9f7db3318279d7dac5a162de3a251e96c3810be2e70fe8926d93e63b9849045" => :high_sierra
