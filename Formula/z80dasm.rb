04b4380ff55e8108b4505fefc25cc602902ec212
FX Coudert <fxcoudert@gmail.com>
2020-11-15 20:02:25 +0000

    z80dasm: update 1.1.6 bottle.

--- Formula/z80dasm.rb
+++ Formula/z80dasm.rb
@@ -12,6 +12,7 @@ class Z80dasm < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7b14f8e49b2e1a7e3ea40bf6f0143b75d4aea3561d9beaccc9526f576893e5a3" => :big_sur
     sha256 "5012e33c0fc342ec32a22462f9a75897fd69d44cf2918c64a593d268fa365c86" => :catalina
     sha256 "0650fc5eadf8ee791201886bd39356af1365f9258c2222e27824fe63500b6eac" => :mojave
     sha256 "a6d8e1d4caa612567de07580a353c82040e5c8005a08117386633e9a11f0df2e" => :high_sierra
