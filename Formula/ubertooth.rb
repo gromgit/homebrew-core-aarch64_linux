449d2ebe0ac797affba9911cf08b42ec4a9a7b55
FX Coudert <fxcoudert@gmail.com>
2020-11-15 13:38:10 +0000

    ubertooth: update 2018-12-R1 bottle.

--- Formula/ubertooth.rb
+++ Formula/ubertooth.rb
@@ -9,6 +9,7 @@ class Ubertooth < Formula
 
   bottle do
     cellar :any
+    sha256 "44519c8ea1f5f557404c950922e1c4303633df759676441fbc9620d72ab012ab" => :big_sur
     sha256 "e08b1229ff32e200e5f6e7d562d83ce26e5dff0e50b4373f1add2a411854ebdf" => :catalina
     sha256 "9e6dcaeeff6974606332371fc0ce861d679bc9d00471f4185a7531b320e581fb" => :mojave
     sha256 "d3891c8cd1e395c8d7acd9f364d6d42d3bcb7d9d1ddd5adea9dfabc7f0aead69" => :high_sierra
