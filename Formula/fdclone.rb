39e3a737a229c16711e609b10b0e9628780458f9
FX Coudert <fxcoudert@gmail.com>
2020-11-15 16:06:13 +0000

    fdclone: update 3.01j bottle.

--- Formula/fdclone.rb
+++ Formula/fdclone.rb
@@ -10,6 +10,7 @@ class Fdclone < Formula
   end
 
   bottle do
+    sha256 "c1c2dcd4d0e97e717dd9444c9ac8b37d77810c8162a481106d68be3c54f999a9" => :big_sur
     sha256 "6272d033132a7a2c355ab19629241021087c606de3114e2ebe4aa301e6bee840" => :catalina
     sha256 "b3a56f6b62622696f4da6554a487557a57c0875c2aba28705e300b7207f6a8ce" => :mojave
     sha256 "f894bed33d254c5c48341485e835f945b60e632a0ecbf484c818f12c61350122" => :high_sierra
