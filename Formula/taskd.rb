66668414029a623455c0ff21311c61fae3492780
Jonathan Chang <me@jonathanchang.org>
2020-11-15 08:35:32 +0000

    taskd: update 1.1.0_1 bottle.

--- Formula/taskd.rb
+++ Formula/taskd.rb
@@ -15,6 +15,7 @@ class Taskd < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "842ffc8f10936b3803281743196d37571057656ccfcd019364b8c5114ddc36cd" => :big_sur
     sha256 "88580976ecb71d4f74d814ff06c88c2082565fee61c7ff8e7f506bce19b460d4" => :catalina
     sha256 "225bedd463f0344572ec985bbb49693dc0b6d5c095c87a5157bcfc437317c1d7" => :mojave
     sha256 "f9737943f0b2877414bf8c0d957a88d79010334a145be6420fd93f64b9569cb3" => :high_sierra
