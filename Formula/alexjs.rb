873646e16a2edb890161da43cee4b80b3d18681d
FX Coudert <fxcoudert@gmail.com>
2020-11-15 16:40:21 +0000

    alexjs: update 9.1.0 bottle.

--- Formula/alexjs.rb
+++ Formula/alexjs.rb
@@ -9,6 +9,7 @@ class Alexjs < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1102acddac4146addf4d4b59ac66bf5d5d57b2badabfe9038c286ab5660c63f6" => :big_sur
     sha256 "e37814ad3315475e5484cc5c334089213a023ea443ccb2207702b8667f3e1a0e" => :catalina
     sha256 "7c2d9fedf79264bd59e73b7f062512e03afdd1e6a66152c233996471636549f1" => :mojave
     sha256 "462578faccc1fd6ba5df072a50c90c28522067e6cbba84a3b6a6dfa29c7a2eaf" => :high_sierra
