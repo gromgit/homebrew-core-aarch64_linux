7caa39e7908d021d039470995203de3be399678e
FX Coudert <fxcoudert@gmail.com>
2020-11-15 18:06:22 +0000

    mruby: update 2.1.2 bottle.

--- Formula/mruby.rb
+++ Formula/mruby.rb
@@ -8,6 +8,7 @@ class Mruby < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e1d5fd18c921cf19742a2c484f472d04b41fe84297c6c193ffc932cbc6c375df" => :big_sur
     sha256 "b6a638c04a991a249a737d0ad0d9f7fac31d35a7b2fd3c8507304e68f13bc983" => :catalina
     sha256 "1f31eadc8801f65d42e2cfec43cda336324daf86978529dfc76338e6b094b16c" => :mojave
     sha256 "5b5dca177d9fdd6a2b543c9aeb2117e0d112d1578fadbb709d8565d83b21d6a7" => :high_sierra
