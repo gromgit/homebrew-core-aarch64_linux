e51f2f625345318cb813ffb0276aab5bf5332953
FX Coudert <fxcoudert@gmail.com>
2020-11-15 02:33:51 +0000

    nuttcp: update 8.2.2 bottle.

--- Formula/nuttcp.rb
+++ Formula/nuttcp.rb
@@ -12,6 +12,7 @@ class Nuttcp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9001ef97c90c4097f1ebabed20e63305f82a5f04d7ffc0f0d788c249c49d236d" => :big_sur
     sha256 "0f5e7a2b61f91360023ef643c0a77fa711855b34006ff07867f1283051aded5c" => :catalina
     sha256 "de0d1395983d9980dfff73de8282b76bf70c987fb36c68ff5e341f245507100a" => :mojave
     sha256 "8d9ea3c88d5347b34cc4b3385b6898f942b8e4ff5a4f4a26897e4b66297e2692" => :high_sierra
