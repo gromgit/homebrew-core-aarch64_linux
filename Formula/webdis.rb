8a42163b7d74d008182cdd8b75c17756ee5a7746
FX Coudert <fxcoudert@gmail.com>
2020-11-15 20:00:21 +0000

    webdis: update 0.1.11 bottle.

--- Formula/webdis.rb
+++ Formula/webdis.rb
@@ -7,6 +7,7 @@ class Webdis < Formula
 
   bottle do
     cellar :any
+    sha256 "fca39d9796541e09c5c69ef97dc79940a7c939ae13d0c9271a823cd16229157f" => :big_sur
     sha256 "d0f5052f9479bba534cdc8f5acfb047207d46b7e03aa3bea8bc9b17a07a27948" => :catalina
     sha256 "e27da82c3099bbc9194c2a53dd3113580874e06ac8dc206f1523636cb678d3a2" => :mojave
     sha256 "1947a8b3ffcb642053eefc5fa48aca88604148713fe8ae57eb4ab80a6991b097" => :high_sierra
