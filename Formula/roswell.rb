ae01a2989fc9e42bd11548be2fee2b0977e59257
FX Coudert <fxcoudert@gmail.com>
2020-11-15 05:28:34 +0000

    roswell: update 20.06.14.107 bottle.

--- Formula/roswell.rb
+++ Formula/roswell.rb
@@ -7,6 +7,7 @@ class Roswell < Formula
   head "https://github.com/roswell/roswell.git"
 
   bottle do
+    sha256 "0ebfd2486d641e65b51886a2d094a102d7fe5e1a9167382e65f7467bde4f7a9a" => :big_sur
     sha256 "dcd9cf15c21d13724f36fd736529faba40dc0c2af2008c970b94049796d6d171" => :catalina
     sha256 "2d28ef4f1a1cbf94fba0132f7e1f05abb87854af6ce65f70c71877c7152d5622" => :mojave
     sha256 "a6d5d3ddafc4afefbb84d077b2157a1e7101c5c057bc7fe25714c19b83659017" => :high_sierra
