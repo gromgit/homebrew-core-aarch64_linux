95cb8cc890a4f25432fe5d89ee7be717f8b48aa6
FX Coudert <fxcoudert@gmail.com>
2020-11-15 13:44:29 +0000

    gost: update 2.11.1 bottle.

--- Formula/gost.rb
+++ Formula/gost.rb
@@ -8,6 +8,7 @@ class Gost < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7622f651be0def2be1b80cf0a290f0b42d87deee3107488ae0a13ba4a0cf6799" => :big_sur
     sha256 "4669c79b11e368446e14667c237c710b05e69b2be23a699fe7e4d9355765c063" => :catalina
     sha256 "fe984a0b5d0323ddb2306a078265b246c751c0dc08d0f14cbe0ddb0c00293f13" => :mojave
     sha256 "6db1c66d9a848ea55930dfb17fc0da9ec64a3e053631a29667026d58a34c2246" => :high_sierra
