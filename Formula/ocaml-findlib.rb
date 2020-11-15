7d1f0e6cf62c93368ce3fd57be99b34fe83810df
FX Coudert <fxcoudert@gmail.com>
2020-11-15 12:59:38 +0000

    ocaml-findlib: update 1.8.1_3 bottle.

--- Formula/ocaml-findlib.rb
+++ Formula/ocaml-findlib.rb
@@ -11,6 +11,7 @@ class OcamlFindlib < Formula
   end
 
   bottle do
+    sha256 "d0049172eb8e73000a7f085348dc8c2d89c891a67ade6a46b61acd7c9a330da8" => :big_sur
     sha256 "ba498040816b2b5b00ed84a96107119a99a52a0815b86ace5e5708f807be1ddb" => :catalina
     sha256 "b9af770177876ae3ffff6cca808a7ea72866a0bfe3b92a987878629fc42b3eff" => :mojave
     sha256 "a412ed75fa6bd7180846f2305eea5d2a4170bb41535c26fb047fbbd2b0adef8a" => :high_sierra
