bf274ca5aae8d2f99266e27bcec2b48df7a465dc
FX Coudert <fxcoudert@gmail.com>
2020-11-15 05:20:07 +0000

    pdfcrack: update 0.19 bottle.

--- Formula/pdfcrack.rb
+++ Formula/pdfcrack.rb
@@ -11,6 +11,7 @@ class Pdfcrack < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b2f128d92c00fe5d1fa81d7b61e7458fcdb8b58e656b312679fbefe33d4295c4" => :big_sur
     sha256 "2c26ba3ca3a1edd9cc9e6570cb5d1c7838a152a3d8281a2a16c6be5276d47b23" => :catalina
     sha256 "12310e0c5841db19ecf51f527cde0aaba1663a04aa78c760a5d6e02218dc4e3c" => :mojave
     sha256 "9923fadda54414a1d0cd5e86b0796aec8bbaf7c80e96e7518b5ae136d50d5674" => :high_sierra
