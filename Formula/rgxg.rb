e2fe5d4df750e8df3cc9f82fe9b9b4ec8333d7a8
FX Coudert <fxcoudert@gmail.com>
2020-11-15 19:41:00 +0000

    rgxg: update 0.1.2 bottle.

--- Formula/rgxg.rb
+++ Formula/rgxg.rb
@@ -7,6 +7,7 @@ class Rgxg < Formula
 
   bottle do
     cellar :any
+    sha256 "37ed8cafce126a6ab77b1e367dbe2a72a68c6556569694366e16844b18071dce" => :big_sur
     sha256 "4a07550d93bedfa3b2ac3cb77a8484951321697ca9384d2c2a0301ea261aa954" => :catalina
     sha256 "b410fe9ea150e0fb52326e4f7ce6642f946098b0713c5741c64699de3f55f762" => :mojave
     sha256 "286318be76fc55c094da739c44176d5babd814df1e4f0462711aea283db042f5" => :high_sierra
