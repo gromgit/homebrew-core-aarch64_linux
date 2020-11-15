1a521ef4f60a7838cf71edef1d28fe006648bace
FX Coudert <fxcoudert@gmail.com>
2020-11-15 20:37:09 +0000

    foreman: update 0.87.2 bottle.

--- Formula/foreman.rb
+++ Formula/foreman.rb
@@ -8,6 +8,7 @@ class Foreman < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "70c762dd642d8f5aa3ca5a28e420b6c9f7befaf7699de073b7d62e174fdee88f" => :big_sur
     sha256 "5c2b39c1f7e9667b9ebc6b7228b6cf31f06c2261c85019028272cfdda7073ea5" => :catalina
     sha256 "674b5fc005986f47294acedccba6b2a2bcdc1d423e392a356f8d58cc88a2c81a" => :mojave
     sha256 "b0d289ff31caf33f3d549af6dd615e37588aadb243355395380c4df5b0e52d63" => :high_sierra
