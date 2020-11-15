1eca95b7ee3f486297cedf7c0413133c7b0502ea
FX Coudert <fxcoudert@gmail.com>
2020-11-15 07:49:35 +0000

    zig: update 0.7.0 bottle.

--- Formula/zig.rb
+++ Formula/zig.rb
@@ -8,6 +8,7 @@ class Zig < Formula
 
   bottle do
     cellar :any
+    sha256 "49ed801fbd9d811765e6154805bcb259c4efa831d729c65e2d8d97edc1e770da" => :big_sur
     sha256 "e1961b8c92810f085db84f43d4a20e3f14efdfe4d8d816cbd4620e412761edc7" => :catalina
     sha256 "fe8d287d02eaea270f69a51455f4aa27dda3bc4984231e592e9f56eaafba194a" => :mojave
     sha256 "e1cf0c30406fc5b9b0cbc00c56879674118b89cb40e2b1b840cac04996df29b4" => :high_sierra
