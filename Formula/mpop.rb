28dbf4535b5433ec7f10e07236779006631d2c29
FX Coudert <fxcoudert@gmail.com>
2020-11-15 19:21:34 +0000

    mpop: update 1.4.11 bottle.

--- Formula/mpop.rb
+++ Formula/mpop.rb
@@ -6,6 +6,7 @@ class Mpop < Formula
   license "GPL-3.0"
 
   bottle do
+    sha256 "e95b6c6b714ae9ed4d88e084e42ba46b7e86b608cc4a6eac7f9e65a5116fcf92" => :big_sur
     sha256 "c1d09e98b098f34e6552280fa575430faa97456c075708465588ded47b1dfc0b" => :catalina
     sha256 "0ddab1250be82811b51619ace9638303c9576fc32b0b4c3c8b9f19e2cee4ab51" => :mojave
     sha256 "c92742ff97f25590c21b64c2a19ce47dae7b2d1ce405f6d50a3e086ee8eba081" => :high_sierra
