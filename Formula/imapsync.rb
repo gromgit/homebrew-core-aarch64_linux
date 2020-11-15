f158e83d509f3f860483f56cdff41f0332d9fc62
FX Coudert <fxcoudert@gmail.com>
2020-11-15 06:00:26 +0000

    imapsync: update 1.977 bottle.

--- Formula/imapsync.rb
+++ Formula/imapsync.rb
@@ -14,6 +14,7 @@ class Imapsync < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "88c192e6f93482a9ed5902b1b369cd9f788611f256889ed9650888999edce9fd" => :big_sur
     sha256 "6c1a1bed4c3ad926f34a5efe6b6bfe5d2776ed4ee0d000679b21679c47ff81cb" => :catalina
     sha256 "5b259d1a7096a887af61eef5a379122c8e94e71f912b2670a35031e6c159351c" => :mojave
     sha256 "fa0a19bf66b24d332f6e0f4f827b2abdff2b904b483c4927f2ed7d6d944ab261" => :high_sierra
