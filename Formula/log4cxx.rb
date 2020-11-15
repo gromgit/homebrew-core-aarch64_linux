b53a6fceef4edbcb71f41aa067ef5a0116537a0f
FX Coudert <fxcoudert@gmail.com>
2020-11-15 19:15:10 +0000

    log4cxx: update 0.11.0 bottle.

--- Formula/log4cxx.rb
+++ Formula/log4cxx.rb
@@ -12,6 +12,7 @@ class Log4cxx < Formula
 
   bottle do
     cellar :any
+    sha256 "33929bf44a188a1e7f16ae25a4b6495b63846640ed74a7d5f0c94db15151f5d0" => :big_sur
     sha256 "ec9ff34b2c49aa9a48536f7d109da16cc32f9ce83e95ac1dc3efc8a982709908" => :catalina
     sha256 "23a968d63f8a181a73410cffcde5fd16fbacd5867453e7c0d7b0cb3815942bf8" => :mojave
     sha256 "11478b4f5ece24ec391954cc0538bb28f11ae6256a9499ca1e95103c2eb1d75c" => :high_sierra
