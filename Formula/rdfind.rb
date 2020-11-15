bd450179bd55f669b48b7c19d4aa985af94c5451
FX Coudert <fxcoudert@gmail.com>
2020-11-15 13:37:06 +0000

    rdfind: update 1.4.1_1 bottle.

--- Formula/rdfind.rb
+++ Formula/rdfind.rb
@@ -12,6 +12,7 @@ class Rdfind < Formula
 
   bottle do
     cellar :any
+    sha256 "ade2aaffee4c52e143fdcd0a22fae933e60e4c3d5057e4bc74a9aaffaf3c2146" => :big_sur
     sha256 "e890406a4cbbd8d026a4c583644efa537433ac71c095a1e582b0454d85a87d00" => :catalina
     sha256 "489e104d2c5e5d939439f5b100cd97e19ed070181d355b49fbd1ad2b3320d789" => :mojave
     sha256 "2ce91e3b8a129c0fadb57fba46074e74e0d896287c23eb1844cd99e5eef093b1" => :high_sierra
