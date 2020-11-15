0f915d8f3322bbbb65aaef7b69ce3913fd250447
Jonathan Chang <me@jonathanchang.org>
2020-11-15 08:53:19 +0000

    act: update 0.2.17 bottle.

--- Formula/act.rb
+++ Formula/act.rb
@@ -7,6 +7,7 @@ class Act < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2faa63c64245afacf70f1c61ae445a26571bfe7c190ee39169ab0d73b47cceda" => :big_sur
     sha256 "e91282fdbe17e8e559f99ac0e0d704ea541197587277428a21b9927cb30b009c" => :catalina
     sha256 "37e68207cd6dd80222e79501a2fc6870e0093b257c8b2700a428f91e4cefc834" => :mojave
     sha256 "259e716e51ba28f1b73f203acd3af2cd368789d41f83b000e7a8cf6b98c6e1ac" => :high_sierra
