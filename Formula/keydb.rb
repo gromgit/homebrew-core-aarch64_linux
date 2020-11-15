6ace615a9a8764cae6f59d92683625a63a0f0722
FX Coudert <fxcoudert@gmail.com>
2020-11-15 17:49:42 +0000

    keydb: update 6.0.16 bottle.

--- Formula/keydb.rb
+++ Formula/keydb.rb
@@ -7,6 +7,7 @@ class Keydb < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "af7c50a7f480f837135ee2a174fbbfddc70049fc185e5cca67978ad295ada657" => :big_sur
     sha256 "399a17386d452bac3d05d7997af196954b4dddb4dd3c2dd753e112405c8eefe6" => :catalina
     sha256 "19e86e52f647fb1557683cd6f4b682a5e150095f0f99e13c19b7816c81d3bd17" => :mojave
     sha256 "ab28323dbffb9fcc0f11eeb18d9490808592b5caab0f8168e310937abb83b212" => :high_sierra
