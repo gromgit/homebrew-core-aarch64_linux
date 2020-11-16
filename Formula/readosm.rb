0f801d6702cae8396a2aeccc4d0f29e9039932f9
Issy Long <me@issyl0.co.uk>
2020-11-16 21:47:26 +0000

    readosm: Update download URL to its new location
    - The previous one didn't redirect, and this formula was therefore
      uninstallable from source.
    - Also appease the shims audit.
    
    Closes #64977.
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/readosm.rb
+++ Formula/readosm.rb
@@ -1,7 +1,7 @@
 class Readosm < Formula
   desc "Extract valid data from an Open Street Map input file"
   homepage "https://www.gaia-gis.it/fossil/readosm/index"
-  url "https://www.gaia-gis.it/gaia-sins/readosm-1.1.0.tar.gz"
+  url "https://www.gaia-gis.it/gaia-sins/readosm-sources/readosm-1.1.0.tar.gz"
   sha256 "c508cde9c49b955613d9a30dcf622fa264a5c0e01f58074e93351ea39abd40ec"
 
   livecheck do
@@ -22,6 +22,12 @@ class Readosm < Formula
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
+
+    # Remove references to the Homebrew shims dir.
+    %w[Makefile test_osm1 test_osm2 test_osm3].each do |file|
+      inreplace "examples/#{file}", "#{HOMEBREW_SHIMS_PATH}/mac/super/", "/usr/bin/"
+    end
+
     doc.install "examples"
   end
 
