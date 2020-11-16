7d64938e58c0ea7ab1670d454097b3581459dec2
Jonathan Chang <me@jonathanchang.org>
2020-11-16 10:12:14 +0000

    nift: update 2.3.10 bottle.

--- Formula/nift.rb
+++ Formula/nift.rb
@@ -7,6 +7,7 @@ class Nift < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "fd954d10e59e79bdeea45897fc6691cd7f06497c4398ec0f1f7adae62869fc01" => :big_sur
     sha256 "f08a936baa0f3e81ec0aa8343fb4970c39a7c73b308bad8df4e21fd665320ade" => :catalina
     sha256 "56e8f7ea837fbff3d3a887bd57134364c55f352c0c4e9a5bf38301dbb7e4bdda" => :mojave
     sha256 "ad7c89b1c61ba4659dff5fb3b021b3283f253e158ed72830b0598afdc33198c8" => :high_sierra
