f95ee3b7214dfef8361c7f1486098c1de09f227f
Jonathan Chang <me@jonathanchang.org>
2020-11-16 11:01:13 +0000

    airspy: update 1.0.9 bottle.

--- Formula/airspy.rb
+++ Formula/airspy.rb
@@ -6,6 +6,7 @@ class Airspy < Formula
   head "https://github.com/airspy/airspyone_host.git"
 
   bottle do
+    sha256 "e08af9bd327b9ceb880ccac50a0b4a0eba82bf250e9709a96397847caccd90ea" => :big_sur
     sha256 "17cb281bc432bcb77c33c38be4550be3a786225252b99d69db7a003daf74ef8b" => :catalina
     sha256 "d593672c7c08aff7b0056aa06171129b9fba717837de653dfb96b26ec78d6553" => :mojave
     sha256 "44736e1193e3f63fca0c984ac4f594d1ad11a83a810898103652f69af2bce63f" => :high_sierra
