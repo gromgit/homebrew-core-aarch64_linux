af432911fc31ffbb3544fe17adb5c1eee98b1dec
Jonathan Chang <me@jonathanchang.org>
2020-11-16 09:50:55 +0000

    supermodel: update 0.2a bottle.

--- Formula/supermodel.rb
+++ Formula/supermodel.rb
@@ -7,6 +7,7 @@ class Supermodel < Formula
 
   bottle do
     rebuild 1
+    sha256 "672d2a2500076ae34e896fb21f84b1237ae9d80c6b7858afe4316c55831f6ce7" => :big_sur
     sha256 "16ce3b8995d5c9036111032cdbbde5dfc2fefc18c6f841e722242c9b791c92ac" => :catalina
     sha256 "85678e40606c4bff6ff454ec15bafd2ab317887b2fb48865433d8cb0cdae7a3a" => :mojave
     sha256 "83c0dbca7a5c28564eba4e7a73894746004aab5025071b350c3c47271fc42625" => :high_sierra
