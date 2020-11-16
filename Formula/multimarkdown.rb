64c677fc8e4e949ede544cca8994d7b96b46dd3f
FX Coudert <fxcoudert@gmail.com>
2020-11-16 08:38:09 +0000

    multimarkdown: update 6.6.0 bottle.

--- Formula/multimarkdown.rb
+++ Formula/multimarkdown.rb
@@ -8,6 +8,7 @@ class Multimarkdown < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "15b87bf8b7be554761d0114af63d3789df6db6cb58616afc408f569ea8ac50d0" => :big_sur
     sha256 "f4a26eb7603d38d0f67db4edbde56334fce2024c1c78fd5f49a7b8b69ba48683" => :catalina
     sha256 "f095caaf1f01dd0611afcdfc77252dc2f21a3d89f8e41210e4d00307b835eb2d" => :mojave
     sha256 "308d597802afebc412f38df92dda2b98cef91845bb0e9c8e27d1bd2d38ee9d56" => :high_sierra
