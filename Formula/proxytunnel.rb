e55bd5536f92508404a9c90c975ffc7793271468
Jonathan Chang <me@jonathanchang.org>
2020-11-16 09:47:04 +0000

    proxytunnel: update 1.10.20200907 bottle.

--- Formula/proxytunnel.rb
+++ Formula/proxytunnel.rb
@@ -7,6 +7,7 @@ class Proxytunnel < Formula
 
   bottle do
     cellar :any
+    sha256 "3f06eb49b8c96b0e61876e08198fb5ab21e70b8b211c07cc23d8ece0d5249946" => :big_sur
     sha256 "18a5771a429cb73cc2f8de55c44ba2ba061238157458533302b5d609478039c7" => :catalina
     sha256 "257d06dda3279ca934dead8774ac3800bf97404af51d5fbfda7fba4ec99ce36c" => :mojave
     sha256 "46362f2b3cd8118b315e5eaae4c0fd5e391a2c741399f33c772cac3992221846" => :high_sierra
