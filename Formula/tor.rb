7b7e03d61eca716177ed866df018d9795dfcaf98
FX Coudert <fxcoudert@gmail.com>
2020-11-16 08:02:38 +0000

    tor: update 0.4.4.6 bottle.

--- Formula/tor.rb
+++ Formula/tor.rb
@@ -11,10 +11,10 @@ class Tor < Formula
   end
 
   bottle do
-    sha256 "85923e2089910ad3212bcb59f9f5bde647a32d02f4b88419fd1b02736381b43e" => :big_sur
-    sha256 "79aeb69314b4130ecfc3123bd34a425be5a8e9a89a97fb4efbdb9b1ce73aca7e" => :catalina
-    sha256 "904be653cf0e2fb3e7680f711288c0bba4cbe762b9dba8133796431ef2228d57" => :mojave
-    sha256 "3241af3dbfee60533efc5740897ecec05433789f3bcb9c92bd0c64bcd2481abb" => :high_sierra
+    sha256 "345a31685faa4b551c32ea7265191ef71db9dc90ab41af189535b831b6877f47" => :big_sur
+    sha256 "a6f0a222f1ce3670521392887eea1b491f4cefa8031e1cea7e2e33dea93d715b" => :catalina
+    sha256 "cc4678fc4cdf9a93cb5dc7f10a02df0e1bd950becd2b944afad59b7d64bbad3b" => :mojave
+    sha256 "52937701615bbe2ec97bdccd6dcd287a095f20128fc574fff1bfe04d775dac4a" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
