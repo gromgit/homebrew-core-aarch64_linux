e4770954c58109fbe8d75d77a6d42360b3fe1c78
Jonathan Chang <me@jonathanchang.org>
2020-11-16 19:03:25 +0000

    unifdef: update 2.12 bottle.

--- Formula/unifdef.rb
+++ Formula/unifdef.rb
@@ -7,6 +7,7 @@ class Unifdef < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "aa7b0d6df5dfa2fbaa5886881def3b22b1bb55917f3734f7aede03816c257b28" => :big_sur
     sha256 "ae908aa0c1845ce059576df3922808db790fb0ea91109f89aa930c8da7a68904" => :catalina
     sha256 "ded3ae5ba762ee493fc5ed45d0e6ed5a8261b4bf9bc2de54880f4a373b1ba075" => :mojave
     sha256 "74ec90a8fc2467e8d613f2a6347d973dcd6387fdd9734bc66088a64ace7e0d4d" => :high_sierra
