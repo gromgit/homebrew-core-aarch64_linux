d61ff1d437fdc11607a81e6d72ae883adca9e5fb
Jonathan Chang <me@jonathanchang.org>
2020-11-16 06:25:48 +0000

    dscanner: update 0.11.0 bottle.

--- Formula/dscanner.rb
+++ Formula/dscanner.rb
@@ -9,6 +9,7 @@ class Dscanner < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e88d2f23ed2dac725bd2fb4984f7c5271136054beeb32d261c634d2c892cacf0" => :big_sur
     sha256 "21c95d6cad0fa39bb72a762b371858bda0553a6520cffc4f7ab10114d9799366" => :catalina
     sha256 "35f721658f883a3eea9a2d979e31b8cb67ecf0f62a78112daa27a2c5c9ef4516" => :mojave
     sha256 "ae2a741fe6d195a79dd24ccce02afa7693742fee9454ad1d0f97c50c512a62a5" => :high_sierra
