034355e2850ef6d83ab6095b9f2c5454c3025929
FX Coudert <fxcoudert@gmail.com>
2020-11-16 12:04:18 +0000

    vulkan-headers: update 1.2.161 bottle.

--- Formula/vulkan-headers.rb
+++ Formula/vulkan-headers.rb
@@ -7,9 +7,10 @@ class VulkanHeaders < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6649473e272caf9186376d0ee5bace71dcb413de22ff8020db101929a618f845" => :catalina
-    sha256 "a226dcd65ef2260c0fadf1f8d39a3dafce11bba4644fea58f1e7701377045213" => :mojave
-    sha256 "4d604112d797af7da4b0c0d87110304937e12af376c77ec5944d2010364c5950" => :high_sierra
+    sha256 "7f240b89c18d3dbe05a52f891688c1ceacdfb0f2afb3d393eb846c23d64442b6" => :big_sur
+    sha256 "6c191b51b49ecda4cacb578ad944e63d626784135cbc467c837010d66ff2dd79" => :catalina
+    sha256 "e821c64a708cab7e30777abd29390e4ed34e9a9b4998338f5f8d7ecae9f58f26" => :mojave
+    sha256 "09161ad797df360d986fb964a5e4813d14b8ffc91b38e2f8fae13ec235756aab" => :high_sierra
   end
 
   depends_on "cmake" => :build
