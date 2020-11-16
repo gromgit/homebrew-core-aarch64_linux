605a161554440c3cac79ccc6b153b563a4b7eedd
FX Coudert <fxcoudert@gmail.com>
2020-11-16 19:49:49 +0000

    gcab: update 1.4_1 bottle.

--- Formula/gcab.rb
+++ Formula/gcab.rb
@@ -13,6 +13,7 @@ class Gcab < Formula
   end
 
   bottle do
+    sha256 "1d850c754fe6688bc5534637a9888215163d187569f80a4b57fc82f0e74aa14b" => :big_sur
     sha256 "7ed919ea9c7d4ec04f9d5f361f8628936e016318475fec26fdf6ef5ea56491cc" => :catalina
     sha256 "c9ef02142502a47b006db735b87fe7d55611d46ecc087c697d3142ce8bd9c27a" => :mojave
     sha256 "ca3d97d649c89be881528e7a7cf42f51c18c3a8e4c4b47c9a5fad29f355afd30" => :high_sierra
