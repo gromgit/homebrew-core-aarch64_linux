993a428d5e0e363987c8e1a340788a312704452d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-16 08:14:52 +0000

    kde-ki18n: update 5.76.0 bottle.

--- Formula/kde-ki18n.rb
+++ Formula/kde-ki18n.rb
@@ -6,10 +6,10 @@ class KdeKi18n < Formula
   head "https://invent.kde.org/frameworks/ki18n.git"
 
   bottle do
-    sha256 "384b80d8a17e7ffb2c58dde821c617135262bebb23d9fc1c7c615f71021aac1f" => :big_sur
-    sha256 "c3cde8b606dcad50a4bb84b1007929e0a4e250e420988cefb2eb088e1b551b3c" => :catalina
-    sha256 "da4701402263d28df238af8e2ec9df819d75282d9f550ab318bead98d7f3859b" => :mojave
-    sha256 "e0a76b6beb62d241d814461aa8eb41c0806a7fca65af6537cf2050ed9a2b0abf" => :high_sierra
+    sha256 "0e8403161f6fd5f090486bd0bdfebe2762f68ccf5d59fb5257a84369799869ac" => :big_sur
+    sha256 "26dd834903911b1ec75733cd98871585843b60ac8608a19ee4ae8ae467113106" => :catalina
+    sha256 "bba65f49e90464862f8dbd975c6ddffd29fa205c685e1f2199db857636d4659f" => :mojave
+    sha256 "548b625ee495995b8b566395de47f8f823a1c39b52c046b1498472bfa46b2f50" => :high_sierra
   end
 
   depends_on "cmake" => [:build, :test]
