81538e12b7410c1f4d7c9d63356a989057874fe6
Jonathan Chang <me@jonathanchang.org>
2020-11-16 03:41:31 +0000

    paperkey: update 1.6 bottle.

--- Formula/paperkey.rb
+++ Formula/paperkey.rb
@@ -12,6 +12,7 @@ class Paperkey < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c6af240418bfb7c29113a1861966302d2be55fc578298f6fb0a4f71bc8dbf89e" => :big_sur
     sha256 "12be9f841cfb0d4069be3e461cd5e783ba4ea11195507a13763f90ccc026f31e" => :catalina
     sha256 "894ef3339013be6574f736e316c61cbf54fbc3dcac358df14f1d54b1d7387854" => :mojave
     sha256 "82e49c6aa559a349ce73521a90881acb74a540de03d355ad7461c177d00bb8e8" => :high_sierra
