df5b6558af6bc9171f5cca2a529271a40342adec
FX Coudert <fxcoudert@gmail.com>
2020-11-16 16:23:24 +0000

    lxc: update 4.8 bottle.

--- Formula/lxc.rb
+++ Formula/lxc.rb
@@ -12,6 +12,7 @@ class Lxc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "54a721303f341f48a4dfe85983eb3ad80e36a5f1047c46087400fbdbb11a33aa" => :big_sur
     sha256 "8675b214d8d8dba0cea05084104952fce09f25ef43e0a3b1759d2b736823cda5" => :catalina
     sha256 "9a8303d460b15e9fd4c95ff980df5485efa2011fc96170f50fbf851c3158b3da" => :mojave
     sha256 "ebd08abe30cd9d0e27cad7b494b0b850c552f98770d7bf4181f7093fb09926d5" => :high_sierra
