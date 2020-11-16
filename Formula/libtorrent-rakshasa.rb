917058b91cd670ddf71760fb67ad45d41c5f80d9
FX Coudert <fxcoudert@gmail.com>
2020-11-16 20:18:30 +0000

    libtorrent-rakshasa: update 0.13.8 bottle.

--- Formula/libtorrent-rakshasa.rb
+++ Formula/libtorrent-rakshasa.rb
@@ -12,6 +12,7 @@ class LibtorrentRakshasa < Formula
 
   bottle do
     cellar :any
+    sha256 "1d2275d886729ab6076582c7b399df2e0748f8a50b31f88a3a2f871ca097985d" => :big_sur
     sha256 "207e33009028a8721a89c91139fe78fea1cd9fb8a05862286264dfc53548886a" => :catalina
     sha256 "94afd9fcef673d4e3945c13085df931e12c1bf2422bf6a2ad2c6848634c2fa65" => :mojave
     sha256 "135df02ce3bb98b05d9f849b8014087e8acaefcc24b1547ff9b1740bbd74492a" => :high_sierra
