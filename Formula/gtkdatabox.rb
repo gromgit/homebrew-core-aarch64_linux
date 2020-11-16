62d47383a1cab0493ab66ef523f2d5cda89dbdfb
Jonathan Chang <me@jonathanchang.org>
2020-11-16 04:45:47 +0000

    gtkdatabox: update 0.9.3.1 bottle.

--- Formula/gtkdatabox.rb
+++ Formula/gtkdatabox.rb
@@ -11,6 +11,7 @@ class Gtkdatabox < Formula
 
   bottle do
     cellar :any
+    sha256 "4dbfe9fe9f52c6bef15219cbb413d49528957a5db32d82510bed44336569d761" => :big_sur
     sha256 "e85124ef7180c8803f8700e9269a47d3b5841c1cdace25e6914a50a0b7389dd1" => :catalina
     sha256 "bfb185bdad65010d94e7ec0d343f11dafb0135adecf0480a43b705a54b062ff1" => :mojave
     sha256 "f0b09b52354b68f4bc4e2d01dcd970f5f5f6f549141dd75af3734a9b3c1f0b2f" => :high_sierra
