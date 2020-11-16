f5bf45d4f141c11df519596a2bec8290547d300a
Jonathan Chang <me@jonathanchang.org>
2020-11-16 15:11:34 +0000

    qjackctl: update 0.6.3 bottle.

--- Formula/qjackctl.rb
+++ Formula/qjackctl.rb
@@ -12,6 +12,7 @@ class Qjackctl < Formula
   end
 
   bottle do
+    sha256 "14ca4a66d897c1b8d6dcec1da501b778a11bd175be0623fa79ee056451bf3fc2" => :big_sur
     sha256 "8e30b2f2b2587c3177287a8a47f785862d0f6201e92f8cfc90ea16e17e2e405c" => :catalina
     sha256 "902219e2f8d6e223a2375eeeb92470e0fefce0acb4d960b250a5c2ac5e5cef97" => :mojave
     sha256 "c77bf5a0063a9c265b1c9c343a47e399f000a49acc496ace4c081398fa6e16cb" => :high_sierra
