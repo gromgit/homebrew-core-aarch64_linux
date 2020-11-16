dc5252a138e0ce55a27083356b6e80ae5bfcae78
Jonathan Chang <me@jonathanchang.org>
2020-11-16 02:01:55 +0000

    bookloupe: update 2.0_1 bottle.

--- Formula/bookloupe.rb
+++ Formula/bookloupe.rb
@@ -7,6 +7,7 @@ class Bookloupe < Formula
 
   bottle do
     cellar :any
+    sha256 "7ccdee4a97e6c705e478e38aeca1648b06a39c2edfcfa807a4a07ab12eb0d3c8" => :big_sur
     sha256 "83e920e882a00717b094b14477917ed477fa3ab9ae02433d79bf4d374d5723a6" => :catalina
     sha256 "f5e7f38cfa342d15025f798e9476a7091d3dbd60a15a6635d9fd784033dd531c" => :mojave
     sha256 "8cade7bb36828e32d7be412d29404748198079745defd97ed2ec533ff91f5645" => :high_sierra
