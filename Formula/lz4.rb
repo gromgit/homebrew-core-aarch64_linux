8290992cb73758b29c5ddd0f70a1c8939426aeeb
miccal <miccal.matthews@gmail.com>
2020-11-16 16:05:05 +0800

    lz4 1.9.3
    Closes #64915.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/lz4.rb
+++ Formula/lz4.rb
@@ -1,8 +1,8 @@
 class Lz4 < Formula
   desc "Extremely Fast Compression algorithm"
   homepage "https://lz4.github.io/lz4/"
-  url "https://github.com/lz4/lz4/archive/v1.9.2.tar.gz"
-  sha256 "658ba6191fa44c92280d4aa2c271b0f4fbc0e34d249578dd05e50e76d0e5efcc"
+  url "https://github.com/lz4/lz4/archive/v1.9.3.tar.gz"
+  sha256 "030644df4611007ff7dc962d981f390361e6c97a34e5cbc393ddfbe019ffe2c1"
   license "BSD-2-Clause"
   head "https://github.com/lz4/lz4.git"
 
