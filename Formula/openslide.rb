f38434e0957847c84c3dc123a5c4aea5b64d1cf3
Jonathan Chang <me@jonathanchang.org>
2020-11-16 09:45:29 +0000

    openslide: update 3.4.1_5 bottle.

--- Formula/openslide.rb
+++ Formula/openslide.rb
@@ -8,6 +8,7 @@ class Openslide < Formula
 
   bottle do
     cellar :any
+    sha256 "5cc9273868f2816ac3b2cfd9d12bcbc44d68fb461000469896c4f10b8047511f" => :big_sur
     sha256 "6b59207518417bb5a45a716d6c26a01ed8d9977af51992b00d0479a7d9a4ffae" => :catalina
     sha256 "d90e3ee5514064389cea2bdf1d4369cc2be4e1d965ac9d56f47e0c6e22f310af" => :mojave
     sha256 "1d70f22fad80e061bcfa5d4955d522d37bd077c51cad4697579a104759233ad2" => :high_sierra
