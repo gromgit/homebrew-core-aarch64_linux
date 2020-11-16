a2a0c8c8bbf1e6dce0d5f99c5d7c2c54d940b61f
Jonathan Chang <me@jonathanchang.org>
2020-11-16 05:46:33 +0000

    pdftohtml: update 0.40a bottle.

--- Formula/pdftohtml.rb
+++ Formula/pdftohtml.rb
@@ -12,6 +12,7 @@ class Pdftohtml < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "17a699cdc73b7674f537adec7ea8ad696374921c84a52dfaacaa05882f5696da" => :big_sur
     sha256 "d8a6e5bb1d84ee766898543d77307b4a9a6e6f826ebe9cc48ce6db8bb24c8923" => :catalina
     sha256 "c49245634c48c7c24501cfb848a98e4b6a281ff0cf89235bb7a7ce09619e66ad" => :mojave
     sha256 "200be428031e013f58b792b092b56e74743d6362d747b0c883bb95269d7a5e72" => :high_sierra
