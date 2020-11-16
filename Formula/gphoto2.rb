60f68dab7ffeec4f116b8d260fade5f09b50742b
Jonathan Chang <me@jonathanchang.org>
2020-11-16 01:59:55 +0000

    gphoto2: update 2.5.26 bottle.

--- Formula/gphoto2.rb
+++ Formula/gphoto2.rb
@@ -12,6 +12,7 @@ class Gphoto2 < Formula
 
   bottle do
     cellar :any
+    sha256 "f78b5393c2d68ce33d628f59222612882fe93738ecbeffe76c2de4abccc3c296" => :big_sur
     sha256 "c4c42c50ee8bddf1a6b930f0eed9165a9e45ef1fb92476616833726b002ab077" => :catalina
     sha256 "3b0f309d8d21165b945bb2c22a29f55396cf345a3f9255bc143ab4020fae5442" => :mojave
     sha256 "a5c57a8b09c87b31c07cd3dea6ca6c859486c55268aa9745df740b1ac52368b2" => :high_sierra
