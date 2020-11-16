27ab98d571b2e5238119735b9c856f0b3ba1d61e
Jonathan Chang <me@jonathanchang.org>
2020-11-16 04:14:38 +0000

    breezy: update 3.1.0_1 bottle.

--- Formula/breezy.rb
+++ Formula/breezy.rb
@@ -14,6 +14,7 @@ class Breezy < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "573375e163fbfa507e6fbd20e52559b30ca1c97ba8ef2f45eecc3b4ae8b4bb0d" => :big_sur
     sha256 "1a5f132188241df2ada428e97f0dc09712b9bbf803dc4ea974e07b38f9ddc247" => :catalina
     sha256 "ef7f756a7ff7beb049bde8f7a9e41eba5cc2f331c7efdb9d8b5d8ff419836384" => :mojave
     sha256 "d818d00021c542d21438b4014f5e9461378144de16d2525c52868d5d2998922c" => :high_sierra
