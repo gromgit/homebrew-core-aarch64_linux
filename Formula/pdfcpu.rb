289970f6bc80fbafc9cc3f7f67d9de62a4eda477
Jonathan Chang <me@jonathanchang.org>
2020-11-16 21:20:07 +0000

    pdfcpu: update 0.3.7 bottle.

--- Formula/pdfcpu.rb
+++ Formula/pdfcpu.rb
@@ -7,6 +7,7 @@ class Pdfcpu < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7b4663e900c5f692f973cd821fffd7eb6568300706710f6ef4054fcc4dc33135" => :big_sur
     sha256 "ac34d106830f0151a271addd85bb96c74826a8a48b8a2c7a1f5518df6db628d5" => :catalina
     sha256 "293b075f32a26c3b835b9a45f125c61cc87273bea16449b4785d791501dc8170" => :mojave
     sha256 "d3485d7bd4d37008a2e4d38b7f8cb772eb9b76827dd198056ef3895296611842" => :high_sierra
