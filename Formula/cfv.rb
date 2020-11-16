4674d1f939c94008d172ad326ec9eecd55dc5a13
Jonathan Chang <me@jonathanchang.org>
2020-11-16 13:10:52 +0000

    cfv: update 1.18.3 bottle.

--- Formula/cfv.rb
+++ Formula/cfv.rb
@@ -12,6 +12,7 @@ class Cfv < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "8ce1654aa4805deb6a80b9ec470306b879b79d5e25e151df1e4c78e498c0e214" => :big_sur
     sha256 "cd4fac08aac6490ade28d8b370e006c720bab5df939caadb92b25af278a4384a" => :catalina
     sha256 "251348813c0a811e6ac298432967d19e42bfa73bbc3217eaa0b63bec4b78d98d" => :mojave
     sha256 "7452ead7901f4f4ab2683cd391af82f856eba1a57c11d07c038ca18507535dac" => :high_sierra
