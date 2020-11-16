30853f5502bc70aeb718ca34b1b69f7072f5123a
Jonathan Chang <me@jonathanchang.org>
2020-11-16 06:19:59 +0000

    uptimed: update 0.4.3 bottle.

--- Formula/uptimed.rb
+++ Formula/uptimed.rb
@@ -7,6 +7,7 @@ class Uptimed < Formula
 
   bottle do
     cellar :any
+    sha256 "fc45435953f14fc7a182884dfa4e2672213c71675c9e73fb6a3799a140e66caa" => :big_sur
     sha256 "1e20c4955ff14a05da57be77e08e163e164e41995411c21aeaa5a5bf3919fb7c" => :catalina
     sha256 "d5d96957debd223a243d71dc0d9858d19179c94841f6640822b1db841c0bfd48" => :mojave
     sha256 "8585595184bf697772b292e123f63c97513e242c7d04194c9e1990d60fcef571" => :high_sierra
