115a6329f4363f397afe326c220803586d938df6
Jonathan Chang <me@jonathanchang.org>
2020-11-16 12:13:14 +0000

    dieharder: update 3.31.1_3 bottle.

--- Formula/dieharder.rb
+++ Formula/dieharder.rb
@@ -7,6 +7,7 @@ class Dieharder < Formula
 
   bottle do
     cellar :any
+    sha256 "24603f6e3c5376e294cdcd0d94cc045e48dec3402fd69a3b927ec1291f7b5c26" => :big_sur
     sha256 "3f53c783d640819b446cbf91c3293d47aa0b0c334a630d25f2c5b5b514aeb844" => :catalina
     sha256 "b7b1bdbb6f105e4286320ad067689d8e3f7a2c7821a53382ebc2007b47d06dc9" => :mojave
     sha256 "341bdf1e0fce90d69db4e6749ec3ee3b8c5903559e365a19e9f5a8ba2723d403" => :high_sierra
