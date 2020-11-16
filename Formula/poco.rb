bc5a9ae34773af7ecf1187218d8dcecf35c74828
Jonathan Chang <me@jonathanchang.org>
2020-11-16 06:33:24 +0000

    poco: update 1.10.1 bottle.

--- Formula/poco.rb
+++ Formula/poco.rb
@@ -13,6 +13,7 @@ class Poco < Formula
 
   bottle do
     cellar :any
+    sha256 "a2483cf9eaff5857285e2ec3cc4086f74a7edfb240815e75bca7ba153861f1c5" => :big_sur
     sha256 "0755dff1346ea80aa6202ce3e8269c608960abd4bf0a4566e56075cc99364b57" => :catalina
     sha256 "7abccb2c17823c6dda9dee9e5918fa28ef846d8095252681c83c47bbb674f5c8" => :mojave
     sha256 "70cea3a570e187c3e70a8dbbe1ad2e43be1c159d0d9118c1bfc1a8cc6441e2a4" => :high_sierra
