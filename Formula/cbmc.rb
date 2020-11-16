f024598453218dcb1e0d313037bdaefb400b4662
Jonathan Chang <me@jonathanchang.org>
2020-11-16 13:50:25 +0000

    cbmc: update 5.18.0 bottle.

--- Formula/cbmc.rb
+++ Formula/cbmc.rb
@@ -8,6 +8,7 @@ class Cbmc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9b5eaa087d9c11e5ac8120362ebfe3d5fe76f9fd5313e21848929abfd0f516a7" => :big_sur
     sha256 "ed3c1e642156409d322f635bf7c140f6d7c7ce6958003878f4b02e9340515b63" => :catalina
     sha256 "b81ec3bb2734293ed39d0c336c1c6445b7d87032994e642ea7ff5b195fbe16e5" => :mojave
     sha256 "d7db72ec3bbb3f568e9ba432f5b146eb20d15b8a2fc5c4e3f9680d0d27852ce8" => :high_sierra
