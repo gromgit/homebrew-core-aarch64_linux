02cd84e62e52095993e5fc4ae9801492926d73d6
FX Coudert <fxcoudert@gmail.com>
2020-11-16 12:13:08 +0000

    cogl: update 1.22.8 bottle.

--- Formula/cogl.rb
+++ Formula/cogl.rb
@@ -9,6 +9,7 @@ class Cogl < Formula
   end
 
   bottle do
+    sha256 "ec1ef03d2e1e855ae5277a2f599fb7ed83c221f0ae29d8c8a5f45277be96d869" => :big_sur
     sha256 "37fdd46a2845adf0e8f4ce85d5a80384ea235e435ef5f42167622f5224e4e51f" => :catalina
     sha256 "eb37baaa178631afac43c8bb1c93cdf9b78dd7d44862c63dec598d54a51b201e" => :mojave
     sha256 "46de52386a1123e828d94598279a99a88e3819d8f1dac1a51f39850a321ff7f2" => :high_sierra
