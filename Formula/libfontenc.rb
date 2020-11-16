129f333d3a22d416a86ac54bfa13f34da3f944ec
FX Coudert <fxcoudert@gmail.com>
2020-11-16 13:52:10 +0000

    libfontenc: update 1.1.4 bottle.

--- Formula/libfontenc.rb
+++ Formula/libfontenc.rb
@@ -7,6 +7,7 @@ class Libfontenc < Formula
 
   bottle do
     cellar :any
+    sha256 "05f64e556ed6c0576407b084aba036ff0fed95f831778ad8b6363b4fe6f0836e" => :big_sur
     sha256 "b57f18c5d875f7ceded9115cd1971be92a3c3887c7c606ff5028ea1ddb160b1c" => :catalina
     sha256 "e79ca92c5f40a57da0b5745df2eb64fe532e90bbf292e4d97a4703d5b0e15791" => :mojave
     sha256 "83bf803c71ed38edf5204ee3bdd89f6569c4c04a41c64003f20ab67b7021f2f2" => :high_sierra
