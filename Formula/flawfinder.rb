943045165e961f014e2473ca746221fad269b0ab
Jonathan Chang <me@jonathanchang.org>
2020-11-16 15:23:52 +0000

    flawfinder: update 2.0.11_2 bottle.

--- Formula/flawfinder.rb
+++ Formula/flawfinder.rb
@@ -16,6 +16,7 @@ class Flawfinder < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "79d63099bc5b10544dd3a42ea7f2b71247ed13569846917537bd992241051fed" => :big_sur
     sha256 "c4b2f7f2353d8d032e0e1f352860f69c23c21092dc718f4d4832bc1dbe20a2f1" => :catalina
     sha256 "ba1a903081761bfb7ba492ac5fc84300da3ffc20819ed240a6854c3441b69eff" => :mojave
     sha256 "38fc9609b4526463f7d4d3d4dd1377fb979a64208562de1a5d4d6f1ff9237c67" => :high_sierra
