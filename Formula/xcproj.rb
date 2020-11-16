dad84fcfeb6b44a4581b28a6a14ee2ff4d24aff6
Jonathan Chang <me@jonathanchang.org>
2020-11-16 03:05:51 +0000

    xcproj: update 0.2.1 bottle.

--- Formula/xcproj.rb
+++ Formula/xcproj.rb
@@ -7,6 +7,7 @@ class Xcproj < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e153fbfaf308d9fa35f228a0ad5ab4f1fbbbac9f7040eb0fc1ace90ba9541b7c" => :big_sur
     sha256 "46aa93499933dd1599eb4d38ba2e5b8587092c08f8acb691b29a1ccee6a80b17" => :catalina
     sha256 "7efa30f2f581bbcc0962605710b1125965b6b8d13ca8e5fab8517adfe1c9334d" => :mojave
     sha256 "d34b031444c1122392afb789036d3197a0d333ae11447c819509f1f31de30c9f" => :high_sierra
