ae82ae74c54cb3b6389f413eb56cd76838abe084
Jonathan Chang <me@jonathanchang.org>
2020-11-16 21:09:21 +0000

    mr: update 1.20180726 bottle.

--- Formula/mr.rb
+++ Formula/mr.rb
@@ -8,6 +8,7 @@ class Mr < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9b2f724c76a7b2a2504301eeb2dcd5d16d2b919e53fe7f43b404c79f56ce7c75" => :big_sur
     sha256 "90ab23bd6811b507860b5ddcc7e9a181abd3f126fc2ab193739987d6d4b31612" => :catalina
     sha256 "73c8b9b421ea776366f9ded68d90c6c3b75b50401172b5c5248556f6f7f47d6e" => :mojave
     sha256 "a41bcee5b050ec9f98cf5960a457421528b05773867d8c8dbb8eb32716e09fd5" => :high_sierra
