32c70bbcc1c9ef69c7feed3389214c59dc1ad378
Jonathan Chang <me@jonathanchang.org>
2020-11-16 07:15:22 +0000

    sipp: update 3.6.1 bottle.

--- Formula/sipp.rb
+++ Formula/sipp.rb
@@ -7,6 +7,7 @@ class Sipp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "334632e3c7eb535151bb272f1dd3b5f8679682ed6930313fc91f51366a2cebe6" => :big_sur
     sha256 "b5363f025eb4ab69bd4a5d255396115d16ec5740a7de7cc810cffaed2b6e8b7a" => :catalina
     sha256 "43d25e09e529be8802eca6fcc67ea3a9b50927acf71ecc3095a074b8f693afb5" => :mojave
     sha256 "41183e24e41ad9d05017652d0e1ed94e3aeb74883daa165210eafc7f6e034ab7" => :high_sierra
