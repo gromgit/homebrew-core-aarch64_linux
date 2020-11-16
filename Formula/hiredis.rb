15a3feac64e3a249f40a0e45acc790f20498a022
Jonathan Chang <me@jonathanchang.org>
2020-11-16 14:33:38 +0000

    hiredis: update 0.14.1_1 bottle.

--- Formula/hiredis.rb
+++ Formula/hiredis.rb
@@ -9,6 +9,7 @@ class Hiredis < Formula
 
   bottle do
     cellar :any
+    sha256 "e56680ffe95af8db4989e9a0d9bb0fe546cd61b337049d570cfb073cff8a066c" => :big_sur
     sha256 "4b089bee868b4fe533bea75917f21a42a11a9e04d5b396ec5a94d5623372ec0b" => :catalina
     sha256 "8974f8bacac7ce018f6f238915c6c5f07751c5183283337634a1715624d344b8" => :mojave
     sha256 "5ed94f51e6dbe91f7d195be5a5d085c7929f82bafeecba8645dd2315a94dad8d" => :high_sierra
