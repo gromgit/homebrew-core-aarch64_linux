4a6d5683d24f9e846a0f4203435253cce7dc3ae7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-16 00:34:49 +0000

    amtterm: update 1.6 bottle.

--- Formula/amtterm.rb
+++ Formula/amtterm.rb
@@ -8,12 +8,11 @@ class Amtterm < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "21db28f850cc553e66e71cf419cf7a975e18e73b6185b3fc1a1f244944d21706" => :catalina
-    sha256 "f8ed67be384a24abd8114f058092b923e101a2991f32f712d437c6a292b40a12" => :mojave
-    sha256 "eef6949767ce3e19a88f0eb4b146b1444ea39541ab7a075311a8bf11a327f781" => :high_sierra
-    sha256 "256736b4dd1d0b427bdb7ca2f9931cc1c403ffe1e43695920160f4f3ffabfe88" => :sierra
-    sha256 "4035d9bcb4f6785255a5ea3df1dcc309dbf69441b69dde15b1936e8522fc8e95" => :el_capitan
-    sha256 "387897642ba8bcb6daff01adae021264eaf9deea1f264e210e9592b6bc5cc44a" => :yosemite
+    rebuild 1
+    sha256 "3de2c8131b610bcf1d4d9cf1bb537d2a66b19dbc49e76f26e0ca280e48c1827c" => :big_sur
+    sha256 "ed7067b9e98f43c6a13bd5dc43b5467508e33f209399b4e276da21091ae74907" => :catalina
+    sha256 "aab6ab711f9b407ef0df77a386b005cc8d10f7c0fb3c9c581659fea65e0edd00" => :mojave
+    sha256 "29180333af292e440f077a00a958ceb6f0035bcee9945233bc33177d0b3549f2" => :high_sierra
   end
 
   resource "SOAP::Lite" do
