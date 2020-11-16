0ceddbcda5edc2cb4e98cbc769f897d547ba8fe1
Jonathan Chang <me@jonathanchang.org>
2020-11-16 06:18:53 +0000

    apib: update 1.2.1 bottle.

--- Formula/apib.rb
+++ Formula/apib.rb
@@ -8,6 +8,7 @@ class Apib < Formula
 
   bottle do
     cellar :any
+    sha256 "24b8a6e851fe6e262765131c025e8dddada1c740ed7f3aa978d4fbc3bec939c1" => :big_sur
     sha256 "b925a48cd3a9047184b5373e4718e4142117a487467cea95be9f43f6c9951712" => :catalina
     sha256 "fd1f74b58c7a51240d463c8ec1203bce33677409aca81ba0ffba9ea718471433" => :mojave
     sha256 "aee416aff5715b96b81cfeda0be036a2510ffe760187e396c143a64bae2c25c2" => :high_sierra
