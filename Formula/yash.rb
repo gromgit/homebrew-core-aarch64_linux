d2b3fa40d4f0fe17affe03f2206a20273c9bb3db
Jonathan Chang <me@jonathanchang.org>
2020-11-16 07:52:39 +0000

    yash: update 2.50 bottle.

--- Formula/yash.rb
+++ Formula/yash.rb
@@ -12,6 +12,7 @@ class Yash < Formula
   end
 
   bottle do
+    sha256 "e9ce00f183e818cc273cae2748dc2c5021565083870351b5ac391548baa49580" => :big_sur
     sha256 "7e92afc1391f9d80bcab553e29705fd0cb11c88ae10448a7d57d408284c8490f" => :catalina
     sha256 "1ce52a85f6ed173af4e6e296a1051b08536a31d7e7c8704d0324bb4d2c751fad" => :mojave
     sha256 "026106ab6ee3296619ad4cd762756d4676713feb7060172cabc39e92d8f222a0" => :high_sierra
