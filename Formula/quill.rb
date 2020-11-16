81db8058a0c7c64d0d6fe72e2eec7ccf75f59b0d
Jonathan Chang <me@jonathanchang.org>
2020-11-16 07:15:17 +0000

    quill: update 1.5.2 bottle.

--- Formula/quill.rb
+++ Formula/quill.rb
@@ -8,6 +8,7 @@ class Quill < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9bd7b9972f4b0dba81202bb49a47695c08e7b6581627cd7df94e8072c66c0aa5" => :big_sur
     sha256 "005bd68d183a4ef347d73b59ddbd273b1c8bc2d35400f5acef88294fbb288bc8" => :catalina
     sha256 "a665a6c752e4482c0582bdbd0fe1248f72b5369bce53d331bae366db53fe2b10" => :mojave
     sha256 "d76aae3da313beb3338b134472377298f5fb56d6d619bd0cd1465557e25870c5" => :high_sierra
