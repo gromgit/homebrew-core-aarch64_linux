3ef3011b002777efe2f4d7ff1de9a73aadc585c5
Jonathan Chang <me@jonathanchang.org>
2020-11-16 23:23:22 +0000

    irrtoolset: update 5.1.3 bottle.

--- Formula/irrtoolset.rb
+++ Formula/irrtoolset.rb
@@ -13,6 +13,7 @@ class Irrtoolset < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "632c6b4036c71036b6b4038816dd20a3f791e9d06aab981f01429fc07bb4d3a3" => :big_sur
     sha256 "958df309df54264b13dba2185761e5d4ce1397e3c6b079dbd9396e054d02d306" => :catalina
     sha256 "fd790b230ed1c3559d79c5e86080a6c5163d71817c13980a3abc904e15535d98" => :mojave
     sha256 "250f93336659350a65426d86c28053763f530b56ae9513b44f086196a91a59c3" => :high_sierra
