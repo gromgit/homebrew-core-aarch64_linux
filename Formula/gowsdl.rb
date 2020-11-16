679eba3b42019468cd2d7f7fe67fd1861db53b04
Jonathan Chang <me@jonathanchang.org>
2020-11-16 08:09:49 +0000

    gowsdl: update 0.4.0 bottle.

--- Formula/gowsdl.rb
+++ Formula/gowsdl.rb
@@ -9,6 +9,7 @@ class Gowsdl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1651f1b4a5e360d71f2dd5aafb947810afec7b30e9a446bc21de460106266188" => :big_sur
     sha256 "ac79869fae62091277dad289c73f337cee0c9d92a42b38af5dc4d59b53f59885" => :catalina
     sha256 "650f3a704f6918e069c60d64807ddffe3fd327b6ef2e5688768d08f646ccfa59" => :mojave
     sha256 "64d483de68fc8f23043ee9652addf1a918e79a3c68c70324652ab5d0ac4b1c64" => :high_sierra
