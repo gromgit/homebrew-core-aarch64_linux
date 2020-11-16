bf409661ad7d7f17d041f473ad44ab58004b9c05
Jonathan Chang <me@jonathanchang.org>
2020-11-16 10:08:44 +0000

    devtodo: update 0.1.20_2 bottle.

--- Formula/devtodo.rb
+++ Formula/devtodo.rb
@@ -6,6 +6,7 @@ class Devtodo < Formula
   revision 2
 
   bottle do
+    sha256 "fd6bd62ec91c76dcbb047b0eb8cfe9ffaf7ff045913493dc5070ee0c090d7809" => :big_sur
     sha256 "24cc0693b8b69ac2fe7d926cbc3b8fae6e09df83b6a979ac71c68b8ae5ee6196" => :catalina
     sha256 "80c04083f48a5791985a5cf02e86ddff3e40b4523177a947f0bd3f7f066f47a1" => :mojave
     sha256 "1d5279b22730d8983887f91866a913b4714f8e453e382116bee294bbacee0e97" => :high_sierra
