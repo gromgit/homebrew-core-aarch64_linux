e517791af24f0ad24956033063702760da138ea3
Jonathan Chang <me@jonathanchang.org>
2020-11-16 03:49:11 +0000

    glslviewer: update 1.6.8 bottle.

--- Formula/glslviewer.rb
+++ Formula/glslviewer.rb
@@ -8,6 +8,7 @@ class Glslviewer < Formula
 
   bottle do
     cellar :any
+    sha256 "f3ce8f0e2fc1954a66507e0035001a53999b3db181800a7e603adb49d2f545e7" => :big_sur
     sha256 "8af79674f9aec125cac3abaca849ac39aaaf6189685818d95509cec45c026536" => :catalina
     sha256 "db43cd0ba38d9cf6a1d73f2be1b4d4d3bc0c8224219ef9a8c15449c89f75bd51" => :mojave
     sha256 "b491ac4f3101dd7c0ba68d66f0df68400522aa1ab9d777f7c1f29ffabb56c3c2" => :high_sierra
