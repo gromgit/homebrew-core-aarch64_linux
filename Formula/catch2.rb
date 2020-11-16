a15bfa898e525700b6dd51c335fa03942b4b421b
Jonathan Chang <me@jonathanchang.org>
2020-11-16 01:57:25 +0000

    catch2: update 2.13.3 bottle.

--- Formula/catch2.rb
+++ Formula/catch2.rb
@@ -7,6 +7,7 @@ class Catch2 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "10297553fd45e5400ccdea141a4dcd446e7fc0d435ff6fc6642bd085442c4e86" => :big_sur
     sha256 "958032c9813ff1bed035894299c9acc08c87503a14c1a32f0ad60cc9cc6c32ba" => :catalina
     sha256 "09118ef07a7fa43e6327e5034b2f2aa3ba02aae42c86f6aab1d1ee7d914f0dd4" => :mojave
     sha256 "71e22734d3fe0c6c9396679432fa8c5100f176055cfd865e2eb195a7b3634fe1" => :high_sierra
