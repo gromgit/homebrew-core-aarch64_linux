cf4a90e5c77bab6fcfcd118e93323be2a507322c
Jonathan Chang <me@jonathanchang.org>
2020-11-16 14:31:16 +0000

    dcfldd: update 1.3.4-1 bottle.

--- Formula/dcfldd.rb
+++ Formula/dcfldd.rb
@@ -11,6 +11,7 @@ class Dcfldd < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e9355a9c6885f7c22087b05988e99e5fcf563c1bcd9856ee5de0d207e1dfa54e" => :big_sur
     sha256 "17bf5e7a79a3453103e9fd5a70f0b12d49c93c5302a6ada8bd021ca918979992" => :catalina
     sha256 "63b3928acc96ad685b064fa3de4f44c4b96d1cbb610d4ea8b7c205a41385a4e7" => :mojave
     sha256 "95b0c080c543745a3a81751cc175fb99a1b75a7e124518d8e5d3337b76a97e72" => :high_sierra
