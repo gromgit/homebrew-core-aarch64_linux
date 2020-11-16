80e92a604d3fb2f4dc12fc393b308df44079a3e7
FX Coudert <fxcoudert@gmail.com>
2020-11-16 20:33:08 +0000

    userspace-rcu: update 0.12.1 bottle.

--- Formula/userspace-rcu.rb
+++ Formula/userspace-rcu.rb
@@ -11,6 +11,7 @@ class UserspaceRcu < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b32209eb45a390b770b5aec78ee0f400b51a89b7bdf775c720f521bb9df11836" => :big_sur
     sha256 "ddb5e2f5e985cba860f36a5730d933d88b4b0e76b1a450a3e76b244a5a6f1935" => :catalina
     sha256 "09cbfc5e663214ad2df4e95cb2cf022ea6c153c6bc49d6918ec5c2e69e28a97b" => :mojave
     sha256 "501a8f37d104b1a8f5cb625d2e1a17615114caf57054b9df9fb52df62761f138" => :high_sierra
