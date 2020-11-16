2af49bd337979c6403d02e0850a0e9f321e008e7
Jonathan Chang <me@jonathanchang.org>
2020-11-16 07:30:02 +0000

    functionalplus: update 0.2.10 bottle.

--- Formula/functionalplus.rb
+++ Formula/functionalplus.rb
@@ -9,6 +9,7 @@ class Functionalplus < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e30a27bf78a8820d4d63a4ae87520afc97d3f1a0d93ac650b7a9fad79589b47a" => :big_sur
     sha256 "8fee519ccd0485bd871b10f8d12d2324efd929c69d8b2aac3754fb47a4313e2c" => :catalina
     sha256 "d4785dd619c7e46280a175f47ccf8dab03c076190fd2c5a11724bfae6d785e1d" => :mojave
     sha256 "5102b2c7d93ed23aa46aa46e8c455b0edd2b54556c69be9aa459775a2faeed1d" => :high_sierra
