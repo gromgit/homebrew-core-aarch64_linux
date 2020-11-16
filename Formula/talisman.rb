15615fb7022c67be45d02d43d0064982f9d9424d
Jonathan Chang <me@jonathanchang.org>
2020-11-16 12:07:04 +0000

    talisman: update 1.10.0 bottle.

--- Formula/talisman.rb
+++ Formula/talisman.rb
@@ -7,6 +7,7 @@ class Talisman < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f5a8ea0a5a329508e1097c444fb30b048c21a718924fc74058fd272d2d041101" => :big_sur
     sha256 "a6682d9be9f24e3e7251a5006643bd518d485d1f32d1b2a61d52efeea08317e7" => :catalina
     sha256 "468ada5252b7c8383ef357f3cd2d39d43c8870d55638a4bcdf022c0b6b6715df" => :mojave
     sha256 "ed7afe4da9e5d164521b978dc6c680b8026e54fd6a21c0db27e5474d4e89a72e" => :high_sierra
