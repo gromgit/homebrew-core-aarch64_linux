5d95326faa2248534850ffbafccf243877887f41
Jonathan Chang <me@jonathanchang.org>
2020-11-16 10:32:00 +0000

    tth: update 4.15 bottle.

--- Formula/tth.rb
+++ Formula/tth.rb
@@ -11,6 +11,7 @@ class Tth < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9f8333051d4b9025641302175bac6db82a883dc722c8dd8286a424af16cab3a1" => :big_sur
     sha256 "eba438c9ec2902578cd3a043fdfbc1fa6bf22bedbde36e868c4fe02740448b06" => :catalina
     sha256 "a0fcd3029a8a43a59a530537c5fef4df45c0155924f41b07ac14ec2362d73ba7" => :mojave
     sha256 "dc8224b651a9bbb3ab8a5f5dd2e213bfd42518962bd7132cb3d8a2db12ddd5bb" => :high_sierra
