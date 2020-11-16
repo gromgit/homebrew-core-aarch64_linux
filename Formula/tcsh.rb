c986bb663c6480bcda8da91e9ac498a63abf109e
Jonathan Chang <me@jonathanchang.org>
2020-11-16 09:49:19 +0000

    tcsh: update 6.22.02 bottle.

--- Formula/tcsh.rb
+++ Formula/tcsh.rb
@@ -12,6 +12,7 @@ class Tcsh < Formula
 
   bottle do
     rebuild 1
+    sha256 "1b2047b5838ea9b50b6cffcd0c3132c1e69c419291ce2f3a7a18323f9d80a4f8" => :big_sur
     sha256 "d885eaa1411e8fc46cb39e4a11254d37c8dc90aded6684631bfc312d7115c9fa" => :catalina
     sha256 "a070c8c6b4f2ce38be5a84109d307078545911f2a731b0e5e140856a6711bce4" => :mojave
     sha256 "2f81edc8ce902ce12e722003aec60d62d21ca7be7a944b3b2f571b9e9d7d1282" => :high_sierra
