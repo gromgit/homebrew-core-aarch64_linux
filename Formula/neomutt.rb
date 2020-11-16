9ef324f0489a0aa220ec4671aa1e355f9c9fdd7e
FX Coudert <fxcoudert@gmail.com>
2020-11-16 19:33:13 +0000

    neomutt: update 20200925 bottle.

--- Formula/neomutt.rb
+++ Formula/neomutt.rb
@@ -7,6 +7,7 @@ class Neomutt < Formula
   head "https://github.com/neomutt/neomutt.git"
 
   bottle do
+    sha256 "5c4977430b85cd9123c671bd8f70d34579730187f927a6e13112ec8727e04b49" => :big_sur
     sha256 "bb9829de2249a318f926a8f435ece8731f1534c58fb0256ca34409bbb22c42fe" => :catalina
     sha256 "79dff03b1a3485f63c0d2affd5327d70246e72707a0c66830ff871a591bb8300" => :mojave
     sha256 "a5fb829bdcf7cdc5925339fe37bdcdfbd1f4fdbb9d000b73592fe75c5d00ee53" => :high_sierra
