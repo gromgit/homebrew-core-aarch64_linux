c2a201a4d3c57d0ddd560bfdd79cece48953e36f
Jonathan Chang <me@jonathanchang.org>
2020-11-16 05:25:17 +0000

    pdf2json: update 0.71 bottle.

--- Formula/pdf2json.rb
+++ Formula/pdf2json.rb
@@ -7,6 +7,7 @@ class Pdf2json < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "20fe898333fa761b942ee5b0f2d41e47660389a250f5c8604ff1ed22788d9581" => :big_sur
     sha256 "035c69de85f1cad569ff743faef796a88b9f9a706be802bf111a83505858b366" => :catalina
     sha256 "abf950838b700f50ff4279501533176cb5a1929fb0b88c8ccf94b07ac362c66d" => :mojave
     sha256 "4bee4b8c61362c64d72a3f011f8c5ef223c5e80d269e442a18472adc42e108e9" => :high_sierra
