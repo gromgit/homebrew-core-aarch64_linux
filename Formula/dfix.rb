aec28d27f0a7246dc82ae160137286e594aa06cc
Jonathan Chang <me@jonathanchang.org>
2020-11-16 06:23:51 +0000

    dfix: update 0.3.5 bottle.

--- Formula/dfix.rb
+++ Formula/dfix.rb
@@ -15,6 +15,7 @@ class Dfix < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "bb047ac32131d3ec25b60ca86e77959f7b9625ad469d9e0a1ada3fefe8525d9d" => :big_sur
     sha256 "ff3b76977bcbfb5b7a04bbebb53a794cc522b64987f724fe5f8a8236812eb1f8" => :catalina
     sha256 "24d234e206efa754f8bd900102720280d8efb1af6ec93059a467589acddca3ee" => :mojave
     sha256 "13a2621737c198bd0540f507293a9b015a0ebe36cd3373589a69ec834a863d8d" => :high_sierra
