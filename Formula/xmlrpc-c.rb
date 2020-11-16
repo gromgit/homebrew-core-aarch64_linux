f7893652294406bdfc42c66cdb4e2281869ceff5
FX Coudert <fxcoudert@gmail.com>
2020-11-16 12:30:16 +0000

    xmlrpc-c: update 1.51.06 bottle.

--- Formula/xmlrpc-c.rb
+++ Formula/xmlrpc-c.rb
@@ -10,6 +10,7 @@ class XmlrpcC < Formula
 
   bottle do
     cellar :any
+    sha256 "4bf06fc636da854ff791a9d0af52db23e08b42fa9b8fe062cf22e10c57605822" => :big_sur
     sha256 "f7d6a12c5e57d9ceb97b88dba518a6bae79e41e95343395a3f3c94bc168c92ab" => :catalina
     sha256 "638d1d6484717f57228174db4b9e875798b3ee1558ac6bcab59c96d80fcdab56" => :mojave
     sha256 "88a9ab0c2baea0aac627427628b00bce6828e5fc436533c0de8cfb0a99c21a24" => :high_sierra
