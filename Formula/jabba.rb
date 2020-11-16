316241280d384051fdd7ddf2fe6f90988e239dac
Jonathan Chang <me@jonathanchang.org>
2020-11-16 09:51:58 +0000

    jabba: update 0.11.2 bottle.

--- Formula/jabba.rb
+++ Formula/jabba.rb
@@ -9,6 +9,7 @@ class Jabba < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "a730868e347cb89b50393f96062b2c076f1d501b137ead1a795aa39e54ad4611" => :big_sur
     sha256 "7eddb409c7bb2784db21756e624a18b19977bb4df53ab547eaedd8abe876651e" => :catalina
     sha256 "3101ea25ce49c3ed96b3c8595a5441fec3aeb536b56eca21c1dea56f6c1fd86b" => :mojave
     sha256 "8454f5aa9b8832908b1c889531118ea058b2e675ef7f7f37eeb282f454aeec1e" => :high_sierra
