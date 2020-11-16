cc16b44d40a737dad98887094885865ff9d8302a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-16 10:14:48 +0000

    lastpass-cli: update 1.3.3_1 bottle.

--- Formula/lastpass-cli.rb
+++ Formula/lastpass-cli.rb
@@ -9,11 +9,11 @@ class LastpassCli < Formula
 
   bottle do
     cellar :any
-    rebuild 3
-    sha256 "b612b3711fb8ee5808f78c7f5dc06de7085287ee0159a0c6c2e8eb25b4221db9" => :big_sur
-    sha256 "4c908256d426b3f1aecd95b5c9ae5acaa2c8ff327891c1254d30b149c5997752" => :catalina
-    sha256 "8bb9debd4a15ed5c49486469a63db77128890d272a4665418af562bd4fdb6a23" => :mojave
-    sha256 "ce6281beed7e1a0d8733240001f60f58b5d28737e6de4ad8c7f8264594f78c7f" => :high_sierra
+    rebuild 4
+    sha256 "10f9224c8bfebae0cf12df72e6144ba3a309956b1efcce574975cd21cec930c5" => :big_sur
+    sha256 "8643f81d13a40ef8b86efe83fbee1b41b22c492b7725bebab83dcb2d253fd603" => :catalina
+    sha256 "f1b7c42dd889f597ef06f0bd72bb1b273c21dc91e5f3e313da8599254954a7ae" => :mojave
+    sha256 "62629472aafb7e4927d8ab5e9d7189c913e3249a172d0445ffe7eda31b642eb7" => :high_sierra
   end
 
   depends_on "asciidoc" => :build
