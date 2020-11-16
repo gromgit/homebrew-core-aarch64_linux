7504038b1cd5318f7835d6390d7d141fcb7c2e10
Jonathan Chang <me@jonathanchang.org>
2020-11-16 00:50:10 +0000

    codequery: update 0.23.0 bottle.

--- Formula/codequery.rb
+++ Formula/codequery.rb
@@ -7,6 +7,7 @@ class Codequery < Formula
 
   bottle do
     cellar :any
+    sha256 "7f8c243f0fc8d33c04ed5ec3579a85d1c39802c4cafd05df0b83a34a1d6edc47" => :big_sur
     sha256 "b3f585e17783444968f0739397eb41fb14f897584118689bf1dbb862344e4221" => :catalina
     sha256 "94fd84eb8c088b69eeddd1e09ad6bf7ed97ddd052e2ab4f6381776a6b4d3622d" => :mojave
     sha256 "da150e36ca0d9cc56f7b243aa26677e4bed67335b1be5d01f10eeaf83df6884b" => :high_sierra
