1df0d3295a59b5aff2d1941fbdca56789c1956ac
FX Coudert <fxcoudert@gmail.com>
2020-11-16 17:04:03 +0000

    scalapack: update 2.1.0_2 bottle.

--- Formula/scalapack.rb
+++ Formula/scalapack.rb
@@ -13,6 +13,7 @@ class Scalapack < Formula
 
   bottle do
     cellar :any
+    sha256 "da83f379e10c2bcfe543767c7e6a62ccd825e5f9d3cfb7ed066e9dd9c62c8154" => :big_sur
     sha256 "281e3d5317f1616e8d5a6a3b9c37fbe6ee29a03b2abe14055854902a6c009a87" => :catalina
     sha256 "b222f27ffed17605ffca2d1b0b4804f4c66ec916c9d2b5f2dd085ad2427fa791" => :mojave
     sha256 "ea92d3247883a9e0de28483a34d1ca064d395d28c8a622fbac571f4cd6d0e64d" => :high_sierra
