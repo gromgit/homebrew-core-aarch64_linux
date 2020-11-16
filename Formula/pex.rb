ca341e97b7362278f619b7df7c7e37dc8926ec16
Jonathan Chang <me@jonathanchang.org>
2020-11-16 06:30:33 +0000

    pex: update 1.20140409_3 bottle.

--- Formula/pex.rb
+++ Formula/pex.rb
@@ -8,6 +8,7 @@ class Pex < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "02aca9cefc6e949f6a4f2854c70f31cc63fa1c88fb01e2f2e3108a3b608e0552" => :big_sur
     sha256 "d266cf66e50d44748ed83ba2a20ffa0bd0530f637d98a85e3ab1b6eb11794319" => :catalina
     sha256 "427b8a701474aa879f8728ec463d3f20aad7c67f7b0ce330245015ec2830806a" => :mojave
     sha256 "ed1429f15df1e663735f27b1c7660e289953494b84a84bdd919a7eb077576a72" => :high_sierra
