9fc297c2e9e0a2cbb8ae1f92805d5176096f4d97
Jonathan Chang <me@jonathanchang.org>
2020-11-16 15:24:46 +0000

    terrahub: update 0.4.39 bottle.

--- Formula/terrahub.rb
+++ Formula/terrahub.rb
@@ -13,6 +13,7 @@ class Terrahub < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3f54c95e01bf455fdcfa9d611b5ffb2107883713d57bd417a9acbdbd9cdfd84b" => :big_sur
     sha256 "8d3ec469e865ec84a5df634b424bd6553d51593f28fb1bbca8edf34298917429" => :catalina
     sha256 "7f8910ea70fb61f12c60228d8a36034383fd1365bdec96e32bdba7dd03675857" => :mojave
     sha256 "3995a9a171cecd49b7a954cf1d6ea1977ca52511b54855be34a81735b19359d7" => :high_sierra
