da6b72c52e7ca00cbae4b2e7aefc364dde471c3d
FX Coudert <fxcoudert@gmail.com>
2020-11-16 17:20:05 +0000

    abyss: update 2.2.5 bottle.

--- Formula/abyss.rb
+++ Formula/abyss.rb
@@ -12,6 +12,7 @@ class Abyss < Formula
 
   bottle do
     cellar :any
+    sha256 "9fe0e2f711647eda6cfc6f4dc8ff0259f6fa96534fa1bfa9f895cfc2b62830b6" => :big_sur
     sha256 "521a584ab5f11e69de3b4b2362bdcf89cf3b541b32694c30eec6e71d334c8232" => :catalina
     sha256 "8c473ad4f6d9c3b786069c1d933d1ee8e72fb117f1ddbef65b0696163cf34292" => :mojave
     sha256 "7fbea49ff3c1cdf2867ceac467be40d16a37cf104ef7fcd478faf0cfdd726eea" => :high_sierra
