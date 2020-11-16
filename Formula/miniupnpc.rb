410498871af1966f4acf710b17e93b9ceb560339
Jonathan Chang <me@jonathanchang.org>
2020-11-16 06:59:57 +0000

    miniupnpc: update 2.1 bottle.

--- Formula/miniupnpc.rb
+++ Formula/miniupnpc.rb
@@ -14,6 +14,7 @@ class Miniupnpc < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "9fab3b162f13e509a2f5dd1858a91374961898808f28aeb4153f6294b9f1a2b4" => :big_sur
     sha256 "fd72b75df14dc0a23f566031fbddef7110acf0e90b34092ef09ef62fa74a6117" => :catalina
     sha256 "78f72e56f2edb01fb2d7949836050cca173fe9c342b602c7b9c3a8dc31693849" => :mojave
     sha256 "ac7dcda27fedebab8e8c47ce08c74283626a0169b646a09b34de5bd91b673e1a" => :high_sierra
