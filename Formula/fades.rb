626a6a8e05038b0231120c8737ae9ea87f60b348
Jonathan Chang <me@jonathanchang.org>
2020-11-16 05:13:07 +0000

    fades: update 9.0_1 bottle.

--- Formula/fades.rb
+++ Formula/fades.rb
@@ -13,6 +13,7 @@ class Fades < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "21973bd9531e3c6af4359e51ac9e31982bfbdf024843b02226168910a39aabd2" => :big_sur
     sha256 "78532c867a5ab35381edeb565f094fff1d2d269c169f903e10bde65a2ee2b3c7" => :catalina
     sha256 "bc2264df647adc84ef4f5321258ee9030da3269a66d4f50ed5faf0cc3185bd83" => :mojave
     sha256 "bdf1c47688725b9335adaca1dae977fba9aa534d44f4c65b4a1a684d6fc7930e" => :high_sierra
