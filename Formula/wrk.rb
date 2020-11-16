7e6fd34871829cd887add248d01c14fd73b93767
FX Coudert <fxcoudert@gmail.com>
2020-11-16 16:42:27 +0000

    wrk: update 4.1.0 bottle.

--- Formula/wrk.rb
+++ Formula/wrk.rb
@@ -8,6 +8,7 @@ class Wrk < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "39f3b454a93381e9350a3650745039deec765bff1f72eb7b6f3d449f1cabed3a" => :big_sur
     sha256 "65f16f71cfb120d12f0f993044db92b767c728274aa046917bee7ad4deacfd3e" => :mojave
     sha256 "ccc1cc4303847b70f940b765a9229476e7018328fbf202c02a00d9a13c63919e" => :high_sierra
     sha256 "82fd1323ac999e23107dcd1cdae9635ff047afd15532f42f8cb4e82dad0db257" => :sierra
