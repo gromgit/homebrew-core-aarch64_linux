2a877896427888a6184653a59304a53bc7148432
Jonathan Chang <me@jonathanchang.org>
2020-11-16 14:22:32 +0000

    webify: update 1.5.0 bottle.

--- Formula/webify.rb
+++ Formula/webify.rb
@@ -8,6 +8,7 @@ class Webify < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "284df018b49ddc0c2a3b8e0800c1997abebee41d198edbd7d725be2f88a8c5e4" => :big_sur
     sha256 "7b6543358b1c92e8e8cc71584ed52802a039c9327edc839dcc75216fbd23558c" => :catalina
     sha256 "8a58b27bcb9d6f9cd611b8f7dfb6192f617854cfcaf8638b388f6dd88ec40f70" => :mojave
     sha256 "9701f9952fb05880c48c5ca26d14807cf324c2210d4b45d0fb5408243d8d76cc" => :high_sierra
