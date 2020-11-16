9661a8e3cb70cc7f191f7cb9d60ec4bad774a68a
Jonathan Chang <me@jonathanchang.org>
2020-11-16 23:27:32 +0000

    ultralist: update 1.7.0 bottle.

--- Formula/ultralist.rb
+++ Formula/ultralist.rb
@@ -7,6 +7,7 @@ class Ultralist < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b1c9c3e4f51dcec7c482a44e9111fdb3bf42081195d73df63c858c8c60a66eb0" => :big_sur
     sha256 "529daa8fdf264f4f13f8f93d785095d4a803f94902772e25094415691bf7f83c" => :catalina
     sha256 "5bf8a9d39b953f0c24c8a1b978fab945f667a5f4e48c0d2729162f948f3b9118" => :mojave
     sha256 "eff4c2ac2bd4d1a4bfe6f0d2bcd92b4c572d17eaa047df909533d8f510f366a1" => :high_sierra
