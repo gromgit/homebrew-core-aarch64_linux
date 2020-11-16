928dea06df512136a8bc6c8e93cace5038c5e3da
Jonathan Chang <me@jonathanchang.org>
2020-11-16 15:12:13 +0000

    vcftools: update 0.1.16 bottle.

--- Formula/vcftools.rb
+++ Formula/vcftools.rb
@@ -7,6 +7,7 @@ class Vcftools < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "fd610cf54b51ec2ec429edfab36b2cf1b3eb14787ab0ef5ff9d805def8c48cb0" => :big_sur
     sha256 "96424c5e9810127b9f450a88fd314eb94662b35ac88aee4c7efbc8f5420dd989" => :catalina
     sha256 "5d52f2eafbf96fcffd2b8f9804c2d0ca9752af4242c27ed5fe15a6f8cb935498" => :mojave
     sha256 "2fc4ca7c7c23841a1eed8539910737b5986079be6d22d1ff8375f052266bf478" => :high_sierra
