98b2703743d1ec0f3e618a11967a6d142bcf75b4
Jonathan Chang <me@jonathanchang.org>
2020-11-16 14:50:55 +0000

    nnn: update 3.4 bottle.

--- Formula/nnn.rb
+++ Formula/nnn.rb
@@ -9,6 +9,7 @@ class Nnn < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "bbf47e736eb8f34bee3fb1a69b2c80fa78bef2d3fb80105990471ef26902ff73" => :big_sur
     sha256 "44109c19d7e9c4108538ed3fc5a75c3ae176cdecaf64d19d194ccf4802aa6354" => :catalina
     sha256 "a8dd428c9cbdea23be4b13493edfe09583814adbdee2d49627812268481af7df" => :mojave
     sha256 "e0d170451efa4edb22b8899577ee1bd0fb2f5cac90218fcc021f20514ef52ece" => :high_sierra
