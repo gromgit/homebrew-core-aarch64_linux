cf1f70c88ef8e69ac5abad845776e6b18dbf7460
Jonathan Chang <me@jonathanchang.org>
2020-11-16 10:32:24 +0000

    mytop: update 1.9.1_8 bottle.

--- Formula/mytop.rb
+++ Formula/mytop.rb
@@ -12,6 +12,7 @@ class Mytop < Formula
 
   bottle do
     cellar :any
+    sha256 "ea2f5229c929cb23466f75964d1bf294130381b27efd55cf2ce91cb248c43732" => :big_sur
     sha256 "69930f7d5c68b0d6ce75c89820732f269d3b3c6651358875b0db58ae1ead38f0" => :catalina
     sha256 "ac13ecf239ff9d4bb1d39ad584c46ac9a5c95f3b96b3991bf9108280b30c0a19" => :mojave
     sha256 "2862de7630947648898e1ef348a8357fdd25622310c9af03450c40ea33fc925c" => :high_sierra
