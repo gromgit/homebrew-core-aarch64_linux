408e582bce436e5af2c27c47647d7a7db9e3128a
FX Coudert <fxcoudert@gmail.com>
2020-11-16 11:24:47 +0000

    udunits: update 2.2.27.6 bottle.

--- Formula/udunits.rb
+++ Formula/udunits.rb
@@ -5,6 +5,7 @@ class Udunits < Formula
   sha256 "74fd7fb3764ce2821870fa93e66671b7069a0c971513bf1904c6b053a4a55ed1"
 
   bottle do
+    sha256 "06ce12b4caedfc807687c9ab8d2fdacf6a4dd7a26299a2bc6039a690d380e27f" => :big_sur
     sha256 "77dcfaf55565b5d34f1ca0da75bc7aed7cdbb9b66ba684147ada21421f35005b" => :catalina
     sha256 "176548e1d698baf5187088bf16b273af3e3e585f5f765963c396187491ea5fe1" => :mojave
     sha256 "3c12f59317ded4bdc6f89c24a0eec9260a499371c9c92b2d5e34c1b1a9f50a2c" => :high_sierra
