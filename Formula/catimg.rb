1ad0b57032201b6148249406990e76b7eefcdfaa
Jonathan Chang <me@jonathanchang.org>
2020-11-16 11:30:34 +0000

    catimg: update 2.7.0 bottle.

--- Formula/catimg.rb
+++ Formula/catimg.rb
@@ -8,6 +8,7 @@ class Catimg < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4ed745935b27937d85de5e28e9f3345be90bfc725349247cb3b9770a720fe134" => :big_sur
     sha256 "076781a169c35bba3b5bac8b4e5ea89497b9e21993da49739b4d3b690c4fad2b" => :catalina
     sha256 "f680ca7c613325854b5d93185ec4db42a94341d8c4556b9e76adefe90d63eaf9" => :mojave
     sha256 "83a6bf89d47c2347c30872201ea5a77c8af18ada90b1992b28838d10882c0c6b" => :high_sierra
