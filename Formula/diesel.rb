64eb2f57ece864613eceb5124c0738496f4dfb90
Jonathan Chang <me@jonathanchang.org>
2020-11-16 05:38:58 +0000

    diesel: update 1.4.5 bottle.

--- Formula/diesel.rb
+++ Formula/diesel.rb
@@ -9,6 +9,7 @@ class Diesel < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "82f26a0de3595ad68a20edf5ddc56328b38306d245e1dc750915ff9a6df92f01" => :big_sur
     sha256 "a63b7299a7b32a4c6f581ce4aac9b264bb4b1fbd555268126f2db791606846f2" => :catalina
     sha256 "ecff18f1ca293e846f0a1742311c89a146315a051531a3fff6ff27ddb14f9e95" => :mojave
     sha256 "f63d5ac49ce152f424ce1d0072177857a0c9724156bdb5faad34f74af58e24e6" => :high_sierra
