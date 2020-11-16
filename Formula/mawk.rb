e0fd809578a0227546700a7744983174e3dc4616
Jonathan Chang <me@jonathanchang.org>
2020-11-16 03:38:51 +0000

    mawk: update 1.3.4-20200120 bottle.

--- Formula/mawk.rb
+++ Formula/mawk.rb
@@ -12,6 +12,7 @@ class Mawk < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a669698248dacc35f2d82547a846e9ba3fd47dc56c8176c407f73cb24156c775" => :big_sur
     sha256 "03f9aa87a079b35b6f93813e4016e85d102c578d8b65f2f967b0b7c5c5d869ad" => :catalina
     sha256 "802b3592430ca644c6590acad265f45ac892fe47fb37732e678afac13f8cf1f0" => :mojave
     sha256 "d113f78e1c20c8bf86fcf5ce083e206aeca58ee857e7d0a3acb0158d2b01fb45" => :high_sierra
