84bf5f1928e2cb82f51676a3d25dbeaef22c2cf2
Jonathan Chang <me@jonathanchang.org>
2020-11-16 10:15:56 +0000

    x11vnc: update 0.9.16 bottle.

--- Formula/x11vnc.rb
+++ Formula/x11vnc.rb
@@ -7,6 +7,7 @@ class X11vnc < Formula
 
   bottle do
     cellar :any
+    sha256 "50270ae1fd7681db301b3449748f46108d5eb93df535be7085ef6498f936555d" => :big_sur
     sha256 "66ddb190e2e2a183ba662d4c7ac2de508b6ebe3c3c827078a5eec5b550477e5e" => :catalina
     sha256 "cd3d5d0047a8fb2e7b66ac94baf08c2da16aa8e135b8180acacce2d1bf366e58" => :mojave
     sha256 "2660aa48f9545eef71c5a42f9985720629d0391eaef37155264ec4c71cf13b29" => :high_sierra
