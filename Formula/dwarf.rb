c0951286a7a89a2feefc8744e3f34408a31b2472
Jonathan Chang <me@jonathanchang.org>
2020-11-16 03:47:40 +0000

    dwarf: update 0.4.0_1 bottle.

--- Formula/dwarf.rb
+++ Formula/dwarf.rb
@@ -8,6 +8,7 @@ class Dwarf < Formula
 
   bottle do
     cellar :any
+    sha256 "b0bec07c54898e3506e249b3c18fde361772367f1e2d8cc2ee6726a678aea7cd" => :big_sur
     sha256 "1d0e1d803ab771d58a56cd52939d01b7c9e1a8474cb80c21b2251320340e5dc2" => :catalina
     sha256 "c10f3ccbb2dc59b7c76c9dd46a71f1e41d7c7faa8fab5f4326599b3a5467c770" => :mojave
     sha256 "92db022169f222a0ce002e6c20e6256cc5636f61c1e6fa1c44b56481c5a2422d" => :high_sierra
