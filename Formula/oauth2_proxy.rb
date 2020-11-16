4b9d1a1707b4d20655e4097cb48a64e3d58d38bd
Jonathan Chang <me@jonathanchang.org>
2020-11-16 07:12:37 +0000

    oauth2_proxy: update 6.1.1 bottle.

--- Formula/oauth2_proxy.rb
+++ Formula/oauth2_proxy.rb
@@ -8,6 +8,7 @@ class Oauth2Proxy < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7630f6dc4b22801733cfaef601c7c43fc005eae62f16f619126bed9e3566acbe" => :big_sur
     sha256 "f109dd498dd23e387c4338baa42df3e00a3d83829571f9d7c5b3e55857e0d818" => :catalina
     sha256 "17ad8e33c417229882d67128f50ea94c94f834e8a56c62884351fa06eb1c7c2e" => :mojave
     sha256 "b37692fbfcd018de90a1580e99547919adb4d4c25169c02f67ce2d84a3f1bea6" => :high_sierra
