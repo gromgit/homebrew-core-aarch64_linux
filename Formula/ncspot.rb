d12075f9c91405c70d0da78ac20f2afaa95f01dc
FX Coudert <fxcoudert@gmail.com>
2020-11-16 08:55:59 +0000

    ncspot: update 0.2.4 bottle.

--- Formula/ncspot.rb
+++ Formula/ncspot.rb
@@ -7,6 +7,7 @@ class Ncspot < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0157f987a23017c39c7275f99ebb30ef16e701152171ace1535a9b2e100ff943" => :big_sur
     sha256 "168060ab5814e9c3f8fb5234112fee4466d7bcfc58c49254d60765c790128bbf" => :catalina
     sha256 "ab6ccbc785b6a573400378e8f7480296c90fc177aa5f86d45945fded290a00fa" => :mojave
     sha256 "334e1437787e641ce4f80dbc06fbb4368bb87a34e72aa7d4740acf4d8c016a62" => :high_sierra
