4a7ad8452c5a578ca7df9abb0467e67b9fecb70b
Jonathan Chang <me@jonathanchang.org>
2020-11-16 07:59:19 +0000

    shc: update 4.0.3 bottle.

--- Formula/shc.rb
+++ Formula/shc.rb
@@ -8,6 +8,7 @@ class Shc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3866195be89821e424dca28e390d36060ad52be9030677498a300e39b7ece548" => :big_sur
     sha256 "cdfc62c7d9bd39ed7e956066f8d55a189c58b185b6abf7e45b5d8c63a6abe2d5" => :catalina
     sha256 "ff3c55ef1d10c16066e97a20143dbd1e7781ceb9a2c5c8b46d140f6711bc79fa" => :mojave
     sha256 "c19f4586119be579006eace517045998138d83a17e2b5c8ec00ad73ea007b68c" => :high_sierra
