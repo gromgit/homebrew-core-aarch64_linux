18225a13b89aa9d07320e9806457979d2260d6d5
FX Coudert <fxcoudert@gmail.com>
2020-11-16 16:24:58 +0000

    ccls: update 0.20201025 bottle.

--- Formula/ccls.rb
+++ Formula/ccls.rb
@@ -7,6 +7,7 @@ class Ccls < Formula
   head "https://github.com/MaskRay/ccls.git"
 
   bottle do
+    sha256 "aef6e8248dda65a8c4ec322e46c2c96ccdfa3d3086e2f96353f0a5fdca6cc739" => :big_sur
     sha256 "9eeac4bcb801299d73e1d8a838b9b75701cef86506928082c5b63603949fad4e" => :catalina
     sha256 "eb2e73b77b5c3221fe9bc2b3330b9a2456b95558fc3b326fc29d35739467847d" => :mojave
     sha256 "578e24f878766fdcc3e024ddfcd6d124fbeaa79c79e32b89f6286f58d1213894" => :high_sierra
