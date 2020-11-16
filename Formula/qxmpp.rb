98c46aa716a2ecd5c6a052b5689fd16a206d6546
Jonathan Chang <me@jonathanchang.org>
2020-11-16 01:39:13 +0000

    qxmpp: update 1.3.1 bottle.

--- Formula/qxmpp.rb
+++ Formula/qxmpp.rb
@@ -7,6 +7,7 @@ class Qxmpp < Formula
 
   bottle do
     cellar :any
+    sha256 "98e9f506c62ab3b25d0de4fff745309eb36f99230910e83f8258a4853b8c99f9" => :big_sur
     sha256 "ae5ada2da192e552193487318f63c28bc5a1ce71705ac97ff2f2cdaebf1ace20" => :catalina
     sha256 "ace2c4096387d98f5a27d28d73b0ed2453a61a5a9b4bd7ff1c55b105f0373b38" => :mojave
     sha256 "a8342df624addc888a2d409e01a087e7974ff78b2091df7f6dc94949bb55abee" => :high_sierra
