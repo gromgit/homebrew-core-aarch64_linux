4531c05456f66cabe411a0f0d3f5b183eaaa4eef
Jonathan Chang <me@jonathanchang.org>
2020-11-16 07:51:55 +0000

    falcon: update 0.9.6.8_1 bottle.

--- Formula/falcon.rb
+++ Formula/falcon.rb
@@ -8,6 +8,7 @@ class Falcon < Formula
 
   bottle do
     cellar :any
+    sha256 "fab1a5546fe1e1abff7525ef791126c341fc305ef1bee37ad3b1c2788342c451" => :big_sur
     sha256 "0fdfed49f1ba12e66db6a7d9f315677280600f6db52afd52c91e6e66235c3053" => :catalina
     sha256 "d87b0664e797106f23bd5167c32988a7154d177955c2f612803999ebc4306fd9" => :mojave
     sha256 "670ae92a7f950558ea95001b45a848ef6d3f98d5fa414ba3549032d07badca47" => :high_sierra
