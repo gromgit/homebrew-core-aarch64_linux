17af9a3748999cc7bd33a6c2b3016dfc3518fd15
Jonathan Chang <me@jonathanchang.org>
2020-11-16 19:05:40 +0000

    dirt: update 1.1_1 bottle.

--- Formula/dirt.rb
+++ Formula/dirt.rb
@@ -9,6 +9,7 @@ class Dirt < Formula
 
   bottle do
     cellar :any
+    sha256 "ac69059e87e5682411a7442619985d95d79ee67af88366bcea28cc89e35ef46a" => :big_sur
     sha256 "2e9cf5a28852453f9ec5394bb2218fe3366e2cc6ef133e2dda847fbfa71ee968" => :catalina
     sha256 "f90972cf61d77071fec9ab429f8a88a03738699b7e223b30c8655d5c64fede74" => :mojave
     sha256 "b889891f8186b244161241e9c81d20afad20c31bd592fbf6860658334f314d39" => :high_sierra
