9be9ed02acc29e037de3194c3ed4c4b1a7949db7
Jonathan Chang <me@jonathanchang.org>
2020-11-16 05:47:57 +0000

    spiped: update 1.6.1 bottle.

--- Formula/spiped.rb
+++ Formula/spiped.rb
@@ -6,6 +6,7 @@ class Spiped < Formula
 
   bottle do
     cellar :any
+    sha256 "c245e8cf440207b8a2d229b8f8644ebe25da6999b52f2b8b16835c55b7f04b6e" => :big_sur
     sha256 "efe2a93770708c9a8c1474651b7b0b221d263b7fbb7dc75e014ff21caf084510" => :catalina
     sha256 "44c1509c5faf96f0be69fd905525e2070cf25445afddfaf45584bd9c4a1d702c" => :mojave
     sha256 "b5615b6afbc743c7b8b2776c3537ec42a4f1519f1f2f3e12bd06ae4e96ce5f14" => :high_sierra
