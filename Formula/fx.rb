1d736dad5c9ff6d7ed181206b83b32c3ae2dcf9e
Jonathan Chang <me@jonathanchang.org>
2020-11-16 03:35:35 +0000

    fx: update 20.0.2 bottle.

--- Formula/fx.rb
+++ Formula/fx.rb
@@ -13,6 +13,7 @@ class Fx < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6eb8a939cd58bb967b81d2b19a9754269016fb6c517ebfe5a45cecff33f9f72f" => :big_sur
     sha256 "e3af18a1b48a38407825a2d3ffe3bf60ecd9ac37b7762d3cdcb0ebbad29d7c9c" => :catalina
     sha256 "df7a7fd00f429e4db18d0efe676f9ed081efc4175dccb0f59c20e97b87e1bb0a" => :mojave
     sha256 "be1769bcf8dea3bd6db0237948e500334a6813fbfbab34c90eedbbe9518df838" => :high_sierra
