caecffec80ccd03326ada3479d53c5896e89d49a
Jonathan Chang <me@jonathanchang.org>
2020-11-16 14:34:42 +0000

    bgrep: update 0.2 bottle.

--- Formula/bgrep.rb
+++ Formula/bgrep.rb
@@ -8,6 +8,7 @@ class Bgrep < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "cbd5d550e042d764f0cc4c39e58cd40ae87430fb773aae7d77f3ca56d05c3325" => :big_sur
     sha256 "444a8dd0c2190e3a75574f8bee287895aee1d070d3e72e72fd7cda4c9cb77211" => :catalina
     sha256 "8a3633a884feda24b875005550fddbd613987c89edc9418dd23783b4c2f7e8af" => :mojave
     sha256 "c2357ea00756425fec65d8367e7b8653a4d6845b6aa044106b8952d8b8ead0ca" => :high_sierra
