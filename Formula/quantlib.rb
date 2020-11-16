699cf371fd3a0529631b8b661946ef3acc94e958
Jonathan Chang <me@jonathanchang.org>
2020-11-16 23:21:18 +0000

    quantlib: update 1.20 bottle.

--- Formula/quantlib.rb
+++ Formula/quantlib.rb
@@ -7,6 +7,7 @@ class Quantlib < Formula
 
   bottle do
     cellar :any
+    sha256 "bb5ff2517d357ea2e826a631c38c1ba1b2b8da2ffe55c29b3769977d6d657405" => :big_sur
     sha256 "61d144264132e25c31f347dbdf3c44595be790e851d671159ffaa3d6027b0f04" => :catalina
     sha256 "0fb3b38c1c385bdfd8bbe930c3eb64b8b9d3cf4451715325e0f3717c9ade0b69" => :mojave
     sha256 "d6b3bda389b8d94685f47611d4abfa578d9b2166a2e9603ff07a6961a99f70c4" => :high_sierra
