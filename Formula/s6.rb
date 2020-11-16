8758e724b7121fd4afa214caac83122a8769c374
Jonathan Chang <me@jonathanchang.org>
2020-11-16 04:33:04 +0000

    s6: update 2.9.2.0 bottle.

--- Formula/s6.rb
+++ Formula/s6.rb
@@ -6,6 +6,7 @@ class S6 < Formula
   license "ISC"
 
   bottle do
+    sha256 "47924b409c4d52519acf0b7d7757f689e78ce1cb272c297822597a59a37c4275" => :big_sur
     sha256 "94906378d458e78a0909dc89bd5672ca8d33645b1b070be4974ac7ebe0f3aa15" => :catalina
     sha256 "e4024234358147987f7f6f599b73ddfd9bbe5e5fd414efcbc53577045a1dbb78" => :mojave
     sha256 "d6be4dd0697c711c1e35fe25e4f1365bcb1c198ab8fc8c3f3d7169b29ccb4372" => :high_sierra
