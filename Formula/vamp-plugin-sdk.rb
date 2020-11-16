e8cce2dec7cda4d393d4b54e4300e12d3fd821aa
FX Coudert <fxcoudert@gmail.com>
2020-11-16 20:28:58 +0000

    vamp-plugin-sdk: update 2.10.0 bottle.

--- Formula/vamp-plugin-sdk.rb
+++ Formula/vamp-plugin-sdk.rb
@@ -7,6 +7,7 @@ class VampPluginSdk < Formula
 
   bottle do
     cellar :any
+    sha256 "21e590739905e6794c11e4f7037adfa6fa83da4d7c2ab2b083c43563449d8a45" => :big_sur
     sha256 "b31926ceedbd7f79dc9783da8092b543c549d800705d9d8e8d8d0fd451d093de" => :catalina
     sha256 "ee8d69d0b8c72e3e9ed1c79bfa7ca6650d10e36a2b110215b3d803f841ae2ec0" => :mojave
     sha256 "834812edc745c782511f1397fb5e3e6995b9fd25b42426ec784cd5610dbc9eb4" => :high_sierra
