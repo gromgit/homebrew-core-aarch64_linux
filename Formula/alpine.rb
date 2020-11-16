22d28b518c52a413f187e5c508ebc316ee251e3d
Jonathan Chang <me@jonathanchang.org>
2020-11-16 04:15:35 +0000

    alpine: update 2.24 bottle.

--- Formula/alpine.rb
+++ Formula/alpine.rb
@@ -12,6 +12,7 @@ class Alpine < Formula
   end
 
   bottle do
+    sha256 "bc7e92be45c91c784791a4be2cc2569bed0b686d132f4cdfd0d0233be091643d" => :big_sur
     sha256 "8a856082da848d13cc4019f3bed974e896144b0cf192125285e20a7250a72295" => :catalina
     sha256 "43533b14f530c72a3f89dbaebf2c4efcd66c8c7fc89349e56d714ff15f2af02e" => :mojave
     sha256 "bed10deca1df682e23ffec4b21af9f837db1dbf011879ab0df579efc81116db1" => :high_sierra
