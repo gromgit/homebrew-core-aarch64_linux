3ec9f5709aaff5174c6bd2e971177742fbf478f8
Jonathan Chang <me@jonathanchang.org>
2020-11-16 14:05:21 +0000

    numcpp: update 2.3.1 bottle.

--- Formula/numcpp.rb
+++ Formula/numcpp.rb
@@ -8,6 +8,7 @@ class Numcpp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "cac192f81e747be1de8738a455fe0434aa326730bd898b1ca3d7128506f199c5" => :big_sur
     sha256 "45d1e6f87c6c253c5cd1a5b75247c2433dcbb4fe4ac5ecb6c2ac1d0d53a9f231" => :catalina
     sha256 "830cc9312d29337ac066b1861ced8846a54aeddbc7d6401796ee6490fe4881b3" => :mojave
     sha256 "e68ae1eb4314455e0efa51c8a56a2afbf18f3a8c1987b8b587611fe36966fa65" => :high_sierra
