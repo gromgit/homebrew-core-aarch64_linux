02ccc213bc9d5e439e8c315940bd09f561fc4757
Jonathan Chang <me@jonathanchang.org>
2020-11-16 05:06:40 +0000

    marked: update 1.2.4 bottle.

--- Formula/marked.rb
+++ Formula/marked.rb
@@ -13,6 +13,7 @@ class Marked < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "696c6e074dfad53cd54359a26115b7deb149a508f0c56a32ca8b23572d669bb2" => :big_sur
     sha256 "2d7d7d92f82712f012ec042b88a247e45777d2c712f30a1aeab696656b493d85" => :catalina
     sha256 "dedebec9f51f7dbe3f4fbd104c3991b62c362a286c81d5605a95cdcbe20e3076" => :mojave
     sha256 "7cadf28836fe33c3bff11de0e751788a359d8a6dea801650715d991acbd1f9ef" => :high_sierra
