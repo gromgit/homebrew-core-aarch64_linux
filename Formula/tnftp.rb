d018efb5388c4678f3b873b187902bda29528d63
Jonathan Chang <me@jonathanchang.org>
2020-11-16 03:08:43 +0000

    tnftp: update 20200705 bottle.

--- Formula/tnftp.rb
+++ Formula/tnftp.rb
@@ -11,6 +11,7 @@ class Tnftp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f66f2de23252bca8e6ce5073adff9e86a0c928548b5a5c3cd2ea12f3a702d389" => :big_sur
     sha256 "1411f5fe465b0952891ff141431a5d09140c7d53bb3cf689054a2580bd1031fc" => :catalina
     sha256 "ae4beaa65c5f258152fefeeaa196c9e2d70cf3bda2af4e387ddcf807476c7401" => :mojave
     sha256 "900f2ece9b7a6a9edd0d96dc6c061ef6380c0fc99177119e73db65e5d8c012e0" => :high_sierra
