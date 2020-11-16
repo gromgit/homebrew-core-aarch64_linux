4648e2b07a951a3759f87b39965057b5734ccd6c
Jonathan Chang <me@jonathanchang.org>
2020-11-16 11:37:13 +0000

    vgrep: update 2.3.3 bottle.

--- Formula/vgrep.rb
+++ Formula/vgrep.rb
@@ -12,6 +12,7 @@ class Vgrep < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7733f70804557e02c99ec924a67ed89f2f4e05b8b838853863060aead4dec4aa" => :big_sur
     sha256 "5006d83eb22993f4cedcfce32b20b59e74526431bc44ea129b11f112f181c9a9" => :catalina
     sha256 "d9fe404e03ea5f5a7cd2709d0d064e4f41fae3f65d66263d242847af25040613" => :mojave
     sha256 "ab0e2c15aa3814e4e6f24c7d86a901fda915e6aa9ccbc7bbaf9d79df73ef91a4" => :high_sierra
