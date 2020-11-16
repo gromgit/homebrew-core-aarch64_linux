3a25b6da311fce0661f79397dad0d36c245e4a65
FX Coudert <fxcoudert@gmail.com>
2020-11-16 22:18:48 +0000

    libflowmanager: update 3.0.0_1 bottle.

--- Formula/libflowmanager.rb
+++ Formula/libflowmanager.rb
@@ -12,6 +12,7 @@ class Libflowmanager < Formula
 
   bottle do
     cellar :any
+    sha256 "b5b2cf44362d857c11161d326e6b0a1721e9f2e2123351f06b242a3e3c7c0b9d" => :big_sur
     sha256 "41c5f69289236b3362062b471654f0cc9446f93d90066c001a1bda56d9b9b4f3" => :catalina
     sha256 "64843e32762b175f497e00e332bee33dbe2c5e77384ccb64a0957e9a6c2bd40b" => :mojave
     sha256 "156b302b5feade664f79f10358bc7888f9db14da53bd3549f90864b1bd9056f5" => :high_sierra
