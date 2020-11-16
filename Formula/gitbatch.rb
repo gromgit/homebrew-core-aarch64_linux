d1045d57675621d0493ee7f6ae1960ce3f16cf96
Jonathan Chang <me@jonathanchang.org>
2020-11-16 10:12:43 +0000

    gitbatch: update 0.5.0 bottle.

--- Formula/gitbatch.rb
+++ Formula/gitbatch.rb
@@ -7,6 +7,7 @@ class Gitbatch < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0b4a7e403d40bb05852d24ed69bd68a4498b1ba455d618de879a0a759f6497a6" => :big_sur
     sha256 "58b204bd1779e99cade465f98457ec14cadaee9a3b65afe099245faba640da0c" => :catalina
     sha256 "31d8b72293ceacef7d44beb203887ff80628cb5cd3b56a9f0b467704d153b261" => :mojave
     sha256 "73fc219e77776b78635c672111736a1ce26f6f1afe0df4ce7c571341578cd1e9" => :high_sierra
