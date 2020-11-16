333972889263a9d2946314f4733ee2f1d6bd42ac
Jonathan Chang <me@jonathanchang.org>
2020-11-16 07:39:00 +0000

    streamripper: update 1.64.6_1 bottle.

--- Formula/streamripper.rb
+++ Formula/streamripper.rb
@@ -12,6 +12,7 @@ class Streamripper < Formula
 
   bottle do
     cellar :any
+    sha256 "95c5e14ced5421594802535f00d84b763a92c3cf20cf399a5bfdfa77b68b48ab" => :big_sur
     sha256 "bdc01265cc82de8fdd17a432458a22ea22420839daed5d29234efe5c9cf459a2" => :catalina
     sha256 "559e6ce06f450c306178c1e361154f134c3478ad1bc35ca70d0d3f000938043d" => :mojave
     sha256 "9df7827f89ef7f517ccfdb52be976b358ede1ceb2690f8617b4cc52da7c4cf41" => :high_sierra
