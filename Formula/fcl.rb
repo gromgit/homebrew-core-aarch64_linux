2420b6f74af0ae9b449d5b04bf84cd756a0fa09b
FX Coudert <fxcoudert@gmail.com>
2020-11-16 19:23:37 +0000

    fcl: update 0.6.1 bottle.

--- Formula/fcl.rb
+++ Formula/fcl.rb
@@ -6,6 +6,7 @@ class Fcl < Formula
   license "BSD-3-Clause"
 
   bottle do
+    sha256 "64e20a01b7a05f081b6b01ebe88722a0afec56514940130c0720e2ee5e5325df" => :big_sur
     sha256 "05a5dfa094009376e5915ad14289490fa370462153102eb43e402d50663a23f4" => :catalina
     sha256 "7fc28b6f1bd196e83873f61617e590af68ebf861cfc76af9d892c8ef40b25601" => :mojave
     sha256 "625f6117a551777a1f12eba3253886a441a5a00e2759218b9566d40bb9f3ab2c" => :high_sierra
