f076c47e03c86d0292d776ee64c84075d45f6780
Jonathan Chang <me@jonathanchang.org>
2020-11-16 02:36:32 +0000

    owamp: update 3.4-10 bottle.

--- Formula/owamp.rb
+++ Formula/owamp.rb
@@ -14,6 +14,7 @@ class Owamp < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "d9599177f43e538b1fea107a4395cbd466ee5991e8c1d7e8d510baf32878a32a" => :big_sur
     sha256 "a7bce114bb407f1663671ee68793b7751d512e0451cf9bbf35c1f36ad9b4c3f9" => :catalina
     sha256 "22833b09d6faa093c2d186560cd22e328b9ab11efa8f9774543392e7dca127f2" => :mojave
     sha256 "0ce1d8385c1cb2036acbccbcd92ed5778c8ec0aa8e4db5c06a9ea018621f58dc" => :high_sierra
