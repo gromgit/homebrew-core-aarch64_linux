c3cbd8143e6021a66dbd761d52c56b982183c885
Jonathan Chang <me@jonathanchang.org>
2020-11-16 01:26:19 +0000

    maxima: update 5.44.0_1 bottle.

--- Formula/maxima.rb
+++ Formula/maxima.rb
@@ -13,6 +13,7 @@ class Maxima < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "49bdc3f26a3979633b7c232380c93566af03401683a2870fb25e3ea40a18951f" => :big_sur
     sha256 "1b38264f8b5f867034d238be3fea3e8a3c30004f8b2fe5997e73dff3ccc5f7d6" => :catalina
     sha256 "5a9a0e9bcffbce0871cb0e0756ce110d03129f0a2784798890766eb6c9a82c67" => :mojave
     sha256 "96772653ca25c724239a4902a5a0f8844a8b71f2290e25824eec75d032148f98" => :high_sierra
