2c50a6963fc6db2dbe8b259615f8983319c1d07f
Jonathan Chang <me@jonathanchang.org>
2020-11-16 07:33:46 +0000

    libdvbpsi: update 1.3.3 bottle.

--- Formula/libdvbpsi.rb
+++ Formula/libdvbpsi.rb
@@ -7,6 +7,7 @@ class Libdvbpsi < Formula
 
   bottle do
     cellar :any
+    sha256 "255b960c43fac14b8a50af513ca3b2925cdfa0e71efa61d2eced2fd172fe8dff" => :big_sur
     sha256 "b6e6f300bbc36fabf785f74abb083c5cfc3f91fdd51ee7bd058cc579e709c78d" => :catalina
     sha256 "26298540d01f52628385c83cac4b6666543af4cc059fa7ad5b3a8bd458955628" => :mojave
     sha256 "c6d79686bf05346bc473cc148b68901d99ac447a85542ff68d089c71eda1bc87" => :high_sierra
