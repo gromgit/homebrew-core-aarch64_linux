03e8e94ec8e0284ed3f3c121de267efa886bd180
Alexander M. Turek <me@derrabus.de>
2020-11-16 17:35:15 +0100

    node 15.2.1
    Closes #64948.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/node.rb
+++ Formula/node.rb
@@ -1,8 +1,8 @@
 class Node < Formula
   desc "Platform built on V8 to build network applications"
   homepage "https://nodejs.org/"
-  url "https://nodejs.org/dist/v15.2.0/node-v15.2.0.tar.gz"
-  sha256 "a4c807cebda11a5f9ec50f340b0a916fa64423864c553c6f56da4404ece7a384"
+  url "https://nodejs.org/dist/v15.2.1/node-v15.2.1.tar.gz"
+  sha256 "a64aa96e01c097d5bc16c191a647154315e12c5421e38ae985c197c6a20a69f9"
   license "MIT"
   head "https://github.com/nodejs/node.git"
 
