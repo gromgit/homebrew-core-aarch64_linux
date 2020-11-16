35b2158551acbe1cdeba0d13a860d614830d4b11
Jonathan Chang <me@jonathanchang.org>
2020-11-16 10:52:59 +0000

    standardese: update 0.4.1 bottle.

--- Formula/standardese.rb
+++ Formula/standardese.rb
@@ -8,6 +8,7 @@ class Standardese < Formula
   head "https://github.com/standardese/standardese.git"
 
   bottle do
+    sha256 "3979144e637214aa4901c238d01bcfc821035393f11e8e44dc4c66905b87722e" => :big_sur
     sha256 "9d7a5ff92c2cea51bcf4c7814710d9585d92ae7525cc932ee02af0f8a866ab86" => :catalina
     sha256 "1408890c67458e000e8b637a7c585a6114f7c0cefbad2e2069f6d9f8a7ae1024" => :mojave
     sha256 "b6e6f560abcd8a480e35e2613d971432129c8879d66aa55a37d00f18ca473dc6" => :high_sierra
