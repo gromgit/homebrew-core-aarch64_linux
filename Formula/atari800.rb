15f1cba9eb04eadc377108e117a15ae41f2db9d7
Jonathan Chang <me@jonathanchang.org>
2020-11-16 02:24:40 +0000

    atari800: update 4.2.0 bottle.

--- Formula/atari800.rb
+++ Formula/atari800.rb
@@ -12,6 +12,7 @@ class Atari800 < Formula
 
   bottle do
     cellar :any
+    sha256 "b2ea1619d6fba699f3936cfb82c28b6d73aafc9d41ad39ee51e9bd49a5840ab2" => :big_sur
     sha256 "5bc2d96418e21a76809abfa2513f630cb207848b0894f27cda34b2f55bace81c" => :catalina
     sha256 "f5508c8a0021a0fcbd1e35d7a4313d8c9ba52ab937d0fde59ccc5f81e3c49ac0" => :mojave
     sha256 "497ce17afc99e76180b94b76b08c1aca973eab554ee3ccecfd14818c71e97035" => :high_sierra
