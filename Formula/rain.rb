850af7be01c77a5323f52294f289be888d5e6f48
Jonathan Chang <me@jonathanchang.org>
2020-11-16 04:35:24 +0000

    rain: update 1.0.1 bottle.

--- Formula/rain.rb
+++ Formula/rain.rb
@@ -7,6 +7,7 @@ class Rain < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e69463fc1800789c8b6a02560597aeab9e5f5bbf2dd555f2adf68bab49ae5ec8" => :big_sur
     sha256 "b4539bb5e88e61706954b63cc0e330755324e0baee5d78ad63b5e2248b250af8" => :catalina
     sha256 "c48a837ff273ca1f91b11beb8fd65b33edb83e340d73d80b4038f5c432a6801c" => :mojave
     sha256 "d78bf038a48d0618a2263e72e4acd8e3a1ff10340974d585fd826a35c7b8c0fa" => :high_sierra
