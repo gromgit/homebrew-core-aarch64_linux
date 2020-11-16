f427c742b0f56a555d0ff4a9b8e4d09a106396fc
Jonathan Chang <me@jonathanchang.org>
2020-11-16 10:31:00 +0000

    xmlcatmgr: update 2.2 bottle.

--- Formula/xmlcatmgr.rb
+++ Formula/xmlcatmgr.rb
@@ -11,6 +11,7 @@ class Xmlcatmgr < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "cf19153b0ce232b3fe88cd0d2288a4d94056b3092a8c64483fec2634dc821605" => :big_sur
     sha256 "ae788970290574145fa3ca20e389469f1a8582c8b604a50e3e506b7ffcb9faa4" => :catalina
     sha256 "eb8b0acfdaed325cce3e6b7befb53a675f9f7f6dc8aa5d058b4ebecea2d50e53" => :mojave
     sha256 "bbb201365fe9f89dc036d97e7bcb05d5b299e32f2ad427266f1d73934fd03cb4" => :high_sierra
