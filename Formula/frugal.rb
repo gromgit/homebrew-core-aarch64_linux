55964205e7fd337e1a5163807a8c773b38bce0c6
Jonathan Chang <me@jonathanchang.org>
2020-11-16 22:53:09 +0000

    frugal: update 3.12.1 bottle.

--- Formula/frugal.rb
+++ Formula/frugal.rb
@@ -7,6 +7,7 @@ class Frugal < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9dbbc0e9f24568826e15c259b73e66b9ad90ccbd08d6e45f5a877c4dbc484ba7" => :big_sur
     sha256 "c845b9c6362982511fb2ef5fc845e604aed74a743a45a56ae8b13507da003b69" => :catalina
     sha256 "e40ff743a3a9683c8091a58f7d0d2000cc07f0fbfe8e3435c73d28aadee9b193" => :mojave
     sha256 "3db23dd902f7abfc932ca302d3ecdfc456f39cca2080830b84781451d28cd078" => :high_sierra
