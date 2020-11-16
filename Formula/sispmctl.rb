015fc3556910c88cb0a2a6e0bed36378e3190d8c
Jonathan Chang <me@jonathanchang.org>
2020-11-16 06:12:01 +0000

    sispmctl: update 4.8 bottle.

--- Formula/sispmctl.rb
+++ Formula/sispmctl.rb
@@ -10,6 +10,7 @@ class Sispmctl < Formula
   end
 
   bottle do
+    sha256 "363a600681cb459b5c55ccf6217d5444b09cf163a095e11b6cde646c5f2d2206" => :big_sur
     sha256 "2c4a8a22528a34de494a7e5f93873f80cf4bc47646e725baddd86bb2d242ade8" => :catalina
     sha256 "ec93867cb3b46835c74d640fdb329735b15cb4b273b722cb5855808a764e27c9" => :mojave
     sha256 "1d7c6d1b3e44399b748f6e47d08e8b381ac9685cacd2d0993fbdac140760cc4b" => :high_sierra
