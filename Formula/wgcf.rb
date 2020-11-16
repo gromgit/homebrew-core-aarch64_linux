3fe62c7721ddc3f10a5aadc4e90a2aa1fc6c2508
Jonathan Chang <me@jonathanchang.org>
2020-11-16 06:11:42 +0000

    wgcf: update 2.1.4 bottle.

--- Formula/wgcf.rb
+++ Formula/wgcf.rb
@@ -8,6 +8,7 @@ class Wgcf < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "94872893207ebc688892b960271ee100e1ce5a6dc73dd6195bb2756d3a5aa344" => :big_sur
     sha256 "c8f35565606c6fe3cc7b79105076e4d8e1106950cfb0ab3fdd184b089e5e94ff" => :catalina
     sha256 "1635231fe27fd9dfa0a9202dcf266163f131254843de7546e2b910800545a140" => :mojave
     sha256 "0debd1f07625d225e2683b95a158c2d537a3ab7bae64335b6a3e4dfe745bfbdd" => :high_sierra
