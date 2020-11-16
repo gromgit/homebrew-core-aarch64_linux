80e233e3f0cf6143c07f4264802fabf034654626
Jonathan Chang <me@jonathanchang.org>
2020-11-16 22:54:48 +0000

    simgrid: update 3.25_2 bottle.

--- Formula/simgrid.rb
+++ Formula/simgrid.rb
@@ -13,6 +13,7 @@ class Simgrid < Formula
   end
 
   bottle do
+    sha256 "beea9ed8a14d679d2f9aef9be80b61cfe152e0cc1078837ef6c1c1e5f5c04c94" => :big_sur
     sha256 "f735fe9ac565cd1fe49b9117be9ca64a3a15a8dd69dbbf2a4385a82bfd201b4e" => :catalina
     sha256 "9fa0989ffe0e2018e105f8a22ab9e9178bc456dd5430d8468866c6b57ed3bf26" => :mojave
     sha256 "eefddfa608d34b725af614af41a93ca017a761f877257300b182df0f56ad6bfe" => :high_sierra
