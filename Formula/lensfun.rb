6bc93f16f950ba9d9c519225d83f1f35fe717050
Jonathan Chang <me@jonathanchang.org>
2020-11-16 04:28:39 +0000

    lensfun: update 0.3.95_3 bottle.

--- Formula/lensfun.rb
+++ Formula/lensfun.rb
@@ -12,6 +12,7 @@ class Lensfun < Formula
   end
 
   bottle do
+    sha256 "10c8d68811e3d286d340b8cdea0fff28380d719402f25e85ddc280e9d0a4b1fd" => :big_sur
     sha256 "1e83aa5f7ebcb3d3952a384244adfacbf82b2954637cd3df02337f52de1d7b67" => :catalina
     sha256 "d0d64a98f863a5001667095eba97f55500a87610e00955c0edb95f70300e21c1" => :mojave
     sha256 "0050ca39268f8201a1c28169da62380385ba5e77954142404465e6b3cba909d7" => :high_sierra
