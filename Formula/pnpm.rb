8817204addf453a14c8e56266b67c84521b60efa
Jonathan Chang <me@jonathanchang.org>
2020-11-16 19:13:45 +0000

    pnpm: update 5.12.0 bottle.

--- Formula/pnpm.rb
+++ Formula/pnpm.rb
@@ -13,6 +13,7 @@ class Pnpm < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e6bc29ee59311c5a06271af8ac9cfc8d7d42842e878bbe9977adbe0f2503f111" => :big_sur
     sha256 "5af4d9e336fb3e93fd66a08cbb9eaea16c3c927dfc59c08444b0b1d185362be8" => :catalina
     sha256 "1b699ecd9dbebbe1ddd3ccef70c983f773c9d0d966022b345d84fb009f08c9f1" => :mojave
     sha256 "5399489f5309345034d8c45ad57273b1328d2d9bd1859613446713ed8eb1f749" => :high_sierra
