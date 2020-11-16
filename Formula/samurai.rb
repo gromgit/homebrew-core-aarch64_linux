f008e82724d1d4131c027545b41d2e8556d9ed83
Jonathan Chang <me@jonathanchang.org>
2020-11-16 14:10:51 +0000

    samurai: update 1.1 bottle.

--- Formula/samurai.rb
+++ Formula/samurai.rb
@@ -8,6 +8,7 @@ class Samurai < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "070b95f4b8998f10f22e429499f77b885de00d01deff3e2a596501fd2cafe566" => :big_sur
     sha256 "ef3f56caffb50416e4d708cea0910b1658811464b94697b850760bf6acc70e4a" => :catalina
     sha256 "90d42648667443f8bc1e9de25e221b2c16d8c973bac6cda33bfb1f611893144a" => :mojave
     sha256 "e9c857e0bf7f2713d90d21040ab28431c03fe0e560a17d8dab6a3b4d6c463111" => :high_sierra
