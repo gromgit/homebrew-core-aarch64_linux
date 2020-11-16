e333f6a50876565c2c2a0d3b62b6e9a3182c4850
Jonathan Chang <me@jonathanchang.org>
2020-11-16 09:43:32 +0000

    whatmp3: update 3.8_4 bottle.

--- Formula/whatmp3.rb
+++ Formula/whatmp3.rb
@@ -11,6 +11,7 @@ class Whatmp3 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "68a5f39de733c7d95679ac3e9c91e9edd543eb9450592a517b2491dd7089641b" => :big_sur
     sha256 "d449a8bb1339bbc02f27b930a08c21f2acfdbbb49f45dd43ea4015054607244f" => :catalina
     sha256 "73e35194bc0eed4cab900ae2436ca67283bddc942d73a77f84e3aa02cf2e518a" => :mojave
     sha256 "87e78b789996ff11ea55d9224896d581d0aff8a6ad10e14e456588903cf592cb" => :high_sierra
