d3992bdcdf2c6a4568ab6cba6277194babeadb8c
Jonathan Chang <me@jonathanchang.org>
2020-11-17 09:17:56 +0000

    chkrootkit: update 0.53 bottle.

--- Formula/chkrootkit.rb
+++ Formula/chkrootkit.rb
@@ -12,6 +12,7 @@ class Chkrootkit < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b57bf9581b8c27586144ff53aa01dcb7954e930614466d15363982948cd03ff4" => :big_sur
     sha256 "23a9f903721d19c0b6201163cb937823970c66592f094c673b1de1036da8bef9" => :catalina
     sha256 "286de88eef77a53b9c7fab85ef3cec8b9876cf49a48910cbb591e44c9ca5d631" => :mojave
     sha256 "55ab9957505513fd81d670c54e5ad1834fb72ae9cda7bd7cbc63f98feeccf24a" => :high_sierra
