7bb87471293e0ba09511be1a3427ddda37858bb3
Jonathan Chang <me@jonathanchang.org>
2020-11-17 02:45:23 +0000

    mairix: update 0.24 bottle.

--- Formula/mairix.rb
+++ Formula/mairix.rb
@@ -13,6 +13,7 @@ class Mairix < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "26221ca2d6ce638ad3f47a597a4b67654ecaaa335f54577b380404951733113e" => :big_sur
     sha256 "3c74d81ccb04da6f3fa9f0f734861738f6dcc924bde12dc8055fd73ea1be68ce" => :catalina
     sha256 "483128f4a24cbf40c26ceef2a9951c44992c57f114327671883b8ab7b9da8569" => :mojave
     sha256 "5975d9e5b741611279f008a50febebfa9d91c4e3e8448c4d8eda80cbd5c371af" => :high_sierra
