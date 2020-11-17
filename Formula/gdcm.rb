db3eaf14c85394ecdfbfee526151264f5554f996
Jonathan Chang <me@jonathanchang.org>
2020-11-17 15:05:38 +0000

    gdcm: update 3.0.8_1 bottle.

--- Formula/gdcm.rb
+++ Formula/gdcm.rb
@@ -12,6 +12,7 @@ class Gdcm < Formula
   end
 
   bottle do
+    sha256 "2f416ce53cbca29849bf6d3720d289f0b778593620937ba8a833c75a257aaf89" => :big_sur
     sha256 "d5ce02b3b5473665b241484eff50b226d7b7f800253255362c26195fc69e40eb" => :catalina
     sha256 "3e75b52ddc6151a6f39a63e2d4403c960247b0a6b780a2488cc6bda293a773fa" => :mojave
     sha256 "8a5ac94c29c78add6e335dd5a07ef2f2b2f2d0c7da9704d7655d9800f5278dd8" => :high_sierra
