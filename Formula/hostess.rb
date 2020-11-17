eaf8b8d5e41af845ca55594c4ba738fa60d21b3d
Jonathan Chang <me@jonathanchang.org>
2020-11-17 01:38:04 +0000

    hostess: update 0.5.2 bottle.

--- Formula/hostess.rb
+++ Formula/hostess.rb
@@ -8,6 +8,7 @@ class Hostess < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b3ba5dc41977ec8bf7ec2075079131202a24e2a1f30736890ab7b44301c04dd3" => :big_sur
     sha256 "80480773a167fdcad3fadb3feeb298b51aeb89aec5863204f512f941af7271da" => :catalina
     sha256 "f3f06881067507c0d115209d515e6ebbe4090d7aa8fcff7bc685027c49ea6479" => :mojave
     sha256 "15050f5b2f5936fe74e47937323c8a872ec12b75ed639b3df2c6eac11cf7da6f" => :high_sierra
