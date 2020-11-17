bc7fc32a67d505d045928645183231804709068e
Jonathan Chang <me@jonathanchang.org>
2020-11-17 06:10:38 +0000

    ghq: update 1.1.5 bottle.

--- Formula/ghq.rb
+++ Formula/ghq.rb
@@ -9,6 +9,7 @@ class Ghq < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "65c3fa300e2dc1f8e20634feb2acb60b7ba2ee9aa17a87dab39acafd4869246d" => :big_sur
     sha256 "247f4e7b0964b0a27321b24ae06349d9eac65280345dda17680fa8049c29ac75" => :catalina
     sha256 "a863be3a3b307c82821d0dbed2d4d1faa05aaea9c6dd150d7b3f2a878e4bd79c" => :mojave
     sha256 "060f3bc0d7b55e04274383faa21b45e8d8457ca61e25fd3416b2fe0d3177036b" => :high_sierra
