79f5ddd37067862c165fc7c0eb6526d5001f6e2d
Jonathan Chang <me@jonathanchang.org>
2020-11-17 05:22:00 +0000

    oil: update 0.8.3 bottle.

--- Formula/oil.rb
+++ Formula/oil.rb
@@ -7,6 +7,7 @@ class Oil < Formula
   head "https://github.com/oilshell/oil.git"
 
   bottle do
+    sha256 "42068d72ea52048dc6bde38406c5f95199533739001d969a526e30b32d15c264" => :big_sur
     sha256 "b184f8496f001e47b9dbf2822cf69476c74af91042d1fffe855fd347ca24fb80" => :catalina
     sha256 "80bc6f37ee51eec363ac13c69c3910aef46d594a9d16a1de1b14645c3437ef10" => :mojave
     sha256 "43d4aa89e3a88da11799780d154bb629bb051b52c3e251e2f47faf623f02e28a" => :high_sierra
