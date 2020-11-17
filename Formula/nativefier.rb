2fc1cfeb9811613ad649ae99f56d4cc72531f108
Jonathan Chang <me@jonathanchang.org>
2020-11-17 09:48:01 +0000

    nativefier: update 10.1.5 bottle.

--- Formula/nativefier.rb
+++ Formula/nativefier.rb
@@ -13,6 +13,7 @@ class Nativefier < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "dc22acc7d408727d6f19246b52e14aba267860fd4d6e5ac2025e50755636ee84" => :big_sur
     sha256 "f0e3d16de650bcd069d5c9462c7300349b00d3eabd2c51255d49fb7c6ad855ce" => :catalina
     sha256 "9557bec53baccea0b76b019b0f53d585fcdcd9079a30ceb93accddb0f34eea01" => :mojave
     sha256 "acc4390688781e902fbdcc1fb0c6bac93546d371480ed0410c45fd63517c361e" => :high_sierra
