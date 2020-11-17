d7d448aeb0b732e5ff1002fd4ed905740bcbb88b
Jonathan Chang <me@jonathanchang.org>
2020-11-17 13:44:14 +0000

    gcc@7: update 7.5.0_2 bottle.

--- Formula/gcc@7.rb
+++ Formula/gcc@7.rb
@@ -12,6 +12,7 @@ class GccAT7 < Formula
   end
 
   bottle do
+    sha256 "4c0fc49fe4d65a7ab06a9417987cc2c1367959f885d0c77cd3ba36ca78e129d6" => :big_sur
     sha256 "4dca3b07173bffba262e003b345970119626a4d60a25c167d6bc216c46f1d83e" => :catalina
     sha256 "5d4086421866078dc4d9bfe623a38160dbcf04ff88b4d0284dee9da66ff50f4c" => :mojave
     sha256 "3c2135fc0b1c7f0ce048c1f610d2131a237911bdbd66d8bff731bbf9c6d84bcc" => :high_sierra
