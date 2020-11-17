970a96822b4a146d06da07a7bbfbf71e5b54ffb8
Jonathan Chang <me@jonathanchang.org>
2020-11-17 00:06:27 +0000

    primesieve: update 7.5 bottle.

--- Formula/primesieve.rb
+++ Formula/primesieve.rb
@@ -7,6 +7,7 @@ class Primesieve < Formula
 
   bottle do
     cellar :any
+    sha256 "fb8828f5f605378c1e10ebb8b5b997159c4a49f1a29256b0807a307df9d4ac24" => :big_sur
     sha256 "93a49e499f6cca69e9bc62e64e3bbf51fd9c8ab3c51e6ba15b6a1ab3feb4d2e8" => :catalina
     sha256 "7070ec9a601335d1844820e722c346b9d7003ce80af4450f631521a023c291bc" => :mojave
     sha256 "46185fc5980d6e411f4073ea330735498d918bb25dc65cb90909bb41749e8756" => :high_sierra
