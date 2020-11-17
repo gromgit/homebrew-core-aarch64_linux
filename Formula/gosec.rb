a39b091781eb79adbe7f75feb39b514ccc8405a0
Jonathan Chang <me@jonathanchang.org>
2020-11-17 02:07:19 +0000

    gosec: update 2.5.0 bottle.

--- Formula/gosec.rb
+++ Formula/gosec.rb
@@ -8,6 +8,7 @@ class Gosec < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "730bfb268da4ca072d44fc01c8d3f3ba87a1293d637447d996d18c5176e83b84" => :big_sur
     sha256 "f2ab2597115da15b5310c5456a4fe363e591432bc283dd2666f1f587b8f2cde3" => :catalina
     sha256 "c0217676973c37182efc63f728471659f7c5129f413df6923b73b1e91ea744db" => :mojave
     sha256 "8b70996fcd2624ac7bec3602a5f2002935924ad6cf8bbd90a670b9764540759c" => :high_sierra
