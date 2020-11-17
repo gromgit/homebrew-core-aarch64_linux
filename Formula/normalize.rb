2f4bac11b18dffc69a5f1f27d5adaff9f93931c4
Jonathan Chang <me@jonathanchang.org>
2020-11-17 00:02:14 +0000

    normalize: update 0.7.7 bottle.

--- Formula/normalize.rb
+++ Formula/normalize.rb
@@ -13,6 +13,7 @@ class Normalize < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "a35a01c8d74067d94fda21d31a0ab65128842e6dc1ed0629ed6cadf99f13a9b5" => :big_sur
     sha256 "363ac4a56ccb75ff32f3af3ef42a6cf5d74f24b977939bb08c14fddc30ff2ef5" => :catalina
     sha256 "8e1ac6ecbf84164c27a804b158201b75ddaabd3237e5826d7ffc78fbe8ee7377" => :mojave
     sha256 "e4dd195c639807e3e2e25fee6c5c6f3c4263a37a4c7f8f25ab656a96395faeaf" => :high_sierra
