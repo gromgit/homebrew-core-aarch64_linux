22f78ed3b81215be571429c0de4ede5f8ad5762b
Jonathan Chang <me@jonathanchang.org>
2020-11-17 06:50:12 +0000

    mpw: update 2.6-cli-5_2 bottle.

--- Formula/mpw.rb
+++ Formula/mpw.rb
@@ -15,6 +15,7 @@ class Mpw < Formula
 
   bottle do
     cellar :any
+    sha256 "4ae85b3d30e47294436b7fddca456c98ed2bf546793f2ef9d57a372d782fb072" => :big_sur
     sha256 "2f275d762a9c73bd6b3f2e5a7f3f13a9c99ddfc3e2f89a2ededa07ba89b6de40" => :catalina
     sha256 "9103716223529cd3e2cb969e904892bf2022cb8e73918418f2d3d343d1325c80" => :mojave
     sha256 "07b89df8d96f9c1cebbf6296a4e98b2bac833c45f736b646a1eba24bd5244732" => :high_sierra
