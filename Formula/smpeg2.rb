f4ec86c391c8b450ba6f42f48d25e01dd7c3ffaf
Jonathan Chang <me@jonathanchang.org>
2020-11-17 06:34:16 +0000

    smpeg2: update 2.0.0 bottle.

--- Formula/smpeg2.rb
+++ Formula/smpeg2.rb
@@ -11,6 +11,7 @@ class Smpeg2 < Formula
 
   bottle do
     cellar :any
+    sha256 "816b0441d6d10986dea2c3b55f7889a9a72783c2fc55e92520fe9366b53a769c" => :big_sur
     sha256 "97d4ca204752184752822f6c4bbbff2e044d110917f6a0ce30c3c472d6cb622c" => :catalina
     sha256 "4ecef89d7ee22d5d23703a5ac29fb6b4fc0cd025e249219a194ca325d79dfa85" => :mojave
     sha256 "927cb1d5dd58481afd16e893868a0794b42d56588e7fe9d51b881812e2f26eb6" => :high_sierra
