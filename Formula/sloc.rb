93edba740e16a1446bff16eabcf77e1c978345ab
Jonathan Chang <me@jonathanchang.org>
2020-11-17 12:04:26 +0000

    sloc: update 0.2.1 bottle.

--- Formula/sloc.rb
+++ Formula/sloc.rb
@@ -13,6 +13,7 @@ class Sloc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "28fb222768fc7ca90e6a9fa29c97d5f79f11bd9a928024ae84aabe8dfb8f0ff9" => :big_sur
     sha256 "91254bb1e206f528b6b7d7a9afbdeec8390cfc54ad13d0850cc202d6242e08d0" => :catalina
     sha256 "f241a7bf03cb7bb97bb061f5f46442d7a40de893697a5335c821049d471e9466" => :mojave
     sha256 "345308d671b83edb390c143554c64958135cf37bc7cd365ce613011da682a8b7" => :high_sierra
