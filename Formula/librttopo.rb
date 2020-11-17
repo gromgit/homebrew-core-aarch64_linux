da40f23529d16ed1fef8d3602f8328d30e15e127
Alexander Bayandin <a.bayandin@gmail.com>
2020-11-17 17:29:18 +0000

    librttopo 1.1.0 (new formula)
    Closes #65018.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/librttopo.rb
@@ -0,0 +1,34 @@
+class Librttopo < Formula
+  desc "RT Topology Library"
+  homepage "https://git.osgeo.org/gitea/rttopo/librttopo"
+  url "https://git.osgeo.org/gitea/rttopo/librttopo/archive/librttopo-1.1.0.tar.gz"
+  sha256 "2e2fcabb48193a712a6c76ac9a9be2a53f82e32f91a2bc834d9f1b4fa9cd879f"
+  license "GPL-2.0-or-later"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "geos"
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <librttopo.h>
+
+      int main(int argc, char *argv[]) {
+        printf("%s", rtgeom_version());
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lrttopo", "-o", "test"
+    assert_equal version.to_s, shell_output("./test")
+  end
+end
