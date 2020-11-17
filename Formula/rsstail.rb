36a88eac93ac1dd10139119aa5adda9c196421c4
Jonathan Chang <me@jonathanchang.org>
2020-11-17 07:39:27 +0000

    rsstail: update 2.1 bottle.

--- Formula/rsstail.rb
+++ Formula/rsstail.rb
@@ -14,6 +14,7 @@ class Rsstail < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "be1e378b0c375b4087bf57bcb1c97d94eddb25fccf55587a732ff6cd5f1fdc72" => :big_sur
     sha256 "253a99c8187e0dc6fb29049273f08dd4b199c0de1e9171c4a0829f21aaf5c90f" => :catalina
     sha256 "b6f2a222c1bc903a5d0179331398ced65980798d694d186bd52e0b54239d9dfd" => :mojave
     sha256 "29b1cd5b6cbfbd66d250586450e2e24e5706da80b03aa1b54834bd0c01e73202" => :high_sierra
