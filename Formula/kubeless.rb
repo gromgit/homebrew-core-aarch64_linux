a619689dff2f26f17310d15bb674a048911b5fb6
Jonathan Chang <me@jonathanchang.org>
2020-11-17 07:23:29 +0000

    kubeless: update 1.0.7 bottle.

--- Formula/kubeless.rb
+++ Formula/kubeless.rb
@@ -7,6 +7,7 @@ class Kubeless < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0f0bed8b5c424de1bc1552ba8a4063ab9f68213dd32114166388624958fe69f6" => :big_sur
     sha256 "94927a41f4778a3e99934154bda7db05c4048e83d49a840ecf7eca6ddfbc32e4" => :catalina
     sha256 "0b0f24835a3fb21b5a5459a030f821f2c13691ad8978a02067dfb38f15d8ac6f" => :mojave
     sha256 "ec427f71d144c616cfc1803da51caf69f6ead7cefd5ade2f8c23129d73eec705" => :high_sierra
