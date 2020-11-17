7baf825be09172d257b6ab21fe61ef924da89039
Jonathan Chang <me@jonathanchang.org>
2020-11-17 07:48:43 +0000

    lilv: update 0.24.8 bottle.

--- Formula/lilv.rb
+++ Formula/lilv.rb
@@ -7,6 +7,7 @@ class Lilv < Formula
 
   bottle do
     cellar :any
+    sha256 "007ce3e9fd4af5a01085bb5c8a7a5ced7c4221d1b42f8e94fb72c59c53fe0642" => :big_sur
     sha256 "b772a0b962e14b7ae737f3bdf5778b34092e86eeca19e2c520395777b87be9cd" => :catalina
     sha256 "01c5b495e5c288e19a325164f482cc3058865b118f2d5c05a52e44db5deec302" => :mojave
     sha256 "63c518910af136ca8b4f685924ccd3e6f2687cd061cd561c4aab69af7fa62361" => :high_sierra
