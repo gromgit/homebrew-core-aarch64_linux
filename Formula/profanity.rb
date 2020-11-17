f88c9040bd30d6b9c4b070aaea4b46e12b4736bf
Jonathan Chang <me@jonathanchang.org>
2020-11-17 06:04:32 +0000

    profanity: update 0.9.5 bottle.

--- Formula/profanity.rb
+++ Formula/profanity.rb
@@ -6,6 +6,7 @@ class Profanity < Formula
   license "GPL-3.0"
 
   bottle do
+    sha256 "e674dc595aef44e934d0ae9ca90b46a42b2b10295b8dcd732f55475cb20c1acb" => :big_sur
     sha256 "b53f7fbd103d911f55337861bf36957cfaead39e8c38478ed03eceb41b507872" => :catalina
     sha256 "7112d51c1a187ca47b6d245d5600b46b3f0765efd5f2a215ce1a2d2327f2b884" => :mojave
     sha256 "21837ee57161928d0389dcd7170245b61e0d2f2c8b2f702dc6127bd98380b477" => :high_sierra
