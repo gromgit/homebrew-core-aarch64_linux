5c00233f4a01fc180fb92f3b188ea49c463607dc
Jonathan Chang <me@jonathanchang.org>
2020-11-17 09:08:17 +0000

    osm2pgrouting: update 2.3.6_2 bottle.

--- Formula/osm2pgrouting.rb
+++ Formula/osm2pgrouting.rb
@@ -9,6 +9,7 @@ class Osm2pgrouting < Formula
 
   bottle do
     cellar :any
+    sha256 "5e935d46ae6532b2cd75b73ebaf13bcbf25472e29e0281236fdd002c3317f3bd" => :big_sur
     sha256 "99b349358db3adfb8a52dfcd2cfde049cecd5cea0b46cc6fa2a4fb6e2e9b4523" => :catalina
     sha256 "c99a46012e8a582678e3b28e73f8c570606477d2b76d125dee5070fde5516b5c" => :mojave
     sha256 "1ec262b471c1dd25b7609b5c36b53798375fb5d1015eb0d968a8023a698ed113" => :high_sierra
