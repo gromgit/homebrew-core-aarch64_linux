e014703093cfdc89f1439a6e21eb3b80594ab00b
Jonathan Chang <me@jonathanchang.org>
2020-11-17 06:18:12 +0000

    gist: update 6.0.0 bottle.

--- Formula/gist.rb
+++ Formula/gist.rb
@@ -8,6 +8,7 @@ class Gist < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "3df0b9c1971dc59b1ccd20dd0c1516dd8ba46a0fb58ecaa5b6c0bf9386c69c7a" => :big_sur
     sha256 "1d756e91ae99001381dda127f1315af4363cb158e323d83c72f466a5ff7c3e36" => :catalina
     sha256 "af69b6fdaf48f811b2eb1789febe49677d74375da1e4b118b7753ff783f6ce0c" => :mojave
     sha256 "28f8947a2912459cc79536eed7cbc2af958c9282f31990741f2fd0f32fffa70a" => :high_sierra
