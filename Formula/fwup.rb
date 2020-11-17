3ef58e9352c6b1f537845fa3f81a35bd7626675c
Jonathan Chang <me@jonathanchang.org>
2020-11-17 06:42:02 +0000

    fwup: update 1.8.2 bottle.

--- Formula/fwup.rb
+++ Formula/fwup.rb
@@ -7,6 +7,7 @@ class Fwup < Formula
 
   bottle do
     cellar :any
+    sha256 "6c13967bf80a05f2133f617952e4bb2b50087984f55af2e75266113cb865b225" => :big_sur
     sha256 "74af7cfb5ed18a50e9fbd56c4167f7bc3c5ebeec69cc8c7660b80e20020739b8" => :catalina
     sha256 "4e33b196f1f611272cf007528088ac485a19bf0fadbb32b1c4d3b8180540bec6" => :mojave
     sha256 "77a1aa5b48fca51379c19531764e776bd795d4ffbfd61dd2aafbc50bdc33219c" => :high_sierra
