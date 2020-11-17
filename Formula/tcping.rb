552ad8f15e8417fc59f5146dfb2da2fff31631eb
Jonathan Chang <me@jonathanchang.org>
2020-11-17 03:35:06 +0000

    tcping: update 1.3.6 bottle.

--- Formula/tcping.rb
+++ Formula/tcping.rb
@@ -13,6 +13,7 @@ class Tcping < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "ca791764d99fc9e263fbbc3352d1861def26900f099552fcfce866eb25934fe9" => :big_sur
     sha256 "2cf829fa6b3feab933a12f8fbc9fc1e8d585a304f31d918f26ba0d502f4772ab" => :catalina
     sha256 "d769f344e5bdda11b8f0ce6c931e865e982166b3b504cdce33d58c9029786c60" => :mojave
     sha256 "e0d7d617ac3f98158cd25a08728f9f44cce132101368cae250cced7dbb6a0f7a" => :high_sierra
