40944e689d782d9040fd4b4695e4a213fc546ef7
Jonathan Chang <me@jonathanchang.org>
2020-11-17 19:21:51 +0000

    fizsh: update 1.0.9 bottle.

--- Formula/fizsh.rb
+++ Formula/fizsh.rb
@@ -13,6 +13,7 @@ class Fizsh < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7e97c31a73043c5f5b0119865e63ddba6d0df0aeb87eb19fb4434ac2a5d60ba4" => :big_sur
     sha256 "c1cb674d5a7219ea4c41f3becd0997cf55dfd26a06a17d739f14a3d59da7df75" => :catalina
     sha256 "02457429b5257b916207bc7f46acd5577f8e01183437ef03b594991ba3e69466" => :mojave
     sha256 "7916e571aaf891561a5a6be1ef9708e63ee17ecb41fe60b75129c765d3dad1cb" => :high_sierra
