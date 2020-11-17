cf112260e4ec4f99d9bc247d47602f262c6a3e29
Jonathan Chang <me@jonathanchang.org>
2020-11-17 06:41:22 +0000

    webalizer: update 2.23-08_2 bottle.

--- Formula/webalizer.rb
+++ Formula/webalizer.rb
@@ -7,6 +7,7 @@ class Webalizer < Formula
   revision 2
 
   bottle do
+    sha256 "c2b261ea2ecd03ee71f43ecc38ca50ad2de689199fafe0711bc639da2a0af94c" => :big_sur
     sha256 "c7b023658cc745cb0e5d383953e23a2d5a07dcf08b8e4addee7b7a108ef3a725" => :catalina
     sha256 "525c739550139303d96d823e9f50aca6255bb77eac70d45f2c1259aa59755f6b" => :mojave
     sha256 "e27c0dd7038a5a82e6fa127428c0b98750801e343b1b973b05bb08f38b055cdd" => :high_sierra
