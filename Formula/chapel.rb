626871fd301d0066182252620b4cd497b9b62a34
Jonathan Chang <me@jonathanchang.org>
2020-11-17 04:41:55 +0000

    chapel: update 1.23.0 bottle.

--- Formula/chapel.rb
+++ Formula/chapel.rb
@@ -6,6 +6,7 @@ class Chapel < Formula
   license "Apache-2.0"
 
   bottle do
+    sha256 "5a3c5dc65fc572a84d9e4a8a5bcd7cac9c072822dbf674ebe32d7b740aeda63b" => :big_sur
     sha256 "5aa5b6a7e03ed702530959c1983e8989be5f1ba4494af6d9da044e38ef62bfcb" => :catalina
     sha256 "123c5824b82621b4984e1afbf4f01f5c20af8cbc03e7b1266af396f8eaa80f79" => :mojave
     sha256 "101b6a940e07d3e86ec809b3f7737950e0ef12c61daf43ef92c00e95047333c5" => :high_sierra
