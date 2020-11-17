c180c2602b8e1841fab510326655835994d2f864
Jonathan Chang <me@jonathanchang.org>
2020-11-17 20:05:24 +0000

    cvsps: update 3.13 bottle.

--- Formula/cvsps.rb
+++ Formula/cvsps.rb
@@ -7,6 +7,7 @@ class Cvsps < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "e7e61acca8f1ee7d1489615e38299d49fafad781a0efabc45a60a35482deef9a" => :big_sur
     sha256 "98ee59741e5e48ef4ca18f5b0b08cfc8eab19d8e96cebcb75e989334add7411f" => :catalina
     sha256 "8901d9d03137e3ebc2dfa52eeec1b6fb5278aa21fcd2075302ea9c9e20ff1db5" => :mojave
     sha256 "d67b00e52b9688d1249d996eaf94a728691f0b171b1708e83bba07508939d376" => :high_sierra
