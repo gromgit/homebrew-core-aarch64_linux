4ff488ffc18315c31824df560a19e627cc656a57
carlocab <30379873+carlocab@users.noreply.github.com>
2020-11-17 21:24:03 +0000

    groff: revision
    * groff: fix build
      See https://github.com/Homebrew/homebrew-core/issues/65000#issuecomment-728899218
    
      Builds on https://github.com/Homebrew/homebrew-core/pull/64842
    
      - [x] Have you followed the [guidelines for contributing](https://github.com/Homebrew/homebrew-core/blob/HEAD/CONTRIBUTING.md)?
      - [x] Have you checked that there aren't other open [pull requests](https://github.com/Homebrew/homebrew-core/pulls) for the same formula update/change?
      - [x] Have you built your formula locally with `brew install --build-from-source <formula>`, where `<formula>` is the name of the formula you're submitting?
      - [x] Is your test running fine `brew test <formula>`, where `<formula>` is the name of the formula you're submitting?
      - [x] Does your build pass `brew audit --strict <formula>` (after doing `brew install <formula>`)?
    
      Newly added dependencies are for creating PDF and HTML files, and for
      recognising file encoding.
    * Update groff.rb
    * Update groff.rb
    
    Closes #65042.
    
    Co-authored-by: Jonathan Chang <me@jonathanchang.org>
    Signed-off-by: Jonathan Chang <me@jonathanchang.org>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/groff.rb
+++ Formula/groff.rb
@@ -4,7 +4,8 @@ class Groff < Formula
   url "https://ftp.gnu.org/gnu/groff/groff-1.22.4.tar.gz"
   mirror "https://ftpmirror.gnu.org/groff/groff-1.22.4.tar.gz"
   sha256 "e78e7b4cb7dec310849004fa88847c44701e8d133b5d4c13057d876c1bad0293"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
+  revision 1
 
   livecheck do
     url :stable
@@ -17,8 +18,24 @@ class Groff < Formula
     sha256 "2966f4b562c30eb6679d6940b43f4b99b2b625433e6a218489f160eb76c7c360" => :sierra
   end
 
+  depends_on "pkg-config" => :build
+  depends_on "ghostscript"
+  depends_on "netpbm"
+  depends_on "psutils"
+  depends_on "uchardet"
+
+  uses_from_macos "libiconv"
+  uses_from_macos "perl"
+
+  # See https://savannah.gnu.org/bugs/index.php?59276
+  # Fixed in 1.23.0
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/8059b3027a4aa68d8f42e1281cc3a81449ca0010/groff/1.22.4.patch"
+    sha256 "aaea94b65169357a9a2c6e8f71dea35c87eed3e8f49aaa27003cd0893b54f7c4"
+  end
+
   def install
-    system "./configure", "--prefix=#{prefix}", "--without-x"
+    system "./configure", "--prefix=#{prefix}", "--without-x", "--with-uchardet"
     system "make" # Separate steps required
     system "make", "install"
   end
