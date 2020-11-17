8491e5e2a72678e1fa1e3d3f77a318778edd2ac6
Jonathan Chang <me@jonathanchang.org>
2020-11-17 09:52:55 +0000

    blink1: update 2.1.0 bottle.

--- Formula/blink1.rb
+++ Formula/blink1.rb
@@ -9,6 +9,7 @@ class Blink1 < Formula
 
   bottle do
     cellar :any
+    sha256 "669d71aaa543c504392611cbaeecd2b2a0f1d10dad18f297a85152ea544bbb26" => :big_sur
     sha256 "fe997b4328928968cb091e9d748213160a1a0f7f4644621b14cc1a4bf7e06680" => :catalina
     sha256 "4168f9d980daf9f4864426a4447ba004efb492289d9fc8893b49f99d581a8e00" => :mojave
     sha256 "646191cfd60d1a6d332c536bf3e50914a2434d72afee888c40568eebd7617d35" => :high_sierra
