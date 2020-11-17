882204ed5bb554ade1866a7974b440dc22d8ec73
Jonathan Chang <me@jonathanchang.org>
2020-11-17 00:10:33 +0000

    blaze: update 3.8_1 bottle.

--- Formula/blaze.rb
+++ Formula/blaze.rb
@@ -9,6 +9,7 @@ class Blaze < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c05e15582afef1b0d7961736731a844b1939a8c99c1d60aa412aee5c5c5507f0" => :big_sur
     sha256 "a0ee08be16bbc07c8c3447d05768a736d577a78fdf8ad920cf4cf071cecb4ce4" => :catalina
     sha256 "d4c4f3f85b1d9f7759fa7fa9a9c850fd031fd5204b3b0beff5caa879a9561ea8" => :mojave
     sha256 "4cd2aa850c2749e6b8b9c82687e377bf48b0c97e55dba47c99182439677d042b" => :high_sierra
