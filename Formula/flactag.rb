c14d85b4afa8c386ab889ef037974711894136ba
Jonathan Chang <me@jonathanchang.org>
2020-11-17 04:57:25 +0000

    flactag: update 2.0.4_1 bottle.

--- Formula/flactag.rb
+++ Formula/flactag.rb
@@ -11,6 +11,7 @@ class Flactag < Formula
 
   bottle do
     cellar :any
+    sha256 "fd81ade08727163108bf6eb86fa4c971a7e7f902b720b7eec59e21cbe10fd945" => :big_sur
     sha256 "3bd18beb32b957d6adb4a4221fc5b833f4c9099798857911f8552294a104659b" => :catalina
     sha256 "89733c2da8653a9e86b2a4fc3e5693c3c7c434305d9aade353e52fd76f457dda" => :mojave
     sha256 "d066a517308ad0f3cbc6603fd7eeb53dba73dc796298163b6c1ec8c0379f72f6" => :high_sierra
