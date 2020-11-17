480461fe9a8d285e396dc8573f9ecc9220ee4087
Jonathan Chang <me@jonathanchang.org>
2020-11-17 06:59:02 +0000

    jsonnet: update 0.16.0 bottle.

--- Formula/jsonnet.rb
+++ Formula/jsonnet.rb
@@ -13,6 +13,7 @@ class Jsonnet < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e5f579df1018bcd462e4a6d913c7a080689b3cc7e8afb670e311a337206ba931" => :big_sur
     sha256 "af01487239fc6947ef6c27d9b4b18772d2b7773393d7e48704001bf09d380e5b" => :catalina
     sha256 "2255443d01048798797696161de2ddf435565348d7a246647a37b5ec0919dc2b" => :mojave
     sha256 "7cc0ca007b2d56160e93437779f7214f44caef1a76b0647c911b1ca6ac6ab4c5" => :high_sierra
