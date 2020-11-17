f76d9eeef49db09ec51a6ca1e6aa31b2251e0b0f
Steve Engledow <steve@offend.me.uk>
2020-11-17 16:31:30 +0000

    cfn-format 1.0.3 (new formula)
    Closes #65014.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: chenrui <chenrui333@gmail.com>

---
+++ Formula/cfn-format.rb
@@ -0,0 +1,22 @@
+class CfnFormat < Formula
+  desc "Command-line tool for formatting AWS CloudFormation templates"
+  homepage "https://github.com/aws-cloudformation/rain"
+  url "https://github.com/aws-cloudformation/rain/archive/v1.0.3.tar.gz"
+  sha256 "d79afec4780e6fd9561793a40f5d080d91edf7ea5e3fbeb7c5f8ad6accdd812f"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "cmd/cfn-format/main.go"
+  end
+
+  test do
+    (testpath/"test.template").write <<~EOS
+      Resources:
+        Bucket:
+          Type: AWS::S3::Bucket
+    EOS
+    assert_equal "test.template: formatted OK", shell_output("#{bin}/cfn-format -v test.template").strip
+  end
+end
