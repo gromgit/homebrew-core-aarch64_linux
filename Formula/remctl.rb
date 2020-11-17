96bfa86d5f9db62eb6fc0bd6210f09ad68993bb6
Jonathan Chang <me@jonathanchang.org>
2020-11-17 05:31:33 +0000

    remctl: update 3.16 bottle.

--- Formula/remctl.rb
+++ Formula/remctl.rb
@@ -11,6 +11,7 @@ class Remctl < Formula
 
   bottle do
     cellar :any
+    sha256 "d2d2786edce9ddc1b4313db5006026c91770628a90f9f012030363ddb7048902" => :big_sur
     sha256 "0c94a53cbd26ed882a5b4c8f973562120dd1cf0a6f76457d392cc5fe59d5ee89" => :catalina
     sha256 "3472db7e7bd12cb03c12b6dc2db8168439a24eef73bde7fbec775a41db628042" => :mojave
     sha256 "777fb3b5f47e4da6caeacba3c4959a38ebf7113a89555aebe74d41854c70104a" => :high_sierra
