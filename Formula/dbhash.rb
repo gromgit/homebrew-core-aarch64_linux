c61fe1d63d21183843ae3145101be8b8b280d8e5
Jonathan Chang <me@jonathanchang.org>
2020-11-17 08:26:39 +0000

    dbhash: update 3.33.0 bottle.

--- Formula/dbhash.rb
+++ Formula/dbhash.rb
@@ -8,6 +8,7 @@ class Dbhash < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "137c6454f067e88e0257bf8d29bc64d8efa201dcb1cb5770646c1a1569a0e34a" => :big_sur
     sha256 "286b4a753cd443251a2b9b27d2ff813faf384a3c78940c2bded450d391ba7a66" => :catalina
     sha256 "f9f1001a4e2a887070762636737a144d13e53f00dfa39278c9535cc448b6f91c" => :mojave
     sha256 "7e973140b25ac83fb8d8dd77a2b7b4ee5cf13f4b10d1a27b92c1ad4180cf71e8" => :high_sierra
