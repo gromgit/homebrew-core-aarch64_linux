253ec55ef50ee3ed063525e09601552b5a7efb44
Jonathan Chang <me@jonathanchang.org>
2020-11-17 00:37:24 +0000

    algernon: update 1.12.8_1 bottle.

--- Formula/algernon.rb
+++ Formula/algernon.rb
@@ -15,6 +15,7 @@ class Algernon < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "f37e15b90dc31fdcecdeba0b30e97387488aa055a6abf98fb8d81cb6430401ac" => :big_sur
     sha256 "86789f00d6b8d3469c5d3df5087149a72d1673cb7f2b7e31360672a6193e4eb2" => :catalina
     sha256 "71d88d3e20c865c299b4083ce9143b8f5594be8be2d5e6a511d6d7187b8465c3" => :mojave
     sha256 "b78c41a051aac0d969b8d7b49507a11e3e1b69990efe22aa88f9d5a5d544eb46" => :high_sierra
