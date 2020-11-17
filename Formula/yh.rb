4c8435db3137ce5defc57574f39014bc9714c451
Jonathan Chang <me@jonathanchang.org>
2020-11-17 03:57:22 +0000

    yh: update 0.4.0 bottle.

--- Formula/yh.rb
+++ Formula/yh.rb
@@ -8,6 +8,7 @@ class Yh < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e8165967a843d90d96ed71a41b303b7b434cf855e1e456d428c694aeceeee737" => :big_sur
     sha256 "1a2425d399a63df18758dfabf9d50da2559fb489c32bfb4462d7437f64fc0817" => :catalina
     sha256 "69f1ab9c740906f04924c780cb512ea26fa0c51bdf66be85c71c4cbaa9dc6ca1" => :mojave
     sha256 "184eb9a41954f7a3d11f3065dfab42085a724c617ec635681e05784eeebe6329" => :high_sierra
