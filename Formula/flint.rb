e0e22ac1bad87bd7222ba7d3deae6d01c42c1c72
Jonathan Chang <me@jonathanchang.org>
2020-11-17 02:54:24 +0000

    flint: update 2.6.3_1 bottle.

--- Formula/flint.rb
+++ Formula/flint.rb
@@ -8,6 +8,7 @@ class Flint < Formula
   head "https://github.com/wbhart/flint2.git", branch: "trunk"
 
   bottle do
+    sha256 "7ef5f3fb0cb495fee718960ef8a9386acf7d07bb7f39093fa09f48091be39629" => :big_sur
     sha256 "8f03bf2f3e4054c196b5d7a593b5d3e18b85a0482a9b919ec3939db9a0712ef1" => :catalina
     sha256 "af9627560e4128bf27fd088400890595d886dcc52cb6f752ecfe44738ab893bc" => :mojave
     sha256 "072f386f8b52bf213fb7782c562ee92b179ceb304b83e18a208e762fcbc8dd1f" => :high_sierra
