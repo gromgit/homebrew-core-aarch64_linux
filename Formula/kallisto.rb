1d2221523182fe8a0cffae84fdf7f5df355007b2
Jonathan Chang <me@jonathanchang.org>
2020-11-17 03:05:06 +0000

    kallisto: update 0.46.2 bottle.

--- Formula/kallisto.rb
+++ Formula/kallisto.rb
@@ -7,6 +7,7 @@ class Kallisto < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2c004978635382869062996fed516c2edd83597d3e2221984759fc32955854e5" => :big_sur
     sha256 "7ef1f941663072b0a57597992acf8203ba3664129f305cb8626c0c346e51bf0c" => :catalina
     sha256 "b2e59c1cc0fc1b07d02bab1cbc1533bcca1edf4bc0b81791d5ac597a7b84cce0" => :mojave
     sha256 "8491424ec8d4f8e170315e13c5f3bb92895b608c9c7108f260459e06bbbf73f9" => :high_sierra
