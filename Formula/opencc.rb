5abac4d5ea03d3b1e1eee0cc1cdfb2cddd5f2832
Jonathan Chang <me@jonathanchang.org>
2020-11-17 12:16:05 +0000

    opencc: update 1.1.1 bottle.

--- Formula/opencc.rb
+++ Formula/opencc.rb
@@ -6,6 +6,7 @@ class Opencc < Formula
   license "Apache-2.0"
 
   bottle do
+    sha256 "11b266b2cbae3e9ec7912dbb0b4784aae0d2868da37082414e6206e104fc9926" => :big_sur
     sha256 "dddf951ae0363e1e92c34d3d0087233d8d07053682557fd1bbed457aa98c677f" => :catalina
     sha256 "0503b49e5fcec9652ea10a3ab14d182babf48aae43644c68b243b94c2a781182" => :mojave
     sha256 "d48b56dec1d0f6f0fe0eb2690f8ac152e8dc13971c40649fe5e6ba810efdec17" => :high_sierra
