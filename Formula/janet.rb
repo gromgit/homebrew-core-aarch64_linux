ff779e2d71b482b7e76c60a192fea464816cd15e
Jonathan Chang <me@jonathanchang.org>
2020-11-17 08:52:47 +0000

    janet: update 1.12.2 bottle.

--- Formula/janet.rb
+++ Formula/janet.rb
@@ -8,6 +8,7 @@ class Janet < Formula
 
   bottle do
     cellar :any
+    sha256 "ce99c0aa40fd84c95a6e166fc89b2527214352f2803497139a04ab1eed620b58" => :big_sur
     sha256 "1181c2738d9c4479f7d46f5d9518955369b19fad9814eabb3b1481ee0d2055a0" => :catalina
     sha256 "594711312d351ef2f2c97c9fa1d1212e23f20cb36b4f0b685ab2ba35a9a6fb83" => :mojave
     sha256 "53f6d8aba6d1b8a6804c497a34370172b4801cfa1bb76c403a3a9a5894e820cb" => :high_sierra
