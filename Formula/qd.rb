3e74c6b2be8ce98792f4d21bf36150c34be916b2
Jonathan Chang <me@jonathanchang.org>
2020-11-17 00:15:00 +0000

    qd: update 2.3.22 bottle.

--- Formula/qd.rb
+++ Formula/qd.rb
@@ -12,6 +12,7 @@ class Qd < Formula
   bottle do
     cellar :any
     rebuild 2
+    sha256 "380c30f837eed2027f1f1a353cfca4b5f71f551e504c26b2ab635cba4918681b" => :big_sur
     sha256 "9700e6163692f31c736ddd74f535305fef730e021c4ca9f85b5860926397e330" => :catalina
     sha256 "53e4efc8ab2d1c18b1c4198bed031eb1b97b4431b1c0a4e8e4195c9b01659098" => :mojave
     sha256 "0ff67d07426a90d2897f0f69da0bd91bedb8a40ea52c0201c80225dd6c22510d" => :high_sierra
