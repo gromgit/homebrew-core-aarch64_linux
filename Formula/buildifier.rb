906f8cdca6f4aafebf3bdae743f04c7ce37c3766
Jonathan Chang <me@jonathanchang.org>
2020-11-17 09:50:37 +0000

    buildifier: update 3.5.0 bottle.

--- Formula/buildifier.rb
+++ Formula/buildifier.rb
@@ -8,6 +8,7 @@ class Buildifier < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "21da6d6c0020f91ea197b48dbc5a108527d9aa2e41b47406d79403d49fda164e" => :big_sur
     sha256 "154877a4bfb01a9fd0498a42015e75ca3df72d7c2b92c47254866c8b9832ceb4" => :catalina
     sha256 "36f2f0c2946d5e3a79f9287285cf335f14b1f181190907dfc29b12231764bc49" => :mojave
     sha256 "5b54427b4bd78b0b3dccfd66b8003a52aab3b4d1a7683586b66d6a6c835c0b4b" => :high_sierra
