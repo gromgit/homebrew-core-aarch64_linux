07e9806781dfdcf860e8f9e7852b554e450aa9cb
Jonathan Chang <me@jonathanchang.org>
2020-11-17 06:35:06 +0000

    gotop: update 4.0.1 bottle.

--- Formula/gotop.rb
+++ Formula/gotop.rb
@@ -7,6 +7,7 @@ class Gotop < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "398d3e2e32c2d59d08a36086f625c5fe5e3cf94c518e6a5dc1d3a90ea9edc49f" => :big_sur
     sha256 "5a61acb05457b28a32d342fc75376785873ec8f9fd73209f079550051851ea54" => :catalina
     sha256 "c758b1001a0de9af2572871c05cd3ec78341e31e2e870a9a147cea493f85baa6" => :mojave
     sha256 "194a6ee2f9ab9922b23a91d8868ac7802f611f238d56d10c79da4c9263a9afa4" => :high_sierra
