67e8f0c9d29b9686b482e7959e235869851b9290
Jonathan Chang <me@jonathanchang.org>
2020-11-17 01:05:01 +0000

    clockywock: update 0.3.1a bottle.

--- Formula/clockywock.rb
+++ Formula/clockywock.rb
@@ -6,6 +6,7 @@ class Clockywock < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6816f78abb433f6680474028cf20d219d8b6a51dfe7a185e90f12e8092a9ee89" => :big_sur
     sha256 "5bc4dcd5f3b6d995d6245d3f67a55fb2b5bb6d604e9ad214bc687f4ca8d40bd8" => :catalina
     sha256 "3b3b0faab6694a2572ad18b332b0711d43a7bf73715d0826df0adeacef0c64ed" => :mojave
     sha256 "4d1b976443480421f6b666121b31b350d7881b26832a65f13866a81fda61aa9e" => :high_sierra
