c402c34279508923824db456d15920e5a0673b19
Jonathan Chang <me@jonathanchang.org>
2020-11-17 04:30:33 +0000

    openfortivpn: update 1.15.0 bottle.

--- Formula/openfortivpn.rb
+++ Formula/openfortivpn.rb
@@ -6,6 +6,7 @@ class Openfortivpn < Formula
   license "GPL-3.0-or-later"
 
   bottle do
+    sha256 "c7809f586984e5a763db26ca3b783a5a4d3c39ada3a9a49b3578a5cfa05483ad" => :big_sur
     sha256 "e39f7fb2a5e3ab15632ae1568b4c969dec71fab19b99e50da5b061d7599c8906" => :catalina
     sha256 "bc45f74d11de64c9db45bed0c9c2b7b8b0c5b9467c6d4354ba17fa89467c0a2e" => :mojave
     sha256 "58d6f22f8d8f3c3e81abe98ea282d8f0b86139a54c2367a6d91d7c7a7862ceee" => :high_sierra
