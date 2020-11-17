f1e080a2783cb78db3e9a14736a4b0b5cb6e1b1d
Jonathan Chang <me@jonathanchang.org>
2020-11-17 07:20:54 +0000

    yosys: update 0.9_3 bottle.

--- Formula/yosys.rb
+++ Formula/yosys.rb
@@ -8,6 +8,7 @@ class Yosys < Formula
   head "https://github.com/YosysHQ/yosys.git"
 
   bottle do
+    sha256 "6ecd94923b663972312bf1c6e50ddfef817ffb7257118e6d1f0bd6836d3057a5" => :big_sur
     sha256 "30136c3fe55e45d36aa1587a48bc69030930563b2fb0f386ce122d79a4dbba87" => :catalina
     sha256 "6298e8bfeff2fa1f4de993642b43afeacb6c98a3f262c256d495339ee141dff4" => :mojave
     sha256 "a8807693a57f363e1a2d95034feffa3ab14c3645910f154d128990ae0484439e" => :high_sierra
