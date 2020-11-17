8dbed4f43a9b5ccc998ba86651ba08d415e82e7b
Jonathan Chang <me@jonathanchang.org>
2020-11-17 07:38:51 +0000

    gpredict: update 2.2.1_2 bottle.

--- Formula/gpredict.rb
+++ Formula/gpredict.rb
@@ -12,6 +12,7 @@ class Gpredict < Formula
   end
 
   bottle do
+    sha256 "eccf4afd811d590ed5c930840933905bd5b1ea9bdf42e32e52cf4926d0c1eb05" => :big_sur
     sha256 "99fff9473dcc5eaa0c58cf0b2bf04f4240e1598aada45565e4dbbf050d2ac7dc" => :catalina
     sha256 "952941a2ecdb5f75805888dfd020acce48c4f1b29a9c2e3ec8742d35fcd9c829" => :mojave
     sha256 "189249444c490bc7984506a3d041de1d057fff671ff774871f549f6b32efa042" => :high_sierra
