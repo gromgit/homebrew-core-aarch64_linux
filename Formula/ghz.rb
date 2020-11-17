246f015e1905ed6df4166c7ce638140d1850d960
Jonathan Chang <me@jonathanchang.org>
2020-11-17 02:13:28 +0000

    ghz: update 0.63.0 bottle.

--- Formula/ghz.rb
+++ Formula/ghz.rb
@@ -7,6 +7,7 @@ class Ghz < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6f6d413f89022d24fbe18da2457fa8ea8a2a6eabdf2b01cc0e8b724f13f557d1" => :big_sur
     sha256 "48be94edb841c06b25608492d8d5677cd3947c2f6f55e0bb7bf166da0131b7c3" => :catalina
     sha256 "9ed0e1e1c076b658f80e556da6d9bb8e471099fbb7760f8374a59adb1cf05086" => :mojave
     sha256 "532f9c7cb224ec6e6b4474f675d98a765c011be2dbf3def13ca0e79e4dc59f3b" => :high_sierra
