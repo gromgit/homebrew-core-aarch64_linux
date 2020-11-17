e544b6e16929d264854ab73264017166bdc02579
Jonathan Chang <me@jonathanchang.org>
2020-11-17 00:18:58 +0000

    libxfont: update 1.5.4 bottle.

--- Formula/libxfont.rb
+++ Formula/libxfont.rb
@@ -7,6 +7,7 @@ class Libxfont < Formula
 
   bottle do
     cellar :any
+    sha256 "816829490c6b978eaaa6b068ef42e89f1196be5d186d5c407b670f49dfa7f66b" => :big_sur
     sha256 "0321fea5b7329575b6d4b3ed762d741309c329c74df6a9ae2693667828e9a1da" => :catalina
     sha256 "68cfb860815eedac8d96bb1853a64a12c3cc77bcc0e99ffbd693666b2bfb9119" => :mojave
     sha256 "54fe9ff4143205d5d14a416f276193c4f9f5dc83898a057823462ac78c8de891" => :high_sierra
