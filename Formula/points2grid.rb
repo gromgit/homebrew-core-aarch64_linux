9e137498c46e3187b389feeb65bd951939ed5be0
Jonathan Chang <me@jonathanchang.org>
2020-11-17 00:05:19 +0000

    points2grid: update 1.3.1_9 bottle.

--- Formula/points2grid.rb
+++ Formula/points2grid.rb
@@ -8,6 +8,7 @@ class Points2grid < Formula
 
   bottle do
     cellar :any
+    sha256 "28e47b4fd671bd71072983e55340abe1a6552f5de9de40365656073428378e8d" => :big_sur
     sha256 "29ad8732fb7848d6252f94ba0f1ef5500d93afc762bf7dbe83bd6ae7bcbf631d" => :catalina
     sha256 "f17d1ab719822ecf2db6ff06930891fe0dc2fabbeb9c7c18f7d65b0ae76dfc34" => :mojave
     sha256 "bfc619bd4f796b07e2fc20092bd76701414dcf634311c9d121d54a72907f566f" => :high_sierra
