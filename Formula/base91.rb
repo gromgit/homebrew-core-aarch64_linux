b980564c7905b6be2ea8d167c3c5338a9f59e84a
Jonathan Chang <me@jonathanchang.org>
2020-11-17 06:23:57 +0000

    base91: update 0.6.0 bottle.

--- Formula/base91.rb
+++ Formula/base91.rb
@@ -11,6 +11,7 @@ class Base91 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "039bcf75c09fb75a7472e8f92ef349f2908073b4ac76c1c4573d1a393e248229" => :big_sur
     sha256 "239a9f51f67e7b1de09d10c838dffd2c34ce0f6f58641269e2278d6478b36542" => :catalina
     sha256 "fca64b5013c75658646a7d758365a624aa5f3a89488573222f2bbb867b04cc49" => :mojave
     sha256 "3b9c972390a56bc2ea0be9943558018cc271802369b5b36ff0fa10391aaf1f57" => :high_sierra
