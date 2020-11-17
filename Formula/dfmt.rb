cff4e8a839c8d7a79a50cbb006b0e9c0365d3b99
Jonathan Chang <me@jonathanchang.org>
2020-11-17 07:08:46 +0000

    dfmt: update 0.13.2 bottle.

--- Formula/dfmt.rb
+++ Formula/dfmt.rb
@@ -9,6 +9,7 @@ class Dfmt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e4f4475c66dccc8466a59a829e64ca18d5f28f25c882cdb4851b7f60dbac8bbe" => :big_sur
     sha256 "316ce1c0504cf87bd8de9bb045fd855e887c75132993bcbde37d1bf54c9f00d5" => :catalina
     sha256 "0644eb26171306e6e76606c373cda1f641c8afcc91f6c8852521e89a49b69864" => :mojave
     sha256 "9edcdbe4f7bf57c0c79ee7bac6f29c4ca1882bf41ec86564e1f3abb35e48c44d" => :high_sierra
