b3c75e1f9110dace1e3f5beab663a7aa4bee15d8
Jonathan Chang <me@jonathanchang.org>
2020-11-17 02:06:52 +0000

    lbdb: update 0.49 bottle.

--- Formula/lbdb.rb
+++ Formula/lbdb.rb
@@ -12,6 +12,7 @@ class Lbdb < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "169649d76f8f4321f9f682b54aad89d918945463e3d0b855385d017d6671fbd4" => :big_sur
     sha256 "9c79b0bbc8d13e16259bce83501a2d3f285f3ca2c6675f49cdb36a4ef4366fa1" => :catalina
     sha256 "71c3c794994b1ec594bdbbef70848c0e5bb43ab1f19e1edff7e0d17e9a2f9e98" => :mojave
     sha256 "03f4287b844059e3bc9787c97bc22b903a57067add3c3fe2913d2ca53537d34f" => :high_sierra
