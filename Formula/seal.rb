421655f7702a69441f3f0e2ffe034e772f801c8c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-18 09:14:41 +0000

    seal: update 3.6.0 bottle.

--- Formula/seal.rb
+++ Formula/seal.rb
@@ -7,10 +7,9 @@ class Seal < Formula
 
   bottle do
     cellar :any
-    sha256 "9bd89b5be52e81a6c4c0ff588d0d2cbc17075ff44283b37d08ba49952605fbb6" => :big_sur
-    sha256 "e13c03ab99faa6ad88a86622e3646eac5c3db1c6b57542bad702612f714eedfb" => :catalina
-    sha256 "fd36a3416c5bc506c8a56c1e5c3267aec2f28e4720f8cf31db31b7ac2f9cab63" => :mojave
-    sha256 "576df58ef112717404d4c9ced08289fd3f58d7eb275e01175f16552673a26ac8" => :high_sierra
+    sha256 "ce0fb24c40e48c23d4dc49f4b5ee6224e29569764aaa4294cf43d57cd1573e6d" => :big_sur
+    sha256 "2ca03d844ddb12e68af1a8ad95114915620bd6106225b6937a952ab5eb369694" => :catalina
+    sha256 "fd7e10b8cadce6c8eb1ab5cdba2de6165dffeef247d2361296c6e0f03dcafea4" => :mojave
   end
 
   depends_on "cmake" => [:build, :test]
