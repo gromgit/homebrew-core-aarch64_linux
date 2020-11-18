bc5afc4cad9f1546b2fe1293eec33b5799123f7b
miccal <miccal.matthews@gmail.com>
2020-11-18 12:45:27 +0800

    imagemagick@6 6.9.11-39
    Closes #65095.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/imagemagick@6.rb
+++ Formula/imagemagick@6.rb
@@ -4,9 +4,9 @@ class ImagemagickAT6 < Formula
   # Please always keep the Homebrew mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://dl.bintray.com/homebrew/mirror/imagemagick%406-6.9.11-38.tar.xz"
-  mirror "https://www.imagemagick.org/download/releases/ImageMagick-6.9.11-38.tar.xz"
-  sha256 "f61121fceebd17a2bf524aa91ad1d2963b31f642822581bbfccb272ce0182949"
+  url "https://dl.bintray.com/homebrew/mirror/imagemagick%406-6.9.11-39.tar.xz"
+  mirror "https://www.imagemagick.org/download/releases/ImageMagick-6.9.11-39.tar.xz"
+  sha256 "ba83e59e350a054d4adb0e5cb04d291d39f61ea3f46fa19c0d76c1752c6c9841"
   license "ImageMagick"
   head "https://github.com/imagemagick/imagemagick6.git"
 
