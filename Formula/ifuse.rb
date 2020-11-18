e169b309d98147f1666989c59d232c81c8acd622
Issy Long <me@issyl0.co.uk>
2020-11-18 23:12:33 +0000

    ifuse: fix `deprecate!` or `disable!` stanza ordering

--- Formula/ifuse.rb
+++ Formula/ifuse.rb
@@ -6,14 +6,14 @@ class Ifuse < Formula
   license "LGPL-2.1"
   head "https://cgit.sukimashita.com/ifuse.git"
 
-  deprecate! because: "requires FUSE"
-
   bottle do
     cellar :any
     sha256 "cdce9fc5dbaf44641743b4a77434d340ae11cb8ed98f17b1a86a5653d2b6e1a2" => :catalina
     sha256 "e14e4f8e0f73324dc662b47f091261f682eddc73961e3d71a07bfeb62826a1f8" => :mojave
     sha256 "ff5577f28749cf18671eecd953e96f0c52a06dccf827dcf08e2d64f894dfdd5e" => :high_sierra
   end
+
+  deprecate! because: "requires FUSE"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
