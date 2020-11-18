46db63e89fbbb2107bb5c033123292ecbc9f74f3
Jonathan Chang <me@jonathanchang.org>
2020-11-18 04:14:00 +0000

    cryptol: update 2.9.1 bottle.

--- Formula/cryptol.rb
+++ Formula/cryptol.rb
@@ -13,6 +13,7 @@ class Cryptol < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "92e65fe366183e5231c656e5b2bc3585bf8a6fd2e78c2f628ad9a6fa8c13f3c9" => :big_sur
     sha256 "62143f306e77077070a3d304d596a9803411a12384a421c8f8e652fe0912eadb" => :catalina
     sha256 "8a9d5d3b2317174758ff1b0dc0ec1f82e83036b6afad14608d17527fb03bbb13" => :mojave
     sha256 "2f1d272e3306ea64488a4518ad4366680b07c0e0af05f9939db94ad059c08601" => :high_sierra
