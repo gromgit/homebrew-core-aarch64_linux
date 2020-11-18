3f2e03c4a5a2531dece670cbfbcd87fd45a527f5
Issy Long <me@issyl0.co.uk>
2020-11-18 23:12:49 +0000

    s3fs: fix `deprecate!` or `disable!` stanza ordering

--- Formula/s3fs.rb
+++ Formula/s3fs.rb
@@ -6,14 +6,14 @@ class S3fs < Formula
   license "GPL-2.0-or-later"
   head "https://github.com/s3fs-fuse/s3fs-fuse.git"
 
-  deprecate! because: "requires FUSE"
-
   bottle do
     cellar :any
     sha256 "5183ab606057fbe8e46a737b25c1ad4e82dd67389f48827d7bfd567c67cf8417" => :catalina
     sha256 "d691bdeb4abd443bc1f1f3de46c286a97829f95ba3d47b026e535a6688085d07" => :mojave
     sha256 "f475d03b68102dd400a22de99b9ddc044653f6658e2cb84349adf507ffbddcad" => :high_sierra
   end
+
+  deprecate! because: "requires FUSE"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
