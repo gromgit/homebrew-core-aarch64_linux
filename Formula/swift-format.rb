4bd78e6702ee5f70cb8ac8e1972f900dae8939db
Lucas Stomberg <lstomber@epic.com>
2020-11-18 12:55:39 -0600

    swift-format 0.50300.0
    swift-format 5.3
    
    Closes #65134.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/swift-format.rb
+++ Formula/swift-format.rb
@@ -2,8 +2,8 @@ class SwiftFormat < Formula
   desc "Formatting technology for Swift source code"
   homepage "https://github.com/apple/swift-format"
   url "https://github.com/apple/swift-format.git",
-    tag:      "0.50200.1",
-    revision: "f22aade8a6ee061b4a7041601ededd8ad7bc2122"
+    tag:      "0.50300.0",
+    revision: "12089179aa1668a2478b2b2111d98fa37f3531e3"
   license "Apache-2.0"
   version_scheme 1
   head "https://github.com/apple/swift-format.git", branch: "main"
@@ -13,7 +13,14 @@ class SwiftFormat < Formula
     sha256 "d8f72c33efc125e2904e1bec2c8942cca75d75cf81dcab7fcf08ba124af16170" => :catalina
   end
 
-  depends_on xcode: ["11.4", :build]
+  # The bottles are built on systems with the CLT installed, and do not work
+  # out of the box on Xcode-only systems due to an incorrect sysroot.
+  pour_bottle? do
+    reason "The bottle needs the Xcode CLT to be installed."
+    satisfy { MacOS::CLT.installed? }
+  end
+
+  depends_on xcode: ["12.0", :build]
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
