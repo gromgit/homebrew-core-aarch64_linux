cd0bf53d2642216f430da28d9180687baa641193
Issy Long <me@issyl0.co.uk>
2020-11-18 23:12:52 +0000

    securefs: fix `deprecate!` or `disable!` stanza ordering

--- Formula/securefs.rb
+++ Formula/securefs.rb
@@ -7,14 +7,14 @@ class Securefs < Formula
   license "MIT"
   head "https://github.com/netheril96/securefs.git"
 
-  deprecate! because: "requires FUSE"
-
   bottle do
     cellar :any
     sha256 "8a8c7dd74f9b3082b2b128cc058714a27206d910273e4148959a25b7d30c51b5" => :catalina
     sha256 "9bee23af87518f68df56b862ad0ef88f842ebe65f8feff02e90bf5ab8e91522e" => :mojave
     sha256 "632496d8e9ed9fe91d18e9a2c9fef49c920dc091e10108246f8ab2056f75ea38" => :high_sierra
   end
+
+  deprecate! because: "requires FUSE"
 
   depends_on "cmake" => :build
   depends_on :osxfuse
