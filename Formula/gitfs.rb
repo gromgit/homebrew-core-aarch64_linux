5ba9df907c507b9086dd255dc6e7841fd68ec01a
Issy Long <me@issyl0.co.uk>
2020-11-18 23:12:29 +0000

    gitfs: fix `deprecate!` or `disable!` stanza ordering

--- Formula/gitfs.rb
+++ Formula/gitfs.rb
@@ -9,14 +9,14 @@ class Gitfs < Formula
   revision 4
   head "https://github.com/presslabs/gitfs.git"
 
-  deprecate! because: "requires FUSE"
-
   bottle do
     cellar :any
     sha256 "c8c83c94da3b5f1dc480eec0ede90bf678eee59a97bb54a64cf94555d9c57752" => :catalina
     sha256 "189008579b9d28a9084536f62101051648b64a78cd6faf780b3f40041becc188" => :mojave
     sha256 "218c5f19bcecb33e4f18c19cf0f56ce6d9628d4cfad9f095fbb1071af3cd79c2" => :high_sierra
   end
+
+  deprecate! because: "requires FUSE"
 
   depends_on "libgit2"
   depends_on :osxfuse
