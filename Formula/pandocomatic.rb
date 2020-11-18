ca28d2a967fa98410615f311b8792b9c2a54cbbf
Jonathan Chang <me@jonathanchang.org>
2020-11-18 04:54:42 +0000

    pandocomatic: update 0.2.7.4 bottle.

--- Formula/pandocomatic.rb
+++ Formula/pandocomatic.rb
@@ -7,6 +7,7 @@ class Pandocomatic < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "42c46256c89f62097987aace0b1666d21dd8eadbffb24cdfa21c6ba54cd576f9" => :big_sur
     sha256 "462b6fb04be99bd19985476d36d29b26625a8ada343ec41f8373c0f7fcf11033" => :catalina
     sha256 "f4a6df084b43ebe5a123dbeeef27881d737aec895df518050eb948a6d7712b1a" => :mojave
     sha256 "8f6ca37732470b38636fd37d6d5a7c75b15e6642fdd8a3fdbe2e57b196ca2fcf" => :high_sierra
