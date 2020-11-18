bf8230044d9d1c88dfaf858ff8c17f820d006784
Issy Long <me@issyl0.co.uk>
2020-11-18 23:12:51 +0000

    scala@2.11: fix `deprecate!` or `disable!` stanza ordering

--- Formula/scala@2.11.rb
+++ Formula/scala@2.11.rb
@@ -6,11 +6,11 @@ class ScalaAT211 < Formula
   mirror "https://www.scala-lang.org/files/archive/scala-2.11.12.tgz"
   sha256 "b11d7d33699ca4f60bc3b2b6858fd953e3de2b8522c943f4cda4b674316196a8"
   revision 1
-  deprecate! date: "2017-11-09", because: :unsupported
-
   bottle :unneeded
 
   keg_only :versioned_formula
+
+  deprecate! date: "2017-11-09", because: :unsupported
 
   depends_on "openjdk@8"
 
