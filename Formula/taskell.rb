610e6337d70f689b58eaa79cc91079ff1579fef2
Jonathan Chang <me@jonathanchang.org>
2020-11-18 04:20:57 +0000

    taskell: update 1.10.1 bottle.

--- Formula/taskell.rb
+++ Formula/taskell.rb
@@ -7,6 +7,7 @@ class Taskell < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4a093b7024da7e64fa5f4cdb1be1ebca5006fda0f0513412dcdd4df2505edf3c" => :big_sur
     sha256 "d224f2ea895ced6b1a8cc761e3d3c79478a9e210df68f3cbcdb65debacd8a246" => :catalina
     sha256 "6733a5756ba481343d132064e22a6a6447eef1e374ad4a9d4915d8951fe5ccb0" => :mojave
     sha256 "d7fbd709e7713e2f08a1d75a64c2527dfa87f8d935a872054119bedc4338d652" => :high_sierra
