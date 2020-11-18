a50221d5c63f19087087eead392dbbcc02c2e705
chenrui <chenrui333@gmail.com>
2020-11-18 14:24:25 +0000

    faas-cli 0.12.16
    Closes #65124.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/faas-cli.rb
+++ Formula/faas-cli.rb
@@ -2,8 +2,8 @@ class FaasCli < Formula
   desc "CLI for templating and/or deploying FaaS functions"
   homepage "https://www.openfaas.com/"
   url "https://github.com/openfaas/faas-cli.git",
-      tag:      "0.12.14",
-      revision: "c12d57c39ac4cc6eef3c9bba2fb45113d882432f"
+      tag:      "0.12.16",
+      revision: "c7834361318dece8fec443a2a8d085a784a7c9dd"
   license "MIT"
 
   livecheck do
