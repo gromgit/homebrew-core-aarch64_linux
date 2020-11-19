99dfb856061b620c9d1fc2cedbc21eaeb7e79118
FX Coudert <fxcoudert@gmail.com>
2020-11-19 14:49:05 +0000

    libxspf: update 1.2.0 bottle.

--- Formula/libxspf.rb
+++ Formula/libxspf.rb
@@ -7,6 +7,7 @@ class Libxspf < Formula
   bottle do
     cellar :any
     rebuild 2
+    sha256 "446e41e3c0e23961a20038101bd279e90e552600eaf9363006f82ed9caf5d199" => :big_sur
     sha256 "2fac98e985ea3242cbce81bbb9e5e37fba44e47204b4a53b868e01199ece71eb" => :catalina
     sha256 "76c09682c27449a52128a0aa29de091b907327f71cf320b30be5eb3b37442bcb" => :mojave
     sha256 "ae1e960341fb08826fb30de46f3b1c075c22d9e655acb9e44067327c487328a7" => :high_sierra
