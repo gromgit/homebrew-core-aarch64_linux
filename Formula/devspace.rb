1aa26dc2866eb16a721aa20b322f133a2d7cc1e2
chenrui <chenrui333@gmail.com>
2020-11-19 12:34:53 +0000

    devspace 5.3.0
    Closes #65184.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/devspace.rb
+++ Formula/devspace.rb
@@ -2,8 +2,8 @@ class Devspace < Formula
   desc "CLI helps develop/deploy/debug apps with Docker and k8s"
   homepage "https://devspace.cloud/docs"
   url "https://github.com/devspace-cloud/devspace.git",
-    tag:      "v5.2.2",
-    revision: "2b69cf7eee778c892129a7b8233ab7ecd6125690"
+    tag:      "v5.3.0",
+    revision: "b61ab8e6cce42fe8ed4d6308102284ccb1e9e9e4"
   license "Apache-2.0"
 
   livecheck do
