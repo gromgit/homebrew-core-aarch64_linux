b91b321e3ecfa250439ce144d303631b05ad5647
FX Coudert <fxcoudert@gmail.com>
2020-11-19 10:04:09 +0000

    react-native-cli: update 2.0.1 bottle.

--- Formula/react-native-cli.rb
+++ Formula/react-native-cli.rb
@@ -13,6 +13,7 @@ class ReactNativeCli < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "7a02c0e2569f9906069d980a6b0d8c0ca3174407a2588c00e8d428ff9c29aa32" => :big_sur
     sha256 "45feb7f98d12b2ee28b6e5f658e070f60a43571d4d8fab679cb513ac957ca7bd" => :catalina
     sha256 "8e6e253c7801cc276f89f2988245866080c1409602c5903dbb2984b1a645746b" => :mojave
     sha256 "387e6f8c0e9f20b4ae2007185d394ff73cc3392085a6a05045b669512780c55e" => :high_sierra
