e2cfcfab875362339332a8935c4994e3928307f2
FX Coudert <fxcoudert@gmail.com>
2020-11-19 15:05:02 +0000

    orocos-kdl: update 1.4.0 bottle.

--- Formula/orocos-kdl.rb
+++ Formula/orocos-kdl.rb
@@ -7,6 +7,7 @@ class OrocosKdl < Formula
 
   bottle do
     cellar :any
+    sha256 "11f44349bb30654acb195b84796e5b9492e5fa7539c1c8aababf98eaea239cf2" => :big_sur
     sha256 "e65054479e7b34fd559f72f4485a5e34b57b457325bb8487576309ddad7a26fa" => :catalina
     sha256 "3d88fc55d86c9d1194ed3896bf1524405997e601ae75acbd37b176f882f07868" => :mojave
     sha256 "32e9cd3e10a20c046a45122557dda364352619c59acffca07f8c858cdcff9765" => :high_sierra
