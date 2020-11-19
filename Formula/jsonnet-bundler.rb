ebfb429521160b26e5fa56695431b6cf0ebf2004
FX Coudert <fxcoudert@gmail.com>
2020-11-19 11:08:46 +0000

    jsonnet-bundler: update 0.4.0 bottle.

--- Formula/jsonnet-bundler.rb
+++ Formula/jsonnet-bundler.rb
@@ -8,6 +8,7 @@ class JsonnetBundler < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6f52c9470db16a9f06797f01b5af31e55336ad809bb28ed3eacb985a3aa08067" => :big_sur
     sha256 "1d90eef17450bddbe12e3bbc65d55d2b324a34f2cb36a028b32060e416e262c1" => :catalina
     sha256 "a42bfdfd2d149e8667ff0849de992e91160e78aaed6f72f2624f9a191c83fe75" => :mojave
     sha256 "e058cee6482accf6cc38bb4c404148464e4e071cecdb7191e1fbc6315e53f851" => :high_sierra
