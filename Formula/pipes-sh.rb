4856b5ce48efe5f7679212b235931159d72bfa07
FX Coudert <fxcoudert@gmail.com>
2020-11-19 11:18:17 +0000

    pipes-sh: update 1.3.0 bottle.

--- Formula/pipes-sh.rb
+++ Formula/pipes-sh.rb
@@ -13,6 +13,7 @@ class PipesSh < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "777bd274db0376138c72b6d89948ad3b19b622f069477a74cbc8b0ad9e320d72" => :big_sur
     sha256 "68d379998c00ca3662db8047c1c6e649491d65d851af264e04ce7cbdb7cbd2e2" => :catalina
     sha256 "b78492e9f13a815dc97200b33c4e228292a4679eb6d048c1094c64aa46504879" => :mojave
     sha256 "2793ad5fb825b4f805a4731c7028cbcb2ca5e9dd904133df0cce7481c5961322" => :high_sierra
