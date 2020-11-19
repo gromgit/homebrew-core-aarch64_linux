a13e977dadeee45a2ab28ad4d110bed130214ffe
FX Coudert <fxcoudert@gmail.com>
2020-11-19 11:28:49 +0000

    ical-buddy: update 1.10.1 bottle.

--- Formula/ical-buddy.rb
+++ Formula/ical-buddy.rb
@@ -8,6 +8,7 @@ class IcalBuddy < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "41f2928f8a9b5862e9864f5663e6f9cf179e8cfcd95305a41c7c610f7713446d" => :big_sur
     sha256 "4f621e8b12e2c2e5e7c9fdd97ee973b7d4b14ce58eb5a5f7a9db32243f0f99f1" => :catalina
     sha256 "4f621e8b12e2c2e5e7c9fdd97ee973b7d4b14ce58eb5a5f7a9db32243f0f99f1" => :mojave
     sha256 "4f621e8b12e2c2e5e7c9fdd97ee973b7d4b14ce58eb5a5f7a9db32243f0f99f1" => :high_sierra
