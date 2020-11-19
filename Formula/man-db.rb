1833b6b0eaa33d7eec8d43b814d85d1ae968a0d1
FX Coudert <fxcoudert@gmail.com>
2020-11-19 10:02:36 +0000

    man-db: update 2.9.3 bottle.

--- Formula/man-db.rb
+++ Formula/man-db.rb
@@ -11,6 +11,7 @@ class ManDb < Formula
   end
 
   bottle do
+    sha256 "6efbeca1e61f09d7bff5abd5f5e703e8537ddce12292f5bae69b71fba61e50b8" => :big_sur
     sha256 "1f203de0472712f459bee1b66dc93d4fa0c6fc190bdf467c018319add61a22ff" => :catalina
     sha256 "3eb1e906db4927d45eaa4cd26d28bd69cf62eaa94316f32c3f29266d0070a978" => :mojave
     sha256 "6827bc87f0ccf3b9e3f04adb8e0aba1d63497291704773bd9f6691d73cf4ee1d" => :high_sierra
