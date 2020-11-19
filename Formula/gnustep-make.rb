1c7f2a36f962957191a31118f4ce4f68d62bf9be
FX Coudert <fxcoudert@gmail.com>
2020-11-19 13:34:11 +0000

    gnustep-make: update 2.8.0 bottle.

--- Formula/gnustep-make.rb
+++ Formula/gnustep-make.rb
@@ -12,6 +12,7 @@ class GnustepMake < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5b8f96f0d47226cb8f00646acced66850754540352da1b6b682b9a07f80d4b90" => :big_sur
     sha256 "0112f9b5cc350a2e8efc7eff2ea1b3e0b13e62877ce02592eac34052b33de00f" => :catalina
     sha256 "4025644721c7902db42e5f63a1d8980056b809bdab7237289b92381e82492cca" => :mojave
     sha256 "ff2edab383602c3449f074284f992567b0d072a3a442be898be21da0d484d3c3" => :high_sierra
