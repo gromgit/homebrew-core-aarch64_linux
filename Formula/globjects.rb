81eadece0040635f8911889a39a990b2e5feb711
FX Coudert <fxcoudert@gmail.com>
2020-11-19 23:17:21 +0000

    globjects: update 1.1.0 bottle.

--- Formula/globjects.rb
+++ Formula/globjects.rb
@@ -8,6 +8,7 @@ class Globjects < Formula
 
   bottle do
     cellar :any
+    sha256 "7a47a09787bf617fa6616f2cd88567b12b5d5c5d0a29225688908ed8c8b2c88d" => :big_sur
     sha256 "8093cb17f6c1ba5ce345d3a89f0a2330cbdbb88100ad241be0dd8611a6ad52d9" => :catalina
     sha256 "9bbf36b86602a7b0c7bf66bb911e200e4f7b94f05c304afb261781edebf119ce" => :mojave
     sha256 "baae740c033bc384454f81c0abba246f935765ec7decf408777d318d60cbe565" => :high_sierra
