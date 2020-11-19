a74b770f3b4cf5ec31d3b184e41725ba8cfb834c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-19 23:14:08 +0000

    yaegi: update 0.9.7 bottle.

--- Formula/yaegi.rb
+++ Formula/yaegi.rb
@@ -8,10 +8,9 @@ class Yaegi < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3559b590a6988b57ed9ec8b2dd8c9683ac9951e41f4b2c1775bb52ec9ad7b250" => :big_sur
-    sha256 "19593b6f2c76751622909798e95b5bb1758afbae200c54f774831806cf93b39c" => :catalina
-    sha256 "eefbce92ba2c013928e1afd5679685156321308d7f417b61dee6c25da2125490" => :mojave
-    sha256 "f04b5d681038ef05499ad02f197e3c4c13f40c3eab51788dc16cedd8f3008f21" => :high_sierra
+    sha256 "856eff6b596e2d6b0fb8514591ed6cf77559e4e824d924b0066ce03f7e69f5bf" => :big_sur
+    sha256 "91d4e12650a681f34b5eb3a4908837a673fb1d87ac4fcf57bcf96ac399504711" => :catalina
+    sha256 "7ed28d27be8f7cfdc0a9fca2b2398cd2ddba2e1d35c1da30e4bffac633b8f029" => :mojave
   end
 
   depends_on "go" => :build
