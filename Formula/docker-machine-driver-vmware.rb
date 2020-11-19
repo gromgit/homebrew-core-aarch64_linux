82a5b74cc6cfe813180d7e6c65bdfb7edd4f454e
FX Coudert <fxcoudert@gmail.com>
2020-11-19 15:28:20 +0000

    docker-machine-driver-vmware: update 0.1.1 bottle.

--- Formula/docker-machine-driver-vmware.rb
+++ Formula/docker-machine-driver-vmware.rb
@@ -8,6 +8,7 @@ class DockerMachineDriverVmware < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "06e7a06267df68d8200ff678cd9aeb4b214e839f6e8a59dad4617c8cf1d23696" => :big_sur
     sha256 "d73c0be19fba7a7166c65202133b18c63367b5758d04ec19e23d3fd9406a8a7a" => :catalina
     sha256 "df81e5c14ec3961d53f6490a165a17b8ceda29beba747ee659b57d82a9468e26" => :mojave
     sha256 "e65553889741f8c077de12706314e9f95805d673b186d1d545617515d7ab4a03" => :high_sierra
