d91f739e7ffcc2fa43c0ae62f9f6f70a6e20b3c3
FX Coudert <fxcoudert@gmail.com>
2020-11-19 13:55:49 +0000

    pbc-sig: update 0.0.8 bottle.

--- Formula/pbc-sig.rb
+++ Formula/pbc-sig.rb
@@ -8,6 +8,7 @@ class PbcSig < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "9889f70fc5cf42a096c750b61008bf48a97bfece6179db5e7a631010749f1106" => :big_sur
     sha256 "47773fefdfeb3f7381046934974bbaf7f41a641c3d3f3af5802d07a7ea340ba6" => :catalina
     sha256 "134c203178bb93b406b4c5fb5aecf171db6473d558d0bf62cf9b1682b57448e9" => :mojave
     sha256 "79c31a3f1bcc2429648a2258974ccb1185cfe244d4fcbbfa2840c7393e7e058a" => :high_sierra
