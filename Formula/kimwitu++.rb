9ad299a3395d31d9f84e4bdd80c5c2d20f35672b
FX Coudert <fxcoudert@gmail.com>
2020-11-19 13:51:16 +0000

    kimwitu++: update 2.3.13 bottle.

--- Formula/kimwitu++.rb
+++ Formula/kimwitu++.rb
@@ -11,6 +11,7 @@ class Kimwituxx < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0dcd1be78b92b98d73dad285fbaaf507bdc23805835a51f56236ddd8b0eb73f5" => :big_sur
     sha256 "470e06521034cea8db6ad07e8aab45c5bfbe3969cd03891799348eb4e9279c90" => :catalina
     sha256 "067f8d375baa815645201a091c2f8325b469e59d9b2793e5a0dd83bfd9350aa2" => :mojave
     sha256 "26ba22bbcdbea896f4af405631bed60dfc198757b6f879765e8d85e373b122db" => :high_sierra
