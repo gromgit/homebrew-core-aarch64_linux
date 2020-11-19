b908bcf47ee75a5673d68567ec1a9a80015c1069
FX Coudert <fxcoudert@gmail.com>
2020-11-19 13:51:05 +0000

    server-go: update 1.1.59 bottle.

--- Formula/server-go.rb
+++ Formula/server-go.rb
@@ -8,6 +8,7 @@ class ServerGo < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0e168e5705de596db16a8b7e5bfbb07fc8d25d52ce7536badfa2ca06963e27fd" => :big_sur
     sha256 "a8f9eb48ac79a628aa71ca08caeb8577a85e47b81e81968475aa5b318c536b42" => :catalina
     sha256 "58996b6453268397e81337f7feb94b346297ba14cf1db24c23b0f05d0c19e420" => :mojave
     sha256 "8e49eb94a30c9f97b53a6201a165e7f07e1904f4ad9e933c4bc9e613e7c6ff75" => :high_sierra
