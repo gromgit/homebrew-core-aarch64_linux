ed4fe84197fc0918f86e69ec3a595f938bff8103
FX Coudert <fxcoudert@gmail.com>
2020-11-19 10:57:14 +0000

    solarus: update 1.6.4 bottle.

--- Formula/solarus.rb
+++ Formula/solarus.rb
@@ -8,6 +8,7 @@ class Solarus < Formula
 
   bottle do
     cellar :any
+    sha256 "80970673e8217a282440a21de4e4db8fa4435f9743e94c74f300fa55f2963014" => :big_sur
     sha256 "23c646e9a69c966e0f2930ac225661dab3d8b97fbb9e34c12cb49cdfe1e56d67" => :catalina
     sha256 "6d02a298994633961ed83feb34471cf743059aa9daf90d9b5153f2613337e8d2" => :mojave
     sha256 "dedfe91badd887dfdcf0e0d55b662fde86890c1f8e287dddd224b29b0339f4b9" => :high_sierra
