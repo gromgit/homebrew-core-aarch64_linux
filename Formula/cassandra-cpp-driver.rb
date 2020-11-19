31a7fc77e1c9fcaed7523a343bad4593f122312e
FX Coudert <fxcoudert@gmail.com>
2020-11-19 12:38:50 +0000

    cassandra-cpp-driver: update 2.15.3 bottle.

--- Formula/cassandra-cpp-driver.rb
+++ Formula/cassandra-cpp-driver.rb
@@ -8,6 +8,7 @@ class CassandraCppDriver < Formula
 
   bottle do
     cellar :any
+    sha256 "b5abf1a67997fd3f02c070416e258916e9caa3106034bd19c65289ac61ea8e7c" => :big_sur
     sha256 "c0eb554b6a640eed85988e787af1cfc8320b2b4920f2e88921dcf3ae67661ce1" => :catalina
     sha256 "7600578409910afcb3f14146be2eb7f17cc1eb9c862188b10744dd092f6a80b5" => :mojave
     sha256 "0eb84266667f4b10184e85e74784c000853b0ed4307008bdc8ee93237518a47e" => :high_sierra
