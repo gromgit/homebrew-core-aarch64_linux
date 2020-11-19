836c91c2bbbfafab0b2a54b23f469fd6eb1a9598
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-11-20 00:02:57 +0100

    apng2gif: remove old fix

--- Formula/apng2gif.rb
+++ Formula/apng2gif.rb
@@ -22,11 +22,6 @@ class Apng2gif < Formula
 
   depends_on "libpng"
 
-  if MacOS.version <= :yosemite
-    depends_on "gcc"
-    fails_with :clang
-  end
-
   def install
     system "make"
     bin.install "apng2gif"
