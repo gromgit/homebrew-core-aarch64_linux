7d0393cb7ae5d7e63a7ad3ee3ee108c22dfb040c
FX Coudert <fxcoudert@gmail.com>
2020-11-19 13:29:45 +0000

    fork-cleaner: update 1.8.0 bottle.

--- Formula/fork-cleaner.rb
+++ Formula/fork-cleaner.rb
@@ -7,6 +7,7 @@ class ForkCleaner < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "61ad94fb8515bf2f85daa11366635f591646ee79225d8c0b2d0f4d6c87c03c20" => :big_sur
     sha256 "7a963849e6b00d64efe10a5193d91d596611fa4e8a4bb9ee49c8000dbe8985db" => :catalina
     sha256 "5d17b463da2d368f7ba3276a81a0edd0f398fea58edfd865bd5f4795b0379deb" => :mojave
     sha256 "af54b2458a786b5d8ad0ebf398b4b5e1d0b335dd496e0d5157e10d17f62dc86a" => :high_sierra
