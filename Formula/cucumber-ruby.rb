2c4f3698e8c89739d4e9c4aaf3ccb91109dd1e2c
FX Coudert <fxcoudert@gmail.com>
2020-11-19 11:12:03 +0000

    cucumber-ruby: update 5.2.0 bottle.

--- Formula/cucumber-ruby.rb
+++ Formula/cucumber-ruby.rb
@@ -6,6 +6,7 @@ class CucumberRuby < Formula
   license "MIT"
 
   bottle do
+    sha256 "58a17b42e9f46239ca24c2afd3c54cadbf2b73dbf0b1f9499af37ac2b4399ace" => :big_sur
     sha256 "76c38db8edb1a3bdfd36c5e334c6e1c6bfd55f1c93f66719b1880aefe368228e" => :catalina
     sha256 "f1d76f4dc2da8f8e87911d3825b726050334437f65e473d4b581640a7c6a5b1d" => :mojave
     sha256 "81271d17f35e14fd259388bcbfc4d5ac0ab516a0a6ff5f003dc52d02421369a0" => :high_sierra
