e034e2170ee266a4cc9e683be2ec7a2c0c1716d3
FX Coudert <fxcoudert@gmail.com>
2020-11-19 23:41:37 +0000

    git-octopus: update 1.4 bottle.

--- Formula/git-octopus.rb
+++ Formula/git-octopus.rb
@@ -7,6 +7,7 @@ class GitOctopus < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9bd5e54184498eed96197bce98301493eabfcacd9e433bb9bf1768a49bee2e83" => :big_sur
     sha256 "175dcb36b4098a7c406d15a84cd2cfa856d6df0cfd0cdb3d2a08ff4101dc5249" => :catalina
     sha256 "4288690717a7c78406fde14701b2d8c3f7d1d24b27257ceedd02dda4ed81765e" => :mojave
     sha256 "67b8b9950133c0ca6f8a0be544bf192136ebad791eea0070becf47ab99eec270" => :high_sierra
