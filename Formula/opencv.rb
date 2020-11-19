3fde5889a6c13e53b55f255da53a03ff4c75f30c
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-11-20 00:03:55 +0100

    opencv: remove old fix

--- Formula/opencv.rb
+++ Formula/opencv.rb
@@ -88,12 +88,6 @@ class Opencv < Formula
       -DPYTHON3_EXECUTABLE=#{Formula["python@3.9"].opt_bin}/python3
     ]
 
-    # The compiler on older Mac OS cannot build some OpenCV files using AVX2
-    # extensions, failing with errors such as
-    # "error: use of undeclared identifier '_mm256_cvtps_ph'"
-    # Work around this by not trying to build AVX2 code.
-    args << "-DCPU_DISPATCH=SSE4_1,SSE4_2,AVX" if MacOS.version <= :yosemite
-
     args << "-DENABLE_AVX=OFF" << "-DENABLE_AVX2=OFF"
     args << "-DENABLE_SSE41=OFF" << "-DENABLE_SSE42=OFF" unless MacOS.version.requires_sse42?
 
