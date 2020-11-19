19547ed142dc50da4580553e44c4d0c9c4e9bb06
FX Coudert <fxcoudert@gmail.com>
2020-11-19 15:20:40 +0000

    stress-ng: update 0.11.24 bottle.

--- Formula/stress-ng.rb
+++ Formula/stress-ng.rb
@@ -12,6 +12,7 @@ class StressNg < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e602f8c4cebddb1b670ed8661f66b05c6387a081e930f8884a945e9b9710fa75" => :big_sur
     sha256 "4ab191753c668f9c70335153312e2e40b01b4ab965bfda73692d3177a127f70e" => :catalina
     sha256 "ea78a6865c25a9dd4f65b6d493fa459ff3a0ff272cf71b5d25192e9f53069034" => :mojave
     sha256 "4af7903bbbb13db6f9f4e9fd8cb02a662068d5abf859b2df6049ecfa1b208aec" => :high_sierra
