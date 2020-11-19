de1859ac3bd49ef8545516feb124ce4b882d049f
FX Coudert <fxcoudert@gmail.com>
2020-11-19 15:03:58 +0000

    grin-wallet: update 4.0.0 bottle.

--- Formula/grin-wallet.rb
+++ Formula/grin-wallet.rb
@@ -7,6 +7,7 @@ class GrinWallet < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "05e8a1345c2dcda04d4d51a0632e88f11b379c7978cc8df558e4f586e8810d95" => :big_sur
     sha256 "99c3c710cca324b279b9f53db6687c5f35dceb1a7fab99e6da08734f1f14fb64" => :catalina
     sha256 "9527840eae34a19dc485a2f13fe0d0422429f08e2352267a637a41b5bf31bf1d" => :mojave
     sha256 "c9f755928079e4d3073e5f80071b2d7a860b3f0daaf44abd0e0d5c1c1c106cab" => :high_sierra
