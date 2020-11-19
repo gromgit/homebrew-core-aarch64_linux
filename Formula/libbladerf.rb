26d2a422ef5fba921f56b052ad09d5062ca2a137
FX Coudert <fxcoudert@gmail.com>
2020-11-19 12:08:50 +0000

    libbladerf: update 2019.07 bottle.

--- Formula/libbladerf.rb
+++ Formula/libbladerf.rb
@@ -8,6 +8,7 @@ class Libbladerf < Formula
   head "https://github.com/Nuand/bladeRF.git"
 
   bottle do
+    sha256 "53852b1fb3d6fcebfacc6778666f5da33e016324a81b413854ff07717ff99174" => :big_sur
     sha256 "a60bfb3c0e350ec8fc1774b902bb8e151581f11a6669d067cb94da417e266bc3" => :catalina
     sha256 "47cc541e8c1e2061cb842595f08cd9adc65194378bf1303d876e79e6c5a93b85" => :mojave
     sha256 "f276e5ce4058bd486edaff6b97f61bddea9f44b5f88f35997a90c100da8f70d1" => :high_sierra
