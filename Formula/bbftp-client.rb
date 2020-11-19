21307bf907882c641f950d40a4689ba77ca1e504
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-11-20 00:03:05 +0100

    bbftp-client: remove old fix

--- Formula/bbftp-client.rb
+++ Formula/bbftp-client.rb
@@ -1,13 +1,13 @@
 class BbftpClient < Formula
   desc "Secure file transfer software, optimized for large files"
   homepage "https://software.in2p3.fr/bbftp/"
-  url "https://software.in2p3.fr/bbftp/dist/bbftp-client-3.2.1.tar.gz"
+  url "http://software.in2p3.fr/bbftp/dist/bbftp-client-3.2.1.tar.gz"
   mirror "https://dl.bintray.com/homebrew/mirror/bbftp-client-3.2.1.tar.gz"
   sha256 "4000009804d90926ad3c0e770099874084fb49013e8b0770b82678462304456d"
   revision 3
 
   livecheck do
-    url "https://software.in2p3.fr/bbftp/download.html"
+    url "http://software.in2p3.fr/bbftp/download.html"
     regex(/href=.*?bbftp-client[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
@@ -21,7 +21,7 @@ class BbftpClient < Formula
 
   def install
     # Fix ntohll errors; reported 14 Jan 2015.
-    ENV.append_to_cflags "-DHAVE_NTOHLL" if MacOS.version >= :yosemite
+    ENV.append_to_cflags "-DHAVE_NTOHLL"
 
     cd "bbftpc" do
       system "./configure", "--disable-debug",
