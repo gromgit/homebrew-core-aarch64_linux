923458faae9cf8a94a8bde2d1266244fa5e73f8d
FX Coudert <fxcoudert@gmail.com>
2020-11-19 11:43:36 +0000

    libxmi: update 1.2 bottle.

--- Formula/libxmi.rb
+++ Formula/libxmi.rb
@@ -13,6 +13,7 @@ class Libxmi < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "f5e9c2fce42f171773589cb0b1bfbf88cadf5036d86a6f502d5f415b8ad20f62" => :big_sur
     sha256 "eabebd41538c5b53f5ac3d25e71636b8d3561150f4622769107c58a10283e525" => :catalina
     sha256 "ee621ddddf3165736ebe0eb44ee0ea4eac0080ca328404311de57acc99402694" => :mojave
     sha256 "b4fae54573368c35c388850617545ab6f3fdd59bdcc8dde766e863b605278a40" => :high_sierra
