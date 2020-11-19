78e9b801ab19863b74d98d71a822990b5c3c4a6d
FX Coudert <fxcoudert@gmail.com>
2020-11-19 15:31:16 +0000

    qalculate-gtk: update 3.14.0 bottle.

--- Formula/qalculate-gtk.rb
+++ Formula/qalculate-gtk.rb
@@ -6,6 +6,7 @@ class QalculateGtk < Formula
   license "GPL-2.0-or-later"
 
   bottle do
+    sha256 "33b81f8c101d9b5f62604c4e92ea1ee3dccc4d36b68239bfe75979700d09ac64" => :big_sur
     sha256 "75b81c74cf19289d81ff5af2aca7c6589a150bc443875c463aeec9c771bcf506" => :catalina
     sha256 "83a982057c2ccd6db4a535a480696134affb0f1ce00b86ac4999ac70fd3f12dc" => :mojave
     sha256 "6b372cbcd32dbb3a9fd6a178277b3996f7bcd9ef9ed054432d653c2471619ae2" => :high_sierra
