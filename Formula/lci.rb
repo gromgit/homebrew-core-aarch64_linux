3175e62e1f126cc09f22e730c050e4fbb1e8d9d8
FX Coudert <fxcoudert@gmail.com>
2020-11-19 23:04:04 +0000

    lci: update 0.6 bottle.

--- Formula/lci.rb
+++ Formula/lci.rb
@@ -10,6 +10,7 @@ class Lci < Formula
   end
 
   bottle do
+    sha256 "bcdd98bead994b6b6c4551bb89575bfd154dcfff597d94da15a1f6158ca42f11" => :big_sur
     sha256 "a666107791ca81169da0ef403abeb77f5081baa97347d589c49c41c36650db7e" => :catalina
     sha256 "b2bf5e93bb4fdf90b6169b03fe046500fbc196c6d13068160bc92585e66384b3" => :mojave
     sha256 "f6a6d06cb581a5b3c00b05f8785ee33b2c9b69d5b920e8a7234aab3374ca988b" => :high_sierra
