7a69043e7d2754b1af89f7d5486bc29792f6ba0f
FX Coudert <fxcoudert@gmail.com>
2020-11-19 14:53:46 +0000

    coreos-ct: update 0.9.0 bottle.

--- Formula/coreos-ct.rb
+++ Formula/coreos-ct.rb
@@ -7,6 +7,7 @@ class CoreosCt < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a1b57820f00633b3e3af5d7cbee4accb970e8fee0e092fce3901801f3c84ce1e" => :big_sur
     sha256 "db582062a1743b1f01a0a90012e8a66d73b2518d88b4fb897afe9709565f6a95" => :catalina
     sha256 "883c46865e141d74fa7f0815fc51d3f9ea3145019f28dcec402fc457d3f67e27" => :mojave
     sha256 "5acd28f4e5dd0c74938cc7e7e10c5501badbab1d05316537a9afa173ff64f44e" => :high_sierra
