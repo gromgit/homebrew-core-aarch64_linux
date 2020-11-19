8141aef27d3c6c6126774b8e9eac6ea8c54a6b29
FX Coudert <fxcoudert@gmail.com>
2020-11-19 12:28:42 +0000

    kitchen-sync: update 2.10 bottle.

--- Formula/kitchen-sync.rb
+++ Formula/kitchen-sync.rb
@@ -13,6 +13,7 @@ class KitchenSync < Formula
 
   bottle do
     cellar :any
+    sha256 "4d319227763ecb2d8b01adafa821a806c91963e366ea4341a92a850429fb6115" => :big_sur
     sha256 "6b5366aa8413f25ff8985cb7d195723a78c9c42bc4af1eb50b9980e1694e605e" => :catalina
     sha256 "900f81a4a99ad8b1aef571d8c1d20de1edf102dbd9d202832c6f96a09b6f4454" => :mojave
     sha256 "80e91aab20f98adb5265fe24d39cbc4b8a10d66d2ec6663ad7ec294206554eb2" => :high_sierra
