b3a2ef71c012b8bf2c0468bdcf32f42cdc1c7f3d
FX Coudert <fxcoudert@gmail.com>
2020-11-19 09:58:17 +0000

    tty-solitaire: update 1.3.0 bottle.

--- Formula/tty-solitaire.rb
+++ Formula/tty-solitaire.rb
@@ -7,6 +7,7 @@ class TtySolitaire < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5b9ebaa1690fa5a83b91eabb7b91d84a4be3b777bfcf9f89bcca7e68bab1c0d9" => :big_sur
     sha256 "8a9b3d7d5b62d5a82b3e37920d76bdd02902c4705d81d2b158b8eb605232b91f" => :catalina
     sha256 "ad68372ed5eb8f98d1175fd9b014fb0881a6615fe05e21811ed4327ded9aa066" => :mojave
     sha256 "d35722d89335ba81284acdde82cdb5f370860441950f3acd7290a8e821382147" => :high_sierra
