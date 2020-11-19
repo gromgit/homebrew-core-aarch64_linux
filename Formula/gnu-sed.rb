1644eed42b315f3ef8270365696d087b7de69a5d
Michka Popoff <michkapopoff@gmail.com>
2020-11-19 22:30:44 +0100

    gnu-sed: set g prefix only on mac

--- Formula/gnu-sed.rb
+++ Formula/gnu-sed.rb
@@ -24,14 +24,21 @@ class GnuSed < Formula
     args = %W[
       --prefix=#{prefix}
       --disable-dependency-tracking
-      --program-prefix=g
     ]
 
+    on_macos do
+      args << "--program-prefix=g"
+    end
+    on_linux do
+      args << "--without-selinux"
+    end
     system "./configure", *args
     system "make", "install"
 
-    (libexec/"gnubin").install_symlink bin/"gsed" =>"sed"
-    (libexec/"gnuman/man1").install_symlink man1/"gsed.1" => "sed.1"
+    on_macos do
+      (libexec/"gnubin").install_symlink bin/"gsed" =>"sed"
+      (libexec/"gnuman/man1").install_symlink man1/"gsed.1" => "sed.1"
+    end
 
     libexec.install_symlink "gnuman" => "man"
   end
@@ -48,10 +55,16 @@ class GnuSed < Formula
 
   test do
     (testpath/"test.txt").write "Hello world!"
-    system "#{bin}/gsed", "-i", "s/world/World/g", "test.txt"
-    assert_match /Hello World!/, File.read("test.txt")
+    on_macos do
+      system "#{bin}/gsed", "-i", "s/world/World/g", "test.txt"
+      assert_match /Hello World!/, File.read("test.txt")
 
-    system "#{opt_libexec}/gnubin/sed", "-i", "s/world/World/g", "test.txt"
-    assert_match /Hello World!/, File.read("test.txt")
+      system "#{opt_libexec}/gnubin/sed", "-i", "s/world/World/g", "test.txt"
+      assert_match /Hello World!/, File.read("test.txt")
+    end
+    on_linux do
+      system "#{bin}/sed", "-i", "s/world/World/g", "test.txt"
+      assert_match /Hello World!/, File.read("test.txt")
+    end
   end
 end
