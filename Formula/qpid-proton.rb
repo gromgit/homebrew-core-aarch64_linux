43269abdfb2737dd5db0aa996fe298cc6edd6365
FX Coudert <fxcoudert@gmail.com>
2020-11-20 02:09:18 +0000

    qpid-proton: update 0.32.0 bottle.

--- Formula/qpid-proton.rb
+++ Formula/qpid-proton.rb
@@ -13,6 +13,7 @@ class QpidProton < Formula
 
   bottle do
     cellar :any
+    sha256 "f36d96b5b1c8312c04b6ee484db491f9aefc05c0dbed095a3e429bc8d9ad8915" => :big_sur
     sha256 "04bed9699fa8e399d7361dd79e2819780e43cc5a9d3d1d221f1085a644801e09" => :catalina
     sha256 "a6fb688d4b98625e77f1886284ab795374dcba771504301c4114825e79084ef0" => :mojave
     sha256 "754da3603d6c444bd392dcc94b28a1d8da76489dbf972638644889278b75932f" => :high_sierra
