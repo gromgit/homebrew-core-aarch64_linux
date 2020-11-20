bfe5d64dc72d0dffd619c8c11ef5cae493881bc0
FX Coudert <fxcoudert@gmail.com>
2020-11-20 18:16:41 +0000

    omega: update 1.4.16 bottle.

--- Formula/omega.rb
+++ Formula/omega.rb
@@ -6,6 +6,7 @@ class Omega < Formula
   license "GPL-2.0"
 
   bottle do
+    sha256 "d87aa45d8872c68c3d9eb1d02a080680dc2161bab3b5fe41a2d17e5f114b6c8c" => :big_sur
     sha256 "21def29541640883bd956e842a3370d9ac7dde45bd166add19ca83013f86189f" => :catalina
     sha256 "5625e477813a8c7bd720e17416b6dd80faf959f5d7210c3262031ba30c3e1f6e" => :mojave
     sha256 "ff44c076ce6ad71238f2293e35ad7c8fed610dfeb5eb15e8a52c0e68d9a0b62d" => :high_sierra
