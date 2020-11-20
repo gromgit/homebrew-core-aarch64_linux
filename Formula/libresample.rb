15944d9ab1543f0ff22e11b499d6d81149d02ab0
FX Coudert <fxcoudert@gmail.com>
2020-11-20 21:56:59 +0000

    libresample: update 0.1.3 bottle.

--- Formula/libresample.rb
+++ Formula/libresample.rb
@@ -8,6 +8,7 @@ class Libresample < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 2
+    sha256 "623df9f095f427ee794211cd657ad1d1ff5e89f383aa304c9c94d7a27cac36a3" => :big_sur
     sha256 "779b21b26d28a7318e67e0444b74ee5782715b523c1f79ba9bdff41c334cd312" => :catalina
     sha256 "7973809674c5ca9dceaf822abaf482c2a8126928140fa056168644b1196005c2" => :mojave
     sha256 "42b971ed75ad6ba1bd6879c2b7cb5fb416706ed184291d12983e46ab6c90a20c" => :high_sierra
