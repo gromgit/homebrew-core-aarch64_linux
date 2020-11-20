9e0d54f7421e32cf03ba3e294d1f52fea2b8cb83
FX Coudert <fxcoudert@gmail.com>
2020-11-20 21:37:14 +0000

    clutter-gtk: update 1.8.4_4 bottle.

--- Formula/clutter-gtk.rb
+++ Formula/clutter-gtk.rb
@@ -10,6 +10,7 @@ class ClutterGtk < Formula
   end
 
   bottle do
+    sha256 "4d0e9365bfeff618403fb3fa6db9319eb684b4c614af80f1ad6ed0f0fec57db1" => :big_sur
     sha256 "4b5c17d3567f1a5c03f98ccc8d9275f07fa733770d4ee741800505b8894442a1" => :catalina
     sha256 "665ed256370965e0f1e8660bf1f0372a7e5a9ea0fde176c06cb4c8e390a0739c" => :mojave
     sha256 "90929f36d6105b2b046e32f2d661c91305b785da535a7654fde19eb89617008e" => :high_sierra
