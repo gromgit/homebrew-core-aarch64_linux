e4bc6c06e409ebc2c43cc6ccf04e16d57098a3dd
FX Coudert <fxcoudert@gmail.com>
2020-11-20 20:25:11 +0000

    powerline-go: update 1.18.0 bottle.

--- Formula/powerline-go.rb
+++ Formula/powerline-go.rb
@@ -7,6 +7,7 @@ class PowerlineGo < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "215d4b5c4d992b6dae64fb8c304f9971b264959fb2a17e3c5d90e01564b687b9" => :big_sur
     sha256 "5b0d6f9c5324165ee50404989724a0fac3413ac74ee9be71fffc0823d0bae6af" => :catalina
     sha256 "07e4b014438c4de846d49c8ccd7d0f2a362b1271f50386916218c4a1c9edb5ce" => :mojave
     sha256 "1a872e444bba7d8ae32c266817b4b8c1985477b52340310e92f502f20c9b161d" => :high_sierra
