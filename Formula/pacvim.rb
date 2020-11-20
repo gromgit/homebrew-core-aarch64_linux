e05e364cc3a733bf1f426c6af5f711d3907f327f
Michka Popoff <michkapopoff@gmail.com>
2020-11-20 23:03:21 +0100

    pacvim: add linux-only patch

--- Formula/pacvim.rb
+++ Formula/pacvim.rb
@@ -3,7 +3,7 @@ class Pacvim < Formula
   homepage "https://github.com/jmoon018/PacVim"
   url "https://github.com/jmoon018/PacVim/archive/v1.1.1.tar.gz"
   sha256 "c869c5450fbafdfe8ba8a8a9bba3718775926f276f0552052dcfa090d21acb28"
-  license "LGPL-3.0"
+  license "LGPL-3.0-or-later"
   head "https://github.com/jmoon018/PacVim.git"
 
   bottle do
@@ -16,6 +16,15 @@ class Pacvim < Formula
   end
 
   uses_from_macos "ncurses"
+
+  on_linux do
+    # Use ncurses.h instead of cursesw.h which is not installed by brew
+    # https://github.com/jmoon018/PacVim/pull/31
+    patch do
+      url "https://github.com/jmoon018/PacVim/commit/2f95ef4d312d760b8a3aae463e959646b27e774a.patch?full_index=1"
+      sha256 "e5b753de87937c0853a1adbab31eb1ec938add4ceb0df26eafef5b4f613bc3e6"
+    end
+  end
 
   def install
     ENV.cxx11
