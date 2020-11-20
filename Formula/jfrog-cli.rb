344290520de8486eb1eeb8cdb1ea208821450f90
FX Coudert <fxcoudert@gmail.com>
2020-11-20 19:14:29 +0000

    jfrog-cli: update 1.41.1 bottle.

--- Formula/jfrog-cli.rb
+++ Formula/jfrog-cli.rb
@@ -7,6 +7,7 @@ class JfrogCli < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "050d41733fff6d921a90221cc99751bf5f8a13e9c907d81079c04d959f06b459" => :big_sur
     sha256 "d3d8780cc32a80388bb51a35ae2c01238ba33db7ae275cc1ad104b5514554973" => :catalina
     sha256 "2dd25783c525b6c18228d8cc9d8fd1898e2b6d76fb1ee3d6259ab070d5939c16" => :mojave
     sha256 "c331c0ba7d1a5f17043a51c14a89b422edefd18191b1f7deb36d2a212efad698" => :high_sierra
