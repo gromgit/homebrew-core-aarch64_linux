b63f331dc0442b8f4732fdba7171da4134f61de5
FX Coudert <fxcoudert@gmail.com>
2020-11-20 17:55:11 +0000

    luaradio: update 0.8.0 bottle.

--- Formula/luaradio.rb
+++ Formula/luaradio.rb
@@ -8,6 +8,7 @@ class Luaradio < Formula
 
   bottle do
     cellar :any
+    sha256 "5ab17c6deca1953c08081458d66faa0081b6c0c680b05e07b39563057732e90a" => :big_sur
     sha256 "1370415899d4c7c41b0bfd5ac4792ea3447736acf8727cbbe492b4214b2cc3c1" => :catalina
     sha256 "0968a0cd591fd3e3f7af909df152d5065b8b711a4a7a024c2ac13aceea9aac01" => :mojave
     sha256 "190f3227e451c188e7588e4bdd9f9b1883c97516da52386b2f7019a20858270e" => :high_sierra
