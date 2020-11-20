9e787cd0f660e55713e90ebf90945974588d21e0
FX Coudert <fxcoudert@gmail.com>
2020-11-20 01:58:00 +0000

    generate-json-schema: update 2.6.0 bottle.

--- Formula/generate-json-schema.rb
+++ Formula/generate-json-schema.rb
@@ -14,6 +14,7 @@ class GenerateJsonSchema < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "aee39ac19fd8f45785021bd737daf8f8ab15e7265b5e196485259d5a2c2cfede" => :big_sur
     sha256 "4d5a50f712bb6714564574d20cbd771e62ad1da6dcd58d9b7225822af0821d73" => :catalina
     sha256 "e049d098796be43aa340eca884fa71ec90f4fbeda02031142f66752df005de97" => :mojave
     sha256 "3461301c038b8bb6e15b8e183661976e95ea7b7e0659d57f0f21ea2c0eb4e67c" => :high_sierra
