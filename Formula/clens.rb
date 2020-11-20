5fb0cbf2daa47ee9aa8ead1b1939a5a222802079
FX Coudert <fxcoudert@gmail.com>
2020-11-20 22:31:54 +0000

    clens: update 0.7.0 bottle.

--- Formula/clens.rb
+++ Formula/clens.rb
@@ -7,6 +7,7 @@ class Clens < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "602ace92e6b121b004a43a851209b95b0769bc84d9ea0c7725f29f3d2531324f" => :big_sur
     sha256 "fef1ad76413e8e15683a4066276ed7f37f821edcbda4e6d648bd60e09a33a30d" => :catalina
     sha256 "3550adf8f1e9eb8e62ac6c64f3b3c8cae0fba0d0f958f6e6da99e74f4e4d5e19" => :mojave
     sha256 "e9dfe6e9228a928f9cb1a4048d92ec576be5f89f48408401b7f3020c1482a7c6" => :high_sierra
