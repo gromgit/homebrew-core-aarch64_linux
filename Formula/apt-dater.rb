aca4d3723e1d7ba9a332bb5c0e7e2a8d23e2bc7a
FX Coudert <fxcoudert@gmail.com>
2020-11-20 16:06:20 +0000

    apt-dater: update 1.0.4_1 bottle.

--- Formula/apt-dater.rb
+++ Formula/apt-dater.rb
@@ -8,6 +8,7 @@ class AptDater < Formula
   version_scheme 1
 
   bottle do
+    sha256 "cf4a97e076ce5f8820c9a1dc787c5e751b350cc223d17ec0ba6007d6e8d97484" => :big_sur
     sha256 "5fe58574f889c5e29bd2f4c492848281450da398cace807a33c5100b44090665" => :catalina
     sha256 "d736fdabb393e90e6895b9d5694cc0a78f592bd363483e7e935d044fd0331d41" => :mojave
     sha256 "f6b5f606925ac38d24ef56fc52e93c3f5a4e8f1ab2d687ebb376c78d4f91f366" => :high_sierra
