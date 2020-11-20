e850a2625934891cdc997dcbff026735a6c1dfe6
FX Coudert <fxcoudert@gmail.com>
2020-11-20 21:37:38 +0000

    heatshrink: update 0.4.1 bottle.

--- Formula/heatshrink.rb
+++ Formula/heatshrink.rb
@@ -7,6 +7,7 @@ class Heatshrink < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "9131daef95de9b3c7fecb082ade54be4b67f8c4ca3f3bce6d18f19b1492078d9" => :big_sur
     sha256 "5956959544286fc9b6474a0f6df508530431c1632527fa4048091f33f319fab2" => :catalina
     sha256 "504b4b64164343217c6852509b59858494ba38ad9b63e7a9b3bb247290833582" => :mojave
     sha256 "865d11380a3e586a962a5dec0069def43e777f20626bdc5396735d003d90d20b" => :high_sierra
