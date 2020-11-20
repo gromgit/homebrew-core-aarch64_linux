f08fcf0effbe1eb95a1568838c8c94a783511520
Alexander Bayandin <a.bayandin@gmail.com>
2020-11-20 17:38:11 +0000

    blockhash: switch to python3
    Closes #65307.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/blockhash.rb
+++ Formula/blockhash.rb
@@ -16,15 +16,33 @@ class Blockhash < Formula
   end
 
   depends_on "pkg-config" => :build
+  depends_on "python@3.9" => :build
   depends_on "imagemagick"
-  depends_on :macos # Due to Python 2
 
   resource "testdata" do
     url "https://raw.githubusercontent.com/commonsmachinery/blockhash/ce08b465b658c4e886d49ec33361cee767f86db6/testdata/clipper_ship.jpg"
     sha256 "a9f6858876adadc83c8551b664632a9cf669c2aea4fec0c09d81171cc3b8a97f"
   end
 
+  # Add python3 support
+  #
+  # This patch mimics changes from https://github.com/commonsmachinery/blockhash/commit/07268aeaeef880e0749bd22331ee424ddbc156e0
+  # but can't be applied as a formula patch since it contains GIT binary patch
+  #
+  # See https://github.com/commonsmachinery/blockhash/issues/28#issuecomment-417726356
+  #
+  # Remove this in next release
+  resource "waf-2.0.10" do
+    url "https://raw.githubusercontent.com/commonsmachinery/blockhash/07268aeaeef880e0749bd22331ee424ddbc156e0/waf"
+    sha256 "0a855861c793f9b7e46b0077b791e13515e00742e1493e1818f9b369133b83d7"
+  end
+
   def install
+    resource("waf-2.0.10").stage buildpath
+    chmod 0755, "waf"
+
+    ENV.prepend_path "PATH", Formula["python@3.9"].opt_bin
+
     system "./waf", "configure", "--prefix=#{prefix}"
     # pkg-config adds -fopenmp flag during configuring
     # This fails the build on system clang, and OpenMP is not used in blockhash
