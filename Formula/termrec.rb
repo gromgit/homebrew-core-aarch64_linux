d67f694164b0414258186cdcfaaa8c7be6fc6587
FX Coudert <fxcoudert@gmail.com>
2020-11-20 02:13:40 +0000

    termrec: update 0.19 bottle.

--- Formula/termrec.rb
+++ Formula/termrec.rb
@@ -8,6 +8,7 @@ class Termrec < Formula
 
   bottle do
     cellar :any
+    sha256 "81060090e19bbb56f0b991dfa987eb890c00b116b656be2d2bd29ea027f9496a" => :big_sur
     sha256 "1d93149ec34c0bf531da76b0137390ed1f05bf2e35e806f1fe875fe6648c4c2b" => :catalina
     sha256 "e3f9f241763a05de367da2ee91727674e18a126a99480a750b901a21bdad0ffb" => :mojave
     sha256 "d6cb43ed14ec0531824bd4eb55ddc625b5711c28b274ce78eb815501e5f3ebf2" => :high_sierra
