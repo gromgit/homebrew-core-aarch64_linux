eda4b2d722df0eeaf52d01138de416637a0a7560
FX Coudert <fxcoudert@gmail.com>
2020-11-20 16:37:43 +0000

    gulp-cli: update 2.3.0 bottle.

--- Formula/gulp-cli.rb
+++ Formula/gulp-cli.rb
@@ -13,6 +13,7 @@ class GulpCli < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e011832e0a4186df90c8b6131b1ec39e60540de336bd557879351011246cfcdf" => :big_sur
     sha256 "231b635ddf8a704a3be4a6ba34611248ece69ed1de04fb82adfa6a20ac83fddb" => :catalina
     sha256 "29ec2f9cf132be84c577ff6d6ea02845ee96d995e1affdea8961903f9fec616a" => :mojave
     sha256 "e4d363c9d5035fc814ca6a6820b9c8c35a320cd507067cad5eb0c0e6c337c36e" => :high_sierra
