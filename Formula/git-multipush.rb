59a1d37aa4dd71f447329bb66fbd1dde458aaf7f
FX Coudert <fxcoudert@gmail.com>
2020-11-20 00:26:01 +0000

    git-multipush: update 2.3 bottle.

--- Formula/git-multipush.rb
+++ Formula/git-multipush.rb
@@ -8,6 +8,7 @@ class GitMultipush < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "981bdfe608288a854656563e178db2cdb9e9a9db30381203e0f67bfd38d29e16" => :big_sur
     sha256 "b9de9b128791d8c416076ed738016ffe534ce85bacddf297ab9ce13954dcaff6" => :catalina
     sha256 "8f4c2e7a1aee0db75154c4b21aee1a4bd398a9b889f119e7b86a06b1533b9304" => :mojave
     sha256 "edd99d5ec177bccf061f7424aa595a5515fa5728aec649594f42964cec1f371e" => :high_sierra
