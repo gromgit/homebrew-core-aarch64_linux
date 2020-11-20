e45e893ed3bb07962ed910b178b0e90d441f7cc9
chenrui <chenrui333@gmail.com>
2020-11-20 05:35:37 +0000

    gopls: add 0.5.3 bottle.

--- Formula/gopls.rb
+++ Formula/gopls.rb
@@ -5,6 +5,13 @@ class Gopls < Formula
   sha256 "f3a1c142f42c7e9cff62ec9a1a6e41e59305c7519735839ecedccf17597ae00e"
   license "BSD-3-Clause"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "023ac272fd82dd6b22ad86636eab63d3cb536b8e3cb95740b56495abcd2dda39" => :big_sur
+    sha256 "f8ee59eeebfcf3d7016ef0940a1c167bb5a133684e708a907995b2d2966b000b" => :catalina
+    sha256 "2bb0be3febc55e3908c4d6bba6ad10ab34f3724b243435ecdeee6c1d8dc8afbd" => :mojave
+  end
+
   depends_on "go" => :build
 
   def install
