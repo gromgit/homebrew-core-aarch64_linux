7288a691cb0be938e60fbae7e62dd261a8b0d9cd
FX Coudert <fxcoudert@gmail.com>
2020-11-20 19:42:26 +0000

    wla-dx: update 9.11_1 bottle.

--- Formula/wla-dx.rb
+++ Formula/wla-dx.rb
@@ -13,6 +13,7 @@ class WlaDx < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "66a1b0e894ef405ba43690d9692c03756db1698deca8f40b6b15340519ec07d9" => :big_sur
     sha256 "8f0d4747eb9ef0885ddf6c08b3d4ac980bd2b6dbaaa9f5048ea7aa4bc6f681b8" => :catalina
     sha256 "b515cc9b31fd4d978143c518555b02873fabff5ef390d369575c3d3e99606326" => :mojave
     sha256 "0ed73304d947e4ea44431c06df38bb6887a7551f575ade25a6b63ce7b27187c7" => :high_sierra
