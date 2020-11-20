b3fa952a67caccab484cb29430d4f33408e04321
FX Coudert <fxcoudert@gmail.com>
2020-11-20 22:49:15 +0000

    ats2-postiats: update 0.4.2 bottle.

--- Formula/ats2-postiats.rb
+++ Formula/ats2-postiats.rb
@@ -12,6 +12,7 @@ class Ats2Postiats < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "2e7267a095f51b3cdc898bb3dd5c051613383a7b79019b291c1586b3d8fe2ec0" => :big_sur
     sha256 "19ea3eb93cc5ba40ce3c1bdc48666edd4a8cd00027fbb4531392a0f15ecc7a94" => :catalina
     sha256 "9e0b2824b0ea3d67e22c6690d3608c5d09d9855075e9811ad71b5f2703be9304" => :mojave
     sha256 "91d683a9f1c94daff0e9b089cc7a5ceda949eb4b5aa7d286c554d9506ee6d49c" => :high_sierra
