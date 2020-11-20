1ccb130409980c704dbb87bc72899fa8bff05664
FX Coudert <fxcoudert@gmail.com>
2020-11-20 18:00:18 +0000

    spirv-tools: update 2020.3_1 bottle.

--- Formula/spirv-tools.rb
+++ Formula/spirv-tools.rb
@@ -8,6 +8,7 @@ class SpirvTools < Formula
 
   bottle do
     cellar :any
+    sha256 "be3e8a4a7679c08b9072bfa76ffb729f28c6ac741fa58d44066d7291339a0088" => :big_sur
     sha256 "b150e16e5d4d40f29dfd739bf297cebfa232d3ae267fb7965d4151a8ace43ec4" => :catalina
     sha256 "f5ddf173300822bf7f7f0b42bafdf1d242b1de1b63a565d02e0084f7b4107d36" => :mojave
     sha256 "4bcbda12ffa24d5bcc19271cc318a035130d4f168145397704d0ef98fe1adda5" => :high_sierra
