0b464e3e8259a0df18b9ef9f49a4a52a23a7d3b5
FX Coudert <fxcoudert@gmail.com>
2020-11-20 17:13:46 +0000

    mage: update 1.10.0 bottle.

--- Formula/mage.rb
+++ Formula/mage.rb
@@ -8,6 +8,7 @@ class Mage < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "799234d0fd1db793475b5aaa33436d7751e9ea59ef0bf63e383a977f2e3c485f" => :big_sur
     sha256 "515be0f1647600a652fb18c7ca2eae45683e9e22f22ef7a8cfa0257e05ef6024" => :catalina
     sha256 "d785e2a6fb3cb2a03db1a83ea1f5f2105b6dd0b254d868b7b8950ceb8910c97a" => :mojave
     sha256 "743f8a5be5aa6dc79dbbd7f44b5cfe1726862c865042d22183d522c863994e7f" => :high_sierra
