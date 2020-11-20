5695cf2cb5ad10ba1b2401d9972871c81900d200
FX Coudert <fxcoudert@gmail.com>
2020-11-20 02:01:01 +0000

    nats-server: update 2.1.9 bottle.

--- Formula/nats-server.rb
+++ Formula/nats-server.rb
@@ -9,6 +9,7 @@ class NatsServer < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "48e5ce9c0aac69f8ca0f27db652227900df5c772472bb391b8702457ecfe0caf" => :big_sur
     sha256 "42c81987bbcffffee20ece6c4534a77612496c2b082723681f4468eab7a0fd61" => :catalina
     sha256 "c5e96271283bee73b7ff9cfa68f6d1f566765bf2e41374c67ae66c51e502e2d8" => :mojave
     sha256 "e4d101f7ea1263dabd9a95a3b90925082a003274d1a45c2168b4bf7bbb8a1fa1" => :high_sierra
