b2ed371dd59c22f222049fad08094cd205607d96
Alexander Bayandin <a.bayandin@gmail.com>
2020-11-20 15:33:47 +0000

    libtcod: remove :macos dependency
    Closes #65288.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libtcod.rb
+++ Formula/libtcod.rb
@@ -19,7 +19,6 @@ class Libtcod < Formula
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "python@3.9" => :build
-  depends_on :macos # Due to Python 2
   depends_on "sdl2"
 
   def install
