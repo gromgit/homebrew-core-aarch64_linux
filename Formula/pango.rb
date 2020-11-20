3d70b4133b7fd4efc67fc0e3183c2230a582d530
Michka Popoff <michkapopoff@gmail.com>
2020-11-20 23:12:03 +0100

    pango: set -lintl flag only on mac

--- Formula/pango.rb
+++ Formula/pango.rb
@@ -82,10 +82,12 @@ class Pango < Formula
       -lcairo
       -lglib-2.0
       -lgobject-2.0
-      -lintl
       -lpango-1.0
       -lpangocairo-1.0
     ]
+    on_macos do
+      flags << "-lintl"
+    end
     system ENV.cc, "test.c", "-o", "test", *flags
     system "./test"
   end
