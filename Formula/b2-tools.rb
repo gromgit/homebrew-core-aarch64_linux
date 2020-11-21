dac97d418ffc919b78a494b965842a9b10bc4d29
FX Coudert <fxcoudert@gmail.com>
2020-11-21 14:14:15 +0000

    b2-tools: update 2.1.0 bottle.

--- Formula/b2-tools.rb
+++ Formula/b2-tools.rb
@@ -9,6 +9,7 @@ class B2Tools < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "d264b66cfb7d841a069d900e3a5fdfc8003d45de20298df1207b42573de4280f" => :big_sur
     sha256 "afdab50c5e6398ac74355674473569b5f6dbd3e43bdeaaf9fe27dea19cabac4d" => :catalina
     sha256 "0d46e13619fb6fb5fa27c2bf5abb276d484ed12eba08366c6d3794b2b574a70e" => :mojave
     sha256 "e93427f054a8360d0dec4ff73a71f25e938bb427b51bd860a3d1bde035a2bc2b" => :high_sierra
