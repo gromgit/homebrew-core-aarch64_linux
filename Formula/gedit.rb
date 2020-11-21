4aade9d4ee4205c412fa6c5f2a1ca0f0f23fd6fb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-21 03:14:54 +0000

    gedit: update 3.38.1 bottle.

--- Formula/gedit.rb
+++ Formula/gedit.rb
@@ -10,10 +10,9 @@ class Gedit < Formula
   end
 
   bottle do
-    sha256 "e17ca7bf78431383ad4ffa100f17407f8cb174a2584aae0a2d00c635ccdffff5" => :big_sur
-    sha256 "20f526cd2c9cd65e6d3d5745291abab24e90da2297ba9cf3d968fb0a8bca61f1" => :catalina
-    sha256 "829baca554b2379332b40b23726cd07ea5df9c3bb0cde67b91e215aef73fd67f" => :mojave
-    sha256 "4466c99c2f70ddcacda0dd99c52bbc5d0f7094e4153144e8c20ae0300b2dabce" => :high_sierra
+    sha256 "841bf24ceaaace6e6f844bbdf328b5a482682856e70243cd9e9acc041a53c502" => :big_sur
+    sha256 "c1960a0d6062d4dd90839c331654bce9af5ad47fd46aff8612bdb565db6ff63c" => :catalina
+    sha256 "ce6bf4a9ee9e03ea5634b50f26f50a0a7cd032e0f613434fde66de49989b7c01" => :mojave
   end
 
   depends_on "itstool" => :build
