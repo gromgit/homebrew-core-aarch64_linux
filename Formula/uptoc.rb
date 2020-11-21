80cef04ecb2e1324f6964341d3664829353d1610
FX Coudert <fxcoudert@gmail.com>
2020-11-21 20:03:38 +0000

    uptoc: update 1.4.3 bottle.

--- Formula/uptoc.rb
+++ Formula/uptoc.rb
@@ -8,6 +8,7 @@ class Uptoc < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "c063e7fc62e3476ed8585a8a23bc34a78e842840425676d5ae752b48f4400fc1" => :big_sur
     sha256 "6133ca9ca353edec73753c155c22bfc2757eecf8ae735e178f604fdb6e06a313" => :catalina
     sha256 "01ebd4051546e2834975cd9ca921695a5deed06407ef4009e3b6e0c59dffc5ea" => :mojave
     sha256 "8067fc0ce3bc47e786ad0476c477c34cda3dfdf1a10228aae699ee5afd9f3d10" => :high_sierra
