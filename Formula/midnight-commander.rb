aab2e7563d1c3ee9444d5dc8e5970c010b541f54
FX Coudert <fxcoudert@gmail.com>
2020-11-21 19:39:50 +0000

    midnight-commander: update 4.8.25 bottle.

--- Formula/midnight-commander.rb
+++ Formula/midnight-commander.rb
@@ -12,6 +12,7 @@ class MidnightCommander < Formula
   end
 
   bottle do
+    sha256 "1cf10d6382ee603a2b15735029d31b7bea7bb65f29a50dbbc1c61a828d3cf433" => :big_sur
     sha256 "2c36f252c47b8ecff2fa4afb4191a963af7c3d30a8aeb267a40f967873a01643" => :catalina
     sha256 "224d6aa6577e51952833ee65888bb99eacb89508dc9ac2f82a0e679b4635d7e3" => :mojave
     sha256 "79c2208b2097941cf3a792f47ad1f280ddbc3add7bd631084484163b7ba14ae9" => :high_sierra
