5c117e536937425201ff7be036c69ec270321eda
FX Coudert <fxcoudert@gmail.com>
2020-11-21 16:53:31 +0000

    rem: update 20150618 bottle.

--- Formula/rem.rb
+++ Formula/rem.rb
@@ -7,6 +7,7 @@ class Rem < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "e0af2d48a7809890f04480b0b4d28f6354130754609627ed76ce6d76a5135898" => :big_sur
     sha256 "bfab3fd2fd8da4e4620d80a632d774b4742c6c34c5b73d89fafd3d246369fce6" => :catalina
     sha256 "4226be6dc999a4467a061055cb36a68babe84a835f40f32a5a23f6137ddd59b4" => :mojave
     sha256 "0a3365c8653023f2b4de8c5b6243aec2de7c180d1be982adcdbe58afc159800e" => :high_sierra
