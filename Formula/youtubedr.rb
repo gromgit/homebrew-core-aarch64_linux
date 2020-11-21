3e32a1d1d5e5506484ecdd5a7ceaac1f5b949b1d
Kakashi Liu <kakashi1000@gmail.com>
2020-11-21 18:10:35 +0800

    youtubedr 2.2.1 (new formula)
    * youtubedr 2.2.1
    * Add more tests
    * remove bottle :unneeded
    * Tweak testcases
    
    Closes #65343.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/youtubedr.rb
@@ -0,0 +1,32 @@
+class Youtubedr < Formula
+  desc "Download Youtube Video in Golang"
+  homepage "https://github.com/kkdai/youtube"
+  url "https://github.com/kkdai/youtube/archive/v2.2.1.tar.gz"
+  sha256 "e0e298aa4d589813b635bc09e9b50b220f248312ec5893a381bacf70a624a9e1"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    build_time = Utils.safe_popen_read("date -u +'%Y-%m-%dT%H:%M:%SZ' 2> /dev/null").chomp
+
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.date=#{build_time}
+    ]
+
+    ENV["CGO_ENABLED"] = "0"
+    system "go", "build", "-ldflags", ldflags.join(" "), *std_go_args, "./cmd/youtubedr"
+  end
+
+  test do
+    version_output = pipe_output("#{bin}/youtubedr version").split("\n")
+    assert_match(/Version:\s+#{version}/, version_output[0])
+
+    info_output = pipe_output("#{bin}/youtubedr info https://www.youtube.com/watch\?v\=pOtd1cbOP7k").split("\n")
+    assert_match "Title:       History of homebrew-core", info_output[0]
+    assert_match "Author:      Rui Chen", info_output[1]
+    assert_match "Duration:    13m15s", info_output[2]
+  end
+end
