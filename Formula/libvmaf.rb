41f8e1e928fb97e3741f7a99251fdd0829181384
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-21 04:17:01 +0000

    libvmaf: update 1.5.3 bottle.

--- Formula/libvmaf.rb
+++ Formula/libvmaf.rb
@@ -7,9 +7,10 @@ class Libvmaf < Formula
 
   bottle do
     cellar :any
-    sha256 "3ff54681fdedd65aa05925a0a5deb941e2b48ecdc946bb83358013e4a53f46f3" => :catalina
-    sha256 "fe3f8995fa8ce2c13af693e40581dc93e63e22821fa3fe007430a2acd3bca6da" => :mojave
-    sha256 "c701af393db9948a5d804607237678b232a32d7501de39c482095353c5f7d3b6" => :high_sierra
+    rebuild 1
+    sha256 "42337edc375b2eaebf63621360edc0bf827c03f2c5f6e07ffe472a65a7603a29" => :big_sur
+    sha256 "e7b3a833cd602d12291441709a60808e89d6d48c00232fdea155e96fe91911fa" => :catalina
+    sha256 "45d4ff0d068b03980d6192e2a636f6ca37fa429fa0c0aecdc8d5d55dcd8b06bd" => :mojave
   end
 
   depends_on "meson" => :build
