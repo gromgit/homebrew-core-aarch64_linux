06a715409c7c007a3354afadc2537c3e83aa0027
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-21 23:14:08 +0000

    qpdf: update 10.0.4 bottle.

--- Formula/qpdf.rb
+++ Formula/qpdf.rb
@@ -7,10 +7,9 @@ class Qpdf < Formula
 
   bottle do
     cellar :any
-    sha256 "622b938f59652a03977c6faa6b51df6124b74ee60a610cbe5833bc8af6c26b60" => :big_sur
-    sha256 "dd31dda8bb5b39fc025fc52951661a22e5ce87604393271a941151cb592837d3" => :catalina
-    sha256 "34d03d5740b508c2c90db74cc1f59a17ef1dbff5d5e24c96de316bcc78fdbc1e" => :mojave
-    sha256 "0e58fbc2dc0feb2d68f922ebf1885786ad4d3a89759d6bbcdf3f8319a021ac2e" => :high_sierra
+    sha256 "7fcecfff0483197b214c156a1708b3c8cb854dfcae8fdc3839a8bdbd37449bea" => :big_sur
+    sha256 "73dca004aacd24237d98c604885492e13a1156151113485df48877d99a915423" => :catalina
+    sha256 "8556fd9465de6203de5104777092e3263ecb0f8b9146ff35372307ea857acd6f" => :mojave
   end
 
   depends_on "jpeg"
