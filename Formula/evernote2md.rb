0dc762897cf6ead0ecd20f75c189a9ca7c708bab
Stanislav Petrashov <s@petrashov.ru>
2020-11-21 19:16:00 +0100

    evernote2md 0.12.0 (new formula)
    Closes #65372.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: chenrui <chenrui333@gmail.com>

---
+++ Formula/evernote2md.rb
@@ -0,0 +1,31 @@
+class Evernote2md < Formula
+  desc "Convert Evernote .enex file to Markdown"
+  homepage "https://github.com/wormi4ok/evernote2md"
+  url "https://github.com/wormi4ok/evernote2md/archive/v0.12.0.tar.gz"
+  sha256 "ab1103a176b7e82852d71791f0190f0e5f22c9ac8eb07d0881c3e8bea5620a08"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "-ldflags", "-s -w -X main.version=#{version}"
+  end
+
+  test do
+    (testpath/"export.enex").write <<~EOF
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE en-export SYSTEM "http://xml.evernote.com/pub/evernote-export3.dtd">
+      <en-export>
+        <note>
+          <title>Test</title>
+          <content>
+            <![CDATA[<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+      <!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note><div><br /></div></en-note>]]>
+          </content>
+        </note>
+      </en-export>
+    EOF
+    system bin/"evernote2md", "export.enex"
+    assert_predicate testpath/"notes/Test.md", :exist?
+  end
+end
