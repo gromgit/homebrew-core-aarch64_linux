07f0985fdf4311ef25b1b05dfa7d06f8f722754f
FX Coudert <fxcoudert@gmail.com>
2020-11-21 16:53:05 +0000

    yydecode: update 0.2.10 bottle.

--- Formula/yydecode.rb
+++ Formula/yydecode.rb
@@ -12,6 +12,7 @@ class Yydecode < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "4700a77bb5b4bbce8b34f92662661cf13f9560c1637256fd8dc9581ec7caf077" => :big_sur
     sha256 "206152a71458e053c332c7ca52f6db716b146e993c08384afb98e56a43c043b6" => :catalina
     sha256 "18d815befe31bcdeaac8edff43cb878f53c34a608fa946c13a14143139bf887a" => :mojave
     sha256 "e2e7285f1f2b18b4b99800602d15932dba435f6480c5776e5b57b734727f652f" => :high_sierra
