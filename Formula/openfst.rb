a3305ee3343c990733b0e4f44af5494f6c889ea5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-22 18:14:18 +0000

    openfst: update 1.8.0 bottle.

--- Formula/openfst.rb
+++ Formula/openfst.rb
@@ -12,10 +12,9 @@ class Openfst < Formula
 
   bottle do
     cellar :any
-    sha256 "a0289323819255885b7d45a89e2ebd88512f8153c1c956017258a61b07c29506" => :big_sur
-    sha256 "b32fb6cb0eb43a7d8775d8bfc760c49471586eeb33797f3d44a8b53cd45dc792" => :catalina
-    sha256 "7e5a450f383ddfeddcb7ee8d240e7db576fcc32a25c199d6a35eba40fea920d9" => :mojave
-    sha256 "0635e790f390be0a97c78a434e723280339fe0f0d86ee55c4a34339840f160a7" => :high_sierra
+    sha256 "5fd5b3e6e1cab084de39129ee2a5ac9512a45bf564029682598bea2c4fd83aed" => :big_sur
+    sha256 "520a3a1fe07f55b4d12de03cc259b19e12152f3e02b7991abb8512c6d94208bb" => :catalina
+    sha256 "ab87b1086002ff7ce0174cf9fc6e6336960b4e23c890ac9a0572ccc44e5cda5c" => :mojave
   end
 
   def install
