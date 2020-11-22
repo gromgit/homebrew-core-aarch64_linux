8be691d78b468d696debc2623ab958e010d31363
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-22 18:14:20 +0000

    pngquant: update 2.13.1 bottle.

--- Formula/pngquant.rb
+++ Formula/pngquant.rb
@@ -13,10 +13,9 @@ class Pngquant < Formula
 
   bottle do
     cellar :any
-    sha256 "ac9a63953788be2e15e1215b32f3280dcaa4beaebeae7f239247616d9186f7d8" => :big_sur
-    sha256 "cad350e78adc1912e1895b2f1c4abaf27bd14db902bad179424580934b9e1a05" => :catalina
-    sha256 "60f111e8252d2480df50d6fc77e2938c50480dc03e00207a75033b882cbeb740" => :mojave
-    sha256 "eb2b662bda1612dece961a7809ea5336997a00354fe204a001b4191b5b658fed" => :high_sierra
+    sha256 "581ba7e25c2df8ac52853df3771ac13c151a23d2f0d932fa9bf532e483c3aba0" => :big_sur
+    sha256 "a19326b4dd20ac58d8048cb5f540e3a278e454ca58c786ba4a2141926bb28cf3" => :catalina
+    sha256 "a6a7ae1b165e9f571fcb5b9708f9b14f4a566a4b37b256429c98ec6949ac0b06" => :mojave
   end
 
   depends_on "pkg-config" => :build
