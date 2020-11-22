98bb1b465f78d3e73eef6326049b345d0b1ef976
Jonathan Chang <me@jonathanchang.org>
2020-11-22 18:53:26 +1100

    morse: use pulseaudio backend

--- Formula/morse.rb
+++ Formula/morse.rb
@@ -4,7 +4,7 @@ class Morse < Formula
   url "http://www.catb.org/~esr/morse/morse-2.5.tar.gz"
   sha256 "476d1e8e95bb173b1aadc755db18f7e7a73eda35426944e1abd57c20307d4987"
   license "BSD-2-Clause"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
@@ -13,15 +13,16 @@ class Morse < Formula
     sha256 "cf3b1e007ccd9008513ec84b6411ccee4756d61e801902faad5cf4d5243a8b0f" => :high_sierra
   end
 
-  depends_on "libx11"
+  depends_on "pkg-config" => :build
+  depends_on "pulseaudio"
 
   def install
-    system "make", "all", "DEVICE=X11"
-    bin.install "morse"
-    man1.install "morse.1"
+    system "make", "all"
+    bin.install %w[morse QSO]
+    man1.install %w[morse.1 QSO.1]
   end
 
   test do
-    system "#{bin}/morse"
+    assert_match "Could not initialize audio", shell_output("#{bin}/morse -- 2>&1", 1)
   end
 end
