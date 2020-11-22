7d88f41e37cd95e46a3d74bd850709d804510c45
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-22 18:14:19 +0000

    libimagequant: update 2.13.1 bottle.

--- Formula/libimagequant.rb
+++ Formula/libimagequant.rb
@@ -7,10 +7,9 @@ class Libimagequant < Formula
 
   bottle do
     cellar :any
-    sha256 "d6541acf2369f7e438c935f1d6e560e1be686a556de7d9160f9628b3ec44549e" => :big_sur
-    sha256 "cf22020efa8a6816521877e04ac132876ab81362bb5e28b992ddfa76f3d9a228" => :catalina
-    sha256 "cfffbc501d8bf39d8305421921f0ec4d727f7bb5548481e9e4908164b0c45db0" => :mojave
-    sha256 "ddb7eba89c6e65024e72c9cb3272b7c4f7edc0f5b54eb232ebce9f958e8a6480" => :high_sierra
+    sha256 "4eec259853e8c650282ab0f9fe2fe382cfb0c63adf50eefb0e53038e9579b117" => :big_sur
+    sha256 "ddaab4f5d02ee638676c716701a3e4364c76a3e6184787b2b5bc078b95497fb2" => :catalina
+    sha256 "f7902838c7698902828571a5bbb170a1855ee66d96070e7b577b9a332370989b" => :mojave
   end
 
   def install
