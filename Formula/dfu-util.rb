e4435ecb02d26f4a966e60a43245c303f8e30977
Rui Chen <rui@meetup.com>
2020-11-21 23:06:16 -0500

    dfu-util 0.10
    Closes #65390.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dfu-util.rb
+++ Formula/dfu-util.rb
@@ -1,8 +1,8 @@
 class DfuUtil < Formula
   desc "USB programmer"
   homepage "https://dfu-util.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/dfu-util/dfu-util-0.9.tar.gz"
-  sha256 "36428c6a6cb3088cad5a3592933385253da5f29f2effa61518ee5991ea38f833"
+  url "https://downloads.sourceforge.net/project/dfu-util/dfu-util-0.10.tar.gz"
+  sha256 "a03dc58dfc79c056819c0544b2a5970537566460102b3d82cfb038c60e619b42"
   license "GPL-2.0"
 
   livecheck do
