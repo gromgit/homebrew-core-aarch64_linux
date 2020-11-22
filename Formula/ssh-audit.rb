c7101a93082edf6a7daf6e788bc623533c33e053
FX Coudert <fxcoudert@gmail.com>
2020-11-22 22:21:28 +0000

    ssh-audit: update 2.3.1 bottle.

--- Formula/ssh-audit.rb
+++ Formula/ssh-audit.rb
@@ -10,6 +10,7 @@ class SshAudit < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "eb21c8795f1ff8146e521bf89085122d1c26fcd8231f146f520f6f3c11d81726" => :big_sur
     sha256 "6898d35256e2463dc6710f06133a29c07ca9f77b3e13f01671ce9e7a98a95278" => :catalina
     sha256 "227e07ecf11af9dc5a1b4a1a7017c390a3caa5183327e73dba8a2607c648a01d" => :mojave
     sha256 "cb1337c15074044b1dd7aa3a7026c7226bab0469dcee1ec0ed4eb960bc50dd4a" => :high_sierra
