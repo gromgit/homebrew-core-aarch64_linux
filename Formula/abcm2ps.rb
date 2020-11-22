f272d650847a5af99d19bd7d96ce84635dca8bf1
Rui Chen <rui@meetup.com>
2020-11-22 11:03:20 -0500

    abcm2ps 8.14.10
    * abcm2ps 8.14.10
    * abcm2ps: update license
    
    Closes #65421.
    
    Co-authored-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/abcm2ps.rb
+++ Formula/abcm2ps.rb
@@ -1,9 +1,9 @@
 class Abcm2ps < Formula
   desc "ABC music notation software"
   homepage "http://moinejf.free.fr"
-  url "https://github.com/leesavide/abcm2ps/archive/v8.14.9.tar.gz"
-  sha256 "9d91c215d59bb3704fe174ba0729f634e656ab42210cfabf02f0c5c4e0c9d877"
-  license "GPL-3.0"
+  url "https://github.com/leesavide/abcm2ps/archive/v8.14.10.tar.gz"
+  sha256 "5e6594ca9c677518d9f2683e46aee3e862dc0f872492eca8cad89e096203c829"
+  license "GPL-3.0-or-later"
 
   bottle do
     sha256 "0bce0f0d9fc971ebace359477b54809fa501e1c0c33b0adafc2fc69fa5a8903f" => :big_sur
