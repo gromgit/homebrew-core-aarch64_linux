b098db427c5918c613e968427f2474bf40615f08
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-22 13:13:07 +0000

    qpid-proton: update 0.33.0 bottle.

--- Formula/qpid-proton.rb
+++ Formula/qpid-proton.rb
@@ -13,10 +13,9 @@ class QpidProton < Formula
 
   bottle do
     cellar :any
-    sha256 "f36d96b5b1c8312c04b6ee484db491f9aefc05c0dbed095a3e429bc8d9ad8915" => :big_sur
-    sha256 "04bed9699fa8e399d7361dd79e2819780e43cc5a9d3d1d221f1085a644801e09" => :catalina
-    sha256 "a6fb688d4b98625e77f1886284ab795374dcba771504301c4114825e79084ef0" => :mojave
-    sha256 "754da3603d6c444bd392dcc94b28a1d8da76489dbf972638644889278b75932f" => :high_sierra
+    sha256 "8fbc4f2dd6d3da1b582d4ac681efbe6642556f5ac32f15ee9fc1519ffd2d00bd" => :big_sur
+    sha256 "95e0eee7ab97b025c67e28d3463eb903216820361a39e247683814a75c4f6ba7" => :catalina
+    sha256 "2322808f69519175c64b268a7ea75dceab4c3a8d3d898d4637a3dff066d2f69b" => :mojave
   end
 
   depends_on "cmake" => :build
