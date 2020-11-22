0e3c4ba6ae1dc9dab6a3434fceed1efe8696cd6b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-22 08:16:03 +0000

    goaccess: update 1.4.2 bottle.

--- Formula/goaccess.rb
+++ Formula/goaccess.rb
@@ -7,10 +7,9 @@ class Goaccess < Formula
   head "https://github.com/allinurl/goaccess.git"
 
   bottle do
-    sha256 "0b4c04afd1f6a57679c018d8df0d15c13104ad9aaa7ae189bb60895f63335e6e" => :big_sur
-    sha256 "4c595d0c97f0f73ca0135724e1fb270376803558e061cafb510aacd9b3c03bbf" => :catalina
-    sha256 "34b0e6c71bca1e06092f150ae7a71c7fb5a6dd4a1e9fa6a7bac9a4582d768154" => :mojave
-    sha256 "f8c7eea092fc381f163565cf8cb925ae84d2d2f26a3f06e8543b9a7672af8932" => :high_sierra
+    sha256 "d2a6c510116c165f4014324b88e0b83c088d758e9335574234cc175d85d652ac" => :big_sur
+    sha256 "4bdb574b0d364bf51a0e9835cf72f81d50c8ede165a99cb5699a848310babc7c" => :catalina
+    sha256 "99ee5b56fa892baa5c510c0adef0e3077967ac44a543db0a8c70c647401a94f6" => :mojave
   end
 
   depends_on "autoconf" => :build
