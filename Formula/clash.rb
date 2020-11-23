a57fdb62a4cd54d268bb266d309d15200bd179b9
h11 <me@h11.io>
2020-11-23 11:59:00 +0800

    clash 1.3.0 (new formula)
    * clash 1.3.0 (new formula)
    * clash: add proxy test
    * clash: check config
    
    Closes #65481.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/clash.rb
@@ -0,0 +1,80 @@
+class Clash < Formula
+  desc "Rule-based tunnel in Go"
+  homepage "https://github.com/Dreamacro/clash"
+  url "https://github.com/Dreamacro/clash/archive/v1.3.0.tar.gz"
+  sha256 "34a31e12eb7e639bdc2e19421fe3a8d9ad710e7d4c13bfc361c2023c9dfe04f6"
+  license "GPL-3.0-only"
+
+  depends_on "go" => :build
+  depends_on "shadowsocks-libev" => :test
+
+  def install
+    system "go", "build", *std_go_args
+  end
+
+  plist_options manual: "#{HOMEBREW_PREFIX}/opt/clash/bin/clash"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+            <array>
+                <string>#{opt_bin}/clash</string>
+            </array>
+            <key>RunAtLoad</key>
+            <true/>
+            <key>KeepAlive</key>
+            <true/>
+            <key>StandardOutPath</key>
+            <string>#{var}/log/clash.log</string>
+            <key>StandardErrorPath</key>
+            <string>#{var}/log/clash.log</string>
+          </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    ss_port = free_port
+    (testpath/"shadowsocks-libev.json").write <<~EOS
+      {
+          "server":"127.0.0.1",
+          "server_port":#{ss_port},
+          "password":"test",
+          "timeout":600,
+          "method":"chacha20-ietf-poly1305"
+      }
+    EOS
+    server = fork { exec "ss-server", "-c", testpath/"shadowsocks-libev.json" }
+
+    clash_port = free_port
+    (testpath/"config.yaml").write <<~EOS
+      mixed-port: #{clash_port}
+      mode: global
+      proxies:
+        - name: "server"
+          type: ss
+          server: 127.0.0.1
+          port: #{ss_port}
+          password: "test"
+          cipher: chacha20-ietf-poly1305
+    EOS
+    system "#{bin}/clash", "-t", "-d", testpath # test config && download Country.mmdb
+    client = fork { exec "#{bin}/clash", "-d", testpath }
+
+    sleep 3
+    begin
+      system "curl", "--socks5", "127.0.0.1:#{clash_port}", "github.com"
+    ensure
+      Process.kill 9, server
+      Process.wait server
+      Process.kill 9, client
+      Process.wait client
+    end
+  end
+end
