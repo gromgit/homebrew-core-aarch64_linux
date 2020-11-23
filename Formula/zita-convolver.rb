b70fcea78617f2fd337e5802dd6c10469874be25
Michka Popoff <michkapopoff@gmail.com>
2020-11-23 22:28:05 +0100

    zita-convolver: inreplace only on mac
    and fix license

--- Formula/zita-convolver.rb
+++ Formula/zita-convolver.rb
@@ -3,7 +3,7 @@ class ZitaConvolver < Formula
   homepage "https://kokkinizita.linuxaudio.org/linuxaudio/"
   url "https://kokkinizita.linuxaudio.org/linuxaudio/downloads/zita-convolver-4.0.3.tar.bz2"
   sha256 "9aa11484fb30b4e6ef00c8a3281eebcfad9221e3937b1beb5fe21b748d89325f"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
 
   livecheck do
     url "https://kokkinizita.linuxaudio.org/linuxaudio/downloads/index.html"
@@ -22,7 +22,9 @@ class ZitaConvolver < Formula
 
   def install
     cd "source" do
-      inreplace "Makefile", "-Wl,-soname,", "-Wl,-install_name,"
+      on_macos do
+        inreplace "Makefile", "-Wl,-soname,", "-Wl,-install_name,"
+      end
       inreplace "Makefile", "ldconfig", "ln -sf $(ZITA-CONVOLVER_MIN) $(DESTDIR)$(LIBDIR)/$(ZITA-CONVOLVER_MAJ)"
       system "make", "install", "PREFIX=#{prefix}", "SUFFIX="
     end
