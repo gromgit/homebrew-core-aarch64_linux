9022e182a84b97efe9228d199b64a03a51a8ceba
chenrui <chenrui333@gmail.com>
2020-11-23 16:38:51 +0000

    glooctl 1.5.12
    Closes #65514.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -2,8 +2,8 @@ class Glooctl < Formula
   desc "Envoy-Powered API Gateway"
   homepage "https://docs.solo.io/gloo/latest/"
   url "https://github.com/solo-io/gloo.git",
-      tag:      "v1.5.11",
-      revision: "fd499e26fef3928cc8d9be5fc9abcf5268d75ad9"
+      tag:      "v1.5.12",
+      revision: "7d0a61b34c31cbafbc00caff385cc8935dd996fd"
   license "Apache-2.0"
   head "https://github.com/solo-io/gloo.git"
 
