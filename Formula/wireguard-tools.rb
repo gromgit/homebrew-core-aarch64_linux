06ea58894fed24a85de52d991438ef63413c4e60
FX Coudert <fxcoudert@gmail.com>
2020-11-23 00:03:47 +0000

    wireguard-tools: update 1.0.20200827 bottle.

--- Formula/wireguard-tools.rb
+++ Formula/wireguard-tools.rb
@@ -13,6 +13,7 @@ class WireguardTools < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "007564c0016fce46841d7d28f907fc0c9ffa7480f95c846608a689c7f288600e" => :big_sur
     sha256 "45e84d6fd3efe601ecb6c959a356169908bd12aa2aaa42122663619c47c02e4e" => :catalina
     sha256 "7c53423bac89aef0a7a521f04707961bf4416925edb3022d0bfc839f345f991b" => :mojave
     sha256 "7041f9c62ee72513ec0eb67ec1b240ce4be95c771aeecb9302fed746c0029dfc" => :high_sierra
