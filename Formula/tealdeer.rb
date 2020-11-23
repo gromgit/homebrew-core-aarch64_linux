56c13defb63c92001c92bb804374ff6349b80a3f
Michka Popoff <michkapopoff@gmail.com>
2020-11-23 22:51:12 +0100

    tealdeer: set OPENSSL_DIR on Linux

--- Formula/tealdeer.rb
+++ Formula/tealdeer.rb
@@ -23,6 +23,9 @@ class Tealdeer < Formula
   conflicts_with "tldr", because: "both install `tldr` binaries"
 
   def install
+    on_linux do
+      ENV["OPENSSL_DIR"] = Formula["openssl@1.1"].opt_prefix
+    end
     system "cargo", "install", *std_cargo_args
     bash_completion.install "bash_tealdeer" => "tldr"
     zsh_completion.install "zsh_tealdeer" => "_tldr"
