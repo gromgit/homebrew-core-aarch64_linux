44695c67789b5a5076cfa9461bb40b0046f44c37
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-23 08:17:00 +0000

    mecab-ko-dic: update 2.1.1-20180720 bottle.

--- Formula/mecab-ko-dic.rb
+++ Formula/mecab-ko-dic.rb
@@ -12,14 +12,9 @@ class MecabKoDic < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "095bb6b6936fb71be5ec8247aaae525cd8f506896b4e9940a6dbba0a262a8ad4" => :big_sur
-    sha256 "5c3d0709fbddf74067362680d226f5ecf170ee903532f8d34399b731d188a13d" => :catalina
-    sha256 "ce481dc10cc5b42ba1aeb863e3fdec5edd69dbf80eb3636a30adbf088c4d0508" => :mojave
-    sha256 "6e7d71dc788c552a2d2e345ff67aad4a6f5c9078eb776056ccf96dd599c63f52" => :high_sierra
-    sha256 "68715d65e93b56fa18f70579b515b4e69128777e89c069da9af9ab6dd689bc9e" => :sierra
-    sha256 "51c5a40a0aad7906cbd83265fbecbc4de3a4f116abceebf9fdb02d17c75f5f69" => :el_capitan
-    sha256 "92be006bcc8552fdaddf82d21b9f8f528af010128febc721a5a5ba262eca99ce" => :yosemite
-    sha256 "0c958bf826cd358431f144dfb3d2d3da08c67cda59efe1d2998b54a401678515" => :mavericks
+    sha256 "ab72fcbb7b1e0bc1ec8667a2d183ad5beab66279d27486ef1ae241d4114fddd1" => :big_sur
+    sha256 "02f67f9bd82e7310074c4c47097bcb4244c79211af9736db8fa73861dbbb820d" => :catalina
+    sha256 "8d9c37045d060855f558ef8706cee66e918e553ff5c8893811e5cf78767893cb" => :mojave
   end
 
   depends_on "autoconf" => :build
