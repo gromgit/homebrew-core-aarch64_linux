89a60f29c9e691d36424593cfbd16e0237d27c89
Mike McQuaid <mike@mikemcquaid.com>
2020-11-23 16:05:27 +0000

    groovyserv: migrate to openjdk@8.
    Needed for https://github.com/Homebrew/brew/pull/9209.

--- Formula/groovyserv.rb
+++ Formula/groovyserv.rb
@@ -21,7 +21,7 @@ class Groovyserv < Formula
 
   depends_on "go" => :build
   depends_on "groovy"
-  depends_on java: "1.8"
+  depends_on "openjdk@8"
 
   def install
     # Sandbox fix to stop it ignoring our temporary $HOME variable.
