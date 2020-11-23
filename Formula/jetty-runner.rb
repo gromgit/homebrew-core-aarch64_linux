293ac399e540c51d4bb97a7fadcb12778407045a
Jaan Jänesmäe <git@janesmae.com>
2020-11-23 21:23:53 +0200

    jetty-runner 9.4.35.v20201120 (#65528)

--- Formula/jetty-runner.rb
+++ Formula/jetty-runner.rb
@@ -1,9 +1,9 @@
 class JettyRunner < Formula
   desc "Use Jetty without an installed distribution"
   homepage "https://www.eclipse.org/jetty/"
-  url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-runner/9.4.34.v20201102/jetty-runner-9.4.34.v20201102.jar"
-  version "9.4.34.v20201102"
-  sha256 "84f90692d6cddf56487ab40a52fbab27f8af9f6485e6a6c83fc7bad602cf7fa0"
+  url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-runner/9.4.35.v20201120/jetty-runner-9.4.35.v20201120.jar"
+  version "9.4.35.v20201120"
+  sha256 "3cc3aa790df98e8d8fbfe68244fc04150242fa9056972de559f0047e97730103"
   license any_of: ["Apache-2.0", "EPL-1.0"]
 
   livecheck do
