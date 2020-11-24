9f822143ea8bcb62b1d3289cd7ec10e358006af1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-24 08:17:20 +0000

    rclone: update 1.53.2 bottle.

--- Formula/rclone.rb
+++ Formula/rclone.rb
@@ -8,9 +8,10 @@ class Rclone < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3b50cc675b356f43b62f4f5a8acef082a56163ddf1dfdf03a4f360ee6969cdfe" => :catalina
-    sha256 "a59703acea34bb4bff3fcd878af83d425a993fc1012b35b7a4210066e6279eab" => :mojave
-    sha256 "cea1a5bf6e0346731ba8357e313ae6eb3633e14400c2e9e67bd5a3f8524721f6" => :high_sierra
+    rebuild 1
+    sha256 "4346e37a24fad485a91e2a5ac7a083cd191f64cbe5ec220f51ed019127329e87" => :big_sur
+    sha256 "9591cd57d0491adf64e65218b87f123ffcb46ccc1281baf53c35f773080805a2" => :catalina
+    sha256 "acc6f0a73e589b51f82b7e0bb4ec8d09e36b020031ff59ed8a2e99956c64cd19" => :mojave
   end
 
   depends_on "go" => :build
