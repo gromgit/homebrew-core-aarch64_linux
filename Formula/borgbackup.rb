89785cf123181d177c59e8a7421796d4290db1b3
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-11-24 17:38:55 +0100

    borgbackup: do not depend on FUSE
    Closes #65603.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/borgbackup.rb
+++ Formula/borgbackup.rb
@@ -6,7 +6,7 @@ class Borgbackup < Formula
   url "https://github.com/borgbackup/borg/releases/download/1.1.14/borgbackup-1.1.14.tar.gz"
   sha256 "7dbb0747cc948673f695cd6de284af215f810fed2eb2a615ef26ddc7c691edba"
   license "BSD-3-Clause"
-  revision 1
+  revision 2
 
   livecheck do
     url "https://github.com/borgbackup/borg/releases/latest"
@@ -20,9 +20,6 @@ class Borgbackup < Formula
     sha256 "572afe4f34d36ae32c0ec48da8d4a2e9cc59b4ad891f884e5a1efe48e69c478c" => :high_sierra
   end
 
-  deprecate! because: "requires FUSE"
-
-  depends_on osxfuse: :build
   depends_on "pkg-config" => :build
   depends_on "libb2"
   depends_on "lz4"
@@ -30,20 +27,8 @@ class Borgbackup < Formula
   depends_on "python@3.9"
   depends_on "zstd"
 
-  resource "llfuse" do
-    url "https://files.pythonhosted.org/packages/8f/73/d35aaf5f650250756b40c1e718ee6a2d552700729476dee24c9837608e1b/llfuse-1.3.8.tar.gz"
-    sha256 "b9b573108a840fbaa5c8f037160cc541f21b8cbdc15c5c8a39d5ac8c1b6c4cbc"
-  end
-
   def install
     virtualenv_install_with_resources
-  end
-
-  def caveats
-    <<~EOS
-      To use `borg mount`, install osxfuse with Homebrew Cask:
-        brew cask install osxfuse
-    EOS
   end
 
   test do
