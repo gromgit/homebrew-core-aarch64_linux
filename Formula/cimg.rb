bc66b6b27eab1d997c14b29b5d4bfe3143935c50
mprince <marcel.r.prince@gmail.com>
2020-11-24 04:25:35 -0800

    cimg 2.9.4
    Closes #65589.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cimg.rb
+++ Formula/cimg.rb
@@ -1,8 +1,9 @@
 class Cimg < Formula
   desc "C++ toolkit for image processing"
   homepage "https://cimg.eu/"
-  url "https://cimg.eu/files/CImg_2.9.3.zip"
-  sha256 "02d0b62cbb750c285d001a62357a7c7219e4a70dabafedb31410bb993e044e9e"
+  url "https://cimg.eu/files/CImg_2.9.4.zip"
+  sha256 "455945dc035d50bbc042450e2dc81b2ca19ea74cd3bc38b46ac623df6997dfff"
+  license "CECILL-2.0"
 
   livecheck do
     url "https://cimg.eu/files/"
