a68bfd3b605d5cc302fa1b77f5a4ef5c1eae2b48
Callum Waters <cmwaters19@gmail.com>
2020-11-25 11:13:47 +0100

    tendermint 0.34.0 (new formula)
    * tendermint 0.34 (new formula)
    * use a better test and fix description
    * fix syntax
    * shorten description
    * simplify version assignment
    * use testing dir instead of tmp
    * fix mistake with assertions
    
    Closes #65645.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

---
+++ Formula/tendermint.rb
@@ -0,0 +1,23 @@
+class Tendermint < Formula
+  desc "BFT state machine replication for applications in any programming languages"
+  homepage "https://tendermint.com/"
+  url "https://github.com/tendermint/tendermint/archive/v0.34.0.tar.gz"
+  sha256 "3a28fac4c5e610fc32763db1b717ec0a1e12d39262e321d6da223c3b0acfea7f"
+  license "Apache-2.0"
+  head "https://github.com/tendermint/tendermint.git"
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "build", "VERSION=#{version}"
+    bin.install "build/tendermint"
+  end
+
+  test do
+    mkdir(testpath/"staging")
+    shell_output("#{bin}/tendermint init --home #{testpath}/staging")
+    assert_true File.exist?(testpath/"staging/config/genesis.json")
+    assert_true File.exist?(testpath/"staging/config/config.toml")
+    assert_true Dir.exist?(testpath/"staging/data")
+  end
+end
