1b880b792dc13ab9edf9e64cf82c7fa7062810d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-25 09:15:35 +0000

    libmpeg2: update 0.5.1 bottle.

--- Formula/libmpeg2.rb
+++ Formula/libmpeg2.rb
@@ -7,15 +7,10 @@ class Libmpeg2 < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "6330060737ed552d7800b1e88b63d3e31e5c2f3515e4406f1fe00370fcb0a4f0" => :big_sur
-    sha256 "248128226f4e6649a0aabec67dfb20f5b105a58b194c0e5830b5e13caf3b9421" => :catalina
-    sha256 "5b6d4bf134cee2877da39a05a5c918f870c9742fe5823d9ad8b61b013fc7a7f2" => :mojave
-    sha256 "bdd9339459f1a039f480f3c31730265457bf6736ab625359482eb4e3796e455c" => :high_sierra
-    sha256 "4454cdb2561326d53d7680fb004a13a54674b9cabd619cc9a5fede2742c9da9e" => :sierra
-    sha256 "841e93dd99b97b96b475aedff29b58f5be5c4156869b1c0212e5d7ed8dd7f481" => :el_capitan
-    sha256 "3d07c45554ff34036b9eae5a31dc5417c15109ba134d414035b1bf6f9dda7c79" => :yosemite
-    sha256 "f6a868beb10fbf84d3eb1af556478ecbfb238d28608a53b99e607c02910e5e49" => :mavericks
+    rebuild 2
+    sha256 "9f2cfd80d47e975333747fdea41d336071282ae359e9a345835a70611467bd43" => :big_sur
+    sha256 "9a8c812495f38eb0d46bff246c632c5dfd97413b2bc949defd9c5d318b9da439" => :catalina
+    sha256 "81161223100cfa38704d3194519be5651f4fcb47765b7e99f1d53ce05e433142" => :mojave
   end
 
   depends_on "autoconf" => :build
