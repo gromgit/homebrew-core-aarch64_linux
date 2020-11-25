46d43d94957f433f798a0ee8aed5c97df4d36ed1
Michka Popoff <michkapopoff@gmail.com>
2020-11-25 18:58:14 +0100

    hello: revision for Linux build and bottle upload test
    Closes #65664.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -4,6 +4,7 @@ class Hello < Formula
   url "https://ftp.gnu.org/gnu/hello/hello-2.10.tar.gz"
   sha256 "31e066137a962676e89f69d1b65382de95a7ef7d914b8cb956f41ea72e0f516b"
   license "GPL-3.0-or-later"
+  revision 1
 
   livecheck do
     url :stable
