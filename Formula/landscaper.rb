62ce443a099b2d94e0011b8f886a9564eb3660a5
FX Coudert <fxcoudert@gmail.com>
2020-11-25 18:46:57 +0000

    landscaper: update 1.0.24_1 bottle.

--- Formula/landscaper.rb
+++ Formula/landscaper.rb
@@ -10,6 +10,7 @@ class Landscaper < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "bad7cf082826c5d92dd8c09a79b682e1582fcfc3f4e471dde4112393ec7095ce" => :big_sur
     sha256 "74decffaf180e0e0dd9bfa2312877da01443a3418afe0f485c1b655c4af1da41" => :catalina
     sha256 "ff82cdb7be6329f9a4a5ce34bcbb04bc9356ab46fa3ecd30b830cf35df268529" => :mojave
     sha256 "68302c1748fe4eb063855df24420a8681a54b8ce484f2e030616bd4c4a812d52" => :high_sierra
