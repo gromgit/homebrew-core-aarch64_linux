c6b9598a23f5c4f9c8f4da8f025b71c46600029e
Rui Chen <rui@meetup.com>
2020-11-26 13:19:06 -0500

    glances 3.1.5
    * glances 3.1.5
    * update psutil
    * update sha
    
    Closes #59882.
    
    Signed-off-by: Rui Chen <rui@meetup.com>
    Co-authored-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/glances.rb
+++ Formula/glances.rb
@@ -1,10 +1,9 @@
 class Glances < Formula
   desc "Alternative to top/htop"
   homepage "https://nicolargo.github.io/glances/"
-  url "https://github.com/nicolargo/glances/archive/v3.1.4.1.tar.gz"
-  sha256 "0347a0b949451fd0022c0f22e54092fe526120a776af1f2bde1ea7ba61d6b792"
-  license "LGPL-3.0"
-  revision 1
+  url "https://github.com/nicolargo/glances/archive/v3.1.5.tar.gz"
+  sha256 "e60ea1274e882f9d64d16c12690bd71dcc70ebe7e93690d773275f92fa126b24"
+  license "LGPL-3.0-or-later"
 
   bottle do
     cellar :any_skip_relocation
@@ -132,8 +131,8 @@ class Glances < Formula
   end
 
   resource "psutil" do
-    url "https://files.pythonhosted.org/packages/c4/b8/3512f0e93e0db23a71d82485ba256071ebef99b227351f0f5540f744af41/psutil-5.7.0.tar.gz"
-    sha256 "685ec16ca14d079455892f25bd124df26ff9137664af445563c1bd36629b5e0e"
+    url "https://files.pythonhosted.org/packages/aa/3e/d18f2c04cf2b528e18515999b0c8e698c136db78f62df34eee89cee205f1/psutil-5.7.2.tar.gz"
+    sha256 "90990af1c3c67195c44c9a889184f84f5b2320dce3ee3acbd054e3ba0b4a7beb"
   end
 
   resource "py-cpuinfo" do
