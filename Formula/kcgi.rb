c7ffb252c1b40abb8a298421644e5062a1b0b4b5
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-11-26 12:32:35 +0000

    kcgi: add 0.12.2 bottle.

--- Formula/kcgi.rb
+++ Formula/kcgi.rb
@@ -5,6 +5,13 @@ class Kcgi < Formula
   sha256 "59e9219ae439f0c4fbffe6584014715c2fc206b8cd00de7aa8d062ddb52c2a0e"
   license "ISC"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "db56ea0e40697618474f90af5b3f354043027b837e94464650a81d3813cde3d9" => :big_sur
+    sha256 "a3190c30830f6667c7f6df30047610e12845835f3d16c647b3c223e7ec8033db" => :catalina
+    sha256 "096b70bd684486a61c8c393aea6596268701807e27b41da94c0e5e996bfc7b3c" => :mojave
+  end
+
   depends_on "bmake" => :build
 
   def install
