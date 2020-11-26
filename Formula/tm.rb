bb20a907fa7baa2540e27b4d997013a27ac272c8
Sameer Naik <sameer@triggermesh.com>
2020-11-26 16:07:44 +0530

    tm 1.0.0 (new formula)
    * tm 1.0.0 (new formula)
    * some updates
    
    Closes #65701.
    
    Signed-off-by: Sameer Naik <sameer@triggermesh.com>
    Co-authored-by: Rui Chen <rui@meetup.com>
    Signed-off-by: chenrui <chenrui333@gmail.com>

---
+++ Formula/tm.rb
@@ -0,0 +1,84 @@
+class Tm < Formula
+  desc "TriggerMesh CLI to work with knative objects"
+  homepage "https://triggermesh.com"
+  url "https://github.com/triggermesh/tm/archive/v1.0.0.tar.gz"
+  sha256 "cca47e3a51bf0fcaada1e4afe03f4eb6aa63e75a7819fd7e8cae1c91e5d939b4"
+  license "Apache-2.0"
+  head "https://github.com/triggermesh/tm.git"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/triggermesh/tm/cmd.version=v#{version}
+    ]
+
+    system "go", "build", *std_go_args, "-ldflags", ldflags.join(" ")
+  end
+
+  test do
+    (testpath/"kubeconfig").write <<~EOS
+      apiVersion: v1
+      clusters:
+      - cluster:
+          certificate-authority-data: test
+          server: http://127.0.0.1:8080
+        name: test
+      contexts:
+      - context:
+          cluster: test
+          user: test
+        name: test
+      current-context: test
+      kind: Config
+      preferences: {}
+      users:
+      - name: test
+        user:
+          token: test
+    EOS
+
+    ENV["KUBECONFIG"] = testpath/"kubeconfig"
+
+    # version
+    version_output = shell_output("#{bin}/tm version")
+    assert_match "Triggermesh CLI, version v#{version}", version_output
+
+    # node
+    system "#{bin}/tm", "generate", "node", "foo-node"
+    assert_predicate testpath/"foo-node/serverless.yaml", :exist?
+    assert_predicate testpath/"foo-node/handler.js", :exist?
+
+    runtime = "https://raw.githubusercontent.com/triggermesh/knative-lambda-runtime/master/node10/runtime.yaml"
+    yaml = File.read("foo-node/serverless.yaml")
+    assert_match "runtime: #{runtime}", yaml
+
+    # python
+    system "#{bin}/tm", "generate", "python", "foo-python"
+    assert_predicate testpath/"foo-python/serverless.yaml", :exist?
+    assert_predicate testpath/"foo-python/handler.py", :exist?
+
+    runtime = "https://raw.githubusercontent.com/triggermesh/knative-lambda-runtime/master/python37/runtime.yaml"
+    yaml = File.read("foo-python/serverless.yaml")
+    assert_match "runtime: #{runtime}", yaml
+
+    # go
+    system "#{bin}/tm", "generate", "go", "foo-go"
+    assert_predicate testpath/"foo-go/serverless.yaml", :exist?
+    assert_predicate testpath/"foo-go/main.go", :exist?
+
+    runtime = "https://raw.githubusercontent.com/triggermesh/knative-lambda-runtime/master/go/runtime.yaml"
+    yaml = File.read("foo-go/serverless.yaml")
+    assert_match "runtime: #{runtime}", yaml
+
+    # ruby
+    system "#{bin}/tm", "generate", "ruby", "foo-ruby"
+    assert_predicate testpath/"foo-ruby/serverless.yaml", :exist?
+    assert_predicate testpath/"foo-ruby/handler.rb", :exist?
+
+    runtime = "https://raw.githubusercontent.com/triggermesh/knative-lambda-runtime/master/ruby25/runtime.yaml"
+    yaml = File.read("foo-ruby/serverless.yaml")
+    assert_match "runtime: #{runtime}", yaml
+  end
+end
