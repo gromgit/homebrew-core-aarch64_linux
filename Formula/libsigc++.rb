61d7227297ee91652ec8602e78a0ab76342ca457
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-11-26 06:31:00 +0000

    libsigc++ 3.0.6
    Closes #65691.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libsigc++.rb
+++ Formula/libsigc++.rb
@@ -1,8 +1,8 @@
 class Libsigcxx < Formula
   desc "Callback framework for C++"
   homepage "https://libsigcplusplus.github.io/libsigcplusplus/"
-  url "https://download.gnome.org/sources/libsigc++/3.0/libsigc++-3.0.4.tar.xz"
-  sha256 "a3a37410186379df1908957e7aba7519bdcf5bcc8ed70ee8dfea9362c393d545"
+  url "https://download.gnome.org/sources/libsigc++/3.0/libsigc++-3.0.6.tar.xz"
+  sha256 "b70edcf4611651c54a426e109b17196e1fa17da090592a5000e2d134c03ac5ce"
   license "LGPL-3.0-or-later"
 
   livecheck do
@@ -26,7 +26,7 @@ class Libsigcxx < Formula
     ENV.cxx11
 
     mkdir "build" do
-      system "meson", *std_meson_args, "-Dintrospection=enabled", ".."
+      system "meson", *std_meson_args, ".."
       system "ninja"
       system "ninja", "install"
     end
