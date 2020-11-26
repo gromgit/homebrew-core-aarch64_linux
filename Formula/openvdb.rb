c1bc3938e15b79318890880deeb3db9567615692
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-26 13:14:29 +0000

    openvdb: update 7.0.0_2 bottle.

--- Formula/openvdb.rb
+++ Formula/openvdb.rb
@@ -8,10 +8,10 @@ class Openvdb < Formula
   head "https://github.com/AcademySoftwareFoundation/openvdb.git"
 
   bottle do
-    sha256 "e05fd1f0a7e1fa4c2cb9326f31ad58479102867447c2403f397ab6955c1375b8" => :big_sur
-    sha256 "a52e3f045f00380b3b3a0246d36e6f084b197df696c8b229e83772bda2ed3017" => :catalina
-    sha256 "b55dd56f20ba824ad2daa83d4ed41d656911e8b596e45f39d2ed953f04a3ac79" => :mojave
-    sha256 "a7d3e9cc992f2699b1dea70a51dbd9333b423113a1c8ca819265bbbb0df0d3ca" => :high_sierra
+    rebuild 1
+    sha256 "9b19f5e3486376fa6ec29d40f4b6517725d377320674d5ddb5ec26c4cfc1bb3e" => :big_sur
+    sha256 "4259cbdb907f5190f19ef9aa14cea92e68f94513a5ab37c10f7c5f81f51bfaf9" => :catalina
+    sha256 "bd7827344942ebbc393beaecec6e58d55e2ab5d4ba7204deaf5f5aeeba4f4de2" => :mojave
   end
 
   depends_on "cmake" => :build
