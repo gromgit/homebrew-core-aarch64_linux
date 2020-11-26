d4fd09fdef46586fc1401f751d310d8c6c9c14ab
Rui Chen <rui@meetup.com>
2020-11-26 17:56:17 -0500

    grafana 7.3.4
    Closes #65719.
    
    Signed-off-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/grafana.rb
+++ Formula/grafana.rb
@@ -1,8 +1,8 @@
 class Grafana < Formula
   desc "Gorgeous metric visualizations and dashboards for timeseries databases"
   homepage "https://grafana.com"
-  url "https://github.com/grafana/grafana/archive/v7.3.3.tar.gz"
-  sha256 "aa3011b4d2199e2216e754f562b5d7657339832869bd5cc8c136698df650d310"
+  url "https://github.com/grafana/grafana/archive/v7.3.4.tar.gz"
+  sha256 "4fcb46e1ab6e29b4384b401d5fd83303d8683d1d44b6a2e7bb6193abe283636d"
   license "Apache-2.0"
   head "https://github.com/grafana/grafana.git"
 
