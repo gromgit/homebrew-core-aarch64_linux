07a16933664c9fc309fed624aaab87f9c0270a4e
chenrui <chenrui333@gmail.com>
2020-11-27 02:27:07 +0000

    mafft: update 7.475 bottle.

--- Formula/mafft.rb
+++ Formula/mafft.rb
@@ -13,10 +13,9 @@ class Mafft < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c77a7f7d15ad593abfb323f568cd2a940b16e6e5c8fb4d9fd63a824967117505" => :big_sur
-    sha256 "47030809ef8372782fbf5770b2b95057f31bcbf81ead53cb06cb09e2ecbd2f87" => :catalina
-    sha256 "f7820f8386beaa0b0bfcfccc75bea1563d3b5947e32a0aba5051edb621491026" => :mojave
-    sha256 "a194f9f065024b846417688c2da31c22c058598880c4fb8ef5369e10299ecf2f" => :high_sierra
+    sha256 "cce002802d3d258caceaa2a20ecbdef0a75d14344f2aa7aa89aff5313a079c3e" => :big_sur
+    sha256 "798be41afbf9531ec0589c935a43e910cca42d66b25eb931d914324a1bfb1ea4" => :catalina
+    sha256 "5f791516aeaff07e97ebd9966d7321f13436f99a6a232618e2ee2aface522377" => :mojave
   end
 
   def install
