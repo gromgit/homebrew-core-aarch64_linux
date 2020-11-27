46d734aeddc8f0c87eff58c1af973fd8f392a254
chenrui <chenrui333@gmail.com>
2020-11-26 20:01:08 -0500

    mill 0.9.3 (#65728)

--- Formula/mill.rb
+++ Formula/mill.rb
@@ -1,8 +1,8 @@
 class Mill < Formula
   desc "Scala build tool"
   homepage "https://www.lihaoyi.com/mill/"
-  url "https://github.com/lihaoyi/mill/releases/download/0.8.0/0.8.0"
-  sha256 "8162160264399d79b6db9f08ab4caca98166ab1319743da18002afeca339ee12"
+  url "https://github.com/lihaoyi/mill/releases/download/0.9.3/0.9.3"
+  sha256 "e738a1548a668a3b40a767b286513b7e1867b6905ac5cb0c3fd1db5f2adb3575"
   license "MIT"
 
   bottle :unneeded
