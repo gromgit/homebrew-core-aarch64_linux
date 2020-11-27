362ac67694e7019ae56e13be3c8ec98fadd2833b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-27 02:17:22 +0000

    git-hooks-go: update 1.3.1 bottle.

--- Formula/git-hooks-go.rb
+++ Formula/git-hooks-go.rb
@@ -8,10 +8,9 @@ class GitHooksGo < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6779e8aac5ff5f0030415d13df0b1ae2bf6349683ad6926b63d1b5c17e516bac" => :big_sur
-    sha256 "367a0e8d166a30749447132c88d5bf1c43fc8ab7c4316c91a990c13d4e887c32" => :catalina
-    sha256 "b61330cf67d4b8a572bb2f7a22434a00bb74d85bb93254ff6a60e8d3c8f12877" => :mojave
-    sha256 "95786772c28deeaaa6c979f93174e1f49bd6dd8370e8927861f6b950dd5b3910" => :high_sierra
+    sha256 "bb65c1d92db2e31b8d3d2447e3c4642a1865658f9d8075a381439ca311b2ddde" => :big_sur
+    sha256 "c297503f6623a3c258c84a887225f3690433a16e97492f7071cc0c3ebee0d073" => :catalina
+    sha256 "c5323401f5a7f37a895c9b7b579f10e75fccf0f83ba9fa4bfba4782cebeedbb1" => :mojave
   end
 
   depends_on "go" => :build
