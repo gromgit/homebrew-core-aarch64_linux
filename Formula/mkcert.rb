291df357494427ed41ac0e11169bc6574eb0e41e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-27 01:28:33 +0000

    mkcert: update 1.4.3 bottle.

--- Formula/mkcert.rb
+++ Formula/mkcert.rb
@@ -7,10 +7,9 @@ class Mkcert < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "a0023269070477df87289064633f853364647506073d1c27eaca3559e7397bf2" => :big_sur
-    sha256 "5dc54c06caf0211bc7819f2bcfeb845413e1ef0ddab54df5eed93fc0be27212e" => :catalina
-    sha256 "2c6ad2f96bdd13b45a69c4fe204bcec5c7194d2dc3a48b88654164cafc47ec67" => :mojave
-    sha256 "041291da4a29bd7a44cd1c727079efa5af699d6e90f088cdd11abfe347a86d2f" => :high_sierra
+    sha256 "aae4b584f5408d6b17b164fa002c2385ec7ca285f10d832f78b2230ae7b79ed3" => :big_sur
+    sha256 "f53f22e6ddf746c4efb7f8c3c595f143fb60b2134d4cd18976650bd0ad9748ff" => :catalina
+    sha256 "20c5690b998ab1af2ca387ed84b54dcab9488e031538563704fd02c7dcee3589" => :mojave
   end
 
   depends_on "go" => :build
