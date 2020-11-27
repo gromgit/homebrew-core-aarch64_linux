5ac4827e2eb99e52aac0e371cde06b011dfcd3fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-27 02:17:25 +0000

    glow: update 1.2.1 bottle.

--- Formula/glow.rb
+++ Formula/glow.rb
@@ -7,10 +7,9 @@ class Glow < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "c36ed9de017a0f0c0f5a70f7c33fd5387b6f3ccdee14399f1c7b445a3ca9d3fa" => :big_sur
-    sha256 "64bcae8e6372b6db077687eabdfb358e7393403fc7229d430941e352d6151df9" => :catalina
-    sha256 "a5e7dddbdde2fa4dd35db661559eef21cfc8eaf98234912605cd91cbfb1f3671" => :mojave
+    sha256 "bec804d3c8ff8ff3dbd7d265ad39a213cb90173dead7d81d15f8f606ed03e15d" => :big_sur
+    sha256 "f55176ddc5b1f40503ad9b2599c5474e58a8e806dc2a33c7bd54375f110ad865" => :catalina
+    sha256 "bec57a12c75182f65427aceabed7be270be7890e37794429780791f80ac7fd85" => :mojave
   end
 
   depends_on "go" => :build
