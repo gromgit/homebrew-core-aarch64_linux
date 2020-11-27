475a5bbb60dc8a8eb03e255e61b2deeef05113c3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-27 13:14:16 +0000

    goad: update 2.0.4 bottle.

--- Formula/goad.rb
+++ Formula/goad.rb
@@ -8,12 +8,10 @@ class Goad < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "57345918587236f5235fcacc3d6d7f64272a5c1def4e64632fee13b960c027b5" => :catalina
-    sha256 "366aad32eaa55e8561244bc31dc7650d829dd1f588801ad5d986a22cbfc80418" => :mojave
-    sha256 "3a87755d9b4b48b07a7fecd9be72df574d5866fe39d4dc0c60d6a88da40c5081" => :high_sierra
-    sha256 "b10fb1177f1f3548b2a497cb56ce9f10620a06175adec6a1d452b0e136981d1e" => :sierra
-    sha256 "f0c6400909fbe194400717025ad64cba2ccd63b1db9cba5ec7a9195274614b41" => :el_capitan
+    rebuild 2
+    sha256 "0f576b680ef04f935c3af7fd7e1ababdfd8b073659594ad81243018ef3b6cc76" => :big_sur
+    sha256 "89367dad83660f1fc7deae319233bc4b554b92bb0faf406d14ff5145d70226d3" => :catalina
+    sha256 "9f491e354dc372c864fa2ea747ec3f514071b5fe0ad5f2649818c1e788ce97d8" => :mojave
   end
 
   deprecate! because: :repo_archived
