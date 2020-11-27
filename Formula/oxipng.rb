66acf192a33ac1cb82852c5b2c22ff672df8ee76
Rui Chen <rui@meetup.com>
2020-11-27 15:21:39 -0500

    oxipng 4.0.2
    Closes #65819.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/oxipng.rb
+++ Formula/oxipng.rb
@@ -1,8 +1,8 @@
 class Oxipng < Formula
   desc "Multithreaded PNG optimizer written in Rust"
   homepage "https://github.com/shssoichiro/oxipng"
-  url "https://github.com/shssoichiro/oxipng/archive/v4.0.1.tar.gz"
-  sha256 "c5684730757a49a55c3bac66de139da5a1fdcd190f8ea3b9a95e1fc8feccb106"
+  url "https://github.com/shssoichiro/oxipng/archive/v4.0.2.tar.gz"
+  sha256 "f540b788ac761ba20099c6710c3276dfdab9119906c1e9834b7648c0b633a000"
   license "MIT"
 
   bottle do
