ebee391f79a5f87c6cb8ec445b82e57c0884d642
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-28 14:16:21 +0000

    dmagnetic: update 0.28 bottle.

--- Formula/dmagnetic.rb
+++ Formula/dmagnetic.rb
@@ -7,10 +7,9 @@ class Dmagnetic < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5e7cc2cd1e773b273acbc10bf8ab40b398a56e2a95e4c47981bc4ec5181d1525" => :big_sur
-    sha256 "781fbe7020403897458658ee63c4854bc17fcd947750343c5806f4f275b56fec" => :catalina
-    sha256 "d6a8d0fbb621e7c582aefa8565e278388c8cd4a893e6d9d35a881c5abb42d9bd" => :mojave
-    sha256 "9cde03055d52999bccc3266762deada55191f4f73481cd64bce91377e7490ec6" => :high_sierra
+    sha256 "4d6970e081501b287f27119eeab1d5afdebacfdeb416d4f3e920cd3ad86e1e09" => :big_sur
+    sha256 "f3198b2f700c0265ea488cb003bd0eebb17ef2438627a7d4afdc1317186c7d75" => :catalina
+    sha256 "164d9398a64684d8a81540ace3a022a178ef0802d62ee18372900130445c1104" => :mojave
   end
 
   def install
