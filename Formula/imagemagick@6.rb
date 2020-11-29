6c0c2dd9b5b6171cd84d132809d94ee295026e13
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-29 03:16:20 +0000

    imagemagick@6: update 6.9.11-44 bottle.

--- Formula/imagemagick@6.rb
+++ Formula/imagemagick@6.rb
@@ -16,9 +16,9 @@ class ImagemagickAT6 < Formula
   end
 
   bottle do
-    sha256 "ee4c6ccf051999e8f2584a678726d19cdc62edd9c3e024304695dbe95d721d4a" => :big_sur
-    sha256 "83cbe75bcf416d9da5ff3d633954895c2e4ba5e3efee3c10bb2a03be91e37005" => :catalina
-    sha256 "8610c1bac4f2a58eba2c542e249fd215367e6d7666cb710ace334b4538be1364" => :mojave
+    sha256 "0191fc01838de422dc46562a642fb3402e2eb519d3001c14fa3b8948c9b1319b" => :big_sur
+    sha256 "a33e6135d95b8e17e3ae214d6153b3ce3da45c93b3ab7ac98ec507d0c0836d7f" => :catalina
+    sha256 "d2a9e0352a6239f99c61154589a518935ac5cb7b5ed792b889976125ac17bbcf" => :mojave
   end
 
   keg_only :versioned_formula
