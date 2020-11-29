122ad400e39905f49431921ced699843c4f173b2
FX Coudert <fxcoudert@gmail.com>
2020-11-29 17:19:58 +0000

    partio: update 1.10.1_1 bottle.

--- Formula/partio.rb
+++ Formula/partio.rb
@@ -8,10 +8,9 @@ class Partio < Formula
 
   bottle do
     cellar :any
-    sha256 "201ed3e9688506db7cb7f0c0f8191a0c66bd05656a9878f5e27cf6fa700f9915" => :big_sur
-    sha256 "2f968d33da264dca45bec37b40d60bac33d47c158e2ba7ccf3c3589c0fdc5535" => :catalina
-    sha256 "7b6a89f27d152c1ad4593d9df79e79efa4757058b404802b41e68e5c85191eb9" => :mojave
-    sha256 "2a3dbcd7e576aa70904b21e5cee1b733720d2322b1f5ab8fb9064a31b7ed9531" => :high_sierra
+    sha256 "276da2b191f9f6e3505231f97c1bec04beb57516876ad6a29b58f4ce7c39b5a8" => :big_sur
+    sha256 "72063f3cbd49f67e851bf483808f4c08d9052a84c127cf60c73d5f7527459a16" => :catalina
+    sha256 "8f70ed6cbfa1bb929a2a10ddcfa0a1cb527eca76abf02bb7d5b723edf981b612" => :mojave
   end
 
   depends_on "cmake" => :build
