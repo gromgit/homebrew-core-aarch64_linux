2fb12b7015679c35308f6636fe0a40b400a3b628
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-29 22:17:29 +0000

    gpx: update 2.6.8 bottle.

--- Formula/gpx.rb
+++ Formula/gpx.rb
@@ -8,14 +8,9 @@ class Gpx < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "360e8c93e00e1ec95cd2763fc24c2683f1257c5063f793dbf80bc8c3db7cd5be" => :big_sur
-    sha256 "cc71031bc580eeaea3319c3a4e0534feec061620d5c21fca74f7245193413b70" => :catalina
-    sha256 "3a4ae78b868644f171e9005ba2da3169dfb969e607e239280345ae32b0369ced" => :mojave
-    sha256 "057c877225787dc6468db8beb07505870510d9b421e46a1fb7b9b76ad48b0ac4" => :high_sierra
-    sha256 "e51f98467745f27e906fa4d1152cb7fe7e73c2606872f2c9eba8d54fa250a32e" => :sierra
-    sha256 "29621a041a78876e963bdd922c6ea5203f102dbbcd87038d3ade22eca6bafb29" => :el_capitan
-    sha256 "451eab6ecaf8a42858ae0d71cfa141bc14fd3d0608285bc1a677422fcc8a662e" => :yosemite
-    sha256 "d1fdd06431d4efa1043c364c34d1e2f1fd0d904aa58047829ec0beda4aa47340" => :mavericks
+    sha256 "254414afa9fe68137739444a5c514637131eac89d208239d4de86d953bbed5cd" => :big_sur
+    sha256 "a982edd4fb776a077ea51294aea03533e5672dea8a7710329aadc2a3adca9ad1" => :catalina
+    sha256 "f807c588535d7d941470c2d80dd58e97f4ad9e72d7da1b13cbbf87d9912a970a" => :mojave
   end
 
   def install
