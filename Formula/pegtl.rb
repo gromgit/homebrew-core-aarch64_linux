22ac3a1bf39813d47eebc171aea8b254ad5b1c0e
Rui Chen <rui@meetup.com>
2020-11-29 13:27:11 -0500

    pegtl 3.0.0
    * pegtl 3.0.0
    * fix build
    * try some different fix
    * fix syntax
    * pegtl: be more specific about cause
    
    Closes #65905.
    
    Signed-off-by: Rui Chen <rui@meetup.com>
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Co-authored-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pegtl.rb
+++ Formula/pegtl.rb
@@ -1,8 +1,8 @@
 class Pegtl < Formula
   desc "Parsing Expression Grammar Template Library"
   homepage "https://github.com/taocpp/PEGTL"
-  url "https://github.com/taocpp/PEGTL/archive/2.8.3.tar.gz"
-  sha256 "88b8e4ded6ea1f3f2223cc3e37072e2db1e123b90d36c309816341ae9d966723"
+  url "https://github.com/taocpp/PEGTL/archive/3.0.0.tar.gz"
+  sha256 "b47af025450f5c12251cd831f90deab666e18a8029f3dbffb32672b0450af902"
   license "MIT"
 
   bottle do
@@ -15,11 +15,19 @@ class Pegtl < Formula
 
   depends_on "cmake" => :build
 
+  if MacOS.version <= :mojave
+    depends_on "gcc"
+    fails_with :clang do
+      cause "'path' is unavailable in c++ < 17: introduced in macOS 10.15"
+    end
+  end
+
   def install
     mkdir "build" do
       system "cmake", "..", *std_cmake_args,
                             "-DPEGTL_BUILD_TESTS=OFF",
-                            "-DPEGTL_BUILD_EXAMPLES=OFF"
+                            "-DPEGTL_BUILD_EXAMPLES=OFF",
+                            "-DCMAKE_CXX_STANDARD=17"
       system "make", "install"
     end
     rm "src/example/pegtl/CMakeLists.txt"
@@ -27,7 +35,7 @@ class Pegtl < Formula
   end
 
   test do
-    system ENV.cxx, pkgshare/"examples/hello_world.cpp", "-std=c++11", "-o", "helloworld"
+    system ENV.cxx, pkgshare/"examples/hello_world.cpp", "-std=c++17", "-o", "helloworld"
     assert_equal "Good bye, homebrew!\n", shell_output("./helloworld 'Hello, homebrew!'")
   end
 end
