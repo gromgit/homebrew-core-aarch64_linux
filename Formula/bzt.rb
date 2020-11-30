03346387e9138e734efdd6f83830b545b2c14610
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-30 02:18:36 +0000

    bzt: update 1.15.1 bottle.

--- Formula/bzt.rb
+++ Formula/bzt.rb
@@ -14,10 +14,9 @@ class Bzt < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "8f56138fa2be223e2d72358c59a8a42388de9cd496d4756f63b48717bb1d0040" => :big_sur
-    sha256 "005b5ccf365b60ec41f9842175ac0e2ddc49e95f854fa808f171cf3364b3b0cd" => :catalina
-    sha256 "0625ae9ddc783fc64aef537e5822403d6e24c39a974351dc4ee9c711bb314884" => :mojave
-    sha256 "62cc681776a079db76066ab787ca50eddf8f5517df9222a9356af87a7e27ae6c" => :high_sierra
+    sha256 "2914b239cc029c12f7242f5c36651d338810ba6e545e5fb2341946da28463be0" => :big_sur
+    sha256 "87ca5630f029a90be43ba9f1f00fe2fc945ea7d0e5179028f7fbaf987a1665f5" => :catalina
+    sha256 "bc97cf26a459d6492a916e677822c7d6c70044c404b99c357f83920086249d4b" => :mojave
   end
 
   # can only run with python 3.7 due to this issue, https://github.com/simonpercivall/astunparse/issues/43
