3ccd80e965884642cf60804537cf75e12b3b5fdc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-30 06:20:02 +0000

    sbcl: update 2.0.11 bottle.

--- Formula/sbcl.rb
+++ Formula/sbcl.rb
@@ -11,10 +11,9 @@ class Sbcl < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7460b496a8b4a8eff00ab183b84eb5a2633f2f0458c24eed8e159f76c6bce8ff" => :big_sur
-    sha256 "380119855e28b63538777282bb8f2ca785bfadb58269e83d8c54c5c92ef78ec5" => :catalina
-    sha256 "e0ee21b72a5f5ad5632be05d0b6fb61689710866e8d3f9dd395cae53ab087895" => :mojave
-    sha256 "8163f42efd83fcdf2a1345a04955eeb834c91ae5291632b61a736f02b8d9bf50" => :high_sierra
+    sha256 "e6a0d4ba798d435a261e93062461a15801a42af1cab9344cdeab824b00112318" => :big_sur
+    sha256 "82afe9ba6dad370ba0895be28b3591eeae9bb88fbba93a916637cea1e6b140c3" => :catalina
+    sha256 "a8eadb8a7b8a092995d0f29f4b68cf5450c070dd41bdf3f9f15e4907b516d48b" => :mojave
   end
 
   uses_from_macos "zlib"
