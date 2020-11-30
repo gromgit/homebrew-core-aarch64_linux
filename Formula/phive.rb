2d2d3bcf3c0c9b129a9e6b4458facb90a5b4b712
chenrui <chenrui333@gmail.com>
2020-11-30 15:58:57 -0500

    phive 0.14.5 (#65965)

--- Formula/phive.rb
+++ Formula/phive.rb
@@ -1,8 +1,8 @@
 class Phive < Formula
   desc "Phar Installation and Verification Environment (PHIVE)"
   homepage "https://phar.io"
-  url "https://github.com/phar-io/phive/releases/download/0.14.4/phive-0.14.4.phar"
-  sha256 "b6f95e2791a17531f6ba652ce6e4288f35fb73883c472af1d3758f148fc4ed3a"
+  url "https://github.com/phar-io/phive/releases/download/0.14.5/phive-0.14.5.phar"
+  sha256 "70d6de4afb2e4f66ff06e35434299f2a1d81d7ea3fec9c3359adb521c196ff8d"
   license "BSD-3-Clause"
 
   bottle :unneeded
