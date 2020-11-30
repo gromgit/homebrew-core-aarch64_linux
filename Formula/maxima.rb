337a4b891da0610c5216205549bfbadfeb825204
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-11-30 06:20:02 +0000

    maxima: update 5.44.0_2 bottle.

--- Formula/maxima.rb
+++ Formula/maxima.rb
@@ -13,10 +13,9 @@ class Maxima < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "49bdc3f26a3979633b7c232380c93566af03401683a2870fb25e3ea40a18951f" => :big_sur
-    sha256 "1b38264f8b5f867034d238be3fea3e8a3c30004f8b2fe5997e73dff3ccc5f7d6" => :catalina
-    sha256 "5a9a0e9bcffbce0871cb0e0756ce110d03129f0a2784798890766eb6c9a82c67" => :mojave
-    sha256 "96772653ca25c724239a4902a5a0f8844a8b71f2290e25824eec75d032148f98" => :high_sierra
+    sha256 "440125b5bf7dbd305b9e548956a6185f9c384cca33fd3eb71106b7e369e41ff3" => :big_sur
+    sha256 "c7c4716f8b65fa4e24ac5912d21b5d1785ee7f6497219a6038884aaee682ac1e" => :catalina
+    sha256 "1a3edfef96bffeea60b388f2c5d4d52a9b7354c69f8de3bfd29a2a06c51f8af4" => :mojave
   end
 
   depends_on "gawk" => :build
