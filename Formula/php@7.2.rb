901c5420cc64b0fc8b748316dd7f6f028c48e8e6
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-11-30 19:50:44 +0100

    php@7.2: update 7.2.34_1 bottle.

--- Formula/php@7.2.rb
+++ Formula/php@7.2.rb
@@ -9,9 +9,9 @@ class PhpAT72 < Formula
   revision 1
 
   bottle do
-    sha256 "6b8510d6172096f16abcac5bfcc7d660f3c8f98fb2c8704f0c5e524231fa9230" => :big_sur
-    sha256 "8306426b1e7cf45ab64e34e81b328ceeeafe3ed6e9dccb39e6e837837e917459" => :catalina
-    sha256 "18eed639bb5627ef8ad1bdc35417c12dedf512030a3e5578535a4f4ee3dc2845" => :mojave
+    sha256 "e39495f5389c97e3e3e1c2b0ea47832cdff5e5db25e671da0f918d0fc74a7137" => :big_sur
+    sha256 "0069df02b747f6e26b3e3ec5550cc83b96abd7858a6f53c6ff37f839d145fb71" => :catalina
+    sha256 "37be2c076029d9e1884c38166d3120be4ac93bd1db22cb3175d1894b830b73d1" => :mojave
   end
 
   keg_only :versioned_formula
