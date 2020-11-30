4299d2e144aedac5b054c75e5096e10c3c4ffc53
chenrui <chenrui333@gmail.com>
2020-11-30 15:47:19 -0500

    ckan 1.29.2 (#65958)

--- Formula/ckan.rb
+++ Formula/ckan.rb
@@ -1,8 +1,8 @@
 class Ckan < Formula
   desc "Comprehensive Kerbal Archive Network"
   homepage "https://github.com/KSP-CKAN/CKAN/"
-  url "https://github.com/KSP-CKAN/CKAN/releases/download/v1.29.0/ckan.exe"
-  sha256 "3059dc24b265a04fe584453100aeabac7a60131c8de820b6e183fd1ccd9815b0"
+  url "https://github.com/KSP-CKAN/CKAN/releases/download/v1.29.2/ckan.exe"
+  sha256 "f0248b7795967010c44747b4fa4fc33d0a81b558a75973edaafb78c16b306d83"
   license "MIT"
 
   livecheck do
