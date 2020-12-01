87ad1bac3025fe671965a5d9fbb8c7860ecf7ce1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-01 21:25:29 +0000

    podman: update 2.2.0 bottle.

--- Formula/podman.rb
+++ Formula/podman.rb
@@ -7,10 +7,9 @@ class Podman < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "4223c3e232403a0879a0fbf98004c6fb512618aa9d4ae9468104e33dca7a99fa" => :big_sur
-    sha256 "eb411460ee3d91f8286df3eadd4dfd0fec63d61e29a8a6c29c581ac073642011" => :catalina
-    sha256 "09967354204cdd075332de1ba2eaf39ae154da5167200ead9e2db38d96f7a96b" => :mojave
-    sha256 "fb2ca2c9370177bddf1f1683cef3d121fbdb72126a3999eddc097f20ec637540" => :high_sierra
+    sha256 "927cc691a21d6fe634520ebc256be8ff9d1fcf83061c295e2761fb68e7df987d" => :big_sur
+    sha256 "d2a977729d400fce60e1d794fee370a8a2f68af255c81e45b44808de33d4a9bb" => :catalina
+    sha256 "0ff5d26fee613bf26aabc941023b02ad6785873ad114828bddd2885865bcc828" => :mojave
   end
 
   depends_on "go" => :build
