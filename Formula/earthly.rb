bb87c5ce70d5e10b3f062042bb3e69d80b50e67b
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2020-12-01 23:34:20 +0000

    earthly 0.3.16
    Closes #66041.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/earthly.rb
+++ Formula/earthly.rb
@@ -1,8 +1,8 @@
 class Earthly < Formula
   desc "Build automation tool for the post-container era"
   homepage "https://earthly.dev/"
-  url "https://github.com/earthly/earthly/archive/v0.3.15.tar.gz"
-  sha256 "97c4504cd746962765d4ed560c16311e0b67ead15187f5a46da64532f3926a77"
+  url "https://github.com/earthly/earthly/archive/v0.3.16.tar.gz"
+  sha256 "3950985308acacd20abb81403d7de9a4d3fa99ff36ec48f6f6cbbba33b1a6291"
   license "MPL-2.0"
   head "https://github.com/earthly/earthly.git"
 
@@ -17,7 +17,7 @@ class Earthly < Formula
 
   def install
     ldflags = "-X main.DefaultBuildkitdImage=earthly/buildkitd:v#{version} -X main.Version=v#{version} " \
-              "-X main.GitSha=3edfa71924e2ccf1f5dc4cbb1019d50b92f60fb0 "
+              "-X main.GitSha=0dd480b951ce169382bad95b9c1d50cfbe36ed12 "
     tags = "dfrunmount dfrunsecurity dfsecrets dfssh dfrunnetwork"
     system "go", "build",
         "-tags", tags,
