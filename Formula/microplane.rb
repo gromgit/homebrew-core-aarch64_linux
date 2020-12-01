a7ae0b2cd2b63bcc29fcbdbb5282972dab35eb64
Rui Chen <rui@meetup.com>
2020-12-01 12:01:12 -0500

    microplane 0.0.25
    Closes #66012.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/microplane.rb
+++ Formula/microplane.rb
@@ -1,8 +1,8 @@
 class Microplane < Formula
   desc "CLI tool to make git changes across many repos"
   homepage "https://github.com/Clever/microplane"
-  url "https://github.com/Clever/microplane/archive/v0.0.23.tar.gz"
-  sha256 "0243aa58559b8a43f5fa7324eee05d490899aa73294737e47451d1fc994769f5"
+  url "https://github.com/Clever/microplane/archive/v0.0.25.tar.gz"
+  sha256 "1ab714dd01c68aeedf5fb47cc04bd6d2d178cdcd08cfc74d6672c8221c0b0e08"
   license "Apache-2.0"
   head "https://github.com/Clever/microplane.git"
 
