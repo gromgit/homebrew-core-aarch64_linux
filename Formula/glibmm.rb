d99fb2545ac514be7439475d849f3991bd43f0a1
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-12-01 13:01:33 +0000

    glibmm 2.64.5
    Closes #65994.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/glibmm.rb
+++ Formula/glibmm.rb
@@ -1,8 +1,8 @@
 class Glibmm < Formula
   desc "C++ interface to glib"
   homepage "https://www.gtkmm.org/"
-  url "https://download.gnome.org/sources/glibmm/2.64/glibmm-2.64.4.tar.xz"
-  sha256 "405040ab257cef0c8f1b14fdf9f3f92d6e6403715b64f1b75e4b6f04dfb56284"
+  url "https://download.gnome.org/sources/glibmm/2.64/glibmm-2.64.5.tar.xz"
+  sha256 "508fc86e2c9141198aa16c225b16fd6b911917c0d3817602652844d0973ea386"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -21,9 +21,6 @@ class Glibmm < Formula
   depends_on "pkg-config" => :build
   depends_on "glib"
   depends_on "libsigc++@2"
-
-  # submitted upstream at https://gitlab.gnome.org/GNOME/glibmm/-/merge_requests/43
-  patch :DATA
 
   def install
     ENV.cxx11
@@ -70,33 +67,3 @@ class Glibmm < Formula
     system "./test"
   end
 end
-
-__END__
-diff --git a/meson.build b/meson.build
-index 4d2c13a6..fd253a60 100644
---- a/meson.build
-+++ b/meson.build
-@@ -45,21 +45,7 @@ project_build_root = meson.current_build_dir()
- cpp_compiler = meson.get_compiler('cpp')
- is_msvc = cpp_compiler.get_id() == 'msvc'
- is_host_windows = host_machine.system() == 'windows'
--
--is_os_cocoa = false
--if not is_host_windows
--  # This test for Mac OS is copied from glib. If the result of glib's test
--  # is ever made available outside glib, use glib's result instead of this test.
--  # glib: https://bugzilla.gnome.org/show_bug.cgi?id=780309
--  # glibmm: https://bugzilla.gnome.org/show_bug.cgi?id=781947
--  is_os_cocoa = cpp_compiler.compiles(
--    '''#include <Cocoa/Cocoa.h>
--    #ifdef GNUSTEP_BASE_VERSION
--    #error "Detected GNUstep, not Cocoa"
--    #endif''',
--    name: 'Mac OS X Cocoa support'
--  )
--endif
-+is_os_cocoa = host_machine.system() == 'darwin'
-
- python3 = import('python').find_installation()
- python_version = python3.language_version()
-
