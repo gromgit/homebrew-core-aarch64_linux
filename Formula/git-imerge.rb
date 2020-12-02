c0f47630a2a8be9fc6d0bb26f32a389ec2c47771
Rui Chen <rui@meetup.com>
2020-12-01 22:23:02 -0500

    git-imerge 1.2.0
    * git-imerge 1.2.0
    * add some comments about the patch
    
    Closes #66058.
    
    Co-authored-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/git-imerge.rb
+++ Formula/git-imerge.rb
@@ -1,9 +1,11 @@
 class GitImerge < Formula
+  include Language::Python::Virtualenv
+
   desc "Incremental merge for git"
   homepage "https://github.com/mhagger/git-imerge"
-  url "https://github.com/mhagger/git-imerge/archive/v1.1.0.tar.gz"
-  sha256 "62692f43591cc7d861689c60b68c55d7b10c7a201c1026096a7efc771df2ca28"
-  license "GPL-2.0"
+  url "https://files.pythonhosted.org/packages/be/f6/ea97fb920d7c3469e4817cfbf9202db98b4a4cdf71d8740e274af57d728c/git-imerge-1.2.0.tar.gz"
+  sha256 "df5818f40164b916eb089a004a47e5b8febae2b4471a827e3aaa4ebec3831a3f"
+  license "GPL-2.0-or-later"
   head "https://github.com/mhagger/git-imerge.git"
 
   bottle do
@@ -16,13 +18,59 @@ class GitImerge < Formula
     sha256 "76aee24eeb5e7615e4cfbd7aaf3aacac8d8729dfcee79d8542a84cbd9b663459" => :el_capitan
   end
 
+  depends_on "python@3.9"
+
+  # PR ref, https://github.com/mhagger/git-imerge/pull/176
+  # remove in next release
+  patch :DATA
+
   def install
-    bin.mkpath
-    # Work around Makefile insisting to write to $(DESTDIR)/etc/bash_completion.d
-    system "make", "install", "DESTDIR=#{prefix}", "PREFIX="
+    virtualenv_install_with_resources
   end
 
   test do
-    system bin/"git-imerge", "-h"
+    system "git", "init"
+    system "git", "config", "user.name", "BrewTestBot"
+    system "git", "config", "user.email", "BrewTestBot@test.com"
+
+    (testpath/"test").write "foo"
+    system "git", "add", "test"
+    system "git", "commit", "-m", "Initial commit"
+
+    system "git", "checkout", "-b", "test"
+    (testpath/"test").append_lines "bar"
+    system "git", "commit", "-m", "Second commit", "test"
+    assert_equal "Already up-to-date.", shell_output("#{bin}/git-imerge merge master").strip
+
+    system "git", "checkout", "master"
+    (testpath/"bar").write "bar"
+    system "git", "add", "bar"
+    system "git", "commit", "-m", "commit bar"
+    system "git", "checkout", "test"
+
+    expected_output = <<~EOS
+      Attempting automerge of 1-1...success.
+      Autofilling 1-1...success.
+      Recording autofilled block MergeState('master', tip1='test', tip2='master', goal='merge')[0:2,0:2].
+      Merge is complete!
+    EOS
+    assert_match expected_output, shell_output("#{bin}/git-imerge merge master 2>&1")
   end
 end
+
+__END__
+$ git diff
+diff --git a/setup.py b/setup.py
+index 3ee0551..27a03a6 100644
+--- a/setup.py
++++ b/setup.py
+@@ -14,6 +14,9 @@ try:
+ except OSError as e:
+     if e.errno != errno.ENOENT:
+         raise
++except subprocess.CalledProcessError:
++    # `bash-completion` is probably not installed. Just skip it.
++    pass
+ else:
+     completionsdir = completionsdir.strip().decode('utf-8')
+     if completionsdir:
