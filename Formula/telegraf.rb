8e3d7e8665ef8838190542c27d875f572cc5c785
Caleb Xu <calebcenter@live.com>
2020-12-02 11:22:26 -0500

    telegraf 1.16.3
    Closes #66077.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/telegraf.rb
+++ Formula/telegraf.rb
@@ -1,8 +1,8 @@
 class Telegraf < Formula
   desc "Server-level metric gathering agent for InfluxDB"
   homepage "https://www.influxdata.com/"
-  url "https://github.com/influxdata/telegraf/archive/v1.16.2.tar.gz"
-  sha256 "fa317234dcc45a593eb32e06c7f94d9153d9c7ed46683659b3b7db6d32c6167c"
+  url "https://github.com/influxdata/telegraf/archive/v1.16.3.tar.gz"
+  sha256 "576c9cade0ed12d055e3cd3deced35936c96a5a4785dd915463fd1282fcdca09"
   license "MIT"
   head "https://github.com/influxdata/telegraf.git"
 
