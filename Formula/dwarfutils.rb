19599cd12e90fafbd07cfedfab68e1fe72c6364c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-02 06:17:56 +0000

    dwarfutils: update 20201201 bottle.

--- Formula/dwarfutils.rb
+++ Formula/dwarfutils.rb
@@ -11,10 +11,9 @@ class Dwarfutils < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3f38cc1aefc659c6acf1ba09ff229b71f31cbbe1ac8928c164ad05895c202eff" => :big_sur
-    sha256 "1b9a9f2aaae5e305ff265839fe0f71e709814c8c9f0249dc3410b6cac702cadd" => :catalina
-    sha256 "1640ed20f01ce164951d84ae103e797c11545e6aee99fa27eb20335abe38a288" => :mojave
-    sha256 "da07cd4af006c48927a261cce0377289ff738f3d7cf12adf4f7ecb390a01a66d" => :high_sierra
+    sha256 "4b7bb9ec7ab16bc1f7621484fb36af441cb9fd9e749cda79996915cf7bc22372" => :big_sur
+    sha256 "5e63608b1a3ac55a066255d09d57d07105c7adcecbcddfe65fb45738f4480460" => :catalina
+    sha256 "a511dac8825fed2348399e8390170b53e7232b19247f42f59c5bd67ae24af5b4" => :mojave
   end
 
   depends_on "libelf" => :build
