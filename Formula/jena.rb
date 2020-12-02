1e0d9d962c2a751f302f7bab4d3cd5b2164adbfb
Rui Chen <rui@meetup.com>
2020-12-02 13:12:25 -0500

    jena 3.17.0
    * jena 3.17.0
    * fix test build
    
    Closes #66080.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jena.rb
+++ Formula/jena.rb
@@ -1,9 +1,9 @@
 class Jena < Formula
   desc "Framework for building semantic web and linked data apps"
   homepage "https://jena.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-3.16.0.tar.gz"
-  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-3.16.0.tar.gz"
-  sha256 "6ef65ab3e24948f6f8fa97281a936276d74a8732fb4a14c624c1aa9fa93adb30"
+  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-3.17.0.tar.gz"
+  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-3.17.0.tar.gz"
+  sha256 "1c2a51c8a0137c9c12a8928e035351bc6dea99d716a8d2458b9c9467cebc6ec2"
   license "Apache-2.0"
 
   livecheck do
@@ -12,22 +12,24 @@ class Jena < Formula
 
   bottle :unneeded
 
-  def shim_script(target)
-    <<~EOS
-      #!/usr/bin/env bash
-      export JENA_HOME="#{libexec}"
-      "$JENA_HOME/bin/#{target}" "$@"
-    EOS
-  end
+  depends_on "openjdk"
 
   def install
+    env = {
+      JAVA_HOME: Formula["openjdk"].opt_prefix,
+      JENA_HOME: libexec,
+    }
+
     rm_rf "bat" # Remove Windows scripts
 
-    prefix.install %w[LICENSE NOTICE README]
     libexec.install Dir["*"]
-    Dir.glob("#{libexec}/bin/*") do |path|
-      bin_name = File.basename(path)
-      (bin/bin_name).write shim_script(bin_name)
+    Pathname.glob("#{libexec}/bin/*") do |file|
+      next if file.directory?
+
+      basename = file.basename
+      next if basename.to_s == "service"
+
+      (bin/basename).write_env_script file, env
     end
   end
 
