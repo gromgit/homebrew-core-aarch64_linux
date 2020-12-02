79e455fd4ebad00e35bdf80015053fff2b9bc8aa
Rui Chen <rui@meetup.com>
2020-12-02 13:14:32 -0500

    i2p 0.9.48
    Closes #66082.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/i2p.rb
+++ Formula/i2p.rb
@@ -1,9 +1,9 @@
 class I2p < Formula
   desc "Anonymous overlay network - a network within a network"
   homepage "https://geti2p.net"
-  url "https://download.i2p2.de/releases/0.9.47/i2pinstall_0.9.47.jar"
-  mirror "https://launchpad.net/i2p/trunk/0.9.47/+download/i2pinstall_0.9.47.jar"
-  sha256 "3ddf3afb0c06edeed4810c6d1f34d909959dd94640adf7c638781b4a3b282e9e"
+  url "https://download.i2p2.de/releases/0.9.48/i2pinstall_0.9.48.jar"
+  mirror "https://launchpad.net/i2p/trunk/0.9.48/+download/i2pinstall_0.9.48.jar"
+  sha256 "5dd5c300d3d2ca4eb7f7b33a2d4c9e54814f02c199c5176db17f214c8ab655d2"
 
   livecheck do
     url "https://geti2p.net/en/download"
