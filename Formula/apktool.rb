197f172108e778369006a28286d92fae3116da48
Peter Retzlaff <peter@playtestcloud.com>
2020-12-02 16:03:20 +0100

    apktool 2.5.0
    * apktool 2.5.0
    * fix build
    
    Closes #66071.
    
    Signed-off-by: Rui Chen <rui@meetup.com>
    Co-authored-by: Rui Chen <rui@meetup.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/apktool.rb
+++ Formula/apktool.rb
@@ -1,11 +1,13 @@
 class Apktool < Formula
   desc "Tool for reverse engineering 3rd party, closed, binary Android apps"
   homepage "https://github.com/iBotPeaches/Apktool"
-  url "https://github.com/iBotPeaches/Apktool/releases/download/v2.4.1/apktool_2.4.1.jar"
-  sha256 "bdeb66211d1dc1c71f138003ce35f6d0cd19af6f8de7ffbdd5b118d02d825a52"
+  url "https://github.com/iBotPeaches/Apktool/releases/download/v2.5.0/apktool_2.5.0.jar"
+  sha256 "b392d7cb99b592e9c5acc3c06f1b0f180edde96c66b86b3d6932b7c0c4079fe4"
   license "Apache-2.0"
 
   bottle :unneeded
+
+  depends_on "openjdk@8"
 
   resource "sample.apk" do
     url "https://github.com/downloads/stephanenicolas/RoboDemo/robodemo-sample-1.0.1.apk"
@@ -14,7 +16,9 @@ class Apktool < Formula
 
   def install
     libexec.install "apktool_#{version}.jar"
-    bin.write_jar_script libexec/"apktool_#{version}.jar", "apktool"
+    (libexec/"bin").write_jar_script libexec/"apktool_#{version}.jar", "apktool"
+    (libexec/"bin/apktool").chmod 0755
+    (bin/"apktool").write_env_script libexec/"bin/apktool", Language::Java.java_home_env("1.8")
   end
 
   test do
