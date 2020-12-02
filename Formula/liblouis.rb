79bddbcf2e0edca648b06f6cc10f401085c1dec4
Rui Chen <rui@meetup.com>
2020-12-02 13:17:34 -0500

    liblouis 3.16.1
    * liblouis 3.16.1
    * liblouis: update style
    
    Closes #66083.
    
    Co-authored-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/liblouis.rb
+++ Formula/liblouis.rb
@@ -1,15 +1,10 @@
 class Liblouis < Formula
   desc "Open-source braille translator and back-translator"
   homepage "http://liblouis.org"
+  url "https://github.com/liblouis/liblouis/releases/download/v3.16.1/liblouis-3.16.1.tar.gz"
+  sha256 "7ca90c52c1fb04b22cd976495b2325700c7d0135c929a42707fab7bab03658e9"
   license all_of: ["GPL-3.0-or-later", "LGPL-2.1-or-later"]
 
-  stable do
-    url "https://github.com/liblouis/liblouis/releases/download/v3.16.0/liblouis-3.16.0.tar.gz"
-    sha256 "88cda9ad95ce7fdd5a28d75b0a8ed7097fb24aa0112ebce110c505fd200080aa"
-    depends_on "help2man" => :build
-    depends_on "pkg-config" => :build
-    depends_on "python@3.9"
-  end
   bottle do
     sha256 "97393f8dad734bf5f465927f049f72ffeca581df96848b941757420a9a2cc1d9" => :big_sur
     sha256 "b830d1eb331f3e077e98bd04785d8554d3e367fed0b5bdd72aeb57c6987de19f" => :catalina
@@ -18,13 +13,15 @@ class Liblouis < Formula
 
   head do
     url "https://github.com/liblouis/liblouis.git"
+
     depends_on "autoconf" => :build
     depends_on "automake" => :build
-    depends_on "help2man" => :build
     depends_on "libtool" => :build
-    depends_on "pkg-config" => :build
-    depends_on "python@3.9"
   end
+
+  depends_on "help2man" => :build
+  depends_on "pkg-config" => :build
+  depends_on "python@3.9"
 
   def install
     system "./autogen.sh" if build.head?
