31e4542b8b719f0cba8b11c29c61899e33d51c65
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-03 20:16:08 +0000

    direnv: update 2.25.0 bottle.

--- Formula/direnv.rb
+++ Formula/direnv.rb
@@ -8,10 +8,9 @@ class Direnv < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e727cb548c45ac0cb4f341f8f8c852bb24a42a29c8aedbe3c066a584f4957dea" => :big_sur
-    sha256 "6c1ad71d50c04dc82edf046c343c27a822cec108b6968306a8e5e656c7dbf8d9" => :catalina
-    sha256 "67ae040f4951625ea19f26108ffe3681756c85d9aa3d0c0808802f7c18d0dcc1" => :mojave
-    sha256 "4a185e12bb5400949dd1e968f71c29e6d657f4e9dcf829fb51f08eb483dc3fba" => :high_sierra
+    sha256 "f8b6768c9df0f097a87708b77f6cd1f68808c81567609e7f6a4f6c86fcd3bcc9" => :big_sur
+    sha256 "66b13f4889a97b487f9dd2a6b5a02724dc229cbc33ece4c6458e6fa2a2194e34" => :catalina
+    sha256 "183d350c5096cbb818b08fd71b30ffe5e27ed4b27a6cb1211edb90226645c968" => :mojave
   end
 
   depends_on "go" => :build
