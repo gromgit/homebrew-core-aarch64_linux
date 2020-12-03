43ef7cde53730ec37471cf396a293b0f63e6b8d1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-03 10:17:57 +0000

    quill: update 1.6.0 bottle.

--- Formula/quill.rb
+++ Formula/quill.rb
@@ -8,10 +8,9 @@ class Quill < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9bd7b9972f4b0dba81202bb49a47695c08e7b6581627cd7df94e8072c66c0aa5" => :big_sur
-    sha256 "005bd68d183a4ef347d73b59ddbd273b1c8bc2d35400f5acef88294fbb288bc8" => :catalina
-    sha256 "a665a6c752e4482c0582bdbd0fe1248f72b5369bce53d331bae366db53fe2b10" => :mojave
-    sha256 "d76aae3da313beb3338b134472377298f5fb56d6d619bd0cd1465557e25870c5" => :high_sierra
+    sha256 "63adcc4aa824061b010935f824208babaf0dc3537909024a6b8c6e73b00c68d9" => :big_sur
+    sha256 "6c6a52facab9c272c622c090de6217068cf21e9ba24ab91876fda3a73385c618" => :catalina
+    sha256 "9fe8025a5665dce0892a1fe1806d5572c49c463056f4b7eb36f37473d29a53fc" => :mojave
   end
 
   depends_on "cmake" => :build
