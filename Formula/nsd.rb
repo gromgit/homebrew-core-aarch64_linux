286471cf697d3422666e807695b69d1a27f3195b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-03 10:17:31 +0000

    nsd: update 4.3.4 bottle.

--- Formula/nsd.rb
+++ Formula/nsd.rb
@@ -14,10 +14,9 @@ class Nsd < Formula
   end
 
   bottle do
-    sha256 "b3eeb6e4a4145b3384982aa2330690834373f547235a659404eefd974163b199" => :big_sur
-    sha256 "d2d0731a5a4a03fff114250debcc1ff5fa6fe15faf77351afda9a2ccc6cdfbd0" => :catalina
-    sha256 "1f0661c9656ab0d0821a9eeb5b990c9eb6a088654e5406318577084296ab8119" => :mojave
-    sha256 "189a5b486bbfdcc0571e89f67d5f4f11474ce1fe2fb9e5ca720ced3662aba054" => :high_sierra
+    sha256 "89235887c73372327a0f4c372d5b93e5fbccfeb6bde079d02d4c932b1982a020" => :big_sur
+    sha256 "b21b9f1ad17d10473d8acc41b37d75add960de933ae5c7569eec2a1d2a9be79c" => :catalina
+    sha256 "aa51669ef28ff75d2eb3fb4dd9dc02dc905b0bb96c78a6740c54296e0b4626ed" => :mojave
   end
 
   depends_on "libevent"
