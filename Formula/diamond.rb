10f274065e1ee643c9c73e32139c9c1421b13ad2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-03 00:39:56 +0000

    diamond: update 2.0.5 bottle.

--- Formula/diamond.rb
+++ Formula/diamond.rb
@@ -7,10 +7,9 @@ class Diamond < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "8b9bc94a6c96433ba341d4149cf7c9b2726e2ef08abd4a0eebc721444c71c315" => :big_sur
-    sha256 "f68458d1886716c4ee0c34e31187ea85e149cfcd72b190050ab464e5657a600a" => :catalina
-    sha256 "722f1d03a45520b61f51afee74e119732bae1fb90a775a0433382ab7ccf67d61" => :mojave
-    sha256 "ebda6089e20340b49dc7dc3144cc459d3a9b7e075dd699249e2fc6be61bf5a6e" => :high_sierra
+    sha256 "b13ad0b54b2e17928c3d9a14680a8f575914b824bb2d74f1266a034276576a13" => :big_sur
+    sha256 "d50080c145189391f41dbd0e8bc1ff0787825f98bea7c85e62b27424c3b25a08" => :catalina
+    sha256 "5918ebadbedb3cd7809f4251488ba20ba01b65ba879052a3eac3730f0fbbefd5" => :mojave
   end
 
   depends_on "cmake" => :build
