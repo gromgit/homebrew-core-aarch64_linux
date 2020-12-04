2935d2f9ae58efe6bd226a7aee79d60486109ec8
Peter Eisentraut <peter@eisentraut.org>
2020-12-04 17:12:42 +0000

    postgresql@9.5: add deprecation date

--- Formula/postgresql@9.5.rb
+++ Formula/postgresql@9.5.rb
@@ -18,6 +18,9 @@ class PostgresqlAT95 < Formula
 
   keg_only :versioned_formula
 
+  # https://www.postgresql.org/support/versioning/
+  deprecate! date: "2021-02-11", because: :unsupported
+
   depends_on "openssl@1.1"
   depends_on "readline"
 
