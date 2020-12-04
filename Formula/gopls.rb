fa11909f9cb171811cfb02f49d8ebd85fc519642
chenrui <chenrui333@gmail.com>
2020-12-04 20:23:03 +0000

    gopls 0.5.5
    Closes #66264.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gopls.rb
+++ Formula/gopls.rb
@@ -1,8 +1,8 @@
 class Gopls < Formula
   desc "Language server for the Go language"
   homepage "https://github.com/golang/tools/tree/master/gopls"
-  url "https://github.com/golang/tools/archive/gopls/v0.5.4.tar.gz"
-  sha256 "0a93330f0ac894ea90183e2ba87ed076bef29c5767d8fb57dc928116a0b7e5e9"
+  url "https://github.com/golang/tools/archive/gopls/v0.5.5.tar.gz"
+  sha256 "08d477a7c35021ec5d8950e25e1fcac86d7ec0ce8a421c20d932029e00efb1d8"
   license "BSD-3-Clause"
 
   bottle do
