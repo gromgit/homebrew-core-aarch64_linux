f95fb06352489d539777cdf213e5a92d95e8c670
davidcok <davidcok@amazon.com>
2020-12-04 20:03:57 -0500

    parent f9b222792e87e0b226e91a0f206e8ceb17016167
    author davidcok <davidcok@amazon.com> 1607130237 -0500
    committer davidcok <davidcok@amazon.com> 1608132988 -0500
    
    Fixed z3 version
    
    Edits per audit comments
    
    Improving tests
    
    Fixes per review comments -- building Z3 from source
    
    Removing version reference; adding back bottle block
    
    Adding comments
    
    Removing trailing white space
    
    dafny: add license
    
    dafny: style fix
    
    Fixed z3 version
    
    Removing version reference; adding back bottle block
    
    Adding comments
    
    Removing trailing white space
    
    dafny: add license
    
    dafny: style fix
    
    Removing version reference; adding back bottle block
    
    Adding comments
    
    Removing trailing white space
    
    dafny: add license
    
    dafny: style fix
    
    Fixed z3 version
    
    Removing version reference; adding back bottle block
    
    Adding comments
    
    Removing trailing white space
    
    dafny: add license
    
    dafny: style fix
    
    Fixed z3 version
    
    Edits per audit comments
    
    Improving tests
    
    Fixes per review comments -- building Z3 from source
    
    Removing version reference; adding back bottle block
    
    Adding comments
    
    Removing trailing white space
    
    dafny: add license
    
    dafny: style fix
    
    Fixed z3 version
    
    Removing version reference; adding back bottle block
    
    Adding comments
    
    Removing trailing white space
    
    dafny: add license
    
    dafny: style fix
    
    Fixed z3 version
    
    Edits per audit comments
    
    Improving tests
    
    Fixes per review comments -- building Z3 from source
    
    Removing version reference; adding back bottle block
    
    Adding comments
    
    Removing trailing white space
    
    dafny: add license
    
    dafny: style fix
    
    Fixed z3 version
    
    Edits per audit comments
    
    Improving tests
    
    Fixes per review comments -- building Z3 from source
    
    Removing version reference; adding back bottle block
    
    Typographical edits per PR comments
    
    Adding comments
    
    Removing trailing white space
    
    dafny: add license
    
    dafny: style fix
    
    Revising dafny formula to use correct version of Z3
    
    Closes #66278.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/dafny.rb
+++ Formula/dafny.rb
@@ -3,7 +3,8 @@ class Dafny < Formula
   homepage "https://github.com/dafny-lang/dafny/blob/master/README.md"
   url "https://github.com/dafny-lang/dafny/archive/v2.3.0.tar.gz"
   sha256 "ea7ae310282c922772a46a9a85e2b4213043283038b74d012047b5294687d168"
-  revision 2
+  license "MIT"
+  revision 3
 
   livecheck do
     url :stable
@@ -20,11 +21,17 @@ class Dafny < Formula
   depends_on "mono-libgdiplus" => :build
   depends_on "nuget" => :build
   depends_on "mono"
-  depends_on "z3"
 
   resource "boogie" do
     url "https://github.com/boogie-org/boogie.git",
         revision: "9e74c3271f430adb958908400c6f6fce5b59000a"
+  end
+
+  # Use the following along with the z3 build below, as long as dafny
+  # cannot build with latest z3 (https://github.com/dafny-lang/dafny/issues/810)
+  resource "z3" do
+    url "https://github.com/Z3Prover/z3/archive/z3-4.8.4.tar.gz"
+    sha256 "5a18fe616c2a30b56e5b2f5b9f03f405cdf2435711517ff70b076a01396ef601"
   end
 
   def install
@@ -37,10 +44,14 @@ class Dafny < Formula
 
     libexec.install Dir["Binaries/*"]
 
-    # We don't want to resolve opt_bin here.
     dst_z3_bin = libexec/"z3/bin"
     dst_z3_bin.mkpath
-    ln_sf (Formula["z3"].opt_bin/"z3").relative_path_from(dst_z3_bin), dst_z3_bin/"z3"
+
+    resource("z3").stage do
+      system "./configure"
+      system "make", "-C", "build"
+      mv("build/z3", dst_z3_bin/"z3")
+    end
 
     (bin/"dafny").write <<~EOS
       #!/bin/bash
@@ -51,9 +62,16 @@ class Dafny < Formula
   test do
     (testpath/"test.dfy").write <<~EOS
       method Main() {
+        var i: nat;
+        assert i as int >= -1;
         print "hello, Dafny\\n";
       }
     EOS
-    system "#{bin}/dafny", testpath/"test.dfy"
+    assert_equal "\nDafny program verifier finished with 1 verified, 0 errors\n",
+                  shell_output("#{bin}/dafny /nologo /compile:0 #{testpath}/test.dfy")
+    assert_equal "\nDafny program verifier finished with 1 verified, 0 errors\nRunning...\n\nhello, Dafny\n",
+                  shell_output("#{bin}/dafny /nologo /compile:3 #{testpath}/test.dfy")
+    assert_equal "Z3 version 4.8.4 - 64 bit\n",
+                 shell_output("#{libexec}/z3/bin/z3 -version")
   end
 end
