f4ce5c8deaa2cf6c32e91facc6d9e8f2572a480a
chenrui <chenrui333@gmail.com>
2020-12-06 13:16:15 -0500

    plantuml 1.2020.22 (#66334)

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://downloads.sourceforge.net/project/plantuml/1.2020.21/plantuml.1.2020.21.jar"
-  sha256 "27f47135648f1c0f192cff28455ecbedd1daa68a4c4a716628ed946de6d0c241"
+  url "https://downloads.sourceforge.net/project/plantuml/1.2020.22/plantuml.1.2020.22.jar"
+  sha256 "dd93705b5ff0b769ad0563240c1b25aa165d1d2772b76f30b4610e907e514283"
   license "GPL-3.0-or-later"
   version_scheme 1
 
