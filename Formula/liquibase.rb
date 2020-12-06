0968f45c312abd2cda2cc19df338695cd9e52046
chenrui <chenrui333@gmail.com>
2020-12-05 23:48:16 -0500

    liquibase 4.2.1 (#66261)
    * liquibase 4.2.1
    
    * fix build
    
    Signed-off-by: Rui Chen <rui@meetup.com>

--- Formula/liquibase.rb
+++ Formula/liquibase.rb
@@ -1,18 +1,20 @@
 class Liquibase < Formula
   desc "Library for database change tracking"
   homepage "https://www.liquibase.org/"
-  url "https://github.com/liquibase/liquibase/releases/download/v4.2.0/liquibase-4.2.0.tar.gz"
-  sha256 "c70f40bfefabca6050ca373787be8f306f883887e52f9db1222ed0f4c37b4276"
+  url "https://github.com/liquibase/liquibase/releases/download/v4.2.1/liquibase-4.2.1.tar.gz"
+  sha256 "0bf1c51fc8cbccc482f4902de9c81f313abaa27a1321a9481ef39e1b8e1369bb"
   license "Apache-2.0"
 
   bottle :unneeded
+
+  depends_on "openjdk"
 
   def install
     rm_f Dir["*.bat"]
     chmod 0755, "liquibase"
     prefix.install_metafiles
     libexec.install Dir["*"]
-    bin.install_symlink libexec/"liquibase"
+    (bin/"liquibase").write_env_script libexec/"liquibase", JAVA_HOME: Formula["openjdk"].opt_prefix
     (libexec/"lib").install_symlink Dir["#{libexec}/sdk/lib-sdk/slf4j*"]
   end
 
