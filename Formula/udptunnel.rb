5a6803c2f4694d4649128ee87cb5bc0ad7ee440d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-06 14:17:59 +0000

    udptunnel: update 1.1 bottle.

--- Formula/udptunnel.rb
+++ Formula/udptunnel.rb
@@ -10,13 +10,10 @@ class Udptunnel < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c015b88af8ac3590aee162c50428bf931a8a15ab006589022bdace7bf665e32a" => :catalina
-    sha256 "7923707e0f1851b728643b010aba4b2052e076f935467a1b0ec8ef05d0cbe54b" => :mojave
-    sha256 "938663a675b5ef62316fa1f7f4f33895d1b28af656ee722c5f8cf6ba94123f4e" => :high_sierra
-    sha256 "c129b87c78c3297365d7e78b77985b69deb4def8d030b029d812bb25610b8bb0" => :sierra
-    sha256 "acbc74c384071aa6be92bea754274c5025de0123afd389d1b174e444a7beec42" => :el_capitan
-    sha256 "8c4b8fb805fa94098925417a8fa61ced0d546b1b77330fd03cd5a72ee6e43b70" => :yosemite
-    sha256 "8f0051f653f62bef0e85f8ced4806a8bae244a911e05129e4fc2bf05912c9412" => :mavericks
+    rebuild 1
+    sha256 "bfb564a4529a508338776cc02b0b4fcd63ceead924db45ef73f4c41c79f96908" => :big_sur
+    sha256 "de4e78f6f0ff861478dae683d1a6c09ae38e9a9e7ec8780a90a9b849df422089" => :catalina
+    sha256 "46dca7ebedab0825acffeafa11b6090676993a5b7b4a53591db51cc7b856e048" => :mojave
   end
 
   depends_on "autoconf" => :build
