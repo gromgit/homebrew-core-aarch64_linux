cd0af65bfd67510ee85827fab3c23e6c8c4dbee2
FX Coudert <fxcoudert@gmail.com>
2020-12-06 20:35:29 +0000

    libslirp: update 4.4.0 bottle.

--- Formula/libslirp.rb
+++ Formula/libslirp.rb
@@ -7,10 +7,9 @@ class Libslirp < Formula
 
   bottle do
     cellar :any
-    sha256 "51817f70add83d1fb74dd233d683aa88e3d0da1e9f8df5c724347e946c2bad6d" => :big_sur
-    sha256 "1b810179f0b4978a6b06f15156b8f4ecddd7b7b8408e61129a11913cf9ac4145" => :catalina
-    sha256 "d708a39f70d01586c4d9e6fcccd832f638b07325424ce451418bcaee68688669" => :mojave
-    sha256 "a7c30a50420febf21f5969719f1d7d9a0abee04a79586c951799212cb952cb9f" => :high_sierra
+    sha256 "c246bdd605b4dd938e1973e3cc6e9855bfac88d2387aa40d41a85180554dc4ab" => :big_sur
+    sha256 "1e17da7e87c39a76d7597c6030d2ae12d3b2a79742b4533e0113e7442e610833" => :catalina
+    sha256 "a2f636ece566472a1eb9db4761399a0a68bf1c2ca8b901a34d4205858704be90" => :mojave
   end
 
   depends_on "meson" => :build
