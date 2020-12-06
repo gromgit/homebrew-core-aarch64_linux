09e4aa5a11c88f8c9a5e37d9dfc323d69af2c4e0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-06 18:16:26 +0000

    cdktf: update 0.0.19 bottle.

--- Formula/cdktf.rb
+++ Formula/cdktf.rb
@@ -13,10 +13,9 @@ class Cdktf < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d4cde880cefd8c3daa3a5d2a2d54bc3c547ec5d706c37183c80ee29ba0cdc866" => :big_sur
-    sha256 "17cc7f5afc9435c0b77c43661f391a5a7bfcb08d20229db5578c950bb24f41ab" => :catalina
-    sha256 "8ad6f2f031ec6a3ba977069f7325e7107fb4016f5d50d18705eb9da2fd1f44b2" => :mojave
-    sha256 "d7d23d0f82e6a8721fbab1518ecbdd7428ca8474e38b2bd31505ee8c06590349" => :high_sierra
+    sha256 "f480fe5702538db3e3e4e1c55e2c6b2122ba48cac721c84798da3f86c6dc2ad0" => :big_sur
+    sha256 "564574ee0ba53d103d0df3d231f40ec4c5aa33a17d463678f03fd5fbe08db80a" => :catalina
+    sha256 "d7611317b3f80f5ab7bcc8637ddee610378e017cfbec7c93eed1228a5cefc1b0" => :mojave
   end
 
   depends_on "node"
