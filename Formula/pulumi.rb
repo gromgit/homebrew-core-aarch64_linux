c077dc0c23f529bbba9efb9c8c56f07f43653ee6
chenrui <chenrui333@gmail.com>
2020-12-06 16:49:13 +0000

    pulumi: update 2.15.1 bottle.

--- Formula/pulumi.rb
+++ Formula/pulumi.rb
@@ -9,9 +9,9 @@ class Pulumi < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "112fdb639256b270e51ad917ec050c57de6e612e5cf9fe3ca9d83e7a5dfef42a" => :big_sur
-    sha256 "791633f9e2dd9ecb4533fdc3a86c244a47fda5f84521e1525cbe23085cc9dfa1" => :catalina
-    sha256 "f2b50f0ba24fccb88b08e24bff540d9b498c771234212d08953fe1d611d75e53" => :mojave
+    sha256 "9586b6321b15262677845e17bdb529c4234599a758bc2e2502aa7d142972b001" => :big_sur
+    sha256 "24e5965bb004f8101e0ba6d4fa5c0b3cf5996af598ff7ab30e9b234adadcb799" => :catalina
+    sha256 "5972919979e489a18368561039f0f85206923f0d7cc82b28c06f0b4c2f5d988d" => :mojave
   end
 
   depends_on "go" => :build
