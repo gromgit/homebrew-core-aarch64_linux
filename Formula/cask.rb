abb180a57d8f818775b12077e304552079bbb4d5
chenrui <chenrui333@gmail.com>
2020-12-06 15:13:28 -0500

    cask 0.8.5 (#66351)

--- Formula/cask.rb
+++ Formula/cask.rb
@@ -1,9 +1,9 @@
 class Cask < Formula
   desc "Emacs dependency management"
   homepage "https://cask.readthedocs.io/"
-  url "https://github.com/cask/cask/archive/v0.8.4.tar.gz"
-  sha256 "02f8bb20b33b23fb11e7d2a1d282519dfdb8b3090b9672448b8c2c2cacd3e478"
-  license "GPL-3.0"
+  url "https://github.com/cask/cask/archive/v0.8.5.tar.gz"
+  sha256 "b7a6bda663d5a83a99036287cd9362d131ae3a0e0525a48b50eb194684e4447d"
+  license "GPL-3.0-or-later"
   head "https://github.com/cask/cask.git"
 
   bottle :unneeded
