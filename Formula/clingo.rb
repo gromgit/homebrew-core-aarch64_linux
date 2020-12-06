93c13f31999e6ecd7fd596870e545788b1260bd5
chenrui <chenrui333@gmail.com>
2020-12-06 20:05:15 +0000

    clingo 5.4.1
    * clingo 5.4.1
    * clingo: remove patch
    * Update clingo.rb
    * install clingo python module to loadable path
      The `-DPYCLINGO_USER_INSTALL=OFF` setting needs to be passed to get the
      library to be placed in the global site-packages.  Without it, it gets
      installed into a strange home-directory path that is not part of any
      normal python search path.
    
      Also, the variable used to set the executable path for FindPython is
      Python_EXECUTABLE, rather than PYTHON_EXECUTABLE.
    * add head target to clingo
      Allow building upstream head, added url and two extra dependencies
      required by the head version at build time.
    * comply with audit strict
    
    Closes #66945.
    
    Co-authored-by: FX Coudert <fxcoudert@gmail.com>
    Co-authored-by: Tom Scogland <scogland1@llnl.gov>
    Signed-off-by: chenrui <rui@chenrui.dev>

--- Formula/clingo.rb
+++ Formula/clingo.rb
@@ -1,10 +1,9 @@
 class Clingo < Formula
   desc "ASP system to ground and solve logic programs"
   homepage "https://potassco.org/"
-  url "https://github.com/potassco/clingo/archive/v5.4.0.tar.gz"
-  sha256 "e2de331ee0a6d254193aab5995338a621372517adcf91568092be8ac511c18f3"
+  url "https://github.com/potassco/clingo/archive/v5.4.1.tar.gz"
+  sha256 "ac6606388abfe2482167ce8fd4eb0737ef6abeeb35a9d3ac3016c6f715bfee02"
   license "MIT"
-  revision 3
 
   livecheck do
     url :stable
@@ -16,6 +15,12 @@ class Clingo < Formula
     sha256 "89d4cb89080b15aae7e9c53a9d2cb16f4782e704d7af991098ca2310ad29a947" => :arm64_big_sur
     sha256 "5c6dd1f45a2cfe48e4616c6e4bcc45a8b9b5ab050016ad8db6c38bd810129985" => :catalina
     sha256 "55a5d161667e66004aa6d56f92ab00ccefb6863094fd2ba3c113b6d20d741968" => :mojave
+  end
+
+  head do
+    url "https://github.com/potassco/clingo.git"
+    depends_on "bison" => :build
+    depends_on "re2c" => :build
   end
 
   depends_on "cmake" => :build
@@ -31,24 +36,19 @@ class Clingo < Formula
   link_overwrite "bin/lpconvert"
   link_overwrite "bin/reify"
 
-  # Patch adds Python 3.8 compatibility
-  patch do
-    url "https://github.com/potassco/clingo/commit/13a896b6a762e48c2396e3dc9f2e794020f4e6e8.patch?full_index=1"
-    sha256 "f2840bc4ab5159c253c5390c845f0d003f5fef813f49ddca27dbd5245f535e79"
-  end
-
   def install
     system "cmake", ".", "-DCLINGO_BUILD_WITH_PYTHON=ON",
                          "-DCLINGO_BUILD_PY_SHARED=ON",
                          "-DPYCLINGO_USE_INSTALL_PREFIX=ON",
+                         "-DPYCLINGO_USER_INSTALL=OFF",
                          "-DCLINGO_BUILD_WITH_LUA=ON",
-                         "-DPYTHON_EXECUTABLE=#{Formula["python@3.9"].opt_bin}/python3",
+                         "-DPython_EXECUTABLE=#{Formula["python@3.9"].opt_bin}/python3",
                          "-DPYCLINGO_DYNAMIC_LOOKUP=OFF",
                          *std_cmake_args
     system "make", "install"
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/clingo --version")
+    assert_match "clingo version", shell_output("#{bin}/clingo --version")
   end
 end
