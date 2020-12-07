a7cbf1308cf62876002cdee0d86645fbef2622b8
FX Coudert <fxcoudert@gmail.com>
2020-12-07 18:20:54 +0000

    libcmph: update 2.0.2 bottle.

--- Formula/libcmph.rb
+++ Formula/libcmph.rb
@@ -11,6 +11,7 @@ class Libcmph < Formula
   bottle do
     cellar :any
     sha256 "f1cc2211ac56a2702405246535a55613855c3879885ca73aa65d76890c2aa0e5" => :big_sur
+    sha256 "82227b819677c6bd9a4e74cde2e7d1ef7d1cb2699e328109dbf098d131b134c2" => :arm64_big_sur
     sha256 "c38019c153c728a28acbfe340cc86764285ec24edbdba5234b0593f83d355c22" => :catalina
     sha256 "d02c761bd6b52424528bfdcd56b8d469d7cdd2e55f625c719229edb7f011889c" => :mojave
     sha256 "abffeaf075db6387e636d43eb8fda9b76f02091bdb5533368306f899a46406c1" => :high_sierra
