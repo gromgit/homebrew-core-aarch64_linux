a153a4599512a186e05c3834733ee1c7a89e93bc
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-12-07 17:34:31 +0100

    xz: fix bottle ordering

--- Formula/xz.rb
+++ Formula/xz.rb
@@ -14,8 +14,8 @@ class Xz < Formula
 
   bottle do
     cellar :any
-    sha256 "03f10f46ede69b94e279f5f4ddc065c05b8c4dc93ede846df3511ae5e924a5df" => :arm64_big_sur
     sha256 "4fbd4a9e3eb49c27e83bd125b0e76d386c0e12ae1139d4dc9e31841fb8880a35" => :big_sur
+    sha256 "03f10f46ede69b94e279f5f4ddc065c05b8c4dc93ede846df3511ae5e924a5df" => :arm64_big_sur
     sha256 "2dcc8e0121c934d1e34ffdb37fcd70f0f7b5c2f4755f2f7cbcf360e9e54cb43b" => :catalina
     sha256 "44483961b5d2b535b0ece1936c9d40b4bc7d9c7281646cca0fb476291ab9d4dc" => :mojave
     sha256 "1491b2b20c40c3cb0b990f520768d7e876e4ab4a7dc1da9994d0150da34ba5c6" => :high_sierra
