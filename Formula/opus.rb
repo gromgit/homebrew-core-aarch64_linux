3b675ad33956cdb9a8a962977fa9f9b2a244e450
FX Coudert <fxcoudert@gmail.com>
2020-12-07 17:35:03 +0000

    opus: update 1.3.1 bottle.

--- Formula/opus.rb
+++ Formula/opus.rb
@@ -14,6 +14,7 @@ class Opus < Formula
     cellar :any
     rebuild 1
     sha256 "cffbcfe03bf3d0e6a9b9b301dc0ea71974ab6f49e1ab66dd7895679367ecf156" => :big_sur
+    sha256 "5a27b1ad0a626d1ccd78cd5a4d94f8c44cf1780cfb2a1118f60a66158c1b4ea9" => :arm64_big_sur
     sha256 "5cb191f66da0ef2b8d03985c79cb18a59506aaba8a01cc0b1a821c293e88d576" => :catalina
     sha256 "21fa4c22a63bccc5e188dabb9c85af63a57d19582c4f616716bccb063e2befec" => :mojave
     sha256 "8b45ac09baae56bdc2c7ee224d5a1ae68efb826a9aec2220e0b27e8ce633b8aa" => :high_sierra
