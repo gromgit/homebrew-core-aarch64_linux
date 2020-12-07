f4831cf56e51cfdd104e30924d0caf6667c05245
carlocab <30379873+carlocab@users.noreply.github.com>
2020-12-07 12:59:43 +0000

    instead: depend on luajit rather than lua@5.3
    This is recommended by upstream:
    
        https://github.com/instead-hub/instead/blob/82f1ed6e96c49b415f4ea6a40190bca6baf74727/INSTALL#L103
    
    Closes #66405.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/instead.rb
+++ Formula/instead.rb
@@ -4,7 +4,7 @@ class Instead < Formula
   url "https://github.com/instead-hub/instead/archive/3.3.2.tar.gz"
   sha256 "bdb827f36e693dc7b443e69d4678d24f1ccc20dc093c22f58b8d78192da15f2e"
   license "MIT"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "71af1e349e6da503d572dbe2b0cd969a33020fa9101bbe9692c56c85e02e676c" => :big_sur
@@ -13,27 +13,16 @@ class Instead < Formula
   end
 
   depends_on "cmake" => :build
-
-  # Possible patch for lua 5.4 support:
-  # https://github.com/instead-hub/instead/commit/ea4f0e81c6859b0aadde582f47ffd7850f54a264
-  # Alternatively, this dependency may be replaced with luajit, which is the
-  # package's preferred version of lua:
-  # https://github.com/instead-hub/instead/blob/master/INSTALL
-  depends_on "lua@5.3"
-
+  depends_on "luajit"
   depends_on "sdl2"
   depends_on "sdl2_image"
   depends_on "sdl2_mixer"
   depends_on "sdl2_ttf"
 
   def install
-    # Make sure I point to the correct lua version!
-    lua = Formula["lua@5.3"]
-
     mkdir "build" do
       system "cmake", "..", "-DWITH_GTK2=OFF",
-                            "-DLUA_INCLUDE_DIR=#{lua.opt_include}/lua",
-                            "-DLUA_LIBRARY=#{lua.opt_lib}/liblua.dylib",
+                            "-DWITH_LUAJIT=ON",
                             *std_cmake_args
       system "make", "install"
     end
