a041d4f898450d1a980fecb58258229e4902760c
Mike McQuaid <mike@mikemcquaid.com>
2020-12-07 12:51:37 +0000

    autoconf: fix bottle ordering.

--- Formula/autoconf.rb
+++ Formula/autoconf.rb
@@ -14,6 +14,7 @@ class Autoconf < Formula
     cellar :any_skip_relocation
     rebuild 4
     sha256 "4a05c5734bd99dc0adca0160e1ca79a291f2bd7fb8d52dd4605df0da3063c891" => :big_sur
+    sha256 "9d4972cf90465fade2c2e91fadc4aabbb7cfa56fb27b41775e7d3a141418f644" => :arm64_big_sur
     sha256 "ca510b350e941fb9395522a03f9d2fb5df276085d806ceead763acb95889a368" => :catalina
     sha256 "9724736d34773b6e41e2434ffa28fe79feccccf7b7786e54671441ca75115cdb" => :mojave
     sha256 "63957a3952b7af5496012b3819c9956822fd7d895d63339c23fdc65c502e1a40" => :high_sierra
@@ -21,7 +22,6 @@ class Autoconf < Formula
     sha256 "ded69c7dac4bc8747e52dca37d6d561e55e3162649d3805572db0dc2f940a4b8" => :el_capitan
     sha256 "daf70656aa9ff8b2fb612324222aa6b5e900e2705c9f555198bcd8cd798d7dd0" => :yosemite
     sha256 "d153b3318754731ff5e91b45b2518c75880993fa9d1f312a03696e2c1de0c9d5" => :mavericks
-    sha256 "9d4972cf90465fade2c2e91fadc4aabbb7cfa56fb27b41775e7d3a141418f644" => :arm64_big_sur
   end
 
   uses_from_macos "m4"
