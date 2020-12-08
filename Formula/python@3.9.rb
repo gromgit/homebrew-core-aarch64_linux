3a19e6945d726a106f3efb9fb246a4ceb9c580dc
carlocab <30379873+carlocab@users.noreply.github.com>
2020-12-08 16:46:56 +0000

    python@3.9 3.9.1
    Closes #66489.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/python@3.9.rb
+++ Formula/python@3.9.rb
@@ -1,10 +1,9 @@
 class PythonAT39 < Formula
   desc "Interpreted, interactive, object-oriented programming language"
   homepage "https://www.python.org/"
-  url "https://www.python.org/ftp/python/3.9.0/Python-3.9.0.tar.xz"
-  sha256 "9c73e63c99855709b9be0b3cc9e5b072cb60f37311e8c4e50f15576a0bf82854"
+  url "https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tar.xz"
+  sha256 "991c3f8ac97992f3d308fefeb03a64db462574eadbff34ce8bc5bb583d9903ff"
   license "Python-2.0"
-  revision 5
 
   livecheck do
     url "https://www.python.org/ftp/python/"
@@ -62,8 +61,8 @@ class PythonAT39 < Formula
   link_overwrite "Frameworks/Python.framework/Versions/Current"
 
   resource "setuptools" do
-    url "https://files.pythonhosted.org/packages/a7/e0/30642b9c2df516506d40b563b0cbd080c49c6b3f11a70b4c7a670f13a78b/setuptools-50.3.2.zip"
-    sha256 "ed0519d27a243843b05d82a5e9d01b0b083d9934eaa3d02779a23da18077bd3c"
+    url "https://files.pythonhosted.org/packages/12/e1/b9a2926a3c5a3fb055b8f85052f5baa890106a0e21b64a977c10affea751/setuptools-51.0.0.zip"
+    sha256 "029c49fd713e9230f6a41c0298e6e1f5839f2cde7104c0ad5e053a37777e7688"
   end
 
   resource "pip" do
@@ -74,23 +73,6 @@ class PythonAT39 < Formula
   resource "wheel" do
     url "https://files.pythonhosted.org/packages/d4/cf/732e05dce1e37b63d54d1836160b6e24fb36eeff2313e93315ad047c7d90/wheel-0.36.1.tar.gz"
     sha256 "aaef9b8c36db72f8bf7f1e54f85f875c4d466819940863ca0b3f3f77f0a1646f"
-  end
-
-  # Remove this block when upstream adds arm64 and Big Sur compatibility
-  if MacOS.version >= :big_sur
-    # Upstream PRs #20171, #21114, #21224 and #21249
-    # Backport of https://github.com/python/cpython/pull/22855
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/33a9d63f/python/arm64-3.9.patch"
-      sha256 "167e328cf68e9ec142f483fda9fafbb903be9a47dee2826614fdc24b2fbe6e06"
-    end
-
-    # Further patch for Big Sur, remove in 3.9.2
-    # https://github.com/python/cpython/pull/23556
-    patch do
-      url "https://github.com/fxcoudert/cpython/commit/6511bf56.patch?full_index=1"
-      sha256 "3a34fea8a133305bc337d67acfacc36dc8f9d47a808dd592f5b0cd8c9c9384d2"
-    end
   end
 
   def install
