7a8bf8b3c50e0a2dda96e41d2fe6fe3ef6acb809
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-12-08 10:32:23 +0100

    num-utils: disable

--- Formula/num-utils.rb
+++ Formula/num-utils.rb
@@ -16,6 +16,8 @@ class NumUtils < Formula
     sha256 "188ff1f94691f8bf5099ec1012d4732be8fa385bf738671f86780376dd2597b9" => :mavericks
   end
 
+  disable! date: "2020-12-08", because: :unmaintained
+
   conflicts_with "normalize", because: "both install `normalize` binaries"
   conflicts_with "crush-tools", because: "both install an `range` binary"
   conflicts_with "argyll-cms", because: "both install `average` binaries"
