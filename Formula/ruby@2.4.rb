5176c183c79dc179204d68ad300e1363b8359893
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-08 14:18:57 +0000

    ruby@2.4: update 2.4.10 bottle.

--- Formula/ruby@2.4.rb
+++ Formula/ruby@2.4.rb
@@ -6,10 +6,10 @@ class RubyAT24 < Formula
   license "Ruby"
 
   bottle do
-    sha256 "1a7a65cd2e59851019c7629e4e441595722af8918e97a62e439c4245caa2ac3b" => :big_sur
-    sha256 "0af834c6be2b5b377eb53b27895d0eb5cfcf9298b7613197f4d3cc83bc3fdad5" => :catalina
-    sha256 "160204426fc18dfead0ceeb0be27eba371a68996792d6a8258b30bdfca8ef5f6" => :mojave
-    sha256 "c8f457842cfc697ead2e961539742995bf1a6782de9cecd0b293c9fe6107b9d3" => :high_sierra
+    rebuild 1
+    sha256 "155e0689032570a5832a6f26775037997748dd7e3e5fd458fc2ccb8c00a199ae" => :big_sur
+    sha256 "9e555e1492425fad0adc1288ce6bfaeff3b5845327f20c4924195a7020b9be4f" => :catalina
+    sha256 "1e7496f7681fcd00ea19df6f7f15f65472bb18e86c47fc2c479af631304e6dbd" => :mojave
   end
 
   keg_only :versioned_formula
