b858c33f21a49b48b1ccf0262bddf9d215e79c95
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-08 18:17:22 +0000

    avro-c: update 1.10.1 bottle.

--- Formula/avro-c.rb
+++ Formula/avro-c.rb
@@ -11,10 +11,9 @@ class AvroC < Formula
   end
 
   bottle do
-    sha256 "7abc536f295117964af2d32b3d16a48482109653a71a55614b9f0f4a0a4ec7a9" => :big_sur
-    sha256 "44708472b87d7d9375ed156ad510f1884b050d4cc345bfef49e6e9f2bc7a756c" => :catalina
-    sha256 "971102fc6b294cb8b98759edba3e390d376d7660eb49c6ed9063e03cf0cf2067" => :mojave
-    sha256 "6aec75d7260ab6eb56a7c7b6bceb03f547947a2b1c0eba579ad21a7b048b0367" => :high_sierra
+    sha256 "c99913941625740c45fd76a1fa8a69750f585375c0559af04284d8bed2cab872" => :big_sur
+    sha256 "2b10e0adb725faf050ecc7c3ffccec6f9a003714ccd76d544b0d644253d2626f" => :catalina
+    sha256 "0fc00159098c04997d2537f79283517497f01a8100a4a3cba8ac09099844a80f" => :mojave
   end
 
   depends_on "cmake" => :build
