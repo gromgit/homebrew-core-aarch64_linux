4db406e05380d1b3c678dd3a73c2c0d877ac4a27
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-12-08 19:07:46 +0100

    gnu-getopt: remove arm64_big_sur bottle

--- Formula/gnu-getopt.rb
+++ Formula/gnu-getopt.rb
@@ -8,7 +8,6 @@ class GnuGetopt < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "3a9e3eed0d4d9dd91a6a87f4faccad9a2a653581b119fa8cfb637efbb0f5f260" => :big_sur
-    sha256 "9721ed0b3f0c2220ec8401ff0c54a5138bb84aed086e0dcd50701bbebe7656ab" => :arm64_big_sur
     sha256 "5b79b3c5e0792ef471e8a45e7be1c6b53cfc82b06dc702269404bc2105c801e1" => :catalina
     sha256 "be850eb3ab001ca8ef8f34fbe7fed93b784462a9b88c68011854a977cf492b06" => :mojave
     sha256 "94ec5fdb67ca588ae797498c9a3a5b53aa8fea773cb6350d0d611f1248ab693a" => :high_sierra
