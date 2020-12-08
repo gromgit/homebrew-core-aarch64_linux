0d441d5d022d701cddfafe2cde9035000a6cf364
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-12-08 12:25:01 +0100

    protobuf-swift: add disable date

--- Formula/protobuf-swift.rb
+++ Formula/protobuf-swift.rb
@@ -14,7 +14,7 @@ class ProtobufSwift < Formula
   end
 
   # https://github.com/Homebrew/homebrew-core/pull/54471#issuecomment-627430555
-  disable! because: :unmaintained
+  disable! date: "2020-05-10", because: :unmaintained
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
