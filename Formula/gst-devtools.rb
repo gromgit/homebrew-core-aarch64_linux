53b1d72ca92bc4fc1266ad8be49cdde2021af56c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-08 03:21:21 +0000

    gst-devtools: update 1.18.2 bottle.

--- Formula/gst-devtools.rb
+++ Formula/gst-devtools.rb
@@ -9,10 +9,9 @@ class GstDevtools < Formula
   head "https://gitlab.freedesktop.org/gstreamer/gst-devtools.git"
 
   bottle do
-    sha256 "7d0942271189310a4f67660a8850f124172292c91ae3e76be64c62c1bd523a19" => :big_sur
-    sha256 "4befaddc224b6621effeed0b92e336b6ec6cc8ea2be44fb5ccfe681561d7509b" => :catalina
-    sha256 "fbca18af1a8412f2a58ca7c3b0166e3e2019b1673ec493c252ae522c3094ad14" => :mojave
-    sha256 "0eb56251926b5ecc220708f8da188d1585473dfbd59a629d8a8351a971678f76" => :high_sierra
+    sha256 "e156e8077e6ffc8efc6978d498376080d6e16fd39ae967f867c72caca49fc501" => :big_sur
+    sha256 "0ad99a02a5f6b69fabaed767a22af18d97a84695362a9db8a2a267d9fd2afd86" => :catalina
+    sha256 "c6d5be2d2ac2b289d55f48b319f2e84b3c79cb96f41571a629a0f28c31f69be1" => :mojave
   end
 
   depends_on "gobject-introspection" => :build
