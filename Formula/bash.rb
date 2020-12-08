5987b9ad94a078352ed71ec52e0089727918dae3
Alexander Bayandin <a.bayandin@gmail.com>
2020-12-08 12:48:22 +0000

    bash 5.1
    Closes #66471.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bash.rb
+++ Formula/bash.rb
@@ -1,54 +1,12 @@
 class Bash < Formula
   desc "Bourne-Again SHell, a UNIX command interpreter"
   homepage "https://www.gnu.org/software/bash/"
+  url "https://ftp.gnu.org/gnu/bash/bash-5.1.tar.gz"
+  mirror "https://ftpmirror.gnu.org/bash/bash-5.1.tar.gz"
+  mirror "https://mirrors.kernel.org/gnu/bash/bash-5.1.tar.gz"
+  mirror "https://mirrors.ocf.berkeley.edu/gnu/bash/bash-5.1.tar.gz"
   license "GPL-3.0-or-later"
   head "https://git.savannah.gnu.org/git/bash.git"
-
-  stable do
-    url "https://ftp.gnu.org/gnu/bash/bash-5.0.tar.gz"
-    mirror "https://ftpmirror.gnu.org/bash/bash-5.0.tar.gz"
-    mirror "https://mirrors.kernel.org/gnu/bash/bash-5.0.tar.gz"
-    mirror "https://mirrors.ocf.berkeley.edu/gnu/bash/bash-5.0.tar.gz"
-    sha256 "b4a80f2ac66170b2913efbfb9f2594f1f76c7b1afd11f799e22035d63077fb4d"
-    version "5.0.18"
-
-    # Fix configure detection of strsignal() and snprintf() with Xcode 12
-    # Remove for version 5.1
-    # https://savannah.gnu.org/patch/index.php?9991
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/cda4fced/bash/bash.patch"
-      sha256 "4c478ecedcc33efa1b71679d479453940fb3a210709cd03a00c2b56e64328631"
-    end
-
-    %w[
-      001 f2fe9e1f0faddf14ab9bfa88d450a75e5d028fedafad23b88716bd657c737289
-      002 87e87d3542e598799adb3e7e01c8165bc743e136a400ed0de015845f7ff68707
-      003 4eebcdc37b13793a232c5f2f498a5fcbf7da0ecb3da2059391c096db620ec85b
-      004 14447ad832add8ecfafdce5384badd933697b559c4688d6b9e3d36ff36c62f08
-      005 5bf54dd9bd2c211d2bfb34a49e2c741f2ed5e338767e9ce9f4d41254bf9f8276
-      006 d68529a6ff201b6ff5915318ab12fc16b8a0ebb77fda3308303fcc1e13398420
-      007 17b41e7ee3673d8887dd25992417a398677533ab8827938aa41fad70df19af9b
-      008 eec64588622a82a5029b2776e218a75a3640bef4953f09d6ee1f4199670ad7e3
-      009 ed3ca21767303fc3de93934aa524c2e920787c506b601cc40a4897d4b094d903
-      010 d6fbc325f0b5dc54ddbe8ee43020bced8bd589ddffea59d128db14b2e52a8a11
-      011 2c4de332b91eaf797abbbd6c79709690b5cbd48b12e8dfe748096dbd7bf474ea
-      012 2943ee19688018296f2a04dbfe30b7138b889700efa8ff1c0524af271e0ee233
-      013 f5d7178d8da30799e01b83a0802018d913d6aa972dd2ddad3b927f3f3eb7099a
-      014 5d6eee6514ee6e22a87bba8d22be0a8621a0ae119246f1c5a9a35db1f72af589
-      015 a517df2dda93b26d5cbf00effefea93e3a4ccd6652f152f4109170544ebfa05e
-      016 ffd1d7a54a99fa7f5b1825e4f7e95d8c8876bc2ca151f150e751d429c650b06d
-      017 4cf3b9fafb8a66d411dd5fc9120032533a4012df1dc6ee024c7833373e2ddc31
-      018 7c314e375a105a6642e8ed44f3808b9def89d15f7492fe2029a21ba9c0de81d3
-    ].each_slice(2) do |p, checksum|
-      patch :p0 do
-        url "https://ftp.gnu.org/gnu/bash/bash-5.0-patches/bash50-#{p}"
-        mirror "https://ftpmirror.gnu.org/bash/bash-5.0-patches/bash50-#{p}"
-        mirror "https://mirrors.ocf.berkeley.edu/gnu/bash/bash-5.0-patches/bash50-#{p}"
-        mirror "https://mirrors.kernel.org/gnu/bash/bash-5.0-patches/bash50-#{p}"
-        sha256 checksum
-      end
-    end
-  end
 
   livecheck do
     url "http://www.ravenports.com/catalog/bucket_C8/bash/standard/"
@@ -72,11 +30,6 @@ class Bash < Formula
     # things (e.g. git+ssh) will break if the user sets their default shell to
     # Homebrew's bash instead of /bin/bash.
     ENV.append_to_cflags "-DSSH_SOURCE_BASHRC"
-
-    # Work around configure issues with Xcode 12
-    # https://savannah.gnu.org/patch/index.php?9991
-    # Remove for version 5.1
-    ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
 
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
