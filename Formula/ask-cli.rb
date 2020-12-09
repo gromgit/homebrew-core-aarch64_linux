a156b6146588a5830f32256753258528bd5bf11d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-09 03:17:21 +0000

    ask-cli: update 2.21.0 bottle.

--- Formula/ask-cli.rb
+++ Formula/ask-cli.rb
@@ -13,10 +13,9 @@ class AskCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9e8f632debeed50ae61f7414362ea70ceba3508350d61c832ba35305d13e1f15" => :big_sur
-    sha256 "a1e1c843a99423831f8fb2af1a1130d98bcc8cd2d4b8bd3740f091a9cce0345f" => :catalina
-    sha256 "0daab6ab4fd2f156ab0a8364e3a6490df2edb562877ce3f329c2d012a816dd6a" => :mojave
-    sha256 "00c47456c7aefd69be4707a4771d7d998cd0372100a97fcc5f8187b43b473a92" => :high_sierra
+    sha256 "dc2534a94d1e025fcb2cee11f0464eb1f2eee52ff16aa5a25856ebfd9de5e69d" => :big_sur
+    sha256 "874891decbfcdd0cc14c763ff6ee7788e80df09d4b39f3ada55554bd5d9c6ff9" => :catalina
+    sha256 "25257436266db197a29f26b07203ee8651c87afbf7724241701216eb17479485" => :mojave
   end
 
   depends_on "node"
