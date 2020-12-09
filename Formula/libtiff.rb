0a78070b4ad39e51355bce039cfa36aa733492a6
FX Coudert <fxcoudert@gmail.com>
2020-12-09 17:37:48 +0000

    libtiff: update 4.1.0_1 bottle.

--- Formula/libtiff.rb
+++ Formula/libtiff.rb
@@ -15,6 +15,7 @@ class Libtiff < Formula
   bottle do
     cellar :any
     sha256 "0f66c74d4ba96a1e9bac88a58a52335eaa0944de258e608ef62370e80fc5b24a" => :big_sur
+    sha256 "f45e96a9c8bb58afdb284ef80e9936d816714fd86499230939ca94bb68e30c45" => :arm64_big_sur
     sha256 "d92eb164b8fbe723a6006023f883a77bc02d4e54d8bbb1db6855f1ad7f1f1d6e" => :catalina
     sha256 "6194841cb85000404c089288624f3897faa0c888f1653fb5c5388ba58cc8df8f" => :mojave
     sha256 "75d26fd0a430509b838c5b341221c9bb4a343dfa54dffd626c1f93313e4e512c" => :high_sierra
