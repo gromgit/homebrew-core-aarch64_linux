97fde4b6b15e5e44e7162eaedfa75f4b1672312a
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-12-09 12:52:56 +0100

    securefs: add deprecation date

--- Formula/securefs.rb
+++ Formula/securefs.rb
@@ -14,7 +14,7 @@ class Securefs < Formula
     sha256 "632496d8e9ed9fe91d18e9a2c9fef49c920dc091e10108246f8ab2056f75ea38" => :high_sierra
   end
 
-  deprecate! because: "requires FUSE"
+  deprecate! date: "2020-11-10", because: "requires FUSE"
 
   depends_on "cmake" => :build
   depends_on :osxfuse
