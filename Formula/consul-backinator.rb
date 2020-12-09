e662fd318f60d2d140bd4ce68673c8368e25ecd1
chenrui <chenrui333@gmail.com>
2020-12-09 02:31:04 +0000

    consul-backinator 1.6.6
    * consul-backinator 1.6.6
    * consul-backinator: deprecate gopath and update test
    
    Closes #66534.
    
    Signed-off-by: Rui Chen <rui@meetup.com>
    Co-authored-by: Rui Chen <rui@meetup.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/consul-backinator.rb
+++ Formula/consul-backinator.rb
@@ -1,8 +1,8 @@
 class ConsulBackinator < Formula
   desc "Consul backup and restoration application"
   homepage "https://github.com/myENA/consul-backinator"
-  url "https://github.com/myENA/consul-backinator/archive/v1.6.5.tar.gz"
-  sha256 "e464d597a3a28c6376e0d602c9484a465476db13684585bd52c6b5d81b07019d"
+  url "https://github.com/myENA/consul-backinator/archive/v1.6.6.tar.gz"
+  sha256 "b668801ca648ecf888687d7aa69d84c3f2c862f31b92076c443fdea77c984c58"
   license "MPL-2.0"
   head "https://github.com/myENA/consul-backinator.git"
 
@@ -16,27 +16,16 @@ class ConsulBackinator < Formula
     sha256 "bb39c88ad9e3e5aa6b12ea08bbd6ec2b31601d0c14f943aaaf10bfcf14cc5b8d" => :el_capitan
   end
 
-  depends_on "glide" => :build
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
-
-    dir = buildpath/"src/github.com/myENA/consul-backinator"
-    dir.install buildpath.children
-
-    cd dir do
-      system "glide", "install"
-      system "go", "build", "-v", "-ldflags",
-             "-X main.appVersion=#{version}", "-o",
-             bin/"consul-backinator"
-      prefix.install_metafiles
-    end
+    system "go", "build", *std_go_args, "-ldflags", "-s -w -X main.appVersion=#{version}"
   end
 
   test do
-    output = shell_output("#{bin}/consul-backinator --version 2>&1")
-    assert_equal version.to_s, output.strip
+    assert_equal version.to_s, shell_output("#{bin}/consul-backinator --version 2>&1").strip
+
+    assert_match "[Error] Failed to backup key data:",
+      shell_output("#{bin}/consul-backinator backup 2>&1", 1)
   end
 end
