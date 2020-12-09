dd80dd1fc8549b955191f1db8677d7483aacbcb3
carlocab <30379873+carlocab@users.noreply.github.com>
2020-12-09 21:20:17 +0000

    zile 2.6.0.90
    * zile 2.6.0.90
    * zile: add license
      License found on homepage:
    
      > Zile is free software; you can redistribute it and/or modify it under
      > the terms of the GNU General Public License as published by the Free
      > Software Foundation; either version 3 of the License, or (at your
      > option) any later version.
    
      https://www.gnu.org/software/zile/
    * zile: add glib dependency
    * zile: add libgee dependency
    
    Closes #66602.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/zile.rb
+++ Formula/zile.rb
@@ -1,9 +1,10 @@
 class Zile < Formula
   desc "Text editor development kit"
   homepage "https://www.gnu.org/software/zile/"
-  url "https://ftp.gnu.org/gnu/zile/zile-2.4.15.tar.gz"
-  mirror "https://ftpmirror.gnu.org/zile/zile-2.4.15.tar.gz"
-  sha256 "39c300a34f78c37ba67793cf74685935a15568e14237a3a66fda8fcf40e3035e"
+  url "https://ftp.gnu.org/gnu/zile/zile-2.6.0.90.tar.gz"
+  mirror "https://ftpmirror.gnu.org/zile/zile-2.6.0.90.tar.gz"
+  sha256 "239b5b9575e3310205912cb87a25a6bff0d951feb7623722041ee2aa95e50dae"
+  license "GPL-3.0-or-later"
 
   livecheck do
     url :stable
@@ -20,6 +21,8 @@ class Zile < Formula
   depends_on "help2man" => :build
   depends_on "pkg-config" => :build
   depends_on "bdw-gc"
+  depends_on "glib"
+  depends_on "libgee"
 
   uses_from_macos "ncurses"
 
