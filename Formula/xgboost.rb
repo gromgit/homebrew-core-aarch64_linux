fb61e295306fe4fcdd8e53e104d5a0e1a52b1509
chenrui <chenrui333@gmail.com>
2020-12-09 00:41:10 +0000

    xgboost 1.3.0
    * xgboost 1.3.0
    * xgboost: depends_on numpy, scipy
    * xgboost: fix test
    
    Closes #66529.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xgboost.rb
+++ Formula/xgboost.rb
@@ -2,8 +2,8 @@ class Xgboost < Formula
   desc "Scalable, Portable and Distributed Gradient Boosting Library"
   homepage "https://xgboost.ai/"
   url "https://github.com/dmlc/xgboost.git",
-      tag:      "v1.2.1",
-      revision: "bcb15a980f611ac0b7540ddcb55091d0ab9655aa"
+      tag:      "v1.3.0",
+      revision: "1bf389998383f333490155dba4608bff9ca63b42"
   license "Apache-2.0"
 
   bottle do
@@ -16,16 +16,8 @@ class Xgboost < Formula
 
   depends_on "cmake" => :build
   depends_on "libomp"
-
-  resource "numpy" do
-    url "https://files.pythonhosted.org/packages/bf/e8/15aea783ea72e2d4e51e3ec365e8dc4a1a32c9e5eb3a6d695b0d58e67cdd/numpy-1.19.2.zip"
-    sha256 "0d310730e1e793527065ad7dde736197b705d0e4c9999775f212b03c44a8484c"
-  end
-
-  resource "scipy" do
-    url "https://files.pythonhosted.org/packages/93/63/4a566494594a13697c5d5d8a754d6e329d018ddf881520775e0229fa29ef/scipy-1.5.3.tar.gz"
-    sha256 "ddae76784574cc4c172f3d5edd7308be16078dd3b977e8746860c76c195fa707"
-  end
+  depends_on "numpy"
+  depends_on "scipy"
 
   def install
     mkdir "build" do
@@ -39,7 +31,7 @@ class Xgboost < Formula
   test do
     cp_r (pkgshare/"demo"), testpath
     cd "demo/data" do
-      cp "../binary_classification/mushroom.conf", "."
+      cp "../CLI/binary_classification/mushroom.conf", "."
       system "#{bin}/xgboost", "mushroom.conf"
     end
   end
