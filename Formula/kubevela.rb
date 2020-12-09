187784a179325324bc3a54510cbe9c54e7c5bce4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-09 07:17:13 +0000

    kubevela: update 0.2.1 bottle.

--- Formula/kubevela.rb
+++ Formula/kubevela.rb
@@ -8,9 +8,9 @@ class Kubevela < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "496c4a442d98b68f7f2d12ca5888a7585d0074ad6a0b794a9db190f96fede506" => :big_sur
-    sha256 "e6445adb63a542e5684ec4c7396842a99544d87a507de8cb8762778c87abde3b" => :catalina
-    sha256 "6c70afb1e7f05898cfbf721ebd7f6f37239e9d00fc850d2e538a8ee3ddc4a644" => :mojave
+    sha256 "e3a7cf287d88fbdd347424f965207303a20e2487dd9cf41cc22f59dcb876f571" => :big_sur
+    sha256 "3a58d9e061245932fb0ecc5215cb5ba509b1b8b23de528fe585f8c4667e36d9a" => :catalina
+    sha256 "f888334b304d793720e5af1ebdceefe32873dbc8fd1b8fb65f4af0b341f551fc" => :mojave
   end
 
   depends_on "go" => :build
