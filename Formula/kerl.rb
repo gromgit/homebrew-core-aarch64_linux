09ebfecf4e890a5c57d06defd74f152d3b9712c5
bepsvpt <8221099+bepsvpt@users.noreply.github.com>
2020-12-11 02:14:56 +0800

    kerl 2.0.2 (#66665)
    Co-authored-by: homebrew-updater <bepsvpt/homebrew-updater>

--- Formula/kerl.rb
+++ Formula/kerl.rb
@@ -1,8 +1,8 @@
 class Kerl < Formula
   desc "Easy building and installing of Erlang/OTP instances"
   homepage "https://github.com/kerl/kerl"
-  url "https://github.com/kerl/kerl/archive/2.0.1.tar.gz"
-  sha256 "0a23bfe5fb86da536ed0041d21eb775e82dfd6e8c07e5d90013beea5ef4af725"
+  url "https://github.com/kerl/kerl/archive/2.0.2.tar.gz"
+  sha256 "d2addb942489ad96aa73042ed350b06d9add6fb0075fbd28b84b591541e7fac0"
   license "MIT"
   head "https://github.com/kerl/kerl.git"
 
