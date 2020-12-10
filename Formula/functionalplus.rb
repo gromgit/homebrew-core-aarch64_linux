f3bc5a8288fc54094f9804f9a13957e340ce5715
carlocab <30379873+carlocab@users.noreply.github.com>
2020-12-10 06:33:22 +0000

    functionalplus 0.2.13
    Closes #66641.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/functionalplus.rb
+++ Formula/functionalplus.rb
@@ -1,9 +1,9 @@
 class Functionalplus < Formula
   desc "Functional Programming Library for C++"
   homepage "https://github.com/Dobiasd/FunctionalPlus"
-  url "https://github.com/Dobiasd/FunctionalPlus/archive/v0.2.10-p0.tar.gz"
-  version "0.2.10"
-  sha256 "cfb434dc63b6409e5f09e66b997261cd808ee1b59d931f732f2017beaaa88e90"
+  url "https://github.com/Dobiasd/FunctionalPlus/archive/v0.2.13-p0.tar.gz"
+  version "0.2.13"
+  sha256 "62f61ce6500859f0d77306b1644b5c6992287688de38e170b17b8a66b2448b54"
   license "BSL-1.0"
   head "https://github.com/Dobiasd/FunctionalPlus.git"
 
