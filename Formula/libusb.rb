b19ea71d5555e364361c8e67dd1110d1762fef0b
carlocab <30379873+carlocab@users.noreply.github.com>
2020-12-10 19:55:07 +0000

    libusb 1.0.24
    * libusb 1.0.24
    * add license
      See https://github.com/libusb/libusb/search?q=license
    
    Closes #66673.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libusb.rb
+++ Formula/libusb.rb
@@ -1,9 +1,9 @@
 class Libusb < Formula
   desc "Library for USB device access"
   homepage "https://libusb.info/"
-  url "https://github.com/libusb/libusb/releases/download/v1.0.23/libusb-1.0.23.tar.bz2"
-  sha256 "db11c06e958a82dac52cf3c65cb4dd2c3f339c8a988665110e0d24d19312ad8d"
-  license "LGPL-2.1"
+  url "https://github.com/libusb/libusb/releases/download/v1.0.24/libusb-1.0.24.tar.bz2"
+  sha256 "7efd2685f7b327326dcfb85cee426d9b871fd70e22caa15bb68d595ce2a2b12a"
+  license "LGPL-2.1-or-later"
 
   livecheck do
     url :stable
