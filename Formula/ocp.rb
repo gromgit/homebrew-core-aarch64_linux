0d581d6236426eb6e6e84f9de826fae3144ec28c
Mitchell Blank Jr <mitch@bodyfour.com>
2020-12-10 02:39:46 +0000

    ocp 0.2.2
    * ocp: update to 0.2.2
      This also fixes a problem where it couldn't build on Xcode 12
    * ocp: add pkg-config dep
    
    Closes #66623.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ocp.rb
+++ Formula/ocp.rb
@@ -1,9 +1,15 @@
 class Ocp < Formula
   desc "UNIX port of the Open Cubic Player"
   homepage "https://stian.cubic.org/project-ocp.php"
-  url "https://stian.cubic.org/ocp/ocp-0.2.1.tar.bz2"
-  sha256 "238ed6547e4c96b775d93aa6e4252982a763f62ecd201f6312f041212edc1798"
-  license "GPL-2.0"
+  url "https://stian.cubic.org/ocp/ocp-0.2.2.tar.xz"
+  sha256 "afd07a6ae2af4733eb06e516cb607276ee084d1d30aa1cf0c3fd6e62f1d3a144"
+  license "GPL-2.0-or-later"
+  head "https://github.com/mywave82/opencubicplayer.git"
+
+  livecheck do
+    url :head
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
 
   bottle do
     sha256 "d9c557bc2f3161818fcf4701e7cc123bd6d2f85ff9e80df5976392de9102a737" => :catalina
@@ -11,20 +17,16 @@ class Ocp < Formula
     sha256 "6b40bde3ba007a8b18451502bcf49841d8a3f75ec06a7d6a8e748f508e7dc1f9" => :high_sierra
   end
 
+  depends_on "pkg-config" => :build
+  depends_on "xa" => :build
   depends_on "flac"
+  depends_on "jpeg-turbo"
+  depends_on "libpng"
   depends_on "libvorbis"
   depends_on "mad"
 
-  # Fix duplicate symbol errors, remove in next release
-  # https://github.com/mywave82/opencubicplayer/issues/15
-  patch do
-    url "https://github.com/mywave82/opencubicplayer/commit/04368dc54e649050517fe3a058e919fb5fb5f150.patch?full_index=1"
-    sha256 "27ba15871499ffcc540a59fcb2435945ab842af74265d1032a1bf0831c5cbe2b"
-  end
-  patch do
-    url "https://github.com/mywave82/opencubicplayer/commit/1907b8f85e3e5fa61a02ad0ca3ce9bd30bfc0ea6.patch?full_index=1"
-    sha256 "3260bcfbd27e4aa64081674081e9f06963b705b3f89ba8aabaa8f54bb995986f"
-  end
+  uses_from_macos "ncurses"
+  uses_from_macos "zlib"
 
   def install
     ENV.deparallelize
@@ -36,6 +38,11 @@ class Ocp < Formula
       --without-sdl2
       --without-desktop_file_install
     ]
+
+    # Workaround for bad compiler version check: https://github.com/mywave82/opencubicplayer/issues/30
+    inreplace "configure",
+              "2.95.[2-9]|2.95.[2-9][-].*|3.[0-9]|3.[0-9].[0-9]|3.[0-9]|3.[0-9].[0-9]-*|4.*|5.*|6.*|7*|8*|9*|10*",
+              "[1-9]*"
 
     system "./configure", *args
     system "make"
