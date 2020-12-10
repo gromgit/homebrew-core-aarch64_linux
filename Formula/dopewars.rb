410a477bdfbf50293653f63e9ae81ef2fdb3ba83
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-10 11:16:57 +0000

    dopewars: update 1.6.0 bottle.

--- Formula/dopewars.rb
+++ Formula/dopewars.rb
@@ -10,11 +10,9 @@ class Dopewars < Formula
   end
 
   bottle do
-    sha256 "ff4f79046cbffb1d786f4acd203d7bcbc369b929e603011e3f64ce6c22a29339" => :big_sur
-    sha256 "f2e1a3e2e6199fc550af9afc8204b0292a34976f85ec2448fee549b434048c34" => :catalina
-    sha256 "8bb4cbd11a3db0bbdbdd283d531742c9485dc1d86b57b9986f3b86da01947807" => :mojave
-    sha256 "3808bf43bb96b796624f8ffb855b176ea2a908f3b9477fd7d07a1f960dff0ef2" => :high_sierra
-    sha256 "db1c91122cf53f166a5811595bbf84b63227818ca11877b78a21592686a975f2" => :sierra
+    sha256 "8d9aa3ab6faa85c24e9fa3046191b611295785a68f08de5e2d84aa59cdd55713" => :big_sur
+    sha256 "0e20a6168f15c6c1245818efc5d5986b77d90d51f184a8e6b06538579d2b8461" => :catalina
+    sha256 "85744f87e4866796bf4b06ad280f57542d951ec9498ebf814251448f93e24881" => :mojave
   end
 
   depends_on "pkg-config" => :build
