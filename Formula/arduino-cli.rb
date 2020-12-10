6789ca0cfb2adc623715fb2e44d9806430f80717
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-10 16:25:10 +0000

    arduino-cli: update 0.14.0 bottle.

--- Formula/arduino-cli.rb
+++ Formula/arduino-cli.rb
@@ -14,10 +14,9 @@ class ArduinoCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f5bf7e8be260422b006a669efd8440d7aa07e7c8e00d79988c14bb8edc0ee28d" => :big_sur
-    sha256 "b5927718821674c171e8853f97c83331216bea92257168e1bd4e830fe88d5e58" => :catalina
-    sha256 "4522d66a123be600d11f7c7b76dbc327f139cc060564c920fcf7c1d888a07ede" => :mojave
-    sha256 "fc4945e6f995a7367d807edcb31708a2d5d7d82cfec22b26b1187133db40b8a6" => :high_sierra
+    sha256 "234182b6dcb88edd148b34a0602c3ee6473fd23d35cd8d3bb3881076739f10d9" => :big_sur
+    sha256 "b3c8840c27606d351488b2df73145e26fac19851caaf58b07e5cea41caefe1b0" => :catalina
+    sha256 "cd4767c40d06c4da8cdee1b91c2fb75af9509ec67e062a11b69af974bf11222d" => :mojave
   end
 
   depends_on "go" => :build
