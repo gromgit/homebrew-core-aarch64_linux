5e869d6999cff27e0b6ba9d0d7f113f5629e715f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-10 05:17:10 +0000

    firebase-cli: update 8.18.1 bottle.

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -14,9 +14,9 @@ class FirebaseCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "362d2ad76d8bf4166dad897f0febd6a6a1693b411c47d8fb62057133a759858e" => :big_sur
-    sha256 "925b4242499910eb9d415f2e80abd5e4bc77ba710c86846c9547c116b9690549" => :catalina
-    sha256 "96ff650ef7516a89184a08098fdfc9ae0cd0f1d800d3548591b67e0d31541828" => :mojave
+    sha256 "d5dfddc946be8eb568cca4f4ef4f85dfaae3e72dcbc1726da8ed1385a501c679" => :big_sur
+    sha256 "177cdd5283285c331e19b0d6ab96c7f813b1d0595c428c70a2039cfcae4dd78c" => :catalina
+    sha256 "e48269f274126e0aa1134dc5b1eb0a0d8c89def6c4f5adc2b8064232dd861b51" => :mojave
   end
 
   depends_on "node"
