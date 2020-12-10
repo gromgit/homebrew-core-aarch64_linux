91b639d92c92465844c83ea68e377a1374199ba5
Michael Warkentin <mwarkentin@gmail.com>
2020-12-10 12:26:18 -0500

    semtag 0.1.1
    * Update semtag to 0.1.1
    * Revert test change
    
    Closes #66663.
    
    Co-authored-by: Michael Warkentin <mwarkentin@hey.com>
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/semtag.rb
+++ Formula/semtag.rb
@@ -1,8 +1,8 @@
 class Semtag < Formula
   desc "Semantic tagging script for git"
   homepage "https://github.com/pnikosis/semtag"
-  url "https://github.com/pnikosis/semtag/archive/v0.1.0.tar.gz"
-  sha256 "9e16a418b795363a9283318e5dc02da8a67ec96adaac0ae8eb23f1b21b06666f"
+  url "https://github.com/pnikosis/semtag/archive/v0.1.1.tar.gz"
+  sha256 "c7becf71c7c14cdef26d3980c3116cce8dad6cd9eb61abcc4d2ff04e2c0f8645"
   license "Apache-2.0"
 
   bottle :unneeded
