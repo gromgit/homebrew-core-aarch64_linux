c08f95380ad2b39191d999504b8a6b1300165c8e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-10 11:16:37 +0000

    functionalplus: update 0.2.13 bottle.

--- Formula/functionalplus.rb
+++ Formula/functionalplus.rb
@@ -9,10 +9,9 @@ class Functionalplus < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e30a27bf78a8820d4d63a4ae87520afc97d3f1a0d93ac650b7a9fad79589b47a" => :big_sur
-    sha256 "8fee519ccd0485bd871b10f8d12d2324efd929c69d8b2aac3754fb47a4313e2c" => :catalina
-    sha256 "d4785dd619c7e46280a175f47ccf8dab03c076190fd2c5a11724bfae6d785e1d" => :mojave
-    sha256 "5102b2c7d93ed23aa46aa46e8c455b0edd2b54556c69be9aa459775a2faeed1d" => :high_sierra
+    sha256 "244d54ac6b458a8f170a5eba497817b5b22b374e834d65537d674ab9e39c666c" => :big_sur
+    sha256 "73e1e7337735aefb7deed60dd5a2fa21f6e08852c3ed2b030e599196c7af2328" => :catalina
+    sha256 "7c71a5c706e7b89288b9f9897eaee2a22f4b375ef710c68d94fe131b012daa6d" => :mojave
   end
 
   depends_on "cmake" => :build
