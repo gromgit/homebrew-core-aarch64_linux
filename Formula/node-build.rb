770d2b960dc27b94e8e1ce7a00c3a0c2e5f497ab
nodenv machine <68817010+nodenv-bot@users.noreply.github.com>
2020-12-10 05:53:42 -0500

    node-build 4.9.21 (#66645)

--- Formula/node-build.rb
+++ Formula/node-build.rb
@@ -1,8 +1,8 @@
 class NodeBuild < Formula
   desc "Install NodeJS versions"
   homepage "https://github.com/nodenv/node-build"
-  url "https://github.com/nodenv/node-build/archive/v4.9.20.tar.gz"
-  sha256 "059cffeb7a2d321e6a8e0b8e3ef3c42418633035682a250deda351f2782d7175"
+  url "https://github.com/nodenv/node-build/archive/v4.9.21.tar.gz"
+  sha256 "3c751a3ece3eed8366d038eb24c72c4ff2e3f9a712a4b5b57270b15179b207a3"
   license "MIT"
   head "https://github.com/nodenv/node-build.git"
 
