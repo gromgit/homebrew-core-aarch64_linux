e3db24387f503a8016d47d52fbdb82a5359cc2df
Aman Karmani <aman@tmm1.net>
2020-12-09 21:36:11 -0800

    nzbget: rebuild
    * nzbget: use homebrew prefix
    * nzbget: update license
    
    Closes #66632.
    
    Co-authored-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nzbget.rb
+++ Formula/nzbget.rb
@@ -3,7 +3,7 @@ class Nzbget < Formula
   homepage "https://nzbget.net/"
   url "https://github.com/nzbget/nzbget/releases/download/v21.0/nzbget-21.0-src.tar.gz"
   sha256 "65a5d58eb8f301e62cf086b72212cbf91de72316ffc19182ae45119ddd058d53"
-  license "GPL-2.0"
+  license "GPL-2.0-or-later"
   revision 1
   head "https://github.com/nzbget/nzbget.git", branch: "develop"
 
@@ -62,12 +62,23 @@ class Nzbget < Formula
       <dict>
         <key>Label</key>
         <string>#{plist_name}</string>
+        <key>EnvironmentVariables</key>
+        <dict>
+          <key>PATH</key>
+          <string>#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
+        </dict>
         <key>ProgramArguments</key>
         <array>
           <string>#{opt_bin}/nzbget</string>
+          <string>-c</string>
+          <string>#{HOMEBREW_PREFIX}/etc/nzbget.conf</string>
           <string>-s</string>
           <string>-o</string>
           <string>OutputMode=Log</string>
+          <string>-o</string>
+          <string>ConfigTemplate=#{HOMEBREW_PREFIX}/opt/nzbget/share/nzbget/nzbget.conf</string>
+          <string>-o</string>
+          <string>WebDir=#{HOMEBREW_PREFIX}/opt/nzbget/share/nzbget/webui</string>
         </array>
         <key>RunAtLoad</key>
         <true/>
