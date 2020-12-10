704dcd4b7bf7f92ca32ce0133110671fb4ccff95
carlocab <30379873+carlocab@users.noreply.github.com>
2020-12-10 22:08:44 +0000

    mg 6.8.1
    * mg 6.8.1
    * add license
      License information documented here: https://github.com/ibara/mg/blob/47094458190a6704d1797852f4b80a26052df389/README.md#licensing
    
    Closes #66689.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mg.rb
+++ Formula/mg.rb
@@ -1,8 +1,9 @@
 class Mg < Formula
   desc "Small Emacs-like editor"
   homepage "https://github.com/ibara/mg"
-  url "https://github.com/ibara/mg/releases/download/mg-6.7/mg-6.7.tar.gz"
-  sha256 "02583d90df743e994fb1e411befbd23488fd1eaeb82c9db1fd4957d1a8f1abde"
+  url "https://github.com/ibara/mg/releases/download/mg-6.8.1/mg-6.8.1.tar.gz"
+  sha256 "a4af7afa77fed691096be8e2ff0507cc6bdd8efe7255916f714168d02790044c"
+  license all_of: [:public_domain, "ISC", :cannot_represent]
   version_scheme 1
 
   bottle do
