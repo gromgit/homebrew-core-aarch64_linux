023f6a12e1a4346395a73bef671b5d02f026d998
Michka Popoff <michkapopoff@gmail.com>
2020-12-10 23:47:49 +0100

    gettext: build brewed libxml2
    Aligns formula with linuxbrew-core and makes
    sure we do not pick up a random libxml library.
    
    Fix license too
    
    Closes #66693.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gettext.rb
+++ Formula/gettext.rb
@@ -4,7 +4,7 @@ class Gettext < Formula
   url "https://ftp.gnu.org/gnu/gettext/gettext-0.21.tar.xz"
   mirror "https://ftpmirror.gnu.org/gettext/gettext-0.21.tar.xz"
   sha256 "d20fcbb537e02dcf1383197ba05bd0734ef7bf5db06bdb241eb69b7d16b73192"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
 
   livecheck do
     url :stable
@@ -18,25 +18,33 @@ class Gettext < Formula
     sha256 "5ac5783e31205b92907b46bfaaa142620aea7ee3fc4d996876b0913fd2315695" => :high_sierra
   end
 
+  uses_from_macos "libxml2"
   uses_from_macos "ncurses"
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--disable-debug",
-                          "--prefix=#{prefix}",
-                          "--with-included-gettext",
-                          "--with-included-glib",
-                          "--with-included-libcroco",
-                          "--with-included-libunistring",
-                          "--with-emacs",
-                          "--with-lispdir=#{elisp}",
-                          "--disable-java",
-                          "--disable-csharp",
-                          # Don't use VCS systems to create these archives
-                          "--without-git",
-                          "--without-cvs",
-                          "--without-xz"
+    args = [
+      "--disable-dependency-tracking",
+      "--disable-silent-rules",
+      "--disable-debug",
+      "--prefix=#{prefix}",
+      "--with-included-gettext",
+      "--with-included-glib",
+      "--with-included-libcroco",
+      "--with-included-libunistring",
+      "--with-included-libxml",
+      "--with-emacs",
+      "--with-lispdir=#{elisp}",
+      "--disable-java",
+      "--disable-csharp",
+      # Don't use VCS systems to create these archives
+      "--without-git",
+      "--without-cvs",
+      "--without-xz",
+    ]
+    on_linux do
+      args << "--with-libxml2-prefix=#{Formula["libxml2"].opt_prefix}"
+    end
+    system "./configure", *args
     system "make"
     ENV.deparallelize # install doesn't support multiple make jobs
     system "make", "install"
