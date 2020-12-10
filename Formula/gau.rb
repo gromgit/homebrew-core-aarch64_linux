86eaca47bd700db6729bd108676e2082038aaa71
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-10 09:18:01 +0000

    gau: update 1.1.0 bottle.

--- Formula/gau.rb
+++ Formula/gau.rb
@@ -7,10 +7,9 @@ class Gau < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0ae2d716079e4c4d418126c7396446aafc714340368b4fb2ebbd29f63cb2ff30" => :big_sur
-    sha256 "314c4c8636714511a7d09c14a68bf4e60d5096a3e8121863365f35843891ef57" => :catalina
-    sha256 "2ab669a1b9e3321a5c9be26765e62625fb835cf253b7bd3fb702e32036f2c7d8" => :mojave
-    sha256 "1030ebef25ac3961a59c9794a16697d894c084d14933e7df7201d4c99313705b" => :high_sierra
+    sha256 "370fe80090847f4a6ada292142b8b5f591b3cdf6d7c69e73a61b5fe93ab26b24" => :big_sur
+    sha256 "5ed0722411249cadab5035cec15abb31700401404ec1faead6f8ba61be569bff" => :catalina
+    sha256 "b0d9e8d3425c55a20cb3df80644704d280de21949e019ade9ba4fd572a7bf14e" => :mojave
   end
 
   depends_on "go" => :build
