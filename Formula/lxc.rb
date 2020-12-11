961f1081856bf8a99d307e27311831bda9cfc477
Stéphane Graber <stgraber@ubuntu.com>
2020-12-10 19:07:42 -0500

    lxc 4.9
    Closes #66696.
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lxc.rb
+++ Formula/lxc.rb
@@ -1,8 +1,8 @@
 class Lxc < Formula
   desc "CLI client for interacting with LXD"
   homepage "https://linuxcontainers.org"
-  url "https://linuxcontainers.org/downloads/lxd/lxd-4.8.tar.gz"
-  sha256 "de4f096c71448ceb358c0d0d63e34d17ea8e49c15eb9d4f8af5030ce0535337f"
+  url "https://linuxcontainers.org/downloads/lxd/lxd-4.9.tar.gz"
+  sha256 "445e1dfcca4cc14587e083704389fb0bc4de8189597740a35ef3b7acdf56036b"
   license "Apache-2.0"
 
   livecheck do
