9d983c2a202ebe2472294dbb7e0c1ae732e0a093
Rui Chen <rui@meetup.com>
2020-12-11 00:14:24 -0500

    getmail 5.15
    * getmail 5.15
    * Update getmail.rb
    
    Closes #66715.
    
    Co-authored-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/getmail.rb
+++ Formula/getmail.rb
@@ -1,8 +1,9 @@
 class Getmail < Formula
   desc "Extensible mail retrieval system with POP3, IMAP4, SSL support"
   homepage "http://pyropus.ca/software/getmail/"
-  url "http://pyropus.ca/software/getmail/old-versions/getmail-5.14.tar.gz"
-  sha256 "f3a99fe74564237d12ca8d4582e113c067c9205b5ab640f72b4e8447606a99c1"
+  url "http://pyropus.ca/software/getmail/old-versions/getmail-5.15.tar.gz"
+  sha256 "d453805ffc3f8fe2586ee705733bd666777e53693125fdb149494d22bd14162a"
+  license "GPL-2.0-only"
 
   livecheck do
     url :homepage
