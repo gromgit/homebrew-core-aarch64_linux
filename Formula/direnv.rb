9cd0205d819af98831a67b7c96286bc745735f76
chenrui <chenrui333@gmail.com>
2020-12-11 04:34:48 +0000

    direnv: update 2.25.1 bottle.

--- Formula/direnv.rb
+++ Formula/direnv.rb
@@ -8,9 +8,9 @@ class Direnv < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f8b6768c9df0f097a87708b77f6cd1f68808c81567609e7f6a4f6c86fcd3bcc9" => :big_sur
-    sha256 "66b13f4889a97b487f9dd2a6b5a02724dc229cbc33ece4c6458e6fa2a2194e34" => :catalina
-    sha256 "183d350c5096cbb818b08fd71b30ffe5e27ed4b27a6cb1211edb90226645c968" => :mojave
+    sha256 "7bb1d9898ddb856409fc29253bebb8a13842b5c4e7e795c0158b14e352ef7ebf" => :big_sur
+    sha256 "7c5180938264fe48bc3e10975af1c18a5787e138f22df78c6ab769b429f4e37a" => :catalina
+    sha256 "0b94d1d70bd9904bdb6c72674edb3c975500173ceb10a07b63da9382061ee3ca" => :mojave
   end
 
   depends_on "go" => :build
