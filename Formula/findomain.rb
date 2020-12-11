8b229770b28003fe389102f56af05cfc6db78115
FX Coudert <fxcoudert@gmail.com>
2020-12-11 21:43:01 +0000

    findomain: update 2.1.5 bottle.

--- Formula/findomain.rb
+++ Formula/findomain.rb
@@ -7,9 +7,9 @@ class Findomain < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "738cfda36178883eb8083d531385150cba5213c1831af275121d4fe7f6b3d9fb" => :catalina
-    sha256 "0165c9c1915bc01105c4df30f8883e16ede8d48d49bc98735e85da90247864be" => :mojave
-    sha256 "3470286233010baffcff04f55ef25d5bc8662649a14e17832c69fc307857b84a" => :high_sierra
+    sha256 "5b88284143788c5087624b04d751dbedb156e896bfde3ed346b98086ee344524" => :big_sur
+    sha256 "79d8984a41b38f074d1ec4012e5dc88141385ac65f0c56cf6dafd6d6b3642011" => :catalina
+    sha256 "ccfba6e85598fbbf8530070b04b9534cafa9ebf53817926f6c988e5281580929" => :mojave
   end
 
   depends_on "rust" => :build
