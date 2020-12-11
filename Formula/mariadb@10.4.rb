f65f926ae806913d8095e37628ee05c1523bf1c5
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-11 18:07:38 -0500

    mariadb@10.4: add deprecation information

--- Formula/mariadb@10.4.rb
+++ Formula/mariadb@10.4.rb
@@ -19,6 +19,9 @@ class MariadbAT104 < Formula
 
   keg_only :versioned_formula
 
+  # See: https://mariadb.com/kb/en/changes-improvements-in-mariadb-104/
+  deprecate! date: "2024-06-01", because: :unsupported
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "groonga"
