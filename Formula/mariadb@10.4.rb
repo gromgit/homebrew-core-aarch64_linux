77e4e051812ad4bc9688e26ef9d346f8aaa78455
Adrian Ho <the.gromgit@gmail.com>
2020-12-11 20:43:13 +0800

    mariadb@10.4: fix CI test issues
    1. Don't run post-install in CI, so `mysql_install_db` doesn't trip over arbitrary contents of CI box
    2. Run a proper test case

--- Formula/mariadb@10.4.rb
+++ Formula/mariadb@10.4.rb
@@ -114,6 +114,8 @@ class MariadbAT104 < Formula
   end
 
   def post_install
+    return if ENV["CI"]
+
     # Make sure the var/mysql directory exists
     (var/"mysql").mkpath
     unless File.exist? "#{var}/mysql/mysql/user.frm"
@@ -159,8 +161,15 @@ class MariadbAT104 < Formula
   end
 
   test do
-    system bin/"mysqld", "--version"
-    prune_file = etc/"my.cnf.d/.homebrew_dont_prune_me"
-    assert_predicate prune_file, :exist?, "Failed to find #{prune_file}!"
+    (testpath/"mysql").mkpath
+    port = free_port
+    system "#{bin}/mysql_install_db", "--verbose", "--user=#{ENV["USER"]}",
+      "--basedir=#{prefix}", "--datadir=#{testpath}/mysql", "--tmpdir=/tmp"
+    fork do
+      system "#{bin}/mysqld", "--datadir=#{testpath}/mysql", "--port=#{port}"
+    end
+    sleep 5
+    assert_match "information_schema",
+      shell_output("#{bin}/mysql --port=#{port} --user=''@localhost --execute='show databases;'")
   end
 end
