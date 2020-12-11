331a6d379612209a1bea7c87391d70dc0e6eae5c
FX Coudert <fxcoudert@gmail.com>
2020-12-11 21:43:12 +0000

    volta: update 0.9.3 bottle.

--- Formula/volta.rb
+++ Formula/volta.rb
@@ -8,10 +8,9 @@ class Volta < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "2a6e425ea344015228a4d9ce10792a8b754f1b747a65be2f9bd07ff819b2c924" => :big_sur
-    sha256 "4ad0a1987c96c59b41a89c97b81b11a437176308e44672391a6776c4f5e805ca" => :catalina
-    sha256 "a21224ba822d847eaa199e8d4da8dff0d2dd91e8a514f16092d0ed16aba83ada" => :mojave
-    sha256 "261320ab49c1877a2f7fe0e1d6be111697bf2c52a8e5ee39256fee6875cecbdd" => :high_sierra
+    sha256 "1f8252db32da052bd833de2f3e71bfd73b9c35e8b45e18c59130de3e60f73970" => :big_sur
+    sha256 "9ed05f093675d06e11d11527f9a8d84683c425cb1e13c7da23e2727023b8da4b" => :catalina
+    sha256 "dd9a7fa26847cfedde18605a26d26966aaf1f3531e0ba933bd3201c40572ffb9" => :mojave
   end
 
   depends_on "rust" => :build
