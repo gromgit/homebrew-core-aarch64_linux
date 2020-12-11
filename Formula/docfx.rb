6db401fad2ed6379b07c08ea6c149833440b36de
Caleb Xu <calebcenter@live.com>
2020-12-10 20:48:57 -0500

    docfx 2.56.6 (#66690)

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -1,8 +1,8 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/releases/download/v2.56.5/docfx.zip"
-  sha256 "6b7bd06b1edbb922c020911551504a80c6ecba6cda93dbd882a5c78ad55cfbee"
+  url "https://github.com/dotnet/docfx/releases/download/v2.56.6/docfx.zip"
+  sha256 "1d5a205dd93e6a9fc457677fbe7bfca5507af3e1c702f644011b4b29d1a67d16"
   license "MIT"
 
   bottle :unneeded
