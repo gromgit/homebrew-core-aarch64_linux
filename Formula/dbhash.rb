fa773b442be1fad41b616aaa39bff5e528cc7221
Michka Popoff <michkapopoff@gmail.com>
2020-12-12 14:40:06 +0100

    dbhash: make test work on linux

--- Formula/dbhash.rb
+++ Formula/dbhash.rb
@@ -26,7 +26,7 @@ class Dbhash < Formula
     dbpath = testpath/"test.sqlite"
     sqlpath = testpath/"test.sql"
     sqlpath.write "create table test (name text);"
-    system "/usr/bin/sqlite3 #{dbpath} < #{sqlpath}"
+    system "sqlite3 #{dbpath} < #{sqlpath}"
     assert_equal "b6113e0ce62c5f5ca5c9f229393345ce812b7309",
                  shell_output("#{bin}/dbhash #{dbpath}").strip.split.first
   end
