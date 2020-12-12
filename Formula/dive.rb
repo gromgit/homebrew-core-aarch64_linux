835940e2e483e5e842f31f191d9ed2e1c1f323c3
Michka Popoff <michkapopoff@gmail.com>
2020-12-12 15:49:45 +0100

    dive: add linux-only dependencies

--- Formula/dive.rb
+++ Formula/dive.rb
@@ -16,6 +16,12 @@ class Dive < Formula
 
   depends_on "go" => :build
 
+  on_linux do
+    depends_on "gpgme" => :build
+    depends_on "pkg-config" => :build
+    depends_on "device-mapper"
+  end
+
   def install
     system "go", "build", "-ldflags", "-s -w -X main.version=#{version}", "-trimpath", "-o", bin/"dive"
     prefix.install_metafiles
