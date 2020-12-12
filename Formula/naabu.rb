99fdb46ade4bb8a9c9fd2d7ea33d07227e952cbd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-12 12:22:33 +0000

    naabu: update 2.0.3 bottle.

--- Formula/naabu.rb
+++ Formula/naabu.rb
@@ -8,10 +8,9 @@ class Naabu < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "79d2b1d6c4972d60ff0dd08e948b452b29bac1e4d9db0dd80a259fd3bf6a4860" => :big_sur
-    sha256 "63d6a0d8c5839c93e61ce2caf6fbe912722fead979bcaaba42f1f9c066e7d6fd" => :catalina
-    sha256 "340adb52b40c2722030adbc0698d528226b873956b2a65a66f1850b1435bde41" => :mojave
-    sha256 "8a7153836445c301a9510bb9481d20d8cfb51b74c991f6c1e5b0ed23939d260a" => :high_sierra
+    sha256 "76af89c3ff6b834a4ab16b86e981b761ed2811630bc99675f5516ab7fd6c96c0" => :big_sur
+    sha256 "ca6a24af7e25bc3ded93af7982de6e5568a12cbc13d291a484e41d55f652f77b" => :catalina
+    sha256 "74c393d06df9c4dfb29165803b573378f4beacb4b979e07c3d68b4972ba45eec" => :mojave
   end
 
   depends_on "go" => :build
