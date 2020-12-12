42a2aeee7f7d9b58c70b3f2c0e5265d18dfc7db3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-12 12:22:13 +0000

    spdlog: update 1.8.2 bottle.

--- Formula/spdlog.rb
+++ Formula/spdlog.rb
@@ -8,10 +8,9 @@ class Spdlog < Formula
 
   bottle do
     cellar :any
-    sha256 "96c1aa7617928a308908b830a8b79f1d3e7d5aa8f5b41aebe94ebf7f1c09d4cd" => :big_sur
-    sha256 "3599f833d5d9d0c6542c7782deffa7d2788e14ddf68abf55c1fc39eb80254b18" => :catalina
-    sha256 "f8382a1e47f1d92ff14311b0c0706b8684aafbbe0794c6d51dec6fedc12364eb" => :mojave
-    sha256 "ac0a305a6b391825ab51324b9dab2bc3acb6135ec3604bc01f561b403c742042" => :high_sierra
+    sha256 "e3a306a9e37b2fa5d35857daed33e4ebbf0fdb503d0061a1e8ec443521abcf1f" => :big_sur
+    sha256 "0da73847d24190c8f51d017abab07176f6d609a409c58c0df27d1dcfd1bbd375" => :catalina
+    sha256 "40496533a239969ce9da48a76b5db57721eb7fe594e5ab129c9bb6f34281778c" => :mojave
   end
 
   depends_on "cmake" => :build
