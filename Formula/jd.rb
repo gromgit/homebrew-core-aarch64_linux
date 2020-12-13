5de5c181b6f343c2c2a323b5e84f5469f248affb
Rui Chen <rui@meetup.com>
2020-12-13 17:23:22 -0500

    jd 1.2.0
    Closes #66878.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jd.rb
+++ Formula/jd.rb
@@ -1,8 +1,8 @@
 class Jd < Formula
   desc "JSON diff and patch"
   homepage "https://github.com/josephburnett/jd"
-  url "https://github.com/josephburnett/jd/archive/v1.1.tar.gz"
-  sha256 "5192f471658ef1af38060ccd74d767bc7f1b60ba8de04335a6cccb88a1d28e20"
+  url "https://github.com/josephburnett/jd/archive/v1.2.0.tar.gz"
+  sha256 "044a1c459b5255cce83bbfc0e8bc73ea227cf9c1c904fc3dada46f640136cbc5"
   license "MIT"
 
   bottle do
