232a6cf4cc8b33be50b4176f5c2e290fee6cb45b
Rui Chen <rui@meetup.com>
2020-12-13 02:09:39 -0500

    gosu 1.14.20
    Closes #66840.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gosu.rb
+++ Formula/gosu.rb
@@ -1,10 +1,9 @@
 class Gosu < Formula
   desc "Pragmatic language for the JVM"
   homepage "https://gosu-lang.github.io/"
-  url "https://github.com/gosu-lang/gosu-lang/archive/v1.14.18.tar.gz"
-  sha256 "b6bf6967ee127b727b4be56aaa02bc807340d4663e00dafd9fd8a6c1a5cd6958"
+  url "https://github.com/gosu-lang/gosu-lang/archive/v1.14.20.tar.gz"
+  sha256 "56c4782087c20ed9a5d6e8912d128c3f87d6906d74c0b75d3b63d7fabd705e73"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/gosu-lang/gosu-lang.git"
 
   bottle do
