8d3ee11ddb26ab58d45c65877749ce9a83d276f8
FX Coudert <fxcoudert@gmail.com>
2020-12-13 21:00:17 +0000

    aws-console: add 1.1.1 bottle.

--- Formula/aws-console.rb
+++ Formula/aws-console.rb
@@ -5,6 +5,13 @@ class AwsConsole < Formula
   sha256 "5faf25a87a6fc0fde19edcf2fe6f26c81990da91d13fbf3858b1eb33711b0ebd"
   license "Apache-2.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0558c07748aa61fada24326aa0e8f06a5699f66cf1b47be9606e7a042a2e36fe" => :big_sur
+    sha256 "2aa67b0cb10ae529bd38baa85813a66f8463d9b13c0e9848c604aeae213ec0a5" => :catalina
+    sha256 "7c4b25aff982c5e21dd8e0f10fbdd5107a936c0466de4c68e13ee6f23d9d4e31" => :mojave
+  end
+
   depends_on "go" => :build
 
   def install
