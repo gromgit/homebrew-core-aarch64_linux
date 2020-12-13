08fbe800b5705a4d640bbd1fb6e2bcc9e659d79c
Michka Popoff <michkapopoff@gmail.com>
2020-12-13 14:24:22 +0100

    abseil: build shared libraries
    Closes #66843.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

--- Formula/abseil.rb
+++ Formula/abseil.rb
@@ -4,6 +4,7 @@ class Abseil < Formula
   url "https://github.com/abseil/abseil-cpp/archive/20200923.2.tar.gz"
   sha256 "bf3f13b13a0095d926b25640e060f7e13881bd8a792705dd9e161f3c2b9aa976"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -17,7 +18,10 @@ class Abseil < Formula
 
   def install
     mkdir "build" do
-      system "cmake", "..", *std_cmake_args, "-DCMAKE_CXX_STANDARD=17"
+      system "cmake", "..",
+                      *std_cmake_args,
+                      "-DCMAKE_CXX_STANDARD=17",
+                      "-DBUILD_SHARED_LIBS=ON"
       system "make"
       system "make", "install"
     end
