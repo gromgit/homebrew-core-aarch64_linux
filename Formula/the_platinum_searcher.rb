787999fc67070ea035e319d72e1aa2de18ac270e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-13 13:16:27 +0000

    the_platinum_searcher: update 2.2.0 bottle.

--- Formula/the_platinum_searcher.rb
+++ Formula/the_platinum_searcher.rb
@@ -8,12 +8,10 @@ class ThePlatinumSearcher < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "35e13a3b79c8fdd790d2f7368a37b79cec9b6c71503fd53877d6e31511035267" => :big_sur
-    sha256 "fbaca6dca74533a513f9b483607a75fe1ae1b772e39bd3e051d3c859bf378f84" => :catalina
-    sha256 "b2e99bee242a5b9a1667a321de1e777eb83a8023b034ebe0da3fc0953a193f26" => :mojave
-    sha256 "5b85047b2b893e8ec45e3f68b37c09cfb80ceb0a7c2b9c70937f2f2ca1f6f0bc" => :high_sierra
-    sha256 "1e952c6a666f180343cfdc1afa859f702638276e597d4292520fa6cf91ac82b8" => :sierra
-    sha256 "3439437518655cdd74c95eda5a161c01d5fe80604ef9c3e8936449a96ba3dcc1" => :el_capitan
+    rebuild 1
+    sha256 "63cc973af4c1fc612acb86c7a928f1680f84db7edfae52f374b95925c00761dc" => :big_sur
+    sha256 "79066cac44fd6cd21b8feadc9737045f98846832f15bd2a2e1fdaae3a8165e6d" => :catalina
+    sha256 "6b7fb2ff2ca2b5a0d264a7733a59eb0e1b68e211d15a261f6bbcab5664bb6ff7" => :mojave
   end
 
   depends_on "go" => :build
