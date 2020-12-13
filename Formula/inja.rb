e6b2d76e5e70dfac64504d0c365ab0d86c4ba5a1
Rui Chen <rui@meetup.com>
2020-12-13 17:15:47 -0500

    inja 3.1.0
    * inja 3.1.0
    * fix formula link
    
    Closes #66875.
    
    Signed-off-by: Rui Chen <rui@meetup.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/inja.rb
+++ Formula/inja.rb
@@ -1,8 +1,8 @@
 class Inja < Formula
   desc "Template engine for modern C++"
   homepage "https://pantor.github.io/inja/"
-  url "https://github.com/pantor/inja/archive/v3.0.0.tar.gz"
-  sha256 "99cdb0d90ab1adff9ec63b40a867ec14e1b217fe2d7ac07a6124de201de4ffd0"
+  url "https://github.com/pantor/inja/archive/v3.1.0.tar.gz"
+  sha256 "f4210493e7e3c62d3050ca6e5e9dd72823ec4125a469fa9b28519d7f32fc9731"
   license "MIT"
   head "https://github.com/pantor/inja.git"
 
@@ -17,8 +17,18 @@ class Inja < Formula
   depends_on "cmake" => :build
   depends_on "nlohmann-json"
 
+  # fix `nlohman_json` installation issue
+  # remove in next release
+  patch do
+    url "https://github.com/pantor/inja/commit/a57b745.patch?full_index=1"
+    sha256 "69a744a66ac6343204e99f1bdd82ef251472bca9b4f72e16db632740157aebe5"
+  end
+
   def install
-    system "cmake", ".", "-DBUILD_TESTING=OFF", "-DBUILD_BENCHMARK=OFF", *std_cmake_args
+    system "cmake", ".", "-DBUILD_TESTING=OFF",
+                         "-DBUILD_BENCHMARK=OFF",
+                         "-DINJA_USE_EMBEDDED_JSON=OFF",
+                         *std_cmake_args
     system "make", "install"
   end
 
