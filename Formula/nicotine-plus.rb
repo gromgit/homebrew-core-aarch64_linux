3f240844d9c3d8c5c5fb5596c063440cfcca8568
mathiascode <mail@mathias.is>
2020-12-15 00:27:16 +0200

    nicotine-plus 2.2.2
    Closes #66923.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nicotine-plus.rb
+++ Formula/nicotine-plus.rb
@@ -1,10 +1,10 @@
 class NicotinePlus < Formula
   include Language::Python::Virtualenv
 
-  desc "Graphical client for the SoulSeek peer-to-peer system"
-  homepage "https://www.nicotine-plus.org/"
-  url "https://github.com/Nicotine-Plus/nicotine-plus/archive/2.2.0.tar.gz"
-  sha256 "604b81d26670b4c04240e3a922cd90b1b8aa575deb155d2e1209deaddb0e4026"
+  desc "Graphical client for the Soulseek file sharing network"
+  homepage "https://nicotine-plus.org/"
+  url "https://github.com/Nicotine-Plus/nicotine-plus/archive/2.2.2.tar.gz"
+  sha256 "a2734f11beb7ec4e32d1c8270b7328721b841830b22aed2db3c4acf2b66791bb"
   license "GPL-3.0-or-later"
   head "https://github.com/Nicotine-Plus/nicotine-plus.git"
 
@@ -19,17 +19,6 @@ class NicotinePlus < Formula
   depends_on "gtk+3"
   depends_on "pygobject3"
   depends_on "python@3.9"
-  depends_on "taglib"
-
-  resource "miniupnpc" do
-    url "https://files.pythonhosted.org/packages/0c/e8/dbb2747230dfd98a6138cb65b322072eade4d92e1006e518c8711f8f5b85/miniupnpc-2.0.2.tar.gz"
-    sha256 "7ea46c93486fe1bdb31f0e0c2d911d224fce70bf5ea120e4295d647dfe274931"
-  end
-
-  resource "pytaglib" do
-    url "https://files.pythonhosted.org/packages/c7/44/f054737af93d8bc57c3a23906e4e7d1b5538c7d96577746e5c4b2f92b181/pytaglib-1.4.6.tar.gz"
-    sha256 "16daf54e78fb56442293d20d7659097470ecac9031f33037f9d53baa31382952"
-  end
 
   def install
     virtualenv_install_with_resources
