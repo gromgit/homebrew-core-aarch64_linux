9b216f75bcf8a037f98904a13b07be72d87ad744
chimurai <655241+chimurai@users.noreply.github.com>
2020-12-14 23:24:50 +0100

    deno 1.7.0
    - use Homebrew ninja
    - download and build gn from source manually
    - remove FORCE_MAC_SDK_MIN
    - don't delete llvm from HOMEBREW_LIBRARY_PATHS
    - use sccache to speed up build time
    
    Closes #66920.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/deno.rb
+++ Formula/deno.rb
@@ -1,8 +1,8 @@
 class Deno < Formula
   desc "Secure runtime for JavaScript and TypeScript"
   homepage "https://deno.land/"
-  url "https://github.com/denoland/deno/releases/download/v1.6.0/deno_src.tar.gz"
-  sha256 "60491d842e04ce162face61bb8857bf18a41726afbcbcd9fa532055ace7431ae"
+  url "https://github.com/denoland/deno/releases/download/v1.7.0/deno_src.tar.gz"
+  sha256 "f215103d0bea381495008a8f9e90c467248a2a14d3357c6d105c084186423072"
   license "MIT"
 
   bottle do
@@ -13,39 +13,46 @@ class Deno < Formula
   end
 
   depends_on "llvm" => :build
+  depends_on "ninja" => :build
   depends_on "rust" => :build
+  depends_on "sccache" => :build
   depends_on xcode: ["10.0", :build] # required by v8 7.9+
   depends_on :macos # Due to Python 2 (see https://github.com/denoland/deno/issues/2893)
 
   uses_from_macos "xz"
 
-  # Remove at next version bump. Check that new release includes:
-  # https://github.com/denoland/deno/pull/8718
-  patch do
-    url "https://github.com/denoland/deno/commit/cea42bec3272a8020f1d94afcf1a4cd7e3985553.patch?full_index=1"
-    sha256 "640ece7aab8e7486ea0ec4bfd29ac7e980822a57d12123e802d022bdc7bbaed7"
+  resource "gn" do
+    url "https://gn.googlesource.com/gn.git",
+        revision: "53d92014bf94c3893886470a1c7c1289f8818db0"
   end
 
   def install
     # env args for building a release build with our clang, ninja and gn
     ENV["GN"] = buildpath/"gn/out/gn"
+    ENV["NINJA"] = Formula["ninja"].opt_bin/"ninja"
+    ENV["SCCACHE"] = Formula["sccache"].opt_bin/"sccache"
     # build rusty_v8 from source
     ENV["V8_FROM_SOURCE"] = "1"
-    # overwrite Chromium minimum sdk version of 10.15
-    ENV["FORCE_MAC_SDK_MIN"] = "10.13"
-    # build with llvm and link against system libc++ (no runtime dep)
+    # Build with llvm. We don't remove llvm from HOMEBREW_LIBRARY_PATHS
+    # as this causes build failures. This does not create a runtime dependency.
     ENV["CLANG_BASE_PATH"] = Formula["llvm"].prefix
-    ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib
+
+    resource("gn").stage buildpath/"gn"
+    cd "gn" do
+      system "python", "build/gen.py"
+      system "ninja", "-C", "out"
+    end
 
     cd "cli" do
       system "cargo", "install", "-vv", *std_cargo_args
     end
 
-    # Install bash and zsh completion
-    output = Utils.safe_popen_read("#{bin}/deno", "completions", "bash")
-    (bash_completion/"deno").write output
-    output = Utils.safe_popen_read("#{bin}/deno", "completions", "zsh")
-    (zsh_completion/"_deno").write output
+    bash_output = Utils.safe_popen_read("#{bin}/deno", "completions", "bash")
+    (bash_completion/"deno").write bash_output
+    zsh_output = Utils.safe_popen_read("#{bin}/deno", "completions", "zsh")
+    (zsh_completion/"_deno").write zsh_output
+    fish_output = Utils.safe_popen_read("#{bin}/deno", "completions", "fish")
+    (fish_completion/"deno.fish").write fish_output
   end
 
   test do
