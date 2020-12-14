a1514e9f9ffc41283b0e0f0ae318feb30885f0bd
Stefan Weil <sw@weilnetz.de>
2020-12-14 12:48:28 +0100

    uggconv: fix typo (found by codespell)
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- Formula/uggconv.rb
+++ Formula/uggconv.rb
@@ -1,5 +1,5 @@
 class Uggconv < Formula
-  desc "Universal Game Genie code convertor"
+  desc "Universal Game Genie code converter"
   homepage "https://wyrmcorp.com/software/uggconv/index.shtml"
   url "https://wyrmcorp.com/software/uggconv/uggconv-1.0.tar.gz"
   sha256 "9a215429bc692b38d88d11f38ec40f43713576193558cd8ca6c239541b1dd7b8"
