d567808f9c10030658f9dd2b891018dcfd090918
chenrui <chenrui333@gmail.com>
2020-12-15 15:32:33 +0000

    heppdt2: add 2.06.01 bottle.

--- Formula/heppdt2.rb
+++ Formula/heppdt2.rb
@@ -5,6 +5,13 @@ class Heppdt2 < Formula
   sha256 "12a1b6ffdd626603fa3b4d70f44f6e95a36f8f3b6d4fd614bac14880467a2c2e"
   license "AFL-3.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ad2b96b10116b7be43a7fb93f0dd76346c904b9fc281983cbb954dd21674499e" => :big_sur
+    sha256 "5cefdebab8dadafb0c96b121eab2dc0abf0cf6b2f7ff5acdd4d67353c05c275b" => :catalina
+    sha256 "2c0ccad4374004249fc7431acaa358d0b04d096526e6447ec0c10cdc3b4759af" => :mojave
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
