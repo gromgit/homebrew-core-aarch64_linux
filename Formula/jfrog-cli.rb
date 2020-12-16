25ec5343ef8c5817cd8398ee28730d343d266d71
Caleb Xu <calebcenter@live.com>
2020-12-16 11:03:45 -0500

    jfrog-cli 1.42.1
    Closes #67016.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jfrog-cli.rb
+++ Formula/jfrog-cli.rb
@@ -1,8 +1,8 @@
 class JfrogCli < Formula
   desc "Command-line interface for Jfrog Artifactory and Bintray"
   homepage "https://www.jfrog.com/confluence/display/CLI/JFrog+CLI"
-  url "https://github.com/jfrog/jfrog-cli/archive/v1.41.2.tar.gz"
-  sha256 "45eb798d655cc0f2f881b6f4237aa15dffe7b89d2e8a3e260bf937a7de88148a"
+  url "https://github.com/jfrog/jfrog-cli/archive/v1.42.1.tar.gz"
+  sha256 "a11b572b69914f2a95aaee445ebf35e5c6873dc2fada08a729d4e70849a3bf49"
   license "Apache-2.0"
 
   bottle do
