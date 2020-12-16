b95b0d01175788e3f3ae3ab2bec766a4ab8276ef
Dustin Rodrigues <dust.rod@gmail.com>
2020-12-15 21:24:49 -0500

    webpack 5.10.3
    Closes #66991.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/webpack.rb
+++ Formula/webpack.rb
@@ -4,8 +4,8 @@ require "json"
 class Webpack < Formula
   desc "Bundler for JavaScript and friends"
   homepage "https://webpack.js.org/"
-  url "https://registry.npmjs.org/webpack/-/webpack-5.10.2.tgz"
-  sha256 "c72d4831c23814a87db155447891cb293ae0ba6345a4e37be3820218cf0c16f7"
+  url "https://registry.npmjs.org/webpack/-/webpack-5.10.3.tgz"
+  sha256 "d1805ee5729a7478fede25b11bff26f1a3d8d4c25753ddc405afa3ca723a0a7d"
   license "MIT"
   head "https://github.com/webpack/webpack.git"
 
