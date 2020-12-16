c1b738be8f057d5b5ac6fdfe768c9728b9b4c111
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-15 22:12:30 -0500

    jetty/jetty-runner: update livecheck
    * jetty: update livecheck
    * jetty-runner: update livecheck

--- Formula/jetty-runner.rb
+++ Formula/jetty-runner.rb
@@ -7,7 +7,7 @@ class JettyRunner < Formula
   license any_of: ["Apache-2.0", "EPL-1.0"]
 
   livecheck do
-    url "https://www.eclipse.org/jetty/download.html"
+    url "https://www.eclipse.org/jetty/download.php"
     regex(/href=.*?jetty-distribution[._-]v?(\d+(?:\.\d+)+(?:\.v\d+)?)\.t/i)
   end
 
