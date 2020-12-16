3386eed8bb8d01c6ab087ae2c56dd4909cb2f9a4
carlocab <30379873+carlocab@users.noreply.github.com>
2020-12-16 01:03:06 +0000

    alsa-lib 1.2.4

--- Formula/alsa-lib.rb
+++ Formula/alsa-lib.rb
@@ -1,8 +1,8 @@
 class AlsaLib < Formula
   desc "Provides audio and MIDI functionality to the Linux operating system"
   homepage "https://www.alsa-project.org/"
-  url "https://www.alsa-project.org/files/pub/lib/alsa-lib-1.2.1.2.tar.bz2"
-  sha256 "958e260e3673f1f6ff6b2d2c0df3fc2e469bea5b2957163ce96ce17f23e87943"
+  url "https://www.alsa-project.org/files/pub/lib/alsa-lib-1.2.4.tar.bz2"
+  sha256 "f7554be1a56cdff468b58fc1c29b95b64864c590038dd309c7a978c7116908f7"
   license "LGPL-2.1-or-later"
 
   depends_on :linux
