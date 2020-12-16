1439933e1435bae8f88149beaead6c0683269506
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-12-16 11:21:18 +0000

    aida-header: add 3.2.1 bottle.

--- Formula/aida-header.rb
+++ Formula/aida-header.rb
@@ -5,6 +5,13 @@ class AidaHeader < Formula
   sha256 "882d351bc09e830ae2eb512a2cbf44af5a82ef8efe31fbe0d047363da8314c81"
   license "LGPL-3.0-or-later"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "eba4b33299b9ed8ed988c4c17fbffe1e17364a7d284878247c3b0a738fe2b340" => :big_sur
+    sha256 "50a1e944d768d3f6b5d8dcaf8d074d821272056104369e85f72539c628b770f2" => :catalina
+    sha256 "d4559d46451c98728a32679f6d62b7ee4c9a5fa57c18e7ba9315e33d2e7150b8" => :mojave
+  end
+
   def install
     include.install "src/cpp/AIDA"
   end
