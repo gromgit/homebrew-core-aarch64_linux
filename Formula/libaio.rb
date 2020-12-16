89268495d12ac7cb6d10d9b1d78c1a68f22780d9
Caleb Xu <calebcenter@live.com>
2020-12-16 13:03:42 -0500

    libaio 0.3.112 (#67017)

--- Formula/libaio.rb
+++ Formula/libaio.rb
@@ -1,8 +1,8 @@
 class Libaio < Formula
   desc "Linux-native asynchronous I/O access library"
   homepage "https://pagure.io/libaio"
-  url "https://pagure.io/libaio/archive/libaio-0.3.111/libaio-libaio-0.3.111.tar.gz"
-  sha256 "e6bc17cba66e59085e670fea238ad095766b412561f90b354eb4012d851730ba"
+  url "https://pagure.io/libaio/archive/libaio-0.3.112/libaio-libaio-0.3.112.tar.gz"
+  sha256 "b7cf93b29bbfb354213a0e8c0e82dfcf4e776157940d894750528714a0af2272"
   license "LGPL-2.1-or-later"
 
   depends_on :linux
