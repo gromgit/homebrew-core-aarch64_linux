dd510eef3f55f5dc4fb7552c7c8fa97948cb97cb
Michka Popoff <michkapopoff@gmail.com>
2020-12-17 22:10:58 +0100

    wayland-protocols (new formula)

---
+++ Formula/wayland-protocols.rb
@@ -0,0 +1,25 @@
+class WaylandProtocols < Formula
+  desc "Additional Wayland protocols"
+  homepage "https://wayland.freedesktop.org"
+  url "https://wayland.freedesktop.org/releases/wayland-protocols-1.20.tar.xz"
+  sha256 "9782b7a1a863d82d7c92478497d13c758f52e7da4f197aa16443f73de77e4de7"
+  license "MIT"
+
+  depends_on "pkg-config" => :build
+  depends_on "wayland" => :build
+  depends_on :linux
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}",
+                          "--localstatedir=#{var}",
+                          "--disable-silent-rules"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "pkg-config", "--exists", "wayland-protocols"
+    assert_equal 0, $CHILD_STATUS.exitstatus
+  end
+end
