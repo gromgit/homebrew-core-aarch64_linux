3b7beda8bd70b00c1c0018cef056ae2bd17ef462
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2020-12-17 11:10:56 +0000

    xcbeautify: add 0.8.1 bottle.

--- Formula/xcbeautify.rb
+++ Formula/xcbeautify.rb
@@ -7,6 +7,13 @@ class Xcbeautify < Formula
   license "MIT"
   head "https://github.com/thii/xcbeautify.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "71f511194e35bc4e8d75583ad11ca4f8a037b88c5a8490ac54f86faa0d77fea9" => :big_sur
+    sha256 "cd33facf2cacba1da0b35945b822b7b9befac05bbcdb11743bc623ef6d250927" => :catalina
+    sha256 "c1c8db06d34a1d8cea60edf06143abe926b89715539f4b66d4377d25a93e2e02" => :mojave
+  end
+
   depends_on xcode: ["10.0", :build]
 
   def install
