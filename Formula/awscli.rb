4156a20856c317513d2b2cafb9678e1db0195953
shigemk2 <i.am.shige@gmail.com>
2020-12-17 10:11:02 +0900

    awscli 2.1.13
    * awscli 2.1.12
    * awscli 2.1.13
    
    Closes #67032.
    
    Co-authored-by: chenrui <rui@meetup.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -3,8 +3,8 @@ class Awscli < Formula
 
   desc "Official Amazon AWS command-line interface"
   homepage "https://aws.amazon.com/cli/"
-  url "https://github.com/aws/aws-cli/archive/2.1.11.tar.gz"
-  sha256 "cbc0be7bc5991b832cd16cd34a3ef7351237628345e9ffc2c69602f149a8d62f"
+  url "https://github.com/aws/aws-cli/archive/2.1.13.tar.gz"
+  sha256 "04fdbcbb7e62a295618022049e3b6fdeff8fcff4f6489a7f6db3207a1ad95924"
   license "Apache-2.0"
   head "https://github.com/aws/aws-cli.git", branch: "v2"
 
