28e9398976018ef17cc7a40c389cda7ec0019f34
Rui Chen <rui@meetup.com>
2020-12-18 12:36:14 -0500

    flint 2.7.0
    flint: use https for urls
    
    update test
    
    Signed-off-by: Rui Chen <rui@meetup.com>

--- Formula/flint.rb
+++ Formula/flint.rb
@@ -1,10 +1,9 @@
 class Flint < Formula
   desc "C library for number theory"
-  homepage "http://flintlib.org"
-  url "http://flintlib.org/flint-2.6.3.tar.gz"
-  sha256 "ce1a750a01fa53715cad934856d4b2ed76f1d1520bac0527ace7d5b53e342ee3"
+  homepage "https://flintlib.org"
+  url "https://flintlib.org/flint-2.7.0.tar.gz"
+  sha256 "919a722e33b83bb8dad2231fee97caaa40d028ff9c88ee2be645667f3c472f3a"
   license "LGPL-2.1-or-later"
-  revision 1
   head "https://github.com/wbhart/flint2.git", branch: "trunk"
 
   bottle do
@@ -35,32 +34,55 @@ class Flint < Formula
     (testpath/"test.c").write <<-EOS
       #include <stdlib.h>
       #include <stdio.h>
-
-      #include <flint/flint.h>
-      #include <flint/fmpz_mod_poly.h>
+      #include "flint.h"
+      #include "fmpz.h"
+      #include "ulong_extras.h"
 
       int main(int argc, char* argv[])
       {
-          fmpz_t n;
-          fmpz_mod_poly_t x, y;
+          slong i, bit_bound;
+          mp_limb_t prime, res;
+          fmpz_t x, y, prod;
 
-          fmpz_init_set_ui(n, 7);
-          fmpz_mod_poly_init(x, n);
-          fmpz_mod_poly_init(y, n);
-          fmpz_mod_poly_set_coeff_ui(x, 3, 5);
-          fmpz_mod_poly_set_coeff_ui(x, 0, 6);
-          fmpz_mod_poly_sqr(y, x);
-          fmpz_mod_poly_print(x); flint_printf("\\n");
-          fmpz_mod_poly_print(y); flint_printf("\\n");
-          fmpz_mod_poly_clear(x);
-          fmpz_mod_poly_clear(y);
-          fmpz_clear(n);
+          if (argc != 2)
+          {
+              flint_printf("Syntax: crt <integer>\\n");
+              return EXIT_FAILURE;
+          }
+
+          fmpz_init(x);
+          fmpz_init(y);
+          fmpz_init(prod);
+
+          fmpz_set_str(x, argv[1], 10);
+          bit_bound = fmpz_bits(x) + 2;
+
+          fmpz_zero(y);
+          fmpz_one(prod);
+
+          prime = 0;
+          for (i = 0; fmpz_bits(prod) < bit_bound; i++)
+          {
+              prime = n_nextprime(prime, 0);
+              res = fmpz_fdiv_ui(x, prime);
+              fmpz_CRT_ui(y, y, prod, res, prime, 1);
+
+              flint_printf("residue mod %wu = %wu; reconstruction = ", prime, res);
+              fmpz_print(y);
+              flint_printf("\\n");
+
+              fmpz_mul_ui(prod, prod, prime);
+          }
+
+          fmpz_clear(x);
+          fmpz_clear(y);
+          fmpz_clear(prod);
 
           return EXIT_SUCCESS;
       }
     EOS
     system ENV.cc, "test.c", "-I#{include}/flint", "-L#{lib}", "-L#{Formula["gmp"].lib}",
            "-lflint", "-lgmp", "-o", "test"
-    system "./test"
+    system "./test 2"
   end
 end
