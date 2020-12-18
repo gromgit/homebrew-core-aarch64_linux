3e4851717bca24b7559ad13ddb965273a1deee09
chenrui <chenrui333@gmail.com>
2020-12-18 02:58:02 -0500

    flyway 7.3.2 (#67100)

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/7.3.1/flyway-commandline-7.3.1.tar.gz"
-  sha256 "888fd493fe6d97df5ef91efeb7f670492a52c623851e35b9d9bc5129086d31e3"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/7.3.2/flyway-commandline-7.3.2.tar.gz"
+  sha256 "2ad30e7ae22b44082e233aaa360086523fa11de2282f1ce1674d445072a5d8c8"
   license "Apache-2.0"
 
   livecheck do
