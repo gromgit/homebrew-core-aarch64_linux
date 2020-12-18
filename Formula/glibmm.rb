1875d1f036cdb80e367cba1464bb47336e57780c
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-12-18 19:12:02 +0000

    glibmm 2.68.0

--- Formula/glibmm.rb
+++ Formula/glibmm.rb
@@ -1,8 +1,8 @@
 class Glibmm < Formula
   desc "C++ interface to glib"
   homepage "https://www.gtkmm.org/"
-  url "https://download.gnome.org/sources/glibmm/2.64/glibmm-2.64.5.tar.xz"
-  sha256 "508fc86e2c9141198aa16c225b16fd6b911917c0d3817602652844d0973ea386"
+  url "https://download.gnome.org/sources/glibmm/2.68/glibmm-2.68.0.tar.xz"
+  sha256 "c1f38573191dceed85a05600888cf4cf4695941f339715bd67d51c2416f4f375"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -20,7 +20,7 @@ class Glibmm < Formula
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "glib"
-  depends_on "libsigc++@2"
+  depends_on "libsigc++"
 
   def install
     ENV.cxx11
@@ -44,26 +44,26 @@ class Glibmm < Formula
     EOS
     gettext = Formula["gettext"]
     glib = Formula["glib"]
-    libsigcxx = Formula["libsigc++@2"]
+    libsigcxx = Formula["libsigc++"]
     flags = %W[
       -I#{gettext.opt_include}
       -I#{glib.opt_include}/glib-2.0
       -I#{glib.opt_lib}/glib-2.0/include
-      -I#{include}/glibmm-2.4
-      -I#{libsigcxx.opt_include}/sigc++-2.0
-      -I#{libsigcxx.opt_lib}/sigc++-2.0/include
-      -I#{lib}/glibmm-2.4/include
+      -I#{include}/glibmm-2.68
+      -I#{libsigcxx.opt_include}/sigc++-3.0
+      -I#{libsigcxx.opt_lib}/sigc++-3.0/include
+      -I#{lib}/glibmm-2.68/include
       -L#{gettext.opt_lib}
       -L#{glib.opt_lib}
       -L#{libsigcxx.opt_lib}
       -L#{lib}
       -lglib-2.0
-      -lglibmm-2.4
+      -lglibmm-2.68
       -lgobject-2.0
       -lintl
-      -lsigc-2.0
+      -lsigc-3.0
     ]
-    system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test", *flags
+    system ENV.cxx, "-std=c++17", "test.cpp", "-o", "test", *flags
     system "./test"
   end
 end
