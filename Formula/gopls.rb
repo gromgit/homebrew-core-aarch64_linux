1a24fdf7d0d432902571f14a69f91c17b3e8615a
Rui Chen <rui@meetup.com>
2020-12-17 20:50:16 -0500

    gopls 0.6.1
    Closes #67106.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gopls.rb
+++ Formula/gopls.rb
@@ -1,8 +1,8 @@
 class Gopls < Formula
   desc "Language server for the Go language"
   homepage "https://github.com/golang/tools/tree/master/gopls"
-  url "https://github.com/golang/tools/archive/gopls/v0.5.5.tar.gz"
-  sha256 "08d477a7c35021ec5d8950e25e1fcac86d7ec0ce8a421c20d932029e00efb1d8"
+  url "https://github.com/golang/tools/archive/gopls/v0.6.1.tar.gz"
+  sha256 "e02bd50379993220cec2c8a3ebda1214d759a547fb96daa41dbdd12d56e61a4f"
   license "BSD-3-Clause"
 
   livecheck do
