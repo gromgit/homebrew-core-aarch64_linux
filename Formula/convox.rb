70b38370e2ad0e14fd8779fa7774fb8e3fd18939
chenrui <chenrui333@gmail.com>
2020-12-18 22:14:58 +0000

    convox: update 3.0.45 bottle.

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -13,10 +13,9 @@ class Convox < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "20b996d907f1c49626c3b5562d7324ff400e857ac7ea9e1b76da05f73dd9b99d" => :big_sur
-    sha256 "6669f173ecb619316e6471e53d7d394542c2dc9dc623df3b6e2743c04b18cc3f" => :catalina
-    sha256 "f5d197813d4e78a16d535c99efad0b1ffc283eafecc69d346df63f3ab1ef5ce2" => :mojave
-    sha256 "ede78636eb4d9502a5cf008b9005c0f9f4d513ab8ac0e32a17e4f531ed01df85" => :high_sierra
+    sha256 "46c750d57369ba9176f837d83674253af263658acf216880e1038b98f4303504" => :big_sur
+    sha256 "606a7cc3e37e5007e37eae7e6d2f10add0cbf1f272f6158560e01796b1fd2c18" => :catalina
+    sha256 "2b0e40402da441ecc00a803aa71c8bac1ee05ef2ca101b4fc9f60cd4e763c847" => :mojave
   end
 
   depends_on "go" => :build
