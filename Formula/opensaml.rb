8dac66c2ee25eb66260dc457d47670fe664640aa
Rui Chen <rui@meetup.com>
2020-12-17 21:22:45 -0500

    opensaml 3.2.0

--- Formula/opensaml.rb
+++ Formula/opensaml.rb
@@ -1,8 +1,8 @@
 class Opensaml < Formula
   desc "Library for Security Assertion Markup Language"
   homepage "https://wiki.shibboleth.net/confluence/display/OpenSAML/Home"
-  url "https://shibboleth.net/downloads/c++-opensaml/3.1.0/opensaml-3.1.0.tar.bz2"
-  sha256 "7b632d2cf6556b213e80ec1473b5298dbfa17f665cb3911f933c4ad5fe2983b0"
+  url "https://shibboleth.net/downloads/c++-opensaml/3.2.0/opensaml-3.2.0.tar.bz2"
+  sha256 "8c3ba09febcb622f930731f8766e57b3c154987e8807380a4228fbf90e6e1441"
 
   livecheck do
     url "https://shibboleth.net/downloads/c++-opensaml/latest/"
