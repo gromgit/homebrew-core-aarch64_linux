d21b96cf516417821980fe06e6ede825fe5f2c48
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-12-18 19:12:35 +0000

    glibmm@2.64 2.64.5 (new formula)

---
+++ Formula/glibmm@2.64.rb
@@ -0,0 +1,62 @@
+class GlibmmAT264 < Formula
+  desc "C++ interface to glib"
+  homepage "https://www.gtkmm.org/"
+  url "https://download.gnome.org/sources/glibmm/2.64/glibmm-2.64.5.tar.xz"
+  sha256 "508fc86e2c9141198aa16c225b16fd6b911917c0d3817602652844d0973ea386"
+  license "LGPL-2.1-or-later"
+
+  livecheck do
+    url :stable
+  end
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "libsigc++@2"
+
+  def install
+    ENV.cxx11
+
+    mkdir "build" do
+      system "meson", *std_meson_args, ".."
+      system "ninja"
+      system "ninja", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <glibmm.h>
+
+      int main(int argc, char *argv[])
+      {
+         Glib::ustring my_string("testing");
+         return 0;
+      }
+    EOS
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    libsigcxx = Formula["libsigc++@2"]
+    flags = %W[
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{include}/glibmm-2.4
+      -I#{libsigcxx.opt_include}/sigc++-2.0
+      -I#{libsigcxx.opt_lib}/sigc++-2.0/include
+      -I#{lib}/glibmm-2.4/include
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{libsigcxx.opt_lib}
+      -L#{lib}
+      -lglib-2.0
+      -lglibmm-2.4
+      -lgobject-2.0
+      -lintl
+      -lsigc-2.0
+    ]
+    system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test", *flags
+    system "./test"
+  end
+end
