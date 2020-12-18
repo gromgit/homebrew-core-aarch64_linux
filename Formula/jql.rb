dae66a650e08ea05349fec69ee4fb7ec9eb59ed1
Caleb Xu <calebcenter@live.com>
2020-12-18 14:23:35 -0500

    jql 2.8.2 (new formula)
    Closes #67200.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: chenrui <chenrui333@gmail.com>

---
+++ Formula/jql.rb
@@ -0,0 +1,24 @@
+class Jql < Formula
+  desc "JSON query language CLI tool"
+  homepage "https://github.com/yamafaktory/jql"
+  url "https://github.com/yamafaktory/jql/archive/v2.8.2.tar.gz"
+  sha256 "9368075fc1ae8568867177dc90553295b7e5c440fe0a03bd4bfc7fae2e1ab8a7"
+  license "MIT"
+  head "https://github.com/yamafaktory/jql.git"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    (testpath/"example.json").write <<~EOS
+      {
+        "cats": [{ "first": "Pixie" }, { "second": "Kitkat" }, { "third": "Misty" }]
+      }
+    EOS
+    output = shell_output("#{bin}/jql --raw-output '\"cats\".[2:1].[0].\"third\"' example.json")
+    assert_equal "Misty\n", output
+  end
+end
