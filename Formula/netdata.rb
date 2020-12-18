3174edf416a2c6bdc83ead06a3aa9b830d12326b
FX Coudert <fxcoudert@gmail.com>
2020-12-18 09:42:50 +0000

    netdata: update 1.27.0 bottle.

--- Formula/netdata.rb
+++ Formula/netdata.rb
@@ -11,10 +11,9 @@ class Netdata < Formula
   end
 
   bottle do
-    sha256 "781b2daf7c212416145cc15395d8fce21a7453d271c6a64c8e0b83985b00bc51" => :big_sur
-    sha256 "bd2e4c457cb565f03c632527ac62eef558bf54ea770a06bd06a4c53a100d043f" => :catalina
-    sha256 "e2a1165f6dcabb048cc8298213b5ea2b212fb393ab19e24245b6403edb128764" => :mojave
-    sha256 "0e27a3c39108a37709aaaf549b57bbb1d4726f31e2abfdb5f0ba73432bbcc849" => :high_sierra
+    sha256 "66bc116d768d09cf817d734c518b7f381ce9c76159482a9191d4cd0bdb27a1ee" => :big_sur
+    sha256 "c4e5f8c918eb5961616076a1821c3d564ecf43a7b9d2750541f4468887bad262" => :catalina
+    sha256 "db4e3dd284dbf60b10d3dc3eaaccfafe6fa25565e00e2bca78d4318ded424bc5" => :mojave
   end
 
   depends_on "autoconf" => :build
