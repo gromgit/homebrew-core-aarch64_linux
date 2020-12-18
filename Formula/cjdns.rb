8bc5271ffbf9e74a514bc47dbc3f4f0813f325a1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-18 07:21:17 +0000

    cjdns: update 21.1 bottle.

--- Formula/cjdns.rb
+++ Formula/cjdns.rb
@@ -8,10 +8,9 @@ class Cjdns < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "13c629ad726315a5e7e664f45c89cac07c48491791216e312bc259ed9b42803c" => :big_sur
-    sha256 "b40905920fb755137bd4ebb409a92b9472419e974eb634834de0cf4f8ee4b39d" => :catalina
-    sha256 "badca34b50dc0c5eb97d22d1538e8b12cf80d8d28acdeb31fd00ac4fd9074e12" => :mojave
-    sha256 "a3708d0844e30cc69de62232117b683ac8f48426f9c8a12781fcd042c581b0e5" => :high_sierra
+    sha256 "b57d1c38866eab0c671732fdf70890fde76e7b03a2fc6a3dc59d5dea29e9fbaa" => :big_sur
+    sha256 "dc8c73f740f3bfbde7db45dde12cbc57bc34c925b88a15be55e4ba47b73eb1d4" => :catalina
+    sha256 "6f5536caa2f432a96027fa223e20b12d54baa78f96a7d5cabb454ad380faf523" => :mojave
   end
 
   depends_on "node" => :build
