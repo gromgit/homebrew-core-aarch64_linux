25a601f5e524a20f35c4b3daa5aba47af5ca9b7d
Rui Chen <rui@meetup.com>
2020-12-18 17:06:07 -0500

    apollo-cli 2.32.0
    Closes #67211.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/apollo-cli.rb
+++ Formula/apollo-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class ApolloCli < Formula
   desc "Command-line tool for Apollo GraphQL"
   homepage "https://apollographql.com"
-  url "https://registry.npmjs.org/apollo/-/apollo-2.31.2.tgz"
-  sha256 "8616b5b3b2a30c9b54465cebb569dee8bbd34e654c70a3a71e6c73f2b86a1d77"
+  url "https://registry.npmjs.org/apollo/-/apollo-2.32.0.tgz"
+  sha256 "03aa5537731ebb1f8add2533973c254ae6da0d584d9e794dd513e8f42d454454"
   license "MIT"
 
   livecheck do
