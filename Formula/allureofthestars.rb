4eef610428a572de906f6a3c543fc2aeced7ecfa
Seeker <meaningseeking@protonmail.com>
2020-12-19 15:41:37 -0800

    allureofthestars: update license (#67256)

--- Formula/allureofthestars.rb
+++ Formula/allureofthestars.rb
@@ -3,7 +3,7 @@ class Allureofthestars < Formula
   homepage "https://www.allureofthestars.com/"
   url "https://hackage.haskell.org/package/Allure-0.9.5.0/Allure-0.9.5.0.tar.gz"
   sha256 "8180fe070633bfa5515de8f7443421044e7ad4ee050f0a92c048cec5f2c88132"
-  license "AGPL-3.0"
+  license all_of: ["AGPL-3.0-or-later", "GPL-2.0-or-later", "OFL-1.1", "MIT", :cannot_represent]
   head "https://github.com/AllureOfTheStars/Allure.git"
 
   livecheck do
