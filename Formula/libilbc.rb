51c424ea7b712017b1dd54ee2d0f7139ab6fcebe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-19 17:35:35 +0000

    libilbc: update 3.0.2 bottle.

--- Formula/libilbc.rb
+++ Formula/libilbc.rb
@@ -7,14 +7,9 @@ class Libilbc < Formula
 
   bottle do
     cellar :any
-    sha256 "2fead9d7113ffad5aaa75fd4cdf875071865947c69aed68fbe542b1603d9c364" => :big_sur
-    sha256 "3a8caa126466fcf7cde71363a4423e709b249f0a7318a22e5bd545d3d5382ea3" => :catalina
-    sha256 "cbb71770ab1bc1647e719d9b084d710c8c5c4e37a0b1a70e53650b3fb24cda62" => :mojave
-    sha256 "63df659558f7961f6e04c347dad5ef6508578e86e0c13b15d39fb267dcc440db" => :high_sierra
-    sha256 "421757bd970d1ba1d0c1fe2eff208cc0b44f8a75bbf9a09344ba504ad377750f" => :sierra
-    sha256 "fff34390e949e037bb4b16937b62ab4993f55d2fb805656116ceab6a7c9b6e83" => :el_capitan
-    sha256 "7f16b3e0f254f35be8b6275339dc813a6443f65d1c27e1748e08835a49733f6f" => :yosemite
-    sha256 "7aa8495e4050ea38152ec218452d6fac97387ad385a6d63806238e838664471b" => :mavericks
+    sha256 "275bc3bad4478cce3c21090e97cc1053903731f394271d0fa215835802c01cc7" => :big_sur
+    sha256 "e234edab3b84eaa2127ddcaf4a722467a438d69e70fc2e27e3165d18c9cd430f" => :catalina
+    sha256 "d5b7863a05ee6124b1be59fc94c4839d6479f7d0af5bb3e2cff377ec3d3720b0" => :mojave
   end
 
   depends_on "cmake" => :build
