6f4d4eb2c5086678784ed7af5db1045e1141a572
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-12-19 20:31:57 +0000

    gtksourceviewmm: bump revision for pangomm@2.42 and atkmm@2.28

--- Formula/gtksourceviewmm.rb
+++ Formula/gtksourceviewmm.rb
@@ -4,7 +4,7 @@ class Gtksourceviewmm < Formula
   url "https://download.gnome.org/sources/gtksourceviewmm/2.10/gtksourceviewmm-2.10.3.tar.xz"
   sha256 "0000df1b582d7be2e412020c5d748f21c0e6e5074c6b2ca8529985e70479375b"
   license "LGPL-2.1-or-later"
-  revision 9
+  revision 10
 
   livecheck do
     url :stable
@@ -18,7 +18,7 @@ class Gtksourceviewmm < Formula
     sha256 "7922e406c8d456752fdab0dce85355adcaa024bc26b9376c5fbf32820789df0c" => :mojave
   end
 
-  depends_on "pkg-config" => :build
+  depends_on "pkg-config" => [:build, :test]
   depends_on "gtkmm"
   depends_on "gtksourceview"
 
@@ -38,98 +38,8 @@ class Gtksourceviewmm < Formula
       }
     EOS
     ENV.libxml2
-    atk = Formula["atk"]
-    atkmm = Formula["atkmm"]
-    cairo = Formula["cairo"]
-    cairomm = Formula["cairomm@1.14"]
-    fontconfig = Formula["fontconfig"]
-    freetype = Formula["freetype"]
-    gdk_pixbuf = Formula["gdk-pixbuf"]
-    gettext = Formula["gettext"]
-    glib = Formula["glib"]
-    glibmm = Formula["glibmm@2.64"]
-    gtkx = Formula["gtk+"]
-    gtkmm = Formula["gtkmm"]
-    gtksourceview = Formula["gtksourceview"]
-    harfbuzz = Formula["harfbuzz"]
-    libpng = Formula["libpng"]
-    libsigcxx = Formula["libsigc++@2"]
-    pango = Formula["pango"]
-    pangomm = Formula["pangomm"]
-    pixman = Formula["pixman"]
-    flags = %W[
-      -I#{atk.opt_include}/atk-1.0
-      -I#{atkmm.opt_include}/atkmm-1.6
-      -I#{atkmm.opt_lib}/atkmm-1.6/include
-      -I#{cairo.opt_include}/cairo
-      -I#{cairomm.opt_include}/cairomm-1.0
-      -I#{cairomm.opt_lib}/cairomm-1.0/include
-      -I#{fontconfig.opt_include}
-      -I#{freetype.opt_include}/freetype2
-      -I#{gdk_pixbuf.opt_include}/gdk-pixbuf-2.0
-      -I#{gettext.opt_include}
-      -I#{glib.opt_include}/glib-2.0
-      -I#{glib.opt_lib}/glib-2.0/include
-      -I#{glibmm.opt_include}/giomm-2.4
-      -I#{glibmm.opt_include}/glibmm-2.4
-      -I#{glibmm.opt_lib}/giomm-2.4/include
-      -I#{glibmm.opt_lib}/glibmm-2.4/include
-      -I#{gtkmm.opt_include}/gdkmm-2.4
-      -I#{gtkmm.opt_include}/gtkmm-2.4
-      -I#{gtkmm.opt_lib}/gdkmm-2.4/include
-      -I#{gtkmm.opt_lib}/gtkmm-2.4/include
-      -I#{gtksourceview.opt_include}/gtksourceview-2.0
-      -I#{gtkx.opt_include}/gtk-2.0
-      -I#{gtkx.opt_include}/gtk-unix-print-2.0
-      -I#{gtkx.opt_lib}/gtk-2.0/include
-      -I#{harfbuzz.opt_include}/harfbuzz
-      -I#{include}/gtksourceviewmm-2.0
-      -I#{libpng.opt_include}/libpng16
-      -I#{libsigcxx.opt_include}/sigc++-2.0
-      -I#{libsigcxx.opt_lib}/sigc++-2.0/include
-      -I#{lib}/gtksourceviewmm-2.0/include
-      -I#{pango.opt_include}/pango-1.0
-      -I#{pangomm.opt_include}/pangomm-1.4
-      -I#{pangomm.opt_lib}/pangomm-1.4/include
-      -I#{pixman.opt_include}/pixman-1
-      -D_REENTRANT
-      -L#{atk.opt_lib}
-      -L#{atkmm.opt_lib}
-      -L#{cairo.opt_lib}
-      -L#{cairomm.opt_lib}
-      -L#{gdk_pixbuf.opt_lib}
-      -L#{gettext.opt_lib}
-      -L#{glib.opt_lib}
-      -L#{glibmm.opt_lib}
-      -L#{gtkmm.opt_lib}
-      -L#{gtksourceview.opt_lib}
-      -L#{gtkx.opt_lib}
-      -L#{libsigcxx.opt_lib}
-      -L#{lib}
-      -L#{pango.opt_lib}
-      -L#{pangomm.opt_lib}
-      -latk-1.0
-      -latkmm-1.6
-      -lcairo
-      -lcairomm-1.0
-      -lgdk-quartz-2.0
-      -lgdk_pixbuf-2.0
-      -lgdkmm-2.4
-      -lgio-2.0
-      -lgiomm-2.4
-      -lglib-2.0
-      -lglibmm-2.4
-      -lgobject-2.0
-      -lgtk-quartz-2.0
-      -lgtkmm-2.4
-      -lgtksourceview-2.0
-      -lgtksourceviewmm-2.0
-      -lintl
-      -lpango-1.0
-      -lpangocairo-1.0
-      -lpangomm-1.4
-      -lsigc-2.0
-    ]
+    command = "#{Formula["pkg-config"].opt_bin}/pkg-config --cflags --libs gtksourceviewmm-2.0"
+    flags = shell_output(command).strip.split
     system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test", *flags
     system "./test"
   end
