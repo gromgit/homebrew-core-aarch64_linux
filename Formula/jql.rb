74483394cf7577de5b715fcb0e9071d99c0fa2c0
chenrui <chenrui333@gmail.com>
2020-12-19 20:04:25 +0000

    jql: add 2.8.2 bottle.

--- Formula/jql.rb
+++ Formula/jql.rb
@@ -6,6 +6,13 @@ class Jql < Formula
   license "MIT"
   head "https://github.com/yamafaktory/jql.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "46588e489c987d107a9c089673a80de31972f83e879566d8064ba09afa2d4e06" => :big_sur
+    sha256 "98aa864e495a17e4746304d6d8ba602cb9c3ffebde12dc6937bbe9e576a8a51a" => :catalina
+    sha256 "9cf16356d69554077542a655e458fb449f15600a13a09a5227c5e9cc1fe3001d" => :mojave
+  end
+
   depends_on "rust" => :build
 
   def install
