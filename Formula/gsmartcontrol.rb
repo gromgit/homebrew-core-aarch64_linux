46a0dc9ea5bc00f5401cfd2c9daa91f6aa7dc450
FX Coudert <fxcoudert@gmail.com>
2020-12-19 16:58:11 +0000

    gsmartcontrol: update 1.1.3_7 bottle.

--- Formula/gsmartcontrol.rb
+++ Formula/gsmartcontrol.rb
@@ -10,9 +10,9 @@ class Gsmartcontrol < Formula
   end
 
   bottle do
-    sha256 "5b936e741b0d53ac9fdbde2d64ac17f5399265157e1dcfd763cc85c8716d410c" => :big_sur
-    sha256 "39046ff435ac8fe0c3e2caee12a462fcf7235c5eccaa3b7b91326f4465834941" => :catalina
-    sha256 "0289b119ca79206d68ac0b4bcbb47b839f98945d85d1de1d63bb67a1ff408d14" => :mojave
+    sha256 "d35eb9fcef537fc21c297083629239973050d6da92c75ea07f99bbebf1c9c505" => :big_sur
+    sha256 "99f4ec56915a85c7c38d059ce6b7655e3a3ab1f7911afa778a7dbae64a818229" => :catalina
+    sha256 "beab95cdf1fd5926bef93b096e4de7a0df00f30020bd1276386150e3d452ae5d" => :mojave
   end
 
   depends_on "pkg-config" => :build
