e2144fed064b1d328213f8fe5d44f985efae67da
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-12-19 20:16:55 +0000

    gsmartcontrol: bump revision for pangomm@2.42 and atkmm@2.28

--- Formula/gsmartcontrol.rb
+++ Formula/gsmartcontrol.rb
@@ -3,7 +3,7 @@ class Gsmartcontrol < Formula
   homepage "https://gsmartcontrol.sourceforge.io"
   url "https://downloads.sourceforge.net/project/gsmartcontrol/1.1.3/gsmartcontrol-1.1.3.tar.bz2"
   sha256 "b64f62cffa4430a90b6d06cd52ebadd5bcf39d548df581e67dfb275a673b12a9"
-  revision 7
+  revision 8
 
   livecheck do
     url :stable
