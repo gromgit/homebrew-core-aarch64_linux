baacbe78728d3b42db190bd95401fbcc78708baa
chenrui <chenrui333@gmail.com>
2020-12-20 16:50:50 +0000

    flow-cli: add 0.12.1 bottle.

--- Formula/flow-cli.rb
+++ Formula/flow-cli.rb
@@ -6,6 +6,13 @@ class FlowCli < Formula
   license "Apache-2.0"
   head "https://github.com/onflow/flow-cli.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3175e54be30ffde72756f133e3a3c57e041fce1da732c7cceb029a8605222165" => :big_sur
+    sha256 "d2e8eb6e2341f1c4f4539ebd97fbdd5274ade916408a90368977b2161cfe8818" => :catalina
+    sha256 "ff160b6e738b4035978dceeea7e66cf9e18b1cdad87b847f3c212897461c31b2" => :mojave
+  end
+
   depends_on "go" => :build
 
   def install
