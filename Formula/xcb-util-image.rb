050040b3f010bb85540705e9a10631eeb3882ae2
Seeker <meaningseeking@protonmail.com>
2020-12-20 10:49:45 -0800

    xcb-util-image (new formula)
    Closes #67318.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

---
+++ Formula/xcb-util-image.rb
@@ -0,0 +1,34 @@
+class XcbUtilImage < Formula
+  desc "XCB port of Xlib's XImage and XShmImage"
+  homepage "https://xcb.freedesktop.org"
+  url "https://xcb.freedesktop.org/dist/xcb-util-image-0.4.0.tar.bz2"
+  sha256 "2db96a37d78831d643538dd1b595d7d712e04bdccf8896a5e18ce0f398ea2ffc"
+  license "X11"
+
+  head do
+    url "https://gitlab.freedesktop.org/xorg/lib/libxcb-image.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "libxcb"
+  depends_on "xcb-util"
+
+  def install
+    system "./autogen.sh" if build.head?
+    system "./configure", "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}",
+                          "--localstatedir=#{var}",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    assert_match "-I#{include}", shell_output("pkg-config --cflags xcb-image")
+  end
+end
