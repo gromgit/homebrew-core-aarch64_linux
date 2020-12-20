46809d82a7ff045ab6223bc5aeb61a7b9411782d
Seeker <meaningseeking@protonmail.com>
2020-12-20 14:57:01 -0800

    xcb-util-cursor (new formula)
    Closes #67337.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

---
+++ Formula/xcb-util-cursor.rb
@@ -0,0 +1,59 @@
+class XcbUtilCursor < Formula
+  desc "XCB cursor library (replacement for libXcursor)"
+  homepage "https://xcb.freedesktop.org"
+  url "https://xcb.freedesktop.org/dist/xcb-util-cursor-0.1.3.tar.bz2"
+  sha256 "05a10a0706a1a789a078be297b5fb663f66a71fb7f7f1b99658264c35926394f"
+  license "X11"
+
+  head do
+    url "https://gitlab.freedesktop.org/xorg/lib/libxcb-cursor.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "util-macros" => :build
+  end
+
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "libxcb"
+  depends_on "xcb-util"
+  depends_on "xcb-util-image"
+  depends_on "xcb-util-renderutil"
+
+  def install
+    system "./autogen.sh" if build.head?
+    system "./configure", "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}",
+                          "--localstatedir=#{var}",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    flags = shell_output("pkg-config --cflags --libs xcb-util xcb-cursor").chomp.split
+    assert_includes flags, "-I#{include}"
+    assert_includes flags, "-L#{lib}"
+    (testpath/"test.c").write <<~EOS
+      #include <xcb/xcb.h>
+      #include <xcb/xcb_util.h>
+      #include <xcb/xcb_cursor.h>
+
+      int main(int argc, char *argv[]) {
+        int screennr;
+        xcb_connection_t *conn = xcb_connect(NULL, &screennr);
+        if (conn == NULL || xcb_connection_has_error(conn))
+          return 1;
+
+        xcb_screen_t *screen = xcb_aux_get_screen(conn, screennr);
+        xcb_cursor_context_t *ctx;
+        if (xcb_cursor_context_new(conn, screen, &ctx) < 0)
+          return 1;
+
+        xcb_cursor_t cid = xcb_cursor_load_cursor(ctx, "watch");
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", *flags
+  end
+end
