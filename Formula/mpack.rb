baac648e78a50da2854b73bf499ffc3550e04cc5
Mitchell Blank Jr <mitch@bodyfour.com>
2020-12-20 11:10:01 +0000

    mpack: update download URL
    archive.org links don't work for tar.gz downloads because they
    auto-gunzip and therefore break the sha256.
    
    Closes #67290.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/mpack.rb
+++ Formula/mpack.rb
@@ -1,9 +1,10 @@
 class Mpack < Formula
   desc "MIME mail packing and unpacking"
   homepage "https://web.archive.org/web/20190220145801/ftp.andrew.cmu.edu/pub/mpack/"
-  url "https://web.archive.org/web/20190220145801/ftp.andrew.cmu.edu/pub/mpack/mpack-1.6.tar.gz"
+  url "https://ftp.gwdg.de/pub/misc/mpack/mpack-1.6.tar.gz"
   mirror "https://fossies.org/linux/misc/old/mpack-1.6.tar.gz"
   sha256 "274108bb3a39982a4efc14fb3a65298e66c8e71367c3dabf49338162d207a94c"
+  license "BSD-3-Clause"
 
   bottle do
     cellar :any_skip_relocation
