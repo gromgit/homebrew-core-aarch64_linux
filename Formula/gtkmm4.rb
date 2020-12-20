ab4c95f7bd6c9411ce5b5e5ed6ca2965625ff3ff
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-12-20 10:50:15 +0000

    gtkmm4 4.0.0 (new formula)
    Closes #67288.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

---
+++ Formula/gtkmm4.rb
@@ -0,0 +1,44 @@
+class Gtkmm4 < Formula
+  desc "C++ interfaces for GTK+ and GNOME"
+  homepage "https://www.gtkmm.org/"
+  url "https://download.gnome.org/sources/gtkmm/4.0/gtkmm-4.0.0.tar.xz"
+  sha256 "3105adb76b738e4af5c0be7b1915f8e39de15474fcfdced9f5a59a2d9b52f83e"
+  license "LGPL-2.1-or-later"
+
+  livecheck do
+    url :stable
+    regex(/gtkmm[._-]v?(4\.([0-8]\d*?)?[02468](?:\.\d+)*?)\.t/i)
+  end
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "cairomm"
+  depends_on "gtk+4"
+  depends_on "pangomm"
+
+  def install
+    ENV.cxx11
+
+    mkdir "build" do
+      system "meson", *std_meson_args, ".."
+      system "ninja"
+      system "ninja", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <gtkmm.h>
+      class MyLabel : public Gtk::Label {
+        MyLabel(Glib::ustring text) : Gtk::Label(text) {}
+      };
+      int main(int argc, char *argv[]) {
+        return 0;
+      }
+    EOS
+    flags = shell_output("#{Formula["pkg-config"].opt_bin}/pkg-config --cflags --libs gtkmm-4.0").strip.split
+    system ENV.cxx, "-std=c++17", "test.cpp", "-o", "test", *flags
+    system "./test"
+  end
+end
