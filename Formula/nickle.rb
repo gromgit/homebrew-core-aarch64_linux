fc1165082a40d1cce26b8c67abdaa39c974f1a87
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-20 20:19:18 +0000

    nickle: update 2.90 bottle.

--- Formula/nickle.rb
+++ Formula/nickle.rb
@@ -10,10 +10,9 @@ class Nickle < Formula
   end
 
   bottle do
-    sha256 "cf810e5b29755dc5d8cfabb31d609241acfa892a8ff5b8598451ce19fa6954d7" => :big_sur
-    sha256 "d27c9f22a0526d8db09e27371181023ad120132f8a2ae63412b7d3b9e2564f0c" => :catalina
-    sha256 "456ad0c96d0c02e44571cf8e24f4fbe9bc9a1a5296c896a0cfae05fea9e576d2" => :mojave
-    sha256 "5887f0f5de76945279b8fe90c1be0dde47563b4ea5a97433611491c87e2487b3" => :high_sierra
+    sha256 "6e377f6674d6609f634b28941d8c53fef94c9cb429f31d1c765e4a5d8607e88d" => :big_sur
+    sha256 "3e1d028467ee41d963e9eaa9809f288fbc3effd826e09ae69bd4e4bfd26679c5" => :catalina
+    sha256 "6fa77667c30e0dfa186868159076bd2e003c34d32624915481f8c52e68b97f23" => :mojave
   end
 
   depends_on "readline"
