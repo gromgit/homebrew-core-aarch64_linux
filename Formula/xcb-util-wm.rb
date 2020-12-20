e96f4ae3040ebd78743320bad1bc9d6fd9770048
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-12-20 22:30:56 +0000

    xcb-util-wm: add 0.4.1 bottle.

--- Formula/xcb-util-wm.rb
+++ Formula/xcb-util-wm.rb
@@ -5,6 +5,13 @@ class XcbUtilWm < Formula
   sha256 "28bf8179640eaa89276d2b0f1ce4285103d136be6c98262b6151aaee1d3c2a3f"
   license "X11"
 
+  bottle do
+    cellar :any
+    sha256 "d63a72b6a9714c0e1d92e1d0da59fc702f1f8aa44dba75c2dcf85fb5a291908d" => :big_sur
+    sha256 "77611bd19da065ae3e1053ec9b581a52e93bc8669a8efee2d06719632695815f" => :catalina
+    sha256 "1423847ca100bb773cd5f85d1766abbf9004b88e85fc92cc25a30ea23341f0e3" => :mojave
+  end
+
   head do
     url "https://gitlab.freedesktop.org/xorg/lib/libxcb-wm.git"
 
