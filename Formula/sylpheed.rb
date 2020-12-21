2a7878a3cdbf61336e75f104855a6c7e737608d7
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-12-21 10:32:28 +0000

    sylpheed: revision bump for gtk+
    Closes #67356.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/sylpheed.rb
+++ Formula/sylpheed.rb
@@ -3,7 +3,7 @@ class Sylpheed < Formula
   homepage "https://sylpheed.sraoss.jp/en/"
   url "https://sylpheed.sraoss.jp/sylpheed/v3.7/sylpheed-3.7.0.tar.bz2"
   sha256 "eb23e6bda2c02095dfb0130668cf7c75d1f256904e3a7337815b4da5cb72eb04"
-  revision 3
+  revision 4
 
   livecheck do
     url "https://sylpheed.sraoss.jp/en/download.html"
