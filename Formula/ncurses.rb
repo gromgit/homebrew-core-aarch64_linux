9c2b827fb972d3bef774c2bef656309c1614fe25
FX Coudert <fxcoudert@gmail.com>
2020-12-21 11:13:47 +0000

    ncurses: update 6.2 bottle.

--- Formula/ncurses.rb
+++ Formula/ncurses.rb
@@ -12,6 +12,7 @@ class Ncurses < Formula
 
   bottle do
     sha256 "b34b4aa433f23405c6104bf810836eed2686f11c660b8668a7af86f99be2329f" => :big_sur
+    sha256 "111ab64b5ffbe5aebe18096a2ff95e8e8f6426955799181db8338677e8f853e4" => :arm64_big_sur
     sha256 "eae51ad3391edafe3d6c649ba44f607ee1464b4b5d9ee48770e9817ee5f0ccdd" => :catalina
     sha256 "1771e0ce821cf8cbe38d0ce8d1843fd559532923222edc5dbf5b31fcf24fed90" => :mojave
     sha256 "4648be8457b081026d3da80f290abaf3fbfdcb49d62914861a63fc706f9adabe" => :high_sierra
