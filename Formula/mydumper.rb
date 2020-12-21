5b269fd7d068476dd1b08a73d1c8729358e29409
Mitchell Blank Jr <mitch@bodyfour.com>
2020-12-21 10:39:05 +0000

    mydumper: further tweaks to build on Big Sur
    Closes #67361.
    
    Signed-off-by: chenrui <chenrui333@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mydumper.rb
+++ Formula/mydumper.rb
@@ -3,7 +3,7 @@ class Mydumper < Formula
   homepage "https://launchpad.net/mydumper"
   url "https://github.com/maxbube/mydumper/archive/v0.9.5.tar.gz"
   sha256 "544d434b13ec192976d596d9a7977f46b330f5ae3370f066dbe680c1a4697eb6"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
 
   bottle do
     cellar :any
@@ -20,14 +20,25 @@ class Mydumper < Formula
   depends_on "openssl@1.1"
   depends_on "pcre"
 
+  uses_from_macos "zlib"
+
   # This patch allows cmake to find .dylib shared libs in macOS. A bug report has
   # been filed upstream here: https://bugs.launchpad.net/mydumper/+bug/1517966
   # It also ignores .a libs because of an issue with glib's static libraries now
   # being included by default in homebrew.
+  #
+  # Although we override the mysql library location this patch is still required
+  # because the setting of ${CMAKE_FIND_LIBRARY_SUFFIXES} affects other probes as well.
   patch :p0, :DATA
 
   def install
-    system "cmake", ".", *std_cmake_args
+    system "cmake", ".", *std_cmake_args,
+           # Override location of mysql-client:
+           "-DMYSQL_CONFIG_PREFER_PATH=#{Formula["mysql-client"].opt_bin}",
+           "-DMYSQL_LIBRARIES=#{Formula["mysql-client"].opt_lib}/libmysqlclient.dylib",
+           # find_package(ZLIB) has troube on Big Sur since physical libz.dylib
+           # doesn't exist on the filesystem.  Instead provide details ourselves:
+           "-DCMAKE_DISABLE_FIND_PACKAGE_ZLIB=1", "-DZLIB_INCLUDE_DIRS=/usr/include", "-DZLIB_LIBRARIES=-lz"
     system "make", "install"
   end
 
