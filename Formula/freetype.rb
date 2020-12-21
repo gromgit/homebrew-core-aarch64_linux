cdd457817da19e72ec1177fb5b19762e2fe9caa7
FX Coudert <fxcoudert@gmail.com>
2020-12-21 11:04:57 +0000

    freetype: update 2.10.4 bottle.

--- Formula/freetype.rb
+++ Formula/freetype.rb
@@ -14,6 +14,7 @@ class Freetype < Formula
   bottle do
     cellar :any
     sha256 "01b464b98584ba5777d8fc4605121c7a46e713a2f58d729197b82afef1b5f2b9" => :big_sur
+    sha256 "0d3385d0d11a5d0198c09bfb77ba854766a3345067023d2fdc9b486ead52c392" => :arm64_big_sur
     sha256 "b4e7683ae202c49280024faac4ac7437e690cb5dd83edb806fac368bc2b7de35" => :catalina
     sha256 "81c65539bcc98d171fdff7a6e80cdddd7dc4bc9ed34e739c4361ab66f3391991" => :mojave
     sha256 "666892404720bcd855d866976e1cb9beecc3151ca595c3dd115a0daa6bb6c7e1" => :high_sierra
