8baf708c547032406ea768e62bb938a2b88b752b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-21 11:36:13 +0000

    gitui: update 0.11.0 bottle.

--- Formula/gitui.rb
+++ Formula/gitui.rb
@@ -7,10 +7,9 @@ class Gitui < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6d12f2cabd3e20869f4fdf7fcce2325a34d56302cc6fc23922dabf2cf869e05c" => :big_sur
-    sha256 "15fc69a2aef24cc4292dba4f0de18bc6baa176788d044973e79a11fbc2ccb103" => :catalina
-    sha256 "466fbe53bd0775b5cacb9281a9c5846178ae448fbe079d32cb9dc19f037411d2" => :mojave
-    sha256 "5e725a78841410e48a7a932c79cb98bb02d8c567a0c0705334d24549fdfcc81f" => :high_sierra
+    sha256 "60db2601c6791f28adfa590269af2216b644c0aec57c7b76760fdbd2782c9b4c" => :big_sur
+    sha256 "58b881c6404ef201a9f386285cad4be06eed6f9da934895d9feb6e813cd9448d" => :catalina
+    sha256 "724c0c2e5055cea5d49c376c2b6c8681847cf9f0a45079a3fff103f2e5641028" => :mojave
   end
 
   depends_on "rust" => :build
