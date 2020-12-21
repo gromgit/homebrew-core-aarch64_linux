7f81a32858768a6df36ccc2d7523782c4c26f0c7
FX Coudert <fxcoudert@gmail.com>
2020-12-21 11:01:50 +0000

    cscope: update 15.9 bottle.

--- Formula/cscope.rb
+++ Formula/cscope.rb
@@ -12,6 +12,7 @@ class Cscope < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "41553bf0bbc2ce6e41712381fcbc2d86eca0dd4618d138ca70037df8a1bf4e01" => :big_sur
+    sha256 "149cb1ebe576bca4c8c928ed98005054e9ac2b8822c80ce9582d500453bce0ad" => :arm64_big_sur
     sha256 "212b5f945f2a2eae2d07893bb08c490098f4f3e58ec8865499bec550882de29e" => :catalina
     sha256 "0a8c76e372e2c965e654b5024cbf872931e6204b7e2ba79623d5d7d002cd3c2f" => :mojave
     sha256 "ae7b5f716debeb937c3472add41f69c7176e9c4a9a0668090afd63313eabbe86" => :high_sierra
