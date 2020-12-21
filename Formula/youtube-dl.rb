6cbda9640c5fce82c376e33edb23c67455a8cab1
FX Coudert <fxcoudert@gmail.com>
2020-12-21 11:58:47 +0000

    youtube-dl: update 2020.12.14 bottle.

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -10,6 +10,7 @@ class YoutubeDl < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "5f2a2e7aa3c19963af8dc2339dab662a19c8d94f2a83450f18cc78e2ee0cb516" => :big_sur
+    sha256 "a8a2e02547f44713ebbec734a7311cf4687ff84a5d93744d14f4fdd5360a7154" => :arm64_big_sur
     sha256 "ab8a02e2db35d53df6361de57520aa463b55563fe990f0c59e477d1e4e539ec2" => :catalina
     sha256 "55dc22a1db012a2e89fe4e82e3edfebda0693fc31cd1dc3f7891283bd74e678a" => :mojave
   end
