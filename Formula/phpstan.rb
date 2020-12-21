fd9cf75bf3bb28f46b2316a47702a06ead42c9d5
Alexander M. Turek <me@derrabus.de>
2020-12-21 15:56:47 +0100

    phpstan 0.12.64 (#67368)

--- Formula/phpstan.rb
+++ Formula/phpstan.rb
@@ -1,8 +1,8 @@
 class Phpstan < Formula
   desc "PHP Static Analysis Tool"
   homepage "https://github.com/phpstan/phpstan"
-  url "https://github.com/phpstan/phpstan/releases/download/0.12.63/phpstan.phar"
-  sha256 "b0dc6f509a5eb2cc5ca310802a05755a84f73e8a1262ea46ebbd9c6c79d7c80a"
+  url "https://github.com/phpstan/phpstan/releases/download/0.12.64/phpstan.phar"
+  sha256 "babad49139e817be20fa1cdedad64ce645ce602d31c318548cc94c799e353a61"
   license "MIT"
 
   bottle :unneeded
