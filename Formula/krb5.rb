f52d1df007673fa3394e3e1596f5cbf5e4e42bd5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-21 14:21:55 +0000

    krb5: update 1.18.3 bottle.

--- Formula/krb5.rb
+++ Formula/krb5.rb
@@ -11,10 +11,10 @@ class Krb5 < Formula
   end
 
   bottle do
-    sha256 "ce41211e7264912be6fc05d8e5ed3bbc141679b0ab6c9b4099a36da811077a9f" => :big_sur
-    sha256 "888d0d0303ea37c974c22cb790fe6a9cd5bdcc3beab3fc10291dc5410b856e95" => :arm64_big_sur
-    sha256 "f50ff30a8268899f65150363f294ddcbd1969759f8700efac874caa181bca75d" => :catalina
-    sha256 "4e5751f16b7e6d9f27968a9efb1ba5f702292c7fefdb25a32c940ef71f81b7cd" => :mojave
+    rebuild 1
+    sha256 "a72fae06ddd1d796a6c1ab55a9c8bc15e8e051c67e72412dbc86cea9bcd04c62" => :big_sur
+    sha256 "67f67b210947e2bd62d974b2494f1192f169fae35605f38f7b2f0a9a73eb0633" => :catalina
+    sha256 "3d09843ed22dfe2ce8c193eb3c6183eee9c278e06f179773930a8017d649d312" => :mojave
   end
 
   keg_only :provided_by_macos
