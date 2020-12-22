e556821f8fef83e26c2bd32276001f9c214d5104
Paulo Moura <pmoura@logtalk.org>
2020-12-22 02:09:02 +0000

    logtalk 3.43.0
    Closes #67399.
    
    Signed-off-by: Seeker <meaningseeking@protonmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/logtalk.rb
+++ Formula/logtalk.rb
@@ -1,9 +1,9 @@
 class Logtalk < Formula
   desc "Declarative object-oriented logic programming language"
   homepage "https://logtalk.org/"
-  url "https://github.com/LogtalkDotOrg/logtalk3/archive/lgt3420stable.tar.gz"
-  version "3.42.0"
-  sha256 "4c9149dc8b9c57e9cb78cbdb98fb8d9701ff2772fbc996a7e28c309324ef678b"
+  url "https://github.com/LogtalkDotOrg/logtalk3/archive/lgt3430stable.tar.gz"
+  version "3.43.0"
+  sha256 "7e38652183d022abe7f41014037c9d334a445f8294e72426068c1d62170a0ac5"
   license "Apache-2.0"
 
   livecheck do
