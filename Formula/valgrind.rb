74f4ad5605a95d02adb0685f9149056350edd836
Daniel Nachun <daniel.nachun@gmail.com>
2020-12-22 12:07:16 -0800

    valgrind: disable mpicc and make Linux only (#67341)
    * valgrind: disable mpicc and make Linux only
    
    * tests-linux.yaml: add valgrind

--- Formula/valgrind.rb
+++ Formula/valgrind.rb
@@ -1,22 +1,13 @@
 class Valgrind < Formula
   desc "Dynamic analysis tools (memory, debug, profiling)"
   homepage "https://www.valgrind.org/"
+  url "https://sourceware.org/pub/valgrind/valgrind-3.16.1.tar.bz2"
+  sha256 "c91f3a2f7b02db0f3bc99479861656154d241d2fdb265614ba918cc6720a33ca"
   license "GPL-2.0"
-
-  stable do
-    url "https://sourceware.org/pub/valgrind/valgrind-3.16.1.tar.bz2"
-    sha256 "c91f3a2f7b02db0f3bc99479861656154d241d2fdb265614ba918cc6720a33ca"
-
-    depends_on maximum_macos: :high_sierra
-  end
 
   livecheck do
     url "https://sourceware.org/pub/valgrind/"
     regex(/href=.*?valgrind[._-]v?(\d+(?:\.\d+)+)\.t/i)
-  end
-
-  bottle do
-    sha256 "7170a66beb19ccfd79d1559fe57c67fb4a6a7b6369775621f5073af6fea07ea8" => :high_sierra
   end
 
   head do
@@ -27,17 +18,16 @@ class Valgrind < Formula
     depends_on "libtool" => :build
   end
 
-  # Valgrind needs vcpreload_core-*-darwin.so to have execute permissions.
-  # See #2150 for more information.
-  skip_clean "lib/valgrind"
+  depends_on :linux
 
   def install
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
       --enable-only64bit
-      --build=amd64-darwin
+      --without-mpicc
     ]
+
     system "./autogen.sh" if build.head?
 
     system "./configure", *args
@@ -46,6 +36,6 @@ class Valgrind < Formula
   end
 
   test do
-    system "#{bin}/valgrind", "ls", "-l"
+    assert_match "usage", shell_output("#{bin}/valgrind --help")
   end
 end
