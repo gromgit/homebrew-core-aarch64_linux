40bb8471f74257c4abf659b82df082cf2dc55244
FX Coudert <fxcoudert@gmail.com>
2020-12-22 22:31:02 +0000

    lmdb: update 0.9.27 bottle.

--- Formula/lmdb.rb
+++ Formula/lmdb.rb
@@ -15,6 +15,7 @@ class Lmdb < Formula
   bottle do
     cellar :any
     sha256 "6244986c2cd93bd2770fc0cfac79f80e9d98ea197d84acce00322335fe9e2e18" => :big_sur
+    sha256 "706f2a266c430ad3cffcbf5bc9e87a6d1e63a9eb7f2d74b3b95c28e8910cd71d" => :arm64_big_sur
     sha256 "1495b5b154e77771ff450d1687c2afeec377db57498900b2bb692d23b4bd25b8" => :catalina
     sha256 "496f41bc0f050e5657a4ecc1409fccf8f2247521c3d3ebe16bdeb2007e0dbc71" => :mojave
     sha256 "36d2564ec79b8547154e706980fbc25d38575d424299216ec10adae598e6b1c9" => :high_sierra
