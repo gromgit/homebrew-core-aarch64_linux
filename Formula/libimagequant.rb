3158ce1c46b957fa0e3dace0e93229c04d3917be
FX Coudert <fxcoudert@gmail.com>
2020-12-22 22:01:53 +0000

    libimagequant: update 2.13.1 bottle.

--- Formula/libimagequant.rb
+++ Formula/libimagequant.rb
@@ -8,6 +8,7 @@ class Libimagequant < Formula
   bottle do
     cellar :any
     sha256 "4eec259853e8c650282ab0f9fe2fe382cfb0c63adf50eefb0e53038e9579b117" => :big_sur
+    sha256 "3f24de6501d3c1ceea4d4c79d2895332fec3796d09cda4ff5abc456479ca1504" => :arm64_big_sur
     sha256 "ddaab4f5d02ee638676c716701a3e4364c76a3e6184787b2b5bc078b95497fb2" => :catalina
     sha256 "f7902838c7698902828571a5bbb170a1855ee66d96070e7b577b9a332370989b" => :mojave
   end
