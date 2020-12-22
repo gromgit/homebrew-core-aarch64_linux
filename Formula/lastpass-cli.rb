434faddde34cab23d2c9d542af44cdd25a2f4e4d
FX Coudert <fxcoudert@gmail.com>
2020-12-22 22:56:30 +0000

    lastpass-cli: update 1.3.3_1 bottle.

--- Formula/lastpass-cli.rb
+++ Formula/lastpass-cli.rb
@@ -11,6 +11,7 @@ class LastpassCli < Formula
     cellar :any
     rebuild 4
     sha256 "10f9224c8bfebae0cf12df72e6144ba3a309956b1efcce574975cd21cec930c5" => :big_sur
+    sha256 "1bccfb715b94c569d943c9ce5833f74628397abf1311f74c49d6e5d4b25b847b" => :arm64_big_sur
     sha256 "8643f81d13a40ef8b86efe83fbee1b41b22c492b7725bebab83dcb2d253fd603" => :catalina
     sha256 "f1b7c42dd889f597ef06f0bd72bb1b273c21dc91e5f3e313da8599254954a7ae" => :mojave
     sha256 "62629472aafb7e4927d8ab5e9d7189c913e3249a172d0445ffe7eda31b642eb7" => :high_sierra
