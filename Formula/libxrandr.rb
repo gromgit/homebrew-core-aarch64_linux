1a96d6147662785d79c7df47423c57db399f33bc
FX Coudert <fxcoudert@gmail.com>
2020-12-22 22:03:27 +0000

    libxrandr: update 1.5.2 bottle.

--- Formula/libxrandr.rb
+++ Formula/libxrandr.rb
@@ -8,6 +8,7 @@ class Libxrandr < Formula
   bottle do
     cellar :any
     sha256 "68c082bb3a5a94cd881edf9d575cb27ca116836dfdd6d46e69a1b18344b5df5a" => :big_sur
+    sha256 "57638149377d964bb0452e1bdf36b9712ebe35254ed05e06ac2ff66fdbf6beb6" => :arm64_big_sur
     sha256 "62f9efb3fcb658182731de45c6b8a20a941ecb5b1e14e4a5375aa3d1b58ef530" => :catalina
     sha256 "d1cf6d028abfae84918dbfc05a497ee46daa653a2337a3112ecb101193f0ed2b" => :mojave
     sha256 "5cd68c19e9821dff52249e3598b82c2010ddc43cb029fa80ef73c3b620c65bed" => :high_sierra
