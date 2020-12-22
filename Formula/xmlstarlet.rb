dba322355457d15eb8f4d026a345cc0f633ad668
FX Coudert <fxcoudert@gmail.com>
2020-12-22 22:18:49 +0000

    xmlstarlet: update 1.6.1 bottle.

--- Formula/xmlstarlet.rb
+++ Formula/xmlstarlet.rb
@@ -12,6 +12,7 @@ class Xmlstarlet < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "bc3baa847a617f3d67000ff14e96126f9bbdf54e916b6c693a2c8bf633ca0bfa" => :big_sur
+    sha256 "d654b90521d07ffc1304253fd2b927f9813effa5a65c3f6c43b67838077c009d" => :arm64_big_sur
     sha256 "2a679570811f553e345748516fa37c2d4b529a75533bdb73316077aaed5ab8f6" => :catalina
     sha256 "6e5d11ee1419a61a9f043663c1236d064ee692fd187ae15bf2114b42d8f0889e" => :mojave
     sha256 "56ce0e3190080e6e1111ebb31aa06aea53f16cde50359a356c24ff86a4df72b3" => :high_sierra
