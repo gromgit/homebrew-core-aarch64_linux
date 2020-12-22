270133126c4a37f6605c6be1e5a6edc88c6fd737
FX Coudert <fxcoudert@gmail.com>
2020-12-22 18:01:49 +0000

    qpdf: update 10.0.4 bottle.

--- Formula/qpdf.rb
+++ Formula/qpdf.rb
@@ -8,6 +8,7 @@ class Qpdf < Formula
   bottle do
     cellar :any
     sha256 "7fcecfff0483197b214c156a1708b3c8cb854dfcae8fdc3839a8bdbd37449bea" => :big_sur
+    sha256 "129e28c8fb16311abbd8cb7a2fb11a2d819035809f980bc8c5152a2c0a3cd87b" => :arm64_big_sur
     sha256 "73dca004aacd24237d98c604885492e13a1156151113485df48877d99a915423" => :catalina
     sha256 "8556fd9465de6203de5104777092e3263ecb0f8b9146ff35372307ea857acd6f" => :mojave
   end
