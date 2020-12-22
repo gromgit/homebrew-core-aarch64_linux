dd2d8f8d33d40bd71e94da8fd6d58acb6d31b69b
FX Coudert <fxcoudert@gmail.com>
2020-12-22 22:33:16 +0000

    axel: update 2.17.10 bottle.

--- Formula/axel.rb
+++ Formula/axel.rb
@@ -9,6 +9,7 @@ class Axel < Formula
   bottle do
     cellar :any
     sha256 "94b9f93614705dab7c202df271f9bb1bcd30b4e1170f4ab4b160378e8e5c3a2f" => :big_sur
+    sha256 "43a36bca363fd2a2700dbaca686de5d92793ae79b1813e26e6ba1965e9d0acc7" => :arm64_big_sur
     sha256 "32832dd93a31589c7f98e510a2edc54e918ee6bab8eab18f4f4a1b953030f3f1" => :catalina
     sha256 "2df5f78ceaccbdede61b29a191c514a5b86dfb3ab1fd5057506377299d9f8c65" => :mojave
   end
