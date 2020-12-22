2795f1fe95498ecb94c62b3d20d7d2d0b58a6333
FX Coudert <fxcoudert@gmail.com>
2020-12-22 17:09:04 +0000

    librttopo: update 1.1.0 bottle.

--- Formula/librttopo.rb
+++ Formula/librttopo.rb
@@ -8,6 +8,7 @@ class Librttopo < Formula
   bottle do
     cellar :any
     sha256 "59068843a454371abc25ad9421771eb2770febfaa00d41e1527476f4cbfdb05b" => :big_sur
+    sha256 "531ac2ca2e4247da1ebdacac77cbd68faaf5ea5935608de9ff842ae21aa18ce0" => :arm64_big_sur
     sha256 "9512f32068f310fc02c082828e4ebac85a698ef69f370243aa00a5b873569319" => :catalina
     sha256 "d6bc9674875a3eeb44cec544f6cc9ac9ce6435f7fd951f446801a8aadcb1a323" => :mojave
   end
