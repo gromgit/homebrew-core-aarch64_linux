06d7266820ebea04d9171dbbf8f709f8c9c6e799
FX Coudert <fxcoudert@gmail.com>
2020-12-22 18:25:47 +0000

    ntl: update 11.4.3 bottle.

--- Formula/ntl.rb
+++ Formula/ntl.rb
@@ -12,6 +12,7 @@ class Ntl < Formula
   bottle do
     cellar :any
     sha256 "9e02780202c0a4b9b65080df3a731347b2f8fc1cfdeb4073d68440acba012ecd" => :big_sur
+    sha256 "145c34c0546556702339547567005c511610875f6817a49ccc8c08eda9cc1086" => :arm64_big_sur
     sha256 "fc44a358782565b05098a29f2694fe16100c2b5aa096c04875edd093adf78b5d" => :catalina
     sha256 "d0739cc2ebea1427d1fae3b0f871105b69d6f9c4c765415ed2f328af1e925598" => :mojave
     sha256 "5747add8bf85ae5a46d8c12635efbf61a2b5c402e35fdaebcf7499148c682564" => :high_sierra
