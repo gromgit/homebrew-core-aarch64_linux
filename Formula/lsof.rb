8cb94995d8a945dc7eb54747438134230258ed30
FX Coudert <fxcoudert@gmail.com>
2020-12-22 17:15:11 +0000

    lsof: update 4.94.0 bottle.

--- Formula/lsof.rb
+++ Formula/lsof.rb
@@ -8,6 +8,7 @@ class Lsof < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "7dbab0c2a35d97381ed52fa32e1507c0fe83bc405fc40c4d00c79e12c79cffe4" => :big_sur
+    sha256 "3202f83509eefa73603b865ea4aca0433bbbf69d30f43d229a5ee256d1977424" => :arm64_big_sur
     sha256 "58d2ee9a7484541a7280f5a139f2d0454b494f54bca3b9f10273e036d8071bde" => :catalina
     sha256 "9eb185a83e641bd8bd90fab3a8cde572b23ebb1ce269a8832fb85a66c5037318" => :mojave
     sha256 "268fe15ecc8d9e4dd4f2f45737c921e54a5aa999f15ab6b724b9bd34deeef8d1" => :high_sierra
