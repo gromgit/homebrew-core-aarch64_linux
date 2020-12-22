d8aaceb2d63c6b81cffd52ba369a79ea37e867f3
FX Coudert <fxcoudert@gmail.com>
2020-12-22 16:27:33 +0000

    freetds: update 1.2.18 bottle.

--- Formula/freetds.rb
+++ Formula/freetds.rb
@@ -12,6 +12,7 @@ class Freetds < Formula
 
   bottle do
     sha256 "e1915b8ec9a59b5146e50310e778c4cc247df3d12bdfbd41fd1601abc68d4a5b" => :big_sur
+    sha256 "52a717a13ff3fb2541c2dd1ff55448f9201712acf875f740282b5ebf3c207128" => :arm64_big_sur
     sha256 "d50d6963a87b72230cdd9c30e17a05baf73970b26d2ef031e9496181bb41afad" => :catalina
     sha256 "fce5977a4c8d363777846b434747d5dd6207e00273769e31354d090ed35d9a00" => :mojave
   end
