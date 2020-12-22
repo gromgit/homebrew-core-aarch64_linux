2c5ad1d54a54995598ec3b0fbb9a65a2915613b3
FX Coudert <fxcoudert@gmail.com>
2020-12-22 20:58:59 +0000

    wcslib: update 7.3 bottle.

--- Formula/wcslib.rb
+++ Formula/wcslib.rb
@@ -13,6 +13,7 @@ class Wcslib < Formula
   bottle do
     cellar :any
     sha256 "14760499e809a0b5878d03b2f22f15975906fea80bb63b173a35c67645c3654a" => :big_sur
+    sha256 "c733b73fe01c146992c2fa05865c631907c61cbc26e5c66980471441c5c7910f" => :arm64_big_sur
     sha256 "a0e15ea5ee23106c24960feed0c7dad6762d8e75cb9d42445c197fb38f079965" => :catalina
     sha256 "d8b3561a7e87031d7d6f8042af1c75f21663874921da17d5061d3ffe558263f1" => :mojave
     sha256 "941ce001ceb21e53dc6af78e8e09ebc52a24b57efcd51c009f8416789674f8ee" => :high_sierra
