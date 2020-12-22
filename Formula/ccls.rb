341c1a906fda388c4a965fb93ce633a4b55d3634
Rui Chen <rui@meetup.com>
2020-12-22 12:07:02 -0500

    ccls 0.20201219
    Closes #67432.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ccls.rb
+++ Formula/ccls.rb
@@ -1,8 +1,8 @@
 class Ccls < Formula
   desc "C/C++/ObjC language server"
   homepage "https://github.com/MaskRay/ccls"
-  url "https://github.com/MaskRay/ccls/archive/0.20201025.tar.gz"
-  sha256 "1470797b2c1a466e2d8a069efd807aac6fefdef8a556e1edf2d44f370c949221"
+  url "https://github.com/MaskRay/ccls/archive/0.20201219.tar.gz"
+  sha256 "edd3435bc7e55d9e5dc931932f9c98275a6a28d1ab1f66416110e029f3f2882a"
   license "Apache-2.0"
   head "https://github.com/MaskRay/ccls.git"
 
