0ffd02aad40e882ffedf64c683ea4004238b13ab
FX Coudert <fxcoudert@gmail.com>
2020-12-22 17:59:14 +0000

    unibilium: update 2.1.0 bottle.

--- Formula/unibilium.rb
+++ Formula/unibilium.rb
@@ -8,6 +8,7 @@ class Unibilium < Formula
   bottle do
     cellar :any
     sha256 "1ac932c37c4889fdf5382a66a5c909ece4d4c854835b24215c3c96653ebb914d" => :big_sur
+    sha256 "4615fc8228f3975d90334bbc9892e7fc5a0114cce8b1e9b0445c1d42f3caa4d1" => :arm64_big_sur
     sha256 "62b5e586837c4390918437def45064ce86c7ac8b81f570ceb11f98aed2b563af" => :catalina
     sha256 "3b3292fa69eac93918fee92ffdb3b06f98524cc1fa705964a10fef35be4314a2" => :mojave
   end
