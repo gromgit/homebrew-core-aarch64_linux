afe6988743e7e0d7a93175aedfccef9cfc9fd169
FX Coudert <fxcoudert@gmail.com>
2020-12-22 22:57:29 +0000

    octomap: update 1.9.5 bottle.

--- Formula/octomap.rb
+++ Formula/octomap.rb
@@ -7,6 +7,7 @@ class Octomap < Formula
 
   bottle do
     sha256 "61071dabd01169f7677c8a3794920719f5d746b261c52ec31f087d2c9c1cd407" => :big_sur
+    sha256 "ccd5bda09d82b445cec55b443429d18ac9c5567c73f69b1e1a42d190b6f49653" => :arm64_big_sur
     sha256 "dcf938ee253a527143d78cbc39c20046015519b5aff741656016f0a67ff705b9" => :catalina
     sha256 "878681f950a14d49b47bd69605a12bae4f42f127b4cc7750de14b7a16f6a9930" => :mojave
     sha256 "92def71d469ce8d5a660d371eded16f8d25f9346e2fc3f34ffd105f3bbd2060c" => :high_sierra
