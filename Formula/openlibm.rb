5aa3254c0a8dcf8f9e03e37be5b9c3545b155f24
FX Coudert <fxcoudert@gmail.com>
2020-12-22 23:04:37 +0000

    openlibm: update 0.7.3 bottle.

--- Formula/openlibm.rb
+++ Formula/openlibm.rb
@@ -7,6 +7,7 @@ class Openlibm < Formula
   bottle do
     cellar :any
     sha256 "325d0ffd1c5d9ea2fa7c024718fdedbb422cb0413a3d2af4a049721259123605" => :big_sur
+    sha256 "892a6ecb5cd33f2c315331158c9d91138f46fd6bc75f7c85d30ae9da90580eaf" => :arm64_big_sur
     sha256 "7ba4146b71d0ec4a4357bb04a000651b8acf7c4d788c754609cfe3ecb2edf907" => :catalina
     sha256 "440a34d2b12672d844a12b1b9dee61e41920a60d1a743c5ea65f864a592bf046" => :mojave
   end
