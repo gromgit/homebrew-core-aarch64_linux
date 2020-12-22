49e58aa5fe105971f3b81ee38425abb98efeb1cc
FX Coudert <fxcoudert@gmail.com>
2020-12-22 16:25:23 +0000

    intltool: update 0.51.0 bottle.

--- Formula/intltool.rb
+++ Formula/intltool.rb
@@ -12,6 +12,7 @@ class Intltool < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "aacf573a663f8c555bfa8163593386046462856392001b9dcad317fcf889fdfe" => :big_sur
+    sha256 "a95b3272a26918e1a92ad548ca72e1b74f5ade8073193c560c418369f9dacb51" => :arm64_big_sur
     sha256 "853b0f355c1bb6bdfc41d2ad17026d75c93aecb7581e711d7db3edab4ca6b5d4" => :catalina
     sha256 "52ccb5bfce1cda123f30c84335172335cee0706973e6769ec9a5358cb160f364" => :mojave
     sha256 "7924c9c7dc7b3eee0056171df8c6b66c2e0e8888e4638232e967a5ea31ca5b86" => :high_sierra
