1f25963e88136d844d7de78b1676a99459c5234e
FX Coudert <fxcoudert@gmail.com>
2020-12-22 18:02:20 +0000

    nano: update 5.4 bottle.

--- Formula/nano.rb
+++ Formula/nano.rb
@@ -12,6 +12,7 @@ class Nano < Formula
 
   bottle do
     sha256 "ac9aba47448b4561e8c2fecc2fbc9504952e3e3c39506dba7844b2c61ffbb149" => :big_sur
+    sha256 "ca3b7ef1d7422a7d019d8e8299fd07a20d0332941940ebdb37a02ae45538d773" => :arm64_big_sur
     sha256 "2f7dab76ce57ceba10c3c8527ea68e5f060b5b8871128a170585175899a0eb87" => :catalina
     sha256 "ed3c326161300b01b3f83f44a51749e665fc129eae8bbf2bc150bee10bc1ec05" => :mojave
   end
