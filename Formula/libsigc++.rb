621bf5c5b82517be50c6c5f3a4d36be398e2ea1b
FX Coudert <fxcoudert@gmail.com>
2020-12-22 18:03:50 +0000

    libsigc++: update 3.0.6 bottle.

--- Formula/libsigc++.rb
+++ Formula/libsigc++.rb
@@ -12,6 +12,7 @@ class Libsigcxx < Formula
   bottle do
     cellar :any
     sha256 "eea9483112f22c255c7b783333a5a48b90e2fb0747ad00f0ed4b37275481b93a" => :big_sur
+    sha256 "34fc45a5a4a36a592f6cb4b1671e65fddea15b3df12dd114359a5ca3101f3665" => :arm64_big_sur
     sha256 "5f7d8b6e6043bcab63f7f5675746f4d94f447cde8a48513c9db7b36b5a527e05" => :catalina
     sha256 "6f3562f317a110489a2df296f8b8b3cb8bc37295b6aa5d306a5c0078f7fdb7cc" => :mojave
   end
