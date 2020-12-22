bf772c0fb36ccffdd77131e35e4563eda21acc52
FX Coudert <fxcoudert@gmail.com>
2020-12-22 18:22:37 +0000

    fmt: update 7.1.3 bottle.

--- Formula/fmt.rb
+++ Formula/fmt.rb
@@ -8,6 +8,7 @@ class Fmt < Formula
   bottle do
     cellar :any
     sha256 "030400184b37be2dbefd79244622b6ba1db79a7a082063c9731422d67c1ec689" => :big_sur
+    sha256 "ca7feb7f642cd97750e7df4a46229c5c8bf5948673a5b2be5cec24a4e4981ec3" => :arm64_big_sur
     sha256 "bc10923606cbc09de72d80d4fa35f2834cd3661791b493936a8d2853e571ef9d" => :catalina
     sha256 "9ff67f083520e388e629e37267b87099e2335ff8bedecd7662779f3042f11f3e" => :mojave
   end
