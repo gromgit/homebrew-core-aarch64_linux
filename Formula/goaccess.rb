aa4d20cf53aa9728c3ae82c89ec590acf6ee8c7a
FX Coudert <fxcoudert@gmail.com>
2020-12-22 22:42:30 +0000

    goaccess: update 1.4.3 bottle.

--- Formula/goaccess.rb
+++ Formula/goaccess.rb
@@ -8,6 +8,7 @@ class Goaccess < Formula
 
   bottle do
     sha256 "527c1e4d90f44d3e962630e8069033d0ce85ead276e2ef648e41ec75f08d7b90" => :big_sur
+    sha256 "d680eea84ac33cc1265825b9811925f5ae5d565cb4a2be58222b971cb25245e6" => :arm64_big_sur
     sha256 "7d4f0056b16e399c48fbb7c695491dab296b0eccb9b5034cf8473dd1f4d8aa57" => :catalina
     sha256 "aaa132532ebc07d5817cf8f30f3f957f12dcab6b25a3665420bf7aa02ad79c24" => :mojave
   end
