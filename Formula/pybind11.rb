3534d4459a3bf811dc3a654e5696aa588e077727
FX Coudert <fxcoudert@gmail.com>
2020-12-22 18:03:31 +0000

    pybind11: update 2.6.1 bottle.

--- Formula/pybind11.rb
+++ Formula/pybind11.rb
@@ -8,6 +8,7 @@ class Pybind11 < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "2f8d6a8a4ec4e2d35ed5926f56d49e0a3c1a3a853f16f1fe266e4cb25673a2c9" => :big_sur
+    sha256 "03e24dccac463619c2dde2e5dd928515a885cebba2ac9652fb14dd9177710fd9" => :arm64_big_sur
     sha256 "a65ec879104470206955784e0715d9188fd2f3848dcd88714e094313ab8305de" => :catalina
     sha256 "122967134526009627cf8648d4181f4a7e06688d5b74ffa0a2767abeeb54e091" => :mojave
     sha256 "2128187d3a45fbb3dfe2426b8e974ad15a07942a17ae07c09a52f07482b11bdf" => :high_sierra
