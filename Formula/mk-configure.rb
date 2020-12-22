5f01ef97c860fc70179d55039c866ad6d071b807
FX Coudert <fxcoudert@gmail.com>
2020-12-22 22:59:43 +0000

    mk-configure: update 0.35.0 bottle.

--- Formula/mk-configure.rb
+++ Formula/mk-configure.rb
@@ -12,6 +12,7 @@ class MkConfigure < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "5bc4a9bc45792bed751f3dc08ec1c0e73885c15f3e33c66c80bccfff913120e1" => :big_sur
+    sha256 "bab03e2a9f0585c8225bde715df65bab1561472332aec46df45a03d63eed6003" => :arm64_big_sur
     sha256 "46eeafa552c407c5e43ef5ff9fe17331e047063e96c7e4c422ca81e60e448477" => :catalina
     sha256 "d79a5855774a12a0729cd3f18c4fa623a6b0263e89dc83a7b97a31843bc1980f" => :mojave
   end
