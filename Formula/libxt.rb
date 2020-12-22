b89e0e40078d788424683a62809bc6e7cde0b6ef
FX Coudert <fxcoudert@gmail.com>
2020-12-22 22:03:24 +0000

    libxt: update 1.2.0 bottle.

--- Formula/libxt.rb
+++ Formula/libxt.rb
@@ -8,6 +8,7 @@ class Libxt < Formula
   bottle do
     cellar :any
     sha256 "a70e54d9374444cbc8e1ed592ccd24de3120854efb3248085db261411d318058" => :big_sur
+    sha256 "01c92a3915f414ec9880723e6c2928a33bea584ca5c1d1f38f67e001920ecded" => :arm64_big_sur
     sha256 "6b454a895e68fb652f1b54aaa4cdefa9282465d5bc4f6dbebd5fc499fea6a7d1" => :catalina
     sha256 "c4522922f276b72a5ba605b80ca188ae8808d5f5d4cb0de8f4d0639669bd6232" => :mojave
     sha256 "a68f79946b3061006f1b59c6b63f1dfc2f72565a15a05c6b91b1165946f952aa" => :high_sierra
