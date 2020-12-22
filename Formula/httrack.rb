947b39bc2248204453276360bdfef18d102b8eb4
FX Coudert <fxcoudert@gmail.com>
2020-12-22 22:31:03 +0000

    httrack: update 3.49.2_1 bottle.

--- Formula/httrack.rb
+++ Formula/httrack.rb
@@ -14,6 +14,7 @@ class Httrack < Formula
 
   bottle do
     sha256 "2f773ea2f9bdf0234abee17e9bd2003f21396fe1fdda756dd6e4faf7844f9c01" => :big_sur
+    sha256 "e33695d628a65bb1c6a5bb5f1147ea4560f8881482e60e10f0a8837527153609" => :arm64_big_sur
     sha256 "291ab06b376233166dd833422801d0a7be6f06cdabdc568656ec64ad3adc5fe8" => :catalina
     sha256 "6e0d2265e15d103a37b6b594f7f10c85af82012f1e3c1e25fc436e7430502b2c" => :mojave
     sha256 "612d8c3f9ee15fd7c4f42dbca3c5e3b58e968d626aa15f916f85c8cdb44ea31f" => :high_sierra
