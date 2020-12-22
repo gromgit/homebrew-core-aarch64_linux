de3aa48784ee3f2c7ae84c42225ed1a8715f0a2f
Caleb Xu <calebcenter@live.com>
2020-12-22 14:03:16 -0500

    mill 0.9.4 (#67420)

--- Formula/mill.rb
+++ Formula/mill.rb
@@ -1,8 +1,8 @@
 class Mill < Formula
   desc "Scala build tool"
   homepage "https://www.lihaoyi.com/mill/"
-  url "https://github.com/lihaoyi/mill/releases/download/0.9.3/0.9.3"
-  sha256 "e738a1548a668a3b40a767b286513b7e1867b6905ac5cb0c3fd1db5f2adb3575"
+  url "https://github.com/lihaoyi/mill/releases/download/0.9.4/0.9.4"
+  sha256 "99753e34e0b143a5c9d6d00ad534a5744916b9b9a7a8230cb54e53a4add7f6e5"
   license "MIT"
 
   bottle :unneeded
