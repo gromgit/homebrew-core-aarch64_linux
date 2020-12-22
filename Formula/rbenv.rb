515616ec59e06488a6f9bb0ad4502ac7566492b0
FX Coudert <fxcoudert@gmail.com>
2020-12-22 20:52:17 +0000

    rbenv: update 1.1.2 bottle.

--- Formula/rbenv.rb
+++ Formula/rbenv.rb
@@ -9,6 +9,7 @@ class Rbenv < Formula
   bottle do
     cellar :any
     sha256 "60b045c8843745c45d01616ee3f71b91f6a16ee09c47e23a7817a3edabeaccfd" => :big_sur
+    sha256 "19d0b9e3b11bcacdf692e78248c1b5764f7d3f4b58f2b83ad7b39562863ba82e" => :arm64_big_sur
     sha256 "503ed6d818502f00f031b9f49461934e252b9bfba2876e90a326fc27bb1052d6" => :catalina
     sha256 "d1019098dee8d037587069398e5ad04e6d736f834dc44ae73943bec46b10b260" => :mojave
     sha256 "b5984102794a9d39388ca1f6ec77965aeea29b971cc00cb5af8ede8ee6c926d6" => :high_sierra
