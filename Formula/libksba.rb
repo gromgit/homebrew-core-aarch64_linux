504c016f3dd0fc68cf7b989525e55905027042d2
FX Coudert <fxcoudert@gmail.com>
2020-12-22 16:28:20 +0000

    libksba: update 1.5.0 bottle.

--- Formula/libksba.rb
+++ Formula/libksba.rb
@@ -13,6 +13,7 @@ class Libksba < Formula
   bottle do
     cellar :any
     sha256 "688def210a738526e8cebfe8e556c9bfe4b164887c5918463c37d91b3f694945" => :big_sur
+    sha256 "077fb787593058667da21e25d5beca46ced441193c325c936036d466ad60cd6e" => :arm64_big_sur
     sha256 "9a305a28ce4d1635c142ea8e02ce5e6ef17007901223fbdbe3572f06398490f3" => :catalina
     sha256 "a309359bb9d744de1065e91feefdd06f520a6e72a8d266bc256d59fbe23b70d5" => :mojave
   end
