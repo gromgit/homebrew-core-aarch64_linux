761996d825ca1f973d09990d893c61be854e1b59
FX Coudert <fxcoudert@gmail.com>
2020-12-22 18:00:34 +0000

    dialog: update 1.3-20201126 bottle.

--- Formula/dialog.rb
+++ Formula/dialog.rb
@@ -13,6 +13,7 @@ class Dialog < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "eb3455238be98e27905b275e88723a0e0ccee0c959ca1124ba681728626e08b6" => :big_sur
+    sha256 "1123439be33e07dafe32f34271445d3e3a7d7d00aa21f4141ba8a7d88a5164da" => :arm64_big_sur
     sha256 "6fc24c87e6cf32e7702c0f9e64eba5ad79908b84bf1614cc62a99c967bb659ea" => :catalina
     sha256 "0bb780acdabb6d05e8e6465fa8f204758a27c39caf7fc93fa9e5da17dcfeac31" => :mojave
   end
