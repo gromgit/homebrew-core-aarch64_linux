a8a46b5d0be440b76733a7e423e105d940eebf35
FX Coudert <fxcoudert@gmail.com>
2020-12-22 16:08:21 +0000

    xvid: update 1.3.7 bottle.

--- Formula/xvid.rb
+++ Formula/xvid.rb
@@ -13,6 +13,7 @@ class Xvid < Formula
   bottle do
     cellar :any
     sha256 "feabfa1a3df3b916654ba5eef30193b65cdba70a7a49cca6406ec0c214b50338" => :big_sur
+    sha256 "8974d7b8f816f7d5e8d9ae967b94922e0ed212f22f6475b7fa4c80c7a95d6582" => :arm64_big_sur
     sha256 "ace5fea6272f3594b5c8fca6f1fe03c41c50a14af8599751571c5e44a49a5a53" => :catalina
     sha256 "4e119534a1351c85799944eb35f6f5675192e67e077fb3452f73f210a57eabe3" => :mojave
     sha256 "79ea46af3061561427ab0af36b09d61e057084c76f655ec21074fba375a36b01" => :high_sierra
