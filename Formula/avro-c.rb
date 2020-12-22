f04f0068cd7c090e4f566bac130ca9b9165a116f
FX Coudert <fxcoudert@gmail.com>
2020-12-22 22:00:32 +0000

    avro-c: update 1.10.1 bottle.

--- Formula/avro-c.rb
+++ Formula/avro-c.rb
@@ -12,6 +12,7 @@ class AvroC < Formula
 
   bottle do
     sha256 "c99913941625740c45fd76a1fa8a69750f585375c0559af04284d8bed2cab872" => :big_sur
+    sha256 "e4cd6b302cfadbcd1f445c8957fd616b5f2fdc6730ca64ab3c825cd81a9d8dc5" => :arm64_big_sur
     sha256 "2b10e0adb725faf050ecc7c3ffccec6f9a003714ccd76d544b0d644253d2626f" => :catalina
     sha256 "0fc00159098c04997d2537f79283517497f01a8100a4a3cba8ac09099844a80f" => :mojave
   end
