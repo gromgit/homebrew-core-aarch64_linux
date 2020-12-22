5299d2b2b9abca67fa957a7e450a12ca0ca7f54a
FX Coudert <fxcoudert@gmail.com>
2020-12-22 22:48:46 +0000

    supervisor: update 4.2.0_1 bottle.

--- Formula/supervisor.rb
+++ Formula/supervisor.rb
@@ -10,6 +10,7 @@ class Supervisor < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "8f9afc2ab25e747431c943912f26199416222ade9f884947cb42c04fa22d59d4" => :big_sur
+    sha256 "c6a1bcaebde48e24c227d972857208ab0a5a727ba738df6faf5e0ed167a326b3" => :arm64_big_sur
     sha256 "bd9a26f19bc257abded6138d2c0ae208252d6c780ae2daa85b7d093c47234eb1" => :catalina
     sha256 "484eeb4bd246381f6c629023f126515d960a564fd3de4788e3eace9085cf698d" => :mojave
     sha256 "cca1a2525646a0aaeb2bae9c5885bca7757737415bbfe54f12cb1b2367548bb3" => :high_sierra
