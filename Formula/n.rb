6ad05aa64315e8fb60d5b268a4ed7457850b38c3
FX Coudert <fxcoudert@gmail.com>
2020-12-22 22:04:35 +0000

    n: update 7.0.0 bottle.

--- Formula/n.rb
+++ Formula/n.rb
@@ -9,6 +9,7 @@ class N < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "6e5f5c5bda7d49a0cf2c5ec8ff93aa32a37ebae54be832f3ff0a2e79357733c7" => :big_sur
+    sha256 "8f2489b60bd94ca9c15f3c5334eee89b73150c02cb42ba0cd6ee9eb100314295" => :arm64_big_sur
     sha256 "02c2c46f1fbe589f28b5ff91ee5605fa951026894b118f977aa01bbd0f34b67d" => :catalina
     sha256 "7b973a504a2bdb3e39e63162d3ab40ee47dac5dd2a49eeb75a7cc2af95c0d38b" => :mojave
   end
