772a955f508dc6c8064894bc597da25ee53435cc
Michka Popoff <michkapopoff@gmail.com>
2020-12-22 23:25:09 +0100

    gnu-units: define caveats only on mac (#67464)

--- Formula/gnu-units.rb
+++ Formula/gnu-units.rb
@@ -39,12 +39,14 @@ class GnuUnits < Formula
   end
 
   def caveats
-    <<~EOS
-      All commands have been installed with the prefix "g".
-      If you need to use these commands with their normal names, you
-      can add a "gnubin" directory to your PATH from your bashrc like:
-        PATH="#{opt_libexec}/gnubin:$PATH"
-    EOS
+    on_macos do
+      <<~EOS
+        All commands have been installed with the prefix "g".
+        If you need to use these commands with their normal names, you
+        can add a "gnubin" directory to your PATH from your bashrc like:
+          PATH="#{opt_libexec}/gnubin:$PATH"
+      EOS
+    end
   end
 
   test do
