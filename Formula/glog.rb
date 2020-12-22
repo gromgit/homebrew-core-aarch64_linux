c89b88fdec4d04c17d9d93d3d5fbc7cdf54d3c14
FX Coudert <fxcoudert@gmail.com>
2020-12-22 17:05:53 +0000

    glog: update 0.4.0 bottle.

--- Formula/glog.rb
+++ Formula/glog.rb
@@ -9,6 +9,7 @@ class Glog < Formula
   bottle do
     cellar :any
     sha256 "ed282046831a2c49077ed7427a4690f2aa95ba80ae2b9902bac8e5bd47ca0d86" => :big_sur
+    sha256 "8f1417f8d9c708afd66bd4b9ef1b1b1308dcd85d02950b6284ff0f712f92c5e2" => :arm64_big_sur
     sha256 "918710bfd20d088718f33579216eb4574595f27ea234c409dd5848c0b8ad9e15" => :catalina
     sha256 "034a4d2272b48fd7655b467b92c78eebfb11efb33cc6cd31f7b13ee085b7169b" => :mojave
     sha256 "bbe6c4138b5fe8cd58d269a39644176f640fa62e694ffac36337f87661cacc69" => :high_sierra
