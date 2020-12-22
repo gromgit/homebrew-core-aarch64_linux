30a14e7ec588a69acddfb08c64b814b5ccce0f06
FX Coudert <fxcoudert@gmail.com>
2020-12-22 15:32:48 +0000

    hicolor-icon-theme: update 0.17 bottle.

--- Formula/hicolor-icon-theme.rb
+++ Formula/hicolor-icon-theme.rb
@@ -8,6 +8,7 @@ class HicolorIconTheme < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "f4cd50751f22d1aae6156ce3e552dbe0afb21ce1aaa5a7cc7ce284c867a20865" => :big_sur
+    sha256 "16e984b4dc9809fb223592f9b2c8a45cc49e796bd3b03f0b9507e80953167292" => :arm64_big_sur
     sha256 "8ba8d6065b652396583c55a0e73cff0007f96064a330ac20499ff1d887771eb8" => :catalina
     sha256 "5ba4bb6a7e89f5fb0d43504d68d657a536be9540d4cc72552bd5965e15a82b91" => :mojave
     sha256 "b33f58b98a6ca6bb72777eaf7b7a4bb393d5cc9ced6954dd7a7e52e18c214799" => :high_sierra
