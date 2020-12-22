fa5c151cfee9c768a9cd204f9e650b8c94113c4a
FX Coudert <fxcoudert@gmail.com>
2020-12-22 18:29:44 +0000

    serd: update 0.30.6 bottle.

--- Formula/serd.rb
+++ Formula/serd.rb
@@ -13,6 +13,7 @@ class Serd < Formula
   bottle do
     cellar :any
     sha256 "c53043a419c1409b8c1df9a3ab3b304525a69392933ffd421fa6fa3c22df4ab9" => :big_sur
+    sha256 "e5b28b3af541976f1bb6a3e5642ae7cc51ee82d0be992d28a0f533417f0afb69" => :arm64_big_sur
     sha256 "6ab4aae5b944f5fdcbd08d54b9fa55275e5a4cea89e1711b1288353ec75f6f4b" => :catalina
     sha256 "c579e8246255757af42717d1b93989c6422e8fba8459e5446963ab95bd78f383" => :mojave
   end
