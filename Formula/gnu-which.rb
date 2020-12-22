ce0e80a939059366bccdc29506a26da71694768d
Michka Popoff <michkapopoff@gmail.com>
2020-12-22 23:22:16 +0100

    gnu-which: define caveats only on mac (#67465)

--- Formula/gnu-which.rb
+++ Formula/gnu-which.rb
@@ -5,7 +5,7 @@ class GnuWhich < Formula
   url "https://ftp.gnu.org/gnu/which/which-2.21.tar.gz"
   mirror "https://ftpmirror.gnu.org/which/which-2.21.tar.gz"
   sha256 "f4a245b94124b377d8b49646bf421f9155d36aa7614b6ebf83705d3ffc76eaad"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
 
   livecheck do
     url :stable
@@ -42,13 +42,15 @@ class GnuWhich < Formula
   end
 
   def caveats
-    <<~EOS
-      GNU "which" has been installed as "gwhich".
-      If you need to use it as "which", you can add a "gnubin" directory
-      to your PATH from your bashrc like:
+    on_macos do
+      <<~EOS
+        GNU "which" has been installed as "gwhich".
+        If you need to use it as "which", you can add a "gnubin" directory
+        to your PATH from your bashrc like:
 
-          PATH="#{opt_libexec}/gnubin:$PATH"
-    EOS
+            PATH="#{opt_libexec}/gnubin:$PATH"
+      EOS
+    end
   end
 
   test do
