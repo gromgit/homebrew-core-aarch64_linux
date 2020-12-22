cf3ea9bf2fa45e3c3083bfdf4c31c2bfd0d0a47f
FX Coudert <fxcoudert@gmail.com>
2020-12-22 18:07:38 +0000

    serverless: update 2.16.0 bottle.

--- Formula/serverless.rb
+++ Formula/serverless.rb
@@ -9,6 +9,7 @@ class Serverless < Formula
 
   bottle do
     sha256 "9e18d77d6a4c4caec88e7d653bd2db3cb507190306a5bce749cc1bff0688bdbb" => :big_sur
+    sha256 "8c042949db8320af57e129dc00820461e7e0de67c0fe2330574957922234d72b" => :arm64_big_sur
     sha256 "3adbd8f8fa8259ffa734f59779b3b3223c5c72f313326206b19f80eafedc182f" => :catalina
     sha256 "4b58ca7e8d18a2b71bf9788e4d89215383e6f777815acfbe377b4806cefe805c" => :mojave
   end
