806d09fc118391bee7729b829c1cd122b0dbcb68
FX Coudert <fxcoudert@gmail.com>
2020-12-22 20:55:33 +0000

    lv2: update 1.18.0 bottle.

--- Formula/lv2.rb
+++ Formula/lv2.rb
@@ -12,6 +12,7 @@ class Lv2 < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "593bfbd7208eb8cc5ea57318af0fee430cdc0e972413fad746a943f2ae6dd7df" => :big_sur
+    sha256 "0a16890b6aa8ba1ac4088c23b85f7fc769a1db21feda2cbc8925a93652e413e5" => :arm64_big_sur
     sha256 "6cafb26479b24f5b6746557359b665d03bc42dd47ee7acea5a9c0b742c23936e" => :catalina
     sha256 "6cafb26479b24f5b6746557359b665d03bc42dd47ee7acea5a9c0b742c23936e" => :mojave
     sha256 "6cafb26479b24f5b6746557359b665d03bc42dd47ee7acea5a9c0b742c23936e" => :high_sierra
