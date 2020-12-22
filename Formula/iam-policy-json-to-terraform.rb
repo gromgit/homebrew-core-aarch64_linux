f44bc1519052a82b0888eae0179e5c4b9f8b3992
Florian Sellmayr <florian.sellmayr@gmail.com>
2020-12-22 08:53:18 +0700

    iam-policy-json-to-terraform 1.6.0
    Closes #67398.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/iam-policy-json-to-terraform.rb
+++ Formula/iam-policy-json-to-terraform.rb
@@ -1,8 +1,8 @@
 class IamPolicyJsonToTerraform < Formula
   desc "Convert a JSON IAM Policy into terraform"
   homepage "https://github.com/flosell/iam-policy-json-to-terraform"
-  url "https://github.com/flosell/iam-policy-json-to-terraform/archive/1.5.0.tar.gz"
-  sha256 "9d58642d3f532c4334dc63f45e44ff9cae254360f977bca925f20033338fadcb"
+  url "https://github.com/flosell/iam-policy-json-to-terraform/archive/1.6.0.tar.gz"
+  sha256 "714b8aead9bf5a88989a62eb520163565c890f37ee13783a3ae549bb0b8cdead"
   license "Apache-2.0"
   head "https://github.com/flosell/iam-policy-json-to-terraform.git"
 
