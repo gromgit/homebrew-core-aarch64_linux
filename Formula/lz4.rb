bf8d8db9db7231f0c62f22f95c43e4d96a44a441
FX Coudert <fxcoudert@gmail.com>
2020-12-22 15:51:47 +0000

    lz4: update 1.9.3 bottle.

--- Formula/lz4.rb
+++ Formula/lz4.rb
@@ -14,6 +14,7 @@ class Lz4 < Formula
   bottle do
     cellar :any
     sha256 "7024d0b6ee857352cbd3138f752496b87fa27252adbc6daefa4a6c64d3e347e5" => :big_sur
+    sha256 "1d915415cff308983a50f873db4f0de6acab7b57d65a93ec127c06c824ca0269" => :arm64_big_sur
     sha256 "899aeb12833a982e06013a60aa9b1ee69e3f77f969a5aa2dcec02ad329f369bb" => :catalina
     sha256 "e6adc6da46164495cf129c9e54bd69c6620eb4622a38e403edf1b5f488d044a8" => :mojave
     sha256 "46e99b27c33fd51a4394850be3559ea7b69fc26060ab2095dae315be14aa5e94" => :high_sierra
