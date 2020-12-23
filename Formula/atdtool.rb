b71f3d5a8bd5a2960d5843c3ac3b4a502c7bd940
FX Coudert <fxcoudert@gmail.com>
2020-12-23 22:27:02 +0000

    atdtool: update 1.3.3_4 bottle.

--- Formula/atdtool.rb
+++ Formula/atdtool.rb
@@ -16,6 +16,7 @@ class Atdtool < Formula
     cellar :any_skip_relocation
     rebuild 1
     sha256 "ed0bcc8c78addd4e072c55d8476e8ed4d5d5b4942df5c48b359c483a6dc2949f" => :big_sur
+    sha256 "e63fb91618eec0e6af69227020acdd1e9b12fedd834fa68dabc34168ec5f4dfe" => :arm64_big_sur
     sha256 "df0ff285c54b4368cb9e6731a025551c7e73f76a61f38b1e03cf86d8768735fb" => :catalina
     sha256 "267d97304c449f94707c4fac451331d1c9e38e07b774cc4fd78043a0bc94c197" => :mojave
   end
