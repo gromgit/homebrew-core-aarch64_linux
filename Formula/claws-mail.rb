16d7e058f0d4a586a1d62b779febd7f957915281
FX Coudert <fxcoudert@gmail.com>
2020-12-23 23:01:09 +0000

    claws-mail: update 3.17.8_1 bottle.

--- Formula/claws-mail.rb
+++ Formula/claws-mail.rb
@@ -8,6 +8,7 @@ class ClawsMail < Formula
 
   bottle do
     sha256 "cc9a93ec8f3322edcd9c9be0a3c9b059d130008115e19276533675432cae1c67" => :big_sur
+    sha256 "bbd1c67af463fb0645306523ebe20d37833aa8e3d38c042e8ce60d378c53d1d0" => :arm64_big_sur
     sha256 "5a559644851d9b007a6a500ae15558c5193c6e333cbe6c04f0294d03928d789e" => :catalina
     sha256 "f94aacf80c700939dbd023899421a2bd961d78ce3a9f958cc9156234ba92b150" => :mojave
   end
