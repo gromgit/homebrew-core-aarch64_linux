fc5db90cb98d9c813e9e7172ef3f62088acfa9f2
FX Coudert <fxcoudert@gmail.com>
2020-12-23 20:56:09 +0000

    highlight: update 3.60 bottle.

--- Formula/highlight.rb
+++ Formula/highlight.rb
@@ -8,6 +8,7 @@ class Highlight < Formula
 
   bottle do
     sha256 "83dffbd090b52dd14631f638d1efeb9af97787edee3845b81d366733580935ff" => :big_sur
+    sha256 "e96e79486cab16ec84802064017f5e97d0f2e3e321e82b172299aff78dd4b82d" => :arm64_big_sur
     sha256 "f7e4a91b1e26ff28387c0aa4f581a9d8a8973cca4608b04d02b44876cc734219" => :catalina
     sha256 "9add800d3b6e9fba4b5c1dc223b987a7c600eef0c75511b80102f939cca1a659" => :mojave
   end
