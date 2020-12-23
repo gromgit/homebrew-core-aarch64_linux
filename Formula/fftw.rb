4e799308d7f0e2aef48f72df501f21a65686b981
FX Coudert <fxcoudert@gmail.com>
2020-12-23 18:40:04 +0100

    fftw: fix build on ARM (#67528)

--- Formula/fftw.rb
+++ Formula/fftw.rb
@@ -35,7 +35,8 @@ class Fftw < Formula
 
     # FFTW supports runtime detection of CPU capabilities, so it is safe to
     # use with --enable-avx and the code will still run on all CPUs
-    simd_args = ["--enable-sse2", "--enable-avx"]
+    simd_args = []
+    simd_args << "--enable-sse2" << "--enable-avx" if Hardware::CPU.intel?
 
     # single precision
     # enable-sse2, enable-avx and enable-avx2 work for both single and double precision
