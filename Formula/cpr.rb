8cd53e5b0000340a23af63f8d4c8834be25a1ac9
FX Coudert <fxcoudert@gmail.com>
2020-12-23 22:10:28 +0000

    cpr: update 1.5.2 bottle.

--- Formula/cpr.rb
+++ Formula/cpr.rb
@@ -10,6 +10,7 @@ class Cpr < Formula
   bottle do
     cellar :any
     sha256 "0f3457ec4a948fb235d26d9bfdd0c1b3f53297c0e7c505a1f34a3d853907ddc8" => :big_sur
+    sha256 "b93aecd08cd2c187694a572c2c9c8d4cdb0ed181b4387fd3e7a68f67a4847d6a" => :arm64_big_sur
     sha256 "51bbf276165a820d37e9d9dfc829e7dae6f100b57bbb4095283955924027a7e8" => :catalina
     sha256 "66cfe69826f724c686417117ba2ef710e7765a35c39b648d6d239867f6c47473" => :mojave
   end
