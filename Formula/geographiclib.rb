dbc133021a9beba49cd3a0b85fd2dd3089c110d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-23 10:27:31 +0000

    geographiclib: update 1.51 bottle.

--- Formula/geographiclib.rb
+++ Formula/geographiclib.rb
@@ -11,9 +11,10 @@ class Geographiclib < Formula
 
   bottle do
     cellar :any
-    sha256 "e07ad5fe631f3877b528d10857055e2c8c3d25ba0405dddf6c3522cddd9b2364" => :big_sur
-    sha256 "8a525a6c0f80a6081791a40892e64a71f1f9bf819097c78ce4fa94b2f1a4e170" => :catalina
-    sha256 "fb92606cfaa6ec7cf519bed8089886679ade7b98be2609c0c46b783a825bac18" => :mojave
+    rebuild 1
+    sha256 "330b858cbb533864caa4324b2385399e49807ce93671be290ec6b16c09c874d6" => :big_sur
+    sha256 "a8d601014c3a569b8b0ebb48e0f92055c57c2f915354a1e4030a7cda4a1afcfe" => :catalina
+    sha256 "145e83f5076511c2d86ec4d8ff4de7536f909411d70b8ba81fa976f7c756d11d" => :mojave
   end
 
   depends_on "cmake" => :build
