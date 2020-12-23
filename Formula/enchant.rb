2bfc12170796bfdf9c9a1c084f054a9d352191a4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-23 06:24:31 +0000

    enchant: update 2.2.15 bottle.

--- Formula/enchant.rb
+++ Formula/enchant.rb
@@ -6,10 +6,9 @@ class Enchant < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 "f488f13da4f447f7d1d2d4bce0f70673dc7372b106e107961c503353a53a931b" => :big_sur
-    sha256 "d905b3a11a2a10170f028b928a185a3aeafa4404402a443b666989a9b563b7cc" => :arm64_big_sur
-    sha256 "c9bc06ac40094452e842ba22550d11e101dff6ee8f9076eea9830b417e6609c1" => :catalina
-    sha256 "81bbc67b4a2320177e696285759015cd7c263fedfdb1b9cf9b99735c9e2fdaf1" => :mojave
+    sha256 "fa2be4ac2134abd0e26253fec654924b0331abe62588a48840e3a467ec4c20c2" => :big_sur
+    sha256 "822040f225a771c940f4595a3b7e7c592c6a794828bbc34bce8b8f3143c70443" => :catalina
+    sha256 "cb7f88089f5fb746127ad384c7f3cd813f16b1ff9b7b263872bc75b1a9293916" => :mojave
   end
 
   depends_on "pkg-config" => :build
