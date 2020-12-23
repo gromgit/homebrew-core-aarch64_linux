1cb25c2d30cc061d9560e7d34c958adc210f9576
FX Coudert <fxcoudert@gmail.com>
2020-12-23 17:59:00 +0000

    gpgme: update 1.15.0 bottle.

--- Formula/gpgme.rb
+++ Formula/gpgme.rb
@@ -13,6 +13,7 @@ class Gpgme < Formula
   bottle do
     cellar :any
     sha256 "6de40ec526a4d87f6caba431dc5cb8c48ac490cc31d76f64b9e3f451c27f3ea2" => :big_sur
+    sha256 "15e4e53d82f19a390ccf802bf9ebc5310814eef0ee482701a19f2da66737a9c7" => :arm64_big_sur
     sha256 "804b505d6702fb22c25c93a7832648a23aefc877c4d660ce5c7a9026c9442bc7" => :catalina
     sha256 "99d0cc2cb9dcd2b7d38bb48627f3b740d483d7f9e64d6fd6690941ecb5731f2b" => :mojave
     sha256 "02bd3a9334b19890b043256a406bce8c61083fd56a71e4966161de86fd6aa4d8" => :high_sierra
