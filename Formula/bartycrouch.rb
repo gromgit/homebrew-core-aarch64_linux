ac038692433449e658c4eb46dd6d3a41c45f3c16
FX Coudert <fxcoudert@gmail.com>
2020-12-23 23:18:43 +0000

    bartycrouch: update 4.3.1 bottle.

--- Formula/bartycrouch.rb
+++ Formula/bartycrouch.rb
@@ -10,6 +10,7 @@ class Bartycrouch < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "a9b173c9de755e9c45234e503c062748786d7f984749354a1ac4450ec37df6aa" => :big_sur
+    sha256 "470964f2e3f481633f981acb9a32aec8fdfd51b4616f587b1a05319b5b4155b3" => :arm64_big_sur
     sha256 "aa2777a08e4d1a12340638678c4652b4ab5d5aa6f7db2917e7c907550d524119" => :catalina
   end
 
