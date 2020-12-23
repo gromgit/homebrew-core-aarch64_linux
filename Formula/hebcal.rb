e3c60d5998aa053f98d6e36d9b5174a4774a4e97
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-23 20:22:00 +0000

    hebcal: update 4.23 bottle.

--- Formula/hebcal.rb
+++ Formula/hebcal.rb
@@ -7,10 +7,9 @@ class Hebcal < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f6e790acc45c8c23ef75170752861cef1c2797759ea0221422f3bdaba05298b0" => :big_sur
-    sha256 "d6bcc551ab5a7f4da895483e648186db77ed80a8d6f446a79bc62fa0e9bba8ab" => :catalina
-    sha256 "be4e928b3b2e02e80797bc2526625b91f368b234d1e8f8e1801eb9bd24be86da" => :mojave
-    sha256 "1b6d05812ade070433b47ebfb332db7859c23eb60636dcc70f65cca6ad04fa04" => :high_sierra
+    sha256 "c3bd7989cdea11ebf9b0f23db74423f9437e8da3cd0422b79cded311599732a3" => :big_sur
+    sha256 "d7a80ab99402feb40acf3d4fdf58f363af5cd5e36c8bdf168cea5578ae6ae53b" => :catalina
+    sha256 "3370096c6542b25553fd3ad19f4eb5f1b4e4c9911b214979d37af97fe93ffb0f" => :mojave
   end
 
   depends_on "autoconf" => :build
