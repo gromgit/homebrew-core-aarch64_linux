302570c54108b4592c5bf5c46468cfb49ad81f0f
FX Coudert <fxcoudert@gmail.com>
2020-12-23 20:47:09 +0000

    ceres-solver: update 1.14.0_14 bottle.

--- Formula/ceres-solver.rb
+++ Formula/ceres-solver.rb
@@ -10,6 +10,7 @@ class CeresSolver < Formula
     cellar :any
     rebuild 1
     sha256 "b361ff56ef92ad9f331eb3555675c41dcda52bb1f00d0918c09a182207689cf4" => :big_sur
+    sha256 "73ed3e90a8e53fc6f26826138849ca6083e08a904e693efecc44963f182a5e0d" => :arm64_big_sur
     sha256 "e54c5be54b9a2e8d0af4860148d6e7d7c44edca7ce1c397b54a776c1730f03cd" => :catalina
     sha256 "0a091d6adf630d059340d1c4e69836fc9ecbbac804b23f855095ad9b0473a6b0" => :mojave
   end
