8485379abccdf7aa6410bea8e3c3cf2bd98ef7c8
FX Coudert <fxcoudert@gmail.com>
2020-12-23 22:22:24 +0000

    libiptcdata: update 1.0.4_1 bottle.

--- Formula/libiptcdata.rb
+++ Formula/libiptcdata.rb
@@ -11,6 +11,7 @@ class Libiptcdata < Formula
 
   bottle do
     sha256 "5bb2bce1d8a877c84abb51f3b9d9e0c40588bdeb2d6ea8d66c6de230d2e35e8d" => :big_sur
+    sha256 "45d61d51cb3e5607763ed374d5cc88e4a7c6dc8b1ba08ccd276c3379a20646bf" => :arm64_big_sur
     sha256 "1dbcf1dd89b05f7f1fdc1a15d9c56b7e726f7296d8096ccae22fed9adf36790a" => :catalina
     sha256 "78dc7bb6b1e5bcccc1c0c9ef158b8d423f782aa455b1b10c3eebb29de6e7fa58" => :mojave
     sha256 "62f4a032075fbf0b9a43ef474b784bae7c47d503483bdc2e09e851c5568345e3" => :high_sierra
