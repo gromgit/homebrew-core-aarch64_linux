13d25a0acf266cea3a6e1bce03db4351dd1780f7
Rui Chen <rui@meetup.com>
2020-12-23 17:46:11 -0500

    mpg123 1.26.4

--- Formula/mpg123.rb
+++ Formula/mpg123.rb
@@ -1,9 +1,9 @@
 class Mpg123 < Formula
   desc "MP3 player for Linux and UNIX"
   homepage "https://www.mpg123.de/"
-  url "https://www.mpg123.de/download/mpg123-1.26.3.tar.bz2"
-  mirror "https://downloads.sourceforge.net/project/mpg123/mpg123/1.26.3/mpg123-1.26.3.tar.bz2"
-  sha256 "30c998785a898f2846deefc4d17d6e4683a5a550b7eacf6ea506e30a7a736c6e"
+  url "https://www.mpg123.de/download/mpg123-1.26.4.tar.bz2"
+  mirror "https://downloads.sourceforge.net/project/mpg123/mpg123/1.26.4/mpg123-1.26.4.tar.bz2"
+  sha256 "081991540df7a666b29049ad870f293cfa28863b36488ab4d58ceaa7b5846454"
 
   livecheck do
     url "https://www.mpg123.de/download/"
