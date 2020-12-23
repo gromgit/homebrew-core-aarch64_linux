60da898f21817daef8f3c49ce7b8161b4951e9e1
FX Coudert <fxcoudert@gmail.com>
2020-12-23 22:25:54 +0000

    triton: update 7.12.0 bottle.

--- Formula/triton.rb
+++ Formula/triton.rb
@@ -14,6 +14,7 @@ class Triton < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "9eff2740d1ee43579704b15f6cc0ec3ded59d8f2c32991aef2e7dd4243081df1" => :big_sur
+    sha256 "0d26455a277496abe72df7663120b893aecdcde8acb71a210dc85a5944707191" => :arm64_big_sur
     sha256 "0e80b4d4276d3e695321ce821101192f3b0becd70d7f4b90e31092cd2340cbb3" => :catalina
     sha256 "690b346b720a50ee036fe590e5b508f1d4cab865f918bffb9ad0a5eb15f142c1" => :mojave
   end
