49a1512f5b95e6399c0c7f0b6995a3c6ca571f32
Mitchell Blank Jr <mitch@bodyfour.com>
2020-12-23 04:00:02 +0000

    p0f: fix compile on Xcode 12
    I have sent this patch by email to the upstream maintainer as well.
    
    Closes #67506.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/p0f.rb
+++ Formula/p0f.rb
@@ -3,6 +3,7 @@ class P0f < Formula
   homepage "https://lcamtuf.coredump.cx/p0f3/"
   url "https://lcamtuf.coredump.cx/p0f3/releases/p0f-3.09b.tgz"
   sha256 "543b68638e739be5c3e818c3958c3b124ac0ccb8be62ba274b4241dbdec00e7f"
+  license "LGPL-2.1-only"
 
   bottle do
     rebuild 1
@@ -12,6 +13,11 @@ class P0f < Formula
     sha256 "bd25792c98fd8c88599ab373ed8b9265fe4b69c47b6b3ebc84911750f48f190d" => :sierra
     sha256 "37aea629cea6430b8516ea80eaaf687844a2a1656eebe7744ba6f3746381ce48" => :el_capitan
   end
+
+  uses_from_macos "libpcap"
+
+  # Fix Xcode 12 issues with "-Werror,-Wimplicit-function-declaration"
+  patch :DATA
 
   def install
     inreplace "config.h", "p0f.fp", "#{etc}/p0f/p0f.fp"
@@ -24,3 +30,25 @@ class P0f < Formula
     system "#{sbin}/p0f", "-r", test_fixtures("test.pcap")
   end
 end
+
+__END__
+--- p0f-3.09b/build.sh.ORIG	2020-12-23 03:36:51.000000000 +0000
++++ p0f-3.09b/build.sh	2020-12-23 03:41:54.000000000 +0000
+@@ -174,7 +174,7 @@
+ 
+ echo "OK"
+ 
+-echo -n "[*] Checking for *modern* GCC... "
++echo -n "[*] Checking if $CC supports -Wl,-z,relro -pie ... "
+ 
+ rm -f "$TMP" "$TMP.c" "$TMP.log" || exit 1
+ 
+@@ -197,7 +197,7 @@
+ 
+ rm -f "$TMP" "$TMP.c" "$TMP.log" || exit 1
+ 
+-echo -e "#include \"types.h\"\nvolatile u8 tmp[6]; int main() { printf(\"%d\x5cn\", *(u32*)(tmp+1)); return 0; }" >"$TMP.c" || exit 1
++echo -e "#include <stdio.h>\n#include \"types.h\"\nvolatile u8 tmp[6]; int main() { printf(\"%d\x5cn\", *(u32*)(tmp+1)); return 0; }" >"$TMP.c" || exit 1
+ $CC $USE_CFLAGS $USE_LDFLAGS "$TMP.c" -o "$TMP" &>"$TMP.log"
+ 
+ if [ ! -x "$TMP" ]; then
