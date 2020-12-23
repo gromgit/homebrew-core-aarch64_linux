5a8533d098cebd3245479833305be0759466dde4
FX Coudert <fxcoudert@gmail.com>
2020-12-23 22:10:54 +0000

    e2tools: update 0.1.0 bottle.

--- Formula/e2tools.rb
+++ Formula/e2tools.rb
@@ -8,6 +8,7 @@ class E2tools < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "38978d9bea279b06c646bc82631d59edee4a519164f4b0a4f39b33621ae1183b" => :big_sur
+    sha256 "e4c94d559938d504bc7ddc19139c8af004385b57db58bfd24946c451b4752928" => :arm64_big_sur
     sha256 "93eab5f2d207ac8f27a9b27db13408b4b7f8a3cfee4ecbca9d9977a851a41576" => :catalina
     sha256 "1ad81d83b87fc67a54698e6af829dd0945119a41a445383268f1d0190ff7b38d" => :mojave
     sha256 "069988a622ce0587927a4a50b70b778b461840d2db2e49259e1123123bf6a2ff" => :high_sierra
