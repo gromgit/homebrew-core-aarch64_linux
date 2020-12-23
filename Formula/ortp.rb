00d02790df0d7c59b6ae04db71d31ba3fc2d754a
FX Coudert <fxcoudert@gmail.com>
2020-12-23 22:15:28 +0000

    ortp: update 4.4.13_1 bottle.

--- Formula/ortp.rb
+++ Formula/ortp.rb
@@ -9,6 +9,7 @@ class Ortp < Formula
 
   bottle do
     sha256 "4007e619fdf9b62f48dc6897309d600bb7bcc927d94c62d53f9438e1c946dc8e" => :big_sur
+    sha256 "5ab4dc28a31243ab59862f74f5edc4d615115b539532b284ccd17a3bb8f8bbf1" => :arm64_big_sur
     sha256 "5caba650034ac5418a2dfe1d5cb26e667af555dcba655a765ed7a0e21bbdd6ba" => :catalina
     sha256 "1037969f3d5869cdde0f1ca6f3d1f8f7d3cb01c544fbcf14f913f96f29f8cf29" => :mojave
   end
