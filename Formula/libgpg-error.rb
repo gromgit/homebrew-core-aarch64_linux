c3dd429f1356b1780095f2c9e6d9161812b965e5
FX Coudert <fxcoudert@gmail.com>
2020-12-23 17:15:30 +0000

    libgpg-error: update 1.41 bottle.

--- Formula/libgpg-error.rb
+++ Formula/libgpg-error.rb
@@ -12,6 +12,7 @@ class LibgpgError < Formula
 
   bottle do
     sha256 "bc5c5321fa98d6f4dbab215f0e90d9babdcc9c9c801c7ebea9a4423364a13cb7" => :big_sur
+    sha256 "089a2faf07b8eb31c995e7dab97b9e8d5d2fdc77dac8d239ffb391ca4eaf6761" => :arm64_big_sur
     sha256 "33969246a46d4cb42c2401c64038cc48601e5fcd41ae21c14d9775c4955fd825" => :catalina
     sha256 "9efc83a76395081c43b5619b4cca8deb794187a7e54cf28e3e7f106aad5093ae" => :mojave
   end
