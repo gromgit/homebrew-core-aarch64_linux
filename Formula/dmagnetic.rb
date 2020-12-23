8177142f8a323c4e5df2c4afcb9cc759f03273d1
FX Coudert <fxcoudert@gmail.com>
2020-12-23 22:34:45 +0000

    dmagnetic: update 0.28 bottle.

--- Formula/dmagnetic.rb
+++ Formula/dmagnetic.rb
@@ -8,6 +8,7 @@ class Dmagnetic < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "4d6970e081501b287f27119eeab1d5afdebacfdeb416d4f3e920cd3ad86e1e09" => :big_sur
+    sha256 "cf6672c79d6bb083382d7d5090c7b8f38ac3fad9cc955e47b7d441f3883d1fcd" => :arm64_big_sur
     sha256 "f3198b2f700c0265ea488cb003bd0eebb17ef2438627a7d4afdc1317186c7d75" => :catalina
     sha256 "164d9398a64684d8a81540ace3a022a178ef0802d62ee18372900130445c1104" => :mojave
   end
