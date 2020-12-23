18db3594405a51c88755883bdc81139dc88f3008
FX Coudert <fxcoudert@gmail.com>
2020-12-23 18:10:45 +0000

    faust: update 2.27.2 bottle.

--- Formula/faust.rb
+++ Formula/faust.rb
@@ -8,6 +8,7 @@ class Faust < Formula
   bottle do
     cellar :any
     sha256 "6bca64d09f6bc052c71aaa01b1345294dab92dc30e7e02ec2775cf332d6eb310" => :big_sur
+    sha256 "20e2b5c9da498fa087525af5152b1dae659a0469e7aafaa17272a000a8aa7247" => :arm64_big_sur
     sha256 "3e17a2b3203fdf2851afde34281373fb2d8fe908e224f380bb1692aac8ee7564" => :catalina
     sha256 "96f641dec558778e6e652bea11d4b446bcc7c61bebec5ec314862479b22989d7" => :mojave
   end
