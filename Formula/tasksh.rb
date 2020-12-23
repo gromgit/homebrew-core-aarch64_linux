c38fc83efaf3e3df5c61719a94480e620c9d51c5
Dustin Rodrigues <dust.rod@gmail.com>
2020-12-23 06:54:47 -0500

    tasksh: revision bump
    Closes #67519.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tasksh.rb
+++ Formula/tasksh.rb
@@ -4,6 +4,7 @@ class Tasksh < Formula
   url "https://taskwarrior.org/download/tasksh-1.2.0.tar.gz"
   sha256 "6e42f949bfd7fbdde4870af0e7b923114cc96c4344f82d9d924e984629e21ffd"
   license "MIT"
+  revision 1
   head "https://github.com/GothenburgBitFactory/taskshell.git", branch: "1.3.0"
 
   # We check the upstream Git repository tags because the first-party download
