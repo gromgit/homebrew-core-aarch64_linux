9e1bd11157eee5fc621769a73f3d6155a0c9058c
FX Coudert <fxcoudert@gmail.com>
2020-12-23 23:28:57 +0000

    xmrig: update 6.7.0 bottle.

--- Formula/xmrig.rb
+++ Formula/xmrig.rb
@@ -13,6 +13,7 @@ class Xmrig < Formula
 
   bottle do
     sha256 "b074058717cf40e67d21be40d6c36c548dbbf724a9e9f9be6670e192fdd8254b" => :big_sur
+    sha256 "e4856c6690f953cfd0c2fa11eea309822edd404559ce414107be5d6a94acef54" => :arm64_big_sur
     sha256 "384c50fd560bfa34e51ef02a1e0d232d635daccd75cc81754379aa8a902fa70b" => :catalina
     sha256 "1bcc099dc797311394ab8bd71d6be29110f76c631883762169cf2d50e1988e14" => :mojave
   end
