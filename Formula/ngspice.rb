c95460ec3d7f621179d624bac7bb6beb6d300904
FX Coudert <fxcoudert@gmail.com>
2020-12-23 23:28:56 +0000

    ngspice: update 33 bottle.

--- Formula/ngspice.rb
+++ Formula/ngspice.rb
@@ -11,6 +11,7 @@ class Ngspice < Formula
 
   bottle do
     sha256 "b00fa22144c9add0b27a0c169e51877fb680ae309f0b339043898cb7c1be8a22" => :big_sur
+    sha256 "1e9280810ac216eefca4dbff61d8ed764feca6c9c4103195f3894eae0918b25e" => :arm64_big_sur
     sha256 "947aa81277b9bfdfe5b9a52ba502f146cb2c7e5125e88770255a40f6799685bd" => :catalina
     sha256 "0d880665db391a48e55b133b6fea6cbf7f0e56c0428045b4d046b03dfff95685" => :mojave
     sha256 "9f048cc3f136b84cfd2f76afe91f39d7bf844c00e442a9fbb7363f84fa44a8c7" => :high_sierra
