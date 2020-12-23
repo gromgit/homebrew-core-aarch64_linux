11cfe8cde350c2cf1aec54841c8c0c434e773272
FX Coudert <fxcoudert@gmail.com>
2020-12-23 23:49:33 +0000

    gst-rtsp-server: update 1.18.2 bottle.

--- Formula/gst-rtsp-server.rb
+++ Formula/gst-rtsp-server.rb
@@ -13,6 +13,7 @@ class GstRtspServer < Formula
   bottle do
     cellar :any
     sha256 "b4317689acd1ab2242fbf79485c6e41d3f7b6022f655cc5a1c0051a0e83fa58f" => :big_sur
+    sha256 "ad3491746d607e87f9d3c413f993e95deea5f9cb1838fbd3155a241a8ec78886" => :arm64_big_sur
     sha256 "6de0097ebaf534fd685c904cea67866f038bfecf37c81f18b002746194d3a73c" => :catalina
     sha256 "e02cd4c0dcb586239614eb6d7775956933a40661dc9d3863e6c11809463a6085" => :mojave
   end
