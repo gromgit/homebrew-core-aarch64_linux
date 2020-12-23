885e3c25b301c9c02399fd0e584563cc5939ada7
FX Coudert <fxcoudert@gmail.com>
2020-12-23 18:39:20 +0000

    recode: update 3.7.8 bottle.

--- Formula/recode.rb
+++ Formula/recode.rb
@@ -8,6 +8,7 @@ class Recode < Formula
   bottle do
     cellar :any
     sha256 "6d53af7b188693ffa022df92beea56d3963482c8206d1608e25d47ca5bd88848" => :big_sur
+    sha256 "c116a7d29975a1649b79f96067de047149050454d11fb09146066e617fadd13f" => :arm64_big_sur
     sha256 "367ab01690803d0270de4734faf1ef5175c2e3df7528b9f64bdcc0c1a78f1668" => :catalina
     sha256 "34d0491040e447e2a0cbe304d021ea82be00040a7c5533ac82e43907771636b1" => :mojave
   end
