1654ea4506237f01bf4375e3468fc785c542c680
FX Coudert <fxcoudert@gmail.com>
2020-12-23 20:51:28 +0000

    fig2dev: update 3.2.7b bottle.

--- Formula/fig2dev.rb
+++ Formula/fig2dev.rb
@@ -11,6 +11,7 @@ class Fig2dev < Formula
 
   bottle do
     sha256 "6d52c679f208194d6128a64fd9b14db2dd49a2ff1600e925500feb8e248cb0b5" => :big_sur
+    sha256 "19a51590db79ab018f309a3d5a2c2390b5c670fc54145a5256d6b26533377b0e" => :arm64_big_sur
     sha256 "0ffe4d06ce3f489b724facdae111d0358ac5a902733f5c2ccedab8f4f3759893" => :catalina
     sha256 "f640a65192bbae6f8801e6f07d57ff8d24ffda78ea471dacaa1b33684a7858ae" => :mojave
     sha256 "1020f0333374fbfcb88d3bf2b2ca89b40c83f7863968f4d28dad1654ff9905df" => :high_sierra
