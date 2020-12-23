599086d8b7a0eaf49d8a9c2033929ec6aa477d28
Seeker <meaningseeking@protonmail.com>
2020-12-23 01:50:50 -0800

    libva (new formula) (#67385)

---
+++ Formula/libva.rb
@@ -0,0 +1,50 @@
+class Libva < Formula
+  desc "Hardware accelerated video processing library"
+  homepage "https://github.com/intel/libva"
+  url "https://github.com/intel/libva/releases/download/2.6.1/libva-2.6.1.tar.bz2"
+  sha256 "6c57eb642d828af2411aa38f55dc10111e8c98976dbab8fd62e48629401eaea5"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "libdrm"
+  depends_on "libx11"
+  depends_on "libxext"
+  depends_on "libxfixes"
+  depends_on :linux
+  depends_on "wayland"
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}",
+                          "--localstatedir=#{var}",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--enable-drm",
+                          "--enable-x11",
+                          "--disable-glx",
+                          "--enable-wayland"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    %w[libva libva-drm libva-wayland libva-x11].each do |name|
+      assert_match "-I#{include}", shell_output("pkg-config --cflags #{name}")
+    end
+    (testpath/"test.c").write <<~EOS
+      #include <va/va.h>
+      int main(int argc, char *argv[]) {
+        VADisplay display;
+        vaDisplayIsValid(display);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-o", "test", "-I#{include}", "-L#{lib}", "-lva"
+    system "./test"
+  end
+end
