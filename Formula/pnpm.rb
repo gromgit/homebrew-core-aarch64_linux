84ca4d919c53cfe48c22b6a362eb5e381180e459
FX Coudert <fxcoudert@gmail.com>
2020-12-23 23:44:19 +0000

    pnpm: update 5.13.7 bottle.

--- Formula/pnpm.rb
+++ Formula/pnpm.rb
@@ -14,6 +14,7 @@ class Pnpm < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "bc604bb872545e8e2448989f05c6d3afcaa25408a68cfa946ffafe698c1faedb" => :big_sur
+    sha256 "b81309dd2863a5f8b6f4b328acfd22a873e46bcb6b2ad1f5558d096076455c6b" => :arm64_big_sur
     sha256 "98ed94d512a13af20ae2ed3236ce7250ac3f336ccc55b77eda7589ebf5af02c6" => :catalina
     sha256 "1d70cb1c63cc69cbad0934f6c7c96e3d479d65cbddfe3d1dd3acb9dea9915d8b" => :mojave
   end
