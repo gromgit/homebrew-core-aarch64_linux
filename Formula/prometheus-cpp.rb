8e95a3d6689e0594f6fd1955e5b31ebedf91be58
FX Coudert <fxcoudert@gmail.com>
2020-12-23 22:46:00 +0000

    prometheus-cpp: update 0.11.0 bottle.

--- Formula/prometheus-cpp.rb
+++ Formula/prometheus-cpp.rb
@@ -11,6 +11,7 @@ class PrometheusCpp < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "f3881dde4616cd5c5f840b25e5b99f1350f4105fe685c068eb91c582f9eb816b" => :big_sur
+    sha256 "d60d427eb5c81eb3816253c30bdb81ec20abf8ae839f001a77340a92b2f5f740" => :arm64_big_sur
     sha256 "bb1e958b203392f2af6de4a5897473ecb4e46bf9125a7317e6987abdd1b2dbf3" => :catalina
     sha256 "cd9c05bd7cdce396324d8390cbf1ada2d34afc3f7d97189f75e4ad530a005342" => :mojave
   end
