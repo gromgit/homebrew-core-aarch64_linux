c3a517a24ddcb4a3d481fed4c1dd510ee031c7d4
FX Coudert <fxcoudert@gmail.com>
2020-12-24 10:21:50 +0000

    sourcekitten: update 0.31.0 bottle.

--- Formula/sourcekitten.rb
+++ Formula/sourcekitten.rb
@@ -10,6 +10,7 @@ class Sourcekitten < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "c3df0d66fb5d3e5c978b3e0c76b36c3c46d9f63612c99530e8cb1e9a013982b8" => :big_sur
+    sha256 "28df76e8f1933869199c4d9007e66012de6c3405e8009d387500259f1a0cea8c" => :arm64_big_sur
     sha256 "f6ba3676e59393e20190e6e04d70cbfab24217109363ec24799b0dd52ba4ac70" => :catalina
   end
 
