b4af5db445076ce60b5812377aa7b8ee1ce0e4f7
FX Coudert <fxcoudert@gmail.com>
2020-12-24 19:03:32 +0000

    dmalloc: update 5.6.2 bottle.

--- Formula/dmalloc.rb
+++ Formula/dmalloc.rb
@@ -8,6 +8,7 @@ class Dmalloc < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "e1ff3b750a369957c46b920f34eb45ae11c4ee504c0f0ec8a832bc957335f347" => :big_sur
+    sha256 "f76a3f4d2592b9594b2fb0b47ad0389ec862c5b988bdb44302940d318695c9f5" => :arm64_big_sur
     sha256 "e1ef63a055d0c66350308872bb6041d96012e26f0715de5e1636c56f5a6fd8ff" => :catalina
     sha256 "c214b0a0d7d582a3d8229e7320f2b19c5ab4d08396fab3bf90076785f1cc0824" => :mojave
   end
