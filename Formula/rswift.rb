17b55d7839aa3eaa8ce1d3db5218e6beed61e0b3
FX Coudert <fxcoudert@gmail.com>
2020-12-24 11:52:14 +0000

    rswift: update 5.3.1 bottle.

--- Formula/rswift.rb
+++ Formula/rswift.rb
@@ -9,6 +9,7 @@ class Rswift < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "795ba1b73f962b695dadce31ea8c4a133e1640cb47a3d66e3f1610e120e8d6f0" => :big_sur
+    sha256 "0336661379cd6634bc68e0d863932724247416c24abd44fa5ad0eff10cbce234" => :arm64_big_sur
     sha256 "171e1c4edafbafc0eb0435c237bfb5ede731e76b537d3282e995f5c3fb6b30ad" => :catalina
     sha256 "607dcfb0fb765913d682438997b09ce512c0a5e9c8ae53ae957f4b5997ccd47e" => :mojave
   end
