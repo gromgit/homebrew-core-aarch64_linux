8fa003cf98821c9b8d8f5d6d7d7fb2af8835e1ae
chenrui <chenrui333@gmail.com>
2020-12-23 19:52:04 -0500

    libva 2.10.0 (#67549)

--- Formula/libva.rb
+++ Formula/libva.rb
@@ -1,8 +1,8 @@
 class Libva < Formula
   desc "Hardware accelerated video processing library"
   homepage "https://github.com/intel/libva"
-  url "https://github.com/intel/libva/releases/download/2.6.1/libva-2.6.1.tar.bz2"
-  sha256 "6c57eb642d828af2411aa38f55dc10111e8c98976dbab8fd62e48629401eaea5"
+  url "https://github.com/intel/libva/releases/download/2.10.0/libva-2.10.0.tar.bz2"
+  sha256 "fa81e35b50d9818fce5ec9eeeeff08a24a8864ceeb9a5c8e7ae4446eacfc0236"
   license "MIT"
 
   livecheck do
