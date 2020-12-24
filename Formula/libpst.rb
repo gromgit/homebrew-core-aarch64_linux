33ba68c9e9875dee969bf0ece851318c21795ed6
FX Coudert <fxcoudert@gmail.com>
2020-12-24 12:04:55 +0000

    libpst: update 0.6.75 bottle.

--- Formula/libpst.rb
+++ Formula/libpst.rb
@@ -14,6 +14,7 @@ class Libpst < Formula
     cellar :any
     rebuild 1
     sha256 "158b068e4645da1b01bd850f4f9c43d294908fb119fd39ea3459cf2e5f9723e7" => :big_sur
+    sha256 "6f245e65af86df879b94b2d043e9bc17dee7f1c307606d23d8aa0829524d620b" => :arm64_big_sur
     sha256 "9362205843b828388fe50aa733c114886210bbd2f2a8304f3916ecaf606e1cda" => :catalina
     sha256 "763deea9a814a76b5e5964e0de71bf27c6e22549df6ba5bec16c3e340cd1b80d" => :mojave
   end
