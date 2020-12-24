c62afc08ebbe7ab2ecd70907ea7da33d2485a812
FX Coudert <fxcoudert@gmail.com>
2020-12-24 14:38:59 +0000

    pacparser: update 1.3.7 bottle.

--- Formula/pacparser.rb
+++ Formula/pacparser.rb
@@ -8,12 +8,10 @@ class Pacparser < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "4f117a5e86f7763073efd2fa2c9f1a897395f5fc6a549cd7fd9debcdbcada2cc" => :arm64_big_sur
-    sha256 "985bbf12ff200cd4f521eddbc17e084f1cb1fd8166853a52fd4b30228bdefd46" => :catalina
-    sha256 "5a4db686679c753a806fa2e2df5e93263f973f447f9357fcdadc071687c10071" => :mojave
-    sha256 "1bb0af844e0cfd58357987f2f9e6f82b0e72a13df961f13ad8b81b3e00a3dff2" => :high_sierra
-    sha256 "719e5eadacf71e3a2e863447609322c45f3be3a9d3ee63373c05a9a2ae7f31b8" => :sierra
+    rebuild 2
+    sha256 "f13a42eac871188878a53dcd32336dac1da03d9cac519d45710be85392948cdc" => :big_sur
+    sha256 "b93489e78cd052204d255f63c2e8d1b719dfbea1052cfd547332fb585f7bf8f9" => :catalina
+    sha256 "8a61793f2d7ffcfdea7a7b98a5a6a19151d4fe559ce0c659c9cd0483a71b012a" => :mojave
   end
 
   # Fix build for MacOS 11.1
