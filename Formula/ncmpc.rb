3ec9eb11c37d6073b9e978dcf138377eb3c28320
Rui Chen <rui@meetup.com>
2020-12-24 00:29:43 -0500

    ncmpc 0.42
    Closes #67603.
    
    Signed-off-by: Rui Chen <rui@meetup.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/ncmpc.rb
+++ Formula/ncmpc.rb
@@ -1,8 +1,8 @@
 class Ncmpc < Formula
   desc "Curses Music Player Daemon (MPD) client"
   homepage "https://www.musicpd.org/clients/ncmpc/"
-  url "https://www.musicpd.org/download/ncmpc/0/ncmpc-0.40.tar.xz"
-  sha256 "09fa587caceb96b3de71c57c9b07dba9129824aa256272739ee79542bf70d3dd"
+  url "https://www.musicpd.org/download/ncmpc/0/ncmpc-0.42.tar.xz"
+  sha256 "a5f7471d766a71c222374efa4aa17ef6ee0e42ad48d15528edd935d1f0f6cd4d"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -32,6 +32,10 @@ class Ncmpc < Formula
     cause "error: no matching constructor for initialization of 'value_type'"
   end
 
+  # remove in next release
+  # commit reference, https://github.com/MusicPlayerDaemon/ncmpc/commit/1a45eab
+  patch :DATA
+
   def install
     mkdir "build" do
       system "meson", *std_meson_args, "-Dcolors=false", "-Dnls=disabled", ".."
@@ -43,3 +47,29 @@ class Ncmpc < Formula
     system bin/"ncmpc", "--help"
   end
 end
+
+__END__
+diff --git a/src/screen_utils.cxx b/src/screen_utils.cxx
+index 95de70e..e85061f 100644
+--- a/src/screen_utils.cxx
++++ b/src/screen_utils.cxx
+@@ -29,6 +29,7 @@
+
+ #ifndef _WIN32
+ #include "WaitUserInput.hxx"
++#include <cerrno>
+ #endif
+
+ #include <string.h>
+diff --git a/src/signals.cxx b/src/signals.cxx
+index 4c005aa..9f3eb72 100644
+--- a/src/signals.cxx
++++ b/src/signals.cxx
+@@ -17,6 +17,7 @@
+  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+  */
+
++#include <signal.h>
+ #include "Instance.hxx"
+
+ void
