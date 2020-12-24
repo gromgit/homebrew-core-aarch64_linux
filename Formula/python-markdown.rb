9c68c1704a6372e07f8d8b1f24562dd2ce4ddde1
FX Coudert <fxcoudert@gmail.com>
2020-12-24 15:20:52 +0000

    python-markdown: update 3.3.3 bottle.

--- Formula/python-markdown.rb
+++ Formula/python-markdown.rb
@@ -16,6 +16,7 @@ class PythonMarkdown < Formula
     cellar :any_skip_relocation
     rebuild 1
     sha256 "5c0eca089cf51f1a18fa14b08ef2e453940d216bd28c971c1e32c1d92dc924c8" => :big_sur
+    sha256 "e15c00fbbc97ddc7727ed969fa6b66e39d14a0840c701a278cc7d936265b3820" => :arm64_big_sur
     sha256 "2a5f0bc6b8f4e8f8910b638ca9de6d78d1721c670a58b04e85843e486f91b321" => :catalina
     sha256 "800ed7fb5c992646e2f6486eeb369b41a35f38f6aa1b219f91535ebb7817b755" => :mojave
   end
