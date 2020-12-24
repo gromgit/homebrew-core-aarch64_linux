1798989e96d8c9cc85f65aa6884b81445741a88c
FX Coudert <fxcoudert@gmail.com>
2020-12-24 13:54:34 +0000

    inspircd: update 3.8.1 bottle.

--- Formula/inspircd.rb
+++ Formula/inspircd.rb
@@ -12,6 +12,7 @@ class Inspircd < Formula
 
   bottle do
     sha256 "aab78f20c01a81d9b8c5a153e5ba9d629e74d9156d0e0c077e266d18e97228b5" => :big_sur
+    sha256 "704e9f33f97071ca2673b7d4e80b068cea5c48e7958bffefd701ac7f44c540f0" => :arm64_big_sur
     sha256 "9eb14778f35342c7e9ca7b6e17db90ad0ec763da02318b8d4ff15c3b70232504" => :catalina
     sha256 "35d846f23095493edd191b76aa873176fe4dfda4d8fad99fb8a17a556f8b29fc" => :mojave
   end
