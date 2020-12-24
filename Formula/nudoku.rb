51fee0a2fa2c4f35902ceef752f6927769eac273
FX Coudert <fxcoudert@gmail.com>
2020-12-24 15:49:14 +0000

    nudoku: update 2.1.0 bottle.

--- Formula/nudoku.rb
+++ Formula/nudoku.rb
@@ -8,6 +8,7 @@ class Nudoku < Formula
 
   bottle do
     sha256 "ec43523a92bb16fadd4ab7fe3f9189cb13849fc3919bf4525b9074a471453745" => :big_sur
+    sha256 "daa1278f79f9fdf5cd8780ba68d3bf89982728c1ddf636f89475e89b14bd9c64" => :arm64_big_sur
     sha256 "e6aa92aabc32164b2cc69d55d4a243f6aafdd6d6a109a40339d6546548153174" => :catalina
     sha256 "3afc049f852d205bff4b674e751fb1db83d73ab82ba15e5f38a1ba9c33ffd2e5" => :mojave
   end
