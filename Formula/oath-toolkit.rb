c555ec01a431b7d044f8404cebf5c0da3f8d58c7
FX Coudert <fxcoudert@gmail.com>
2020-12-24 09:30:15 +0000

    oath-toolkit: update 2.6.4 bottle.

--- Formula/oath-toolkit.rb
+++ Formula/oath-toolkit.rb
@@ -12,6 +12,7 @@ class OathToolkit < Formula
 
   bottle do
     sha256 "1dd544c61783354f225bbb25ab41645a0acf98693ef72fcdaba64da8c70efaf6" => :big_sur
+    sha256 "5fff47a435c0f709320da77ccdea7bcc2139e094071c87b211ddb3fbc57e1b32" => :arm64_big_sur
     sha256 "03ab59527335488e08f4d77527efa444e7e732d7aa8c2e4c852fe1d829d10146" => :catalina
     sha256 "c4ca091169b76af62be799ebc0d56ccb621ebc1cb8b790e31867ab0b0fd21e45" => :mojave
     sha256 "56cba35b06fd322068776ac49afc3f040fcba5defe28fdbe937cae7e1d01c1e7" => :high_sierra
