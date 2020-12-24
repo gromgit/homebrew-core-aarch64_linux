2dca3f3282168872be008cda8185cdc0255f665b
FX Coudert <fxcoudert@gmail.com>
2020-12-24 17:04:24 +0000

    libslirp: update 4.4.0 bottle.

--- Formula/libslirp.rb
+++ Formula/libslirp.rb
@@ -8,6 +8,7 @@ class Libslirp < Formula
   bottle do
     cellar :any
     sha256 "c246bdd605b4dd938e1973e3cc6e9855bfac88d2387aa40d41a85180554dc4ab" => :big_sur
+    sha256 "3cd403d0ace2d57d506d64ce0bfcc27d3f7adb3e5113481a585a2f8ab0e53f9c" => :arm64_big_sur
     sha256 "1e17da7e87c39a76d7597c6030d2ae12d3b2a79742b4533e0113e7442e610833" => :catalina
     sha256 "a2f636ece566472a1eb9db4761399a0a68bf1c2ca8b901a34d4205858704be90" => :mojave
   end
