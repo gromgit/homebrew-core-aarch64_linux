999cebdc20b6435eb10c460a74f071850bda328d
FX Coudert <fxcoudert@gmail.com>
2020-12-24 16:13:40 +0000

    gcal: update 4.1 bottle.

--- Formula/gcal.rb
+++ Formula/gcal.rb
@@ -13,6 +13,7 @@ class Gcal < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "c3ecf3ac091f690a0962ba9dbc1f83c2147ce8929499fcb6c24f54b3ac750476" => :big_sur
+    sha256 "1fc45b8d1ab6ce7bf8b771d7745eaa1dacd95f9782966b25e8296bc8639b9e67" => :arm64_big_sur
     sha256 "4bb8c5727fb336106016d1158796e0a127fc82eb630ec9e2bccb6d70327aa531" => :catalina
     sha256 "39c897bb2106251109af642741220e8b92e19c52be37ef928258eb1a2f6230ff" => :mojave
     sha256 "0a1e3ab27fcc8d08d78452074137dcdf281068c41a732a64222e9b5ed235e8fb" => :high_sierra
