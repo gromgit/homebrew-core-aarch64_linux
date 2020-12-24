aea85bbedf8f49847aaff178e1bb6939ba448346
FX Coudert <fxcoudert@gmail.com>
2020-12-24 14:08:03 +0000

    pachi: update 12.60 bottle.

--- Formula/pachi.rb
+++ Formula/pachi.rb
@@ -9,6 +9,7 @@ class Pachi < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "d14dec70d5fedd0d7ba63b05f175b06b12c40e1da71d24da64712ce63858dae1" => :big_sur
+    sha256 "71f7bf11f6d68a8768468e4494cdc0785f484a5ccd7713cfc4327f049e79e80a" => :arm64_big_sur
     sha256 "9a2adc64bf7dbfbaf9e3d9ff940d6c5bcb0e4040160ed62f57751ec87281132e" => :catalina
     sha256 "c88f24dd1e7a267848eab540dc2b0961962825ab6e7088fc24b335159dacf31c" => :mojave
   end
