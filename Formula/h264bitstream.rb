f65e29f2515db1d4dc57e7cece4e3c3dc162a201
FX Coudert <fxcoudert@gmail.com>
2020-12-24 18:50:15 +0000

    h264bitstream: update 0.2.0 bottle.

--- Formula/h264bitstream.rb
+++ Formula/h264bitstream.rb
@@ -12,6 +12,7 @@ class H264bitstream < Formula
   bottle do
     cellar :any
     sha256 "2ace75ebe5094b847024fb80a23a82bba3acfa7869399f3962c6910089ebc777" => :big_sur
+    sha256 "86d9c15b08de87f85f791e45669241a1a27fefef2ddbccf6016642505e69c6e6" => :arm64_big_sur
     sha256 "ac1f452b4c4d4d90310ec1f3cd9ec45271665604844dca55df3f7a91885d28d7" => :catalina
     sha256 "ebe66ef0a10e2afacf2b418eb15aa57ed873c6df73d6da71b6252efce8c15a5e" => :mojave
     sha256 "191acedb64e2ab618696fe16c55b81cdadb9819a0b0fc594235d31a28a1cdf96" => :high_sierra
