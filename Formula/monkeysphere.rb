b411e4c3b45bcb8056ca8123df7edd3ead8062ed
FX Coudert <fxcoudert@gmail.com>
2020-12-24 15:50:21 +0000

    monkeysphere: update 0.44_3 bottle.

--- Formula/monkeysphere.rb
+++ Formula/monkeysphere.rb
@@ -14,6 +14,7 @@ class Monkeysphere < Formula
   bottle do
     cellar :any
     sha256 "1c72800477183baed866d396e2ed0fa238a20b3de8767fdf33a78beafcb369d8" => :big_sur
+    sha256 "fdb224329c7d66b8770ff35746c94b8af3af96ad690e4cc24f384fd011ee0c3e" => :arm64_big_sur
     sha256 "1b6e0f281b0885de4151708205d92f3f8f237f8a4c5b966960bf01cc1196bb57" => :catalina
     sha256 "150dfcce34b75ba5e9e96ef6956ba0a6291088dd45ca1a1ebbf4b9d917291f34" => :mojave
   end
