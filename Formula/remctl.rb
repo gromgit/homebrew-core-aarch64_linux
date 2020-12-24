3360f5bc37dd47466e672dc73eaa1855ddec7b5a
FX Coudert <fxcoudert@gmail.com>
2020-12-24 19:23:48 +0000

    remctl: update 3.17 bottle.

--- Formula/remctl.rb
+++ Formula/remctl.rb
@@ -13,6 +13,7 @@ class Remctl < Formula
   bottle do
     cellar :any
     sha256 "c021ec96746bcec68298d67fb9ac2550b059a0b6626e331b79c26e8a18feedcc" => :big_sur
+    sha256 "3e7967965694f25afffdb35ee5f99857811dc64be5bd321278300725248d41e8" => :arm64_big_sur
     sha256 "58267d5b4fc81b44c59521fce5a6c64ece78a67436d702741acaa6e656122caa" => :catalina
     sha256 "3d05fc09916078097c4cf62021d1f92bc9df6aa89e4f8c5dbd6028877a640d84" => :mojave
   end
