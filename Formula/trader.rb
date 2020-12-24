e7d5348f420b5910418b18ed517d585005d6b5b9
FX Coudert <fxcoudert@gmail.com>
2020-12-24 21:50:20 +0000

    trader: update 7.14 bottle.

--- Formula/trader.rb
+++ Formula/trader.rb
@@ -6,6 +6,7 @@ class Trader < Formula
 
   bottle do
     sha256 "d47f18eea0c0f210c48fa15f4ff038869ee2efec31fb6dacc0d8df1abaf1183b" => :big_sur
+    sha256 "5e007ac0ba7b69e1bdc2c4287a6a716273f1645b2e19bbd06ab78c9d190444b7" => :arm64_big_sur
     sha256 "661f7dc5534bda2b721f00c12b868343a762d4f4b2cd8f6c869c2681b7a9c091" => :catalina
     sha256 "1e78bde8014188f21cc16fdf99e52128e3e135031d447c71f1c39fa8d1135b70" => :mojave
     sha256 "8175870d189ac87e617dda58fd61d4cd56109876bc5bf7fe55718ac9db137a27" => :high_sierra
