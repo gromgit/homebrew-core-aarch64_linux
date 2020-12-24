08d8d90fcf6e2cdb2fdac22a252583aaf9a04f49
FX Coudert <fxcoudert@gmail.com>
2020-12-24 13:37:20 +0000

    rhash: update 1.4.0 bottle.

--- Formula/rhash.rb
+++ Formula/rhash.rb
@@ -12,6 +12,7 @@ class Rhash < Formula
 
   bottle do
     sha256 "045a014357e8d4101921c6a2c40bd82cbdcfebc64ddf5c6d7ed5c3f846f08e5d" => :big_sur
+    sha256 "29cb64c69816a5706afd7245be32bc33ba47045bf24dcf54c3601b973c39cd21" => :arm64_big_sur
     sha256 "3fc816254535e1ecf091161b96447efedf2748cdf25a38449f6de70ef652165d" => :catalina
     sha256 "89ae46bbd559e15e9aacb9010e4f4cff6ab402e8bd9eb301f8cf7aa745dbdde3" => :mojave
     sha256 "998d4c8b2195944bc979c11fcd7aff29997994c8457d29343524edea15de74eb" => :high_sierra
