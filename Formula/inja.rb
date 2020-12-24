f937cbb960f8bc601b642b2c8d72fbc065358bf6
FX Coudert <fxcoudert@gmail.com>
2020-12-24 20:39:16 +0000

    inja: update 3.1.0 bottle.

--- Formula/inja.rb
+++ Formula/inja.rb
@@ -9,6 +9,7 @@ class Inja < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "872f713878bc80ab3b5b789dcf85f95e9144f7ae299091b71b9f7851efd139d9" => :big_sur
+    sha256 "953dc5ef432b2331176524d03ee16ee6ea3f94ed59c91f633fe615c85407b5f5" => :arm64_big_sur
     sha256 "2dc3622a52c97e3bd5824127000fc8ee4b5a505c5303591cec8ca2f5c7b3320e" => :catalina
     sha256 "e93bba969f441b2425e10ec5849b5f904a932bad7b6318b07dc62dc20bae7a5a" => :mojave
   end
