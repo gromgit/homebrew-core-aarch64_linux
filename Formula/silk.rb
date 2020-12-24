61e839227f881513c89c3c5650ab72ed2b3cd9e4
FX Coudert <fxcoudert@gmail.com>
2020-12-24 14:29:30 +0000

    silk: update 3.19.1 bottle.

--- Formula/silk.rb
+++ Formula/silk.rb
@@ -11,6 +11,7 @@ class Silk < Formula
 
   bottle do
     sha256 "6fab609033d87aada95d080cbc49b4730bb4b07d77b58eba8f6244773e2ca999" => :big_sur
+    sha256 "8d3c65086a32469d221ca03666c9c54f9b16dc3a83cd3906ac46676d0f7139fb" => :arm64_big_sur
     sha256 "4a88b111ce742a948b91b9441f2bbc7e821ffd3691673086ff46e8e27fbda31e" => :catalina
     sha256 "923bc8b774f207d23073195b49befba72e378e79846b6809066f55f3df87c329" => :mojave
     sha256 "663d2a858210750b8650e4f0e516dd6530fb5d08a7c501f8daa937572d8a81ee" => :high_sierra
