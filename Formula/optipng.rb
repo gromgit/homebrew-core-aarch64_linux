fd16f35fec002e6c517ccbbb07207db23a727830
FX Coudert <fxcoudert@gmail.com>
2020-12-24 13:41:03 +0000

    optipng: update 0.7.7 bottle.

--- Formula/optipng.rb
+++ Formula/optipng.rb
@@ -14,6 +14,7 @@ class Optipng < Formula
     cellar :any
     rebuild 1
     sha256 "5cee26efb92016f057a55b2711a08c4a0350046b7c0b1d969c75a913caf66fc2" => :big_sur
+    sha256 "796af028b1dea8b680e40103712976b4f9df285df553db06d2643779630c716c" => :arm64_big_sur
     sha256 "3d423dfa59e07122f70e2a15026289dfc6884798ac76898065dbe587256c6e35" => :catalina
     sha256 "bd44fa66b00a6ee0340a9a5b239d22823787fcaa26312783b21c0f4afc39fd0b" => :mojave
   end
