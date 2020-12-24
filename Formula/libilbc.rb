7809009c43d386857e892ce50ad73a45357fd4ee
FX Coudert <fxcoudert@gmail.com>
2020-12-24 17:55:49 +0000

    libilbc: update 3.0.3 bottle.

--- Formula/libilbc.rb
+++ Formula/libilbc.rb
@@ -8,6 +8,7 @@ class Libilbc < Formula
   bottle do
     cellar :any
     sha256 "7cf3a9708a5e405ae07b1e30862fe2636813e3d4675f5410907def2f5eeb5de1" => :big_sur
+    sha256 "1549132b9cb58c91b1f437ec1212b9ad59c7788f006b34e7ea29a030f25e3788" => :arm64_big_sur
     sha256 "922b31da0782f99be36442b29a12b894f582153db289e19d33c39377d1a53a16" => :catalina
     sha256 "709aca6a8540a022a9a8f52973f9796eee258c395321d3e311a38b67f960884c" => :mojave
   end
