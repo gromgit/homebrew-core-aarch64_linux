c9cb4fc293e8f0a52fd8bbad43ba5c4eff1c9d13
FX Coudert <fxcoudert@gmail.com>
2020-12-24 21:53:15 +0000

    xcb-util-wm: update 0.4.1 bottle.

--- Formula/xcb-util-wm.rb
+++ Formula/xcb-util-wm.rb
@@ -8,6 +8,7 @@ class XcbUtilWm < Formula
   bottle do
     cellar :any
     sha256 "d63a72b6a9714c0e1d92e1d0da59fc702f1f8aa44dba75c2dcf85fb5a291908d" => :big_sur
+    sha256 "88f837cbf6f6693bc54371d9fab65f211951023bd59dddfa2f2084e831abc4a1" => :arm64_big_sur
     sha256 "77611bd19da065ae3e1053ec9b581a52e93bc8669a8efee2d06719632695815f" => :catalina
     sha256 "1423847ca100bb773cd5f85d1766abbf9004b88e85fc92cc25a30ea23341f0e3" => :mojave
   end
