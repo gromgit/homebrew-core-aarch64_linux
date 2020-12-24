ec08adec93a3b78c7af665786b849219f00b004a
FX Coudert <fxcoudert@gmail.com>
2020-12-24 18:56:26 +0000

    gpx: update 2.6.8 bottle.

--- Formula/gpx.rb
+++ Formula/gpx.rb
@@ -9,6 +9,7 @@ class Gpx < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "254414afa9fe68137739444a5c514637131eac89d208239d4de86d953bbed5cd" => :big_sur
+    sha256 "60d8a577b0d45216452c475ff07e4641aec56c599a491c00409530a8fc5db856" => :arm64_big_sur
     sha256 "a982edd4fb776a077ea51294aea03533e5672dea8a7710329aadc2a3adca9ad1" => :catalina
     sha256 "f807c588535d7d941470c2d80dd58e97f4ad9e72d7da1b13cbbf87d9912a970a" => :mojave
   end
