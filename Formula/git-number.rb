2491410dce53d19f0afeb634fae5e5d25119989a
FX Coudert <fxcoudert@gmail.com>
2020-12-24 14:36:43 +0000

    git-number: update 1.0.1 bottle.

--- Formula/git-number.rb
+++ Formula/git-number.rb
@@ -7,6 +7,7 @@ class GitNumber < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "ca7a48d9827be6cc0d625891e25b7b7b7474d9c33a276955ab3f0eb14b8cb21b" => :big_sur
+    sha256 "f357699b108c2e521721c7a8a93ee188a526b47a5f1af7841cf005d2d1008f4f" => :arm64_big_sur
     sha256 "662840b36a99f95902aee618faed6274d2cf9c6620b9c01855377d85d838eaad" => :catalina
     sha256 "2fc24b4bb5404f85fb6c359ac9b8c969846953176d8a01176c4e6ddba3067bc9" => :mojave
     sha256 "d71548120a8d5d9db4b9b9ae71be947303c6a415e35380d0d8e36551765b827f" => :high_sierra
