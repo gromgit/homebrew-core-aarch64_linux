71ab6f1121ecafe3eb2cbf25e621822dcd02a71f
FX Coudert <fxcoudert@gmail.com>
2020-12-24 14:49:26 +0000

    vmtouch: update 1.3.1 bottle.

--- Formula/vmtouch.rb
+++ Formula/vmtouch.rb
@@ -8,6 +8,7 @@ class Vmtouch < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "89ed86d067750e6bf19a6a79d7f3b9c3b2ad2e39795f174ba2452d11d43f650e" => :big_sur
+    sha256 "6d55c8a93a6826d78dcd439155de21e3da33598bd00c022e1fb4d39635f12c53" => :arm64_big_sur
     sha256 "30c620a4dc06285c41c7194468de50cf0f12aab38c6441d8e1bbad6d4231ee1e" => :catalina
     sha256 "020d4e624a448e4e1b9a6e26b8f506bd65ab789ae1c0f23f25beda78b09bc6dd" => :mojave
     sha256 "edb14ca1ff4cbd4ab535ca9099ea113a36e280ddaf2957a65bdef10f4a7a1b88" => :high_sierra
