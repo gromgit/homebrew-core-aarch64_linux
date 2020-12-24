8087e48b94cc8d39e824ba539879f50a7b2bedad
FX Coudert <fxcoudert@gmail.com>
2020-12-24 15:10:50 +0000

    cmu-sphinxbase: update 0.8 bottle.

--- Formula/cmu-sphinxbase.rb
+++ Formula/cmu-sphinxbase.rb
@@ -14,6 +14,7 @@ class CmuSphinxbase < Formula
     cellar :any
     rebuild 1
     sha256 "774670d79572ac633b5acf646a15ca54055e140044476bfeac7bc1d377a1dbe4" => :big_sur
+    sha256 "d8ca2666d2ee6c5ff4a5a88ad086cfcf3e0cf744e6614ea31b451dfd86196c17" => :arm64_big_sur
     sha256 "0cea9513b180773ff3c45d24453e962ef4ad5d1f923c4c22716437f3580b195f" => :catalina
     sha256 "b55c9f16e8b89fc515d9bf8bd6ed91f532d0c82a46be01cd9792bb27076a6a51" => :mojave
     sha256 "2ebde8d649a3e78c3e219c83e1f12e6cee924f5404b0d68e8fe7d220c8dad0f5" => :high_sierra
