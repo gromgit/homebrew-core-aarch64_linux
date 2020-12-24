69fb3f21c199d9b2fb484547fa93e23ce216f11d
FX Coudert <fxcoudert@gmail.com>
2020-12-24 17:03:49 +0000

    afsctool: update 1.6.4_2 bottle.

--- Formula/afsctool.rb
+++ Formula/afsctool.rb
@@ -11,6 +11,7 @@ class Afsctool < Formula
     cellar :any_skip_relocation
     rebuild 2
     sha256 "e7d401a4f723f58ad588e5b2fb5b19c6d76e7faed0385d0b3eef59d1f933e1ee" => :big_sur
+    sha256 "01af9a80d59e870a48adcbf1eb0cba0a3dd0a4013df397114c4c0f21f16e3916" => :arm64_big_sur
     sha256 "f418e15be4bafdcb1a85e14c3148c8d4af1b300bd6ed3e4a30eca3725459ac48" => :catalina
     sha256 "15c264a828ed98a42cc5ac68869c16b8306f73effe108e50bb1f731574311c51" => :mojave
     sha256 "72e92414d524b82ec1d8381ad50f55bd330f1109a5e10bca4235300fee557caf" => :high_sierra
