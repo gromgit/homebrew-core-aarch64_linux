0403852fd16f62060a290aa3ec1fe7dd8be02048
FX Coudert <fxcoudert@gmail.com>
2020-12-24 15:01:22 +0000

    marked: update 1.2.7 bottle.

--- Formula/marked.rb
+++ Formula/marked.rb
@@ -14,6 +14,7 @@ class Marked < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "beb4dd0e093ad7753855705c734dae9ba522d9e7414b38ac240352d6aab3bbac" => :big_sur
+    sha256 "5050e98f7a5a9715a491030f6a552beb6a90a222a1fee74d330177039f85ac90" => :arm64_big_sur
     sha256 "62b9478edcbf45327e3d4bca454c4c34a505aa9f23a450bacead225c9a227b79" => :catalina
     sha256 "4ba7f9cf413ab50cde3050f96262ec740b186c684e7d5a8f434adecfa8cda8f8" => :mojave
   end
