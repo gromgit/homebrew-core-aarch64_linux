fb233343e2cd8b19668bdbbd026c725a6779806b
FX Coudert <fxcoudert@gmail.com>
2020-12-24 22:17:47 +0000

    xinput: update 1.6.3 bottle.

--- Formula/xinput.rb
+++ Formula/xinput.rb
@@ -8,6 +8,7 @@ class Xinput < Formula
   bottle do
     cellar :any
     sha256 "dab36aa6df662605e220ffce42106ed9eb6668088c425773335b04a59ba29575" => :big_sur
+    sha256 "db714eb643b851dec6aa996c3673b932af70e191d404c8a187b408a9578f7fef" => :arm64_big_sur
     sha256 "5bda55eccff0b10605378a85932776af6105fff7ae85593e2f10d13e9adca128" => :catalina
     sha256 "afad4cf5c8b632f1e8873eee07a2bfd694efb1466dcafc0d4c8c233e370c4195" => :mojave
   end
