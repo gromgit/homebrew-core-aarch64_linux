50d8fb0396e1fa2f3a2456be85fec853b44fa871
FX Coudert <fxcoudert@gmail.com>
2020-12-24 11:43:32 +0000

    bcrypt: update 1.1 bottle.

--- Formula/bcrypt.rb
+++ Formula/bcrypt.rb
@@ -14,6 +14,7 @@ class Bcrypt < Formula
     cellar :any_skip_relocation
     rebuild 1
     sha256 "9c3948e719b6b5cb195fc30c8e0fd555c35d9fcea7a5a6c607b094ce2e097f01" => :big_sur
+    sha256 "7b3e672ab9055b69dc1c7c5a7d13ad3a9a375f60de85cfcc5268f7eb139edcda" => :arm64_big_sur
     sha256 "132998cb8e196f506666943a94a26927a19899cb1e45ee8eaf65e5ad0ee7ef8d" => :catalina
     sha256 "bb843c3b04f9adf57df1c2d07e30303626eedb0f45695dcaf38d0835ea3e35fd" => :mojave
     sha256 "883a4a97b7275e91cd90ab9d1ca69fa2a3c0db3544a2d77863a37bda16c51667" => :high_sierra
