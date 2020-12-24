5a2e54db65f0075b129a84624f96374279686638
FX Coudert <fxcoudert@gmail.com>
2020-12-24 10:51:04 +0000

    pgpool-ii: update 4.2.1 bottle.

--- Formula/pgpool-ii.rb
+++ Formula/pgpool-ii.rb
@@ -10,9 +10,9 @@ class PgpoolIi < Formula
   end
 
   bottle do
-    sha256 "79fe17f3a82a968e823f89744aa5983f8744981b56588bc1ee0359e0c8f44511" => :big_sur
-    sha256 "c5aede9bb5f4e668d154ae3cbf0f2b1412494a52180289658069493b9a140381" => :catalina
-    sha256 "db10d11faa36b3236dbb5c6b3031032cdc3b12e70eed802fc4f6b0628ada7378" => :mojave
+    sha256 "5d9e9684a6889d139033d02927823fe34f23c8feb12b06f4570cf5028bc84d2d" => :big_sur
+    sha256 "7b0245d2cfad6fdb47a8ae12a2c8f787cf917ae521244a42e9bdf79254f492f8" => :catalina
+    sha256 "9847e3f4dfb8351c0e2d0e855b4cb3bd5beb0ea2d3237c544b82a150d0aacfeb" => :mojave
   end
 
   depends_on "postgresql"
