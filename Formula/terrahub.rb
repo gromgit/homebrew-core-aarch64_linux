3dce8783dfbb47e4196e4cd2392f37cac52acde4
FX Coudert <fxcoudert@gmail.com>
2020-12-24 15:58:26 +0000

    terrahub: update 0.4.40 bottle.

--- Formula/terrahub.rb
+++ Formula/terrahub.rb
@@ -14,6 +14,7 @@ class Terrahub < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "bda38c42bf310ba81b79fe2500098b6619fff880e1cbde80610058dfb94b7f6e" => :big_sur
+    sha256 "571332db427f8bb17de8b615c5d8711e57c343909030e5efe8e717aa9804e992" => :arm64_big_sur
     sha256 "1eac10b58b87e07c6a7fa6eb7f4dbfcf1ab69b844c88282a6eeee2584056199a" => :catalina
     sha256 "8b31e7961c3ee86fc4f91a30eb6209e79ea465c864859bdef5567de9649f42a2" => :mojave
   end
