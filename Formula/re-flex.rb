adf450ad19e14e53313b7f0ac7d56cdad6cc7e4d
FX Coudert <fxcoudert@gmail.com>
2020-12-24 13:50:03 +0000

    re-flex: update 3.0.1 bottle.

--- Formula/re-flex.rb
+++ Formula/re-flex.rb
@@ -8,6 +8,7 @@ class ReFlex < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "b81ec2d7ce47750d4e1c7c9f3ef6f1dfb6725b64c15dc91b7a795116088c5ddc" => :big_sur
+    sha256 "dae04f5572e5ef2e6cc5c55adc1b33793f03c3ab6c2cf9bcc66b23907af8d969" => :arm64_big_sur
     sha256 "7e662642c4a570ffabfc137b9c5259b687bd438deaf0da4909ab18f4c877c69b" => :catalina
     sha256 "21297a3a6515110965ace284f6055015439677f6edf811f2803560318f8d286f" => :mojave
   end
