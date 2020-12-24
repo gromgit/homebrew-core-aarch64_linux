67feae7da68dc68ff1b819e25c95147f34cf8be5
FX Coudert <fxcoudert@gmail.com>
2020-12-24 21:38:52 +0000

    json_spirit: update 4.08 bottle.

--- Formula/json_spirit.rb
+++ Formula/json_spirit.rb
@@ -11,6 +11,7 @@ class JsonSpirit < Formula
   bottle do
     cellar :any
     sha256 "850ab2dda8c7ca10c88a25cf0fe971d8aeaee0d5942c44eea746fc44fc857f7d" => :big_sur
+    sha256 "ef7641e5dd587a4595e326e74f438c9f99e411acceaeee75dc7450835e126895" => :arm64_big_sur
     sha256 "31aaf302e4238b13797722028ac46a7deade1df4f042b46feb7d455cb05e4599" => :catalina
     sha256 "2cec376e843919e2f3693e73be0e3a2c6a6f3b283e503b51d42108c5471e8091" => :mojave
     sha256 "55299a7931b4bbbcf1ee5c576fe35283373279cc95b3b5126696ad5741f3d072" => :high_sierra
