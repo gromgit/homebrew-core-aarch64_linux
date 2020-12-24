b05b469e854c194630e463f71f00a623666b3ea9
FX Coudert <fxcoudert@gmail.com>
2020-12-24 19:34:15 +0000

    ssed: update 3.62 bottle.

--- Formula/ssed.rb
+++ Formula/ssed.rb
@@ -9,6 +9,7 @@ class Ssed < Formula
     cellar :any_skip_relocation
     rebuild 2
     sha256 "8ead33fc5954cd35bdff6291cec76e4ee2d6011d0f4d7025e832c9fa2514c31b" => :big_sur
+    sha256 "2b6c860af3e99b067b867a53b1b7135918cf1ff10e27b744a4157c2134b866f5" => :arm64_big_sur
     sha256 "69a3bbba8a87299f96f1b51c612d3335d1114ffb0bc6aa186c6f3e87335767e6" => :catalina
     sha256 "b3b9297ad7ac202de1403508682aaf1dbb187fe3ab1f4c6cc9bee7b7a69ab1d9" => :mojave
   end
