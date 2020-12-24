f2d9be62bfc37549987596a9f13b425a006f55c1
FX Coudert <fxcoudert@gmail.com>
2020-12-24 15:45:23 +0000

    vitetris: update 0.59.0 bottle.

--- Formula/vitetris.rb
+++ Formula/vitetris.rb
@@ -9,6 +9,7 @@ class Vitetris < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "4a773e9655aa93819f5c96d08925c12797a3540975e28b00de8c1d42d4e3a454" => :big_sur
+    sha256 "6b594a11ce43cd4dc66034d95a4abbe9904fd175bb0045973db8b5cc0a7deee3" => :arm64_big_sur
     sha256 "1ced40dfe35a80d2820ea3875024b27d31b590a94ac1cc5ca5b6d6ef7e9fd679" => :catalina
     sha256 "fadb41e0b7ad861cec4883c148c2cfc6ebe52e8a3b4eb099a3ffcb4cd07e5936" => :mojave
   end
