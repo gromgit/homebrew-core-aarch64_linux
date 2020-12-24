f9fed039fb324ee4549258b7f1a35a0663b259b8
FX Coudert <fxcoudert@gmail.com>
2020-12-24 14:34:46 +0000

    samurai: update 1.2 bottle.

--- Formula/samurai.rb
+++ Formula/samurai.rb
@@ -9,6 +9,7 @@ class Samurai < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "5e3819fa6eb240e781c236528297422575c6c2ae9aa4e38e6ddc7dbac0e25339" => :big_sur
+    sha256 "04ca3c9aada344360216791324e673db86948a3eaa2e82a541cb1fd28647b1bf" => :arm64_big_sur
     sha256 "35e183246e80cfe5a6f9b11b12cd2e0c3a754da15b8fb7550b5716de9e219e8d" => :catalina
     sha256 "ef652224d51e64d4e83f921a3870cd9cb4d7dbc315156cb68dd01d30d2d34414" => :mojave
   end
