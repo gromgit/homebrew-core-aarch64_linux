14137f681102183d05973a7de53b0c1bf9aea0d3
FX Coudert <fxcoudert@gmail.com>
2020-12-24 11:14:24 +0000

    mysql-connector-c++: update 8.0.22 bottle.

--- Formula/mysql-connector-c++.rb
+++ Formula/mysql-connector-c++.rb
@@ -12,6 +12,7 @@ class MysqlConnectorCxx < Formula
   bottle do
     cellar :any
     sha256 "90e116fbf60b03abb2dc84296879e60485d40e6296966214406a8ce5a1702990" => :big_sur
+    sha256 "306284af196c840d9c523d22a36e9fe29037ace2c60daa0a34a71e0354c281bb" => :arm64_big_sur
     sha256 "a7ce662aff4a29d35a46486d661bbf1b0149079455a760ea829810ca92423b02" => :catalina
     sha256 "5ae4ddc27990c9ef8019a4668a150af3671ce41a066d7917eb026b224bba54fb" => :mojave
     sha256 "cbb2bfbf652d2d3d55e67de4b3b2af131dc76b520e9eddb3ce4b26c722b1c9ac" => :high_sierra
