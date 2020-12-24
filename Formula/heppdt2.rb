7644cc49f56138724b13b542b30b993e4dd06535
FX Coudert <fxcoudert@gmail.com>
2020-12-24 21:28:06 +0000

    heppdt2: update 2.06.01 bottle.

--- Formula/heppdt2.rb
+++ Formula/heppdt2.rb
@@ -8,6 +8,7 @@ class Heppdt2 < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "ad2b96b10116b7be43a7fb93f0dd76346c904b9fc281983cbb954dd21674499e" => :big_sur
+    sha256 "2b0f2ffc2837fee3dbf5aea96b1a7329c574373578548986c95cdf50b7f0171a" => :arm64_big_sur
     sha256 "5cefdebab8dadafb0c96b121eab2dc0abf0cf6b2f7ff5acdd4d67353c05c275b" => :catalina
     sha256 "2c0ccad4374004249fc7431acaa358d0b04d096526e6447ec0c10cdc3b4759af" => :mojave
   end
