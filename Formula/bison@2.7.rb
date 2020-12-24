825a99349fc10d3329bb9564226fa59e66bafad9
FX Coudert <fxcoudert@gmail.com>
2020-12-24 09:17:27 +0000

    bison@2.7: update 2.7.1_1 bottle.

--- Formula/bison@2.7.rb
+++ Formula/bison@2.7.rb
@@ -8,6 +8,7 @@ class BisonAT27 < Formula
 
   bottle do
     sha256 "01d3b84f13676a4da576df0d7f8f9fafcc7ea734b895a5d3947b1e055d9db330" => :big_sur
+    sha256 "a8889d09761ad553f7b7061947a1715e88a658ce7f4d3755b7d8d00f25a53f1a" => :arm64_big_sur
     sha256 "b9af668b0da3e89f4a2d7b7e4d42009965780d1f7cd1541df85f758c2b7af55a" => :catalina
     sha256 "125fdf2eb737cdb8a3e795234f8e1fb5ec477f8590c534f7895497a6af82e04b" => :mojave
     sha256 "ee0e758aa798809aaa3e94f1e3659c9d33497a577c25cfc03ecfe18c25862837" => :high_sierra
