e804e7d1cfb49ce4ab8629c6c7b20295f0bd84ee
chenrui <chenrui333@gmail.com>
2020-12-24 15:18:38 -0500

    php-cs-fixer 2.17.3 (#67627)

--- Formula/php-cs-fixer.rb
+++ Formula/php-cs-fixer.rb
@@ -1,8 +1,8 @@
 class PhpCsFixer < Formula
   desc "Tool to automatically fix PHP coding standards issues"
   homepage "https://cs.symfony.com/"
-  url "https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v2.17.2/php-cs-fixer.phar"
-  sha256 "e3c976130f8bd49070ded6b97191ea82eb96536e3d99671814d29aa0529d6bcf"
+  url "https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v2.17.3/php-cs-fixer.phar"
+  sha256 "6b9e68fd3e5e1a03da98699c5363b9c93cfb084a4ff1a0fe6487a25776c2633b"
   license "MIT"
 
   bottle :unneeded
