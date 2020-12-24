59c85afeb2c024e51abf25d3e3b1fc7380bd19fa
FX Coudert <fxcoudert@gmail.com>
2020-12-24 16:10:49 +0000

    imapfilter: update 2.7.5 bottle.

--- Formula/imapfilter.rb
+++ Formula/imapfilter.rb
@@ -7,6 +7,7 @@ class Imapfilter < Formula
 
   bottle do
     sha256 "a3f6c7500a3206466979cb184c75e5d06f2a478c04ba7de9a671e0ae4e578a65" => :big_sur
+    sha256 "e49fed469e38c13b29df94f07ce45bb40bdcf167961ba08e7199192346ce8cd7" => :arm64_big_sur
     sha256 "4033f3f9c51c811a9bce55523d337f5d61ab987c742a90c02c12f97c00b768cb" => :catalina
     sha256 "aa77cdfd4279e290c68fe6aa1af6d40d820e10f3f53513d17c4867fcfcade11e" => :mojave
   end
