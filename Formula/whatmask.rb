5b5a7f1c5c1eb7e23e13f66a0dec78df62244e80
FX Coudert <fxcoudert@gmail.com>
2020-12-24 23:58:52 +0000

    whatmask: update 1.2 bottle.

--- Formula/whatmask.rb
+++ Formula/whatmask.rb
@@ -9,6 +9,7 @@ class Whatmask < Formula
     cellar :any_skip_relocation
     rebuild 1
     sha256 "55789adc6a9326b814965c6c0fcf41f912638f2e7d55d4167cbe404ec1a6938d" => :big_sur
+    sha256 "a5bf6f569bef04d197a6eb0c097450e65dcb5082b65ecc82201e15eb873ae755" => :arm64_big_sur
     sha256 "89a44972f8d27003b4c91f04a294f0be9a0d00628fb8db21faf46a55a0720cb2" => :catalina
     sha256 "a3a5a8887d1c7d43f83bf99c2f81f8900af0d83091978f5aac28447d0f093785" => :mojave
   end
