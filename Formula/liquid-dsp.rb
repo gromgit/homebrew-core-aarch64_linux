3075c2685aa63194a6c906dc3e420a4785e734c1
FX Coudert <fxcoudert@gmail.com>
2020-12-24 13:30:30 +0000

    liquid-dsp: update 1.3.2 bottle.

--- Formula/liquid-dsp.rb
+++ Formula/liquid-dsp.rb
@@ -8,6 +8,7 @@ class LiquidDsp < Formula
   bottle do
     cellar :any
     sha256 "3fc321af6dc365c262fe1707c9e96b21685fb1ab364bfc34b244b98f1f35b9fa" => :big_sur
+    sha256 "64eefc1203dbbcfb3e1313fcb1a290ed3d2d0e7373c4eeb683306fbe754f48c8" => :arm64_big_sur
     sha256 "1b1e21733e9789f6c1e3f8c3e5bb2076151f96a1f3602ed0e3905ad3ff45c18b" => :catalina
     sha256 "12702bbff57912c18a4f637df59da56054f8afcc11f96467341af3bd26ec0992" => :mojave
     sha256 "f0a6f6caba39b3e06a972aa89293161fa6d4d3759751709f40f2cc8d7ea3c913" => :high_sierra
