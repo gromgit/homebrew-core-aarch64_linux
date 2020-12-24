13a42267b8439cd8688c5f8c26d336d39043dcae
FX Coudert <fxcoudert@gmail.com>
2020-12-24 10:16:12 +0000

    nativefier: update 42.0.2 bottle.

--- Formula/nativefier.rb
+++ Formula/nativefier.rb
@@ -14,6 +14,7 @@ class Nativefier < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "7d421d5483813150206e73225702ff5beba8b6d28a347eec9a149f064502cd7a" => :big_sur
+    sha256 "15a2b779a84d1c20bd792dd037a4f03a79eddc1b170f0898b0186ad7dd342aad" => :arm64_big_sur
     sha256 "579864e4179cdc08b88bb59802e1cabb6fa8decd5f7c39d6fa5d6127c3934a9c" => :catalina
     sha256 "9f3ff10f644eb28f2beaab8bc08ee6e0ebcd6aae813c4b52aa4cdef26d665cde" => :mojave
   end
