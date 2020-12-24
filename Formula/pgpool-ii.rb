f445bf42cd4b8a8561ffa769cebd8b73194801e5
Rui Chen <rui@meetup.com>
2020-12-23 19:13:14 -0500

    pgpool-ii 4.2.1
    Closes #67590.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/pgpool-ii.rb
+++ Formula/pgpool-ii.rb
@@ -1,8 +1,8 @@
 class PgpoolIi < Formula
   desc "PostgreSQL connection pool server"
   homepage "https://www.pgpool.net/mediawiki/index.php/Main_Page"
-  url "https://www.pgpool.net/mediawiki/images/pgpool-II-4.2.0.tar.gz"
-  sha256 "7220e8f5c0ec14a2d745f4b99f44d97caa51df888c63984580361378eee0cdb4"
+  url "https://www.pgpool.net/mediawiki/images/pgpool-II-4.2.1.tar.gz"
+  sha256 "98938af9fccc37bcea76e6422e42ce003faedb1186cdc3b9940eb6ba948cdae1"
 
   livecheck do
     url "https://www.pgpool.net/mediawiki/index.php/Downloads"
