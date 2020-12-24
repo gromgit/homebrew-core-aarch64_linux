c258af35436f036eccaed21cb4b8b9cf014d2e8e
FX Coudert <fxcoudert@gmail.com>
2020-12-24 13:36:44 +0000

    gdl: update 3.34.0_2 bottle.

--- Formula/gdl.rb
+++ Formula/gdl.rb
@@ -11,6 +11,7 @@ class Gdl < Formula
 
   bottle do
     sha256 "30164281abee8e2047138cddc735d9a9e1ec520f673d468a601b6cfbc988cdd3" => :big_sur
+    sha256 "ef295dc3cfd05bf245b94552e008acc047e03e2806e93374879e7881b5c94871" => :arm64_big_sur
     sha256 "5acba250d8c77d17be5ff312bf11d6aa33cb609c4c351b2a1cd1bf565e73e81a" => :catalina
     sha256 "2e2e04543eaf7ee02a791433fbab570d3b5f44651cec8dd56a40a519c2a38d24" => :mojave
     sha256 "abd9360935baecd914847697e5a21e7b7d91b94c0d5878509921cdb2ba72799c" => :high_sierra
