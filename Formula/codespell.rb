38a4207686fbe8f6917a062e983500d71a503ad7
FX Coudert <fxcoudert@gmail.com>
2020-12-24 16:14:37 +0000

    codespell: update 2.0.0 bottle.

--- Formula/codespell.rb
+++ Formula/codespell.rb
@@ -14,6 +14,7 @@ class Codespell < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "c593fceba8be94074570221a350741dc1f11929f5c1176197e3962f34fb46363" => :big_sur
+    sha256 "75ab1ae15931e2358946065d9d9b53ec0a11516de94e69def4b097933207372e" => :arm64_big_sur
     sha256 "2d338cbcf31abc4712005bca0cd59543403a1525a24952b14fadc6592c2c7791" => :catalina
     sha256 "c53e89d6f26521dac01d2f26952674a12228e8f70070c9a02abd0c31744d2128" => :mojave
   end
