4b24c994fa59622fd4f43849e2c3bd42f9890b2d
FX Coudert <fxcoudert@gmail.com>
2020-12-25 23:42:57 +0000

    lf: update 18 bottle.

--- Formula/lf.rb
+++ Formula/lf.rb
@@ -8,6 +8,7 @@ class Lf < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "6044386633dc91ab445dbf477b18c2e6e5502a382036d6e87dd5cb9ff2d4f59e" => :big_sur
+    sha256 "eb38a263925e44e832d9569d1a4216ae4f5196e34933e7080587986a1b70e155" => :arm64_big_sur
     sha256 "168e8bb96c49691e5dad111bcce04c4877e22a730fe12e41ae2f87fdca139fe9" => :catalina
     sha256 "bfd50575aa60082a49d4710a0dc414ff29e0f46093be5209e53253ba2aac15ca" => :mojave
   end
