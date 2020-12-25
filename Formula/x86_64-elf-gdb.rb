9d82397333f74ce9714299865539d90e22221795
FX Coudert <fxcoudert@gmail.com>
2020-12-25 04:08:32 +0000

    x86_64-elf-gdb: update 10.1 bottle.

--- Formula/x86_64-elf-gdb.rb
+++ Formula/x86_64-elf-gdb.rb
@@ -13,6 +13,7 @@ class X8664ElfGdb < Formula
 
   bottle do
     sha256 "d9c893cd48502f675e5ccfbf139c70dc811683ff8427ee12053af00d63800d4b" => :big_sur
+    sha256 "922a0ed485b4ff144fcf876db7c50465e1a60566b9ea1ee25b57bd356f159a59" => :arm64_big_sur
     sha256 "fc5398cdb8a918f3e0153c3467074145e69112fd42866c3dc8fee5a66869e8da" => :catalina
     sha256 "7f6d20cc6f1d7f23834614f2f159d8438732e31bb3375c380d64e8dd010232c1" => :mojave
     sha256 "bc725f791779a9400ab2b11d485ebba7e71aced9e6cef3365db614dbb273205c" => :high_sierra
