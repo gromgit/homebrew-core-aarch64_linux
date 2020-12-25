64c7ecd8391030b8b25227331df3019337edbf9a
FX Coudert <fxcoudert@gmail.com>
2020-12-25 06:08:30 +0000

    sic: update 1.2 bottle.

--- Formula/sic.rb
+++ Formula/sic.rb
@@ -9,6 +9,7 @@ class Sic < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "2c4ab579805b755bf890003ebf105f1af4963890c0d491f221233a365e5cd233" => :big_sur
+    sha256 "7547b24c7e3e905ceb4b7b774b9ca7d2c165bd35ad2d4c7cee3908c83c19ed06" => :arm64_big_sur
     sha256 "e8f0a94363bdaebc692584e6a0d2782f88238a9cb4b7920ec6393dcf87d171d8" => :catalina
     sha256 "2c50dd89e57fa0764576417365933792e7599dfb8899ec75957be0fb6d46dd5a" => :mojave
     sha256 "f7e19c7d87f5f13e736edcf7f8cb821b4b644f78208c87f2f6655e5b7541abcc" => :high_sierra
