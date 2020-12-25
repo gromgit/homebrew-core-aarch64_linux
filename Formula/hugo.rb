3b3b8fb2049b9fcbfec9ecc49c5c9f7fe1a47da4
FX Coudert <fxcoudert@gmail.com>
2020-12-25 23:25:17 +0000

    hugo: update 0.79.1 bottle.

--- Formula/hugo.rb
+++ Formula/hugo.rb
@@ -9,6 +9,7 @@ class Hugo < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "276cb57cf2e4d0a3ff1a7160043578d8c743d2b35f6aa7777b6772641acb0cf5" => :big_sur
+    sha256 "650beb3d01c5f6e03f33387573e3e7d3ec390aa61e86cb9e2dad964256d89e0a" => :arm64_big_sur
     sha256 "9a177a3e54867523f10c44522cda5c3666fd80cbdfb4238d0ca0fc2bf9684ac7" => :catalina
     sha256 "afee3eeab8930928a9b7656f09826dc2be8dbcd112558a40ca08f304f3e07e00" => :mojave
   end
