ead6e0ff5f0735e924b959b805e9a79af08841f5
FX Coudert <fxcoudert@gmail.com>
2020-12-25 00:20:00 +0000

    yank: update 1.2.0 bottle.

--- Formula/yank.rb
+++ Formula/yank.rb
@@ -8,6 +8,7 @@ class Yank < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "df2425ac44c54c3e55aaf835a40d2ac74663193550ea32a09f88f23490e1f7d9" => :big_sur
+    sha256 "518e0acdc9c56266b1666b5d4ef54367a8c9cd104826f0499b414d4158c540c9" => :arm64_big_sur
     sha256 "ef4da54ce9a56a1767b44dd88df6616c147730d74f390d5a661910dddf8785a7" => :catalina
     sha256 "60431f02c576c640597975986ce62f9d157c49f160d7d6e23f917dc321ca8bac" => :mojave
     sha256 "b87461e809f0bebd615d4da69c31509109de8f86d07d280dab07326293cc851f" => :high_sierra
