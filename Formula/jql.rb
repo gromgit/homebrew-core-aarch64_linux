e26adb1836c2f8bb079c1af26eca77628266de05
FX Coudert <fxcoudert@gmail.com>
2020-12-25 11:25:15 +0000

    jql: update 2.8.3 bottle.

--- Formula/jql.rb
+++ Formula/jql.rb
@@ -8,9 +8,9 @@ class Jql < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "46588e489c987d107a9c089673a80de31972f83e879566d8064ba09afa2d4e06" => :big_sur
-    sha256 "98aa864e495a17e4746304d6d8ba602cb9c3ffebde12dc6937bbe9e576a8a51a" => :catalina
-    sha256 "9cf16356d69554077542a655e458fb449f15600a13a09a5227c5e9cc1fe3001d" => :mojave
+    sha256 "ef2ef62525d88d33ca05ee66c76f742988bab7e515dd7b61a5838788eccab117" => :big_sur
+    sha256 "df9bd45f44731ca4033ed42e27c65324afe68ec4d16539ae90075df3d77e03c7" => :catalina
+    sha256 "6b7303e254e7dfde4619fba4df69688f003b45346d75830e64b6a3d8c875032e" => :mojave
   end
 
   depends_on "rust" => :build
