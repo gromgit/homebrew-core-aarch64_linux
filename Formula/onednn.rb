9401525aaafce4c1f71d586b333e74be994317ee
FX Coudert <fxcoudert@gmail.com>
2020-12-25 01:58:15 +0000

    onednn: update 2.0 bottle.

--- Formula/onednn.rb
+++ Formula/onednn.rb
@@ -9,6 +9,7 @@ class Onednn < Formula
   bottle do
     cellar :any
     sha256 "d17e096d72bd885c7cac190adfc750e482a7e65d44886f73481755792198d5ea" => :big_sur
+    sha256 "51e627c893ce04790d65e625dc2acd3014cd69020bfdb89f3401848283988a49" => :arm64_big_sur
     sha256 "b4fa356473727ddf9506f7705739237c397b7c967df761a5b80cebfed256d648" => :catalina
     sha256 "defca13c472154c8faa951b61476964965824c07baa3ab26419e4d5822a060b0" => :mojave
   end
