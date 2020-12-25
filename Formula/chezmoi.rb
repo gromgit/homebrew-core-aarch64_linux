27631bb22e5a4cb302c388455481dd2a303ecdaa
FX Coudert <fxcoudert@gmail.com>
2020-12-25 22:28:14 +0000

    chezmoi: update 1.8.10 bottle.

--- Formula/chezmoi.rb
+++ Formula/chezmoi.rb
@@ -9,6 +9,7 @@ class Chezmoi < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "3119ebdb1123a77c18fb6d4429bb4f747fcbce8a79f81b45238504ee97bbccf7" => :big_sur
+    sha256 "f0c06a684a52e886e70fbb9dbae0a2152597a0fcc5d13e90f0ee0e04b0cbaa70" => :arm64_big_sur
     sha256 "6b01e98eec925df06c02cb47ee33136d340d9410570862a69de7a5aa8b820968" => :catalina
     sha256 "369c634fdc904be8d9c9af0ce2afe6adb7f90f8c43a15fc54a2969d2115715eb" => :mojave
   end
