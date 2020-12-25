a657b4197d2429b17dd98afda1a3c2ff7dd67a1d
FX Coudert <fxcoudert@gmail.com>
2020-12-25 22:52:41 +0000

    fleet-cli: update 0.3.1 bottle.

--- Formula/fleet-cli.rb
+++ Formula/fleet-cli.rb
@@ -14,6 +14,7 @@ class FleetCli < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "695b73ebc4a6f540b58351a3bf9478031db796ba3ccafa1c0b54231f79d8e5c1" => :big_sur
+    sha256 "18e64d168412856226909f57ea88d844399b79ee16a912b56b624260579c2c21" => :arm64_big_sur
     sha256 "3b67e8efdd84a72182347386c79b28bb792355919ee4e46ad9643f25d37e0b7f" => :catalina
     sha256 "473658446ff24a8873aee2de76d805c1285c7ba62eebd7374c6d339c8f028e02" => :mojave
     sha256 "f43939a331854c0e19236f2b441bb6f68987d0552f57747893e36811e2e9529a" => :high_sierra
