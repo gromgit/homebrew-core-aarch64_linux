0bf11a23d0a42e2e0035e14c15a82ef29b5908c0
FX Coudert <fxcoudert@gmail.com>
2020-12-25 22:23:07 +0000

    cadence: update 0.12.0 bottle.

--- Formula/cadence.rb
+++ Formula/cadence.rb
@@ -9,6 +9,7 @@ class Cadence < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "407f65fe6bdf23ab0868f3af42057c1b06c442f34c322714e8c08cf4cdaf43d9" => :big_sur
+    sha256 "64ee759fa792485b5e241e3a324999852ef7e2236e77af9f5101a93aaa5e03cb" => :arm64_big_sur
     sha256 "7f851ffe33b63403fdf56242c39d719e7451ebf6d8766935c9d4caad30f36a21" => :catalina
     sha256 "8fe846d2d7e85e7a3e65f8d9180f8a37bd5291fead5c7275e2b1f23280ed6c4e" => :mojave
   end
