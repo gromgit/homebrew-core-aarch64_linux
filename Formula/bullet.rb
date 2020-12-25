46417f575cacc3ce5dcee928e0e0cb729a575562
FX Coudert <fxcoudert@gmail.com>
2020-12-25 11:13:00 +0000

    bullet: update 3.08 bottle.

--- Formula/bullet.rb
+++ Formula/bullet.rb
@@ -8,6 +8,7 @@ class Bullet < Formula
 
   bottle do
     sha256 "400f206997a5e1992075bf02574372e7b11393b45472092854fa226f059e367a" => :big_sur
+    sha256 "2d84bf5c5c4066ace5c0a020afd59dea6fbebb5a2bed1b96ec7d660d35a4315b" => :arm64_big_sur
     sha256 "a2ce4674ecc39e802606a82b28897a28c9e03651e4633a38f43d5c09f7b90a6c" => :catalina
     sha256 "80769ee5343d4df206d4e01e29a68c3e3d8e4b72ab1e644e1cdf8509da14a7cf" => :mojave
   end
