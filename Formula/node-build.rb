ed6d0d6104fbbc3dd8563ea52b1d8f233b2b6e48
nodenv machine <68817010+nodenv-bot@users.noreply.github.com>
2020-12-25 08:19:32 -0500

    node-build 4.9.24 (#67685)
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/node-build.rb
+++ Formula/node-build.rb
@@ -1,8 +1,8 @@
 class NodeBuild < Formula
   desc "Install NodeJS versions"
   homepage "https://github.com/nodenv/node-build"
-  url "https://github.com/nodenv/node-build/archive/v4.9.23.tar.gz"
-  sha256 "415dbbfacd63c3d5ae732e2caa0fdfb496fc676090c2179f88327e58646c2aba"
+  url "https://github.com/nodenv/node-build/archive/v4.9.24.tar.gz"
+  sha256 "f632987709d39175d6cc2f335e916b8302b9a47e6096b282444839f4497e2b2c"
   license "MIT"
   head "https://github.com/nodenv/node-build.git"
 
