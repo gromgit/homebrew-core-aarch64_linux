a5962a20d342b57492c70029e3fd00c90bd8a050
FX Coudert <fxcoudert@gmail.com>
2020-12-25 06:28:27 +0000

    gloox: update 1.0.24 bottle.

--- Formula/gloox.rb
+++ Formula/gloox.rb
@@ -12,6 +12,7 @@ class Gloox < Formula
   bottle do
     cellar :any
     sha256 "143b0a77842e9df885799b07e6cfb166c7951841b8b23027db51c9b93a5ba8a7" => :big_sur
+    sha256 "d3a7c4aa4b061fe4dce520662e632bc9983b294cb94e479ea46221f43ea46c9f" => :arm64_big_sur
     sha256 "decf46b20a794855cf3bd3c06e05111d15fd11de4dec1c5fdf6a1253eb865e7a" => :catalina
     sha256 "607baeeadc43775af6799d5bc4715239cbe6455ec72d2e14d82523d425fa7799" => :mojave
     sha256 "011c8a88d0f8970c9ad4ed339972b55b56c26120e64ef9d1576b68c03b10f706" => :high_sierra
