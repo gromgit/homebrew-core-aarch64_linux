e85456205326a54ec845a04d1945bc9b36d78964
FX Coudert <fxcoudert@gmail.com>
2020-12-25 22:50:25 +0000

    flarectl: update 0.13.6 bottle.

--- Formula/flarectl.rb
+++ Formula/flarectl.rb
@@ -9,6 +9,7 @@ class Flarectl < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "7fb5f3992995885604593c9de36aaf5805c95444736591bd0c0ede7f2d7639a4" => :big_sur
+    sha256 "bc341c6a8d40b7f0b71612decc945a793aff97d70efbc32d290943d5f8d127f9" => :arm64_big_sur
     sha256 "4d0a4d3bb0a33b393a44b992ea0e595d7898da141c0ac1cff578c465fbb80dec" => :catalina
     sha256 "a675fcfb7def5e459c4b83b288a4f8fc23984e81cafeefa5d209b6e0ba29fdf6" => :mojave
   end
