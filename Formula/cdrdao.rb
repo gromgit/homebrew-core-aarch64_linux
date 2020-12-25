9cc54c97518ff44b236722c75556af91859a4716
FX Coudert <fxcoudert@gmail.com>
2020-12-25 00:38:46 +0000

    cdrdao: update 1.2.4 bottle.

--- Formula/cdrdao.rb
+++ Formula/cdrdao.rb
@@ -11,6 +11,7 @@ class Cdrdao < Formula
 
   bottle do
     sha256 "1a2440998c344f7b1df9d01d6e3079f86fbc79e8827c440883fda7f8e12aa2fd" => :big_sur
+    sha256 "29b520f278e11f6742704d93aa391c44dc5cb386f04610fefb07a787fbcf0595" => :arm64_big_sur
     sha256 "81acc38e0a51134c0eeebf20e76dcee4e80eabfac72f0ec890e448271a96792c" => :catalina
     sha256 "f8894deccbd18e7d5362ace73618666d9a79b233cea5dc6af367ab9e257332e0" => :mojave
     sha256 "1efaa356872419da65763a5e28faf262b79f5a37e2eb83c06c22e9846bae188f" => :high_sierra
