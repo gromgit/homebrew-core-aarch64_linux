c2b1db1e8432f0b600192884b8afc0164146c100
FX Coudert <fxcoudert@gmail.com>
2020-12-25 20:54:33 +0000

    python@3.9: update 3.9.1_2 bottle.

--- Formula/python@3.9.rb
+++ Formula/python@3.9.rb
@@ -12,10 +12,9 @@ class PythonAT39 < Formula
   end
 
   bottle do
-    sha256 "1c4b459f6c4929f723275620e867cb600805c5590f2475ec91a7ee2675a69e27" => :big_sur
-    sha256 "7259620e42bf1fac17e2ddc3ef64467f448518b8368e7113304bdbde8cfc1011" => :arm64_big_sur
-    sha256 "367924e7be7a76f966acf68c0d909fb34cf8c22959a71769a6838c9bb35fbe87" => :catalina
-    sha256 "502d5df2a1d26d52e55f1e26e19408786d55275a67dd06481bb5622646ed7d99" => :mojave
+    sha256 "efbd809c802a77edc64c4cce04567b759b0a5a083bb9602d9b9a97fe707f2a2d" => :big_sur
+    sha256 "bf8ef81a05a9790968de7b50dc1f9c5c0567a5872f23bbf11d2098cc68d0810d" => :catalina
+    sha256 "fc29320dc4807d2aaa70e559d434b44805ef55b72f91cfb18e8afc1e84eafe86" => :mojave
   end
 
   # setuptools remembers the build flags python is built with and uses them to
