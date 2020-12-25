95ba42992cb2ac82a27546786f9f7bf635422e40
FX Coudert <fxcoudert@gmail.com>
2020-12-25 22:56:52 +0000

    gauge: update 1.1.6 bottle.

--- Formula/gauge.rb
+++ Formula/gauge.rb
@@ -9,6 +9,7 @@ class Gauge < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "068904d828569f25fdac3295d1bf2a5f2b40cd2b62078609b6bc2c41a67508f1" => :big_sur
+    sha256 "2a5fe6fbc2ae6f753ef19df7886a71570770958558964acbabb4bbbc31eb2df7" => :arm64_big_sur
     sha256 "5584c9c4b970fd3b694a58c2ca5d952a170c127f10d0a0d9bed335a9527dc6e9" => :catalina
     sha256 "478cb19879b903805aed5820fa098a993867fd1870376581ca31b1ef2c35c5a4" => :mojave
   end
