8d08415e1dccfdbf0ddcd6d0139deadf28578668
FX Coudert <fxcoudert@gmail.com>
2020-12-25 02:46:13 +0000

    clib: update 2.5.0 bottle.

--- Formula/clib.rb
+++ Formula/clib.rb
@@ -14,6 +14,7 @@ class Clib < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "5c09d1b21de092b57a9fb16170dba836e5f4df51b9a6a47302aa4f2f90b565a0" => :big_sur
+    sha256 "498960fdece8741fdb2e650d5d8a4931666402ca1229a73c93934bf9cd48455c" => :arm64_big_sur
     sha256 "e6f3ca8fbc6b39325af968a81e8f09045de75e90dae2d6db3a5a6e72afee2766" => :catalina
     sha256 "acebab1f84196d10519777630a1dec10858f2729208c111094bdaf49a11b77bb" => :mojave
   end
