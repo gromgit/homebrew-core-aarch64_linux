142dbd5ec0648162544fd41bac7466fde578091b
FX Coudert <fxcoudert@gmail.com>
2020-12-25 23:26:16 +0000

    jd: update 1.2.0 bottle.

--- Formula/jd.rb
+++ Formula/jd.rb
@@ -8,6 +8,7 @@ class Jd < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "881c3988d2ff749b6b98008f025f69d377b6750b48c8fe7746830ebab26cb979" => :big_sur
+    sha256 "e2803ce038346619f7007e08c9ac922b79bb2aaba4a47a65eb164264bfd39e4a" => :arm64_big_sur
     sha256 "cb706ba25f462bd0f8c32b3373022d1fe2bb9416b709c9f2b5d3d2b5c8c8bd64" => :catalina
     sha256 "a3c5ff305e4e52d31cd410dc6b88efaea8a758637cf96e997b95be2d8a049abd" => :mojave
   end
