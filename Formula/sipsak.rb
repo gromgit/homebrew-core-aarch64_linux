9c4624dc7574349b0af830f53641440033b21d36
FX Coudert <fxcoudert@gmail.com>
2020-12-25 16:59:00 +0000

    sipsak: update 0.9.8 bottle.

--- Formula/sipsak.rb
+++ Formula/sipsak.rb
@@ -8,6 +8,7 @@ class Sipsak < Formula
   bottle do
     cellar :any
     sha256 "7386f87aa1a38a6004a2bccb2f1a60f508d0c8f01f9021cf88b2938223de11d9" => :big_sur
+    sha256 "492a1eb36a775220599efe23c9a4d49037cf49f8a3f4c1e8b9df4dce72c8b49e" => :arm64_big_sur
     sha256 "47c8709eafb9b97b72b22c1d01ac0e9686de5ebc2c1c57cc29c72d7bdadda2b7" => :catalina
     sha256 "7a3693652e8a56ea265a73fbc26044ee9b51f076233c154b40618bf6e8cd5c42" => :mojave
   end
