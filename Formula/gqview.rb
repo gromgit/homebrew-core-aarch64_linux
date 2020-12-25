c8a50aeeb6539a9de92408683bb3ae8e3d734ff5
FX Coudert <fxcoudert@gmail.com>
2020-12-25 17:42:26 +0000

    gqview: update 2.0.4_3 bottle.

--- Formula/gqview.rb
+++ Formula/gqview.rb
@@ -14,6 +14,7 @@ class Gqview < Formula
 
   bottle do
     sha256 "34819384f6dd734a0000543eed0865a48ad9b218d9bbd0662b64cf2edc4cd3c4" => :big_sur
+    sha256 "9b80b415172fc45373bcb1c68b03b3c0c26e6181e3c5ff4e353c7b685fbf6524" => :arm64_big_sur
     sha256 "e8e56389d265444d10d7859b63736370c2b88b98d4f8b4254bdecf2f3b7c8ab4" => :catalina
     sha256 "dc9cc0efc66c0e2156efeba84201c54711288e96868367bde264dbfaff14236f" => :mojave
     sha256 "faeb25a25899fc5d18b2097574c3975648aaab4b8a55545e5ba6579335c2f587" => :high_sierra
