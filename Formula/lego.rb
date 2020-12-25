afa02513a42838dcf64494bdea249b58d24ee1ce
FX Coudert <fxcoudert@gmail.com>
2020-12-25 23:44:02 +0000

    lego: update 4.1.3 bottle.

--- Formula/lego.rb
+++ Formula/lego.rb
@@ -9,6 +9,7 @@ class Lego < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "b99349308a1af2cc963b3c825cb0fefc37ea21b7ea98497c2e9e49f1c9329bd6" => :big_sur
+    sha256 "f5023447b0ac0ae205c301e961b2199a748fdd5a8b78381822b4b54e3a2925a5" => :arm64_big_sur
     sha256 "20a37f8e5ccbd6f40908a2d60fd2c4f41aadfd5fc65b861174c181e908c60320" => :catalina
     sha256 "fc8c01b9e2e5cefcbf9f3f0d002c534d1a7c320b4e7a53849bdcd01fd4f5e4a2" => :mojave
   end
