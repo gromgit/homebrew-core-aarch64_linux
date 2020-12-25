dcb5034b39673c2e3e5143908068296e95f21e31
FX Coudert <fxcoudert@gmail.com>
2020-12-25 23:03:49 +0000

    glab: update 1.12.1 bottle.

--- Formula/glab.rb
+++ Formula/glab.rb
@@ -9,6 +9,7 @@ class Glab < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "067a8bb9cfaa93259199eccd5a547305c24580de4d379c35ce530d6431342c9f" => :big_sur
+    sha256 "046533288aab0f06c1df69d187e4896857c64bb9d9596a1512e8f625f3ce7a8d" => :arm64_big_sur
     sha256 "8772f1729514811539f8f52bb42101205e1a1bcb1fa3afc49f0196476c4ed3b7" => :catalina
     sha256 "e3a24235b21b7f5e929aee5125ba571a84452233337eda85ff03393b82947cf1" => :mojave
   end
