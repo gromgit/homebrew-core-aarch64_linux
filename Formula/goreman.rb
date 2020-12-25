450e8580590a3b873e6c8566203bc02ac097935c
FX Coudert <fxcoudert@gmail.com>
2020-12-25 23:11:23 +0000

    goreman: update 0.3.7 bottle.

--- Formula/goreman.rb
+++ Formula/goreman.rb
@@ -14,6 +14,7 @@ class Goreman < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "7935ca50f9a9c6fc0caceb81d64a6439aab37c61fb75edbbff99729e3542568b" => :big_sur
+    sha256 "b23fc772de996cff73c26a93ba73c826b5f7e56929f0c717c9388066bb689067" => :arm64_big_sur
     sha256 "8985d410d3b9c56064ceb7a01be4fd448e46c414f0a0b8c3a4f6ec7374c2f5b6" => :catalina
     sha256 "d7781e6ce9c1ab5844f06d77dbbb8355a5f749daa5cd3c2b12266385d73b9a77" => :mojave
     sha256 "df59dbb8a079d4eaf095b7a807dcbd0a96de11874dec3b6e560454617eed9b2b" => :high_sierra
