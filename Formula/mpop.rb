d0955925faf7b0f09dd59c0bfac8fb70ff2387d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-25 12:26:02 +0000

    mpop: update 1.4.12 bottle.

--- Formula/mpop.rb
+++ Formula/mpop.rb
@@ -6,11 +6,9 @@ class Mpop < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 "e95b6c6b714ae9ed4d88e084e42ba46b7e86b608cc4a6eac7f9e65a5116fcf92" => :big_sur
-    sha256 "749fefcde3a5064b1f9a35d25af6eb1709bd0059bcfa943d043500bf52d276c8" => :arm64_big_sur
-    sha256 "c1d09e98b098f34e6552280fa575430faa97456c075708465588ded47b1dfc0b" => :catalina
-    sha256 "0ddab1250be82811b51619ace9638303c9576fc32b0b4c3c8b9f19e2cee4ab51" => :mojave
-    sha256 "c92742ff97f25590c21b64c2a19ce47dae7b2d1ce405f6d50a3e086ee8eba081" => :high_sierra
+    sha256 "77c41a741e2a15b06cbcdb58f662ce5766b0e54f65bd096b0bceb5eab3ce3420" => :big_sur
+    sha256 "7cfa5ab2e69e875b11145a0ad3bfe4a372b522cb5ecb8d81738f67e619aa5eeb" => :catalina
+    sha256 "58689c559173e5e3a15cdffbd055f4a6466ebcba2278d1492a26e86cc19ce244" => :mojave
   end
 
   depends_on "pkg-config" => :build
