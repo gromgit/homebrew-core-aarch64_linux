520b46311ec2e216d1685debb7ac64f06958f173
FX Coudert <fxcoudert@gmail.com>
2020-12-25 23:01:45 +0000

    git-lfs: update 2.13.1 bottle.

--- Formula/git-lfs.rb
+++ Formula/git-lfs.rb
@@ -8,6 +8,7 @@ class GitLfs < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "ebc56da970278c2f7fd44a73d855c8bdbbb27ecb3526b0e44e59124301300b0b" => :big_sur
+    sha256 "8187fc30c663ba60b09ade690c90fcf4e842050ba7f72969c53c46a03f29a17e" => :arm64_big_sur
     sha256 "069c9abf9aae2690f9f30ddea080d21aeb78e8cecbceee843d8eefe6e2248551" => :catalina
     sha256 "a9d3e9326f96e0684eaed9fd9fd992dd9ad9f8e1ff01630d9c73ababe72b654f" => :mojave
   end
