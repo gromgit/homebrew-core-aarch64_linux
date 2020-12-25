3711d9768a0ba60afcc3a3dd97b579df782ef4c1
chenrui <chenrui333@gmail.com>
2020-12-25 21:30:07 +0000

    notmuch: update 0.31.3 bottle.

--- Formula/notmuch.rb
+++ Formula/notmuch.rb
@@ -13,11 +13,9 @@ class Notmuch < Formula
 
   bottle do
     cellar :any
-    sha256 "86205549d8971bcc17798ef64d43853a96c180f1997e6d991ad106c654f00eb3" => :big_sur
-    sha256 "77831c5c770e9a68678e822b2959c76724e44f3b6215f5b31f5293f68ac05c42" => :arm64_big_sur
-    sha256 "33019e1569b78dc2ac4f72f6f901b05526d4678a2310f931609943f0f578c22b" => :catalina
-    sha256 "feef25a27ef288c9506b51b1c3f5482ccf29b98e4341f89bdeccb4452d8fd07b" => :mojave
-    sha256 "650579d43babe2435ba190a8cce066f1a3f6621ba840d7ea9a961b0f084ae589" => :high_sierra
+    sha256 "9bd204d311516cb411413209cfb264d05c654b7436ee89bb3822b2808ba2a5a2" => :big_sur
+    sha256 "9f08fa00d635ecafc717d500dff1aa03756bccd70ac5bde856d14487e52252e5" => :catalina
+    sha256 "b97c6eeda07929f0645bfc4c9d0c5604c1581a1136ec93c936f10ef607f7b5cf" => :mojave
   end
 
   depends_on "doxygen" => :build
