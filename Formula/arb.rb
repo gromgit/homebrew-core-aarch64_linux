9d724df62b2213c0f46f0ea37be38510d25deab9
FX Coudert <fxcoudert@gmail.com>
2020-12-25 16:56:30 +0000

    arb: update 2.19.0_1 bottle.

--- Formula/arb.rb
+++ Formula/arb.rb
@@ -10,6 +10,7 @@ class Arb < Formula
   bottle do
     cellar :any
     sha256 "0e09a51868eb29ac963d7a05de8d69582b84fcaa0daeda65d36a4836b3e8ce7a" => :big_sur
+    sha256 "d18804a179a6ea97014edb435487532e308fd3418250b0de06306e810c10e863" => :arm64_big_sur
     sha256 "735bd3b8dc3f89429d51b1fed7b0a651176d2816402eb5c88b7e7a33709d723f" => :catalina
     sha256 "e2001e15a3cab1166c517a366269ad88258748cd8373253f071450d433c7f8cb" => :mojave
   end
