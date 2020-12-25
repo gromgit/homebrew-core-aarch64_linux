5e85e2114fe2c982f73966d70e9f7c57f285616b
FX Coudert <fxcoudert@gmail.com>
2020-12-25 23:20:09 +0000

    hostess: update 0.5.2 bottle.

--- Formula/hostess.rb
+++ Formula/hostess.rb
@@ -10,6 +10,7 @@ class Hostess < Formula
     cellar :any_skip_relocation
     rebuild 1
     sha256 "addb5bc6b7ff84ad6d2a33f2e0c46298f16865473ad82a32c02434def068c26b" => :big_sur
+    sha256 "4272e75f8cba2d02e038dca00f7620a70ca396f3486aaf57a6a9fde77645c562" => :arm64_big_sur
     sha256 "9386f4841bb16ea44d5131b0a360138a3d33d7595e85d0baba3b9546762d7ae6" => :catalina
     sha256 "90e6b36f4131f2e8a914cd81e6a17e59075e734fa83b583654e178c6e7e65aa3" => :mojave
   end
