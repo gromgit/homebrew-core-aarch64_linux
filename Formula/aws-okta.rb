27de56d6e071e1cfcf6c8670b1c9538c488484cc
FX Coudert <fxcoudert@gmail.com>
2020-12-25 22:18:50 +0000

    aws-okta: update 1.0.9 bottle.

--- Formula/aws-okta.rb
+++ Formula/aws-okta.rb
@@ -8,6 +8,7 @@ class AwsOkta < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "e1aa60743bc643a46d18ba5d7d771a95801a315eb24fe36d88b0456adbd9d114" => :big_sur
+    sha256 "f4120a9e99a77029ec99b86459e2a786a06a317ed815d88ba6ce0bfd1be776ca" => :arm64_big_sur
     sha256 "2cdea0519af672242bc8646bf0532ebbe9457434486111053fd6443763546ad8" => :catalina
     sha256 "01c36d36db51311dc275cce9b1bddb4b24fc729a9d3747cb4dbd9fae635ab6b4" => :mojave
   end
