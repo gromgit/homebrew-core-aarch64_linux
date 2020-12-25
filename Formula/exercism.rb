8f272033535ff57bb2fcedabe65851055a4daf49
FX Coudert <fxcoudert@gmail.com>
2020-12-25 22:48:43 +0000

    exercism: update 3.0.13 bottle.

--- Formula/exercism.rb
+++ Formula/exercism.rb
@@ -15,6 +15,7 @@ class Exercism < Formula
     cellar :any_skip_relocation
     rebuild 2
     sha256 "2b67328f03633996542bda37f25a7cf84e74732445cb89e64d2e3ae1fdf07b9e" => :big_sur
+    sha256 "b67c57c567b36681731de3a6fa14fcc163aba8d063432dc3fd4fe9866ba7dfb4" => :arm64_big_sur
     sha256 "9a4080f7e35f37dc4eb15e733692314cec32cba7e0f76e8f58eb99850f708cb1" => :catalina
     sha256 "7319920cfd6779984dfabbecdf3e15a37603f6bfbecfc1121bfa2a044fb8ed17" => :mojave
     sha256 "b094a8441575b02f312f04760589f94d9f2b1d76330c07a67f7d07a40ad561a9" => :high_sierra
