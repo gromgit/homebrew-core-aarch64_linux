f0369942b1dfbf3088c36ef1d49d1d67a4a1e217
FX Coudert <fxcoudert@gmail.com>
2020-12-25 17:38:14 +0000

    fb-client: update 2.1.1_2 bottle.

--- Formula/fb-client.rb
+++ Formula/fb-client.rb
@@ -14,6 +14,7 @@ class FbClient < Formula
   bottle do
     cellar :any
     sha256 "76cab48a5e41ea108da84e1228ddea7c23ee92727206d0e0ef00aa11e65167ae" => :big_sur
+    sha256 "202db4557ba4a133b92ae0d2ced7c1c63e04190bb3a3dac7b8219daa798f8f16" => :arm64_big_sur
     sha256 "0f2e6cd24defedab9ce9b5a843b75a7082592808035f00082008c47f5ba26024" => :catalina
     sha256 "9a7adf6509265cf7c9ae67d68b108685d49e35f44ab00bdfe1c77be073942596" => :mojave
   end
