bf856fbd6b8df39294a5ab1a98fdc07072175e20
Rui Chen <rui@meetup.com>
2020-12-24 23:50:26 -0500

    aida-header: update homepage
    Signed-off-by: Rui Chen <rui@meetup.com>

--- Formula/aida-header.rb
+++ Formula/aida-header.rb
@@ -1,6 +1,6 @@
 class AidaHeader < Formula
   desc "Abstract Interfaces for Data Analysis define interfaces for physics analysis"
-  homepage "https://aida.freehep.org/index.thtml"
+  homepage "https://aida.freehep.org/"
   url "ftp://ftp.slac.stanford.edu/software/freehep/AIDA/v3.2.1/aida-3.2.1-src.tar.gz"
   sha256 "882d351bc09e830ae2eb512a2cbf44af5a82ef8efe31fbe0d047363da8314c81"
   license "LGPL-3.0-or-later"
