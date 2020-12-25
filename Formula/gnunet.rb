a26ab1ac29abc52c59dc0688e9546dd8a54839f7
FX Coudert <fxcoudert@gmail.com>
2020-12-25 17:34:46 +0000

    gnunet: update 0.13.3 bottle.

--- Formula/gnunet.rb
+++ Formula/gnunet.rb
@@ -13,6 +13,7 @@ class Gnunet < Formula
   bottle do
     cellar :any
     sha256 "cea8501f8fa52cbb4b1d1af8628d4c0f09f3f3ae667a45f0445ab79e47c5b255" => :big_sur
+    sha256 "413b9db61db66ea96a9f1cbbb3471c35313ff195c1c1ee10d526e37e59f92fbf" => :arm64_big_sur
     sha256 "1e2b31e022792cc40dcd11061644808f761e378a2178b1bfdd9191a31363ad1e" => :catalina
     sha256 "ef84c90eb8ea9783adbaaa8b7034cb0662d7b697d2e5b7953365bac1681d5277" => :mojave
     sha256 "5470d0894a0ca01ca53d3376e4bbc7ad038621827e7ff1e075e1dd2c07c579f3" => :high_sierra
