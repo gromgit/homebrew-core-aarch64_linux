7abaea0a2681ce7dfbdbb0ef82cc6fca9250dd5e
FX Coudert <fxcoudert@gmail.com>
2020-12-25 01:03:50 +0000

    x3270: update 4.0ga12 bottle.

--- Formula/x3270.rb
+++ Formula/x3270.rb
@@ -12,6 +12,7 @@ class X3270 < Formula
 
   bottle do
     sha256 "6debec33ebab916d38e8f4543e8bc22a18a8e7e1d6474277bea040bd63a52e60" => :big_sur
+    sha256 "1a673289bc4538fd2e1592548562a3fec3c1a789e0e87452dfad68d4f2f1e619" => :arm64_big_sur
     sha256 "910cfb45bab78112705869fd8bb3fac970d0bd84194ab9555635ff484d355bcb" => :catalina
     sha256 "99fbe83bf0dfbfab1f11b536608d02cd93d530eca7f0ec72ebde4d4424293444" => :mojave
     sha256 "e30468e4081beeab927279e9317f30107db6391ac4e8f9f5334d2ecc0e8e57e2" => :high_sierra
