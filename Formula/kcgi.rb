a8e5f5bc28c835a3cedb5899228d5a6f84489a34
FX Coudert <fxcoudert@gmail.com>
2020-12-25 05:49:27 +0000

    kcgi: update 0.12.2 bottle.

--- Formula/kcgi.rb
+++ Formula/kcgi.rb
@@ -8,6 +8,7 @@ class Kcgi < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "db56ea0e40697618474f90af5b3f354043027b837e94464650a81d3813cde3d9" => :big_sur
+    sha256 "6c8f9cb34365dd0fae747d312badca68cb42bfcdddc1ab43e652b40bc01161d6" => :arm64_big_sur
     sha256 "a3190c30830f6667c7f6df30047610e12845835f3d16c647b3c223e7ec8033db" => :catalina
     sha256 "096b70bd684486a61c8c393aea6596268701807e27b41da94c0e5e996bfc7b3c" => :mojave
   end
