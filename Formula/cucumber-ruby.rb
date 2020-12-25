611266791554a15fa567abf896dc6e4fb297916a
Seeker <meaningseeking@protonmail.com>
2020-12-25 11:43:46 -0800

    cucumber-ruby: require brewed ruby on catalina

--- Formula/cucumber-ruby.rb
+++ Formula/cucumber-ruby.rb
@@ -4,6 +4,7 @@ class CucumberRuby < Formula
   url "https://github.com/cucumber/cucumber-ruby/archive/v5.2.0.tar.gz"
   sha256 "273c9d98c5de487d91a6f35f289dd305964611fd01d5080df346bd42f11fffba"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 "58a17b42e9f46239ca24c2afd3c54cadbf2b73dbf0b1f9499af37ac2b4399ace" => :big_sur
@@ -15,7 +16,7 @@ class CucumberRuby < Formula
   depends_on "pkg-config" => :build
 
   uses_from_macos "libffi", since: :catalina
-  uses_from_macos "ruby", since: :catalina
+  uses_from_macos "ruby", since: :big_sur
 
   def install
     ENV["GEM_HOME"] = libexec
