31e016264b13fb608c654e21310d4fe09586930a
FX Coudert <fxcoudert@gmail.com>
2020-12-25 23:12:57 +0000

    goreleaser: update 0.149.0 bottle.

--- Formula/goreleaser.rb
+++ Formula/goreleaser.rb
@@ -9,6 +9,7 @@ class Goreleaser < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "ef98a47f59fa7139fe76678ee085328a19be0a734eeb46197f5d45595f5d7b99" => :big_sur
+    sha256 "6717b2c0b2f94a02e94b8a8c88c970feda84bb136b8e4cf1b92b5d9bab0f9dcd" => :arm64_big_sur
     sha256 "ec932595c140e164728f532fbc31ed7148c8f60bac76db1784cd6f4d0aeeb2aa" => :catalina
     sha256 "78a6e30e81b8cfdcf2251b69bed8478bf53160abc56884e62700f339619072c2" => :mojave
   end
