5783a37fa258c916607e9c01ace1f720b6e4fe27
FX Coudert <fxcoudert@gmail.com>
2020-12-25 23:03:01 +0000

    gitleaks: update 7.2.0 bottle.

--- Formula/gitleaks.rb
+++ Formula/gitleaks.rb
@@ -8,6 +8,7 @@ class Gitleaks < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "aab0bbfe5d3e939cba39a43050c5b6a5ccfc501d3f688cc4d493f830f612726c" => :big_sur
+    sha256 "5ada282781ef2efa9cc6e0edbdca7f638b2d571423be321e41d4d600435af762" => :arm64_big_sur
     sha256 "613da0ab2d60ddd4c80dcead4f4942d43056a5a18d88370190865fceaac8f503" => :catalina
     sha256 "92f09149a962293db4244062e475d949db12a2dcdafad909a788c87c693e1f2b" => :mojave
   end
