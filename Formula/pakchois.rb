068979c365c4eb8988fcb9ab42b7fc06f6432fb3
Mitchell Blank Jr <mitch@bodyfour.com>
2020-12-25 07:36:31 +0000

    pakchois: switch back to normal homepage
    About 2.5 years ago this formula switched its URLs to archive.org.
    We've been having trouble using archive.org for downloading tarballs
    (it likes to sometimes decompress them automatically, breaking sha256)
    However the main webpage seems fine today and is even https now.
    
    Closes #67680.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/pakchois.rb
+++ Formula/pakchois.rb
@@ -1,8 +1,14 @@
 class Pakchois < Formula
   desc "PKCS #11 wrapper library"
-  homepage "https://web.archive.org/web/www.manyfish.co.uk/pakchois/"
-  url "https://web.archive.org/web/20161220165909/www.manyfish.co.uk/pakchois/pakchois-0.4.tar.gz"
+  homepage "https://www.manyfish.co.uk/pakchois/"
+  url "https://www.manyfish.co.uk/pakchois/pakchois-0.4.tar.gz"
   sha256 "d73dc5f235fe98e4d1e8c904f40df1cf8af93204769b97dbb7ef7a4b5b958b9a"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?pakchois[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
 
   bottle do
     cellar :any
