4af43eb06c4ba356d8b06a6359b13082395bcae9
FX Coudert <fxcoudert@gmail.com>
2020-12-25 23:29:17 +0000

    k3sup: update 0.9.13 bottle.

--- Formula/k3sup.rb
+++ Formula/k3sup.rb
@@ -15,6 +15,7 @@ class K3sup < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "b7e0f8b937cb4f02682d9dbd5538a9935d4ddffbfc7bae057d50376b6aff8bd6" => :big_sur
+    sha256 "53f6867bbd187b582c1d6e3e8a3cebfa6b2eba8368564a8d28c3491aefba3b7e" => :arm64_big_sur
     sha256 "1dbdb2ddf6906b4a52d395c55a104fb3b95f57b8be5c30b39e69e241877d3cf7" => :catalina
     sha256 "c6331b1dcc087e01600504993f80a5f6e9f4437779ef336a293ec5ea7349cd4a" => :mojave
   end
