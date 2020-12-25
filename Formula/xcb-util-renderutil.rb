db8cdd5e4b437dc9ad6016c4e7d8c0632eba16af
FX Coudert <fxcoudert@gmail.com>
2020-12-25 05:06:21 +0000

    xcb-util-renderutil: update 0.3.9 bottle.

--- Formula/xcb-util-renderutil.rb
+++ Formula/xcb-util-renderutil.rb
@@ -8,6 +8,7 @@ class XcbUtilRenderutil < Formula
   bottle do
     cellar :any
     sha256 "0941200260ef409b5daa61664cad100fe69b08e99b8cb440297079387e2dadff" => :big_sur
+    sha256 "7091c73aa3571aa8c4cc2568175f91b3dffe7714dbb1ab334c86da174395d2e1" => :arm64_big_sur
     sha256 "5fb7ef030a443af89504e74d04fccf3000ac04bf152798e7d4242247e2378ae2" => :catalina
     sha256 "b0a2c992673650129ee49fcb4fbe6873ef4b8d29a5677ae873a27e05fc7a0d27" => :mojave
   end
