5444a3bbc09552b4efbf8936734204b08acc0ee0
FX Coudert <fxcoudert@gmail.com>
2020-12-25 00:09:14 +0000

    etl: update 1.4.0 bottle.

--- Formula/etl.rb
+++ Formula/etl.rb
@@ -14,6 +14,7 @@ class Etl < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "3e50fc4f2e8a00bdb55f276839b7bebfde795c6da8a170395baa932009f1891c" => :big_sur
+    sha256 "8918bb95db8660e8b30ac2d0d508fc35272e5dea1585bc1c057cfd0cfd4cceb2" => :arm64_big_sur
     sha256 "36399b703008be7d253bbbd1313c22929982319d3e7e52dbeb92a1acbc554cb4" => :catalina
     sha256 "3499623804687865757dec0f5df9ae2b8c70ed8d8c8c6cfa2e8bd6bf839b55db" => :mojave
   end
