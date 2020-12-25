3205250b454f50e27138b94f515aeda96dc73b06
FX Coudert <fxcoudert@gmail.com>
2020-12-25 15:25:39 +0000

    lilv: update 0.24.10 bottle.

--- Formula/lilv.rb
+++ Formula/lilv.rb
@@ -8,6 +8,7 @@ class Lilv < Formula
   bottle do
     cellar :any
     sha256 "ab87185c1c9276cf74a6c060a6f9d7f32f3012e581da421277d9851d860cc921" => :big_sur
+    sha256 "536270c05516a916e48c80f79ffe0afdfbbb7a30558708c7692a9080707693f6" => :arm64_big_sur
     sha256 "60284df0084d62e48620065570b21aa97fcbb0f03a19cb4cb6b94e6dcec822b3" => :catalina
     sha256 "c1608ba36ea14c920193a418bb8d5cf101c4a4f4e12b59a84b52a87ef993329e" => :mojave
   end
