610ac96e497692191c1cc7e229b5482fd0d7dcad
FX Coudert <fxcoudert@gmail.com>
2020-12-25 23:10:53 +0000

    gopls: update 0.6.1 bottle.

--- Formula/gopls.rb
+++ Formula/gopls.rb
@@ -14,6 +14,7 @@ class Gopls < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "a4f60e4c2e8a70aae414995319bb6b4dc5dfe4295a5d105fe0b0fcf84a02f5a9" => :big_sur
+    sha256 "38711952665cce5d9e6bf606c6ad08dba09f7ebef09f84ea6a35fb2a0a248b4c" => :arm64_big_sur
     sha256 "a4d25c188943c90cd324b052cd43ab4560328051249f4be410dc9c5903e5b8c7" => :catalina
     sha256 "1d79c7f315bf4e0737036293faf6c70af25d7162c463e0969915bf5c8d08d12c" => :mojave
   end
