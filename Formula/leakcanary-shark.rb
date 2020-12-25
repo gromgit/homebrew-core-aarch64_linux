1b980b79c67d0dc2fe4456cad5ac99e3eca92759
chenrui <chenrui333@gmail.com>
2020-12-25 15:37:21 -0500

    leakcanary-shark 2.6 (#67693)
    * leakcanary-shark 2.6
    
    * leakcanary-shark: update test source url
    
    * leakcanary-shark: fix build
    
    Signed-off-by: Rui Chen <rui@meetup.com>
    
    * fix code style

--- Formula/leakcanary-shark.rb
+++ Formula/leakcanary-shark.rb
@@ -1,8 +1,8 @@
 class LeakcanaryShark < Formula
   desc "CLI Java memory leak explorer for LeakCanary"
   homepage "https://square.github.io/leakcanary/shark/"
-  url "https://github.com/square/leakcanary/releases/download/v2.5/shark-cli-2.5.zip"
-  sha256 "23154247a6020143ccbb143d426f61fffbd9b316ed50acfe671264d27836201c"
+  url "https://github.com/square/leakcanary/releases/download/v2.6/shark-cli-2.6.zip"
+  sha256 "03c1f1f8e9a3e9114d7da70d244ff534e27f895a422fd1c5a17b7722e825e75b"
   license "Apache-2.0"
 
   bottle :unneeded
@@ -10,16 +10,16 @@ class LeakcanaryShark < Formula
   depends_on "openjdk"
 
   resource "sample_hprof" do
-    url "https://github.com/square/leakcanary/raw/v2.2/shark-android/src/test/resources/leak_asynctask_m.hprof"
+    url "https://github.com/square/leakcanary/raw/v2.6/shark-android/src/test/resources/leak_asynctask_m.hprof"
     sha256 "7575158108b701e0f7233bc208decc243e173c75357bf0be9231a1dcb5b212ab"
   end
 
   def install
     # Remove Windows scripts
-    rm_rf Dir["bin/*.bat"]
+    rm_f Dir["bin/*.bat"]
 
     libexec.install Dir["*"]
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    (bin/"shark-cli").write_env_script libexec/"bin/shark-cli", Language::Java.overridable_java_home_env
   end
 
   test do
