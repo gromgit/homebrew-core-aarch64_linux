6d17bf709fa26fad120ed5dda5f5f250a053cd24
FX Coudert <fxcoudert@gmail.com>
2020-12-25 23:31:54 +0000

    k9s: update 0.24.2 bottle.

--- Formula/k9s.rb
+++ Formula/k9s.rb
@@ -9,6 +9,7 @@ class K9s < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "11eea882d339e31c60c8e3a18ee1d1f4d139896fabb812d87678a63b6d803be7" => :big_sur
+    sha256 "ed3986986c2b2ed043008ec07abf8aec0f9d778ae4a5129cfdbf07e975d6cc15" => :arm64_big_sur
     sha256 "f79446010fd169eb3177e37542116e1197f85010394cca9e146c8088a8cfdab2" => :catalina
     sha256 "975c7f0640f603439df7280e9cd836be0b7fc4dfb27d5c728c8084bf4eea2376" => :mojave
   end
