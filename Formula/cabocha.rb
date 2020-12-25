ab1292b8298486fbda0ce254b6f0d38cca64a2cb
FX Coudert <fxcoudert@gmail.com>
2020-12-25 01:51:11 +0000

    cabocha: update 0.69 bottle.

--- Formula/cabocha.rb
+++ Formula/cabocha.rb
@@ -8,6 +8,7 @@ class Cabocha < Formula
 
   bottle do
     sha256 "bd52ed1080db697c45768449954fcbc1ead6b41bd6a2481b843a1d7177155b34" => :big_sur
+    sha256 "2864cfdd653904c86dc8715c2c03681498c487375f7a94bf71b74e7c5ad9cdad" => :arm64_big_sur
     sha256 "70e045edcdf30456dfcf96777f0f8fa68807d638ec90995b076c4ce47730e5ff" => :catalina
     sha256 "7142730e5fa6dd1203d6f34168f85d7704050cf620891a9d0db4f9eefc49370a" => :mojave
     sha256 "beafa5ccf84633bed67d405f22ac8e570d2dc2fe0e10fccf8c11076639c672ae" => :high_sierra
