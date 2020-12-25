e626d38cfeb3d47b0b2a0e59caf82b5c70eb0cbd
FX Coudert <fxcoudert@gmail.com>
2020-12-25 22:19:51 +0000

    azcopy: update 10.8.0 bottle.

--- Formula/azcopy.rb
+++ Formula/azcopy.rb
@@ -8,6 +8,7 @@ class Azcopy < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "8e0a80ce0dba2d742dedd7b6ad79abaa240b4ff9d67795a4cf7a3fe8e0b53b4a" => :big_sur
+    sha256 "e7a8629f1bb894ea1e781abb5af973344d7b667fe0b077c6a7563ab6714665df" => :arm64_big_sur
     sha256 "b9a7efa7205e5e6fd246884eb6858ed020c76abbece1d8c8c6cc61cf941564a5" => :catalina
     sha256 "0cc3410bb462732ca1ecad141c88d97c87c43d7672ead705e6d52bc286c94422" => :mojave
   end
