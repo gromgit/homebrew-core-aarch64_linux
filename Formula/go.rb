6e4c150dbd10d4be829f4c25fc0f6381b50346c4
FX Coudert <fxcoudert@gmail.com>
2020-12-25 21:21:38 +0000

    go: update 1.15.6 bottle.

--- Formula/go.rb
+++ Formula/go.rb
@@ -28,6 +28,7 @@ class Go < Formula
 
   bottle do
     sha256 "2107796e255869aae4a2b1bd5766182f38c87544f968750d78b7ba520d907b1a" => :big_sur
+    sha256 "c2bd24b5a24c19bfdf97a8904aaa76c4db2a8f59096aade83310fee7af4b186c" => :arm64_big_sur
     sha256 "9dac57ef268c5fee434ac7896fc77f16f16f34462822e216c9973ade6a768e0b" => :catalina
     sha256 "af0b8702944cde293206a5847bea8fb5aed66babed82fb202aff696ac5211691" => :mojave
   end
