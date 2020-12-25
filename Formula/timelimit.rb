c68fa798a38d3ef0de380c4b5c35262d85d00302
FX Coudert <fxcoudert@gmail.com>
2020-12-25 17:18:34 +0000

    timelimit: update 1.9.0 bottle.

--- Formula/timelimit.rb
+++ Formula/timelimit.rb
@@ -12,6 +12,7 @@ class Timelimit < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "6660e64fd509f5e3b58a1ce3b6e8dffbdc4cbb35eedefca5b133e5bd3009f378" => :big_sur
+    sha256 "9f890d907623dd9b857710b9d7eb1d3256ba0ba7f620576a28313522343b648f" => :arm64_big_sur
     sha256 "5a4d896430260125864a8b8e22aabf172e36c8f8942faece7db8d502c568a690" => :catalina
     sha256 "4b868a4ee5bd9e424e6ea4aa36fc828d6efd3f07b1d72063bbdb5a0c59baa276" => :mojave
     sha256 "872dbbd21820c6d08513f29bc0c024f8584c7d7b7b6b4b436553414d08fd6950" => :high_sierra
