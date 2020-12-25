661ddb3b28c34f6fd5aeaaf00c11be9d7a7dc169
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-12-25 14:46:10 +0100

    erlang@21: depend on x86_64

--- Formula/erlang@21.rb
+++ Formula/erlang@21.rb
@@ -23,6 +23,7 @@ class ErlangAT21 < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on arch: :x86_64
   depends_on "openssl@1.1"
   depends_on "wxmac" # for GUI apps like observer
 
