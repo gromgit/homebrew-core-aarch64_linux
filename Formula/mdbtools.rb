5edca9f7344cd6fedf3c76ce31558f7c2d591ccc
FX Coudert <fxcoudert@gmail.com>
2020-12-25 22:02:06 +0000

    mdbtools: update 0.9.0 bottle.

--- Formula/mdbtools.rb
+++ Formula/mdbtools.rb
@@ -8,6 +8,7 @@ class Mdbtools < Formula
   bottle do
     cellar :any
     sha256 "951f06fb69f1a28942e6eca0a2d850f9d916f011489fdf5f020bc3cf651b7c83" => :big_sur
+    sha256 "438ff8e5d6621ec779e8a0f61ad4ee28eb90475145ecaecc55decfb79d140286" => :arm64_big_sur
     sha256 "bf7540031032765b8e7fdc8e178a026f0e8510f08351e1f79fe1a17108807f1a" => :catalina
     sha256 "20d12c72a3b59fe33f0989ebda42a2baf211e323e347d90e6f4cf33a64c2213d" => :mojave
   end
