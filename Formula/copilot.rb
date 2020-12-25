9b5a36b94e0c86f503d657610ed5c44dc4678cff
FX Coudert <fxcoudert@gmail.com>
2020-12-25 22:35:15 +0000

    copilot: update 1.0.0 bottle.

--- Formula/copilot.rb
+++ Formula/copilot.rb
@@ -12,6 +12,7 @@ class Copilot < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "00d35d5ada87fda4b7c2cdef4da58e9a026e1a3e942dc30b575cbd8d3a8260e6" => :big_sur
+    sha256 "482aa57df717ff9fbbbdea49c8ddb507f7d507121a0cdfa3a7a97d6c63957ee7" => :arm64_big_sur
     sha256 "fc8ad0a5cbb32d5c68fa2e2e20e0fd57aa7d93b0a866329e36b7887947d845aa" => :catalina
     sha256 "c90fa63256de5549f88b3f0fe88b3c3def47866bf5999a71b0fc5e9f7eeee690" => :mojave
   end
