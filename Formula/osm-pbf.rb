cc55a069c1bb3848e3bdb642cefaad8fd9cc9c97
FX Coudert <fxcoudert@gmail.com>
2020-12-25 03:06:55 +0000

    osm-pbf: update 1.3.3_6 bottle.

--- Formula/osm-pbf.rb
+++ Formula/osm-pbf.rb
@@ -9,6 +9,7 @@ class OsmPbf < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "bb6525bab64e792c04a42dc14f4c282357a1ca810528291c708ad4bb675850ef" => :big_sur
+    sha256 "d69b8678764efa4aa16a5eebd85610bdd3ec411655946447d56ca7571f8057db" => :arm64_big_sur
     sha256 "848d0ffd20470d7988d5bb9f4a93e5b58f799646a4c551732c271d4d57b5a1f8" => :catalina
     sha256 "324c716503518b77533db927144643db877d3cf3297234333c056ae45f85d911" => :mojave
   end
