975bbc684adad18bd07e64fef7a60be4d6b2efe1
FX Coudert <fxcoudert@gmail.com>
2020-12-25 22:46:09 +0000

    efm-langserver: update 0.0.26 bottle.

--- Formula/efm-langserver.rb
+++ Formula/efm-langserver.rb
@@ -10,6 +10,7 @@ class EfmLangserver < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "df90e5180b1d93ffbe8435bf849cc80cd0890e7df34b5ce17ecf1af15f013f45" => :big_sur
+    sha256 "5eafbd614fbb194cc3a61d7a7f4a3904863b4fa6b65fc91f3c762fddc37f669a" => :arm64_big_sur
     sha256 "77d85f1f8510268844579470f58f48dd0a4f12657196133fa526574bdbd7788e" => :catalina
     sha256 "1bff252177ab0969be92b57fb25c5a80711bccdf533a235d68522f68c7191eb4" => :mojave
   end
