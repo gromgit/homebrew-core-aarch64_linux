eb2836a914f9f64ce67a3a529f5e0a1fed74339e
FX Coudert <fxcoudert@gmail.com>
2020-12-25 23:47:38 +0000

    mkcert: update 1.4.3 bottle.

--- Formula/mkcert.rb
+++ Formula/mkcert.rb
@@ -8,6 +8,7 @@ class Mkcert < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "aae4b584f5408d6b17b164fa002c2385ec7ca285f10d832f78b2230ae7b79ed3" => :big_sur
+    sha256 "5f3aa1b67a8f95a8c77d8257e5a6321bd73ad044206878d4fa9fb28765724980" => :arm64_big_sur
     sha256 "f53f22e6ddf746c4efb7f8c3c595f143fb60b2134d4cd18976650bd0ad9748ff" => :catalina
     sha256 "20c5690b998ab1af2ca387ed84b54dcab9488e031538563704fd02c7dcee3589" => :mojave
   end
