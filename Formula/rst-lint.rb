ce1c0d0a7cc156d4e35dbcadcec39b4b9673a92f
FX Coudert <fxcoudert@gmail.com>
2020-12-25 00:34:52 +0000

    rst-lint: update 1.3.2 bottle.

--- Formula/rst-lint.rb
+++ Formula/rst-lint.rb
@@ -10,6 +10,7 @@ class RstLint < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "46d51dedf610ea9518195fb7e2f1d8a3298b4640b3ae490cfbb212ca665132b2" => :big_sur
+    sha256 "7c6ef27de3684c2b642bb510abf6834ff7fea8a142cc1c95f754fff31a354270" => :arm64_big_sur
     sha256 "7dced462fec105930e7c757faf9065d8e82cf73299440ee7a67834ba7a1d813f" => :catalina
     sha256 "3f9bcd92d9df3e9f7c6a6aeb1bc395f9bcfeb963efd597c832c438b62040b3c3" => :mojave
   end
