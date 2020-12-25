7460d14604990c741cf4f12451feb39037ae2d8b
FX Coudert <fxcoudert@gmail.com>
2020-12-25 02:42:07 +0000

    sxiv: update 26_1 bottle.

--- Formula/sxiv.rb
+++ Formula/sxiv.rb
@@ -10,6 +10,7 @@ class Sxiv < Formula
   bottle do
     cellar :any
     sha256 "0fbf88dbb8f6744d36254023302ea2c88521bd4b8b8172eff00c7dfe2bfd4495" => :big_sur
+    sha256 "11aff8aaab1a32a0694672b802f9399d5002f1871329054671273a2d919b4d5d" => :arm64_big_sur
     sha256 "caafa51424cd97f030b9156aeba0ba64f6ab5821197453136a240c7ca38869d9" => :catalina
     sha256 "14b4f8a7137ea1ff12dde1d0a8cda063227e48d77ba75d93ecbde6193584d2cf" => :mojave
     sha256 "b8f60f5b9bb6987f0042ac485eb0d4c5c5c3cdc4ea4c32fc13def537e51d39dc" => :high_sierra
