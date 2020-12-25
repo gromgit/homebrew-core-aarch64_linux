256994c20d15282fcb56460958bf84eb89ad7942
FX Coudert <fxcoudert@gmail.com>
2020-12-25 12:33:55 +0000

    gsmartcontrol: update 1.1.3_8 bottle.

--- Formula/gsmartcontrol.rb
+++ Formula/gsmartcontrol.rb
@@ -11,6 +11,7 @@ class Gsmartcontrol < Formula
 
   bottle do
     sha256 "a7edc375fddc2f1bd0a0ef54579aebb80941b51ce0b16d6dc0e6e174b894376b" => :big_sur
+    sha256 "87f1f004a8d37328c3a246bd66a3338ad14247ed38f7c7e75c5f8e274795b610" => :arm64_big_sur
     sha256 "013dc10e96aee3db96ad08f2b460839cd1c2afa7440965b0f64f12dda5dd1728" => :catalina
     sha256 "1fd22b0404a9b3da97aa728c6421340380d271d708c0c15734eadcf82bde6410" => :mojave
   end
