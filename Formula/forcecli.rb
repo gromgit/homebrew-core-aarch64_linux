2f919d4c3273cdd1c13d834f3c53e199120d214a
FX Coudert <fxcoudert@gmail.com>
2020-12-25 22:53:07 +0000

    forcecli: update 0.28.1 bottle.

--- Formula/forcecli.rb
+++ Formula/forcecli.rb
@@ -9,6 +9,7 @@ class Forcecli < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "bd272e2bbe76e54caced1523e90ec249296d36b83dafdd27b74f3cfe2ab2ac0b" => :big_sur
+    sha256 "f8754b57754c8f8055c525f1eb7d237f491ec9441a5498bc970c9b18c759f796" => :arm64_big_sur
     sha256 "b4e2f2425b38eb74ed2cff1b2ab9bfb99210f5a527bc82f3c2abafbd4a21d94e" => :catalina
     sha256 "168945571441dec3ee1eece262ab3f8363e831a4ff585748355b31a59de24feb" => :mojave
     sha256 "6597c18df4c105cda4053a33da3536c16bb5560d46caeb28b0040c4e50fa3d75" => :high_sierra
