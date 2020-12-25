1fcbc531836fbdea47dcfe8549aec52b0c3c8e37
FX Coudert <fxcoudert@gmail.com>
2020-12-25 23:01:42 +0000

    gitbatch: update 0.5.0 bottle.

--- Formula/gitbatch.rb
+++ Formula/gitbatch.rb
@@ -8,6 +8,7 @@ class Gitbatch < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "0b4a7e403d40bb05852d24ed69bd68a4498b1ba455d618de879a0a759f6497a6" => :big_sur
+    sha256 "b456ea020c3f71390fe9678a34f4e4570be60b74b3e72337ab977920b6a34acd" => :arm64_big_sur
     sha256 "58b204bd1779e99cade465f98457ec14cadaee9a3b65afe099245faba640da0c" => :catalina
     sha256 "31d8b72293ceacef7d44beb203887ff80628cb5cd3b56a9f0b467704d153b261" => :mojave
     sha256 "73fc219e77776b78635c672111736a1ce26f6f1afe0df4ce7c571341578cd1e9" => :high_sierra
