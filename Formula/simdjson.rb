cbc993fa1887a29da3b675c7e12426d7c8e5e340
FX Coudert <fxcoudert@gmail.com>
2020-12-25 17:29:36 +0000

    simdjson: update 0.7.1 bottle.

--- Formula/simdjson.rb
+++ Formula/simdjson.rb
@@ -9,6 +9,7 @@ class Simdjson < Formula
   bottle do
     cellar :any
     sha256 "85aeb3340cc39d800c15b663fb78c188c36fb1db7d403836b4d61b02bf075682" => :big_sur
+    sha256 "55f52dae81a764b8a2510718c5ce8589a2d18e09daecd7fe8d89732a582bb970" => :arm64_big_sur
     sha256 "d63e7c62294fb267b500121c473a2e383757b6c855abb883cea57a1022bf1f11" => :catalina
     sha256 "56b858b76bb485b334eefbefafb37b37be75f9bc42992d888f5d3a46e2c5a0b5" => :mojave
   end
