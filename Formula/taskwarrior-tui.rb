db789b788160a91c1aa47637a0d939f721306de1
FX Coudert <fxcoudert@gmail.com>
2020-12-26 02:28:57 +0000

    taskwarrior-tui: update 0.9.5 bottle.

--- Formula/taskwarrior-tui.rb
+++ Formula/taskwarrior-tui.rb
@@ -14,6 +14,7 @@ class TaskwarriorTui < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "a56fdc4e59440cd36f0225f721dc3f9eb003c201c2adaeaccdd0b3cb7fb1a016" => :big_sur
+    sha256 "a1c85ab6586bc00477e6683c77b9527096f7bfdc78c7f9482ed83127c6e78bee" => :arm64_big_sur
     sha256 "58cdb481a8fc3c715617070588fad8fd4ebf392a882d872de3df7dea47b9a587" => :catalina
     sha256 "6f0e8fbb45323b4a0d894dac8bd52152740beb2477e8a80ddcca5b177eb4704f" => :mojave
   end
