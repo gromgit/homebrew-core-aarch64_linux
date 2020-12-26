eca843027cc47affa27be41d3a5031a396de3732
Dawid Dziurla <dawidd0811@gmail.com>
2020-12-26 17:52:40 +0000

    travis: update 1.10.0_1 bottle.

--- Formula/travis.rb
+++ Formula/travis.rb
@@ -8,11 +8,9 @@ class Travis < Formula
 
   bottle do
     cellar :any
-    sha256 "21ab8e73b2aab1a67e6ad0cf2dfe4e0f2337b1fd743d22b134baf27354c9dd54" => :big_sur
-    sha256 "c27b3f49ed453047c62be6a72c9c67e495fa1ec88355d212b2919905c377114f" => :arm64_big_sur
-    sha256 "e6c6d41d24822636377d55de42b627fd901a6864512efbc1dab418dec752911d" => :catalina
-    sha256 "cdc6ac367f2c79ecce4b33d6ad0c3c5c71457db7ff818eb6de67411a2820ce52" => :mojave
-    sha256 "24bebab17d6e415efacc5314f4fc9c94b93b0da4c31b21069187acba83fc6be0" => :high_sierra
+    sha256 "c0f91c1577a34eeea38e2b28d322741f8d734b89b820e31d5d2298d5b8e6e68d" => :big_sur
+    sha256 "5fc09b846132893e34c32ae7d2e197a013dbcf7e45bb4ea94071bbf7f5ef8396" => :catalina
+    sha256 "605b7d21dedd57046d5a02bb06e88162ef90384d3bf54f7236388f83a19fb7b8" => :mojave
   end
 
   depends_on "pkg-config" => :build
