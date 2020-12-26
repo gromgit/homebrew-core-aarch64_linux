d0133ecffa894850d1705a53856f3a5e933e4d24
FX Coudert <fxcoudert@gmail.com>
2020-12-26 22:33:57 +0000

    qbs: update 1.18.0 bottle.

--- Formula/qbs.rb
+++ Formula/qbs.rb
@@ -9,6 +9,7 @@ class Qbs < Formula
   bottle do
     cellar :any
     sha256 "7d794beab13a4b2c18474a85412e1cbeb5acfb5b4a40c35c45bfb06eab8f2d8e" => :big_sur
+    sha256 "091c4c7af98fe1dc6805064d871fddd4da518fcf335f2640724fa702e5392b7a" => :arm64_big_sur
     sha256 "81095756000121b7f58830c2a3986e6e005b88802a732880aa4bd20d0ae65962" => :catalina
     sha256 "710ff184af0065767d3d2a512306656676d6bc0d123f40dc8e9d9cfabfb18f44" => :mojave
   end
