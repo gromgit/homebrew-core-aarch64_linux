7fb429ae7ac38c00d4532e8885546e828a4de712
Rui Chen <rui@meetup.com>
2020-12-26 18:23:19 -0500

    iam-policy-json-to-terraform: deprecate gopath
    Closes #67783.
    
    Signed-off-by: Rui Chen <rui@meetup.com>
    Signed-off-by: chenrui <chenrui333@gmail.com>

--- Formula/iam-policy-json-to-terraform.rb
+++ Formula/iam-policy-json-to-terraform.rb
@@ -4,6 +4,7 @@ class IamPolicyJsonToTerraform < Formula
   url "https://github.com/flosell/iam-policy-json-to-terraform/archive/1.6.0.tar.gz"
   sha256 "714b8aead9bf5a88989a62eb520163565c890f37ee13783a3ae549bb0b8cdead"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/flosell/iam-policy-json-to-terraform.git"
 
   bottle do
@@ -13,19 +14,10 @@ class IamPolicyJsonToTerraform < Formula
     sha256 "01baa53333aa1aeeafc231c6fb985a21cc98bdee8a3b4812700036f3aa81401e" => :mojave
   end
 
-  depends_on "dep" => :build
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-
-    dir = buildpath/"src/github.com/flosell/iam-policy-json-to-terraform"
-    dir.install buildpath.children
-    cd "src/github.com/flosell/iam-policy-json-to-terraform" do
-      system "make", "iam-policy-json-to-terraform_darwin"
-      mv "iam-policy-json-to-terraform_darwin", "iam-policy-json-to-terraform"
-      bin.install "iam-policy-json-to-terraform"
-    end
+    system "go", "build", *std_go_args, "-ldflags", "-s -w"
   end
 
   test do
