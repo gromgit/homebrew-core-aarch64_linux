f73e758005f1708f8a6475be786265f0e4c8766b
FX Coudert <fxcoudert@gmail.com>
2020-12-26 01:13:01 +0000

    yj: update 5.0.0 bottle.

--- Formula/yj.rb
+++ Formula/yj.rb
@@ -10,6 +10,7 @@ class Yj < Formula
     cellar :any_skip_relocation
     rebuild 1
     sha256 "6c1c51234dc8c8590af22184be13472c9939426c98db9c9bcd58fade1cbb1840" => :big_sur
+    sha256 "9093ad6c4366b3d6cd6d37b1300f1e80fbd30b051e3d934a498db64c46bed6d6" => :arm64_big_sur
     sha256 "918450aaf162067fe6fa7979518a7fc998853a4ab215c01f2c69e756739fb710" => :catalina
     sha256 "918450aaf162067fe6fa7979518a7fc998853a4ab215c01f2c69e756739fb710" => :mojave
     sha256 "918450aaf162067fe6fa7979518a7fc998853a4ab215c01f2c69e756739fb710" => :high_sierra
