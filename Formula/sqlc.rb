0247a2de3542ac7a59f9975292c111065d9b043d
FX Coudert <fxcoudert@gmail.com>
2020-12-26 00:28:18 +0000

    sqlc: update 1.6.0 bottle.

--- Formula/sqlc.rb
+++ Formula/sqlc.rb
@@ -9,6 +9,7 @@ class Sqlc < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "159376fbb6da52240161c2ac689432121ea7110838758552b735dcc6e4fb8180" => :big_sur
+    sha256 "d6e1795618e027c9cf06506877f5ec6285d41c0f9e28b638335f8de522eec9a2" => :arm64_big_sur
     sha256 "debad9ab4e258cf867de532c0f6715ed69bebe970e43c0dcf875d6f1c4f761d4" => :catalina
     sha256 "be41455ec25177178d8d2c89d995c32cc82332c0ecb9bf5748d5619bdaff4c36" => :mojave
   end
