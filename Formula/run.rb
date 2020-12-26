5e30dea63ce2ad6806588cf18ec0d975a9a36f93
FX Coudert <fxcoudert@gmail.com>
2020-12-26 00:15:37 +0000

    run: update 0.7.2 bottle.

--- Formula/run.rb
+++ Formula/run.rb
@@ -8,6 +8,7 @@ class Run < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "0beed1b3bd5d70f9a8a270a7996d0f309f2cab7cf622556b5d833f8311b8cf7a" => :big_sur
+    sha256 "32fff2fd9c8b242912438be13118185afaa20bccad7bb134788ffcc07b66791d" => :arm64_big_sur
     sha256 "4a2329ae36f59ed71c1b6364828c465a27f92506cd77e9a7c8217def3e2b7c9e" => :catalina
     sha256 "4a2329ae36f59ed71c1b6364828c465a27f92506cd77e9a7c8217def3e2b7c9e" => :mojave
     sha256 "4a2329ae36f59ed71c1b6364828c465a27f92506cd77e9a7c8217def3e2b7c9e" => :high_sierra
