9d7147fab616f7fae52189a30386c7f781e75004
Alexander Ronald Altman <alexanderaltman@me.com>
2020-12-26 01:37:06 -0800

    dashing: replace test resource
    Closes #67755.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dashing.rb
+++ Formula/dashing.rb
@@ -16,9 +16,9 @@ class Dashing < Formula
 
   depends_on "go" => :build
 
-  resource "ruby_docs_tarball" do
-    url "http://ruby-doc.com/downloads/ruby_2_6_5_core_rdocs.tgz"
-    sha256 "f9f74cf85c84e934d7127c2e86f4c3b0b70380a92c400decdc8a77ac977097fe"
+  resource "redux_saga_docs_tarball" do
+    url "https://github.com/dmitrytut/redux-saga-docset/archive/7df9e3070934c0f4b92d66d2165312bf78ecd6a0.tar.gz"
+    sha256 "08e5cc1fc0776fd60492ae90961031b1419ea6ed02e2c2d9db2ede67d9d67852"
   end
 
   def install
@@ -30,11 +30,20 @@ class Dashing < Formula
   test do
     # Make sure that dashing creates its settings file and then builds an actual
     # docset for Dash
-    testpath.install resource("ruby_docs_tarball")
+    testpath.install resource("redux_saga_docs_tarball")
+    innerpath = testpath
     system bin/"dashing", "create"
-    assert_predicate testpath/"dashing.json", :exist?
+    assert_predicate innerpath/"dashing.json", :exist?
     system bin/"dashing", "build", "."
-    file = testpath/"dashing.docset/Contents/Resources/Documents/goruby_c.html"
-    assert_predicate file, :exist?
+    innerpath /= "dashing.docset/Contents"
+    assert_predicate innerpath/"Info.plist", :exist?
+    innerpath /= "Resources"
+    assert_predicate innerpath/"docSet.dsidx", :exist?
+    innerpath /= "Documents"
+    assert_predicate innerpath/"README.md", :exist?
+    innerpath /= "docs"
+    assert_predicate innerpath/"index.html", :exist?
+    innerpath /= "introduction"
+    assert_predicate innerpath/"SagaBackground.html", :exist?
   end
 end
