e63bd3d3e763427ad92ab3474a9bae42ae4d14bf
Seeker <meaningseeking@protonmail.com>
2020-12-26 09:04:19 -0800

    a2ps: apply macOS-only patch on macOS (#67756)

--- Formula/a2ps.rb
+++ Formula/a2ps.rb
@@ -24,23 +24,23 @@ class A2ps < Formula
     satisfy { HOMEBREW_PREFIX.to_s == Homebrew::DEFAULT_PREFIX }
   end
 
-  # Software was last updated in 2007.
-  # https://svn.macports.org/ticket/20867
-  # https://trac.macports.org/ticket/18255
   on_macos do
+    # Software was last updated in 2007.
+    # https://trac.macports.org/ticket/18255
     patch :p0 do
       url "https://raw.githubusercontent.com/Homebrew/formula-patches/0ae366e6/a2ps/patch-contrib_sample_Makefile.in"
       sha256 "5a34c101feb00cf52199a28b1ea1bca83608cf0a1cb123e6af2d3d8992c6011f"
+    end
+
+    # https://trac.macports.org/ticket/20867
+    patch :p0 do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/0ae366e6/a2ps/patch-lib__xstrrpl.c"
+      sha256 "89fa3c95c329ec326e2e76493471a7a974c673792725059ef121e6f9efb05bf4"
     end
   end
 
   on_linux do
     depends_on "gperf"
-  end
-
-  patch :p0 do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/0ae366e6/a2ps/patch-lib__xstrrpl.c"
-    sha256 "89fa3c95c329ec326e2e76493471a7a974c673792725059ef121e6f9efb05bf4"
   end
 
   def install
