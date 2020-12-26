566f3680b44687210aebeb10ef94e647158129db
FX Coudert <fxcoudert@gmail.com>
2020-12-26 00:45:35 +0000

    tty-share: update 2.2.0 bottle.

--- Formula/tty-share.rb
+++ Formula/tty-share.rb
@@ -8,6 +8,7 @@ class TtyShare < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "004c70273ec6b94d912745c657639878149b86cdf1f1296d9d5498460f8b01b4" => :big_sur
+    sha256 "4207e631fd61f1e5ea1a7659d09b606cf10b5012a9b52153a956af15f5a7e160" => :arm64_big_sur
     sha256 "e02d15913aa63a1cbff110af076743dacc3c4d56cf828a0b22cf94d4e025b6e8" => :catalina
     sha256 "1fe5cd2eb19d7a0b0ee61a9b0dbddc13805055752827de2af6221e53d42f1b9f" => :mojave
   end
