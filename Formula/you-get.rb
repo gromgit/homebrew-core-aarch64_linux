c05719c9d8f00a0e982014d59ab706700a31f0f8
FX Coudert <fxcoudert@gmail.com>
2020-12-26 10:31:42 +0000

    you-get: update 0.4.1488 bottle.

--- Formula/you-get.rb
+++ Formula/you-get.rb
@@ -16,6 +16,7 @@ class YouGet < Formula
     cellar :any_skip_relocation
     rebuild 1
     sha256 "b53a803d844d85abe7f6a14d3ef33fc8867595bc5f58e35f5c4229aaf04f7b8a" => :big_sur
+    sha256 "be8623a5faa96a3bb083e03b6340a72e7209b50d35e0fddb9b6f9dc230adc4ad" => :arm64_big_sur
     sha256 "b3c8d2f9616e12c4c97896bdedd334ec69c206bbe521d187c226f4d7a5edcdca" => :catalina
     sha256 "0a8e12599e2eaaf5eceddddb75c04cb3c83a37eb80fbe385023dd05774b11b17" => :mojave
   end
