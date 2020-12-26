5e3c4eddd2c9ad555fec439e1d3119aee67cf9ad
FX Coudert <fxcoudert@gmail.com>
2020-12-26 01:31:10 +0000

    duckscript: update 0.7.1 bottle.

--- Formula/duckscript.rb
+++ Formula/duckscript.rb
@@ -9,6 +9,7 @@ class Duckscript < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "2e80eaf10dfa5b1bc6d3593d34020f48deb0e4d2df88c7ba828cd9eb2da29f76" => :big_sur
+    sha256 "78556d9bd5b9dc1e08f3848b1488f375e85029f3222979476b71fffa95c7528b" => :arm64_big_sur
     sha256 "8b1f02d6c68eb9b67420f7d2b9c1b1ca205b15a54d3fc6d7c5764f6dbc1b512c" => :catalina
     sha256 "c14e28b195ed1d2e7cd4a87be420a52f5619ddb6741c58c9273a3c1a2060f453" => :mojave
   end
