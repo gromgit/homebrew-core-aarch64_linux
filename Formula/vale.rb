c7e7b9675a751dbb2504a4ad5fa1f7600fc01c69
FX Coudert <fxcoudert@gmail.com>
2020-12-26 00:57:15 +0000

    vale: update 2.6.8 bottle.

--- Formula/vale.rb
+++ Formula/vale.rb
@@ -8,6 +8,7 @@ class Vale < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "1986dd9f269658d6ad06e4106ede968b1fa6c2a63e092e20a1d9ee36fe446488" => :big_sur
+    sha256 "8bc3ad611839c17d26ef7b7944a8031d771a97f79aab5095c694a09144b5ad8b" => :arm64_big_sur
     sha256 "37e466ff727faf8326863f16548c1cd10ca2fc12232836dbf24e99751e2d67f2" => :catalina
     sha256 "5930b7dc78e4a216979083212dafd8b9ff18a5b327a7bb6962c40b30a6da2b08" => :mojave
   end
