e8aee44a2936fac6181ca676680bc59b99cc787d
FX Coudert <fxcoudert@gmail.com>
2020-12-26 02:06:59 +0000

    onefetch: update 2.8.0 bottle.

--- Formula/onefetch.rb
+++ Formula/onefetch.rb
@@ -8,6 +8,7 @@ class Onefetch < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "07c62d13046813adf43ec8376ce1abcc9fafb2ff8f81e0c0ac2dcbac44318319" => :big_sur
+    sha256 "14bec354bd1904734e6f2abe59a0d63f65890faee50d63853f07175fea7eaca3" => :arm64_big_sur
     sha256 "0d3a78ed3865cf967746b8e696beed1c290ba456fd6a8314346334f723a254ad" => :catalina
     sha256 "250e3decffc886853d99200fb428c3a7bb8bd08d9ad10f9c4eb222102933f7df" => :mojave
   end
