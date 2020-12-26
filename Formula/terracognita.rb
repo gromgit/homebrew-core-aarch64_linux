8957332f385e3a6e52b9bc2157a0c709d56a8e32
FX Coudert <fxcoudert@gmail.com>
2020-12-26 00:42:04 +0000

    terracognita: update 0.6.0 bottle.

--- Formula/terracognita.rb
+++ Formula/terracognita.rb
@@ -9,6 +9,7 @@ class Terracognita < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "91aedc3bc0275d4519a0bca398cdc42f1c419cc4979b89e9063c04cb58ee9769" => :big_sur
+    sha256 "398ddc41ce7c7e92ba66e3e5a7e2d95b0105370db5d6cffb9f825e5d3a5ac393" => :arm64_big_sur
     sha256 "ed1875f71c6e7c16a0857815f3e97e2c258d591b21d7a3fb3db2cd0d4434db5a" => :catalina
     sha256 "f4233f3a50dcd2e872eef49d00b7cbdc80af701dd686039be71d6b2fbb6f2251" => :mojave
   end
