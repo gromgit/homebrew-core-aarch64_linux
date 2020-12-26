dbc33a9bd5a399e2181a093ec26e8ace8de13765
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-26 12:25:53 +0000

    scour: update 0.38.2 bottle.

--- Formula/scour.rb
+++ Formula/scour.rb
@@ -15,10 +15,9 @@ class Scour < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "59c8544ab5300901bbe06b2279cfd2e81db2b2341506b14522d8f9d1630a3f00" => :big_sur
-    sha256 "f0cbfbf1df3579bbd13c1ad1ff27943f89fc79adb1c81d34fc1a8455eec793ab" => :catalina
-    sha256 "206139ff183f844e57b8b93a9689d5e2120c5fe9e6d3d618bc5fc884d306de7b" => :mojave
-    sha256 "969e45c7db350a7506591221ffdf774b2c272bc14bca989a6241612ee4768f6f" => :high_sierra
+    sha256 "8a91c497d4200b82a6e24f3104b5fd4d7d6cba4d8abb4a9c500a903ad69c5e37" => :big_sur
+    sha256 "bf48e8efd48691881109cee3441291fe9216ef200c6592a10f968a23d4525017" => :catalina
+    sha256 "83b18dcaf7f38451261de53be10328a6dc2ebc570d1e1d5840961c89a79748f3" => :mojave
   end
 
   depends_on "python@3.9"
