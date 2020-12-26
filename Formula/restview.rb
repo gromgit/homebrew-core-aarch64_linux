9b4fe39b9ec29790e5ec6ee00da50e86eb825643
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-26 08:22:57 +0000

    restview: update 2.9.2_3 bottle.

--- Formula/restview.rb
+++ Formula/restview.rb
@@ -14,11 +14,10 @@ class Restview < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "4ba3029792fba4cc83b0b623744f972c28cdb923fcffadeac09529f338705287" => :big_sur
-    sha256 "a57ef09b32b094bac63bf07d794485acb46a94962df7bed53b541adafc0c6c7a" => :arm64_big_sur
-    sha256 "a7d9331d1da6155733bd914f4d0d987887ddeb116d11d71a39161f9213d85e41" => :catalina
-    sha256 "cc035715443be21749ec92c08ca2e25b9f7773e1f6078d46deb9c754bea59a28" => :mojave
-    sha256 "b526e9a774ed4ef1b83d48c2367dafe105365f059f0085d12650bf4a1dee778c" => :high_sierra
+    rebuild 1
+    sha256 "1357703d188eca6969188e8f65210916b2a7abc43479df35e23de1aa5f830726" => :big_sur
+    sha256 "863871057c097330e17eeb8f0d449177e458108cd5b19b3fe5e4cdee11124352" => :catalina
+    sha256 "4435543bf0faad23da84a50c350172a6a717b648de5cc195d4e56ab185aefd0c" => :mojave
   end
 
   depends_on "python@3.9"
