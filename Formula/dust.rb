441b99b05b0a07e62ea09a6bfa4fdbc0b5c5193a
FX Coudert <fxcoudert@gmail.com>
2020-12-26 01:30:52 +0000

    dust: update 0.5.3 bottle.

--- Formula/dust.rb
+++ Formula/dust.rb
@@ -14,6 +14,7 @@ class Dust < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "bfcdb4a3fe8417d4bb88d1b0945b002e818271a8b1f5e7ac3a9502763c0e4157" => :big_sur
+    sha256 "88a327584a516af51e1b5a27b918f705fb06694b11f81cc2fd91fc78bf1f0f79" => :arm64_big_sur
     sha256 "693714f8f0d1207b8e54357f924e3a1242dd54d93f21de76b669217812065711" => :catalina
     sha256 "4c5c1cda666e8d405cf39ae79b3327dd8581555c18417b0834c2d334ad4568ed" => :mojave
     sha256 "52750a57ee2e58c7b95661b61cb0d855ba653946f6f21881ed041f2ce2cea747" => :high_sierra
