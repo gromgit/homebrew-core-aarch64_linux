daa5b659ef08215bcd97674b9d52853bfd2b9714
Rui Chen <rui@meetup.com>
2020-12-26 11:04:53 -0500

    libarchive 3.5.1
    * libarchive 3.5.1
    * libarchive: add license
    
    Closes #67763.
    
    Co-authored-by: chenrui <rui@meetup.com>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libarchive.rb
+++ Formula/libarchive.rb
@@ -1,8 +1,9 @@
 class Libarchive < Formula
   desc "Multi-format archive and compression library"
   homepage "https://www.libarchive.org"
-  url "https://www.libarchive.org/downloads/libarchive-3.4.3.tar.xz"
-  sha256 "0bfc3fd40491768a88af8d9b86bf04a9e95b6d41a94f9292dbc0ec342288c05f"
+  url "https://www.libarchive.org/downloads/libarchive-3.5.1.tar.xz"
+  sha256 "0e17d3a8d0b206018693b27f08029b598f6ef03600c2b5d10c94ce58692e299b"
+  license "BSD-2-Clause"
 
   livecheck do
     url "https://libarchive.org/downloads/"
