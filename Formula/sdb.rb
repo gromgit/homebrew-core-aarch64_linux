7b8d5fe72dd70a7d7e0cef16036d75ac93de71c8
FX Coudert <fxcoudert@gmail.com>
2020-12-26 17:53:51 +0000

    sdb: update 1.5.0 bottle.

--- Formula/sdb.rb
+++ Formula/sdb.rb
@@ -9,6 +9,7 @@ class Sdb < Formula
   bottle do
     cellar :any
     sha256 "95163d62cbd702fabddba0dd1bde7213254e35a54181874308c9778f51dfcdd2" => :big_sur
+    sha256 "a41f0dfc6fe035b7206c543c92a2bc95889c444914cf4fe404f8c88b0a855201" => :arm64_big_sur
     sha256 "04aa601e84f7f4d95e17ca2675564d4d3027e9736e57d4db85461c7a843ea025" => :catalina
     sha256 "8b110c8f73ccbae52f0e600fb3b67bef8088c541710fbe690b0548e4c2f09703" => :mojave
     sha256 "baa2ae3e71b94ba5241956188b631ece503344129f41a407268bafd82a9b009d" => :high_sierra
