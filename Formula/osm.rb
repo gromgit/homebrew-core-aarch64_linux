46897f70f5f792a89ed26e3c6d17176507908b5c
FX Coudert <fxcoudert@gmail.com>
2020-12-26 00:01:24 +0000

    osm: update 0.6.0 bottle.

--- Formula/osm.rb
+++ Formula/osm.rb
@@ -8,6 +8,7 @@ class Osm < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "2ea4693e7560013d61b5173c7c10207c051926812c06f9173be36bc484ac71c6" => :big_sur
+    sha256 "00e3e57986deab39a5dcdb4bdfd43694fcba09b19cda22e91b1d2729bb9805b1" => :arm64_big_sur
     sha256 "e7a7990a6378f86e7faa9f323dbf74b0125ca21939ddfa334ab149d0f97b7c6c" => :catalina
     sha256 "e6cc48d691a875b8e21833da92821de372ecbd4f6a424da66429c20f751d819c" => :mojave
   end
