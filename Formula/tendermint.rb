aefa1963480a70b1a6327da7a7be538f8522232e
FX Coudert <fxcoudert@gmail.com>
2020-12-26 00:35:16 +0000

    tendermint: update 0.34.0 bottle.

--- Formula/tendermint.rb
+++ Formula/tendermint.rb
@@ -9,6 +9,7 @@ class Tendermint < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "ad7854f901ae4ac1ab4f7af7f02d4ee00d807789914515fd25b1c5b2c8c2ca04" => :big_sur
+    sha256 "7c874d945d0ddd2893f9004e9b515a00ed384ad2359fb570bfab3dc7d81294a6" => :arm64_big_sur
     sha256 "3901a4b32d2fd8c576929b52603c2166f08978a228f8c18491b258c72f0777e9" => :catalina
     sha256 "65a04941f0c97cdb98ffdf3b4e16fe4640566d35b107b634bcd789779fa58deb" => :mojave
   end
