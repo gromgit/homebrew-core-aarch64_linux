bf2106efa6952e9d556e1c37b074414fbe09e71c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-26 08:22:58 +0000

    rst-lint: update 1.3.2 bottle.

--- Formula/rst-lint.rb
+++ Formula/rst-lint.rb
@@ -13,10 +13,10 @@ class RstLint < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "46d51dedf610ea9518195fb7e2f1d8a3298b4640b3ae490cfbb212ca665132b2" => :big_sur
-    sha256 "7c6ef27de3684c2b642bb510abf6834ff7fea8a142cc1c95f754fff31a354270" => :arm64_big_sur
-    sha256 "7dced462fec105930e7c757faf9065d8e82cf73299440ee7a67834ba7a1d813f" => :catalina
-    sha256 "3f9bcd92d9df3e9f7c6a6aeb1bc395f9bcfeb963efd597c832c438b62040b3c3" => :mojave
+    rebuild 1
+    sha256 "b2f2daab471857924b79de844c77047a02452d4fb201b9844a6293577dbd1e50" => :big_sur
+    sha256 "ba3a1093fb3d07a4293e80c9cb04f0b3abf6d6ad1e341f73ee107d96d5a6584b" => :catalina
+    sha256 "1ab54f054de10974bc5eebd92c710a7ea2ce95c71f5de4628f4febf99ca517b3" => :mojave
   end
 
   depends_on "python@3.9"
