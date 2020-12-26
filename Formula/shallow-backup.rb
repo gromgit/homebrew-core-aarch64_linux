63223dee2864907c8904f283211042595ea0fbf9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-26 07:25:36 +0000

    shallow-backup: update 5.0.1_1 bottle.

--- Formula/shallow-backup.rb
+++ Formula/shallow-backup.rb
@@ -15,11 +15,10 @@ class ShallowBackup < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ed8d7fd1723a6cc043f76799099cf7267f96baa8e9d2a9ea01f6b29ba1167c27" => :big_sur
-    sha256 "05d268442debafc90917368ab541e9cf855f53204fd1de2352dc6f1d5729246d" => :arm64_big_sur
-    sha256 "ab0480d15430cc6fdfcde70fca423784efcf2a4e32aa5286a60b53ba5d713b23" => :catalina
-    sha256 "1dbe77e521a242d1b1c3d833b981427724006c82c3885fb87062c30d272e4cc2" => :mojave
-    sha256 "1de7f1b14391ee61eb08f4016d92ecac7e431337bfea369bcb6bb95cfb1461f6" => :high_sierra
+    rebuild 1
+    sha256 "adfc473f09b59e45bdc9942cd6f5eb95898691a1643d4f22da2dc18a471a1356" => :big_sur
+    sha256 "26ccb36f907869ddeb01460ec46a45773acb3dbd2212f6247e79e731fa4a1b00" => :catalina
+    sha256 "67ea3ad0afb6b8107f0ca8a6ae33cac5a8be7c409e9c76111e41da43cb3a3c51" => :mojave
   end
 
   depends_on "python@3.9"
