b3a83e2fdec689b58a337a1b29eb0eb02b2aa1db
FX Coudert <fxcoudert@gmail.com>
2020-12-26 00:38:35 +0000

    terraform-ls: update 0.11.0 bottle.

--- Formula/terraform-ls.rb
+++ Formula/terraform-ls.rb
@@ -9,6 +9,7 @@ class TerraformLs < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "6f16bd864634135cdf9db9db07cbbb24ef1f1a7165707b4aed736b97376998d6" => :big_sur
+    sha256 "b42f94ad98ba2a95b9a68326adee1f0f0e6aba040a6134b694d4c3188089ae35" => :arm64_big_sur
     sha256 "49be1f2b0720b75a357f62294a1435d9911153752791bf187220be7d35b41f4b" => :catalina
     sha256 "e929d98b6ec3f7ded0f52a4509591a91dd4a61f7d2419432540e5254443e6e8b" => :mojave
   end
