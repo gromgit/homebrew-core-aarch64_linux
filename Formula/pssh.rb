67c9721c28d6baf18d64a838810215218af49c0e
Rui Chen <rui@meetup.com>
2020-12-26 00:13:43 -0500

    pssh: rebuild
    * pssh: switch to pypi source
    * pssh: add livecheck
    
    Closes #67729.
    
    Signed-off-by: Rui Chen <rui@meetup.com>
    Co-authored-by: Seeker <meaningseeking@protonmail.com>
    Signed-off-by: Seeker <meaningseeking@protonmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pssh.rb
+++ Formula/pssh.rb
@@ -2,10 +2,14 @@ class Pssh < Formula
   include Language::Python::Virtualenv
   desc "Parallel versions of OpenSSH and related tools"
   homepage "https://code.google.com/archive/p/parallel-ssh/"
-  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/parallel-ssh/pssh-2.3.1.tar.gz"
+  url "https://files.pythonhosted.org/packages/60/9a/8035af3a7d3d1617ae2c7c174efa4f154e5bf9c24b36b623413b38be8e4a/pssh-2.3.1.tar.gz"
   sha256 "539f8d8363b722712310f3296f189d1ae8c690898eca93627fc89a9cb311f6b4"
   license "BSD-3-Clause"
   revision 3
+
+  livecheck do
+    url :stable
+  end
 
   bottle do
     cellar :any_skip_relocation
