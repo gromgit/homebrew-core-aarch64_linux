c3d4b959dc54cdf773a2de2b2b824c5b68435f7f
FX Coudert <fxcoudert@gmail.com>
2020-12-26 01:21:36 +0000

    cargo-c: update 0.7.0 bottle.

--- Formula/cargo-c.rb
+++ Formula/cargo-c.rb
@@ -8,6 +8,7 @@ class CargoC < Formula
   bottle do
     cellar :any
     sha256 "cba2140415ab264d8be8f471ac06a9ff12e270f6d25475663096b6302c03e5fe" => :big_sur
+    sha256 "a591286629fc88d60cfec3a41188582cf67652d9f2acd7db4c564bb99b70c01a" => :arm64_big_sur
     sha256 "13999f706b3bba61b2380ab26ddf327e7110816f72bb0d5c1454d6571b2fe361" => :catalina
     sha256 "4f6e019268d3263fc50faff74c47fb1559ae3fcbe319f3263deb7a4c8aba86a3" => :mojave
   end
