ec598ac73aefe6784963a0d7212842150c3d485a
FX Coudert <fxcoudert@gmail.com>
2020-12-26 02:11:53 +0000

    procs: update 0.10.10 bottle.

--- Formula/procs.rb
+++ Formula/procs.rb
@@ -8,6 +8,7 @@ class Procs < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "212bb791a261e19856eda7ff7cdae3ee3d96a5553f5f1a6fd94a882b8663121f" => :big_sur
+    sha256 "20c0b4166c8968e34e777e67f934890ad4449d4619db6cae53f6b14d3d289c3b" => :arm64_big_sur
     sha256 "7bd872fd51bc821cb102ed245545918b6c95356395a66665807dc089c245409e" => :catalina
     sha256 "435573168fd92844915ffc08bb1d29b424549fb805ee9205094d27892ec0c33a" => :mojave
   end
