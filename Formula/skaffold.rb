38900c18775a7d9c5f238c23a19ccad221fd8b7b
FX Coudert <fxcoudert@gmail.com>
2020-12-26 00:22:28 +0000

    skaffold: update 1.17.2 bottle.

--- Formula/skaffold.rb
+++ Formula/skaffold.rb
@@ -10,6 +10,7 @@ class Skaffold < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "3c3bed94b02b9ab1a8218db62154f69d779475e15a7961b1ae90870c7bbd395a" => :big_sur
+    sha256 "141dad75d334fadd92438a4e2417ab8f476025d64eca3c476d763cc741bb168d" => :arm64_big_sur
     sha256 "cf1f406c9f1a6c0d16fcb87ce35f0c012f9e9ee0bf430e5fb321c8f60d42db7a" => :catalina
     sha256 "80c54de785afe4a20bde82af3e2e1e4eefe932cd082ad3732a2c8123b9d6e3e9" => :mojave
   end
