7f7ab4c1023738c41331e6854d9b1b48d36c9f36
FX Coudert <fxcoudert@gmail.com>
2020-12-26 02:18:44 +0000

    sccache: update 0.2.14 bottle.

--- Formula/sccache.rb
+++ Formula/sccache.rb
@@ -9,6 +9,7 @@ class Sccache < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "02f4a0a5af2c1c5cf446fa67ad86a535565e1d7cc84f18a596216cad4492eb68" => :big_sur
+    sha256 "ae7f1b07fbd74584704a0b9acde2899bf1baf5e4eff460e3b27441cb4289840b" => :arm64_big_sur
     sha256 "900c42698ed29c08b034fdeaa990fde48baf9fc608399673d99fb15ade0ba3f7" => :catalina
     sha256 "afc4e8ae1f1febbea962db840e49a89079b67af20e6058da8b302bc634c9d60b" => :mojave
   end
