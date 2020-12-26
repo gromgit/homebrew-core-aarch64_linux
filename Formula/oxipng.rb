6400f9e58c6049adc089c6a1b29def2274c9b4ce
FX Coudert <fxcoudert@gmail.com>
2020-12-26 02:07:07 +0000

    oxipng: update 4.0.2 bottle.

--- Formula/oxipng.rb
+++ Formula/oxipng.rb
@@ -8,6 +8,7 @@ class Oxipng < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "22501fe46163d2c9c88dc66fdf1babc314e58d151cf0ce2547f742d846da0d97" => :big_sur
+    sha256 "37060778a44559e4b1f12deca73c61c724877c97f86a39e77eef294799962076" => :arm64_big_sur
     sha256 "d06a103b94a394483f8bd916e0d30812ccdf46254591ae56000a81fcd68b2713" => :catalina
     sha256 "fa5dc699252f50ecab26aea7edfe4c8e5fdcd21d06dbdf5efa2a09a79f3c3187" => :mojave
   end
