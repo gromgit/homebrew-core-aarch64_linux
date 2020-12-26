367f1dbbaf682671d62bc448658b058009007374
Rui Chen <rui@meetup.com>
2020-12-26 00:28:05 -0500

    scour 0.38.2
    * scour 0.38.2
    * scour: add livecheck
    * scour: bump version scheme
    
    Closes #67735.
    
    Signed-off-by: Rui Chen <rui@meetup.com>
    Co-authored-by: Seeker <meaningseeking@protonmail.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/scour.rb
+++ Formula/scour.rb
@@ -3,11 +3,15 @@ class Scour < Formula
 
   desc "SVG file scrubber"
   homepage "https://www.codedread.com/scour/"
-  url "https://github.com/scour-project/scour/archive/v038.1.tar.gz"
-  sha256 "0d2f88170305d54b143410276ff84da98e3ae9c36abe52430d9c2b510fa77884"
+  url "https://files.pythonhosted.org/packages/75/19/f519ef8aa2f379935a44212c5744e2b3a46173bf04e0110fb7f4af4028c9/scour-0.38.2.tar.gz"
+  sha256 "6881ec26660c130c5ecd996ac6f6b03939dd574198f50773f2508b81a68e0daf"
   license "Apache-2.0"
-  revision 1
+  version_scheme 1
   head "https://github.com/scour-project/scour.git"
+
+  livecheck do
+    url :stable
+  end
 
   bottle do
     cellar :any_skip_relocation
