995f17a3dfa27b14fbb6604f835e7e0fcbd460d7
FX Coudert <fxcoudert@gmail.com>
2020-12-26 02:17:27 +0000

    rust-analyzer: update 2020-12-21 bottle.

--- Formula/rust-analyzer.rb
+++ Formula/rust-analyzer.rb
@@ -10,6 +10,7 @@ class RustAnalyzer < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "cf1ca044c6683c8eaec18ab5af44f62fff22003979c369e47d8b43773c28b37d" => :big_sur
+    sha256 "cd4ea8739c270340b5aca642b02afaf82e362bb235ff62fa689000f3270c0475" => :arm64_big_sur
     sha256 "46655a896e916fe30e299ccf09985fce3e0ee24c90fa5d83083025f496284f5d" => :catalina
     sha256 "7c384905a810f88a7610fb7eeee844855df63a4695b73b468bbd8d834adea91c" => :mojave
   end
