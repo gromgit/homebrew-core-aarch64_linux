abf464b31a84df01d940aaa5661214a563d1432c
FX Coudert <fxcoudert@gmail.com>
2020-12-26 17:26:48 +0000

    elixir: update 1.11.2 bottle.

--- Formula/elixir.rb
+++ Formula/elixir.rb
@@ -9,6 +9,7 @@ class Elixir < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "9b7654b37bdb51dfd52e4b794153f305fa1b847e29d0d02427325e8e8cd1fb1a" => :big_sur
+    sha256 "e10203444858ca74fadbcf6383c2c3aaf866ff44a9c6337ad82769953c269dc1" => :arm64_big_sur
     sha256 "44de459466702ce2534b211cf19bb0855a4af2ee7cc8b472574137fc8c1dcd0d" => :catalina
     sha256 "72c8d551c7a3c884b5be801fd64b03ea0d6ec00da8a7fd8dec2fbccb29afdf45" => :mojave
     sha256 "81ef2b9d5b6de22ef96175761afa61809eafc94719d3a67df85ef074217380a5" => :high_sierra
