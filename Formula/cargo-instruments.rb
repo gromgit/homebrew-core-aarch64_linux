0ed3251abc58d4aa6663b3469b7854dcf5f1b600
FX Coudert <fxcoudert@gmail.com>
2020-12-26 01:22:41 +0000

    cargo-instruments: update 0.3.4 bottle.

--- Formula/cargo-instruments.rb
+++ Formula/cargo-instruments.rb
@@ -8,6 +8,7 @@ class CargoInstruments < Formula
   bottle do
     cellar :any
     sha256 "131aaf3d9347d12853503afdb1bdd94b27e84ff665e105a35eeac2edb3e7d736" => :big_sur
+    sha256 "3b55dda8cb06ac96e43545dbc5424170d3ce43dbd8450191af3ba32e0a059184" => :arm64_big_sur
     sha256 "eec9683c797fc7e8d800f6131b61998619390a8723bb5fe3171e185c72d53caf" => :catalina
     sha256 "ede51dd3eead58236d0de240a86f649e00c584be7a438792ff3eb04a9287ad4a" => :mojave
   end
