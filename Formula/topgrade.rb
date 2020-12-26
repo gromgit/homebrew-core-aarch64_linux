33e60bd8aecbed3542350e828b8a5dcd706b624d
FX Coudert <fxcoudert@gmail.com>
2020-12-26 02:32:57 +0000

    topgrade: update 6.1.1 bottle.

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -8,6 +8,7 @@ class Topgrade < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "03ad4c55d161e794eebc21d4148ebb0c6ae3c6441eff70f1bf736e5dbe287704" => :big_sur
+    sha256 "e6bf578a0312967a5cd6947adeac5889aeeec00b224d920213ace6c73a4e5e33" => :arm64_big_sur
     sha256 "18adf109b719ca194203f371b7e26ab69fe5078b65f3643467488c007fc8009a" => :catalina
     sha256 "d43f6d94667974fdb90c5475748e34c9673a3b3dfdbbdca195410b1d1ef07d03" => :mojave
   end
