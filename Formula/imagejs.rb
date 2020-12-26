e7d73e2c2b37f5df0cbba43b4633c7ae04147b6e
Mitchell Blank Jr <mitch@bodyfour.com>
2020-12-26 10:32:09 +0000

    imagejs: remove unreachable homepage (#67752)

--- Formula/imagejs.rb
+++ Formula/imagejs.rb
@@ -1,10 +1,15 @@
 class Imagejs < Formula
   desc "Tool to hide JavaScript inside valid image files"
-  homepage "https://jklmnn.de/imagejs/"
+  homepage "https://github.com/jklmnn/imagejs"
   url "https://github.com/jklmnn/imagejs/archive/0.7.2.tar.gz"
   sha256 "ba75c7ea549c4afbcb2a516565ba0b762b5fc38a03a48e5b94bec78bac7dab07"
-  license "GPL-3.0"
+  license "GPL-3.0-only"
   head "https://github.com/jklmnn/imagejs.git"
+
+  livecheck do
+    url :head
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
 
   bottle do
     cellar :any_skip_relocation
