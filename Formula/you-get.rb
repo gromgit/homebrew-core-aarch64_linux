720a809113cc5040cadf4153dca5210354625f0f
Rui Chen <rui@meetup.com>
2020-12-26 19:05:54 -0500

    you-get 0.4.1500
    Closes #67789.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/you-get.rb
+++ Formula/you-get.rb
@@ -3,8 +3,8 @@ class YouGet < Formula
 
   desc "Dumb downloader that scrapes the web"
   homepage "https://you-get.org/"
-  url "https://files.pythonhosted.org/packages/8e/f2/14b34acc03f2185fc24cba33da0d757d6c265149d7b7776c6940008a620e/you-get-0.4.1488.tar.gz"
-  sha256 "28aec2f15e86ea1cbf9900827ade41388aa3f1ac43b4ab49999bce48f37cf9c3"
+  url "https://files.pythonhosted.org/packages/b3/eb/e24fa3352fa27e1be3b7f8b3d158a3b12f6ee294c09614eb61b5d85fa054/you-get-0.4.1500.tar.gz"
+  sha256 "5a6cc0d661fe0cd4210bf467d6c89afd8611609e402690254722c1415736da92"
   license "MIT"
   head "https://github.com/soimort/you-get.git", branch: "develop"
 
@@ -23,11 +23,6 @@ class YouGet < Formula
 
   depends_on "python@3.9"
   depends_on "rtmpdump"
-
-  resource "PySocks" do
-    url "https://files.pythonhosted.org/packages/bd/11/293dd436aea955d45fc4e8a35b6ae7270f5b8e00b53cf6c024c83b657a11/PySocks-1.7.1.tar.gz"
-    sha256 "3f8804571ebe159c380ac6de37643bb4685970655d3bba243530d6558b799aa0"
-  end
 
   def install
     virtualenv_install_with_resources
