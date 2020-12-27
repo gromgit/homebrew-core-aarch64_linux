953a235bcb07d88e5a96d0dc8ff6315488699437
FX Coudert <fxcoudert@gmail.com>
2020-12-27 19:23:50 +0000

    t-rec: update 0.3.1 bottle.

--- Formula/t-rec.rb
+++ Formula/t-rec.rb
@@ -8,6 +8,7 @@ class TRec < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "cbcb07617527b300131eef20b2146f9122fe547b611cbf1b1ef35b4a36484120" => :big_sur
+    sha256 "1417f82dcc41d5c95bbb8ad8051e890ad641eaf830711aeb759d903c7bff9201" => :arm64_big_sur
     sha256 "59b687bbdebebdec22b2c6b71993e71d6f2bc9d0c91ef0b78c7ec14e98e78a80" => :catalina
     sha256 "73f405fa2311aeef26f0ce46ec77fca484b281cbeff48bb3430d3e3b143096f5" => :mojave
   end
