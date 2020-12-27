555b0deb492240d767ecdfc12dbb0849ff705a54
FX Coudert <fxcoudert@gmail.com>
2020-12-27 21:13:42 +0000

    wbox: update 5 bottle.

--- Formula/wbox.rb
+++ Formula/wbox.rb
@@ -7,6 +7,7 @@ class Wbox < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "21c7d9bd3cdbc504149109c8f9c1b3245ce31cc1a34536e326703b4e108e4c8b" => :big_sur
+    sha256 "e87df4ac144cb716bd7528c15170376927aebc7f50d72dcd704ff4a5d3b45246" => :arm64_big_sur
     sha256 "512db23fe4356c552e240cf3457ff50225bb6d01fd786df356fb39e4b8f18288" => :catalina
     sha256 "bce72c30f26da03ab104082abaccd775fa721b4db98a0ae6e16e2946f59a8bed" => :mojave
     sha256 "6e8b41d8caf8ae84cf5ff0d7fe9bdb7d83b9c7afae9a746fe319e67fe145cf2c" => :high_sierra
