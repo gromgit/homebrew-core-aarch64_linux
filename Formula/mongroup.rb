f785d6983cd7c34ce8f8f38f58b1284e1d82d36c
FX Coudert <fxcoudert@gmail.com>
2020-12-27 23:31:43 +0000

    mongroup: update 0.4.1 bottle.

--- Formula/mongroup.rb
+++ Formula/mongroup.rb
@@ -8,6 +8,7 @@ class Mongroup < Formula
     cellar :any_skip_relocation
     rebuild 1
     sha256 "d2112e201508ee22715b2e318b2fd97ae0cf4044e1d74d0893abf12598fb72bc" => :big_sur
+    sha256 "bc40264f75aafc4f194f10d2b7472e152a4c9f58ec104db6ebeac3dc346c4370" => :arm64_big_sur
     sha256 "57c107a2746fae7e9db832f54df3d5170449defc30334094939794288659f026" => :catalina
     sha256 "4c11751013bae001ff2dcf55c3566613e83fe0d9257e0691c9da7b2aec298918" => :mojave
     sha256 "230996b629ff1a72b405ba6c7fbb8cdd0fd06292b16bacf124bc2e30c5f9917e" => :high_sierra
