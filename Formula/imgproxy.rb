1da815af850d5ea02e14550a1214cea8233b3780
FX Coudert <fxcoudert@gmail.com>
2020-12-27 21:38:44 +0000

    imgproxy: update 2.15.0_1 bottle.

--- Formula/imgproxy.rb
+++ Formula/imgproxy.rb
@@ -10,6 +10,7 @@ class Imgproxy < Formula
   bottle do
     cellar :any
     sha256 "ccd0a102ec452a68a26280f0173534784d0a7797de833bafbc2015027634fc0a" => :big_sur
+    sha256 "215159ed8fcf0216e0f81cb38c460316349828680d5376358c21ba6ee77138b2" => :arm64_big_sur
     sha256 "8cefa46ab480ea6b763077f8d12b0783e9482355aa2b1bf99ee2c8126279f8c6" => :catalina
     sha256 "ca7688ab9b51b8f6e71d664bd210ccacefd66c08f16fce12fd26b89b8017358e" => :mojave
     sha256 "7572ce24b608a282fc82231c0d2455f03dcc15953df73de4f302bf11375fb39c" => :high_sierra
