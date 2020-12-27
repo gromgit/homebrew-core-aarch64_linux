4edc00575c839b942453413a3c5231cd07a024f2
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2020-12-27 12:20:01 +0000

    agda: update 2.6.1.2 bottle.

--- Formula/agda.rb
+++ Formula/agda.rb
@@ -18,9 +18,10 @@ class Agda < Formula
   end
 
   bottle do
-    sha256 "42684dd2f90978479dabdac6c5069009eda3897c7fba8729e24649a26239ae4a" => :big_sur
-    sha256 "4267f9a1f33d064bc6fd65c1142e74e230a2b33f3a86dcf04d0f6e3974f1ba27" => :catalina
-    sha256 "bbc5b2aae1544a7e61858a6fdd4f859da8537d664f4d9640be6c1544909f461c" => :mojave
+    rebuild 1
+    sha256 "23ff8d4cfc8b39f2059a3c92b999a48548d8efb0546494438a337f716017c2f6" => :big_sur
+    sha256 "656972ecb09c1fea73920f0d4c8a3666581d2e347036d0b0df398063062aad20" => :catalina
+    sha256 "1059ee247f77d6175f182e8fe96ee1b8ffa7000efa9385583c9a041516592292" => :mojave
   end
 
   head do
