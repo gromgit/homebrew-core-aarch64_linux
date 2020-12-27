adfe781ab930eb162878382b9c9c0715bd63ffd4
FX Coudert <fxcoudert@gmail.com>
2020-12-27 15:21:01 +0000

    mozjpeg: update 4.0.0 bottle.

--- Formula/mozjpeg.rb
+++ Formula/mozjpeg.rb
@@ -11,10 +11,11 @@ class Mozjpeg < Formula
   end
 
   bottle do
-    sha256 "e57dce5b59377e8a411d161ad5f3c774289bcca43cdd36925eb395674cc6ed50" => :big_sur
-    sha256 "d0beedd400a16f5bf2feac98a80133673cefce67ce2480de792088a524765110" => :catalina
-    sha256 "d15b8408ce94dcc80d4fcfe4d484b65f540a70f8427952c52e2486a985aaae58" => :mojave
-    sha256 "1ab02dcf47af9ccdbecc73ead82521f4e14c5d8c24181dd234e6ed822119bdd6" => :high_sierra
+    rebuild 1
+    sha256 "3009c242ebe825a0c9150839f62a92a8a317725f9411bbfe93a0881c8a7436eb" => :big_sur
+    sha256 "b76cd545bd4829c589107a5c723086bda97d9060dd76d5b2626d45502b279487" => :arm64_big_sur
+    sha256 "71f02633dddb7ea71193a7c21e1da0a6546fcd7c5ce10a725001b64aebf32a0f" => :catalina
+    sha256 "4ddb9a3d5b4618528eae92f95d4b08090cfefe8797402aa27da0712ef7121a09" => :mojave
   end
 
   keg_only "mozjpeg is not linked to prevent conflicts with the standard libjpeg"
