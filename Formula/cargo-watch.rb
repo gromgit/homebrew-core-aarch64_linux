990593f5d8fc2b3f6ac07b1731d6cf390c066197
chenrui <chenrui333@gmail.com>
2020-12-27 04:34:50 +0000

    cargo-watch: add 7.5.0 bottle.

--- Formula/cargo-watch.rb
+++ Formula/cargo-watch.rb
@@ -6,6 +6,13 @@ class CargoWatch < Formula
   license "CC0-1.0"
   head "https://github.com/passcod/cargo-watch.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "07d9a040ecf1f8fa64404f3fe54d1fee1942e947c1840a35630eacae1589d844" => :big_sur
+    sha256 "60215fc98f1860979b64335e42e77720c9eecdc0c915e134118d3e6b3cb40703" => :catalina
+    sha256 "581b7f6d973525e0565179a7aa2cf0127ae2af70882159d602046ac910e30eed" => :mojave
+  end
+
   depends_on "rust" => :build
 
   def install
