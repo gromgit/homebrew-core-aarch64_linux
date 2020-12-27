e2a8a38bd8c66980d1bea8987551897c9cfc65df
chenrui <chenrui333@gmail.com>
2020-12-27 22:15:29 +0000

    git-cal: update 0.9.1_1 bottle.

--- Formula/git-cal.rb
+++ Formula/git-cal.rb
@@ -14,14 +14,9 @@ class GitCal < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "d409cf68f427e71e33abba6f0393860230882cb86a6cf58f8771135516d3afe2" => :catalina
-    sha256 "b6b4128e6c8dfc678bf4301a506795533a3c1a20a449d342e57b78cd3274db5d" => :mojave
-    sha256 "286c10cc47b2b26de4511fac0518d6edef3e05c676a45c14b32d8342891ecf2f" => :high_sierra
-    sha256 "82847887556bd0334e65c1c7a3a063c2d62e5f71e81e89c53aa8e0df1cc41e31" => :sierra
-    sha256 "8f928d65dc3414900ace5751d9e93bc712f03ffcabfefeb0d659e18db3998622" => :el_capitan
-    sha256 "d61d27644f236dd221d2ea15aea7ee088afaf08c305217084b63fb85cb0d7dba" => :yosemite
-    sha256 "2f09af33dd202d56be91b6bb7ead320cbc322332e411a7b712879a9198364e64" => :mavericks
+    sha256 "1d191bdf9da21ef2dbe3eeb3909fbf738df652931b5ee9876b9868b429644899" => :big_sur
+    sha256 "ee5e258bbc598978be1d2e3e3220c28b7ef1ff4d7e5a34bdcc852107f68b5f67" => :catalina
+    sha256 "80bbebc06dc4f05e6aa34324276650f303a714efe857e72f67861d7cf9194451" => :mojave
   end
 
   def install
