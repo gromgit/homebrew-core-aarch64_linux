35e4a05c8033d618346f215e2bc44de855d2267b
Jonathan Chang <me@jonathanchang.org>
2020-12-28 20:04:24 +0000

    pnpm: update 5.14.1 bottle.

--- Formula/pnpm.rb
+++ Formula/pnpm.rb
@@ -13,10 +13,11 @@ class Pnpm < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "dd799b43f9dd51078051f56915dd0bcc83237d5f119584bdd93395bbb31e5201" => :big_sur
-    sha256 "80475de050edf456e69724313c06215adaa67cc153a41786eb7e4a6027c4bc86" => :arm64_big_sur
-    sha256 "eed9eef6ffdd632c209b5d0d82926c36ae5f901d38f406da49c81446e355ec5d" => :catalina
-    sha256 "054d9fb35acc10419475cf32b7e2b44d591c549cd3aaeeb37a08df553cd42a8c" => :mojave
+    rebuild 1
+    sha256 "98aa6e906837e49c1941fd90678fb0cfe738d8d5caeee8b0880f03acb538249e" => :big_sur
+    sha256 "ace839c620622bd4fbfaca3d1b110b790abdecbb7fe403ca850ff15c706f50cc" => :arm64_big_sur
+    sha256 "e76e31ac6d87291971c98bcf741e60eb41b0d6e486553462adeb38608cc0893f" => :catalina
+    sha256 "642f3a15f9065c4cce643bce6bf8f8252ec4168ac1a825c48cb04c68ef1edc1c" => :mojave
   end
 
   depends_on "node"
