365ba73d943e960a0030813d065694ff82a7977d
Steffen Kötte <steffen.koette@gmail.com>
2020-12-28 00:31:26 -0800

    repo 2.10 (#67800)
    * repo 2.10
    
    * repo: adjust url to main repo instead of mirror

--- Formula/repo.rb
+++ Formula/repo.rb
@@ -1,9 +1,9 @@
 class Repo < Formula
   desc "Repository tool for Android development"
   homepage "https://source.android.com/source/developing.html"
-  url "https://github.com/GerritCodeReview/git-repo.git",
-      tag:      "v2.9",
-      revision: "4b325813fc0ba3f6525a9bacb4daf78fccccc3f6"
+  url "https://gerrit.googlesource.com/git-repo.git",
+      tag:      "v2.10",
+      revision: "352c93b68084e97ec7a3c09f0bd3f2a161290211"
   license "Apache-2.0"
   version_scheme 1
 
