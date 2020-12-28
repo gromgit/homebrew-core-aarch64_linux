74ff3893498bad9232c9d19878a2ee77ddcc0eb6
FX Coudert <fxcoudert@gmail.com>
2020-12-28 00:14:21 +0000

    yaze-ag: update 2.40.5 bottle.

--- Formula/yaze-ag.rb
+++ Formula/yaze-ag.rb
@@ -8,6 +8,7 @@ class YazeAg < Formula
 
   bottle do
     sha256 "fa3420cfae542f27c772d484109f1a930fa82525c3349d7a4ea7bdbc8ad42649" => :big_sur
+    sha256 "ee904628f8d8bdcafb50f18e4909d0d5a3cffe73f864ae66214fa91c8fabaa92" => :arm64_big_sur
     sha256 "f250f5ad984f31c1f96c744b81195c96bdccce6f74dd7548ceed19ba1172c117" => :catalina
     sha256 "86fb203ac02bad9477b7d3c7b78022df5feb126ae08df3ff93238d766f08a362" => :mojave
     sha256 "9f3e2a6e51423a97f03e99ed2bca0c7778fcf4f6b223332a824743bdbad20e09" => :high_sierra
