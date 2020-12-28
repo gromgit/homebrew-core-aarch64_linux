af1a642171f78dde126eefa25b1e321ca16808d7
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-12-28 13:03:56 +0100

    libarchive: revision bump
    Closes #67865.
    
    Signed-off-by: Jonathan Chang <me@jonathanchang.org>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libarchive.rb
+++ Formula/libarchive.rb
@@ -4,6 +4,7 @@ class Libarchive < Formula
   url "https://www.libarchive.org/downloads/libarchive-3.5.1.tar.xz"
   sha256 "0e17d3a8d0b206018693b27f08029b598f6ef03600c2b5d10c94ce58692e299b"
   license "BSD-2-Clause"
+  revision 1
 
   livecheck do
     url "https://libarchive.org/downloads/"
