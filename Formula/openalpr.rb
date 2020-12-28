435c28688d37569a1bdb9759632856e1fc60f9ec
Jonathan Chang <me@jonathanchang.org>
2020-12-28 03:03:08 +0000

    openalpr: add 2.3.0 bottle.

--- Formula/openalpr.rb
+++ Formula/openalpr.rb
@@ -5,6 +5,12 @@ class Openalpr < Formula
   sha256 "1cfcaab6f06e9984186ee19633a949158c0e2aacf9264127e2f86bd97641d6b9"
   license "AGPL-3.0-or-later"
 
+  bottle do
+    sha256 "8cf1530f286a3223c92ff60f13f377f6a1016b1bca53dd85b91b7f2c273aa6e7" => :big_sur
+    sha256 "82858b6fa8b3882e8e7a890fd256420b2aa0dff86ad79071b26f256cb7823b26" => :catalina
+    sha256 "4cbe016b79dbe619488e08f3be8d89b573fb4307437efc837fe8be16172f4625" => :mojave
+  end
+
   depends_on "cmake" => :build
   depends_on "leptonica"
   depends_on "libtiff"
