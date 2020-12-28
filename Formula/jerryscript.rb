af0b8e4a9b14b02e374555e252b1633765403059
FX Coudert <fxcoudert@gmail.com>
2020-12-28 20:24:08 +0000

    jerryscript: update 2.3.0 bottle.

--- Formula/jerryscript.rb
+++ Formula/jerryscript.rb
@@ -9,6 +9,7 @@ class Jerryscript < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "3242e1c2c1490eda68d6cd82c83c2173285a84356d3c2c5d16fb2dcd1e07f577" => :big_sur
+    sha256 "ad2ad8226a865b0417ed391f360a1a45f710325246d2c64c4013bc443335e4be" => :arm64_big_sur
     sha256 "30cce6deddb3cd879374dd6e80fc0a7ec7fcf32aa5b940713c60ee520b9b030a" => :catalina
     sha256 "aecaf8cb9cc69ed2ac8694691f3f8b40b16fe72ce30f1a571244aa9cbe0c0591" => :mojave
     sha256 "8c9890bfc739d8903d493316d3d1e258f6883d15115d247a8493aa259823cf47" => :high_sierra
