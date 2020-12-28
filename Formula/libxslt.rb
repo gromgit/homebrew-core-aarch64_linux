5985978221acca8d48d9f45b0f084bec510f6568
Francois-Xavier Coudert <fxcoudert@gmail.com>
2020-12-28 13:06:44 +0100

    libxslt: revision bump
    Closes #67872.
    
    Signed-off-by: Jonathan Chang <me@jonathanchang.org>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libxslt.rb
+++ Formula/libxslt.rb
@@ -4,7 +4,7 @@ class Libxslt < Formula
   url "http://xmlsoft.org/sources/libxslt-1.1.34.tar.gz"
   sha256 "98b1bd46d6792925ad2dfe9a87452ea2adebf69dcb9919ffd55bf926a7f93f7f"
   license "X11"
-  revision 1
+  revision 2
 
   livecheck do
     url "http://xmlsoft.org/sources/"
