34d1f7b9b37743b94491e743e6dd2609282cd57d
Jonathan Chang <me@jonathanchang.org>
2020-12-28 22:40:30 +1100

    cocoapods: revision bump
    Closes #67856.
    
    Signed-off-by: Jonathan Chang <me@jonathanchang.org>

--- Formula/cocoapods.rb
+++ Formula/cocoapods.rb
@@ -4,7 +4,7 @@ class Cocoapods < Formula
   url "https://github.com/CocoaPods/CocoaPods/archive/1.10.0.tar.gz"
   sha256 "d44c70047020ea114ca950df6d4499b1f86e5e7bbd3536a46f6fa3e32f1df21e"
   license "MIT"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "d55424b1e441e989fe4865369e0322739bce49806accea459f5e726677e532d7" => :big_sur
