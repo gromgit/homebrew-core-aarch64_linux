9d3775718d3ce9bb2911e21245add295f3de429e
FX Coudert <fxcoudert@gmail.com>
2020-12-28 00:10:50 +0000

    rakudo: update 2020.12 bottle.

--- Formula/rakudo.rb
+++ Formula/rakudo.rb
@@ -8,6 +8,7 @@ class Rakudo < Formula
 
   bottle do
     sha256 "83076a1b58459f6a0e491f4346002d99ebec4bbe7930ed2768ac49d356878bb0" => :big_sur
+    sha256 "037e2f8a092fba323ac8aa8356c2623902284e3bd281d48187e4b135f487b146" => :arm64_big_sur
     sha256 "52360ea3f95f2bf105d09733a6ba6bd4f3f0664ac3e838e3b21020020dcd1d52" => :catalina
     sha256 "b2cef9b265ce3245062cb4bc92eced599807104f63f918bab1a22605de2828a9" => :mojave
   end
