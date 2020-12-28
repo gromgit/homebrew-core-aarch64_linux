fd63cd9b03394d524ff1bae934a7e4bbc198820e
Jonathan Chang <me@jonathanchang.org>
2020-12-28 18:43:55 +0000

    radare2: update 5.0.0 bottle.

--- Formula/radare2.rb
+++ Formula/radare2.rb
@@ -7,10 +7,11 @@ class Radare2 < Formula
   head "https://github.com/radareorg/radare2.git"
 
   bottle do
-    sha256 "ec8134b5087e85f7c7fc1e99705107167d4dd7a702405bb423286ed754e32fbc" => :big_sur
-    sha256 "6d6c03f6ce57a453498d49ba7438d2d49bcab9227a3a69b27eea6aa1b669d791" => :arm64_big_sur
-    sha256 "4e89bca3b80f1adbbcbc375ca69766584fdc02de638e04b6f2aab441d0d31184" => :catalina
-    sha256 "9b47bdbfc447a4f4a3ece3375209b6a378b444d06f1f5b1b8902de9ccdb9494a" => :mojave
+    rebuild 1
+    sha256 "cb89eeb473aaf506f0a1a0ef5de94d27ce9051dc25a307807e58b81f6ff77def" => :big_sur
+    sha256 "806bf4d9bdd9714586cee4a32efb3cf75c735b7329a05e34fa79aa6cce1ee17f" => :arm64_big_sur
+    sha256 "2e7491f612ffa9161ed34670cf345f52998b39a99cbe6c7fb3f660e2ac02e592" => :catalina
+    sha256 "d40bc7db7d6a8e16bc41aa3db7e622bf324e99af7283501c595140aaeacbe286" => :mojave
   end
 
   def install
