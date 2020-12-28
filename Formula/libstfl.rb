904604005f7c64ed27216ea4587d09aff9217c66
Jonathan Chang <me@jonathanchang.org>
2020-12-28 18:54:32 +0000

    libstfl: update 0.24_12 bottle.

--- Formula/libstfl.rb
+++ Formula/libstfl.rb
@@ -12,10 +12,11 @@ class Libstfl < Formula
 
   bottle do
     cellar :any
-    sha256 "2be12f503697bd427bac888ef5a45672c9fa29a3a3d5952d7d4e1f363ab5d646" => :big_sur
-    sha256 "b6ac2ed270bf310230646658cf69b0029d7ef8c44bfb12dbd059e66dee62e148" => :arm64_big_sur
-    sha256 "8f01970ef49df0a256cae4e9b487a70b30e34b2958a742f486911a19df5195f3" => :catalina
-    sha256 "7ed2e7bb46d76f5c8db0ae28e5adb586d9f1bde981a4e221ec64f8cea8ba9ded" => :mojave
+    rebuild 1
+    sha256 "9e57ebd3ab126b784b46969fa3c2baf49b4351183274d23f3075370e50735d48" => :big_sur
+    sha256 "f38715316e6a6a3df8b25e6cd97c742ac2168d608ecdb2bcd7773e3e69117802" => :arm64_big_sur
+    sha256 "ab1e06a1a46305fbcaadca0e490dd6584f149e704f1475e9d5243acebeaff7e1" => :catalina
+    sha256 "0164c390d3ccad76079fe8b5af6e8cec440036e4e97dd91c5a1c86848832c0ab" => :mojave
   end
 
   depends_on "swig" => :build
