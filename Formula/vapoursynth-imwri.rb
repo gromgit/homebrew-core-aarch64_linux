71db337c629fd630ddb01f552557e351de4ba568
FX Coudert <fxcoudert@gmail.com>
2020-12-28 00:20:48 +0000

    vapoursynth-imwri: update 52 bottle.

--- Formula/vapoursynth-imwri.rb
+++ Formula/vapoursynth-imwri.rb
@@ -9,6 +9,7 @@ class VapoursynthImwri < Formula
   bottle do
     cellar :any
     sha256 "cfcc6facbd1f40b54d6d0530187233f0db36bb71bda5095d5b49ef17c70ab74b" => :big_sur
+    sha256 "bec45bc1302ecfededdb61c0f8624dcb1f72af4b76ca6d9444e7a41f7c7e92b3" => :arm64_big_sur
     sha256 "11f86645d5a0eebd7250373a5ac4a04a6df630366afada2fc3f1af8d0ea69793" => :catalina
     sha256 "07794b0baa5f4a3f544d6bd0a1d990066596c8c99d03f29474a33bca101ec408" => :mojave
   end
