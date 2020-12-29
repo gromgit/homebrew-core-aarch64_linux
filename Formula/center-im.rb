f43ecad5b1a2abb9f8ac35347bc2701d9e04dd8c
FX Coudert <fxcoudert@gmail.com>
2020-12-29 13:10:02 +0000

    center-im: update 4.22.10_2 bottle.

--- Formula/center-im.rb
+++ Formula/center-im.rb
@@ -8,6 +8,7 @@ class CenterIm < Formula
   bottle do
     rebuild 1
     sha256 "2b44902a2be528a4d9cae18e3b402691dc54a4c2241e72827a74bafe422d85cf" => :big_sur
+    sha256 "182513b7096a23e8888d0d76858ad1c1d2ef92648f8f3d4140e291c41224ccbb" => :arm64_big_sur
     sha256 "11a339b812d7fa164fce8e873e837d1ab07256e73ce0c4e483eeb60327ef6fa6" => :catalina
     sha256 "42a8b8f09b9530139c5d9eaf7c83a435962c61631eea00a13bf70a670044c7a2" => :mojave
     sha256 "9b40fc34ba5177765f01bdd821bec40377f44828421509491d90fb7a329ba400" => :high_sierra
