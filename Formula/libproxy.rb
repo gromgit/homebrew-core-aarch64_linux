f340b905ee94f3a208d7d6efa19933d5781c4e92
chenrui <rui@chenrui.dev>
2020-12-29 22:10:10 +0000

    libproxy: update 0.4.17 bottle.

--- Formula/libproxy.rb
+++ Formula/libproxy.rb
@@ -7,10 +7,10 @@ class Libproxy < Formula
   head "https://github.com/libproxy/libproxy.git"
 
   bottle do
-    sha256 "f3d87ec7e6d5ee417691c26a776886216436ab1b190bf6674c8dede66da0ec00" => :big_sur
-    sha256 "868bf3c73324f4e9275dfe482be9b23a251563ba12f51a7c9d2df678fc4b439b" => :arm64_big_sur
-    sha256 "76cde5260a836b3ce6c3ed0d1e588c29159018d702866209fe36c0be24995603" => :catalina
-    sha256 "69e02ca786abaa1fb825995039e67750f2315a8d93206c15ec60e839830c0bf7" => :mojave
+    sha256 "d094201c939cfab859da673186809a6c7a24b9a216829b862a1bb53059309d4c" => :big_sur
+    sha256 "aa72de0f8f5be2c730d84f20308df804c156e61ff321de0a4b63ba5623517ab7" => :arm64_big_sur
+    sha256 "c847a5adafa14e2614351edc46fdf1f8884908912845a9e425ce30925bb55e32" => :catalina
+    sha256 "5f6f14d95746e1b4c3328f23c7d9018e7e6a1fab70eba1255276ad89c0c405e5" => :mojave
   end
 
   depends_on "cmake" => :build
