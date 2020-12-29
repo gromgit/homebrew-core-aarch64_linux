20547bba97ea825371a16f079f99da4409a8e479
Jonathan Chang <me@jonathanchang.org>
2020-12-29 03:48:54 +0000

    libpst: update 0.6.75 bottle.

--- Formula/libpst.rb
+++ Formula/libpst.rb
@@ -12,11 +12,11 @@ class Libpst < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "158b068e4645da1b01bd850f4f9c43d294908fb119fd39ea3459cf2e5f9723e7" => :big_sur
-    sha256 "6f245e65af86df879b94b2d043e9bc17dee7f1c307606d23d8aa0829524d620b" => :arm64_big_sur
-    sha256 "9362205843b828388fe50aa733c114886210bbd2f2a8304f3916ecaf606e1cda" => :catalina
-    sha256 "763deea9a814a76b5e5964e0de71bf27c6e22549df6ba5bec16c3e340cd1b80d" => :mojave
+    rebuild 2
+    sha256 "6f48557a8529e0bc989baaa72788c52289896194e069217bf8fe5cc771207a22" => :big_sur
+    sha256 "669e325cb32cbad435d86606d40012aee6d9101b2ffbc6efc9fa101e9bcdf97f" => :arm64_big_sur
+    sha256 "cbf301e72e23ecad7be367063b933bb9ce0ea430f5af413ad44f71b04e4ccae3" => :catalina
+    sha256 "b5dff8dd482a5688ce97bc7407ad7a18d620dc264ba1962e155e862ae2973d2b" => :mojave
   end
 
   depends_on "pkg-config" => :build
