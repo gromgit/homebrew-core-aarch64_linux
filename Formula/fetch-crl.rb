471132123dcfed07c28d40432cb8da2db2763b79
FX Coudert <fxcoudert@gmail.com>
2020-12-29 11:07:22 +0000

    fetch-crl: update 3.0.21 bottle.

--- Formula/fetch-crl.rb
+++ Formula/fetch-crl.rb
@@ -13,6 +13,7 @@ class FetchCrl < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "a4c9b13b21861b5d6f591234a814e7fafcf83622aa2aefa4d11d64352538d017" => :big_sur
+    sha256 "96804f5b491baeb338b76d851e8f7f3b06c509e6105abb7c9b826060c0c83980" => :arm64_big_sur
     sha256 "7c4aedc9178b36cf45d9a05ed4213c5c2ede584dc1c2754f2370b91f42a1efe3" => :catalina
     sha256 "7c4aedc9178b36cf45d9a05ed4213c5c2ede584dc1c2754f2370b91f42a1efe3" => :mojave
     sha256 "7c4aedc9178b36cf45d9a05ed4213c5c2ede584dc1c2754f2370b91f42a1efe3" => :high_sierra
