f7136f74a5536db36909cc03d60d8e9900784006
Jonathan Chang <me@jonathanchang.org>
2020-12-29 03:53:28 +0000

    ncmpc: update 0.42 bottle.

--- Formula/ncmpc.rb
+++ Formula/ncmpc.rb
@@ -12,10 +12,11 @@ class Ncmpc < Formula
 
   bottle do
     cellar :any
-    sha256 "0f9ea71e567da470344475a361e294ac08abb30c3d7accf802e224fbe9caf4c7" => :big_sur
-    sha256 "a9ccd098b4e0277f21e167f1bd99ee59595bceeddc0dc81a0cef22ff07744e3b" => :arm64_big_sur
-    sha256 "fd422c49a76b2d474a474357aa44c2340f109b9309d4045698d2be1a94d37775" => :catalina
-    sha256 "79a2da8c50a7d3cec1ed463d98b97a46a497f7b2a8b52d4ea99f7a460d0c47fd" => :mojave
+    rebuild 1
+    sha256 "dcaf0d0af1cff22c8e2913fd55fef28a0205ca0ef88d38955e7dabaa06e3e241" => :big_sur
+    sha256 "812169b73cb47055ee1574ff7278afa59ea92b70cafead63992052e7b193f4c0" => :arm64_big_sur
+    sha256 "b94f6244a29cbc00d222d8b938fc44a550c2779490b1323c0d2df65eda0585c6" => :catalina
+    sha256 "ada5b8151c3e9801a270cb9c5c20cc5d24770b0d6afb71ab6ac773eccdb221d2" => :mojave
   end
 
   depends_on "boost" => :build
