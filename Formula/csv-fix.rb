246143411b51f88c2cf190d7871dfb05ed9c1302
Rui Chen <chenrui333@gmail.com>
2020-12-29 00:45:30 -0500

    cvs-fix: remove livecheck, update reason
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/csv-fix.rb
+++ Formula/csv-fix.rb
@@ -5,10 +5,6 @@ class CsvFix < Formula
   sha256 "32982aa0daa933140e1ea5a667fb71d8adc731cc96068de3a8e83815be62c52b"
   license "MIT"
 
-  livecheck do
-    skip "Bitbucket repository is missing"
-  end
-
   bottle do
     cellar :any_skip_relocation
     sha256 "3cc10313342650c680f23241e19ff8ec6b64df8fcc2123c5161b15e992c8973b" => :catalina
@@ -20,7 +16,7 @@ class CsvFix < Formula
     sha256 "0b86933c8e32830d5abd0f26ef83b1a60e0254da67542b695fd50ab1e3ba2e68" => :mavericks
   end
 
-  disable! date: "2020-12-08", because: :unmaintained
+  disable! date: "2020-12-08", because: :repo_removed
 
   def install
     # clang on Mt. Lion will try to build against libstdc++,
