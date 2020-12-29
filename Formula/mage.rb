a5a1c30d2e5c08e294cb1b55bd57c3c48f16d9ee
Rui Chen <rui@chenrui.dev>
2020-12-29 02:28:42 -0500

    mage 1.11.0
    Closes #67937.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/mage.rb
+++ Formula/mage.rb
@@ -2,8 +2,8 @@ class Mage < Formula
   desc "Make/rake-like build tool using Go"
   homepage "https://magefile.org"
   url "https://github.com/magefile/mage.git",
-      tag:      "v1.10.0",
-      revision: "9a10961401323a8a888d46e35d5a59d7433e092b"
+      tag:      "v1.11.0",
+      revision: "07afc7d24f4d6d6442305d49552f04fbda5ccb3e"
   license "Apache-2.0"
 
   bottle do
@@ -18,13 +18,15 @@ class Mage < Formula
   depends_on "go"
 
   def install
-    ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/magefile/mage").install buildpath.children
-    cd "src/github.com/magefile/mage" do
-      system "go", "run", "bootstrap.go"
-      bin.install buildpath/"bin/mage"
-      prefix.install_metafiles
-    end
+    commit = Utils.safe_popen_read("git", "rev-parse", "--short", "HEAD").chomp
+
+    ldflags = %W[
+      -s -w
+      -X github.com/magefile/mage/mage.timestamp=#{Date.today}
+      -X github.com/magefile/mage/mage.commitHash=#{commit}
+      -X github.com/magefile/mage/mage.gitTag=#{version}
+    ]
+    system "go", "build", *std_go_args, "-ldflags", ldflags.join(" ")
   end
 
   test do
