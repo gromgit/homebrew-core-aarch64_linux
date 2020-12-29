a0cba22b646980cfb0fa56ebf2e6a0c76e936123
FX Coudert <fxcoudert@gmail.com>
2020-12-29 14:09:28 +0000

    wait_on: update 1.1 bottle.

--- Formula/wait_on.rb
+++ Formula/wait_on.rb
@@ -8,6 +8,7 @@ class WaitOn < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "b7a8f90a892e9b2cbe4455d9fd4aedfdf7b65c3534e00b8849e94652171bd4e3" => :big_sur
+    sha256 "ca26e2aeef0c80f7b61b392ea50b03f7465bcdc0c8c3be2248aca538935fdc78" => :arm64_big_sur
     sha256 "befef0facd28d26c22ed14197e74c1bff41c9cd2f787ae4bee59027cfb89b2c7" => :catalina
     sha256 "500c8192d5cc65f906844fcca12f837d1e498d396ac60f368c195cf773bb6d4d" => :mojave
     sha256 "ca2dfaf2a1449ae1066826846cbdc9963efb837277f258c57304669f8615ad0f" => :high_sierra
