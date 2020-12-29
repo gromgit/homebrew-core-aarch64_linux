8caaaf534cdb539ba653326aaeb1928d0d1309d9
Evan Miller <emmiller@gmail.com>
2020-12-29 15:33:07 -0500

    mdbtools: update homepage (#67980)

--- Formula/mdbtools.rb
+++ Formula/mdbtools.rb
@@ -1,6 +1,6 @@
 class Mdbtools < Formula
   desc "Tools to facilitate the use of Microsoft Access databases"
-  homepage "https://github.com/brianb/mdbtools/"
+  homepage "https://github.com/mdbtools/mdbtools/"
   url "https://github.com/mdbtools/mdbtools/releases/download/v0.9.0/mdbtools-0.9.0.tar.gz"
   sha256 "8ce95f62c32f9c5c1c1dcb1c853a35b735e2158bf5ceb0c041e2e9557ff536af"
   license "GPL-2.0-or-later"
