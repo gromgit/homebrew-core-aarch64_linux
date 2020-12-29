4eab54845a3dc6b11f79869636268eb147329a8f
Jonathan Chang <me@jonathanchang.org>
2020-12-29 21:59:57 +1100

    cvs-fast-export: update 1.55 bottle.

--- Formula/cvs-fast-export.rb
+++ Formula/cvs-fast-export.rb
@@ -7,10 +7,10 @@ class CvsFastExport < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "af7e3b2fa6fd3c2e4095364bbf13af5e1aa9c00f7ff066f95f2b8485ce4b22b5" => :big_sur
-    sha256 "57328518c731d726a7c7f4d1af902638c042711e79f73e34002d71f08a70d432" => :catalina
-    sha256 "70e1bc966678021164929473ca45b68b3fc3a1daa94646ab75c1e24d409ab4d6" => :mojave
-    sha256 "b95ad7499c774198531e33bbfc0dc6872af239d17e40a9e9fbf7102de8fdb067" => :high_sierra
+    rebuild 1
+    sha256 "7ee763f03cb997d6717bad92d53983324e4bb89be855aedb37af0ef38cec8180" => :big_sur
+    sha256 "e6faed55a91a59f97af42d5bb102d3438aab4509e5c7fd4e5d28f238012de2f5" => :catalina
+    sha256 "b2b8f37662e652697231b8efb9fe6cb4927513f849d4853b54fbe90220d55d77" => :mojave
   end
 
   depends_on "asciidoc" => :build
