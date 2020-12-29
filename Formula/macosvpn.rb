13a17c4973690b09a90f86a309f9102507a2bdbf
Jonathan Chang <me@jonathanchang.org>
2020-12-29 02:59:43 +0000

    macosvpn: update 1.0.3 bottle.

--- Formula/macosvpn.rb
+++ Formula/macosvpn.rb
@@ -12,10 +12,11 @@ class Macosvpn < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "fe46251025193a2b7ab448c8094cae06b1671b60ed176421726e791dd05b64b9" => :big_sur
-    sha256 "e02bb2714e5ad70a64e47efdecf2fd37072ca23a9c0bb462131c0c4b52fc1743" => :arm64_big_sur
-    sha256 "c08986d8e595ec51aa48a309197bdfbdb6daa2bf3b470352a7f931daf54f740e" => :catalina
-    sha256 "e8e62ac32fd934867c66b405a6b9f91c25574a34451827f8cff259e0235482dc" => :mojave
+    rebuild 1
+    sha256 "115f97b8ad4c7242c0baa1ec4990024c6984b758febf7109e9831a6ddd25fe5b" => :big_sur
+    sha256 "6862be0cd39d91775f3336d40da31d7e7f6f645e767e77efbbf132a6b25f9955" => :arm64_big_sur
+    sha256 "d56030780813971500593d4ccf3d672da95fc69655c432107a378877d9e2a38e" => :catalina
+    sha256 "3d65153cae182cf1d98625bda798c191d348ff22a32f6992bcec0a6de5df4d0f" => :mojave
   end
 
   depends_on xcode: ["11.1", :build]
