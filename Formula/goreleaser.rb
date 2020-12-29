3835c08168f073942ff32fcee81b0d0cf6f81498
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-29 19:35:49 +0000

    goreleaser: update 0.150.0 bottle.

--- Formula/goreleaser.rb
+++ Formula/goreleaser.rb
@@ -8,10 +8,10 @@ class Goreleaser < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ef98a47f59fa7139fe76678ee085328a19be0a734eeb46197f5d45595f5d7b99" => :big_sur
-    sha256 "6717b2c0b2f94a02e94b8a8c88c970feda84bb136b8e4cf1b92b5d9bab0f9dcd" => :arm64_big_sur
-    sha256 "ec932595c140e164728f532fbc31ed7148c8f60bac76db1784cd6f4d0aeeb2aa" => :catalina
-    sha256 "78a6e30e81b8cfdcf2251b69bed8478bf53160abc56884e62700f339619072c2" => :mojave
+    sha256 "bf1681fe79400c15578cb5cb144fa55b05b49c1033c5a5cee50a3299cca14f8a" => :big_sur
+    sha256 "14142d10a07a4577b6e3fa544781c8112292c7095c4ddb546cf799c780a2aeb7" => :arm64_big_sur
+    sha256 "233279814ac8f7578b03d93e354ebf9b77714216da6eec5ccee9c25b8a7a5d28" => :catalina
+    sha256 "47ebbc0657f2457524af0457b050e34f142b92ca4c0f14f9a6d1aa13f659f632" => :mojave
   end
 
   depends_on "go" => :build
