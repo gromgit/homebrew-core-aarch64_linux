dd0275f67740fa2011b0ee85bbaab57e1c83ee59
George Lund <george.lund@digital.cabinet-office.gov.uk>
2020-12-30 20:56:31 +0000

    xmlsectool 3.0.0 (#68018)
    * xmlsectool 3.0.0
     - avoid use of 'latest' directory as URI breaks with each release
     - also updated link to home page which is version-specific
    
    * Add license per note on https://wiki.shibboleth.net/confluence/display/XSTJ3/Home

--- Formula/xmlsectool.rb
+++ Formula/xmlsectool.rb
@@ -1,9 +1,9 @@
 class Xmlsectool < Formula
   desc "Check schema validity and signature of an XML document"
-  homepage "https://wiki.shibboleth.net/confluence/display/XSTJ2/xmlsectool+V2+Home"
-  url "https://shibboleth.net/downloads/tools/xmlsectool/latest/xmlsectool-2.0.0-bin.zip"
-  sha256 "83ea1e977d947dd5f2f5600a3280bc5d372a4913f0dbce1ddf2c0ca1c869ca57"
-  revision 1
+  homepage "https://wiki.shibboleth.net/confluence/display/XSTJ3/Home"
+  url "https://shibboleth.net/downloads/tools/xmlsectool/3.0.0/xmlsectool-3.0.0-bin.zip"
+  sha256 "5b430dda0bf78df224495b39f83cf043d96c0ab9c8ccaa23fbdb56068c46abbc"
+  license "Apache-2.0"
 
   bottle :unneeded
 
