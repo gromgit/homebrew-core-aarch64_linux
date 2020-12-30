b8f6e1a76409628ddfa41390c99514446b25e116
Jonathan Chang <me@jonathanchang.org>
2020-12-30 12:06:48 +1100

    nicotine-plus: update homepage
    Closes #68000.
    
    Signed-off-by: Jonathan Chang <me@jonathanchang.org>

--- Formula/nicotine-plus.rb
+++ Formula/nicotine-plus.rb
@@ -2,7 +2,7 @@ class NicotinePlus < Formula
   include Language::Python::Virtualenv
 
   desc "Graphical client for the Soulseek file sharing network"
-  homepage "https://nicotine-plus.org/"
+  homepage "https://nicotine-plus.github.io/nicotine-plus/"
   url "https://files.pythonhosted.org/packages/1e/d5/35536e21b33b881d2cd13c79f6404691a23268eafad00300214788b3ca2c/nicotine-plus-2.2.2.tar.gz"
   sha256 "6913aabd98cb841d6c05213f8004300c2e90d9afdaf5aa081269b272494762f5"
   license "GPL-3.0-or-later"
