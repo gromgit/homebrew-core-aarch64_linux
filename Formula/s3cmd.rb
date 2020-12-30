fa0c5a94dff665b5997c6dc8e9090a9bbfcb71c8
FX Coudert <fxcoudert@gmail.com>
2020-12-30 11:09:25 +0000

    s3cmd: update 2.1.0_2 bottle.

--- Formula/s3cmd.rb
+++ Formula/s3cmd.rb
@@ -16,6 +16,7 @@ class S3cmd < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "e664b592e99416b945694ac030208b0ff12c41d9cfd7905dea7f9a5bc46d577d" => :big_sur
+    sha256 "e2625bb936c5d4b7965544f722e94acef3565a4844735eedd036b70b3d11ad80" => :arm64_big_sur
     sha256 "140b574c93db1f67b40b7cf22e8468bbcb066ecbbfffb8cf649b31d268a82775" => :catalina
     sha256 "c76760a661e4c9438bd2d0b7016430a99a934ac8e6705727b55bc868a466031d" => :mojave
     sha256 "6c2188352b0662521ee7dac484183e0e0a65a1f802e282dad4d5287fa3c955e6" => :high_sierra
