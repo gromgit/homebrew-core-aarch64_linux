b44164d729b83ed286239fb5ad4514bca8111f00
John Benninghoff <jbenninghoff@mac.com>
2020-12-30 09:47:02 -0600

    cvs-fast-export: add head, livecheck
    Co-authored-by: Sam Ford <1584702+samford@users.noreply.github.com>

--- Formula/cvs-fast-export.rb
+++ Formula/cvs-fast-export.rb
@@ -5,12 +5,22 @@ class CvsFastExport < Formula
   sha256 "af58e16667c6e02e8431ff666effe306d3b30086fab483170257890da1afc21b"
   license "GPL-2.0-or-later"
 
+  livecheck do
+    url :homepage
+    regex(/href=.*?cvs-fast-export[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
   bottle do
     cellar :any_skip_relocation
     rebuild 1
     sha256 "7ee763f03cb997d6717bad92d53983324e4bb89be855aedb37af0ef38cec8180" => :big_sur
     sha256 "e6faed55a91a59f97af42d5bb102d3438aab4509e5c7fd4e5d28f238012de2f5" => :catalina
     sha256 "b2b8f37662e652697231b8efb9fe6cb4927513f849d4853b54fbe90220d55d77" => :mojave
+  end
+
+  head do
+    url "https://gitlab.com/esr/cvs-fast-export.git"
+    depends_on "bison" => :build
   end
 
   depends_on "asciidoc" => :build
