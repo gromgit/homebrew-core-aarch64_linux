8b2c5f8a3ae039cf624505e27049bf14a1a4ef9e
Rui Chen <rui@chenrui.dev>
2020-12-30 15:24:40 -0500

    asciiquarium: add perl dependency declaration
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/asciiquarium.rb
+++ Formula/asciiquarium.rb
@@ -21,6 +21,8 @@ class Asciiquarium < Formula
     sha256 "6b20abf264f40c7123e40f0f34cfc11f0c12a03b1a74a324e3f3a7ae75e94f3f" => :yosemite
   end
 
+  uses_from_macos "perl"
+
   resource "Curses" do
     url "https://cpan.metacpan.org/authors/id/G/GI/GIRAFFED/Curses-1.34.tar.gz"
     sha256 "808e44d5946be265af5ff0b90f3d0802108e7d1b39b0fe68a4a446fe284d322b"
