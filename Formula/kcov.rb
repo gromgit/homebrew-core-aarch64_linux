4e13b70bd40a411e1f524b2d95916ce8d448abbb
Alexander Bayandin <a.bayandin@gmail.com>
2020-12-30 17:15:20 +0000

    kcov: do not depend on macos
    Closes #68025.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kcov.rb
+++ Formula/kcov.rb
@@ -26,7 +26,6 @@ class Kcov < Formula
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "python@3.9" => :build
-  depends_on :macos # Due to Python 2
 
   def install
     mkdir "build" do
