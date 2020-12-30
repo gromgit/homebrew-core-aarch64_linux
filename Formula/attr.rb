3e3639c4e8e41fa453c8328edb77d2d2b0f86800
chenrui <chenrui333@gmail.com>
2020-12-30 14:28:01 -0500

    attr: add livecheck (#68007)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/attr.rb
+++ Formula/attr.rb
@@ -1,9 +1,15 @@
 class Attr < Formula
   desc "Manipulate filesystem extended attributes"
   homepage "https://savannah.nongnu.org/projects/attr"
-  url "https://mirror.csclub.uwaterloo.ca/nongnu/attr/attr-2.4.48.tar.gz"
+  url "https://download.savannah.nongnu.org/releases/attr/attr-2.4.48.tar.gz"
+  mirror "https://mirror.csclub.uwaterloo.ca/nongnu/attr/attr-2.4.48.tar.gz"
   sha256 "5ead72b358ec709ed00bbf7a9eaef1654baad937c001c044fe8b74c57f5324e7"
   license "GPL-2.0-or-later"
+
+  livecheck do
+    url "https://download.savannah.nongnu.org/releases/attr/"
+    regex(/href=.*?attr[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
 
   depends_on "gettext" => :build
   depends_on :linux
