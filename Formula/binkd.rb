acf3af585db72d554b8b6dd5620c726f62d11093
FX Coudert <fxcoudert@gmail.com>
2020-12-30 16:01:21 +0000

    binkd: update 1.0.4 bottle.

--- Formula/binkd.rb
+++ Formula/binkd.rb
@@ -9,6 +9,7 @@ class Binkd < Formula
     cellar :any_skip_relocation
     rebuild 1
     sha256 "3b309f2c4a6ee43441a32c0396a570454737bb85c5467f3e27a8c666140bda59" => :big_sur
+    sha256 "4cd8c44619d3c04b646a1ea8ad7db21feb95e950656c78d5dc1a983d6472b469" => :arm64_big_sur
     sha256 "263219b174c193cd86454770d838013daa797994d8d21cdca45493a59d8a5a79" => :catalina
     sha256 "1582b44b77979d7258c51baec8bb54f367fc21f8cc03838c9c1b1351ea9f77e7" => :mojave
     sha256 "e890bed8ae5c89dfabd589e2c9654b2c8da6811bd24fbfd99aa4fc520a535e26" => :high_sierra
