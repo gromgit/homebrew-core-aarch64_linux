9ad2586f3120e322bdce9b21f1bfbf79d2f4e8e6
Mitchell Blank Jr <mitch@bodyfour.com>
2020-12-30 15:05:27 +0000

    libevhtp: point homepage url at github repo (#68016)
    The criticalstack.com URL redirects to a generic technology page
    at capitalone.com which "brew audit --strict --online" doesn't approve
    of.  The page doesn't seem to have much libevhtp-specific content
    so let's just point at the github page instead.

--- Formula/libevhtp.rb
+++ Formula/libevhtp.rb
@@ -1,6 +1,6 @@
 class Libevhtp < Formula
   desc "Create extremely-fast and secure embedded HTTP servers with ease"
-  homepage "https://criticalstack.com/"
+  homepage "https://github.com/criticalstack/libevhtp/"
   url "https://github.com/criticalstack/libevhtp/archive/1.2.18.tar.gz"
   sha256 "316ede0d672be3ae6fe489d4ac1c8c53a1db7d4fe05edaff3c7c853933e02795"
   license "BSD-3-Clause"
