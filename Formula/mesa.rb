006d4f001a5d3c37770ec831d0b3dea7a8552180
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-31 11:28:58 +0000

    mesa: update 20.3.2 bottle.

--- Formula/mesa.rb
+++ Formula/mesa.rb
@@ -13,10 +13,10 @@ class Mesa < Formula
   end
 
   bottle do
-    sha256 "5f27f0ebe825e601032269563dc0952469d717c36330f1f7168132142731818a" => :big_sur
-    sha256 "4c13e1d6783317c73e5affe7ff2a1ff70924eea3f0816c562c1b30a5fe187616" => :arm64_big_sur
-    sha256 "0e11b188a0f24b12fccbfb7e0a20843ac73bfd4184be0a6c4d1f57e0ff942d7d" => :catalina
-    sha256 "5c68d4275cddb201114e2a1f5bf3bba0909e66b5fac3f67388aab0823dcc77eb" => :mojave
+    sha256 "118d01556e5a7410cc01527da2087cd82e557ddf85c6f39bb53bdb80b9e21879" => :big_sur
+    sha256 "fd0368e10f736aebc0709ada911ad45d28cfd73e75f63e3197868c4715364f33" => :arm64_big_sur
+    sha256 "72afadeb2d338adb20f2d2b20c473355e2f4128091d1ce14650a0d57533971a0" => :catalina
+    sha256 "8456a4b58b0288bfed4756410475fc3cb5894d74a895d19d9b4733fd4df4360d" => :mojave
   end
 
   depends_on "meson" => :build
