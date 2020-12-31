b02206e9798315c9823c678d6ae14fd7f385f02d
FX Coudert <fxcoudert@gmail.com>
2020-12-31 16:50:48 +0000

    gifski: update 1.2.4 bottle.

--- Formula/gifski.rb
+++ Formula/gifski.rb
@@ -8,6 +8,7 @@ class Gifski < Formula
   bottle do
     cellar :any
     sha256 "20e76add05177bebdbee36334ad956f521c1702f440e50c85b63d306f951575f" => :big_sur
+    sha256 "528c768120c91ded3d7ffdcdd4b3bc4f4daf33e678e98ff123324312fff8e0db" => :arm64_big_sur
     sha256 "df5f0713070679c35ef4d96c04cfe41c814cd6b10193746376ca98af636b46fb" => :catalina
     sha256 "8b495ff4083d519ff85a62bf0a1f18bdc43f537575abc2709d6d873367bf4b1d" => :mojave
     sha256 "cf2bb34c28d2eb6e610e130798638dc68712cd6337520c409e950abc516f4f59" => :high_sierra
