53425ede6f3ba1242b7c40cbaa3e777fed678b3d
Rui Chen <rui@chenrui.dev>
2020-12-31 13:14:45 -0500

    mame 0.227
    * mame 0.227
      remove patch
    
      add comment
    * move pugixml as runtime dep
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    Co-authored-by: rui <rui@meetup.com>

--- Formula/mame.rb
+++ Formula/mame.rb
@@ -1,9 +1,10 @@
 class Mame < Formula
   desc "Multiple Arcade Machine Emulator"
   homepage "https://mamedev.org/"
-  url "https://github.com/mamedev/mame/archive/mame0226.tar.gz"
-  version "0.226"
-  sha256 "7c4c9ec232ba988e65fd29665c9b8e40b5ac3aa9f561eeb107cebbf08ba94baf"
+  # NOTE: Please keep these values in sync with rom-tools.rb when updating.
+  url "https://github.com/mamedev/mame/archive/mame0227.tar.gz"
+  version "0.227"
+  sha256 "95dbce00a4f05a35f66ef966fe9efad1e4e78ce62e0eba3f7031dfa6737829a5"
   license "GPL-2.0-or-later"
   head "https://github.com/mamedev/mame.git"
 
@@ -27,7 +28,6 @@ class Mame < Formula
 
   depends_on "glm" => :build
   depends_on "pkg-config" => :build
-  depends_on "pugixml" => :build
   depends_on "python@3.9" => :build
   depends_on "rapidjson" => :build
   depends_on "sphinx-doc" => :build
@@ -37,18 +37,13 @@ class Mame < Formula
   depends_on macos: :high_sierra
   depends_on "portaudio"
   depends_on "portmidi"
+  depends_on "pugixml"
   depends_on "sdl2"
   depends_on "sqlite"
   depends_on "utf8proc"
 
   uses_from_macos "expat"
   uses_from_macos "zlib"
-
-  # Disable BGFX threading. Remove in next version.
-  patch do
-    url "https://github.com/mamedev/mame/commit/48d1f0de37fc6c429051dd1bcd1a49dbef581b1a.patch?full_index=1"
-    sha256 "9f9aba588ab1e82d927d1c101780415672c54cb463e80d435be3cb1d0ec34217"
-  end
 
   def install
     # Cut sdl2-config's invalid option.
