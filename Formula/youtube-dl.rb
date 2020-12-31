b00ab2e37630f0179cfb103055ade15a8c6cae0b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2020-12-31 05:32:49 +0000

    youtube-dl: update 2020.12.31 bottle.

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -9,10 +9,10 @@ class YoutubeDl < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "60895c95d771ee9bad583614415d9faee69aea72e8a11d1fe5c0286085e479ab" => :big_sur
-    sha256 "de918e2b96aeb4663bdcb47f330001d3f79f08e8f063577b2d3887edf4dbbee6" => :arm64_big_sur
-    sha256 "9da1c597f91306484fb10e772f428307f9df9ef1b4a4440ebea9867eefd7cc87" => :catalina
-    sha256 "9c3cf92a9a3f21b6124e096a9a76ca876a634a47516f9c5c0fcc9dbf584c17c0" => :mojave
+    sha256 "17996d3eb68214578340e389340f40ce00c4f48263d160fa6cc6302e3b5d3145" => :big_sur
+    sha256 "44650dec017b1d3729bccac21a41ae0eefc32092083de5e64bf886566933dd5a" => :arm64_big_sur
+    sha256 "8d258573cddaa987f7422e06fd7aa4f2c05b213616f265e559a8262c20adc1a1" => :catalina
+    sha256 "0f8e413d475812290e4abb16565abc89cb3a16bc44e0961c85bc11a1d9505fc7" => :mojave
   end
 
   depends_on "python@3.9"
