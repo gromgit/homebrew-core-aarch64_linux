6c5d8b7fab8aab709004aeca06b33e4be9457d55
Tim Byrne <sultan@locehilios.com>
2020-12-31 00:28:28 -0600

    yadm 3.0.0 (#68053)
    * yadm 3.0.0
    
    * Update license

--- Formula/yadm.rb
+++ Formula/yadm.rb
@@ -1,21 +1,22 @@
 class Yadm < Formula
   desc "Yet Another Dotfiles Manager"
   homepage "https://yadm.io/"
-  url "https://github.com/TheLocehiliosan/yadm/archive/2.5.0.tar.gz"
-  sha256 "d022d118a1a477e09afa00c80e10bd94b150d99709e57f01ba48ca2eaaeeb2de"
-  license "GPL-3.0"
+  url "https://github.com/TheLocehiliosan/yadm/archive/3.0.0.tar.gz"
+  sha256 "af968c815817e9de85b60dc9c9a7e6159ed34e4f91ea78cadbcd6c73a0301c06"
+  license "GPL-3.0-or-later"
 
   bottle :unneeded
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
-    bash_completion.install "completion/yadm.bash_completion"
-    zsh_completion.install  "completion/yadm.zsh_completion" => "_yadm"
+    bash_completion.install "completion/bash/yadm"
+    fish_completion.install "completion/fish/yadm.fish"
+    zsh_completion.install "completion/zsh/_yadm"
   end
 
   test do
     system bin/"yadm", "init"
-    assert_predicate testpath/".config/yadm/repo.git/config", :exist?, "Failed to init repository."
+    assert_predicate testpath/".local/share/yadm/repo.git/config", :exist?, "Failed to init repository."
     assert_match testpath.to_s, shell_output("#{bin}/yadm gitconfig core.worktree")
 
     # disable auto-alt
