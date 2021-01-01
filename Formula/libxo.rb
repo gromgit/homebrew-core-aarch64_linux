8285615414540cecf2eb4a6b661b57e0e2b6d4a8
FX Coudert <fxcoudert@gmail.com>
2021-01-01 12:06:49 +0000

    libxo: update 1.4.0 bottle.

--- Formula/libxo.rb
+++ Formula/libxo.rb
@@ -7,6 +7,7 @@ class Libxo < Formula
 
   bottle do
     sha256 "b076f04180c037c1aa92d74773b8a2577b6a2af803ed05d3b7238a94c3eea17b" => :big_sur
+    sha256 "c84ed94e5db633d8cfd3d1bf7a3391ee47b292f6ba92c8fefcc2c764e02cd380" => :arm64_big_sur
     sha256 "3dffbef9394a617fa8c901c44ce7b31b8843c947762744a8ea1374780d5c1224" => :catalina
     sha256 "9c137a2e9828de98fb201d4d8da74eda20e6a276d90a83d8d1fa3017ba7059d1" => :mojave
     sha256 "59d5d434c2ec21fb06dd22b87780e82a31b6eebfd9db795fafece858bc1e4755" => :high_sierra
