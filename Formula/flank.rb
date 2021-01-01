70c150ec8a67fe9195b1c9c1c3819a81c2888e2c
chenrui <chenrui333@gmail.com>
2021-01-01 18:46:45 -0500

    flank 21.01.0 (#68142)

--- Formula/flank.rb
+++ Formula/flank.rb
@@ -1,8 +1,8 @@
 class Flank < Formula
   desc "Massively parallel Android and iOS test runner for Firebase Test Lab"
   homepage "https://github.com/Flank/flank"
-  url "https://github.com/Flank/flank/releases/download/v20.12.0/flank.jar"
-  sha256 "9acb1065436e83e581bfc1e9e2c44e450cee6831587a24dbb2f2d7f95bc41737"
+  url "https://github.com/Flank/flank/releases/download/v21.01.0/flank.jar"
+  sha256 "2ee22e0c495666ca2b8bde34f77d575f7dd9fb37cfb66be76b8b8d4c8918decf"
   license "Apache-2.0"
 
   livecheck do
