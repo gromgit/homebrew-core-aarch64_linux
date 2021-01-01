2ebcc62c5c9287a8e8d00575d184f61e1c8bcb80
Rui Chen <rui@chenrui.dev>
2021-01-01 16:59:37 -0500

    gsasl 1.10.0
    * gsasl 1.10.0
    * gsasl: update license
    
    Closes #68144.
    
    Co-authored-by: chenrui <rui@meetup.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gsasl.rb
+++ Formula/gsasl.rb
@@ -1,10 +1,10 @@
 class Gsasl < Formula
   desc "SASL library command-line interface"
   homepage "https://www.gnu.org/software/gsasl/"
-  url "https://ftp.gnu.org/gnu/gsasl/gsasl-1.8.1.tar.gz"
-  mirror "https://ftpmirror.gnu.org/gsasl/gsasl-1.8.1.tar.gz"
-  sha256 "4dda162a511976bfef14ad1fecd7733719dfd9c361b5d09dc8414ea9d472d8d2"
-  license "GPL-3.0"
+  url "https://ftp.gnu.org/gnu/gsasl/gsasl-1.10.0.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gsasl/gsasl-1.10.0.tar.gz"
+  sha256 "85bcbd8ee6095ade7870263a28ebcb8832f541ea7393975494926015c07568d3"
+  license "GPL-3.0-or-later"
 
   livecheck do
     url :stable
