f932fe748fecd4e5076d749b61d9c8dc03983dd2
FX Coudert <fxcoudert@gmail.com>
2021-01-01 11:53:09 +0000

    minio-mc: update 20201218105353 bottle.

--- Formula/minio-mc.rb
+++ Formula/minio-mc.rb
@@ -16,6 +16,7 @@ class MinioMc < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "218b71bea4be4c2253b65c3d703771d6ed83170457101b597a623f8c3457c148" => :big_sur
+    sha256 "0964617177ed76e4bb3bc0e2470ff5aa704669fb4b57f76d8bfd82d34b2fdeb7" => :arm64_big_sur
     sha256 "a845d3658f70670bea009bd80da55cb2d7329f504f4d8e6390daeaad79b9c24f" => :catalina
     sha256 "5ac98c7afd304272465c645ace6a1796765b0c45990c7fc451b0cb77c744b728" => :mojave
   end
