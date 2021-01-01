b9e65b234c8cd176c41f3eb310da9ae012b1ebfe
FX Coudert <fxcoudert@gmail.com>
2021-01-01 13:17:15 +0000

    opensaml: update 3.2.0 bottle.

--- Formula/opensaml.rb
+++ Formula/opensaml.rb
@@ -13,6 +13,7 @@ class Opensaml < Formula
   bottle do
     cellar :any
     sha256 "220a34c0915da2d3641b88d96615138e7a5341d4e21cfa654300a6ccab16651d" => :big_sur
+    sha256 "28d745aaa6bc776ee02233852c46e2aeb4aceb7bebb18544616636afeb61867a" => :arm64_big_sur
     sha256 "09eb04c9b5475a70c1cd95e13e349c30c650433a0908fb078cf99f7126c4c4c5" => :catalina
     sha256 "24938a715d29e9db821774514452b5b1289ce243c5a48c1a492286234ed8c945" => :mojave
   end
