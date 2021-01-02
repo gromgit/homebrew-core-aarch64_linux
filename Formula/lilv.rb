7c6f18ebdedc74467d5394e733a9f0def7bfed07
Sam Ford <1584702+samford@users.noreply.github.com>
2021-01-01 23:42:39 -0500

    lilv: add livecheck

--- Formula/lilv.rb
+++ Formula/lilv.rb
@@ -5,6 +5,11 @@ class Lilv < Formula
   sha256 "d1bba93d6ddacadb5e742fd10ad732727edb743524de229c70cc90ef81ffc594"
   license "ISC"
 
+  livecheck do
+    url "https://download.drobilla.net/"
+    regex(/href=.*?lilv[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
   bottle do
     cellar :any
     sha256 "ab87185c1c9276cf74a6c060a6f9d7f32f3012e581da421277d9851d860cc921" => :big_sur
