8708c90137f6366991d104d0918bbe896a55cd07
Rui Chen <rui@chenrui.dev>
2021-01-02 14:50:35 -0500

    pdfgrep: revision for poppler
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/pdfgrep.rb
+++ Formula/pdfgrep.rb
@@ -4,6 +4,7 @@ class Pdfgrep < Formula
   url "https://pdfgrep.org/download/pdfgrep-2.1.2.tar.gz"
   sha256 "0ef3dca1d749323f08112ffe68e6f4eb7bc25f56f90a2e933db477261b082aba"
   license "GPL-2.0"
+  revision 1
 
   bottle do
     cellar :any
