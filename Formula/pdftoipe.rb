cbc5b64451f84213e4d9bc8198ef6cb7a1204ac3
Rui Chen <rui@chenrui.dev>
2021-01-02 14:50:35 -0500

    pdftoipe: revision for poppler
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/pdftoipe.rb
+++ Formula/pdftoipe.rb
@@ -4,7 +4,7 @@ class Pdftoipe < Formula
   url "https://github.com/otfried/ipe-tools/archive/v7.2.20.1.tar.gz"
   sha256 "233f5629986ade3d70de6dd1af85d578d6aa0f92f9bcd1ecd4e8e5a94b508376"
   license "GPL-2.0-or-later"
-  revision 4
+  revision 5
 
   bottle do
     cellar :any
