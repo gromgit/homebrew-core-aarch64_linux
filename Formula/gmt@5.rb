9f0c83c456b1d3143a21e3ea3dfc955ae1cf8dfb
Rui Chen <rui@chenrui.dev>
2021-01-02 14:54:18 -0500

    gmt@5: revision for poppler
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/gmt@5.rb
+++ Formula/gmt@5.rb
@@ -6,7 +6,7 @@ class GmtAT5 < Formula
   mirror "https://fossies.org/linux/misc/GMT/gmt-5.4.5-src.tar.gz"
   sha256 "225629c7869e204d5f9f1a384c4ada43e243f83e1ed28bdca4f7c2896bf39ef6"
   license "LGPL-3.0"
-  revision 7
+  revision 8
 
   bottle do
     sha256 "635628e4a75448039d1ecb4bdc1c1ad94a63f4480fb64e6ee096e1ada87b231e" => :big_sur
