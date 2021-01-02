80661794bf2b2541cc41e5fc39f6a04833a240a0
Andreas Sandberg <andreas@sandberg.uk>
2021-01-02 17:16:55 +0000

    boost-mpi: Fix build on Apple silicon
    Patch Boost's build system to build cleanly on Apple silicon. This is
    a port of PR #59257 that fixes the main boost package.
    
    Signed-off-by: Andreas Sandberg <andreas@sandberg.uk>

--- Formula/boost-mpi.rb
+++ Formula/boost-mpi.rb
@@ -19,6 +19,14 @@ class BoostMpi < Formula
   depends_on "boost"
   depends_on "open-mpi"
 
+  # Fix build system issues on Apple silicon. This change has aleady
+  # been merged upstream, remove this patch once it lands in a release.
+  patch do
+    url "https://github.com/boostorg/build/commit/456be0b7ecca065fbccf380c2f51e0985e608ba0.patch?full_index=1"
+    sha256 "e7a78145452fc145ea5d6e5f61e72df7dcab3a6eebb2cade6b4cfae815687f3a"
+    directory "tools/build"
+  end
+
   def install
     # "layout" should be synchronized with boost
     args = %W[
