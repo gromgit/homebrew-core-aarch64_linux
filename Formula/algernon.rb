0a2e48057f878112d77e2872124f700997ec4f53
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-03 09:31:05 +0000

    algernon: update 1.12.11 bottle.

--- Formula/algernon.rb
+++ Formula/algernon.rb
@@ -14,10 +14,9 @@ class Algernon < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f37e15b90dc31fdcecdeba0b30e97387488aa055a6abf98fb8d81cb6430401ac" => :big_sur
-    sha256 "86789f00d6b8d3469c5d3df5087149a72d1673cb7f2b7e31360672a6193e4eb2" => :catalina
-    sha256 "71d88d3e20c865c299b4083ce9143b8f5594be8be2d5e6a511d6d7187b8465c3" => :mojave
-    sha256 "b78c41a051aac0d969b8d7b49507a11e3e1b69990efe22aa88f9d5a5d544eb46" => :high_sierra
+    sha256 "125ca6ca368cd6e9d92cdca03f4d5e1fa0f606c04024a043f4b530dc5350982a" => :big_sur
+    sha256 "1f227a42c3708e7ba3f22a69c88d360d10bfc9f82bee7d0a8d2548a49b815ab1" => :catalina
+    sha256 "40130b27669fc8c7d2b842818096ff1a6db34442cb9f88e06137c45dc8733d06" => :mojave
   end
 
   depends_on "go" => :build
