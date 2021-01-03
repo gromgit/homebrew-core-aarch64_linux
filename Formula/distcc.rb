1ffad06c0e95fc44e177e6a8161f04949a7819b1
Mitchell Blank Jr <mitch@bodyfour.com>
2021-01-03 16:12:51 +0000

    distcc 3.3.4
    Closes #68206.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/distcc.rb
+++ Formula/distcc.rb
@@ -1,10 +1,9 @@
 class Distcc < Formula
   desc "Distributed compiler client and server"
   homepage "https://github.com/distcc/distcc/"
-  url "https://github.com/distcc/distcc/releases/download/v3.3.3/distcc-3.3.3.tar.gz"
-  sha256 "bead25471d5a53ecfdf8f065a6fe48901c14d5008956c318c700e56bc87bf0bc"
-  license "GPL-2.0"
-  revision 2
+  url "https://github.com/distcc/distcc/releases/download/3.3.4/distcc-3.3.4.tar.gz"
+  sha256 "9d4cddfa8cd510f157c6c082145bc0949e6851e96f5ee907b5948bb6896b7d7b"
+  license "GPL-2.0-or-later"
   head "https://github.com/distcc/distcc.git"
 
   bottle do
@@ -15,11 +14,11 @@ class Distcc < Formula
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "python@3.8"
+  depends_on "python@3.9"
 
   resource "libiberty" do
-    url "https://deb.debian.org/debian/pool/main/libi/libiberty/libiberty_20200409.orig.tar.xz"
-    sha256 "1807b6d4c70040d71d5c93abdbcb2c05c9ad4f64ed1802583d1107fcdfc2c722"
+    url "https://ftp.debian.org/debian/pool/main/libi/libiberty/libiberty_20201110.orig.tar.xz"
+    sha256 "91e14f26da5bd65e3e74c036e7b7775aec17204fde62aea4b12b686eff2e3911"
   end
 
   def install
@@ -34,6 +33,9 @@ class Distcc < Formula
     end
     ENV.append "LDFLAGS", "-L#{buildpath}/libiberty"
     ENV.append_to_cflags "-I#{buildpath}/include"
+
+    # Fix for https://github.com/distcc/distcc/issues/408
+    inreplace "src/util.c", /\bsd_is_socket_internal/, "not_sd_is_socket_internal"
 
     # Make sure python stuff is put into the Cellar.
     # --root triggers a bug and installs into HOMEBREW_PREFIX/lib/python2.7/site-packages instead of the Cellar.
