8affe66ea0b5a6682940c4304dc99a621fcb5b1f
Rui Chen <rui@chenrui.dev>
2021-01-03 20:09:33 -0500

    pdfpc: update license
    Closes #68172.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/pdfpc.rb
+++ Formula/pdfpc.rb
@@ -3,7 +3,7 @@ class Pdfpc < Formula
   homepage "https://pdfpc.github.io/"
   url "https://github.com/pdfpc/pdfpc/archive/v4.4.1.tar.gz"
   sha256 "4adb42fd1844a7e2ab44709dd043ade618c87f2aaec03db64f7ed659e8d3ddad"
-  license "GPL-2.0-or-later"
+  license "GPL-3.0-or-later"
   revision 1
   head "https://github.com/pdfpc/pdfpc.git"
 
