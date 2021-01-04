0f6044cdcc3ef3544bf886b3a586e841b69bd016
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-04 02:27:41 +0000

    minicom: update 2.8 bottle.

--- Formula/minicom.rb
+++ Formula/minicom.rb
@@ -11,14 +11,10 @@ class Minicom < Formula
   end
 
   bottle do
-    sha256 "37646be23a39a2c08c798221a12997e9309c9ac68ded36cb94e407f2fafe2ec9" => :big_sur
-    sha256 "6121d7bcee3dc81cbcbc4efb1b84b5d5250fe0dc97c3b076a6131cd002f6fbfc" => :arm64_big_sur
-    sha256 "9a95136e2a0176a25874a996fd93ff2e77da00b17e450fc532c85bbdc80edfd3" => :catalina
-    sha256 "77cc043be40a99634caa99d5f309741d85309fed07c1c21f313c6d99c4732966" => :mojave
-    sha256 "ec4beca9c9816db86a3bb7a11d7507fe0740feb62461341232a425a5156cd4a1" => :high_sierra
-    sha256 "63584b5ee8463dfb6cef69ad32308c51a4e83778dd44b80fc4c1e7c40cb48b2e" => :sierra
-    sha256 "820aae10f1c298350f51f7571d4d6becb4b0cfc876fb77126ea1e43bec8466e4" => :el_capitan
-    sha256 "5f17b6f15c2417acbda3a91b64f7df166b29fd2389adc52f011e2541f1fdbcb9" => :yosemite
+    sha256 "ac0a7c58888a3eeb78bbc24d8a47fa707d7e3761c4b28f46527434d49e254b55" => :big_sur
+    sha256 "396aa4bed62d6a9162d061ff1b97a1c5fe25e5a890141d4f39c1849564e3521f" => :arm64_big_sur
+    sha256 "9cee8e5839a3e19aa732307ee70246b1567ddc3a643ef39aa91b6d888301f6e5" => :catalina
+    sha256 "e2b702dec206101120ce947ca2a999c9f5fe7e8c62f95b65091146b865acb268" => :mojave
   end
 
   def install
