0e275e269c4c13ae89c446dc8a4ac0e1633efda1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-04 21:50:51 +0000

    pv: add license
    Taken from COPYING file in source tarball.
    
    Closes #68300.
    
    Signed-off-by: chenrui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pv.rb
+++ Formula/pv.rb
@@ -3,6 +3,7 @@ class Pv < Formula
   homepage "https://www.ivarch.com/programs/pv.shtml"
   url "https://www.ivarch.com/programs/sources/pv-1.6.6.tar.bz2"
   sha256 "608ef935f7a377e1439c181c4fc188d247da10d51a19ef79bcdee5043b0973f1"
+  license "Artistic-2.0"
 
   livecheck do
     url :homepage
