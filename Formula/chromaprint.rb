e33d4fef9d7f9acaa8e5eb9be5d99a9ba8172f4d
Rui Chen <rui@chenrui.dev>
2021-01-04 12:20:25 -0500

    chromaprint: revision for dav1d
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/chromaprint.rb
+++ Formula/chromaprint.rb
@@ -4,7 +4,7 @@ class Chromaprint < Formula
   url "https://github.com/acoustid/chromaprint/releases/download/v1.5.0/chromaprint-1.5.0.tar.gz"
   sha256 "573a5400e635b3823fc2394cfa7a217fbb46e8e50ecebd4a61991451a8af766a"
   license "LGPL-2.1"
-  revision 5
+  revision 6
 
   bottle do
     cellar :any
