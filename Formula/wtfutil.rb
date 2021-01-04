369bd0ce0fa24a5077c97015609efe2429ee68b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-04 01:01:40 +0000

    wtfutil: update 0.35.0 bottle.

--- Formula/wtfutil.rb
+++ Formula/wtfutil.rb
@@ -8,11 +8,10 @@ class Wtfutil < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b4a7f245c9de5a281320cf8aed45b5e2945624b0d857f51596f95d2ed167eb9b" => :big_sur
-    sha256 "ec849266e599cee42eb5d514e3655f20406c870f9a8d3d8fd18cd0c39a31198c" => :arm64_big_sur
-    sha256 "46305f2330fc44b896d093ea784fff0dba2e8d7287b4d6f56e55ad45760c59df" => :catalina
-    sha256 "079056d8d962c9a16227d0c7e0b66e5b930c285b91b7f59d623153e9205bffa5" => :mojave
-    sha256 "b29318bb00155fa79aed26ff3d9b48dd7bec475a42e549c971b536de9159119a" => :high_sierra
+    sha256 "d51620e6cf86db673ce46f663d332e113f06470b3ba276965ae779891b5c2437" => :big_sur
+    sha256 "4f4c122267e04735a6529555cc70d14d3161e941509c54f552a11f6210c2f510" => :arm64_big_sur
+    sha256 "7858804dd5d4555868d15c9fd83d150b980f611cff4731fc7ea004e505a44806" => :catalina
+    sha256 "f3dcb23b05d19acdcd2f1511a193ceab69e45e18beb2a3c88c242dc9922459ea" => :mojave
   end
 
   depends_on "go" => :build
