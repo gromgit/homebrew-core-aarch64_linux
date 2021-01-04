a7f34b95a76a66f4e5377db7ccfbb2c565f5f918
Rui Chen <rui@chenrui.dev>
2021-01-04 11:19:11 -0500

    v2ray 4.34.0
    Closes #68262.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/v2ray.rb
+++ Formula/v2ray.rb
@@ -1,8 +1,8 @@
 class V2ray < Formula
   desc "Platform for building proxies to bypass network restrictions"
   homepage "https://v2fly.org/"
-  url "https://github.com/v2fly/v2ray-core/archive/v4.33.0.tar.gz"
-  sha256 "ce456df0a798e1ed76ec014cb619e89c508bfb812c689260067575ee94e18c76"
+  url "https://github.com/v2fly/v2ray-core/archive/v4.34.0.tar.gz"
+  sha256 "b250f569cb0369f394f63184e748f1df0c90500feb8a1bf2276257c4c8b81bee"
   license all_of: ["MIT", "CC-BY-SA-4.0"]
   head "https://github.com/v2fly/v2ray-core.git"
 
