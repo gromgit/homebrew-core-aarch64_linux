8604e5619f621cdc083ba55fc34977324e2fce55
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-04 18:00:28 +0000

    aws-okta: update 1.0.10 bottle.

--- Formula/aws-okta.rb
+++ Formula/aws-okta.rb
@@ -7,10 +7,10 @@ class AwsOkta < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e1aa60743bc643a46d18ba5d7d771a95801a315eb24fe36d88b0456adbd9d114" => :big_sur
-    sha256 "f4120a9e99a77029ec99b86459e2a786a06a317ed815d88ba6ce0bfd1be776ca" => :arm64_big_sur
-    sha256 "2cdea0519af672242bc8646bf0532ebbe9457434486111053fd6443763546ad8" => :catalina
-    sha256 "01c36d36db51311dc275cce9b1bddb4b24fc729a9d3747cb4dbd9fae635ab6b4" => :mojave
+    sha256 "881e73a2f0d930d86e6acced82d09337495cc6ccfa75b20e5289dc81292752b2" => :big_sur
+    sha256 "1d00b2be99691ee24c1d5f8991aadb197f098bed8525687c0de2b985e31d8bb9" => :arm64_big_sur
+    sha256 "91ae8da87a6ce6e55d098e937fbd3ceb442f9e7150c01bd6d277b4e22bb408d5" => :catalina
+    sha256 "b015f08f2f46be5f46479e451aed41005e205bccd1aed51e6b4497c6287b8d38" => :mojave
   end
 
   depends_on "go" => :build
