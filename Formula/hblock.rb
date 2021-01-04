e7023e43736ceb0f240ab8139eb32c3d5ef8975b
Héctor Molinero Fernández <hector@molinero.dev>
2021-01-04 21:42:54 +0100

    Update Formula/hblock.rb
    Closes #68289.
    
    Co-authored-by: chenrui <rui@meetup.com>
    Signed-off-by: chenrui <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/hblock.rb
+++ Formula/hblock.rb
@@ -1,6 +1,6 @@
 class Hblock < Formula
   desc "Adblocker that creates a hosts file from multiple sources"
-  homepage "https://github.com/hectorm/hblock"
+  homepage "https://hblock.molinero.dev/"
   url "https://github.com/hectorm/hblock/archive/v3.1.3.tar.gz"
   sha256 "e7dad9febc5205f19b36ce9268a41374931d27e0195a2d7504b62c2d1721ab7f"
   license "MIT"
