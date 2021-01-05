0ccd92d17538d7ce438b96b81131763633e64dcf
Henry Schreiner <henryschreineriii@gmail.com>
2021-01-05 11:49:49 -0500

    pybind11: revision
    * pybind11: python package added
    * Fix audit errors
    
    Closes #68342.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/pybind11.rb
+++ Formula/pybind11.rb
@@ -4,6 +4,7 @@ class Pybind11 < Formula
   url "https://github.com/pybind/pybind11/archive/v2.6.1.tar.gz"
   sha256 "cdbe326d357f18b83d10322ba202d69f11b2f49e2d87ade0dc2be0c5c34f8e2a"
   license "BSD-3-Clause"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -15,14 +16,26 @@ class Pybind11 < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "python@3.9" => :test
+  depends_on "python@3.9"
 
   def install
+    # Install /include and /share/cmake to the global location
     system "cmake", "-S", ".", "-B", "build",
            "-DPYBIND11_TEST=OFF",
            "-DPYBIND11_NOPYTHON=ON",
            *std_cmake_args
     system "cmake", "--install", "build"
+
+    # Install Python package too
+    system Formula["python@3.9"].opt_bin/"python3", *Language::Python.setup_install_args(libexec)
+
+    version = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
+    site_packages = "lib/python#{version}/site-packages"
+    pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
+    (prefix/site_packages/"homebrew-pybind11.pth").write pth_contents
+
+    # Also pybind11-config
+    bin.install Dir[libexec/"bin/*"]
   end
 
   test do
@@ -44,8 +57,17 @@ class Pybind11 < Formula
       example.add(1,2)
     EOS
 
+    version = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
+    site_packages = "lib/python#{version}/site-packages"
+
     python_flags = `#{Formula["python@3.9"].opt_bin}/python3-config --cflags --ldflags --embed`.split
     system ENV.cxx, "-O3", "-shared", "-std=c++11", *python_flags, "example.cpp", "-o", "example.so"
     system Formula["python@3.9"].opt_bin/"python3", "example.py"
+
+    test_module = shell_output("#{Formula["python@3.9"].opt_bin/"python3"} -m pybind11 --includes")
+    assert_match (libexec/site_packages).to_s, test_module
+
+    test_script = shell_output("#{opt_bin/"pybind11-config"} --includes")
+    assert_match test_module, test_script
   end
 end
