b26e8dba061960682e63b364237051854101549f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-05 01:01:47 +0000

    qalculate-gtk: update 3.16.0 bottle.

--- Formula/qalculate-gtk.rb
+++ Formula/qalculate-gtk.rb
@@ -7,10 +7,10 @@ class QalculateGtk < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 "52ad7189e1a9e1b24cc072c807796f0b6f31eff42dbb7cb2202dbff4a65e358f" => :big_sur
-    sha256 "3131939575ad77dfdd4d69dca9455b4ec64d8bb697d93835654b47c7144fd067" => :arm64_big_sur
-    sha256 "d159b4287212ebf82c9f699dd886fcf94712454c3079e57c7c6bd34c99d28895" => :catalina
-    sha256 "2de05c4349f359fbba723e26a13a9f8268ce9e8da907418a977b3c7fd08bcb77" => :mojave
+    sha256 "65abac4ee336a518d1053ba89586224c84d226052d97899844402b648a4255ac" => :big_sur
+    sha256 "1f1d9ad6b73b670101b01b9fabcf310625808eb75f9df8fc8a1ef5b8cdcff296" => :arm64_big_sur
+    sha256 "b2047d43780b60f88c4bd935c115865ae853131746b15f258f93a56b66016046" => :catalina
+    sha256 "7baa64b8011060b0df6364a23754b4da89b7622af1dd44decac89c4a4164a712" => :mojave
   end
 
   depends_on "intltool" => :build
