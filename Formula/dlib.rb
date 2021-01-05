ed60a9f1952fd0faca99b00af2b27b46d07daf22
Francois-Xavier Coudert <fxcoudert@gmail.com>
2021-01-06 00:58:34 +0100

    dlib: remove old fixes

--- Formula/dlib.rb
+++ Formula/dlib.rb
@@ -17,7 +17,6 @@ class Dlib < Formula
   depends_on "cmake" => :build
   depends_on "jpeg"
   depends_on "libpng"
-  depends_on macos: :el_capitan # needs thread-local storage
   depends_on "openblas"
 
   def install
