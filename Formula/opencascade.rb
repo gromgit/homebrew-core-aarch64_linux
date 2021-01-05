8dc51a74e2f772876f9f5353cdb77590f35656a6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-05 21:55:59 +0000

    opencascade: revision
    * opencascade: depend on brewed tcl-tk
      Fixes CI failures found in #67718. See also #67378.
    * fix linkage with tcl-tk
    
    Closes #68353.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/opencascade.rb
+++ Formula/opencascade.rb
@@ -4,6 +4,8 @@ class Opencascade < Formula
   url "https://git.dev.opencascade.org/gitweb/?p=occt.git;a=snapshot;h=refs/tags/V7_5_0;sf=tgz"
   version "7.5.0"
   sha256 "c8df7d23051b86064f61299a5f7af30004c115bdb479df471711bab0c7166654"
+  license "LGPL-2.1-only"
+  revision 1
 
   livecheck do
     url "https://www.opencascade.com/content/latest-release"
@@ -24,8 +26,10 @@ class Opencascade < Formula
   depends_on "freeimage"
   depends_on "freetype"
   depends_on "tbb"
+  depends_on "tcl-tk"
 
   def install
+    tcltk = Formula["tcl-tk"]
     system "cmake", ".",
                     "-DUSE_FREEIMAGE=ON",
                     "-DUSE_RAPIDJSON=ON",
@@ -36,9 +40,14 @@ class Opencascade < Formula
                     "-D3RDPARTY_RAPIDJSON_DIR=#{Formula["rapidjson"].opt_prefix}",
                     "-D3RDPARTY_RAPIDJSON_INCLUDE_DIR=#{Formula["rapidjson"].opt_include}",
                     "-D3RDPARTY_TBB_DIR=#{Formula["tbb"].opt_prefix}",
-                    "-D3RDPARTY_TCL_DIR:PATH=#{MacOS.sdk_path_if_needed}/usr",
-                    "-D3RDPARTY_TCL_INCLUDE_DIR=#{MacOS.sdk_path_if_needed}/usr/include",
-                    "-D3RDPARTY_TK_INCLUDE_DIR=#{MacOS.sdk_path_if_needed}/usr/include",
+                    "-D3RDPARTY_TCL_DIR:PATH=#{tcltk.opt_prefix}",
+                    "-D3RDPARTY_TK_DIR:PATH=#{tcltk.opt_prefix}",
+                    "-D3RDPARTY_TCL_INCLUDE_DIR:PATH=#{tcltk.opt_include}",
+                    "-D3RDPARTY_TK_INCLUDE_DIR:PATH=#{tcltk.opt_include}",
+                    "-D3RDPARTY_TCL_LIBRARY_DIR:PATH=#{tcltk.opt_lib}",
+                    "-D3RDPARTY_TK_LIBRARY_DIR:PATH=#{tcltk.opt_lib}",
+                    "-D3RDPARTY_TCL_LIBRARY:FILEPATH=#{tcltk.opt_lib}/libtcl#{tcltk.version.major_minor}.dylib",
+                    "-D3RDPARTY_TK_LIBRARY:FILEPATH=#{tcltk.opt_lib}/libtk#{tcltk.version.major_minor}.dylib",
                     *std_cmake_args
     system "make", "install"
 
