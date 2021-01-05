f39488f08fde1a68a48435b1bbaad7e8a27666b5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-05 06:04:50 +0000

    logswan: add 2.1.8 bottle.

--- Formula/logswan.rb
+++ Formula/logswan.rb
@@ -5,6 +5,13 @@ class Logswan < Formula
   sha256 "b3201d3f8a9863d51a0c3caec9e89c74dab8de7a604a5761454df64c30908ef8"
   license "BSD-2-Clause"
 
+  bottle do
+    cellar :any
+    sha256 "dea1726c54eb9acaf4745a9cf67e46992ce6d8d7651b1f47faa76df225d831f4" => :big_sur
+    sha256 "844574a208157568c6e3b320a6be97f48fe1138340bed1eb465d223ae4dd50de" => :catalina
+    sha256 "bb06c502e3d6ecb4dba59130e58465e1b91f5848eb5d42b04ba609877e34ba91" => :mojave
+  end
+
   depends_on "cmake" => :build
   depends_on "jansson"
   depends_on "libmaxminddb"
