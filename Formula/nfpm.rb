5182bb90a245d9f091b68fcd30421166ff7f7642
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-05 05:34:42 +0000

    nfpm: update 2.2.2 bottle.

--- Formula/nfpm.rb
+++ Formula/nfpm.rb
@@ -8,10 +8,10 @@ class Nfpm < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e06aefbef3da540181206325ddebed7712bf43ffe307ad6802438cf5eaf84f9c" => :big_sur
-    sha256 "cb89330466d60841270629c42f3d192d4b4b118afffb803ec4e2b2872c9a76c7" => :arm64_big_sur
-    sha256 "d6be6a3d4ddacaca2fee655f566c95cfde762a45473eac4ecccd98a8bf9eb7e4" => :catalina
-    sha256 "606ecb9b7c2b0f52b47a02c9c625709f0352bbba7e3c066c68ce8a75b05f71f6" => :mojave
+    sha256 "ba07678fb6f5704777eacca77ea28f90fb1059e6b2dffdd7f0defaea83e85727" => :big_sur
+    sha256 "98955879ccd8cf225f21d2545ffec231ef04f3e79ed82ef8baf86e57983f20d3" => :arm64_big_sur
+    sha256 "64445d6f4557555b29220fc6ec2681aa31779d0309a62d2ef38490bd070afd1a" => :catalina
+    sha256 "9ecf7f3bdde9c508bf68deeb9190e50e7112e5a22a3813f1b19d009db8a20514" => :mojave
   end
 
   depends_on "go" => :build
