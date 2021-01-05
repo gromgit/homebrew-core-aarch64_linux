e82e208c9b51449bb54cf1a18b29bc25ccb42943
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-05 00:54:56 +0000

    isl: update 0.23 bottle.

--- Formula/isl.rb
+++ Formula/isl.rb
@@ -19,11 +19,10 @@ class Isl < Formula
 
   bottle do
     cellar :any
-    sha256 "15376fb7aff7adec3786e6a31ec9b5cad585fd01ecbd5c4744ef9461b10965ff" => :big_sur
-    sha256 "ab5b1ccbb471b04c3e4c33608857057188f96d95f18575838f526860d8ac4f44" => :arm64_big_sur
-    sha256 "b5319e3bbbb36ef3536d841999b7497b3dce4bf9e07fb04f6b0db716e087896d" => :catalina
-    sha256 "29213891860c971e084d1e2a3d1ad00c92371140dea599aae2894e26ec0d6874" => :mojave
-    sha256 "a1193c8b06c31abc4bf9c1ef9bb93c4879ff5ba4050b4a06c22c2a0048d3c87f" => :high_sierra
+    sha256 "77907a43a415210de713d9e82588b452e5546a31b42194c7c75e07486d319a51" => :big_sur
+    sha256 "5b066bc471862c8d166082f0d1bf6b132aac0117f67e19bba139dfe907eb2614" => :arm64_big_sur
+    sha256 "bb4c986e9f49c7eea6349a536889e6223549885c0aab3d7692542cd48bc06481" => :catalina
+    sha256 "066330367dcc69e8d200a1d26a7f6ca580ecc3c397a686fa3b2fbd36d5d88ada" => :mojave
   end
 
   head do
