47bda89126f64e7e4c592739a49653aee8a8c60f
chenrui <rui@chenrui.dev>
2021-01-05 00:44:19 +0000

    clair: update 4.0.0 bottle.

--- Formula/clair.rb
+++ Formula/clair.rb
@@ -12,10 +12,9 @@ class Clair < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0a67ddd722919a47f88db76cfa2f10d052d5a72dbb78819dd5d6841cbba1f552" => :big_sur
-    sha256 "0d68ea1e0d988662effc3192756c8b5c35051641980d7853582b9af313c5cdda" => :catalina
-    sha256 "65fef5fe64e988408e491d404ec8514526c550ea785f37bad807626e3e82714a" => :mojave
-    sha256 "902c4992f09e044694e5cad96191e5df4f4e1e6dfb51b6a25122a25f0c9ad9a9" => :high_sierra
+    sha256 "6f00ace419ae1a0f45123ee9f4c02aa35b245ecc2cd3e06e039e5d63bb242ed6" => :big_sur
+    sha256 "4f1e227f77567ac98ce3acd89b4cee3def5ba8e4e77ae6959857477ce8f917d6" => :catalina
+    sha256 "9538f5af26cd96d6bda8d0b803e6ba639675fc1672c81f330499aad32a4cac5f" => :mojave
   end
 
   depends_on "go" => :build
