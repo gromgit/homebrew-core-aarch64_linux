1aff354a3dfcbcd107e0c81488e2e7263506d513
Rui Chen <rui@chenrui.dev>
2021-01-05 00:20:05 -0500

    phoronix-test-suite 10.2.0

--- Formula/phoronix-test-suite.rb
+++ Formula/phoronix-test-suite.rb
@@ -1,8 +1,8 @@
 class PhoronixTestSuite < Formula
   desc "Open-source automated testing/benchmarking software"
   homepage "https://www.phoronix-test-suite.com/"
-  url "https://github.com/phoronix-test-suite/phoronix-test-suite/archive/v10.0.1.tar.gz"
-  sha256 "3d65dc03c0e20e2419521157cb8189b90e93826dfbf81e7e4d088bc2f14b0650"
+  url "https://github.com/phoronix-test-suite/phoronix-test-suite/archive/v10.2.0.tar.gz"
+  sha256 "3563b5b5c46530bb19cf89393877f006bb627bb6c7c8640e1d131b2b37779615"
   license "GPL-3.0-or-later"
   head "https://github.com/phoronix-test-suite/phoronix-test-suite.git"
 
