944de7f0d7b6b1c0ed9a2d02bdd0ee360cf2f490
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-05 05:55:25 +0000

    pdm: update 1.0.0 bottle.

--- Formula/pdm.rb
+++ Formula/pdm.rb
@@ -14,10 +14,10 @@ class Pdm < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "fc99fb36f2f50b12cda7ec9686da46a78dbc5f569af002d85986119dc6583db4" => :big_sur
-    sha256 "ea12d97d61258c42d21a79847a04b9cdba6cd5e939fa75f1e1131bd73b2cd446" => :arm64_big_sur
-    sha256 "29ea7f1596e0ef6c968706bf39ea6297623bb4a1ea383ea9d9beffe5e992dfde" => :catalina
-    sha256 "5b042502410d504c3d5d5f390dc23cef481853c4e6930f125aa90df8cc000340" => :mojave
+    sha256 "4fa9c4c312e302f8d38fdfbb9c3c2dbcfe7ad4d3fa295c27f0eac3d6897118e8" => :big_sur
+    sha256 "b44cc479d868a2a9f82f2b5793618602484bded75f2d303c3fa45ba826771291" => :arm64_big_sur
+    sha256 "becf8b188d9d44865839bbbba4a3de14ff273ef22dbf8c5e2228c4ff45b69a30" => :catalina
+    sha256 "b641604f6eaccf9c6b8cfa196d076bca03e72e8d65e2111ec2b7bab3c0b80871" => :mojave
   end
 
   depends_on "python@3.9"
