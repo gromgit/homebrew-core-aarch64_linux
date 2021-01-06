aae37a2d3b31a50bea88dd024e6207e8c537c9b1
Rui Chen <rui@chenrui.dev>
2021-01-06 14:49:19 -0500

    terraform@0.12 0.12.30
    Closes #68402.
    
    Signed-off-by: chenrui <rui@chenrui.dev>

--- Formula/terraform@0.12.rb
+++ Formula/terraform@0.12.rb
@@ -1,8 +1,8 @@
 class TerraformAT012 < Formula
   desc "Tool to build, change, and version infrastructure"
   homepage "https://www.terraform.io/"
-  url "https://github.com/hashicorp/terraform/archive/v0.12.29.tar.gz"
-  sha256 "17df1258f99865681fda2086c804336246407e74c195ae7a45e3e34de98b82ca"
+  url "https://github.com/hashicorp/terraform/archive/v0.12.30.tar.gz"
+  sha256 "09db85a429ade5cc43cf49a3e19a921093a7e3c97073732019a9dd7634e0de9d"
   license "MPL-2.0"
 
   bottle do
