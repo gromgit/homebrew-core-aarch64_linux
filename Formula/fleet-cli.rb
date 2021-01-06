f81b3e52957f7818af585fbbc90eaa3834861930
Rui Chen <rui@chenrui.dev>
2021-01-06 01:49:01 -0500

    fleet-cli 0.3.2
    Closes #68375.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/fleet-cli.rb
+++ Formula/fleet-cli.rb
@@ -2,8 +2,8 @@ class FleetCli < Formula
   desc "Manage large fleets of Kubernetes clusters"
   homepage "https://github.com/rancher/fleet"
   url "https://github.com/rancher/fleet.git",
-      tag:      "v0.3.1",
-      revision: "e5ff6fdd2d8e08ccfe11b1efc6438d3cbc467152"
+      tag:      "v0.3.2",
+      revision: "fbae9ec285468937e759e16179f01a6324d661ad"
   license "Apache-2.0"
 
   livecheck do
