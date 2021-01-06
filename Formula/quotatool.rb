c3da6541906d2b7251755ad3e79f2989a0fc8b8c
FX Coudert <fxcoudert@gmail.com>
2021-01-06 18:43:44 +0000

    quotatool: update 1.6.2 bottle.

--- Formula/quotatool.rb
+++ Formula/quotatool.rb
@@ -8,6 +8,7 @@ class Quotatool < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "e5dbc4f83caec774f6f05d65515c51bd8963c28415020698666d534030e91b23" => :big_sur
+    sha256 "33cf581ce810cb4704669a05ee01b5cc963008f02393db65453ef06216ed257f" => :arm64_big_sur
     sha256 "b8787cedb9e50c5ea14b10fa2e6cf9ec948e7842a97d5e7212abef0ea87e26c6" => :catalina
     sha256 "2d2b6f53466ec7b211f44b0319966b7120e3bbf0e1d57c1f0ae3d272bc8f4ce4" => :mojave
     sha256 "bbf7543458972806f3c15b25bf7cd71276159b54ae1ada3beb12e6d29328ec0e" => :high_sierra
