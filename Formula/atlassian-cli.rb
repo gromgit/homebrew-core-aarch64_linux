8f16f7084114a937f717a5cfdf344f7993e38165
Rui Chen <rui@chenrui.dev>
2021-01-06 01:47:11 -0500

    atlassian-cli 9.5.0

--- Formula/atlassian-cli.rb
+++ Formula/atlassian-cli.rb
@@ -1,8 +1,8 @@
 class AtlassianCli < Formula
   desc "Command-line interface clients for Atlassian products"
   homepage "https://bobswift.atlassian.net/wiki/pages/viewpage.action?pageId=1966101"
-  url "https://bobswift.atlassian.net/wiki/download/attachments/16285777/atlassian-cli-9.4.0-distribution.zip"
-  sha256 "37d35c3c51486915ee2af596b28cd623a33d751d9ad76f97420bb1d25893790e"
+  url "https://bobswift.atlassian.net/wiki/download/attachments/16285777/atlassian-cli-9.5.0-distribution.zip"
+  sha256 "3fee080b7d1042d24721227a0a7182ad6e6c00833d9e66ac14c77b0f39920f05"
 
   livecheck do
     url "https://marketplace.atlassian.com/apps/10886/atlassian-command-line-interface-cli/version-history"
