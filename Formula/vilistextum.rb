3e43b1de8063392de012027573f9e11b2d830583
FX Coudert <fxcoudert@gmail.com>
2021-01-06 18:41:59 +0000

    vilistextum: update 2.6.9 bottle.

--- Formula/vilistextum.rb
+++ Formula/vilistextum.rb
@@ -8,6 +8,7 @@ class Vilistextum < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "c1107f3edeb308819c5b074f1ed2072583c3bc5a7800af162ab10ef460548f18" => :big_sur
+    sha256 "dfd4ab35a880dbac2c93e43eed5e0001093fad04c94c32f955c3f91822d84ccd" => :arm64_big_sur
     sha256 "cead55f6cb7e4d66d3f6ca2bf013f0cb653144a0fe79620fdd5735a1e57566a5" => :catalina
     sha256 "c36418e1556b9f5f9c0126811fddca3149137abfed6b36596ec4612c3806a3ec" => :mojave
     sha256 "6005ce3b4c593707dfe7ffbc10ea64f26ce6e441803a9133ab46ba0fbaee422f" => :high_sierra
