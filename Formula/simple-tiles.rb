e74a593c8df3cbe0803d02795e9ca86942573541
FX Coudert <fxcoudert@gmail.com>
2021-01-06 17:05:39 +0000

    simple-tiles: update 0.6.1_9 bottle.

--- Formula/simple-tiles.rb
+++ Formula/simple-tiles.rb
@@ -10,6 +10,7 @@ class SimpleTiles < Formula
   bottle do
     cellar :any
     sha256 "4cce6d8d42a396e231de41e20a5b0fae80d775619f552d3fe2b8f8a608bbf926" => :big_sur
+    sha256 "cb42c9630b6156cd4b1478f5b6116eb5aad521f90c6a003e0d6d7cbdfa6605e2" => :arm64_big_sur
     sha256 "67877ff99089d483f2b3bb515d3e680fa926cf7de384e8a5bbfa6925fef92e4c" => :catalina
     sha256 "ccc6df1dbcb3f0385b599fc30624d8fecb16bd9164391b104d17633b5d78f3e1" => :mojave
     sha256 "a8197a9dbc2b26296c88f7ebad0ece48e128a1194ffb7053dcf93d1718786ff4" => :high_sierra
