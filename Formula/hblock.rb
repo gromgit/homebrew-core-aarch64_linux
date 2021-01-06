9b6bf6c1a525414c3ae1441ef0238179243c6a9b
FX Coudert <fxcoudert@gmail.com>
2021-01-06 16:52:03 +0000

    hblock: update 3.1.3 bottle.

--- Formula/hblock.rb
+++ Formula/hblock.rb
@@ -8,6 +8,7 @@ class Hblock < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "e983c78b4e45ffcef65cea8dbd253d0c7893670dd2edebfd489c234598122b0c" => :big_sur
+    sha256 "f4a73b36a9d7e96dee2632760f5bf4d449f0445d9f782b88726ab12d1a544695" => :arm64_big_sur
     sha256 "400142a4275501a8ea5630260a41169e4b2bd9ab711cd4053bb716ac14510c52" => :catalina
     sha256 "631a6da27cd7728a9c87caed412ccfa1236a8023385bbd623b482609a18cc30f" => :mojave
   end
