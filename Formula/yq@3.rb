7216be10a6d0a93d5ffb70f0c6eeb613b271ecbf
FX Coudert <fxcoudert@gmail.com>
2021-01-06 18:42:01 +0000

    yq@3: update 3.4.1 bottle.

--- Formula/yq@3.rb
+++ Formula/yq@3.rb
@@ -8,6 +8,7 @@ class YqAT3 < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "f4b71750b38057dd5d5df339859fb76a945c916b666c221098b95c1dda2508c5" => :big_sur
+    sha256 "6ff1ee3e11e18b1a3f12754f37627b9641b24db8b99cf5725bdad22352812394" => :arm64_big_sur
     sha256 "2009fa7cc5c8aaa95856a401cea51c60ba2b21b49cc5d4227aab8f290a27e760" => :catalina
     sha256 "4180e832dac7686fc6e0db67ebde2aa4c28dc42934795fed810e657853b47ab2" => :mojave
   end
