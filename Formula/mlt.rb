4f6b0146c25a65c42e6263251a49dc9aa2136ab3
Seeker <meaningseeking@protonmail.com>
2021-01-06 16:29:47 +0000

    mlt: update 6.24.0_1 bottle.

--- Formula/mlt.rb
+++ Formula/mlt.rb
@@ -8,9 +8,9 @@ class Mlt < Formula
   head "https://github.com/mltframework/mlt.git"
 
   bottle do
-    sha256 "50d28e5c661af49faf660bb7fbae1c5e696b20fc91c087bfe8ffb5a4ce29b528" => :big_sur
-    sha256 "5919ce61cb9eab5166aa97695d59c9fad673cf97afad3dcd59f9a0d4ad1ced8c" => :catalina
-    sha256 "f28b8247fc5717bc56d79fc646d05310087b0289c22d096a2474c62334e03885" => :mojave
+    sha256 "e11526070c750b8d4d27a3e7e7e61c74fb17c4bc95624f001876f2d2336bfd37" => :big_sur
+    sha256 "b25a49d0d108710e0defe0f8fa8662c5799140bbe0f56810ad9b9ff1652e1f1b" => :catalina
+    sha256 "0e14497ab767e5cb89bcc1b8a2f03fe80010dcc275b02c584f4cd6020def3163" => :mojave
   end
 
   depends_on "pkg-config" => :build
