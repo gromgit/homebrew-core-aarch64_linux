63b93b74239c2cbc775936fb8f93ef6d52a35ac0
FX Coudert <fxcoudert@gmail.com>
2021-01-07 15:39:40 +0000

    allegro: update 5.2.6.0 bottle.

--- Formula/allegro.rb
+++ Formula/allegro.rb
@@ -14,6 +14,7 @@ class Allegro < Formula
   bottle do
     cellar :any
     sha256 "fccd4a2c8eda6101d7a7f0dffe598cdafc4594b8fc247548d48d6eafe85e0cc3" => :big_sur
+    sha256 "709d62d7f2191f409671467cf25ec25edcb97641729277a62cc6cca065db4b0e" => :arm64_big_sur
     sha256 "9e71511f6c8faa8449dd06bc30bd74497ee832e3e0ca7f3eb02bcef263ab4b3f" => :catalina
     sha256 "ead9f69a2af4720ad8a9e020657b1db71e49cb3e83d9d8477d425de9d948ce07" => :mojave
     sha256 "4ab4367b267e257a1aeee6cd65301922cf38cb37e8c11865edecedac5960f96e" => :high_sierra
