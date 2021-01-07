c891fdd8c3d79a82caedcfb98a34ac8c5b681c6b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-07 15:29:33 +0000

    theharvester: update cffi resource
    See #68463.
    
    Closes #68475.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/theharvester.rb
+++ Formula/theharvester.rb
@@ -6,6 +6,7 @@ class Theharvester < Formula
   url "https://github.com/laramies/theHarvester/archive/3.2.2.tar.gz"
   sha256 "597b5fdfccf76a30258d56e9c62dffc56fdd78682f75f2ed5b62b5a703d633b4"
   license "GPL-2.0-only"
+  revision 1
   head "https://github.com/laramies/theHarvester.git"
 
   bottle do
@@ -54,8 +55,8 @@ class Theharvester < Formula
   end
 
   resource "cffi" do
-    url "https://files.pythonhosted.org/packages/cb/ae/380e33d621ae301770358eb11a896a34c34f30db188847a561e8e39ee866/cffi-1.14.3.tar.gz"
-    sha256 "f92f789e4f9241cd262ad7a555ca2c648a98178a953af117ef7fad46aa1d5591"
+    url "https://files.pythonhosted.org/packages/66/6a/98e023b3d11537a5521902ac6b50db470c826c682be6a8c661549cb7717a/cffi-1.14.4.tar.gz"
+    sha256 "1a465cbe98a7fd391d47dce4b8f7e5b921e6cd805ef421d04f5f66ba8f06086c"
   end
 
   resource "chardet" do
