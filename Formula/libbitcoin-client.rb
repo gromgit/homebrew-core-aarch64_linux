14516f6088f84c80960e5a643bac6826d1b1c9b0
FX Coudert <fxcoudert@gmail.com>
2021-01-07 18:38:18 +0000

    libbitcoin-client: update 3.6.0_6 bottle.

--- Formula/libbitcoin-client.rb
+++ Formula/libbitcoin-client.rb
@@ -8,9 +8,11 @@ class LibbitcoinClient < Formula
 
   bottle do
     cellar :any
-    sha256 "2e6b9c76aa995bb275d841a8b895032e75153edada35ee349602083b5cec3c4e" => :big_sur
-    sha256 "90a8577e21703dc41fa58ea17f57692aba40afad1d8290add9b7fa8363e4c79f" => :catalina
-    sha256 "24e5312ba815627a47853ef4024b9a41c4ad145657fee7c9ad0023b57600f5cf" => :mojave
+    rebuild 1
+    sha256 "8bfcea7f689c6fc88052ce69aba9d4f6697ebd87b437254c14a1fd42ffbc5cfa" => :big_sur
+    sha256 "9ec8c3681172a8043fbb24a321b2b07fbc100073a6bf6fd65d089389ef954b45" => :arm64_big_sur
+    sha256 "536dcc26e62ab4b2f302e197e44976ef594da699aff73243581f6dbb723bf007" => :catalina
+    sha256 "53775e2543b62055f7fa37824c51582e06999db7ae6e1934828e3f14165716b7" => :mojave
   end
 
   depends_on "autoconf" => :build
