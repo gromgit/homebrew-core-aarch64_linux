f3d7402cf9fa9ac6a53f740d507f96792ac97071
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-07 16:52:12 +0000

    svtplay-dl: update cffi resource
    See #68463.
    
    Closes #68483.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/svtplay-dl.rb
+++ Formula/svtplay-dl.rb
@@ -6,6 +6,7 @@ class SvtplayDl < Formula
   url "https://files.pythonhosted.org/packages/5d/99/da03abbaf27691fb684a22db5c6b65e255e4aa11f48b1ff7b3329f4fe329/svtplay-dl-2.8.tar.gz"
   sha256 "8c232a5272335f6133144a4a0faa62792a51bc87f5e8a7a9902efd6fa37d8049"
   license "MIT"
+  revision 1
 
   livecheck do
     url :stable
@@ -27,8 +28,8 @@ class SvtplayDl < Formula
   end
 
   resource "cffi" do
-    url "https://files.pythonhosted.org/packages/cb/ae/380e33d621ae301770358eb11a896a34c34f30db188847a561e8e39ee866/cffi-1.14.3.tar.gz"
-    sha256 "f92f789e4f9241cd262ad7a555ca2c648a98178a953af117ef7fad46aa1d5591"
+    url "https://files.pythonhosted.org/packages/66/6a/98e023b3d11537a5521902ac6b50db470c826c682be6a8c661549cb7717a/cffi-1.14.4.tar.gz"
+    sha256 "1a465cbe98a7fd391d47dce4b8f7e5b921e6cd805ef421d04f5f66ba8f06086c"
   end
 
   resource "asn1crypto" do
