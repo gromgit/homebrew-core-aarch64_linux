8a2b90c20de7f242f7808c0acea24c073914d2a4
Mike McQuaid <mike@mikemcquaid.com>
2021-01-07 15:04:15 +0000

    ruby@2.6: cleanup MJIT_CC usage.
    Use a single value that works on macOS and Linux.

--- Formula/ruby@2.6.rb
+++ Formula/ruby@2.6.rb
@@ -50,12 +50,7 @@ class RubyAT26 < Formula
     end
 
     # Correct MJIT_CC to not use superenv shim
-    on_macos do
-      args << "MJIT_CC=/usr/bin/clang"
-    end
-    on_linux do
-      args << "MJIT_CC=/usr/bin/gcc"
-    end
+    args << "MJIT_CC=/usr/bin/#{DevelopmentTools.default_compiler}"
 
     system "./configure", *args
 
