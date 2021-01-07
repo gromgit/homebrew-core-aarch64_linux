8f2c4b3e19bc3312c645ea52e2529b9b2ce775b6
Francois-Xavier Coudert <fxcoudert@gmail.com>
2021-01-07 11:00:58 +0100

    gifski: update 1.2.4_1 bottle.

--- Formula/gifski.rb
+++ Formula/gifski.rb
@@ -8,11 +8,10 @@ class Gifski < Formula
 
   bottle do
     cellar :any
-    sha256 "20e76add05177bebdbee36334ad956f521c1702f440e50c85b63d306f951575f" => :big_sur
-    sha256 "528c768120c91ded3d7ffdcdd4b3bc4f4daf33e678e98ff123324312fff8e0db" => :arm64_big_sur
-    sha256 "df5f0713070679c35ef4d96c04cfe41c814cd6b10193746376ca98af636b46fb" => :catalina
-    sha256 "8b495ff4083d519ff85a62bf0a1f18bdc43f537575abc2709d6d873367bf4b1d" => :mojave
-    sha256 "cf2bb34c28d2eb6e610e130798638dc68712cd6337520c409e950abc516f4f59" => :high_sierra
+    sha256 "e28252b1eca7477fb8bf612345cb89436a76d2ba5d559fc5a9cc11ca66a4c1fc" => :big_sur
+    sha256 "8ff94a08d8988c62f9ef1991324d0205b29fd128890746555a546f31c1272b9b" => :arm64_big_sur
+    sha256 "38deabd72c6a81faf79f9441623eb06a80fb45d468ff5e64976915416cf60231" => :catalina
+    sha256 "a7f1fe0ecc811745025d5da4ead971f292f40e9f097c4195cc9c24b24778fc84" => :mojave
   end
 
   depends_on "pkg-config" => :build
