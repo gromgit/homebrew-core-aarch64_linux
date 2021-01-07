430a9036ac06cbc73e77b22cba2b0a7b4608dae8
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-07 20:27:52 +0000

    lxc: update 4.9 bottle.

--- Formula/lxc.rb
+++ Formula/lxc.rb
@@ -12,9 +12,11 @@ class Lxc < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d68bfcf7ddb0fdd312c48ce45ef7fadb8737f9833915fc1efa481f880a054319" => :big_sur
-    sha256 "01db96a44e283ac88acc477f7d6e273da038407036bbea7d0fe7eb73a8ffc0c3" => :catalina
-    sha256 "6f836ee1ad36d07b200c788fc389642fcb5129697e4a461e7c0fd40429fba50f" => :mojave
+    rebuild 1
+    sha256 "e29577824bb7dde4080ff33b3e7df929c5a84208b92f30016c742fa56b119f6c" => :big_sur
+    sha256 "c5510864bc481d58d4149608083c93eaddd2b7c89795cece3205e1f69acbc03e" => :arm64_big_sur
+    sha256 "804e3817c0ef27e95a3ace0541cde6ae6242d7e015fc3da349abd079b1426c1e" => :catalina
+    sha256 "6c0fff4393b1ceed7761c90de3d6c4bae7fd918b47c4aa80254e6f2927dfba5f" => :mojave
   end
 
   depends_on "go" => :build
