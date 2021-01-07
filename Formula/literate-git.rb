93367693e7f30ac7c0cfb4b8d32b9aedb31b1741
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-07 20:35:59 +0000

    literate-git: update cffi resource
    See #68463.
    
    Closes #68508.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/literate-git.rb
+++ Formula/literate-git.rb
@@ -6,7 +6,7 @@ class LiterateGit < Formula
   url "https://github.com/bennorth/literate-git/archive/v0.3.1.tar.gz"
   sha256 "f1dec77584236a5ab2bcee9169e16b5d976e83cd53d279512136bdc90b04940a"
   license "GPL-3.0-or-later"
-  revision 5
+  revision 6
 
   bottle do
     cellar :any
@@ -30,8 +30,8 @@ class LiterateGit < Formula
   end
 
   resource "cffi" do
-    url "https://files.pythonhosted.org/packages/05/54/3324b0c46340c31b909fcec598696aaec7ddc8c18a63f2db352562d3354c/cffi-1.14.0.tar.gz"
-    sha256 "2d384f4a127a15ba701207f7639d94106693b6cd64173d6c8988e2c25f3ac2b6"
+    url "https://files.pythonhosted.org/packages/66/6a/98e023b3d11537a5521902ac6b50db470c826c682be6a8c661549cb7717a/cffi-1.14.4.tar.gz"
+    sha256 "1a465cbe98a7fd391d47dce4b8f7e5b921e6cd805ef421d04f5f66ba8f06086c"
   end
 
   resource "docopt" do
