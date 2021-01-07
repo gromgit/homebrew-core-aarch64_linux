4433c6d551a36dd1472d135e54476c592bef89ba
James Morris <jamesmorris2@gmail.com>
2021-01-07 15:45:53 -0500

    gnu-tar 1.33
    Closes #68510.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gnu-tar.rb
+++ Formula/gnu-tar.rb
@@ -1,10 +1,10 @@
 class GnuTar < Formula
   desc "GNU version of the tar archiving utility"
   homepage "https://www.gnu.org/software/tar/"
-  url "https://ftp.gnu.org/gnu/tar/tar-1.32.tar.gz"
-  mirror "https://ftpmirror.gnu.org/tar/tar-1.32.tar.gz"
-  sha256 "b59549594d91d84ee00c99cf2541a3330fed3a42c440503326dab767f2fbb96c"
-  license "GPL-3.0"
+  url "https://ftp.gnu.org/gnu/tar/tar-1.33.tar.gz"
+  mirror "https://ftpmirror.gnu.org/tar/tar-1.33.tar.gz"
+  sha256 "7c77c427e8cce274d46a6325d45a55b08e13e2d2d0c9e6c0860a6d2b9589ff0e"
+  license "GPL-3.0-or-later"
 
   livecheck do
     url :stable
