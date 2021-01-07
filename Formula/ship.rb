ef6d830e908c89f521fc4cceb913760b181a0454
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-07 15:22:46 +0000

    ship: update 0.55.0 bottle.

--- Formula/ship.rb
+++ Formula/ship.rb
@@ -7,11 +7,10 @@ class Ship < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c6b0158bfbccbade0b475b4ebc29f34eaa0cd1987f5a51ff57f621f3fbae7d1f" => :big_sur
-    sha256 "b27f8e7eec97b2b507d794752daf9958f0c358643cb7454175a310153c744421" => :arm64_big_sur
-    sha256 "4dd63ad1bc12b9e1d359d763ea4e4e51ef93a0b3a0c711fd897e7c53ec6642b3" => :catalina
-    sha256 "883a7b5236bfafaf23b748218b09ae75ac9d1420c0c5e0cd2ba9ffb6a3c2cef0" => :mojave
-    sha256 "f6069bab80cc5dde1e19b353ed41faf449f299a5cf97f61aac8ea3a76cc4b22f" => :high_sierra
+    sha256 "87de3fe44a1b8d881b0cf6be5f74c6261dc8b38deb85e7edb48a0b1ac9d73805" => :big_sur
+    sha256 "17b2f10424c9728a9bbc19bef3af5d9fab3b350fa2aa2620fced62056065f651" => :arm64_big_sur
+    sha256 "7395a181b4bb2581d17a9a45fd054f5f1c15fe1a43a85f559316aea65e2da66e" => :catalina
+    sha256 "740a9cd2f1eef9bf09cb029827f5c4fabbc84dd14ee68babf065f85cceee0f12" => :mojave
   end
 
   depends_on "go" => :build
