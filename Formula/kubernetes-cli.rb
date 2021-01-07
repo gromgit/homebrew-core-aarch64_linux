3cde7dd02981336c981610e27c2f274b41c6dd86
Francois-Xavier Coudert <fxcoudert@gmail.com>
2021-01-07 13:24:56 +0100

    kubernetes-cli: revision for ARM bottle
    * kubernetes-cli: build for Apple Silicon
    * kubernetes-cli: apply upstream ARM patch
    
    Closes #68455.
    
    Co-authored-by: Sean Molenaar <smillerdev@me.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/kubernetes-cli.rb
+++ Formula/kubernetes-cli.rb
@@ -5,6 +5,7 @@ class KubernetesCli < Formula
       tag:      "v1.20.4",
       revision: "e87da0bd6e03ec3fea7933c4b5263d151aafd07c"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/kubernetes/kubernetes.git"
 
   livecheck do
@@ -21,6 +22,12 @@ class KubernetesCli < Formula
   depends_on "go" => :build
 
   uses_from_macos "rsync" => :build
+
+  # ARM support, remove for next release and reinstate test
+  patch do
+    url "https://github.com/kubernetes/kubernetes/commit/f719624654c68d143cad4e4af2f21cca0bfde8fd.patch?full_index=1"
+    sha256 "3d40308a2f639c1a45a2105889776f0d02dd97000c188bc4cc78de739d5f0776"
+  end
 
   def install
     # Don't dirty the git tree
@@ -49,7 +56,10 @@ class KubernetesCli < Formula
     assert_match "kubectl controls the Kubernetes cluster manager.", run_output
 
     version_output = shell_output("#{bin}/kubectl version --client 2>&1")
-    assert_match "GitTreeState:\"clean\"", version_output
+
+    # Due to ARM patch reinstate for next release
+    # assert_match "GitTreeState:\"clean\"", version_output
+
     if build.stable?
       assert_match stable.instance_variable_get(:@resource)
                          .instance_variable_get(:@specs)[:revision],
