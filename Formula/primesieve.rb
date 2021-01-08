1736d2f9a3bae5853200ca97b8a5d8751578f13a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-08 09:35:34 +0000

    primesieve: update 7.6 bottle.

--- Formula/primesieve.rb
+++ Formula/primesieve.rb
@@ -7,10 +7,9 @@ class Primesieve < Formula
 
   bottle do
     cellar :any
-    sha256 "fb8828f5f605378c1e10ebb8b5b997159c4a49f1a29256b0807a307df9d4ac24" => :big_sur
-    sha256 "93a49e499f6cca69e9bc62e64e3bbf51fd9c8ab3c51e6ba15b6a1ab3feb4d2e8" => :catalina
-    sha256 "7070ec9a601335d1844820e722c346b9d7003ce80af4450f631521a023c291bc" => :mojave
-    sha256 "46185fc5980d6e411f4073ea330735498d918bb25dc65cb90909bb41749e8756" => :high_sierra
+    sha256 "50ab785aea91644c88fae8d3a75118dbebd2206a99942d1dd5d813df5afd1d56" => :big_sur
+    sha256 "46b89a5aebe93c324af325e1a4f15dd84a334564569c5a6816f998396289192c" => :catalina
+    sha256 "379ba3585b2ac86f43f58654ee9c8d9bc8e552bfda6d3b476399744880f85f2a" => :mojave
   end
 
   depends_on "cmake" => :build
