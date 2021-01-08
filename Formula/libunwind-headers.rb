34ac630a27d1457a0891125a402352105da448da
Seeker <meaningseeking@protonmail.com>
2021-01-07 21:37:55 -0800

    libunwind-headers 200.10 (#68540)

--- Formula/libunwind-headers.rb
+++ Formula/libunwind-headers.rb
@@ -1,8 +1,8 @@
 class LibunwindHeaders < Formula
   desc "C API for determining the call-chain of a program"
   homepage "https://opensource.apple.com/"
-  url "https://opensource.apple.com/tarballs/libunwind/libunwind-35.4.tar.gz"
-  sha256 "5ca0fcb257a33eb376b19cd26ddc5f34d00f9099c8ffb462d7484bfdca654d7d"
+  url "https://opensource.apple.com/tarballs/libunwind/libunwind-200.10.tar.gz"
+  sha256 "82ead8e12f7d5e70024ae8d116aa68755fb7932090e16f6077f44c7731abbede"
   license "APSL-2.0"
 
   bottle :unneeded
@@ -10,8 +10,9 @@ class LibunwindHeaders < Formula
   keg_only :shadowed_by_macos, "macOS provides libunwind.dylib (but nothing else)"
 
   def install
-    include.install Dir["include/*"]
-    (include/"libunwind").install Dir["src/*.h*"]
-    (include/"libunwind/libunwind_priv.h").unlink
+    cd "libunwind" do
+      include.install Dir["include/*"]
+      (include/"libunwind").install Dir["src/*.h*"]
+    end
   end
 end
