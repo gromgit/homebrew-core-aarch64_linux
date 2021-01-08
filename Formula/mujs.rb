c60f30ecf9f22b863e134b20cb4f387381db3096
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-08 01:04:25 +0000

    mujs: update 1.0.9_1 bottle.

--- Formula/mujs.rb
+++ Formula/mujs.rb
@@ -10,12 +10,11 @@ class Mujs < Formula
   head "https://github.com/ccxvii/mujs.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "0fdceb128ad3fed5618bb9d29d9afc81da346df39982c44050e0a831a1bd9267" => :big_sur
-    sha256 "3f80db9f82ac77a48f0dfed8e624f1ca39f84fa20a034d3937665ea072b4f92e" => :arm64_big_sur
-    sha256 "eb7344e1f8e8e407fddc9484dd7392be6c6e7a4b88ca05f912a2028ffd780991" => :catalina
-    sha256 "1c945e15f08632584cde81b06d233bf62bcd3fee4bde016cbdb37bb067e85a22" => :mojave
-    sha256 "9d94f85a85c383c131a32b31f87548d7a8bd89595227a63470925ce5272b41d8" => :high_sierra
+    cellar :any
+    sha256 "352e5c6c8ad24b3f36ad44e1b929e6f34a00360e5144a4fd490b228e2c9837bb" => :big_sur
+    sha256 "b346f30e520540c9bf22be4438ea215ef7954eaf85c66ba71f4fc7be0f5c7f1d" => :arm64_big_sur
+    sha256 "f0392da52d7b94279981010c643dc106946cb6a669e1cdb4de03f4732d260341" => :catalina
+    sha256 "138027da2db61c148fb93d5176717c10265467551ec5762163d9b7017614c774" => :mojave
   end
 
   def install
