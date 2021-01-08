43c1601b39aa8b88e50dcbd5bd92360a3e4b308a
FX Coudert <fxcoudert@gmail.com>
2021-01-08 14:42:06 +0000

    singular: update 4.2.0 bottle.

--- Formula/singular.rb
+++ Formula/singular.rb
@@ -7,6 +7,7 @@ class Singular < Formula
 
   bottle do
     sha256 "26709b976c059b3cd9ebd8b3a5c397d6379503786dbd282a7b25a69af612cb1a" => :big_sur
+    sha256 "51ede0e2a1ab0fbdef4bfd7fd9136865bf10c9378a01a7f2517c469d90bc8ef8" => :arm64_big_sur
     sha256 "bcbff484908f20d9677e051686bf44822430a12b18e4f8ec44782977f5ca1d2b" => :catalina
     sha256 "f3e2e200d751f2b7d39d011388a4b2c7cb59ac67bf7ea3583b200a677ee9938c" => :mojave
   end
