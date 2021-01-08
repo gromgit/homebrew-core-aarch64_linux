a70b16aa35e35110ecda1d1939970e625851e44e
FX Coudert <fxcoudert@gmail.com>
2021-01-08 12:36:53 +0000

    docker-machine-parallels: update 2.0.1 bottle.

--- Formula/docker-machine-parallels.rb
+++ Formula/docker-machine-parallels.rb
@@ -9,11 +9,11 @@ class DockerMachineParallels < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "43448ab3d84fa30ea33242cbbe0e9bc87f8d4c873272d5c2ea9f5704b659a80b" => :big_sur
-    sha256 "e9232de0aa3b564d52c7caffbe57240213f940e6cf44df8dcdb48483c35670ba" => :arm64_big_sur
-    sha256 "bccbce2df5d50177efc369a84f52e8ceb35e9e1d1c140aaa671d220a018e6feb" => :catalina
-    sha256 "32455b74320269c8d50cc3db1916eedca0013dd500e766ba5d56c57897a955a5" => :mojave
-    sha256 "a2b35f8457b064eb6a86bae1af28708730fbf3ad4a23007aa197375fb404b8ba" => :high_sierra
+    rebuild 1
+    sha256 "4613d3ea83c5afdcd717d5027d254b5d01d4b715ed859f98b183e070bd51c9f6" => :big_sur
+    sha256 "1bb048b170f5ca273b7e2e3be17459a9049e29982e2a3c6c866d088772b6744f" => :arm64_big_sur
+    sha256 "b419812f98208b1fccbbc24f198af6a1235110203b0377d642f80886c5c5fd36" => :catalina
+    sha256 "cce66a6fcdea79b33095c2ae7c49c93a9f730353d92738534fdbe03b3488ee43" => :mojave
   end
 
   depends_on "go" => :build
