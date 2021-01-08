d46e97f9b397173e0c9c7341b3eddd96bb496991
FX Coudert <fxcoudert@gmail.com>
2021-01-08 14:36:19 +0000

    ruby@2.5: update 2.5.8 bottle.

--- Formula/ruby@2.5.rb
+++ Formula/ruby@2.5.rb
@@ -7,6 +7,7 @@ class RubyAT25 < Formula
 
   bottle do
     sha256 "e1c9a7e1f481bdb5390b911e17616f96e2dd4824ef15d134cb96bf579991fb29" => :big_sur
+    sha256 "bbae2085587722707f40c1edff065697a150902c171c1c1279ff2d2d54b291d6" => :arm64_big_sur
     sha256 "77c3f363f6d159824f6a6b1b80ac7e1d2bcb79528f79a0bf78ab3feaa837ad94" => :catalina
     sha256 "bbd75b74d1694bfb1378de37a21d692ca4ecdf3e4c5f6ad7f381ab1ac4942119" => :mojave
     sha256 "cb21f7f22afc2f304a9dc9ab7aba730563656a054b0407943ba0cb1f68b010a2" => :high_sierra
