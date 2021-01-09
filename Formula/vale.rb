c2bdb9c2c4a9950b29986bf8e8dbc2fae90416bb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-09 09:35:44 +0000

    vale: update 2.7.1 bottle.

--- Formula/vale.rb
+++ Formula/vale.rb
@@ -7,10 +7,10 @@ class Vale < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "37e430e4afb894f3c4d40fab91f5a81334b03805b0168f23ca9df92e6ee0323e" => :big_sur
-    sha256 "9297ad3ab8521bd5fadbf2c76c7e02904d5e053312d862fa02d87da39d118fd0" => :arm64_big_sur
-    sha256 "4f063e24cc9f0035f426f7a8038a6249edd792ab36ac89fec40bf004d6b03f1a" => :catalina
-    sha256 "44e713c5fe32c5ad8b02da82ff4c34453003b5c4d022555928ab62a18acebf59" => :mojave
+    sha256 "8de72e99a7bd2fd8d156c8e26fbac388af484a26100a71518983d3041458abd8" => :big_sur
+    sha256 "c32347bf6026b0d8e95b4e0ce972eacea5624cabf23e15a9c1c5ca8a75ac9d83" => :arm64_big_sur
+    sha256 "7a69622772a8df80ff575afea2b9c7752cf01c5cc9a0e5f880f1b6eb6b138335" => :catalina
+    sha256 "131161266422f69c7aea9acd32053243862d5db06906f70acd260f162d69431a" => :mojave
   end
 
   depends_on "go" => :build
