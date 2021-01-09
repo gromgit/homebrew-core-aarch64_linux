e1bc97065329bbd92b076aa43ba02a5877825290
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-09 21:19:54 +0000

    v8 8.7.220.31
    * v8 8.7.220.31
    * build with llvm on ARM
    
    Closes #68668.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/v8.rb
+++ Formula/v8.rb
@@ -2,8 +2,8 @@ class V8 < Formula
   desc "Google's JavaScript engine"
   homepage "https://github.com/v8/v8/wiki"
   # Track V8 version from Chrome stable: https://omahaproxy.appspot.com
-  url "https://github.com/v8/v8/archive/8.7.220.29.tar.gz"
-  sha256 "36ebf7a55ccc0f2c765a45f23ed152caceb7612f31ce29d3f49ff1614afbe54d"
+  url "https://github.com/v8/v8/archive/8.7.220.31.tar.gz"
+  sha256 "f8b74ea7cda54bfd37c46533fa543909954a44848f97d9f08d1c372a303abfab"
   license "BSD-3-Clause"
 
   livecheck do
@@ -19,7 +19,7 @@ class V8 < Formula
     sha256 "b8222edfa40c8838b910eefe67b17c5b278447e94dc4d365ae9cb55d0e35d7e9" => :mojave
   end
 
-  depends_on "llvm" => :build if DevelopmentTools.clang_build_version < 1200
+  depends_on "llvm" => :build if DevelopmentTools.clang_build_version < 1200 || Hardware::CPU.arm?
   depends_on "ninja" => :build
 
   depends_on xcode: ["10.0", :build] # required by v8
@@ -104,7 +104,7 @@ class V8 < Formula
     }
 
     # use clang from homebrew llvm formula for XCode 11- , because the system clang is too old for V8
-    if DevelopmentTools.clang_build_version < 1200
+    if DevelopmentTools.clang_build_version < 1200 || Hardware::CPU.arm?
       ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib # but link against system libc++
       gn_args[:clang_base_path] = "\"#{Formula["llvm"].prefix}\""
     end
