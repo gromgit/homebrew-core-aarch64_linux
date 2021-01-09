405d7c4a03042df7d28003771ef25c8670e5bbcd
Rui Chen <rui@chenrui.dev>
2021-01-09 16:00:14 -0500

    profanity 0.10.0
    * profanity 0.10.0
    * profanity: update license
    
    Closes #68663.
    
    Co-authored-by: chenrui <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/profanity.rb
+++ Formula/profanity.rb
@@ -1,9 +1,9 @@
 class Profanity < Formula
   desc "Console based XMPP client"
   homepage "https://profanity-im.github.io"
-  url "https://profanity-im.github.io/profanity-0.9.5.tar.gz"
-  sha256 "23f7b2e5c6cf85913b9e7a228802bca9ecb1d1cf3bf8f8f285e9676176a24902"
-  license "GPL-3.0"
+  url "https://profanity-im.github.io/profanity-0.10.0.tar.gz"
+  sha256 "4a05e32590f9ec38430e33735bd02cfa199b257922b4116613f23912ca39ff8c"
+  license "GPL-3.0-or-later"
 
   bottle do
     sha256 "e674dc595aef44e934d0ae9ca90b46a42b2b10295b8dcd732f55475cb20c1acb" => :big_sur
