e6be16ffc1247f4ce0b56fe47ebbb02c9f6c9a2f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-09 09:35:38 +0000

    rhash: update 1.4.1 bottle.

--- Formula/rhash.rb
+++ Formula/rhash.rb
@@ -11,11 +11,10 @@ class Rhash < Formula
   end
 
   bottle do
-    sha256 "045a014357e8d4101921c6a2c40bd82cbdcfebc64ddf5c6d7ed5c3f846f08e5d" => :big_sur
-    sha256 "29cb64c69816a5706afd7245be32bc33ba47045bf24dcf54c3601b973c39cd21" => :arm64_big_sur
-    sha256 "3fc816254535e1ecf091161b96447efedf2748cdf25a38449f6de70ef652165d" => :catalina
-    sha256 "89ae46bbd559e15e9aacb9010e4f4cff6ab402e8bd9eb301f8cf7aa745dbdde3" => :mojave
-    sha256 "998d4c8b2195944bc979c11fcd7aff29997994c8457d29343524edea15de74eb" => :high_sierra
+    sha256 "6f7648fc30e68060747fb9be6480be57c7b30680e429b619f34ead13b9cc80d6" => :big_sur
+    sha256 "8eb637a12522739222253513a13aa3fafdc9ab586987f5648290349543017aca" => :arm64_big_sur
+    sha256 "108986af36d715a05223344f3f338c04b0ce5aa6d6cf0c26776be015adaef36a" => :catalina
+    sha256 "87ac3199498088f7d465dafefc6f014e10b4692ed3997895bbf1eb288dce8cdd" => :mojave
   end
 
   def install
