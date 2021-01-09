d78ea0c8da76be3e3b97e23b7ffb6956904e117d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-09 19:42:45 +0000

    lp_solve: add 5.5.2.11 bottle.

--- Formula/lp_solve.rb
+++ Formula/lp_solve.rb
@@ -5,6 +5,14 @@ class LpSolve < Formula
   sha256 "6d4abff5cc6aaa933ae8e6c17a226df0fc0b671c438f69715d41d09fe81f902f"
   license "LGPL-2.1-or-later"
 
+  bottle do
+    cellar :any
+    sha256 "53e611f8df08011c71022b51b9cab75469e9799f4de457b769f407741ef5fe8c" => :big_sur
+    sha256 "9a798c3fc6356bdbae35a8b3134ae5a51a7610c325cdf9f8ee4a3b41b7c1fcb0" => :arm64_big_sur
+    sha256 "0275d7331a6b593f84cc59a0f6a79e83be9362404b0b837aac78ce1cbe604ad7" => :catalina
+    sha256 "e52d1c29dede6d743305aafd18ac7c6d6ba350d8e6fe32dae3af086bdec59ea5" => :mojave
+  end
+
   def install
     cd "lpsolve55" do
       system "sh", "ccc.osx"
