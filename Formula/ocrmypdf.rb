53699597ce08ff9eee85a2165882e9d2c8ff67aa
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-09 14:12:32 +0000

    ocrmypdf: update 11.4.5_1 bottle.

--- Formula/ocrmypdf.rb
+++ Formula/ocrmypdf.rb
@@ -14,9 +14,9 @@ class Ocrmypdf < Formula
 
   bottle do
     cellar :any
-    sha256 "5155e8fd8f24aa3b8ee378c4e618bbf2eb9419a15f3adc8e1b465e8b91b39963" => :big_sur
-    sha256 "dba298a9dae23ed47fa2a24fcd81007a4d4b12a4ab5323588211d7c34738212b" => :catalina
-    sha256 "a228bf706b21eae1785b9f58b123196ffc1fdb7062e3006088d89be0095a012f" => :mojave
+    sha256 "c28e97f3add92f837c87d254212a20b543750bb8e3da9989e29a7f58af7853d2" => :big_sur
+    sha256 "ddac40175c136b76b14ddb472c8852b2c6ad03df2a734394b85a4fe221106c47" => :catalina
+    sha256 "7beabcd343d9f872912dd281462592183cbcd48d93a4674e128273266497498e" => :mojave
   end
 
   depends_on "pkg-config" => :build
