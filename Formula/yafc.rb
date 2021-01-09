326a4efd04c3f5264ab50da4f1aa2a33f8c12cfd
Dustin Rodrigues <dust.rod@gmail.com>
2021-01-09 10:39:09 -0500

    yafc: change call to Checksum

--- Formula/yafc.rb
+++ Formula/yafc.rb
@@ -30,7 +30,7 @@ class Yafc < Formula
 
   test do
     download_file = testpath/"gcc-10.2.0.tar.xz.sig"
-    expected_checksum = Checksum.new("sha256", "8e271266e0e3312bb1c384c48b01374e9c97305df781599760944e0a093fad38")
+    expected_checksum = Checksum.new("8e271266e0e3312bb1c384c48b01374e9c97305df781599760944e0a093fad38")
     output = pipe_output("#{bin}/yafc -W #{testpath} -a ftp://ftp.gnu.org/gnu/gcc/gcc-10.2.0/",
                          "get #{download_file.basename}", 0)
     assert_match version.to_s, output
