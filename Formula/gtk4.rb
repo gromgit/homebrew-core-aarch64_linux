a549e2846423d35985d5c85d77cf4efb427bca32
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2021-01-09 10:08:17 +0000

    gtk4 4.0.1
    Closes #68615.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/gtk4.rb
+++ Formula/gtk4.rb
@@ -1,8 +1,8 @@
 class Gtk4 < Formula
   desc "Toolkit for creating graphical user interfaces"
   homepage "https://gtk.org/"
-  url "https://download.gnome.org/sources/gtk/4.0/gtk-4.0.0.tar.xz"
-  sha256 "d46cf5b127ea27dd9e5d2ff6ed500cb4067eeb2cb1cd2c313ccde8013b0b9bf9"
+  url "https://download.gnome.org/sources/gtk/4.0/gtk-4.0.1.tar.xz"
+  sha256 "5e778f75e3808529d8300aae47b873675891de7d474c43bea32cc32e10d6df9c"
   license "LGPL-2.0-or-later"
 
   livecheck do
@@ -32,9 +32,6 @@ class Gtk4 < Formula
   depends_on "pango"
 
   uses_from_macos "libxslt" => :build # for xsltproc
-
-  # submitted upstream at https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/2962
-  patch :DATA
 
   def install
     args = std_meson_args + %w[
@@ -84,19 +81,3 @@ class Gtk4 < Formula
     assert_match version.to_s, shell_output("cat #{lib}/pkgconfig/gtk4.pc").strip
   end
 end
-
-__END__
-diff --git a/gdk/macos/gdkmacostoplevelsurface.c b/gdk/macos/gdkmacostoplevelsurface.c
-index e1f60e8221..4a84dc8304 100644
---- a/gdk/macos/gdkmacostoplevelsurface.c
-+++ b/gdk/macos/gdkmacostoplevelsurface.c
-@@ -97,7 +97,7 @@ _gdk_macos_toplevel_surface_present (GdkToplevel       *toplevel,
-   GdkSurfaceHints mask;
-   NSWindowStyleMask style_mask;
-   gboolean maximize;
--  gboolean fullscreen
-+  gboolean fullscreen;
-
-   g_assert (GDK_IS_MACOS_TOPLEVEL_SURFACE (self));
-   g_assert (GDK_IS_MACOS_WINDOW (nswindow));
-
