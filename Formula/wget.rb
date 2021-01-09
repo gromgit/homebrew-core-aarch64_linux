6c0f94bdcbea12c462a586ace79f6467929a3519
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-09 21:16:25 +0000

    wget 1.21.1
    - removed patch
    - build without included regex. It uses the glibc regex library, which
      doesn't work on macOS.
    
    Closes #68667.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/wget.rb
+++ Formula/wget.rb
@@ -1,8 +1,8 @@
 class Wget < Formula
   desc "Internet file retriever"
   homepage "https://www.gnu.org/software/wget/"
-  url "https://ftp.gnu.org/gnu/wget/wget-1.21.tar.gz"
-  sha256 "b3bc1a9bd0c19836c9709c318d41c19c11215a07514f49f89b40b9d50ab49325"
+  url "https://ftp.gnu.org/gnu/wget/wget-1.21.1.tar.gz"
+  sha256 "59ba0bdade9ad135eda581ae4e59a7a9f25e3a4bde6a5419632b31906120e26e"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -33,10 +33,6 @@ class Wget < Formula
     depends_on "util-linux"
   end
 
-  # commit ref, https://git.savannah.gnu.org/cgit/gnulib.git/patch/?id=6a76832db224ac5671599ce332717f985a2addc7
-  # remove in next release
-  patch :DATA
-
   def install
     system "./bootstrap", "--skip-po" if build.head?
     system "./configure", "--prefix=#{prefix}",
@@ -46,7 +42,8 @@ class Wget < Formula
                           "--disable-debug",
                           "--disable-pcre",
                           "--disable-pcre2",
-                          "--without-libpsl"
+                          "--without-libpsl",
+                          "--without-included-regex"
     system "make", "install"
   end
 
@@ -54,16 +51,3 @@ class Wget < Formula
     system bin/"wget", "-O", "/dev/null", "https://google.com"
   end
 end
-
-__END__
-diff --git a/lib/utime.c b/lib/utime.c
-index bf7d7c5..3372179 100644
---- a/lib/utime.c
-+++ b/lib/utime.c
-@@ -261,6 +261,7 @@ utime (const char *name, const struct utimbuf *ts)
-
- #else
-
-+# include <errno.h>
- # include <sys/stat.h>
- # include "filename.h"
