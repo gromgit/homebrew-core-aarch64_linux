88c097ecf3f7bd90a2ec0d460601b7c81c7edd3e
FX Coudert <fxcoudert@gmail.com>
2021-01-09 14:31:44 +0000

    sblim-sfcc: update 2.2.8_1 bottle.

--- Formula/sblim-sfcc.rb
+++ Formula/sblim-sfcc.rb
@@ -13,6 +13,7 @@ class SblimSfcc < Formula
   bottle do
     cellar :any
     sha256 "8a334b1f1c440e4103ab947b0871ea160a0d319cf095e8ce8b45521ec9440770" => :big_sur
+    sha256 "9b75fdfd7494479190a2651966a17408dfa29a1f2f62c860e165caa5f135a6df" => :arm64_big_sur
     sha256 "d20839c77aaa1c968981ca9af0e92bf1c7f600392ea5674971f532040dae518a" => :catalina
     sha256 "ff61a006626a9a36dafb474f352d798805b1a44adba341d8422bd0820eaae1ab" => :mojave
     sha256 "0ee558ce892d6e04acfe7ca2408a96e2837c7c858e71f6047b3a57a15b75ece0" => :high_sierra
