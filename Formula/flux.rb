2346c5c79c426311aba95d7eb146ec469074640c
Rui Chen <rui@chenrui.dev>
2021-01-09 13:08:46 -0500

    flux 0.100.0
    Closes #68627.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/flux.rb
+++ Formula/flux.rb
@@ -2,8 +2,8 @@ class Flux < Formula
   desc "Lightweight scripting language for querying databases"
   homepage "https://www.influxdata.com/products/flux/"
   url "https://github.com/influxdata/flux.git",
-      tag:      "v0.99.0",
-      revision: "2c56fe7a54c0c9a61b24854eb6ad9e143886c56e"
+      tag:      "v0.100.0",
+      revision: "6a9526df69e3aed5e0f4dc785f93350b8fe1c237"
   license "MIT"
   head "https://github.com/influxdata/flux.git"
 
