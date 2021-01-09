b55e7f5f4a18ce8317af5c703453b88a4e44a871
Rui Chen <rui@chenrui.dev>
2021-01-09 15:46:30 -0500

    macos-trash 1.1.1
    * macos-trash 1.1.1
    * requires swift 5.3.0
    * Update macos-trash.rb
    
    Closes #68656.
    
    Co-authored-by: chenrui <rui@chenrui.dev>
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/macos-trash.rb
+++ Formula/macos-trash.rb
@@ -1,8 +1,8 @@
 class MacosTrash < Formula
   desc "Move files and folders to the trash"
   homepage "https://github.com/sindresorhus/macos-trash"
-  url "https://github.com/sindresorhus/macos-trash/archive/1.1.0.tar.gz"
-  sha256 "31c09d385bb50b0f76818a1fe2c850cf56b9575c9fa27ea963cba38dfaba7d04"
+  url "https://github.com/sindresorhus/macos-trash/archive/v1.1.1.tar.gz"
+  sha256 "e215bda0c485c89a893b5ad8f4087b99b78b3616f26f0c8da3f0bb09022136dc"
   license "MIT"
   head "https://github.com/sindresorhus/macos-trash.git"
 
@@ -14,14 +14,14 @@ class MacosTrash < Formula
     sha256 "030b17bc67400a3f0873bd6983954fe0b55166b26c764e77b1d8cd1adc1bc1b7" => :mojave
   end
 
-  depends_on xcode: ["11.0", :build]
+  depends_on xcode: ["12.0", :build]
 
   conflicts_with "trash", because: "both install a `trash` binary"
   conflicts_with "trash-cli", because: "both install a `trash` binary"
 
   def install
-    system "./build"
-    bin.install "trash"
+    system "swift", "build", "--disable-sandbox", "-c", "release"
+    bin.install ".build/release/trash"
   end
 
   test do
