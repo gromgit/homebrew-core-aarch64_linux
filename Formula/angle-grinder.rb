13b49e049f4706e4efa77b894d12220262fc56bf
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-10 19:15:18 +0000

    angle-grinder: update Cargo.lock file
    See #68301.
    
    Closes #68729.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/angle-grinder.rb
+++ Formula/angle-grinder.rb
@@ -1,9 +1,11 @@
 class AngleGrinder < Formula
   desc "Slice and dice log files on the command-line"
   homepage "https://github.com/rcoh/angle-grinder"
+  # Remove "Cargo.lock" resource at version bump!
   url "https://github.com/rcoh/angle-grinder/archive/v0.15.0.tar.gz"
   sha256 "5359d6e241eca2bc3bdb7ddf9344b4ef8315cbe7629775c09e0ab7ed70310c8d"
   license "MIT"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -20,7 +22,15 @@ class AngleGrinder < Formula
     depends_on "openssl@1.1"
   end
 
+  # Replace stale lock file. Remove at version bump.
+  resource "Cargo.lock" do
+    url "https://raw.githubusercontent.com/rcoh/angle-grinder/6c5cf552dec605aac75c3da04e8618518b63abf2/Cargo.lock"
+    sha256 "a52f0e1d81eb77bdb10f9fa6b4bc5a2f4551045794570ac5d766de9085da4a54"
+  end
+
   def install
+    rm_f "Cargo.lock"
+    resource("Cargo.lock").stage buildpath
     system "cargo", "install", *std_cargo_args
   end
 
