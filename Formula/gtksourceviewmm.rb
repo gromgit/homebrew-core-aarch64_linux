bc98c153d998645b375a8f1f36d58477781aff0d
FX Coudert <fxcoudert@gmail.com>
2021-01-10 18:14:18 +0000

    gtksourceviewmm: update 2.10.3_11 bottle.

--- Formula/gtksourceviewmm.rb
+++ Formula/gtksourceviewmm.rb
@@ -14,6 +14,7 @@ class Gtksourceviewmm < Formula
   bottle do
     cellar :any
     sha256 "bf108d9937d07f20169bf75ad8a5152dd0e49a1b22f280cb0512d44e71a42656" => :big_sur
+    sha256 "2916245ab0c9b6b1fc59b1962cf2c21ed517e8adb686ca2b63395a9a66432499" => :arm64_big_sur
     sha256 "db7588b5e582cd8cebe9360a47119c6ffbd8f67e253e47125f2341d1b7b441b4" => :catalina
     sha256 "72ed8cbfc8f32720633bae6c041d04c7bfec0cae44f3aec08d36834cb648f46e" => :mojave
   end
