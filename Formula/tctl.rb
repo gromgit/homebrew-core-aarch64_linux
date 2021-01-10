78db076bcc232c93d3df78209e72e1d6b9a358b1
chenrui <rui@chenrui.dev>
2021-01-10 00:17:29 +0000

    tctl: add 1.5.1 bottle.

--- Formula/tctl.rb
+++ Formula/tctl.rb
@@ -5,6 +5,14 @@ class Tctl < Formula
   sha256 "a2f75873d7c211e07c40653c605e64fcf732543c9d50683b2d61a985f6b3f37f"
   license "MIT"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "544ac03608bb7f81ee4bbb8ddd278cfc9edfac72bb6d58bf5934bd140d5a6e2d" => :big_sur
+    sha256 "ccadc7ef85b244ed249bfdf05d5562ee2d5769262635117de1a069a35b561e97" => :arm64_big_sur
+    sha256 "6884961bcc2127b758efc784bcb11d1ed011dc88efd5c99f5f4ed0bdd013623b" => :catalina
+    sha256 "96c17b53863b45d71c8c895e292a673c27670f22c402732655a7cc5a396cc4fa" => :mojave
+  end
+
   depends_on "go" => :build
 
   def install
