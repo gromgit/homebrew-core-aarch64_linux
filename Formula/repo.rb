c24f7a4ecb788d751584de5ce9e974bb29b29917
rui <chenrui333@gmail.com>
2021-01-10 16:37:27 -0500

    repo 2.11.1 (#68682)
    * repo 2.11.1
    
    * repo: depends_on python3
    
    * Fix audit error
    
    * rewrite shebang for python3
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/repo.rb
+++ Formula/repo.rb
@@ -1,16 +1,22 @@
 class Repo < Formula
+  include Language::Python::Shebang
+
   desc "Repository tool for Android development"
   homepage "https://source.android.com/source/developing.html"
   url "https://gerrit.googlesource.com/git-repo.git",
-      tag:      "v2.10",
-      revision: "352c93b68084e97ec7a3c09f0bd3f2a161290211"
+      tag:      "v2.11.1",
+      revision: "b64bec6acc0e2a835f427d8424af89dfcddaf08f"
   license "Apache-2.0"
   version_scheme 1
 
   bottle :unneeded
 
+  depends_on "python@3.9"
+
   def install
     bin.install "repo"
+    rewrite_shebang detected_python_shebang, bin/"repo"
+
     doc.install (buildpath/"docs").children
   end
 
