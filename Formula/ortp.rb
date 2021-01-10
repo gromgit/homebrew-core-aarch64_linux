6381e9d28562e6380a75e0f382e3d28cb03fcf2d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-10 03:53:58 +0000

    ortp: update 4.4.22 bottle.

--- Formula/ortp.rb
+++ Formula/ortp.rb
@@ -7,10 +7,10 @@ class Ortp < Formula
   head "https://gitlab.linphone.org/BC/public/ortp.git"
 
   bottle do
-    sha256 "4007e619fdf9b62f48dc6897309d600bb7bcc927d94c62d53f9438e1c946dc8e" => :big_sur
-    sha256 "5ab4dc28a31243ab59862f74f5edc4d615115b539532b284ccd17a3bb8f8bbf1" => :arm64_big_sur
-    sha256 "5caba650034ac5418a2dfe1d5cb26e667af555dcba655a765ed7a0e21bbdd6ba" => :catalina
-    sha256 "1037969f3d5869cdde0f1ca6f3d1f8f7d3cb01c544fbcf14f913f96f29f8cf29" => :mojave
+    sha256 "5dce0b23909a21de289776a3722484857d9fa036dd6cf52696d87edd3c9e922d" => :big_sur
+    sha256 "6fbaa811dadbabf6b0913a45c8defb2fb222838491c32036ab032ffed5338318" => :arm64_big_sur
+    sha256 "9407713d2867b9b72e4485c2d33c439d5945774f4b99a0b018c41d036967c17a" => :catalina
+    sha256 "563de4996cc3532fc7d6c3bc79b3350f245eed0ff1f8e4fea93bd73c7ce4edb8" => :mojave
   end
 
   depends_on "cmake" => :build
