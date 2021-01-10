10394f85781f3919430caddae98cc695d3295d2e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-10 05:41:47 +0000

    sstp-client: update 1.0.14 bottle.

--- Formula/sstp-client.rb
+++ Formula/sstp-client.rb
@@ -11,11 +11,10 @@ class SstpClient < Formula
   end
 
   bottle do
-    sha256 "dfa678dbed0f557f73449091d107882f7f8f02debcbd6f665d180882c59f3a0a" => :big_sur
-    sha256 "4947d03bb68da6cc01050173ec3d6ef9eab6b83dc372f8330ade3897c8aa1b66" => :arm64_big_sur
-    sha256 "07d9e21c21591a675d760f059838f26bbbe02a04b27518bce5d3a9b0d0069194" => :catalina
-    sha256 "0c32039442cbe0c26bc90660fd99e19940b71677637b60d03350a4c90b3ca35c" => :mojave
-    sha256 "cfc794cfd038b84da0b1f329bc7eb6f5c5165e1727ec53c259a5d93ab48d47b0" => :high_sierra
+    sha256 "82b2a0b491752d77b6d47a0a4fc1cffe0656e0a04a5e49f2a6802f9b302378c0" => :big_sur
+    sha256 "a67f8784f0a716ed85a181bd5010b9f5ca700f0f44a107dcaf78560ba349e876" => :arm64_big_sur
+    sha256 "014b4af9e8f774591bb335fc49b2f010bf54cade6149dc0367cabee0a604f5f9" => :catalina
+    sha256 "060f97c76ca3086fc58ab20be2dad388a1299e4731f4571bca9d807f168d788e" => :mojave
   end
 
   depends_on "pkg-config" => :build
