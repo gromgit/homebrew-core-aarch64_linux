1ad8084c39c68491c314cf138019f84ed44de6e8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-10 05:42:10 +0000

    lzip: update 1.22 bottle.

--- Formula/lzip.rb
+++ Formula/lzip.rb
@@ -12,12 +12,10 @@ class Lzip < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "98412a7d26e986502ad50442e7d68de09809617d4fb16f5761bfe9506386780d" => :big_sur
-    sha256 "ede6a7fd4bf00698e706fd9f99dcc4712403fb92eaf10c40669ce493483cf4ed" => :arm64_big_sur
-    sha256 "e69bdf0e079d8b94221625310d0a0f6ad5cee544b4055d6a55d721f791a5b7f9" => :catalina
-    sha256 "54716b72b43e41cb8d9912fe3f61aeef651f890bf42f8b92482adfd0f2c99798" => :mojave
-    sha256 "bfe47a5379c4d793e15d1d71f5f6b12047a486e2531718f31f675683d54df595" => :high_sierra
-    sha256 "aba0ea18470d9aa4f619c0f6c133a4f459d4b02327ff539a44a1f473a2112369" => :sierra
+    sha256 "dd3e7b00a64e6de1f1cbb0446c2a3c0dac5033dd9b2de5f52fe56d7375c0d339" => :big_sur
+    sha256 "78014ac2f6011ba98beeabd7aa79e37b6ee78a5e9c72b7bf7594005bfcc7f082" => :arm64_big_sur
+    sha256 "91a7214e357c949e0a06736e6a73eb667c0c487efaeebeb4df6fae99ee660575" => :catalina
+    sha256 "7c4d9d33bda8dd4043a48903d9348e683c1c64c1b0ab39b1680fcaadb952896f" => :mojave
   end
 
   def install
