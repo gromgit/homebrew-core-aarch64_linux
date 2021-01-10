c7d2b2102b732a16931291eeefbfc71c7752b95b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-10 04:44:37 +0000

    tclap: update 1.2.3 bottle.

--- Formula/tclap.rb
+++ Formula/tclap.rb
@@ -12,13 +12,10 @@ class Tclap < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b47ec931191894dada537833a53058fbf33fd134b32c4bd2a4d23e71cfdfd5d8" => :big_sur
-    sha256 "9aa20fbd7d5f371e993181a9dbdbfc4b56b83cbcf55cd6efc84d68546c159aaf" => :arm64_big_sur
-    sha256 "025227d952333920194194626d4e80eb02439a7de957a6722bd672a0cb2c7631" => :catalina
-    sha256 "31d001dc926c1faf748a2dd0ad34fe9f3c3908400eac998802ce36ba78fab794" => :mojave
-    sha256 "8e85cfa71594680dc52d5ff18d93cf585fc5990c28316f0b60f42584ff3a2697" => :high_sierra
-    sha256 "8e85cfa71594680dc52d5ff18d93cf585fc5990c28316f0b60f42584ff3a2697" => :sierra
-    sha256 "8e85cfa71594680dc52d5ff18d93cf585fc5990c28316f0b60f42584ff3a2697" => :el_capitan
+    sha256 "1805257b4ea89658de13fa55b18386c5c342b0d53095ac425cbf326ceec35640" => :big_sur
+    sha256 "7759ca2808c4112bc4feaaca64f6dd93762554ac87dd2f7af531508535a70237" => :arm64_big_sur
+    sha256 "2d096686e490335890260c02e10b5fcc914372d43c6d9d6201186c367376dfe1" => :catalina
+    sha256 "e7ae47f1e056dd98bb0e60f8a827c2d895b9ab3ab71bb9b4f1cf9778408b4055" => :mojave
   end
 
   def install
