f284efb3443e5979a5f941385cb1791666dc7a0f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-10 01:08:33 +0000

    aws-sdk-cpp: update 1.8.120 bottle.

--- Formula/aws-sdk-cpp.rb
+++ Formula/aws-sdk-cpp.rb
@@ -8,10 +8,10 @@ class AwsSdkCpp < Formula
   head "https://github.com/aws/aws-sdk-cpp.git"
 
   bottle do
-    sha256 "92521fa0aff56aa4eccf0398cf597abecdc93a8f6cbc59dadadd8f7acf6f9f7d" => :big_sur
-    sha256 "1b9d34aeb806c71b31c46a863e71ca8cb11903e814e6adeece9f65d562b1cfc4" => :arm64_big_sur
-    sha256 "32046e5e0f89c53597aa5e8698ec9190b396a2719cef4638198bcaad1da5c34a" => :catalina
-    sha256 "4693d20e0a1cfaf8091078cf85552c51e925550cf425a70de41317aa2c23e83b" => :mojave
+    sha256 "d98285da26fd55808f07710eda4cb72bcd38fb30c5aff69a0ac0eca4af9ee815" => :big_sur
+    sha256 "02b978cfad1460bbbfb86be25c96d5607295b8c9be70fcd019b04c180a509599" => :arm64_big_sur
+    sha256 "150a1e02ec92257f0802ffc0d41ca6e1c92a3d01516eee927f5885964871b3d1" => :catalina
+    sha256 "5f0726c5265c2d0df15eabe33d3d3faddc2e51371031c55ed02f0a3fa4841a33" => :mojave
   end
 
   depends_on "cmake" => :build
