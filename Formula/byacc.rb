25609ec4dfd5efaa1a1881aaf4e5ac499d221e7f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-10 16:48:18 +0000

    byacc: update 20210109 bottle.

--- Formula/byacc.rb
+++ Formula/byacc.rb
@@ -12,11 +12,10 @@ class Byacc < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "72d12d69a0acb74ca78d7b6e78200601b696d9c9267fc7f1d57d08f4654c172f" => :big_sur
-    sha256 "7a08cd6c6f6a4fb998ef65c16e481dd4212ecfb359fed6b86acbf1272b5dc40b" => :arm64_big_sur
-    sha256 "57aeaa751341ad11d735626ad343aaae7dbb9401697c917a4c7268241751e435" => :catalina
-    sha256 "552cd6f3ad41dc11ddb8535b495594e7c63e9208e594f9bef29d317669512d6a" => :mojave
-    sha256 "f8ea7c1fea86a6814bf332b8a35e81a19a28f380c4beb28d7bf0eabebe96cf78" => :high_sierra
+    sha256 "f622ecf64a8e634f59004fa158f67bd707779557fa50ecc7f394e4d62c046614" => :big_sur
+    sha256 "8893ed78ff7c45e04651cbd56e5b4acd1e3f09cbee425615516f7a4f7e305d8c" => :arm64_big_sur
+    sha256 "d96b70d8631bc6feffabef7e00d98b5f41b00a49c9542cf6390f561dcb3bcd32" => :catalina
+    sha256 "e9422616cbec15f16ab180fc1fcfbabd9680a2a06f8163c1483c25f18cfe3a2f" => :mojave
   end
 
   def install
