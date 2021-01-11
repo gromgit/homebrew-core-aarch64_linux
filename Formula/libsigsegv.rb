e7337d1ee1668080cc6006bc8184b4ba681c1df3
Francois-Xavier Coudert <fxcoudert@gmail.com>
2021-01-11 10:09:29 +0100

    libsigsegv 2.13
    * libsigsegv: revision bump
    * libsigsegv 2.13
    
    Closes #68766.
    
    Co-authored-by: rui <rui@meetup.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/libsigsegv.rb
+++ Formula/libsigsegv.rb
@@ -1,9 +1,9 @@
 class Libsigsegv < Formula
   desc "Library for handling page faults in user mode"
   homepage "https://www.gnu.org/software/libsigsegv/"
-  url "https://ftp.gnu.org/gnu/libsigsegv/libsigsegv-2.12.tar.gz"
-  mirror "https://ftpmirror.gnu.org/libsigsegv/libsigsegv-2.12.tar.gz"
-  sha256 "3ae1af359eebaa4ffc5896a1aee3568c052c99879316a1ab57f8fe1789c390b6"
+  url "https://ftp.gnu.org/gnu/libsigsegv/libsigsegv-2.13.tar.gz"
+  mirror "https://ftpmirror.gnu.org/libsigsegv/libsigsegv-2.13.tar.gz"
+  sha256 "be78ee4176b05f7c75ff03298d84874db90f4b6c9d5503f0da1226b3a3c48119"
   license "GPL-2.0-or-later"
 
   livecheck do
