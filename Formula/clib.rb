60711a55a6184398c8ae0a930717fd00e5a74a19
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-11 08:37:21 +0000

    clib: update 2.6.0 bottle.

--- Formula/clib.rb
+++ Formula/clib.rb
@@ -13,10 +13,10 @@ class Clib < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5c09d1b21de092b57a9fb16170dba836e5f4df51b9a6a47302aa4f2f90b565a0" => :big_sur
-    sha256 "498960fdece8741fdb2e650d5d8a4931666402ca1229a73c93934bf9cd48455c" => :arm64_big_sur
-    sha256 "e6f3ca8fbc6b39325af968a81e8f09045de75e90dae2d6db3a5a6e72afee2766" => :catalina
-    sha256 "acebab1f84196d10519777630a1dec10858f2729208c111094bdaf49a11b77bb" => :mojave
+    sha256 "270425824692bd5bdf84569139ec0ff874b6b1e742630caaff9e391f49e138d7" => :big_sur
+    sha256 "d41e1b435b736381ccce4b89b064b03883c81b0cf3155e17f194a8b90dfce454" => :arm64_big_sur
+    sha256 "cf908d84d60db5f00592e9a7df93b91d2da0628ee9de2b6d2cc06f6f8c2feda6" => :catalina
+    sha256 "006e13cc8877c80227846fde77754cae52abcb2780a3fc6361581ea0c0d561b3" => :mojave
   end
 
   uses_from_macos "curl"
