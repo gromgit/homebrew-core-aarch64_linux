8a6807be6abb44634e7d6d153348b6bba2a5ddc6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-11 06:37:34 +0000

    petsc-complex: update 3.14.3 bottle.

--- Formula/petsc-complex.rb
+++ Formula/petsc-complex.rb
@@ -6,10 +6,10 @@ class PetscComplex < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 "06f085d7e2d65268531f3dfb1c3a41fc954310843c9dd2b0f2f81f261d0602ad" => :big_sur
-    sha256 "f098cf417f6ca1759a93932ef34841531bc971730b501514323f015253fd5ebe" => :arm64_big_sur
-    sha256 "dd6321b74cae89523563d6245bb054e8324d73dac127de8a906452aae5f36566" => :catalina
-    sha256 "6a328e638d1cdfa0ba9a89300b0a090013de490bd5e5153966b3c7d173153b25" => :mojave
+    sha256 "6ac5b57d4d2de403a56d5789213d3f9b99ed434087b16bab042819cc5ebce9b6" => :big_sur
+    sha256 "76e4200aaa81dfd2d8c800cedf6a6f24557ae83952f4ffc99cda02bcaed39b0d" => :arm64_big_sur
+    sha256 "31ca2ed2565105782d06018fb473e25983c234523c86a196582d4ced9f1e40c6" => :catalina
+    sha256 "97becdd5e411b15d4be63945f5d3b1b48a95a85251f1e6ffdd906631acc4e70a" => :mojave
   end
 
   depends_on "hdf5"
