b33db2fe358351af043bb38a17eb865b0f23e3ef
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-11 13:36:15 +0000

    luajit-openresty 2.1-20201229 (new formula)
    See discussion at #68013 for context.
    
    Closes #68787.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

---
+++ Formula/luajit-openresty.rb
@@ -0,0 +1,61 @@
+class LuajitOpenresty < Formula
+  desc "OpenResty's Branch of LuaJIT 2"
+  homepage "https://github.com/openresty/luajit2"
+  url "https://github.com/openresty/luajit2/archive/v2.1-20201229.tar.gz"
+  sha256 "4275bf97356d713826e7195d1c330568b6089ff1fd4c6954f998e0f60a2baa30"
+  license "MIT"
+  head "https://github.com/openresty/luajit2.git", branch: "v2.1-agentzh"
+
+  keg_only "it conflicts with the LuaJIT formula"
+
+  def install
+    # 1 - Override the hardcoded gcc.
+    # 2 - Remove the "-march=i686" so we can set the march in cflags.
+    # Both changes should persist and were discussed upstream.
+    inreplace "src/Makefile" do |f|
+      f.change_make_var! "CC", ENV.cc
+      f.change_make_var! "CCOPT_x86", ""
+    end
+
+    # Per https://luajit.org/install.html: If MACOSX_DEPLOYMENT_TARGET
+    # is not set then it's forced to 10.4, which breaks compile on Mojave.
+    ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version
+
+    args = %W[
+      PREFIX=#{prefix}
+      XCFLAGS=-DLUAJIT_ENABLE_GC64
+    ]
+
+    system "make", "amalg", *args
+    system "make", "install", *args
+
+    # LuaJIT doesn't automatically symlink unversioned libraries:
+    # https://github.com/Homebrew/homebrew/issues/45854.
+    lib.install_symlink lib/"libluajit-5.1.dylib" => "libluajit.dylib"
+    lib.install_symlink lib/"libluajit-5.1.a" => "libluajit.a"
+
+    # Fix path in pkg-config so modules are installed
+    # to permanent location rather than inside the Cellar.
+    inreplace lib/"pkgconfig/luajit.pc" do |s|
+      s.gsub! "INSTALL_LMOD=${prefix}/share/lua/${abiver}",
+              "INSTALL_LMOD=#{HOMEBREW_PREFIX}/share/lua/${abiver}"
+      s.gsub! "INSTALL_CMOD=${prefix}/${multilib}/lua/${abiver}",
+              "INSTALL_CMOD=#{HOMEBREW_PREFIX}/${multilib}/lua/${abiver}"
+      unless build.head?
+        s.gsub! "Libs:",
+                "Libs: -pagezero_size 10000 -image_base 100000000"
+      end
+    end
+
+    # Having an empty Lua dir in lib/share can mess with other Homebrew Luas.
+    %W[#{lib}/lua #{share}/lua].each { |d| rm_rf d }
+  end
+
+  test do
+    system "#{bin}/luajit", "-e", <<~EOS
+      local ffi = require("ffi")
+      ffi.cdef("int printf(const char *fmt, ...);")
+      ffi.C.printf("Hello %s!\\n", "#{ENV["USER"]}")
+    EOS
+  end
+end
