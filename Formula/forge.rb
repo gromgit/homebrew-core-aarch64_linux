b692fd8528780e4f7e62b7f393050c06bf414b7f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-11 02:32:58 +0000

    forge: add 1.0.5 bottle.

--- Formula/forge.rb
+++ Formula/forge.rb
@@ -5,6 +5,13 @@ class Forge < Formula
   sha256 "4ed631cfde6a9c0daf786d68b47719edec024928b3e23dbfb2e786e60ab01097"
   license "BSD-3-Clause"
 
+  bottle do
+    cellar :any
+    sha256 "af25416dba5b9b0ea4fd3cc26270a9ee9cd955d5f265e3b2e325dc89628be568" => :big_sur
+    sha256 "d97b409d7830196f87123ef062672df3ccbea3363d6dce0e74c98e2e192b0c0a" => :catalina
+    sha256 "239cd658c7042ab7591e1575ee80b09a6e74279276b7227ed2ad8a47907e6a84" => :mojave
+  end
+
   depends_on "boost" => :build
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
