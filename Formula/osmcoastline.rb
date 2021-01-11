de556deaec8fd12bb6ddf0e1c96495874d8770c2
kkdd <kkondo_z0@nifty.com>
2021-01-11 19:42:34 +0900

    osmcoastline 2.3.0 (new formula)
    Closes #68781.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

---
+++ Formula/osmcoastline.rb
@@ -0,0 +1,33 @@
+class Osmcoastline < Formula
+  desc "Extracts coastline data from OpenStreetMap planet file"
+  homepage "https://osmcode.org/osmcoastline/"
+  url "https://github.com/osmcode/osmcoastline/archive/v2.3.0.tar.gz"
+  sha256 "b08ddcc8cb494a19cbf4d9f1501c47443fe374a4fe171e6f55c730ca1e710689"
+  license "GPL-3.0-or-later"
+
+  depends_on "cmake" => :build
+  depends_on "libosmium" => :build
+  depends_on "gdal"
+  depends_on "geos"
+  depends_on "libspatialite"
+
+  uses_from_macos "sqlite"
+  uses_from_macos "zlib"
+
+  def install
+    protozero = Formula["libosmium"].opt_libexec/"include"
+    system "cmake", ".", "-DPROTOZERO_INCLUDE_DIR=#{protozero}", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"input.opl").write <<~EOS
+      n100 v1 x1.01 y1.01
+      n101 v1 x1.04 y1.01
+      n102 v1 x1.04 y1.04
+      n103 v1 x1.01 y1.04
+      w200 v1 Tnatural=coastline Nn100,n101,n102,n103,n100
+    EOS
+    system "#{bin}/osmcoastline", "-v", "-o", "output.db", "input.opl"
+  end
+end
