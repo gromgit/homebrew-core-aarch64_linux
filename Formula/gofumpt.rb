a6feb5ad059a3bfecde779caf50dbe43ee7575e3
rui <rui@chenrui.dev>
2021-01-12 03:30:36 +0000

    gofumpt: add 0.1.0 bottle.

--- Formula/gofumpt.rb
+++ Formula/gofumpt.rb
@@ -5,6 +5,14 @@ class Gofumpt < Formula
   sha256 "802c92d3df222c9b266d785305107c58a26ea186c4dbb5989b0db97b9bce0367"
   license "BSD-3-Clause"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "85b2788203df32191b839d607d908e43a250d7cd687ad11705afc76f80e0c0d7" => :big_sur
+    sha256 "5bcea30433a93afeef620532b03d9c0d19e4bb864ee8be5e2a132615911f0c77" => :arm64_big_sur
+    sha256 "b69839b084e95f9339b776d979ebffc1382d93917aeabc6c1e1880fb6a9056c7" => :catalina
+    sha256 "b254aafd541d63d411ec6a034cf88c926b5354cf59bbc37fbaf6eb051fe3a14e" => :mojave
+  end
+
   depends_on "go"
 
   def install
