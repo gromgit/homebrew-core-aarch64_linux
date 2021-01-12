2ea4b963687a0b4f0f5c5bab30dce3351d8575a9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-12 15:29:47 +0000

    act: update 0.2.18 bottle.

--- Formula/act.rb
+++ Formula/act.rb
@@ -7,11 +7,10 @@ class Act < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "2faa63c64245afacf70f1c61ae445a26571bfe7c190ee39169ab0d73b47cceda" => :big_sur
-    sha256 "8804c231613bbf7be0e348683fe3482f7f09c3c7c9396278c64db0986f803d46" => :arm64_big_sur
-    sha256 "e91282fdbe17e8e559f99ac0e0d704ea541197587277428a21b9927cb30b009c" => :catalina
-    sha256 "37e68207cd6dd80222e79501a2fc6870e0093b257c8b2700a428f91e4cefc834" => :mojave
-    sha256 "259e716e51ba28f1b73f203acd3af2cd368789d41f83b000e7a8cf6b98c6e1ac" => :high_sierra
+    sha256 "129324772b70bddbac8bf5f05ad2c446a898287089d07c7590b0812efc66cf95" => :big_sur
+    sha256 "00d1cd0957e9bc4c316d331891cb490657f1203fda35f9d2ea7033edcb830065" => :arm64_big_sur
+    sha256 "63e4c98ff67ce6e8c39dc1cdc96e782328adfbe08c3ee6b420dd75f942dea242" => :catalina
+    sha256 "f44412d572c2ae10f1e547ff26f6c01b55609ebe34fdc4fe78be18e4ef7bb0b5" => :mojave
   end
 
   depends_on "go" => :build
