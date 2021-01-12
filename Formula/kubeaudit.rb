736c68b16a9df5c45ac6cd043fe30aaa9847cb80
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-12 20:35:51 +0000

    kubeaudit: update 0.11.8 bottle.

--- Formula/kubeaudit.rb
+++ Formula/kubeaudit.rb
@@ -8,10 +8,10 @@ class Kubeaudit < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "dd32165aba7df729e178e435113fa25b0252913bce4388b3c435c1f2ea1d77af" => :big_sur
-    sha256 "f431e84f4150d874e9360128fad0281231fff800b2bf56aee5f21d9b17834b70" => :arm64_big_sur
-    sha256 "253c78f4db9497e15f90cf3ba3ca4a5578a05264c4fbebb59bf82b51bc48d016" => :catalina
-    sha256 "af1c1b0dd2b999356ac5a66fb331f17bf54098e0627eb36d7526655a5407a40e" => :mojave
+    sha256 "0d4b2eb5ce9eb6b7ca61ce188878bc4b57b88de1d33937385661517c12e84d9e" => :big_sur
+    sha256 "d5132b86c9683d1568b4e603f796b338de04b03140b93e141c48ccce75c3395b" => :arm64_big_sur
+    sha256 "77543931b661a0ecfe9e0a91dd9b25c87fc0b54e70835d4db47e033f32fc0fe7" => :catalina
+    sha256 "6deea8095f482a21689c036cdbb89766fd042c5b92d1c9915b2fedd797c904b1" => :mojave
   end
 
   depends_on "go" => :build
