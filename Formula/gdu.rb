8b7e01a125d1df1e4c8d51d324159cf24e9bb19e
Dawid Dziurla <dawidd0811@gmail.com>
2021-01-12 09:02:49 +0000

    gdu: add 2.2.0 bottle.

--- Formula/gdu.rb
+++ Formula/gdu.rb
@@ -5,6 +5,14 @@ class Gdu < Formula
   sha256 "94f4faaee6c1676b73bb75f031cc3d004b8df713fb12d1dfc90ed592bf302df7"
   license "MIT"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2e394192ec479375b483f5a882e01f1ade6b03fa298e7d52b85c3013a27e0282" => :big_sur
+    sha256 "05a6430db194b98832d8c63383176e1f7fd77a4633feabd71703e9fa48090830" => :arm64_big_sur
+    sha256 "203d2846cfdc7f20db09dde616c72cbae7fcea10b55076aa5c4c0fdf499c01eb" => :catalina
+    sha256 "ac292f74b86e7d96493e34a2c003e14aab9a3a913e099a8ca5ee295c866de75a" => :mojave
+  end
+
   depends_on "go" => :build
 
   def install
