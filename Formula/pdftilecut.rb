008ec108303ff4b2c19a0010d40ea579aeb34e3c
Laurent Goussard <loranger@free.fr>
2021-01-12 21:56:03 +0100

    pdftilecut 0.4 (new formula)
    Closes #68881.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

---
+++ Formula/pdftilecut.rb
@@ -0,0 +1,21 @@
+class Pdftilecut < Formula
+  desc "Sub-divide a PDF page(s) into smaller pages so you can print them"
+  homepage "https://github.com/oxplot/pdftilecut"
+  url "https://github.com/oxplot/pdftilecut/archive/v0.4.tar.gz"
+  sha256 "8a75a0d2e196d156bca4dc006f84c9b7730f20be8c0fc2c90521c538aa627188"
+  license "BSD-3-Clause"
+
+  depends_on "go" => :build
+  depends_on "jpeg-turbo"
+  depends_on "qpdf"
+
+  def install
+    system "go", "build", *std_go_args
+  end
+
+  test do
+    testpdf = test_fixtures("test.pdf")
+    system "#{bin}/pdftilecut", "-tile-size", "A6", "-in", testpdf, "-out", "split.pdf"
+    assert_predicate testpath/"split.pdf", :exist?, "Failed to create split.pdf"
+  end
+end
