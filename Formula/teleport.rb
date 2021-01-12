faf411222498e9ff5dabf44dc2ebe217cceb8e96
Michka Popoff <michkapopoff@gmail.com>
2021-01-12 10:30:32 +0100

    teleport: uses netcat from macos

--- Formula/teleport.rb
+++ Formula/teleport.rb
@@ -26,11 +26,8 @@ class Teleport < Formula
   depends_on "go" => :build
 
   uses_from_macos "curl" => :test
+  uses_from_macos "netcat" => :test
   uses_from_macos "zip"
-
-  on_linux do
-    depends_on "netcat" => :test
-  end
 
   conflicts_with "etsh", because: "both install `tsh` binaries"
 
