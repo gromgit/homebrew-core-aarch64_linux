b30469c17cf4f092e22bbcf55f86ad55d3b1399f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-13 20:48:57 +0000

    tendermint: update 0.34.2 bottle.

--- Formula/tendermint.rb
+++ Formula/tendermint.rb
@@ -8,10 +8,10 @@ class Tendermint < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "042388d290bcb338f4fe17b494a3b7b88f7d8265917f4def5d26e584a6683f83" => :big_sur
-    sha256 "b907fe7d59b9d069fa8c0d7ad3744c9cb64e66410987e3d7542ed72a0debc382" => :arm64_big_sur
-    sha256 "daf9d9d25ab8cbf9684c4c796e7ce4f1bcd99a94276d60afad046ebd4cbe7b7e" => :catalina
-    sha256 "b49e5a312fba9613b4c95d7966d43ffe2fce22805522d93110c2bff1bdbc9e70" => :mojave
+    sha256 "305ebc63c28d0b66822e201bc2551d67b5186386d7964c5657087eef20257ab7" => :big_sur
+    sha256 "dbdff265a952ff04ef727ca2ef4c5537ae1d38045f103ce644f4f516c1be09e9" => :arm64_big_sur
+    sha256 "434b66b1a2e9dc6fd3b363e3120eaf8d226945f89c195b333b12627a74da05ca" => :catalina
+    sha256 "3a410e6879341212259a38a4078ba686910d22bb2913869914d79af3180edb32" => :mojave
   end
 
   depends_on "go" => :build
