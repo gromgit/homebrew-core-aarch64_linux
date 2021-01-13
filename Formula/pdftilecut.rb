eb942502e0c91c0c77d349b69908b7cfd5fabb29
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-13 18:30:35 +0000

    pdftilecut: add 0.4 bottle.

--- Formula/pdftilecut.rb
+++ Formula/pdftilecut.rb
@@ -5,6 +5,14 @@ class Pdftilecut < Formula
   sha256 "8a75a0d2e196d156bca4dc006f84c9b7730f20be8c0fc2c90521c538aa627188"
   license "BSD-3-Clause"
 
+  bottle do
+    cellar :any
+    sha256 "e9cd816b6b435a80e0860b9a3d4e62aea35e85bbb6df82ba9308c4b8c10245d1" => :big_sur
+    sha256 "a36f6efee23fc0af788bdef8017bcfee5b0fa25031541a499508f4942d743083" => :arm64_big_sur
+    sha256 "015c4f2b9481772318dabbf7f8cb5aa0640e46781d7cd7ef5cfc9f33b91afb79" => :catalina
+    sha256 "59229d726c29240bd14240e9011bfc1ec731ffcef8e52373c5a720649e6cacb7" => :mojave
+  end
+
   depends_on "go" => :build
   depends_on "jpeg-turbo"
   depends_on "qpdf"
