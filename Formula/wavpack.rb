fcef5e5fdf3fa95349a74598d05f634a4a26e81a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-13 20:48:59 +0000

    wavpack: update 5.4.0 bottle.

--- Formula/wavpack.rb
+++ Formula/wavpack.rb
@@ -7,11 +7,10 @@ class Wavpack < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "78570562d7429a48decbdd52fad54a20f8c768d8a529c314e424a60ede55ecb9" => :big_sur
-    sha256 "53e06631a0fd39762ad7d858b059df1a45af5244f1d1951832ae6c7faabba3d9" => :arm64_big_sur
-    sha256 "faef125fe8557e79bc97e1ded99e0050997fff0aba8cf0628771b686d5251244" => :catalina
-    sha256 "f8237f75aa50b6e8bd97bb4e3729a477ac569936d63d67a0df6e665ec4069a59" => :mojave
+    sha256 "711535e6329af607e974ee9e3e957fd82b056af39e95a78226bd9e8f9a0025dd" => :big_sur
+    sha256 "3e51f2f593fffc58cd550236d6a9711900841d388d6ac7833c09f272270d5156" => :arm64_big_sur
+    sha256 "29360357a18df2827e8acb41fdd874495b62f431deb230c96b8d5dcca5f6d316" => :catalina
+    sha256 "89815dc5f1b4af0d647d2b1d3ad484ddce39208958bb315793240aa7be0c4565" => :mojave
   end
 
   head do
