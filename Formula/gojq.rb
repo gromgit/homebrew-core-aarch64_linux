e3f75661e189f72d433d8f862b35f7e6d08d18cc
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-14 14:51:43 +0000

    gojq: update 0.12.0 bottle.

--- Formula/gojq.rb
+++ Formula/gojq.rb
@@ -8,6 +8,7 @@ class Gojq < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "b8937d8be16fa3f13d0e9f971d2d7e5c0ff0f8cb4cfb02ec53d72344ecd84cf6" => :big_sur
+    sha256 "7ab6960bd90fb64aeff25ce6af2fc96e1bafc2128501e4096e6f4fc442f1034b" => :arm64_big_sur
     sha256 "522b3e31ccd269471af44917ecde15099000bb78505bbf31d2879faa1af7af4f" => :catalina
     sha256 "dd0167516da92c42f829858caf8fddbb8ee55f060ee622fd187fc10bd25c0a0d" => :mojave
   end
