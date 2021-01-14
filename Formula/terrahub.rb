8439736fa8d5dd2bdccb9c3bac626ddf258a8588
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-14 18:33:05 +0000

    terrahub: update 0.4.45 bottle.

--- Formula/terrahub.rb
+++ Formula/terrahub.rb
@@ -13,10 +13,10 @@ class Terrahub < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "cc43b7a60a4b4e3e9ab212b60a8f6f79d1dda07b0324f566c53b62a91a25a0b1" => :big_sur
-    sha256 "38db019ecbb50f5d7f38f8a610cc99f25c3c5c38c53901065223cb5f9b27eedc" => :arm64_big_sur
-    sha256 "53d9a273e958a975c4fa6f655382b71d35ca5ce46191c608a7fe41f6b3841ae4" => :catalina
-    sha256 "f4d624a41241a5f9736a4495092b19a2b71a17ca87312ae8676571654d6355bc" => :mojave
+    sha256 "c6db2371894dbf89d9b3d231ce39279fa36b3f41c90668d8edcb3f61cbad7daf" => :big_sur
+    sha256 "145e3f988531b8c835c3c8427839525280d4348935ed8056a79c507437945c95" => :arm64_big_sur
+    sha256 "531571fdfafece71b4762f5348892abd535fbd7d1cd92a848dd30f62fe84202c" => :catalina
+    sha256 "bfa6c5a7cc985f29cb4903b6cc26279b5fd0ae2fd7cd7b4bc09d84c4e3bba4c4" => :mojave
   end
 
   depends_on "node"
