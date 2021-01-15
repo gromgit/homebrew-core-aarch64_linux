83e4cfced9494bb6b1fe6a99b716db329de56fe9
FX Coudert <fxcoudert@gmail.com>
2021-01-15 10:34:25 +0000

    abcl: update 1.8.0 bottle.

--- Formula/abcl.rb
+++ Formula/abcl.rb
@@ -17,6 +17,7 @@ class Abcl < Formula
     cellar :any_skip_relocation
     rebuild 1
     sha256 "48a45fe17949a01e3164abecfe77a30849cd280faac0233f11a38a216194146a" => :big_sur
+    sha256 "eae344c99094aed9e35305346291d0adf0d6475c4315b6e0f6057b76261952fb" => :arm64_big_sur
     sha256 "a289f1940cf66e136607417d82ea29ab1df61523f5e6bc6608989c683eba509f" => :catalina
     sha256 "69eae372ce01d3e1844747e79ea2dd68763e3997d5bcdbaaf8641fc6f398a23c" => :mojave
     sha256 "a420b36787e573ecb71a14c9b45780881a25ba7d7897eb79ae74595eab21853d" => :high_sierra
