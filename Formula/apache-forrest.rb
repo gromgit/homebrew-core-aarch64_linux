e0c1632d83acc65fefd3d771fbf568d1fec64500
FX Coudert <fxcoudert@gmail.com>
2021-01-15 10:32:48 +0000

    apache-forrest: update 0.9_1 bottle.

--- Formula/apache-forrest.rb
+++ Formula/apache-forrest.rb
@@ -15,6 +15,7 @@ class ApacheForrest < Formula
     cellar :any_skip_relocation
     rebuild 1
     sha256 "2767298b0c6c15419a22f4a9ed56e0eaf8b0f466a23a7edbaf4db0ae7e179871" => :big_sur
+    sha256 "141e43e9268d661881cd2873e71bfce3e7c0e95b92cb6f5786ef0905d0dbb03f" => :arm64_big_sur
     sha256 "53aed268e732c00ae5d57d4b98287c59f12c124f5a1b925d02aefacdc6dc5132" => :catalina
     sha256 "53aed268e732c00ae5d57d4b98287c59f12c124f5a1b925d02aefacdc6dc5132" => :mojave
     sha256 "53aed268e732c00ae5d57d4b98287c59f12c124f5a1b925d02aefacdc6dc5132" => :high_sierra
