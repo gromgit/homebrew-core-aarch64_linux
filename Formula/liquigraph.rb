bed989942f3fb2be10b4b8a92f4d532cbc40f2b5
FX Coudert <fxcoudert@gmail.com>
2021-01-15 10:44:43 +0000

    liquigraph: update 4.0.2 bottle.

--- Formula/liquigraph.rb
+++ Formula/liquigraph.rb
@@ -9,6 +9,7 @@ class Liquigraph < Formula
   bottle do
     cellar :any_skip_relocation
     sha256 "1fa0da0d2e5c22f63faed17097c320d4a639c4d3855385f541cf948aa6a5a61b" => :big_sur
+    sha256 "bf45c4112732a17a867a17a18cc1ad77d71dbebfdca84f00843bd9526c4d6ed2" => :arm64_big_sur
     sha256 "33d7c1bae094524db782c9b3d3b0f37b4353f772529ab143b456c6271e262059" => :catalina
     sha256 "3a6ed6c8c176e1ffa0e3faef3cd1ed0025663dc23fd2d839372a9766e26fd2b7" => :mojave
     sha256 "d8c4ae157ed9d5ea8aad53d9b07784c21e41a4ac5a7756f0dacb9f526e809405" => :high_sierra
