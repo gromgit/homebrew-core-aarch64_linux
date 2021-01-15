aa894a42f4ce0759ff9518b7b27cdd3957b90eed
Fabio Sangiovanni <sjh+github@sanjioh.org>
2021-01-15 21:52:21 +0100

    volta 1.0.1
    Closes #69126.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/volta.rb
+++ Formula/volta.rb
@@ -2,8 +2,8 @@ class Volta < Formula
   desc "JavaScript toolchain manager for reproducible environments"
   homepage "https://volta.sh"
   url "https://github.com/volta-cli/volta.git",
-      tag:      "v1.0.0",
-      revision: "d523e32f03f8c4a653ded9a73fb4cc8a8b49ea22"
+      tag:      "v1.0.1",
+      revision: "56a306c4fd0c0a88d7ff986724d5cf8e1af920b1"
   license "BSD-2-Clause"
 
   livecheck do
