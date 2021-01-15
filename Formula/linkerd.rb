20c161ca656115fc5dc83429f4e3b1657bc6082c
Rui Chen <rui@chenrui.dev>
2021-01-14 20:33:26 -0500

    linkerd 2.9.2
    Closes #69062.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/linkerd.rb
+++ Formula/linkerd.rb
@@ -3,8 +3,8 @@ class Linkerd < Formula
   homepage "https://linkerd.io"
 
   url "https://github.com/linkerd/linkerd2.git",
-      tag:      "stable-2.9.1",
-      revision: "c3cb84a6382c781f9650d90875a1e89d95346d0c"
+      tag:      "stable-2.9.2",
+      revision: "d5e9d56ce431bdf5ec02e51461ec0cf4bfdd783c"
   license "Apache-2.0"
 
   livecheck do
