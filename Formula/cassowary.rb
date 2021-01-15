49774f51f23d1fd6cdee90cacc7608f3347511a1
Rui Chen <rui@chenrui.dev>
2021-01-15 11:30:20 -0500

    cassowary 0.12.0
    Closes #69116.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- Formula/cassowary.rb
+++ Formula/cassowary.rb
@@ -1,8 +1,8 @@
 class Cassowary < Formula
   desc "Modern cross-platform HTTP load-testing tool written in Go"
   homepage "https://github.com/rogerwelin/cassowary"
-  url "https://github.com/rogerwelin/cassowary/archive/v0.11.0.tar.gz"
-  sha256 "60d1bc68b75a59bc5511fd33eb77b14acd735887c74af1bbc4ea68badd271606"
+  url "https://github.com/rogerwelin/cassowary/archive/v0.12.0.tar.gz"
+  sha256 "e6df0f7ee0a93aa8abc87e241d64f3906f3a21e845aacc494d22ffb9517ef81e"
   license "MIT"
   head "https://github.com/rogerwelin/cassowary.git"
 
