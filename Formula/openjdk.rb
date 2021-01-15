a5c4f3ae2c4354d34b6355f162f12268f1cdc7b0
FX Coudert <fxcoudert@gmail.com>
2021-01-15 09:19:16 +0000

    openjdk: update 15.0.1 bottle.

--- Formula/openjdk.rb
+++ Formula/openjdk.rb
@@ -21,6 +21,7 @@ class Openjdk < Formula
   bottle do
     cellar :any
     sha256 "6f31366f86a5eacf66673fca9ad647b98b207820f8cfea49a22af596395d3dba" => :big_sur
+    sha256 "679c34348a8ce43888280c0a9724b644e503b0476c6ef679358234845986af21" => :arm64_big_sur
     sha256 "9376a1c6fdf8b0268b6cb56c9878358df148b530fcb0e3697596155fad3ca8d7" => :catalina
     sha256 "a4f00dc8b4c69bff53828f32c82b0a6be41b23a69a7775a95cdbc9e01d9bdb68" => :mojave
     sha256 "bef2e4a43a6485253c655979cfc719332fb8631792720c0b9f6591559fb513f1" => :high_sierra
