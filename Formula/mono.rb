38464dff41172272d0e6651ba2277cc11332bf0c
Mikhail Filippov <mikhail@filippov.me>
2021-01-15 11:03:29 +0300

    mono 6.12.0.107
    * mono 6.12.0.107
    * mono: add a patch for msbuild
    * fsharp 11.0.0
    * Fix another MSBuild build issue
    * Fix MSBuild install command
    * Fix F# build
    
    Closes #69089.
    
    Co-authored-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>

--- Formula/mono.rb
+++ Formula/mono.rb
@@ -1,8 +1,8 @@
 class Mono < Formula
   desc "Cross platform, open source .NET development framework"
   homepage "https://www.mono-project.com/"
-  url "https://download.mono-project.com/sources/mono/mono-6.12.0.90.tar.xz"
-  sha256 "51de5c02ad511333f93ff585bca54c8784de35af4ff27b759d16b46c4402cdac"
+  url "https://download.mono-project.com/sources/mono/mono-6.12.0.107.tar.xz"
+  sha256 "61f3cd629f8e99371c6b47c1f8d96b8ac46d9e851b5531eef20cdf9ab60d2a5f"
   license "MIT"
 
   livecheck do
@@ -38,16 +38,57 @@ class Mono < Formula
   link_overwrite "lib/cli"
 
   resource "fsharp" do
-    url "https://github.com/fsharp/fsharp.git",
-        tag:      "10.2.3",
-        revision: "e31bc96e8a5e5742af1c6c45d55d5cc06bb524cb"
+    url "https://github.com/dotnet/fsharp.git",
+        tag:      "v11.0.0-beta.20471.5",
+        revision: "03283e07f6bd5717797acb288cf6044cedca2202"
+    # F# patches hhen upgrading Mono, make sure to use the revision from
+    # https://github.com/mono/mono/blob/mono-#{version}/packaging/MacSDK/fsharp.py
+    patch do
+      url "https://raw.githubusercontent.com/mono/mono/a22ed3f094e18f1f82e1c6cead28d872d3c57e40/packaging/MacSDK/patches/fsharp-portable-pdb.patch"
+      sha256 "5b09b0c18b7815311680cc3ecd9bb30d92a307f3f2103a5b58b06bc3a0613ed4"
+    end
+    patch do
+      url "https://raw.githubusercontent.com/mono/mono/a22ed3f094e18f1f82e1c6cead28d872d3c57e40/packaging/MacSDK/patches/fsharp-netfx-multitarget.patch"
+      sha256 "112f885d4833effb442cf586492cdbd7401d6c2ba9d8078fe55e896cc82624d7"
+    end
+    patch do
+      url "https://github.com/dotnet/fsharp/commit/be6b22d11ae996b2d9b8e0724d9cf05ad65a0485.patch?full_index=1"
+      sha256 "793a39da798673b99289f3ac344ff8bd23d7eea2d3366c28e7e42229d8b130ca"
+    end
+  end
+
+  resource "fsharp-layout-patch" do
+    url "https://raw.githubusercontent.com/mono/mono/3070886a1c5e3e3026d1077e36e67bd5310e0faa/packaging/MacSDK/fsharp-layout.sh"
+    sha256 "f2cc63bf77e50663d91c6d102ba1d9217d1b9100c57071f79f0ae5a45e80ef42"
   end
 
   # When upgrading Mono, make sure to use the revision from
   # https://github.com/mono/mono/blob/mono-#{version}/packaging/MacSDK/msbuild.py
   resource "msbuild" do
     url "https://github.com/mono/msbuild.git",
-        revision: "ad9c9926a76e3db0d2b878a24d44446d73640d19"
+        revision: "db750f72af92181ec860b5150b40140583972c22"
+
+    # Remove in next release
+    # https://github.com/dotnet/msbuild/issues/6041
+    # https://github.com/dotnet/msbuild/pull/5381
+    patch do
+      url "https://github.com/mono/msbuild/commit/e2e4dfee543269ccb0a459263985b1c993feacec.patch?full_index=1"
+      sha256 "b64e93fbe1f5a5b8bcdb46ddd7d51a714f0e671b1b8ce2d1c2a0b80710ecb293"
+    end
+    patch do
+      url "https://github.com/mono/msbuild/commit/509c3190cf77be9422bddfad30b89e158f6229c3.patch?full_index=1"
+      sha256 "cf5fc342319cc1cb3b7bff02ec7d7d69af07f2777cf5b1910274d757cb14d92a"
+    end
+    patch do
+      url "https://github.com/mono/msbuild/commit/70bf6710473a2b6ffe363ea588f7b3ab87682a8d.patch?full_index=1"
+      sha256 "630b4187e882c162cd09e14f16ef2cca29b588dbea71bc444d925e5ef3f8f067"
+    end
+  end
+
+  # Temporary patch remove in the next mono release
+  patch do
+    url "https://github.com/mono/mono/commit/3070886a1c5e3e3026d1077e36e67bd5310e0faa.patch?full_index=1"
+    sha256 "b415d632ced09649f1a3c1b93ffce097f7c57dac843f16ec0c70dd93c9f64d52"
   end
 
   def install
@@ -67,16 +108,21 @@ class Mono < Formula
     resource("msbuild").stage do
       system "./eng/cibuild_bootstrapped_msbuild.sh", "--host_type", "mono",
              "--configuration", "Release", "--skip_tests"
-      system "./artifacts/mono-msbuild/msbuild", "mono/build/install.proj",
+
+      system "./stage1/mono-msbuild/msbuild", "mono/build/install.proj",
              "/p:MonoInstallPrefix=#{prefix}", "/p:Configuration=Release-MONO",
              "/p:IgnoreDiffFailure=true"
     end
 
     # Finally build and install fsharp as well
     resource("fsharp").stage do
-      ENV.prepend_path "PKG_CONFIG_PATH", lib/"pkgconfig"
-      system "make"
-      system "make", "install"
+      # Temporary fix for use propper .NET SDK remove in next release
+      inreplace "./global.json", "3.1.302", "3.1.405"
+      system "./build.sh", "-c", "Release"
+      ENV["version"]=""
+      system "./.dotnet/dotnet", "restore", "setup/Swix/Microsoft.FSharp.SDK/Microsoft.FSharp.SDK.csproj",
+        "--packages", "fsharp-nugets"
+      system "bash", "#{buildpath}/packaging/MacSDK/fsharp-layout.sh", ".", prefix
     end
   end
 
@@ -117,7 +163,7 @@ class Mono < Formula
         <Import Project="$(MSBuildBinPath)\\Microsoft.CSharp.targets" />
       </Project>
     EOS
-    system bin/"xbuild", "test.csproj"
+    system bin/"msbuild", "test.csproj"
 
     # Test that fsharpi is working
     ENV.prepend_path "PATH", bin
@@ -137,6 +183,7 @@ class Mono < Formula
           <ProjectGuid>{B6AB4EF3-8F60-41A1-AB0C-851A6DEB169E}</ProjectGuid>
           <OutputType>Exe</OutputType>
           <FSharpTargetsPath>$(MSBuildExtensionsPath32)\\Microsoft\\VisualStudio\\v$(VisualStudioVersion)\\FSharp\\Microsoft.FSharp.Targets</FSharpTargetsPath>
+          <TargetFrameworkVersion>v4.7.2</TargetFrameworkVersion>
         </PropertyGroup>
         <Import Project="$(FSharpTargetsPath)" Condition="Exists('$(FSharpTargetsPath)')" />
         <ItemGroup>
@@ -153,6 +200,6 @@ class Mono < Formula
       [<EntryPoint>]
       let main _ = printfn "#{test_str}"; 0
     EOS
-    system bin/"xbuild", "test.fsproj"
+    system bin/"msbuild", "test.fsproj"
   end
 end
