c0a4202e943316cc8cd2d5538b3a02efcde1a1c5
Michka Popoff <michkapopoff@gmail.com>
2021-01-16 12:21:02 +0100

    tcl-tk: align with linux

--- Formula/tcl-tk.rb
+++ Formula/tcl-tk.rb
@@ -22,9 +22,13 @@ class TclTk < Formula
 
   depends_on "openssl@1.1"
 
+  uses_from_macos "zlib"
+
   on_linux do
     depends_on "freetype" => :build
     depends_on "pkg-config" => :build
+    depends_on "libx11"
+    depends_on "libxext"
   end
 
   resource "critcl" do
@@ -74,8 +78,10 @@ class TclTk < Formula
 
     resource("tk").stage do
       cd "unix" do
-        system "./configure", *args, "--enable-aqua=yes",
-                              "--without-x", "--with-tcl=#{lib}"
+        on_macos do
+          args << "--enable-aqua=yes"
+        end
+        system "./configure", *args, "--without-x", "--with-tcl=#{lib}"
         system "make"
         system "make", "install"
         system "make", "install-private-headers"
@@ -90,10 +96,14 @@ class TclTk < Formula
     resource("tcllib").stage do
       system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
       system "make", "install"
-      ENV["SDKROOT"] = MacOS.sdk_path
+      on_macos do
+        ENV["SDKROOT"] = MacOS.sdk_path
+      end
       system "make", "critcl"
       cp_r "modules/tcllibc", "#{lib}/"
-      ln_s "#{lib}/tcllibc/macosx-x86_64-clang", "#{lib}/tcllibc/macosx-x86_64"
+      on_macos do
+        ln_s "#{lib}/tcllibc/macosx-x86_64-clang", "#{lib}/tcllibc/macosx-x86_64"
+      end
     end
 
     resource("tcltls").stage do
@@ -117,10 +127,18 @@ class TclTk < Formula
       system "make"
       system "make", "install"
     end
+
+    # Conflicts with perl
+    mv man/"man3/Thread.3", man/"man3/ThreadTclTk.3"
   end
 
   test do
     assert_equal "honk", pipe_output("#{bin}/tclsh", "puts honk\n").chomp
+
+    on_linux do
+      # Fails with: no display name and no $DISPLAY environment variable
+      return if ENV["CI"]
+    end
 
     test_itk = <<~EOS
       # Check that Itcl and Itk load, and that we can define, instantiate,
