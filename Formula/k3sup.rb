b3cf22cce3aaf2f7cf1ac641ac7fab67017bf441
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-16 11:46:13 +0000

    k3sup: update 0.9.13 bottle.

--- Formula/k3sup.rb
+++ Formula/k3sup.rb
@@ -15,11 +15,11 @@ class K3sup < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "363e864b29ea1593d2f4346508d9d9ceb3f9635e1991259711a4b16a53637ecf" => :big_sur
-    sha256 "54fd7175e82cbabba94660f3b99c7fe29867e9d008b115ff2b06b7bce30300a1" => :arm64_big_sur
-    sha256 "fd21ede843c6e875c6849755dba5e67719a829e326c7dee1056df4b901a9faff" => :catalina
-    sha256 "9a4d44bee7592c5ca2bcae669bffd81bd58d53940fe9076900b071afa5f4ceee" => :mojave
+    rebuild 2
+    sha256 "1a58b4c61dd608e30e4ae2010d0cbcc5cc1208fe4035e4b152759b3e9b5c1424" => :big_sur
+    sha256 "857e5232a524b1458ef42038817b8a93713ea8e92e4d60441241990fc5bd5d22" => :arm64_big_sur
+    sha256 "4303e006d4fafd8622976c09af83003e59512e062fb57cd4622bf4d99f0da691" => :catalina
+    sha256 "4790e0a97346b18606cdaa1f1f4fcccc6c1dfecccf4b7ba40abefd4960bb096d" => :mojave
   end
 
   depends_on "go" => :build
