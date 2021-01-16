ed2cb1174e59acaa2b2ed7b99d4abc61a07c61ea
rui <rui@chenrui.dev>
2021-01-16 15:47:16 +0000

    nativefier: update 42.1.0 bottle.

--- Formula/nativefier.rb
+++ Formula/nativefier.rb
@@ -13,10 +13,10 @@ class Nativefier < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7d421d5483813150206e73225702ff5beba8b6d28a347eec9a149f064502cd7a" => :big_sur
-    sha256 "15a2b779a84d1c20bd792dd037a4f03a79eddc1b170f0898b0186ad7dd342aad" => :arm64_big_sur
-    sha256 "579864e4179cdc08b88bb59802e1cabb6fa8decd5f7c39d6fa5d6127c3934a9c" => :catalina
-    sha256 "9f3ff10f644eb28f2beaab8bc08ee6e0ebcd6aae813c4b52aa4cdef26d665cde" => :mojave
+    sha256 "84f6c924bf750f8636abaf5f996d64328c9f26f228115d9749f56d64ae6c2f55" => :big_sur
+    sha256 "373bd45630fe672e9511892e515db341abdc36def22a4b91bdd55cc385779366" => :arm64_big_sur
+    sha256 "59d17a28db9fd8f7236f7af5214708fe103beb6fa6250bca9003ead60700fa86" => :catalina
+    sha256 "500121f8263dcb70123f11c864e7de59a52753caaf1a03d6fbfd52e571a0ce6c" => :mojave
   end
 
   depends_on "node"
