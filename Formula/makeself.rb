c16216c275398cc56b574e70adc736e91c9c01c4
Rui Chen <rui@chenrui.dev>
2021-01-16 13:35:46 -0500

    makeself 2.4.3
    * makeself 2.4.3
    * makeself: update license
    
    Closes #69200.
    
    Co-authored-by: rui <rui@meetup.com>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/makeself.rb
+++ Formula/makeself.rb
@@ -1,9 +1,9 @@
 class Makeself < Formula
   desc "Generates a self-extracting compressed tar archive"
   homepage "http://www.megastep.org/makeself/"
-  url "https://github.com/megastep/makeself/archive/release-2.4.2.tar.gz"
-  sha256 "8789312db5e93fc471a1e4fb88ec13227ef63a9c7aad297f3bbf35f9511f8d62"
-  license "GPL-2.0"
+  url "https://github.com/megastep/makeself/archive/release-2.4.3.tar.gz"
+  sha256 "b4c81c3d0a7c81459459319e9fc75320e057ea908430aa8a020be1993d6b74c8"
+  license "GPL-2.0-or-later"
   head "https://github.com/megastep/makeself.git"
 
   bottle :unneeded
