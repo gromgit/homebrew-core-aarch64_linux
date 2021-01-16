c209e239268bb5f93bfc7d679f921d2863c6f6ee
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-16 17:52:52 +0000

    httpd: revision bump for apr
    Closes #68915.

--- Formula/httpd.rb
+++ Formula/httpd.rb
@@ -5,7 +5,7 @@ class Httpd < Formula
   mirror "https://archive.apache.org/dist/httpd/httpd-2.4.46.tar.bz2"
   sha256 "740eddf6e1c641992b22359cabc66e6325868c3c5e2e3f98faf349b61ecf41ea"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   livecheck do
     url :stable
