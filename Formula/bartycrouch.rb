733ca6dd6f8826de4db4a2e41c54296286cce21a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-16 16:56:05 +0000

    bartycrouch: update 4.4.1 bottle.

--- Formula/bartycrouch.rb
+++ Formula/bartycrouch.rb
@@ -9,9 +9,9 @@ class Bartycrouch < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "816cdbda1f0ebbb8335e1741226390af48180b9f8edc0e99d24b508f8382941f" => :big_sur
-    sha256 "e90904157ef22734bf159fda04570eb3ef44f2b7be85e33bc149660969f46054" => :arm64_big_sur
-    sha256 "e7260d3270b169b9f0ee875fcae3dc702ac86d2739698c953f2714d3ef50b96e" => :catalina
+    sha256 "635a9fff91e57290f013826f3b102fd1e639ed4d8dd9f2a5fc84ee3ee86b3383" => :big_sur
+    sha256 "290d66a6a7164de7c459553fcaaf6ffdc2c8e48673d4536626aac4a644a18107" => :arm64_big_sur
+    sha256 "a403e05eef2353f041f499275a0ebcea1fc9381bea71b9205227a319ed5547c9" => :catalina
   end
 
   depends_on xcode: ["12.0", :build]
