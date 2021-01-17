2ad6364a465daf004d273606af326f1ee29e7221
Dawid Dziurla <dawidd0811@gmail.com>
2021-01-17 18:53:10 +0000

    kustomize 3.9.2
    Closes #69252.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kustomize.rb
+++ Formula/kustomize.rb
@@ -2,8 +2,8 @@ class Kustomize < Formula
   desc "Template-free customization of Kubernetes YAML manifests"
   homepage "https://github.com/kubernetes-sigs/kustomize"
   url "https://github.com/kubernetes-sigs/kustomize.git",
-      tag:      "kustomize/v3.9.1",
-      revision: "7439f1809e5ccd4677ed52be7f98f2ad75122a93"
+      tag:      "kustomize/v3.9.2",
+      revision: "e98eada7365fc564c9aba392e954f306a9cbf1dd"
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/kustomize.git"
 
