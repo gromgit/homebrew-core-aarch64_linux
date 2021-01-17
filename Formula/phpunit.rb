8f1db0112a088dac46576fe712257882c553de92
bepsvpt <8221099+bepsvpt@users.noreply.github.com>
2021-01-17 16:32:45 +0800

    phpunit 9.5.1 (#69233)
    Co-authored-by: homebrew-updater <bepsvpt/homebrew-updater>

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -1,8 +1,8 @@
 class Phpunit < Formula
   desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-9.5.0.phar"
-  sha256 "aa56801ece16304dbd403fe7f82cd21e91ba01f45be6cdb3f4b3135489dcedfc"
+  url "https://phar.phpunit.de/phpunit-9.5.1.phar"
+  sha256 "062f128573e120c8894086b50b60a89971833834e040cfb9358eaa22224e68ac"
   license "BSD-3-Clause"
 
   livecheck do
