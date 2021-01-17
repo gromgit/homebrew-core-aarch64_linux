c113c98ff2588d48993efabb7c604fa265af9efb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-17 08:44:47 +0000

    kustomize: update 3.9.1 bottle.

--- Formula/kustomize.rb
+++ Formula/kustomize.rb
@@ -14,10 +14,11 @@ class Kustomize < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ca2e4574188847e6eb57e6ac80f47db7536c6299e028b725c97b217c7fac8e62" => :big_sur
-    sha256 "9e4849f2333cecbf758944fb92a39ea32dae6f436700a00a8678677c15601997" => :arm64_big_sur
-    sha256 "c02742de0ac86c7e945a30003c1b2a845798e43b8ab678b0e342985b55b4ba1d" => :catalina
-    sha256 "b3282632d1e656fa66aeeb8de4a4c3c08d5dd994549324fd09203efc57d5ffe4" => :mojave
+    rebuild 1
+    sha256 "b4c961f78d27562655fac7e3f1c611e77692c7c68d56f7da0bdc59b4605c7100" => :big_sur
+    sha256 "48ef55519e8c3d1cb2175e3905b3b2e4045a7b9955b70342b208d48690c5588e" => :arm64_big_sur
+    sha256 "445f6f2aa0abe3b760d9fab744ff7642c8f2fd5492dd41f01f6945e647f9afb1" => :catalina
+    sha256 "4722f5144260b06f94a2886eb14fdcb9114e744a78f5fe21be60b45623b75c9b" => :mojave
   end
 
   depends_on "go" => :build
