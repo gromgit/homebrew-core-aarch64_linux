4c93283f44006925ec38daaa710980c38e3fd576
FX Coudert <fxcoudert@gmail.com>
2021-01-17 22:27:47 +0000

    pam-u2f: update 1.1.0_1 bottle.

--- Formula/pam-u2f.rb
+++ Formula/pam-u2f.rb
@@ -15,6 +15,7 @@ class PamU2f < Formula
   bottle do
     cellar :any
     sha256 "f839f2092a454245e7d803c2173963be8eb084574707205277121d9047ec870b" => :big_sur
+    sha256 "506a178827a1b2b381815ba1a8ae693b0fcb68a6eb452044d2bd4adc1690154d" => :arm64_big_sur
     sha256 "cdc5fa2db8788501c8fe8c9142bc0686d5ad2b1e7c3cfb4dc35d95788cb58485" => :catalina
     sha256 "30cc76b0b4d582c076c9fed1ed880442b67b987973b57fd52e26a93356e5eef6" => :mojave
     sha256 "bfd1309cb6deff47c4473b0af86f645b0ee29eca712ebce67d031b770f742a24" => :high_sierra
