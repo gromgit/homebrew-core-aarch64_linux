81f012c96110c856c210206a144e1b52e22f9c8f
rui <rui@chenrui.dev>
2021-01-18 16:47:02 +0000

    ngt: update 1.13.1 bottle.

--- Formula/ngt.rb
+++ Formula/ngt.rb
@@ -11,10 +11,10 @@ class Ngt < Formula
   end
 
   bottle do
-    cellar :any
-    sha256 "eeaeaf23e4b3c9680af6b509936ac8df44299cff92bed77fbc37967cae0f198d" => :big_sur
-    sha256 "9200a72e8f12c6019402d11a269ef8e46784c396b6a3f8bd8e7e4bb8a30d9a69" => :catalina
-    sha256 "5c83844458ab7b28e4a3e384e1559ccf0a192a5be6a8994cc867189399858703" => :mojave
+    sha256 "a5d33b8d1f31687d6f8c241472e6af6464568a3fa407fb8191efacab00fee33e" => :big_sur
+    sha256 "726916f6ef145e1a541a84526842d0a4cdc922ebd990b102a69346aea2d232a5" => :arm64_big_sur
+    sha256 "85514f57252ce423ba7499b56ed88783aaed73ecc9c68f5a76337ae73de91ac5" => :catalina
+    sha256 "bbf7d4d86273b6f28c05e8e1f736631b7614d67171d3127b8b746d0b730c94e1" => :mojave
   end
 
   depends_on "cmake" => :build
