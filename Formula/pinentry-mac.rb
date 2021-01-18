121ea9b3b563c78a2289d8e865667d802b3e3d73
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-18 11:49:14 +0000

    pinentry-mac: update 1.1.0.3 bottle.

--- Formula/pinentry-mac.rb
+++ Formula/pinentry-mac.rb
@@ -7,16 +7,11 @@ class PinentryMac < Formula
   head "https://github.com/GPGTools/pinentry.git", branch: "dev"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "123dd2e07b5199d9096a4834257e5c000d270e72ecad185b9188578627aa413e" => :big_sur
-    sha256 "203acdf241ddeb27258929c6902a8bd2f5f46cb0c4b82d3075cc88ec9a8bd0a1" => :arm64_big_sur
-    sha256 "936dba5c3bdd8279e5380012645a349a6ef8c69d1cc9066f28f9c6fa6642fd64" => :catalina
-    sha256 "6b2e8f260b2c9d2d7cd3f39516d7794656492cf7726a92a847aa86ba02d83179" => :mojave
-    sha256 "aa7a00ec594541e43a74cf7ae16cb05317d5b73c7b6b6647ca349584280eaad7" => :high_sierra
-    sha256 "3da6a88abbd4bb04eb2455d8e6998f4fc4db77c3f765d52b7eadf364e82aeaa7" => :sierra
-    sha256 "c3d508c96256c50b6a62f9e64fc4cb28810a910927c21f7defbe8af11a3c5961" => :el_capitan
-    sha256 "b96a51a263a9447101d4bb8dc4247f324531bd4fd96218f6e170edfc983a87f7" => :yosemite
-    sha256 "c2538b57edce2eb7ccc10a32e16ccfbbbe8e61c384c4db8d5a62b04d3815c0ed" => :mavericks
+    cellar :any
+    sha256 "bf4fab5722e014d64397d0cc9078a95779f7b2553f858af4444ca913c3ce979f" => :big_sur
+    sha256 "b33a7f22470f2fc0f81fa45259c0d338196d0a2c1a4dff3ee7e38cc002c16744" => :arm64_big_sur
+    sha256 "149e9ddc31176346b936fbec386c49d3fb322132d65d415854033203ba1db467" => :catalina
+    sha256 "90c757fe0590e14c8e2d13a6e11629eb1f6893696b720b849a19db09b3c932dd" => :mojave
   end
 
   depends_on "autoconf" => :build
