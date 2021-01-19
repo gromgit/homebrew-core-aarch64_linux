69e17a705b2640992c7cf5b7a22eb1b6566eb357
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-19 07:53:14 +0000

    mockery: update 2.6.0 bottle.

--- Formula/mockery.rb
+++ Formula/mockery.rb
@@ -8,10 +8,10 @@ class Mockery < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "4094897406ceffdbab71ea9e336e2afcbc33307534fe98e6ed09697a85b4399c" => :big_sur
-    sha256 "57baf3bd06647ad294f5ecbaf5d34f10cc962cd6638521433aa6a3e004445d24" => :arm64_big_sur
-    sha256 "df9ec002cd7bd632b81fcddbdcbc43da09646a26cb40f46a8dac45d606cef1da" => :catalina
-    sha256 "0dc65625a3f8c0504f8d40edbbb4a25b01c6883876947afd312b712104b5871c" => :mojave
+    sha256 "9042c369628ba6ed7f01ed774114ed5194f2a06db3c91f6bd3baa958b387a52d" => :big_sur
+    sha256 "73af9616980727010bb370b90f195acdf7fae8fe8aed870afa7059b37cbd6cf0" => :arm64_big_sur
+    sha256 "1e9b569d40889982fc89ed2e6182e40c8d7151e4f696ea1d8ef8c49a7be9a71f" => :catalina
+    sha256 "21c9319ca3d75ca69883c1df8cbdb504ddc307577f6e5aebd10ac2970655e4fe" => :mojave
   end
 
   depends_on "go"
