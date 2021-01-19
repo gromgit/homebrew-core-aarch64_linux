58794bae24a9cf14c1990ee028152ccd5a25152b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-19 21:34:57 +0000

    dnsmasq: update 2.83 bottle.

--- Formula/dnsmasq.rb
+++ Formula/dnsmasq.rb
@@ -11,11 +11,10 @@ class Dnsmasq < Formula
   end
 
   bottle do
-    sha256 "d452e2a6562538d398ffa39c7a1761d222d613b2cbcadf5796a7dc2a25697d55" => :big_sur
-    sha256 "fa45a739d36bf76d47b35d72ab36d8282abab1c7d5c920350f4532fa2fa970a3" => :arm64_big_sur
-    sha256 "0cab9dbb88d09946faed36b09a1f0919141b3e56eea45e5f77969619e00796e0" => :catalina
-    sha256 "c70c3fe42cdabac2497377e6718678eb5c433658ce1ea92e4193d6877d81c628" => :mojave
-    sha256 "77e52871d7bd6af6f4f1e7b1d55bae2fa235c1d6cafc14b290758b741bc19f95" => :high_sierra
+    sha256 "8c76a679924b5f2a31d9d90236d6c35f4b05074f447656f3e0e814d3e823e74a" => :big_sur
+    sha256 "5eaac9c49cc46b01e2c969edb078414945d75827e930002475742ee22949e00e" => :arm64_big_sur
+    sha256 "cd4bccd3c41ef17b4a71c9dec0e8ceaed15e5e4dd4c9ca23cc681a1b0d391566" => :catalina
+    sha256 "f122bdd0e288193e65cf366c560570e9de1aee2d68ed04d1c94b49e9e519a3e5" => :mojave
   end
 
   depends_on "pkg-config" => :build
