b41efd3ee12ce396734db0acabb02ae2f8ea0817
rui <rui@chenrui.dev>
2021-01-19 00:16:20 +0000

    angle-grinder: update 0.16 bottle.

--- Formula/angle-grinder.rb
+++ Formula/angle-grinder.rb
@@ -7,10 +7,10 @@ class AngleGrinder < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5a1256b4d29478fbaf24b5683d1a97f78c5acce41292f5e49c164545c197388b" => :big_sur
-    sha256 "4ff200cdd8142e3d16a89a3050c690be506023da7ea6c6ca59700a209aa647dd" => :arm64_big_sur
-    sha256 "15943cf324e460ab66b499fe6399c8da05b305289ef42e5dfc385ca2e3c7177d" => :catalina
-    sha256 "4b3d867ea87b7c6a478aa7e6e5ea5909e01946c59473d4467254298c2ec524e8" => :mojave
+    sha256 "25e2cec189b99c508be9de6ef7076028e49dfb8c806bb2c0637071c4c4479f6a" => :big_sur
+    sha256 "15d4fb499d5ba409e212603382bfa5277d24d84ab53561fb78bbb14ac1e6bfaa" => :arm64_big_sur
+    sha256 "567fb4aea30314ed1ff6b5bfe07b84ccdebaadf5de7e150c849fd2bd59fe020b" => :catalina
+    sha256 "003d5093badf3df2e29df03a0098d1ad346f788979e03568638c5c7067168112" => :mojave
   end
 
   depends_on "rust" => :build
