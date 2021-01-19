676c5f2b4e558792af3429d2d575ab0fcc4338b8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-19 03:15:38 +0000

    mysql-connector-c++: update 8.0.23 bottle.

--- Formula/mysql-connector-c++.rb
+++ Formula/mysql-connector-c++.rb
@@ -11,11 +11,10 @@ class MysqlConnectorCxx < Formula
 
   bottle do
     cellar :any
-    sha256 "90e116fbf60b03abb2dc84296879e60485d40e6296966214406a8ce5a1702990" => :big_sur
-    sha256 "306284af196c840d9c523d22a36e9fe29037ace2c60daa0a34a71e0354c281bb" => :arm64_big_sur
-    sha256 "a7ce662aff4a29d35a46486d661bbf1b0149079455a760ea829810ca92423b02" => :catalina
-    sha256 "5ae4ddc27990c9ef8019a4668a150af3671ce41a066d7917eb026b224bba54fb" => :mojave
-    sha256 "cbb2bfbf652d2d3d55e67de4b3b2af131dc76b520e9eddb3ce4b26c722b1c9ac" => :high_sierra
+    sha256 "7814f13c4b6bd77627e4eea7a82aebe2b97ebe83744dbdf5541509848f89a727" => :big_sur
+    sha256 "9526dc9dfbcb3e82de1a20cac8f1f1c33637b979c34c1fb7e9f825a8a11b41e7" => :arm64_big_sur
+    sha256 "2e6246b9c2c7adf2c700d88ed5e0d07406c78c5479a064aa87f59a85f9bd8b3d" => :catalina
+    sha256 "083e6ef2e4af3c9d45b0e2b2b2fa89beb63add05e1ef8d30ebc6a39d24fdd9e0" => :mojave
   end
 
   depends_on "boost" => :build
