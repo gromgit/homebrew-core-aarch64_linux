257b4360413b4656f62b5ce8309c5ae9da5db19f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-20 10:56:48 +0000

    infracost: update 0.7.14 bottle.

--- Formula/infracost.rb
+++ Formula/infracost.rb
@@ -8,10 +8,10 @@ class Infracost < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0ef7962b02c540fd39cd6d9a703410e1884a06341294e797cc1015539ca26f80" => :big_sur
-    sha256 "feefbc643626faa12de2177731febd010ff7bc7684c3f9f59798b5e19b857f6b" => :arm64_big_sur
-    sha256 "d7b91de058dce9e91ef89aab89b9eaff229b21296655bbaaabb1b0832808410b" => :catalina
-    sha256 "ff2d87ad5450309eb2f8333058a56984afb56382866eeeb4e1bb11b963f20d5a" => :mojave
+    sha256 "4f2339f284ed258c845664e154cddf3b0560f7941f5c34eaf9b3e250980a5668" => :big_sur
+    sha256 "066b7dca83bc8a899614306862d0b59480f737b22feaa1a2f6ec625341134dd1" => :arm64_big_sur
+    sha256 "0ffddbf0d367c4f51ec869ad67a95ee19c63a267be6a2aaed43e3d18072106b7" => :catalina
+    sha256 "fd9e368d9b0487731d8d4028284fb01acac68a440ce38aed20579fc96eb7310f" => :mojave
   end
 
   depends_on "go" => :build
