43869e39e18054f8be3d46cb8a358abd2a9e7062
Rui Chen <rui@chenrui.dev>
2021-01-19 20:05:38 -0500

    prometheus 2.24.1
    Closes #69360.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/prometheus.rb
+++ Formula/prometheus.rb
@@ -1,8 +1,8 @@
 class Prometheus < Formula
   desc "Service monitoring system and time series database"
   homepage "https://prometheus.io/"
-  url "https://github.com/prometheus/prometheus/archive/v2.24.0.tar.gz"
-  sha256 "aaf32298f7342bf01e2215c376575d22d3d35bcbb91645766eb42540b1ba3385"
+  url "https://github.com/prometheus/prometheus/archive/v2.24.1.tar.gz"
+  sha256 "9e08bacde869c6c4b68a9e34c7074b812be1391b33d033d3072a5e1ad2debd87"
   license "Apache-2.0"
 
   livecheck do
