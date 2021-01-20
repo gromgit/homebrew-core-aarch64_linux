af01c83dde3ec3259ed39ff8dd38168ab675f88d
Seeker <meaningseeking@protonmail.com>
2021-01-19 16:20:12 -0800

    dovecot 2.3.13
    * dovecot 2.3.13
    * dovecot: update pigeonhole to 0.5.13
    
    Closes #69339.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dovecot.rb
+++ Formula/dovecot.rb
@@ -1,8 +1,8 @@
 class Dovecot < Formula
   desc "IMAP/POP3 server"
   homepage "https://dovecot.org/"
-  url "https://dovecot.org/releases/2.3/dovecot-2.3.10.1.tar.gz"
-  sha256 "6642e62f23b1b23cfac235007ca6e21cb67460cca834689fad450724456eb10c"
+  url "https://dovecot.org/releases/2.3/dovecot-2.3.13.tar.gz"
+  sha256 "a3f875b80ec11a452480690108660030978c94fa8e796ad6d943a874b496f1c4"
 
   livecheck do
     url "https://dovecot.org/download"
@@ -22,8 +22,8 @@ class Dovecot < Formula
   uses_from_macos "sqlite"
 
   resource "pigeonhole" do
-    url "https://pigeonhole.dovecot.org/releases/2.3/dovecot-2.3-pigeonhole-0.5.9.tar.gz"
-    sha256 "36da68aae5157b83e21383f711b8977e5b6f5477f369f71e7e22e76a738bbd05"
+    url "https://pigeonhole.dovecot.org/releases/2.3/dovecot-2.3-pigeonhole-0.5.13.tar.gz"
+    sha256 "911fe566da5b638eab1b11105314300bc9049cc3832d4bd2aed44c265013bf17"
   end
 
   def install
