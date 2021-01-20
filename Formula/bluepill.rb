561896089ce5b4a71529497e28d4cecdfe3a2d6a
Shawn Chen <swchen@linkedin.com>
2021-01-19 16:47:36 -0800

    bluepill 5.6.0
    Closes #69359.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/bluepill.rb
+++ Formula/bluepill.rb
@@ -2,8 +2,8 @@ class Bluepill < Formula
   desc "Testing tool for iOS that runs UI tests using multiple simulators"
   homepage "https://github.com/linkedin/bluepill"
   url "https://github.com/linkedin/bluepill.git",
-      tag:      "v5.4.2",
-      revision: "506e39951ea835b9240b15bd6a6bf593cf6534f9"
+      tag:      "v5.6.0",
+      revision: "ca19302bfbc48e1cf0ccf28eaad431abc6f78582"
   license "BSD-2-Clause"
   head "https://github.com/linkedin/bluepill.git"
 
