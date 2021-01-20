dcb2b0b00c19b2f650cb15bf76d22210ae9719c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-20 23:45:31 +0000

    fossil: update 2.14 bottle.

--- Formula/fossil.rb
+++ Formula/fossil.rb
@@ -13,11 +13,10 @@ class Fossil < Formula
 
   bottle do
     cellar :any
-    sha256 "b457dfa340e0880a1f782372556d892b3bd76b71515abb8535745270781f1cb7" => :big_sur
-    sha256 "0a9877c27188015e87c4c2585256fe95741917969936cbe0b85182badcf229fa" => :arm64_big_sur
-    sha256 "660528f6b90d429f05434f5c8d1c7ac8a6c5df0bf6ea94f3bcdb6095365df7da" => :catalina
-    sha256 "d756776b71f3fb2d0ddf99defd245142d2bac68450444133fd969433d2808e12" => :mojave
-    sha256 "9e8925d6033240a79da02222aa272de41daea0e45cf67b54117f1f0cb42ab6be" => :high_sierra
+    sha256 "47b62a05daf6fc12735cc10ac3d661a058df73c918c0d425c8a06b5d33ceae40" => :big_sur
+    sha256 "d0ac7394862b01c94118282bc7fe014ecc004818d0bdb5d7ad62ead3a4f8c789" => :arm64_big_sur
+    sha256 "514265081557287157fad22327aed27e937229caa445a5ea363c7edee0ada162" => :catalina
+    sha256 "a2c6c8c85d54a253ac499ea88402b397562db29e4f1d21d6103728921ff50335" => :mojave
   end
 
   depends_on "openssl@1.1"
