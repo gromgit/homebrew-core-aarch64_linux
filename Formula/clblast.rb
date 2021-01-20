7c654e3ea68c05e3c729e11204c362a181243b8b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-20 18:31:09 +0000

    clblast: update 1.5.2 bottle.

--- Formula/clblast.rb
+++ Formula/clblast.rb
@@ -7,11 +7,10 @@ class Clblast < Formula
 
   bottle do
     cellar :any
-    sha256 "011236dce7c79bbe239feb3d23db5614ced6ff55de8c2e284de2ea067e9e76ae" => :big_sur
-    sha256 "d0c66aca34c66fe7c62a485ca1eeaf24f5f36d840c62669f6bbf904750b7e10a" => :arm64_big_sur
-    sha256 "e1908450ec5ca862fc49fd80a2c33565729187372749e395a88baa49371465e5" => :catalina
-    sha256 "b6b81483ab6621bb1a9002e771c50d8fa90364b39aabe293dc276991ab5e60b5" => :mojave
-    sha256 "d4fc7ec77ca33d0d8182e345000044a925128ffdbc39f7e0802c8500475007c8" => :high_sierra
+    sha256 "7dc30133bf7d780a3377672a6bb4d224f57c138b1aa063bda8a6a0cf88276210" => :big_sur
+    sha256 "e6c6c8e7221e6311ffa712d1bd2331fe3fa727f0b7e51759b2723ae53eeada4d" => :arm64_big_sur
+    sha256 "e7470605bb8000f9fe5ed84eea0f839127fded8ce3a0949b0a93bd5ce18e6f49" => :catalina
+    sha256 "2aef3e5730b1109de45d3748d0e17d78b2181c8eff7b417200c8b2e187681385" => :mojave
   end
 
   depends_on "cmake" => :build
