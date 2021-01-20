d5055afd3151c2df340785fe5adc5fd83b81400e
Rui Chen <rui@chenrui.dev>
2021-01-19 20:10:59 -0500

    webpack 5.16.0
    * webpack 5.16.0
    * webpack: bump cli
    
    Closes #69366.
    
    Co-authored-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/webpack.rb
+++ Formula/webpack.rb
@@ -4,8 +4,8 @@ require "json"
 class Webpack < Formula
   desc "Bundler for JavaScript and friends"
   homepage "https://webpack.js.org/"
-  url "https://registry.npmjs.org/webpack/-/webpack-5.15.0.tgz"
-  sha256 "9be7d224c654faf9f85954b01559554a602d5b1a4d48badf8a52391c1829f075"
+  url "https://registry.npmjs.org/webpack/-/webpack-5.16.0.tgz"
+  sha256 "e27904f89e5faf214489bba9f0ec5d84535444ed59bc2f24a3692529741f0b05"
   license "MIT"
   head "https://github.com/webpack/webpack.git"
 
@@ -24,8 +24,8 @@ class Webpack < Formula
   depends_on "node"
 
   resource "webpack-cli" do
-    url "https://registry.npmjs.org/webpack-cli/-/webpack-cli-4.3.1.tgz"
-    sha256 "9d12d225e965d5c7d7d7145b215d297b93f47dc621f61a3185e0c69f09a64d43"
+    url "https://registry.npmjs.org/webpack-cli/-/webpack-cli-4.4.0.tgz"
+    sha256 "11fcc682d0a7ec669415518b80c874be348d865997bddf8a2b8cd801918157ae"
   end
 
   def install
