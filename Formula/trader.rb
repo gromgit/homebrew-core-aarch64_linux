e6a0c74295a4b68a46ea3c5a5ca2270ced86a576
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-21 06:34:17 +0000

    trader: update 7.16 bottle.

--- Formula/trader.rb
+++ Formula/trader.rb
@@ -5,11 +5,10 @@ class Trader < Formula
   sha256 "7fced3adfe61099679cec67dd7d7187314b0f6d4378a4aece1a1b5eab9f81ef2"
 
   bottle do
-    sha256 "d47f18eea0c0f210c48fa15f4ff038869ee2efec31fb6dacc0d8df1abaf1183b" => :big_sur
-    sha256 "5e007ac0ba7b69e1bdc2c4287a6a716273f1645b2e19bbd06ab78c9d190444b7" => :arm64_big_sur
-    sha256 "661f7dc5534bda2b721f00c12b868343a762d4f4b2cd8f6c869c2681b7a9c091" => :catalina
-    sha256 "1e78bde8014188f21cc16fdf99e52128e3e135031d447c71f1c39fa8d1135b70" => :mojave
-    sha256 "8175870d189ac87e617dda58fd61d4cd56109876bc5bf7fe55718ac9db137a27" => :high_sierra
+    sha256 "cacf85ddfc82657267a51c2a6159b0b5f4f5f26029bc32a5cdf895e433578158" => :big_sur
+    sha256 "04f065f3b4f8cc435bc4cd005efac64d3c106538b5f3261a70dba73ffb34d57b" => :arm64_big_sur
+    sha256 "f88445380e3d80b13e99a9c8c657609941a3f696be2e5039521b1d0a847f03c6" => :catalina
+    sha256 "55412b011ce76032c044f0a5839ee05f8b26ecd45471f79b1862a91c2fdc0011" => :mojave
   end
 
   depends_on "pkg-config" => :build
