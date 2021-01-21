79502c5688630815ac81021efbe5ed793b11a7b7
Caleb Xu <calebcenter@live.com>
2021-01-20 22:16:07 -0500

    bfg 1.13.2 (#69440)

--- Formula/bfg.rb
+++ Formula/bfg.rb
@@ -1,8 +1,8 @@
 class Bfg < Formula
   desc "Remove large files or passwords from Git history like git-filter-branch"
   homepage "https://rtyley.github.io/bfg-repo-cleaner/"
-  url "https://search.maven.org/remotecontent?filepath=com/madgag/bfg/1.13.1/bfg-1.13.1.jar"
-  sha256 "e5b3d90e45cafb6687a7c8f56b01aeb437705e52cb4cc6bfe010f2a728cd2c26"
+  url "https://search.maven.org/remotecontent?filepath=com/madgag/bfg/1.13.2/bfg-1.13.2.jar"
+  sha256 "63671bdd52238c17d28af2db632253def925181832e26d2182be2dd103362fa3"
   license "GPL-3.0-or-later"
 
   livecheck do
