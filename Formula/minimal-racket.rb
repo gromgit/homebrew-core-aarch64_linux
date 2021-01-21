afddaa491aea0857f8e7032e8dbf0369f2c47228
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-21 11:33:59 +0000

    minimal-racket: remove `cellar :any` (#69471)
    The bottle is not relocatable. See
    
        https://github.com/Homebrew/homebrew-core/pull/69459#issuecomment-764549044
    
    Thanks to @aisk for the report.

--- Formula/minimal-racket.rb
+++ Formula/minimal-racket.rb
@@ -12,7 +12,6 @@ class MinimalRacket < Formula
   end
 
   bottle do
-    cellar :any
     sha256 "e505c77a1703d75d214e081250cff9cbdbb13d604f8995703bd96f5a5454803d" => :big_sur
     sha256 "68ce8bdaed9890086696fe63ce655c994848e58da24040363441bdc6eaa0d9d6" => :catalina
     sha256 "4fd0070df83c2d0761bc64e31b479f776f9cee55fe51a770811748706742e528" => :mojave
