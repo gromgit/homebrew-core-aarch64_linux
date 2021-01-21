ce151a6a4f6108a6833e313beb92c8373571c746
rui <rui@chenrui.dev>
2021-01-21 08:44:36 +0000

    cgrep 6.6.33
    Closes #69467.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/cgrep.rb
+++ Formula/cgrep.rb
@@ -1,10 +1,9 @@
 class Cgrep < Formula
   desc "Context-aware grep for source code"
   homepage "https://github.com/awgn/cgrep"
-  url "https://github.com/awgn/cgrep/archive/v6.6.32.tar.gz"
-  sha256 "c45d680a2a00ef9524fc921e4c10fc7e68f02e57f4d6f1e640b7638a2f49c198"
+  url "https://github.com/awgn/cgrep/archive/v6.6.33.tar.gz"
+  sha256 "f0d7114e9c26dc3ff3515711cce63864f3995ac06ed3743acf2560fc5a1eb78e"
   license "GPL-2.0"
-  revision 1
   head "https://github.com/awgn/cgrep.git"
 
   bottle do
