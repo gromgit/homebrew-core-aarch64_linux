41b5c55fa604897dca6150829b8fae84079ebf7a
An Long <aisk@users.noreply.github.com>
2021-01-21 12:01:11 +0800

    minimal-racket: revision
    * minimal-racket: update revision
    * Update minimal-racket.rb
    * Update minimal-racket.rb
    
    Closes #69459.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/minimal-racket.rb
+++ Formula/minimal-racket.rb
@@ -2,8 +2,9 @@ class MinimalRacket < Formula
   desc "Modern programming language in the Lisp/Scheme family"
   homepage "https://racket-lang.org/"
   url "https://mirror.racket-lang.org/installers/7.9/racket-minimal-7.9-src-builtpkgs.tgz"
-  sha256 "293aa8ef709a6240472f16833351ba66a9e461261d2813c7fb1cc5ddf59c3000"
+  sha256 "85b201aebc1ad1ec98ac590e18052d7ef8a81af280244d00ca1c28e8543b3fe9"
   license any_of: ["MIT", "Apache-2.0"]
+  revision 1
 
   livecheck do
     url "https://download.racket-lang.org/all-versions.html"
@@ -68,6 +69,8 @@ class MinimalRacket < Formula
         #{version}
       catalogs:
         https://download.racket-lang.org/releases/#{version}/catalog/
+        https://pkgs.racket-lang.org
+        https://planet-compats.racket-lang.org
       default-scope:
         installation
     EOS
