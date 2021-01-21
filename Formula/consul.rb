398df95b4e654605714de7ac8f8d925d2c83afed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-21 04:51:19 +0000

    consul: update 1.9.2 bottle.

--- Formula/consul.rb
+++ Formula/consul.rb
@@ -14,9 +14,9 @@ class Consul < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e3252a0c80a1fe4e068e046dd9dfb999a676b888b33ea543ed9078bb0220776f" => :big_sur
-    sha256 "797e30eb70688f226343491cf8fb776a751f19e973cea9093984a2a814f54a52" => :catalina
-    sha256 "f18664062a69d912ec82f7541b1d89551fb8aed2d159406c2f30f109f163c61e" => :mojave
+    sha256 "9c7099cd353c37ea92c066a0a841c0b5eb26c2b992f8fbad872f002970bca3ac" => :big_sur
+    sha256 "6759ecab21796a2f9f13e0be800b9d56e48a2ad1dd8b64223e7c7885e2a19d3a" => :catalina
+    sha256 "c4d00c32bb71bf3fe0bbe1630daa3738e5fd76c6eba5d55584d021d0630bdbb6" => :mojave
   end
 
   depends_on "go" => :build
