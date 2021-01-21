284b4acd46a81868e03608dfda186d062da0ddc2
Kreeblah <kreeblah@gmail.com>
2021-01-21 03:57:58 -0800

    minipro: Bumped version to 0.5 and updated tests to match
    Closes #69472.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/minipro.rb
+++ Formula/minipro.rb
@@ -1,9 +1,9 @@
 class Minipro < Formula
   desc "Open controller for the MiniPRO TL866xx series of chip programmers"
   homepage "https://gitlab.com/DavidGriffith/minipro/"
-  url "https://gitlab.com/DavidGriffith/minipro/-/archive/0.4/minipro-0.4.tar.gz"
-  sha256 "05e0090eab33a236992f5864f3485924fb5dfad95d8f16916a17296999c094cc"
-  license "GPL-3.0"
+  url "https://gitlab.com/DavidGriffith/minipro/-/archive/0.5/minipro-0.5.tar.gz"
+  sha256 "80ce742675f93fd4e2a30ab31a7e4f3fcfed8d56aa7cf9b3938046268004dae7"
+  license "GPL-3.0-or-later"
 
   bottle do
     cellar :any
@@ -24,14 +24,15 @@ class Minipro < Formula
   end
 
   test do
-    output_minipro = shell_output("#{bin}/minipro 2>&1")
+    output_minipro = shell_output("#{bin}/minipro 2>&1", 1)
     assert_match "minipro version #{version}", output_minipro
-    output_miniprohex = shell_output("#{bin}/miniprohex 2>&1")
+    output_miniprohex = shell_output("#{bin}/miniprohex 2>&1", 1)
     assert_match "miniprohex by Al Williams", output_miniprohex
 
     output_minipro_read_nonexistent = shell_output("#{bin}/minipro -p \"ST21C325@DIP7\" -b 2>&1", 1)
     if output_minipro_read_nonexistent.exclude?("Device ST21C325@DIP7 not found!") &&
-       output_minipro_read_nonexistent.exclude?("Error opening device")
+       output_minipro_read_nonexistent.exclude?("Error opening device") &&
+       output_minipro_read_nonexistent.exclude?("No programmer found.")
       raise "Error validating minipro device database."
     end
   end
