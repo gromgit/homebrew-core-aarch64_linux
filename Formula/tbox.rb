4b74040d04a43a35158b81d8a2220241346e2263
ruki <waruqi@gmail.com>
2021-01-21 11:58:45 +0800

    tbox: 1.6.6
    Closes #69458.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tbox.rb
+++ Formula/tbox.rb
@@ -1,8 +1,8 @@
 class Tbox < Formula
   desc "Glib-like multi-platform C library"
   homepage "https://tboox.org/"
-  url "https://github.com/tboox/tbox/archive/v1.6.5.tar.gz"
-  sha256 "076599f8699a21934f633f1732977d0df9181891ca982fd23ba172047d2cf4ab"
+  url "https://github.com/tboox/tbox/archive/v1.6.6.tar.gz"
+  sha256 "13b8fa0b10c2c0ca256878a9c71ed2880980659dffaadd123c079c2126d64548"
   license "Apache-2.0"
   head "https://github.com/tboox/tbox.git"
 
