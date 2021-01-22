af7e59d6bda711dd49819b008a4c3f37d8bc2c44
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-22 12:46:33 +0000

    mpd: update 0.22.4 bottle.

--- Formula/mpd.rb
+++ Formula/mpd.rb
@@ -8,11 +8,10 @@ class Mpd < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "2b9a24b8bdb3da4fb71e4b5aa1f5d6fb9a7a1a847671b88504388a07b81212dc" => :big_sur
-    sha256 "23df8397351a16961971569dad4ef3f77ed8e8f72dc28c49e07594565410ba53" => :arm64_big_sur
-    sha256 "dffbb2a8ef211800bcdfe6a61923b0451f67c5955a9f5b803f2b4d61733e8bf3" => :catalina
-    sha256 "d2b7b5007c8eee0164af24e48a01c80c2b9cb7620cde0e6ee814fb1f7afa9088" => :mojave
+    sha256 "293d2d667c2f43bc297ae7b3d77d63a975eea5d3147741116d229b5cfb5d2c5b" => :big_sur
+    sha256 "802738717b4b021a445ca0e3fb21df4035ecb9b19cb424cf10a027adce163ddc" => :arm64_big_sur
+    sha256 "e9ce21bbe1ef07bbb152ce91c7fccfacfc7d94ba32c21866ce69a237079b3e90" => :catalina
+    sha256 "d1706d2192de306c0884dff62de90409a3f89d5eb4f44a27b03362bae1365817" => :mojave
   end
 
   depends_on "boost" => :build
