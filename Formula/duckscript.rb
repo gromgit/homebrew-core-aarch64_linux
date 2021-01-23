845be154ddbf3ca967c56699bc3e8d9e8c95d8d1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-23 19:51:57 +0000

    duckscript: update 0.7.2 bottle.

--- Formula/duckscript.rb
+++ Formula/duckscript.rb
@@ -8,10 +8,10 @@ class Duckscript < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "2e80eaf10dfa5b1bc6d3593d34020f48deb0e4d2df88c7ba828cd9eb2da29f76" => :big_sur
-    sha256 "78556d9bd5b9dc1e08f3848b1488f375e85029f3222979476b71fffa95c7528b" => :arm64_big_sur
-    sha256 "8b1f02d6c68eb9b67420f7d2b9c1b1ca205b15a54d3fc6d7c5764f6dbc1b512c" => :catalina
-    sha256 "c14e28b195ed1d2e7cd4a87be420a52f5619ddb6741c58c9273a3c1a2060f453" => :mojave
+    sha256 "ea6f60db53df1104716d49e12559d5fee612dee3fd2bdbde4fae82c2972e5735" => :big_sur
+    sha256 "b225f3f5c277f260b1cd19f7aeff3ea823b73ef77772b79480c8886fc82c9db1" => :arm64_big_sur
+    sha256 "f2a4df37ab700852e3ceb13f32fc6067bd3ef619d9394b17f58398ad368911e4" => :catalina
+    sha256 "5407dc856b08410b8ce096717bb4509bc91133c278ce7303e22bb7a339ab5f42" => :mojave
   end
 
   depends_on "rust" => :build
