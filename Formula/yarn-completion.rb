c0ff72fc575d5c2230e8c0dfc8105001b59aee6c
Seeker <meaningseeking@protonmail.com>
2021-01-23 03:26:33 -0800

    yarn-completion 0.17.0 (#69358)

--- Formula/yarn-completion.rb
+++ Formula/yarn-completion.rb
@@ -1,11 +1,13 @@
 class YarnCompletion < Formula
   desc "Bash completion for Yarn"
   homepage "https://github.com/dsifford/yarn-completion"
-  url "https://github.com/dsifford/yarn-completion/archive/v0.8.0.tar.gz"
-  sha256 "f359dbfd4d9dd28e231e81da16f308e73662fd586546672569a0eac994f2bcd6"
+  url "https://github.com/dsifford/yarn-completion/archive/v0.17.0.tar.gz"
+  sha256 "cc9d86bd8d4c662833424f86f1f86cfa0516c0835874768d9cf84aaf79fb8b21"
   license "MIT"
 
   bottle :unneeded
+
+  depends_on "bash"
 
   def install
     bash_completion.install "yarn-completion.bash" => "yarn"
@@ -13,6 +15,6 @@ class YarnCompletion < Formula
 
   test do
     assert_match "complete -F _yarn yarn",
-      shell_output("source #{bash_completion}/yarn && complete -p yarn")
+      shell_output("#{Formula["bash"].opt_bin}/bash -c 'source #{bash_completion}/yarn && complete -p yarn'")
   end
 end
