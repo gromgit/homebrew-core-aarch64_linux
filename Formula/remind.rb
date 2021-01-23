b84517883d49e99454d0f93b52c8226c798abb56
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-23 08:46:55 +0000

    remind: update 03.03.05 bottle.

--- Formula/remind.rb
+++ Formula/remind.rb
@@ -13,10 +13,10 @@ class Remind < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6c479910c960f05d2a3a3b2f12323628d41f03553de2584696f1a95ffc7ad6ee" => :big_sur
-    sha256 "01501198dbbfcc40670c97d38cff99ccaf00644e259a01b33ac57d44c4446267" => :arm64_big_sur
-    sha256 "39f0993b9ceeeb6e96e050f5959e1bf58c9f59657e0174d7bfec73a036d032b0" => :catalina
-    sha256 "de6b40ee75e1ad87d9a99dafe0c0d58e03c179d7327cb60e485456352dc9338c" => :mojave
+    sha256 "17c14cafbf48b0326b70b6492e5d27aceaa14b1642ce766a454d58487ce4448b" => :big_sur
+    sha256 "db0687a396b900b0f079101f1b900711da228403b2705f39d13ecfad40a85248" => :arm64_big_sur
+    sha256 "7bb46ad0649a5264cd7d9d94122e21a1c7d815cb09ebf7f6d055b80bc15e91f1" => :catalina
+    sha256 "2b82a2c8e1223b5563950148a26601e33f06a723e4c7890034499412d5404f50" => :mojave
   end
 
   conflicts_with "rem", because: "both install `rem` binaries"
