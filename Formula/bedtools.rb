9dcbb5be8fc81f45b7afbb29e12a08100b689441
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-23 21:32:27 +0000

    bedtools: update 2.30.0 bottle.

--- Formula/bedtools.rb
+++ Formula/bedtools.rb
@@ -7,11 +7,10 @@ class Bedtools < Formula
 
   bottle do
     cellar :any
-    sha256 "6cc3f3e0e540b594a44a839d62fc4dbd4a75a92de660b83fdceba81fd0f7651c" => :big_sur
-    sha256 "882d1ba4c75198623d0004751fcb3184ffdd458de21ee30eb8d0854381e179c4" => :arm64_big_sur
-    sha256 "ccf7c496bef6f4504e099ff6dfd0a04a277fe3481dcf0c4cae5b50e605fe9329" => :catalina
-    sha256 "86d19c05b45021f1eedbccd910d0e1401e7b2aa7552bdf5160e77d9bf42fd4a6" => :mojave
-    sha256 "9f2a0e41c2463fef3092b57f0a6a888ef4f50be07fa8b78b3185d6e971bc920d" => :high_sierra
+    sha256 "6554348743d3efba64e47294f8cb229229f168902234dd7e0ee0b4dfa85bb4d7" => :big_sur
+    sha256 "d43458d5a8f50a7db0f7f728ed82c0a980d9202126624d594d17c99c2c9ea76d" => :arm64_big_sur
+    sha256 "32d302a56df9044ce36d44db851318fc4fb45676086e48a6d913f2286ae3a756" => :catalina
+    sha256 "69b80814d21b11edf9d45d67de8536d5db965dd6743e56079e1f3d6cf77bb6d2" => :mojave
   end
 
   depends_on "python@3.9" => :build
