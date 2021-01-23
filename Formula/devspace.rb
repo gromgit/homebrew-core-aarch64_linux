911ff7a94757b48a5b2c5598d175a0c02bd5615b
Seeker <meaningseeking@protonmail.com>
2021-01-22 17:37:10 -0800

    devspace 5.7.3
    Closes #69539.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/devspace.rb
+++ Formula/devspace.rb
@@ -2,8 +2,8 @@ class Devspace < Formula
   desc "CLI helps develop/deploy/debug apps with Docker and k8s"
   homepage "https://devspace.cloud/docs"
   url "https://github.com/devspace-cloud/devspace.git",
-      tag:      "v5.7.1",
-      revision: "7ba03fa139f02840cb7561f57e045709823dcc0d"
+      tag:      "v5.7.3",
+      revision: "99eb86780cf63102573e6b6caf17777d41c2e008"
   license "Apache-2.0"
   head "https://github.com/devspace-cloud/devspace.git"
 
