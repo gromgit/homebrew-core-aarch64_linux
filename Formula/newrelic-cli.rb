a3094625019c6a1dde2cf54c4b8bb880b38e2a96
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-23 07:49:36 +0000

    newrelic-cli: update 0.18.17 bottle.

--- Formula/newrelic-cli.rb
+++ Formula/newrelic-cli.rb
@@ -8,10 +8,10 @@ class NewrelicCli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "8dc7daef4cd0d46f21eb9e7185c468347c83bf1f91c841fd501a0d68178f0f3e" => :big_sur
-    sha256 "49e99b9b932763595f24c44f47269ba3cebf8134d2e81a80b35a331305d9b5bb" => :arm64_big_sur
-    sha256 "04289be71086e1920443f94c6897a7c08d7427096958f38344283b367e2cdc8c" => :catalina
-    sha256 "dff95fc783be84541c474ace17aa67adbf138eef6feaac776ea9fd5f7cb0a55b" => :mojave
+    sha256 "424446b6412e3ef984d81c73bfd52760e2900e07b0a3eb4404c61aefc88ef0e1" => :big_sur
+    sha256 "240174224c946f7f65efd59322ff99c5bb456718957d6cd24d606d4201aca935" => :arm64_big_sur
+    sha256 "1751df3bae3b6ab4d26c9e517b35b67125fba3fc7b5a8439f7136c678114dc65" => :catalina
+    sha256 "21d70626d9632933d510932bf9f87516da98140f047e964079a468c9f1ec8676" => :mojave
   end
 
   depends_on "go" => :build
