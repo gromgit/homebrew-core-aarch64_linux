beb04acb1033754b26c10b32598fbf485fbf92ec
Seeker <meaningseeking@protonmail.com>
2021-01-22 20:58:13 -0800

    docker-compose-completion 1.28.0 (#69540)

--- Formula/docker-compose-completion.rb
+++ Formula/docker-compose-completion.rb
@@ -1,8 +1,8 @@
 class DockerComposeCompletion < Formula
   desc "Completion script for docker-compose"
   homepage "https://docs.docker.com/compose/completion/"
-  url "https://github.com/docker/compose/archive/1.27.4.tar.gz"
-  sha256 "1c0458f37e9de4bf2d79fe82c9ab0065c8a6132496c3c2f477599604e294a422"
+  url "https://github.com/docker/compose/archive/1.28.0.tar.gz"
+  sha256 "cd6deb23cf93c426fccf223f88cd0b81a251e6f7e3dbf6e16b98228c61b88192"
   license "Apache-2.0"
   head "https://github.com/docker/compose.git"
 
