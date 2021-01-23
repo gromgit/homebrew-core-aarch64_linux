de1be422410b366961243da267bd46c2cbac7ea6
Rui Chen <rui@chenrui.dev>
2021-01-22 19:47:44 -0500

    argocd 1.8.3
    Closes #69530.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/argocd.rb
+++ Formula/argocd.rb
@@ -2,8 +2,8 @@ class Argocd < Formula
   desc "GitOps Continuous Delivery for Kubernetes"
   homepage "https://argoproj.io"
   url "https://github.com/argoproj/argo-cd.git",
-      tag:      "v1.8.2",
-      revision: "94017f2c8d97588d4aa2213713a71d51005ed62d"
+      tag:      "v1.8.3",
+      revision: "0f9c68427882bf4633d395cbfcd7c9271795fd9b"
   license "Apache-2.0"
 
   bottle do
