b83118fdcea6bf86a1d01ed6f9c5c6b8f8a458eb
bleepnetworks <bleepnetworks@users.noreply.github.com>
2021-01-23 08:40:24 -0800

    nuclei 2.2.0 (new formula)
    Closes #69605.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

---
+++ Formula/nuclei.rb
@@ -0,0 +1,40 @@
+class Nuclei < Formula
+  desc "HTTP/DNS scanner configurable via YAML templates"
+  homepage "https://nuclei.projectdiscovery.io/"
+  url "https://github.com/projectdiscovery/nuclei/archive/v2.2.0.tar.gz"
+  sha256 "c503b565230f5375339c72872d496137abf8be26c156e20447d1ae0752169e40"
+  license "MIT"
+  head "https://github.com/projectdiscovery/nuclei.git"
+
+  depends_on "go" => :build
+
+  def install
+    cd "v2/cmd/nuclei" do
+      system "go", "build", *std_go_args, "main.go"
+    end
+  end
+
+  test do
+    (testpath/"test.yaml").write <<~EOS
+      id: homebrew-test
+
+      info:
+        name: Homebrew test
+        author: bleepnetworks
+        severity: INFO
+        description: Check DNS functionality
+
+      dns:
+        - name: \"{{FQDN}}\"
+          type: A
+          class: inet
+          recursion: true
+          retries: 3
+          matchers:
+            - type: word
+              words:
+                - \"IN\tA\"
+    EOS
+    system "nuclei", "-target", "google.com", "-t", "test.yaml"
+  end
+end
