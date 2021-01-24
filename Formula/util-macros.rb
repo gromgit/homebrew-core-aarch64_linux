bfe3326c63461fc359f66e2d5dfc2253973576bc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-24 19:53:29 +0000

    util-macros: update 1.19.3 bottle.

--- Formula/util-macros.rb
+++ Formula/util-macros.rb
@@ -7,11 +7,10 @@ class UtilMacros < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "dca6b81cec458f1d2fe9d367d91e9dc5f2459246b9e94fd77ed8ccf26c902c5d" => :big_sur
-    sha256 "b87b4a899e8187eae34e6befe1f2b287035c036a3abd8ceca3eaf3d4190e1961" => :arm64_big_sur
-    sha256 "298dfa88698206b08ce19e5daf66903da4a94e561ec639487c17b55c3d09ee3f" => :catalina
-    sha256 "bd71cb44913a35fb4f8e63a0fd90229d8912d7af40f02c78b056cf72eb88b684" => :mojave
-    sha256 "38d6cb7e6c900a555c5602af513224d346166e109b7226cc5b86cb51b9b55fe4" => :high_sierra
+    sha256 "75380fbb4f54df33cc028ca19b05b7350fc0ee864dbe6e4ee6a4fa9cdec19ad9" => :big_sur
+    sha256 "c264994ad25a15c84c92929eaf5dd8ca14c10938f58be3161f58ffcebbb3eb07" => :arm64_big_sur
+    sha256 "3aebaa717cf69676ff38b74538a34b3ab96e6344a2303da8f12f420a66b73719" => :catalina
+    sha256 "17d679f4c969c41701b1dcb897957f5772555453aff321eacff94bf91cf19e56" => :mojave
   end
 
   depends_on "pkg-config" => :test
