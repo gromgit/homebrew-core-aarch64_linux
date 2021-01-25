300a7e0dff54e010c553ff539d883629c0c82e6c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-25 07:57:19 +0000

    nuclei: add 2.2.0 bottle.

--- Formula/nuclei.rb
+++ Formula/nuclei.rb
@@ -6,6 +6,14 @@ class Nuclei < Formula
   license "MIT"
   head "https://github.com/projectdiscovery/nuclei.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bf27703a6eb5f03a9c52e9aba2b4803b2da61fc55bd0bf0f825e75b13d3aa425" => :big_sur
+    sha256 "209ad76cc1ee1f93c1d5e71776d9da4e27b0d527fb7ab70439c6fa4500f632ca" => :arm64_big_sur
+    sha256 "00c3dc4a1d22d4b4387e0ab4eddc769b2183d46db1069118bc0157d71398b404" => :catalina
+    sha256 "93a6882b2612447957dc350faa6488028b0a8b68b43355a31f5aaa978de8aa77" => :mojave
+  end
+
   depends_on "go" => :build
 
   def install
