348b479e90ee044625f3cefa828cd2962c59919d
rui <rui@chenrui.dev>
2021-01-25 02:25:40 +0000

    libsndfile: update 1.0.31 bottle.

--- Formula/libsndfile.rb
+++ Formula/libsndfile.rb
@@ -12,10 +12,10 @@ class Libsndfile < Formula
 
   bottle do
     cellar :any
-    sha256 "ee9bc99bab89708e8f371173efc13c71c82d63cd46de50c4dd6ca89f4e2bd1f9" => :big_sur
-    sha256 "0c6dd62351317eac23e245450cded41a654084da265d74cc25350b7aa8628d36" => :arm64_big_sur
-    sha256 "8777b541acc126315428bcee3f3453a240157dcdca48d2e51609158ad3539284" => :catalina
-    sha256 "eaa0e886d88536970a5618557f1fb4e7a06d0c1429bf8bcb874cd90e31e05e0a" => :mojave
+    sha256 "a4a734e58220fc8615d86e4563e9a874447d568151b366aa94391dfe07c4e0fb" => :big_sur
+    sha256 "8e2fc3b0df09a21840f8643f644bd3a0bb3c3551d21f600b344f6b316d3ef44d" => :arm64_big_sur
+    sha256 "671a3cc9c7dafd89cbaffeccf4de826262c144184bf5779320c236e87e7636cc" => :catalina
+    sha256 "8b2876610f9188e8125f636e85bcbd525343b216c6d0787954e78b88dfe8f101" => :mojave
   end
 
   depends_on "autoconf" => :build
