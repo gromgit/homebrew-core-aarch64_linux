99bc0941319f4424f6a550995004529fad69669c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-25 14:59:54 +0000

    ace: update 7.0.0 bottle.

--- Formula/ace.rb
+++ Formula/ace.rb
@@ -13,10 +13,9 @@ class Ace < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "35421f2b6d685d304bf091673f470509e4c7e91c682d49001807b8449d8b6b0c" => :big_sur
-    sha256 "c8df669ae59fcf41cbfea88a8316965332f46e77886e21e1ea3848352c6b0e48" => :catalina
-    sha256 "40e6474093ba8816d8693a6979e121138edd3b4f3619da51ca1ab8c0e9a3f9ae" => :mojave
+    sha256 "8d5842334724e12e37e34afb93c42a14f1baa9bc49d2643c573b201534e72a30" => :big_sur
+    sha256 "72e9b7ecba3c88f74b8d0e4f4b1cce66cc80614f56bbd2941fc0a2aab97f6c7e" => :catalina
+    sha256 "2635c73c42b1d550183a9dd7f4e26037287c0d4e75664bbb12f04395003ee8aa" => :mojave
   end
 
   def install
