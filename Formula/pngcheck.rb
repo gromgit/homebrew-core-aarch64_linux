b80e755b27f58e031e030c3385e9daffbd8d827e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-25 20:43:52 +0000

    pngcheck: update 3.0.1 bottle.

--- Formula/pngcheck.rb
+++ Formula/pngcheck.rb
@@ -12,15 +12,10 @@ class Pngcheck < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0fef6b0676fa1f23b9fcd00e404a7d8b737d5deb7a940e8ca19df104dd767993" => :big_sur
-    sha256 "744c6241d76de0249e24a3c754dcc5df2d39eb067d552ec86300ca9d607f439e" => :arm64_big_sur
-    sha256 "7fb0d821218aba52e2c261c4cdcc50438d71cbf232baba97ef13e861c3386a11" => :catalina
-    sha256 "22033aa6f7b96ecb9d7eb038b7103e5faa782f4d36c142c3220f1e1ff1fc9e9e" => :mojave
-    sha256 "f4cdf56cdf51ab156bcc1009cce5cdd46d86de12811549136d50a1a18295b7c7" => :high_sierra
-    sha256 "c98b0fd09e8b615f98d4ee9762485a8e9026c9cdb3dc576ef81ee0bbff6058d7" => :sierra
-    sha256 "98e0a49125511f279c09c99fdb33ea5e2d44f489be4a8a6d70ce9faba48e8b92" => :el_capitan
-    sha256 "2f8901f0259f2ec24478268e5fa4cd8fe904a160592f118efdddf4ba20221dd6" => :yosemite
-    sha256 "af2af2a3771b7730c0da6fe3c74f6044b3664498c0b9f5070be3cf4d7ec1274e" => :mavericks
+    sha256 "e4d716fb0d3ff48095b36044e50b79fa80b8cfd962e9a58f0b227ade97bdbbc8" => :big_sur
+    sha256 "bf4febdb6ed3286ae86ceadee979aac9e829e477abd5fb83c595d0edf605194b" => :arm64_big_sur
+    sha256 "02af550fd568214ed59e4fc9d85359b48f4a9520d653fdfa53157f201a803208" => :catalina
+    sha256 "312428dc1c9917eb598d32e0889047adf9789e6b5aca9462f09817c93cfb2193" => :mojave
   end
 
   def install
