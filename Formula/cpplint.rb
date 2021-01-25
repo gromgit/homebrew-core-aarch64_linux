70b3a8f72adfe05370510066eb06d3240015230e
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-01-25 07:37:52 +0000

    cpplint: add 1.5.4 bottle.

--- Formula/cpplint.rb
+++ Formula/cpplint.rb
@@ -11,6 +11,14 @@ class Cpplint < Formula
     url :stable
   end
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e9294810d16c29dc48b4cfb5c2100c031bed1afa1e2dca8c69914e2ea3b2bb75" => :big_sur
+    sha256 "2e90a23cdf7c41fc41541d8ce4716eccae1fb9de74acb4a0c43027532eb8772c" => :arm64_big_sur
+    sha256 "f6e997327f819be03b1bed333bd633463a752cd97fd381b3e837ad07d4583273" => :catalina
+    sha256 "a0436c4fb92a6727d44692c399fc572c176c8f85c051e5b98c57ab01ec5f51c9" => :mojave
+  end
+
   depends_on "python@3.9"
 
   def install
