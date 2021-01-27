fe4b44006aef5dffe389ff8b1dcb65f454b4eb4b
Karl Fischer <fishi0x01@gmail.com>
2021-01-27 16:02:24 +0100

    vsh 0.8.0
    Closes #69861.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/vsh.rb
+++ Formula/vsh.rb
@@ -1,8 +1,8 @@
 class Vsh < Formula
   desc "HashiCorp Vault interactive shell"
   homepage "https://github.com/fishi0x01/vsh"
-  url "https://github.com/fishi0x01/vsh/archive/v0.7.2.tar.gz"
-  sha256 "f20885425a9c6ee04ed447046e6df113585469b37332e02b7a23639b718314a1"
+  url "https://github.com/fishi0x01/vsh/archive/v0.8.0.tar.gz"
+  sha256 "7eb6e2dcd758a38acfe12437e40d5f22a1f0cefa369dab9aeabd3417ae46ba12"
   license "MIT"
 
   bottle do
