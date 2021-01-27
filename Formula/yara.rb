41993f39e464244711c1a120b5f4fc802beff422
Thomas Dupuy <thom4s.d@gmail.com>
2021-01-27 12:05:59 -0500

    yara 4.0.4
    yara v4.0.4
    
    Closes #69866.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/yara.rb
+++ Formula/yara.rb
@@ -1,8 +1,8 @@
 class Yara < Formula
   desc "Malware identification and classification tool"
   homepage "https://github.com/VirusTotal/yara/"
-  url "https://github.com/VirusTotal/yara/archive/v4.0.3.tar.gz"
-  sha256 "d95b7f5e2981328a10ea206e3384d661bd4d488e43e8d1785152bdea44d89880"
+  url "https://github.com/VirusTotal/yara/archive/v4.0.4.tar.gz"
+  sha256 "67fdc6f1050261914cf4a9e379b60961f62c2f76af676bafb2ceb47dd642d44f"
   license "BSD-3-Clause"
   head "https://github.com/VirusTotal/yara.git"
 
