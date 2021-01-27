682a8c7ac606a881205218965dc96755a38b9b93
Rui Chen <rui@chenrui.dev>
2021-01-27 15:26:37 -0500

    ncmpc 0.43
    * ncmpc 0.43
    * remove patch
    
    Closes #69883.
    
    Co-authored-by: rui <rui@chenrui.dev>
    Signed-off-by: Alexander Bayandin <a.bayandin@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ncmpc.rb
+++ Formula/ncmpc.rb
@@ -1,8 +1,8 @@
 class Ncmpc < Formula
   desc "Curses Music Player Daemon (MPD) client"
   homepage "https://www.musicpd.org/clients/ncmpc/"
-  url "https://www.musicpd.org/download/ncmpc/0/ncmpc-0.42.tar.xz"
-  sha256 "a5f7471d766a71c222374efa4aa17ef6ee0e42ad48d15528edd935d1f0f6cd4d"
+  url "https://www.musicpd.org/download/ncmpc/0/ncmpc-0.43.tar.xz"
+  sha256 "7abf3c83d3a77c18681a8ce90126c8cb1e1884ebde5be2a735293a4f535ed382"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -27,10 +27,6 @@ class Ncmpc < Formula
   depends_on "libmpdclient"
   depends_on "pcre"
 
-  # remove in next release
-  # commit reference, https://github.com/MusicPlayerDaemon/ncmpc/commit/1a45eab
-  patch :DATA
-
   def install
     mkdir "build" do
       system "meson", *std_meson_args, "-Dcolors=false", "-Dnls=disabled", ".."
@@ -42,29 +38,3 @@ class Ncmpc < Formula
     system bin/"ncmpc", "--help"
   end
 end
-
-__END__
-diff --git a/src/screen_utils.cxx b/src/screen_utils.cxx
-index 95de70e..e85061f 100644
---- a/src/screen_utils.cxx
-+++ b/src/screen_utils.cxx
-@@ -29,6 +29,7 @@
-
- #ifndef _WIN32
- #include "WaitUserInput.hxx"
-+#include <cerrno>
- #endif
-
- #include <string.h>
-diff --git a/src/signals.cxx b/src/signals.cxx
-index 4c005aa..9f3eb72 100644
---- a/src/signals.cxx
-+++ b/src/signals.cxx
-@@ -17,6 +17,7 @@
-  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-  */
-
-+#include <signal.h>
- #include "Instance.hxx"
-
- void
