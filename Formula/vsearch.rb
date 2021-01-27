9bc9456ad951e61b5f85801c03a028250c3439e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-27 23:21:56 +0000

    vsearch: update 2.15.2 bottle.

--- Formula/vsearch.rb
+++ Formula/vsearch.rb
@@ -7,11 +7,10 @@ class Vsearch < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f93eaf8d2c94693f7eaf185790604b49e2dae03a7a7569c031e5a73a2f1fdd15" => :big_sur
-    sha256 "e93c5e950ad68cbfe3fa1568fa8975b294e0dd7d4cca86484433fa3a9614321b" => :arm64_big_sur
-    sha256 "25c75ec8b1a1dc243b11145ba0cddcfb74d910aa682a20b0faa7d297f12de945" => :catalina
-    sha256 "54adb18eb8744a4c6cd3c827c5088962e6587e4bc2becc1c296ce52304d62be2" => :mojave
-    sha256 "c308c67a02b669b546cb179df8d3ec7b90089ccfc40fb4bdc11ea7e91994b55f" => :high_sierra
+    sha256 "6801f717e69fb4c30da5fa87c54bd2e19e05af88f7b08b8efbf0df0444a6ef15" => :big_sur
+    sha256 "4d94291e05b0e22797c10612563c129aa0095843459eeb98d2ff96292dcd9e28" => :arm64_big_sur
+    sha256 "907ea4fbfaae550577d4f9569e7602bc55668fc8a0f0594907f69ab2ce80b25f" => :catalina
+    sha256 "f0a8d4c30ce9921d29d574266c83bfd47ec4a0d8e7428790ca82a07b410f0deb" => :mojave
   end
 
   depends_on "autoconf" => :build
