b9de34feaab4d80ecadb1a07ac8e52981be86bd3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-28 12:55:10 +0000

    gox: update 1.0.1 bottle.

--- Formula/gox.rb
+++ Formula/gox.rb
@@ -8,11 +8,10 @@ class Gox < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c09b516c00852371c0422598390b06391f363deb151db428131b02cd0c31bcee" => :big_sur
-    sha256 "6693416aef377d44385c25eb786d25f963258c0895491beedbf6039d7c84c06d" => :catalina
-    sha256 "008ec56acef96c3ad3117bcde87f1998fcf4ef9c93f82ae363ed6ac39914a95d" => :mojave
-    sha256 "c2d77e6fadb6c7585a5df89eb91aaf1f41f6b88829e1a647efb4ebbc70277b3b" => :high_sierra
-    sha256 "1d48879bdbbd84d2406aeaf5f052c51ed2f0b8f9484508ad6085bf537be6f5f6" => :sierra
+    rebuild 1
+    sha256 "ee0ae4811c2bd84db661328bda7ff64e3560aae5bea9b9447dc75f0c4528ea03" => :big_sur
+    sha256 "0faf261981e71df491ffe35d995aeefe1660bf73c204206ad88668981882e88e" => :catalina
+    sha256 "96cf3f477de802c91e532a362cfaab221854f8683414b0abecbbe1e4b66c6b56" => :mojave
   end
 
   depends_on "go"
