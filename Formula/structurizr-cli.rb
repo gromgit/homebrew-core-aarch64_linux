19240282e1caeaad12a049f2a421d85ae006c603
Gilberto Taccari <gilberto.taccari@gmail.com>
2021-01-28 22:29:28 +0100

    structurizr 1.8.0

--- Formula/structurizr-cli.rb
+++ Formula/structurizr-cli.rb
@@ -1,8 +1,8 @@
 class StructurizrCli < Formula
   desc "Command-line utility for Structurizr"
   homepage "https://structurizr.com"
-  url "https://github.com/structurizr/cli/releases/download/v1.7.0/structurizr-cli-1.7.0.zip"
-  sha256 "3f5b8b35d85f0d9dbddb874c644af3ed08f46bcde4c6d8161116e72c60360171"
+  url "https://github.com/structurizr/cli/releases/download/v1.8.0/structurizr-cli-1.8.0.zip"
+  sha256 "f5352d0b4cb2dc14fbd314ac64588103ca3e8d1bd407714a2acd392c9ce4d8aa"
   license "Apache-2.0"
 
   bottle :unneeded
@@ -17,7 +17,7 @@ class StructurizrCli < Formula
   test do
     expected_output = <<~EOS.strip
       Structurizr CLI v#{version}
-      Usage: structurizr push|pull|export [options]
+      Usage: structurizr push|pull|lock|unlock|export [options]
     EOS
     result = pipe_output("#{bin}/structurizr-cli").strip
     assert_equal result, expected_output
