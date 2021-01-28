518819a6abef3d9c262bcd956be4b533e27b8717
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-28 00:49:35 +0000

    nushell: update 0.26.0 bottle.

--- Formula/nushell.rb
+++ Formula/nushell.rb
@@ -14,10 +14,10 @@ class Nushell < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "72434b0bd7d06c865e49c70e523b5a2caabf1fee7207a71eee248e32803fe7a3" => :big_sur
-    sha256 "b384174752cf334c56eaf4359f423ab7ad40c9945465851cc66a8ae5b7d63d44" => :arm64_big_sur
-    sha256 "e75e7e0494ebe1157797213aaa6b1a2bb33f7da507601537dd19f81c83a97fa5" => :catalina
-    sha256 "eb00ba8dec78c540d2fae052e746b26664cc27eced6affc46dec069ef5cc33fc" => :mojave
+    sha256 "05d199ea12d531abc34548b808d0701ff14d00a986481f9ae8f1776dac765c8c" => :big_sur
+    sha256 "34500e7bd0ca8a0cd8e187ade89a81b7beb372b1715337f17111c4e1acfd99f6" => :arm64_big_sur
+    sha256 "82e2ed4ad465d0bf59a6d2cd042ebd6b51e0da46d32d4df4d0a7d091b5b4a16a" => :catalina
+    sha256 "06683151d468a8fbe3dac4fafb9a2133b48e832cf7555fe78e06bc7096e95e3f" => :mojave
   end
 
   depends_on "rust" => :build
