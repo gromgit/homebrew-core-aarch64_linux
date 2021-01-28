67c31c6fccd9619620bb623d3ff8229a1503be00
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-28 23:23:02 +0000

    minikube: update 1.17.1 bottle.

--- Formula/minikube.rb
+++ Formula/minikube.rb
@@ -8,10 +8,9 @@ class Minikube < Formula
   head "https://github.com/kubernetes/minikube.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "9543a3d3316da0e7727938b4c517e5dc18958328c8c0b2cfc9998685819fc9e3" => :big_sur
-    sha256 "ff66b35900830df46f2178601e0cdd103348615c4e8abc964ad544a189906b13" => :catalina
-    sha256 "24aec8391f1188177782c730621b1862e80d681adac891a0a7ae4bd656bd7c04" => :mojave
+    sha256 cellar: :any_skip_relocation, big_sur: "645cc05655411bddc944818278eca867049e7e2712411dd79028f404ed83d08a"
+    sha256 cellar: :any_skip_relocation, catalina: "4d5263bf35d8800cec7c820f6efcb32fac1677627f21954888b97daedbeeb7a1"
+    sha256 cellar: :any_skip_relocation, mojave: "0edb5f37a1d108806f20966b02a0cb805e69f285a8b00ece64f1e0dad42449e6"
   end
 
   depends_on "go" => :build
