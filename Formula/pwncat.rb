c827d7c287442f2c56893a50b1c941febad2dd38
Dustin Rodrigues <dust.rod@gmail.com>
2021-01-27 23:56:02 -0500

    pwncat: add license
    Closes #69915.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pwncat.rb
+++ Formula/pwncat.rb
@@ -5,7 +5,9 @@ class Pwncat < Formula
   homepage "https://pwncat.org"
   url "https://files.pythonhosted.org/packages/31/8a/efc45ecc5e91afc76de9c56f89de99af01d575529fead6ee24331a3fddf2/pwncat-0.1.0.tar.gz"
   sha256 "4f711c3d0f22650e20ad1429a7f0c9116b930be04435e7f690746ca0e1c5cd69"
+  license "MIT"
   revision 1
+  head "https://github.com/cytopia/pwncat.git"
 
   livecheck do
     url :stable
