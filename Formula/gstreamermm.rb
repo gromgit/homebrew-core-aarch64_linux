930139bbb0df92e91e9e74f480f767a75aa89e3c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-30 07:54:23 +0000

    gstreamermm: update 1.10.0_6 bottle.

--- Formula/gstreamermm.rb
+++ Formula/gstreamermm.rb
@@ -10,11 +10,10 @@ class Gstreamermm < Formula
   end
 
   bottle do
-    cellar :any
-    sha256 "d30aeafe0ac35fa2ce631e2c8e47f72eebbfcf5e395f3e8703a72f8e68dfe136" => :big_sur
-    sha256 "f568885efde3d1d18801014eb944b87138db9ea2fa067aa9c714c0a0c404fad1" => :arm64_big_sur
-    sha256 "683541895f76162db0eb4f0637fd5d1e131d2385e68ac25b3401dcc991e496d3" => :catalina
-    sha256 "d11aaca4754a2420e206829837a34a8aaf16c2ad1ebaba5cd9a6f5d85e4f50cc" => :mojave
+    sha256 cellar: :any, big_sur: "2b46f0bc113d6125e1823d24365a8c4c664576de16db5ad3ce2e2a0157b1f99a"
+    sha256 cellar: :any, arm64_big_sur: "ccfdabeca639a0c10260c232dc8f4a24af5fc7b4b8c9e5befee65be29314dbb1"
+    sha256 cellar: :any, catalina: "f65023da1c10400540a04a1bb422f42342406d013e89a1344a04778283824eb8"
+    sha256 cellar: :any, mojave: "d5cbb27b618ded15a4b8dd132195307927a03358a88575e4934d7b0da0148e75"
   end
 
   depends_on "pkg-config" => :build
