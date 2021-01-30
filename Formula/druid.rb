1e642b15b4c6437aa25c6b2ab761040956a9ae26
rui <chenrui333@gmail.com>
2021-01-29 21:23:46 -0500

    druid 0.20.1 (#70019)

--- Formula/druid.rb
+++ Formula/druid.rb
@@ -1,9 +1,9 @@
 class Druid < Formula
   desc "High-performance, column-oriented, distributed data store"
   homepage "https://druid.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=druid/0.20.0/apache-druid-0.20.0-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/druid/0.20.0/apache-druid-0.20.0-bin.tar.gz"
-  sha256 "734f836375bc1121100712b0501149f8bcbcb6d4cf2109b522af06367a8b1cb5"
+  url "https://www.apache.org/dyn/closer.lua?path=druid/0.20.1/apache-druid-0.20.1-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/druid/0.20.1/apache-druid-0.20.1-bin.tar.gz"
+  sha256 "9ed9f0b4090043ed7daf73d8a1a9bfa068be7a418967d88df78ad94209b835f4"
   license "Apache-2.0"
 
   livecheck do
