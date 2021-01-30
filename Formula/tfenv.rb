862c623903330fd39465b0b5a5bd0e01d7691055
rui <chenrui333@gmail.com>
2021-01-30 15:22:50 -0500

    tfenv 2.1.0 (#70062)
    * tfenv 2.1.0
    
    * remove patch
    
    * fix code style
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/tfenv.rb
+++ Formula/tfenv.rb
@@ -1,21 +1,10 @@
 class Tfenv < Formula
   desc "Terraform version manager inspired by rbenv"
   homepage "https://github.com/tfutils/tfenv"
+  url "https://github.com/tfutils/tfenv/archive/v2.1.0.tar.gz"
+  sha256 "f8f1e14e1d5064c47ddbb041e31ce15ac0a849e620541598db09efff575b3c9b"
   license "MIT"
   head "https://github.com/tfutils/tfenv.git"
-
-  stable do
-    url "https://github.com/tfutils/tfenv/archive/v2.0.0.tar.gz"
-    sha256 "de3dcf13768cb078e94d68ca85071b8d6e44104394336d952255ca558b854b0b"
-
-    # fix bash 3.x compatibility
-    # removed in the next release
-    # Original source: "https://github.com/tfutils/tfenv/pull/181.patch?full_index=1"
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/526faca9830646b974f563532fa27a1515e51ca1/tfenv/2.0.0.patch"
-      sha256 "b1365be51a8310a44b330f9b008dabcdfe2d16b0349f38988e7a24bcef6cae09"
-    end
-  end
 
   livecheck do
     url :head
