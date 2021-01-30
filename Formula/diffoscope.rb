5ae50eca2ea704763d91b4fbdbd80b574663e9ee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-30 16:33:28 +0000

    diffoscope: update 166 bottle.

--- Formula/diffoscope.rb
+++ Formula/diffoscope.rb
@@ -12,11 +12,10 @@ class Diffoscope < Formula
   end
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "fe14f34879e07c0d0b9296da3f6a423920390abb261f97c132a233b4b73a28bd" => :big_sur
-    sha256 "b1c4bef0c194f34d6e1acca358224a8e6897140590e3c90138120d4f93d595ef" => :arm64_big_sur
-    sha256 "2eff4543a80d793ed35b0710af3c1be9e4fcdaf94ad7a46e5f7a7e6901bf0333" => :catalina
-    sha256 "407ba3201357bef783ce1a457a12df99064aaefc6ca11372052909ae8a5afd90" => :mojave
+    sha256 cellar: :any_skip_relocation, big_sur: "1ba45f252714dd8d0ad8c06b1faf096ef3b1c9eecf2a3404fc88280e085ff1b2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9f653b6e9b2c87e5bd8b7510393ca221ffbb41091ab3d7c845e6bac72702e92b"
+    sha256 cellar: :any_skip_relocation, catalina: "a8bf294f2d6bf89b91ea4813524125d2290eee907c464e1fb1b9bfa807954b83"
+    sha256 cellar: :any_skip_relocation, mojave: "93271ba0f90255a8a93e2bd4d75701b9e4404fed70c27bb4dd97bcebe6d386a2"
   end
 
   depends_on "gnu-tar"
