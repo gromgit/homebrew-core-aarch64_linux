2cca524c8bae5feb3c88fb50f2943b416a61ed96
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-30 04:25:34 +0000

    vercel-cli: update 21.2.2 bottle.

--- Formula/vercel-cli.rb
+++ Formula/vercel-cli.rb
@@ -12,11 +12,10 @@ class VercelCli < Formula
   end
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "79fce2ada65bda3b75f5df6a8787be3b8b942d0e2d5c58a9d0373b2c7ce7a075" => :big_sur
-    sha256 "b6676fc47590426b78833c10b0cc04aebe80ed9e55b8a939060d943ca0173ab9" => :arm64_big_sur
-    sha256 "d016df1cff85ff21a026696d7f934eb51bfa2b449ef283d0ce8848d01a1a9e96" => :catalina
-    sha256 "ba17b5562f6b291a71d3c57c16dc7678fa31f303c93846e6ae466d8c20d9abb7" => :mojave
+    sha256 cellar: :any_skip_relocation, big_sur: "9b1625cbeb9e146fa3dce6b7a8a9d77860ddb92314169056985ea1cb84cc0d97"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "14898515b34eef752230b77a29474d843a8d446ff05570dedaea495a6c4db773"
+    sha256 cellar: :any_skip_relocation, catalina: "5f1ff0f828e5cf8ffbf4a4f00dee0096d46975986aeccea9f56c178c7fba13ee"
+    sha256 cellar: :any_skip_relocation, mojave: "df1de79b6830d01ae33454ef939aad4aead5437a808a8fa27d6f123690403da2"
   end
 
   depends_on "node"
