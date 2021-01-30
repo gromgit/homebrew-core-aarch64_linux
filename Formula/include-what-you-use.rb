2148d4226a9d4d0ec73492026e93ca79f07f8b4d
Andi-Bogdan Postelnicu <abpostelnicu@me.com>
2021-01-30 17:45:56 +0200

    include-what-you-use: bump revision (llvm 11.0.1)
    Closes #69984.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/include-what-you-use.rb
+++ Formula/include-what-you-use.rb
@@ -4,6 +4,7 @@ class IncludeWhatYouUse < Formula
   url "https://include-what-you-use.org/downloads/include-what-you-use-0.15.src.tar.gz"
   sha256 "2bd6f2ae0d76e4a9412f468a5fa1af93d5f20bb66b9e7bf73479c31d789ac2e2"
   license "NCSA"
+  revision 1
 
   # This omits the 3.3, 3.4, and 3.5 versions, which come from the older
   # version scheme like `Clang+LLVM 3.5` (25 November 2014). The current
