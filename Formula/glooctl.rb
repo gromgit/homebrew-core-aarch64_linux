9277ca99aa376509601fc611aff943d9af3a478d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-30 01:40:49 +0000

    glooctl: update 1.6.7 bottle.

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -8,11 +8,10 @@ class Glooctl < Formula
   head "https://github.com/solo-io/gloo.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "77646b8e8fa67ed710557ab992d68336447a548548ceace5d53087e42edd45e6" => :big_sur
-    sha256 "e92009aaf0ee7e74ab6bec1d32e0ed31d21d454226477b0f164fabe5001e5d64" => :arm64_big_sur
-    sha256 "4a73a8630f7b1b8621b6d7cd62c136a942afd58aedb276aaa743b8bfdc126dd7" => :catalina
-    sha256 "371bf88a460e06ba1123d811b6daf110d01fae1c448972551b5f7aecbb6e110c" => :mojave
+    sha256 cellar: :any_skip_relocation, big_sur: "7a41d441b1391c5e228be254c22f8e123c32b5de031c68af6c431cc4f4a98619"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "643f59831d5a574a81ebea30f74122810e1059d1fcd83d1d60d90b02d798b377"
+    sha256 cellar: :any_skip_relocation, catalina: "03bcda2941a51d3098df14f8fafd5a3fbe4c616aa5892b8143aa892edd6093e3"
+    sha256 cellar: :any_skip_relocation, mojave: "dc34fe497e374cdfa736dd0790f49bcec80ea82a98173541d574ddba086369ac"
   end
 
   depends_on "go" => :build
