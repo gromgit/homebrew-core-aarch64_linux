e1712ce4509f7f055ec94e9b6575091ccd87ab26
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-30 01:55:19 +0000

    vault: update 1.6.2 bottle.

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -16,10 +16,9 @@ class Vault < Formula
   end
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "1555821f878b60445d22fecf2c7c8985b9d1aa1d2af62580e2ad6e0f338feb90" => :big_sur
-    sha256 "074038967a6c49ec0fb380d35808885cb658fe93851f180261e8b77ee0029dda" => :catalina
-    sha256 "bf0e522e936f71d02e6fd33b167392b9987c6052d34adce4337be834525a7c4a" => :mojave
+    sha256 cellar: :any_skip_relocation, big_sur: "ad51499dcf00457610616b2215f56cc9ad65b4448c3f7f29dc11a63c5175af41"
+    sha256 cellar: :any_skip_relocation, catalina: "8e74b52c077f59164841d2ef1c001cc774125c6e48eda784c0f2815255e87356"
+    sha256 cellar: :any_skip_relocation, mojave: "68c388c43880ffb476a0f4754dbd618eafd76bd5f359d7446f4980f888dbe1bc"
   end
 
   depends_on "go" => :build
