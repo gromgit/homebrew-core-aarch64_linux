24c0a2d5a7fffc2b4a21007794ab1655fee9f997
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-30 22:20:05 +0000

    n: update 7.0.1 bottle.

--- Formula/n.rb
+++ Formula/n.rb
@@ -7,11 +7,10 @@ class N < Formula
   head "https://github.com/tj/n.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "6e5f5c5bda7d49a0cf2c5ec8ff93aa32a37ebae54be832f3ff0a2e79357733c7" => :big_sur
-    sha256 "8f2489b60bd94ca9c15f3c5334eee89b73150c02cb42ba0cd6ee9eb100314295" => :arm64_big_sur
-    sha256 "02c2c46f1fbe589f28b5ff91ee5605fa951026894b118f977aa01bbd0f34b67d" => :catalina
-    sha256 "7b973a504a2bdb3e39e63162d3ab40ee47dac5dd2a49eeb75a7cc2af95c0d38b" => :mojave
+    sha256 cellar: :any_skip_relocation, big_sur: "9acfff0beb0b3f16c4d5ce59aeec0d62d69020f9dd2dc3457f65fd56be640e60"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "67abc6b90563433086f1ee8579175828270501c6247497254aab6aba52665429"
+    sha256 cellar: :any_skip_relocation, catalina: "f21d5d3400bf1675f9b361491a7cc33d2edf779836f2e7d99454e0e8cc185c18"
+    sha256 cellar: :any_skip_relocation, mojave: "371646873ae44abe06dfe6637cac561b10c490be8326e6d77006da52cf50c54b"
   end
 
   def install
