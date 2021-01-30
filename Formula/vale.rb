95a8136e9252a60f8a83fe7dcb80b3d0577ca171
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-30 01:40:17 +0000

    vale: update 2.9.1 bottle.

--- Formula/vale.rb
+++ Formula/vale.rb
@@ -6,11 +6,10 @@ class Vale < Formula
   license "MIT"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "8687613a0848f629fd78f643f8520047d45b26d8ebba33b1337ecab3ad9ae0cb" => :big_sur
-    sha256 "f80fd5f1f81d8f98b029314199e981809a35b68a9f433c5fccdf139416a222f3" => :arm64_big_sur
-    sha256 "21379b4aac73c5140f8782eabe461cb2bc6bc03f7305ad5ad23ad054b3d29f7a" => :catalina
-    sha256 "58c5c4aa11d98d062285a8b7bb0f1a8f5067f3a303dbba4cb9a90bcaf665d74b" => :mojave
+    sha256 cellar: :any_skip_relocation, big_sur: "2392a04b032d4405ffa4db3abae9f72253d49f8b12a66dcb5c5a960ae5e4fb60"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "44ce5d217119a6d62792bb5e0b50654f128dce259e95f67680f0a617f3d1c436"
+    sha256 cellar: :any_skip_relocation, catalina: "7ddfb6f000d0b7f1b2f3ab2fed8ad0809bbe72845eb68d93828b1668057f2841"
+    sha256 cellar: :any_skip_relocation, mojave: "432767548946854640f80fe77fa0fe4751994163d4b32133df0a80a11b447609"
   end
 
   depends_on "go" => :build
