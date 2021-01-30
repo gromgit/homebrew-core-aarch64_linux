65647d6e1cdf33831738dd6dd286b4201fd3ecfc
Caleb Xu <calebcenter@live.com>
2021-01-30 10:30:01 -0500

    truffle 5.1.63 (new formula)
    Closes #70057.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

---
+++ Formula/truffle.rb
@@ -0,0 +1,22 @@
+require "language/node"
+
+class Truffle < Formula
+  desc "Development environment, testing framework and asset pipeline for Ethereum"
+  homepage "https://trufflesuite.com"
+  url "https://registry.npmjs.org/truffle/-/truffle-5.1.63.tgz"
+  sha256 "80425e794393b2b8a61b915d6f8389c5bc443524c9108a4f985269d1edec1612"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system bin/"truffle", "init"
+    system bin/"truffle", "compile"
+    system bin/"truffle", "test"
+  end
+end
