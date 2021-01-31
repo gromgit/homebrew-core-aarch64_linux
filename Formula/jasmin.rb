6fa53f78d337a45faa393de4be60c10e40c0d8e2
Jonathan Chang <me@jonathanchang.org>
2021-01-31 19:00:32 +1100

    jasmin: fix test

--- Formula/jasmin.rb
+++ Formula/jasmin.rb
@@ -3,6 +3,7 @@ class Jasmin < Formula
   homepage "https://jasmin.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/jasmin/jasmin/jasmin-2.4/jasmin-2.4.zip"
   sha256 "eaa10c68cec68206fd102e9ec7113739eccd790108a1b95a6e8c3e93f20e449d"
+  license "BSD-4-Clause"
   revision 2
 
   livecheck do
@@ -40,6 +41,6 @@ class Jasmin < Formula
       .end method
     EOS
     system "#{bin}/jasmin", "#{testpath}/test.j"
-    assert_equal "Hello Homebrew\n", shell_output("java HomebrewTest")
+    assert_equal "Hello Homebrew\n", shell_output("#{Formula["openjdk"].bin}/java HomebrewTest")
   end
 end
