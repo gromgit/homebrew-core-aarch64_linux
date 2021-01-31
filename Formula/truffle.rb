4676a79356e49284e6664911edb26014470b2785
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-01-31 08:53:46 +0000

    truffle: add 5.1.63 bottle.

--- Formula/truffle.rb
+++ Formula/truffle.rb
@@ -7,6 +7,13 @@ class Truffle < Formula
   sha256 "80425e794393b2b8a61b915d6f8389c5bc443524c9108a4f985269d1edec1612"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, big_sur: "3d1a5e925f9d727e194645e6a81011ea29335985af9e457a9d2ebae7f5edfe4e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "84151c7dfa2734e0ee1c5452ebe7ce8069368eb0fe7020f50a8250ca73f9a527"
+    sha256 cellar: :any_skip_relocation, catalina: "593d40a836e8a4839ba484ef3c611a6e2fcd576142ebe4589a41a60a5ab77b65"
+    sha256 cellar: :any_skip_relocation, mojave: "35efcc9d7d72e8ebeb49d331ede34f3247df0fad3023a14ad333076ff49ec2a9"
+  end
+
   depends_on "node"
 
   def install
