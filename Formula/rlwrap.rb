cd6ca5ca53a784a214bd5057fde6d1e33c48801a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-31 08:23:28 +0000

    rlwrap: update 0.44 bottle.

--- Formula/rlwrap.rb
+++ Formula/rlwrap.rb
@@ -12,12 +12,10 @@ class Rlwrap < Formula
   end
 
   bottle do
-    sha256 "d1569b44bcf5e3cbc42a6c9e456ad57a56cdf2906eab3a7ec837f5e83f3e838c" => :big_sur
-    sha256 "1749685e3fdae4734269eff4de64dcaf09d5aaa0664998f2f6061918d05167a6" => :arm64_big_sur
-    sha256 "d4a2ef8e6f2c143c9156ed301d9b835b74b8e1c8bb232feb7fd8494e5262441c" => :catalina
-    sha256 "1bd82e889c7f88df7b45b31201d563d7185c362c7a5d3814b5474d295de65f22" => :mojave
-    sha256 "d8d5c5402f1b0041bc26c2161b2822cfd025e93674a7a5dc0046185d5870181d" => :high_sierra
-    sha256 "3dd6233bbef1327553b5bd92d070c4d7bd24f5414628c1c367c4c3ad6f1acc76" => :sierra
+    sha256 big_sur: "8abc19f0485a74b99eaa365cc31928d3542162f3496738d9b263d86ec0424f16"
+    sha256 arm64_big_sur: "a28df3b1be0fc19dad13cf331050d506f523c1d0b6a9d640f43539579897e789"
+    sha256 catalina: "52fbaa3a697ba20bfab89f15025834ed5f3345c0584fe10aab466a65cd6b2aae"
+    sha256 mojave: "98a7899c374b6525e8a2d8e4a2f4f7a2fd0adc1e18a1f306fa5d4dd195fa151b"
   end
 
   depends_on "autoconf" => :build
