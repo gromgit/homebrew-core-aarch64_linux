e15b42232487449e856fb25cd569ee3f66080b0d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-31 04:26:57 +0000

    imgproxy: update 2.15.0_2 bottle.

--- Formula/imgproxy.rb
+++ Formula/imgproxy.rb
@@ -8,12 +8,10 @@ class Imgproxy < Formula
   head "https://github.com/imgproxy/imgproxy.git"
 
   bottle do
-    cellar :any
-    sha256 "ccd0a102ec452a68a26280f0173534784d0a7797de833bafbc2015027634fc0a" => :big_sur
-    sha256 "215159ed8fcf0216e0f81cb38c460316349828680d5376358c21ba6ee77138b2" => :arm64_big_sur
-    sha256 "8cefa46ab480ea6b763077f8d12b0783e9482355aa2b1bf99ee2c8126279f8c6" => :catalina
-    sha256 "ca7688ab9b51b8f6e71d664bd210ccacefd66c08f16fce12fd26b89b8017358e" => :mojave
-    sha256 "7572ce24b608a282fc82231c0d2455f03dcc15953df73de4f302bf11375fb39c" => :high_sierra
+    sha256 cellar: :any, big_sur: "808ea1ab203cd442649754abdbf872a7d362d23e0146e6decb5922c6004c8622"
+    sha256 cellar: :any, arm64_big_sur: "4c90b8a7f56a51076160771080c7f6616c1fa4480c33ffd49d6454a043f506f4"
+    sha256 cellar: :any, catalina: "6c093d73221961f2ee0a588f4992c6fe587cdcb0c3a6a3266df0218ec1f44fca"
+    sha256 cellar: :any, mojave: "0f2f4d88aeb190bb6aa9169e4d7918de639cccad263cd2da93468bd0c6194136"
   end
 
   depends_on "go" => :build
