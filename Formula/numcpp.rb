9ee9093b65f0a185579331c61b1053358bc6c5a9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-01-31 19:23:40 +0000

    numcpp: update 2.4.0 bottle.

--- Formula/numcpp.rb
+++ Formula/numcpp.rb
@@ -7,12 +7,10 @@ class Numcpp < Formula
   head "https://github.com/dpilger26/NumCpp.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "cac192f81e747be1de8738a455fe0434aa326730bd898b1ca3d7128506f199c5" => :big_sur
-    sha256 "4cf1e18e75ebfcb953d41eda2e63bd4861ac70a60797cff9873c2bca07a7f527" => :arm64_big_sur
-    sha256 "45d1e6f87c6c253c5cd1a5b75247c2433dcbb4fe4ac5ecb6c2ac1d0d53a9f231" => :catalina
-    sha256 "830cc9312d29337ac066b1861ced8846a54aeddbc7d6401796ee6490fe4881b3" => :mojave
-    sha256 "e68ae1eb4314455e0efa51c8a56a2afbf18f3a8c1987b8b587611fe36966fa65" => :high_sierra
+    sha256 cellar: :any_skip_relocation, big_sur: "8f553497c1071941948df03b492c1545b0e7c78993ef7319880e11860da7744e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "dc65d6ab452da76bc43ea0bc472adfcc6e34c868e907c6bb7b1c07d6e4c0ae9d"
+    sha256 cellar: :any_skip_relocation, catalina: "f87c93639d7fa92051882b55f0878d3bd6ca4ec0d7fced6432bef13280869271"
+    sha256 cellar: :any_skip_relocation, mojave: "c93e65a45a86781f6f7f3d86b80e616ac57c2f79e1ecff27b97dfe549d63c5f7"
   end
 
   depends_on "cmake" => :build
