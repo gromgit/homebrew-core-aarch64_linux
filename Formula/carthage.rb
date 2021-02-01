5be583636e486fb694c9b84d0783265002685394
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-01 17:46:39 +0000

    carthage: update 0.37.0 bottle.

--- Formula/carthage.rb
+++ Formula/carthage.rb
@@ -9,11 +9,10 @@ class Carthage < Formula
   head "https://github.com/Carthage/Carthage.git", shallow: false
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "67de3116e7bbb3e138070146194be5988d0ba74037b551ecaa39bb36c94650f9" => :big_sur
-    sha256 "ce689a0d6f78a4c6626de681d91cb38adc302c47381d4cc9e560b4ef7abe9a62" => :arm64_big_sur
-    sha256 "ea7058f92d8ce4b4d42b466cf32b9b65a9296174ec9028190f82b6149b5b44da" => :catalina
-    sha256 "3887e31cb3f75087bd9a8a83b84f194add534ef039039ea14b4843adc50bf82d" => :mojave
+    sha256 cellar: :any_skip_relocation, big_sur: "0770b4dd885f3018031c2d27fc090a34027d5856a248f33fa2a415d58da74632"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "cd0c716682b5b094b82a589fb79def4eb696f70a3fd92423923a5cb86c2c79b3"
+    sha256 cellar: :any_skip_relocation, catalina: "8a07c198835cb179d4054313b199ce126e64bb9414eaaa91f55162a4aed63134"
+    sha256 cellar: :any_skip_relocation, mojave: "7fb777ac169aa4cb05683f0f8bfb5b56dbb0b0e8b673df995ef2fb2bbe0d90d2"
   end
 
   depends_on xcode: ["10.0", :build]
