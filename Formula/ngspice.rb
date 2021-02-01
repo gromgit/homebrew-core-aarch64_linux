21ccecb988f435538d75d9e24dcb510c45867134
rui <rui@chenrui.dev>
2021-02-01 04:33:15 +0000

    ngspice: update 34 bottle.

--- Formula/ngspice.rb
+++ Formula/ngspice.rb
@@ -10,11 +10,10 @@ class Ngspice < Formula
   end
 
   bottle do
-    sha256 "b00fa22144c9add0b27a0c169e51877fb680ae309f0b339043898cb7c1be8a22" => :big_sur
-    sha256 "1e9280810ac216eefca4dbff61d8ed764feca6c9c4103195f3894eae0918b25e" => :arm64_big_sur
-    sha256 "947aa81277b9bfdfe5b9a52ba502f146cb2c7e5125e88770255a40f6799685bd" => :catalina
-    sha256 "0d880665db391a48e55b133b6fea6cbf7f0e56c0428045b4d046b03dfff95685" => :mojave
-    sha256 "9f048cc3f136b84cfd2f76afe91f39d7bf844c00e442a9fbb7363f84fa44a8c7" => :high_sierra
+    sha256 big_sur: "6f8f86cccb6242f21a09359f1009344acadcd7d7726766327019e4e5a0ad5655"
+    sha256 arm64_big_sur: "b7e6619a5f7f1b951b0464d4fa18cba7a5d933ec85eccbeb47c67cf22f817520"
+    sha256 catalina: "9dd5b4b1e8164e5b35729838237e68c61d44c94c3aa5209e2613e9ab659f3e0d"
+    sha256 mojave: "0003f68a5390917cd6e0c6b5179a22b02f6fccd0b1f4a359ad17cdd831c456ad"
   end
 
   head do
