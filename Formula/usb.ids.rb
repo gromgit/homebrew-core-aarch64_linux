3451fab821f9f71016745ed5cd7ed3bc85461805
Caleb Xu <calebcenter@live.com>
2021-02-01 12:13:40 -0500

    usb.ids 2021.01.29 (#70156)

--- Formula/usb.ids.rb
+++ Formula/usb.ids.rb
@@ -1,8 +1,8 @@
 class UsbIds < Formula
   desc "Repository of vendor, device, subsystem and device class IDs used in USB devices"
   homepage "http://www.linux-usb.org/usb-ids.html"
-  url "https://deb.debian.org/debian/pool/main/u/usb.ids/usb.ids_2021.01.26.orig.tar.xz"
-  sha256 "5108c81ab11865a754afeaeff4cfaac72b5c99e561e77079fb8b3a67b077edf7"
+  url "https://deb.debian.org/debian/pool/main/u/usb.ids/usb.ids_2021.01.29.orig.tar.xz"
+  sha256 "1c56660f3fe0d362469b5fae2e4ab86fc035911a2581faf4e7d5affb3de9f1af"
   license any_of: ["GPL-2.0-or-later", "BSD-3-Clause"]
 
   livecheck do
