f794e7754c83ef6ca17ebc8e6dfe53527d750f93
Nick Sieger <nick@nicksieger.com>
2021-02-01 13:21:54 -0600

    brew-gem 1.1.0 (#70171)

--- Formula/brew-gem.rb
+++ Formula/brew-gem.rb
@@ -1,8 +1,8 @@
 class BrewGem < Formula
   desc "Install RubyGems as Homebrew formulae"
   homepage "https://github.com/sportngin/brew-gem"
-  url "https://github.com/sportngin/brew-gem/archive/v1.0.0.tar.gz"
-  sha256 "bc321722746e82243be50bc999d02f3d0754d40e7e40552ed2d61103191c82cf"
+  url "https://github.com/sportngin/brew-gem/archive/v1.1.0.tar.gz"
+  sha256 "c968b767b1d73d4b4abbf263a4e102796896d6439dca42f71c8650166ad6f63d"
   license "MIT"
   head "https://github.com/sportngin/brew-gem.git"
 
