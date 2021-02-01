ea3149fb685160439dcb7dd309acbd67078c1cee
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-01 18:09:48 +0000

    gawk: update license (#70165)

--- Formula/gawk.rb
+++ Formula/gawk.rb
@@ -4,7 +4,7 @@ class Gawk < Formula
   url "https://ftp.gnu.org/gnu/gawk/gawk-5.1.0.tar.xz"
   mirror "https://ftpmirror.gnu.org/gawk/gawk-5.1.0.tar.xz"
   sha256 "cf5fea4ac5665fd5171af4716baab2effc76306a9572988d5ba1078f196382bd"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
 
   livecheck do
     url :stable
