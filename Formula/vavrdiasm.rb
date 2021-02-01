0e594418ed67ae243310c951cada373f983a6c4f
Sam Ford <1584702+samford@users.noreply.github.com>
2021-02-01 12:37:14 -0500

    vavrdiasm: update disable date and license

--- Formula/vavrdiasm.rb
+++ Formula/vavrdiasm.rb
@@ -3,7 +3,7 @@ class Vavrdiasm < Formula
   homepage "https://github.com/vsergeev/vAVRdisasm"
   url "https://github.com/vsergeev/vavrdisasm/archive/v3.1.tar.gz"
   sha256 "4fe5edde40346cb08c280bd6d0399de7a8d2afdf20fb54bf41a8abb126636360"
-  license "GPL-3.0"
+  license "GPL-3.0-only"
 
   bottle do
     cellar :any_skip_relocation
@@ -16,7 +16,7 @@ class Vavrdiasm < Formula
     sha256 "f881c5a6d94581c4fc9efb13118c84c40700f13d130302f6ee4cb16968d1f6b0" => :mavericks
   end
 
-  disable! date: "2020-12-08", because: :unmaintained
+  disable! date: "2020-09-17", because: :unmaintained
 
   # Patch:
   # - BSD `install(1)' does not have a GNU-compatible `-D' (create intermediate
