aac7ab3b3a3ed3aecdffab8e6427b07c1da18e90
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-02 20:36:23 +0000

    libgccjit: fix bugurl style

--- Formula/libgccjit.rb
+++ Formula/libgccjit.rb
@@ -58,7 +58,7 @@ class Libgccjit < Formula
       --with-isl=#{Formula["isl"].opt_prefix}
       --with-system-zlib
       --with-pkgversion=#{pkgversion}
-      --with-bugurl=https://github.com/Homebrew/homebrew-core/issues
+      --with-bugurl=#{tap.issues_url}
     ]
 
     # Xcode 10 dropped 32-bit support
