e28dc02a547e9b64613a0b5ae6963d7e8d2440a9
Alexander M. Turek <me@derrabus.de>
2021-02-02 18:04:35 +0100

    phpunit 9.5.2 (#70222)

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -1,8 +1,8 @@
 class Phpunit < Formula
   desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-9.5.1.phar"
-  sha256 "062f128573e120c8894086b50b60a89971833834e040cfb9358eaa22224e68ac"
+  url "https://phar.phpunit.de/phpunit-9.5.2.phar"
+  sha256 "bcf913565bc60dfb5356cf67cbbccec1d8888dbd595b0fbb8343a5019342c67c"
   license "BSD-3-Clause"
 
   livecheck do
