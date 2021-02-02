31de52ea52b4f9ec629a7dd6665baf280d438d07
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2021-02-02 21:42:40 +0000

    msitools 0.101
    Closes #70260.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>

--- Formula/msitools.rb
+++ Formula/msitools.rb
@@ -1,9 +1,9 @@
 class Msitools < Formula
   desc "Windows installer (.MSI) tool"
   homepage "https://wiki.gnome.org/msitools"
-  url "https://download.gnome.org/sources/msitools/0.100/msitools-0.100.tar.xz"
-  sha256 "bbf1a6e3a9c2323b860a3227ac176736a3eafc4a44a67346c6844591f10978ea"
-  license "GPL-2.0"
+  url "https://download.gnome.org/sources/msitools/0.101/msitools-0.101.tar.xz"
+  sha256 "0cc4d2e0d108fa6f2b4085b9a97dd5bc6d9fcadecdd933f2094f86bafdbe85fe"
+  license "LGPL-2.1-or-later"
 
   livecheck do
     url :stable
@@ -17,35 +17,23 @@ class Msitools < Formula
     sha256 "fd8689ba0902ed4d784f85969d281a0e1c58bb76f0fe17a93d96ba2d3f845cdb" => :high_sierra
   end
 
-  depends_on "intltool" => :build
+  depends_on "bison" => :build
+  depends_on "gobject-introspection" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "e2fsprogs"
   depends_on "gcab"
   depends_on "gettext"
   depends_on "glib"
   depends_on "libgsf"
   depends_on "vala"
 
-  # Workaround for https://gitlab.gnome.org/GNOME/msitools/issues/15
-  # Merged upstream in the following commits:
-  # https://gitlab.gnome.org/GNOME/msitools/commit/248450a2f2a23df59428fa816865a26f7e2496e0
-  # https://gitlab.gnome.org/GNOME/msitools/commit/9bbcc6da06ccf6144258c26ddcaab3262538d3ce
-  # Remove in next release.
-  patch do
-    url "https://gitlab.gnome.org/GNOME/msitools/commit/248450a2f2a23df59428fa816865a26f7e2496e0.patch"
-    sha256 "32bf8c2995085c2751c3fe8cd67878ea28b2ee255f5d00ed3ea7c5fddea3d902"
-  end
-
-  patch do
-    url "https://gitlab.gnome.org/GNOME/msitools/commit/9bbcc6da06ccf6144258c26ddcaab3262538d3ce.patch"
-    sha256 "5178df1577a967e887a859fe1a6c791071712e3acfc6f898e1e83352b7336a9a"
-  end
-
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    system "make", "install"
+    mkdir "build" do
+      system "meson", *std_meson_args, "-Dintrospection=true", ".."
+      system "ninja"
+      system "ninja", "install"
+    end
   end
 
   test do
