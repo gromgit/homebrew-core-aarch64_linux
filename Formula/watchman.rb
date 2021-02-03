27aeb30388fe140b25b5eb7263914a71e367f014
Sam Ford <1584702+samford@users.noreply.github.com>
2021-02-03 14:55:22 -0500

    watchman: update livecheck

--- Formula/watchman.rb
+++ Formula/watchman.rb
@@ -19,7 +19,7 @@ class Watchman < Formula
   # The Git repo contains a few tags like `2020.05.18.00`, so we have to
   # restrict matching to versions with two to three parts (e.g., 1.2, 1.2.3).
   livecheck do
-    url :head
+    url :stable
     regex(/^v?(\d+(?:\.\d+){,2})$/i)
   end
 
