b9114a201988bf149841db19089d5c678c392953
Dustin Rodrigues <dust.rod@gmail.com>
2021-02-02 21:36:43 -0500

    babel 7.12.13
    * babel 7.12.13
    * babel: update cli
    
    Closes #70274.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/babel.rb
+++ Formula/babel.rb
@@ -4,8 +4,8 @@ require "json"
 class Babel < Formula
   desc "Compiler for writing next generation JavaScript"
   homepage "https://babeljs.io/"
-  url "https://registry.npmjs.org/@babel/core/-/core-7.12.10.tgz"
-  sha256 "77478bc9855682349a40ecc3ff4f00329f47989dcca3f6379a7585201fecb6dd"
+  url "https://registry.npmjs.org/@babel/core/-/core-7.12.13.tgz"
+  sha256 "a128a4fa43e5b9032c709caaaf9b93a166a9bb7f7e78dbaff689a76e48900cdb"
   license "MIT"
 
   livecheck do
@@ -23,8 +23,8 @@ class Babel < Formula
   depends_on "node"
 
   resource "babel-cli" do
-    url "https://registry.npmjs.org/@babel/cli/-/cli-7.12.10.tgz"
-    sha256 "37dce2d25c075206996913da0dab9970d420bb171f83f495ccbeba2161e88dab"
+    url "https://registry.npmjs.org/@babel/cli/-/cli-7.12.13.tgz"
+    sha256 "4c1b4b189e4d2f68d891a33aeac7f74531a004685dde6d102e4aad357553dc3e"
   end
 
   def install
