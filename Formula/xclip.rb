252f41dfeccc33f03543b746a56f8b63ca9c5f08
Jonathan Chang <me@jonathanchang.org>
2021-02-03 12:17:03 +1100

    xclip: update description

--- Formula/xclip.rb
+++ Formula/xclip.rb
@@ -1,5 +1,5 @@
 class Xclip < Formula
-  desc "Command-line utility that is designed to run on any system with an X11"
+  desc "Access X11 clipboards from the command-line"
   homepage "https://github.com/astrand/xclip"
   url "https://github.com/astrand/xclip/archive/0.13.tar.gz"
   sha256 "ca5b8804e3c910a66423a882d79bf3c9450b875ac8528791fb60ec9de667f758"
