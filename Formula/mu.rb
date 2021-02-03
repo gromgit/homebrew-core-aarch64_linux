87f4c4759393b7f0dbea405cebc3cb408d20c6ad
Sam Ford <1584702+samford@users.noreply.github.com>
2021-02-03 14:55:07 -0500

    mu: update livecheck

--- Formula/mu.rb
+++ Formula/mu.rb
@@ -12,7 +12,7 @@ class Mu < Formula
   # as an odd-numbered minor version number indicates a development version:
   # https://github.com/djcb/mu/commit/23f4a64bdcdee3f9956a39b9a5a4fd0c5c2370ba
   livecheck do
-    url :head
+    url :stable
     regex(/^v?(\d+\.\d*[02468](?:\.\d+)*)$/i)
   end
 
