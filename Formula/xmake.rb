3b97338fc01697a1965b5cc3124ca28ad8d82335
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-03 13:24:33 +0000

    xmake: update 2.5.1 bottle.

--- Formula/xmake.rb
+++ Formula/xmake.rb
@@ -7,11 +7,11 @@ class Xmake < Formula
   head "https://github.com/xmake-io/xmake.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "1eaa98c1e4392635f05c444b1056c93ee925299fa0bb2179b3ca9a16c9a39fb0" => :big_sur
-    sha256 "36493b07ef38543e274d0d0e1d91a03b76ad2122cf733229f193c6c4b0150592" => :arm64_big_sur
-    sha256 "82c6849b806ac4b28e916bf46d958c3d9de9de2a14161e5f6b240f7ab0064705" => :catalina
-    sha256 "a230d9772bba3647568fc6c5fdb2c2d129c3ff35deaa78e530c498f39fed58f8" => :mojave
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, big_sur: "1056810f311df733789d9bcdd6a8381019db4f620ef2ac0e684df45048123d6c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7031171595fd4a98fadeff92ed6310e18f88220d9fb2852a170a0c952b7fea30"
+    sha256 cellar: :any_skip_relocation, catalina: "af687b62d4a9d802d91ce6793c1b186d31aef77baeea1bf873b2accae8205f0d"
+    sha256 cellar: :any_skip_relocation, mojave: "832eef81f79a01ed8af0cfe6a75ea5dc3ecff56edb60c42ea0991809ec80b49b"
   end
 
   on_linux do
