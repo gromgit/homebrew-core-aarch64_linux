e50ec98aed66526dd71960fe515c6c1a9f45c06c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-03 10:19:27 +0000

    aliddns: update 0.0.9 bottle.

--- Formula/aliddns.rb
+++ Formula/aliddns.rb
@@ -8,11 +8,10 @@ class Aliddns < Formula
   head "https://github.com/OpenIoTHub/aliddns.git"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "b91312e27065e23f3444144d9e1dddcad8337c6c60d3e597235089befb50dcc7" => :big_sur
-    sha256 "93f5b40f2357c7b904c52ea10f8bba2924143e2e8214670037db5435ac2e3108" => :arm64_big_sur
-    sha256 "868f0972a9c769d8854cf4b9d86b0c1ac75c542764054f867d909ddc9b091b1d" => :catalina
-    sha256 "3eaeedd4962169e2ac8660fdccfa628c389e01e00b9435dda1adebdb18488a70" => :mojave
+    sha256 cellar: :any_skip_relocation, big_sur: "cb06193d5ebd8d4faa2e358f6d40589653a0a593e7265e21dfc15bfc99816cab"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "16e5bfb49b749adb380450cde6092020afd9ef4d6ed3fa030c8c6ede11a6bf9d"
+    sha256 cellar: :any_skip_relocation, catalina: "5bb2c74ba522eaf8713fb5c04b5e726e0bca16c4cfc406d7b4523187646acfdb"
+    sha256 cellar: :any_skip_relocation, mojave: "a5f4c51abd1deb28e02f1dc97ea2fb4bcde0d297e0da9d6c5a3b4cfb0bf6c135"
   end
 
   depends_on "go" => :build
