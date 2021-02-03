e890378a4ab622bf9440de274556245113aa6c80
Sam Ford <1584702+samford@users.noreply.github.com>
2021-02-03 14:54:54 -0500

    dosbox-x: update livecheck

--- Formula/dosbox-x.rb
+++ Formula/dosbox-x.rb
@@ -8,7 +8,7 @@ class DosboxX < Formula
   head "https://github.com/joncampbell123/dosbox-x.git"
 
   livecheck do
-    url :head
+    url :stable
     regex(/^dosbox-x[._-]v?(\d+(?:\.\d+)+)$/i)
   end
 
