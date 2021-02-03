219806843c4416a8db86684c1197ad57cb9f1081
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-03 08:21:59 +0000

    gdb: update 10.1_1 bottle.

--- Formula/gdb.rb
+++ Formula/gdb.rb
@@ -13,10 +13,9 @@ class Gdb < Formula
   end
 
   bottle do
-    sha256 "3a9d6b7bfe4530321ab858ca8817431a28010ffbdbe5b0db1d700c81a392c91f" => :big_sur
-    sha256 "fbd7836617ad0bb335ad9175280f14162dfdf0dd2f5e6b634d37b2d93765b51e" => :catalina
-    sha256 "f3656f04b8725825296ce977461af0d0e6133ec9a1f9086ca3b889661f40802e" => :mojave
-    sha256 "a57db00038cdcdc85f80c064332f3b4ef45571a2c0cd0defa55f2e6bde6a751d" => :high_sierra
+    sha256 big_sur: "e091e16129311d4c9c444e415dc8f8cd58f0ba2f3647952cab242c32c049426e"
+    sha256 catalina: "33e0973094baeb29eeeb29472a8534e11f8c870e94864777ab3d66a1ba68416f"
+    sha256 mojave: "e9629327e54e84b14ae2c3f54712376428f6c62e070760e6c62b0bdf83f691af"
   end
 
   depends_on "python@3.9"
