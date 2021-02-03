200f491019fd5fd15a78c006a001c380f04bf255
Dustin Rodrigues <dust.rod@gmail.com>
2021-02-03 03:09:32 +0000

    flux: update 0.104.0 bottle.

--- Formula/flux.rb
+++ Formula/flux.rb
@@ -13,10 +13,10 @@ class Flux < Formula
   end
 
   bottle do
-    sha256 cellar: :any, big_sur: "01176f94a74d94c33d1c4b46e337239c1771923b1997b92d9e48b5f3f828e8c5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "cdc96323915a1a9517058316ae98554d22106c4f8ba7264d4db870b9579e9b1b"
-    sha256 cellar: :any, catalina: "fc0be0dc6d16e47168830aa3f19df62a8d32b7545ef49c308f50c5254e1129c8"
-    sha256 cellar: :any, mojave: "506742a4de59cf9f68504934744ff8cc4fd1e391ebebf9aa735b532e67194982"
+    sha256 cellar: :any, big_sur: "11c834d452e4ad81e8f43a76c785ef641440969916e425c60dab4edb383741db"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "67d12f917aa7cbdfc842e31dd45e2103a13b7c42bb523dcc955bead4abf43d70"
+    sha256 cellar: :any, catalina: "8ae9c9eb21a4a6ef5ac62afff99b6acd60b827be79ff5e70c0d469ce0af2a944"
+    sha256 cellar: :any, mojave: "43858d6b6f0d2f47eb95c1d0e47b3b8ce9dff3f316246f1636954b12f77cac0d"
   end
 
   depends_on "go" => :build
