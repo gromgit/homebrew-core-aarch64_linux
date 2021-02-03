36f4ee3cceb87d48f2787789616886df3a0ada07
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-03 22:18:07 +0000

    fetch: update 0.3.14 bottle.

--- Formula/fetch.rb
+++ Formula/fetch.rb
@@ -7,10 +7,10 @@ class Fetch < Formula
   head "https://github.com/gruntwork-io/fetch.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur: "65120b76f68775953d031c64ce61534a9cdf4e1147472a36eb3a26efd361c3bd"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8f7e8e5825e53aa7a94aa4f8b1d644b96e905aa5aa36801fd7ec3079b4939453"
-    sha256 cellar: :any_skip_relocation, catalina: "b8fb96174cd37b851c82fd778a3e8ddf05f38e4e5dac2bd83c105a14ea5999b7"
-    sha256 cellar: :any_skip_relocation, mojave: "b82c58ff4af5d06d99bf4f0a665579a416280a7f5df57811f7ebe49b595baded"
+    sha256 cellar: :any_skip_relocation, big_sur: "61109bd1f647acef7cb68a09cfd4a8fa04053dfff90a3be32deb8027ee6b06f7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "678a2b7373830d0dc64ca9a335706f0638c810298094b8ceb2eb7dff0db973f4"
+    sha256 cellar: :any_skip_relocation, catalina: "194dba2e7ccdaf0691b3b17146ac1ab77088251823b143fe9cbcf3d790ca2a59"
+    sha256 cellar: :any_skip_relocation, mojave: "544a2cc87fed7545d1e220954b445a6519c47662cd41b64c9ff8ec9f0fd0742b"
   end
 
   depends_on "go" => :build
