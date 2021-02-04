9015ff807b0aa1ec28e7ec1b8a3eea2d823053a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-04 18:19:06 +0000

    php@7.4: update 7.4.15 bottle.

--- Formula/php@7.4.rb
+++ Formula/php@7.4.rb
@@ -8,10 +8,10 @@ class PhpAT74 < Formula
   license "PHP-3.01"
 
   bottle do
-    sha256 arm64_big_sur: "8cae32443624f408470a0a0f5b98d6a6273496c87052fec7bb050d85f1ed43ef"
-    sha256 big_sur:       "328f1afb2d59eeadd9f9754ec71ba80b7323fcd67b9758a5728672322d2cd638"
-    sha256 catalina:      "8e7b09cc26c8a5162b1601c352362436602c75cdbae6fdd0b193994701a9e66d"
-    sha256 mojave:        "a36b62f17251fd341a8008dc0c5c174c2cc893e7320300900e7d8ef4b4d41dbc"
+    sha256 big_sur:       "11cbe697525464636f1e1ecc9de2900b341e8566a0b66239ab05d57271f85076"
+    sha256 arm64_big_sur: "3120baa405c79acaae9622e4c99599d8fe09ccdbaeecb8965d131c9559c257f1"
+    sha256 catalina:      "09d8e3bd11eea8df1020621ba7e9d5e7d540a7042372879893a070ab09c3ca37"
+    sha256 mojave:        "9e023ddc63e4269895fb520acc3b21e6619028a94e55706f52d3e998978e57ec"
   end
 
   keg_only :versioned_formula
