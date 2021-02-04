13421f3a1f979f2353e6c9984c51a91690e9384d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-04 16:55:41 +0000

    kubecm: update 0.15.0 bottle.

--- Formula/kubecm.rb
+++ Formula/kubecm.rb
@@ -6,10 +6,10 @@ class Kubecm < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5bf8d656174d2639ccd9e34941980a8ff80c7542a911ad2131866a80aa6632b7"
-    sha256 cellar: :any_skip_relocation, big_sur:       "7757369261a5c7d40cdb04142db6611f0380b98ac2af586f21844dc1732802c3"
-    sha256 cellar: :any_skip_relocation, catalina:      "399db5e8bb27e86ac3a419cf501bdba2375a71f043f329cea4f96d3577fc62df"
-    sha256 cellar: :any_skip_relocation, mojave:        "982f00093846cd65e112049e1c86602da029b6d2b05c56f78a74c705de3039d3"
+    sha256 cellar: :any_skip_relocation, big_sur:       "30d33110dd11731e16a97cd0051da789097693b776a6840d9ace343641416f9d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9c063e2246b4b2a33ca1289fa2f425e426d59371d306dba91c3aad3df65bae69"
+    sha256 cellar: :any_skip_relocation, catalina:      "8d06b3ea72107fd7577048d5490aeaf35f6fa570ee84e18cfe95ac3ea5c7a822"
+    sha256 cellar: :any_skip_relocation, mojave:        "89760c4372178e1cc4d59380ca90e76e4c9d43d037869d8420cc55ad03540887"
   end
 
   depends_on "go" => :build
