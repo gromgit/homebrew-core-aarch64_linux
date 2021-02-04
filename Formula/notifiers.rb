d7b349dedb7fdb5400ef2d0d7cfb40e0a1371a71
Dustin Rodrigues <dust.rod@gmail.com>
2021-02-04 07:53:35 -0500

    notifiers: add license (#70360)

--- Formula/notifiers.rb
+++ Formula/notifiers.rb
@@ -5,6 +5,7 @@ class Notifiers < Formula
   homepage "https://pypi.org/project/notifiers/"
   url "https://files.pythonhosted.org/packages/4f/36/4c300f55949b9be84284d51253ae48d564dc2c4f2bffb94f26c8c1485f07/notifiers-1.2.1.tar.gz"
   sha256 "34625af405f4aa19293eaaefe145ccc92c6018ae9798f53a03a7fcc996e541aa"
+  license "MIT"
   revision 3
 
   livecheck do
