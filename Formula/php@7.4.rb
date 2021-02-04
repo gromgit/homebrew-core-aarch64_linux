cd9004fc237f14134c13fff079b6110d70905bfb
Alexander M. Turek <me@derrabus.de>
2021-02-04 12:38:31 +0100

    php@7.4 7.4.15
    Closes #70394.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/php@7.4.rb
+++ Formula/php@7.4.rb
@@ -2,11 +2,10 @@ class PhpAT74 < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
   # Should only be updated if the new version is announced on the homepage, https://www.php.net/
-  url "https://www.php.net/distributions/php-7.4.14.tar.xz"
-  mirror "https://fossies.org/linux/www/php-7.4.14.tar.xz"
-  sha256 "f9f3c37969fcd9006c1dbb1dd76ab53f28c698a1646fa2dde8547c3f45e02886"
+  url "https://www.php.net/distributions/php-7.4.15.tar.xz"
+  mirror "https://fossies.org/linux/www/php-7.4.15.tar.xz"
+  sha256 "9b859c65f0cf7b3eff9d4a28cfab719fb3d36a1db3c20d874a79b5ec44d43cb8"
   license "PHP-3.01"
-  revision 1
 
   bottle do
     sha256 arm64_big_sur: "8cae32443624f408470a0a0f5b98d6a6273496c87052fec7bb050d85f1ed43ef"
