fe73b8cc79984f8c74d22b2c818f1ae9998bee97
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-04 15:41:58 +0000

    xterm: update 365 bottle.

--- Formula/xterm.rb
+++ Formula/xterm.rb
@@ -12,10 +12,10 @@ class Xterm < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "041ea0a035b63c3ff2d07ea5fb48f433ad5e217313043aac9e7382b9b6a84000"
-    sha256 big_sur:       "88dd9c054cc9987d5d8f1117af8e1be5a38823a3cbb500f0fbba600ceea18ebe"
-    sha256 catalina:      "68c40fc4958dea9eab534a07cc7cba9f92f39212f9a2dc698d8913a0f8329e14"
-    sha256 mojave:        "adf66a50385caabbe943c4316f20ba874eff91af8d4816d5068e7617e97ed307"
+    sha256 big_sur:       "c60885eba8c6f0cf1197793dd0fb82ec3cfec9aa7e234e90920cf4cf3fe62b7b"
+    sha256 arm64_big_sur: "feedc30fa7545317fcd21891a8107287d3141454266b316ae3316874e545a3de"
+    sha256 catalina:      "5d74ee6b4b620a1d7193686d1050c901f65219ecd0e36abcabdabd9c2959268f"
+    sha256 mojave:        "ab6cf49a29e28028be468d0a1dc9bae2e832f9547b4d489aebe6eef5ede2ee60"
   end
 
   depends_on "fontconfig"
