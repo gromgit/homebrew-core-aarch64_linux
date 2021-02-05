628333afeef7f4250763e7bf5826cee1cbeb9955
Yuuki Ebihara <yuuki.ebihara813@gmail.com>
2021-02-06 04:59:56 +0900

    closure-compiler new homepage

--- Formula/closure-compiler.rb
+++ Formula/closure-compiler.rb
@@ -1,6 +1,6 @@
 class ClosureCompiler < Formula
   desc "JavaScript optimizing compiler"
-  homepage "https://github.com/google/closure-compiler"
+  homepage "https://developers.google.com/closure/compiler"
   url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20210202/closure-compiler-v20210202.jar"
   sha256 "1f1a8d2348857d70f5092618db3f2141d53621b10ee665c82b3a7925edeef14b"
   license "Apache-2.0"
