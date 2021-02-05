94a26a582c0e80b5c4bab95b0dca08291bfba41d
FX Coudert <fxcoudert@gmail.com>
2021-02-05 07:01:41 +0000

    lmdb: update 0.9.28 bottle.

--- Formula/lmdb.rb
+++ Formula/lmdb.rb
@@ -13,11 +13,10 @@ class Lmdb < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "706f2a266c430ad3cffcbf5bc9e87a6d1e63a9eb7f2d74b3b95c28e8910cd71d"
-    sha256 cellar: :any, big_sur:       "6244986c2cd93bd2770fc0cfac79f80e9d98ea197d84acce00322335fe9e2e18"
-    sha256 cellar: :any, catalina:      "1495b5b154e77771ff450d1687c2afeec377db57498900b2bb692d23b4bd25b8"
-    sha256 cellar: :any, mojave:        "496f41bc0f050e5657a4ecc1409fccf8f2247521c3d3ebe16bdeb2007e0dbc71"
-    sha256 cellar: :any, high_sierra:   "36d2564ec79b8547154e706980fbc25d38575d424299216ec10adae598e6b1c9"
+    sha256 cellar: :any, arm64_big_sur: "e841311d68877f2fb3d1ed488b33ce07c1d4b164babd5bbe6552c4f01765fce1"
+    sha256 cellar: :any, big_sur:       "1fe7ed983f30164a0558431649e1b6f86b28dc36660c0ffc215db205f85bbe39"
+    sha256 cellar: :any, catalina:      "cb42f85558a8825f775ae80c79a9f5f7ed015d0d4f46f91eba5a4f61474a8b99"
+    sha256 cellar: :any, mojave:        "9b1e6d9b6436d526e1798e132d1529366511d18b5c7a8547336cbd3f1683121f"
   end
 
   def install
