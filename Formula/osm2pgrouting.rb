4a781c5641f0057b0bf0204149890c656d78cc23
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-05 01:18:11 +0000

    osm2pgrouting: update 2.3.7 bottle.

--- Formula/osm2pgrouting.rb
+++ Formula/osm2pgrouting.rb
@@ -7,10 +7,9 @@ class Osm2pgrouting < Formula
   head "https://github.com/pgRouting/osm2pgrouting.git"
 
   bottle do
-    sha256 cellar: :any, big_sur:     "5e935d46ae6532b2cd75b73ebaf13bcbf25472e29e0281236fdd002c3317f3bd"
-    sha256 cellar: :any, catalina:    "99b349358db3adfb8a52dfcd2cfde049cecd5cea0b46cc6fa2a4fb6e2e9b4523"
-    sha256 cellar: :any, mojave:      "c99a46012e8a582678e3b28e73f8c570606477d2b76d125dee5070fde5516b5c"
-    sha256 cellar: :any, high_sierra: "1ec262b471c1dd25b7609b5c36b53798375fb5d1015eb0d968a8023a698ed113"
+    sha256 cellar: :any, big_sur:  "b3a5695ec0da3196e6a0db7fff372f4792e9b1afc8b1cd2f7608973deba24916"
+    sha256 cellar: :any, catalina: "fc5653e1d1c6f002603fb38aa117260feff5c4784e0c5cb04d9cc0a72f331d0d"
+    sha256 cellar: :any, mojave:   "1a09c4bd948c742744f6dc377cbacf03281ae0bd705341b05ada202e540ef43d"
   end
 
   depends_on "cmake" => :build
