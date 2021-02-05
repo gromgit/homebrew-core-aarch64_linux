31a6bb2df897cd818f4cce0fc4c892585bfa9f55
Misty De Meo <mistydemeo@gmail.com>
2021-02-05 02:16:34 -0800

    ssh-copy-id: backport upstream patch (#70483)
    Without this, certain situations will lead to the error
    `EOF: command not found`.

--- Formula/ssh-copy-id.rb
+++ Formula/ssh-copy-id.rb
@@ -6,11 +6,20 @@ class SshCopyId < Formula
   version "8.4p1"
   sha256 "5a01d22e407eb1c05ba8a8f7c654d388a13e9f226e4ed33bd38748dafa1d2b24"
   license "SSH-OpenSSH"
+  revision 1
   head "https://github.com/openssh/openssh-portable.git"
 
   bottle :unneeded
 
   keg_only :provided_by_macos
+
+  # Fixes an invalid heredoc within a multiline `$()` block.
+  # Fixed upstream; will be in the next release.
+  # https://github.com/openssh/openssh-portable/pull/206
+  patch do
+    url "https://github.com/openssh/openssh-portable/commit/d9e727dcc04a52caaac87543ea1d230e9e6b5604.patch?full_index=1"
+    sha256 "fbca48deeaf0e51ddf220fa18088142ade2d406ad06bb03720389313df282651"
+  end
 
   def install
     bin.install "contrib/ssh-copy-id"
