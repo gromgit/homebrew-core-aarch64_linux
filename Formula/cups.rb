b024c23fcfcc26440a08dd0d325b289577d5617e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-05 16:26:22 +0000

    cups: update 2.3.3op2 bottle.

--- Formula/cups.rb
+++ Formula/cups.rb
@@ -9,10 +9,10 @@ class Cups < Formula
   head "https://github.com/OpenPrinting/cups.git"
 
   bottle do
-    sha256 arm64_big_sur: "d874c7af4aea94eb15304c13bc1b5c10c63db79f3d4e78ae6f41a73ecc78d4fd"
-    sha256 big_sur:       "8eed2907ba0d36d073a53a2556693826113b2aad09256c1451f3c9defd275c75"
-    sha256 catalina:      "8f00138524b80aa2b4df62a75ddee6e2ebbd89670335b733f58fc529b2e2872d"
-    sha256 mojave:        "e749bd85f784c552184fce875caa01d6095991362a95796e4ceeb68bf68f9bd3"
+    sha256 arm64_big_sur: "ac83d0ea52bebcdb2eff93c6ccbaf29c14369a7d2643bf3a85780ed34c13e0a4"
+    sha256 big_sur:       "4943fd51e0dcd79d2f02cebe3ff007af6b69252ee6a2bb2ea9cdd2d7c0ec7ebc"
+    sha256 catalina:      "eddaee8d8b01941fc65ff0689424221956c02fa98c09ba056e3f00baac63652f"
+    sha256 mojave:        "a83b539b7e2f0865be0b6f0123c32c82788b8c52ecd5cf972cc15882a7ac0ba5"
   end
 
   keg_only :provided_by_macos
