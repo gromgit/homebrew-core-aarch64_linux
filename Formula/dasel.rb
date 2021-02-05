c4711b6c12b9069510c39d8c1b5c0be436bd74ac
Caleb Xu <calebcenter@live.com>
2021-02-05 02:15:46 -0500

    dasel 1.12.2 (new formula)
    Closes #70488.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

---
+++ Formula/dasel.rb
@@ -0,0 +1,20 @@
+class Dasel < Formula
+  desc "JSON, YAML, TOML, XML, and CSV query and modification tool"
+  homepage "https://github.com/TomWright/dasel"
+  url "https://github.com/TomWright/dasel/archive/v1.12.2.tar.gz"
+  sha256 "e0f6f9e43265ee385d8bea08c662e7d985fdc8169fd88589c27e6019e6c37a31"
+  license "MIT"
+  head "https://github.com/TomWright/dasel.git"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-X 'github.com/tomwright/dasel/internal.Version=#{version}'"
+    system "go", "build", *std_go_args, "-ldflags", ldflags, "./cmd/dasel"
+  end
+
+  test do
+    json = "[{\"name\": \"Tom\"}, {\"name\": \"Jim\"}]"
+    assert_equal "Tom\nJim", pipe_output("#{bin}/dasel --plain -p json -m '.[*].name'", json).chomp
+  end
+end
