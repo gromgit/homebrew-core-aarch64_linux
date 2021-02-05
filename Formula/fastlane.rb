d21a5b7513ddfdeafa9fbdd17ab26fcf98eecd26
Bas Kerkhoven <bas@m2mobi.com>
2021-02-05 12:04:40 +0100

    fastlane 2.173.0
    Closes #70498.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/fastlane.rb
+++ Formula/fastlane.rb
@@ -1,8 +1,8 @@
 class Fastlane < Formula
   desc "Easiest way to build and release mobile apps"
   homepage "https://fastlane.tools"
-  url "https://github.com/fastlane/fastlane/archive/2.172.0.tar.gz"
-  sha256 "09653f41f43814dbfb58d0d7e82a3b49a550d404fa6d4f37f23b4d110dfc0859"
+  url "https://github.com/fastlane/fastlane/archive/2.173.0.tar.gz"
+  sha256 "5d1444a63e7acd40d6ca95bca8b6359355497750e0ceb179e4f530fed925db76"
   license "MIT"
   head "https://github.com/fastlane/fastlane.git"
 
