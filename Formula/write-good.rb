65657ea8f028808c284a3d43d31f5f6f3f225936
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-06 16:25:11 +0000

    write-good: update 1.0.5 bottle.

--- Formula/write-good.rb
+++ Formula/write-good.rb
@@ -8,10 +8,10 @@ class WriteGood < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "785f60e72a7fb8b15d1d839515df63f46f04487ebd5594cfa22bf39226096942"
-    sha256 cellar: :any_skip_relocation, big_sur:       "37567d2c4ae6274cb8c02ce524d52bb30e09fb88013d558e76b74c67a9196bfb"
-    sha256 cellar: :any_skip_relocation, catalina:      "93462e20926597984079f3d4d31a2228bce4ccfa31b646174344e5fa134505bb"
-    sha256 cellar: :any_skip_relocation, mojave:        "66b39cc376254efd0a62240deb6212416c002d75643188f76c51fc586940899b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "dcb36d15082245bd32277d964bd77d11f3610a9e0cf48401bf20ca7cff2291bc"
+    sha256 cellar: :any_skip_relocation, big_sur:       "17ed562f70bddc92d7f52a4c1e8cf62bbc1aef49d51e78076dd357044737545b"
+    sha256 cellar: :any_skip_relocation, catalina:      "af36a1a3ec8609ee811afd157bacc62503c0f96b7b91571a2d4b114277fca274"
+    sha256 cellar: :any_skip_relocation, mojave:        "5f60c1de800c479bfa7d1f4995e9fb7bb0e58e718314888329a7bfbc00ed9aa5"
   end
 
   depends_on "node"
