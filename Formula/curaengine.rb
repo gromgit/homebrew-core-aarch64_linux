b9a1142754932faca9fdc34b32e8eeb468273742
Sam Ford <1584702+samford@users.noreply.github.com>
2021-02-05 20:30:06 -0500

    curaengine: update livecheck

--- Formula/curaengine.rb
+++ Formula/curaengine.rb
@@ -8,13 +8,13 @@ class Curaengine < Formula
   head "https://github.com/Ultimaker/CuraEngine.git"
 
   # Releases like xx.xx or xx.xx.x are older than releases like x.x.x, so we
-  # work around this less-than-ideal situation by restricting the minor version
-  # to one digit. This won't pick up versions where the minor version is 10+
+  # work around this less-than-ideal situation by restricting the major version
+  # to one digit. This won't pick up versions where the major version is 10+
   # but thankfully that hasn't been true yet. This should be handled in a better
   # way in the future, to avoid the possibility of missing good versions.
   livecheck do
-    url :head
-    regex(/^v?(\d+\.\d(?:\.\d+)+)$/i)
+    url :stable
+    regex(/^v?(\d(?:\.\d+)+)$/i)
   end
 
   bottle do
