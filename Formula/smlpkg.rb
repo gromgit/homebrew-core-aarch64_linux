2a74776bb0cb5ae4a08d1605d40bb7b9ef3ae19a
Caleb Xu <calebcenter@live.com>
2021-02-06 02:05:23 -0500

    smlpkg 0.1.5
    Closes #70567.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/smlpkg.rb
+++ Formula/smlpkg.rb
@@ -1,8 +1,8 @@
 class Smlpkg < Formula
   desc "Package manager for Standard ML libraries and programs"
   homepage "https://github.com/diku-dk/smlpkg"
-  url "https://github.com/diku-dk/smlpkg/archive/v0.1.3.tar.gz"
-  sha256 "cfa7eeff951e04df428694fda38917ee2aaaf0532e2d1dbea7ab4c150f4fe2f0"
+  url "https://github.com/diku-dk/smlpkg/archive/v0.1.5.tar.gz"
+  sha256 "53440d8b0166dd689330fc686738076225ac883a00b283e65394cf9312575c33"
   license "MIT"
   head "https://github.com/diku-dk/smlpkg.git"
 
