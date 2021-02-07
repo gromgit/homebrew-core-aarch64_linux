6a4087f34b7ea435bb4668e2192534eb6568f024
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-07 21:18:34 +0000

    ht-rust: update 0.5.0 bottle.

--- Formula/ht-rust.rb
+++ Formula/ht-rust.rb
@@ -7,10 +7,10 @@ class HtRust < Formula
   head "https://github.com/ducaale/ht.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3d5a821858a26bcd63b8d7d3dd13a03696eaa62426d573f54e441af6dec2c495"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e5273e5b99f250e9cff4643678f46a4383e5c07a9b5ca3e7f55d7abb24e3e97c"
-    sha256 cellar: :any_skip_relocation, catalina:      "2a9205e969a23615ae1951e98d7f9fe59955f89eba380f1ccbad82bfbb6f3e21"
-    sha256 cellar: :any_skip_relocation, mojave:        "3ef31bc091ac3c0bb4d6dba576383c4d2d1acd5fa37f3e9194ea5603bb064ee0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "89da38044281fe03f6870429d66ad499a1c94e30293fda5ed833cb5590bb0617"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a2c87c59ef867068f56acf7c9391ce1517b28a5245ed4e25720574568a230b86"
+    sha256 cellar: :any_skip_relocation, catalina:      "113ba41bd6f22cd7b0962eb7906004c4189fee5ddacddb62c1c24e56b98f0e0a"
+    sha256 cellar: :any_skip_relocation, mojave:        "af09c8b2723fd151effb7050fc767db573ce43d730f6935506a7868c2c27249e"
   end
 
   depends_on "rust" => :build
