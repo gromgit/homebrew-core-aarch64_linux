2587917130ed9a77ec150f6e0ba6e64c6f1bdb04
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-07 05:16:53 +0000

    pigz: update 2.6 bottle.

--- Formula/pigz.rb
+++ Formula/pigz.rb
@@ -11,10 +11,10 @@ class Pigz < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9ca4d00080d69cb2df3f9301a86c9518693a75143d6dac70eee7fbe005a297d6"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ab03009238f5a5181cd69b03905f9e85e89773ec643691ce3eb010d008d39aa0"
-    sha256 cellar: :any_skip_relocation, catalina:      "1f054b879a528740c7a6b854ff7fe948d2706dae808088fa19f82102b1ad174b"
-    sha256 cellar: :any_skip_relocation, mojave:        "0338683c663acf7cf0e403cf044ae62512215739bb811c3751165f2368edead3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "487e4803119cf79952f0604f7c22f400299412210e771d1a5312f75b19903751"
+    sha256 cellar: :any_skip_relocation, big_sur:       "6ed76215af752e06d11b176c3c169a55f76cf2010c01e67e62f7543081137b5e"
+    sha256 cellar: :any_skip_relocation, catalina:      "c1e5284c99ef13e0401035597901b5b63d19bbb642fd7206d648193c89d0e6e7"
+    sha256 cellar: :any_skip_relocation, mojave:        "0d382426d9bf386f21f8412317b58d05fcaf6ada17b260a1b6e95e8f2e955cf4"
   end
 
   uses_from_macos "zlib"
