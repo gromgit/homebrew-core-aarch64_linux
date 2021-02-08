6613002f3a5f1348f5d951aa24f075972b8d61bf
Mike McQuaid <mike@mikemcquaid.com>
2021-02-08 11:45:43 +0000

    postgresql@11: fix ENV["CI"] usage.
    Part of https://github.com/Homebrew/homebrew-core/issues/70493.

--- Formula/postgresql@11.rb
+++ Formula/postgresql@11.rb
@@ -77,10 +77,12 @@ class PostgresqlAT11 < Formula
   end
 
   def post_install
-    return if ENV["CI"]
-
     (var/"log").mkpath
     postgresql_datadir.mkpath
+
+    # Don't initialize database, it clashes when testing other PostgreSQL versions.
+    return if ENV["HOMEBREW_GITHUB_ACTIONS"]
+
     system "#{bin}/initdb", "--locale=C", "-E", "UTF-8", postgresql_datadir unless pg_version_exists?
   end
 
@@ -137,7 +139,7 @@ class PostgresqlAT11 < Formula
   end
 
   test do
-    system "#{bin}/initdb", testpath/"test" unless ENV["CI"]
+    system "#{bin}/initdb", testpath/"test" unless ENV["HOMEBREW_GITHUB_ACTIONS"]
     assert_equal opt_pkgshare.to_s, shell_output("#{bin}/pg_config --sharedir").chomp
     assert_equal opt_lib.to_s, shell_output("#{bin}/pg_config --libdir").chomp
     assert_equal opt_lib.to_s, shell_output("#{bin}/pg_config --pkglibdir").chomp
