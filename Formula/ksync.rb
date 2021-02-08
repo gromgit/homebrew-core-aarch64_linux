9d3a12933758bb7346352ad44ba9c4afa67ea141
rui <rui@chenrui.dev>
2021-02-08 00:59:17 +0000

    ksync 0.4.6
    Closes #70653.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ksync.rb
+++ Formula/ksync.rb
@@ -2,8 +2,8 @@ class Ksync < Formula
   desc "Sync files between your local system and a kubernetes cluster"
   homepage "https://ksync.github.io/ksync/"
   url "https://github.com/ksync/ksync.git",
-      tag:      "0.4.5",
-      revision: "9f40bf134329814a57e1a58d73b84761a2b06c73"
+      tag:      "0.4.6",
+      revision: "a63f23dba397a540cca9cbde5001afa5cbd8b2ae"
   license "Apache-2.0"
   head "https://github.com/ksync/ksync.git"
 
