a2a73c0172fbbc39ee0df6560fa58c70f669c6a4
Michka Popoff <michkapopoff@gmail.com>
2021-02-08 20:00:28 +0100

    spotify-tui: add linux dependencies (#70698)

--- Formula/spotify-tui.rb
+++ Formula/spotify-tui.rb
@@ -15,6 +15,12 @@ class SpotifyTui < Formula
 
   depends_on "rust" => :build
 
+  on_linux do
+    depends_on "pkg-config" => :build
+    depends_on "libxcb"
+    depends_on "openssl@1.1"
+  end
+
   def install
     system "cargo", "install", *std_cargo_args
   end
