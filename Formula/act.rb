0922bd0d13f0cdaff1f448ab0486187d6163348d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-08 20:17:15 +0000

    act: update 0.2.20 bottle.

--- Formula/act.rb
+++ Formula/act.rb
@@ -6,10 +6,10 @@ class Act < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d2bd2042f26d5a8be7454c8e5f2ab636691f02d99aad91ecafa6b1f1018ba631"
-    sha256 cellar: :any_skip_relocation, big_sur:       "073ec3c987e044b3f730a5e451884c74390f0521c25e3d887a4fcaa511608175"
-    sha256 cellar: :any_skip_relocation, catalina:      "7cbf53abcecbf19075fdf1199998c61a0c6c3fd8dadf40e6a1295fc866169ebe"
-    sha256 cellar: :any_skip_relocation, mojave:        "39ab75db53d6260817eb8c2169d40fdf77b681f5c42470a21cd341f3b7eb929f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "100f2beffca5042bd9ef4b06bbcc2e035b1a309d8d3005bf8fc0ca73aec69ccf"
+    sha256 cellar: :any_skip_relocation, big_sur:       "fca37fbe614be35345521363bf02aa5b34ceb5a3c12dd389d9b3338740f5a34e"
+    sha256 cellar: :any_skip_relocation, catalina:      "d8ed01206792faf3edb49cc0c23f0758a1bbbecb291321d607adc5c7f16aeb4a"
+    sha256 cellar: :any_skip_relocation, mojave:        "cba5b986ce90051fb94b593fd59499a9d1072824d1ab0eb26094fae54a1e281a"
   end
 
   depends_on "go" => :build
