eeb68c5a9a0ab3b1854dbf96670b0e3be5788b45
rui <rui@chenrui.dev>
2021-02-08 16:54:55 +0000

    chronograf 1.8.10
    Closes #70702.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/chronograf.rb
+++ Formula/chronograf.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Chronograf < Formula
   desc "Open source monitoring and visualization UI for the TICK stack"
   homepage "https://docs.influxdata.com/chronograf/latest/"
-  url "https://github.com/influxdata/chronograf/archive/1.8.9.1.tar.gz"
-  sha256 "edf5038b301bc0cf49b2f74f1fcdc0a2d66fe5e4b69f753fd879972b7e04a7ee"
+  url "https://github.com/influxdata/chronograf/archive/1.8.10.tar.gz"
+  sha256 "dbfd73757f1e46534782efb834bb953234a8dd23f79d412708e48998e94ae28d"
   license "AGPL-3.0-or-later"
   head "https://github.com/influxdata/chronograf.git"
 
