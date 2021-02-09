047c6fe18541d37cd94e67b50389f109574653d7
Jon Huhn <huhnjon@gmail.com>
2021-02-09 17:23:37 -0600

    osm: fix missing version info
    Closes #70865.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/osm.rb
+++ Formula/osm.rb
@@ -1,9 +1,11 @@
 class Osm < Formula
   desc "Open Service Mesh (OSM)"
   homepage "https://openservicemesh.io/"
-  url "https://github.com/openservicemesh/osm/archive/v0.7.0.tar.gz"
-  sha256 "ab396a2f156e92fb2215905562f062ee92f96abd8a4d979cfb8c63940c8d5f26"
+  url "https://github.com/openservicemesh/osm.git",
+      tag:      "v0.7.0",
+      revision: "75423a0b4a3d5e9114fd93a133d2485171243c4b"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "217cac47e47bcd4d47b5244302681524ad9dfce9943e87c4bdbf6faf547ebdb8"
@@ -15,11 +17,13 @@ class Osm < Formula
   depends_on "go" => :build
 
   def install
+    ENV["VERSION"] = "v"+version
     system "make", "build-osm"
     bin.install "bin/osm"
   end
 
   test do
     assert_match "Error: Could not list namespaces related to osm", shell_output("#{bin}/osm namespace list 2>&1", 1)
+    assert_match "Version: v#{version};", shell_output("#{bin}/osm version 2>&1")
   end
 end
