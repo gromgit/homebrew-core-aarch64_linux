6bc854fb847510af66b9460579b7b74c854ae4f7
Dmitry Obedkov <dr.fruct@gmail.com>
2021-02-09 22:06:03 +0300

    rtl_443 20.11 (new formula)

---
+++ Formula/rtl_433.rb
@@ -0,0 +1,42 @@
+class Rtl433 < Formula
+  desc "Program to decode radio transmissions from devices"
+  homepage "https://github.com/merbanan/rtl_433"
+  url "https://github.com/merbanan/rtl_433/",
+    using:    :git,
+    tag:      "20.11",
+    revision: "0bb4672b1afb716d107b3060afc2d80a2159dbef"
+  license "GPL-2.0-or-later"
+  head "https://github.com/merbanan/rtl_433.git"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "librtlsdr"
+  depends_on "libusb"
+
+  resource("test_cu8") do
+    url "https://raw.githubusercontent.com/merbanan/rtl_433_tests/master/tests/oregon_scientific/uvr128/g001_433.92M_250k.cu8"
+    sha256 "7aa07b72cec9926f463410cda6056eb2411ac9e76006ba4917a0527492c5f65d"
+  end
+
+  resource("expected_json") do
+    url "https://raw.githubusercontent.com/merbanan/rtl_433_tests/master/tests/oregon_scientific/uvr128/g001_433.92M_250k.json"
+    sha256 "5054c0f322030dd1ee3ca78261b64e691da832900a2c6e4d13cc22f0fbbfbbfa"
+  end
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    resource("test_cu8").stage testpath
+    resource("expected_json").stage testpath
+
+    expected_output = shell_output("cat #{testpath}/g001_433.92M_250k.json")
+    rtl_433_output = shell_output("#{bin}/rtl_433 -c 0 -F json -r #{testpath}/g001_433.92M_250k.cu8")
+
+    assert_equal rtl_433_output, expected_output
+  end
+end
