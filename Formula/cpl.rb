fa2263c1bc18762d34f148cf1c69c03f5c66afbe
Bo Anderson <mail@boanderson.me>
2021-02-09 23:25:30 +0000

    cpl: update 7.1.2_6 bottle.

--- Formula/cpl.rb
+++ Formula/cpl.rb
@@ -14,9 +14,10 @@ class Cpl < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, big_sur:  "740461b2b2bde934347eec401daa1322bd54d726c7ec9902561a629042bfa16f"
-    sha256 cellar: :any, catalina: "458b9de38a1e292da0e9d70b453dc7ca8bc199b253c2a1120bb61d608595497c"
-    sha256 cellar: :any, mojave:   "dcaa92d135d443432230d2ac087f73f87e515959d42950eeb19419aebe926158"
+    sha256 cellar: :any, arm64_big_sur: "7ebb518afea5306674b1b4fe01826e3e3f3531eaf5f0d87069df31f88475caca"
+    sha256 cellar: :any, big_sur:       "740461b2b2bde934347eec401daa1322bd54d726c7ec9902561a629042bfa16f"
+    sha256 cellar: :any, catalina:      "458b9de38a1e292da0e9d70b453dc7ca8bc199b253c2a1120bb61d608595497c"
+    sha256 cellar: :any, mojave:        "dcaa92d135d443432230d2ac087f73f87e515959d42950eeb19419aebe926158"
   end
 
   depends_on "cfitsio"
