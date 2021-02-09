cae0d842a5b806a2a402f42e0b2de9a34ec6a728
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-09 12:22:15 +0000

    just: update 0.8.4 bottle.

--- Formula/just.rb
+++ Formula/just.rb
@@ -6,11 +6,10 @@ class Just < Formula
   license "CC0-1.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8b136aa2a953b88985016b0db6ca12afdf9f6beeea04ba1f62331416bd0aa526"
-    sha256 cellar: :any_skip_relocation, big_sur:       "78f15ce05d070d3703a12700d6451a581c5f557e87431c08172d767556ce568c"
-    sha256 cellar: :any_skip_relocation, catalina:      "b545b774d8b02c850f4ef476a4b0d0e14db13920765ec86c7778fea1affd1d84"
-    sha256 cellar: :any_skip_relocation, mojave:        "2254dc738e8f62d5fe20467e2bbd93207ce2670628f644ed320b8640d59b51f0"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "40b53f4c712088b72fc5a44f1be1040f9a6223f2a66a5126bae20b30fc2edd57"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f2ba1e974312ef1b70483c1d74bdf8f3c669e70872908b7014082b8959c4724a"
+    sha256 cellar: :any_skip_relocation, big_sur:       "d72a96af3be4416741b7bed8a806ce6ee7b36355b355d663062d9fe32aa814d4"
+    sha256 cellar: :any_skip_relocation, catalina:      "1d43b3fb6efcba5944483ba89d9b3b19af9b1745d1e7d5dcf2d047aa869a156d"
+    sha256 cellar: :any_skip_relocation, mojave:        "5101411c95e187d0a1184029f5b11f45119e8438d68fb62c15c3303e75e9287e"
   end
 
   depends_on "rust" => :build
