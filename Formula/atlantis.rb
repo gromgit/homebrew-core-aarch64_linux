4d2448db3dc5b8995a2ea3f8b71944f09f37f1e5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-09 03:17:53 +0000

    atlantis: update 0.16.1 bottle.

--- Formula/atlantis.rb
+++ Formula/atlantis.rb
@@ -7,9 +7,9 @@ class Atlantis < Formula
   head "https://github.com/runatlantis/atlantis.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "1b80f8b3397702e8a1c0d80f58874e77175ddbb3999b82fffd3a0be52b339ca4"
-    sha256 cellar: :any_skip_relocation, catalina: "a20268869c1a55294b841c74663ceeadbd0434b6f6895a78c08f5e64da613130"
-    sha256 cellar: :any_skip_relocation, mojave:   "105ae4b23881eeb51206fed19de86cb610ed4b35d0786d82e25aac365d7da2f6"
+    sha256 cellar: :any_skip_relocation, big_sur:  "ba03abca37b8286e5362d347fbc8589c40181edb38f9b62b5ce792845c89231f"
+    sha256 cellar: :any_skip_relocation, catalina: "b28d79960d9d5723f4839c8b41049862a7e399d19774c669e2d1b144d35076a9"
+    sha256 cellar: :any_skip_relocation, mojave:   "97bd297603b845ba27b2d87a24ec2191b6b1ac419fd297ff480d51517ac65f6f"
   end
 
   depends_on "go" => :build
