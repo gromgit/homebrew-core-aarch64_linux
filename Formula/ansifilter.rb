6ce6b201a1ce117d5d2a50e77284c7a9bab58734
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-09 12:22:17 +0000

    ansifilter: update 2.18 bottle.

--- Formula/ansifilter.rb
+++ Formula/ansifilter.rb
@@ -11,11 +11,10 @@ class Ansifilter < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d5b4b56e737b3dd6bea07aeca8cb06cce2bd5b2c92923e4e065f8cc8e85a0cc5"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4eb6f695e2770188371433e28574888c0e869a94eed6384eb346dea9447ac24d"
-    sha256 cellar: :any_skip_relocation, catalina:      "393daa46a9bc661060ca7132feb7d635cc08ad828bd3512e4d27d29a315a26b3"
-    sha256 cellar: :any_skip_relocation, mojave:        "8a1021a90a26783b1b2127dadac4dcd7d50f0c3e9e8d412a6ee2b0b61160088d"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "a6b73540c7ed94ba539e24ade74aaf0281b1895184766085dc0af5b7923a1180"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "604a45cdc202a4cb1ee084a350b8ad989f135e29d1d4153a9c9e20818da8e31e"
+    sha256 cellar: :any_skip_relocation, big_sur:       "bce4e87b0f16217d84a01e664c6e96c530d561da301912bfd9e39ec7ff584a74"
+    sha256 cellar: :any_skip_relocation, catalina:      "95487d963289a6fbc97a933a492a8f82ae88ad7087ca525d710edea34e100f0a"
+    sha256 cellar: :any_skip_relocation, mojave:        "06193d389cb3f1ba85f07fda25e0f17ac732efd289597a6ea2e6ce440123bb5c"
   end
 
   def install
