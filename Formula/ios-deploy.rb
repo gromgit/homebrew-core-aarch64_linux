0d7a1276beaf6c841ea56f5acf21bdcb22013535
Gabe Giosia <gabe@ghearing.com>
2021-02-09 18:17:11 -0500

    ios-deploy 1.11.4
    * ios-deploy 1.11.4
    * Try only building for the desired CPU's arch
    * fix whitespace issue
    
    Closes #70803.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ios-deploy.rb
+++ Formula/ios-deploy.rb
@@ -1,8 +1,8 @@
 class IosDeploy < Formula
   desc "Install and debug iPhone apps from the command-line"
   homepage "https://github.com/ios-control/ios-deploy"
-  url "https://github.com/ios-control/ios-deploy/archive/1.11.3.tar.gz"
-  sha256 "0bdf81aeaef06bbc222fff0cb3a2ec9575593db4027be326a505ae8ece4c424f"
+  url "https://github.com/ios-control/ios-deploy/archive/1.11.4.tar.gz"
+  sha256 "52aa0a5985fb5638c9b35351f7380b416651d172a460ca991fc02d1ae84611f6"
   license all_of: ["GPL-3.0-or-later", "BSD-3-Clause"]
   head "https://github.com/ios-control/ios-deploy.git"
 
@@ -18,9 +18,15 @@ class IosDeploy < Formula
   depends_on :macos
 
   def install
-    xcodebuild "-configuration", "Release", "SYMROOT=build"
+    xcodebuild "-configuration", "Release",
+               "SYMROOT=build",
+               "-arch", Hardware::CPU.arch
 
-    xcodebuild "test", "-scheme", "ios-deploy-tests", "-configuration", "Release", "SYMROOT=build"
+    xcodebuild "test",
+               "-scheme", "ios-deploy-tests",
+               "-configuration", "Release",
+               "SYMROOT=build",
+               "-arch", Hardware::CPU.arch
 
     bin.install "build/Release/ios-deploy"
   end
