9bbd2a93162f7e80ccc5405823633e4f1e975476
rui <rui@chenrui.dev>
2021-02-09 08:29:48 +0000

    muffet 2.4.0
    * muffet 2.4.0
    * update sha
    * fix test
    
    Closes #70757.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: rui <rui@meetup.com>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/muffet.rb
+++ Formula/muffet.rb
@@ -1,8 +1,8 @@
 class Muffet < Formula
   desc "Fast website link checker in Go"
   homepage "https://github.com/raviqqe/muffet"
-  url "https://github.com/raviqqe/muffet/archive/v2.3.2.tar.gz"
-  sha256 "ee69f8c256fed8d0e692a35a5b206e24b04a00f7d82e8d9a645c28cf7d9b3b20"
+  url "https://github.com/raviqqe/muffet/archive/v2.4.0.tar.gz"
+  sha256 "31a894186a0452c4bbf281c48950146c3191cfa3dc73596696f9525be245c320"
   license "MIT"
   head "https://github.com/raviqqe/muffet.git"
 
@@ -23,6 +23,7 @@ class Muffet < Formula
     assert_match "failed to fetch root page: lookup does.not.exist: no such host",
         shell_output("#{bin}/muffet https://does.not.exist 2>&1", 1)
 
-    system bin/"muffet", "https://httpbin.org/"
+    assert_match "https://httpbin.org/",
+        shell_output("#{bin}/muffet https://httpbin.org 2>&1", 1)
   end
 end
