7269850ebdb0ee3a42f5fef1373f5e71744ee27e
rui <rui@chenrui.dev>
2021-02-10 20:25:48 +0000

    nfpm 2.2.4
    Closes #70860.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nfpm.rb
+++ Formula/nfpm.rb
@@ -1,8 +1,8 @@
 class Nfpm < Formula
   desc "Simple deb and rpm packager"
   homepage "https://nfpm.goreleaser.com/"
-  url "https://github.com/goreleaser/nfpm/archive/v2.2.3.tar.gz"
-  sha256 "8e5143d732d49dbd02ef8f778de5bbb6ca16cb9f91843f9c401596f73c1c4294"
+  url "https://github.com/goreleaser/nfpm/archive/v2.2.4.tar.gz"
+  sha256 "afff55ac4cb4875c6d42bb9f2eed96e942d39ebb7e3c26b27b8ef415ba3525b8"
   license "MIT"
   head "https://github.com/goreleaser/nfpm.git"
 
