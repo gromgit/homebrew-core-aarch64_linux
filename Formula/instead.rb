3f702f73dbe5ec2261909a60924c62777c7ff522
rui <rui@chenrui.dev>
2021-02-10 12:11:07 +0000

    instead 3.3.3
    * instead 3.3.3
    * instead: add pkg-config build dependency
    * instead: depend on gtk+3
    
    Closes #70831.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/instead.rb
+++ Formula/instead.rb
@@ -1,10 +1,9 @@
 class Instead < Formula
   desc "Interpreter of simple text adventures"
   homepage "https://instead.syscall.ru/"
-  url "https://github.com/instead-hub/instead/archive/3.3.2.tar.gz"
-  sha256 "bdb827f36e693dc7b443e69d4678d24f1ccc20dc093c22f58b8d78192da15f2e"
+  url "https://github.com/instead-hub/instead/archive/3.3.3.tar.gz"
+  sha256 "1425c559ab91c8ec3e02ad74f6989a4408ee7141d69361d17aeff174d194fe57"
   license "MIT"
-  revision 3
 
   bottle do
     sha256 arm64_big_sur: "eb951c76454e1555ab7aa01910a03ddfcec6c73ebede17f642646b8dc317d7c2"
@@ -14,6 +13,8 @@ class Instead < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "gtk+3"
   depends_on "luajit-openresty"
   depends_on "sdl2"
   depends_on "sdl2_image"
