71f150c5652bf3a80965e2def4e62e6c617644b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-10 17:27:04 +0000

    gtkmm: update 2.24.5_8 bottle.

--- Formula/gtkmm.rb
+++ Formula/gtkmm.rb
@@ -12,10 +12,11 @@ class Gtkmm < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "d0ed84fa9b225419e612a9b230914a7ffc89edb7911d8c9c93259c4b22ae7d42"
-    sha256 cellar: :any, big_sur:       "d38edddb1e41fead71b2758b3f73e98745d4437e7be367be7ceb02d461524cb3"
-    sha256 cellar: :any, catalina:      "36bb5da441b53edde4c238f3db1cf9c6ac4c7d6cbf615dc6f8ab6a9aaf75c6be"
-    sha256 cellar: :any, mojave:        "23182625b2ad79dac53a931d009b4e4621fbd75682c598e54d58ea8bfebb73f1"
+    rebuild 1
+    sha256 cellar: :any, arm64_big_sur: "ae2cb84a696c040281a8131961c755865f4ba96ef223e77cf8e5a8d02b88edb8"
+    sha256 cellar: :any, big_sur:       "cf3e818aadeda99afd5c51cdfd8ae950bdf56ce16c090d78f23e5a80631f6f13"
+    sha256 cellar: :any, catalina:      "bc967efcc4b25a56a79089c73db15a7fc61d5d83a62bd5c899777f7169f2e437"
+    sha256 cellar: :any, mojave:        "1ed0b8b0445bcb223f2d20112004ead1c8b5d598f9e0012180831e069375b6f6"
   end
 
   depends_on "pkg-config" => :build
