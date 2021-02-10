621fdf67ad1aafed10988b7eeb839f582de5b58e
Michka Popoff <michkapopoff@gmail.com>
2021-02-10 23:17:26 +0100

    pianod: add linux-only dependency
    Closes #70870.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pianod.rb
+++ Formula/pianod.rb
@@ -18,6 +18,11 @@ class Pianod < Formula
   depends_on "libao"
   depends_on "libgcrypt"
 
+  on_linux do
+    # pianod uses avfoundation on macOS, ffmpeg on Linux
+    depends_on "ffmpeg"
+  end
+
   def install
     ENV["OBJCXXFLAGS"] = "-std=c++11"
     system "./configure", "--disable-debug",
