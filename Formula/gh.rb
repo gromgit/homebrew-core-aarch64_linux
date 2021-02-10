c10f7b351b3c86756b62089ceeec553bf35941dd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-10 19:13:15 +0000

    gh: update 1.5.0 bottle.

--- Formula/gh.rb
+++ Formula/gh.rb
@@ -13,10 +13,11 @@ class Gh < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "46285720614f0a69645bd94265c5e1d798beaa92d41a6f3aabcf97f8747859a3"
-    sha256 cellar: :any_skip_relocation, big_sur:       "663bd535afe56f4d0cdd1a9677a1bcbf7f603805349120d4938e596beef65cfb"
-    sha256 cellar: :any_skip_relocation, catalina:      "0448e049f21a24a2ed9fe20bb5b5a5325b76570ea6faf78d50733954cc8cd715"
-    sha256 cellar: :any_skip_relocation, mojave:        "ae889bc195283a5bf49e887dd2ba521f4c36971170941c4d6105eb76b820c40f"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "41dfc99e72e47bb0a96fae9d7694fd081f309d8cadc1a30f51e38e8ad2ea5d5a"
+    sha256 cellar: :any_skip_relocation, big_sur:       "b7cc2f2572ca67b64313f751e096180f938640c83e3b894d6b8a7decf717d552"
+    sha256 cellar: :any_skip_relocation, catalina:      "f57eae8d861c55b40c2b613b45b1a6d0e17987cd995d7b851f1ca9ca8809d216"
+    sha256 cellar: :any_skip_relocation, mojave:        "ebd7caca02b77e13437f4b9835206e8398095423a33c304cb55d2e90be30fd61"
   end
 
   depends_on "go" => :build
