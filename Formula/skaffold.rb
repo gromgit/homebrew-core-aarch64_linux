2d05efd797a99c1b774606937f0c08ba31516033
rui <rui@chenrui.dev>
2021-02-11 22:54:41 +0000

    skaffold 1.20.0
    Closes #70947.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/skaffold.rb
+++ Formula/skaffold.rb
@@ -2,8 +2,8 @@ class Skaffold < Formula
   desc "Easy and Repeatable Kubernetes Development"
   homepage "https://skaffold.dev/"
   url "https://github.com/GoogleContainerTools/skaffold.git",
-      tag:      "v1.19.0",
-      revision: "63949e28f40deed44c8f3c793b332191f2ef94e4"
+      tag:      "v1.20.0",
+      revision: "c48e97690d8daffd68141c2a68fcbe3df6f6936a"
   license "Apache-2.0"
   head "https://github.com/GoogleContainerTools/skaffold.git"
 
