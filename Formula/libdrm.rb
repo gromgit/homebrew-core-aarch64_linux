4e47ccfc16f2514787ffbc4ddac0185e89209050
Caleb Xu <calebcenter@live.com>
2021-02-10 20:47:56 -0500

    libdrm 2.4.104 (#69032)

--- Formula/libdrm.rb
+++ Formula/libdrm.rb
@@ -1,8 +1,8 @@
 class Libdrm < Formula
   desc "Library for accessing the direct rendering manager"
   homepage "https://dri.freedesktop.org"
-  url "https://dri.freedesktop.org/libdrm/libdrm-2.4.103.tar.xz"
-  sha256 "3fe0affdba6460166a7323290c18cf68e9b59edcb520722826cb244e9cb50222"
+  url "https://dri.freedesktop.org/libdrm/libdrm-2.4.104.tar.xz"
+  sha256 "d66ad8b5c2441015ac1333e40137bb803c3bde3612ff040286fcc12158ea1bcb"
   license "MIT"
 
   livecheck do
