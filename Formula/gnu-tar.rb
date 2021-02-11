a0a79c2e66408dbed74920d0a6b778dfd330a119
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-11 03:17:42 +0000

    gnu-tar: update 1.33 bottle.

--- Formula/gnu-tar.rb
+++ Formula/gnu-tar.rb
@@ -7,10 +7,11 @@ class GnuTar < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "992da32921e3033679cc2323a34f21e0c847661aedf0d8c59e04c2d6a47fed45"
-    sha256 cellar: :any_skip_relocation, big_sur:       "14c85bf6742a4055f0a6d3444993af7866d4963cd264eb2f2419bfe07cafda74"
-    sha256 cellar: :any_skip_relocation, catalina:      "f99e9b8b33b9fd07a04bf6661cbc3e56267f2b682f2ffd12d3775c7838795381"
-    sha256 cellar: :any_skip_relocation, mojave:        "0320a427ff60c2665ee85898f45a96df4e0824d7ef0d985a8434d6fd4c1e0c74"
+    rebuild 1
+    sha256 arm64_big_sur: "561f31d09837edf9398d73b0843010b4154b209196106df07cff06624e3df746"
+    sha256 big_sur:       "ae7e03fc22f5e216f8dfb1aae1822595e5c11f9c78b06e1623577a8d76bc8d75"
+    sha256 catalina:      "906c7a5265feb825505d1fe0ca563fe03f941f8d9d5487c83a90c0eade960d2b"
+    sha256 mojave:        "057667a5bc2671beb8371b4da25b576167de95538c2a2e5cea9c16c76e14a20d"
   end
 
   head do
