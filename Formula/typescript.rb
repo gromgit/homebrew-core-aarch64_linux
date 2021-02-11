fcf8889850bde366168a287bc742bdf9bdf5ef37
Dustin Rodrigues <dust.rod@gmail.com>
2021-02-10 19:33:33 -0500

    typescript 4.1.5
    Closes #70878.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/typescript.rb
+++ Formula/typescript.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Typescript < Formula
   desc "Language for application scale JavaScript development"
   homepage "https://www.typescriptlang.org/"
-  url "https://registry.npmjs.org/typescript/-/typescript-4.1.4.tgz"
-  sha256 "dba5c640a6fd0d5418d26ebf9f61a95ca50a78587f5d4603af3fbd49ea891901"
+  url "https://registry.npmjs.org/typescript/-/typescript-4.1.5.tgz"
+  sha256 "b39221f974d2d2f7c770ba66323ce4b12152813fa31b86e20602580d64e7dbb4"
   license "Apache-2.0"
   head "https://github.com/Microsoft/TypeScript.git"
 
