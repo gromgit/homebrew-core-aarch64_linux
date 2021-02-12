052dedcdb98112016a78848e83541971c59943b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-12 09:18:05 +0000

    libspatialite: update 5.0.1 bottle.

--- Formula/libspatialite.rb
+++ Formula/libspatialite.rb
@@ -13,10 +13,10 @@ class Libspatialite < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "ba20ea1010d62219b45458ad9768f963ff0684497834d00a81a398c3ed6e1dbb"
-    sha256 cellar: :any, big_sur:       "18444b8c0c78ed66b7dd84e50bbf24b67b0d955c2f1fd8716bbe77b41353a7bf"
-    sha256 cellar: :any, catalina:      "e4a083ce451aa9bad3ca209a042c32f946a9ed3964d498790c6d141ac9d70aab"
-    sha256 cellar: :any, mojave:        "3c8bedbf668ba7627e00dfb7dc4b16eab35681fec2d0a06275d40f5091e278a2"
+    sha256 cellar: :any, arm64_big_sur: "18ecddab7cd064038e15a9e023fc1cb94dfabcb4ea1dbd527e19754c80829445"
+    sha256 cellar: :any, big_sur:       "eea8b32b264f5183558158fe3d76cfd676bb731620f67fc1da66519f28171b6b"
+    sha256 cellar: :any, catalina:      "054c1286dcdffded43a8cf768eac32c4f20e9870bc0b0e0aa04c40fb31280836"
+    sha256 cellar: :any, mojave:        "c136e8503fda01a139f00fbc6ac5e3b24989961a17485c28e231b355090e98e4"
   end
 
   head do
