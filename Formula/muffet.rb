d4e690d5bf56dbf72a408e8d6d00f14db560b0de
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-12 20:17:28 +0000

    muffet: update 2.4.0 bottle.

--- Formula/muffet.rb
+++ Formula/muffet.rb
@@ -7,10 +7,10 @@ class Muffet < Formula
   head "https://github.com/raviqqe/muffet.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a4bc44ac5a226e09826145e99e3ff61ab2a131f7f47c6d9ac0538c36d23ff860"
-    sha256 cellar: :any_skip_relocation, big_sur:       "69140cc1b455447e3d854ddf1f72bf111f16b1172bb6c6b252eb91bfd33ba9dc"
-    sha256 cellar: :any_skip_relocation, catalina:      "81e67b81c6f1ee2eb044713f4731fd52675c8680b50e9f7bbf8c88a6c759f50a"
-    sha256 cellar: :any_skip_relocation, mojave:        "8dd42b1d41d88aae4737aba6ad652449a3750e19b3ee633ebfe279289cd015a8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bb6a1f79fe2c70c8b54f5606f157338f508f5b8454e8b8f025a5921a4dbf9400"
+    sha256 cellar: :any_skip_relocation, big_sur:       "dc8eaca3727e3f44cd4782473933a291d124e1fdfac42ecfa6fcc09ee8330d3e"
+    sha256 cellar: :any_skip_relocation, catalina:      "6b400e2c66b268040095257db15819b093956816f2b7763d13ad95c87c34db66"
+    sha256 cellar: :any_skip_relocation, mojave:        "7969b64e9a823734c1c96f0356302bd6edc7a740bcdf8d75fa97a7a8bdb3bf60"
   end
 
   depends_on "go" => :build
