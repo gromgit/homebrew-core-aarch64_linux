c1d23a857dccd4b073ddbe191133cbd6ece8f517
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2021-02-12 20:32:50 +0000

    gtk+3 3.24.25
    Closes #71009.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gtk+3.rb
+++ Formula/gtk+3.rb
@@ -1,8 +1,8 @@
 class Gtkx3 < Formula
   desc "Toolkit for creating graphical user interfaces"
   homepage "https://gtk.org/"
-  url "https://download.gnome.org/sources/gtk+/3.24/gtk+-3.24.24.tar.xz"
-  sha256 "cc9d4367c55b724832f6b09ab85481738ea456871f0381768a6a99335a98378a"
+  url "https://download.gnome.org/sources/gtk+/3.24/gtk+-3.24.25.tar.xz"
+  sha256 "87e26b111d3b8a85ff218980a56f3e814257b8dd11e5c4d9a2803b423b08297c"
   license "LGPL-2.0-or-later"
 
   livecheck do
