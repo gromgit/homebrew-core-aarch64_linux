71512672479c80621d77a4cd07ac17eff9f5bca0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-12 15:16:31 +0000

    kyma-cli: update 1.19.0 bottle.

--- Formula/kyma-cli.rb
+++ Formula/kyma-cli.rb
@@ -7,9 +7,9 @@ class KymaCli < Formula
   head "https://github.com/kyma-project/cli.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "2374eb6ec693d46e62b1b77e1642c45c5137a622350aa5b3ab52e5019c0d0562"
-    sha256 cellar: :any_skip_relocation, catalina: "ef735c9c5d497af110d10cd2c7e0d96b9a101ad2e69e2c36f0ec50a7f0e1487b"
-    sha256 cellar: :any_skip_relocation, mojave:   "973f107d736522d3d0e59631057985771b8e54545fd08686134423f6b6032e5c"
+    sha256 cellar: :any_skip_relocation, big_sur:  "f916d2e34ce347f5e9fd201a348b787477f154d553dd54a2f9108dd2bdfad816"
+    sha256 cellar: :any_skip_relocation, catalina: "7cddd24bd0f447a412fcbdf262881b2b23d94065abad9557c73185655fa64191"
+    sha256 cellar: :any_skip_relocation, mojave:   "404fdac1b20854feeb541ced5f29b751d82863d75761dc3d6d1eb4197664be3d"
   end
 
   depends_on "go@1.14" => :build
