9a9f433d3d20dc40e97bcf41f574331e2e976c51
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-12 21:45:54 +0000

    netlify-cli: update 3.7.1 bottle.

--- Formula/netlify-cli.rb
+++ Formula/netlify-cli.rb
@@ -9,10 +9,9 @@ class NetlifyCli < Formula
   head "https://github.com/netlify/cli.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c91d1e2be39205db743d7d742004719b109b4ad718eff7696f6314ed6b4f148f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5b526e8d4c36837f79428d01872b5d1ac8a495e8871ba4953f43a566cc12ecca"
-    sha256 cellar: :any_skip_relocation, catalina:      "0ecca1ff9c38b0ad88924f8ade628d2dd753eb0445da010ad03bf6e04efea3a4"
-    sha256 cellar: :any_skip_relocation, mojave:        "a1ea0bfe1dccb23870b52005c92040458d7d92031bb5daced1c6dd944753ebba"
+    sha256 cellar: :any_skip_relocation, big_sur:  "6c3909b70f0a1bd112ee0f881cc593e673015861af3ad55e764a00be7ea1a1ba"
+    sha256 cellar: :any_skip_relocation, catalina: "3fc17b93831a45210dfe03210f652e7efff34eb592946df4bdeb3fd1cb55da9b"
+    sha256 cellar: :any_skip_relocation, mojave:   "9810855dfc456d01dedca08c18e113fd7f7f9144f25613a75bbb510424e3fbc2"
   end
 
   depends_on "node"
