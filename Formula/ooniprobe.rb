e2f9d787cdd3e5efa2f6ed5bbcad3ec87f61d83c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-12 21:04:02 +0000

    ooniprobe: update 3.5.2 bottle.

--- Formula/ooniprobe.rb
+++ Formula/ooniprobe.rb
@@ -6,9 +6,10 @@ class Ooniprobe < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "722f55422e32fe5105948df1b67bce7aae7d369b88b2bcd120c19349d50f5bf1"
-    sha256 cellar: :any_skip_relocation, catalina: "1ba7f6ceb6f0562caa62a219e2304037a94971fd3e7296121ef8364cd70d1820"
-    sha256 cellar: :any_skip_relocation, mojave:   "493b512097b808920858bcf052302d7c8d57b021d484a527cfd001b4bad94a79"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6e8b378612ee1c8062b858e5ebfeaba669992f950811ac0eb363ec4f4b7d3bc7"
+    sha256 cellar: :any_skip_relocation, big_sur:       "722f55422e32fe5105948df1b67bce7aae7d369b88b2bcd120c19349d50f5bf1"
+    sha256 cellar: :any_skip_relocation, catalina:      "1ba7f6ceb6f0562caa62a219e2304037a94971fd3e7296121ef8364cd70d1820"
+    sha256 cellar: :any_skip_relocation, mojave:        "493b512097b808920858bcf052302d7c8d57b021d484a527cfd001b4bad94a79"
   end
 
   depends_on "go" => :build
