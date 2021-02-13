5f2751a11ec3172bd6a85d80158b02914e0bbffc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-13 01:40:18 +0000

    code-server: update 3.9.0 bottle.

--- Formula/code-server.rb
+++ Formula/code-server.rb
@@ -6,10 +6,10 @@ class CodeServer < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5c7f2f97dccfea452f85bd97068e2018575afe4633173887b2c6751649ded431"
-    sha256 cellar: :any_skip_relocation, big_sur:       "52be90308e8b09b5fd9d74d1778dc2d4cf43d35e048ad0e171bbfa0ff41f19fc"
-    sha256 cellar: :any_skip_relocation, catalina:      "de0c1e1abca52e96877b8e62d8b52e177a67e873882f7e72bce6f2eab7736440"
-    sha256 cellar: :any_skip_relocation, mojave:        "2d99d114e087e90336e5257caf14fce64b0d5db3caff357c7f86b2f81fb2292a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "fc691d8a7ee8bb0196b88b07e7632f72f5672c6fc282bf43ceac105477bd76c6"
+    sha256 cellar: :any_skip_relocation, big_sur:       "d2a74e6e8ddf3d237bfc88db314f6f08359aa20f1d281fc362c98e434abae926"
+    sha256 cellar: :any_skip_relocation, catalina:      "01367ad466c969e70d3dec46a3cd4dcd73ca1757ce7716240968a71b05ac1422"
+    sha256 cellar: :any_skip_relocation, mojave:        "ae5c19804b128fc9efa1f580ecc5fb5b97118410c82a7058cefdaf0dfd268a8e"
   end
 
   depends_on "python@3.9" => :build
