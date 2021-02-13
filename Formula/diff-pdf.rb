cba8ba2fb954d958947f0e4d7a02eb788c5f23f3
Thierry Moisan <thierry.moisan@gmail.com>
2021-02-13 14:10:52 -0500

    diff-pdf 0.5
    * diff-pdf 0.5
    * update diff-pdf.rb
    
    Closes #71055.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/diff-pdf.rb
+++ Formula/diff-pdf.rb
@@ -1,10 +1,9 @@
 class DiffPdf < Formula
   desc "Visually compare two PDF files"
   homepage "https://vslavik.github.io/diff-pdf/"
-  url "https://github.com/vslavik/diff-pdf/releases/download/v0.4.1/diff-pdf-0.4.1.tar.gz"
-  sha256 "0eb81af6b06593488acdc5924a199f74fe3df6ecf2a0f1be208823c021682686"
+  url "https://github.com/vslavik/diff-pdf/releases/download/v0.5/diff-pdf-0.5.tar.gz"
+  sha256 "e7b8414ed68c838ddf6269d11abccdb1085d73aa08299c287a374d93041f172e"
   license "GPL-2.0-only"
-  revision 10
 
   bottle do
     sha256 cellar: :any, arm64_big_sur: "86e670f863f6b811b886a7ae58754a44325ec03b72967e594e07c26f9b2f4e5c"
@@ -21,21 +20,7 @@ class DiffPdf < Formula
   depends_on "poppler"
   depends_on "wxmac"
 
-  # Fix build with recent cairo, remove in next release
-  # https://github.com/vslavik/diff-pdf/pull/69
-  patch do
-    url "https://github.com/vslavik/diff-pdf/commit/00fd9ab8.patch?full_index=1"
-    sha256 "62e37adf219f9b822f5a313367b41596873308eb3699fa1578486bfc4f10821c"
-  end
-
   def install
-    # Remove when patch is removed
-    touch "AUTHORS"
-    touch "NEWS"
-    touch "README"
-    touch "ChangeLog"
-    system "autoreconf", "-fiv"
-
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
