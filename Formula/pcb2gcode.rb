d639107542e21225247689ca2bb68afd7dee79b4
Bo Anderson <mail@boanderson.me>
2021-02-13 23:50:22 +0000

    pcb2gcode 2.2.3
    Closes #71076.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pcb2gcode.rb
+++ Formula/pcb2gcode.rb
@@ -1,10 +1,9 @@
 class Pcb2gcode < Formula
   desc "Command-line tool for isolation, routing and drilling of PCBs"
   homepage "https://github.com/pcb2gcode/pcb2gcode"
-  url "https://github.com/pcb2gcode/pcb2gcode/archive/v2.1.0.tar.gz"
-  sha256 "ee546f0e002e83434862c7a5a2171a2276038d239909a09adb36e148e7d7319a"
+  url "https://github.com/pcb2gcode/pcb2gcode/archive/v2.2.3.tar.gz"
+  sha256 "2400917b84d05b45aab6d1af8d7b2aefc6f3fbc199414ed825bf6cb3d4aba28e"
   license "GPL-3.0-or-later"
-  revision 1
   head "https://github.com/pcb2gcode/pcb2gcode.git"
 
   bottle do
@@ -16,78 +15,25 @@ class Pcb2gcode < Formula
   # Release 2.0.0 doesn't include an autoreconfed tarball
   # glibmm, gtkmm and librsvg are used only in unittests,
   # and are therefore not needed at runtime.
-  depends_on "atkmm" => :build
+  depends_on "atkmm@2.28" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "cairomm@1.14" => :build
-  depends_on "glibmm" => :build
+  depends_on "glibmm@2.66" => :build
   depends_on "gtkmm" => :build
   depends_on "librsvg" => :build
   depends_on "libsigc++@2" => :build
   depends_on "libtool" => :build
-  depends_on "pangomm" => :build
+  depends_on "pangomm@2.46" => :build
   depends_on "pkg-config" => :build
+  depends_on "boost"
   depends_on "gerbv"
 
-  # Upstream maintainer claims that the geometry library from boost >= 1.67
-  # is severely broken. Remove the vendoring once fixed.
-  # See https://github.com/Homebrew/homebrew-core/pull/30914#issuecomment-411662760
-  # and https://svn.boost.org/trac10/ticket/13645
-  resource "boost" do
-    url "https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.bz2"
-    sha256 "5721818253e6a0989583192f96782c4a98eb6204965316df9f5ad75819225ca9"
-
-    # Fix build on Xcode 11.4
-    patch do
-      url "https://github.com/boostorg/build/commit/b3a59d265929a213f02a451bb63cea75d668a4d9.patch?full_index=1"
-      sha256 "04a4df38ed9c5a4346fbb50ae4ccc948a1440328beac03cb3586c8e2e241be08"
-      directory "tools/build"
-    end
-  end
-
   def install
-    resource("boost").stage do
-      # Force boost to compile with the desired compiler
-      open("user-config.jam", "a") do |file|
-        file.write "using darwin : : #{ENV.cxx} ;\n"
-      end
-
-      bootstrap_args = %W[
-        --prefix=#{buildpath}/boost
-        --libdir=#{buildpath}/boost/lib
-        --with-libraries=program_options
-        --without-icu
-      ]
-
-      args = %W[
-        --prefix=#{buildpath}/boost
-        --libdir=#{buildpath}/boost/lib
-        -d2
-        -j#{ENV.make_jobs}
-        --ignore-site-config
-        --layout=tagged
-        --user-config=user-config.jam
-        install
-        threading=multi
-        link=static
-        optimization=space
-        variant=release
-        cxxflags=-std=c++11
-      ]
-
-      args << "cxxflags=-stdlib=libc++" << "linkflags=-stdlib=libc++" if ENV.compiler == :clang
-
-      system "./bootstrap.sh", *bootstrap_args
-      system "./b2", "headers"
-      system "./b2", *args
-    end
-
     system "autoreconf", "-fvi"
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
-                          "--prefix=#{prefix}",
-                          "--with-boost=#{buildpath}/boost",
-                          "--enable-static-boost"
+                          "--prefix=#{prefix}"
     system "make", "install"
   end
 
