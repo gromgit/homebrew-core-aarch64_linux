a7a97a9818dbbfc299a2d21a4e22921513917052
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-13 21:14:23 +0000

    mas: update 1.8.0 bottle.

--- Formula/mas.rb
+++ Formula/mas.rb
@@ -8,9 +8,9 @@ class Mas < Formula
   head "https://github.com/mas-cli/mas.git"
 
   bottle do
-    sha256 cellar: :any, big_sur:  "432091a534d0a1f753fcf953fb1c20c10ef5762b7f8922cb378c3dcd36898d30"
-    sha256 cellar: :any, catalina: "e6de2f98ebd21885a9827664fe1b7eea5516a7fd75d5b7383e3a9b088591a27f"
-    sha256 cellar: :any, mojave:   "8b6dc4e261ebdc8e2f93afe87da3db4d482eb3d8a0358ab19a0485d434550e77"
+    sha256 cellar: :any, arm64_big_sur: "ef2ed904d1283722af006811471484cb1c9c73b255a766b5c0c65ecd1654c8d8"
+    sha256 cellar: :any, big_sur:       "dc98d69cfa94467e046b443c088a6097fe0ce0d2935e37046815fa3a984a0ca4"
+    sha256 cellar: :any, catalina:      "2e7ffedf674543f98c2b95868b6a23db208cb2e6a3ec1ddbb3553ddab0cf9a68"
   end
 
   depends_on "carthage" => :build
