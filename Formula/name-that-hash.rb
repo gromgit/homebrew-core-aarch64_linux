8a7fb1d47e035eebf34464c45ca19da29d199ac0
harens <harensdeveloper@gmail.com>
2021-02-13 16:47:28 +0000

    name-that-hash 1.1.0 (new formula)
    Closes #71042.
    
    Signed-off-by: Caleb Xu <3536482+alebcay@users.noreply.github.com>
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>

---
+++ Formula/name-that-hash.rb
@@ -0,0 +1,58 @@
+class NameThatHash < Formula
+  include Language::Python::Virtualenv
+
+  desc "Modern hash identification system"
+  homepage "https://nth.skerritt.blog/"
+  url "https://files.pythonhosted.org/packages/95/d4/992779ebd201df89a84099cdb0c5cec911998c297414ab3c54065d3027d4/name-that-hash-1.1.0.tar.gz"
+  sha256 "9b7c7fac719958bef4226ba4df66b314264a82193fa14eeffb4b5fd7e82a61f4"
+  license "GPL-3.0-or-later"
+  head "https://github.com/HashPals/Name-That-Hash.git", branch: "main"
+
+  depends_on "python@3.9"
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/27/6f/be940c8b1f1d69daceeb0032fee6c34d7bd70e3e649ccac0951500b4720e/click-7.1.2.tar.gz"
+    sha256 "d2b5255c7c6349bc1bd1e59e08cd12acbbd63ce649f2588755783aa94dfb6b1a"
+  end
+
+  resource "colorama" do
+    url "https://files.pythonhosted.org/packages/1f/bb/5d3246097ab77fa083a61bd8d3d527b7ae063c7d8e8671b1cf8c4ec10cbe/colorama-0.4.4.tar.gz"
+    sha256 "5941b2b48a20143d2267e95b1c2a7603ce057ee39fd88e7329b0c292aa16869b"
+  end
+
+  resource "commonmark" do
+    url "https://files.pythonhosted.org/packages/60/48/a60f593447e8f0894ebb7f6e6c1f25dafc5e89c5879fdc9360ae93ff83f0/commonmark-0.9.1.tar.gz"
+    sha256 "452f9dc859be7f06631ddcb328b6919c67984aca654e5fefb3914d54691aed60"
+  end
+
+  resource "loguru" do
+    url "https://files.pythonhosted.org/packages/6d/25/0d65383fc7b4f4ce9505d16773b2b2a9f0f465ef00ab337d66afff47594a/loguru-0.5.3.tar.gz"
+    sha256 "b28e72ac7a98be3d28ad28570299a393dfcd32e5e3f6a353dec94675767b6319"
+  end
+
+  resource "Pygments" do
+    url "https://files.pythonhosted.org/packages/e1/86/8059180e8217299079d8719c6e23d674aadaba0b1939e25e0cc15dcf075b/Pygments-2.7.4.tar.gz"
+    sha256 "df49d09b498e83c1a73128295860250b0b7edd4c723a32e9bc0d295c7c2ec337"
+  end
+
+  resource "rich" do
+    url "https://files.pythonhosted.org/packages/8c/65/8743a4b98585dbebf943aa8d8d30421606b492decfde9b8ffc3d5812a791/rich-9.10.0.tar.gz"
+    sha256 "e0f2db62a52536ee32f6f584a47536465872cae2b94887cf1f080fb9eaa13eb2"
+  end
+
+  resource "typing-extensions" do
+    url "https://files.pythonhosted.org/packages/16/06/0f7367eafb692f73158e5c5cbca1aec798cdf78be5167f6415dd4205fa32/typing_extensions-3.7.4.3.tar.gz"
+    sha256 "99d4073b617d30288f569d3f13d2bd7548c3a7e4c8de87db09a9d29bb3a4a60c"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    hash = "5f4dcc3b5aa765d61d8327deb882cf99"
+    output = shell_output("#{bin}/nth --text #{hash}")
+    assert_match "#{hash}\n", output
+    assert_match "MD5, HC: 0 JtR: raw-md5 Summary: Used for Linux Shadow files.\n", output
+  end
+end
