b81bdd21a50ee64cc7b796db14e835ad1a6764f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-13 06:18:41 +0000

    feedgnuplot: update 1.56 bottle.

--- Formula/feedgnuplot.rb
+++ Formula/feedgnuplot.rb
@@ -6,9 +6,9 @@ class Feedgnuplot < Formula
   license any_of: ["GPL-1.0-or-later", "Artistic-1.0"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, catalina:    "7d394a581a614dcc5130eac02310e58f994067b94a8dbd413c983157e3d37cc2"
-    sha256 cellar: :any_skip_relocation, mojave:      "76988d6017ae6c60402ef6eb02046e4a73fbc67e64ac8f55442a661dd1689832"
-    sha256 cellar: :any_skip_relocation, high_sierra: "55c59a68946e0979048dc4ef95f8746053d4b9e8c5f0d1709f781a69708051a8"
+    sha256 cellar: :any_skip_relocation, big_sur:  "ed67bb9cd920c46a467874b497e9b74b273d49cbe42b43ca0a17bb0639e0453e"
+    sha256 cellar: :any_skip_relocation, catalina: "0878f19ce28a67c6a9ef317e79bb1650b22047ff19afd863f3cdaa0a5a726967"
+    sha256 cellar: :any_skip_relocation, mojave:   "806ef63d60125e9489079ccd72985cc484fd3f671a97b396ed00aad5166676b3"
   end
 
   depends_on "gnuplot"
