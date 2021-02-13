bd3bc1c7feeec5677c9c4e9b6874a5780bcc5ff8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-13 18:18:14 +0000

    sourcery: update 1.2.0 bottle.

--- Formula/sourcery.rb
+++ Formula/sourcery.rb
@@ -7,9 +7,9 @@ class Sourcery < Formula
   head "https://github.com/krzysztofzablocki/Sourcery.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4ecd800da10736b64af20c2349af45f7bf3f02b6a04ad66451b64dea39ce3ece"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2dc8d382419ed945345bf1cd438798efbfe034390e79f909565d6e528f71f934"
-    sha256 cellar: :any_skip_relocation, catalina:      "d0e76778b9bd1386d55104e94d4b7ff17790fcd75e3366804224c3ae9902f9e2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4cee4c391628bf3b03420cc83b62aa81404b4be9bfa058d13dfc1d6220a3fea4"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a7a4609988ba6f890a9a96cab4ee94777b740aed1786241ea8ab818e66ab7c65"
+    sha256 cellar: :any_skip_relocation, catalina:      "d7caf63356f1df5977bc2452590144f794763388d4beecc49697735f98c9b7a7"
   end
 
   depends_on xcode: "12.0"
