484fdda646d4ca08bf03d36dc92d352703b34aad
Rui Chen <rui@chenrui.dev>
2021-02-13 18:18:12 -0500

    kde-ki18n 5.79.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/kde-ki18n.rb
+++ Formula/kde-ki18n.rb
@@ -1,8 +1,8 @@
 class KdeKi18n < Formula
   desc "KDE Gettext-based UI text internationalization"
   homepage "https://api.kde.org/frameworks/ki18n/html/index.html"
-  url "https://download.kde.org/stable/frameworks/5.78/ki18n-5.78.0.tar.xz"
-  sha256 "13352d2ce158c09edde129712adc12ca50c81bbcafe2df924192b5faf2dc4e55"
+  url "https://download.kde.org/stable/frameworks/5.79/ki18n-5.79.0.tar.xz"
+  sha256 "33be21c3e4b0de8d942fde7a0d3c34a85cc48440ee745375b6a71ed6993f4a8b"
   license all_of: [
     "BSD-3-Clause",
     "LGPL-2.0-or-later",
