8609a2455834b67937a1604f3e5d210f5669dbd1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-14 00:51:23 +0000

    xh: add 0.7.0 bottle.

--- Formula/xh.rb
+++ Formula/xh.rb
@@ -6,6 +6,13 @@ class Xh < Formula
   license "MIT"
   head "https://github.com/ducaale/xh.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ba56c393af8c7c69f36da15e7b31fd3dc74352633660b2b245b477a7e6cbfbee"
+    sha256 cellar: :any_skip_relocation, big_sur:       "6d8dea8525852e9fc1215e150e4d4bc273905036b3835483a51a860881afeee8"
+    sha256 cellar: :any_skip_relocation, catalina:      "8530dbb7d683d9c96ea7a463b0327955d3da49264a4fea07371b8d39b6288e3d"
+    sha256 cellar: :any_skip_relocation, mojave:        "fe97a5675e84bf52d192fc964917a34ca02e98afe3d762e76ade103d62a0ea23"
+  end
+
   depends_on "rust" => :build
 
   def install
