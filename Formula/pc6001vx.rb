6dca2dccd4cd544400df71dbcebdbf9d4ccf6785
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-14 04:21:44 +0000

    pc6001vx: update 3.7.0 bottle.

--- Formula/pc6001vx.rb
+++ Formula/pc6001vx.rb
@@ -7,10 +7,10 @@ class Pc6001vx < Formula
   head "https://github.com/eighttails/PC6001VX.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "44404d7b3752cecfa0a5f6ddca04fa30f8f8dd10fb3c262cd9d2e69c5cb3958c"
-    sha256 cellar: :any, big_sur:       "8b5a64b028a8534033f6ea1b6101aeb1599bb497a8f2fd55ddff718e020a19a3"
-    sha256 cellar: :any, catalina:      "80de7e25b021a3913630a7c56ed5e5d5b56313806ab837dbc7111b8a6561e115"
-    sha256 cellar: :any, mojave:        "f8ed76c4f02f54d813f476a83ea4500e4c79d72105283a55cd94eaf47eb6d692"
+    sha256 cellar: :any, arm64_big_sur: "f2f336642c7a262778f1488c7393141001d44f9926561317f718dbf21e684000"
+    sha256 cellar: :any, big_sur:       "89f9afe028baa7e2ff9bac2791f5d0c59186bafe5de3eb90817f229157ab35ce"
+    sha256 cellar: :any, catalina:      "38f1c392ed9cb5042619b663c8ef659f64ee3d8004beac5966af534cf3a977f8"
+    sha256 cellar: :any, mojave:        "12b257f629ecda8063fdede742c95ac88bf0adac8b99923cda54acacfc83136b"
   end
 
   depends_on "pkg-config" => :build
