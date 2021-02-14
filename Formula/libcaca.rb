ed42b2ec2a6187d52b0376bccd9e5db9872dc66e
Sam Ford <1584702+samford@users.noreply.github.com>
2021-02-14 13:34:41 -0500

    libcaca: update stable and livecheck (#71140)

--- Formula/libcaca.rb
+++ Formula/libcaca.rb
@@ -1,7 +1,7 @@
 class Libcaca < Formula
   desc "Convert pixel information into colored ASCII art"
   homepage "http://caca.zoy.org/wiki/libcaca"
-  url "http://caca.zoy.org/files/libcaca/libcaca-0.99.beta19.tar.gz"
+  url "http://caca.zoy.org/raw-attachment/wiki/libcaca/libcaca-0.99.beta19.tar.gz"
   mirror "https://www.mirrorservice.org/sites/distfiles.macports.org/libcaca/libcaca-0.99.beta19.tar.gz"
   mirror "https://fossies.org/linux/privat/libcaca-0.99.beta19.tar.gz"
   version "0.99b19"
@@ -9,14 +9,12 @@ class Libcaca < Formula
   license "WTFPL"
   revision 3
 
-  # The regex here matches unstable releases and is loose about it (`.*`), as
-  # there are currently only beta releases and we don't know if there will be
-  # releases candidates, etc. before there's a stable release. Hopefully we can
-  # restrict this to stable releases in the future but it has to be loose for
-  # the moment.
+  # The regex here is looser than usual because it has to match unstable
+  # versions for now. If this software is updated in the future to a stable
+  # version, this regex should be modified to not match unstable versions.
   livecheck do
-    url :head
-    regex(/^v?(\d+(?:\.\d+)+.*)/i)
+    url :homepage
+    regex(/href=.*?libcaca[._-]v?(\d+(?:\.\d+)+.*?)\.t/i)
   end
 
   bottle do
