122588111c36bb249c88d956a3322b460be24f26
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-14 04:22:13 +0000

    libupnp: update 1.14.1 bottle.

--- Formula/libupnp.rb
+++ Formula/libupnp.rb
@@ -11,11 +11,10 @@ class Libupnp < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "ab146b72831a64ee990f311d4894b020550f241095d2adcc030642d2324afd99"
-    sha256 cellar: :any, big_sur:       "13f2cab18c8b12b11802c2bd8dd0cf62b15c44e8f3ad008f2acc679d112adb11"
-    sha256 cellar: :any, catalina:      "f687168c37ba09bb0ee06b2d0d373c6de2f8d5511c563191788e45282fd104af"
-    sha256 cellar: :any, mojave:        "b7e3d1d7fc0a4011e093f040f71578f549e06b7702d9ec29c9595fb11fa206f6"
-    sha256 cellar: :any, high_sierra:   "70340bed620fd9c537881d58a6b0f720689fbf0ebf9dbfa9acfdb846190e9c69"
+    sha256 cellar: :any, arm64_big_sur: "b1f237de939574c2c4e793fa678207bd05d22a215de0caf8353079aef307292d"
+    sha256 cellar: :any, big_sur:       "92fc6f925846fbbb86f4015c98f50aa10179d63b5fc9db18fc62fd502585edb6"
+    sha256 cellar: :any, catalina:      "b500f8ab453b1496f8a5ca83daf29a201114729bf8b6a5e60773aa9dffc1c7ce"
+    sha256 cellar: :any, mojave:        "2a380ff4f135f0cade7152de5250ce257afd7e2f68527ebe64add7afc248ab83"
   end
 
   def install
