47e5d010f3749eadcafb94b5746093cfcaeeeea6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-14 13:32:26 +0000

    antlr@2: update 2.7.7_4 bottle.

--- Formula/antlr@2.rb
+++ Formula/antlr@2.rb
@@ -7,9 +7,10 @@ class AntlrAT2 < Formula
   revision 4
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "cc27645bb089a3ffc38aeeb4dcc7c5352d93472ac31d7e9853b0b5b90a695e64"
-    sha256 cellar: :any_skip_relocation, catalina: "b3a7378ef4a657176107a37a6d5661b9eb3750f7407ebe081200aa8b45d6d480"
-    sha256 cellar: :any_skip_relocation, mojave:   "5642de8d8012c11705b3199f5daf8758d8029333ae9eb4ab113e80069e49ef57"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "130f56b4182f57e74a535c97948667ff1b1e13bd821562ef573d048676db1e3f"
+    sha256 cellar: :any_skip_relocation, big_sur:       "cc27645bb089a3ffc38aeeb4dcc7c5352d93472ac31d7e9853b0b5b90a695e64"
+    sha256 cellar: :any_skip_relocation, catalina:      "b3a7378ef4a657176107a37a6d5661b9eb3750f7407ebe081200aa8b45d6d480"
+    sha256 cellar: :any_skip_relocation, mojave:        "5642de8d8012c11705b3199f5daf8758d8029333ae9eb4ab113e80069e49ef57"
   end
 
   keg_only :versioned_formula
