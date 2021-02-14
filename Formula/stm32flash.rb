60b8bf40b90d93147bbb0de12e7b78ee4d9bdc2e
Rui Chen <rui@chenrui.dev>
2021-02-13 20:12:25 -0500

    stm32flash 0.6
    Closes #71107.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/stm32flash.rb
+++ Formula/stm32flash.rb
@@ -1,8 +1,8 @@
 class Stm32flash < Formula
   desc "Open source flash program for STM32 using the ST serial bootloader"
   homepage "https://sourceforge.net/projects/stm32flash/"
-  url "https://downloads.sourceforge.net/project/stm32flash/stm32flash-0.5.tar.gz"
-  sha256 "97aa9422ef02e82f7da9039329e21a437decf972cb3919ad817f70ac9a49e306"
+  url "https://downloads.sourceforge.net/project/stm32flash/stm32flash-0.6.tar.gz"
+  sha256 "ee9b40d4d3e5cd28b993e08ae2a2c3c559b6bea8730cd7e1d40727dedb1dda09"
 
   livecheck do
     url :stable
