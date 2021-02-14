f63f8a3341b1d8ae22bca7d67bd298515762aee2
rui <chenrui333@gmail.com>
2021-02-13 23:14:48 -0500

    mysqltuner 1.7.21 (#71086)

--- Formula/mysqltuner.rb
+++ Formula/mysqltuner.rb
@@ -1,8 +1,8 @@
 class Mysqltuner < Formula
   desc "Increase performance and stability of a MySQL installation"
   homepage "https://raw.github.com/major/MySQLTuner-perl/master/mysqltuner.pl"
-  url "https://github.com/major/MySQLTuner-perl/archive/1.7.17.tar.gz"
-  sha256 "c82f29aa017360ab8888808393bd06e79af6fa447a62908a3b3dddea92b768b5"
+  url "https://github.com/major/MySQLTuner-perl/archive/1.7.21.tar.gz"
+  sha256 "b6e39f0768a132bca59708f4d7b0edba935a0f59bbb3ac38023d5835f7563caa"
   license "GPL-3.0-or-later"
   head "https://github.com/major/MySQLTuner-perl.git"
 
