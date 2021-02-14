66d843643b7242122e2955415506bffd3e7c43fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-14 03:17:16 +0000

    imagemagick: update 7.0.11-0 bottle.

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -13,10 +13,10 @@ class Imagemagick < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "7cde5a382da6be1a8f33e4988cce150e6f8df7c46fa9a25fbefb826c4e6d84fe"
-    sha256 big_sur:       "a43cd230ad609c812e488c7dfd25d6f5001aebb64e070497662720b04a9b1286"
-    sha256 catalina:      "616d4677ac3e33403c871a6b505d99a4945787614ea617247df8922e4b31fae1"
-    sha256 mojave:        "29577f2b4518ae48564758c4e6175911afd6c93687700916447820bf700057fb"
+    sha256 arm64_big_sur: "4387cfdac0516df057960f2a5e3a56cdd75ef9816133c6d1cd69d3e4e4dfe290"
+    sha256 big_sur:       "8f4a95955f6d22e2c38e45a1503ddaed48d431bc1d5a59d2b06f2b1b4e5dbf8f"
+    sha256 catalina:      "c99648a4dd4fe817a36153de258077e8b2d67f2859d24647ad57cb26017a11d2"
+    sha256 mojave:        "01b9a1e6237d03fa1669e4c1a9c4b7e66b81ea5e8dbbbad7a1bdeebfa13280ae"
   end
 
   depends_on "pkg-config" => :build
