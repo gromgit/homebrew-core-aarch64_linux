fead4d02121ca1a11f097b86690d11742b057c01
Will Lovett <will.lovett@arm.com>
2021-02-14 13:08:38 +0000

    llvm: use main branch for HEAD (#71127)
    Co-authored-by: Will Lovett <willov01@Wills-MacBook-Air.local>

--- Formula/llvm.rb
+++ Formula/llvm.rb
@@ -3,7 +3,7 @@ class Llvm < Formula
   homepage "https://llvm.org/"
   # The LLVM Project is under the Apache License v2.0 with LLVM Exceptions
   license "Apache-2.0"
-  head "https://github.com/llvm/llvm-project.git"
+  head "https://github.com/llvm/llvm-project.git", branch: "main"
 
   stable do
     url "https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.1/llvm-project-11.0.1.src.tar.xz"
