2582a54a1962bfae2ff53b99e366024bc8c78866
Dustin Rodrigues <dust.rod@gmail.com>
2021-02-15 19:29:42 +0000

    docker-credential-helper-ecr: update 0.5.0 bottle.

--- Formula/docker-credential-helper-ecr.rb
+++ Formula/docker-credential-helper-ecr.rb
@@ -12,9 +12,10 @@ class DockerCredentialHelperEcr < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "822e7f36e21c109c0228fd53858781f872a4176a712b591a775c38769edc5189"
-    sha256 cellar: :any_skip_relocation, catalina: "8506b1954205b0ccc6622738219606deb595da298797455442f5e7590886a1d8"
-    sha256 cellar: :any_skip_relocation, mojave:   "ade7af932bef1787cd560cb2befcd2ce9dcaa4ee694cac37dbf4a86265186667"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0f76feceea1f591784fb2bb273950336675228c1d9e04d91af48d9bc6b700ddb"
+    sha256 cellar: :any_skip_relocation, big_sur:       "822e7f36e21c109c0228fd53858781f872a4176a712b591a775c38769edc5189"
+    sha256 cellar: :any_skip_relocation, catalina:      "8506b1954205b0ccc6622738219606deb595da298797455442f5e7590886a1d8"
+    sha256 cellar: :any_skip_relocation, mojave:        "ade7af932bef1787cd560cb2befcd2ce9dcaa4ee694cac37dbf4a86265186667"
   end
 
   depends_on "go" => :build
