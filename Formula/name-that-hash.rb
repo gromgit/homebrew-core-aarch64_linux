eca20a8c5abe2f60fb2756f5721e18c0b1658d26
Dustin Rodrigues <dust.rod@gmail.com>
2021-02-15 18:01:45 -0500

    name-that-hash 1.1.3
    Closes #71222.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/name-that-hash.rb
+++ Formula/name-that-hash.rb
@@ -3,8 +3,8 @@ class NameThatHash < Formula
 
   desc "Modern hash identification system"
   homepage "https://nth.skerritt.blog/"
-  url "https://files.pythonhosted.org/packages/95/d4/992779ebd201df89a84099cdb0c5cec911998c297414ab3c54065d3027d4/name-that-hash-1.1.0.tar.gz"
-  sha256 "9b7c7fac719958bef4226ba4df66b314264a82193fa14eeffb4b5fd7e82a61f4"
+  url "https://files.pythonhosted.org/packages/1b/45/c4545b48088fb5d2c10af10b7dc050dfe5f579ac9a25ca38a22fd6957c46/name-that-hash-1.1.3.tar.gz"
+  sha256 "a33dafe987a38ea03439001a26449cb546214749eaf1154610a5b0b656c64f4f"
   license "GPL-3.0-or-later"
   head "https://github.com/HashPals/Name-That-Hash.git", branch: "main"
 
@@ -38,13 +38,13 @@ class NameThatHash < Formula
   end
 
   resource "Pygments" do
-    url "https://files.pythonhosted.org/packages/e1/86/8059180e8217299079d8719c6e23d674aadaba0b1939e25e0cc15dcf075b/Pygments-2.7.4.tar.gz"
-    sha256 "df49d09b498e83c1a73128295860250b0b7edd4c723a32e9bc0d295c7c2ec337"
+    url "https://files.pythonhosted.org/packages/19/d0/dec5604a275b19b0ebd2b9c43730ce39549c8cd8602043eaf40c541a7256/Pygments-2.8.0.tar.gz"
+    sha256 "37a13ba168a02ac54cc5891a42b1caec333e59b66addb7fa633ea8a6d73445c0"
   end
 
   resource "rich" do
-    url "https://files.pythonhosted.org/packages/8c/65/8743a4b98585dbebf943aa8d8d30421606b492decfde9b8ffc3d5812a791/rich-9.10.0.tar.gz"
-    sha256 "e0f2db62a52536ee32f6f584a47536465872cae2b94887cf1f080fb9eaa13eb2"
+    url "https://files.pythonhosted.org/packages/7b/5a/5ade9c4713b1745970b6276b6c7d5ae37d0cafd453c6c4f1a4b44833980a/rich-9.11.0.tar.gz"
+    sha256 "f8f08fdac6bd67dc2dd7fe976da702d748487aa9eb5d050c48b2321bc67ed659"
   end
 
   resource "typing-extensions" do
