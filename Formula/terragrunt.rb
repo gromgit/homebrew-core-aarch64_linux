dc2811cbdc87b9383926107a920297dcc7ede015
rui <rui@chenrui.dev>
2021-02-16 16:43:26 +0000

    terragrunt 0.28.5
    * terragrunt 0.28.5
    * Update terragrunt.rb
    
    Closes #71285.
    
    Co-authored-by: rui <rui@meetup.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -1,8 +1,8 @@
 class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state"
   homepage "https://github.com/gruntwork-io/terragrunt"
-  url "https://github.com/gruntwork-io/terragrunt/archive/v0.28.4.tar.gz"
-  sha256 "8ad4fa6e9e9c075a8a04f76117a6ab05a062cd6fe0fd0ed1c23937c7ec272839"
+  url "https://github.com/gruntwork-io/terragrunt/archive/v0.28.5.tar.gz"
+  sha256 "8b8dfd95e997878533116b71716eb749434f10fa3ade549f887c451d59fb804c"
   license "MIT"
 
   bottle do
@@ -15,7 +15,7 @@ class Terragrunt < Formula
   depends_on "terraform"
 
   def install
-    system "go", "build", "-ldflags", "-X main.VERSION=v#{version}", *std_go_args
+    system "go", "build", "-ldflags", "-s -w -X main.VERSION=v#{version}", *std_go_args
   end
 
   test do
