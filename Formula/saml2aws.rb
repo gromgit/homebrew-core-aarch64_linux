b9a6fd6791dab17e8b3137a2a73d311a70ffb05f
rui <rui@chenrui.dev>
2021-02-16 17:20:37 +0000

    saml2aws: add 2.28.0 bottle.

--- Formula/saml2aws.rb
+++ Formula/saml2aws.rb
@@ -6,6 +6,13 @@ class Saml2aws < Formula
   revision: "5199bd35d3cffbfed3683b728828f55c64501c21"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "fdd949819602c09ed3533d0b284070c8efa5d1176690128685d41b8cb8ccfa79"
+    sha256 cellar: :any_skip_relocation, big_sur:       "64dfca55124c588a0550085c9bba5048e3747df684c66248f63b522879cdf23e"
+    sha256 cellar: :any_skip_relocation, catalina:      "43b5b26c143c42ab2093cb5a6a3511b7417ec681a6130f73d22b64f9ff7b90ff"
+    sha256 cellar: :any_skip_relocation, mojave:        "e4ca2bac31ead962d44e9769fd04e8d322c53a5f6e169a1d04e942eeb6bdd35e"
+  end
+
   depends_on "go" => :build
 
   def install
