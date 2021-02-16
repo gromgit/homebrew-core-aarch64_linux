ea94a7858ee8c5af20dc9633024b9469bb146167
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-16 01:41:35 +0000

    r: update 4.0.4 bottle.

--- Formula/r.rb
+++ Formula/r.rb
@@ -11,10 +11,10 @@ class R < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "67c3d8b4f5adcfe3da2651c5f300b56a2129130db579c65eda952023ef305849"
-    sha256 big_sur:       "d3859bfc06a1cac65a60a43f105aec2b3c2ebdb465b69eb6988a4686eefd0819"
-    sha256 catalina:      "057a9e1fafd85ec2f65ecbfb651f6299cbbaab630158d6cf6d1057b43f7b9f29"
-    sha256 mojave:        "ffffeb9ad7f2892187d26fde97bc8f47ba4b27bf95ea8bcc1ce43a09771f7d51"
+    sha256 arm64_big_sur: "850201a102aa7b0ae7865b32ed27b1f252e968d93aa663929abc475394a2d21b"
+    sha256 big_sur:       "1bf6b564592e5908384a43a78b8039a9077d2953cccb8ec7cff3aeb11961596f"
+    sha256 catalina:      "9ac4f83c55c651d5dfb22f1b9bca3f90ac60494b226a55bb74085a454dcd8027"
+    sha256 mojave:        "0a7797f9411a5339be8caf9e7efb0ceb283002916156e1f8a36b4ada02d60d4f"
   end
 
   depends_on "pkg-config" => :build
