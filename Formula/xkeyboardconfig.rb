c363efbf163ace9b08e486eaa7c42f0924c72b3e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-16 02:22:12 +0000

    xkeyboardconfig: update 2.32 bottle.

--- Formula/xkeyboardconfig.rb
+++ Formula/xkeyboardconfig.rb
@@ -7,10 +7,10 @@ class Xkeyboardconfig < Formula
   head "https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ed5f4d5be9381ef91eadba64cf368ab3e4b0e7d490a5b472097c8671a837e83b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5d2d8508d8ea7a1aeb33520285088526d460f3e796911bde60af52e0fee0cab7"
-    sha256 cellar: :any_skip_relocation, catalina:      "657ec08d8201cd932171f8f9acecd94e62f66657ea7c730810fb2dec70e17c1f"
-    sha256 cellar: :any_skip_relocation, mojave:        "20a9ec45d3b7fbeca4dfd3bef4dff0108e7ed2481a2b3256c0dd4f152b996de5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "24e5f5e5a8367296bf39a7a4d6e821beea5030ba613bcebe6a19cefa5ddb5cc6"
+    sha256 cellar: :any_skip_relocation, big_sur:       "8baa880e15f628d34d896a1132b99923b64df53794ddb29cb366fa3272b46495"
+    sha256 cellar: :any_skip_relocation, catalina:      "292c62a8b20d29a384c7f8140205e3d9a2a5d21bc3364426e3540b874a6396e0"
+    sha256 cellar: :any_skip_relocation, mojave:        "fe7a46767c174a67d05b3342611d112716441f8d5e7f13a9c317f64b59cca3dd"
   end
 
   depends_on "gettext" => :build
