8cd4e443ba9a44ea90cb854418eb906663328ed6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-17 16:03:12 +0000

    libvisio: revision bump (icu4c 68.2)

--- Formula/libvisio.rb
+++ Formula/libvisio.rb
@@ -3,7 +3,7 @@ class Libvisio < Formula
   homepage "https://wiki.documentfoundation.org/DLP/Libraries/libvisio"
   url "https://dev-www.libreoffice.org/src/libvisio/libvisio-0.1.7.tar.xz"
   sha256 "8faf8df870cb27b09a787a1959d6c646faa44d0d8ab151883df408b7166bea4c"
-  revision 2
+  revision 3
 
   livecheck do
     url "https://dev-www.libreoffice.org/src/"
