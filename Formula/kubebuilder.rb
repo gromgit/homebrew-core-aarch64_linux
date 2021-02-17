206d88f38b103c38eeefbb3598891b29979ee617
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-17 03:17:39 +0000

    kubebuilder: update 2.3.2 bottle.

--- Formula/kubebuilder.rb
+++ Formula/kubebuilder.rb
@@ -8,11 +8,10 @@ class Kubebuilder < Formula
   head "https://github.com/kubernetes-sigs/kubebuilder.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6c271a2044413fe81d9c4671efbd2a66bce5ee9693731bfb2001292be1901a76"
-    sha256 cellar: :any_skip_relocation, big_sur:       "c9fbe3f2af36d56afc501528827dd63aa7c2e450ae55d29372ea83b717b34d59"
-    sha256 cellar: :any_skip_relocation, catalina:      "b587ddd6d67b12a7fd2635f8f4da56402133a036fe79e635b08427b401a9b71b"
-    sha256 cellar: :any_skip_relocation, mojave:        "7de399f00ecd47e3150e05d213a44886f499456ed5480c095100e329203ab399"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "62040031af53761dbe639796b5dc95278be2a048380f691563fb9cd4ef7f8041"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c34a138dbc1aaba1d5d311d536380e6a68773d126fb06e034a9f4a89f811aa2a"
+    sha256 cellar: :any_skip_relocation, big_sur:       "454a6e3ea94ed342b13768292a465b80d6b39450cd9bcf71a1d5576aec493f14"
+    sha256 cellar: :any_skip_relocation, catalina:      "34b4cc95b3a1506ab217000a44373b4615849658cdbbf21d8cc4392f747a7806"
+    sha256 cellar: :any_skip_relocation, mojave:        "ef1e51ec7876df398dcac8b1c11abc17e83f030e3fe10efa30458f94bdb2c469"
   end
 
   depends_on "git-lfs" => :build
