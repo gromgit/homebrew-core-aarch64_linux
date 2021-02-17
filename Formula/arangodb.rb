5563c044176e0f8b01c703e74c3f37d9b1d91763
rui <rui@chenrui.dev>
2021-02-17 20:16:21 +0000

    arangodb 3.7.8
    Closes #71364.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/arangodb.rb
+++ Formula/arangodb.rb
@@ -1,10 +1,9 @@
 class Arangodb < Formula
   desc "Multi-Model NoSQL Database"
   homepage "https://www.arangodb.com/"
-  url "https://download.arangodb.com/Source/ArangoDB-3.7.7.tar.gz"
-  sha256 "83ee8aeda753967043e3326843a55c5df5f2f9430f7a0a37da950be48e17265e"
+  url "https://download.arangodb.com/Source/ArangoDB-3.7.8.tar.gz"
+  sha256 "8f9aebeaee95628df3a64b29d3301fd55fabd635da2ec4d05ac694f24ff80dcd"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/arangodb/arangodb.git", branch: "devel"
 
   bottle do
