1fbfabd6c9a05e7772d920247f303e28cb1d0ac6
Caleb Xu <calebcenter@live.com>
2021-02-17 16:08:42 -0500

    libedit 20210216-3.1
    Closes #71371.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libedit.rb
+++ Formula/libedit.rb
@@ -1,9 +1,9 @@
 class Libedit < Formula
   desc "BSD-style licensed readline alternative"
   homepage "https://thrysoee.dk/editline/"
-  url "https://thrysoee.dk/editline/libedit-20191231-3.1.tar.gz"
-  version "20191231-3.1"
-  sha256 "dbb82cb7e116a5f8025d35ef5b4f7d4a3cdd0a3909a146a39112095a2d229071"
+  url "https://thrysoee.dk/editline/libedit-20210216-3.1.tar.gz"
+  version "20210216-3.1"
+  sha256 "2283f741d2aab935c8c52c04b57bf952d02c2c02e651172f8ac811f77b1fc77a"
 
   livecheck do
     url :homepage
