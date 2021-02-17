df7a53447ebc592b3e3b5932bd5ef394d248efd2
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-02-17 14:39:34 +0000

    pcalc: add 2.0.1 bottle.

--- Formula/pcalc.rb
+++ Formula/pcalc.rb
@@ -5,6 +5,13 @@ class Pcalc < Formula
   sha256 "a3b8b59bd4da9a1ee39f73303e18005f2b4a45b655f7a7cca10aa9ce173610e7"
   license "GPL-3.0-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8d7268ccb14450e15d1615919272edc4e365670c54daf7fb6b9de272c6eddf6b"
+    sha256 cellar: :any_skip_relocation, big_sur:       "b81ac1f8cc3d7ab174d2cd4873571da42988259af0ec6eaf21a277c5dcf08842"
+    sha256 cellar: :any_skip_relocation, catalina:      "cb6c78a7399eaeeee80f0d23863bbe60dd011171b0251e512d6c581b3947c4ea"
+    sha256 cellar: :any_skip_relocation, mojave:        "a66a10045261aece457eba2a4d7ef8bdd4af4e3218877a94c48797d34d54d137"
+  end
+
   uses_from_macos "ncurses"
 
   def install
