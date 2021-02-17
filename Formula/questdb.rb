641798367e74e2298732b97e645792c8446ba883
rui <chenrui333@gmail.com>
2021-02-17 11:20:14 -0500

    questdb 5.0.6.1 (#71325)

--- Formula/questdb.rb
+++ Formula/questdb.rb
@@ -1,8 +1,8 @@
 class Questdb < Formula
   desc "Time Series Database"
   homepage "https://questdb.io"
-  url "https://github.com/questdb/questdb/releases/download/5.0.6/questdb-5.0.6-no-jre-bin.tar.gz"
-  sha256 "12660aecc12040ada5b2559f039d70f13ef51333b4ec53c6f81314245d778d8a"
+  url "https://github.com/questdb/questdb/releases/download/5.0.6.1/questdb-5.0.6.1-no-jre-bin.tar.gz"
+  sha256 "0c6f1ebc33305d40463b09c05c20cc22cb3a2f6e4f054a10513f434a5faebf6e"
   license "Apache-2.0"
 
   bottle :unneeded
