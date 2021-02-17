8ec04d0deb27bc65a8c991813b1528c879f72714
Dawid Dziurla <dawidd0811@gmail.com>
2021-02-17 12:23:11 +0000

    goreleaser 0.156.2
    Closes #71327.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/goreleaser.rb
+++ Formula/goreleaser.rb
@@ -2,8 +2,8 @@ class Goreleaser < Formula
   desc "Deliver Go binaries as fast and easily as possible"
   homepage "https://goreleaser.com/"
   url "https://github.com/goreleaser/goreleaser.git",
-      tag:      "v0.156.1",
-      revision: "5e1ea32203c719404e83b06ab21dcb1d9ef18530"
+      tag:      "v0.156.2",
+      revision: "43fcfafe7ae7633c9bc0790088d1da22613d43b1"
   license "MIT"
   head "https://github.com/goreleaser/goreleaser.git"
 
