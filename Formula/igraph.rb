6895358780d342c771322ed99d245cad9c51670e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-17 17:34:16 +0000

    igraph: update 0.9.0 bottle.

--- Formula/igraph.rb
+++ Formula/igraph.rb
@@ -6,10 +6,11 @@ class Igraph < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d9b4b79ede67853df488dc2fb2fe813035eff927a301a7b74d3ec76800a7e436"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3e1078d3f10bdafee36b61d69c5af9d62db1b967ea59bf36ec4eeb2b014f48af"
-    sha256 cellar: :any_skip_relocation, catalina:      "8dc7914759c40567d1e70af9b52de41994873cae21a5e33302790e8e8f6543fb"
-    sha256 cellar: :any_skip_relocation, mojave:        "356ed288fb210fffdef90bfb55ec379cb33cd4322e74d5268fabcd9d0b945b6f"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6ae842ab97a92237987c202eb561dc309571af3d9452570d8c47edc0f4474b5f"
+    sha256 cellar: :any_skip_relocation, big_sur:       "b9d9cbcfc223d72bc33add33264d8bf6f4eb677107f1e72984114f083336fe59"
+    sha256 cellar: :any_skip_relocation, catalina:      "c02bb0b1563dfcc3e5511be93d1d20d0f38a34a588919112299b0be4780dd8f6"
+    sha256 cellar: :any_skip_relocation, mojave:        "990acbc314d5fec00ccc08bf76f3b436ac297abf3c5c303364315f41961d0aba"
   end
 
   depends_on "cmake" => :build
