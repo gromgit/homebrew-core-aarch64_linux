604748f602248522c7c8295a4de9962a55a540f5
Rylan Polster <rslpolster@gmail.com>
2021-02-17 23:05:57 -0500

    tio: replace regex assertions with string assertions

--- Formula/tio.rb
+++ Formula/tio.rb
@@ -25,6 +25,6 @@ class Tio < Formula
   test do
     # Test that tio emits the correct error output when run with an argument that is not a tty.
     # Use `script` to run tio with its stdio attached to a PTY, otherwise it will complain about that instead.
-    assert_match /Error: Not a tty device/, shell_output("script -q /dev/null #{bin}/tio /dev/null", 1).strip
+    assert_match "Error: Not a tty device", shell_output("script -q /dev/null #{bin}/tio /dev/null", 1).strip
   end
 end
