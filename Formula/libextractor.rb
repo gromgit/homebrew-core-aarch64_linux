59b7a649c91d5cda5e6636de86eaa356e01cd453
Rylan Polster <rslpolster@gmail.com>
2021-02-17 23:05:47 -0500

    libextractor: replace regex assertions with string assertions

--- Formula/libextractor.rb
+++ Formula/libextractor.rb
@@ -30,6 +30,6 @@ class Libextractor < Formula
 
   test do
     fixture = test_fixtures("test.png")
-    assert_match /Keywords for file/, shell_output("#{bin}/extract #{fixture}")
+    assert_match "Keywords for file", shell_output("#{bin}/extract #{fixture}")
   end
 end
