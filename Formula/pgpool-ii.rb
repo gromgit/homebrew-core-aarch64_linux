b166a373edd0fd0c74f1a72cc7f40aa9d4995b8c
rui <rui@chenrui.dev>
2021-02-18 04:18:36 +0000

    pgpool-ii 4.2.2
    Closes #71394.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pgpool-ii.rb
+++ Formula/pgpool-ii.rb
@@ -1,8 +1,8 @@
 class PgpoolIi < Formula
   desc "PostgreSQL connection pool server"
   homepage "https://www.pgpool.net/mediawiki/index.php/Main_Page"
-  url "https://www.pgpool.net/mediawiki/images/pgpool-II-4.2.1.tar.gz"
-  sha256 "98938af9fccc37bcea76e6422e42ce003faedb1186cdc3b9940eb6ba948cdae1"
+  url "https://www.pgpool.net/mediawiki/images/pgpool-II-4.2.2.tar.gz"
+  sha256 "99f264fc953d2a9d1b454f62d0991ef3a0004825ebcc95f9fd9bb7cbe1783ba1"
 
   livecheck do
     url "https://www.pgpool.net/mediawiki/index.php/Downloads"
