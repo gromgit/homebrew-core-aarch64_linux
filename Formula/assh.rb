58d582a2aaf20fb40773adbbb673f670b27cbc5a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-18 14:17:41 +0000

    assh: update 2.11.3 bottle.

--- Formula/assh.rb
+++ Formula/assh.rb
@@ -7,9 +7,9 @@ class Assh < Formula
   head "https://github.com/moul/assh.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "b6d3bd7c93bc22801b7d14ccefdd58e4014d0b29441d4f321b5c4e3332220b1e"
-    sha256 cellar: :any_skip_relocation, catalina: "43248132ae1cb81c554834afa0e0fd5ad4507e57aa54f7596fe7f0f5ab9821b1"
-    sha256 cellar: :any_skip_relocation, mojave:   "9c228764205ed2cd98f80e7baa13397f4cc7f944370498b42048f785e8366778"
+    sha256 cellar: :any_skip_relocation, big_sur:  "38f180a404deea504c730e59c9c4fa867f7af1c01558d64a891cd2803bf7777b"
+    sha256 cellar: :any_skip_relocation, catalina: "486b60df723e01996d1654b53ff9ee8ff46d02500290ceb35a42a1aa8fdf22f6"
+    sha256 cellar: :any_skip_relocation, mojave:   "0432605530886e05a3bae571cc33c0ae4043e8f7616fa7245d6d352bf54e2a6b"
   end
 
   depends_on "go" => :build
