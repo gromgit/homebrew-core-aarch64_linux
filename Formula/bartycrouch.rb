d152acb9ced62ef49ebddd77a77c001c8cfd1ea4
Rylan Polster <rslpolster@gmail.com>
2021-02-17 23:05:32 -0500

    bartycrouch: replace regex assertions with string assertions

--- Formula/bartycrouch.rb
+++ Formula/bartycrouch.rb
@@ -36,7 +36,7 @@ class Bartycrouch < Formula
     EOS
 
     system bin/"bartycrouch", "update"
-    assert_match /"oldKey" = "/, File.read("en.lproj/Localizable.strings")
-    assert_match /"test" = "/, File.read("en.lproj/Localizable.strings")
+    assert_match '"oldKey" = "', File.read("en.lproj/Localizable.strings")
+    assert_match '"test" = "', File.read("en.lproj/Localizable.strings")
   end
 end
