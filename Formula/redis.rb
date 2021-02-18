eb9bc908ed49345efec83ab2561ddde962262bfb
Rylan Polster <rslpolster@gmail.com>
2021-02-17 23:03:43 -0500

    redis: remove ambiguous regexp literals

--- Formula/redis.rb
+++ Formula/redis.rb
@@ -29,7 +29,7 @@ class Redis < Formula
     inreplace "redis.conf" do |s|
       s.gsub! "/var/run/redis.pid", var/"run/redis.pid"
       s.gsub! "dir ./", "dir #{var}/db/redis/"
-      s.sub!  /^bind .*$/, "bind 127.0.0.1 ::1"
+      s.sub!(/^bind .*$/, "bind 127.0.0.1 ::1")
     end
 
     etc.install "redis.conf"
