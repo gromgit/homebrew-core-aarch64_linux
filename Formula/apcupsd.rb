94a4e4566988ab31bd57d5d532a366ff0900859b
Rylan Polster <rslpolster@gmail.com>
2021-02-17 23:05:30 -0500

    apcupsd: replace regex assertions with string assertions

--- Formula/apcupsd.rb
+++ Formula/apcupsd.rb
@@ -87,6 +87,6 @@ class Apcupsd < Formula
 
   test do
     system "#{sbin}/apcupsd", "--version"
-    assert_match /usage/, shell_output("#{sbin}/apctest --help", 1)
+    assert_match "usage", shell_output("#{sbin}/apctest --help", 1)
   end
 end
