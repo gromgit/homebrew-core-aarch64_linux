cf8d5f9fa9584b794c82eccaf9e2af8e281d378c
Caleb Xu <calebcenter@live.com>
2021-02-18 08:29:37 -0500

    modman 1.14 (#71399)

--- Formula/modman.rb
+++ Formula/modman.rb
@@ -1,8 +1,8 @@
 class Modman < Formula
   desc "Module deployment script geared towards Magento development"
   homepage "https://github.com/colinmollenhour/modman"
-  url "https://github.com/colinmollenhour/modman/archive/1.13.tar.gz"
-  sha256 "d9efe8786826f54c3d2afa16f62a08e9a4e108cac429653a447055f6e5a0e8ec"
+  url "https://github.com/colinmollenhour/modman/archive/1.14.tar.gz"
+  sha256 "58ac5b27b11def9ba162881c3687f2085c06a6ed4cfb496bafdc64ce1a2eaac6"
   license "Apache-2.0"
 
   bottle :unneeded
