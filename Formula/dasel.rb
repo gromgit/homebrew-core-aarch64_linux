f9f88cacfe8b408f07d3555b5707812ec9f71dc5
Tom Wright <contact@tomwright.me>
2021-02-18 16:32:36 +0000

    dasel 1.13.1
    Closes #71427.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/dasel.rb
+++ Formula/dasel.rb
@@ -1,8 +1,8 @@
 class Dasel < Formula
   desc "JSON, YAML, TOML, XML, and CSV query and modification tool"
   homepage "https://github.com/TomWright/dasel"
-  url "https://github.com/TomWright/dasel/archive/v1.13.0.tar.gz"
-  sha256 "fa58488fb308a8219be61e37b0e2e670c361311469012a3d6471a2bea4392870"
+  url "https://github.com/TomWright/dasel/archive/v1.13.1.tar.gz"
+  sha256 "6c3110e4e7a23dc0fdd6fab851de7579ac35e70cb1a7eb7056ddda31a4d00c6a"
   license "MIT"
   head "https://github.com/TomWright/dasel.git"
 
