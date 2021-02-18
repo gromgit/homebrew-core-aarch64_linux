beb8e3f17c9779a0eca3772bb48d6504b3ef1cbc
Rylan Polster <rslpolster@gmail.com>
2021-02-17 23:03:21 -0500

    ctags: remove ambiguous regexp literals

--- Formula/ctags.rb
+++ Formula/ctags.rb
@@ -80,7 +80,7 @@ class Ctags < Formula
       }
     EOS
     system "#{bin}/ctags", "-R", "."
-    assert_match /func.*test\.c/, File.read("tags")
+    assert_match(/func.*test\.c/, File.read("tags"))
     assert_match "+regex", shell_output("ctags --version")
   end
 end
