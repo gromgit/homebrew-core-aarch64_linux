6c86b82bd81fdf54029097d6e1ce47ff9c14c9ad
Rylan Polster <rslpolster@gmail.com>
2021-02-17 23:03:26 -0500

    git-annex-remote-rclone: remove ambiguous regexp literals

--- Formula/git-annex-remote-rclone.rb
+++ Formula/git-annex-remote-rclone.rb
@@ -28,7 +28,7 @@ class GitAnnexRemoteRclone < Formula
 
     (testpath/"Hello.txt").write "Hello!"
     assert !File.symlink?("Hello.txt")
-    assert_match /^add Hello.txt.*ok.*\(recording state in git\.\.\.\)/m, shell_output("git annex add .")
+    assert_match(/^add Hello.txt.*ok.*\(recording state in git\.\.\.\)/m, shell_output("git annex add ."))
     system "git", "commit", "-a", "-m", "Initial Commit"
     assert File.symlink?("Hello.txt")
 
