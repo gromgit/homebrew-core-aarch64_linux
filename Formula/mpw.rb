331db7748ef52a984ed4ed49cae8fc087bc151a6
Maarten Billemont <lhunath@lyndir.com>
2021-02-18 11:22:43 -0500

    mpw 2.7-cli-1
    * mpw 2.7-cli-1
    * fixup! mpw 2.7-cli-1
    * fixup! mpw 2.7-cli-1
    * fixup! mpw 2.7-cli-1
    * fixup! mpw 2.7-cli-1
    
    Closes #71428.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mpw.rb
+++ Formula/mpw.rb
@@ -1,11 +1,10 @@
 class Mpw < Formula
   desc "Stateless/deterministic password and identity manager"
-  homepage "https://masterpasswordapp.com/"
-  url "https://masterpasswordapp.com/mpw-2.6-cli-5-0-g344771db.tar.gz"
-  version "2.6-cli-5"
-  sha256 "954c07b1713ecc2b30a07bead9c11e6204dd774ca67b5bdf7d2d6ad1c4eec170"
-  license "GPL-3.0"
-  revision 2
+  homepage "https://masterpassword.app/"
+  url "https://masterpassword.app/mpw-2.7-cli-1-0-gd5959582.tar.gz"
+  version "2.7-cli-1"
+  sha256 "480206dfaad5d5a7d71fba235f1f3d9041e70b02a8c1d3dda8ecba1da39d3e96"
+  license "GPL-3.0-or-later"
   head "https://gitlab.com/MasterPassword/MasterPassword.git"
 
   # The first-party site doesn't seem to list version information, so it's
@@ -28,21 +27,19 @@ class Mpw < Formula
   depends_on "ncurses"
 
   def install
+    cd "cli" unless build.head?
     cd "platform-independent/c/cli" if build.head?
 
     ENV["targets"] = "mpw"
-    # not compatible with json-c 0.14 yet
-    ENV["mpw_json"] = "0"
+    ENV["mpw_json"] = "1"
     ENV["mpw_color"] = "1"
 
     system "./build"
-    system "./mpw-cli-tests"
     bin.install "mpw"
   end
 
   test do
-    assert_equal "Jejr5[RepuSosp",
-      shell_output("#{bin}/mpw -q -Fnone -u 'Robert Lee Mitchell' -M 'banana colored duckling' " \
-                   "-tlong -c1 -a3 'masterpasswordapp.com'").strip
+    assert_equal "CefoTiciJuba7@",
+      shell_output("#{bin}/mpw -Fnone -q -u 'test' -M 'test' 'test'").strip
   end
 end
