c0024346ee53d0765a168cad20613b228ea23a67
Rylan Polster <rslpolster@gmail.com>
2021-02-17 23:05:40 -0500

    drone-cli: replace regex assertions with string assertions

--- Formula/drone-cli.rb
+++ Formula/drone-cli.rb
@@ -23,6 +23,6 @@ class DroneCli < Formula
   test do
     assert_match version.to_s, shell_output("#{bin}/drone --version")
 
-    assert_match /manage logs/, shell_output("#{bin}/drone log 2>&1")
+    assert_match "manage logs", shell_output("#{bin}/drone log 2>&1")
   end
 end
