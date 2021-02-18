f1110e7602602a79034079f0981a221a89ed6436
Rylan Polster <rslpolster@gmail.com>
2021-02-17 23:15:02 -0500

    orbit: replace regex assertions with string assertions
    This commit may also make some minor modifications to the tests
    to further improve the style (e.g. removing unecessary escapes,
    using `assert_equal` instead of `assert_match`, and removing
    unecessary regex modifiers).
    
    I have done some additional work to ensure that this formula
    continues to work as expected. I have not necessarily tested
    every aspect of this particular formula, but I have tested one
    where I made similar changes and feel confident that this will
    not cause major breakage. In some cases, I did test this particular
    formula. This is a generic commit message which has not been
    tailored to this particular formula, so it is very possible that I
    did actually test this formula.

--- Formula/orbit.rb
+++ Formula/orbit.rb
@@ -41,6 +41,6 @@ class Orbit < Formula
   end
 
   test do
-    assert_match /#{version}/, shell_output("#{bin}/orbit2-config --prefix --version")
+    assert_match version.to_s, shell_output("#{bin}/orbit2-config --prefix --version")
   end
 end
