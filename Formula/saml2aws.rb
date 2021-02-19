dff5a8065a60e5ec2279698ed4ef2037d02726e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-19 14:17:07 +0000

    saml2aws: update 2.28.2 bottle.

--- Formula/saml2aws.rb
+++ Formula/saml2aws.rb
@@ -7,10 +7,10 @@ class Saml2aws < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1ace313a31af2f3e1187769a1ad7848ae33631a04b67c1f4a1251be47deea810"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ca178f5b6fa3824d4502a4e5fb1086322bfaa076f00151c1e0070487ebb01585"
-    sha256 cellar: :any_skip_relocation, catalina:      "00799579ec4d3e0cc0655b12ced072523742e4e0a19a14568e5b7889c49467fe"
-    sha256 cellar: :any_skip_relocation, mojave:        "b3a05a3763d8d43220291725e14ba82309e2f2860b49c39bf405cb8ad8067ccb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3a15ac811a4276bca7ffd9e7c90f8e5b260c78e82d6001f8e1b61c86010a6051"
+    sha256 cellar: :any_skip_relocation, big_sur:       "02c8c34a19c9b252b0ccf3daad37e2cffa9881ac987a3e74b345e3662ce17dc4"
+    sha256 cellar: :any_skip_relocation, catalina:      "cf5705ec3df26799def37c758a884ea4dd8952bae57f758b1d290e6e506d98c4"
+    sha256 cellar: :any_skip_relocation, mojave:        "f049679b8169b048ecb4d16dda5b818a5f8d4ea2ee62b771ee7bb8b1348e1889"
   end
 
   depends_on "go" => :build
