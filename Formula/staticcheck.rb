fbf380f00b8bcd5671f57d7dd183d20090a9724d
Dustin Rodrigues <dust.rod@gmail.com>
2021-02-19 20:50:42 +0000

    staticcheck: update 2020.2.2 bottle.

--- Formula/staticcheck.rb
+++ Formula/staticcheck.rb
@@ -7,10 +7,10 @@ class Staticcheck < Formula
   head "https://github.com/dominikh/go-tools.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6b7c908b171158a2e7bd736d903cf349542d504197a753c9967be8fcb42400e4"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a515f5fc81be1069541c86a0dbb0c10ce406d5f63420e25d6736eea2d970f011"
-    sha256 cellar: :any_skip_relocation, catalina:      "c207bf44becf6d82ad8fed9eff75db5a14a106df080fb90eb8ed6e1b7a0c0375"
-    sha256 cellar: :any_skip_relocation, mojave:        "2e7d67e9a2aba779e109c18b0aac9729438033ae3666e332b83f2a9c5c84f3fe"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "316303ad3c75386ac655cccb6b8b3403e19e1ebb69ff064ec72a77530adf6615"
+    sha256 cellar: :any_skip_relocation, big_sur:       "bac9edcf563533c769e51ef5f0e57fde02cbaf1971e83abd4496013279cea909"
+    sha256 cellar: :any_skip_relocation, catalina:      "b29dc5550502b8f28464aad8d968304c9989fd63b4da499d7d8355320f82d31d"
+    sha256 cellar: :any_skip_relocation, mojave:        "debe36229bdb50d14cdd32f1727544bf82cd29f8fd269cdc59520bc78fca47d4"
   end
 
   depends_on "go"
