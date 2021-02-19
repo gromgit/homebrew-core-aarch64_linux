d69cfc3bdc308ebf064418f9dfa9faaae22c06b2
rui <chenrui333@gmail.com>
2021-02-18 19:11:00 -0500

    cromwell 57 (#71425)
    * cromwell 57
    
    * womtool 57

--- Formula/cromwell.rb
+++ Formula/cromwell.rb
@@ -1,8 +1,8 @@
 class Cromwell < Formula
   desc "Workflow Execution Engine using Workflow Description Language"
   homepage "https://github.com/broadinstitute/cromwell"
-  url "https://github.com/broadinstitute/cromwell/releases/download/56/cromwell-56.jar"
-  sha256 "955502419cfec2a687ea56f7f4a433b395af09c9a078e60a5676c7ad56c3965d"
+  url "https://github.com/broadinstitute/cromwell/releases/download/57/cromwell-57.jar"
+  sha256 "49ca5f8bc70f63cc05e450470bb9092e8826b04e3652de09788aa5435fbfeb7b"
   license "BSD-3-Clause"
 
   head do
@@ -15,8 +15,8 @@ class Cromwell < Formula
   depends_on "openjdk"
 
   resource "womtool" do
-    url "https://github.com/broadinstitute/cromwell/releases/download/56/womtool-56.jar"
-    sha256 "0f561c0d387412715f1f688ad5ae7cd4d8af3dc8d68c69ae9fc35ecae4f481c6"
+    url "https://github.com/broadinstitute/cromwell/releases/download/57/womtool-57.jar"
+    sha256 "a5cd2fb67a3ebb076d682cc8dbde04098177ca9cbd20e03a69ff92f2afd955b7"
   end
 
   def install
