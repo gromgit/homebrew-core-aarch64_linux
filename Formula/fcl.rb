52e055f74a1d8e4b23ac3777678c3a52ee229c7e
Hill Ma <maahiuzeon@gmail.com>
2021-02-20 00:34:52 -0800

    fcl: apply upstream patches to fix build error on arm.
    Co-authored-by: Bo Anderson <mail@boanderson.me>

--- Formula/fcl.rb
+++ Formula/fcl.rb
@@ -1,10 +1,24 @@
 class Fcl < Formula
   desc "Flexible Collision Library"
   homepage "https://flexible-collision-library.github.io/"
-  url "https://github.com/flexible-collision-library/fcl/archive/v0.6.1.tar.gz"
-  sha256 "c8a68de8d35a4a5cd563411e7577c0dc2c626aba1eef288cb1ca88561f8d8019"
   license "BSD-3-Clause"
   head "https://github.com/flexible-collision-library/fcl.git"
+
+  stable do
+    url "https://github.com/flexible-collision-library/fcl/archive/v0.6.1.tar.gz"
+    sha256 "c8a68de8d35a4a5cd563411e7577c0dc2c626aba1eef288cb1ca88561f8d8019"
+
+    # Fix build on ARM.
+    # Remove these patches once they are released.
+    patch do
+      url "https://github.com/flexible-collision-library/fcl/commit/83a1af61ba4efa81ec0b552b3121100044a8cf46.patch?full_index=1"
+      sha256 "0473f82522940274f8bfcd7a7b6cfb72c7cd100eb3cb34d6793c4b25bdf16c61"
+    end
+    patch do
+      url "https://github.com/flexible-collision-library/fcl/commit/cbfe1e9405aa68138ed1a8f33736429b85500dea.patch?full_index=1"
+      sha256 "510c49be01bc3f5daef9817eb2db5e26ed6888a5eab052bea07ba50aacc9a61f"
+    end
+  end
 
   bottle do
     sha256 big_sur:     "64e20a01b7a05f081b6b01ebe88722a0afec56514940130c0720e2ee5e5325df"
