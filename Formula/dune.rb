6e894f56dcb94849134ff6724370ff61f1a52bb1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-21 22:06:25 +0000

    dune: update 2.8.2 bottle.

--- Formula/dune.rb
+++ Formula/dune.rb
@@ -7,9 +7,10 @@ class Dune < Formula
   head "https://github.com/ocaml/dune.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "207990a41328767e8c46a8d3d7d8aa1db494a1243fbdf5bcd8fcca83668d57b1"
-    sha256 cellar: :any_skip_relocation, catalina: "a3603b422c90d1fa525f5b9581f18b4645aeb39e3ccb7450f11d6e833a92e697"
-    sha256 cellar: :any_skip_relocation, mojave:   "b6906bda3c1557367516849bda5957596a4cc4ca0407c04282a7d3d4ae3d873c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "91a8b8257c944a1701090324e761f397a0034996e605a95063ef0ae3e98d95f7"
+    sha256 cellar: :any_skip_relocation, big_sur:       "207990a41328767e8c46a8d3d7d8aa1db494a1243fbdf5bcd8fcca83668d57b1"
+    sha256 cellar: :any_skip_relocation, catalina:      "a3603b422c90d1fa525f5b9581f18b4645aeb39e3ccb7450f11d6e833a92e697"
+    sha256 cellar: :any_skip_relocation, mojave:        "b6906bda3c1557367516849bda5957596a4cc4ca0407c04282a7d3d4ae3d873c"
   end
 
   depends_on "ocaml" => [:build, :test]
