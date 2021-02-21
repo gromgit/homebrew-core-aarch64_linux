fcbd637f1601481254b71f9edc12949008228ed7
FX Coudert <fxcoudert@gmail.com>
2021-02-21 00:22:08 +0000

    cmt: update 0.7.1_1 bottle.

--- Formula/cmt.rb
+++ Formula/cmt.rb
@@ -8,6 +8,7 @@ class Cmt < Formula
   head "https://github.com/smallhadroncollider/cmt.git"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, big_sur:     "23a0cc2f3262599f732a0618d534cef712a49c14928549d110c2d969c6da1058"
     sha256 cellar: :any_skip_relocation, catalina:    "2690f2f872ce08413832b0b6fd9e23b384fe7984f2a80e44285edd02d36cbaa5"
     sha256 cellar: :any_skip_relocation, mojave:      "2f077c726546a39809f627d4d6bb24c0b172252eac7a65139fbcba9b9f1dd296"
     sha256 cellar: :any_skip_relocation, high_sierra: "cbfab965d0ca391ed029d21df3d8da1a21368263b549b65ecf200aa5209d7eba"
