64d4ef9c20cb81ff82da68346d88017fb1e69259
Alexander Zigelski <crunchtime-ali@users.noreply.github.com>
2021-02-21 00:46:13 +0000

    ffmpeg 4.3.2
    * ffmpeg 4.3.2
    * ffmpeg: remove patch
    
    Closes #71635.
    
    Co-authored-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -4,19 +4,11 @@ class Ffmpeg < Formula
   # None of these parts are used by default, you have to explicitly pass `--enable-gpl`
   # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
   license "GPL-2.0-or-later"
-  revision 9
   head "https://github.com/FFmpeg/FFmpeg.git"
 
   stable do
-    url "https://ffmpeg.org/releases/ffmpeg-4.3.1.tar.xz"
-    sha256 "ad009240d46e307b4e03a213a0f49c11b650e445b1f8be0dda2a9212b34d2ffb"
-
-    # https://trac.ffmpeg.org/ticket/8760
-    # Remove in next release
-    patch do
-      url "https://github.com/FFmpeg/FFmpeg/commit/7c59e1b0f285cd7c7b35fcd71f49c5fd52cf9315.patch?full_index=1"
-      sha256 "1cbe1b68d70eadd49080a6e512a35f3e230de26b6e1b1c859d9119906417737f"
-    end
+    url "https://ffmpeg.org/releases/ffmpeg-4.3.2.tar.xz"
+    sha256 "46e4e64f1dd0233cbc0934b9f1c0da676008cad34725113fb7f802cfa84ccddb"
   end
 
   livecheck do
