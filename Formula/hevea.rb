059473321ef2beaa8c4ae275d42e562c1885c204
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-21 22:15:20 +0000

    hevea: update 2.35 bottle.

--- Formula/hevea.rb
+++ Formula/hevea.rb
@@ -10,9 +10,10 @@ class Hevea < Formula
   end
 
   bottle do
-    sha256 big_sur:  "7679aa58989eb2715fad0c5967407ce69b94bc3ec2aa7b3ad9fe7992be315858"
-    sha256 catalina: "6d654577f6c28ddd3c1029df88c7ecfce23dcc3ddac12fba90fc247abfcdb43e"
-    sha256 mojave:   "6e0aa3139d0f799090295e989d8aa53d27b6d3735011ee9a8cedd85a0fd3b95b"
+    sha256 arm64_big_sur: "83f3e0a6a87ad437058b5bdc840e4663dc1e57b5b34abf32b4f00a72ac436070"
+    sha256 big_sur:       "7679aa58989eb2715fad0c5967407ce69b94bc3ec2aa7b3ad9fe7992be315858"
+    sha256 catalina:      "6d654577f6c28ddd3c1029df88c7ecfce23dcc3ddac12fba90fc247abfcdb43e"
+    sha256 mojave:        "6e0aa3139d0f799090295e989d8aa53d27b6d3735011ee9a8cedd85a0fd3b95b"
   end
 
   depends_on "ocamlbuild" => :build
