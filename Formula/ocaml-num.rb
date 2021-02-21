ff8bb0d52d0c24678ba9d9234ba6083a78c4f4e5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-21 12:39:41 +0000

    ocaml-num: revision bump (ocaml 4.10.2)

--- Formula/ocaml-num.rb
+++ Formula/ocaml-num.rb
@@ -4,6 +4,7 @@ class OcamlNum < Formula
   url "https://github.com/ocaml/num/archive/v1.4.tar.gz"
   sha256 "015088b68e717b04c07997920e33c53219711dfaf36d1196d02313f48ea00f24"
   license "LGPL-2.1"
+  revision 1
 
   bottle do
     sha256 cellar: :any, big_sur:  "88c24c066deb255c20828f2032bbecf933a34acacb9fae21c99ba9ecd84f4449"
