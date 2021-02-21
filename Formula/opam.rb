2ff2f1b9dd7f9af8970588aad6f623ed87a463c3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-21 22:07:57 +0000

    opam: update 2.0.8 bottle.

--- Formula/opam.rb
+++ Formula/opam.rb
@@ -7,9 +7,10 @@ class Opam < Formula
   head "https://github.com/ocaml/opam.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "d34e0dcbfa4302960a8f813d4e06c113e24beff31d2fbf8e55e470c5b51ecc0b"
-    sha256 cellar: :any_skip_relocation, catalina: "882bf7f9d3f94fbbc2d5f08019456f533e0a71fd58c0a02650aa5781faefca9a"
-    sha256 cellar: :any_skip_relocation, mojave:   "e091ed13ebfa241890e0489cdc2645d66c9c189f618466cf8f7576751b381726"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "83fedf7b107a1cc3ea02a3782e3d830feeec7b8482a8e015707af65c0bb94ac9"
+    sha256 cellar: :any_skip_relocation, big_sur:       "d34e0dcbfa4302960a8f813d4e06c113e24beff31d2fbf8e55e470c5b51ecc0b"
+    sha256 cellar: :any_skip_relocation, catalina:      "882bf7f9d3f94fbbc2d5f08019456f533e0a71fd58c0a02650aa5781faefca9a"
+    sha256 cellar: :any_skip_relocation, mojave:        "e091ed13ebfa241890e0489cdc2645d66c9c189f618466cf8f7576751b381726"
   end
 
   depends_on "ocaml" => [:build, :test]
