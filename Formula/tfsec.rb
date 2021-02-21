97d6f61a6041844cd3122763cb145cc74200bb04
rui <rui@chenrui.dev>
2021-02-21 10:27:20 +0000

    tfsec 0.38.5
    Closes #71651.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tfsec.rb
+++ Formula/tfsec.rb
@@ -1,8 +1,8 @@
 class Tfsec < Formula
   desc "Static analysis powered security scanner for your terraform code"
   homepage "https://github.com/tfsec/tfsec"
-  url "https://github.com/tfsec/tfsec/archive/v0.38.4.tar.gz"
-  sha256 "791322da015a3f1e5741e3e06140c65a87e90564b6d36bee2a29207ab45628ef"
+  url "https://github.com/tfsec/tfsec/archive/v0.38.5.tar.gz"
+  sha256 "b00f7135ed4d4cdc2c7a1b4abd92929a6aca74c6e7d3994499f9ece8d2f01f87"
   license "MIT"
 
   livecheck do
