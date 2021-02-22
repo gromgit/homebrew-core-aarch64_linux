cf0fbda529c6a841784f7368fe78b3ccabe0a146
Bo Anderson <mail@boanderson.me>
2021-02-22 15:10:01 +0000

    packr: build packr2 & fix tests for Go 1.16
    Closes #71748.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/packr.rb
+++ Formula/packr.rb
@@ -4,6 +4,7 @@ class Packr < Formula
   url "https://github.com/gobuffalo/packr/archive/v2.8.1.tar.gz"
   sha256 "648f8690e0349039300d3603708bd383f3568193ebaeb0760a87da8760dc7fa7"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, big_sur:     "4b36cf364f3a9c6dc7b7a3b27994172723baf660ce61e6ac8adfcee924d7f6f9"
@@ -16,6 +17,9 @@ class Packr < Formula
 
   def install
     system "go", "build", *std_go_args, "./packr"
+    cd "v2" do
+      system "go", "build", *std_go_args, "-o", bin/"packr2", "./packr2"
+    end
   end
 
   test do
@@ -39,11 +43,11 @@ class Packr < Formula
         "fmt"
         "log"
 
-        "github.com/gobuffalo/packr"
+        "github.com/gobuffalo/packr/v2"
       )
 
       func main() {
-        box := packr.NewBox("./templates")
+        box := packr.New("myBox", "./templates")
 
         s, err := box.FindString("admin/index.html")
         if err != nil {
@@ -55,9 +59,11 @@ class Packr < Formula
     EOS
 
     system "go", "mod", "init", "example"
-    system bin/"packr"
+    system "go", "mod", "edit", "-require=github.com/gobuffalo/packr/v2@v#{version}"
+    system "go", "mod", "download"
+    system bin/"packr2"
     system "go", "build"
-    system bin/"packr", "clean"
+    system bin/"packr2", "clean"
 
     assert_equal File.read("templates/admin/index.html"), shell_output("./example")
   end
