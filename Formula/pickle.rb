a58b0de74ac65154e3108cfb1988ddb6106e9d3b
rui <chenrui333@gmail.com>
2021-02-22 14:37:01 -0500

    pickle 0.7.2 (#71754)

--- Formula/pickle.rb
+++ Formula/pickle.rb
@@ -1,8 +1,8 @@
 class Pickle < Formula
   desc "PHP Extension installer"
   homepage "https://github.com/FriendsOfPHP/pickle"
-  url "https://github.com/FriendsOfPHP/pickle/releases/download/v0.7.1/pickle.phar"
-  sha256 "69bd6db9d9a2b795f1bf68dcba66e35be1b2dc82848c1a1a6ed9f9a1ef429dbe"
+  url "https://github.com/FriendsOfPHP/pickle/releases/download/v0.7.2/pickle.phar"
+  sha256 "4fee7dbfe44c93c939fbc7d4b1929a3255e2d042b0e46685dac80a5e181c102e"
   license "BSD-3-Clause"
 
   bottle :unneeded
