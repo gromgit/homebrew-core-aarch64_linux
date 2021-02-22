dafcbe39da572d1a121bb90713d05dd143395fa0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-22 00:24:34 +0000

    devd: update 0.9 bottle.

--- Formula/devd.rb
+++ Formula/devd.rb
@@ -8,9 +8,10 @@ class Devd < Formula
 
   bottle do
     rebuild 2
-    sha256 cellar: :any_skip_relocation, big_sur:  "9dad47b2a2c803d6ddf5a4153359a2d8e9188f37f745e964fed2a56159731597"
-    sha256 cellar: :any_skip_relocation, catalina: "89f6654470b9ef03bd42046e147f0ae372c607428f7bb934b5474f76ff397e3c"
-    sha256 cellar: :any_skip_relocation, mojave:   "8806190656fd2634dadf577e7df5957b5bcaca434585f10f2a82197d8e59f03a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "84be8385183d30ad91445a33da1d1d4fc30e9e74f225abeafce8993df3cfcf59"
+    sha256 cellar: :any_skip_relocation, big_sur:       "9dad47b2a2c803d6ddf5a4153359a2d8e9188f37f745e964fed2a56159731597"
+    sha256 cellar: :any_skip_relocation, catalina:      "89f6654470b9ef03bd42046e147f0ae372c607428f7bb934b5474f76ff397e3c"
+    sha256 cellar: :any_skip_relocation, mojave:        "8806190656fd2634dadf577e7df5957b5bcaca434585f10f2a82197d8e59f03a"
   end
 
   depends_on "go" => :build
