ef7428b3d791403ad93cc8c9ba67b2a06bab7a2f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-22 00:25:42 +0000

    docker-credential-helper: update 0.6.3 bottle.

--- Formula/docker-credential-helper.rb
+++ Formula/docker-credential-helper.rb
@@ -7,11 +7,12 @@ class DockerCredentialHelper < Formula
   head "https://github.com/docker/docker-credential-helpers.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:     "eacf170b769cf42b4ca6f916a66436abf6b2860d56c1086542503f25819353da"
-    sha256 cellar: :any_skip_relocation, catalina:    "6b29ba43b81fd4cbd72defa6c9ed49d730b520555b2d5dd16e35c4da2835896d"
-    sha256 cellar: :any_skip_relocation, mojave:      "a4553f6ca40dd3c012a0ddc910b7ceb4b819ee89d1366613c9205f356f5a2a69"
-    sha256 cellar: :any_skip_relocation, high_sierra: "71b6622a61da363350ca6065c5683a7a157b4ec404c8e11f66752ac2f0e09063"
-    sha256 cellar: :any_skip_relocation, sierra:      "3810d91647e6385bb9d57da3c800d1107603551285a94643f0d530bb63821a2d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "85f26b741ecb84e2779777b3b1c2c8f4eec188c757c4f3faef3583a5cb9533c7"
+    sha256 cellar: :any_skip_relocation, big_sur:       "eacf170b769cf42b4ca6f916a66436abf6b2860d56c1086542503f25819353da"
+    sha256 cellar: :any_skip_relocation, catalina:      "6b29ba43b81fd4cbd72defa6c9ed49d730b520555b2d5dd16e35c4da2835896d"
+    sha256 cellar: :any_skip_relocation, mojave:        "a4553f6ca40dd3c012a0ddc910b7ceb4b819ee89d1366613c9205f356f5a2a69"
+    sha256 cellar: :any_skip_relocation, high_sierra:   "71b6622a61da363350ca6065c5683a7a157b4ec404c8e11f66752ac2f0e09063"
+    sha256 cellar: :any_skip_relocation, sierra:        "3810d91647e6385bb9d57da3c800d1107603551285a94643f0d530bb63821a2d"
   end
 
   depends_on "go" => :build
