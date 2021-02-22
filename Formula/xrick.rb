6776d96794a9c947457ac17a1b49b55df554dcd1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-22 04:20:00 +0000

    xrick: update 021212 bottle.

--- Formula/xrick.rb
+++ Formula/xrick.rb
@@ -7,10 +7,11 @@ class Xrick < Formula
   sha256 "aa8542120bec97a730258027a294bd16196eb8b3d66134483d085f698588fc2b"
 
   bottle do
-    rebuild 1
-    sha256 big_sur:  "301f75c04e2c98c81b58868d7357e817b7dec1bc8a29747b27f088a10c1863ec"
-    sha256 catalina: "b391e62a9cf0a3537ec68c03d215370d875a70a90fb7a8cb48cac8cc7281e2c2"
-    sha256 mojave:   "cf85542617f6e39a8fec0239a57d1bc1707f50289e5c91bee89e84664603f43c"
+    rebuild 2
+    sha256 arm64_big_sur: "06c5d5c23d3a83ae82c8efa8303c186cb8b07f2f274b911062c0a56df4207e96"
+    sha256 big_sur:       "07143daa9065782675e5288a66dc2ea8ce418754d4842175b016dbc097b982cd"
+    sha256 catalina:      "67d2a811a5ecd67de860f226fc88feec5ed479ae05fc22e917d6fbe0686be783"
+    sha256 mojave:        "1a2f4d0cc564fd627d83f2b54606c59845b50fb3db04e7a118ca281377b4805a"
   end
 
   depends_on "sdl"
