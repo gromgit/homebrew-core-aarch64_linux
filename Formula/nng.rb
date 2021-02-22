4160737eccd457d0422b343421eab53f44961ac9
Dustin Rodrigues <dust.rod@gmail.com>
2021-02-22 02:42:06 +0000

    nng: update 1.4.0 bottle.

--- Formula/nng.rb
+++ Formula/nng.rb
@@ -11,9 +11,10 @@ class Nng < Formula
   end
 
   bottle do
-    sha256 big_sur:  "116a7c22766d5db5ae416e06ea53f15e0511520319a7500e2b5c7cc28198c166"
-    sha256 catalina: "fd68af7c0f9cabe18c673c60e92f4252d10e91b6f979c8bb625d7c48698e1655"
-    sha256 mojave:   "ca12fdc7635dac1cc2b8551ca5d72b51d01a8b4ab775bc6555cc6aaf048c7f20"
+    sha256 arm64_big_sur: "2e6c5612976c754881d86240a6ba850330ab06cc99e6b4d292d354f1ca8e88b3"
+    sha256 big_sur:       "116a7c22766d5db5ae416e06ea53f15e0511520319a7500e2b5c7cc28198c166"
+    sha256 catalina:      "fd68af7c0f9cabe18c673c60e92f4252d10e91b6f979c8bb625d7c48698e1655"
+    sha256 mojave:        "ca12fdc7635dac1cc2b8551ca5d72b51d01a8b4ab775bc6555cc6aaf048c7f20"
   end
 
   depends_on "asciidoctor" => :build
