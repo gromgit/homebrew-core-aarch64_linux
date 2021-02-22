bf49008ab0d0785fdeff70612d664b00bb605894
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-22 04:12:54 +0000

    fsql: update 0.3.1 bottle.

--- Formula/fsql.rb
+++ Formula/fsql.rb
@@ -7,11 +7,12 @@ class Fsql < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, big_sur:     "fec9ae6f2720a408b0bf11aa6f83d7de2bbd99eb8054a8ede09809b0ca470cfb"
-    sha256 cellar: :any_skip_relocation, catalina:    "de9764e9754827795839738de7d4377786b3532b7a2157806ecf3e145f626146"
-    sha256 cellar: :any_skip_relocation, mojave:      "7cb63d8939e7af0391938aea8a138daccbaddce50b42802d32e510772e004b9a"
-    sha256 cellar: :any_skip_relocation, high_sierra: "7b4353a346425e4db5d14419c4dbacf6038606778a7ce2b98ddd0fdb7c2ca233"
-    sha256 cellar: :any_skip_relocation, sierra:      "f651c7c2dad44ee6b6f32aa699df223bd427421990f2c2c170d0928b1a31ef87"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9266f63cddbc0ca9c73655ecee949deae745e991ca31727a954958c81cfe4615"
+    sha256 cellar: :any_skip_relocation, big_sur:       "fec9ae6f2720a408b0bf11aa6f83d7de2bbd99eb8054a8ede09809b0ca470cfb"
+    sha256 cellar: :any_skip_relocation, catalina:      "de9764e9754827795839738de7d4377786b3532b7a2157806ecf3e145f626146"
+    sha256 cellar: :any_skip_relocation, mojave:        "7cb63d8939e7af0391938aea8a138daccbaddce50b42802d32e510772e004b9a"
+    sha256 cellar: :any_skip_relocation, high_sierra:   "7b4353a346425e4db5d14419c4dbacf6038606778a7ce2b98ddd0fdb7c2ca233"
+    sha256 cellar: :any_skip_relocation, sierra:        "f651c7c2dad44ee6b6f32aa699df223bd427421990f2c2c170d0928b1a31ef87"
   end
 
   depends_on "go" => :build
