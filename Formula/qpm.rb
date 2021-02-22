e1c8b05a1dbc00c2e767a32ffac9f1133d22b006
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-22 05:01:51 +0000

    qpm: update 0.11.0 bottle.

--- Formula/qpm.rb
+++ Formula/qpm.rb
@@ -8,10 +8,11 @@ class Qpm < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, big_sur:     "545f5e5f8e982649cf4977920de2c00b6da7c547ef694e7070b39e0a9408415a"
-    sha256 cellar: :any_skip_relocation, catalina:    "5d5edc32931995dfa82429a1d8708e700de70208f36767808a433c1e9bb2ffb2"
-    sha256 cellar: :any_skip_relocation, mojave:      "f8208ec60e2af6e9d1da2caa0ad1b48b5b027955c2daa51860fa1606b8c5acef"
-    sha256 cellar: :any_skip_relocation, high_sierra: "8c9d0dde0b7a4292f8fa04337805755ac16ce1aab08710463323afec2f73d551"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9b0932dcd3977d04fab01619bc01e76d69b3194f1827be9e19f544587d5d53ca"
+    sha256 cellar: :any_skip_relocation, big_sur:       "545f5e5f8e982649cf4977920de2c00b6da7c547ef694e7070b39e0a9408415a"
+    sha256 cellar: :any_skip_relocation, catalina:      "5d5edc32931995dfa82429a1d8708e700de70208f36767808a433c1e9bb2ffb2"
+    sha256 cellar: :any_skip_relocation, mojave:        "f8208ec60e2af6e9d1da2caa0ad1b48b5b027955c2daa51860fa1606b8c5acef"
+    sha256 cellar: :any_skip_relocation, high_sierra:   "8c9d0dde0b7a4292f8fa04337805755ac16ce1aab08710463323afec2f73d551"
   end
 
   depends_on "go" => :build
