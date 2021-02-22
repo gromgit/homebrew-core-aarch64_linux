1c33964133e7de7647afdaac0d4fde97a260acf5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-22 11:35:05 +0000

    amazon-ecs-cli: update 1.21.0 bottle.

--- Formula/amazon-ecs-cli.rb
+++ Formula/amazon-ecs-cli.rb
@@ -8,9 +8,10 @@ class AmazonEcsCli < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, big_sur:  "66dcb9af8a67215a8a1f4fef00dbf0c16e836cc65985a86d113cb4f208dff50c"
-    sha256 cellar: :any_skip_relocation, catalina: "0bb03d95203b20aebc66ee008946951dfc66a991d6015f38d9158cda3dc36b8c"
-    sha256 cellar: :any_skip_relocation, mojave:   "ac8ff57a4b7de517c767f53626f19d134732c9b3a9d68143cac9d4440f01cc2a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "dfe17f71140b30b124679fee0a7b402f8f16a3d284ea357b315f2f9896f67beb"
+    sha256 cellar: :any_skip_relocation, big_sur:       "66dcb9af8a67215a8a1f4fef00dbf0c16e836cc65985a86d113cb4f208dff50c"
+    sha256 cellar: :any_skip_relocation, catalina:      "0bb03d95203b20aebc66ee008946951dfc66a991d6015f38d9158cda3dc36b8c"
+    sha256 cellar: :any_skip_relocation, mojave:        "ac8ff57a4b7de517c767f53626f19d134732c9b3a9d68143cac9d4440f01cc2a"
   end
 
   depends_on "go" => :build
