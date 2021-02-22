e2fa9b122d5e154531cd65f0543c46ba566fd3cd
rui <rui@chenrui.dev>
2021-02-22 12:35:53 +0000

    spaceinvaders-go 1.2.1
    * spaceinvaders-go 1.2.1
    * spaceinvaders-go: deprecate gopath
    
    Closes #71742.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/spaceinvaders-go.rb
+++ Formula/spaceinvaders-go.rb
@@ -1,10 +1,8 @@
-require "language/go"
-
 class SpaceinvadersGo < Formula
   desc "Space Invaders in your terminal written in Go"
   homepage "https://github.com/asib/spaceinvaders"
-  url "https://github.com/asib/spaceinvaders/archive/v1.2.tar.gz"
-  sha256 "e5298c4c13ff42f5cb3bf3913818c5155cf6918fd757124920045485d7ab5b9e"
+  url "https://github.com/asib/spaceinvaders/archive/v1.2.1.tar.gz"
+  sha256 "3fef982b94784d34ac2ae68c1d5dec12e260974907bce83528fe3c4132bed377"
   license "MIT"
 
   bottle do
@@ -19,33 +17,8 @@ class SpaceinvadersGo < Formula
 
   depends_on "go" => :build
 
-  go_resource "github.com/mattn/go-runewidth" do
-    url "https://github.com/mattn/go-runewidth.git",
-        revision: "12e0ff74603c9a3209d8bf84f8ab349fe1ad9477"
-  end
-
-  go_resource "github.com/nsf/termbox-go" do
-    url "https://github.com/nsf/termbox-go.git",
-        revision: "347ab0bc907040257edaf8b580f729e12c93ab6b"
-  end
-
-  go_resource "github.com/simulatedsimian/joystick" do
-    url "https://github.com/simulatedsimian/joystick.git",
-        revision: "6aa8abe045a796cf36b720d0484809e3f70dc5bd"
-  end
-
   def install
-    # This builds with Go.
-    ENV["GOPATH"] = buildpath
-    ENV["GO111MODULE"] = "auto"
-    sipath = buildpath/"src/github.com/asib/spaceinvaders"
-    sipath.install Dir["{*,.git}"]
-    Language::Go.stage_deps resources, buildpath/"src"
-    cd "src/github.com/asib/spaceinvaders/" do
-      system "go", "build"
-      bin.install "spaceinvaders"
-      prefix.install_metafiles
-    end
+    system "go", "build", *std_go_args, "-ldflags", "-s -w", "-o", bin/"spaceinvaders"
   end
 
   test do
