8d4fa244f879b5f4e60066cfbb6489cc3a0708de
Dustin Rodrigues <dust.rod@gmail.com>
2021-02-22 02:31:01 +0000

    wrangler: update 1.2_3 bottle.

--- Formula/wrangler.rb
+++ Formula/wrangler.rb
@@ -22,10 +22,11 @@ class Wrangler < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:     "709fa007e8e40d82cf7a73b79c55fcefbaf24f5d6bd8eb9cf5a4ad0168a2bcff"
-    sha256 cellar: :any_skip_relocation, catalina:    "8d67285352be09f209dba8e1fe678bb9e88a77c74e5178687f890cf5ba19c8ca"
-    sha256 cellar: :any_skip_relocation, mojave:      "1f122b48da35f344074d239e3d23fcf3d66e309dd0425062547d080bd3285a12"
-    sha256 cellar: :any_skip_relocation, high_sierra: "b3aa1c943b1de15308be2cf7ac540daa95b4a843788a662fcdf34ed30e2ec29d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c5a38558160ce09e6bc4aa970a96190778f36cabbc45e8ad477980f22b34e5e1"
+    sha256 cellar: :any_skip_relocation, big_sur:       "709fa007e8e40d82cf7a73b79c55fcefbaf24f5d6bd8eb9cf5a4ad0168a2bcff"
+    sha256 cellar: :any_skip_relocation, catalina:      "8d67285352be09f209dba8e1fe678bb9e88a77c74e5178687f890cf5ba19c8ca"
+    sha256 cellar: :any_skip_relocation, mojave:        "1f122b48da35f344074d239e3d23fcf3d66e309dd0425062547d080bd3285a12"
+    sha256 cellar: :any_skip_relocation, high_sierra:   "b3aa1c943b1de15308be2cf7ac540daa95b4a843788a662fcdf34ed30e2ec29d"
   end
 
   depends_on "erlang@22"
