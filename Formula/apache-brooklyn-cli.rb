d95696b59a3023919c21dfb841996f4f3508b2e4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-22 11:35:17 +0000

    apache-brooklyn-cli: update 1.0.0 bottle.

--- Formula/apache-brooklyn-cli.rb
+++ Formula/apache-brooklyn-cli.rb
@@ -11,10 +11,11 @@ class ApacheBrooklynCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:     "ee39617c71638a87e473fe90b002d6d1ab34d149a40218cb53758ca08e162593"
-    sha256 cellar: :any_skip_relocation, catalina:    "7769a15fc55f1a6943165e78c0cc3c9677815686b935a888c3db708fbaf2b8dd"
-    sha256 cellar: :any_skip_relocation, mojave:      "1b73cb46bdd10be0d426298ec972fd37362352b28fadb484374e701619d3a1dc"
-    sha256 cellar: :any_skip_relocation, high_sierra: "b64f20e59f179c2a359d180be65931e06743aea8c62295f58d1afdbd967871d9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1294ffaaa4cebe9accd9686c723154f18b0c132200eddcfaefc9ec69cfc47b57"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ee39617c71638a87e473fe90b002d6d1ab34d149a40218cb53758ca08e162593"
+    sha256 cellar: :any_skip_relocation, catalina:      "7769a15fc55f1a6943165e78c0cc3c9677815686b935a888c3db708fbaf2b8dd"
+    sha256 cellar: :any_skip_relocation, mojave:        "1b73cb46bdd10be0d426298ec972fd37362352b28fadb484374e701619d3a1dc"
+    sha256 cellar: :any_skip_relocation, high_sierra:   "b64f20e59f179c2a359d180be65931e06743aea8c62295f58d1afdbd967871d9"
   end
 
   depends_on "go" => :build
