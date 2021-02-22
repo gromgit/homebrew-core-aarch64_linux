847abc5c4a4137b70dedec270d0f5994102d4295
Dustin Rodrigues <dust.rod@gmail.com>
2021-02-22 02:41:15 +0000

    feedgnuplot: update 1.56 bottle.

--- Formula/feedgnuplot.rb
+++ Formula/feedgnuplot.rb
@@ -6,9 +6,10 @@ class Feedgnuplot < Formula
   license any_of: ["GPL-1.0-or-later", "Artistic-1.0"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "ed67bb9cd920c46a467874b497e9b74b273d49cbe42b43ca0a17bb0639e0453e"
-    sha256 cellar: :any_skip_relocation, catalina: "0878f19ce28a67c6a9ef317e79bb1650b22047ff19afd863f3cdaa0a5a726967"
-    sha256 cellar: :any_skip_relocation, mojave:   "806ef63d60125e9489079ccd72985cc484fd3f671a97b396ed00aad5166676b3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "199b67996a0d83244709c69f9e9f97ce708d414774f97024e94a9fa2fa1a1db4"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ed67bb9cd920c46a467874b497e9b74b273d49cbe42b43ca0a17bb0639e0453e"
+    sha256 cellar: :any_skip_relocation, catalina:      "0878f19ce28a67c6a9ef317e79bb1650b22047ff19afd863f3cdaa0a5a726967"
+    sha256 cellar: :any_skip_relocation, mojave:        "806ef63d60125e9489079ccd72985cc484fd3f671a97b396ed00aad5166676b3"
   end
 
   depends_on "gnuplot"
