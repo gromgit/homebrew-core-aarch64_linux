d4c5b84dd0a259fd2bb551d700a54e4a3265c47b
Dustin Rodrigues <dust.rod@gmail.com>
2021-02-21 21:30:54 -0500

    xrick: update url
    Closes #71696.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xrick.rb
+++ Formula/xrick.rb
@@ -1,7 +1,7 @@
 class Xrick < Formula
   desc "Clone of Rick Dangerous"
-  homepage "http://www.bigorno.net/xrick/"
-  url "http://www.bigorno.net/xrick/xrick-021212.tgz"
+  homepage "https://www.bigorno.net/xrick/"
+  url "https://www.bigorno.net/xrick/xrick-021212.tgz"
   # There is a repo at https://github.com/zpqrtbnk/xrick but it is organized
   # differently than the tarball
   sha256 "aa8542120bec97a730258027a294bd16196eb8b3d66134483d085f698588fc2b"
