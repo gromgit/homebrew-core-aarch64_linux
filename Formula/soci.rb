9a1a03742134fb14d858694617abb6b41ff361e7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-23 07:31:47 +0000

    soci: update 4.0.1 bottle.

--- Formula/soci.rb
+++ Formula/soci.rb
@@ -6,10 +6,11 @@ class Soci < Formula
   license "BSL-1.0"
 
   bottle do
-    sha256 big_sur:     "1110442edee6672ede9b93dccef0a933508d6aa9936ad7d7c82b9429965092ff"
-    sha256 catalina:    "367f4d37091b11f2e63e220361f9344622a93f8c961122901889c5ef132fb0ec"
-    sha256 mojave:      "c51aea80672de81e7663b36e6ff39ce9cc3025aa3c531539424dd089c1b347a8"
-    sha256 high_sierra: "6e6fced1aa11defaed5f6ea4461b5bbf763b8f55349035e587c80c4bbd68df27"
+    sha256 arm64_big_sur: "3dd152a973084669c4083615a101837067aae6c9b6f703e8148acb62314c6e11"
+    sha256 big_sur:       "1110442edee6672ede9b93dccef0a933508d6aa9936ad7d7c82b9429965092ff"
+    sha256 catalina:      "367f4d37091b11f2e63e220361f9344622a93f8c961122901889c5ef132fb0ec"
+    sha256 mojave:        "c51aea80672de81e7663b36e6ff39ce9cc3025aa3c531539424dd089c1b347a8"
+    sha256 high_sierra:   "6e6fced1aa11defaed5f6ea4461b5bbf763b8f55349035e587c80c4bbd68df27"
   end
 
   depends_on "cmake" => :build
