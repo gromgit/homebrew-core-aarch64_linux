9ed16f9f9b099406d5e8c5ebd88a8d02bd2a3876
rui <rui@chenrui.dev>
2021-02-24 00:42:09 +0000

    juju 2.8.9
    Closes #71873.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -2,8 +2,8 @@ class Juju < Formula
   desc "DevOps management tool"
   homepage "https://juju.is/"
   url "https://github.com/juju/juju.git",
-      tag:      "juju-2.8.8",
-      revision: "1d2677ae1d65f10bd15e9e29ab5e0f0cca70898a"
+      tag:      "juju-2.8.9",
+      revision: "a48bedf0ae2096d0cf750b7676da9b0a994cff09"
   license "AGPL-3.0-only"
   version_scheme 1
   head "https://github.com/juju/juju.git"
