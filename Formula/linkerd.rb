00212b1d7e12eb1eec6f619a96421c7b69bb2769
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-24 09:23:31 +0000

    linkerd: update 2.9.4 bottle.

--- Formula/linkerd.rb
+++ Formula/linkerd.rb
@@ -13,10 +13,10 @@ class Linkerd < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "789baae017bd544ee8761535d050d2ddad5c674832f09c74f2f0739a3b9b3266"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4107b12082208ce860eb94d7eba294d9c35dcebec21c419e0943eca21742c4a7"
-    sha256 cellar: :any_skip_relocation, catalina:      "86e0b85857dd6c2d86a02ffe09cfe673a45c6d64924cc87e32cf813725ceb2b3"
-    sha256 cellar: :any_skip_relocation, mojave:        "ec563871946a601ed88d98de6b3c70d189f2b55c9a949c48b161bf49393d86e4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "07a36af650207fe73ad428e9073f9aa4d2be325c2ee9712e4002b765cc451528"
+    sha256 cellar: :any_skip_relocation, big_sur:       "c7febb2775499447608d28e889060b2de09aa55be6ad252343c2af485f54fcf4"
+    sha256 cellar: :any_skip_relocation, catalina:      "edf4fe6c38dd321d0fc79d719463097454cb7c99cbf915d51a2ab4d21d79e53d"
+    sha256 cellar: :any_skip_relocation, mojave:        "5fbc42faf0ffd0e33caf915c030b222cad0c8c5a4d717983a6cb9ab8a498996b"
   end
 
   depends_on "go" => :build
