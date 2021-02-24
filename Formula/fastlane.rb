e3012f5450ff936f775e9d4eb9e27883047602fc
Josh Holtz <josh@rokkincat.com>
2021-02-24 15:43:24 -0600

    fastlane 2.176.0
    Closes #71930.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fastlane.rb
+++ Formula/fastlane.rb
@@ -1,8 +1,8 @@
 class Fastlane < Formula
   desc "Easiest way to build and release mobile apps"
   homepage "https://fastlane.tools"
-  url "https://github.com/fastlane/fastlane/archive/2.175.0.tar.gz"
-  sha256 "72ca303e7423caaa99df3aecfb841b9f15a74af8547f2aad9c047870458aa590"
+  url "https://github.com/fastlane/fastlane/archive/2.176.0.tar.gz"
+  sha256 "724a33775bfa52e6355422952f40d397fd7edff17cbe5878b6c1a7cec7ac892d"
   license "MIT"
   head "https://github.com/fastlane/fastlane.git"
 
