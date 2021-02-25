1f74919ad33044141bf311e26f3b9ff21f960a3f
Morozov-5F <morozov.evgeniy.95@gmail.com>
2021-02-25 16:25:48 +0300

    crispy-doom 5.10
    Closes #71965.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

---
+++ Formula/crispy-doom.rb
@@ -0,0 +1,37 @@
+class CrispyDoom < Formula
+  desc "Limit-removing enhanced-resolution Doom source port based on Chocolate Doom"
+  homepage "https://github.com/fabiangreffrath/crispy-doom"
+  url "https://github.com/fabiangreffrath/crispy-doom/archive/crispy-doom-5.10.0.tar.gz"
+  sha256 "0b6bac20816d057692e4f5c15dbbb27cc49e404e821f774e2b16ce997a6f3fe1"
+  license "GPL-2.0-only"
+
+  head "https://github.com/fabiangreffrath/crispy-doom"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libpng"
+  depends_on "libsamplerate"
+  depends_on "sdl2"
+  depends_on "sdl2_mixer"
+  depends_on "sdl2_net"
+
+  def install
+    system "autoreconf", "-fiv"
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--disable-sdltest"
+    system "make", "install", "execgamesdir=#{bin}"
+  end
+
+  test do
+    testdata = <<~EOS
+      Inavlid IWAD file
+    EOS
+    (testpath/"test_invalid.wad").write testdata
+
+    expected_output = "Wad file test_invalid.wad doesn't have IWAD or PWAD id"
+    assert_match expected_output, shell_output("#{bin}/crispy-doom -nogui -iwad test_invalid.wad 2>&1", 255)
+  end
+end
