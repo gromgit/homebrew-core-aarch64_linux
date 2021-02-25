b6c2397ded1a2cb9883a3aa0a60018bf31201888
rui <rui@chenrui.dev>
2021-02-25 02:32:16 +0000

    ghq 1.1.6
    Closes #71940.
    
    Signed-off-by: Seeker <meaningseeking@protonmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ghq.rb
+++ Formula/ghq.rb
@@ -2,8 +2,8 @@ class Ghq < Formula
   desc "Remote repository management made easy"
   homepage "https://github.com/x-motemen/ghq"
   url "https://github.com/x-motemen/ghq.git",
-      tag:      "v1.1.5",
-      revision: "f5ac3e5655ced9bf3aee2690a2aaa43431cc567f"
+      tag:      "v1.1.6",
+      revision: "7c3498b1d29a00b361ed10410f61fb4b520ab050"
   license "MIT"
   head "https://github.com/x-motemen/ghq.git"
 
