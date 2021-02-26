7c14c645aefcb72b86dd653510e667148102cb42
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-26 13:18:16 +0000

    sofia-sip: update 1.13.3 bottle.

--- Formula/sofia-sip.rb
+++ Formula/sofia-sip.rb
@@ -6,11 +6,10 @@ class SofiaSip < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "e7b5462066dbb0bce9d0e6d16e709fc3adaf99f3624d136d5c929a8d6d5b68ba"
-    sha256 cellar: :any, big_sur:       "4f581160ed9d9e1c478aca087b53483b493fd747afd480ad18fa2bc6ce5931b2"
-    sha256 cellar: :any, catalina:      "ab14de67ceb92a79d8095b94d3e76680765185088773fe4c5b014574ef32a892"
-    sha256 cellar: :any, mojave:        "5f9a402b196aa592405fb10ddca2bd8f3736d62cfd8f2709d52fb18d9402af8f"
-    sha256 cellar: :any, high_sierra:   "7729020836b875104c6044afd0a72cf8755619978216430d89a585cf9783c8b0"
+    sha256 cellar: :any, arm64_big_sur: "140bf310b37cc869a010b610d5fa96932f3110804f347901ffd149cf5a2de5e8"
+    sha256 cellar: :any, big_sur:       "8af7baa5cb43c43281212108323663c4905ec5f0413e194d833d320986dbf1df"
+    sha256 cellar: :any, catalina:      "99edefb3cd8c43b53e160eb74dca06fbb2d1d9b75d1363d7b81b8e9eba263cc9"
+    sha256 cellar: :any, mojave:        "166390e2e233d0378d1dc7777a515bb18ad112ea78eef627abca8562b4110511"
   end
 
   depends_on "autoconf" => :build
