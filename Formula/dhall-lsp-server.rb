b0ae7234f556a40f4855324e3b61a03ff01ac33e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-26 17:34:21 +0000

    dhall-lsp-server: update 1.0.13 bottle.

--- Formula/dhall-lsp-server.rb
+++ Formula/dhall-lsp-server.rb
@@ -7,9 +7,10 @@ class DhallLspServer < Formula
   head "https://github.com/dhall-lang/dhall-haskell.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "7c9f9d9f40fdca93c74c702d4d0ec4562dba40b108c23a199dbc421b8a0c7010"
-    sha256 cellar: :any_skip_relocation, catalina: "796f9f64bca695133ac77808829b3061d440a8f553d088aebcf43722faa97d61"
-    sha256 cellar: :any_skip_relocation, mojave:   "2315da10f4c2901f23639342719cedff401c42de3c5c07fed0030ecc6675443d"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, big_sur:  "5527e7912597fa0f7a243e56abf4ca7d1314f11a7b3f3d20e657352aa92ddf17"
+    sha256 cellar: :any_skip_relocation, catalina: "ddb16a6b5f23877ce240ed1fb00fced177c9d4839a5110acceda749723366da6"
+    sha256 cellar: :any_skip_relocation, mojave:   "5d8295828215b009bdf19784fde2b79feb13bfbfc7831a1a41278c9ea07b0fd7"
   end
 
   depends_on "cabal-install" => :build
