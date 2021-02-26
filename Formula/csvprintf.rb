ea1510ba9005de1d98413944fe7d50174d068fbf
Archie L. Cobbs <archie.cobbs@gmail.com>
2021-02-26 14:53:40 -0600

    csvprintf 1.1.0
    * csvprintf 1.1.0
    * Fix incorrect checksum.
    
    Closes #72026.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/csvprintf.rb
+++ Formula/csvprintf.rb
@@ -1,8 +1,8 @@
 class Csvprintf < Formula
   desc "Command-line utility for parsing CSV files"
   homepage "https://github.com/archiecobbs/csvprintf"
-  url "https://github.com/archiecobbs/csvprintf/archive/1.0.4.tar.gz"
-  sha256 "022188ced570203d6084e6eab68f7ad96054a4ab8aa54db1857a8fd076280568"
+  url "https://github.com/archiecobbs/csvprintf/archive/1.1.0.tar.gz"
+  sha256 "a64e48a81a4416f47c224a67b9554c93763429e25e4b15e0d23f3067bd6a0ffc"
   license "Apache-2.0"
 
   bottle do
