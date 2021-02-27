ad8f56dfd82b1e5fec83915682e6d577fab3fdcc
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-27 09:47:24 +0000

    crispy-doom: add 5.10.0 bottle.

--- Formula/crispy-doom.rb
+++ Formula/crispy-doom.rb
@@ -7,6 +7,13 @@ class CrispyDoom < Formula
 
   head "https://github.com/fabiangreffrath/crispy-doom"
 
+  bottle do
+    sha256 cellar: :any, arm64_big_sur: "2b5e266f3d833820c6f012714b82e025359a0f93b7a35ce0d69ca60a27dafdc0"
+    sha256 cellar: :any, big_sur:       "dea4133cb1b8869964d77da2a9f1cf551868ef85320738f6588e866665938c95"
+    sha256 cellar: :any, catalina:      "1b77f36d5151b7e5a627838a6fed8f559e147a9bd8c9d4d60fd7888c0fd06cf8"
+    sha256 cellar: :any, mojave:        "72135b7e809104c36e71495432f7338c64ad6b1c861bc125bcc0c3bb4aac5ec0"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
