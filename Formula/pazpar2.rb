ce27f1a1a9f7cb091457f98b20d95107f5de20fd
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-27 23:48:34 +0000

    pazpar2: update 1.14.0_3 bottle.

--- Formula/pazpar2.rb
+++ Formula/pazpar2.rb
@@ -12,11 +12,10 @@ class Pazpar2 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "db30d87db45b5e54488f4438929c68fa6a60ad9d8249ce188803c77ba08d3637"
-    sha256 cellar: :any, big_sur:       "17618f15fd88ee0512684d6844fa04abf01b9f6491ebe943ab6a38f6750949c5"
-    sha256 cellar: :any, catalina:      "6341978a6229069f3078497aa242dfacfb265fdd54c22a8864a516906c83921d"
-    sha256 cellar: :any, mojave:        "1921d7b34876024b8c0145df01e1c88a385c2851752fb30b2dc918f9ed8ee6d8"
-    sha256 cellar: :any, high_sierra:   "bde0d429ddb8f2012ea87a66a0ea1928ef491d414eec5efe231dec219f4d4675"
+    sha256 cellar: :any, arm64_big_sur: "60db540ce6032e173dad803d41cb870309b6459f7e61ee051305be90100df997"
+    sha256 cellar: :any, big_sur:       "a03f03e415910f4a4d2f0a69d1c132ad41c09581f9986f828f370a013169d05d"
+    sha256 cellar: :any, catalina:      "61a154c49f39f4b85c9e6a90b26f7240bf1345d04982bfd7a6b8d7fca47ae5a6"
+    sha256 cellar: :any, mojave:        "12269d35c8cc99cef098c71cb7d19ea42f594619915eff41dd3a0b5f43613f91"
   end
 
   head do
