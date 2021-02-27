b6710110f1c0239d26e33f942c58c38ebb39e25c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-27 09:51:50 +0000

    projectm: add 3.1.12 bottle.

--- Formula/projectm.rb
+++ Formula/projectm.rb
@@ -4,6 +4,13 @@ class Projectm < Formula
   url "https://github.com/projectM-visualizer/projectm/releases/download/v3.1.12/projectM-3.1.12.tar.gz"
   sha256 "b6b99dde5c8f0822ae362606a0429628ee478f4ec943a156723841b742954707"
   license "LGPL-2.1-or-later"
+
+  bottle do
+    sha256 arm64_big_sur: "4124ed10310e00ab4d706dcf40814adf0497af26cc95733aec708b82f4aaeced"
+    sha256 big_sur:       "c8ece4df06966643cf9aaae5f31610b98eaacddbfb7b0e56b21531d5e2f8f1a5"
+    sha256 catalina:      "8d11933c220cde67c4515ee5d42d99bc8e1c18479a4d3b746074c6080712cf0f"
+    sha256 mojave:        "9f7aef06ab68d557c1c989e08709903511a4fcd74fd166559d4f7bbf6af55548"
+  end
   head do
     url "https://github.com/projectM-visualizer/projectm.git"
 
