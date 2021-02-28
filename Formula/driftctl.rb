c6c9579e47948898ad5ffac7dcb93cf1b38f31db
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-28 07:38:18 +0000

    driftctl: update 0.5.0 bottle.

--- Formula/driftctl.rb
+++ Formula/driftctl.rb
@@ -6,9 +6,10 @@ class Driftctl < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "7787c3081a951a5153504c27646b4272e0cee36ebe1c9c5583ad08ebccb6744a"
-    sha256 cellar: :any_skip_relocation, catalina: "86ea68f9ab7c7fbf007e4aa5f03f1b75ff9509b773274c02127be38d745802e9"
-    sha256 cellar: :any_skip_relocation, mojave:   "a79c09d1ea891798b4cf978e47efaeb03801e6370d7f38c98012a2a8eb6460b0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9dd419bfb397bfa5039b9ce3795fed9ce3676c35619f46fa8e711359221fc483"
+    sha256 cellar: :any_skip_relocation, big_sur:       "7787c3081a951a5153504c27646b4272e0cee36ebe1c9c5583ad08ebccb6744a"
+    sha256 cellar: :any_skip_relocation, catalina:      "86ea68f9ab7c7fbf007e4aa5f03f1b75ff9509b773274c02127be38d745802e9"
+    sha256 cellar: :any_skip_relocation, mojave:        "a79c09d1ea891798b4cf978e47efaeb03801e6370d7f38c98012a2a8eb6460b0"
   end
 
   depends_on "go" => :build
