6ec504471c1443ff041bdf2160d7435117f2f3b1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-28 08:03:45 +0000

    terragrunt: update 0.28.7 bottle.

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -6,9 +6,10 @@ class Terragrunt < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "148b28e641e7764cbde59b8a8fd280a577826408f87c0cd8730efd49c5695793"
-    sha256 cellar: :any_skip_relocation, catalina: "3d3e94d1849b25e056b4d509137b7b1ceacb048005607e1316aabb1f8c5e9a55"
-    sha256 cellar: :any_skip_relocation, mojave:   "372f50a8985ab33529a03e9d942735be7c50fac8b0a3957b184c473478820a8a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "16d4538c5040ad09dee44322b197f8bdacde1033ea755ac24ebde420f66dc368"
+    sha256 cellar: :any_skip_relocation, big_sur:       "148b28e641e7764cbde59b8a8fd280a577826408f87c0cd8730efd49c5695793"
+    sha256 cellar: :any_skip_relocation, catalina:      "3d3e94d1849b25e056b4d509137b7b1ceacb048005607e1316aabb1f8c5e9a55"
+    sha256 cellar: :any_skip_relocation, mojave:        "372f50a8985ab33529a03e9d942735be7c50fac8b0a3957b184c473478820a8a"
   end
 
   depends_on "go" => :build
