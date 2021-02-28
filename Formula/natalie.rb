6a9d28bd5bf8f793de684030669b3a963503535c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-28 01:34:51 +0000

    natalie: update 0.7.0_1 bottle.

--- Formula/natalie.rb
+++ Formula/natalie.rb
@@ -9,6 +9,7 @@ class Natalie < Formula
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "df7c39d8bcb8d60b095d9b9b90207042791d994f22a53c97e709aa16768a9b28"
+    sha256 cellar: :any_skip_relocation, big_sur:       "6e745ce1d0a34e0887319f32d39ae10e7ae34475fcb9a54916569f7672074b22"
     sha256 cellar: :any_skip_relocation, catalina:      "218ec8bb0ac3ac4de7a6fa8489f3ad7013b1beb051a7c0e74a6e37ade79eee6c"
     sha256 cellar: :any_skip_relocation, mojave:        "9dcc093fc648175eb165aec20413246ace7427d0d3c4a9884d37cfad9a851dca"
     sha256 cellar: :any_skip_relocation, high_sierra:   "dd51e00a1969ffdd478e954bed48bedd1c5a9813b67931aa146711f49cb58223"
