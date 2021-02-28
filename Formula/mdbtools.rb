2da876bb4aa4bd05c14858e098dd62529a34a8bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-28 12:22:21 +0000

    mdbtools: update 0.9.2 bottle.

--- Formula/mdbtools.rb
+++ Formula/mdbtools.rb
@@ -6,10 +6,10 @@ class Mdbtools < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "51482d465b9bf4315b80f9b4b31423dbf1349b24f29358fdf0e6ea9a87a12b0b"
-    sha256 cellar: :any, big_sur:       "6a653b4fc6a4c990a1ea0f08085875010a1560fffcf100eafe0d81de52778464"
-    sha256 cellar: :any, catalina:      "04bb52a1f30aed9209498ea4fdbdf1b575a290f931632a2b37d1e6433b43f7b7"
-    sha256 cellar: :any, mojave:        "d2d9d42dee37437e5e9c6c701efd38b13456d59ebb1fb2fbebbcba8af7ff32c6"
+    sha256 cellar: :any, arm64_big_sur: "9053e31cbbc87cb451115fbe2fe039071e85cc05cdee130b1890d44888d9c6e3"
+    sha256 cellar: :any, big_sur:       "64736fd8868a154014317e36924e1246091fc788e1b3a037ba14678600ea3034"
+    sha256 cellar: :any, catalina:      "b37102d082ed7e84a5251551fa5e84618b118ebaa3224a5ec1d19e043551f4fc"
+    sha256 cellar: :any, mojave:        "49918a104b998fd7cd87f164d5d9ff47933e0d7cf3a9fb66214979fdfeedf45e"
   end
 
   depends_on "autoconf" => :build
