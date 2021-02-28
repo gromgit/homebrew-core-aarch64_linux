5ce118a9e95e22c726b17bc97c647fe05966bc51
Sam Ford <1584702+samford@users.noreply.github.com>
2021-02-27 20:39:56 -0500

    gpcslots2: update livecheck

--- Formula/gpcslots2.rb
+++ Formula/gpcslots2.rb
@@ -7,7 +7,7 @@ class Gpcslots2 < Formula
 
   livecheck do
     url :stable
-    regex(%r{url=.*?/gpcslots2[._-]v?(\d+(?:[-_.]\d+)+[a-z]?)}i)
+    regex(%r{url=.*?/gpcslots2[._-]v?(\d+(?:[._-]\d+)+[a-z]?)}i)
   end
 
   bottle :unneeded
