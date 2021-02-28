277efed0b3de7d1f0e5f4402c526737f6d7a2870
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-28 16:26:48 +0000

    xmlrpc-c: update 1.51.07 bottle.

--- Formula/xmlrpc-c.rb
+++ Formula/xmlrpc-c.rb
@@ -6,11 +6,10 @@ class XmlrpcC < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "43d9a3b95d745bc46c1a22e4d853a7f874d3f833c437032963331490a34ce00b"
-    sha256 cellar: :any, big_sur:       "4bf06fc636da854ff791a9d0af52db23e08b42fa9b8fe062cf22e10c57605822"
-    sha256 cellar: :any, catalina:      "f7d6a12c5e57d9ceb97b88dba518a6bae79e41e95343395a3f3c94bc168c92ab"
-    sha256 cellar: :any, mojave:        "638d1d6484717f57228174db4b9e875798b3ee1558ac6bcab59c96d80fcdab56"
-    sha256 cellar: :any, high_sierra:   "88a9ab0c2baea0aac627427628b00bce6828e5fc436533c0de8cfb0a99c21a24"
+    sha256 cellar: :any, arm64_big_sur: "d2f823ee08ce4b5cf1db9b3c7df182b213c4e4757e89207de1147def940544be"
+    sha256 cellar: :any, big_sur:       "a270cdb18af53d135dc5b46e772ebde0b0174aa4edcb334a3522ee79b7301f8b"
+    sha256 cellar: :any, catalina:      "8f2f988116b29088d76766843fa5d2eafc2f33d919990baeb31f67fd9b8c0b53"
+    sha256 cellar: :any, mojave:        "b058d27a7d2a0ba5a265186a5c4bcb26c9a9a0c9d4a60e639b30a72a2d4a7169"
   end
 
   def install
