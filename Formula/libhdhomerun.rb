5ce1b6a40c737cd34533666f8683ead679dd4383
Rui Chen <rui@chenrui.dev>
2021-02-28 02:31:21 -0500

    libhdhomerun 20210224
    Closes #72132.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libhdhomerun.rb
+++ Formula/libhdhomerun.rb
@@ -1,7 +1,7 @@
 class Libhdhomerun < Formula
   desc "C library for controlling SiliconDust HDHomeRun TV tuners"
   homepage "https://www.silicondust.com/support/linux/"
-  url "https://download.silicondust.com/hdhomerun/libhdhomerun_20210210.tgz"
+  url "https://download.silicondust.com/hdhomerun/libhdhomerun_20210224.tgz"
   sha256 "b996389aa6f124a6d9dc1e75ec749e86d06102e2b3e7359d57163d4cc6e633f8"
   license "LGPL-2.1-or-later"
 
