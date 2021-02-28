d4d029bf7af49f618ec8f1682fa9e10ba0921097
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-28 14:16:53 +0000

    corral: update 0.5.0 bottle.

--- Formula/corral.rb
+++ Formula/corral.rb
@@ -7,9 +7,9 @@ class Corral < Formula
   head "https://github.com/ponylang/corral.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "9b378d0793448c2d5ecf703f8915df5cbf4b1fd071fc032d8edc81a39740600a"
-    sha256 cellar: :any_skip_relocation, catalina: "bfcfe42cf0a50c636d8f5e8e6e69130fd2038c7321987fca00f691da854cef3a"
-    sha256 cellar: :any_skip_relocation, mojave:   "ffcbe9c301b1f6376fc7041e4b805b95c07443c34c9e320e5c73bbf5573fe67d"
+    sha256 cellar: :any_skip_relocation, big_sur:  "8f056b212b4030f5cb4c007f39ba13dd199c1439ecaf4ee415e212a08fed0182"
+    sha256 cellar: :any_skip_relocation, catalina: "9598cb8eabd2a7b7b3e59a85b1b580129c49c9654e4d9d358553a87133f6ab1c"
+    sha256 cellar: :any_skip_relocation, mojave:   "26664b499d3f4032ae2b66263ebf6324099175c00e25bb5b7530b1fcfc29220b"
   end
 
   depends_on "ponyc"
