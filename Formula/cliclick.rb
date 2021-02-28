e959807ae691099ba5dadfdd7aa294c4831f1370
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-02-28 09:18:16 +0000

    cliclick: update 5.0 bottle.

--- Formula/cliclick.rb
+++ Formula/cliclick.rb
@@ -7,13 +7,10 @@ class Cliclick < Formula
   head "https://github.com/BlueM/cliclick.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0e97f9a134eea73034dc32929f85e4206976925ee75a8e9f07dff74c86d1b16f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "bb164e96e155204666a83d0731f2115a13ecabc4ba07f8885a1194418ecd2068"
-    sha256 cellar: :any_skip_relocation, catalina:      "9ffcef6839fb396f022e415925f0e5f2cd1434d0eb723694d950d84fbaa3fcda"
-    sha256 cellar: :any_skip_relocation, mojave:        "bc373ee9ec978d416491fd82b3c25d02755b004b0abc1f5a73ff0b39e57b6b33"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "c371cb5b700e4a5f6495545b5f83e5414fc0e0cb98be05231a7abdf6c6607ff4"
-    sha256 cellar: :any_skip_relocation, sierra:        "bb47d1d8f4c81f31dc9372b5c06d3f74ecb25026805936f62e08744d1b888ecb"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "e8eca6037032ad2b68b9e8c4faa40adb770b10f868448e47852571f1829aa462"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b69ae3c108b346648be43b6a4c20278a4ee2964351e43f677079757343aa23d2"
+    sha256 cellar: :any_skip_relocation, big_sur:       "7e6bcf10902d1e4b2da9d1b8a4bd3999ab5fe6257ee67175f9e124d479fedb0b"
+    sha256 cellar: :any_skip_relocation, catalina:      "4cc28069cf249576d31df617e2be6be38dacb9896f21967d7ad3797e9d65a87c"
+    sha256 cellar: :any_skip_relocation, mojave:        "7ff1aa3722085a9bbcdbd3fe496a84dcef9774a75b0374c0c3f404517aa79eca"
   end
 
   def install
