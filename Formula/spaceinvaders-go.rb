078db9e73557df2cd2e537a62cc515e42b37903f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-28 07:43:34 +0000

    spaceinvaders-go: update 1.2.1 bottle.

--- Formula/spaceinvaders-go.rb
+++ Formula/spaceinvaders-go.rb
@@ -6,9 +6,10 @@ class SpaceinvadersGo < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "c0ed01fe110f5d7d681d12883f58962ae5c0ea721b032e8d3ef0adee41956841"
-    sha256 cellar: :any_skip_relocation, catalina: "1c4712409711d84aa1a7ce64214bb620e13660991f2afacd41681278ae0c3ba1"
-    sha256 cellar: :any_skip_relocation, mojave:   "596f084c8dad9588158ea9419e8a7ce4e33e8193d18b18e4095a6443b5e2fbbd"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6843984a81fcb723c3b1487a77c44f2a186a8cb17f00b0531bc8c8259afa303c"
+    sha256 cellar: :any_skip_relocation, big_sur:       "c0ed01fe110f5d7d681d12883f58962ae5c0ea721b032e8d3ef0adee41956841"
+    sha256 cellar: :any_skip_relocation, catalina:      "1c4712409711d84aa1a7ce64214bb620e13660991f2afacd41681278ae0c3ba1"
+    sha256 cellar: :any_skip_relocation, mojave:        "596f084c8dad9588158ea9419e8a7ce4e33e8193d18b18e4095a6443b5e2fbbd"
   end
 
   depends_on "go" => :build
