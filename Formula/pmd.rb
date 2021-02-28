8d7579f929c5ab22df869ec957ed4af306f8370a
Caleb Xu <calebcenter@live.com>
2021-02-28 09:05:12 -0500

    pmd 6.32.0 (#72150)

--- Formula/pmd.rb
+++ Formula/pmd.rb
@@ -1,8 +1,8 @@
 class Pmd < Formula
   desc "Source code analyzer for Java, JavaScript, and more"
   homepage "https://pmd.github.io"
-  url "https://github.com/pmd/pmd/releases/download/pmd_releases/6.31.0/pmd-bin-6.31.0.zip"
-  sha256 "ec9e81569fdde3ed52f504ff29d9f535560852435d3c467b605fb8d7728a0189"
+  url "https://github.com/pmd/pmd/releases/download/pmd_releases/6.32.0/pmd-bin-6.32.0.zip"
+  sha256 "abf7589fc74a3abeb04decb563a92c72116dc553fc88efb23bf68b81924d4569"
   license "BSD-4-Clause"
 
   bottle :unneeded
