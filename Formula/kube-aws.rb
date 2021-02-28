255f266b2a7bd1d1e390cd42cec0a80a0ca3ed52
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-28 07:41:41 +0000

    kube-aws: update 0.16.4 bottle.

--- Formula/kube-aws.rb
+++ Formula/kube-aws.rb
@@ -8,9 +8,10 @@ class KubeAws < Formula
   head "https://github.com/kubernetes-retired/kube-aws.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, catalina:    "1c5004445c0be8fd055ff78439bd3c0b413cd56247385c1453c5956fbe9503b1"
-    sha256 cellar: :any_skip_relocation, mojave:      "f05e8f3cfbe5f8c17f2cd6d3a854b7c329d7f922f03271bb36ca8497589ef7d4"
-    sha256 cellar: :any_skip_relocation, high_sierra: "5172a4ad55d3977c81d405bc67d91a35ead719e24c555d5843529d2489323d79"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7af14149a6b45de2067d7fbb256bcc452582134eda27688a82527d7a7a074b41"
+    sha256 cellar: :any_skip_relocation, catalina:      "1c5004445c0be8fd055ff78439bd3c0b413cd56247385c1453c5956fbe9503b1"
+    sha256 cellar: :any_skip_relocation, mojave:        "f05e8f3cfbe5f8c17f2cd6d3a854b7c329d7f922f03271bb36ca8497589ef7d4"
+    sha256 cellar: :any_skip_relocation, high_sierra:   "5172a4ad55d3977c81d405bc67d91a35ead719e24c555d5843529d2489323d79"
   end
 
   # Fork can be found at: https://github.com/kube-aws/kube-aws
