e0e3b7f355ca615ff6e799341310afba80fc3a59
loyalsoldier <10487845+Loyalsoldier@users.noreply.github.com>
2021-03-01 20:03:22 +0800

    buildozer 4.0.1
    Closes #72220.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/buildozer.rb
+++ Formula/buildozer.rb
@@ -1,11 +1,9 @@
 class Buildozer < Formula
   desc "Rewrite bazel BUILD files using standard commands"
   homepage "https://github.com/bazelbuild/buildtools"
-  url "https://github.com/bazelbuild/buildtools.git",
-      tag:      "3.5.0",
-      revision: "10384511ce98d864faf064a8ed54cdf31b98ac04"
+  url "https://github.com/bazelbuild/buildtools/archive/4.0.1.tar.gz"
+  sha256 "c28eef4d30ba1a195c6837acf6c75a4034981f5b4002dda3c5aa6e48ce023cf1"
   license "Apache-2.0"
-  head "https://github.com/bazelbuild/buildtools.git"
 
   bottle do
     sha256 cellar: :any_skip_relocation, big_sur:     "bec839741e5ae4f36dfb254bc52abc9bc9b89834920755ed2e84cffc078b5cca"
@@ -14,13 +12,10 @@ class Buildozer < Formula
     sha256 cellar: :any_skip_relocation, high_sierra: "a257921eb9df552c485cb2656ce7f535a708846308de4aa1ee47942492a61aee"
   end
 
-  disable! date: "2021-02-17", because: :does_not_build
-
-  depends_on "bazelisk" => :build
+  depends_on "go" => :build
 
   def install
-    system "bazelisk", "build", "--config=release", "buildozer:buildozer"
-    bin.install "bazel-bin/buildozer/darwin_amd64_stripped/buildozer"
+    system "go", "build", *std_go_args, "./buildozer"
   end
 
   test do
