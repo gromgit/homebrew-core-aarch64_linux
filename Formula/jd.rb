20cd8c3c745fe377296f020bbe6518ccb1ff3912
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-01 01:48:40 +0000

    jd: update 1.3.0 bottle.

--- Formula/jd.rb
+++ Formula/jd.rb
@@ -6,11 +6,11 @@ class Jd < Formula
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "dcc2f40817719ab6db214cf102ef282e7d5bd24fdb55b3a0f3e242bea28c3e9a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "838e83415f9a81d03dbde62ddc086233979d5b52b279c0a1edbc6ef754099a4d"
-    sha256 cellar: :any_skip_relocation, catalina:      "ed5e5d307438bf853693618019f776d6907ae948fd06ae1f7317b6d886645895"
-    sha256 cellar: :any_skip_relocation, mojave:        "d58d2bdb2a3a7f81f4ee78ac8dec5b71a1f0c2b6b89e83be8a921a74a263ac2d"
+    root_url "https://dl.bintray.com/homebrew/bottles"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5b45be90cd17c04df3705c2ee08cdef96e46e74367d8203b0e5d78b03ac925e7"
+    sha256 cellar: :any_skip_relocation, big_sur:       "25a7769810ab7ae690b0eb36ca77ebabb2c1d7bccf69fc5d84997019c1ecce42"
+    sha256 cellar: :any_skip_relocation, catalina:      "b2598d40731b157f9bad27e48397db6dd44f8df036f66657ea9961e6a04573a2"
+    sha256 cellar: :any_skip_relocation, mojave:        "82b255b04f78303e4df2fb1d68c8d2d0ff852372516efe6a9d1b476db96d9166"
   end
 
   depends_on "go" => :build
