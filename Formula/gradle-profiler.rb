3a2c0ce915cdca84e12c0d52937f6e3772236a0e
Jochen Schalanda <jochen@schalanda.name>
2021-03-01 17:37:10 +0100

    gradle-profiler 0.16.0
    * gradle-profiler 0.16.0
      Release notes: https://github.com/gradle/gradle-profiler/releases/tag/v0.16.0
    * Use OpenJDK 11 on M1 Macs
    
    Closes #72240.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gradle-profiler.rb
+++ Formula/gradle-profiler.rb
@@ -1,18 +1,28 @@
 class GradleProfiler < Formula
   desc "Profiling and benchmarking tool for Gradle builds"
   homepage "https://github.com/gradle/gradle-profiler/"
-  url "https://repo.gradle.org/gradle/ext-releases-local/org/gradle/profiler/gradle-profiler/0.15.0/gradle-profiler-0.15.0.zip"
-  sha256 "0cd279754502f796ea4bc684decd201cd8eea67566b3a6ef8b55f84cc4021d34"
+  url "https://repo.gradle.org/gradle/ext-releases-local/org/gradle/profiler/gradle-profiler/0.16.0/gradle-profiler-0.16.0.zip"
+  sha256 "f376581ed7b788d9d3d640a2ddde88747ce2e8a0e297991a77b98e6b7a257fbb"
   license "Apache-2.0"
 
   bottle :unneeded
 
-  depends_on "openjdk"
+  # gradle currently does not support Java 16
+  if Hardware::CPU.arm?
+    depends_on "openjdk@11"
+  else
+    depends_on "openjdk"
+  end
 
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %w[bin lib]
-    (bin/"gradle-profiler").write_env_script libexec/"bin/gradle-profiler", Language::Java.overridable_java_home_env
+    env = if Hardware::CPU.arm?
+      Language::Java.overridable_java_home_env("11")
+    else
+      Language::Java.overridable_java_home_env
+    end
+    (bin/"gradle-profiler").write_env_script libexec/"bin/gradle-profiler", env
   end
 
   test do
