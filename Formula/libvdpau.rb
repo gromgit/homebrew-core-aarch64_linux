05eed85c570eabaa51228fdcc5990a4cff1f380a
morusn <morusny12@outlook.com>
2021-03-01 22:48:54 +0100

    libvdpau 1.4
    * libvdpau 1.4
    * libvdpau change to meson, test needs update
    * libvdpau test
    * libvdpau update test
    
    Closes #72268.
    
    Co-authored-by: morusn <morusn@users.noreply.github.com>
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libvdpau.rb
+++ Formula/libvdpau.rb
@@ -1,8 +1,8 @@
 class Libvdpau < Formula
   desc "Open source Video Decode and Presentation API library"
   homepage "https://www.freedesktop.org/wiki/Software/VDPAU/"
-  url "https://gitlab.freedesktop.org/vdpau/libvdpau/uploads/14b620084c027d546fa0b3f083b800c6/libvdpau-1.2.tar.bz2"
-  sha256 "6a499b186f524e1c16b4f5b57a6a2de70dfceb25c4ee546515f26073cd33fa06"
+  url "https://gitlab.freedesktop.org/vdpau/libvdpau/-/archive/1.4/libvdpau-1.4.tar.bz2"
+  sha256 "4258824c5a4555ef31de0a7d82b3caf19e75a16a13949f1edafc5f6fb2d33f30"
   license "MIT"
 
   livecheck do
@@ -17,24 +17,20 @@ class Libvdpau < Formula
     sha256 mojave:        "59980ec6bf90b676354ddda5e3c93a6240c4564d1c01aa35b1f1aa804d7b949a"
   end
 
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => [:build, :test]
   depends_on "libx11"
   depends_on "libxext"
   depends_on "xorgproto"
 
   def install
-    on_macos do
-      ENV.append "LDFLAGS", "-lx11"
+    mkdir "build" do
+      system "meson", *std_meson_args, ".."
+      system "ninja"
+      system "ninja", "install"
     end
-    system "./configure", "--prefix=#{prefix}",
-                          "--sysconfdir=#{etc}",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--enable-dri2"
-    system "make"
-    system "make", "install"
   end
-
   test do
     assert_match "-I#{include}", shell_output("pkg-config --cflags vdpau")
   end
