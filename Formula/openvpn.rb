dd3e4da962094ff7325b45407594f3f210ad1e9b
morusn <morusny12@outlook.com>
2021-03-01 09:37:09 +0100

    openvpn 2.5.1
    * openvpn 2.5.1
    * Update Formula/openvpn.rb
    
    Closes #72216.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Co-authored-by: morusn <morusn@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/openvpn.rb
+++ Formula/openvpn.rb
@@ -1,9 +1,10 @@
 class Openvpn < Formula
   desc "SSL/TLS VPN implementing OSI layer 2 or 3 secure network extension"
   homepage "https://openvpn.net/community/"
-  url "https://swupdate.openvpn.org/community/releases/openvpn-2.5.0.tar.xz"
-  mirror "https://build.openvpn.net/downloads/releases/openvpn-2.5.0.tar.xz"
-  sha256 "029a426e44d656cb4e1189319c95fe6fc9864247724f5599d99df9c4c3478fbd"
+  url "https://swupdate.openvpn.org/community/releases/openvpn-2.5.1.tar.xz"
+  mirror "https://build.openvpn.net/downloads/releases/openvpn-2.5.1.tar.xz"
+  sha256 "40930489c837c05f6153f38e1ebaec244431ef1a034e4846ff732d71d59ff194"
+  license "GPL-2.0-only" => { with: "openvpn-openssl-exception" }
 
   livecheck do
     url "https://openvpn.net/community-downloads/"
