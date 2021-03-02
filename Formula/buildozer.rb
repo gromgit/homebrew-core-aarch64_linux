2ce9e8a1155184b721b597d9f1222825235eb34c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-02 11:03:21 +0000

    buildozer: update 4.0.1 bottle.

--- Formula/buildozer.rb
+++ Formula/buildozer.rb
@@ -6,10 +6,10 @@ class Buildozer < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:     "bec839741e5ae4f36dfb254bc52abc9bc9b89834920755ed2e84cffc078b5cca"
-    sha256 cellar: :any_skip_relocation, catalina:    "9fe7f3968698450308a92d4d9518ccc0b67720d730acd2533e752184f8bc1d86"
-    sha256 cellar: :any_skip_relocation, mojave:      "8279fdac655641ecc196576b16eeac00727bbeba8d9a811eb20916485c6d6f4a"
-    sha256 cellar: :any_skip_relocation, high_sierra: "a257921eb9df552c485cb2656ce7f535a708846308de4aa1ee47942492a61aee"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6e6fbe3529f8024adcfb4a07a878d24290e0005bdb139a370f955750311470c6"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a4d0c0665fff008552b629d97d0194a47e272e110fa01843fc95042163ae8d53"
+    sha256 cellar: :any_skip_relocation, catalina:      "9860c62aa97c12fdf060ec4046bddecd00235d420649ceccd23a290c251ef4bd"
+    sha256 cellar: :any_skip_relocation, mojave:        "8dd939f4e3e8575d5ad4f8a493f318a5f6659dbb9efc52a53fc3ca3821bd59e8"
   end
 
   depends_on "go" => :build
