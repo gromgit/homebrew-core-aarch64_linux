7d69e63a288d587e724bc02ccff31f7949f201b6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-02 20:42:16 +0000

    tengo: update 2.7.0 bottle.

--- Formula/tengo.rb
+++ Formula/tengo.rb
@@ -6,11 +6,10 @@ class Tengo < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9d5f43136699df81970b5abb38b77f012698b30527037ee682977fdd2d64b7ff"
-    sha256 cellar: :any_skip_relocation, big_sur:       "cb5e85ed9514dee925ffa888f3227e51dce06a1729df74dc7bc9af2415a22dee"
-    sha256 cellar: :any_skip_relocation, catalina:      "85740f093117930ce8a885a1fd9cad7e260c222d2e38be2d7299464806bffee8"
-    sha256 cellar: :any_skip_relocation, mojave:        "843a89b74e67bc348c99419750292fd0e4ff2c77913badb99a4de784493c75b1"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "035c213b4c8b252bfb81667a80b3bdef77bdb0dd7bed28fa7c557b1958f0e3e1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c3d202d97283a438d794342dbe1493da490d8037a39bdaca4390def38b3aafe0"
+    sha256 cellar: :any_skip_relocation, big_sur:       "c4135fd4371df7996b6212d4f389773c867a5709436d7ee81d65cac06399789e"
+    sha256 cellar: :any_skip_relocation, catalina:      "3ee279cb8c0fc8ca23feb76bc82e1744995904847006090423b303e3f07f5bc1"
+    sha256 cellar: :any_skip_relocation, mojave:        "bdc1d71d25be76a26f038353cb2824a8f7bdfe2e53122ae19e0723439db0d1f3"
   end
 
   depends_on "go" => :build
