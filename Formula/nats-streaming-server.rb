b9a097cbdea14150be5291c42f0a89d2ed34284e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-02 14:16:41 +0000

    nats-streaming-server: update 0.21.0 bottle.

--- Formula/nats-streaming-server.rb
+++ Formula/nats-streaming-server.rb
@@ -7,10 +7,10 @@ class NatsStreamingServer < Formula
   head "https://github.com/nats-io/nats-streaming-server.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4808703efa495eb81c0bc7ad9f50e1b648645306ad2e8577795040e9059a22c0"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4ef4e3919976de139871f911a13c23b4c8262a467ad000dc5f05a4bef62c039e"
-    sha256 cellar: :any_skip_relocation, catalina:      "1b4d959aeefda1c3f6e6c478df75f7d516eeea54846a65e0a3d40041b6b2f7f9"
-    sha256 cellar: :any_skip_relocation, mojave:        "5c16d0a8c941ba73efb754bafffb1451b5796ed20f7ac56d08de5469bf15a89d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2231ef6bea67f02148ccf57c244bcafcadeee0a67e2d4ba3ac10a157ba5fb3ae"
+    sha256 cellar: :any_skip_relocation, big_sur:       "7b566296fd02004932643b2155b5ce5942d099ae1313297b785edbb36c43e3e0"
+    sha256 cellar: :any_skip_relocation, catalina:      "b8751cfead6041112c3dc550a21b0accdab3c3e605c100a34c5c99c45350fbae"
+    sha256 cellar: :any_skip_relocation, mojave:        "e4366b1b27a50eeda6027513248897b406d6ae266e7da66e7872dec422ec1177"
   end
 
   depends_on "go" => :build
