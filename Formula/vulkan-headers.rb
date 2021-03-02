b22a054c49fa703dc7341089a9ec1632d4bf0741
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-02 19:43:19 +0000

    vulkan-headers: update 1.2.171 bottle.

--- Formula/vulkan-headers.rb
+++ Formula/vulkan-headers.rb
@@ -6,10 +6,10 @@ class VulkanHeaders < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "79e8ad5d156ab4699f44925f1f17d55400fb5dad56f6b1a2c03255b743af9363"
-    sha256 cellar: :any_skip_relocation, big_sur:       "78af0c01a3c3fc809824e1d96b7da5f8bdd508ee7b569a7332b9b8e28c7b2db4"
-    sha256 cellar: :any_skip_relocation, catalina:      "bb5a5c7098e04a7d12daebfc4d93b43e9da080976da683297130c3bcfac551f7"
-    sha256 cellar: :any_skip_relocation, mojave:        "eacf7d282e32dca90b8d30ebbd5787891cdfbef04347c6d6fbb74ca33e5512ad"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "15bb5201c7f58c17f0210bf429905376ea1d58b2a46bd0fc00f40c8d026fc927"
+    sha256 cellar: :any_skip_relocation, big_sur:       "6721d9160ac068df7d08a059a9243f06cb16590f6e816630d6e2d0b61377613c"
+    sha256 cellar: :any_skip_relocation, catalina:      "bbc7555fb4d6d580f990b00267038aef208399c65d2f97bd845b1460f6f5a300"
+    sha256 cellar: :any_skip_relocation, mojave:        "105b90d17804b6faf18bcbd44da93adfbcb009befff844d9788195c554fbcac5"
   end
 
   depends_on "cmake" => :build
