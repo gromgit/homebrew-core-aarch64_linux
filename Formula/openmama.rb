e8b2161e57c60fb8d6ae0b8e86d7bf172d4a0e31
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-02 22:41:51 +0000

    openmama: add 6.3.1 bottle.

--- Formula/openmama.rb
+++ Formula/openmama.rb
@@ -5,6 +5,13 @@ class Openmama < Formula
   sha256 "43e55db00290bc6296358c72e97250561ed4a4bb3961c1474f0876b81ecb6cf9"
   license "LGPL-2.1-only"
 
+  bottle do
+    sha256 arm64_big_sur: "ffa2cc7594c9a2f097bcbfe74884bc6675efe83ac80343bac9b46c1672555de3"
+    sha256 big_sur:       "70690e4a07aadfa4c28e9149fd615119e26336903bd182192ea64440df95012a"
+    sha256 catalina:      "a211e0f4ab73bd0b0d620797405b000e0bae0f043b068bac5244e389d8c38f87"
+    sha256 mojave:        "76d5eaf476206348c18eb981a5ff1c4d0b42a3ecf128e26df724d374e04d100d"
+  end
+
   depends_on "cmake" => :build
   depends_on "apr"
   depends_on "apr-util"
