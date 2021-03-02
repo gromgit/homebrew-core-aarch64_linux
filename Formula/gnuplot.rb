d95af860bea982de7b1974a89b06a1fbaa53a422
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-02 15:58:59 +0000

    gnuplot: update 5.4.1_2 bottle.

--- Formula/gnuplot.rb
+++ Formula/gnuplot.rb
@@ -7,11 +7,10 @@ class Gnuplot < Formula
   revision 2
 
   bottle do
-    rebuild 1
-    sha256 arm64_big_sur: "72a503fc93c60629c22d4f286d45365037d792d76c2f7ff8a76e6469641b0cc7"
-    sha256 big_sur:       "9a9db26ab2b8537521d4ff508d4797c8535434ddd34748ab4044867d8eef65a0"
-    sha256 catalina:      "1de9920502210ab56fbedc9bf4025ab8f0c88d164f022a1e767863f64b6e9954"
-    sha256 mojave:        "19ed248f7d406ade2e6fd1faa28069878cd5b2a0b73911d735289478faaab8c3"
+    sha256 arm64_big_sur: "b716ef439ed271b604f1f62a921f4134e25e10f4145eea6ba5fdb1f10e3369e9"
+    sha256 big_sur:       "71b95705ec9b7a5e03668d6491b75e66dbee794b4c7a02f20d54e4dfcbf4fa4e"
+    sha256 catalina:      "88f18dff95d0609b0cbf6eb53446869b1639998c69180b34a18c45c8a6bae520"
+    sha256 mojave:        "906aa3c73c481c21e252ace7653703f815ed15c71717baa1ff299f4004624368"
   end
 
   head do
