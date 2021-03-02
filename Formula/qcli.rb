874c5b0765679ecfa6e837feba36784303b38216
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-02 15:58:58 +0000

    qcli: update 1.2_2 bottle.

--- Formula/qcli.rb
+++ Formula/qcli.rb
@@ -8,9 +8,9 @@ class Qcli < Formula
   head "https://github.com/bavc/qctools.git"
 
   bottle do
-    sha256 cellar: :any, big_sur:  "8a4928c33d4be801e30927b50b2badf54a88c38a9e1c57715c0b4c87a893c739"
-    sha256 cellar: :any, catalina: "a25bcb4a21a2a5eaa3fd3789fc2065312bbe09ce080810fc663d1cd133bb81e2"
-    sha256 cellar: :any, mojave:   "c543c4a06c3c4c72c3d003047ece7850ac63d9e6e2e044fa09b48f20b9abfd5e"
+    sha256 cellar: :any, big_sur:  "227a1bf258d07d729f5060e75c512b708bf6fa816e40adc9d4126dafd3fabbdf"
+    sha256 cellar: :any, catalina: "b4822c9f864d0fe3b0a0d1e81efaef5daaad3e3f7efd3d520a0ebb711181622d"
+    sha256 cellar: :any, mojave:   "39f76c5605152c55bc40454dfbf2ec895af7d2a359c88692bbe0c49388c0a478"
   end
 
   depends_on "pkg-config" => :build
