032d82f01cd13ec0bf42a01f126a00ea5d40c097
Sam Ford <1584702+samford@users.noreply.github.com>
2021-03-02 12:15:57 -0500

    gegl: update livecheck

--- Formula/gegl.rb
+++ Formula/gegl.rb
@@ -8,7 +8,7 @@ class Gegl < Formula
 
   livecheck do
     url "https://download.gimp.org/pub/gegl/0.4/"
-    regex(/href=.*?gegl[._-]v?(\d+(?:\.\d+)*)\.t/i)
+    regex(/href=.*?gegl[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
   bottle do
