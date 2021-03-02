39e8aea792944f95b4a678404c2b7a5efa606730
morusn <morusny12@outlook.com>
2021-03-02 17:15:19 +0100

    opencbm 0.4.99.103
    * opencbm 0.4.99.103
    * opencbm patch removed
    * opencbm add build dependency pkg-config
    
    Closes #72322.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/opencbm.rb
+++ Formula/opencbm.rb
@@ -1,8 +1,8 @@
 class Opencbm < Formula
   desc "Provides access to various floppy drive formats"
   homepage "https://spiro.trikaliotis.net/opencbm"
-  url "https://spiro.trikaliotis.net/Download/opencbm-0.4.99.99/opencbm-0.4.99.99.tar.bz2"
-  sha256 "b1e4cd73c8459acd48c5e8536d47439bafea51f136f43fde5a4d6a5f7dbaf6c6"
+  url "https://github.com/OpenCBM/OpenCBM/archive/v0.4.99.103.tar.gz"
+  sha256 "026b0aa874b85763027641cfd206af92172d1120b9c667f35050bcfe53ba0b73"
   license "GPL-2.0-only"
   head "https://git.code.sf.net/p/opencbm/code.git"
 
@@ -24,14 +24,8 @@ class Opencbm < Formula
   # cc65 is only used to build binary blobs included with the programs; it's
   # not necessary in its own right.
   depends_on "cc65" => :build
+  depends_on "pkg-config" => :build
   depends_on "libusb-compat"
-
-  # Fix "usb_echo_test.c:32:10: fatal error: 'endian.h' file not found"
-  # Reported 24 Nov 2017 to www-201506 AT spiro DOT trikaliotis DOT net
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/48bd0fd/opencbm/endian.diff"
-    sha256 "2221fab81cdc0ca0cfbd55eff01ae3cd10b4e8bfca86082c7cbffb0b73b651cf"
-  end
 
   def install
     # This one definitely breaks with parallel build.
