b6bcdc9ddbc7bc1374f9890a1a280e8564ca5e56
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-03 15:28:50 +0000

    erlang: update 23.2.7 bottle.

--- Formula/erlang.rb
+++ Formula/erlang.rb
@@ -13,10 +13,10 @@ class Erlang < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "c309846f3db69e8de598ab4bcaaa918e9ae572b9ecd3ef5fcf73c8da333b7222"
-    sha256 cellar: :any, big_sur:       "5f075de1dd335f81de0d1a94b20a0160df950636e6b3e4e544ee4e64a899e08a"
-    sha256 cellar: :any, catalina:      "011ac4923a14306267552c31fd927fd6b3109f78f2281592f0e44f05510509a7"
-    sha256 cellar: :any, mojave:        "2cbad16ec2d28a7ade9614e05a956165ae4805b79689210315ffc975c4a9c4d3"
+    sha256 cellar: :any, arm64_big_sur: "17dd54ac9dc1f0b21422e781bdfbb6ee8db53e9c06d2a663a4d89a8eb6ce05b9"
+    sha256 cellar: :any, big_sur:       "42d12c0f32085c4e898d3a68531dd9d38b48b5a5eff085e000a3ba3cf736ce94"
+    sha256 cellar: :any, catalina:      "ba3e82593aee59d4ba65a63680575adc36f2efc008c91ec3724e28ca546b04b7"
+    sha256 cellar: :any, mojave:        "fd2d929a9f235fa7692d8a7586fce72fb8e600a631ccf962977ec6613fe1ae25"
   end
 
   depends_on "autoconf" => :build
