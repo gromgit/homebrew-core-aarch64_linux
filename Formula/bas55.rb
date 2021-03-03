9c87aa39d278145fa2a3e75e72704634c7c176e4
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-03-03 08:13:44 +0000

    bas55: add 1.19 bottle.

--- Formula/bas55.rb
+++ Formula/bas55.rb
@@ -5,6 +5,13 @@ class Bas55 < Formula
   sha256 "566097e216dab029d51afefdacf7806f249d57d117ca3e875e27c6cf61098ee0"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ab48670d6620654c1e88b8076d9c9805e6f6b2fde8f1d8773ac96fd5bd234789"
+    sha256 cellar: :any_skip_relocation, big_sur:       "c9a50cf6904cafd3a75cbb762ea68d3545eadc832be0b2c0313aaadf03dfe453"
+    sha256 cellar: :any_skip_relocation, catalina:      "7eaaf506f35bbc8f2149028c7b379f3b5fc7d5bd7899d021b016f94272e9441e"
+    sha256 cellar: :any_skip_relocation, mojave:        "1c7f697e391226ba6ba292cdb80801b2b221ed355918fa6ef14deb61abbb73ac"
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
