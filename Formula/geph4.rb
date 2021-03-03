f0f66f3b4ac2870bf2ee9458dc254cdcdfb10ae5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-03 17:25:20 +0000

    geph4: add 4.2.1 bottle.

--- Formula/geph4.rb
+++ Formula/geph4.rb
@@ -5,6 +5,13 @@ class Geph4 < Formula
   sha256 "bb5aaadb4fc55a22ec0be59761f9f5e71299e2c944c8b74432b45f278fb755c9"
   license "GPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8a0babcae532a1c52d15807c1da33337260ed0130a176b91dff3b3462e304e7c"
+    sha256 cellar: :any_skip_relocation, big_sur:       "edbe96c7e8395475bf3bb99a2b183ee34b474ebb325fc095eaa2fd052cc770c3"
+    sha256 cellar: :any_skip_relocation, catalina:      "484bf78360f764332036a34b5d260465a9ac52a57655087efac375389fa22d23"
+    sha256 cellar: :any_skip_relocation, mojave:        "97f3ed332edc560ae226d9b1c49ec3a7a17fa4583a90f9154fac24a81fc1fd58"
+  end
+
   depends_on "rust" => :build
 
   def install
