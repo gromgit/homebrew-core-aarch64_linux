7f39a1d8c6f7da84e0afe257797f63c55fa12284
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-03 22:36:42 +0000

    clojure-lsp 20210301T191854
    Closes #72418.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/clojure-lsp.rb
+++ Formula/clojure-lsp.rb
@@ -1,13 +1,13 @@
 class ClojureLsp < Formula
   desc "Language Server (LSP) for Clojure"
-  homepage "https://github.com/snoe/clojure-lsp"
+  homepage "https://github.com/clojure-lsp/clojure-lsp"
   # Switch to use git tag/revision as needed by `lein-git-version`
-  url "https://github.com/snoe/clojure-lsp.git",
-      tag:      "release-20201207T142850",
-      revision: "ab32504073688d507b53e47c354733cd6603bc88"
-  version "20201207T142850"
+  url "https://github.com/clojure-lsp/clojure-lsp.git",
+      tag:      "2021.03.01-19.18.54",
+      revision: "3c2dde99451a545da61d6a30ca3e7071581a1aa9"
+  version "20210301T191854"
   license "MIT"
-  head "https://github.com/snoe/clojure-lsp.git"
+  head "https://github.com/clojure-lsp/clojure-lsp.git"
 
   bottle do
     sha256 cellar: :any_skip_relocation, big_sur:  "a647293f345eead229f83e2707fb2c542958c9b4e33fb0bf4e63c7217548d392"
@@ -17,26 +17,24 @@ class ClojureLsp < Formula
 
   depends_on "leiningen" => :build
   # The Java Runtime version only recognizes class file versions up to 52.0
-  depends_on "openjdk@8"
+  depends_on "openjdk@11"
 
   def install
     system "lein", "uberjar"
     jar = Dir["target/clojure-lsp-*-standalone.jar"][0]
     libexec.install jar
-    bin.write_jar_script libexec/File.basename(jar), "clojure-lsp"
+    bin.write_jar_script libexec/File.basename(jar), "clojure-lsp", java_version: "11"
   end
 
   test do
-    require "Open3"
+    input =
+      "Content-Length: 152\r\n" \
+      "\r\n" \
+      "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"initialize\",\"params\":{\"" \
+      "processId\":88075,\"rootUri\":null,\"capabilities\":{},\"trace\":\"ver" \
+      "bose\",\"workspaceFolders\":null}}\r\n"
 
-    stdin, stdout, _, wait_thr = Open3.popen3("#{bin}/clojure-lsp")
-    pid = wait_thr.pid
-    stdin.write <<~EOF
-      Content-Length: 58
-
-      {"jsonrpc":"2.0","method":"initialize","params":{},"id":1}
-    EOF
-    assert_match "Content-Length", stdout.gets("\n")
-    Process.kill "SIGKILL", pid
+    output = pipe_output("#{bin}/clojure-lsp", input, 0)
+    assert_match "Content-Length", output
   end
 end
