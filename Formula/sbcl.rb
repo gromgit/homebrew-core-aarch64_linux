4a93a4487ab5164e919258e05e0f7e45e740b119
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-03 16:07:54 +0000

    sbcl: update 2.1.2 bottle.

--- Formula/sbcl.rb
+++ Formula/sbcl.rb
@@ -7,9 +7,10 @@ class Sbcl < Formula
   head "https://git.code.sf.net/p/sbcl/sbcl.git", shallow: false
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "e6a0d4ba798d435a261e93062461a15801a42af1cab9344cdeab824b00112318"
-    sha256 cellar: :any_skip_relocation, catalina: "82afe9ba6dad370ba0895be28b3591eeae9bb88fbba93a916637cea1e6b140c3"
-    sha256 cellar: :any_skip_relocation, mojave:   "a8eadb8a7b8a092995d0f29f4b68cf5450c070dd41bdf3f9f15e4907b516d48b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d915b3200cbce70f8fa0ee4e490bb48b2e8c821870c3b5e640b4ad34741665eb"
+    sha256 cellar: :any_skip_relocation, big_sur:       "4b72ee3689681c976edd70bdf8260dccfc8a91af597a9bc3bb00cb9adaceea5e"
+    sha256 cellar: :any_skip_relocation, catalina:      "6b9e2e3539da50cbdb40cd33e8cf1bbd3414a2c4ac1af01c59040471d2382646"
+    sha256 cellar: :any_skip_relocation, mojave:        "205d6fa7d4c8c0bf18269eca87b38ba47e051935c4e6d29555d4f7c5b93149e7"
   end
 
   depends_on "ecl" => :build
