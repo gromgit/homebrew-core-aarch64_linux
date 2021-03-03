c6ebfc6f27e3e216123d5b9f5ea606852f5970f8
rui <rui@chenrui.dev>
2021-03-03 18:54:03 +0000

    pulumi 2.22.0
    Closes #72413.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pulumi.rb
+++ Formula/pulumi.rb
@@ -2,8 +2,8 @@ class Pulumi < Formula
   desc "Cloud native development platform"
   homepage "https://pulumi.io/"
   url "https://github.com/pulumi/pulumi.git",
-      tag:      "v2.21.2",
-      revision: "543cbe194c17a515eb682b9dd105b0dcfd30a810"
+      tag:      "v2.22.0",
+      revision: "011fa0577e1a1fa9723bc5933aa562e2fc03d365"
   license "Apache-2.0"
   head "https://github.com/pulumi/pulumi.git"
 
