78f9c8d6f25c72a5e59441c981d63b8af8013a8b
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-03-03 09:05:07 +0000

    latino: add 1.3.0 bottle.

--- Formula/latino.rb
+++ Formula/latino.rb
@@ -6,6 +6,13 @@ class Latino < Formula
   license "MIT"
   head "https://github.com/MelvinG24/Latino"
 
+  bottle do
+    sha256 cellar: :any, arm64_big_sur: "dab07196acce245e9876fac1be97127e65d0111ffdbf7e08695cb7468c12b060"
+    sha256 cellar: :any, big_sur:       "3f1d3892f16fd82ce53434d6f0b3d7e27a799e390450f2cc5bb851a47b88aa92"
+    sha256 cellar: :any, catalina:      "cb134d3aef396a2d84acfb1807a2d9735c3252baeab1313dc4d3654823253168"
+    sha256 cellar: :any, mojave:        "73c7c6ce9b5ff470368fb1bb0e54be92aa376577c396428535b79bf1f76aefc1"
+  end
+
   depends_on "cmake" => :build
 
   def install
