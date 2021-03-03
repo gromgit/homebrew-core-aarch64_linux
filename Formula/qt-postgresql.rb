19305be17edf4ec236f3bd519d4c9f45ce155ac9
PaperChalice <lgamma@163.com>
2021-03-03 10:12:21 +0800

    qt-postgresql: new formula

---
+++ Formula/qt-postgresql.rb
@@ -0,0 +1,72 @@
+class QtPostgresql < Formula
+  desc "Qt SQL Database Driver"
+  homepage "https://www.qt.io/"
+  url "https://download.qt.io/official_releases/qt/6.0/6.0.2/submodules/qtbase-everywhere-src-6.0.2.tar.xz"
+  sha256 "991a0e4e123104e76563067fcfa58602050c03aba8c8bb0c6198347c707817f1"
+  license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
+
+  depends_on "cmake" => [:build, :test]
+
+  depends_on "postgresql"
+  depends_on "qt"
+
+  def install
+    cd "src/plugins/sqldrivers" do
+      system "qmake"
+      system "make", "sub-psql"
+      (share/"qt").install "plugins/"
+    end
+    Pathname.glob(share/"qt/plugins/sqldrivers/#{shared_library("*")}") do |plugin|
+      system "strip", "-S", "-x", plugin
+    end
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<~EOS
+      cmake_minimum_required(VERSION 3.19.0)
+      project(test VERSION 1.0.0 LANGUAGES CXX)
+      set(CMAKE_CXX_STANDARD 17)
+      set(CMAKE_CXX_STANDARD_REQUIRED ON)
+      set(CMAKE_AUTOMOC ON)
+      set(CMAKE_AUTORCC ON)
+      set(CMAKE_AUTOUIC ON)
+      find_package(Qt6 COMPONENTS Core Sql REQUIRED)
+      add_executable(test
+          main.cpp
+      )
+      target_link_libraries(test PRIVATE Qt6::Core Qt6::Sql)
+    EOS
+
+    (testpath/"test.pro").write <<~EOS
+      QT       += core sql
+      QT       -= gui
+      TARGET = test
+      CONFIG   += console debug
+      CONFIG   -= app_bundle
+      TEMPLATE = app
+      SOURCES += main.cpp
+    EOS
+
+    (testpath/"main.cpp").write <<~EOS
+      #include <QCoreApplication>
+      #include <QtSql>
+      #include <cassert>
+      int main(int argc, char *argv[])
+      {
+        QCoreApplication a(argc, argv);
+        QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL");
+        assert(db.isValid());
+        return 0;
+      }
+    EOS
+
+    system "cmake", "-DCMAKE_BUILD_TYPE=Debug", testpath
+    system "make"
+    system "./test"
+
+    ENV.delete "CPATH"
+    system "qmake"
+    system "make"
+    system "./test"
+  end
+end
