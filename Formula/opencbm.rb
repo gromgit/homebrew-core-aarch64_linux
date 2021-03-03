bc19624c31250bf8a52d92082d0be7bd669f116e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-03 08:21:03 +0000

    opencbm: update 0.4.99.103 bottle.

--- Formula/opencbm.rb
+++ Formula/opencbm.rb
@@ -12,13 +12,10 @@ class Opencbm < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "badee8af25b105994dbae61bc458d97cece5d627cc801d131d5f9575dcd2a7d7"
-    sha256 big_sur:       "2c6f695b90e4b6dec2954d4cab254abe8563998311634ced199641a9b8c65aca"
-    sha256 catalina:      "0fcf92ca18ebde6b9d431dfd1ab8667ca93ee59c53f85e818eed9f0b8ba78306"
-    sha256 mojave:        "489ae793f9f226c93667cf67f23d7eac1cde97d4ed33549bbe9731fcc824eb2a"
-    sha256 high_sierra:   "d9555da83fc70f801663f13cfc4ed34241feec72d29125fb12a2105fce414210"
-    sha256 sierra:        "6ff076233d442f7f15e22595623cf46c38cf1024997240bd48db1e4bb01c44c2"
-    sha256 el_capitan:    "6ba3fc869e59f002f6ae897cbb34b4ece023c11371c3d611453b330714b65cba"
+    sha256 arm64_big_sur: "14d82a0d9ca1fb0a90a012526fe7ca814291baabded7c794fdfedb050fe8dd1d"
+    sha256 big_sur:       "aaf54902db867f5d5116e5a5701320c52fd8bdaf587fe3824ccf57051671a5e6"
+    sha256 catalina:      "6d85698fdf68fcd53e794ff94495bb95de4f9f1bed54d8c2b31ede5e0e9dfb56"
+    sha256 mojave:        "78c7219df463cc71eb494724a5dee0444a218dadeb2a14308c529d8e0ea29126"
   end
 
   # cc65 is only used to build binary blobs included with the programs; it's
