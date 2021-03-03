f9ef9cb4bc6398b7bf4ba82166a8d7d636b34a62
PaperChalice <lgamma@163.com>
2021-03-03 10:11:24 +0800

    qt-percona-server: new formula

---
+++ Formula/qt-percona-server.rb
@@ -0,0 +1,75 @@
+class QtPerconaServer < Formula
+  desc "Qt SQL Database Driver"
+  homepage "https://www.qt.io/"
+  url "https://download.qt.io/official_releases/qt/6.0/6.0.2/submodules/qtbase-everywhere-src-6.0.2.tar.xz"
+  sha256 "991a0e4e123104e76563067fcfa58602050c03aba8c8bb0c6198347c707817f1"
+  license all_of: ["LGPL-2.1-only", "LGPL-3.0-only"]
+
+  depends_on "cmake" => [:build, :test]
+
+  depends_on "percona-server"
+  depends_on "qt"
+
+  conflicts_with "qt-mysql", "qt-mariadb",
+    because: "qt-mysql, qt-mariadb, and qt-percona-server install the same binaries"
+
+  def install
+    cd "src/plugins/sqldrivers" do
+      system "qmake"
+      system "make", "sub-mysql"
+      (share/"qt").install "plugins/"
+    end
+    Pathname.glob(share/"qt/plugins/sqldrivers/#{shared_library("*")}") do |plugin|
+      system "strip", "-S", "-x", plugin
+    end
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<~EOS
+      cmake_minimum_required(VERSION 3.16.0)
+      project(test VERSION 1.0.0 LANGUAGES CXX)
+      set(CMAKE_CXX_STANDARD 17)
+      set(CMAKE_CXX_STANDARD_REQUIRED ON)
+      set(CMAKE_AUTOMOC ON)
+      set(CMAKE_AUTORCC ON)
+      set(CMAKE_AUTOUIC ON)
+      find_package(Qt6 COMPONENTS Core Sql REQUIRED)
+      add_executable(test
+          main.cpp
+      )
+      target_link_libraries(test PRIVATE Qt6::Core Qt6::Sql)
+    EOS
+
+    (testpath/"test.pro").write <<~EOS
+      QT       += core sql
+      QT       -= gui
+      TARGET = test
+      CONFIG   += console
+      CONFIG   -= app_bundle
+      TEMPLATE = app
+      SOURCES += main.cpp
+    EOS
+
+    (testpath/"main.cpp").write <<~EOS
+      #include <QCoreApplication>
+      #include <QtSql>
+      #include <cassert>
+      int main(int argc, char *argv[])
+      {
+        QCoreApplication a(argc, argv);
+        QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+        assert(db.isValid());
+        return 0;
+      }
+    EOS
+
+    system "cmake", "-DCMAKE_BUILD_TYPE=Debug", testpath
+    system "make"
+    system "./test"
+
+    ENV.delete "CPATH"
+    system "qmake"
+    system "make"
+    system "./test"
+  end
+end
