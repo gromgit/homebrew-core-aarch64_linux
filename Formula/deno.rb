39474653332762ab74c944de843c1b4cc01e745e
Bo Anderson <mail@boanderson.me>
2021-03-03 03:00:01 +0000

    deno 1.8.0
    Closes #72365.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/deno.rb
+++ Formula/deno.rb
@@ -1,8 +1,8 @@
 class Deno < Formula
   desc "Secure runtime for JavaScript and TypeScript"
   homepage "https://deno.land/"
-  url "https://github.com/denoland/deno/releases/download/v1.7.2/deno_src.tar.gz"
-  sha256 "a35b2cfef924fe0404eb901a3bfa0c4d50825db3c14e4da823963e645f96e83d"
+  url "https://github.com/denoland/deno/releases/download/v1.8.0/deno_src.tar.gz"
+  sha256 "5cd695ef8f16e947d5155e53b159a147e538405310f892028fe5e2d21eddc3f9"
   license "MIT"
 
   bottle do
@@ -15,15 +15,18 @@ class Deno < Formula
   depends_on "llvm" => :build
   depends_on "ninja" => :build
   depends_on "rust" => :build
-  depends_on "sccache" => :build
   depends_on xcode: ["10.0", :build] # required by v8 7.9+
-  depends_on :macos # Due to Python 2 (see https://github.com/denoland/deno/issues/2893)
+  depends_on :macos # Due to Python 2 (see https://bugs.chromium.org/p/chromium/issues/detail?id=942720)
 
   uses_from_macos "xz"
 
+  # To find the version of gn used:
+  # 1. Find rusty_v8 version: https://github.com/denoland/deno/blob/v#{version}/core/Cargo.toml
+  # 2. Find buildtools submodule commit: https://github.com/denoland/rusty_v8/tree/v#{rusty_v8_version}
+  # 3. Check gn_version: https://github.com/denoland/chromium_buildtools/blob/#{buildtools_commit}/DEPS
   resource "gn" do
     url "https://gn.googlesource.com/gn.git",
-        revision: "53d92014bf94c3893886470a1c7c1289f8818db0"
+        revision: "dfcbc6fed0a8352696f92d67ccad54048ad182b3"
   end
 
   def install
@@ -33,12 +36,11 @@ class Deno < Formula
     # env args for building a release build with our clang, ninja and gn
     ENV["GN"] = buildpath/"gn/out/gn"
     ENV["NINJA"] = Formula["ninja"].opt_bin/"ninja"
-    ENV["SCCACHE"] = Formula["sccache"].opt_bin/"sccache"
     # build rusty_v8 from source
     ENV["V8_FROM_SOURCE"] = "1"
-    # Build with llvm. We don't remove llvm from HOMEBREW_LIBRARY_PATHS
-    # as this causes build failures. This does not create a runtime dependency.
+    # Build with llvm and link against system libc++ (no runtime dep)
     ENV["CLANG_BASE_PATH"] = Formula["llvm"].prefix
+    ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib
 
     resource("gn").stage buildpath/"gn"
     cd "gn" do
@@ -47,7 +49,9 @@ class Deno < Formula
     end
 
     cd "cli" do
-      system "cargo", "install", "-vv", *std_cargo_args
+      # cargo seems to build rusty_v8 twice in parallel, which causes problems,
+      # hence the need for -j1
+      system "cargo", "install", "-vv", "-j1", *std_cargo_args
     end
 
     bash_output = Utils.safe_popen_read("#{bin}/deno", "completions", "bash")
