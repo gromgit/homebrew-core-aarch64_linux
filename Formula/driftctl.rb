a731f3a21da7e2a3eb584fddef8529dca6360c9f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-04 13:25:56 +0000

    driftctl: update 0.6.0 bottle.

--- Formula/driftctl.rb
+++ Formula/driftctl.rb
@@ -6,10 +6,10 @@ class Driftctl < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9dd419bfb397bfa5039b9ce3795fed9ce3676c35619f46fa8e711359221fc483"
-    sha256 cellar: :any_skip_relocation, big_sur:       "7787c3081a951a5153504c27646b4272e0cee36ebe1c9c5583ad08ebccb6744a"
-    sha256 cellar: :any_skip_relocation, catalina:      "86ea68f9ab7c7fbf007e4aa5f03f1b75ff9509b773274c02127be38d745802e9"
-    sha256 cellar: :any_skip_relocation, mojave:        "a79c09d1ea891798b4cf978e47efaeb03801e6370d7f38c98012a2a8eb6460b0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c3d0b4e12bb73c2ed504d81cfebd6cd32374388d5847a59a59367b1cd9701df5"
+    sha256 cellar: :any_skip_relocation, big_sur:       "7b2e9c5e7cf215752910878ab3aad22aa596dcbd2bd47c8b521395abae5814b9"
+    sha256 cellar: :any_skip_relocation, catalina:      "1ac34b52b8fff225f804473e5ddf57d7ef5177e72a942a8a0264984e3ea1f16c"
+    sha256 cellar: :any_skip_relocation, mojave:        "2479a4f61803209f070f6c5abd55ac5781aeb7e10d2e589aa5c4524cd1bd6bd2"
   end
 
   depends_on "go" => :build
