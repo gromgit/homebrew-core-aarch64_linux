3f9da88c8851b93a0a31d6a2de09b3803114b680
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-04 12:03:20 +0000

    kotlin-language-server: add 1.1.1 bottle.

--- Formula/kotlin-language-server.rb
+++ Formula/kotlin-language-server.rb
@@ -5,6 +5,13 @@ class KotlinLanguageServer < Formula
   sha256 "889a8866affe175c0c3fcd37b7a11e3f67b319ab3c1404d69c63b09a1796d3b4"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c9e6d9feaaba28afcd3e8e472f24afab703597b863fff06c4986453457b732a9"
+    sha256 cellar: :any_skip_relocation, big_sur:       "2667fa432ee3189383e116565bd5d857bc996a7134991d5ff302769b4baf5b3d"
+    sha256 cellar: :any_skip_relocation, catalina:      "24343fc079a65ec189c3ea2b97f6321ca5922cbeb62015971e6c16634e746bf5"
+    sha256 cellar: :any_skip_relocation, mojave:        "44853329fb56e71e5b6670c2e7a018738f1e258dfd44184af9fc0e883bb46662"
+  end
+
   depends_on "gradle" => :build
   depends_on "openjdk@11"
 
