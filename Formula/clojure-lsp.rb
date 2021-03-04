098325fa064dee0bab5da41b74093a3ed13dde04
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-04 13:25:48 +0000

    clojure-lsp: update 20210301T191854 bottle.

--- Formula/clojure-lsp.rb
+++ Formula/clojure-lsp.rb
@@ -10,9 +10,10 @@ class ClojureLsp < Formula
   head "https://github.com/clojure-lsp/clojure-lsp.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "a647293f345eead229f83e2707fb2c542958c9b4e33fb0bf4e63c7217548d392"
-    sha256 cellar: :any_skip_relocation, catalina: "079f2087995cd399f1c99dddc5f1d6d92e55af2facf67b427fb633a80faba842"
-    sha256 cellar: :any_skip_relocation, mojave:   "fc1b26dc8f000fc728c26bbedff9f9ab0d6f2071ef17eeb4a0f71c9626184cc7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9d390d657349ecad4b0a0c56fabf6a4ce322ed49df08b100b8543e3578425723"
+    sha256 cellar: :any_skip_relocation, big_sur:       "dc21005dde338ac2c543c60fc0fe91fd26eaea952cd50e5c440569e879905ef4"
+    sha256 cellar: :any_skip_relocation, catalina:      "b9a7dc46c9e19171e96bee191c38a3b6c2b36fcba85862d0a8e5043bb0075e5c"
+    sha256 cellar: :any_skip_relocation, mojave:        "79236bf7ddf83da7c5cc667978526941879d45bb906595d5ff1000696bb528eb"
   end
 
   depends_on "leiningen" => :build
