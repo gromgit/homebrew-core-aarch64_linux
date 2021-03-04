58c0d2272a35dba66580a74dcf9cf4d549f0db74
Bo Anderson <mail@boanderson.me>
2021-03-04 17:10:10 +0000

    percona-xtrabackup 8.0.22-15

--- Formula/percona-xtrabackup.rb
+++ Formula/percona-xtrabackup.rb
@@ -1,9 +1,8 @@
 class PerconaXtrabackup < Formula
   desc "Open source hot backup tool for InnoDB and XtraDB databases"
   homepage "https://www.percona.com/software/mysql-database/percona-xtrabackup"
-  url "https://www.percona.com/downloads/Percona-XtraBackup-LATEST/Percona-XtraBackup-8.0.13/source/tarball/percona-xtrabackup-8.0.13.tar.gz"
-  sha256 "760f556e85ad55bd54019ad78b1064557c68e31b6e37dc4f4ce1f0065b911f71"
-  revision 1
+  url "https://www.percona.com/downloads/Percona-XtraBackup-LATEST/Percona-XtraBackup-8.0.22-15/source/tarball/percona-xtrabackup-8.0.22-15.tar.gz"
+  sha256 "064fe405f8c1f94edd9300756bdf979eae0a41c5cad15762caabfe24f5332eac"
 
   livecheck do
     url "https://github.com/percona/percona-xtrabackup.git"
@@ -24,47 +23,58 @@ class PerconaXtrabackup < Formula
   depends_on "libgcrypt"
   depends_on "mysql-client"
   depends_on "openssl@1.1"
+  depends_on "protobuf"
 
   uses_from_macos "perl"
 
-  conflicts_with "protobuf", because: "both install libprotobuf(-lite) libraries"
-  conflicts_with "percona-server", because: "both install comp_err.1 man page"
+  # Should be installed before DBD::mysql
+  resource "Devel::CheckLib" do
+    url "https://cpan.metacpan.org/authors/id/M/MA/MATTN/Devel-CheckLib-1.14.tar.gz"
+    sha256 "f21c5e299ad3ce0fdc0cb0f41378dca85a70e8d6c9a7599f0e56a957200ec294"
+  end
 
-  resource "DBI" do
-    url "https://cpan.metacpan.org/authors/id/T/TI/TIMB/DBI-1.641.tar.gz"
-    sha256 "5509e532cdd0e3d91eda550578deaac29e2f008a12b64576e8c261bb92e8c2c1"
+  # In Mojave, this is not part of the system Perl anymore
+  if MacOS.version >= :mojave
+    resource "DBI" do
+      url "https://cpan.metacpan.org/authors/id/T/TI/TIMB/DBI-1.643.tar.gz"
+      sha256 "8a2b993db560a2c373c174ee976a51027dd780ec766ae17620c20393d2e836fa"
+    end
   end
 
   resource "DBD::mysql" do
-    url "https://cpan.metacpan.org/authors/id/C/CA/CAPTTOFU/DBD-mysql-4.046.tar.gz"
-    sha256 "6165652ec959d05b97f5413fa3dff014b78a44cf6de21ae87283b28378daf1f7"
+    url "https://cpan.metacpan.org/authors/id/D/DV/DVEEDEN/DBD-mysql-4.050.tar.gz"
+    sha256 "4f48541ff15a0a7405f76adc10f81627c33996fbf56c95c26c094444c0928d78"
   end
 
+  # https://github.com/percona/percona-xtrabackup/blob/percona-xtrabackup-#{version}/cmake/boost.cmake
   resource "boost" do
-    url "https://downloads.sourceforge.net/project/boost/boost/1.70.0/boost_1_70_0.tar.bz2"
-    sha256 "430ae8354789de4fd19ee52f3b1f739e1fba576f0aded0897c3c2bc00fb38778"
+    url "https://dl.bintray.com/boostorg/release/1.73.0/source/boost_1_73_0.tar.bz2"
+    sha256 "4eb3b8d442b426dc35346235c8733b5ae35ba431690e38c6a8263dce9fcbb402"
   end
 
   def install
-    cmake_args = %w[
+    cmake_args = %W[
       -DBUILD_CONFIG=xtrabackup_release
       -DCOMPILATION_COMMENT=Homebrew
       -DINSTALL_PLUGINDIR=lib/percona-xtrabackup/plugin
       -DINSTALL_MANDIR=share/man
       -DWITH_MAN_PAGES=ON
       -DINSTALL_MYSQLTESTDIR=
-      -DCMAKE_CXX_FLAGS="-DBOOST_NO_CXX11_HDR_ARRAY"
+      -DWITH_SSL=#{Formula["openssl@1.1"].opt_prefix}
+      -DWITH_PROTOBUF=system
     ]
 
     # macOS has this value empty by default.
     # See https://bugs.python.org/issue18378#msg215215
     ENV["LC_ALL"] = "en_US.UTF-8"
 
-    # 1.70.0 specifically required. Detailed in cmake/boost.cmake
-    (buildpath/"boost_1_70_0").install resource("boost")
-    cmake_args << "-DWITH_BOOST=#{buildpath}/boost_1_70_0"
+    (buildpath/"boost").install resource("boost")
+    cmake_args << "-DWITH_BOOST=#{buildpath}/boost"
 
     cmake_args.concat std_cmake_args
+
+    # Remove conflicting manpages
+    rm (Dir["man/*"] - ["man/CMakeLists.txt"])
 
     mkdir "build" do
       system "cmake", "..", *cmake_args
@@ -74,6 +84,21 @@ class PerconaXtrabackup < Formula
 
     # remove conflicting library that is already installed by mysql
     rm lib/"libmysqlservices.a"
+
+    on_macos do
+      # Remove libssl copies as the binaries use the keg anyway and they create problems for other applications
+      rm lib/"libssl.dylib"
+      rm lib/"libssl.1.1.dylib"
+      rm lib/"libcrypto.1.1.dylib"
+      rm lib/"libcrypto.dylib"
+    end
+
+    ENV.prepend_create_path "PERL5LIB", buildpath/"build_deps/lib/perl5"
+
+    resource("Devel::CheckLib").stage do
+      system "perl", "Makefile.PL", "INSTALL_BASE=#{buildpath}/build_deps"
+      system "make", "install"
+    end
 
     ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
 
@@ -89,7 +114,8 @@ class PerconaXtrabackup < Formula
       system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
       system "make", "install"
     end
-    bin.env_script_all_files(libexec/"bin", PERL5LIB: ENV["PERL5LIB"])
+
+    bin.env_script_all_files(libexec/"bin", PERL5LIB: libexec/"lib/perl5")
   end
 
   test do
