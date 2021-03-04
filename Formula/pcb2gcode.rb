a0472c137c30def8bc985258934b863f389b9069
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-04 21:22:06 +0000

    pcb2gcode: update 2.3.0 bottle.

--- Formula/pcb2gcode.rb
+++ Formula/pcb2gcode.rb
@@ -7,10 +7,10 @@ class Pcb2gcode < Formula
   head "https://github.com/pcb2gcode/pcb2gcode.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "34850262d5d86ab272a3348972bc1e90add961c1b798afb84d05104176b95573"
-    sha256 cellar: :any, big_sur:       "28aaa8d461c0c1736e80025af7676468a782457157b79a64b1f5e6b8aa75999b"
-    sha256 cellar: :any, catalina:      "5980b526dae830404d14f715f2d37cb5c4564df900a7f678102fd373f0eca04c"
-    sha256 cellar: :any, mojave:        "168ee156d7a6165493bb6b1b1033ebb2d58805aea3c3530975c68ba79eded7c8"
+    sha256 cellar: :any, arm64_big_sur: "b2913d5e7cd0249a9deced42f32460d53ce041cf722ef596006d52a0ad589e00"
+    sha256 cellar: :any, big_sur:       "2bc5b19eac04e1e705a59e90c797f0bdd942f5d8014e85fce5230ec802db2a76"
+    sha256 cellar: :any, catalina:      "cfbd3a15f30408288d3e7597e1e81a56940a107f5b9b6bc1274bb7b3edb87a1d"
+    sha256 cellar: :any, mojave:        "d29249c9fa2980d6ad5fea05b27e6255a8893be1cacb146606e2cab1b21dd4ed"
   end
 
   # Release 2.0.0 doesn't include an autoreconfed tarball
