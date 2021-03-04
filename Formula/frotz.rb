0fb5836b27dda3a1a5993e0e80ae30f49e598df3
Bo Anderson <mail@boanderson.me>
2021-03-04 23:29:20 +0000

    frotz 2.53
    Closes #72530.
    
    Signed-off-by: rui <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/frotz.rb
+++ Formula/frotz.rb
@@ -1,9 +1,9 @@
 class Frotz < Formula
   desc "Infocom-style interactive fiction player"
   homepage "https://661.org/proj/if/frotz/"
-  url "https://gitlab.com/DavidGriffith/frotz/-/archive/2.52/frotz-2.52.tar.bz2"
-  sha256 "7e81789d7958ef42426a3067855cb3dc8eda04a5aa80d2803e32dd9282452932"
-  license "GPL-2.0"
+  url "https://gitlab.com/DavidGriffith/frotz/-/archive/2.53/frotz-2.53.tar.bz2"
+  sha256 "8da558828dd74d6d6ee30483bb32276ef918b8b72b7f6e89b4f7cb27e7abf58b"
+  license "GPL-2.0-or-later"
   head "https://gitlab.com/DavidGriffith/frotz.git"
 
   bottle do
@@ -30,15 +30,20 @@ class Frotz < Formula
   uses_from_macos "zlib"
 
   resource("testdata") do
-    url "https://gitlab.com/DavidGriffith/frotz/-/raw/master/src/test/etude/etude.z5"
+    url "https://gitlab.com/DavidGriffith/frotz/-/raw/2.53/src/test/etude/etude.z5"
     sha256 "bfa2ef69f2f5ce3796b96f9b073676902e971aedb3ba690b8835bb1fb0daface"
   end
 
   def install
-    args = %W[PREFIX=#{prefix} MANDIR=#{man} SYSCONFDIR=#{etc}]
-    system "make", "all", *args
+    args = %W[PREFIX=#{prefix} MANDIR=#{man} SYSCONFDIR=#{etc} ITALIC=]
+    targets = %w[frotz dumb sdl]
+    targets.each do |target|
+      system "make", target, *args
+    end
     ENV.deparallelize # install has race condition
-    system "make", "install_all", *args
+    targets.each do |target|
+      system "make", "install_#{target}", *args
+    end
   end
 
   test do
