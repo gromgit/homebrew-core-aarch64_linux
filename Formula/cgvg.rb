dff38ffea4dda871ac67c74bab2e7fd475d2373c
Bo Anderson <mail@boanderson.me>
2021-03-04 05:35:36 +0000

    cgvg: update urls

--- Formula/cgvg.rb
+++ Formula/cgvg.rb
@@ -1,9 +1,9 @@
 class Cgvg < Formula
   desc "Command-line source browsing tool"
-  homepage "http://www.uzix.org/cgvg.html"
-  url "http://www.uzix.org/cgvg/cgvg-1.6.3.tar.gz"
+  homepage "https://uzix.org/cgvg.html"
+  url "https://uzix.org/cgvg/cgvg-1.6.3.tar.gz"
   sha256 "d879f541abcc988841a8d86f0c0781ded6e70498a63c9befdd52baf4649a12f3"
-  license "GPL-2.0"
+  license "GPL-2.0-or-later"
 
   bottle do
     rebuild 1
