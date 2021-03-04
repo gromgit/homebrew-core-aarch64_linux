6cf32ad6bec9bce85c14fe86a367c4fde23985ac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-04 23:23:02 +0000

    darkhttpd: update 1.13 bottle.

--- Formula/darkhttpd.rb
+++ Formula/darkhttpd.rb
@@ -6,15 +6,10 @@ class Darkhttpd < Formula
   license "ISC"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7fc5138e8e4af4039f2321403960ede2d5feba70fe6445decb14a48163f393c6"
-    sha256 cellar: :any_skip_relocation, big_sur:       "090491867be6297c68dc7f31f80f42c62aad0a7288148442d0e764424936b881"
-    sha256 cellar: :any_skip_relocation, catalina:      "9fbb24a5ba183c6e582a0a4960c86503ed6dca8b1fd37080f44c8516d0ef5117"
-    sha256 cellar: :any_skip_relocation, mojave:        "5af24e2fb0bd38aec2b8b3c0bcf685b54297bde3e1311f2d38d48f0bf10912ab"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "b0da49675fa62225da80fd711cc426d2b58116355ae8c89c80de080479b777a5"
-    sha256 cellar: :any_skip_relocation, sierra:        "1271ecbcd73b02bc8977235ea0bfcfdceb1819cb476213e74bd7d352df8a464f"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "cf8e5885072baed885238dc1a6b23466f80d96a32eb48d5f61f3b9d519df88b5"
-    sha256 cellar: :any_skip_relocation, yosemite:      "2fc16040a837b47ac947b8462f93530a387f9db0e0d6a594e4b7dba3437e6e11"
-    sha256 cellar: :any_skip_relocation, mavericks:     "0ac0b5be3f8e944981806ed255740a6feaee64cd14d78d817e8c5a75391d9837"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "36ae262543c3c5a76c282267e400ed1566cf31d1955f9a2d2298e2cb14a42cc8"
+    sha256 cellar: :any_skip_relocation, big_sur:       "1643b2894325e4ed51b6007c1f1c6a935cc0780f48f3d953620d7b0ab50d6dbc"
+    sha256 cellar: :any_skip_relocation, catalina:      "161992a2da584f5704fc6923d26fe6675a2ac23b3a66d9c1bba154b2a5888833"
+    sha256 cellar: :any_skip_relocation, mojave:        "fdc947505f7ee3885b23431afe3603cb6e75f7edeea9784dab45975b30956086"
   end
 
   def install
