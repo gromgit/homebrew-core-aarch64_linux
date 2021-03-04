2d2bae17903c17c7f8a36d887d2179c0d727ff9b
Bo Anderson <mail@boanderson.me>
2021-03-04 05:35:38 +0000

    tailor: update urls

--- Formula/tailor.rb
+++ Formula/tailor.rb
@@ -1,6 +1,6 @@
 class Tailor < Formula
   desc "Cross-platform static analyzer and linter for Swift"
-  homepage "https://tailor.sh"
+  homepage "https://sleekbyte.github.io/tailor/"
   url "https://github.com/sleekbyte/tailor/releases/download/v0.12.0/tailor-0.12.0.tar"
   sha256 "ec3810b27e9a35ecdf3a21987f17cad86918240d773172264e9abbb1a7efc415"
   license "MIT"
