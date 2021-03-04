42b747cf4070d3b8c71bc1edce5feef62f1f61d0
morusn <morusn@users.noreply.github.com>
2021-03-04 18:29:18 +0100

    jsonschema2pojo 1.1.0 (#72488)

--- Formula/jsonschema2pojo.rb
+++ Formula/jsonschema2pojo.rb
@@ -1,10 +1,9 @@
 class Jsonschema2pojo < Formula
   desc "Generates Java types from JSON Schema (or example JSON)"
   homepage "http://www.jsonschema2pojo.org/"
-  url "https://github.com/joelittlejohn/jsonschema2pojo/releases/download/jsonschema2pojo-1.0.2/jsonschema2pojo-1.0.2.tar.gz"
-  sha256 "158e927558e3ae4779dc2b7cd00e2010e43465af5a5aebdb55c889ccacb4d6e3"
+  url "https://github.com/joelittlejohn/jsonschema2pojo/releases/download/jsonschema2pojo-1.1.0/jsonschema2pojo-1.1.0.tar.gz"
+  sha256 "ebb8bf7f1676b823c2c60591c08665cb3de52827bfe17b30d2dc0c5850108b90"
   license "Apache-2.0"
-  revision 2
 
   livecheck do
     url :stable
