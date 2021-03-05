1aeb0495ba9ae9f791377db5182fa3cddc6a9b53
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-05 23:23:32 +0000

    gitleaks: update 7.3.0 bottle.

--- Formula/gitleaks.rb
+++ Formula/gitleaks.rb
@@ -6,10 +6,10 @@ class Gitleaks < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "92f4ea25d97089e136f0ed5fd5d6e696f2da6e6d90f35cae30cf0047a11daac3"
-    sha256 cellar: :any_skip_relocation, big_sur:       "12b782a67f02fdf5544fa5b4fc8ab40243848e40f2254da0f12c9e473aebce7b"
-    sha256 cellar: :any_skip_relocation, catalina:      "54f4dcacd3943a063711db03610063b21301122ef57b096a31a4147e547b0f29"
-    sha256 cellar: :any_skip_relocation, mojave:        "49f130b583e05eebbf6f980ec606f40360451b38d4f10c1cb98f1f1252d66fbc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a5bf734f0735cb099e1766cf804286fc88337153d1dc3e735b624b3f6c40a707"
+    sha256 cellar: :any_skip_relocation, big_sur:       "bb0b7dffff40b8f273021c853108071c06e2d2538282f088a4a4deeae4c9d533"
+    sha256 cellar: :any_skip_relocation, catalina:      "ab96fb1cc07d528418b9af02b8edb303501b4d034e933f81a888dede6fc71338"
+    sha256 cellar: :any_skip_relocation, mojave:        "b1d472405693f32f074fb0846eec17118f060f78cd81082b28b5b63c4bccd299"
   end
 
   depends_on "go" => :build
