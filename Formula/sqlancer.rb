9dd26c2aeb8431633ee6621b1871c81aceaeb5c0
Alexander Bayandin <a.bayandin@gmail.com>
2021-03-05 16:21:15 +0000

    sqlancer: add 1.1.0 bottle.

--- Formula/sqlancer.rb
+++ Formula/sqlancer.rb
@@ -5,6 +5,13 @@ class Sqlancer < Formula
   sha256 "ce36d338e7af3649256ff1af89d26ca59fee8e8965529c293ef5592e103953fc"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "92a80ca266d0c95b5030d5745a7eb3d42ea30059c07d84b42ed97160e080f744"
+    sha256 cellar: :any_skip_relocation, big_sur:       "29f1b9a3d118889abcaa721e5ce5ee48a0e3205a5a6e1ca4fcd1ceef3668f7dc"
+    sha256 cellar: :any_skip_relocation, catalina:      "9f37e37e733fe9afd2b70f3feadf73638fa0ac6c445172e6afcce4e51d68a530"
+    sha256 cellar: :any_skip_relocation, mojave:        "a7ebed5feab8b8f95b938c24f061dd89323f7735c04da967c5fa471390acd280"
+  end
+
   depends_on "maven" => :build
   depends_on "openjdk"
 
