d1e15c80de49710ad67a8c635330c99c1cd8e1a0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-05 22:23:56 +0000

    goreleaser: update 0.159.0 bottle.

--- Formula/goreleaser.rb
+++ Formula/goreleaser.rb
@@ -8,10 +8,10 @@ class Goreleaser < Formula
   head "https://github.com/goreleaser/goreleaser.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "266f90115bdc6529e6d292109dc9da563f244c6f3af9752b3695c7a28709b453"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5ed58795187845e740dfa987fc918f411defe02a25286e1aa2b717afe5ccd28b"
-    sha256 cellar: :any_skip_relocation, catalina:      "0a986999d72e8a2667e59e5c90a9a20624651ff850cc592684ef90b07e62663d"
-    sha256 cellar: :any_skip_relocation, mojave:        "347cefc456ef6fb34f00a67ee59f78b23ba97595740ec8c4c449bdb811b65bfe"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e64a434aa3eb4249580fe4a545f9697ffe5e5ae8f543ac98093c43f5719a707d"
+    sha256 cellar: :any_skip_relocation, big_sur:       "2add03392c41eada288eb39e4d7d9a6d4d8126d244213580593d03a1e419462b"
+    sha256 cellar: :any_skip_relocation, catalina:      "9f39bea9793a724f10a93537793d0c0154c6eb20ebc712111394143759b81310"
+    sha256 cellar: :any_skip_relocation, mojave:        "8d59f2b0b349de1700b1985b994d688e77c179b77e761f61e3825415f1360616"
   end
 
   depends_on "go" => :build
