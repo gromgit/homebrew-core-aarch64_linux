adf8929562ae226cd9a140806a46863193e4f167
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-05 05:21:40 +0000

    triton: update 7.14.0 bottle.

--- Formula/triton.rb
+++ Formula/triton.rb
@@ -8,10 +8,10 @@ class Triton < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5ec9a519d279db791a9a2ba2df1555cc1fc7ac18caec1fed6c03e8281ff05c98"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2432730a23b371f308fc65ff859f290edec33886cc4e1f434d2f31e53b101132"
-    sha256 cellar: :any_skip_relocation, catalina:      "a5dcdafdbf31e722c90ee98d49dff3fbdb3c2534b0829e6de7a3bd638dee4f49"
-    sha256 cellar: :any_skip_relocation, mojave:        "46eec03b96a0c947df6f59d13f1de651d7905f652abad6cfbb0c97f62d0c9e96"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "794b472b3bf4bc30fc1f34c52856d122fcec3fdbf19b027818acee112460bdaf"
+    sha256 cellar: :any_skip_relocation, big_sur:       "b4a13bf55599a12405fe995f6205a5c89650b37edc724d7d6b950c43bd159a09"
+    sha256 cellar: :any_skip_relocation, catalina:      "506183f304838197472fa6012308460e66365386ad38ff9c3cb748d0a9aff934"
+    sha256 cellar: :any_skip_relocation, mojave:        "08bd0a1aad4c30e8086d13db84cdc154caafed7d10122da68b5190adde747a82"
   end
 
   depends_on "node"
