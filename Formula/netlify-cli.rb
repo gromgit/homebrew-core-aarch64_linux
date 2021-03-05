89f80a4c52c36eede20e9d7f0315e12cfb01d3d9
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-05 23:54:34 +0000

    netlify-cli 3.10.6
    Closes #72600.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/netlify-cli.rb
+++ Formula/netlify-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class NetlifyCli < Formula
   desc "Netlify command-line tool"
   homepage "https://www.netlify.com/docs/cli"
-  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-3.10.5.tgz"
-  sha256 "65ef8a55c9e91a52c06126e9112ba5afa7874d4763b7fa87f7b9ae1784c0720b"
+  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-3.10.6.tgz"
+  sha256 "3e4ea80d6e0289a8accee2d886c456a793867fa05fc589541dfebaab189cfc8f"
   license "MIT"
   head "https://github.com/netlify/cli.git"
 
