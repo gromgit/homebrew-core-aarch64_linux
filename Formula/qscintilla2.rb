12facea51b97f9c64d7df14ff5a6e81f35f78cf5
PaperChalice <lgamma@163.com>
2021-03-05 11:30:40 +0800

    qscintilla2: 2.12.1

--- Formula/qscintilla2.rb
+++ Formula/qscintilla2.rb
@@ -1,10 +1,9 @@
 class Qscintilla2 < Formula
   desc "Port to Qt of the Scintilla editing component"
   homepage "https://www.riverbankcomputing.com/software/qscintilla/intro"
-  url "https://www.riverbankcomputing.com/static/Downloads/QScintilla/2.11.6/QScintilla-2.11.6.tar.gz"
-  sha256 "e7346057db47d2fb384467fafccfcb13aa0741373c5d593bc72b55b2f0dd20a7"
+  url "https://www.riverbankcomputing.com/static/Downloads/QScintilla/2.12.1/QScintilla_src-2.12.1.tar.gz"
+  sha256 "a7331c44b5d7320cbf58cb2382c38857e9e9f4fa52c405bd7776c8b6649836c2"
   license "GPL-3.0-only"
-  revision 2
 
   livecheck do
     url "https://www.riverbankcomputing.com/software/qscintilla/download"
@@ -18,28 +17,37 @@ class Qscintilla2 < Formula
     sha256 cellar: :any, mojave:        "20794671c986947c27acddf7d5770559ac1567a445b452a99af2669089493cc5"
   end
 
-  depends_on "pyqt"
+  depends_on "pyqt-builder" => :build
+
+  # TODO: use qt when octave can migrate to qt6
+  depends_on "pyqt@5"
   depends_on "python@3.9"
   depends_on "qt@5"
   depends_on "sip"
 
-  # Fix for rpath in library install name. Taken from
-  # https://github.com/macports/macports-ports/pull/7790
-  # https://www.riverbankcomputing.com/pipermail/qscintilla/2020-March/001444.html
-  patch :DATA
-
   def install
+    # TODO: when qt 6.1 is released, modify the spec
     spec = (ENV.compiler == :clang) ? "macx-clang" : "macx-g++"
     spec << "-arm64" if Hardware::CPU.arm?
     args = %W[-config release -spec #{spec}]
 
-    cd "Qt4Qt5" do
+    pyqt = Formula["pyqt@5"]
+    qt = Formula["qt@5"]
+    xy = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
+
+    cd "src" do
       inreplace "qscintilla.pro" do |s|
+        s.gsub! "QMAKE_POST_LINK += install_name_tool -id @rpath/$(TARGET1) $(TARGET)",
+          "QMAKE_POST_LINK += install_name_tool -id #{lib}/$(TARGET1) $(TARGET)"
         s.gsub! "$$[QT_INSTALL_LIBS]", lib
         s.gsub! "$$[QT_INSTALL_HEADERS]", include
+        # TODO: use qt6 directory layout when octave can migrate to qt6
         s.gsub! "$$[QT_INSTALL_TRANSLATIONS]", prefix/"trans"
         s.gsub! "$$[QT_INSTALL_DATA]", prefix/"data"
         s.gsub! "$$[QT_HOST_DATA]", prefix/"data"
+        # s.gsub! "$$[QT_INSTALL_TRANSLATIONS]", share/"qt/translations"
+        # s.gsub! "$$[QT_INSTALL_DATA]", share/"qt"
+        # s.gsub! "$$[QT_HOST_DATA]", share/"qt"
       end
 
       inreplace "features/qscintilla2.prf" do |s|
@@ -47,60 +55,42 @@ class Qscintilla2 < Formula
         s.gsub! "$$[QT_INSTALL_HEADERS]", include
       end
 
-      qt5 = Formula["qt@5"].opt_prefix
-      system "#{qt5}/bin/qmake", "qscintilla.pro", *args
+      system qt.opt_bin/"qmake", "qscintilla.pro", *args
       system "make"
       system "make", "install"
     end
 
-    # Add qscintilla2 features search path, since it is not installed in Qt keg's mkspecs/features/
-    ENV["QMAKEFEATURES"] = prefix/"data/mkspecs/features"
-
     cd "Python" do
-      (share/"sip").mkpath
-      version = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
-      pydir = "#{lib}/python#{version}/site-packages/PyQt5"
-      system Formula["python@3.9"].opt_bin/"python3", "configure.py", "-o", lib, "-n", include,
-                        "--apidir=#{prefix}/qsci",
-                        "--destdir=#{pydir}",
-                        "--stubsdir=#{pydir}",
-                        "--qsci-sipdir=#{share}/sip",
-                        "--qsci-incdir=#{include}",
-                        "--qsci-libdir=#{lib}",
-                        "--pyqt=PyQt5",
-                        "--pyqt-sipdir=#{Formula["pyqt"].opt_share}/sip/Qt5",
-                        "--sip-incdir=#{Formula["sip"].opt_include}",
-                        "--spec=#{spec}",
-                        "--no-dist-info"
-      system "make"
-      system "make", "install"
-      system "make", "clean"
+      mv "pyproject-qt#{qt.version.major}.toml", "pyproject.toml"
+      (buildpath/"Python/pyproject.toml").append_lines <<~EOS
+        [tool.sip.project]
+        sip-include-dirs = ["#{pyqt.opt_lib}/python#{xy}/site-packages/PyQt#{pyqt.version.major}/bindings"]
+      EOS
+
+      # TODO: qt6 options
+      # --qsci-features-dir #{share}/qt/mkspecs/features
+      # --api-dir #{share}/qt/qsci/api/python
+      args = %W[
+        --target-dir #{prefix}
+
+        --qsci-features-dir #{prefix}/data/mkspecs/features
+        --qsci-include-dir #{include}
+        --qsci-library-dir #{lib}
+        --api-dir #{prefix}/data/qsci/api/python
+      ]
+      system "sip-install", *args
+
+      (lib/"python#{xy}/site-packages").install %W[#{prefix}/PyQt#{pyqt.version.major} #{prefix}/QScintilla-#{version}.dist-info]
     end
   end
 
   test do
+    pyqt = Formula["pyqt@5"]
     (testpath/"test.py").write <<~EOS
-      import PyQt5.Qsci
-      assert("QsciLexer" in dir(PyQt5.Qsci))
+      import PyQt#{pyqt.version.major}.Qsci
+      assert("QsciLexer" in dir(PyQt#{pyqt.version.major}.Qsci))
     EOS
 
     system Formula["python@3.9"].opt_bin/"python3", "test.py"
   end
 end
-
-__END__
-diff --git a/Qt4Qt5/qscintilla.pro b/Qt4Qt5/qscintilla.pro
-index 35b37da..7953c1b 100644
---- a/Qt4Qt5/qscintilla.pro
-+++ b/Qt4Qt5/qscintilla.pro
-@@ -37,10 +37,6 @@ CONFIG(debug, debug|release) {
-     TARGET = qscintilla2_qt$${QT_MAJOR_VERSION}
- }
- 
--macx:!CONFIG(staticlib) {
--    QMAKE_POST_LINK += install_name_tool -id @rpath/$(TARGET1) $(TARGET)
--}
--
- INCLUDEPATH += . ../include ../lexlib ../src
- 
- !CONFIG(staticlib) {
