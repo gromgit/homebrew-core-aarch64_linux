2665b5eedf3fb57b2748315028d27e68cce9faac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-06 12:22:24 +0000

    sdcc: update 4.0.0_1 bottle.

--- Formula/sdcc.rb
+++ Formula/sdcc.rb
@@ -13,11 +13,10 @@ class Sdcc < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "e002b79aa971132f16eca044273a2048fca8c162a4231b69f7d90086315e3d76"
-    sha256 big_sur:       "5f6504340cb02b7de8cd7562e5b91533c9ff090e620f1e458a4843ebe6460ad3"
-    sha256 catalina:      "876e548b2a8c31c2d45d753b59e528c82101d193398d8c158270849fe9703ece"
-    sha256 mojave:        "214547215aa0b7598ecfd80cd291bbc64bd8b2d95c867fca9653e5d0aef042d6"
-    sha256 high_sierra:   "1f2423cb4c4f66c34b8a68f9c7a967c4256ca438646260dbf50e7f4c0b5f8f59"
+    sha256 arm64_big_sur: "384195a1fe4a6b681b54a6a5cb93d1da698ef1f7ed4ef34b1b1ba92b52b8e406"
+    sha256 big_sur:       "705bf57ac5c173a893408644a2fb69fd9c6941cb44fe3ea17b5ea44ca4afc4b3"
+    sha256 catalina:      "b99efe742281c6aa552426b782cf9c372df35e60ef01a65980d4b53071f8581e"
+    sha256 mojave:        "9c2bbf7462ef3b3fa92701e11c9a58b497d3814159aea69933d8ff674b4fa40f"
   end
 
   depends_on "autoconf" => :build
