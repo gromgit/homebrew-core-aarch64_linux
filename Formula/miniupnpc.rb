5c98cf1ddae0c9ed5b887980b33d5f5d4ed1031f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-06 10:19:27 +0000

    miniupnpc: update 2.2.2 bottle.

--- Formula/miniupnpc.rb
+++ Formula/miniupnpc.rb
@@ -13,12 +13,10 @@ class Miniupnpc < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "fcebf3eb8eb91936643c693155de21d72c697091ffa3dcc7b490dec235719dda"
-    sha256 cellar: :any, big_sur:       "9fab3b162f13e509a2f5dd1858a91374961898808f28aeb4153f6294b9f1a2b4"
-    sha256 cellar: :any, catalina:      "fd72b75df14dc0a23f566031fbddef7110acf0e90b34092ef09ef62fa74a6117"
-    sha256 cellar: :any, mojave:        "78f72e56f2edb01fb2d7949836050cca173fe9c342b602c7b9c3a8dc31693849"
-    sha256 cellar: :any, high_sierra:   "ac7dcda27fedebab8e8c47ce08c74283626a0169b646a09b34de5bd91b673e1a"
+    sha256 cellar: :any, arm64_big_sur: "bb414f85009c261710a00d438afdd3270f5c38039c02992eae612a8fa71442d0"
+    sha256 cellar: :any, big_sur:       "8493cfd956df3566d8025042e44d4cf60995ff0a7a1c1cce4d6ef388e20c0b58"
+    sha256 cellar: :any, catalina:      "bde7e04d31203a1a5b1c89d167c8cc4e7962ba4beaa351f1edffe47e03561223"
+    sha256 cellar: :any, mojave:        "f9d73d4839a56b72369a506c52676b055a584135f9ec94d6ecd9ebb1e6407f9e"
   end
 
   conflicts_with "wownero", because: "wownero ships its own copy of miniupnpc"
