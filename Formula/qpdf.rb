09239ccaa32d6efe3eba9100d921450293e1cd36
Thierry Moisan <thierry.moisan@gmail.com>
2021-03-05 20:30:17 -0500

    qpdf 10.3.0
    Closes #72604.
    
    Signed-off-by: Dustin Rodrigues <dust.rod@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/qpdf.rb
+++ Formula/qpdf.rb
@@ -1,8 +1,8 @@
 class Qpdf < Formula
   desc "Tools for and transforming and inspecting PDF files"
   homepage "https://github.com/qpdf/qpdf"
-  url "https://github.com/qpdf/qpdf/releases/download/release-qpdf-10.2.0/qpdf-10.2.0.tar.gz"
-  sha256 "43ef260f4e70672660e1882856d59b9319301c6f170673ab465430a71cffe44c"
+  url "https://github.com/qpdf/qpdf/releases/download/release-qpdf-10.3.0/qpdf-10.3.0.tar.gz"
+  sha256 "4649c66ddb3a4fc4b18927667fd3ae20a9edfbbdc1017ee1ed1e548d3abe00d1"
   license "Apache-2.0"
 
   bottle do
