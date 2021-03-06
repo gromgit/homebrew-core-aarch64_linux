a6414982fb7c777da4871f96f029a56993dbc37c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-06 22:18:07 +0000

    fsql: update 0.4.0 bottle.

--- Formula/fsql.rb
+++ Formula/fsql.rb
@@ -6,13 +6,10 @@ class Fsql < Formula
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9266f63cddbc0ca9c73655ecee949deae745e991ca31727a954958c81cfe4615"
-    sha256 cellar: :any_skip_relocation, big_sur:       "fec9ae6f2720a408b0bf11aa6f83d7de2bbd99eb8054a8ede09809b0ca470cfb"
-    sha256 cellar: :any_skip_relocation, catalina:      "de9764e9754827795839738de7d4377786b3532b7a2157806ecf3e145f626146"
-    sha256 cellar: :any_skip_relocation, mojave:        "7cb63d8939e7af0391938aea8a138daccbaddce50b42802d32e510772e004b9a"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "7b4353a346425e4db5d14419c4dbacf6038606778a7ce2b98ddd0fdb7c2ca233"
-    sha256 cellar: :any_skip_relocation, sierra:        "f651c7c2dad44ee6b6f32aa699df223bd427421990f2c2c170d0928b1a31ef87"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "77ec663d81151aebf469db5626893d130ff21114f603227046999a27ea843f8f"
+    sha256 cellar: :any_skip_relocation, big_sur:       "53d773ca5cba807627e4e3b7cdb36990c18c3b1944c847756c1acf95d99d6af6"
+    sha256 cellar: :any_skip_relocation, catalina:      "c97d021f2047654b1141141bfdaceba40953a39cc150233cb9ec3bb85ee9b675"
+    sha256 cellar: :any_skip_relocation, mojave:        "5d984747c498019e9950c57084e2549417dce8206ab489650fcba917ad7a30af"
   end
 
   depends_on "go" => :build
