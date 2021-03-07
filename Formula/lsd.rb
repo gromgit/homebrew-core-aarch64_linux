cd61412357bd2aee1281751fd6f16b04ebc03948
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-07 10:24:39 +0000

    lsd: update 0.20.1 bottle.

--- Formula/lsd.rb
+++ Formula/lsd.rb
@@ -6,10 +6,10 @@ class Lsd < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "01f7b9b3d821ccfdb495c2391b0f668f89b9e13df20ceb578f246761905a2361"
-    sha256 cellar: :any_skip_relocation, big_sur:       "4de553270f78d0760dffb2ae706df46baf1104379dd78c9fa88046bf024cdaaa"
-    sha256 cellar: :any_skip_relocation, catalina:      "351d9ceef129e8b2c27f4f1c6c6edb60cecbb5db1785f350141494d67de4a1b9"
-    sha256 cellar: :any_skip_relocation, mojave:        "b58b1b7fc1d76f81b5d089cdd8b8cdfbe876e4011231a4fe85107d935ae4ded6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6837d97dc37d9ffd1e0c84e974db7598f47db4a24099b8718019c7090b6c515a"
+    sha256 cellar: :any_skip_relocation, big_sur:       "0c9103225c2dd6c8faeb893947100bfb6395a0f6ce008f8968a2598fe99f3d46"
+    sha256 cellar: :any_skip_relocation, catalina:      "7710d954067c99c7cace2bb0c58c1b7a61d88d5df1e997b95a050265ba13e4ba"
+    sha256 cellar: :any_skip_relocation, mojave:        "bb72efd16f431bf09fa76661ed68b5919e6326f83da79d1f6e9d5e63fef90ab0"
   end
 
   depends_on "rust" => :build
