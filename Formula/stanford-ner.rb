cee3270c1de2c660a76e4bf722966c41a623fe95
Matt Moore <matt@mattmoore.io>
2021-03-07 14:44:16 -0600

    stanford-ner 4.2.0 (#72672)

--- Formula/stanford-ner.rb
+++ Formula/stanford-ner.rb
@@ -1,10 +1,9 @@
 class StanfordNer < Formula
   desc "Stanford NLP Group's implementation of a Named Entity Recognizer"
   homepage "https://nlp.stanford.edu/software/CRF-NER.shtml"
-  url "https://nlp.stanford.edu/software/stanford-ner-2018-10-16.zip"
-  version "3.9.2"
-  sha256 "fa737bc6d7ac01de6d13a4628b229b7eabae24c446bc616608b7aad04ed6c65a"
-  revision 1
+  url "https://nlp.stanford.edu/software/stanford-ner-4.2.0.zip"
+  sha256 "06dd9f827106359bad90049c6952137502bc59ed40b9c88b448831b32cf55b2a"
+  license "GPL-2.0-or-later"
 
   bottle :unneeded
 
