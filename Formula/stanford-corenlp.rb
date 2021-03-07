5e7a5fe636303ac485bad38f98bff59c50a1c047
Matt Moore <matt@mattmoore.io>
2021-03-07 07:55:19 -0600

    stanford-corenlp 4.2.0 (#72577)
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/stanford-corenlp.rb
+++ Formula/stanford-corenlp.rb
@@ -1,10 +1,8 @@
 class StanfordCorenlp < Formula
   desc "Java suite of core NLP tools"
   homepage "https://stanfordnlp.github.io/CoreNLP/"
-  url "https://nlp.stanford.edu/software/stanford-corenlp-full-2018-10-05.zip"
-  version "3.9.2"
-  sha256 "833f0f5413a33e7fbc98aeddcb80eb0a55b672f67417b8d956ed9c39abe8d26c"
-  revision 1
+  url "https://nlp.stanford.edu/software/stanford-corenlp-4.2.0.zip"
+  sha256 "984aff1c431311a7dd5b4f0cb3bec4f5ba81da37aecc0d020b270571aafc5385"
 
   bottle :unneeded
 
@@ -18,7 +16,7 @@ class StanfordCorenlp < Formula
 
   test do
     (testpath/"test.txt").write("Stanford is a university, founded in 1891.")
-    system "#{bin}/corenlp.sh", "-annotators tokenize,ssplit,pos", "-file test.txt"
-    assert_predicate (testpath/"test.txt.xml"), :exist?
+    system "#{bin}/corenlp.sh", "-annotators tokenize,ssplit,pos", "-file test.txt", "-outputFormat json"
+    assert_predicate (testpath/"test.txt.json"), :exist?
   end
 end
