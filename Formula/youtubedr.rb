83b91a3a5001dd5d6d03611a52c8e5ece21b34a3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-07 16:43:58 +0000

    youtubedr: use built-in Time class

--- Formula/youtubedr.rb
+++ Formula/youtubedr.rb
@@ -15,7 +15,7 @@ class Youtubedr < Formula
   depends_on "go" => :build
 
   def install
-    build_time = Utils.safe_popen_read("date -u +'%Y-%m-%dT%H:%M:%SZ' 2> /dev/null").chomp
+    build_time = Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")
 
     ldflags = %W[
       -s -w
