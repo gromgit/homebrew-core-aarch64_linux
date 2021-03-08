c50162627d1fb3fd85cf2ff3c271e337af541793
morusn <morusn@users.noreply.github.com>
2021-03-09 00:29:13 +0100

    iso-codes 4.6.0
    * iso-codes 4.6.0
    * Update Formula/iso-codes.rb

--- Formula/iso-codes.rb
+++ Formula/iso-codes.rb
@@ -1,9 +1,9 @@
 class IsoCodes < Formula
   desc "Provides lists of various ISO standards"
   homepage "https://salsa.debian.org/iso-codes-team/iso-codes"
-  url "https://deb.debian.org/debian/pool/main/i/iso-codes/iso-codes_4.5.0.orig.tar.xz"
-  sha256 "2a63118f8c91faa2102e6381ae498e7fa83b2bdf667963e0f7dbae2a23b827dd"
-  license "LGPL-2.1"
+  url "https://deb.debian.org/debian/pool/main/i/iso-codes/iso-codes_4.6.0.orig.tar.xz"
+  sha256 "41c672c18554e979e6191f950f454cdf1bfb67a6369fffe2997ff68e34845409"
+  license "LGPL-2.1-or-later"
   head "https://salsa.debian.org/iso-codes-team/iso-codes.git"
 
   bottle do
