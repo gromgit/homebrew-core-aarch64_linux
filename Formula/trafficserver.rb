e3f5fbf86388d916ba9ae345bef42c016a307eb0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-08 21:33:10 +0000

    trafficserver 9.0.1
    Depend on yaml-cpp. Otherwise, it builds its own.
    
    Closes #72735.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/trafficserver.rb
+++ Formula/trafficserver.rb
@@ -1,9 +1,9 @@
 class Trafficserver < Formula
   desc "HTTP/1.1 compliant caching proxy server"
   homepage "https://trafficserver.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=trafficserver/trafficserver-8.1.0.tar.bz2"
-  mirror "https://archive.apache.org/dist/trafficserver/trafficserver-8.1.0.tar.bz2"
-  sha256 "01bcc5d5cc58d5368366e193b6091e2d6af000badc19be3c49db7aa96955bbe2"
+  url "https://downloads.apache.org/trafficserver/trafficserver-9.0.1.tar.bz2"
+  mirror "https://archive.apache.org/dist/trafficserver/trafficserver-9.0.1.tar.bz2"
+  sha256 "bf00d0399e359f2b52bb0664743e5ccd81c6a9491eafe1ff1b10afeaf62526e0"
   license "Apache-2.0"
 
   bottle do
@@ -24,13 +24,14 @@ class Trafficserver < Formula
     end
   end
 
+  depends_on "pkg-config" => :build
+  depends_on "hwloc"
   depends_on macos: :mojave # `error: call to unavailable member function 'value': introduced in macOS 10.14`
   depends_on "openssl@1.1"
   depends_on "pcre"
+  depends_on "yaml-cpp"
 
   def install
-    ENV.cxx11 if build.stable?
-
     # Per https://luajit.org/install.html: If MACOSX_DEPLOYMENT_TARGET
     # is not set then it's forced to 10.4, which breaks compile on Mojave.
     ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version
@@ -41,7 +42,7 @@ class Trafficserver < Formula
       --localstatedir=#{var}
       --sysconfdir=#{etc}/trafficserver
       --with-openssl=#{Formula["openssl@1.1"].opt_prefix}
-      --with-tcl=#{MacOS.sdk_path}/System/Library/Frameworks/Tcl.framework
+      --with-yaml-cpp=#{Formula["yaml-cpp"].opt_prefix}
       --with-group=admin
       --disable-silent-rules
       --enable-experimental-plugins
