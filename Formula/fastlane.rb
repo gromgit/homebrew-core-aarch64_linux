7ec2d116a180efd718f0db47e087cf0fe5aa2302
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-08 20:26:58 +0000

    fastlane: update 2.177.0 bottle.

--- Formula/fastlane.rb
+++ Formula/fastlane.rb
@@ -12,10 +12,10 @@ class Fastlane < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "71c3d76952314627a7d08bc2239314d3d1649b3079d8858b2da916402bda2a9a"
-    sha256 cellar: :any, big_sur:       "ca135fe59180785254545addf34ca887961c16664c5a5b735a87f525d22fa7ed"
-    sha256 cellar: :any, catalina:      "0e67dd9aab27c07e06d33e5547cafa74e74d13806cd9411f16c00a6061e62455"
-    sha256 cellar: :any, mojave:        "b425cee27fc586de4df0e84cc060e9fb4eb1c3181dbd32f71ba9213a39024c82"
+    sha256 cellar: :any, arm64_big_sur: "50fa19fcdfb47169d3d692827283b048074588f2d51d1dd0f2d86153162d1243"
+    sha256 cellar: :any, big_sur:       "1d3b87229fefe94ddbd9d9860bcf2af41dced3db9e715ad8d1324f9108d6d2f0"
+    sha256 cellar: :any, catalina:      "ac5e76f63a527770964a6c11dce39fe959dc864304c08ece8d7eee3fd9d6ec54"
+    sha256 cellar: :any, mojave:        "1392efa64011706ca7010190139ddf8c9407ddba6180b74272416ee84499d847"
   end
 
   depends_on "ruby@2.7"
