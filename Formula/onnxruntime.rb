89a36ca9460b3d322a30217bcbde20926df68ae4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-08 08:26:07 +0000

    onnxruntime: update 1.7.1 bottle.

--- Formula/onnxruntime.rb
+++ Formula/onnxruntime.rb
@@ -12,10 +12,10 @@ class Onnxruntime < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "66a6e15cfb53c88797492f936e07dacbb5236a4ced576d05b4e6b80b784dadaa"
-    sha256 cellar: :any, big_sur:       "156c92b0dbe1a9bd525f52f953930a7a38aeaf43a7bb8c68f0f6463922918586"
-    sha256 cellar: :any, catalina:      "fea226f84a61d74cfc5787056cbac1a2371450ba8ddff73e81d8b7a15d50a563"
-    sha256 cellar: :any, mojave:        "6b8d1a27a5303e58e6a81bd5efd997725784676d4fbc773fa8a764a3d23564a9"
+    sha256 cellar: :any, arm64_big_sur: "e2844199bfcfcae12e9d15da1559d87bf0d6945dd302e23e16b646aef451f869"
+    sha256 cellar: :any, big_sur:       "75f2c88ccb0c65bcdf6d5441e53c05d389a266ed3f8894bf2b6f4dcdad3bf8dc"
+    sha256 cellar: :any, catalina:      "2eb0fe993ca4fbf621f51ecdb7e0d502fe369de12a1f9863c3eef3b950c9814d"
+    sha256 cellar: :any, mojave:        "7ef9a5fcf95576b1d62b08ed09ec4d15d567ea3a5799333c36889fcc5e29194a"
   end
 
   depends_on "cmake" => :build
