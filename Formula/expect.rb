ba58e5190393b7b5a98354aeb6d36f70f2826f26
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-08 16:27:11 +0000

    expect: use brewed tcl-tk
    From license.terms:
    
        Design and implementation of this program was paid for by U.S. tax
        dollars.  Therefore it is public domain.  However, the author and NIST
        would appreciate credit if this program or parts of it are used.
    
    See also #72714, #72450, #67838.
    
    Closes #72717.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/expect.rb
+++ Formula/expect.rb
@@ -3,6 +3,8 @@ class Expect < Formula
   homepage "https://expect.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/expect/Expect/5.45.4/expect5.45.4.tar.gz"
   sha256 "49a7da83b0bdd9f46d04a04deec19c7767bb9a323e40c4781f89caf760b92c34"
+  license :public_domain
+  revision 1
 
   livecheck do
     url :stable
@@ -21,7 +23,7 @@ class Expect < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  uses_from_macos "tcl-tk"
+  depends_on "tcl-tk"
 
   def install
     args = %W[
@@ -30,16 +32,20 @@ class Expect < Formula
       --mandir=#{man}
       --enable-shared
       --enable-64bit
-      --with-tcl=#{MacOS.sdk_path}/System/Library/Frameworks/Tcl.framework
+      --with-tcl=#{Formula["tcl-tk"].opt_lib}
     ]
-
-    ENV.prepend "CFLAGS",
-      "-I#{MacOS.sdk_path}/System/Library/Frameworks/Tcl.framework/Versions/8.5/Headers/tcl-private"
 
     # Temporarily workaround build issues with building 5.45.4 using Xcode 12.
     # Upstream bug (with more complicated fix) is here:
     #   https://core.tcl-lang.org/expect/tktview/0d5b33c00e5b4bbedb835498b0360d7115e832a0
     ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
+
+    # Workaround for ancient config files not recognising aarch64 macos.
+    am = Formula["automake"]
+    am_share = am.opt_share/"automake-#{am.version.major_minor}"
+    %w[config.guess config.sub].each do |fn|
+      cp am_share/fn, "tclconfig/#{fn}"
+    end
 
     # Regenerate configure script. Remove after patch applied in newer
     # releases.
