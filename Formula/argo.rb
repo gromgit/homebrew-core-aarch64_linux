6785be622edf85179f982dc8874decc5e61c18e2
rui <rui@chenrui.dev>
2021-03-08 22:27:44 +0000

    argo 2.12.10
    Closes #72737.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/argo.rb
+++ Formula/argo.rb
@@ -2,8 +2,8 @@ class Argo < Formula
   desc "Get stuff done with container-native workflows for Kubernetes"
   homepage "https://argoproj.io"
   url "https://github.com/argoproj/argo.git",
-      tag:      "v2.12.9",
-      revision: "737905345d70ba1ebd566ce1230e4f971993dfd0"
+      tag:      "v2.12.10",
+      revision: "f1e0c6174b48af69d6e8ecd235a2d709f44f8095"
   license "Apache-2.0"
 
   bottle do
