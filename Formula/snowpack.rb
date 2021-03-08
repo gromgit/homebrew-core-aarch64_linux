114456d6f479824eef24d2dd81f193237443edc8
Dawid Dziurla <dawidd0811@gmail.com>
2021-03-08 13:49:11 +0000

    snowpack: add 3.0.13 bottle.

--- Formula/snowpack.rb
+++ Formula/snowpack.rb
@@ -7,6 +7,13 @@ class Snowpack < Formula
   sha256 "35cf6805e4253f22a79a1496f529f88c39a441d798b0e691c53cd94e1413516f"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4148264a15951547102b58c903a2f34709b79643d4d4991789aac3d9d8d65219"
+    sha256 cellar: :any_skip_relocation, big_sur:       "b99f05b87d6539b8312e7cf983e2fc7027f5372649b91907c187b63d7deaa323"
+    sha256 cellar: :any_skip_relocation, catalina:      "e45083a22c9a458ae356f158199c73ad5a7210f10d5a6b4d0e369f6d2d2d2b3a"
+    sha256 cellar: :any_skip_relocation, mojave:        "8bec25b62e92fc68ba75c975df570fc6fcaff05684c2a848c6252487bd51d9be"
+  end
+
   depends_on "node"
 
   def install
