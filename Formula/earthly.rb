c28b9f7a572f865b0d43e3e5c632597b9dc0c447
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-09 07:17:14 +0000

    earthly: update 0.5.5 bottle.

--- Formula/earthly.rb
+++ Formula/earthly.rb
@@ -12,10 +12,10 @@ class Earthly < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "96c082fd77293140b50a602a93bf5f40f59b262c532973334baa4ada8fcc4c78"
-    sha256 cellar: :any_skip_relocation, big_sur:       "c21141390522a59074123624c5564aaef8caa1e1e9d507846a1d78d35eed9aa0"
-    sha256 cellar: :any_skip_relocation, catalina:      "e416d54d0d327d8143d071852cf9454d7de8e015b2a117ef8fc7b947876c2faf"
-    sha256 cellar: :any_skip_relocation, mojave:        "07e94a1265811798006285f81351691c2df32dcc9cb7a55168018e9b157d1e20"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e5e6d8fcf6f9479146486325c409e18bdde3f9386fae3ddb79b6a4d98e10cc27"
+    sha256 cellar: :any_skip_relocation, big_sur:       "f35410b23b4b7c3b88126342ade4753966c050ae05b51d30ab899fec5d7ed591"
+    sha256 cellar: :any_skip_relocation, catalina:      "fa5deb9c4bc3bc3b208ef2218f9d7b378bd9de8090e395b3f6809011db340d18"
+    sha256 cellar: :any_skip_relocation, mojave:        "5d01d5883cc6df40c47724eaada70e52889455dfd6e00fbd373fe2fc2338dee3"
   end
 
   depends_on "go" => :build
