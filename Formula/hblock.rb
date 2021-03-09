1544ff43007fc5eaad38dcc0ba36f9283f2095aa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-09 00:42:25 +0000

    hblock: update 3.2.1 bottle.

--- Formula/hblock.rb
+++ Formula/hblock.rb
@@ -6,10 +6,10 @@ class Hblock < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "07cf3cb10349f9afbd7adb36ae2f4c7e3444ec9fc4c6bcfb5cd65c77488eaabd"
-    sha256 cellar: :any_skip_relocation, big_sur:       "8752ac1c6b5d9a16197c952cab95d0ed0f7259db9f98738471b679434ea533e6"
-    sha256 cellar: :any_skip_relocation, catalina:      "4599d523e51403bc471db40003a8c340d261acda6ed2b5eb26f92d5800aeec68"
-    sha256 cellar: :any_skip_relocation, mojave:        "1a04f7ac7b843758d8fd74b9885f6bd608c28fffd2c127904515922720c6b360"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "77960cc7206e2279204fd92e30b466aee5d5efb275726a882951a6209bfaf019"
+    sha256 cellar: :any_skip_relocation, big_sur:       "8c125bbc495e7429b58a9dd52c3f2c03b64f46d4f4193f7bc63c4ab60059a22b"
+    sha256 cellar: :any_skip_relocation, catalina:      "463fcbc3b50a8ca76dffcf3d706637b8a613ec121421038fba5a249dc90c39bb"
+    sha256 cellar: :any_skip_relocation, mojave:        "1851ab7697763b69a1dc9af241fa29877c42940634ee881b99feb0bea10ab297"
   end
 
   uses_from_macos "curl"
