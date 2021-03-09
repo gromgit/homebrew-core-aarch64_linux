d2660c15308c9c4fc7ef773b79eb4f75a36c37fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-09 12:28:13 +0000

    testdisk: update 7.1 bottle.

--- Formula/testdisk.rb
+++ Formula/testdisk.rb
@@ -11,12 +11,11 @@ class Testdisk < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b4c61e6cdb6119dacda42b08e912bdaa5a08dc380e40c6d80de5c1a1b7e60d48"
-    sha256 cellar: :any_skip_relocation, big_sur:       "fa46e925f4438d7f9cb855efd07499daa5622b331a8b16a22684633650d7315d"
-    sha256 cellar: :any_skip_relocation, catalina:      "66c4088c77794a244fd5b38fa39216eb8d6a09b9e4efd5e68a249e9b5df65606"
-    sha256 cellar: :any_skip_relocation, mojave:        "1e77fbc276d986fcf378901b2ba0d5957f17b569e512980017ecd09926505a4a"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "8cd43adea2ddf632e5c9305609cf377b47fcf5836805075d06dd3ccd2142ccc6"
-    sha256 cellar: :any_skip_relocation, sierra:        "752a686f8fa7717cbbdef064eefd80503eccdddfc587bd48fd24256e23332470"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "02338490d4e16fa0a61e422ec36ce72e97ad1d24406cda45f4d61396ba4cab36"
+    sha256 cellar: :any_skip_relocation, big_sur:       "325a572e2238e551d8415f58a463d80619850a9026b614d0c23da46838f2e9ea"
+    sha256 cellar: :any_skip_relocation, catalina:      "b0035f42c03dbbe94000ae373b1a8c5f9bbb6f9534ea3d64b5754475ee8fbc7b"
+    sha256 cellar: :any_skip_relocation, mojave:        "7431beee8948638cadaf5b7f439e32f798955caf403fdcfda5c9948afa5af3cc"
   end
 
   uses_from_macos "ncurses"
