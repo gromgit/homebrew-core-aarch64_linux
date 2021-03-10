21a80f394ad73f9a745f83bbccdf0b933c2a1604
rui <rui@chenrui.dev>
2021-03-10 00:36:50 +0000

    awscli 2.1.30
    Closes #72844.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -3,8 +3,8 @@ class Awscli < Formula
 
   desc "Official Amazon AWS command-line interface"
   homepage "https://aws.amazon.com/cli/"
-  url "https://github.com/aws/aws-cli/archive/2.1.29.tar.gz"
-  sha256 "0e37c4d0eb34c840cd21e317a2f702898f8cce156108c219a60e89d841258d3b"
+  url "https://github.com/aws/aws-cli/archive/2.1.30.tar.gz"
+  sha256 "0902b0399684174bd9e865f14895314d0b4ab535ad24b3079400ff85717b14a9"
   license "Apache-2.0"
   head "https://github.com/aws/aws-cli.git", branch: "v2"
 
