3672b6587912df34e9c8510959869da5a6390160
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-10 19:19:10 +0000

    helmsman: update 3.6.5 bottle.

--- Formula/helmsman.rb
+++ Formula/helmsman.rb
@@ -7,9 +7,9 @@ class Helmsman < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "dac90517617b5d9f6d9b80b783f5e089a96e8cd89254c994b5615b57874fa69f"
-    sha256 cellar: :any_skip_relocation, catalina: "b75b6387c3b3f853252aee65583262d5c5a63b36a6a5d5e0ee5eb51b11c204f0"
-    sha256 cellar: :any_skip_relocation, mojave:   "afd929bf50f142dddc7af348d611848cdb2f9be24f437bc9981b0e2ca229e6ab"
+    sha256 cellar: :any_skip_relocation, big_sur:  "e16d023ce23134a3f963207c48758491b8b3d0b69d793d491ca62868cc7b98f1"
+    sha256 cellar: :any_skip_relocation, catalina: "5c8e12d137925f1fabd2cc63033d28fb203a83bc22a7583549c4f09c061b5bbc"
+    sha256 cellar: :any_skip_relocation, mojave:   "ae9e192a7627f0f17bf4e432eee2c262960cdf0d1c66e69c6ceaa7b4498c2438"
   end
 
   depends_on "go" => :build
