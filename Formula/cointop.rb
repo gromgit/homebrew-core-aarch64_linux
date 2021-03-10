47c3c2426aa62957ff8c696184b6e6113ef4a633
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-10 10:20:29 +0000

    cointop: update 1.6.3 bottle.

--- Formula/cointop.rb
+++ Formula/cointop.rb
@@ -6,10 +6,10 @@ class Cointop < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ff3431d71ca12ce0ec94c592910e69548ab27636604637197c06cffb08e27ce2"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2093dfb9e5670f8f66f38b7f577d38787f6fb690e350eb30210f2ecceb3ee5c4"
-    sha256 cellar: :any_skip_relocation, catalina:      "599bf9e1d156b4a4ad948fe3f1565810be778465918b1eafd74153b788ae05e9"
-    sha256 cellar: :any_skip_relocation, mojave:        "8a924865d9b5dc65af1c75ad4cc1e4c7cc87f07083a7259cec724f1793b24541"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c7f83ffd254a7c243bb492e01a84e74acf7c28f1e79c699c44dc66f3a79e90b7"
+    sha256 cellar: :any_skip_relocation, big_sur:       "879b18f3d7a683748009462c983c6c683d53a0c940a8a50c59c5f9edfb205019"
+    sha256 cellar: :any_skip_relocation, catalina:      "99a20daae576755c17c9c8efcbdfb8d7f4eb4a2d0df4248c7b7891d1e7f70202"
+    sha256 cellar: :any_skip_relocation, mojave:        "cf4455df03b81390c281e664b20b662efe203446656b10060e48501ead2545a6"
   end
 
   depends_on "go" => :build
