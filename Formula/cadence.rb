4d3f6877082d4264e51a904526b2a7b12e57d478
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-10 00:42:54 +0000

    cadence: update 0.13.6 bottle.

--- Formula/cadence.rb
+++ Formula/cadence.rb
@@ -12,10 +12,10 @@ class Cadence < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5d06c7764c00faf563a7f02e1e4f90a0c1a5eef66c71c3b68d69f6535ef629ad"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1fb34aca981a872d53fdde2098d7105c02231edef77c7230701b09d8f1b48c9f"
-    sha256 cellar: :any_skip_relocation, catalina:      "40e889fb4b6ceb6501e9ed6c50d2a7b78a89a39bdc54fe0fae071cb39788775c"
-    sha256 cellar: :any_skip_relocation, mojave:        "4a4d666461b77e9141f84e134dea26966464c20732eed50b903bf468c63825d2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6a6ad7b27304145b5029b5473621e073ccf962d388fd4a2685219026a6f9f385"
+    sha256 cellar: :any_skip_relocation, big_sur:       "b3dbda524a438ff5db8bf40c585c05d8ff29a43df6c36e696cc01b2e966ce9c8"
+    sha256 cellar: :any_skip_relocation, catalina:      "0cd3c4071b3e287cd03226d66da03f93e020800ce07f125c14257108c75226dc"
+    sha256 cellar: :any_skip_relocation, mojave:        "cb034c8a34c2d63bf699e906fb82d0d5dae4314723d3d99444200a8a32f9fb5f"
   end
 
   depends_on "go" => :build
