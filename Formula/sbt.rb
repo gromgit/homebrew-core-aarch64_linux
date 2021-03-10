8b13e3d11946b5599b2f9221f11a5c19ed270701
Andrii <aabrasha@gmail.com>
2021-03-10 12:15:09 +0200

    sbt 1.4.9 (#72877)

--- Formula/sbt.rb
+++ Formula/sbt.rb
@@ -1,9 +1,9 @@
 class Sbt < Formula
   desc "Build tool for Scala projects"
   homepage "https://www.scala-sbt.org/"
-  url "https://github.com/sbt/sbt/releases/download/v1.4.8/sbt-1.4.8.tgz"
-  mirror "https://sbt-downloads.cdnedge.bluemix.net/releases/v1.4.8/sbt-1.4.8.tgz"
-  sha256 "59722dbda3d6d1d7ec7dc3e21d61a2e80023030a4243823eeeadd77ed9c5a39d"
+  url "https://github.com/sbt/sbt/releases/download/v1.4.9/sbt-1.4.9.tgz"
+  mirror "https://sbt-downloads.cdnedge.bluemix.net/releases/v1.4.9/sbt-1.4.9.tgz"
+  sha256 "95468119f7641499367330a60a4b8a6211e6ea7f8bde7d647c67b19dd8fddb6e"
   license "Apache-2.0"
 
   livecheck do
