3db8b411afffe441ff92f755879e9e1da7f74c7b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-11 00:42:54 +0000

    libvoikko: update 4.3.1 bottle.

--- Formula/libvoikko.rb
+++ Formula/libvoikko.rb
@@ -11,11 +11,10 @@ class Libvoikko < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "32d024915b3a784c2142507c1d8c0b2ef675635ef9e387880b467a2c8aa7044b"
-    sha256 cellar: :any, big_sur:       "0d36dae546a309c83750f42e8eba3b6d9b51765f78093a9b6bffa5c9d5bc0cd8"
-    sha256 cellar: :any, catalina:      "94d12634dd73cba44a8e8c0fad1f2a2bea4db5ef50bffb277faaa400f798ccb0"
-    sha256 cellar: :any, mojave:        "bc325b6ce79a331c7046c54449174ca7cd2640703e90f50244b5ddfe5ed8de3b"
-    sha256 cellar: :any, high_sierra:   "5171bd4eeaba0f6712fd945622f76c0d615f3e6fad826329fa2ce77d8711380b"
+    sha256 cellar: :any, arm64_big_sur: "b0a624c9b02009d2eac3b6adbf6db56c05d6cde859ea5cef7b6a06973afd8619"
+    sha256 cellar: :any, big_sur:       "02041f6b02bbdf49d1399b6c8b0f99e00a003a9f03bf13b57fe449759f98e27e"
+    sha256 cellar: :any, catalina:      "ffc0a9565f9806e59b80b69523230d550a9c3cdfacf6d892a13a7c3b11ac428e"
+    sha256 cellar: :any, mojave:        "79b5cb80a3e95beb1d57485d549724334d687dfdbc60520d3b437f5646ae756d"
   end
 
   depends_on "foma" => :build
