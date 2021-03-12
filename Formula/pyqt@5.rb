38eb9816fa7a4e1f8ab458c38da1fcf610e3e1c1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-12 20:21:37 +0000

    pyqt@5: add 5.15.4 bottle.

--- Formula/pyqt@5.rb
+++ Formula/pyqt@5.rb
@@ -5,6 +5,13 @@ class PyqtAT5 < Formula
   sha256 "2a69597e0dd11caabe75fae133feca66387819fc9bc050f547e5551bce97e5be"
   license "GPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any, arm64_big_sur: "213f50aaf7e3d11d41799431b5303791114a158a0db6db0d6ea61e0410c6af16"
+    sha256 cellar: :any, big_sur:       "fb6b6568ef31f8acf69288c2d2def7e919e2ea052b83abf2e35a2c0f439b0020"
+    sha256 cellar: :any, catalina:      "b6be23fb0704f405dc3e09a3711629a145081e0adaec99c50f78c6e55971e8e2"
+    sha256 cellar: :any, mojave:        "b86645249f0bf56ea3fcb9ae5738d32ad49d0974c2a78cd77ec0f7f99e6424b5"
+  end
+
   depends_on "pyqt-builder" => :build
   depends_on "python@3.9"
   depends_on "qt@5"
