1674f9968aa87593ea3aa51dde3147d88adf6e3b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-12 20:21:35 +0000

    pyqt-networkauth: add 6.0.2 bottle.

--- Formula/pyqt-networkauth.rb
+++ Formula/pyqt-networkauth.rb
@@ -5,6 +5,13 @@ class PyqtNetworkauth < Formula
   sha256 "ba3c55dc7ce7f4b9034f8675c5fe8d37830d986716ff2ef0cca29ee80240f609"
   license "GPL-3.0-only"
 
+  bottle do
+    sha256 arm64_big_sur: "e030e90dcfebb133cf2b63e68c2e5d3ac5e02a6bf92556f0635a75ed58743875"
+    sha256 big_sur:       "999d21309897cb785a202c58cc9d5a887265526218bdd996d214632b731b5706"
+    sha256 catalina:      "030504e723d97a3dd04a919a6b540c73f44fec280718518080a1efda222c0bb9"
+    sha256 mojave:        "95dfccb61e147d87c38cfe810ec56603ad2e105275c3d5d4eb181afef98c9a8c"
+  end
+
   depends_on "pyqt-builder" => :build
 
   depends_on "pyqt"
