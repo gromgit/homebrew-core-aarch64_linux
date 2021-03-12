e76b2711664d484f32a0a9c6022650a1d64f03ff
rui <rui@chenrui.dev>
2021-03-12 14:28:05 +0000

    inframap 0.6.1
    Closes #73046.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/inframap.rb
+++ Formula/inframap.rb
@@ -1,8 +1,8 @@
 class Inframap < Formula
   desc "Read your tfstate or HCL to generate a graph"
   homepage "https://github.com/cycloidio/inframap"
-  url "https://github.com/cycloidio/inframap/archive/v0.6.0.tar.gz"
-  sha256 "a58f7b899053c296ff15d1fadbee201c5bd141c51e97994206736e45955b356a"
+  url "https://github.com/cycloidio/inframap/archive/v0.6.1.tar.gz"
+  sha256 "4424f400b4cd9b18bd499e865640ef6626e1cf89c7465af2d6626551cb2bf192"
   license "MIT"
   head "https://github.com/cycloidio/inframap.git"
 
