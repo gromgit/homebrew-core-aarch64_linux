5b9428927f64a4aeff46bedfe1d229d411de7c1d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-12 20:21:34 +0000

    trimage: update 1.0.6_2 bottle.

--- Formula/trimage.rb
+++ Formula/trimage.rb
@@ -7,11 +7,10 @@ class Trimage < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c276cb15466c11675e091c6a0f86cd214c223e508ce05bcbe9ea673a81655a1e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "c11c650219774302d6337e0764c405f533999ef136f266c4ddccd9f163848df0"
-    sha256 cellar: :any_skip_relocation, catalina:      "26b150cd030648ed8ca36448b80299a51c90d5487b7a9ad4d449b8540428e9a7"
-    sha256 cellar: :any_skip_relocation, mojave:        "1e1d69b2eb37119b8e9078476443a4944ae84eee458d7346a9ad1a8cbd3a9e75"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "0e6f831f6c28348eb20f29e1b12bf9b6dcdbf665bf896f377c4d840aae967715"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1a6dca1721ee66b96bcc5f010fb96657024b17059eefc82db0e61c6e2ff12a21"
+    sha256 cellar: :any_skip_relocation, big_sur:       "eb3768183ebb466b03e1134b498a0331e43f9aa19ca2f5fb3550f14bf28c1998"
+    sha256 cellar: :any_skip_relocation, catalina:      "2174157bed654961ae8f5b1b60653c40a6a336aa4f26292ebe842d02652c62c5"
+    sha256 cellar: :any_skip_relocation, mojave:        "638e2b76d476287fad690b99c20759798e1eab19bf3beadfad35c8f177bd59c8"
   end
 
   depends_on "advancecomp"
