979ef82dfe6390ce75425f907437dc85b25a1d71
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-12 02:33:44 +0000

    k6: update 0.31.0 bottle.

--- Formula/k6.rb
+++ Formula/k6.rb
@@ -6,10 +6,10 @@ class K6 < Formula
   license "AGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9188e1dc69294c2f6066e9d81ec3f211fa6388645c1daab9e5a5ff57dc9912e6"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2137c88a32ac557df0d8738c18d010772ebde7467bcdc97c55c71cf7b6696b79"
-    sha256 cellar: :any_skip_relocation, catalina:      "9417f296cc2f5a7e360f466577ab4a001dc0b8ed5ea99fb998280eeae39a13a1"
-    sha256 cellar: :any_skip_relocation, mojave:        "ada361abbd7089c348837364177526524198ccb569468cadb9b01b95ac20a9c9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "341acd3abfbe17f3f996a7b09643e12bd1945734fb22a2278a259ff6d407345e"
+    sha256 cellar: :any_skip_relocation, big_sur:       "04b202b8ddcf6d8f9c3fbd690f4afdd9fe2f93d42e9b638a4d6362b43e4bfba5"
+    sha256 cellar: :any_skip_relocation, catalina:      "35f8b9f837e3e1aea7a8e5df0a2fb21e1ba1888f55dde659989f70a6e0d7ec2a"
+    sha256 cellar: :any_skip_relocation, mojave:        "cb169f9a2012619aec625cbcdea9b654da0fb98c50e926f027598be0eb776de4"
   end
 
   depends_on "go" => :build
