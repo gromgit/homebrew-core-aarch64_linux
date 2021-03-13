2da1b8bb5b86a23f584dbe24c5075b89303f02b8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-13 19:12:36 +0000

    termcolor: update 2.0.0 bottle.

--- Formula/termcolor.rb
+++ Formula/termcolor.rb
@@ -6,11 +6,10 @@ class Termcolor < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5db4ac397b8826be095a0fcd81d01460ddbb1340fb401f1958df5deaa46c4779"
-    sha256 cellar: :any_skip_relocation, big_sur:       "def1f0c727fa7494adaaae105c6567ff1a01cf86a06713ba38272a2c99a2542c"
-    sha256 cellar: :any_skip_relocation, catalina:      "bf8dbd74a3f8661b2dcc5968698dc6d0797d32ad4022744fca1c3c2ac1e7b863"
-    sha256 cellar: :any_skip_relocation, mojave:        "bf8dbd74a3f8661b2dcc5968698dc6d0797d32ad4022744fca1c3c2ac1e7b863"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "bf8dbd74a3f8661b2dcc5968698dc6d0797d32ad4022744fca1c3c2ac1e7b863"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6600a6345ddca25f57a8ac394b62edac659399eb1c7eab287c201108ac390461"
+    sha256 cellar: :any_skip_relocation, big_sur:       "86b0707099a56545dc78d87c81407b998c900c22d14b23d53ffa35eafa2e01f2"
+    sha256 cellar: :any_skip_relocation, catalina:      "41012d8fe7ea77d22b30dc9baeac6aa0472b66c4f40e490b214ae34d20076cb1"
+    sha256 cellar: :any_skip_relocation, mojave:        "49ed93a14e7bd15d692c8222d161dc732b3836b829f1dbc4b2e7326d66620674"
   end
 
   depends_on "cmake" => :build
