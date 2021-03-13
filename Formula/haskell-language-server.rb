88d3d70b00469f10bb4d10eb45f5a14680dea796
Andrzej Undzillo <andrzej.undzillo@gmail.com>
2021-03-13 18:38:01 +0100

    haskell-language-server 1.0.0 (new formula)
    Closes #73117.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

---
+++ Formula/haskell-language-server.rb
@@ -0,0 +1,40 @@
+class HaskellLanguageServer < Formula
+  desc "Integration point for ghcide and haskell-ide-engine. One IDE to rule them all"
+  homepage "https://github.com/haskell/haskell-language-server"
+  url "https://github.com/haskell/haskell-language-server/archive/1.0.0.tar.gz"
+  sha256 "14e28d6621d029f027fae44bc4a4ef62c869dab24ff01b88a2e51e6679cbff6c"
+  license "Apache-2.0"
+  head "https://github.com/haskell/haskell-language-server.git"
+
+  # we need :github_latest here because otherwise
+  # livecheck picks up spurious non-release tags
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "cabal-install" => [:build, :test]
+  depends_on "ghc" => [:build, :test]
+
+  def install
+    system "cabal", "v2-update"
+    system "cabal", "v2-install", *std_cabal_v2_args
+  end
+
+  def caveats
+    versioned_ghc = "ghc #{Formula["ghc"].version}"
+
+    <<~EOS
+      #{name} is built for #{versioned_ghc}. You need
+      to provide your own #{versioned_ghc} or install one with
+        brew install ghc
+    EOS
+  end
+
+  test do
+    (testpath/"valid.hs").write("f :: Int -> Int\nf x = x + 1")
+    (testpath/"invalid.hs").write("f :: Int -> Int")
+    assert_match "Completed (1 file worked, 1 file failed)",
+      shell_output("#{bin}/haskell-language-server-wrapper #{testpath}/*.hs", 1)
+  end
+end
