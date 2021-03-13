ebb33f0ebd68cbaa3548f621f74b7c9d5103fadf
danielnachun <daniel.nachun@gmail.com>
2021-03-12 18:21:24 -0800

    gnumeric: fix build for Linux
    Closes #73086.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gnumeric.rb
+++ Formula/gnumeric.rb
@@ -22,7 +22,30 @@ class Gnumeric < Formula
   depends_on "libxml2"
   depends_on "rarian"
 
+  uses_from_macos "bison"
+
+  on_linux do
+    depends_on "perl"
+
+    resource "XML::Parser" do
+      url "https://cpan.metacpan.org/authors/id/T/TO/TODDR/XML-Parser-2.44.tar.gz"
+      sha256 "1ae9d07ee9c35326b3d9aad56eae71a6730a73a116b9fe9e8a4758b7cc033216"
+    end
+  end
+
   def install
+    on_linux do
+      ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+
+      resources.each do |res|
+        res.stage do
+          system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+          system "make", "PERL5LIB=#{ENV["PERL5LIB"]}"
+          system "make", "install"
+        end
+      end
+    end
+
     # ensures that the files remain within the keg
     inreplace "component/Makefile.in",
               "GOFFICE_PLUGINS_DIR = @GOFFICE_PLUGINS_DIR@",
