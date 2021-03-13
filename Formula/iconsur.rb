a1585e34436c08467993fa87b68fb21823dfb38f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-13 21:05:50 +0000

    iconsur: add 1.6.2 bottle.

--- Formula/iconsur.rb
+++ Formula/iconsur.rb
@@ -7,6 +7,13 @@ class Iconsur < Formula
   sha256 "86d0f47221ec638c7157e0857f9e4648f9fee89de443b94c252bd7661747bf35"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f1f5ce940aedd4c2e3028f770b89027dedf33e5f13a022389afc76eec80c1e2d"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a0a04c38eb4980fdfdee8588a981e28f9bc523c4fec0a10cbe8063de5c99bf9a"
+    sha256 cellar: :any_skip_relocation, catalina:      "37eb82fad66b4a633e056a178b260b47fbd7e5786e2bd01de435f42ca7ca454d"
+    sha256 cellar: :any_skip_relocation, mojave:        "43b9e604727a3cf6c5cf25a10bbadee1ed9b1c31be8bb4bf38a76e09cf547e92"
+  end
+
   depends_on "node"
 
   def install
