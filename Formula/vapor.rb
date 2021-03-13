d2e41e9cac918f8ffad9eaedc5ed4d37d7983303
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-13 09:18:24 +0000

    vapor: update 18.3.2 bottle.

--- Formula/vapor.rb
+++ Formula/vapor.rb
@@ -7,9 +7,9 @@ class Vapor < Formula
   head "https://github.com/vapor/toolbox.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "fa4ef9390d0d07445aa299db3e27b7c4a278c40261ad25bdc6bdce41b43d44e7"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3a90bf17013c0a14265579cf25c9b3eb1639a4b7493ed1828760281fcc208efb"
-    sha256 cellar: :any_skip_relocation, catalina:      "4a865f0938fec6da6bd461b1bdbbdb70b15a7e01371ac5ac1b41f862a481bf4b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "fd4aea6ebc48a69adc44a5c203fa99a3f2407773b01103ebc97e39c94a337c3d"
+    sha256 cellar: :any_skip_relocation, big_sur:       "48f27984e8ff7eb6a16574782bb96c18aefe60690d63a2cd4c39239d6a13c8bb"
+    sha256 cellar: :any_skip_relocation, catalina:      "3e17e3a8020ea64789f6c2ec1eead0b04d9357390d9004a285af26f565828a40"
   end
 
   depends_on xcode: "11.4"
