4324a52c3e56786e2e25af4453a9598a974dd2b9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-14 07:17:10 +0000

    glow: update 1.4.0 bottle.

--- Formula/glow.rb
+++ Formula/glow.rb
@@ -6,10 +6,10 @@ class Glow < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "56362f0bc55784e418d45eb431b57153fed78e7da7ddd2c47373038735ca71fe"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5b7608d88429fd32f4905bb3506d00835d03a314083024815ce43581c30e2756"
-    sha256 cellar: :any_skip_relocation, catalina:      "74925c63282ec0f602131805f9733850d4b27f70fca23efd81d5451316b45606"
-    sha256 cellar: :any_skip_relocation, mojave:        "c98f41246b644d11f0562b5a79129fa6d4d673094bf8d9b9a4f92282751ebdba"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1eae4d2edadf09016118ba6ef4220acade395c455ad997857eab3dc0803ff82d"
+    sha256 cellar: :any_skip_relocation, big_sur:       "c379ca5e4ac4a0e5e4fce06b5615e11a349d891cad0c6b1d7c3084fca4da2082"
+    sha256 cellar: :any_skip_relocation, catalina:      "bd1c7fe06a82180bc34e136ca27ae1681555d5fb7b0bc9f4e374bfe6fd0ff0c9"
+    sha256 cellar: :any_skip_relocation, mojave:        "4c3069a7e719ab28a236df5fedc2a7f57a9e6429d9c6c1d606f3a827e44e0a7f"
   end
 
   depends_on "go" => :build
