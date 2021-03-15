4326523bcf41231617cd912ca8954e2084f4a738
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-15 19:42:47 +0000

    sqlite-utils: depend on python-tabulate
    Closes #73214.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/sqlite-utils.rb
+++ Formula/sqlite-utils.rb
@@ -5,6 +5,7 @@ class SqliteUtils < Formula
   url "https://files.pythonhosted.org/packages/0d/7e/111bde53d3f4c4f03152963086a28aa11ea5536c6980f64b48eff43d7c7d/sqlite-utils-3.6.tar.gz"
   sha256 "582a9bcf4b6cb32ee2efa4a0d8f79ec630e8965ca93c69ceaaa7d424e1c01560"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "9e37e4cee6496ffc04f0baa54e9cd053be627329cbae5a1c3e9cfb31bd8a2b25"
@@ -13,6 +14,7 @@ class SqliteUtils < Formula
     sha256 cellar: :any_skip_relocation, mojave:        "e2ab94b6a41c963426c3bd51107625871ce931d788674d1c32791ff20a9ce0db"
   end
 
+  depends_on "python-tabulate"
   depends_on "python@3.9"
 
   resource "click" do
@@ -28,11 +30,6 @@ class SqliteUtils < Formula
   resource "sqlite-fts4" do
     url "https://files.pythonhosted.org/packages/62/30/63e64b7b8fa69aabf97b14cbc204cb9525eb2132545f82231c04a6d40d5c/sqlite-fts4-1.0.1.tar.gz"
     sha256 "b2d4f536a28181dc4ced293b602282dd982cc04f506cf3fc491d18b824c2f613"
-  end
-
-  resource "tabulate" do
-    url "https://files.pythonhosted.org/packages/69/44/6c7326b95268c16cf8dc1376ed1a4f404fa9fd04c1371c0917d3f2806832/tabulate-0.8.8.tar.gz"
-    sha256 "26f2589d80d332fefd2371d396863dedeb806f51b54bdb4b264579270b621e92"
   end
 
   def install
