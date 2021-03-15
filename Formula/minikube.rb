291e84183126b12ec13ab3af4d3ade116ca1d5f3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-15 11:03:16 +0000

    minikube: update 1.18.1 bottle.

--- Formula/minikube.rb
+++ Formula/minikube.rb
@@ -8,9 +8,10 @@ class Minikube < Formula
   head "https://github.com/kubernetes/minikube.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "e6b1490bdfbf8f3dde31f8bdc3f8222b7ab11dc3842682c2770a5ba5aa5cfbfe"
-    sha256 cellar: :any_skip_relocation, catalina: "ca09a0fb9854641e6352fc1a983529f21bcac35be52b39d0cf664033459af893"
-    sha256 cellar: :any_skip_relocation, mojave:   "12a604af770bc1d6cf2fafc709009b1f7b8bc791b04df68e05d09ee54be13aa1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "714a97e881e2daa8082569743a53a715f40634809f352c0cb8639182aa79b54c"
+    sha256 cellar: :any_skip_relocation, big_sur:       "e6b1490bdfbf8f3dde31f8bdc3f8222b7ab11dc3842682c2770a5ba5aa5cfbfe"
+    sha256 cellar: :any_skip_relocation, catalina:      "ca09a0fb9854641e6352fc1a983529f21bcac35be52b39d0cf664033459af893"
+    sha256 cellar: :any_skip_relocation, mojave:        "12a604af770bc1d6cf2fafc709009b1f7b8bc791b04df68e05d09ee54be13aa1"
   end
 
   depends_on "go" => :build
