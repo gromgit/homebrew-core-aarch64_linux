dc2414c1bb90dc148cb2cebc73610e56ff757726
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-15 10:59:38 +0000

    devspace: update 5.8.4 bottle.

--- Formula/devspace.rb
+++ Formula/devspace.rb
@@ -13,9 +13,10 @@ class Devspace < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "e3370fe849ddaa8e51305586966317f003c510f68a6e6b5939c2f5d1ddd3d0a8"
-    sha256 cellar: :any_skip_relocation, catalina: "1d1e43324219582605aaac321abc63c97e7422949a95eb233de7c56c1214ad1d"
-    sha256 cellar: :any_skip_relocation, mojave:   "206be63e9e43564885f24f43534f6ad0f42c98fab64edb729924663fbd18dff0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e20c1b97099aa696d66e0c520b22daba8383d14d55ebcd1ae4b65a82c54c62f7"
+    sha256 cellar: :any_skip_relocation, big_sur:       "e3370fe849ddaa8e51305586966317f003c510f68a6e6b5939c2f5d1ddd3d0a8"
+    sha256 cellar: :any_skip_relocation, catalina:      "1d1e43324219582605aaac321abc63c97e7422949a95eb233de7c56c1214ad1d"
+    sha256 cellar: :any_skip_relocation, mojave:        "206be63e9e43564885f24f43534f6ad0f42c98fab64edb729924663fbd18dff0"
   end
 
   depends_on "go" => :build
