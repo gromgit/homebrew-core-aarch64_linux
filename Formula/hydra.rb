468a11d9b12accbcbfc42a61a85eefab5ed3843b
Thierry Moisan <thierry.moisan@gmail.com>
2021-03-15 19:30:27 -0400

    hydra 9.2
    * hydra 9.2
    * Update hydra.rb
    * update hydra.rb
    
    Closes #73232.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/hydra.rb
+++ Formula/hydra.rb
@@ -1,9 +1,9 @@
 class Hydra < Formula
   desc "Network logon cracker which supports many services"
   homepage "https://github.com/vanhauser-thc/thc-hydra"
-  url "https://github.com/vanhauser-thc/thc-hydra/archive/v9.1.tar.gz"
-  sha256 "ce08a5148c0ae5ff4b0a4af2f7f15c5946bc939a57eae1bbb6dda19f34410273"
-  license "AGPL-3.0"
+  url "https://github.com/vanhauser-thc/thc-hydra/archive/v9.2.tar.gz"
+  sha256 "1a28f064763f9144f8ec574416a56ef51c0ab1ae2276e35a89ceed4f594ec5d2"
+  license "AGPL-3.0-only"
   head "https://github.com/vanhauser-thc/thc-hydra.git"
 
   bottle do
@@ -45,6 +45,11 @@ class Hydra < Formula
     # https://github.com/vanhauser-thc/thc-hydra/issues/22
     system "./configure", "--prefix=#{prefix}"
     bin.mkpath
+    # remove unsupported ld flags on mac
+    # related to https://github.com/vanhauser-thc/thc-hydra/issues/622
+    on_macos do
+      inreplace "Makefile", "-Wl,--allow-multiple-definition", ""
+    end
     system "make", "all", "install"
     share.install prefix/"man" # Put man pages in correct place
   end
