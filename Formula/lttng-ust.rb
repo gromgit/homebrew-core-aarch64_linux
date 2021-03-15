745e0358bbeec0bc464ec9dd746e3845d521ba8a
lexicol <67622970+lexicol@users.noreply.github.com>
2021-03-15 15:42:45 -0700

    lttng-ust 2.12.1 (new formula)

---
+++ Formula/lttng-ust.rb
@@ -0,0 +1,22 @@
+class LttngUst < Formula
+  desc "Linux Trace Toolkit Next Generation Userspace Tracer"
+  homepage "https://lttng.org/"
+  url "https://lttng.org/files/lttng-ust/lttng-ust-2.12.1.tar.bz2"
+  sha256 "48a3948b168195123a749d22818809bd25127bb5f1a66458c3c012b210d2a051"
+  license all_of: ["LGPL-2.1-only", "MIT", "GPL-2.0-only", "BSD-3-Clause", "BSD-2-Clause", "GPL-3.0-or-later"]
+
+  depends_on :linux
+  depends_on "numactl"
+  depends_on "userspace-rcu"
+
+  def install
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "make", "install"
+  end
+
+  test do
+    cp_r (share/"doc/lttng-ust/examples/demo").children, testpath
+    system "make"
+    system "./demo"
+  end
+end
