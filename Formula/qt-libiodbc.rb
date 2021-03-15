183d684eb4cc953e592ece3af883162db5cc2fd1
PaperChalice <lgamma@163.com>
2021-03-15 13:34:23 +0800

    qt-libiodbc: use cmake

--- Formula/qt-libiodbc.rb
+++ Formula/qt-libiodbc.rb
@@ -4,6 +4,7 @@ class QtLibiodbc < Formula
   url "https://download.qt.io/official_releases/qt/6.0/6.0.2/submodules/qtbase-everywhere-src-6.0.2.tar.xz"
   sha256 "991a0e4e123104e76563067fcfa58602050c03aba8c8bb0c6198347c707817f1"
   license all_of: ["GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
+  revision 1
 
   bottle do
     sha256 cellar: :any, arm64_big_sur: "732b22f49ca9dac967f1bf392d3d01b74502f7d69c4161b56b9269297a8dbdfc"
@@ -21,19 +22,27 @@ class QtLibiodbc < Formula
     because: "qt-unixodbc and qt-libiodbc install the same binaries"
 
   def install
+    args = std_cmake_args + %W[
+      -DCMAKE_STAGING_PREFIX=#{prefix}
+
+      -DFEATURE_sql_ibase=OFF
+      -DFEATURE_sql_mysql=OFF
+      -DFEATURE_sql_oci=OFF
+      -DFEATURE_sql_odbc=ON
+      -DFEATURE_sql_psql=OFF
+      -DFEATURE_sql_sqlite=OFF
+    ]
+
     cd "src/plugins/sqldrivers" do
-      system "qmake", "--", "ODBC_PREFIX=#{Formula["libiodbc"].opt_prefix}"
-      system "make", "sub-odbc"
-      (share/"qt").install "plugins/"
-    end
-    Pathname.glob(share/"qt/plugins/sqldrivers/#{shared_library("*")}") do |plugin|
-      system "strip", "-S", "-x", plugin
+      system "cmake", ".", *args
+      system "cmake", "--build", "."
+      system "cmake", "--install", "."
     end
   end
 
   test do
     (testpath/"CMakeLists.txt").write <<~EOS
-      cmake_minimum_required(VERSION 3.19.0)
+      cmake_minimum_required(VERSION #{Formula["cmake"].version})
       project(test VERSION 1.0.0 LANGUAGES CXX)
       set(CMAKE_CXX_STANDARD 17)
       set(CMAKE_CXX_STANDARD_REQUIRED ON)
@@ -70,9 +79,9 @@ class QtLibiodbc < Formula
       }
     EOS
 
-    system "cmake", "-DCMAKE_BUILD_TYPE=Debug", testpath
-    system "make"
-    system "./test"
+    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_BUILD_TYPE=Debug"
+    system "cmake", "--build", "build"
+    system "./build/test"
 
     ENV.delete "CPATH"
     system "qmake"
