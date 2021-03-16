903dc34bd7f2a00455494b2aad9c59f5cc661f9c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-16 21:16:34 +0000

    luv: build shared library
    Building the shared library requires a small patch.
    
    Additionally:
    
    - simplify install method
    - improve test
    - add HEAD spec

--- Formula/luv.rb
+++ Formula/luv.rb
@@ -1,10 +1,26 @@
 class Luv < Formula
   desc "Bare libuv bindings for lua"
   homepage "https://github.com/luvit/luv"
-  url "https://github.com/luvit/luv/archive/1.36.0-0.tar.gz"
   version "1.36.0-0"
-  sha256 "739d733d32741a9e6caa3ff3a4416dcf121f39f622ee143c7d63130ce7de27be"
   license "Apache-2.0"
+  revision 1
+
+  stable do
+    url "https://github.com/luvit/luv/archive/1.36.0-0.tar.gz"
+    sha256 "739d733d32741a9e6caa3ff3a4416dcf121f39f622ee143c7d63130ce7de27be"
+
+    resource "lua-compat-5.3" do
+      url "https://github.com/keplerproject/lua-compat-5.3/archive/v0.10.tar.gz"
+      sha256 "d1ed32f091856f6fffab06232da79c48b437afd4cd89e5c1fc85d7905b011430"
+    end
+
+    # Patch to fix shared library build failure on macOS. Remove at next release.
+    # https://github.com/luvit/luv/pull/533
+    patch do
+      url "https://github.com/luvit/luv/commit/10a27c2b58cb6c2338223b70382397be508cdc86.patch?full_index=1"
+      sha256 "793ada6b314f5c6bc308a0ff7d1713632c75d641578455a6570e7bf4a4187e4d"
+    end
+  end
 
   bottle do
     sha256 cellar: :any, arm64_big_sur: "f6206cb6d576a9d76340207c9bb0ea606fa436396e8e6f47c8f112cb5ff30b9a"
@@ -13,14 +29,17 @@ class Luv < Formula
     sha256 cellar: :any, mojave:        "5fad043a019896b644261362348caf757e256890c46fc4e268ad79710d45b57b"
   end
 
+  head do
+    url "https://github.com/luvit/luv.git"
+
+    resource "lua-compat-5.3" do
+      url "https://github.com/keplerproject/lua-compat-5.3.git"
+    end
+  end
+
   depends_on "cmake" => :build
   depends_on "luajit-openresty" => [:build, :test]
   depends_on "libuv"
-
-  resource "lua-compat-5.3" do
-    url "https://github.com/keplerproject/lua-compat-5.3/archive/v0.10.tar.gz"
-    sha256 "d1ed32f091856f6fffab06232da79c48b437afd4cd89e5c1fc85d7905b011430"
-  end
 
   def install
     resource("lua-compat-5.3").stage buildpath/"deps/lua-compat-5.3"
@@ -30,23 +49,18 @@ class Luv < Formula
       -DWITH_LUA_ENGINE=LuaJIT
       -DLUA_BUILD_TYPE=System
       -DLUA_COMPAT53_DIR=#{buildpath}/deps/lua-compat-5.3
+      -DBUILD_MODULE=ON
+      -DBUILD_SHARED_LIBS=ON
+      -DBUILD_STATIC_LIBS=ON
     ]
 
-    system "cmake", ".", "-B", "build-module", *args
-    cd "build-module" do
-      system "cmake", "--build", "."
-      system "cmake", "--build", ".", "--target", "install"
-    end
-
-    system "cmake", ".", "-B", "build-static", *args, "-DBUILD_MODULE=OFF", "-DBUILD_STATIC_LIBS=ON"
-    cd "build-static" do
-      system "cmake", "--build", "."
-      system "cmake", "--build", ".", "--target", "install"
-    end
+    system "cmake", "-S", ".", "-B", "build", *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
-    ENV["LUA_CPATH"] = lib/"lua/5.1/?.so"
+    ENV["LUA_CPATH"] = opt_lib/"lua/5.1/?.so"
     ENV.prepend_path "PATH", Formula["luajit-openresty"].opt_bin
     (testpath/"test.lua").write <<~EOS
       local uv = require('luv')
@@ -58,6 +72,6 @@ class Luv < Formula
       print("Sleeping");
       uv.run()
     EOS
-    system "luajit", testpath/"test.lua"
+    assert_equal "Sleeping\nAwake!\n", shell_output("luajit test.lua")
   end
 end
