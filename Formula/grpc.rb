78a1cde173f448984bd2f53d4d430d38b39de938
Thierry Moisan <thierry.moisan@gmail.com>
2021-03-15 23:10:25 -0400

    grpc 1.36.3
    Closes #73239.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/grpc.rb
+++ Formula/grpc.rb
@@ -2,8 +2,8 @@ class Grpc < Formula
   desc "Next generation open source RPC library and framework"
   homepage "https://grpc.io/"
   url "https://github.com/grpc/grpc.git",
-      tag:      "v1.36.2",
-      revision: "c3438a0c5d7bc499eb31fd4853ca72c771f758a5",
+      tag:      "v1.36.3",
+      revision: "ce05bf557ced2d311bad8ee520f9f8088f715bd8",
       shallow:  false
   license "Apache-2.0"
   head "https://github.com/grpc/grpc.git"
