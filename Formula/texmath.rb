69fe26dd72d74bd12b6eda582123c0b120842c53
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-16 02:41:32 +0000

    texmath: update 0.12.2 bottle.

--- Formula/texmath.rb
+++ Formula/texmath.rb
@@ -6,9 +6,9 @@ class Texmath < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "cdc91e162794f7481b0b4d881b8217e7b51d7c1613d5fa73772fd778d1eac7c0"
-    sha256 cellar: :any_skip_relocation, catalina: "dd439f733a8c27d6eabb310d73679e100ffef8de14f0f0a705ac5d715ada5164"
-    sha256 cellar: :any_skip_relocation, mojave:   "1ac2b2157f436c43c9857b7506677fa875780b74a819127dcbbee2c83bab34be"
+    sha256 cellar: :any_skip_relocation, big_sur:  "1539fd49ad850acc19dd026905a77b8066112ac4fc16e4e4cb258f189207879c"
+    sha256 cellar: :any_skip_relocation, catalina: "e5ce6a5a729671e4ca2ed7df9c08901d8da34437f5a39cfde0fcb15f3dc8ec7a"
+    sha256 cellar: :any_skip_relocation, mojave:   "020445b8f86d18b2bc7a5c0a8fbc7a52180bb935dca30216d23ac34903b54fe8"
   end
 
   depends_on "cabal-install" => :build
