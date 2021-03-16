e5d8d2d98cb4f211268ae06f792e905012a08130
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-16 22:24:56 +0000

    sdl2_net: add license

--- Formula/sdl2_net.rb
+++ Formula/sdl2_net.rb
@@ -3,6 +3,7 @@ class Sdl2Net < Formula
   homepage "https://www.libsdl.org/projects/SDL_net/"
   url "https://www.libsdl.org/projects/SDL_net/release/SDL2_net-2.0.1.tar.gz"
   sha256 "15ce8a7e5a23dafe8177c8df6e6c79b6749a03fff1e8196742d3571657609d21"
+  license "Zlib"
 
   livecheck do
     url :homepage
