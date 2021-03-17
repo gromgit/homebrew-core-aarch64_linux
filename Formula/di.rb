c088e8d4d9b8f6a719c844cdd3ac8a71a84beda4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-17 05:17:57 +0000

    di: update 4.49 bottle.

--- Formula/di.rb
+++ Formula/di.rb
@@ -11,10 +11,10 @@ class Di < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f73ef475c2e9c76a19cb31c8554d5bcfa2f3160e5d16b90c05c7623263c490d2"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e3be4bfb7fcbde3a31b282323e46ad7c7fedb057dd0bc60504c60863dc9f14f8"
-    sha256 cellar: :any_skip_relocation, catalina:      "3fec602b01937a696deb147c5a0a22f9ea2bfc535980a9f10b5ec89d230d9999"
-    sha256 cellar: :any_skip_relocation, mojave:        "6e8b6d40cf8cdbc8b73524d01c87dcf1adf4039155d5370436aac3923aad402e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a48d716bb48256f028b7d25f4cebef804946833b4778619bb7bcb3a18856bf66"
+    sha256 cellar: :any_skip_relocation, big_sur:       "50ce1db9a6fcc59b37e873b62750c82a1bc8ed36af64342afa533f39a52046d7"
+    sha256 cellar: :any_skip_relocation, catalina:      "2ad832b9d2eba7c3c197e67828d41b4ff112dca9af8db7fe71b2fd745f983b74"
+    sha256 cellar: :any_skip_relocation, mojave:        "7381006ae44f23fab65ddb4da6aad65e1aca43180cc212a3bf12e51bbc808503"
   end
 
   def install
