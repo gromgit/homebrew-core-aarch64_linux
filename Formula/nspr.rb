9beb3eede431228c6015235ae92721c341f36e64
Thierry Moisan <thierry.moisan@gmail.com>
2021-03-16 20:15:34 -0400

    nspr 4.30
    Closes #73312.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nspr.rb
+++ Formula/nspr.rb
@@ -1,8 +1,8 @@
 class Nspr < Formula
   desc "Platform-neutral API for system-level and libc-like functions"
   homepage "https://developer.mozilla.org/docs/Mozilla/Projects/NSPR"
-  url "https://archive.mozilla.org/pub/nspr/releases/v4.29/src/nspr-4.29.tar.gz"
-  sha256 "22286bdb8059d74632cc7c2865c139e63953ecfb33bf4362ab58827e86e92582"
+  url "https://archive.mozilla.org/pub/nspr/releases/v4.30/src/nspr-4.30.tar.gz"
+  sha256 "8d4cd8f8409484dc4c3d31e180354bfc506573eccf86cd691106a1ef7edc913b"
   license "MPL-2.0"
 
   livecheck do
