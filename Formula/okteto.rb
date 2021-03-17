4d57631ed75bc946607ced9d0970fefddabc049c
rui <rui@chenrui.dev>
2021-03-17 22:34:54 +0000

    okteto 1.11.5
    Closes #73374.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/okteto.rb
+++ Formula/okteto.rb
@@ -1,8 +1,8 @@
 class Okteto < Formula
   desc "Build better apps by developing and testing code directly in Kubernetes"
   homepage "https://okteto.com"
-  url "https://github.com/okteto/okteto/archive/1.11.4.tar.gz"
-  sha256 "a1e98bde8c20556e35bcf928dedf4ffb0015ccc98cb33e5dc322d6ad7029b2f6"
+  url "https://github.com/okteto/okteto/archive/1.11.5.tar.gz"
+  sha256 "a5ab37aae2ed7b0d3d257ec2594667fb626b341931c7a040d008d542b6fb1c9d"
   license "Apache-2.0"
   head "https://github.com/okteto/okteto.git"
 
