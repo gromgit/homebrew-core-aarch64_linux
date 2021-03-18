c75ce10e8f1eca53b1ea64011ee7df931e32bade
Michka Popoff <michkapopoff@gmail.com>
2021-03-18 07:30:45 +0100

    openssh: align formula with Linux version

--- Formula/openssh.rb
+++ Formula/openssh.rb
@@ -27,28 +27,41 @@ class Openssh < Formula
   depends_on "libfido2"
   depends_on "openssl@1.1"
 
+  uses_from_macos "lsof" => :test
+  uses_from_macos "krb5"
+  uses_from_macos "libedit"
+  uses_from_macos "zlib"
+
+  on_macos do
+    # Both these patches are applied by Apple.
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/patches/1860b0a745f1fe726900974845d1b0dd3c3398d6/openssh/patch-sandbox-darwin.c-apple-sandbox-named-external.diff"
+      sha256 "d886b98f99fd27e3157b02b5b57f3fb49f43fd33806195970d4567f12be66e71"
+    end
+
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/patches/d8b2d8c2612fd251ac6de17bf0cc5174c3aab94c/openssh/patch-sshd.c-apple-sandbox-named-external.diff"
+      sha256 "3505c58bf1e584c8af92d916fe5f3f1899a6b15cc64a00ddece1dc0874b2f78f"
+    end
+  end
+
+  on_linux do
+    depends_on "linux-pam"
+  end
+
   resource "com.openssh.sshd.sb" do
     url "https://opensource.apple.com/source/OpenSSH/OpenSSH-209.50.1/com.openssh.sshd.sb"
     sha256 "a273f86360ea5da3910cfa4c118be931d10904267605cdd4b2055ced3a829774"
   end
 
-  # Both these patches are applied by Apple.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/patches/1860b0a745f1fe726900974845d1b0dd3c3398d6/openssh/patch-sandbox-darwin.c-apple-sandbox-named-external.diff"
-    sha256 "d886b98f99fd27e3157b02b5b57f3fb49f43fd33806195970d4567f12be66e71"
-  end
-
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/patches/d8b2d8c2612fd251ac6de17bf0cc5174c3aab94c/openssh/patch-sshd.c-apple-sandbox-named-external.diff"
-    sha256 "3505c58bf1e584c8af92d916fe5f3f1899a6b15cc64a00ddece1dc0874b2f78f"
-  end
-
   def install
-    ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__"
+    on_macos do
+      ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__"
 
-    # Ensure sandbox profile prefix is correct.
-    # We introduce this issue with patching, it's not an upstream bug.
-    inreplace "sandbox-darwin.c", "@PREFIX@/share/openssh", etc/"ssh"
+      # Ensure sandbox profile prefix is correct.
+      # We introduce this issue with patching, it's not an upstream bug.
+      inreplace "sandbox-darwin.c", "@PREFIX@/share/openssh", etc/"ssh"
+    end
 
     args = %W[
       --prefix=#{prefix}
@@ -60,6 +73,10 @@ class Openssh < Formula
       --with-ssl-dir=#{Formula["openssl@1.1"].opt_prefix}
       --with-security-key-builtin
     ]
+
+    on_linux do
+      args << "--with-privsep-path=#{var}/lib/sshd"
+    end
 
     system "./configure", *args
     system "make"
