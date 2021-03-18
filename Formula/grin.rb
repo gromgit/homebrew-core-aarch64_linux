31eaf2b4a77254657187cfc72a8daef924daaf0b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-18 15:21:46 +0000

    grin: update 5.0.2 bottle.

--- Formula/grin.rb
+++ Formula/grin.rb
@@ -6,9 +6,9 @@ class Grin < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "23508e8aeb1f492d02cab86a37ee13a01b7c91e51ba9f99915bb290168aa65e3"
-    sha256 cellar: :any_skip_relocation, catalina: "614b5706b1411d4fe034892ebd04757c8130dce05c839bb49c530ef35aff7c18"
-    sha256 cellar: :any_skip_relocation, mojave:   "367c827e429afa3bff4c03f2c272d8e6b18346eda070994afe32230a6f3f59da"
+    sha256 cellar: :any_skip_relocation, big_sur:  "1e11fe8b10b498cf7658a74f435b0f6c69413725ce1d2c3911dab100b6db7a7b"
+    sha256 cellar: :any_skip_relocation, catalina: "1b936d7ad3720895f67decc1e68c75f60be17d1f9e428f0cb68d38fa55bea449"
+    sha256 cellar: :any_skip_relocation, mojave:   "d5db0c0384a829c326df7e85b970da1cffeee900c447e132881d9abb6cd5558f"
   end
 
   depends_on "llvm" => :build # for libclang
