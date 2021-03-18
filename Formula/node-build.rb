ec4638615d88efb81b8b8e8ef8400792d7cd6a42
nodenv machine <68817010+nodenv-bot@users.noreply.github.com>
2021-03-18 06:33:08 -0400

    node-build 4.9.33 (#73406)
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/node-build.rb
+++ Formula/node-build.rb
@@ -1,8 +1,8 @@
 class NodeBuild < Formula
   desc "Install NodeJS versions"
   homepage "https://github.com/nodenv/node-build"
-  url "https://github.com/nodenv/node-build/archive/v4.9.32.tar.gz"
-  sha256 "e2c98e06f9fb31865d7e23be8d49b8adb5ed1ede1d16ec87b0e06865689ea718"
+  url "https://github.com/nodenv/node-build/archive/v4.9.33.tar.gz"
+  sha256 "980d3c826b7a8c817163b61a9c4b011ca42a75583468c362a54584e456709c04"
   license "MIT"
   head "https://github.com/nodenv/node-build.git"
 
