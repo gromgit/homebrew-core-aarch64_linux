8b32416b6a4ca55a7fe295d5116ef923d0d17d0c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-18 17:34:24 +0000

    mockery: update 2.7.4 bottle.

--- Formula/mockery.rb
+++ Formula/mockery.rb
@@ -7,10 +7,10 @@ class Mockery < Formula
   head "https://github.com/vektra/mockery.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "838bc4e94d6889dab090984c0f70062d5a6e65c25076eb891140e0caa80cb3b7"
-    sha256 cellar: :any_skip_relocation, big_sur:       "775e3c5ab4ff9baa1e464a3402cc5df4d40938f8e1830743e713e48dbefc6861"
-    sha256 cellar: :any_skip_relocation, catalina:      "8b4d00f30efe40382d919e3deebbb89cedb178c9b7a26be8008105bd8b8ecb9d"
-    sha256 cellar: :any_skip_relocation, mojave:        "95b2ea44dbbc182f2b870a3526fc41abd98b43dcb700d6a4b10f789229a17ed2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "52b5d054b7f4dc2e94d65e4b0cf9da43c12babc8bdaae0cfa69db666eb7f6ec4"
+    sha256 cellar: :any_skip_relocation, big_sur:       "d10aaaa636ca0d1d3f1196ff645aca0c78eb8d8908556bddb786df9b233f7a24"
+    sha256 cellar: :any_skip_relocation, catalina:      "e3ba46770f9b31a0dcbcdacd60f6d55317c0e554d57729787b72b08f1a254828"
+    sha256 cellar: :any_skip_relocation, mojave:        "c6021ba38358d6dfaec61d4a4c6ebd5729ea78f95ef41c52a5903083e87d6b05"
   end
 
   depends_on "go"
