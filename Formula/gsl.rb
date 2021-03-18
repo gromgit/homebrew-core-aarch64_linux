76004547721e96a161d597a991227a6c10cb1f0d
John Marshall <jmarshall@hey.com>
2021-03-18 13:22:46 +0000

    gsl: clarify that it's GPL-3.0-or-later
    1148 GSL 2.6 source files contain "[...] under the terms of the
    GNU General Public License [...]; either version 3 of the License,
    or (at your option) any later version".
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/gsl.rb
+++ Formula/gsl.rb
@@ -4,7 +4,7 @@ class Gsl < Formula
   url "https://ftp.gnu.org/gnu/gsl/gsl-2.6.tar.gz"
   mirror "https://ftpmirror.gnu.org/gsl/gsl-2.6.tar.gz"
   sha256 "b782339fc7a38fe17689cb39966c4d821236c28018b6593ddb6fd59ee40786a8"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
 
   bottle do
     sha256 cellar: :any, arm64_big_sur: "b4d8687427eb3a7f951c211f5a0b8051ca0bbaf174e68265fb6619d43b282aeb"
