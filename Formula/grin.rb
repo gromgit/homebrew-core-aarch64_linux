daca79e5530778dc78cb06a6b334c70019df6556
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-18 19:40:28 +0000

    grin: update 5.0.3 bottle.

--- Formula/grin.rb
+++ Formula/grin.rb
@@ -6,9 +6,9 @@ class Grin < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "1e11fe8b10b498cf7658a74f435b0f6c69413725ce1d2c3911dab100b6db7a7b"
-    sha256 cellar: :any_skip_relocation, catalina: "1b936d7ad3720895f67decc1e68c75f60be17d1f9e428f0cb68d38fa55bea449"
-    sha256 cellar: :any_skip_relocation, mojave:   "d5db0c0384a829c326df7e85b970da1cffeee900c447e132881d9abb6cd5558f"
+    sha256 cellar: :any_skip_relocation, big_sur:  "b65ab48f15d777244a45fc67e569f3990f6cd8fdd6da60888162218f3b484418"
+    sha256 cellar: :any_skip_relocation, catalina: "dc83704a6fbc2410810b61314eecbbd37faadb2a2f068b4b16d7ab42f04d75a9"
+    sha256 cellar: :any_skip_relocation, mojave:   "7e174b9bdd34dec0179e1d1ce8bb4f60a4d377e137e64462edd0f7cb0245a3e4"
   end
 
   depends_on "llvm" => :build # for libclang
