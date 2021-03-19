799a3c7ca68cbf28cfd312306a42466cade0c5d9
Andrzej Undzillo <andrzej.undzillo@gmail.com>
2021-03-19 16:14:34 +0100

    haskell-language-server: revision
    haskell-language-server 1.0.0: add support for additional GHC versions
    
    Closes #73522.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/haskell-language-server.rb
+++ Formula/haskell-language-server.rb
@@ -4,6 +4,7 @@ class HaskellLanguageServer < Formula
   url "https://github.com/haskell/haskell-language-server/archive/1.0.0.tar.gz"
   sha256 "14e28d6621d029f027fae44bc4a4ef62c869dab24ff01b88a2e51e6679cbff6c"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/haskell/haskell-language-server.git"
 
   # we need :github_latest here because otherwise
@@ -21,26 +22,53 @@ class HaskellLanguageServer < Formula
 
   depends_on "cabal-install" => [:build, :test]
   depends_on "ghc" => [:build, :test]
+  depends_on "ghc@8.6" => [:build, :test]
+  depends_on "ghc@8.8" => [:build, :test]
 
   def install
     system "cabal", "v2-update"
-    system "cabal", "v2-install", *std_cabal_v2_args
+
+    %w[ghc@8.6 ghc@8.8 ghc].each do |ghc_str|
+      ghc = Formula[ghc_str]
+
+      system "cabal", "v2-install", "-w", ghc.bin/"ghc", *std_cabal_v2_args
+
+      bin.install bin/"haskell-language-server" => "haskell-language-server-#{ghc.version}"
+      rm bin/"haskell-language-server-wrapper" unless ghc_str == "ghc"
+    end
   end
 
   def caveats
-    versioned_ghc = "ghc #{Formula["ghc"].version}"
+    ghc_versions = [
+      Formula["ghc@8.6"].version,
+      Formula["ghc@8.8"].version,
+      Formula["ghc"].version,
+    ].join ", "
 
     <<~EOS
-      #{name} is built for #{versioned_ghc}. You need
-      to provide your own #{versioned_ghc} or install one with
+      #{name} is built for GHC versions #{ghc_versions}.
+      You need to provide your own GHC or install one with
         brew install ghc
     EOS
   end
 
   test do
-    (testpath/"valid.hs").write("f :: Int -> Int\nf x = x + 1")
-    (testpath/"invalid.hs").write("f :: Int -> Int")
-    assert_match "Completed (1 file worked, 1 file failed)",
-      shell_output("#{bin}/haskell-language-server-wrapper #{testpath}/*.hs", 1)
+    valid_hs = testpath/"valid.hs"
+    valid_hs.write <<~EOS
+      f :: Int -> Int
+      f x = x + 1
+    EOS
+
+    invalid_hs = testpath/"invalid.hs"
+    invalid_hs.write <<~EOS
+      f :: Int -> Int
+    EOS
+
+    %w[ghc@8.6 ghc@8.8 ghc].each do |ghc_str|
+      ghc = Formula[ghc_str]
+
+      assert_match "Completed (1 file worked, 1 file failed)",
+        shell_output("PATH=#{ghc.bin}:$PATH #{bin}/haskell-language-server-#{ghc.version} #{testpath}/*.hs", 1)
+    end
   end
 end
