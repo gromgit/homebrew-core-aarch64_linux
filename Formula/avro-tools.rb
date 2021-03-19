31a97fbcc45cedb40c3e28e9bb2b540fd7bc4935
rui <chenrui333@gmail.com>
2021-03-19 08:21:50 -0400

    avro-tools 1.10.2 (#73461)

--- Formula/avro-tools.rb
+++ Formula/avro-tools.rb
@@ -1,9 +1,9 @@
 class AvroTools < Formula
   desc "Avro command-line tools and utilities"
   homepage "https://avro.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=avro/avro-1.10.1/java/avro-tools-1.10.1.jar"
-  mirror "https://archive.apache.org/dist/avro/avro-1.10.1/java/avro-tools-1.10.1.jar"
-  sha256 "172bf20f983ab82c785ccbc31504fa6e6c44f3e93b78c0f201c25ce990d671cb"
+  url "https://www.apache.org/dyn/closer.lua?path=avro/avro-1.10.2/java/avro-tools-1.10.2.jar"
+  mirror "https://archive.apache.org/dist/avro/avro-1.10.2/java/avro-tools-1.10.2.jar"
+  sha256 "ab158f4af8f767d2358a29d8678939b2a0f96017490acfb4e7ed0708cea07913"
   license "Apache-2.0"
 
   bottle :unneeded
