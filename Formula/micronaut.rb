a8bc9c480b338e15749bb63454d275f335192531
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-19 12:29:18 +0000

    micronaut: update 2.4.1 bottle.

--- Formula/micronaut.rb
+++ Formula/micronaut.rb
@@ -11,9 +11,9 @@ class Micronaut < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "c29fada4f38c7b308c0ba34f4cf9feba8124eb30635599980ff749868a6a3952"
-    sha256 cellar: :any_skip_relocation, catalina: "e4cd323f3bbe3c5a7891f557e5bba4b8b1ee280a7a899daf9395fb979baada04"
-    sha256 cellar: :any_skip_relocation, mojave:   "5e67f7c759113b9324445dd0cf88168afe355a5448a79c52fb34e9c113d48b3d"
+    sha256 cellar: :any_skip_relocation, big_sur:  "d5ab03fd173b8f2bdef0a9b4cb84384c6f6ec3c85c6f2a980619fea1e486bb03"
+    sha256 cellar: :any_skip_relocation, catalina: "b41191654d1367d0de6e95d4ffe55b9a1dad6cd7eb42426841aa1eef7073f4eb"
+    sha256 cellar: :any_skip_relocation, mojave:   "7fc6a36f6a6830d56a6c7013fdf558390872f6fd9f8a4040d0b0ca7f8532c3c2"
   end
 
   depends_on "gradle" => :build
