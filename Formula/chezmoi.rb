33f7d331886c3bb2b20b1e6188be2936e5c79052
rui <rui@chenrui.dev>
2021-03-19 01:06:10 +0000

    chezmoi 2.0.2
    Closes #73453.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/chezmoi.rb
+++ Formula/chezmoi.rb
@@ -2,8 +2,8 @@ class Chezmoi < Formula
   desc "Manage your dotfiles across multiple diverse machines, securely"
   homepage "https://chezmoi.io/"
   url "https://github.com/twpayne/chezmoi.git",
-      tag:      "v2.0.1",
-      revision: "e0143cd0c7ec8b7bbe5f6a04d42a5dd5ed823b75"
+      tag:      "v2.0.2",
+      revision: "f5a63291beaf3b3b18dc93890d9d08ad5558f2ec"
   license "MIT"
   head "https://github.com/twpayne/chezmoi.git"
 
