6720ad5cf79f014d76aac00b1b61112b1270fdf8
rui <rui@chenrui.dev>
2021-03-19 04:33:24 +0000

    pyside 6.0.2
    Closes #73490.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pyside.rb
+++ Formula/pyside.rb
@@ -1,8 +1,8 @@
 class Pyside < Formula
   desc "Official Python bindings for Qt"
   homepage "https://wiki.qt.io/Qt_for_Python"
-  url "https://download.qt.io/official_releases/QtForPython/pyside6/PySide6-6.0.1-src/pyside-setup-opensource-src-6.0.1.tar.xz"
-  sha256 "baac59a71d5d8d28badd4b484b3722500a6616684f932f0652b33a5b5feaf365"
+  url "https://download.qt.io/official_releases/QtForPython/pyside6/PySide6-6.0.2-src/pyside-setup-opensource-src-6.0.2.tar.xz"
+  sha256 "55e129e044770c173d64e0144c7754125e6ded4a13ee3c1629dd6ae2ffae5e05"
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-3.0-only"]
 
   livecheck do
