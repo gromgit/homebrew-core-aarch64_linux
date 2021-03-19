d126cd7cbe1e3f0cc4aed1f4d8dfe0976ad80758
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-19 14:23:04 +0000

    graphene: update 1.10.4 bottle.

--- Formula/graphene.rb
+++ Formula/graphene.rb
@@ -6,11 +6,10 @@ class Graphene < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "ed93a2b7f3830d38f82bece133afa43636e1cb90b3342c222909229260e63716"
-    sha256 cellar: :any, big_sur:       "0e6af428f089a813408d7edc7a5196372a1ee8dd3ed13f7edbf4acfb1f62407a"
-    sha256 cellar: :any, catalina:      "d8519d2811ee796969121cd0b087fb7a5e96c2952c69bb2dfe206f3efc299e31"
-    sha256 cellar: :any, mojave:        "0e7e238034cfcd390b8bd0e49bd6ba3c23dcdbff7c9a1cea5e626b34a3381acb"
-    sha256 cellar: :any, high_sierra:   "7bcd5ab83e1509ddeec6b710ba2a69c99bb92438344694423b1c886346fd44c2"
+    sha256 cellar: :any, arm64_big_sur: "7630fe796cca6bd73eed68a742fc9f3b36f7246be829af8035f370229340b385"
+    sha256 cellar: :any, big_sur:       "70cb37a4d8f83d64b08b028970ae895cdac896c13a4a14b51bbbc10df15f7ad4"
+    sha256 cellar: :any, catalina:      "236ccbb6dddc03eb4ca4bbc41e5488f7abeb5943bd73650a7c9887c3a3da5afb"
+    sha256 cellar: :any, mojave:        "d6ff56ed1cbaf09f158c1936e6b60aa08ea65c5342a31596dda6c1f05312abe2"
   end
 
   depends_on "gobject-introspection" => :build
