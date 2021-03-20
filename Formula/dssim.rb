e492267e3715a8471102cf7892d32eea23ccc199
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-20 00:43:47 +0000

    dssim: update 3.0.1 bottle.

--- Formula/dssim.rb
+++ Formula/dssim.rb
@@ -6,10 +6,10 @@ class Dssim < Formula
   license "AGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b9a567ce8d4d748ff5f2267008c456f1dbe4493d5b19624ec711262d45236f52"
-    sha256 cellar: :any_skip_relocation, big_sur:       "919d94af16d5c1b81bf3368819ac71d73cb404fcef347fe90cdefcc1f5bd7f4f"
-    sha256 cellar: :any_skip_relocation, catalina:      "3bbf4c9dc9acf08ea2f16a2ba393fd2a0b90b6a675555cecb3095136b7361938"
-    sha256 cellar: :any_skip_relocation, mojave:        "3610c3253c0838fd558d6d8fdd95913d068458869f8600c88f60fb3021d980a0"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3d930263379be0a4b38c1813f3b7a058629c2f3a46ec6ce57aea1c199483430c"
+    sha256 cellar: :any_skip_relocation, big_sur:       "948f53eefeb6e2c9827faab44cde97a01378c35549021361d99ed2d7b7319c6f"
+    sha256 cellar: :any_skip_relocation, catalina:      "be60aea7ecbb61c23b4139c06037b65e29a6fd250f52aa5b02eb327ac870bbdd"
+    sha256 cellar: :any_skip_relocation, mojave:        "17fdc6ce9b5c1d4e4003b6aeb152219741bec3052997f981bc565e1236e00c5c"
   end
 
   depends_on "nasm" => :build
