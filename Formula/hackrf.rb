fbb7ef4b2e8c88115b1f7dd43ae1cd93130c1dc2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-20 08:21:42 +0000

    hackrf: update 2021.03.1 bottle.

--- Formula/hackrf.rb
+++ Formula/hackrf.rb
@@ -12,13 +12,10 @@ class Hackrf < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "04c7f5c356012bcc072a080439f7edb20742ad3a8917a5a7f07ba83e0d4605a9"
-    sha256 cellar: :any, big_sur:       "fafc41d9d60bcfee157d7b30efc8ea97b507ad20135f60479ae9840235bae2ad"
-    sha256 cellar: :any, catalina:      "4004e867109e43fb7f9613c01a99ffd3d8dee0949d6f27232b06bf740d1e1776"
-    sha256 cellar: :any, mojave:        "9c0610e7d8fe8f1e840b38d3ce6eeab741842a95f227025fbca24c417ae30549"
-    sha256 cellar: :any, high_sierra:   "430173362cc05912520a38f41ce465a0966f1c8d849fd492f0b40074425c3f88"
-    sha256 cellar: :any, sierra:        "f33bc6bde41e6522d587bc574c01e1402ccbde6759dec5e9d1a1e5f593e189b3"
-    sha256 cellar: :any, el_capitan:    "909a5a9aca6f81cbab08bb7c063f3ee0e666bb5b44af86ebbec62cbdaf3e3b33"
+    sha256 cellar: :any, arm64_big_sur: "233c56f5a051ad74e30f3a18741a39c750b4973e20d580388c11606488e61fc0"
+    sha256 cellar: :any, big_sur:       "54e9e6ad6edbac05a150b07cab7e23ff6053e2c64c927fdbc63bea7007c7ec9e"
+    sha256 cellar: :any, catalina:      "903c9d309035f261f336a5c3d456a7947eb14390be917f43f40dbaf2ff0146b6"
+    sha256 cellar: :any, mojave:        "68d1ffb38ef62af28483d702189ea4019cf81764767d91dd0fa113f39bcbdc0c"
   end
 
   depends_on "cmake" => :build
