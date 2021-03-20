44b0ad44769af60b73f0c5b01d596b3f3e786751
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-20 18:39:56 +0000

    openstackclient: update 5.5.0 bottle.

--- Formula/openstackclient.rb
+++ Formula/openstackclient.rb
@@ -8,10 +8,10 @@ class Openstackclient < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "efa827b3bf78979962f0883719229c1c250d1fc1a6e0fa16940f9c80cd1c5ac7"
-    sha256 cellar: :any, big_sur:       "d457a82ceb9d43b5089b03f3d94aa15e5ff08fbd95dc82b107f368c3b3eaa169"
-    sha256 cellar: :any, catalina:      "883710d6c9447fc21749e21907ea4ac6899de08b53e9b1f8875df4e5530cab50"
-    sha256 cellar: :any, mojave:        "562b0c8618c43e49b36f1dfe66997d1d260478cae70eab6d60db16672f9c543c"
+    sha256 cellar: :any, arm64_big_sur: "2f29787b721969574fd33180a44413ca53716ba6398abd393a786e912580136a"
+    sha256 cellar: :any, big_sur:       "bccccc98d1077c9fbabb2a01016b8f9c69dba2d4aba4950ba31e57f617f997eb"
+    sha256 cellar: :any, catalina:      "03d89b5f94e1d72df64ccf3b5b616d0a085cc787fcf8b0d15144a010f5d16c4d"
+    sha256 cellar: :any, mojave:        "1d9ebfa870d73deb2de20e96320ee401f56794443f3ff722476e52727324adda"
   end
 
   depends_on "rust" => :build
