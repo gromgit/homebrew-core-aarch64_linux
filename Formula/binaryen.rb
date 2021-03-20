3351b9af4b09943b444eda3c71f16cc563cd04d4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-20 23:18:40 +0000

    binaryen: update 100 bottle.

--- Formula/binaryen.rb
+++ Formula/binaryen.rb
@@ -7,10 +7,11 @@ class Binaryen < Formula
   head "https://github.com/WebAssembly/binaryen.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "44403c6f0ffc22a7f270fe936702d62981535e234f38f8d95544c9c663bacbbd"
-    sha256 cellar: :any, big_sur:       "dc1687811e112b21a6fb4aac217bf12ecedba0ccfe806fdb537a19c48bfa9ec8"
-    sha256 cellar: :any, catalina:      "abdee68b35c42a59d9a71f38f50e704fba8c9b475d7df8f34957c1b1c4221d3c"
-    sha256 cellar: :any, mojave:        "bc4ca1066209a412279aaeafb2c2a5c6472fd5cc8dbcc88c8aa91da0eadee680"
+    rebuild 1
+    sha256 cellar: :any, arm64_big_sur: "56165cbe52e86afde8f5030d13c60885f1e3a98013d81ae294f93b06e92c5da1"
+    sha256 cellar: :any, big_sur:       "6ec130eba9a753aeef345c1521613a950bc703943c86685a9eca97db367d29b0"
+    sha256 cellar: :any, catalina:      "8843e1dd958ab41d475afda03b3b27e62ea791d0bb7c515ee5b9804429c5c6f0"
+    sha256 cellar: :any, mojave:        "096b306c87fcb4b8c5e566bca323f9087ad04984c5aa36e51cfa90bc919bc512"
   end
 
   depends_on "cmake" => :build
