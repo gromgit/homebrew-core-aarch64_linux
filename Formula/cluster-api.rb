b1100aa942ea7c627550b7e6f07b7c23f869b5fa
Ludovico Russo <ludovico@ludusrusso.space>
2021-03-21 09:46:58 +0100

    cluster-api 0.3.14 (new formula)
    Closes #73610.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

---
+++ Formula/cluster-api.rb
@@ -0,0 +1,18 @@
+class ClusterApi < Formula
+  desc "Home for the Cluster Management API work, a subproject of sig-cluster-lifecycle"
+  homepage "https://cluster-api.sigs.k8s.io"
+  url "https://github.com/kubernetes-sigs/cluster-api/archive/refs/tags/v0.3.14.tar.gz"
+  sha256 "fc7c7eb07bce70505721c8bc82ff4b8f111172cdfef94964b7ceac82e45faace"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "-o", bin/"clusterctl"
+  end
+
+  test do
+    output = shell_output("KUBECONFIG=/homebrew.config  #{bin}/clusterctl 2>&1", 1)
+    assert_match "invalid configuration: no configuration has been provided", output
+  end
+end
