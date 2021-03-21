177cabb7a0fe6835915f1706517221ebb77040ad
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-21 05:22:26 +0000

    coccinelle: update 1.1.0_1 bottle.

--- Formula/coccinelle.rb
+++ Formula/coccinelle.rb
@@ -14,10 +14,10 @@ class Coccinelle < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "999cd887edc470102f1e318fbe59ad68a60786a8447a8e55ee453b5fe36f5008"
-    sha256 cellar: :any, big_sur:       "48c2002495d02950c1f6caa2b330d435d57f20ecda6a80b3912df5a11ae0c3b0"
-    sha256 cellar: :any, catalina:      "3705b898ce264c250741a0b464e0e87b53dcc658aa46ca82ae2e59a7bd19a00c"
-    sha256 cellar: :any, mojave:        "3bd2cb4ba4904b6eb84961a282d19ee8edb725af1ffd3b85ec378aaa873df8b0"
+    sha256 cellar: :any, arm64_big_sur: "b5ca24e58034e0508e39b726b79b5c357251d98e7901bd4b7f2e4ff2ad378cb0"
+    sha256 cellar: :any, big_sur:       "f2ae96a0c2ae00d31ba0c1014b960af9ac6e283559f4dad9e0576e0356bfc750"
+    sha256 cellar: :any, catalina:      "808ea650f39c39e91f0699864651762b4f5dbbb51e68c6a6503662b650509dae"
+    sha256 cellar: :any, mojave:        "90144f713b2f9b3658889689bfae1ce5e75aab916fbd0196edbfe3c5969567d5"
   end
 
   depends_on "autoconf" => :build
