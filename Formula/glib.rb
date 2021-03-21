3f7c96754c6767921ca209bea16c88e944c1feab
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2021-03-21 17:28:05 +0000

    glib: update patch url

--- Formula/glib.rb
+++ Formula/glib.rb
@@ -28,7 +28,7 @@ class Glib < Formula
 
   # replace several hardcoded paths with homebrew counterparts
   patch do
-    url "https://github.com/Homebrew/formula-patches/raw/c4d9edffc4fd5a28f0d3440a22e893632826ad20/glib/hardcoded-paths.diff"
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/43467fd8dfc0e8954892ecc08fab131242dca025/glib/hardcoded-paths.diff"
     sha256 "d81c9e8296ec5b53b4ead6917f174b06026eeb0c671dfffc4965b2271fb6a82c"
   end
 
