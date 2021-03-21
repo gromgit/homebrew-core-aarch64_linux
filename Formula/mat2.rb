2a665c33c1c849fe19fa6b2c55a342624543f53e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-21 12:29:01 +0000

    mat2: update 0.12.1 bottle.

--- Formula/mat2.rb
+++ Formula/mat2.rb
@@ -6,9 +6,9 @@ class Mat2 < Formula
   license "LGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "0ff6f3cef76685da4a59ef493eac3c45798fb55ccbb21ccbd564d471ea2945e3"
-    sha256 cellar: :any_skip_relocation, catalina: "e18191fd01e8af033fdecdbb88b07767cd423356bce770ac872c2de4dc2278fb"
-    sha256 cellar: :any_skip_relocation, mojave:   "f6fa5bad2b6f642c82736041a7e83d722ec988d124110d310da96063b6db4d21"
+    sha256 cellar: :any_skip_relocation, big_sur:  "2cf257a8aaf56858e195df29823cb3fdef6b25deb2c7df0a19d17e02044bfc05"
+    sha256 cellar: :any_skip_relocation, catalina: "6134535ba8f2fd9b654df05cb0398eb167140ff636e8988228791102227695c0"
+    sha256 cellar: :any_skip_relocation, mojave:   "130e341149d1005c85efc2b5421b133f8897b98913b27186ef1360c3de138726"
   end
 
   depends_on "exiftool"
