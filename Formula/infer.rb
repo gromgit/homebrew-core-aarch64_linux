0d7f66e852c80cdf18ef5add771ed6a9c589b9a8
Andrew V. Jones <andrewvaughanj@gmail.com>
2021-03-22 15:21:10 +0000

    infer 1.1.0
    Closes #73440.
    
    Signed-off-by: Andrew V. Jones <andrewvaughanj@gmail.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/infer.rb
+++ Formula/infer.rb
@@ -1,11 +1,10 @@
 class Infer < Formula
   desc "Static analyzer for Java, C, C++, and Objective-C"
   homepage "https://fbinfer.com/"
-  # pull from git tag to get submodules
-  url "https://github.com/facebook/infer.git",
-      tag:      "v0.17.0",
-      revision: "99464c01da5809e7159ed1a75ef10f60d34506a4"
+  url "https://github.com/facebook/infer/archive/v1.1.0.tar.gz"
+  sha256 "201c7797668a4b498fe108fcc13031b72d9dbf04dab0dc65dd6bd3f30e1f89ee"
   license "MIT"
+  head "https://github.com/facebook/infer.git"
 
   livecheck do
     url :stable
@@ -19,20 +18,16 @@ class Infer < Formula
     sha256 cellar: :any, high_sierra: "7630571f8e391ce0ba991ffe7a5d7b2b4a1029cda1d56497800d8ae0a260d4b6"
   end
 
-  deprecate! date: "2020-11-13", because: :does_not_build
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "cmake" => :build
   depends_on "libtool" => :build
-  depends_on "ocaml" => :build
-  depends_on "ocaml-findlib" => :build
-  depends_on "ocaml-num" => :build
+  depends_on "ninja" => :build
   depends_on "opam" => :build
-  depends_on "openjdk@8" => [:build, :test]
+  depends_on "openjdk@11" => [:build, :test]
   depends_on "pkg-config" => :build
+  depends_on "python@3.9" => :build
   depends_on "gmp"
-  depends_on :macos # Due to Python 2 (https://github.com/facebook/infer/issues/934)
   depends_on "mpfr"
   depends_on "sqlite"
 
@@ -42,19 +37,15 @@ class Infer < Formula
   uses_from_macos "xz"
   uses_from_macos "zlib"
 
-  # Remove camlp4 dependency, which is deprecated
-  # Addressed in 0.18.x
-  patch do
-    url "https://github.com/facebook/infer/commit/f52b5fc981c692776210d7eb9681c2b8c3117c93.patch?full_index=1"
-    sha256 "5487b9b39607c94821bede8d4f0ec2a0ed08d5213d5f048b1344819dac53b2f5"
-  end
-
   def install
     # needed to build clang
     ENV.permit_arch_flags
 
     # Apple's libstdc++ is too old to build LLVM
     ENV.libcxx if ENV.compiler == :clang
+
+    # Use JDK11
+    ENV["JAVA_HOME"] = Formula["openjdk@11"].opt_prefix
 
     opamroot = buildpath/"opamroot"
     opamroot.mkpath
@@ -70,46 +61,22 @@ class Infer < Formula
     # Let's try build clang faster
     ENV["JOBS"] = ENV.make_jobs.to_s
 
-    ENV["CLANG_CMAKE_ARGS"] = "-DLLVM_OCAML_INSTALL_PATH=#{`opam var lib`.chomp}/ocaml"
-
     # Release build
     touch ".release"
 
-    # Pin updated dependencies which are required to build on brew ocaml
-    # Remove from this when Infer updates their opam.locked to use at least these versions
-    pinned_deps = {
-      "mlgmpidl"  => "1.2.12",
-      "octavius"  => "1.2.1",
-      "parmap"    => "1.0-rc11",
-      "ppx_tools" => "5.3+4.08.0",
-    }
-    pinned_deps.each { |dep, ver| system "opam", "pin", "add", dep, ver, "--locked" }
-
-    # Unfortunately, opam can't cope if a system ocaml-num happens to be installed.
-    # Instead, we depend on Homebrew's ocaml-num and fool opam into using it.
-    # https://github.com/ocaml/opam-repository/issues/14646
-    system "opam", "pin", "add", "ocamlfind", Formula["ocaml-findlib"].version.to_s, "--locked", "--fake"
-    system "opam", "pin", "add", "num", Formula["ocaml-num"].version.to_s, "--locked", "--fake"
-
-    # Relax the dependency lock on a specific ocaml
-    # Also ignore anything we pinned above
-    ENV["OPAMIGNORECONSTRAINTS"] = "ocaml,ocamlfind,num,#{pinned_deps.keys.join(",")}"
-
-    # Remove ocaml-variants dependency (we won't be using it)
-    inreplace "opam.locked", /^ +"ocaml-variants" \{= ".*?"\}$\n/, ""
-
-    system "opam", "exec", "--", "./build-infer.sh", "all", "--yes", "--user-opam-switch"
-    system "opam", "exec", "--", "make", "install-with-libs"
+    system "./build-infer.sh", "all", "--yes"
+    system "make", "install-with-libs"
   end
 
   test do
+    ENV["JAVA_HOME"] = Formula["openjdk@11"].opt_prefix
+
     (testpath/"FailingTest.c").write <<~EOS
       #include <stdio.h>
 
       int main() {
         int *s = NULL;
         *s = 42;
-
         return 0;
       }
     EOS
@@ -122,13 +89,34 @@ class Infer < Formula
         if (s != NULL) {
           *s = 42;
         }
-
         return 0;
       }
     EOS
 
-    shell_output("#{bin}/infer --fail-on-issue -P -- clang -c FailingTest.c", 2)
-    shell_output("#{bin}/infer --fail-on-issue -P -- clang -c PassingTest.c")
+    no_issues_output = "\n  No issues found  \n"
+
+    failing_c_output = <<~EOS
+
+      FailingTest.c:5: error: Null Dereference
+      \  pointer `s` last assigned on line 4 could be null and is dereferenced at line 5, column 3.
+      \  3. int main() {
+      \  4.   int *s = NULL;
+      \  5.   *s = 42;
+      \       ^
+      \  6.   return 0;
+      \  7. }
+
+
+      Found 1 issue
+      \          Issue Type(ISSUED_TYPE_ID): #
+      \  Null Dereference(NULL_DEREFERENCE): 1
+    EOS
+
+    assert_equal failing_c_output.to_s,
+      shell_output("#{bin}/infer --fail-on-issue -P -- clang -c FailingTest.c", 2)
+
+    assert_equal no_issues_output.to_s,
+      shell_output("#{bin}/infer --fail-on-issue -P -- clang -c PassingTest.c")
 
     (testpath/"FailingTest.java").write <<~EOS
       class FailingTest {
@@ -164,7 +152,26 @@ class Infer < Formula
       }
     EOS
 
-    shell_output("#{bin}/infer --fail-on-issue -P -- javac FailingTest.java", 2)
-    shell_output("#{bin}/infer --fail-on-issue -P -- javac PassingTest.java")
+    failing_java_output = <<~EOS
+
+      FailingTest.java:12: error: Null Dereference
+      \  object `s` last assigned on line 11 could be null and is dereferenced at line 12.
+      \  10.     int mayCauseNPE() {
+      \  11.       String s = mayReturnNull(0);
+      \  12. >     return s.length();
+      \  13.     }
+      \  14.   }
+
+
+      Found 1 issue
+      \          Issue Type(ISSUED_TYPE_ID): #
+      \  Null Dereference(NULL_DEREFERENCE): 1
+    EOS
+
+    assert_equal failing_java_output.to_s,
+      shell_output("#{bin}/infer --fail-on-issue -P -- javac FailingTest.java", 2)
+
+    assert_equal no_issues_output.to_s,
+      shell_output("#{bin}/infer --fail-on-issue -P -- javac PassingTest.java")
   end
 end
