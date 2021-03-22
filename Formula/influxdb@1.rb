4d8076b8942ef30501c53683709227adc85c9008
Denis Pershin <positivemind@gmail.com>
2021-03-22 20:09:05 +0300

    influxdb@1 1.8.4 (new formula)
    Closes #73680.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

---
+++ Formula/influxdb@1.rb
@@ -0,0 +1,95 @@
+class InfluxdbAT1 < Formula
+  desc "Time series, events, and metrics database"
+  homepage "https://influxdata.com/time-series-platform/influxdb/"
+  url "https://github.com/influxdata/influxdb/archive/v1.8.4.tar.gz"
+  sha256 "9f2c135c8f9f50ca469196e6b4e575e26f1a338538788e71b664212e03b4df7b"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    regex(/^v?(1(?:\.\d+)+)$/i)
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.version=#{version}"
+
+    %w[influxd influx influx_tsm influx_stress influx_inspect].each do |f|
+      system "go", "build", "-ldflags", ldflags, *std_go_args, "-o", bin/f, "./cmd/#{f}"
+    end
+
+    etc.install "etc/config.sample.toml" => "influxdb.conf"
+    inreplace etc/"influxdb.conf" do |s|
+      s.gsub! "/var/lib/influxdb/data", "#{var}/influxdb/data"
+      s.gsub! "/var/lib/influxdb/meta", "#{var}/influxdb/meta"
+      s.gsub! "/var/lib/influxdb/wal", "#{var}/influxdb/wal"
+    end
+
+    (var/"influxdb/data").mkpath
+    (var/"influxdb/meta").mkpath
+    (var/"influxdb/wal").mkpath
+  end
+
+  plist_options manual: "influxd -config #{HOMEBREW_PREFIX}/etc/influxdb.conf"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>KeepAlive</key>
+          <dict>
+            <key>SuccessfulExit</key>
+            <false/>
+          </dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/influxd</string>
+            <string>-config</string>
+            <string>#{HOMEBREW_PREFIX}/etc/influxdb.conf</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>WorkingDirectory</key>
+          <string>#{var}</string>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/influxdb.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/influxdb.log</string>
+          <key>SoftResourceLimits</key>
+          <dict>
+            <key>NumberOfFiles</key>
+            <integer>10240</integer>
+          </dict>
+        </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    (testpath/"config.toml").write shell_output("#{bin}/influxd config")
+    inreplace testpath/"config.toml" do |s|
+      s.gsub! %r{/.*/.influxdb/data}, "#{testpath}/influxdb/data"
+      s.gsub! %r{/.*/.influxdb/meta}, "#{testpath}/influxdb/meta"
+      s.gsub! %r{/.*/.influxdb/wal}, "#{testpath}/influxdb/wal"
+    end
+
+    begin
+      pid = fork do
+        exec "#{bin}/influxd -config #{testpath}/config.toml"
+      end
+      sleep 6
+      output = shell_output("curl -Is localhost:8086/ping")
+      assert_match "X-Influxdb-Version:", output
+    ensure
+      Process.kill("SIGTERM", pid)
+      Process.wait(pid)
+    end
+  end
+end
