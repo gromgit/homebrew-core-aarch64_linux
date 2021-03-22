0accd628d6a1df5b7ca22f088976e6e2e249c368
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-22 20:20:05 +0000

    mydumper: update 0.10.3 bottle.

--- Formula/mydumper.rb
+++ Formula/mydumper.rb
@@ -6,10 +6,10 @@ class Mydumper < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "2a6e0ac0dc3666bd2b4968a3e860781b06972794124579cffa4d01f8f7aac0e5"
-    sha256 cellar: :any, big_sur:       "7d69132e5dc16095b3505e9e0d9a32d007ed59aaddc434080aca281be67b89f0"
-    sha256 cellar: :any, catalina:      "c4209a1c5683a68bbbf7ef285bd3426ccc356c0c4d3b605ebe239883b060e743"
-    sha256 cellar: :any, mojave:        "474f771d2bc3621402c21ffe07286f7a67b5e344209514ef63d55c1f2bf8b266"
+    sha256 cellar: :any, arm64_big_sur: "455f95559b2dbe864afba6e25ca4632c08ea01427d893e6b74da705f6ddc3df6"
+    sha256 cellar: :any, big_sur:       "cef3fb7db8b705a3aff4a08105637454015184774d0a967e5ad012a176e5c76f"
+    sha256 cellar: :any, catalina:      "dbd667367fe99a0b5ae562554dc8a8b83d6d66f53be8ab6279dbc52c920c7115"
+    sha256 cellar: :any, mojave:        "8a774ee31ebfcf66b836daeb7e45150040276ad0bde488a4699908bc17bf8663"
   end
 
   depends_on "cmake" => :build
