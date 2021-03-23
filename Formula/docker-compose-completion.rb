00a8c14a93c20b672c83f69797559cd30667eb22
rui <rui@chenrui.dev>
2021-03-23 16:58:32 +0000

    docker-compose-completion 1.28.6
    Closes #73759.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/docker-compose-completion.rb
+++ Formula/docker-compose-completion.rb
@@ -1,8 +1,8 @@
 class DockerComposeCompletion < Formula
   desc "Completion script for docker-compose"
   homepage "https://docs.docker.com/compose/completion/"
-  url "https://github.com/docker/compose/archive/1.28.5.tar.gz"
-  sha256 "41981d62e69e3bb2220a18306cebf9b2f564afe5f2b11f0cf8bba3bf0cf34938"
+  url "https://github.com/docker/compose/archive/1.28.6.tar.gz"
+  sha256 "873c5c94056be7cf4a10c9c94c862d51c339f9f03c4bc0e1752b74b969a94a1a"
   license "Apache-2.0"
   head "https://github.com/docker/compose.git"
 
