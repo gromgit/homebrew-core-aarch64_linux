7e25a595c7a348805507292c85f73db62145ec4d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-23 04:15:44 +0000

    blis: update 0.8.1 bottle.

--- Formula/blis.rb
+++ Formula/blis.rb
@@ -7,9 +7,9 @@ class Blis < Formula
   head "https://github.com/flame/blis.git"
 
   bottle do
-    sha256 cellar: :any, big_sur:  "2e70e19be54e6996848fdce52de417d9b657e8b1c73786d716e77aa2f55e31f8"
-    sha256 cellar: :any, catalina: "8cf27cca34ada36f300cfea6abc844a01644930b4a416edee439dd58ef04b36d"
-    sha256 cellar: :any, mojave:   "d5e0edecffeaff6b7ffb80d3e3657e68518d420e7b9f1517cf98539f297ec096"
+    sha256 cellar: :any, big_sur:  "ad2e6862fd4b5a425769c108e7a36e33ac7e7fc77ce699756fe051e68524518d"
+    sha256 cellar: :any, catalina: "b26e5e7deb7b85319fa539a061ff84df842378a902e3695a4f6df63eba9f5cdb"
+    sha256 cellar: :any, mojave:   "333cceec593098d68f438ddcfc6415d44cf0af565601c0163496e23bdf4a8aec"
   end
 
   def install
