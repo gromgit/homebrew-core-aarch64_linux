0174dece5e2656a743d56588ceea3c65850b6a20
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-23 03:40:23 +0000

    pyqt-networkauth 6.0.3
    * pyqt-networkauth 6.0.3
    * pyqt-networkauth: update pyproject.toml
    
    Closes #73717.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pyqt-networkauth.rb
+++ Formula/pyqt-networkauth.rb
@@ -1,8 +1,8 @@
 class PyqtNetworkauth < Formula
   desc "Python bindings for The Qt Company’s Qt Network Authorization library"
   homepage "https://www.riverbankcomputing.com/software/pyqtnetworkauth"
-  url "https://files.pythonhosted.org/packages/56/7e/a400ce770ca83194cca6a129ca6e8339b2ec71b5dbe03fd8f1570be385e0/PyQt6_NetworkAuth-6.0.2.tar.gz"
-  sha256 "ba3c55dc7ce7f4b9034f8675c5fe8d37830d986716ff2ef0cca29ee80240f609"
+  url "https://files.pythonhosted.org/packages/21/de/a7c4ef992a66be7ee458ab25ad7cdf93483712517ba807a74c30dc7c9375/PyQt6_NetworkAuth-6.0.3.tar.gz"
+  sha256 "ae3c1540e9504eea024ee4da13c6ead146f987aa5fe1942c8d7465ef631e8ba8"
   license "GPL-3.0-only"
 
   bottle do
@@ -23,10 +23,11 @@ class PyqtNetworkauth < Formula
     pyqt = Formula["pyqt"]
     xy = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
 
-    inreplace "pyproject.toml", "[tool.sip.project]", <<~EOS
-      [tool.sip.project]
-      sip-include-dirs = ["#{pyqt.opt_lib}/python#{xy}/site-packages/PyQt#{pyqt.version.major}/bindings"]
-    EOS
+    open("pyproject.toml", "a") do |f|
+      f.puts "[tool.sip.project]"
+      f.puts "sip-include-dirs = [\"#{pyqt.opt_lib}/python#{xy}/site-packages/PyQt#{pyqt.version.major}/bindings\"]"
+    end
+
     system "sip-install", "--target-dir", prefix
     (lib/"python#{xy}/site-packages").install %W[#{prefix}/PyQt#{pyqt.version.major} #{prefix}/PyQt#{pyqt.version.major}_NetworkAuth-#{version}.dist-info]
   end
