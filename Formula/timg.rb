061148726094e6830f84e7417594693a9cefe07d
Daniel Seiler <ds@ds-m1air.lan>
2021-03-23 19:23:21 +0100

    timg 1.2.1 (new formula)
    Add formula for timg to homebrew-core.
    Timg is a terminal image and video viewer.
    
    Closes #73770.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

---
+++ Formula/timg.rb
@@ -0,0 +1,29 @@
+class Timg < Formula
+  desc "Terminal image and video viewer"
+  homepage "https://timg.sh/"
+  url "https://github.com/hzeller/timg/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "2ef2e1b79c3bf82e951ad35628bc0387fe8260052614a213385de82fcfa901ec"
+  license "GPL-2.0-only"
+  head "https://github.com/hzeller/timg.git", branch: "main"
+
+  depends_on "cmake" => :build
+  depends_on "ffmpeg"
+  depends_on "graphicsmagick"
+  depends_on "jpeg-turbo"
+  depends_on "libexif"
+  depends_on "libpng"
+  depends_on "webp"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/timg", "--version"
+    system "#{bin}/timg", "-g10x10", test_fixtures("test.gif")
+    system "#{bin}/timg", "-g10x10", test_fixtures("test.png")
+    system "#{bin}/timg", "-pq", "-g10x10", "-o", testpath/"test-output.txt", test_fixtures("test.jpg")
+    assert_match "38;2;255;38;0;49m", (testpath/"test-output.txt").read
+  end
+end
