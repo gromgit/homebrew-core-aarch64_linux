59f7c471509280e9bd77ebae549c7ff9a7c78190
rui <rui@chenrui.dev>
2021-03-23 16:33:47 +0000

    xtensor 0.23.3
    Closes #73751.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/xtensor.rb
+++ Formula/xtensor.rb
@@ -1,8 +1,8 @@
 class Xtensor < Formula
   desc "Multi-dimensional arrays with broadcasting and lazy computing"
   homepage "https://xtensor.readthedocs.io/en/latest/"
-  url "https://github.com/QuantStack/xtensor/archive/0.23.2.tar.gz"
-  sha256 "fde26dcf93f5d95996b8cc7e556b84930af41ff699492b7b20b2e3335e12f862"
+  url "https://github.com/QuantStack/xtensor/archive/0.23.3.tar.gz"
+  sha256 "97c43372b6bd1634b6d647a4b318fae541d0c305bac9ec299d3d1bd42790d1f2"
   license "BSD-3-Clause"
 
   bottle do
