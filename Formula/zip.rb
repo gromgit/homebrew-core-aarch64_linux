d41f52b7added9d8be33aedea572968228b2758b
Michka Popoff <michkapopoff@gmail.com>
2021-03-24 13:19:14 +0100

    zip: disable 2 tests on Linux to avoid a circular dependency
    zip depends on unzip; and unzip depends on zip for their respective tests

--- Formula/zip.rb
+++ Formula/zip.rb
@@ -54,11 +54,17 @@ class Zip < Formula
 
     system "#{bin}/zip", "test.zip", "test1", "test2", "test3"
     assert_predicate testpath/"test.zip", :exist?
-    assert_match "test of test.zip OK", shell_output("#{bin}/zip -T test.zip")
+    on_macos do
+      # zip -T needs unzip, disabled under Linux to avoid a circular dependency
+      assert_match "test of test.zip OK", shell_output("#{bin}/zip -T test.zip")
+    end
 
     # test bzip2 support that should be automatically linked in using the bzip2 library in macOS
     system "#{bin}/zip", "-Z", "bzip2", "test2.zip", "test1", "test2", "test3"
     assert_predicate testpath/"test2.zip", :exist?
-    assert_match "test of test2.zip OK", shell_output("#{bin}/zip -T test2.zip")
+    on_macos do
+      # zip -T needs unzip, disabled under Linux to avoid a circular dependency
+      assert_match "test of test2.zip OK", shell_output("#{bin}/zip -T test2.zip")
+    end
   end
 end
