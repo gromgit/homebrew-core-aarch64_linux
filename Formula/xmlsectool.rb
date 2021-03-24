3ea67957004027b62ca8627d0a3e7a3563d41caa
Bo Anderson <mail@boanderson.me>
2021-03-24 14:54:00 +0000

    xmlsectool: update homepage

--- Formula/xmlsectool.rb
+++ Formula/xmlsectool.rb
@@ -1,6 +1,6 @@
 class Xmlsectool < Formula
   desc "Check schema validity and signature of an XML document"
-  homepage "https://wiki.shibboleth.net/confluence/display/XSTJ3/Home"
+  homepage "https://wiki.shibboleth.net/confluence/display/XSTJ3/xmlsectool+V3+Home"
   url "https://shibboleth.net/downloads/tools/xmlsectool/3.0.0/xmlsectool-3.0.0-bin.zip"
   sha256 "5b430dda0bf78df224495b39f83cf043d96c0ab9c8ccaa23fbdb56068c46abbc"
   license "Apache-2.0"
