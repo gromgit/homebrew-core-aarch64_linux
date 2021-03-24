cf86323aae91a44a6bc5b2b2a52c5e76a6206e1e
Michka Popoff <michkapopoff@gmail.com>
2021-03-24 21:29:40 +0100

    postgresql: add linux-only dependencies

--- Formula/postgresql.rb
+++ Formula/postgresql.rb
@@ -31,9 +31,11 @@ class Postgresql < Formula
 
   uses_from_macos "libxml2"
   uses_from_macos "libxslt"
+  uses_from_macos "openldap"
   uses_from_macos "perl"
 
   on_linux do
+    depends_on "linux-pam"
     depends_on "util-linux"
   end
 
