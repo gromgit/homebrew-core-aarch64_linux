c551d515f1694da61ae507b51c668bce312e2e60
Michka Popoff <michkapopoff@gmail.com>
2021-03-24 21:20:40 +0100

    dovecot: add linux-only dependency

--- Formula/dovecot.rb
+++ Formula/dovecot.rb
@@ -18,8 +18,13 @@ class Dovecot < Formula
   end
 
   depends_on "openssl@1.1"
+
   uses_from_macos "bzip2"
   uses_from_macos "sqlite"
+
+  on_linux do
+    depends_on "linux-pam"
+  end
 
   resource "pigeonhole" do
     url "https://pigeonhole.dovecot.org/releases/2.3/dovecot-2.3-pigeonhole-0.5.14.tar.gz"
