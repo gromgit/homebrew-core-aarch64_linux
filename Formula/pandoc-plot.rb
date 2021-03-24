57280f25b8777d4a4e0a3d2e2bacf135d95d492a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-24 11:42:58 +0000

    pandoc-plot: add 1.1.1 bottle.

--- Formula/pandoc-plot.rb
+++ Formula/pandoc-plot.rb
@@ -5,6 +5,12 @@ class PandocPlot < Formula
   sha256 "00b67fda0d7faa2da6a149d4e69d1f547036ba5dd55f4bbbacf0ce714f649c83"
   license "GPL-2.0-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, big_sur:  "916e4cb6d7a0ebbacc030cb93d831ec9284bfe3c266bb97b7a292fa8c23eef7d"
+    sha256 cellar: :any_skip_relocation, catalina: "8cbfc7637c688db43a4ee9a879ed8b37360b7069e8ab72441a42b60a9f1d992e"
+    sha256 cellar: :any_skip_relocation, mojave:   "faafca14fbfeed888d05ae4a715b1b57c386511ee9f9006e405a3a58cf50e5a3"
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
   depends_on "graphviz" => :test
