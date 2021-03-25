7b0e4ce7d11967baf8e0a14aba60c1f9a3ffc43a
Michka Popoff <michkapopoff@gmail.com>
2021-03-25 18:52:21 +0100

    libdv: set mac-specific flag only on macOS

--- Formula/libdv.rb
+++ Formula/libdv.rb
@@ -20,12 +20,14 @@ class Libdv < Formula
   depends_on "popt"
 
   def install
-    # This fixes an undefined symbol error on compile.
-    # See the port file for libdv:
-    #   https://trac.macports.org/browser/trunk/dports/multimedia/libdv/Portfile
-    # This flag is the preferred method over what macports uses.
-    # See the apple docs: https://cl.ly/2HeF bottom of the "Finding Imported Symbols" section
-    ENV.append "LDFLAGS", "-undefined dynamic_lookup"
+    on_macos do
+      # This fixes an undefined symbol error on compile.
+      # See the port file for libdv:
+      #   https://trac.macports.org/browser/trunk/dports/multimedia/libdv/Portfile
+      # This flag is the preferred method over what macports uses.
+      # See the apple docs: https://cl.ly/2HeF bottom of the "Finding Imported Symbols" section
+      ENV.append "LDFLAGS", "-undefined dynamic_lookup"
+    end
 
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
