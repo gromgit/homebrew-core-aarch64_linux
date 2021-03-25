9c980f85467ebb7d654a8780185562c585af06be
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-25 02:18:54 +0000

    timg: add 1.2.1 bottle.

--- Formula/timg.rb
+++ Formula/timg.rb
@@ -6,6 +6,13 @@ class Timg < Formula
   license "GPL-2.0-only"
   head "https://github.com/hzeller/timg.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any, arm64_big_sur: "01c5028b5b8342cf87366b90bef3b9d24485e09014e0c77350ac54012620804c"
+    sha256 cellar: :any, big_sur:       "ece0ca4585f66286c254d61192c1cedb8e0627fdc6c784115bea1eb4e1bf1ce5"
+    sha256 cellar: :any, catalina:      "918cb24b877c141e37813aa406d0fdc82f0d031a98966f880f9fa5948bfe73e2"
+    sha256 cellar: :any, mojave:        "417fb0934de8ff856fcf9b0f70b360734c874ef6ebb921fff283e76622ce5f68"
+  end
+
   depends_on "cmake" => :build
   depends_on "ffmpeg"
   depends_on "graphicsmagick"
