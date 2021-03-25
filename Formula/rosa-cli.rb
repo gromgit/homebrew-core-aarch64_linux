1abcc90e5f6d7a8588f0f2ba6092d8b6498498ce
Gustavo Veloso <gjmveloso@users.noreply.github.com>
2021-03-25 12:39:04 -0300

    rosa-cli 1.0.2 (new formula)
    Closes #73918.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

---
+++ Formula/rosa-cli.rb
@@ -0,0 +1,27 @@
+class RosaCli < Formula
+  desc "RedHat OpenShift Service on AWS (ROSA) command-line interface"
+  homepage "https://www.openshift.com/products/amazon-openshift"
+  url "https://github.com/openshift/rosa/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "e2bc5f03e5d295cb32b9b45523790572601823386444d4c4e6c86c99ef8248ee"
+  license "Apache-2.0"
+  head "https://github.com/openshift/rosa.git"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+    regex(%r{href=.*?/tag/v?(\d+\.\d+\.\d+)["' >]}i)
+  end
+
+  depends_on "go" => :build
+  depends_on "awscli"
+
+  def install
+    system "go", "build", *std_go_args, "-o", bin/"rosa", "./cmd/rosa"
+    (bash_completion/"rosa").write Utils.safe_popen_read("#{bin}/rosa", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/rosa version")
+    assert_match "Error creating AWS client", shell_output("#{bin}/rosa create cluster 2<&1", 1)
+  end
+end
