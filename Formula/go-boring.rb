832004d043dd76301cb909e604944d5bb81a3bff
Di Wu <di.wu@snowflake.com>
2021-03-25 15:58:25 -0700

    go-boring 1.16.2b7 (new formula)
    Closes #73952.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

---
+++ Formula/go-boring.rb
@@ -0,0 +1,80 @@
+class GoBoring < Formula
+  desc "Go programming language with BoringCrypto"
+  homepage "https://go.googlesource.com/go/+/dev.boringcrypto/README.boringcrypto.md"
+  url "https://go-boringcrypto.storage.googleapis.com/go1.16.2b7.src.tar.gz"
+  version "1.16.2b7"
+  sha256 "8991c28e30bdd9b34076c2871ab69aac7f306dba08d202aa7301f3adb54ad9f3"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url "https://go-boringcrypto.storage.googleapis.com/"
+    regex(/>go[._-]?(\d+(?:\.\d+)+b\d+)[._-]src\.t/i)
+  end
+
+  keg_only "it conflicts with the Go formula"
+
+  # Don't update this unless this version cannot bootstrap the new version.
+  resource "gobootstrap" do
+    on_macos do
+      if Hardware::CPU.arm?
+        url "https://storage.googleapis.com/golang/go1.16.darwin-arm64.tar.gz"
+        version "1.16"
+        sha256 "4dac57c00168d30bbd02d95131d5de9ca88e04f2c5a29a404576f30ae9b54810"
+      else
+        url "https://storage.googleapis.com/golang/go1.16.darwin-amd64.tar.gz"
+        version "1.16"
+        sha256 "6000a9522975d116bf76044967d7e69e04e982e9625330d9a539a8b45395f9a8"
+      end
+    end
+
+    on_linux do
+      url "https://storage.googleapis.com/golang/go1.16.linux-amd64.tar.gz"
+      version "1.16"
+      sha256 "013a489ebb3e24ef3d915abe5b94c3286c070dfe0818d5bca8108f1d6e8440d2"
+    end
+  end
+
+  def install
+    (buildpath/"gobootstrap").install resource("gobootstrap")
+    ENV["GOROOT_BOOTSTRAP"] = buildpath/"gobootstrap"
+
+    cd "src" do
+      ENV["GOROOT_FINAL"] = libexec
+      system "./make.bash", "--no-clean"
+    end
+
+    (buildpath/"pkg/obj").rmtree
+    rm_rf "gobootstrap" # Bootstrap not required beyond compile.
+    libexec.install Dir["*"]
+    bin.install_symlink Dir[libexec/"bin/go*"]
+
+    system bin/"go", "install", "-race", "std"
+
+    # Remove useless files.
+    # Breaks patchelf because folder contains weird debug/test files
+    Dir.glob(libexec/"**/testdata").each { |testdata| rm_rf testdata }
+  end
+
+  test do
+    (testpath/"hello.go").write <<~EOS
+      package main
+
+      import (
+          "fmt"
+          _ "crypto/tls/fipsonly"
+      )
+
+      func main() {
+          fmt.Println("Hello World")
+      }
+    EOS
+    # Run go fmt check for no errors then run the program.
+    # This is a a bare minimum of go working as it uses fmt, build, and run.
+    system bin/"go", "fmt", "hello.go"
+    assert_equal "Hello World\n", shell_output("#{bin}/go run hello.go")
+
+    ENV["GOOS"] = "freebsd"
+    ENV["GOARCH"] = "amd64"
+    system bin/"go", "build", "hello.go"
+  end
+end
