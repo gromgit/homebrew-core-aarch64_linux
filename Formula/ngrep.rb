52216b80fb9ab69ffda920354f6d5cfbddff1d7d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-26 09:58:56 +0000

    Revert "ngrep: update 1.47 bottle."
    This reverts commit 8e19617be0384bf054b92a15cb2e709b8766b45c.

--- Formula/ngrep.rb
+++ Formula/ngrep.rb
@@ -6,11 +6,14 @@ class Ngrep < Formula
   license :cannot_represent # Described as 'BSD with advertising' here: https://src.fedoraproject.org/rpms/ngrep/blob/rawhide/f/ngrep.spec#_8
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9748a4ae1ac8cd7fb8284cd40bf6e01ce11dfafea0cdc5f3e50ebf59b80ca076"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3e3dd83416ffb0136a2e26ed7f98c8e0243914966791ec259ecdd2d835f73db2"
-    sha256 cellar: :any_skip_relocation, catalina:      "b53ca835c8c61966f89d5bb77444182b59353896217e997f038ed95387fa3baf"
-    sha256 cellar: :any_skip_relocation, mojave:        "7ac3968d637cc2ae7f6dbc34f0001096a86a3bbc64a2bf79213cf4eac47b41e5"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5bc88f61eaba46026963de70f44e84f73f04b041e913051fb21f3351d16cd9e4"
+    sha256 cellar: :any_skip_relocation, big_sur:       "6ab0d459dad3462b127af805de369dac2f099844126d70e89e531ea181d0e794"
+    sha256 cellar: :any_skip_relocation, catalina:      "53bf6d68b15a2f07a01d828cdcd137131a45871141da411328c376ed90768265"
+    sha256 cellar: :any_skip_relocation, mojave:        "0d1948b2fbf7c60fb6e46f15d32a51a3f7754e7372924e4e984cce98282ca281"
+    sha256 cellar: :any_skip_relocation, high_sierra:   "390424274552105e21b3f3e926b933322a09333cee02274d2f84a5e23f4ea74d"
+    sha256 cellar: :any_skip_relocation, sierra:        "0e915d1e3b7e7da8e58a48457de4e40359cb0f870eb45a77302d36c1b767d044"
+    sha256 cellar: :any_skip_relocation, el_capitan:    "d057c167d3b695ff915c13fd39e3cd7b3e6e2a5b3f82bce6bb8ea4c030e8f6e7"
   end
 
   uses_from_macos "libpcap"
