8daf41db67cbc058325f69a57f4fac5bcd54cf01
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-27 03:13:00 +0000

    okteto: update 1.12.1 bottle.

--- Formula/okteto.rb
+++ Formula/okteto.rb
@@ -7,10 +7,10 @@ class Okteto < Formula
   head "https://github.com/okteto/okteto.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "925e016b0da4ebd3723ef3afdc8dd0c801bf38caf19284bc71a5fdd661156cf4"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ea15b209c4b8820b9be22f7215ece4efb6de57563d42bbcbf2754080596ba2be"
-    sha256 cellar: :any_skip_relocation, catalina:      "6135bdbbb6fa94a8c1c2878e6ca7dda454ead162788aa7f47cd3599096ea679e"
-    sha256 cellar: :any_skip_relocation, mojave:        "76473ae02eaeed13b84a18ae37d3953ba06ef21d8267b09cc47ba5fca2513788"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d754232e4c9ba6e2704450f42f180f5776d4ccb0e5c7dfdd6926977493e69410"
+    sha256 cellar: :any_skip_relocation, big_sur:       "9048b3a4f91c9d67141cbcfc40172eedab6368f6e1f9833d417d4cfae111a7b5"
+    sha256 cellar: :any_skip_relocation, catalina:      "d1bbea1d6e8120ad7b510c8763cd40b16be4d1fbed9b9fb4d605a358d2ed1aad"
+    sha256 cellar: :any_skip_relocation, mojave:        "11043dba4c04b617f742c7f5c8c5328e2d7e3afc6ed963b29182d1920b96f030"
   end
 
   depends_on "go" => :build
