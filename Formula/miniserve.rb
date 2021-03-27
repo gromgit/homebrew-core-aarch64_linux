3169f06383cadd954369719646a3cf23ae7f405b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-27 05:10:10 +0000

    miniserve: update 0.12.1 bottle.

--- Formula/miniserve.rb
+++ Formula/miniserve.rb
@@ -6,10 +6,10 @@ class Miniserve < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1ab58551c5fe4542ff3f35cb6d60e2837c58cda0408f0a415f5f4ac089c948a3"
-    sha256 cellar: :any_skip_relocation, big_sur:       "177f3ab786153b2cbcb28b39425922b08cd30615cf063c957213d49e68011496"
-    sha256 cellar: :any_skip_relocation, catalina:      "bef63b785a98fcbef2344999a8f9ae7154835a309190103536859901aa1d8608"
-    sha256 cellar: :any_skip_relocation, mojave:        "c430415f1a9db9e193586ef4d1b48eed504d5ee8382334d521a18e0b5384086f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ab0f48f372b54392e6b83e71f830d988e0137db522011635a843039745e5d694"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a8cbe41787db5cde1a0b47a4e67cb1f8d97eac9d64cf8270206e36541c504630"
+    sha256 cellar: :any_skip_relocation, catalina:      "241d099eb1913e5ee44a5f7f69dfd7d67a2953672438ab87f3f3b378c81f4ea4"
+    sha256 cellar: :any_skip_relocation, mojave:        "4b9ac6c29ca4d042f195d45934994a7d7a74c897324f4ca4ba17dda1601301f9"
   end
 
   depends_on "rust" => :build
