244d3615badf9aa3a74aceeb678277e9606c3e78
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-27 02:24:24 +0000

    eslint: update 7.23.0 bottle.

--- Formula/eslint.rb
+++ Formula/eslint.rb
@@ -8,10 +8,10 @@ class Eslint < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9b0fdc8afbfa7d2bb86217c8c8cd003cc81f737b4d62c99dd2cfd8fb9320caca"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a554e0b8b8ba515425a87298ec942bff2dca6ecbde84a986f04671ba1ebc5419"
-    sha256 cellar: :any_skip_relocation, catalina:      "4f51b1896ec9b882f119794a451f4f0efafc5587e3a55b96c14f58a0e6d9cbbb"
-    sha256 cellar: :any_skip_relocation, mojave:        "244ed207ea838793369aa8f442a48020df32f4237f80a188c1a18a729bcaf3cf"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "64458615bb4ebbbe500b0efb985698bc7f78d609ef7faf2523c0c430a981023e"
+    sha256 cellar: :any_skip_relocation, big_sur:       "6d682f337247873d37d6483e97b9543fa9eb58d969ac414781434f6e2845ba35"
+    sha256 cellar: :any_skip_relocation, catalina:      "6b531eba34aa5107936c8d067900db1cc003b0580487ac7c2c4126e458bf75fd"
+    sha256 cellar: :any_skip_relocation, mojave:        "b2d83e96e2f745e38a7d2a0fff9d7b2f07836b5d211a8448ebe8412d6dd6fffa"
   end
 
   depends_on "node"
