07410bb3999ee377a58a73defaf6e53af792564f
rui <rui@chenrui.dev>
2021-03-27 16:08:06 +0000

    libvmaf 2.1.1
    * libvmaf 2.1.1
    * libvmaf: remove patch
    
    Closes #74031.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/libvmaf.rb
+++ Formula/libvmaf.rb
@@ -1,8 +1,8 @@
 class Libvmaf < Formula
   desc "Perceptual video quality assessment based on multi-method fusion"
   homepage "https://github.com/Netflix/vmaf"
-  url "https://github.com/Netflix/vmaf/archive/v1.5.3.tar.gz"
-  sha256 "440652ae417d88be083ffd9fa5967662172601e31c458a9743f6008d7150c900"
+  url "https://github.com/Netflix/vmaf/archive/v2.1.1.tar.gz"
+  sha256 "e7fc00ae1322a7eccfcf6d4f1cdf9c67eec8058709887c8c6c3795c617326f77"
   license "BSD-2-Clause-Patent"
 
   bottle do
@@ -16,13 +16,6 @@ class Libvmaf < Formula
   depends_on "meson" => :build
   depends_on "nasm" => :build
   depends_on "ninja" => :build
-
-  # Upstream patch for Xcode 12, remove in next version
-  # https://github.com/Netflix/vmaf/pull/676
-  patch do
-    url "https://github.com/Netflix/vmaf/commit/b7851292.patch?full_index=1"
-    sha256 "686a01b0cc0f6b0e07a12964492e7702ac0b54cc92f5370f1a31d44fd0855ced"
-  end
 
   def install
     Dir.chdir("libvmaf") do
