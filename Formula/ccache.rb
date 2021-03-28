548d173ac19ff608f94d14583318d47f1cab8edc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-28 00:17:52 +0000

    ccache: update 4.2.1 bottle.

--- Formula/ccache.rb
+++ Formula/ccache.rb
@@ -7,10 +7,10 @@ class Ccache < Formula
   head "https://github.com/ccache/ccache.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "582e21a87c6025f4de138f2a5b47a14a0487f2e6deabf4dd54cfb0d34d27190b"
-    sha256 cellar: :any, big_sur:       "4323fd450d0e58cb7c4b76d5254e5a5b44d960d5216073dfeeda41e9baf298f3"
-    sha256 cellar: :any, catalina:      "e84ad1c22e01e75f740c910f562935c2d00058aaf4e8bbd09050dfee18f45324"
-    sha256 cellar: :any, mojave:        "143ee0131253764d489ba4ff6569ec565c50d50662d878065bdf44d290d23c2b"
+    sha256 cellar: :any, arm64_big_sur: "035a9c51376fbd87424fd98b75e304ca361b3bd0dc92bdcae7b1e5dcfa22a3c0"
+    sha256 cellar: :any, big_sur:       "098a882c947780055053e5d961910f411026cea492a0435077b3c70a347b4ca7"
+    sha256 cellar: :any, catalina:      "fdcfcc5d633cbae1334d7862f92e00cfaca04bc1722299b37acd61647779325b"
+    sha256 cellar: :any, mojave:        "ffc07ff2a5079f66f770534bf9d0db2cbd330e8a6bd560f2e4509866be09aad9"
   end
 
   depends_on "cmake" => :build
