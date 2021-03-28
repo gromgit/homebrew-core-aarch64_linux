1b40ec98534dbe2138e75b41359c0f2beaef7122
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-03-28 01:29:04 +0000

    k9s: update 0.24.6 bottle.

--- Formula/k9s.rb
+++ Formula/k9s.rb
@@ -8,10 +8,10 @@ class K9s < Formula
   head "https://github.com/derailed/k9s.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "85d6dc47f2b29749c9ce43d80299a5ff38a5b5b1732d1ae165bcf6d2437ef1f0"
-    sha256 cellar: :any_skip_relocation, big_sur:       "74b8b7ba0ce48f14f1e3b970e02f0f9b7cf44063ad29cdf62db0cc7161be1164"
-    sha256 cellar: :any_skip_relocation, catalina:      "6eeaca5f6841241186701c085a8ca7287e49e3917d435a18b03d332496d41481"
-    sha256 cellar: :any_skip_relocation, mojave:        "a669301e9599d1e17e5ef69ca2ce792e9e48e01da66318a65412db17b99e7cf4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "56a22471606a3514eb4f65a23492f199e3876459da202811016f7e59c1ee66e0"
+    sha256 cellar: :any_skip_relocation, big_sur:       "872f1f83bbc5e51247f28393c26b1f8d7774e8a4086855736810076c17398f23"
+    sha256 cellar: :any_skip_relocation, catalina:      "af37a22da0b088ed01df459e3dcd2ef4b952eb0998132ae8fad75b2367b8a191"
+    sha256 cellar: :any_skip_relocation, mojave:        "06b633993e49dea023f1904d6ac5dc22e702b9376914aba6861feb87e702cc09"
   end
 
   depends_on "go" => :build
