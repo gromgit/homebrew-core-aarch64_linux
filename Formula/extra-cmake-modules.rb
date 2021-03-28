5aaad37a0fd0b6bfeb5b3e6d5d5e544315724a8e
Bo Anderson <mail@boanderson.me>
2021-03-28 04:01:32 +0100

    extra-cmake-modules 5.80.0

--- Formula/extra-cmake-modules.rb
+++ Formula/extra-cmake-modules.rb
@@ -1,10 +1,9 @@
 class ExtraCmakeModules < Formula
   desc "Extra modules and scripts for CMake"
   homepage "https://api.kde.org/frameworks/extra-cmake-modules/html/index.html"
-  url "https://download.kde.org/stable/frameworks/5.79/extra-cmake-modules-5.79.0.tar.xz"
-  sha256 "b29602db99c566d88fa92106abe114bd57b7ffc6ca20773426f896ffde68bed8"
+  url "https://download.kde.org/stable/frameworks/5.80/extra-cmake-modules-5.80.0.tar.xz"
+  sha256 "2370fd80f685533d0b96efa6fa443ceea68e0ceba4e8a9d7c151d297b1c96f64"
   license all_of: ["BSD-2-Clause", "BSD-3-Clause", "MIT"]
-  revision 1
   head "https://invent.kde.org/frameworks/extra-cmake-modules.git"
 
   # We check the tags from the `head` repository because the latest stable
