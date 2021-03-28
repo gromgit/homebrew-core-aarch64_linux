f1b927a2930042933b57a6d1de352d5911d48753
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-28 05:46:20 +0000

    leaf-proxy: add 0.2.13 bottle.

--- Formula/leaf-proxy.rb
+++ Formula/leaf-proxy.rb
@@ -6,6 +6,13 @@ class LeafProxy < Formula
   license "Apache-2.0"
   head "https://github.com/eycorsican/leaf.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5cf0070ceacfd72e04d6adbd77851b1c15ec01ff79f2add28407a8452f5bb2b3"
+    sha256 cellar: :any_skip_relocation, big_sur:       "d95b88877ccd9d3a77758887f0696fffa46b23ae492960a1716005b2349e4bef"
+    sha256 cellar: :any_skip_relocation, catalina:      "a0ee193feb50ee14874647967eaca61de66b267265ee5035a24047ad8ebb30fa"
+    sha256 cellar: :any_skip_relocation, mojave:        "8c8694440e773387ed663a650f8609aa27ec5c454166337e3f09941803292b91"
+  end
+
   depends_on "rust" => :build
 
   conflicts_with "leaf", because: "both install a `leaf` binary"
