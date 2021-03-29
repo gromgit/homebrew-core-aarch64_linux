78626362d33cd8409c9055b556f1f379d8ded2a4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-29 08:48:03 +0100

    jnethack 3.6.6-0.2
    Closes #74103.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/jnethack.rb
+++ Formula/jnethack.rb
@@ -6,9 +6,9 @@ class Jnethack < Formula
   homepage "https://jnethack.osdn.jp/"
   # We use a git checkout to avoid patching the upstream NetHack tarball.
   url "https://scm.osdn.net/gitroot/jnethack/source.git",
-      tag:      "v3.6.6-0.1",
-      revision: "0ffd620440b5b61e21b40bf32e148d20c0c8349f"
-  version "3.6.6-0.1"
+      tag:      "v3.6.6-0.2",
+      revision: "1dff0026832d47d3c93ff263afad233cced3a387"
+  version "3.6.6-0.2"
   license "NGPL"
   head "https://github.com/jnethack/jnethack-alpha.git", branch: "develop"
 
