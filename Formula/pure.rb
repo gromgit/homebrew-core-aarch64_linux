07a8d506fd23a28f7401ce4282000a44339740ba
Daniel Bayley <daniel.bayley@me.com>
2021-03-29 05:54:37 -0700

    pure 1.15.0 (new formula) (#73820)

---
+++ Formula/pure.rb
@@ -0,0 +1,21 @@
+class Pure < Formula
+  desc "Pretty, minimal and fast ZSH prompt"
+  homepage "https://github.com/sindresorhus/pure"
+  url "https://github.com/sindresorhus/pure/archive/v1.16.0.tar.gz"
+  sha256 "152ffa2cffb6b79e10b78c0fb1de0b8461bae87822439d10ea9c749836060eca"
+  license "MIT"
+
+  bottle :unneeded
+
+  depends_on "zsh" => :test
+  depends_on "zsh-async"
+
+  def install
+    zsh_function.install "pure.zsh" => "prompt_pure_setup"
+  end
+
+  test do
+    zsh_command = "setopt prompt_subst; autoload -U promptinit; promptinit && prompt -p pure"
+    assert_match "❯", shell_output("zsh -c '#{zsh_command}'")
+  end
+end
