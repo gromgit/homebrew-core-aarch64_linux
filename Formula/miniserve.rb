4536a9e0cac91ebcc9cab7dcf56046b2b3fe9ac5
MoonFruit <dkmoonfruit@gmail.com>
2021-03-29 10:42:21 +0800

    miniserve: add shell completions
    Closes #74082.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/miniserve.rb
+++ Formula/miniserve.rb
@@ -4,6 +4,7 @@ class Miniserve < Formula
   url "https://github.com/svenstaro/miniserve/archive/v0.13.0.tar.gz"
   sha256 "3578fd2dfe8dbebecd15b1e82cfb6d6656fed5e54ae4fccc4e7a6879b61dd1e1"
   license "MIT"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "42d87b45cbb447afb5a429878eebfa980a8b984ecd2bf772127973525e401d48"
@@ -16,6 +17,13 @@ class Miniserve < Formula
 
   def install
     system "cargo", "install", *std_cargo_args
+
+    bash_output = Utils.safe_popen_read("#{bin}/miniserve", "--print-completions", "bash")
+    (bash_completion/"miniserve").write bash_output
+    zsh_output = Utils.safe_popen_read("#{bin}/miniserve", "--print-completions", "zsh")
+    (zsh_completion/"_miniserve").write zsh_output
+    fish_output = Utils.safe_popen_read("#{bin}/miniserve", "--print-completions", "fish")
+    (fish_completion/"miniserve.fish").write fish_output
   end
 
   test do
