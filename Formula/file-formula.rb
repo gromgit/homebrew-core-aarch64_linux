d4b74cc98e4c8e86f42289480104f88203654f22
MoonFruit <dkmoonfruit@gmail.com>
2021-03-31 10:13:02 +0800

    file-formula 5.40
    * file-formula 5.40
    * file-formula: update license
    
    Closes #74232.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/file-formula.rb
+++ Formula/file-formula.rb
@@ -2,10 +2,10 @@
 class FileFormula < Formula
   desc "Utility to determine file types"
   homepage "https://www.darwinsys.com/file/"
-  url "https://astron.com/pub/file/file-5.39.tar.gz"
-  sha256 "f05d286a76d9556243d0cb05814929c2ecf3a5ba07963f8f70bfaaa70517fad1"
-  license "BSD-2-Clause"
-  revision 1
+  url "https://astron.com/pub/file/file-5.40.tar.gz"
+  sha256 "167321f43c148a553f68a0ea7f579821ef3b11c27b8cbe158e4df897e4a5dd57"
+  # file-formula has a BSD-2-Clause-like license
+  license :cannot_represent
   head "https://github.com/file/file.git"
 
   bottle do
