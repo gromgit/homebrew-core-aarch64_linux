9434f9248589af3b29ab7c8192f35e9db44ff6b4
rui <rui@chenrui.dev>
2021-03-31 00:16:27 +0000

    geph4 4.3.0
    Closes #74227.
    
    Signed-off-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/geph4.rb
+++ Formula/geph4.rb
@@ -1,8 +1,8 @@
 class Geph4 < Formula
   desc "Modular Internet censorship circumvention system to deal with national filtering"
   homepage "https://geph.io/"
-  url "https://github.com/geph-official/geph4/archive/v4.2.6.tar.gz"
-  sha256 "771267c0f8e382940e6d4a5d3982b05d02e497c98f32a6759387dc39463ca2e0"
+  url "https://github.com/geph-official/geph4/archive/v4.3.0.tar.gz"
+  sha256 "2158e562a39f0e6deecf0cf4a60f17314d99cc7e185c3803ddc219c3c5d66adf"
   license "GPL-3.0-only"
 
   bottle do
