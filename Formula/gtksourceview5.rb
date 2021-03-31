9eb3cce5e6ee94336cc7117680bdb05e9d742faf
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2021-03-31 06:21:28 +0100

    gtksourceview5 5.0.0 (new formula)
    Closes #74240.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/gtksourceview5.rb
@@ -0,0 +1,47 @@
+class Gtksourceview5 < Formula
+  desc "Text view with syntax, undo/redo, and text marks"
+  homepage "https://projects.gnome.org/gtksourceview/"
+  url "https://download.gnome.org/sources/gtksourceview/5.0/gtksourceview-5.0.0.tar.xz"
+  sha256 "64826633c0c8d2c6a6eb4ec653215ef04e31e13a2d4156a09ca5fd9013acd9c3"
+  license "LGPL-2.1-or-later"
+
+  livecheck do
+    url :stable
+    regex(/gtksourceview[._-]v?(5\.([0-8]\d*?)?[02468](?:\.\d+)*?)\.t/i)
+  end
+
+  depends_on "gobject-introspection" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "vala" => :build
+  depends_on "gtk4"
+  depends_on "pcre2"
+
+  def install
+    args = std_meson_args + %w[
+      -Dgir=true
+      -Dvapi=true
+    ]
+
+    mkdir "build" do
+      system "meson", *args, ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <gtksourceview/gtksource.h>
+
+      int main(int argc, char *argv[]) {
+        gchar *text = gtk_source_utils_unescape_search_text("hello world");
+        return 0;
+      }
+    EOS
+    flags = shell_output("#{Formula["pkg-config"].opt_bin}/pkg-config --cflags --libs gtksourceview-5").strip.split
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
