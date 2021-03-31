5c15ae551200df3a4efe9fa32e82dc3cc1330f82
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-03-31 20:28:42 +0000

    qt: update 6.0.3 bottle.

--- Formula/qt.rb
+++ Formula/qt.rb
@@ -14,10 +14,10 @@ class Qt < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "fb36682730c3261967347efd1e1219970ddcf9e39998183b2aeaa76acad98cce"
-    sha256 cellar: :any, big_sur:       "99950b7eb8c3fd4a91f5622f1199b77d99d9d2aa9deeb2e2f11a1ed568f1194b"
-    sha256 cellar: :any, catalina:      "30f9131632cfa2088eb24315a5f34090e004413261e11b7290e37d78d7f9e42e"
-    sha256 cellar: :any, mojave:        "9498b72fda65897df32aa8ecd754b45d0f02551280170beb6b76e7001d4fff01"
+    sha256 cellar: :any, arm64_big_sur: "d54796b612c7c9a03ef61cb46c7371dd61c9f0d158450ceffc63641f5023a917"
+    sha256 cellar: :any, big_sur:       "2de2bde3a0246ac6f41e82e68b19f130e06855a8ab74d9d3f156ada706d743a1"
+    sha256 cellar: :any, catalina:      "09527b3f023b7f31822be933615636a7b7c17dce67848a3b5544f2081df2bfe8"
+    sha256 cellar: :any, mojave:        "4e1a1ac8a420e29898499eae3f770c51118c235e68fa457f32a311232f3c0e68"
   end
 
   depends_on "cmake" => [:build, :test]
