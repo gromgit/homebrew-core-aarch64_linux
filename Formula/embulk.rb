1c4233c97d52809aaf7874f6188534770b0392d3
Sean Molenaar <SMillerDev@users.noreply.github.com>
2021-04-01 11:46:56 +0200

    embulk: move to github releases (#74336)

--- Formula/embulk.rb
+++ Formula/embulk.rb
@@ -4,7 +4,7 @@ class Embulk < Formula
   # https://www.embulk.org/articles/2020/03/13/embulk-v0.10.html
   # v0.10.* is a "development" series, not for your production use.
   # In your production, keep using v0.9.* stable series.
-  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.9.23.jar"
+  url "https://github.com/embulk/embulk/releases/download/v0.9.23/embulk-0.9.23.jar"
   sha256 "153977fad482bf52100dd96f47e897c87b48de4fb13bccd6b3101475d3a5ebb9"
   license "Apache-2.0"
   version_scheme 1
