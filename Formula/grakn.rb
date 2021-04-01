7a586118773f95462eefa7fe51a395335608dd32
rui <rui@chenrui.dev>
2021-04-01 00:13:47 +0000

    grakn 2.0.0
    * grakn 2.0.0
    * update test
    * Update grakn.rb
    * Update grakn.rb
    * Update grakn.rb
    
    Closes #74292.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/grakn.rb
+++ Formula/grakn.rb
@@ -1,22 +1,21 @@
 class Grakn < Formula
   desc "Distributed hyper-relational database for knowledge engineering"
   homepage "https://grakn.ai"
-  url "https://github.com/graknlabs/grakn/releases/download/1.8.4/grakn-core-all-mac-1.8.4.zip"
-  sha256 "136b6933a643959d31d1f9c964bcb161db52a01c7f51ff7fa156335c0004ce54"
+  url "https://github.com/graknlabs/grakn/releases/download/2.0.0/grakn-core-all-mac-2.0.0.zip"
+  sha256 "b3dc73f3a22d94f8f8d546c6b96e186db3fc9369bde306343098fed777c8fa7f"
   license "AGPL-3.0-or-later"
-  revision 1
 
   bottle :unneeded
 
-  depends_on "openjdk@8"
+  depends_on "openjdk@11"
 
   def install
     libexec.install Dir["*"]
     bin.install libexec/"grakn"
-    bin.env_script_all_files(libexec, Language::Java.java_home_env("1.8"))
+    bin.env_script_all_files(libexec, Language::Java.java_home_env("11"))
   end
 
   test do
-    assert_match "RUNNING", shell_output("#{bin}/grakn server status")
+    assert_match "THE KNOWLEDGE GRAPH", shell_output("#{bin}/grakn server status")
   end
 end
