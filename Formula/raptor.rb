0f8a4106b413fa40812697ae69154708b161acb0
Sam Ford <1584702+samford@users.noreply.github.com>
2021-03-31 22:38:39 -0400

    raptor: add livecheck

--- Formula/raptor.rb
+++ Formula/raptor.rb
@@ -4,6 +4,11 @@ class Raptor < Formula
   url "http://download.librdf.org/source/raptor2-2.0.15.tar.gz"
   sha256 "ada7f0ba54787b33485d090d3d2680533520cd4426d2f7fb4782dd4a6a1480ed"
 
+  livecheck do
+    url :homepage
+    regex(/href=.*?raptor2[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
   bottle do
     sha256 cellar: :any, arm64_big_sur: "2970bdade24bb5ec9afe4b79e89234016147cb506f6b450bcfd66c50fce1cede"
     sha256 cellar: :any, big_sur:       "5b58712f0ba9fc647c6b241f80ce3697e25c851f80b26cb8f89f28809905126f"
