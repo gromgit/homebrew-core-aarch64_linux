0bf083ea72ad3a3756bff6b9a4eab06afdd20002
Caleb Xu <calebcenter@live.com>
2021-04-01 02:37:28 -0400

    mapcidr 0.0.5 (new formula)
    Closes #74323.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

---
+++ Formula/mapcidr.rb
@@ -0,0 +1,20 @@
+class Mapcidr < Formula
+  desc "Subnet/CIDR operation utility"
+  homepage "https://projectdiscovery.io"
+  url "https://github.com/projectdiscovery/mapcidr/archive/v0.0.5.tar.gz"
+  sha256 "5c1b4a530d02bf99192d3240e2f57fa1722d4249e41a08bd6f51ea6cdd47d07e"
+  license "MIT"
+  head "https://github.com/projectdiscovery/mapcidr.git"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "./cmd/mapcidr"
+  end
+
+  test do
+    expected = "192.168.0.0/18\n192.168.64.0/18\n192.168.128.0/18\n192.168.192.0/18\n"
+    output = shell_output("#{bin}/mapcidr -cidr 192.168.1.0/16 -sbh 16384 -silent")
+    assert_equal expected, output
+  end
+end
