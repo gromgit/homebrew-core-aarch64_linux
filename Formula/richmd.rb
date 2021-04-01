87668c28d4940383725e4ecc7ac3db190cf41de1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-01 06:07:18 +0000

    richmd: update 10.0.1 bottle.

--- Formula/richmd.rb
+++ Formula/richmd.rb
@@ -8,10 +8,10 @@ class Richmd < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d3cb05a5be962f914908e05d1e788ce639c0873de907a322c266b5387eb5088f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d297c65f21ca4d9f0f2b1f629a58f878eb1262aec6fb48202ca51a56536d99c6"
-    sha256 cellar: :any_skip_relocation, catalina:      "7b39ce207934d9327bb4ea8f63898124be3253990adfe0245b36c1bc3a2975a3"
-    sha256 cellar: :any_skip_relocation, mojave:        "2264250d39f74ff907b290a7386eb1262e4ac8af99fd236a214c5e529842644c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4ebd3d175aea0749da84ab26b97e43ec227ad7fccb7583e814d8fb78eb656a9d"
+    sha256 cellar: :any_skip_relocation, big_sur:       "838163b4a1bd311bcb57098c11529603cb6c86599078ba0ad50a715b17a64011"
+    sha256 cellar: :any_skip_relocation, catalina:      "265ce83bc1bfc5fb1e81d217871c7d1d758f9e78353c931f7b6007611c781685"
+    sha256 cellar: :any_skip_relocation, mojave:        "e3d99906d05311288edf719b6a10aa8ce110375623800e8d20a888c2263236f9"
   end
 
   depends_on "python@3.9"
