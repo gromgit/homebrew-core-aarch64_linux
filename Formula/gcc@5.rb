4a8376d159fb6fa1d443fa0fb7b0ce06e5c39b29
Michka Popoff <michkapopoff@gmail.com>
2021-04-02 23:17:17 +0200

    gcc@5: add linux dependencies

--- Formula/gcc@5.rb
+++ Formula/gcc@5.rb
@@ -33,6 +33,10 @@ class GccAT5 < Formula
 
   uses_from_macos "zlib"
 
+  on_linux do
+    depends_on "binutils"
+  end
+
   # GCC bootstraps itself, so it is OK to have an incompatible C++ stdlib
   cxxstdlib_check :skip
 
