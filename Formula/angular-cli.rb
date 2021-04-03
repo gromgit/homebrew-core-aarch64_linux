7c950cb42eecbc32d17274a8b518a7d87df982a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-03 03:12:48 +0000

    angular-cli: update 11.2.7 bottle.

--- Formula/angular-cli.rb
+++ Formula/angular-cli.rb
@@ -8,10 +8,10 @@ class AngularCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "081fac4ba8f416c353b2f4f4d893e1caa35e30ddf696e44ee25a80461956a759"
-    sha256 cellar: :any_skip_relocation, big_sur:       "df6299e75b052cbc06c894307b4c656345904ff9366f651e0be9689fc2b6e0cf"
-    sha256 cellar: :any_skip_relocation, catalina:      "eadeb81ceab35c34e05f8e1990a78ffd278593d57ccf0968410e1581e5070d1d"
-    sha256 cellar: :any_skip_relocation, mojave:        "228f8722e0ac300f56e12e1e3b9864f3a8dbefedb10af0e5238436871886a1a5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f2049afd06a06be723fd073d81edb3a3c53da740b7b0d3230fee4c8b6f77c644"
+    sha256 cellar: :any_skip_relocation, big_sur:       "9c0b60599f01d652373dffff755d5c77e7a0a36052ceed70b39f85434dcfa069"
+    sha256 cellar: :any_skip_relocation, catalina:      "c8ca6874da2cfd4c95591f64f174a913ee2f8ae1bbf3eed263f78242b295d08b"
+    sha256 cellar: :any_skip_relocation, mojave:        "b3b13c4f24e15b226d1baf981765d6e328d946f6437a188419c73bca067c174c"
   end
 
   depends_on "node"
