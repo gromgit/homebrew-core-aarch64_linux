78b5a005124695f6650a286490a4dae3df790c16
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-04-03 18:40:38 +0000

    haruhi-dl: add 2021.4.1 bottle.

--- Formula/haruhi-dl.rb
+++ Formula/haruhi-dl.rb
@@ -7,6 +7,13 @@ class HaruhiDl < Formula
   sha256 "0854dc9c61ad3b639fb90f87545eb12a92fc3ce499eb5848f844ec37aa28d591"
   license "LGPL-3.0-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "74dc7e58a265c64127e4c7db0e3532c504c8b1d52a2bfd6296d419bafe6292d3"
+    sha256 cellar: :any_skip_relocation, big_sur:       "20540e0bbc073a1c3e2cc37f46e537cb2952ae3e1125f2229bff0bb82d82afa3"
+    sha256 cellar: :any_skip_relocation, catalina:      "307d026e98537305bd6ea425582212d6dec8efc318b3a0bac281033d37e54bef"
+    sha256 cellar: :any_skip_relocation, mojave:        "9c8795b77635086b45641e6590ae51f41c6aa8d9a496e8c2ecfa8a3bada0f640"
+  end
+
   depends_on "python@3.9"
 
   def install
