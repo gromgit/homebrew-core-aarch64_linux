64c9edf2164b1245bc6c8e5ae4d636122c75bd7e
Kid <44045911+kidonng@users.noreply.github.com>
2021-04-04 01:13:29 +0800

    exa 0.10.0
    * exa 0.10.0
    * exa: skip building docs on ARM
      Building the docs uses pandoc, which does not work on ARM yet.
    
    Closes #74493.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/exa.rb
+++ Formula/exa.rb
@@ -1,11 +1,10 @@
 class Exa < Formula
   desc "Modern replacement for 'ls'"
   homepage "https://the.exa.website"
-  # Remove Cargo.lock resource at version bump!
-  url "https://github.com/ogham/exa/archive/v0.9.0.tar.gz"
-  sha256 "96e743ffac0512a278de9ca3277183536ee8b691a46ff200ec27e28108fef783"
+  url "https://github.com/ogham/exa/archive/v0.10.0.tar.gz"
+  sha256 "27420f7b805941988399d63f388be4f6077eee94a505bf01c2fb0e7d15cbf78d"
   license "MIT"
-  revision 2
+  head "https://github.com/ogham/exa.git"
 
   livecheck do
     url :stable
@@ -19,11 +18,7 @@ class Exa < Formula
     sha256 cellar: :any_skip_relocation, mojave:        "af7d2f089dc99cab221dbc57c33c443e112a843911d561588621e60378f56a46"
   end
 
-  head do
-    url "https://github.com/ogham/exa.git"
-    depends_on "pandoc" => :build
-  end
-
+  depends_on "pandoc" => :build unless Hardware::CPU.arm?
   depends_on "rust" => :build
 
   uses_from_macos "zlib"
@@ -32,42 +27,24 @@ class Exa < Formula
     depends_on "libgit2"
   end
 
-  # Replace stale lock file. Remove at version bump.
-  resource "Cargo.lock" do
-    url "https://raw.githubusercontent.com/ogham/exa/61c5df7c111fc7451bf6b8f0dfdcb2b6b46577d0/Cargo.lock"
-    sha256 "0bc38c483120874c42b9ada35d13530f16850274cfa8ff1defc1e55bba509698"
-  end
-
   def install
-    # Remove at version bump
-    unless build.head?
-      rm_f "Cargo.lock"
-      resource("Cargo.lock").stage buildpath
-    end
-
     system "cargo", "install", *std_cargo_args
 
-    # Remove in 0.9+
-    if build.head?
-      bash_completion.install "completions/completions.bash" => "exa"
-      zsh_completion.install  "completions/completions.zsh"  => "_exa"
-      fish_completion.install "completions/completions.fish" => "exa.fish"
+    bash_completion.install "completions/completions.bash" => "exa"
+    zsh_completion.install  "completions/completions.zsh"  => "_exa"
+    fish_completion.install "completions/completions.fish" => "exa.fish"
 
-      args = %w[
-        --standalone
-        --to=man
-      ]
+    args = %w[
+      --standalone
+      --to=man
+    ]
 
+    unless Hardware::CPU.arm?
       system "pandoc", *args, "man/exa.1.md", "-o", "exa.1"
       system "pandoc", *args, "man/exa_colors.5.md", "-o", "exa_colors.5"
 
       man1.install "exa.1"
       man5.install "exa_colors.5"
-    else
-      bash_completion.install "contrib/completions.bash" => "exa"
-      zsh_completion.install  "contrib/completions.zsh"  => "_exa"
-      fish_completion.install "contrib/completions.fish" => "exa.fish"
-      man1.install "contrib/man/exa.1"
     end
   end
 
