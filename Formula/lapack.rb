a976b848e90401560529c6283fda6ee2087df1f5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-03 21:07:38 +0000

    lapack: update 3.9.1 bottle.

--- Formula/lapack.rb
+++ Formula/lapack.rb
@@ -7,11 +7,10 @@ class Lapack < Formula
   head "https://github.com/Reference-LAPACK/lapack.git"
 
   bottle do
-    sha256 arm64_big_sur: "7b84850a2c6dd2c589630c1c61157594c05689b516e1e072d1e0ad62e0df40e2"
-    sha256 big_sur:       "4545eca9af03f52eb2080a3e45017d877b1832a9096c5c023886bd8aa1116bec"
-    sha256 catalina:      "be00d33447ab0bb47a05f89517640c984cafb7723fd6fff9749de773c304f2df"
-    sha256 mojave:        "ad84de82af78dc5ee4b305c76a79a3fc18420beeac2be72d1c262936d5a9110e"
-    sha256 high_sierra:   "fd360d9073e8a483e88a0bca6c8dc96a8159488fa3a6009bb8cbe03747f20f4f"
+    sha256 arm64_big_sur: "d9cb8ec5b3323036392af942ca33f408648fd8c08965653a599d918f5f2790a7"
+    sha256 big_sur:       "b030a88ee8fa327ecdd5b33ba31a5ceaf5137e5f6c1ebb861911cca6d5bd23f3"
+    sha256 catalina:      "6c5f2f1fd02df12472fa05eb39dd7bfd0c8f1a7e367afebef9848c2c4503a97b"
+    sha256 mojave:        "6d637980e356e094bb6f35bc171cdac72ef73d94fe3dc50d3a6ecc1af7eb14a2"
   end
 
   keg_only :shadowed_by_macos, "macOS provides LAPACK in Accelerate.framework"
