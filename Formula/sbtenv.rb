aa4e0aadb7723893b984d7dfa57266d9231c15ba
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2021-04-04 23:56:48 +0200

    sbtenv 0.0.22 (#74529)

--- Formula/sbtenv.rb
+++ Formula/sbtenv.rb
@@ -1,8 +1,8 @@
 class Sbtenv < Formula
   desc "Command-line tool for managing sbt environments"
   homepage "https://github.com/sbtenv/sbtenv"
-  url "https://github.com/sbtenv/sbtenv/archive/version/0.0.21.tar.gz"
-  sha256 "a322b8db197068c7c9889bb14eb0537b175c7095dcf6ae938289f1aa2e97768e"
+  url "https://github.com/sbtenv/sbtenv/archive/version/0.0.22.tar.gz"
+  sha256 "7af43d2d12b7165ce5cf68d2e7fb1673152f522f2ad574e19dfa6dedfc8836c3"
   license "MIT"
   head "https://github.com/sbtenv/sbtenv.git"
 
