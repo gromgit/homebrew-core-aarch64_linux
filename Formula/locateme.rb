818dd12b6c6d1cbc7eb08f08b67dc7b524407428
Daniel Nachun <daniel.nachun@gmail.com>
2021-04-03 19:34:50 -0700

    locateme: explicitly make macOS only (#74472)

--- Formula/locateme.rb
+++ Formula/locateme.rb
@@ -3,6 +3,7 @@ class Locateme < Formula
   homepage "https://iharder.sourceforge.io/current/macosx/locateme"
   url "https://downloads.sourceforge.net/project/iharder/locateme/LocateMe-v0.2.1.zip"
   sha256 "137016e6c1a847bbe756d8ed294b40f1d26c1cb08869940e30282e933e5aeecd"
+  license :public_domain
 
   livecheck do
     url :stable
@@ -19,6 +20,8 @@ class Locateme < Formula
     sha256 cellar: :any_skip_relocation, el_capitan:    "5f8e1febc1886565bfa9691cb3ffc0486999f8b682a52276c1d9ea6e0f1f4470"
     sha256 cellar: :any_skip_relocation, yosemite:      "a7876905a4c06452431e506523c5fdf142e2de364427600122fbb9b4928bc6d1"
   end
+
+  depends_on :macos
 
   def install
     system ENV.cc, "-framework", "Foundation", "-framework", "CoreLocation", "LocateMe.m", "-o", "LocateMe"
