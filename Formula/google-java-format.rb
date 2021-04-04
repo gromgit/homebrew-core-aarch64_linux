d76c938fe1fc0b0a1b2c9e0042b99c2586bb8ff2
Thierry Moisan <thierry.moisan@gmail.com>
2021-04-04 15:25:57 -0400

    google-java-format 1.10.0 (#74524)

--- Formula/google-java-format.rb
+++ Formula/google-java-format.rb
@@ -1,8 +1,8 @@
 class GoogleJavaFormat < Formula
   desc "Reformats Java source code to comply with Google Java Style"
   homepage "https://github.com/google/google-java-format"
-  url "https://github.com/google/google-java-format/releases/download/google-java-format-1.9/google-java-format-1.9-all-deps.jar"
-  sha256 "1d98720a5984de85a822aa32a378eeacd4d17480d31cba6e730caae313466b97"
+  url "https://github.com/google/google-java-format/releases/download/v1.10.0/google-java-format-1.10.0-all-deps.jar"
+  sha256 "9d404cf6fe5f6aa7672693a3301ef2a22016ba540eca5d835be43104b71eb5d6"
   license "Apache-2.0"
 
   bottle :unneeded
