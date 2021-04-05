712bd13ea944970d89edbb7465a9515c958b1e7a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-04-05 16:17:17 +0000

    s2n: update 1.0.2 bottle.

--- Formula/s2n.rb
+++ Formula/s2n.rb
@@ -11,11 +11,10 @@ class S2n < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "e777f779afcfcdedf621642693d6c257fbf2f7bb3730e57011063643e6b32697"
-    sha256 cellar: :any, big_sur:       "d2de6735ea9b3b5dbc6c027959beb240f5fe7dbcf66c5d7be10eb2e1fd9e6230"
-    sha256 cellar: :any, catalina:      "563c56399c77a3d3a6a7fa265a854c11671e671ad679316e0f5eb3fadfe1d3ea"
-    sha256 cellar: :any, mojave:        "5dfe9d90d210cf4df21c785d866efb35a4e2a2c23fc79e8de2c77d732ae666c7"
-    sha256 cellar: :any, high_sierra:   "fa1f38966a646891d5fda5573743d5212f462a3d816e7879c92e27ef243858e8"
+    sha256 cellar: :any, arm64_big_sur: "7da7a2e3631d8f0afd0582c174b431b386323d0ca2b83bfe26b08e80a808535c"
+    sha256 cellar: :any, big_sur:       "ea8880c629f8a77d587e5e060f5c211c2b4a0e4c5e61cd90d701f4b546818926"
+    sha256 cellar: :any, catalina:      "96929f93c5ab2dbeb4c48c2e3a5f50f1a500f573fde98fc33cfdd703ce6cc65d"
+    sha256 cellar: :any, mojave:        "fd8736dbdb414c24f2b40c909b41cf28671f2e9bcd0e929e0e068c388377ad64"
   end
 
   depends_on "cmake" => :build
