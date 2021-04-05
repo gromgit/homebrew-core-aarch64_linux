8165a72cb277a7a583ad0c865b4b4d764287499e
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-04-05 15:15:13 +0100

    rav1e: update 0.4.1_1 bottle.

--- Formula/rav1e.rb
+++ Formula/rav1e.rb
@@ -13,10 +13,10 @@ class Rav1e < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "e5dd3ae4c9492d6984dbc4f67a0b0fac23b4bd9294f6ba80a6621c94e841f70c"
-    sha256 cellar: :any, big_sur:       "7c9ead158025ca4faa1236a5a14194dfc849ef5e199abf87e9d63cbef70b44ae"
-    sha256 cellar: :any, catalina:      "360c7d268a838420b20b7c59cf7df38d37b50a3f5b75d02ea326f4a4de96b433"
-    sha256 cellar: :any, mojave:        "a9d58a74c083653d4cdbde52224b8aecf01d30a60d9c11b8fc4cfc78b89e9ab3"
+    sha256 cellar: :any, arm64_big_sur: "80c76875e03241980641acdfa1c25c87e437e7950424fce2304841ef9bd5957b"
+    sha256 cellar: :any, big_sur:       "45b2b8adc65f38c42d453665f302385df54b251846355d002b753365a0701a3e"
+    sha256 cellar: :any, catalina:      "e08ce645cc56ed8258cc84c6aedaa7e78ce02ce4fed0ad7032845a2c0052aed0"
+    sha256 cellar: :any, mojave:        "8ed66c0b3180c82355a8d9201ace97ac281b8dab4bc152ad9e58a855b01429ba"
   end
 
   depends_on "cargo-c" => :build
