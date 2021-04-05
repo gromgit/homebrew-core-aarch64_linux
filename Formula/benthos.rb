8b11e80afeb3e00ff2c0162a52b9457cf5c76082
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-05 22:07:46 +0000

    benthos: update 3.43.1 bottle.

--- Formula/benthos.rb
+++ Formula/benthos.rb
@@ -6,10 +6,10 @@ class Benthos < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8539d5d5377497481dbc62facdd530e39fbc343796c4d248163332db67cb369b"
-    sha256 cellar: :any_skip_relocation, big_sur:       "d5b89a656e9e54a0b43e0a763cbc25d192185dcbbc5b9cd889593bb6089f2afc"
-    sha256 cellar: :any_skip_relocation, catalina:      "e6ad322a186c75ee6c34c5b96cc2c23182a787bb73c324785df73b12b2787566"
-    sha256 cellar: :any_skip_relocation, mojave:        "cefa8343113d3f55522bb637778533b93cb9a8f2aad1999fbe98f0eff2404036"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ee7786484541dc610ebf2447abcb5a1041783bd1ddb2a30a18e4834b54364e20"
+    sha256 cellar: :any_skip_relocation, big_sur:       "80684f2e3f3d7b6347d071245949db8eff1d73078915e5f9483127cef4e13020"
+    sha256 cellar: :any_skip_relocation, catalina:      "5e7f3ac9e9f5267b152bea9ce4e5178a8ed85d950a796316e659b654e312996a"
+    sha256 cellar: :any_skip_relocation, mojave:        "5e1a2ad2b0a3e5dc704b4f4781c513af2613e21e540558232f803c149c1165d6"
   end
 
   depends_on "go" => :build
