26264c1620875dc5dc223a219b034626a2f5fbb1
Rylan Polster <rslpolster@gmail.com>
2021-04-05 16:18:48 +0000

    nift: update 2.4.10 bottle.

--- Formula/nift.rb
+++ Formula/nift.rb
@@ -6,9 +6,9 @@ class Nift < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "04b46551d7a1e0ae9adb2e1af9a768d119543490aca901287d9dbb96c03bfd9d"
-    sha256 cellar: :any_skip_relocation, catalina: "82824e8885876faacc066c28a4a79092202effbf7f8eb41d0ec881308bbb25e1"
-    sha256 cellar: :any_skip_relocation, mojave:   "69d14f5ba918e3b828998e898b58dbd4239482fe5ea9520363c5f784eb62b5a4"
+    sha256 cellar: :any_skip_relocation, big_sur:  "fb6510a7d3eef37e3274009abe1062585c07294c2453d563b70b9501fa70f719"
+    sha256 cellar: :any_skip_relocation, catalina: "613a21f5107f0e099cd32b67779587807686faf7666366ce4cb1f218855a2d4f"
+    sha256 cellar: :any_skip_relocation, mojave:   "62b96189d61ca0c360aa7c00718f0995fde9d20ad81c5a700836083800093aef"
   end
 
   def install
