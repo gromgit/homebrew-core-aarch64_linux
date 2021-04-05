c23a439cef429ad03f5bc294f0cdcbb2dc3cf661
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-05 22:08:21 +0000

    pdfcpu: update 0.3.11 bottle.

--- Formula/pdfcpu.rb
+++ Formula/pdfcpu.rb
@@ -6,10 +6,10 @@ class Pdfcpu < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "efd9484e1d092b0ece59f5eab702065a3e561548ccc2c7ea2784690df80ffdb9"
-    sha256 cellar: :any_skip_relocation, big_sur:       "51cd5f3eaa11f138ceea6908cbaa8589eaa242aaadd05b503963ada21fe1326c"
-    sha256 cellar: :any_skip_relocation, catalina:      "03a91d21d7c2c45c039c59ce0d1dcd65c93e7a9b0086d66d0deb5eb16be7fc1f"
-    sha256 cellar: :any_skip_relocation, mojave:        "530f7b93cb4eea8e7bfde44d8a5a1d7e24dcd2647ff71e41a9c5770e7e800726"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "624219b4260f843a569e0118a960265074b37d37dd2ea540634d0dc95c79df69"
+    sha256 cellar: :any_skip_relocation, big_sur:       "929a1b6879bf834d195e9811940469fdf5d6b26a0260e41b8821f0d3ca8a0fd2"
+    sha256 cellar: :any_skip_relocation, catalina:      "cff4d99d6c310dc77d4c760b52e1c4b345b18682742f5b4bc575b4e4310a3275"
+    sha256 cellar: :any_skip_relocation, mojave:        "cbade3938d02906c5689f3f16aeccddede49fb054264cb18a89972fc6398cf6e"
   end
 
   depends_on "go" => :build
