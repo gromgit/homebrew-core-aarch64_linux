ecb2a74119b68dd2f4243bc8812d21677592e0cd
Michka Popoff <michkapopoff@gmail.com>
2021-04-06 23:45:17 +0200

    unrtf: set -liconv only on macOS (#74547)
    and fix license

--- Formula/unrtf.rb
+++ Formula/unrtf.rb
@@ -4,7 +4,7 @@ class Unrtf < Formula
   url "https://ftp.gnu.org/gnu/unrtf/unrtf-0.21.10.tar.gz"
   mirror "https://ftpmirror.gnu.org/unrtf/unrtf-0.21.10.tar.gz"
   sha256 "b49f20211fa69fff97d42d6e782a62d7e2da670b064951f14bbff968c93734ae"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
   head "https://hg.savannah.gnu.org/hgweb/unrtf/", using: :hg
 
   bottle do
@@ -21,7 +21,11 @@ class Unrtf < Formula
 
   def install
     system "./bootstrap"
-    system "./configure", "LIBS=-liconv", "--prefix=#{prefix}"
+    args = %W[--prefix=#{prefix}]
+    on_macos do
+      args << "LIBS=-liconv"
+    end
+    system "./configure", *args
     system "make", "install"
   end
 
