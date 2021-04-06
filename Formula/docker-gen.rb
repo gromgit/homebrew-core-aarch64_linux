11b74fcef49c1816fd117bdd8addcd3e4a5d74e2
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-04-06 14:18:11 +0000

    docker-gen: update 0.7.5 bottle.

--- Formula/docker-gen.rb
+++ Formula/docker-gen.rb
@@ -6,13 +6,10 @@ class DockerGen < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "719613c9b090bdddc48ffebe3ca13347fcaf8ccdadbcddee6bbec4367dbc7795"
-    sha256 cellar: :any_skip_relocation, big_sur:       "77385699141202354e27dbfbe1d57c936e4eec4beb1c5f7537f29db4cfc57602"
-    sha256 cellar: :any_skip_relocation, catalina:      "d5305ec74f29526e6e7b01632dda0e48b0af397d9a65baf233db4da48f96ee8a"
-    sha256 cellar: :any_skip_relocation, mojave:        "00f1f34756eadc57f39945a00bc4e8c9e8ff2beefafbb58052667c2611e29e0f"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "42d2757b01271ef6c14de5441b3c65507538388db1e00e69f322272a5ba5b59c"
-    sha256 cellar: :any_skip_relocation, sierra:        "222a5586670fec7643e9e7651f0b1fa82ff012048bd29b959ac720743f1a1a4f"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "c274701a545e5a4885995718f5f01ca6df2f9c6b9a143d4ffcf46b1771ac4cbc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3c85b99b9c196d88d88f1b841b945581dbf532a535d5ae9eddcba113186479c2"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a8e0c7dc323e6d12139778d740422435c4f7e8c5515ea1fe1b3020cbf570aefa"
+    sha256 cellar: :any_skip_relocation, catalina:      "de774435b70a8ef725753d6f21c53059ff9702e40579eeca873b2bbd58d57e9f"
+    sha256 cellar: :any_skip_relocation, mojave:        "f9fd70bd526ee9deedac3d51b76fc680ab9bd6ff0a15bec040ecec682c8ee000"
   end
 
   depends_on "go" => :build
