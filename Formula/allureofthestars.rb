29517cad96b7b1e06b7acb7f6a3c858e40e82913
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2021-04-06 23:51:04 +0200

    allureofthestars 0.10.2.0
    * allureofthestars v0.10.2.0
    * Remove 'revision 2', as told by the CI
    
    Closes #74699.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/allureofthestars.rb
+++ Formula/allureofthestars.rb
@@ -1,10 +1,9 @@
 class Allureofthestars < Formula
   desc "Near-future Sci-Fi roguelike and tactical squad combat game"
   homepage "https://www.allureofthestars.com/"
-  url "https://hackage.haskell.org/package/Allure-0.9.5.0/Allure-0.9.5.0.tar.gz"
-  sha256 "8180fe070633bfa5515de8f7443421044e7ad4ee050f0a92c048cec5f2c88132"
+  url "https://hackage.haskell.org/package/Allure-0.10.2.0/Allure-0.10.2.0.tar.gz"
+  sha256 "fcb9f38ea543d3277fa90eee004f7624d1168bf7f2c17902cda1870293b7c2f4"
   license all_of: ["AGPL-3.0-or-later", "GPL-2.0-or-later", "OFL-1.1", "MIT", :cannot_represent]
-  revision 2
   head "https://github.com/AllureOfTheStars/Allure.git"
 
   bottle do
@@ -28,8 +27,8 @@ class Allureofthestars < Formula
     assert_equal "",
       shell_output("#{bin}/Allure --dbgMsgSer --dbgMsgCli --logPriority 0 --newGame 3 --maxFps 100000 " \
                                  "--stopAfterFrames 50 --automateAll --keepAutomated --gameMode battle " \
-                                 "--setDungeonRng 7 --setMainRng 7")
+                                 "--setDungeonRng \"SMGen 7 7\" --setMainRng \"SMGen 7 7\"")
     assert_equal "", (testpath/".Allure/stderr.txt").read
-    assert_match "UI client FactionId 1 stopped", (testpath/".Allure/stdout.txt").read
+    assert_match "Client FactionId 1 closed frontend.", (testpath/".Allure/stdout.txt").read
   end
 end
