a4d0a8fbae45c1637a3c9a3a16c5af2df562411d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-04-08 00:21:22 +0100

    dbmate: update 1.12.0 bottle.

--- Formula/dbmate.rb
+++ Formula/dbmate.rb
@@ -7,10 +7,10 @@ class Dbmate < Formula
   head "https://github.com/amacneil/dbmate.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "466238443d72e6d0f7bcf0c92a977ee34ca7c1003d0f355aae895725fe12be3a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2ff8a77e409a899dd70559f72a8e8501df068ef6c6f1095f3ac1f28b0122c241"
-    sha256 cellar: :any_skip_relocation, catalina:      "5223d60533452d4ddb6d9d140418f74b263fec8ea000cda0f9c1757b4c2dddef"
-    sha256 cellar: :any_skip_relocation, mojave:        "547cad2cb3074d94999ddc6e3fc0a4b3000a0e91287bfb5192ce74fe89ca62e4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "34669b5f5f37d77540206b0274e97a505c1e26072f5d60f456ddba5bdf17d885"
+    sha256 cellar: :any_skip_relocation, big_sur:       "928e602baa978c891124ab0ab595563976a953a1672b6d6f207bd7bba087f046"
+    sha256 cellar: :any_skip_relocation, catalina:      "bdf5803badd7a93502382027d83ff70488d9270eb89e5ef5f41b73c777c3326b"
+    sha256 cellar: :any_skip_relocation, mojave:        "136e800b7486127c794d7c7b57a3c490f5a5e556ba051ef63729b612b60f7ddf"
   end
 
   depends_on "go" => :build
