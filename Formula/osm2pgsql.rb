9356722441fcb5b4feee0dfd37904c4d29dccacc
rui <rui@chenrui.dev>
2021-04-07 08:02:48 +0000

    osm2pgsql 1.4.2
    Closes #74730.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/osm2pgsql.rb
+++ Formula/osm2pgsql.rb
@@ -1,10 +1,9 @@
 class Osm2pgsql < Formula
   desc "OpenStreetMap data to PostgreSQL converter"
   homepage "https://osm2pgsql.org"
-  url "https://github.com/openstreetmap/osm2pgsql/archive/1.4.1.tar.gz"
-  sha256 "33c4817dceed99764b089ead0e8e2f67c4c6675e761772339b635800970e66e2"
+  url "https://github.com/openstreetmap/osm2pgsql/archive/1.4.2.tar.gz"
+  sha256 "fc68283930ccd468ed9b28685150741b16083fec86800a4b011884ae22eb061c"
   license "GPL-2.0-only"
-  revision 1
   head "https://github.com/openstreetmap/osm2pgsql.git"
 
   bottle do
