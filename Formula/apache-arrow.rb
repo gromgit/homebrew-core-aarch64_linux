b688dd510084508b7ef498338936541b883afc98
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-04-07 22:05:21 +0100

    apache-arrow: revision bump (grpc 1.37.0)

--- Formula/apache-arrow.rb
+++ Formula/apache-arrow.rb
@@ -5,6 +5,7 @@ class ApacheArrow < Formula
   mirror "https://archive.apache.org/dist/arrow/arrow-4.0.0/apache-arrow-4.0.0.tar.gz"
   sha256 "4a31d0bf702e953bdbcda67af10762a33308281bd247fcbd152ee177419649ae"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/apache/arrow.git"
 
   bottle do
