149f76551747f8e9b81ea9a0a247eee448a02c72
Mike McQuaid <mike@mikemcquaid.com>
2021-04-08 15:11:09 +0100

    encfs: disable
    :osxfuse removed in https://github.com/Homebrew/brew/pull/11075.

--- Formula/encfs.rb
+++ Formula/encfs.rb
@@ -22,8 +22,7 @@ class Encfs < Formula
   depends_on "openssl@1.1"
 
   on_macos do
-    deprecate! date: "2020-11-10", because: "requires FUSE"
-    depends_on :osxfuse
+    disable! date: "2021-04-08", because: "requires FUSE"
   end
 
   on_linux do
