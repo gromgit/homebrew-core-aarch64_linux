bb7ddf6662646b255950186e35039eff1efe40aa
James Billingham <git@jamesbillingham.com>
2021-04-08 20:35:35 +0100

    kubernetes-cli 1.21.0
    Closes #74832.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/kubernetes-cli.rb
+++ Formula/kubernetes-cli.rb
@@ -2,10 +2,9 @@ class KubernetesCli < Formula
   desc "Kubernetes command-line interface"
   homepage "https://kubernetes.io/"
   url "https://github.com/kubernetes/kubernetes.git",
-      tag:      "v1.20.4",
-      revision: "e87da0bd6e03ec3fea7933c4b5263d151aafd07c"
+      tag:      "v1.21.0",
+      revision: "cb303e613a121a29364f75cc67d3d580833a7479"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/kubernetes/kubernetes.git"
 
   livecheck do
@@ -20,15 +19,10 @@ class KubernetesCli < Formula
     sha256 cellar: :any_skip_relocation, mojave:        "c35dd6b593c8f0ba3544b8788f7058d289b0131300e13b243655e495069f9d43"
   end
 
+  depends_on "bash" => :build
   depends_on "go" => :build
 
   uses_from_macos "rsync" => :build
-
-  # ARM support, remove for next release and reinstate test
-  patch do
-    url "https://github.com/kubernetes/kubernetes/commit/f719624654c68d143cad4e4af2f21cca0bfde8fd.patch?full_index=1"
-    sha256 "3d40308a2f639c1a45a2105889776f0d02dd97000c188bc4cc78de739d5f0776"
-  end
 
   def install
     # Don't dirty the git tree
@@ -58,8 +52,7 @@ class KubernetesCli < Formula
 
     version_output = shell_output("#{bin}/kubectl version --client 2>&1")
 
-    # Due to ARM patch reinstate for next release
-    # assert_match "GitTreeState:\"clean\"", version_output
+    assert_match "GitTreeState:\"clean\"", version_output
 
     if build.stable?
       assert_match stable.instance_variable_get(:@resource)
