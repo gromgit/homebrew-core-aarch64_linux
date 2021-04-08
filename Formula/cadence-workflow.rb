b8c45f02cd8c43ddbe1f3a5f5e7d1891c51f42f0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-04-08 14:40:54 +0100

    cadence-workflow: remove root_url

--- Formula/cadence-workflow.rb
+++ Formula/cadence-workflow.rb
@@ -7,7 +7,6 @@ class CadenceWorkflow < Formula
   head "https://github.com/uber/cadence.git"
 
   bottle do
-    root_url "https://homebrew.bintray.com/bottles"
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "4108b02d4a11dba98b26af83c2633550d922eeb00160218458f9896244cc6b25"
     sha256 cellar: :any_skip_relocation, big_sur:       "04a9302cdc9bd7ac3986ba5dba8d786dda09737410071b5d33d59645369bb0fe"
     sha256 cellar: :any_skip_relocation, catalina:      "2402f801a03a4d4a5a557958e0f7342e70901c988475eaed26ac02e1f9e84fa8"
