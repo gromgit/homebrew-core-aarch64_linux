08a74aee70dca6fb041d04984f25eda79a907455
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-04-08 12:54:51 +0000

    cadence-workflow: add 0.18.2 bottle.

--- Formula/cadence-workflow.rb
+++ Formula/cadence-workflow.rb
@@ -6,6 +6,14 @@ class CadenceWorkflow < Formula
   license "MIT"
   head "https://github.com/uber/cadence.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4108b02d4a11dba98b26af83c2633550d922eeb00160218458f9896244cc6b25"
+    sha256 cellar: :any_skip_relocation, big_sur:       "04a9302cdc9bd7ac3986ba5dba8d786dda09737410071b5d33d59645369bb0fe"
+    sha256 cellar: :any_skip_relocation, catalina:      "2402f801a03a4d4a5a557958e0f7342e70901c988475eaed26ac02e1f9e84fa8"
+    sha256 cellar: :any_skip_relocation, mojave:        "5f123ab1904790aa0442ad18d3f9fc09ee99799711403e96e613964c3ad84ac3"
+  end
+
   depends_on "go" => :build
 
   conflicts_with "cadence", because: "both install an `cadence` executable"
