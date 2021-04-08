a543336b7fb6b6abafea3a1e9dcef3561d7256f8
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-04-08 13:33:43 +0000

    sqlite: update 3.35.4 bottle.

--- Formula/sqlite.rb
+++ Formula/sqlite.rb
@@ -12,10 +12,11 @@ class Sqlite < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "dc73e344c364d891047ab1b4fa54c25196b5fb61ac646d3ac3c731311d81e9a4"
-    sha256 cellar: :any, big_sur:       "6f491b7ef85515ede5f193db760117c39f4d4f2dadb482c2f48410b2ea00d1eb"
-    sha256 cellar: :any, catalina:      "615597e87a1804124b019e35609539878eaf17e52f58ef03ce2ed1cd9536b299"
-    sha256 cellar: :any, mojave:        "996aa3f07c2a41ef497bed0c00fbff1272d89e3a282ac8661e7ad5533f09d533"
+    root_url "https://homebrew.bintray.com/bottles"
+    sha256 cellar: :any, arm64_big_sur: "ecb945b9df98160dd9c935fc128e862fbc7d4593ff5088916fc01f20ebd8f592"
+    sha256 cellar: :any, big_sur:       "861dc044ea531c39a2105010107feb95cf10977c3da94a4c4fce6aa1b3041c34"
+    sha256 cellar: :any, catalina:      "a1aee82c8dec9baf0073f52d89ecc2ec3003a49afaa4180315ebf533e08a152b"
+    sha256 cellar: :any, mojave:        "a1f00de2cbdf2b0f7749e54abfb2117fc0d9d48ead5d71c472994fbe7b29c3e2"
   end
 
   keg_only :provided_by_macos
