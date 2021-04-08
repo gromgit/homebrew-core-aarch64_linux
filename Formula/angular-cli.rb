afd19d261a614bc8f767fe530a2ab93169c9ae12
Alexander Zigelski <crunchtime-ali@users.noreply.github.com>
2021-04-08 01:05:28 +0000

    angular-cli 11.2.8
    Closes #74787.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/angular-cli.rb
+++ Formula/angular-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class AngularCli < Formula
   desc "CLI tool for Angular"
   homepage "https://cli.angular.io/"
-  url "https://registry.npmjs.org/@angular/cli/-/cli-11.2.7.tgz"
-  sha256 "91df59a3dc7c9fd943b01ea8ff23864df1f75c31d4efc59579e2ca9c1291ae8e"
+  url "https://registry.npmjs.org/@angular/cli/-/cli-11.2.8.tgz"
+  sha256 "aa866ac857be14e9a0681f1addd2d8e09c31f07a71ce694212d1e0be2e5459e6"
   license "MIT"
 
   bottle do
