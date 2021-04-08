b9c100b6603dfad5cee4cf6326be78e83617f23d
Mike McQuaid <mike@mikemcquaid.com>
2021-04-08 15:11:06 +0100

    archivemount: disable
    :osxfuse removed in https://github.com/Homebrew/brew/pull/11075.

--- Formula/archivemount.rb
+++ Formula/archivemount.rb
@@ -14,8 +14,7 @@ class Archivemount < Formula
   depends_on "libarchive"
 
   on_macos do
-    deprecate! date: "2020-11-10", because: "requires FUSE"
-    depends_on :osxfuse
+    disable! date: "2021-04-08", because: "requires FUSE"
   end
 
   on_linux do
