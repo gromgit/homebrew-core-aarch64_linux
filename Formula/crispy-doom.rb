7615b4bff1f65219db6097ee85e9d9d1eb004997
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-08 00:22:21 +0000

    crispy-doom: update 5.10.1 bottle.

--- Formula/crispy-doom.rb
+++ Formula/crispy-doom.rb
@@ -8,10 +8,10 @@ class CrispyDoom < Formula
   head "https://github.com/fabiangreffrath/crispy-doom"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "2b5e266f3d833820c6f012714b82e025359a0f93b7a35ce0d69ca60a27dafdc0"
-    sha256 cellar: :any, big_sur:       "dea4133cb1b8869964d77da2a9f1cf551868ef85320738f6588e866665938c95"
-    sha256 cellar: :any, catalina:      "1b77f36d5151b7e5a627838a6fed8f559e147a9bd8c9d4d60fd7888c0fd06cf8"
-    sha256 cellar: :any, mojave:        "72135b7e809104c36e71495432f7338c64ad6b1c861bc125bcc0c3bb4aac5ec0"
+    sha256 cellar: :any, arm64_big_sur: "5e88cf832e124281f4eda3126ea53ae012fd8a9680387eede0ec342c15633f82"
+    sha256 cellar: :any, big_sur:       "6ba5a4d2d248da3374409e2cb94dbce8391518a1ec54ac0f4267139a194f3812"
+    sha256 cellar: :any, catalina:      "5dfb49f1137d3776c4476e1e40ee5d218a70e4b379f2d768d5f2120f2ac71542"
+    sha256 cellar: :any, mojave:        "0a08c94e89eeeccb0e7a8a6e1f4968aa1f0bcfcbd9020fed9342727cc2954871"
   end
 
   depends_on "autoconf" => :build
