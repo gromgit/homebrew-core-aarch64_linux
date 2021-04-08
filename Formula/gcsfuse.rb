a7136f76bbb9eedb23ae7106211bbc0a368c4ccd
Mike McQuaid <mike@mikemcquaid.com>
2021-04-08 15:11:11 +0100

    gcsfuse: disable
    :osxfuse removed in https://github.com/Homebrew/brew/pull/11075.

--- Formula/gcsfuse.rb
+++ Formula/gcsfuse.rb
@@ -15,8 +15,7 @@ class Gcsfuse < Formula
   depends_on "go" => :build
 
   on_macos do
-    deprecate! date: "2020-11-10", because: "requires FUSE"
-    depends_on :osxfuse
+    disable! date: "2021-04-08", because: "requires FUSE"
   end
 
   on_linux do
