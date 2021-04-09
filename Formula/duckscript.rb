e92daa160780cc57a4afb6a8c0423b6e1736ff30
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-09 19:11:33 +0000

    duckscript: update 0.8.1 bottle.

--- Formula/duckscript.rb
+++ Formula/duckscript.rb
@@ -7,10 +7,10 @@ class Duckscript < Formula
   head "https://github.com/sagiegurari/duckscript.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4c0d0a1863df44b72e28dc15fd27ceece212cff748203b9b958b3bbbd6567b3e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2ec1cd3b275985be201979fd4392523e279c13e6543e1fbd246766da8e04a57f"
-    sha256 cellar: :any_skip_relocation, catalina:      "2edd02358821010a840afe0cd3d65419840d0cd1737abf3214c56d5ab4248028"
-    sha256 cellar: :any_skip_relocation, mojave:        "42fa3a52dc5be789deea08c481d3a4bb7d3d06343687e5a9617e55638d881060"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a2d128203160905a2bc11a0865c65ac55a8a84aaac41291d972659a29560530a"
+    sha256 cellar: :any_skip_relocation, big_sur:       "467601d561fc88e1ed8d04d8578fe1d7e0aa787a2aa79725b06997c4ff6c1606"
+    sha256 cellar: :any_skip_relocation, catalina:      "be38be5a2ac8be096aad74d435e8f5afe27078507be14999782f7d937b349487"
+    sha256 cellar: :any_skip_relocation, mojave:        "815654c60404277f05dc7bb04791b80f166386696734a30a9c204df3044cc6c9"
   end
 
   depends_on "rust" => :build
