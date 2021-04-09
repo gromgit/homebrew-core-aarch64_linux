00f8d2c5d8b8f8f5c31520caf3365f6d167d2607
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:38:45 +0100

    ftgl: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/ftgl.rb
+++ Formula/ftgl.rb
@@ -18,7 +18,6 @@ class Ftgl < Formula
     sha256 cellar: :any, sierra:        "946a9530f7eae5c8f2bc71dfc91b3a8138ae2228cd441fd7cf39f047b957ce47"
     sha256 cellar: :any, el_capitan:    "6462eb0b97ab120639f1a191f6e3a39419bbb813abd71f5c741303dbf0aed7fb"
     sha256 cellar: :any, yosemite:      "26db05485600adfb7ead23d04fae9b1ee1d1a4b7ac304e1453ad83b4b2c39f64"
-    sha256 cellar: :any, mavericks:     "50a41f3c95a363b52bc367abf4b5b9dc272d71c8b35fe8e63f058c7cf7162225"
   end
 
   depends_on "freetype"
