09e3e2312284e63350a50ff231199036105ea697
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:41:14 +0100

    xlslib: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/xlslib.rb
+++ Formula/xlslib.rb
@@ -14,7 +14,6 @@ class Xlslib < Formula
     sha256 cellar: :any, sierra:        "bcdef576e03aa1cad74d341f6fcc72a1e7944a54542941f96cb8ef8063c2190e"
     sha256 cellar: :any, el_capitan:    "a4d5714e19c1d4e44d67bbe9cda064120dc01e9cf207771ae5ef208e76ed2cd9"
     sha256 cellar: :any, yosemite:      "cfd77ea71da12276124cb9cb08bb9ad51ab38b17e912d4dd8c4fec8a428714a1"
-    sha256 cellar: :any, mavericks:     "b23da4149e9db9bf4152cfc8943ac83d254796d88741592949c3928ae7988149"
   end
 
   depends_on "autoconf" => :build
