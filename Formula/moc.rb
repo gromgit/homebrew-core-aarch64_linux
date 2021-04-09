4dffd6ddcfe0cd91f84bf5d53998b8999b3499ab
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 12:18:21 +0000

    moc: update 2.5.2_5 bottle.

--- Formula/moc.rb
+++ Formula/moc.rb
@@ -42,12 +42,11 @@ class Moc < Formula
   end
 
   bottle do
-    sha256 big_sur:     "4910b2a48422741e1002b79d2bb985fc470da2e4322d31b862f994709376525a"
-    sha256 catalina:    "c2fce2f2fdc2d5eb7efddf393de7dc3d75ca4e387d84ae10029120eb5e2a4e53"
-    sha256 mojave:      "b15db412cd58492ce684fd50a6bec93e18cc42f5543bfd7f45fd6e5636c56291"
-    sha256 high_sierra: "8a570805d563e3ee3d4c374eb5a8e5d649b7364286e738f9d8bef864663073e1"
-    sha256 sierra:      "fe941dffd41e1485f85b3d9bb28a1a30cccfe27d3cac438cc5b71fb347122003"
-    sha256 el_capitan:  "9e39666cb49b6fd60c16b1b4535d0b39363fcc655e6495cc17d74923df13ff27"
+    rebuild 1
+    sha256 arm64_big_sur: "1c1dbfb0ba7a9dc7615aac9f0a16b3e94062333c5db842edecfd9b10849c53ac"
+    sha256 big_sur:       "1d670ee6bdc6441b88295a64d325837e0b9aa845a9457b8b1bb173a6589d4135"
+    sha256 catalina:      "80578cca9b28f6e2afbf628b12cfecdec8560675710e49f73b607a0a7d039c12"
+    sha256 mojave:        "40df76109b9d7cc1f23ad67ab9824029375d083b37c8dc0e417ff5ad5379892c"
   end
 
   head do
