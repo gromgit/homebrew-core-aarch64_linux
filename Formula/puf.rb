35ae6a3834e7dc70e54baa637063795bd01f50a3
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:17 +0100

    puf: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/puf.rb
+++ Formula/puf.rb
@@ -14,7 +14,6 @@ class Puf < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "3153e22f42620f0ceb69f11080e6ba113765d7847cbbb2672f30a7a6766db972"
     sha256 cellar: :any_skip_relocation, el_capitan:    "24952b79335eb08d7a8880a16714e6afe3b73a65f5f26c59b106020198c1b3f3"
     sha256 cellar: :any_skip_relocation, yosemite:      "d96385896fd7831b71af3b05d55f3c5cd2c3a9565f9083c2efe96309989dcf15"
-    sha256 cellar: :any_skip_relocation, mavericks:     "7b76565452fb4f2b03a8b01d6e7495634965ba0bad74906cb907c0642805e5e5"
   end
 
   def install
