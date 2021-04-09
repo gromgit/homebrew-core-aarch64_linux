003eaa079195c1cdfed04457f808354736091e01
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:38:19 +0100

    csv-fix: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/csv-fix.rb
+++ Formula/csv-fix.rb
@@ -12,7 +12,6 @@ class CsvFix < Formula
     sha256 cellar: :any_skip_relocation, sierra:      "b52224f7cd1dd228ffe751c67993f854a8a59f17d6099d40fca02374f1949d02"
     sha256 cellar: :any_skip_relocation, el_capitan:  "ba19053a978b57b6b962f8fa24d099d964ceb90cd28304e3a6c2a7fe0d3abc32"
     sha256 cellar: :any_skip_relocation, yosemite:    "b8dbaf2e14e35cc4c1d7b5d04a5615377f7eeb4d9b1f25fe554b8711511c28f6"
-    sha256 cellar: :any_skip_relocation, mavericks:   "0b86933c8e32830d5abd0f26ef83b1a60e0254da67542b695fd50ab1e3ba2e68"
   end
 
   disable! date: "2020-12-08", because: :repo_removed
