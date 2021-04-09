48e30fe5017c47eaee33f7b96c0d15416fb2617a
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:38:44 +0100

    freealut: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/freealut.rb
+++ Formula/freealut.rb
@@ -14,7 +14,6 @@ class Freealut < Formula
     sha256 cellar: :any, sierra:        "5b592930278516c32e0ecdbf5e244abc4f18b2f766242af145ea0def25df3c99"
     sha256 cellar: :any, el_capitan:    "301e3825367cee8b41747fae0b3495e94b09668d93980032f5fdb92d1c597b62"
     sha256 cellar: :any, yosemite:      "491e2736570843c5d42576563f7797f2f5c13fb3bb97ece3c9396e1fdb7e054a"
-    sha256 cellar: :any, mavericks:     "7438514f5d0b1cc9875fc0db4c4dbf48eb65049cc634c3115da4525a813f13d7"
   end
 
   depends_on "autoconf" => :build
@@ -39,7 +38,7 @@ index 2b26d6d..4001db1 100644
 --- a/configure.ac
 +++ b/configure.ac
 @@ -83,7 +83,8 @@ AC_DEFINE([ALUT_BUILD_LIBRARY], [1], [Define to 1 if you want to build the ALUT
- 
+
  # Checks for libraries. (not perfect yet)
  AC_SEARCH_LIBS([pthread_self], [pthread])
 -AC_SEARCH_LIBS([alGetError], [openal32 openal])
