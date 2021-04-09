2cb9bb8c20f01a6f875a8785194802bd38051315
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:41:15 +0100

    xmlstarlet: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/xmlstarlet.rb
+++ Formula/xmlstarlet.rb
@@ -14,7 +14,6 @@ class Xmlstarlet < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "4958bf868beefb9a2b391c0fe05f5289b67a4cded708d71c4cc5fced130bac55"
     sha256 cellar: :any_skip_relocation, el_capitan:    "2d9a9b5f875b91c78378e7f3df12595528d8e4b9ec9e321131b7f9f78f30acd8"
     sha256 cellar: :any_skip_relocation, yosemite:      "525eafe6cab96cc6e04fef756e25316119b3c96cb61e5f7f51770cd062ad1bec"
-    sha256 cellar: :any_skip_relocation, mavericks:     "7004b98b4dd9195a35f736ba3f6282369a2c63397a710056c5d1ae71d149fa3a"
   end
 
   uses_from_macos "libxslt"
