d205015cfc6a99cba17ccbbea5b2f52ff0473b47
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:38:34 +0100

    enca: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/enca.rb
+++ Formula/enca.rb
@@ -15,7 +15,6 @@ class Enca < Formula
     sha256 sierra:        "0920a4dd92de3f4d7725e6753a37d1cb5f2468063f4020def9167648ff21e046"
     sha256 el_capitan:    "889b9d13ff462aee05bb0afdbe012f6a388a2b5e30e13b55954f94a18db69a13"
     sha256 yosemite:      "c7e41db5725d169800674add5dfc3ab82d888f55f6703e44cda109348ed509e7"
-    sha256 mavericks:     "9dcf7437c2ccedd4a7e10b167ba37c184a0e3aed08714af382f85771196cb0bb"
   end
 
   def install
