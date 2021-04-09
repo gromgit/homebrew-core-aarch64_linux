4fc225391c3bd79b55b265e601b5a16d6cea57e0
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:38:33 +0100

    dxflib: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/dxflib.rb
+++ Formula/dxflib.rb
@@ -18,7 +18,6 @@ class Dxflib < Formula
     sha256 cellar: :any_skip_relocation, sierra:      "db45aa2b00f82b996370eaf1321e0cce79fc3868c42a9524e10adce478139bc2"
     sha256 cellar: :any_skip_relocation, el_capitan:  "aff6c3f5e5bca552c5962e8ef5c43d1dd5fb0630d091e206a164e99ed8b70637"
     sha256 cellar: :any_skip_relocation, yosemite:    "e883aa60c9baab1198671db178c0723e4331ed9fb65ad4d87ba72ca921d7d0b4"
-    sha256 cellar: :any_skip_relocation, mavericks:   "0e591fba7cac298bf4afbb4d7f9895c10865998c6ae64ad4db31c7a33c3377cc"
   end
 
   def install
