27715bcd1716027df4d2debfc7251b5529afa2db
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:38:43 +0100

    flvstreamer: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/flvstreamer.rb
+++ Formula/flvstreamer.rb
@@ -20,7 +20,6 @@ class Flvstreamer < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "e257779383d236611212078f2e6db28d457d51a282d6163806e0232134a046b0"
     sha256 cellar: :any_skip_relocation, el_capitan:    "5a4b649ce0f2c32bca4091f4867a37cca0e8ae2a292d4ef29aa2949530bdd651"
     sha256 cellar: :any_skip_relocation, yosemite:      "243e6ce44b77212ff84e3a739bf2b203c687bdcdd36b17ba24daa5335bf0a151"
-    sha256 cellar: :any_skip_relocation, mavericks:     "26ba92a604070dd27301456d120121618865108b33089191cd7ddcee78fbc465"
   end
 
   conflicts_with "rtmpdump", because: "both install 'rtmpsrv', 'rtmpsuck' and 'streams' binary"
