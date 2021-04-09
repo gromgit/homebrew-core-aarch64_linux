531d74df3b94f74154041c73ba269a56c2d889a3
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:09 +0100

    pgdbf: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/pgdbf.rb
+++ Formula/pgdbf.rb
@@ -14,7 +14,6 @@ class Pgdbf < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "7d0eabf3051e9cf450d985987f89cf0d70476b37202b3b5bdc84ec48e8cb670d"
     sha256 cellar: :any_skip_relocation, el_capitan:    "72ad6b801d25db2008d0ab4badd2bb280f55eb6f6956925ee5620d62d8f06bbb"
     sha256 cellar: :any_skip_relocation, yosemite:      "4042a284cac8abe88e7d1c9e6c003e00a9b8247905739829cd768a824df7993b"
-    sha256 cellar: :any_skip_relocation, mavericks:     "c53298c57bb2d31d82ddce41ed65057d7781de2118857f5f795aaaefe3c00110"
   end
 
   def install
