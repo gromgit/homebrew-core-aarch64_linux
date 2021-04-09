68d58fd0519ccb7695ec924eb98da32631f8534a
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:39:25 +0100

    libident: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/libident.rb
+++ Formula/libident.rb
@@ -13,7 +13,6 @@ class Libident < Formula
     sha256 cellar: :any, sierra:      "4fb8a991f9f83d499b32a814b2d68465327b7b77c0108764e58e4296a968100f"
     sha256 cellar: :any, el_capitan:  "6236d3b4ee424795859cc64da30997ff67f7ac5bd42702e8eabe10f99339ca48"
     sha256 cellar: :any, yosemite:    "53db8e889d8efa34b4a3b6a145bcec2bcb53595e7db0cfdd55c8d857dff3a442"
-    sha256 cellar: :any, mavericks:   "d8e20cc9d2015b83785d4eb05110afc082a6389267df82546cc89e693dd2db6a"
   end
 
   def install
