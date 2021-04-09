b73ae41b25cb8968ffb01ac6a09e5eef28b28f4a
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:39:33 +0100

    libtecla: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/libtecla.rb
+++ Formula/libtecla.rb
@@ -12,7 +12,6 @@ class Libtecla < Formula
     sha256 cellar: :any, sierra:      "21cd696f6e79ae6401dd19f832ac24263f016a62c2d15ec31e25d515bbea5983"
     sha256 cellar: :any, el_capitan:  "3ceb3942ea4ae1434dcc0aea00fa58b6f16787bc1a0067e9497ad4cb050f771a"
     sha256 cellar: :any, yosemite:    "836d6100343197540f079ea7f6b9e5641fd8efc4e331d3492f8be4cd41ced6e9"
-    sha256 cellar: :any, mavericks:   "d7f9b95bbe7540504751d42589e8500a77d15dc3e6b2f7fe501ed872172f1129"
   end
 
   uses_from_macos "ncurses"
