aa70511282c3bf151094a16ea75d5cf71c3c08c4
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:14 +0100

    postmark: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/postmark.rb
+++ Formula/postmark.rb
@@ -11,7 +11,6 @@ class Postmark < Formula
     sha256 cellar: :any_skip_relocation, sierra:      "9d702b7bc49f646fd1623b316fd62c84ee6413e035a81e95866b0ca7240f100e"
     sha256 cellar: :any_skip_relocation, el_capitan:  "784b46fe9883d27d347a44da73413ccf5c589088c0b57da577ebc1c79e64e1e6"
     sha256 cellar: :any_skip_relocation, yosemite:    "7fb38c3960e124a836cdc48650fd5f4d1fc446897b590e0dd6b6b6b5cbdec522"
-    sha256 cellar: :any_skip_relocation, mavericks:   "2ccb3812b371bc02e66d84ff853cb9684f8941485af3287424b4c183205bc649"
   end
 
   disable! date: "2020-12-08", because: :unmaintained
