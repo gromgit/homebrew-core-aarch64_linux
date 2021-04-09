53e2dc6b5713c22cc020f881a20a4787c71a85f4
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:41:15 +0100

    xml-coreutils: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/xml-coreutils.rb
+++ Formula/xml-coreutils.rb
@@ -15,7 +15,6 @@ class XmlCoreutils < Formula
     sha256 cellar: :any, sierra:        "5f7519c9be40f731b0dca6238b3bedf4070f0663fc47ab8e4b0eff02d187718c"
     sha256 cellar: :any, el_capitan:    "19bdcacd49657e78f82fd7743a50266ff4945e644b069ac2c39a8787a57911a5"
     sha256 cellar: :any, yosemite:      "1342c807e5ddc23a72e750f07258864fdf2fc1a8ce9072cb7797955fdd0e3656"
-    sha256 cellar: :any, mavericks:     "6bada2b9690d698b77f9293cf7f1066e77d36c8c07ba2892b2869ddb0516bc6d"
   end
 
   depends_on "s-lang"
