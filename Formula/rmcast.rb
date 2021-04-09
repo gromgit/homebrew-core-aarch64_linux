98e87b2b16633b45e602557a87244335cd2af71f
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:28 +0100

    rmcast: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/rmcast.rb
+++ Formula/rmcast.rb
@@ -13,7 +13,6 @@ class Rmcast < Formula
     sha256 cellar: :any, sierra:        "9ef73c5d52886029cd89d829cdceccca0d03bce0dc72647d8cce6704d492f080"
     sha256 cellar: :any, el_capitan:    "4fe0a1745659bb99748972c2fa0640e6b864e92739ba192a89ed12c0614b1372"
     sha256 cellar: :any, yosemite:      "502e09994a9b455d9040f8e346419a2a3ef0156a73b0249bc161446448729292"
-    sha256 cellar: :any, mavericks:     "1afd20258226dc16873b567b1b5ab920b9e414f4733bfa99fa917074685f2b8a"
   end
 
   def install
