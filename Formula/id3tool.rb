e70ef73f0b663d3fba3d208412c608817cff259a
Nanda H Krishna <nanda.harishankar@gmail.com>
2021-04-10 04:42:27 +0530

    id3tool: add license (#74884)

--- Formula/id3tool.rb
+++ Formula/id3tool.rb
@@ -3,6 +3,7 @@ class Id3tool < Formula
   homepage "http://nekohako.xware.cx/id3tool/"
   url "http://nekohako.xware.cx/id3tool/id3tool-1.2a.tar.gz"
   sha256 "7908d66c5aabe2a53ae8019e8234f4231485d80be4b2fe72c9d04013cff1caec"
+  license "BSD-3-Clause"
 
   livecheck do
     url :homepage
