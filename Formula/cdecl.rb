f9ad6803a8f5545b29dffae7a04aa928157c2f68
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:38:03 +0100

    cdecl: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/cdecl.rb
+++ Formula/cdecl.rb
@@ -13,7 +13,6 @@ class Cdecl < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "1d424613881cf9109d824664fc77fc947f2968b9850d448db4b02c6f0a562b5c"
     sha256 cellar: :any_skip_relocation, el_capitan:    "4f0e990d88823aa9f3d1dcea71ffa442c13640ce82cc9da41f90a1be5ef457dc"
     sha256 cellar: :any_skip_relocation, yosemite:      "e8f53a0e5b3649f0c691c60380b9c77af573387240f3479a41550583fcc4e22c"
-    sha256 cellar: :any_skip_relocation, mavericks:     "b1e1618d0f1bcbb801c669c314c36c72e47e8829950a8bf0899d0517f3036ccc"
   end
 
   def install
