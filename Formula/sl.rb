f709e3145d99d38e0f1b61229a217808efcc771f
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:38 +0100

    sl: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/sl.rb
+++ Formula/sl.rb
@@ -15,7 +15,6 @@ class Sl < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "afd30cb3a99d238a8ac52810834244d5f47fc2ff597db9ad61012bd2014395b9"
     sha256 cellar: :any_skip_relocation, el_capitan:    "f186cb86f4d48929aa671434dbd6be0a861069608098a30dc952697bcca85972"
     sha256 cellar: :any_skip_relocation, yosemite:      "696104243a18e08279d461e66e6a696791e6c36b67df43e361ad6f6de1200440"
-    sha256 cellar: :any_skip_relocation, mavericks:     "c7d4432bfc169f7338eeb0c8300a975495b229d6e85bfff4fdd6bbd11eb8da17"
   end
 
   uses_from_macos "ncurses"
