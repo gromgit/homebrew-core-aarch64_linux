d7f27dce394b661882ae9b96a2cf479e7e8fc8dd
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:39:04 +0100

    id3ed: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/id3ed.rb
+++ Formula/id3ed.rb
@@ -14,7 +14,6 @@ class Id3ed < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "e930552e37599e7926efebaf0d893f888576a26bddef6a91e356cf1b5de15b9e"
     sha256 cellar: :any_skip_relocation, el_capitan:    "6448c8e19c8e0874ed5141193c7db06c443ac6c33ab2f6bbe8811098b063c0d1"
     sha256 cellar: :any_skip_relocation, yosemite:      "8ca64da5c8c0cbbc7ec64af436fcf3a7ae457c8d8a8073887fc63ec4e89c98b9"
-    sha256 cellar: :any_skip_relocation, mavericks:     "8dd4a14922e94245dd016a266aa23c7bcebb18a56e574c8179df83c2d68ff23c"
   end
 
   def install
