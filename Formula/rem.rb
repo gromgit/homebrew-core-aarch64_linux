6c7f68e661b152edb6565d565b6e452d88adafe2
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:25 +0100

    rem: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/rem.rb
+++ Formula/rem.rb
@@ -14,7 +14,6 @@ class Rem < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "326f7a21f696b7614a55a5edeb57e08482ff7b4c72506bcecff5deaa0552828e"
     sha256 cellar: :any_skip_relocation, el_capitan:    "c9892df4f6aa5d58097e4cc4d62388ccbb1e0c02604b1139cfe829d47d992442"
     sha256 cellar: :any_skip_relocation, yosemite:      "d9a6303ff3935923ba53d093e95387caaf24460a4cd7fb7d330fa5c3988b551c"
-    sha256 cellar: :any_skip_relocation, mavericks:     "bf65e89ec4ca486b95f04c1c737627b2e0091af8a5c137795e521b96664d75e2"
   end
 
   depends_on xcode: :build
