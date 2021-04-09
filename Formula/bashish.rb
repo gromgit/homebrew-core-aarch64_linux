429fb7445fceebb719b39ddf234e651494a6c651
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:37:55 +0100

    bashish: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/bashish.rb
+++ Formula/bashish.rb
@@ -14,7 +14,6 @@ class Bashish < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "31134b56c7ad43b04ef186485af8581dbf8d8d8fcf615d259554d9c5adc7233f"
     sha256 cellar: :any_skip_relocation, el_capitan:    "114d2ce95e530c6850bc36a52a1053ecf05185d774ed499bd1725811b3c1b88c"
     sha256 cellar: :any_skip_relocation, yosemite:      "cb3bfee8b595277be04660817eb269e97744d5f49dcac431ae7473982ad5d405"
-    sha256 cellar: :any_skip_relocation, mavericks:     "048bdedf5840f06f7ce38c153663d9c7b440f8829ade2474a47672a9e33b2c12"
   end
 
   depends_on "dialog"
