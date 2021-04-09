7057d2933894853bfc8af41c07ee1bfc173ae9c3
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:38:35 +0100

    ent: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/ent.rb
+++ Formula/ent.rb
@@ -14,7 +14,6 @@ class Ent < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "9f20aba355ecd3310d5e4cd425fe69d88e20e9e1bc128a4b6a97c5d98a828135"
     sha256 cellar: :any_skip_relocation, el_capitan:    "f5244a065b7aafe3ba60077de0072e9b5d231a7fd1eb348cd7f6583a69a08ad3"
     sha256 cellar: :any_skip_relocation, yosemite:      "072e3e71ee3b6813dafb15e53e8b474c1d15f26865b9cd05652e46c220e3926d"
-    sha256 cellar: :any_skip_relocation, mavericks:     "cb4bd5766cdb804092f73a908921e034da352b890fdc34f5cc1f0d56a27d3c3a"
   end
 
   def install
