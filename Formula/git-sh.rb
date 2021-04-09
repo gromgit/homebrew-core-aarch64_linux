ffab44ce2175c40b85638a12bb266978ad00fc86
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:38:53 +0100

    git-sh: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/git-sh.rb
+++ Formula/git-sh.rb
@@ -13,7 +13,6 @@ class GitSh < Formula
     sha256 cellar: :any_skip_relocation, sierra:      "d371fba61367507f5e88818eb1f0630e388d198c37faa957ce410d97675a7f5d"
     sha256 cellar: :any_skip_relocation, el_capitan:  "e30e7836919a5d79712e3fd51a118279b412c44da909053b9b185eb48963323f"
     sha256 cellar: :any_skip_relocation, yosemite:    "cdbc6fc62300722f613f314e2859422edcf938c6807a3039bcf476e02fbe222c"
-    sha256 cellar: :any_skip_relocation, mavericks:   "1b9aa141f32145516db62304dda799611e1fc35ec57275ee75bf566325a6bfa5"
   end
 
   disable! date: "2020-12-08", because: :unmaintained
