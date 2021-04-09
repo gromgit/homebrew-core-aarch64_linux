98d0633bd8e751ce198297159c36d94ea5940456
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:39:15 +0100

    lci: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/lci.rb
+++ Formula/lci.rb
@@ -14,7 +14,6 @@ class Lci < Formula
     sha256 sierra:        "207ba1b6eec2e084af44e03c96a25db72fc6fc6a265824a8aad0550bd3178167"
     sha256 el_capitan:    "88603e7d22fa41138940318a9ce703087062a5466e796a17d636f9ca212a9fc3"
     sha256 yosemite:      "46a84d5644606edb37c1f915df039901ac96d6728345bb95d57ed52fe783a34d"
-    sha256 mavericks:     "d9c2381543f5fff02005a66d07f5c8fb925e4c2c40d87392816947c3c1b22816"
   end
 
   conflicts_with "lolcode", because: "both install `lci` binaries"
