7f635883c336c602c53f2c383ba808f621075303
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:18 +0100

    qdbm: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/qdbm.rb
+++ Formula/qdbm.rb
@@ -15,7 +15,6 @@ class Qdbm < Formula
     sha256 cellar: :any, sierra:        "547ecf82252706d276c8359448b7f4e738264999028b06cd3738af34ba58276c"
     sha256 cellar: :any, el_capitan:    "6fd80b953a53cdf048bf686d2ac3620deda19a022a10a1e7cbd7aea073bf9b6a"
     sha256 cellar: :any, yosemite:      "4784d30c880c089dcef588c7d91d537269404a4917c9b2b1ef8b5123a727cee1"
-    sha256 cellar: :any, mavericks:     "bf5c5c1a087e22f9f06d29e2e139e55f6866ac1826ef725733d108ace6cf4d67"
   end
 
   def install
