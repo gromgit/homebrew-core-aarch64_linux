4a98a5ba6249246634fa471df94bad4b04701684
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:38:20 +0100

    cunit: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/cunit.rb
+++ Formula/cunit.rb
@@ -13,7 +13,6 @@ class Cunit < Formula
     sha256 cellar: :any, sierra:        "dc987998ebcfc175c9c9e70c6b83db4197bd5b79d383235b85ee8a30835785df"
     sha256 cellar: :any, el_capitan:    "0b92535641c86f38bf7a3a1b08a07aa6523e4c0135792dd92829e00579a5e3a9"
     sha256 cellar: :any, yosemite:      "63c1d2905cf9214281b8dad71a7d86fcf57d46ccbeb026a43dc73dddac569840"
-    sha256 cellar: :any, mavericks:     "4add1b332febce1ba3ee01ab98213be0729f3d2cad14f435f4064fec0273c3cd"
   end
 
   depends_on "autoconf" => :build
