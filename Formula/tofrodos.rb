17381472e2e7910f27c54f9bdeada66960526712
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:53 +0100

    tofrodos: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/tofrodos.rb
+++ Formula/tofrodos.rb
@@ -14,7 +14,6 @@ class Tofrodos < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "3d5363cda2170ce2fbcb7e03c84f715b62ead1e5646000dd06395f5677fd2269"
     sha256 cellar: :any_skip_relocation, el_capitan:    "4a2b22ff08d0fb65c80be7359be2f04d12b70f4e6d490b96cb819ea69b3e3d88"
     sha256 cellar: :any_skip_relocation, yosemite:      "4a5427c6870c3d4822ef4da3ddd8d79c18b91e5b7f14edb4aa449a53da70114e"
-    sha256 cellar: :any_skip_relocation, mavericks:     "c9759ec570e6a284b250563b8d66076401641f40c1836e293f4eab82cc9fe4ff"
   end
 
   def install
