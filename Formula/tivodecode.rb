beca65b4ab9a76057a0e96c252c786c7691662d7
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:52 +0100

    tivodecode: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/tivodecode.rb
+++ Formula/tivodecode.rb
@@ -18,7 +18,6 @@ class Tivodecode < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "5682668b2e721933054656cebc49ccb46c382428b77409d94251c6f1dfd3092d"
     sha256 cellar: :any_skip_relocation, el_capitan:    "d50450e62c6fcf71643ceaf5f33dcf4e904e389c89597ccbe148de3053839ccd"
     sha256 cellar: :any_skip_relocation, yosemite:      "cb74afdc87eca67025849836bee29e04f6c8e7755753a9daa52c84cfc9201cd8"
-    sha256 cellar: :any_skip_relocation, mavericks:     "7622e7c60ecd22127b454e303d7c15b14c6cc79bcddf00c0de0de58e9350b5bd"
   end
 
   def install
