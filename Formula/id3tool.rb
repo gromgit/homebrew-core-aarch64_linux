111349b17e914b8b0641f7bc83f390a49e8db847
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:39:05 +0100

    id3tool: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/id3tool.rb
+++ Formula/id3tool.rb
@@ -13,7 +13,6 @@ class Id3tool < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "2b7bcf184092881638221c606ae50879d02478bf9d433dc946f63dbe2e68368a"
     sha256 cellar: :any_skip_relocation, el_capitan:    "b5b4d49b4710a67df5200149873dd8c17131ef4a4eaac722a2095b1445d053ff"
     sha256 cellar: :any_skip_relocation, yosemite:      "348a229d26dd699013d8e3372c5382da10b12d02d286304e61d9f2c71ed77101"
-    sha256 cellar: :any_skip_relocation, mavericks:     "dd989abacf0c1113879b6864a5ca08a59ac766a320897b9c323e5f7eaeeb2e70"
   end
 
   def install
