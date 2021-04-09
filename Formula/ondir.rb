ab300a72b8bee5d5bf1e3a8c5adeaa4d8faf8b45
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:04 +0100

    ondir: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/ondir.rb
+++ Formula/ondir.rb
@@ -15,7 +15,6 @@ class Ondir < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "90e85060a76337368083c889379b71cda5994ab163b73337050819472f41800c"
     sha256 cellar: :any_skip_relocation, el_capitan:    "8d841a2a8b98a512265dc05deb3ea74e7458a4d5412da786f595c31420b7fadd"
     sha256 cellar: :any_skip_relocation, yosemite:      "3d7b419d963bcd2be6d04cb3f666c8c58866f9556251f6efcb2f0b6abcad5902"
-    sha256 cellar: :any_skip_relocation, mavericks:     "a994efec34c5c9edd1b014959b3fe1a1a95c1d9ece14c5d7d7c51c2b421c7a11"
   end
 
   def install
