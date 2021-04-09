48f562ec69c5264918b8a310e48fe5421b7c7982
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:39:22 +0100

    libfishsound: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/libfishsound.rb
+++ Formula/libfishsound.rb
@@ -15,7 +15,6 @@ class Libfishsound < Formula
     sha256 cellar: :any, sierra:        "50187bc6adea9322f20e1706d66859c941d6d2e8d1d8bfab091f088b20061760"
     sha256 cellar: :any, el_capitan:    "9cf94c3c6963895940e8720aef21c29b001257c918fce6b65685c33f8430f0e4"
     sha256 cellar: :any, yosemite:      "4fcfc4270d73ac2b0e8d8a4d1fe6b94a1093502b802ed327febb5286ad5140b9"
-    sha256 cellar: :any, mavericks:     "b8c54b7d3b2bc5e433b20f89f67c6cb3d03b18e0881126a526ae1ff028d8c220"
   end
 
   depends_on "pkg-config" => :build
