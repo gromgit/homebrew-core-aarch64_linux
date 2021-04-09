caa47754b036520fa49485eeda12f12622724c9a
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:38:31 +0100

    duff: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/duff.rb
+++ Formula/duff.rb
@@ -13,7 +13,6 @@ class Duff < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "2af1262a9b02e687c0efc14eed3d837920ab746fe8fca9b12b9361c4729f06ef"
     sha256 cellar: :any_skip_relocation, el_capitan:    "8a469e92a6303d80752ebc80ade382261d263b9c7226ca6652eddc8954e5ff2f"
     sha256 cellar: :any_skip_relocation, yosemite:      "927ba61ce39cf9be33f796197063b1a6865bbc2db2f4b1340ad6786acf0494df"
-    sha256 cellar: :any_skip_relocation, mavericks:     "a66cbddeb95dd67055ef6db7acf729a925427778d6cc88ed2ef52a2dd40b7856"
   end
 
   def install
