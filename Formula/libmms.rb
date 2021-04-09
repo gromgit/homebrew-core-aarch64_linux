f52432a2897e3dac2fd6666ba761bdf5edb18c9a
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:39:26 +0100

    libmms: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/libmms.rb
+++ Formula/libmms.rb
@@ -14,7 +14,6 @@ class Libmms < Formula
     sha256 cellar: :any, sierra:        "5319927f799dd20effbfc9f8bb90ebc844b39852c433bf434ab6b36c11c36417"
     sha256 cellar: :any, el_capitan:    "61c4dd24598198386342dd9c700e218b6b83c82627efc781daa89acfaca96066"
     sha256 cellar: :any, yosemite:      "f915d916dd81ad9f767b6905e166dae07df72e70dc0c844c8011abed9f144252"
-    sha256 cellar: :any, mavericks:     "b55ae55a0d684ba1e3654eee96769d206ce0c22a4ab7bad5241eb1c51bda7778"
   end
 
   depends_on "pkg-config" => :build
