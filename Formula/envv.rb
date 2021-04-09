8b18902db1ea39ab95d6f72a69a46252c2b7103b
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:38:36 +0100

    envv: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/envv.rb
+++ Formula/envv.rb
@@ -14,7 +14,6 @@ class Envv < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "cc30a2317f78124c609d6313a33cea58c9d428a95903966da4cb42051630ef97"
     sha256 cellar: :any_skip_relocation, el_capitan:    "3b2fb0b35749280461b3982797ceea34bfa42d63cb5c6547986cf106669ee744"
     sha256 cellar: :any_skip_relocation, yosemite:      "90a718606ec61e5a0e494d3e41b7d87048de803567f4ba2c65231fe41880bd97"
-    sha256 cellar: :any_skip_relocation, mavericks:     "9ac7617d6475a67c60604fcd72d0ae1a5515df331944e8fb2c2a9223c16e3504"
   end
 
   def install
