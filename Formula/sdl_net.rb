4df151edc89b6a0f4862ec3cee3f6971ac4f67ee
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:33 +0100

    sdl_net: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/sdl_net.rb
+++ Formula/sdl_net.rb
@@ -18,7 +18,6 @@ class SdlNet < Formula
     sha256 cellar: :any, sierra:        "65cc3ae3104620de06f03ca0d9b3a545d90f2a36955dcb528f5f42af6db11bcf"
     sha256 cellar: :any, el_capitan:    "036938975b4060fdc944c2258a8d1d5d73f536860a9c807116e6c4fb2aa65dc8"
     sha256 cellar: :any, yosemite:      "fe6b8eda1d640db450ed12f79feb731d49a62263c4b83601d69659498d697538"
-    sha256 cellar: :any, mavericks:     "99f9035f95e548ea81eb11ac9c06ae5eab8d2797ea9ca03ac074fe30bb357748"
   end
 
   depends_on "pkg-config" => :build
