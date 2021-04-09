4c773eb17dd3ca23135ce815eac9089712b55fee
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:41:19 +0100

    zsync: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/zsync.rb
+++ Formula/zsync.rb
@@ -13,7 +13,6 @@ class Zsync < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "8d6e7eade289c62689e752151021e7bccac7900a5e7217e8885f2c38aec42c2c"
     sha256 cellar: :any_skip_relocation, el_capitan:    "9bbe0e102ca6a2b7ca57af6b2b29984f7da59ce97d15ce550bbbb206f1ad1815"
     sha256 cellar: :any_skip_relocation, yosemite:      "b7436466e25e1fe44e2169059d613d9df279a69c31183f6cacce953fc6a47e8b"
-    sha256 cellar: :any_skip_relocation, mavericks:     "c44baf1fc7c83e88bb255307121de1546a0b89d43048e6c0f951648a649bc5fd"
   end
 
   def install
