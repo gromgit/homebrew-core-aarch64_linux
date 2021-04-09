18cf69faf2288f6828363eba14276879ddba46ae
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:41:11 +0100

    whitedb: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/whitedb.rb
+++ Formula/whitedb.rb
@@ -12,7 +12,6 @@ class Whitedb < Formula
     sha256 cellar: :any, sierra:      "44639bc83668def2e81b68318dbdb5347f9262937ddb6cfdfd7303aae1ce05a6"
     sha256 cellar: :any, el_capitan:  "c0f4e666e9cc755bbff0711a1494c9705928a34a565701147bae31793f505163"
     sha256 cellar: :any, yosemite:    "0fa38dca524c08f51fa724fb49df5a3ebdde46a3251b2a282d5343b36c4c249f"
-    sha256 cellar: :any, mavericks:   "ba756975f0dbdfa4259a5a4271414765644b0abe8c771d0c091238909f0968d2"
   end
 
   disable! date: "2020-12-08", because: :unmaintained
