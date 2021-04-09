242c8a03655cfbee8cdb1e03f5e6c7d48ee45752
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:08 +0100

    pelikan: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/pelikan.rb
+++ Formula/pelikan.rb
@@ -15,7 +15,6 @@ class Pelikan < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "37a675674b7ef33f07099029042f56c054f09b5d22400010d583fbfa41c0ce50"
     sha256 cellar: :any_skip_relocation, el_capitan:    "e314ce6288bf76e271bf69ce844e2e846b16cad68ce635faf1e5130c3c6911d0"
     sha256 cellar: :any_skip_relocation, yosemite:      "ab04b8488e6272d0000c8e67842c4b286eb23459a6de9e9a392f14aa87c9978e"
-    sha256 cellar: :any_skip_relocation, mavericks:     "80459134cbab7aa94ab55d38488b2058696f7408869306f75e80cfa0350ed40d"
   end
 
   depends_on "cmake" => :build
