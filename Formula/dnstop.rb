e2b3ae235b3a2fc691b3051da8d409c99d35d39d
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:38:27 +0100

    dnstop: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/dnstop.rb
+++ Formula/dnstop.rb
@@ -15,7 +15,6 @@ class Dnstop < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "dc995c2857fdd5093ae753844ce5c45ed00bae59184528a184e0313b25882802"
     sha256 cellar: :any_skip_relocation, el_capitan:    "1d5b1ad056475ce9a27f40b48cbbf58421e4eb66fd134ac318413de2d025db66"
     sha256 cellar: :any_skip_relocation, yosemite:      "aa3b72d1432e7c13b9b7e0722cde3f7fafef17aff557489662029698929638dc"
-    sha256 cellar: :any_skip_relocation, mavericks:     "4a57a6144a94b3eb2cce64ec7f8f97447eafd1c0be2c5789593920d045e9a189"
   end
 
   def install
