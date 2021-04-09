0355f09b1362dd1d32e96e27bb24ec99d3345572
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:08 +0100

    perceptualdiff: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/perceptualdiff.rb
+++ Formula/perceptualdiff.rb
@@ -13,7 +13,6 @@ class Perceptualdiff < Formula
     sha256 cellar: :any, sierra:        "eb2da458eda1cebc7872b2621c96e5aa627d9711f8d31fb792cb092d92d060db"
     sha256 cellar: :any, el_capitan:    "d47d680df91ee88897f95123e6b9f972351a603a5f4921726b2877cc2e67924f"
     sha256 cellar: :any, yosemite:      "7a1956479cc1176b7340f4614db1b556318513b6359a025dca942142956b65d9"
-    sha256 cellar: :any, mavericks:     "99baa893fa0ceaa71fd8c4443a315c2d8e51c567f65a6917eaa5e4ab3952a900"
   end
 
   depends_on "cmake" => :build
