6dfbd8acc1692abde5f9850ddcff35a5394355f0
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:37 +0100

    sipcalc: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/sipcalc.rb
+++ Formula/sipcalc.rb
@@ -18,7 +18,6 @@ class Sipcalc < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "1ccdaec0a816dde9f7caa0f7a77cd984ece78a61a5886032c4c8821915753482"
     sha256 cellar: :any_skip_relocation, el_capitan:    "56aa686252ac703ed3dbe91f5737ec4d4b95d52516f4ab52947df15b77d1c58f"
     sha256 cellar: :any_skip_relocation, yosemite:      "6b2fc300755693d382fd5ea971c272a7c8c7bff49614dd88d8db4270aa496012"
-    sha256 cellar: :any_skip_relocation, mavericks:     "7ddf7b200984de97143828faf6385314a2ff3f4436432d810e5aaf7dfe44e78c"
   end
 
   def install
