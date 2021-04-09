c3687b93d982849903ea73aa31236252ec59054b
Nanda H Krishna <me@nandahkrishna.com>
2021-04-09 20:53:12 +0000

    benthos: update 3.44.0 bottle.

--- Formula/benthos.rb
+++ Formula/benthos.rb
@@ -6,10 +6,11 @@ class Benthos < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d4438bcb55065abd98e061bffe4629533f7a8ec0c47436fd14c1fb722670dd42"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f4eedb5e89f0ed9ae398f90fc98e559fb23c25cc460274344d73e571647fab43"
-    sha256 cellar: :any_skip_relocation, catalina:      "af2b9a35eb7274f97b12c125d11c81a521e7ccdcd2c41fbdbd0c4f03ebcfd645"
-    sha256 cellar: :any_skip_relocation, mojave:        "b73043150f8ae385adab04f7c421bfc2fa4492e60cb7cec24e3b7aae7111d86a"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b8fbfdd77c61c2286dc23c20e215b6942a8c8cd8182c4c2aac1089741697298c"
+    sha256 cellar: :any_skip_relocation, big_sur:       "e86b590065c47dcd1c1c9c5ace5a0c825d36d28106cc48b7d5baaf85c84aee56"
+    sha256 cellar: :any_skip_relocation, catalina:      "04754d891224be6ae4312a5dc715ea904854285294c167fa4c9ca267a8860b88"
+    sha256 cellar: :any_skip_relocation, mojave:        "fe150ab7cd45f4ec33423940f6831cbd992eb45dfc109b54aea922082abb1fdf"
   end
 
   depends_on "go" => :build
