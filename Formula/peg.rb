147d0a65fc74521691a77929f25256a3f6742689
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:07 +0100

    peg: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/peg.rb
+++ Formula/peg.rb
@@ -20,7 +20,6 @@ class Peg < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "15dfb147f388a8a486714d17d519a1ad1195f79bad5843d37726e8efaab1ae79"
     sha256 cellar: :any_skip_relocation, el_capitan:    "44d0ab83d1bc3ee71294d328dc70dd14206b8d8ddf05a195f2cdf354d746d5dc"
     sha256 cellar: :any_skip_relocation, yosemite:      "9abe69e43c8e2672aa7b5b26df5c80976c2d0365b5d85d672e8948cebe88646f"
-    sha256 cellar: :any_skip_relocation, mavericks:     "bbe71ecc8acb17bdf2538f41ae56472bc104a69e310cfd533565507c3468c53c"
   end
 
   def install
