24ba0815ec35573f62addf6f8a593fc37aa60f93
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:38:55 +0100

    globe: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/globe.rb
+++ Formula/globe.rb
@@ -14,7 +14,6 @@ class Globe < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "20488fcd0137e0d2a05ea3bfa91adc2f45460f05bb01f26e41005ccafc3e8c54"
     sha256 cellar: :any_skip_relocation, el_capitan:    "11acded7be5d1ba22260d039e3daf4fdc4cac49ebcd234c879da655a1532c22f"
     sha256 cellar: :any_skip_relocation, yosemite:      "a3ccdf74813e704ab1c8d50bb32f3f9b3f62110c8a6a143e3df85eb6ab7ecd7d"
-    sha256 cellar: :any_skip_relocation, mavericks:     "8ff4dd96c5ab3c846ac61ff66c866e3d4969560611405db6e20436ff75494715"
   end
 
   def install
