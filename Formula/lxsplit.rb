471b98ab4750af017f28e190edf22e717fd9f7e3
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:39:39 +0100

    lxsplit: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/lxsplit.rb
+++ Formula/lxsplit.rb
@@ -14,7 +14,6 @@ class Lxsplit < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "f4d271c94546ca73b9e5262ff53bf7b51dcde2a83998d5a2e4b663109f2f69d8"
     sha256 cellar: :any_skip_relocation, el_capitan:    "25699d54183a01f446015fb02521a50b3967ef2d250e56bb1fe3fd0a5aaec2e1"
     sha256 cellar: :any_skip_relocation, yosemite:      "d7d8d9eb204599056a4e109c63114c90e3be797d2be700e114cc3675d8eba0bb"
-    sha256 cellar: :any_skip_relocation, mavericks:     "da0ee88012d21dc120c7247fd856305fe14a213e38babcd39ab652af06483b7e"
   end
 
   def install
