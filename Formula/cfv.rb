28cbee6a5d12d2ea38b9daf1eefb99430146d92f
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:38:05 +0100

    cfv: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/cfv.rb
+++ Formula/cfv.rb
@@ -15,7 +15,6 @@ class Cfv < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "449f4b10a0371005f04bffa6271364824a83fbb68cb15208168c19457b987b6e"
     sha256 cellar: :any_skip_relocation, el_capitan:    "49b83783b5737a364504fdd9fd09672134e0103c7bb8152741d67fca455fde04"
     sha256 cellar: :any_skip_relocation, yosemite:      "df85f8ee2901bb0b3033a3158d04848bb2fbc455f8af12d7d6eb6869c1471ed9"
-    sha256 cellar: :any_skip_relocation, mavericks:     "f251efc545293925f29093f8574495ebbbfe1cbad2a285a7a531e357310e3d1f"
   end
 
   def install
