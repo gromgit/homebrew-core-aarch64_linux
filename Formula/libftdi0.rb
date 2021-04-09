46982ac5601a3f88254e36001fd28ed497b2bb57
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:39:23 +0100

    libftdi0: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/libftdi0.rb
+++ Formula/libftdi0.rb
@@ -19,7 +19,6 @@ class Libftdi0 < Formula
     sha256 cellar: :any, sierra:        "f89e79f5eb94d55e130dcc55deca87f1241b386bf45070dda52c2c22707ead15"
     sha256 cellar: :any, el_capitan:    "8fbc5ef456600b919430d86dd4e7090ae1f1064e4a51d631ab9bd7b39887ead2"
     sha256 cellar: :any, yosemite:      "92ecdb3a110e1abcba05561f0def8e573d1f8174d4e04247375dd5cd47d3bc24"
-    sha256 cellar: :any, mavericks:     "29d786dcb87e4251c4cbd25c0d5e215eb248b74304f175054b31cbd6a464c071"
   end
 
   depends_on "libusb-compat"
