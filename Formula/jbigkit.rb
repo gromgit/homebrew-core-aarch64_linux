04f57511ec3984a54578f17f3284b2dd55da8b5a
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:39:09 +0100

    jbigkit: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/jbigkit.rb
+++ Formula/jbigkit.rb
@@ -16,7 +16,6 @@ class Jbigkit < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "831dd1ec7e8013ddc6c23641a21292eae26f397e8b61d95382a6240f18fc5602"
     sha256 cellar: :any_skip_relocation, el_capitan:    "bdec08cd92dd59183b698c6bbd9072881fdfce64b4ecb6182e405e0f2ad26c00"
     sha256 cellar: :any_skip_relocation, yosemite:      "764396342e87b84253aa06f5046f90c778cacca998ce970900cb2fdf1cfdc3fa"
-    sha256 cellar: :any_skip_relocation, mavericks:     "0ce925915b984307d2e679622138143c5cc5baf832b0a16003fa1e6111a5df9f"
   end
 
   conflicts_with "netpbm", because: "both install `pbm.5` and `pgm.5` files"
