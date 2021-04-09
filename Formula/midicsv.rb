538249683cd12d4c67233cadb4793a8779cebad4
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:39:49 +0100

    midicsv: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/midicsv.rb
+++ Formula/midicsv.rb
@@ -13,7 +13,6 @@ class Midicsv < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "314a21ac6aaad39594a54bae4bf3ecc64e3ef0cd655e7c18c6ff05ebd72c9b86"
     sha256 cellar: :any_skip_relocation, el_capitan:    "230ba9ec9cbb40c2c128c1a063152fd07888210f59bf37f1f68bcd2f33d4d863"
     sha256 cellar: :any_skip_relocation, yosemite:      "f649e02908dee31c35e9b73954b8faad2da70e1c7e621fddb64f18dbad897036"
-    sha256 cellar: :any_skip_relocation, mavericks:     "289d13313a2f5d7dbc8ebf61450ba336bdba9849be352dc5b705c0d2c4b13d3e"
   end
 
   def install
