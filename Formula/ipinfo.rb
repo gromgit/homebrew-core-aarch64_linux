3d47a5e43e6a6d4b07284273e6d558c2811f634f
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:39:08 +0100

    ipinfo: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/ipinfo.rb
+++ Formula/ipinfo.rb
@@ -13,7 +13,6 @@ class Ipinfo < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "255c10eb2f0f885ba301fa2977ae3c45b5f7117388739adb58ce4312515ff98f"
     sha256 cellar: :any_skip_relocation, el_capitan:    "ecb331ae035cf5963afc8e8adf371d80f936960bf0d5ba379b18761263a1b040"
     sha256 cellar: :any_skip_relocation, yosemite:      "e1ce332c726d060521e97a5402746a60778d91beaf28704d9ce5bb6e17451fb3"
-    sha256 cellar: :any_skip_relocation, mavericks:     "686fe99fef85ecbfdcc9c922f6cda898362d70bb9f5b9b7e1aeba8e30c284196"
   end
 
   def install
