933424157042202da7951a32998485339187b1bf
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:38:58 +0100

    gvp: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/gvp.rb
+++ Formula/gvp.rb
@@ -14,7 +14,6 @@ class Gvp < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "2405a1e481ebfafcd4fbfdc2874feacc402b851fafdc69596d1afa120924c157"
     sha256 cellar: :any_skip_relocation, el_capitan:    "ddd00ded9d21c3ecfe23e807619d3ab1b3011bc586db0d7d4aa8d5d87e3689c6"
     sha256 cellar: :any_skip_relocation, yosemite:      "5e63da6d9c8d065277491db1658fee5c53089f7dd1bf1180e5d9e7172b376cde"
-    sha256 cellar: :any_skip_relocation, mavericks:     "afbe7d649883c750c182976a7c09035fe30f4d56b1b5859c8d214c01334874f7"
   end
 
   # Upstream fix for "syntax error near unexpected token `;'"
