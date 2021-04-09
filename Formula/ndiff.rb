3f2aabe1f59c40d8aeed384497f3dc24de7cf9fe
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:01 +0100

    ndiff: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/ndiff.rb
+++ Formula/ndiff.rb
@@ -14,7 +14,6 @@ class Ndiff < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "ed6f753f9fe240486de3b6589350fcc0e7afbe345ae2e01bf6b47e132de9be4e"
     sha256 cellar: :any_skip_relocation, el_capitan:    "6faf20ce4c88110019c76cc4253cd65e5743fab7cff109fc8a7d41c8f411012e"
     sha256 cellar: :any_skip_relocation, yosemite:      "80adff8ec563059b7f49005c7e567b950ca58b392a4a5db18ae4957fe18b296d"
-    sha256 cellar: :any_skip_relocation, mavericks:     "7451587f9747af6e7ffd0e5dbacd337a72cd9b7f3c45a1240c2033e0731d5d46"
   end
 
   conflicts_with "nmap", because: "both install `ndiff` binaries"
