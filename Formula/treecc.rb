b48550d3d2a018147b6b2a0d9b3fd7c7fb6718b8
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:56 +0100

    treecc: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/treecc.rb
+++ Formula/treecc.rb
@@ -19,7 +19,6 @@ class Treecc < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "0b3e61d5a910222d170fcee80d094be0dcd2707b7bebc6d40667a8f25b4b2e5c"
     sha256 cellar: :any_skip_relocation, el_capitan:    "e74d23594113e594ad8021fe55b0f0f863fcd4b01140c3fd8b1a5f2bb6c8ad74"
     sha256 cellar: :any_skip_relocation, yosemite:      "595dada9ecb2cef6d3e225e99a98997968d15f8009038511c464b6499cbcd872"
-    sha256 cellar: :any_skip_relocation, mavericks:     "9f9a9e6a66c9e0a60888ad2af502070683637b5cd19dec6e080211a45c3313e6"
   end
 
   def install
