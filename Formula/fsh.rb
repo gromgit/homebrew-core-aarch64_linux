7ebae2142cc0604559b9a1142805b536cff0d148
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:38:45 +0100

    fsh: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/fsh.rb
+++ Formula/fsh.rb
@@ -14,7 +14,6 @@ class Fsh < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "13a7134ef9d20899642d8dd96e77603d74573cf3a0e1ef5063f6eefc856dbd37"
     sha256 cellar: :any_skip_relocation, el_capitan:    "cec52eb07f9db79b15ff5907f30363bbb538c01b7c4eb7ae8634e7ce17eb5431"
     sha256 cellar: :any_skip_relocation, yosemite:      "8a49ad906b045a293259c199fd5d1737894099c487b1bfc83fb60d18acf065ac"
-    sha256 cellar: :any_skip_relocation, mavericks:     "ed852d51f5a0a4024d4a195c9cffd604758a11a115620a3da0975b541c912770"
   end
 
   def install
