5e9d7175f68d705c3e2d43cbdddc999dd044bc86
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:39:26 +0100

    libmetalink: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/libmetalink.rb
+++ Formula/libmetalink.rb
@@ -19,7 +19,6 @@ class Libmetalink < Formula
     sha256 cellar: :any, sierra:        "87711da9c52d75d2d6fed5829a70b0f6561f55fdcf1129266001b6e96bc69b1b"
     sha256 cellar: :any, el_capitan:    "4ca3f82ac2e2520c677def661168c6ba14ee2b8f0139fbe33cf5be66745244d3"
     sha256 cellar: :any, yosemite:      "e627cde406e135a735fd2586622d2fa05cf6ef9fda8a0ea08d09ddfce20dca75"
-    sha256 cellar: :any, mavericks:     "9ff31fac7dcd9f9231c067eff04d9b59e1de9f8416142d519799746dbfa34c24"
   end
 
   depends_on "pkg-config" => :build
