b2930f8ebbdbd1e910dd9b22aa44aefaa7ccd85d
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:39:11 +0100

    jxrlib: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/jxrlib.rb
+++ Formula/jxrlib.rb
@@ -13,7 +13,6 @@ class Jxrlib < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "0eb8602ab389d9fff5bce803b085af322309592dc41aa0902b223e353a9f8abc"
     sha256 cellar: :any_skip_relocation, el_capitan:    "47c308b47ec8227d813a21c8092b32ada9b7f862aef102bf619d6bb19f0144e1"
     sha256 cellar: :any_skip_relocation, yosemite:      "0dae977caf9e34289c9dd09f7e12bdf7158ccc42d9fa2dc00b4164b82c1caf3f"
-    sha256 cellar: :any_skip_relocation, mavericks:     "cc49654146951a561ca6fce7c485f7f3f3511ab5549a7db8263e4e8ddc95e740"
   end
 
   def install
