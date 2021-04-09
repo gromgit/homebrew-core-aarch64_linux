48e27dd3d7e593e3e68ad0e997a78b89545f4b8a
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:39:01 +0100

    hexcurse: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/hexcurse.rb
+++ Formula/hexcurse.rb
@@ -14,7 +14,6 @@ class Hexcurse < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "580efaffc5d8dccb0f4f6532ad5be35e372c6b8d91dfb6d3930aa773c9bf7ea1"
     sha256 cellar: :any_skip_relocation, el_capitan:    "ffe690a87522627dc0088c391f7237fc6a3f2aa12fc5a3487c0aa6694905dc4d"
     sha256 cellar: :any_skip_relocation, yosemite:      "ef5644e4e96604f6f3bbe802e7824a7fd82e01163d532d0e2be6a93cc6595eab"
-    sha256 cellar: :any_skip_relocation, mavericks:     "fe4f9ddeaa505e9b8554f1118a0298fecf8008c2e4d4569675b349e51feba00b"
   end
 
   def install
