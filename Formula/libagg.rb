bbfccafa27c4e9391509456990761b553227f4de
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:39:17 +0100

    libagg: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/libagg.rb
+++ Formula/libagg.rb
@@ -25,7 +25,6 @@ class Libagg < Formula
     sha256 cellar: :any, sierra:        "872f49f0fd96ee65dca4bedba3e82c4fcf0e0b0c45de15afc82a9e70e0f0623c"
     sha256 cellar: :any, el_capitan:    "5b9ab7a9ef2f4075bd55561f0fda99c7203a70020288747ebf90cfc1b2ee626b"
     sha256 cellar: :any, yosemite:      "9d3da78ab9824db755cbfeb9e6596527db1ace71525cb079465b1a9fb1c00417"
-    sha256 cellar: :any, mavericks:     "9704ec5652775cbab7af51e48eb42b19cb55f7cdb5894e6e1abac3e478581e2a"
   end
 
   depends_on "autoconf" => :build
