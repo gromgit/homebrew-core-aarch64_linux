69269dcc381cc1f799eae71cdbbf3bef47ce3bb2
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:41:04 +0100

    vilistextum: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/vilistextum.rb
+++ Formula/vilistextum.rb
@@ -14,7 +14,6 @@ class Vilistextum < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "b8fa6ddde71b9b86128e12bbc343935ca5ec58e15d28da2a1a9972a23df9becd"
     sha256 cellar: :any_skip_relocation, el_capitan:    "d46bae51c7e9a7193a735660af8583960c98e50f03aa33c8a9d81c22b2d9bf61"
     sha256 cellar: :any_skip_relocation, yosemite:      "77ab66b58db8649e9444584b5ee85e6c6c7badeb665425263c50282367eea003"
-    sha256 cellar: :any_skip_relocation, mavericks:     "be608cbb3892c81f92392253a86456f042e804b55c58e1331a01531781137f74"
   end
 
   def install
