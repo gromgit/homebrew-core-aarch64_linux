dc7db4e46721791500eb1341c1b93297965fb930
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:07 +0100

    pcal: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/pcal.rb
+++ Formula/pcal.rb
@@ -13,7 +13,6 @@ class Pcal < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "134df5abc458995e6092041db145e9bca45e2ff71eeeec9de410d497afbe7177"
     sha256 cellar: :any_skip_relocation, el_capitan:    "271667aef1031a0007e042fb3f933708aa33398d6bf9982a7353e6023d0d955c"
     sha256 cellar: :any_skip_relocation, yosemite:      "f88d2fc2ede97fd94333dea90617d02405b008ef359edb694926f4e476c6ae53"
-    sha256 cellar: :any_skip_relocation, mavericks:     "b0662dd7e1841adf260b267fa8f9187c60c87c7b7c07075e6179cdabd230e2bd"
   end
 
   def install
