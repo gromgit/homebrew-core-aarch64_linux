ec526f10cf04f476910486d5cd7705f88a8290fd
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:39:45 +0100

    mboxgrep: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/mboxgrep.rb
+++ Formula/mboxgrep.rb
@@ -12,7 +12,6 @@ class Mboxgrep < Formula
     sha256 cellar: :any, sierra:      "44a294d075cb08e577c4d1c1e45c222bea93f3c8488f9bed54ded5b36797f536"
     sha256 cellar: :any, el_capitan:  "ecc1d1a83a7ffbc3414feb24c970b89509737f7d1de5c0d8dbd71ba55e008220"
     sha256 cellar: :any, yosemite:    "08cbb574005db6e34cc191ae4f46670ca35af252e4e6bbc2041a92e32397bc49"
-    sha256 cellar: :any, mavericks:   "bb5cefa83e2fb8b37dac858f3119aedb338b29e0eb2715cbff08b4644689ad86"
   end
 
   disable! date: "2020-12-08", because: :unmaintained
