e261363ffe0aa3fc2f3b8164b9e4cef6cacfbbc4
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:40:55 +0100

    torrentcheck: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/torrentcheck.rb
+++ Formula/torrentcheck.rb
@@ -11,7 +11,6 @@ class Torrentcheck < Formula
     sha256 cellar: :any_skip_relocation, sierra:      "c1169f112306b1f235297cba2c8920894e063b9b1f774e36be75f3f2c194bda5"
     sha256 cellar: :any_skip_relocation, el_capitan:  "ea6fbaa86be1c799c3baa4405aa1a750c2b3e1deb4bea0a412027d427f0922da"
     sha256 cellar: :any_skip_relocation, yosemite:    "46426cdf1c627f448d54895b7f08379b90948030be346753104f5f6a5fabca3b"
-    sha256 cellar: :any_skip_relocation, mavericks:   "ed300dfc8d1f7f7fe3c9c161b8f86cc6a379c7a4cca3914bb0c665d66ec6596a"
   end
 
   disable! date: "2020-12-08", because: :unmaintained
