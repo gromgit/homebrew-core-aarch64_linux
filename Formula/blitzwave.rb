3a385f0ba4c3884d28acaac72caf2dbd391fa22b
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 15:37:58 +0100

    blitzwave: remove mavericks bottle.
    We haven't supported Mavericks for a while.

--- Formula/blitzwave.rb
+++ Formula/blitzwave.rb
@@ -13,7 +13,6 @@ class Blitzwave < Formula
     sha256 cellar: :any, sierra:      "1722c7dfacc458ca54d05dcc06a5281bbe48935f66eaaf7374c2551ad50298a8"
     sha256 cellar: :any, el_capitan:  "be9ba4deb07a468b23f430fe2f0896206b120f70e07f94d48267448c0524d3bc"
     sha256 cellar: :any, yosemite:    "609c85eec329a8aa988a2b026522642f41b392039936661ce428d13887dfa84d"
-    sha256 cellar: :any, mavericks:   "af7d02c7520db927c0d835992719922753e89d26588a00a2a53601a1e5aabd8b"
   end
 
   depends_on "autoconf" => :build
