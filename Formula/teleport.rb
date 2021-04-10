7c2bbe39a4adabd61a12427e82b3b2cacdeddaa7
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-04-10 01:35:09 +0000

    teleport: update 6.1.0 bottle.

--- Formula/teleport.rb
+++ Formula/teleport.rb
@@ -17,10 +17,10 @@ class Teleport < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c2cc6a12fc79fc612166b04c25832199eedc4a97fa313eef9aceda05524a93d6"
-    sha256 cellar: :any_skip_relocation, big_sur:       "85525c1637ed59237926cbeba0ddf3434f9886b3693300080832e9ef3a677e8e"
-    sha256 cellar: :any_skip_relocation, catalina:      "ad17ed9714d78246e50330e420afee51db51d9fdfcceb933c063c30f85b37e6e"
-    sha256 cellar: :any_skip_relocation, mojave:        "cfc88d1629ed9fce17eafdf286226ebebced49fcf0bb774486c88a9cd7a79f91"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d90f1325e02b5297be13525f3bda1405448e99d732efcb63c362769f10a13f8c"
+    sha256 cellar: :any_skip_relocation, big_sur:       "42f04894e8a1abbdde5704d6ef2194249915e5233d6ba21c574a4ed993dfeed3"
+    sha256 cellar: :any_skip_relocation, catalina:      "47f0d244281e24879b7c5526c28c80c151bc72ea980de8883c1957531b2bf41d"
+    sha256 cellar: :any_skip_relocation, mojave:        "e97f5e5885742a031e52ec73869332b5267df3c8bd29070a78c8a54567958516"
   end
 
   depends_on "go" => :build
