51c51dc4b0a0ded9249b95eebf26fc5bca72cb3d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-04-11 19:10:21 +0000

    dua-cli: add 2.11.1 bottle.

--- Formula/dua-cli.rb
+++ Formula/dua-cli.rb
@@ -5,6 +5,13 @@ class DuaCli < Formula
   sha256 "906a2c1cf5b19247fe1af92fb0bcb844095af91015341f04816e46c4b7b69872"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3f43fdadeeabf46c7e7cb7b209e4cfcc96bd29e3d1dee7b0ef9bbbca0eebe102"
+    sha256 cellar: :any_skip_relocation, big_sur:       "d6d58b3a2af258c74c614a61ad6c3c3775613ce42b5cb5b6d27218d59698f975"
+    sha256 cellar: :any_skip_relocation, catalina:      "116d93f67866472620cae7dc477d9858723b6efd763def40825862856ed5a196"
+    sha256 cellar: :any_skip_relocation, mojave:        "dae832906bd8be5f001f5a7ed2ee271aef6ec78ee761e85d4576a3693285a30c"
+  end
+
   depends_on "rust" => :build
 
   def install
