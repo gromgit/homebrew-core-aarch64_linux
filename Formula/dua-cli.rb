3025f84fd0ec9ac53d203ea631015d367b01a92e
bosr <bosr@bosr.cc>
2021-04-11 11:25:47 +0200

    dua-cli 2.11.1 (new formula)
    Closes #74944.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/dua-cli.rb
@@ -0,0 +1,29 @@
+class DuaCli < Formula
+  desc "View disk space usage and delete unwanted data, fast"
+  homepage "https://lib.rs/crates/dua-cli"
+  url "https://github.com/Byron/dua-cli/archive/refs/tags/v2.11.1.tar.gz"
+  sha256 "906a2c1cf5b19247fe1af92fb0bcb844095af91015341f04816e46c4b7b69872"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # Test that usage is correct for these 2 files.
+    (testpath/"empty.txt").write("")
+    (testpath/"file.txt").write("01")
+
+    # The "-EOS" is needed instead of "~EOS" in order to keep
+    # the expected indentation at the start of each line.
+    expected = <<-EOS
+      0  B #{testpath}/empty.txt
+      2  B #{testpath}/file.txt
+      2  B total
+    EOS
+
+    assert_equal expected, shell_output("#{bin}/dua -A #{testpath}/*.txt")
+  end
+end
