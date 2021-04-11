5f6373c5d9bb26dcab28fa28b1126d7b5f52ad33
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-11 00:21:25 +0000

    marked: update 2.0.2 bottle.

--- Formula/marked.rb
+++ Formula/marked.rb
@@ -8,10 +8,10 @@ class Marked < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "722f50a4da81765a90ffb0ffa92bff6ca94ba570480d7a9f45f498a18e013ab5"
-    sha256 cellar: :any_skip_relocation, big_sur:       "39f9f1f360a49f502d39196627cb76b570981645cacc3858a4edb5387146f23b"
-    sha256 cellar: :any_skip_relocation, catalina:      "364cbf030af81bace489c131dedfca66edd7a530f531b19ed847826d2610c461"
-    sha256 cellar: :any_skip_relocation, mojave:        "2fb7473810118285da26992ce1de87e4c9d7897e8fa77089201b90a3e1d70c0d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6ac264e413d3e006ea3d92bd41926d0b44faa328188fb057b881a52d55f501f1"
+    sha256 cellar: :any_skip_relocation, big_sur:       "fa6d2d29bae0c1ea4d48c2be7b7ecbf71e956f0a12a714ac825198fbd79a3a4c"
+    sha256 cellar: :any_skip_relocation, catalina:      "864b32fcbed0c8f4553d80ed263cb03901d6783fd273e53081b201c49685537a"
+    sha256 cellar: :any_skip_relocation, mojave:        "24636afa19f402cfecf6a64f038bd0a92ca7abed7be798ca4cefae2fc90fb5ea"
   end
 
   depends_on "node"
