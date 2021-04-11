16079a60bee25b65403cd795fa64c1ca8f067bb0
Chris Baker <ChrisBaker97@users.noreply.github.com>
2021-04-11 11:30:17 -0400

    asciidoctor 2.0.13
    * asciidoctor 2.0.13
    * update Ruby gem resources
    * revert certain Ruby gem resources to pass tests
    * attempt to fix Mojave build
    * refactor
    * asciidoctor: fix gem resources
    * asciidoctor: add back syntax highlighting gems
    * asciidoctor: fix style
    * asciidoctor: switch back to depends_on for ruby 2.7
    
    Closes #74956.
    
    Co-authored-by: Rylan Polster <rslpolster@gmail.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/asciidoctor.rb
+++ Formula/asciidoctor.rb
@@ -1,11 +1,9 @@
 class Asciidoctor < Formula
   desc "Text processor and publishing toolchain for AsciiDoc"
   homepage "https://asciidoctor.org/"
-  url "https://github.com/asciidoctor/asciidoctor/archive/v2.0.12.tar.gz"
-  sha256 "cd98047c68bf1074a7058b174c93a64f2bd39c2f33941398f6889c562ed5bce8"
+  url "https://github.com/asciidoctor/asciidoctor/archive/v2.0.13.tar.gz"
+  sha256 "4812dd15bb71b3ae8351e8e3c2df4528c7c40dd97ef1954a56442b56b59019a6"
   license "MIT"
-
-  depends_on "ruby" if MacOS.version <= :sierra
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "bd10fcb661d700a6dc30113b6af905708978ca6d4198ea6531abc80628a34f2a"
@@ -15,109 +13,142 @@ class Asciidoctor < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:   "d4fa41fc1f142f4d8ad25c2063ed79dd04091386d87c7996c17c9adcb10be301"
   end
 
+  depends_on "ruby@2.7" if MacOS.version <= :mojave
+
+  # All of these resources are for the asciidoctor-pdf, coderay and rouge gems. To update the asciidoctor-pdf
+  # resources, check https://rubygems.org/gems/asciidoctor-pdf for the latest dependency versions. Make sure to select
+  # the correct version of each dependency gem because the allowable versions can differ between versions.
+  # To help, click on "Show all transitive dependencies" for a tree view of all dependencies. I've added comments
+  # above each resource to make updating them easier, but please update those comments as the dependencies change.
+
+  # asciidoctor-pdf requires concurrent-ruby ~> 1.1.0
   resource "concurrent-ruby" do
-    url "https://rubygems.org/gems/concurrent-ruby-1.1.7.gem"
-    sha256 "ff4befc88d522ccb2109596da26309f4b0b041683ca62d3cb903b313e1caddee"
+    url "https://rubygems.org/gems/concurrent-ruby-1.1.8.gem"
+    sha256 "e35169e8e01c33cddc9d322e4e793a9bc8c3c00c967d206d17457e0d301f2257"
   end
 
+  # prawn 2.2.0 requires pdf-core ~> 0.7.0
   resource "pdf-core" do
     url "https://rubygems.org/gems/pdf-core-0.7.0.gem"
     sha256 "c1afdbb79edaf7c9fea69fd4b8d3b2c68eeb7203ce4db0e80be5392e20e394a6"
   end
 
+  # prawn 2.2.0 requires ttfunk ~> 1.5
+  # asciidoctor-pdf requires ttfunk ~> 1.5.0, >= 1.5.1
+  # pdf-reader requires ttfunk
   resource "ttfunk" do
     url "https://rubygems.org/downloads/ttfunk-1.5.1.gem"
     sha256 "8da1c20cc9e010a4b083376e6ae6996c4aa517558420bb23d9a1d8a228b6f9d5"
   end
 
+  # asciidoctor-pdf requires prawn ~> 2.2.0
+  # prawn-icon requires prawn >= 1.1.0, < 3.0.0
+  # prawn-svg requires prawn >= 0.11.1, < 3
+  # prawn-templates requires prawn ~> 2.2
   resource "prawn" do
     url "https://rubygems.org/gems/prawn-2.2.2.gem"
     sha256 "95284b761f0ea99334ef840ab85f577cfe2cc9448f769cc723843a6d7670b2e1"
   end
 
+  # asciidoctor-pdf requires prawn-icon ~> 2.5.0
   resource "prawn-icon" do
     url "https://rubygems.org/gems/prawn-icon-2.5.0.gem"
     sha256 "dc88129676707c983e914ca2d2f066fb244e946075ed933c8422b996916b73c7"
   end
 
+  # addressable requires public_suffix  >= 2.0.2, < 5.0
   resource "public_suffix" do
     url "https://rubygems.org/gems/public_suffix-4.0.6.gem"
     sha256 "a99967c7b2d1d2eb00e1142e60de06a1a6471e82af574b330e9af375e87c0cf7"
   end
 
+  # css_parser requires addressable
   resource "addressable" do
     url "https://rubygems.org/gems/addressable-2.7.0.gem"
     sha256 "5e9b62fe1239091ea9b2893cd00ffe1bcbdd9371f4e1d35fac595c98c5856cbb"
   end
 
+  # prawn-svg requires css_parser ~> 1.6
   resource "css_parser" do
-    url "https://rubygems.org/gems/css_parser-1.7.1.gem"
-    sha256 "dc35cf9ea61daac68007b0ffb0d84fd3159013f546e13441c93297ca106d8411"
+    url "https://rubygems.org/gems/css_parser-1.9.0.gem"
+    sha256 "a19cbe6edf9913b596c63bc285681b24288820bbe32c51564e09b49e9a8d4477"
   end
 
+  # asciidoctor-pdf requires prawn-svg ~> 0.31.0
   resource "prawn-svg" do
-    url "https://rubygems.org/gems/prawn-svg-0.30.0.gem"
-    sha256 "e7d85c2b7c05427cfe9860c29db99ce9c935bd88158eb340e4b70fcf12a1c335"
+    url "https://rubygems.org/gems/prawn-svg-0.31.0.gem"
+    sha256 "d40435a7880e76d7b6053c819c8033862ffa8c0fc5271ea9f11b1286658565e5"
   end
 
+  # asciidoctor-pdf requires prawn-table ~> 0.2.0
   resource "prawn-table" do
     url "https://rubygems.org/gems/prawn-table-0.2.2.gem"
     sha256 "336d46e39e003f77bf973337a958af6a68300b941c85cb22288872dc2b36addb"
   end
 
+  # pdf-reader requires afm ~> 0.2.1
   resource "afm" do
     url "https://rubygems.org/gems/afm-0.2.2.gem"
     sha256 "c83e698e759ab0063331ff84ca39c4673b03318f4ddcbe8e90177dd01e4c721a"
   end
 
+  # pdf-reader requires Ascii85 ~> 1.0
   resource "Ascii85" do
-    url "https://rubygems.org/gems/Ascii85-1.0.3.gem"
-    sha256 "7ae3f2eb83ef5962016802caf0ce7db500c1cc25f385877f6ec64a29cfa8a818"
+    url "https://rubygems.org/gems/Ascii85-1.1.0.gem"
+    sha256 "9ce694467bd69ab2349768afd27c52ad721cdc6f642aeaa895717bfd7ada44b7"
   end
 
+  # pdf-reader requires hashery ~> 2.0
   resource "hashery" do
     url "https://rubygems.org/gems/hashery-2.1.2.gem"
     sha256 "d239cc2310401903f6b79d458c2bbef5bf74c46f3f974ae9c1061fb74a404862"
   end
 
+  # pdf-reader requires ruby-rc4
   resource "ruby-rc4" do
     url "https://rubygems.org/gems/ruby-rc4-0.1.5.gem"
     sha256 "00cc40a39d20b53f5459e7ea006a92cf584e9bc275e2a6f7aa1515510e896c03"
   end
 
+  # prawn-templates requires pdf-reader ~> 2.0
   resource "pdf-reader" do
-    url "https://rubygems.org/gems/pdf-reader-2.4.1.gem"
-    sha256 "705502cf151ac59a774a368819bd96c174bc63552cdc4a2f7db80ad0187d0725"
+    url "https://rubygems.org/gems/pdf-reader-2.4.2.gem"
+    sha256 "26a27981377a856ccbcaddc5c3001eab7b887066c388351499b0a1e07b53b4b3"
   end
 
+  # asciidoctor-pdf requries prawn-templates ~> 0.1.0
   resource "prawn-templates" do
     url "https://rubygems.org/gems/prawn-templates-0.1.2.gem"
     sha256 "117aa03db570147cb86fcd7de4fd896994f702eada1d699848a9529a87cd31f1"
   end
 
+  # asciidoctor-pdf requries safe_yaml ~> 1.0.0
   resource "safe_yaml" do
     url "https://rubygems.org/gems/safe_yaml-1.0.5.gem"
     sha256 "a6ac2d64b7eb027bdeeca1851fe7e7af0d668e133e8a88066a0c6f7087d9f848"
   end
 
+  # asciidoctor-pdf requries thread_safe ~> 0.3.0
   resource "thread_safe" do
     url "https://rubygems.org/gems/thread_safe-0.3.6.gem"
     sha256 "9ed7072821b51c57e8d6b7011a8e282e25aeea3a4065eab326e43f66f063b05a"
   end
 
+  # treetop requries polyglot ~> 0.3
   resource "polyglot" do
     url "https://rubygems.org/gems/polyglot-0.3.5.gem"
     sha256 "59d66ef5e3c166431c39cb8b7c1d02af419051352f27912f6a43981b3def16af"
   end
 
+  # asciidoctor-pdf requries treetop ~> 1.6.0
   resource "treetop" do
     url "https://rubygems.org/gems/treetop-1.6.11.gem"
     sha256 "102e13adf065fc916eae60b9539a76101902a56e4283c847468eaea9c2c72719"
   end
 
   resource "asciidoctor-pdf" do
-    url "https://rubygems.org/gems/asciidoctor-pdf-1.5.3.gem"
-    sha256 "3e70d0e513f4d631a4b667fed634700d93b104e0d3b4c33f993979df6a67d3f1"
+    url "https://rubygems.org/gems/asciidoctor-pdf-1.5.4.gem"
+    sha256 "c7a8998e905770628829972320017415174e69dea29fd0717e08e49d69b2104d"
   end
 
   resource "coderay" do
@@ -126,14 +157,13 @@ class Asciidoctor < Formula
   end
 
   resource "rouge" do
-    url "https://rubygems.org/gems/rouge-3.24.0.gem"
-    sha256 "228df3eae97599f49546b2b34ffba34bff86a9f3a712327b27586d301f00f4c5"
+    url "https://rubygems.org/gems/rouge-3.26.0.gem"
+    sha256 "a3deb40ae6a07daf67ace188b32c63df04cffbe3c9067ef82495d41101188b2c"
   end
 
   def install
     ENV["GEM_HOME"] = libexec
     resources.each do |r|
-      r.fetch
       system "gem", "install", r.cached_download, "--ignore-dependencies",
              "--no-document", "--install-dir", libexec
     end
@@ -146,31 +176,36 @@ class Asciidoctor < Formula
   end
 
   test do
-    (testpath/"test.adoc").write <<~EOS
-      = AsciiDoc is Writing Zen
-      Random J. Author <rjauthor@example.com>
-      :icons: font
+    %w[rouge coderay].each do |highlighter|
+      (testpath/"test.adoc").atomic_write <<~EOS
+        = AsciiDoc is Writing Zen
+        Random J. Author <rjauthor@example.com>
+        :icons: font
+        :source-highlighter: #{highlighter}
 
-      Hello, World!
+        Hello, World!
 
-      == Syntax Highlighting
+        == Syntax Highlighting
 
-      Python source.
+        Python source.
 
-      [source, python]
-      ----
-      import something
-      ----
+        [source, python]
+        ----
+        import something
+        ----
 
-      List
+        List
 
-      - one
-      - two
-      - three
-    EOS
-    system bin/"asciidoctor", "-b", "html5", "-o", "test.html", "test.adoc"
-    assert_match "<h1>AsciiDoc is Writing Zen</h1>", File.read("test.html")
-    system bin/"asciidoctor", "-r", "asciidoctor-pdf", "-b", "pdf", "-o", "test.pdf", "test.adoc"
-    assert_match "/Title (AsciiDoc is Writing Zen)", File.read("test.pdf", mode: "rb")
+        - one
+        - two
+        - three
+      EOS
+      output = Utils.popen_read bin/"asciidoctor", "-b", "html5", "-o", "test.html", "test.adoc", err: :out
+      refute_match(/optional gem '#{highlighter}' is not available/, output)
+      assert_match "<h1>AsciiDoc is Writing Zen</h1>", File.read("test.html")
+      assert_match(/<pre class="#{highlighter} highlight">/i, File.read("test.html"))
+      system bin/"asciidoctor", "-r", "asciidoctor-pdf", "-b", "pdf", "-o", "test.pdf", "test.adoc"
+      assert_match "/Title (AsciiDoc is Writing Zen)", File.read("test.pdf", mode: "rb")
+    end
   end
 end
