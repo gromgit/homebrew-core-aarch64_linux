07afdb5ceb6e5498f555a2c7d3dc871409683f66
Caleb Xu <calebcenter@live.com>
2021-04-11 18:14:37 -0400

    cimg 2.9.7 (#74965)

--- Formula/cimg.rb
+++ Formula/cimg.rb
@@ -1,8 +1,8 @@
 class Cimg < Formula
   desc "C++ toolkit for image processing"
   homepage "https://cimg.eu/"
-  url "https://cimg.eu/files/CImg_2.9.6.zip"
-  sha256 "1645894956d858c1cab0458451a3bf2f8b7b8d48004d7dd9446ac8fd3ceadcce"
+  url "https://cimg.eu/files/CImg_2.9.7.zip"
+  sha256 "595dda9718431a123b418fa0db88e248c44590d47d9b1646970fa0503e27fa5c"
   license "CECILL-2.0"
 
   livecheck do
