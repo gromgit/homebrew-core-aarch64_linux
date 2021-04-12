0cfa9706a58f7364770bd6774140fa32e3eb855e
rui <rui@chenrui.dev>
2021-04-12 22:14:00 +0000

    tektoncd-cli 0.17.2
    Closes #75053.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/tektoncd-cli.rb
+++ Formula/tektoncd-cli.rb
@@ -1,8 +1,8 @@
 class TektoncdCli < Formula
   desc "CLI for interacting with TektonCD"
   homepage "https://github.com/tektoncd/cli"
-  url "https://github.com/tektoncd/cli/archive/v0.17.1.tar.gz"
-  sha256 "d5f734f5c5f443a3dee7a2b773de9b356c6a4bc281aa65c1fc6923f71df0e7f7"
+  url "https://github.com/tektoncd/cli/archive/v0.17.2.tar.gz"
+  sha256 "0f6103026e4ac0f76ab1af5f5fef2dafbe3dc673861557479310186aa1d341cf"
   license "Apache-2.0"
 
   bottle do
