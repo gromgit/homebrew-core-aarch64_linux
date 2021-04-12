28da513c4a297fc65de1b29ab4591263ce7120d2
Jie Ma <im@jma.dev>
2021-04-12 15:35:37 +1000

    zsh-vi-mode 0.8.2 (#74978)

---
+++ Formula/zsh-vi-mode.rb
@@ -0,0 +1,26 @@
+class ZshViMode < Formula
+  desc "Better and friendly vi(vim) mode plugin for ZSH"
+  homepage "https://github.com/jeffreytse/zsh-vi-mode"
+  url "https://github.com/jeffreytse/zsh-vi-mode/archive/refs/tags/v0.8.2.tar.gz"
+  sha256 "888d68b451fc3221c2ee1648dbfbff38e524048b2c78620a244650fc76946c28"
+  license "MIT"
+
+  bottle :unneeded
+
+  def install
+    pkgshare.install "zsh-vi-mode.zsh"
+    pkgshare.install "zsh-vi-mode.plugin.zsh"
+  end
+
+  def caveats
+    <<~EOS
+      To activate the zsh vi mode, add the following at the end of your .zshrc:
+        source #{opt_pkgshare}/zsh-vi-mode.plugin.zsh
+    EOS
+  end
+
+  test do
+    assert_match "zsh-vi-mode",
+      shell_output("zsh -c '. #{pkgshare}/zsh-vi-mode.plugin.zsh && echo $ZVM_NAME'")
+  end
+end
