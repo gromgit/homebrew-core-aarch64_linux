53c1c316358ba85dae79a68b11b8d3d3bb2ec0c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-12 13:19:24 +0000

    xtensor: update 0.23.5 bottle.

--- Formula/xtensor.rb
+++ Formula/xtensor.rb
@@ -6,10 +6,10 @@ class Xtensor < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "24812099320c70245b72bf8fe3037ff4bf09e89e2ad5d4175f8df01ebc2333dd"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2efff1531132e26e1c90e8f4045fb00f309b2c76e0f1eaef3f2ed4653e15421b"
-    sha256 cellar: :any_skip_relocation, catalina:      "8d262e5167037efa8f018225a9cd8bf57c2c9cfa3fd10338ff2aaf8813217eff"
-    sha256 cellar: :any_skip_relocation, mojave:        "16150b10735944a9da90e827ebfebb41ed31633cdd95cf1514966b342600dafe"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e81ac3906bfb5dab9de687fcbc25b2c55a5bd514f3d45e7863191cdbf0bbe8b2"
+    sha256 cellar: :any_skip_relocation, big_sur:       "9ca378965e78b03e8a16b3949675abdb100ad82bf14f4413edd9d847bfdb763a"
+    sha256 cellar: :any_skip_relocation, catalina:      "b1116d7f4b37123344cb07cc91d8e4d64d8cfdb7c8eb7548aabf8b679e52e6c3"
+    sha256 cellar: :any_skip_relocation, mojave:        "1aafbc4e728019cd20dba3e9541731d085eb77cf2a4a8c2531d261a9f157418e"
   end
 
   depends_on "cmake" => :build
