cfd88b37bb455330296b60138ea55114b91763a4
Caleb Xu <calebcenter@live.com>
2021-04-12 19:58:36 -0400

    repo 2.13.8
    Closes #75065.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/repo.rb
+++ Formula/repo.rb
@@ -4,8 +4,8 @@ class Repo < Formula
   desc "Repository tool for Android development"
   homepage "https://source.android.com/source/developing.html"
   url "https://gerrit.googlesource.com/git-repo.git",
-      tag:      "v2.13.7",
-      revision: "d34af28ac2b5f8c2bc84342d86d98eca916319b3"
+      tag:      "v2.13.8",
+      revision: "ddab0604eee41e26572f0cf9f3fd5ff7a0637594"
   license "Apache-2.0"
   version_scheme 1
 
