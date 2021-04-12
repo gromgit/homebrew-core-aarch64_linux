3d2a44f35d1313c0e3aadbe69e7bfe6e26d875e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-12 05:13:17 +0000

    sc-im: update 0.8.1 bottle.

--- Formula/sc-im.rb
+++ Formula/sc-im.rb
@@ -7,10 +7,10 @@ class ScIm < Formula
   head "https://github.com/andmarti1424/sc-im.git", branch: "freeze"
 
   bottle do
-    sha256 arm64_big_sur: "d62fcbf7991ec40f521e475f89352aaa377ebbdd0f855d4873cb05c6dbc7a612"
-    sha256 big_sur:       "20aa7d533af3006637e41c0d321fc962a699192cafed8b4412ba5fd636ed87c1"
-    sha256 catalina:      "853ebd92243deb555e37b98fe07336182fff4b733981d871a2df45420b6ecece"
-    sha256 mojave:        "6527827b290f1a40a154d2fd32a7142720c1413c801b9bb9bc49ddaa845915ed"
+    sha256 arm64_big_sur: "892bdf2037d8a344f8b254fbe43268a7cf3ed2309b5c43560261b17d9dca7759"
+    sha256 big_sur:       "e50fc96b8e339bcad23a9c9e67beebc9aa4657a98d3acd9d7f3917090f80780e"
+    sha256 catalina:      "8524932993aa6195a7fa6627c40dc477d32b5fc66839c95b5d5273abd6650f18"
+    sha256 mojave:        "4477325a85962aba8dacce2082179bfa096f220f622b9c9b3af1d37d0e287e13"
   end
 
   depends_on "ncurses"
