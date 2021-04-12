022a3638ceb2fdde76815cfa218bdfd926b4d0ce
aspen <aspenuwu@protonmail.com>
2021-04-12 15:10:07 -0400

    ldid 2.1.4
    Closes #75043.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ldid.rb
+++ Formula/ldid.rb
@@ -2,8 +2,8 @@ class Ldid < Formula
   desc "Lets you manipulate the signature block in a Mach-O binary"
   homepage "https://cydia.saurik.com/info/ldid/"
   url "https://git.saurik.com/ldid.git",
-      tag:      "v2.1.3",
-      revision: "d4a4dbe847733d55834ebc60f0678c350fa3ebd5"
+      tag:      "v2.1.4",
+      revision: "2edb2a9307f1bd3909dadc20e80857c6e40c00c5"
   license "AGPL-3.0-or-later"
   head "https://git.saurik.com/ldid.git"
 
