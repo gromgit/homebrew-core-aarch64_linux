ec89b44212465985fd610942d77164b1fa0ffee8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-12 14:08:14 +0000

    httpd: update 2.4.46_2 bottle.

--- Formula/httpd.rb
+++ Formula/httpd.rb
@@ -8,10 +8,11 @@ class Httpd < Formula
   revision 2
 
   bottle do
-    sha256 arm64_big_sur: "35357c35f6be07c0f3e60d64c88eae200158dca6e390a341569a9f0296ed33fb"
-    sha256 big_sur:       "5a979ae3affd408b4ab51f917ef34e662a9cb85eb3918e56e05e1bcfac1aedac"
-    sha256 catalina:      "c540cd4ba596ff6f0df9d772c41487c26201f548a3756ee7a02108c70fee147e"
-    sha256 mojave:        "b88153894953fa0c976f0f74bec8abf2646b320424cc92a5cbdebb6a493ab729"
+    rebuild 1
+    sha256 arm64_big_sur: "4c58ed58ba2e47a10192bffb9bcf067e7d6b947819ee85bd014e9c67cf8be5d8"
+    sha256 big_sur:       "9c6b3d19822ad929ef477fd6936c753e37477315b3011754858013f4a355f7cb"
+    sha256 catalina:      "28299ea97da7c443d880865b5669fd0341e01d2891f91095ebcfd5ad30679452"
+    sha256 mojave:        "d5ef11b940eda27e5fda1addb6b914b76768f3fc0fa5652aa4470d22a87de4d9"
   end
 
   depends_on "apr"
