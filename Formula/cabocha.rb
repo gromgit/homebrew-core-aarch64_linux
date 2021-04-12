502a3d8721558bdf346e8a4f9d573f5bbf1118a9
Mike McQuaid <mike@mikemcquaid.com>
2021-04-12 13:30:28 +0100

    cabocha: remove Bintray url.
    Imminent brownout, shut down in under a month.

--- Formula/cabocha.rb
+++ Formula/cabocha.rb
@@ -2,8 +2,7 @@ class Cabocha < Formula
   desc "Yet Another Japanese Dependency Structure Analyzer"
   homepage "https://taku910.github.io/cabocha/"
   # Files are listed in https://drive.google.com/drive/folders/0B4y35FiV1wh7cGRCUUJHVTNJRnM
-  url "https://dl.bintray.com/homebrew/mirror/cabocha-0.69.tar.bz2"
-  mirror "https://mirrorservice.org/sites/ftp.netbsd.org/pub/pkgsrc/distfiles/cabocha-20160909/cabocha-0.69.tar.bz2"
+  url "https://mirrorservice.org/sites/ftp.netbsd.org/pub/pkgsrc/distfiles/cabocha-20160909/cabocha-0.69.tar.bz2"
   sha256 "9db896d7f9d83fc3ae34908b788ae514ae19531eb89052e25f061232f6165992"
 
   bottle do
