426c1e1bc51c7c9b2068538ab1a4989bcf70b3e8
Nikolas Grottendieck <git@nikolasgrottendieck.com>
2021-04-13 17:38:19 +0200

    butane: renamed from fcct
    Renamed the formula based on the official rename (see coreos/butane/issues/167 ) as discussed in the version bump pull request (see Homebrew/homebrew-core/pull/75077 ).

---
+++ Formula/butane.rb
@@ -0,0 +1,56 @@
+class Butane < Formula
+  desc "Translates human-readable Butane Configs into machine-readable Ignition Configs"
+  homepage "https://github.com/coreos/butane"
+  url "https://github.com/coreos/butane/archive/v0.11.0.tar.gz"
+  sha256 "3da1138aa86dc069e6f08b7fa9a4f3b910593406a0cc48f71edc7b3e1d1b1c49"
+  license "Apache-2.0"
+  head "https://github.com/coreos/butane.git"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-mod=vendor",
+      *std_go_args(ldflags: "-w -X github.com/coreos/butane/internal/version.Raw=#{version}"), "internal/main.go"
+
+    prefix.install_metafiles
+  end
+
+  test do
+    (testpath/"example.bu").write <<~EOS
+      variant: fcos
+      version: 1.3.0
+      passwd:
+        users:
+          - name: core
+            ssh_authorized_keys:
+              - ssh-rsa mykey
+    EOS
+
+    (testpath/"broken.bu").write <<~EOS
+      variant: fcos
+      version: 1.3.0
+      passwd:
+        users:
+          - name: core
+            broken_authorized_keys:
+              - ssh-rsa mykey
+    EOS
+
+    system "#{bin}/butane", "--strict", "--output=#{testpath}/example.ign", "#{testpath}/example.bu"
+    assert_predicate testpath/"example.ign", :exist?
+    assert_match(/.*"sshAuthorizedKeys":\["ssh-rsa mykey"\].*/m, File.read(testpath/"example.ign").strip)
+
+    output = shell_output("#{bin}/butane --strict #{testpath}/example.bu")
+    assert_match(/.*"sshAuthorizedKeys":\["ssh-rsa mykey"\].*/m, output.strip)
+
+    shell_output("#{bin}/butane --strict --output=#{testpath}/broken.ign #{testpath}/broken.bu", 1)
+    refute_predicate testpath/"broken.ign", :exist?
+
+    assert_match version.to_s, shell_output("#{bin}/butane --version 2>&1")
+  end
+end
