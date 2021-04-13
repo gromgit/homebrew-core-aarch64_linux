b575cccd8ba4429ba0cda893097500f54085a693
Nanda H Krishna <nanda.harishankar@gmail.com>
2021-04-13 20:12:50 +0530

    librasterlite: add license (#75105)

--- Formula/librasterlite.rb
+++ Formula/librasterlite.rb
@@ -3,6 +3,7 @@ class Librasterlite < Formula
   homepage "https://www.gaia-gis.it/fossil/librasterlite/index"
   url "https://www.gaia-gis.it/gaia-sins/librasterlite-sources/librasterlite-1.1g.tar.gz"
   sha256 "0a8dceb75f8dec2b7bd678266e0ffd5210d7c33e3d01b247e9e92fa730eebcb3"
+  license any_of: ["MPL-1.1", "GPL-2.0-or-later", "LGPL-2.1-or-later"]
   revision 7
 
   livecheck do
