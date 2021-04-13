f3b51d213ba66fd5f43128ecdb72fd18627a7598
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-13 14:08:28 +0000

    vulkan-headers: update 1.2.175 bottle.

--- Formula/vulkan-headers.rb
+++ Formula/vulkan-headers.rb
@@ -6,10 +6,10 @@ class VulkanHeaders < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7631e29076f850b3e0715c09c2c9088dc08f9233a9862f7fd2e3740934aac52d"
-    sha256 cellar: :any_skip_relocation, big_sur:       "aa2e5aa36eec3aa7aaebd85045370724dd3e0f4c77d488509459ff08ddae9d55"
-    sha256 cellar: :any_skip_relocation, catalina:      "3f525e6070e586a7d4cbdf3d10a21fd3f051eccde0f1535c41fd1901e00dbcbb"
-    sha256 cellar: :any_skip_relocation, mojave:        "58ed68fcaff777b01dd3d3c218e2ee10d97cc150c10c9a4ff56cb1f767a08f89"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a3bb94b28b142b247cc38278061fe9e889e7f3688ca7575d44278d89f764c2df"
+    sha256 cellar: :any_skip_relocation, big_sur:       "11663ef50ac98a0919684171ae2ccc6260f91a10cfa003a7ef6c016a4f7447ef"
+    sha256 cellar: :any_skip_relocation, catalina:      "503b924cc87af76eb47aead1f90ce986edd768b5394d9c67cf5beef1748bc658"
+    sha256 cellar: :any_skip_relocation, mojave:        "a84be3e9cabde865b4ad5d1d725759f8f2e86c19d8d4311bca31baad6c9fc2dc"
   end
 
   depends_on "cmake" => :build
