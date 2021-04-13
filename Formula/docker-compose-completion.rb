9b803c751792669d66440c30f8ae290c1dbc03f3
rui <rui@chenrui.dev>
2021-04-13 20:20:14 +0000

    docker-compose-completion 1.29.1
    Closes #75121.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/docker-compose-completion.rb
+++ Formula/docker-compose-completion.rb
@@ -1,8 +1,8 @@
 class DockerComposeCompletion < Formula
   desc "Completion script for docker-compose"
   homepage "https://docs.docker.com/compose/completion/"
-  url "https://github.com/docker/compose/archive/1.29.0.tar.gz"
-  sha256 "a798ec994096654d0e50b408fdb55015ce3e520b31e1819797edc425e9413388"
+  url "https://github.com/docker/compose/archive/1.29.1.tar.gz"
+  sha256 "c132b6be73f7816c0fc5af15be0fe0e9a8d2fe31d1f31de73dedc229e344bb27"
   license "Apache-2.0"
   head "https://github.com/docker/compose.git"
 
