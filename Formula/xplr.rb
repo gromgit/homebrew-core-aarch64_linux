d8e7ad7175fc01a1afa5156fdb2869f045201c7d
Caleb Xu <calebcenter@live.com>
2021-04-13 00:53:16 -0400

    xplr 0.4.3 (new formula)
    Closes #75083.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/xplr.rb
@@ -0,0 +1,28 @@
+class Xplr < Formula
+  desc "Hackable, minimal, fast TUI file explorer"
+  homepage "https://github.com/sayanarijit/xplr"
+  url "https://github.com/sayanarijit/xplr/archive/v0.4.3.tar.gz"
+  sha256 "d9df75d949085b0ed14a55a40a0132889e674f2851a0d3fd4c4cb4de54368e63"
+  license "MIT"
+  head "https://github.com/sayanarijit/xplr.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    input, = Open3.popen2 "SHELL=/bin/sh script -q output.txt"
+    input.puts "stty rows 80 cols 130"
+    input.puts "#{bin}/xplr"
+    input.putc "q"
+    input.puts "exit"
+
+    sleep 5
+    File.open(testpath/"output.txt", "r:ISO-8859-7") do |f|
+      contents = f.read
+      assert_match testpath.to_s, contents
+    end
+  end
+end
