d1ba1a2cb05be04bbf2661ae45c64ba9fc057988
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-14 01:30:33 +0000

    swift-protobuf: update 1.16.0 bottle.

--- Formula/swift-protobuf.rb
+++ Formula/swift-protobuf.rb
@@ -7,10 +7,10 @@ class SwiftProtobuf < Formula
   head "https://github.com/apple/swift-protobuf.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "22319befbba8c0cd170d31342957469f73a0af90be39e2aa6f27b6ba4ec1bf54"
-    sha256 cellar: :any_skip_relocation, big_sur:       "04a76e3cade018871acf0f19b7764d5d398f1e1302fbfe7157a56a776f2c8e12"
-    sha256 cellar: :any_skip_relocation, catalina:      "9877fa712cedfba58ea291afb38daf1cfc22a97adfc277944eb1213257e6bbaf"
-    sha256 cellar: :any_skip_relocation, mojave:        "4aa49d0388ad38fe94e589f8a250a159c03f5d086576cb1fbfb9b59c7775fb1c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bfce79a8f87aa3068fdd3dd9e94741063726296219ca6f94e4f9fe6bff2fd8d9"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ba9c25b3633a182124ad138ea6c804c200e2d18e0a85705fdbce9228f34d09b8"
+    sha256 cellar: :any_skip_relocation, catalina:      "4fdd4cd86dc6313637f3ddb8d312f41cf69dd942eaf01aba8bda0ee8db470740"
+    sha256 cellar: :any_skip_relocation, mojave:        "3ee13d668071884c84516f7d4e3c1fa980e5b70655aea7c46f68b7c95d68efc4"
   end
 
   depends_on xcode: ["8.3", :build]
