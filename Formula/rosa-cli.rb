46b61bf0dbc2a9e55c697960a83198187d51cf60
rui <rui@chenrui.dev>
2021-04-14 22:13:44 +0000

    rosa-cli 1.0.4
    Closes #75198.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/rosa-cli.rb
+++ Formula/rosa-cli.rb
@@ -1,8 +1,8 @@
 class RosaCli < Formula
   desc "RedHat OpenShift Service on AWS (ROSA) command-line interface"
   homepage "https://www.openshift.com/products/amazon-openshift"
-  url "https://github.com/openshift/rosa/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "e2bc5f03e5d295cb32b9b45523790572601823386444d4c4e6c86c99ef8248ee"
+  url "https://github.com/openshift/rosa/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "d734578190559d42f7484a0896f7d79a409b26fbc2abf0f292e277e5d1dd586a"
   license "Apache-2.0"
   head "https://github.com/openshift/rosa.git"
 
