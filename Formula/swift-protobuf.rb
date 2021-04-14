7efc7e1e9fa6716e39ad6d1e2fec3f9f9e8ffa0f
rui <rui@chenrui.dev>
2021-04-14 00:09:54 +0000

    swift-protobuf 1.16.0
    Closes #75139.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/swift-protobuf.rb
+++ Formula/swift-protobuf.rb
@@ -1,10 +1,9 @@
 class SwiftProtobuf < Formula
   desc "Plugin and runtime library for using protobuf with Swift"
   homepage "https://github.com/apple/swift-protobuf"
-  url "https://github.com/apple/swift-protobuf/archive/1.15.0.tar.gz"
-  sha256 "e9b4d467e67692708f6a2cfc78372156f3945ba5fdd21a3c799fa2c29b150563"
+  url "https://github.com/apple/swift-protobuf/archive/1.16.0.tar.gz"
+  sha256 "a8c06b3fb05b18d74bd63151f0074904e8223cbe8217d0c6cbb4fba470dfb766"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/apple/swift-protobuf.git"
 
   bottle do
