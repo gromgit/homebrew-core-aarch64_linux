4514670d544bbb2dad45aee82a26c6bf9387e2a5
Nanda H Krishna <nanda.harishankar@gmail.com>
2021-04-15 23:06:39 +0530

    jbake: add license (#75252)

--- Formula/jbake.rb
+++ Formula/jbake.rb
@@ -3,6 +3,7 @@ class Jbake < Formula
   homepage "https://jbake.org/"
   url "https://github.com/jbake-org/jbake/releases/download/v2.6.6/jbake-2.6.6-bin.zip"
   sha256 "4783f5bc33a3a5eb24f5557cd61c79128397271d8e145cd74be33547662e5274"
+  license "MIT"
 
   bottle :unneeded
 
