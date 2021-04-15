dd4c0c3b24f18b5a58e69b5f766f0bfcc52a1602
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-15 09:07:31 +0000

    yq: update 4.7.0 bottle.

--- Formula/yq.rb
+++ Formula/yq.rb
@@ -6,10 +6,10 @@ class Yq < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "537d41f308288e5534bdf9dd6f338e697dfd0c26269a6f4d1b79e6595dc9c977"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3fb80f419b64c09b4c6aa7978551bdeaec6bb35f762d48803dadc3bd11564cd3"
-    sha256 cellar: :any_skip_relocation, catalina:      "65589802caab855197875d4d9c45d5e1589bbb43c596e2961c2b8e5d8d9334ca"
-    sha256 cellar: :any_skip_relocation, mojave:        "119fc499558d21cf55a6efe09296a4024d64769a64c4400066fd9c38059cf937"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7c9002c49eedbf653b5d78bddd9c572f41f5e6754d02902a42cdd9764617afda"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a216cf0ef5e58c3da870a30da90d7253b1b7eb8ae77ebeef734fcb4be4fde8a9"
+    sha256 cellar: :any_skip_relocation, catalina:      "50ee9266dce3db9fa929d194b2057d3f481108bbb6c0cee12c29cd3fdfffaade"
+    sha256 cellar: :any_skip_relocation, mojave:        "fc07b88a3c60cebb468ca7aed7b4c1fbf140e51824924e19a3c95f7618194ac7"
   end
 
   depends_on "go" => :build
