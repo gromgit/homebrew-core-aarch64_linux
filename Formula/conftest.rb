9dcf3d64e601be8e7c9d09835c17f693db248572
John Reese <john@reese.dev>
2021-04-15 14:38:43 -0400

    conftest 0.23.0 (new formula)
    Closes #75259.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/conftest.rb
@@ -0,0 +1,23 @@
+class Conftest < Formula
+  desc "Test your configuration files using Open Policy Agent"
+  homepage "https://www.conftest.dev/"
+  url "https://github.com/open-policy-agent/conftest/archive/v0.23.0.tar.gz"
+  sha256 "116bfd3c00fcaa31a3468cace6df5b03eeff0150d0d096aee53dd74c7c9647bd"
+  license "Apache-2.0"
+  head "https://github.com/open-policy-agent/conftest.git"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-X github.com/open-policy-agent/conftest/internal/commands.version=#{version}")
+  end
+
+  test do
+    assert_match "Test your configuration files using Open Policy Agent", shell_output("#{bin}/conftest --help")
+
+    # Using the policy parameter changes the default location to look for policies.
+    # If no policies are found, a non-zero status code is returned.
+    (testpath/"test.rego").write("package main")
+    system "#{bin}/conftest", "verify", "-p", "test.rego"
+  end
+end
