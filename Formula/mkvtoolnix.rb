c27a73ad35e3221fb560e9ef3da0ef7d64bba315
Michka Popoff <michkapopoff@gmail.com>
2021-04-16 23:53:56 +0200

    mkvtoolnix: build with gcc 10 on Linux

--- Formula/mkvtoolnix.rb
+++ Formula/mkvtoolnix.rb
@@ -42,6 +42,12 @@ class Mkvtoolnix < Formula
   uses_from_macos "libxslt" => :build
   uses_from_macos "ruby" => :build
 
+  on_linux do
+    depends_on "gcc" => :build
+  end
+
+  fails_with gcc: "5"
+
   def install
     ENV.cxx11
 
