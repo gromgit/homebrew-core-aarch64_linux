e8403323529e72077a47a1db7574608ef22f72c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-16 23:07:11 +0000

    node-sass: update 1.32.9 bottle.

--- Formula/node-sass.rb
+++ Formula/node-sass.rb
@@ -8,10 +8,10 @@ class NodeSass < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "20d0865456da38121799ae943aa1f4c457ebe205195a6236f28531e3d4c173dd"
-    sha256 cellar: :any_skip_relocation, big_sur:       "944b0e428a830f4fda5f07da2dfbbebd8f32759c13f812554c97b31c7a9ef09a"
-    sha256 cellar: :any_skip_relocation, catalina:      "6a24e91e010142cef27b15c500bdb0b3f474cb0255372d999a04803e13b2ffdd"
-    sha256 cellar: :any_skip_relocation, mojave:        "68ad962cc9633875b77685c844bae2f5ee25c155c20ece05529dadb788c04e8d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4b718fb39c3ee21e39da390239c2e9cba9e637277b376a26a3615c7810ad08ce"
+    sha256 cellar: :any_skip_relocation, big_sur:       "218e0e4bcec8520ba13c479804fba2ff296411b34d9376b570d8c4e8593c38ca"
+    sha256 cellar: :any_skip_relocation, catalina:      "218e0e4bcec8520ba13c479804fba2ff296411b34d9376b570d8c4e8593c38ca"
+    sha256 cellar: :any_skip_relocation, mojave:        "68f8e6548ed2a68a73a6ba54615c3f649783c3435a12c7ed575f0f4ea39149a0"
   end
 
   depends_on "node"
