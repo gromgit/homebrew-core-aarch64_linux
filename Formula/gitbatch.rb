e609c195f7a77ec9453aaa96f30c30de3dff01ac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-17 09:07:40 +0000

    gitbatch: update 0.6.1 bottle.

--- Formula/gitbatch.rb
+++ Formula/gitbatch.rb
@@ -6,10 +6,10 @@ class Gitbatch < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "435b26ee9b78a811d918ef891a9e57570179c4a543095cd83c86e02d017787af"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f9e66be24dac6acb5c3bb771971ac7d8736002a51b400a5922d9e99a33868d75"
-    sha256 cellar: :any_skip_relocation, catalina:      "725d490cf957518eb5e169564c77a044cb48ec64c40bf1003ff859fef9461b16"
-    sha256 cellar: :any_skip_relocation, mojave:        "4fbdaf04019dc3fc925754a6401b17793aab30860227123f8df17aaa114fa531"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a6870e51c395a0c2bb7da37785ddd6932d18dd516905d0ab5c02632015edb39c"
+    sha256 cellar: :any_skip_relocation, big_sur:       "3ae2045b1f348f381d15ff6eb708ac5ace0f550c733f3f5002c5edde50514c08"
+    sha256 cellar: :any_skip_relocation, catalina:      "6ea0a1220269223eb0ffb99eda340f726a146dafe10b8e558e44eea278d15a37"
+    sha256 cellar: :any_skip_relocation, mojave:        "35e4351bc3abfae50c14c8d32f0fdfd756259c77246d19720f9178a20a79ad1d"
   end
 
   depends_on "go" => :build
