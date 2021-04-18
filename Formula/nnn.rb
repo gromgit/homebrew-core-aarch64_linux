7e2f39e1bd7fe873bec359716b081a368938ce8c
Daniel Nachun <daniel.nachun@gmail.com>
2021-04-18 02:56:35 -0700

    nnn: disable test on Linux (#75408)

--- Formula/nnn.rb
+++ Formula/nnn.rb
@@ -27,6 +27,12 @@ class Nnn < Formula
   end
 
   test do
+    on_linux do
+      # Test fails on CI: Input/output error @ io_fread - /dev/pts/0
+      # Fixing it involves pty/ruby voodoo, which is not worth spending time on
+      return if ENV["HOMEBREW_GITHUB_ACTIONS"]
+    end
+
     # Testing this curses app requires a pty
     require "pty"
 
