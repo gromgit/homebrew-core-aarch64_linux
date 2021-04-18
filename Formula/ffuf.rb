22c24aae8a90468662dac9df32beb9d0343d1502
rui <rui@chenrui.dev>
2021-04-18 10:11:05 +0000

    ffuf 1.3.0
    * ffuf 1.3.0
    * Update ffuf.rb
    
    Closes #75418.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/ffuf.rb
+++ Formula/ffuf.rb
@@ -1,8 +1,8 @@
 class Ffuf < Formula
   desc "Fast web fuzzer written in Go"
   homepage "https://github.com/ffuf/ffuf"
-  url "https://github.com/ffuf/ffuf/archive/v1.2.1.tar.gz"
-  sha256 "ff474b21e192005a2df0f09f942b0370bdcb45d64ee35bd8782eb44a5c636e96"
+  url "https://github.com/ffuf/ffuf/archive/v1.3.0.tar.gz"
+  sha256 "f717ede958c16846c7f67c14fac2a9296fcb3ad249045d04ec97c2d1d30e83ea"
   license "MIT"
 
   bottle do
