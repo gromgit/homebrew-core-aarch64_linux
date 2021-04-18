feab120c421293fbdff72a42afb4446fe0b20324
rui <rui@chenrui.dev>
2021-04-18 04:14:11 +0000

    luv 1.41.0-0
    * luv 1.41.0-0
    * luv: update checksum
      The release was retagged. See https://github.com/luvit/luv/pull/537.
    
    Closes #75395.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/luv.rb
+++ Formula/luv.rb
@@ -1,9 +1,9 @@
 class Luv < Formula
   desc "Bare libuv bindings for lua"
   homepage "https://github.com/luvit/luv"
-  url "https://github.com/luvit/luv/archive/1.40.0-0.tar.gz"
-  version "1.40.0-0"
-  sha256 "23167a3d5dbc1e30df1f106ffae0a4c5bd50993da2066dc1f7e1842bb9fb6cd0"
+  url "https://github.com/luvit/luv/archive/1.41.0-0.tar.gz"
+  version "1.41.0-0"
+  sha256 "13382bc5e896f6247c0e8f7b7cbc12c99388b9a858118a8dc5477f5b7a977c8e"
   license "Apache-2.0"
   head "https://github.com/luvit/luv.git"
 
