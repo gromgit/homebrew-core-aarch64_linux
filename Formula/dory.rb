381aa5d856063233ec3905db8dee7cfac3deb074
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-04-18 17:22:45 +0000

    dory: add 1.1.1 bottle.

--- Formula/dory.rb
+++ Formula/dory.rb
@@ -5,6 +5,13 @@ class Dory < Formula
   sha256 "1571e54dab39bc7884523aabeedde71921e64a11ef25b9c59a7b63282f97a237"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "cbe36a5c87731cae07c1de8b9f7e2cfc04e17bba6e1a6918678ef49f62256bad"
+    sha256 cellar: :any_skip_relocation, big_sur:       "0b772ed42dcb84a49c2a223a2fd8a899b942deb457291bef1c281a825fa7069b"
+    sha256 cellar: :any_skip_relocation, catalina:      "0b772ed42dcb84a49c2a223a2fd8a899b942deb457291bef1c281a825fa7069b"
+    sha256 cellar: :any_skip_relocation, mojave:        "52197dfd1668a9c0c3c2daa1cd7e808d89bbb4ba3f3500746e60b56cbc05058b"
+  end
+
   depends_on "ruby@2.7"
 
   def install
