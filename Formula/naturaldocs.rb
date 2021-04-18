b8ec4024d78c723b07d2ef6a619bf6f3be6fee6e
rui <chenrui333@gmail.com>
2021-04-17 21:33:43 -0400

    naturaldocs 2.1.1 (#75151)

--- Formula/naturaldocs.rb
+++ Formula/naturaldocs.rb
@@ -1,9 +1,9 @@
 class Naturaldocs < Formula
   desc "Extensible, multi-language documentation generator"
   homepage "https://www.naturaldocs.org/"
-  url "https://downloads.sourceforge.net/project/naturaldocs/Stable%20Releases/2.1/Natural_Docs_2.1.zip"
-  mirror "https://naturaldocs.org/download/natural_docs/2.1/Natural_Docs_2.1.zip"
-  sha256 "681a452ae6e981b0c0a0670d859873cceb3326939f2ae1e5f38bb27e91ac18c0"
+  url "https://downloads.sourceforge.net/project/naturaldocs/Stable%20Releases/2.1.1/Natural_Docs_2.1.1.zip"
+  mirror "https://naturaldocs.org/download/natural_docs/2.1.1/Natural_Docs_2.1.1.zip"
+  sha256 "00ebfee968c4b88ebd213d1e48be37686d717d938dfa6c739c23b769bdf03c1f"
   license "AGPL-3.0-only"
 
   livecheck do
