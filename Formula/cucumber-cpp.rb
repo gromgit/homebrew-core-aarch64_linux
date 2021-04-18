bc85e4db5d760cd09df4218ef566224bfd57531a
Michka Popoff <michkapopoff@gmail.com>
2021-04-18 18:46:12 +0200

    cucumber-cpp: Fix test for Linux (#75420)

--- Formula/cucumber-cpp.rb
+++ Formula/cucumber-cpp.rb
@@ -61,7 +61,8 @@ class CucumberCpp < Formula
     system ENV.cxx, "test.cpp", "-o", "test", "-I#{include}", "-L#{lib}",
            "-lcucumber-cpp", "-I#{Formula["boost"].opt_include}",
            "-L#{Formula["boost"].opt_lib}", "-lboost_regex", "-lboost_system",
-           "-lboost_program_options", "-lboost_filesystem", "-lboost_chrono"
+           "-lboost_program_options", "-lboost_filesystem", "-lboost_chrono",
+           "-pthread"
     begin
       pid = fork { exec "./test" }
       expected = <<~EOS
