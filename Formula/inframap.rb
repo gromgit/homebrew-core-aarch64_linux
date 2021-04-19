11d394162d996e10323735050f7489f5bb1bae65
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-19 19:11:13 +0000

    inframap: update 0.6.3 bottle.

--- Formula/inframap.rb
+++ Formula/inframap.rb
@@ -7,10 +7,10 @@ class Inframap < Formula
   head "https://github.com/cycloidio/inframap.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4bdceafea8ec6a321512dc5a58ac83283f5421bd32e10c41fb419f882916625f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5e7cb2d33cec50d151ea8424a49993d92432984babcff88793de7ca80fcbadfe"
-    sha256 cellar: :any_skip_relocation, catalina:      "4cbfd3e69a9b85bb3d15c0d5844003e67644d99efdbcb7b74ac5b67f414d6fe4"
-    sha256 cellar: :any_skip_relocation, mojave:        "3e7f66cbeaff89411eda9e630ba72d6b35e93bdc5b7c3ad6f1718ef562a7ac9e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ca16d0feef0d4a43bb6e31b92623d7f7ef840dc625e73e37bab6ec3f6a5d5319"
+    sha256 cellar: :any_skip_relocation, big_sur:       "414396db3ba33db00ee7f420c19748790004053daec9a3f5a9bf6574b26db729"
+    sha256 cellar: :any_skip_relocation, catalina:      "c1ed49255a527ddeaa69ee66a02b5a126caf73abf54c190b2a4c3aa0d3fcae02"
+    sha256 cellar: :any_skip_relocation, mojave:        "f07b9c5a1dee3d80e3d79bfa7ecb4efda8b01b023c4cfc9aa32d14b21545e2d4"
   end
 
   depends_on "go" => :build
