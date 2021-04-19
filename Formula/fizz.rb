4bc6f4fa8eaa8b3e8cc732b1b1ea771230b68780
Dustin Rodrigues <dust.rod@gmail.com>
2021-04-19 07:26:48 -0400

    fizz: 2021.04.19.00

--- Formula/fizz.rb
+++ Formula/fizz.rb
@@ -1,8 +1,8 @@
 class Fizz < Formula
   desc "C++14 implementation of the TLS-1.3 standard"
   homepage "https://github.com/facebookincubator/fizz"
-  url "https://github.com/facebookincubator/fizz/releases/download/v2021.04.12.00/fizz-v2021.04.12.00.tar.gz"
-  sha256 "e9b3a4059a6396e0a96a3b1fa77c2b7031925bc63f3abd33634b7b36713db54b"
+  url "https://github.com/facebookincubator/fizz/releases/download/v2021.04.19.00/fizz-v2021.04.19.00.tar.gz"
+  sha256 "d2f9c9f72bdfed623b9b8336faa78b37d217760e2ae38233ac61e0ff7155082e"
   license "BSD-2-Clause"
   head "https://github.com/facebookincubator/fizz.git"
 
