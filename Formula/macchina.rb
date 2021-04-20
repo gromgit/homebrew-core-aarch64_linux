c0fee5e834c77d3cd96161e9a6c698823bd060a1
Marvin Haschker <123marvin123@users.noreply.github.com>
2021-04-20 23:13:23 +0200

    macchina: change description

--- Formula/macchina.rb
+++ Formula/macchina.rb
@@ -1,5 +1,5 @@
 class Macchina < Formula
-  desc "Basic system information fetcher, with a focus on performance and minimalism"
+  desc "A system information fetcher, with an emphasis on performance and minimalism."
   homepage "https://github.com/Macchina-CLI/macchina"
   url "https://github.com/Macchina-CLI/macchina/archive/v0.7.0.tar.gz"
   sha256 "6ff1497864a400f5eade2b46984cbe1259a0f7a75bf7191a2a75e111a8ffe119"
