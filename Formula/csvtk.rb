7624bb79e4c9a787c891b44053449c73d7fd9942
Nanda H Krishna <me@nandahkrishna.com>
2021-04-20 06:25:43 +0000

    csvtk: add 0.23.0 bottle.

--- Formula/csvtk.rb
+++ Formula/csvtk.rb
@@ -5,6 +5,13 @@ class Csvtk < Formula
   sha256 "b8ccf2b86c815693af1c7d743ca711a68fcdde94d9a9279ac8557fdffa3f2fc8"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "85d884da7d8fd0d6ed0d53eabeccdb900baeec93522b3f817f3dfb1c8a9e2d18"
+    sha256 cellar: :any_skip_relocation, big_sur:       "f2869fb31ea0a291dff303db08ee8387cfab96ee7e77fe98c82511c6b5805613"
+    sha256 cellar: :any_skip_relocation, catalina:      "1d9d5d3ecf0c81b1162f88813f871265ea174ae2535624ad345c1e52364ca692"
+    sha256 cellar: :any_skip_relocation, mojave:        "0e0b648de5a24b410a9d56d663668741cb7429e1295ed1cc120014aaa516c961"
+  end
+
   depends_on "go" => :build
 
   resource "testdata" do
