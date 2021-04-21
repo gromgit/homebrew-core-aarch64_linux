1445304685e5d72936436cdad4d67cda26c34c16
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-21 19:12:48 +0000

    jhipster: update 7.0.1_1 bottle.

--- Formula/jhipster.rb
+++ Formula/jhipster.rb
@@ -10,10 +10,10 @@ class Jhipster < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "37810f4f38600d90a53173949ba3eb8adb25820dd568d99e82f286f815ae38e6"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f349ed13bdf8df18006d534fb6d7ff4829eaa718de2d70b64bc81729991186ef"
-    sha256 cellar: :any_skip_relocation, catalina:      "87c266c3e874222e0d4b5323514c05e0b65bd30c46656c84d39658354ee87286"
-    sha256 cellar: :any_skip_relocation, mojave:        "13daac9bfb8bbb368fdd4d4670c870e6ceb2d43c10990219a790a41e697aaa4c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5936f76445f075c195b45b857fbef95cf7c01bf56f751d8f365a905e682596bd"
+    sha256 cellar: :any_skip_relocation, big_sur:       "dbd46ebc8d781c5080f672bd434099672d030564f1ba24fd4c67221d8c47a6f0"
+    sha256 cellar: :any_skip_relocation, catalina:      "0a206a02b69010d2aaf8ad762ab9ca91950fd8681e3b9df4fccbf9ed7dfa54e2"
+    sha256 cellar: :any_skip_relocation, mojave:        "883ad7178c717dd8ecf7366cd39023d025092e5f9f60122a8013d2f8bfe4028c"
   end
 
   depends_on "node@14"
