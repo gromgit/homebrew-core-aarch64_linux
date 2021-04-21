27d1037ca6643d061f29cac97e791b6b3b3c2b3b
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-04-21 09:18:45 +0000

    macchina: add 0.7.0 bottle.

--- Formula/macchina.rb
+++ Formula/macchina.rb
@@ -5,6 +5,13 @@ class Macchina < Formula
   sha256 "6ff1497864a400f5eade2b46984cbe1259a0f7a75bf7191a2a75e111a8ffe119"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "81795c02dec7c9dacfb7f13e19d78ecad1ad136d85c22906a9e6cecbba6e4ec4"
+    sha256 cellar: :any_skip_relocation, big_sur:       "6c28004ad4a39a4197528f351946d38224559e8a332eec5530c8ae7e4d3c7056"
+    sha256 cellar: :any_skip_relocation, catalina:      "f77b37d8ffd61ea1bb485a494f131ac4a49bfd8fa644085ebffb7b5cb8beecf0"
+    sha256 cellar: :any_skip_relocation, mojave:        "99acae50b39051e9f7bf62b1ae9bc98a093a22d54ad074171673424192b68395"
+  end
+
   depends_on "rust" => :build
 
   def install
