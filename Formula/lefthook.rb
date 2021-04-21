f93cb368558b9c995de20e49784057edf05a03f0
Svyatoslav Kryukov <s.g.kryukov@yandex.ru>
2021-04-21 22:55:08 +0300

    lefthook 0.7.3 (new formula)
    Closes #75706.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/lefthook.rb
@@ -0,0 +1,21 @@
+class Lefthook < Formula
+  desc "Fast and powerful Git hooks manager for any type of projects"
+  homepage "https://github.com/evilmartians/lefthook"
+  url "https://github.com/evilmartians/lefthook/archive/refs/tags/v0.7.3.tar.gz"
+  sha256 "643bb3d2a1b67d865285a9cadcad11f2588ba284570ab9924c8e3d2e902301ca"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    system "git", "init"
+    system bin/"lefthook", "install"
+
+    assert_predicate testpath/"lefthook.yml", :exist?
+    assert_match version.to_s, shell_output("#{bin}/lefthook version")
+  end
+end
