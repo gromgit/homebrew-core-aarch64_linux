d2a0e81aee473e0f78537f34c5e0e09c815fb0c0
rui <rui@chenrui.dev>
2021-04-21 06:08:35 +0000

    volta 1.0.4
    Closes #75644.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/volta.rb
+++ Formula/volta.rb
@@ -2,8 +2,8 @@ class Volta < Formula
   desc "JavaScript toolchain manager for reproducible environments"
   homepage "https://volta.sh"
   url "https://github.com/volta-cli/volta.git",
-      tag:      "v1.0.3",
-      revision: "5f39583bc64dce3be465d6c91dd35fd85a4c913c"
+      tag:      "v1.0.4",
+      revision: "e4ab3604097bd48b5d32fa8adf69daa336d66c21"
   license "BSD-2-Clause"
 
   livecheck do
