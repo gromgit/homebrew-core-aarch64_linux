febaf72a3e766145be45b6b76d9f4f4d5b269ceb
Bo Anderson <mail@boanderson.me>
2021-04-22 01:01:35 +0100

    pod2man: fix executable not using own lib files
    Closes #75722.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pod2man.rb
+++ Formula/pod2man.rb
@@ -3,7 +3,7 @@ class Pod2man < Formula
   homepage "https://www.eyrie.org/~eagle/software/podlators/"
   url "https://archives.eyrie.org/software/perl/podlators-4.14.tar.xz"
   sha256 "e504c3d9772b538d7ea31ce2c5e7a562d64a5b7f7c26277b1d7a0de1f6acfdf4"
-  revision 1
+  revision 2
 
   livecheck do
     url "https://archives.eyrie.org/software/perl/"
@@ -24,11 +24,13 @@ class Pod2man < Formula
                    "INSTALLSITEMAN1DIR=#{man1}", "INSTALLSITEMAN3DIR=#{man3}"
     system "make"
     system "make", "install"
+    bin.env_script_all_files libexec/"bin", PERL5LIB: "#{lib}/perl5"
   end
 
   test do
     (testpath/"test.pod").write "=head2 Test heading\n"
     manpage = shell_output("#{bin}/pod2man #{testpath}/test.pod")
     assert_match '.SS "Test heading"', manpage
+    assert_match "Pod::Man #{version}", manpage
   end
 end
