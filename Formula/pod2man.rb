b5a0c20a69cb60ec0fb29685b21cd1257f6aebcf
Bo Anderson <mail@boanderson.me>
2021-04-22 14:04:29 +0100

    pod2man: make keg_only on all platforms; bundle Pod::Simple
    Closes #75751.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/pod2man.rb
+++ Formula/pod2man.rb
@@ -3,7 +3,7 @@ class Pod2man < Formula
   homepage "https://www.eyrie.org/~eagle/software/podlators/"
   url "https://archives.eyrie.org/software/perl/podlators-4.14.tar.xz"
   sha256 "e504c3d9772b538d7ea31ce2c5e7a562d64a5b7f7c26277b1d7a0de1f6acfdf4"
-  revision 2
+  revision 3
 
   livecheck do
     url "https://archives.eyrie.org/software/perl/"
@@ -17,14 +17,26 @@ class Pod2man < Formula
     sha256 cellar: :any_skip_relocation, mojave:        "6a46194855951dff9cd5040fb70725947b65a1433868d6fda21fcc6262b598ae"
   end
 
-  keg_only :provided_by_macos
+  keg_only "perl ships with pod2man"
+
+  resource "Pod::Simple" do
+    url "https://cpan.metacpan.org/authors/id/K/KH/KHW/Pod-Simple-3.42.tar.gz"
+    sha256 "a9fceb2e0318e3786525e6bf205e3e143f0cf3622740819cab5f058e657e8ac5"
+  end
 
   def install
+    resource("Pod::Simple").stage do
+      system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+      system "make"
+      system "make", "install"
+    end
+    ENV.prepend_path "PERL5LIB", libexec/"lib/perl5"
+
     system "perl", "Makefile.PL", "INSTALL_BASE=#{prefix}",
                    "INSTALLSITEMAN1DIR=#{man1}", "INSTALLSITEMAN3DIR=#{man3}"
     system "make"
     system "make", "install"
-    bin.env_script_all_files libexec/"bin", PERL5LIB: "#{lib}/perl5"
+    bin.env_script_all_files libexec/"bin", PERL5LIB: "#{lib}/perl5:#{libexec}/lib/perl5"
   end
 
   test do
