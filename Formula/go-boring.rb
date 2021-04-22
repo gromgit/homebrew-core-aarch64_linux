e04831f0a54970991b3624015930b54e55ad0da9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-22 08:13:45 +0000

    go-boring: update 1.16.3b7 bottle.

--- Formula/go-boring.rb
+++ Formula/go-boring.rb
@@ -12,10 +12,10 @@ class GoBoring < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "5331c496ac45b0db7ecd75e42ba57679785665dfecc394bb621b7a153a6c7e56"
-    sha256 big_sur:       "12fb5d792b66e2a15c3e91a15384a82529e38f91fec78309ac303186f2adebe6"
-    sha256 catalina:      "bde128f58f456a72b3e7b5b6d4e8386cdbb5c3c2b9173aee0056a1b3d19ffd2d"
-    sha256 mojave:        "aab4af70585789c3e6c983f41b86b5d939a7b70c5abeecce31456d4625b654cb"
+    sha256 arm64_big_sur: "9e098a93c0fe477e0e090e22267961402f142a105af5786d442f9db6661d280b"
+    sha256 big_sur:       "1bb6162897cb0d7f064289bc573b75ec66c690eb06b47f6304c72f3690cc0971"
+    sha256 catalina:      "75be07f66ae61c99f0846dd8973712343917b54cc8b6032c403dd7083d7c7e6a"
+    sha256 mojave:        "75f25d86aad48ba1a7aa7b80c118107449a99c7bff70398d4e720e103670b8e0"
   end
 
   keg_only "it conflicts with the Go formula"
