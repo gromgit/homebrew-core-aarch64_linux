a93936e1205fbb7710a8d8775d0d14915b76550a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-22 08:08:04 +0000

    quantlib: update 1.22 bottle.

--- Formula/quantlib.rb
+++ Formula/quantlib.rb
@@ -6,10 +6,10 @@ class Quantlib < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "b930efbd942abbb5f9317d3d15fc0ca6a551cc3f7d5c3eb7cdb77464770d5ae0"
-    sha256 cellar: :any, big_sur:       "d5e6c206260e8ef5e297e464dd45979fe56bdaa938c75a071f70fce97b1b391e"
-    sha256 cellar: :any, catalina:      "18dd1cc854b50e811fec338ba31f7ea3d6aacf80a4f0c1f90c8d8db8d891b2b7"
-    sha256 cellar: :any, mojave:        "03c142589e975e9ba78966cd9f813fc77ae22aefea1049b43f3e7406cd090193"
+    sha256 cellar: :any, arm64_big_sur: "214a9ab699f70d2ebb04b657765226d2d1fed652b58569ad8c4313ff44bd8d9f"
+    sha256 cellar: :any, big_sur:       "603406285fa983b9d1554e9d63ac32aace6411ffe25c4159dba099d9e1a7db02"
+    sha256 cellar: :any, catalina:      "90c93befaffe6c8a9ef0375d9973cebc308efa015842c30e7230797586efb8c6"
+    sha256 cellar: :any, mojave:        "cd8bf24144bb637bffc30582a3bea35a1c4a14aee394ae355a541d414e88484f"
   end
 
   head do
