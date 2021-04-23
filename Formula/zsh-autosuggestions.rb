8fb3d1c768efb1f0d24a3bf4eed1919aac002bee
Mike McQuaid <mike@mikemcquaid.com>
2021-04-23 15:05:24 +0000

    zsh-autosuggestions: add 0.6.4 bottle.

--- Formula/zsh-autosuggestions.rb
+++ Formula/zsh-autosuggestions.rb
@@ -5,6 +5,10 @@ class ZshAutosuggestions < Formula
   sha256 "0b6e251ced5fd7b5b78ea01f798ecc1b46169743a717567f0ec0a21198a372e8"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "6f79c9ded23bb32288fc81bcb969ee71b2c6cdba4f88c2c999db6eaf3286bb8c"
+  end
+
   def install
     pkgshare.install "zsh-autosuggestions.zsh"
   end
