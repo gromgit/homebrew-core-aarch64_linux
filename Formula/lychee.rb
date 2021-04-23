11d3369762a7eccf2349eefc732ffe183cb0ba13
Nanda H Krishna <me@nandahkrishna.com>
2021-04-23 16:51:02 +0000

    lychee: add 0.7.0 bottle.

--- Formula/lychee.rb
+++ Formula/lychee.rb
@@ -6,6 +6,13 @@ class Lychee < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/lycheeverse/lychee.git"
 
+  bottle do
+    sha256 cellar: :any, arm64_big_sur: "557cbb8aa12ce8173aba69f12c5cfee97b31e489a08661b002bbcc8480032ae3"
+    sha256 cellar: :any, big_sur:       "851c0881b1a76ce34c018c231248e6a17f11a7703ece7d0ea24c9ca36f69826d"
+    sha256 cellar: :any, catalina:      "1174396eb8beb8828a66e0c5fe25cf4ecf177d305ec57404d1620fa5243f5d9d"
+    sha256 cellar: :any, mojave:        "e36701a4a372cd127f0b02f0dee5ff51cd6a98ee1df6e828af36b7865677ec91"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@1.1"
 
