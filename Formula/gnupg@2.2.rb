c9f2940e1fc5c68f6c9e21bfbb13ca1179b6a9ac
Christoph Blecker <admin@toph.ca>
2021-04-22 21:39:46 -0700

    gnupg@2.2 (new versioned formula)
    Closes #75796.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/gnupg@2.2.rb
@@ -0,0 +1,72 @@
+class GnupgAT22 < Formula
+  desc "GNU Pretty Good Privacy (PGP) package"
+  homepage "https://gnupg.org/"
+  url "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.2.27.tar.bz2"
+  sha256 "34e60009014ea16402069136e0a5f63d9b65f90096244975db5cea74b3d02399"
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    url "https://gnupg.org/ftp/gcrypt/gnupg/"
+    regex(/href=.*?gnupg[._-]v?(2\.2(?:\.\d+)+)\.t/i)
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
+  depends_on "gnutls"
+  depends_on "libassuan"
+  depends_on "libgcrypt"
+  depends_on "libgpg-error"
+  depends_on "libksba"
+  depends_on "libusb"
+  depends_on "npth"
+  depends_on "pinentry"
+
+  uses_from_macos "sqlite" => :build
+
+  on_linux do
+    depends_on "libidn"
+  end
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--sbindir=#{bin}",
+                          "--sysconfdir=#{etc}",
+                          "--enable-all-tests",
+                          "--enable-symcryptrun",
+                          "--with-pinentry-pgm=#{Formula["pinentry"].opt_bin}/pinentry"
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  def post_install
+    (var/"run").mkpath
+    quiet_system "killall", "gpg-agent"
+  end
+
+  test do
+    (testpath/"batch.gpg").write <<~EOS
+      Key-Type: RSA
+      Key-Length: 2048
+      Subkey-Type: RSA
+      Subkey-Length: 2048
+      Name-Real: Testing
+      Name-Email: testing@foo.bar
+      Expire-Date: 1d
+      %no-protection
+      %commit
+    EOS
+    begin
+      system bin/"gpg", "--batch", "--gen-key", "batch.gpg"
+      (testpath/"test.txt").write "Hello World!"
+      system bin/"gpg", "--detach-sign", "test.txt"
+      system bin/"gpg", "--verify", "test.txt.sig"
+    ensure
+      system bin/"gpgconf", "--kill", "gpg-agent"
+    end
+  end
+end
