128f9e24adad3d2ec5979a16190597fa7d2f7783
Dominyk Tiller <dominyktiller@gmail.com>
2021-04-23 05:00:32 +0100

    amp: fix ARM64/Apple Silicon build; update Cargo deps
    Closes #75790.
    
    Signed-off-by: Bo Anderson <mail@boanderson.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/amp.rb
+++ Formula/amp.rb
@@ -3,7 +3,8 @@ class Amp < Formula
   homepage "https://amp.rs"
   url "https://github.com/jmacdonald/amp/archive/0.6.2.tar.gz"
   sha256 "9279efcecdb743b8987fbedf281f569d84eaf42a0eee556c3447f3dc9c9dfe3b"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
+  revision 1
   head "https://github.com/jmacdonald/amp.git"
 
   bottle do
@@ -19,6 +20,20 @@ class Amp < Formula
   uses_from_macos "libiconv"
 
   def install
+    # Upstream specifies very old versions of onig_sys/cc that
+    # cause issues when using Homebrew's clang shim on Apple Silicon.
+    # Forcefully upgrade `onig_sys` and `cc` to slightly newer versions
+    # that enable a succesful build.
+    # https://github.com/jmacdonald/amp/issues/222
+    inreplace "Cargo.lock" do |f|
+      f.gsub! "68.0.1", "68.2.1"
+      f.gsub! "5c6be7c4f985508684e54f18dd37f71e66f3e1ad9318336a520d7e42f0d3ea8e",
+              "195ebddbb56740be48042ca117b8fb6e0d99fe392191a9362d82f5f69e510379"
+      f.gsub! "1.0.45", "1.0.67"
+      f.gsub! "4fc9a35e1f4290eb9e5fc54ba6cf40671ed2a2514c3eeb2b2a908dda2ea5a1be",
+              "e3c69b077ad434294d3ce9f1f6143a2a4b89a8a2d54ef813d85003a4fd1137fd"
+    end
+
     system "cargo", "install", *std_cargo_args
   end
 
