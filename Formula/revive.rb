c41d796a68f398cb60c50ab8b3733fe08a4a159b
rui <rui@chenrui.dev>
2021-04-24 17:08:27 +0000

    revive: add 1.0.6 bottle.

--- Formula/revive.rb
+++ Formula/revive.rb
@@ -7,6 +7,13 @@ class Revive < Formula
   license "MIT"
   head "https://github.com/mgechev/revive.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5140eea58dffb9525b63a9d6483e3696662f3f44352fc0555862d23047226e79"
+    sha256 cellar: :any_skip_relocation, big_sur:       "936d94d5807f1b3ca1c110ff51347848875cdc83f077f0c14ac50363d526528c"
+    sha256 cellar: :any_skip_relocation, catalina:      "3c5a1640c8619108a6775da06575c571f1f935b510e2f762b66ec4e8a7100338"
+    sha256 cellar: :any_skip_relocation, mojave:        "f0f79abb069d51418d4ef23f0567ed9ee2c116f3d37d59d5e92eca1c62de94ae"
+  end
+
   depends_on "go" => :build
 
   def install
