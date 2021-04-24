3b4300bd4d26aa8244478c602cc6424ae93bb471
Thierry Moisan <thierry.moisan@gmail.com>
2021-04-23 20:48:52 -0400

    nodebrew 1.1.0
    * nodebrew 1.1.0
    * nodebrew: remove `bottle :unneeded`
    
    Closes #75837.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/nodebrew.rb
+++ Formula/nodebrew.rb
@@ -1,12 +1,10 @@
 class Nodebrew < Formula
   desc "Node.js version manager"
   homepage "https://github.com/hokaccha/nodebrew"
-  url "https://github.com/hokaccha/nodebrew/archive/v1.0.1.tar.gz"
-  sha256 "c34e7186d4fd493c5417ad5563ad39fd493a42695bd9a7758c3df10380e43399"
+  url "https://github.com/hokaccha/nodebrew/archive/v1.1.0.tar.gz"
+  sha256 "b2046d97392ed971254bee2026cfcf8fb59225f51b566ec4b77e9355a861c8a7"
   license "MIT"
   head "https://github.com/hokaccha/nodebrew.git"
-
-  bottle :unneeded
 
   def install
     bin.install "nodebrew"
