834dc00cb14238d305008e3937ff2283ce11785b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-24 19:11:32 +0000

    pmd: add 6.34.0 bottle.

--- Formula/pmd.rb
+++ Formula/pmd.rb
@@ -5,6 +5,13 @@ class Pmd < Formula
   sha256 "fb2f85fd36a243116340b6aafcbbb74eac982c27f8c22cd0860f1e08aef63a4c"
   license "BSD-4-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9b1d9d7cc7df65b103bdffba16119bfc49c6a4922dbf0ef59a4083ce4c3e4e8b"
+    sha256 cellar: :any_skip_relocation, big_sur:       "779884d72d65f696a47dd59c6524852c287d3f1d205d09ed37d041b7ada58415"
+    sha256 cellar: :any_skip_relocation, catalina:      "779884d72d65f696a47dd59c6524852c287d3f1d205d09ed37d041b7ada58415"
+    sha256 cellar: :any_skip_relocation, mojave:        "779884d72d65f696a47dd59c6524852c287d3f1d205d09ed37d041b7ada58415"
+  end
+
   depends_on "openjdk"
 
   def install
