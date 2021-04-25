57958057ef5b8a900ebfbb3487002c865e56d0e0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-25 17:24:40 +0000

    lean: update 3.29.0 bottle.

--- Formula/lean.rb
+++ Formula/lean.rb
@@ -14,10 +14,10 @@ class Lean < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "54bf309b9271ff9f2f4b60945fe6348dfe517d6ac61670fc2ba1e1bbbe48bded"
-    sha256 cellar: :any, big_sur:       "69220ca1e33cba1e97e69c49c8fef33a44cec6b51bc1d5c8fab24266f05fa98a"
-    sha256 cellar: :any, catalina:      "7061fcd438716294d81532240eca7ca206a87198d3715d6227b0ce91a2182f58"
-    sha256 cellar: :any, mojave:        "6220b1f134ce7fe7376dd8076f28619eaf1fd57d45f8e316022e6d9275a7300f"
+    sha256 cellar: :any, arm64_big_sur: "4bc6829c4bd04da5b824636f38cc33b28a91b6c0897623b979cfa7047d70c74a"
+    sha256 cellar: :any, big_sur:       "d3535e7716eb63e12b18814158bcd71831df116732a3b1b31d9f5b538570bb1d"
+    sha256 cellar: :any, catalina:      "1c311fa94cdef29fa311e97112c94342f292017584767fda2b1327f01dfd5151"
+    sha256 cellar: :any, mojave:        "b71d9c9389dddada7c4cb26559258515f2d270b3bd548c0d6776270722118687"
   end
 
   depends_on "cmake" => :build
