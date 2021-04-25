79bc5b2d2391a6ba7c92be1f79929731aaa72ef5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-04-25 23:35:25 +0100

    freeling: revision bump (boost 1.76.0)
    Also, add patches to fix build failure.
    
    See https://github.com/TALP-UPC/FreeLing/issues/114.

--- Formula/freeling.rb
+++ Formula/freeling.rb
@@ -4,7 +4,7 @@ class Freeling < Formula
   url "https://github.com/TALP-UPC/FreeLing/releases/download/4.2/FreeLing-src-4.2.tar.gz"
   sha256 "f96afbdb000d7375426644fb2f25baff9a63136dddce6551ea0fd20059bfce3b"
   license "AGPL-3.0-only"
-  revision 4
+  revision 5
 
   bottle do
     sha256 arm64_big_sur: "b381f9741689e57db0de8cfa8e71758127fd33aaff347ce650f15669a143ac3c"
@@ -21,6 +21,27 @@ class Freeling < Formula
   conflicts_with "eigen", because: "freeling ships its own copy of eigen"
   conflicts_with "foma", because: "freeling ships its own copy of foma"
   conflicts_with "hunspell", because: "both install 'analyze' binary"
+
+  # Fixes `error: use of undeclared identifier 'log'`
+  # https://github.com/TALP-UPC/FreeLing/issues/114
+  patch do
+    url "https://github.com/TALP-UPC/FreeLing/commit/e052981e93e0a663784b04391471a5aa9c37718f.patch?full_index=1"
+    sha256 "02c8b9636413182df420cb2f855a96de8760202a28abeff2ea7bdbe5f95deabc"
+  end
+
+  # Fixes `error: use of undeclared identifier 'fabs'`
+  # Also reported in issue linked above
+  patch do
+    url "https://github.com/TALP-UPC/FreeLing/commit/36e267b453c4f2bce88014382c7e661657d1b234.patch?full_index=1"
+    sha256 "6cf1d4dfa381d7d43978cde194599ffadf7596bab10ff48cdb214c39363b55a0"
+  end
+
+  # Also fixes `error: use of undeclared identifier 'fabs'`
+  # See issue in first patch
+  patch do
+    url "https://github.com/TALP-UPC/FreeLing/commit/34a1a78545fb6a4ca31ee70e59fd46211fd3f651.patch?full_index=1"
+    sha256 "8f7f87a630a9d13ea6daebf210b557a095b5cb8747605eb90925a3aecab97e18"
+  end
 
   def install
     # Allow compilation without extra data (more than 1 GB), should be fixed
