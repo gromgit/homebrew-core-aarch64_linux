f3eb96e924d81a112a87fbcb7ac4257a29c7f7fd
miccal <miccal.matthews@gmail.com>
2021-04-26 07:57:50 +0800

    imagemagick@6 6.9.12-9
    Closes #75910.
    
    Signed-off-by: Thierry Moisan <thierry.moisan@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/imagemagick@6.rb
+++ Formula/imagemagick@6.rb
@@ -4,8 +4,8 @@ class ImagemagickAT6 < Formula
   # Please always use a mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://www.imagemagick.org/download/releases/ImageMagick-6.9.12-7.tar.xz"
-  sha256 "57631b1665d004680a9f7c072434c4897f445a7cded258ec28b0006587b485db"
+  url "https://www.imagemagick.org/download/releases/ImageMagick-6.9.12-9.tar.xz"
+  sha256 "b8c75ade00884ed06007111f2aa144ff3125e15c6e0667d6aea8cec742cbc1ee"
   license "ImageMagick"
   head "https://github.com/imagemagick/imagemagick6.git"
 
