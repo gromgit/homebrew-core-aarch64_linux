35c0ed80f6a2d49651f5b0e87554a35b28171ea5
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-04-25 17:18:59 +0100

    hfstospell: update 0.5.2_2 bottle.

--- Formula/hfstospell.rb
+++ Formula/hfstospell.rb
@@ -12,10 +12,10 @@ class Hfstospell < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "758ed5ec9ea9fc762b891ff8f27d83a840195fe86636cd9706115063841dcf0d"
-    sha256 cellar: :any, big_sur:       "9bef29b829d94f52c22206e365f364a604620c194fefea2134ba0827fee6b5a8"
-    sha256 cellar: :any, catalina:      "93b6e9890e78fcde61b18277861e3c3ec77039e22f4c9c723078791d787521e0"
-    sha256 cellar: :any, mojave:        "f33ba3c1c11a54a7c349b41a6c230735b54cf6c58314a3a4d3fa3a8d9b1fe809"
+    sha256 cellar: :any, arm64_big_sur: "1a5437ebb7e8abeae096734d53edbbc8cf154f6635f8e15ac3a1cfa038782e85"
+    sha256 cellar: :any, big_sur:       "6fb2851153c12aa38ed01a7335781df78be3490380e6713b2a9c642f88e737d0"
+    sha256 cellar: :any, catalina:      "0651d2057fcf3c0242bcd277b0ddafb247c0f00fc78d2652e9eae9c82776f923"
+    sha256 cellar: :any, mojave:        "25a4f7bfe15fae7efd0ce6cf1ccedb150571935de5e0266cbf7fa472290bbf6d"
   end
 
   depends_on "autoconf" => :build
