481420f32ce63debe3d600169fadbbc3a494e1f7
Antonio Camargo <antoniop.camargo@gmail.com>
2021-04-26 09:36:20 -0700

    seqkit 0.16.0 (new formula)
    Closes #75950.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/seqkit.rb
@@ -0,0 +1,25 @@
+class Seqkit < Formula
+  desc "Cross-platform and ultrafast toolkit for FASTA/Q file manipulation in Golang"
+  homepage "https://bioinf.shenwei.me/seqkit"
+  url "https://github.com/shenwei356/seqkit/archive/refs/tags/v0.16.0.tar.gz"
+  sha256 "1a348c57dafb4ba6c96815ad7298004db1faa120211032dd6e7948908c2a12bf"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  resource "testdata" do
+    url "https://raw.githubusercontent.com/shenwei356/seqkit/e37d70a7e0ca0e53d6dbd576bd70decac32aba64/tests/seqs4amplicon.fa"
+    sha256 "b0f09da63e3c677cc698d5cdff60e2d246368263c22385937169a9a4c321178a"
+  end
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./seqkit"
+  end
+
+  test do
+    resource("testdata").stage do
+      assert_equal ">seq1\nCCCACTGAAA",
+      shell_output("#{bin}/seqkit amplicon --quiet -F CCC -R TTT seqs4amplicon.fa").strip
+    end
+  end
+end
