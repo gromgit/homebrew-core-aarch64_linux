ced33841e188fd0cf559bde57bff843245ab4af0
rui <rui@chenrui.dev>
2021-04-26 02:28:15 +0000

    just 0.9.1
    Closes #75918.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/just.rb
+++ Formula/just.rb
@@ -1,8 +1,8 @@
 class Just < Formula
   desc "Handy way to save and run project-specific commands"
   homepage "https://github.com/casey/just"
-  url "https://github.com/casey/just/archive/v0.9.0.tar.gz"
-  sha256 "877fdd517722ec1a554b046e1a9fd112f3503d91211702895c5ba12b29dcbcc6"
+  url "https://github.com/casey/just/archive/v0.9.1.tar.gz"
+  sha256 "ac3eba079f11447e58c06d3efd02a7c2fed9581de533db7f2032f217efdaa446"
   license "CC0-1.0"
 
   bottle do
