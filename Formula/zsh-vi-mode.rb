2009bd60791173697793c30c0aac600f380dfddd
JT <jeffreytse.mail@gmail.com>
2021-04-26 08:20:30 -0700

    zsh-vi-mode: add head, update caveats and test (#75284)
    * chore: update the formula of zsh-vi-mode
    
    * chore: update caveats of zsh-vi-mode

--- Formula/zsh-vi-mode.rb
+++ Formula/zsh-vi-mode.rb
@@ -4,6 +4,7 @@ class ZshViMode < Formula
   url "https://github.com/jeffreytse/zsh-vi-mode/archive/refs/tags/v0.8.2.tar.gz"
   sha256 "888d68b451fc3221c2ee1648dbfbff38e524048b2c78620a244650fc76946c28"
   license "MIT"
+  head "https://github.com/jeffreytse/zsh-vi-mode.git"
 
   bottle :unneeded
 
@@ -14,13 +15,13 @@ class ZshViMode < Formula
 
   def caveats
     <<~EOS
-      To activate the zsh vi mode, add the following at the end of your .zshrc:
+      To activate the zsh vi mode, add the following line to your .zshrc:
         source #{opt_pkgshare}/zsh-vi-mode.plugin.zsh
     EOS
   end
 
   test do
     assert_match "zsh-vi-mode",
-      shell_output("zsh -c '. #{pkgshare}/zsh-vi-mode.plugin.zsh && echo $ZVM_NAME'")
+      shell_output("zsh -c '. #{pkgshare}/zsh-vi-mode.plugin.zsh && zvm_version'")
   end
 end
