bc6fa91ea855140807d5035f9e7b72e1cc017468
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-27 08:08:33 +0000

    dmd: update 2.096.1 bottle.

--- Formula/dmd.rb
+++ Formula/dmd.rb
@@ -24,9 +24,9 @@ class Dmd < Formula
   end
 
   bottle do
-    sha256 big_sur:  "0c7fbdf3cd08b39f8b4b930483c0873f39db7e45b7ecbb003bb89b38b208f0ac"
-    sha256 catalina: "d3ff6f70a572e29933b12c8354856c1edbc8c3316527a29f3c238b71454c52e1"
-    sha256 mojave:   "ec9279e77c4783583a0d2afc8667d373808db62535aabf32364b10067519fb3b"
+    sha256 big_sur:  "a69862b78ab828f5339a5c12841bad47ae4266e2abba8fe03e4af618295cd555"
+    sha256 catalina: "cd0e6df75c5ed352d99f443d4d90554971abbc882db4b0bd7ddf46c5b4d02b18"
+    sha256 mojave:   "9a1deb1735ba1301680e04485ac3727256bfd494fc2d3cc19b8a8719931b0038"
   end
 
   head do
