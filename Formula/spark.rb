71eff3327275c645a416b0636e8b6b606b911a02
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-27 19:13:34 +0000

    spark: add 1.0.1 bottle.

--- Formula/spark.rb
+++ Formula/spark.rb
@@ -5,6 +5,10 @@ class Spark < Formula
   sha256 "a81c1bc538ce8e011f62264fe6f33d28042ff431b510a6359040dc77403ebab6"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "c98b248ad29955ccf560e99cb9c325fb28d881391c5bf9c0888251a02f41792d"
+  end
+
   def install
     bin.install "spark"
   end
