287ca059bb54af25f367241bfc25d86d821f47fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-27 21:07:03 +0000

    xclogparser: update 0.2.27 bottle.

--- Formula/xclogparser.rb
+++ Formula/xclogparser.rb
@@ -6,9 +6,9 @@ class Xclogparser < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "28153d5c4b6ac4cc17573c80ea3ff2a56fb2933c2442d484369864990dd705fb"
-    sha256 cellar: :any_skip_relocation, big_sur:       "b1adea699bc508e81a3df58c63cf17a7f65f45a5de475b9815581cd263af3e22"
-    sha256 cellar: :any_skip_relocation, catalina:      "b21d1b69a9754b7d2677e28d16106109864648e32044f7fd13943f0c8122c244"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "516b419cf3e2174b5559a4480a79d9ebc5e9c083a5ecdf5899bc129f8d3c1a74"
+    sha256 cellar: :any_skip_relocation, big_sur:       "f01442000648fe2f63f605770b4cccf2381cbd5a506d00364646982d718c2a09"
+    sha256 cellar: :any_skip_relocation, catalina:      "6811638510465dff36153b98f42a530eae9b7f14b2c8df1dc2a46580d3ebdd5e"
   end
 
   depends_on xcode: "12.0"
