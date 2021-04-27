b70069d1259eeb0e81620bbe8d7d56e05df9c78f
rui <rui@chenrui.dev>
2021-04-27 12:11:10 +0000

    structurizr-cli 1.10.0
    Closes #76023.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/structurizr-cli.rb
+++ Formula/structurizr-cli.rb
@@ -1,8 +1,8 @@
 class StructurizrCli < Formula
   desc "Command-line utility for Structurizr"
   homepage "https://structurizr.com"
-  url "https://github.com/structurizr/cli/releases/download/v1.9.0/structurizr-cli-1.9.0.zip"
-  sha256 "3c7a8166666cb87d6598d3987d553f02f39036426f016369774becc8f2e3449e"
+  url "https://github.com/structurizr/cli/releases/download/v1.10.0/structurizr-cli-1.10.0.zip"
+  sha256 "c9810e934ef9b5cf27dbb50392599b6de6451969b0232acdacc526d56d4b735d"
   license "Apache-2.0"
 
   bottle :unneeded
