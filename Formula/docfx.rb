c84b48e905e5cf61e200f579949c53d2fa03fb16
Amar1729 <amar.paul16@gmail.com>
2021-04-28 19:37:37 -0400

    docfx: remove `bottle :unneeded`, restrict to x86_64
    Closes #76195.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -5,8 +5,7 @@ class Docfx < Formula
   sha256 "95f96be280998e7ea6581d418c9b0d99dffb0c86bcca7f1dd257062163e0a79c"
   license "MIT"
 
-  bottle :unneeded
-
+  depends_on arch: :x86_64
   depends_on "mono"
 
   def install
