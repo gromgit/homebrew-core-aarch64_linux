56671451dcc662b73e69d007e0bee30a45c54def
Amar1729 <amar.paul16@gmail.com>
2021-04-27 23:44:06 -0400

    apm-bash-completion: remove `bottle :unneeded`
    Closes #76111.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/apm-bash-completion.rb
+++ Formula/apm-bash-completion.rb
@@ -4,8 +4,6 @@ class ApmBashCompletion < Formula
   url "https://github.com/vigo/apm-bash-completion/archive/1.0.0.tar.gz"
   sha256 "1043a7f19eabe69316ea483830fb9f78d6c90853aaf4dd7ed60007af7f0d6e9d"
 
-  bottle :unneeded
-
   def install
     bash_completion.install "apm"
   end
