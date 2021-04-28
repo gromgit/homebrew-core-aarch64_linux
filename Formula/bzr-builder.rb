2148bf5e7c5ef983dd7e3587ef2e4b2e8f19ee02
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-28 06:07:28 +0000

    bzr-builder: add 0.7.3 bottle.

--- Formula/bzr-builder.rb
+++ Formula/bzr-builder.rb
@@ -4,6 +4,10 @@ class BzrBuilder < Formula
   url "https://launchpad.net/bzr-builder/trunk/0.7.3/+download/bzr-builder-0.7.3.tar.gz"
   sha256 "9f8a078eafd6700ccbefa4e7e3f7df3240e15a2003c9538135c4be945ac90c91"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "54b83a353f26372404bf40fc06f56b2105527e58e9f7aa88c22cc89b8ee3254d"
+  end
+
   depends_on "bazaar"
 
   def install
