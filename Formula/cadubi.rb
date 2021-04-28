eb73ae544eb8013ce422b0abf2c279ded5cfac91
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-28 05:09:58 +0000

    cadubi: add 1.3.4 bottle.

--- Formula/cadubi.rb
+++ Formula/cadubi.rb
@@ -5,6 +5,10 @@ class Cadubi < Formula
   sha256 "624f85bb16d8b0bc392d761d1121828d09cfc79b3ded5b1220e9b4262924a1a0"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "9c2832e6449cf5fd7ebbadd2427732b242633ba5ad041046e94e80da63e963e6"
+  end
+
   def install
     inreplace "cadubi", "$Bin/help.txt", "#{doc}/help.txt"
     bin.install "cadubi"
