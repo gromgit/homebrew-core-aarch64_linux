fa1ad2f383718b5ed039eafefbe0519f972cf07d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-28 08:07:17 +0000

    sniffglue: update 0.12.1 bottle.

--- Formula/sniffglue.rb
+++ Formula/sniffglue.rb
@@ -6,10 +6,10 @@ class Sniffglue < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7711fba881a079e3de12648a63c98d4e97539973408ec444b091b6b301eff433"
-    sha256 cellar: :any_skip_relocation, big_sur:       "6d6894afc00176326c7b4932573c427d545b99faebecd38e95eefe41b3135872"
-    sha256 cellar: :any_skip_relocation, catalina:      "5fc37f26b3b0f6732901f1d6a2ad4143c133c787b49266517f509ef7c59c48c1"
-    sha256 cellar: :any_skip_relocation, mojave:        "d6a6dc71f619a033e206bd37a96ee59256b68996630ce32574cf6f5c99977c35"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6553139c58aa3dd91eed5d56e58c25bdc851084f3973421e21cb5ac5cb908df0"
+    sha256 cellar: :any_skip_relocation, big_sur:       "807b2d72684f604b661b2539e5fbe326f8cbf2060f68cfedbd51ca5602146013"
+    sha256 cellar: :any_skip_relocation, catalina:      "ff602a3b7afc7b6193399651cb923ae61610800b1e1a15ad6b39a833faf28e1d"
+    sha256 cellar: :any_skip_relocation, mojave:        "071909cb1e3ba64edf4abf0fda2b6ccf4de286c9c5a2ae5c0d408d7977020b30"
   end
 
   depends_on "rust" => :build
