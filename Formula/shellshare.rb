282f70240540be588a3055e0a24e6a94b0eb9d47
Amar1729 <amar.paul16@gmail.com>
2021-04-28 19:45:18 -0400

    shellshare: remove `bottle :unneeded`
    Closes #76198.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/shellshare.rb
+++ Formula/shellshare.rb
@@ -5,8 +5,6 @@ class Shellshare < Formula
   sha256 "0a102c863f60402ab48908563edde38450add0ae02454360fa94065824a61907"
   license "Apache-2.0"
 
-  bottle :unneeded
-
   def install
     bin.install "public/bin/shellshare"
   end
