ac40016b7811407dcfad8f55f1675b2cd182d169
Amar1729 <amar.paul16@gmail.com>
2021-04-27 23:41:50 -0400

    mix-completion: remove `bottle :unneeded`
    Closes #76109.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/mix-completion.rb
+++ Formula/mix-completion.rb
@@ -5,8 +5,6 @@ class MixCompletion < Formula
   sha256 "0e3e94b199f847926f3668b4cebf1b132e63a44d438425dd5c45ac4a299f28f3"
   head "https://github.com/davidhq/mix-power-completion.git"
 
-  bottle :unneeded
-
   def install
     bash_completion.install "mix"
   end
