e79017a922e8ffd57ac68f9eb4992a14f05239db
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-28 04:10:21 +0000

    launchctl-completion: add 1.0 bottle.

--- Formula/launchctl-completion.rb
+++ Formula/launchctl-completion.rb
@@ -4,6 +4,10 @@ class LaunchctlCompletion < Formula
   url "https://github.com/CamJN/launchctl-completion/archive/v1.0.tar.gz"
   sha256 "b21c39031fa41576d695720b295dce57358c320964f25d19a427798d0f0df7a0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "14ff31532b6b8df8c32ffaad844a0a7fa5d1d12b0fc9df3f3497bb4a38b67945"
+  end
+
   def install
     bash_completion.install "launchctl-completion.sh" => "launchctl"
   end
