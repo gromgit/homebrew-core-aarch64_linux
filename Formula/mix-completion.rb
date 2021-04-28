2152dedab4fccfe2cc863b7fc642f7c104cafb1c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-28 05:10:57 +0000

    mix-completion: add 0.8.2 bottle.

--- Formula/mix-completion.rb
+++ Formula/mix-completion.rb
@@ -5,6 +5,10 @@ class MixCompletion < Formula
   sha256 "0e3e94b199f847926f3668b4cebf1b132e63a44d438425dd5c45ac4a299f28f3"
   head "https://github.com/davidhq/mix-power-completion.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "faa9e0edf1a9e4aac06ea640bfa2ff4ee4b6e81d086c33cd3aa999ef77b54a55"
+  end
+
   def install
     bash_completion.install "mix"
   end
