542081e4efeefb6eca63ae80281bd428aa47e17d
Nicco <hi@nicco.io>
2021-04-28 13:10:41 +0000

    autorestic 1.0.8 (new formula)
    Closes #76160.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/autorestic.rb
@@ -0,0 +1,31 @@
+class Autorestic < Formula
+  desc "High level CLI utility for restic"
+  homepage "https://autorestic.vercel.app/"
+  url "https://github.com/cupcakearmy/autorestic/archive/v1.0.8.tar.gz"
+  sha256 "0b27fbc39586471c63f9943029c0c082ba425befb17ff6f7172f10f8941c9210"
+  license "Apache-2.0"
+  head "https://github.com/cupcakearmy/autorestic.git"
+
+  depends_on "go" => :build
+  depends_on "restic"
+
+  def install
+    system "go", "build", *std_go_args, "./main.go"
+    (bash_completion/"autorestic").write Utils.safe_popen_read("#{bin}/autorestic", "completion", "bash")
+    (zsh_completion/"_autorestic").write Utils.safe_popen_read("#{bin}/autorestic", "completion", "zsh")
+    (fish_completion/"autorestic.fish").write Utils.safe_popen_read("#{bin}/autorestic", "completion", "fish")
+  end
+
+  test do
+    require "yaml"
+    config = {
+      "locations" => { "foo" => { "from" => "repo", "to" => ["bar"] } },
+      "backends"  => { "bar" => { "type" => "local", "key" => "secret", "path" => "data" } },
+    }
+    File.open(testpath/".autorestic.yml", "w") { |file| file.write(config.to_yaml) }
+    (testpath/"repo"/"test.txt").write("This is a testfile")
+    system "#{bin}/autorestic", "backup", "-a"
+    system "#{bin}/autorestic", "restore", "-l", "foo", "--to", "restore"
+    assert compare_file testpath/"repo"/"test.txt", testpath/"restore"/"test.txt"
+  end
+end
