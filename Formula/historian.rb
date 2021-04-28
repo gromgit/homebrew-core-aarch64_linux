7297f89602308f2cdb4053f85595f3c974d9bc1e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-28 06:07:27 +0000

    historian: add 0.0.2 bottle.

--- Formula/historian.rb
+++ Formula/historian.rb
@@ -5,6 +5,10 @@ class Historian < Formula
   sha256 "691b131290ddf06142a747755412115fec996cb9cc2ad8e8f728118788b3fe05"
   license "BSD-2-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "f1887b168f03631cb1a51fb419940d8f2bee1e9fb658cc1cd130ded4aa0951eb"
+  end
+
   def install
     bin.install "hist"
   end
