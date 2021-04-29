152a738ce66955cb72d2b1c9567b366875de0634
rui <rui@chenrui.dev>
2021-04-29 04:11:02 +0000

    apache-flink 1.12.3
    * apache-flink 1.12.3
    * apache-flink: remove `bottle :unneeded`
      See #75943.
    
    Closes #76246.
    
    Co-authored-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/apache-flink.rb
+++ Formula/apache-flink.rb
@@ -1,14 +1,12 @@
 class ApacheFlink < Formula
   desc "Scalable batch and stream data processing"
   homepage "https://flink.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.12.2/flink-1.12.2-bin-scala_2.12.tgz"
-  mirror "https://archive.apache.org/dist/flink/flink-1.12.2/flink-1.12.2-bin-scala_2.12.tgz"
-  version "1.12.2"
-  sha256 "f9509529a6fbcde6b7a9a58e527aa40e68140957ea72f1b842afb06da9a82f91"
+  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.12.3/flink-1.12.3-bin-scala_2.12.tgz"
+  mirror "https://archive.apache.org/dist/flink/flink-1.12.3/flink-1.12.3-bin-scala_2.12.tgz"
+  version "1.12.3"
+  sha256 "1b0c854fc12b85dc2225ed39e06973e35a30a60b5dcb6e82c5616dd7cf234f9a"
   license "Apache-2.0"
   head "https://github.com/apache/flink.git"
-
-  bottle :unneeded
 
   depends_on "openjdk@11"
 
