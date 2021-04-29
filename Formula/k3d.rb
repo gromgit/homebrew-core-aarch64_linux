7d52004bd3f7438c09fb3bdc4a52ff7bb1c5610d
rui <rui@chenrui.dev>
2021-04-29 08:10:32 +0000

    k3d 4.4.3
    Closes #76262.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/k3d.rb
+++ Formula/k3d.rb
@@ -1,10 +1,9 @@
 class K3d < Formula
   desc "Little helper to run Rancher Lab's k3s in Docker"
   homepage "https://k3d.io"
-  url "https://github.com/rancher/k3d/archive/v4.4.2.tar.gz"
-  sha256 "e3e6bc85b7f548580023671f59cec18c4c7f99ad3c7c37d1838984f82d4bf86c"
+  url "https://github.com/rancher/k3d/archive/v4.4.3.tar.gz"
+  sha256 "b7ff3d5fac9d0bc6c58c3e5abdb5b2f38bf63a7a6bf0c3872e64f63879f4c160"
   license "MIT"
-  revision 1
 
   livecheck do
     url :stable
