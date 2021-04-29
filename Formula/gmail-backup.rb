5a7c8ede0632db3a71d8ab4fe4c214c639c57410
Amar1729 <amar.paul16@gmail.com>
2021-04-28 20:43:55 -0400

    gmail-backup: remove `bottle :unneeded`
    Closes #76226.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gmail-backup.rb
+++ Formula/gmail-backup.rb
@@ -4,8 +4,6 @@ class GmailBackup < Formula
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/gmail-backup-com/gmail-backup-20110307.tar.gz"
   sha256 "caf7cb40ea580e506f90a6029a64fedaf1234093c729ca7e6e36efbd709deb93"
 
-  bottle :unneeded
-
   def install
     bin.install "gmail-backup.py" => "gmail-backup"
     libexec.install Dir["*"]
