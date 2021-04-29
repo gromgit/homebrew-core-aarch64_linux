edd3a488f5d1c48b1cba1128c90ac8d486e6f7a7
Nanda H Krishna <me@nandahkrishna.com>
2021-04-29 15:44:55 +0530

    acl2: revision bump (sbcl 2.1.4)
    Closes #76193.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/acl2.rb
+++ Formula/acl2.rb
@@ -4,7 +4,7 @@ class Acl2 < Formula
   url "https://github.com/acl2/acl2/archive/8.3.tar.gz"
   sha256 "45eedddb36b2eff889f0dba2b96fc7a9b1cf23992fcfdf909bc179f116f2c5ea"
   license "BSD-3-Clause"
-  revision 6
+  revision 7
 
   bottle do
     sha256 big_sur:  "16bf4f05647196031a70cb9ce96762b69fe48afe4b1ab65dfae5f93573c93430"
