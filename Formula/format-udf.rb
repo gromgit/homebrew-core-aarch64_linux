f6edec7a6721c8c4079fac4e266a3bc740c264fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-29 02:26:47 +0000

    format-udf: add 1.8.0 bottle.

--- Formula/format-udf.rb
+++ Formula/format-udf.rb
@@ -5,6 +5,10 @@ class FormatUdf < Formula
   sha256 "52854097db9044d729fbd7cff012f4b554df01c15225ee17ec159c71da174c8d"
   license "GPL-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "b108eb2ba15d5bcf6458a29f6f05e57f9629370d4677f4e09930aad406f92da6"
+  end
+
   def install
     bin.install "format-udf.sh" => "format-udf"
   end
