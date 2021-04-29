da25a85fe1e9f21299cfde6bb0b02587c50d38a7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-29 15:20:18 +0000

    picard-tools: add 2.25.3 bottle.

--- Formula/picard-tools.rb
+++ Formula/picard-tools.rb
@@ -10,6 +10,10 @@ class PicardTools < Formula
     strategy :github_latest
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "21713247184c588e95e6d25f06ce101b0b2bb7d80af78fb8988d32188ec6bce7"
+  end
+
   depends_on "openjdk"
 
   def install
