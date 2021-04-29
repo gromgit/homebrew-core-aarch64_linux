d9bd7d7db61a4b76b85d353063a4e42acd9bdd03
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-04-29 08:09:56 +0000

    tssh: add 2.1.0 bottle.

--- Formula/tssh.rb
+++ Formula/tssh.rb
@@ -5,6 +5,13 @@ class Tssh < Formula
   sha256 "e1086f9cba293bfce8492aef6742ed62f0ed99eb61bf1fc5829b6ef35f5ff881"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "46ac1a79cdc2d63683d6b99faa613823cf70de0d6bd3f5274dee4e7d6b1612d4"
+    sha256 cellar: :any_skip_relocation, big_sur:       "81ba6ad8ced1af17b7cc50c53666a1789fd45ac522c700bed8ce74b60ce156da"
+    sha256 cellar: :any_skip_relocation, catalina:      "25c95255b2c535a584697d87a14a1e8c70930d78f21cb056e2697bf79232f400"
+    sha256 cellar: :any_skip_relocation, mojave:        "d4b7738f0b5cc63bfcbca2594bbd1514843f290649aea95880b223f71ba7ab9d"
+  end
+
   depends_on "go" => :build
 
   def install
