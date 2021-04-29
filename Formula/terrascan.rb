e2f75a0d7d66722d5a8a30b27879d5c9b5846ab0
rui <rui@chenrui.dev>
2021-04-29 18:19:07 +0000

    terrascan 1.5.1
    * terrascan 1.5.1
    * terrascan: add patch
      Fixes version error.
    
    Closes #76294.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/terrascan.rb
+++ Formula/terrascan.rb
@@ -1,8 +1,8 @@
 class Terrascan < Formula
   desc "Detect compliance and security violations across Infrastructure as Code"
   homepage "https://www.accurics.com/products/terrascan/"
-  url "https://github.com/accurics/terrascan/archive/v1.5.0.tar.gz"
-  sha256 "e969960d9748e50125359097f5004c34ccf72e638dd06d10204a62468d57b260"
+  url "https://github.com/accurics/terrascan/archive/v1.5.1.tar.gz"
+  sha256 "0ef2490c711fc089e05a926ba1935be62f60bd20b7226906e207382aaae48f84"
   license "Apache-2.0"
   head "https://github.com/accurics/terrascan.git"
 
@@ -14,6 +14,12 @@ class Terrascan < Formula
   end
 
   depends_on "go" => :build
+
+  # Fixes version, remove in next release.
+  patch do
+    url "https://github.com/accurics/terrascan/commit/d8fd9c4bae5b12ffbe8d7c7e1a1d67042dfd8edf.patch?full_index=1"
+    sha256 "994c4bc35899286edf48baad868b7482e5dc1090ad8a7dceffe25e4df438ca2f"
+  end
 
   def install
     system "go", "build", *std_go_args, "./cmd/terrascan"
