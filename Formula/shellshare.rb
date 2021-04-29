96e5b6a1ca38ab19a6723b6dbb05caff38527c87
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-29 01:29:45 +0000

    shellshare: add 1.1.0 bottle.

--- Formula/shellshare.rb
+++ Formula/shellshare.rb
@@ -5,6 +5,10 @@ class Shellshare < Formula
   sha256 "0a102c863f60402ab48908563edde38450add0ae02454360fa94065824a61907"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "cc656c34170ea6d6e3b9573a0077403840e9a84e9ea764d6e4add6ad9397f9e2"
+  end
+
   def install
     bin.install "public/bin/shellshare"
   end
