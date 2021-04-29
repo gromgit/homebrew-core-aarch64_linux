488ea4078c17f68738fa9ae159ae47836cc06b57
lrs <anwu@douyu.tv>
2021-04-29 12:03:12 +0800

    tssh 2.1.0 (new formula)
    tssh 2.1.0
    
    Closes #76243.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/tssh.rb
@@ -0,0 +1,20 @@
+class Tssh < Formula
+  desc "SSH Lightweight management tools"
+  homepage "https://github.com/luanruisong/tssh"
+  url "https://github.com/luanruisong/tssh/archive/refs/tags/2.1.0.tar.gz"
+  sha256 "e1086f9cba293bfce8492aef6742ed62f0ed99eb61bf1fc5829b6ef35f5ff881"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-X main.version=#{version}")
+  end
+
+  test do
+    output_v = shell_output("#{bin}/tssh -v")
+    assert_match "version #{version}", output_v
+    output_e = shell_output("#{bin}/tssh -e")
+    assert_match "TSSH_HOME", output_e
+  end
+end
