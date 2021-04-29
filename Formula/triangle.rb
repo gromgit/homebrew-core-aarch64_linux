ea0aabe9f6f87ff2c195ab676c35005d80eab7ec
rui <rui@chenrui.dev>
2021-04-29 14:12:43 +0000

    triangle 1.2.0
    * triangle 1.2.0
    * fix the build
    
    Closes #76280.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/triangle.rb
+++ Formula/triangle.rb
@@ -1,8 +1,8 @@
 class Triangle < Formula
   desc "Convert images to computer generated art using Delaunay triangulation"
   homepage "https://github.com/esimov/triangle"
-  url "https://github.com/esimov/triangle/archive/v1.1.2.tar.gz"
-  sha256 "6050c25f32e8ab94919b9a8320a09023cb36a97e797ec22f73c2dfaf7a774d6a"
+  url "https://github.com/esimov/triangle/archive/v1.2.0.tar.gz"
+  sha256 "358798d2a35bfdd0e75887638e70773d696fd0692ee33257a796e3855613296e"
   license "MIT"
 
   bottle do
@@ -14,9 +14,14 @@ class Triangle < Formula
 
   depends_on "go" => :build
 
+  # fix the build, remove in the next release
+  patch do
+    url "https://github.com/chenrui333/triangle/commit/1b77095.patch?full_index=1"
+    sha256 "32f15bd419ca122ae6e5d38793d1f3e4446569327ad05281f1a499226faf8f73"
+  end
+
   def install
     system "go", "build", "-mod=vendor", "-o", "#{bin}/triangle", "./cmd/triangle"
-    prefix.install_metafiles
   end
 
   test do
