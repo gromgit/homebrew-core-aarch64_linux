f9ccf1b8fe108b31679ef4ae33868d8132640e3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-29 04:06:46 +0000

    docfx: add 2.56.7 bottle.

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -5,6 +5,10 @@ class Docfx < Formula
   sha256 "95f96be280998e7ea6581d418c9b0d99dffb0c86bcca7f1dd257062163e0a79c"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "e1d454227a7f161c22bac66a7e29923dd20aaebe3492f84fa44be951683a3e81"
+  end
+
   depends_on arch: :x86_64
   depends_on "mono"
 
