fd58f6d620298a2c87841cb7f37c483d164552d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-29 02:26:37 +0000

    git-open: add 1.3 bottle.

--- Formula/git-open.rb
+++ Formula/git-open.rb
@@ -5,6 +5,10 @@ class GitOpen < Formula
   sha256 "a1217e9b0a76382a96afd33ecbacad723528ec1116381c22a17cc7458de23701"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "b9a415294b615c483c64ea122544f80ccde480db013ced6c62b558c5cdabff74"
+  end
+
   def install
     bin.install "git-open.sh" => "git-open"
   end
