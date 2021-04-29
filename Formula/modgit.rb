4a7f378c88e93ec893b01bcf69967bb947645417
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-29 02:26:04 +0000

    modgit: add 1.1.0 bottle.

--- Formula/modgit.rb
+++ Formula/modgit.rb
@@ -4,6 +4,10 @@ class Modgit < Formula
   url "https://github.com/jreinke/modgit/archive/v1.1.0.tar.gz"
   sha256 "9d279c370eee29f54017ca20cf543efda87534bd6a584e7c0f489bbf931dccb8"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "0428f040a4b3f361834936dfc098f2ba08f979b81d9b6fc6b7cf6b401e5c308a"
+  end
+
   def install
     bin.install "modgit"
   end
