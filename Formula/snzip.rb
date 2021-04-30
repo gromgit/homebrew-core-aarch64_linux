366a2b307818c7f5a7350e80fb4a5fc4250ed97e
Bo Anderson <mail@boanderson.me>
2021-04-30 13:56:02 +0100

    snzip: migrate from Bintray

--- Formula/snzip.rb
+++ Formula/snzip.rb
@@ -1,7 +1,7 @@
 class Snzip < Formula
   desc "Compression/decompression tool based on snappy"
   homepage "https://github.com/kubo/snzip"
-  url "https://bintray.com/artifact/download/kubo/generic/snzip-1.0.4.tar.gz"
+  url "https://github.com/kubo/snzip/releases/download/1.0.4/snzip-1.0.4.tar.gz"
   sha256 "a45081354715d48ed31899508ebed04a41d4b4a91dca37b79fc3b8ee0c02e25e"
   license "BSD-2-Clause"
   revision 2
