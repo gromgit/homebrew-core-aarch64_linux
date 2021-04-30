167e34a6f834db9b6404fd8729ef2b58423a6c30
Harry Mallon <harry.mallon@codex.online>
2021-04-30 11:04:02 +0100

    imath: new formula
    Closes #76325.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/imath.rb
@@ -0,0 +1,39 @@
+class Imath < Formula
+  desc "Library of 2D and 3D vector, matrix, and math operations"
+  homepage "https://www.openexr.com/"
+  url "https://github.com/AcademySoftwareFoundation/Imath/archive/refs/tags/v3.0.1.tar.gz"
+  sha256 "9cd984bb6b0a9572dd4a373b1fab60bc4c992a52ec5c68328fe0f48f194ba3c0"
+  license "BSD-3-Clause"
+
+  depends_on "cmake" => :build
+
+  conflicts_with "ilmbase",
+    because: "imath replaces ilmbase and installs conflicting libraries"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~'EOS'
+      #include <ImathRoots.h>
+      #include <algorithm>
+      #include <iostream>
+
+      int main(int argc, char *argv[])
+      {
+        double x[2] = {0.0, 0.0};
+        int n = IMATH_NAMESPACE::solveQuadratic(1.0, 3.0, 2.0, x);
+
+        if (x[0] > x[1])
+          std::swap(x[0], x[1]);
+
+        std::cout << n << ", " << x[0] << ", " << x[1] << "\n";
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "-I#{include}/Imath", "-o", testpath/"test", "test.cpp"
+    assert_equal "2, -2, -1\n", shell_output("./test")
+  end
+end
