f62f6b2505e99aa078b1af465b50ede91fdde873
miccal <miccal.matthews@gmail.com>
2021-04-30 12:03:13 +0800

    lighttpd: revision bump

--- Formula/lighttpd.rb
+++ Formula/lighttpd.rb
@@ -4,6 +4,7 @@ class Lighttpd < Formula
   url "https://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.59.tar.xz"
   sha256 "fb953db273daef08edb6e202556cae8a3d07eed6081c96bd9903db957d1084d5"
   license "BSD-3-Clause"
+  revision 1
 
   livecheck do
     url "https://download.lighttpd.net/lighttpd/releases-1.4.x/"
