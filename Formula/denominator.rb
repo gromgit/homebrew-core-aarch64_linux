331d81dfe9dde336551455bf3fc550ba38204c93
Bo Anderson <mail@boanderson.me>
2021-04-30 13:53:57 +0100

    denominator: migrate from Bintray

--- Formula/denominator.rb
+++ Formula/denominator.rb
@@ -1,7 +1,7 @@
 class Denominator < Formula
   desc "Portable Java library for manipulating DNS clouds"
   homepage "https://github.com/Netflix/denominator/tree/v4.7.1/cli"
-  url "https://bintray.com/artifact/download/netflixoss/maven/com/netflix/denominator/denominator-cli/4.7.1/denominator-cli-4.7.1-fat.jar"
+  url "https://search.maven.org/remotecontent?filepath=com/netflix/denominator/denominator-cli/4.7.1/denominator-cli-4.7.1-fat.jar"
   sha256 "f2d09aaebb63ccb348dcba3a5cc3e94a42b0eae49e90ac0ec2b0a14adfbe5254"
   license "Apache-2.0"
 
