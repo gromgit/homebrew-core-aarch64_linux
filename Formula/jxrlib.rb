783e6c7b6759c5a1b8d21f532cc48b3318676d23
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-30 10:06:48 +0000

    jxrlib: update 1.1_1 bottle.

--- Formula/jxrlib.rb
+++ Formula/jxrlib.rb
@@ -7,14 +7,10 @@ class Jxrlib < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "54cfa4c3ba6f80ae22012ceb65b6c50340489d6a8cf99b0abd26a93e4be16628"
-    sha256 cellar: :any_skip_relocation, big_sur:       "8cd0235dd1a6537562f6c7939ffd99bab6568eab5b72326ef6670ee1299fe5dc"
-    sha256 cellar: :any_skip_relocation, catalina:      "51c5db55544b873ddbb50eb5681ab3d6ce1552df452bc31f3865d33e36176888"
-    sha256 cellar: :any_skip_relocation, mojave:        "923729f8257b2ff225affe4a2a822f2dd40e799ca45be9ac423f04b5e4e603f0"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "6d24053d591022594ab92f880c56da4057a9e4f8d8ecb6f942e3558900385def"
-    sha256 cellar: :any_skip_relocation, sierra:        "0eb8602ab389d9fff5bce803b085af322309592dc41aa0902b223e353a9f8abc"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "47c308b47ec8227d813a21c8092b32ada9b7f862aef102bf619d6bb19f0144e1"
-    sha256 cellar: :any_skip_relocation, yosemite:      "0dae977caf9e34289c9dd09f7e12bdf7158ccc42d9fa2dc00b4164b82c1caf3f"
+    sha256 cellar: :any, arm64_big_sur: "5fed25d3e908ae5f825c8ef875363e5f191c8ba5e932ee4675747874c33f570a"
+    sha256 cellar: :any, big_sur:       "a2bd992f147e427f8021e0f12a1f228887a890b23cbab7cd734c3d016fb90dae"
+    sha256 cellar: :any, catalina:      "33134735fa04107eabadae73a3dffc8cabb8bcadf60dabe68321461366877c01"
+    sha256 cellar: :any, mojave:        "44b1eef414a2ed12f6a647dcdb0b4d01e55445188ea7e835eaa3968f4d7ea0cf"
   end
 
   depends_on "cmake" => :build
