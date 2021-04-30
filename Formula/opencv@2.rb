9565e96979b94cc2fb77dfba696336b4ac081ff1
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-04-30 20:31:42 +0100

    opencv@2: revision bump (openexr 3.0.1)

--- Formula/opencv@2.rb
+++ Formula/opencv@2.rb
@@ -4,7 +4,7 @@ class OpencvAT2 < Formula
   url "https://github.com/opencv/opencv/archive/2.4.13.7.tar.gz"
   sha256 "192d903588ae2cdceab3d7dc5a5636b023132c8369f184ca89ccec0312ae33d0"
   license "BSD-3-Clause"
-  revision 11
+  revision 12
 
   bottle do
     rebuild 1
