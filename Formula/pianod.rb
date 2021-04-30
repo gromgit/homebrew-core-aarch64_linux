c1b1ed68b35108e0f87449a11c15866894ca0438
Daniel Nachun <daniel.nachun@gmail.com>
2021-04-30 05:00:23 -0700

    pianod: fix build for Linux (#76320)

--- Formula/pianod.rb
+++ Formula/pianod.rb
@@ -3,6 +3,7 @@ class Pianod < Formula
   homepage "https://deviousfish.com/pianod/"
   url "https://deviousfish.com/Downloads/pianod2/Devel/pianod2-301.tar.gz"
   sha256 "d6fa01d786af65fe3b4e6f4f97fa048db6619b9443e23f655d3ea8ab4766caee"
+  license "MIT"
   revision 1
 
   bottle do
@@ -21,6 +22,8 @@ class Pianod < Formula
   on_linux do
     # pianod uses avfoundation on macOS, ffmpeg on Linux
     depends_on "ffmpeg"
+    depends_on "gnutls"
+    depends_on "libbsd"
   end
 
   def install
