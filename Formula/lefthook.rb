c89e3ddf1a3c16b4b4668a0e0bee95433eb93550
Svyatoslav Kryukov <s.g.kryukov@yandex.ru>
2021-04-30 16:02:25 +0300

    lefthook 0.7.4
    Closes #76335.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/lefthook.rb
+++ Formula/lefthook.rb
@@ -1,8 +1,8 @@
 class Lefthook < Formula
   desc "Fast and powerful Git hooks manager for any type of projects"
   homepage "https://github.com/evilmartians/lefthook"
-  url "https://github.com/evilmartians/lefthook/archive/refs/tags/v0.7.3.tar.gz"
-  sha256 "643bb3d2a1b67d865285a9cadcad11f2588ba284570ab9924c8e3d2e902301ca"
+  url "https://github.com/evilmartians/lefthook/archive/refs/tags/v0.7.4.tar.gz"
+  sha256 "f4247621abd7ee9860d8c18d6357077978f09e439bde9ff16462e3e743a93ee9"
   license "MIT"
 
   bottle do
