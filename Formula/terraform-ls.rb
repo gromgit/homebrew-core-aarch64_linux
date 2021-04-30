60ecc8c8f1278f6a7aa93fd8b31b9a4ef13c0329
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-30 19:10:54 +0000

    terraform-ls: update 0.16.1 bottle.

--- Formula/terraform-ls.rb
+++ Formula/terraform-ls.rb
@@ -12,10 +12,10 @@ class TerraformLs < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "31d82105d4de66fecd97f3f144534ab48fb7a2352c55c4223bfd260a92ea86a0"
-    sha256 cellar: :any_skip_relocation, big_sur:       "35872acf6aad3fda0e752178d51d76cf01fb83e6ec72e465dd3d4da1b255f2b1"
-    sha256 cellar: :any_skip_relocation, catalina:      "aaa06010ec3cba3a02b785d6044869dd33f4b7d0e7e863998cd702b211f466d4"
-    sha256 cellar: :any_skip_relocation, mojave:        "74116b2a90f58b1bd6c0d4bda96dbca250bbd455435b294b9923b477eef2699e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "18de6f811fbb10fa27afe5fddcc76711b707128a6708052eaa52676861b6c43e"
+    sha256 cellar: :any_skip_relocation, big_sur:       "f4d030c3b8834e0ceb2a94c4ef2e63755c5f3620e444ae51d66da527ca5d0d61"
+    sha256 cellar: :any_skip_relocation, catalina:      "44b60512f79644f4c5ba341c7a9d2770118d66e42697db5c8537d38ed5bffcf0"
+    sha256 cellar: :any_skip_relocation, mojave:        "1f509b7dfed66791a5feb37eaad738d503b29404df324ea0f78536e9992fd53c"
   end
 
   depends_on "go" => :build
