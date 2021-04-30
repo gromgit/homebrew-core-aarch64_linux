9abac1199eb41bc5daeae06d03578d65c6c01b50
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-30 05:09:37 +0000

    buildkit: update 0.8.3 bottle.

--- Formula/buildkit.rb
+++ Formula/buildkit.rb
@@ -13,10 +13,10 @@ class Buildkit < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "481ce58bfba185e6614761e4db47cfd5bad298c7d1befcda37a38ed88c98e2b0"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f0136996daa0dba57602f1cd2a662c21e24927feb758b3269c2be81533b87453"
-    sha256 cellar: :any_skip_relocation, catalina:      "b425bf61e8799358958db28a875f16069f5278d066f934c624bec1ace320a24e"
-    sha256 cellar: :any_skip_relocation, mojave:        "04805349df94e16f5cb3e59e14c2bdd29d8b72b266ee11119934a677b521e068"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "dfc1a7eb1b24c248f64d4925d4c52f4d88d6ec6c19502a9677becbf95a534dee"
+    sha256 cellar: :any_skip_relocation, big_sur:       "3d8d9b69f4757bcfff266f90086a607d428b6dce9ef18fe19fc7d2dc9f1b9169"
+    sha256 cellar: :any_skip_relocation, catalina:      "c8f6f789e3ac8141f0229e9145f251093e06d072ac10110406844476a874a1b2"
+    sha256 cellar: :any_skip_relocation, mojave:        "4574312ef4f5f9c6d6d9beb6049c77cad27f316609f93ad556f969bed317d425"
   end
 
   depends_on "go" => :build
