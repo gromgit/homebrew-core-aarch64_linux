46706a90fd0bf798558e611fed6e96123de38ffc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-30 06:06:52 +0000

    triangle: update 1.2.0 bottle.

--- Formula/triangle.rb
+++ Formula/triangle.rb
@@ -6,10 +6,10 @@ class Triangle < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "24e275d2b33f33b26fed232868d1485ce5a1d4a882d3710b715b484919a57fcd"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ad745cc5ef31c405301232d11cb75b768a001293a9d150704a4c955c8f38a698"
-    sha256 cellar: :any_skip_relocation, catalina:      "34e735d76b7417fa426161d4ed2b27b891e0f040070ccef3d6f430a637b9c453"
-    sha256 cellar: :any_skip_relocation, mojave:        "66ea6247ba2d105e5953814cdb9746ea2264a22689d879e16ace3c18e42f92f1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b0827e2034c1338c8bc7d2f8175aff11fb15aff32c4c08582f3f511d6104c4ed"
+    sha256 cellar: :any_skip_relocation, big_sur:       "ed83fbdc0021f6550e878586af042b422d594c77c7dbb33c6e20ae4d098157cd"
+    sha256 cellar: :any_skip_relocation, catalina:      "fe347e5f26966d9cdaefefc36c3d8e8584b4bc25edb30b71a5c90e06dc615534"
+    sha256 cellar: :any_skip_relocation, mojave:        "2799817d0e4b094a7dd98549d94a3372193cab26a9d2d9d5059fcff0e520faa2"
   end
 
   depends_on "go" => :build
