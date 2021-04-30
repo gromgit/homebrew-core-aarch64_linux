aac37670373acaa86e3ce88d12bddf0bbb9d4a3a
rui <rui@chenrui.dev>
2021-04-30 04:13:24 +0000

    buildkit 0.8.3
    Closes #76316.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/buildkit.rb
+++ Formula/buildkit.rb
@@ -2,8 +2,8 @@ class Buildkit < Formula
   desc "Сoncurrent, cache-efficient, and Dockerfile-agnostic builder toolkit"
   homepage "https://github.com/moby/buildkit"
   url "https://github.com/moby/buildkit.git",
-      tag:      "v0.8.2",
-      revision: "9065b18ba4633c75862befca8188de4338d9f94a"
+      tag:      "v0.8.3",
+      revision: "81c2cbd8a418918d62b71e347a00034189eea455"
   license "Apache-2.0"
   head "https://github.com/moby/buildkit.git"
 
