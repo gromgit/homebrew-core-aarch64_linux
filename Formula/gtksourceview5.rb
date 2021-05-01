baea1e875cb088a000928f51cc3352d3609a2c3f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-05-01 05:22:38 +0000

    gtksourceview5: add 5.0.0 bottle.

--- Formula/gtksourceview5.rb
+++ Formula/gtksourceview5.rb
@@ -10,6 +10,13 @@ class Gtksourceview5 < Formula
     regex(/gtksourceview[._-]v?(5\.([0-8]\d*?)?[02468](?:\.\d+)*?)\.t/i)
   end
 
+  bottle do
+    sha256 arm64_big_sur: "195195221b285f8158d2a261d7e7b6b081a702aeac2f868e0ba7d33300589836"
+    sha256 big_sur:       "ed1b0232d384c9b9aca135cacb412a53f1ea08a6c964f0911dbc0af15481b5d1"
+    sha256 catalina:      "f2be3a8df55eebb30a6f873631bbb4142f51c0f99a5df55a9ba3c26ef0dcda07"
+    sha256 mojave:        "2c31df3348cd823f136f70a4bf6f57a16a3b95ad819a5113aea83450f83896d1"
+  end
+
   depends_on "gobject-introspection" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
