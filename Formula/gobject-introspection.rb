994f5a21b68091844cc3b253962a713ac43a60d3
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2021-05-01 20:31:17 +0100

    gobject-introspection: fix library search on Silicon
    Closes #76383.
    
    Signed-off-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gobject-introspection.rb
+++ Formula/gobject-introspection.rb
@@ -6,6 +6,7 @@ class GobjectIntrospection < Formula
   url "https://download.gnome.org/sources/gobject-introspection/1.68/gobject-introspection-1.68.0.tar.xz"
   sha256 "d229242481a201b84a0c66716de1752bca41db4133672cfcfb37c93eb6e54a27"
   license all_of: ["GPL-2.0-or-later", "LGPL-2.0-or-later", "MIT"]
+  revision 1
 
   bottle do
     sha256 arm64_big_sur: "41f7a223a257f2da33389b61128ece298cde0c63a036ee45f78c9b65ac71ea3f"
@@ -30,6 +31,11 @@ class GobjectIntrospection < Formula
         revision: "499ac89f8a9ad17d250e907f74912159ea216416"
   end
 
+  patch do
+    url "https://gitlab.gnome.org/tschoonj/gobject-introspection/-/commit/a7be304478b25271166cd92d110f251a8742d16b.diff"
+    sha256 "740c9fba499b1491689b0b1216f9e693e5cb35c9a8565df4314341122ce12f81"
+  end
+
   def install
     ENV["GI_SCANNER_DISABLE_CACHE"] = "true"
     inreplace "giscanner/transformer.py", "/usr/share", "#{HOMEBREW_PREFIX}/share"
@@ -39,7 +45,9 @@ class GobjectIntrospection < Formula
 
     mkdir "build" do
       system "meson", *std_meson_args,
-        "-Dpython=#{Formula["python@3.9"].opt_bin}/python3", ".."
+        "-Dpython=#{Formula["python@3.9"].opt_bin}/python3",
+        "-Dextra_library_paths=#{HOMEBREW_PREFIX}/lib",
+        ".."
       system "ninja", "-v"
       system "ninja", "install", "-v"
       bin.find { |f| rewrite_shebang detected_python_shebang, f }
