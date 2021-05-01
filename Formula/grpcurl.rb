8deb10429e1deb5ebadc911627af7f9818aea9ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-01 01:32:22 +0000

    grpcurl: update 1.8.1 bottle.

--- Formula/grpcurl.rb
+++ Formula/grpcurl.rb
@@ -6,10 +6,10 @@ class Grpcurl < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6fff430f23353c620fec2bf5994759846139337f1aaf379cf3520eff089e0b35"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ce5d605bad57e29e0cfdb0c7abacf7557c37709487da1568b3b61c6ae78dac5e"
-    sha256 cellar: :any_skip_relocation, catalina:      "ffe4d86a9401a8f8967400709fcda90c88a7bff84d90b429639ef3674920097d"
-    sha256 cellar: :any_skip_relocation, mojave:        "b1d296a3c4a2a3eb668cb84330473cf30d2960929d5af7578c8e2716d600c538"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "5551aeee38f48b7a3c31117d0c5c1791f1a71a62c96d0231dc8da712f95ad6f2"
+    sha256 cellar: :any_skip_relocation, big_sur:       "be03266644292007336a1927e1a3c32c26132c10e6cdddf24f2be2db723616ad"
+    sha256 cellar: :any_skip_relocation, catalina:      "cc7aa37de783837e837c441575df7f6b5ef169b83a5017b2b62fc1ce4c4c09c3"
+    sha256 cellar: :any_skip_relocation, mojave:        "a4cf31d2358f4d8e36a695a2a9c401672cd7d80f0be54dda2d4d7fb62702b49c"
   end
 
   depends_on "go" => :build
