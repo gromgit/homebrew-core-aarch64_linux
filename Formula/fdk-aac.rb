76f68f9791b326fba8dd3dcddaf1919a598ddda0
Thierry Moisan <thierry.moisan@gmail.com>
2021-05-01 12:28:33 -0400

    fdk-aac 2.0.2
    * fdk-aac 2.0.2
    * Update fdk-aac.rb
    
    Closes #76375.
    
    Signed-off-by: Nanda H Krishna <me@nandahkrishna.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/fdk-aac.rb
+++ Formula/fdk-aac.rb
@@ -1,8 +1,9 @@
 class FdkAac < Formula
   desc "Standalone library of the Fraunhofer FDK AAC code from Android"
   homepage "https://sourceforge.net/projects/opencore-amr/"
-  url "https://downloads.sourceforge.net/project/opencore-amr/fdk-aac/fdk-aac-2.0.1.tar.gz"
-  sha256 "840133aa9412153894af03b27b03dde1188772442c316a4ce2a24ed70093f271"
+  url "https://downloads.sourceforge.net/project/opencore-amr/fdk-aac/fdk-aac-2.0.2.tar.gz"
+  sha256 "c9e8630cf9d433f3cead74906a1520d2223f89bcd3fa9254861017440b8eb22f"
+  license "Apache-2.0"
 
   bottle do
     sha256 cellar: :any, arm64_big_sur: "90dee33598510f7bca41e3da463e18f5400d71639af4394668ade28b89dc6175"
